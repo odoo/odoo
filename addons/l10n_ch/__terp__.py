e7cdee1cf6c33619eb0811b8b8a17a1b6f953b39
ced <>
2006-12-08 08:04:24 +0000

    Update l10n_ch
    bzr revid: ced-1147b05938567e590a81984fb012d257bee74521

--- addons/l10n_ch/__terp__.py
+++ addons/l10n_ch/__terp__.py
@@ -7,11 +7,10 @@
 	"author" : "Camptocamp",
 	"category" : "Localisation/Europe",
 	"website": "http://www.tinyerp.com",
-	"depends" : ["base", "account"],#l10n_ch
-	"init_xml" : [],
-	"demo_xml" : [],#"zip_code_default.xml"
+	"depends" : ["base", "account"],
+	"init_xml" : ["zip_code_default.xml"],
+	"demo_xml" : ["vaudtax_data_demo.xml"],
 	"update_xml" : ["vaudtax_data.xml","account_vat.xml","base_config.xml","account_config.xml"],
-	#"update_xml" : ["vaudtax_data.xml"],
 	"active": False,
 	"installable": True
 }
