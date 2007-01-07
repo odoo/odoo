3af6ec6aa7cbb9c5ae2a99e8e21f02a771924916
pinky <>
2007-01-07 23:34:36 +0000

    RL2
    bzr revid: pinky-520e58b1b108f297ba250cddc95dcd6effa8c3fb

---
+++ bin/reportlab/extensions/__init__.py
@@ -0,0 +1,7 @@
+#Copyright ReportLab Europe Ltd. 2000-2004
+#see license.txt for license details
+#history http://www.reportlab.co.uk/cgi-bin/viewcvs.cgi/public/reportlab/trunk/reportlab/extensions/__init__.py
+__version__=''' $Id: __init__.py 2385 2004-06-17 15:26:05Z rgbecker $ '''
+__doc__="""
+"""
+# No usable content. Do not add to this file!
