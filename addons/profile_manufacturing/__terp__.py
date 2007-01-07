5e48225be251c9f3244f56db99d4e7a09d406eb3
pinky <>
2007-01-07 23:48:44 +0000

    Removed board_* for C2C
    bzr revid: pinky-e76bc9c06641176cdb3f390944ab93a8875e231a

--- addons/profile_manufacturing/__terp__.py
+++ addons/profile_manufacturing/__terp__.py
@@ -3,7 +3,7 @@
 	"version":"1.0",
 	"author":"Tiny",
 	"category":"Profile",
-	"depends":["mrp", "crm", "sale", "delivery", "board_sale"],
+	"depends":["mrp", "crm", "sale", "delivery"],
 	"demo_xml":[],
 	"update_xml":[],
 	"active":False,
