3ca866031b8430ef548d32ce2e1c02ffcba2fcf2
ced <>
2007-01-19 07:13:51 +0000

    Set version 4.1.1 and improve report_intrastat
    bzr revid: ced-c7f41f4c364f423db4ef8a87f24ca75e637b415a

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from stat import ST_MODE
 opj = os.path.join
 
 name = 'tinyerp-server'
-version = '4.1.0'
+version = '4.1.1'
 
 # get python short version
 py_short_version = '%s.%s' % sys.version_info[:2]
