6b621cbb8d2bc84a046e8a63a8fa417e49f7a662
ced <>
2007-01-26 12:51:51 +0000

    REPORT_STOCK: add report on stock by stock location and production lots
    bzr revid: ced-ad9dae55bc0b2f4b08b6d28ec89be438b90f320b

---
+++ addons/report_stock/__terp__.py
@@ -0,0 +1,14 @@
+{
+	"name" : "Stock report",
+	"version" : "1.0",
+	"author" : "Tiny",
+	"website" : "http://tinyerp.com/",
+	"depends" : ["stock", "product",],
+	"category" : "Generic Modules/Inventory Control",
+	"description" : "A module that adds new reports based on the stock module.",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : ["report_stock_view.xml",],
+	"active": False,
+	"installable": True
+}
