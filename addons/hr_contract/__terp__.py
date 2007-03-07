337cfa511cbc6320bb40563905a1108707071e01
pinky <>
2007-03-07 18:10:55 +0000

    New module
    bzr revid: pinky-18bde0a42a7ccf5befdb18a4fe02ea8a99cbe4d7

---
+++ addons/hr_contract/__terp__.py
@@ -0,0 +1,14 @@
+{
+	"name" : "Human Resources Contracts",
+	"version" : "0.1",
+	"author" : "Tiny",
+	"category" : "Generic Modules/Human Resources",
+	"website" : "http://tinyerp.com/module_hr.html",
+	"depends" : ["hr"],
+	"module": "",
+	"init_xml" : ["hr_contract_data.xml"],
+	"demo_xml" : [],
+	"update_xml" : ["hr_contract_view.xml"],
+	"active": False,
+	"installable": True
+}
--- addons/hr_timesheet_sheet/__terp__.py
+++ addons/hr_timesheet_sheet/__terp__.py
@@ -5,7 +5,7 @@
 	"category" : "Generic Modules/Human Resources",
 	"website" : "http://tinyerp.com/module_hr.html",
 	"description": "Timesheet by sheets",
-	"depends" : ["hr_timesheet", "hr_timesheet_project"],
+	"depends" : ["hr_timesheet", "hr_timesheet_project", "hr_timesheet_invoice"],
 	"init_xml" : [],
 	"demo_xml" : [],
 	"update_xml" : ["hr_timesheet_sheet_view.xml", "hr_timesheet_workflow.xml"],
