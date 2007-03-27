7f5bf76542521e040810f86590b2ab7e97517db0
ced <>
2007-03-27 07:57:36 +0000

    MULTI_COMPANY_ACCOUNT: new module
    bzr revid: ced-42df3813744178c24958034237683e818300a823

---
+++ addons/multi_company_account/__terp__.py
@@ -0,0 +1,12 @@
+{
+	"name" : "Multi company for account module",
+	"version" : "0.1",
+	"author" : "Tiny",
+	"depends" : ["account",],
+	"category" : "Generic Modules/Base",
+	"init_xml" : [],
+	"demo_xml" : ["multi_company_account_demo.xml",],
+	"update_xml" : ["account_view.xml"],
+	"active": False,
+	"installable": True
+}
