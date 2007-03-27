bc5f48318861c5cf077aaf9beb2d8df5291a251c
ced <>
2007-03-27 08:00:55 +0000

    MULTI_COMPANY_ACCOUNT: add demo xml and init file
    bzr revid: ced-8a9426cfea4bbb41b005e41c9aaec8e1dcb717ba

--- addons/multi_company/__init__.py
+++ addons/multi_company/__init__.py
@@ -1,3 +1,2 @@
-import account
 import project
 import hr
--- addons/multi_company_account/__terp__.py
+++ addons/multi_company_account/__terp__.py
@@ -6,7 +6,7 @@
 	"category" : "Generic Modules/Base",
 	"init_xml" : [],
 	"demo_xml" : ["multi_company_account_demo.xml",],
-	"update_xml" : ["account_view.xml"],
+	"update_xml" : ["multi_company_account_view.xml"],
 	"active": False,
 	"installable": True
 }
