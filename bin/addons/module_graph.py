d4d0e528ac7104e21eca7e9087c5e40dc6b37ecf
ced <>
2007-04-21 13:30:47 +0000

    KERNEL: fix to display all installable modules
    bzr revid: ced-6dc061c55512c2687bdb6f21b397f47e07eb1bdd

--- bin/addons/module_graph.py
+++ bin/addons/module_graph.py
@@ -6,7 +6,7 @@ print 'digraph G {'
 for f in os.listdir('.'):
 	if os.path.isfile(os.path.join(f,"__terp__.py")):
 		info=eval(file(os.path.join(f,"__terp__.py")).read())
-		if info.get('active', True):
+		if info.get('installable', True):
 			for name in info['depends']:
 				print '\t%s -> %s;' % (f, name)
 print '}'
