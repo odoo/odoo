38690b0da6c5c04dce6954e397b21aa0b3785302
Fabien Pinckaers <fp@tinyerp.com>
2007-04-22 22:10:18 +0000

    New description for modules
    bzr revid: fp@tinyerp.com-f515b3f28c3a09a8314a80e8eae70a84a070dead

--- addons/sale_crm/__terp__.py
+++ addons/sale_crm/__terp__.py
@@ -5,6 +5,14 @@
 	"website" : "http://tinyerp.com/module_sale.html",
 	"depends" : ["sale", "crm"],
 	"category" : "Generic Modules/Sales & Purchases",
+	"description": """
+	This module adds a shortcut on one or several cases in the CRM.
+	This shortcut allows you to generate a sale order based the selected case.
+	If different cases are open (a list), it generates one sale order by
+	case.
+
+	The case is then closed and linked to the generated sale order.
+	""",
 	"init_xml" : [],
 	"demo_xml" : [],
 	"update_xml" : ["sale_crm_wizard.xml"],
--- addons/scrum/__terp__.py
+++ addons/scrum/__terp__.py
@@ -5,6 +5,28 @@
 	"depends" : ["project"],
 	"category" : "Enterprise Specific Modules/Information Technology",
 	"init_xml" : [],
+	"description": """
+	This modules implements all concepts defined by the scrum project
+	management methodology for IT companies:
+	* Project with sprints, product owner, scrum master
+	* Sprints with reviews, daily meetings, feedbacks
+	* Product backlog
+	* Sprint backlog
+
+	It adds some concepts to the project management module:
+	* Mid-term, long-term roadmaps
+	* Customers/functionnal requests, vs technical ones
+
+	It also create a new reporting:
+	* Burndown chart
+
+	The scrum projects and tasks inherits from the real projects and
+	tasks, so you can continue working on normal tasks that will also
+	include tasks from scrum projects.
+
+	More information on the methodology:
+	* http://controlchaos.com
+	""",
 	"demo_xml" : ["scrum_demo.xml"],
 	"update_xml": ["scrum_view.xml","scrum_report.xml", "scrum_wizard.xml"],
 	"active": False,
