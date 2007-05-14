08aab087a29ab640932db50720103b31253ea47b
ced <>
2007-05-14 06:35:51 +0000

    BASE: set iban to 27
    bzr revid: ced-7a948390a37a9987f5b01fff665968b4b58ddd8f

--- bin/addons/base/res/partner/partner.py
+++ bin/addons/base/res/partner/partner.py
@@ -319,7 +319,7 @@ class res_partner_bank(osv.osv):
 		'name': fields.char('Account Name', size=64, required=True),
 		'sequence': fields.integer('Sequence'),
 		'number': fields.char('Account Number', size=64),
-		'iban': fields.char('Account Iban', size=21), 
+		'iban': fields.char('Account Iban', size=27),
 		'type_id' : fields.many2one('res.partner.bank.type', 'Account Type', required=True),
 		'bank_name': fields.char('Bank Name', size=64, required= True),
 		'bank_code': fields.char('Bank Code', size=64),
