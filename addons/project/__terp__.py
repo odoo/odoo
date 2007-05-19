449a17a032460ee5917f98671cdb7626f20162e9
acysos <>
2007-05-19 09:44:25 +0000

    fix acysos commit
    bzr revid: acysos-298e417fe2caf1471256044cb9cbb6548d048b47

--- addons/project/__terp__.py
+++ addons/project/__terp__.py
@@ -8,7 +8,7 @@
 	"description": "Project management module that track multi-level projects, tasks, works done on tasks, eso. It is able to render planning, order tasks, eso.",
 	"init_xml" : [],
 	"demo_xml" : ["project_demo.xml"],
-#	"update_xml": ["project_data.xml", "project_wizard.xml", "project_view.xml", "project_report.xml", "project_workflow.xml"],
+	"update_xml": ["project_data.xml", "project_wizard.xml", "project_view.xml", "project_report.xml", "project_workflow.xml"],
 	"active": False,
 	"installable": True
 }
