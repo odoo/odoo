7fa5a2daa4dae74984144507a3b881f8bbcbd611
acysos <>
2007-05-19 09:39:26 +0000

    fix acysos commit
    bzr revid: acysos-fc4c460fa818e294e512dadaa85117d4dae8a8d6

--- addons/stock/__terp__.py
+++ addons/stock/__terp__.py
@@ -6,9 +6,7 @@
 	"depends" : ["product", "account"],
 	"category" : "Generic Modules/Inventory Control",
 	"init_xml" : [],
-	"demo_xml" : [
-#		"stock_demo.xml"
-	],
+	"demo_xml" : ["stock_demo.xml"],
 	"update_xml" : ["stock_workflow.xml", "stock_data.xml", "stock_incoterms.xml","stock_wizard.xml", "stock_view.xml", "stock_report.xml", "stock_sequence.xml", "product_data.xml",],
 	"active": False,
 	"installable": True
