5c64b4d14f74f3e8d266742f25c650d259f22d91
ced <>
2007-06-18 11:23:18 +0000

    HR_EXPENSE: fix demo data
    bzr revid: ced-9f75846ec7b124bbab948eb403626a194eaa21e8

--- addons/hr_expense/__terp__.py
+++ addons/hr_expense/__terp__.py
@@ -20,8 +20,7 @@
 	re-invoice your customer's expenses if your work by project.
 	""",
 	"init_xml" : [],
-	"demo_xml" : [],
-	#"demo_xml" : ["hr_expense_demo.xml", "hr.expense.expense.csv"],
+	"demo_xml" : ["hr_expense_demo.xml", "hr.expense.expense.csv"],
 	"update_xml" : ["hr_expense_sequence.xml", "hr_expense_workflow.xml", "hr_expense_view.xml", "hr_expense_report.xml",],
 	"active": False,
 	"installable": True
