27d7749d81998ed6dcd3fbc012456343f64862be
ced <>
2007-06-25 11:59:37 +0000

    L10N_FR: add chart account from sisalp
    bzr revid: ced-84b530b8c8387d00592bba7214449675dd23835a

--- addons/l10n_fr/__terp__.py
+++ addons/l10n_fr/__terp__.py
@@ -1,12 +1,20 @@
+#
+# Plan comptable général pour la France, conforme au 
+# Règlement n° 99-03 du 29 avril 1999
+# Version applicable au 1er janvier 2005.
+# Règlement disponible sur http://comptabilite.erp-libre.info
+# Mise en forme et paramétrage par http://sisalp.fr et http://nbconseil.net
+# 
 {
-	"name" : "France",
+	"name" : "France - Plan comptable Societe - 99-03",
 	"version" : "1.0",
-	"author" : "Tiny",
-	"category" : "Localisation/Account charts",
-	"depends" : ["base", "account", "base_vat", "base_iban"],
+	"author" : "SISalp-NBconseil",
+	"category" : "Localisation//Account charts",
+	"website": "http://erp-libre.info",
+	"depends" : ["base", "account"],
 	"init_xml" : [],
+	"update_xml" : ["types.xml", "plan-99-03_societe.xml", "taxes.xml",],
 	"demo_xml" : [],
-	"update_xml" : ["account_pcg_france.xml"],
 	"active": False,
 	"installable": True
 }
