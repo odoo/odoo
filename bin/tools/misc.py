4457276111adaa6680f375b8a74786f7345ccdb5
ced <>
2007-07-11 15:10:02 +0000

    crm,tools: remove 'Reply-To' header before set an other one
    bzr revid: ced-07b56f48c51b2ac2c7ece29bccee3c2ef4894820

--- bin/tools/misc.py
+++ bin/tools/misc.py
@@ -200,6 +200,7 @@ def email_send(email_from, email_to, subject, body, email_cc=[], email_bcc=[], o
 	msg = MIMEText(body or '', _charset='utf-8')
 	msg['Subject'] = Header(subject.decode('utf8'), 'utf-8')
 	msg['From'] = email_from
+	del msg['Reply-To']
 	if reply_to:
 		msg['Reply-To'] = msg['From']+', '+reply_to
 	msg['To'] = COMMASPACE.join(email_to)
@@ -236,6 +237,7 @@ def email_send_attach(email_from, email_to, subject, body, email_cc=[], email_bc
 
 	msg['Subject'] = Header(subject.decode('utf8'), 'utf-8')
 	msg['From'] = email_from
+	del msg['Reply-To']
 	if reply_to:
 		msg['Reply-To'] = reply_to
 	msg['To'] = COMMASPACE.join(email_to)
