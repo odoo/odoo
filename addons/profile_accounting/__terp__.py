7b6b825dea0eb53ee3ffeaea44264464457c7959
Fabien Pinckaers <fp@tinyerp.com>
2007-08-02 12:20:26 +0000

    Removed unused base dependencies
    bzr revid: fp@tinyerp.com-45f5e8e06796d288dd6f376fa63fbdf378008d29

--- addons/profile_accounting/__terp__.py
+++ addons/profile_accounting/__terp__.py
@@ -3,7 +3,7 @@
 	"version":"1.0",
 	"author":"Tiny",
 	"category":"Profile",
-	"depends":["base", "account","report_analytic","board_account"],
+	"depends":["account","report_analytic","board_account"],
 	"demo_xml":[],
 	"update_xml":[],
 	"active":False,
