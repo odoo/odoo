69c3ef6567e5301e4949630453067ab25e0050cc
Fabien Pinckaers <fp@tinyerp.com>
2007-08-08 07:23:03 +0000

    Added followup in account profile
    bzr revid: fp@tinyerp.com-abbe7eedf71257be82660cac04bd02ebc79e0fca

--- addons/profile_accounting/__terp__.py
+++ addons/profile_accounting/__terp__.py
@@ -3,7 +3,7 @@
 	"version":"1.0",
 	"author":"Tiny",
 	"category":"Profile",
-	"depends":["account","report_analytic","board_account"],
+	"depends":["account","report_analytic","board_account","account_followup"],
 	"demo_xml":[],
 	"update_xml":[],
 	"active":False,
