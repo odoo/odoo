ec7e9635a24c248f2599dbd4da4a9f4e5e99a3f7
ced <>
2007-08-10 14:31:46 +0000

    New release
    bzr revid: ced-852b7405e83c94b4f0da147581830e838e3ccfce

--- bin/release.py
+++ bin/release.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 name = 'tinyerp-server'
-version = '4.1.2'
+version = '4.2.0'
 description = 'Tiny\'s ERP Server'
 long_desc = '''\
 Tiny ERP is a complete ERP and CRM. The main features are accounting (analytic
@@ -12,10 +12,10 @@ customizable reports, and SOAP and XML-RPC interfaces.
 '''
 classifiers = """\
 Development Status :: 5 - Production/Stable
-License :: OSI Approved :: GNU General Public License (GPL)
+License :: OSI Approved :: GNU General Public License Version 2 (GPL-2)
 Programming Language :: Python
 """
-url = 'http://tinyerp.com'
+url = 'http://www.tinyerp.com'
 author = 'Tiny.be'
 author_email = 'info@tiny.be'
-license = 'GPL'
+license = 'GPL-2'
