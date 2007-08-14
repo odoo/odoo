0ad9c7052a9d14e95b69743c9a8b818f501a38f9
ced <>
2007-08-14 14:13:15 +0000

    Improve zip/state/country on partner
    bzr revid: ced-73e94b102437ca84fba20727db3e733c90d8e208

--- bin/addons/base/res/partner/partner.py
+++ bin/addons/base/res/partner/partner.py
@@ -332,8 +332,8 @@ class res_partner_address(osv.osv):
 		'street2': fields.char('Street2', size=128),
 		'zip': fields.char('Zip', change_default=True, size=24),
 		'city': fields.char('City', size=128),
-		'state_id': fields.many2one("res.country.state", 'State', domain="[('country_id','=',country_id)]"),
-		'country_id': fields.many2one('res.country', 'Country'),
+		'state_id': fields.many2one("res.country.state", 'State', change_default=True, domain="[('country_id','=',country_id)]"),
+		'country_id': fields.many2one('res.country', 'Country', change_default=True),
 		'email': fields.char('E-Mail', size=64),
 		'phone': fields.char('Phone', size=64),
 		'fax': fields.char('Fax', size=64),
@@ -422,10 +422,10 @@ class res_partner_bank(osv.osv):
 		'street': fields.char('Street', size=128),
 		'zip': fields.char('Zip', change_default=True, size=24),
 		'city': fields.char('City', size=128),
-		'country_id': fields.many2one('res.country', 'Country'),
-		
+		'country_id': fields.many2one('res.country', 'Country', change_default=True),
+		'state_id': fields.many2one("res.country.state", 'State', change_default=True, domain="[('country_id','=',country_id)]"),
 		'partner_id': fields.many2one('res.partner', 'Partner', required=True, ondelete='cascade', select=True),
-		'state': fields.selection(_bank_type_get, 'Bank type', required=True),
+		'state': fields.selection(_bank_type_get, 'Bank type', required=True, change_default=True),
 	}
 	def fields_get(self, cr, uid, *args):
 		res = super(res_partner_bank, self).fields_get(cr, uid, *args)
