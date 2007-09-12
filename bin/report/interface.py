0c568d1f7473b4028304433a6a74806df23d7229
ced <>
2007-09-12 13:21:01 +0000

    Fix auto init report for new report_sxw function
    bzr revid: ced-c04686fc2d39ec4bb5384d50ca1371d5fbc7bc47

--- bin/report/interface.py
+++ bin/report/interface.py
@@ -189,7 +189,6 @@ from report_sxw import report_sxw
 
 def register_all(db):
 	opj = os.path.join
-	#FIXME: multi-db, quoique... ca init le code donc ok. Enfin, du moins si les modules sont les memes.
 	cr = db.cursor()
 	cr.execute("SELECT * FROM ir_act_report_xml WHERE auto ORDER BY id")
 	result = cr.dictfetchall()
@@ -197,8 +196,11 @@ def register_all(db):
 	for r in result:
 		if netsvc.service_exist('report.'+r['report_name']):
 			continue
-		if r['report_rml'] or r['report_rml_content']:
-			report_sxw('report.'+r['report_name'], r['model'], opj('addons',r['report_rml'] or '/'), header=r['header'])
+		if r['report_rml'] or r['report_rml_content_data']:
+			report_sxw('report.'+r['report_name'], r['model'],
+					opj('addons',r['report_rml'] or '/'), header=r['header'])
 		if r['report_xsl']:
-			report_rml('report.'+r['report_name'], r['model'], opj('addons',r['report_xml']), r['report_xsl'] and opj('addons',r['report_xsl']))
+			report_rml('report.'+r['report_name'], r['model'],
+					opj('addons',r['report_xml']),
+					r['report_xsl'] and opj('addons',r['report_xsl']))
 
