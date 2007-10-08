541443b30c1829c4efa2d32be3e4611be04351c9
ced <>
2007-10-08 14:07:38 +0000

    Raise exception when publish uninstalled module
    bzr revid: ced-bde23520a954b0b4240cf94057165e13d9f867e4

--- addons/base_module_publish/wizard/module_zip.py
+++ addons/base_module_publish/wizard/module_zip.py
@@ -25,24 +25,30 @@ def _zippy(archive, fromurl, path, src=True):
 	return True
 
 def createzip(cr, uid, moduleid, context, b64enc=True, src=True):
-	module_obj=pooler.get_pool(cr.dbname).get('ir.module.module')
-	module_name = module_obj.browse(cr, uid, moduleid).name
-	module_version = module_obj.browse(cr, uid, moduleid).installed_version
+	pool = pooler.get_pool(cr.dbname)
+	module_obj = pool.get('ir.module.module')
+
+	module = module_obj.browse(cr, uid, moduleid)
+
+	if module.state != 'installed':
+		raise wizard.except_wizard('Error',
+				'Can not export module that is not installed!')
 
 	ad = tools.config['addons_path']
-	if os.path.isdir(os.path.join(ad,module_name)):
+	if os.path.isdir(os.path.join(ad, module.name)):
 		archname = StringIO.StringIO('wb')
 		archive = PyZipFile(archname, "w", ZIP_DEFLATED)
-		archive.writepy(os.path.join(ad,module_name))
-		_zippy(archive, ad, module_name, src=src)
+		archive.writepy(os.path.join(ad, module.name))
+		_zippy(archive, ad, module.name, src=src)
 		archive.close()
 		val =archname.getvalue()
 		archname.close()
-	elif os.path.isfile(os.path.join(ad,module_name+'.zip')):
-		val = file(os.path.join(ad,module_name+'.zip'),'rb').read()
+	elif os.path.isfile(os.path.join(ad, module.name + '.zip')):
+		val = file(os.path.join(ad, module.name + '.zip'), 'rb').read()
 	else:
 		raise wizard.except_wizard('Error', 'Could not find the module to export!')
 	if b64enc:
 		val =base64.encodestring(val)
-	return {'module_file':val, 'module_filename': module_name+'-'+(module_version or '0')+'.zip'}
+	return {'module_file':val, 'module_filename': module.name + '-' + \
+			(module.installed_version or '0') + '.zip'}
 
