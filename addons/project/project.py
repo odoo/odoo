5805984b73ccd45440be341e05bda592ed373ae9
Fabien Pinckaers <fp@tinyerp.com>
2007-10-09 05:58:34 +0000

    Project Bugfix
    bzr revid: fp@tinyerp.com-ab567034967aaa265f7be519b18171ffd29160b1

--- addons/project/project.py
+++ addons/project/project.py
@@ -171,7 +171,7 @@ class task(osv.osv):
 	_description = "Task"
 	_date_name = "date_deadline"
 	def _str_get(self, task, level=0, border='***', context={}):
-		return border+' '+task.user_id.name.upper()+(level and (': L'+str(level)) or '')+(' - %.1fh / %.1fh'%(task.effective_hours or 0.0,task.planned_hours))+' '+border+'\n'+ \
+		return border+' '+(task.user_id and task.user_id.name.upper() or '')+(level and (': L'+str(level)) or '')+(' - %.1fh / %.1fh'%(task.effective_hours or 0.0,task.planned_hours))+' '+border+'\n'+ \
 			border[0]+' '+(task.name or '')+'\n'+ \
 			(task.description or '')+'\n\n'
 
