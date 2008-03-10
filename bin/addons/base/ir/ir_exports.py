6a7771f6bbe534581159ed0075434a0d43321f89
Fabien Pinckaers <fp@tinyerp.com>
2008-03-10 07:02:55 +0000

    Better ident
    bzr revid: fp@tinyerp.com-bc1760187fe9c73d512f07a2313de06a5a3f5656

--- bin/addons/base/ir/ir_exports.py
+++ bin/addons/base/ir/ir_exports.py
@@ -33,10 +33,10 @@ from osv import fields,osv
 class ir_exports(osv.osv):
 	_name = "ir.exports"
 	_columns = {
-			'name': fields.char('Export name', size=128),
-			'resource': fields.char('Resource', size=128),
-			'export_fields': fields.one2many('ir.exports.line', 'export_id',
-											 'Export Id'),
+		'name': fields.char('Export name', size=128),
+		'resource': fields.char('Resource', size=128),
+		'export_fields': fields.one2many('ir.exports.line', 'export_id',
+										 'Export Id'),
 	}
 ir_exports()
 
@@ -44,7 +44,7 @@ ir_exports()
 class ir_exports_line(osv.osv):
 	_name = 'ir.exports.line'
 	_columns = {
-			'name': fields.char('Field name', size=64),
-			'export_id': fields.many2one('ir.exports', 'Exportation', select=True),
-			}
+		'name': fields.char('Field name', size=64),
+		'export_id': fields.many2one('ir.exports', 'Exportation', select=True, ondelete='cascade'),
+	}
 ir_exports_line()
