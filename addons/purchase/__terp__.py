0cd72bee703e25532aa0d966f7c2da70d85a0fd5
Mantavya Gajjar <mga@tinyerp.com>
2008-03-28 06:18:03 +0000

    small change remove security file from update and add in to init section and add attribute noupdate="1" in data tag
    bzr revid: mga@tinyerp.com-765a6e4877e9013fbb917e7154160fd37417cac8

--- addons/purchase/__terp__.py
+++ addons/purchase/__terp__.py
@@ -5,12 +5,11 @@
 	"website" : "http://tinyerp.com/module_purchase.html",
 	"depends" : ["base", "account", "stock"],
 	"category" : "Generic Modules/Sales & Purchases",
-	"init_xml" : [],
+	"init_xml" : ["purchase_security.xml"],
 	"demo_xml" : ["purchase_demo.xml", 
 				  #"purchase_unit_test.xml"
 				  ],
 	"update_xml" : [
-		"purchase_security.xml",
 		"purchase_workflow.xml", 
 		"purchase_sequence.xml", 
 		"purchase_data.xml", 
