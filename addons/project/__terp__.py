fe19ac7f1fd5899584b8df1a0788ea2effed199d
Mantavya Gajjar <mga@tinyerp.com>
2008-03-28 06:17:14 +0000

    small change and add attribute noupdate="1" in data tag
    bzr revid: mga@tinyerp.com-54a2d94a448052dcc7c73e8e7f9a12b7be626452

--- addons/project/__terp__.py
+++ addons/project/__terp__.py
@@ -7,14 +7,13 @@
 	"depends" : ["product", "account", 'mrp', 'sale', 'base'],
 	"description": "Project management module that track multi-level projects, tasks, works done on tasks, eso. It is able to render planning, order tasks, eso.",
 	"init_xml" : [],
-	"demo_xml" : ["project_demo.xml"],
+	"demo_xml" : ["project_demo.xml", "project_security.xml"],
 	"update_xml": [
 		"project_data.xml", 
 		"project_wizard.xml", 
 		"project_view.xml", 
 		"project_report.xml", 
 		"project_workflow.xml",
-		"project_security.xml",
 	],
 	"active": False,
 	"installable": True
