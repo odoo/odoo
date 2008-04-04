63c235f775cf681b65fa163e7e3c56998c2ca62c
qdp <>
2008-04-04 10:12:03 +0000

    *added some context in the call of the price_get() of product.product from product.pricelist in order to improve the membership module (pricelists can now use member_price)
    bzr revid: qdp-f89c7b0c542ea8ebbe72b3aa9db0c16c16728940

--- addons/product/pricelist.py
+++ addons/product/pricelist.py
@@ -206,7 +206,7 @@ class product_pricelist(osv.osv):
 					price = currency_obj.compute(cr, uid,
 							price_type.currency_id.id, res['currency_id'],
 							product_obj.price_get(cr, uid, [prod_id],
-								price_type.field)[prod_id], round=False)
+								price_type.field, context)[prod_id], round=False)
 
 				price_limit = price
 
