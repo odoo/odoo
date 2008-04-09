3fb1f89c7b6d7c5b9c48f56253ca12eafadab3a0
Mantavya Gajjar <mga@tinyerp.com>
2008-04-09 06:10:39 +0000

    bzr revid: mga@tinyerp.com-ffa461aaeff1e9f196ab3ce6d6004981c0296cfd

--- addons/hr_contract/__terp__.py
+++ addons/hr_contract/__terp__.py
@@ -14,11 +14,11 @@
 
 	You can assign several contracts per employee.
 	""",
-	"init_xml" : ["hr_contract_data.xml", "hr_contract_security.xml"],
+	"init_xml" : ["hr_contract_data.xml", ],
 	"demo_xml" : [],
 	"update_xml" : [
 		"hr_contract_view.xml",
-		
+		"hr_contract_security.xml"
 	],
 	"active": False,
 	"installable": True
