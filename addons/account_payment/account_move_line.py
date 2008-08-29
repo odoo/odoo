c009012c29a3c4cbdb84c9a8bc7068f355da1e8a
qdp <qdp@tinyerp.com>
2008-08-29 15:51:15 +0200

    *added raising error condition if line has no partners or if a bank account cannot be found for this line (these 2 fields are mandatory for payment order)
    bzr revid: qdp@tinyerp.com-20080829135115-br0he00tl3lwccvn

--- addons/account_payment/account_move_line.py
+++ addons/account_payment/account_move_line.py
@@ -103,12 +103,16 @@ class account_move_line(osv.osv):
             if line.invoice and line.invoice.partner_bank:
                 line2bank[line.id] = line.invoice.partner_bank.id
             elif line.partner_id:
+                if not line.partner_id.bank_ids:
+                    raise osv.except_osv(_('Error !'), _('Partner '+ line.partner_id.name+ ' has no bank account defined'))
                 for bank in line.partner_id.bank_ids:
                     if bank.state in bank_type:
                         line2bank[line.id] = bank.id
                         break
                 if line.id not in line2bank and line.partner_id.bank_ids:
                     line2bank[line.id] = line.partner_id.bank_ids[0].id
+            else:
+                raise osv.except_osv(_('Error !'), _('No partner defined on entry line'))
         return line2bank
 
     _columns = {
