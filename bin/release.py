faa13e27a7a06b1fbc6212ef0d93efd96734fe93
Christophe Simonis <christophe@tinyerp.com>
2008-09-02 17:26:01 +0200

    tinyerp -> openerp
    bzr revid: christophe@tinyerp.com-20080902152601-rdiln6oi1a94wsmy

--- bin/release.py
+++ bin/release.py
@@ -28,11 +28,11 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ###############################################################################
 
-name = 'tinyerp-server'
+name = 'openerp-server'
 version = '4.3.0'
-description = 'Tiny\'s ERP Server'
+description = 'OpenERP Server'
 long_desc = '''\
-Tiny ERP is a complete ERP and CRM. The main features are accounting (analytic
+OpenERP is a complete ERP and CRM. The main features are accounting (analytic
 and financial), stock management, sales and purchases management, tasks
 automation, marketing campaigns, help desk, POS, etc. Technical features include
 a distributed server, flexible workflows, an object database, a dynamic GUI,
@@ -43,7 +43,7 @@ Development Status :: 5 - Production/Stable
 License :: OSI Approved :: GNU General Public License Version 2 (GPL-2)
 Programming Language :: Python
 """
-url = 'http://www.tinyerp.com'
+url = 'http://www.openerp.com'
 author = 'Tiny.be'
 author_email = 'info@tiny.be'
 license = 'GPL-2'
