da5e2f791814cf4f20f1c553d6e000a7bc9fb85b
Mustufa Rangwala <mra@tinyerp.com>
2008-09-12 12:51:22 +0530

    account_payment_tab move to account module
    bzr revid: mra@tinyerp.com-20080912072122-4fzuwtbr3krrrbk7

--- addons/account/invoice.py
+++ addons/account/invoice.py
@@ -98,6 +98,35 @@ class account_invoice(osv.osv):
     def _get_reference_type(self, cursor, user, context=None):
         return [('none', 'Free Reference')]
 
+    def _amount_residual(self, cr, uid, ids, name, args, context={}):
+        res = {}
+        data_inv = self.browse(cr, uid, ids)
+        for inv in data_inv:
+            paid_amt = 0.0
+            to_pay = inv.amount_total
+            for lines in inv.move_lines:
+                paid_amt = paid_amt + lines.credit
+            res[inv.id] = to_pay - paid_amt
+        return res
+
+    def _get_lines(self, cr, uid, ids, name, arg, context=None):
+        res = {}
+        for id in ids:
+            move_lines = self.move_line_id_payment_get(cr,uid,[id])
+            if not move_lines:
+                res[id] = []
+                continue
+            data_lines = self.pool.get('account.move.line').browse(cr,uid,move_lines)
+            for line in data_lines:
+                ids_line = []
+                if line.reconcile_id:
+                    ids_line = line.reconcile_id.line_id
+                elif line.reconcile_partial_id:
+                    ids_line = line.reconcile_partial_id.line_partial_ids
+                l = map(lambda x: x.id, ids_line)
+                res[id]=[x for x in l if x <> line.id]
+        return res
+
     _name = "account.invoice"
     _description = 'Invoice'
     _order = "number"
@@ -151,6 +180,9 @@ class account_invoice(osv.osv):
         'reconciled': fields.function(_reconciled, method=True, string='Paid/Reconciled', type='boolean'),
         'partner_bank': fields.many2one('res.partner.bank', 'Bank Account',
             help='The bank account to pay to or to be paid from'),
+        'move_lines':fields.function(_get_lines , method=True,type='many2many' , relation='account.move.line',string='Move Lines'),
+        'residual': fields.function(_amount_residual, method=True, digits=(16,2),string='Residual', store=True),
+
     }
     _defaults = {
         'type': _get_type,
