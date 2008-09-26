457ada6dff32509b78495d22c4d9e60da40232d3
Priyesh <solanki.priyesh@gmail.com>
2008-09-26 20:18:14 +0530

    Demo of all process
    bzr revid: solanki.priyesh@gmail.com-20080926144814-4lyid0srfi35ff1d

--- addons/process/__terp__.py
+++ addons/process/__terp__.py
@@ -10,7 +10,18 @@ This module allows you to manage your process for the end-users.
     """,
     "depends" : ["base", "sale"],
     "init_xml" : [],
-    "demo_xml" : ['process_demo.xml'],
+#    "demo_xml" : ['process_demo.xml'],
+    "demo_xml" : [
+        '../sale/process/sale_process.xml',
+        '../hr_timesheet_sheet/process/hr_timesheet_sheet_process.xml',
+        '../account/process/invoice_process.xml',
+        '../account/process/statement_process.xml',
+        '../project/process/task_process.xml',
+        '../hr_timesheet/process/hr_timesheet_process.xml',
+        '../purchase/process/purchase_process.xml',
+        '../product/process/product_process.xml', 
+    ]
+
     "update_xml" : [
         "security/ir.model.access.csv",
         'process_view.xml',
