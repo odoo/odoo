80c7b660aa5e8d2e3e8e8882a965374cc8f57800
Jay Vora <jvo@tinyerp.com>
2008-10-08 16:52:31 +0530

    Merged code of Account_report_history to account_report and minor bugfixes
    bzr revid: jvo@tinyerp.com-20081008112231-pnmjgn406frkonyh

--- addons/account_report/account.py
+++ addons/account_report/account.py
@@ -30,7 +30,7 @@
 import time
 import netsvc
 from osv import fields, osv
-
+import pooler
 from tools.misc import currency
 
 import mx.DateTime
@@ -176,6 +176,35 @@ class account_report(osv.osv):
     ]
 
 account_report()
+
+class account_report_history(osv.osv):
+
+    def _calc_value(self, cr, uid, ids, name, args, context):
+        acc_report_id=self.read(cr,uid,ids,['tmp','period_id'])
+        tmp_ids={}
+        for a in acc_report_id:
+            period_val=pooler.get_pool(cr.dbname).get('account.period').read(cr,uid,[a['period_id'][0]])[0]
+            period_id=pooler.get_pool(cr.dbname).get('account.period').search(cr,uid,[('date_start','<=',period_val['date_start']),('fiscalyear_id','=',period_val['fiscalyear_id'][0])])
+            tmp_ids[a['id']] = pooler.get_pool(cr.dbname).get('account.report.report').read(cr,uid,[a['tmp']],context={'periods':period_id})[0]['amount']
+        return tmp_ids
+
+    _name = "account.report.history"
+    _description = "Indicator"
+    _table = "account_report"
+    _auto = False
+    _order='name'
+    _columns = {
+        'period_id': fields.many2one('account.period','Period', readonly=True, select=True),
+        'fiscalyear_id': fields.many2one('account.fiscalyear','Fiscal Year', readonly=True, select=True),
+        'name': fields.many2one('account.report.report','Indicator', readonly=True, select=True),
+        'val': fields.function(_calc_value, method=True, string='Value', readonly=True),
+        'tmp' : fields.integer(string='temp',readonly=True)
+    }
+
+    def init(self, cr):
+        cr.execute('''create or replace view account_report as (select ar.id as tmp,((pr.id*100000)+ar.id) as id,ar.id as name,pr.id as period_id,pr.fiscalyear_id as fiscalyear_id from account_report_report as ar cross join account_period as pr group by ar.id,pr.id,pr.fiscalyear_id)''')
+
+account_report_history()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
