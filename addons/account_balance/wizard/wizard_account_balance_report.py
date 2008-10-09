6ff404942d58e0ef103869decb8e404d2b5c75b3
Olivier Laurent <olt@tinyerp.com>
2008-10-09 16:30:08 +0200

    typo: 1 years -> 1 year
    bzr revid: olt@tinyerp.com-20081009143008-41ej548j4d0k2nsv

--- addons/account_balance/wizard/wizard_account_balance_report.py
+++ addons/account_balance/wizard/wizard_account_balance_report.py
@@ -90,7 +90,7 @@ back_fields={
 zero_form='''<?xml version="1.0"?>
 <form string="Notification">
 <separator string="You have to select at least 1 Fiscal Year. Try again." colspan="4"/>
-<label string="You may have selected the compare options with more than 1 years with credit/debit columns and % option.This can lead contents to be printed out of the paper.Please try again."/>
+<label string="You may have selected the compare options with more than 1 year with credit/debit columns and % option.This can lead contents to be printed out of the paper.Please try again."/>
 </form>'''
 
 zero_fields={
