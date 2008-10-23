a44bd1d6db9aee03443a00dd935f6695756d0a2b
Mustufa Rangwala <mra@tinyerp.com>
2008-10-23 16:00:52 +0530

    add print general journal report in wizard (F.M->legal statments->generic reports)
    bzr revid: mra@tinyerp.com-20081023103052-t5l9w9lf7o3j5y98

--- addons/account/wizard/wizard_general_journal.py
+++ addons/account/wizard/wizard_general_journal.py
@@ -28,47 +28,33 @@
 #
 ##############################################################################
 
-import wizard_automatic_reconcile
-import wizard_reconcile_select
-import wizard_unreconcile_select
-import wizard_reconcile
-import wizard_unreconcile
-import wizard_refund
-import wizard_pay_invoice
+import wizard
+import pooler
 
-import wizard_journal
-import wizard_journal_select
-import wizard_bank_reconcile
-import wizard_subscription_generate
+form = '''<?xml version="1.0"?>
+<form string="Print General Journal">
+    <field name="journal_id"/>
+    <field name="period_id"/>
+</form>'''
 
-import wizard_aged_trial_balance
-import wizard_general_ledger_report
-import wizard_third_party_ledger
-import wizard_account_balance_report
-import wizard_partner_balance_report
+fields = {
+  'journal_id': {'string': 'Journal', 'type': 'many2many', 'relation': 'account.journal', 'required': True},
+  'period_id': {'string': 'Period', 'type': 'many2many', 'relation': 'account.period', 'required': True},
+}
 
-import wizard_period_close
-import wizard_fiscalyear_close
-import wizard_open_closed_fiscalyear
 
-import wizard_vat
-
-import wizard_invoice_state
-import wizard_account_duplicate
-import wizard_account_chart
-import wizard_move_line_select
-
-import wizard_validate_account_move
-import wizard_use_model
-
-import wizard_state_open
-
-import wizard_statement_from_invoice
-import wizard_print_journal
-import wizard_central_journal
-import wizard_general_journal
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+class wizard_print_journal(wizard.interface):
+    states = {
+        'init': {
+            'actions': [],
+            'result': {'type': 'form', 'arch': form, 'fields': fields, 'state': (('end', 'Cancel'), ('print', 'Print'))},
+        },
+        'print': {
+            'actions': [],
+            'result': {'type':'print', 'report':'account.general.journal.wiz', 'state':'end'},
+        },
+    }
+wizard_print_journal('account.general.journal.report')
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
