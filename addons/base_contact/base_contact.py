68d6f182661b7a575b839429c4ff46487872ffc7
Fabien Pinckaers <fp@tinyerp.com>
2008-10-29 14:34:45 +0100

    improved_views
    bzr revid: fp@tinyerp.com-20081029133445-mt0jdpv1qamifk8m

--- addons/base_contact/base_contact.py
+++ addons/base_contact/base_contact.py
@@ -91,7 +91,7 @@ class res_partner_address(osv.osv):
     _inherit='res.partner.address'
     _description ='Partner Address'
     _columns = {
-        'job_ids':fields.one2many('res.partner.job', 'address_id', 'Functions and Addresses'),
+        'job_ids':fields.one2many('res.partner.job', 'address_id', 'Contacts'),
         'email': fields.related('job_ids', 'email', type='char', string='Default Email'),
     }
 res_partner_address()
