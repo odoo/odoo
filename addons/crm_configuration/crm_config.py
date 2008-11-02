c5abe8831f563c964de7eaec324e7bf00f799e8d
Fabien Pinckaers <fp@tinyerp.com>
2008-11-02 15:50:00 +0100

    improvement
    bzr revid: fp@tinyerp.com-20081102145000-tbkbeav8andntm95

--- addons/crm_configuration/crm_config.py
+++ addons/crm_configuration/crm_config.py
@@ -47,7 +47,7 @@ class crm_case_category2(osv.osv):
     _description = "Category2 of case"
     _rec_name = "name"
     _columns = {
-        'name': fields.char('Case Category2 Name', size=64, required=True),
+        'name': fields.char('Case Category2 Name', size=64, required=True, translate=True),
         'section_id': fields.many2one('crm.case.section', 'Case Section'),
     }
 crm_case_category2()
@@ -57,7 +57,7 @@ class crm_case_stage(osv.osv):
     _description = "Stage of case"
     _rec_name = 'name'
     _columns = {
-        'name': fields.char('Stage Name', size=64, required=True),
+        'name': fields.char('Stage Name', size=64, required=True, translate=True),
         'section_id': fields.many2one('crm.case.section', 'Case Section'),
     }
 crm_case_stage()
@@ -70,11 +70,26 @@ class crm_cases(osv.osv):
         'category2_id': fields.many2one('crm.case.category2','Category Name', domain="[('section_id','=',section_id)]"),
         'duration': fields.float('Duration'),
         'note': fields.text('Note'),
+        'case_id': fields.many2one('crm.case','Related Case'),
         'partner_name': fields.char('Employee Name', size=64),
         'partner_name2': fields.char('Employee Email', size=64),
         'partner_phone': fields.char('Phone', size=16),
         'partner_mobile': fields.char('Mobile', size=16),
     }
+    def onchange_case_id(self, cr, uid, ids, case_id, name, partner_id, context={}):
+        if not case_id: return {}
+        case = self.browse(cr, uid, case_id, context=context)
+        value = {}
+        if not name:
+            value['name'] = case.name
+        if (not partner_id) and case.partner_id:
+            value['partner_id'] = case.partner_id.id
+            if case.partner_address_id:
+                value['partner_address_id'] = case.partner_address_id.id
+            if case.email_from:
+                value['email_from'] = case.email_from
+        return {'value':value}
+
 crm_cases()
 
 class crm_menu_config_wizard(osv.osv_memory):
