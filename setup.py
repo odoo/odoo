a5c7360280d8cb3d2a31bc92a6adce87e76ef31e
Christophe Simonis <christophe@tinyerp.com>
2008-11-03 19:20:13 +0100

    improvement
    bzr revid: christophe@tinyerp.com-20081103182013-9b7ak4p7fx3lz1og

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-
 ##############################################################################
 #
 # Copyright (c) 2004-2008 Tiny SPRL (http://tiny.be) All Rights Reserved.
@@ -28,12 +27,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ###############################################################################
+
 # setup from TinERP
 #   taken from straw http://www.nongnu.org/straw/index.html
 #   taken from gnomolicious http://www.nongnu.org/gnomolicious/
 #   adapted by Nicolas Évrard <nicoe@altern.org>
 #
-# $Id$
 
 import imp
 import sys
