cef21ce434c86f48afdaf23b2c1e5ea303adae04
Fabien Pinckaers <fp@tinyerp.com>
2008-11-03 08:48:13 +0100

    improved_trans
    bzr revid: fp@tinyerp.com-20081103074813-ltlm1oi2r0z8brcd

--- bin/addons/base/res/partner/partner.py
+++ bin/addons/base/res/partner/partner.py
@@ -140,7 +140,6 @@ class res_partner(osv.osv):
         'ref': fields.char('Code', size=64),
         'lang': fields.selection(_lang_get, 'Language', size=5, help="If the selected language is loaded in the system, all documents related to this partner will be printed in this language. If not, it will be english."),
         'user_id': fields.many2one('res.users', 'Dedicated Salesman', help='The internal user that is in charge of communicating with this partner if any.'),
-        'responsible': fields.many2one('res.users', 'Users'),
         'vat': fields.char('VAT',size=32 ,help="Value Added Tax number"),
         'bank_ids': fields.one2many('res.partner.bank', 'partner_id', 'Banks'),
         'website': fields.char('Website',size=64),
