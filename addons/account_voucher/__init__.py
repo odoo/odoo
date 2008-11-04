cd02196d3d08b84066095d08aa7e839c64401e7d
Mantavya Gajjar <mga@tinyerp.com>
2008-11-04 12:34:05 +0530

    test commit
    bzr revid: mga@tinyerp.com-20081104070405-25i73uuf97gq3qga

--- addons/account_voucher/__init__.py
+++ addons/account_voucher/__init__.py
@@ -19,6 +19,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 import voucher
 import voucher_account
 import report
