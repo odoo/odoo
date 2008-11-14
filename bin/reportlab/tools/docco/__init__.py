3dae09ac14990a82aa9ef8afc2a0429b877ab123
Stephane Wirtel <stephane@tinyerp.com>
2008-11-14 16:06:02 +0100

    Remove pydot, pychart and reportlab.
    bzr revid: stephane@tinyerp.com-20081114150602-cpp1eeo3d0837fev

--- bin/reportlab/tools/docco/__init__.py
+++
@@ -1,3 +0,0 @@
-#Copyright ReportLab Europe Ltd. 2000-2004
-#see license.txt for license details
-#history http://www.reportlab.co.uk/cgi-bin/viewcvs.cgi/public/reportlab/trunk/reportlab/tools/docco/__init__.py
