f2d5d3ef82cee936dc4cd5d8e44865ddbe3eb1c1
Mantavya Gajjar <mga@tinyerp.com>
2008-11-21 20:39:23 +0530

    adding the wizard to create a menu for the groups
    bzr revid: mga@tinyerp.com-20081121150923-g7j0gfugdp0surcq

--- addons/wiki/__terp__.py
+++ addons/wiki/__terp__.py
@@ -1,9 +1,15 @@
 {
-    "name" : "Wiki System",
+    "name" : "Document Management - Wiki",
     "version" : "1.0",
-    "author" : "Tiny ERP",
+    "author" : "Tiny",
+    "description": """
+    The base module to manage documents(wiki) 
+    
+    keep track for the wiki groups, pages, and history
+    """,
     "category" : "Generic Modules/Others",
     "depends" : ["base"],
+    "website" : "http://openerp.com",
     "init_xml" : [],
     "demo_xml" : [],
     "update_xml" : [
