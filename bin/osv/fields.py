aac96b549f3d59f4d1f480a3a36b2c22118084ee
hda@tinyerp.com <>
2008-11-25 12:37:40 +0530

    remove prints
    bzr revid: hda@tinyerp.com-20081125070740-wxunzjfblfyu20bh

--- bin/osv/fields.py
+++ bin/osv/fields.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution	
+#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
