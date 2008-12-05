638ee7ffea55fdf6b0d762f63dfeccdf681b964d
Mustufa Rangwala <mra@tinyerp.com>
2008-12-05 16:28:19 +0530

    base_module_qualtiy modifs
    bzr revid: mra@tinyerp.com-20081205105819-2bsfctoovewgvuxa

--- addons/base_module_quality/wizard/pylint_test/pylint_test.py
+++ addons/base_module_quality/wizard/pylint_test/pylint_test.py
@@ -24,8 +24,16 @@ import netsvc
 from osv import fields, osv
 import os
 
-def _test_pylint(self, url):
+def _test_pylint(self, url, add_folder=None):
     list_files = os.listdir(url)
+    new_list = []
+    subfolder = {}
+    for i in list_files:
+        if os.path.isdir(i):
+            path = os.path.join(url, i)
+            new_list.append(os.listdir(path))
+            res = _test_pylint(self, path, add_folder=i)
+            subfolder.update(res)
     dict_files = {}
     for file in list_files:
         if file.split('.')[-1] == 'py':
@@ -45,117 +53,14 @@ def _test_pylint(self, url):
                 fp.write('')
                 fp.close()
             if str_result.startswith('Global'):
-                dict_files[file] = str_result
+                if add_folder:
+                    dict_files[add_folder + '/' + file] = str_result
+                else:
+                    dict_files[file] = str_result
+    dict_files.update(subfolder)
     return dict_files
+
 #
-#class res_partner_contact(osv.osv):
-#    _name = "res.partner.contact"
-#    _description = "res.partner.contact"
-#
-#    def _title_get(self,cr, user, context={}):
-#        obj = self.pool.get('res.partner.title')
-#        ids = obj.search(cr, user, [])
-#        res = obj.read(cr, user, ids, ['shortcut', 'name','domain'], context)
-#        res = [(r['shortcut'], r['name']) for r in res if r['domain']=='contact']
-#        return res
-#
-#    _columns = {
-#        'name': fields.char('Last Name', size=30,required=True),
-#        'first_name': fields.char('First Name', size=30),
-#        'mobile':fields.char('Mobile',size=30),
-#        'title': fields.selection(_title_get, 'Title'),
-#        'website':fields.char('Website',size=120),
-#        'lang_id':fields.many2one('res.lang','Language'),
-#        'job_ids':fields.one2many('res.partner.job','contact_id','Functions and Addresses'),
-#        'country_id':fields.many2one('res.country','Nationality'),
-#        'birthdate':fields.date('Birth Date'),
-#        'active' : fields.boolean('Active'),
-#        'partner_id':fields.related('job_ids','address_id','partner_id',type='many2one', relation='res.partner', string='Main Employer'),
-#        'function_id':fields.related('job_ids','function_id',type='many2one', relation='res.partner.function', string='Main Job'),
-#    }
-#    _defaults = {
-#        'active' : lambda *a: True,
-#    }
-#    def name_get(self, cr, user, ids, context={}):
-#        #will return name and first_name.......
-#        if not len(ids):
-#            return []
-#        res = []
-#        for r in self.read(cr, user, ids, ['name','first_name','title']):
-#            addr = r['title'] and str(r['title'])+" " or ''
-#            addr +=str(r['name'] or '')
-#            if r['name'] and r['first_name']:
-#                addr += ' '
-#            addr += str(r['first_name'] or '')
-#            res.append((r['id'], addr))
-#        return res
-#res_partner_contact()
-#
-#class res_partner_address(osv.osv):
-#
-#    #overriding of the name_get defined in base in order to remove the old contact name
-#    def name_get(self, cr, user, ids, context={}):
-#        if not len(ids):
-#            return []
-#        res = []
-#        for r in self.read(cr, user, ids, ['zip','city','partner_id', 'street']):
-#            if context.get('contact_display', 'contact')=='partner':
-#                res.append((r['id'], r['partner_id'][1]))
-#            else:
-#                addr = str('')
-#                addr += str(r['street'] or '') + ' ' + str(r['zip'] or '') + ' ' + str(r['city'] or '')
-#                res.append((r['id'], addr.strip() or '/'))
-#        return res
-#
-#    _name = 'res.partner.address'
-#    _inherit='res.partner.address'
-#    _description ='Partner Address'
-#    _columns = {
-#        'job_ids':fields.one2many('res.partner.job', 'address_id', 'Contacts'),
-#    }
-#res_partner_address()
-#
-#class res_partner_job(osv.osv):
-#
-#    def name_get(self, cr, uid, ids, context={}):
-#        if not len(ids):
-#            return []
-#        res = []
-#        for r in self.browse(cr, uid, ids):
-#            res.append((r.id, self.pool.get('res.partner.contact').name_get(cr, uid, [r.contact_id.id])[0][1] +", "+ r.function_id.name))
-#        return res
-#
-#    def search(self, cr, user, args, offset=0, limit=None, order=None,
-#            context=None, count=False):
-#        for arg in args:
-#            if arg[0]=='address_id':
-#                self._order = 'sequence_partner'
-#            if arg[0]=='contact_id':
-#                self._order = 'sequence_contact'
-#        return super(res_partner_job,self).search(cr, user, args, offset, limit, order, context, count)
-#
-#    _name = 'res.partner.job'
-#    _description ='Contact Job Title'
-#    _order = 'sequence_contact'
-#    _columns = {
-#        'name': fields.related('address_id','partner_id', type='many2one', relation='res.partner', string='Partner'),
-#        'address_id':fields.many2one('res.partner.address','Address'),
-#        'contact_id':fields.many2one('res.partner.contact','Contact', required=True, ondelete='cascade'),
-#        'function_id': fields.many2one('res.partner.function','Job Title'),
-#        'sequence_contact':fields.integer('Sequence',help='Order of importance of this address in the list of addresses of the linked contact'),
-#        'sequence_partner':fields.integer('Sequence',help='Order of importance of this job title in the list of job title of the linked partner'),
-#        'email': fields.char('E-Mail', size=240),
-#        'phone': fields.char('Phone', size=64),
-#        'date_start' : fields.date('Date Start'),
-#        'date_stop' : fields.date('Date Stop'),
-#        'state' : fields.selection([('past', 'Past'),('current', 'Current')], 'State', required=True),
-#    }
-#
-#    _defaults = {
-#        'sequence_contact' : lambda *a: 0,
-#        'state' : lambda *a: 'current',
-#    }
-#res_partner_job()
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
