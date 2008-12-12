0a9426cf068aa74c9c34a9f417564077da8130e6
Christophe Simonis <christophe@tinyerp.com>
2008-12-12 11:54:17 +0100

    [IMP] the logging of the workflows is made with the logger
    bzr revid: christophe@tinyerp.com-20081212105417-up42ltvbothbibpt

--- bin/workflow/wkf_logs.py
+++ bin/workflow/wkf_logs.py
@@ -23,9 +23,18 @@
 #
 # May be uncommented to logs workflows modifications
 #
+import netsvc
 
 def log(cr,ident,act_id,info=''):
-    pass
+    msg = """
+res_type: %r
+res_id: %d
+uid: %d
+act_id: %d
+info: %s
+""" % (ident[1], ident[2], ident[0], act_id, info)
+    netsvc.Logger().notifyChannel('wkf_log', netsvc.LOG_DEBUG, msg)
+
     #cr.execute('insert into wkf_logs (res_type, res_id, uid, act_id, time, info) values (%s,%s,%s,%s,current_time,%s)', (ident[1],int(ident[2]),int(ident[0]),int(act_id),info))
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
