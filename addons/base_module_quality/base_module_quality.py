503a081c9d83c479d9832979418fadfe8b81813d
qdp <qdp@tinyerp.com>
2008-12-17 18:12:44 +0100

    *base_module_quality: 	* added a configuration file 	* added possiblity to define tests on installed module only or no 	* cleaned 	* improved
    bzr revid: qdp@tinyerp.com-20081217171244-x8rnbx32yd2bj3v3

--- addons/base_module_quality/base_module_quality.py
+++ addons/base_module_quality/base_module_quality.py
@@ -20,21 +20,38 @@
 #
 ##############################################################################
 
-import netsvc
-from osv import fields, osv
 
 class abstract_quality_check(object):
     '''
         This Class provide...
     '''
 
-    _score = 0.0
-    _result = ""
-    _result_details = ""
+    #This float have to store the rating of the module.
+    #Used to compute the final score (average of all scores).
+    score = 0.0
 
-    def __init__(self, module_path=""):
+    #This char have to store the result.
+    #Used to display the result of the test.
+    result = ""
+
+    #This char have to store the result with more details.
+    #Used to provide more details if necessary.
+    result_details = ""
+
+    #This bool defines if the test can be run only if the module is installed.
+    #True => the module have to be installed.
+    #False => the module can be uninstalled.
+    bool_installed_only = True  
+
+    def __init__(self):
         '''
-        this method should do the test and fill the _score, _result and _result_details var
+        this method should initialize the var 
+        '''
+        raise 'Not Implemented'
+
+    def run_test(self, module_path=""):
+        '''
+        this method should do the test and fill the score, result and result_details var
         '''
         raise 'Not Implemented'
 
