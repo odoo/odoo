6de8fb60593372dba23186207c2c7cc81fc3eb77
Christophe Simonis <christophe@taupe>
2008-12-26 17:34:58 +0100

    [FIX] project_gtd: format exception correctly
    lp bug: https://launchpad.net/bugs/311548 fixed
    
    bzr revid: christophe@taupe-20081226163458-btibjzmn0wu9jx3o

--- addons/project_gtd/wizard/project_gtd_daily.py
+++ addons/project_gtd/wizard/project_gtd_daily.py
@@ -33,7 +33,7 @@ class wiz_timebox_open(wizard.interface):
         pool = pooler.get_pool(cr.dbname)
         ids = pool.get('project.gtd.timebox').search(cr, uid, [('user_id','=',uid),('type','=',tbtype)])
         if not len(ids):
-            raise wizard.except_wizard('Error !', 'No timebox of the type "%s" defined !')
+            raise wizard.except_wizard('Error !', 'No timebox of the type "%s" defined !' % (tbtype,))
         view_type = 'form,tree'
         if len(ids) >= 1:
             domain = "[('id','in',["+','.join(map(str,ids))+"])]"
