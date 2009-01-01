8e1ab5a2772b00e116a929ba7f87684a6a77d85c
Jay (Open ERP) <jvo@tinyerp.com>
2009-01-01 10:51:32 +0530

    Correction
    bzr revid: jvo@tinyerp.com-20090101052132-zqqstwkqojn7kn0x

--- addons/l10n_be/__terp__.py
+++ addons/l10n_be/__terp__.py
@@ -34,7 +34,7 @@
     * We have the account templates which can be helpful to generate Charts of Accounts.
     * On that particular wizard,You will be asked to pass the name of the company,the chart template to follow,the no. of digits to generate the code for your account and Bank account,currency  to create Journals.
         Thus,the pure copy of Chart Template is generated.
-    * This is the same wizard that runs from Financial Managament/Configuration/Financial Accounting/Financial Accounts/Generate Chart of Accounts from a Chart Template.
+    * This is the same wizard that runs from Financial Management/Configuration/Financial Accounting/Financial Accounts/Generate Chart of Accounts from a Chart Template.
 
     Wizards provided by this module:
     * Enlist the partners with their related VAT  and invoiced amounts.Prepares an XML file format.Path to access : Financial Management/Reporting/Listing of VAT Customers.
