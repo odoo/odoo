3378bd4d3a4d503d59d259ae7c1220f1547be7df
Christophe Simonis <christophe@tinyerp.com>
2009-01-06 14:02:10 +0100

    project_timesheet: [FIX] handle the case when the task work has no summary [IMP] PEP8
    lp bug: https://launchpad.net/bugs/314337 fixed
    
    bzr revid: christophe@tinyerp.com-20090106130210-yzjpfzyex6g8y224

--- addons/project_timesheet/project_timesheet.py
+++ addons/project_timesheet/project_timesheet.py
@@ -31,11 +31,11 @@ class project_work(osv.osv):
     def create(self, cr, uid, vals, *args, **kwargs):
 
         obj = self.pool.get('hr.analytic.timesheet')
-        vals_line={}
+        vals_line = {}
         obj_task = self.pool.get('project.task').browse(cr, uid, vals['task_id'])
 
         emp_obj = self.pool.get('hr.employee')
-        emp_id = emp_obj.search(cr, uid, [('user_id', '=', vals.get('user_id',uid))])
+        emp_id = emp_obj.search(cr, uid, [('user_id', '=', vals.get('user_id', uid))])
 
         if not emp_id:
             raise osv.except_osv(_('Bad Configuration !'),
@@ -55,49 +55,49 @@ class project_work(osv.osv):
         vals_line['general_account_id'] = a
         vals_line['journal_id'] = emp.journal_id.id
 
-        vals_line['name']=obj_task.name + ': ' + vals['name']
-        vals_line['user_id']=vals['user_id']
-        vals_line['date']=vals['date'][:10]
-        vals_line['unit_amount']=vals['hours']
-        vals_line['account_id']=obj_task.project_id.category_id.id
-        vals_line['amount']=00.0
-        timeline_id=obj.create(cr, uid,vals_line,{})
+        vals_line['name'] = '%s: %s' % (obj_task.name, vals['name'] or '/')
+        vals_line['user_id'] = vals['user_id']
+        vals_line['date'] = vals['date'][:10]
+        vals_line['unit_amount'] = vals['hours']
+        vals_line['account_id'] = obj_task.project_id.category_id.id
+        vals_line['amount'] = 00.0
+        timeline_id = obj.create(cr, uid, vals_line, {})
 
-        vals_line['amount']=(-1) * vals['hours']*obj.browse(cr,uid,timeline_id).product_id.standard_price
-        obj.write(cr, uid,[timeline_id],vals_line,{})
-        vals['hr_analytic_timesheet_id']=timeline_id
+        vals_line['amount'] = (-1) * vals['hours'] * obj.browse(cr, uid, timeline_id).product_id.standard_price
+        obj.write(cr, uid,[timeline_id], vals_line, {})
+        vals['hr_analytic_timesheet_id'] = timeline_id
         return super(project_work,self).create(cr, uid, vals, *args, **kwargs)
 
-    def write(self, cr, uid, ids,vals,context={}):
-        vals_line={}
+    def write(self, cr, uid, ids, vals, context=None):
+        vals_line = {}
 
-        task=self.pool.get('project.task.work').browse(cr,uid,ids)[0]
-        line_id=task.hr_analytic_timesheet_id
+        task = self.pool.get('project.task.work').browse(cr, uid, ids)[0]
+        line_id = task.hr_analytic_timesheet_id
         # in case,if a record is deleted from timesheet,but we change it from tasks!
-        list_avail_ids=self.pool.get('hr.analytic.timesheet').search(cr,uid,[])
+        list_avail_ids = self.pool.get('hr.analytic.timesheet').search(cr, uid, [])
         if line_id in list_avail_ids:
             obj = self.pool.get('hr.analytic.timesheet')
             if 'name' in vals:
-                vals_line['name']=task.name+': '+vals['name']
+                vals_line['name'] = '%s: %s' % (task.name, vals['name'] or '/')
             if 'user_id' in vals:
-                vals_line['user_id']=vals['user_id']
+                vals_line['user_id'] = vals['user_id']
             if 'date' in vals:
-                vals_line['date']=vals['date'][:10]
+                vals_line['date'] = vals['date'][:10]
             if 'hours' in vals:
-                vals_line['unit_amount']=vals['hours']
-                vals_line['amount']=(-1) * vals['hours'] * obj.browse(cr,uid,line_id).product_id.standard_price
-            obj.write(cr, uid,[line_id],vals_line,{})
+                vals_line['unit_amount'] = vals['hours']
+                vals_line['amount'] = (-1) * vals['hours'] * obj.browse(cr, uid, line_id).product_id.standard_price
+            obj.write(cr, uid, [line_id], vals_line, {})
 
-        return super(project_work,self).write(cr, uid, ids,vals,context)
+        return super(project_work,self).write(cr, uid, ids, vals, context)
 
     def unlink(self, cr, uid, ids, *args, **kwargs):
-        timesheet_id=self.pool.get('project.task.work').browse(cr,uid,ids)[0].hr_analytic_timesheet_id
+        timesheet_id = self.pool.get('project.task.work').browse(cr, uid, ids)[0].hr_analytic_timesheet_id
         # delete entry from timesheet too while deleting entry to task.
-        list_avail_ids=self.pool.get('hr.analytic.timesheet').search(cr,uid,[])
+        list_avail_ids = self.pool.get('hr.analytic.timesheet').search(cr, uid, [])
         if timesheet_id in list_avail_ids:
-            obj = self.pool.get('hr.analytic.timesheet').unlink(cr,uid,[timesheet_id],*args)
+            obj = self.pool.get('hr.analytic.timesheet').unlink(cr, uid, [timesheet_id], *args)
 
-        return super(project_work,self).unlink(cr, uid, ids,*args, **kwargs)
+        return super(project_work,self).unlink(cr, uid, ids, *args, **kwargs)
 
     _columns={
         'hr_analytic_timesheet_id':fields.integer('Related Timeline Id')
@@ -111,10 +111,10 @@ class project_project(osv.osv):
     def name_get(self, cr, user, ids, context=None):
         result = []
         for project in self.browse(cr, user, ids, context):
-            if project.category_id and project.category_id.code:
-                result.append((project.id, '['+(project.category_id.code or '')+'] '+project.name))
-            else:
-                result.append((project.id, '[?] '+project.name))
+            name = "[%s] %s" % (project.category_id and project.category_id.code or '?', project.name)
+            result.append((project.id, name))
         return result
 project_project()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
