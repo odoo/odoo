83a8db6b975861d806c1de8dd8a45d715cb90320
Stephane Wirtel <stephane@tinyerp.com>
2009-01-23 13:14:04 +0100

    [FIX] Redhat Package Manager
    lp bug: https://launchpad.net/bugs/311981 fixed
    lp bug: https://launchpad.net/bugs/314575 fixed
    
    bzr revid: stephane@tinyerp.com-20090123121404-345vxufyn1beqjt4

--- setup.py
+++ setup.py
@@ -44,6 +44,9 @@ opj = os.path.join
 
 execfile(opj('bin', 'release.py'))
 
+if sys.argv[1] != 'bdist_rpm':
+    version = version + '-' + release
+
 # get python short version
 py_short_version = '%s.%s' % sys.version_info[:2]
 
