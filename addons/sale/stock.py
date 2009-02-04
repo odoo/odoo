53bdfd5278e0f40fd0f5f8923886c2faad896b44
Fabien Pinckaers <fp@tinyerp.com>
2009-02-04 23:10:49 +0100

    bugfixe
    bzr revid: fp@tinyerp.com-20090204221049-rw7q2jty42ecl6a5

--- addons/sale/stock.py
+++ addons/sale/stock.py
@@ -72,7 +72,7 @@ class stock_picking(osv.osv):
                 picking)
 
     def _get_price_unit_invoice(self, cursor, user, move_line, type):
-        if move_line.sale_line_id:
+        if move_line.sale_line_id and move_line.sale_line_id.product_id.id == move_line.product_id.id:
             return move_line.sale_line_id.price_unit
         return super(stock_picking, self)._get_price_unit_invoice(cursor,
                 user, move_line, type)
@@ -84,7 +84,7 @@ class stock_picking(osv.osv):
                 move_line)
 
     def _get_taxes_invoice(self, cursor, user, move_line, type):
-        if move_line.sale_line_id:
+        if move_line.sale_line_id and move_line.sale_line_id.product_id.id == move_line.product_id.id:
             return [x.id for x in move_line.sale_line_id.tax_id]
         return super(stock_picking, self)._get_taxes_invoice(cursor, user,
                 move_line, type)
