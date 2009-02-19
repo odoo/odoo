062c792152b62c4f7190bea58f221df500d49615
Fabien Pinckaers <fp@tinyerp.com>
2009-02-19 19:51:09 +0100

    merge
    bzr revid: vmt@openerp.com-20090219185109-qlyshbp6av7njujp

--- addons/base_crypt/__terp__.py
+++ addons/base_crypt/__terp__.py
@@ -20,18 +20,18 @@
 #
 ##############################################################################
 {
-	"name" : "Base",
-	"version" : "1.0",
-	"author" : "FS3 , Review Tiny",
-	"website" : "http://tinyerp.com",
-	"category" : "Generic Modules/Base",
-	"description": "Module for password encryption.",
-	"depends" : ["base"],
-	"init_xml" : [],
-	"demo_xml" : [],
-	"update_xml" : [
-		"base_update.xml",
-	],
-	"active": False,
-	"installable": True,
+    "name" : "Base",
+    "version" : "1.0",
+    "author" : "FS3 , Review Tiny",
+    "website" : "http://www.openerp.com",
+    "category" : "Generic Modules/Base",
+    "description": "Module for password encryption.",
+    "depends" : ["base"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [
+        "base_update.xml",
+    ],
+    "active": False,
+    "installable": True,
 }
