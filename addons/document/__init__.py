8f1d6918d24dd139e0d4340c6739a41b6ff6348a
Christophe Chauvet <christophe.chauvet@syleam.fr>
2009-03-03 10:51:57 +0100

    [IMP]  * Correct bug with locale, use string.ascii_letters instead of string.letters  * Remove mixing tabs with spaces (PEP8 compliant)  * Add UTF8 header in python file  * Add vim comment at the end of file  * Add license header that missing on many files
    bzr revid: christophe.chauvet@syleam.fr-20090303095157-qdtic4sqftars0hv

--- addons/document/__init__.py
+++ addons/document/__init__.py
@@ -22,3 +22,5 @@
 
 import document
 import ftpserver
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- addons/document/odt2txt.py
+++ addons/document/odt2txt.py
@@ -2,7 +2,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution	
+#    OpenERP, Open Source Management Solution   
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
@@ -25,28 +25,30 @@ import sys, zipfile, xml.dom.minidom
 import StringIO
 
 class OpenDocumentTextFile :
-	def __init__ (self, filepath) :
-		zip = zipfile.ZipFile(filepath)
-		self.content = xml.dom.minidom.parseString(zip.read("content.xml"))
+    def __init__ (self, filepath) :
+        zip = zipfile.ZipFile(filepath)
+        self.content = xml.dom.minidom.parseString(zip.read("content.xml"))
 
-	def toString (self) :
-		""" Converts the document to a string. """
-		buffer = u""
-		for val in ["text:p", "text:h", "text:list"]:
-			for paragraph in self.content.getElementsByTagName(val) :
-				buffer += self.textToString(paragraph) + "\n"
-		return buffer
+    def toString (self) :
+        """ Converts the document to a string. """
+        buffer = u""
+        for val in ["text:p", "text:h", "text:list"]:
+            for paragraph in self.content.getElementsByTagName(val) :
+                buffer += self.textToString(paragraph) + "\n"
+        return buffer
 
-	def textToString(self, element) :
-		buffer = u""
-		for node in element.childNodes :
-			if node.nodeType == xml.dom.Node.TEXT_NODE :
-				buffer += node.nodeValue
-			elif node.nodeType == xml.dom.Node.ELEMENT_NODE :
-				buffer += self.textToString(node)
-		return buffer
+    def textToString(self, element) :
+        buffer = u""
+        for node in element.childNodes :
+            if node.nodeType == xml.dom.Node.TEXT_NODE :
+                buffer += node.nodeValue
+            elif node.nodeType == xml.dom.Node.ELEMENT_NODE :
+                buffer += self.textToString(node)
+        return buffer
 
 if __name__ == "__main__" :
-	s =StringIO.StringIO(file(sys.argv[1]).read())
-	odt = OpenDocumentTextFile(s)
-	print odt.toString().encode('ascii','replace')
+    s =StringIO.StringIO(file(sys.argv[1]).read())
+    odt = OpenDocumentTextFile(s)
+    print odt.toString().encode('ascii','replace')
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
