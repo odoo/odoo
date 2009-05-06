8fdcadcd9900f948f776c13543185ef4cc26445f
P. Christeas <p_christ@hol.gr>
2009-05-06 23:10:28 +0300

    Workflow print: use portrait mode, top-to bottom diagram.
    bzr revid: p_christ@hol.gr-20090506201028-lk1obx8sbuc4q0n2

--- bin/addons/base/ir/workflow/print_instance.py
+++ bin/addons/base/ir/workflow/print_instance.py
@@ -147,7 +147,8 @@ showpage'''
                 else:
                     inst_id = inst_id[0]
                     graph = pydot.Dot(fontsize='16', label="""\\\n\\nWorkflow: %s\\n OSV: %s""" % (wkfinfo['name'],wkfinfo['osv']),
-                                      size='10.1, 7.3', center='1', ratio='auto', rotate='90', rankdir='LR'
+                                      size='7.3, 10.1', center='1', ratio='auto', rotate='0', rankdir='TB',
+				      ordering='out'
                                      )
                     graph_instance_get(cr, graph, inst_id, data.get('nested', False))
                     ps_string = graph.create(prog='dot', format='ps')
