37134f7c2a7232917833d0bc71c98a325ace3512
Jay (Open ERP) <jvo@tinyerp.com>
2009-05-11 17:08:51 +0530

    Corrected Generic Reports(Ref: CDA,SME,SSI)
    bzr revid: jvo@tinyerp.com-20090511113851-9ewhtf3p80q9m4px

--- addons/account/wizard/wizard_print_journal.py
+++ addons/account/wizard/wizard_print_journal.py
@@ -43,6 +43,25 @@ fields = {
 
 }
 
+def _check_data(self, cr, uid, data, *args):
+    period_id = data['form']['period_id'][0][2]
+    journal_id=data['form']['journal_id'][0][2]
+
+    if type(period_id)==type([]):
+        
+        ids_final = []
+
+        for journal in journal_id:
+            for period in period_id:
+                ids_journal_period = pooler.get_pool(cr.dbname).get('account.journal.period').search(cr,uid, [('journal_id','=',journal),('period_id','=',period)])
+
+                if ids_journal_period:
+                    ids_final.append(ids_journal_period)
+
+            if not ids_final:
+                raise wizard.except_wizard(_('No Data Available'), _('No records found for your selection!'))
+    return data['form']
+
 class wizard_print_journal(wizard.interface):
     states = {
         'init': {
@@ -50,7 +69,7 @@ class wizard_print_journal(wizard.interface):
             'result': {'type': 'form', 'arch': form, 'fields': fields, 'state': (('end', 'Cancel'), ('print', 'Print'))},
         },
         'print': {
-            'actions': [],
+            'actions': [_check_data],
             'result': {'type':'print', 'report':'account.journal.period.print.wiz', 'state':'end'},
         },
     }
