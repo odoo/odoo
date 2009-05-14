050dabba9b0eaef6408863d9f213e170a502c7dc
husen daudi <hda@hda>
2009-05-14 12:36:09 +0530

    replace account_voucher from trunk (ref:mga,vra)
    bzr revid: hda@hda-20090514070609-f277t69x5kyezagn

--- addons/account_voucher/__init__.py
+++ addons/account_voucher/__init__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution	
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -21,6 +21,6 @@
 ##############################################################################
 
 import voucher
-import voucher_account
+import account
 import report
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+import wizard
