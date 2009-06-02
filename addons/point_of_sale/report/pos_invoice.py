19d09b4ec82c3fb420cacd9fed8d5a93df5f4094
Olivier Laurent <olt@tinyerp.com>
2009-06-02 16:09:17 +0200

    modifs
    bzr revid: olt@tinyerp.com-20090602140917-hdmc91wf2ml59vid

--- addons/point_of_sale/report/pos_invoice.py
+++ addons/point_of_sale/report/pos_invoice.py
@@ -22,12 +22,9 @@
 
 import time
 from report import report_sxw
-from osv import osv
-from tools.translate import _
 
 
 class pos_invoice(report_sxw.rml_parse):
-
     def __init__(self, cr, uid, name, context):
         super(pos_invoice, self).__init__(cr, uid, name, context)
         self.localcontext.update({
@@ -36,26 +33,19 @@ class pos_invoice(report_sxw.rml_parse):
 
     def preprocess(self, objects, data, ids):
         super(pos_invoice, self).preprocess(objects, data, ids)
-        iids = []
-        nids = []
 
-        for order in objects:
-            order.write({'nb_print': order.nb_print + 1})
+        post_objects = []
+        for obj in objects:
+            if obj.invoice_id:
+                post_objects.append(obj.invoice_id)
 
-            if order.invoice_id and order.invoice_id not in iids:
-                if not order.invoice_id:
-                    raise osv.except_osv(_('Error !'), _('Please create an invoice for this sale.'))
-                iids.append(order.invoice_id)
-                nids.append(order.invoice_id.id)
-        self.cr.commit()
-        data['ids'] = nids
-        self.datas = data
-        self.ids = nids
-        self.objects = iids
-        self.localcontext['data'] = data
-        self.localcontext['objects'] = iids
+        #self.localcontext['objects'] = objects
+        self.localcontext['objects'] = post_objects
 
-report_sxw.report_sxw('report.pos.invoice', 'pos.order', 'addons/libeurop/report/invoice.rml', parser= pos_invoice)
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+report_sxw.report_sxw(
+    'report.pos.invoice',
+    'pos.order',
+    'addons/point_of_sale/report/pos_invoice.rml',
+    parser=pos_invoice)
 
