946205be17efaad6043cd9f3c78588ecd14e74b7
P. Christeas <p_christ@hol.gr>
2009-06-08 23:29:44 +0300

    Translate: option to only export installed modules.
    With the 'all' parameter, the DB would contain even non-installed
    modules, for which it would export an empty .pot file. Now, allow only
    the useful .pot files to be exported.
    
    bzr revid: p_christ@hol.gr-20090608202944-nq2rvc3zr3bozpw0

--- bin/tools/translate.py
+++ bin/tools/translate.py
@@ -424,11 +424,14 @@ def trans_generate(lang, modules, dbname=None):
 
     query = 'SELECT name, model, res_id, module'    \
             '  FROM ir_model_data'
-    if not 'all' in modules:
+    query_param = None
+    if 'all_installed' in modules:
+        query += ' WHERE module IN ( SELECT name FROM ir_module_module WHERE state = \'installed\') '
+    elif not 'all' in modules:
         query += ' WHERE module IN (%s)' % ','.join(['%s']*len(modules))
+        query_param = modules
     query += ' ORDER BY module, model, name'
 
-    query_param = not 'all' in modules and modules or None
     cr.execute(query, query_param)
 
     _to_translate = []
