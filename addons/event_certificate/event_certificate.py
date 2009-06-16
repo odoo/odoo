db73adab598ebbbb54dfcb10f4752824dd68bd89
vra (Tiny) <vra@tinyerp.com>
2009-06-16 18:44:37 +0530

    [ADD] event_certificate module : for Certificate report.
    bzr revid: vra@tinyerp.com-20090616131437-bd7rr3frzlx88wsg

---
+++ addons/event_certificate/event_certificate.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+
+class event(osv.osv):
+    _name = 'event.event'
+    _inherit = 'event.event'
+    _columns = {
+        'trainer_id': fields.many2one('res.partner', 'Trainer'),
+    }
+
+event()
