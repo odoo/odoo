f7ad498ef2d6e93937d77c4531f3193db9f475f3
vra (Tiny) <vra@tinyerp.com>
2009-06-17 17:05:16 +0530

    [FIX] l10n_ch module : Improved Code.
    lp bug: https://launchpad.net/bugs/388346 fixed
    
    bzr revid: vra@tinyerp.com-20090617113516-fzsg3ka948m0ctpv

--- addons/l10n_ch/partner.py
+++ addons/l10n_ch/partner.py
@@ -40,7 +40,7 @@ class res_partner(osv.osv):
 		'ref_companies': fields.one2many('res.company', 'partner_id',
 		'Companies that refers to partner'),
 	}
-	
+
 
 
 res_partner()
@@ -56,7 +56,7 @@ class res_partner_bank(osv.osv):
 		'bvr_adherent_num': fields.char('BVR adherent number', size=11),
 		'dta_code': fields.char('DTA code', size=5),
 	}
-	
+
 	def _default_value(self, cursor, user, field, context=None):
 		if field in ('country_id', 'state_id'):
 			value = False
@@ -67,14 +67,17 @@ class res_partner_bank(osv.osv):
 		for ham, spam, address in context['address']:
 			if 'type' in address.keys() :
 				if address['type'] == 'default':
-					return address[field]
+					if field in address.keys():
+						return address[field]
+					else:
+						return False
 				elif not address['type']:
 					value = address[field]
 			else :
 				value = False
 		return value
-		
-		
+
+
 	def name_get(self, cr, uid, ids, context=None):
 		if not len(ids):
 			return []
@@ -89,11 +92,11 @@ class res_partner_bank(osv.osv):
 		for r in self.read(cr, uid, ids, ['name','state'], context):
 			res.append((r['id'], r['name']+' : '+bank_type_names[r['state']]))
 		return res
-	
+
 	_sql_constraints = [
 		('bvr_adherent_uniq', 'unique (bvr_adherent_num)', 'The BVR adherent number must be unique !')
 	]
 
-	
+
 res_partner_bank()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
