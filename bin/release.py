fe82cf7fac1bfc8583ef77e29e30a44e8f290d9b
Christophe Simonis <christophe@tinyerp.com>
2009-08-21 17:27:31 +0200

    [REL] 5.0.3
    bzr revid: christophe@tinyerp.com-20090821152731-0utzy021ac629lbx

--- bin/release.py
+++ bin/release.py
@@ -22,7 +22,7 @@
 ##############################################################################
 
 name = 'openerp-server'
-version = '5.0.3-bzr'
+version = '5.0.3'
 major_version = '5.0'
 description = 'OpenERP Server'
 long_desc = '''\
