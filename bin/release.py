d8c6e2bab34290f9c924c770e97a4af295f38c2d
Christophe Simonis <christophe@tinyerp.com>
2009-08-28 19:03:37 +0200

    [IMP] cchange version number
    bzr revid: christophe@tinyerp.com-20090828170337-7vefeuaq0975w3qi

--- bin/release.py
+++ bin/release.py
@@ -22,7 +22,7 @@
 ##############################################################################
 
 name = 'openerp-server'
-version = '5.0.4'
+version = '5.0.5-bzr'
 major_version = '5.0'
 description = 'OpenERP Server'
 long_desc = '''\
