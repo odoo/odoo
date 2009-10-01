777942a9ba67e889dfe57757498668b2c2f52eaf
SME(OpenERP) <>
2009-10-01 17:02:20 +0530

    [FIX] Product : Pricelist types getting translated on Pricelist
    lp bug: https://launchpad.net/bugs/439469 fixed
    
    bzr revid: jvo@tinyerp.com-20091001113220-v5j8aaop9y560yen

--- addons/product/pricelist.py
+++ addons/product/pricelist.py
@@ -81,8 +81,18 @@ product_pricelist_type()
 
 class product_pricelist(osv.osv):
     def _pricelist_type_get(self, cr, uid, context={}):
-        cr.execute('select key,name from product_pricelist_type order by name')
-        return cr.fetchall()
+        pricelist_type_obj = self.pool.get('product.pricelist.type')
+        pricelist_type_ids = pricelist_type_obj.search(cr, uid, [], order='name')
+        pricelist_types = pricelist_type_obj.read(cr, uid, pricelist_type_ids, ['key','name'], context=context)
+        
+        res = []
+        
+        for type in pricelist_types:
+            res.append((type['key'],type['name']))
+        
+        return res
+#        cr.execute('select key,name from product_pricelist_type order by name')
+#        return cr.fetchall()
     _name = "product.pricelist"
     _description = "Pricelist"
     _columns = {
