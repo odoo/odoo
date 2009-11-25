d6bc38363b126956c5876c73977a78a1bee84747
Harry (Open ERP) <hmo@tinyerp.com>
2009-11-25 12:31:53 +0530

    [FIX] resolved error to print complex workflow diagram
    bzr revid: hmo@tinyerp.com-20091125070153-z2eaptf14ae5d5z3

--- bin/addons/base/ir/workflow/print_instance.py
+++ bin/addons/base/ir/workflow/print_instance.py
@@ -146,8 +146,7 @@ showpage'''
                 else:
                     inst_id = inst_id[0]
                     graph = pydot.Dot(fontsize='16', label="""\\\n\\nWorkflow: %s\\n OSV: %s""" % (wkfinfo['name'],wkfinfo['osv']),
-                                      size='7.3, 10.1', center='1', ratio='auto', rotate='0', rankdir='TB',
-                                      ordering='out'
+                                      size='7.3, 10.1', center='1', ratio='auto', rotate='0', rankdir='TB',                                      
                                      )
                     graph_instance_get(cr, graph, inst_id, data.get('nested', False))
                     ps_string = graph.create(prog='dot', format='ps')
