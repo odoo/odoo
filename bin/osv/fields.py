21749941eef20aeaf0173619ea7bf48c6957feb4
olt@tinyerp.com <>
2009-11-30 17:01:31 +0100

    [FIX] fields: '_fnct_write' should pass context object to the 'write' method
    lp bug: https://launchpad.net/bugs/489355 fixed
    
    bzr revid: olt@tinyerp.com-20091130160131-bv9g7y9vf0gdpa53

--- bin/osv/fields.py
+++ bin/osv/fields.py
@@ -721,7 +721,7 @@ class related(function):
                         t_id=t_data['id']
                         t_data = t_data[self.arg[i]]
                 if t_id:
-                    obj.pool.get(field_detail['object']).write(cr,uid,[t_id],{args[-1]:values})
+                    obj.pool.get(field_detail['object']).write(cr,uid,[t_id],{args[-1]:values}, context=context)
 
     def _fnct_read(self, obj, cr, uid, ids, field_name, args, context=None):
         self._field_get2(cr, uid, obj, context)
