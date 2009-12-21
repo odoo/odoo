6ff8fbdabbad88d8add9b13e689aa6d24c731fad
Harry (Open ERP) <>
2009-12-21 14:56:03 +0530

    [IMP]: Caldav: Moved fix from hmo@tinyerp.com-20091218102513-1benptetg7q2x4qu to common file for configuration wizard problem
    bzr revid: rpa@tinyerp.com-20091221092603-yblvo0r8jxelwhf5

--- addons/caldav/common.py
+++ addons/caldav/common.py
@@ -204,7 +204,12 @@ virtual_report_spool()
 class virtual_wizard(web_services.wizard):
     def exp_execute(self, db, uid, wiz_id, datas, action='init', context=None):
         if wiz_id not in self.wiz_uid:
-            super(virtual_wizard,self).exp_create(db, uid, 'module.upgrade', datas)
+            # TODO : To Check why need it
+            if wiz_id == 1:
+                wiz_name ='base_setup.base_setup'
+            if wiz_id == 2:
+                wiz_name ='module.upgrade'
+            super(virtual_wizard,self).exp_create(db, uid, wiz_name, datas)
         new_ids = []
         if 'id' in datas:
             datas['id'] = caldevIDs2readIDs(datas['id'])
