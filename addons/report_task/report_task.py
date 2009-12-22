6eb9fc83e08350d69e19f63ebabdb3184f116e2a
Jay (Open ERP) <jvo@tinyerp.com>
2009-12-22 17:08:41 +0530

    [REV/FIX] Reverted bad commit for report_task module
    lp bug: https://launchpad.net/bugs/499060 fixed
    
    bzr revid: jvo@tinyerp.com-20091222113841-8yoa0d9iemw5no8e

--- addons/report_task/report_task.py
+++ addons/report_task/report_task.py
@@ -154,7 +154,7 @@ class report_timesheet_task_user(osv.osv):
             union
                 select to_char(h.name,'YYYY-MM-01') as name,
                 to_char(h.name,'MM') as m_id
-                from hr_timesheet_sheet_sheet_day h) as month) """)
+                from hr_timesheet_sheet_sheet_day h) as months) """)
 
 report_timesheet_task_user()
 
