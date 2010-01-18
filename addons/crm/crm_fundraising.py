321475b45a492da025b9315bea5abd13f01ef67c
Xavier Morel <xmo@tinyerp.com>
2010-01-18 09:42:43 +0100

    [fix] crm: forgotten AVAILABLE_PRIORITIES in crm_fundraising
    bzr revid: xmo@tinyerp.com-20100118084243-eev0ssj2r5bw6ipg

--- addons/crm/crm_fundraising.py
+++ addons/crm/crm_fundraising.py
@@ -103,7 +103,7 @@ class crm_fundraising(osv.osv):
         }
    
     _defaults = {
-                 'priority': lambda *a: AVAILABLE_PRIORITIES[2][0],
+                 'priority': lambda *a: crm.AVAILABLE_PRIORITIES[2][0],
     }
     def onchange_categ_id(self, cr, uid, ids, categ, context={}):
         if not categ:
