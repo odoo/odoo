3ff7980647a2aadf9dbf95cf43a41eb9bb39eb90
Harry (Open ERP) <hmo@tinyerp.com>
2010-01-20 19:21:30 +0530

    [FIX] crm : pass section, description when schedule in meeting
    bzr revid: hmo@tinyerp.com-20100120135130-a0g1ofbi9udn89iu

--- addons/crm/wizard/crm_opportunity_wizard.py
+++ addons/crm/wizard/crm_opportunity_wizard.py
@@ -139,7 +139,9 @@ class opportunity2meeting(wizard.interface):
             new_meeting_id = meeting_case_obj.create(cr, uid, {
                 'name': opportunity.name,
                 'date': opportunity.date,
+                'section_id' : opportunity.section_id and opportunity.section_id.id or False,
                 'date_deadline': opportunity.date_deadline,
+                'description':opportunity.description,
                 })
             new_meeting = meeting_case_obj.browse(cr, uid, new_meeting_id)
             vals = {}
--- addons/crm/wizard/crm_phonecall_wizard.py
+++ addons/crm/wizard/crm_phonecall_wizard.py
@@ -102,8 +102,7 @@ class phonecall2opportunity(wizard.interface):
                 'name': data['form']['name'],
                 'planned_revenue': data['form']['planned_revenue'],
                 'probability': data['form']['probability'],
-                'partner_id': data['form']['partner_id'],
-                'phonecall_id':phonecall.id, 
+                'partner_id': data['form']['partner_id'],                 
                 'section_id':phonecall.section_id.id,
                 'description':phonecall.description,                
             })
@@ -200,7 +199,9 @@ class phonecall2meeting(wizard.interface):
             new_meeting_id = meeting_case_obj.create(cr, uid, {
                 'name': phonecall.name,
                 'date': phonecall.date,
+                'section_id' : phonecall.section_id and phonecall.section_id.id or False,
                 'duration': phonecall.duration,
+                'description':phonecall.description,
                 })
             new_meeting = meeting_case_obj.browse(cr, uid, new_meeting_id)
             vals = {}
