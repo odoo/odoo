132e65a01c3db9c9e9a6cd8d7c49d29949ec6cf3
Xavier Morel <xmo@tinyerp.com>
2010-01-21 13:13:11 +0100

    [add] base transformation of main company setup from base_setup wizard to in-memory res.config
    bzr revid: xmo@tinyerp.com-20100121121311-wi8j7g9q5xu2ptzm

--- addons/base_setup/__init__.py
+++ addons/base_setup/__init__.py
@@ -20,6 +20,7 @@
 ##############################################################################
 
 import installer
+import todo
 import wizard
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
