5392263cec1a71db6db9ab10d0a58b9d1f99249e
sbh (Open ERP) <sbh@tinyerp.com>
2010-01-21 11:14:25 +0530

    [Add]document_change: add a document_change file
    bzr revid: sbh@tinyerp.com-20100121054425-ty9oo2l7sxop4znt

--- addons/document_change/document_change.py
+++ addons/document_change/document_change.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,10 +15,22 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+import tools
+from osv import fields, osv
+import os
+import pooler
+import netsvc
+from tools.translate import _
 
-import document_change
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+class document_type(osv.osv):
+    _name = "document.type"
+    _description = "Document Type"
+    _columns = {
+        'name': fields.char("Document Type", size=64)
+    }
+    
+document_type()
