6ed7e4ca628406c9b1d6ea4d501012c632de12a8
sbh (Open ERP) <sbh@tinyerp.com>
2010-02-03 16:54:57 +0530

    [Fix] crm: set the name of model in send_mail
    bzr revid: sbh@tinyerp.com-20100203112457-skbfxvag02yldp1l

--- addons/crm/wizard/wizard_crm_new_send_email.py
+++ addons/crm/wizard/wizard_crm_new_send_email.py
@@ -70,7 +70,7 @@ def _mass_mail_send(self, cr, uid, data, context):
     attach = map(lambda x: x and ('Attachment'+str(attach.index(x)+1), base64.decodestring(x)), attach)
 
     pool = pooler.get_pool(cr.dbname)
-    case_pool=pool.get('crm.case')
+    case_pool=pool.get(data['model'])
 
     case = case_pool.browse(cr,uid,data['ids'])[0]
     case_pool.write(cr, uid, [case.id], {
@@ -112,7 +112,7 @@ def _get_info(self, cr, uid, data, context):
         return {}
         
     pool = pooler.get_pool(cr.dbname)
-    case = pool.get('crm.lead').browse(cr, uid, data['id'])
+    case = pool.get(data['model']).browse(cr, uid, data['id'])
 
     if not case.user_id:
         raise wizard.except_wizard(_('Error'),_('You must define a responsible user for this case in order to use this action!'))
