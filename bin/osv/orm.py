7dc05435ff40cd9d48d66646c63715d2c1283add
nch@tinyerp.com <>
2010-02-03 17:48:35 +0530

    [FIX]:minor bug related to groupby
    bzr revid: nch@tinyerp.com-20100203121835-abth6i33yy6lftxi

--- bin/osv/orm.py
+++ bin/osv/orm.py
@@ -1836,7 +1836,7 @@ class orm(orm_template):
             if fget.has_key(groupby):
                 if fget[groupby]['type'] == 'many2one':
                     d[groupby] = d[groupby] and d[groupby][1] or ''
-                if fget[groupby]['type'] in ('date','datetime'):
+                if d[groupby] and fget[groupby]['type'] in ('date','datetime'):
                    today = datetime.date.today()
                    if d[groupby][:10] == str(today):
                        d[groupby] = 'Today'
