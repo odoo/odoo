5c119663a033c4a89c93caded88860ba77f06c7c
VIR(OpenERP) <>
2010-02-03 17:56:25 +0530

    [FIX] Ir.sequence : removed unrelated field from view
    lp bug: https://launchpad.net/bugs/516456 fixed
    
    bzr revid: jvo@tinyerp.com-20100203122625-2eog64zqhxpz5mog

--- bin/osv/orm.py
+++ bin/osv/orm.py
@@ -1450,6 +1450,7 @@ class orm_template(object):
                     'client_action_relate', [(self._name, False)], False,
                     context)
             resprint = map(clean, resprint)
+            print "resprintresprint",resprint
             resaction = map(clean, resaction)
             resaction = filter(lambda x: not x.get('multi', False), resaction)
             resprint = filter(lambda x: not x.get('multi', False), resprint)
