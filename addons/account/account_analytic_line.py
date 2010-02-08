231c5c04c24c461c56da577c53a4c932d3993432
Christophe Simonis <chs@tinyerp.com>
2010-02-08 18:17:26 +0100

    [FIX] Unable to validate supplier invoice/refund with reference greater than 32 characters. The reference is used as reference for the move lines and the analytic account line but the reference of these objects are limited to 32 characters, raising an sql error. It also broke the workflow of the invoice.
    lp bug: https://launchpad.net/bugs/518914 fixed
    
    bzr revid: chs@tinyerp.com-20100208171726-3nabrcko8q8zo215

--- addons/account/account_analytic_line.py
+++ addons/account/account_analytic_line.py
@@ -42,7 +42,7 @@ class account_analytic_line(osv.osv):
         'journal_id' : fields.many2one('account.analytic.journal', 'Analytic Journal', required=True, ondelete='cascade', select=True),
         'code' : fields.char('Code', size=8),
         'user_id' : fields.many2one('res.users', 'User',),
-        'ref': fields.char('Ref.', size=32),
+        'ref': fields.char('Ref.', size=64),
     }
     _defaults = {
         'date': lambda *a: time.strftime('%Y-%m-%d'),
