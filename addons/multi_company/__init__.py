fb4db629b490e71537c582d20801e673699e1e58
Joël Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2010-02-11 15:23:26 +0100

    [DEL] Remove the demo data in multi-company module as asked by Fabien
    bzr revid: joel.grandguillaume@camptocamp.com-20100211142326-lc011kadf8kluj64

--- addons/multi_company/__init__.py
+++ addons/multi_company/__init__.py
@@ -18,7 +18,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
 #
 ##############################################################################
-import product
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
