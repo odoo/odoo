0504c236e119268f1e2f406022821ef0bb46cfcd
Harry (Open ERP) <hmo@tinyerp.com>
2010-02-11 12:29:32 +0530

    [IMP+ADD] crm, crm_caldav: Removed caldav stuff from crm module. Now core crm module has  only  simple meeting  future like stable 5.0.
    and crm_caldav has more  futures  like:
            *  Recurrent meeting
            *  Alarms
            *  Invitation facility
            *  Share meeting with other calendar clients like sunbird
    
    bzr revid: hmo@tinyerp.com-20100211065932-lm4lzinl15zq0dqb

--- addons/crm_caldav/crm_meeting.py
+++ addons/crm_caldav/crm_meeting.py
@@ -20,53 +20,24 @@
 ##############################################################################
 
 from osv import fields, osv
-import crm
-from datetime import datetime, timedelta
-from datetime import datetime, timedelta
-
-class crm_opportunity(osv.osv):
-    _name = 'crm.opportunity'
-crm_opportunity()    
-class crm_phonecall(osv.osv):
-    _name = 'crm.phonecall'
-crm_phonecall()    
+from crm import crm
 class crm_meeting(osv.osv):
     _name = 'crm.meeting'
-    _description = "Meeting Cases"
+    _description = "Meetings"
     _order = "id desc"
-    _inherit = "crm.case"
+    _inherit = ["crm.case", "calendar.event"]
 
-    def _get_duration(self, cr, uid, ids, name, arg, context):
-        res = {}
-        for event in self.browse(cr, uid, ids, context=context):
-            start = datetime.strptime(event.date, "%Y-%m-%d %H:%M:%S")
-            res[event.id] = 0
-            if event.date_deadline:
-                end = datetime.strptime(event.date_deadline[:19], "%Y-%m-%d %H:%M:%S")
-                diff = end - start
-                duration =  float(diff.days)* 24 + (float(diff.seconds) / 3600)
-                res[event.id] = round(duration, 2)
-        return res
-
-    def _set_duration(self, cr, uid, id, name, value, arg, context):
-        event = self.browse(cr, uid, id, context=context)
-        start = datetime.strptime(event.date, "%Y-%m-%d %H:%M:%S")
-        end = start + timedelta(hours=value)
-        cr.execute("UPDATE %s set date_deadline='%s' \
-                        where id=%s"% (self._table, end.strftime("%Y-%m-%d %H:%M:%S"), id))
-        return True
-
-    _columns = {
+    _columns = { 
         'priority': fields.selection(crm.AVAILABLE_PRIORITIES, 'Priority'), 
         'categ_id': fields.many2one('crm.case.categ', 'Category', \
                             domain="[('section_id','=',section_id),\
                             ('object_id.model', '=', 'crm.meeting')]", \
             help='Category related to the section.Subdivide the CRM cases \
-independently or section-wise.'), 
-        'duration': fields.function(_get_duration, method=True, \
-                                    fnct_inv=_set_duration, string='Duration'),        
+independently or section-wise.'),            
         'phonecall_id':fields.many2one ('crm.phonecall', 'Phonecall'),        
-        'opportunity_id':fields.many2one ('crm.opportunity', 'Opportunity'),
+        'opportunity_id':fields.many2one ('crm.opportunity', 'Opportunity'),       
+        'attendee_ids': fields.many2many('calendar.attendee', 'event_attendee_rel', 'event_id', 'attendee_id', 'Attendees'),
+        
     }
 
 crm_meeting()
