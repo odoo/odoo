da64215ba1b85199f49b3992c60989620cd04b6c
GPA(OpenERP),Ana <>
2010-02-26 18:38:15 +0530

    [FIX] Project_mrp: Better effect for service products creating tasks/projects
    lp bug: https://launchpad.net/bugs/499645 fixed
    
    bzr revid: jvo@tinyerp.com-20100226130815-8n1o66ml89gqlsbb

--- addons/project_mrp/mrp.py
+++ addons/project_mrp/mrp.py
@@ -30,27 +30,53 @@ class mrp_procurement(osv.osv):
     def action_produce_assign_service(self, cr, uid, ids, context={}):
         for procurement in self.browse(cr, uid, ids):
             sline = self.pool.get('sale.order.line')
-            sale_ids = sline.search(cr, uid, [('procurement_id','=',procurement.id)], context)
             content = ''
+            sale_order = self.pool.get('sale.order')
+            so_ref =  procurement.name.split(':')[0]
+            order_ids = sale_order.search(cr, uid, [('name','=',so_ref)], context)
+
+            if order_ids:
+                sale_ids = sale_order.read(cr, uid, order_ids[0],['order_line'],context=context)['order_line']
+            else:
+                so_ref =  procurement.origin.split(':')[0]
+                sale_ids = sline.search(cr, uid, [('procurement_id','=',procurement.id)], context)
             l = None
             project_id = None
+            analytic_account_id = False
+            partner_id = False
+            
             for line in sline.browse(cr, uid, sale_ids, context=context):
                 content += (line.notes or '')
                 l = line
+                partner_id = line.order_id.partner_id.id
                 if line.order_id.project_id:
+                    analytic_account_id = line.order_id.project_id.id
+                    partner_id = line.order_id.partner_id.id
                     content+="\n\n"+line.order_id.project_id.complete_name
+                    break
             
             # Creating a project for task.Project is created from Procurement.
-            proj_name = tools.ustr(procurement.name)
+            proj_name = tools.ustr(so_ref)
             proj_exist_id = self.pool.get('project.project').search(cr, uid, [('name','=',proj_name)], context=context)
             if  not proj_exist_id:
-                project_id = self.pool.get('project.project').create(cr, uid, {'name':proj_name})
+                project_id = self.pool.get('project.project').create(cr, uid, {'name':proj_name,'category_id':analytic_account_id, 'partner_id':partner_id})
             else:
                 project_id = proj_exist_id[0]
                 
             self.write(cr, uid, [procurement.id], {'state':'running'})
+            
+            name_task = ('','')
+            if procurement.product_id.type == 'service':
+                proc_name = procurement.name
+                if procurement.origin == proc_name:
+                    proc_name = procurement.product_id.name
+                    
+                name_task = (procurement.origin, proc_name or '')
+            else:
+                name_task = (procurement.product_id.name or procurement.origin, procurement.name or '')
+                
             task_id = self.pool.get('project.task').create(cr, uid, {
-                'name': '%s:%s' %(procurement.product_id.name or procurement.origin, procurement.name or ''),
+                'name': '%s:%s' % name_task,
                 'date_deadline': procurement.date_planned,
                 'planned_hours': procurement.product_qty,
                 'remaining_hours': procurement.product_qty,
