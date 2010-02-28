c25e9765f2244642f5f6f165ec1e0498f20de964
Fabien Pinckaers <fp@tinyerp.com>
2010-02-28 21:02:29 +0100

    typo
    bzr revid: fp@tinyerp.com-20100228200229-ijg8rovl1sumc8dj

--- addons/hr_evaluation/__terp__.py
+++ addons/hr_evaluation/__terp__.py
@@ -25,7 +25,7 @@
     "author" : "Tiny",
     "category" : "Generic Modules/Human Resources",
     "website" : "http://www.openerp.com",
-    "depends" : ["hr",'hr_recruitement','survey'],
+    "depends" : ["hr",'hr_recruitment','survey'],
     "description": "Ability to create employees evaluation.",
     "init_xml" : [],
     "demo_xml" : ["hr_evaluation_demo.xml"],
