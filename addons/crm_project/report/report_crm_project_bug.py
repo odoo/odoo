7b2affe88b2758a4fa14502296dc8a1582699c2e
sbh (Open ERP) <sbh@tinyerp.com>
2010-03-03 14:16:16 +0530

    [IMP] report_crm,crm_project: modify the search view and object
    bzr revid: sbh@tinyerp.com-20100303084616-4fbdf15a6mumgy8j

--- addons/crm_project/report/report_crm_project_bug.py
+++ addons/crm_project/report/report_crm_project_bug.py
@@ -1,12 +1,14 @@
 from osv import fields,osv
 import tools
 
-class report_crm_project_bug_user(osv.osv):
-    _name = "report.crm.project.bug.user"
+class report_crm_project_bug(osv.osv):
+    _name = "report.crm.project.bug"
     _description = "Project Bug by user and section"
     _auto = False
-    _inherit = "report.crm.case.user"
+    _inherit = "report.crm.case"
     _columns = {
+        'categ_id': fields.many2one('crm.case.categ', 'Category', domain="[('section_id','=',section_id),('object_id.model', '=', 'crm.project.bug')]"),
+        'stage_id': fields.many2one ('crm.case.stage', 'Stage', domain="[('object_id.model', '=', 'crm.project.bug')]"),                
         'probability': fields.float('Avg. Probability', readonly=True),
         'amount_revenue': fields.float('Est.Revenue', readonly=True),
         'amount_costs': fields.float('Est.Cost', readonly=True),
@@ -14,9 +16,9 @@ class report_crm_project_bug_user(osv.osv):
         'delay_close': fields.char('Delay to close', size=20, readonly=True),
     }
     def init(self, cr):
-        tools.drop_view_if_exists(cr, 'report_crm_project_bug_user')
+        tools.drop_view_if_exists(cr, 'report_crm_project_bug')
         cr.execute("""
-            create or replace view report_crm_project_bug_user as (
+            create or replace view report_crm_project_bug as (
                 select
                     min(c.id) as id,
                     to_char(c.create_date, 'YYYY') as name,
@@ -24,43 +26,8 @@ class report_crm_project_bug_user(osv.osv):
                     c.state,
                     c.user_id,
                     c.section_id,
-                    count(*) as nbr,
-                    sum(planned_revenue) as amount_revenue,
-                    sum(planned_cost) as amount_costs,
-                    sum(planned_revenue*probability)::decimal(16,2) as amount_revenue_prob,
-                    avg(probability)::decimal(16,2) as probability,
-                    to_char(avg(date_closed-c.create_date), 'DD"d" HH24:MI:SS') as delay_close
-                from
-                    crm_project_bug c
-                group by to_char(c.create_date, 'YYYY'), to_char(c.create_date, 'MM'), c.state, c.user_id,c.section_id
-            )""")
-report_crm_project_bug_user()
-
-class report_crm_project_bug_categ(osv.osv):
-    _name = "report.crm.project.bug.categ"
-    _description = "Project Bug by section and category"
-    _auto = False
-    _inherit = "report.crm.case.categ"
-    _columns = {
-        'categ_id': fields.many2one('crm.case.categ', 'Category', domain="[('section_id','=',section_id),('object_id.model', '=', 'crm.project.bug')]"),
-        'amount_revenue': fields.float('Est.Revenue', readonly=True),
-        'amount_costs': fields.float('Est.Cost', readonly=True),
-        'amount_revenue_prob': fields.float('Est. Rev*Prob.', readonly=True),
-        'probability': fields.float('Avg. Probability', readonly=True),
-        'delay_close': fields.char('Delay Close', size=20, readonly=True),
-    }
-    
-    def init(self, cr):
-        tools.drop_view_if_exists(cr, 'report_crm_project_bug_categ')
-        cr.execute("""
-            create or replace view report_crm_project_bug_categ as (
-                select
-                    min(c.id) as id,
-                    to_char(c.create_date, 'YYYY') as name,
-                    to_char(c.create_date, 'MM') as month,
                     c.categ_id,
-                    c.state,
-                    c.section_id,
+                    c.stage_id,
                     count(*) as nbr,
                     sum(planned_revenue) as amount_revenue,
                     sum(planned_cost) as amount_costs,
@@ -69,63 +36,9 @@ class report_crm_project_bug_categ(osv.osv):
                     to_char(avg(date_closed-c.create_date), 'DD"d" HH24:MI:SS') as delay_close
                 from
                     crm_project_bug c
-                group by c.categ_id,to_char(c.create_date, 'YYYY'), to_char(c.create_date, 'MM'), c.state,c.section_id
+                group by to_char(c.create_date, 'YYYY'), to_char(c.create_date, 'MM'), c.state, c.user_id,c.section_id,c.categ_id,c.stage_id
             )""")
-report_crm_project_bug_categ()
+report_crm_project_bug()
 
-class report_crm_project_bug_section(osv.osv):
-    _name = "report.crm.project.bug.section"
-    _description = "Project bug by Section"
-    _auto = False
-    _inherit = "report.crm.case.section"
-    
-    def _get_data(self, cr, uid, ids, field_name, arg, context={}):
-        res = {}
-        state_perc = 0.0
-        avg_ans = 0.0
-        
-        for case in self.browse(cr, uid, ids, context):
-            if field_name != 'avg_answers':
-                state = field_name[5:]
-                cr.execute("select count(*) from crm_project_bug where section_id =%s and state='%s'"%(case.section_id.id,state))
-                state_cases = cr.fetchone()[0]
-                perc_state = (state_cases / float(case.nbr_cases) ) * 100
-                
-                res[case.id] = perc_state
-            else:
-                cr.execute('select count(*) from crm_case_log l  where l.section_id=%s'%(case.section_id.id))
-                logs = cr.fetchone()[0]
-                
-                avg_ans = logs / case.nbr_cases
-                res[case.id] = avg_ans       
-        
-        return res
-    
-    _columns = {
-        'avg_answers': fields.function(_get_data,string='Avg. Answers', method=True,type="integer"),
-        'perc_done': fields.function(_get_data,string='%Done', method=True,type="float"),
-        'perc_cancel': fields.function(_get_data,string='%Cancel', method=True,type="float"),
-        'delay_close': fields.char('Delay to close', size=20, readonly=True),
-    }
-    _order = 'name desc, section_id'
-    def init(self, cr):
-        tools.drop_view_if_exists(cr, 'report_crm_project_bug_section')
-        cr.execute("""
-            create or replace view report_crm_project_bug_section as (
-                select
-                    min(c.id) as id,
-                    to_char(c.create_date, 'YYYY') as name,
-                    to_char(c.create_date, 'MM') as month,
-                    count(*) as nbr_cases,
-                    c.section_id as section_id,
-                    0 as avg_answers,
-                    0.0 as perc_done,
-                    0.0 as perc_cancel,
-                    to_char(avg(date_closed-c.create_date), 'DD"d" HH24:MI:SS') as delay_close
-                from
-                    crm_project_bug c
-                group by to_char(c.create_date, 'YYYY'),to_char(c.create_date, 'MM'),c.section_id
-            )""")
-report_crm_project_bug_section()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
