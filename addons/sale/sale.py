76c6da87917f369c2fd606e598b808306be4818c
Fabien Pinckaers <fp@tinyerp.com>
2010-03-03 14:59:07 +0100

    merge
    bzr revid: fp@tinyerp.com-20100303135907-28m82yckqmlb25lb

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -198,7 +198,7 @@ class sale_order(osv.osv):
             ], 'Order State', readonly=True, help="Gives the state of the quotation or sale order. The exception state is automatically set when a cancel operation occurs in the invoice validation (Invoice Exception) or in the picking list process (Shipping Exception). The 'Waiting Schedule' state is set when the invoice is confirmed but waiting for the scheduler to run on the date 'Ordered Date'.", select=True),
         'date_order': fields.date('Ordered Date', required=True, readonly=True, states={'draft': [('readonly', False)]}),
         'create_date': fields.date('Creation Date', readonly=True),
-        'date_confirm': fields.date('confirmation Date', readonly=True),
+        'date_confirm': fields.date('Confirmation Date', readonly=True),
         'user_id': fields.many2one('res.users', 'Salesman', states={'draft': [('readonly', False)]}, select=True),
         'partner_id': fields.many2one('res.partner', 'Customer', readonly=True, states={'draft': [('readonly', False)]}, required=True, change_default=True, select=True),
         'partner_invoice_id': fields.many2one('res.partner.address', 'Invoice Address', readonly=True, required=True, states={'draft': [('readonly', False)]}),
