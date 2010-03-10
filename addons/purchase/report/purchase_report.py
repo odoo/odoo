c9be1c2271f987177a41fb9ec2d16c6ed79a8584
apa-tiny <apa@tinyerp.com>
2010-03-10 12:57:42 +0530

    [IMP]:Improved SQL purchase report.
    bzr revid: apa@tinyerp.com-20100310072742-28lym17cjluthmf6

--- addons/purchase/report/purchase_report.py
+++ addons/purchase/report/purchase_report.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -26,11 +26,12 @@
 from osv import fields,osv
 import tools
 
-class report_purchase_order_product(osv.osv):
-    _name = "report.purchase.order.product"
-    _description = "Purchases Orders by Products"
+class report_purchase_order(osv.osv):
+    _name = "report.purchase.order"
+    _description = "Purchases Orders"
     _auto = False
     _columns = {
+        'date': fields.date('Date', readonly=True),
         'name': fields.char('Year',size=64,required=False, readonly=True),
         'state': fields.selection([
             ('draft','Quotation'),
@@ -43,6 +44,10 @@ class report_purchase_order_product(osv.osv):
             ('cancel','Cancel')
         ], 'Order State', readonly=True),
         'product_id':fields.many2one('product.product', 'Product', readonly=True),
+        'category_id': fields.many2one('product.category', 'Categories', readonly=True),
+        'partner_id':fields.many2one('res.partner', 'Partner', readonly=True),
+        'company_id':fields.many2one('res.company', 'Company', readonly=True),
+        'user_id':fields.many2one('res.users', 'Responsible', readonly=True),
         'quantity': fields.float('# of Products', readonly=True),
         'price_total': fields.float('Total Price', readonly=True),
         'price_average': fields.float('Average Price', readonly=True),
@@ -53,62 +58,19 @@ class report_purchase_order_product(osv.osv):
     }
     _order = 'name desc,price_total desc'
     def init(self, cr):
-        tools.sql.drop_view_if_exists(cr, 'report_purchase_order_product')
+        tools.sql.drop_view_if_exists(cr, 'report_purchase_order')
         cr.execute("""
-            create or replace view report_purchase_order_product as (
+            create or replace view report_purchase_order as (
                 select
                     min(l.id) as id,
+                    s.date_order as date,
                     to_char(s.date_order, 'YYYY') as name,
                     to_char(s.date_order, 'MM') as month,
                     s.state,
+                    s.partner_id as partner_id,
+                    s.create_uid as user_id,
+                    s.company_id as company_id,
                     l.product_id,
-                    sum(l.product_qty*u.factor) as quantity,
-                    count(*),
-                    sum(l.product_qty*l.price_unit) as price_total,
-                    (sum(l.product_qty*l.price_unit)/sum(l.product_qty*u.factor))::decimal(16,2) as price_average
-                from purchase_order s
-                    left join purchase_order_line l on (s.id=l.order_id)
-                    left join product_uom u on (u.id=l.product_uom)
-                where l.product_id is not null
-                group by l.product_id, to_char(s.date_order, 'YYYY'),to_char(s.date_order, 'MM'),s.state
-            )
-        """)
-report_purchase_order_product()
-
-class report_purchase_order_category(osv.osv):
-    _name = "report.purchase.order.category"
-    _description = "Purchases Orders by Categories"
-    _auto = False
-    _columns = {
-        'name': fields.char('Year',size=64,required=False, readonly=True),
-        'state': fields.selection([
-            ('draft','Quotation'),
-            ('waiting_date','Waiting Schedule'),
-            ('manual','Manual in progress'),
-            ('progress','In progress'),
-            ('shipping_except','Shipping Exception'),
-            ('invoice_except','Invoice Exception'),
-            ('done','Done'),
-            ('cancel','Cancel')
-        ], 'Order State', readonly=True),
-        'category_id': fields.many2one('product.category', 'Categories', readonly=True),
-        'quantity': fields.float('# of Products', readonly=True),
-        'price_total': fields.float('Total Price', readonly=True),
-        'price_average': fields.float('Average Price', readonly=True),
-        'count': fields.integer('# of Lines', readonly=True),
-        'month':fields.selection([('01','January'), ('02','February'), ('03','March'), ('04','April'), ('05','May'), ('06','June'),
-                  ('07','July'), ('08','August'), ('09','September'), ('10','October'), ('11','November'), ('12','December')],'Month',readonly=True),
-    }
-    _order = 'name desc,price_total desc'
-    def init(self, cr):
-        tools.sql.drop_view_if_exists(cr, 'report_purchase_order_category')
-        cr.execute("""
-            create or replace view report_purchase_order_category as (
-                select
-                    min(l.id) as id,
-                    to_char(s.date_order, 'YYYY') as name,
-                    to_char(s.date_order, 'MM') as month,
-                    s.state,
                     t.categ_id as category_id,
                     sum(l.product_qty*u.factor) as quantity,
                     count(*),
@@ -120,10 +82,12 @@ class report_purchase_order_category(osv.osv):
                     left join product_template t on (t.id=p.product_tmpl_id)
                     left join product_uom u on (u.id=l.product_uom)
                 where l.product_id is not null
-                group by t.categ_id, to_char(s.date_order, 'YYYY'),to_char(s.date_order, 'MM'),s.state
-             )
+                group by s.company_id,s.create_uid,s.partner_id,
+                         t.categ_id,l.product_id,s.date_order,
+                         to_char(s.date_order, 'YYYY'),to_char(s.date_order, 'MM'),s.state
+            )
         """)
-report_purchase_order_category()
+report_purchase_order()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
