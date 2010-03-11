723b0b10882bb3948bc094a2469ef96971c7cf5f
Xavier Morel <xmo@tinyerp.com>
2010-03-11 15:09:57 +0100

    [fix] disabled "code" server action due to the security threat it poses, added --enable-code-actions command-line flag to enable it
    bzr revid: xmo@tinyerp.com-20100311140957-89p42puy6u8c8v8p

--- bin/addons/base/ir/ir_actions.py
+++ bin/addons/base/ir/ir_actions.py
@@ -519,19 +519,26 @@ class actions_server(osv.osv):
                 return result
 
             if action.state=='code':
-                localdict = {
-                    'self': self.pool.get(action.model_id.model),
-                    'context': context,
-                    'time': time,
-                    'ids': ids,
-                    'cr': cr,
-                    'uid': uid,
-                    'object':obj,
-                    'obj': obj,
-                }
-                exec action.code in localdict
-                if 'action' in localdict:
-                    return localdict['action']
+                if config['server_actions_allow_code']:
+                    localdict = {
+                        'self': self.pool.get(action.model_id.model),
+                        'context': context,
+                        'time': time,
+                        'ids': ids,
+                        'cr': cr,
+                        'uid': uid,
+                        'object':obj,
+                        'obj': obj,
+                        }
+                    exec action.code in localdict
+                    if 'action' in localdict:
+                        return localdict['action']
+                else:
+                    netsvc.Logger().notifyChannel(
+                        self._name, netsvc.LOG_ERROR,
+                        "%s is a `code` server action, but "
+                        "it isn't allowed in this configuration.\n\n"
+                        "See server options to enable it"%action)
 
             if action.state == 'email':
                 user = config['email_from']
