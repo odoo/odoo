e8c8414c8b242ae0cd7ebb92a7449b0e96a936f0
ksa (Open ERP) <ksa@tinyerp.co.in>
2010-03-17 19:11:06 +0530

    [IMP]: olap: code optimization and apply csv file and doc string
    bzr revid: ksa@tinyerp.co.in-20100317134106-p2f47zevn98vnxhi

--- addons/olap/wizard/olap_load_column.py
+++ addons/olap/wizard/olap_load_column.py
@@ -1,27 +1,21 @@
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (c) 2005-2006 TINY SPRL. (http://tiny.be) All Rights Reserved.
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsability of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# garantees and support are strongly adviced to contract a Free Software
-# Service Company
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -32,6 +26,7 @@ class olap_load_column(osv.osv_memory):
     _description = "Olap Load Column"
 
     def get_table_data(self, cr, uid, ids, context={}):
+
         """
         This function load column
         @param cr: the current row, from the database cursor,
@@ -41,13 +36,13 @@ class olap_load_column(osv.osv_memory):
 
         """
         data = context and context.get('active_id', False) or False
-        ids_cols = self.pool.get('olap.database.columns').search(cr, uid,\
-                             ([('table_id','=',data)]),context={})
-        model_data_ids = self.pool.get('ir.model.data').search(cr,uid,\
-                        [('model','=','ir.ui.view'),\
-                    ('name','=','view_olap_database_columns_form')],context={})
-        resource_id = self.pool.get('ir.model.data').read(cr,uid,\
-                                model_data_ids,fields=['res_id'])[0]['res_id']
+        ids_cols = self.pool.get('olap.database.columns').search(cr, uid, \
+                             ([('table_id', '=', data)]), context={})
+        model_data_ids = self.pool.get('ir.model.data').search(cr, uid, \
+                            [('model', '=', 'ir.ui.view'), \
+                            ('name', '=', 'view_olap_database_columns_form')], context={})
+        resource_id = self.pool.get('ir.model.data').read(cr, uid, \
+                                model_data_ids, fields=['res_id'])[0]['res_id']
         return {
             'domain': "[('id','in', ["+','.join(map(str,ids_cols))+"])]",
             'name': 'Database Columns',
@@ -57,9 +52,6 @@ class olap_load_column(osv.osv_memory):
             'views': [(False,'tree'),(resource_id,'form')],
             'type': 'ir.actions.act_window',
         }
-    _columns = {
-
-            }
 
 olap_load_column()
 
