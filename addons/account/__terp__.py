36ed0af6068757b93cc21116bbfa2fe56915af5e
apa-tiny <apa@tinyerp.com>
2010-03-17 17:00:22 +0530

    [ADD]:Added SQL view for Analyatic Accounts.
    bzr revid: apa@tinyerp.com-20100317113022-idk8rcm184pqcizq

--- addons/account/__terp__.py
+++ addons/account/__terp__.py
@@ -63,6 +63,7 @@
         'account_installer.xml',
         'report/account_invoice_report_view.xml',
         'report/account_report_view.xml',
+        'report/account_analytic_report_view.xml',
     ],
     'demo_xml': [
         #'demo/price_accuracy00.yml',
