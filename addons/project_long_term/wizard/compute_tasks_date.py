e2067637dc09b2ca8b3804b63472378d1aebae13
psi (Open ERP) <psi@tinyerp.co.in>
2010-03-23 16:56:25 +0530

    [IMP] project_long_term: Set focus and add icon on button on wizards
    bzr revid: psi@tinyerp.co.in-20100323112625-wwpzyp2oeiyce3kb

--- addons/project_long_term/wizard/compute_tasks_date.py
+++ addons/project_long_term/wizard/compute_tasks_date.py
@@ -147,8 +147,8 @@ class wizard_compute_tasks(wizard.interface):
         'init': {
             'actions': [],
             'result': {'type': 'form', 'arch': compute_form, 'fields': compute_fields, 'state': [
-                ('end', 'Cancel'),
-                ('compute', 'Compute')
+                ('end', 'Cancel', 'gtk-cancel'),
+                ('compute', 'Compute', 'gtk-ok', True)
             ]},
         },
         'compute': {
