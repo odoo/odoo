1abdc8745180ceabd27d9818d27d5876448d0a5b
psi (Open ERP) <psi@tinyerp.co.in>
2010-03-23 16:52:10 +0530

    [IMP] project_gtd: Set focus on button on wizards
    bzr revid: psi@tinyerp.co.in-20100323112210-5cwl0cbg4h3su05b

--- addons/project_gtd/wizard/project_gtd_fill.py
+++ addons/project_gtd/wizard/project_gtd_fill.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -61,7 +61,7 @@ class wiz_timebox_fill(wizard.interface):
                 'arch':_gtd_arch,
                 'fields':_gtd_field,
                 'state':[
-                    ('end','Cancel'),
+                    ('end','Cancel', 'gtk-cancel'),
                     ('process','Add to Timebox')
                 ]
             }
