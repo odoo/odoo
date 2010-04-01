80a2d0463f261e39ce77e3d8c0128e529133111b
ksa (Open ERP) <ksa@tinyerp.co.in>
2010-04-01 17:01:30 +0530

    [IMP]: crm: Apply some changes and add function
    bzr revid: ksa@tinyerp.co.in-20100401113130-48zf5bsafdt1x3p6

--- addons/crm/crm_opportunity.py
+++ addons/crm/crm_opportunity.py
@@ -21,6 +21,7 @@
 
 from datetime import datetime
 from osv import fields,osv,orm
+from tools.translate import _
 import crm
 
 AVAILABLE_STATES = [
@@ -165,6 +166,58 @@ class crm_opportunity(osv.osv):
         'company_id': lambda s,cr,uid,c: s.pool.get('res.company')._company_default_get(cr, uid, 'crm.opportunity', context=c),
         'priority': lambda *a: crm.AVAILABLE_PRIORITIES[2][0],
     }
+
+    def action_makeMeeting(self, cr, uid, ids, context=None):
+        """
+        This opens Meeting's calendar view to schedule meeting on current Opportunity
+        @param self: The object pointer
+        @param cr: the current row, from the database cursor,
+        @param uid: the current user’s ID for security checks,
+        @param ids: List of Opportunity to Meeting IDs
+        @param context: A standard dictionary for contextual values
+
+        @return : Dictionary value for created Meeting view
+        """
+        value = {}
+        for opp in self.browse(cr, uid, ids):
+            data_obj = self.pool.get('ir.model.data')
+
+            # Get meeting views
+            result = data_obj._get_id(cr, uid, 'crm', 'view_crm_case_meetings_filter')
+            res = data_obj.read(cr, uid, result, ['res_id'])
+            id1 = data_obj._get_id(cr, uid, 'crm', 'crm_case_calendar_view_meet')
+            id2 = data_obj._get_id(cr, uid, 'crm', 'crm_case_form_view_meet')
+            id3 = data_obj._get_id(cr, uid, 'crm', 'crm_case_tree_view_meet')
+            if id1:
+                id1 = data_obj.browse(cr, uid, id1, context=context).res_id
+            if id2:
+                id2 = data_obj.browse(cr, uid, id2, context=context).res_id
+            if id3:
+                id3 = data_obj.browse(cr, uid, id3, context=context).res_id
+
+            context = {
+                'default_opportunity_id': opp.id,
+                'default_partner_id': opp.partner_id and opp.partner_id.id or False,
+                'default_section_id': opp.section_id and opp.section_id.id or False,
+                'default_email_from': opp.email_from,
+                'default_state': 'open',
+                'default_name': opp.name
+            }
+            value = {
+                'name': _('Meetings'),
+                'domain': "[('user_id','=',%s)]" % (uid),
+                'context': context,
+                'view_type': 'form',
+                'view_mode': 'calendar,form,tree',
+                'res_model': 'crm.meeting',
+                'view_id': False,
+                'views': [(id1, 'calendar'), (id2, 'form'), (id3, 'tree')],
+                'type': 'ir.actions.act_window',
+                'search_view_id': res['res_id'],
+                'nodestroy': True
+            }
+
+        return value
 
 crm_opportunity()
 
--- addons/crm/wizard/__init__.py
+++ addons/crm/wizard/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -24,13 +24,9 @@ import crm_email_add_cc
 
 import crm_lead_to_partner
 import crm_lead_to_opportunity
-
-import crm_opportunity_to_meeting
 import crm_opportunity_to_phonecall
-
 import crm_phonecall_to_phonecall
 import crm_phonecall_to_partner
-import crm_phonecall_to_meeting
 import crm_phonecall_to_opportunity
 import crm_partner_to_opportunity
 
