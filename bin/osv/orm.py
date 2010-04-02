792a2bc8ba9ef47e11b8fd2731384b63937c5f33
Jay (OpenERP) <>
2010-04-02 13:32:46 +0200

    [FIX] Behaviour of fields.related corrected for copy() and write() calls
    lp bug: https://launchpad.net/bugs/544087 fixed
    
    bzr revid: stephane@openerp.com-20100402113246-dq42pvoiy5n1xh7f

--- bin/osv/orm.py
+++ bin/osv/orm.py
@@ -2796,7 +2796,8 @@ class orm(orm_template):
                     upd1 = upd1 + ',' + self._columns[field]._symbol_set[0]
                     upd2.append(self._columns[field]._symbol_set[1](vals[field]))
                 else:
-                    upd_todo.append(field)
+                    if not isinstance(self._columns[field],fields.related):
+                        upd_todo.append(field)
             if field in self._columns \
                     and hasattr(self._columns[field], 'selection') \
                     and vals[field]:
