9515a0096430d6ce5d52ab0d28b9b432b37227de
Stephane Wirtel <stephane@openerp.com>
2010-04-06 19:04:44 +0200

    [RELEASE] OpenERP 5.0.9
    bzr revid: stephane@openerp.com-20100406170444-6ejgfdl1ldqrpcpj

--- bin/release.py
+++ bin/release.py
@@ -22,7 +22,7 @@
 ##############################################################################
 
 name = 'openerp-server'
-version = '5.0.8'
+version = '5.0.9'
 major_version = '5.0'
 description = 'OpenERP Server'
 long_desc = '''\
