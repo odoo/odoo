2be4475cbae79cb8d8cdcf7bf32761a4e372710c
mra (Open ERP) <mra@tinyerp.com>
2010-04-07 11:54:16 +0530

    [ADD] project_gtd: Project timebox empty wizard => osv memory
    bzr revid: mra@tinyerp.com-20100407062416-zjbwr9zme1zxroqs

--- addons/project_gtd/__terp__.py
+++ addons/project_gtd/__terp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -45,6 +45,7 @@ performing those tasks.
         'project_gtd_view.xml',
         'project_gtd_wizard.xml',
         'security/ir.model.access.csv',
+        'wizard/project_gtd_empty_view.xml'
     ],
     'demo_xml': ['project_gtd_demo.xml'],
     'installable': True,
