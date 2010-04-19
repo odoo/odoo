30f690ae7edd5e2451e501c8524729a1a49080e7
Fabien Pinckaers <fp@tinyerp.com>
2010-04-20 00:09:47 +0200

    [IMP] menu display search filter
    bzr revid: fp@tinyerp.com-20100419220947-pbswr72tsds5exci

--- bin/addons/base/ir/ir_ui_menu.py
+++ bin/addons/base/ir/ir_ui_menu.py
@@ -69,9 +69,6 @@ class ir_ui_menu(osv.osv):
             context = {}
         ids = osv.orm.orm.search(self, cr, uid, args, offset, limit, order, context=context, count=(count and uid==1))
 
-        #if uid==1:
-        #    return ids
-
         if not ids:
             if count:
                 return 0
@@ -86,8 +83,8 @@ class ir_ui_menu(osv.osv):
             if key in self._cache:
                 if self._cache[key]:
                     result.append(menu.id)
-                elif not menu.groups_id and not menu.action:
-                    result.append(menu.id)
+                #elif not menu.groups_id and not menu.action:
+                #    result.append(menu.id)
                 continue
 
             self._cache[key] = False
