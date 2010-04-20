743b36fa38c1a68f302f630d6fc5ea89772c9dc3
rch (Open ERP) <>
2010-04-20 13:03:35 +0530

    [IMP]: Improve SQL view report for project_issue module.
    bzr revid: rch_open_erp-20100420073335-v3u5b3kbjvrbzx86

--- addons/project_issue/report/project_issue_report.py
+++ addons/project_issue/report/project_issue_report.py
@@ -10,7 +10,8 @@ class project_issue_report(osv.osv):
         'categ_id': fields.many2one('crm.case.categ', 'Category', domain="[('section_id','=',section_id),('object_id.model', '=', 'project.issue')]"),
         'stage_id': fields.many2one ('crm.case.stage', 'Stage', domain="[('object_id.model', '=', 'project.issue')]"),
         'nbr': fields.integer('# of Issues', readonly=True),
-        'delay_close': fields.char('Delay to close', size=20, readonly=True),
+        'delay_close': fields.float('Avg Closing Delay', digits=(16,2), readonly=True, group_operator="avg",
+                                       help="Number of Days to close the project issue"),
         'company_id' : fields.many2one('res.company', 'Company'),
         'priority': fields.selection(crm.AVAILABLE_PRIORITIES, 'Priority'),
         'project_id':fields.many2one('project.project', 'Project',readonly=True),
@@ -20,7 +21,8 @@ class project_issue_report(osv.osv):
         'assigned_to' : fields.many2one('res.users', 'Assigned to',readonly=True),
         'partner_id': fields.many2one('res.partner','Partner',domain="[('object_id.model', '=', 'project.issue')]"),
         'canal_id': fields.many2one('res.partner.canal', 'Channel',readonly=True),
-        'task_id': fields.many2one('project.task', 'Task',domain="[('object_id.model', '=', 'project.issue')]" )
+        'task_id': fields.many2one('project.task', 'Task',domain="[('object_id.model', '=', 'project.issue')]" ),
+        'day': fields.char('Day', size=128, readonly=True)
     }
     def init(self, cr):
         tools.drop_view_if_exists(cr, 'project_issue_report')
@@ -30,6 +32,7 @@ class project_issue_report(osv.osv):
                     min(c.id) as id,
                     to_char(c.create_date, 'YYYY') as name,
                     to_char(c.create_date, 'MM') as month,
+                    to_char(c.create_date, 'YYYY-MM-DD') as day,
                     c.state,
                     c.user_id,
                     c.section_id,
@@ -46,14 +49,30 @@ class project_issue_report(osv.osv):
                     c.canal_id,
                     c.task_id,
                     date_trunc('day',c.create_date) as create_date,
-                    to_char(avg(date_closed-c.create_date), 'DD"d" HH24:MI:SS') as delay_close
+                    avg(extract('epoch' from (c.date_closed-c.create_date)))/(3600*24) as  delay_close
                 from
                     project_issue c
                 left join
                     res_users u on (c.id = u.id)
-                group by to_char(c.create_date, 'YYYY'), to_char(c.create_date, 'MM'), c.state, c.user_id,c.section_id,c.categ_id,c.stage_id
-                ,c.date_closed,u.company_id,c.priority,c.project_id,c.type_id
-                ,date_trunc('day',c.create_date),c.assigned_to,c.partner_id,c.canal_id,c.task_id
+                group by
+                    to_char(c.create_date, 'YYYY'),
+                    to_char(c.create_date, 'MM'),
+                    to_char(c.create_date, 'YYYY-MM-DD'),
+                    c.state,
+                    c.user_id,
+                    c.section_id,
+                    c.categ_id,
+                    c.stage_id,
+                    c.date_closed,
+                    u.company_id,
+                    c.priority,
+                    c.project_id,
+                    c.type_id,
+                    date_trunc('day',c.create_date),
+                    c.assigned_to,
+                    c.partner_id,
+                    c.canal_id,
+                    c.task_id
             )""")
 
 
