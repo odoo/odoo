56406bebe409e0f6f98381644f483677a53785a2
mra (Open ERP) <mra@tinyerp.com>
2010-04-23 14:43:23 +0530

    [ADD] hr_recruitement: Create Partner for job applicant => osv memory
    bzr revid: mra@tinyerp.com-20100423091323-wmcxo1b378klm7bw

--- addons/hr_recruitment/__openerp__.py
+++ addons/hr_recruitment/__openerp__.py
@@ -40,7 +40,6 @@ system to store and search in your CV base.
     ],
     'update_xml': [
         'wizard/hr_recruitment_phonecall_view.xml',
-        'hr_recruitment_wizard.xml',
         'hr_recruitment_view.xml',
         'hr_recruitment_menu.xml',
 #        'report_hr_recruitment_view.xml',
@@ -48,6 +47,7 @@ system to store and search in your CV base.
         'security/ir.model.access.csv',
         'board_hr_recruitment_statistical_view.xml',
         'report/hr_recruitment_report_view.xml',
+        'wizard/hr_recruitment_create_partner_job_view.xml',
      ],
     'demo_xml': [
         'hr_recruitment_demo.xml'
--- addons/hr_recruitment/wizard/__init__.py
+++ addons/hr_recruitment/wizard/__init__.py
@@ -21,9 +21,7 @@
 
 
 import hr_recruitment_phonecall
-
-
-
+import hr_recruitment_create_partner_job
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
