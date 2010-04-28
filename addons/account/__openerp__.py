832d8604a7d0d513ed97a91ff9fc0e581329dd5d
psi (Open ERP) <psi@tinyerp.co.in>
2010-04-28 10:19:23 +0530

    [ADD] account/project/wizard : convert account_analytic_cost_ledger wizard to osv_memory
    bzr revid: psi@tinyerp.co.in-20100428044923-ptsxxzmhpq17oh7a

--- addons/account/__openerp__.py
+++ addons/account/__openerp__.py
@@ -92,6 +92,7 @@ module named account_voucherss
         'project/project_report.xml',
         'project/wizard/account_analytic_check_view.xml',
         'project/wizard/account_analytic_balance_report_view.xml',
+        'project/wizard/account_analytic_cost_ledger_view.xml',
         'product_view.xml',
         'account_assert_test.xml',
         'process/statement_process.xml',
