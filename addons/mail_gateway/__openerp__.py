f7e79ce200a4b5ef2a9bd36443aed698475c6f22
rpa (Open ERP) <rpa@tinyerp.com>
2010-04-29 17:29:23 +0530

    [IMP]: fetchmail, mail_gateway: changes for * fetchmail depends on mailgate, * fecthmail does not depends on smtpclient
    bzr revid: rpa@tinyerp.com-20100429115923-ng88qf17lpat0viy

--- addons/mail_gateway/__openerp__.py
+++ addons/mail_gateway/__openerp__.py
@@ -32,7 +32,7 @@
     * Easy Integration with any Module""",
     'author': 'Tiny',
     'website': 'http://www.openerp.com',
-    'depends': ['smtpclient', 'fetchmail'],
+    'depends': ['smtpclient'],
     'init_xml': [],
     'update_xml': [
         
