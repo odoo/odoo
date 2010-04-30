cc31ffeb59f320330590fa0d3b97fca2bc8f80e9
psi (Open ERP) <psi@tinyerp.co.in>
2010-04-30 11:47:19 +0530

    [FIX] project_issue: small change in default project function
    bzr revid: psi@tinyerp.co.in-20100430061719-7w9cir9cd7ck20mw

--- addons/project_issue/project_issue.py
+++ addons/project_issue/project_issue.py
@@ -133,7 +133,7 @@ class project_issue(osv.osv):
     def _get_project(self, cr, uid, context):
        user = self.pool.get('res.users').browse(cr,uid,uid, context=context)
        if user.context_project_id:
-           return user.context_project_id
+           return user.context_project_id.id
        return False
 
     def convert_issue_task(self, cr, uid, ids, context=None):
