13365268dbd28738db870f3bb5abf2778634ad30
mra (Open ERP) <mra@tinyerp.com>
2010-04-30 12:33:21 +0530

    [ADD] Scrum: Create task from backlog wizard => OSV memory
    bzr revid: mra@tinyerp.com-20100430070321-bqm5ou9u63nv72z4

--- addons/scrum/__openerp__.py
+++ addons/scrum/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -54,7 +54,8 @@
         'scrum_report.xml',
         'scrum_wizard.xml',
         'scrum_view.xml',
-        'process/scrum_process.xml'
+        'process/scrum_process.xml',
+        'wizard/scrum_backlog_create_task_view.xml'
     ],
     'demo_xml': ['scrum_demo.xml'],
     'installable': True,
