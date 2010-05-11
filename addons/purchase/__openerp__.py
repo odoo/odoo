9a8d8ef7caae2bb2013538e05770f2a6433360e1
gpa <gpa@tinyerp.com>
2010-05-11 13:58:56 +0530

    [MERGE] merge donkirkby's patch to re-enable purchase module smoke test
    bzr revid: gpa@tinyerp.com-20100511082856-fa5dgasx3xou1lg6

--- addons/purchase/__openerp__.py
+++ addons/purchase/__openerp__.py
@@ -50,7 +50,7 @@
         'process/purchase_process.xml',
         'report/purchase_report_view.xml',
     ],
-    'demo_xml': ['purchase_demo.xml'],
+    'demo_xml': ['purchase_demo.xml','purchase_unit_test.xml'],
     'installable': True,
     'active': False,
     'certificate': '0057234283549',
