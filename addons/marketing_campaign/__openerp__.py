e74b683ab5101ce1e495e097e6cfd87129a8eba5
ssi (Open ERP) <ssi@tinyerp.com>
2010-05-28 12:54:14 +0530

    [IMP] marketing_campaign: modified menus and dependency
    bzr revid: ssi@tinyerp.com-20100528072414-8uatbvbl7g3rlrj4

--- addons/marketing_campaign/__openerp__.py
+++ addons/marketing_campaign/__openerp__.py
@@ -23,7 +23,8 @@
 {
     "name" : "",
     "version" : "1.1",
-    "depends" : ["crm", 
+    "depends" : ["marketing",
+                 "crm", 
                 "document",
                 "poweremail" # need to get this module form the branch 
                 # lp:poweremail  
@@ -34,11 +35,13 @@
     """,
     'website': 'http://www.openerp.com',
     'init_xml': [],
+    'demo_xml':[],
     'update_xml': [
         'marketing_campaign_view.xml',
         'marketing_campaign_data.xml',    
         'marketing_campaign_workflow.xml',    
         'report/campaign_analysis_view.xml',
+        'marketing_campaign_demo.xml'
     ],
     'demo_xml': [
         'marketing_campaign_demo.xml',
