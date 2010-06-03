9b4bc3c5eaf4bc7eb791b554432b0ec4dfd38270
Vir (Open ERP) <vir@tinyerp.com>
2010-06-03 19:11:46 +0530

    [ADD] New icon image added
    bzr revid: vir@tinyerp.com-20100603134146-bwiny8w7c8f13fxh

--- bin/tools/misc.py
+++ bin/tools/misc.py
@@ -1137,7 +1137,7 @@ icons = map(lambda x: (x,x), ['STOCK_ABOUT', 'STOCK_ADD', 'STOCK_APPLY', 'STOCK_
 'terp-face-plain','terp-folder-blue','terp-folder-green','terp-folder-orange','terp-folder-yellow',
 'terp-gdu-smart-failing','terp-go-week','terp-gtk-select-all','terp-locked','terp-mail-forward',
 'terp-mail-message-new','terp-mail-replied','terp-rating-rated','terp-stage','terp-stock_format-scientific',
-'terp-dolar_ok!'
+'terp-dolar_ok!','terp-idea'
 ])
 
 def extract_zip_file(zip_file, outdirectory):
