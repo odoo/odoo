2319190c5a20cb8095f85cacca89a33164ba93d7
Harry (OpenERP) <hmo@tinyerp.com>
2010-06-21 17:22:19 +0530

    [FIX] document_webdav: connection problem after login
    bzr revid: hmo@tinyerp.com-20100621115219-wagfjnes7l01tul2

--- addons/document_webdav/webdav_server.py
+++ addons/document_webdav/webdav_server.py
@@ -43,8 +43,12 @@ def OpenDAVConfig(**kw):
         def __init__(self, **kw):
             self.__dict__.update(**kw)
 
+        def getboolean(self, name):
+            return (str(getattr(self, name, 0)) in ('1', "yes", "true", "on", "True"))
+
     class Config:
         DAV = OpenDAV(**kw)
+
 
     return Config()
 
