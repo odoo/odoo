c179005fc74c169bf6bb351021322e06188b62da
Mustufa Rangwala <mra@mra-laptop>
2010-07-03 15:51:52 +0530

    [IMP] Clean hr_* module (Still need to done)
    bzr revid: mra@mra-laptop-20100703102152-4ulrrzbkorpd7xei

--- addons/hr_expense/report/expense.py
+++ addons/hr_expense/report/expense.py
@@ -24,11 +24,10 @@ from report import report_sxw
 import datetime
 
 class expense(report_sxw.rml_parse):
+
     def __init__(self, cr, uid, name, context):
         super(expense, self).__init__(cr, uid, name, context=context)
-        self.localcontext.update( {
-            'time': time,
-        })
+        self.localcontext.update({'time': time, })
 
 report_sxw.report_sxw('report.hr.expense', 'hr.expense.expense', 'addons/hr_expense/report/expense.rml',parser=expense)
 
