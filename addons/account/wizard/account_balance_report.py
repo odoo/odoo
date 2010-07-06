470916f5d65c81bab4e149301f34513bd44ccebb
qdp-launchpad@tinyerp.com <>
2010-07-06 11:32:31 +0200

    account: account balance report improvement (not yet finished)
    bzr revid: qdp-launchpad@tinyerp.com-20100706093231-n3zj7v8nq42aujuy

--- addons/account/wizard/account_balance_report.py
+++ addons/account/wizard/account_balance_report.py
@@ -25,45 +25,41 @@ from osv import osv, fields
 from tools.translate import _
 
 class account_balance_report(osv.osv_memory):
-
+    
     _inherit = "account.common.report"
     _name = 'account.balance.report'
     _description = 'Account Balance Report'
     _columns = {
-        'company_id': fields.many2one('res.company', 'Company', required=True),
         'display_account': fields.selection([('bal_all','All'), ('bal_mouvement','With movements'),
                          ('bal_solde','With balance is not equal to 0'),
-                         ],'Display accounts'),
-                }
+                         ],'Display accounts', required=True),
+    }
 
     _defaults = {
         'display_account': 'bal_all'
-                 }
+    }
 
-    def default_get(self, cr, uid, fields, context=None):
-        """ To get default values for the object.
-         @param self: The object pointer.
-         @param cr: A database cursor
-         @param uid: ID of the user currently logged in
-         @param fields: List of fields for which we want default values
-         @param context: A standard dictionary
-         @return: A dictionary which of fields with values.
-        """
-        res = {}
-        if 'journal_ids' in fields:# FIX me!!
-            res['journal_ids'] = []
-            return res
-        else:
-            result = super(account_balance_report, self).default_get(cr, uid, fields, context=context)
-        result.update({'company_id':self.pool.get('account.account').read(cr, uid, result['chart_account_id'], context=context)['company_id']})
-        return result
+#    def default_get(self, cr, uid, fields, context=None):
+#        """ To get default values for the object.
+#         @param self: The object pointer.
+#         @param cr: A database cursor
+#         @param uid: ID of the user currently logged in
+#         @param fields: List of fields for which we want default values
+#         @param context: A standard dictionary
+#         @return: A dictionary which of fields with values.
+#        """
+#        res = {}
+#        if 'journal_ids' in fields:# FIX me!!
+#            res['journal_ids'] = []
+#            return res
+#        else:
+#            result = super(account_balance_report, self).default_get(cr, uid, fields, context=context)
+#        result.update({'company_id':self.pool.get('account.account').read(cr, uid, result['chart_account_id'], context=context)['company_id']})
+#        return result
 
     def _print_report(self, cr, uid, ids, data, query_line, context=None):
-
-        if data['model']=='ir.ui.menu':
-            data['ids'] = [data['form']['chart_account_id']]
         data['form'].update(self.read(cr, uid, ids, ['display_account',  'company_id',])[0])
-        data['form']['query_get'] = query_line
+        data['form']['query_line'] = query_line
         return { 'type': 'ir.actions.report.xml', 'report_name': 'account.account.balance', 'datas': data, 'nodestroy':True, }
 
 account_balance_report()
