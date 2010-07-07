2405f334e2e39f06e2de569b86042b2693400d8b
Numerigraphe - Lionel Sausin <ls@numerigraphe.fr>
2010-07-07 17:29:09 +0200

    [FIX] account: wrong invoice number sequence for fiscal year in a certain case [FIX] account: context not initialized correctly in sequence
    lp bug: https://launchpad.net/bugs/602188 fixed
    
    bzr revid: ls@numerigraphe.fr-20100707152909-ocjbj6c784oqeg54

--- addons/account/invoice.py
+++ addons/account/invoice.py
@@ -923,12 +923,16 @@ class account_invoice(osv.osv):
         obj_inv = self.browse(cr, uid, ids)[0]
         for (id, invtype, number, move_id, reference) in cr.fetchall():
             if not number:
+                tmp_context = {
+                    'fiscalyear_id': inv.period_id.fiscalyear_id.id
+                }
                 if obj_inv.journal_id.invoice_sequence_id:
                     sid = obj_inv.journal_id.invoice_sequence_id.id
                     number = self.pool.get('ir.sequence').get_id(cr, uid, sid, 'id', {'fiscalyear_id': obj_inv.period_id.fiscalyear_id.id})
                 else:
-                    number = self.pool.get('ir.sequence').get(cr, uid,
-                            'account.invoice.' + invtype)
+                    number = obj_seq.get_id(cr, uid,
+                                            'account.invoice.%s' % invtype,
+                                            'code=%s', context=tmp_context)
                 if invtype in ('in_invoice', 'in_refund'):
                     ref = reference
                 else:
--- addons/account/sequence.py
+++ addons/account/sequence.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 
-from osv import fields,osv
+from osv import fields, osv
 
 class ir_sequence_fiscalyear(osv.osv):
     _name = 'account.sequence.fiscalyear'
@@ -31,7 +31,7 @@ class ir_sequence_fiscalyear(osv.osv):
     }
 
     _sql_constraints = [
-        ('main_id', 'CHECK (sequence_main_id != sequence_id)',  'Main Sequence must be different from current !'),
+        ('main_id', 'CHECK (sequence_main_id != sequence_id)', 'Main Sequence must be different from current !'),
     ]
 
 ir_sequence_fiscalyear()
@@ -41,15 +41,16 @@ class ir_sequence(osv.osv):
     _columns = {
         'fiscal_ids' : fields.one2many('account.sequence.fiscalyear', 'sequence_main_id', 'Sequences')
     }
-    def get_id(self, cr, uid, sequence_id, test='id', context={}):
+    def get_id(self, cr, uid, sequence_id, test='id', context=None):
+        if context is None:
+            context = {}
         cr.execute('select id from ir_sequence where '+test+'=%s and active=%s', (sequence_id, True,))
         res = cr.dictfetchone()
         if res:
             for line in self.browse(cr, uid, res['id'], context=context).fiscal_ids:
                 if line.fiscalyear_id.id==context.get('fiscalyear_id', False):
                     return super(ir_sequence, self).get_id(cr, uid, line.sequence_id.id, test="id", context=context)
-        return super(ir_sequence, self).get_id(cr, uid, sequence_id, test, context)
+        return super(ir_sequence, self).get_id(cr, uid, sequence_id, test, context=context)
 ir_sequence()
-
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
