a47be2ef0d72c5de183b9af1d44f33055c6c2dcc
P. Christeas <p_christ@hol.gr>
2010-07-13 23:54:18 +0300

    Document ftp: improve exception handling, size may return 404
    bzr revid: p_christ@hol.gr-20100713205418-8oe6svsdvw2yy4jn

--- addons/document_ftp/ftpserver/abstracted_fs.py
+++ addons/document_ftp/ftpserver/abstracted_fs.py
@@ -163,6 +163,8 @@ class abstracted_fs(object):
             ret = child.open_data(cr, mode)
             cr.commit()
             return ret
+        except EnvironmentError:
+            raise
         except Exception,e:
             self._log.exception('Cannot create item %s at node %s', objname, repr(node))
             raise OSError(1, 'Operation not permited.')
@@ -432,7 +434,7 @@ class abstracted_fs(object):
     def getsize(self, datacr):
         """Return the size of the specified file in bytes."""
         if not (datacr and datacr[1]):
-            return 0L
+            raise IOError(errno.ENOENT, "No such file or directory")
         if datacr[1].type in ('file', 'content'):
             return datacr[1].get_data_len(datacr[0]) or 0L
         return 0L
