9b5f933b1353c170f6f08a5d2faab064bda05314
rpa (Open ERP) <rpa@tinyerp.com>
2010-07-15 17:32:34 +0530

    [IMP]: base: Improvement in name get of partner address to display address details in the order: Name, zip city country street
    bzr revid: rpa@tinyerp.com-20100715120234-azoxxxii4pcpk8cr

--- bin/addons/base/res/partner/partner.py
+++ bin/addons/base/res/partner/partner.py
@@ -297,14 +297,14 @@ class res_partner_address(osv.osv):
         if not len(ids):
             return []
         res = []
-        for r in self.read(cr, user, ids, ['name','zip','city','partner_id', 'street']):
+        for r in self.read(cr, user, ids, ['name','zip','country_id', 'city','partner_id', 'street']):
             if context.get('contact_display', 'contact')=='partner' and r['partner_id']:
                 res.append((r['id'], r['partner_id'][1]))
             else:
                 addr = r['name'] or ''
-                if r['name'] and (r['zip'] or r['city']):
+                if r['name'] and (r['zip'] or r['country_id']):
                     addr += ', '
-                addr += (r['street'] or '') + ' ' + (r['zip'] or '') + ' ' + (r['city'] or '')
+                addr += (r['zip'] or '') + ' ' + (r['city'] or '') + ' ' + (r['country_id'] and r['country_id'][1] or '') + ' ' + (r['street'] or '')
                 res.append((r['id'], addr.strip() or '/'))
         return res
 
