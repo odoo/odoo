869cde3704a313d470e8c66527e6661032ddf0d9
Mustufa Rangwala <mra@mra-laptop>
2010-07-15 11:05:16 +0530

    [IMP] account: General ledger & Account balance => if print from object should not have charts of account field
    bzr revid: mra@mra-laptop-20100715053516-qgdjllsp9s717tkq

--- addons/account/wizard/account_report_balance.py
+++ addons/account/wizard/account_report_balance.py
@@ -18,6 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+from lxml import etree
 
 from osv import osv, fields
 from tools.translate import _
@@ -35,6 +36,18 @@ class account_balance_report(osv.osv_memory):
     _defaults = {
         'display_account': 'bal_all'
                 }
+
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        mod_obj = self.pool.get('ir.model.data')
+        res = super(account_balance_report, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar, submenu=False)
+        if context.get('active_model', False) == 'account.account' and view_id:
+            doc = etree.XML(res['arch'])
+            nodes = doc.xpath("//field[@name='chart_account_id']")
+            for node in nodes:
+                node.set('readonly', '1')
+                node.set('help', 'If you print the report from Account list/form view it will not consider Charts of account')
+            res['arch'] = etree.tostring(doc)
+        return res
 
     def _print_report(self, cr, uid, ids, data, query_line, context=None):
         data['form'].update(self.read(cr, uid, ids, ['display_account'])[0])
