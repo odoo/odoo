59426ee2760aed87a52d78a37d051aaff6d42525
rpa (Open ERP) <rpa@tinyerp.com>
2010-08-03 11:13:42 +0530

    [FIX]: caldav: Fixed problem of importing calendar without CN value
    bzr revid: rpa@tinyerp.com-20100803054342-rfz3bmqb3rydj20f

--- addons/caldav/calendar.py
+++ addons/caldav/calendar.py
@@ -230,7 +230,7 @@ class CalDAV(object):
         att_data = []
         for cal_data in child.getChildren():
             if cal_data.name.lower() == 'organizer':
-                self.ical_set(cal_data.name.lower(), {'name': cal_data.params['CN'][0]}, 'value')
+                self.ical_set(cal_data.name.lower(), {'name': cal_data.params.get('CN') and cal_data.params.get('CN')[0]}, 'value')
                 continue
             if cal_data.name.lower() == 'attendee':
                 ctx = context.copy()
