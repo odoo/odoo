da1b9356baa98cec5da78774a24ef6d0095ed0e9
Mustufa Rangwala <mra@mra-laptop>
2010-08-09 13:08:59 +0530

    [IMP] sale_analytic_plans: clean code
    bzr revid: mra@mra-laptop-20100809073859-iei4gtbj9dsny2ky

--- addons/sale_analytic_plans/sale_analytic_plans.py
+++ addons/sale_analytic_plans/sale_analytic_plans.py
@@ -18,22 +18,23 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import time
 
 from osv import fields, osv
 
 class sale_order_line(osv.osv):
     _inherit = 'sale.order.line'
     _columns = {
-        'analytics_id':fields.many2one('account.analytic.plan.instance','Analytic Distribution'),
+        'analytics_id': fields.many2one('account.analytic.plan.instance', 'Analytic Distribution'),
     }
     def invoice_line_create(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
         line_obj = self.pool.get('account.invoice.line')
         create_ids = super(sale_order_line,self).invoice_line_create(cr, uid, ids, context=context)
         i = 0
         for line in self.browse(cr, uid, ids, context):
-            line_obj.write(cr, uid, [create_ids[i]], {'analytics_id':line.analytics_id.id})
-            i = i+1
+            line_obj.write(cr, uid, [create_ids[i]], {'analytics_id': line.analytics_id.id})
+            i = i + 1
         return create_ids
 
 sale_order_line()
