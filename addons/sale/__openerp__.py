f585e43f1560e5296d17f6ecea6572aedda2d082
Mustufa Rangwala <mra@mra-laptop>
2010-08-18 14:41:04 +0530

    [IMP] Sale: highest level dependancy on sale
    bzr revid: mra@mra-laptop-20100818091104-nhfdtez1ef8ytfoh

--- addons/sale/__openerp__.py
+++ addons/sale/__openerp__.py
@@ -47,7 +47,7 @@
     """,
     'author': 'OpenERP SA',
     'website': 'http://www.openerp.com',
-    'depends': ['product', 'stock', 'procurement', 'process','board'],
+    'depends': ['stock', 'procurement', 'board'],
     'init_xml': [],
     'update_xml': [
         'wizard/sale_make_invoice_advance.xml',
