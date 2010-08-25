a6941afcf78e89dc70fbb69bd2289c177ca56878
ksa (Open ERP) <ksa@tinyerp.co.in>
2010-08-25 17:10:15 +0530

    [IMP]: Minor changes
    bzr revid: ksa@tinyerp.co.in-20100825114015-hdenybj5f0gx0a40

--- bin/addons/base/__openerp__.py
+++ bin/addons/base/__openerp__.py
@@ -60,7 +60,7 @@
         'res/partner/wizard/partner_sms_send_view.xml',
         'res/partner/wizard/partner_wizard_spam_view.xml',
         'res/partner/wizard/partner_clear_ids_view.xml',
-#        'res/partner/wizard/partner_wizard_ean_check_view.xml',
+        'res/partner/wizard/partner_wizard_ean_check_view.xml',
         'res/partner/partner_data.xml',
         'res/ir_property_view.xml',
         'security/base_security.xml',
