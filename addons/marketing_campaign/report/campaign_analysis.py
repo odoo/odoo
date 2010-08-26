df2283c00733efdafd0da0d19e1234619b8fdfef
rpa (Open ERP) <rpa@tinyerp.com>
2010-08-26 15:33:25 +0530

    [IMP]: marketing_campaign: Improevement in reporting
    bzr revid: rpa@tinyerp.com-20100826100325-t3tjytoi0w5tks3j

--- addons/marketing_campaign/report/campaign_analysis.py
+++ addons/marketing_campaign/report/campaign_analysis.py
@@ -42,6 +42,7 @@ class campaign_analysis(osv.osv):
             result[ca_obj.id] = total_cost
         return result
     _columns = {
+        'res_id' : fields.integer('Resource', readonly=True),
         'year': fields.char('Year', size=4, readonly=True),
         'month':fields.selection([('01','January'), ('02','February'),
                     ('03','March'), ('04','April'),('05','May'), ('06','June'),
@@ -69,6 +70,7 @@ class campaign_analysis(osv.osv):
             create or replace view campaign_analysis as (
             select
                 min(wi.id) as id,
+                min(wi.res_id) as res_id,
                 to_char(wi.date::date, 'YYYY') as year,
                 to_char(wi.date::date, 'MM') as month,
                 wi.date::date as date,
