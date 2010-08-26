6bd70936d1bbec058bf56039224b1d71f01cbccb
sbh (Open ERP) <sbh@tinyerp.com>
2010-08-26 12:37:03 +0530

    [IMP] Project: Improve view of Delegrate wizard , Done wizard:Imroive the view and update the description
    bzr revid: sbh@tinyerp.com-20100826070703-4sz1lk8lmjlp5e97

--- addons/project/wizard/project_close_task.py
+++ addons/project/wizard/project_close_task.py
@@ -75,8 +75,14 @@ class project_close_task(osv.osv_memory):
     }
 
     def close(self, cr, uid, ids, context=None):
+        data=self.read(cr,uid,ids)[0]
+        description=""
+        user_name= self.pool.get('res.users').browse(cr, uid, uid).name
+        description=user_name +"   At   " + time.strftime('%Y-%m-%d %H:%M:%S')
+        description+"\n"+"======================="
+        description=description+ "\n "+"Closed task:\n"+data['description']
         if 'task_id' in context:
-            self.pool.get('project.task').write(cr, uid, [context['task_id']], {'state': 'done', 'date_end':time.strftime('%Y-%m-%d %H:%M:%S'), 'remaining_hours': 0.0})
+            self.pool.get('project.task').write(cr, uid, [context['task_id']], {'description':description,'state': 'done', 'date_end':time.strftime('%Y-%m-%d %H:%M:%S'), 'remaining_hours': 0.0})
         return {}
 
     def confirm(self, cr, uid, ids, context=None):
