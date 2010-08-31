4b8d7618a505cb704d70813ec07b8b4193d5f532
sbh (Open ERP) <sbh@tinyerp.com>
2010-08-31 15:07:34 +0530

    [Fix] stock: Small bug
    bzr revid: sbh@tinyerp.com-20100831093734-ysx0pag5at0qccvf

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -1698,7 +1698,7 @@ class stock_move(osv.osv):
                     #the test does not work correctly if the same product occurs multiple times
                     #in the same order. This is e.g. the case when using the button 'split in two' of
                     #the stock outgoing form
-                    self.write(cr, uid, move.id, {'state':'assigned'})
+                    self.write(cr, uid, [move.id], {'state':'assigned'})
                     done.append(move.id)
                     pickings[move.picking_id.id] = 1
                     r = res.pop(0)
