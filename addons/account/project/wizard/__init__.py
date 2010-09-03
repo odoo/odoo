dca67f7fd51121c6139d526912b3c39f67e4c8f2
qdp-launchpad@tinyerp.com <>
2010-09-03 17:29:11 +0200

    [IMP] account: removed account.analytic.check report, as this feature should now be replaced by the Analytic Entries Analysis
    bzr revid: qdp-launchpad@tinyerp.com-20100903152911-r2z5pzn96repc91q

--- addons/account/project/wizard/__init__.py
+++ addons/account/project/wizard/__init__.py
@@ -24,7 +24,6 @@ import account_analytic_balance_report
 import account_analytic_inverted_balance_report
 import account_analytic_cost_ledger_report
 import account_analytic_cost_ledger_for_journal_report
-import account_analytic_check
 import project_account_analytic_line
 import account_analytic_chart
 
