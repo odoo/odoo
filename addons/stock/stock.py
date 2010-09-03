23052ece49e87d5bee8044cbf82223784f74fb40
Olivier Dony <odo@openerp.com>
2010-09-03 18:56:58 +0200

    [IMP] stock: incoming stock.moves from a supplier location should be assigned automatically
    bzr revid: odo@openerp.com-20100903165658-mllymlj50r35xeke

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -1752,7 +1752,7 @@ class stock_move(osv.osv):
         if context is None:
             context = {}
         for move in self.browse(cr, uid, ids, context=context):
-            if move.product_id.type == 'consu':
+            if move.product_id.type == 'consu' or move.location_id.usage == 'supplier':
                 if move.state in ('confirmed', 'waiting'):
                     done.append(move.id)
                 pickings[move.picking_id.id] = 1
