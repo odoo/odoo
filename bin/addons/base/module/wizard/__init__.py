05b270a9cf3c6f71a1db952d494b0a28cd496e9a
rpa (Open ERP) <rpa@tinyerp.com>
2010-09-16 17:59:27 +0530

    [IMP]: base: Improvement in configuration wizard action, added view
    bzr revid: rpa@tinyerp.com-20100916122927-7yzlr8l8tftiw67d

--- bin/addons/base/module/wizard/__init__.py
+++ bin/addons/base/module/wizard/__init__.py
@@ -27,6 +27,7 @@ import base_module_update
 import base_language_install
 import base_import_language
 import base_module_upgrade
+import base_module_configuration
 import base_export_language
 import base_update_translations
 
