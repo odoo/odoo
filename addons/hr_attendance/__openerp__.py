ba148dba0a21f895b698801ff35158d80e08989d
RME (OpenERP) <rme@tinyerp.com>
2010-09-16 17:59:49 +0530

    [IMP] hr_attendance, hr_timesheet_invoice, hr_payroll : yml report test added
    bzr revid: rme@tinyerp.com-20100916122949-gisdwwxrjc7xia6t

--- addons/hr_attendance/__openerp__.py
+++ addons/hr_attendance/__openerp__.py
@@ -43,7 +43,7 @@
         'wizard/hr_attendance_sign_in_out_view.xml',
         ],
     'demo_xml': ['hr_attendance_demo.xml'],
-    'test': ['test/test_hr_attendance.yml'],
+    'test': ['test/test_hr_attendance.yml','test/hr_attendance_error.yml'],
     'installable': True,
     'active': False,
     'certificate': '0063495605613',
