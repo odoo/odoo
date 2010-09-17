aaea54012aa3fbb89be07b731f88fd596a03760b
Vir (Open ERP) <vir@tinyerp.com>
2010-09-17 17:38:14 +0530

    [FIX,MOD] hr_*, survey : problem with access rights are sovled
    bzr revid: vir@tinyerp.com-20100917120814-e45m5gwpqvrirk8y

--- addons/hr_evaluation/__openerp__.py
+++ addons/hr_evaluation/__openerp__.py
@@ -40,6 +40,7 @@
                  ],
     "update_xml": [
         "security/ir.model.access.csv",
+        "security/hr_evaluation_security.xml",
         "wizard/hr_evaluation_mail_view.xml",
         "hr_evaluation_view.xml",
         "report/hr_evaluation_report_view.xml"
