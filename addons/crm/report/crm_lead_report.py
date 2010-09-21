bf1c253a1dbd4f587824b8c0b27dd9d54d9d1ed2
Fabien Pinckaers <fp@tinyerp.com>
2010-09-21 18:42:16 +0200

    merge
    bzr revid: fp@tinyerp.com-20100921164216-nel6o6b446jd900q

--- addons/crm/report/crm_lead_report.py
+++ addons/crm/report/crm_lead_report.py
@@ -101,8 +101,7 @@ class crm_lead_report(osv.osv):
         'planned_revenue': fields.float('Planned Revenue',digits=(16,2),readonly=True),
         'probable_revenue': fields.float('Probable Revenue', digits=(16,2),readonly=True),
         'categ_id': fields.many2one('crm.case.categ', 'Category',\
-                         domain="[('section_id','=',section_id),\
-                        ('object_id.model', '=', 'crm.lead')]" , readonly=True),
+                         domain="[('section_id','=',section_id)]" , readonly=True),
         'stage_id': fields.many2one ('crm.case.stage', 'Stage', \
                          domain="[('section_id','=',section_id),\
                         ('object_id.model', '=', 'crm.lead')]", readonly=True),
