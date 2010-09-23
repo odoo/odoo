ee989c3b7a589b9c3c82d5a78f111bc0dfa305ad
mtr <mtr@mtr>
2010-09-23 10:46:22 +0530

    [IMP] crm:-converted negative value of 'Overpassed Deadline' field of Opportunities Analysis to positive
    bzr revid: mtr@mtr-20100923051622-geplxssm3pej1moy

--- addons/crm/report/crm_lead_report.py
+++ addons/crm/report/crm_lead_report.py
@@ -156,7 +156,7 @@ class crm_lead_report(osv.osv):
                     (SELECT count(id) FROM mailgate_message WHERE model='crm.lead' AND res_id=c.id AND history=True) AS email,
                     date_trunc('day',c.create_date) as create_date,
                     extract('epoch' from (c.date_closed-c.create_date))/(3600*24) as  delay_close,
-                    extract('epoch' from (c.date_deadline - c.date_closed))/(3600*24) as  delay_expected,
+                    abs(extract('epoch' from (c.date_deadline - c.date_closed))/(3600*24)) as  delay_expected,
                     extract('epoch' from (c.date_open-c.create_date))/(3600*24) as  delay_open
                 FROM
                     crm_lead c
