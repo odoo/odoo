33d62323f1b4cbc0ab99f9176c3a83157e97aa8d
psi (Open ERP) <psi@tinyerp.co.in>
2010-09-29 18:21:08 +0530

    [IMP] account_budget: Code cleaning [FIX] account_payment, account_coda, l10n_ch: lines got wrong value when using invoice with payment terms
    bzr revid: psi@tinyerp.co.in-20100929125108-pyb29e9hujuk03rw

--- addons/account_payment/wizard/account_payment_populate_statement.py
+++ addons/account_payment/wizard/account_payment_populate_statement.py
@@ -91,7 +91,8 @@ class account_payment_populate_statement(osv.osv_memory):
                 'period_id': statement.period_id.id
                 }
                 voucher_id = voucher_obj.create(cr, uid, voucher_res, context=context)
-                result = voucher_obj.onchange_partner_id(cr, uid, [], partner_id=line.partner_id.id, journal_id=statement.journal_id.id, price=abs(amount), currency_id= statement.currency.id, ttype='payment')
+                context.update({'move_line_ids': [line.id]})
+                result = voucher_obj.onchange_partner_id(cr, uid, [], partner_id=line.partner_id.id, journal_id=statement.journal_id.id, price=abs(amount), currency_id= statement.currency.id, ttype='payment', context=context)
                 voucher_line_dict =  False
                 if result['value']['line_ids']:
                     for line_dict in result['value']['line_ids']:
