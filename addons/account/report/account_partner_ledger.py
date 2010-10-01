5c07eacf45e06e17e8f8a51bfe94ed14e3745fb9
Mustufa Rangwala <mra@mra-laptop>
2010-10-01 17:36:00 +0530

    [FIX] Account: Partner ledger report init query with new changes from qdp (just used init_bal on contexxt)
    bzr revid: mra@mra-laptop-20101001120600-rw8olwlssfvmgn7i

--- addons/account/report/account_partner_ledger.py
+++ addons/account/report/account_partner_ledger.py
@@ -58,7 +58,9 @@ class third_party_ledger(rml_parse.rml_parse, common_report_header):
     def set_context(self, objects, data, ids, report_type=None):
         obj_move = self.pool.get('account.move.line')
         self.query = obj_move._query_get(self.cr, self.uid, obj='l', context=data['form'].get('used_context', {}))
-        self.init_query = obj_move._query_get(self.cr, self.uid, obj='l', context=data['form'].get('used_context_initial_bal', {}))
+        ctx2 = data['form'].get('used_context',{}).copy()
+        ctx2.update({'initial_bal': True})
+        self.init_query = obj_move._query_get(self.cr, self.uid, obj='l', context=ctx2)
         self.reconcil = data['form'].get('reconcil', True)
         self.initial_balance = data['form'].get('initial_balance', True)
         self.result_selection = data['form'].get('result_selection', 'customer')
