8e1b7362c9acc00e6804f857e29a1d67142e1121
olt@tinyerp.com <>
2010-10-04 13:37:37 +0200

    [IMP] stock: 'inventory analysis' report now uses decimal.precision
    bzr revid: olt@tinyerp.com-20101004113737-rms5p1vbty6fdkj9

--- addons/stock/report/report_stock_move.py
+++ addons/stock/report/report_stock_move.py
@@ -137,7 +137,7 @@ class report_stock_inventory(osv.osv):
         'prodlot_id': fields.many2one('stock.production.lot', 'Lot', readonly=True),
         'company_id': fields.many2one('res.company', 'Company', readonly=True),
         'product_qty':fields.float('Qty', digits=(16,2), readonly=True),
-        'value' : fields.float('Total Value', digits=(16,2), required=True),
+        'value' : fields.float('Total Value', digits=(16,2), required=True, digits_compute=dp.get_precision('Sale Price')),
         'state': fields.selection([('draft', 'Draft'), ('waiting', 'Waiting'), ('confirmed', 'Confirmed'), ('assigned', 'Available'), ('done', 'Done'), ('cancel', 'Cancelled')], 'State', readonly=True, select=True,
               help='When the stock move is created it is in the \'Draft\' state.\n After that it is set to \'Confirmed\' state.\n If stock is available state is set to \'Avaiable\'.\n When the picking it done the state is \'Done\'.\
               \nThe state is \'Waiting\' if the move is waiting for another one.'),
