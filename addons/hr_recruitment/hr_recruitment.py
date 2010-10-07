4572cbebba113684e3c15ab274136a643a0a5855
Vir (Open ERP) <vir@tinyerp.com>
2010-10-07 15:05:09 +0530

    [MOD] hr_recruitment,hr : changes in code and added state field in group by of search view of job position
    bzr revid: vir@tinyerp.com-20101007093509-d2qh2t2tivr7qngl

--- addons/hr_recruitment/hr_recruitment.py
+++ addons/hr_recruitment/hr_recruitment.py
@@ -436,11 +436,11 @@ class hr_applicant(crm.crm_case, osv.osv):
         if applicant.job_id :
             emp_id = employee_obj.create(cr,uid,{'name':applicant.name,'job_id':applicant.job_id.id})
             job_data = job_obj.browse(cr,uid, applicant.job_id.id)
-            if job_data.expected_employees == int(job_data.no_of_employee):
+            total_expected_emp = job_data['expected_employees']
+            expected_emp = job_data['expected_employees'] - 1
+            job_obj.write(cr,uid, [applicant.job_id.id],{'expected_employees':expected_emp})
+            if total_expected_emp == int(job_data.no_of_employee):
                 job_obj.write(cr,uid, [applicant.job_id.id],{'state':'old'})
-#            expected_emp = job_data['expected_employees'] - 1
-#            job_obj.write(cr,uid, [applicant.job_id.id],{'expected_employees':expected_emp})
-
         return res
 
     def case_reset(self, cr, uid, ids, *args):
