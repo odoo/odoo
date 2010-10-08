05ed5b453100d4d0cbb09af0856be5978df82ce7
sbh (Open ERP) <sbh@tinyerp.com>
2010-10-08 10:46:10 +0530

    [Fix] hr_attendance: Fix the pylint warning
    bzr revid: sbh@tinyerp.com-20101008051610-glvci1rbuc5ytuyo

--- addons/hr_attendance/report/timesheet.py
+++ addons/hr_attendance/report/timesheet.py
@@ -83,7 +83,6 @@ class report_custom(report_rml):
                         dt = datetime.strptime(att['name'], '%Y-%m-%d %H:%M:%S')
                         if att['action'] == 'sign_out':
                             week_wh[ldt.date().weekday()] = week_wh.get(ldt.date().weekday(), 0) + ((dt - ldt).seconds/3600)
-                        ldt = dt
 
                 # Week xml representation
                 week_repr = ['<week>', '<weekstart>%s</weekstart>' % monday.strftime('%Y-%m-%d'), '<weekend>%s</weekend>' % n_monday.strftime('%Y-%m-%d')]
