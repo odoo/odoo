3dab6ea173a74418c7f2dd30054047e2c8e80271
mtr <mtr@mtr>
2010-10-08 12:35:33 +0530

    [IMP] l10n_be:-improvements in the view of wizards
    bzr revid: mtr@mtr-20101008070533-xokjnmxlepeodjr6

--- addons/l10n_be/wizard/l10n_be_account_vat_declaration.py
+++ addons/l10n_be/wizard/l10n_be_account_vat_declaration.py
@@ -39,8 +39,8 @@ class l10n_be_vat_declaration(osv.osv_memory):
         'tax_code_id': fields.many2one('account.tax.code', 'Tax Code', domain=[('parent_id', '=', False)]),
         'msg': fields.text('File created', size=64, readonly=True),
         'file_save': fields.binary('Save File'),
-        'ask_resitution': fields.boolean('Ask Restitution'),
-        'ask_payment': fields.boolean('Ask Payment'),
+        'ask_resitution': fields.boolean('Ask Restitution',help='It indicates whether a resitution is to made or not?'),
+        'ask_payment': fields.boolean('Ask Payment',help='It indicates whether a payment is to made or not?'),
         'client_nihil': fields.boolean('Last Declaration of Enterprise',help='Tick this case only if it concerns only the last statement on the civil or cessation of activity'),
     }
     _defaults = {
