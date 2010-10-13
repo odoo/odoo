a79d0ee8e3d7284169e00e57351aaff17983b52b
Olivier Dony <odo@openerp.com>
2010-10-13 18:39:27 +0200

    [FIX] osv.fields.one2many: corrected get() after incorrect patches
    bzr revid: odo@openerp.com-20101013163927-rsvoademz4iumjbh

--- bin/osv/fields.py
+++ bin/osv/fields.py
@@ -458,7 +458,7 @@ class one2many(_column):
 
         ids2 = obj.pool.get(self._obj).search(cr, user, self._domain + [(self._fields_id, 'in', ids)], limit=self._limit, context=context)
         for r in obj.pool.get(self._obj)._read_flat(cr, user, ids2, [self._fields_id], context=context, load='_classic_write'):
-            if r[self._fields_id] in res.values():
+            if r[self._fields_id] in res:
                 res[r[self._fields_id]].append(r['id'])
         return res
 
