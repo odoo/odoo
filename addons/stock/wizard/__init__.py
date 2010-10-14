42f572a889ff449f1de786036a6e88f9c634dac3
sbh (Open ERP) <sbh@tinyerp.com>
2010-10-14 11:59:42 +0530

    [Fix] stock: remove the name from init
    bzr revid: sbh@tinyerp.com-20101014062942-8xbckw3h857oxjt2

--- addons/stock/wizard/__init__.py
+++ addons/stock/wizard/__init__.py
@@ -32,7 +32,6 @@ import stock_inventory_line_split
 import stock_invoice_onshipping
 import stock_location_product
 import stock_change_standard_price
-import stock_picking_make
 import stock_return_picking
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
