56daa2bcb6d45ba4645078544b71d108dea8d7a7
Fabien Pinckaers <fp@tinyerp.com>
2010-10-16 23:44:35 +0200

    typo
    bzr revid: fp@tinyerp.com-20101016214435-6j3ku5dtjh2bjczq

--- bin/addons/base/res/partner/partner.py
+++ bin/addons/base/res/partner/partner.py
@@ -104,7 +104,7 @@ class res_partner(osv.osv):
     _columns = {
         'name': fields.char('Name', size=128, required=True, select=True),
         'date': fields.date('Date', select=1),
-        'title': fields.many2one('res.partner.title','Legal Status'),
+        'title': fields.many2one('res.partner.title','Partner Form'),
         'parent_id': fields.many2one('res.partner','Parent Partner', select=2),
         'child_ids': fields.one2many('res.partner', 'parent_id', 'Partner Ref.'),
         'ref': fields.char('Reference', size=64),
