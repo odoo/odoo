2d71f013e7d971d03a4d0f23c3e7289e9b07e5a9
Fabien Pinckaers <fp@tinyerp.com>
2010-10-18 01:33:01 +0200

    fix
    bzr revid: fp@tinyerp.com-20101017233301-yyao1l9b1jnyq3kz

--- addons/project/project.py
+++ addons/project/project.py
@@ -712,7 +712,25 @@ class project_work(osv.osv):
     }
 
     _order = "date desc"
+    def create(self, cr, uid, vals, *args, **kwargs):
+        if 'hours' in vals and (not vals['hours']):
+            vals['hours'] = 0.00
+        if 'task_id' in vals:
+            cr.execute('update project_task set remaining_hours=remaining_hours - %s where id=%s', (vals.get('hours',0.0), vals['task_id']))
+        return super(project_work,self).create(cr, uid, vals, *args, **kwargs)
 
+    def write(self, cr, uid, ids,vals,context={}):
+        if 'hours' in vals and (not vals['hours']):
+            vals['hours'] = 0.00
+        if 'hours' in vals:
+            for work in self.browse(cr, uid, ids, context):
+                cr.execute('update project_task set remaining_hours=remaining_hours - %s + (%s) where id=%s', (vals.get('hours',0.0), work.hours, work.task_id.id))
+        return super(project_work,self).write(cr, uid, ids, vals, context)
+
+    def unlink(self, cr, uid, ids, *args, **kwargs):
+        for work in self.browse(cr, uid, ids):
+            cr.execute('update project_task set remaining_hours=remaining_hours + %s where id=%s', (work.hours, work.task_id.id))
+        return super(project_work,self).unlink(cr, uid, ids,*args, **kwargs)
 project_work()
 
 class account_analytic_account(osv.osv):
