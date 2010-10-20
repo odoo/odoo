d78dfbf38b3fba118d31a0354cad7b9b6599d2e0
Xavier Morel <xmo@openerp.com>
2010-10-20 15:54:11 +0200

    [IMP] style buttons in sidebar headers to remove redundant styling from the various header buttons
    bzr revid: xmo@openerp.com-20101020135411-s3sqwn3acflmfhtn

--- addons/piratepad/editors.py
+++ addons/piratepad/editors.py
@@ -14,7 +14,7 @@ class SidebarTemplateEditor(openobject.templating.TemplateEditor):
                 output.index(self.ADD_ATTACHMENT_BUTTON)) + 1
         output = output[:form_opener_insertion] + \
                  '''<a href="#" id="add-pad" class="button-a"
-                       style="position: absolute; top: 3px; right: 44px; line-height: 13px;"
+                       style="right: 44px;"
                        >${_("Pad")}</a>\n''' + \
                  output[form_opener_insertion:]
         return output
