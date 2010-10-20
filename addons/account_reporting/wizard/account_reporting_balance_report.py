e152553153a4ad16203017c9e7b85f7802be3f09
pso <pso@tinyerp.com>
2010-10-20 12:06:52 +0530

    [IMP] account_reporting: cleaning module
    bzr revid: pso@tinyerp.com-20101020063652-3pze1frj02vjxda1

--- addons/account_reporting/wizard/account_reporting_balance_report.py
+++ addons/account_reporting/wizard/account_reporting_balance_report.py
@@ -18,6 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 from osv import fields, osv
 from tools.translate import _
 
@@ -33,7 +34,7 @@ class account_reporting_balance_report(osv.osv_memory):
     _description = 'Account balance report'
     _columns = {
         'fiscalyear': fields.many2one('account.fiscalyear', 'Fiscal year', required=True),
-        'periods': fields.many2many('account.period', 'acc_reporting_relation', 'acc_id','period_id', 'Periods', help='All periods if empty'),
+        'periods': fields.many2many('account.period', 'acc_reporting_relation', 'acc_id', 'period_id', 'Periods', help='All periods if empty'),
             }
     _defaults = {
         'fiscalyear' : _get_fiscalyear,
@@ -45,7 +46,7 @@ class account_reporting_balance_report(osv.osv_memory):
             context = {}
         data = self.read(cr, uid, ids)[0]
         datas = {
-             'ids': context.get('active_ids',[]),
+             'ids': context.get('active_ids', []),
              'model': 'account.report.bs',
              'form': data
             }
