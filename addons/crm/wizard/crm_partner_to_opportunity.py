75b72c807449a85222448b367f131209161fdce7
ron@tinyerp.com <>
2010-10-21 13:03:27 +0530

    [FIX]Purchase->Addressbook->supplier:Create opportunities
    bzr revid: ron@tinyerp.com-20101021073327-5fi72y0fcwum949h

--- addons/crm/wizard/crm_partner_to_opportunity.py
+++ addons/crm/wizard/crm_partner_to_opportunity.py
@@ -84,7 +84,7 @@ class crm_partner2opportunity(osv.osv_memory):
 
 
             categ_obj = self.pool.get('crm.case.categ')
-            categ_ids = categ_obj.search(cr, uid, [('name','ilike','Part%')])
+            categ_ids = categ_obj.search(cr, uid, [('name','ilike','other%')])
 
             case_obj = self.pool.get('crm.lead')
             opp_id = case_obj.create(cr, uid, {
