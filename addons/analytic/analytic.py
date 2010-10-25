12ddd46d74b6b77734d72dc08af7ec11c690554b
qdp-launchpad@tinyerp.com <>
2010-10-25 16:26:16 +0200

    [IMP] analytic: removed decimal precision on quantity function field on analytic account
    bzr revid: qdp-launchpad@tinyerp.com-20101025142616-vgmzxx8ff8nlz2if

--- addons/analytic/analytic.py
+++ addons/analytic/analytic.py
@@ -118,7 +118,7 @@ class account_analytic_account(osv.osv):
         'balance': fields.function(_debit_credit_bal_qtty, method=True, type='float', string='Balance', multi='debit_credit_bal_qtty', digits_compute=dp.get_precision('Account')),
         'debit': fields.function(_debit_credit_bal_qtty, method=True, type='float', string='Debit', multi='debit_credit_bal_qtty', digits_compute=dp.get_precision('Account')),
         'credit': fields.function(_debit_credit_bal_qtty, method=True, type='float', string='Credit', multi='debit_credit_bal_qtty', digits_compute=dp.get_precision('Account')),
-        'quantity': fields.function(_debit_credit_bal_qtty, method=True, type='float', string='Quantity', multi='debit_credit_bal_qtty', digits_compute=dp.get_precision('Account')),
+        'quantity': fields.function(_debit_credit_bal_qtty, method=True, type='float', string='Quantity', multi='debit_credit_bal_qtty'),
         'quantity_max': fields.float('Maximum Quantity', help='Sets the higher limit of quantity of hours.'),
         'partner_id': fields.many2one('res.partner', 'Partner'),
         'contact_id': fields.many2one('res.partner.address', 'Contact'),
