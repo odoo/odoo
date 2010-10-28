8f0095a006d3d40057737d23baeca48aaef04c81
ron@tinyerp.com <>
2010-10-28 12:50:11 +0530

    [FIX] avoid negative wording
    lp bug: https://launchpad.net/bugs/666362 fixed
    
    bzr revid: ron@tinyerp.com-20101028072011-19v0gxf5dr3kqbpa

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -636,7 +636,7 @@ class stock_picking(osv.osv):
         'invoice_state': fields.selection([
             ("invoiced", "Invoiced"),
             ("2binvoiced", "To Be Invoiced"),
-            ("none", "From Ordered Quantities")], "Invoice Control",
+            ("none", "Not Applicable")], "Invoice Control",
             select=True, required=True, readonly=True, states={'draft': [('readonly', False)]}),
         'company_id': fields.many2one('res.company', 'Company', required=True, select=True),
     }
