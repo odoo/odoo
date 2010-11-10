c27a83738934ecae4a94113d8e37e6bda4e59ce5
apa-tiny <apa@tinyerp.com>
2010-11-10 16:03:57 +0530

    [IMP]:stock:Packing List
    bzr revid: apa@tinyerp.com-20101110103357-t579do98v7bbcj2f

--- addons/stock/report/picking.py
+++ addons/stock/report/picking.py
@@ -32,10 +32,11 @@ class picking(report_sxw.rml_parse):
             'get_qtytotal':self._get_qtytotal
         })
     def _get_qtytotal(self,move_lines):
-        total=0.0
+        total = 0.0
+        uom = move_lines[0].product_uom.name
         for move in move_lines:
             total+=move.product_qty
-        return total
+        return {'quantity':total,'uom':uom}
 
 report_sxw.report_sxw('report.stock.picking.list','stock.picking','addons/stock/report/picking.rml',parser=picking)
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
