67ea374f16f4a22a1d6b5a24dd921f0043efc2c6
ARA (OpenERP) <ara@tinyerp.com>
2010-11-11 11:41:02 +0530

    [REF] l10n_*:Correct warning from buildbot
    bzr revid: ara@tinyerp.com-20101111061102-022fskf3gtew1846

--- addons/l10n_cr/__openerp__.py
+++ addons/l10n_cr/__openerp__.py
@@ -1,35 +1,35 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    __openerp__.py
-#    l10n_cr_account
-#    First author: Carlos Vásquez <carlos.vasquez@clearcorp.co.cr> (ClearCorp S.A.)
-#    Copyright (c) 2010-TODAY ClearCorp S.A. (http://clearcorp.co.cr). All rights reserved.
-#    
-#    Redistribution and use in source and binary forms, with or without modification, are
-#    permitted provided that the following conditions are met:
-#    
-#       1. Redistributions of source code must retain the above copyright notice, this list of
+#	__openerp__.py
+#	l10n_cr_account
+#	First author: Carlos Vásquez <carlos.vasquez@clearcorp.co.cr> (ClearCorp S.A.)
+#	Copyright (c) 2010-TODAY ClearCorp S.A. (http://clearcorp.co.cr). All rights reserved.
+#
+#	Redistribution and use in source and binary forms, with or without modification, are
+#	permitted provided that the following conditions are met:
+#
+#		1. Redistributions of source code must retain the above copyright notice, this list of
 #          conditions and the following disclaimer.
-#    
-#       2. Redistributions in binary form must reproduce the above copyright notice, this list
+#
+#		2. Redistributions in binary form must reproduce the above copyright notice, this list
 #          of conditions and the following disclaimer in the documentation and/or other materials
 #          provided with the distribution.
-#    
-#    THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR IMPLIED
-#    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-#    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
-#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-#    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-#    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-#    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-#    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-#    ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#    
-#    The views and conclusions contained in the software and documentation are those of the
-#    authors and should not be interpreted as representing official policies, either expressed
-#    or implied, of ClearCorp S.A..
-#    
+#
+#	THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#	WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+#	FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
+#	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#	ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#	The views and conclusions contained in the software and documentation are those of the
+#	authors and should not be interpreted as representing official policies, either expressed
+#	or implied, of ClearCorp S.A..
+#
 ##############################################################################
 
 {
@@ -46,7 +46,7 @@
       * account.tax.template
       * account.tax.code.template
       * account.chart.template
-    
+
     Everything is in English with Spanish translation. Further translations are welcome, please go to
     http://translations.launchpad.net/openerp-costa-rica
 	""",
@@ -66,3 +66,5 @@
 	'installable': True,
 	'active': False,
 }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- addons/l10n_it/report/libroIVA_debito.py
+++ addons/l10n_it/report/libroIVA_debito.py
@@ -1,27 +1,25 @@
-import datetime
 import time
 from report import report_sxw
-from osv import osv
 import pooler
-            
+
 
 class l10n_chart_it_servabit_report_libroIVA_debito(report_sxw.rml_parse):
-	
+
     def __init__(self, cr, uid, name, context):
         super(l10n_chart_it_servabit_report_libroIVA_debito,self).__init__(cr,uid,name,context)
         self.localcontext.update({
-            'time' : time,
-	    'get_company' : self.get_company,
-	    'get_periods' : self.get_periods,
-	    'get_lines' : self.get_lines,
+        'time' : time,
+        'get_company' : self.get_company,
+        'get_periods' : self.get_periods,
+        'get_lines' : self.get_lines,
         })
-	
-	
+
+
     def get_company(self,fiscal_year):
-	#print 'COMP = ',fiscal_year
+	    #print 'COMP = ',fiscal_year
         return ""
-	
-	
+
+
     def get_periods(self,fiscal_year):
 	#print 'Fiscal year id:',fiscal_year.id
 	obj=pooler.get_pool(self.cr.dbname).get('account.fiscalyear')
@@ -30,13 +28,13 @@ class l10n_chart_it_servabit_report_libroIVA_debito(report_sxw.rml_parse):
 	res=[rec for rec in fy.period_ids]
 	#return fy.periods  => non funziona?!? bool object !?!?
 	return res
-	
+
     def get_invoices(self,period):
 	#print 'PERIOD = ',period.name
 	obj=pooler.get_pool(self.cr.dbname).get('account.invoice')
 	# Selezione tutte le fatture emesse nel periodo
 	self.cr.execute(	"""
-					SELECT id FROM account_invoice 
+					SELECT id FROM account_invoice
 					WHERE 	(state='open' OR state='paid') AND
 							period_id="""+str(period.id)+"""
 							AND (type='in_invoice' OR type='in_refund')
@@ -48,7 +46,7 @@ class l10n_chart_it_servabit_report_libroIVA_debito(report_sxw.rml_parse):
 	invoices=obj.browse(self.cr,self.uid,ids)
 	#print 'INVOICES = ',invoices
 	return invoices
-	
+
     def get_lines(self,fiscal_year):
 	res=[]
 	obj_fy=pooler.get_pool(self.cr.dbname).get('account.fiscalyear')
@@ -73,8 +71,8 @@ class l10n_chart_it_servabit_report_libroIVA_debito(report_sxw.rml_parse):
 		    d={'periodo':'', 'protocollo':'', 'causale':'', 'numero':'', 'data_doc':'', }
 	return res
 
-
-        
 report_sxw.report_sxw('report.l10n_it.report.libroIVA_debito','account.report_libroiva',
                       'l10n_it/report/libroIVA_debito.rml',
                        parser=l10n_chart_it_servabit_report_libroIVA_debito,header=False)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
