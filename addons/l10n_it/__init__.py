e0a750d9b57e1dc85fd78035e84108a0dac38ca9
eLBati <lorenzo.battistini@domsense.com>
2010-11-12 12:31:19 +0100

    FIX copyright
    bzr revid: lorenzo.battistini@domsense.com-20101112113119-1x1phl3b3bpi3kot

--- addons/l10n_it/__init__.py
+++ addons/l10n_it/__init__.py
@@ -2,7 +2,12 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010 Italian OpenERP Community (<http://www.openerp-italia.com>)                            
+#    Copyright (C) 2010
+#    Italian OpenERP Community (<http://www.openerp-italia.com>)
+#    Servabit srl
+#    Agile Business Group sagl
+#    Domsense srl
+#    Albatos srl                        
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
