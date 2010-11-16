e59e5eb818e729e975ac74670bb9a337f0b3927a
P. Christeas <p_christ@hol.gr>
2010-11-16 16:55:19 +0200

    event_project: fix init.py after rename of event_project.py
    bzr revid: p_christ@hol.gr-20101116145519-km3q9g508wtc4mf6

--- addons/event_project/__init__.py
+++ addons/event_project/__init__.py
@@ -18,7 +18,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
 #
 ##############################################################################
-import event
+
+import event_project
 import wizard
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
