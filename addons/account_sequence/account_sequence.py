7303b9b76a58793ace4f5870dea35b290d40bc88
ARA (OpenERP) <ara@tinyerp.com>
2010-11-19 17:58:29 +0530

    [MERGE] resolve conflicts
    bzr revid: ara@tinyerp.com-20101119122829-wx2e1zst75hghumr

--- addons/account_sequence/account_sequence.py
+++ addons/account_sequence/account_sequence.py
@@ -51,15 +51,4 @@ class account_journal(osv.osv):
 
 account_journal()
 
-<<<<<<< TREE
-class account_move_line(osv.osv):
-    _inherit = "account.move.line"
-
-    _columns = {
-        'internal_sequence_number': fields.related('move_id','internal_sequence_number', type='char', relation='account.move', string='Internal Number'),
-    }
-account_move_line()
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:=======
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
->>>>>>> MERGE-SOURCE
