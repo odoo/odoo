fa3bda7f54979813105ff472a55d26e5b17dfa12
Xavier Morel <xmo@openerp.com>
2010-11-22 13:14:22 +0100

    [IMP] share: cleanup whitespace in web module
    bzr revid: xmo@openerp.com-20101122121422-f36nnn86uc7jt6b5

--- addons/share/web/controllers.py
+++ addons/share/web/controllers.py
@@ -9,7 +9,7 @@ import cherrypy
 
 class ShareWizardController(openerp.controllers.SecuredController):
     _cp_path = "/share"
-    
+
     @expose()
     def index(self, domain, search_domain, context, name):
         domain = ast.literal_eval(domain)
@@ -28,4 +28,3 @@ class ShareWizardController(openerp.controllers.SecuredController):
                 context.update({'active_ids': share_wiz_id, 'active_id': share_wiz_id[0], '_terp_view_name': 'Share Wizard', 'share_root_url': share_root_url})
                 sharing_view_id = rpc.RPCProxy(share_model).create({'domain': str(domain), 'action_id':action_id}, context)
                 return actions.execute(rpc.session.execute('object', 'execute', share_model, 'go_step_1', [sharing_view_id], context), ids=[sharing_view_id], context=context)
-
