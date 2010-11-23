6ff627d27548a97a65860e796af3a8b62d1d2ffd
Olivier Dony <odo@openerp.com>
2010-11-23 17:05:06 +0100

    [IMP] ir.model.data,res.log: allow marking res.log entries as read upon creation via context + do it during module install to avoid useless log spam
    bzr revid: odo@openerp.com-20101123160506-qsxa7gm5j8scphdz

--- bin/addons/base/ir/ir_model.py
+++ bin/addons/base/ir/ir_model.py
@@ -429,6 +429,10 @@ class ir_model_data(osv.osv):
         model_obj = self.pool.get(model)
         if not context:
             context = {}
+
+        # records created during module install should result in res.log entries that are already read!
+        context = dict(context, res_log_read=True)
+
         if xml_id and ('.' in xml_id):
             assert len(xml_id.split('.'))==2, _("'%s' contains too many dots. XML ids should not contain dots ! These are used to refer to other modules data, as in module.reference_id") % (xml_id)
             module, xml_id = xml_id.split('.')
