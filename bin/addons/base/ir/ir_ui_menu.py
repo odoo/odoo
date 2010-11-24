1a6ab82f7bd3c03020ea077029a8a174d7342727
gpa <gpa@tinyerp.com>
2010-11-24 16:19:36 +0530

    Improvement for the web client dash board
    bzr revid: gpa@tinyerp.com-20101124104936-rk38vyhz7bll6f9r

--- bin/addons/base/ir/ir_ui_menu.py
+++ bin/addons/base/ir/ir_ui_menu.py
@@ -22,6 +22,7 @@
 from osv import fields, osv
 import re
 import tools
+import base64
 
 def one_in(setA, setB):
     """Check the presence of an element of setA in setB
@@ -260,8 +261,26 @@ class ir_ui_menu(osv.osv):
                 return False
             level -= 1
         return True
-    
-    
+
+    def read_image(self, path):
+        icon = tools.file_open(path,'rb').read()
+        return base64.encodestring(icon)
+
+    def _get_image_icon(self, cr, uid, ids, name, args, context=None):
+        if context.get('bin_size', False):
+            context.update({'bin_size':False})
+        res = {}
+        for menu in self.browse(cr, uid, ids, context=context):
+            res[menu.id] = {
+                'web_icon_data': False,
+                'web_icon_hover_data': False,
+            }
+            if not menu.parent_id:
+                if menu.web_icon_hover:
+                    res[menu.id]['web_icon_hover_data'] = self.read_image(menu.web_icon_hover)
+                if menu.web_icon:
+                    res[menu.id]['web_icon_data'] = self.read_image(menu.web_icon)
+        return res
 
     _columns = {
         'name': fields.char('Menu', size=64, required=True, translate=True),
@@ -275,6 +294,10 @@ class ir_ui_menu(osv.osv):
             string='Complete Name', type='char', size=128),
         'icon': fields.selection(tools.icons, 'Icon', size=64),
         'icon_pict': fields.function(_get_icon_pict, method=True, type='char', size=32),
+        'web_icon': fields.char('Icon File', size=128),
+        'web_icon_hover':fields.char('Icon hover File', size=128),
+        'web_icon_data': fields.function(_get_image_icon, string='Web Icons', type='binary', method=True, readonly=True, store=True, multi='icon'),
+        'web_icon_hover_data':fields.function(_get_image_icon, string='Web Icons Hover', type='binary', method=True, readonly=True, store=True,multi='icon'),
         'action': fields.function(_action, fnct_inv=_action_inv,
             method=True, type='reference', string='Action',
             selection=[
