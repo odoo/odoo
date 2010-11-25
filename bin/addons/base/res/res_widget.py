eb3f4bdb25ba6a166b7b3d5ae85f432de4942337
Xavier Morel <xmo@openerp.com>
2010-11-25 13:48:34 +0100

    [FIX] res_widget: use context correctly
    bzr revid: xmo@openerp.com-20101125124834-daf5p22of6r9unvc

--- bin/addons/base/res/res_widget.py
+++ bin/addons/base/res/res_widget.py
@@ -43,14 +43,15 @@ class res_widget_wizard(osv.osv_memory):
     _name = "res.widget.wizard"
     _description = "Add a widget for User"
     _columns = {
-        'widget_id': fields.many2many("res.widget", "res_widget_user_rel", "uid", "wid", "Widget"),
+        'widget_id': fields.many2many("res.widget",
+                                      "res_widget_user_rel", "uid", "wid",
+                                      "Widget"),
     }
-    
+
     def res_widget_add(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        wizard = self.read(cr, uid, ids)[0]
+        wizard = self.read(cr, uid, ids, context=context)[0]
         for wiz_id in wizard['widget_id']:
-            self.pool.get('res.widget.user').create(cr, uid, {'user_id':uid, 'widget_id':wiz_id})
+            self.pool.get('res.widget.user').create(
+                cr, uid, {'user_id':uid, 'widget_id':wiz_id}, context=context)
         return {'type': 'ir.actions.act_window_close'}
 res_widget_wizard()
