56bbbfcd62e0037bd86c5de77768b281d25da450
qdp-launchpad@tinyerp.com <>
2010-11-26 15:35:24 +0100

    [FIX] account_sequence: ir.values for default internal sequence on new journals
    bzr revid: qdp-launchpad@tinyerp.com-20101126143524-gl3g2xkmr3eiy453

--- addons/account_sequence/account_sequence_installer.py
+++ addons/account_sequence/account_sequence_installer.py
@@ -75,6 +75,7 @@ class account_sequence_installer(osv.osv_memory):
                 j_ids.append(journal.id)
         if j_ids:
             jou_obj.write(cr, uid, j_ids, {'internal_sequence_id': ir_seq})
+        self.pool.get('ir.values').set(cr, uid, key='default', key2=False, name='internal_sequence_id', models =[('account.journal', False)], value=ir_seq)
         return res
 
 account_sequence_installer()
