bc9bab7db406b0ed3c1b6fabf13c33c7b3929ccf
psi (Open ERP) <psi@tinyerp.co.in>
2010-12-02 18:32:17 +0530

    [IMP] account_voucher: misc changes and moveline with same amount with higer priority
    bzr revid: psi@tinyerp.co.in-20101202130217-siz1ozkcq5har3mv

--- addons/account_voucher/account_voucher.py
+++ addons/account_voucher/account_voucher.py
@@ -418,6 +418,18 @@ class account_voucher(osv.osv):
         ids.reverse()
         moves = move_line_pool.browse(cr, uid, ids)
 
+        priority_moves = []
+        for line in moves:
+            original_amount = line.credit or line.debit or 0.0
+            if original_amount == price:
+                priority_moves.append(line)
+
+        if priority_moves:
+            for line in moves:
+                if line not in priority_moves:
+                    priority_moves.append(line)
+            moves = priority_moves
+
         company_currency = journal.company_id.currency_id.id
         if company_currency != currency_id and ttype == 'payment':
             total_debit = currency_pool.compute(cr, uid, currency_id, company_currency, total_debit)
@@ -438,13 +450,13 @@ class account_voucher(osv.osv):
             if line.debit and line.reconcile_partial_id and ttype == 'payment':
                 continue
 
-            orignal_amount = line.credit or line.debit or 0.0
+            original_amount = line.credit or line.debit or 0.0
             rs = {
                 'name':line.move_id.name,
                 'type': line.credit and 'dr' or 'cr',
                 'move_line_id':line.id,
                 'account_id':line.account_id.id,
-                'amount_original':currency_pool.compute(cr, uid, company_currency, currency_id, orignal_amount),
+                'amount_original':currency_pool.compute(cr, uid, company_currency, currency_id, original_amount),
                 'date_original':line.date,
                 'date_due':line.date_maturity,
                 'amount_unreconciled':currency_pool.compute(cr, uid, company_currency, currency_id, line.amount_unreconciled)
@@ -468,7 +480,6 @@ class account_voucher(osv.osv):
                 default['value']['pre_line'] = 1
             elif ttype == 'receipt' and len(default['value']['line_dr_ids']) > 0:
                 default['value']['pre_line'] = 1
-
         return default
 
     def onchange_date(self, cr, user, ids, date, context={}):
