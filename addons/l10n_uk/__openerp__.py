172c0cda3e4d5aa90436ccb6de7ede9ce92147c8
qdp-launchpad@tinyerp.com <>
2010-12-09 12:57:12 +0100

    [FIX] l10n_uk: set a valid certificate number
    bzr revid: qdp-launchpad@tinyerp.com-20101209115712-tg62m1j4h4wsqli5

--- addons/l10n_uk/__openerp__.py
+++ addons/l10n_uk/__openerp__.py
@@ -75,6 +75,6 @@
     'demo_xml': [],
     'installable': True,
     'active': False,
-    'certificate': '8956603329629',
+    'certificate': '18956603329629',
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
