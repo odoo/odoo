9c3580b781562ae6f9e8e7837992297dd03cc23f
Sbh (OpenERP) <sbh@tinyerp.com>
2010-12-14 14:20:02 +0530

    [IMP] base_calendar: Remove th option of seconds and minutes
    bzr revid: sbh@tinyerp.com-20101214085002-7ljpk6siot6ph678

--- addons/base_calendar/base_calendar.py
+++ addons/base_calendar/base_calendar.py
@@ -1151,9 +1151,7 @@ e.g.: Every other month on the last Sunday of the month for 10 occurrences:\
                                 ('daily', 'Days'), \
                                 ('weekly', 'Weeks'), \
                                 ('monthly', 'Months'), \
-                                ('yearly', 'Years'), \
-                                ('secondly', 'Seconds'), \
-                                ('minutely', 'Minutes') ], 'Frequency'),
+                                ('yearly', 'Years'), ], 'Frequency'),
         'interval': fields.integer('Interval', help="Repeat every x"),
         'count': fields.integer('Count', help="Repeat max that times"),
         'mo': fields.boolean('Mon'),
