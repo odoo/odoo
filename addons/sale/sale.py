5ddf27083cd86792c76be63d171ab5843e8bb17b
rha@tinyerp.com <>
2010-12-29 18:44:20 +0530

    [Fix]: Bug #613286 [stock]: returned products from customers have cost price of the day instead of historical cost price
    bzr revid: rha@tinyerp.com-20101229131420-k97xl7mrx9htvchi

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -702,6 +702,7 @@ class sale_order(osv.osv):
                         #'state': 'waiting',
                         'note': line.notes,
                         'company_id': order.company_id.id,
+                        'returned_price': line.price_unit,
                     })
 
                 if line.product_id:
