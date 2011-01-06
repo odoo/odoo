96f5598a6a1cc01f13bd3893167c2ebbe13e7375
ARA (OpenERP) <ara@tinyerp.com>
2011-01-06 17:02:21 +0530

    [IMP] Add readonly=true and store=true where company_id is related field in all addons module
    bzr revid: ara@tinyerp.com-20110106113221-7rkpjapyqawg25a6

--- addons/account_voucher/account_voucher.py
+++ addons/account_voucher/account_voucher.py
@@ -839,7 +839,7 @@ class account_voucher_line(osv.osv):
         'date_due': fields.related('move_line_id','date_maturity', type='date', relation='account.move.line', string='Due Date', readonly=1),
         'amount_original': fields.function(_compute_balance, method=True, multi='dc', type='float', string='Original Amount', store=True),
         'amount_unreconciled': fields.function(_compute_balance, method=True, multi='dc', type='float', string='Open Balance', store=True),
-        'company_id': fields.related('voucher_id','company_id', relation='res.company', type='many2one', string='Company', store=True),
+        'company_id': fields.related('voucher_id','company_id', relation='res.company', type='many2one', string='Company', store=True, readonly=True),
     }
     _defaults = {
         'name': ''
