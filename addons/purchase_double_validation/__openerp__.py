db0f75705c99e2fa2f61ca125b6e3a891bb14203
aag (OpenERP) <aag@tinyerp.co.in>
2011-01-07 17:45:07 +0530

    [ADD] purchase_double_validation:Add yml for Double validation
    bzr revid: aag@tinyerp.co.in-20110107121507-yqcn0cusopw2fu57

--- addons/purchase_double_validation/__openerp__.py
+++ addons/purchase_double_validation/__openerp__.py
@@ -35,6 +35,9 @@
 	   'purchase_double_validation_installer.xml'
 	    ],
     'demo_xml': [],
+    'test': [
+        'test/purchase_double_validation_test.yml',
+         ],
     'installable': True,
     'active': False,
 
