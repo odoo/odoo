423bf2121351bf496c183e9bc6302a4eb70d088e
Olivier Dony <odo@openerp.com>
2011-01-10 14:55:17 +0100

    [FIX] ir.ui.view_sc: ref_id cannot be a m2o as it can reference different resources
    bzr revid: odo@openerp.com-20110110135517-wi3dfqqzvfhx5n9f

--- bin/addons/base/ir/ir_ui_view.py
+++ bin/addons/base/ir/ir_ui_view.py
@@ -182,7 +182,7 @@ class view_sc(osv.osv):
     _name = 'ir.ui.view_sc'
     _columns = {
         'name': fields.char('Shortcut Name', size=64, required=True),
-        'res_id': fields.many2one('ir.ui.menu','Resource Ref.', ondelete='cascade'),
+        'res_id': fields.integer('Resource Ref.', help="Reference of the target resource, whose model/table depends on the 'Resource Name' field."),
         'sequence': fields.integer('Sequence'),
         'user_id': fields.many2one('res.users', 'User Ref.', required=True, ondelete='cascade', select=True),
         'resource': fields.char('Resource Name', size=64, required=True, select=True)
