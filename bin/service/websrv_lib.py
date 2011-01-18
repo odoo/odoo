5168d0bd09fb1e498ac4902957fd722042db0e9c
P. Christeas <p_christ@hol.gr>
2011-01-18 14:59:07 +0200

    websrv_lib: defer one socket.flush(), keep packets minimum.
    bzr revid: p_christ@hol.gr-20110118125907-t7dz40a40pj6k8gs

--- bin/service/websrv_lib.py
+++ bin/service/websrv_lib.py
@@ -182,11 +182,12 @@ class FixSendError:
         self.send_header('Connection', 'close')
         self.send_header('Content-Length', len(content) or 0)
         self.end_headers()
-        if hasattr(self, '_flush'):
-            self._flush()
         
         if self.command != 'HEAD' and code >= 200 and code not in (204, 304):
             self.wfile.write(content)
+
+        if hasattr(self, '_flush'):
+            self._flush()
 
 class HttpOptions:
     _HTTP_OPTIONS = {'Allow': ['OPTIONS' ] }
