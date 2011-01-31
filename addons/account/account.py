6847838d4b9bc1505a2f894bf7934437eac1ce33
mtr <mtr@mtr>
2011-01-31 13:32:07 +0530

    [IMP] account:improved code for the account duplication bug
    bzr revid: mtr@mtr-20110131080207-hmsbxkmonkvdh3to

--- addons/account/account.py
+++ addons/account/account.py
@@ -301,8 +301,8 @@ class account_account(osv.osv):
                 for fn in field_names:
                     sums.setdefault(current.id, {})[fn] = accounts.get(current.id, {}).get(fn, 0.0)
                     for child in current.child_id:
-                        if child.company_id.currency_id.id == current.company_id.currency_id.id:
-                            if sums.get(child.id, False):
+                        if sums.get(child.id, False):
+                            if child.company_id.currency_id.id == current.company_id.currency_id.id:
                                 sums[current.id][fn] += sums[child.id][fn]
                             else:
                                 sums[current.id][fn] += currency_obj.compute(cr, uid, child.company_id.currency_id.id, current.company_id.currency_id.id, sums[child.id][fn], context=context)
