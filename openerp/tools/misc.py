ec17bb9e74548a78da9a75ab4475e153d7f624b6
Vo Minh Thu <vmt@openerp.com>
2011-02-08 10:24:58 +0100

    [IMP] forgot to add loglevels.py and reexport 3 functions in tools.misc.
    bzr revid: vmt@openerp.com-20110208092458-qjfvf1vgflervdpm

--- openerp/tools/misc.py
+++ openerp/tools/misc.py
@@ -59,6 +59,10 @@ import openerp.loglevels as loglevels
 from config import config
 from lru import LRU
 
+# get_encodings, ustr and exception_to_unicode were originally from tools.misc.
+# There are moved to loglevels until we refactor tools.
+from openerp.loglevels import get_encodings, ustr, exception_to_unicode
+
 _logger = logging.getLogger('tools')
 
 # List of etree._Element subclasses that we choose to ignore when parsing XML.
@@ -840,74 +844,6 @@ class cache(object):
 
 def to_xml(s):
     return s.replace('&','&amp;').replace('<','&lt;').replace('>','&gt;')
-
-def get_encodings(hint_encoding='utf-8'):
-    fallbacks = {
-        'latin1': 'latin9',
-        'iso-8859-1': 'iso8859-15',
-        'cp1252': '1252',
-    }
-    if hint_encoding:
-        yield hint_encoding
-        if hint_encoding.lower() in fallbacks:
-            yield fallbacks[hint_encoding.lower()]
-
-    # some defaults (also taking care of pure ASCII)
-    for charset in ['utf8','latin1']:
-        if not (hint_encoding) or (charset.lower() != hint_encoding.lower()):
-            yield charset
-
-    from locale import getpreferredencoding
-    prefenc = getpreferredencoding()
-    if prefenc and prefenc.lower() != 'utf-8':
-        yield prefenc
-        prefenc = fallbacks.get(prefenc.lower())
-        if prefenc:
-            yield prefenc
-
-
-def ustr(value, hint_encoding='utf-8'):
-    """This method is similar to the builtin `str` method, except
-       it will return unicode() string.
-
-    @param value: the value to convert
-    @param hint_encoding: an optional encoding that was detected
-                          upstream and should be tried first to
-                          decode ``value``.
-
-    @rtype: unicode
-    @return: unicode string
-    """
-    if isinstance(value, Exception):
-        return exception_to_unicode(value)
-
-    if isinstance(value, unicode):
-        return value
-
-    if not isinstance(value, basestring):
-        try:
-            return unicode(value)
-        except Exception:
-            raise UnicodeError('unable to convert %r' % (value,))
-
-    for ln in get_encodings(hint_encoding):
-        try:
-            return unicode(value, ln)
-        except Exception:
-            pass
-    raise UnicodeError('unable to convert %r' % (value,))
-
-
-def exception_to_unicode(e):
-    if (sys.version_info[:2] < (2,6)) and hasattr(e, 'message'):
-        return ustr(e.message)
-    if hasattr(e, 'args'):
-        return "\n".join((ustr(a) for a in e.args))
-    try:
-        return ustr(e)
-    except Exception:
-        return u"Unknown message"
-
 
 # to be compatible with python 2.4
 import __builtin__
