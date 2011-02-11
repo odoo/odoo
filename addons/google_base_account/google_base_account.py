fb26e7924e5086c32ef964a6ffa891dcf7cbdc32
Sbh (OpenERP) <sbh@tinyerp.com>
2011-02-11 18:07:53 +0530

    [ADD] Add google_base_account module
    bzr revid: sbh@tinyerp.com-20110211123753-vag9bnriwc0a934o

---
+++ addons/google_base_account/google_base_account.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields,osv,orm
+
+    
+class res_users(osv.osv):
+    _inherit = "res.users"
+    _columns = {
+        'gmail_user': fields.char('User Name', size=64,),
+        'gmail_password': fields.char('Password', size=64),
+    }
+    def check_login(self, cr, uid, ids, context=None):
+        # we can check login detail is correct or not
+        return {'type': 'ir.actions.act_window_close'}    
+res_users()    
+# vim:expandtab:smartindent:toabstop=4:softtabstop=4:shiftwidth=4:
+
