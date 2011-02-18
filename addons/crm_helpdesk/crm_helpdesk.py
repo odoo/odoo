3e1b8764eb998c7224e1c76947ca9c0907b764ea
Yogesh (OpenERP) <ysa@tinyerp.com>
2011-02-18 16:58:50 +0530

    [IMP] crm_helpdesk :- call email.message.send.wizard in oprtunity instead of crm.send.mail wizard.
    bzr revid: ysa@tinyerp.com-20110218112850-k9bpkg9v7rceixq7

--- addons/crm_helpdesk/crm_helpdesk.py
+++ addons/crm_helpdesk/crm_helpdesk.py
@@ -22,6 +22,7 @@
 from crm import crm
 from osv import fields, osv
 import time
+import tools
 
 class crm_helpdesk(crm.crm_case, osv.osv):
     """ Helpdesk Cases """
@@ -85,6 +86,37 @@ class crm_helpdesk(crm.crm_case, osv.osv):
         'company_id': lambda s, cr, uid, c: s.pool.get('res.company')._company_default_get(cr, uid, 'crm.helpdesk', context=c),
         'priority': lambda *a: crm.AVAILABLE_PRIORITIES[2][0],
     }
+
+    def action_send_email(self, cr, uid, ids, context=None):
+        """
+            Open Send email wizard.
+        """
+        if context is None:
+            context = {}
+        for lead in self.browse(cr, uid ,ids, context):
+            context.update({
+                    'mail':'new',
+                    'model': 'crm.lead',
+                    'default_name': lead.name,
+                    'default_email_to': lead.email_from,
+                    'default_email_from': lead.user_id and lead.user_id.address_id and lead.user_id.address_id.email,
+                    'default_description': '\n' + (tools.ustr(lead.user_id.signature or '')),
+                    'default_reply_to': lead.section_id and lead.section_id.reply_to or False,
+                    'default_model': context.get('model',''),
+                    'default_email_cc': tools.ustr(lead.email_cc or ''),
+                    'default_res_id': context.get('rec_id',0)
+                })
+        result = {
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_model': 'email.message.wizard_send',
+                'view_id': False,
+                'context': context,
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+                'nodestroy': True
+                }
+        return result
 
 crm_helpdesk()
 
