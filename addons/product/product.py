00f0b7d9affc8486cb8e4ee2028154e0ab75e5ce
Quentin THEURET <quentin@tempo-quentin>
2011-02-28 10:34:50 +0100

    [FIX] Fix the name_search method of product_product
    lp bug: https://launchpad.net/bugs/726415 fixed
    
    bzr revid: quentin@tempo-quentin-20110228093450-pffa9txsgza5eamm

--- addons/product/product.py
+++ addons/product/product.py
@@ -557,7 +557,7 @@ class product_product(osv.osv):
         if not args:
             args=[]
         if name:
-            ids = self.search(cr, user, [('default_code',operator,name)]+ args, limit=limit, context=context)
+            ids = self.search(cr, user, [('default_code','=',name)]+ args, limit=limit, context=context)
             if not len(ids):
                 ids = self.search(cr, user, [('ean13','=',name)]+ args, limit=limit, context=context)
             if not len(ids):
