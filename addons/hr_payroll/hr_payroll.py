cbaaaba3833ae5d717e230eba985be339302f1ff
mtr <mtr@mtr>
2011-03-02 14:23:59 +0530

    [IMP] hr_payroll:improved help for salary rules.
    bzr revid: mtr@mtr-20110302085359-19ayfxa43pe2aasa

--- addons/hr_payroll/hr_payroll.py
+++ addons/hr_payroll/hr_payroll.py
@@ -1276,8 +1276,8 @@ class hr_salary_rule(osv.osv):
     _name = 'hr.salary.rule'
     _columns = {
         'appears_on_payslip': fields.boolean('Appears on Payslip', help="Do you want to display rule on payslip?"),
-        'min_range': fields.float('Minimum Range', required=False, help="The minimum amount that can be applied for this rule."),
-        'max_range': fields.float('Maximum Range', required=False, help="The maximum amount that can be applied for this rule."),
+        'min_range': fields.float('Minimum Range', required=False, help="The minimum amount, applied for this rule."),
+        'max_range': fields.float('Maximum Range', required=False, help="The maximum amount, applied for this rule."),
         'sal_rule_id':fields.many2one('hr.salary.rule', 'Parent Salary Structure', select=True),
         'child_depend':fields.boolean('Children Rule'),
         'child_ids':fields.one2many('hr.salary.rule', 'sal_rule_id', 'Child Salary Sructure'),
