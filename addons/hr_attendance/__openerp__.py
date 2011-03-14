721d40f606e29faf8da519b35e5519823441eed0
rch-openerp <rch@tinyerp.com>
2011-03-14 16:50:13 +0530

    [ADD]: Images of hr, hr_attendance and hr_contract modules.
    bzr revid: rch@tinyerp.com-20110314112013-zwazl3oanlsh7fz5

--- addons/hr_attendance/__openerp__.py
+++ addons/hr_attendance/__openerp__.py
@@ -26,10 +26,12 @@
     'category': 'Generic Modules/Human Resources',
     'description': """
     This module aims to manage employee's attendances.
+    ==================================================
     Keeps account of the attendances of the employees on the basis of the
     actions(Sign in/Sign out) performed by them.
        """,
     'author': 'OpenERP SA',
+    'images': ['images/hr_attendances.jpeg'],
     'depends': ['hr'],
     'update_xml': [
         'security/ir.model.access.csv',
