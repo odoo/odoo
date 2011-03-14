d5dbf3c1f070bf16a1a0b40bc9725dc5f97f5b72
vro <vro@tinyerp.com>
2011-03-14 17:25:31 +0530

    [ADD]: Images: anonymization, association, auction, auditrail
    bzr revid: vro@tinyerp.com-20110314115531-mqsc31bn9h4u4ccm

--- addons/association/__openerp__.py
+++ addons/association/__openerp__.py
@@ -24,7 +24,10 @@
     'name': 'Association profile',
     'version': '0.1',
     'category': 'Profile',
-    'description': """This module is to create Profile for Associates""",
+    'description': """
+    This module is to configure modules related to an association.
+    ==============================================================
+    """,
     'author': 'OpenERP SA',
     'depends': ['membership', 'event'],
     'update_xml': ['security/ir.model.access.csv', 'profile_association.xml'],
@@ -32,5 +35,6 @@
     'installable': True,
     'active': False,
     'certificate': '0078696047261',
+    'images': ['images/association1.jpeg'],
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
