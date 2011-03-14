9672c7c83f16abc115d59757cc844e5a0a955ec0
Mustufa Rangwala <mra@mra-laptop>
2011-03-14 17:14:47 +0530

    [FIX] small fix
    bzr revid: mra@mra-laptop-20110314114447-h1zwjsaedpmlnmdl

--- addons/hr_payroll/hr_payroll.py
+++ addons/hr_payroll/hr_payroll.py
@@ -21,6 +21,7 @@
 ##############################################################################
 
 import time
+import datetime
 from datetime import date
 from datetime import datetime
 from datetime import timedelta
