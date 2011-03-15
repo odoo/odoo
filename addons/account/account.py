9c83ebedd2418ad866b1815f98f77bad53ec35fe
ARA (OpenERP) <ara@tinyerp.com>
2011-03-15 15:36:53 +0530

    [FIX] account: [6.0.1] Exception raised when setting an Account InActive
    bzr revid: ara@tinyerp.com-20110315100653-g6ic3b6s8sz1bdxp

--- addons/account/account.py
+++ addons/account/account.py
@@ -247,9 +247,6 @@ class account_account(osv.osv):
         }
         #get all the necessary accounts
         children_and_consolidated = self._get_children_and_consol(cr, uid, ids, context=context)
-        #children_and_consolidated is empty when account has not any child account and try to make that account as inactive
-        if not children_and_consolidated:
-            children_and_consolidated = ids
         #compute for each account the balance/debit/credit from the move lines
         accounts = {}
         res = {}
