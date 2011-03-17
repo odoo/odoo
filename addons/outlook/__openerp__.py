79bf0f73da6a231d8af08f5f24219524ee9f326a
vro <vro@tinyerp.com>
2011-03-17 16:34:08 +0530

    [ADD]: Images: outlook, wiki, wiki_faq, wiki_quality_manual
    bzr revid: vro@tinyerp.com-20110317110408-2o5nbazf4a1b9iru

--- addons/outlook/__openerp__.py
+++ addons/outlook/__openerp__.py
@@ -28,11 +28,12 @@
     'depends' : ['base', 'mail_gateway'],
     'category' : 'Generic Modules/Outlook interface',
     'description': '''
-      This module provide the Outlook plug-in. 
+      This module provides the Outlook Plug-in.
+      =========================================
 
       Outlook plug-in allows you to select an object that you’d like to add
       to your email and its attachments from MS Outlook. You can select a partner, a task,
-      a project, an analytical account, or any other object and Archived selected
+      a project, an analytical account, or any other object and archive selected
       mail in mailgate.messages with attachments.
 
       ''',
@@ -42,6 +43,7 @@
     'active': False,
     'installable': True,
     'certificate' : '001278773815818292125',
+    'images': ['images/config_outlook.jpeg','images/outlook_config_openerp.jpeg','images/outlook_push.jpeg'],
 
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
