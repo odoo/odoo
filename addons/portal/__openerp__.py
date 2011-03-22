8877996161a36d8fc9179ad1e518d9f91ece965f
Raphael Collet <rco@openerp.com>
2011-03-22 15:38:25 +0100

    first sketch of module portal
    bzr revid: rco@openerp.com-20110322143825-swyxasmu0apui9k5

---
+++ addons/portal/__openerp__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2011 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    "name" : "Portal",
+    "version" : "0.1",
+    "depends" : ["base"],
+    "author" : "OpenERP SA",
+    "category": 'Tools',
+    "description": """
+This module defines 'portals' to customize the access to your OpenERP database.
+
+A portal defines customized user menu and access rights for a group of users
+(the ones associated to that portal).  Portals are convenient to provide a
+specific access to your OpenERP instance for external users.
+
+It also associates user groups to the portal users (adding a group in the
+portal automatically adds it to the portal users, etc).  That feature is very
+handy when used in combination with the module 'share'.
+    """,
+    'website': 'http://www.openerp.com',
+    'demo_xml': [],
+    'data': [],
+    'installable': True,
+    'web': True,
+    'certificate' : '',
+    'images': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ addons/portal/portal.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2011 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+from tools.translate import _
+
+class portal(osv.osv):
+    _name = 'res.portal'
+    _description = _('Portal')
+    _columns = {
+        'name': fields.char(_('Name'), size=64, required=True),
+    }
+
