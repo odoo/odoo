9ae71a441637b0dd0858fc2316a8eb1bd70908dc
Raphael Collet <rco@openerp.com>
2011-03-23 13:56:25 +0100

    add portal form view
    bzr revid: rco@openerp.com-20110323125625-rmtw8aa1yizbo3eh

--- addons/portal/__openerp__.py
+++ addons/portal/__openerp__.py
@@ -27,19 +27,18 @@
     "author" : "OpenERP SA",
     "category": 'Tools',
     "description": """
-This module defines 'portals' to customize the access to your OpenERP database.
+This module defines 'portals' to customize the access to your OpenERP database
+for external users.
 
 A portal defines customized user menu and access rights for a group of users
-(the ones associated to that portal).  Portals are convenient to provide a
-specific access to your OpenERP instance for external users.
-
-It also associates user groups to the portal users (adding a group in the
-portal automatically adds it to the portal users, etc).  That feature is very
-handy when used in combination with the module 'share'.
+(the ones associated to that portal).  It also associates user groups to the
+portal users (adding a group in the portal automatically adds it to the portal
+users, etc).  That feature is very handy when used in combination with the
+module 'share'.
     """,
     'website': 'http://www.openerp.com',
     'demo_xml': [],
-    'data': [],
+    'data': ['portal_view.xml'],
     'installable': True,
     'web': True,
     'certificate' : '',
