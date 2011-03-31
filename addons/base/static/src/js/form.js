6910a9c19af797bdee9c7d2dc315154810738c40
Fabien Meghazi <fme@openerp.com>
2011-03-31 18:03:23 +0200

    [ADD] Activated simple datepicker on date field
    bzr revid: fme@openerp.com-20110331160323-opcovdlczzi6ugxs

--- addons/base/static/src/js/form.js
+++ addons/base/static/src/js/form.js
@@ -462,6 +462,10 @@ openerp.base.form.FieldDate = openerp.base.form.FieldChar.extend({
     init: function(view, node) {
         this._super(view, node);
         this.template = "FieldDate";
+    },
+    start: function() {
+        this._super.apply(this, arguments);
+        this.$element.find('input').datepicker();
     }
 });
 
