e21a561cb19cb3b99730f4fb7970dbc202584354
skh <skh@tinyerp.com>
2011-04-05 18:09:58 +0530

    [FIX] payment mode - account must belong to company
    lp bug: https://launchpad.net/bugs/739562 fixed
    
    bzr revid: skh@tinyerp.com-20110405123958-savml2himnvrhsr3

--- addons/account_payment/account_payment.py
+++ addons/account_payment/account_payment.py
@@ -33,7 +33,9 @@ class payment_mode(osv.osv):
             required=True,help='Bank Account for the Payment Mode'),
         'journal': fields.many2one('account.journal', 'Journal', required=True,
             domain=[('type', 'in', ('bank','cash'))], help='Bank or Cash Journal for the Payment Mode'),
-        'company_id': fields.many2one('res.company', 'Company', required=True),
+        'company_id': fields.many2one('res.company', 'Company',required=True),
+        'partner_id':fields.related('company_id','partner_id',type='many2one',relation='res.partner',string='Partner',store=True,),
+        
     }
     _defaults = {
         'company_id': lambda self,cr,uid,c: self.pool.get('res.users').browse(cr, uid, uid, c).company_id.id
@@ -49,6 +51,13 @@ class payment_mode(osv.osv):
             JOIN payment_mode pm ON (pm.bank_id = pb.id)
             WHERE pm.id = %s """, [payment_code])
         return [x[0] for x in cr.fetchall()]
+    
+    def onchange_company_id (self,cr,uid,ids,company_id,context=None):
+        vals={}
+        if company_id:
+            partner = self.pool.get('res.company').read(cr, uid, [company_id], ['partner_id'])[0]['partner_id']
+        return {'value': {'partner_id': partner}}
+                
 
 payment_mode()
 
