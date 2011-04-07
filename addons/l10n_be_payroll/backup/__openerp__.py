d5222f88618f6912357d987cbe49962bf907f87c
Osenda <>
2011-04-07 17:05:01 +0200

    [ADD] l10n_be_payroll: initial commit
    bzr revid: osenda-20110407150501-bvnenhpbcmhyp7k4

---
+++ addons/l10n_be_payroll/backup/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+{
+    'name': 'hr_payroll_salary_rule',
+    'category': 'payroll',
+    'init_xml':[],
+    'author': 'AOS',
+    'depends': ['base','hr_payroll','l10n_be'],
+    'version': '1.0',
+    'active': False,
+    'demo_xml': [],
+    'update_xml':[
+#	 'res.users.csv',
+#	 'security/hr.salary.rule.csv',
+	 'hr_payroll_rule.xml',
+	 'data/hr.salary.rule.csv',
+    ],
+    'installable': True
+}
