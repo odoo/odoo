874099dbcfeeec5619f42266148b889e90258435
Osenda <>
2011-04-07 17:05:01 +0200

    [ADD] l10n_be_payroll: initial commit
    bzr revid: osenda-20110407150501-hzo5uf7omflrx7r2

---
+++ addons/l10n_be_payroll/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+{
+    'name': 'hr_payroll_salary_rule',
+    'category': 'payroll',
+    'init_xml':[],
+    'author': 'AOS',
+    'depends': ['hr_payroll','hr_contract'],
+    'version': '1.0',
+    'active': False,
+    'demo_xml': [],
+    'update_xml':[
+#	 'res.users.csv',
+#	 'security/hr.salary.rule.csv',
+	 'hr_payroll_rule_view.xml',
+	 'hr_payroll_rule_data.xml',
+	 'data/hr.salary.rule.csv',
+    ],
+    'installable': True
+}
---
+++ addons/l10n_be_payroll/hr_payroll_rule.py
@@ -0,0 +1,68 @@
+#-*- coding:utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    d$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+
+import netsvc
+from osv import fields, osv
+import tools
+from tools.translate import _
+import decimal_precision as dp
+
+
+
+class hr_contract_be(osv.osv):
+
+    _inherit = 'hr.contract'
+    _description = 'Add for belgium users'
+    _columns = {
+
+	'rem_frais_dep':fields.float('Reimbursement of travel expenses', digits=(16,2)),
+	'voiture_soc':fields.float('Company car ', digits=(16,2)),
+	'divers_ex_onss':fields.float('Miscellaneous exempt ONSS ', digits=(16,2)),
+	'valeur_cheque':fields.float('Check Value Meal ', digits=(16,2)),
+	'part_trav':fields.float('Check Value Meal - by worker ', digits=(16,2)),
+	'assurance':fields.float('Insurance Group - by worker ', digits=(16,2)),
+	'avantage':fields.float('Benefits of various nature ', digits=(16,2)),
+	'suppl_net':fields.float('Net supplements', digits=(16,2)),
+	'retenus_net':fields.float('Net retained ', digits=(16,2)),
+	'volontariat_fiscal':fields.float('Voluntary tax ', digits=(16,2)),
+	'horaire_effectif': fields.many2one('resource.calendar','Actual Work', help="Hours of work means the actual working time elapsing between the beginning and end of the workday, regardless of where it runs, excluding the stop work devoted to meals, breaks and, more generally, any interruptions between 2 sequences of work that are not actually worked since the employee can go freely about his personal affairs"),
+
+    }
+hr_contract_be()
+
+class hr_employee_be(osv.osv):
+
+    _inherit = 'hr.employee'
+    _description = 'same as before'
+    _columns = {
+	'statut_fiscal':fields.selection([('without income','Without Income'),('with income','With Income')], 'Statut Fiscal'),
+	'handicap':fields.boolean('Handicap'),
+        'handicap_child':fields.boolean('Handicap Children'),
+	'resident':fields.boolean('Residente'),
+	'number_handicap':fields.integer('Number of Handicap'),
+    }
+hr_employee_be()
