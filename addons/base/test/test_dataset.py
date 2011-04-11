5a901eef839cf82dae9886c2e5d7929234e9d86d
Xavier Morel <xmo@openerp.com>
2011-04-11 08:27:25 +0200

    [FIX] dataset tests
    bzr revid: xmo@openerp.com-20110411062725-7eh31m9vtuasmsjz

--- addons/base/test/test_dataset.py
+++ addons/base/test/test_dataset.py
@@ -14,13 +14,13 @@ class TestDataSetController(unittest2.TestCase):
         self.search.return_value = []
         self.read.return_value = []
 
-        self.assertFalse(self.dataset.do_find(self.request, 'fake.model'))
+        self.assertFalse(self.dataset.do_search_read(self.request, 'fake.model'))
         self.read.assert_called_once_with([], False)
 
     def test_regular_find(self):
         self.search.return_value = [1, 2, 3]
 
-        self.dataset.do_find(self.request, 'fake.model')
+        self.dataset.do_search_read(self.request, 'fake.model')
         self.read.assert_called_once_with([1, 2, 3], False)
 
     def test_ids_shortcut(self):
@@ -32,7 +32,7 @@ class TestDataSetController(unittest2.TestCase):
         ]
 
         self.assertEqual(
-            self.dataset.do_find(self.request, 'fake.model', ['id']),
+            self.dataset.do_search_read(self.request, 'fake.model', ['id']),
             [{'id': 1}, {'id': 2}, {'id': 3}])
         self.assertFalse(self.read.called)
 
@@ -46,7 +46,7 @@ class TestDataSetController(unittest2.TestCase):
         result = self.dataset.do_get(
             self.request, 'fake.model', [3, 2, 1])
         self.read.assert_called_once_with(
-            [3, 2, 1])
+            [3, 2, 1], False)
         self.assertFalse(self.search.called)
 
         self.assertEqual(
