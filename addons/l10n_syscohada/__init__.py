8d02d91e37aac3273dc42f6398d3e5f8ef1dda1f
Tidiane Sy (Baamtu) <tsy@baamtu.com>
2011-04-11 17:25:03 +0200

    [FIX] 3 improvements suggested by Olivier Dony.1currency management - 2copyrigth fix - 3precise covered countries
    bzr revid: tsy@baamtu.com-20110411152503-mcaropfxxdpla2fs

--- addons/l10n_syscohada/__init__.py
+++ addons/l10n_syscohada/__init__.py
@@ -1,21 +1,20 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   Copyright (C) 2010-2011 BAAMTU SARL (<http://www.baamtu.sn>). All Rights Reserved
-#    $Id$
-#   contact : leadsn@baamtu.com
+#    Copyright (C) 2010-2011 BAAMTU SARL (<http://www.baamtu.sn>).
+#    contact: leadsn@baamtu.com
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
