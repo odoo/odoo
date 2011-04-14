8acaa289a0a8dc20239c78c9bf815c2633e60284
Xavier Morel <xmo@openerp.com>
2011-04-14 15:23:10 +0200

    [FIX] correctly initialize web_chat addon
    bzr revid: xmo@openerp.com-20110414132310-qfuewowccf1f2fx9

--- addons/web_chat/__openerp__.py
+++ addons/web_chat/__openerp__.py
@@ -1,8 +1,14 @@
 {
     "name": "Web Chat",
     "version": "2.0",
-    "depends": [],
-    "js": ["static/src/web_chat.js"],
+    "depends": ['base'],
+    "js": [
+        'static/lib/AjaxIM/js/jquery.jsonp-1.1.0.js',
+        'static/lib/AjaxIM/js/jquery.jstore-all-min.js',
+        'static/lib/AjaxIM/js/jquery.md5.js',
+        'static/lib/AjaxIM/js/im.js',
+        'static/src/js/web_chat.js'
+    ],
     "css": [],
-    'active': False,
+    'active': True,
 }
