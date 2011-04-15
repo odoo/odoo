4b46fd652aef91324875955e04256ca158d89ce3
mtr <mtr@mtr>
2011-04-15 15:05:16 +0530

    [IMP] sale:improved code for revision no 4640
    bzr revid: mtr@mtr-20110415093516-r3ffuz6mrbaqca3c

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -1204,15 +1204,16 @@ class sale_config_picking_policy(osv.osv_memory):
     def execute(self, cr, uid, ids, context=None):
         ir_values_obj = self.pool.get('ir.values')
         ir_model_data_obj = self.pool.get('ir.model.data')
+        stock_location_obj = self.pool.get('stock.location')
         location_id = ir_model_data_obj.get_object_reference(cr, uid, 'stock', 'stock_location_output')
         location_id = location_id and location_id[1] or False
         for o in self.browse(cr, uid, ids, context=context):
             ir_values_obj.set(cr, uid, 'default', False, 'picking_policy', ['sale.order'], o.picking_policy)
             ir_values_obj.set(cr, uid, 'default', False, 'order_policy', ['sale.order'], o.order_policy)
             if o.step == 'one':
-                self.pool.get('stock.location').write(cr, uid, [location_id], {'chained_auto_packing': 'transparent'})
+                stock_location_obj.write(cr, uid, [location_id], {'chained_auto_packing': 'transparent'})
             if o.step == 'two':
-                self.pool.get('stock.location').write(cr, uid, [location_id], {'chained_auto_packing': 'manual'})
+                stock_location_obj.write(cr, uid, [location_id], {'chained_auto_packing': 'manual'})
 
 sale_config_picking_policy()
 
