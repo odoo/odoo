e217bb0e3964131d942e9748926f30a73ffe32e3
Rifakat Haradwala (Open ERP) <rha@tinyerp.com>
2011-04-20 17:34:59 +0530

    [REM] crm: removed crm_send_email.py as it is unused
    bzr revid: rha@tinyerp.com-20110420120459-f2ek7ifx0x0lgzhe

--- addons/crm/__openerp__.py
+++ addons/crm/__openerp__.py
@@ -84,7 +84,6 @@ Creates a dashboard for CRM that includes:
         'wizard/crm_opportunity_to_phonecall_view.xml',
         'wizard/crm_partner_to_opportunity_view.xml',
 
-#        'wizard/crm_send_email_view.xml',
         'wizard/crm_add_note_view.xml',
         'wizard/crm_merge_opportunities_view.xml',
 
