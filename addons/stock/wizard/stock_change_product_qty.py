10c8c4cf196afd95103b49811a3d6b7dffa606ff
ron@tinyerp.com <>
2011-05-06 11:33:58 +0530

    [FIX]stock:entering a negative value in the stock level when click on update button on product form
    lp bug: https://launchpad.net/bugs/777803 fixed
    
    bzr revid: ron@tinyerp.com-20110506060358-audc98nfxyt6b116

--- addons/stock/wizard/stock_change_product_qty.py
+++ addons/stock/wizard/stock_change_product_qty.py
@@ -85,6 +85,8 @@ class stock_change_product_qty(osv.osv_memory):
 
         res_original = prod_obj_pool.browse(cr, uid, rec_id, context=context)
         for data in self.browse(cr, uid, ids, context=context):
+            if data.new_quantity <= 0:
+                raise osv.except_osv(_('Warning!'), _('Please provide Proper Quantity !'))
             inventory_id = inventry_obj.create(cr , uid, {'name': _('INV: %s') % tools.ustr(res_original.name)}, context=context)
             line_data ={
                 'inventory_id' : inventory_id,
