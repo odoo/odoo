98c4cf6c21b2c319e6a8fbcfa882452ec505906a
niv-openerp <nicolas.vanhoren@openerp.com>
2011-05-09 17:17:57 +0200

    [IMP] Style change.
    bzr revid: nicolas.vanhoren@openerp.com-20110509151757-16901jxeokul7auh

--- addons/base_gantt/controllers/main.py
+++ addons/base_gantt/controllers/main.py
@@ -14,10 +14,7 @@ class GanttView(View):
         return {'fields_view':fields_view}
 
     @openerpweb.jsonrequest
-    def reload_gantt(self, req, **kw):
-
-        model = req.session.model(kw['model'])
-        domain = kw['domain']
-
+    def reload_gantt(self, req, model, domain):
+        model = req.session.model(model)
         event_ids = model.search(domain)
         return event_ids
