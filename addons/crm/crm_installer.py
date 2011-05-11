6fe5e838ad47a255982bc568d105bb3098a918f7
Ujjvala Collins (OpenERP) <uco@tinyerp.com>
2011-05-11 15:10:13 +0530

    [FIX] crm: Corrected typo.
    bzr revid: uco@tinyerp.com-20110511094013-8mo05py17r30zw7r

--- addons/crm/crm_installer.py
+++ addons/crm/crm_installer.py
@@ -37,7 +37,7 @@ class crm_installer(osv.osv_memory):
         'thunderbird': fields.boolean('Thunderbird', help="Allows you to link your e-mail to OpenERP's documents. You can attach it to any existing one in OpenERP or create a new one."),
         'outlook': fields.boolean('MS-Outlook', help="Allows you to link your e-mail to OpenERP's documents. You can attach it to any existing one in OpenERP or create a new one."),
         'wiki_sale_faq': fields.boolean('Sale FAQ', help="Helps you manage wiki pages for Frequently Asked Questions on Sales Application."),
-        'import_google_contact': fields.boolean('Google Contacts', Help="It will import contacts from your google account."),
+        'import_google_contact': fields.boolean('Google Contacts', help="It will import contacts from your google account."),
     }
 
     def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
