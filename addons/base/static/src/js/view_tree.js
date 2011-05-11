2397f09340102f48939c75dedf2267d4c5f1fc52
Antony Lesuisse <al@openerp.com>
2011-05-11 17:25:48 +0200

    dummy treeview
    bzr revid: al@openerp.com-20110511152548-ayesmkxgwzc0q9ef

---
+++ addons/base/static/src/js/view_tree.js
@@ -0,0 +1,26 @@
+/*---------------------------------------------------------
+ * OpenERP base library
+ *---------------------------------------------------------*/
+
+openerp.base.view_tree = function(openerp) {
+
+openerp.base.views.add('tree', 'openerp.base.TreeView');
+openerp.base.TreeView = openerp.base.Controller.extend({
+/**
+ * Genuine tree view (the one displayed as a tree, not the list)
+ */
+    start: function () {
+        this._super();
+        this.$element.append('Tree view');
+    },
+    do_show: function () {
+        this.$element.show();
+    },
+    do_hide: function () {
+        this.$element.hide();
+    }
+});
+
+};
+
+// vim:et fdc=0 fdl=0 foldnestmax=3 fdm=syntax:
