999b70480b24412c65dca613e5842d60cc37b510
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2011-05-18 15:51:19 +0200

    [IMP] partenr ledger report: empty the fiscalyear on selection of 'unreconciled entries' filter
    bzr revid: qdp-launchpad@openerp.com-20110518135119-f6teo1g19jfldq4a

--- addons/account/wizard/account_report_partner_ledger.py
+++ addons/account/wizard/account_report_partner_ledger.py
@@ -44,6 +44,8 @@ class account_partner_ledger(osv.osv_memory):
     def onchange_filter(self, cr, uid, ids, filter='filter_no', fiscalyear_id=False, context=None):
         res = super(account_partner_ledger, self).onchange_filter(cr, uid, ids, filter=filter, fiscalyear_id=fiscalyear_id, context=context)
         if filter in ['filter_no', 'unreconciled']:
+            if filter  == 'unreconciled':
+                 res['value'].update({'fiscalyear_id': False})
             res['value'].update({'initial_balance': False, 'period_from': False, 'period_to': False, 'date_from': False ,'date_to': False})
         return res
 
