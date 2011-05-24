db63cecdcbf119691fb8ad1767e299087fff6035
mayur maheshwari (Open ERP) <mma@tinyerp.com>
2011-05-24 17:34:42 +0530

    [FIX]stock_planning_:Now edit generated Sales Forecasts in Form View By wizard
    lp bug: https://launchpad.net/bugs/781655 fixed
    
    bzr revid: mma@tinyerp.com-20110524120442-xcvn3e0mslp5mdfo

--- addons/stock_planning/wizard/stock_planning_forecast.py
+++ addons/stock_planning/wizard/stock_planning_forecast.py
@@ -102,7 +102,7 @@ class stock_sale_forecast_createlines(osv.osv_memory):
         return {
             'domain': "[('id','in', ["+','.join(map(str, forecast_lines))+"])]",
             'view_type': 'form',
-            "view_mode": 'tree, form',
+            "view_mode": 'tree,form',
             'res_model': 'stock.sale.forecast',
             'type': 'ir.actions.act_window',
             'search_view_id': id['res_id'],
