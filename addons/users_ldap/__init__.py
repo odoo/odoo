4447c691a2d6854a37fd370f653dcc37c0f44f96
mayur maheshwari (Open ERP) <mma@tinyerp.com>
2011-06-04 15:22:46 +0530

    [IMP]delivery,hr_timesheet,lunch,users_ldap:Improvement is Done and a installer in user_ldap
    bzr revid: mma@tinyerp.com-20110604095246-vpd2gdecslk5wd73

--- addons/users_ldap/__init__.py
+++ addons/users_ldap/__init__.py
@@ -19,5 +19,6 @@
 ##############################################################################
 
 import users_ldap
+import installer
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
