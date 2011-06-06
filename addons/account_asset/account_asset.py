b1928c32d9d369d59a641146aa08fc9decec4378
Ujjvala Collins (OpenERP) <uco@tinyerp.com>
2011-06-06 17:56:18 +0530

    [REM] account_asset: Removed analytic journal field from asset category as it is not used anywhere.
    bzr revid: uco@tinyerp.com-20110606122618-wt9xmz5xtsybempd

--- addons/account_asset/account_asset.py
+++ addons/account_asset/account_asset.py
@@ -31,7 +31,6 @@ class account_asset_category(osv.osv):
     _columns = {
         'name': fields.char('Name', size=64, required=True, select=1),
         'note': fields.text('Note'),
-        'journal_analytic_id': fields.many2one('account.analytic.journal', 'Analytic journal'),
         'account_analytic_id': fields.many2one('account.analytic.account', 'Analytic account'),
         'account_asset_id': fields.many2one('account.account', 'Asset Account', required=True),
         'account_depreciation_id': fields.many2one('account.account', 'Depreciation Account', required=True),
