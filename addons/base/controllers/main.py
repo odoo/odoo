feb346b1f6135568351a5dabcc0584b72a080f3e
Fabien Meghazi <fme@openerp.com>
2011-06-14 11:00:50 +0200

    [FIX] Repair callbacks
    bzr revid: fme@openerp.com-20110614090050-fj59w1ntc2utq4eu

--- addons/base/controllers/main.py
+++ addons/base/controllers/main.py
@@ -430,9 +430,9 @@ class DataSet(openerpweb.Controller):
         return Model.unlink(ids)
 
     @openerpweb.jsonrequest
-    def call(self, req, model, method, args):
+    def call(self, req, model, method, ids, args):
         m = req.session.model(model)
-        r = getattr(m, method)(*args)
+        r = getattr(m, method)(ids, *args)
         return {'result': r}
 
     @openerpweb.jsonrequest
