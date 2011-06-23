8cfb7dae73471b3e7e3c6c71068b4aaa83990f9d
P. Christeas <xrg@openerp.com>
2011-06-23 12:04:57 +0300

    API docs: docstrings, module titles
    bzr revid: xrg@linux.gr-20110623090457-y5o7pd5y1ltpo9t9

--- openerp/addons/base/module/report/__init__.py
+++ openerp/addons/base/module/report/__init__.py
@@ -21,5 +21,15 @@
 
 import ir_module_reference_print
 
+#.apidoc title: RPC Services
+
+""" Classes of this module implement the network protocols that the
+    OpenERP server uses to communicate with remote clients.
+
+    Some classes are mostly utilities, whose API need not be visible to
+    the average user/developer. Study them only if you are about to
+    implement an extension to the network protocols, or need to debug some
+    low-level behavior of the wire.
+"""
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
