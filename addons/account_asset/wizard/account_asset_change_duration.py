d62d538ace73072852f676a3015eb577692e9650
Ujjvala Collins (OpenERP) <uco@tinyerp.com>
2011-06-24 18:31:31 +0530

    [IMP] account_asset: Improved+Fixed change duration wizard.
    bzr revid: uco@tinyerp.com-20110624130131-cwcnk09qy8bes4li

---
+++ addons/account_asset/wizard/account_asset_change_duration.py
@@ -0,0 +1,77 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+import time
+from osv import osv,fields
+
+class asset_modify(osv.osv_memory):
+    _name = 'asset.modify'
+    _description = 'Modify Asset'
+
+    _columns = {
+                'name': fields.char('Reason', size=64, required=True),
+                'method_delay': fields.float('Number of interval'),
+                'method_period': fields.float('Period per interval'),
+                'method_end': fields.date('Ending date'),
+                'note': fields.text('Notes'),
+    }
+    
+    def default_get(self, cr, uid, fields, context=None):
+        asset_obj = self.pool.get('account.asset.asset')
+        res = super(asset_modify, self).default_get(cr, uid, fields, context=context)
+        asset_id = context.get('active_id', False)
+        asset = asset_obj.browse(cr, uid, asset_id, context=context)
+        if 'name' in fields:
+            res.update({'name': asset.name})
+        if 'method_delay' in fields and asset.method_time == 'delay':
+            res.update({'method_delay': asset.method_delay})
+        if 'method_period' in fields:
+            res.update({'method_period': asset.method_period})
+        if 'method_end' in fields and asset.method_time == 'end':
+            res.update({'method_end': asset.method_end})
+        return res
+    
+    def modify(self, cr, uid, ids, context=None):
+        asset_obj = self.pool.get('account.asset.asset')
+        history_obj = self.pool.get('account.asset.history')
+        asset_id = context.get('active_id', False)
+        asset = asset_obj.browse(cr, uid, asset_id, context=context)
+        data = self.browse(cr, uid, ids[0], context=context)
+        history_obj.create(cr, uid, {
+            'asset_id': asset_id,
+            'name': asset.name,
+            'method_delay': asset.method_delay,
+            'method_period': asset.method_period,
+            'method_end': asset.method_end,
+            'user_id': uid,
+            'date': time.strftime('%Y-%m-%d'),
+            'note': data.note,
+        }, context=context)
+        asset_obj.write(cr, uid, [asset_id], {
+            'name': data.name,
+            'method_delay': data.method_delay,
+            'method_period': data.method_period,
+            'method_end': data.method_end,
+        }, context=context)
+        return {'type': 'ir.actions.act_window_close'}
+
+asset_modify()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
