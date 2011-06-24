a62aacaf7cc2d428f1b941a9546efeeb2ec807c9
Rucha (Open ERP) <rpa@tinyerp.com>
2011-06-24 19:08:00 +0530

    [IMP]: base_setup: Added context to open company wizard with data filled
    bzr revid: rpa@tinyerp.com-20110624133800-q3owydbtbwz54e7d

--- addons/base_setup/todo.py
+++ addons/base_setup/todo.py
@@ -18,10 +18,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from operator import itemgetter
 
-from osv import osv, fields
-import netsvc
+from osv import osv
 import tools
 
 class base_setup_company(osv.osv_memory):
@@ -29,131 +27,25 @@ class base_setup_company(osv.osv_memory):
     """
     _name = 'base.setup.company'
     _inherit = 'res.config'
-    logger = netsvc.Logger()
-
-    def _get_all(self, cr, uid, model, context=None):
-        models = self.pool.get(model)
-        all_model_ids = models.search(cr, uid, [])
-
-        output = [(False, '')]
-        output.extend(
-            sorted([(o.id, o.name)
-                    for o in models.browse(cr, uid, all_model_ids,
-                                           context=context)],
-                   key=itemgetter(1)))
-        return output
-
-    def _get_all_states(self, cr, uid, context=None):
-        return self._get_all(
-            cr, uid, 'res.country.state', context=context)
-    def _get_all_countries(self, cr, uid, context=None):
-        return self._get_all(cr, uid, 'res.country', context=context)
-
-    def _show_company_data(self, cr, uid, context=None):
-        # We only want to show the default company data in demo mode, otherwise users tend to forget
-        # to fill in the real company data in their production databases
-        return self.pool.get('ir.model.data').get_object(cr, uid, 'base', 'module_meta_information').demo
-
-
-    def default_get(self, cr, uid, fields_list=None, context=None):
-        """ get default company if any, and the various other fields
-        from the company's fields
-        """
-        defaults = super(base_setup_company, self)\
-              .default_get(cr, uid, fields_list=fields_list, context=context)
-        companies = self.pool.get('res.company')
-        company_id = companies.search(cr, uid, [], limit=1, order="id")
-        if not company_id or 'company_id' not in fields_list:
-            return defaults
-        company = companies.browse(cr, uid, company_id[0], context=context)
-        defaults['company_id'] = company.id
-
-        if not self._show_company_data(cr, uid, context=context):
-            return defaults
-
-        defaults['currency'] = company.currency_id.id
-        for field in ['name','rml_header1','rml_footer1','rml_footer2']:
-            defaults[field] = company[field]
-
-        if company.partner_id.address:
-            address = company.partner_id.address[0]
-            for field in ['street','street2','zip','city','email','phone']:
-                defaults[field] = address[field]
-            for field in ['country_id','state_id']:
-                if address[field]:
-                    defaults[field] = address[field].id
-
-        return defaults
-
-    _columns = {
-        'company_id':fields.many2one('res.company', 'Company'),
-        'name':fields.char('Company Name', size=64, required=True),
-        'street':fields.char('Street', size=128),
-        'street2':fields.char('Street 2', size=128),
-        'zip':fields.char('Zip Code', size=24),
-        'city':fields.char('City', size=128),
-        'state_id':fields.selection(_get_all_states, 'Fed. State'),
-        'country_id':fields.selection(_get_all_countries, 'Country'),
-        'email':fields.char('E-mail', size=64),
-        'phone':fields.char('Phone', size=64),
-        'currency':fields.many2one('res.currency', 'Currency', required=True),
-        'rml_header1':fields.char('Report Header', size=200,
-            help='''This sentence will appear at the top right corner of your reports.
-We suggest you to put a slogan here:
-"Open Source Business Solutions".'''),
-        'rml_footer1':fields.char('Report Footer 1', size=200,
-            help='''This sentence will appear at the bottom of your reports.
-We suggest you to write legal sentences here:
-Web: http://openerp.com - Fax: +32.81.73.35.01 - Fortis Bank: 126-2013269-07'''),
-        'rml_footer2':fields.char('Report Footer 2', size=200,
-            help='''This sentence will appear at the bottom of your reports.
-We suggest you to put bank information here:
-IBAN: BE74 1262 0121 6907 - SWIFT: CPDF BE71 - VAT: BE0477.472.701'''),
-        'account_no':fields.char('Bank Account No', size=64),
-        'website': fields.char('Company Website', size=64, help="Example: http://openerp.com"),
-    }
 
     def execute(self, cr, uid, ids, context=None):
-        assert len(ids) == 1, "We should only get one object from the form"
-        payload = self.browse(cr, uid, ids[0], context=context)
-        if not getattr(payload, 'company_id', None):
-            raise ValueError('Case where no default main company is setup '
-                             'not handled yet')
-        company = payload.company_id
-        company.write({
-            'name':payload.name,
-            'rml_header1':payload.rml_header1,
-            'rml_footer1':payload.rml_footer1,
-            'rml_footer2':payload.rml_footer2,
-            'currency_id':payload.currency.id,
-            'account_no':payload.account_no,
-        })
-
-        company.partner_id.write({
-            'name':payload.name,
-            'website':payload.website,
-        })
-
-        address_data = {
-            'name':payload.name,
-            'street':payload.street,
-            'street2':payload.street2,
-            'zip':payload.zip,
-            'city':payload.city,
-            'email':payload.email,
-            'phone':payload.phone,
-            'country_id':int(payload.country_id),
-            'state_id':int(payload.state_id),
+        ir_pool = self.pool.get('ir.model.data')
+        model, company_view_form = ir_pool.get_object_reference(cr, uid, 'base', 'view_company_form')
+        model, company_view_tree = ir_pool.get_object_reference(cr, uid, 'base', 'view_company_tree')
+        company_id = self.pool.get('res.users')._get_company(cr, uid, context=context)
+        return {
+            'name': _('Configure Company'),
+            'view_type': 'form',
+            'view_mode': 'form,tree',
+            'res_model': 'crm.lead',
+            'domain': [('id', '=', company_id)],
+            'res_id': company_id,
+            'view_id': False,
+            'views': [(company_view_form, 'form'),
+                      (company_view_tree, 'tree')],
+            'type': 'ir.actions.act_window'
         }
 
-        if company.partner_id.address:
-            company.partner_id.address[0].write(
-                address_data)
-        else:
-            self.pool.get('res.partner.address').create(cr, uid,
-                    dict(address_data,
-                         partner_id=int(company.partner_id)),
-                    context=context)
 base_setup_company()
 
 class res_currency(osv.osv):
