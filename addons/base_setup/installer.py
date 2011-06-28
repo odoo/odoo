d5f84bcceffd0708f9d2dc61fdebc802345e0ebb
Rucha (Open ERP) <rpa@tinyerp.com>
2011-06-28 15:25:16 +0530

    [REM]: base_setup: Moved name_get of res.currency at server side
    bzr revid: rpa@tinyerp.com-20110628095516-nb986uegxx0w6w5p

--- addons/base_setup/installer.py
+++ addons/base_setup/installer.py
@@ -320,22 +320,4 @@ class specify_partner_terminology(osv.osv_memory):
 specify_partner_terminology()
 
 
-# this code is needed to be moved in main currency class
-class res_currency(osv.osv):
-    _inherit = 'res.currency'
-
-    def name_get(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-#        We can use the following line,if we want to restrict this name_get for company setup only
-#        But, its better to show currencies as name(Code).
-        if not len(ids):
-            return []
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        reads = self.read(cr, uid, ids, ['name','symbol'], context, load='_classic_write')
-        return [(x['id'], tools.ustr(x['name']) + (x['symbol'] and (' (' + tools.ustr(x['symbol']) + ')') or '')) for x in reads]
-
-res_currency()
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
