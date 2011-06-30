048421ac28be009f2b18b6752cb98dc7c9a53baa
Ujjvala Collins (OpenERP) <uco@tinyerp.com>
2011-06-30 17:44:58 +0530

    [IMP] account_asset: Corrections according to python standards.
    bzr revid: uco@tinyerp.com-20110630121458-x3b5anmgxovhjz1x

--- addons/account_asset/report/account_asset_report.py
+++ addons/account_asset/report/account_asset_report.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 import tools
-from osv import fields,osv
+from osv import fields, osv
 
 class asset_asset_report(osv.osv):
     _name = "asset.asset.report"
@@ -28,20 +28,20 @@ class asset_asset_report(osv.osv):
     _auto = False
     _columns = {
         'name': fields.char('Year', size=16, required=False, readonly=True),
-        'month':fields.selection([('01', 'January'), ('02', 'February'), \
-                                  ('03', 'March'), ('04', 'April'),\
-                                  ('05', 'May'), ('06', 'June'), \
-                                  ('07', 'July'), ('08', 'August'),\
-                                  ('09', 'September'), ('10', 'October'),\
-                                  ('11', 'November'), ('12', 'December')], 'Month', readonly=True),
+        'month': fields.selection([('01', 'January'), ('02', 'February'), \
+                                   ('03', 'March'), ('04', 'April'),\
+                                   ('05', 'May'), ('06', 'June'),\
+                                   ('07', 'July'), ('08', 'August'),\
+                                   ('09', 'September'), ('10', 'October'),\
+                                   ('11', 'November'), ('12', 'December')], 'Month', readonly=True),
         'day': fields.char('Day', size=16, readonly=True),
-        'purchase_date': fields.date('Purchase Date', required=True),
+        'purchase_date': fields.date('Purchase Date', readonly=True),
         'depreciation_date': fields.date('Depreciation Date', readonly=True),
         'asset_id': fields.many2one('account.asset.asset', string='Asset', readonly=True),
         'asset_category_id': fields.many2one('account.asset.category',string='Asset category'),
         'partner_id': fields.many2one('res.partner', 'Partner', readonly=True),
-        'state': fields.selection([('draft','Draft'),('open','Running'),('close','Close')], 'State', required=True, readonly=True),
-        'remaining_value': fields.float('Amount of Depreciation Lines', required=True, readonly=True),
+        'state': fields.selection([('draft','Draft'),('open','Running'),('close','Close')], 'State', readonly=True),
+        'remaining_value': fields.float('Amount of Depreciation Lines', readonly=True),
         'move_check': fields.boolean('Posted', readonly=True),
         'nbr': fields.integer('# of Depreciation Lines', readonly=True),
         'gross_value': fields.float('Gross Value', readonly=True),
@@ -97,9 +97,7 @@ class asset_asset_report(osv.osv):
                 from account_asset_depreciation_line dl
                     left join account_asset_asset a on (dl.asset_id=a.id)
                 group by 
-                    dl.amount,dl.remaining_value,dl.depreciated_value,dl.asset_id,
-                    to_char(a.purchase_date, 'YYYY'),to_char(a.purchase_date, 'MM'),
-                    to_char(a.purchase_date, 'YYYY-MM-DD'),to_date(dl.depreciation_date, 'YYYY-MM-DD'),
+                    dl.amount,dl.asset_id,to_date(dl.depreciation_date, 'YYYY-MM-DD'),
                     a.purchase_date, dl.move_check, a.state, a.category_id, a.partner_id, a.company_id,
                     a.purchase_value, a.id, a.salvage_value
         )""")
