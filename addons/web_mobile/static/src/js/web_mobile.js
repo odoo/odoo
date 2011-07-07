7ba544da8612745f4c0c1e6380086371390657a0
Jiten Rangwala (OpenERP) <jra@tinyerp.com>
2011-07-07 18:31:57 +0530

    [IMP] Improved code for call formview.
    bzr revid: jra@tinyerp.com-20110707130157-26jqqigzwuehmwqq

--- addons/web_mobile/static/src/js/web_mobile.js
+++ addons/web_mobile/static/src/js/web_mobile.js
@@ -129,6 +129,20 @@ openerp.web_mobile.ListView = openerp.base.Controller.extend({
         $record = $(ev.currentTarget);
         var self = this;
         id = $record.data('id');
+        this.formview = new openerp.web_mobile.FormView(this.session, "oe_app", id, this.action);
+        this.formview.start();
+    }
+ });
+
+openerp.web_mobile.FormView = openerp.base.Controller.extend({
+    init: function(session, element_id, list_id, action) {
+        this._super(session, element_id);
+        this.list_id = list_id;
+        this.action = action;
+    },
+    start: function() {
+        var self = this;
+        id = this.list_id;
         model = this.action.res_model;
         view_id = this.action.views[1][0];
         this.dataset = new openerp.base.DataSetSearch(this.session, this.action.res_model, null, null);
@@ -141,7 +155,7 @@ openerp.web_mobile.ListView = openerp.base.Controller.extend({
             self.rpc("/base/formview/load", {"model": model, "view_id": view_id },
                 function(result){
                     var view_fields = result.fields_view.arch.children;
-                    get_fields = self.filter_fields(view_fields);
+                    get_fields = self.get_fields(view_fields);
                     for (var j = 0; j < view_fields.length; j++) {
                         if (view_fields[j].tag == 'notebook') {
                             var notebooks = view_fields[j];
@@ -152,19 +166,19 @@ openerp.web_mobile.ListView = openerp.base.Controller.extend({
                 });
         });
     },
-    filter_fields: function(view_fields, fields) {
+    get_fields: function(view_fields, fields) {
         this.fields = fields || [];
         for (var i=0; i < view_fields.length; i++){
             if (view_fields[i].tag == 'field') {
                 this.fields.push(view_fields[i]);
             }
             if (view_fields[i].tag == 'group') {
-                this.filter_fields(view_fields[i].children, this.fields);
+                this.get_fields(view_fields[i].children, this.fields);
             }
         }
         return this.fields;
     }
- });
+});
 
 openerp.web_mobile.Secondary =  openerp.base.Controller.extend({
     init: function(session, element_id, secondary_menu_id) {
