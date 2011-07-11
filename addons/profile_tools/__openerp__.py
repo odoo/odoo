76111033ef4ce1c0591a4e82121388027bad124d
Antony Lesuisse <al@openerp.com>
2011-07-11 13:45:12 +0200

    [FIX] more base_setup fix dependencies fixes runbot tests
    bzr revid: al@openerp.com-20110711114512-iheogdbs0og14w13

--- addons/profile_tools/__openerp__.py
+++ addons/profile_tools/__openerp__.py
@@ -23,7 +23,7 @@
 {
     "name" : "Miscellaneous Tools",
     "version" : "1.0",
-    "depends" : ["base"],
+    "depends" : ["base", "base_setup"],
     "author" : "OpenERP SA",
     "category" : "Tools",
     "description": """
