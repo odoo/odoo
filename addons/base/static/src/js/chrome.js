287ed124d5b55faab758b431eb061828ffc182ba
niv-openerp <nicolas.vanhoren@openerp.com>
2011-07-12 11:14:31 +0200

    [imp] some imp to previous imp
    bzr revid: nicolas.vanhoren@openerp.com-20110712091431-uf3o5z017c399izp

--- addons/base/static/src/js/chrome.js
+++ addons/base/static/src/js/chrome.js
@@ -922,9 +922,9 @@ openerp.base.Login =  openerp.base.Controller.extend({
             this.selected_login = localStorage.getItem('last_login_login_success');
         }
         if (jQuery.deparam(jQuery.param.querystring()).debug != undefined) {
-            this.selected_db = "trunk";
-            this.selected_login = "admin";
-            this.selected_password = "a";
+            this.selected_db = this.selected_db || "trunk";
+            this.selected_login = this.selected_login || "admin";
+            this.selected_password = this.selected_password || "a";
         }
     },
     start: function() {
