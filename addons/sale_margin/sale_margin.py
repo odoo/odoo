f6b3aa31e022494adb65793fb161a0af6bd69147
Amit Parik <>
2011-07-13 16:42:31 +0530

    [FIX] sale : Improved the product_id_change method in sale_margin.py
    lp bug: https://launchpad.net/bugs/809751 fixed
    
    bzr revid: bde@tinyerp.com-20110713111231-novysjd1bldn6bv3

--- addons/sale_margin/sale_margin.py
+++ addons/sale_margin/sale_margin.py
@@ -25,7 +25,7 @@ class sale_order_line(osv.osv):
 
     def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
             uom=False, qty_uos=0, uos=False, name='', partner_id=False,
-            lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False):
+            lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
         res = super(sale_order_line, self).product_id_change(cr, uid, ids, pricelist, product, qty=qty,
             uom=uom, qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
             lang=lang, update_tax=update_tax, date_order=date_order, packaging=packaging, fiscal_position=fiscal_position, flag=flag)
