211d3b6194eeb2b7b38f83e0bc954350da8dd42d
Bhumi Thakkar (Open ERP) <bth@tinyerp.com>
2011-07-27 16:56:17 +0530

    [FIX] Minor Changes For Selection Field.
    bzr revid: bth@tinyerp.com-20110727112617-rkr4zyu3kk3vvhcd

--- addons/base/static/src/js/tree.js
+++ addons/base/static/src/js/tree.js
@@ -64,13 +64,13 @@ openerp.base.TreeView = openerp.base.View.extend({
 
         self.dataset.domain = [['parent_id', '=', parseInt(id, 10)]];
         self.dataset.read_slice([], 0, false, function (response) {
-
             var is_padding, row_id, record_id;
             var curr_node = $('tr #treerow_' + id);
 
             if (curr_node.length == 1) {
                 curr_node.find('td').children(':first-child').attr('src','/base/static/src/img/collapse.gif');
-                curr_node.after(QWeb.render('TreeView_Secondry', {'child_data': response, 'flag': '0' }));
+                curr_node.after(QWeb.render('TreeView_Secondry', {'child_data': response, 'flag': '0', 'selection_fields' : self.fields.type.selection }));
+
 
                 for (var i = 0; i < response.length; i++) {
                     row_id = $('tr #treerow_' + response[i].id);
@@ -109,11 +109,12 @@ openerp.base.TreeView = openerp.base.View.extend({
                 if (!flag) {
                     self.$element.find('table').remove();
                 }
-                self.$element.append(QWeb.render('TreeView_Secondry', {'child_data': response, 'flag' : '1', 'fields' : self.fields_view.fields , 'fields_view' : self.fields_view.arch.children }));
+
+                self.$element.append(QWeb.render('TreeView_Secondry', {'child_data': response, 'flag' : '1', 'fields' : self.fields_view.fields , 'fields_view' : self.fields_view.arch.children, 'selection_fields' : self.fields.type.selection}));
                 self.$element.find('tr[id ^= treerow_]').each( function() {
                     $(this).find('td').children(':first-child').addClass("parent_top");
                     if (!($(this).find('td').children(':first-child').attr('id'))) {
-                        $(this).find('td').css('paddingLeft', '20px');
+                        $(this).find('td:first').css('paddingLeft', '20px');
                     }
                 });
             }
