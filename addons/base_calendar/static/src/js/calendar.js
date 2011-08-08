a0a0505c7e6dfe00c54ab75e14924306b7702bc0
Fabien Meghazi <fme@openerp.com>
2011-08-08 16:18:40 +0200

    [FIX] Fixed event deletion in calendar view
    bzr revid: fme@openerp.com-20110808141840-8gxx1ssqd5e0l8y1

--- addons/base_calendar/static/src/js/calendar.js
+++ addons/base_calendar/static/src/js/calendar.js
@@ -276,10 +276,10 @@ openerp.base_calendar.CalendarView = openerp.base.View.extend({
     do_delete_event: function(event_id, event_obj) {
         // dhtmlx sends this event even when it does not exist in openerp.
         // Eg: use cancel in dhtmlx new event dialog
-        if (_.indexOf(this.dataset.ids, event_id) > -1) {
-            this.dataset.unlink(parseInt(event_id, 10, function() {
+        if (_.indexOf(this.dataset.ids, parseInt(event_id, 10)) > -1) {
+            this.dataset.unlink(parseInt(event_id, 10), function() {
                 self.refresh_minical();
-            }));
+            });
         }
     },
     get_event_data: function(event_obj) {
