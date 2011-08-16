6bf354e880a63c5f1ddfd4c45f585778dc286304
niv-openerp <nicolas.vanhoren@openerp.com>
2011-08-16 15:48:51 +0200

    [imp] added translations for all modules
    bzr revid: nicolas.vanhoren@openerp.com-20110816134851-nlkinf7mjrh8m07z

--- gen_translations.sh
+++ gen_translations.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-usage()
-{
+usage() {
 cat << EOF
 usage: $0 -a
 usage: $0 DIR OUTPUT_FILE
@@ -32,7 +31,20 @@ shift $((OPTIND-1))
 if [ -n "$do_all" ]
 then
     echo "Extracting all the translations"
-    $0  addons/base/static/src/ addons/base/po/base.pot    
+    executable=$0
+    extract_module() {
+       $executable addons/$1 addons/$1/po/$1.pot 
+    }
+    extract_module base
+    extract_module base_calendar
+    extract_module base_dashboard
+    extract_module base_default_home
+    extract_module base_diagram
+    extract_module base_gantt
+    extract_module base_graph
+    extract_module base_hello
+    extract_module web_chat
+    extract_module web_mobile
 elif [ -n "$2" ]
 then
     ./npybabel.py extract -F babel.cfg -o $2 -k _t --no-default-keywords $1
