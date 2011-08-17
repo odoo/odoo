73742c80a39fb820e9a87c4eb7fa31a1b042fd7a
ksa (Open ERP) <ksa@tinyerp.co.in>
2011-08-17 14:24:04 +0530

    [IMP]: remove stage type for crm fundraising
    bzr revid: ksa@tinyerp.co.in-20110817085404-uaezg715cn4e5310

--- addons/crm_fundraising/crm_fundraising.py
+++ addons/crm_fundraising/crm_fundraising.py
@@ -61,7 +61,7 @@ class crm_fundraising(crm.crm_case, osv.osv):
             'partner_name2': fields.char('Employee Email', size=64), 
             'partner_phone': fields.char('Phone', size=32), 
             'partner_mobile': fields.char('Mobile', size=32), 
-            'stage_id': fields.many2one ('crm.case.stage', 'Stage', domain="[('type', '=', 'fundraising')]"), 
+            'stage_id': fields.many2one ('crm.case.stage', 'Stage', domain="[('section_ids', '=', section_id)]"), 
             'type_id': fields.many2one('crm.case.resource.type', 'Campaign', \
                              domain="[('section_id','=',section_id)]"), 
             'duration': fields.float('Duration'), 
@@ -95,18 +95,3 @@ class crm_fundraising(crm.crm_case, osv.osv):
 
 crm_fundraising()
 
-
-class crm_stage_fundraising(osv.osv):
-    
-    def _get_type_value(self, cr, user, context):
-        list = super(crm_stage_fundraising, self)._get_type_value(cr, user, context)
-        list.append(('fundraising','Fundraising'))
-        return list
-    
-    _inherit = "crm.case.stage"
-    _columns = {
-            'type': fields.selection(_get_type_value, 'Type'),
-    }
-   
-    
-crm_stage_fundraising()
