93fb2b4a6168195401462f75a64b00f5c1083009
Stephane Wirtel <stw@openerp.com>
2011-08-18 13:17:36 +0200

    [RELEASE] OpenERP 6.0.3
    bzr revid: stw@openerp.com-20110818111736-o3my3or5dqjci7yt

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 name = 'openerp-server'
-version = '6.0.2'
+version = '6.0.3'
 major_version = '6.0'
 description = 'OpenERP Server'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
