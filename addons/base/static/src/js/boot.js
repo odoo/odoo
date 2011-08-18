ed5097650b236bbd7bb4b96690258a9dbfb9d2c9
Antony Lesuisse <al@openerp.com>
2011-08-18 21:53:49 +0200

    [IMP] more production mode move Session above session aware, jquery ui css is not absolute
    bzr revid: al@openerp.com-20110818195349-zebyew12sefo93ml

--- addons/base/static/src/js/boot.js
+++ addons/base/static/src/js/boot.js
@@ -14,8 +14,6 @@
 
     /** @lends openerp */
     var openerp = this.openerp =  {
-        // debug flag
-        debug: true,
         // Per session namespace
         // openerp.<module> will map to
         // openerp.sessions.sessionname.<module> using a closure
@@ -57,32 +55,11 @@ openerp.base = function(instance) {
     openerp.base.formats(instance);
     openerp.base.chrome(instance);
     openerp.base.data(instance);
-    if (openerp.base.views) {
-        openerp.base.views(instance);
-    }
-    if (openerp.base.search) {
-        openerp.base.search(instance);
-    }
-    if (openerp.base.list) {
-        openerp.base.list(instance);
-    }
-    if (openerp.base. m2o) {
-        openerp.base.m2o(instance);
-    }
-    if (openerp.base.form) {
-        openerp.base.form(instance);
-    }
-    if (openerp.base.list && openerp.base.list.editable) {
-        openerp.base.list.editable(instance);
-    }
-    if (openerp.web_mobile) {
-        openerp.web_mobile(instance);
-    }
-    if (openerp.base.view_tree) {
-        openerp.base.view_tree(instance);
-    }
-    if (openerp.base.data_export) {
-        openerp.base.data_export(instance);
+    files = ["views","search","list","form","list_editable","web_mobile","view_tree","data_export"];
+    for(i=0; i<files.length; i++) {
+        if(openerp.base[files[i]]) {
+            openerp.base[files[i]](instance);
+        }
     }
 };
 
