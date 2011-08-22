cd60aa30e52a3b303b24b8ffa5c68159b579f2a1
niv-openerp <nicolas.vanhoren@openerp.com>
2011-08-22 16:39:24 +0200

    [imp] now loads correct datejs localization file, also added labjs to make fme happy
    bzr revid: nicolas.vanhoren@openerp.com-20110822143924-7bl4m4dac26g0g5v

---
+++ addons/base/static/lib/datejs/globalization/kk-KZ.js
@@ -0,0 +1,195 @@
+Date.CultureInfo = {
+	/* Culture Name */
+    name: "kk-KZ",
+    englishName: "Kazakh (Kazakhstan)",
+    nativeName: "Қазақ (Қазақстан)",
+    
+    /* Day Name Strings */
+    dayNames: ["Жексенбі", "Дүйсенбі", "Сейсенбі", "Сәрсенбі", "Бейсенбі", "Жұма", "Сенбі"],
+    abbreviatedDayNames: ["Жк", "Дс", "Сс", "Ср", "Бс", "Жм", "Сн"],
+    shortestDayNames: ["Жк", "Дс", "Сс", "Ср", "Бс", "Жм", "Сн"],
+    firstLetterDayNames: ["Ж", "Д", "С", "С", "Б", "Ж", "С"],
+    
+    /* Month Name Strings */
+    monthNames: ["қаңтар", "ақпан", "наурыз", "сәуір", "мамыр", "маусым", "шілде", "тамыз", "қыркүйек", "қазан", "қараша", "желтоқсан"],
+    abbreviatedMonthNames: ["Қаң", "Ақп", "Нау", "Сәу", "Мам", "Мау", "Шіл", "Там", "Қыр", "Қаз", "Қар", "Жел"],
+
+	/* AM/PM Designators */
+    amDesignator: "",
+    pmDesignator: "",
+
+    firstDayOfWeek: 1,
+    twoDigitYearMax: 2029,
+    
+    /**
+     * The dateElementOrder is based on the order of the 
+     * format specifiers in the formatPatterns.DatePattern. 
+     *
+     * Example:
+     <pre>
+     shortDatePattern    dateElementOrder
+     ------------------  ---------------- 
+     "M/d/yyyy"          "mdy"
+     "dd/MM/yyyy"        "dmy"
+     "yyyy-MM-dd"        "ymd"
+     </pre>
+     *
+     * The correct dateElementOrder is required by the parser to
+     * determine the expected order of the date elements in the
+     * string being parsed.
+     */
+    dateElementOrder: "dmy",
+    
+    /* Standard date and time format patterns */
+    formatPatterns: {
+        shortDate: "dd.MM.yyyy",
+        longDate: "d MMMM yyyy 'ж.'",
+        shortTime: "H:mm",
+        longTime: "H:mm:ss",
+        fullDateTime: "d MMMM yyyy 'ж.' H:mm:ss",
+        sortableDateTime: "yyyy-MM-ddTHH:mm:ss",
+        universalSortableDateTime: "yyyy-MM-dd HH:mm:ssZ",
+        rfc1123: "ddd, dd MMM yyyy HH:mm:ss GMT",
+        monthDay: "d MMMM",
+        yearMonth: "MMMM yyyy"
+    },
+
+    /**
+     * NOTE: If a string format is not parsing correctly, but
+     * you would expect it parse, the problem likely lies below. 
+     * 
+     * The following regex patterns control most of the string matching
+     * within the parser.
+     * 
+     * The Month name and Day name patterns were automatically generated
+     * and in general should be (mostly) correct. 
+     *
+     * Beyond the month and day name patterns are natural language strings.
+     * Example: "next", "today", "months"
+     *
+     * These natural language string may NOT be correct for this culture. 
+     * If they are not correct, please translate and edit this file
+     * providing the correct regular expression pattern. 
+     *
+     * If you modify this file, please post your revised CultureInfo file
+     * to the Datejs Forum located at http://www.datejs.com/forums/.
+     *
+     * Please mark the subject of the post with [CultureInfo]. Example:
+     *    Subject: [CultureInfo] Translated "da-DK" Danish(Denmark)
+     * 
+     * We will add the modified patterns to the master source files.
+     *
+     * As well, please review the list of "Future Strings" section below. 
+     */	
+    regexPatterns: {
+        jan: /^қаң(тар)?/i,
+        feb: /^ақп(ан)?/i,
+        mar: /^нау(рыз)?/i,
+        apr: /^сәу(ір)?/i,
+        may: /^мам(ыр)?/i,
+        jun: /^мау(сым)?/i,
+        jul: /^шіл(де)?/i,
+        aug: /^там(ыз)?/i,
+        sep: /^қыр(күйек)?/i,
+        oct: /^қаз(ан)?/i,
+        nov: /^қар(аша)?/i,
+        dec: /^жел(тоқсан)?/i,
+
+        sun: /^жексенбі/i,
+        mon: /^дүйсенбі/i,
+        tue: /^сейсенбі/i,
+        wed: /^сәрсенбі/i,
+        thu: /^бейсенбі/i,
+        fri: /^жұма/i,
+        sat: /^сенбі/i,
+
+        future: /^next/i,
+        past: /^last|past|prev(ious)?/i,
+        add: /^(\+|aft(er)?|from|hence)/i,
+        subtract: /^(\-|bef(ore)?|ago)/i,
+        
+        yesterday: /^yes(terday)?/i,
+        today: /^t(od(ay)?)?/i,
+        tomorrow: /^tom(orrow)?/i,
+        now: /^n(ow)?/i,
+        
+        millisecond: /^ms|milli(second)?s?/i,
+        second: /^sec(ond)?s?/i,
+        minute: /^mn|min(ute)?s?/i,
+		hour: /^h(our)?s?/i,
+		week: /^w(eek)?s?/i,
+        month: /^m(onth)?s?/i,
+        day: /^d(ay)?s?/i,
+        year: /^y(ear)?s?/i,
+		
+        shortMeridian: /^(a|p)/i,
+        longMeridian: /^(a\.?m?\.?|p\.?m?\.?)/i,
+        timezone: /^((e(s|d)t|c(s|d)t|m(s|d)t|p(s|d)t)|((gmt)?\s*(\+|\-)\s*\d\d\d\d?)|gmt|utc)/i,
+        ordinalSuffix: /^\s*(st|nd|rd|th)/i,
+        timeContext: /^\s*(\:|a(?!u|p)|p)/i
+    },
+
+	timezones: [{name:"UTC", offset:"-000"}, {name:"GMT", offset:"-000"}, {name:"EST", offset:"-0500"}, {name:"EDT", offset:"-0400"}, {name:"CST", offset:"-0600"}, {name:"CDT", offset:"-0500"}, {name:"MST", offset:"-0700"}, {name:"MDT", offset:"-0600"}, {name:"PST", offset:"-0800"}, {name:"PDT", offset:"-0700"}]
+};
+
+/********************
+ ** Future Strings **
+ ********************
+ * 
+ * The following list of strings may not be currently being used, but 
+ * may be incorporated into the Datejs library later. 
+ *
+ * We would appreciate any help translating the strings below.
+ * 
+ * If you modify this file, please post your revised CultureInfo file
+ * to the Datejs Forum located at http://www.datejs.com/forums/.
+ *
+ * Please mark the subject of the post with [CultureInfo]. Example:
+ *    Subject: [CultureInfo] Translated "da-DK" Danish(Denmark)b
+ *
+ * English Name        Translated
+ * ------------------  -----------------
+ * about               about
+ * ago                 ago
+ * date                date
+ * time                time
+ * calendar            calendar
+ * show                show
+ * hourly              hourly
+ * daily               daily
+ * weekly              weekly
+ * bi-weekly           bi-weekly
+ * fortnight           fortnight
+ * monthly             monthly
+ * bi-monthly          bi-monthly
+ * quarter             quarter
+ * quarterly           quarterly
+ * yearly              yearly
+ * annual              annual
+ * annually            annually
+ * annum               annum
+ * again               again
+ * between             between
+ * after               after
+ * from now            from now
+ * repeat              repeat
+ * times               times
+ * per                 per
+ * min (abbrev minute) min
+ * morning             morning
+ * noon                noon
+ * night               night
+ * midnight            midnight
+ * mid-night           mid-night
+ * evening             evening
+ * final               final
+ * future              future
+ * spring              spring
+ * summer              summer
+ * fall                fall
+ * winter              winter
+ * end of              end of
+ * end                 end
+ * long                long
+ * short               short
+ */
