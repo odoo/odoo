f48a5ab9997c6ff8028d4d828d9f76c18437f422
sma (Tiny) <sma@tinyerp.com>
2011-08-23 16:10:54 +0530

    [IMP] Add listview action.
    bzr revid: sma@tinyerp.com-20110823104054-vvme3sgbsoepcxxw

--- addons/base/static/src/js/view_editor.js
+++ addons/base/static/src/js/view_editor.js
@@ -13,7 +13,46 @@ openerp.base.ViewEditor = openerp.base.Dialog.extend({
     start: function() {
 
         var self = this;
+        var action = {
+            name:'ViewEditor',
+            context:this.session.user_context,
+//            domain: ["model", "=", this.dataset.model],
+            res_model: 'ir.ui.view',
+            views : [[false, 'list']],
+            type: 'ir.actions.act_window',
+            target: "current",
+            limit : 80,
+            auto_search : true,
+            flags: {
+                sidebar: false,
+                views_switcher: false,
+                action_buttons:false,
+                search_view:false,
+                pager:false,
+            },
+        };
+        var action_manager = new openerp.base.ActionManager(this);
+        this.dialog = new openerp.base.Dialog(this,{
+            modal: true,
+            title: 'ViewEditor',
+            width: 600,
+            height: 500,
+            buttons: {
 
+                Cancel: function(){
+                     $(this).dialog('destroy');
+                },
+                Edit: function(){
+                },
+                Create: function(){
+                }
+            },
+        });
+       this.dialog.start();
+       this.dialog.open();
+       action_manager.appendTo(this.dialog);
+       action_manager.render(this.dialog);
+       action_manager.do_action(action);
     },
 });
 };
