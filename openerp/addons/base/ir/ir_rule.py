93f9a0b0cd51595e8706f4c5faec87ee487e5070
Antony Lesuisse <al@openerp.com>
2011-08-24 18:19:18 +0200

    [IMP] ir_rule distribute clear_cache
    bzr revid: al@openerp.com-20110824161918-dkexqhuc1mzpx9fs

--- openerp/addons/base/ir/ir_rule.py
+++ openerp/addons/base/ir/ir_rule.py
@@ -138,9 +138,6 @@ class ir_rule(osv.osv):
             return domain
         return []
 
-    def clear_cache(self, cr, uid):
-        self._compute_domain.clear_cache(self)
-
     def domain_get(self, cr, uid, model_name, mode='read', context=None):
         dom = self._compute_domain(cr, uid, model_name, mode)
         if dom:
@@ -154,17 +151,17 @@ class ir_rule(osv.osv):
 
     def unlink(self, cr, uid, ids, context=None):
         res = super(ir_rule, self).unlink(cr, uid, ids, context=context)
-        self.clear_cache()
+        self._compute_domain.clear_cache(self)
         return res
 
     def create(self, cr, uid, vals, context=None):
         res = super(ir_rule, self).create(cr, uid, vals, context=context)
-        self.clear_cache(cr, uid)
+        self._compute_domain.clear_cache(self)
         return res
 
     def write(self, cr, uid, ids, vals, context=None):
         res = super(ir_rule, self).write(cr, uid, ids, vals, context=context)
-        self.clear_cache(cr,uid)
+        self._compute_domain.clear_cache(self)
         return res
 
 ir_rule()
