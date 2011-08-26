8a6ab52dfb75d6841317090beba37c64a38f5879
DBR (OpenERP) <dbr@tinyerp.com>
2011-08-26 17:47:57 +0530

    [ADD]project_scrum : Add file for scrum sprit test cases
    bzr revid: dbr@tinyerp.com-20110826121757-71ku92r0g5jnzipe

--- addons/project_scrum/__openerp__.py
+++ addons/project_scrum/__openerp__.py
@@ -65,7 +65,10 @@ More information on the methodology:
         "board_project_scrum_view.xml",
     ],
     'demo_xml': ['project_scrum_demo.xml'],
-    'test': ['test/project_scrum_report.yml'],
+    'test': [
+            'test/project_scrum_report.yml',
+            'test/scrum_sprint_test.yml'
+            ],
     'installable': True,
     'active': False,
     'certificate' : '00736750152003010781',
