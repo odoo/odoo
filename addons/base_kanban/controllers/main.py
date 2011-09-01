2ac291cd8eab006e3794906a3e85ba0f53e050b7
Fabien Meghazi <fme@openerp.com>
2011-09-01 17:19:15 +0200

    [FIX] openerpweb module has been renamed
    bzr revid: fme@openerp.com-20110901151915-u1wefykkoprzq9kv

--- addons/base_kanban/controllers/main.py
+++ addons/base_kanban/controllers/main.py
@@ -1,5 +1,6 @@
+import base.common as openerpweb
+
 from base.controllers.main import View
-import openerpweb
 
 class KanbanView(View):
     _cp_path = "/base_kanban/kanbanview"
@@ -7,5 +8,4 @@ class KanbanView(View):
     @openerpweb.jsonrequest
     def load(self, req, model, view_id):
         fields_view = self.fields_view_get(req, model, view_id, 'kanban')
-        all_fields = req.session.model(model).fields_get()
-        return {'fields_view': fields_view, 'all_fields':all_fields}
+        return {'fields_view': fields_view}
