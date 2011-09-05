6fe22fe364ae38c8b9a99b6204ccb79ef334c9fe
niv-openerp <nicolas.vanhoren@openerp.com>
2011-09-05 14:42:14 +0200

    [imp] externalized release parameters
    bzr revid: nicolas.vanhoren@openerp.com-20110905124214-byhgf4ogglm92ryj

--- setup.py
+++ setup.py
@@ -4,16 +4,7 @@ import re
 import sys
 from setuptools import setup
 
-name = 'openerp-web-proto'
-version = '6.0.1'
-description = "Web Client of OpenERP, the Enterprise Management Software"
-long_description = "OpenERP Web is the web client of the OpenERP, a free enterprise management software"
-author = "OpenERP S.A."
-author_email = "info@openerp.com"
-support_email = 'support@openerp.com'
-url = "http://www.openerp.com/"
-download_url = ''
-license = "OEPL"
+execfile('webrelease.py')
 
 version_dash_incompatible = False
 if 'bdist_rpm' in sys.argv:
