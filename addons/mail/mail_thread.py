0b6e8fba7b0caaaaf2e2e5ba5540c06c45a268a3
Olivier Dony <odo@openerp.com>
2011-09-07 18:28:19 +0200

    [IMP] mail.thread: check for empty arg in message_partner_by_email()
    bzr revid: odo@openerp.com-20110907162819-71ib7va3h10mgqk2

--- addons/mail/mail_thread.py
+++ addons/mail/mail_thread.py
@@ -445,12 +445,13 @@ class mail_thread(osv.osv):
             'partner_address_id': False,
             'partner_id': False
         }
-        email = to_email(email)[0]
-        address_ids = address_pool.search(cr, uid, [('email', '=', email)])
-        if address_ids:
-            address = address_pool.browse(cr, uid, address_ids[0])
-            res['partner_address_id'] = address_ids[0]
-            res['partner_id'] = address.partner_id.id
+        if email:
+            email = to_email(email)[0]
+            address_ids = address_pool.search(cr, uid, [('email', '=', email)])
+            if address_ids:
+                address = address_pool.browse(cr, uid, address_ids[0])
+                res['partner_address_id'] = address_ids[0]
+                res['partner_id'] = address.partner_id.id
         return res
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
