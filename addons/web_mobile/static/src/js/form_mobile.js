7c08fb6324b8411651ebd5b651c9ba737878f4ac
Bhumi Thakkar (Open ERP) <bth@tinyerp.com>
2011-09-14 12:37:30 +0530

    [IMP]Theme for Formview and header-footer.
    bzr revid: bth@tinyerp.com-20110914070730-1ikmiwu4m9aarklz

--- addons/web_mobile/static/src/js/form_mobile.js
+++ addons/web_mobile/static/src/js/form_mobile.js
@@ -41,11 +41,32 @@ openerp.web_mobile.FormView = openerp.web.Widget.extend({
                 }
                 self.$element.html(QWeb.render("FormView", {'get_fields': get_fields, 'notebooks': notebooks || false, 'fields' : fields, 'values' : data }));
 
-                    self.$element.find("#header").find('h1').html(self.action.name);
+                    self.$element.find("[data-role=header]").find('h1').html(self.action.name);
+                    self.$element.find("[data-role=header]").find('#home').click(function(){
+                        $.mobile.changePage($("#oe_menu"), "slide", true, true);
+                    });
+                    self.$element.find("[data-role=footer]").find('#shrotcuts').click(function(){
+                        if(!$('#oe_shortcuts').html().length){
+                            this.shortcuts = new openerp.web_mobile.Shortcuts(self, "oe_shortcuts");
+                            this.shortcuts.start();
+                        }
+                        else{
+                            $.mobile.changePage($("#oe_shortcuts"), "slide", true, true);
+                        }
+                    });
+                    self.$element.find("[data-role=footer]").find('#preference').click(function(){
+                        if(!$('#oe_options').html().length){
+                            this.options = new openerp.web_mobile.Options(self, "oe_options");
+                            this.options.start();
+                        }
+                        else{
+                            $.mobile.changePage($("#oe_options"), "slide", true, true);
+                        }
+                    });
                     self.$element.find('select').change(function(ev){
                         selection.on_select_option(ev);
                     });
-                    self.$element.find('a').click(function(){
+                    /*self.$element.find('a').click(function(){
                         for (var k = 0; k < notebooks.children.length; k++) {
                             var text = $(this).find('.ui-btn-text').text();
                             var next = $(this).next();
@@ -84,7 +105,7 @@ openerp.web_mobile.FormView = openerp.web.Widget.extend({
                                     }else{
                                         next.find('.detail').append(QWeb.render("FormView", {'get_fields': get_fields,'fields' : result.fields, 'values' : data }));
                                     }
-                                }
+                                } */
 
                                 //$.mobile.changePage($("#oe_form"), "slide", true, true);
                                 /*next.find('.detail').find('li').click(function(){
@@ -115,14 +136,14 @@ openerp.web_mobile.FormView = openerp.web.Widget.extend({
                                         });
                                     }
                                 });*/
-                            }
+                            /*}
                         }
                         self.$element.find('select').change(function(ev){
                             selection.on_select_option(ev);
                         });
-                    });
+                    });*/
+                    $.mobile.changePage($("#oe_form"), "slide", true, true);
                 });
-                $.mobile.changePage($("#oe_form"), "slide", true, true);
                 //$("#oe_header").find("h1").html(result.arch.attrs.string);
         });
     },
