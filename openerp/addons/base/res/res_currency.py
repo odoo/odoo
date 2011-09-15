73f8c52ad2fb05177dffeddc7db2a0154cfa9f31
Stephane Wirtel <stw@openerp.com>
2011-09-15 16:26:38 +0200

    [FIX] base: res_currency#company_id remove the default value based on the currency of the company
    bzr revid: stw@openerp.com-20110915142638-gmpk24otjtrv0jtz

--- openerp/addons/base/res/res_currency.py
+++ openerp/addons/base/res/res_currency.py
@@ -67,7 +67,6 @@ class res_currency(osv.osv):
     }
     _defaults = {
         'active': lambda *a: 1,
-        'company_id': lambda self,cr,uid,c: self.pool.get('res.company')._company_default_get(cr, uid, 'res.currency', context=c)
     }
     _order = "name"
 
