bd03e6aabfe178476894912df1025a17253c6ddc
RavishchanraMurari (Open ERP) <rmu@tinyerp.com>
2011-09-15 18:06:42 +0530

    project_issue_kanban
    bzr revid: rmu@tinyerp.com-20110915123642-hiq0sfcunrciytk3

--- addons/project_issue/project_issue.py
+++ addons/project_issue/project_issue.py
@@ -227,6 +227,7 @@ class project_issue(crm.crm_case, osv.osv):
                                 multi='compute_day', type="float", store=True),
         'inactivity_days': fields.function(_compute_day, string='Days since last action', \
                                 multi='compute_day', type="integer", help="Difference in days between last action and current date"),
+         'color': fields.integer('Color Index'),
         'message_ids': fields.one2many('mail.message', 'res_id', 'Messages', domain=[('model','=',_name)]),
         'date_action_last': fields.datetime('Last Action', readonly=1),
         'date_action_next': fields.datetime('Next Action', readonly=1),
@@ -268,7 +269,22 @@ class project_issue(crm.crm_case, osv.osv):
         'project_id':_get_project,
         'categ_id' : lambda *a: False,
         #'assigned_to' : lambda obj, cr, uid, context: uid,
-    }
+         }
+
+    def set_priority(self, cr, uid, ids, priority):
+        """Set lead priority
+        """
+        return self.write(cr, uid, ids, {'priority' : priority})
+
+    def set_high_priority(self, cr, uid, ids, *args):
+        """Set lead priority to high
+        """
+        return self.set_priority(cr, uid, ids, '1')
+
+    def set_normal_priority(self, cr, uid, ids, *args):
+        """Set lead priority to normal
+        """
+        return self.set_priority(cr, uid, ids, '3')
 
     def convert_issue_task(self, cr, uid, ids, context=None):
         case_obj = self.pool.get('project.issue')
