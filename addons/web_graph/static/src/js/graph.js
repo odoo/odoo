9adf06ef6f96acb7bac5ca3c3582c41b666d69ad
Khushbu <kmo@tinyerp.com>
2011-09-23 14:25:57 +0530

    [FIX] Used views own element id.
    bzr revid: kmo@tinyerp.com-20110923085557-t6alqpum8aa5ic4i

--- addons/web_graph/static/src/js/graph.js
+++ addons/web_graph/static/src/js/graph.js
@@ -49,8 +49,7 @@ openerp.web_graph.GraphView = openerp.web.View.extend({
         this.operator = [];
         this.group_field = [];
         this.orientation = this.fields_view.arch.attrs.orientation || '';
-        this.elem_id = this.view_manager.element_id;
-
+        this.elem_id = this.element_id;
         _.each(this.fields_view.arch.children, function (field) {
             if (field.attrs.operator) {
                 this.operator.push(field.attrs.name);
