72d5ab8b9954f77e71e8db6b1d07965e1617bbbf
Olivier Dony <odo@openerp.com>
2011-10-05 13:38:46 +0200

    [IMP] email_template: added opt_out flag on res.partner
    In the context of email templates or for related features
    like marketing campaigns or automated business document
    notification, this flag may be used to disable the sending
    of template-based emails to the partner.
    It will not prevent sending an email based on a template
    by default, each feature using a template should implement
    this if needed.
    
    bzr revid: odo@openerp.com-20111005113846-qik6d5gxd4xqeqdj

--- addons/email_template/__init__.py
+++ addons/email_template/__init__.py
@@ -21,5 +21,6 @@
 ##############################################################################
 import email_template
 import wizard
+import res_partner
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
