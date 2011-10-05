367706427455a68b17dff1980aefc7666db38127
Christophe Simonis <chs@openerp.com>
2011-10-05 17:35:39 +0200

    [IMP] notification: warnings does not expire
    bzr revid: chs@openerp.com-20111005153539-xxds5bog4p08m2yl

--- addons/web/static/src/js/chrome.js
+++ addons/web/static/src/js/chrome.js
@@ -28,7 +28,9 @@ openerp.web.Notification =  openerp.web.Widget.extend(/** @lends openerp.web.Not
     warn: function(title, text) {
         this.$element.notify('create', 'oe_notification_alert', {
             title: title,
-            text: text
+            text: text,
+        }, {
+            expires: false,
         });
     }
 });
