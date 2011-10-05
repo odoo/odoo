770af0d0c21b2bf537ffb0f988f3004989187b9a
Olivier Dony <odo@openerp.com>
2011-10-05 03:09:36 +0200

    [IMP] account: updated to latest edi web addon changes (needs review)
    bzr revid: odo@openerp.com-20111005010936-te7dn7ced0jceh19

--- addons/account/static/src/js/edi_invoice.js
+++ addons/account/static/src/js/edi_invoice.js
@@ -1,8 +1,13 @@
 /*---------------------------------------------------------
- * OpenERP Web_edi
+ * EDI Web Interface for Invoice
  *---------------------------------------------------------*/
 //
-openerp.edi.InvoiceEdiView = Class.extend({
+
+openerp.web.edi_invoice = function(openerp) {
+var QWeb = new QWeb2.Engine();
+
+openerp.web.edi_views.add('view_center_account_invoice' , 'openerp.web.EdiViewCenterInvoice');
+openerp.web.EdiViewCenterInvoice = openerp.web.Class.extend({
     init: function(element, edi){
         var self = this;
         this.edi_document = eval(edi.document)
@@ -30,21 +35,21 @@ openerp.edi.InvoiceEdiView = Class.extend({
         var current_url = $(location).attr('href')
         var pathName = current_url.substring(0, current_url.lastIndexOf('/') +1);
 
-        if ($element.find('#oe_edi_invoice_rd_pay_paypal').attr('checked') == true)
+        if ($element.find('#oe_edi_invoice_rd_pay_paypal').attr('checked') == 'checked')
         {
             alert('Pay Invoice using Paypal service');
             
         }
-        if ($element.find('#oe_edi_invoice_rd_pay_google_checkout').attr('checked') == true)
+        if ($element.find('#oe_edi_invoice_rd_pay_google_checkout').attr('checked') == 'checked')
         {
             alert('Pay Invoice using Google Checkout');
         }
-        if ($element.find('#oe_edi_invoice_rd_pay_bank').attr('checked') == true)
+        if ($element.find('#oe_edi_invoice_rd_pay_bank').attr('checked') == 'checked')
         {
             alert('Pay Invoice using Bankwire Trasnfer')
         }
     }
 });
 
-openerp.edi.forms.add('edi_center_account_invoice' , 'openerp.edi.InvoiceEdiView');
+}
 // vim:et fdc=0 fdl=0 foldnestmax=3 fdm=syntax:
