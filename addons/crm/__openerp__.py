da3aca4263643ca170dd395b81827a2e96dd4fd3
ron@tinyerp.com <>
2011-10-11 18:30:50 +0530

    [ADD, REF]: crm: 1) Moved test_crm_phonecall.yml to process/process_crm_phonecall.yml 2) Removed Phonecall by log seduling, 3) Improved varibles names and conversion, 4) changed string Interview call to interest with openerp, 5) Improved all string regarding to end user and remove all string like coder, 6) Added stage after completion of phonecall and cheked that working properly or not
    bzr revid: ron@tinyerp.com-20111011130050-vk24chpik6y4ijc4

--- addons/crm/__openerp__.py
+++ addons/crm/__openerp__.py
@@ -123,7 +123,7 @@ Creates a dashboard for CRM that includes:
             'test/process/cancle_crm_lead.yml',
             'test/process/process_crm_meeting.yml',
             'test/test_crm_opportunity.yml',
-            'test/test_crm_phonecall.yml',
+            'test/process/process_crm_phonecall.yml',
             'test/test_crm_recurrent_meeting.yml',
             'test/test_crm_stage_changes.yml',
             'test/test_crm_recurrent_meeting_case2.yml',
