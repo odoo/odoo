6b19d9baa1d501d18a4dd1550ff50d88d6354faa
ron@tinyerp.com <>
2011-10-12 16:22:21 +0530

    [IMP] crm: 1) Improvement in openerp for correct path of  crm_meeting.yml
    bzr revid: ron@tinyerp.com-20111012105221-mxlfnzeg16s93d7u

--- addons/crm/__openerp__.py
+++ addons/crm/__openerp__.py
@@ -122,7 +122,7 @@ Creates a dashboard for CRM that includes:
             'test/process/crm_lead_open.yml',
             'test/process/crm_lead_to_opportunity.yml',
             'test/process/cancle_crm_lead.yml',
-            'test/process/process_crm_meeting.yml',
+            'test/process/crm_meeting.yml',
             'test/test_crm_opportunity.yml',
             'test/process/process_crm_phonecall.yml',
             'test/test_crm_recurrent_meeting.yml',
