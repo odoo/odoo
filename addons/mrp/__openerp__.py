ab9a47b96e0d3596b6174fda0e7f7813fc9b3ce6
Rucha (Open ERP) <rpa@tinyerp.com>
2011-10-13 17:10:17 +0530

    [IMP]: mrp: Commented some yml files that are not properly formed to fulfill functionalities needed
    bzr revid: rpa@tinyerp.com-20111013114017-qsm5qlvn9m2od2vb

--- addons/mrp/__openerp__.py
+++ addons/mrp/__openerp__.py
@@ -93,13 +93,12 @@ Dashboard provided by this module:
          'mrp_demo.xml',
     ],
     'test': [
-         'test/mrp_procurement.yml',
-         'test/mrp_packs.yml',
-         'test/mrp_phantom_bom.yml',
          'test/mrp_production_order.yml',
+         'test/mrp_packs.yml',
          'test/mrp_production_cancel.yml',
          'test/mrp_report.yml',
-
+#         'test/mrp_procurement.yml',
+#         'test/mrp_phantom_bom.yml',
     ],
     'installable': True,
     'active': False,
