6a0d99cde92866bbf5dd8a82a9515c20eb6f1e66
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2011-10-13 19:00:29 +0200

    [IMP] account_voucher: added case2_usd_eur_debtor_in_eur.yml
    bzr revid: qdp-launchpad@openerp.com-20111013170029-337fw976foqdkfzb

--- addons/account_voucher/__openerp__.py
+++ addons/account_voucher/__openerp__.py
@@ -62,7 +62,7 @@ Account Voucher module includes all the basic requirements of Voucher Entries fo
         "test/account_voucher_report.yml",
         "test/case1_usd_usd.yml",
         #"test/case2_suppl_usd_eur.yml",
-        #"test/case2_usd_eur.yml",
+        "test/case2_usd_eur_debtor_in_eur.yml",
         #"test/case3_eur_eur.yml",
         #"test/case4_cad_chf.yml",
     ],
