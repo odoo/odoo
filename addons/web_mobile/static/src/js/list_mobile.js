27598512e3e267d92a863c7df41974208ea8b554
Bhumi Thakkar (Open ERP) <bth@tinyerp.com>
2011-10-19 17:20:55 +0530

    [IMP] html name_get method is implemeted for additional information.
    bzr revid: bth@tinyerp.com-20111019115055-296oz520fu2megh9

--- addons/web_mobile/static/src/js/list_mobile.js
+++ addons/web_mobile/static/src/js/list_mobile.js
@@ -19,36 +19,52 @@ openerp.web_mobile.ListView = openerp.web.Widget.extend({
         var self = this;
         if (data.action.length) {
             this.action = data.action[0][2];
-            this.on_search_data();
+            return this.rpc('/web/listview/load', {
+                model: this.action.res_model,
+                view_id: this.action.views[0][0],
+                view_type: "tree",
+                context: this.action.context,
+                toolbar: false
+            }, this.on_search_data);
         }
     },
     on_search_data: function(ev){
         var self = this;
-        var dataset = new openerp.web.DataSetStatic(this, this.action.res_model, this.action.context);
-        dataset.name_search('', this.action.domain, 'ilike', false, function(result){
-            self.$element.html(self.render({'records' : result}));
-            self.$element.find("[data-role=header]").find('h1').html(self.action.name);
-            self.$element.find("[data-role=header]").find('#home').click(function(){
-                $.mobile.changePage("#oe_menu", "slide", false, true);
-            });
-            self.$element.find("[data-role=footer]").find('#shrotcuts').click(function(){
-                if (!$('#oe_shortcuts').html().length) {
-                    this.shortcuts = new openerp.web_mobile.Shortcuts(self, "oe_shortcuts");
-                    this.shortcuts.start();
-                }else{
-                    $.mobile.changePage("#oe_shortcuts", "slide", false, true);
+        var dataset = new openerp.web.DataSetSearch(this, this.action.res_model,this.action.context);
+        dataset.domain = self.action.domain;
+        dataset.read_slice([], {}, function(result,ids){
+            _.extend(self.action.context,{"html_name_get" : true});
+
+            var dataset1 = new openerp.web.DataSet(self, self.action.res_model,self.action.context);
+            dataset1.name_get(dataset.ids,function(res){
+                var additional = "";
+                if(res['html_name_get']){
+                    additional = res['display'];
                 }
+                self.$element.html(self.render({'records': res, 'data': additional }));
+                self.$element.find("[data-role=header]").find('h1').html(self.action.name);
+                self.$element.find("[data-role=header]").find('#home').click(function(){
+                    $.mobile.changePage("#oe_menu", "slide", false, true);
+                });
+                self.$element.find("[data-role=footer]").find('#shrotcuts').click(function(){
+                    if (!$('#oe_shortcuts').html().length) {
+                        this.shortcuts = new openerp.web_mobile.Shortcuts(self, "oe_shortcuts");
+                        this.shortcuts.start();
+                    }else{
+                        $.mobile.changePage("#oe_shortcuts", "slide", false, true);
+                    }
+                });
+                self.$element.find("[data-role=footer]").find('#preference').click(function(){
+                    if(!$('#oe_options').html().length){
+                        this.options = new openerp.web_mobile.Options(self, "oe_options");
+                        this.options.start();
+                    }else{
+                        $.mobile.changePage("#oe_options", "slide", false, true);
+                    }
+                });
+                self.$element.find("a#list-id").click(self.on_list_click);
+                $.mobile.changePage("#"+self.element_id, "slide", false, true);
             });
-            self.$element.find("[data-role=footer]").find('#preference').click(function(){
-                if(!$('#oe_options').html().length){
-                    this.options = new openerp.web_mobile.Options(self, "oe_options");
-                    this.options.start();
-                }else{
-                    $.mobile.changePage("#oe_options", "slide", false, true);
-                }
-            });
-            self.$element.find("a#list-id").click(self.on_list_click);
-            $.mobile.changePage("#"+self.element_id, "slide", false, true);
         });
     },
     on_list_click: function(ev) {
