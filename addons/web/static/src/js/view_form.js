19bb1dda1a0d59b3010f06b53e47473ee0923105
Xavier Morel <xmo@openerp.com>
2011-10-24 17:20:13 +0200

    [IMP] use empty box/check mark for page boolean fields, instead of cross vs check mark
    bzr revid: xmo@openerp.com-20111024152013-nhi8kwra46se0yva

--- addons/web/static/src/js/view_form.js
+++ addons/web/static/src/js/view_form.js
@@ -2848,7 +2848,7 @@ openerp.web.form.FieldUrlReadonly = openerp.web.form.FieldURIReadonly.extend({
 });
 openerp.web.form.FieldBooleanReadonly = openerp.web.form.FieldCharReadonly.extend({
     set_value: function (value) {
-        this._super(value ? '\u2714' : '\u2718');
+        this._super(value ? '\u2611' : '\u2610');
     }
 });
 openerp.web.form.FieldSelectionReadonly = openerp.web.form.FieldReadonly.extend({
