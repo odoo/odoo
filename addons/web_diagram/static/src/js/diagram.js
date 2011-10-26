7ba5602f719f7d22359bed14534241ea49bc434f
Xavier Morel <xmo@openerp.com>
2011-10-26 15:15:56 +0200

    [FIX] turns out raphael's event system looks like jquery's but behaves *nothing like it*.
    Correctly fix deduplication of event handlers: add events on the nodes collection directly
    
    bzr revid: xmo@openerp.com-20111026131556-v7a0bbkhk633r6i9

--- addons/web_diagram/static/src/js/diagram.js
+++ addons/web_diagram/static/src/js/diagram.js
@@ -125,18 +125,13 @@ openerp.web.DiagramView = openerp.web.View.extend({
 
             var node = r[shape](n.node.x, n.node.y).attr({
                 "fill": n.node.color
-            }).dblclick(function() {
-                self.add_edit_node(n.node.id, self.node);
             });
 
-            var propagate_to_previous_sibling = function (e) {
-                $(this).prev().trigger(e.type);
-            };
-            var nodes = r.set()
-                .push(node, r.text(n.node.x, n.node.y, (n.label || n.id))
-                        .click(propagate_to_previous_sibling)
-                        .dblclick(propagate_to_previous_sibling)
-                ).attr("cursor", "pointer");
+            var nodes = r.set(node, r.text(n.node.x, n.node.y, (n.label || n.id)))
+                .attr("cursor", "pointer")
+                .dblclick(function() {
+                    self.add_edit_node(n.node.id, self.node);
+                });
 
             if (shape === 'rect') {
                 node.attr({width: "60", height: "44"});
