f45e2468bc96a191d82fc1b081e8388c531af1e0
DBR (OpenERP) <dbr@tinyerp.com>
2011-11-02 13:05:52 +0530

    [ADD,IMP] purchase_requisition: [ADD]Add New file for define the main flow of exclusive requisition "purchase_requisition_exclusive.yml" [IMP]Improve the demo data for exclisive code improvement [IMP]Add one new file in __openerp__.py
    bzr revid: dbr@tinyerp.com-20111102073552-yruxrxuvb9qji3pg

--- addons/purchase_requisition/__openerp__.py
+++ addons/purchase_requisition/__openerp__.py
@@ -45,6 +45,7 @@ This new object will regroup and will allow you to easily keep track and order a
     "active": False,
     "test":[
             'test/process/purchase_requisition.yml',
+            'test/process/purchase_requisition_exclusive.yml',            
             'test/process/cancel_purchase_requisition.yml',
             'test/ui/onchange_events.yml',
             'test/ui/purchase_requisition_report.yml',
