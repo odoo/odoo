2ae078e58cec253c36828abd10460c528f36b948
Christophe Simonis <chs@openerp.com>
2011-11-08 15:52:14 +0100

    [FIX] mergecss: allow spaces after (
    bzr revid: chs@openerp.com-20111108145214-ahdaiyk0f616zq74

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -111,7 +111,7 @@ class WebClient(openerpweb.Controller):
         file_map = dict(files)
 
         rx_import = re.compile(r"""@import\s+('|")(?!'|"|/|https?://)""", re.U)
-        rx_url = re.compile(r"""url\s*\(('|"|)(?!'|"|/|https?://)""", re.U)
+        rx_url = re.compile(r"""url\s*\(\s*('|"|)(?!'|"|/|https?://)""", re.U)
 
 
         def reader(f):
