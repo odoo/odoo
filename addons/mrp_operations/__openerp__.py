c6d2ee303f322e3be7fc7cbcab31c64e24b9041a
Rucha (Open ERP) <rpa@tinyerp.com>
2011-11-17 15:03:44 +0530

    [IMP]: mrp_operations: Given proper name to test file name
    bzr revid: rpa@tinyerp.com-20111117093344-hbjlz07uw7j7kgk1

--- addons/mrp_operations/__openerp__.py
+++ addons/mrp_operations/__openerp__.py
@@ -65,7 +65,7 @@ So that we can compare the theoretic delay and real delay.
         'process/mrp_operation_process.xml'
     ],
     'demo_xml': ['mrp_operation_data.xml'],
-    'test': ['test/check_mo2operations.yml', 'test/mrp_operations_report.yml','test/mrp_operation_test.yml'],
+    'test': ['test/check_mo2operations.yml', 'test/check_mo_cancel.yml', 'test/mrp_operations_report.yml'],
     'installable': True,
     'active': False,
     'certificate': '0056233813133',
