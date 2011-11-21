eaebfd814d76a4767040d49e819a959fb89379f3
Ujjvala Collins (OpenERP) <uco@tinyerp.com>
2011-11-21 18:31:53 +0530

    [IMP] mrp_operations: Some usability improvements mentioned on pad.
    bzr revid: uco@tinyerp.com-20111121130153-re5blfwujjwjvqy6

--- addons/mrp_operations/mrp_operations.py
+++ addons/mrp_operations/mrp_operations.py
@@ -86,10 +86,10 @@ class mrp_production_workcenter_line(osv.osv):
     _order = "sequence, date_planned"
 
     _columns = {
-       'state': fields.selection([('draft','Draft'),('startworking', 'In Progress'),('pause','Pause'),('cancel','Cancelled'),('done','Finished')],'State', readonly=True,
+       'state': fields.selection([('draft','Draft'),('startworking', 'In Progress'),('pause','Pending'),('cancel','Cancelled'),('done','Finished')],'State', readonly=True,
                                  help="* When a work order is created it is set in 'Draft' state.\n" \
                                        "* When user sets work order in start mode that time it will be set in 'In Progress' state.\n" \
-                                       "* When work order is in running mode, during that time if user wants to stop or to make changes in order then can set in 'Pause' state.\n" \
+                                       "* When work order is in running mode, during that time if user wants to stop or to make changes in order then can set in 'Pending' state.\n" \
                                        "* When the user cancels the work order it will be set in 'Canceled' state.\n" \
                                        "* When order is completely processed that time it is set in 'Finished' state."),
        'date_start_date': fields.function(_get_date_date, string='Start Date', type='date'),
