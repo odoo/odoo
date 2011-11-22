becbd5b33d0a20708adc78130a5e3f9a4f74c4e7
Jigar Amin - OpenERP <jam@tinyerp.com>
2011-11-22 17:15:27 +0530

    [IMP] Added yaml udner test section
    bzr revid: jam@tinyerp.com-20111122114527-1e9uqmt80b6g4ldw

--- addons/resource/__openerp__.py
+++ addons/resource/__openerp__.py
@@ -45,7 +45,10 @@ It also manages the leaves of every resource.
     ],
     'demo_xml': ['resource_demo.xml'
     ],
-    'test': ['test/on_chnage_resource.yml'],#TODO: Add Resource test cases
+    'test': [
+                'test/on_chnage_resource.yml',
+                'test/test_resource.yml',
+            ],
     'installable': True,
     'active': False,
     'certificate': '00746371192190459469',
