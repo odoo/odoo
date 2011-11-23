84c1bedc378ab42a9dd6f6c1a900f34a2fcd1e61
Sébastien LANGE <sebastien.lange@syleam.fr>
2011-11-23 15:22:46 +0530

    [FIX] Hr_timesheet_sheet : Fixed issue of Date From in the Timesheet (Ref case : 50628)
    bzr revid: dhs@tinyerp.com-20111123095246-kt71rn1v9bp0ddzc

--- addons/hr_timesheet_sheet/hr_timesheet_sheet.py
+++ addons/hr_timesheet_sheet/hr_timesheet_sheet.py
@@ -21,7 +21,7 @@
 
 import time
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
+from dateutil.relativedelta import relativedelta, MO
 
 from osv import fields, osv
 from tools.translate import _
@@ -297,7 +297,7 @@ class hr_timesheet_sheet(osv.osv):
         if r=='month':
             return time.strftime('%Y-%m-01')
         elif r=='week':
-            return (datetime.today() + relativedelta(weekday=0, weeks=-1)).strftime('%Y-%m-%d')
+            return (datetime.today() + relativedelta(weekday=MO(-1))).strftime('%Y-%m-%d')
         elif r=='year':
             return time.strftime('%Y-01-01')
         return time.strftime('%Y-%m-%d')
