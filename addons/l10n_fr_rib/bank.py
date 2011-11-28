25f83943e19904a9358ad15fb59e6f26f2baffcf
Numerigraphe - Lionel Sausin <ls@numerigraphe.fr>
2011-11-28 15:43:48 +0100

    [IMP] extensive module description [IMP] assign (c) back to Numérigraphe
    lp bug: https://launchpad.net/bugs/387958 fixed
    
    bzr revid: ls@numerigraphe.fr-20111128144348-ccl8ays59rc2hvj2

--- addons/l10n_fr_rib/bank.py
+++ addons/l10n_fr_rib/bank.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2011 OpenERP SA (<http://openerp.com>).
+#    Copyright (C) 2011 Numérigraphe SARL.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
