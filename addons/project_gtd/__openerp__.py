4b072366b2d0b1be4bb6fb429a19105e057295ed
Hemendra Paregi (Open ERP) <hpa@tinyerp.com>
2011-11-28 10:50:04 +0530

    [IMP]project_gtd: improvement in yml
    bzr revid: hpa@tinyerp.com-20111128052004-cv0w7uty6d99cehh

--- addons/project_gtd/__openerp__.py
+++ addons/project_gtd/__openerp__.py
@@ -57,8 +57,7 @@ performing those tasks.
     ],
     'demo_xml': ['project_gtd_demo.xml'],
     'test':[
-        #'test/gtd_test.yml',
-         'test/timebox.yml'
+         'test/task_timebox.yml'
     ],
     'installable': True,
     'active': False,
