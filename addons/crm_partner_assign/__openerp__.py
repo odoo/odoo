ad7e7c48e02b7b5ac2392bfde38ee2d1c7b634fc
ron@tinyerp.com <>
2011-11-30 14:36:19 +0530

    [REM] crm_partner_assign: removed crm_partner_assign/test/ui/partner_assign_demo.yml
    bzr revid: ron@tinyerp.com-20111130090619-37he2tau7vb3a2en

--- addons/crm_partner_assign/__openerp__.py
+++ addons/crm_partner_assign/__openerp__.py
@@ -51,7 +51,6 @@ You can also use the geolocalization without using the GPS coordinates.
     ],
     'test': [
         'test/process/partner_assign.yml',
-        'test/ui/partner_assign_demo.yml'
     ],
     'installable': True,
     'active': False,
