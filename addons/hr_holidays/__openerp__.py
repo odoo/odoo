43a42b95998057afb17cb58555688a7a63e26763
Fabien Pinckaers <fp@tinyerp.com>
2011-12-06 13:05:11 +0100

    [IMP] due to rewrite we did some months ago
    bzr revid: fp@tinyerp.com-20111206120511-gdtg6o9askurv6hy

--- addons/hr_holidays/__openerp__.py
+++ addons/hr_holidays/__openerp__.py
@@ -23,7 +23,7 @@
 {
     "name": "Leaves Management",
     "version": "1.5",
-    "author": ['OpenERP SA', 'Axelor'],
+    "author": "OpenERP SA",
     "category": "Human Resources",
     'complexity': "easy",
     "website": "http://www.openerp.com",
