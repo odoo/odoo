0b687827bd7b770774e9c230139b9fd4bf91a8db
Kirti Savalia (OpenERP) <ksa@tinyerp.com>
2011-12-07 11:50:31 +0530

    [IMP]:yml for double validation
    bzr revid: ksa@tinyerp.com-20111207062031-lxtpm8bvx09ehp0l

--- addons/purchase_double_validation/__openerp__.py
+++ addons/purchase_double_validation/__openerp__.py
@@ -41,7 +41,10 @@ that exceeds minimum amount set by configuration wizard.
         'purchase_double_validation_installer.xml',
         'board_purchase_view.xml'
     ],
-    'test': ['test/purchase_double_validation_test.yml'],
+    'test': [
+             'test/process/purchase_double_validation_demo.yml',
+             'test/process/purchase_double_validation_test.yml'
+             ],
     'demo_xml': [],
     'installable': True,
     'active': False,
