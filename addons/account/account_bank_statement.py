5f3bb33a4fe0473ff037c4111c20929fca36dbba
Bharat (OpenERP) <bde@tinyerp.com>
2011-12-07 16:43:47 +0530

    Used proper case in tooltip
    bzr revid: bde@tinyerp.com-20111207111347-udhlw1wzwy6wai5y

--- addons/account/account_bank_statement.py
+++ addons/account/account_bank_statement.py
@@ -129,7 +129,7 @@ class account_bank_statement(osv.osv):
     _name = "account.bank.statement"
     _description = "Bank Statement"
     _columns = {
-        'name': fields.char('Name', size=64, required=True, states={'draft': [('readonly', False)]}, readonly=True, help='If you enter the Statement name other than /, the Moves created for accounting Entries will be same as statement name. This allows the statement entries to have a reference to the bank statement they appeared on.'), # readonly for account_cash_statement
+        'name': fields.char('Name', size=64, required=True, states={'draft': [('readonly', False)]}, readonly=True, help='If you enter statement name other than /, the moves created for accounting entries will be same as statement name. This allows the statement entries to have a reference to the bank statement they appeared on.'), # readonly for account_cash_statement
         'date': fields.date('Date', required=True, states={'confirm': [('readonly', True)]}),
         'journal_id': fields.many2one('account.journal', 'Journal', required=True,
             readonly=True, states={'draft':[('readonly',False)]}),
