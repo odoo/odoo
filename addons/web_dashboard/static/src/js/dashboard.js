e8e7f3dba7030ff4e6e2dc989eabe9b2a98a6ec6
niv-openerp <nicolas.vanhoren@openerp.com>
2011-12-09 11:04:39 +0100

    [fix] problem with menu disappearing
    bzr revid: nicolas.vanhoren@openerp.com-20111209100439-tttr4skd7mawnzs9

--- addons/web_dashboard/static/src/js/dashboard.js
+++ addons/web_dashboard/static/src/js/dashboard.js
@@ -445,9 +445,11 @@ openerp.web_dashboard.ApplicationInstaller = openerp.web.Widget.extend({
                 pager: false
             },
         });
+        $('.secondary_menu', this.$element.closest('.openerp')).hide();
         return r;
     },
     stop: function() {
+        $('.secondary_menu', this.$element.closest('.openerp')).show();
         this.action_manager.stop();
         return this._super();
     },
