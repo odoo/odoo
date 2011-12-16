7ecdf32616e6b6efb7b3fefa4694cbf2e4e33170
Jigar Amin - OpenERP <jam@tinyerp.com>
2011-12-16 15:57:29 +0530

    [FIX] File name typo
    bzr revid: jam@tinyerp.com-20111216102729-9v2pekfjsuqc9j0t

--- addons/survey/__openerp__.py
+++ addons/survey/__openerp__.py
@@ -50,7 +50,7 @@ Partners are also sent mails with user name and password for the invitation of t
     'test': [
         'test/draft2open2close_survey.yml',
         'test/draft2open2close_request.yml',
-        'test/surey_question_type.yml',
+        'test/survey_question_type.yml',
         'test/survey_report.yml',
     ],
     'installable': True,
