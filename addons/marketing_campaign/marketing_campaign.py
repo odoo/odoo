dd2f75849e5dc3839b4092890817fd62bdd216bc
Nimesh (Open ERP) <nco@tinyerp.com>
2012-01-06 17:03:53 +0530

    [FIX] remove the required attribute from m2o compaign_id of marketing.campaign.activity .
    bzr revid: nco@tinyerp.com-20120106113353-o3to6e2kos0acdxn

--- addons/marketing_campaign/marketing_campaign.py
+++ addons/marketing_campaign/marketing_campaign.py
@@ -404,7 +404,7 @@ class marketing_campaign_activity(osv.osv):
     _columns = {
         'name': fields.char('Name', size=128, required=True),
         'campaign_id': fields.many2one('marketing.campaign', 'Campaign',
-                                            required = True, ondelete='cascade', select=1),
+                                            ondelete='cascade', select=1),
         'object_id': fields.related('campaign_id','object_id',
                                       type='many2one', relation='ir.model',
                                       string='Object', readonly=True),
