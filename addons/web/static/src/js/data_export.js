d09c6bcfe558c2566820d625b347cd0813a4c2f6
Xavier Morel <xmo@openerp.com>
2012-01-16 11:16:31 +0100

    [FIX] small issue in xmo@openerp.com-20120116095337-ko0y6xha6oqx4vw1
    bzr revid: xmo@openerp.com-20120116101631-ln5oa2fdlt670rqi

--- addons/web/static/src/js/data_export.js
+++ addons/web/static/src/js/data_export.js
@@ -362,7 +362,9 @@ openerp.web.DataExport = openerp.web.Dialog.extend({
         return export_field;
     },
     on_click_export_data: function() {
+        var self = this;
         var exported_fields = this.$element.find('#fields_list option').map(function () {
+            var name = self.records[this.value] || this.value
             // DOM property is textContent, but IE8 only knows innerText
             return {name: this.value,
                     label: this.textContent || this.innerText};
