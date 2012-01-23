8310fc0122f8c127bc12edd9905c254d93c567d4
Olivier Dony <odo@openerp.com>
2012-01-23 16:11:40 +0100

    [REV] plugin_outlook: partial revert of accidental certificate removal in previous revision
    bzr revid: odo@openerp.com-20120123151140-8aznfzi0byhxtms8

--- addons/plugin_outlook/__openerp__.py
+++ addons/plugin_outlook/__openerp__.py
@@ -40,6 +40,7 @@ mail into mail.message with attachments.
     'update_xml' : ['plugin_outlook.xml'],
     'active': False,
     'installable': True,
+    'certificate' : '001278773815818292125',
     'images': ['images/config_outlook.jpeg','images/outlook_config_openerp.jpeg','images/outlook_push.jpeg'],
 
 }
