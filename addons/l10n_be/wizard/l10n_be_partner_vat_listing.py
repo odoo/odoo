9267d3dd671b088daf9209de8f998dd60f0d88a5
Bharat Devnani (OpenERP) <bde@tinyerp.com>
2012-01-31 11:17:13 +0530

    [REM] removed required=False attribute from partner_ids field in l10n_be/wizard/l10n_be_partner_vat_listing.py
    bzr revid: bde@tinyerp.com-20120131054713-8yutobg67ohfh3cc

--- addons/l10n_be/wizard/l10n_be_partner_vat_listing.py
+++ addons/l10n_be/wizard/l10n_be_partner_vat_listing.py
@@ -145,7 +145,7 @@ class partner_vat_list_13(osv.osv_memory):
     _name = "partner.vat.list_13"
     _columns = {
         # TODO the referenced model has been deleted at revno 4672.1.2.
-        'partner_ids': fields.many2many('vat.listing.clients', 'vat_partner_rel', 'vat_id', 'partner_id', 'Clients', required=False, help='You can remove clients/partners which you do not want to show in xml file'),
+        'partner_ids': fields.many2many('vat.listing.clients', 'vat_partner_rel', 'vat_id', 'partner_id', 'Clients', help='You can remove clients/partners which you do not want to show in xml file'),
         'name': fields.char('File Name', size=32),
         'msg': fields.text('File created', size=64, readonly=True),
         'file_save' : fields.binary('Save File', readonly=True),
