58f68b3ec2e7512e2a48502f7571573f8f3152f7
Antony Lesuisse <al@openerp.com>
2012-02-01 17:22:13 +0100

    add js module skeleton
    bzr revid: al@openerp.com-20120201162213-kwtur61bst82argt

---
+++ addons/mail/static/src/js/mail.js
@@ -0,0 +1,25 @@
+openerp.mail = function(session) {
+
+var mail = session.mail = {};
+
+mail.Wall = session.web.Widget.extend({
+    init: function(parent) {
+    },
+    start: function() {
+        this.$element
+    },
+});
+
+session.ThreadView
+
+session.MessgageInput
+
+
+
+var w = new session.mail.ThreadView(this);
+
+w.appendTo($("div.wall"));
+
+
+};
+// vim:et fdc=0 fdl=0 foldnestmax=3 fdm=syntax:
