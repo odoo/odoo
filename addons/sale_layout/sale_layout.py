c6287a3e23b509be7c33859f5b807aa8167b012e
Antony Lesuisse <al@openerp.com>
2012-02-09 02:59:40 +0100

    fix typos
    bzr revid: al@openerp.com-20120209015940-mim5twc6wknv8f88

--- addons/sale_layout/sale_layout.py
+++ addons/sale_layout/sale_layout.py
@@ -165,10 +165,10 @@ class stock_picking(osv.osv):
     _inherit = "stock.picking"
 
     def _prepare_invoice_line(self, cr, uid, group, picking, move_line, invoice_id, invoice_vals, context=None):
-        vals = super(stock_picking, self).__prepare_invoice_line(cr, uid, group, picking, move_line, invoice_id, invoice_vals, context)
+        vals = super(stock_picking, self)._prepare_invoice_line(cr, uid, group, picking, move_line, invoice_id, invoice_vals, context)
 
         if "sequence" in self.pool.get("account.invoice.line")._all_columns and move_line.sale_line_id:
-            vals['sequence'] == move_line.sale_line_id.sequence
+            vals['sequence'] = move_line.sale_line_id.sequence
 
         return vals
 
