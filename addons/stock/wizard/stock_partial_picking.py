447df6e9618d77ebff9e693e49b1ddfdc981d856
tfr@openerp.com <>
2012-02-13 13:11:16 +0100

    [FIX] OPW 17161 : don't use the same relational field for two different osv_memory, avoid to mess up 'foreign key'
    bzr revid: tfr@openerp.com-20120213121116-2xd3xk2q9197sbux

--- addons/stock/wizard/stock_partial_picking.py
+++ addons/stock/wizard/stock_partial_picking.py
@@ -30,8 +30,8 @@ class stock_partial_picking(osv.osv_memory):
     _description = "Partial Picking"
     _columns = {
         'date': fields.datetime('Date', required=True),
-        'product_moves_out' : fields.one2many('stock.move.memory.out', 'wizard_id', 'Moves'),
-        'product_moves_in' : fields.one2many('stock.move.memory.in', 'wizard_id', 'Moves'),
+        'product_moves_out' : fields.one2many('stock.move.memory.out', 'wizard_pick_id', 'Moves'),
+        'product_moves_in' : fields.one2many('stock.move.memory.in', 'wizard_pick_id', 'Moves'),
      }
 
     def get_picking_type(self, cr, uid, picking, context=None):
