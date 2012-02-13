8e0efe168a0bd62a689d063891a1a002f9674817
Najlaâ <nel@tinyerp.com>
2012-02-13 15:57:46 +0100

    [MERGE]
    bzr revid: nel@tinyerp.com-20120213145746-w8z1ruqwr6v6e9ps

--- addons/account/wizard/account_invoice_refund.py
+++ addons/account/wizard/account_invoice_refund.py
@@ -34,7 +34,7 @@ class account_invoice_refund(osv.osv_memory):
     _columns = {
        'date': fields.date('Operation date', help='This date will be used as the invoice date for Refund Invoice and Period will be chosen accordingly!'),
        'period': fields.many2one('account.period', 'Force period'),
-       'journal_id': fields.many2one('account.journal', 'Refund Journal', help='You can select here the journal to use for the refund invoice that will be created. If you leave that field empty, it will use the same journal as the current invoice.'),
+       'journal_id': fields.many2one('account.journal', 'Refund Journal', help='You should select here the journal to use for the refund invoice that will be created.'),
        'description': fields.char('Description', size=128, required=True),
        'filter_refund': fields.selection([('modify', 'Modify'), ('refund', 'Refund'), ('cancel', 'Cancel')], "Refund Type", required=True, help='Refund invoice base on this type. You can not Modify and Cancel if the invoice is already reconciled'),
     }
