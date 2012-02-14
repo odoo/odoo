f078a29e7db139591cb5620bef2de9b679016ae7
Antony Lesuisse <al@openerp.com>
2012-02-15 00:24:48 +0100

    update as per qdp review
    bzr revid: al@openerp.com-20120214232448-54yp5vbnk1jw90dz

--- addons/sale_layout/sale_layout.py
+++ addons/sale_layout/sale_layout.py
@@ -165,9 +165,9 @@ class stock_picking(osv.osv):
     _inherit = "stock.picking"
 
     def _prepare_invoice_line(self, cr, uid, group, picking, move_line, invoice_id, invoice_vals, context=None):
-        vals = super(stock_picking, self)._prepare_invoice_line(cr, uid, group, picking, move_line, invoice_id, invoice_vals, context)
+        vals = super(stock_picking, self)._prepare_invoice_line(cr, uid, group, picking, move_line, invoice_id, invoice_vals, context=context)
 
-        if "sequence" in self.pool.get("account.invoice.line")._all_columns and move_line.sale_line_id:
+        if move_line.sale_line_id:
             vals['sequence'] = move_line.sale_line_id.sequence
 
         return vals
