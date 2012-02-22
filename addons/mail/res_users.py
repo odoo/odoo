bb1b6fe1766ac240e2d6e0154bd0d6dcd4e714e6
Thibault Delavallée <tde@openerp.com>
2012-02-22 12:59:24 +0100

    [ADD] res_users: added res_users.py file, making res.users in heriting from mail.thread. This is necessary to use mail.thread API on users (for tweets).
    bzr revid: tde@openerp.com-20120222115924-i55twzupuuzuc3gl

--- addons/mail/res_users.py
+++ addons/mail/res_users.py
@@ -2,30 +2,32 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2009-Today OpenERP SA (<http://www.openerp.com>).
+#    Copyright (C) 2009-Today OpenERP SA (<http://www.openerp.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    License, or (at your option) any later version
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU Affero General Public License for more details
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 ##############################################################################
 
-import mail_message
-import mail_thread
-import mail_group
-import mail_subscription
-import res_users
-import res_partner
-import wizard
+from osv import osv
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+class res_users(osv.osv):
+    _name = 'res.users'
+    _inherit = ['res.users', 'mail.thread']
+    
+    def create(self, cr, uid, data, context=None):
+        user_id = super(res_users, self).create(cr, uid, data, context=context)
+        # make user follow itself
+        self.message_subscribe(cr, uid, [user_id], [user_id], context=context)
+        return user_id
 
