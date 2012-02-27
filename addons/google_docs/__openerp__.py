ebfb0be2e156b49a14790300ce746dba78a159d5
Roberto López López (OpenERP) <rlo@openerp.com>
2012-02-27 10:02:24 +0100

    [IMP] copy from pad
    bzr revid: rlo@openerp.com-20120227090224-zujpg9zhf2nd5ioe

--- addons/google_docs/__openerp__.py
+++ addons/google_docs/__openerp__.py
@@ -1,6 +1,18 @@
 {
     'name': 'Wiki',
     'version': '0.1',
+    'author': 'OpenERP SA',
+    'website': 'http://openerp.com',
+    'data': [
+        'res_gdocs.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'web': True,
+    'js': ['static/src/js/gdocs.js'],
+    'qweb' : [
+        "static/src/xml/*.xml",
+    ],
     'depends': ['google_base_account'],
     'description': 'Module to attach a google document to any model.'
 }
