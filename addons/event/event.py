c54a2e518420ecd8c8f179741a7496f1a97ece30
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2012-02-27 16:17:35 +0100

    [IMP] event_moodle: overall improvement, cleaned the code, removed unused files, ...
    bzr revid: qdp-launchpad@openerp.com-20120227151735-w683kz6502cx5usu

--- addons/event/event.py
+++ addons/event/event.py
@@ -263,7 +263,7 @@ class event_registration(osv.osv):
         self.message_append(cr, uid, ids,_('State set to...'),body_text= _('Open'))
         return res
 
-    def case_close(self, cr, uid, ids, context=None):
+    def button_reg_close(self, cr, uid, ids, context=None):
         """ Close Registration
         """
         if context is None:
@@ -273,24 +273,9 @@ class event_registration(osv.osv):
         self.message_append(cr, uid, ids,_('State set to...'),body_text= _('Done'))
         return res
 
-    # event uses add_note wizard from crm, which expects case_* methods
-    def case_open(self, cr, uid, ids, context):
-        return self.registration_open(cr, uid, ids, context)
-
-    # event uses add_note wizard from crm, which expects case_* methods
-    def case_cancel(self, cr, uid, ids, context=None):
-        """ Cancel Registration
-        """
+    def button_reg_cancel(self, cr, uid, ids, context=None, *args):
         self.message_append(cr, uid, ids,_('State set to...'),body_text= _('Cancel'))
         return self.write(cr, uid, ids, {'state': 'cancel'})
-
-    def button_reg_close(self, cr, uid, ids, context=None):
-        """This Function Close Event Registration.
-        """
-        return self.case_close(cr, uid, ids)
-
-    def button_reg_cancel(self, cr, uid, ids, context=None, *args):
-        return self.case_cancel(cr, uid, ids)
 
     def mail_user(self, cr, uid, ids, confirm=False, context=None):
         """
