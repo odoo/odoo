d709d602d8e139b09b19bb842c1560b2f87052d8
Vo Minh Thu <vmt@openerp.com>
2012-02-28 12:24:18 +0100

    [ADD] FEATURES.rst: whenever a feature branch is reviewed (but normally also when it is started), the description of the implented features must be provided, so here they are.
    bzr revid: vmt@openerp.com-20120228112418-r8tjx3o6xc2cbaej

--- openerp/tests/__init__.py
+++ openerp/tests/__init__.py
@@ -4,17 +4,8 @@ Tests for the OpenERP library.
 
 This module groups a few sub-modules containing unittest2 test cases.
 
-Some of those test sub-modules are explicitely listed in the `fast_suite`
-variable.  Most of the tests should be considered fast enough to be included in
-that `fast_suite` list and only tests that take a long time to run (e.g. more
-than a minute) should not be listed.
-
-Some other test sub-modules are sanity checks explicitely listed in the
-`checks` variable. These test sub-modules are invariants that must be
-full-filled at any time. They are expected to always pass: obviously they must
-pass right after the module is installed, but they must also pass after any
-other module is installed, after a migration, or even after the database was
-put in production for a few months.
+Tests can be explicitely added to the `fast_suite` or `checks` lists or not.
+See the :ref:`test-framework` section in the :ref:`features` list.
 """
 
 import test_orm
