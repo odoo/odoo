4bef7bd1aa22dbf6d7a570014d9bdb48c3aaafbd
Bhumika (OpenERP) <sbh@tinyerp.com>
2012-03-05 17:58:01 +0530

    [Fix] base/res: Fix typo
    bzr revid: sbh@tinyerp.com-20120305122801-1tscam6gm7krharu

--- openerp/addons/base/res/res_partner.py
+++ openerp/addons/base/res/res_partner.py
@@ -434,8 +434,8 @@ class res_partner(osv.osv):
         
     def default_get(self, cr, uid, fields, context=None):
         res =  super(res_partner, self).default_get( cr, uid, fields, context)
-        if 'is_comapny' in res:
-            res.update({'photo': self._get_photo(self, cr, uid, res.get('is_comapny', 'contact'), context)})
+        if 'is_company' in res:
+            res.update({'photo': self._get_photo(self, cr, uid, res.get('is_company', 'contact'), context)})
         return res
 
 res_partner()
