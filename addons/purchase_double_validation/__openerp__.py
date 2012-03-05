900d064e3ea853108656215dece270b79781d8f5
Mayur Maheshwari (OpenERP) <mma@tinyerp.com>
2012-03-05 11:16:20 +0530

    [IMP]: purchase: Improvements in purchase search view: used the same for RfQ and Purchase Orders
    bzr revid: mma@tinyerp.com-20120305054620-zxzydncz12nj6ms5

--- addons/purchase_double_validation/__openerp__.py
+++ addons/purchase_double_validation/__openerp__.py
@@ -39,6 +39,7 @@ that exceeds minimum amount set by configuration wizard.
     'update_xml': [
         'purchase_double_validation_workflow.xml',
         'purchase_double_validation_installer.xml',
+        'purchase_double_validation_view.xml',
         'board_purchase_view.xml'
     ],
     'test': [
