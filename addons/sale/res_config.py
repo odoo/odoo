6da32d8cc82bb9286acc992fd92b482c1eea6072
Turkesh Patel (Open ERP) <tpa@tinyerp.com>
2012-03-07 12:49:05 +0530

    [IMP] remove unused code
    bzr revid: tpa@tinyerp.com-20120307071905-lazefo5xyb7qo75d

--- addons/sale/res_config.py
+++ addons/sale/res_config.py
@@ -27,13 +27,13 @@ class sale_configuration(osv.osv_memory):
     _inherit = 'res.config.settings'
 
     _columns = {
-        'sale_orders': fields.boolean('Based on Sales Orders', 
+        'sale_orders': fields.boolean('Based on Sales Orders',
                                       help="To allow your salesman to make invoices for sale order lines using 'Lines to Invoice' menu."),
         'deli_orders': fields.boolean('Based on Delivery Orders',
                                       help="To allow your salesman to make invoices for Delivery Orders using 'Deliveries to Invoice' menu."),
         'task_work': fields.boolean('Based on Tasks\' Work',
                                     help="""Lets you transfer the entries under tasks defined for Project Management to
-                                    the Timesheet line entries for particular date and particular user  with the effect of creating, editing and deleting either ways 
+                                    the Timesheet line entries for particular date and particular user  with the effect of creating, editing and deleting either ways
                                     and to automatically creates project tasks from procurement lines.
                                     It installs the project_timesheet and project_mrp modules."""),
         'module_account_analytic_analysis': fields.boolean('Based on Timesheet',
@@ -43,7 +43,7 @@ class sale_configuration(osv.osv_memory):
         'default_order_policy': fields.selection([
                                 ('manual', 'Invoice Based on Sales Orders'),
                                 ('picking', 'Invoice Based on Deliveries'),
-                            ], 'Main Method Based On', required=True, default_model='sale.order', 
+                            ], 'Main Method Based On', required=True, default_model='sale.order',
                             help="You can generate invoices based on sales orders or based on shippings."),
         'module_delivery': fields.boolean('Do you charge the delivery?',
                                    help ="""
@@ -52,7 +52,7 @@ class sale_configuration(osv.osv_memory):
                                    It installs the delivery module.
                                    """),
         'time_unit': fields.many2one('product.uom','Working Time Unit'),
-        'default_picking_policy' : fields.boolean("Deliver all products at once?", default_model='sale.order', 
+        'default_picking_policy' : fields.boolean("Deliver all products at once?", default_model='sale.order',
                                                   help = "You can set picking policy on sale order that will allow you to deliver all products at once."),
         'group_sale_delivery_address':fields.boolean("Multiple Address", group='base.group_user', implied_group='base.group_sale_delivery_address',
                                                      help="Allows you to set different delivery address and invoice address. It assigns Multiple Address group to all employees."),
@@ -60,7 +60,7 @@ class sale_configuration(osv.osv_memory):
                                                              help="This allows you to apply discounts per sale order lines, it assigns Discounts per sale order lines group to all employees."),
         'module_sale_layout':fields.boolean("Notes & subtotals per line",help="Allows to format sale order lines using notes, separators, titles and subtotals. It installs the sale_layout module."),
         'module_warning': fields.boolean("Alerts by products or customers",
-                                  help="""To raise user specific warning messages on different products used in Sales Orders, Purchase Orders, Invoices and Deliveries. 
+                                  help="""To raise user specific warning messages on different products used in Sales Orders, Purchase Orders, Invoices and Deliveries.
                                   It installs the warning module."""),
         'module_sale_margin': fields.boolean("Display Margin For Users",
                         help="""This adds the 'Margin' on sales order.
@@ -71,7 +71,7 @@ class sale_configuration(osv.osv_memory):
                         It installs the sale_journal module."""),
         'module_analytic_user_function' : fields.boolean("User function by contracts",
                                     help="""Allows you to define what is the default function of a specific user on a given account.
-                                    This is mostly used when a user encodes his timesheet. The values are retrieved and the fields are auto-filled. 
+                                    This is mostly used when a user encodes his timesheet. The values are retrieved and the fields are auto-filled.
                                     But the possibility to change these values is still available.
                                     It installs analytic_user_function module."""),
         'module_analytic_journal_billing_rate' : fields.boolean("Billing rates by contracts",
@@ -82,7 +82,7 @@ class sale_configuration(osv.osv_memory):
                 ('no_tax', 'No Tax'),
                 ('global_on_order', 'Global On Order'),
                 ('on_order_line', 'On Order Lines'),
-            ], 'Taxes', required=True, 
+            ], 'Taxes', required=True,
             help="""
                 If you want to apply global tax on sale order then select 'Global On Order' it will add 'Global On Order' group to employees.
                 If you want to apply different taxes for sale order lines then select 'On Order Lines' it will add 'On Order Lines' group to employees.
@@ -131,7 +131,7 @@ class sale_configuration(osv.osv_memory):
         'time_unit': lambda self, cr, uid, c: self.pool.get('product.uom').search(cr, uid, [('name', '=', _('Hour'))], context=c) and self.pool.get('product.uom').search(cr, uid, [('name', '=', _('Hour'))], context=c)[0] or False,
         'tax_policy': 'global_on_order',
     }
-    
+
     def _check_default_tax(self, cr, uid, context=None):
         ir_values_obj = self.pool.get('ir.values')
         for tax in ir_values_obj.get(cr, uid, 'default', False, ['product.product']):
@@ -156,7 +156,7 @@ class sale_configuration(osv.osv_memory):
             menu_id = data_obj.get_object(cr, uid, 'sale', 'menu_action_picking_list_to_invoice').id
             menu_obj.write(cr, uid, menu_id, {'groups_id':[(4,group_id)]})
             ir_values_obj.set(cr, uid, 'default', False, 'groups_id', ['ir.ui.menu'], [(4,group_id)])
-            
+
         if wizard.default_picking_policy:
             ir_values_obj.set(cr, uid, 'default', False, 'picking_policy', ['sale.order'], 'one')
 
@@ -171,7 +171,7 @@ class sale_configuration(osv.osv_memory):
             self.pool.get('res.company').write(cr, uid, [company_id], {
                 'project_time_mode_id': wizard.time_unit.id
             }, context=context)
-            
+
         return res
 
     def onchange_tax_policy(self, cr, uid, ids, tax_policy, context=None):
@@ -196,40 +196,6 @@ class sale_configuration(osv.osv_memory):
             ir_values_obj.set(cr, uid, 'default', False, 'tax_id', ['sale.order'], taxes[0])
             ir_values_obj.set(cr, uid, 'default', False, 'tax_id', ['sale.order.line'], taxes)
             ir_values_obj.set(cr, uid, 'default', False, 'taxes_id', ['product.product'], taxes)
-    
-    #TODO: TO BE REMOVED
-#    def set_tax_policy(self, cr, uid, ids, context=None):
-#        data_obj = self.pool.get('ir.model.data')
-#        users_obj = self.pool.get('res.users')
-#        groups_obj = self.pool.get('res.groups')
-#        ir_values_obj = self.pool.get('ir.values')
-#        dummy,user_group_id = data_obj.get_object_reference(cr, uid, 'base', 'group_user')
-#        tax_policy = self.browse(cr, uid, ids[0], context=context).tax_policy
-#        order_group_id = data_obj.get_object(cr, uid, 'base', 'group_sale_taxes_global_on_order').id
-#        order_line_group_id = data_obj.get_object(cr, uid, 'base', 'group_sale_taxes_on_order_line').id
-#        group_id = False
-#        remove_group_id = False
-#
-#        if tax_policy == 'global_on_order':
-#            group_id = order_group_id
-#            remove_group_id = order_line_group_id
-#        elif tax_policy == 'on_order_line':
-#            group_id = order_line_group_id
-#            remove_group_id = order_group_id
-#
-#        if group_id:
-#            groups_obj.write(cr, uid, [user_group_id], {'implied_ids': [(4,group_id)]})
-#            users_obj.write(cr, uid, [uid], {'groups_id': [(4,group_id)]})
-#            ir_values_obj.set(cr, uid, 'default', False, 'groups_id', ['res.users'], [(4,group_id)])
-#            groups_obj.write(cr, uid, [user_group_id], {'implied_ids': [(3,remove_group_id)]})
-#            users_obj.write(cr, uid, [uid], {'groups_id': [(3,remove_group_id)]})
-#            ir_values_obj.set(cr, uid, 'default', False, 'groups_id', ['res.users'], [(3,remove_group_id)])
-#        else:
-#            groups = [order_group_id, order_line_group_id]
-#            for group_id in groups:
-#                groups_obj.write(cr, uid, [user_group_id], {'implied_ids': [(3,group_id)]})
-#                users_obj.write(cr, uid, [uid], {'groups_id': [(3,group_id)]})
-#                ir_values_obj.set(cr, uid, 'default', False, 'groups_id', ['res.users'], [(3,group_id)])
 
 sale_configuration()
 
