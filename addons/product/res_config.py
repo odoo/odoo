e2c8783f1b48c90e22f23202a45eaec9aafd1d18
Ujjvala Collins (OpenERP) <uco@tinyerp.com>
2012-03-12 15:57:02 +0530

    [ADD]: Added common configuration for UoM per product group.
    bzr revid: uco@tinyerp.com-20120312102702-xp63smj3qcn1eu31

---
+++ addons/product/res_config.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+
+class product_groups_configuration(osv.osv_memory):
+    _inherit = 'res.config.settings'
+
+    _columns = {
+        'group_sale_pricelist_per_customer':fields.boolean("Pricelist per customer",
+                                                           group='base.group_user', implied_group='base.group_sale_pricelist_per_customer',
+                                                           help="""Allows to manage different prices based on rules per category of customers. 
+                                                           Example: 10% for retailers, promotion of 5 EUR on this product, etc.
+                                                           It assigns the "Pricelist" group to all employees."""),
+        'group_sale_uom_per_product':fields.boolean("UOM per product",
+                                                    group='base.group_user', implied_group='base.group_sale_uom_per_product',
+                                                    help="""
+                                                    Allows you to select and maintain different unit of measures per product.
+                                                    It assigns the "UOM per product" group to all employees.
+                                                    """),
+}
+
+product_groups_configuration()
