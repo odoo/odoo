7774af75d71376aa937acbbe3804daa3cdd92b01
Roberto López López (OpenERP) <rlo@openerp.com>
2012-03-14 11:42:05 +0100

    [imp] menu entry
    bzr revid: rlo@openerp.com-20120314104205-y3rpasw7vft8lcb7

--- addons/google_docs/__openerp__.py
+++ addons/google_docs/__openerp__.py
@@ -4,15 +4,18 @@
     'author': 'OpenERP SA',
     'website': 'http://openerp.com',
     'category': 'Tools',
-    'data': [
+    ''' 'data': [
         'google_docs.xml'
-    ],
+    ],'''
     'installable': True,
     'auto_install': False,
     'web': True,
     'js': ['static/src/js/gdocs.js'],
     'qweb' : [
         "static/src/xml/gdocs.xml",
+    ],
+    'update_xml': [
+        'res_config_user_view.xml'
     ],
     'depends': ['google_base_account'],
     'description': 'Module to attach a google document to any model.'
