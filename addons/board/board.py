3a9a5e99e4ce05b4a4a59478c5805eff16481e9e
Thibault Delavallée <tde@openerp.com>
2012-04-03 17:29:28 +0200

    [REM] Temp remove of administration board, that must be updated with removal of res.logs. TODO: update this module.
    bzr revid: tde@openerp.com-20120403152928-0ix0qg2s46ppd479

--- addons/board/board.py
+++ addons/board/board.py
@@ -186,45 +186,46 @@ class board_line(osv.osv):
         'position': lambda *args: 'left'
     }
 
-class res_log_report(osv.osv):
-    """ Log Report """
-    _name = "res.log.report"
-    _auto = False
-    _description = "Log Report"
-    _columns = {
-        'name': fields.char('Year', size=64, required=False, readonly=True),
-        'month':fields.selection([('01', 'January'), ('02', 'February'), \
-                                  ('03', 'March'), ('04', 'April'),\
-                                  ('05', 'May'), ('06', 'June'), \
-                                  ('07', 'July'), ('08', 'August'),\
-                                  ('09', 'September'), ('10', 'October'),\
-                                  ('11', 'November'), ('12', 'December')], 'Month', readonly=True),
-        'day': fields.char('Day', size=128, readonly=True),
-        'creation_date': fields.date('Creation Date', readonly=True),
-        'res_model': fields.char('Object', size=128),
-        'nbr': fields.integer('# of Entries', readonly=True)
-     }
+#class res_log_report(osv.osv):
+    # CSV:: access_res_log_report all,res.log.report,model_res_log_report,,1,0,0,0
+    #""" Log Report """
+    #_name = "res.log.report"
+    #_auto = False
+    #_description = "Log Report"
+    #_columns = {
+        #'name': fields.char('Year', size=64, required=False, readonly=True),
+        #'month':fields.selection([('01', 'January'), ('02', 'February'), \
+                                  #('03', 'March'), ('04', 'April'),\
+                                  #('05', 'May'), ('06', 'June'), \
+                                  #('07', 'July'), ('08', 'August'),\
+                                  #('09', 'September'), ('10', 'October'),\
+                                  #('11', 'November'), ('12', 'December')], 'Month', readonly=True),
+        #'day': fields.char('Day', size=128, readonly=True),
+        #'creation_date': fields.date('Creation Date', readonly=True),
+        #'res_model': fields.char('Object', size=128),
+        #'nbr': fields.integer('# of Entries', readonly=True)
+     #}
 
-    def init(self, cr):
-        """
-            Log Report
-            @param cr: the current row, from the database cursor
-        """
-        tools.drop_view_if_exists(cr,'res_log_report')
-        cr.execute("""
-            CREATE OR REPLACE VIEW res_log_report AS (
-                SELECT
-                    l.id as id,
-                    1 as nbr,
-                    to_char(l.create_date, 'YYYY') as name,
-                    to_char(l.create_date, 'MM') as month,
-                    to_char(l.create_date, 'YYYY-MM-DD') as day,
-                    to_char(l.create_date, 'YYYY-MM-DD') as creation_date,
-                    l.res_model as res_model,
-                    date_trunc('day',l.create_date) as create_date
-                FROM
-                    res_log l
-            )""")
-res_log_report()
+    #def init(self, cr):
+        #"""
+            #Log Report
+            #@param cr: the current row, from the database cursor
+        #"""
+        #tools.drop_view_if_exists(cr,'res_log_report')
+        #cr.execute("""
+            #CREATE OR REPLACE VIEW res_log_report AS (
+                #SELECT
+                    #l.id as id,
+                    #1 as nbr,
+                    #to_char(l.create_date, 'YYYY') as name,
+                    #to_char(l.create_date, 'MM') as month,
+                    #to_char(l.create_date, 'YYYY-MM-DD') as day,
+                    #to_char(l.create_date, 'YYYY-MM-DD') as creation_date,
+                    #l.res_model as res_model,
+                    #date_trunc('day',l.create_date) as create_date
+                #FROM
+                    #res_log l
+            #)""")
+#res_log_report()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
