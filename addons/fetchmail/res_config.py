bfc9eb0c4eefe29c77082380ea5adbeae03c7210
Raphael Collet <rco@openerp.com>
2012-04-03 10:44:25 +0200

    [IMP] fix copyright notice on top of new files
    bzr revid: rco@openerp.com-20120403084425-vhuy0969i0e6oua1

--- addons/fetchmail/res_config.py
+++ addons/fetchmail/res_config.py
@@ -1,14 +1,13 @@
-#-*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    mga@tinyerp.com
+#    OpenERP, Open Source Business Applications
+#    Copyright (C) 2004-2012 OpenERP S.A. (<http://openerp.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
