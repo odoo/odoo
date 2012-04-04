7a42d939a34a5252cbe0e94ecc82ba03524ad111
Khushboo Bhatt (Open ERP) <kbh@tinyerp.com>
2012-04-04 14:25:52 +0530

    [IMP]dropdown menu
    bzr revid: kbh@tinyerp.com-20120404085552-5ykkcivzdpowarnn

--- addons/project/static/src/js/project.js
+++ addons/project/static/src/js/project.js
@@ -4,7 +4,14 @@ openerp.project = function(openerp) {
             self = this
             self._super()
             if(this.view.dataset.model == 'project.project') {
-                console.log("this");
+                $('.oe_project_kanban_vignette').mouseover(function() {
+                    return $(this).find('.oe_project_kanban_action').show();
+                    }).mouseout(function() {
+                    return $(this).find('.oe_project_kanban_action').hide();
+                });
+                $('.oe_project_kanban_action').click(function(){
+                     $('.dropdown-toggle').dropdown();
+                 })
             }
         }
     });
