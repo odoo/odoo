9cee7d8b8e3fcc63a7d86df535173bc2b868bb35
Amit Patel (OpenERP) <apa@tinyerp.com>
2012-04-04 17:54:35 +0530

    [IMP]
    bzr revid: apa@tinyerp.com-20120404122435-321j5dapi882s830

--- addons/project/static/src/js/project.js
+++ addons/project/static/src/js/project.js
@@ -1,16 +1,14 @@
 openerp.project = function(openerp) {
     openerp.web_kanban.ProjectKanban = openerp.web_kanban.KanbanRecord.include({
         bind_events: function() {
-            self = this
-            self._super()
-            if(this.view.dataset.model == 'project.project') {
-                $('.oe_project_kanban_vignette').mouseover(function() {
-                    return $(this).find('.oe_project_kanban_action').show();
-                    }).mouseout(function() {
-                    return $(this).find('.oe_project_kanban_action').hide();
-                });
-                $('.dropdown-toggle').dropdown();
-            }
+            self = this;
+            $('.oe_project_kanban_vignette').mouseover(function() {
+                return $(this).find('.oe_project_kanban_action').show();
+                }).mouseout(function() {
+                return $(this).find('.oe_project_kanban_action').hide();
+            });
+            $('.dropdown-toggle').dropdown();
+            self._super();
         }
     });
 }
