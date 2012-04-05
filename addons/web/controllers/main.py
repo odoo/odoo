f4848e106e0f0dfc1b3dad7691bce91ee79bf742
Turkesh Patel (Open ERP) <tpa@tinyerp.com>
2012-04-05 16:06:13 +0530

    [IMP] Improved code for set the name of pdf report
    bzr revid: tpa@tinyerp.com-20120405103613-0q1dizma6lrg2si3

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -1893,9 +1893,15 @@ class Reports(View):
             report = zlib.decompress(report)
         report_mimetype = self.TYPES_MAPPING.get(
             report_struct['format'], 'octet-stream')
+        if 'name' not in action:
+            reports = req.session.model('ir.actions.report.xml')
+            res_id = reports.search([('report_name', '=',action['report_name']),],
+                                    0, False, False, context)
+            action['name'] = reports.read(res_id, ['name'], context)[0]['name']
+
         return req.make_response(report,
              headers=[
-                 ('Content-Disposition', 'attachment; filename="%s.%s"' % (action['report_name'], report_struct['format'])),
+                 ('Content-Disposition', 'attachment; filename="%s.%s"' % (action['name'], report_struct['format'])),
                  ('Content-Type', report_mimetype),
                  ('Content-Length', len(report))],
              cookies={'fileToken': int(token)})
