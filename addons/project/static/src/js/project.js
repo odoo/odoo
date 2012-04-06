fb994730974ca1e0ac17864387ed9bad52281f51
Amit Patel (OpenERP) <apa@tinyerp.com>
2012-04-06 12:50:40 +0530

    [IMP]
    bzr revid: apa@tinyerp.com-20120406072040-kzcd9fql1if5vyz9

--- addons/project/static/src/js/project.js
+++ addons/project/static/src/js/project.js
@@ -2,26 +2,20 @@ openerp.project = function(openerp) {
     openerp.web_kanban.ProjectKanban = openerp.web_kanban.KanbanRecord.include({
         bind_events: function() {
             self = this;
-            self._super();
             $('.dropdown-toggle').dropdown();
             $('.oe_project_kanban_vignette').mouseover(function() {
                 return $(this).find('.oe_project_kanban_action').show();
                 }).mouseout(function() {
                 return $(this).find('.oe_project_kanban_action').hide();
             });
-            $('.click_button').attr('data-name','open_tasks');
-            	
-            _.each($('.oe_project_kanban_vignette'),function(record){
-                _.each(record.getElementsByTagName('img'),function(img){
-                    var domain = [['id','=',img.getAttribute("id")]]; 
-                    var dataset = new openerp.web.DataSetSearch(this, 'res.users', self.session.context, domain);
-                    dataset.read_slice([]).then(function(result){
-                        img.setAttribute("title",result[0].name)
-                    });
+            $('.project_avatar').mouseover(function() {
+                avatar = this
+                var dataset = new openerp.web.DataSetSearch(this, 'res.users', self.session.context, [['id','=',avatar.getAttribute("id")]]);
+                dataset.read_slice([]).then(function(result){
+                    avatar.setAttribute("title",result[0].name)
                 });
-                
             });
-            
+            self._super();
         }
     });
 }
