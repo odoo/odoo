6baa76220e74f2c27ef6ac2fded41d808641c21c
Sanjay Gohel (Open ERP) <sgo@tinyerp.com>
2012-04-06 16:38:52 +0530

    [IMP]
    bzr revid: sgo@tinyerp.com-20120406110852-b4z3nj5k47kbnh21

--- addons/project_timesheet/static/src/js/project_timesheet.js
+++ addons/project_timesheet/static/src/js/project_timesheet.js
@@ -3,7 +3,11 @@ openerp.project_timesheet = function(openerp) {
         bind_events: function() {
             self = this;
             self._super();
-            console.log("project_timesheet :: ",self)
+            if(this.view.dataset.model == 'project.project') {
+	            if(this.record.task.raw_value && this.record.issues.raw_value && this.record.timesheets.raw_value)$(this.$element).find('.click_button').attr('data-name','open_tasks');
+	            if(!this.record.task.raw_value && !this.record.issues.raw_value && this.record.timesheets.raw_value)$(this.$element).find('.click_button').attr('data-name','open_timesheets');
+	            //if(!this.record.task.raw_value)$(this.$element).find('.click_button').attr('data-name','dummy');
+            };
         }
     });
 }
