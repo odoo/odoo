2d8773b56943ab9b3ee38b6a48cf6ed34bfad621
Amit Patel (OpenERP) <apa@tinyerp.com>
2012-04-10 18:12:25 +0530

    [IMP]
    bzr revid: apa@tinyerp.com-20120410124225-j0cnku20zpx76cbh

--- addons/project_timesheet/static/src/js/project_timesheet.js
+++ addons/project_timesheet/static/src/js/project_timesheet.js
@@ -5,14 +5,13 @@ openerp.project_timesheet = function(openerp) {
             self._super();
             if(this.view.dataset.model == 'project.project') {
             	function include(arr, obj) {
-				    for(var i=0; i<arr.length; i++) {
-				        if (arr[i] == obj) return true;
-				    }
-				}
+		    for(var i=0; i<arr.length; i++) {
+			if (arr[i] == obj) return true;
+		    }
+		}
             	if(include(this.view.fields_keys,"issues"))
             	{
-            		if(!this.record.task.raw_value && !this.record.issues.raw_value && this.record.timesheets.raw_value)$(this.$element).find('.click_button').attr('data-name','open_timesheets');
-	            //if(!this.record.task.raw_value)$(this.$element).find('.click_button').attr('data-name','dummy');
+            	    if(!this.record.task.raw_value && !this.record.issues.raw_value && this.record.timesheets.raw_value)$(this.$element).find('.click_button').attr('data-name','open_timesheets');
             	};
             	if(this.record.task.raw_value && this.record.timesheets.raw_value)$(this.$element).find('.click_button').attr('data-name','open_tasks');
             };
