41d750c1d4a3ffe44315d6e9132f4bfc647a1f48
Alexis de Lattre <alexis@via.ecp.fr>
2012-04-16 10:47:14 +0200

    [IMP] Performance improvement : if location_id already has the right value, we don't write it !
    bzr revid: alexis@via.ecp.fr-20120416084714-otfuezu6tjy533nt

--- addons/mrp/mrp.py
+++ addons/mrp/mrp.py
@@ -660,7 +660,7 @@ class mrp_production(osv.osv):
 
         for (production_id,name) in self.name_get(cr, uid, ids):
             production = self.browse(cr, uid, production_id)
-            if production.move_prod_id:
+            if production.move_prod_id and production.move_prod_id.location_id.id != production.location_dest_id.id:
                 move_obj.write(cr, uid, [production.move_prod_id.id],
                         {'location_id': production.location_dest_id.id})
             self.action_ready_send_note(cr, uid, [production_id], context)
