ff16b9c9888a51855d54c4203ecdabbe49c46713
Antony Lesuisse <al@openerp.com>
2012-04-19 10:28:15 +0200

    [FIX] pad
    bzr revid: al@openerp.com-20120419082815-3rkd76h0bztnb3p1

--- addons/pad/static/src/js/pad.js
+++ addons/pad/static/src/js/pad.js
@@ -1,6 +1,10 @@
 openerp.pad = function(instance) {
 
-instance.web.form.SidebarAttachments = instance.web.form.SidebarAttachments.extend({
+instance.web.Sidebar = instance.web.Sidebar.extend({
+    init: function(parent) {
+        this._super(parent);
+        this.add_items('other',[{ label: "Pad", callback: this.on_add_pad }]);
+    },
     on_attachments_loaded: function(attachments) {
         this._super(attachments);
         var self = this;
@@ -8,18 +12,11 @@ instance.web.form.SidebarAttachments = instance.web.form.SidebarAttachments.exte
         var is_pad = function(a) {
             return a.type == 'url' && a.name == 'Pad';
         };
-        if (_.any(attachments, is_pad)) {
-            $padbtn.hide();
-        } else {
-            $padbtn.show().click(self.on_add_pad);
-        }
     },
     on_add_pad: function() {
         var self = this;
-        var $padbtn = this.$element.find('button.pad');
-        $padbtn.attr('disabled', 'true').find('img, span').toggle();
-        this.view.dataset.call_button('pad_get', [[this.view.datarecord.id], this.view.dataset.get_context()], function(r) {
-            $padbtn.hide();
+        var view = this.getParent();
+        view.dataset.call_button('pad_get', [[view.datarecord.id], view.dataset.get_context()], function(r) {
             self.do_update();
             self.do_action(r.result);
         });
