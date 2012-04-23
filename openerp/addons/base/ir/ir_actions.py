9df4a02a51854821696122616c3d7872cc9da9eb
Divyesh Makwana (Open ERP) <mdi@tinyerp.com>
2012-04-23 16:56:47 +0530

    [IMP] base : Improved the typos.
    bzr revid: mdi@tinyerp.com-20120423112647-85b0ifcb107rydk5

--- openerp/addons/base/ir/ir_actions.py
+++ openerp/addons/base/ir/ir_actions.py
@@ -118,7 +118,7 @@ class report_xml(osv.osv):
         'report_type': fields.char('Report Type', size=32, required=True, help="Report Type, e.g. pdf, html, raw, sxw, odt, html2html, mako2html, ..."),
         'groups_id': fields.many2many('res.groups', 'res_groups_report_rel', 'uid', 'gid', 'Groups'),
         'multi': fields.boolean('On multiple doc.', help="If set to true, the action will not be displayed on the right toolbar of a form view."),
-        'attachment': fields.char('Save As Attachment Prefix', size=128, help='This is the filename of the attachment used to store the printing result. Keep empty to not save the printed reports. You can use a python expression with the object and time variables.'),
+        'attachment': fields.char('Save as Attachment Prefix', size=128, help='This is the filename of the attachment used to store the printing result. Keep empty to not save the printed reports. You can use a python expression with the object and time variables.'),
         'attachment_use': fields.boolean('Reload from Attachment', help='If you check this, then the second time the user prints with same attachment name, it returns the previous report.'),
         'auto': fields.boolean('Custom python parser'),
 
