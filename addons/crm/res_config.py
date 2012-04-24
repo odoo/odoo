9e9c32573b44cad7ae1a6b171e0b3c7828dd37df
Raphael Collet <rco@openerp.com>
2012-04-24 11:51:40 +0200

    [IMP] fetchmail config: remove fields from config wizards, and replace them by a button to open the fetchmail server form
    bzr revid: rco@openerp.com-20120424095140-vn9e95sfn24iwkr7

--- addons/crm/res_config.py
+++ addons/crm/res_config.py
@@ -26,23 +26,12 @@ class crm_configuration(osv.osv_memory):
     _inherit = ['sale.config.settings', 'fetchmail.config.settings']
 
     _columns = {
+        'fetchmail_lead': fields.boolean("Create Leads from Incoming Mails", readonly=True,
+            fetchmail_model='crm.lead', fetchmail_name='Incoming Leads',
+            help="""Allows you to configure your incoming mail server, and create leads from incoming emails."""),
         'module_crm_caldav': fields.boolean("Caldav Synchronization",
             help="""Use protocol caldav to synchronize meetings with other calendar applications (like Sunbird).
                 This installs the module crm_caldav."""),
-        'fetchmail_lead': fields.boolean("Create leads from an email account",
-            fetchmail_model='crm.lead', fetchmail_name='Incoming leads',
-            help="""Allows you to configure your incoming mail server, and create leads from incoming emails."""),
-        'lead_server': fields.char('Server', size=256),
-        'lead_port': fields.integer('Port'),
-        'lead_type': fields.selection([
-                ('pop', 'POP Server'),
-                ('imap', 'IMAP Server'),
-                ('local', 'Local Server'),
-            ], 'Type'),
-        'lead_is_ssl': fields.boolean('SSL/TLS',
-            help="Connections are encrypted with SSL/TLS through a dedicated port (default: IMAPS=993, POP=995)"),
-        'lead_user': fields.char('Username', size=256),
-        'lead_password': fields.char('Password', size=1024),
         'module_import_sugarcrm': fields.boolean("SugarCRM Import",
             help="""Import SugarCRM leads, opportunities, users, accounts, contacts, employees, meetings, phonecalls, emails, project and project tasks data.
                 This installs the module import_sugarcrm."""),
@@ -55,10 +44,6 @@ class crm_configuration(osv.osv_memory):
         'module_google_map': fields.boolean("Google maps on customer",
             help="""Locate customers on Google Map.
                 This installs the module google_map."""),
-    }
-
-    _defaults = {
-        'lead_type': 'pop',
     }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
