feea6393a94fb6ed73f19c450e5db004f21aae26
Alexis de Lattre <alexis@via.ecp.fr>
2012-05-07 20:07:32 +0200

    Meaningfull IDs for fiscal positions and tax mappings Add tax-included taxes for purchases Add property_reserve_and_surplus_account Add some usefull information in the description of the module
    bzr revid: alexis@via.ecp.fr-20120507180732-nquuaasxq1f39hui

--- addons/l10n_fr/__openerp__.py
+++ addons/l10n_fr/__openerp__.py
@@ -37,6 +37,10 @@ This is the module to manage the accounting chart for France in OpenERP.
 
 This module applies to companies based in France mainland. It doesn't apply to companies based in the DOM-TOMs (Guadeloupe, Martinique, Guyane, Réunion, Mayotte, etc...)
 
+This localisation module creates the VAT taxes of type "tax included" for purchases (it is notably required when you use the module "hr_expense"). Beware that these "tax included" VAT taxes are not managed by the fiscal positions provided by this module (because it is complex to manage both "tax excluded" and "tax included" scenarios in fiscal positions).
+
+This localisation module doesn't properly handle the scenario when a France-mainland company sells services to a company based in the DOMs. We could manage it in the fiscal positions, but it would require to differentiate between "product" VAT taxes and "service" VAT taxes. We consider that it is too "heavy" to have this by default in l10n_fr ; companies that sell services to DOM-based companies should update the configuration of their taxes and fiscal positions manually.
+
 Credits: Sistheo, Zeekom, CrysaLEAD, Akretion and Camptocamp.
 """,
     "depends" : ['base_iban', 'account', 'account_chart', 'base_vat', 'l10n_fr_rib'],
