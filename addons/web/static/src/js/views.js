5121d6693f6eae84ab6f18fbe3b7457a43cd5b33
Minh Tran <mit@openerp.com>
2012-05-10 13:18:46 +0200

    Dropdown menu can be dismissed with a click anywhere
    bzr revid: mit@openerp.com-20120510111846-0bribqysds5w125f

--- addons/web/static/src/js/views.js
+++ addons/web/static/src/js/views.js
@@ -701,6 +701,7 @@ instance.web.Sidebar = instance.web.Widget.extend({
         var self = this;
         this._super(this);
         this.redraw();
+        $('html').bind('click', function() { self.$element.find('.oe_dropdown_menu').hide(); });
         this.$element.on('click','.oe_dropdown_toggle',function(event) {
             $(this).parent().find('ul').toggle();
             return false;
