3536f267795e490cdba70df2ba99c04753108613
Turkesh Patel (Open ERP) <tpa@tinyerp.com>
2012-05-14 16:59:14 +0530

    [IMP] improved erro message.
    bzr revid: tpa@tinyerp.com-20120514112914-z1v8uuuv3upf1e1y

--- addons/sale/wizard/sale_make_invoice_advance.py
+++ addons/sale/wizard/sale_make_invoice_advance.py
@@ -113,7 +113,7 @@ class sale_advance_payment_inv(osv.osv_memory):
                     res['account_id'] = account_id
                     if not res.get('account_id'):
                         raise osv.except_osv(_('Configuration Error !'),
-                                _('There is no income account defined '))
+                                _('There is no income account defined as global property.'))
 
                 if not res.get('account_id'):
                     raise osv.except_osv(_('Configuration Error !'),
