9d95952436ce9a79c39b78997554f8720f30eb6b
msh-openerp <msh@tinyerp.com>
2012-05-16 17:39:46 +0530

    [FIX]Refixed the translation issue in import.
    bzr revid: msh@tinyerp.com-20120516120946-bxroqa9tiwhqxbpo

--- addons/web/static/src/js/data_import.js
+++ addons/web/static/src/js/data_import.js
@@ -330,7 +330,7 @@ openerp.web.DataImport = openerp.web.Dialog.extend({
         if (_.isEmpty(duplicates)) {
             this.toggle_import_button(required_valid);
         } else {
-            var $err = $('<div id="msg" style="color: red;">Destination fields should only be selected once, some fields are selected more than once:</div>').insertBefore(this.$element.find('#result'));
+            var $err = $('<div id="msg" style="color: red;">'+_t("Destination fields should only be selected once, some fields are selected more than once:")+'</div>').insertBefore(this.$element.find('#result'));
             var $dupes = $('<dl>').appendTo($err);
             _(duplicates).each(function(elements, value) {
                 $('<dt>').text(value).appendTo($dupes);
