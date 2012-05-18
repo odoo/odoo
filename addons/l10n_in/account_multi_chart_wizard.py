9ee1727eb7c65267726a82337be81cc10b73d0a9
Jagdish Panchal (Open ERP) <jap@tinyerp.com>
2012-05-18 10:29:17 +0530

    [IMP] l10n_in: improve code for tax account
    bzr revid: jap@tinyerp.com-20120518045917-rv2mgpo2tlel5fm2

--- addons/l10n_in/account_multi_chart_wizard.py
+++ addons/l10n_in/account_multi_chart_wizard.py
@@ -26,49 +26,48 @@ from lxml import etree
 class account_multi_charts_wizard(osv.osv_memory):
     _inherit ='wizard.multi.charts.accounts'
     _columns = {
-        'sales_tax': fields.boolean('Sales tax central'),     
-        'vat': fields.boolean('VAT resellers'),
-        'service_tax': fields.boolean('Service tax'),
-        'excise_duty': fields.boolean('Excise duty'),
+        'sales_tax': fields.boolean('Sales tax central', help='If this field is true it allows you use Sales Tax'),     
+        'vat': fields.boolean('VAT resellers',help='If this field is true it allows you use VAT'),
+        'service_tax': fields.boolean('Service tax', help='If this field is true it allows you use Service tax'),
+        'excise_duty': fields.boolean('Excise duty', help='If this field is true it allows you use Excise duty'),
     }    
 
     def execute(self, cr, uid, ids, context=None):
         obj_multi = self.browse(cr, uid, ids[0])
-        acc_template_id= self.pool.get('account.chart.template').search(cr, uid, [('name','=',obj_multi.chart_template_id.name)], context=context)
-        for chart_name in self.pool.get('account.chart.template').browse(cr, uid, acc_template_id,context=context):
-            if obj_multi.chart_template_id.name == chart_name.name:
-                if obj_multi.sales_tax == True:
-                    path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_sales_tax.xml'))
-                    tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
-                    path.close()            
-                if obj_multi.vat == True:
-                    path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_vat.xml'))
-                    tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
-                    path.close()             
-                if obj_multi.service_tax == True:
-                    path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_service.xml'))
-                    tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
-                    path.close()  
-                if obj_multi.excise_duty == True:
-                    path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_excise_duty.xml'))
-                    tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
-                    path.close()  
-            elif obj_multi.chart_template_id.name == chart_name.name:
-                if obj_multi.sales_tax == True:
-                    path = tools.file_open(opj('l10n_in', 'tax', 'privete_sale_tax.xml'))
-                    tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
-                    path.close() 
-                if obj_multi.vat == True:
-                    path = tools.file_open(opj('l10n_in', 'tax', 'private_vat.xml'))
-                    tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
-                    path.close()             
-                if obj_multi.service_tax == True:
-                    path = tools.file_open(opj('l10n_in', 'tax', 'private_service.xml'))
-                    tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
-                    path.close()  
-                if obj_multi.excise_duty == True:
-                    path = tools.file_open(opj('l10n_in', 'tax', 'private_exice_duty.xml'))
-                    tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
-                    path.close()                        
+        if obj_multi.chart_template_id.name == 'Public Firm Chart of Account':
+            if obj_multi.sales_tax == True:
+                path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_sales_tax.xml'))
+                tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
+                path.close()            
+            if obj_multi.vat == True:
+                path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_vat.xml'))
+                tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
+                path.close()             
+            if obj_multi.service_tax == True:
+                path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_service.xml'))
+                tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
+                path.close()  
+            if obj_multi.excise_duty == True:
+                path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_excise_duty.xml'))
+                tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
+                path.close()  
+        elif obj_multi.chart_template_id.name == 'Partnership/Private Firm Chart of Account':
+            if obj_multi.sales_tax == True:
+                path = tools.file_open(opj('l10n_in', 'tax', 'privete_sale_tax.xml'))
+                tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
+                path.close() 
+            if obj_multi.vat == True:
+                path = tools.file_open(opj('l10n_in', 'tax', 'private_vat.xml'))
+                tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
+                path.close()             
+            if obj_multi.service_tax == True:
+                path = tools.file_open(opj('l10n_in', 'tax', 'private_service.xml'))
+                tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
+                path.close()  
+            if obj_multi.excise_duty == True:
+                path = tools.file_open(opj('l10n_in', 'tax', 'private_exice_duty.xml'))
+                tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
+                path.close()                        
         return super(account_multi_charts_wizard, self).execute(cr, uid, ids, context)
+    
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
