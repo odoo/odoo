3a3a10ef465cec721332a067ae2059e2362d93b7
Tejas Tank <tta@openerp.com>
2012-05-18 19:14:39 +0530

    Improvement in hr-attendance absent/present widget with usermenu
    bzr revid: tta@openerp.com-20120518134439-icg8i0uad64ewv4d

--- addons/hr_attendance/static/src/js/attendance.js
+++ addons/hr_attendance/static/src/js/attendance.js
@@ -1,5 +1,5 @@
 openerp.hr_attendance = function(instance) {
-    var QWeb = instance.web.qweb;
+   /** var QWeb = instance.web.qweb;
     instance.hr_attendance.attendancestatus = false,
     instance.hr_attendance.checkstatus = function(){
         attendance = instance.hr_attendance.currentstatus;
@@ -27,9 +27,24 @@ openerp.hr_attendance = function(instance) {
                 self.employee = employee[0];
 	            self.do_update_notifier();
 	        });
+	        var el = $("div.presence_slider");
+            el.click(function(){
+                    if(self.employee.state == 'present'){                        
+                        el.animate({"left": "48px"}, "slow");                        
+                        self.on_click();
+                    }else{                        
+                        el.animate({"left": "-8px"}, "slow");
+                        
+                    }
+             });
         },
         do_update_notifier: function(){
             var self = this;
+            var el = $('img.oe_topbar_avatar');
+
+
+            
+
             this.$element = $(QWeb.render(this.template, {'employee':this.employee}));
             this.$element.click(self.on_click);
             element = $('.oe_attendance_button')
@@ -46,12 +61,12 @@ openerp.hr_attendance = function(instance) {
             var self = this;
             hr_employee = new instance.web.DataSet(self, 'hr.employee');
             hr_employee.call('attendance_action_change', [[self.employee.id]]).done(function(result){
-                window.location.reload();
-                //self.renderElement()
+               //window.location.reload();
+                self.renderElement()
             });
         },
     });
-    instance.web.DataSet.include({
+    /* instance.web.DataSet.include({
         create: function(data, callback, error_callback) {
             if (this._model.name == "hr.attendance"){
     			callback = instance.hr_attendance.callback(callback);
@@ -76,16 +91,55 @@ openerp.hr_attendance = function(instance) {
     		}
             return this._super(method, args, callback, error_callback);
         },
+        callback: function()
+        {
+            this.__super();
+            
+                .....
+        }
 
-    });
+    }); */
 
     instance.web.UserMenu.include({
-        do_update: function() {
+        do_update_attendance: function() {
             var self = this;
-            this._super();
-            this.update_promise.then(function() {
-                instance.hr_attendance.checkstatus();
+            console.log("click.........",self);
+            hr_employee = new instance.web.DataSet(self, 'hr.employee');
+            hr_employee.call('attendance_action_change', [[self.employee.id]]).done(function(result){
+                if (!result) return;
+                if(self.employee.state == 'present')
+                    self.employee.state = 'absent';
+                else
+                    self.employee.state = 'present';
+                self.do_slide(self.employee.state);                
             });
+        },
+        do_slide:function(attendance_state)
+        {
+            if(attendance_state == 'present')
+                this.oe_attendance_slider.animate({"left": "48px"}, "slow");                                               
+            else                        
+                this.oe_attendance_slider.animate({"left": "-8px"}, "slow");
+        },
+        check_attendance: function(){
+            var self = this;
+            var employee = new instance.web.DataSetSearch(self, 'hr.employee', self.session.user_context, [['user_id','=', self.session.uid]]);
+            return employee.read_slice(['id','name','state']).pipe(function(res) {
+                if(_.isEmpty(res)) return;
+                self.employee = res[0];
+                self.do_slide(self.employee.state);
+            });
+        },
+        do_update: function () {
+            this._super();
+            var self = this;
+            var fct = function() {
+                self.oe_attendance_slider = self.$element.find('.oe_attendance_slider');
+                self.oe_attendance_slider.click(self.do_update_attendance);
+                return self.check_attendance();
+                
+            };
+            this.update_promise = this.update_promise.pipe(fct, fct);
         },
     });
 }
