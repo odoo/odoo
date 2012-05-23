2672c529790c48faef2abb771640c6c03d1f60e8
Khushboo Bhatt (Open ERP) <kbh@tinyerp.com>
2012-05-23 17:42:10 +0530

    [IMP]improved method for gratuity,added House rent Allowance rule and food coupon rule,added Contribution Register.
    bzr revid: kbh@tinyerp.com-20120523121210-qy0sv0xq2be1v7gw

--- addons/l10n_in_hr_payroll/l10n_in_hr_payroll.py
+++ addons/l10n_in_hr_payroll/l10n_in_hr_payroll.py
@@ -20,29 +20,31 @@
 ##############################################################################
 
 import time
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 from calendar import isleap
 from dateutil.relativedelta import relativedelta
+
 from osv import fields, osv
 import decimal_precision as dp
 
 class hr_contract_in(osv.osv):
     _inherit = 'hr.contract'
-
+    _description = 'contract'
+    
     _columns = {
         'tds': fields.float('TDS', digits_compute=dp.get_precision('Payroll')),
-        'house_rent_income': fields.float('House Rent Income ', digits_compute=dp.get_precision('Payroll'),help="Income from house property."),
-        'saving_bank_account': fields.float('Saving Bank Account Income ', digits_compute=dp.get_precision('Payroll'),help="Saving income for bank account."),
-        'other_income': fields.float('Other Income ', digits_compute=dp.get_precision('Payroll'),help="Other income of employee."),
-        'short_term_gain':fields.float('Short Term Gain from Share Trading/Equity MFs ', digits_compute=dp.get_precision('Payroll'),help="Stocks/equity mutual funds are sold before one year."),
-        'long_term_gain':fields.float('Long Term Gain from Share Trading/Equity MFs', digits_compute=dp.get_precision('Payroll'),help="Stocks/equity mutual funds are kept for more than a year."),
-        'food_coupon_amount': fields.float('Food Coupons ', digits_compute=dp.get_precision('Payroll'),help="Amount of food coupon per day."),
-        'driver_salay': fields.boolean('Driver Salary',help="If checked,driver get fixed salary per month."),
-        'professional_tax': fields.float('Professional Tax ', digits_compute=dp.get_precision('Payroll'),help="Professional tax deducted from salary"),
-        'leave_avail_dedution': fields.float('Leave Availed Deduction ', digits_compute=dp.get_precision('Payroll'),help="Deduction for emergency leave of employee."),
-        'medical_insurance': fields.float('Medical Insurance', digits_compute=dp.get_precision('Payroll'),help="Deduction towards company provided medical insurance."), 
-        'voluntarily_provident_fund': fields.float('Voluntarily Provident Fund', digits_compute=dp.get_precision('Payroll'),help="VPF computed as percentage.(%)"), 
-        'company_transport': fields.float('Company Provided Transport', digits_compute=dp.get_precision('Payroll'),help="Deduction for company provided transport."), 
+        'house_rent_income': fields.float('House Rent Income ', digits_compute=dp.get_precision('Payroll'), help="Income from house property."),
+        'saving_bank_account': fields.float('Saving Bank Account Income ', digits_compute=dp.get_precision('Payroll'), help="Saving income for bank account."),
+        'other_income': fields.float('Other Income ', digits_compute=dp.get_precision('Payroll'), help="Other income of employee."),
+        'short_term_gain':fields.float('Short Term Gain from Share Trading/Equity MFs ', digits_compute=dp.get_precision('Payroll'), help="Stocks/equity mutual funds are sold before one year."),
+        'long_term_gain':fields.float('Long Term Gain from Share Trading/Equity MFs', digits_compute=dp.get_precision('Payroll'), help="Stocks/equity mutual funds are kept for more than a year."),
+        'food_coupon_amount': fields.float('Food Coupons ', digits_compute=dp.get_precision('Payroll'), help="Amount of food coupon per day."),
+        'driver_salay': fields.boolean('Driver Salary', help=" Allowance for company provided driver."),
+        'professional_tax': fields.float('Professional Tax ', digits_compute=dp.get_precision('Payroll'), help="Professional tax deducted from salary"),
+        'leave_avail_dedution': fields.float('Leave Availed Deduction ', digits_compute=dp.get_precision('Payroll'), help="Deduction for emergency leave of employee."),
+        'medical_insurance': fields.float('Medical Insurance', digits_compute=dp.get_precision('Payroll'), help="Deduction towards company provided medical insurance."), 
+        'voluntary_provident_fund': fields.float('Voluntary Provident Fund', digits_compute=dp.get_precision('Payroll'), help="VPF computed as percentage.(%)"), 
+        'company_transport': fields.float('Company Provided Transport', digits_compute=dp.get_precision('Payroll'), help="Deduction for company provided transport."), 
     }
 
 hr_contract_in()
@@ -50,7 +52,8 @@ hr_contract_in()
 
 class hr_employee(osv.osv):
     _inherit = 'hr.employee'
-    
+    _description = 'employee'
+
     def _compute_year(self, cr, uid, ids, fields, args, context=None):
         """
         @param cr: the current row, from the database cursor,
@@ -63,7 +66,7 @@ class hr_employee(osv.osv):
         c_date = time.strftime('%Y-%m-%d')
         DATETIME_FORMAT = "%Y-%m-%d"
         current_date = datetime.strptime(c_date,DATETIME_FORMAT)
-        for employee in self.browse(cr, uid, ids, context):
+        for employee in self.browse(cr, uid, ids, context=context):
             if employee.join_date:
                 date_start = datetime.strptime(employee.join_date, DATETIME_FORMAT)
                 diffyears = current_date.year - date_start.year
@@ -73,15 +76,15 @@ class hr_employee(osv.osv):
                 total_years = relativedelta(current_date, date_start).years
                 total_months = relativedelta(current_date, date_start).months
                 if total_months < 10:
-                     year_month= float(total_months)/10 + float(total_years)
+                    year_month= float(total_months)/10 + total_years
                 else:
-                    year_month = float(total_months)/100 + float(total_years)
+                    year_month = float(total_months)/100 + total_years
                 res[employee.id] = year_month
         return res
     
     _columns = {
-        'join_date': fields.date('Join Date',help="joining date of employee "),
-        'number_of_year':fields.function(_compute_year, string='No. of Years of Service',type="float",help="Total years of work experience.",store=True),
+        'join_date': fields.date('Join Date', help="joining date of employee "),
+        'number_of_year':fields.function(_compute_year, string='No. of Years of Service', type="float", help="Total years of work experience."),
                 }
 hr_employee()
 
