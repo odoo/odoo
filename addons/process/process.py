0a8051245f4827263106d9760d22aa8e29b405d6
msh-openerp <msh@tinyerp.com>
2012-05-23 19:02:45 +0530

    [FIX]Fixed the issue of color of process node, here relative field's state was not evaluated.
    bzr revid: msh@tinyerp.com-20120523133245-c9kd8hd9q3v7fd4y

--- addons/process/process.py
+++ addons/process/process.py
@@ -118,6 +118,7 @@ class process_process(osv.osv):
             data['active'] = False
             data['gray'] = False
             data['url'] = node.help_url
+            data['model_states'] = node.model_states
 
             # get assosiated workflow
             if data['model']:
@@ -198,6 +199,11 @@ class process_process(osv.osv):
             resource['name'] = refobj.name_get(context)[0][1]
             resource['perm'] = pool.get(ref_model).perm_read(cr, uid, [ref_id], context)[0]
 
+            ref_expr_context = Env(refobj, current_user)
+            try:
+                nodes[nid]['active'] = eval(nodes[nid]['model_states'], ref_expr_context)
+            except:
+                pass 
             for r in relatives:
                 node = nodes[r]
                 if 'res' not in node:
