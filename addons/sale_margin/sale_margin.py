405f134bd3e7bb430d25602b838af439bdf2630c
Amit (OpenERP) <amp@tinyerp.com>
2012-05-25 12:17:09 +0530

    [FIX] sale_margin : correction on super method parameter
    lp bug: https://launchpad.net/bugs/1003948 fixed
    
    bzr revid: amp@tinyerp.com-20120525064709-a918ieuh4zb298zo

--- addons/sale_margin/sale_margin.py
+++ addons/sale_margin/sale_margin.py
@@ -70,7 +70,7 @@ class sale_order(osv.osv):
         return result
 
     def _get_order(self, cr, uid, ids, context=None):
-        return super(self,sale_order)._get_order(cr, uid, ids, context=context)
+        return super(sale_order, self)._get_order(cr, uid, ids, context=context)
 
     _columns = {
         'margin': fields.function(_product_margin, string='Margin', help="It gives profitability by calculating the difference between the Unit Price and Cost Price.", store={
