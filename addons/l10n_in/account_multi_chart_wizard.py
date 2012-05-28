f86c011e90cb81208d6f4739134ce7e3e37c45be
Jagdish Panchal (Open ERP) <jap@tinyerp.com>
2012-05-28 17:31:17 +0530

    [IMP] l10n_in: Improve code and use proper id name in l10n_in_partnership_private_chart.xml
    bzr revid: jap@tinyerp.com-20120528120117-iinpi1vuh2v71dvt

--- addons/l10n_in/account_multi_chart_wizard.py
+++ addons/l10n_in/account_multi_chart_wizard.py
@@ -18,28 +18,25 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from lxml import etree
+
 from osv import fields, osv
 from os.path import join as opj
 import tools
 
-
 class account_multi_charts_wizard(osv.osv_memory):
     _inherit ='wizard.multi.charts.accounts'
     _columns = {
-        'sales_tax': fields.boolean('Sales Tax', help='If this field is true it allows you use Sales Tax'),     
-        'vat': fields.boolean('VAT',help='If this field is true it allows you use VAT'),
-        'service_tax': fields.boolean('Service Tax', help='If this field is true it allows you use Service tax'),
-        'excise_duty': fields.boolean('Excise Duty', help='If this field is true it allows you use Excise duty'),
-        'is_indian_chart': fields.boolean('Flag')
+        'sales_tax': fields.boolean('Sales Tax', help='If this field is true it allows you install Sales Tax'),     
+        'vat': fields.boolean('VAT', help='If this field is true it allows install use VAT'),
+        'service_tax': fields.boolean('Service Tax', help='If this field is true it allows you install Service tax'),
+        'excise_duty': fields.boolean('Excise Duty', help='If this field is true it allows you install Excise duty'),
+        'is_indian_chart': fields.boolean('Indian Chart?')
     }    
 
     def onchange_chart_template_id(self, cr, uid, ids, chart_template_id=False, context=None):
-        res = super(account_multi_charts_wizard, self).onchange_chart_template_id(cr, uid, ids, chart_template_id, context)
-        tax_templ_obj = self.pool.get('account.tax.template')
-        res['value'] = {'complete_tax_set': False, 'sale_tax': False, 'purchase_tax': False}        
+        res = super(account_multi_charts_wizard, self).onchange_chart_template_id(cr, uid, ids, chart_template_id, context=context)
         data = self.pool.get('account.chart.template').browse(cr, uid, chart_template_id, context=context)
-        if data.name in ('India - Chart of Accounts for Public Firm','India - Chart of Accounts for Partnership/Private Firm'):
+        if data.name in ('India - Chart of Accounts for Public Firm', 'India - Chart of Accounts for Partnership/Private Firm'):
             res['value'].update({'is_indian_chart': True})
         else:
             res['value'].update({'is_indian_chart': False}) 
@@ -50,42 +47,43 @@ class account_multi_charts_wizard(osv.osv_memory):
         return res
             
     def execute(self, cr, uid, ids, context=None):
-        obj_multi = self.browse(cr, uid, ids[0])
-        if obj_multi.chart_template_id.name == 'India - Chart of Accounts for Public Firm':
-            if obj_multi.sales_tax:
+        multichart_data = self.browse(cr, uid, ids[0], context=context)
+        if multichart_data.chart_template_id.name == 'India - Chart of Accounts for Public Firm':
+            if multichart_data.sales_tax:
                 path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_sales_tax.xml'))
                 tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
                 path.close()
-            if obj_multi.vat:
+            if multichart_data.vat:
                 path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_vat.xml'))
                 tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
                 path.close()   
-            if obj_multi.service_tax:
+            if multichart_data.service_tax:
                 path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_service.xml'))
                 tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
                 path.close()  
-            if obj_multi.excise_duty:
+            if multichart_data.excise_duty:
                 path = tools.file_open(opj('l10n_in', 'tax', 'public_firm_excise_duty.xml'))
                 tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
                 path.close()  
-        elif obj_multi.chart_template_id.name == 'India - Chart of Accounts for Partnership/Private Firm':
-            if obj_multi.sales_tax:
+        elif multichart_data.chart_template_id.name == 'India - Chart of Accounts for Partnership/Private Firm':
+            if multichart_data.sales_tax:
                 path = tools.file_open(opj('l10n_in', 'tax', 'privete_sale_tax.xml'))
                 tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
                 path.close() 
-            if obj_multi.vat:
+            if multichart_data.vat:
                 path = tools.file_open(opj('l10n_in', 'tax', 'private_vat.xml'))
                 tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
                 path.close()             
-            if obj_multi.service_tax:
+            if multichart_data.service_tax:
                 path = tools.file_open(opj('l10n_in', 'tax', 'private_service.xml'))
                 tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
                 path.close()  
-            if obj_multi.excise_duty:
+            if multichart_data.excise_duty:
                 path = tools.file_open(opj('l10n_in', 'tax', 'private_exice_duty.xml'))
                 tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
-                path.close()   
-        return super(account_multi_charts_wizard, self).execute(cr, uid, ids, context)
+                path.close()  
+        res = super(account_multi_charts_wizard, self).execute(cr, uid, ids, context=context) 
+        return res
 
 account_multi_charts_wizard()
     
--- addons/l10n_in/installer.py
+++ addons/l10n_in/installer.py
@@ -18,9 +18,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 from osv import fields, osv
 from os.path import join as opj
-import netsvc
 import tools
 
 class l10n_installer(osv.osv_memory):
@@ -35,9 +35,9 @@ class l10n_installer(osv.osv_memory):
     }
     
     def execute_simple(self, cr, uid, ids, context=None):
+        res = super(l10n_installer, self).execute_simple(cr, uid, ids, context=context)
         if context is None:
             context = {}
-        fy_obj = self.pool.get('account.fiscalyear')
         for res in self.read(cr, uid, ids, context=context):
             if res['charts'] =='l10n_in' and res['company_type']=='public_company':
                 path = tools.file_open(opj('l10n_in', 'l10n_in_public_firm_chart.xml'))
@@ -46,25 +46,7 @@ class l10n_installer(osv.osv_memory):
             if res['charts'] =='l10n_in' and res['company_type']=='partnership_private_company':
                 path = tools.file_open(opj('l10n_in', 'l10n_in_partnership_private_chart.xml'))
                 tools.convert_xml_import(cr, 'l10n_in', path, {}, 'init', True, None)
-                path.close()      
-            if 'date_start' in res and 'date_stop' in res:
-                f_ids = fy_obj.search(cr, uid, [('date_start', '<=', res['date_start']), ('date_stop', '>=', res['date_stop']), ('company_id', '=', res['company_id'][0])], context=context)
-                if not f_ids:
-                    name = code = res['date_start'][:4]
-                    if int(name) != int(res['date_stop'][:4]):
-                        name = res['date_start'][:4] +'-'+ res['date_stop'][:4]
-                        code = res['date_start'][2:4] +'-'+ res['date_stop'][2:4]
-                    vals = {
-                        'name': name,
-                        'code': code,
-                        'date_start': res['date_start'],
-                        'date_stop': res['date_stop'],
-                        'company_id': res['company_id'][0]
-                    }
-                    fiscal_id = fy_obj.create(cr, uid, vals, context=context)
-                    if res['period'] == 'month':
-                        fy_obj.create_period(cr, uid, [fiscal_id])
-                    elif res['period'] == '3months':
-                        fy_obj.create_period3(cr, uid, [fiscal_id])                         
+                path.close()  
+        return res
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
