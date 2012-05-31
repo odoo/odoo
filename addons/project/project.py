818977e167b98bd0fbd02da0821998a1f9b651b4
DBR (OpenERP) <dbr@tinyerp.com>
2012-05-31 15:16:19 +0530

    [REM]project: remove extra field
    bzr revid: dbr@tinyerp.com-20120531094619-bvi3o0skj6m713u4

--- addons/project/project.py
+++ addons/project/project.py
@@ -1236,7 +1236,6 @@ class account_analytic_account(osv.osv):
     _columns = {
         'use_tasks': fields.boolean('Tasks Management'),
         'company_uom_id': fields.related('company_id', 'project_time_mode_id', type='many2one', relation='product.uom'),
-        'company_uom_id1': fields.related('company_id', 'project_time_mode_id', type='many2one', relation='product.uom'),
         'members': fields.many2many('res.users', 'project_user_rel', 'project_id', 'uid', 'Project Members',
             states={'close':[('readonly',True)], 'cancelled':[('readonly',True)]}),
     }
