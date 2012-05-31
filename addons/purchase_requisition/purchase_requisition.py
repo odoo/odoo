5703f9fd8d07fe5e9e4e92cd2b15a57d9f57d04b
Nimesh (Open ERP) <nco@tinyerp.com>
2012-05-31 14:56:14 +0530

    [add] add open chatter and move Request a quotation button to product tab and add attr in it.
    bzr revid: nco@tinyerp.com-20120531092614-dvmi3famh400o4sd

--- addons/purchase_requisition/purchase_requisition.py
+++ addons/purchase_requisition/purchase_requisition.py
@@ -31,6 +31,7 @@ import decimal_precision as dp
 class purchase_requisition(osv.osv):
     _name = "purchase.requisition"
     _description="Purchase Requisition"
+    _inherit = "mail.thread"
     _columns = {
         'name': fields.char('Requisition Reference', size=32,required=True),
         'origin': fields.char('Source', size=32),
