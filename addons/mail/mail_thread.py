f90e29cad8abd13504859a10de28ce11b8e27836
Thibault Delavallée <tde@openerp.com>
2012-06-01 15:26:44 +0200

    [CLEAN] mail_thread: cleaned code.
    bzr revid: tde@openerp.com-20120601132644-oisqczkahi0iytnk

--- addons/mail/mail_thread.py
+++ addons/mail/mail_thread.py
@@ -413,8 +413,8 @@ class mail_thread(osv.osv):
         """ OpenChatter feature: return thread messages
         """
         msg_ids = self.message_load_ids(cr, uid, ids, limit, offset, domain, ascent, root_ids, context=context)
-        msgs=self.pool.get('mail.message').read(cr, uid, msg_ids, context=context)
-        msgs= sorted(msgs, key=lambda d: (-d['id']))
+        msgs = self.pool.get('mail.message').read(cr, uid, msg_ids, context=context)
+        msgs = sorted(msgs, key=lambda d: (-d['id']))
         return msgs
 
     def get_pushed_messages(self, cr, uid, ids, limit=100, offset=0, msg_search_domain=[], ascent=False, root_ids=[], context=None):
