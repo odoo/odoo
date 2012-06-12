fb41245f7843bdedfea2e621698927b83c079c6b
RGA(OpenERP) <>
2012-06-12 12:49:58 +0530

    [IMP] added form view at header on list view
    bzr revid: rgaopenerp-20120612071958-2wlk6b4gek0xjc8v

--- addons/account/static/src/js/account.js
+++ addons/account/static/src/js/account.js
@@ -1,1 +1,19 @@
-console.log('loading account.....')
+openerp.account = function(instance) {
+    instance.account.extend_view = instance.web.ViewManager.include({
+        start : function(){
+            this._super()
+            this.setup_exended_list_view()
+        },
+        
+        setup_exended_list_view: function(parent){
+            if (this.action && this.action.extended_form_view_id){
+                view_id = this.action.extended_form_view_id[0]
+                var from_view = this.registry.get_object('form');
+                var options = {}
+                var obj_from_view = new from_view(this, this.dataset, view_id, options);
+                obj_from_view.template = 'ExtendedFormView' 
+                obj_from_view.appendTo(this.$element.find('.oe_extended_form_view'))
+            }
+        },
+    })
+}
