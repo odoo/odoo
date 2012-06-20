0c69035459806dc3643723b349bde19951518b0f
Olivier Dony <odo@openerp.com>
2012-06-20 18:27:35 +0200

    [FIX] OPW 575500: voucher: invoice_id must be passed when computing voucher lines, to properly reconcile
    bzr revid: odo@openerp.com-20120620162735-7bkh0hsf60nn9hud

--- addons/account_voucher/wizard/account_statement_from_invoice.py
+++ addons/account_voucher/wizard/account_statement_from_invoice.py
@@ -74,7 +74,8 @@ class account_statement_from_invoice_lines(osv.osv_memory):
                 amount = currency_obj.compute(cr, uid, line.invoice.currency_id.id,
                     statement.currency.id, amount, context=ctx)
 
-            context.update({'move_line_ids': [line.id]})
+            context.update({'move_line_ids': [line.id],
+                            'invoice_id': line.invoice.id})
             result = voucher_obj.onchange_partner_id(cr, uid, [], partner_id=line.partner_id.id, journal_id=statement.journal_id.id, amount=abs(amount), currency_id= statement.currency.id, ttype=(amount < 0 and 'payment' or 'receipt'), date=line_date, context=context)
             voucher_res = { 'type':(amount < 0 and 'payment' or 'receipt'),
                             'name': line.name,
