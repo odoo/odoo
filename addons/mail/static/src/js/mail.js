688ced2225e6491a78c9a100aed8ed7984a7fbc1
Antonin Bourguignon <abo@openerp.com>
2012-06-20 17:54:34 +0200

    [ADD] introduce timeago.js (check the end of mail.js)
    bzr revid: abo@openerp.com-20120620155434-8qos8oooxfwxkm6u

--- addons/mail/static/src/js/mail.js
+++ addons/mail/static/src/js/mail.js
@@ -5,21 +5,21 @@ openerp.mail = function(session) {
     var mail = session.mail = {};
 
     /**
-    * Add records to sorted_comments array
-    * @param {Array} records records from mail.message sorted by date desc
-    * @returns {Object} cs comments_structure: dict
-    *                      cs.model_to_root_ids = {model: [root_ids], }
-    *                      cs.new_root_ids = [new_root_ids]
-    *                      cs.root_ids = [root_ids]
-    *                      cs.msgs = {record.id: record,}
-    *                      cs.tree_struct = {record.id: {
-    *                          'level': record_level in hierarchy, 0 is root,
-    *                          'msg_nbr': number of childs,
-    *                          'direct_childs': [msg_ids],
-    *                          'all_childs': [msg_ids],
-    *                          'for_thread_msgs': [records],
-    *                          'ancestors': [msg_ids], } }
-    */
+     * Add records to sorted_comments array
+     * @param {Array} records records from mail.message sorted by date desc
+     * @returns {Object} cs comments_structure: dict
+     *                      cs.model_to_root_ids = {model: [root_ids], }
+     *                      cs.new_root_ids = [new_root_ids]
+     *                      cs.root_ids = [root_ids]
+     *                      cs.msgs = {record.id: record,}
+     *                      cs.tree_struct = {record.id: {
+     *                          'level': record_level in hierarchy, 0 is root,
+     *                          'msg_nbr': number of childs,
+     *                          'direct_childs': [msg_ids],
+     *                          'all_childs': [msg_ids],
+     *                          'for_thread_msgs': [records],
+     *                          'ancestors': [msg_ids], } }
+     */
     function tools_sort_comments(cs, records, parent_id) {
         var cur_iter = 0; var max_iter = 10; var modif = true;
         while ( modif && (cur_iter++) < max_iter) {
@@ -250,8 +250,10 @@ openerp.mail = function(session) {
                 if (records.length <= self.params.limit) self.display.show_more = false;
                 else { self.display.show_more = true; records.pop(); }
                 
-                //build attachments download urls
+                //build attachments download urls and compute time-relative from dates
                 for (var k in records) {
+                    records[k].timerelative = $.timeago(records[k].date);
+                    
                     if (records[k].attachments) {
                         for (var l in records[k].attachments) {
                             records[k].attachments[l].url = self.session.origin + '/web/binary/saveas?session_id=' + self.session.session_id + '&model=ir.attachment&field=datas&filename_field=datas_fname&id='+records[k].attachments[l].id;
@@ -839,6 +841,164 @@ openerp.mail = function(session) {
             var call_done = this.ds_users.call('message_append_note', [[this.session.uid], 'Tweet', body_text, false, 'comment', 'html']).then(this.proxy('init_and_fetch_comments'));
         },
     });
+
+    /**
+     * Compute relative time from a date (ISO format)
+     * Code from http://timeago.yarp.com/
+     * 
+     * 
+     * 
+     * Timeago is a jQuery plugin that makes it easy to support automatically
+     * updating fuzzy timestamps (e.g. "4 minutes ago" or "about 1 day ago").
+     *
+     * @name timeago
+     * @version 0.11.3
+     * @requires jQuery v1.2.3+
+     * @author Ryan McGeary
+     * @license MIT License - http://www.opensource.org/licenses/mit-license.php
+     *
+     * For usage and examples, visit:
+     * http://timeago.yarp.com/
+     *
+     * Copyright (c) 2008-2012, Ryan McGeary (ryan -[at]- mcgeary [*dot*] org)
+     */
+    (function($) {
+      $.timeago = function(timestamp) {
+        if (timestamp instanceof Date) {
+          return inWords(timestamp);
+        } else if (typeof timestamp === "string") {
+          return inWords($.timeago.parse(timestamp));
+        } else if (typeof timestamp === "number") {
+          return inWords(new Date(timestamp));
+        } else {
+          return inWords($.timeago.datetime(timestamp));
+        }
+      };
+      var $t = $.timeago;
+    
+      $.extend($.timeago, {
+        settings: {
+          refreshMillis: 60000,
+          allowFuture: false,
+          strings: {
+            prefixAgo: null,
+            prefixFromNow: null,
+            suffixAgo: "ago",
+            suffixFromNow: "from now",
+            seconds: "less than a minute",
+            minute: "about a minute",
+            minutes: "%d minutes",
+            hour: "about an hour",
+            hours: "about %d hours",
+            day: "a day",
+            days: "%d days",
+            month: "about a month",
+            months: "%d months",
+            year: "about a year",
+            years: "%d years",
+            wordSeparator: " ",
+            numbers: []
+          }
+        },
+        inWords: function(distanceMillis) {
+          var $l = this.settings.strings;
+          var prefix = $l.prefixAgo;
+          var suffix = $l.suffixAgo;
+          if (this.settings.allowFuture) {
+            if (distanceMillis < 0) {
+              prefix = $l.prefixFromNow;
+              suffix = $l.suffixFromNow;
+            }
+          }
+    
+          var seconds = Math.abs(distanceMillis) / 1000;
+          var minutes = seconds / 60;
+          var hours = minutes / 60;
+          var days = hours / 24;
+          var years = days / 365;
+    
+          function substitute(stringOrFunction, number) {
+            var string = $.isFunction(stringOrFunction) ? stringOrFunction(number, distanceMillis) : stringOrFunction;
+            var value = ($l.numbers && $l.numbers[number]) || number;
+            return string.replace(/%d/i, value);
+          }
+    
+          var words = seconds < 45 && substitute($l.seconds, Math.round(seconds)) ||
+            seconds < 90 && substitute($l.minute, 1) ||
+            minutes < 45 && substitute($l.minutes, Math.round(minutes)) ||
+            minutes < 90 && substitute($l.hour, 1) ||
+            hours < 24 && substitute($l.hours, Math.round(hours)) ||
+            hours < 42 && substitute($l.day, 1) ||
+            days < 30 && substitute($l.days, Math.round(days)) ||
+            days < 45 && substitute($l.month, 1) ||
+            days < 365 && substitute($l.months, Math.round(days / 30)) ||
+            years < 1.5 && substitute($l.year, 1) ||
+            substitute($l.years, Math.round(years));
+    
+          var separator = $l.wordSeparator === undefined ?  " " : $l.wordSeparator;
+          return $.trim([prefix, words, suffix].join(separator));
+        },
+        parse: function(iso8601) {
+          var s = $.trim(iso8601);
+          s = s.replace(/\.\d\d\d+/,""); // remove milliseconds
+          s = s.replace(/-/,"/").replace(/-/,"/");
+          s = s.replace(/T/," ").replace(/Z/," UTC");
+          s = s.replace(/([\+\-]\d\d)\:?(\d\d)/," $1$2"); // -04:00 -> -0400
+          return new Date(s);
+        },
+        datetime: function(elem) {
+          var iso8601 = $t.isTime(elem) ? $(elem).attr("datetime") : $(elem).attr("title");
+          return $t.parse(iso8601);
+        },
+        isTime: function(elem) {
+          // jQuery's `is()` doesn't play well with HTML5 in IE
+          return $(elem).get(0).tagName.toLowerCase() === "time"; // $(elem).is("time");
+        }
+      });
+    
+      $.fn.timeago = function() {
+        var self = this;
+        self.each(refresh);
+    
+        var $s = $t.settings;
+        if ($s.refreshMillis > 0) {
+          setInterval(function() { self.each(refresh); }, $s.refreshMillis);
+        }
+        return self;
+      };
+    
+      function refresh() {
+        var data = prepareData(this);
+        if (!isNaN(data.datetime)) {
+          $(this).text(inWords(data.datetime));
+        }
+        return this;
+      }
+    
+      function prepareData(element) {
+        element = $(element);
+        if (!element.data("timeago")) {
+          element.data("timeago", { datetime: $t.datetime(element) });
+          var text = $.trim(element.text());
+          if (text.length > 0 && !($t.isTime(element) && element.attr("title"))) {
+            element.attr("title", text);
+          }
+        }
+        return element.data("timeago");
+      }
+    
+      function inWords(date) {
+        return $t.inWords(distance(date));
+      }
+    
+      function distance(date) {
+        return (new Date().getTime() - date.getTime());
+      }
+    
+      // fix for IE6 suckage
+      document.createElement("abbr");
+      document.createElement("time");
+    }(jQuery));
 };
 
 // vim:et fdc=0 fdl=0 foldnestmax=3 fdm=syntax:
