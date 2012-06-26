774d4867271fe64fe5d45eb4ff3e949bc97b1d07
Twinkle (OpenERP) <tch@tinyerp.com>
2012-06-26 12:02:25 +0530

    [IMP]Apply css class flow on wizards
    bzr revid: tch@tinyerp.com-20120626063225-o721r3aikxrtx14o

--- openerp/addons/base/module/wizard/base_module_upgrade.py
+++ openerp/addons/base/module/wizard/base_module_upgrade.py
@@ -48,7 +48,7 @@ class base_module_upgrade(osv.osv_memory):
         if not ids:
             res['arch'] = '''<form string="Apply Scheduled Upgrades" version="7.0">
                                 <header>
-                                    <button name="config" string="Start configuration" type="object" icon="gtk-ok" class="oe_highlight_editable"/>
+                                    <button name="config" string="Start configuration" type="object" icon="gtk-ok" class="oe_highlight_on_dirty"/>
                                 </header>
                                 <group>
                                     <separator string="System update completed" colspan="4"/>
