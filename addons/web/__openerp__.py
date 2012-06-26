15565c8a299fb9abe94e9e413f89ec26b8d4c983
Fabien Meghazi <fme@openerp.com>
2012-06-26 12:06:13 +0200

    [IMP] Updated jQuery to version 1.7.2 stable
    bzr revid: fme@openerp.com-20120626100613-d29ayxivda1f4lzf

--- addons/web/__openerp__.py
+++ addons/web/__openerp__.py
@@ -15,7 +15,7 @@
         "static/lib/datejs/parser.js",
         "static/lib/datejs/sugarpak.js",
         "static/lib/datejs/extras.js",
-        "static/lib/jquery/jquery-1.7.2b1.js",
+        "static/lib/jquery/jquery-1.7.2.js",
         "static/lib/jquery.MD5/jquery.md5.js",
         "static/lib/jquery.form/jquery.form.js",
         "static/lib/jquery.validate/jquery.validate.js",
