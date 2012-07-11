53b809b3864ecdd1fe4e59c2dad6597ae274c6f1
Xavier ALT <xal@openerp.com>
2012-07-11 10:09:36 +0200

    [MERGE] l10n_lu: merge PCMN 2011, remove KLUWER (should not be used anymore)
    bzr revid: xal@openerp.com-20120711080936-fjhdtz6gmcmxd9t2

--- addons/l10n_lu/__init__.py
+++ addons/l10n_lu/__init__.py
@@ -20,5 +20,6 @@
 ##############################################################################
 
 import wizard
+import res_company
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
