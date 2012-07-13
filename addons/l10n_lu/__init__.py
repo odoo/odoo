71f8ebc3c45a24164d124a46aee15188de5a91a2
Xavier ALT <xal@openerp.com>
2012-07-13 10:34:33 +0200

    [FIX] l10n_lu: remove unused res_company (badly revived from v6.0 merge)
    bzr revid: xal@openerp.com-20120713083433-ix4d069io71vlhfm

--- addons/l10n_lu/__init__.py
+++ addons/l10n_lu/__init__.py
@@ -20,6 +20,5 @@
 ##############################################################################
 
 import wizard
-import res_company
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
