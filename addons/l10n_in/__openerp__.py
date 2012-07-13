12f3cef6292cf11e20827ce397f9d10aac0b6f35
Ajay Chauhan (OpenERP) <cha@tinyerp.com>
2012-07-13 11:32:55 +0530

    [IMP] : Update Description for modules
    bzr revid: cha@tinyerp.com-20120713060255-izdtqq9196n2emz6

--- addons/l10n_in/__openerp__.py
+++ addons/l10n_in/__openerp__.py
@@ -23,8 +23,8 @@
     "name": "India - Accounting",
     "version": "1.0",
     "description": """
-Indian Accounting : Chart of Account.
-=====================================
+Indian Accounting: Chart of Account.
+====================================
 
 Indian accounting chart and localization.
     """,
