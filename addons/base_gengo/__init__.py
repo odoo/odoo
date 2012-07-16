70e73559d2f91e54215f6a2de779dce8a0f0580d
ajay javiya (OpenERP) <aja@tinyerp.com>
2012-07-16 18:36:51 +0530

    [IMP]: change license in base_gengo module
    bzr revid: aja@tinyerp.com-20120716130651-qk7z3c1uaypjr9z4

--- addons/base_gengo/__init__.py
+++ addons/base_gengo/__init__.py
@@ -22,10 +22,4 @@
 import res_company
 import ir_translation
 
-
-
-
-
-
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- addons/base_gengo/res_company.py
+++ addons/base_gengo/res_company.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    OpenERP, Open Source Business Applications
+#    Copyright (C) 2004-2012 OpenERP S.A. (<http://openerp.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
