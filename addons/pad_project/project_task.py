d708c4ae0cfe7062573d43c74284c1e4a292b325
Fabien Pinckaers <fp@openerp.com>
2012-07-18 01:00:06 +0200

    imp
    bzr revid: fp@openerp.com-20120717230006-dgs6r4f2pm73ancp

--- addons/pad_project/project_task.py
+++ addons/pad_project/project_task.py
@@ -5,7 +5,10 @@ from osv import fields, osv
 class task(osv.osv):
     _name = "project.task"
     _inherit = ["project.task",'pad.common']
-    _pad_url = 'description'
+    _pad_url = 'description_pad'
+    _columns = {
+        'description_pad': fields.char('PAD Description', size=250)
+    }
     _defaults = {
         _pad_url: lambda self, cr, uid, context: self.pad_generate_url(cr, uid, self._name, context),
     }
