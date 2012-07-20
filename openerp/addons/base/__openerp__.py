9bbd6236d91354ec637b46f6a4c44382a312a37f
Raphael Collet <rco@openerp.com>
2012-07-20 10:30:59 +0200

    [IMP] base: add partner categories in partner kanban view
    bzr revid: rco@openerp.com-20120720083059-q1bdcupzuge088jf

--- openerp/addons/base/__openerp__.py
+++ openerp/addons/base/__openerp__.py
@@ -96,6 +96,7 @@
     'installable': True,
     'auto_install': True,
     'certificate': '0076807797149',
-    "css": [ 'static/src/css/modules.css' ],
+    'css': ['static/src/css/modules.css'],
+    'js': ['static/src/js/base.js'],
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
