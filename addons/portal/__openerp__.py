4e3cc2fbf41c643cde7d3129a4034b0de3cc2f46
Antonin Bourguignon <abo@openerp.com>
2012-07-24 18:21:25 +0200

    [IMP] portal module description
    bzr revid: abo@openerp.com-20120724162125-cx1a8n2q9yjopf2a

--- addons/portal/__openerp__.py
+++ addons/portal/__openerp__.py
@@ -26,14 +26,15 @@
     'author' : "OpenERP SA",
     'category': 'Portal',
     'description': """
-This module defines 'portals' to customize the access to your OpenERP database
-for external users.
+Customize acces to your OpenERP database to external users by creating portals.
 
-A portal defines customized user menu and access rights for a group of users
-(the ones associated to that portal).  It also associates user groups to the
-portal users (adding a group in the portal automatically adds it to the portal
-users, etc).  That feature is very handy when used in combination with the
-module 'share'.
+A portal defines a specific user menu and access rights for its members.  This
+menu can ben seen by portal members, anonymous users and any other user that
+have the access to technical features (e.g. the administrator).
+
+It also associates user groups to the portal users (adding a group in the
+portal automatically adds it to the portal users, etc).  That feature is very
+handy when used in combination with the module 'share'.
     """,
     'website': 'http://www.openerp.com',
     'data': [
