9d5336923fb73bd9f50f14c8381eea9e8010a1cf
Atul Patel (OpenERP) <atp@tinyerp.com>
2012-07-27 13:03:21 +0530

    [FIX]: Fix dependency problem
    bzr revid: atp@tinyerp.com-20120727073321-9i655wdu63tdvf8n

--- addons/sale_order_dates/__openerp__.py
+++ addons/sale_order_dates/__openerp__.py
@@ -36,7 +36,7 @@ You can add the following additional dates to a sale order:
     'author': 'OpenERP SA',
     'website': 'http://www.openerp.com',
     'images': ["images/sale_order_dates.jpeg"],
-    'depends': ["sale"],
+    'depends': ["sale_stock"],
     'init_xml': [
     ],
 
