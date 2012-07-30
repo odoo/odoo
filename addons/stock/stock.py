64240ea54b755de3f78353047c387f44610dda51
Amit (OpenERP) <amp@tinyerp.com>
2012-07-30 12:48:23 +0530

    [FIX] stock : Phyical inventory should check_tracking if incoming track true
    lp bug: https://launchpad.net/bugs/1030037 fixed
    
    bzr revid: amp@tinyerp.com-20120730071823-ucsozzcqln2ijgju

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -1590,7 +1590,8 @@ class stock_move(osv.osv):
                    (move.product_id.track_production and move.location_id.usage == 'production') or \
                    (move.product_id.track_production and move.location_dest_id.usage == 'production') or \
                    (move.product_id.track_incoming and move.location_id.usage == 'supplier') or \
-                   (move.product_id.track_outgoing and move.location_dest_id.usage == 'customer') \
+                   (move.product_id.track_outgoing and move.location_dest_id.usage == 'customer') or \
+                   (move.product_id.track_incoming and move.location_id.usage == 'inventory') \
                )):
                 return False
         return True
