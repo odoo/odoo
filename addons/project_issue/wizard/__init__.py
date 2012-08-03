a48431b0bee10a7eeab706f4cd3deff4cb8708ef
Olivier Dony <odo@openerp.com>
2012-08-03 15:14:06 +0200

    [FIX] project.issue: actually use the Reply-To address of the project when sending out messages
    bzr revid: odo@openerp.com-20120803131406-vh5migr9bb5ij29y

--- addons/project_issue/wizard/__init__.py
+++ addons/project_issue/wizard/__init__.py
@@ -1,13 +1,13 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Business Applications
-#    Copyright (c) 2004-2012 OpenERP S.A. <http://openerp.com>
+#    Copyright (c) 2012 OpenERP S.A. <http://openerp.com>
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,8 +19,6 @@
 #
 ##############################################################################
 
-import project_issue
-import report
-import wizard
+import mail_compose_message
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
