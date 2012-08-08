b5fdcedbc95fbdb7feb328a52de2749efe1806c5
Antonin Bourguignon <abo@openerp.com>
2012-08-08 14:08:56 +0200

    [FIX] remove the weird balance_check method in account (what was it doing there ?)
    bzr revid: abo@openerp.com-20120808120856-r5qxpxigaehdbfyt

--- addons/account/account_cash_statement.py
+++ addons/account/account_cash_statement.py
@@ -256,13 +256,6 @@ class account_cash_statement(osv.osv):
             self.write(cr, uid, [statement.id], vals, context=context)
         return True
 
-    def balance_check(self, cr, uid, cash_id, journal_type='bank', context=None):
-        if journal_type == 'bank':
-            return super(account_cash_statement, self).balance_check(cr, uid, cash_id, journal_type, context)
-        if not self._equal_balance(cr, uid, cash_id, context):
-            raise osv.except_osv(_('Error!'), _('The closing balance should be equal to compute balance on the cash register.'))
-        return True
-
     def statement_close(self, cr, uid, ids, journal_type='bank', context=None):
         if journal_type == 'bank':
             return super(account_cash_statement, self).statement_close(cr, uid, ids, journal_type, context)
