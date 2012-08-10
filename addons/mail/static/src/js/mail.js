687e25a7fc9af2121da924047095f6c4fbbd2aee
Minh Tran <mit@openerp.com>
2012-08-10 14:47:54 +0200

    more visual improvements to mail module
    bzr revid: mit@openerp.com-20120810124754-78ui3odg5d5odi20

--- addons/mail/static/src/js/mail.js
+++ addons/mail/static/src/js/mail.js
@@ -857,9 +857,7 @@ openerp.mail = function(session) {
             this.$element.find('button.oe_mail_button_followers').click(function () { self.do_toggle_followers(); });
             if (! this.params.see_subscribers_options) {
                 this.$element.find('button.oe_mail_button_followers').hide(); }
-            this.$element.find('button.oe_mail_button_follow').click(function () { self.do_follow(); })
-                .mouseover(function () { $(this).html('Follow').removeClass('oe_mail_button_mouseout').addClass('oe_mail_button_mouseover'); })
-                .mouseleave(function () { $(this).html('Not following').removeClass('oe_mail_button_mouseover').addClass('oe_mail_button_mouseout'); });
+            this.$element.find('button.oe_mail_button_follow').click(function () { self.do_follow(); });
             this.$element.find('button.oe_mail_button_unfollow').click(function () { self.do_unfollow(); })
                 .mouseover(function () { $(this).html('Unfollow').removeClass('oe_mail_button_mouseout').addClass('oe_mail_button_mouseover'); })
                 .mouseleave(function () { $(this).html('Following').removeClass('oe_mail_button_mouseover').addClass('oe_mail_button_mouseout'); });
