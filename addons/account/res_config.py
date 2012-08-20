aef562c3bec8f35368bca32292b0e9a91e58b898
Rajesh Prajapati (OpenERP) <rpr@tinyerp.com>
2012-08-20 17:03:57 +0530

    [IMP] account : rml_footer1 and rml_footer2 are merged in rml_footer text field
    bzr revid: rpr@tinyerp.com-20120820113357-lzcj72pl3hjh9xts

--- addons/account/res_config.py
+++ addons/account/res_config.py
@@ -43,7 +43,7 @@ class account_config_settings(osv.osv_memory):
             string='Default company currency', help="Main currency of the company."),
         'paypal_account': fields.related('company_id', 'paypal_account', type='char', size=128,
             string='Paypal account', help="Paypal account (email) for receiving online payments (credit card, etc.) If you set a paypal account, the customer  will be able to pay your invoices or quotations with a button \"Pay with  Paypal\" in automated emails or through the OpenERP portal."),
-        'company_footer': fields.related('company_id', 'rml_footer2', type='char', size=250, readonly=True,
+        'company_footer': fields.related('company_id', 'rml_footer', type='text', readonly=True,
             string='Bank accounts on reports will display as followed', help="Bank accounts as printed in the footer of each customer  document. This is for information purpose only, you should configure these bank accounts through the above button \"Configure Bank Accounts\"."),
 
         'has_chart_of_accounts': fields.boolean('Company has a chart of accounts'),
@@ -154,7 +154,7 @@ class account_config_settings(osv.osv_memory):
             'expects_chart_of_accounts': company.expects_chart_of_accounts,
             'currency_id': company.currency_id.id,
             'paypal_account': company.paypal_account,
-            'company_footer': company.rml_footer2,
+            'company_footer': company.rml_footer,
             'has_chart_of_accounts': has_chart_of_accounts,
             'has_fiscal_year': bool(fiscalyear_count),
             'chart_template_id': False,
