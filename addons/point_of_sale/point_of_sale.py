7d120eb1d10385849be55ad70ad0992329077f2a
Stephane Wirtel <stw@openerp.com>
2012-08-29 17:46:34 +0200

    [FIX] point_of_sale: Set the right balance end for a bank statement [FIX] point_of_sale: Show/Hide some tabs
    bzr revid: stw@openerp.com-20120829154634-ko3v2bi6oephr04e

--- addons/point_of_sale/point_of_sale.py
+++ addons/point_of_sale/point_of_sale.py
@@ -396,6 +396,9 @@ class pos_session(osv.osv):
                         'account_id': account_id
                     }, context=context)
 
+                if st.journal_id.type == 'bank':
+                    st.write({'balance_end_real' : st.balance_end})
+
                 getattr(st, 'button_confirm_%s' % st.journal_id.type)(context=context)
         self._confirm_orders(cr, uid, ids, context=context)
         self.write(cr, uid, ids, {'state' : 'closed'}, context=context)
