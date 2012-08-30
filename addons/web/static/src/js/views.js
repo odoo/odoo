060baf72f947c119bb10c45e902ba4c307383add
Fabien Meghazi <fme@openerp.com>
2012-08-30 11:48:11 +0200

    [FIX] TranslateDialog#start() should return a deferred
    bzr revid: fme@openerp.com-20120830094811-p88qbd8tiesu8ry2

--- addons/web/static/src/js/views.js
+++ addons/web/static/src/js/views.js
@@ -1077,14 +1077,13 @@ instance.web.TranslateDialog = instance.web.Dialog.extend({
     start: function() {
         var self = this;
         this._super();
-        $.when(this.languages_loaded).then(function() {
+        return $.when(this.languages_loaded).then(function() {
             self.$el.html(instance.web.qweb.render('TranslateDialog', { widget: self }));
             self.$fields_form = self.$el.find('.oe_translation_form');
             self.$fields_form.find('.oe_trad_field').change(function() {
                 $(this).toggleClass('touched', ($(this).val() != $(this).attr('data-value')));
             });
         });
-        return this;
     },
     on_languages_loaded: function(langs) {
         this.languages = langs;
