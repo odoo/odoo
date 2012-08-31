31025afe28c289daa5a0c58096c75da316182800
Stephane Wirtel <stw@openerp.com>
2012-08-31 16:14:15 +0200

    [FIX] base: Remove the default value for the image
    bzr revid: stw@openerp.com-20120831141415-gqsvhh08fum874gn

--- openerp/addons/base/res/res_users.py
+++ openerp/addons/base/res/res_users.py
@@ -251,7 +251,6 @@ class res_users(osv.osv):
         'company_id': _get_company,
         'company_ids': _get_companies,
         'groups_id': _get_group,
-        'image': lambda self, cr, uid, context: self._get_default_image(cr, uid, context),
     }
 
     def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
