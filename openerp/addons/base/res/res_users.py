df107e19ec3e51827721f96557dd97927c23126d
Turkesh Patel (Open ERP) <tpa@tinyerp.com>
2012-09-03 14:16:25 +0530

    [FIX] pass context in search and name_get method inside name_search.
    bzr revid: tpa@tinyerp.com-20120903084625-nnkl9k2ljaauv4bh

--- openerp/addons/base/res/res_users.py
+++ openerp/addons/base/res/res_users.py
@@ -310,10 +310,10 @@ class res_users(osv.osv):
             context={}
         ids = []
         if name:
-            ids = self.search(cr, user, [('login','=',name)]+ args, limit=limit)
+            ids = self.search(cr, user, [('login','=',name)]+ args, limit=limit, context=context)
         if not ids:
-            ids = self.search(cr, user, [('name',operator,name)]+ args, limit=limit)
-        return self.name_get(cr, user, ids)
+            ids = self.search(cr, user, [('name',operator,name)]+ args, limit=limit, context=context)
+        return self.name_get(cr, user, ids, context=context)
 
     def copy(self, cr, uid, id, default=None, context=None):
         user2copy = self.read(cr, uid, [id], ['login','name'])[0]
