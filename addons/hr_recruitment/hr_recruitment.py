daf36804f928be57a7d9c96d6f0dc0b53fb4bebb
Pinakin Nayi (OpenERP) <pna@tinyerp.com>
2012-09-03 10:35:25 +0530

    [IMP]rename categories to Tags
    bzr revid: pna@tinyerp.com-20120903050525-ousyvlyo0a67byb1

--- addons/hr_recruitment/hr_recruitment.py
+++ addons/hr_recruitment/hr_recruitment.py
@@ -193,7 +193,7 @@ class hr_applicant(base_stage, osv.Model):
                       When the case is over, the state is set to \'Done\'.\
                       If the case needs to be reviewed then the state is \
                       set to \'Pending\'.'),
-        'categ_ids': fields.many2many('hr.applicant_category', string='Categories'),
+        'categ_ids': fields.many2many('hr.applicant_category', string='Tags'),
         'company_id': fields.many2one('res.company', 'Company'),
         'user_id': fields.many2one('res.users', 'Responsible'),
         # Applicant Columns
