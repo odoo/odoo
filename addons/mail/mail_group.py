c52ab94960f0346ff82e2ecf72cda67d667d9f6f
Thibault Delavallée <tde@openerp.com>
2012-09-04 11:28:20 +0200

    [REVIEW] addons: small cleanups nex to a diff read. Some improvements will follow, this commit is about small cleanups.
    bzr revid: tde@openerp.com-20120904092820-56zgwpkqfpd7as5k

--- addons/mail/mail_group.py
+++ addons/mail/mail_group.py
@@ -28,12 +28,8 @@ from osv import fields
 from tools.translate import _
 
 class mail_group(osv.Model):
-    """
-    A mail_group is a collection of users sharing messages in a discussion
-    group. Group users are users that follow the mail group, using the
-    subscription/follow mechanism of OpenSocial. A mail group has nothing
-    in common with res.users.group.
-    """
+    """ A mail_group is a collection of users sharing messages in a discussion
+        group. The group mechanics are based on the followers. """
     _description = 'Discussion group'
     _name = 'mail.group'
     _inherit = ['mail.thread']
