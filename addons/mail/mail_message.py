9b9bdae9e008e930eaf1cf208b90f66bdbdf4e7d
Thibault Delavallée <tde@openerp.com>
2012-09-06 15:42:01 +0200

    [IMP] composer: attachments are attached to the mail.compose.message record, not the related document anymore. This way, when deleting the mail.compose.message, attachments will be unlinked. Added deletion cascaded to res_model and res_id = 0, because of attachments added on a not-already-saved composer. BTW, cleaned mail/__openerp__.py.
    bzr revid: tde@openerp.com-20120906134201-8845xov0mjbxh8mr

--- addons/mail/mail_message.py
+++ addons/mail/mail_message.py
@@ -306,11 +306,12 @@ class mail_message(osv.Model):
 
     def unlink(self, cr, uid, ids, context=None):
         # cascade-delete attachments that are directly attached to the message (should only happen
-        # for mail.messages that act as parent for a standalone mail.mail record.
+        # for mail.messages that act as parent for a standalone mail.mail record). Also remove
+        # attachment with a void id, because this can happen with the composer.
         attachments_to_delete = []
-        for mail in self.browse(cr, uid, ids, context=context):
-            for attach in mail.attachment_ids:
-                if attach.res_model == self._name and attach.res_id == mail.id:
+        for message in self.browse(cr, uid, ids, context=context):
+            for attach in message.attachment_ids:
+                if attach.res_model == self._name and (attach.res_id == message.id or attach.res_id == 0):
                     attachments_to_delete.append(attach.id)
         if attachments_to_delete:
             self.pool.get('ir.attachment').unlink(cr, uid, attachments_to_delete, context=context)
--- addons/mail/wizard/mail_compose_message.py
+++ addons/mail/wizard/mail_compose_message.py
@@ -262,6 +262,8 @@ class mail_compose_message(osv.TransientModel):
                     post_values.update(email_dict)
                 # post the message
                 active_model_pool.message_post(cr, uid, [res_id], type='comment', context=context, **post_values)
+            # post process: update attachments, because id is not necessarily known when adding attachments in Chatter
+            self.pool.get('ir.attachment').write(cr, uid, [attach.id for attach in wizard.attachment_ids], {'res_id': wizard.id}, context=context)
 
         return {'type': 'ir.actions.act_window_close'}
 
