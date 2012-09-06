6f888ccb5e62753465fb2c04cc2ab16cf1824dbc
Vidhin Mehta (OpenERP) <vme@tinyerp.com>
2012-09-06 15:14:04 +0530

    [IMP]Set title again which is replace by on_record_loaded method.
    bzr revid: vme@tinyerp.com-20120906094404-ufxfcls2f6vntnob

--- addons/board/static/src/js/dashboard.js
+++ addons/board/static/src/js/dashboard.js
@@ -24,6 +24,10 @@ instance.web.form.DashBoard = instance.web.form.FormWidget.extend({
             scroll: false
         }).bind('sortstop', self.do_save_dashboard);
 
+        var old_title = this.__parentedParent.get('title');
+        this.__parentedParent.on_record_loaded.add_last(function(){
+            self.__parentedParent.set({ 'title' : old_title});
+        });
         // Events
         this.$el.find('.oe_dashboard_link_reset').click(this.on_reset);
         this.$el.find('.oe_dashboard_link_change_layout').click(this.on_change_layout);
