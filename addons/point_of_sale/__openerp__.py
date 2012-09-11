9899e87dd0439ff747540c9c03ed8d3044a80a72
Rucha (Open ERP) <rpa@tinyerp.com>
2012-09-11 15:39:41 +0530

    [IMP]: point_of_sale: Improved module description for point_of_sale
    bzr revid: rpa@tinyerp.com-20120911100941-3r0a3q3ojry8si4h

--- addons/point_of_sale/__openerp__.py
+++ addons/point_of_sale/__openerp__.py
@@ -27,17 +27,24 @@
     'sequence': 6,
     'summary': 'Touchscreen Interface for Shops',
     'description': """
-This module provides a quick and easy sale process.
-===================================================
+Quick and Easy sale process
+============================
+This module allows you to manage your shop sales very easily with fully web based touchscreen interface.
+It is compatible with all PC tablets and iPad, offering multiple payment methods. 
 
-Main features:
---------------
-    * Fast encoding of the sale
-    * Allow to choose one payment mode (the quick way) or to split the payment between several payment mode
-    * Computation of the amount of money to return
-    * Create and confirm picking list automatically
-    * Allow the user to create invoice automatically
-    * Allow to refund former sales
+Product selection can be done in several ways: 
+
+* Using a barcode reader
+* Browsing through categories of products or via a text search.
+
+Main Features
+-------------
+* Fast encoding of the sale
+* Allow to choose one payment mode (the quick way) or to split the payment between several payment mode
+* Computation of the amount of money to return
+* Create and confirm picking list automatically
+* Allow the user to create invoice automatically
+* Allow to refund former sales
     """,
     'author': 'OpenERP SA',
     'images': ['images/cash_registers.jpeg', 'images/pos_analysis.jpeg','images/register_analysis.jpeg','images/sale_order_pos.jpeg','images/product_pos.jpeg'],
