651b5c2cdc327e833a8f04bbea2f683b777b90c4
Pinakin Nayi (OpenERP) <pna@tinyerp.com>
2012-09-12 18:05:54 +0530

    [IMP]hr_timesheet:journal_id
    bzr revid: pna@tinyerp.com-20120912123554-08yyv9xnm47qukcd

--- addons/hr_timesheet/hr_timesheet.py
+++ addons/hr_timesheet/hr_timesheet.py
@@ -141,7 +141,10 @@ class hr_analytic_timesheet(osv.osv):
             emp = emp_obj.browse(cr, uid, emp_id[0], context=context)
             if emp.journal_id:
                 return emp.journal_id.id
-        return False
+            else :
+                raise osv.except_osv(_('Warning!'), _('No \'Analytic Journal\' is defined for this employee. \nDefine an employee for the selected user and assign an \'Analytic Journal\'!'))
+        if not emp_id :
+            raise osv.except_osv(_('Warning!'), _('Please create an employee for this user, using the menu: Human Resources > Employees.'))
 
 
     _defaults = {
