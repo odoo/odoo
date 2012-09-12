c5b72854a7e09de15a2819c10f6b3f6a22795e2b
Rucha (Open ERP) <rpa@tinyerp.com>
2012-09-12 16:00:46 +0530

    [IMP]: project_gtd: Improved module description for project_gtd
    bzr revid: rpa@tinyerp.com-20120912103046-3gmhha28jue82d2i

--- addons/project_gtd/__openerp__.py
+++ addons/project_gtd/__openerp__.py
@@ -25,25 +25,17 @@
     'version': '1.0',
     'category': 'Project Management',
     'sequence': 20,
-    'summary': 'Personal Tasks',
+    'summary': 'Personal Tasks, Contexts, Timeboxes',
     'description': """
-This module implements all concepts defined by the Getting Things Done methodology.
-===================================================================================
+Implement concepts of "Getting Things Done" methodology 
+========================================================
+This module implements a simple personnal Todo list based on tasks. It adds in the project application an editable list of tasks simplified to the minimum required fields.
 
-This module implements a simple personnal Todo list based on tasks. It adds in
-the project application an editable list of tasks simplified to the minimum
-required fields.
+The todo list is based on the GTD methodology. This world-wide used methodology is used for personal time management improvement.
 
-The todo list is based on the GTD methodology. This world-wide used methodology
-is used for personal time management improvement.
+Getting Things Done (commonly abbreviated as GTD) is an action management method created by David Allen, and described in a book of the same name.
 
-Getting Things Done (commonly abbreviated as GTD) is an action management
-method created by David Allen, and described in a book of the same name.
-
-GTD rests on the principle that a person needs to move tasks out of the mind by
-recording them externally. That way, the mind is freed from the job of
-remembering everything that needs to be done, and can concentrate on actually
-performing those tasks.
+GTD rests on the principle that a person needs to move tasks out of the mind by recording them externally. That way, the mind is freed from the job of remembering everything that needs to be done, and can concentrate on actually performing those tasks.
     """,
     'author': 'OpenERP SA',
     'images': ['images/project_gtd.jpeg'],
