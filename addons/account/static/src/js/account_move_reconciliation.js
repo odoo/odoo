ff4c8c0c60e2e336e6dbbe1472e8ace47c5c2da3
niv-openerp <nicolas.vanhoren@openerp.com>
2012-09-13 14:00:00 +0200

    corrected javascript crap
    bzr revid: nicolas.vanhoren@openerp.com-20120913120000-8lttbfrbgekz32ui

--- addons/account/static/src/js/account_move_reconciliation.js
+++ addons/account/static/src/js/account_move_reconciliation.js
@@ -5,43 +5,44 @@ instance.web.views.add('form_clone', 'instance.account.extend_form_view');
  
 instance.account.extend_viewmanager = instance.web.ViewManagerAction.include({
     start : function(){
-        this._super()
-        if(this.action.context && this.action.context.extended_view_id && this.action.context.extended_model)
+        this._super();
+        if(this.action.context && this.action.context.extended_view_id && this.action.context.extended_model) {
             this.setup_exended_form_view(this.action.context.extended_model, this.action.context.extended_view_id);
+        }
     }, 
     setup_exended_form_view: function(view_model, view_id){
         var self = this;
-        from_view = this.registry.get_object('form_clone');
+        var from_view = this.registry.get_object('form_clone');
         this.dataset_form = new instance.web.DataSetSearch(this, view_model, this.action.context, this.action.domain);
         this.dataset_loaded  = this.dataset_form.read_slice();
-        obj_from_view = new from_view(self, self.dataset_form, view_id, options={});
+        var obj_from_view = new from_view(self, self.dataset_form, view_id, {});
         obj_from_view.template = 'ExtendedFormView';
-        view_form = obj_from_view.appendTo(self.$el.find('.oe_extended_form_view'));
+        var view_form = obj_from_view.appendTo(self.$el.find('.oe_extended_form_view'));
         $.when(view_form, this.dataset_loaded).then(function() {
                 obj_from_view.on_pager_action('first');
         });
     } 
-    
-})
+});
+
 instance.account.extend_form_view = instance.web.FormView.extend({
     on_loaded: function(data) {
          this._super.apply(this,arguments);
-         var self = this
-         this.$el.find(".oe_reconcile").on('click', this.do_reconcilation)
-         this.$el.find(".oe_nothing_to_reconcile").on('click', this.do_nothing_to_reconcile)
+         var self = this;
+         this.$el.find(".oe_reconcile").on('click', this.do_reconcilation);
+         this.$el.find(".oe_nothing_to_reconcile").on('click', this.do_nothing_to_reconcile);
          this.$el.on('click','a[data-pager-action]',function() {
             var action = $(this).data('pager-action');
             self.on_pager_action(action);
         });
     },
     do_reconcilation:function(event){
-        var self = this
+        var self = this;
         if (!self.datarecord.id){
             return false;
         }
-        var list_view = this.getParent().views['list'].controller
-        ids = list_view.get_selected_ids()
-        if (ids.length == 0) {
+        var list_view = this.getParent().views['list'].controller;
+        var ids = list_view.get_selected_ids();
+        if (ids.length === 0) {
             instance.web.dialog($("<div />").text(_t("You must choose at least one record.")), { title: _t("Warning"), modal: true });
             return false;
         }
@@ -72,6 +73,8 @@ instance.account.extend_form_view = instance.web.FormView.extend({
         if (!self.datarecord.id){
             return false;
         }
+        // how do you want this to not fail ???
+        var event = null;
         this.dataset.call(event.target.name, [[self.datarecord.id], self.dataset.context]).then(function() {
             self.dataset.read_slice().done(function(){
                 self.on_pager_action('next');
@@ -80,8 +83,8 @@ instance.account.extend_form_view = instance.web.FormView.extend({
     },
     
     do_update_pager: function(hide_index) {
-        var index = this.dataset.index + 1
-        if (this.dataset.ids.length == 0)
+        var index = this.dataset.index + 1;
+        if (this.dataset.ids.length === 0)
             index = 0;
         index = hide_index ? '-' : index;
         this.$el.find('span.oe_pager_index_extend').html(index).end()
@@ -96,10 +99,10 @@ instance.account.extend_form_view = instance.web.FormView.extend({
     },
     
     on_pager_action: function(action) {
-        var self = this
+        var self = this;
         
-        if (this.dataset.ids.length == 0){
-            self.datarecord = {}
+        if (this.dataset.ids.length === 0){
+            self.datarecord = {};
             _(this.fields).each(function (field, f) {
                 field.set_value(self.datarecord[f] || false);
             });
@@ -113,6 +116,6 @@ instance.account.extend_form_view = instance.web.FormView.extend({
             });
         }
     },
-  })
+  });
 
-}
+};
