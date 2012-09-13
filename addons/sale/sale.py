9eb850d55274491301d56531cf472f39cbf0b30a
Harry (OpenERP) <hmo@tinyerp.com>
2012-09-13 18:48:08 +0530

    [FIX] sale: should not create the invoice directly, make as on demands
    bzr revid: hmo@tinyerp.com-20120913131808-rr6bfu9chszdn533

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -758,7 +758,7 @@ class sale_order(osv.osv):
         for o in self.browse(cr, uid, ids):
             if not o.order_line:
                 raise osv.except_osv(_('Error!'),_('You cannot confirm a sale order which has no line.'))
-            if (o.order_policy == 'manual'):
+            if (o.order_policy == 'manual' or self.test_no_product(cr, uid, [o.id])):
                 self.write(cr, uid, [o.id], {'state': 'manual', 'date_confirm': fields.date.context_today(self, cr, uid, context=context)})
             else:
                 self.write(cr, uid, [o.id], {'state': 'progress', 'date_confirm': fields.date.context_today(self, cr, uid, context=context)})
