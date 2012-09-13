01f6ac8d16080258fdd403b33c0d2147af1750f4
Thibault Delavallée <tde@openerp.com>
2012-09-13 12:34:01 +0200

    [FIX] portal invite wizard: generated URL improvement (partner_id and portal_group_id should be correct now).
    bzr revid: tde@openerp.com-20120913103401-waepkmjq8flsu13j

--- addons/portal/mail_mail.py
+++ addons/portal/mail_mail.py
@@ -43,7 +43,9 @@ class mail_mail_portal(osv.Model):
             :param partner: browse_record of the specific recipient partner
         """
         if partner:
-            url = self._generate_portal_url(cr, uid, partner.id, 12, 1234, context=context)
+            portal_ref = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'portal', 'portal')
+            portal_id = portal_ref and portal_ref[1] or False
+            url = self._generate_portal_url(cr, uid, partner.id, portal_id, 1234, context=context)
             body = tools.append_content_to_html(mail.body_html, url)
             return body
         else:
