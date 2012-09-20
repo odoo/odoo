586914863f88e654662b8b5e1ba41d3258fdb2c7
Cedric Snauwaert <csn@openerp.com>
2012-09-20 15:11:32 +0200

    [MERGE]add object brand
    bzr revid: csn@openerp.com-20120920131132-i3eq3et5y3x0cyuk

--- addons/fleet/fleet.py
+++ addons/fleet/fleet.py
@@ -9,14 +9,21 @@ class fleet_vehicle_model(osv.Model):
 
 class fleet_vehicle_model(osv.Model):
     _name = 'fleet.vehicle.model'
-    _description = '...'
+    _description = ''
 
     _columns = {
+        'brand' : fields.many2one('fleet.vehicle.model.brand', 'brand', required=True),
         'type' : fields.many2one('fleet.vehicle.type', 'Vehicle Type', required=True),
-        'brand' : fields.char('Brand', size=32, required=True),
         'name' : fields.char('Name', size=32, required=True),
         'make' : fields.char('Make',size=32,required=True),
         'partner_id': fields.many2many('res.partner','fleet_vehicle_model_vendors','model_id', 'partner_id',string='Vendors',required=False),
+    }
+
+class fleet_vehicle_model_brand(osv.Model):
+    _name = 'fleet.vehicle.model.brand'
+    _description = 'Brand model of the vehicle'
+    _columns = {
+        'name' : fields.char('Brand Name',size=32, required=True),
     }
 
 class fleet_vehicle(osv.Model):
