3d2607669f964f0ab5b1acdf1a548061a3595e7d
Denis Ledoux dle@openerp.com <>
2012-09-24 14:51:11 +0200

    [ADD][FIX]Add services logs and fix relation between driver and his vehicles
    bzr revid: dle@openerp.com-20120924125111-7g3wheu0689pw4nv

--- addons/fleet/fleet.py
+++ addons/fleet/fleet.py
@@ -78,14 +78,13 @@ class fleet_vehicle(osv.Model):
         'model_id' : fields.many2one('fleet.vehicle.model', 'Model', required=True, help='Model of the vehicle'),
         'log_ids' : fields.one2many('fleet.vehicle.log', 'vehicle_id', 'Logs'),
         'log_fuel' : fields.one2many('fleet.vehicle.log.fuel','vehicle_id', 'Fuel Logs'),
+        'log_services' : fields.one2many('fleet.vehicle.log.services','vehicle_id', 'Services Logs'),
         'acquisition_date' : fields.date('Acquisition date', required=False, help='Date when the vehicle has been bought'),
         'acquisition_price' : fields.integer('Price', help='Price of the bought vehicle'),
         'color' : fields.char('Color',size=32, help='Color of the vehicle'),
         'status' : fields.char('Status',size=32, help='Status of the vehicle (in repair, active, ...)'),
         'location' : fields.char('Location',size=32, help='Location of the vehicle (garage, ...)'),
         'doors' : fields.integer('Number of doors', help='Number of doors of the vehicle'),
-
-        'next_repair_km' : fields.integer('Next Repair Km'),
 
         'transmission' : fields.selection([('manual', 'Manual'),('automatic','Automatic')], 'Transmission', help='Transmission Used by the vehicle',required=False),
         'fuel_type' : fields.selection([('gasoline', 'Gasoline'),('diesel','Diesel'),('electric','Electric'),('hybrid','Hybrid')], 'Fuel Type', help='Fuel Used by the vehicle',required=False),
@@ -179,17 +178,33 @@ class fleet_vehicle_log_insurance(osv.Model):
         'description' : fields.text('Description'),
     }
 
+class service_type(osv.Model):
+    _name = 'fleet.service.type'
+    _columns = {
+        'name': fields.char('Name', required=True, translate=True),
+    }
+
 class fleet_vehicle_log_services(osv.Model):
+    def _compute_complete_service(self, cr, uid, ids, field, arg, context=None):
+        return dict(
+            (record.id, " ".join(service.name for service in record.service_ids))
+            for record in self.browse(cr, uid, ids, context=context)
+        )
     _inherit = 'fleet.vehicle.log'
     _name = 'fleet.vehicle.log.services'
     _columns = {
-        'description' : fields.text('Description'),
+        'vehicle_id' :fields.many2one('fleet.vehicle', 'Vehicle', required=True),
+        'vendor_id' :fields.many2one('res.partner', 'Vendor', domain="[('supplier','=',True)]"),
+        'amount' :fields.float('Amount', help="Total cost of the service"),
+        'reference' :fields.char('Reference',size=128),
+        'service_ids' :fields.many2many('fleet.service.type','vehicle_service_type_rel','vehicle_service_type_id','service_id','Services completed'),
+        'complete_service': fields.function(_compute_complete_service, type="char", string='Services completed'),
     }
 
 class hr_employee(osv.Model):
     _inherit = 'hr.employee'
 
     _columns = {
-        'vehicle_id' : fields.many2one('fleet.vehicle', 'Vehicle', required=True),
+        'vehicle_id' : fields.one2many('fleet.vehicle','driver', 'Vehicle',type="char"),
         'log_ids' : fields.one2many('fleet.vehicle.log', 'employee_id', 'Logs'),
     }
