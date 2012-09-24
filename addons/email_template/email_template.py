f7b90af93ad6d97444d56ec81ee9d3ce959299e4
Antonin Bourguignon <abo@openerp.com>
2012-09-24 17:56:00 +0200

    [FIX] forgot to remove })
    bzr revid: abo@openerp.com-20120924155600-dnaanyupa5jm0yre

--- addons/email_template/email_template.py
+++ addons/email_template/email_template.py
@@ -217,7 +217,6 @@ class email_template(osv.osv):
         default = default.copy()
         default['name'] = template.name + _('(copy)')
         default.update(ref_ir_act_window=False, ref_ir_value=False)
-        })
         return super(email_template, self).copy(cr, uid, id, default, context)
 
     def build_expression(self, field_name, sub_field_name, null_value):
