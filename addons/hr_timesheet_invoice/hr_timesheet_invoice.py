b478be2675a4709b17d94cbb7663fc754fc5e31a
Ajay Chauhan (OpenERP) <cha@tinyerp.com>
2012-09-27 17:49:48 +0530

    [IMP] hr_timesheet_sheet, hr_timesheet_invoice : made changes for warning message
    bzr revid: cha@tinyerp.com-20120927121948-iqrtxdijiknwggh6

--- addons/hr_timesheet_invoice/hr_timesheet_invoice.py
+++ addons/hr_timesheet_invoice/hr_timesheet_invoice.py
@@ -135,6 +135,8 @@ class account_analytic_line(osv.osv):
     def _default_general_account(self, cr, uid, context=None):
         proxy = self.pool.get('hr.employee')
         record_ids = proxy.search(cr, uid, [('user_id', '=', uid)], context=context)
+        if not len(record_ids):
+            raise osv.except_osv(_('Error!'), _('Please create an employee and associate it with this user.'))
         employee = proxy.browse(cr, uid, record_ids[0], context=context)
         if employee.product_id and employee.product_id.property_account_income:
             return employee.product_id.property_account_income.id
