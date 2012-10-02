eeb08e163edbf7e33cd2b9ea8af165457e574679
Arnaud Pineux <api@openerp.com>
2012-10-02 09:21:49 +0200

    [ADD] Lunch module
    bzr revid: api@openerp.com-20121002072149-9gcyy64wap5z7b2h

--- addons/lunch/__init__.py
+++ addons/lunch/__init__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #    
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2004-2012 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,7 +20,4 @@
 ##############################################################################
 
 import lunch
-import wizard
-import report
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
+import partner
--- addons/lunch/__openerp__.py
+++ addons/lunch/__openerp__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2004-2012 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -22,32 +22,21 @@
 {
     'name': 'Lunch Orders',
     'author': 'OpenERP SA',
-    'version': '0.1',
-    'depends': ['base_tools'],
+    'version': '0.2',
+    'depends': ['base'],
     'category' : 'Tools',
     'description': """
 The base module to manage lunch.
 ================================
 
-keep track for the Lunch Order, Cash Moves, CashBox, Product. Apply Different
+keep track for the Lunch Order, Cash Moves and Product. Apply Different
 Category for the product.
     """,
-    'data': [
-        'security/lunch_security.xml',
-        'security/ir.model.access.csv',
-        'wizard/lunch_order_cancel_view.xml',
-        'wizard/lunch_order_confirm_view.xml',
-        'wizard/lunch_cashbox_clean_view.xml',
-        'lunch_view.xml',
-        'lunch_report.xml',
-        'report/report_lunch_order_view.xml',
-        'lunch_installer_view.xml'
-    ],
-    'demo': ['lunch_demo.xml'],
-    'test': ['test/test_lunch.yml', 'test/lunch_report.yml'],
+    'data': ['lunch_view.xml','partner_view.xml'],
+    'demo': [],
+    'test': [],
     'installable': True,
+    'application' : True,
     'certificate' : '001292377792581874189',
-    'images': ['images/cash_moves.jpeg','images/lunch_orders.jpeg','images/products.jpeg'],
+    'images': [],
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
