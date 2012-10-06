bc3cf0b74ff033d247a826ba55b608922454548a
Antony Lesuisse <al@openerp.com>
2012-10-06 19:10:53 +0200

    rm files, move postload hook
    bzr revid: al@openerp.com-20121006171053-qggp1r5263h7v7vd

--- addons/web/common/http.py
+++ addons/web/common/http.py
@@ -579,4 +579,28 @@ class Root(object):
                     ps = '/'
         return None
 
+class Options(object):
+    pass
+
+def wsgi_postload():
+    import openerp
+    import tempfile
+    import getpass
+    _logger.info("embedded mode")
+    o = Options()
+    o.dbfilter = openerp.tools.config['dbfilter']
+    o.server_wide_modules = openerp.conf.server_wide_modules or ['web']
+    try:
+        username = getpass.getuser()
+    except Exception:
+        username = "unknown"
+    o.session_storage = os.path.join(tempfile.gettempdir(), "oe-sessions-" + username)
+    o.addons_path = openerp.modules.module.ad_paths
+    o.serve_static = True
+    o.backend = 'local'
+
+    app = Root(o)
+    openerp.wsgi.register_wsgi_handler(app)
+
+
 # vim:et:ts=4:sw=4:
