66f00492b140566fa136c5ef55cd95dbb362d1ec
Vijaykumar Baladaniya <vba@tinyerp.com>
2012-10-15 13:08:03 +0530

    [IMP]Improve message.
    bzr revid: vba@tinyerp.com-20121015073803-vr85y6d1n03al7u2

--- addons/google_docs/google_docs.py
+++ addons/google_docs/google_docs.py
@@ -128,7 +128,7 @@ class google_docs_ir_attachment(osv.osv):
         try:
             name_gdocs = name_gdocs % model_fields_dic
         except:
-            raise osv.except_osv(_('Key Error!'), _("Your Google Doc Name Pattern is not correct."))
+            raise osv.except_osv(_('Key Error!'), _("Your Google Doc Name Pattern's key does not found in object."))
             google_template_id = pool_gdoc_config.browse(cr, uid, google_docs_config[0], context=context).gdocs_resource_id
             google_document = pool_ir_attachment.copy_gdoc(cr, uid, res_model, res_id, name_gdocs, google_template_id, context=context)
         else:
