91c37856915459cbb4bce674b6f76c778765094a
Vishmita <vja@tinyerp.com>
2012-10-16 12:46:16 +0530

    [FIX]7.0 Problem with Point of Sale - Can't Close Uncaught TypeError
    lp bug: https://launchpad.net/bugs/1066288 fixed
    
    bzr revid: vja@tinyerp.com-20121016071616-kzjifxm9eala1fjx

--- addons/point_of_sale/static/src/js/widgets.js
+++ addons/point_of_sale/static/src/js/widgets.js
@@ -1056,7 +1056,7 @@ function openerp_pos_widgets(instance, module){ //module is instance.point_of_sa
             return new instance.web.Model("ir.model.data").get_func("search_read")([['name', '=', 'action_client_pos_menu']], ['res_id']).pipe(
                     _.bind(function(res) {
                 return this.rpc('/web/action/load', {'action_id': res[0]['res_id']}).pipe(_.bind(function(result) {
-                    var action = result.result;
+                    var action = result;
                     action.context = _.extend(action.context || {}, {'cancel_action': {type: 'ir.actions.client', tag: 'reload'}});
                     //self.destroy();
                     this.do_action(action);
