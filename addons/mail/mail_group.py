b4fbe43de11dfc4801d34fd5f3bbfcf15b08b24f
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2012-10-18 16:26:01 +0200

    [REV] reverting some useless patches before merge in feature branch
    bzr revid: qdp-launchpad@openerp.com-20121018142601-trgb2kbgrvzl4jk2

--- addons/mail/mail_group.py
+++ addons/mail/mail_group.py
@@ -61,7 +61,7 @@ class mail_group(osv.Model):
         'image_medium': fields.function(_get_image, fnct_inv=_set_image,
             string="Medium-sized photo", type="binary", multi="_get_image",
             store={
-                'mail.group': (lambda self, cr, uid, ids, c={}: ids, ['image', 'image_medium'], 10),
+                'mail.group': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
             },
             help="Medium-sized photo of the group. It is automatically "\
                  "resized as a 128x128px image, with aspect ratio preserved. "\
@@ -69,7 +69,7 @@ class mail_group(osv.Model):
         'image_small': fields.function(_get_image, fnct_inv=_set_image,
             string="Small-sized photo", type="binary", multi="_get_image",
             store={
-                'mail.group': (lambda self, cr, uid, ids, c={}: ids, ['image', 'image_small'], 10),
+                'mail.group': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
             },
             help="Small-sized photo of the group. It is automatically "\
                  "resized as a 64x64px image, with aspect ratio preserved. "\
