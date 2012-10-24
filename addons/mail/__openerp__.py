2055dacf04edcec544332d84d3f3701e049d8049
Christophe Matthieu <chm@openerp.com>
2012-10-24 11:25:03 +0200

    [IMP] mail: use widget one2many_binary
    bzr revid: chm@openerp.com-20121024092503-mpk7f1vd8jqynvqc

--- addons/mail/__openerp__.py
+++ addons/mail/__openerp__.py
@@ -90,6 +90,7 @@ Main Features
         'static/lib/jquery.expander/jquery.expander.js',
         'static/src/js/mail.js',
         'static/src/js/mail_followers.js',
+        'static/src/js/mail_uploader.js',
     ],
     'qweb': [
         'static/src/xml/mail.xml',
