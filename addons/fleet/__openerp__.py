026a62928cbebcee6235c69ea27645f5349a5b2c
Cedric Snauwaert <csn@openerp.com>
2012-10-25 16:18:02 +0200

    [ADD]message for module description
    bzr revid: csn@openerp.com-20121025141802-mchcnojygt9wn674

--- addons/fleet/__openerp__.py
+++ addons/fleet/__openerp__.py
@@ -3,14 +3,25 @@
     'name' : 'Fleet Management',
     'version' : '0.1',
     'author' : 'OpenERP S.A.',
-    'category': 'Vehicle, leasing, insurances, costs',
+    'category': 'Managing vehicles and contracts',
     'website' : 'http://www.openerp.com',
-    'summary' : 'Manage all your vehicles and contracts',
+    'summary' : 'Vehicle, leasing, insurances, costs',
     'description' : """
 Vehicle, leasing, insurances, cost
 ==================================
+With this module, OpenERP helps you managing all your vehicles, the
+contracts associated to those vehicle as well as services, fuel log
+entries, costs and many other features necessary to the management 
+of your fleet of vehicle(s)
 
-
+Main Features
+-------------
+* Add vehicles to your fleet
+* Manage contracts for vehicles
+* Reminder when a contract reach its expiration date
+* Add services, fuel log entry, odometer values for all vehicles
+* Show all costs associated to a vehicle or to a type of service
+* Analysis graph for costs
 """,
     'depends' : [
         'base',
