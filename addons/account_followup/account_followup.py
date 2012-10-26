c953d9978c0e847f40ceffb2485f842695511190
Josse Colpaert <jco@openerp.com>
2012-10-26 10:09:14 +0200

    before merge from trunk
    bzr revid: jco@openerp.com-20121026080914-8az79d9rkbfoucyf

--- addons/account_followup/account_followup.py
+++ addons/account_followup/account_followup.py
@@ -188,6 +188,150 @@ class res_partner(osv.osv):
             #Now search one level higher
         return res
 
+
+    def do_partner_phonecall(self, cr, uid, partner_ids, context=None):
+        #partners = self.browse(cr, uid, partner_ids, context)
+        print partner_ids
+        self.write(cr, uid, partner_ids, {'payment_next_action': 'Phone...'}, context)
+
+    def do_partner_print(self, cr, uid, partner_ids, data, context=None):        
+        #data.update({'date': context['date']})
+        if not partner_ids: 
+            return {}
+        data['partner_ids'] = partner_ids
+        print data
+        datas = {
+             'ids': [],
+             'model': 'account_followup.followup',
+             'form': data
+        }
+        
+        return {
+            'type': 'ir.actions.report.xml', 
+            'report_name': 'account_followup.followup.print', 
+            'datas': datas, 
+            }
+
+    def do_partner_mail(self, cr, uid, partner_ids, context=None):
+        #get the mail template to use
+        mail_template_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'account_followup', 'email_template_account_followup')
+        mtp = self.pool.get('email.template')
+        #mtp.subject = "Invoices overdue"
+
+        #user_obj = self.pool.get('res.users')
+
+        #mtp.email_from = user_obj.browse(cr, uid, uid, context=context)
+        for partner in self.browse(cr, uid, partner_ids, context):
+            pass
+         #   mtp.send_mail(cr, uid, partner.id, mail_template_id, context=context)
+
+
+        #complete the mail body with partner information
+        #(to be discussed with fp) attach the report to the mail or include the move lines in the mail body
+        #send the mail (need to check the function name)
+
+        
+#        mod_obj = self.pool.get('ir.model.data')
+#        move_obj = self.pool.get('account.move.line')
+#        user_obj = self.pool.get('res.users')
+#
+#        if context is None:
+#            context = {}
+#        data = self.browse(cr, uid, ids, context=context)[0]
+#        stat_by_partner_line_ids = [partner_id.id for partner_id in data.partner_ids]
+#        partners = [stat_by_partner_line / 10000 for stat_by_partner_line in stat_by_partner_line_ids]
+#        model_data_ids = mod_obj.search(cr, uid, [('model','=','ir.ui.view'),('name','=','view_account_followup_print_all_msg')], context=context)
+#        resource_id = mod_obj.read(cr, uid, model_data_ids, fields=['res_id'], context=context)[0]['res_id']
+#        if True: #should not depend on this
+#            msg_sent = ''
+#            msg_unsent = ''
+#            data_user = user_obj.browse(cr, uid, uid, context=context)
+#            for partner in self.pool.get('res.partner').browse(cr, uid, partners, context=context):
+#                if partner.next_followup_level_id.send_email:
+#                    ids_lines = move_obj.search(cr,uid,[('partner_id','=',partner.id),('reconcile_id','=',False),('account_id.type','in',['receivable']),('company_id','=',context.get('company_id', False))])
+#                    data_lines = move_obj.browse(cr, uid, ids_lines, context=context)
+#                    total_amt = 0.0
+#                    for line in data_lines:
+#                        total_amt += line.debit - line.credit
+#                    dest = False
+#                    if partner:
+#                        dest = [partner.email]
+#                    if not data.partner_lang:
+#                        body = data.email_body 
+#                    else:
+#                        cxt = context.copy()
+#                        cxt['lang'] = partner.lang
+#                        body = user_obj.browse(cr, uid, uid, context=cxt).company_id.follow_up_msg
+#                    move_line = ''
+#                    subtotal_due = 0.0
+#                    subtotal_paid = 0.0
+#                    subtotal_maturity = 0.0
+#                    balance = 0.0
+#                    l = '--------------------------------------------------------------------------------------------------------------------------'
+#                    head = l+ '\n' + 'Date'.rjust(10) + '\t' + 'Description'.rjust(10) + '\t' + 'Ref'.rjust(10) + '\t' + 'Due date'.rjust(10) + '\t' + 'Due'.rjust(10) + '\t' + 'Paid'.rjust(10) + '\t' + 'Maturity'.rjust(10) + '\t' + 'Litigation'.rjust(10) + '\n' + l
+#                    for i in data_lines:
+#                        maturity = 0.00
+#                        if i.date_maturity < time.strftime('%Y-%m-%d') and (i.debit - i.credit):
+#                            maturity = i.debit - i.credit
+#                        subtotal_due = subtotal_due + i.debit
+#                        subtotal_paid = subtotal_paid + i.credit
+#                        subtotal_maturity = subtotal_maturity + int(maturity)
+#                        balance = balance + (i.debit - i.credit)
+#                        move_line = move_line + (i.date).rjust(10) + '\t'+ (i.name).rjust(10) + '\t'+ (i.ref or '').rjust(10) + '\t' + (i.date_maturity or '').rjust(10) + '\t' + str(i.debit).rjust(10)  + '\t' + str(i.credit).rjust(10)  + '\t' + str(maturity).rjust(10) + '\t' + str(i.blocked).rjust(10) + '\n'
+#                    move_line = move_line + l + '\n'+ '\t\t\t' + 'Sub total'.rjust(35) + '\t' + (str(subtotal_due) or '').rjust(10) + '\t' + (str(subtotal_paid) or '').rjust(10) + '\t' + (str(subtotal_maturity) or '').rjust(10)+ '\n'
+#                    move_line = move_line + '\t\t\t' + 'Balance'.rjust(33) + '\t' + str(balance).rjust(10) + '\n' + l
+#                    val = {
+#                        'partner_name':partner.name,
+#                        'followup_amount':total_amt,
+#                        'user_signature':data_user.name,
+#                        'company_name':data_user.company_id.name,
+#                        'company_currency':data_user.company_id.currency_id.name,
+#                        'line':move_line,
+#                        'heading': head,
+#                        'date':time.strftime('%Y-%m-%d'),
+#                    }
+#                    body = body%val
+#                    sub = tools.ustr(data.email_subject)
+#                    msg = ''
+#                    if dest:
+#                        try:
+#                            vals = {'state': 'outgoing',
+#                                    'subject': sub,
+#                                    'body_html': '<pre>%s</pre>' % body,
+#                                    'email_to': dest,
+#                                    'email_from': data_user.email or tools.config.options['email_from']}
+#                            self.pool.get('mail.mail').create(cr, uid, vals, context=context)
+#                            msg_sent += partner.name + '\n'
+#                        except Exception, e:
+#                            raise osv.except_osv('Error !', e )
+#                    else:
+#                        msg += partner.name + '\n'
+#                        msg_unsent += msg
+#            if not msg_unsent:
+#                summary = _("All Emails have been successfully sent to Partners:.\n\n%s") % msg_sent
+#            else:
+#                msg_unsent = _("Email not sent to following Partners, Email not available !\n\n%s") % msg_unsent
+#                msg_sent = msg_sent and _("\n\nEmail sent to following Partners successfully. !\n\n%s") % msg_sent
+#                line = '=========================================================================='
+#                summary = msg_unsent + line + msg_sent
+#            context.update({'summary': summary})
+#        else:
+#            context.update({'summary': '\n\n\nEmail has not been sent to any partner. If you want to send it, please tick send email confirmation on wizard.'})
+#
+#        return {
+#            'name': _('Followup Summary'),
+#            'view_type': 'form',
+#            'context': context,
+#            'view_mode': 'tree,form',
+#            'res_model': 'account.followup.print.all',
+#            'views': [(resource_id,'form')],
+#            'type': 'ir.actions.act_window',
+#            'target': 'new',
+#            'nodestroy': True
+#            }
+
+
+
     _inherit = "res.partner"
     _columns = {
         'payment_responsible_id':fields.many2one('res.users', ondelete='set null', string='Responsible', help="Responsible"), 
@@ -201,8 +345,8 @@ class res_partner(osv.osv):
         'latest_followup_level_id':fields.function(_get_latest_followup_level_id, method=True, 
             type='many2one', relation='account_followup.followup.line', string="latest followup level", store=True), 
         'next_followup_level_id':fields.function(_get_next_followup_level_id, method=True, type='many2one', relation='account_followup.followup.line', string="next level"),
-    }
 
+    }
 
 res_partner()
 
--- addons/account_followup/wizard/account_followup_print.py
+++ addons/account_followup/wizard/account_followup_print.py
@@ -79,6 +79,14 @@ class account_followup_print(osv.osv_memory):
     _columns = {
         'date': fields.date('Follow-up Sending Date', required=True, help="This field allow you to select a forecast date to plan your follow-ups"),
         'followup_id': fields.many2one('account_followup.followup', 'Follow-Up', required=True, readonly = True),
+        'partner_ids': fields.many2many('account_followup.stat.by.partner', 'partner_stat_rel', 'osv_memory_id', 'partner_id', 'Partners', required=True),
+        'company_id':fields.related('followup_id', 'company_id', type='many2one', relation='res.company'), 
+        #'email_conf': fields.boolean('Send Email Confirmation'),
+        #'email_subject': fields.char('Email Subject', size=64),
+        #'partner_lang': fields.boolean('Send Email in Partner Language', help='Do not change message text, if you want to send email in partner language, or configure from company'),
+        #'email_body': fields.text('Email Body'),
+        'summary': fields.text('Summary', required=True, readonly=True),
+        'test_print': fields.boolean('Test Print', help='Check if you want to print follow-ups without changing follow-ups level.')
     }
 
     def _get_followup(self, cr, uid, context=None):
@@ -115,19 +123,28 @@ class account_followup_print(osv.osv_memory):
          'followup_id': _get_followup,
     }
 
-    def process_partner(self, cr, uid, partner_id, context=None):
-        partner = self.pool.get('account_followup.stat.by.partner').browse(cr, uid, partner_id, context=context)
-        if partner.next_followup_level_id.phonecall:
-            self.do_partner_phonecall(cr, uid, partner_id, context)
-        if partner.next_followup_level_id.mail:
-            self.do_partner_mail()
-        if partner.next_followup_level_id.manual:
-            self.do_partner_manual()
 
-    def do_update_followup_level(self, cr, uid, to_update, context=None):
+
+    def process_partners(self, cr, uid, partner_ids, data, context=None):
+        partner_obj = self.pool.get('res.partner')
+        partner_ids_to_print = []
+        for partner in self.pool.get('account_followup.stat.by.partner').browse(cr, uid, partner_ids, context=context):
+            if partner.max_followup_id.phonecall:
+                partner_obj.do_partner_phonecall(cr, uid, [partner.id], context)
+            if partner.max_followup_id.send_email:
+                partner_obj.do_partner_mail(cr, uid, [partner.id], context)
+            if partner.max_followup_id.send_letter:
+                partner_ids_to_print.append(partner.id)
+
+        action = partner_obj.do_partner_print(cr, uid, partner_ids_to_print, data,  context)
+        return action or {}
+
+
+    def do_update_followup_level(self, cr, uid, to_update, partner_list, date, context=None):
         #update the followupo level on account.move.line
         for id in to_update.keys():
-            if to_update[id]['partner_id'] in data['partner_ids']:
+            if to_update[id]['partner_id'] in partner_list:
+
                 cr.execute(
                     "UPDATE account_move_line "\
                     "SET followup_line_id=%s, followup_date=%s "\
@@ -139,9 +156,12 @@ class account_followup_print(osv.osv_memory):
         tmp = self._get_partners_followp(cr, uid, ids, context=context)
         partner_list = tmp['partner_ids']
         to_update = tmp['to_update']
-        self.do_update_followup_level(cr, uid, to_update, context=context)
-        for partner in partner_list:
-            process_partner(cr, uid, partner, context=context)
+        date = self.browse(cr, uid, ids, context)[0].date
+        data = self.read(cr, uid, ids, [], context)[0]
+        self.do_update_followup_level(cr, uid, to_update, partner_list, date, context=context)
+        res = self.process_partners(cr, uid, partner_list, data, context=context)
+        return res
+
 #account_followup_print()
 
 
@@ -177,13 +197,9 @@ class account_followup_print(osv.osv_memory):
 #    }
 
     def _get_partners_followp(self, cr, uid, ids, context=None):
-        data = {}
-        if context is None:
-            context = {}
-        if ids:
-            data = self.browse(cr, uid, ids, context=context)[0]
-        company_id = 'company_id' in context and context['company_id'] or data.company_id.id
-
+        data = {}       
+        data = self.browse(cr, uid, ids, context=context)[0]
+        company_id = data.company_id.id
 
         cr.execute(
             "SELECT l.partner_id, l.followup_line_id,l.date_maturity, l.date, l.id "\
@@ -250,112 +266,8 @@ class account_followup_print(osv.osv_memory):
 
         return {'partner_ids': partner_list, 'to_update': to_update}
 
-    def do_partner_mail(self, cr, uid, partner_ids, context=None):
-        #get the mail template to use
-        mail_template_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'account_followup', 'email_template_account_followup', context=context)
-        #complete the mail body with partner information
-        #(to be discussed with fp) attach the report to the mail or include the move lines in the mail body
-        #send the mail (need to check the function name)
 
-        
-#        mod_obj = self.pool.get('ir.model.data')
-#        move_obj = self.pool.get('account.move.line')
-#        user_obj = self.pool.get('res.users')
-#
-#        if context is None:
-#            context = {}
-#        data = self.browse(cr, uid, ids, context=context)[0]
-#        stat_by_partner_line_ids = [partner_id.id for partner_id in data.partner_ids]
-#        partners = [stat_by_partner_line / 10000 for stat_by_partner_line in stat_by_partner_line_ids]
-#        model_data_ids = mod_obj.search(cr, uid, [('model','=','ir.ui.view'),('name','=','view_account_followup_print_all_msg')], context=context)
-#        resource_id = mod_obj.read(cr, uid, model_data_ids, fields=['res_id'], context=context)[0]['res_id']
-#        if True: #should not depend on this
-#            msg_sent = ''
-#            msg_unsent = ''
-#            data_user = user_obj.browse(cr, uid, uid, context=context)
-#            for partner in self.pool.get('res.partner').browse(cr, uid, partners, context=context):
-#                if partner.next_followup_level_id.send_email:
-#                    ids_lines = move_obj.search(cr,uid,[('partner_id','=',partner.id),('reconcile_id','=',False),('account_id.type','in',['receivable']),('company_id','=',context.get('company_id', False))])
-#                    data_lines = move_obj.browse(cr, uid, ids_lines, context=context)
-#                    total_amt = 0.0
-#                    for line in data_lines:
-#                        total_amt += line.debit - line.credit
-#                    dest = False
-#                    if partner:
-#                        dest = [partner.email]
-#                    if not data.partner_lang:
-#                        body = data.email_body 
-#                    else:
-#                        cxt = context.copy()
-#                        cxt['lang'] = partner.lang
-#                        body = user_obj.browse(cr, uid, uid, context=cxt).company_id.follow_up_msg
-#                    move_line = ''
-#                    subtotal_due = 0.0
-#                    subtotal_paid = 0.0
-#                    subtotal_maturity = 0.0
-#                    balance = 0.0
-#                    l = '--------------------------------------------------------------------------------------------------------------------------'
-#                    head = l+ '\n' + 'Date'.rjust(10) + '\t' + 'Description'.rjust(10) + '\t' + 'Ref'.rjust(10) + '\t' + 'Due date'.rjust(10) + '\t' + 'Due'.rjust(10) + '\t' + 'Paid'.rjust(10) + '\t' + 'Maturity'.rjust(10) + '\t' + 'Litigation'.rjust(10) + '\n' + l
-#                    for i in data_lines:
-#                        maturity = 0.00
-#                        if i.date_maturity < time.strftime('%Y-%m-%d') and (i.debit - i.credit):
-#                            maturity = i.debit - i.credit
-#                        subtotal_due = subtotal_due + i.debit
-#                        subtotal_paid = subtotal_paid + i.credit
-#                        subtotal_maturity = subtotal_maturity + int(maturity)
-#                        balance = balance + (i.debit - i.credit)
-#                        move_line = move_line + (i.date).rjust(10) + '\t'+ (i.name).rjust(10) + '\t'+ (i.ref or '').rjust(10) + '\t' + (i.date_maturity or '').rjust(10) + '\t' + str(i.debit).rjust(10)  + '\t' + str(i.credit).rjust(10)  + '\t' + str(maturity).rjust(10) + '\t' + str(i.blocked).rjust(10) + '\n'
-#                    move_line = move_line + l + '\n'+ '\t\t\t' + 'Sub total'.rjust(35) + '\t' + (str(subtotal_due) or '').rjust(10) + '\t' + (str(subtotal_paid) or '').rjust(10) + '\t' + (str(subtotal_maturity) or '').rjust(10)+ '\n'
-#                    move_line = move_line + '\t\t\t' + 'Balance'.rjust(33) + '\t' + str(balance).rjust(10) + '\n' + l
-#                    val = {
-#                        'partner_name':partner.name,
-#                        'followup_amount':total_amt,
-#                        'user_signature':data_user.name,
-#                        'company_name':data_user.company_id.name,
-#                        'company_currency':data_user.company_id.currency_id.name,
-#                        'line':move_line,
-#                        'heading': head,
-#                        'date':time.strftime('%Y-%m-%d'),
-#                    }
-#                    body = body%val
-#                    sub = tools.ustr(data.email_subject)
-#                    msg = ''
-#                    if dest:
-#                        try:
-#                            vals = {'state': 'outgoing',
-#                                    'subject': sub,
-#                                    'body_html': '<pre>%s</pre>' % body,
-#                                    'email_to': dest,
-#                                    'email_from': data_user.email or tools.config.options['email_from']}
-#                            self.pool.get('mail.mail').create(cr, uid, vals, context=context)
-#                            msg_sent += partner.name + '\n'
-#                        except Exception, e:
-#                            raise osv.except_osv('Error !', e )
-#                    else:
-#                        msg += partner.name + '\n'
-#                        msg_unsent += msg
-#            if not msg_unsent:
-#                summary = _("All Emails have been successfully sent to Partners:.\n\n%s") % msg_sent
-#            else:
-#                msg_unsent = _("Email not sent to following Partners, Email not available !\n\n%s") % msg_unsent
-#                msg_sent = msg_sent and _("\n\nEmail sent to following Partners successfully. !\n\n%s") % msg_sent
-#                line = '=========================================================================='
-#                summary = msg_unsent + line + msg_sent
-#            context.update({'summary': summary})
-#        else:
-#            context.update({'summary': '\n\n\nEmail has not been sent to any partner. If you want to send it, please tick send email confirmation on wizard.'})
-#
-#        return {
-#            'name': _('Followup Summary'),
-#            'view_type': 'form',
-#            'context': context,
-#            'view_mode': 'tree,form',
-#            'res_model': 'account.followup.print.all',
-#            'views': [(resource_id,'form')],
-#            'type': 'ir.actions.act_window',
-#            'target': 'new',
-#            'nodestroy': True
-#            }
+    
 
     def do_print(self, cr, uid, ids, context=None):
         if context is None:
@@ -376,7 +288,7 @@ class account_followup_print(osv.osv_memory):
                         (to_update[id]['level'],
                         date, int(id),))
         data.update({'date': context['date']})
-        print data
+        
         datas = {
              'ids': [],
              'model': 'account_followup.followup',
@@ -406,7 +318,7 @@ class account_followup_print(osv.osv_memory):
 #        model_data_ids = mod_obj.search(cr, uid, [('model','=','ir.ui.view'),('name','=','view_account_followup_print_all_msg')], context=context)
 #        resource_id = mod_obj.read(cr, uid, model_data_ids, fields=['res_id'], context=context)[0]['res_id']
 #        print "start changeof new action..."
-#
+#    
 #        partnersfilt = []
 #        partner_obj = self.pool.get('res.partner')
 #        for partner in partner_obj.browse(cr, uid, partners, context):
@@ -419,6 +331,6 @@ class account_followup_print(osv.osv_memory):
 #        return resu2
 
 
-account_followup_print_all()
+#account_followup_print_all()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
