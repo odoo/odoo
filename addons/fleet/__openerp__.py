bc6c61b35b4dc73111e7b0a680db6f35fb90329d
Cedric Snauwaert <csn@openerp.com>
2012-10-29 14:56:15 +0100

    [RENAME] rename files to match openerp standard
    bzr revid: csn@openerp.com-20121029135615-xt4uzh3wz4l0n0p8

--- addons/fleet/__openerp__.py
+++ addons/fleet/__openerp__.py
@@ -30,12 +30,12 @@ Main Features
     ],
     'data' : [
         'fleet_view.xml',
-        'data.xml',
-        'board_fleet_view.xml',
+        'fleet_data.xml',
+        'fleet_board_view.xml',
     ],
     'update_xml' : ['security/ir.model.access.csv'],
 
-    'demo': ['cars.xml','demo.xml'],
+    'demo': ['fleet_cars.xml','fleet_demo.xml'],
 
     'installable' : True,
     'application' : True,
