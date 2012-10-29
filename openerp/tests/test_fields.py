7e7d877e17564366179c7e24439105da02d8e92e
Raphael Collet <rco@openerp.com>
2012-10-29 16:35:04 +0100

    fields.related: simplify read and write methods
    bzr revid: rco@openerp.com-20121029153504-157ss9948o86uv6z

--- openerp/tests/test_fields.py
+++ openerp/tests/test_fields.py
@@ -79,4 +79,34 @@ class TestRelatedField(common.TransactionCase):
         # restore res.partner fields
         self.partner._columns = old_columns
 
+    def test_3_read_write(self):
+        """ write on a related field """
+        # add a related field test_related_company_id on res.partner
+        old_columns = self.partner._columns
+        self.partner._columns = dict(old_columns)
+        self.partner._columns.update({
+            'related_company_partner_id': fields.related('company_id', 'partner_id', type='many2one', obj='res.partner'),
+        })
+
+        # find a company with a non-null partner_id
+        company_ids = self.company.search(self.cr, self.uid, [('partner_id', '!=', False)], limit=1)
+        company = self.company.browse(self.cr, self.uid, company_ids[0])
+
+        # find partners that satisfy [('partner_id.company_id', '=', company.id)]
+        partner_ids = self.partner.search(self.cr, self.uid, [('related_company_partner_id', '=', company.id)])
+        partner = self.partner.browse(self.cr, self.uid, partner_ids[0])
+
+        # create a new partner, and assign it to company
+        new_partner_id = self.partner.create(self.cr, self.uid, {'name': 'Foo'})
+        partner.write({'related_company_partner_id': new_partner_id})
+
+        company = self.company.browse(self.cr, self.uid, company_ids[0])
+        self.assertEqual(company.partner_id.id, new_partner_id)
+
+        partner = self.partner.browse(self.cr, self.uid, partner_ids[0])
+        self.assertEqual(partner.related_company_partner_id.id, new_partner_id)
+
+        # restore res.partner fields
+        self.partner._columns = old_columns
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
