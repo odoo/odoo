1607b1ddc7793f81d477e8d8c738bc7ebcfff162
Purnendu Singh (OpenERP) <psi@tinyerp.com>
2012-10-30 18:17:27 +0530

    [FIX] product_margin: Fix header translation issue
    lp bug: https://launchpad.net/bugs/937856 fixed
    
    bzr revid: psi@tinyerp.com-20121030124727-tm5vdpld7oiwpjlx

--- addons/product_margin/wizard/product_margin.py
+++ addons/product_margin/wizard/product_margin.py
@@ -64,7 +64,7 @@ class product_margin(osv.osv_memory):
         #get the current product.margin object to obtain the values from it
         product_margin_obj = self.browse(cr, uid, ids, context=context)[0]
 
-        context = {'invoice_state' : product_margin_obj.invoice_state}
+        context.update({'invoice_state' : product_margin_obj.invoice_state})
         if product_margin_obj.from_date:
             context.update({'date_from': product_margin_obj.from_date})
         if product_margin_obj.to_date:
