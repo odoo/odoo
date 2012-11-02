13a49a855cfe9124c72e4bd9e040100c9dc6d50a
Thibault Delavallée <tde@openerp.com>
2012-11-02 15:23:22 +0100

    [IMP] mail: updated notified_partner_ids field string and help; udpated demo data; updated __init__ to avoid issues with non existing columns in mail_favorite.
    bzr revid: tde@openerp.com-20121102142322-prq9vvtec93u9mah

--- addons/mail/__init__.py
+++ addons/mail/__init__.py
@@ -22,12 +22,12 @@
 import mail_message_subtype
 import mail_alias
 import mail_followers
+import mail_vote
+import mail_favorite
 import mail_message
 import mail_mail
 import mail_thread
 import mail_group
-import mail_vote
-import mail_favorite
 import res_partner
 import res_users
 import report
