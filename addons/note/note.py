fd0ea2a2e6cd7a9858c3feaf3531d5b88a5bbbc1
Nimesh (Open ERP) <nco@tinyerp.com>
2012-11-02 18:47:59 +0530

    [IMP] improve code.
    bzr revid: nco@tinyerp.com-20121102131759-e7q27o3w2npu9mgr

--- addons/note/note.py
+++ addons/note/note.py
@@ -192,18 +192,15 @@ class res_users(osv.Model):
     _inherit = ['res.users']
     def create(self, cr, uid, data, context=None):
         user_id = super(res_users, self).create(cr, uid, data, context=context)
+        user = self.browse(cr, uid, uid, context=context)
         note_obj = self.pool.get('note.stage')
         data_obj = self.pool.get('ir.model.data')
-        model_id = data_obj.get_object_reference(cr, uid, 'base', 'group_user')
+        model_id = data_obj.get_object_reference(cr, uid, 'base', 'group_user') #Employee Group
         group_id = model_id and model_id[1] or False
-        if not group_id: return user_id
-        cr.execute('SELECT * FROM res_groups_users_rel WHERE uid = %s and gid = %s', (user_id, group_id,))
-        if not cr.fetchone():
-            return user_id
-        note_ids = ['note_stage_01','note_stage_02','note_stage_03','note_stage_04']
-        for note_id in note_ids:
-            data_id = data_obj._get_id(cr, uid, 'note', note_id)
-            view_id  = data_obj.browse(cr, uid, data_id, context=context).res_id
-            note_obj.copy(cr, uid, view_id, default = { 
-                                    'user_id': user_id}, context=context)
+        if group_id in [x.id for x in user.groups_id]:
+            for note_xml_id in ['note_stage_01','note_stage_02','note_stage_03','note_stage_04']:
+                data_id = data_obj._get_id(cr, uid, 'note', note_xml_id)
+                stage_id  = data_obj.browse(cr, uid, data_id, context=context).res_id
+                note_obj.copy(cr, uid, stage_id, default = { 
+                                        'user_id': user_id}, context=context)
         return user_id
