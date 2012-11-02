5212e76c4717dd6ba577fcea83c1a1c0952258d8
Arnaud Pineux <api@openerp.com>
2012-11-02 11:16:29 +0100

    Lunch
    bzr revid: api@openerp.com-20121102101629-g5iktkcf8pbu1kyk

--- addons/lunch/__openerp__.py
+++ addons/lunch/__openerp__.py
@@ -29,8 +29,15 @@
 The base module to manage lunch.
 ================================
 
-keep track for the Lunch Order, Cash Moves and Product. Apply Different
-Category for the product.
+Many companies order sandwiches, pizzas and other, from usual suppliers, for their employees to offer them more facilities. 
+
+However lunches management within the company requires proper administration especially when the number of employees or suppliers is important. 
+
+The “Lunch Order” module has been developed to make this management easier but also to offer employees more tools and usability. 
+
+In addition to a full meal and supplier management, this module offers the possibility to display warning and provides quick order selection based on employee’s preferences.
+
+If you want to save your employees' time and avoid them to always have coins in their pockets, this module is essential.
     """,
     'data': ['security/groups.xml','lunch_view.xml','wizard/lunch_order_view.xml','wizard/lunch_validation_view.xml','wizard/lunch_cancel_view.xml','lunch_report.xml',
         'report/report_lunch_order_view.xml',
