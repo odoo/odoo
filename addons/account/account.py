f4f45c476c2a734d45170f12400413bcfaa0a7ac
Rajesh Prajapati (OpenERP) <rpr@tinyerp.com>
2012-11-06 11:57:36 +0530

    [IMP] account : added constraints to check parent account
    bzr revid: rpr@tinyerp.com-20121106062736-28vgz6515h4rt3s2

--- addons/account/account.py
+++ addons/account/account.py
@@ -541,10 +541,18 @@ class account_account(osv.osv):
                 return False
         return True
 
+    def _check_parent_id(self, cr, uid, ids, context=None):
+        parent = self.browse(cr, uid, ids, context=context)[0]
+        if parent.parent_id:
+            if parent.company_id != parent.parent_id.company_id:
+                return False
+        return True
+
     _constraints = [
         (_check_recursion, 'Error!\nYou cannot create recursive accounts.', ['parent_id']),
         (_check_type, 'Configuration Error!\nYou cannot define children to an account with internal type different of "View".', ['type']),
         (_check_account_type, 'Configuration Error!\nYou cannot select an account type with a deferral method different of "Unreconciled" for accounts with internal type "Payable/Receivable".', ['user_type','type']),
+        (_check_parent_id, 'You cannot create an account which has parent account of different company.', ['parent_id']),
     ]
     _sql_constraints = [
         ('code_company_uniq', 'unique (code,company_id)', 'The code of the account must be unique per company !')
