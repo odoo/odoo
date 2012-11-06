5ead513bf26d1b92122d3732841e9177d68794d1
Rajesh Prajapati (OpenERP) <rpr@tinyerp.com>
2012-11-06 14:54:01 +0530

    [IMP] account : string changed
    bzr revid: rpr@tinyerp.com-20121106092401-slfgkto8kvrv2o4v

--- addons/account/account.py
+++ addons/account/account.py
@@ -552,7 +552,7 @@ class account_account(osv.osv):
         (_check_recursion, 'Error!\nYou cannot create recursive accounts.', ['parent_id']),
         (_check_type, 'Configuration Error!\nYou cannot define children to an account with internal type different of "View".', ['type']),
         (_check_account_type, 'Configuration Error!\nYou cannot select an account type with a deferral method different of "Unreconciled" for accounts with internal type "Payable/Receivable".', ['user_type','type']),
-        (_check_company_account, 'You cannot create an account which has parent account of different company.', ['parent_id']),
+        (_check_company_account, 'Error!\nYou cannot create an account which has parent account of different company.', ['parent_id']),
     ]
     _sql_constraints = [
         ('code_company_uniq', 'unique (code,company_id)', 'The code of the account must be unique per company !')
