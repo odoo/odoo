7ea98f28d788ab11b5f6a0f0fa0c210c3fa32d31
Christophe Simonis <chs@openerp.com>
2012-11-08 17:46:11 +0100

    [IMP] add TODO/FIXME comments
    bzr revid: chs@openerp.com-20121108164611-34l07gsjer1lrqc2

--- openerp/addons/base/static/src/js/apps.js
+++ openerp/addons/base/static/src/js/apps.js
@@ -1,5 +1,10 @@
 openerp.base = function(instance) {
 
+// FIXME do not work on safari
+// TODO test on IE
+// FIXME when apps server is not available, redirect to failback action but do not highligth the good menu
+// FIXME when back from oe_account (via oe_apps), error is show: load action 3 times !!!
+
     instance.base = {apps:{}};
 
     instance.base.apps.remote_instance = null;
