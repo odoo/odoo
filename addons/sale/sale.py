1d49a9608943bd5d50c3f01c439965d70a1b124d
Turkesh Patel (Open ERP) <tpa@tinyerp.com>
2012-11-09 12:05:31 +0530

    [IMP] change name of method.
    bzr revid: tpa@tinyerp.com-20121109063531-ywqipjlza24tl48x

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -264,7 +264,7 @@ class sale_order(osv.osv):
 
         return osv.osv.unlink(self, cr, uid, unlink_ids, context=context)
 
-    def new_quotation(self, cr, uid, ids, default=None, context=None):
+    def copy_quotation(self, cr, uid, ids, context=None):
         id = self.copy(cr, uid, ids[0], context=None)
         view_ref = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'sale', 'view_order_form')
         view_id = view_ref and view_ref[1] or False,
@@ -277,6 +277,7 @@ class sale_order(osv.osv):
             'view_mode': 'form',
             'view_id': view_id,
             'target': 'current',
+            'nodestroy': True,
         }
 
     def onchange_pricelist_id(self, cr, uid, ids, pricelist_id, order_lines, context=None):
