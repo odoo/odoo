bba12d9e95d681e17d2898ea598ac0f1f4e90207
Denis Ledoux dle@openerp.com <>
2012-11-15 16:57:08 +0100

    [FIX]wrong out id when adding shortcut to system tray list
    lp bug: https://launchpad.net/bugs/1077138 fixed
    
    bzr revid: dle@openerp.com-20121115155708-vbz1djvtp15dit78

--- addons/web_shortcuts/static/src/js/web_shortcuts.js
+++ addons/web_shortcuts/static/src/js/web_shortcuts.js
@@ -57,7 +57,7 @@ instance.web_shortcuts.Shortcuts = instance.web.Widget.extend({
         this.dataset.create(sc).then(function(out){
             self.trigger('display', {
                 name : sc.name,
-                id : out.result,
+                id : out,
                 res_id : sc.res_id
             });
         });
@@ -108,24 +108,24 @@ instance.web.UserMenu.include({
 });
 
 instance.web.ViewManagerAction.include({
-    switch_mode: function (view_type, no_store) {
+    switch_mode: function () {
         var self = this;
         this._super.apply(this, arguments).done(function() {
-            self.shortcut_check(self.views[view_type]);
+            self.shortcut_check();
         });
     },
-    shortcut_check : function(view) {
+    shortcut_check : function() {
         var self = this;
         var shortcuts_menu = instance.webclient.user_menu.shortcuts;
         var grandparent = this.getParent() && this.getParent().getParent();
         // display shortcuts if on the first view for the action
         var $shortcut_toggle = this.$el.find('.oe_shortcuts_toggle');
-        if (!this.action.name ||
+        /*if (!this.action.name ||
                 !(view.view_type === this.views_src[0].view_type
                     && view.view_id === this.views_src[0].view_id)) {
             $shortcut_toggle.hide();
             return;
-        }
+        }*/
         $shortcut_toggle.toggleClass('oe_shortcuts_remove', shortcuts_menu.has(self.session.active_id));
         $shortcut_toggle.unbind("click").click(function() {
             if ($shortcut_toggle.hasClass("oe_shortcuts_remove")) {
