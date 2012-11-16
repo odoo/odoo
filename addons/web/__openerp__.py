cb182407f099ce42ee5b85418ff32c985e57d9d4
Xavier Morel <xmo@openerp.com>
2012-11-16 12:39:36 +0100

    [FIX] missing comma
    bzr revid: xmo@openerp.com-20121116113936-opg1gxan0jat2a4d

--- addons/web/__openerp__.py
+++ addons/web/__openerp__.py
@@ -80,7 +80,7 @@ This module provides the core of the OpenERP Web Client.
         "static/test/evals.js",
         "static/test/search.js",
         "static/test/Widget.js",
-        "static/test/list.js"
+        "static/test/list.js",
         "static/test/list-editable.js"
     ],
     'bootstrap': True,
