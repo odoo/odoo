2f47a6be9fcf5d842d8656e5e2d1739f1d00c683
Ajay Chauhan (OpenERP) <cha@tinyerp.com>
2012-11-23 19:06:19 +0530

    [IMP] purchase: added print button to print quotation
    bzr revid: cha@tinyerp.com-20121123133619-t827uucsw6obilcz

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -413,6 +413,20 @@ class purchase_order(osv.osv):
             'context': ctx,
         }
 
+    def print_quotation(self, cr, uid, ids, context=None):
+        '''
+        This function prints the request for quotation and mark it as sent, so that we can see more easily the next step of the workflow
+        '''
+        assert len(ids) == 1, 'This option should only be used for a single id at a time'
+        wf_service = netsvc.LocalService("workflow")
+        wf_service.trg_validate(uid, 'purchase.order', ids[0], 'send_rfq', cr)
+        datas = {
+                 'model': 'purchase.order',
+                 'ids': ids,
+                 'form': self.read(cr, uid, ids[0], context=context),
+        }
+        return {'type': 'ir.actions.report.xml', 'report_name': 'purchase.quotation', 'datas': datas, 'nodestroy': True}
+
     #TODO: implement messages system
     def wkf_confirm_order(self, cr, uid, ids, context=None):
         todo = []
