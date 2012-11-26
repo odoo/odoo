98a1f3d3bcb6ff0472ce4b3b416a4930433aaff8
Hardik <hsa@tinyerp.com>
2012-11-26 17:07:13 +0530

    [IMP]Create customer invoice and select the customer without demo data
    bzr revid: hsa@tinyerp.com-20121126113713-trqn1l2xlkxucnk3

--- addons/account/account_invoice.py
+++ addons/account/account_invoice.py
@@ -457,7 +457,7 @@ class account_invoice(osv.osv):
             invoice_addr_id = res['invoice']
             p = self.pool.get('res.partner').browse(cr, uid, partner_id)
             if company_id:
-                if p.property_account_receivable.company_id.id != company_id and p.property_account_payable.company_id.id != company_id:
+                if p.property_account_receivable.company_id != company_id and p.property_account_payable.company_id != company_id:
                     property_obj = self.pool.get('ir.property')
                     rec_pro_id = property_obj.search(cr,uid,[('name','=','property_account_receivable'),('res_id','=','res.partner,'+str(partner_id)+''),('company_id','=',company_id)])
                     pay_pro_id = property_obj.search(cr,uid,[('name','=','property_account_payable'),('res_id','=','res.partner,'+str(partner_id)+''),('company_id','=',company_id)])
