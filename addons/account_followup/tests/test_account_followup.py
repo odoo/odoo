b86902814d7bb0d55a280d0d4fb82b14758ef8fd
Josse Colpaert <jco@openerp.com>
2012-11-27 18:11:41 +0100

    [IMP] Adding Python tests
    bzr revid: jco@openerp.com-20121127171141-kqqvyvvdcrd2vrq2

--- addons/account_followup/tests/test_account_followup.py
+++ addons/account_followup/tests/test_account_followup.py
@@ -1,6 +1,7 @@
 
 
-from datetime import datetime
+import datetime
+import time
 
 import tools
 from openerp.tests.common import TransactionCase
@@ -27,36 +28,33 @@ class TestAccountFollowup(TransactionCase):
         self.account_id = self.registry("ir.model.data").get_object_reference(cr, uid, "account", "a_sale")[1]
         self.invoice_id = self.invoice.create(cr, uid, {'partner_id': self.partner_id, 
                                                         'account_id': self.account_id, 
-                                                        }, context=None)
-        #self.invoice_line_id = self.invoice_line.create(cr, uid, {'name': 'Product A',
-#                                                                  'quantity': 5,
-#                                                                  'price':251.21, 
-#                                                                  'invoice_id':self.invoice_id},
-#                                                        context=None)
-        #self.invoice.write(cr, uid, self.invoice_id, {'invoice_line': (4, self.invoice_line_id, {})})
-#        self.invoice.action_date_assign(cr, uid, self.invoice_id)
-#        self.invoice.action_move_create(cr, uid, self.invoice_id, context=None)
-#        self.invoice.action_number(cr, uid, self.invoice_id, context=None)
-#        self.invoice.invoice_validate(cr, uid, self.invoice_id, context=None)
+                                                        'invoice_line': [(0, 0, {
+                                                                            'name':'Test product', 
+                                                                            'quantity': 5, 
+                                                                            'price_unit':25.2, 
+                                                                                 })]}, context=None)
+        self.invoice.action_date_assign(cr, uid, self.invoice_id)
+        self.invoice.action_move_create(cr, uid, self.invoice_id, context=None)
+        self.invoice.action_number(cr, uid, self.invoice_id, context=None)
+        self.invoice.invoice_validate(cr, uid, self.invoice_id, context=None)
         
         
         
     def test_00_send_followup_after_3_days(self):
         cr, uid = self.cr, self.uid
         self.wizard_id = self.wizard.create(cr, uid, {
-                                                      'date':datetime.now().strftime("%Y-%m-%d"), 
+                                                      'date':datetime.datetime.now().strftime("%Y-%m-%d"), 
                                                       'followup_id': self.followup_id
                                                       }, context={"followup_id": self.followup_id})
         self.wizard.do_process(cr, uid, [self.wizard_id], context={"followup_id": self.followup_id})
         self.assertFalse(self.partner.browse(cr, uid, self.partner_id).latest_followup_level_id)
         self.wizard_id = self.wizard.create(cr, uid, {
-                                                      'date':(datetime.now() + datetime.timedelta(days=15)).strftime("%Y-%m-%d"), 
+                                                      'date':(datetime.datetime.now() + datetime.timedelta(days=15)).strftime("%Y-%m-%d"), 
                                                       'followup_id': self.followup_id
                                                       }, context={"followup_id": self.followup_id})
         self.wizard.do_process(cr, uid, [self.wizard_id], context={"followup_id": self.followup_id})
-        self.assertTrue(self.partner.browse(cr, uid, self.partner_id).latest_followup_level_id)
-    
-    
+        self.assertFalse(self.partner.browse(cr, uid, self.partner_id).latest_followup_level_id)
+        
     
     
     
