63de663ece486d37b094d8994f5c53b445ec2aa5
Vo Minh Thu <vmt@openerp.com>
2012-11-28 16:06:37 +0100

    [IMP] wkf_expr: Ignore empty lines and lines with blanks.
    bzr revid: vmt@openerp.com-20121128150637-pikxf9tw2nj0qpuk

--- openerp/workflow/wkf_expr.py
+++ openerp/workflow/wkf_expr.py
@@ -44,8 +44,10 @@ class Env(dict):
 def _eval_expr(cr, ident, workitem, action):
     ret=False
     assert action, 'You used a NULL action in a workflow, use dummy node instead.'
-    for line in filter(None, action.split('\n')):
+    for line in action.split('\n'):
         line = line.strip()
+        if not line:
+            continue
         uid=ident[0]
         model=ident[1]
         ids=[ident[2]]
