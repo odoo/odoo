3f626523ac7576ce5711d3274dd223996a3df07e
Khushboo Bhatt (Open ERP) <kbh@tinyerp.com>
2012-11-28 11:06:58 +0530

    [IMP]improve code for merge lead/opportunity message
    bzr revid: kbh@tinyerp.com-20121128053658-kiosagyvz7s78x4s

--- addons/crm/crm_lead.py
+++ addons/crm/crm_lead.py
@@ -457,7 +457,6 @@ class crm_lead(base_stage, format_address, osv.osv):
 
     def _mail_body(self, cr, uid, lead, fields, title=False, context=None):
         body = []
-        str = ""
         if title:
             body.append("%s\n" % (title))
         for field_name in fields:
@@ -465,7 +464,7 @@ class crm_lead(base_stage, format_address, osv.osv):
             if field_info is None:
                 continue
             field = field_info.column
-            value = None
+            value = None or ''
 
             if field._type == 'selection':
                 if hasattr(field.selection, '__call__'):
@@ -473,20 +472,18 @@ class crm_lead(base_stage, format_address, osv.osv):
                 else:
                     key = field.selection
                 value = dict(key).get(lead[field_name], lead[field_name])
-                body.append("%s: %s" % (field.string, value or ''))
             elif field._type == 'many2one':
                 if lead[field_name]:
                     value = lead[field_name].name_get()[0][1]
-                    body.append("%s: %s" % (field.string, value or ''))
             elif field._type == 'many2many':
                 if lead[field_name]:
                     for val in lead[field_name]:
                         field_value = val.name_get()[0][1]
-                        str += field_value + ","
-                    body.append("%s: %s" % (field.string, str or ''))
+                        value += field_value + ","
             else:
                 value = lead[field_name]
-                body.append("%s: %s" % (field.string, value or ''))
+
+            body.append("%s: %s" % (field.string, value or ''))
         return "\n".join(body + ['---'])
 
     def _merge_notification(self, cr, uid, opportunity_id, opportunities, context=None):
