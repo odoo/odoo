3fc7e06583733d6f547ce719aa44b207c988a70d
Hardik <hsa@tinyerp.com>
2012-11-30 14:48:26 +0530

    [IMP]Set Indentation for Accoutning tooltip
    bzr revid: hsa@tinyerp.com-20121130091826-q1d3xs4mlyzhosi5

--- addons/purchase/res_config.py
+++ addons/purchase/res_config.py
@@ -74,7 +74,7 @@ class account_config_settings(osv.osv_memory):
     _columns = {
         'module_purchase_analytic_plans': fields.boolean('Use multiple analytic accounts on orders',
             help ="""Allows the user to maintain several analysis plans. These let you split lines on a purchase order between several accounts and analytic plans.
-                This installs the module purchase_analytic_plans."""),
+This installs the module purchase_analytic_plans."""),
         'group_analytic_account_for_purchases': fields.boolean('Analytic accounting for purchases',
             implied_group='purchase.group_analytic_accounting',
             help="Allows you to specify an analytic account on purchase orders."),
