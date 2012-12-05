8786a35c6068db5241c05284afefbd0b9a164123
Nhomar Hernandez <nhomar@gmail.com>
2012-12-04 23:57:52 -0430

    [FIX] Some fixes order on xml files [FIX] Added tax==0 necesary for Vat reports in localization. [FIX] Bad named some xml_ids [IMP] Problem with Typo in my bad english
    bzr revid: nhomar@gmail.com-20121205042752-clwxbsere4ljegox

--- addons/l10n_ve/__openerp__.py
+++ addons/l10n_ve/__openerp__.py
@@ -30,33 +30,33 @@
     'category': 'Localization/Account Charts',
     'description':
 """
-This is the module to manage the accounting chart for Venezuela.
-================================================================
+Chart of Account for Venezuela.
+===============================
 
 Venezuela doesn't have any chart of account by law, but the default
-proposed in OpenERP should comply with soma accounting bases, without specify
-precise account for precise business Verticals.
+proposed in OpenERP should comply with some Accepted best practices in Venezuela, 
+this plan comply with this practices.
 
-This module pretend comply with commonly accepted standards, and it has 
-been tested as base for more of 1000 companies, because it is based as a 
-mix of most common softwares in the Venezuelan market what will allow for 
-sure to accountants feel the use of OpenERP as them actual software and as 
-they learn in University.
+This module has been tested as base for more of 1000 companies, because 
+it is based in a mixtures of most common softwares in the Venezuelan 
+market what will allow for sure to accountants feel them first steps with 
+OpenERP more confortable.
 
-This module doesn't pretend be the total localization for Venezuela, but 
-it will help to you to start really quickly with OpenERP in this country.
+This module doesn't pretend be the total localization for Venezuela, 
+but it will help you to start really quickly with OpenERP in this country.
 
-This module allow.
-------------------
+This module give you.
+---------------------
 
-- All taxes for Venezuela.
+- Basic taxes for Venezuela.
 - Have basic data to run tests with community localization.
 - Start a company from 0 if your needs are basic from an accounting PoV.
-- Basic Taxs for Venezuela.
 
 We recomend install account_anglo_saxon if you want valued your 
-stock as Venezuela does.
+stocks as Venezuela does with out invoices.
 
+If you install this module, and select Custom chart a basic chart will be proposed, 
+but you will need set manually account defaults for taxes.
 """,
     'depends': ['account',
                 'base_vat', 
@@ -64,14 +64,15 @@ stock as Venezuela does.
     ],
     'demo': [],
     'data': ['data/account_tax_code.xml',
+             'data/account_user_types.xml',
              'data/account_chart.xml',
              'data/account_tax.xml',
-             'data/account_user_types.xml',
              'data/l10n_chart_ve_wizard.xml'
     ],
     'auto_install': False,
     'installable': True,
-    'images': ['images/config_chart_l10n_ve.jpeg','images/l10n_ve_chart.jpeg'],
+    'images': ['images/config_chart_l10n_ve.jpeg',
+               'images/l10n_ve_chart.jpeg'],
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
