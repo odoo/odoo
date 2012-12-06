0a859cddcdb108109f3a8acd207c7e6939105a2d
Bhumi Thakkar (Open ERP) <bth@tinyerp.com>
2012-12-06 12:22:14 +0530

    [IMP] Add string and renamed string.
    bzr revid: bth@tinyerp.com-20121206065214-2dp3hmqhuu0r9g8n

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -206,7 +206,7 @@ class sale_order(osv.osv):
 
         'order_line': fields.one2many('sale.order.line', 'order_id', 'Order Lines', readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}),
         'invoice_ids': fields.many2many('account.invoice', 'sale_order_invoice_rel', 'order_id', 'invoice_id', 'Invoices', readonly=True, help="This is the list of invoices that have been generated for this sales order. The same sales order may have been invoiced in several times (by line for example)."),
-        'invoiced_rate': fields.function(_invoiced_rate, string='Invoice Ratio', type='float'),
+        'invoiced_rate': fields.function(_invoiced_rate, string='Invoiced Ratio', type='float'),
         'invoiced': fields.function(_invoiced, string='Paid',
             fnct_search=_invoiced_search, type='boolean', help="It indicates that an invoice has been paid."),
         'invoice_exists': fields.function(_invoice_exists, string='Invoiced',
