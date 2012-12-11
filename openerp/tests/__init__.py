055f46f29029f054a792b19619081221f41085b2
Vo Minh Thu <vmt@openerp.com>
2012-12-11 13:28:44 +0100

    [IMP] tests: add test_acl to the openerp.tests suite.
    bzr revid: vmt@openerp.com-20121211122844-6y060gex2lbq76hp

--- openerp/tests/__init__.py
+++ openerp/tests/__init__.py
@@ -8,6 +8,7 @@ Tests can be explicitely added to the `fast_suite` or `checks` lists or not.
 See the :ref:`test-framework` section in the :ref:`features` list.
 """
 
+from . import test_acl
 from . import test_expression, test_mail, test_ir_sequence, test_orm, \
               test_fields, test_basecase, \
               test_view_validation, test_uninstall, test_misc, test_db_cursor, \
@@ -20,6 +21,7 @@ fast_suite = [
 ]
 
 checks = [
+    test_acl,
     test_expression,
     test_mail,
     test_db_cursor,
