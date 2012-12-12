55f9f4dc7470d3b54fd7562107c0310df0291323
ajay javiya (OpenERP) <aja@tinyerp.com>
2012-12-12 10:58:11 +0530

    [REM]: Remove warning
    bzr revid: aja@tinyerp.com-20121212052811-lfq1vyyu2bagx77f

--- addons/web_tests_demo/__openerp__.py
+++ addons/web_tests_demo/__openerp__.py
@@ -1,6 +1,10 @@
 {
     'name': "Demonstration of web/javascript tests",
     'category': 'Hidden',
+    'description': """ 
+        OpenERP Web test demo suite.
+        =======================
+    """,
     'depends': ['web'],
     'js': ['static/src/js/demo.js'],
     'test': ['static/test/demo.js'],
