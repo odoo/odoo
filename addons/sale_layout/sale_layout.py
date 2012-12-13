4e6e5b2ebb9d3389d19564b45ae63803076aa3a6
Pinakin Nayi (OpenERP) <pna@tinyerp.com>
2012-12-13 11:22:08 +0530

    [FIX]problem not impact precision of UoM in sale order
    bzr revid: pna@tinyerp.com-20121213055208-lwj7v3n3ha8dgsh1

--- addons/sale_layout/sale_layout.py
+++ addons/sale_layout/sale_layout.py
@@ -120,7 +120,7 @@ class sale_order_line(osv.osv):
             ,'Line Type', select=True, required=True),
         'sequence': fields.integer('Line Sequence', select=True),
         'price_unit': fields.float('Unit Price', required=True, digits_compute= dp.get_precision('Sale Price'), readonly=True, states={'draft': [('readonly', False)]}),
-        'product_uom_qty': fields.float('Quantity (UoM)', digits=(16,2)),
+        'product_uom_qty': fields.float('Quantity (UoM)', digits_compute= dp.get_precision('Product UoM')),
         'product_uom': fields.many2one('product.uom', 'Product UoM'),
         # Override the field to call the overridden _amount_line function
         'price_subtotal': fields.function(_amount_line, method=True, string='Subtotal', digits_compute= dp.get_precision('Sale Price')),
