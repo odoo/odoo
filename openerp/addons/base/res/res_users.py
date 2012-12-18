d98b1b0ff55223ae118e8461a84f908995d6c736
Raphael Collet <rco@openerp.com>
2012-12-18 11:19:16 +0100

    [IMP] res_groups: add m2m field to see view restrictions with given group
    bzr revid: rco@openerp.com-20121218101916-r97xja82556schfg

--- openerp/addons/base/res/res_users.py
+++ openerp/addons/base/res/res_users.py
@@ -67,6 +67,7 @@ class groups(osv.osv):
         'rule_groups': fields.many2many('ir.rule', 'rule_group_rel',
             'group_id', 'rule_group_id', 'Rules', domain=[('global', '=', False)]),
         'menu_access': fields.many2many('ir.ui.menu', 'ir_ui_menu_group_rel', 'gid', 'menu_id', 'Access Menu'),
+        'view_access': fields.many2many('ir.ui.view', 'ir_ui_view_group_rel', 'group_id', 'view_id', 'Views'),
         'comment' : fields.text('Comment', size=250, translate=True),
         'category_id': fields.many2one('ir.module.category', 'Application', select=True),
         'full_name': fields.function(_get_full_name, type='char', string='Group Name', fnct_search=_search_group),
