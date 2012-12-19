164a8933fb2b79b7112440255546b0f1820c4b42
Vishmita <vja@tinyerp.com>
2012-12-19 15:02:27 +0530

    [FIX]In Application,when create new application it should be comes in Initial Qualification
    bzr revid: vja@tinyerp.com-20121219093227-qxsyow5lix6y2qa3

--- addons/hr_recruitment/hr_recruitment.py
+++ addons/hr_recruitment/hr_recruitment.py
@@ -253,9 +253,12 @@ class hr_applicant(base_stage, osv.Model):
         return {'value': {'department_id': False}}
 
     def onchange_department_id(self, cr, uid, ids, department_id=False, context=None):
-        stage_ids = self.browse(cr, uid,ids,context=context)
-        stage_id = stage_ids and stage_ids[0].stage_id.id or False
-        return {'value': {'stage_id': stage_id}}
+        obj_recru_stage = self.pool.get('hr.recruitment.stage')
+        list_stage_ids = obj_recru_stage.search(cr, uid, ['|',('department_id','=',department_id),('department_id','=',False)], context=context)
+        if ids:
+            stage_ids = self.browse(cr, uid,ids,context=context)[0].stage_id.id
+            return {'value': {'stage_id': stage_ids}}
+        return {'value': {'stage_id': list_stage_ids[0]}}
 
     def onchange_partner_id(self, cr, uid, ids, partner_id, context=None):
         data = {'partner_phone': False,
