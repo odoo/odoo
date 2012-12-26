d7185be38c2d386c1826d7e0aefacf15828d7050
Thibault Delavallée <tde@openerp.com>
2012-12-26 17:25:05 +0100

    [IMP] html_sanitize: now uses the builtin cleaner of lxml.html. We also allows style attribute; the cleaner ensures no javascript or malicious code goes through those attributes. Updated and added tests for the sanitizer, especially about malicious code, and EDI-like html code.
    bzr revid: tde@openerp.com-20121226162505-8rq060p375z1k9il

--- openerp/tools/mail.py
+++ openerp/tools/mail.py
@@ -23,8 +23,8 @@ from lxml import etree
 import cgi
 import logging
 import lxml.html
+import lxml.html.clean as clean
 import openerp.pooler as pooler
-import operator
 import random
 import re
 import socket
@@ -40,71 +40,23 @@ _logger = logging.getLogger(__name__)
 # HTML Sanitizer
 #----------------------------------------------------------
 
+# FIXME: shouldn't this be a whitelist rather than a blacklist?!
+tags_to_kill = ["script", "head", "meta", "title", "link", "style", "frame", "iframe", "base", "object", "embed"]
+tags_to_remove = ['html', 'body', 'font']
+
+
 def html_sanitize(src):
     if not src:
         return src
     src = ustr(src, errors='replace')
-    root = lxml.html.fromstring(u"<div>%s</div>" % src)
-    result = handle_element(root)
-    res = []
-    for element in children(result[0]):
-        if isinstance(element, basestring):
-            res.append(element)
-        else:
-            element.tail = ""
-            res.append(lxml.html.tostring(element))
-    return ''.join(res)
 
-# FIXME: shouldn't this be a whitelist rather than a blacklist?!
-to_remove = set(["script", "head", "meta", "title", "link", "img"])
-to_unwrap = set(["html", "body"])
-
-javascript_regex = re.compile(r"^\s*javascript\s*:.*$", re.IGNORECASE)
-
-def handle_a(el, new):
-    href = el.get("href", "#")
-    if javascript_regex.search(href):
-        href = "#"
-    new.set("href", href)
-
-special = {
-    "a": handle_a,
-}
-
-def handle_element(element):
-    if isinstance(element, basestring):
-        return [element]
-    if element.tag in to_remove:
-        return []
-    if element.tag in to_unwrap:
-        return reduce(operator.add, [handle_element(x) for x in children(element)])
-    result = lxml.html.fromstring("<%s />" % element.tag)
-    for c in children(element):
-        append_to(handle_element(c), result)
-    if element.tag in special:
-        special[element.tag](element, result)
-    return [result]
-
-def children(node):
-    res = []
-    if node.text is not None:
-        res.append(node.text)
-    for child_node in node.getchildren():
-        res.append(child_node)
-        if child_node.tail is not None:
-            res.append(child_node.tail)
-    return res
-
-def append_to(elements, dest_node):
-    for element in elements:
-        if isinstance(element, basestring):
-            children = dest_node.getchildren()
-            if len(children) == 0:
-                dest_node.text = element
-            else:
-                children[-1].tail = element
-        else:
-            dest_node.append(element)
+    # some cases make the parser crash (such as SCRIPT/XSS in test_mail)
+    try:
+        cleaner = clean.Cleaner(page_structure=True, style=False, safe_attrs_only=False, forms=False, kill_tags=tags_to_kill, remove_tags=tags_to_remove)
+        cleaned = cleaner.clean_html(src)
+    except:
+        cleaned = 'Impossible to parse'
+    return cleaned
 
 
 #----------------------------------------------------------
