6d69fe663960991fc86faa7dc0bf31005733b6a1
Ravi Gohil (OpenERP) <rgo@tinyerp.com>
2013-01-01 15:28:10 +0530

    [FIX] report_webkit: Wrong Report template file path throws 'Coercing to Unicode' error : (Maintenance Case : 583490)
    bzr revid: rgo@tinyerp.com-20130101095810-7cbw337ul443q66g

--- addons/report_webkit/webkit_report.py
+++ addons/report_webkit/webkit_report.py
@@ -220,7 +220,7 @@ class WebKitParser(report_sxw):
 
         if report_xml.report_file :
             path = addons.get_module_resource(*report_xml.report_file.split(os.path.sep))
-            if os.path.exists(path) :
+            if path and os.path.exists(path) :
                 template = file(path).read()
         if not template and report_xml.report_webkit_data :
             template =  report_xml.report_webkit_data
