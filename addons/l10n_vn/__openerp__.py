e6be210641bc58bc11117d8d55858e61c8469e7e
Tien Tran <tientranphuoc@gmail.com>
2013-01-18 20:24:38 +0700

    Updated with Stefan comment
    bzr revid: tientranphuoc@gmail.com-20130118132438-hnncmyf38kod493u

--- addons/l10n_vn/__openerp__.py
+++ addons/l10n_vn/__openerp__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010 General Solutions (http://gscom.vn) All Rights Reserved.
+#    This module is Copyright (c) 2009-2013 General Solutions (http://gscom.vn) All Rights Reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
