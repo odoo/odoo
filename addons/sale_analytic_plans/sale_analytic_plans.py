5af2e2ff78dab87eb2fca2b821e341e1340964ad
sgo@tinyerp.com <>
2013-01-21 15:41:04 +0530

    [IMP]remove sale.shop from sale_analytic_plans
    bzr revid: sgo@tinyerp.com-20130121101104-g5xzeitx6exjdrmk

--- addons/sale_analytic_plans/sale_analytic_plans.py
+++ addons/sale_analytic_plans/sale_analytic_plans.py
@@ -39,14 +39,4 @@ class sale_order_line(osv.osv):
 
 sale_order_line()
 
-class sale_order(osv.osv):
-    _inherit = "sale.order"
-
-    def onchange_shop_id(self, cr, uid, ids, shop_id, context=None):
-        # Remove the project_id from the result of super() call, if any, as this field is not in the view anymore
-        res = super(sale_order, self).onchange_shop_id(cr, uid, ids, shop_id, context=context)
-        if res.get('value',{}).get('project_id'):
-            del(res['value']['project_id'])
-        return res
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
