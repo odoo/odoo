9d10d3e198d74679bf9db072175a3031f1fc16c1
Virgil Dupras <virgil.dupras@savoirfairelinux.com>
2013-01-29 11:52:07 -0500

    [FIX] Replaced french leftover tax names in l10n_ca's account_tax_en.xml.
    Replaced TVH by HST and TVQ by QST.
    
    lp bug: https://launchpad.net/bugs/1095382 fixed
    
    bzr revid: virgil.dupras@savoirfairelinux.com-20130129165207-xnul9189p27nqcm1

--- addons/l10n_ca/__openerp__.py
+++ addons/l10n_ca/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Canada - Accounting',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Savoir-faire Linux',
     'website': 'http://www.savoirfairelinux.com',
     'category': 'Localization/Account Charts',
