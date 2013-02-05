86db33296779d8dd173118f44428651e278ae683
niv-openerp <nicolas.vanhoren@openerp.com>
2013-02-05 16:15:06 +0100

    wip
    bzr revid: nicolas.vanhoren@openerp.com-20130205151506-gqy3b1900dsbu1lt

--- addons/live_support/live_support.py
+++ addons/live_support/live_support.py
@@ -50,6 +50,19 @@ class ImportController(openerp.addons.web.http.Controller):
         return req.make_response(env.get_template("loader.js").render(info),
              headers=[('Content-Type', "text/javascript")])
 
+    @openerp.addons.web.http.httprequest
+    def web_page(self, req, **kwargs):
+        p = json.loads(kwargs["p"])
+        db = p["db"]
+        channel = p["channel"]
+        req.session._db = db
+        req.session._uid = None
+        req.session._login = "anonymous"
+        req.session._password = "anonymous"
+        script = req.session.model('live_support.channel').read(channel, ["script"])["script"]
+        return req.make_response(env.get_template("web_page.html").render({"script": script}),
+             headers=[('Content-Type', "text/html")])
+
     @openerp.addons.web.http.jsonrequest
     def available(self, req, db, channel):
         req.session._db = db
@@ -71,10 +84,28 @@ class live_support_channel(osv.osv):
                     break
         return res
 
+    def _script(self, cr, uid, ids, name, arg, context=None):
+        res = {}
+        for record in self.browse(cr, uid, ids, context=context):
+            res[record.id] = env.get_template("include.html").render({
+                "url": self.pool.get('ir.config_parameter').get_param(cr, uid, 'web.base.url'),
+                "parameters": {"db":cr.dbname, "channel":record.id},
+            })
+        return res
+
+    def _web_page(self, cr, uid, ids, name, arg, context=None):
+        res = {}
+        for record in self.browse(cr, uid, ids, context=context):
+            res[record.id] = self.pool.get('ir.config_parameter').get_param(cr, uid, 'web.base.url') + \
+                "/live_support/web_page?p=" + json.dumps({"db":cr.dbname, "channel":record.id})
+        return res
+
     _columns = {
         'name': fields.char(string="Name", size=200, required=True),
         'user_ids': fields.many2many('im.user', 'live_support_channel_im_user', 'channel_id', 'user_id', string="Users"),
         'are_you_inside': fields.function(_are_you_inside, type='boolean', string='Are you inside the matrix?', store=False),
+        'script': fields.function(_script, type='text', string='Script', store=False),
+        'web_page': fields.function(_web_page, type='url', string='Web Page', store=False, size="200"),
     }
 
     def get_available_user(self, cr, uid, channel_id, context=None):
