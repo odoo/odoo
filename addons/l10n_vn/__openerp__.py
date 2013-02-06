5171a1d716c310be3d78fae08d4ad8eae459a34a
Tien Tran <tientranphuoc@gmail.com>
2013-02-06 23:06:57 +0700

    change module description and add vn flag icon
    bzr revid: tientranphuoc@gmail.com-20130206160657-d5aml51ogq6stnnz

--- addons/l10n_vn/__openerp__.py
+++ addons/l10n_vn/__openerp__.py
@@ -25,10 +25,18 @@
     "author" : "General Solutions",
     'website': 'http://gscom.vn',
     "category" : "Localization/Account Charts",
-    "description": "This is the module to manage the accounting chart for Vietnam.",
+    "description": """
+This is the module to manage the accounting chart for Vietnam in OpenERP.
+========================================================================
+
+This module applies to companies based in Vietnamese Accounting Standard (VAS).
+
+**Credits:** General Solutions.
+""",
     "depends" : ["account","base_vat","base_iban"],
     "data" : ["account_tax_code.xml","account_chart.xml","account_tax.xml","l10n_vn_wizard.xml"],
     "demo" : [],
     'auto_install': False,
     "installable": True,
 }
+
