89d45ad9ecddcce6ed7f2e28209d9db210a333a7
Paramjit Singh Sahota <psa@tinyerp.com>
2013-02-14 12:06:55 +0530

    [IMP] Added message post for 'Invoice Paid.' when the invoice is paid in PO.
    bzr revid: psa@tinyerp.com-20130214063655-15kjqgc67fwcqw9f

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -557,6 +557,7 @@ class purchase_order(osv.osv):
 
     def invoice_done(self, cr, uid, ids, context=None):
         self.write(cr, uid, ids, {'state':'approved'}, context=context)
+        self.message_post(cr, uid, ids, body=_("Invoice <b>Paid.</b>"), context=context)
         return True
 
     def has_stockable_product(self, cr, uid, ids, *args):
