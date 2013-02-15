a388774265e6c926386233dc335f01f9e8d65144
Bhumi Thakkar (Open ERP) <bth@tinyerp.com>
2013-02-15 18:49:33 +0530

    [IMP] Message on chatter Quotation created for SO and RFQ created when PO created when record created.
    bzr revid: bth@tinyerp.com-20130215131933-moln7gh4563r1gdx

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -338,7 +338,10 @@ class sale_order(osv.osv):
     def create(self, cr, uid, vals, context=None):
         if vals.get('name','/')=='/':
             vals['name'] = self.pool.get('ir.sequence').get(cr, uid, 'sale.order') or '/'
-        return super(sale_order, self).create(cr, uid, vals, context=context)
+        context = dict(context, mail_create_nolog=True)        
+        res = super(sale_order, self).create(cr, uid, vals, context=context)
+        self.message_post(cr, uid, [res], body=_("Quotation <b>Created</b>"), context=context)
+        return res
 
     def button_dummy(self, cr, uid, ids, context=None):
         return True
