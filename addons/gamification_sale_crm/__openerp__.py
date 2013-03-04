102064ee0d7db94c6abd07ff3b3f44b3b732d8d2
Martin Trigaux <mat@openerp.com>
2013-03-04 16:33:25 +0100

    [ADD] data for gamification plan in the sale_crm module
    bzr revid: mat@openerp.com-20130304153325-nzc6f04groiuqsn8

---
+++ addons/gamification_sale_crm/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2013 Tiny SPRL (<http://openerp.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'CRM Gamification',
+    'version': '1.0',
+    'author': 'OpenERP SA',
+    'category': 'CRM',
+    'depends': ['gamification','sale_crm'],
+    'description': """Example of goal types and plans that can be used related to the usage of the CRM Sale module.""",
+
+    'data': [
+    	'sale_crm_goals.xml',
+        ],
+    'auto_install': True,
+}
