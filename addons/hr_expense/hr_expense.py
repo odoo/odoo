ee72f0953aec63c1f5f5444541a9075e0f3c9c43
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2013-03-06 15:38:37 +0100

    [FIX] hr_expense: typo
    bzr revid: qdp-launchpad@openerp.com-20130306143837-gc7edztg71h5hi56

--- addons/hr_expense/hr_expense.py
+++ addons/hr_expense/hr_expense.py
@@ -166,7 +166,7 @@ class hr_expense_expense(osv.osv):
             if not journal_id:
                 raise osv.except_osv(_('Error!'), _("No expense journal found. Please make sure you have a journal with type 'purchase' configured."))
             journal_id = journal_id[0]
-        return self.pool.get('account_move').account_move_prepare(cr, uid, journal_id, date=date, ref=ref, company_id=company_id, context=context)
+        return self.pool.get('account.move').account_move_prepare(cr, uid, journal_id, date=date, ref=ref, company_id=company_id, context=context)
 
     def line_get_convert(self, cr, uid, x, part, date, context=None):
         #partner_id  = self.pool.get('res.partner')._find_partner(part)
