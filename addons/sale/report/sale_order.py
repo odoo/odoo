80674364689bc4544df9355dc50213a6bb9755c1
Josse Colpaert <jco@openerp.com>
2013-03-07 09:25:10 +0100

    [FIX] Discount should only appear on sales order if it is checked in the settings.
    bzr revid: jco@openerp.com-20130307082510-mckayx175qu9ispo

--- addons/sale/report/sale_order.py
+++ addons/sale/report/sale_order.py
@@ -27,8 +27,17 @@ class order(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context=None):
         super(order, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
-            'time': time,
+            'time': time, 
+            'show_discount':self._show_discount, 
         })
+
+    def _show_discount(self, uid, context=None):
+        cr = self.cr
+        try: 
+            group_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'sale', 'group_discount_per_so_line')[1]
+        except:
+            return False
+        return group_id in [x.id for x in self.pool.get('res.users').browse(cr, uid, uid, context=context).groups_id]
 
 report_sxw.report_sxw('report.sale.order', 'sale.order', 'addons/sale/report/sale_order.rml', parser=order, header="external")
 
