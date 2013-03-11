80e667a53fd3a473df31040dc22b3cac55319325
Christophe Simonis <chs@openerp.com>
2013-03-11 15:16:54 +0100

    [CHERRYPICK 7.0][FIX] crm_partner_assign: Use Google Maps API V3.
    Old API is deprecated for 3 years!
    
    bzr revid: chs@openerp.com-20130311141654-o6pniq9mnth00plm

--- addons/crm_partner_assign/partner_geo_assign.py
+++ addons/crm_partner_assign/partner_geo_assign.py
@@ -19,29 +19,36 @@
 #
 ##############################################################################
 
+import urllib
+import random
+
+try:
+    import simplejson as json
+except ImportError:
+    import json     # noqa
+
 from openerp.osv import osv
 from openerp.osv import fields
-import urllib,re
-import random, time
 from openerp.tools.translate import _
 from openerp import tools
 
 def geo_find(addr):
-    addr = addr.encode('utf8')
-    regex = '<coordinates>([+-]?[0-9\.]+),([+-]?[0-9\.]+),([+-]?[0-9\.]+)</coordinates>'
-    url = 'http://maps.google.com/maps/geo?q=' + urllib.quote(addr) + '&output=xml&oe=utf8&sensor=false'
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address='
+    url += urllib.quote(addr.encode('utf8'))
+
     try:
-        xml = urllib.urlopen(url).read()
+        result = json.load(urllib.urlopen(url))
     except Exception, e:
         raise osv.except_osv(_('Network error'),
                              _('Cannot contact geolocation servers. Please make sure that your internet connection is up and running (%s).') % e)
+    if result['status'] != 'OK':
+        return None
 
-    if '<error>' in xml:
+    try:
+        geo = result['results'][0]['geometry']['location']
+        return float(geo['lat']), float(geo['lng'])
+    except (KeyError, ValueError):
         return None
-    result = re.search(regex, xml, re.M|re.I)
-    if not result:
-        return None
-    return float(result.group(2)),float(result.group(1))
 
 def geo_query_address(street=None, zip=None, city=None, state=None, country=None):
     if country and ',' in country and (country.endswith(' of') or country.endswith(' of the')):
