7a14260535ed35126167dd2c633fd4e1054a0c19
Ajay Chauhan (OpenERP) <cha@tinyerp.com>
2013-03-12 16:25:16 +0530

    [IMP] account: change string of three fields to solve import issue of 'Taxes'
    bzr revid: cha@tinyerp.com-20130312105516-4eidfo5322rbb6tc

--- addons/account/account.py
+++ addons/account/account.py
@@ -1878,7 +1878,7 @@ class account_tax(osv.osv):
         'child_depend':fields.boolean('Tax on Children', help="Set if the tax computation is based on the computation of child taxes rather than on the total amount."),
         'python_compute':fields.text('Python Code'),
         'python_compute_inv':fields.text('Python Code (reverse)'),
-        'python_applicable':fields.text('Python Code'),
+        'python_applicable':fields.text('Applicable Code'),
 
         #
         # Fields used for the Tax declaration
@@ -1892,8 +1892,8 @@ class account_tax(osv.osv):
 
         'ref_base_code_id': fields.many2one('account.tax.code', 'Refund Base Code', help="Use this code for the tax declaration."),
         'ref_tax_code_id': fields.many2one('account.tax.code', 'Refund Tax Code', help="Use this code for the tax declaration."),
-        'ref_base_sign': fields.float('Base Code Sign', help="Usually 1 or -1."),
-        'ref_tax_sign': fields.float('Tax Code Sign', help="Usually 1 or -1."),
+        'ref_base_sign': fields.float('Base Sign Code', help="Usually 1 or -1."),
+        'ref_tax_sign': fields.float('Tax Sign Code', help="Usually 1 or -1."),
         'include_base_amount': fields.boolean('Included in base amount', help="Indicates if the amount of tax must be included in the base amount for the computation of the next taxes"),
         'company_id': fields.many2one('res.company', 'Company', required=True),
         'description': fields.char('Tax Code'),
