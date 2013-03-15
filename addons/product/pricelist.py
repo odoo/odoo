5cb1835e26a3868df3a0a71b3329bea17f455327
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-03-15 15:24:03 +0100

    [FIX] add missing context on compute call in pricelist price_get_multi context
    lp bug: https://launchpad.net/bugs/1155624 fixed
    
    bzr revid: yannick.vaucher@camptocamp.com-20130315142403-gz7fy7etwzls96rt

--- addons/product/pricelist.py
+++ addons/product/pricelist.py
@@ -236,7 +236,10 @@ class product_pricelist(osv.osv):
                                         qty, context=context)[res['base_pricelist_id']]
                                 ptype_src = self.browse(cr, uid, res['base_pricelist_id']).currency_id.id
                                 uom_price_already_computed = True
-                                price = currency_obj.compute(cr, uid, ptype_src, res['currency_id'], price_tmp, round=False)
+                                price = currency_obj.compute(cr, uid,
+                                        ptype_src, res['currency_id'],
+                                        price_tmp, round=False,
+                                        context=context)
                         elif res['base'] == -2:
                             # this section could be improved by moving the queries outside the loop:
                             where = []
