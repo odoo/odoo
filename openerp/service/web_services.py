ffd258e3f0de02e8c8aef8838b00a5c5b379fc2f
Olivier Dony <odo@openerp.com>
2013-03-18 15:41:56 +0100

    [FIX] web_services: incorrect error message in logfile after failed DB dump, courtesy of Brendan Clune (Logic Supply)
    lp bug: https://launchpad.net/bugs/1124440 fixed
    
    bzr revid: odo@openerp.com-20130318144156-q7yw9qy83tngdg6x

--- openerp/service/web_services.py
+++ openerp/service/web_services.py
@@ -279,7 +279,7 @@ class db(netsvc.ExportService):
             if not data or res:
                 logger.error(
                         'DUMP DB: %s failed! Please verify the configuration of the database password on the server. '
-                        'It should be provided as a -w <PASSWD> command-line option, or as `db_password` in the '
+                        'You may need to create a .pgpass file for authentication, or specify `db_password` in the '
                         'server configuration file.\n %s', db_name, data)
                 raise Exception, "Couldn't dump database"
             logger.info('DUMP DB successful: %s', db_name)
