333e83fc5ba22bcd16cfd3b4f492710c2c4a8d5e
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2013-03-18 13:27:48 +0100

    [REF] account_payment: removal of amount_to_pay field on account.move.line and account.invoice, replaced by amount_residual
    bzr revid: qdp-launchpad@openerp.com-20130318122748-ac9k5bhka1foadvr

--- addons/account_payment/account_invoice.py
+++ addons/account_payment/account_invoice.py
@@ -19,9 +19,8 @@
 #
 ##############################################################################
 
-from datetime import datetime
 from openerp.tools.translate import _
-from openerp.osv import fields, osv
+from openerp.osv import osv
 
 class Invoice(osv.osv):
     _inherit = 'account.invoice'
@@ -42,13 +41,6 @@ class Invoice(osv.osv):
                 payment_order_name = ','.join(map(lambda x: x.order_id.reference, pay_line))
                 raise osv.except_osv(_('Error!'), _("You cannot cancel an invoice which has already been imported in a payment order. Remove it from the following payment order : %s."%(payment_order_name)))
         return super(Invoice, self).action_cancel(cr, uid, ids, context=context)
-
-
-    _columns = {
-        'amount_to_pay': fields.related('residual',
-            type='float', string='Amount to be paid',
-            help='The amount which should be paid at the current date. '),
-    }
 
 Invoice()
 
