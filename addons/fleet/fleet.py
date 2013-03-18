6adefa6ef1056b04afaec7c34657c6f32d9daf3f
ajay javiya (OpenERP) <aja@tinyerp.com>
2013-03-18 16:42:02 +0530

    [IMP]:Label
    bzr revid: aja@tinyerp.com-20130318111202-iflro7fty315cddh

--- addons/fleet/fleet.py
+++ addons/fleet/fleet.py
@@ -351,7 +351,7 @@ class fleet_vehicle(osv.Model):
         'fuel_type': fields.selection([('gasoline', 'Gasoline'), ('diesel', 'Diesel'), ('electric', 'Electric'), ('hybrid', 'Hybrid')], 'Fuel Type', help='Fuel Used by the vehicle'),
         'horsepower': fields.integer('Horsepower'),
         'horsepower_tax': fields.float('Horsepower Taxation'),
-        'power': fields.integer('Power (kW)', help='Power in kW of the vehicle'),
+        'power': fields.integer('Power', help='Power in kW of the vehicle'),
         'co2': fields.float('CO2 Emissions', help='CO2 emissions of the vehicle'),
         'image': fields.related('model_id', 'image', type="binary", string="Logo"),
         'image_medium': fields.related('model_id', 'image_medium', type="binary", string="Logo"),
