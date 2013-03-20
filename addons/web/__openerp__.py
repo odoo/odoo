8fd08f900f99bac57bf4b7db923692e9fd9ae54e
Christophe Simonis <chs@openerp.com>
2013-03-20 15:36:52 +0100

    [FIX] web: Fix old versions of Webkit (such as ones used on iOS < 6 or PhantomJS <= 1.7) which does not have Function.prototype.bind function
    bzr revid: chs@openerp.com-20130320143652-xqglvte5kwvtwl5i

--- addons/web/__openerp__.py
+++ addons/web/__openerp__.py
@@ -13,6 +13,7 @@ This module provides the core of the OpenERP Web Client.
     'auto_install': True,
     'post_load': 'wsgi_postload',
     'js' : [
+        "static/src/fixbind.js",
         "static/lib/datejs/globalization/en-US.js",
         "static/lib/datejs/core.js",
         "static/lib/datejs/parser.js",
