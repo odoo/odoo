7175b3e3fb7d482976239a3da3e5d571d4896be6
Turkesh Patel (Open ERP) <tpa@tinyerp.com>
2013-03-21 11:08:55 +0530

    [IMP] make strings translatable
    bzr revid: tpa@tinyerp.com-20130321053855-2cg08qtcsjrouo35

--- addons/account/account_invoice.py
+++ addons/account/account_invoice.py
@@ -420,7 +420,7 @@ class account_invoice(osv.osv):
             'mark_invoice_as_sent': True,
             })
         return {
-            'name': 'Compose Email',
+            'name': _('Compose Email'),
             'type': 'ir.actions.act_window',
             'view_type': 'form',
             'view_mode': 'form',
--- addons/mail/static/src/js/mail.js
+++ addons/mail/static/src/js/mail.js
@@ -1932,7 +1932,7 @@ openerp.mail = function (session) {
             this.$(".oe_write_full").click(function (event) {
                 event.stopPropagation();
                 var action = {
-                    name: 'Compose Email',
+                    name: _t('Compose Email'),
                     type: 'ir.actions.act_window',
                     res_model: 'mail.compose.message',
                     view_mode: 'form',
