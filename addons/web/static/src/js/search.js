03330ea230f51e392942a1db00f246c49f7e7cc0
Xavier Morel <xmo@openerp.com>
2013-03-22 11:36:06 +0100

    [FIX] minor presentational offset issue
    missed mis-handling of oe_selected in xmo@openerp.com-20130321155123-211iht7c6zme712e
    
    bzr revid: xmo@openerp.com-20130322103606-60ayhrsn5gmgyu2h

--- addons/web/static/src/js/search.js
+++ addons/web/static/src/js/search.js
@@ -1046,7 +1046,9 @@ instance.web.search.FilterGroup = instance.web.search.Input.extend(/** @lends in
         facet.values.each(function (v) {
             var i = _(self.filters).indexOf(v.get('value'));
             if (i === -1) { return; }
-            $filters.eq(i).addClass('oe_selected');
+            $filters.filter(function () {
+                return Number($(this).data('index')) === i;
+            }).addClass('oe_selected');
         });
     },
     /**
