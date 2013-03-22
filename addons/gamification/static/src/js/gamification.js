391947f7cc94c237d05f970872fdbec02f56fc65
Martin Trigaux <mat@openerp.com>
2013-03-22 16:18:03 +0100

    [IMP] using sidebar and better javascript
    bzr revid: mat@openerp.com-20130322151803-nzb41s0mxvaidrel

--- addons/gamification/static/src/js/gamification.js
+++ addons/gamification/static/src/js/gamification.js
@@ -1,7 +1,9 @@
 openerp.gamification = function(instance) {
     console.log("Debug statement: file loaded");
+    var QWeb = instance.web.qweb;
 
-    instance.Side_bar = instance.mail.Widget.include( {
+    instance.gamification.Sidebar = instance.web.Widget.extend({
+        template: 'gamification.user_wall_sidebar',
         init: function (parent, action) {
             var self = this;
             this._super(parent, action);
@@ -12,8 +14,7 @@ openerp.gamification = function(instance) {
         events: {
             'click a.oe_update_goal': function(event) {
                 var self = this;
-                var goal_id = parseInt(event.target.id);
-                console.log(goal_id);
+                var goal_id = parseInt(event.currentTarget.id);
                 var goal_updated = new instance.web.Model('gamification.goal').call('update', [[goal_id]]);
                 $.when(goal_updated).done(function() {
                     self.get_goal_todo_info();
@@ -23,37 +24,46 @@ openerp.gamification = function(instance) {
         renderElement: function() {
             var self = this;
             this._super.apply(this, arguments);
-            self.render_template(self.$el.filter(".oe_user_sidebar"),'user_wall');
             self.get_goal_todo_info();
         },
         render_template: function(target,template) {
             var self = this;
-            target.append(instance.web.qweb.render(template,{'widget': self}));
+            target.append(QWeb.render(template,{'widget': self}));
         },
         render_template_replace: function(target,template) {
             var self = this;
-            target.html(instance.web.qweb.render(template,{'widget': self}));
+            target.html(QWeb.render(template,{'widget': self}));
         },
         get_goal_todo_info: function() {
             var self = this;
-            console.log(this);
             var goals_info = this.res_user.call('get_goals_todo_info', {}).then(function(res) {
                 self.goals_info['info'] = res
             });
             $.when(goals_info).done(function() {
                 console.log(self.goals_info.info);
                 if(self.goals_info.info.length > 0){
-                    self.render_template_replace(self.$el.find(".oe_gamification_goal"),'goal_list_to_do');
+                    self.render_template_replace(self.$el.filter(".oe_gamification_goal"),'gamification.goal_list_to_do');
                 }
             });
         },
         update_goal: function() {
-            console.log(this);
-            console.log(instance.Side_bar);
             var goal_update = new instance.web.Model('gamification.goal').call('update', [[parseInt(this.id)]]);
             $.when(goal_update).done(function() {
-                // $(".oe_user_sidebar").find(".oe_gamification_goal").html(instance.web.qweb.render('goal_list_to_do'));
+                $(".oe_user_sidebar").find(".oe_gamification_goal").html(QWeb.render('gamification.goal_list_to_do'));
             });
         },
+    });
+
+    instance.mail.Widget.include({
+        init: function (parent, action) {
+            var self = this;
+            this._super(parent, action);
+        },
+        start: function() {
+            this._super();
+            var self = this;
+            var sidebar = new instance.gamification.Sidebar(self);
+            sidebar.appendTo($('.oe_mail_wall_aside'));
+        }
     });
 };
