1fd2b50bde2acff1e0177ac90c0f8d003b3d4fdd
Cedric Snauwaert <csn@openerp.com>
2013-03-22 13:43:33 +0100

    [FIX]purchase_resconfig: add missing onchange
    bzr revid: csn@openerp.com-20130322124333-v1nl0kv1q1a17myy

--- addons/purchase/res_config.py
+++ addons/purchase/res_config.py
@@ -66,6 +66,11 @@ Example: Product: this product is deprecated, do not purchase more than 5.
         'default_invoice_method': 'manual',
     }
 
+    def onchange_purchase_analytic_plans(self, cr, uid, ids, module_purchase_analytic_plans, context=None):
+        """ change group_analytic_account_for_purchases following module_purchase_analytic_plans """
+        if not module_purchase_analytic_plans:
+            return {}
+        return {'value': {'group_analytic_account_for_purchases': module_purchase_analytic_plans}}
 
 
 
