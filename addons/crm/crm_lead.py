9c1b0f6164617faedef6f4e9a800641b83d94343
dle@openerp.com <>
2013-04-02 12:13:31 +0200

    [FIX]crm: crm lead/opp merge, when opp has stage with state cancel, then it is less important than leads with stage which are not in state cancel
    bzr revid: dle@openerp.com-20130402101331-12skhykfzrv35nno

--- addons/crm/crm_lead.py
+++ addons/crm/crm_lead.py
@@ -632,7 +632,7 @@ class crm_lead(base_stage, format_address, osv.osv):
             sequence = -1
             if opportunity.stage_id and opportunity.stage_id.state != 'cancel':
                 sequence = opportunity.stage_id.sequence
-            sequenced_opps.append(((int(opportunity.type == 'opportunity'), sequence, -opportunity.id), opportunity))
+            sequenced_opps.append(((int(sequence != -1 and opportunity.type == 'opportunity'), sequence, -opportunity.id), opportunity))
 
         sequenced_opps.sort(reverse=True)
         opportunities = map(itemgetter(1), sequenced_opps)
