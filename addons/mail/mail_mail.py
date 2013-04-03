d300e2b0cee3141c9dfd48e9b1c0cb9bfbd524d9
Thibault Delavallée <tde@openerp.com>
2013-04-03 14:10:22 +0200

    [FIX] mail: notifications: email_to now contains reference to 'Followers of Doc Name' when having a related document.
    bzr revid: tde@openerp.com-20130403121022-lijarue6r2t7mh3w

--- addons/mail/mail_mail.py
+++ addons/mail/mail_mail.py
@@ -232,7 +232,19 @@ class mail_mail(osv.Model):
         subject = self.send_get_mail_subject(cr, uid, mail, partner=partner, context=context)
         reply_to = self.send_get_mail_reply_to(cr, uid, mail, partner=partner, context=context)
         body_alternative = tools.html2plaintext(body)
-        email_to = ['%s <%s>' % (partner.name, partner.email)] if partner else tools.email_split(mail.email_to)
+
+        # generate email_to, heuristic:
+        # 1. if 'partner' is specified and there is a related document: Followers of 'Doc' <email>
+        # 2. if 'partner' is specified, but no related document: Partner Name <email>
+        # 3; fallback on mail.email_to that we split to have an email addresses list
+        if partner and mail.record_name:
+            sanitized_record_name = re.sub(r'[^\w+.]+', '-', mail.record_name)
+            email_to = [_('"Followers of %s" <%s>') % (sanitized_record_name, partner.email)]
+        elif partner:
+            email_to = ['%s <%s>' % (partner.name, partner.email)]
+        else:
+            email_to = tools.email_split(mail.email_to)
+
         return {
             'body': body,
             'body_alternative': body_alternative,
