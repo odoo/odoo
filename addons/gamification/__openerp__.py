ff12c44391021e0d9792d18b576fd0fe9fe90e0d
Martin Trigaux <mat@openerp.com>
2013-04-10 16:57:09 +0200

    [IMP] better text
    bzr revid: mat@openerp.com-20130410145709-crvmss21ocw7e0m8

--- addons/gamification/__openerp__.py
+++ addons/gamification/__openerp__.py
@@ -27,21 +27,14 @@
     'description': """
 Gamification process
 ====================
-The Gamification module provide ways to evaluate and motivate the users of OpenERP.
-The two main functions are goals and badges.
+The Gamification module provides ways to evaluate and motivate the users of OpenERP.
 
-Goals
------
-A **Goal** is an objective applied to an user with a numerical target to reach. It can have starting and end date. Users usually do not create goals but relies on goal plans.
+The users can be evaluated using goals and numerical objectives to reach.
+**Goals** are assigned through **plans** to evaluate and compare members of a team with each others and through time.
 
-A **Goal Type** is a generic objective that can be applied to any structure stored in the database and use numerical value. The creation of goal types is quite technical and should rarely be done. Once a generic goal is created, it can be associated to several goal plans with different numerical targets.
+For non-numerical achievements, **badges** can be granted to users. From a simple "thank you" to an exceptional achievement, a badge is an easy way to exprimate gratitude to a user for their good work.
 
-A **Goal Plan** is a a set of goal types with a target value to reach applied to a group of users. It can be periodic to create and evaluate easily the performances of a team.
-
-Badges
-------
-A **Badge** is a symbolic token granted to a user as a sign of reward. It can be offered by a user to another or automatically offered when some conditions are met. The conditions can either be a list of goal types succeeded or a user definied python code executed.
-
+Both goals and badges are flexibles and can be adapted to a large range of modules and actions. When installed, this module creates easy goals to help new users to discover OpenERP and configure their user profile.
 """,
 
     'data': [
