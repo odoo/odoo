41146460c027d8839fbe3f49d60f2566785d25bf
Olivier Dony <odo@openerp.com>
2013-04-22 19:04:20 +0200

    [FIX] rml2html: incorrect license headers in some source files, reported and contributed by Alec Leamas
    Mixing LGPL 2.1 and AGPL 3.0 in the same source file
    is incorrect, and the AGPLv3 license was added by
    mistake: keep only LGPL license and preserve
    copypright info.
    
    lp bug: https://launchpad.net/bugs/1167034 fixed
    
    bzr revid: odo@openerp.com-20130422170420-in1wo45v3q8q0fhr

--- openerp/report/render/rml2html/rml2html.py
+++ openerp/report/render/rml2html/rml2html.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
-#
-##############################################################################
-
 # Copyright (C) 2005, Fabien Pinckaers, UCL, FSA
+# Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -34,6 +17,8 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+##############################################################################
 
 import sys
 import cStringIO
