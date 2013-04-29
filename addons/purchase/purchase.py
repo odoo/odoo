d2ebc393fcdf9c6335c95df243904a8de1c18ed8
Cedric Snauwaert <csn@openerp.com>
2013-04-29 13:48:32 +0200

    [FIX]purchase: in a PO line, description of product should take into account product supplier name and code
    bzr revid: csn@openerp.com-20130429114832-jqtes30p1qnywotz

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -948,7 +948,7 @@ class purchase_order_line(osv.osv):
             lang = res_partner.browse(cr, uid, partner_id).lang
             context_partner.update( {'lang': lang, 'partner_id': partner_id} )
         product = product_product.browse(cr, uid, product_id, context=context_partner)
-        name = product.name
+        id_p, name = product_product.name_get(cr, uid, product_id, context=context_partner)[0]
         if product.description_purchase:
             name += '\n' + product.description_purchase
         res['value'].update({'name': name})
