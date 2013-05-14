66544243880e9c37752afb9d38e54673a83c96c4
Martin Trigaux <mat@openerp.com>
2013-05-14 16:37:28 +0200

    [IMP] use exact writing for fsf address
    bzr revid: mat@openerp.com-20130514143728-s341hmkw5dw9l7o1

--- addons/base_report_designer/plugin/openerp_report_designer/bin/script/lib/actions.py
+++ addons/base_report_designer/plugin/openerp_report_designer/bin/script/lib/actions.py
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
 # See:  http://www.gnu.org/licenses/lgpl.html
 #
