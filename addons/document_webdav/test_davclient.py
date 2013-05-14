3c11652facfe1c540b005fb961e20a48b7a1b3d9
Martin Trigaux <mat@openerp.com>
2013-05-14 16:11:32 +0200

    [IMP] trmove double space in fsf address
    bzr revid: mat@openerp.com-20130514141132-610k885sxtcr8vpb

--- addons/document_webdav/test_davclient.py
+++ addons/document_webdav/test_davclient.py
@@ -23,7 +23,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 ###############################################################################
 
 """ A trivial HTTP/WebDAV client, used for testing the server
