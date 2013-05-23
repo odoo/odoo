653495b23f35fe36153234556e6361ea72a00157
alexandre.fayolle@camptocamp.com <>
2013-05-23 07:39:49 +0200

    [FIX] missing import in previous commmit
    bzr revid: alexandre.fayolle@camptocamp.com-20130523053949-cvvc8k9y0clx5pot

--- addons/mrp/mrp.py
+++ addons/mrp/mrp.py
@@ -23,7 +23,7 @@ import time
 from datetime import datetime
 
 import openerp.addons.decimal_precision as dp
-from openerp.osv import fields, osv
+from openerp.osv import fields, osv, orm
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT, DATETIME_FORMATS_MAP
 from openerp.tools import float_compare
 from openerp.tools.translate import _
