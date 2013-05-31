2abd4d22227e8ad3acc9e7b4a11e1c8d0a8925f4
Arnaud Pineux <api@openerp.com>
2013-05-31 11:49:36 +0200

    [FIX] return back in breadcrumb
    bzr revid: api@openerp.com-20130531094936-a27pmhrh4iqb2jre

--- addons/purchase_requisition/static/src/js/web_addons.js
+++ addons/purchase_requisition/static/src/js/web_addons.js
@@ -23,7 +23,7 @@ openerp.purchase_requisition = function(instance) {
         generate_purchase_order: function () {
             var self = this;
             new instance.web.Model(self.dataset.model).call("generate_po",[self.dataset.context.active_id,self.dataset.context]).then(function(result) {
-                self.do_action(result);
+                self.ViewManager.ActionManager.history_back();
             });
         },
     });
