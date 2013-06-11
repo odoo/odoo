0a44c547862b917d047a51c43f2c89d4843b3c2f
Jignesh Rathod (OpenERP) <jir@tinyerp.com>
2013-06-11 19:01:41 +0530

    Improve code.
    bzr revid: jir@tinyerp.com-20130611133141-v9jm2gjst33bdf4c

--- addons/hr_applicant_document/hr_applicant_document.py
+++ addons/hr_applicant_document/hr_applicant_document.py
@@ -27,31 +27,31 @@ from openerp.osv import fields, osv
 from openerp.tools.translate import _
 
 class applicant_document(osv.osv):
-	_name = 'hr.applicant'
-	_inherit = 'hr.applicant'
+    _name = 'hr.applicant'
+    _inherit = 'hr.applicant'
 
-	def _get_index_content(self, cr, uid, ids, fields, args, context=None):
-		res = {}
-		attachment_pool = self.pool.get('ir.attachment')
-		for applicant in self.browse(cr, uid, ids, context=context):
-			res[applicant.id] = 0
-			attach_id = attachment_pool.search(cr, uid, [('res_model','=','hr.applicant'),('res_id','=',applicant.id)])
-			if attach_id:
-				for attach in attachment_pool.browse(cr, uid, attach_id, context):
-					res[applicant.id] = attach.index_content
-		return res
+    def _get_index_content(self, cr, uid, ids, fields, args, context=None):
+        res = {}
+        attachment_pool = self.pool.get('ir.attachment')
+        for applicant in self.browse(cr, uid, ids, context=context):
+            res[applicant.id] = 0
+            attach_id = attachment_pool.search(cr, uid, [('res_model','=','hr.applicant'),('res_id','=',applicant.id)])
+            if attach_id:
+                for attach in attachment_pool.browse(cr, uid, attach_id, context):
+                    res[applicant.id] = attach.index_content
+        return res
 
-	def _content_search(self, cursor, user, obj, name, args, context=None):
-		record_ids = []
-		attachment_pool = self.pool.get('ir.attachment')
-		args += [('res_model','=','hr.applicant')]
-		attach_ids = attachment_pool.search(cursor, user, args)
-		for attach in attachment_pool.browse(cursor, user, attach_ids):
-			record_ids.append(attach.res_id)			
-		return [('id', 'in', record_ids)]
+    def _content_search(self, cursor, user, obj, name, args, context=None):
+        record_ids = []
+        attachment_pool = self.pool.get('ir.attachment')
+        args += [('res_model','=','hr.applicant')]
+        attach_ids = attachment_pool.search(cursor, user, args)
+        for attach in attachment_pool.browse(cursor, user, attach_ids):
+            record_ids.append(attach.res_id)			
+        return [('id', 'in', record_ids)]
 
-	_columns = {
-	'index_content': fields.function(_get_index_content, string='Index Content', \
+    _columns = {
+        'index_content': fields.function(_get_index_content, string='Index Content', \
                                  fnct_search=_content_search,type="text"),
-	}
+    }
 
