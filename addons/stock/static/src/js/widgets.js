1e20612f170e09026b8186e9061cc71babfa5916
Frédéric van der Essen <fva@openerp.com>
2013-06-26 15:11:31 +0200

    [IMP] stock.ui: styling
    bzr revid: fva@openerp.com-20130626131131-8hn0f5cmqw19k7no

--- addons/stock/static/src/js/widgets.js
+++ addons/stock/static/src/js/widgets.js
@@ -2,97 +2,18 @@
 function openerp_picking_widgets(instance){
     var module = instance.stock;
 
-    /*
-     * new TactileListWidget(parent,{
-     *   height: '500px',
-     *   width:  '100%',
-     *   title:  'Sample List',
-     *   collumns: [
-     *      {header:'Foobar', classes:'oe_small'},
-     *      {header:'Foobar', classes:'oe_medium'},
-     *      {header:''      , classes:'oe_big'},
-     *      {header:'BarFoo', classes:'oe_expand'},
-     *   ],
-     *   rows: [
-     *      {cols: [0,1,2,3] },
-     *      {cols: [1,2,3,4], classes:'oe_selected oe_invalid'},
-     *      {cols: [2,1,3,4] },
-     *   ]
-     * });
-     */
-
-    module.TactileListWidget = instance.web.Widget.extend({
-        template: 'TactileListWidget',
-        init: function(parent, options){
-            this._super(parent,options);
-            this._collumns = options.collumns || [];
-            this._rows = options.rows || [];
-            this._title = options.title || '';
-            this._width = options.width || 625;
-            this._height = options.height || 200;
-            this._scrollbarwidth = 15;
-        },
-        renderElement: function(){
-            var self = this;
-            this._super();
-            this.$('.js_vresizer, .js_hresizer').mousedown(function(event){
-                self._startsize = {height: self.get_height(), width: self.get_width()};
-                console.log('drag init:',self._startsize);
-            });
-            this.$('.js_vresizer').drag(function(event,dragevent){
-                self.set_height(self._startsize.height + dragevent.deltaY);
-            });
-            this.$('.js_hresizer').drag(function(event,dragevent){
-                self.set_height(self._startsize.height + dragevent.deltaY);
-                self.set_width(self._startsize.width + dragevent.deltaX);
-            });
-        },
-        get_collumns: function(){
-            return this._collumns;
-        },
-        get_rows: function(){
-            return this._rows;
-        },
-        add_row: function(row){
-            this._rows.push(row);
-            this.renderElement();
-        },
-        get_title: function(){
-            return this._title;
-        },
-        get_width: function(){
-            return this._width;
-        },
-        get_innerwidth: function(){
-            return this._width - this._scrollbarwidth;
-        },
-        set_width: function(width){
-            if(width >= 0){
-                this._width = width;
-                this.$('.js_innerwidth').css({'width':this.get_innerwidth()+'px'});
-                this.$el.css({'width':this.get_width()+'px'});
-            }
-        },
-        get_height: function(){
-            return this._height;
-        },
-        set_height: function(height){
-            if(height >= 0){
-                this._height = height;
-                this.$('.js_height').css({'height':this.get_height()+'px'});
-            }
-        },
-    });
-
     module.PickingEditorWidget = instance.web.Widget.extend({
         template: 'PickingEditorWidget',
+        init: function(parent,options){
+            this._super(parent,options);
+        },
         get_header: function(){
-            return 'Picking: INTERNAL/00112';
+            return this._header || 'Picking: INTERNAL/00112';
         },
         get_rows: function(){
-            return [
+            return this._rows || [
                 { cols: { product:'abc', qty:1, rem: 4, uom: 5, loc: 'def', stat: 'available' },   },
-                { cols: { product:'dec', qty:2, rem: 3, uom: 5, loc: 'rcd', stat: 'available' },  classes: 'oe_invalid' },
+                { cols: { product:'dec', qty:2, rem: 3, uom: 5, loc: 'rcd', stat: 'available' },  classes: '' },
                 { cols: { product:'qcr', qty:4, rem: 4, uom: 5, loc: 'hre', stat: 'unavailable' }, },
             ];
         },
@@ -101,12 +22,12 @@ function openerp_picking_widgets(instance){
     module.PackageEditorWidget = instance.web.Widget.extend({
         template: 'PackageEditorWidget',
         get_header: function(){
-            return 'Package: 032 ';
+            return this._header || 'Package: 032 ';
         },
         get_rows: function(){
-            return [
+            return this._rows || [
                 { cols: { product:'abc', qty:1, uom: 5, },   },
-                { cols: { product:'dec', qty:2, uom: 5, },  classes: 'oe_invalid' },
+                { cols: { product:'dec', qty:2, uom: 5, },  classes: '' },
                 { cols: { product:'qcr', qty:4, uom: 5, }, },
             ];
         },
@@ -115,18 +36,14 @@ function openerp_picking_widgets(instance){
     module.PackageSelectorWidget = instance.web.Widget.extend({
         template: 'PackageSelectorWidget',
         get_header: function(){
-            return 'Package Selector';
+            return this._header || 'Package Selector';
         },
         get_rows: function(){
-            return [
-                { cols: { pack:'abc' },   },
-                { cols: { pack:'def' }, classes: 'oe_invalid'   },
-                { cols: { pack:'ghi' },   },
+            return this._rows || [
+                { cols: { pack:'abc', qty:12  },   },
+                { cols: { pack:'def', qty:500 }, classes: ''   },
+                { cols: { pack:'ghi', qty:1   },   },
             ];
-        },
-        increment: function(){
-        },
-        decrement: function(){
         },
     });
 
@@ -134,12 +51,37 @@ function openerp_picking_widgets(instance){
         template: 'PickingMainWidget',
         init: function(parent,params){
             this._super(parent,params);
-            console.log(params);
+            
+            console.log('Action params:', params);
 
-            var packages = new instance.web.Model('stock.quant.package');
-            packages.query('name','packaging_id','quant_ids','parent_id','children_ids','location_id','pack_operation_id').all().then(function(result){
-                console.log(result);
-            });
+            new instance.web.Model('stock.picking.in')
+                .query()
+                .all()
+                .then(function(results){
+                    console.log('Picking In:',results);
+                });
+
+            new instance.web.Model('stock.picking.out')
+                .query()
+                .all()
+                .then(function(results){
+                    console.log('Picking Out:',results);
+                });
+
+            new instance.web.Model('stock.quant.package')
+                .query(['name','packaging_id','quant_ids','parent_id','children_ids','location_id','pack_operation_id'])
+                .all()
+                .then(function(result){
+                    console.log('Packages: ',result);
+                 });
+
+            new instance.web.Model('stock.pack.operation')
+                .query(['picking_id','product_id','product_uom','product_qty','package_id','quant_id','result_package_id'])
+                .all()
+                .then(function(results){
+                    console.log('Pack Operations:',results);
+                });
+
         },
         start: function(){
             var self = this;
