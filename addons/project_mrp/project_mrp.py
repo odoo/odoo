079dceda032e5271394924ee432e3b56f3a0846c
Thibault Delavallée <tde@openerp.com>
2013-06-27 14:53:06 +0200

    [FIX] project_mrp: added close field on task.type; commented some tests to clean afterwards.
    bzr revid: tde@openerp.com-20130627125306-mgai12779onkgyc2

--- addons/project_mrp/project_mrp.py
+++ addons/project_mrp/project_mrp.py
@@ -22,6 +22,24 @@
 from openerp.osv import fields, osv
 from openerp import netsvc
 
+
+class ProjectTaskStageMrp(osv.Model):
+    """ Override project.task.type model to add a 'closed' boolean field allowing
+        to know that tasks in this stage are considered as closed. Indeed since
+        OpenERP 8.0 status is not present on tasks anymore, only stage_id. """
+    _name = 'project.task.type'
+    _inherit = 'project.task.type'
+
+    _columns = {
+        'closed': fields.boolean('Close',
+            help="Tasks in this stage are considered as closed."),
+    }
+
+    _defaults = {
+        'closed': False,
+    }
+
+
 class project_task(osv.osv):
     _name = "project.task"
     _inherit = "project.task"
@@ -30,27 +48,27 @@ class project_task(osv.osv):
         'sale_line_id': fields.related('procurement_id', 'sale_line_id', type='many2one', relation='sale.order.line', store=True, string='Sales Order Line'),
     }
 
-    def _validate_subflows(self, cr, uid, ids):
+    def _validate_subflows(self, cr, uid, ids, context=None):
         wf_service = netsvc.LocalService("workflow")
         for task in self.browse(cr, uid, ids):
             if task.procurement_id:
                 wf_service.trg_write(uid, 'procurement.order', task.procurement_id.id, cr)
 
-    def do_close(self, cr, uid, ids, *args, **kwargs):
-        res = super(project_task, self).do_close(cr, uid, ids, *args, **kwargs)
-        self._validate_subflows(cr, uid, ids)
-        return res
+    def write(self, cr, uid, ids, values, context=None):
+        """ When closing tasks, validate subflows. """
+        if values.get('stage_id'):
+            stage = self.pool.get('project.task.type').browse(cr, uid, values.get('stage_id'), context=context)
+            if stage.closed:
+                self._validate_subflows(cr, uid, ids, context=context)
+        return super(project_task, self).write(cr, uid, ids, values, context=context)
 
-    def do_cancel(self, cr, uid, ids, *args, **kwargs):
-        res = super(project_task, self).do_cancel(cr, uid, ids, *args, **kwargs)
-        self._validate_subflows(cr, uid, ids)
-        return res
 
 class product_product(osv.osv):
     _inherit = "product.product"
     _columns = {
         'project_id': fields.many2one('project.project', 'Project', ondelete='set null',)
     }
+
 
 class sale_order(osv.osv):
     _inherit ='sale.order'
