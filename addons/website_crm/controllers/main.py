702f955eccc2eb4d1480276dc54c6340dd4bf393
Christophe Matthieu <chm@openerp.com>
2013-07-02 18:21:35 +0200

    [IMP] website_crm: contact form
    bzr revid: chm@openerp.com-20130702162135-cjhbxu1a1h7mhnf7

--- addons/website_crm/controllers/main.py
+++ addons/website_crm/controllers/main.py
@@ -9,12 +9,12 @@ from openerp.addons.website.controllers.main import template_values
 class contactus(http.Controller):
 
     @http.route(['/crm/contactus'], type='http', auth="db")
-    def contactus(self, *arg, **kwarg):
-        values = template_values()
+    def contactus(self, *arg, **post):
         cr = request.cr
         uid = request.session._uid or openerp.SUPERUSER_ID
-
-        html = request.registry.get("ir.ui.view").render(cr, uid, "website_crm.contactus", self.get_values())
+        post['user_id'] = False
+        request.registry['crm.lead'].create(cr, uid, post)
+        html = request.registry.get("ir.ui.view").render(cr, uid, "website_crm.thanks", template_values())
         return html
 
 # vim:expandtab:tabstop=4:softtabstop=4:shiftwidth=4:
