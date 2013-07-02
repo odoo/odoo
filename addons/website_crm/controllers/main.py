fe985b36f1c631e1a64688fe39f745ef40f65cf6
Christophe Simonis <chs@openerp.com>
2013-07-02 23:54:46 +0200

    [FIX] website_crm: correct thank page
    bzr revid: chs@openerp.com-20130702215446-vn5xj3bmxd8ag9fb

--- addons/website_crm/controllers/main.py
+++ addons/website_crm/controllers/main.py
@@ -5,6 +5,7 @@ from openerp.addons.web import http
 from openerp.addons.web.http import request
 from openerp.addons.website.controllers.main import template_values
 
+from urllib import quote_plus
 
 class contactus(http.Controller):
 
@@ -14,7 +15,13 @@ class contactus(http.Controller):
         uid = request.session._uid or openerp.SUPERUSER_ID
         post['user_id'] = False
         request.registry['crm.lead'].create(cr, uid, post)
-        html = request.registry.get("ir.ui.view").render(cr, uid, "website_crm.thanks", template_values())
+        values = template_values()
+        company = request.registry['res.company'].browse(request.cr, uid, 1)
+        values.update({
+            'res_company': company,
+            'google_map_url': "http://maps.googleapis.com/maps/api/staticmap?center=%s&sensor=false&zoom=8&size=298x298" % quote_plus('%s, %s %s, %s' % (company.street, company.city, company.zip, company.country_id and company.country_id.name_get()[0][1] or ''))
+        })
+        html = request.registry.get("ir.ui.view").render(cr, uid, "website_crm.thanks", values)
         return html
 
 # vim:expandtab:tabstop=4:softtabstop=4:shiftwidth=4:
