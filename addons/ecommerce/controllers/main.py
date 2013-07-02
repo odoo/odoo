432f9d93de87e9204caa6a95c29703f9d02ac4ac
Christophe Matthieu <chm@openerp.com>
2013-07-02 11:41:38 +0200

    [IMP] ecommerce: inherit template
    bzr revid: chm@openerp.com-20130702094138-v2qqcscopc74sfbe

--- addons/ecommerce/controllers/main.py
+++ addons/ecommerce/controllers/main.py
@@ -10,23 +10,32 @@ import urllib
 class Ecommerce(http.Controller):
 
     def get_cr_uid(self):
+        cr = request.cr
         try:
             request.session.check_security()
             uid = request.session._uid
         except http.SessionExpiredException:
             uid = openerp.SUPERUSER_ID
-        return (request.cr, uid)
+        partner_id = request.session.get('partner_id') or request.registry.get('res.users').browse(cr, uid, uid).partner_id.id
+        return (cr, uid, partner_id)
 
     def get_values(self):
-        cr, uid = self.get_cr_uid()
+        cr, uid, partner_id = self.get_cr_uid()
+        order_obj = request.registry.get('sale.order')
+        context = {}
 
-        total_price = 0
+        if not request.session.get('ecommerce'):
+            fields = [k for k, v in order_obj._columns.items()]
+            order_value = order_obj.default_get(cr, uid, fields, context=context)
+            order_value['partner_id'] = partner_id
+            order_value.update(order_obj.onchange_partner_id(cr, uid, [], uid, context=context)['value'])
+            order_id = order_obj.create(cr, uid, order_value, context=context)
+            request.session['ecommerce'] = order_id
+
         cart = {}
-        if request.session.get('ecommerce'):
-            order = request.registry.get('sale.order').browse(cr, uid, request.session.get('ecommerce'))
-            total_price = order.amount_total
-            for line in order.order_line:
-                cart[line.product_id.id] = {'product_id': line.product_id.id, 'quantity': int(line.product_uom_qty)}
+        order = order_obj.browse(cr, uid, request.session.get('ecommerce'))
+        for line in order.order_line:
+            cart[line.product_id.id] = {'product_id': line.product_id.id, 'quantity': int(line.product_uom_qty)}
 
         values = {
             'request': request,
@@ -34,14 +43,14 @@ class Ecommerce(http.Controller):
             'cr': cr,
             'uid': uid,
             'cart': cart,
-            'total_price': total_price,
+            'order': order,
         }
         return values
 
     @http.route(['/shop', '/shop/category/<cat_id>'], type='http', auth="db")
     def category(self, cat_id=0, offset=0):
         values = self.get_values()
-        cr, uid = self.get_cr_uid()
+        cr, uid, partner_id = self.get_cr_uid()
 
         cat_id = cat_id and int(cat_id) or 0
         category_obj = request.registry.get('pos.category')
@@ -54,13 +63,13 @@ class Ecommerce(http.Controller):
             'categories': category_obj.browse(cr, uid, category_ids),
             'products': product_obj.browse(cr, uid, product_ids),
         })
-        html = request.registry.get("ir.ui.view").render(cr, uid, "ecommerce.categories", values)
+        html = request.registry.get("ir.ui.view").render(cr, uid, "ecommerce.page", values)
         return html
 
     @http.route(['/shop/product/<product_id>'], type='http', auth="db")
     def product(self, cat_id=0, product_id=0, offset=0):
         values = self.get_values()
-        cr, uid = self.get_cr_uid()
+        cr, uid, partner_id = self.get_cr_uid()
 
         product_id = product_id and int(product_id) or 0
         category_obj = request.registry.get('pos.category')
@@ -77,7 +86,7 @@ class Ecommerce(http.Controller):
     @http.route(['/shop/my_cart'], type='http', auth="db")
     def my_cart(self, cat_id=0, offset=0):
         values = self.get_values()
-        cr, uid = self.get_cr_uid()
+        cr, uid, partner_id = self.get_cr_uid()
 
         cat_id = cat_id and int(cat_id) or 0
         category_obj = request.registry.get('pos.category')
@@ -87,34 +96,21 @@ class Ecommerce(http.Controller):
         product_ids = [product for product in values['cart']]
 
         values.update({
-            'my_cart': True,
             'categories': category_obj.browse(cr, uid, category_ids),
-            'products': product_obj.browse(cr, uid, product_ids),
         })
-        html = request.registry.get("ir.ui.view").render(cr, uid, "ecommerce.categories", values)
+        html = request.registry.get("ir.ui.view").render(cr, uid, "ecommerce.my_cart", values)
         return html
 
     @http.route(['/shop/add_cart'], type='http', auth="db")
     def add_cart(self, product_id=0, remove=False):
-        cr, uid = self.get_cr_uid()
+        cr, uid, partner_id = self.get_cr_uid()
         context = {}
 
         order_obj = request.registry.get('sale.order')
         order_line_obj = request.registry.get('sale.order.line')
 
         product_id = product_id and int(product_id) or 0
-        partner = request.registry.get('res.users').browse(cr, uid, uid, context=context).partner_id
-
         order_id = request.session.get('ecommerce')
-
-        # find or set order_id
-        if not order_id:
-            fields = [k for k, v in order_obj._columns.items()]
-            order_value = order_obj.default_get(cr, uid, fields, context=context)
-            order_value['partner_id'] = partner.id
-            order_value.update(order_obj.onchange_partner_id(cr, uid, [], uid, context=context)['value'])
-            order_id = order_obj.create(cr, uid, order_value, context=context)
-            request.session['ecommerce'] = order_id
         order = order_obj.browse(cr, uid, order_id, context=context)
 
         # values initialisation
@@ -136,7 +132,7 @@ class Ecommerce(http.Controller):
 
         # change and record value
         pricelist_id = order.pricelist_id and order.pricelist_id.id or False
-        values.update(order_line_obj.product_id_change(cr, uid, [], pricelist_id, product_id, partner_id=partner.id, context=context)['value'])
+        values.update(order_line_obj.product_id_change(cr, uid, [], pricelist_id, product_id, partner_id=partner_id, context=context)['value'])
         if order_line_ids:
             order_line_obj.write(cr, uid, order_line_ids, values, context=context)
         else:
@@ -149,8 +145,14 @@ class Ecommerce(http.Controller):
     def remove_cart(self, product_id=0):
         return self.add_cart(product_id=product_id, remove=True)
 
-    @http.route(['/shop/confirm_cart'], type='http', auth="db")
+    @http.route(['/shop/customer'], type='http', auth="db")
     def confirm_cart(self):
-        return ""
+        cr, uid, partner_id = self.get_cr_uid()
+        values = self.get_values()
+        values.update({
+            'partner_id': request.registry.get('res.users').browse(cr, uid, partner_id),
+        })
+        html = request.registry.get("ir.ui.view").render(cr, uid, "ecommerce.customer", values)
+        return html
 
 # vim:expandtab:tabstop=4:softtabstop=4:shiftwidth=4:
