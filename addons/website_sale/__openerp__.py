fbae203ac76cc4f13aed1ee433566a27e9e6cbc0
Christophe Matthieu <chm@openerp.com>
2013-07-16 11:36:24 +0200

    [ADD] website_sale: pricelist
    bzr revid: chm@openerp.com-20130716093624-lf15mryypg3aevl8

--- addons/website_sale/__openerp__.py
+++ addons/website_sale/__openerp__.py
@@ -13,8 +13,9 @@ OpenERP E-Commerce
         'views/ecommerce.xml',
         'views/pricelist.xml'
     ],
-    'js': ['static/src/js/ecommerce.js'],
-    'css': ['static/src/css/ecommerce.css'],
+    'js': ['static/src/js/*.js'],
+    'css': ['static/src/css/*.css'],
+    'qweb': ['static/src/xml/*.xml'],
     'installable': True,
     'auto_install': True,
 }
--- addons/website_sale/static/src/js/ecommerce.js
+++ addons/website_sale/static/src/js/ecommerce.js
@@ -23,3 +23,44 @@ $(document).ready(function (){
         $('.oe_ecommerce form').toggle();
     });
 });
+
+
+openerp.website = function(instance) {
+
+    instance.website.sale = {};
+    instance.website.sale.Checkout = instance.web.Widget.extend({
+        template: 'Website.sale.Checkout',
+        events: {
+            'click button[data-action=gotostep]': 'gotostep',
+        },
+        start: function() {
+            console.log("instance.website.sale.Checkout", this);
+            return this._super.apply(this, arguments);
+        },
+        goto: function (e) {
+
+        },
+        save: function () {
+        },
+    });
+};
+
+
+$(document).ready(function () {
+    if (!$('.oe_placeholder_checkout').size())
+        return;
+    // Init headless webclient
+    // TODO: Webclient research : use iframe embedding mode
+    //       Meanwhile, let's HACK !!!
+    var s = new openerp.init(['web', 'website']);
+    s.web.WebClient.bind_hashchange = s.web.WebClient.show_common = s.web.blockUI = s.web.unblockUI = function() {};
+    s.web.WebClient.include({ do_push_state: function() {} });
+    var wc = new s.web.WebClient();
+    wc.start();
+    var instance = openerp.instances[wc.session.name];
+    // Another hack since we have no callback when webclient has loaded modules.
+    instance.web.qweb.add_template('/website_sale/static/src/xml/ecommerce.xml');
+
+    var editor = new instance.website.sale.Checkout(instance.webclient);
+    editor.prependTo($('.oe_placeholder_checkout'));
+});
