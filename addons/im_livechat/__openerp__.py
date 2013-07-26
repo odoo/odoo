fe6e0e39e8e1cfd7d5d5bf997cb6d162b803fd76
Christophe Matthieu <chm@openerp.com>
2013-07-26 16:44:36 +0200

    [FIX] depends with portal inside of website
    bzr revid: chm@openerp.com-20130726144436-2i8q9348hiku4wch

--- addons/im_livechat/__openerp__.py
+++ addons/im_livechat/__openerp__.py
@@ -22,7 +22,7 @@ chat operators.
     'demo': [
         "im_livechat_demo.xml",
     ],
-    'depends' : ["im", "mail", "website"],
+    'depends' : ["im", "mail", "portal"],
     'installable': True,
     'auto_install': False,
     'application': True,
