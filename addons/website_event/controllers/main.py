686b44454aa7349bcf59b64294e67899f5f197ba
Christophe Matthieu <chm@openerp.com>
2013-08-02 12:45:04 +0200

    [IMP] website_event: search dates
    bzr revid: chm@openerp.com-20130802104504-7o2uubwabn3yafqc

--- addons/website_event/controllers/main.py
+++ addons/website_event/controllers/main.py
@@ -11,21 +11,27 @@ from openerp import tools
 
 class website_hr(http.Controller):
 
-    @http.route(['/event', '/event/search/<path:path>'], type='http', auth="public")
-    def blog(self, path=None, **post):
+    @http.route(['/event', '/event/search/<path:searches>'], type='http', auth="public")
+    def events(self, searches=None, **post):
         data_obj = request.registry['event.event']
 
+        _search = {}
+        for search in searches.split("/"):
+            search = search.split("-")
+            if len(search) == 2:
+                _search[search[0]] = [search[1], []]
+
         def sd(date):
-            return date.strftime(tools.DEFAULT_SERVER_DATE_FORMAT)
+            return date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)
         today = datetime.today()
         dates = [
-            [None, _('All Dates'), [(1, "=", 1)]],
+            ['all', _('All Dates'), [(1, "=", 1)], 0],
             ['today', _('Today'), [
-                ("date_begin", ">=", sd(today)),
+                ("date_begin", ">", sd(today)),
                 ("date_begin", "<", sd(today + relativedelta(days=1)))],
                 0],
             ['tomorrow', _('Tomorrow'), [
-                ("date_begin", ">=", sd(today + relativedelta(days=1))),
+                ("date_begin", ">", sd(today + relativedelta(days=1))),
                 ("date_begin", "<", sd(today + relativedelta(days=2)))],
                 0],
             ['week', _('This Week'), [
@@ -42,15 +48,40 @@ class website_hr(http.Controller):
                 0],
         ]
 
-        obj_ids = data_obj.search(request.cr, request.uid, [(1, "=", 1)])
+        # search domains
+        for date in dates:
+            if _search.get("date") and _search["date"][0] == date[0]:
+                _search["date"].append(date[2])
+
+        # count by domains without self search
+        for date in dates:
+            domain = [(1, "=", 1)]
+            for key, search in _search.items():
+                if key != 'date':
+                    domain += search[1]
+            date[3] = data_obj.search(request.cr, request.uid, domain + date[2], count=True)
+
+        # domain and search_path
+        domain = [(1, "=", 1)]
+        search_path = '/search'
+        for key, search in _search.items():
+            search_path = "%s/%s-%s" % (search_path, key, search[0])
+            domain += search[1]
+
+        obj_ids = data_obj.search(request.cr, request.uid, domain)
         values = {
             'event_ids': data_obj.browse(request.cr, request.uid, obj_ids),
             'dates': dates,
-            'date_active': None,
+            'search': _search,
+            'search_path': search_path,
         }
 
         html = website.render("website_event.index", values)
         return html
+
+    @http.route(['/event/<int:event_id>'], type='http', auth="public")
+    def event(self, event_id=None, **post):
+        return ""
 
     @http.route(['/event/publish'], type='http', auth="public")
     def publish(self, **post):
