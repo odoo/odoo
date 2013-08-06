24a790e95f3f2aec5201f62d77d5fa9a71e1e767
Christophe Matthieu <chm@openerp.com>
2013-08-06 17:33:23 +0200

    [FIX] website_hr
    bzr revid: chm@openerp.com-20130806153323-ctvf6y0yskaiwv8y

--- addons/website_hr/hr.py
+++ addons/website_hr/hr.py
@@ -7,6 +7,5 @@ class hr(osv.osv):
     _inherit = 'hr.employee'
     _columns = {
         'website_published': fields.boolean('Available in the website'),
-        'website_published_on_contact_form': fields.boolean('Publish', help="Publish also on contact form"),
     }
 
