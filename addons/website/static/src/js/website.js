39d5bced7f5289205d10b6318b29630f5d8b4692
Christophe Matthieu <chm@openerp.com>
2013-08-14 10:34:39 +0200

    [IMP] website: refact. javascript,xml for website publish button
    bzr revid: chm@openerp.com-20130814083439-wvc07n0sg2vblq1y

--- addons/website/static/src/js/website.js
+++ addons/website/static/src/js/website.js
@@ -382,4 +382,31 @@ $(function(){
     }
 });
 
+$(function () {
+    $(document).on('click', '.js_publish, .js_unpublish', function (e) {
+        e.preventDefault();
+        var $link = $(this).parent();
+        $link.find('.js_publish, .js_unpublish').addClass("hidden");
+        var $unp = $link.find(".js_unpublish");
+        var $p = $link.find(".js_publish");
+
+        var paths = document.location.pathname.split('/');
+        var url = '/' + paths[1];
+        if (paths[1] === 'page') {
+            url += '/' + paths[2];
+        }
+        url += '/publish';
+
+        $.post(url, {'id': $link.data('id')}, function (result) {
+            if (+result) {
+                $p.addClass("hidden");
+                $unp.removeClass("hidden");
+            } else {
+                $p.removeClass("hidden");
+                $unp.addClass("hidden");
+            }
+        });
+    });
+});
+
 };
--- addons/website_sale/static/src/js/website_sale.js
+++ addons/website_sale/static/src/js/website_sale.js
@@ -1,22 +1,4 @@
 $(document).ready(function () {
-    $('.oe_website_sale').on('click', '.js_publish, .js_unpublish', function (e) {
-        e.preventDefault();
-        var $link = $(this).parent();
-        $link.find('.js_publish, .js_unpublish').addClass("hidden");
-        var $unp = $link.find(".js_unpublish");
-        var $p = $link.find(".js_publish");
-        $.post('/shop/publish', {'id': $link.data('id')}, function (result) {
-            if (+result) {
-                $p.addClass("hidden");
-                $unp.removeClass("hidden");
-            } else {
-                $p.removeClass("hidden");
-                $unp.addClass("hidden");
-            }
-        });
-    });
-
-
     $(".oe_website_sale input[name='shipping_different']").change(function() {
         $(".oe_website_sale .js_shipping").toggle();
     });
