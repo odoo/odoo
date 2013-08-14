9f5c0f54ec52a8aed6a9635eae5aaac68050929a
Martin Trigaux <mat@openerp.com>
2013-08-14 15:53:46 +0200

    [FIX] point_of_sale: oups, needs an id
    bzr revid: mat@openerp.com-20130814135346-6zpzl6tc0efowquj

--- addons/point_of_sale/point_of_sale.py
+++ addons/point_of_sale/point_of_sale.py
@@ -110,7 +110,7 @@ class pos_config(osv.osv):
 
     def _default_sale_journal(self, cr, uid, context=None):
         company_id = self.pool.get('res.users').browse(cr, uid, uid, context=context)
-        res = self.pool.get('account.journal').search(cr, uid, [('type', '=', 'sale'), ('company_id', '=', company_id)], limit=1, context=context)
+        res = self.pool.get('account.journal').search(cr, uid, [('type', '=', 'sale'), ('company_id', '=', company_id.id)], limit=1, context=context)
         return res and res[0] or False
 
     def _default_shop(self, cr, uid, context=None):
