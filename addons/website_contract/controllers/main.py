84208fd7ae465766f1ebaa53a315d79ec551af4d
Christophe Matthieu <chm@openerp.com>
2013-08-19 17:10:34 +0200

    [IMP] website_contract: published
    bzr revid: chm@openerp.com-20130819151034-o079dfazpo043kw2

--- addons/website_contract/controllers/main.py
+++ addons/website_contract/controllers/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import openerp
 from openerp.addons.web import http
 from openerp.addons.web.http import request
 
@@ -9,8 +10,11 @@ class website_contract(http.Controller):
     def blog(self, **post):
         website = request.registry['website']
         partner_obj = request.registry['res.partner']
-        project_ids = partner_obj.search(request.cr, request.uid, [(1, "=", 1)])
+        project_ids = partner_obj.search(request.cr, openerp.SUPERUSER_ID, [('website_testimonial', "!=", False), ('website_published', '=', True)])
+        if request.uid != website.get_public_user().id:
+            project_ids += partner_obj.search(request.cr, request.uid, [(1, "=", 1)])
+            project_ids = list(set(project_ids))
         values = website.get_rendering_context({
-            'partner_ids': partner_obj.browse(request.cr, request.uid, project_ids)
+            'partner_ids': partner_obj.browse(request.cr, openerp.SUPERUSER_ID, project_ids)
         })
         return website.render("website_contract.index", values)
