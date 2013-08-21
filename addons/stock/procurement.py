038aee2d06f89d97b075d825f9f862b3aabf3dfd
Bharat R. Devnani (OpenERP) <bde@tinyerp.com>
2013-08-21 15:07:42 +0530

    [IMP] improved the code
    bzr revid: bde@tinyerp.com-20130821093742-rt0lvwdw98aiwx3w

--- addons/stock/procurement.py
+++ addons/stock/procurement.py
@@ -97,7 +97,6 @@ class procurement_order(osv.osv):
             move_obj = self.pool.get('stock.move')
             move_dict = self._run_move_create(cr, uid, procurement, context=context)
             move_id = move_obj.create(cr, uid, move_dict, context=context)
-            self.write(cr, uid, procurement.id, {'move_dest_id': move_id}, context=context)
             move_obj.action_confirm(cr, uid, [move_id], context=context)
             return move_id
         return super(procurement_order, self)._run(cr, uid, procurement, context)
