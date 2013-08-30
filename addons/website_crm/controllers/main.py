5b83f20086557351f34b0104f6ba7d113aa27df4
Christophe Matthieu <chm@openerp.com>
2013-08-30 16:08:58 +0200

    [IMP] website_crm: remove non used code
    bzr revid: chm@openerp.com-20130830140858-hqku5ync7gxxzcb2

--- addons/website_crm/controllers/main.py
+++ addons/website_crm/controllers/main.py
@@ -22,37 +22,4 @@ class contactus(http.Controller):
         })
         return website.render("website_crm.thanks", values)
 
-    @http.route(['/crm/google_map/'], type='http', auth="admin")
-    def google_map(self, *arg, **post):
-        website = request.registry['website']
-        values = website.get_rendering_context()
-        values['partner_ids'] = post.get('partner_ids', "")
-        return website.render("website_crm.google_map", values)
-
-    @http.route(['/crm/google_map/partners.json'], type='http', auth="admin")
-    def google_map_data(self, *arg, **post):
-        website = request.registry['website']
-        partner_obj = request.registry['res.partner']
-
-        domain = [("id", "in", [int(p) for p in post.get('partner_ids', "").split(",") if p])]
-        partner_ids = partner_obj.search(request.cr, openerp.SUPERUSER_ID, domain + [('website_published', '=', True)])
-        if request.uid != website.get_public_user().id:
-            partner_ids += partner_obj.search(request.cr, request.uid, domain)
-
-        data = {
-            "counter": len(partner_ids),
-            "partners": []
-            }
-        for partner in partner_obj.browse(request.cr, openerp.SUPERUSER_ID, partner_ids, context={'show_address': True}):
-            data["partners"].append({
-                'id': partner.id,
-                'name': partner.name,
-                'address': '\n'.join(partner.name_get()[0][1].split('\n')[1:]),
-                'type': "Silver Partners"
-                })
-
-        mime = 'application/json'
-        body = "var data = " + "}, \n{".join(simplejson.dumps(data).split("}, {"))
-        return werkzeug.wrappers.Response(body, headers=[('Content-Type', mime), ('Content-Length', len(body))])
-
 # vim:expandtab:tabstop=4:softtabstop=4:shiftwidth=4:
