c4434a92467267196db8711ce53cecdf0cc84dae
ajay javiya (OpenERP) <aja@tinyerp.com>
2013-09-03 14:32:46 +0530

    [IMP]: Rename Hide scheduled call menu to Show scheduled call menu because by default menu is hidden
    bzr revid: aja@tinyerp.com-20130903090246-45zw6cqc94u0le7e

--- addons/crm/res_config.py
+++ addons/crm/res_config.py
@@ -29,7 +29,7 @@ class crm_configuration(osv.TransientModel):
     def set_group_multi_salesteams(self, cr, uid, ids, context=None):
         """ This method is automatically called by res_config as it begins
             with set. It is used to implement the 'one group or another'
-            behavior. We have to perform some group manipulation by hand
+            behavior. We have to perform some group manipulation by hand    
             because in res_config.execute(), set_* methods are called
             after group_*; therefore writing on an hidden res_config file
             could not work.
@@ -65,9 +65,9 @@ class crm_configuration(osv.TransientModel):
         'group_multi_salesteams': fields.boolean("Organize Sales activities into multiple Sales Teams",
             implied_group='base.group_multi_salesteams',
             help="""Allows you to use Sales Teams to manage your leads and opportunities."""),
-        'group_scheduled_calls_menu': fields.boolean("Hide Scheduled Calls Menu",
+        'group_scheduled_calls_menu': fields.boolean("Show Scheduled Calls Menu",
             implied_group='crm.group_scheduled_calls_menu',
-            help="""Allows to hide Scheduled Calls Menu in Sales/Phone Calls Menu""")
+            help="""Allows to Show Scheduled Calls Menu in Sales/Phone Calls Menu""")
     }
 
 
