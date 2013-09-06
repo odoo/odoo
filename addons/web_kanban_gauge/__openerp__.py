5245e732bc31dfe65d2480565bf0b9f3be307dd0
Thibault Delavallée <tde@openerp.com>
2013-09-06 12:05:28 +0200

    [MOV] [ADD] Moved gauge and sparkline widgets into their own modules.
    Introducing two new web modules: web_kanban_gauge and web_kanban_sparkline.
    
    bzr revid: tde@openerp.com-20130906100528-rfk9u9op5u8kadl9

---
+++ addons/web_kanban_gauge/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Gauge Widget for Kanban',
+    'category': 'Hidden',
+    'description': """
+This widget allows to display gauges using jquery.sparkline library.
+""",
+    'version': '1.0',
+    'depends': ['web_kanban'],
+    'js': [
+        'static/lib/justgage/justgage.js',
+        'static/src/js/kanban_gauge.js'
+    ],
+    'css': [
+    ],
+    'qweb': [
+    ],
+    'auto_install': False,
+}
