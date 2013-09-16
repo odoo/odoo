da358b54ab12b175d673a067132eaa5df16af449
Turkesh Patel (Open ERP) <tpa@tinyerp.com>
2013-09-16 18:37:00 +0530

    [IMP] improved code to show bar required help and with delay
    bzr revid: tpa@tinyerp.com-20130916130700-e9ipoezcjeyva3mb

--- addons/crm/static/src/js/crm_case_section.js
+++ addons/crm/static/src/js/crm_case_section.js
@@ -9,30 +9,17 @@ var _t = openerp.web._t;
             }
         },
     });
-    openerp.crm.SparklineBarWidget = openerp.web_kanban.AbstractField.extend({
-        className: "oe_sparkline_bar",
-        start: function() {
-            var self = this;
-            var title = this.$node.html();
-            setTimeout(function () {
-                var value = _.pluck(self.field.value, 'value');
-                var tooltips = _.pluck(self.field.value, 'tooltip');
-                var currency_symbol = "";
-                if (self.getParent()){
-                    currency_symbol = self.getParent().record.currency_symbol.raw_value;
-                };
-                self.$el.sparkline(value, {
-                    type: 'bar',
-                    barWidth: 5,
-                    tooltipFormat: _.str.sprintf("{{offset:offset}}: {{value}} %s",self.$el.closest("div[class='oe_salesteams_leads']").attr('class') ? _t('Lead(s)') : currency_symbol),
-                    tooltipValueLookups: {
-                        'offset': tooltips
-                    },
-                });
-                self.$el.tipsy({'delayIn': 3000, 'html': true, 'title': function(){return title}, 'gravity': 'n'});
-            }, 0);
-        },
-    });
-    openerp.web_kanban.fields_registry.add("sparkline_bar", "openerp.crm.SparklineBarWidget");
 
+    openerp.web_kanban.SparklineBarWidgetSalesTeam = openerp.web_kanban.SparklineBarWidget.extend({
+        set_offset:function(){
+            var self = this;
+            var currency_symbol = "";
+            if (self.getParent()){
+                currency_symbol = self.getParent().record.currency_symbol.raw_value;
+            };
+            return _.str.sprintf("{{offset:offset}}: {{value}} %s",self.$el.closest("div[class='oe_salesteams_leads']").attr('class') ? _t('Lead(s)') : currency_symbol)
+        }
+    });
+
+openerp.web_kanban.fields_registry.add("sparkline_bar_sales", "openerp.web_kanban.SparklineBarWidgetSalesTeam");
 };
