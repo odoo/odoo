b855aada49edacbb776cd60c8299f9d7b80c75d6
Paramjit Singh Sahota <psa@tinyerp.com>
2013-09-18 15:44:37 +0530

    Improved
    bzr revid: psa@tinyerp.com-20130918101437-p7jljc3mco70ay8g

--- addons/account/account_bank_statement.py
+++ addons/account/account_bank_statement.py
@@ -128,7 +128,7 @@ class account_bank_statement(osv.osv):
         'currency': fields.function(_currency, string='Currency',
             type='many2one', relation='res.currency'),
         'account_id': fields.related('journal_id', 'default_debit_account_id', type='many2one', relation='account.account', string='Account used in this journal', readonly=True, help='used in statement reconciliation domain, but shouldn\'t be used elswhere.'),
-        'cash_control': fields.related('journal_id', 'cash_control' , type='boolean', relation='account.journal',string='Cash control'),
+        'cash_control': fields.related('journal_id', 'cash_control' , type='boolean', relation='account.journal',string='Cash control', store=True),
     }
 
     _defaults = {
