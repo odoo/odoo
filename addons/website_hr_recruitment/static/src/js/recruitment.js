83dee1fd7844e809aa6a27d132c16afe9173aece
bth-openerp <bth@tinyerp.com>
2013-09-20 16:08:46 +0530

    [IMP]Improve code for publish and removed div.
    bzr revid: bth@tinyerp.com-20130920103846-dx3hlg9clu95u2mh

--- addons/website_hr_recruitment/static/src/js/recruitment.js
+++ addons/website_hr_recruitment/static/src/js/recruitment.js
@@ -1,26 +1,8 @@
 $(function () {
     $(document).on('click', '.js_publish', function (e) {
         var id = $(this).data('id');
-        var row = $(this).parents().find('tr#'+id);
-        var counting = row.find('td:first').find('span#counting');
-        var msg = row.find('td:first').find('span#norecruit');
-        var div = row.find('td:first').find('div#'+id+' div:first');
-        var job_post = row.find('td:first').find('span#job_post');
-        var no_job_post = row.find('td:first').find('span#no_job_post');
-
         var loadpublish = function () {
-            return openerp.jsonRpc('/recruitment/published', 'call', {'id': id}).then(function (result) {
-                if (result['published']) {
-                    msg.addClass('hidden');
-                    counting.removeClass('hidden');
-                    counting.find('span#counting_num').html(result['count']);
-                    div.addClass('hidden');
-                } else {
-                    msg.removeClass('hidden');
-                    counting.addClass('hidden');
-                    div.removeClass('hidden');
-                }
-            });
+            return openerp.jsonRpc('/recruitment/published', 'call', {'id': id});
         }
         var i = 0;
         $(this).ajaxComplete(function(el, xhr, settings) {
