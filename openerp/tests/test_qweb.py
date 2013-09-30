bfd080beb3c01e95ab938745801898bd682409b2
Xavier Morel <xmo@openerp.com>
2013-09-30 16:46:34 +0200

    [ADD] qweb test case for weird encoding
    bzr revid: xmo@openerp.com-20130930144634-wb3j7lypj6bqtfyg

--- openerp/tests/test_qweb.py
+++ openerp/tests/test_qweb.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import cgi
 from collections import namedtuple
 from xml.dom import minidom as dom
 
@@ -44,6 +45,30 @@ class TestQWebTField(common.TransactionCase):
                   'data-oe-expression="company.name">%s</span>' % (
                 company_id,
                 "My Test Company",))
+
+    def test_i18n(self):
+        field = document.createElement('span')
+        field.setAttribute('t-field', u'company.name')
+
+        Companies = self.registry('res.company')
+        s = u"Testing «ταБЬℓσ»: 1<2 & 4+1>3, now 20% off!"
+        company_id = Companies.create(self.cr, self.uid, {
+            'name': s,
+        })
+        root_company = Companies.browse(self.cr, self.uid, company_id)
+
+        result = self.engine.render_node(field, {
+            'company': root_company,
+            'request': Request(self.cr, self.uid, RegistryProxy(self.registry))
+        })
+        self.assertEqual(
+            result,
+            '<span data-oe-model="res.company" data-oe-id="%d" '
+                  'data-oe-field="name" data-oe-type="char" '
+                  'data-oe-translate="0" '
+                  'data-oe-expression="company.name">%s</span>' % (
+                company_id,
+                cgi.escape(s.encode('utf-8')),))
 
     def test_reject_crummy_tags(self):
         field = document.createElement('td')
