4deb9c8605a5fb72a1773a5e6c3979faca6ea9c0
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2013-10-02 17:11:22 +0200

    [FIX] stock: get_picking_for_packing_ui now filters on the picking type
    bzr revid: qdp-launchpad@openerp.com-20131002151122-y10jyz46hxqx3ryb

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -1007,7 +1007,7 @@ class stock_picking(osv.osv):
     # Methods for the barcode UI
 
     def get_picking_for_packing_ui(self, cr, uid, context=None):
-        return self.search(cr, uid, [('state', 'in', ('confirmed', 'assigned'))], context=context)
+        return self.search(cr, uid, [('state', 'in', ('confirmed', 'assigned')), ('picking_type_id', '=', context.get('default_picking_type_id'))], context=context)
 
     def action_done_from_packing_ui(self, cr, uid, picking_id, only_split_lines=False, context=None):
         self.do_partial(cr, uid, picking_id, only_split_lines, context=context)
