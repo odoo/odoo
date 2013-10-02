3492c4ce651c78dae1c7e311217434bca37ab966
ddm <ddm@openerp.com>
2013-10-02 15:57:23 +0200

    [IMP] Basic first tour
    bzr revid: ddm@openerp.com-20131002135723-cxt0mhjeumr2bciq

--- addons/website/static/src/js/website.tour.js
+++ addons/website/static/src/js/website.tour.js
@@ -4,93 +4,49 @@
     var website = openerp.website;
     website.templates.push('/website/static/src/xml/website.tour.xml');
 
-    var tourStorage = window.sessionStorage;
-
     website.EditorTour = openerp.Class.extend({
+        editor: undefined,
+        reset: true,
         observers: [],
+        steps: [],
+        tourStorage: window.localStorage,
+        init: function (editor) {
+            this.editor = editor;
+            if (this.reset) {
+                this.tourStorage.removeItem('tour_current_step');
+                this.tourStorage.removeItem('tour_end');
+                $('.popover.tour').remove();
+            }
+            this.tour = new Tour({
+                storage: this.tourStorage,
+                keyboard: false,
+            });
+        },
         start: function () {
             var self = this;
-            tourStorage.removeItem("tour_current_step");
-            tourStorage.removeItem("tour_end");
-            var tour = new Tour({
-                storage: tourStorage,
+            self.tour.addSteps(_.map(self.steps, function (step) {
+               step.title = openerp.qweb.render ('website.tour_title', { title: step.title });
+               return step;
+            }));
+            self.tour.start(self.reset);
+        },
+        movetoStep: function (step_id) {
+            $('.popover.tour').remove();
+            var index = -1;
+            _.each(this.steps, function (step, i) {
+               if (step.step_id === step_id) {
+                   index = i;
+               }
             });
-            tour.addSteps([
-                {
-                    element: "button[data-action=edit]",
-                    template: openerp.qweb.render("website.tour_simple"),
-                    backdrop: true,
-                    title: "Switch to edit mode",
-                    content: "Press the 'Edit' button to start modifying the page",
-                },
-                {
-                    element: "#oe_rte_toolbar",
-                    template: openerp.qweb.render("website.tour_confirm"),
-                    placement: "bottom",
-                    title: "Edit the page inline",
-                    content: "Use rich text editing to make inline modifications to your page",
-                },
-                {
-                    element: "button[data-action=snippet]",
-                    template: openerp.qweb.render("website.tour_simple"),
-                    backdrop: true,
-                    reflex: true,
-                    title: "Add a snippet to your page",
-                    content: "Click on the 'Insert Blocks' button to open the snippet collection",
-                },
-                {
-                    element: "[data-snippet-id=title]",
-                    template: openerp.qweb.render("website.tour_simple"),
-                    placement: "right",
-                    title: "Choose a title for the page",
-                    content: "Drag the 'Title' block to the body of the page and drop it on a purple zone",
-                    onShown: function () {
-                        $('[data-snippet-id=title]').on('mousedown', function () {
-                            function goToNextStep () {
-                                self.toEditTitle();
-                                $('body').off('mouseup', goToNextStep);
-                            }
-                            $('body').on('mouseup', goToNextStep);
-                        });
-                    }
-                },
-                {
-                    element: "#wrap [data-snippet-id=title]",
-                    template: openerp.qweb.render("website.tour_confirm"),
-                    placement: "bottom",
-                    backdrop: true,
-                    title: "Modify the title",
-                    content: "Click on the title and modify it to fit your needs",
-                },
-                {
-                    element: "button[data-action=save]",
-                    template: openerp.qweb.render("website.tour_simple"),
-                    backdrop: true,
-                    title: "Save your modifications",
-                    content: "Click on the 'Save' button to persist your changes to the CMS database",
-                    reflex: true,
-                    onNext: function () {
-                        self.stop();
-                    }
-                },
-            ]);
-            tour.start(true);
-            self.tour = tour;
-            self.tourStorage = tourStorage;
-        },
-        toBuildingBlocks: function () {
-            $('#step-0').remove();
-            this.tour.goto(1);
-        },
-        toEditTitle: function () {
-            $('#step-3').remove();
-            this.tour.goto(4);
+            if (index > -1) {
+                this.tour.goto(index);
+            }
         },
         onDomMutation: function (selector, callback) {
             var target = document.querySelector(selector);
             var observer = new MutationObserver(function(mutations) {
                 _.each(mutations, function(mutation) {
-                    callback(mutation)
+                    callback(mutation);
                 });
             });
             var config = {
@@ -109,15 +65,94 @@
         },
     });
 
+    website.EditorBasicTour = website.EditorTour.extend({
+        start: function () {
+            var self = this;
+            self.steps = [
+                {
+                    step_id: 'welcome',
+                    orphan: true,
+                    backdrop: true,
+                    title: "Welcome to your website!",
+                    content: "This tutorial will guide you through the firsts steps to build your enterprise class website.",
+                    template: openerp.qweb.render('website.tour_confirm', { confirm: "Ok, tell me more!" }),
+                },
+                {
+                    step_id: 'edit-page',
+                    element: 'button[data-action=edit]',
+                    placement: 'right',
+                    reflex: true,
+                    title: "Edit this page",
+                    content: "Every page of your website can be edited. Click the <b>Edit</b> button to modify your homepage.",
+                    template: openerp.qweb.render('website.tour_simple'),
+                },
+                {
+                    step_id: 'show-bar',
+                    element: '#website-top-navbar',
+                    placement: 'bottom',
+                    title: "Editor bar",
+                    content: "This is the <b>Editor Bar</b>, use it to modify your homepage.",
+                    template: openerp.qweb.render('website.tour_confirm', { confirm: "Got it, now what?" }),
+                },
+                {
+                    step_id: 'add-block',
+                    element: 'button[data-action=snippet]',
+                    placement: 'right',
+                    reflex: true,
+                    title: "Add a block to your page",
+                    content: "Click on the <b>Insert Blocks</b> button to open the block collection.",
+                    template: openerp.qweb.render('website.tour_simple'),
+                },
+                {
+                    step_id: 'drag-banner',
+                    element: '[data-snippet-id=carousel]',
+                    placement: 'bottom',
+                    title: "Add a banner to your page",
+                    content: "Drag the <b>Banner</b> block to the body of the page and drop it on a purple zone.",
+                    template: openerp.qweb.render('website.tour_simple'),
+                    onShown: function () {
+                        function beginDrag () {
+                            $('.popover.tour').remove();
+                            $('body').off('mousedown', beginDrag);
+                            function goToNextStep () {
+                                self.movetoStep('edit-title');
+                                $('body').off('mouseup', goToNextStep);
+                            }
+                            $('body').on('mouseup', goToNextStep);
+                        }
+                        $('body').on('mousedown', beginDrag);
+                    },
+                },
+                {
+                    step_id: 'edit-title',
+                    element: '#wrap [data-snippet-id=carousel] .carousel-caption',
+                    placement: 'top',
+                    title: "Change the title",
+                    content: "Click on the title and modify it to fit your needs.",
+                    template: openerp.qweb.render('website.tour_confirm', { confirm: "Ok, done!" }),
+                },
+                {
+                    step_id: 'save-changes',
+                    element: 'button[data-action=save]',
+                    placement: 'right',
+                    reflex: true,
+                    title: "Save your modifications",
+                    content: "Click the <b>Save</b> button to apply modifications on your website.",
+                    template: openerp.qweb.render('website.tour_simple'),
+                    onNext: function () {
+                        self.stop();
+                    },
+                },
+            ];
+            return this._super();
+        },
+    });
+
     website.EditorBar.include({
         start: function () {
             if (window.location.hash.indexOf("tour") >= 0) {
                 window.location.hash = "";
-                var editorTour = new website.EditorTour();
-                editorTour.start();
-                this.on('rte:ready', this, function () {
-                    editorTour.toBuildingBlocks()
-                });
+                new website.EditorBasicTour(this).start();
             }
             return this._super();
         },
