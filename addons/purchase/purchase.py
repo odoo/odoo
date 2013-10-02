25ead5971309fb43f851f878639f1a8ce70832a8
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2013-10-02 23:10:40 +0200

    [DEBUG] debug statement
    bzr revid: qdp-launchpad@openerp.com-20131002211040-s5tglavh3g8hok6w

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -1135,7 +1135,10 @@ class procurement_order(osv.osv):
         return rule_id
 
     def _run(self, cr, uid, procurement, context=None):
-        print "dans le run de purchase", procurement.rule_id and procurement.rule_id.name or 'pas de rule'
+        print "dans le run de purchase"
+        print procurement
+        a = procurement.rule_id and procurement.rule_id.name or 'pas de rule'
+        print a
         if procurement.rule_id and procurement.rule_id.action == 'buy':
             print "go pour make_po"
             #make a purchase order for the procurement
