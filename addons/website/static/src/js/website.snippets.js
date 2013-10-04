865a7cb53a0aa8c3b639f6abb96c0e23c69260ea
Christophe Matthieu <chm@openerp.com>
2013-10-04 12:41:33 +0200

    [IMP] website: xml snippets server side + improve loading speed
    bzr revid: chm@openerp.com-20131004104133-q1p2fdf153izeqyj

--- addons/website/static/src/js/website.snippets.js
+++ addons/website/static/src/js/website.snippets.js
@@ -18,13 +18,8 @@
         },
         edit: function () {
             $("body").off('click');
-            var self = this;
-            self.on('rte:ready', this, function () {
-
-                window.snippets = self.snippets = new website.snippet.BuildingBlock(this);
-                self.snippets.appendTo(self.$el);
-
-            });
+            window.snippets = this.snippets = new website.snippet.BuildingBlock(this);
+            this.snippets.appendTo(this.$el);
             return this._super.apply(this, arguments);
         },
         save: function () {
@@ -85,21 +80,25 @@
             hack_to_add_snippet_id();
             $("body").on('DOMNodeInserted', hack_to_add_snippet_id);
         },
-        dom_filter: function (dom) {
+        dom_filter: function (dom, sibling) {
+            console.log(dom, sibling);
             if (typeof dom === "string") {
                 var include = "[data-oe-model]";
                 var sdom = dom.split(',');
                 dom = "";
                 _.each(sdom, function (val) {
                     val = val.replace(/^\s+|\s+$/g, '');
-                    dom += "body " + include + " " + val + ", ";
-                    val = val.split(" ");
-                    dom += "body "+ val.shift() + include + val.join(" ") + ", ";
+                    dom += include + " " + val + ", ";
+                    if (!sibling) {
+                        val = val.split(" ");
+                        dom += val.shift() + include + val.join(" ") + ", ";
+                    }
                 });
                 dom = dom.replace(/,\s*$/g, '');
+                console.log($(dom));
                 return $(dom);
             } else {
-                return $(dom).is("[data-oe-model]") || $(dom).parents("[data-oe-model]").length ? $(dom) : $("");
+                return (!sibling && $(dom).is("[data-oe-model]")) || $(dom).parents("[data-oe-model]").length ? $(dom) : $("");
             }
         },
         start: function() {
@@ -115,11 +114,8 @@
 
             this.fetch_snippet_templates();
 
-            this.bind_selected_manipulator();
             this.bind_snippet_click_editor();
-            this.activate_overlay_zones();
 
-            this.scrollspy();
             this.$el.addClass("hidden");
 
             this.$modal = $(openerp.qweb.render('website.snippets_modal'));
@@ -129,59 +125,28 @@
             var self = this;
             this.style_templates = {};
 
-            _.each(openerp.qweb.compiled_templates, function (val, key) {
-                if (key.indexOf('website.snippets.') === 0) {
-                    var $snippet = $(openerp.qweb.render(key)).addClass("oe_snippet");
-                    var snipped_id = $snippet.data("snippet-id");
-                    if (snipped_id) {
+            openerp.jsonRpc("/website/snippets", 'call', {})
+                .then(function (html) {
+                    var $html = $(html);
 
-                        if ($snippet.data('category') === 'style') {
-                            self.style_templates[snipped_id] = {
-                                'snipped-id' : snipped_id,
-                                'selector': $snippet.data('selector'),
-                                'class': $snippet.find(".oe_snippet_class").text(),
-                                'label': $snippet.find(".oe_snippet_label").text(),
-                                '$el': $snippet
-                            };
-                        }
-                        if ($snippet.find(".oe_snippet_thumbnail").length) {
-                            self.$el.find('#snippet_' + $snippet.data('category')).append($snippet);
-                            self.make_snippet_draggable($snippet);
-                        }
-                    }
-                }
-            });
-        },
-        scrollspy: function (){
-            var self = this;
-            var $ul = this.$("ul");
-            var $pill = self.$(".pill-content");
-            var padding = parseInt($pill.css("padding-left"));
-            var $scroll = this.$(".scroll");
-            
-           $scroll.scroll(function () {
-                $pill.find("> div").each(function () {
-                	/*
-                    if ($(this).position().left <= padding) {
-                        $ul.find("> li").removeClass('active');
-                        $ul.find("a[href='#" + $(this).attr("id") + "']").parent("li").addClass('active');
-                    }*/
+                    var $styles = $html.find("#snippet_styles");
+                    $styles.find("> [data-snippet-id]").each(function () {
+                        var $style = $(this);
+                        var snipped_id = $style.data('snippet-id');
+                        self.style_templates[snipped_id] = {
+                            'snipped-id' : snipped_id,
+                            'selector': $style.data('selector'),
+                            'class': $style.find(".oe_snippet_class").text(),
+                            'label': $style.find(".oe_snippet_label").text()
+                        };
+                    });
+                    $styles.remove();
+
+                    self.$snippets = $html.find(".tab-content > div > div").addClass("oe_snippet");
+                    self.$el.append($html);
+
+                    self.make_snippet_draggable(self.$snippets);
                 });
-            });
-
-            $ul.find("a").click(function (event) {
-                event.preventDefault();
-                $pill.find("> div").hide();
-            	$pill.find( $(event.currentTarget).attr('href')).show();
-            	$ul.find('> li').removeClass('active');
-            	$(event.currentTarget).parent().addClass('active');
-                return false;
-            });
-
-           // $pill.css("padding-right", self.$el.outerWidth() - padding + 10 - $pill.find("> div:last").outerWidth());
-            $pill.find("> div").hide();
-            $pill.find("div:first").show();
-            $ul.find("> li:first").addClass('active');
         },
         cover_target: function ($el, $target){
             var pos = $target.offset();
@@ -260,7 +225,8 @@
         },
         create_overlay: function ($snipped_id) {
             if (typeof $snipped_id.data("snippet-editor") === 'undefined') {
-                this.activate_overlay_zones($snipped_id);
+                var $targets = this.activate_overlay_zones($snipped_id);
+                if (!$targets.length) return;
                 var editor = website.snippet.editorRegistry[$snipped_id.data("snippet-id")] || website.snippet.editorRegistry.resize;
                 $snipped_id.data("snippet-editor", new editor(this, $snipped_id));
             }
@@ -276,35 +242,17 @@
             return obj;
         },
 
-        bind_selected_manipulator: function () {
-            var self = this;
-            var $selected_target = null;
-            $("body").mouseover(function (event){
-                var $target = $(event.srcElement).parents("[data-snippet-id]:first");
-                $target = self.dom_filter($target);
-                if($target.length && !self.editor_busy) {
-                    if($selected_target != $target){
-                        if($selected_target && $selected_target.data('overlay')){
-                            $selected_target.data('overlay').removeClass('oe_hover');
-                        }
-                        $selected_target = $target;
-                        self.create_overlay($target);
-                        $target.data('overlay').addClass('oe_hover');
-                    }
-                } else if($selected_target && $selected_target.data('overlay')) {
-                    $selected_target.data('overlay').removeClass('oe_hover');
-                }
-            });
-        },
-
         // activate drag and drop for the snippets in the snippet toolbar
-        make_snippet_draggable: function($snippet){
+        make_snippet_draggable: function($snippets){
             var self = this;
             var $toInsert = false;
-            var $tumb = $snippet.find(".oe_snippet_thumbnail");
+            var $tumb = $snippets.find(".oe_snippet_thumbnail:first");
+            var left = $tumb.outerWidth()/2;
+            var top = $tumb.outerHeight()/2;
             var dropped = false;
+            var $snippet = false;
 
-            $snippet.draggable({
+            $snippets.draggable({
                 greedy: true,
                 helper: 'clone',
                 zIndex: '1000',
@@ -312,12 +260,13 @@
                 cursor: "move",
                 handle: ".oe_snippet_thumbnail",
                 cursorAt: {
-                    'left': $tumb.outerWidth()/2,
-                    'top': $tumb.outerHeight()/2
+                    'left': left,
+                    'top': top
                 },
                 start: function(){
                     self.hide();
                     dropped = false;
+                    $snippet = $(this);
                     var snipped_id = $snippet.data('snippet-id');
                     var action = $snippet.find('.oe_snippet_body').size() ? 'insert' : 'mutate';
                     if( action === 'insert'){
@@ -462,7 +411,7 @@
             }
 
             if(sibling_selector){
-                self.dom_filter(sibling_selector).each(function (){
+                self.dom_filter(sibling_selector, true).each(function (){
                     var $zone = $(this);
                     if($zone.prev('.oe_drop_zone:visible').length === 0){
                         $zone.before(zone_template);
@@ -513,6 +462,11 @@
         activate_overlay_zones: function(selector){
             var $targets = this.dom_filter(selector || '[data-snippet-id]');
             var self = this;
+
+            if (typeof selector !== 'string' && !$targets.length) {
+                console.debug( "A good node must have a [data-oe-model] attribute or must have at least one parent with [data-oe-model] attribute.");
+                console.debug( "Wrong node(s): ", selector);
+            }
             
             function is_visible($el){
                 return     $el.css('display')    != 'none'
@@ -536,6 +490,7 @@
                     $zone.appendTo('#oe_manipulators');
                     $zone.data('target',$target);
                     $target.data('overlay',$zone);
+                    console.log($target[0], $zone);
 
                     $target.on("DOMNodeInserted DOMNodeRemoved DOMSubtreeModified", function () {
                         self.cover_target($zone, $target);
@@ -602,19 +557,10 @@
         *  this.$editor :
         *       content of .oe_snippet_options
         *       Displayed into the overlay options on focus
-        *  this.$thumbnail :
-        *       content of .oe_snippet_thumbnail
-        *       Displayed in bottom editor menu, when the user click on "Building Blocks"
-        *  this.$body :
-        *       content of .oe_snippet_body
-        *       Insert into the view when the thumbnail is drag and droped into a drop zone
         */
         _readXMLData: function() {
-            this.$el = $(openerp.qweb.render("website.snippets." + this.snippet_id, {widget: this}).trim());
+            this.$el = this.parent.$snippets.siblings("[data-snippet-id='"+this.snippet_id+"']").clone();
             this.$editor = this.$el.find(".oe_snippet_options");
-            this.$thumbnail = this.$el.find(".oe_snippet_thumbnail");
-            this.$body = this.$el.find(".oe_snippet_body");
-
             var $options = this.$overlay.find(".oe_overlay_options");
             this.$editor.prependTo($options.find(".oe_options ul"));
             if ($options.find(".oe_options ul li").length) {
