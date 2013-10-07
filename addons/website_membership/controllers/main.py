532ab2333f53269b1567fd38c07b57d60ee771d5
Thibault Delavallée <tde@openerp.com>
2013-10-07 16:53:22 +0200

    [REF] website_membership cleaning
    Cleaning of controllers and views to hav ea look similar to the one currently existing on
    openerp.com
    
    Added dependance towards website_google_map.
    
    bzr revid: tde@openerp.com-20131007145322-zdm3nzye5ir4qn3j

--- addons/website_membership/controllers/main.py
+++ addons/website_membership/controllers/main.py
@@ -2,70 +2,69 @@
 
 import openerp
 from openerp.addons.web import http
-from openerp.tools.translate import _
 from openerp.addons.web.http import request
 from openerp.addons.website.models import website
-import urllib
 
-class website_crm_partner_assign(http.Controller):
 
-    @website.route(['/members/', '/members/page/<int:page>/'], type='http', auth="public", multilang=True)
-    def members(self, page=0, **post):
-        membership_obj = request.registry['membership.membership_line']
+class WebsiteMembership(http.Controller):
+    _references_per_page = 20
 
-        def dom_without(without):
-            domain = openerp.SUPERUSER_ID != request.uid and [('partner.website_published', '=', True)] or [(1, "=", 1)]
-            for key, search in domain_search.items():
-                if key != without:
-                    domain += search
-            return domain
+    @website.route([
+        '/members/',
+        '/members/page/<int:page>/',
+        '/members/association/<int:membership_id>/',
+        '/members/association/<int:membership_id>/page/<int:page>/',
+    ], type='http', auth="public", multilang=True)
+    def members(self, membership_id=None, page=0, **post):
+        cr, uid, context = request.cr, request.uid, request.context
+        product_obj = request.registry['product.product']
+        membership_line_obj = request.registry['membership.membership_line']
+        post_name = post.get('search', '')
 
-        # search domains
-        domain_search = {}
-        if post.get('search'):
-            domain_search["search"] += ['|',
-                ('name', 'ilike', "%%%s%%" % post.get("search")),
-                ('website_description', 'ilike', "%%%s%%" % post.get("search"))]
-        if post.get("membership_id", "all") != 'all':
-            domain_search["membership"] = [("membership", "=", int(post.get("membership")))]
+        # format displayed membership lines domain
+        if request.context['is_public_user']:
+            line_domain = [('partner.website_published', '=', True)]
+        else:
+            line_domain = [(1, '=', 1)]
+        if membership_id:
+            line_domain += [('membership_id', '=', membership_id)]
+        if post_name:
+            line_domain += ['|', ('partner.name', 'ilike', "%%%s%%" % post_name), ('partner.website_description', 'ilike', "%%%s%%" % post_name)]
 
-        # public membership
-        membership_ids = membership_obj.search(request.cr, openerp.SUPERUSER_ID, dom_without(False), context=request.context)
-        memberships = membership_obj.browse(request.cr, openerp.SUPERUSER_ID, membership_ids, request.context)
-        google_map_partner_ids = ",".join([str(m.partner.id) for m in memberships])
+        membership_line_ids = membership_line_obj.search(cr, uid, line_domain, context=context)
+        membership_lines = membership_line_obj.browse(cr, uid, membership_line_ids, context=context)
+        google_map_partner_ids = ",".join([str(m.partner.id) for m in membership_lines])
 
-        # group by membership_id
-        domain = dom_without("membership")
-        memberships = membership_obj.read_group(request.cr, request.uid, domain, ["id", "membership_id"], groupby="membership_id", orderby="membership_id", context=request.context)
-        memberships.insert(0, {'membership_id_count': membership_obj.search(request.cr, request.uid, domain, count=True, context=request.context), 'membership_id': ("all", _("All Groups"))})
+        # format domain for group_by and memberships
+        membership_domain = [('membership', '=', True)]
+        membership_ids = product_obj.search(cr, openerp.SUPERUSER_ID, membership_domain, context=context)
+        memberships = product_obj.browse(cr, openerp.SUPERUSER_ID, membership_ids, context=context)
 
-        step = 20
-        pager = request.website.pager(url="/members/", total=len(membership_ids), page=page, step=step, scope=7, url_args=post)
-        membership_ids = membership_obj.search(
-            request.cr, openerp.SUPERUSER_ID, [('id', 'in', membership_ids)],
-            limit=step, offset=pager['offset'], order="membership_id ASC,date DESC",
-            context=request.context)
+        # request pager for lines
+        pager = request.website.pager(url="/members/", total=len(membership_line_ids), page=page, step=self._references_per_page, scope=7, url_args=post)
 
         values = {
-            'memberships': memberships,
-            'membership_line_ids': membership_obj.browse(request.cr, openerp.SUPERUSER_ID, membership_ids, request.context),
+            'membership_line_ids': membership_lines,
+            'membership_ids': memberships,
             'google_map_partner_ids': google_map_partner_ids,
             'pager': pager,
-            'searches': post,
-            'search_path': "?%s" % urllib.urlencode(post),
+            'name_search': post_name,
         }
         return request.website.render("website_membership.index", values)
 
-    @website.route(['/members/<int:ref_id>/'], type='http', auth="public", multilang=True)
-    def partners_ref(self, ref_id=0, **post):
+    @website.route(['/members/<int:partner_id>/'], type='http', auth="public", multilang=True)
+    def partners_ref(self, partner_id=0, **post):
         partner_obj = request.registry['res.partner']
-        partner_ids = partner_obj.search(request.cr, openerp.SUPERUSER_ID, [('website_published', '=', True), ('id', '=', ref_id)], context=request.context)
-        if not request.context['is_public_user']:
-            partner_ids += partner_obj.search(request.cr, request.uid, [('id', '=', ref_id)], context=request.context)
+        if request.context['is_public_user']:
+            partner_ids = partner_obj.search(request.cr, openerp.SUPERUSER_ID, [('website_pushished', '=', True), ('id', '=', partner_id)], context=request.context)
+        else:
+            partner_ids = partner_obj.search(request.cr, request.uid, [('id', '=', partner_id)], context=request.context)
+        if not partner_ids:
+            return self.members(post)
 
         values = {
             'partner_id': partner_obj.browse(
                 request.cr, openerp.SUPERUSER_ID, partner_ids[0],
                 context=dict(request.context, show_address=True)),
         }
-        return request.website.render("website_membership.details", values)
+        return request.website.render("website_membership.partner", values)
