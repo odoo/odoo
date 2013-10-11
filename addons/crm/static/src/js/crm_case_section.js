6279bcf4b3b37ec61c159e77a2603e8c61b592fd
Turkesh Patel (Open ERP) <tpa@tinyerp.com>
2013-10-11 13:00:04 +0530

    [IMP] improved code to set tooltip suffix in sparkline bar, pass suffix in opetion insted of overriding widget.
    bzr revid: tpa@tinyerp.com-20131011073004-0swv6l1aomjt71xp

--- addons/crm/static/src/js/crm_case_section.js
+++ addons/crm/static/src/js/crm_case_section.js
@@ -1,5 +1,4 @@
 openerp.crm = function(openerp) {
-var _t = openerp.web._t;
     openerp.web_kanban.KanbanRecord.include({
         on_card_clicked: function() {
             if (this.view.dataset.model === 'crm.case.section') {
@@ -9,17 +8,4 @@ var _t = openerp.web._t;
             }
         },
     });
-
-    openerp.web_kanban.SparklineBarWidgetSalesTeam = openerp.web_kanban.SparklineBarWidget.extend({
-        set_offset:function(){
-            var self = this;
-            var currency_symbol = "";
-            if (self.getParent()){
-                currency_symbol = self.getParent().record.currency_symbol.raw_value;
-            };
-            return _.str.sprintf("{{offset:offset}}: {{value}} %s",self.$el.closest("div[class='oe_salesteams_leads']").attr('class') ? _t('Lead(s)') : currency_symbol)
-        }
-    });
-
-openerp.web_kanban.fields_registry.add("sparkline_bar_sales", "openerp.web_kanban.SparklineBarWidgetSalesTeam");
 };
