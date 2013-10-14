7d2938394fab7e47f8c0e75d710ada5f06c7a431
Xavier Morel <xmo@openerp.com>
2013-10-14 17:57:09 +0200

    [FIX] null >= 0 is true...
    bzr revid: xmo@openerp.com-20131014155709-9cg4bzof02bhtyts

--- addons/web/static/src/js/view_list.js
+++ addons/web/static/src/js/view_list.js
@@ -503,10 +503,14 @@ instance.web.ListView = instance.web.View.extend( /** @lends instance.web.ListVi
         var reloaded = $.Deferred();
         this.$el.find('.oe_list_content').append(
             this.groups.render(function () {
-                if (self.dataset.index == null && self.records.length ||
-                    self.dataset.index >= self.records.length) {
+                if (self.dataset.index == null) {
+                    if (self.records.length) {
                         self.dataset.index = 0;
+                    }
+                } else if (self.dataset.index >= self.records.length) {
+                    self.dataset.index = 0;
                 }
+
                 self.compute_aggregates();
                 reloaded.resolve();
             }));
