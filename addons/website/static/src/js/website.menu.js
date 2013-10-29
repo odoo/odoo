7ac3061c42428c3650374901420b12c14da55d6b
Fabien Meghazi <fme@openerp.com>
2013-10-29 12:24:50 +0100

    [WIP] Edit menu dialog
    bzr revid: fme@openerp.com-20131029112450-gnu2c3fl9ddypdf1

--- addons/website/static/src/js/website.menu.js
+++ addons/website/static/src/js/website.menu.js
@@ -5,32 +5,74 @@
     website.menu = {};
     website.templates.push('/website/static/src/xml/website.menu.xml');
 
+    website.menu.EditMenuDialog = website.editor.Dialog.extend({
+        template: 'website.menu.dialog.edit',
+        events: _.extend({}, website.editor.Dialog.prototype.events, {
+            'click button.add-menu': 'add_menu',
+        }),
+        init: function (menu) {
+            this.menu = menu;
+            this._super();
+        },
+        start: function () {
+            var r = this._super.apply(this, arguments);
+            var button = openerp.qweb.render('website.menu.dialog.footer-button');
+            this.$('.modal-footer').prepend(button);
+            this.$('.oe_menu_editor').nestedSortable({
+                listType: 'ul',
+                handle: 'div',
+                items: 'li',
+                toleranceElement: '> div',
+                forcePlaceholderSize: true,
+                opacity: 0.6,
+                placeholder: 'oe_menu_placeholder',
+                tolerance: 'pointer',
+            });
+            return r;
+        },
+        add_menu: function () {
+            var self = this;
+            var dialog = new website.menu.AddMenuDialog();
+            dialog.on('add-menu', this, function (link) {
+                var context = {
+                    submenu: {
+                        name: link[2] || link[0],
+                        new_window: link[1],
+                        url: link[0],
+                        children: [],
+                    },
+                };
+                self.$('.oe_menu_editor').append(
+                    openerp.qweb.render(
+                        'website.menu.dialog.submenu', context));
+            });
+            dialog.appendTo(document.body);
+        },
+        save: function () {
+            debugger
+        },
+    });
+
     website.menu.AddMenuDialog = website.editor.LinkDialog.extend({
-        template: 'website.menu.dialog',
+        template: 'website.menu.dialog.add',
         make_link: function (url, new_window, label) {
+            this.trigger('add-menu', [url, new_window, label]);
         },
     });
 
     website.dom_ready.then(function () {
-        $('.js_add_menu').on('click', function () {
-            return new website.menu.AddMenuDialog().appendTo(document.body);
-        });
-        $("#top_menu").sortable({
-            group: 'nav',
-            nested: true,
-            vertical: false,
-            exclude: '.divider',
-            onDragStart: function($item, container, _super) {
-                $item.find('ul.dropdown-menu').sortable('disable');
-                _super($item, container);
-            },
-            onDrop: function($item, container, _super) {
-                $item.find('ul.dropdown-menu').sortable('enable');
-                _super($item, container);
-            }
-        });
-        $("ul.dropdown-menu").sortable({
-            group: 'nav'
+        $('.js_edit_menu').on('click', function () {
+            var context = website.get_context();
+            openerp.jsonRpc('/web/dataset/call_kw', 'call', {
+                model: 'website.menu',
+                method: 'get_tree',
+                args: [[context.website_id]],
+                kwargs: {
+                    context: context
+                },
+            }).then(function (menu) {
+                return new website.menu.EditMenuDialog(menu).appendTo(document.body);
+            });
         });
     });
 
