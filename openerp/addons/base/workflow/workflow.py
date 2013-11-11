b204ddc9347490c24058ed2577830a8ed2f944ab
Jitendra Prajapati (OpenERP) <prajapatijitendra7969@gmail.com>
2013-11-11 12:47:22 +0530

    [IMP]improve code
    bzr revid: prajapatijitendra7969@gmail.com-20131111071722-oczrttxev513wn0g

--- openerp/addons/base/workflow/workflow.py
+++ openerp/addons/base/workflow/workflow.py
@@ -123,11 +123,13 @@ class wkf_transition(osv.osv):
     _defaults = {
         'condition': lambda *a: 'True',
     }
+
     def name_get(self, cr, uid, ids, context=None):
         if not ids:
             return []
         line = self.browse(cr, uid, ids, context=context)
-        return [(line.id, (line.act_from.name or '')+'+'+line.act_to.name+'') if line.signal == False else (line.id, line.signal) for line in line ]
+        return [(line.id, (line.act_from.name)+'+'+(line.act_to.name)) if line.signal == False else (line.id, line.signal) for line in line ]
+
 wkf_transition()
 
 class wkf_instance(osv.osv):
