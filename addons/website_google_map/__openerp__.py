18d8f6968934d00275373e8eeb51736820546d12
Fabien Pinckaers <fp@tinyerp.com>
2013-11-13 19:38:25 +0100

    [IMP] Customer Reference Page, better layout
    bzr revid: fp@tinyerp.com-20131113183825-zslkr1uru109tj3k

--- addons/website_google_map/__openerp__.py
+++ addons/website_google_map/__openerp__.py
@@ -9,7 +9,7 @@ OpenERP Website Google Map
 
         """,
     'author': 'OpenERP SA',
-    'depends': ['base_geolocalize', 'website_partner'],
+    'depends': ['base_geolocalize', 'website_partner', 'crm_partner_assign'],
     'data': [
         'views/google_map.xml',
     ],
