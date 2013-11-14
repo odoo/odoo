bf5fcc7b13d543844acc41240f5a27d555ffb4bf
ddm <ddm@openerp.com>
2013-11-14 17:17:40 +0100

    [IMP] Website builder tour (first steps)
    bzr revid: ddm@openerp.com-20131114161740-ymggmzdwwyacz60f

--- addons/website/static/src/js/website.tour.builder.js
+++ addons/website/static/src/js/website.tour.builder.js
@@ -13,12 +13,30 @@
             var $body = $(document.body);
             self.steps = [
                 {
-                    stepId: 'welcome',
+                    stepId: 'welcome-builder',
                     orphan: true,
                     backdrop: true,
                     title: "Website builder",
                     content: "We will guide you to build your website content, editing or add a new menu and install an app.",
                     template: render('website.tour_popover', { next: "Start Tutorial", end: "Skip It" }),
+                },
+                {
+                    stepId: 'content-menu',
+                    element: '#content-menu-button',
+                    placement: 'left',
+                    reflex: true,
+                    title: "Edit the content",
+                    content: "Click here to edit the menu.",
+                    template: render('website.tour_popover'),
+                },
+                {
+                    stepId: 'add-menu-entry',
+                    element: 'a[data-action=edit-structure]',
+                    placement: 'left',
+                    reflex: true,
+                    title: "Add a new menu entry",
+                    content: "Click here to create a new menu entry and manage options.",
+                    template: render('website.tour_popover'),
                 },
             ];
             return this._super();
