883fbec65e162ad2c8e4d1a22a3251425de43b35
ddm <ddm@openerp.com>
2013-11-14 16:53:21 +0100

    [IMP] Added scaffholding for website builder tour
    bzr revid: ddm@openerp.com-20131114155321-to23qtlpk9anydql

---
+++ addons/website/static/src/js/website.tour.builder.js
@@ -0,0 +1,49 @@
+(function () {
+    'use strict';
+
+    var website = openerp.website;
+
+    var render = website.tour.render;
+
+    website.EditorBuilderTour = website.EditorTour.extend({
+        id: 'builder_tour',
+        name: "Website builder",
+        init: function (editor) {
+            var self = this;
+            var $body = $(document.body);
+            self.steps = [
+                {
+                    stepId: 'welcome',
+                    orphan: true,
+                    backdrop: true,
+                    title: "Website builder",
+                    content: "We will guide you to build your website content, editing or add a new menu and install an app.",
+                    template: render('website.tour_popover', { next: "Start Tutorial", end: "Skip It" }),
+                },
+            ];
+            return this._super();
+        },
+        canResume: function () {
+            return this._super();
+        },
+    });
+
+    website.EditorBar.include({
+        start: function () {
+            var menu = $('#help-menu');
+            var builderTour = new website.EditorBuilderTour(this);
+            var $menuItem = $($.parseHTML('<li><a href="#">'+builderTour.name+'</a></li>'));
+            $menuItem.click(function () {
+                builderTour.reset();
+                builderTour.start();
+            });
+            menu.append($menuItem);
+            $('.tour-backdrop').click(function (e) {
+                e.stopImmediatePropagation();
+                e.preventDefault();
+            });
+            return this._super();
+        },
+    });
+
+}());
