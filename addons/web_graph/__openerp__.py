08a26f9e95ab5a432385d803679eba030fcb6673
Gery Debongnie <ged@openerp.com>
2013-11-15 16:37:01 +0100

    [REF] refactoring in progress. moves some util functions to its own file in addon web_graph
    bzr revid: ged@openerp.com-20131115153701-rxh1voksnu0ule4t

--- addons/web_graph/__openerp__.py
+++ addons/web_graph/__openerp__.py
@@ -18,6 +18,7 @@ Graph Views for Web Client.
         'static/lib/nvd3/d3.v3.js',
         'static/lib/nvd3/nv.d3.js',
         'static/lib/bootstrap/bootstrap.js',
+        'static/src/js/utility.js',
         'static/src/js/charts.js',
         'static/src/js/graph.js',
     ],
