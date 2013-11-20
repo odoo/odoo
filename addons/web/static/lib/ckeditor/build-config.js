f39d4398b851f266b9a96ea017dfa81194744610
Xavier Morel <xmo@openerp.com>
2013-11-20 09:01:11 +0100

    [ADD] custom build of CKEditor 4.3 final
    bzr revid: xmo@openerp.com-20131120080111-vvtfcpjlxhz66wn2

---
+++ addons/web/static/lib/ckeditor/build-config.js
@@ -0,0 +1,80 @@
+﻿
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time at http://ckeditor.com/builder to build CKEditor again.
+ * 
+ * NOTE: 
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono',
+	preset: 'basic',
+	ignore: [
+		'dev',
+		'.gitignore',
+		'.gitattributes',
+		'README.md',
+		'.mailmap'
+	],
+	plugins : {
+		'dialogui' : 1,
+		'dialog' : 1,
+		'a11yhelp' : 1,
+		'basicstyles' : 1,
+		'blockquote' : 1,
+		'clipboard' : 1,
+		'button' : 1,
+		'panelbutton' : 1,
+		'panel' : 1,
+		'floatpanel' : 1,
+		'colorbutton' : 1,
+		'colordialog' : 1,
+		'dialogadvtab' : 1,
+		'elementspath' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'popup' : 1,
+		'filebrowser' : 1,
+		'find' : 1,
+		'floatingspace' : 1,
+		'listblock' : 1,
+		'richcombo' : 1,
+		'format' : 1,
+		'htmlwriter' : 1,
+		'fakeobjects' : 1,
+		'iframe' : 1,
+		'indent' : 1,
+		'indentblock' : 1,
+		'indentlist' : 1,
+		'justify' : 1,
+		'list' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'preview' : 1,
+		'removeformat' : 1,
+		'resize' : 1,
+		'save' : 1,
+		'selectall' : 1,
+		'stylescombo' : 1,
+		'wysiwygarea' : 1,
+		'table' : 1,
+		'templates' : 1,
+		'toolbar' : 1,
+		'sharedspace' : 1,
+		'undo' : 1,
+		'lineutils' : 1,
+		'widget' : 1,
+		'link' : 1,
+		'image' : 1
+	},
+	languages : {
+		'en' : 1
+	}
+};
