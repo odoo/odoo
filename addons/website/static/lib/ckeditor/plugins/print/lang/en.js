36b21e3eca22f17ed63faafb96c5ce83e315892d
Xavier Morel <xmo@openerp.com>
2013-11-20 09:01:42 +0100

    [REM] CKEDITOR development source, 4.3 final added to web client
    bzr revid: xmo@openerp.com-20131120080142-x5vewroum39jwn3g

--- addons/website/static/lib/ckeditor/plugins/print/lang/en.js
+++
@@ -1,7 +0,0 @@
-﻿/*
-Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.md or http://ckeditor.com/license
-*/
-CKEDITOR.plugins.setLang( 'print', 'en', {
-	toolbar: 'Print'
-});
