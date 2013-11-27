fbb5ddce184b70814681e9cc843eb53bc4031873
Xavier Morel <xmo@openerp.com>
2013-11-27 12:16:14 +0100

    [FIX] MockRequestCase, broken when set_request was removed in server revid:fme@openerp.com-20131120192550-i9xzyqkufe3stnb3
    bzr revid: xmo@openerp.com-20131127111614-n6xjqljzviy2i59m

--- addons/web/tests/common.py
+++ addons/web/tests/common.py
@@ -8,10 +8,10 @@ from openerp import http
 class MockRequestCase(unittest2.TestCase):
     def setUp(self):
         super(MockRequestCase, self).setUp()
-        self.tmp_req = http.set_request(mock.Mock())
-        self.tmp_req.__enter__()
+        self.tmp_req = mock.Mock()
+        http._request_stack.push(self.tmp_req)
 
     def tearDown(self):
-        self.tmp_req.__exit__(None, None, None)
+        http._request_stack.pop()
         super(MockRequestCase, self).tearDown()
 
