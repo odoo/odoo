ffd0f66ad0e4d51dfbf61bd3c422a8edef824dc9
ddm <ddm@openerp.com>
2013-11-27 11:19:46 +0100

    [IMP] Event tour: edit price
    bzr revid: ddm@openerp.com-20131127101946-atfk09jm579jw4t9

--- addons/website_event/static/src/js/website.tour.event.js
+++ addons/website_event/static/src/js/website.tour.event.js
@@ -58,6 +58,14 @@
                     template: self.popover({ next: "OK" }),
                 },
                 {
+                    stepId: 'event-price',
+                    element: '[data-oe-field=price]',
+                    placement: 'top',
+                    title: "Ticket price",
+                    content: "Edit your ticket price.",
+                    template: self.popover({ next: "OK" }),
+                },
+                {
                     stepId: 'add-banner',
                     element: 'button[data-action=snippet]',
                     placement: 'bottom',
