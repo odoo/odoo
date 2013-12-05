4543fe40a09fdfffd0a4e4efc9d04500d866913c
bth-openerp <bth@tinyerp.com>
2013-12-05 12:53:31 +0530

    [IMP]On canvas added tooltip.
    bzr revid: bth@tinyerp.com-20131205072331-95yoyhvakb8hf453

--- addons/web_kanban_sparkline/static/src/js/kanban_sparkline.js
+++ addons/web_kanban_sparkline/static/src/js/kanban_sparkline.js
@@ -27,7 +27,7 @@ instance.web_kanban.SparklineBarWidget = instance.web_kanban.AbstractField.exten
                     }
                 }, self.options);
             self.$el.sparkline(value, sparkline_options);
-            self.$el.tooltip({'html': true, 'title': function(){return title}, 'placement': 'bottom', 'container': 'body'});
+            self.$el.children().tooltip({'html': true, 'title': function(){return title}, 'container': 'body', 'placement': 'bottom'});
         }, 0);
     },
 });
