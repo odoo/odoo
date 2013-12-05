d181c63d07178119b691b89dda2202e09a6dff72
Thibault Delavallée <tde@openerp.com>
2013-12-05 15:45:05 +0100

    [CLEAN] sale_crm: cleaning before merging - sales report: the group by salesteam was wrongly placed in the view - sale_crm: fixed computation for sparklines, now bar graph should display the same result as the sales analysis - added a forgottent cursor: pointer for a gauge - moved the gauges in the dom - sale_crm: report: removed extra content not necessary
    bzr revid: tde@openerp.com-20131205144505-jfsd8lh91r1b13a1

--- addons/sale_crm/report/sale_report.py
+++ addons/sale_crm/report/sale_report.py
@@ -1,26 +1,7 @@
  # -*- coding: utf-8 -*-
- ##############################################################################
- #
- #    OpenERP, Open Source Management Solution
- #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
- #
- #    This program is free software: you can redistribute it and/or modify
- #    it under the terms of the GNU Affero General Public License as
- #    published by the Free Software Foundation, either version 3 of the
- #    License, or (at your option) any later version.
- #
- #    This program is distributed in the hope that it will be useful,
- #    but WITHOUT ANY WARRANTY; without even the implied warranty of
- #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- #    GNU Affero General Public License for more details.
- #
- #    You should have received a copy of the GNU Affero General Public License
- #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- #
- ############################################################################## 
- 
+
 from openerp.osv import fields, osv
-from openerp import tools
+
 
 class sale_report(osv.osv):
     _inherit = "sale.report"
@@ -29,7 +10,7 @@ class sale_report(osv.osv):
     }
 
     def _select(self):
-        return  super(sale_report, self)._select() + ", s.section_id as section_id"
+        return super(sale_report, self)._select() + ", s.section_id as section_id"
 
     def _group_by(self):
         return super(sale_report, self)._group_by() + ", s.section_id"
