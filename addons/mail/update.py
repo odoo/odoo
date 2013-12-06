4eec5e416c7682eaa22a5b29da297f7d232cf539
Thibault Delavallée <tde@openerp.com>
2013-12-06 11:02:30 +0100

    [IMP] mail: update: call to get_object updated
    bzr revid: tde@openerp.com-20131206100230-n4xxvskdbf3m0h1f

--- addons/mail/update.py
+++ addons/mail/update.py
@@ -94,7 +94,7 @@ class publisher_warranty_contract(osv.osv):
             # old behavior based on res.log; now on mail.message, that is not necessarily installed
             IMD = self.pool['ir.model.data']
             user = self.pool['res.users'].browse(cr, SUPERUSER_ID, SUPERUSER_ID)
-            poster = IMD.get_object(cr, SUPERUSER_ID, 'mail', 'group_all_employees')
+            poster = IMD.get_object(cr, SUPERUSER_ID, 'mail', 'group_all_employees', context=context, check_existence_and_raise=False)
             if not poster.exists():
                 if not user.exists():
                     return True
