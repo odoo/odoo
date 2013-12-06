3a038b60f2440bd5c049f635c1a7aa2e311edd81
Thibault Delavallée <tde@openerp.com>
2013-12-06 17:39:11 +0100

    [CLEAN] hr_applicant_document: code cleaning - new module organisation: model-related files in models/, demo in demo/, view data in views/ - code cleaning to lessen the code size
    bzr revid: tde@openerp.com-20131206163911-qx1k1edogqxg52kv

--- addons/hr_applicant_document/__init__.py
+++ addons/hr_applicant_document/__init__.py
@@ -19,6 +19,4 @@
 #
 ##############################################################################
 
-import hr_applicant_document
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+import models
--- addons/hr_applicant_document/__openerp__.py
+++ addons/hr_applicant_document/__openerp__.py
@@ -1,24 +1,4 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-today OpenERP SA (<http://www.openerp.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-##############################################################################
-
 
 {
     'name': 'Applicant Resumes and Letters',
@@ -26,17 +6,20 @@
     'category': 'Human Resources',
     'sequence': 25,
     'summary': 'Search job applications by Index content.',
-    'description': """
-    This module allows you to search job applications by Index content
-    of Resumes & Letters.
-""",
+    'description': """This module allows you to search job applications by content
+    of resumes and letters.""",
     'author': 'OpenERP SA',
     'website': 'http://www.openerp.com',
-    'images': [],
-    'depends': ['hr_recruitment','document'],
-    'data': ['hr_applicant_document_view.xml'],
-    'demo': ['hr_applicant_document_demo.xml'],
-    'test': [],
+    'depends': [
+        'hr_recruitment',
+        'document'
+    ],
+    'data': [
+        'views/hr_applicant.xml'
+    ],
+    'demo': [
+        'demo/hr_applicant.xml'
+    ],
     'installable': True,
     'auto_install': True,
     'application': True,
