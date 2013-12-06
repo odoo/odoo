74ed3d2e5f2d0a3434bdd8c5704a079cdba59073
ddm <ddm@openerp.com>
2013-12-06 14:32:39 +0100

    [FIX] Make tour declarative to enable autotests
    bzr revid: ddm@openerp.com-20131206133239-mrhduf2fi0sm8sh7

--- addons/website/static/src/js/website.tour.banner.js
+++ addons/website/static/src/js/website.tour.banner.js
@@ -11,7 +11,7 @@
     });
 
     website.BannerTour = website.Tour.extend({
-        id: 'banner-tutorial',
+        id: 'banner',
         name: "Insert a banner",
         startPath: '/page/website.homepage',
         init: function (editor) {
@@ -19,11 +19,10 @@
             self.steps = [
                 {
                     stepId: 'welcome',
-                    orphan: true,
-                    backdrop: true,
                     title: "Welcome to your website!",
                     content: "This tutorial will guide you to build your home page. We will start by adding a banner.",
                     template: self.popover({ next: "Start Tutorial", end: "Skip It" }),
+                    backdrop: true,
                 },
                 {
                     stepId: 'edit-page',
@@ -31,8 +30,10 @@
                     placement: 'bottom',
                     title: "Edit this page",
                     content: "Every page of your website can be modified through the <i>Edit</i> button.",
-                    triggers: function () {
-                        editor.on('tour:editor_bar_loaded', self, self.moveToNextStep);
+                    trigger: {
+                        emitter: editor,
+                        type: 'openerp',
+                        id: 'tour:editor_bar_loaded',
                     },
                 },
                 {
@@ -41,11 +42,7 @@
                     placement: 'bottom',
                     title: "Insert building blocks",
                     content: "To add content in a page, you can insert building blocks.",
-                    triggers: function () {
-                        $('button[data-action=snippet]').one('click', function () {
-                            self.moveToNextStep();
-                        });
-                    }
+                    trigger: 'click',
                 },
                 {
                     stepId: 'drag-banner',
@@ -53,9 +50,7 @@
                     placement: 'bottom',
                     title: "Drag & Drop a Banner",
                     content: "Drag the Banner block and drop it in your page.",
-                    triggers: function () {
-                        self.onSnippetDraggedAdvance();
-                    },
+                    trigger: 'drag',
                 },
                 {
                     stepId: 'edit-title',
@@ -64,7 +59,7 @@
                     title: "Customize banner's text",
                     content: "Click in the text and start editing it. Click continue once it's done.",
                     template: self.popover({ next: "Continue" }),
-                    triggers: function () {
+                    onStart: function () {
                         var $banner = $("#wrap [data-snippet-id=carousel]:first");
                         if ($banner.length) {
                             $banner.click();
@@ -85,11 +80,7 @@
                     placement: 'bottom',
                     title: "Add Another Block",
                     content: "Let's add another building block to your page.",
-                    triggers: function () {
-                        $('button[data-action=snippet]').one('click', function () {
-                            self.moveToNextStep();
-                        });
-                    }
+                    trigger: 'click',
                 },
                 {
                     stepId: 'drag-three-columns',
@@ -97,9 +88,7 @@
                     placement: 'bottom',
                     title: "Drag & Drop a Block",
                     content: "Drag the <em>'3 Columns'</em> block and drop it below the banner.",
-                    triggers: function () {
-                        self.onSnippetDraggedAdvance();
-                    },
+                    trigger: 'drag',
                 },
                 {
                     stepId: 'activate-text-block-title',
@@ -107,31 +96,28 @@
                     placement: 'top',
                     title: "Edit an Area",
                     content: "Select any area of the page to modify it. Click on this subtitle.",
-                    triggers: function () {
-                        $('#wrap [data-snippet-id=three-columns] .text-center[data-snippet-id=colmd]').one('click', function () {
-                            self.moveToNextStep();
-                        });
+                    trigger: {
+                        id: 'tour:snippet_activated'
                     },
                 },
                 {
                     stepId: 'remove-text-block-title',
                     element: '.ui-draggable.oe_active .oe_snippet_remove',
                     placement: 'top',
-                    reflex: true,
                     title: "Delete the Title",
                     content: "From this toolbar you can move, duplicate or delete the selected zone. Click on the trash can to delete the title.",
+                    trigger: 'click',
                 },
                 {
                     stepId: 'save-changes',
                     element: 'button[data-action=save]',
                     placement: 'right',
-                    reflex: true,
                     title: "Save your modifications",
                     content: "Publish your page by clicking on the <em>'Save'</em> button.",
+                    trigger: 'click',
                 },
                 {
                     stepId: 'part-2',
-                    orphan: true,
                     title: "Congratulation!",
                     content: "Your homepage has been updated.",
                     template: self.popover({ next: "Continue" }),
@@ -142,34 +128,30 @@
                     placement: 'bottom',
                     title: "Test Your Mobile Version",
                     content: "Let's check how your homepage looks like on mobile devices.",
-                    triggers: function () {
-                        $(document).one('shown.bs.modal', function () {
-                            self.moveToNextStep();
-                        });
-                    }
+                    trigger: {
+                        emitter: $(document),
+                        id: 'shown.bs.modal',
+                    },
                 },
                 {
                     stepId: 'show-mobile-close',
                     element: 'button[data-dismiss=modal]',
                     placement: 'right',
-                    reflex: true,
                     title: "Close Mobile Preview",
                     content: "Scroll in the mobile preview to test the rendering. Once it's ok, close this dialog.",
+                    trigger: 'click',
                 },
                 {
                     stepId: 'show-tutorials',
                     element: '#help-menu-button',
                     placement: 'left',
-                    reflex: true,
                     title: "More Tutorials",
                     content: "Get more tutorials through this <em>'Help'</em> menu or click on the left <em>'Edit'</em> button to continue modifying this page.",
                     template: self.popover({ end: "Close Tutorial" }),
+                    trigger: 'click',
                 }
             ];
             return this._super();
-        },
-        resume: function () {
-            return this.isCurrentStep('part-2') && this._super();
         },
     });
 
