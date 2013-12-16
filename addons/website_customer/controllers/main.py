7cd60fb225fa0f7fe1eec928624bb212508eb445
ddm <ddm@openerp.com>
2013-12-16 10:58:47 +0100

    [FIX] Customers CMS bug
    bzr revid: ddm@openerp.com-20131216095847-i4dqzk4ix26hh3cg

--- addons/website_customer/controllers/main.py
+++ addons/website_customer/controllers/main.py
@@ -81,11 +81,11 @@ class WebsiteCustomer(http.Controller):
             return self.customers(**post)
 
         partner_obj = request.registry['res.partner']
-        if values['partner_data']['assigned_partner_id']:
+        if values['partner_data'].get('assigned_partner_id', None):
             values['assigned_partner_data'] = partner_obj.read(
                 request.cr, openerp.SUPERUSER_ID, [values['partner_data']['assigned_partner_id'][0]],
                 request.website.get_partner_white_list_fields(), context=request.context)[0]
-        if values['partner_data']['implemented_partner_ids']:
+        if values['partner_data'].get('implemented_partner_ids', None):
             implemented_partners_data = partner_obj.read(
                 request.cr, openerp.SUPERUSER_ID, values['partner_data']['implemented_partner_ids'],
                 request.website.get_partner_white_list_fields(), context=request.context)
