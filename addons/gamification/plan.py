3e028490f6018f182ce8b5dcc0eb7e501858c6d1
Martin Trigaux <mat@openerp.com>
2013-12-17 11:48:13 +0100

    [IMP] gamification: licency stuff
    bzr revid: mat@openerp.com-20131217104813-7nh422l1d85s6y51

--- addons/gamification/plan.py
+++ addons/gamification/plan.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-Today OpenERP SA (<http://www.openerp.com>)
+#    Copyright (C) 2013 OpenERP SA (<http://www.openerp.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
