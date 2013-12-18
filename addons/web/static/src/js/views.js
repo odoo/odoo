cb0074ab15efd5e98ca0472ec66a8a89fef42719
Denis Ledoux <dle@openerp.com>
2013-12-18 12:24:16 +0100

    [FIX] web: context propagation was broken on buttons of action and object type.
    The context on the button must be propagated to the resulting action
    
    bzr revid: dle@openerp.com-20131218112416-2vtaq442qo8opq4u

--- addons/web/static/src/js/views.js
+++ addons/web/static/src/js/views.js
@@ -1364,10 +1364,11 @@ instance.web.View = instance.web.Widget.extend({
                 // Wrong default_* and search_default_* values will no give the expected result
                 // Wrong group_by values will simply fail and forbid rendering of the destination view
                 var ncontext = new instance.web.CompoundContext(
-                    _.object(_.reject(_.pairs(context.eval()), function(pair) {
+                    _.object(_.reject(_.pairs(dataset.get_context().eval()), function(pair) {
                       return pair[0].match('^(?:(?:default_|search_default_).+|group_by|group_by_no_leaf|active_id|active_ids)$') !== null;
                     }))
                 );
+                ncontext.add(action_data.context || {});
                 ncontext.add({active_model: dataset.model});
                 if (record_id) {
                     ncontext.add({
