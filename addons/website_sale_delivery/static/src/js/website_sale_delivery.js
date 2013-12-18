531d40cd3bac1fcf0a2481b440607318e342ba37
Thibault Delavallée <tde@openerp.com>
2013-12-18 15:27:35 +0100

    [IMP] website_sale_delivery: when choosing a carrier, immediately update the sale order
    bzr revid: tde@openerp.com-20131218142735-r444egdcmxfap01v

--- addons/website_sale_delivery/static/src/js/website_sale_delivery.js
+++ addons/website_sale_delivery/static/src/js/website_sale_delivery.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
     var $carrier = $("#delivery_carrier");
     $carrier.find("input[name='delivery_type']").click(function (ev) {
         var carrier_id = $(ev.currentTarget).val();
-        console.log('choosing carrier', carrier_id);
         var link = $carrier.find('a');
-        link.attr('href', '/shop/payment?carrier_id=' + carrier_id)
+        link.attr('href', '/shop/payment?carrier_id=' + carrier_id);
+        window.location.href = link.attr('href');
     });
 
 });
