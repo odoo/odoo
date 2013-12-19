86fc66e797c2b12f5af9cd8f0c216dfe1195b3a8
Thibault Delavallée <tde@openerp.com>
2013-12-19 11:51:22 +0100

    [FIX] website_sale: checkout process: fixed carrier choice redirection
    bzr revid: tde@openerp.com-20131219105122-awib6gys6uklbvxa

--- addons/website_sale_delivery/static/src/js/website_sale_delivery.js
+++ addons/website_sale_delivery/static/src/js/website_sale_delivery.js
@@ -4,9 +4,7 @@ $(document).ready(function () {
     var $carrier = $("#delivery_carrier");
     $carrier.find("input[name='delivery_type']").click(function (ev) {
         var carrier_id = $(ev.currentTarget).val();
-        var link = $carrier.find('a');
-        link.attr('href', '/shop/payment?carrier_id=' + carrier_id);
-        window.location.href = link.attr('href');
+        window.location.href = '/shop/payment?carrier_id=' + carrier_id;
     });
 
 });
