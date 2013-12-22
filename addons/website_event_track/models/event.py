ef77ac935ba4feddc239ed1fc1182bfb6d5c8ad3
Fabien Pinckaers <fp@tinyerp.com>
2013-12-22 18:48:04 +0100

    [IMP] better tempaltes for talks
    bzr revid: fp@tinyerp.com-20131222174804-2l2yjzk690qjon1q

--- addons/website_event_track/models/event.py
+++ addons/website_event_track/models/event.py
@@ -143,7 +143,7 @@ class event_event(osv.osv):
         context = context or {}
         result = super(event_event, self)._get_new_menu_pages(cr, uid, event, context=context)
         if event.show_tracks:
-            result.append( (_('Agenda'), '/event/%s/track/' % event.id))
+            result.append( (_('Talks'), '/event/%s/track/' % event.id))
         if event.blog_id:
             result.append( (_('News'), '/blogpost/'+str(event.blog_ig.id)))
         if event.show_track_proposal:
