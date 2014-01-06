0562d90529bd43141d1996a13e8760cd5d1b6875
Mahendra Barad (OpenERP) <mba@tinyerp.com>
2014-01-06 15:46:43 +0530

    [IMP]added the legend widget for priority and kanban state
    bzr revid: mba@tinyerp.com-20140106101643-q71iuge9zojl49s8

--- addons/project/project.py
+++ addons/project/project.py
@@ -753,7 +753,7 @@ class task(osv.osv):
         'active': fields.function(_is_template, store=True, string='Not a Template Task', type='boolean', help="This field is computed automatically and have the same behavior than the boolean 'active' field: if the task is linked to a template or unactivated project, it will be hidden unless specifically asked."),
         'name': fields.char('Task Summary', size=128, required=True, select=True),
         'description': fields.text('Description'),
-        'priority': fields.selection([('4','Very Low'), ('3','Low'), ('2','Medium'), ('1','Important'), ('0','Very important')], 'Priority', select=True),
+        'priority': fields.selection([('0','Very Low'), ('1','Low'), ('2','Medium'), ('3','Important'), ('4','Very important')], 'Priority', select=True),
         'sequence': fields.integer('Sequence', select=True, help="Gives the sequence order when displaying a list of tasks."),
         'stage_id': fields.many2one('project.task.type', 'Stage', track_visibility='onchange',
                         domain="[('project_ids', '=', project_id)]"),
@@ -821,11 +821,12 @@ class task(osv.osv):
         'partner_id': lambda self, cr, uid, ctx=None: self._get_default_partner(cr, uid, context=ctx),
     }
     _order = "priority, sequence, date_start, name, id"
-
-    def set_high_priority(self, cr, uid, ids, *args):
-        """Set task priority to high
-        """
-        return self.write(cr, uid, ids, {'priority' : '0'})
+    
+    def set_kanban_state(self, cr, uid, ids, state, context=None):
+        self.write(cr, uid, ids, {'kanban_state': state }, context=context);
+        
+    def set_priority(self, cr, uid, ids, priority, context=None):
+        return self.write(cr, uid, ids, {'priority' : str(priority)}, context=context)
 
     def set_normal_priority(self, cr, uid, ids, *args):
         """Set task priority to normal
