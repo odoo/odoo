a8262eb44f79f434336be4345effdd48544c9225
Frédéric van der Essen <fva@openerp.com>
2014-01-13 12:07:05 +0100

    [FIX] hw_escpos: use pip module name for external dependencies
    bzr revid: fva@openerp.com-20140113110705-3qob6gdpir3f3rfb

--- addons/hw_escpos/__openerp__.py
+++ addons/hw_escpos/__openerp__.py
@@ -38,7 +38,7 @@ that would need such functionality.
     'author': 'OpenERP SA',
     'depends': ['hw_proxy'],
     'external_dependencies': {
-        'python' : ['usb.core','serial','qrcode'],
+        'python' : ['pyusb','pyserial','qrcode'],
     },
     'test': [
     ],
