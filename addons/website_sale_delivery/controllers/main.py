9624ec21f8b32cbdc4df06e50922b0400345321a
Christophe Matthieu <chm@openerp.com>
2014-01-14 15:58:33 +0100

    [FIX] website_sale_delivery: public mode
    bzr revid: chm@openerp.com-20140114145833-73t9cc60z9i8bniw

--- addons/website_sale_delivery/controllers/main.py
+++ addons/website_sale_delivery/controllers/main.py
@@ -2,6 +2,7 @@
 from openerp.addons.website_sale.controllers.main import Ecommerce
 from openerp.addons.web.http import request
 from openerp.addons.website.models import website
+from openerp import SUPERUSER_ID
 
 
 class Ecommerce(Ecommerce):
@@ -15,8 +16,8 @@ class Ecommerce(Ecommerce):
         if order and carrier_id:
             # recompute delivery costs
             SaleOrder = request.registry['sale.order']
-            SaleOrder.write(cr, uid, [order.id], {'carrier_id': carrier_id}, context=context)
-            SaleOrder.delivery_set(cr, uid, [order.id], context=context)
+            SaleOrder.write(cr, SUPERUSER_ID, [order.id], {'carrier_id': carrier_id}, context=context)
+            SaleOrder.delivery_set(cr, SUPERUSER_ID, [order.id], context=context)
 
         res = super(Ecommerce, self).payment(**post)
         return res
