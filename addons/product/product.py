33af9858b55f365d9ccf2335aa9ae3cafd20bb38
Darshan Kalola (OpenERP) <dka@tinyerp.com>
2014-01-15 17:04:55 +0530

    [IMP]put ondelete='restrict' on product_tmpl_id to restrict deletion of product template if template having a variants.
    bzr revid: dka@tinyerp.com-20140115113455-26aupx34va3b0km5

--- addons/product/product.py
+++ addons/product/product.py
@@ -690,7 +690,7 @@ class product_product(osv.osv):
         'default_code' : fields.char('Internal Reference', size=64, select=True),
         'active': fields.boolean('Active', help="If unchecked, it will allow you to hide the product without removing it."),
         'variants': fields.char('Variants', size=64, translate=True),
-        'product_tmpl_id': fields.many2one('product.template', 'Product Template', required=True, ondelete="cascade", select=True),
+        'product_tmpl_id': fields.many2one('product.template', 'Product Template', required=True, ondelete="restrict", select=True),
         'ean13': fields.char('EAN13 Barcode', size=13, help="International Article Number used for product identification."),
         'packaging' : fields.one2many('product.packaging', 'product_id', 'Logistical Units', help="Gives the different ways to package the same product. This has no impact on the picking order and is mainly used if you use the EDI module."),
         'price_extra': fields.float('Variant Price Extra', digits_compute=dp.get_precision('Product Price'), help="Price Extra: Extra price for the variant on sale price. eg. 200 price extra, 1000 + 200 = 1200."),
