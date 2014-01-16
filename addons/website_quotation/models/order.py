1bcdc4dc2fd4b0b55077542ee698af891c704256
ajay javiya (OpenERP) <aja@tinyerp.com>
2014-01-16 12:13:29 +0530

    [IMP]: better table of content [IMP] : The user should not be able to delete a sale order line ,unless it's an option that has been added previously [IMP]: open the sent email link, it open the cms homepage, not the quote
    bzr revid: aja@tinyerp.com-20140116064329-1da392uplmsm01me

--- addons/website_quotation/models/order.py
+++ addons/website_quotation/models/order.py
@@ -73,6 +73,7 @@ class sale_order_line(osv.osv):
     _description = "Sales Order Line"
     _columns = {
         'website_description': fields.html('Line Description'),
+        'is_optional': fields.boolean('Optional Product')
     }
     def product_id_change(self, cr, uid, ids, pricelist, product, qty=0, uom=False, qty_uos=0, uos=False, name='', partner_id=False, lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
         res = super(sale_order_line, self).product_id_change(cr, uid, ids, pricelist, product, qty, uom, qty_uos, uos, name, partner_id, lang, update_tax, date_order, packaging, fiscal_position, flag, context)
@@ -152,7 +153,7 @@ class sale_option_line(osv.osv):
         'uom_id': fields.many2one('product.uom', 'Unit of Measure ', required=True),
         'quantity': fields.float('Quantity', required=True),
     }
-    
+
     _defaults = {
         'quantity': 1,
     }
