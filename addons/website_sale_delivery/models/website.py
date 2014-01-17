379f3fa3ff0aa577e6504c2e78dc750007353c23
Christophe Matthieu <chm@openerp.com>
2014-01-17 18:38:36 +0100

    [FIX] website_sale: auto set or remove carrier in function of product (service or not)
    bzr revid: chm@openerp.com-20140117173836-s5mpospo9g5xifew

--- addons/website_sale_delivery/models/website.py
+++ addons/website_sale_delivery/models/website.py
@@ -6,17 +6,35 @@ from openerp import SUPERUSER_ID
 class Website(orm.Model):
     _inherit = 'website'
 
-    def _ecommerce_get_quotation_values(self, cr, uid, context=None):
-        """ Override the quotation values generation to add carrier_id data """
-        values = super(Website, self)._ecommerce_get_quotation_values(cr, uid, context=context)
-        DeliveryCarrier = self.pool.get('delivery.carrier')
-        carrier_ids = DeliveryCarrier.search(cr, uid, [(1,"=",1)], context=context, limit=1)
-        # By default, select the first carrier
-        if carrier_ids:
-            values['carrier_id'] = carrier_ids[0]
-        return values
-
     def _ecommerce_create_quotation(self, cr, uid, context=None):
         order_id = super(Website, self)._ecommerce_create_quotation(cr, uid, context=context)
-        self.pool['sale.order'].delivery_set(cr, SUPERUSER_ID, [order_id], context=context)
+        order = self.pool['sale.order'].browse(cr, SUPERUSER_ID, order_id, context=context)
+        self._check_carrier_quotation(cr, uid, order, context=context)
         return order_id
+
+    def _ecommerce_add_product_to_cart(self, cr, uid, product_id=0, order_line_id=0, number=1, set_number=-1, context=None):
+        quantity = super(Website, self)._ecommerce_add_product_to_cart(cr, uid,
+            product_id=product_id, order_line_id=order_line_id, number=number, set_number=set_number,
+            context=context)
+        order = self.ecommerce_get_current_order(cr, uid, context=context)
+        return self._check_carrier_quotation(cr, uid, order, context=context) and quantity or None
+    
+    def _check_carrier_quotation(self, cr, uid, order, context=None):
+        # check to add or remove carrier_id
+        carrier_id = False
+        for line in order.website_order_line:
+            if line.product_id.type != "service":
+                carrier_id = True
+                break
+
+        if not carrier_id:
+            order.write({'carrier_id': None}, context=context)
+            self.pool['sale.order']._delivery_unset(cr, SUPERUSER_ID, order, context=context)
+            return None
+        elif not order.carrier_id:
+            carrier_ids = self.pool.get('delivery.carrier').search(cr, uid, [], context=context)
+            order.write({'carrier_id': carrier_ids[0]}, context=context)
+            order.delivery_set(context=context)
+            return None
+
+        return carrier_id
