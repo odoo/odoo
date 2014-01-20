77457bd8b2763013623936be27edcbcd7edfa551
Raphael Collet <rco@openerp.com>
2014-01-20 12:49:01 +0100

    [FIX] calendar: type of related field should be 'integer', not 'int'
    bzr revid: rco@openerp.com-20140120114901-ryrqgu0drewsjx4y

--- addons/calendar/calendar.py
+++ addons/calendar/calendar.py
@@ -831,7 +831,7 @@ class calendar_event(osv.Model):
         'end_date': fields.date('Repeat Until'),
         'allday': fields.boolean('All Day', states={'done': [('readonly', True)]}),
         'user_id': fields.many2one('res.users', 'Responsible', states={'done': [('readonly', True)]}),
-        'color_partner_id': fields.related('user_id', 'partner_id', 'id', type="int", string="colorize", store=False),  # Color of creator
+        'color_partner_id': fields.related('user_id', 'partner_id', 'id', type="integer", string="colorize", store=False),  # Color of creator
         'active': fields.boolean('Active', help="If the active field is set to true, it will allow you to hide the event alarm information without removing it."),
         'categ_ids': fields.many2many('calendar.event.type', 'meeting_category_rel', 'event_id', 'type_id', 'Tags'),
         'attendee_ids': fields.one2many('calendar.attendee', 'event_id', 'Attendees', ondelete='cascade'),
