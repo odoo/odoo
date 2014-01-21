5d78f8f0646c855ea1b82e87b044d5aef05ba536
Christophe Matthieu <chm@openerp.com>
2014-01-21 11:05:24 +0100

    [FIX] website: import full_path
    bzr revid: chm@openerp.com-20140121100524-elxz0n8qfve1iefr

--- addons/website_blog/tests/test_ui.py
+++ addons/website_blog/tests/test_ui.py
@@ -1,5 +1,5 @@
-import openerp.addons.website.tests.test_ui as test_ui,full_path
+import openerp.addons.website.tests.test_ui as test_ui
 
 def load_tests(loader, base, _):
-    base.addTest(test_ui.WebsiteUiSuite(full_path(__file__,'post_test.js'),   { 'action': 'website.action_website_homepage' }, 60.0))
+    base.addTest(test_ui.WebsiteUiSuite(test_ui.full_path(__file__,'post_test.js'),   { 'action': 'website.action_website_homepage' }, 60.0))
     return base
--- addons/website_sale/tests/test_ui.py
+++ addons/website_sale/tests/test_ui.py
@@ -1,6 +1,6 @@
-import openerp.addons.website.tests.test_ui as test_ui,full_path
+import openerp.addons.website.tests.test_ui as test_ui
 
 def load_tests(loader, base, _):
-    base.addTest(test_ui.WebsiteUiSuite(full_path(__file__,'website_sale-sale_process-test.js'),
+    base.addTest(test_ui.WebsiteUiSuite(test_ui.full_path(__file__,'website_sale-sale_process-test.js'),
         { 'action': 'website.action_website_homepage' }, 60.0))
     return base
