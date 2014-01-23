020921a7f32aa6e211b2b278a54c44cdb09f5263
Raphael Collet <rco@openerp.com>
2014-01-23 10:49:57 +0100

    [IMP] orm: remove commented code
    bzr revid: rco@openerp.com-20140123094957-knrgx6sy66j4tj39

--- openerp/osv/orm.py
+++ openerp/osv/orm.py
@@ -5002,32 +5002,6 @@ class BaseModel(object):
         """ stuff to do right after the registry is built """
         pass
 
-
-    #def __getattr__(self, name):
-    #    """
-    #    Proxies attribute accesses to the `inherits` parent so we can call methods defined on the inherited parent
-    #    (though inherits doesn't use Python inheritance).
-    #    Handles translating between local ids and remote ids.
-    #    Known issue: doesn't work correctly when using python's own super(), don't involve inherit-based inheritance
-    #                 when you have inherits.
-    #    """
-    #    for model, field in self._inherits.iteritems():
-    #        proxy = self.pool.get(model)
-    #        if hasattr(proxy, name):
-    #            attribute = getattr(proxy, name)
-    #            if not hasattr(attribute, '__call__'):
-    #                return attribute
-    #            break
-    #    else:
-    #        return super(orm, self).__getattr__(name)
-
-    #    def _proxy(cr, uid, ids, *args, **kwargs):
-    #        objects = self.browse(cr, uid, ids, kwargs.get('context', None))
-    #        lst = [obj[field].id for obj in objects if obj[field]]
-    #        return getattr(proxy, name)(cr, uid, lst, *args, **kwargs)
-
-    #    return _proxy
-
     def __getattr__(self, name):
         if name.startswith('signal_'):
             signal_name = name[len('signal_'):]
