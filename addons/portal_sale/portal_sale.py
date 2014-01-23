7678ba5bfce0642088db43710be66c943b7c996b
Thibault Delavallée <tde@openerp.com>
2014-01-23 17:20:59 +0100

    [FIX] portal_sale: last occurence of portal.payment.acquirer -> payment.acquirer
    bzr revid: tde@openerp.com-20140123162059-6omcrt4a32x7t47c

--- addons/portal_sale/portal_sale.py
+++ addons/portal_sale/portal_sale.py
@@ -85,7 +85,7 @@ class account_invoice(osv.Model):
 
     def _portal_payment_block(self, cr, uid, ids, fieldname, arg, context=None):
         result = dict.fromkeys(ids, False)
-        payment_acquirer = self.pool.get('portal.payment.acquirer')
+        payment_acquirer = self.pool.get('payment.acquirer')
         for this in self.browse(cr, uid, ids, context=context):
             if this.type == 'out_invoice' and this.state not in ('draft', 'done') and not this.reconciled:
                 result[this.id] = payment_acquirer.render_payment_block(
