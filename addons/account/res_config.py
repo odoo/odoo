9c74cab9a56a1b67461851bf3753454327223d98
Thibault Delavallée <tde@openerp.com>
2014-01-24 10:39:47 +0100

    [REVIEW] account: wording of the new config field
    bzr revid: tde@openerp.com-20140124093947-kbz534udanag4v14

--- addons/account/res_config.py
+++ addons/account/res_config.py
@@ -106,7 +106,8 @@ class account_config_settings(osv.osv_memory):
             help='This allows to automate letters for unpaid invoices, with multi-level recalls.\n'
                  '-This installs the module account_followup.'),
         'module_account_product_template': fields.boolean('Send products tools and information at the invoice confirmation',
-            help='Whith this module, link your products to a template to send complete information and tools to your customer. For instance, you invoice a training, link a template to this training product with training agenda and materials.'),
+            help='Whith this module, link your products to a template to send complete information and tools to your customer.\n'
+                 'For instance when invoicing a training, the training agenda and materials will automatically be send to your customers.'),
         'group_proforma_invoices': fields.boolean('Allow pro-forma invoices',
             implied_group='account.group_proforma_invoices',
             help="Allows you to put invoices in pro-forma state."),
