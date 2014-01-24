093e8633dcc2dd22513789ffe260c3a981d68184
Thibault Delavallée <tde@openerp.com>
2014-01-24 15:56:28 +0100

    [FIX] payment_transfer: fixed controler, request.redirect -> werkzeug.redirect
    bzr revid: tde@openerp.com-20140124145628-3e9pvz88wyww9kr3

--- addons/payment_transfer/controllers/main.py
+++ addons/payment_transfer/controllers/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import pprint
+import werkzeug
 
 from openerp.addons.web import http
 from openerp.addons.web.http import request
@@ -18,4 +19,4 @@ class OgoneController(http.Controller):
         cr, uid, context = request.cr, request.uid, request.context
         _logger.info('Beginning form_feedback with post data %s', pprint.pformat(post))  # debug
         request.registry['payment.transaction'].form_feedback(cr, uid, post, 'transfer', context)
-        return request.redirect(post.pop('return_url', '/'))
+        return werkzeug.utils.redirect(post.pop('return_url', '/'))
