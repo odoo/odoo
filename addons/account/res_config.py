5d17667bb12bfbb7188354c5a4417e9712e91309
Thibault Delavallée <tde@openerp.com>
2014-01-24 12:08:02 +0100

    [RENAME] account_product_template -> product_email_template, to avoid confusion with product templates. The main feature are email template on product therefore renaming the module.
    bzr revid: tde@openerp.com-20140124110802-unn4u75ad4jt0bb2

--- addons/account/res_config.py
+++ addons/account/res_config.py
@@ -105,7 +105,7 @@ class account_config_settings(osv.osv_memory):
         'module_account_followup': fields.boolean('Manage customer payment follow-ups',
             help='This allows to automate letters for unpaid invoices, with multi-level recalls.\n'
                  '-This installs the module account_followup.'),
-        'module_account_product_template': fields.boolean('Send products tools and information at the invoice confirmation',
+        'module_product_email_template': fields.boolean('Send products tools and information at the invoice confirmation',
             help='With this module, link your products to a template to send complete information and tools to your customer.\n'
                  'For instance when invoicing a training, the training agenda and materials will automatically be send to your customers.'),
         'group_proforma_invoices': fields.boolean('Allow pro-forma invoices',
