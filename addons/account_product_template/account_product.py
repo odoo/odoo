0ad36cf35ff01f2fe92f9f7081b1297620c8b54a
Turkesh Patel (Open ERP) <tpa@tinyerp.com>
2014-01-24 11:39:45 +0530

    [IMP] improved code and send template only on cutomer invoice
    bzr revid: tpa@tinyerp.com-20140124060945-zn1bpzt41xwkh3p7

--- addons/account_product_template/account_product.py
+++ addons/account_product_template/account_product.py
@@ -32,23 +32,26 @@ class account_invoice(osv.Model):
     def invoice_validate(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
-        mail_obj = self.pool.get('mail.compose.message')
-        template_obj = self.pool.get('email.template')
+        mail_msg_obj = self.pool['mail.compose.message']
+        template_obj = self.pool['email.template']
         res = super(account_invoice, self).invoice_validate(cr, uid, ids, context=context)
         for invoice in self.browse(cr, uid, ids, context=context):
-            # fetch the partner's id and subscribe the partner to the invoice
+            # send template only on customer invoice
+            if invoice.type != 'out_invoice':
+                continue
+            # subscribe the partner to the invoice
             if invoice.partner_id.id not in invoice.message_follower_ids:
                 self.message_subscribe(cr, uid, [invoice.id], [invoice.partner_id.id], context=context)
             for line in invoice.invoice_line:
                 if line.product_id.email_template_id:
                     template_res = template_obj.get_email_template_batch(cr, uid, template_id=line.product_id.email_template_id.id, res_ids=[line.product_id.product_tmpl_id.id], context=context)
                     mail = template_res[line.product_id.product_tmpl_id.id]
-                    message_wiz_id = mail_obj.create(cr, uid, {
+                    message_wiz_id = mail_msg_obj.create(cr, uid, {
                         'model': 'account.invoice',
                         'res_id': invoice.id,
                         'body': mail.body_html,
                     }, context=context)
-                    mail_obj.send_mail(cr, uid, [message_wiz_id], context=context)
+                    mail_msg_obj.send_mail(cr, uid, [message_wiz_id], context=context)
         return res
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
