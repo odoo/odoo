2ef9fbe42f0452289649c08fe0bd00117380ea9a
Antony Lesuisse <al@openerp.com>
2014-01-26 22:20:18 +0100

    [FIX] livechat dataweb-style rename
    bzr revid: al@openerp.com-20140126212018-q8f63sy21t1ph8dn

--- addons/im_livechat/im_livechat.py
+++ addons/im_livechat/im_livechat.py
@@ -5,16 +5,16 @@
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General anonymous License as
+#    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General anonymous License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General anonymous License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
