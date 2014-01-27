f6fec727974c1cf4094f044fbf88184d93603ae5
Gery Debongnie <ged@openerp.com>
2014-01-27 10:49:18 +0100

    [IMP] improves the Event reporting view by using the new graph view in pivot mode (addon event)
    bzr revid: ged@openerp.com-20140127094918-52w9xt1nj1ae93fn

--- addons/event/report/report_event_registration.py
+++ addons/event/report/report_event_registration.py
@@ -27,12 +27,7 @@ class report_event_registration(osv.osv):
     _description = "Events Analysis"
     _auto = False
     _columns = {
-        'event_date': fields.char('Event Start Date', size=64, readonly=True),
-        'year': fields.char('Year', size=4, readonly=True),
-        'month': fields.selection([
-            ('01','January'), ('02','February'), ('03','March'), ('04','April'),
-            ('05','May'), ('06','June'), ('07','July'), ('08','August'),
-            ('09','September'), ('10','October'), ('11','November'), ('12','December')], 'Month',readonly=True),
+        'event_date': fields.datetime('Event Date', readonly=True),
         'event_id': fields.many2one('event.event', 'Event', required=True),
         'draft_state': fields.integer(' # No of Draft Registrations', size=20),
         'confirm_state': fields.integer(' # No of Confirmed Registrations', size=20),
@@ -65,9 +60,7 @@ class report_event_registration(osv.osv):
                 r.name AS name_registration,
                 e.company_id AS company_id,
                 e.main_speaker_id AS speaker_id,
-                to_char(e.date_begin, 'YYYY-MM-DD') AS event_date,
-                to_char(e.date_begin, 'YYYY') AS year,
-                to_char(e.date_begin, 'MM') AS month,
+                e.date_begin AS event_date,
                 count(e.id) AS nbevent,
                 CASE WHEN r.state IN ('draft') THEN r.nb_register ELSE 0 END AS draft_state,
                 CASE WHEN r.state IN ('open','done') THEN r.nb_register ELSE 0 END AS confirm_state,
@@ -92,8 +85,6 @@ class report_event_registration(osv.osv):
                 event_state,
                 e.company_id,
                 e.main_speaker_id,
-                year,
-                month,
                 e.register_max,
                 name_registration
         )
