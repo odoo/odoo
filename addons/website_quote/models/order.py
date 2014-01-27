c83ad0d1b6c1d683dedbcb477fa15329529f4af0
Christophe Simonis <chs@openerp.com>
2014-01-27 18:59:56 +0100

    [FIX] website_roller: remove useless on_change
    bzr revid: chs@openerp.com-20140127175956-04nmxodpnbn59bmc

--- addons/website_quote/models/order.py
+++ addons/website_quote/models/order.py
@@ -80,13 +80,6 @@ class sale_order_line(osv.osv):
         'website_description': fields.html('Line Description'),
         'option_line_id':fields.one2many('sale.order.option', 'line_id', 'Optional Products Lines'),
     }
-    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0, uom=False, qty_uos=0, uos=False, name='', partner_id=False, lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
-        res = super(sale_order_line, self).product_id_change(cr, uid, ids, pricelist, product, qty, uom, qty_uos, uos, name, partner_id, lang, update_tax, date_order, packaging, fiscal_position, flag, context)
-        if product:
-            desc = self.pool.get('product.product').browse(cr, uid, product, context).website_description
-            res.get('value').update({'website_description': desc})
-        return res
-
 
 class sale_order(osv.osv):
     _inherit = 'sale.order'
