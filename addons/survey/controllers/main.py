b17d06b77994f83183200b18c0ede30e3d840918
Richard Mathot (OpenERP) <rim@openerp.com>
2014-01-28 15:52:17 +0100

    [FIX] Some forgottent contexts
    bzr revid: rim@openerp.com-20140128145217-vdnrqvkc5qmrjler

--- addons/survey/controllers/main.py
+++ addons/survey/controllers/main.py
@@ -100,13 +100,13 @@ class WebsiteSurvey(http.Controller):
         # Manual surveying
         if not token:
             if survey.visible_to_user:
-                user_input_id = user_input_obj.create(cr, uid, {'survey_id': survey.id})
+                user_input_id = user_input_obj.create(cr, uid, {'survey_id': survey.id}, context=context)
                 user_input = user_input_obj.browse(cr, uid, [user_input_id], context=context)[0]
             else:  # An user cannot open hidden surveys without token
                 return request.website.render("website.403")
         else:
             try:
-                user_input_id = user_input_obj.search(cr, uid, [('token', '=', token)])[0]
+                user_input_id = user_input_obj.search(cr, uid, [('token', '=', token)], context=context)[0]
             except IndexError:  # Invalid token
                 return request.website.render("website.403")
             else:
