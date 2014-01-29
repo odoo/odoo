9328ac99082109ef98628ecfff31f00be6db6ac7
Sunil Sharma (OpenERP) <sunilsharma.sharma07@gmail.com>
2014-01-29 18:43:53 +0530

    [imp]:remove sapece
    bzr revid: sunilsharma.sharma07@gmail.com-20140129131353-26259ntx22t8wd4z

--- addons/stock/__openerp__.py
+++ addons/stock/__openerp__.py
@@ -28,8 +28,8 @@
 Manage multi-warehouses, multi- and structured stock locations
 ==============================================================
 
-The warehouse and inventory management is based on a hierarchical location structure, from warehouses to storage bins.
-The double entry inventory system allows you to manage customers, suppliers as well as manufacturing inventories.
+The warehouse and inventory management is based on a hierarchical location structure, from warehouses to storage bins. 
+The double entry inventory system allows you to manage customers, suppliers as well as manufacturing inventories. 
 
 OpenERP has the capacity to manage lots and serial numbers ensuring compliance with the traceability requirements imposed by the majority of industries.
 
