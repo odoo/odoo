bf2198e4c8c22775d91ac074ce0a40a3612a05ba
Antony Lesuisse <al@openerp.com>
2014-02-10 00:45:31 +0100

    [IMP] website test shorter code
    bzr revid: al@openerp.com-20140209234531-0i2xgr94yjaqzen8

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -8,11 +8,11 @@ fname, _ = os.path.splitext(__file__)
 class TestUiAdmin(openerp.tests.HttpCase):
     def test(self):
         for i in glob.glob('%s_admin_*.js' % fname):
-            self.phantomjs(openerp.modules.module.get_module_resource('website','tests', i)) 
+            self.phantomjs(i) 
 
 class TestUiPublic(openerp.tests.HttpCase):
     def test(self):
         for i in glob.glob('%s_public_*.js' % fname):
-            self.phantomjs(openerp.modules.module.get_module_resource('website','tests', i)) 
+            self.phantomjs(i) 
 
 
