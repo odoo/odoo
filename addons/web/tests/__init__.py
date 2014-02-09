c954e9adf273a77403e6134a9e8d07f8e7bb814a
Antony Lesuisse <al@openerp.com>
2014-02-09 01:43:10 +0100

    fix web tests
    bzr revid: al@openerp.com-20140209004310-0jpc29beh7k1e27c

--- addons/web/tests/__init__.py
+++ addons/web/tests/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-from . import test_menu, test_serving_base, test_js
-
-fast_suite = []
-checks = [
-    test_menu,
-    test_serving_base,
-]
+import test_js
+import test_menu
+import test_serving_base
