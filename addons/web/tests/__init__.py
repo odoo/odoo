85d37619e427d1845f7f859485372339a5e59cd6
Antony Lesuisse <al@openerp.com>
2014-02-10 00:20:01 +0100

    [IMP] add test_ui
    bzr revid: al@openerp.com-20140209232001-1efvws2pzeldbuhb

--- addons/web/tests/__init__.py
+++ addons/web/tests/__init__.py
@@ -2,3 +2,4 @@
 import test_js
 import test_menu
 import test_serving_base
+import test_ui
---
+++ addons/web/tests/test_ui.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import os
+import glob
+
+import openerp.tests
+
+fname, _ = os.path.splitext(__file__)
+
+class TestUi(openerp.tests.HttpCase):
+    def test(self):
+        for i in glob.glob('%s*.js' % fname):
+            self.phantomjs(openerp.modules.module.get_module_resource('base','tests', i))
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
