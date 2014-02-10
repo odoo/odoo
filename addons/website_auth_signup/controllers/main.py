e44ff1a350edcc6dd6499ff861cc20fa81b43259
Fabien Meghazi <fme@openerp.com>
2014-02-10 15:21:22 +0100

    [IMP] Not forced to use request.website.render() anymore
    bzr revid: fme@openerp.com-20140210142122-xuqnfnukrfcktflc

--- addons/website_auth_signup/controllers/main.py
+++ addons/website_auth_signup/controllers/main.py
@@ -1,20 +1,13 @@
 import openerp
 from openerp import http
-from openerp.http import request, LazyResponse
 
 class AuthSignupWebsiteLogin(openerp.addons.web.controllers.main.Home):
     @http.route(website=True, auth="public", multilang=True)
     def web_auth_signup(self, *args, **kw):
-        response = super(AuthSignupWebsiteLogin, self).web_auth_signup(*args, **kw)
-        if isinstance(response, LazyResponse):
-            response = request.website.render(response.params['template'], response.params['values'])
-        return response
+        return super(AuthSignupWebsiteLogin, self).web_auth_signup(*args, **kw)
 
     @http.route(website=True, auth="public", multilang=True)
     def web_auth_reset_password(self, *args, **kw):
-        response = super(AuthSignupWebsiteLogin, self).web_auth_reset_password(*args, **kw)
-        if isinstance(response, LazyResponse):
-            response = request.website.render(response.params['template'], response.params['values'])
-        return response
+        return super(AuthSignupWebsiteLogin, self).web_auth_reset_password(*args, **kw)
 
 # vim:expandtab:tabstop=4:softtabstop=4:shiftwidth=4:
