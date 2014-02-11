ce2a772ae39754bb6bf1b59385e519551289586e
ajay javiya (OpenERP) <aja@tinyerp.com>
2014-02-11 11:53:56 +0530

    [ADD]: animation on down arrow in blog cover
    bzr revid: aja@tinyerp.com-20140211062356-0gm6b73n4p3brp0u

--- addons/website_blog/static/src/js/website_blog.js
+++ addons/website_blog/static/src/js/website_blog.js
@@ -23,4 +23,17 @@ $(document).ready(function() {
             }
         });
     }
+
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 });
