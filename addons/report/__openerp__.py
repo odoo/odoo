71be2de1efd510a4f9c2c91e672bbe3e32ca5b2b
openerp-sle <openerp-sle@openerp-sle.home>
2014-02-12 18:22:14 +0100

    [IMP] Add structure of the 'report' module
    bzr revid: openerp-sle@openerp-sle.home-20140212172214-r0n8wzngr1hkp8q0

---
+++ addons/report/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Report',
+    'category': 'Website',
+    'summary': 'Report',
+    'version': '1.0',
+    'description': """
+Report
+        """,
+    'author': 'OpenERP SA',
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': True,
+}
