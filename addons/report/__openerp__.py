0d01d96dd43641dd8608288974f9ad9383cc6145
openerp-sle <openerp-sle@openerp-sle.home>
2014-02-12 18:54:12 +0100

    [IMP] Added a 'paperformat' model that will handle properties of pdf documents that are generated from html reports. A company is linked to a default paper format and a report can be linked to a specific paperformat in order to override the default one.
    bzr revid: openerp-sle@openerp-sle.home-20140212175412-aeij8e9ifwjwgc1m

--- addons/report/__openerp__.py
+++ addons/report/__openerp__.py
@@ -9,6 +9,10 @@ Report
     'author': 'OpenERP SA',
     'depends': ['base'],
     'data': [
+        'views/paperformat_view.xml',
+        'views/res_company_view.xml',
+        'data/paperformat_defaults.xml',
+        'security/ir.model.access.csv',
     ],
     'installable': True,
 }
--- addons/report/models/__init__.py
+++ addons/report/models/__init__.py
@@ -1,2 +1,3 @@
 import report
+import paperformat
 
