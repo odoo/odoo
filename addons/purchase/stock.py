0e0443e2d27894de1b2505edc2aeb1c9f4b2ce58
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2014-02-17 21:42:44 +0100

    [FIX] purchase: fixed workflow problem in purchase.order (if a PO was having its picking split, then its workflow wasn't listenning to the new move.. which lead to wrong PO state and 'shipped' boolean)
    bzr revid: qdp-launchpad@openerp.com-20140217204244-78sui3bdzw2yl2wp

--- addons/purchase/stock.py
+++ addons/purchase/stock.py
@@ -35,8 +35,14 @@ class stock_move(osv.osv):
             ids = [ids]
         res = super(stock_move, self).write(cr, uid, ids, vals, context=context)
         from openerp import workflow
-        for id in ids:
-            workflow.trg_trigger(uid, 'stock.move', id, cr)
+        if 'state' in vals:
+            for move in self.browse(cr, uid, ids, context=context):
+                if move.purchase_line_id and move.purchase_line_id.order_id:
+                    order_id = move.purchase_line_id.order_id.id
+                    if self.pool.get('purchase.order').test_moves_done(cr, uid, [order_id], context=context):
+                        workflow.trg_validate(uid, 'purchase.order', order_id, 'picking_done', cr)
+                    if self.pool.get('purchase.order').test_moves_except(cr, uid, [order_id], context=context):
+                        workflow.trg_validate(uid, 'purchase.order', order_id, 'picking_cancel', cr)
         return res
 
     def copy(self, cr, uid, id, default=None, context=None):
