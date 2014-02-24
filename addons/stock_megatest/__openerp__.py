8f1192e0b04a3868d0cbdb6525bd244615c5d706
Josse Colpaert <jco@openerp.com>
2014-02-24 10:41:24 +0100

    [ADD] Add meagtest
    bzr revid: jco@openerp.com-20140224094124-2vmukbsy006d8j4y

---
+++ addons/stock_megatest/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Dropshipping Module',
+    'version': '1.0',
+    'category': 'Hidden',
+    'summary': 'Dropshipping',
+    'description': """
+Manage sales quotations and stock locations
+==========================================
+
+This adds the route to make dropshipping sales orders in which the product sold are directly transfered from the reseller to the customer (direct delivery) without creating any internal document for the transfer.
+
+""",
+    'author': 'OpenERP SA',
+    'website': 'http://www.openerp.com',
+    'images': [],
+    'depends': ['stock_dropshipping'],
+    'init_xml': [],
+    'data': [],
+    'demo_xml': [],
+    'test': [
+        'test/megatestmtobuy.yml'
+        ],
+    'installable': True,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
