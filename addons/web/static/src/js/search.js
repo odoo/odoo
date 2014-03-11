6e9f3ad8064bcd8491e85817d704aba6eb0d944c
Denis Ledoux <dle@openerp.com>
2014-03-11 11:49:47 +0100

    [FIX] web: set z-index of 9999 for autocompletion pane of search bar, to avoid being behind jquery ui dialogs. Besides, this pane should always be on top, therefore we can assume that setting 9999 will not have bad side effects
    bzr revid: dle@openerp.com-20140311104947-is5qpw71y1m2oakf

--- addons/web/static/src/js/search.js
+++ addons/web/static/src/js/search.js
@@ -341,7 +341,7 @@ instance.web.SearchView = instance.web.Widget.extend(/** @lends instance.web.Sea
             }
         },
         'autocompleteopen': function () {
-            this.$el.autocomplete('widget').css('z-index', 1004);
+            this.$el.autocomplete('widget').css('z-index', 9999);
         },
     },
     /**
