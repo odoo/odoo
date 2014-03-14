1de65c1d5d66e2824323ef3d913c8a2eed52d4f5
Vidhin Mehta (OpenERP) <vme@tinyerp.com>
2014-03-14 15:45:46 +0530

    [IMP]Remove row when it's totally empty(taken care of rowspan), now filter will find only its related table.
    bzr revid: vme@tinyerp.com-20140314101546-ani4rne4jjldm7gi

--- addons/website_event_track/static/src/js/website_event_track.js
+++ addons/website_event_track/static/src/js/website_event_track.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
-    var search_object = {};
-    $.each($("td#seach_enable"), function(key, element){
-        var value_td = ($(element).text()).trim();
-        if(value_td)search_object[key] = [value_td.toLowerCase(), element];
-    });
-    $("#start_search").bind('keyup',function(e){
-        var change_text = ($(this).val()).toLowerCase();
-        $.each(search_object, function(key, value){
-            $(value[1]).css("visibility", (value[0].indexOf(change_text) < 0)?'hidden':'visible');
+    function set_value(td_contain){
+        var search_object = {};
+        var self = this;
+        $.each(td_contain, function(key, element2){
+            var value_td = ($(element2).find("a").text()).trim();
+            if(value_td)search_object[key] = [value_td.toLowerCase(), element2];
+        });
+        return search_object;
+    };
+    $.each($("table#table_search"), function(key, element){
+        $.each($("tr#agenda_tr"),function(key, element1){
+            var th_child = $(element1).siblings("#agenda_th").children().length;
+            var tr_child = $(element1).children();
+            var td_contain = $(tr_child).filter("td#seach_enable");
+            if(th_child == tr_child.length && td_contain.length == 0){
+                $(element1).remove();
+            }
+        });
+        var search_object = set_value($(element).find("td#seach_enable"));
+        var element_search = $(element).prev().find("#start_search");
+        $(element_search).bind('keyup',function(e){
+            var change_text = ($(this).val()).toLowerCase();
+            $.each(search_object, function(key, value){
+                $(value[1]).css("visibility", (value[0].indexOf(change_text) < 0)?'hidden':'visible');
+            });
         });
     });
 });
