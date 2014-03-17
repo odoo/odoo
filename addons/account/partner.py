aac67e8bc528ea9c7bc771428bba952c984bf8ab
Gery Debongnie <ged@openerp.com>
2014-03-17 16:28:28 +0100

    [IMP] removes the testing field testpercent in res.partner and improves the way the buttons are displayed in form view (addon account)
    bzr revid: ged@openerp.com-20140317152828-v2den23s32kzaqxh

--- addons/account/partner.py
+++ addons/account/partner.py
@@ -21,7 +21,6 @@
 
 from operator import itemgetter
 import time
-import random
 
 from openerp.osv import fields, osv
 
@@ -163,21 +162,8 @@ class res_partner(osv.osv):
     def _debit_search(self, cr, uid, obj, name, args, context=None):
         return self._asset_difference_search(cr, uid, obj, name, 'payable', args, context=context)
 
-    def _invoices_stat_button(self, cr, uid, ids, field_name, arg, context=None):
-        res = {}
-        obj = self.pool.get('account.invoice')        
-        for partner in self.browse(cr, uid, ids, context):
-            domain = [('id', 'in', map(int, partner.invoice_ids))]
-            group_obj = obj.read_group(cr, uid, domain, ['amount_total'], [''], context=context)
-            total = group_obj[0]['amount_total'] or 0
-            res[partner.id] = """
-                    <div><strong>%s</strong> Invoices</div>
-                    <div>Total: %s</div>
-            """ % (len(partner.invoice_ids), total)
-        return res
-
-    def _test_percent(self, cr, uid, ids, field_name, arg, context=None):
-        return {partner.id: random.random()*100 for partner in self.browse(cr, uid, ids, context)}
+    def _invoice_count(self, cr, uid, ids, field_name, arg, context=None):
+        return {partner.id: len(partner.invoice_ids) for partner in self.browse(cr, uid, ids, context)}
 
     def _journal_item_count(self, cr, uid, ids, field_name, arg, context=None):
         return {partner.id: len(partner.journal_items_ids) for partner in self.browse(cr, uid, ids, context)}
@@ -210,8 +196,7 @@ class res_partner(osv.osv):
             fnct_search=_credit_search, string='Total Receivable', multi='dc', help="Total amount this customer owes you."),
         'debit': fields.function(_credit_debit_get, fnct_search=_debit_search, string='Total Payable', multi='dc', help="Total amount you have to pay to this supplier."),
         'debit_limit': fields.float('Payable Limit'),
-        'invoices_stat_button': fields.function(_invoices_stat_button, string="Invoices", type='html'),
-        'testpercent': fields.function(_test_percent, string="TestPercent", type='float'),
+        'invoice_count': fields.function(_invoice_count, string="Invoices", type='html'),
         'journal_items_ids': fields.one2many('account.move.line', 'partner_id', 'Journal Items'),
         'journal_item_count': fields.function(_journal_item_count, string="Journal Items", type="integer"),
         'property_account_payable': fields.property(
