93284177d8368262e381b107b2f9c669e55a782f
Thibault Delavallée <tde@openerp.com>
2014-03-17 18:16:34 +0100

    [ADD] Added module mass_mailing_crm, that adds the lead and opportunities as available for mass mailign campaigns.
    bzr revid: tde@openerp.com-20140317171634-4ucthtbaw542ealj

---
+++ addons/mass_mailing_crm/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Mass Mailing with CRM',
+    'version': '1.0',
+    'depends': ['mass_mailing', 'crm'],
+    'author': 'OpenERP SA',
+    'category': 'Hidden/Dependency',
+    'description': """
+Bridge module between Mass Mailing and CRM
+    """,
+    'website': 'http://www.openerp.com',
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+}
