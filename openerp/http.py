46cfd8940f7f3f0557335538892389751730ee6d
Fabien Meghazi <fme@openerp.com>
2014-03-19 09:54:54 +0100

    [FIX] doc typo
    bzr revid: fme@openerp.com-20140319085454-1pnjm55b4vxfm1wc

--- openerp/http.py
+++ openerp/http.py
@@ -1027,7 +1027,7 @@ class Root(object):
         return self.dispatch(environ, start_response)
 
     def load_addons(self):
-        """ Load all addons from addons patch containg static files and
+        """ Load all addons from addons path containing static files and
         controllers and configure them.  """
         statics = {}
 
