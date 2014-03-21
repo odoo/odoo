77cab2ac941afd9c971cdaebab65d84251faec41
Stefan Rijnhart <stefan@therp.nl>
2014-03-21 09:05:04 +0000

    Fix binary fields in one2many widgets:
    - Don't retrieve the binary contents just to display the size, but pass context with bin_size=True instead
    - Always pass filename in download link
    
    Combination of patches from the bug report from Enrico Ganzaroli, Cedric Le Brouster and the last commit from myself which actually prevents the preloading of the binaries.
    
    https://launchpad.net/bugs/1167429

--- addons/web/static/src/js/view_list.js
+++ addons/web/static/src/js/view_list.js
@@ -2305,13 +2305,9 @@ instance.web.list.Binary = instance.web.list.Column.extend({
         var text = _t("Download");
         var value = row_data[this.id].value;
         var download_url;
-        if (value && value.substr(0, 10).indexOf(' ') == -1) {
-            download_url = "data:application/octet-stream;base64," + value;
-        } else {
-            download_url = instance.session.url('/web/binary/saveas', {model: options.model, field: this.id, id: options.id});
-            if (this.filename) {
-                download_url += '&filename_field=' + this.filename;
-            }
+        download_url = instance.session.url('/web/binary/saveas', {model: options.model, field: this.id, id: options.id});
+        if (this.filename) {
+            download_url += '&filename_field=' + this.filename;
         }
         if (this.filename && row_data[this.filename]) {
             text = _.str.sprintf(_t("Download \"%s\""), instance.web.format_value(
