1185f7b8b51da1e217d32b9018185ea5b7e3eab1
Sunil Sharma (OpenERP) <sunilsharma.sharma07@gmail.com>
2014-03-21 11:16:42 +0530

    [imp]:improve gamification challenge line category forum and add clss gamification challenge assign reward user
    bzr revid: sunilsharma.sharma07@gmail.com-20140321054642-bjbgm4e92tur9f83

--- addons/website_forum/models/website_forum.py
+++ addons/website_forum/models/website_forum.py
@@ -353,3 +353,36 @@ class Tags(osv.Model):
             }
         ),
    }
+
+class gamification_challenge(osv.Model):
+    _inherit = 'gamification.challenge'
+
+    def _get_categories(self, cr, uid, context=None):
+        res = super(gamification_challenge, self)._get_categories(cr, uid,
+                context=context)
+        res.append(('forum', 'Website / Q.A'))
+        return res
+
+    _columns = {
+        'category': fields.selection(lambda s, *a, **k: s._get_categories(*a, **k),
+            string="Appears in", help="Define the visibility of the challenge through menus", required=True),
+    }
+    
+    def check_challenge_reward(self, cr, uid, ids, force=False, context=None):
+        if isinstance(ids, (int,long)):
+            ids = [ids]
+        context = context or {}
+        badge_user_obj = self.pool.get('gamification.badge.user');
+        for challenge in self.browse(cr, uid, ids, context=context):
+            if challenge.category == 'forum':
+                for user in challenge.user_ids:
+                    bages_user_ids = badge_user_obj.search(cr, uid, [('user_id', '=', user.id),('badge_id', '=', challenge.reward_id.id)],context=None)
+                    if not bages_user_ids:
+                        reached_goal_ids = self.pool.get('gamification.goal').search(cr, uid, [
+                            ('challenge_id', '=', challenge.id),
+                            ('user_id', '=', user.id),
+                            ('state', '=', 'reached'),
+                        ], context=context)
+                        if len(reached_goal_ids) == len(challenge.line_ids):
+                            super(gamification_challenge, self).check_challenge_reward(cr, uid, ids, force=False, context=context)
+                            self.reward_user(cr, uid, user.id, challenge.reward_id.id, context)
