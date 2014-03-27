29d85d27342060737cd8b23ae49d582385d34252
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2014-03-27 15:05:49 +0100

    [REN] renamed mrp_jit into procurement_jit and removed auto_install = True on this module
    bzr revid: qdp-launchpad@openerp.com-20140327140549-720uzzakwcsx4h0y

--- addons/procurement_jit/__init__.py
+++ addons/procurement_jit/__init__.py
@@ -19,6 +19,6 @@
 #
 ##############################################################################
 
-import mrp_jit
+import procurement_jit
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
