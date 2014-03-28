14f1c7836e9321d9085f271246480fbee4af62e5
Thibault Delavallée <tde@openerp.com>
2014-03-28 11:53:08 +0100

    [IMP] mass_mailing: when creating a new list, display customers / contacts / leads available for mass mailing using a default search fitler.
    bzr revid: tde@openerp.com-20140328105308-cp4n11lnd5ov8gef

--- addons/mass_mailing/wizard/create_list.py
+++ addons/mass_mailing/wizard/create_list.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import osv, fields
+from openerp.osv import osv
 from openerp.tools.translate import _
 
 
@@ -14,7 +14,7 @@ class MailingListWizard(osv.TransientModel):
     def action_new_list(self, cr, uid, ids, context=None):
         wizard = self.browse(cr, uid, ids[0], context=context)
         action_id = self.pool['mail.mass_mailing']._get_model_to_list_action_id(cr, uid, wizard.model, context=context)
-        ctx = dict(context, view_manager_highlight=[action_id], default_name=wizard.name, default_model=wizard.model)
+        ctx = dict(context, search_default_not_opt_out=True, view_manager_highlight=[action_id], default_name=wizard.name, default_model=wizard.model)
         return {
             'name': _('Choose Recipients'),
             'type': 'ir.actions.act_window',
