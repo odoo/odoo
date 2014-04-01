8c1585f5f8065e9ef3e8d8b1596d53fb077b8927
Josse Colpaert <jco@openerp.com>
2014-04-01 10:36:03 +0200

    [IMP] Comment point of sale report as reports will be replaced by qweb
    bzr revid: jco@openerp.com-20140401083603-t6jb4k32t53h3yuw

--- addons/point_of_sale/__openerp__.py
+++ addons/point_of_sale/__openerp__.py
@@ -65,7 +65,7 @@ Main Features
         'wizard/pos_payment.xml',
         'wizard/pos_box.xml',
         'wizard/pos_session_opening.xml',
-        'point_of_sale_report.xml',
+        #'point_of_sale_report.xml',
         'point_of_sale_view.xml',
         'point_of_sale_data.xml',
         'report/pos_order_report_view.xml',
