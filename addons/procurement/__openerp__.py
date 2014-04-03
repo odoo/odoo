63a6dc1d0f4f4a92499c67dea4aeb15c44eda8ae
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2014-04-03 10:00:38 +0200

    [FIX] procurement: added back the logistic group in res.company form view for other modules to add fields in it
    bzr revid: qdp-launchpad@openerp.com-20140403080038-qjgwt2l5p336oae4

--- addons/procurement/__openerp__.py
+++ addons/procurement/__openerp__.py
@@ -49,6 +49,7 @@ depending on the product's configuration.
         'procurement_data.xml',
         'wizard/schedulers_all_view.xml',
         'procurement_view.xml',
+        'company_view.xml',
     ],
     'demo': [],
     'test': ['test/procurement.yml'],
