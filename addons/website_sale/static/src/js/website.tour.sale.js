a06d9099ea07636fa838ceaf7e28415c67666ce7
Thibault Delavallée <tde@openerp.com>
2014-04-04 11:49:55 +0200

    [FIX] website_sale: fixed stupid test usign a title
    bzr revid: tde@openerp.com-20140404094955-51zhadhaavpjvkzl

--- addons/website_sale/static/src/js/website.tour.sale.js
+++ addons/website_sale/static/src/js/website.tour.sale.js
@@ -77,7 +77,7 @@
                 },
                 {
                     title:     "Pay Now",
-                    waitFor:   '#payment_method label:has(input:checked):has(img[title="transfer"])',
+                    waitFor:   '#payment_method label:has(input:checked):has(img[title="Wire Transfer"])',
                     element:   '.oe_sale_acquirer_button .btn[name="submit"]:visible',
                 },
                 {
