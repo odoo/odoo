5d4d42903436d492efa0e03cca6ad8fe098082d4
Thibault Delavallée <tde@openerp.com>
2014-04-09 13:54:39 +0200

    [CLEAN] website_forum: cleaned manifests before merging
    bzr revid: tde@openerp.com-20140409115439-jflj09sdvc3d19hv

--- addons/website_forum/__openerp__.py
+++ addons/website_forum/__openerp__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013-Today OpenERP SA (<http://www.openerp.com>).
+#    Copyright (C) 2014-Today OpenERP SA (<http://www.openerp.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -29,16 +28,21 @@
 Ask questions, get answers, no distractions
         """,
     'author': 'OpenERP SA',
-    'depends': ['website', 'website_mail', 'gamification', 'website_partner', 'auth_signup'],
+    'depends': [
+        'auth_signup',
+        'gamification',
+        'website_mail',
+        'website_partner'
+    ],
     'data': [
-         'data/forum_data.xml',
-         'views/website_forum.xml',
-         'security/ir.model.access.csv',
-         'security/website_forum.xml',
-         'data/forum_badges_data.xml',
+        'data/forum_data.xml',
+        'views/website_forum.xml',
+        'security/ir.model.access.csv',
+        'security/website_forum.xml',
+        'data/forum_badges_data.xml',
     ],
     'qweb': [
-         'static/src/xml/*.xml'
+        'static/src/xml/*.xml'
     ],
     'demo': [
         'data/forum_demo.xml',
