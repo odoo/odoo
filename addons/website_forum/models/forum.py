cbac424f915a99c0a08f338482e5ce1af4430f52
Thibault Delavallée <tde@openerp.com>
2014-04-10 15:39:28 +0200

    [REF] website_forum: renamed models (website.forum -> forum) + cleaned models. Added favorite_count function field, like vote_count and so on, to be able to have badges based on a simple model.
    bzr revid: tde@openerp.com-20140410133928-i0lncz8us9wueb13

--- addons/website_forum/models/forum.py
+++ addons/website_forum/models/forum.py
@@ -9,14 +9,14 @@ from openerp.tools.translate import _
 
 
 class Forum(osv.Model):
-    _name = 'website.forum'
+    _name = 'forum.forum'
     _description = 'Forums'
-    _inherit = ['mail.thread', 'website.seo.metadata']
+    _inherit = ['website.seo.metadata']
 
     _columns = {
         'name': fields.char('Name', required=True, translate=True),
         'faq': fields.html('Guidelines'),
-        'description': fields.text('Description'),
+        'description': fields.html('Description'),
     }
 
     def _get_default_faq(self, cr, uid, context=None):
@@ -38,23 +38,49 @@ class Forum(osv.Model):
 
 
 class Post(osv.Model):
-    _name = 'website.forum.post'
-    _description = "Question"
+    _name = 'forum.post'
+    _description = 'Forum Post'
     _inherit = ['mail.thread', 'website.seo.metadata']
+
+    def _get_user_vote(self, cr, uid, ids, field_name, arg, context):
+        res = dict.fromkeys(ids, 0)
+        vote_ids = self.pool['forum.post.vote'].search(cr, uid, [('post_id', 'in', ids), ('user_id', '=', uid)], context=context)
+        for vote in self.pool['forum.post.vote'].browse(cr, uid, vote_ids, context=context):
+            res[vote.post_id.id] = vote.vote
+        return res
 
     def _get_vote_count(self, cr, uid, ids, field_name, arg, context):
         res = dict.fromkeys(ids, 0)
         for post in self.browse(cr, uid, ids, context=context):
-            if post.vote_ids:
-                for vote in post.vote_ids:
-                    res[post.id] += int(vote.vote)
+            for vote in post.vote_ids:
+                res[post.id] += int(vote.vote)
         return res
 
-    def _get_vote(self, cr, uid, ids, context=None):
+    def _get_post_from_vote(self, cr, uid, ids, context=None):
         result = {}
-        for vote in self.pool['website.forum.post.vote'].browse(cr, uid, ids, context=context):
+        for vote in self.pool['forum.post.vote'].browse(cr, uid, ids, context=context):
             result[vote.post_id.id] = True
         return result.keys()
+
+    def _get_user_favourite(self, cr, uid, ids, field_name, arg, context):
+        res = dict.fromkeys(ids, False)
+        for post in self.browse(cr, uid, ids, context=context):
+            if uid in [f.user_id.id for f in post.favourite_ids]:
+                res[post.id] = True
+        return res
+
+    def _get_favorite_count(self, cr, uid, ids, field_name, arg, context):
+        res = dict.fromkeys(ids, 0)
+        for post in self.browse(cr, uid, ids, context=context):
+            res[post.id] += len(post.favorite_ids)
+        return res
+
+    def _get_post_from_hierarchy(self, cr, uid, ids, context=None):
+        post_ids = set(ids)
+        for post in self.browse(cr, SUPERUSER_ID, ids, context=context):
+            if post.parent_id:
+                post_ids.add(post.parent_id.id)
+        return list(post_ids)
 
     def _get_child_count(self, cr, uid, ids, field_name=False, arg={}, context=None):
         res = dict.fromkeys(ids, 0)
@@ -65,71 +91,62 @@ class Post(osv.Model):
                 res[post.id] = len(post.child_ids)
         return res
 
-    def _get_user_vote(self, cr, uid, ids, field_name, arg, context):
-        res = dict.fromkeys(ids, 0)
-        vote_ids = self.pool['website.forum.post.vote'].search(cr, uid, [('post_id', 'in', ids), ('user_id', '=', uid)], context=context)
-        for vote in self.pool['website.forum.post.vote'].browse(cr, uid, vote_ids, context=context):
-            res[vote.post_id.id] = vote.vote
-        return res
-
-    def _get_user_favourite(self, cr, uid, ids, field_name, arg, context):
-        res = dict.fromkeys(ids, False)
-        user = self.pool['res.users'].browse(cr, uid, uid, context=context)
-        for post in self.browse(cr, uid, ids, context=context):
-            if user in post.favourite_ids:
-                res[post.id] = True
-        return res
-
     _columns = {
         'name': fields.char('Title', size=128),
-        'forum_id': fields.many2one('website.forum', 'Forum', required=True),
-        'content': fields.text('Content'),
-        'create_date': fields.datetime('Asked on', select=True, readonly=True),
-        'user_id': fields.many2one('res.users', 'Asked by', select=True, readonly=True),
-        'write_date': fields.datetime('Update on', select=True, readonly=True),
-        'write_uid': fields.many2one('res.users', 'Update by', select=True, readonly=True),
-        'tag_ids': fields.many2many('website.forum.tag', 'forum_tag_rel', 'forum_id', 'forum_tag_id', 'Tag'),
-        'vote_ids': fields.one2many('website.forum.post.vote', 'post_id', 'Votes'),
-        'user_vote': fields.function(_get_user_vote, string="My Vote", type='integer'),
-        'favourite_ids': fields.many2many('res.users', 'forum_favourite_rel', 'forum_id', 'user_id', 'Favourite'),
-        'user_favourite': fields.function(_get_user_favourite, string="My Favourite", type='boolean'),
+        'forum_id': fields.many2one('forum.forum', 'Forum', required=True),
+        'content': fields.html('Content'),
+        'tag_ids': fields.many2many('forum.tag', 'forum_tag_rel', 'forum_id', 'forum_tag_id', 'Tags'),
         'state': fields.selection([('active', 'Active'), ('close', 'Close'), ('offensive', 'Offensive')], 'Status'),
-        'active': fields.boolean('Active'),
         'views': fields.integer('Number of Views'),
-        'parent_id': fields.many2one('website.forum.post', 'Question', ondelete='cascade'),
-        'child_ids': fields.one2many('website.forum.post', 'parent_id', 'Answers'),
-        'child_count': fields.function(
-            _get_child_count, string="Answers", type='integer',
-            store={
-                'website.forum.post': (lambda self, cr, uid, ids, c={}: ids, ['parent_id', 'child_ids'], 10),
-            }
-        ),
+        'active': fields.boolean('Active'),
+        'is_correct': fields.boolean('Valid Answer', help='Correct Answer/ Answer on this question accepted.'),
         'website_message_ids': fields.one2many(
             'mail.message', 'res_id',
             domain=lambda self: [
                 '&', ('model', '=', self._name), ('type', '=', 'comment')
             ],
-            string='Post Messages',
-            help="Comments on forum post",
+            string='Post Messages', help="Comments on forum post",
         ),
+        # history
+        'create_date': fields.datetime('Asked on', select=True, readonly=True),
+        'create_uid': fields.many2one('res.users', 'Created by', select=True, readonly=True),
+        'write_date': fields.datetime('Update on', select=True, readonly=True),
+        'write_uid': fields.many2one('res.users', 'Updated by', select=True, readonly=True),
+        # vote fields
+        'vote_ids': fields.one2many('forum.post.vote', 'post_id', 'Votes'),
+        'user_vote': fields.function(_get_user_vote, string='My Vote', type='integer'),
         'vote_count': fields.function(
             _get_vote_count, string="Votes", type='integer',
             store={
-                'website.forum.post': (lambda self, cr, uid, ids, c={}: ids, ['vote_ids'], 10),
-                'website.forum.post.vote': (_get_vote, [], 10),
-            }
-        ),
-        'is_correct': fields.boolean('Valid Answer', help='Correct Answer/ Answer on this question accepted.'),
-        'closed_reason_id': fields.many2one('website.forum.post.reason', 'Reason'),
-        'closed_by': fields.many2one('res.users', 'Closed by'),
+                'forum.post': (lambda self, cr, uid, ids, c={}: ids, ['vote_ids'], 10),
+                'forum.post.vote': (_get_post_from_vote, [], 10),
+            }),
+        # favorite fields
+        'favourite_ids': fields.many2many('res.users', 'Favourite'),
+        'user_favourite': fields.function(_get_user_favourite, string="My Favourite", type='boolean'),
+        'favorite_count': fields.function(
+            _get_favorite_count, string='Favorite Count', type='integer',
+            store={
+                'forum.post': (lambda self, cr, uid, ids, c={}: ids, ['favorite_ids'], 10),
+            }),
+        # hierarchy
+        'parent_id': fields.many2one('forum.post', 'Question', ondelete='cascade'),
+        'child_ids': fields.one2many('forum.post', 'parent_id', 'Answers'),
+        'child_count': fields.function(
+            _get_child_count, string="Answers", type='integer',
+            store={
+                'forum.post': (_get_post_from_hierarchy, ['parent_id', 'child_ids'], 10),
+            }),
+        # closing
+        'closed_reason_id': fields.many2one('forum.post.reason', 'Reason'),
+        'closed_uid': fields.many2one('res.users', 'Closed by', select=1),
         'closed_date': fields.datetime('Closed on', readonly=True),
     }
 
     _defaults = {
-        'user_id': lambda self, cr, uid, ctx=None: uid,
+        'create_uid': lambda self, cr, uid, ctx=None: uid,
         'state': 'active',
         'views': 0,
-        'vote_count': 0,
         'active': True,
     }
 
@@ -172,13 +189,13 @@ class Post(osv.Model):
         return res
 
     def vote(self, cr, uid, ids, upvote=True, context=None):
-        Vote = self.pool['website.forum.post.vote']
+        Vote = self.pool['forum.post.vote']
         user = self.pool['res.users'].browse(cr, uid, uid, context=context)
         # must have at least 10 karma to vote
         if not upvote and user.karma <= 10:
             return {'error': 'lessthen_10_karma'}
         # user can not vote on own post
-        posts = self.pool['website.forum.post'].browse(cr, uid, ids, context=context)
+        posts = self.browse(cr, uid, ids, context=context)
         if any(post.user_id.id == uid for post in posts):
             return {'error': 'own_post'}
 
@@ -203,18 +220,18 @@ class Post(osv.Model):
 
 
 class PostReason(osv.Model):
-    _name = "website.forum.post.reason"
-    _description = "Post Reason"
+    _name = "forum.post.reason"
+    _description = "Post Closing Reason"
     _columns = {
-        'name': fields.char('Post Reason'),
+        'name': fields.char('Post Reason', required=True),
     }
 
 
 class Vote(osv.Model):
-    _name = 'website.forum.post.vote'
+    _name = 'forum.post.vote'
     _description = 'Vote'
     _columns = {
-        'post_id': fields.many2one('website.forum.post', 'Post', ondelete='cascade', required=True),
+        'post_id': fields.many2one('forum.post', 'Post', ondelete='cascade', required=True),
         'user_id': fields.many2one('res.users', 'User'),
         'vote': fields.selection([('1', '1'), ('-1', '-1'), ('0', '0')], 'Vote', required=True),
         'create_date': fields.datetime('Create Date', select=True, readonly=True),
@@ -234,8 +251,6 @@ class Vote(osv.Model):
     def create(self, cr, uid, vals, context=None):
         vote_id = super(Vote, self).create(cr, uid, vals, context=context)
         self.update_karma(cr, uid, [vote_id], new_vote=vals.get('vote', '1'), context=context)
-        # body = "voted %s %s" % ('answer' if record.parent_id else 'question','up' if vals.get('vote')==1 else 'down')
-        # Post.message_post(cr, uid, [record.id], body=_(body), context=context)
         return vote_id
 
     def write(self, cr, uid, ids, values, context=None):
@@ -247,25 +262,26 @@ class Vote(osv.Model):
 
 
 class Tags(osv.Model):
-    _name = "website.forum.tag"
+    _name = "forum.tag"
     _description = "Tag"
     _inherit = ['website.seo.metadata']
 
     def _get_posts_count(self, cr, uid, ids, field_name, arg, context=None):
-        return dict((tag_id, self.pool['website.forum.post'].search_count(cr, uid, [('tag_ids', 'in', tag_id)], context=context)) for tag_id in ids)
+        return dict((tag_id, self.pool['forum.post'].search_count(cr, uid, [('tag_ids', 'in', tag_id)], context=context)) for tag_id in ids)
 
     def _get_tag_from_post(self, cr, uid, ids, context=None):
         return list(set(
-            [tag.id for post in self.pool['website.forum.post'].browse(cr, SUPERUSER_ID, ids, context=context) for tag in post.tag_ids]
+            [tag.id for post in self.pool['forum.post'].browse(cr, SUPERUSER_ID, ids, context=context) for tag in post.tag_ids]
         ))
 
     _columns = {
-        'name': fields.char('Name', size=64, required=True),
-        'forum_id': fields.many2one('website.forum', 'Forum', required=True),
+        'name': fields.char('Name', required=True),
+        'forum_id': fields.many2one('forum.forum', 'Forum', required=True),
+        'post_ids': fields.many2many('forum.post', 'forum_tag_rel', 'tag_id', 'post_id', 'Posts'),
         'posts_count': fields.function(
-            _get_posts_count, type='integer', string="# of Posts",
+            _get_posts_count, type='integer', string="Number of Posts",
             store={
-                'website.forum.post': (_get_tag_from_post, ['tag_ids'], 10),
+                'forum.post': (_get_tag_from_post, ['tag_ids'], 10),
             }
         ),
     }
