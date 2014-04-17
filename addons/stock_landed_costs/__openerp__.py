b9a1104e48e954622527b1a5a81a0a692fb2b442
DJ Patel <mdi@tinyerp.com>
2014-04-17 14:28:27 +0530

    [IMP] stock_landed_cost : Changed the 'auto_install' to False.
    bzr revid: mdi@tinyerp.com-20140417085827-rg5wwf7u8owczqrt

--- addons/stock_landed_costs/__openerp__.py
+++ addons/stock_landed_costs/__openerp__.py
@@ -44,7 +44,7 @@
     ],
     'installable': True,
     'application': True,
-    'auto_install': True,
+    'auto_install': False,
 }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
