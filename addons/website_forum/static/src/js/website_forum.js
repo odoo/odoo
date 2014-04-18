018624ea43cf003e1639c1d91b4f81558bb6fe55
Thibault Delavallée <tde@openerp.com>
2014-04-18 17:16:08 +0200

    [IMP] website_forum: better error messages about karma
    bzr revid: tde@openerp.com-20140418151608-0pb3ui97aksb9en1

--- addons/website_forum/static/src/js/website_forum.js
+++ addons/website_forum/static/src/js/website_forum.js
@@ -18,9 +18,9 @@ $(document).ready(function () {
                             '</div>');
                     }
                     else if (data['error'] == 'not_enough_karma') {
-                        var $warning = $('<div class="alert alert-danger alert-dismissable" id="vote_alert" style="position:absolute; margin-top: -30px; margin-left: 90px;">'+
+                        var $warning = $('<div class="alert alert-danger alert-dismissable" id="vote_alert" style="max-width: 500px; position:absolute; margin-top: -30px; margin-left: 90px;">'+
                             '<button type="button" class="close notification_close" data-dismiss="alert" aria-hidden="true">&times;</button>'+
-                            'Sorry, at least ' + data['karma'] + ' karma is required to vote'+
+                            'Sorry, at least ' + data['karma'] + ' karma is required to vote. You can gain karma by answering questions and receiving votes.'+
                             '</div>');
                     }
                     vote_alert = $link.parent().find("#vote_alert");
@@ -60,9 +60,9 @@ $(document).ready(function () {
                         'Sorry, only the user who asked this question can accept the answer as correct.'+
                         '</div>');
                 } else if (data['error'] == 'not_enough_karma') {
-                    var $warning = $('<div class="alert alert-danger alert-dismissable" id="vote_alert" style="position:absolute; margin-top: -30px; margin-left: 90px;">'+
+                    var $warning = $('<div class="alert alert-danger alert-dismissable" id="vote_alert" style="max-width: 500px; position:absolute; margin-top: -30px; margin-left: 90px;">'+
                         '<button type="button" class="close notification_close" data-dismiss="alert" aria-hidden="true">&times;</button>'+
-                        'Sorry, at least ' + data['karma'] + ' karma is required to accept your own answers'+
+                        'Sorry, at least ' + data['karma'] + ' karma is required to accept your own answers. You can gain karma by answering questions and receiving votes.'+
                         '</div>');
                 }
                 correct_answer_alert = $link.parent().find("#correct_answer_alert");
