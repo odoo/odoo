a2b3b47b38bd67007ec88a9d7a375fcf9091b4e8
Christophe Simonis <chs@openerp.com>
2014-04-22 14:35:33 +0200

    [FIX] cli mode: initialisation of the addons path do not setup the logger (as we only parse the addons path, we ends with the default logging config)
    bzr revid: chs@openerp.com-20140422123533-b6lcm76ydf8vpw9u

--- openerp/cli/__init__.py
+++ openerp/cli/__init__.py
@@ -38,7 +38,8 @@ def main():
     # The only shared option is '--addons-path=' needed to discover additional
     # commands from modules
     if len(args) > 1 and args[0].startswith('--addons-path=') and not args[1].startswith("-"):
-        tools.config.parse_config([args[0]])
+        # parse only the addons-path, do not setup the logger...
+        tools.config._parse_config([args[0]])
         args = args[1:]
 
     # Default legacy command
