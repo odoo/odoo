873e65edfaf112b327fb7ffb4f44b1c64f72d00f
Quentin (OpenERP) <qdp-launchpad@openerp.com>
2014-04-24 11:17:15 +0200

    [IMP] stock_account: better error message
    bzr revid: qdp-launchpad@openerp.com-20140424091715-xykdsi2dxacm4ybh

--- addons/stock_account/stock_account.py
+++ addons/stock_account/stock_account.py
@@ -180,11 +180,12 @@ class stock_quant(osv.osv):
 
         if not all([acc_src, acc_dest, acc_valuation, journal_id]):
             raise osv.except_osv(_('Error!'), _('''One of the following information is missing on the product or product category and prevents the accounting valuation entries to be created:
+    Product: %s
     Stock Input Account: %s
     Stock Output Account: %s
     Stock Valuation Account: %s
     Stock Journal: %s
-    ''') % (acc_src, acc_dest, acc_valuation, journal_id))
+    ''') % (move.product_id.name, acc_src, acc_dest, acc_valuation, journal_id))
         return journal_id, acc_src, acc_dest, acc_valuation
 
     def _prepare_account_move_line(self, cr, uid, move, qty, cost, credit_account_id, debit_account_id, context=None):
