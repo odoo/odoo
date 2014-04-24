aca0ad4b8faa45c65e26cf545ca3139a62b46c64
Fabien Meghazi <fme@openerp.com>
2014-04-24 23:24:15 +0200

    [FIX] crm_partner_assign missing js file
    bzr revid: fme@openerp.com-20140424212415-s843tzw826a992n9

--- addons/crm_partner_assign/__openerp__.py
+++ addons/crm_partner_assign/__openerp__.py
@@ -46,7 +46,6 @@ to an appropriate local partner, based on the distance and the weight that was a
         'portal_data.xml',
         'report/crm_lead_report_view.xml',
         'report/crm_partner_report_view.xml',
-        'views/crm_partner_assign.xml',
     ],
     'demo': [
         'res_partner_demo.xml',
