0efb6fc28bad228c673b24f2c690d4b69a8c75d2
Fabien Meghazi <fme@openerp.com>
2014-04-25 10:35:21 +0200

    [FIX] Inverse logic for /page/website.<id> shortcut
    bzr revid: fme@openerp.com-20140425083521-8rd1u3y4da8sf89i

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -6,6 +6,7 @@ import json
 import logging
 import os
 import datetime
+import re
 
 from sys import maxint
 
@@ -101,7 +102,9 @@ class Website(openerp.addons.web.controllers.main.Home):
                     'url': "/page/" + xml_id,
                     'parent_id': id,
                 }, context=request.context)
-        url = "/page/" + xml_id
+        # Reverse action in order to allow shortcut for /page/<website_xml_id>
+        url = "/page/" + re.sub(r"^website\.", '', xml_id)
+
         if noredirect:
             return werkzeug.wrappers.Response(url, mimetype='text/plain')
         return werkzeug.utils.redirect(url)
