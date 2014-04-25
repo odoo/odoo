12eb1573973d2352a0b26c685ac2571de4387595
Denis Ledoux <dle@openerp.com>
2014-04-25 14:55:07 +0200

    [FIX] product: price computation failed when for pricelists based on Cost price for non-employee users
    bzr revid: dle@openerp.com-20140425125507-njfyl1r6wn11vqwx

--- addons/product/product.py
+++ addons/product/product.py
@@ -24,7 +24,7 @@ import re
 
 from _common import ceiling
 
-from openerp import tools
+from openerp import tools, SUPERUSER_ID
 from openerp.osv import osv, fields
 from openerp.tools.translate import _
 
@@ -703,7 +703,7 @@ class product_product(osv.osv):
 
         res = {}
         product_uom_obj = self.pool.get('product.uom')
-        for product in self.browse(cr, uid, ids, context=context):
+        for product in self.browse(cr, SUPERUSER_ID, ids, context=context):
             res[product.id] = product[ptype] or 0.0
             if ptype == 'list_price':
                 res[product.id] = (res[product.id] * (product.price_margin or 1.0)) + \
