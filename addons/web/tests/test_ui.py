e82a652a7c3dfcf2b606b989573a6cdeda0f634f
Fabien Meghazi <fme@openerp.com>
2014-04-28 14:24:23 +0200

    [DAMN] !
    bzr revid: fme@openerp.com-20140428122423-pbo2pnm5ecqwibnf

--- addons/web/tests/test_ui.py
+++ addons/web/tests/test_ui.py
@@ -10,11 +10,7 @@ class TestUi(openerp.tests.HttpCase):
         self.phantom_jsfile(os.path.join(os.path.dirname(__file__), 'test_ui_load.js'))
     def test_03_js_public(self):
         self.phantom_js('/',"console.log('ok')","console")
-
-@openerp.tests.common.at_install(False)
-@openerp.tests.common.post_install(True)
-class TestUiPostload(openerp.tests.HttpCase):
-    def test_01_js_admin(self):
-        self.phantom_js('/web',"console.log('ok')","openerp.client.action_manager.inner_widget.views.form", login='admin')
+    # def test_04_js_admin(self):
+    #     self.phantom_js('/web',"console.log('ok')","openerp.client.action_manager.inner_widget.views.form", login='admin')
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
