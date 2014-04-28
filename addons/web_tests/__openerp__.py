d9eec7345b20bda8e8e996d09d03966ac630b065
Antony Lesuisse <al@openerp.com>
2014-04-28 17:28:57 +0200

    convert js tests to HttpCase
    bzr revid: al@openerp.com-20140428152857-h7z61vdw5crezv2u

--- addons/web_tests/__openerp__.py
+++ addons/web_tests/__openerp__.py
@@ -7,7 +7,7 @@ OpenERP Web test suite.
 
 """,
     'version': '2.0',
-    'depends': [],
+    'depends': ['web', 'web_kanban'],
     'js': ['static/src/js/*.js'],
     'css': ['static/src/css/*.css'],
     'auto_install': True,
