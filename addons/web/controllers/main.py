5600a79007565d364f878ef18de5476cd7033db8
Mohammed Shekha <msh@tinyerp.com>
2014-05-02 18:53:49 +0530

    [IMP]Improved the database manager, instead of calling html_template, created separate html file which is rendered using jinja2, so that we don't need html_template once pos and edit support render_bootstrap_template.
    bzr revid: msh@tinyerp.com-20140502132349-bw9zdyaou4yf8o7q

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -745,75 +745,9 @@ class Database(http.Controller):
     def manager(self, **kw):
         # TODO: migrate the webclient's database manager to server side views
         request.session.logout()
-        css = """
-            <link href="/web/static/lib/fontawesome/css/font-awesome.css" rel="stylesheet"/>
-            <link href="/web/static/lib/cleditor/jquery.cleditor.css" rel="stylesheet"/>
-            <link href="/web/static/lib/jquery.textext/jquery.textext.css" rel="stylesheet"/>
-            <link href="/web/static/lib/select2/select2.css" rel="stylesheet"/>
-            <link href="/web/static/lib/jquery.ui.bootstrap/css/custom-theme/jquery-ui-1.9.0.custom.css" rel="stylesheet"/>
-            <link href="/web/static/lib/jquery.ui.timepicker/css/jquery-ui-timepicker-addon.css" rel="stylesheet"/>
-            <link href="/web/static/lib/jquery.ui.notify/css/ui.notify.css" rel="stylesheet"/>
-            <link href="/web/static/lib/bootstrap/css/bootstrap.css" rel="stylesheet"/>
-            <link href="/web/static/src/css/base.css" rel="stylesheet"/>
-            <link href="/web/static/src/css/data_export.css" rel="stylesheet"/>
-            <link href="/base/static/src/css/modules.css" rel="stylesheet"/>"""
-        js = """
-            <script src="/web/static/lib/es5-shim/es5-shim.min.js" type="text/javascript"></script>
-            <script src="/web/static/lib/underscore/underscore.js" type="text/javascript"></script>
-            <script src="/web/static/lib/underscore.string/lib/underscore.string.js" type="text/javascript"></script>
-            <script src="/web/static/lib/datejs/globalization/en-US.js" type="text/javascript"></script>
-            <script src="/web/static/lib/spinjs/spin.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery/jquery.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.blockUI/jquery.blockUI.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.hotkeys/jquery.hotkeys.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.placeholder/jquery.placeholder.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.timeago/jquery.timeago.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.form/jquery.form.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.ba-bbq/jquery.ba-bbq.js" type="text/javascript"></script>
-            <script src="/web/static/lib/datejs/core.js" type="text/javascript"></script>
-            <script src="/web/static/lib/datejs/parser.js" type="text/javascript"></script>
-            <script src="/web/static/lib/datejs/sugarpak.js" type="text/javascript"></script>
-            <script src="/web/static/lib/datejs/extras.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.validate/jquery.validate.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.autosize/jquery.autosize.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.scrollTo/jquery.scrollTo-min.js" type="text/javascript"></script>
-            <script src="/web/static/lib/cleditor/jquery.cleditor.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.textext/jquery.textext.js" type="text/javascript"></script>
-            <script src="/web/static/lib/select2/select2.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.ui/js/jquery-ui-1.9.1.custom.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.ui.timepicker/js/jquery-ui-timepicker-addon.js" type="text/javascript"></script>
-            <script src="/web/static/lib/jquery.ui.notify/js/jquery.notify.js" type="text/javascript"></script>
-            <script src="/web/static/lib/bootstrap/js/bootstrap.js" type="text/javascript"></script>
-            <script src="/web/static/lib/backbone/backbone.js" type="text/javascript"></script>
-            <script src="/web/static/lib/qweb/qweb2.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/openerpframework.js" type="text/javascript"></script>
-            <script src="/web/static/lib/py.js/lib/py.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/boot.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/testing.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/pyeval.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/core.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/formats.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/chrome.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/views.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/data.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/data_export.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/search.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/view_list.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/view_form.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/view_list_editable.js" type="text/javascript"></script>
-            <script src="/web/static/src/js/view_tree.js" type="text/javascript"></script>
-            <script src="/base/static/src/js/apps.js" type="text/javascript"></script>"""
-
-        r = html_template % {
-            'js': js,
-            'css': css,
+        return env.get_template("database_manager.html").render({
             'modules': simplejson.dumps(module_boot()),
-            'init': """
-                var wc = new s.web.WebClient(null, { action: 'database_manager' });
-                wc.appendTo($(document.body));
-            """
-        }
-        return r
+        })
 
     @http.route('/web/database/get_list', type='json', auth="none")
     def get_list(self):
