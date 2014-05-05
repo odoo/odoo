06c63594bec9d64042c157c084e60ee43ce18bbb
chm@openerp.com <>
2014-05-05 11:34:23 +0200

    [FIX] website_sale: message confirmation
    bzr revid: chm@openerp.com-20140505093423-nrmou02ov1txod3k

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -579,7 +579,8 @@ class website_sale(http.Controller):
                 message = '<p>The payment seems to have been canceled.</p>'
             elif state == 'pending' and tx.acquirer_id.validation == 'manual':
                 message = '<p>Your transaction is waiting confirmation.</p>'
-                message += tx.acquirer_id.post_msg
+                if tx.acquirer_id.post_msg:
+                    message += tx.acquirer_id.post_msg
             else:
                 message = '<p>Your transaction is waiting confirmation.</p>'
             validation = tx.acquirer_id.validation
