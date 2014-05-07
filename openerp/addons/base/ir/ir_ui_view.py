cea55917424b7500b97ef2db09f19a636441708a
Fabien Meghazi <fme@openerp.com>
2014-05-07 09:43:10 +0200

    [FIX] runbot color
    bzr revid: fme@openerp.com-20140507074310-bt0r1vt6rsgy9wqe

--- openerp/addons/base/ir/ir_ui_view.py
+++ openerp/addons/base/ir/ir_ui_view.py
@@ -885,7 +885,7 @@ class view(osv.osv):
         # TODO: remove this as soon as the following branch is merged
         #       lp:~openerp-dev/openerp-web/trunk-module-closure-style-msh
         from openerp.addons.web.controllers.main import module_boot
-        qcontext['modules'] = simplejson.dumps(module_boot())
+        qcontext['modules'] = simplejson.dumps(module_boot()) if request else None
 
         def loader(name):
             return self.read_template(cr, uid, name, context=context)
