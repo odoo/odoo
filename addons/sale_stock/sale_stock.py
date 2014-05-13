11a9c2d409411029c9a69b38fe22d6091f189847
Anael Closson <acl@openerp.com>
2014-05-13 14:00:06 +0200

    [FIX] sale_stock: new product uom is done twice in sale order line on change - opw 607150 [FIX] sale_stock: comparing stock and sale order line qty is always done with default uom rounding precision

--- addons/sale_stock/sale_stock.py
+++ addons/sale_stock/sale_stock.py
@@ -610,7 +610,7 @@ class sale_order_line(osv.osv):
         res_packing = self.product_packaging_change(cr, uid, ids, pricelist, product, qty, uom, partner_id, packaging, context=context)
         res['value'].update(res_packing.get('value', {}))
         warning_msgs = res_packing.get('warning') and res_packing['warning']['message'] or ''
-        compare_qty = float_compare(product_obj.virtual_available * uom2.factor, qty * product_obj.uom_id.factor, precision_rounding=product_obj.uom_id.rounding)
+        compare_qty = float_compare(product_obj.virtual_available, qty, precision_rounding=uom2.rounding)
         if (product_obj.type=='product') and int(compare_qty) == -1 \
           and (product_obj.procure_method=='make_to_stock'):
             warn_msg = _('You plan to sell %.2f %s but you only have %.2f %s available !\nThe real stock is %.2f %s. (without reservations)') % \
