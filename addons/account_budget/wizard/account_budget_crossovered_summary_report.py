88d7ebc3829f65995d156cdd07bd00786acc2281
Romain Deheele <romain.deheele@camptocamp.com>
2014-05-16 16:58:34 +0200

    fix #11: fix spelling of model name in account_budget wizard

--- addons/account_budget/wizard/account_budget_crossovered_summary_report.py
+++ addons/account_budget/wizard/account_budget_crossovered_summary_report.py
@@ -44,7 +44,7 @@ class account_budget_crossvered_summary_report(osv.osv_memory):
         data = self.read(cr, uid, ids, context=context)[0]
         datas = {
             'ids': context.get('active_ids',[]),
-            'model': 'crossovered.budge',
+            'model': 'crossovered.budget',
             'form': data
         }
         datas['form']['ids'] = datas['ids']
