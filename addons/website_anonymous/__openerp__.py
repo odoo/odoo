8c8b500909cbeae91e966bfa0b4b20a2a9d2ff96
Dharmraj Jhala <dja@openerp.com>
2014-05-21 15:59:02 +0530

    [ADD] added instantclick library to load data faster in anonymous mode

---
+++ addons/website_anonymous/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Website Anonymous',
+    'category': 'Website',
+    'summary': 'Anonymous mode on website',
+    'version': '1.0',
+    'description': """
+OpenERP Website CMS Anonymous Mode
+==================================
+
+        """,
+    'author': 'OpenERP SA',
+    'depends': ['website'],
+    'installable': True,
+    'data': [
+        'views/website_anonymoustemplate.xml',
+    ],
+    
+}
