0ceee2a1209b789c9cfcd9f6c8b9da50be425dca
Alexandre Fayolle <afayolle.ml@free.fr>
2014-06-06 14:47:33 +0200

    [IMP] add an index on account_move_line(date, id)
    fixes https://launchpad.net/bugs/1311004

--- addons/account/account_move_line.py
+++ addons/account/account_move_line.py
@@ -566,6 +566,9 @@ class account_move_line(osv.osv):
         cr.execute('SELECT indexname FROM pg_indexes WHERE indexname = \'account_move_line_journal_id_period_id_index\'')
         if not cr.fetchone():
             cr.execute('CREATE INDEX account_move_line_journal_id_period_id_index ON account_move_line (journal_id, period_id)')
+        cr.execute('SELECT indexname FROM pg_indexes WHERE indexname = %s', ('account_move_line_date_id_index',))
+        if not cr.fetchone():
+            cr.execute('CREATE INDEX account_move_line_date_id_index ON account_move_line (date DESC, id desc)')
         return res
 
     def _check_no_view(self, cr, uid, ids, context=None):
