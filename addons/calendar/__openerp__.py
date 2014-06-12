f33e0406b71392d9e57e39cf4eedf22301f85171
Stéphane Wirtel <stephane@wirtel.be>
2014-06-12 15:08:33 +0200

    [IMP] Optimize the schedulers of the calendar, fetchmail and base_action_rules modules
    Disable the the crons when they'd have nothing to do, re-enable them when a
    task is (probably) introduced

--- addons/calendar/__openerp__.py
+++ addons/calendar/__openerp__.py
@@ -40,6 +40,7 @@ If you need to manage your meetings, you should install the CRM module.
     'website': 'http://www.openerp.com',
     'demo': ['calendar_demo.xml'],
     'data': [
+        'calendar_cron.xml',
         'security/calendar_security.xml',
         'security/ir.model.access.csv',
         'calendar_view.xml',
