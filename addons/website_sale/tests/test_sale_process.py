e915f4ca4d4baf41b3ad4e98e023ababffa21d0f
Christophe Matthieu <chm@odoo.com>
2014-06-13 18:32:56 +0200

    [FIX] website_sale: tour test customize

--- addons/website_sale/tests/test_sale_process.py
+++ addons/website_sale/tests/test_sale_process.py
@@ -12,9 +12,9 @@ inject = [
 class TestUi(openerp.tests.HttpCase):
     def test_01_admin_shop_tour(self):
         self.phantom_js("/", "openerp.website.Tour.run('shop', 'test')", "openerp.website.Tour.tours.shop", login="admin")
+        self.phantom_js("/", "openerp.website.Tour.run('shop_customize', 'test')", "openerp.website.Tour.tours.shop_customize", login="admin", inject=inject)
 
     def test_02_admin_checkout(self):
-        self.phantom_js("/", "openerp.website.Tour.run('shop_customize', 'test')", "openerp.website.Tour.tours.shop_customize", login="admin", inject=inject)
         self.phantom_js("/", "openerp.website.Tour.run('shop_buy_product', 'test')", "openerp.website.Tour.tours.shop_buy_product", login="admin", inject=inject)
 
     def test_03_demo_checkout(self):
