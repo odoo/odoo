6ebf08f295f35c4613cfaaab0e79ea813503db7c
julioserna <julio@vauxoo.com>
2014-06-24 19:38:56 -0500

    test

--- addons/account/__openerp__.py
+++ addons/account/__openerp__.py
@@ -43,7 +43,7 @@ Creates a dashboard for accountants that includes:
     * List of Customer Invoice to Approve
     * Company Analysis
     * Graph of Treasury
-
+test
 The processes like maintaining of general ledger is done through the defined financial Journals (entry move line orgrouping is maintained through journal) 
 for a particular financial year and for preparation of vouchers there is a module named account_voucher.
     """,
