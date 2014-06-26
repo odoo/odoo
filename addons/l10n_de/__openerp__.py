45beb95e0664801cdea2c3d11ef74b3d92a14d16
root <root@localhost>
2014-06-26 10:15:43 +0200

    Update for german accounting

--- addons/l10n_de/__openerp__.py
+++ addons/l10n_de/__openerp__.py
@@ -19,121 +19,35 @@
 #
 ##############################################################################
 
-# SKR03
-# =====
-
-# Dieses Modul bietet Ihnen einen deutschen Kontenplan basierend auf dem SKR03.
-# Gemäss der aktuellen Einstellungen ist die Firma nicht Umsatzsteuerpflichtig.
-# Diese Grundeinstellung ist sehr einfach zu ändern und bedarf in der Regel
-# grundsätzlich eine initiale Zuweisung von Steuerkonten zu Produkten und / oder
-# Sachkonten oder zu Partnern.
-# Die Umsatzsteuern (voller Steuersatz, reduzierte Steuer und steuerfrei)
-# sollten bei den Produktstammdaten hinterlegt werden (in Abhängigkeit der
-# Steuervorschriften). Die Zuordnung erfolgt auf dem Aktenreiter Finanzbuchhaltung
-# (Kategorie: Umsatzsteuer).
-# Die Vorsteuern (voller Steuersatz, reduzierte Steuer und steuerfrei)
-# sollten ebenso bei den Produktstammdaten hinterlegt werden (in Abhängigkeit
-# der Steuervorschriften). Die Zuordnung erfolgt auf dem Aktenreiter
-# Finanzbuchhaltung (Kategorie: Vorsteuer).
-# Die Zuordnung der Steuern für Ein- und Ausfuhren aus EU Ländern, sowie auch
-# für den Ein- und Verkauf aus und in Drittländer sollten beim Partner
-# (Lieferant/Kunde)hinterlegt werden (in Anhängigkeit vom Herkunftsland
-# des Lieferanten/Kunden). Die Zuordnung beim Kunden ist 'höherwertig' als
-# die Zuordnung bei Produkten und überschreibt diese im Einzelfall.
-#
-# Zur Vereinfachung der Steuerausweise und Buchung bei Auslandsgeschäften
-# erlaubt OpenERP ein generelles Mapping von Steuerausweis und Steuerkonten
-# (z.B. Zuordnung 'Umsatzsteuer 19%' zu 'steuerfreie Einfuhren aus der EU')
-# zwecks Zuordnung dieses Mappings zum ausländischen Partner (Kunde/Lieferant).
-
-# Die Rechnungsbuchung beim Einkauf bewirkt folgendes:
-# Die Steuerbemessungsgrundlage (exklusive Steuer) wird ausgewiesen bei den
-# jeweiligen Kategorien für den Vorsteuer Steuermessbetrag (z.B. Vorsteuer
-# Steuermessbetrag Voller Steuersatz 19%).
-# Der Steuerbetrag erscheint unter der Kategorie 'Vorsteuern' (z.B. Vorsteuer
-# 19%). Durch multidimensionale Hierachien können verschiedene Positionen
-# zusammengefasst werden und dann in Form eines Reports ausgegeben werden.
-#
-# Die Rechnungsbuchung beim Verkauf bewirkt folgendes:
-# Die Steuerbemessungsgrundlage (exklusive Steuer) wird ausgewiesen bei den
-# jeweiligen Kategorien für den Umsatzsteuer Steuermessbetrag
-# (z.B. Umsatzsteuer Steuermessbetrag Voller Steuersatz 19%).
-# Der Steuerbetrag erscheint unter der Kategorie 'Umsatzsteuer'
-# (z.B. Umsatzsteuer 19%). Durch multidimensionale Hierachien können
-# verschiedene Positionen zusammengefasst werden.
-# Die zugewiesenen Steuerausweise können auf Ebene der einzelnen
-# Rechnung (Eingangs- und Ausgangsrechnung) nachvollzogen werden,
-# und dort gegebenenfalls angepasst werden.
-
-# Rechnungsgutschriften führen zu einer Korrektur (Gegenposition)
-# der Steuerbuchung, in Form einer spiegelbildlichen Buchung.
-
-# SKR04
-# =====
-
-# Dieses Modul bietet Ihnen einen deutschen Kontenplan basierend auf dem SKR04.
-# Gemäss der aktuellen Einstellungen ist die Firma nicht Umsatzsteuerpflichtig,
-# d.h. im Standard existiert keine Zuordnung von Produkten und Sachkonten zu
-# Steuerschlüsseln.
-# Diese Grundeinstellung ist sehr einfach zu ändern und bedarf in der Regel
-# grundsätzlich eine initiale Zuweisung von Steuerschlüsseln zu Produkten und / oder
-# Sachkonten oder zu Partnern.
-# Die Umsatzsteuern (voller Steuersatz, reduzierte Steuer und steuerfrei)
-# sollten bei den Produktstammdaten hinterlegt werden (in Abhängigkeit der
-# Steuervorschriften). Die Zuordnung erfolgt auf dem Aktenreiter Finanzbuchhaltung
-# (Kategorie: Umsatzsteuer).
-# Die Vorsteuern (voller Steuersatz, reduzierte Steuer und steuerfrei)
-# sollten ebenso bei den Produktstammdaten hinterlegt werden (in Abhängigkeit
-# der Steuervorschriften). Die Zuordnung erfolgt auf dem Aktenreiter
-# Finanzbuchhaltung (Kategorie: Vorsteuer).
-# Die Zuordnung der Steuern für Ein- und Ausfuhren aus EU Ländern, sowie auch
-# für den Ein- und Verkauf aus und in Drittländer sollten beim Partner
-# (Lieferant/Kunde) hinterlegt werden (in Anhängigkeit vom Herkunftsland
-# des Lieferanten/Kunden). Die Zuordnung beim Kunden ist 'höherwertig' als
-# die Zuordnung bei Produkten und überschreibt diese im Einzelfall.
-#
-# Zur Vereinfachung der Steuerausweise und Buchung bei Auslandsgeschäften
-# erlaubt OpenERP ein generelles Mapping von Steuerausweis und Steuerkonten
-# (z.B. Zuordnung 'Umsatzsteuer 19%' zu 'steuerfreie Einfuhren aus der EU')
-# zwecks Zuordnung dieses Mappings zum ausländischen Partner (Kunde/Lieferant).
-
-# Die Rechnungsbuchung beim Einkauf bewirkt folgendes:
-# Die Steuerbemessungsgrundlage (exklusive Steuer) wird ausgewiesen bei den
-# jeweiligen Kategorien für den Vorsteuer Steuermessbetrag (z.B. Vorsteuer
-# Steuermessbetrag Voller Steuersatz 19%).
-# Der Steuerbetrag erscheint unter der Kategorie 'Vorsteuern' (z.B. Vorsteuer
-# 19%). Durch multidimensionale Hierachien können verschiedene Positionen
-# zusammengefasst werden und dann in Form eines Reports ausgegeben werden.
-#
-# Die Rechnungsbuchung beim Verkauf bewirkt folgendes:
-# Die Steuerbemessungsgrundlage (exklusive Steuer) wird ausgewiesen bei den
-# jeweiligen Kategorien für den Umsatzsteuer Steuermessbetrag
-# (z.B. Umsatzsteuer Steuermessbetrag Voller Steuersatz 19%).
-# Der Steuerbetrag erscheint unter der Kategorie 'Umsatzsteuer'
-# (z.B. Umsatzsteuer 19%). Durch multidimensionale Hierachien können
-# verschiedene Positionen zusammengefasst werden.
-# Die zugewiesenen Steuerausweise können auf Ebene der einzelnen
-# Rechnung (Eingangs- und Ausgangsrechnung) nachvollzogen werden,
-# und dort gegebenenfalls angepasst werden.
-
-# Rechnungsgutschriften führen zu einer Korrektur (Gegenposition)
-# der Steuerbuchung, in Form einer spiegelbildlichen Buchung.
-
-
 {
     'name': 'Deutschland - Accounting',
-    'version': '1.0',
-    'author': 'openbig.org',
-    'website': 'http://www.openbig.org',
+    'version': '1.1',
+    'author': 'copado MEDIA UG',
+	'website': 'http://www.copado.de',
     'category': 'Localization/Account Charts',
     'description': """
-Dieses  Modul beinhaltet einen deutschen Kontenrahmen basierend auf dem SKR03.
+Dieses Modul beinhaltet die deutschen Kontenrahmen SKR03 und SKR04 nach Datev
+und den IKR (Industriekontenrahmen).
 ==============================================================================
+	* Letzte Überarbeitung SKR03: Juni 2014
+	* Letzte Überarbeitung SKR04: noch offen
+	* In Kürze neuer zusätzlicher Kontenrahmen: IKR
 
-German accounting chart and localization.
+
+English:
+German accounting chart and localization (DATEV SKR03 and SKR04).
+	* Last change SKR03: Jun 2014
+	* Last change SKR04: open
+	* New chart will be comming soon: IKR
     """,
-    'depends': ['base', 'account', 'base_iban', 'base_vat', 'account_chart'],
-    'demo': [ ],
+    'depends': [
+		'base',
+		'account',
+		'base_iban',
+		'base_vat',
+		'account_chart'
+	],
+    'demo': [],
     'data': [
         'account_tax_skr03.xml',
         'account_types_skr03.xml',
@@ -145,6 +59,11 @@ German accounting chart and localization.
         'account_chart_skr04.xml',
         'account_chart_template_skr04.xml',
         'account_tax_fiscal_position_skr04.xml',
+        #'account_tax_ikr.xml',						=> todo: add new chart-template (Industriekontenrahmen (IKR)) which is also used sometimes in Germany
+        #'account_types_ikr.xml', 					=> part of Industriekontenrahmen
+        #'account_chart_ikr.xml', 					=> part of Industriekontenrahmen
+        #'account_chart_template_ikr.xml',			=> part of Industriekontenrahmen
+        #'account_tax_fiscal_position_ikr.xml',		=> part of Industriekontenrahmen
         'l10n_de_wizard.xml',
     ],
     'installable': True,
