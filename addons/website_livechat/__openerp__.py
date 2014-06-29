8caf967e6783e50a21f04e1b53ec058e5259a500
Antony Lesuisse <al@openerp.com>
2014-06-29 03:46:29 +0200

    MERGE] odoo bus, im_chat and im_livechat
    Add a generic bus for instant communication based on postgres LISTEN/NOTIFY and HTTP comet.
    Both threaded and gevent greenlet mode are supported. Chat should now work on every platform.
    
    im_chat improvements
    - proper support for multiple windows
    - present, away and offline status
    - improved data model for multi user chat session
    
    im_livechat improvements
    - standard css js assets are now used
    - qweb templates are now used instead of jinnja

--- addons/website_livechat/__openerp__.py
+++ addons/website_livechat/__openerp__.py
@@ -1,12 +1,12 @@
 {
-    'name': 'LiveChat',
+    'name': 'Website Live Support',
     'category': 'Website',
     'summary': 'Chat With Your Website Visitors',
     'version': '1.0',
     'description': """
-OpenERP Website LiveChat
+Odoo Website LiveChat
 ========================
-
+For website built with Odoo CMS, this module include a chat button on your Website, and allow your visitors to chat with your collabarators.
         """,
     'author': 'OpenERP SA',
     'depends': ['website', 'im_livechat'],
