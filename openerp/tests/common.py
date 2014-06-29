78e044b5c71e4d593679162720c43724d5454607
Antony Lesuisse <al@openerp.com>
2014-06-29 16:32:26 +0200

    [IMP] test impprove HTTPCase opener
    Simplify website crawler using the generic HTTPCase opener

--- openerp/tests/common.py
+++ openerp/tests/common.py
@@ -130,6 +130,25 @@ class SingleTransactionCase(BaseCase):
         cls.cr.rollback()
         cls.cr.close()
 
+class RedirectHandler(urllib2.HTTPRedirectHandler):
+    """
+    HTTPRedirectHandler is predicated upon HTTPErrorProcessor being used and
+    works by intercepting 3xy "errors".
+
+    Inherit from it to handle 3xy non-error responses instead, as we're not
+    using the error processor
+    """
+
+    def http_response(self, request, response):
+        code, msg, hdrs = response.code, response.msg, response.info()
+
+        if 300 <= code < 400:
+            return self.parent.error(
+                'http', request, response, code, msg, hdrs)
+
+        return response
+
+    https_response = http_response
 
 class HttpCase(TransactionCase):
     """ Transactionnal HTTP TestCase with url_open and phantomjs helpers.
@@ -152,6 +171,14 @@ class HttpCase(TransactionCase):
         self.session.db = DB
         openerp.http.root.session_store.save(self.session)
         self.localstorage_path = mkdtemp()
+        # setup an url opener helper
+        self.opener = urllib2.OpenerDirector()
+        self.opener.add_handler(urllib2.UnknownHandler())
+        self.opener.add_handler(urllib2.HTTPHandler())
+        self.opener.add_handler(urllib2.HTTPSHandler())
+        self.opener.add_handler(urllib2.HTTPCookieProcessor())
+        self.opener.add_handler(RedirectHandler())
+        self.opener.addheaders.append(('Cookie', 'session_id=%s' % self.session_id))
 
     def tearDown(self):
         rmtree(self.localstorage_path)
@@ -159,11 +186,15 @@ class HttpCase(TransactionCase):
         super(HttpCase, self).tearDown()
 
     def url_open(self, url, data=None, timeout=10):
-        opener = urllib2.build_opener()
-        opener.addheaders.append(('Cookie', 'session_id=%s' % self.session_id))
         if url.startswith('/'):
             url = "http://localhost:%s%s" % (PORT, url)
-        return opener.open(url, data, timeout)
+        return self.opener.open(url, data, timeout)
+
+    def authenticate(self, user, password):
+        if user is not None:
+            url = '/login?%s' % werkzeug.urls.url_encode({'db': DB,'login': user, 'key': password})
+            auth = self.url_open(url)
+            assert auth.getcode() < 400, "Auth failure %d" % auth.getcode()
 
     def phantom_poll(self, phantom, timeout):
         """ Phantomjs Test protocol.
