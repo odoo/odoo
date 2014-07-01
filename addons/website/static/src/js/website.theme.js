0e3e611d5fe2a54a316f2548e89f6e2d3cdbde51
Christophe Matthieu <chm@odoo.com>
2014-07-02 01:41:22 +0200

    [IMP] website.theme: convert into odoo javascript Widget; speed up the loading and complute of stylesheets

--- addons/website/static/src/js/website.theme.js
+++ addons/website/static/src/js/website.theme.js
@@ -1,146 +1,44 @@
 (function () {
     'use strict';
 
-    function theme_customize () {
-        var uniqueID = 0;
-        openerp.jsonRpc('/website/theme_customize_modal', 'call').then(function (modal) {
-            if ($('.theme_customize_modal').size()) return false;
-            var $modal = $(modal);
-
-            // force the browse to re-compute the stylesheets
-            var up, timer;
-            function compute_stylesheets () {
-                $modal.addClass("loading");
-                timer = setInterval(function () {
-                    $('body').toggleClass('theme_customize_css_loading');
-                },50);
-            }
-            openerp.website.theme_customize_css_loaded = function () {
-                clearInterval(timer);
-                $modal.removeClass("loading");
-            };
-
-            $modal.addClass("theme_customize_modal")
-                .appendTo("body")
-                .on('click', '.close', function () {
-                    openerp.website.theme_customize_css_loaded();
-                    $modal.removeClass('in');
-                    $modal.addClass('out');
-                    setTimeout(function () {$modal.remove();}, 500);
+    openerp.website.Theme = openerp.Widget.extend({
+        events: {
+            'change input[data-xmlid],input[data-enable],input[data-disable]': 'change_selection',
+            'click .close': 'close',
+        },
+        init: function (parent) {
+            this.rpc_modal = openerp.jsonRpc('/website/theme_customize_modal', 'call');
+            return this._super.apply(this, arguments);
+        },
+        __widgetRenderAndInsert: function (insertion, target) {
+            var self = this;
+            var def = new $.Deferred();
+            this.rpc_modal.then(function (modal) {
+                self.replaceElement($(modal));
+                insertion(target);
+                $.when(self.start()).then(function () {
+                    def.resolve.apply(def, arguments);
                 });
-
-            function get_inputs (string) {
-                return $modal.find('#'+string.split(",").join(", #"));
-            }
-            function get_xml_ids ($inputs) {
-                var xml_ids = [];
-                $inputs.each(function () {
-                    if ($(this).data('xmlid') && $(this).data('xmlid').length) {
-                        xml_ids = xml_ids.concat($(this).data('xmlid').split(","));
-                    }
-                });
-                return xml_ids;
-            }
-
-            function update_style(enable, disable, reload) {
-                var req = openerp.jsonRpc('/website/theme_customize', 'call', {
-                        'enable': enable,
-                        'disable': disable
-                    });
-                var $assets = $('link[href*=".assets_"]');
-                if (!reload && $assets.size()) {
-                    compute_stylesheets();
-                    req.then(function () {
-                        $assets.each(function () {
-                            var href = $(this).attr("href").split("?")[0]+"?v="+new Date().getTime();
-                            var $asset = $('<link rel="stylesheet" href="'+href+'" data-loading="true"/>');
-                            $asset.attr("onload", "$(this).prev().attr('disable', true).remove();"+
-                                "$(this).removeAttr('data-loading');"+
-                                "if (!$('link[data-loading]').size()) openerp.website.theme_customize_css_loaded();");
-                            $(this).after($asset);
-                        });
-                    });
-                } else {
-                    setTimeout(function () {
-                        window.location.hash = "theme=true";
-                        window.location.reload();
-                    },25);
-                }
-            }
-
-            function enable_disable (data, enable) {
-                if (!data) return;
-                $modal.find('#'+data.split(",").join(", #")).each(function () {
-                    var check = $(this).is(":checked");
-                    var $label = $(this).closest("label");
-                    $(this).attr("checked", enable);
-                    if (enable) $label.addClass("checked");
-                    else $label.removeClass("checked");
-                    if (check != enable) {
-                        $(this).change();
-                    }
-                });
-            }
-
-            var run = false;
-            var reload = false;
-            var time;
-            $modal.on('change', 'input[data-xmlid],input[data-enable],input[data-disable]', function () {
-                var $option = $(this), checked = $(this).is(":checked");
-
-                if (checked) {
-                    enable_disable($option.data('enable'), true);
-                    enable_disable($option.data('disable'), false);
-                    $option.closest("label").addClass("checked");
-                } else {
-                    $option.closest("label").removeClass("checked");
-                }
-                $option.attr("checked", checked);
-
-                var $enable = $modal.find('[data-xmlid]:checked');
-                $enable.closest("label").addClass("checked");
-                var $disable = $modal.find('[data-xmlid]:not(:checked)');
-                $disable.closest("label").removeClass("checked");
-
-                var $sets = $modal.find('input[data-enable]:not([data-xmlid]), input[data-disable]:not([data-xmlid])');
-                $sets.each(function () {
-                    var $set = $(this);
-                    var checked = true;
-                    if ($set.data("enable")) {
-                        get_inputs($(this).data("enable")).each(function () {
-                            if ($(this).is(":not(:checked)")) checked = false;
-                        });
-                    }
-                    if ($set.data("disable")) {
-                        get_inputs($(this).data("disable")).each(function () {
-                            if ($(this).is(":checked")) checked = false;
-                        });
-                    }
-                    if (checked) {
-                        $set.attr("checked", true).closest("label").addClass("checked");
-                    } else {
-                        $set.attr("checked", false).closest("label").removeClass("checked");
-                    }
-                    $set.trigger('update');
-                });
-
-                if (run && $(this).data('reload') && document.location.href.match(new RegExp( $(this).data('reload') ))) {
-                    reload = true;
-                }
-
-                clearTimeout(time);
-                if (run) {
-                    time = setTimeout(function () {
-                        update_style(get_xml_ids($enable), get_xml_ids($disable), reload);
-                        reload = false;
-                    },0);
-                } else {
-                    time = setTimeout(function () { reload = false; },0);
-                }
             });
-
-            // feature to use select field
-            var $selects = $modal.find('select:has(option[data-xmlid],option[data-enable],option[data-disable])');
+            return def;
+        },
+        start: function () {
+            var self = this;
+            this.timer = null;
+            this.reload = false;
+            this.flag = false;
+            this.$el.addClass("theme_customize_modal");
+            this.active_select_tags();
+            this.$inputs = $("input[data-xmlid]");
+            setTimeout(function () {self.$el.addClass('in');}, 0);
+            return this.load_xml_data().then(function () {
+                self.flag = true;
+            });
+        },
+        active_select_tags: function () {
+            var uniqueID = 0;
+            var self = this;
+            var $selects = this.$('select:has(option[data-xmlid],option[data-enable],option[data-disable])');
             $selects.each(function () {
                 uniqueID++;
                 var $select = $(this);
@@ -157,7 +55,7 @@
                     $input.on('update', function () {
                         $option.attr('selected', $(this).is(":checked"));
                     });
-                    $modal.append($input);
+                    self.$el.append($input);
                 });
                 $select.data("value", $options.first());
                 $options.first().attr("selected", true);
@@ -168,31 +66,160 @@
                 $(this).data("value", $option);
                 $option.data("input").change();
             });
-
-            var $inputs = $("input[data-xmlid]");
-            openerp.jsonRpc('/website/theme_customize_get', 'call', {
-                    'xml_ids': get_xml_ids($inputs)
+        },
+        load_xml_data: function (xml_ids) {
+            var self = this;
+            return openerp.jsonRpc('/website/theme_customize_get', 'call', {
+                    'xml_ids': this.get_xml_ids(this.$inputs)
                 }).then(function (data) {
-                    $inputs.each(function () {
-                        if (!_.difference(get_xml_ids($(this)), data[0]).length) {
+                    self.$inputs.each(function () {
+                        if (!_.difference(self.get_xml_ids($(this)), data[1]).length) {
+                            $(this).attr("checked", false).change();
+                        }
+                        if (!_.difference(self.get_xml_ids($(this)), data[0]).length) {
                             $(this).attr("checked", true).change();
                         }
                     });
-                    run = true;
                 });
-
-            $('a').each(function () {
-                var href = $(this).attr("href") || "";
-                if (href.match(/^[^#]*\//) && href.indexOf('theme=true') === -1) {
-                    $(this).attr("href", href + (href.indexOf("#") === -1 ? '#' : '&' ) + 'theme=true');
+        },
+        get_xml_ids: function ($inputs) {
+            var xml_ids = [];
+            $inputs.each(function () {
+                if ($(this).data('xmlid') && $(this).data('xmlid').length) {
+                    xml_ids = xml_ids.concat($(this).data('xmlid').split(","));
                 }
             });
+            return xml_ids;
+        },
+        compute_stylesheets: function () {
+            var self = this;
+            if (this.$el.hasClass("loading")) return;
+            this.$el.addClass("loading");
+            function theme_customize_css_onload() {
+                if ($('link[data-loading]').size()) {
+                    $('body').toggleClass('theme_customize_css_loading');
+                    setTimeout(theme_customize_css_onload, 50);
+                } else {
+                    $('body').removeClass('theme_customize_css_loading');
+                    self.$el.removeClass("loading");
+                }
+            }
+            theme_customize_css_onload();
+        },
+        update_style: function (enable, disable, reload) {
+            var req = openerp.jsonRpc('/website/theme_customize', 'call', {
+                    'enable': enable,
+                    'disable': disable
+                });
+            var $assets = $('link[href*=".assets_"]');
+            if (!reload && $assets.size()) {
+                $assets.attr('data-loading', true);
+                this.compute_stylesheets();
+                req.then(function () {
+                    $assets.each(function () {
+                        var href = $(this).attr("href").replace(/[^\/]+$/, new Date().getTime());
+                        var $asset = $('<link rel="stylesheet" href="'+href+'"/>');
+                        var clear_link = "$(this).prev().attr('disable', true).remove(); $(this).removeAttr('onload').removeAttr('onerror');";
+                        $asset.attr("onload", clear_link);
+                        $asset.attr("onerror", clear_link);
+                        $(this).after($asset);
+                    });
+                });
+                return req;
+            } else {
+                setTimeout(function () {
+                    window.location.hash = "theme=true";
+                    window.location.reload();
+                },25);
+                return $.Deferred();
+            }
+        },
+        enable_disable: function (data, enable) {
+            if (!data) return;
+            this.$('#'+data.split(",").join(", #")).each(function () {
+                var check = $(this).is(":checked");
+                var $label = $(this).closest("label");
+                $(this).attr("checked", enable);
+                if (enable) $label.addClass("checked");
+                else $label.removeClass("checked");
+                if (check != enable) {
+                    $(this).change();
+                }
+            });
+        },
+        change_selection: function (event) {
+            var self = this;
+            if (this.$el.hasClass("loading")) return;
 
-            setTimeout(function () {$modal.addClass('in');}, 0);
-        });
-    }
+            var $option = $(event.target),
+                checked = $option.is(":checked");
+
+            if (checked) {
+                this.enable_disable($option.data('enable'), true);
+                this.enable_disable($option.data('disable'), false);
+                $option.closest("label").addClass("checked");
+            } else {
+                $option.closest("label").removeClass("checked");
+            }
+            $option.attr("checked", checked);
+
+            var $enable = this.$('[data-xmlid]:checked');
+            $enable.closest("label").addClass("checked");
+            var $disable = this.$('[data-xmlid]:not(:checked)');
+            $disable.closest("label").removeClass("checked");
+
+            var $sets = this.$('input[data-enable]:not([data-xmlid]), input[data-disable]:not([data-xmlid])');
+            $sets.each(function () {
+                var $set = $(this);
+                var checked = true;
+                if ($set.data("enable")) {
+                    get_inputs($(this).data("enable")).each(function () {
+                        if ($(this).is(":not(:checked)")) checked = false;
+                    });
+                }
+                if ($set.data("disable")) {
+                    get_inputs($(this).data("disable")).each(function () {
+                        if ($(this).is(":checked")) checked = false;
+                    });
+                }
+                if (checked) {
+                    $set.attr("checked", true).closest("label").addClass("checked");
+                } else {
+                    $set.attr("checked", false).closest("label").removeClass("checked");
+                }
+                $set.trigger('update');
+            });
+
+            if (this.flag && $option.data('reload') && document.location.href.match(new RegExp( $option.data('reload') ))) {
+                this.reload = true;
+            }
+
+            clearTimeout(this.timer);
+            if (this.flag) {
+                this.timer = setTimeout(function () {
+                    self.update_style(self.get_xml_ids($enable), self.get_xml_ids($disable), self.reload);
+                    self.reload = false;
+                },0);
+            } else {
+                this.timer = setTimeout(function () { self.reload = false; },0);
+            }
+        },
+        close: function () {
+            var self = this;
+            $('link[href*=".assets_"]').removeAttr('data-loading');
+            this.$el.removeClass('in');
+            this.$el.addClass('out');
+            setTimeout(function () {self.destroy();}, 500);
+        }
+    });
 
     openerp.website.ready().done(function() {
+        function theme_customize() {
+            var Theme = openerp.website.Theme;
+            if (Theme.open && !Theme.open.isDestroyed()) return;
+            Theme.open = new Theme();
+            Theme.open.appendTo("body");
+        }
         $(document).on('click', "#theme_customize a",theme_customize);
         if ((window.location.hash || "").indexOf("theme=true") !== -1) {
             theme_customize();
