fbbc3a54e91e7768ecf33038154a2bdbd87505ba
Richard Mathot <rim@openerp.com>
2014-07-02 17:29:13 +0200

    [FIX] RML reporting: understandable crash message
    When tag <drawString> is misused in an RML report, logs a more explicit
    error

--- openerp/report/render/rml2pdf/trml2pdf.py
+++ openerp/report/render/rml2pdf/trml2pdf.py
@@ -373,7 +373,11 @@ class _rml_canvas(object):
         v = utils.attr_get(node, ['x','y'])
         text=self._textual(node, **v)
         text = utils.xml2str(text)
-        self.canvas.drawString(text=text, **v)
+        try:
+            self.canvas.drawString(text=text, **v)
+        except TypeError as e:
+            _logger.error("Bad RML: <drawReport> tag requires attributes 'x' and 'y'!")
+            raise e
 
     def _drawCenteredString(self, node):
         v = utils.attr_get(node, ['x','y'])
