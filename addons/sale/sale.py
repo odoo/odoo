39f3e403293e51f1e6e544638831dd090bcc0847
Guewen Baconnier <guewen@gmail.com>
2014-07-09 11:39:58 +0200

    [FIX] sale: copy_quotation must propagate the context

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -281,7 +281,7 @@ class sale_order(osv.osv):
         return osv.osv.unlink(self, cr, uid, unlink_ids, context=context)
 
     def copy_quotation(self, cr, uid, ids, context=None):
-        id = self.copy(cr, uid, ids[0], context=None)
+        id = self.copy(cr, uid, ids[0], context=context)
         view_ref = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'sale', 'view_order_form')
         view_id = view_ref and view_ref[1] or False,
         return {
