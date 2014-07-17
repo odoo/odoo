c80f0273ae2788e6494f4d02fc5cbea48d825a51
Yann Papouin <yann.papouin@gmail.com>
2014-07-17 16:12:22 +0200

    Convert date_confirm from date to datetime

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -658,7 +658,8 @@ class sale_order(osv.osv):
         return self.write(cr, uid, ids, {'state': 'done'}, context=context)
 
     def _prepare_order_line_procurement(self, cr, uid, order, line, group_id=False, context=None):
-        date_planned = self._get_date_planned(cr, uid, order, line, order.date_confirm, context=context)
+        date_confirm = fields.date.date_to_datetime(self, cr, uid, order.date_confirm, context=context)
+        date_planned = self._get_date_planned(cr, uid, order, line, date_confirm, context=context)
         return {
             'name': line.name,
             'origin': order.name,
