b97d999bdc34bdafe1ff39405babe591b9f82972
Leandro Ezequiel Baldi <baldileandro@gmail.com>
2014-07-24 16:03:36 -0300

    [FIX] Function action_wait
    File "/opt/openerp/openerp/osv/orm.py", line 374, in function_proxy
        return attr(self._cr, self._uid, [self._id], *args, **kwargs)
      File "/opt/openerp/openerp/addons/sale_stock/sale_stock.py", line 222, in action_wait
        res = super(sale_order, self).action_wait(cr, uid, ids, context=context)
    TypeError: action_wait() got an unexpected keyword argument 'context'

--- addons/sale_stock/sale_stock.py
+++ addons/sale_stock/sale_stock.py
@@ -163,6 +163,8 @@ class sale_order(osv.osv):
         return super(sale_order, self).action_cancel(cr, uid, ids, context=context)
 
     def action_wait(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
         res = super(sale_order, self).action_wait(cr, uid, ids, context=context)
         for o in self.browse(cr, uid, ids):
             noprod = self.test_no_product(cr, uid, o, context)
