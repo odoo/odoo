0d11544af0d85ee7ccca300048e701b940e490ba
Ravi Gadhia <rga@tinyerp.com>
2014-07-31 18:54:10 +0530

    [IMP] rename moduel name slides ==> website_slides: re-structure dir, remove unused import and apply pep8

--- addons/website_slides/__openerp__.py
+++ addons/website_slides/__openerp__.py
@@ -36,8 +36,8 @@ You can link and publish documents and slides to the event and talks also you ca
     'website': 'http://www.openerp.com',
     'depends': ['website', 'document'],
     'data': [
-        'view/slides.xml',
-        'view/slides_attachment.xml',
+        'view/slides_website.xml',
+        'view/slides_backend.xml',
     ],   
     'installable': True,
     'auto_install': False,   
