8ec696ab3158d5e32dd713568acfc073b2dc0c14
Daniel Dico <dd@oerp.ca>
2014-08-01 00:22:15 -0400

    FIX onchange_dates at init - start and end are false and will trigger an error

--- addons/calendar/calendar.py
+++ addons/calendar/calendar.py
@@ -977,7 +977,7 @@ class calendar_event(osv.Model):
         """
         value = {}
 
-        if checkallday != allday:
+        if checkallday != allday or not (start and end):
             return value
 
         value['allday'] = checkallday  # Force to be rewrited
