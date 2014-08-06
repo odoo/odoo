095be21ab17f7f731877d61c99b48b51119ff39a
Denis Ledoux <dle@odoo.com>
2014-08-06 12:47:40 +0200

    [FIX] crm_claim: allow to create claim as portal user
    It wasn't possible to create claims as a portal user when the portal user had a parent_id

--- addons/portal_claim/portal_claim.py
+++ addons/portal_claim/portal_claim.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2004-today OpenERP SA (<http://www.openerp.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,4 +19,21 @@
 #
 ##############################################################################
 
-import portal_claim
+from openerp import SUPERUSER_ID
+from openerp.addons.base_status.base_stage import base_stage
+from openerp.osv import osv
+
+
+class crm_claim(base_stage, osv.osv):
+    _inherit = "crm.claim"
+
+    def default_get(self, cr, uid, fields, context=None):
+        res = super(crm_claim, self).default_get(cr, uid, fields, context=context)
+        if isinstance(res.get('partner_id'), (int, long)):
+            # Special case for portal users, as they are not allowed to call name_get on res.partner
+            # We save this call for the web client by returning it in default get
+            res['partner_id'] = self.pool['res.partner'].name_get(
+                cr, SUPERUSER_ID, [res['partner_id']], context=context)[0]
+        return res
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
