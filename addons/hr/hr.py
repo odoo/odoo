2bbff57691a33a02ca6a85a600f0b32035daa2a4
Olivier Dony <odo@openerp.com>
2014-08-07 12:50:13 +0200

    [IMP] hr,mail,product,project,base: use @auto_join=True on _inherits FK field
    This can give a performance boost on large databases
    and should not be a concern in terms of access control
    as the inheritance already grants access to the parent
    records.

--- addons/hr/hr.py
+++ addons/hr/hr.py
@@ -207,7 +207,7 @@ class hr_employee(osv.osv):
         'parent_id': fields.many2one('hr.employee', 'Manager'),
         'category_ids': fields.many2many('hr.employee.category', 'employee_category_rel', 'emp_id', 'category_id', 'Tags'),
         'child_ids': fields.one2many('hr.employee', 'parent_id', 'Subordinates'),
-        'resource_id': fields.many2one('resource.resource', 'Resource', ondelete='cascade', required=True),
+        'resource_id': fields.many2one('resource.resource', 'Resource', ondelete='cascade', required=True, auto_join=True),
         'coach_id': fields.many2one('hr.employee', 'Coach'),
         'job_id': fields.many2one('hr.job', 'Job Title'),
         # image: all image fields are base64 encoded and PIL-supported
