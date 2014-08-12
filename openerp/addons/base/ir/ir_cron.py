8886b72f70564cee846154a684b756e197793084
Paul Catinean <paulcatinean@gmail.com>
2014-08-12 16:22:44 +0300

    [FIX] Removed casting astimezone from nextcall twice (already done in context_timestamp)

--- openerp/addons/base/ir/ir_cron.py
+++ openerp/addons/base/ir/ir_cron.py
@@ -176,8 +176,7 @@ class ir_cron(osv.osv):
                 addsql = ''
                 if not numbercall:
                     addsql = ', active=False'
-                cron_cr.execute("UPDATE ir_cron SET nextcall=%s, numbercall=%s"+addsql+" WHERE id=%s",
-                           (nextcall.astimezone(pytz.UTC).strftime(DEFAULT_SERVER_DATETIME_FORMAT), numbercall, job['id']))
+                cron_cr.execute("UPDATE ir_cron SET nextcall=%s, numbercall=%s"+addsql+" WHERE id=%s", (nextcall, numbercall, job['id']))
                 self.invalidate_cache(job_cr, SUPERUSER_ID)
 
         finally:
