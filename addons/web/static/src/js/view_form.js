c8396010e7088b7d22403db098943b6fce5209a5
fka-odoo <fka@openerp.com>
2014-08-13 12:43:34 +0530

    [FIX] mozila browser issue: remove fixed width

--- addons/web/static/src/js/view_form.js
+++ addons/web/static/src/js/view_form.js
@@ -2733,7 +2733,7 @@ instance.web.form.FieldTextHtml = instance.web.form.AbstractField.extend(instanc
         if (! this.get("effective_readonly")) {
             self._updating_editor = false;
             this.$textarea = this.$el.find('textarea');
-            var width = ((this.node.attrs || {}).editor_width || '100%');
+            var width = ((this.node.attrs || {}).editor_width || 'auto');
             var height = ((this.node.attrs || {}).editor_height || 250);
             this.$textarea.cleditor({
                 width:      width, // width not including margins, borders or padding
