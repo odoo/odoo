0d13d05db89fb5667d818aab3cd8a2a87c93b49b
falinwa <hans.yonathan@falinwa.com>
2014-08-14 16:28:18 +0800

    [Merge]l10n_cn: add general business chart
    [Merge]l10n_cn: add general business chart

--- addons/l10n_cn/__openerp__.py
+++ addons/l10n_cn/__openerp__.py
@@ -1,9 +1,10 @@
-# -*- encoding: utf-8 -*-
+﻿# -*- encoding: utf-8 -*-
 ##############################################################################
 #
 #    Copyright (C) 2008-2008 凯源吕鑫 lvxin@gmail.com   <basic chart data>
 #                            维智众源 oldrev@gmail.com  <states data>
 #    Copyright (C) 2012-2012 南京盈通 ccdos@intoerp.com <small business chart>
+#    Copyright (C) 2014-now  南京盈通 hans.yonathan@falinwa.com    <general business chart>
 #    Copyright (C) 2008-now  开阖软件 jeff@osbzr.com    < PM and LTS >
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -24,9 +25,9 @@
     'name': '中国会计科目表',
     'version': '1.8',
     'category': 'Localization/Account Charts',
-    'author': 'www.openerp-china.org',
+    'author': 'www.openerp-china.org ; Falinwa Limited',
     'maintainer':'jeff@osbzr.com',
-    'website':'http://openerp-china.org',
+    'website':'http://openerp-china.org ; http://falinwa.com',
     'description': """
 
     科目类型\会计科目表模板\增值税\辅助核算类别\管理会计凭证簿\财务会计凭证簿
@@ -43,6 +44,7 @@
         'account_chart_type.xml',
         'account_chart_template.xml',
         'account_chart_small_business_template.xml',
+        'account_chart_general_business_template.xml',
         'l10n_chart_cn_wizard.xml',
         'base_data.xml',
     ],
