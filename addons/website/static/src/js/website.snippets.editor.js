d12974eee38d7560dedd234a9b6ee9ae891fffdb
Christophe Matthieu <chm@odoo.com>
2014-08-19 16:19:53 +0200

    [FIX] website.snippet: can't edit website with firefox; remove firefox attr for media video is edit mode

--- addons/website/static/src/js/website.snippets.editor.js
+++ addons/website/static/src/js/website.snippets.editor.js
@@ -321,13 +321,14 @@
             var self = this;
             var snipped_event_flag;
             $("#wrapwrap").on('click', function (event) {
-                if (snipped_event_flag || !event.srcElement) {
+                var srcElement = event.srcElement || (event.originalEvent && (event.originalEvent.originalTarget || event.originalEvent.target));
+                if (snipped_event_flag || !srcElement) {
                     return;
                 }
                 snipped_event_flag = true;
 
                 setTimeout(function () {snipped_event_flag = false;}, 0);
-                var $target = $(event.srcElement);
+                var $target = $(srcElement);
 
                 if ($target.parents(".oe_overlay").length) {
                     return;
@@ -1537,6 +1538,9 @@
                 new website.editor.MediaDialog(self, self.element).appendTo(document.body);
                 self.BuildingBlock.make_active(false);
             }
+            setTimeout(function () {
+                self.$target.find(".css_editable_mode_display").removeAttr("_moz_abspos");
+            },0);
         },
     });
 
