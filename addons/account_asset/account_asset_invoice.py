67e0404ef9c99d2dbb89d86cdadae68abf3e8e74
Fekete Mihai <feketemihai@gmail.com>
2014-08-20 15:44:30 +0300

    Remove field period_id from creation of asset from invoice, update field depreciated_start_date instead of depreciation_date, mistake from previous commit.

--- addons/account_asset/account_asset_invoice.py
+++ addons/account_asset/account_asset_invoice.py
@@ -52,12 +52,11 @@ class account_invoice_line(osv.osv):
                     'code': line.invoice_id.number or False,
                     'category_id': line.asset_category_id.id,
                     'purchase_value': line.price_subtotal,
-                    'period_id': line.invoice_id.period_id.id,
                     'partner_id': line.invoice_id.partner_id.id,
                     'company_id': line.invoice_id.company_id.id,
                     'currency_id': line.invoice_id.currency_id.id,
                     'purchase_date' : line.invoice_id.date_invoice,
-                    'depreciation_date' : line.invoice_id.date_invoice,
+                    'depreciation_start_date' : line.invoice_id.date_invoice,
                 }
                 changed_vals = asset_obj.onchange_category_id(cr, uid, [], vals['category_id'], context=context)
                 vals.update(changed_vals['value'])
