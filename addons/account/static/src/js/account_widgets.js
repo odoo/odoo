4a0605aaa9f5b17505a3833ff1b2a5709092cc09
Arthur Maniet <ama@odoo.com>
2014-08-21 13:22:06 +0200

    [WIP] account reconciliation

--- addons/account/static/src/js/account_widgets.js
+++ addons/account/static/src/js/account_widgets.js
@@ -1,4 +1,9 @@
+/*global _:false */
+/*global openerp:false */
+
 openerp.account = function (instance) {
+    'use strict';
+
     openerp.account.quickadd(instance);
     var _t = instance.web._t,
         _lt = instance.web._lt;
@@ -34,26 +39,6 @@ openerp.account = function (instance) {
         start: function() {
             var self = this;
             return $.when(this._super()).then(function(){
-
-                // Inject variable styles
-                var style = document.createElement("style");
-                style.appendChild(document.createTextNode(""));
-                document.head.appendChild(style);
-                var css_selector = ".oe_reconciliation_line .toggle_match, .oe_reconciliation_line .toggle_create,  .oe_reconciliation_line .initial_line > td";
-                if(style.sheet.insertRule) {
-                    style.sheet.insertRule(css_selector + " { -webkit-transition-duration: "+self.aestetic_animation_speed+"ms; }", 0);
-                    style.sheet.insertRule(css_selector + " { -moz-transition-duration: "+self.aestetic_animation_speed+"ms; }", 0);
-                    style.sheet.insertRule(css_selector + " { -ms-transition-duration: "+self.aestetic_animation_speed+"ms; }", 0);
-                    style.sheet.insertRule(css_selector + " { -o-transition-duration: "+self.aestetic_animation_speed+"ms; }", 0);
-                    style.sheet.insertRule(css_selector + " { transition-duration: "+self.aestetic_animation_speed+"ms; }", 0);
-                } else {
-                    style.sheet.addRule(css_selector, "-webkit-transition-duration: "+self.aestetic_animation_speed+"ms;");
-                    style.sheet.addRule(css_selector, "-moz-transition-duration: "+self.aestetic_animation_speed+"ms;");
-                    style.sheet.addRule(css_selector, "-ms-transition-duration: "+self.aestetic_animation_speed+"ms;");
-                    style.sheet.addRule(css_selector, "-o-transition-duration: "+self.aestetic_animation_speed+"ms;");
-                    style.sheet.addRule(css_selector, "-webkit-transition-duration: "+self.aestetic_animation_speed+"ms;");
-                }
-
                 var deferred_promises = [];
 
                 // Create a dict account id -> account code for display facilities
@@ -913,6 +898,7 @@ openerp.account = function (instance) {
                 self.is_valid = true;
                 self.is_consistent = true;
                 self.filter = "";
+                self.propositions_lines = [];
                 self.set("balance", undefined, {silent: true});
                 self.set("mode", undefined, {silent: true});
                 self.set("pager_index", 0, {silent: true});
@@ -1858,12 +1844,13 @@ openerp.account = function (instance) {
             self.set("balance", balance);
 
             self.$(".button_reconcile").removeClass("oe_highlight");
-            self.$(".button_reconcile").text("Reconcile");
             if (mv_lines_selected.length === 0) {
-                self.$(".button_reconcile").text("Don't reconcile"); // Leave unreconciled
+                self.$(".button_reconcile").text(_t("Keep Open"));
             } else if (Math.abs(balance).toFixed(3) === "0.000") {
                 self.$(".button_reconcile").addClass("oe_highlight");
+                self.$(".button_reconcile").text(_t("Full Reconcile"));
             } else {
+                self.$(".button_reconcile").text(_t("Partial Reconcile"));
             }
         },
 
@@ -1922,7 +1909,7 @@ openerp.account = function (instance) {
                 var id = this.data.account_id;
                 var model = "account.account";
             }
-            return $.when(new instance.web.Model(model).call("mark_as_reconciled", [[id]])).then(function() {
+            return new instance.web.Model(model).call("mark_as_reconciled", [[id]]).then(function() {
                 return self.persistAndDestroy();
             });
         },
@@ -1931,7 +1918,7 @@ openerp.account = function (instance) {
             var self = this;
             var params = [self.data.account_id];
             if (self.data.partner_id) params.push(self.data.partner_id);
-            return $.when(self.model_aml.call("get_move_lines_for_manual_reconciliation_by_account_and_parter", params)).then(function(lines) {
+            return self.model_aml.call("get_move_lines_for_manual_reconciliation_by_account_and_parter", params).then(function(lines) {
                 if (lines.length === 0) self.persistAndDestroy();
                 else {
                     // TODO : mettre les nouvelles lignes dans l'ordre préexistant
@@ -1946,127 +1933,4 @@ openerp.account = function (instance) {
             });
         },
     });
-
-
-    instance.web.views.add('tree_account_reconciliation', 'instance.web.account.ReconciliationListView');
-    instance.web.account.ReconciliationListView = instance.web.ListView.extend({
-        init: function() {
-            this._super.apply(this, arguments);
-            var self = this;
-            this.current_partner = null;
-            this.on('record_selected', this, function() {
-                if (self.get_selected_ids().length === 0) {
-                    self.$(".oe_account_recon_reconcile").attr("disabled", "");
-                } else {
-                    self.$(".oe_account_recon_reconcile").removeAttr("disabled");
-                }
-            });
-        },
-        load_list: function() {
-            var self = this;
-            var tmp = this._super.apply(this, arguments);
-            if (this.partners) {
-                this.$el.prepend(QWeb.render("AccountReconciliation", {widget: this}));
-                this.$(".oe_account_recon_previous").click(function() {
-                    self.current_partner = (((self.current_partner - 1) % self.partners.length) + self.partners.length) % self.partners.length;
-                    self.search_by_partner();
-                });
-                this.$(".oe_account_recon_next").click(function() {
-                    self.current_partner = (self.current_partner + 1) % self.partners.length;
-                    self.search_by_partner();
-                });
-                this.$(".oe_account_recon_reconcile").click(function() {
-                    self.reconcile();
-                });
-                this.$(".oe_account_recom_mark_as_reconciled").click(function() {
-                    self.mark_as_reconciled();
-                });
-            }
-            return tmp;
-        },
-        do_search: function(domain, context, group_by) {
-            var self = this;
-            this.last_domain = domain;
-            this.last_context = context;
-            this.last_group_by = group_by;
-            this.old_search = _.bind(this._super, this);
-            var mod = new instance.web.Model("account.move.line", context, domain);
-            return mod.call("list_partners_to_reconcile", []).then(function(result) {
-                var current = self.current_partner !== null ? self.partners[self.current_partner][0] : null;
-                self.partners = result;
-                var index = _.find(_.range(self.partners.length), function(el) {
-                    if (current === self.partners[el][0])
-                        return true;
-                });
-                if (index !== undefined)
-                    self.current_partner = index;
-                else
-                    self.current_partner = self.partners.length == 0 ? null : 0;
-                self.search_by_partner();
-            });
-        },
-        search_by_partner: function() {
-            var self = this;
-            var fct = function() {
-                return self.old_search(new instance.web.CompoundDomain(self.last_domain, 
-                    [["partner_id", "in", self.current_partner === null ? [] :
-                    [self.partners[self.current_partner][0]] ]]), self.last_context, self.last_group_by);
-            };
-            if (self.current_partner === null) {
-                self.last_reconciliation_date = _t("Never");
-                return fct();
-            } else {
-                return new instance.web.Model("res.partner").call("read",
-                    [self.partners[self.current_partner][0], ["last_reconciliation_date"]]).then(function(res) {
-                    self.last_reconciliation_date = 
-                        instance.web.format_value(res.last_reconciliation_date, {"type": "datetime"}, _t("Never"));
-                    return fct();
-                });
-            }
-        },
-        reconcile: function() {
-            var self = this;
-            var ids = this.get_selected_ids();
-            if (ids.length === 0) {
-                new instance.web.Dialog(this, {
-                    title: _t("Warning"),
-                    size: 'medium',
-                }, $("<div />").text(_t("You must choose at least one record."))).open();
-                return false;
-            }
-
-            new instance.web.Model("ir.model.data").call("get_object_reference", ["account", "action_view_account_move_line_reconcile"]).then(function(result) {
-                var additional_context = _.extend({
-                    active_id: ids[0],
-                    active_ids: ids,
-                    active_model: self.model
-                });
-                return self.rpc("/web/action/load", {
-                    action_id: result[1],
-                    context: additional_context
-                }).done(function (result) {
-                    result.context = instance.web.pyeval.eval('contexts', [result.context, additional_context]);
-                    result.flags = result.flags || {};
-                    result.flags.new_window = true;
-                    return self.do_action(result, {
-                        on_close: function () {
-                            self.do_search(self.last_domain, self.last_context, self.last_group_by);
-                        }
-                    });
-                });
-            });
-        },
-        mark_as_reconciled: function() {
-            var self = this;
-            var id = self.partners[self.current_partner][0];
-            new instance.web.Model("res.partner").call("mark_as_reconciled", [[id]]).then(function() {
-                self.do_search(self.last_domain, self.last_context, self.last_group_by);
-            });
-        },
-        do_select: function (ids, records) {
-            this.trigger('record_selected')
-            this._super.apply(this, arguments);
-        },
-    });
-    
 };
