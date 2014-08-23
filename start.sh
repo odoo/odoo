b125917ff2faefb736a1972999a57c981ba9494d
jolev <jolevq@gmail.com>
2014-08-23 12:42:11 -0500

    a

---
+++ start.sh
@@ -0,0 +1,2 @@
+# Script para iniciar el servidor OpenERP
+./openerp-server -c .openerp_serverrc --logfile=log/openerp-server.log --pidfile=.oer.pid &
---
+++ stop.sh
@@ -0,0 +1,2 @@
+# Script para detener el servidor OpenERP
+kill -9 ‘cat .oer.pid’
