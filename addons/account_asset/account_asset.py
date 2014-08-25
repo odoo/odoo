ea547b6422cfd8b7a523fe064db0da76b8840381
Fekete Mihai <feketemihai@gmail.com>
2014-08-25 11:43:46 +0300

    Show historical move line containing the account_depreciation_id instead of the account_depreciation_expense_id, should take in consideration comparition of type of the asset when the revenue recognition will be merged.

--- addons/account_asset/account_asset.py
+++ addons/account_asset/account_asset.py
@@ -422,6 +422,7 @@ class account_asset_depreciation_line(osv.osv):
                 'currency_id': company_currency != current_currency and  current_currency or False,
                 'amount_currency': company_currency != current_currency and - sign * line.amount or 0.0,
                 'date': depreciation_date,
+                'asset_id': line.asset_id.id
             })
             move_line_obj.create(cr, uid, {
                 'name': asset_name,
@@ -437,7 +438,6 @@ class account_asset_depreciation_line(osv.osv):
                 'amount_currency': company_currency != current_currency and sign * line.amount or 0.0,
                 'analytic_account_id': line.asset_id.category_id.account_analytic_id.id,
                 'date': depreciation_date,
-                'asset_id': line.asset_id.id
             })
             self.write(cr, uid, line.id, {'move_id': move_id}, context=context)
             created_move_ids.append(move_id)
