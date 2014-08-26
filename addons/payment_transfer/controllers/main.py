d4f47438d05ead9baa4f12c4cf06dad2747f07a2
FalcoBolger <vta@openerp.com>
2014-08-26 10:54:33 +0200

    [ADD] Payment Custom

--- addons/payment_transfer/controllers/main.py
+++ addons/payment_transfer/controllers/main.py
@@ -20,3 +20,40 @@ class OgoneController(http.Controller):
         _logger.info('Beginning form_feedback with post data %s', pprint.pformat(post))  # debug
         request.registry['payment.transaction'].form_feedback(cr, uid, post, 'transfer', context)
         return werkzeug.utils.redirect(post.pop('return_url', '/'))
+
+
+class CustomController(http.Controller):
+
+    @http.route([
+        '/payment/custom/payment_details',
+    ], type='http', auth='public', website=True)
+    def custom_payment_details(self, **kwargs):
+        if not request.website.sale_get_order():
+            return request.redirect('/shop')
+
+        values = {}
+        values.update(kwargs=kwargs.items())
+        return request.render('payment_transfer.payment_details', values)
+
+    def update_transaction(self, request, tx_id, values):
+        return request.registry['payment.transaction'].write(request.cr, SUPERUSER_ID, tx_id, values, context=request.context)
+
+    @http.route([
+        '/payment/custom/feedback',
+    ], type='http', auth='public', website=True)
+    def custom_payment_feedback(self, **kwargs):
+        values = {}
+
+        return_url = kwargs.pop('return_url', '/shop/payment/validate')
+
+        for field_name, field_value in kwargs.items():
+            if field_name.startswith('x_') and field_name in request.registry['payment.transaction']._all_columns:
+                values[field_name] = field_value
+
+        tx = request.website.sale_get_transaction()
+        if not tx:
+            return request.redirect('/shop')
+
+        self.update_transaction(request, tx.id, values)
+
+        return request.redirect(return_url)
