a49a9d09fe3ea5b6d24482c1cb8168d6cc29710a
Jay Patel <jpa@openerp.com>
2014-08-26 12:09:48 +0530

    [IMP] account, account_bank_statement_import: onboarding tips. Added 2 data files holding 2 tips for account, 1 for account_bank_statement_import.

--- addons/account/__openerp__.py
+++ addons/account/__openerp__.py
@@ -112,6 +112,7 @@ for a particular financial year and for preparation of vouchers there is a modul
         'edi/invoice_action_data.xml',
         'account_bank_view.xml',
         'res_config_view.xml',
+        'account_tip_data.xml',
         'account_pre_install.yml',
         'views/report_vat.xml',
         'views/report_invoice.xml',
