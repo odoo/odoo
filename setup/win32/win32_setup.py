29132669a16792a84960f03c5627f04e0ad8ff6a
Simon Lejeune <sle@openerp.com>
2014-09-02 14:02:37 +0200

    [REF][FIX]: Windows packaging: lint, cleanup and make it work
    setup.nsi: make it build and use postgresql 9.3
    win32_se*: lint and simplified
    Makefile: only one mode

--- setup/win32/win32_setup.py
+++ setup/win32/win32_setup.py
@@ -1,32 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
 import os
 import glob
-from distutils.core import setup
 import py2exe
+from distutils.core import setup
 
 
-meta = {}
-execfile(os.path.join(os.path.dirname(__file__), '..', 'openerp', 'release.py'), meta)
+execfile(os.path.join(os.path.dirname(__file__), '..', '..', 'openerp', 'release.py'))
+
 
 def generate_files():
     actions = {
@@ -43,29 +24,31 @@ def generate_files():
         with open(fname, 'w') as fp:
             fp.write('@PATH=%WINDIR%\system32;%WINDIR%;%WINDIR%\System32\Wbem;.\n')
             for step in steps:
-                fp.write('@net %s %s\n' % (step, meta['nt_service_name']))
-
-    files.append('meta.py')
-    with open('meta.py', 'w') as fp:
-        for m in 'description serie nt_service_name'.split():
-            fp.write("%s = %r\n" % (m, meta[m],))
-
+                fp.write('@net %s %s\n' % (step, nt_service_name))
     return files
 
-excludes = "Tkconstants Tkinter tcl _imagingtk PIL._imagingtk ImageTk PIL.ImageTk FixTk".split()
-
-setup(service      = ["OpenERPServerService"],
-      version      = meta['version'],
-      license      = meta['license'],
-      url          = meta['url'],
-      author       = meta['author'],
-      author_email = meta['author_email'],
-      data_files   = generate_files(),
-      options      = {"py2exe": {
-                        "excludes": excludes,
-                        "skip_archive": 1,
-                        "optimize": 2,
-                     }},
-      )
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+setup(
+    service=["win32_service"],
+    version=version,
+    license=license,
+    url=url,
+    author=author,
+    author_email=author_email,
+    data_files=generate_files(),
+    options={
+        "py2exe": {
+            "excludes": [
+                'Tkconstants',
+                'Tkinter',
+                'tcl',
+                '_imagingtk',
+                'PIL._imagingtk',
+                'ImageTk',
+                'PIL.ImageTk',
+                'FixTk'
+            ],
+            "skip_archive": 1,
+            "optimize": 2,
+        }
+    },
+)
