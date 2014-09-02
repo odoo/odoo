b5a41ec696ea2761d7b594e7cc55260a726ad885
qdp-odoo <qdp@openerp.com>
2014-09-02 18:04:50 +0200

    [REF] base, res_currency: refactoring of function used in bank statement reconciliation widget to format amoutns accordingly to the currency symbol, position and digits

--- openerp/addons/base/res/res_currency.py
+++ openerp/addons/base/res/res_currency.py
@@ -273,18 +273,10 @@ class res_currency(osv.osv):
 
     def get_format_currencies_js_function(self, cr, uid, context=None):
         """ Returns a string that can be used to instanciate a javascript function.
-            That function formats a number according to the currency whose id is passed, or the company currency """
-        company_currency_id = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.currency_id.id
-        cr.execute("SELECT id, name, symbol, rounding, position FROM res_currency")
-        rows = cr.dictfetchall()
+            That function formats a number according to the currency whose id is passed as parameter"""
         function = ""
-
-        for row in rows:
-            if row['id'] == company_currency_id:
-                company_currency = row
-            # Number of digits from decimal rounding
-            # Warning : will fail miserably if rounding method is not a number of digits (ex : 0.05)
-            digits = math.log10(1/row['rounding'])
+        for row in self.search_read(cr, uid, domain=[], fields=['id', 'name', 'symbol', 'rounding', 'position'], context=context):
+            digits = math.log10(1 / row['rounding'])
             symbol = row['symbol'] or row['name']
 
             if row['position'] == 'after':
@@ -292,15 +284,6 @@ class res_currency(osv.osv):
             else:
                 return_str = "return '" + symbol + "\\xA0' + amount.toFixed(" + str(digits) + ");"
             function += "if (currency_id === " + str(row['id']) + ") { " + return_str + " }"
-
-        digits = math.log10(1/company_currency['rounding'])
-        symbol = company_currency['symbol'] or company_currency['name']
-        if company_currency['position'] == 'after':
-            return_str = "return amount.toFixed(" + str(digits) + ") + '\\xA0" + symbol + "';"
-        else:
-            return_str = "return '" + symbol + "\\xA0' + amount.toFixed(" + str(digits) + ");"
-        function += "else { " + return_str + " }"
-
         return function
 
 class res_currency_rate(osv.osv):
@@ -318,4 +301,3 @@ class res_currency_rate(osv.osv):
     _order = "name desc"
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
