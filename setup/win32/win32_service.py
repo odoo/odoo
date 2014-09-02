e67c06a588532fd7c6358ad514c90be8e75fb708
Simon Lejeune <sle@openerp.com>
2014-09-02 14:02:37 +0200

    [REF] Package: backport of 29132669a16792a84960f03c5627f04e0ad8ff6a
    [REF][FIX]: Windows packaging: lint, cleanup and make it work
    
    setup.nsi: make it build and use postgresql 9.3
    win32_se*: lint and simplified
    Makefile: only one mode

--- setup/win32/win32_service.py
+++ setup/win32/win32_service.py
@@ -1,82 +1,53 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
-import win32serviceutil
-import win32service
+import servicemanager
 import win32api
 import win32process
-import servicemanager
+import win32service
+import win32serviceutil
 
-import sys
 import subprocess
-import os
+import sys
+from os.path import dirname, join, split
 
-try:
-    import meta
-except ImportError:
-    if hasattr(sys, 'frozen'):
-        raise
-    from setup import generate_files
-    generate_files()
-    import meta     # noqa
 
-class OpenERPServerService(win32serviceutil.ServiceFramework):
-    # required info
-    _svc_name_ = meta.nt_service_name
-    _svc_display_name_ = "%s %s" % (meta.description, meta.serie)
+execfile(join(dirname(__file__), '..', 'server', 'openerp', 'release.py'))
+
+
+class OdooService(win32serviceutil.ServiceFramework):
+    _svc_name_ = nt_service_name
+    _svc_display_name_ = "%s %s" % (nt_service_name, serie)
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
-        # a reference to the server's process
-        self.terpprocess = None
+        self.odooprocess = None  # Reference to the server's process
 
     def SvcStop(self):
         # Before we do anything, tell the SCM we are starting the stop process.
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        # stop the running OpenERP Server: say it's a normal exit
-        win32api.TerminateProcess(int(self.terpprocess._handle), 0)
-        servicemanager.LogInfoMsg("OpenERP Server stopped correctly")
-
-    def StartTERP(self):
-        # The server finds now its configuration automatically on Windows
-        # We start the ERP Server as an independent process, but we keep its handle
-        # The server's binary must be one directory above the service's binary (when py2exe'd the python libraries shouldn' mix)
-        service_dir = os.path.dirname(sys.argv[0])
-        server_dir = os.path.split(service_dir)[0]
-        server_path = os.path.join(server_dir, 'server', 'openerp-server.exe')
-        self.terpprocess = subprocess.Popen([server_path], cwd=server_dir, creationflags=win32process.CREATE_NO_WINDOW)
+        # Stop the running Odoo: say it's a normal exit
+        win32api.TerminateProcess(int(self.odooprocess._handle), 0)
+        servicemanager.LogInfoMsg("Odoo stopped correctly")
 
     def SvcDoRun(self):
-        self.StartTERP()
-        servicemanager.LogInfoMsg("OpenERP Server up and running")
-        # exit with same exit code as OpenERP process
-        sys.exit(self.terpprocess.wait())
+        # We start Odoo as an independent process, but we keep its handle
+        service_dir = dirname(sys.argv[0])
+        server_dir = split(service_dir)[0]
+        server_path = join(server_dir, 'server', 'openerp-server.exe')
+        self.odooprocess = subprocess.Popen(
+            [server_path], cwd=server_dir, creationflags=win32process.CREATE_NO_WINDOW
+        )
+        servicemanager.LogInfoMsg('Odoo up and running')
+        # exit with same exit code as Odoo process
+        sys.exit(self.odooprocess.wait())
 
 
 def option_handler(opts):
     # configure the service to auto restart on failures...
-    subprocess.call(['sc', 'failure', meta.nt_service_name, 'reset=', '0', 'actions=', 'restart/0/restart/0/restart/0'])
+    subprocess.call([
+        'sc', 'failure', nt_service_name, 'reset=', '0', 'actions=', 'restart/0/restart/0/restart/0'
+    ])
+
 
 if __name__ == '__main__':
-    # Do with the service whatever option is passed in the command line
-    win32serviceutil.HandleCommandLine(OpenERPServerService, customOptionHandler=option_handler)
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    win32serviceutil.HandleCommandLine(OdooService, customOptionHandler=option_handler)
