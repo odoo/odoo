18a9d77ebfacf15c8f22c63a5bf5990adfccb802
Jaysinh Shukla <jsh@openerp.com>
2014-09-05 15:07:11 +0530

    [ADD] : Adding Javascript fiels and website view for website_url

---
+++ addons/website_url/static/src/js/website_url.js
@@ -0,0 +1,18 @@
+$(document).ready( function() {
+    $("#btn_shorten_url").click( function() {
+        openerp.jsonRpc("/r/new", 'call', {'url' : $("#url").val()})
+            .then(function (result) {
+                $("#btn_shorten_url").text("Copy")
+                if (result == false) {
+                    //Impliment functionality of coping shorten url to clipboard
+                    // create alert-error of bootstrap which is above the url
+                    /*<div class="alert alert-warning alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+  <strong>Warning!</strong> Better check yourself, you're not looking too good.
+</div>*/
+                } else {
+                    $("#url").val(result).focus().select();
+                }
+            });
+    });
+});
