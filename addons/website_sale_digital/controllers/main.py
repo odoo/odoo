125a7b5d0f00872d258a732c2d6b9e567e4f136a
David Monjoie <dmo@openerp.com>
2014-09-07 12:36:26 +0200

    [FIX] Fixed attachments rights verification, added last session downloads for public user, renamed downloads page

--- addons/website_sale_digital/controllers/main.py
+++ addons/website_sale_digital/controllers/main.py
@@ -7,9 +7,12 @@ from openerp.addons.web.http import request
 from openerp.addons.website_sale.controllers.main import website_sale
 from itertools import groupby
 from werkzeug.exceptions import NotFound
+from werkzeug.utils import redirect
 
 
 class website_sale_digital(website_sale):
+
+    downloads_page = '/shop/downloads'
 
     @http.route([
         '/shop/confirmation',
@@ -22,26 +25,47 @@ class website_sale_digital(website_sale):
         '/shop/attachment',
     ], auth='public')
     def download_attachment(self, attachment_id):
-        res = request.env(user=SU)['ir.attachment'].search_read([('id', '=', int(attachment_id))], ["name", "datas", "file_type"])
-        if res:
-            res = res[0]
+        from pprint import pprint; pprint(request.session)
+        # Check if this is a valid attachment id
+        attachment = request.env(user=SU)['ir.attachment'].search_read([('id', '=', int(attachment_id))], ["name", "datas", "file_type", "res_model", "res_id"])
+        if attachment:
+            attachment = attachment[0]
         else:
-            raise NotFound()
+            redirect(self.downloads_page)
 
-        # TODO: Check if the user has bought the associated product before handing him the attachment and the product is digital
-        data = base64.standard_b64decode(res["datas"])
+        # Check if the user has bought the associated product
+        res_model = attachment['res_model']
+        res_id = attachment['res_id']
+        purchased_products = self._get_purchased_digital_products(request.uid)
+        product_ids = map(lambda x: x['product_id'][0], purchased_products)
+
+        if res_model == 'product.template':
+            P = request.env['product.product']
+            template_ids = map(lambda x: P.browse(x).product_tmpl_id.id, product_ids)
+            if res_id not in template_ids:
+                return redirect(self.downloads_page)
+
+        elif res_model == 'product.product':
+            if res_id not in product_ids:
+                return redirect(self.downloads_page)
+
+        else:
+            return redirect(self.downloads_page)
+
+        # The client has bought the product, otherwise it would have been blocked by now
+        data = base64.standard_b64decode(attachment["datas"])
         headers = [
-            ('Content-Type', res['file_type']),
+            ('Content-Type', attachment['file_type']),
             ('Content-Length', len(data)),
-            ('Content-Disposition', 'attachment; filename="' + res['name'] + '"')
+            ('Content-Disposition', 'attachment; filename="' + attachment['name'] + '"')
         ]
         return request.make_response(data, headers)
 
     @http.route([
-        '/website_sale_digital/downloads',
+        downloads_page,
     ], type='http', auth='public', website=True)
     def get_downloads(self):
-        purchased_products = self._get_purchased_digital_products_from_uid(request.uid)
+        purchased_products = self._get_purchased_digital_products(request.uid)
 
         products_ids = []
         names = {}
@@ -79,14 +103,24 @@ class website_sale_digital(website_sale):
         return request.website.render('website_sale_digital.downloads', {
             'products': products_ids,
             'names': names,
-            'attachments': attachments
+            'attachments': attachments,
         })
 
-    def _get_purchased_digital_products_from_uid(self, uid):
+    def _get_purchased_digital_products(self, uid):
         user = request.env['res.users'].browse(uid)
         partner = user.partner_id
+        sale_orders = request.env(user=SU)['sale.order.line']
+        fields = ['product_id']
 
-        return request.env(user=SU)['sale.order.line'].search_read(
+        purchases = sale_orders.search_read(
             domain = [('order_id.partner_id', '=', partner.id), ('state', '=', 'confirmed'), ('product_id.product_tmpl_id.digital_content','=', True)],
-            fields = ['product_id'],
+            fields = fields,
         )
+
+        # Hack for public user last session
+        last_purchase = sale_orders.search_read(
+            domain = [('order_id', '=', request.session['sale_last_order_id'])],
+            fields = fields,
+        )
+
+        return purchases + last_purchase
