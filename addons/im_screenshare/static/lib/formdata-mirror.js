8dbe66cc03170d813bdd158860e618fdf8867c01
Jérome Maes <jem@openerp.com>
2014-09-08 12:16:32 +0200

    [IMP] im_screenshare : form changes in real time

--- addons/im_screenshare/static/lib/formdata-mirror.js
+++ addons/im_screenshare/static/lib/formdata-mirror.js
@@ -5,29 +5,45 @@ var FormDataMirrorClient = (function(){
         this.mirror = mirror;
     };
 
-    FormDataMirrorClient.prototype.setData = function() {
-        var elems = [];
-        _.each($('input, textarea, select'), function(el){
-            var arg = {
-                id : el.id,
-                tagName: el.tagName
-            };
-            if(el.tagName === 'INPUT'){
-                arg['type'] = el.type;
-                if(el.type === "text"){
-                    arg['value'] = el.value;
-                }
-                if(el.type === 'checkbox' || el.type === 'radio'){
-                    arg['value'] = el.checked ? true : false;
-                }
-            }
-            if(el.tagName === 'TEXTEAREA'){
+    FormDataMirrorClient.prototype._create_mutation = function(el) {
+        var arg = {
+            id : el.id,
+            tagName: el.tagName
+        };
+        if(el.tagName === 'INPUT'){
+            arg['type'] = el.type;
+            if(el.type === "text"){
                 arg['value'] = el.value;
             }
-            if(el.tagName === 'SELECT'){
-                arg['value'] = el.selectedIndex;
+            if(el.type === 'checkbox' || el.type === 'radio'){
+                arg['value'] = el.checked ? true : false;
             }
-            elems.push(arg);
+        }
+        if(el.tagName === 'TEXTEAREA'){
+            arg['value'] = el.value;
+        }
+        if(el.tagName === 'SELECT'){
+            arg['value'] = el.selectedIndex;
+        }
+        return arg;
+    };
+
+    FormDataMirrorClient.prototype.setData = function() {
+        var self = this;
+        var elems = [];
+        $elements = $('input, textarea, select');
+        // detect input changes
+        $elements.change(function(e) {
+            if(e.srcElement){
+                var change = self._create_mutation(e.srcElement);
+                var index = $elements.index(e.srcElement);
+                change["_index"] = index;
+                self.mirror.setData([change]);
+            }
+        });
+        // detect initial input values
+        _.each($elements, function(el){
+            elems.push(self._create_mutation(el));
         });
         this.mirror.setData(elems);
     };
@@ -43,19 +59,20 @@ var FormDataMirror = (function(){
     FormDataMirror.prototype.formData = function(data) {
         var $elements = $('input, textarea, select');
         _.each(data, function(el, i){
+            var index = el._index ? el._index : i;
             if(el.tagName === 'INPUT'){
                 if(el.type === "text"){
-                    $elements[i].value = el.value
+                    $elements[index].value = el.value
                 }
                 if(el.type === 'checkbox' || el.type === 'radio'){
-                    $elements[i].checked = el.value;
+                    $elements[index].checked = el.value;
                 }
             }
             if(el.tagName === 'TEXTEAREA'){
-                $elements[i].value = el.value;
+                $elements[index].value = el.value;
             }
             if(el.tagName === 'SELECT'){
-                $elements[i].selectedIndex = el.value;
+                $elements[index].selectedIndex = el.value;
             }
         });
     };
