8481fab1b1b3762f73ba5b472e12b702680144c2
Jamin Shah <jas@openerp.com>
2014-09-09 17:56:33 +0530

    [IMP] account_analytic_analysis : Add recurring field to product and add product to contract if it is recurring product on creation of sale order

--- addons/account_analytic_analysis/__openerp__.py
+++ addons/account_analytic_analysis/__openerp__.py
@@ -41,6 +41,7 @@ Adds menu to show relevant information to each manager.You can also view the rep
         'account_analytic_analysis_cron.xml',
         'res_config_view.xml',
         'views/account_analytic_analysis.xml',
+        'product_template_view.xml',
     ],
     'demo': ['analytic_account_demo.xml'],
     'test': ['test/account_analytic_analysis.yml'],
