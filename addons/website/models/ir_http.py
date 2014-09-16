819d1df64207b59a97732615e723105cac3c9f14
Fabien Meghazi <fme@openerp.com>
2014-09-16 11:27:06 +0200

    [FIX] request.website browse record bad context

--- addons/website/models/ir_http.py
+++ addons/website/models/ir_http.py
@@ -123,6 +123,8 @@ class ir_http(orm.AbstractModel):
                         # to url without language so google doesn't see duplicate content
                         return request.redirect(path + '?' + request.httprequest.query_string, code=301)
                     return self.reroute(path)
+            # bind modified context
+            request.website = request.website.with_context(request.context)
         return super(ir_http, self)._dispatch()
 
     def reroute(self, path):
