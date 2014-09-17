10c3b8c3c8f4b82301c03ea4fd6d910226cbd235
Martin Trigaux <mat@openerp.com>
2014-09-17 16:32:43 +0200

    [FIX] purchase: keep reference to purchase order line
    When generating an invoice from a stock.picking, the reference to the purchase.order.line needs to be kept (e.g. this is needed by anglo-saxon for price valuation). (opw 600767)

--- addons/purchase/stock.py
+++ addons/purchase/stock.py
@@ -54,6 +54,8 @@ class stock_move(osv.osv):
         return super(stock_move, self).copy(cr, uid, id, default, context)
 
     def _create_invoice_line_from_vals(self, cr, uid, move, invoice_line_vals, context=None):
+        if move.purchase_line_id:
+            invoice_line_vals['purchase_line_id'] = move.purchase_line_id.id
         invoice_line_id = super(stock_move, self)._create_invoice_line_from_vals(cr, uid, move, invoice_line_vals, context=context)
         if move.purchase_line_id:
             purchase_line = move.purchase_line_id
