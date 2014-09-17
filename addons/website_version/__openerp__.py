ce1ed475a8d9166b68704df26eeffa7ddb5e79af
Goffin Simon <simon.goffin@outlook.com>
2014-09-17 14:34:06 +0200

    [ADD] Google analytics.

--- addons/website_version/__openerp__.py
+++ addons/website_version/__openerp__.py
@@ -12,6 +12,7 @@ OpenERP Website CMS
     'depends': ['website','marketing'],
     'installable': True,
     'data': [
+        'data/data.xml',
         'security/ir.model.access.csv',
         'views/website_templates.xml',
         'views/marketing_view.xml',
