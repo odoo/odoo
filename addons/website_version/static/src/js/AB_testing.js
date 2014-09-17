575dab64418871820ea7ce558e16a23bede5cdab
Goffin Simon <simon.goffin@outlook.com>
2014-09-17 17:27:14 +0200

    [ADD] push view_id.

--- addons/website_version/static/src/js/AB_testing.js
+++ addons/website_version/static/src/js/AB_testing.js
@@ -1,18 +1,23 @@
 (function() {
-    'use strict';
-    var _t = openerp._t;
-    var website=openerp.website;
-
-
-    
-    website.EditorBarContent.include({
-        start: function() {
-            return this._super();
-        },
-    });
-    
+       
     $(document).ready(function() {
-        console.log($('html').attr('data-view-xmlid'));
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-7333765-1']);
+        _gaq.push(['_setDomainName', '.openerp.com']);
+
+        (function() {
+            var ga   = document.createElement('script');
+            ga.type  = 'text/javascript';
+            ga.async = true;
+            ga.src   = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga,s);
+        })();
+
+        var view_id = $('html').attr('data-view-xmlid');
+        console.log(view_id);
+        _gaq.push(['_setCustomVar', 4, 'view_id', view_id, 1]);
     });
     
 })();
