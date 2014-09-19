5be3ac1fd08922a25f051263544b6642bb3b8ac2
Lionel Sausin (Numérigraphe) <ls@numerigraphe.com>
2014-09-19 16:38:31 +0200

    [FIX] sale_stock: use current product's UoM to check availability
    When selecting a product in a sale.order, the asked quantity is verified against
    the available quantity.
    If the user changes the UoM or the onchange from super call changes it,
    the context should be updated accordingly.
    Fixes #2559

--- addons/sale_stock/sale_stock.py
+++ addons/sale_stock/sale_stock.py
@@ -601,6 +601,14 @@ class sale_order_line(osv.osv):
             res['value'].update({'product_packaging': False})
             return res
 
+        # set product uom in context to get virtual stock in current uom
+        if res.get('value', {}).get('product_uom'):
+            # use the uom changed by super call
+            context.update({'uom': res['value']['product_uom']})
+        elif uom:
+            # fallback on selected
+            context.update({'uom': uom})
+
         #update of result obtained in super function
         product_obj = product_obj.browse(cr, uid, product, context=context)
         res['value']['delay'] = (product_obj.sale_delay or 0.0)
