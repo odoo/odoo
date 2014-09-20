2c430b54097513c9d55405cadc382e31863887dc
Goffin Simon <simon.goffin@outlook.com>
2014-09-20 15:26:32 +0200

    [IMP] Experience by website_id.

--- addons/website_version/models/website.py
+++ addons/website_version/models/website.py
@@ -14,7 +14,7 @@ class NewWebsite(osv.Model):
         snapshot_id=request.context.get('snapshot_id')
         experiment_id=request.context.get('experiment_id')
         if experiment_id:
-            exp_run_ids = request.registry['website_version.experiment'].search(cr, uid, [('state','=','running')], context=context)
+            exp_run_ids = request.registry['website_version.experiment'].search(cr, uid, [('state','=','running'),('website_id','=',context.get('website_id'))], context=context)
             return (0, 'experiment',len(exp_run_ids))
 
         if not snapshot_id:
