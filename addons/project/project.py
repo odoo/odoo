10d2c994bd0356328059b514074449215314d1e7
Ronak Baxi <rba@openerp.com>
2014-09-26 16:52:31 +0530

    [IMP]Kanban background when there is no task to display

--- addons/project/project.py
+++ addons/project/project.py
@@ -673,7 +673,7 @@ class task(osv.osv):
         project_id = self._resolve_project_id_from_context(cr, uid, context=context)
         if project_id:
             search_domain += ['|', ('project_ids', '=', project_id)]
-        search_domain += [('id', 'in', ids)]
+            search_domain += [('id', 'in', ids)]
         stage_ids = stage_obj._search(cr, uid, search_domain, order=order, access_rights_uid=access_rights_uid, context=context)
         result = stage_obj.name_get(cr, access_rights_uid, stage_ids, context=context)
         # restore order of the search
