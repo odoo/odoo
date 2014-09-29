393331f9484d069e390d444d530171cc1b01f061
Xavier Morel <xmo@openerp.com>
2014-09-29 16:13:52 +0200

    [FIX] sphinx 'project' value
    The project name automatically gets the release and the literal string
    "documentation" appended by default (and "html_title" can be set to generate a
    title differently), so having "documentation" set in the project variable
    duplicates it in the page title.

--- doc/conf.py
+++ doc/conf.py
@@ -44,7 +44,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'odoo developer documentation'
+project = u'odoo'
 copyright = u'2014, OpenERP s.a.'
 
 # The version info for the project you're documenting, acts as replacement for
