0f41dd4c52bc7b45e6d92ab71ddefb1009bd1914
Benjamin Bachmann <bbachmann@bloopark.de>
2014-09-30 14:46:10 +0200

    [IMP] 8.0 Fontloader integration for odoo core to improve website loading speed

--- openerp/addons/base/ir/ir_ui_view.py
+++ openerp/addons/base/ir/ir_ui_view.py
@@ -979,12 +979,16 @@ class view(osv.osv):
 
         if values is None:
             values = dict()
-        if not values.get('website', False):
+
+        # if no website object is provided it will be added to the values dictionary to ensure the availability
+        # of the current website object in all templates
+        if not 'website' in values:
             w_obj = self.pool.get('website')
             if w_obj:
                 current_website = w_obj.get_current_website(cr, uid, context=context)
                 if current_website:
                     values['website'] = current_website
+
         qcontext = dict(
             keep_query=keep_query,
             request=request, # might be unbound if we're not in an httprequest context
