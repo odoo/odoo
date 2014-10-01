f48f4c68dcabbeac16b0836eede5fa7fba1612bc
Frederic van der Essen <fva@openerp.com / fvdessen+o@gmail.com>
2014-10-01 12:24:30 +0200

    [FIX] pos_loyalty: missing code from pos_loyalty MERGE

--- addons/point_of_sale/static/src/js/models.js
+++ addons/point_of_sale/static/src/js/models.js
@@ -1029,6 +1029,12 @@ function openerp_pos_models(instance, module){ //module is instance.point_of_sal
                 line.set_discount(options.discount);
             }
 
+            if(options.extras !== undefined){
+                for (prop in options.extras) { 
+                    line[prop] = options.extras[prop];
+                }
+            }
+
             var last_orderline = this.getLastOrderline();
             if( last_orderline && last_orderline.can_be_merged_with(line) && options.merge !== false){
                 last_orderline.merge(line);
