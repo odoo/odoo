7aa5d397c8cc2a1e6fae4c6a289cd635b952ea42
Denis Ledoux <dle@odoo.com>
2014-10-03 14:02:25 +0200

    [Revert] cb30783aba642627c6bf71d4aa812de00e7d536d & 6349048ba0e5e74c74e783ffce8c3c0083d2f060
    [FIX] web: avoid force_reload in list editable
    [FIX] web: force load record after reload page
    
    These fixes prevent to open existing record form

--- addons/web/static/src/js/view_form.js
+++ addons/web/static/src/js/view_form.js
@@ -282,9 +282,9 @@ instance.web.FormView = instance.web.View.extend(instance.web.form.FieldManagerM
         this.$el.add(this.$buttons).removeClass('oe_form_dirty');
 
         var shown = this.has_been_loaded;
-        if (options.reload !== false || !this.datarecord.id) {
+        if (options.reload !== false) {
             shown = shown.then(function() {
-                if (self.dataset.index === null || !self.datarecord.id) {
+                if (self.dataset.index === null) {
                     // null index means we should start a new record
                     return self.on_button_new();
                 }
