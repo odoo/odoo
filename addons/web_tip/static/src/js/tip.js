b74cc67ec40f56c905da4fbbc75a659e1765976d
Julien Legros <jle@odoo.com>
2014-10-06 13:36:03 +0200

    [ADD] mail_tip: link module between mail and web_tip

--- addons/web_tip/static/src/js/tip.js
+++ addons/web_tip/static/src/js/tip.js
@@ -304,16 +304,4 @@
             instance.webclient.tip_handler.reposition();
         }
     });
-
-    instance.web_tip = function(session) {
-        if (session.mail) {
-            session.mail.Thread.include({
-                message_fetch: function() {
-                    return this._super.apply(this, arguments).done(function() {
-                        instance.web.bus.trigger('chatter_messages_fetched');
-                    });
-                }
-            });
-        }
-    };
 })();
