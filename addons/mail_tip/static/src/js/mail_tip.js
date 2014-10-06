06a1d3cfabab6ffdc980590b321be4815dc77814
Julien Legros <jle@odoo.com>
2014-10-06 14:18:25 +0200

    [FIX] squash me

--- addons/mail_tip/static/src/js/mail_tip.js
+++ addons/mail_tip/static/src/js/mail_tip.js
@@ -1,12 +1,9 @@
-(function() {
-    var instance = openerp;
-    instance.web_tip = function(session) {
-        session.mail.Thread.include({
-            message_fetch: function() {
-                return this._super.apply(this, arguments).done(function() {
-                    instance.web.bus.trigger('chatter_messages_fetched');
-                });
-            }
-        });
-    };
-})();
+openerp.web_tip = function(session) {
+    session.mail.Thread.include({
+        message_fetch: function() {
+            return this._super.apply(this, arguments).done(function() {
+                openerp.web.bus.trigger('chatter_messages_fetched');
+            });
+        }
+    });
+};
