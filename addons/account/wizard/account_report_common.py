0adc1bc25a1cd085e97e2980fe08550cc753a10c
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2014-10-06 09:28:01 +0200

    Reformat for PEP8

--- addons/account/wizard/account_report_common.py
+++ addons/account/wizard/account_report_common.py
@@ -138,7 +138,7 @@ class account_common_report(osv.osv_memory):
     def _get_all_journal(self, cr, uid, context=None):
         # The 'active_test' context is used, because reports must
         # return entries created on inactive journals.
-        return self.pool.get('account.journal').search(
+        return self.pool['account.journal'].search(
             cr, uid, [], context=dict(context or {}, active_test=False)
         )
 
@@ -178,7 +178,12 @@ class account_common_report(osv.osv_memory):
         data = {}
         data['ids'] = context.get('active_ids', [])
         data['model'] = context.get('active_model', 'ir.ui.menu')
-        data['form'] = self.read(cr, uid, ids, ['date_from', 'date_to', 'fiscalyear_id', 'period_from', 'period_to', 'filter', 'chart_account_id', 'target_move'], context=context)[0]
+        data['form'] = self.read(
+            cr, uid, ids,
+            [
+                'date_from', 'date_to', 'fiscalyear_id', 'period_from',
+                'period_to', 'filter', 'chart_account_id', 'target_move'
+            ], context=context)[0]
         # The 'active_test' context is used, because reports must
         # return entries created on inactive journals.
         data['form']['journal_ids'] = self.read(
