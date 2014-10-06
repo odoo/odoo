3e353378ab67f71923ef68ef17c2f42799f185f8
Your Name <code@openerp.com>
2014-10-06 12:15:14 +0530

    [FIX] account : Fixed the ParseError while evaluating 'active'.

--- addons/account/account_move_line.py
+++ addons/account/account_move_line.py
@@ -1231,8 +1231,8 @@ class account_move_line(osv.osv):
                 vals['company_id'] = move.company_id.id
             if move.date and not vals.get('date'):
                 vals['date'] = move.date
-        if ('account_id' in vals) and not account_obj.read(cr, uid, [vals['account_id']], ['active'])[0]['active']:
-            raise osv.except_osv(_('Bad Account!'), _('You cannot use an inactive account.'))
+        if ('account_id' in vals) and not account_obj.read(cr, uid, [vals['account_id']], ['deprecated'])[0]['deprecated']:
+            raise osv.except_osv(_('Bad Account!'), _('You cannot use deprecated account.'))
         if 'journal_id' in vals and vals['journal_id']:
             context['journal_id'] = vals['journal_id']
         if 'period_id' in vals and vals['period_id']:
