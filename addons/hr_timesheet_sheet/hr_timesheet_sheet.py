35ad3b176ec27615788ca1970ef196b29a0de6b0
dhr-odoo <dhr@openerp.com>
2014-10-13 18:00:12 +0530

    [IMP]Replaced %U by isocalendar() for naming weeks in hr timesheet as isocalendar is the ISO standard for counting of weeks

--- addons/hr_timesheet_sheet/hr_timesheet_sheet.py
+++ addons/hr_timesheet_sheet/hr_timesheet_sheet.py
@@ -245,7 +245,7 @@ class hr_timesheet_sheet(osv.osv):
             return []
         if isinstance(ids, (long, int)):
             ids = [ids]
-        return [(r['id'], _('Week ')+datetime.strptime(r['date_from'], '%Y-%m-%d').strftime('%U')) \
+        return [(r['id'], _('Week ')+str(datetime.strptime(r['date_from'], '%Y-%m-%d').isocalendar()[1])) \
                 for r in self.read(cr, uid, ids, ['date_from'],
                     context=context, load='_classic_write')]
 
