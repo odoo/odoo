fcf21d2ed4cb590731f3cc27e7e406d0bd365cae
Goffin Simon <simon.goffin@outlook.com>
2014-10-14 14:01:31 +0200

    [ADD] Smart button for publish version.

--- addons/website_version/models/snapshot.py
+++ addons/website_version/models/snapshot.py
@@ -15,3 +15,19 @@ class Snapshot(osv.Model):
     _sql_constraints = [
         ('name_uniq', 'unique(name)', 'You cannot have multiple snapshots with the same name!'),
     ]
+
+    def action_publish(self,cr,uid,ids,context=None):
+        if context is None:
+            context = {}
+        snapshot_id = context.get('active_id')
+        print snapshot_id
+        if snapshot_id:
+            snapshot = self.browse(cr, uid, [snapshot_id],context)[0]
+            del_l = []
+            for view in snapshot.view_ids:
+                master_id = self.pool['ir.ui.view'].search(cr, uid, [('key','=',view.key),('snapshot_id', '=', False),('website_id', '=', view.website_id.id)],context=context)
+                del_l += master_id
+            if del_l:
+                self.pool['ir.ui.view'].unlink(cr, uid, del_l, context=context)        
+            for view in self.browse(cr, uid, [snapshot_id],context).view_ids:
+                view.copy({'snapshot_id': None})
