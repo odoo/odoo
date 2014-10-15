ee6fec7772b7c9ab7ecb81b7bced7780e77fdcf0
mdi-odoo <mdi@tinyerp.com>
2014-10-15 12:02:28 +0530

    [IMP] account : Converted the 'res.company' object to new API.

--- addons/account/models/company.py
+++ addons/account/models/company.py
@@ -19,33 +19,27 @@
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
+from openerp import models, fields
 
-class res_company(osv.osv):
+class res_company(models.Model):
     _inherit = "res.company"
-    _columns = {
-        'expects_chart_of_accounts': fields.boolean('Expects a Chart of Accounts'),
-        'tax_calculation_rounding_method': fields.selection([
+
+    expects_chart_of_accounts = fields.Boolean(string='Expects a Chart of Accounts', default=True)
+    tax_calculation_rounding_method = fields.Selection([
             ('round_per_line', 'Round per Line'),
             ('round_globally', 'Round Globally'),
-            ], 'Tax Calculation Rounding Method',
-            help="If you select 'Round per Line' : for each tax, the tax amount will first be computed and rounded for each PO/SO/invoice line and then these rounded amounts will be summed, leading to the total amount for that tax. If you select 'Round Globally': for each tax, the tax amount will be computed for each PO/SO/invoice line, then these amounts will be summed and eventually this total tax amount will be rounded. If you sell with tax included, you should choose 'Round per line' because you certainly want the sum of your tax-included line subtotals to be equal to the total amount with taxes."),
-        'paypal_account': fields.char("Paypal Account", size=128, help="Paypal username (usually email) for receiving online payments."),
-        'overdue_msg': fields.text('Overdue Payments Message', translate=True),
-    }
-
-    _defaults = {
-        'expects_chart_of_accounts': True,
-        'tax_calculation_rounding_method': 'round_per_line',
-        'overdue_msg': '''Dear Sir/Madam,
+        ], default='round_per_line', string='Tax Calculation Rounding Method',
+        help="If you select 'Round per Line' : for each tax, the tax amount will first be computed and rounded for each PO/SO/invoice line and then these rounded amounts will be summed, leading to the total amount for that tax. If you select 'Round Globally': for each tax, the tax amount will be computed for each PO/SO/invoice line, then these amounts will be summed and eventually this total tax amount will be rounded. If you sell with tax included, you should choose 'Round per line' because you certainly want the sum of your tax-included line subtotals to be equal to the total amount with taxes.")
+    paypal_account = fields.Char(string='Paypal Account', size=128, help="Paypal username (usually email) for receiving online payments.")
+    overdue_msg = fields.Text(string='Overdue Payments Message', translate=True,
+        default='''Dear Sir/Madam,
 
 Our records indicate that some payments on your account are still due. Please find details below.
 If the amount has already been paid, please disregard this notice. Otherwise, please forward us the total amount stated below.
 If you have any queries regarding your account, Please contact us.
 
 Thank you in advance for your cooperation.
-Best Regards,'''
-    }
+Best Regards,''')
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
