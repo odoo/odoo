eba66d702be388dbb9cc666bed73976d684ae49f
Arthur Maniet <me@whisno.be>
2014-10-16 11:21:28 +0200

    [FIX] l10n_be_coda: use float_compare rather than assertEquals for floats

--- addons/l10n_be_coda/tests/test_import_bank_statement.py
+++ addons/l10n_be_coda/tests/test_import_bank_statement.py
@@ -1,5 +1,6 @@
 from openerp.tests.common import TransactionCase
 from openerp.modules.module import get_module_resource
+from openerp.tools import float_compare
 
 class TestCodaFile(TransactionCase):
     """Tests for import bank statement coda file format (account.bank.statement.import)
@@ -22,8 +23,8 @@ class TestCodaFile(TransactionCase):
         self.statement_import_model.parse_file(cr, uid, [self.bank_statement_id])
         statement_id = self.bank_statement_model.search(cr, uid, [('name', '=', '135')])[0]
         bank_st_record = self.bank_statement_model.browse(cr, uid, statement_id)
-        self.assertEquals(bank_st_record.balance_start, 11812.70)
-        self.assertEquals(bank_st_record.balance_end_real, 13646.05)
+        self.assertEqual(float_compare(bank_st_record.balance_start, 11812.70, precision_digits=2), 0)
+        self.assertEqual(float_compare(bank_st_record.balance_end_real, 13646.05, precision_digits=2), 0)
 
     def test_coda_file_import_twice(self):
         cr, uid = self.cr, self.uid
