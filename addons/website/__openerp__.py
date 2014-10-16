c9ad204acab214de2629692161f6b40a5a961eb6
Goffin Simon <simon.goffin@outlook.com>
2014-10-16 22:10:22 +0200

    [ADD] 4.

--- addons/website/__openerp__.py
+++ addons/website/__openerp__.py
@@ -14,6 +14,7 @@ OpenERP Website CMS
     'installable': True,
     'data': [
         'data/data.xml',
+        'data/demo.xml',
         'security/ir.model.access.csv',
         'security/ir_ui_view.xml',
         'views/website_templates.xml',
