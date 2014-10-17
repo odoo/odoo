d79cb099ce4e338556e6d236f0cf83255bdac533
Ajay javiya <aja@openerp.com>
2014-10-17 18:01:47 +0530

    [IMP][MIG] lunch module : migrare to new api and lint a module
    [IMP] : use lxml.builder instead of etree.Element , for more code readability
    [FIX] : order total is not updated when order line price is change and small code improvements
    
    [IMP] : small code improvments
    
    [IMP] : typos and remove size from field attribute , use TransientModel for wizard
    
    [IMP] : raise exception  when pref_id fails
    
    [IMP]: move can_display_alert to lunch.order and rename it to get_alert_message
    [REN] : field active_from to start_hour and active_to to end_hour
    
    [REM] : onchange_price, as total is compute field that is automatically update every time when record is save
    
    [REF] fields view get method

--- addons/lunch/__init__.py
+++ addons/lunch/__init__.py
@@ -1,8 +1,8 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2012 Tiny SPRL (<http://tiny.be>).
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2014-TODAY Odoo SA (<https://www.odoo.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
