6a36b80e0e66ee1ff74977f2ddb10079790c055c
Géry Debongnie <ged@odoo.com>
2014-10-20 12:42:58 +0200

    [FIX] css and layout fix in advanced search view
    * make sure the searchbar allows text selection
    * revert back to col-md (from col-lg) classes => looks better

--- addons/web/static/src/js/views.js
+++ addons/web/static/src/js/views.js
@@ -661,7 +661,7 @@ instance.web.ViewManager =  instance.web.Widget.extend({
             if (self.searchview) {
                 var is_hidden = self.active_view.controller.searchable === false;
                 self.searchview.toggle_visibility(!is_hidden);
-                self.$header_col.toggleClass('col-lg-6', !is_hidden).toggleClass('col-lg-12', is_hidden);
+                self.$header_col.toggleClass('col-md-6', !is_hidden).toggleClass('col-md-12', is_hidden);
                 self.$search_col.toggle(!is_hidden);
             }
             self.display_breadcrumbs();
