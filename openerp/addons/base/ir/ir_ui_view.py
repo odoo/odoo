0aa0787ac067625d1564c9c13f2850147235a9aa
Goffin Simon <simon.goffin@outlook.com>
2014-10-21 13:42:33 +0200

    [IMP] apply_inheritance_specs.

--- openerp/addons/base/ir/ir_ui_view.py
+++ openerp/addons/base/ir/ir_ui_view.py
@@ -541,7 +541,9 @@ class view(osv.osv):
                     if attr != 'position'
                 ])
                 tag = "<%s%s>" % (spec.tag, attrs)
-                self.raise_view_error(cr, uid, _("Element '%s' cannot be located in parent view") % tag, inherit_id, context=context)
+                view = self.browse(cr, uid, inherit_id, context)
+                _logger.warning("From view %s element '%s' cannot be located in parent view %s" % (inherit_id, tag, view.inherit_id.id))
+                # self.raise_view_error(cr, uid, _("Element '%s' cannot be located in parent view") % tag, inherit_id, context=context)
 
         return source
 
