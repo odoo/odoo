9f20f41fb3880355918d386ed2d7882777d64f75
Simon Lejeune <sle@openerp.com>
2014-10-27 16:45:04 +0100

    [FIX] mass_mailing: new try to prevent google cache

--- addons/mass_mailing/controllers/main.py
+++ addons/mass_mailing/controllers/main.py
@@ -13,8 +13,7 @@ class MassMailController(http.Controller):
         mail_mail_stats = request.registry.get('mail.mail.statistics')
         mail_mail_stats.set_opened(request.cr, SUPERUSER_ID, mail_mail_ids=[mail_id])
         response = werkzeug.wrappers.Response()
-        response.headers['Content-Type'] = 'image/gif'
-        response.headers['Cache-Control'] = 'no-cache, max-age=0'
+        response.headers['Cache-Control'] = 'no-cache'
         response.data = 'R0lGODlhAQABAIAAANvf7wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='.decode('base64')
         return response
 
