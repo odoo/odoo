041ce44a912b72e0ca6b2aac6da2b0fe16857a90
Goffin Simon <simon.goffin@outlook.com>
2014-10-27 22:52:03 +0100

    [ADD] Test js to create a version.

--- addons/website_version/static/src/js/website_version.tour.js
+++ addons/website_version/static/src/js/website_version.tour.js
@@ -48,8 +48,15 @@
             {
                 title:     "Give a version name",
                 waitFor:   '.modal button[type="button"]:contains("Continue")',
+                element:   '.modal input[type="text"]',
+                sampleText: 'Test',
+            },
+
+            {
+                title:     "Validate the version name",
                 element:   '.modal button[type="button"]:contains("Continue")',
             },
+
 
             {
                 title:     "Confirm",
@@ -58,15 +65,71 @@
             },
 
             {
-                title:     "Finish",
+                title:     "You are on version Test",
                 waitNot:   '.modal button[type="button"]:contains("Ok")',
             },
 
-            // {
-            //     title:     "Click on Edit",
-            //     waitFor:   'button[data-action="edit"]:contains("Edit")',
-            //     element:   'button[data-action="edit"]:contains("Edit")',
-            // },
+            {
+                title:     "Click on Edit",
+                waitFor:   'button[data-action="edit"]:contains("Edit")',
+                element:   'button[data-action="edit"]:contains("Edit")',
+            },
+
+            {
+                element:   'button[data-action=snippet]',
+                placement: 'bottom',
+                title:     "Insert building blocks",
+                
+            },
+            {
+                snippet:   '#snippet_structure .oe_snippet:first',
+                placement: 'bottom',
+                title:     "Drag & Drop a Banner",
+            },
+            {
+                waitFor:   '.oe_overlay_options .oe_options:visible',
+                element:   '#wrap .carousel:first div.carousel-content',
+                placement: 'top',
+                title:     "Customize banner's text",
+                sampleText: 'Here, a customized text',
+            },
+            {
+                waitNot:   '#wrap .carousel:first div.carousel-content:has(h2:'+
+                    'containsExact('+'Your Banner Title'+')):has(h3:'+
+                    'containsExact('+'Click to customize this text'+'))',
+                element:   '.oe_snippet_parent:visible',
+                placement: 'bottom',
+                title:     "Get banner properties",
+            },
+            {
+                element:   '.oe_overlay_options .oe_options:visible',
+                placement: 'left',
+                title:     "Customize the banner",
+            },
+            {
+                waitNot:   '.popover.tour',
+                element:   'button[data-action=snippet]',
+                placement: 'bottom',
+                title:     "Add Another Block",
+            },
+            {
+                snippet:   '#snippet_structure .oe_snippet:eq(6)',
+                placement: 'bottom',
+                title:     "Drag & Drop This Block",
+            },
+            {
+                waitFor:   '.oe_overlay_options .oe_options:visible',
+                element:   'button[data-action=save]',
+                placement: 'right',
+                title:     "Save your modifications",
+            },
+            {
+                waitFor:   'button[data-action=save]:not(:visible)',
+                title:     "Good Job!",
+            },
+
+
+
             // {
             //     title:     'Click on discard',
             //     waitFor:   'button[data-action="save"]:contains("Save")',
