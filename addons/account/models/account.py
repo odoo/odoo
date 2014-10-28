c89cf0c825b1ab590dd8bb1ab90f6e674edea5ac
tpa-odoo <tpa@tinyerp.com>
2014-10-28 15:00:28 +0530

    [IMP] improved _compute method to new API.

--- addons/account/models/account.py
+++ addons/account/models/account.py
@@ -205,97 +205,25 @@ class account_account(models.Model):
     @api.multi
     def _compute(self, query='', query_params=()):
         """ compute the balance, debit and/or credit for the provided
-        account ids
         Arguments:
-        `ids`: account ids
-        `field_names`: the fields to compute (a list of any of
-                       'balance', 'debit' and 'credit')
-        `arg`: unused fields.function stuff
         `query`: additional query filter (as a string)
         `query_params`: parameters for the provided query string
                         (__compute will handle their escaping) as a
                         tuple
         """
-#         mapping = {
-#             'balance': "COALESCE(SUM(l.debit),0) - COALESCE(SUM(l.credit), 0) as balance",
-#             'debit': "COALESCE(SUM(l.debit), 0) as debit",
-#             'credit': "COALESCE(SUM(l.credit), 0) as credit",
-#             # by convention, foreign_balance is 0 when the account has no secondary currency, because the amounts may be in different currencies
-#             'foreign_balance': "(SELECT CASE WHEN currency_id IS NULL THEN 0 ELSE COALESCE(SUM(l.amount_currency), 0) END FROM account_account WHERE id IN (l.account_id)) as foreign_balance",
-#         }
-#         #get all the necessary accounts
-#         children_and_consolidated = self._get_children_and_consol(cr, uid, ids, context=context)
-#         #compute for each account the balance/debit/credit from the move lines
-#         accounts = {}
-#         res = {}
-#         null_result = dict((fn, 0.0) for fn in field_names)
-#         if children_and_consolidated:
-#             aml_query = self.pool.get('account.move.line')._query_get(cr, uid, context=context)
-# 
-#             wheres = [""]
-#             if query.strip():
-#                 wheres.append(query.strip())
-#             if aml_query.strip():
-#                 wheres.append(aml_query.strip())
-#             filters = " AND ".join(wheres)
-#             # IN might not work ideally in case there are too many
-#             # children_and_consolidated, in that case join on a
-#             # values() e.g.:
-#             # SELECT l.account_id as id FROM account_move_line l
-#             # INNER JOIN (VALUES (id1), (id2), (id3), ...) AS tmp (id)
-#             # ON l.account_id = tmp.id
-#             # or make _get_children_and_consol return a query and join on that
-#             request = ("SELECT l.account_id as id, " +\
-#                        ', '.join(mapping.values()) +
-#                        " FROM account_move_line l" \
-#                        " WHERE l.account_id IN %s " \
-#                             + filters +
-#                        " GROUP BY l.account_id")
-#             params = (tuple(children_and_consolidated),) + query_params
-#             cr.execute(request, params)
-# 
-#             for row in cr.dictfetchall():
-#                 accounts[row['id']] = row
-# 
-#             # consolidate accounts with direct children
-#             children_and_consolidated.reverse()
-#             brs = list(self.browse(cr, uid, children_and_consolidated, context=context))
-#             sums = {}
-#             currency_obj = self.pool.get('res.currency')
-#             while brs:
-#                 current = brs.pop(0)
-# #                can_compute = True
-# #                for child in current.child_id:
-# #                    if child.id not in sums:
-# #                        can_compute = False
-# #                        try:
-# #                            brs.insert(0, brs.pop(brs.index(child)))
-# #                        except ValueError:
-# #                            brs.insert(0, child)
-# #                if can_compute:
-# #                 for fn in field_names:
-# #                     sums.setdefault(current.id, {})[fn] = accounts.get(current.id, {}).get(fn, 0.0)
-# #                     for child in current.child_id:
-# #                         if child.company_id.currency_id.id == current.company_id.currency_id.id:
-# #                             sums[current.id][fn] += sums[child.id][fn]
-# #                         else:
-# #                             sums[current.id][fn] += currency_obj.compute(cr, uid, child.company_id.currency_id.id, current.company_id.currency_id.id, sums[child.id][fn], context=context)
-# 
-#                 # as we have to relay on values computed before this is calculated separately than previous fields
-#                 if current.currency_id and current.exchange_rate and \
-#                             ('adjusted_balance' in field_names or 'unrealized_gain_loss' in field_names):
-#                     # Computing Adjusted Balance and Unrealized Gains and losses
-#                     # Adjusted Balance = Foreign Balance / Exchange Rate
-#                     # Unrealized Gains and losses = Adjusted Balance - Balance
-#                     adj_bal = sums[current.id].get('foreign_balance', 0.0) / current.exchange_rate
-#                     sums[current.id].update({'adjusted_balance': adj_bal, 'unrealized_gain_loss': adj_bal - sums[current.id].get('balance', 0.0)})
-# 
-#             for id in ids:
-#                 res[id] = sums.get(id, null_result)
-#         else:
-#             for id in ids:
-#                 res[id] = null_result
-#         return res
+        field_names = ['balance', 'credit', 'debit', 'foreign_balance', 'adjusted_balance', 'unrealized_gain_loss']
+        mapping = {
+            'balance': "COALESCE(SUM(l.debit),0) - COALESCE(SUM(l.credit), 0) as balance",
+            'debit': "COALESCE(SUM(l.debit), 0) as debit",
+            'credit': "COALESCE(SUM(l.credit), 0) as credit",
+            # by convention, foreign_balance is 0 when the account has no secondary currency, because the amounts may be in different currencies
+            'foreign_balance': "(SELECT CASE WHEN currency_id IS NULL THEN 0 ELSE COALESCE(SUM(l.amount_currency), 0) END FROM account_account WHERE id IN (l.account_id)) as foreign_balance",
+        }
+        #get all the necessary accounts
+        children_and_consolidated = self._get_children_and_consol()
+        #compute for each account the balance/debit/credit from the move lines
+        accounts = {}
+        res = {}
         for account in self:
             account.balance = 0.0
             account.credit = 0.0
@@ -303,6 +231,74 @@ class account_account(models.Model):
             account.foreign_balance = 0.0
             account.adjusted_balance = 0.0
             account.unrealized_gain_loss = 0.0
+        if children_and_consolidated:
+            aml_query = self.env['account.move.line']._query_get()
+ 
+            wheres = [""]
+            if query.strip():
+                wheres.append(query.strip())
+            if aml_query.strip():
+                wheres.append(aml_query.strip())
+            filters = " AND ".join(wheres)
+            # IN might not work ideally in case there are too many
+            # children_and_consolidated, in that case join on a
+            # values() e.g.:
+            # SELECT l.account_id as id FROM account_move_line l
+            # INNER JOIN (VALUES (id1), (id2), (id3), ...) AS tmp (id)
+            # ON l.account_id = tmp.id
+            # or make _get_children_and_consol return a query and join on that
+            request = ("SELECT l.account_id as id, " +\
+                       ', '.join(mapping.values()) +
+                       " FROM account_move_line l" \
+                       " WHERE l.account_id IN %s " \
+                            + filters +
+                       " GROUP BY l.account_id")
+            params = (tuple(children_and_consolidated),) + query_params
+            cr.execute(request, params)
+ 
+            for row in cr.dictfetchall():
+                accounts[row['id']] = row
+ 
+            # consolidate accounts with direct children
+            children_and_consolidated.reverse()
+            brs = list(self.browse(children_and_consolidated))
+            sums = {}
+            while brs:
+                current = brs.pop(0)
+#                can_compute = True
+#                for child in current.child_id:
+#                    if child.id not in sums:
+#                        can_compute = False
+#                        try:
+#                            brs.insert(0, brs.pop(brs.index(child)))
+#                        except ValueError:
+#                            brs.insert(0, child)
+#                if can_compute:
+                for fn in field_names:
+                    sums.setdefault(current.id, {})[fn] = accounts.get(current.id, {}).get(fn, 0.0)
+                    for child in current.child_id:
+                        if child.company_id.currency_id.id == current.company_id.currency_id.id:
+                            sums[current.id][fn] += sums[child.id][fn]
+                        else:
+                            sums[current.id][fn] += self.env['res.currency'].compute(child.company_id.currency_id.id, current.company_id.currency_id.id, sums[child.id][fn])
+ 
+                # as we have to relay on values computed before this is calculated separately than previous fields
+                if current.currency_id and current.exchange_rate and \
+                            ('adjusted_balance' in field_names or 'unrealized_gain_loss' in field_names):
+                    # Computing Adjusted Balance and Unrealized Gains and losses
+                    # Adjusted Balance = Foreign Balance / Exchange Rate
+                    # Unrealized Gains and losses = Adjusted Balance - Balance
+                    adj_bal = sums[current.id].get('foreign_balance', 0.0) / current.exchange_rate
+                    sums[current.id].update({'adjusted_balance': adj_bal, 'unrealized_gain_loss': adj_bal - sums[current.id].get('balance', 0.0)})
+ 
+            for account in self:
+                if sums.get(account.id):
+                    account.balance = sums[account.id][balance]
+                    account.credit = sums[account.id][credit]
+                    account.debit = sums[account.id][debit]
+                    account.foreign_balance = sums[account.id][foreign_balance]
+                    account.adjusted_balance = sums[account.id][adjusted_balance]
+                    account.unrealized_gain_loss = sums[account.id][unrealized_gain_loss]
 
     @api.depends('company_id', 'company_id.currency_id')
     @api.multi
