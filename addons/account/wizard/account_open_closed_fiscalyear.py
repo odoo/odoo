b01fffb5726f5357957d06cf48a71379e4c75c39
tpa-odoo <tpa@tinyerp.com>
2014-10-29 18:02:55 +0530

    [IMP] account: convert account_open_closed_fiscalyear.py to new API

--- addons/account/wizard/account_open_closed_fiscalyear.py
+++ addons/account/wizard/account_open_closed_fiscalyear.py
@@ -1,48 +1,22 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+from openerp import models, api, _
+from openerp.exceptions import Warning
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
-
-class account_open_closed_fiscalyear(osv.osv_memory):
+class account_open_closed_fiscalyear(models.TransientModel):
     _name = "account.open.closed.fiscalyear"
     _description = "Choose Fiscal Year"
-    _columns = {
-       'fyear_id': fields.many2one('account.fiscalyear', \
-                                 'Fiscal Year', required=True, help='Select Fiscal Year which you want to remove entries for its End of year entries journal'),
-    }
 
-    def remove_entries(self, cr, uid, ids, context=None):
-        move_obj = self.pool.get('account.move')
+    fyear_id = fields.Many2one('account.fiscalyear', string='Fiscal Year', required=True, 
+        help='Select Fiscal Year which you want to remove entries for its End of year entries journal')
 
-        data = self.browse(cr, uid, ids, context=context)[0]
-        period_journal = data.fyear_id.end_journal_id or False
+    @api.multi
+    def remove_entries(self):
+        period_journal = self.fyear_id.end_journal_id or False
         if not period_journal:
-            raise osv.except_osv(_('Error!'), _("You have to set the 'End  of Year Entries Journal' for this Fiscal Year which is set after generating opening entries from 'Generate Opening Entries'."))
+            raise Warning(_("You have to set the 'End  of Year Entries Journal' for this Fiscal Year which is set after generating opening entries from 'Generate Opening Entries'."))
 
-        ids_move = move_obj.search(cr, uid, [('journal_id','=',period_journal.journal_id.id),('period_id','=',period_journal.period_id.id)])
+        ids_move = self.env['account.move'].search([('journal_id','=',period_journal.journal_id.id),('period_id','=',period_journal.period_id.id)])
         if ids_move:
-            cr.execute('delete from account_move where id IN %s', (tuple(ids_move),))
-            self.invalidate_cache(cr, uid, context=context)
+            self._cr.execute('delete from account_move where id IN %s', (tuple(ids_move.ids),))
+            self.invalidate_cache()
         return {'type': 'ir.actions.act_window_close'}
 
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
