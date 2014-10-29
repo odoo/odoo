0224eaf4f41f637be5ad5f251988acf2ef4c2e4f
mdi-odoo <mdi@tinyerp.com>
2014-10-29 12:08:12 +0530

    [IMP] account : Converted the 'account.fiscalyear.close.state' wizard to new API.

--- addons/account/wizard/account_fiscalyear_close_state.py
+++ addons/account/wizard/account_fiscalyear_close_state.py
@@ -1,51 +1,24 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+from openerp import models, fields, api
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
 
-class account_fiscalyear_close_state(osv.osv_memory):
+class account_fiscalyear_close_state(models.TransientModel):
     """
     Closes  Account Fiscalyear
     """
     _name = "account.fiscalyear.close.state"
     _description = "Fiscalyear Close state"
-    _columns = {
-       'fy_id': fields.many2one('account.fiscalyear', \
-                                 'Fiscal Year to Close', required=True, help="Select a fiscal year to close"),
-    }
 
-    def data_save(self, cr, uid, ids, context=None):
+    fy_id = fields.Many2one('account.fiscalyear', \
+        string='Fiscal Year to Close', required=True, help="Select a fiscal year to close")
+
+    @api.multi
+    def data_save(self):
         """
         This function close account fiscalyear
-        @param cr: the current row, from the database cursor,
-        @param uid: the current user’s ID for security checks,
-        @param ids: List of Account fiscalyear close state’s IDs
-
         """
-        journal_period_obj = self.pool.get('account.journal.period')
-        period_obj = self.pool.get('account.period')
-        fiscalyear_obj = self.pool.get('account.fiscalyear')
 
-        for data in  self.read(cr, uid, ids, context=context):
+        for data in  self.read():
             fy_id = data['fy_id'][0]
 
 #             cr.execute('UPDATE account_journal_period ' \
@@ -53,13 +26,10 @@ class account_fiscalyear_close_state(osv.osv_memory):
 #                         'WHERE period_id IN (SELECT id FROM account_period \
 #                         WHERE fiscalyear_id = %s)',
 #                     ('done', fy_id))
-            cr.execute('UPDATE account_period SET state = %s ' \
+            self._cr.execute('UPDATE account_period SET state = %s ' \
                     'WHERE fiscalyear_id = %s', ('done', fy_id))
-            cr.execute('UPDATE account_fiscalyear ' \
+            self._cr.execute('UPDATE account_fiscalyear ' \
                     'SET state = %s WHERE id = %s', ('done', fy_id))
-            self.invalidate_cache(cr, uid, context=context)
+            self.invalidate_cache()
 
             return {'type': 'ir.actions.act_window_close'}
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
