bd7f83eb1541f7b4c9a73082623811a9cd1d9b0b
tpa-odoo <tpa@tinyerp.com>
2014-10-29 12:40:02 +0530

    [IMP] account: convert account_statement_from_invoice.py to new API

--- addons/account/wizard/account_statement_from_invoice.py
+++ addons/account/wizard/account_statement_from_invoice.py
@@ -1,60 +1,31 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 import time
+from openerp import models, fields, api, _
 
-from openerp.osv import fields, osv
-
-class account_statement_from_invoice_lines(osv.osv_memory):
+class account_statement_from_invoice_lines(models.TransientModel):
     """
     Generate Entries by Statement from Invoices
     """
     _name = "account.statement.from.invoice.lines"
     _description = "Entries by Statement from Invoices"
-    _columns = {
-        'line_ids': fields.many2many('account.move.line', 'account_move_line_relation', 'move_id', 'line_id', 'Invoices'),
-    }
 
-    def populate_statement(self, cr, uid, ids, context=None):
-        context = dict(context or {})
-        statement_id = context.get('statement_id', False)
+    line_ids = fields.many2many('account.move.line', string='Invoices')
+
+    @api.multi
+    def populate_statement(self):
+        statement_id = self._context.get('statement_id', False)
         if not statement_id:
             return {'type': 'ir.actions.act_window_close'}
-        data = self.read(cr, uid, ids, context=context)[0]
-        line_ids = data['line_ids']
-        if not line_ids:
+        if not self.line_ids:
             return {'type': 'ir.actions.act_window_close'}
 
-        line_obj = self.pool.get('account.move.line')
-        statement_obj = self.pool.get('account.bank.statement')
-        statement_line_obj = self.pool.get('account.bank.statement.line')
-        currency_obj = self.pool.get('res.currency')
-        line_date = time.strftime('%Y-%m-%d')
-        statement = statement_obj.browse(cr, uid, statement_id, context=context)
+        CurrencyObj = self.env['res.currency']
+        statement = self.env['account.bank.statement'].browse(statement_id)
 
         # for each selected move lines
-        for line in line_obj.browse(cr, uid, line_ids, context=context):
-            ctx = context.copy()
+        for line in self.line_ids:
+            ctx = self._context.copy()
             #  take the date for computation of currency => use payment date
-            ctx['date'] = line_date
+            ctx['date'] = time.strftime('%Y-%m-%d')
             amount = 0.0
 
             if line.debit > 0:
@@ -63,24 +34,22 @@ class account_statement_from_invoice_lines(osv.osv_memory):
                 amount = -line.credit
 
             if line.amount_currency:
-                amount = currency_obj.compute(cr, uid, line.currency_id.id,
-                    statement.currency.id, line.amount_currency, context=ctx)
+                amount = CurrencyObj.with_context(ctx).compute(line.currency_id.id,
+                    statement.currency.id, line.amount_currency)
             elif (line.invoice and line.invoice.currency_id.id != statement.currency.id):
-                amount = currency_obj.compute(cr, uid, line.invoice.currency_id.id,
-                    statement.currency.id, amount, context=ctx)
+                amount = CurrencyObj.with_context(ctx).compute(line.invoice.currency_id.id,
+                    statement.currency.id, amount)
 
+            context = self._context.copy()
             context.update({'move_line_ids': [line.id],
                             'invoice_id': line.invoice.id})
 
-            statement_line_obj.create(cr, uid, {
+            self.env['account.bank.statement.line'].with_context(context).create({
                 'name': line.name or '?',
                 'amount': amount,
                 'partner_id': line.partner_id.id,
                 'statement_id': statement_id,
                 'ref': line.ref,
                 'date': statement.date,
-            }, context=context)
+            })
         return {'type': 'ir.actions.act_window_close'}
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
