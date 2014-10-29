c4d07721bbfa56e687f38be3c9ac63fdb49a8c1b
tpa-odoo <tpa@tinyerp.com>
2014-10-29 15:59:36 +0530

    [IMP] account: convert account_report_common.py to new API

--- addons/account/wizard/account_report_common.py
+++ addons/account/wizard/account_report_common.py
@@ -1,80 +1,60 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
 import time
 from lxml import etree
 
-from openerp.osv import fields, osv
+from openerp import models, api, _
+from openerp.exceptions import Warning
 from openerp.osv.orm import setup_modifiers
-from openerp.tools.translate import _
 
-class account_common_report(osv.osv_memory):
+class account_common_report(models.TransientModel):
     _name = "account.common.report"
     _description = "Account Common Report"
 
-    def onchange_chart_id(self, cr, uid, ids, chart_account_id=False, context=None):
+    @api.multi
+    def onchange_chart_id(self, chart_account_id=False):
         res = {}
         if chart_account_id:
-            company_id = self.pool.get('account.account').browse(cr, uid, chart_account_id, context=context).company_id.id
+            company_id = self.env['account.account'].browse(chart_account_id).company_id.id
             now = time.strftime('%Y-%m-%d')
             domain = [('company_id', '=', company_id), ('date_start', '<', now), ('date_stop', '>', now)]
-            fiscalyears = self.pool.get('account.fiscalyear').search(cr, uid, domain, limit=1)
+            fiscalyears = self.env['account.fiscalyear'].search(domain, limit=1)
             res['value'] = {'company_id': company_id, 'fiscalyear_id': fiscalyears and fiscalyears[0] or False}
         return res
 
-    _columns = {
-        'chart_account_id': fields.many2one('account.account', 'Chart of Account', help='Select Charts of Accounts', required=True, domain = [('parent_id','=',False), ('deprecated', '=', False)]),
-        'company_id': fields.related('chart_account_id', 'company_id', type='many2one', relation='res.company', string='Company', readonly=True),
-        'fiscalyear_id': fields.many2one('account.fiscalyear', 'Fiscal Year', help='Keep empty for all open fiscal year'),
-        'filter': fields.selection([('filter_no', 'Do not apply filter'), ('filter_date', 'Filter by date'), ('filter_period', 'Filter by period')], "Filter by", required=True),
-        'period_from': fields.many2one('account.period', 'Start Period'),
-        'period_to': fields.many2one('account.period', 'End Period'),
-        'journal_ids': fields.many2many('account.journal', string='Journals', required=True),
-        'date_from': fields.date("Start Date"),
-        'date_to': fields.date("End Date"),
-        'target_move': fields.selection([('posted', 'All Posted Entries'),
-                                         ('all', 'All Entries'),
-                                        ], 'Target Moves', required=True),
+    chart_account_id = fields.Many2one('account.account', string='Chart of Account', 
+        help='Select Charts of Accounts', default=lambda self: self._get_account(),
+        required=True, domain = [('parent_id','=',False), ('deprecated', '=', False)])
+    company_id = fields.Many2one('res.company', related='chart_account_id.company_id',
+        string='Company', readonly=True, default=lambda self: self.env['res.company']._company_default_get('account.common.report'))
+    fiscalyear_id = fields.Many2one('account.fiscalyear', string='Fiscal Year', 
+        help='Keep empty for all open fiscal year', default=lambda self: self._get_fiscalyear())
+    filter = fields.Selection([('filter_no', 'Do not apply filter'), ('filter_date', 'Filter by date'), ('filter_period', 'Filter by period')], 
+        string='Filter by', required=True, default='filter_no')
+    period_from = fields.Many2one('account.period', string='Start Period')
+    period_to = fields.Many2one('account.period', string='End Period')
+    journal_ids = fields.Many2many('account.journal', string='Journals', required=True, 
+        default=lambda self: self._get_all_journal())
+    date_from = fields.Date(string='Start Date')
+    date_to = fields.Date(string='End Date')
+    target_move = fields.Selection([('posted', 'All Posted Entries'), ('all', 'All Entries'), ],
+        string='Target Moves', required=True, default='posted')
 
-        }
-
-    def _check_company_id(self, cr, uid, ids, context=None):
-        for wiz in self.browse(cr, uid, ids, context=context):
+    @api.multi
+    @api.constrains('chart_account_id','fiscalyear_id','period_from','period_to')
+    def _check_company_id(self):
+        for wiz in self:
             company_id = wiz.company_id.id
             if wiz.fiscalyear_id and company_id != wiz.fiscalyear_id.company_id.id:
-                return False
+                raise Warning(_('The fiscalyear, periods or chart of account chosen have to belong to the same company.'))
             if wiz.period_from and company_id != wiz.period_from.company_id.id:
-                return False
+                raise Warning(_('The fiscalyear, periods or chart of account chosen have to belong to the same company.'))
             if wiz.period_to and company_id != wiz.period_to.company_id.id:
-                return False
-        return True
+                raise Warning(_('The fiscalyear, periods or chart of account chosen have to belong to the same company.'))
 
-    _constraints = [
-        (_check_company_id, 'The fiscalyear, periods or chart of account chosen have to belong to the same company.', ['chart_account_id','fiscalyear_id','period_from','period_to']),
-    ]
-
-    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
-        if context is None:context = {}
-        res = super(account_common_report, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar, submenu=False)
-        if context.get('active_model', False) == 'account.account':
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        res = super(account_common_report, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=False)
+        if self._context.get('active_model', False) == 'account.account':
             doc = etree.XML(res['arch'])
             nodes = doc.xpath("//field[@name='chart_account_id']")
             for node in nodes:
@@ -84,7 +64,8 @@ class account_common_report(osv.osv_memory):
             res['arch'] = etree.tostring(doc)
         return res
 
-    def onchange_filter(self, cr, uid, ids, filter='filter_no', fiscalyear_id=False, context=None):
+    @api.multi
+    def onchange_filter(self, filter='filter_no', fiscalyear_id=False):
         res = {'value': {}}
         if filter == 'filter_no':
             res['value'] = {'period_from': False, 'period_to': False, 'date_from': False ,'date_to': False}
@@ -92,7 +73,7 @@ class account_common_report(osv.osv_memory):
             res['value'] = {'period_from': False, 'period_to': False, 'date_from': time.strftime('%Y-01-01'), 'date_to': time.strftime('%Y-%m-%d')}
         if filter == 'filter_period' and fiscalyear_id:
             start_period = end_period = False
-            cr.execute('''
+            self._cr.execute('''
                 SELECT * FROM (SELECT p.id
                                FROM account_period p
                                LEFT JOIN account_fiscalyear f ON (p.fiscalyear_id = f.id)
@@ -109,47 +90,37 @@ class account_common_report(osv.osv_memory):
                                AND p.special = false
                                ORDER BY p.date_stop DESC
                                LIMIT 1) AS period_stop''', (fiscalyear_id, fiscalyear_id))
-            periods =  [i[0] for i in cr.fetchall()]
+            periods =  [i[0] for i in self._cr.fetchall()]
             if periods and len(periods) > 1:
                 start_period = periods[0]
                 end_period = periods[1]
             res['value'] = {'period_from': start_period, 'period_to': end_period, 'date_from': False, 'date_to': False}
         return res
 
-    def _get_account(self, cr, uid, context=None):
-        user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
-        accounts = self.pool.get('account.account').search(cr, uid, [('parent_id', '=', False), ('company_id', '=', user.company_id.id)], limit=1)
+    @api.model
+    def _get_account(self):
+        accounts = self.env['account.account'].search([('parent_id', '=', False), ('company_id', '=', self.env.user.company_id.id)], limit=1)
         return accounts and accounts[0] or False
 
-    def _get_fiscalyear(self, cr, uid, context=None):
-        if context is None:
-            context = {}
+    @api.model
+    def _get_fiscalyear(self):
         now = time.strftime('%Y-%m-%d')
         company_id = False
-        ids = context.get('active_ids', [])
-        if ids and context.get('active_model') == 'account.account':
-            company_id = self.pool.get('account.account').browse(cr, uid, ids[0], context=context).company_id.id
+        ids = self._context.get('active_ids', [])
+        if ids and self._context.get('active_model') == 'account.account':
+            company_id = self.env['account.account'].browse(ids[0]).company_id.id
         else:  # use current company id
-            company_id = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.id
+            company_id = self.env.user.company_id.id
         domain = [('company_id', '=', company_id), ('date_start', '<', now), ('date_stop', '>', now)]
-        fiscalyears = self.pool.get('account.fiscalyear').search(cr, uid, domain, limit=1)
+        fiscalyears = self.env['account.fiscalyear'].search(domain, limit=1)
         return fiscalyears and fiscalyears[0] or False
 
-    def _get_all_journal(self, cr, uid, context=None):
-        return self.pool.get('account.journal').search(cr, uid ,[])
+    @api.model
+    def _get_all_journal(self):
+        return self.env['account.journal'].search([])
 
-    _defaults = {
-            'fiscalyear_id': _get_fiscalyear,
-            'company_id': lambda self,cr,uid,c: self.pool.get('res.company')._company_default_get(cr, uid, 'account.common.report',context=c),
-            'journal_ids': _get_all_journal,
-            'filter': 'filter_no',
-            'chart_account_id': _get_account,
-            'target_move': 'posted',
-    }
-
-    def _build_contexts(self, cr, uid, ids, data, context=None):
-        if context is None:
-            context = {}
+    @api.multi
+    def _build_contexts(self, data):
         result = {}
         result['fiscalyear'] = 'fiscalyear_id' in data['form'] and data['form']['fiscalyear_id'] or False
         result['journal_ids'] = 'journal_ids' in data['form'] and data['form']['journal_ids'] or False
@@ -160,28 +131,26 @@ class account_common_report(osv.osv_memory):
             result['date_to'] = data['form']['date_to']
         elif data['form']['filter'] == 'filter_period':
             if not data['form']['period_from'] or not data['form']['period_to']:
-                raise osv.except_osv(_('Error!'),_('Select a starting and an ending period.'))
+                raise Warning(_('Select a starting and an ending period.'))
             result['period_from'] = data['form']['period_from']
             result['period_to'] = data['form']['period_to']
         return result
 
-    def _print_report(self, cr, uid, ids, data, context=None):
-        raise (_('Error!'), _('Not implemented.'))
+    @api.multi
+    def _print_report(self, data):
+        raise Warning(_('Not implemented.'))
 
-    def check_report(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
+    @api.multi
+    def check_report(self):
         data = {}
-        data['ids'] = context.get('active_ids', [])
-        data['model'] = context.get('active_model', 'ir.ui.menu')
-        data['form'] = self.read(cr, uid, ids, ['date_from',  'date_to',  'fiscalyear_id', 'journal_ids', 'period_from', 'period_to',  'filter',  'chart_account_id', 'target_move'], context=context)[0]
+        data['ids'] = self._context.get('active_ids', [])
+        data['model'] = self._context.get('active_model', 'ir.ui.menu')
+        data['form'] = self.read(self.ids, ['date_from',  'date_to',  'fiscalyear_id', 'journal_ids', 'period_from', 'period_to',  'filter',  'chart_account_id', 'target_move'])[0]
         for field in ['fiscalyear_id', 'chart_account_id', 'period_from', 'period_to']:
             if isinstance(data['form'][field], tuple):
                 data['form'][field] = data['form'][field][0]
-        used_context = self._build_contexts(cr, uid, ids, data, context=context)
+        used_context = self._build_contexts(data)
         data['form']['periods'] = used_context.get('periods', False) and used_context['periods'] or []
-        data['form']['used_context'] = dict(used_context, lang=context.get('lang', 'en_US'))
-        return self._print_report(cr, uid, ids, data, context=context)
+        data['form']['used_context'] = dict(used_context, lang=self._context.get('lang', 'en_US'))
+        return self._print_report(data)
 
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
