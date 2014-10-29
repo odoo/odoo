86c5415632e3e97a7c6aa582050892b32ff63dbf
tpa-odoo <tpa@tinyerp.com>
2014-10-29 14:49:15 +0530

    [IMP] account: convert account_report_partner_balance.py to new API

--- addons/account/wizard/account_report_partner_balance.py
+++ addons/account/wizard/account_report_partner_balance.py
@@ -1,49 +1,20 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-from openerp.osv import fields, osv
+from openerp import models, api
 
 
-class account_partner_balance(osv.osv_memory):
+class account_partner_balance(models.TransientModel):
     """
         This wizard will provide the partner balance report by periods, between any two dates.
     """
     _inherit = 'account.common.partner.report'
     _name = 'account.partner.balance'
     _description = 'Print Account Partner Balance'
-    _columns = {
-        'display_partner': fields.selection([('non-zero_balance', 'Display partners with balance is not equal to 0'), ('all', 'Display all partners')]
-                                    ,'Display Partners'),
-        'journal_ids': fields.many2many('account.journal', 'account_partner_balance_journal_rel', 'account_id', 'journal_id', 'Journals', required=True),
-    }
 
-    _defaults = {
-        'display_partner': 'non-zero_balance',
-    }
+    display_partner = fields.Selection([('non-zero_balance', 'Display partners with balance is not equal to 0'), ('all', 'Display all partners')],
+        default='non-zero_balance' ,string='Display Partners')
+    journal_ids = fields.Many2many('account.journal', string='Journals', required=True)
 
-    def _print_report(self, cr, uid, ids, data, context=None):
-        if context is None:
-            context = {}
-        data = self.pre_print_report(cr, uid, ids, data, context=context)
-        data['form'].update(self.read(cr, uid, ids, ['display_partner'])[0])
-        return self.pool['report'].get_action(cr, uid, [], 'account.report_partnerbalance', data=data, context=context)
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    @api.multi
+    def _print_report(self, data):
+        data = self.pre_print_report(data)
+        data['form'].update(self.read(self.ids, ['display_partner'])[0])
+        return self.pool['report'].get_action([], 'account.report_partnerbalance', data=data)
