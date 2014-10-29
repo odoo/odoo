31bb18f4146f0574e9a4a087e1a131a5e87fc81f
mdi-odoo <mdi@tinyerp.com>
2014-10-29 17:52:18 +0530

    [IMP] account : Converted the 'account.invoice.refund' wizard to new API.

--- addons/account/wizard/account_invoice_refund.py
+++ addons/account/wizard/account_invoice_refund.py
@@ -1,121 +1,88 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 import time
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning
 
-class account_invoice_refund(osv.osv_memory):
+class account_invoice_refund(models.TransientModel):
 
     """Refunds invoice"""
 
     _name = "account.invoice.refund"
     _description = "Invoice Refund"
-    _columns = {
-       'date': fields.date('Date', help='This date will be used as the invoice date for credit note and period will be chosen accordingly!'),
-       'period': fields.many2one('account.period', 'Force period'),
-       'journal_id': fields.many2one('account.journal', 'Refund Journal', help='You can select here the journal to use for the credit note that will be created. If you leave that field empty, it will use the same journal as the current invoice.'),
-       'description': fields.char('Reason', required=True),
-       'filter_refund': fields.selection([('refund', 'Create a draft refund'), ('cancel', 'Cancel: create refund and reconcile'),('modify', 'Modify: create refund, reconcile and create a new draft invoice')], "Refund Method", required=True, help='Refund base on this type. You can not Modify and Cancel if the invoice is already reconciled'),
-    }
 
-    def _get_journal(self, cr, uid, context=None):
-        obj_journal = self.pool.get('account.journal')
-        user_obj = self.pool.get('res.users')
-        if context is None:
-            context = {}
+    date = fields.Date(string='Date', help='This date will be used as the invoice date for credit note and period will be chosen accordingly!',
+        default=lambda *a: time.strftime('%Y-%m-%d'))
+    period = fields.Many2one('account.period', string='Force period')
+    journal_id = fields.Many2one('account.journal', string='Refund Journal', default=lambda self: self._get_journal(),
+        help='You can select here the journal to use for the credit note that will be created. If you leave that field empty, it will use the same journal as the current invoice.')
+    description = fields.Char(string='Reason', required=True, default=lambda self: self._get_reason())
+    filter_refund = fields.Selection([('refund', 'Create a draft refund'), ('cancel', 'Cancel: create refund and reconcile'), ('modify', 'Modify: create refund, reconcile and create a new draft invoice')],
+        default='refund', string='Refund Method', required=True, help='Refund base on this type. You can not Modify and Cancel if the invoice is already reconciled')
+
+    @api.model
+    def _get_journal(self):
+        context = dict(self._context or {})
         inv_type = context.get('type', 'out_invoice')
-        company_id = user_obj.browse(cr, uid, uid, context=context).company_id.id
+        company_id = self.env.user.company_id.id
         type = (inv_type == 'out_invoice') and 'sale_refund' or \
                (inv_type == 'out_refund') and 'sale' or \
                (inv_type == 'in_invoice') and 'purchase_refund' or \
                (inv_type == 'in_refund') and 'purchase'
-        journal = obj_journal.search(cr, uid, [('type', '=', type), ('company_id','=',company_id)], limit=1, context=context)
+        journal = self.env['account.journal'].search([('type', '=', type), ('company_id', '=', company_id)], limit=1)
         return journal and journal[0] or False
 
-    def _get_reason(self, cr, uid, context=None):
-        active_id = context and context.get('active_id', False)
+    @api.model
+    def _get_reason(self):
+        context = dict(self._context or {})
+        active_id = context.get('active_id', False)
         if active_id:
-            inv = self.pool.get('account.invoice').browse(cr, uid, active_id, context=context)
+            inv = self.env['account.invoice'].browse(active_id)
             return inv.name
         else:
             return ''
 
-    _defaults = {
-        'date': lambda *a: time.strftime('%Y-%m-%d'),
-        'journal_id': _get_journal,
-        'filter_refund': 'refund',
-        'description': _get_reason,
-    }
-
-    def fields_view_get(self, cr, uid, view_id=None, view_type=False, context=None, toolbar=False, submenu=False):
-        journal_obj = self.pool.get('account.journal')
-        user_obj = self.pool.get('res.users')
+    @api.model
+    def fields_view_get(self, view_id=None, view_type=False, toolbar=False, submenu=False):
+        journal_obj = self.env['account.journal']
         # remove the entry with key 'form_view_ref', otherwise fields_view_get crashes
-        context = dict(context or {})
+        context = dict(self._context or {})
         context.pop('form_view_ref', None)
-        res = super(account_invoice_refund,self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar, submenu=submenu)
+        res = super(account_invoice_refund, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
         type = context.get('type', 'out_invoice')
-        company_id = user_obj.browse(cr, uid, uid, context=context).company_id.id
+        company_id = self.env.user.company_id.id
         journal_type = (type == 'out_invoice') and 'sale_refund' or \
                        (type == 'out_refund') and 'sale' or \
                        (type == 'in_invoice') and 'purchase_refund' or \
                        (type == 'in_refund') and 'purchase'
         for field in res['fields']:
             if field == 'journal_id':
-                journal_select = journal_obj._name_search(cr, uid, '', [('type', '=', journal_type), ('company_id','child_of',[company_id])], context=context)
+                journal_select = journal_obj._name_search('', [('type', '=', journal_type), ('company_id', 'child_of', [company_id])])
                 res['fields'][field]['selection'] = journal_select
         return res
 
-    def compute_refund(self, cr, uid, ids, mode='refund', context=None):
-        """
-        @param cr: the current row, from the database cursor,
-        @param uid: the current user’s ID for security checks,
-        @param ids: the account invoice refund’s ID or list of IDs
+    @api.multi
+    def compute_refund(self, mode='refund'):
+        inv_obj = self.env['account.invoice']
+        account_m_line_obj = self.env['account.move.line']
+        act_obj = self.env['ir.actions.act_window']
+        inv_tax_obj = self.env['account.invoice.tax']
+        inv_line_obj = self.env['account.invoice.line']
+        context = dict(self._context or {})
+        cr = self._cr
 
-        """
-        inv_obj = self.pool.get('account.invoice')
-        reconcile_obj = self.pool.get('account.move.reconcile')
-        account_m_line_obj = self.pool.get('account.move.line')
-        mod_obj = self.pool.get('ir.model.data')
-        act_obj = self.pool.get('ir.actions.act_window')
-        inv_tax_obj = self.pool.get('account.invoice.tax')
-        inv_line_obj = self.pool.get('account.invoice.line')
-        res_users_obj = self.pool.get('res.users')
-        if context is None:
-            context = {}
-
-        for form in self.browse(cr, uid, ids, context=context):
+        for form in self:
             created_inv = []
             date = False
             period = False
             description = False
-            company = res_users_obj.browse(cr, uid, uid, context=context).company_id
+            company = self.env.user.company_id
             journal_id = form.journal_id.id
-            for inv in inv_obj.browse(cr, uid, context.get('active_ids'), context=context):
+            for inv in inv_obj.browse(context.get('active_ids')):
                 if inv.state in ['draft', 'proforma2', 'cancel']:
-                    raise osv.except_osv(_('Error!'), _('Cannot %s draft/proforma/cancel invoice.') % (mode))
+                    raise Warning(_('Cannot %s draft/proforma/cancel invoice.') % (mode))
                 if inv.reconciled and mode in ('cancel', 'modify'):
-                    raise osv.except_osv(_('Error!'), _('Cannot %s invoice which is already reconciled, invoice should be unreconciled first. You can only refund this invoice.') % (mode))
+                    raise Warning(_('Cannot %s invoice which is already reconciled, invoice should be unreconciled first. You can only refund this invoice.') % (mode))
                 if form.period.id:
                     period = form.period.id
                 else:
@@ -150,14 +117,12 @@ class account_invoice_refund(osv.osv_memory):
                     description = inv.name
 
                 if not period:
-                    raise osv.except_osv(_('Insufficient Data!'), \
-                                            _('No period found on the invoice.'))
+                    raise Warning(_('No period found on the invoice.'))
 
-                refund_id = inv_obj.refund(cr, uid, [inv.id], date, period, description, journal_id, context=context)
-                refund = inv_obj.browse(cr, uid, refund_id[0], context=context)
-                inv_obj.write(cr, uid, [refund.id], {'date_due': date,
-                                                'check_total': inv.check_total})
-                inv_obj.button_compute(cr, uid, refund_id)
+                refund_id = inv.refund(date, period, description, journal_id)
+                refund = inv_obj.browse(refund_id[0])
+                refund.write({'date_due': date, 'check_total': inv.check_total})
+                refund.button_compute()
 
                 created_inv.append(refund_id[0])
                 if mode in ('cancel', 'modify'):
@@ -169,7 +134,7 @@ class account_invoice_refund(osv.osv_memory):
                         if line.reconcile_id:
                             line.reconcile_id.unlink()
                     refund.signal_workflow('invoice_open')
-                    refund = inv_obj.browse(cr, uid, refund_id[0], context=context)
+                    refund = inv_obj.browse(refund_id[0])
                     for tmpline in  refund.move_id.line_id:
                         if tmpline.account_id.id == inv.account_id.id:
                             to_reconcile_ids[tmpline.account_id.id].append(tmpline.id)
@@ -180,7 +145,7 @@ class account_invoice_refund(osv.osv_memory):
                                         writeoff_acc_id=inv.account_id.id
                                         )
                     if mode == 'modify':
-                        invoice = inv_obj.read(cr, uid, [inv.id],
+                        invoice = inv.read(
                                     ['name', 'type', 'number', 'reference',
                                     'comment', 'date_due', 'partner_id',
                                     'partner_insite', 'partner_contact',
@@ -189,10 +154,10 @@ class account_invoice_refund(osv.osv_memory):
                                     'journal_id', 'period_id'], context=context)
                         invoice = invoice[0]
                         del invoice['id']
-                        invoice_lines = inv_line_obj.browse(cr, uid, invoice['invoice_line'], context=context)
-                        invoice_lines = inv_obj._refund_cleanup_lines(cr, uid, invoice_lines, context=context)
-                        tax_lines = inv_tax_obj.browse(cr, uid, invoice['tax_line'], context=context)
-                        tax_lines = inv_obj._refund_cleanup_lines(cr, uid, tax_lines, context=context)
+                        invoice_lines = inv_line_obj.browse(invoice['invoice_line'])
+                        invoice_lines = inv_obj._refund_cleanup_lines(invoice_lines)
+                        tax_lines = inv_tax_obj.browse(invoice['tax_line'])
+                        tax_lines = inv_obj._refund_cleanup_lines(tax_lines)
                         invoice.update({
                             'type': inv.type,
                             'date_invoice': date,
@@ -206,30 +171,27 @@ class account_invoice_refund(osv.osv_memory):
                         for field in ('partner_id', 'account_id', 'currency_id',
                                          'payment_term', 'journal_id'):
                                 invoice[field] = invoice[field] and invoice[field][0]
-                        inv_id = inv_obj.create(cr, uid, invoice, {})
+                        inv_id = inv_obj.create(invoice)
                         if inv.payment_term.id:
-                            data = inv_obj.onchange_payment_term_date_invoice(cr, uid, [inv_id], inv.payment_term.id, date)
+                            data = inv_id.onchange_payment_term_date_invoice(inv.payment_term.id, date)
                             if 'value' in data and data['value']:
-                                inv_obj.write(cr, uid, [inv_id], data['value'])
-                        created_inv.append(inv_id)
+                                inv_id.write(data['value'])
+                        created_inv.append(inv_id.id)
 
             xml_id = (inv.type == 'out_refund') and 'action_invoice_tree1' or \
                      (inv.type == 'in_refund') and 'action_invoice_tree2' or \
                      (inv.type == 'out_invoice') and 'action_invoice_tree3' or \
                      (inv.type == 'in_invoice') and 'action_invoice_tree4'
-            result = mod_obj.get_object_reference(cr, uid, 'account', xml_id)
-            id = result and result[1] or False
+            str1 = 'account.%s' %(var)
+            result = self.env.ref('account.%s' %(xml_id))
 
-            result = act_obj.read(cr, uid, [id], context=context)[0]
+            result = act_obj.read([result])[0]
             invoice_domain = eval(result['domain'])
             invoice_domain.append(('id', 'in', created_inv))
             result['domain'] = invoice_domain
             return result
 
-    def invoice_refund(self, cr, uid, ids, context=None):
-        data_refund = self.read(cr, uid, ids, ['filter_refund'],context=context)[0]['filter_refund']
-        return self.compute_refund(cr, uid, ids, data_refund, context=context)
-
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    @api.multi
+    def invoice_refund(self):
+        data_refund = self.read(['filter_refund'])[0]['filter_refund']
+        return self.compute_refund(data_refund)
