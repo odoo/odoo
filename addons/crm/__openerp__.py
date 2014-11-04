4b94a58bf8a212bcd4baf58cda25ee8b7800ca4c
Jusab Sida <jsi@openerp.com>
2014-11-04 14:01:43 +0530

    [IMP]CRM: removing comments.

--- addons/crm/__openerp__.py
+++ addons/crm/__openerp__.py
@@ -103,8 +103,8 @@ Dashboard for CRM will include:
     ],
     'test': [
         'test/crm_access_group_users.yml',
-        #'test/crm_lead_message.yml',
-        #'test/lead2opportunity2win.yml',
+        'test/crm_lead_message.yml',
+        'test/lead2opportunity2win.yml',
         'test/lead2opportunity_assign_salesmen.yml',
         'test/crm_lead_merge.yml',
         'test/crm_lead_cancel.yml',
