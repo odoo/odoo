eb43fbe248128ceb45124ec9b516a91a700e76b6
Aaron Bohy <aab@odoo.com>
2014-11-05 10:03:26 +0100

    [FIX] ean13 field in res.partner and res.users renamed in Barcode (only in views) + no more check_ean done on these fields

--- addons/point_of_sale/res_partner.py
+++ addons/point_of_sale/res_partner.py
@@ -9,16 +9,17 @@ import openerp.addons.product.product
 class res_users(osv.osv):
     _inherit = 'res.partner'
     _columns = {
-        'ean13' : fields.char('EAN13', size=13, help="BarCode"),
+        # 'ean13' : fields.char('EAN13', size=13, help="BarCode"),
+        'ean13' : fields.char('Barcode', help="BarCode"),
     }
 
-    def _check_ean(self, cr, uid, ids, context=None):
-        return all(
-            openerp.addons.product.product.check_ean(user.ean13) == True
-            for user in self.browse(cr, uid, ids, context=context)
-        )
+    # def _check_ean(self, cr, uid, ids, context=None):
+    #     return all(
+    #         openerp.addons.product.product.check_ean(user.ean13) == True
+    #         for user in self.browse(cr, uid, ids, context=context)
+    #     )
 
-    _constraints = [
-        (_check_ean, "Error: Invalid ean code", ['ean13'],),
-    ]
+    # _constraints = [
+    #     (_check_ean, "Error: Invalid ean code", ['ean13'],),
+    # ]
 
