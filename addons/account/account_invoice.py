670c6b4d14b737fa3d8e5d867b503e54fc1e1cb2
Mohammad Alhashash <alhashash@alhashash.net>
2014-11-06 22:47:15 +0200

    [FIX] Fixes incorrect merge 3e1d5be951fb8
    Merge conflict at account/account_invoice.py (commit ab797f0cd8)
    was incorrectly resolved. As a result, cannot validate invoices with 
    tax line.

--- addons/account/account_invoice.py
+++ addons/account/account_invoice.py
@@ -1768,7 +1768,7 @@ class account_invoice_tax(osv.osv):
                 if not val.get('account_analytic_id') and line.account_analytic_id and val['account_id'] == line.account_id.id:
                     val['account_analytic_id'] = line.account_analytic_id.id
 
-                key = (val['tax_code_id'], val['base_code_id'], val['account_id'], val['account_analytic_id'])
+                key = (val['tax_code_id'], val['base_code_id'], val['account_id'])
                 if not key in tax_grouped:
                     tax_grouped[key] = val
                 else:
