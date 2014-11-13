3fbcc35dd6afa089944eea6649f848fda764e8a9
tpa-odoo <tpa@tinyerp.com>
2014-11-13 18:48:21 +0530

    [FIX] renamed field to rule_type from set_type. otherwise it will give error when we will apply value from configaration view.

--- addons/inter_company_rules/models/res_config.py
+++ addons/inter_company_rules/models/res_config.py
@@ -6,7 +6,7 @@ class inter_company_rules_configuration(models.TransientModel):
     company_id = fields.Many2one('res.company', string='Select Company',
         default=lambda self: self.env['res.company']._company_default_get('base.config.settings'),
         help='Select company to setup Inter company rules.')
-    set_type = fields.Selection([('so_and_po', 'SO and PO setting for inter company'),
+    rule_type = fields.Selection([('so_and_po', 'SO and PO setting for inter company'),
         ('invoice_and_refunds', 'Create Invoice/Refunds when encoding invoice/refunds')],
         help='Select the type to setup inter company rules in selected company.')
     so_from_po = fields.Boolean(string='Create Sale Orders when buying to this company',
@@ -19,26 +19,26 @@ class inter_company_rules_configuration(models.TransientModel):
     warehouse_id = fields.Many2one('stock.warehouse', string='Warehouse For Purchase Orders', 
         help='Default value to set on Purchase Orders that will be created based on Sale Orders made to this company.')
 
-    @api.onchange('set_type')
-    def onchange_set_type(self):
-        if self.set_type == 'invoice_and_refunds':
+    @api.onchange('rule_type')
+    def onchange_rule_type(self):
+        if self.rule_type == 'invoice_and_refunds':
             self.so_from_po = False
             self.po_from_so = False
             self.auto_validation = False
 
-        elif self.set_type == 'so_and_po':
+        elif self.rule_type == 'so_and_po':
             self.invoice_and_refunds = False
 
     @api.onchange('company_id')
     def onchange_company_id(self):
         if self.company_id:
-            set_type = False
+            rule_type = False
             if self.company_id.so_from_po or self.company_id.po_from_so or self.company_id.auto_validation:
-                set_type = 'so_and_po'
+                rule_type = 'so_and_po'
             elif self.company_id.auto_generate_invoices:
-                set_type = 'invoice_and_refunds'
+                rule_type = 'invoice_and_refunds'
 
-            self.set_type = set_type
+            self.rule_type = rule_type
             self.so_from_po = self.company_id.so_from_po
             self.po_from_so = self.company_id.po_from_so
             self.auto_validation = self.company_id.auto_validation
@@ -51,7 +51,7 @@ class inter_company_rules_configuration(models.TransientModel):
                 'so_from_po': self.so_from_po,
                 'po_from_so': self.po_from_so,
                 'auto_validation': self.auto_validation,
-                'auto_generate_invoices': True if self.set_type == 'invoice_and_refunds' else False,
+                'auto_generate_invoices': True if self.rule_type == 'invoice_and_refunds' else False,
                 'warehouse_id': self.warehouse_id.id
             }
             self.company_id.write(vals)
