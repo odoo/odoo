911fa4a3f2b0f04ef70d93a438a625656719ab2a
Josse Colpaert <jco@odoo.com>
2014-11-13 15:24:31 +0100

    [IMP] Pass the invoice type from sale to picking for the sale_journal module that is installable again

--- addons/sale_journal/__openerp__.py
+++ addons/sale_journal/__openerp__.py
@@ -59,7 +59,7 @@ Some statistics by journals are provided.
     ],
     'demo': ['sale_journal_demo.xml'],
     'test': [ ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
