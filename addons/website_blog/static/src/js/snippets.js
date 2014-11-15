25ff79bfdff56cc6ea2582e683d54e634cf7d487
Goffin Simon <simon.goffin@outlook.com>
2014-11-15 21:06:45 +0100

    1.

--- addons/website_blog/static/src/js/snippets.js
+++ addons/website_blog/static/src/js/snippets.js
@@ -2,7 +2,9 @@
     "use strict";
 
     var website = openerp.website;
+    var QWeb = openerp.qweb;
     var _t = openerp._t;
+    website.add_template_file('/website_blog/static/src/xml/website_blog.contact_search.xml');
 
     website.snippet.options.contact_editor = website.snippet.Option.extend({
         start: function () {
@@ -10,15 +12,20 @@
             self.$target.attr('contentEditable', 'false');
             var $btn = $('<a href="#" class="btn btn-default btn-sm search_contact" title="Search Contact">&nbsp;<i class="fa fa-search"></i>&nbsp;</a>');
             self.$overlay.find('.oe_options').after($btn);
-            var $input =$('<input type="email"  class="xxx" placeholder="Search contact">');
+            var $input =$('<div class="dropdown"><input type="email"  class="xxx dropdown-toggle" data-toggle="dropdown" placeholder="Search contact"></div>');
             self.$overlay.find('.oe_options').after($input);
             $('.xxx').on('click', function () {
-                $('.xxx').focus(); });
-            $('.search_contact').on('click', function () {
-                //console.log($('.xxx').val());
+                $('.xxx').focus();
+                self.$el.find(".contact_menu").remove();
                 self.name = $('.xxx').val();
                 self.find_existing();
+
             });
+            // $('.search_contact').on('click', function () {
+            //     //console.log($('.xxx').val());
+            //     self.name = $('.xxx').val();
+            //     self.find_existing();
+            // });
         },
 
         find_existing: function () {
@@ -33,11 +40,13 @@
                 args: [domain, ['name']],
                 kwargs: {
                     order: 'id desc',
-                    limit: 10,
+                    limit: 3,
                     context: website.get_context(),
                 }
             }).then(function (result){
                 console.log(result);
+                $(".xxx").after(QWeb.render("blog_contact_search",{contacts:result}));
+
             });
         }
 
