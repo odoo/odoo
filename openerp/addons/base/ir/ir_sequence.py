bfa21a91d8e9ad2dc302b314abdfc3280bd83846
pvy <pvy@odoo.com>
2014-11-18 11:02:41 +0100

    [IMP] base/ir/sequence : imp based on reviews

--- openerp/addons/base/ir/ir_sequence.py
+++ openerp/addons/base/ir/ir_sequence.py
@@ -18,7 +18,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 import logging
 import time
 
