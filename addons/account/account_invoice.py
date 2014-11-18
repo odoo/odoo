4badf0fae0ab81cf73a0dd76c5a57ad453a8a4fa
mdi-odoo <mdi@tinyerp.com>
2014-11-18 10:57:43 +0530

    [REM] account : Removed the 'type' field from 'account.account' object.

--- addons/account/account_invoice.py
+++ addons/account/account_invoice.py
@@ -87,7 +87,6 @@ class account_invoice(models.Model):
     @api.depends(
         'state', 'currency_id', 'invoice_line.price_subtotal',
 #TODO: Need to refactor the code of '_compute_residual' method
-        'move_id.line_id.account_id.type',
 #         'move_id.line_id.amount_residual',
 #         'move_id.line_id.amount_residual_currency',
         'move_id.line_id.currency_id',
