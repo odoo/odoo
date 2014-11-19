fc7d5ba2add58b630d21be170015a0cdd419365e
Jitendra Prajapati <jpr@openerp.com>
2014-11-19 14:43:54 +0530

    [IMP/add]account: add test_edi_invoice.py file.

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -1,13 +1,15 @@
 from . import account_test_users
 from . import test_account_customer_invoice
+from . import test_edi_invoice
 # from . import test_tax
 # from . import test_search
 # from . import test_reconciliation
 
 fast_suite = [
-	account_test_users,
-	test_account_customer_invoice
-	# test_tax,
-	# test_search,
-	# test_reconciliation,
+    account_test_users,
+    test_account_customer_invoice,
+    test_edi_invoice
+    # test_tax,
+    # test_search,
+    # test_reconciliation,
 ]
---
+++ addons/account/tests/test_edi_invoice.py
@@ -0,0 +1,10 @@
+from openerp.addons.account.tests.account_test_users import AccountTestUsers
+
+
+class TestEdiInvoice(AccountTestUsers):
+    """ In order to test the EDI export features of Invoices. """
+
+    def test_edi_invoice(self):
+        print ">>>>>>"
+        
+
