20c97205054aeaf16de62cc4cbc1c6b8046c83b7
Ajay Patel <apa@openerp.com>
2014-11-19 19:08:11 +0530

    [IMP]website:add render method to share.

--- addons/website/static/src/js/website.frontend.js
+++ addons/website/static/src/js/website.frontend.js
@@ -15,6 +15,7 @@
         init: function(element, social_list, configure){
             //Initialization
             this._super.apply(this, arguments);
+            var self = this;
             this.element = element;
             this.social_list = social_list;
             this.target = 'social_share';
@@ -22,14 +23,15 @@
             this.renderElement();
             this.bind_events();
         },
-        set_value: function(type, description, title, url, image){   
-            console.log("call set_value");
-        },
+        // set_value: function(type, description, title, url, image){   
+        //     console.log("call set_value");
+        // },
         bind_events: function() {
-            $('.fa-facebook').on('click', this.facebook);
-            $('.fa-twitter').on('click', this.twitter);
-            $('.fa-linkedin').on('click', this.linkedin);
-            $('.fa-google-plus').on('click', this.google_plus);
+            var self = this;
+            $('.fa-facebook').on('click', self.proxy('facebook'));
+            $('.fa-twitter').on('click', self.proxy('twitter'));
+            $('.fa-linkedin').on('click', self.proxy('linkedin'));
+            $('.fa-google-plus').on('click', self.proxy('google_plus'));
         },
         renderElement: function() {
             var self = this;
@@ -37,7 +39,7 @@
             console.log("calll renderElement",this.element, this.$el);
             
             self.element.popover({
-                'content': this.$el.html(),
+                'content': self.$el.html(),
                 'placement': 'bottom',
                 'container': 'body',
                 'html': true,
@@ -56,34 +58,54 @@
                 $(this).hide();
             });
         },
-        render: function(){
-            //find on which social icon click and reneder one of below function
-        },
         google_plus: function(){
             //call schema template
             console.log("call google-plus");
+            this.renderSocial('google-plus');
         },
         facebook: function(){
             // call opengraphtemplate
             console.log("call facebook");
+            this.renderSocial('facebook');
         },
         twitter: function(){
             console.log("call twitter");
+            this.renderSocial('twitter');
         },
         linkedin: function(){
             console.log("call linkedin");
+            this.renderSocial('linkedin');
+        },
+        renderSocial: function(social){
+            var url = this.element.data('url') || window.location.href.split(/[?#]/)[0]; // get current url without query string if not pass 
+            var text_to_share = this.element.data('share_content');
+            console.log("url", url, text_to_share);
+            var social_network = {
+                'facebook':'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url),
+                'twitter': 'https://twitter.com/intent/tweet?original_referer=' + encodeURIComponent(url) + '&amp;text=' + encodeURIComponent(text_to_share + ' - ' + url),
+                'linkedin': 'https://www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(text_to_share) + '&summary=' + encodeURIComponent(this.element.data('description')),
+                'google-plus': 'https://plus.google.com/share?url=' + encodeURIComponent(url)
+            };
+            if (_.contains(_.keys(social_network), social)){
+                console.log("cllllll")
+                var window_height = 500, window_width = 500, left = (screen.width/2)-(window_width/2), top = (screen.height/2)-(window_height/2);
+                window.open(social_network[social], '', 'menubar=no, toolbar=no, resizable=yes, scrollbar=yes, height=' + window_height + ',width=' + window_width + ', top=' + top + ', left=' + left);
+            }
+            //find on which social icon click and reneder one of below function
         },
     });
 
     website.ready().done(function() {
         $(document.body).on('hover', 'a.social_share', function() {
-            var social_list = eval($(this).data('social')) || ['facebook','twitter', 'linkedin', 'google-plus']
+            var self = $(this);
+            var default_social_list = ['facebook','twitter', 'linkedin', 'google-plus']
+            var social_list = _.intersection(eval($(this).data('social')) || default_social_list, default_social_list);
             new website.social_share(
                         $(this),
                         social_list,
                         {'facebook': {'width':100, 'height':100}}
                     );
-            //social_share_obj.set_value('type', 'description', 'title', 'url', 'image');
+            // social_share_obj.set_value('type', 'description', 'title', 'url', 'image');
         });
     });
 })();
