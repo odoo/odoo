7d0e31d6ec7500bc5b6196163331aeb41d4d2025
qdp-odoo <qdp@odoo.com>
2014-11-25 06:13:18 +0100

    [REF] base, ir_sequence: refactored date_range test in order to avoid having to deal with cursors, which just simplify the code and make it more elegant while being less error-prone

--- openerp/addons/base/tests/test_ir_sequence_date_range.py
+++ openerp/addons/base/tests/test_ir_sequence_date_range.py
@@ -27,48 +27,38 @@ def drop_sequence(code):
         cr.close()
 
 
-class test_ir_sequence_date_range_standard(unittest2.TestCase):
+class test_ir_sequence_date_range_standard(common.SingleTransactionCase):
     """ A few tests for a 'Standard' (i.e. PostgreSQL) sequence. """
 
     def test_ir_sequence_date_range_1_create(self):
         """ Try to create a sequence object with date ranges enabled. """
-        cr = cursor()
-        try:
-            d = dict(code='test_sequence_date_range', name='Test sequence', use_date_range=True)
-            c = registry('ir.sequence').create(cr, ADMIN_USER_ID, d, {})
-            assert c
-            cr.commit()
-        finally:
-            cr.close()
+        d = dict(code='test_sequence_date_range', name='Test sequence', use_date_range=True)
+        c = self.registry('ir.sequence').create(self.cr, self.uid, d, {})
+        assert c
 
     def test_ir_sequence_date_range_2_change_dates(self):
         """ Draw numbers to create a first subsequence then change its date range. Then, try to draw a new number adn check a new subsequence was correctly created. """
-        cr = cursor()
-        try:
-            year = str(int(date.today().strftime("%Y")) - 1)
-            dt = "{}-01-16".format(year)
-            n = registry('ir.sequence').next_by_code(cr, ADMIN_USER_ID, 'test_sequence_date_range', {'ir_sequence_date': dt})
-            self.assertEqual(n, '1')
-            n = registry('ir.sequence').next_by_code(cr, ADMIN_USER_ID, 'test_sequence_date_range', {'ir_sequence_date': dt})
-            self.assertEqual(n, '2')
-            # modify the range of date created
-            domain = [('sequence_id.code', '=', 'test_sequence_date_range'), ('date_from', '=', '{}-01-01'.format(year))]
-            seq_date_range_id = registry('ir.sequence.date_range').search(cr, ADMIN_USER_ID, domain, {})
-            registry('ir.sequence.date_range').write(cr, ADMIN_USER_ID, seq_date_range_id, {'date_from': '{}-01-18'.format(year)}, {})
-            n = registry('ir.sequence').next_by_code(cr, ADMIN_USER_ID, 'test_sequence_date_range', {'ir_sequence_date': dt})
-            self.assertEqual(n, '1')
-            # check the newly created sequence stops at the 17th of January
-            domain = [('sequence_id.code', '=', 'test_sequence_date_range'), ('date_from', '=', '{}-01-01'.format(year))]
-            seq_date_range_id = registry('ir.sequence.date_range').search(cr, ADMIN_USER_ID, domain, {})
-            seq_date_range = registry('ir.sequence.date_range').browse(cr, ADMIN_USER_ID, seq_date_range_id[0])
-            self.assertEqual(seq_date_range.date_to, '{}-01-17'.format(year))
-            cr.commit()
-        finally:
-            cr.close()
+        year = str(int(date.today().strftime("%Y")) - 1)
+        dt = "{}-01-16".format(year)
+        n = self.registry('ir.sequence').next_by_code(self.cr, self.uid, 'test_sequence_date_range', {'ir_sequence_date': dt})
+        self.assertEqual(n, '1')
+        n = self.registry('ir.sequence').next_by_code(self.cr, self.uid, 'test_sequence_date_range', {'ir_sequence_date': dt})
+        self.assertEqual(n, '2')
+        # modify the range of date created
+        domain = [('sequence_id.code', '=', 'test_sequence_date_range'), ('date_from', '=', '{}-01-01'.format(year))]
+        seq_date_range_id = self.registry('ir.sequence.date_range').search(self.cr, self.uid, domain, {})
+        self.registry('ir.sequence.date_range').write(self.cr, self.uid, seq_date_range_id, {'date_from': '{}-01-18'.format(year)}, {})
+        n = self.registry('ir.sequence').next_by_code(self.cr, self.uid, 'test_sequence_date_range', {'ir_sequence_date': dt})
+        self.assertEqual(n, '1')
+        # check the newly created sequence stops at the 17th of January
+        domain = [('sequence_id.code', '=', 'test_sequence_date_range'), ('date_from', '=', '{}-01-01'.format(year))]
+        seq_date_range_id = self.registry('ir.sequence.date_range').search(self.cr, self.uid, domain, {})
+        seq_date_range = self.registry('ir.sequence.date_range').browse(self.cr, self.uid, seq_date_range_id[0])
+        self.assertEqual(seq_date_range.date_to, '{}-01-17'.format(year))
 
-    @classmethod
-    def tearDownClass(cls):
-        drop_sequence('test_sequence_date_range')
+    def test_ir_sequence_date_range_3_unlink(self):
+        ids = self.registry('ir.sequence').search(self.cr, self.uid, [('code', '=', 'test_sequence_date_range')])
+        self.registry('ir.sequence').unlink(self.cr, self.uid, ids)
 
 
 class test_ir_sequence_date_range_no_gap(unittest2.TestCase):
