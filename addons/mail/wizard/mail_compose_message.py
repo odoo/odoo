2592f149a0f0e1ba77c2fa8c429aa344b3b19d36
Denis Ledoux <dle@odoo.com>
2014-12-02 16:50:15 +0100

    [FIX] mail: model can be None or uninstalled
    This is related to rev. 83b28737e9277d0612211900d49f539bb631dfb6

--- addons/mail/wizard/mail_compose_message.py
+++ addons/mail/wizard/mail_compose_message.py
@@ -73,7 +73,7 @@ class mail_compose_message(osv.TransientModel):
         result['res_id'] = result.get('res_id', context.get('active_id'))
         result['parent_id'] = result.get('parent_id', context.get('message_id'))
 
-        if not hasattr(self.pool[result['model']], 'message_post'):
+        if not result['model'] or not self.pool.get(result['model']) or not hasattr(self.pool[result['model']], 'message_post'):
             result['no_auto_thread'] = True
 
         # default values according to composition mode - NOTE: reply is deprecated, fall back on comment
