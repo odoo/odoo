6f08d67220b4ab4dc920ac0625d52ba7948fe9b7
Simon Lejeune <sle@openerp.com>
2014-12-03 15:57:33 +0100

    [IMP] web: jquery.ba-bbq: browser helper from jquery extracted
    This way we don't integrate jquery.migrate and bbq is working.
    This commit is bad and i should feel bad.

--- addons/web/static/lib/jquery.ba-bbq/jquery.ba-bbq.js
+++ addons/web/static/lib/jquery.ba-bbq/jquery.ba-bbq.js
@@ -1284,7 +1284,7 @@
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     // vvvvvvvvvvvvvvvvvvv REMOVE IF NOT SUPPORTING IE6/7/8 vvvvvvvvvvvvvvvvvvv
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    $.browser.msie && !supports_onhashchange && (function(){
+    window.browser.msie && !supports_onhashchange && (function(){
       // Not only do IE6/7 need the "magical" Iframe treatment, but so does IE8
       // when running in "IE7 compatibility" mode.
       
