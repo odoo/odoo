02cf0f36490b36159dc9a5389c2d9186de0cb5aa
Frédéric van der Essen <fvdessen@gmail.com>
2014-12-09 21:45:06 +0100

    [IMP] point_of_sale: removed the barcode field; already defined on res.partner

--- addons/point_of_sale/res_users.py
+++ addons/point_of_sale/res_users.py
@@ -9,7 +9,6 @@ import openerp.addons.product.product
 class res_users(osv.osv):
     _inherit = 'res.users'
     _columns = {
-        'barcode' : fields.char('Barcode', help="BarCode", oldname='ean13'),
         'pos_security_pin': fields.char('Security PIN',size=32, help='A Security PIN used to protect sensible functionality in the Point of Sale'),
         'pos_config' : fields.many2one('pos.config', 'Default Point of Sale', domain=[('state', '=', 'active')]),
     }
