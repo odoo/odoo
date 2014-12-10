af8a6096c95df533da1b92096121ed52ef225ef2
Denis Ledoux <dle@odoo.com>
2014-12-10 17:30:34 +0100

    [FIX] google_calendar: typo, Google is not plural

--- addons/google_calendar/google_calendar.py
+++ addons/google_calendar/google_calendar.py
@@ -669,7 +669,7 @@ class google_calendar(osv.AbstractModel):
                         self.pool['res.users'].write(cur, uid, [uid], {'google_calendar_last_sync_date': False}, context=context)
                 error_key = simplejson.loads(str(e))
                 error_key = error_key.get('error', {}).get('message', 'nc')
-                error_msg = "Google are lost... the next synchro will be a full synchro. \n\n %s" % error_key
+                error_msg = "Google is lost... the next synchro will be a full synchro. \n\n %s" % error_key
                 raise self.pool.get('res.config.settings').get_config_warning(cr, _(error_msg), context=context)
 
             my_google_att_ids = att_obj.search(cr, uid, [
