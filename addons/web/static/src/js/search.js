851140c7f4a302e8b7f49b4e836a75b7d487c8d3
Géry Debongnie <ged@odoo.com>
2014-12-10 09:49:48 +0100

    [FIX] web: correctly init searchview when no action_id
    the method prepare_dropdown_menu wasn't called when the searchview did
    not have an action id.  This can cause problems in some cases (for
    example, a crash in messaging when toggling the favorite menu)

--- addons/web/static/src/js/search.js
+++ addons/web/static/src/js/search.js
@@ -1615,7 +1615,10 @@ instance.web.search.FavoriteMenu = instance.web.Widget.extend({
                 }
             })
             .on('reset', this.proxy('clear_selection'));
-        if (!this.action_id) return $.when();
+        if (!this.action_id) {
+            this.prepare_dropdown_menu([]);
+            return $.when();
+        }
         return this.model.call('get_filters', [this.target_model, this.action_id])
             .done(this.proxy('prepare_dropdown_menu'));
     },
