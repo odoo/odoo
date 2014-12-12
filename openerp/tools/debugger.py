646c4f6e961c3d2ce8765131e2b261fdff4c27f3
Daniel Reis <dgreis@sapo.pt>
2014-12-12 15:18:36 +0000

    Add ipdb debugger support

--- openerp/tools/debugger.py
+++ openerp/tools/debugger.py
@@ -2,10 +2,14 @@
 # Copyright: 2014 - OpenERP S.A. <http://openerp.com>
 import types
 
+
 def post_mortem(config, info):
     if config['debug_mode'] and isinstance(info[2], types.TracebackType):
         try:
             import pudb as pdb
         except ImportError:
-            import pdb
+            try:
+                import ipdb as pdb
+            except ImportError:
+                import pdb
         pdb.post_mortem(info[2])
