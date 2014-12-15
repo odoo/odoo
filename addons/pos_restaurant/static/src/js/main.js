765018178b807f96fbab29e3b251c968a246832f
Frédéric van der Essen <fvdessen@gmail.com>
2014-12-15 17:35:20 +0100

    [FIX] pos_restaurant: properly merging the notes feature that allows custom notes on orderlines (Think Raw/Well-Done)

--- addons/pos_restaurant/static/src/js/main.js
+++ addons/pos_restaurant/static/src/js/main.js
@@ -2,6 +2,8 @@ openerp.pos_restaurant = function(instance){
 
     var module = instance.point_of_sale;
 
+    openerp_restaurant_notes(instance,module);
+
     openerp_restaurant_splitbill(instance,module);
 
     openerp_restaurant_printbill(instance,module);
