2784f655caf7bd8f76dab206fa99f0e173315661
Martin <mdo@odoo.com>
2014-12-15 14:23:01 +0100

    [FIX] test

--- addons/website_form_builder/tests/test_form_builder.py
+++ addons/website_form_builder/tests/test_form_builder.py
@@ -13,6 +13,8 @@ class TestFormBuilder(openerp.tests.HttpCase):
              ('body_html', 'like', 'State : be'),
              ('body_html', 'like', 'Products : galaxy S,Xperia')])
 
+        print mail;
+
         lead = self.registry('crm.lead').search_read(self.cr, SUPERUSER_ID,
             [('contact_name', '='   , 'John Smith'),
              ('phone'       , '='   , '118.218'),
@@ -20,12 +22,16 @@ class TestFormBuilder(openerp.tests.HttpCase):
              ('name'        , '='   , 'Usless Message'),
              ('description' , 'like', 'The complete usless Message')])
 
+        print lead;
+
         appl = self.registry('hr.applicant').search_read(self.cr, SUPERUSER_ID,
             [('partner_name'    , '='   , 'John Smith'),
              ('partner_phone'   , '='   , '118.218'),
              ('email_from'      , '='   , 'john@smith.com'),
              ('description'     , 'like', 'The complete usless Message')])
 
+        print appl;
+        
         self.assertNotEqual(mail, [], 'ERROR :: mail not inserted');
         self.assertNotEqual(lead, [], 'ERROR :: lead not inserted');
         self.assertNotEqual(appl, [], 'ERROR :: appl not inserted');
