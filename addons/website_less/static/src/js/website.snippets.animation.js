9d14f8615647abc1600200553c5013a641518bf6
David Monjoie <dmo@odoo.com>
2014-12-19 14:38:04 +0100

    [IMP] website.snippet: add data-oe-call to call a template from the website and keep all content editable; used for demo page of snippets
    Backport of 5c524ca

---
+++ addons/website_less/static/src/js/website.snippets.animation.js
@@ -0,0 +1,25 @@
+// Backported from 52ca425
+
+(function () {
+    'use strict';
+
+    var website = openerp.website;
+
+    function load_called_template () {
+        var ids_or_xml_ids = _.uniq($("[data-oe-call]").map(function () {return $(this).data('oe-call');}).get());
+        if (ids_or_xml_ids.length) {
+            openerp.jsonRpc('/website/multi_render', 'call', {
+                'ids_or_xml_ids': ids_or_xml_ids
+            }).then(function (data) {
+                for (var k in data) {
+                    var $data = $(data[k]).addClass('o_block_'+k);
+                    $("[data-oe-call='"+k+"']").each(function () {
+                        $(this).replaceWith($data.clone());
+                    });
+                }
+            });
+        }
+    }
+
+    $(document).ready(load_called_template);
+})
