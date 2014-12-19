9ed6accd2e8fca404018b2f998d0c913453e101b
Simon Lejeune <sle@openerp.com>
2014-12-19 17:31:12 +0100

    [REF] Packaging: Backport of v8 packaging
    [REF] Packaging: backport of a7ab5a4109ec99d77f61a6d4c8bc7696149ea09c
    [REF] Packaging: backport of a700a1139ee19d71bc4669ff74bc53c87b38dc7b
    [REF] Packaging: backport of 9d0bd2807e2c2b949ece62fd6be04a1cf39c8e0d
    [REF] Packaging: backport of b0722d3e5d6bebdf19dc29b8ba04651ae508839f
    [REF] Packaging: backport of 24b2c93814dfa18fa1aa88bb0497a73b7b8b3e6e
    [REF] Packaging: backport of e874cdafaa9675251cd7852b8e9b1662cee7d19a
    [REF] Packaging: backport of 56b61da16387e642130477e604022207a77e3f74
    [REF] Packaging: backport of be96dd85959528b86f7535b9b4cc0dc86981ca33
    [REF] Packaging: backport of 50a4da99f051d125b8435477657ee2b115a8e0bd
    [REF] Packaging: backport of 1c41f3699303da770797002025b7c8226fb562eb
    [REF] Packaging: backport of cc11331733a5cb8aff6c7f0d4def1404e83b80e8
    [FIX] Packaging: backport of 134716308266ff
    [REF] Package: backport of 93845d683c0420118c162eb855abb690150b692d
    [REF] Package: backport of 012bc01c113c8d008a0eb94219f1cb519519ff0d
    [FIX] Package: partial apply of d9e72a3716488ce8
    [REF] Package: backport of 67036feff4f3d2d86a9e9e3e19f85fc183f466c6
    [REF] Package: backport of 73de0138dedfe0faaae857544746915315a0dbeb
    [REF] Package: backport of 30d987ed30e64a498ef392a0aa83ca7f4d220a97
    [REF] Package: backport of 29132669a16792a84960f03c5627f04e0ad8ff6a
    [REF] Package: backport of 7db12dc47b1e89ba0a7315c567fafdc722f47b90
    [REF] Package: backport of f668f9c6cdc4fcea1c647959feed42359eb78009
    [REF] Package: backport of 2cf4051a1556d64a1ff321658c4c2e0a60a13132
    [REF] Packaging: backport of 735924878f89041f3b74540848baef71790872a1
    [REF] Packaging: backport of f2598ecdce169f8134ff1f5c84eb12e14a5d6f34
    [REF] Package: backport of fd21912cfe76f90447059cdf2617bcd7631cc122
    [REF] Package: backport of 9a8cdf49b08ce9e1b4238bf122ee2c4271d44f62
    [REF] Package: backport of 2f4984b8381c6407a720b122733242e6cf3101da
    [REF] Package: backport of 6e9edeeb46d4161bf7be02f570073c9c1edf45c7
    [REF] Package: partial backport of d4624fa826a5e9542782f2c53c8ba2c766d093bc
    [REF] Package: backport of 9f13722f7837a80e423ca6e7a7337d66f900e564

--- setup.py
+++ setup.py
@@ -1,152 +1,155 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
-import glob, os, re, setuptools, sys
-from os.path import join
+import os
+import re
+from glob import glob
+from setuptools import find_packages, setup
+from os.path import join, dirname
 
-# List all data files
-def data():
-    r = {}
+
+execfile(join(dirname(__file__), 'openerp', 'release.py'))  # Load release variables
+lib_name = 'openerp'
+
+
+def py2exe_datafiles():
+    data_files = {}
+    data_files['Microsoft.VC90.CRT'] = glob('C:\Microsoft.VC90.CRT\*.*')
+
     for root, dirnames, filenames in os.walk('openerp'):
         for filename in filenames:
             if not re.match(r'.*(\.pyc|\.pyo|\~)$', filename):
-                r.setdefault(root, []).append(os.path.join(root, filename))
+                data_files.setdefault(root, []).append(join(root, filename))
 
-    if os.name == 'nt':
-        r["Microsoft.VC90.CRT"] = glob.glob('C:\Microsoft.VC90.CRT\*.*')
+    import babel
+    data_files['babel/localedata'] = glob(join(dirname(babel.__file__), 'localedata', '*'))
+    others = ['global.dat', 'numbers.py', 'support.py', 'plural.py']
+    data_files['babel'] = map(lambda f: join(dirname(babel.__file__), f), others)
+    others = ['frontend.py', 'mofile.py']
+    data_files['babel/messages'] = map(lambda f: join(dirname(babel.__file__), 'messages', f), others)
 
-        import babel
-        # Add data, but also some .py files py2exe won't include automatically.
-        # TODO This should probably go under `packages`, instead of `data`,
-        # but this will work fine (especially since we don't use the ZIP file
-        # approach).
-        r["babel/localedata"] = glob.glob(os.path.join(os.path.dirname(babel.__file__), "localedata", '*'))
-        others = ['global.dat', 'numbers.py', 'support.py']
-        r["babel"] = map(lambda f: os.path.join(os.path.dirname(babel.__file__), f), others)
-        others = ['frontend.py', 'mofile.py']
-        r["babel/messages"] = map(lambda f: os.path.join(os.path.dirname(babel.__file__), "messages", f), others)
+    import pytz
+    tzdir = dirname(pytz.__file__)
+    for root, _, filenames in os.walk(join(tzdir, 'zoneinfo')):
+        base = join('pytz', root[len(tzdir) + 1:])
+        data_files[base] = [join(root, f) for f in filenames]
 
-        import pytz
-        tzdir = os.path.dirname(pytz.__file__)
-        for root, _, filenames in os.walk(os.path.join(tzdir, "zoneinfo")):
-            base = os.path.join('pytz', root[len(tzdir) + 1:])
-            r[base] = [os.path.join(root, f) for f in filenames]
+    import docutils
+    dudir = dirname(docutils.__file__)
+    for root, _, filenames in os.walk(dudir):
+        base = join('docutils', root[len(dudir) + 1:])
+        data_files[base] = [join(root, f) for f in filenames if not f.endswith(('.py', '.pyc', '.pyo'))]
 
-        import docutils
-        dudir = os.path.dirname(docutils.__file__)
-        for root, _, filenames in os.walk(dudir):
-            base = os.path.join('docutils', root[len(dudir) + 1:])
-            r[base] = [os.path.join(root, f) for f in filenames if not f.endswith(('.py', '.pyc', '.pyo'))]
+    return data_files.items()
 
-    return r.items()
-
-def gen_manifest():
-    file_list="\n".join(data())
-    open('MANIFEST','w').write(file_list)
-
-if os.name == 'nt':
-    sys.path.append("C:\Microsoft.VC90.CRT")
 
 def py2exe_options():
     if os.name == 'nt':
         import py2exe
         return {
-            "console" : [ { "script": "openerp-server", "icon_resources": [(1, join("install","openerp-icon.ico"))], }],
-            'options' : {
-                "py2exe": {
-                    "skip_archive": 1,
-                    "optimize": 0, # keep the assert running, because the integrated tests rely on them.
-                    "dist_dir": 'dist',
-                    "packages": [ "DAV", "HTMLParser", "PIL", "asynchat", "asyncore", "commands", "dateutil", "decimal", "docutils", "email", "encodings", "imaplib", "jinja2", "lxml", "lxml._elementpath", "lxml.builder", "lxml.etree", "lxml.objectify", "mako", "openerp", "poplib", "pychart", "pydot", "pyparsing", "pytz", "reportlab", "select", "simplejson", "smtplib", "uuid", "vatnumber", "vobject", "xml", "xml.dom", "yaml", ],
-                    "excludes" : ["Tkconstants","Tkinter","tcl"],
+            'console': [
+                {'script': 'openerp-server', 'icon_resources': [
+                    (1, join('setup', 'win32', 'static', 'pixmaps', 'openerp-icon.ico'))
+                ]},
+            ],
+            'options': {
+                'py2exe': {
+                    'skip_archive': 1,
+                    'optimize': 0,  # Keep the assert running as the integrated tests rely on them.
+                    'dist_dir': 'dist',
+                    'packages': [
+                        'asynchat', 'asyncore',
+                        'commands',
+                        'dateutil',
+                        'decimal',
+                        'docutils',
+                        'email',
+                        'encodings',
+                        'HTMLParser',
+                        'imaplib',
+                        'jinja2',
+                        'lxml', 'lxml._elementpath', 'lxml.builder', 'lxml.etree', 'lxml.objectify',
+                        'mako',
+                        'markupsafe',
+                        'mock',
+                        'openerp',
+                        'openid',
+                        'PIL',
+                        'poplib',
+                        'psutil',
+                        'pychart',
+                        'pydot',
+                        'pyparsing',
+                        'pytz',
+                        'reportlab',
+                        'select',
+                        'simplejson',
+                        'smtplib',
+                        'uuid',
+                        'vatnumber',
+                        'vobject',
+                        'win32service', 'win32serviceutil',
+                        'xlwt',
+                        'xml', 'xml.dom',
+                        'yaml',
+                    ],
+                    'excludes': ['Tkconstants', 'Tkinter', 'tcl'],
                 }
-            }
+            },
+            'data_files': py2exe_datafiles()
         }
     else:
         return {}
 
-execfile(join(os.path.dirname(__file__), 'openerp', 'release.py'))
 
-# Notes for OpenERP developer on windows:
-#
-# To setup a windows developer evironement install python2.7 then pip and use
-# "pip install <depencey>" for every dependency listed below.
-#
-# Dependecies that requires DLLs are not installable with pip install, for
-# them we added comments with links where you can find the installers.
-#
-# OpenERP on windows also require the pywin32, the binary can be found at
-# http://pywin32.sf.net
-#
-# Both python2.7 32bits and 64bits are known to work.
-
-setuptools.setup(
-      name             = 'openerp',
-      version          = version,
-      description      = description,
-      long_description = long_desc,
-      url              = url,
-      author           = author,
-      author_email     = author_email,
-      classifiers      = filter(None, classifiers.split("\n")),
-      license          = license,
-      scripts          = ['openerp-server'],
-      data_files       = data(),
-      packages         = setuptools.find_packages(),
-      dependency_links = ['http://download.gna.org/pychart/'],
-      #include_package_data = True,
-      install_requires = [
-          'pychart', # not on pypi, use: pip install http://download.gna.org/pychart/PyChart-1.39.tar.gz
-          'babel',
-          'docutils',
-          'feedparser',
-          'gdata',
-          'Jinja2',
-          'lxml', # windows binary http://www.lfd.uci.edu/~gohlke/pythonlibs/
-          'mako',
-          'mock',
-          'PIL', # windows binary http://www.lfd.uci.edu/~gohlke/pythonlibs/
-          'psutil', # windows binary code.google.com/p/psutil/downloads/list
-          'psycopg2 >= 2.2',
-          'pydot',
-          'python-dateutil < 2',
-          'python-ldap', # optional
-          'python-openid',
-          'pytz',
-          'pywebdav < 0.9.8',
-          'pyyaml',
-          'reportlab', # windows binary pypi.python.org/pypi/reportlab
-          'simplejson',
-          'unittest2',
-          'vatnumber',
-          'vobject',
-          'werkzeug',
-          'xlwt',
-      ],
-      extras_require = {
-          'SSL' : ['pyopenssl'],
-      },
-      tests_require = ['unittest2'],
-      **py2exe_options()
+setup(
+    name='openerp',
+    version=version,
+    description=description,
+    long_description=long_desc,
+    url=url,
+    author=author,
+    author_email=author_email,
+    classifiers=filter(None, classifiers.split('\n')),
+    license=license,
+    scripts=['openerp-server'],
+    packages=find_packages(),
+    package_dir={'%s' % lib_name: 'openerp'},
+    include_package_data=True,
+    dependency_links=['http://download.gna.org/pychart/'],
+    install_requires=[
+        'babel',
+        'docutils',
+        'feedparser',
+        'gdata',
+        'Jinja2',
+        'lxml',  # windows binary http://www.lfd.uci.edu/~gohlke/pythonlibs/
+        'mako',
+        'mock',
+        'PIL', # windows binary http://www.lfd.uci.edu/~gohlke/pythonlibs/
+        'psutil',  # windows binary code.google.com/p/psutil/downloads/list
+        'psycopg2 >= 2.2',
+        'pychart',  # not on pypi, use: pip install http://download.gna.org/pychart/PyChart-1.39.tar.gz
+        'pydot',
+        'python-dateutil < 2',
+        'python-ldap',  # optional
+        'python-openid',
+        'pytz',
+        'pywebdav < 0.9.8',
+        'pyyaml',
+        'reportlab',  # windows binary pypi.python.org/pypi/reportlab
+        'simplejson',
+        'unittest2',
+        'vatnumber',
+        'vobject',
+        'werkzeug',
+        'xlwt',
+    ],
+    extras_require={
+        'SSL': ['pyopenssl'],
+    },
+    tests_require=[
+        'unittest2',
+    ],
+    **py2exe_options()
 )
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
