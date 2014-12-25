b397d5ab0292f75471e40daf74f4d54b50431b6a
Binjal Desai <dbi@openerp.com>
2014-12-25 15:22:25 +0530

    [IMP]CRM_CLIAM: module in inherit kanban view.CRM_HELPDESK module change in a state to satge_id field and  also add kanban view.In both module in remove a _resolve_team_id_from_context instead of add context in default_team_id. also change a stage_find method in use list comprehension for team_ids.

--- addons/crm_helpdesk/crm_helpdesk.py
+++ addons/crm_helpdesk/crm_helpdesk.py
@@ -26,14 +26,72 @@ from openerp import tools
 from openerp.tools.translate import _
 from openerp.tools import html2plaintext
 
+class crm_helpdesk_stage(osv.Model):
+    """ Model for helpdesk stages. This models the main stages of a helpdesk
+        management flow. Main CRM objects (leads, opportunities, project
+        issues, ...) will now use only stages, instead of state and stages.
+        Stages are for example used to display the kanban view of records.
+    """
+    _name = "crm.helpdesk.stage"
+    _description = "helpdesk stages"
+    _order = "sequence"
+
+    _columns = {
+        'name': fields.char('Stage Name', required=True, translate=True),
+        'sequence': fields.integer('Sequence', help="Used to order stages. Lower is better."),
+        'team_ids': fields.many2many('crm.team', 'crm_team_helpdesk_stage_rel', 'stage_id', 'team_id', string='Teams',
+                        help="Link between stages and sales teams. When set, this limitate the current stage to the selected sales teams."),
+        'case_default': fields.boolean('Common to All Teams',
+                        help="If you check this field, this stage will be proposed by default on each sales team. It will not assign this stage to existing teams."),
+        'fold': fields.boolean('Folded in Kanban View',
+                               help='This stage is folded in the kanban view when'
+                               'there are no records in that stage to display.')
+    }
+
+    _defaults = {
+        'sequence': lambda *args: 1,
+    }
 
 class crm_helpdesk(osv.osv):
     """ Helpdesk Cases """
 
     _name = "crm.helpdesk"
     _description = "Helpdesk"
-    _order = "id desc"
-    _inherit = ['mail.thread']
+    _order = "priority, id desc"
+    _inherit = ['mail.thread', 'ir.needaction_mixin']
+
+    _mail_post_access = 'read'
+
+    def _get_default_stage_id(self, cr, uid, context=None):
+        """ Gives default stage_id """
+        team_id = context.get('default_team_id')
+        return self.stage_find(cr, uid, [], team_id, [('fold', '=', False)], context=context)
+
+    def _read_group_stage_ids(self, cr, uid, ids, domain, read_group_order=None, access_rights_uid=None, context=None):
+        access_rights_uid = access_rights_uid or uid
+        stage_obj = self.pool.get('crm.helpdesk.stage')
+        order = stage_obj._order
+        # lame hack to allow reverting search, should just work in the trivial case
+        if read_group_order == 'stage_id desc':
+            order = "%s desc" % order
+        # retrieve team_id from the context and write the domain
+        # - ('id', 'in', 'ids'): add columns that should be present
+        # - OR ('case_default', '=', True), ('fold', '=', False): add default columns that are not folded
+        # - OR ('team_ids', 'in', team_id), ('fold', '=', False) if team_id: add team columns that are not folded
+        search_domain = []
+        team_id = context.get('default_team_id')
+        if team_id:
+            search_domain += ['|', ('team_ids', '=', team_id), ('id', 'in', ids)]
+        # perform search
+        stage_ids = stage_obj._search(cr, uid, search_domain, order=order, access_rights_uid=access_rights_uid, context=context)
+        result = stage_obj.name_get(cr, access_rights_uid, stage_ids, context=context)
+        # restore order of the search
+        result.sort(lambda x,y: cmp(stage_ids.index(x[0]), stage_ids.index(y[0])))
+
+        fold = {}
+        for stage in stage_obj.browse(cr, access_rights_uid, stage_ids, context=context):
+            fold[stage.id] = stage.fold or False
+        return result, fold
 
     _columns = {
             'id': fields.integer('ID', readonly=True),
@@ -45,7 +103,7 @@ class crm_helpdesk(osv.osv):
             'create_date': fields.datetime('Creation Date' , readonly=True),
             'write_date': fields.datetime('Update Date' , readonly=True),
             'date_deadline': fields.date('Deadline'),
-            'user_id': fields.many2one('res.users', 'Responsible'),
+            'user_id': fields.many2one('res.users', 'Responsible', track_visibility='always'),
             'team_id': fields.many2one('crm.team', 'Sales Team', oldname='section_id',\
                             select=True, help='Responsible sales team. Define Responsible user and Email account for mail gateway.'),
             'company_id': fields.many2one('res.company', 'Company'),
@@ -54,6 +112,7 @@ class crm_helpdesk(osv.osv):
             'email_cc': fields.text('Watchers Emails', size=252 , help="These email addresses will be added to the CC field of all inbound and outbound emails for this record before being sent. Separate multiple email addresses with a comma"),
             'email_from': fields.char('Email', size=128, help="Destination email for email gateway"),
             'date': fields.datetime('Date'),
+            'date_last_stage_update': fields.datetime('Last Stage Update', select=True),
             'ref': fields.reference('Reference', selection=openerp.addons.base.res.res_request.referencable_models),
             'ref2': fields.reference('Reference 2', selection=openerp.addons.base.res.res_request.referencable_models),
             'channel_id': fields.many2one('utm.medium', 'Channel', help="Communication channel."),
@@ -63,26 +122,56 @@ class crm_helpdesk(osv.osv):
             'probability': fields.float('Probability (%)'),
             'categ_id': fields.many2one('crm.helpdesk.category', 'Category'),
             'duration': fields.float('Duration', states={'done': [('readonly', True)]}),
-            'state': fields.selection(
-                [('draft', 'New'),
-                 ('open', 'In Progress'),
-                 ('pending', 'Pending'),
-                 ('done', 'Closed'),
-                 ('cancel', 'Cancelled')], 'Status', readonly=True, track_visibility='onchange',
-                                  help='The status is set to \'Draft\', when a case is created.\
-                                  \nIf the case is in progress the status is set to \'Open\'.\
-                                  \nWhen the case is over, the status is set to \'Done\'.\
-                                  \nIf the case needs to be reviewed then the status is set to \'Pending\'.'),
+            'color': fields.integer('Color Index'),
+            'stage_id': fields.many2one('crm.helpdesk.stage', 'Stage', track_visibility='onchange',
+                domain="['|', ('team_ids', '=', team_id), ('case_default', '=', True)]"),
+            'kanban_state': fields.selection([('normal', 'Normal'),('blocked', 'Blocked'),('done', 'Ready for next stage')], 'Kanban State',
+                                         track_visibility='onchange',
+                                         help="A helpdesk's kanban state indicates special situations affecting it:\n"
+                                              " * Normal is the default situation\n"
+                                              " * Blocked indicates something is preventing the progress of this issue\n"
+                                              " * Ready for next stage indicates the helpdesk is ready to be pulled to the next stage",
+                                         required=False),
     }
 
     _defaults = {
         'active': lambda *a: 1,
         'user_id': lambda s, cr, uid, c: uid,
-        'state': lambda *a: 'draft',
+        'kanban_state': 'normal',
+        'team_id': lambda s, cr, uid, c: c.get('default_team_id'),
+        'date_last_stage_update': fields.datetime.now,
+        'stage_id': lambda s, cr, uid, c: s._get_default_stage_id(cr, uid, c),
         'date': fields.datetime.now,
         'company_id': lambda s, cr, uid, c: s.pool.get('res.company')._company_default_get(cr, uid, 'crm.helpdesk', context=c),
         'priority': '1',
     }
+
+    _group_by_full = {
+        'stage_id': _read_group_stage_ids
+    }
+
+    def stage_find(self, cr, uid, cases, team_id, domain=[], order='sequence', context=None):
+        """ Override of the base.stage method
+            Parameter of the stage search taken from the lead:
+            - team_id: if set, stages must belong to this team or
+              be a default case
+        """
+        if isinstance(cases, (int, long)):
+            cases = self.browse(cr, uid, cases, context=context)
+        # collect all team_ids
+        team_ids = [helpdesk.team_id.id for helpdesk in cases if helpdesk.team_id]
+        # OR all team_ids and OR with case_default
+        search_domain = []
+        if team_ids:
+            search_domain += [('|'), ('team_ids', 'in', team_ids)]
+        search_domain.append(('case_default', '=', True))
+        # AND with the domain in parameter
+        search_domain += list(domain)
+        # perform search, return the first found
+        stage_ids = self.pool.get('crm.helpdesk.stage').search(cr, uid, search_domain, order=order, context=context)
+        if stage_ids:
+            return stage_ids[0]
+        return False
 
     def on_change_partner_id(self, cr, uid, ids, partner_id, context=None):
         values = {}
@@ -93,14 +182,32 @@ class crm_helpdesk(osv.osv):
             }
         return {'value': values}
 
-    def write(self, cr, uid, ids, values, context=None):
-        """ Override to add case management: open/close dates """
-        if values.get('state'):
-            if values.get('state') in ['draft', 'open'] and not values.get('date_open'):
-                values['date_open'] = fields.datetime.now()
-            elif values.get('state') == 'close' and not values.get('date_closed'):
-                values['date_closed'] = fields.datetime.now()
-        return super(crm_helpdesk, self).write(cr, uid, ids, values, context=context)
+    def write(self, cr, uid, ids, vals, context=None):
+        # stage change: update date_last_stage_update
+        if vals.get('stage_id'):
+            vals['date_last_stage_update'] = fields.datetime.now()
+            if not vals.get( 'kanban_state'):
+                vals['kanban_state'] = 'normal'
+        # user_id change: update date_start
+        if vals.get('user_id'):
+            vals['date_start'] = fields.datetime.now()
+        return super(crm_helpdesk, self).write(cr, uid, ids, vals, context)
+
+    def create(self, cr, uid, vals, context=None):
+        context = dict(context or {})
+        if vals.get('team_id') and not context.get('default_team_id'):
+            context['default_team_id'] = vals.get('team_id')
+
+        # context: no_log, because subtype already handle this
+        create_context = dict(context, mail_create_nolog=True)
+        return super(crm_helpdesk, self).create(cr, uid, vals, context=create_context)
+
+    def copy(self, cr, uid, id, default=None, context=None):
+        helpdesk = self.browse(cr, uid, id, context=context)
+        default = dict(default or {},
+            stage_id=self._get_default_stage_id(cr, uid, context=context),
+            name=_('%s (copy)') % helpdesk.name)
+        return super(crm_helpdesk, self).copy(cr, uid, id, default, context=context)
 
     def case_escalate(self, cr, uid, ids, context=None):
         """ Escalates case to parent level """
--- addons/crm_helpdesk/report/crm_helpdesk_report.py
+++ addons/crm_helpdesk/report/crm_helpdesk_report.py
@@ -23,14 +23,6 @@ from openerp.osv import fields,osv
 from openerp import tools
 
 
-AVAILABLE_STATES = [
-    ('draft','Draft'),
-    ('open','Open'),
-    ('cancel', 'Cancelled'),
-    ('done', 'Closed'),
-    ('pending','Pending')
-]
-
 class crm_helpdesk_report(osv.osv):
     """ Helpdesk report after Sales Services """
 
@@ -43,7 +35,7 @@ class crm_helpdesk_report(osv.osv):
         'user_id':fields.many2one('res.users', 'User', readonly=True),
         'team_id':fields.many2one('crm.team', 'Team', readonly=True, oldname='section_id'),
         'nbr': fields.integer('# of Requests', readonly=True),  # TDE FIXME master: rename into nbr_requests
-        'state': fields.selection(AVAILABLE_STATES, 'Status', readonly=True),
+        'stage_id': fields.many2one('crm.helpdesk.stage', 'Stage', readonly=True, domain="[('team_ids','=',team_id)]"),
         'delay_close': fields.float('Delay to Close',digits=(16,2),readonly=True, group_operator="avg"),
         'partner_id': fields.many2one('res.partner', 'Partner' , readonly=True),
         'company_id': fields.many2one('res.company', 'Company', readonly=True),
@@ -74,7 +66,7 @@ class crm_helpdesk_report(osv.osv):
                     c.date as date,
                     c.create_date,
                     c.date_closed,
-                    c.state,
+                    c.stage_id,
                     c.user_id,
                     c.team_id,
                     c.partner_id,
@@ -92,7 +84,7 @@ class crm_helpdesk_report(osv.osv):
                     crm_helpdesk c
                 where c.active = 'true'
                 group by c.date,\
-                     c.state, c.user_id,c.team_id,c.priority,\
+                     c.stage_id, c.user_id,c.team_id,c.priority,\
                      c.partner_id,c.company_id,c.date_deadline,c.create_date,c.date,c.date_closed,\
                      c.categ_id,c.channel_id,c.planned_cost,c.id
             )""")
