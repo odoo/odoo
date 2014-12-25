2ff1795426a49108411b9bdeb1d9de3afba1b7c7
Manuel Vázquez Acosta <mva.led@gmail.com>
2014-12-25 14:50:34 -0500

    [IMP] module: Expose Odoo's version information to __openerp__.py files.

--- openerp/modules/module.py
+++ openerp/modules/module.py
@@ -243,9 +243,12 @@ def load_information_from_description_file(module, mod_path=None):
                 'depends data demo test init_xml update_xml demo_xml'.split(),
                 iter(list, None)))
 
+            globals_dict = {
+                'ODOO_VERSION_INFO': release.version_info,
+            }
             f = tools.file_open(terp_file)
             try:
-                info.update(eval(f.read()))
+                info.update(eval(f.read(), globals_dict=globals_dict))
             finally:
                 f.close()
 
