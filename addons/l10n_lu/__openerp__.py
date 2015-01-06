f56dc1c99434d5d5d68d3d87890478d9436b4b74
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-01-06 15:13:45 +0100

    [IMP] l10n_lu: VAT 2015 - add a note in manifest

--- addons/l10n_lu/__openerp__.py
+++ addons/l10n_lu/__openerp__.py
@@ -34,7 +34,13 @@ This is the base module to manage the accounting chart for Luxembourg.
     * the Luxembourg Official Chart of Accounts (law of June 2009 + 2015 chart and Taxes),
     * the Tax Code Chart for Luxembourg
     * the main taxes used in Luxembourg
-    * default fiscal position for local, intracom, extracom """,
+    * default fiscal position for local, intracom, extracom
+
+Notes:
+    * the 2015 chart of taxes is implemented to a large extent,
+      see the first sheet of tax.xls for details of coverage
+    * to update the chart of tax template, update tax.xls and run tax2csv.py
+""",
     'author': 'OpenERP SA, ADN, ACSONE SA/NV',
     'website': 'http://www.openerp.com http://www.adn-luxembourg.com',
     'depends': ['account', 'base_vat', 'base_iban'],
