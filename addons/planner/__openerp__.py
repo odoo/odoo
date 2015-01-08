4009a7476484df296ce77d59df800a9dcc270eaa
dka-odoo <dka@odoo.com>
2015-01-08 11:54:50 +0100

    [ADD] planner, planner_crm : add the planner engine, and the planner for CRM module. A planner is a tool helping people to set up quickly. For CRM, it helps to define sales strategy, objectives, leads, KPIs and much more !

---
+++ addons/planner/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Planner',
+    'category': 'Planner',
+    'summary': 'Help to configure application',
+    'version': '1.0',
+    'description': """Application Planner""",
+    'author': 'Odoo SA',
+    'depends': ['web'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/planner.xml',
+    ],
+    'qweb': ['static/src/xml/planner.xml'],
+    'installable': True,
+    'auto_install': True,
+}
