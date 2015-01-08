4992777333c2f545455e593f202c060ea2ed0af2
rmu-odoo <rmu@openerp.com>
2015-01-08 13:20:14 +0530

    [FIX: entry items does not have reconcile tag in opening entries]

--- addons/account/wizard/account_fiscalyear_close.py
+++ addons/account/wizard/account_fiscalyear_close.py
@@ -60,8 +60,10 @@ class account_fiscalyear_close(osv.osv_memory):
             cr.execute('select distinct(company_id) from account_move_line where id in %s',(tuple(ids),))
             if len(cr.fetchall()) > 1:
                 raise osv.except_osv(_('Warning!'), _('The entries to reconcile should belong to the same company.'))
-            r_id = self.pool.get('account.move.reconcile').create(cr, uid, {'type': 'auto', 'opening_reconciliation': True})
-            cr.execute('update account_move_line set reconcile_id = %s where id in %s',(r_id, tuple(ids),))
+            reconcile_obj = self.pool.get('account.move.reconcile')   
+            r_id = reconcile_obj.create(cr, uid, {'type': 'auto', 'opening_reconciliation': True})
+            reconcile = reconcile_obj.browse(cr, uid, r_id, context=context)
+            cr.execute('update account_move_line set reconcile_id = %s ,reconcile_ref = %s where id in %s',(r_id,reconcile.name, tuple(ids),))
             obj_acc_move_line.invalidate_cache(cr, uid, ['reconcile_id'], ids, context=context)
             return r_id
 
