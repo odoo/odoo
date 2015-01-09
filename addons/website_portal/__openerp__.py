a984b32a5ee56ba3ec41370820913834851e1acb
Damien Bouvy <dbo@odoo.com>
2015-01-09 10:00:29 +0100

    [FIX] website_portal: unnecesary depedency no longer causes errors when installing from base db

--- addons/website_portal/__openerp__.py
+++ addons/website_portal/__openerp__.py
@@ -6,7 +6,7 @@
     'author': 'OpenERP SA',
     'category': 'Website',
     'version': '1.0',
-    'depends': ["base", "portal", "website", "web"],
+    'depends': ["base", "portal", "website"],
     'data': ["views/website_portal.xml"],
     'auto_install': False,
 }
