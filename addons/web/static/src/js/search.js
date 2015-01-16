7994025ecf835cd3deaec452118de5d946eb207a
Nandan Jani <nja@openerp.com>
2015-01-16 16:55:28 +0530

    [FIX] Mail: Fixed the issue of default filter not applying after reloading the page

--- addons/web/static/src/js/search.js
+++ addons/web/static/src/js/search.js
@@ -624,7 +624,7 @@ instance.web.SearchView = instance.web.Widget.extend(/** @lends instance.web.Sea
                 return filter.user_id && filter.is_default;
             });
             if (personal_filter) {
-                this.drawer.custom_filters.toggle_filter(personal_filter, true);
+                this.drawer.custom_filters.toggle_filter(personal_filter, false);
                 return;
             }
 
