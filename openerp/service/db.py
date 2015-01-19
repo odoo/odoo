ec9a543014a91be17398ba6e443578fe37c3a885
Antony Lesuisse <al@openerp.com>
2015-01-19 01:41:08 +0100

    [FIX] dbmanager: backup support both zip and pg_dump custom format
    - let the user choose between the pg_dump custom format or the zip format including the filestore
    - use file objects to allow dumps larger than memory
    - postgres subprocess invocation is now clean and thread-safe, we dont touch the local process environ anymore
    - add a manifest to the zip dump format with version information about odoo, postgres (pg_dump doesnt output it) and modules

--- openerp/service/db.py
+++ openerp/service/db.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
-from contextlib import closing
-from functools import wraps
+
+import json
 import logging
 import os
 import shutil
+import tempfile
 import threading
 import traceback
-import tempfile
 import zipfile
+
+from functools import wraps
+from contextlib import closing
 
 import psycopg2
 
@@ -143,70 +146,65 @@ def exp_drop(db_name):
         shutil.rmtree(fs)
     return True
 
-def _set_pg_password_in_environment(func):
-    """ On systems where pg_restore/pg_dump require an explicit
-    password (i.e. when not connecting via unix sockets, and most
-    importantly on Windows), it is necessary to pass the PG user
-    password in the environment or in a special .pgpass file.
-
-    This decorator handles setting
-    :envvar:`PGPASSWORD` if it is not already
-    set, and removing it afterwards.
-
-    See also http://www.postgresql.org/docs/8.4/static/libpq-envars.html
-
-    .. note:: This is not thread-safe, and should never be enabled for
-         SaaS (giving SaaS users the super-admin password is not a good idea
-         anyway)
-    """
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if os.environ.get('PGPASSWORD') or not openerp.tools.config['db_password']:
-            return func(*args, **kwargs)
-        else:
-            os.environ['PGPASSWORD'] = openerp.tools.config['db_password']
-            try:
-                return func(*args, **kwargs)
-            finally:
-                del os.environ['PGPASSWORD']
-    return wrapper
-
 def exp_dump(db_name):
     with tempfile.TemporaryFile() as t:
         dump_db(db_name, t)
         t.seek(0)
         return t.read().encode('base64')
 
-@_set_pg_password_in_environment
-def dump_db(db, stream):
+def dump_db_manifest(cr):
+    pg_version = "%d.%d" % divmod(cr._obj.connection.server_version / 100, 100)
+    env = openerp.api.Environment(cr, SUPERUSER_ID, {})
+    modules = dict([(i.name,i.latest_version) for i in env['ir.module.module'].search([('state','=','installed')])])
+    manifest = {
+        'odoo_dump': '1',
+        'db_name': cr.dbname,
+        'version': openerp.release.version,
+        'version_info': openerp.release.version_info,
+        'major_version': openerp.release.major_version,
+        'pg_version': pg_version,
+        'modules': modules,
+    }
+    return manifest
+
+def dump_db(db_name, stream, backup_format='zip'):
     """Dump database `db` into file-like object `stream`"""
-    with openerp.tools.osutil.tempdir() as dump_dir:
-        registry = openerp.modules.registry.RegistryManager.get(db)
-        with registry.cursor() as cr:
-            filestore = registry['ir.attachment']._filestore(cr, SUPERUSER_ID)
-            if os.path.exists(filestore):
-                shutil.copytree(filestore, os.path.join(dump_dir, 'filestore'))
 
-        dump_file = os.path.join(dump_dir, 'dump.sql')
-        cmd = ['pg_dump', '--format=p', '--no-owner', '--file=' + dump_file]
-        if openerp.tools.config['db_user']:
-            cmd.append('--username=' + openerp.tools.config['db_user'])
-        if openerp.tools.config['db_host']:
-            cmd.append('--host=' + openerp.tools.config['db_host'])
-        if openerp.tools.config['db_port']:
-            cmd.append('--port=' + str(openerp.tools.config['db_port']))
-        cmd.append(db)
+    cmd = ['pg_dump', '--no-owner']
+    if openerp.tools.config['db_user']:
+        cmd.append('--username=' + openerp.tools.config['db_user'])
+    if openerp.tools.config['db_host']:
+        cmd.append('--host=' + openerp.tools.config['db_host'])
+    if openerp.tools.config['db_port']:
+        cmd.append('--port=' + str(openerp.tools.config['db_port']))
+    cmd.append(db_name)
 
-        if openerp.tools.exec_pg_command(*cmd):
-            _logger.error('DUMP DB: %s failed! Please verify the configuration of the database '
-                          'password on the server. You may need to create a .pgpass file for '
-                          'authentication, or specify `db_password` in the server configuration '
-                          'file.', db)
-            raise Exception("Couldn't dump database")
+    if backup_format == 'zip':
+        with openerp.tools.osutil.tempdir() as dump_dir:
+            registry = openerp.modules.registry.RegistryManager.get(db_name)
+            with registry.cursor() as cr:
+                filestore = registry['ir.attachment']._filestore(cr, SUPERUSER_ID)
+                if os.path.exists(filestore):
+                    shutil.copytree(filestore, os.path.join(dump_dir, 'filestore'))
+                manifest = dump_db_manifest(cr)
+                with open(os.path.join(dump_dir, 'manifest.json'), 'w') as fh:
+                    json.dump(manifest, fh, indent=4)
+            cmd.insert(-1, '--file=' + os.path.join(dump_dir, 'dump.sql'))
+            openerp.tools.exec_pg_command(*cmd)
+            openerp.tools.osutil.zip_dir(dump_dir, stream, include_dir=False)
+    else:
+        cmd.insert(-1, '--format=c')
+        print cmd
+        stdin, stdout = openerp.tools.exec_pg_command_pipe(*cmd)
+        shutil.copyfileobj(stdout, stream)
 
-        openerp.tools.osutil.zip_dir(dump_dir, stream, include_dir=False)
+    _logger.info('DUMP DB successful: %s', db_name)
 
-    _logger.info('DUMP DB successful: %s', db)
+def dump_db_stream(db_name, backup_format='zip'):
+    t=tempfile.TemporaryFile()
+    dump_db(db_name, t, backup_format)
+    t.seek(0)
+    return t
 
 def exp_restore(db_name, data, copy=False):
     data_file = tempfile.NamedTemporaryFile(delete=False)
@@ -218,7 +216,6 @@ def exp_restore(db_name, data, copy=False):
         os.unlink(data_file.name)
     return True
 
-@_set_pg_password_in_environment
 def restore_db(db, dump_file, copy=False):
     assert isinstance(db, basestring)
     if exp_db_exist(db):
@@ -351,4 +348,3 @@ def exp_migrate_databases(databases):
         openerp.modules.registry.RegistryManager.new(db, force_demo=False, update_module=True)
     return True
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
