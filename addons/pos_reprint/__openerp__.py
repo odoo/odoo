e4730b3e579be78d47a1e900d030873c3c9f75e4
Martin <mdo@odoo.com>
2015-01-20 13:44:22 +0100

    [IMP] Optional button to allow cashier to reprint receipt

---
+++ addons/pos_reprint/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Point of Sale Reprint',
+    'version': '1.0',
+    'category': 'Point of Sale',
+    'sequence': 6,
+    'summary': 'Allow cashier to reprint tickets',
+    'description': """
+
+=======================
+
+Allow cashier to reprint tickets
+
+""",
+    'author': 'Odoo SA',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/templates.xml',
+        'views/views.xml',
+    ],
+    'installable': True,
+    'website': 'https://www.odoo.com/page/point-of-sale',
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ addons/pos_reprint/static/src/js/reprint.js
@@ -0,0 +1,42 @@
+openerp.pos_reprint = function(instance){
+    var module   = instance.point_of_sale;
+    var QWeb = instance.web.qweb;
+    var _t = instance.web._t;
+
+    QWeb.add_template('/pos_reprint/static/src/xml/reprint.xml');
+
+    module.PosWidget.include({
+        build_widgets: function(){
+            var self = this;
+            this._super();
+            this.pos.old_receipt = null;
+            
+            if(!this.pos.config.reprint) return;
+
+            var reprint = $(QWeb.render('ReprintButton'));
+
+            reprint.click(function(){
+                if(self.pos.old_receipt) {
+                    self.pos.proxy.print_receipt(self.pos.old_receipt);
+                }
+                else {
+                    self.pos_widget.screen_selector.show_popup("error", {
+                        message: _t("Nothing to Print"),
+                        comment: _t("There is no previous receipt to print."),
+                    });
+                }
+            });
+
+            reprint.appendTo(this.$('.control-buttons'));
+            this.$('.control-buttons').removeClass('oe_hidden');
+        },
+    });
+
+    module.ProxyDevice.include({
+        print_receipt: function(receipt) {
+            this.pos.old_receipt = this.pos.old_receipt || receipt;
+            this._super(receipt);
+        }
+    });
+
+};
