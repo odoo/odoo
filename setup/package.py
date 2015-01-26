odoo/odoo

commit 91026647d43358594df0e17f3d408a3aa415c665

[FIX] Packaging: Debian: do not include setup/win32 in sources
This prevents to put postgresql-9.3.5-1-windows.exe, node.exe and
wkhtmltopdf.exe in the tar.gz produced with debhelper, saving
approx. 60MB.

setup/package.py
@@ -241,8 +241,10 @@ class KVMWinTestExe(KVM):
 #----------------------------------------------------------
 # Stage: building
 #----------------------------------------------------------
-def _prepare_build_dir(o):
+def _prepare_build_dir(o, win32=False):
     cmd = ['rsync', '-a', '--exclude', '.git', '--exclude', '*.pyc', '--exclude', '*.pyo']
+    if not win32:
+        cmd += ['--exclude', 'setup/win32']
     system(cmd + ['%s/' % o.odoo_dir, o.build_dir])
     for i in glob(join(o.build_dir, 'addons/*')):
         if i.split(os.path.sep)[-1] not in ADDONS_NOT_TO_PUBLISH:
@@ -458,6 +460,7 @@ def main():
             except Exception, e:
                 print("Won't publish the rpm release.\n Exception: %s" % str(e))
         if not o.no_windows:
+            _prepare_build_dir(o, win32=True)
             build_exe(o)
             try:
                 if not o.no_testing:

commit 1db27f417d20b92e6c390550dbe8365dfd7968f0

[FIX] Packaging: Windows: _prepare_build_dir
Catch shutil.Error thrown when addons have already been moved to
openerp/addons. Fixes the bug introduced by commit 91026647d43358594df0e17f3d408a3aa415c665.

setup/package.py
@@ -246,9 +246,14 @@ def _prepare_build_dir(o, win32=False):
     if not win32:
         cmd += ['--exclude', 'setup/win32']
     system(cmd + ['%s/' % o.odoo_dir, o.build_dir])
-    for i in glob(join(o.build_dir, 'addons/*')):
-        if i.split(os.path.sep)[-1] not in ADDONS_NOT_TO_PUBLISH:
-            shutil.move(i, join(o.build_dir, 'openerp/addons'))
+    try:
+        for addon_path in glob(join(o.build_dir, 'addons/*')):
+            if addon_path.split(os.path.sep)[-1] not in ADDONS_NOT_TO_PUBLISH:
+                shutil.move(addon_path, join(o.build_dir, 'openerp/addons'))
+    except shutil.Error:
+        # Thrown when the add-on is already in openerp/addons (if _prepare_build_dir
+        # has already been called once)
+        pass
 
 def build_tgz(o):
     system(['python2', 'setup.py', 'sdist', '--quiet', '--formats=gztar,zip'], o.build_dir)