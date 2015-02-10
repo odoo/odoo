8f7ceab30b585381350f89d7c3360f23206f8cba
Ravi Gohil <rgo@odoo.com>
2015-02-10 15:12:32 +0530

    [website_certification] Migrated to new api.

--- addons/website_certification/__init__.py
+++ addons/website_certification/__init__.py
@@ -1,8 +1,8 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-TODAY OpenERP S.A. <http://www.openerp.com>
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2004-TODAY Odoo S.A. (<https://www.odoo.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,5 +19,5 @@
 #
 ##############################################################################
 
-import models
 import controllers
+import models
