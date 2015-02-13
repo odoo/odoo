5b5c0d9d45347f4c1810dc8475237b8a11eb7993
odoo-tac <tac@odoo.com>
2015-02-13 17:17:00 +0100

    [IMP] Further refactoring and cleaning of project_timesheet UI

--- addons/project_timesheet/static/src/js/project_timesheet.js
+++ addons/project_timesheet/static/src/js/project_timesheet.js
@@ -2,15 +2,21 @@ openerp.project_timesheet = function(openerp) {
 
 	//Main widget to instantiate the app
 	openerp.project_timesheet.ProjectTimesheet = openerp.Widget.extend({
-		//template:"test_screen",
-		init: function(){
-
-		},
 		start: function(){
 			self = this;
-			this.load_template().then(function(){
-				self.build_widgets();
-			});
+
+			// Load session, if there is any :
+			this.session = new openerp.Session();
+            this.session.session_reload().then(function(){
+            	// Load (demo) data from local storage
+            	self.stored_data = JSON.parse(localStorage.getItem("pt_data"));
+             	self.user_local_data = _.findWhere(self.stored_data, {session_user : self.session.username})
+
+             	//Load templates for widgets
+            	self.load_template().then(function(){
+					self.build_widgets();
+				});
+            });
 		},
 		load_template: function(){
             var self = this;
@@ -19,30 +25,59 @@ openerp.project_timesheet = function(openerp) {
             });
         },
         build_widgets: function(){
-        	screenWidget = new openerp.project_timesheet.Activites_screen();	
-        	screenWidget.appendTo(this.$el);
+        	this.activities_screen = new openerp.project_timesheet.Activities_screen(this);
+        	this.activities_screen.appendTo(this.$el);
+        	this.activities_screen.show();
+
+        	this.day_planner_screen = new openerp.project_timesheet.Day_planner_screen(this);
+        	this.day_planner_screen.appendTo(this.$el);
+
+        	this.settings_screen = new openerp.project_timesheet.Settings_screen(this);
+        	this.settings_screen.appendTo(this.$el);
+        	debugger
         }
 	});
 
 	// Basic screen widget, inherited by all screens
 	openerp.project_timesheet.BasicScreenWidget = openerp.Widget.extend({
-		init: function(){
-
+		events:{
+            "click .pt_day_planner_link" : "goto_day_planner",
+            "click .pt_settings_link" : "goto_settings",
+            "click .pt_activities_link" : "goto_activities",
+            "click .pt_validate_btn" : "goto_activities"
+        },
+		init: function(parent){
+			this._super(parent);
 		},
 		start: function(){
-
-		}
+			this.hide();
+		},
+		show: function(){
+            this.$el.show();
+        },
+        hide: function(){
+            this.$el.hide();
+        },
+		goto_day_planner: function(){
+            this.hide();
+            this.getParent().day_planner_screen.show();
+        },
+        goto_settings: function(){
+        	this.hide();
+            this.getParent().settings_screen.show();
+        },
+        goto_activities: function(){
+        	this.hide();
+            this.getParent().activities_screen.show();
+        }
 	});
 
-	openerp.project_timesheet.Activites_screen = openerp.project_timesheet.BasicScreenWidget.extend({
+	openerp.project_timesheet.Activities_screen = openerp.project_timesheet.BasicScreenWidget.extend({
         template: "activities_screen",
-        init: function() {
+        init: function(parent) {
             self = this;
-            // self._super.apply(this, arguments);
-            // self.project_timesheet_widget = project_timesheet_widget;
             self.screen_name = "Activities";
-            // self.project_timesheet_model = options.project_timesheet_model;
-
+            self._super(parent);
             // Events specific to this screen
             _.extend(self.events,
                 {
@@ -63,8 +98,6 @@ openerp.project_timesheet = function(openerp) {
         },
         //Go to the create / edit activity
         goto_edit_activity_screen: function(){
-            // => chemin d'accès à l'activité du widget d'édition. à modifier avec les infos récupérées lors du click sur une acti
-            // this.project_timesheet_widget.edit_activity_screen.activity;
             this.project_timesheet_widget.edit_activity_screen.reset_activity();
             this.project_timesheet_widget.screen_selector.set_current_screen("edit_activity_screen");
         },
@@ -76,62 +109,63 @@ openerp.project_timesheet = function(openerp) {
         start_activity: function(){
 
         },
-        // test_fct: function(){
-        //     this.project_timesheet_model.load_server_data();
-        // }
+        test_fct: function(){
+        	//this.getParent();
+            // this.project_timesheet_model.load_server_data();
+        }
     });
 
-    // openerp.project_timesheet.Day_planner_screen = project_timesheet.BasicScreenWidget.extend({
-    //     template: "day_planner_screen",
-    //     init: function(project_timesheet_widget, options) {
-    //         this._super.apply(this, arguments);
-    //         this.project_timesheet_widget = project_timesheet_widget;
-    //         this.screen_name = "Day Planner";
+    openerp.project_timesheet.Day_planner_screen = openerp.project_timesheet.BasicScreenWidget.extend({
+        template: "day_planner_screen",
+        init: function(parent) {
+        	this._super(parent);
+            this.screen_name = "Day Planner";
+            _.extend(self.events,
+                {
+                    "click .pt_day_plan_select":"add_to_day_plan"
+                }
+            );
+            this.tasks = this.getParent().user_local_data.data.tasks;
+        },
+        add_to_day_plan: function(event){
+            console.log(event.currentTarget.dataset.task_id);
+        }
+    });
 
-    //         _.extend(self.events,
-    //             {
-    //                 "click .pt_day_plan_select":"add_to_day_plan"
-    //             }
-    //         );
-
-    //         this.tasks = options.project_timesheet_model.tasks;
-    //     },
-    //     add_to_day_plan: function(event){
-    //         console.log(event.currentTarget.dataset.task_id);
-    //     }
-    // });
-
-    // openerp.project_timesheet.Settings_screen = project_timesheet.BasicScreenWidget.extend({
-    //     template: "settings_screen",
-    //     init: function(project_timesheet_widget, options) {
-    //         this._super.apply(this, arguments);
-    //         this.project_timesheet_widget = project_timesheet_widget;
-    //         this.screen_name = "Settings";
-    //         this.project_timesheet_model = options.project_timesheet_model;
-
-    //         _.extend(self.events,
-    //             {
-    //                 "change input.pt_minimal_duration":"on_change_minimal_duration",
-    //                 "change input.pt_time_unit":"on_change_time_unit",
-    //                 "click .pt_default_project":"on_change_default_project"
-    //             }
-    //         );
-
-    //         this.settings = this.project_timesheet_model.settings;
-    //     },
-    //     on_change_minimal_duration: function(){
-    //         this.project_timesheet_model.set_minimal_duration(this.$("input.pt_minimal_duration").val());
-    //     },
-    //     on_change_time_unit: function(){
-    //         this.project_timesheet_model.set_time_unit(this.$("input.pt_time_unit").val());
-    //     },
-    //     on_change_default_project: function(event){
-    //         this.project_timesheet_model.set_default_project(event.target.dataset.project_id);
-    //         this.renderElement();
-    //         this.goto_settings();
-    //     }
-
-    // });
+    openerp.project_timesheet.Settings_screen = openerp.project_timesheet.BasicScreenWidget.extend({
+        template: "settings_screen",
+        init: function(parent) {
+            this._super(parent);
+            this.screen_name = "Settings";
+            _.extend(self.events,
+                {
+                    "change input.pt_minimal_duration":"on_change_minimal_duration",
+                    "change input.pt_time_unit":"on_change_time_unit",
+                    "click .pt_default_project":"on_change_default_project"
+                }
+            );
+            // Demo Data :
+            this.settings = {
+            	"default_project":{
+            		"id":1,
+            		"name":"Implementation"
+            	},
+            	"minimal_duration":15,
+            	"time_unit":15
+            };
+        },
+        on_change_minimal_duration: function(){
+            this.project_timesheet_model.set_minimal_duration(this.$("input.pt_minimal_duration").val());
+        },
+        on_change_time_unit: function(){
+            this.project_timesheet_model.set_time_unit(this.$("input.pt_time_unit").val());
+        },
+        on_change_default_project: function(event){
+            this.project_timesheet_model.set_default_project(event.target.dataset.project_id);
+            this.renderElement();
+            this.goto_settings();
+        }
+    });
 
     // openerp.project_timesheet.Edit_activity_screen = project_timesheet.BasicScreenWidget.extend({
     //     template: "edit_activity_screen",
