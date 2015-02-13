4ba88838e24fabc069102bd7f21e500ea6781e7a
sha-odoo <sha@odoo.com>
2015-02-13 16:37:49 +0530

    [MIG]website_mail_group : migrate to new api

--- addons/website_mail_group/controllers/main.py
+++ addons/website_mail_group/controllers/main.py
@@ -3,10 +3,9 @@
 import datetime
 from dateutil import relativedelta
 
-from openerp import tools, SUPERUSER_ID
-from openerp.addons.web import http
+from openerp import http, tools
 from openerp.addons.website.models.website import slug
-from openerp.addons.web.http import request
+from openerp.http import request
 
 
 class MailGroup(http.Controller):
@@ -14,59 +13,54 @@ class MailGroup(http.Controller):
     _replies_per_page = 10
 
     def _get_archives(self, group_id):
-        MailMessage = request.registry['mail.message']
+        MailMessage = request.env['mail.message']
         groups = MailMessage.read_group(
-            request.cr, request.uid, [('model', '=', 'mail.group'), ('res_id', '=', group_id)], ['subject', 'date'],
-            groupby="date", orderby="date desc", context=request.context)
+            [('model', '=', 'mail.group'), ('res_id', '=', group_id)],
+            fields=['subject', 'date'], groupby="date", orderby="date desc"
+        )
         for group in groups:
-            begin_date = datetime.datetime.strptime(group['__domain'][0][2], tools.DEFAULT_SERVER_DATETIME_FORMAT).date()
-            end_date = datetime.datetime.strptime(group['__domain'][1][2], tools.DEFAULT_SERVER_DATETIME_FORMAT).date()
+            begin_date = datetime.datetime.strptime(group['__domain'][0][2],
+                                                    tools.DEFAULT_SERVER_DATETIME_FORMAT).date()
+            end_date = datetime.datetime.strptime(group['__domain'][1][2],
+                                                  tools.DEFAULT_SERVER_DATETIME_FORMAT).date()
             group['date_begin'] = '%s' % datetime.date.strftime(begin_date, tools.DEFAULT_SERVER_DATE_FORMAT)
             group['date_end'] = '%s' % datetime.date.strftime(end_date, tools.DEFAULT_SERVER_DATE_FORMAT)
         return groups
 
     @http.route("/groups", type='http', auth="public", website=True)
     def view(self, **post):
-        cr, uid, context = request.cr, request.uid, request.context
-        group_obj = request.registry.get('mail.group')
-        mail_message_obj = request.registry.get('mail.message')
-        group_ids = group_obj.search(cr, uid, [('alias_id', '!=', False), ('alias_id.alias_name', '!=', False)], context=context)
-        groups = group_obj.browse(cr, uid, group_ids, context)
+        Group = request.env['mail.group']
+        MailMessage = request.env['mail.message']
+        groups = Group.search([('alias_id', '!=', False), ('alias_id.alias_name', '!=', False)])
 
         # compute statistics
         month_date = datetime.datetime.today() - relativedelta.relativedelta(months=1)
-        result = mail_message_obj.read_group(
-            cr, SUPERUSER_ID,
-            [('model', '=', 'mail.group'), ('date', '>=', month_date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT))],
-            [], ['res_id'],
-            context=context)
+        result = MailMessage.sudo().read_group([('model', '=', 'mail.group'),
+                                                     ('date', '>=',
+                                                      month_date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT))],
+                                                    fields=['res_id'], groupby=['res_id'])
         result = dict([(x['res_id'], x['res_id_count']) for x in result])
 
         group_data = dict()
         for group in groups:
             group_data[group.id] = {'monthly_message_nbr': result.get(group.id, 0)}
-
         values = {'groups': groups, 'group_data': group_data}
         return request.website.render('website_mail_group.mail_groups', values)
 
     @http.route(["/groups/subscription/"], type='json', auth="user")
     def subscription(self, group_id=0, action=False, **post):
         """ TDE FIXME: seems dead code """
-        cr, uid, context = request.cr, request.uid, request.context
-        group_obj = request.registry.get('mail.group')
+        Group = request.env['mail.group']
         if action:
-            group_obj.message_subscribe_users(cr, uid, [group_id], context=context)
+            Group.message_subscribe_users([group_id])
         else:
-            group_obj.message_unsubscribe_users(cr, uid, [group_id], context=context)
+            Group.message_unsubscribe_users([group_id])
         return []
 
-    @http.route([
-        "/groups/<model('mail.group'):group>",
-        "/groups/<model('mail.group'):group>/page/<int:page>"
-    ], type='http', auth="public", website=True)
+    @http.route(["/groups/<model('mail.group'):group>", "/groups/<model('mail.group'):group>/page/<int:page>"],
+                type='http', auth="public", website=True)
     def thread_headers(self, group, page=1, mode='thread', date_begin=None, date_end=None, **post):
-        cr, uid, context = request.cr, request.uid, request.context
-        thread_obj = request.registry.get('mail.message')
+        MailMessage = request.env['mail.message']
 
         domain = [('model', '=', 'mail.group'), ('res_id', '=', group.id)]
         if mode == 'thread':
@@ -74,7 +68,7 @@ class MailGroup(http.Controller):
         if date_begin and date_end:
             domain += [('date', '>=', date_begin), ('date', '<=', date_end)]
 
-        thread_count = thread_obj.search_count(cr, uid, domain, context=context)
+        thread_count = MailMessage.search_count(domain)
         pager = request.website.pager(
             url='/groups/%s' % slug(group),
             total=thread_count,
@@ -82,8 +76,7 @@ class MailGroup(http.Controller):
             step=self._thread_per_page,
             url_args={'mode': mode, 'date_begin': date_begin or '', 'date_end': date_end or ''},
         )
-        thread_ids = thread_obj.search(cr, uid, domain, limit=self._thread_per_page, offset=pager['offset'])
-        messages = thread_obj.browse(cr, uid, thread_ids, context)
+        messages = MailMessage.search(domain, limit=self._thread_per_page, offset=pager['offset'])
         values = {
             'messages': messages,
             'group': group,
@@ -96,24 +89,17 @@ class MailGroup(http.Controller):
         }
         return request.website.render('website_mail_group.group_messages', values)
 
-    @http.route([
-        '''/groups/<model('mail.group'):group>/<model('mail.message', "[('model','=','mail.group'), ('res_id','=',group[0])]"):message>''',
-    ], type='http', auth="public", website=True)
+    @http.route(['''/groups/<model('mail.group'):group>/<model('mail.message', "[('model','=','mail.group'), ('res_id','=',group[0])]"):message>''', ],
+                type='http', auth="public", website=True)
     def thread_discussion(self, group, message, mode='thread', date_begin=None, date_end=None, **post):
-        cr, uid, context = request.cr, request.uid, request.context
-        Message = request.registry['mail.message']
+        Message = request.env['mail.message']
         if mode == 'thread':
-            base_domain = [('model', '=', 'mail.group'), ('res_id', '=', group.id), ('parent_id', '=', message.parent_id and message.parent_id.id or False)]
+            base_domain = [('model', '=', 'mail.group'), ('res_id', '=', group.id),
+                           ('parent_id', '=', message.parent_id and message.parent_id.id or False)]
         else:
             base_domain = [('model', '=', 'mail.group'), ('res_id', '=', group.id)]
-        next_message = None
-        next_message_ids = Message.search(cr, uid, base_domain + [('date', '<', message.date)], order="date DESC", limit=1, context=context)
-        if next_message_ids:
-            next_message = Message.browse(cr, uid, next_message_ids[0], context=context)
-        prev_message = None
-        prev_message_ids = Message.search(cr, uid, base_domain + [('date', '>', message.date)], order="date ASC", limit=1, context=context)
-        if prev_message_ids:
-            prev_message = Message.browse(cr, uid, prev_message_ids[0], context=context)
+        next_message = Message.search(base_domain + [('date', '<', message.date)], order="date DESC", limit=1)
+        prev_message = Message.search(base_domain + [('date', '>', message.date)], order="date ASC", limit=1)
         values = {
             'message': message,
             'group': group,
@@ -127,18 +113,16 @@ class MailGroup(http.Controller):
         }
         return request.website.render('website_mail_group.group_message', values)
 
-    @http.route(
-        '''/groups/<model('mail.group'):group>/<model('mail.message', "[('model','=','mail.group'), ('res_id','=',group[0])]"):message>/get_replies''',
-        type='json', auth="public", methods=['POST'], website=True)
+    @http.route('''/groups/<model('mail.group'):group>/<model('mail.message', "[('model','=','mail.group'), ('res_id','=',group[0])]"):message>/get_replies''',
+                type='json', auth="public", methods=['POST'], website=True)
     def render_messages(self, group, message, **post):
         last_displayed_id = post.get('last_displayed_id')
         if not last_displayed_id:
             return False
-        Message = request.registry['mail.message']
+        Message = request.env['mail.message']
         replies_domain = [('id', '<', int(last_displayed_id)), ('parent_id', '=', message.id)]
-        msg_ids = Message.search(request.cr, request.uid, replies_domain, limit=self._replies_per_page, context=request.context)
-        msg_count = Message.search(request.cr, request.uid, replies_domain, count=True, context=request.context)
-        messages = Message.browse(request.cr, request.uid, msg_ids, context=request.context)
+        messages = Message.search(replies_domain, limit=self._replies_per_page)
+        msg_count = Message.search_count(replies_domain)
         values = {
             'group': group,
             'thread_header': message,
@@ -146,10 +130,11 @@ class MailGroup(http.Controller):
             'msg_more_count': msg_count - self._replies_per_page,
             'replies_per_page': self._replies_per_page,
         }
-        return request.registry['ir.ui.view'].render(request.cr, request.uid, 'website_mail_group.messages_short', values, engine='ir.qweb', context=request.context)
+        return request.env['ir.ui.view'].render('website_mail_group.messages_short', values)
 
     @http.route("/groups/<model('mail.group'):group>/get_alias_info", type='json', auth='public', website=True)
     def get_alias_info(self, group, **post):
         return {
-            'alias_name': group.alias_id and group.alias_id.alias_name and group.alias_id.alias_domain and '%s@%s' % (group.alias_id.alias_name, group.alias_id.alias_domain) or False
+            'alias_name': group.alias_id and group.alias_id.alias_name and group.alias_id.alias_domain and '%s@%s' % (
+                group.alias_id.alias_name, group.alias_id.alias_domain) or False
         }
--- addons/website_mail_group/models/mail_group.py
+++ addons/website_mail_group/models/mail_group.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import osv
-from openerp import tools
-from openerp.tools.translate import _
+from openerp import api, models, tools, _
 from openerp.tools.safe_eval import safe_eval as eval
 from openerp.addons.website.models.website import slug
 
-class MailGroup(osv.Model):
+
+class MailGroup(models.Model):
     _inherit = 'mail.group'
 
-    def message_get_email_values(self, cr, uid, id, notif_mail=None, context=None):
-        res = super(MailGroup, self).message_get_email_values(cr, uid, id, notif_mail=notif_mail, context=context)
-        group = self.browse(cr, uid, id, context=context)
-        base_url = self.pool['ir.config_parameter'].get_param(cr, uid, 'web.base.url')
+    @api.multi
+    def message_get_email_values(self, notif_mail=None):
+        res = super(MailGroup, self).message_get_email_values(notif_mail=notif_mail)[0]
+        base_url = self.env['ir.config_parameter'].get_param('web.base.url')
         headers = {}
         if res.get('headers'):
             try:
@@ -20,7 +19,7 @@ class MailGroup(osv.Model):
             except Exception:
                 pass
         headers.update({
-            'List-Archive': '<%s/groups/%s>' % (base_url, slug(group)),
+            'List-Archive': '<%s/groups/%s>' % (base_url, slug(self)),
             'List-Subscribe': '<%s/groups>' % (base_url),
             'List-Unsubscribe': '<%s/groups?unsubscribe>' % (base_url,),
         })
@@ -28,17 +27,18 @@ class MailGroup(osv.Model):
         return res
 
 
-class MailMail(osv.Model):
+class MailMail(models.Model):
     _inherit = 'mail.mail'
 
-    def send_get_mail_body(self, cr, uid, mail, partner=None, context=None):
+    @api.model
+    def send_get_mail_body(self, mail, partner=None):
         """ Short-circuit parent method for mail groups, replace the default
             footer with one appropriate for mailing-lists."""
 
         if mail.model == 'mail.group' and mail.res_id:
             # no super() call on purpose, no private links that could be quoted!
-            group = self.pool['mail.group'].browse(cr, uid, mail.res_id, context=context)
-            base_url = self.pool['ir.config_parameter'].get_param(cr, uid, 'web.base.url')
+            group = self.env['mail.group'].browse(mail.res_id)
+            base_url = self.env['ir.config_parameter'].get_param('web.base.url')
             vals = {
                 'maillist': _('Mailing-List'),
                 'post_to': _('Post to'),
@@ -55,6 +55,4 @@ class MailMail(osv.Model):
             body = tools.append_content_to_html(mail.body, footer, container_tag='div')
             return body
         else:
-            return super(MailMail, self).send_get_mail_body(cr, uid, mail,
-                                                            partner=partner,
-                                                            context=context)
+            return super(MailMail, self).send_get_mail_body(mail, partner=partner)
