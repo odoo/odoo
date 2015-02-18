4d59f715f21c6a0718ac409a0e7cfbfb6375fc72
hardik <hna@openerp.com>
2015-02-18 14:05:34 +0530

    [MIG] website_project migrated into new api

--- addons/website_project/controllers/main.py
+++ addons/website_project/controllers/main.py
@@ -1,32 +1,12 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013-Today OpenERP SA (<http://www.openerp.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+from openerp import http
+from openerp.http import request
 
-from openerp.addons.web import http
-from openerp.addons.web.http import request
 
-class website_project(http.Controller):
+class WebsiteProject(http.Controller):
 
     @http.route(['/project/<model("project.project"):project>'], type='http', auth="public", website=True)
     def project(self, project=None, **post):
-        cr, uid, context = request.cr, request.uid, request.context
         render_values = {
             'project': project,
             'main_object': project,
