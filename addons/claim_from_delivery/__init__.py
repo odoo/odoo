2e1f7841ff6b8886102a8d481e94cedee170eab2
rmu-odoo <rmu@openerp.com>
2015-02-19 16:40:50 +0530

    [MIG:migrate claim from delivery module]
    [claim on delivery new api]
    
    [IMP:updated with all asked changes]
    
    [modified the code]

--- addons/claim_from_delivery/__init__.py
+++ addons/claim_from_delivery/__init__.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 ##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2004-Today Odoo S.A. <http://www.odoo.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -14,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
