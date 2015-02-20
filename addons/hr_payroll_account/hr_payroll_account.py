c61cacee0e21e46a51a4a4289d7047ae58abd5a2
Cedric Snauwaert <csn@openerp.com>
2015-02-20 10:58:40 +0100

    [FIX] account.tax.code does not exist anymore

--- addons/hr_payroll_account/hr_payroll_account.py
+++ addons/hr_payroll_account/hr_payroll_account.py
@@ -178,7 +178,7 @@ class hr_salary_rule(osv.osv):
     _inherit = 'hr.salary.rule'
     _columns = {
         'analytic_account_id':fields.many2one('account.analytic.account', 'Analytic Account'),
-        'account_tax_id':fields.many2one('account.tax.code', 'Tax Code'),
+        'account_tax_id':fields.many2one('account.tax', 'Tax'),
         'account_debit': fields.many2one('account.account', 'Debit Account', domain=[('deprecated', '=', False)]),
         'account_credit': fields.many2one('account.account', 'Credit Account', domain=[('deprecated', '=', False)]),
     }
