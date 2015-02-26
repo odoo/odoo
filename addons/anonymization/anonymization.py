10a1d443891001b1ba2094600e8635b59901f33d
Vijay Baladaniya <vba@tinyerp.com>
2015-02-26 11:17:06 +0530

    [FIX] Change the target_version for anonymization.
    Fixed #5476

--- addons/anonymization/anonymization.py
+++ addons/anonymization/anonymization.py
@@ -538,7 +538,7 @@ class ir_model_fields_anonymize_wizard(osv.osv_memory):
             data = pickle.loads(base64.decodestring(wizard.file_import))
 
             migration_fix_obj = self.pool.get('ir.model.fields.anonymization.migration.fix')
-            fix_ids = migration_fix_obj.search(cr, uid, [('target_version', '=', '7.0')])
+            fix_ids = migration_fix_obj.search(cr, uid, [('target_version', '=', '8.0')])
             fixes = migration_fix_obj.read(cr, uid, fix_ids, ['model_name', 'field_name', 'query', 'query_type', 'sequence'])
             fixes = group(fixes, ('model_name', 'field_name'))
 
