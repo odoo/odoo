81999cd58e033508a55b29943609b6be3d8e106c
Bhavik Bagdiya <bba@odoo.com>
2015-02-27 14:59:26 +0530

    [ADD] website_sale_coupon : add Coupons mechanism concept

--- addons/website_sale/res_config.py
+++ addons/website_sale/res_config.py
@@ -7,3 +7,6 @@ class website_sale_configuration(models.TransientModel):
 
     module_website_sale_digital = fields.Boolean('Sell digital products', 
         help="Allow you to set mark a product as a digital product, allowing customers that have purchased the product to download its attachments. This installs the module website_sale_digital.")
+    module_website_sale_coupon = fields.Boolean(string='Pre Sale Voucher',
+        help='- Allocates a coupon code to Customer which can be applied on successive purchase(s) of a particular Product.\n'
+        '- This installs the module website_sale_coupon.')
