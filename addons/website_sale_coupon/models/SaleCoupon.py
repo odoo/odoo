28f302710133cf722a04b829a31cf2af4d66ad2c
pgu-odoo <pgu@openerp.com>
2015-03-02 12:07:38 +0530

    [IMP] : Recovering model design

--- addons/website_sale_coupon/models/SaleCoupon.py
+++ addons/website_sale_coupon/models/SaleCoupon.py
@@ -39,15 +39,15 @@ class SaleReward(models.Model):
                                     ('discount', 'Discount'),
                                     ('coupon', 'Coupon')], string="Reward Type", help="Type of reward to give to customer")
     reward_shipping_free = fields.Boolean(string="Free Shipping", default=False, help="Shipment of the order is free or not")
-   # reward_gift_product_id = fields.Many2One('product.product', string="Gift Product")
+    reward_product_product_id = fields.Many2one('product.product', string="Reward Product")
     reward_quantity = fields.Integer(string="Product Quantity")
-   # reward_product_uom_id = fields.Many2One('product.uom')
-    #reward_gift_coupon_id = fields.Many2One('WebsiteSaleCoupon.SaleCouponProgram', string="Coupon Id")
+    reward_product_uom_id = fields.Many2one('product.uom')
+    #reward_gift_coupon_id = fields.Many2one('sale.couponprogram', string="Coupon Id")
     reward_discount_type = fields.Selection([('no', 'No'), ('percentage', 'Percentage'),
                                            ('amount', 'Amount')], string="Discount Type")
     reward_discount = fields.Float("Discount", help='The discount in percentage or fixed amount')
     reward_discount_on = fields.Selection([('cart'), ('cheapest_product'), ('specific_product')], string="Discount On")
-    #reward_discount_on_product_id = fields.Many2One('product.product', string="Product")
+    reward_discount_on_product_id = fields.Many2one('product.product', string="Product")
     reward_tax = fields.Selection([('tax_included', 'Tax included'),
                                    ('tax_excluded', 'Tax excluded')])
     reward_partial_use = fields.Boolean("The reward can be used partially or not")
@@ -57,13 +57,13 @@ class SaleCoupon(models.Model):
     _name = 'sale.coupon'
     _description = "Sales Coupon"
 
-    #program_id = fields.Many2One('WebsiteSaleCoupon.SaleCouponProgram')
+    program_id = fields.Many2one('sale.couponprogram', string="Program Id")
     coupon_code = fields.Char(string="Coupon Code",
                               default=lambda self: 'SC' + (hashlib.sha1(str(random.getrandbits(256)).encode('utf-8')).hexdigest()[:7]).upper(),
                               required=True, readonly=True, help="Coupon Code")
     nbr_used = fields.Integer(string="Number of times coupon is used")
     nbr_uses = fields.Integer(string="Number of times coupon can be use")
-    #order_line_id = fields.One2Many('sale.order.line')
+    #order_line_id = fields.One2many('sale.coupon.orderline', string="Sale order line")
     state = fields.Selection([
                              ('new', 'New'),
                              ('used', 'Used'),
@@ -72,8 +72,25 @@ class SaleCoupon(models.Model):
                              default='new')
     ean13 = fields.Char(string="Bar Code")
     origin = fields.Char(string="Origin", help="Coupon is originated from")
-    #origin_order_id = fields.Many2One('sale.order', string='Order Reference', readonly=True, help="The Sales order id from which coupon is generated")
+    origin_order_id = fields.Many2one('sale.order', string='Order Reference', readonly=True, help="The Sales order id from which coupon is generated")
     txt_reward = fields.Char(string="Reward", help="Reward on coupon")
+
+
+class SaleCouponProgram(models.Model):
+    _name = 'sale.couponprogram'
+    _description = "Sales Coupon Program"
+
+    program_name = fields.Char(help="Program name")
+    program_code = fields.Char(string="Code", help="Unique code to provide the reward")
+    program_type = fields.Selection([('apply immediately', 'Apply Immediately'), ('public unique code',
+                                     'Public Unique Code'), ('generated coupon', 'Generated Coupon')],
+                                    string="Program Type", help="The type of the coupon program")
+    is_program_active = fields.Boolean(string="Active", default=True, help="Coupon program is active or inactive")
+    program_sequence = fields.Integer(string="Sequence", help="According to sequence, one rule is selected from multiple defined rules to apply")
+    # Getting the error here
+    coupon_ids = fields.One2many('sale.coupon', string="Coupon Id")
+    applicability_id = fields.Many2one('sale.applicability', string="Applicability Id")
+    reward_id = fields.Many2one('sale.reward', string="Reward Id")
 
 
 class SaleOrderLine(models.Model):
--- addons/website_sale_coupon/models/__init__.py
+++ addons/website_sale_coupon/models/__init__.py
@@ -1,2 +1,1 @@
 import SaleCoupon
-import SaleCoupon_Program
