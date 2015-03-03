87bc14644a9ca8fe32bfa87270347fb4a8cc7ea1
Damien Bouvy <dbo@odoo.com>
2015-03-03 15:06:34 +0100

    [IMP] website_quote: - add on_change_uom behaviour to quote templates                      - fixes frontend layout issues                      - use action_button_confirm instead of signal_workflow when confirming the quote in the frontend to trigger all events properly

--- addons/website_quote/controllers/main.py
+++ addons/website_quote/controllers/main.py
@@ -72,7 +72,7 @@ class sale_quote(http.Controller):
         if token != order.access_token:
             return request.website.render('website.404')
         attachments=sign and [('signature.png', sign.decode('base64'))] or []
-        order_obj.signal_workflow(request.cr, SUPERUSER_ID, [order_id], 'order_confirm', context=request.context)
+        order_obj.action_button_confirm(request.cr, SUPERUSER_ID, [order_id], context=request.context)
         message = _('Order signed by %s') % (signer,)
         self.__message_post(message, order_id, type='comment', subtype='mt_comment', attachments=attachments)
         return True
