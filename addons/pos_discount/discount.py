479ba22b5c588410dd1a0f0c598effc3e93c3ac9
Frédéric van der Essen <fvdessen@gmail.com>
2015-03-03 11:11:55 +0100

    [IMP] pos_discount: variable percentage on order discounts

--- addons/pos_discount/discount.py
+++ addons/pos_discount/discount.py
@@ -30,9 +30,11 @@ from openerp.tools.translate import _
 class pos_config(osv.osv):
     _inherit = 'pos.config' 
     _columns = {
-        'discount_pc': fields.float('Discount Percentage', help='The discount percentage'),
+        'iface_discount': fields.boolean('Order Discounts', help='Allow the cashier to give discounts on the whole order.'),
+        'discount_pc': fields.float('Discount Percentage', help='The default discount percentage'),
         'discount_product_id': fields.many2one('product.product','Discount Product', help='The product used to model the discount'),
     }
     _defaults = {
+        'iface_discount': True,
         'discount_pc': 10,
     }
