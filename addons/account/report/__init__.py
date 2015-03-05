1a920821f79bb3b493e4989a42145431bf478fab
pvy <pvy@odoo.com>
2015-03-05 17:09:04 +0100

    [REM] account : removed unused reports

--- addons/account/report/__init__.py
+++ addons/account/report/__init__.py
@@ -19,15 +19,6 @@
 #
 ##############################################################################
 
-import account_balance
-import account_partner_balance
-import account_general_ledger
-import account_partner_ledger
-import account_print_overdue
-import account_aged_partner_balance
-import account_invoice_report
-import account_entries_report
-import account_analytic_entries_report
 import account_report_context_common
 import account_financial_report
 import account_generic_tax_report
