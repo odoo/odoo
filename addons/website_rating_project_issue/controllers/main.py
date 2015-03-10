f798d843228b93d9062d492e38bfc7eeab2f2dfd
Jérome Maes <jem@openerp.com>
2015-03-10 11:11:59 +0100

    [FIX] website_rating_project_issue : fix the domain to retrieve rating of particular project and use website.image_url to display user avatar.

--- addons/website_rating_project_issue/controllers/main.py
+++ addons/website_rating_project_issue/controllers/main.py
@@ -9,21 +9,22 @@ class WebsiteRatingProject(http.Controller):
 
     @http.route(['/project/rating/'], type='http', auth="public", website=True)
     def index(self, **kw):
-        projects = request.env['project.project'].search([])
+        projects = request.env['project.project'].search([('is_visible_happy_customer', '=', 1)])
         values = {'projects' : projects}
         return request.website.render('website_rating_project_issue.index', values)
 
     @http.route(['''/project/rating/<model("project.project", "[('is_visible_happy_customer','=',1)]"):project>'''], type='http', auth="public", website=True)
     def page(self, project=None, **kw):
         # create domain for rating
-        domain = [('res_model', '=', 'project.issue')]
+        issues = request.env['project.issue'].search([('project_id', '=', project.id)])
+        domain = [('res_model', '=', 'project.issue'), ('res_id', 'in', issues.ids)]
         ratings = request.env['rating.rating'].search(domain, order="id desc", limit=100)
 
         yesterday = (datetime.date.today()-datetime.timedelta(days=-1)).strftime('%Y-%m-%d 23:59:59')
         stats = {}
         for x in (7, 30, 90):
-            todate = (datetime.date.today()-datetime.timedelta(days=-x)).strftime('%Y-%m-%d 00:00:00')
-            domdate = domain + [('create_date','<=',yesterday),('create_date','>=',todate)]
+            todate = (datetime.date.today()-datetime.timedelta(days=x)).strftime('%Y-%m-%d 00:00:00')
+            domdate = domain + [('create_date', '<=', yesterday), ('create_date', '>=', todate)]
             stats[x] = {0: 0, 5: 0, 10: 0}
             rating_stats = request.env['rating.rating'].read_group(domdate, [], ['rating'])
             total = reduce(lambda x, y: y['rating_count']+x, rating_stats, 0)
