81333ade84f8e422599bcf82ded414ea21ea0f88
Géry Debongnie <ged@odoo.com>
2015-03-10 08:48:09 +0100

    [REF] mass_mailing: update to the new module system

--- addons/mass_mailing/static/src/js/mass_mailing.js
+++ addons/mass_mailing/static/src/js/mass_mailing.js
@@ -1,22 +1,28 @@
-openerp.mass_mailing = function (instance) {
-    var _t = instance.web._t;
+odoo.define('mass_mailing.mass_mailing', ['web.core', 'web_kanban.common', 'web_kanban.KanbanView'], function (require) {
 
-    openerp.web_kanban.KanbanRecord.include({
-        on_card_clicked: function (event) {
-            if (this.view.dataset.model === 'mail.mass_mailing.campaign') {
-                this.$('.oe_mailings').click();
-            } else {
-                this._super.apply(this, arguments);
-            }
-        },
-    });
+var core = require('web.core');
+var common = require('web_kanban.common');
+var KanbanView = require('web_kanban.KanbanView');
 
-    openerp.web_kanban.KanbanView.include({
-        on_groups_started: function() {
+var _t = core._t;
+
+common.KanbanRecord.include({
+    on_card_clicked: function (event) {
+        if (this.view.dataset.model === 'mail.mass_mailing.campaign') {
+            this.$('.oe_mailings').click();
+        } else {
             this._super.apply(this, arguments);
-            if (this.dataset.model === 'mail.mass_mailing') {  
-                this.$el.find('.oe_kanban_draghandle').removeClass('oe_kanban_draghandle');
-            }
-        },
-    });
-};
+        }
+    },
+});
+
+KanbanView.include({
+    on_groups_started: function() {
+        this._super.apply(this, arguments);
+        if (this.dataset.model === 'mail.mass_mailing') {  
+            this.$el.find('.oe_kanban_draghandle').removeClass('oe_kanban_draghandle');
+        }
+    },
+});
+
+});
