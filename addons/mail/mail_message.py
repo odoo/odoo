287fc204fbc9b635da0f2f42e8476d8ce988b02d
Valerie Pirenne <vpi@odoo.com>
2015-03-12 14:02:28 +0100

    [WIP] mail: timeline view refactoring for messaging

--- addons/mail/mail_message.py
+++ addons/mail/mail_message.py
@@ -747,7 +747,7 @@ class mail_message(osv.Model):
                                     'parent_id': parent_id});
 
         nb_read = 0
-        if 'mail_read_set_read' in context and context['mail_read_set_read']: 
+        if context and 'mail_read_set_read' in context and context['mail_read_set_read']: 
             nb_read = self.set_message_read(cr, uid, ids, True, create_missing=False, context=context)
 
         return {'nb_read': nb_read, 'threads': parent_list}
