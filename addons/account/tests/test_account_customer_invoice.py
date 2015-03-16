fb59f1b883f20a78375745a402a5ee7a9a08a4ad
Arthur Maniet <arthurmaniet@me.com>
2015-03-16 10:08:41 +0100

    [FIX] tests

--- addons/account/tests/test_account_customer_invoice.py
+++ addons/account/tests/test_account_customer_invoice.py
@@ -90,7 +90,7 @@ class TestAccountCustomerInvoive(AccountTestUsers):
 
         # I totally pay the Invoice
         pay = self.account_invoice_customer0.pay_and_reconcile(
-            10050.0, datetime.date.today(), self.env.ref('account.bank_journal').id,
+            10050.0, datetime.date.today(), self.env.ref('account.bank_journal'),
         )
 
         # I verify that invoice is now in Paid state
