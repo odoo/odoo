7780004f95203fb9f86273eea9ea81a261277a52
Christophe Simonis <chs@odoo.com>
2015-03-19 18:41:45 +0100

    [FIX] web: correct many2one dropdown visibility.
    The dropdown should only be append to visible elements.

--- addons/web/static/src/js/view_form.js
+++ addons/web/static/src/js/view_form.js
@@ -3740,9 +3740,9 @@ instance.web.form.FieldMany2One = instance.web.form.AbstractField.extend(instanc
             minLength: 0,
             delay: 200,
         });
-        var appendTo = this.$el.parents('.oe_view_manager_body, .modal-dialog').last();
+        var appendTo = this.$el.parents('.oe_view_manager_body:visible, .modal-dialog:visible').last();
         if (appendTo.length === 0){
-            appendTo = '.oe_application > *';
+            appendTo = '.oe_application > *:visible:last';
         }
         this.$input.autocomplete({
             appendTo: appendTo
