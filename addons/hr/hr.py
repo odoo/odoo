26c44a58df2cab1790f78ed127390075f84c1de7
Richard Mathot <rim@odoo.com>
2015-03-23 12:11:41 +0100

    [FIX] hr: allow translating job offers

--- addons/hr/hr.py
+++ addons/hr/hr.py
@@ -95,7 +95,7 @@ class hr_job(osv.Model):
     _description = "Job Position"
     _inherit = ['mail.thread']
     _columns = {
-        'name': fields.char('Job Name', required=True, select=True),
+        'name': fields.char('Job Name', required=True, select=True, translate=True),
         'expected_employees': fields.function(_get_nbr_employees, string='Total Forecasted Employees',
             help='Expected number of employees for this job position after new recruitment.',
             store = {
