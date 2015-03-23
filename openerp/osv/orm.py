a06b467645d4f9b4a6d0e5507b185eca0a4bdb9f
Yuriy A. Apollov <apollovy@gmail.com>
2015-03-23 18:45:07 +0300

    Fix iteraton by browse_null object lasts forever.

--- openerp/osv/orm.py
+++ openerp/osv/orm.py
@@ -289,6 +289,9 @@ class browse_null(object):
     def __unicode__(self):
         return u''
 
+    def __iter__(self):
+        raise NotImplementedError("Iteration is not allowed on browse_null")
+
 
 #
 # TODO: execute an object method on browse_record_list
