58d6bb30ab9dd101285def6db20b9db5df300240
Raphael Collet <rco@openerp.com>
2015-03-26 14:55:03 +0100

    [FIX] ormcache: fix calls to `ormcache` with unexpected context parameter
    The implementation of ormcache does not work on methods that take a context
    parameter. Because of the decorator decorator, the arguments of the call are
    passed positionally to the method ormcache.lookup, and positional arguments
    are used in the cache key.
    
    The fix consists in removing the context parameter from the faulty methods,
    either directly, or by caching a private method called by the public method.

--- addons/website/models/ir_http.py
+++ addons/website/models/ir_http.py
@@ -37,10 +37,9 @@ class ir_http(orm.AbstractModel):
 
     def _auth_method_public(self):
         if not request.session.uid:
-            domain_name = request.httprequest.environ.get('HTTP_HOST', '').split(':')[0]
-            website_id = self.pool['website']._get_current_website_id(request.cr, openerp.SUPERUSER_ID, domain_name, context=request.context)
-            if website_id:
-                request.uid = self.pool['website'].browse(request.cr, openerp.SUPERUSER_ID, website_id, request.context).user_id.id
+            website = self.pool['website'].get_current_website(request.cr, openerp.SUPERUSER_ID, context=request.context)
+            if website:
+                request.uid = website.user_id.id
             else:
                 request.uid = self.pool['ir.model.data'].xmlid_to_res_id(request.cr, openerp.SUPERUSER_ID, 'base', 'public_user')
         else:
