odoo/odoo

commit 27591ec937ed36ddee667d9b3694667bc26c6466

[FIX] account: Fixed the regression that were introduced during last merge with master.

addons/account/res_config.py
@@ -131,11 +131,11 @@ class AccountConfigSettings(models.TransientModel):
     income_currency_exchange_account_id = fields.Many2one('account.account',
         related='company_id.income_currency_exchange_account_id',
         string="Gain Exchange Rate Account",
-        domain="[('type', '=', 'other'), ('company_id', '=', company_id)]]")
+        domain=[('internal_type', '=', 'other'), ('company_id', '=', company_id), ('deprecated', '=', False)])
     expense_currency_exchange_account_id = fields.Many2one('account.account',
         related='company_id.expense_currency_exchange_account_id',
         string="Loss Exchange Rate Account",
-        domain="[('type', '=', 'other'), ('company_id', '=', company_id)]]")
+        domain=[('internal_type', '=', 'other'), ('company_id', '=', company_id), ('deprecated', '=', False)])
 
     @api.multi
     @api.constrains('income_currency_exchange_account_id', 'expense_currency_exchange_account_id')

addons/account/data/configurable_account_chart.xml
@@ -69,10 +69,6 @@
         </record>
 
         <record id="transfer_account_id" model="account.account.template">
-            
-        </record>
-
-        <record id="transfer_account_id" model="account.account.template">
             <field name="code">1017</field>
             <field name="name">Liquidity Transfers</field>
             <field name="reconcile" eval='True'/>

commit f912c0650d4017bb2c5185c208bb674aaa3d38dd

[FIX] account: Fix account settings menu
Fix a bug introduced in commit 27591ec937ed3

addons/account/res_config.py
@@ -131,11 +131,11 @@ class AccountConfigSettings(models.TransientModel):
     income_currency_exchange_account_id = fields.Many2one('account.account',
         related='company_id.income_currency_exchange_account_id',
         string="Gain Exchange Rate Account",
-        domain=[('internal_type', '=', 'other'), ('company_id', '=', company_id), ('deprecated', '=', False)])
+        domain=lambda self: [('internal_type', '=', 'other'), ('company_id', '=', self.company_id.id), ('deprecated', '=', False)])
     expense_currency_exchange_account_id = fields.Many2one('account.account',
         related='company_id.expense_currency_exchange_account_id',
         string="Loss Exchange Rate Account",
-        domain=[('internal_type', '=', 'other'), ('company_id', '=', company_id), ('deprecated', '=', False)])
+        domain=lambda self: [('internal_type', '=', 'other'), ('company_id', '=', self.company_id.id), ('deprecated', '=', False)])
 
     @api.multi
     @api.constrains('income_currency_exchange_account_id', 'expense_currency_exchange_account_id')