49e7e67a27b3d393a8234bc2c466b0c642ce157b
Martin Trigaux <mat@odoo.com>
2015-04-02 15:00:05 +0200

    [FIX] mrp: allow same product on different bom line
    Relax the constraint on BoM to allow to have two different lines with the same
    product. As the error message says, the purpose of the constraint was to forbid
    having the manufactured product as one of the components but had this side
    effect.
    Such scenario of the same product twice makes sense when using the date
    attributes on the lines (e.g. changing quantities)
    opw 621468

--- addons/mrp/mrp.py
+++ addons/mrp/mrp.py
@@ -257,19 +257,17 @@ class mrp_bom(osv.osv):
         return True
 
     def _check_product(self, cr, uid, ids, context=None):
-        all_prod = []
         boms = self.browse(cr, uid, ids, context=context)
-        def check_bom(boms):
+        def check_bom(boms, all_prod):
             res = True
             for bom in boms:
                 if bom.product_id.id in all_prod:
-                    res = res and False
-                all_prod.append(bom.product_id.id)
-                lines = bom.bom_lines
-                if lines:
-                    res = res and check_bom([bom_id for bom_id in lines if bom_id not in boms])
+                    return False
+                if bom.bom_lines:
+                    res = res and check_bom([b for b in bom.bom_lines if b not in boms], all_prod + [bom.product_id.id])
             return res
-        return check_bom(boms)
+        return check_bom(boms, [])
+
 
     _constraints = [
         (_check_recursion, 'Error ! You cannot create recursive BoM.', ['parent_id']),
