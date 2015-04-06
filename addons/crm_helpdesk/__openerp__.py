b92ec7748453dc630003ce4088881eb2382d42a8
Amit Dodiya <ado@odoo.com>
2015-04-06 15:25:54 +0530

    [MOVE] change module structure

--- addons/crm_helpdesk/__openerp__.py
+++ addons/crm_helpdesk/__openerp__.py
@@ -13,16 +13,16 @@ to trace your interventions. This menu is more adapted to oral communication,
 which is not necessarily related to a claim. Select a customer, add notes
 and categorize your interventions with a channel and a priority level.
     """,
-    'author': 'Odoo SA',
+    'author': 'Odoo S.A.',
     'website': 'https://www.odoo.com',
     'depends': ['crm'],
     'data': [
-        'crm_helpdesk_view.xml',
-        'crm_helpdesk_menu.xml',
+        'views/crm_helpdesk_view.xml',
+        'views/crm_helpdesk_menu.xml',
         'security/ir.model.access.csv',
         'report/crm_helpdesk_report_view.xml',
-        'crm_helpdesk_data.xml',
+        'data/crm_helpdesk_data.xml',
     ],
-    'demo': ['crm_helpdesk_demo.xml'],
+    'demo': ['data/crm_helpdesk_demo.xml'],
     'installable': True,
 }
