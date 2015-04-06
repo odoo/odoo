12819ebe957a0bfcd181d3acd57e31ee41dc11ca
rsi-odoo <rsi@openerp.com>
2015-04-06 15:59:57 +0530

    [IMP] website_hr_recruitment: RENAMED files according to new api guidelines

--- addons/website_hr_recruitment/__openerp__.py
+++ addons/website_hr_recruitment/__openerp__.py
@@ -10,12 +10,12 @@ Odoo Contact Form
         """,
     'depends': ['website_partner', 'hr_recruitment', 'website_mail', 'website_form'],
     'data': [
-        'security/ir.model.access.csv',
         'security/website_hr_recruitment_security.xml',
+        'security/ir.model.access.csv',
         'data/config_data.xml',
         'views/hr_job_views.xml',
-        'views/templates.xml',
-        'views/hr_recruitment_view.xml',
+        'views/hr_recruitment_views.xml',
+        'views/website_hr_recruitment_templates.xml'
     ],
     'demo': [
         'data/hr_job_demo.xml',
