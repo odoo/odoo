Odew/odoo-1

[MIG]: migrated crm_helpdesk into new api

addons/crm_helpdesk/report/crm_helpdesk_report.py
@@ -1,63 +1,41 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
-from openerp.osv import fields,osv
+from openerp import api, fields, models
 from openerp import tools
 
 
-AVAILABLE_STATES = [
-    ('draft','Draft'),
-    ('open','Open'),
-    ('cancel', 'Cancelled'),
-    ('done', 'Closed'),
-    ('pending','Pending')
-]
-
-class crm_helpdesk_report(osv.osv):
+class CrmHelpdeskReport(models.Model):
     """ Helpdesk report after Sales Services """
 
     _name = "crm.helpdesk.report"
     _description = "Helpdesk report after Sales Services"
     _auto = False
 
-    _columns = {
-        'date': fields.datetime('Date', readonly=True),
-        'user_id':fields.many2one('res.users', 'User', readonly=True),
-        'team_id':fields.many2one('crm.team', 'Team', readonly=True, oldname='section_id'),
-        'nbr': fields.integer('# of Requests', readonly=True),  # TDE FIXME master: rename into nbr_requests
-        'state': fields.selection(AVAILABLE_STATES, 'Status', readonly=True),
-        'delay_close': fields.float('Delay to Close',digits=(16,2),readonly=True, group_operator="avg"),
-        'partner_id': fields.many2one('res.partner', 'Partner' , readonly=True),
-        'company_id': fields.many2one('res.company', 'Company', readonly=True),
-        'date_deadline': fields.date('Deadline', select=True),
-        'priority': fields.selection([('5', 'Lowest'), ('4', 'Low'), \
-                    ('3', 'Normal'), ('2', 'High'), ('1', 'Highest')], 'Priority'),
-        'channel_id': fields.many2one('utm.medium', 'Channel'),
-        'categ_id': fields.many2one('crm.helpdesk.category', 'Category'),
-        'planned_cost': fields.float('Planned Costs'),
-        'create_date': fields.datetime('Creation Date' , readonly=True, select=True),
-        'date_closed': fields.datetime('Close Date', readonly=True, select=True),
-        'delay_expected': fields.float('Overpassed Deadline',digits=(16,2),readonly=True, group_operator="avg"),
-        'email': fields.integer('# Emails', size=128, readonly=True),
-    }
+    date = fields.Datetime(string='Date', readonly=True)
+    user_id = fields.Many2one('res.users', string='User', readonly=True)
+    team_id = fields.Many2one('crm.team', string='Team', oldname='section_id', readonly=True)
+    nbr_requests = fields.Integer(string='# of Requests', readonly=True)
+    state = fields.Selection([('draft', 'Draft'),
+                            ('open', 'Open'),
+                            ('cancel', 'Cancelled'),
+                            ('done', 'Closed'),
+                            ('pending', 'Pending')], string='Status', readonly=True)
+    delay_close = fields.Float(string='Delay to Close', digits=(16,2), readonly=True, group_operator="avg")
+    partner_id = fields.Many2one('res.partner', string='Partner', readonly=True)
+    company_id = fields.Many2one('res.company', string='Company', readonly=True)
+    date_deadline = fields.Date(string='Deadline', select=True)
+    priority = fields.Selection([('5', 'Lowest'),
+                                ('4', 'Low'),
+                                ('3', 'Normal'),
+                                ('2', 'High'),
+                                ('1', 'Highest')], string='Priority')
+    channel_id = fields.Many2one('utm.medium', string='Channel')
+    categ_id = fields.Many2one('crm.helpdesk.category', string='Category')
+    planned_cost = fields.Float(string='Planned Costs')
+    create_date = fields.Datetime(string='Creation Date', readonly=True, select=True)
+    date_closed = fields.Datetime(string='Close Date', readonly=True, select=True)
+    delay_expected = fields.Float(string='Overpassed Deadline', digits=(16,2), readonly=True, group_operator="avg")
+    email = fields.Integer(string='# Emails', size=128, readonly=True)
 
     def init(self, cr):
 
@@ -84,7 +62,7 @@ class crm_helpdesk_report(osv.osv):
                     c.categ_id,
                     c.channel_id,
                     c.planned_cost,
-                    count(*) as nbr,
+                    count(*) as nbr_requests,
                     extract('epoch' from (c.date_closed-c.create_date))/(3600*24) as  delay_close,
                     (SELECT count(id) FROM mail_message WHERE model='crm.helpdesk' AND res_id=c.id AND type = 'email') AS email,
                     abs(avg(extract('epoch' from (c.date_deadline - c.date_closed)))/(3600*24)) as delay_expected

odoo/odoo

addons/crm_helpdesk/report/crm_helpdesk_report.py
@@ -1,63 +1,41 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
-from openerp.osv import fields,osv
+from openerp import api, fields, models
 from openerp import tools
 
 
-AVAILABLE_STATES = [
-    ('draft','Draft'),
-    ('open','Open'),
-    ('cancel', 'Cancelled'),
-    ('done', 'Closed'),
-    ('pending','Pending')
-]
-
-class crm_helpdesk_report(osv.osv):
+class CrmHelpdeskReport(models.Model):
     """ Helpdesk report after Sales Services """
 
     _name = "crm.helpdesk.report"
     _description = "Helpdesk report after Sales Services"
     _auto = False
 
-    _columns = {
-        'date': fields.datetime('Date', readonly=True),
-        'user_id':fields.many2one('res.users', 'User', readonly=True),
-        'team_id':fields.many2one('crm.team', 'Team', readonly=True, oldname='section_id'),
-        'nbr': fields.integer('# of Requests', readonly=True),  # TDE FIXME master: rename into nbr_requests
-        'state': fields.selection(AVAILABLE_STATES, 'Status', readonly=True),
-        'delay_close': fields.float('Delay to Close',digits=(16,2),readonly=True, group_operator="avg"),
-        'partner_id': fields.many2one('res.partner', 'Partner' , readonly=True),
-        'company_id': fields.many2one('res.company', 'Company', readonly=True),
-        'date_deadline': fields.date('Deadline', select=True),
-        'priority': fields.selection([('5', 'Lowest'), ('4', 'Low'), \
-                    ('3', 'Normal'), ('2', 'High'), ('1', 'Highest')], 'Priority'),
-        'channel_id': fields.many2one('utm.medium', 'Channel'),
-        'categ_id': fields.many2one('crm.helpdesk.category', 'Category'),
-        'planned_cost': fields.float('Planned Costs'),
-        'create_date': fields.datetime('Creation Date' , readonly=True, select=True),
-        'date_closed': fields.datetime('Close Date', readonly=True, select=True),
-        'delay_expected': fields.float('Overpassed Deadline',digits=(16,2),readonly=True, group_operator="avg"),
-        'email': fields.integer('# Emails', size=128, readonly=True),
-    }
+    date = fields.Datetime(string='Date', readonly=True)
+    user_id = fields.Many2one('res.users', string='User', readonly=True)
+    team_id = fields.Many2one('crm.team', string='Team', oldname='section_id', readonly=True)
+    nbr_requests = fields.Integer(string='# of Requests', readonly=True)
+    state = fields.Selection([('draft', 'Draft'),
+                            ('open', 'Open'),
+                            ('cancel', 'Cancelled'),
+                            ('done', 'Closed'),
+                            ('pending', 'Pending')], string='Status', readonly=True)
+    delay_close = fields.Float(string='Delay to Close', digits=(16,2), readonly=True, group_operator="avg")
+    partner_id = fields.Many2one('res.partner', string='Partner', readonly=True)
+    company_id = fields.Many2one('res.company', string='Company', readonly=True)
+    date_deadline = fields.Date(string='Deadline', index=True)
+    priority = fields.Selection([('5', 'Lowest'),
+                                ('4', 'Low'),
+                                ('3', 'Normal'),
+                                ('2', 'High'),
+                                ('1', 'Highest')], string='Priority')
+    channel_id = fields.Many2one('utm.medium', string='Channel')
+    categ_id = fields.Many2one('crm.helpdesk.category', string='Category')
+    planned_cost = fields.Float(string='Planned Costs')
+    create_date = fields.Datetime(string='Creation Date', readonly=True, index=True)
+    date_closed = fields.Datetime(string='Close Date', readonly=True, index=True)
+    delay_expected = fields.Float(string='Overpassed Deadline', digits=(16,2), readonly=True, group_operator="avg")
+    email = fields.Integer(string='# Emails', size=128, readonly=True)
 
     def init(self, cr):
 
@@ -84,7 +62,7 @@ class crm_helpdesk_report(osv.osv):
                     c.categ_id,
                     c.channel_id,
                     c.planned_cost,
-                    count(*) as nbr,
+                    count(*) as nbr_requests,
                     extract('epoch' from (c.date_closed-c.create_date))/(3600*24) as  delay_close,
                     (SELECT count(id) FROM mail_message WHERE model='crm.helpdesk' AND res_id=c.id AND type = 'email') AS email,
                     abs(avg(extract('epoch' from (c.date_deadline - c.date_closed)))/(3600*24)) as delay_expected