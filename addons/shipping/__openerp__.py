933188a02ccfc39125e4fdca451ebd3463c9ac3d
jom-odoo <jom@odoo.com>
2015-04-08 10:27:44 +0200

    [IMP] Shipping Provider Fedex #2

--- addons/shipping/__openerp__.py
+++ addons/shipping/__openerp__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/shipping_provider.xml',
         'templates.xml',
     ],
--- addons/shipping_fedex/models/fedex.py
+++ addons/shipping_fedex/models/fedex.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-'8' "-*-"
 
-from openerp import models, fields 
+from openerp import models, fields, api, exceptions
 
 
 class ProviderFedex(models.Model):
@@ -13,7 +13,7 @@ class ProviderFedex(models.Model):
     account_number = fields.Char(string='Account Number')
     meter_number = fields.Char(string='Meter Number')
 
-    default_droppof_type = fields.Selection(
+    default_droppoff_type = fields.Selection(
         [('BUSINESS_SERVICE_CENTER','BUSINESS_SERVICE_CENTER'),
         ('DROP_BOX','DROP_BOX'),
         ('REGULAR_PICKUP','REGULAR_PICKUP'),
@@ -41,24 +41,64 @@ class ProviderFedex(models.Model):
         ('DOCUMENTS_ONLY','DOCUMENTS_ONLY'),
         ('NON_DOCUMENTS','NON_DOCUMENTS')],default='NON_DOCUMENTS'
         )
+
     default_image_type=fields.Selection(
         [('PDF','PDF'),
         ('PNG','PNG')],default='PDF'
         )
- #   default_label_stock_type=fields.Selection(
-  #      [('')
-   #     ])
+
+    default_label_format_type=fields.Selection(
+        [('COMMON2D','COMMON2D'),
+        ('FEDEX_FREIGHT_STRAIGHT_BILL_OF_LADING','FEDEX_FREIGHT_STRAIGHT_BILL_OF_LADING'),
+        ('LABEL_DATA_ONLY','LABEL_DATA_ONLY'),
+        ('VICS_BILL_OF_LADING','VICS_BILL_OF_LADING')], default='COMMON2D'
+        )
+    
+    default_label_stock_type=fields.Selection(
+        [('PAPER_4X6','PAPER_4X6'),
+        ('PAPER_4X8','PAPER_4X8'),
+        ('PAPER_4X9','PAPER_4X9'),
+        ('PAPER_7X4.75','PAPER_7X4.75'),
+        ('PAPER_8.5X11_BOTTOM_HALF_LABEL','PAPER_8.5X11_BOTTOM_HALF_LABEL'),
+        ('PAPER_8.5X11_TOP_HALF_LABEL','PAPER_8.5X11_TOP_HALF_LABEL'),
+        ('PAPER_LETTER','PAPER_LETTER')], default='PAPER_LETTER'
+        )
 
     default_label_order=fields.Selection(
         [('SHIPPING_LABEL_FIRST','SHIPPING_LABEL_FIRST'),
-        ('SHIPPING_LABEL_LAST','SHIPPING_LABEL_LAST')], default='SHIPPING_LABEL_FIRST')
+        ('SHIPPING_LABEL_LAST','SHIPPING_LABEL_LAST')], default='SHIPPING_LABEL_FIRST' 
+        )
+
     default_label_printing_orientation=fields.Selection(
         [('BOTTOM_EDGE_OF_TEXT_FIRST','BOTTOM_EDGE_OF_TEXT_FIRST'),
         ('TOP_EDGE_OF_TEXT_FIRST','TOP_EDGE_OF_TEXT_FIRST')], default='TOP_EDGE_OF_TEXT_FIRST'
         )
 
     default_weight_unit=fields.Selection(
-        [('LB','LB'),('KG','KG')],default='KG')
+        [('LB','LB'),('KG','KG')],default='KG'
+        )
+
+    default_edt_request_type = fields.Selection(
+        [('ALL','ALL'),('NONE','NONE')],default='ALL'
+        )
+
+    @api.one
+    @api.constrains('default_edt_request_type','default_service_type')
+    def _check_international_shipping(self):
+        if "INTERNATIONAL" in self.default_service_type:
+            if self.default_edt_request_type =="NONE" :
+                raise exceptions.ValidationError("The EDT must be ALL when INTERNATIONAL")
+        else :
+            if self.default_edt_request_type =="ALL":
+                raise exceptions.ValidationError("The EDT must be NONE when NOT INTERNATIONAL ")
+
+"""
+    @api.one
+    @api.constrains('company_id')
+    def _check_company_address(self):
+        if not self.company_id.city :
+            raise exceptions.ValidationError("The City must be completed")"""
+
 
 
 class TxFedex(models.Model):
@@ -91,17 +131,65 @@ class TxFedex(models.Model):
         ('NON_DOCUMENTS','NON_DOCUMENTS')]
         )
 
+    image_type=fields.Selection(
+        [('PDF','PDF'),
+        ('PNG','PNG')]
+        )
+
+    label_format_type=fields.Selection(
+        [('COMMON2D','COMMON2D'),
+        ('FEDEX_FREIGHT_STRAIGHT_BILL_OF_LADING','FEDEX_FREIGHT_STRAIGHT_BILL_OF_LADING'),
+        ('LABEL_DATA_ONLY','LABEL_DATA_ONLY'),
+        ('VICS_BILL_OF_LADING','VICS_BILL_OF_LADING')]
+        )
+    
+    label_stock_type=fields.Selection(
+        [('PAPER_4X6','PAPER_4X6'),
+        ('PAPER_4X8','PAPER_4X8'),
+        ('PAPER_4X9','PAPER_4X9'),
+        ('PAPER_7X4.75','PAPER_7X4.75'),
+        ('PAPER_8.5X11_BOTTOM_HALF_LABEL','PAPER_8.5X11_BOTTOM_HALF_LABEL'),
+        ('PAPER_8.5X11_TOP_HALF_LABEL','PAPER_8.5X11_TOP_HALF_LABEL'),
+        ('PAPER_LETTER','PAPER_LETTER')]
+        )
+
+    label_order=fields.Selection(
+        [('SHIPPING_LABEL_FIRST','SHIPPING_LABEL_FIRST'),
+        ('SHIPPING_LABEL_LAST','SHIPPING_LABEL_LAST')]
+        )
+
+    label_printing_orientation=fields.Selection(
+        [('BOTTOM_EDGE_OF_TEXT_FIRST','BOTTOM_EDGE_OF_TEXT_FIRST'),
+        ('TOP_EDGE_OF_TEXT_FIRST','TOP_EDGE_OF_TEXT_FIRST')]
+        )
+
+    weight_unit=fields.Selection(
+        [('LB','LB'),('KG','KG')]
+        )
+
+    default_edt_request_type = fields.Selection(
+        [('ALL','ALL'),('NONE','NONE')]
+        )
+
+    @api.one
+    @api.constrains('edt_request_type','service_type')
+    def _check_international_shipping(self):
+        if "INTERNATIONAL" in self.service_type:
+            if self.edt_request_type =="NONE" :
+                raise exceptions.ValidationError("The EDT must be ALL when INTERNATIONAL")
+        else :
+            if self.edt_request_type =="ALL":
+                raise exceptions.ValidationError("The EDT must be NONE when NOT INTERNATIONAL ")
+
     """
     
 
-    label_format_type = fields.Selection()
-    image_type = fields.Selection
-    label_stock_type= fields.Selection
-    edt_request_type = fields.Selection
-    label_printing_orientation=fields.Selection
-    label_order = fields.Selection
 
-    overall_weight_unit=fields.Selection
+    edt_request_type = fields.Selection
+
+
+
+
 
     customs_value_currency = fields.Selection
     customs_value_amount=fields.Float()
