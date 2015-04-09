Odew/odoo-1

[IMP]website_twitter: Migrate to newAPI

addons/website_twitter/models/twitter_config.py
@@ -3,35 +3,31 @@
 import logging
 from urllib2 import URLError, HTTPError
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
+from openerp import models, fields, api, _
 from openerp.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
 
-class twitter_config_settings(osv.TransientModel):
+class twitter_config_settings(models.TransientModel):
     _inherit = 'website.config.settings'
 
-    _columns = {
-        'twitter_api_key': fields.related(
-            'website_id', 'twitter_api_key', type="char",
-            string='Twitter API Key',
-            help="Twitter API key you can get it from https://apps.twitter.com/app/new"),
-        'twitter_api_secret': fields.related(
-            'website_id', 'twitter_api_secret', type="char",
-            string='Twitter API secret',
-            help="Twitter API secret you can get it from https://apps.twitter.com/app/new"),
-        'twitter_tutorial': fields.dummy(
-            type="boolean", string="Show me how to obtain the Twitter API Key and Secret"),
-        'twitter_screen_name': fields.related(
-            'website_id', 'twitter_screen_name',
-            type="char", string='Get favorites from this screen name',
-            help="Screen Name of the Twitter Account from which you want to load favorites."
-                 "It does not have to match the API Key/Secret."),
-    }
+    twitter_api_key = fields.Char(
+        related='website_id.twitter_api_key',
+        string='Twitter API Key',
+        help="Twitter API key you can get it from https://apps.twitter.com/app/new")
+    twitter_api_secret = fields.Char(
+        related='website_id.twitter_api_secret',
+        string='Twitter API secret',
+        help="Twitter API secret you can get it from https://apps.twitter.com/app/new")
+    twitter_tutorial = fields.Boolean(string="Show me how to obtain the Twitter API Key and Secret")
+    twitter_screen_name = fields.Char(
+        related='website_id.twitter_screen_name',
+        string='Get favorites from this screen name',
+        help="Screen Name of the Twitter Account from which you want to load favorites."
+             "It does not have to match the API Key/Secret.")
 
-    def _get_twitter_exception_message(self, error_code, context=None):
+    def _get_twitter_exception_message(self, error_code):
         TWITTER_EXCEPTION = {
             304: _('There was no new data to return.'),
             400: _('The request was invalid or cannot be otherwise served. Requests without authentication are considered invalid and will yield this response.'),
@@ -48,15 +44,13 @@ class twitter_config_settings(osv.TransientModel):
         else:
             return _('HTTP Error: Something is misconfigured')
 
-    def _check_twitter_authorization(self, cr, uid, config_id, context=None):
-        website_obj = self.pool['website']
-        website_config = self.browse(cr, uid, config_id, context=context)
+    def _check_twitter_authorization(self):
         try:
-            website_obj.fetch_favorite_tweets(cr, uid, [website_config.website_id.id], context=context)
+            self.website_id.fetch_favorite_tweets()
 
         except HTTPError, e:
             _logger.info("%s - %s" % (e.code, e.reason), exc_info=True)
-            raise UserError("%s - %s" % (e.code, e.reason) + ':' + self._get_twitter_exception_message(e.code, context))
+            raise UserError("%s - %s" % (e.code, e.reason) + ':' + self._get_twitter_exception_message(e.code))
         except URLError, e:
             _logger.info(_('We failed to reach a twitter server.'), exc_info=True)
             raise UserError(_('Internet connection refused') + ' ' + _('We failed to reach a twitter server.'))
@@ -64,14 +58,16 @@ class twitter_config_settings(osv.TransientModel):
             _logger.info(_('Please double-check your Twitter API Key and Secret!'), exc_info=True)
             raise UserError(_('Twitter authorization error!') + ' ' + _('Please double-check your Twitter API Key and Secret!'))
 
-    def create(self, cr, uid, vals, context=None):
-        res_id = super(twitter_config_settings, self).create(cr, uid, vals, context=context)
+    @api.model
+    def create(self, vals):
+        TwitterConfig = super(twitter_config_settings, self).create(vals)
         if vals.get('twitter_api_key') or vals.get('twitter_api_secret') or vals.get('twitter_screen_name'):
-            self._check_twitter_authorization(cr, uid, res_id, context=context)
-        return res_id
+            TwitterConfig._check_twitter_authorization()
+        return TwitterConfig
 
-    def write(self, cr, uid, ids, vals, context=None):
-        res_id = super(twitter_config_settings, self).write(cr, uid, ids, vals, context=context)
+    @api.multi
+    def write(self, vals):
+        TwitterConfig = super(twitter_config_settings, self).write(vals)
         if vals.get('twitter_api_key') or vals.get('twitter_api_secret') or vals.get('twitter_screen_name'):
-            self._check_twitter_authorization(cr, uid, ids, context=context)
-        return res_id
+            self._check_twitter_authorization()
+        return TwitterConfig

odoo/odoo

addons/website_twitter/models/twitter_config.py
@@ -3,35 +3,31 @@
 import logging
 from urllib2 import URLError, HTTPError
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
+from openerp import models, fields, api, _
 from openerp.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
 
-class twitter_config_settings(osv.TransientModel):
+class TwitterConfigSettings(models.TransientModel):
     _inherit = 'website.config.settings'
 
-    _columns = {
-        'twitter_api_key': fields.related(
-            'website_id', 'twitter_api_key', type="char",
-            string='Twitter API Key',
-            help="Twitter API key you can get it from https://apps.twitter.com/app/new"),
-        'twitter_api_secret': fields.related(
-            'website_id', 'twitter_api_secret', type="char",
-            string='Twitter API secret',
-            help="Twitter API secret you can get it from https://apps.twitter.com/app/new"),
-        'twitter_tutorial': fields.dummy(
-            type="boolean", string="Show me how to obtain the Twitter API Key and Secret"),
-        'twitter_screen_name': fields.related(
-            'website_id', 'twitter_screen_name',
-            type="char", string='Get favorites from this screen name',
-            help="Screen Name of the Twitter Account from which you want to load favorites."
-                 "It does not have to match the API Key/Secret."),
-    }
+    twitter_api_key = fields.Char(
+        related='website_id.twitter_api_key',
+        string='Twitter API Key',
+        help="Twitter API key you can get it from https://apps.twitter.com/app/new")
+    twitter_api_secret = fields.Char(
+        related='website_id.twitter_api_secret',
+        string='Twitter API secret',
+        help="Twitter API secret you can get it from https://apps.twitter.com/app/new")
+    twitter_tutorial = fields.Boolean(string="Show me how to obtain the Twitter API Key and Secret")
+    twitter_screen_name = fields.Char(
+        related='website_id.twitter_screen_name',
+        string='Get favorites from this screen name',
+        help="Screen Name of the Twitter Account from which you want to load favorites."
+             "It does not have to match the API Key/Secret.")
 
-    def _get_twitter_exception_message(self, error_code, context=None):
+    def _get_twitter_exception_message(self, error_code):
         TWITTER_EXCEPTION = {
             304: _('There was no new data to return.'),
             400: _('The request was invalid or cannot be otherwise served. Requests without authentication are considered invalid and will yield this response.'),
@@ -48,15 +44,13 @@ class twitter_config_settings(osv.TransientModel):
         else:
             return _('HTTP Error: Something is misconfigured')
 
-    def _check_twitter_authorization(self, cr, uid, config_id, context=None):
-        website_obj = self.pool['website']
-        website_config = self.browse(cr, uid, config_id, context=context)
+    def _check_twitter_authorization(self):
         try:
-            website_obj.fetch_favorite_tweets(cr, uid, [website_config.website_id.id], context=context)
+            self.website_id.fetch_favorite_tweets()
 
         except HTTPError, e:
             _logger.info("%s - %s" % (e.code, e.reason), exc_info=True)
-            raise UserError("%s - %s" % (e.code, e.reason) + ':' + self._get_twitter_exception_message(e.code, context))
+            raise UserError("%s - %s" % (e.code, e.reason) + ':' + self._get_twitter_exception_message(e.code))
         except URLError, e:
             _logger.info(_('We failed to reach a twitter server.'), exc_info=True)
             raise UserError(_('Internet connection refused') + ' ' + _('We failed to reach a twitter server.'))
@@ -64,14 +58,16 @@ class twitter_config_settings(osv.TransientModel):
             _logger.info(_('Please double-check your Twitter API Key and Secret!'), exc_info=True)
             raise UserError(_('Twitter authorization error!') + ' ' + _('Please double-check your Twitter API Key and Secret!'))
 
-    def create(self, cr, uid, vals, context=None):
-        res_id = super(twitter_config_settings, self).create(cr, uid, vals, context=context)
+    @api.model
+    def create(self, vals):
+        TwitterConfig = super(TwitterConfigSettings, self).create(vals)
         if vals.get('twitter_api_key') or vals.get('twitter_api_secret') or vals.get('twitter_screen_name'):
-            self._check_twitter_authorization(cr, uid, res_id, context=context)
-        return res_id
+            TwitterConfig._check_twitter_authorization()
+        return TwitterConfig
 
-    def write(self, cr, uid, ids, vals, context=None):
-        res_id = super(twitter_config_settings, self).write(cr, uid, ids, vals, context=context)
+    @api.multi
+    def write(self, vals):
+        TwitterConfig = super(TwitterConfigSettings, self).write(vals)
         if vals.get('twitter_api_key') or vals.get('twitter_api_secret') or vals.get('twitter_screen_name'):
-            self._check_twitter_authorization(cr, uid, ids, context=context)
-        return res_id
+            self._check_twitter_authorization()
+        return TwitterConfig