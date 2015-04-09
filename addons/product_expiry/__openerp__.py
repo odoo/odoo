64c39a7ff4444b53fc7392ffa6091376103f2ce9
Jignesh Rathod <jir@openerp.com>
2015-04-09 16:40:13 +0530

    [product_expiry] Migrated into new api.

--- addons/product_expiry/__openerp__.py
+++ addons/product_expiry/__openerp__.py
@@ -1,26 +1,9 @@
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+
 {
     'name' : 'Products Expiry Date',
     'version' : '1.0',
-    'author' : 'OpenERP SA',
+    'author' : 'Odoo S.A.',
     'category' : 'Specific Industry Applications',
     'website': 'https://www.odoo.com',
     'depends' : ['stock'],
@@ -41,6 +24,5 @@ Also implements the removal strategy First Expiry First Out (FEFO) widely used, 
     'data' : ['views/product_template_views.xml',
               'views/stock_production_lot_views.xml',
               'data/product_expiry_data.xml'],
-    'auto_install': False,
     'installable': True,
 }
