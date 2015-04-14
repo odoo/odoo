Odew/odoo-1

[IMP] Finalize revenue recognition on account_asset with @cod work

addons/account_asset/account_asset_invoice.py
@@ -1,66 +1,97 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
-from openerp.osv import fields, osv
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from openerp import models, fields, api
+import openerp.addons.decimal_precision as dp
 
-class account_invoice(osv.osv):
 
+class account_invoice(models.Model):
     _inherit = 'account.invoice'
-    def action_number(self, cr, uid, ids, *args):
-        result = super(account_invoice, self).action_number(cr, uid, ids, *args)
-        for inv in self.browse(cr, uid, ids):
-            self.pool.get('account.invoice.line').asset_create(cr, uid, inv.invoice_line)
+
+    @api.multi
+    def action_number(self):
+        result = super(account_invoice, self).action_number()
+        for inv in self:
+            inv.invoice_line.asset_create()
         return result
 
-    def line_get_convert(self, cr, uid, x, part, date, context=None):
-        res = super(account_invoice, self).line_get_convert(cr, uid, x, part, date, context=context)
-        res['asset_id'] = x.get('asset_id', False)
+    @api.model
+    def line_get_convert(self, line, part, date):
+        res = super(account_invoice, self).line_get_convert(line, part, date)
+        res['asset_id'] = line.get('asset_id', False)
         return res
 
 
-class account_invoice_line(osv.osv):
-
+class account_invoice_line(models.Model):
     _inherit = 'account.invoice.line'
-    _columns = {
-        'asset_category_id': fields.many2one('account.asset.category', 'Asset Category'),
-    }
-    def asset_create(self, cr, uid, lines, context=None):
-        context = context or {}
-        asset_obj = self.pool.get('account.asset.asset')
-        for line in lines:
-            if line.asset_category_id:
-                vals = {
-                    'name': line.name,
-                    'code': line.invoice_id.number or False,
-                    'category_id': line.asset_category_id.id,
-                    'purchase_value': line.price_subtotal,
-                    'period_id': line.invoice_id.period_id.id,
-                    'partner_id': line.invoice_id.partner_id.id,
-                    'company_id': line.invoice_id.company_id.id,
-                    'currency_id': line.invoice_id.currency_id.id,
-                    'purchase_date' : line.invoice_id.date_invoice,
-                }
-                changed_vals = asset_obj.onchange_category_id(cr, uid, [], vals['category_id'], context=context)
-                vals.update(changed_vals['value'])
-                asset_id = asset_obj.create(cr, uid, vals, context=context)
-                if line.asset_category_id.open_asset:
-                    asset_obj.validate(cr, uid, [asset_id], context=context)
+
+    asset_category_id = fields.Many2one('account.asset.category', string='Asset Category')
+    asset_start_date = fields.Date(string='Asset End Date', compute='_get_asset_date', readonly=True, store=True)
+    asset_end_date = fields.Date(string='Asset Start Date', compute='_get_asset_date', readonly=True, store=True)
+    mrr = fields.Float(string='Monthly Recurring Revenue', compute='_get_mrr', store=True, readonly=True, digits=dp.get_precision('Account'))
+
+    @api.one
+    @api.depends('asset_category_id')
+    def _get_mrr(self):
+        # TODO: use @MAT field called to caculate MRR in master-account-new-api-mdi
+        if self.asset_category_id and self.invoice_id.type == 'out_invoice':
+            try:
+                self.mrr = self.price_subtotal/(self.asset_category_id.method_period*self.asset_category_id.method_number)
+            except ZeroDivisionError:
+                self.mrr = 0.
+        else:
+            self.mrr = 0.
+
+    @api.multi
+    def product_id_change(self, product, uom_id, qty=0, name='', type='out_invoice',
+                          partner_id=False, fposition_id=False, price_unit=False, currency_id=False,
+                          company_id=None):
+        # TODO: onchange product id in old API in account, overwrite it when it has changed
+        res = super(account_invoice_line, self).product_id_change(
+            product, uom_id, qty=qty, name=name, type=type, partner_id=partner_id,
+            fposition_id=fposition_id, price_unit=price_unit, currency_id=currency_id,
+            company_id=company_id
+        )
+        if product and res['value']:
+            product_obj = self.env['product.product'].browse(product)
+            if type == 'out_invoice':
+                res['value']['asset_category_id'] = product_obj.product_tmpl_id.deferred_revenue_category_id
+            elif type == 'in_invoice':
+                res['value']['asset_category_id'] = product_obj.product_tmpl_id.asset_category_id
+
+        return res
+
+    @api.one
+    @api.depends('asset_category_id', 'invoice_id.date_due')
+    def _get_asset_date(self):
+        if self.asset_category_id and self.invoice_id.date_due:
+            self.asset_start_date = self.invoice_id.date_due
+            months = self.asset_category_id.method_number * self.asset_category_id.method_period
+            start_date = datetime.strptime(self.invoice_id.date_due, '%Y-%m-%d')
+            end_date = start_date + relativedelta(months=months)
+            self.asset_end_date = end_date.strftime('%Y-%m-%d')
+        else:
+            self.asset_start_date = False
+            self.asset_end_date = False
+
+    @api.one
+    def asset_create(self):
+        if self.asset_category_id and self.asset_category_id.method_number > 1:
+            vals = {
+                'name': self.name,
+                'code': self.invoice_id.number or False,
+                'category_id': self.asset_category_id.id,
+                'value': self.price_subtotal,
+                'partner_id': self.invoice_id.partner_id.id,
+                'company_id': self.invoice_id.company_id.id,
+                'currency_id': self.invoice_id.currency_id.id,
+                'date': self.asset_start_date or self.invoice_id.date_invoice,
+                'invoice_id': self.invoice_id.id,
+            }
+            changed_vals = self.env['account.asset.asset'].onchange_category_id_values(vals['category_id'])
+            vals.update(changed_vals['value'])
+            asset = self.env['account.asset.asset'].create(vals)
+            if self.asset_category_id.open_asset:
+                asset.validate()
         return True

addons/account_asset/report/account_asset_report.py
@@ -1,64 +1,46 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
-#
-##############################################################################
+from openerp import tools, models, fields
 
-from openerp import tools
-from openerp.osv import fields, osv
 
-class asset_asset_report(osv.osv):
+class asset_asset_report(models.Model):
     _name = "asset.asset.report"
     _description = "Assets Analysis"
     _auto = False
-    _columns = {
-        'name': fields.char('Year', required=False, readonly=True),
-        'purchase_date': fields.date('Purchase Date', readonly=True),
-        'depreciation_date': fields.date('Depreciation Date', readonly=True),
-        'asset_id': fields.many2one('account.asset.asset', string='Asset', readonly=True),
-        'asset_category_id': fields.many2one('account.asset.category',string='Asset category'),
-        'partner_id': fields.many2one('res.partner', 'Partner', readonly=True),
-        'state': fields.selection([('draft','Draft'),('open','Running'),('close','Close')], 'Status', readonly=True),
-        'depreciation_value': fields.float('Amount of Depreciation Lines', readonly=True),
-        'move_check': fields.boolean('Posted', readonly=True),
-        'nbr': fields.integer('# of Depreciation Lines', readonly=True),
-        'gross_value': fields.float('Gross Amount', readonly=True),
-        'posted_value': fields.float('Posted Amount', readonly=True),
-        'unposted_value': fields.float('Unposted Amount', readonly=True),
-        'company_id': fields.many2one('res.company', 'Company', readonly=True),
-    }
-    
+
+    name = fields.Char(string='Year', required=False, readonly=True)
+    date = fields.Date(readonly=True)
+    depreciation_date = fields.Date(string='Depreciation Date', readonly=True)
+    asset_id = fields.Many2one('account.asset.asset', string='Asset', readonly=True)
+    asset_category_id = fields.Many2one('account.asset.category', string='Asset category')
+    partner_id = fields.Many2one('res.partner', string='Partner', readonly=True)
+    state = fields.Selection([('draft', 'Draft'), ('open', 'Running'), ('close', 'Close')], string='Status', readonly=True)
+    depreciation_value = fields.Float(string='Amount of Depreciation Lines', readonly=True)
+    installment_value = fields.Float(string='Amount of Installment Lines', readonly=True)
+    move_check = fields.Boolean(string='Posted', readonly=True)
+    installment_nbr = fields.Integer(string='# of Installment Lines', readonly=True)
+    depreciation_nbr = fields.Integer(string='# of Depreciation Lines', readonly=True)
+    gross_value = fields.Float(string='Gross Amount', readonly=True)
+    posted_value = fields.Float(string='Posted Amount', readonly=True)
+    unposted_value = fields.Float(string='Unposted Amount', readonly=True)
+    company_id = fields.Many2one('res.company', string='Company', readonly=True)
+
     def init(self, cr):
-    	tools.drop_view_if_exists(cr, 'asset_asset_report')
-     	cr.execute("""
-    	    create or replace view asset_asset_report as (
-                select 
+        tools.drop_view_if_exists(cr, 'asset_asset_report')
+        cr.execute("""
+            create or replace view asset_asset_report as (
+                select
                     min(dl.id) as id,
                     dl.name as name,
                     dl.depreciation_date as depreciation_date,
-                    a.purchase_date as purchase_date,
+                    a.date as date,
                     (CASE WHEN (select min(d.id) from account_asset_depreciation_line as d
                                 left join account_asset_asset as ac ON (ac.id=d.asset_id)
                                 where a.id=ac.id) = min(dl.id)
-                      THEN a.purchase_value
+                      THEN a.value
                       ELSE 0
                       END) as gross_value,
-                    dl.amount as depreciation_value, 
+                    dl.amount as depreciation_value,
+                    dl.amount as installment_value,
                     (CASE WHEN dl.move_check
                       THEN dl.amount
                       ELSE 0
@@ -72,13 +54,13 @@ class asset_asset_report(osv.osv):
                     a.category_id as asset_category_id,
                     a.partner_id as partner_id,
                     a.state as state,
-                    count(dl.*) as nbr,
+                    count(dl.*) as installment_nbr,
+                    count(dl.*) as depreciation_nbr,
                     a.company_id as company_id
                 from account_asset_depreciation_line dl
                     left join account_asset_asset a on (dl.asset_id=a.id)
-                group by 
+                group by
                     dl.amount,dl.asset_id,dl.depreciation_date,dl.name,
-                    a.purchase_date, dl.move_check, a.state, a.category_id, a.partner_id, a.company_id,
-                    a.purchase_value, a.id, a.salvage_value
+                    a.date, dl.move_check, a.state, a.category_id, a.partner_id, a.company_id,
+                    a.value, a.id, a.salvage_value
         )""")
-	

odoo/odoo

addons/account_asset/account_asset_invoice.py
@@ -1,66 +1,99 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
 
-from openerp.osv import fields, osv
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from openerp import api, fields, models
+import openerp.addons.decimal_precision as dp
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT as DF
 
-class account_invoice(osv.osv):
 
+class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
-    def action_number(self, cr, uid, ids, *args):
-        result = super(account_invoice, self).action_number(cr, uid, ids, *args)
-        for inv in self.browse(cr, uid, ids):
-            self.pool.get('account.invoice.line').asset_create(cr, uid, inv.invoice_line)
+
+    @api.multi
+    def action_number(self):
+        result = super(AccountInvoice, self).action_number()
+        for inv in self:
+            inv.invoice_line.asset_create()
         return result
 
-    def line_get_convert(self, cr, uid, x, part, date, context=None):
-        res = super(account_invoice, self).line_get_convert(cr, uid, x, part, date, context=context)
-        res['asset_id'] = x.get('asset_id', False)
+    @api.model
+    def line_get_convert(self, line, part, date):
+        res = super(AccountInvoice, self).line_get_convert(line, part, date)
+        res['asset_id'] = line.get('asset_id', False)
         return res
 
 
-class account_invoice_line(osv.osv):
-
+class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
-    _columns = {
-        'asset_category_id': fields.many2one('account.asset.category', 'Asset Category'),
-    }
-    def asset_create(self, cr, uid, lines, context=None):
-        context = context or {}
-        asset_obj = self.pool.get('account.asset.asset')
-        for line in lines:
-            if line.asset_category_id:
-                vals = {
-                    'name': line.name,
-                    'code': line.invoice_id.number or False,
-                    'category_id': line.asset_category_id.id,
-                    'purchase_value': line.price_subtotal,
-                    'period_id': line.invoice_id.period_id.id,
-                    'partner_id': line.invoice_id.partner_id.id,
-                    'company_id': line.invoice_id.company_id.id,
-                    'currency_id': line.invoice_id.currency_id.id,
-                    'purchase_date' : line.invoice_id.date_invoice,
-                }
-                changed_vals = asset_obj.onchange_category_id(cr, uid, [], vals['category_id'], context=context)
-                vals.update(changed_vals['value'])
-                asset_id = asset_obj.create(cr, uid, vals, context=context)
-                if line.asset_category_id.open_asset:
-                    asset_obj.validate(cr, uid, [asset_id], context=context)
+
+    asset_category_id = fields.Many2one('account.asset.category', string='Asset Category')
+    asset_start_date = fields.Date(string='Asset End Date', compute='_get_asset_date', readonly=True, store=True)
+    asset_end_date = fields.Date(string='Asset Start Date', compute='_get_asset_date', readonly=True, store=True)
+    mrr = fields.Float(string='Monthly Recurring Revenue', compute='_get_mrr', store=True, readonly=True, digits=dp.get_precision('Account'))
+
+    @api.one
+    @api.depends('asset_category_id')
+    def _get_mrr(self):
+        # TODO: use @MAT field called to caculate MRR in master-account-new-api-mdi
+        if self.asset_category_id and self.invoice_id.type in ['out_invoice', 'out_refund']:
+            sign = -1 if self.invoice_id.type == 'out_refund' else 1
+            try:
+                self.mrr = sign*self.price_subtotal/(self.asset_category_id.method_period*self.asset_category_id.method_number)
+            except ZeroDivisionError:
+                self.mrr = 0.
+        else:
+            self.mrr = 0.
+
+    @api.multi
+    def product_id_change(self, product, uom_id, qty=0, name='', type='out_invoice',
+                          partner_id=False, fposition_id=False, price_unit=False, currency_id=False,
+                          company_id=None):
+        # TODO: onchange product id in old API in account, overwrite it when it has changed
+        res = super(AccountInvoiceLine, self).product_id_change(
+            product, uom_id, qty=qty, name=name, type=type, partner_id=partner_id,
+            fposition_id=fposition_id, price_unit=price_unit, currency_id=currency_id,
+            company_id=company_id
+        )
+        if product and res['value']:
+            product_obj = self.env['product.product'].browse(product)
+            if type == 'out_invoice':
+                res['value']['asset_category_id'] = product_obj.product_tmpl_id.deferred_revenue_category_id
+            elif type == 'in_invoice':
+                res['value']['asset_category_id'] = product_obj.product_tmpl_id.asset_category_id
+
+        return res
+
+    @api.one
+    @api.depends('asset_category_id', 'invoice_id.date_invoice')
+    def _get_asset_date(self):
+        if self.asset_category_id and self.invoice_id.date_invoice:
+            start_date = datetime.strptime(self.invoice_id.date_invoice, DF).replace(day=1)
+            self.asset_start_date = start_date.strftime(DF)
+            months = self.asset_category_id.method_number * self.asset_category_id.method_period
+            end_date = start_date + relativedelta(months=months, days=-1)
+            self.asset_end_date = end_date.strftime(DF)
+        else:
+            self.asset_start_date = False
+            self.asset_end_date = False
+
+    @api.one
+    def asset_create(self):
+        if self.asset_category_id and self.asset_category_id.method_number > 1:
+            vals = {
+                'name': self.name,
+                'code': self.invoice_id.number or False,
+                'category_id': self.asset_category_id.id,
+                'value': self.price_subtotal,
+                'partner_id': self.invoice_id.partner_id.id,
+                'company_id': self.invoice_id.company_id.id,
+                'currency_id': self.invoice_id.currency_id.id,
+                'date': self.asset_start_date or self.invoice_id.date_invoice,
+                'invoice_id': self.invoice_id.id,
+            }
+            changed_vals = self.env['account.asset.asset'].onchange_category_id_values(vals['category_id'])
+            vals.update(changed_vals['value'])
+            asset = self.env['account.asset.asset'].create(vals)
+            if self.asset_category_id.open_asset:
+                asset.validate()
         return True

addons/account_asset/report/account_asset_report.py
@@ -1,64 +1,46 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+from openerp import fields, models, tools
 
-from openerp import tools
-from openerp.osv import fields, osv
 
-class asset_asset_report(osv.osv):
+class AssetAssetReport(models.Model):
     _name = "asset.asset.report"
     _description = "Assets Analysis"
     _auto = False
-    _columns = {
-        'name': fields.char('Year', required=False, readonly=True),
-        'purchase_date': fields.date('Purchase Date', readonly=True),
-        'depreciation_date': fields.date('Depreciation Date', readonly=True),
-        'asset_id': fields.many2one('account.asset.asset', string='Asset', readonly=True),
-        'asset_category_id': fields.many2one('account.asset.category',string='Asset category'),
-        'partner_id': fields.many2one('res.partner', 'Partner', readonly=True),
-        'state': fields.selection([('draft','Draft'),('open','Running'),('close','Close')], 'Status', readonly=True),
-        'depreciation_value': fields.float('Amount of Depreciation Lines', readonly=True),
-        'move_check': fields.boolean('Posted', readonly=True),
-        'nbr': fields.integer('# of Depreciation Lines', readonly=True),
-        'gross_value': fields.float('Gross Amount', readonly=True),
-        'posted_value': fields.float('Posted Amount', readonly=True),
-        'unposted_value': fields.float('Unposted Amount', readonly=True),
-        'company_id': fields.many2one('res.company', 'Company', readonly=True),
-    }
-    
+
+    name = fields.Char(string='Year', required=False, readonly=True)
+    date = fields.Date(readonly=True)
+    depreciation_date = fields.Date(string='Depreciation Date', readonly=True)
+    asset_id = fields.Many2one('account.asset.asset', string='Asset', readonly=True)
+    asset_category_id = fields.Many2one('account.asset.category', string='Asset category', readonly=True)
+    partner_id = fields.Many2one('res.partner', string='Partner', readonly=True)
+    state = fields.Selection([('draft', 'Draft'), ('open', 'Running'), ('close', 'Close')], string='Status', readonly=True)
+    depreciation_value = fields.Float(string='Amount of Depreciation Lines', readonly=True)
+    installment_value = fields.Float(string='Amount of Installment Lines', readonly=True)
+    move_check = fields.Boolean(string='Posted', readonly=True)
+    installment_nbr = fields.Integer(string='# of Installment Lines', readonly=True)
+    depreciation_nbr = fields.Integer(string='# of Depreciation Lines', readonly=True)
+    gross_value = fields.Float(string='Gross Amount', readonly=True)
+    posted_value = fields.Float(string='Posted Amount', readonly=True)
+    unposted_value = fields.Float(string='Unposted Amount', readonly=True)
+    company_id = fields.Many2one('res.company', string='Company', readonly=True)
+
     def init(self, cr):
-    	tools.drop_view_if_exists(cr, 'asset_asset_report')
-     	cr.execute("""
-    	    create or replace view asset_asset_report as (
-                select 
+        tools.drop_view_if_exists(cr, 'asset_asset_report')
+        cr.execute("""
+            create or replace view asset_asset_report as (
+                select
                     min(dl.id) as id,
                     dl.name as name,
                     dl.depreciation_date as depreciation_date,
-                    a.purchase_date as purchase_date,
+                    a.date as date,
                     (CASE WHEN (select min(d.id) from account_asset_depreciation_line as d
                                 left join account_asset_asset as ac ON (ac.id=d.asset_id)
                                 where a.id=ac.id) = min(dl.id)
-                      THEN a.purchase_value
+                      THEN a.value
                       ELSE 0
                       END) as gross_value,
-                    dl.amount as depreciation_value, 
+                    dl.amount as depreciation_value,
+                    dl.amount as installment_value,
                     (CASE WHEN dl.move_check
                       THEN dl.amount
                       ELSE 0
@@ -72,13 +54,13 @@ class asset_asset_report(osv.osv):
                     a.category_id as asset_category_id,
                     a.partner_id as partner_id,
                     a.state as state,
-                    count(dl.*) as nbr,
+                    count(dl.*) as installment_nbr,
+                    count(dl.*) as depreciation_nbr,
                     a.company_id as company_id
                 from account_asset_depreciation_line dl
                     left join account_asset_asset a on (dl.asset_id=a.id)
-                group by 
+                group by
                     dl.amount,dl.asset_id,dl.depreciation_date,dl.name,
-                    a.purchase_date, dl.move_check, a.state, a.category_id, a.partner_id, a.company_id,
-                    a.purchase_value, a.id, a.salvage_value
+                    a.date, dl.move_check, a.state, a.category_id, a.partner_id, a.company_id,
+                    a.value, a.id, a.salvage_value
         )""")
-	