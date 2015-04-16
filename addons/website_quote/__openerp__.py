8d2a2083f1c95bcc3e57153856cc2846b1984cf7
Damien Bouvy <dbo@odoo.com>
2015-04-16 12:11:02 +0200

    [IMP] website_quote: add payment management Until now, you could only sign and confirm a sale order from the frontend.
    From now on, you can also pay sale orders if the 'Immediate Payment'
    option is checked on the sale order.

--- addons/website_quote/__openerp__.py
+++ addons/website_quote/__openerp__.py
@@ -10,7 +10,7 @@ OpenERP Sale Quote Roller
 
         """,
     'author': 'OpenERP SA',
-    'depends': ['website', 'sale', 'mail', 'web_tip'],
+    'depends': ['website', 'sale', 'mail', 'web_tip', 'payment'],
     'data': [
         'quotation_report.xml',
         'views/website_quotation.xml',
