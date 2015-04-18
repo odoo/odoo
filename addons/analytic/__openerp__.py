e5da560fa953abea6731c78eafa82807db57424a
Christophe Combelles <ccomb@free.fr>
2015-04-18 20:38:04 +0300

    moved the test in the account module which depends on analytic

--- addons/analytic/__openerp__.py
+++ addons/analytic/__openerp__.py
@@ -42,7 +42,6 @@ that have no counterpart in the general financial accounts.
         'analytic_data.xml',
     ],
     'demo': [],
-    'test': ['test/analytic_hierarchy.yml'],
     'installable': True,
     'auto_install': False,
 }
