84f3a534e26e97267a32c6010da1ab3df03ad63c
Vincent Vinet <vincent.vinet@savoirfairelinux.com>
2015-04-20 14:25:02 -0400

    do not sort if we return a count

--- addons/document/document.py
+++ addons/document/document.py
@@ -110,9 +110,11 @@ class document_file(osv.osv):
             ids.extend(parents[parent_id])
 
         # sort result according to the original sort ordering
-        set_ids = set(ids)
-        ids = [id for id in orig_ids if id in set_ids]
-        return len(ids) if count else ids
+        if count:
+            return len(ids)
+        else:
+            set_ids = set(ids)
+            return [id for id in orig_ids if id in set_ids]
 
     def copy(self, cr, uid, id, default=None, context=None):
         if not default:
