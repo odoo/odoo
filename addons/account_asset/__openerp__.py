6120babb4569cb846dacaae885cd2ba27f587d06
Chirag Dodiya <cod@openerp.com>
2015-04-21 13:03:09 +0200

    [IMP] account_asset: new api conversion, added asset type

--- addons/account_asset/__openerp__.py
+++ addons/account_asset/__openerp__.py
@@ -1,31 +1,12 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
 
 {
     'name': 'Assets Management',
     'version': '1.0',
-    'depends': ['account'],
-    'author': 'OpenERP S.A.',
+    'depends': ['account_accountant'],
+    'author': 'Odoo S.A.',
     'description': """
-Financial and accounting asset management.
+Assets management.
 ==========================================
 
 This Module manages the assets owned by a company or an individual. It will keep 
@@ -36,22 +17,20 @@ of the depreciation lines.
     'website': 'https://www.odoo.com/page/accounting',
     'category': 'Accounting & Finance',
     'sequence': 32,
-    'demo': [ 'account_asset_demo.xml'],
-    'test': [
-        'test/account_asset_demo.yml',
-        'test/account_asset.yml',
-        'test/account_asset_wizard.yml',
-    ],
+    'demo': ['account_asset_demo.xml'],
     'data': [
         'security/account_asset_security.xml',
         'security/ir.model.access.csv',
         'wizard/account_asset_change_duration_view.xml',
         'wizard/wizard_asset_compute_view.xml',
-        'account_asset_view.xml',
-        'account_asset_invoice_view.xml',
+        'views/account_asset_view.xml',
+        'views/account_asset_invoice_view.xml',
         'report/account_asset_report_view.xml',
+        'views/account_asset.xml',
     ],
-    'auto_install': False,
+    'qweb': [
+        "static/src/xml/account_asset_template.xml",
+    ],
     'installable': True,
     'application': False,
 }
--- addons/account_asset/account_asset_invoice.py
+++ addons/account_asset/account_asset_invoice.py
@@ -1,66 +1,51 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
 
-from openerp.osv import fields, osv
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from openerp import api, fields, models
+import openerp.addons.decimal_precision as dp
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT as DF
 
-class account_invoice(osv.osv):
 
+class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
-    def action_number(self, cr, uid, ids, *args):
-        result = super(account_invoice, self).action_number(cr, uid, ids, *args)
-        for inv in self.browse(cr, uid, ids):
-            self.pool.get('account.invoice.line').asset_create(cr, uid, inv.invoice_line)
+
+    @api.multi
+    def action_number(self):
+        result = super(AccountInvoice, self).action_number()
+        for inv in self:
+            inv.invoice_line.asset_create()
         return result
 
-    def line_get_convert(self, cr, uid, x, part, date, context=None):
-        res = super(account_invoice, self).line_get_convert(cr, uid, x, part, date, context=context)
-        res['asset_id'] = x.get('asset_id', False)
+    @api.model
+    def line_get_convert(self, line, part, date):
+        res = super(AccountInvoice, self).line_get_convert(line, part, date)
+        res['asset_id'] = line.get('asset_id', False)
         return res
 
 
-class account_invoice_line(osv.osv):
-
+class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
-    _columns = {
-        'asset_category_id': fields.many2one('account.asset.category', 'Asset Category'),
-    }
-    def asset_create(self, cr, uid, lines, context=None):
-        context = context or {}
-        asset_obj = self.pool.get('account.asset.asset')
-        for line in lines:
-            if line.asset_category_id:
-                vals = {
-                    'name': line.name,
-                    'code': line.invoice_id.number or False,
-                    'category_id': line.asset_category_id.id,
-                    'purchase_value': line.price_subtotal,
-                    'period_id': line.invoice_id.period_id.id,
-                    'partner_id': line.invoice_id.partner_id.id,
-                    'company_id': line.invoice_id.company_id.id,
-                    'currency_id': line.invoice_id.currency_id.id,
-                    'purchase_date' : line.invoice_id.date_invoice,
-                }
-                changed_vals = asset_obj.onchange_category_id(cr, uid, [], vals['category_id'], context=context)
-                vals.update(changed_vals['value'])
-                asset_id = asset_obj.create(cr, uid, vals, context=context)
-                if line.asset_category_id.open_asset:
-                    asset_obj.validate(cr, uid, [asset_id], context=context)
+
+    asset_category_id = fields.Many2one('account.asset.category', string='Asset Category')
+
+    @api.one
+    def asset_create(self):
+        if self.asset_category_id and self.asset_category_id.method_number > 1:
+            vals = {
+                'name': self.name,
+                'code': self.invoice_id.number or False,
+                'category_id': self.asset_category_id.id,
+                'value': self.price_subtotal,
+                'partner_id': self.invoice_id.partner_id.id,
+                'company_id': self.invoice_id.company_id.id,
+                'currency_id': self.invoice_id.currency_id.id,
+                'date': self.asset_start_date or self.invoice_id.date_invoice,
+                'invoice_id': self.invoice_id.id,
+            }
+            changed_vals = self.env['account.asset.asset'].onchange_category_id_values(vals['category_id'])
+            vals.update(changed_vals['value'])
+            asset = self.env['account.asset.asset'].create(vals)
+            if self.asset_category_id.open_asset:
+                asset.validate()
         return True
