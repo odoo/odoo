5748d98a89501e33815ce79530ebbe3161f7ecea
Yannick Tivisse <yti@odoo.com>
2015-04-27 11:39:04 +0200

    [FIX] web : Trying to reconnect banner was creating a banner each time the rpc_error function was called. There was several call to setInterval method, and several block were created with the same content. Also, the banner was not centered, and a blur effect was created by the fa-spin class

--- addons/web/static/src/js/chrome.js
+++ addons/web/static/src/js/chrome.js
@@ -249,19 +249,24 @@ instance.web.CrashManager = instance.web.Class.extend({
     },
 
     rpc_error: function(error) {
+        var self = this;
         if (!this.active) {
             return;
         }
         if (error.code == -32098) {
+            if (this.$indicator){
+                return;
+            }
             $.blockUI({ message: '' , overlayCSS: {'z-index': 9999, backgroundColor: '#FFFFFF', opacity: 0.0, cursor: 'wait'}});
-            var $indicator = $('<div class="oe_indicator">' + _t("Trying to reconnect... ") + '<i class="fa fa-refresh fa-spin"></i></div>');
-            $indicator.prependTo("body");
+            this.$indicator = $('<div class="oe_indicator">' + _t("Trying to reconnect... ") + '<i class="fa fa-refresh fa-spin"></i></div>');
+            this.$indicator.prependTo("body");
             var timeinterval = setInterval(function(){
                 openerp.jsonRpc('/web/webclient/version_info').then(function() {
                     clearInterval(timeinterval);
-                    $indicator.html(_t("You are back online"));
-                    $indicator.delay(2000).fadeOut('slow',function(){
-                        $indicator.remove();
+                    self.$indicator.html(_t("You are back online"));
+                    self.$indicator.delay(2000).fadeOut('slow', function() {
+                        $(this).remove();
+                        self.$indicator = null;
                     });
                     $.unblockUI();
                 });
