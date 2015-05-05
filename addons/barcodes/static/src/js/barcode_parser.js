00236aafcc2a18f550d9eb6b4d6fcb926401470a
Frédéric van der Essen <fvdessen@gmail.com>
2015-05-05 14:04:18 +0200

    [FIX] point_of_sale: barcode patterns must match barcode as prefix, (and not anywhere in barcode)

--- addons/barcodes/static/src/js/barcode_parser.js
+++ addons/barcodes/static/src/js/barcode_parser.js
@@ -155,7 +155,14 @@ openerp.barcodes = function(instance) {
                 match['base_code'] = match['base_code'].replace("\\\\", "\\").replace("\{", "{").replace("\}","}").replace("\.",".");
             }
 
-            match['match'] = match['base_code'].substr(0,base_pattern.length).match(base_pattern);
+            if (base_pattern[0] !== '^') {
+                base_pattern = "^" + base_pattern;
+            }
+            if (base_pattern[base_pattern.length -1] !== '$') {
+                base_pattern = base_pattern + ".*$";
+            }
+            match.match = match.base_code.match(base_pattern);
+
             return match;
         },
                 
