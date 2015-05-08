c1ecdb25a59b34da108b3e4b3dd6800a02779c70
Josse Colpaert <jco@odoo.com>
2015-05-08 11:45:26 +0200

    [WIP] Change queries, but still something wrong with quantities

--- addons/stock_valuation_report/report/valuation_report.py
+++ addons/stock_valuation_report/report/valuation_report.py
@@ -7,53 +7,70 @@ class valuation_report(report_sxw.rml_parse):
     def _get_real(self, form):
         date = form['date']
         category = form['product_category_id'][0]
+        company = form['company_id'][0]
         liste = self.pool['product.product'].search(self.cr, self.uid, [('product_tmpl_id.cost_method', '=', 'real'),
                                                                         ('product_tmpl_id.categ_id', 'child_of', category)])
         res = []
         if liste:
-            self.cr.execute("select id, name_template, sum(qty), sum(qty * cost) \
-                                from\
-                                ((select p.id, p.name_template, sq.qty, sq.cost\
-                                from stock_move sm\
-                                inner join product_product p on p.id = sm.product_id\
-                                inner join product_price_history pph on p.product_tmpl_id = pph.product_template_id\
-                                inner join stock_location destination on sm.location_dest_id = destination.id\
-                                inner join stock_location source on sm.location_id = source.id\
-                                inner join stock_quant_move_rel sqmr on sm.id = sqmr.move_id\
-                                inner join stock_quant sq on sqmr.quant_id = sq.id\
-                                where sm.product_id in %s and sm.company_id = pph.company_id and pph.datetime <= %s \
-                                and sq.qty>0 and destination.usage in ('internal', 'transit') and source.usage not in ('internal'))\
-                                union\
-                                (select p.id, p.name_template, -sq.qty, sq.cost\
-                                from stock_move sm\
-                                inner join product_product p on p.id = sm.product_id\
-                                inner join product_price_history pph on p.product_tmpl_id = pph.product_template_id\
-                                inner join stock_location destination on sm.location_dest_id = destination.id\
-                                inner join stock_location source on sm.location_id = source.id\
-                                inner join stock_quant_move_rel sqmr on sm.id = sqmr.move_id\
-                                inner join stock_quant sq on sqmr.quant_id = sq.id\
-                                where sm.product_id in %s and sm.company_id = pph.company_id and pph.datetime <= %s \
-                                and sq.qty>0 and destination.usage not in ('internal') and source.usage in ('internal', 'transit')))\
-                                as foo \
-                                group by p.id\
-                                ", (tuple(liste), date, tuple(liste), date))
+            self.cr.execute("""select id, name_template, sum(qty), sum(qty * cost)
+                                from
+                                ((select p.id, p.name_template, sq.qty, sq.cost
+                                from stock_move sm
+                                inner join product_product p on p.id = sm.product_id
+                                inner join stock_location destination on sm.location_dest_id = destination.id
+                                inner join stock_location source on sm.location_id = source.id
+                                inner join stock_quant_move_rel sqmr on sm.id = sqmr.move_id
+                                inner join stock_quant sq on sqmr.quant_id = sq.id
+                                where sm.product_id in %s and sm.date <= %s and sm.company_id = %s
+                                and sq.qty>0 and destination.usage in ('internal') and source.usage not in ('internal') and sm.state='done')
+                                union
+                                (select p.id, p.name_template, -sq.qty, sq.cost
+                                from stock_move sm
+                                inner join product_product p on p.id = sm.product_id
+                                inner join stock_location destination on sm.location_dest_id = destination.id
+                                inner join stock_location source on sm.location_id = source.id
+                                inner join stock_quant_move_rel sqmr on sm.id = sqmr.move_id
+                                inner join stock_quant sq on sqmr.quant_id = sq.id
+                                where sm.product_id in %s and sm.date <= %s and sm.company_id = %s
+                                and sq.qty>0 and destination.usage not in ('internal') and source.usage in ('internal') and sm.state='done'))
+                                as foo
+                                group by id, name_template
+                                """, (tuple(liste), date, company, tuple(liste), date, company))
             res = [{'name': t[1], 'qty': t[2], 'value': t[3]} for t in self.cr.fetchall()]
         return res
 
     def _get_standard_average(self, form):
         date = form['date']
         category = form['product_category_id'][0]
+        company = form['company_id'][0]
+
         liste = self.pool['product.product'].search(self.cr, self.uid, [('product_tmpl_id.cost_method', 'in', ('standard', 'average')),
                                                                         ('product_tmpl_id.categ_id', 'child_of', category)])
         res = []
         if liste:
-            self.cr.execute("select p.id, p.name_template, sum(sm.product_qty), sum(sm.product_qty * pph.cost) \
-                                from stock_move sm\
-                                inner join product_product p on p.id = sm.product_id\
-                                inner join product_price_history pph on p.product_tmpl_id = pph.product_template_id\
-                                where product_id in %s and sm.company_id = pph.company_id and pph.datetime <= %s group by p.id\
-                                ", (tuple(liste), date))
-            res = [{'name': t[1], 'qty': t[2], 'value': t[3]} for t in self.cr.fetchall()]
+            self.cr.execute("""select product_id, name_template, product_tmpl_id, sum(qty), sum(qty) * pph.cost
+                                from
+                                ((select p.id as product_id, p.name_template, p.product_tmpl_id, sm.product_qty as qty
+                                from stock_move sm
+                                inner join product_product p on p.id = sm.product_id
+                                inner join stock_location destination on sm.location_dest_id = destination.id
+                                inner join stock_location source on sm.location_id = source.id
+                                where sm.product_id in %s and sm.date <= %s and sm.company_id = %s
+                                 and destination.usage in ('internal') and source.usage not in ('internal') and sm.state='done')
+                                union
+                                (select p.id as product_id, p.name_template, p.product_tmpl_id, -sm.product_qty as qty
+                                from stock_move sm
+                                inner join product_product p on p.id = sm.product_id
+                                inner join stock_location destination on sm.location_dest_id = destination.id
+                                inner join stock_location source on sm.location_id = source.id
+                                where sm.product_id in %s and sm.date <= %s and sm.company_id = %s
+                                and destination.usage not in ('internal') and source.usage in ('internal') and sm.state='done'))
+                                as foo, product_price_history pph,
+                                 (select product_template_id, max(datetime) as datetime from product_price_history where datetime <= %s and company_id = %s group by product_template_id) as pph_date
+                                 where foo.product_tmpl_id = pph_date.product_template_id and pph.datetime = pph_date.datetime and pph.product_template_id = pph_date.product_template_id
+                                 group by product_id, name_template, product_tmpl_id, pph.cost
+                                """, (tuple(liste), date, company, tuple(liste), date, company, date, company))
+            res = [{'name': t[1], 'qty': t[3], 'value': t[4]} for t in self.cr.fetchall()]
         return res
 
     def __init__(self, cr, uid, name, context):
--- addons/stock_valuation_report/wizard/wizard.py
+++ addons/stock_valuation_report/wizard/wizard.py
@@ -8,17 +8,19 @@ class wizard_valuation_report(osv.osv_memory):
     _columns = {
         'date': fields.datetime('Date', required=True),
         'product_category_id': fields.many2one('product.category', 'Category', required=True),
+        'company_id': fields.many2one('res.company', 'Company', required=True),
     }
 
     _defaults = {
         'date': fields.datetime.now,
+        'company_id': lambda self, cr, uid, c: self.pool.get('res.users').browse(cr, uid, uid, c).company_id.id,
     }
 
     def print_report(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
         datas = {'ids': context.get('active_ids', [])}
-        res = self.read(cr, uid, ids, ['date', 'product_category_id'], context=context)
+        res = self.read(cr, uid, ids, ['date', 'product_category_id', 'company_id'], context=context)
         res = res and res[0] or {}
         if res.get('id', False):
             datas['ids'] = [res['id']]
