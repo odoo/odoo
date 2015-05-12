417f012ce97267d9571b8e2dc903947a77b88357
Denis Ledoux <dle@odoo.com>
2015-05-12 12:19:47 +0200

    [IMP] mail: use `has_group` to check a user is part of a group
    `has_groups` is more efficient, and won't lead to useless
    prefetches.
    
    opw-634402

--- addons/mail/mail_thread.py
+++ addons/mail/mail_thread.py
@@ -182,9 +182,8 @@ class mail_thread(osv.AbstractModel):
 
     def read_followers_data(self, cr, uid, follower_ids, context=None):
         result = []
-        technical_group = self.pool.get('ir.model.data').get_object(cr, uid, 'base', 'group_no_one', context=context)
         for follower in self.pool.get('res.partner').browse(cr, uid, follower_ids, context=context):
-            is_editable = uid in map(lambda x: x.id, technical_group.users)
+            is_editable = self.pool['res.users'].has_group(cr, uid, 'base.group_no_one')
             is_uid = uid in map(lambda x: x.id, follower.user_ids)
             data = (follower.id,
                     follower.name,
