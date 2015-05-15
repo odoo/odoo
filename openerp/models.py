bec2a1bef21ff936b635a210bc696a48a4a73c3d
Lionel Sausin <ls@numerigraphe.com>
2015-05-15 10:15:46 +0200

    [IMP] Always respect order of entry
    When a model has an _order attribute, results need to be ordered by that AND id, to respect the order in which the records were entered.
    Users expect record order to be kept when all other sort fields are equal, and PostgrSQL does not guarantee that.
    
    Feature suggested by @mart-e in https://github.com/odoo/odoo/pull/6074#issuecomment-101669152

--- openerp/models.py
+++ openerp/models.py
@@ -4460,6 +4460,7 @@ class BaseModel(object):
         """
         order_by_clause = ''
         order_spec = order_spec or self._order
+        has_id = False
         if order_spec:
             order_by_elements = []
             self._check_qorder(order_spec)
@@ -4471,6 +4472,7 @@ class BaseModel(object):
                 inner_clause = None
                 if order_field == 'id':
                     order_by_elements.append('"%s"."%s" %s' % (self._table, order_field, order_direction))
+                    has_id = True
                 elif order_field in self._columns:
                     order_column = self._columns[order_field]
                     if order_column._classic_read:
@@ -4498,8 +4500,10 @@ class BaseModel(object):
                             order_by_elements.append("%s %s" % (clause, order_direction))
                     else:
                         order_by_elements.append("%s %s" % (inner_clause, order_direction))
-            if order_by_elements:
-                order_by_clause = ",".join(order_by_elements)
+            # Order by id as a last resort to respect order of entry
+            if not has_id:
+                order_by_elements.append('"%s"."%s"' % (self._table, 'id'))
+            order_by_clause = ",".join(order_by_elements)
 
         return order_by_clause and (' ORDER BY %s ' % order_by_clause) or ''
 
