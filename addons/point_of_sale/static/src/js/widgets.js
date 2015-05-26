19eda68547917cfabe923ea41edb8ac7beb141b6
Frédéric van der Essen <fvdessen@gmail.com>
2015-05-26 17:35:29 +0200

    [IMP] web, point_of_sale: basic implementation of the cordova integration.
      This time without a new js file

--- addons/point_of_sale/static/src/js/widgets.js
+++ addons/point_of_sale/static/src/js/widgets.js
@@ -972,6 +972,7 @@ function openerp_pos_widgets(instance, module){ //module is instance.point_of_sa
                 $('.oe_web_client').off();
                 $('.openerp_webclient_container').off();
 
+
                 self.renderElement();
                 
                 self.$('.neworder-button').click(function(){
@@ -1017,6 +1018,8 @@ function openerp_pos_widgets(instance, module){ //module is instance.point_of_sa
                 instance.webclient.set_content_full_screen(true);
 
                 self.$('.loader').animate({opacity:0},1500,'swing',function(){self.$('.loader').addClass('oe_hidden');});
+                
+                instance.web.cordova.send('posready');
 
                 self.pos.push_order();
 
@@ -1231,6 +1234,7 @@ function openerp_pos_widgets(instance, module){ //module is instance.point_of_sa
 
             function close(){
                 self.pos.push_order().then(function(){
+                    instance.web.cordova.send('poslogout');
                     return new instance.web.Model("ir.model.data").get_func("search_read")([['name', '=', 'action_client_pos_menu']], ['res_id']).pipe(function(res) {
                         window.location = '/web#action=' + res[0]['res_id'];
                     },function(err,event) {
