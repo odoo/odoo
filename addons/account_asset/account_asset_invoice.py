fb28f8c0a552067aaa593a80128ed44a6ba3ab78
agb80 <atin81@gmail.com>
2015-05-26 13:17:18 -0500

    Replaced account_invoice workflow with blinker signals
    Odoo workflow framework have some limitations like context propagation
    they are very slow, and others, you can read follow links:
    
    - https://www.odoo.com/nl_NL/groups/community-59/community-9313683?mode=date
    - https://code.launchpad.net/~openerp-community/openobject-server/context-in-workflows/+merge/85518
    - https://www.odoo.com/fr_FR/forum/help-1/question/big-issue-untraceable-workflow-stucked-73306
    
    We have been facing a problem with several of our customers where
    invoices where stuck at Open state even Remain Balance is zero,
    the origin was always a missing workflow instance on database.
    
    So instead of mess with the Odoo workflow engine we decided to replace
    the invoice workflow with blinker signals, that solves the 3 problems
    mentioned above for us.
    
    I'm pretty sure this commit has no chande to be merged on master neither
    v7.0 branch but don't found any way to implement this change on a module
    neither could figure on how to apply a monkey patch to account addon,
    the intention is to get some retroalimentation and know if will be a
    better aproach for make this change.

--- addons/account_asset/account_asset_invoice.py
+++ addons/account_asset/account_asset_invoice.py
@@ -22,10 +22,12 @@
 from openerp.osv import fields, osv
 
 class account_invoice(osv.osv):
-
     _inherit = 'account.invoice'
-    def action_number(self, cr, uid, ids, *args):
-        result = super(account_invoice, self).action_number(cr, uid, ids, *args)
+
+    def action_number(self, cr, uid, ids, context=None):
+        if not context:
+            context = {}
+        result = super(account_invoice, self).action_number(cr, uid, ids, context=context)
         for inv in self.browse(cr, uid, ids):
             self.pool.get('account.invoice.line').asset_create(cr, uid, inv.invoice_line)
         return result
