d7d953b927ac2b1b7ac9fbb8b5a4690f9d75354c
Mahendra barad <mba@openerp.com>
2015-06-01 16:28:18 +0530

    [RESTRUCTURE]Account : Files Restructure according to new api guideliens

--- addons/account/__init__.py
+++ addons/account/__init__.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import partner
-import account
-import account_payment
-import account_invoice
-import account_bank_statement
-import account_bank
-import account_move
-import chart_template
-import account_analytic_line
-import account_journal_dashboard
-import product
-import company
-import res_config
+import models
 
 import wizard
 import report
