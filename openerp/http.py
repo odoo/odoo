8741d9695e6d902efdd4b298f02e631c298d927f
Alexis de Lattre <alexis@via.ecp.fr>
2015-06-04 11:58:46 +0200

    Cleaner fix for bug #6948 as suggested by Christophe Simonis

--- openerp/http.py
+++ openerp/http.py
@@ -955,7 +955,6 @@ class OpenERPSession(werkzeug.contrib.sessions.Session):
             uid = dispatch_rpc('common', 'authenticate', [db, login, password, env])
         else:
             security.check(db, uid, password)
-        self.rotate = False
         self.db = db
         self.uid = uid
         self.login = login
