a8f3ba3396df1b7940a019faea059c707f43f89e
Joren Van Onder <jov@odoo.com>
2015-06-16 10:30:40 +0200

    [IMP] card_reader: better module description

--- addons/card_reader/__openerp__.py
+++ addons/card_reader/__openerp__.py
@@ -1,18 +1,23 @@
 {
-    'name': 'card_reader',
+    'name': 'Card Reader',
     'version': '1.0',
     'category': '',
     'sequence': 6,
-    'summary': 'Magnetic Code Nomenclatures Setup',
+    'summary': 'Credit card support for Point Of Sale',
     'description': """
+Allow credit card POS payments
+==============================
 
-=======================
+This module allows customers to pay for their orders with credit
+cards. The transactions are processed by Mercury (developed by Wells
+Fargo Bank). A Mercury merchant account is necessary. It allows the
+following:
 
-This module defines magnetic code nomenclatures whose rules identify e.g. visa, giftcards, ... .
-It contains the following features:
-- Magnetic code patterns to identify cards containing code for payment transaction
-- Unlimited magnetic code patterns and definitions.
-""",
+* Fast payment by just swiping a credit card while on the payment screen
+* Combining of cash payments and credit card payments
+* Cashback
+* Supported cards: Visa, MasterCard, American Express, Discover
+    """,
     'author': 'Odoo SA',
     'depends': ['web', 'barcodes', 'point_of_sale'],
     'website': '',
