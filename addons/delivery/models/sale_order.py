2ea2b77be64f106206bcc40759090b3d10104766
Mahendra barad <mba@openerp.com>
2015-06-19 15:26:26 +0530

    [IMP] delivery, website_sale_delivery: refactoring and usability
    * migration to new API
    * removed delivery.grid object, has been merged with delivery.carrier
    * change view of delivery.carrier (price rules, etc.)
    * tests from yml to py
    * website : restrict the list of countries for delivery address in the shop

--- addons/delivery/models/sale_order.py
+++ addons/delivery/models/sale_order.py
@@ -1,23 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Odoo, Open Source Business Applications
-#    Copyright (c) 2015 Odoo S.A. <http://openerp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from openerp import models, fields, api, _
 from openerp.exceptions import UserError
@@ -27,18 +9,28 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     delivery_price = fields.Float(string='Estimated Delivery Price', compute='_compute_delivery_price', store=True)
+    carrier_id = fields.Many2one("delivery.carrier", string="Delivery Method", help="Fill this field if you plan to invoice the shipping based on picking.")
 
     @api.depends('carrier_id', 'partner_id', 'order_line')
     def _compute_delivery_price(self):
         for order in self:
             if order.state != 'draft':
-                # we do not want to recompute the shipping price of an already validated/done SO
+                # We do not want to recompute the shipping price of an already validated/done SO
                 continue
             elif order.carrier_id.delivery_type != 'grid' and not order.order_line:
-                # prevent SOAP call to external shipping provider when SO has no lines yet
+                # Prevent SOAP call to external shipping provider when SO has no lines yet
                 continue
             else:
                 order.delivery_price = order.carrier_id.with_context(order_id=order.id).price
+
+    @api.onchange('partner_id')
+    def onchange_partner_id_dtype(self):
+        if self.partner_id:
+            self.carrier_id = self.partner_id.property_delivery_carrier_id
+
+    @api.multi
+    def _delivery_unset(self):
+        self.env['sale.order.line'].search([('order_id', 'in', self.ids), ('is_delivery', '=', True)]).unlink()
 
     @api.multi
     def delivery_set(self):
@@ -49,16 +41,26 @@ class SaleOrder(models.Model):
         self._delivery_unset()
 
         for order in self:
-            if order.carrier_id and order.carrier_id.delivery_type != 'grid':
-                # Shipping providers are used when delivery_type is other than 'grid'
-
+            carrier = order.carrier_id
+            if carrier:
                 if order.state not in ('draft', 'sent'):
                     raise UserError(_('The order state have to be draft to add delivery lines.'))
 
-                carrier = order.carrier_id
-                account_id = carrier.product_id.property_account_income.id
+                if carrier.delivery_type not in ['fixed', 'base_on_rule']:
+                    # Shipping providers are used when delivery_type is other than 'fixed' or 'base_on_rule'
+                    price_unit = order.carrier_id.get_shipping_price_from_so(order)[0]
+                else:
+                    # Classic grid-based carriers
+                    carrier = order.carrier_id.verify_carrier(order.partner_shipping_id)
+                    if not carrier:
+                        raise UserError(_('No carrier matching.'))
+                    price_unit = carrier.get_price_available(order)
+                    if order.company_id.currency_id.id != order.pricelist_id.currency_id.id:
+                        price_unit = order.company_id.currency_id.with_context(date=order.date_order).compute(order.pricelist_id.currency_id.id, price_unit)
+
+                account_id = carrier.product_id.property_account_income_id.id
                 if not account_id:
-                    account_id = carrier.product_id.categ_id.property_account_income_categ.id
+                    account_id = carrier.product_id.categ_id.property_account_income_categ_id.id
 
                 # Apply fiscal position
                 taxes = carrier.product_id.taxes_id
@@ -67,17 +69,23 @@ class SaleOrder(models.Model):
                     account_id = order.fiscal_position_id.map_account(account_id)
                     taxes_ids = order.fiscal_position_id.map_tax(taxes).ids
 
+                # Create the sale order line
                 SaleOrderLine.create({
                     'order_id': order.id,
-                    'name': order.carrier_id.name,
+                    'name': carrier.name,
                     'product_uom_qty': 1,
-                    'product_uom': order.carrier_id.product_id.uom_id.id,
-                    'product_id': order.carrier_id.product_id.id,
-                    'price_unit': order.carrier_id.get_shipping_price_from_so(order)[0],
+                    'product_uom': carrier.product_id.uom_id.id,
+                    'product_id': carrier.product_id.id,
+                    'price_unit': price_unit,
                     'tax_id': [(6, 0, taxes_ids)],
                     'is_delivery': True
                 })
 
             else:
-                # Classic grid-based carriers
-                super(SaleOrder, self).delivery_set()
+                raise UserError(_('No carrier set for this order.'))
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    is_delivery = fields.Boolean(string="Is a Delivery", default=False)
---
+++ addons/delivery/tests/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing detailsself.
+
+from . import test_delivery_cost, test_delivery_stock_move
