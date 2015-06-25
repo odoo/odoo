190a640cb7e8a8096d62771133c6979c75b7b150
Bhavik Bagdiya <bba@odoo.com>
2015-06-25 10:29:27 +0530

    [MIG] website_membership: Migrated to new api.

--- addons/website_membership/models/membership.py
+++ addons/website_membership/models/membership.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import osv, fields
+from odoo import models
 
-class membership_membership_line(osv.Model):
+
+class MembershipMembershipLine(models.Model):
     _inherit = 'membership.membership_line'
 
-    def get_published_companies(self, cr, uid, ids, limit=None, context=None):
-        if not ids:
+    def get_published_companies(self, limit=None):
+        if not self.exists():
             return []
-        limit_clause = '' if limit is None else ' LIMIT %d' % limit
-        cr.execute('SELECT DISTINCT p.id \
-                    FROM res_partner p INNER JOIN membership_membership_line m \
-                    ON  p.id = m.partner \
-                    WHERE website_published AND is_company AND m.id IN %s ' + limit_clause, (tuple(ids),))
-        return [partner_id[0] for partner_id in cr.fetchall()]
+        return self.mapped('partner').filtered(lambda partner: partner.website_published and partner.is_company).ids[:limit]
