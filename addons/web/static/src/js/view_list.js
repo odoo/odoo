58ccdf1ae11a484b8ebbe4a21bec68e8844ea580
Holger Brunn <hbrunn@therp.nl>
2015-06-25 09:45:25 +0200

    [FIX] revert disabling dataurls, use download
    attribute on link

--- addons/web/static/src/js/view_list.js
+++ addons/web/static/src/js/view_list.js
@@ -2302,21 +2302,31 @@ instance.web.list.Binary = instance.web.list.Column.extend({
      * @private
      */
     _format: function (row_data, options) {
-        var text = _t("Download");
+        var text = _t("Download"), filename=_t('Binary file');
         var value = row_data[this.id].value;
+        if (!value) {
+            return options.value_if_empty || '';
+        }
+
         var download_url;
-        download_url = instance.session.url('/web/binary/saveas', {model: options.model, field: this.id, id: options.id});
-        if (this.filename) {
-            download_url += '&filename_field=' + this.filename;
+        if (value.substr(0, 10).indexOf(' ') == -1) {
+            download_url = "data:application/octet-stream;base64," + value;
+        } else {
+            download_url = instance.session.url('/web/binary/saveas', {model: options.model, field: this.id, id: options.id});
+            if (this.filename) {
+                download_url += '&filename_field=' + this.filename;
+            }
         }
         if (this.filename && row_data[this.filename]) {
             text = _.str.sprintf(_t("Download \"%s\""), instance.web.format_value(
                     row_data[this.filename].value, {type: 'char'}));
+            filename = row_data[this.filename].value;
         }
-        return _.template('<a href="<%-href%>"><%-text%></a> (<%-size%>)', {
+        return _.template('<a download="<%-download%>" href="<%-href%>"><%-text%></a> (<%-size%>)', {
             text: text,
             href: download_url,
             size: instance.web.binary_to_binsize(value),
+            download: filename,
         });
     }
 });
