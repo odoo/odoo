6ecb87cc11762f5e9838c34027ca1fdc53af0d62
Mahendra barad <mba@openerp.com>
2015-06-26 15:15:56 +0530

    [MIG]Delivery: migrate to new api

--- addons/delivery/models/delivery_carrier.py
+++ addons/delivery/models/delivery_carrier.py
@@ -20,14 +20,14 @@
 ##############################################################################
 
 import logging
-from openerp import models, fields, api
+from openerp import models, fields, api, _
 from openerp.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
 
 class DeliveryCarrier(models.Model):
-    _inherit = 'delivery.carrier'
+    _name = 'delivery.carrier'
     ''' A Shipping Provider
 
     In order to add your own external provider, follow these steps:
@@ -47,10 +47,20 @@ class DeliveryCarrier(models.Model):
     # Internals for shipping providers #
     # -------------------------------- #
 
+    name = fields.Char(string='Delivery Method', required=True, translate=True)
+    sequence = fields.Integer(default=10, help="Determine the display order")
     delivery_type = fields.Selection([('grid', 'Default Delivery')], string="Delivery Type", default='grid', required='True')
-
     price = fields.Float(compute='get_price')
     available = fields.Boolean(compute='get_price')
+    partner_id = fields.Many2one('res.partner', string='Transport Company', required=True, help="The partner that is doing the delivery service.")
+    product_id = fields.Many2one('product.product', string='Delivery Product', required=True)
+    grid_ids = fields.One2many('delivery.grid', 'carrier_id', string='Delivery Grids', oldname='grids_id')
+    active = fields.Boolean(help="If the active field is set to False, it will allow you to hide the delivery carrier without removing it.", default=True)
+    normal_price = fields.Float(help="Keep empty if the pricing depends on the advanced pricing per destination")
+    free_if_more_than = fields.Boolean(string='Free If Order Total Amount Is More Than', default=False, help="If the order is more expensive than a certain amount, the customer can benefit from a free shipping")
+    amount = fields.Float(help="Amount of the order to benefit from a free shipping, expressed in the company currency")
+    use_detailed_pricelist = fields.Boolean(string='Advanced Pricing per Destination', help="Check this box if you want to manage delivery prices that depends on the destination, the weight, the total of the order, etc.""")
+    pricelist_ids = fields.One2many('delivery.grid', 'carrier_id', string='Advanced Pricing')
 
     @api.one
     def get_price(self):
@@ -123,3 +133,87 @@ class DeliveryCarrier(models.Model):
         self.ensure_one()
         if hasattr(self, '%s_cancel_shipment' % self.delivery_type):
             return getattr(self, '%s_cancel_shipment' % self.delivery_type)(pickings)
+
+    @api.multi
+    def grid_get(self, contact_id):
+        contact = self.env['res.partner'].browse(contact_id)
+        for carrier in self:
+            for grid in carrier.grid_ids:
+                get_id = lambda x: x.id
+                country_ids = map(get_id, grid.country_ids)
+                state_ids = map(get_id, grid.state_ids)
+                if country_ids and not contact.country_id.id in country_ids:
+                    continue
+                if state_ids and not contact.state_id.id in state_ids:
+                    continue
+                if grid.zip_from and (contact.zip or '') < grid.zip_from:
+                    continue
+                if grid.zip_to and (contact.zip or '') > grid.zip_to:
+                    continue
+                return grid.id
+        return False
+
+    @api.multi
+    def create_grid_lines(self, vals):
+        GridLine = self.env['delivery.grid.line']
+        Grid = self.env['delivery.grid']
+        for record in self:
+            # if using advanced pricing per destination: do not change
+            if record.use_detailed_pricelist:
+                continue
+            # not using advanced pricing per destination: override grid
+            grids = Grid.search([('carrier_id', '=', record.id)])
+            if grids and not (record.normal_price or record.free_if_more_than):
+                grids.unlink()
+                grids = None
+
+            # Check that float, else 0.0 is False
+            if not (isinstance(record.normal_price, float) or record.free_if_more_than):
+                continue
+
+            if not grids:
+                grid_data = {
+                    'name': record.name,
+                    'carrier_id': record.id,
+                    'sequence': 10,
+                }
+                grids = Grid.create(grid_data)
+                grids.line_ids.unlink()
+
+            # create the grid lines
+            if record.free_if_more_than:
+                line_data = {
+                    'grid_id': grids.id,
+                    'name': _('Free if more than %.2f') % record.amount,
+                    'variable': 'price',
+                    'operator': '>=',
+                    'max_value': record.amount,
+                    'standard_price': 0.0,
+                    'list_price': 0.0,
+                }
+                GridLine.create(line_data)
+
+            if isinstance(record.normal_price, float):
+                line_data = {
+                    'grid_id': grids.id,
+                    'name': _('Default price'),
+                    'variable': 'price',
+                    'operator': '>=',
+                    'max_value': 0.0,
+                    'standard_price': record.normal_price,
+                    'list_price': record.normal_price,
+                }
+                GridLine.create(line_data)
+        return True
+
+    @api.multi
+    def write(self, vals):
+        res = super(DeliveryCarrier, self).write(vals)
+        self.create_grid_lines(vals)
+        return res
+
+    @api.model
+    def create(self, vals):
+        carrier = super(DeliveryCarrier, self).create(vals)
+        carrier.create_grid_lines(vals)
+        return carrier
--- addons/delivery/models/stock_picking.py
+++ addons/delivery/models/stock_picking.py
@@ -20,12 +20,43 @@
 ##############################################################################
 
 from openerp import models, fields, api
+import openerp.addons.decimal_precision as dp
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
+    @api.depends('product_id', 'move_lines')
+    def _cal_weight(self):
+        for picking in self:
+            total_weight = total_weight_net = 0.00
+
+            for move in picking.move_lines:
+                total_weight += move.weight
+                total_weight_net += move.weight_net
+
+                move.total_weight = total_weight
+                move.total_weight_net = total_weight_net
+
+    def _default_uom(self):
+        uom_categ_id = self.env.ref('product.product_uom_categ_kgm').id
+        return self.env['product.uom'].search([('category_id', '=', uom_categ_id), ('factor', '=', 1)], limit=1)
+
+    @api.multi
+    def _get_picking_line(self):
+        result = {}
+        for line in self.env['stock.move']:
+            result[line.picking_id.id] = True
+        return result.keys()
+
     carrier_price = fields.Float(string="Shipping Cost", readonly=True)
     delivery_type = fields.Selection(related='carrier_id.delivery_type', readonly=True)
+    carrier_id = fields.Many2one('delivery.carrier', string='Carrier')
+    volume = fields.Float(copy=False)
+    weight = fields.Float(compute='_cal_weight', digits=dp.get_precision('Stock Weight'), store=True)
+    weight_net = fields.Float(compute='_cal_weight', string='Net Weight', digits=dp.get_precision('Stock Weight'), store=True)
+    carrier_tracking_ref = fields.Char(string='Carrier Tracking Ref', copy=False)
+    number_of_packages = fields.Integer(string='Number of Packages', copy=False)
+    weight_uom_id = fields.Many2one('product.uom', string='Unit of Measure', required=True, readonly=True, help="Unit of measurement for Weight", default=_default_uom)
 
     @api.multi
     def do_transfer(self):
@@ -83,6 +114,22 @@ class StockPicking(models.Model):
 
         return res
 
+    @api.model
+    def _invoice_create_line(self, moves, journal_id, inv_type='out_invoice'):
+        InvoiceLine = self.env['account.invoice.line']
+        invoice_ids = super(StockPicking, self)._invoice_create_line(moves, journal_id, inv_type=inv_type)
+        delivey_invoices = {}
+        for move in moves:
+            for invoice in move.picking_id.sale_id.invoice_ids:
+                if invoice.id in invoice_ids:
+                    delivey_invoices[invoice] = move.picking_id
+        if delivey_invoices:
+            for invoice, picking in delivey_invoices.items():
+                invoice_line = self._prepare_shipping_invoice_line(picking, invoice)
+                if invoice_line:
+                    InvoiceLine.create(invoice_line)
+        return invoice_ids
+
     @api.one
     def send_to_shipper(self):
         res = self.carrier_id.send_shipping(self)[0]
@@ -108,3 +155,46 @@ class StockPicking(models.Model):
         msg = "Shipment %s cancelled" % self.carrier_tracking_ref
         self.message_post(body=msg)
         self.carrier_tracking_ref = False
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _default_uom(self):
+        uom_categ_id = self.env.ref('product.product_uom_categ_kgm').id
+        return self.env['product.uom'].search([('category_id', '=', uom_categ_id), ('factor', '=', 1)], limit=1)
+
+    weight = fields.Float(compute='_cal_move_weight', digits=dp.get_precision('Stock Weight'), store=True)
+    weight_net = fields.Float(compute='_cal_move_weight', string='Net weight', digits=dp.get_precision('Stock Weight'), store=True)
+    weight_uom_id = fields.Many2one('product.uom', string='Unit of Measure', required=True, readonly=True, help="Unit of Measure (Unit of Measure) is the unit of measurement for Weight", default=_default_uom)
+
+    @api.depends('product_id')
+    def _cal_move_weight(self):
+        for move in self.filtered(lambda moves: moves.product_id.weight > 0.00):
+            weight = weight_net = 0.00
+            converted_qty = move.product_qty
+            weight = (converted_qty * move.product_id.weight)
+
+            if move.product_id.weight_net > 0.00:
+                weight_net = (converted_qty * move.product_id.weight_net)
+
+            move.weight = weight
+            move.weight_net = weight_net
+
+    @api.multi
+    def action_confirm(self):
+        """
+            Pass the carrier to the picking from the sales order
+            (Should also work in case of Phantom BoMs when on explosion the
+                original move is deleted)
+        """
+        procs_to_check = []
+        for move in self:
+            if move.procurement_id and move.procurement_id.sale_line_id and move.procurement_id.sale_line_id.order_id.carrier_id:
+                procs_to_check += [move.procurement_id]
+        res = super(StockMove, self).action_confirm()
+        StockPiking = self.env["stock.picking"]
+        for proc in procs_to_check:
+            pickings = list(set([x.picking_id.id for x in proc.move_ids if x.picking_id and not x.picking_id.carrier_id]))
+            if pickings:
+                StockPiking.write({'carrier_id': proc.sale_line_id.order_id.carrier_id.id})
+        return res
