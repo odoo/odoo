905ac8543e528457bef9e64fa1d417cd6a84032a
Parth Mehta <pme@odoo.com>
2015-06-26 12:26:43 +0530

    [MIGR] delivery: migrated to new api.

--- addons/delivery/models/delivery_carrier.py
+++ addons/delivery/models/delivery_carrier.py
@@ -1,29 +1,79 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+##############################################################################
+#
+#    Odoo, Open Source Business Applications
+#    Copyright (c) 2015 Odoo S.A. <http://openerp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 import logging
-import time
-
-from openerp import api, fields, models, _
+from openerp import models, fields, api, _
 from openerp.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
 
 class DeliveryCarrier(models.Model):
-    _name = "delivery.carrier"
-    _description = "Carrier"
-    _order = 'sequence, id'
+    _inherit = 'delivery.carrier'
+    ''' A Shipping Provider
+    In order to add your own external provider, follow these steps:
+    1. Create your model MyProvider that _inherit 'delivery.carrier'
+    2. Extend the selection of the field "delivery_type" with a pair
+       ('<my_provider>', 'My Provider')
+    3. Add your methods:
+       <my_provider>_get_shipping_price_from_so
+       <my_provider>_send_shipping
+       <my_provider>_open_tracking_page
+       <my_provider>_cancel_shipment
+       (they are documented here# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Business Applications
+#    Copyright (c) 2015 Odoo S.A. <http://openerp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################under)
+    '''
+
+    # -------------------------------- #
+    # Internals for shipping providers #
+    # -------------------------------- #
 
     name = fields.Char(string='Delivery Method', required=True, translate=True)
     sequence = fields.Integer(default=10, help="Determine the display order")
+    delivery_type = fields.Selection([('grid', 'Default Delivery')], string="Delivery Type", default='grid', required='True')
+
+    price = fields.Float(compute='get_price')
+    available = fields.Boolean(compute='get_price')
     partner_id = fields.Many2one('res.partner', string='Transport Company', required=True,
         help="The partner that is doing the delivery service.")
     product_id = fields.Many2one('product.product', string='Delivery Product', required=True)
     grid_ids = fields.One2many('delivery.grid', 'carrier_id', string='Delivery Grids', oldname='grids_id')
-    available = fields.Boolean(compute="get_price", help="Is the carrier method possible with the current order.")
-    price = fields.Float(compute="get_price")
-    active = fields.Boolean(help="If the active field is set to False, it will allow you to hide the delivery carrier without removing it.", default=True)
     normal_price = fields.Float(help="Keep empty if the pricing depends on the advanced pricing per destination")
     free_if_more_than = fields.Boolean(string='Free If Order Total Amount Is More Than',
         default=False, help="If the order is more expensive than a certain amount, the customer can benefit from a free shipping")
@@ -32,36 +82,73 @@ class DeliveryCarrier(models.Model):
         , help="Check this box if you want to manage delivery prices that depends on the destination, the weight, the total of the order, etc.""")
     pricelist_ids = fields.One2many('delivery.grid', 'carrier_id', string='Advanced Pricing')
 
-    @api.multi
-    def name_get(self):
-        order_id = self.env.context.get('order_id')
-        if order_id:
-            currency = self.env['sale.order'].browse(order_id).pricelist_id.currency_id.name or ''
-            return [(rec.id, '%s (%s %s)' % (rec.name, rec.price, currency)) for rec in self]
-        return super(DeliveryCarrier, self).name_get()
-
-    @api.multi
+    @api.one
     def get_price(self):
         SaleOrder = self.env['sale.order']
-        for carrier in self:
-            order_id = self.env.context.get('order_id')
-            price = available = False
-            if order_id:
-                order = SaleOrder.browse(order_id)
-                carrier_grid = carrier.grid_get(order.partner_shipping_id)
-                if carrier_grid:
-                    try:
-                        price = carrier.grid_ids.get_price(order, time.strftime('%Y-%m-%d'))
-                        available = True
-                    except UserError, e:
-                        # no suitable delivery method found, probably
-                        # configuration error
-                        _logger.info("Carrier %s: %s", carrier.name, e.name)
-                        price = 0.0
-                else:
-                    price = 0.0
-                carrier.price = price
-                carrier.available = available
+
+        self.available = False
+        self.price = False
+
+        order_id = self.env.context.get('order_id')
+        if order_id:
+            # FIXME: temporary hack until we refactor the delivery API in master
+
+            if self.delivery_type != 'grid':
+                try:
+                    order = SaleOrder.browse(order_id)
+                    self.price = self.get_shipping_price_from_so(order)[0]
+                    self.available = True
+                except UserError as e:
+                        # no suitable delivery method found, probably configuration error
+                        _logger.info("Carrier %s: %s, not found", self.name, e.name)
+                        self.price = 0.0
+            else:
+                res = super(DeliveryCarrier, self).get_price('price', [])
+                self.available = res[self.id]['available']
+                self.price = res[self.id]['price']
+
+    # -------------------------- #
+    # API for external providers #
+    # -------------------------- #
+
+    # TODO define and handle exceptions that could be thrown by providers
+
+    def get_shipping_price_from_so(self, orders):
+        ''' For every sale order, compute the price of the shipment
+        :param orders: A recordset of sale orders
+        :return list: A list of floats, containing the estimated price for the shipping of the sale order
+        '''
+        self.ensure_one()
+        if hasattr(self, '%s_get_shipping_price_from_so' % self.delivery_type):
+            return getattr(self, '%s_get_shipping_price_from_so' % self.delivery_type)(orders)
+
+    def send_shipping(self, pickings):
+        ''' Send the package to the service provider
+        :param pickings: A recordset of pickings
+        :return list: A list of dictionaries (one per picking) containing of the form::
+                         { 'exact_price': price,
+                           'tracking_number': number }
+        '''
+        self.ensure_one()
+        if hasattr(self, '%s_send_shipping' % self.delivery_type):
+            return getattr(self, '%s_send_shipping' % self.delivery_type)(pickings)
+
+    def get_tracking_link(self, pickings):
+        ''' Ask the tracking link to the service provider
+        :param pickings: A recordset of pickings
+        :return list: A list of string URLs, containing the tracking links for every picking
+        '''
+        self.ensure_one()
+        if hasattr(self, '%s_get_tracking_link' % self.delivery_type):
+            return getattr(self, '%s_get_tracking_link' % self.delivery_type)(pickings)
+
+    def cancel_shipment(self, pickings):
+        ''' Cancel a shipment
+        :param pickings: A recordset of pickings
+        '''
+        self.ensure_one()
+        if hasattr(self, '%s_cancel_shipment' % self.delivery_type):
+            return getattr(self, '%s_cancel_shipment' % self.delivery_type)(pickings)
 
     @api.one
     def grid_get(self, contact_id):
