a15d36fe6e5a858db11a73a516ca964370ac6d85
Martin Trigaux <mat@odoo.com>
2015-06-26 11:11:44 +0200

    [IMP] base: OpenERP to Odoo

--- openerp/addons/base/ir/ir_translation.py
+++ openerp/addons/base/ir/ir_translation.py
@@ -394,7 +394,7 @@ class ir_translation(osv.osv):
         domain = ['&', ('res_id', '=', id), ('name', '=like', model + ',%')]
         langs_ids = self.pool.get('res.lang').search(cr, uid, [('code', '!=', 'en_US')], context=context)
         if not langs_ids:
-            raise UserError(_("Translation features are unavailable until you install an extra OpenERP translation."))
+            raise UserError(_("Translation features are unavailable until you install an extra Odoo translation."))
         langs = [lg.code for lg in self.pool.get('res.lang').browse(cr, uid, langs_ids, context=context)]
         main_lang = 'en_US'
         translatable_fields = []
