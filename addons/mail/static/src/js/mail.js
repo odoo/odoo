74e6cc1c4ed4c7f513c40fa3067ed3fe8c51d947
Vidhin Mehta <vme@odoo.com>
2015-06-29 14:09:56 +0530

    [ADD]Mail: Added a new feature @ functionity for enable add partner to send mail while type its name in body.

--- addons/mail/static/src/js/mail.js
+++ addons/mail/static/src/js/mail.js
@@ -6,6 +6,7 @@ var mail_utils = require('mail.utils');
 var core = require('web.core');
 var data = require('web.data');
 var Dialog = require('web.Dialog');
+var Model = require('web.Model');
 var form_common = require('web.form_common');
 var session = require('web.session');
 var SystrayMenu = require('web.SystrayMenu');
@@ -14,6 +15,7 @@ var utils = require('web.utils');
 var web_client = require('web.web_client');
 var Widget = require('web.Widget');
 var View = require('web.View');
+var Mention = require('mail.mention');
 
 var _t = core._t;
 var QWeb = core.qweb;
@@ -359,6 +361,8 @@ var MailThread = Attachment.extend ({
 
         // add some data to messages for the display
         _.each(this.messages, function (msg) {
+            if(msg.body)
+                msg.body = self.convert_into_link(msg.body);
             if (msg.message_type != "expandable") {
                 msg = self.format_message(msg);
             }
@@ -834,6 +838,7 @@ var MailThread = Attachment.extend ({
      * insert a message in the DOM
      */
     insert_message: function (message) {
+        message.body = this.convert_into_link(message.body);
         message.content = this.view.qweb.render("timeline-message", {record: message});
         this.$('.oe_view_nocontent').remove();
         if (this.options.view_inbox && !this.root) {
@@ -847,6 +852,15 @@ var MailThread = Attachment.extend ({
         }
     },
 
+     /**
+     * Used for @ mention functionality
+     * Convert span tag into link, To make link of partner
+     */
+    convert_into_link: function(msg) {
+        var re = /@<span data-oe-model="([^"]*)"\s+data-oe-id="([^"]*)">(.*?)\<\/span>/g; 
+        var subst = "<a href='#model=$1&id=$2' class='o_timeline_action_author' data-partner='$2'>$3</a>";
+        return msg.replace(re, subst);
+    },
     /**
      * add and convert thread data for display
      */
@@ -1070,11 +1084,16 @@ var ComposeMessage = Attachment.extend ({
     },
 
     start: function () {
+        var self = this;
         this.ds_attachment = new data.DataSetSearch(this, 'ir.attachment');
         this.fileupload_id = _.uniqueId('oe_fileupload_temp');
         $(window).on(this.fileupload_id, this.on_attachment_loaded);
         this.$(".o_timeline_msg_attachment_list").html(this.display_attachments(this));
         this.bind_events();
+        // Store result: current user is from employee group ? (For Mention widget)
+        new Model('res.users').call('has_group', ['base.group_user']).done(function(is_employee) {
+            self.is_employee = is_employee;
+        });
         return this._super.apply(this, arguments);
     },
 
@@ -1083,6 +1102,11 @@ var ComposeMessage = Attachment.extend ({
         this.replaceElement($render);
         this.$(".o_timeline_msg_attachment_list").html(this.display_attachments(this));
         this.bind_events();
+        // Initialize Mention widget if current user is from employee group and it's not log
+        if(this.is_employee && !this.is_log) {
+            this.mail_mention = new Mention(this);
+            this.mail_mention.insertAfter(this.$el.find(".field_text"));
+        }
     },
 
     bind_events: function () {
@@ -1177,8 +1201,26 @@ var ComposeMessage = Attachment.extend ({
 
         return suggested_partners;
     },
-
+    /**
+     * preprocess_message_post: Used for @ mention functionality
+     * If mention partner remove from textarea we don't send mail to those partner.
+     * Make <span> for mentioned parners that is convert into link at read time.
+     */
+    preprocess_message_post: function() {
+        var self = this;
+        this.mail_body = self.$('textarea').val();
+        this.selected_partners = this.mail_mention ? this.mail_mention.selected_partners : [];
+        _.each(this.selected_partners, function(value, key){
+            var word = _.str.sprintf("@%s", value);
+            if(self.mail_body.indexOf(word) != -1) {
+                self.mail_body = self.mail_body.replace(word, _.str.sprintf("@<span data-oe-model='res.partner' data-oe-id='%s'>%s</span> ", key, value));
+               return;
+            }
+            delete self.selected_partners[key];
+        });
+    },
     on_message_post: function (event) {
+        this.preprocess_message_post();
         if (this.flag_post){
             return;
         }
@@ -1200,20 +1242,20 @@ var ComposeMessage = Attachment.extend ({
      * post a message and fetch the message
      */
     do_send_message_post: function (partner_ids, log) {
-        var self = this;
+        var self = this, selected_partners_ids = _.keys(this.selected_partners);
 
         var values = {
-            'body': this.$('textarea').val(),
+            'body': this.mail_body,
             'subject': false,
             'parent_id': this.parent_id || this.id,
-            'partner_ids': partner_ids,
+            'partner_ids': _.uniq(partner_ids.concat(selected_partners_ids)),
             'attachment_ids': _.map(this.attachment_ids, function (file) {return file.id;}),
             'context': _.extend(this.parent_thread.context, {
                 'mail_post_autofollow': true,
                 'mail_post_autofollow_partner_ids': partner_ids,
             }),
             'message_type': 'comment',
-            'content_subtype': 'plaintext',
+            'content_subtype': 'html',
         };
 
         if (log) {
@@ -1227,7 +1269,6 @@ var ComposeMessage = Attachment.extend ({
         } else {
             values['subtype'] = 'mail.mt_comment';
         }
-
         this.parent_thread.ds_thread._model.call('message_post', [this.context.default_res_id], values).done(function (message_id) {
             var thread = self.parent_thread;
 
---
+++ addons/mail/static/src/js/mail_mention.js
@@ -0,0 +1,224 @@
+odoo.define('mail.mention', function (require) {
+"use strict";
+
+var core = require('web.core');
+var Model = require('web.Model');
+var Widget = require('web.Widget');
+var form_common = require('web.form_common');
+
+var _t = core._t;
+var QWeb = core.qweb;
+
+/**
+ * ------------------------------------------------------------
+ * Mention widget
+ * ------------------------------------------------------------
+ *
+ * This widget handles @ mention functionality.
+ * Open dropdown when user mention partner with @
+ * Dropdown open with highlighted string
+ * Start search if word have minimum length is 3
+ * Wait for at least 400 milliseconds without typing any new letter
+ * Maximum number of partner in dropdown list is 8
+ */
+var Mention = Widget.extend({
+    template: null,
+    init: function(parent) {
+        this._super.apply(this, arguments);
+        this.$textarea = parent.$el.find(".field_text");
+        this.$dropdown = $("<div class='mention'><ul></ul></div>");
+        this.model_res_partner = new Model("res.partner");
+        this.selected_partners = {};
+        this.search_keys = {};
+        this.partners = {};
+        this.keyup_list = [40, 38, 13, 35, 33, 34, 27];
+        this.partner_limit = 8;
+        this.typing_speed = 400;
+        this.min_charactor = 4;
+        this.search_string = '';
+        this.recent_string = '';
+    },
+
+    start: function() {
+        this._super.apply(this, arguments);
+        this.bind_events();
+    },
+    bind_events: function() {
+        var self = this;
+        this.$dropdown.insertAfter(this.$textarea);
+        
+        //textear event binding
+        this.$textarea.on('keyup', this.proxy('textarea_keyup'))
+        .on('click', function(e){ self.clear_dropdown(); })
+        .on('keydown', function(e){
+            var $active = self.$dropdown.find("li.active");
+            if(self.recent_string){
+                if (e.keyCode == 40) {
+                    $active.next().trigger("mouseover");
+                    return false;
+                } else if (e.keyCode == 38) {
+                    $active.prev().trigger("mouseover");
+                    return false;
+                } else if(e.keyCode == 13){
+                    self.find_n_replace(parseInt($active.find("span").attr("id")));
+                    return false;
+                }
+            }
+        });
+        
+        //dropdown event binding
+        this.$dropdown.on('mouseover', 'li', function(e) {
+            $(e.currentTarget).addClass('active').siblings().removeClass();
+        }).on('click', 'li', function(e){
+            self.find_n_replace(parseInt($(e.currentTarget).find("span").attr("id")));
+        });
+        this.clear_dropdown();
+    },
+    show_dropdown: function(){
+        var self = this;
+        var highlight = function(description){
+            if(description)
+            return description.replace(new RegExp(self.search_string, "gi"), function(str) {return _.str.sprintf("<b><u>%s</u></b>",str);});};
+
+        var res = _.map(self.search_keys[self.search_string], function(id) {
+                return {
+                    "id": id, 
+                    "name": highlight(self.partners[id]["name"]), 
+                    "email": highlight(self.partners[id]["email"])
+                    };
+        });
+        this.clear_dropdown();
+        this.$dropdown.find("ul").append(QWeb.render('Mention', {"result": res}));
+        this.$dropdown.find("li:first").addClass("active");
+        this.$dropdown.find("ul").show();
+    },
+
+    clear_dropdown: function() {
+        this.$dropdown.find("ul").hide();
+        this.$dropdown.find("ul").empty();
+    },
+    
+    search_n_store: function() {
+        var self = this;
+        if(!this.search_keys[this.search_string] && this.recent_string) {
+            this.model_res_partner.call("search_read", {
+                domain: ['|', ['name', 'ilike', self.search_string], ['email', 'ilike', self.search_string]],
+                fields: ['name', 'email'],
+                limit: this.partner_limit
+            }).done(function(res) {
+                if(!res.length) return;
+                _.each(res, function(r) {self.partners[r.id] = r;});
+                self.search_keys[self.search_string] = _.pluck(res, "id");
+                self.show_dropdown();
+            });
+            return;
+        }
+        self.show_dropdown();
+    },
+    find_n_test: function(){
+        var self = this;
+        var test_string = function(search_str){
+             var pattern = /(^@|(^\s@))/g;
+             var regex_start = new RegExp(pattern);
+             if (regex_start.test(search_str) && search_str.length >= self.min_charactor ){
+                 search_str = _.str.ltrim(search_str.replace(pattern, ''));
+                 return search_str.indexOf(' ') < 0 &&  _.isNull(/\r|\n/.exec(search_str))? search_str:'';
+             }
+            return '';
+        };
+        var desctiption = this.$textarea.val(),
+        left_string = desctiption.substring(0, this.get_cursor_position(this.$textarea)),
+        search_str = desctiption.substring(left_string.lastIndexOf("@") - 1, this.get_cursor_position(this.$textarea));
+        return test_string(search_str);
+    },
+    textarea_keyup: function(e) {
+        var self = this;
+        if(_.contains(this.keyup_list, e.which)) { return;}
+        
+        this.recent_string = this.find_n_test();
+        if(this.recent_string){
+            this.search_string = this.recent_string;
+            clearTimeout(this.timer);
+            this.timer = setTimeout(function() {
+                self.search_n_store();
+            }, this.typing_speed);
+        }else if(this.$dropdown.find("li")){
+            this.clear_dropdown();
+        }
+    },
+
+    show_form_popup: function(email){
+        var deferred = $.Deferred();
+        if(email){
+            deferred.resolve();
+            return deferred;
+        }
+        var partner_form_popup = new form_common.FormOpenPopup(this);
+            partner_form_popup.show_element(
+                'res.partner',
+                id,
+                {
+                    'force_email': true,
+                    'ref': "compound_context",
+                    'default_name': name
+                },
+                {
+                    title: _t("Please complete partner's informations"),
+                    write_function: function(id, data, options) {
+                        return this._super(id, data, options).done(function() {
+                            if(data.name)
+                                self.partners[id]['name'] = data.name;
+                            self.partners[id]['email'] = data.email;
+                            deferred.resolve();
+                        });
+                    },
+                }
+            );
+            partner_form_popup.on('closed', self, function() {
+                self.$textarea.focus();
+            });
+        return deferred;
+    },
+    find_n_replace: function(id) {
+        var self = this;
+        var partner = this.partners[id],
+            name = partner["name"], 
+            email = partner["email"];
+        
+        this.show_form_popup(email).done(function() {
+            self.clear_dropdown();
+            self.selected_partners[id] = name;
+            var index = self.get_cursor_position(self.$textarea), value = self.$textarea.val();
+            self.$textarea.val(_.str.sprintf("%s%s%s", 
+                    value.substring(0, index - self.search_string.length), 
+                    name, 
+                    value.substring(index, value.length)));
+            self.set_cursor_position(self.$textarea, index - self.search_string.length + name.length);
+        });
+    },
+    get_cursor_position: function(ctrl) {
+        var el = $(ctrl).get(0);
+        if(!el) return 0;
+        if('selectionStart' in el) {
+            return el.selectionStart;
+        } else if('selection' in document) {
+            var cr = document.selection.createRange();
+            return cr.moveStart('character', -el.focus().value.length).text.length - cr.text.length;
+        }
+        return 0;
+    },
+
+    set_cursor_position: function(ctrl, pos) {
+        ctrl.each(function(index, elem) {
+            if (elem.setSelectionRange)
+              elem.setSelectionRange(pos, pos);
+            else if (elem.createTextRange)
+              elem.createTextRange().collapse(true).moveEnd('character', pos).moveStart('character', pos).select();
+          });
+        return ctrl;
+    }
+});
+
+return Mention;
+
+});
