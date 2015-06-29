872254bca65a31285f109d56c2050f0aeda93958
Jigar Patel <jat@openerp.com>
2015-06-29 16:46:03 +0530

    [MOV] marketing_campaign_crm_demo: Move file according to new module structure

--- addons/marketing_campaign_crm_demo/__openerp__.py
+++ addons/marketing_campaign_crm_demo/__openerp__.py
@@ -18,7 +18,7 @@ Creates demo data like leads, campaigns and segments for the module marketing_ca
     """,
     'website': 'https://www.odoo.com/page/lead-automation',
     'data': [],
-    'demo': ['marketing_campaign_demo.xml'],
+    'demo': ['demo/marketing_campaign_demo.xml'],
     'installable': True,
     'auto_install': False,
 }
