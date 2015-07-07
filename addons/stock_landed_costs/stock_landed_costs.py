9ce56a4cc134d95cec07c4af364103e5edad6ce0
Erwin van der Ploeg <erwin@bas-solutions.nl>
2015-07-07 18:13:33 +0200

    [IMP] stock_landed_cost: better message why landed cost can't be validated

--- addons/stock_landed_costs/stock_landed_costs.py
+++ addons/stock_landed_costs/stock_landed_costs.py
@@ -178,7 +178,7 @@ class stock_landed_cost(osv.osv):
             if cost.state != 'draft':
                 raise UserError(_('Only draft landed costs can be validated'))
             if not cost.valuation_adjustment_lines or not self._check_sum(cr, uid, cost, context=context):
-                raise UserError(_('You cannot validate a landed cost which has no valid valuation lines.'))
+                raise UserError(_('You cannot validate a landed cost which has no valid valuation adjustments lines. Did you click on Compute?'))
             move_id = self._create_account_move(cr, uid, cost, context=context)
             quant_dict = {}
             for line in cost.valuation_adjustment_lines:
