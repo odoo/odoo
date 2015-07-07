1c1593eae226ffcda01e7590c0573a4aa612c94d
Goffin Simon <sig@odoo.com>
2015-07-07 11:36:28 +0200

    [FIX] web: lunch filters
    When you clicked on "Search more" to add a product in your lunch order,
    the default filter didn't appear because the wizard didn't contain an action id.
    
    Introduced by 5cd78b8
    The bug solved by this commit didn't appear without the commit.
    
    opw:643951

--- addons/web/static/src/js/search.js
+++ addons/web/static/src/js/search.js
@@ -1630,10 +1630,6 @@ instance.web.search.FavoriteMenu = instance.web.Widget.extend({
                 }
             })
             .on('reset', this.proxy('clear_selection'));
-        if (!this.action_id) {
-            this.prepare_dropdown_menu([]);
-            return $.when();
-        }
         return this.model.call('get_filters', [this.target_model, this.action_id],
                                {context: this.searchview.dataset.context})
             .done(this.proxy('prepare_dropdown_menu'));
