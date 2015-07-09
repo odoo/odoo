080191f38cd48c1fc45da6fa600c39bf1451d891
Arthur Maniet <arthurmaniet@me.com>
2015-07-09 10:31:06 +0200

    [REF] barcodes: update the manifest

--- addons/barcodes/__openerp__.py
+++ addons/barcodes/__openerp__.py
@@ -1,23 +1,29 @@
 {
     'name': 'Barcodes',
-    'version': '1.0',
-    'category': '',
-    'sequence': 6,
-    'summary': 'Barcode Nomenclatures Setup',
+    'version': '2.0',
+    'category': 'Hidden/Dependency',
+    'summary': 'Barcodes Scanning and Parsing',
     'description': """
+This module adds support for barcode scanning and parsing.
 
-=======================
+Scanning
+--------
+Use an USB scanner (that mimics keyboard inputs) in order to work with barcodes in Odoo.
+The scanner must be configured to use no prefix and a carriage return or tab as suffix.
+The delay between each character input must be less than or equal to 50 milliseconds.
+This is how most barcode scanners will work out of the box.
+However, make sure the scanner uses the same keyboard layout as the device it's plugged in.
 
-This module defines barcode nomenclatures whose rules identify e.g. products, locations.
-It contains the following features:
-- Barcode patterns to identify barcodes containing a numerical value (e.g. weight, price)
-- Definitin of barcode aliases that allow to identify the same product with different barcodes
-- Unlimited barcode patterns and definitions. 
-- Barcode EAN13 encoding supported
+Parsing
+-------
+The barcodes are interpreted using the rules defined by a nomenclature.
+It provides the following features:
+- Patterns to identify barcodes containing a numerical value (e.g. weight, price)
+- Definition of barcode aliases that allow to identify the same product with different barcodes
+- Support for encodings EAN-13, EAN-8 and UPC-A
 """,
-    'author': 'OpenERP SA',
+    'author': 'Odoo SA',
     'depends': ['web'],
-    'website': '',
     'data': [
         'data/barcodes_data.xml',
         'barcodes_view.xml',
