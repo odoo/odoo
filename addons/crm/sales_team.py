b6418a116d5b1b9e6763bf1886ac94423ae02b68
Fabien Pinckaers <fp@openerp.com>
2015-07-13 12:52:58 -0700

    [FIX] When creating a column from the opportunity kanban, assign column to right team [IMP] Creating New, Won, Lost stages when creating a new sales team

--- addons/crm/sales_team.py
+++ addons/crm/sales_team.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp.osv import fields, osv
+from openerp.tools.translate import _
 
 
 class crm_team(osv.Model):
@@ -33,6 +34,39 @@ class crm_team(osv.Model):
         team_id = super(crm_team, self).create(cr, uid, vals, context=create_context)
         team = self.browse(cr, uid, team_id, context=context)
         self.pool.get('mail.alias').write(cr, uid, [team.alias_id.id], {'alias_parent_thread_id': team_id, 'alias_defaults': {'team_id': team_id, 'type': 'lead'}}, context=context)
+        if (not vals) or (not len(vals.get('stage_ids', []))):
+            self.pool.get('crm.stage').create(
+                cr, uid, {
+                    'name': _('New'),
+                    'sequence': 0,
+                    'on_change': False,
+                    'team_ids': [(6, 0, [team_id])],
+                    'fold': False,
+                    'type': 'both'
+                }, context=context
+            )
+            self.pool.get('crm.stage').create(
+                cr, uid, {
+                    'name': _('Lost'),
+                    'sequence': 19,
+                    'probability': 0,
+                    'on_change': True,
+                    'team_ids': [(6, 0, [team_id])],
+                    'fold': True,
+                    'type': 'both'
+                }, context=context
+            )
+            self.pool.get('crm.stage').create(
+                cr, uid, {
+                    'name': _('Won'),
+                    'sequence': 20,
+                    'probability': 100,
+                    'on_change': True,
+                    'team_ids': [(6, 0, [team_id])],
+                    'fold': True,
+                    'type': 'both'
+                }, context=context
+            )
         return team_id
 
     def unlink(self, cr, uid, ids, context=None):
