4287b5e5905ae4c0c6c450cd2c0b0178a6ab928a
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2015-07-14 14:54:02 +0300

    Update l10n_ro with the v8 modifications.

--- addons/l10n_ro/__init__.py
+++ addons/l10n_ro/__init__.py
@@ -2,7 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 # @author -  Fekete Mihai <feketemihai@gmail.com>
-# Copyright (C) 2011 TOTAL PC SYSTEMS (http://www.www.erpsystems.ro).
+# Copyright (C) 2015 Forest and Biomass Services Romania (http://www.forbiom.eu).
+# Copyright (C) 2011 TOTAL PC SYSTEMS (http://www.erpsystems.ro).
 # Copyright (C) 2009 (<http://www.filsystem.ro>)
 
 import res_partner
