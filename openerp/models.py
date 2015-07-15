9f364ff400a04bf1d61539eee3dcb4f90d1d10c4
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2015-07-15 11:51:02 +0200

    update the naming of the m2m indices
    use the shorter name suggested by @antonylessuisse
    
    closes #2780

--- openerp/models.py
+++ openerp/models.py
@@ -2753,9 +2753,9 @@ class BaseModel(object):
             cr.execute("SELECT relkind FROM pg_class WHERE relkind IN ('v') AND relname=%s", (self._table,))
             if not cr.fetchall():
                 self._m2o_add_foreign_key_unchecked(m2m_tbl, col1, self, 'cascade')
-            index_name_pattern = "%s_%s_index"
-            for col in (col1, col2):
-                index_name = index_name_pattern % (m2m_tbl, col)
+            index_name_pattern = "%s_idx%d"
+            for num, col in enumerate((col1, col2)):
+                index_name = index_name_pattern % (m2m_tbl, num+1)
                 if len(index_name) > 63:
                     _logger.warning(
                         'Index creation for m2m relation %s %s-> %s will '
@@ -2763,7 +2763,7 @@ class BaseModel(object):
                         '"relation=\'<forced_rel_table_name>\' in the m2m '
                         'field declaration.',
                         m2m_tbl, self._table, ref)
-                cr.execute('CREATE INDEX "%s" ON "%s" ("%s")' % (index, m2m_tbl, col))
+                cr.execute('CREATE INDEX "%s" ON "%s" ("%s")' % (index_name, m2m_tbl, col))
             cr.execute("COMMENT ON TABLE \"%s\" IS 'RELATION BETWEEN %s AND %s'" % (m2m_tbl, self._table, ref))
             cr.commit()
             _schema.debug("Create table '%s': m2m relation between '%s' and '%s'", m2m_tbl, self._table, ref)
