034f540664fa3d2c939f2d22e65fcf575e05940d
Jusab Sida <jsi@odoo.com>
2015-07-17 15:30:43 +0530

    [IMP] crm, base_calendar: help messages on the active field were misleading. Actually, not really misleading, just wrong. Because the behavior is the opposite of what the help tells.

--- addons/base_calendar/base_calendar.py
+++ addons/base_calendar/base_calendar.py
@@ -647,7 +647,7 @@ class res_alarm(osv.osv):
 are both optional, but if one occurs, so MUST the other"""),
         'repeat': fields.integer('Repeat'),
         'active': fields.boolean('Active', help="If the active field is set to \
-true, it will allow you to hide the event alarm information without removing it.")
+false, it will allow you to hide the event alarm information without removing it.")
     }
     _defaults = {
         'trigger_interval': 'minutes',
@@ -1108,7 +1108,7 @@ rule or repeating pattern of time to exclude from the recurring rule."),
                                  'event_id', 'attendee_id', 'Attendees'),
         'allday': fields.boolean('All Day', states={'done': [('readonly', True)]}),
         'active': fields.boolean('Active', help="If the active field is set to \
-         true, it will allow you to hide the event alarm information without removing it."),
+         false, it will allow you to hide the event alarm information without removing it."),
         'recurrency': fields.boolean('Recurrent', help="Recurrent Meeting"),
         'partner_ids': fields.many2many('res.partner', string='Attendees', states={'done': [('readonly', True)]}),
     }
