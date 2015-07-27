4ae642869ac40148e94a78821ac040c767e4ed60
vbh-odoo <vbh@odoo.com>
2015-07-27 11:43:19 +0530

    [MOV] project_issue: re-organized file structure

--- addons/project_issue/models/__init__.py
+++ addons/project_issue/models/__init__.py
@@ -1,1 +1,6 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import project_issue
 import project_task_type
+import project_issue_config_settings
