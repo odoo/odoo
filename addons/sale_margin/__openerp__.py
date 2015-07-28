6b0e9ab6b01a616d6a8828286e9df8d6c3604d36
Anand Kansagra <aka@openerp.com>
2015-07-28 11:06:27 +0530

    [ADD/REMOVE/MODIFY] sale_margin: Directory restructure as per new guideline.

--- addons/sale_margin/__openerp__.py
+++ addons/sale_margin/__openerp__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
@@ -11,11 +12,10 @@ This module adds the 'Margin' on sales order.
 This gives the profitability by calculating the difference between the Unit
 Price and Cost Price.
     """,
-    'author':'OpenERP SA',
+    'author':'Odoo S.A.',
     'depends':['sale'],
-    'demo':['sale_margin_demo.xml'],
+    'demo':['data/sale_margin_demo.xml'],
     'test': ['test/sale_margin.yml'],
-    'data':['security/ir.model.access.csv','sale_margin_view.xml'],
-    'auto_install': False,
+    'data':['security/ir.model.access.csv','views/sale_margin_view.xml'],
     'installable': True,
 }
