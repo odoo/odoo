43e3663d932654ed1a866a891096a3cac5680175
Fabien Pinckaers <fp@openerp.com>
2015-07-29 14:52:29 -0700

    [FIX] Scheduling of orderpoint: it was wrong to add the supplier lead time at every rocurment (what do you do if you manufature) and wrong for PO: you were always late

--- addons/stock/procurement.py
+++ addons/stock/procurement.py
@@ -422,7 +422,11 @@ class procurement_order(osv.osv):
         return {}
 
     def _get_orderpoint_date_planned(self, cr, uid, orderpoint, start_date, context=None):
-        date_planned = start_date + relativedelta(days=orderpoint.product_id.seller_delay or 0.0)
+        days = orderpoint.lead_days or 0.0
+        if orderpoint.lead_type=='purchase':
+            # These days will be substracted when creating the PO
+            days += orderpoint.product_id.seller_delay or 0.0
+        date_planned = start_date + relativedelta(days=days)
         return date_planned.strftime(DEFAULT_SERVER_DATE_FORMAT)
 
     def _prepare_orderpoint_procurement(self, cr, uid, orderpoint, product_qty, context=None):
