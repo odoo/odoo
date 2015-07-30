beb97ed93a2fc36a38a43cb2e1f914ee75671645
Anand Kansagra <aka@openerp.com>
2015-07-30 12:56:16 +0530

    [IMP] sale_margin: split sale_order_line, sale_order models.

--- addons/sale_margin/models/__init__.py
+++ addons/sale_margin/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import sale_margin
+import inherited_sale_order
+import inherited_sale_order_line
---
+++ addons/sale_margin/models/inherited_sale_order.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from openerp.osv import fields, osv
+import openerp.addons.decimal_precision as dp
+
+
+class sale_order(osv.osv):
+    _inherit = "sale.order"
+
+    def _product_margin(self, cr, uid, ids, field_name, arg, context=None):
+        result = {}
+        for sale in self.browse(cr, uid, ids, context=context):
+            result[sale.id] = 0.0
+            for line in sale.order_line:
+                if line.state == 'cancel':
+                    continue
+                result[sale.id] += line.margin or 0.0
+        return result
+
+    def _get_order(self, cr, uid, ids, context=None):
+        result = {}
+        for line in self.pool.get('sale.order.line').browse(cr, uid, ids, context=context):
+            result[line.order_id.id] = True
+        return result.keys()
+
+    _columns = {
+        'margin': fields.function(_product_margin, string='Margin', help="It gives profitability by calculating the difference between the Unit Price and the cost price.", store={
+                'sale.order.line': (_get_order, ['margin', 'purchase_price'], 20),
+                'sale.order': (lambda self, cr, uid, ids, c={}: ids, ['order_line'], 20),
+                }, digits_compute= dp.get_precision('Product Price')),
+    }
