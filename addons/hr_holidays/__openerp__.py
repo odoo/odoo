d5ded741370aaed1c252dbafe65d7a3a33caba0c
Bhavik Bagadiya <bba@odoo.com>
2015-07-30 16:38:19 +0200

    [IMP] hr_holidays: usability improvements
    - New calendar dashboard with the leaves of every employee
    - New "Leaves to Approve" menu with to-approve department leaves
    - Renamed and reorder menus
    
    Odoo Task 12913
    Closes #7292

--- addons/hr_holidays/__openerp__.py
+++ addons/hr_holidays/__openerp__.py
@@ -6,16 +6,16 @@
     'name': 'Leave Management',
     'version': '1.5',
     'category': 'Human Resources',
-    'sequence': 85,
+    'sequence': 27,
     'summary': 'Holidays, Allocation and Leave Requests',
     'website': 'https://www.odoo.com/page/employees',
     'description': """
-Manage leaves and allocation requests
+Manage leave requests and allocations
 =====================================
 
-This application controls the holiday schedule of your company. It allows employees to request holidays. Then, managers can review requests for holidays and approve or reject them. This way you can control the overall holiday planning for the company or department.
+This application controls the leave schedule of your company. It allows employees to request leaves. Then, managers can review requests for leaves and approve or reject them. This way you can control the overall leave planning for the company or department.
 
-You can configure several kinds of leaves (sickness, holidays, paid days, ...) and allocate leaves to an employee or department quickly using allocation requests. An employee can also make a request for more days off by making a new Allocation. It will increase the total of available days for that leave type (if the request is accepted).
+You can configure several kinds of leaves (sickness, paid days, ...) and allocate leaves to an employee or department quickly using leave allocation. An employee can also make a request for more days off by making a new Leave allocation. It will increase the total of available days for that leave type (if the request is accepted).
 
 You can keep track of leaves in different ways by following reports: 
 
@@ -23,7 +23,7 @@ You can keep track of leaves in different ways by following reports:
 * Leaves by Department
 * Leaves Analysis
 
-A synchronization with an internal agenda (Meetings of the CRM module) is also possible in order to automatically create a meeting when a holiday request is accepted by setting up a type of meeting in Leave Type.
+A synchronization with an internal agenda (Meetings of the CRM module) is also possible in order to automatically create a meeting when a leave request is accepted by setting up a type of meeting in Leave Type.
 """,
     'depends': ['hr', 'calendar', 'resource'],
     'data': [
