52f7f0b126160beee9fb02d27719e5c4cd788104
Christophe Simonis <chs@odoo.com>
2015-08-05 13:00:27 +0200

    [FIX] web: `/web` route should be reachable in non-monodb mode

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -456,9 +456,12 @@ class Home(http.Controller):
     def index(self, s_action=None, db=None, **kw):
         return http.local_redirect('/web', query=request.params, keep_hash=True)
 
-    @http.route('/web', type='http', auth="user")
+    # ideally, this route should be `auth="user"` but that don't work in non-monodb mode.
+    @http.route('/web', type='http', auth="none")
     def web_client(self, s_action=None, **kw):
         ensure_db()
+        if not request.session.uid:
+            return werkzeug.utils.redirect('/web/login', 303)
         if kw.get('redirect'):
             return werkzeug.utils.redirect(kw.get('redirect'), 303)
 
