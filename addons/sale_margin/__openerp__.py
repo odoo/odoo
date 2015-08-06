1ca86e70dec275e24503c5441ce452a8e4763927
Anand Kansagra <aka@openerp.com>
2015-08-06 11:34:44 +0530

    [IMP] sale_margin: Migrate into new api.

--- addons/sale_margin/__openerp__.py
+++ addons/sale_margin/__openerp__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
@@ -11,11 +12,8 @@ This module adds the 'Margin' on sales order.
 This gives the profitability by calculating the difference between the Unit
 Price and Cost Price.
     """,
-    'author':'OpenERP SA',
     'depends':['sale'],
     'demo':['data/sale_margin_demo.xml'],
     'test': ['test/sale_margin.yml'],
     'data':['security/ir.model.access.csv','views/sale_margin_view.xml'],
-    'auto_install': False,
-    'installable': True,
 }
--- addons/sale_margin/models/sale_order.py
+++ addons/sale_margin/models/sale_order.py
@@ -1,11 +1,15 @@
+# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp import api
-from openerp.osv import fields, osv
-import openerp.addons.decimal_precision as dp
+from odoo import api, fields, models
+import odoo.addons.decimal_precision as dp
 
-class sale_order_line(osv.osv):
+
+class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
+
+    margin = fields.Float(compute='_product_margin', digits=dp.get_precision('Product Price'), store=True)
+    purchase_price = fields.Float(string='Cost', digits=dp.get_precision('Product Price'))
 
     @api.multi
     @api.onchange('product_id', 'product_uom_qty')
@@ -22,46 +26,19 @@ class sale_order_line(osv.osv):
                 price = frm_cur.with_context(ctx).compute(purchase_price, to_cur, round=False)
                 line.purchase_price = price
 
-    def _product_margin(self, cr, uid, ids, field_name, arg, context=None):
-        cur_obj = self.pool.get('res.currency')
-        res = {}
-        for line in self.browse(cr, uid, ids, context=context):
-            cur = line.order_id.pricelist_id.currency_id
-            res[line.id] = 0
-            if line.product_id:
-                tmp_margin = line.price_subtotal - ((line.purchase_price or line.product_id.standard_price) * line.product_uom_qty)
-                res[line.id] = cur_obj.round(cr, uid, cur, tmp_margin)
-        return res
-
-    _columns = {
-        'margin': fields.function(_product_margin, string='Margin', digits_compute= dp.get_precision('Product Price'),
-              store = True),
-        'purchase_price': fields.float('Cost', digits_compute= dp.get_precision('Product Price'))
-    }
+    @api.depends('product_id', 'purchase_price', 'product_uom_qty', 'price_unit')
+    def _product_margin(self):
+        for line in self:
+            currency = line.order_id.pricelist_id.currency_id
+            line.margin = currency.round(line.price_subtotal - ((line.purchase_price or line.product_id.standard_price) * line.product_uom_qty))
 
 
-class sale_order(osv.osv):
+class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _product_margin(self, cr, uid, ids, field_name, arg, context=None):
-        result = {}
-        for sale in self.browse(cr, uid, ids, context=context):
-            result[sale.id] = 0.0
-            for line in sale.order_line:
-                if line.state == 'cancel':
-                    continue
-                result[sale.id] += line.margin or 0.0
-        return result
+    margin = fields.Monetary(compute='_product_margin', help="It gives profitability by calculating the difference between the Unit Price and the cost.", currency_field='currency_id', digits=dp.get_precision('Product Price'), store=True)
 
-    def _get_order(self, cr, uid, ids, context=None):
-        result = {}
-        for line in self.pool.get('sale.order.line').browse(cr, uid, ids, context=context):
-            result[line.order_id.id] = True
-        return result.keys()
-
-    _columns = {
-        'margin': fields.function(_product_margin, string='Margin', help="It gives profitability by calculating the difference between the Unit Price and the cost.", store={
-                'sale.order.line': (_get_order, ['margin', 'purchase_price'], 20),
-                'sale.order': (lambda self, cr, uid, ids, c={}: ids, ['order_line'], 20),
-                }, digits_compute= dp.get_precision('Product Price')),
-    }
+    @api.depends('order_line.margin')
+    def _product_margin(self):
+        for order in self:
+            order.margin = sum(order.order_line.filtered(lambda r: r.state != 'cancel').mapped('margin'))
