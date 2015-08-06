93261e07de568cc42d0b0295311a1d87a40461b2
Yannick Tivisse <yti@odoo.com>
2015-08-06 11:53:43 +0200

    [IMP] hr_recruitment:  Add a button to refuse an applicant and archive him       Add a button to reset the application in the first stage

--- addons/hr_recruitment/models/hr_recruitment.py
+++ addons/hr_recruitment/models/hr_recruitment.py
@@ -481,6 +481,18 @@ class Applicant(models.Model):
         dict_act_window['view_mode'] = 'form,tree'
         return dict_act_window
 
+    @api.multi
+    def archive_applicant(self):
+        """ Archive an hr.applicant as it was refused """
+        for applicant in self:
+            applicant.write({'active': False})
+
+    @api.multi
+    def reset_applicant(self):
+        """ Reinsert the applicant into the recruitment pipe"""
+        for applicant in self:
+            first_stage_obj = self.env['hr.recruitment.stage'].search([('job_ids', 'in', applicant.job_id.id)], order="sequence asc", limit=1)
+            applicant.write({'active': True, 'stage_id': first_stage_obj.id})
 
 class applicant_category(models.Model):
     _name = "hr.applicant.category"
