bba0ff15bc873df91f4db00087c2d72493862a3f
Laurent Mignon <laurent.mignon@acsone.eu>
2015-08-07 17:24:20 +0200

    fixes #6929 Use nominatim service from OSM to retrieve lat/long for a partner to avoid licence infringement  with google

--- addons/base_geolocalize/models/res_partner.py
+++ addons/base_geolocalize/models/res_partner.py
@@ -30,34 +30,34 @@ from openerp import tools
 from openerp.tools.translate import _
 
 
-def geo_find(addr):
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address='
-    url += urllib.quote(addr.encode('utf8'))
-
+def geo_find(street=None, zip=None, city=None, state=None, country_code=None):
+    url = 'http://nominatim.openstreetmap.org/search'
+    p = {
+        'limit': 1,
+        'format': 'json',
+        'street': street or '',
+        'city': city or '',
+        'country': country_code or '',
+        'postalcode': zip or '',
+    }
+    params = {}
+    for k, v in p.iteritems():
+        params[k] = tools.ustr(v).encode('utf-8')
+    query = urllib.urlencode(params)
+    url = url+'?%s' % query
     try:
         result = json.load(urllib.urlopen(url))
     except Exception, e:
         raise osv.except_osv(_('Network error'),
                              _('Cannot contact geolocation servers. Please make sure that your internet connection is up and running (%s).') % e)
-    if result['status'] != 'OK':
+    if not result:
         return None
 
     try:
-        geo = result['results'][0]['geometry']['location']
-        return float(geo['lat']), float(geo['lng'])
+        geo = result[0]
+        return float(geo['lat']), float(geo['lon'])
     except (KeyError, ValueError):
         return None
-
-
-def geo_query_address(street=None, zip=None, city=None, state=None, country=None):
-    if country and ',' in country and (country.endswith(' of') or country.endswith(' of the')):
-        # put country qualifier in front, otherwise GMap gives wrong results,
-        # e.g. 'Congo, Democratic Republic of the' => 'Democratic Republic of the Congo'
-        country = '{1} {0}'.format(*country.split(',', 1))
-    return tools.ustr(', '.join(filter(None, [street,
-                                              ("%s %s" % (zip or '', city or '')).strip(),
-                                              state,
-                                              country])))
 
 
 class res_partner(osv.osv):
@@ -74,11 +74,11 @@ class res_partner(osv.osv):
         for partner in self.browse(cr, uid, ids):
             if not partner:
                 continue
-            result = geo_find(geo_query_address(street=partner.street,
-                                                zip=partner.zip,
-                                                city=partner.city,
-                                                state=partner.state_id.name,
-                                                country=partner.country_id.name))
+            result = geo_find(street=partner.street,
+                              zip=partner.zip,
+                              city=partner.city,
+                              state=partner.state_id.name,
+                              country_code=partner.country_id.code)
             if result:
                 self.write(cr, uid, [partner.id], {
                     'partner_latitude': result[0],
