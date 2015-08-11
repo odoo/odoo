0a2f036d46b15661b690a0ccaf20a1310f7c33a7
Thibault Delavallée <tde@openerp.com>
2015-08-11 10:47:53 +0200

    [MOVE] event: reorganize files according to the guidelines.
    No change has been made in this commit, only file move and renaming.

--- addons/event/models/__init__.py
+++ addons/event/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import models
-import wizard
-import report
+import event
+import event_mail
+import event_config_settings
