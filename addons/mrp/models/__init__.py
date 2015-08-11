25f0bd6cfaaff91c87e2ce51d25b316882c75669
Dhaval Panchal <dpa@odoo.com>
2015-08-11 18:04:31 +0530

    [REF] mrp: split file class wise.

--- addons/mrp/models/__init__.py
+++ addons/mrp/models/__init__.py
@@ -2,8 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import company
-import mrp
+import mrp_bom
+import mrp_production
+import mrp_property
+import mrp_routing
+import mrp_workcenter
 import procurement
 import product
 import res_config
-import stock
+import stock_move
+import stock_warehouse
