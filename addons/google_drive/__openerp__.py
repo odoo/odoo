a121e795052658559c44fd744a38057bc8b5da07
vbh-odoo <vbh@odoo.com>
2015-08-12 19:49:17 +0530

    [MIG] google_drive: Migrated to new API

--- addons/google_drive/__openerp__.py
+++ addons/google_drive/__openerp__.py
@@ -5,24 +5,23 @@
     'name': 'Google Drive™ integration',
     'version': '0.2',
     'category': 'Tools',
-    'installable': True,
-    'auto_install': False,
-    'data': [
-        'security/ir.model.access.csv',
-        'views/res_config_user_view.xml',
-        'data/google_drive_data.xml',
-        'views/google_drive.xml',
-        'views/base_config_setting_views.xml',
-    ],
-    'demo': [
-        'data/google_drive_demo.xml'
-    ],
-    'depends': ['base_setup', 'google_account'],
     'description': """
 Integrate google document to Odoo record.
 ============================================
 
 This module allows you to integrate google documents to any of your Odoo record quickly and easily using OAuth 2.0 for Installed Applications,
-You can configure your google Authorization Code from Settings > Configuration > General Settings by clicking on "Generate Google Authorization Code"
-"""
+You can configure your google Authorization Code from Settings > General Settings by clicking on "Generate Google Authorization Code"
+""",
+    'depends': ['base_setup', 'google_account'],
+    'data': [
+        'data/google_drive_data.xml',
+        'security/ir.model.access.csv',
+        'views/res_config_user_view.xml',
+        'views/google_drive_views.xml',
+        'views/base_config_setting_views.xml',
+    ],
+    'demo': [
+        'data/google_drive_demo.xml'
+    ],
+    'installable': True,
 }
