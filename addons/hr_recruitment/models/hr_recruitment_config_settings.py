310d93cb1a15af54c182c4bfd8d350dfc64a1183
Yannick Tivisse <yti@odoo.com>
2015-08-12 13:00:33 +0200

    [REM] hr_recruitment: Remaining old api res_config.py file Update new_api one also

--- addons/hr_recruitment/models/hr_recruitment_config_settings.py
+++ addons/hr_recruitment/models/hr_recruitment_config_settings.py
@@ -8,9 +8,12 @@ class RecruitmentSettings(models.TransientModel):
     _name = 'hr.recruitment.config.settings'
     _inherit = ['res.config.settings', 'fetchmail.config.settings']
 
-    module_document = fields.Boolean(string='Allow the automatic indexation of resumes',
-                                     help='Manage your CV\'s and motivation letter related to all applicants.\n'
-                                     '-This installs the module document_ftp. This will install the knowledge management  module in order to allow you to search using specific keywords through  the content of all documents (PDF, .DOCx...)')
+    module_document = fields.Selection(selection=[
+            (0, "Do not manage CVs and motivation letter"),
+            (1, 'Allow the automatic indexation of resumes')
+            ], string='Resumes',
+            help='Manage your CV\'s and motivation letter related to all applicants.\n'
+                            '-This installs the module document_ftp. This will install the knowledge management  module in order to allow you to search using specific keywords through  the content of all documents (PDF, .DOCx...)')
     alias_prefix = fields.Char(string='Default Alias Name for Jobs')
     alias_domain = fields.Char(string='Alias Domain',
                                default=lambda self: self.env["ir.config_parameter"].get_param("mail.catchall.domain"))
