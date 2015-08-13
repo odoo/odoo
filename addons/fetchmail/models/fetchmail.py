17b7a2045f401d7ee8fb99361864dc992fff6bcc
Keyur Gajjar <kga@odoo.com>
2015-08-13 12:26:04 +0530

    [MOV] fetchmail: splitted models and views through spearated files
    - splitted the models/mail_mail.py from models/fetchmail.py.
    - splitted the views/mail_mail_views.xml from views/fetchmail_views.xml

--- addons/fetchmail/models/fetchmail.py
+++ addons/fetchmail/models/fetchmail.py
@@ -270,30 +270,3 @@ openerp_mailgate: "|/path/to/openerp-mailgate.py --host=localhost -u %(uid)d -p 
         res = super(fetchmail_server, self).unlink(cr, uid, ids, context=context)
         self._update_cron(cr, uid, context=context)
         return res
-
-class mail_mail(osv.osv):
-    _inherit = "mail.mail"
-    _columns = {
-        'fetchmail_server_id': fields.many2one('fetchmail.server', "Inbound Mail Server",
-                                               readonly=True,
-                                               select=True,
-                                               oldname='server_id'),
-    }
-
-    def create(self, cr, uid, values, context=None):
-        if context is None:
-            context = {}
-        fetchmail_server_id = context.get('fetchmail_server_id')
-        if fetchmail_server_id:
-            values['fetchmail_server_id'] = fetchmail_server_id
-        res = super(mail_mail, self).create(cr, uid, values, context=context)
-        return res
-
-    def write(self, cr, uid, ids, values, context=None):
-        if context is None:
-            context = {}
-        fetchmail_server_id = context.get('fetchmail_server_id')
-        if fetchmail_server_id:
-            values['fetchmail_server_id'] = fetchmail_server_id
-        res = super(mail_mail, self).write(cr, uid, ids, values, context=context)
-        return res
