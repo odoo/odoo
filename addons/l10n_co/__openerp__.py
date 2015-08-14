30b39ec49793bda5f2ffdbf69a2bf62eceb3f2a2
qdp-odoo <qdp@odoo.com>
2015-08-14 09:19:26 +0200

    [FIX] l10n_co is back, compliant with the new accounting

--- addons/l10n_co/__openerp__.py
+++ addons/l10n_co/__openerp__.py
@@ -19,12 +19,13 @@
         'base_vat',
     ],
     'data': [
-        'data/account.account.type.csv',
+        'data/account.account.tag.csv',
+        'data/account_chart_template_before.xml',
         'data/account.account.template.csv',
         'data/account_chart_template.xml',
         'data/account.tax.template.csv',
-        'wizard/account_wizard.xml',
+        'data/account_chart_template.yml',
     ],
     'demo': [],
-    'installable': False,
+    'installable': True,
 }
