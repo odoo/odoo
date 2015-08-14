38cdc1376ecb52ab7aafc311d299536e0e84f1d7
fka-odoo <fka@openerp.com>
2015-08-14 10:47:52 +0530

    [IMP] Improve code and added groupby account_id in general ledger report

--- addons/account/wizard/account_report_common.py
+++ addons/account/wizard/account_report_common.py
@@ -31,9 +31,9 @@ class AccountCommonReport(models.TransientModel):
     def check_report(self):
         self.ensure_one()
         data = {}
-        data['ids'] = self._context.get('active_ids', [])
-        data['model'] = self._context.get('active_model', 'ir.ui.menu')
+        data['ids'] = self.env.context.get('active_ids', [])
+        data['model'] = self.env.context.get('active_model', 'ir.ui.menu')
         data['form'] = self.read(['date_from', 'date_to', 'journal_ids', 'target_move'])[0]
         used_context = self._build_contexts(data)
-        data['form']['used_context'] = dict(used_context, lang=self._context.get('lang', 'en_US'))
+        data['form']['used_context'] = dict(used_context, lang=self.env.context.get('lang', 'en_US'))
         return self._print_report(data)
