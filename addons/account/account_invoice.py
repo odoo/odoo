1eae43b10be1fc6a3026fd80b2bb06869b8ea262
julioserna <julio@vauxoo.com>
2015-08-19 23:25:12 +0000

    [IMP][account] About commit sha #cfbd086 and issue #813the round should be the same to amount_currency

--- addons/account/account_invoice.py
+++ addons/account/account_invoice.py
@@ -739,7 +739,7 @@ class account_invoice(models.Model):
             if self.currency_id != company_currency:
                 currency = self.currency_id.with_context(date=self.date_invoice or fields.Date.context_today(self))
                 line['currency_id'] = currency.id
-                line['amount_currency'] = line['price']
+                line['amount_currency'] = currency.round(line['price'])
                 line['price'] = currency.compute(line['price'], company_currency)
             else:
                 line['currency_id'] = False
