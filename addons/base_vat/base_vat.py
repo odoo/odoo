f9f978cbfd8ae7374e28b017cba8d40ff0157bc0
dsabrinarg <sabrina@vauxoo.com>
2015-08-19 10:50:28 -0500

    [IMP] filter option by partner with vat number added.

--- addons/base_vat/base_vat.py
+++ addons/base_vat/base_vat.py
@@ -226,6 +226,53 @@ class res_partner(osv.osv):
         #Valid format and valid date
         return True
 
+    def name_search(self, cr, user, name, args=None, operator='ilike',
+                    context=None, limit=100):
+        if not args:
+            args = []
+        if context is None:
+            context = {}
+        if name:
+            ptrn_name = re.compile('(\[(.*?)\])')
+            res_name = ptrn_name.search(name)
+            if res_name:
+                name = name.replace('['+res_name.group(2)+'] ', '')
+            partner_search = super(res_partner, self).name_search(cr, user,
+                                                                  name, args,
+                                                                  operator,
+                                                                  context,
+                                                                  limit)
+            ids = [partner[0] for partner in partner_search]
+            if not ids:
+                ids = self.search(cr, user, [('vat', operator, name)] + args,
+                                  limit=limit, context=context)
+            if not ids:
+                ptrn = re.compile('(\[(.*?)\])')
+                res = ptrn.search(name)
+                if res:
+                    ids = self.search(cr, user,
+                                      [('vat', operator, res.group(2))] + args,
+                                      limit=limit, context=context)
+            return self.name_get(cr, user, ids, context=context)
+        else:
+            return super(res_partner, self).name_search(cr, user,
+                                                        name, args, operator,
+                                                        context, limit)
+
+    def name_get(self, cr, uid, ids, context=None):
+        if isinstance(ids, (list, tuple)) and not len(ids):
+            return []
+        if isinstance(ids, (long, int)):
+            ids = [ids]
+        res_name = super(res_partner, self).name_get(cr, uid, ids, context)
+        res = []
+        for record in res_name:
+            partner = self.browse(cr, uid, record[0], context=context)
+            name = record[1]
+            if partner.vat:
+                name = '['+partner.vat+'] '+name
+            res.append((record[0], name))
+        return res
 
     # Norway VAT validation, contributed by Rolv Råen (adEgo) <rora@adego.no>
     def check_vat_no(self, vat):
