362dcdd99a188c6f641d6d0da6cf263747130331
Aaron Bohy <aab@odoo.com>
2015-08-20 09:22:32 +0200

    [FIX] web: search_menu: add missing 'use strict' statement

--- addons/web/static/src/js/views/search_menus.js
+++ addons/web/static/src/js/views/search_menus.js
@@ -258,10 +258,11 @@ return Widget.extend({
 });
 
 odoo.define('web.FilterMenu', function (require) {
+"use strict";
 
-var search_filters = require('web.search_filters'),
-    search_inputs = require('web.search_inputs'),
-    Widget = require('web.Widget');
+var search_filters = require('web.search_filters');
+var search_inputs = require('web.search_inputs');
+var Widget = require('web.Widget');
 
 return Widget.extend({
     template: 'SearchView.FilterMenu',
@@ -365,10 +366,11 @@ return Widget.extend({
 });
 
 odoo.define('web.GroupByMenu', function (require) {
+"use strict";
 
-var Widget = require('web.Widget'),
-    core = require('web.core'),
-    search_inputs = require('web.search_inputs');
+var Widget = require('web.Widget');
+var core = require('web.core');
+var search_inputs = require('web.search_inputs');
 
 var QWeb = core.qweb;
 
