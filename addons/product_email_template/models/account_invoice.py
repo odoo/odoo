95cdf93211a7a342c391e0d521945d4bc23c123e
Prashant Panchal <ppa@odoo.com>
2015-08-24 17:00:39 +0530

    [REF] product_email_template: migration to the new API

--- addons/product_email_template/models/account_invoice.py
+++ addons/product_email_template/models/account_invoice.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import osv
+from odoo import api, models
 
 
-class account_invoice(osv.Model):
+class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    def invoice_validate_send_email(self, cr, uid, ids, context=None):
-        Composer = self.pool['mail.compose.message']
-        for invoice in self.browse(cr, uid, ids, context=context):
+    def invoice_validate_send_email(self):
+        for invoice in self.filtered(lambda x: x.type == 'out_invoice'):
             # send template only on customer invoice
-            if invoice.type != 'out_invoice':
-                continue
             # subscribe the partner to the invoice
             if invoice.partner_id not in invoice.message_partner_ids:
-                self.message_subscribe(cr, uid, [invoice.id], [invoice.partner_id.id], context=context)
+                invoice.message_subscribe([invoice.partner_id.id])
             for line in invoice.invoice_line_ids:
                 if line.product_id.email_template_id:
                     invoice.message_post_with_template(line.product_id.email_template_id.id, composition_mode='comment')
         return True
 
-    def invoice_validate(self, cr, uid, ids, context=None):
-        res = super(account_invoice, self).invoice_validate(cr, uid, ids, context=context)
-        self.invoice_validate_send_email(cr, uid, ids, context=context)
+    @api.multi
+    def invoice_validate(self):
+        res = super(AccountInvoice, self).invoice_validate()
+        self.invoice_validate_send_email()
         return res
