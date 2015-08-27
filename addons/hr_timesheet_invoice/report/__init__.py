9294e73e96e09fb18113a4404962e7be104d09c0
Nicolas Martinelli <nim@odoo.com>
2015-08-27 11:53:38 +0200

    [DEL] hr_timesheet_invoice: deprecated module
    Reason: complete rewrite of the Sale module. Invoicing is now managed through
    Sale.
    
    Responsible: fp, dbo, nim

--- addons/hr_timesheet_invoice/report/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-import account_analytic_profit
-import report_analytic
-import hr_timesheet_invoice_report
