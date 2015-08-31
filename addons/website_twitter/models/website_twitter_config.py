c3ff15b147d8ba394dc9a608583f4c3a1de08a48
Rohan Patel <rop@odoo.com>
2015-08-31 11:01:25 +0530

    [MIG] website_twitter: migration to new api

--- addons/website_twitter/models/website_twitter_config.py
+++ addons/website_twitter/models/website_twitter_config.py
@@ -1,62 +1,57 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import logging
 from urllib2 import URLError, HTTPError
-
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
-from openerp.exceptions import UserError
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
+TWITTER_EXCEPTION = {
+    304: _('There was no new data to return.'),
+    400: _('The request was invalid or cannot be otherwise served. Requests without authentication are considered invalid and will yield this response.'),
+    401: _('Authentication credentials were missing or incorrect. Maybe screen name tweets are protected.'),
+    403: _('The request is understood, but it has been refused or access is not allowed. Please check your Twitter API Key and Secret.'),
+    429: _('Request cannot be served due to the applications rate limit having been exhausted for the resource.'),
+    500: _('Twitter seems broken. Please retry later. You may consider posting an issue on Twitter forums to get help.'),
+    502: _('Twitter is down or being upgraded.'),
+    503: _('The Twitter servers are up, but overloaded with requests. Try again later.'),
+    504: _('The Twitter servers are up, but the request could not be serviced due to some failure within our stack. Try again later.')
+}
 
-class twitter_config_settings(osv.TransientModel):
+
+class WebsiteTwitterConfig(models.TransientModel):
     _inherit = 'website.config.settings'
 
-    _columns = {
-        'twitter_api_key': fields.related(
-            'website_id', 'twitter_api_key', type="char",
-            string='Twitter API Key',
-            help="Twitter API key you can get it from https://apps.twitter.com/app/new"),
-        'twitter_api_secret': fields.related(
-            'website_id', 'twitter_api_secret', type="char",
-            string='Twitter API secret',
-            help="Twitter API secret you can get it from https://apps.twitter.com/app/new"),
-        'twitter_tutorial': fields.dummy(
-            type="boolean", string="Show me how to obtain the Twitter API Key and Secret"),
-        'twitter_screen_name': fields.related(
-            'website_id', 'twitter_screen_name',
-            type="char", string='Get favorites from this screen name',
-            help="Screen Name of the Twitter Account from which you want to load favorites."
-                 "It does not have to match the API Key/Secret."),
-    }
+    twitter_api_key = fields.Char(
+        related='website_id.twitter_api_key',
+        string='Twitter API Key',
+        help='Twitter API key you can get it from https://apps.twitter.com/app/new')
+    twitter_api_secret = fields.Char(
+        related='website_id.twitter_api_secret',
+        string='Twitter API secret',
+        help='Twitter API secret you can get it from https://apps.twitter.com/app/new')
+    twitter_tutorial = fields.Boolean(string='Show me how to obtain the Twitter API Key and Secret')
+    twitter_screen_name = fields.Char(
+        related='website_id.twitter_screen_name',
+        string='Get favorites from this screen name',
+        help='Screen Name of the Twitter Account from which you want to load favorites.'
+             'It does not have to match the API Key/Secret.')
 
-    def _get_twitter_exception_message(self, error_code, context=None):
-        TWITTER_EXCEPTION = {
-            304: _('There was no new data to return.'),
-            400: _('The request was invalid or cannot be otherwise served. Requests without authentication are considered invalid and will yield this response.'),
-            401: _('Authentication credentials were missing or incorrect. Maybe screen name tweets are protected.'),
-            403: _('The request is understood, but it has been refused or access is not allowed. Please check your Twitter API Key and Secret.'),
-            429: _('Request cannot be served due to the applications rate limit having been exhausted for the resource.'),
-            500: _('Twitter seems broken. Please retry later. You may consider posting an issue on Twitter forums to get help.'),
-            502: _('Twitter is down or being upgraded.'),
-            503: _('The Twitter servers are up, but overloaded with requests. Try again later.'),
-            504: _('The Twitter servers are up, but the request could not be serviced due to some failure within our stack. Try again later.')
-        }
+    def _get_twitter_exception_message(self, error_code):
         if error_code in TWITTER_EXCEPTION:
             return TWITTER_EXCEPTION[error_code]
         else:
             return _('HTTP Error: Something is misconfigured')
 
-    def _check_twitter_authorization(self, cr, uid, config_id, context=None):
-        website_obj = self.pool['website']
-        website_config = self.browse(cr, uid, config_id, context=context)
+    def _check_twitter_authorization(self):
         try:
-            website_obj.fetch_favorite_tweets(cr, uid, [website_config.website_id.id], context=context)
+            self.website_id.fetch_favorite_tweets()
 
         except HTTPError, e:
             _logger.info("%s - %s" % (e.code, e.reason), exc_info=True)
-            raise UserError("%s - %s" % (e.code, e.reason) + ':' + self._get_twitter_exception_message(e.code, context))
+            raise UserError("%s - %s" % (e.code, e.reason) + ':' + self._get_twitter_exception_message(e.code))
         except URLError, e:
             _logger.info(_('We failed to reach a twitter server.'), exc_info=True)
             raise UserError(_('Internet connection refused') + ' ' + _('We failed to reach a twitter server.'))
@@ -64,14 +59,16 @@ class twitter_config_settings(osv.TransientModel):
             _logger.info(_('Please double-check your Twitter API Key and Secret!'), exc_info=True)
             raise UserError(_('Twitter authorization error!') + ' ' + _('Please double-check your Twitter API Key and Secret!'))
 
-    def create(self, cr, uid, vals, context=None):
-        res_id = super(twitter_config_settings, self).create(cr, uid, vals, context=context)
+    @api.model
+    def create(self, vals):
+        TwitterConfig = super(WebsiteTwitterConfig, self).create(vals)
         if vals.get('twitter_api_key') or vals.get('twitter_api_secret') or vals.get('twitter_screen_name'):
-            self._check_twitter_authorization(cr, uid, res_id, context=context)
-        return res_id
+            TwitterConfig._check_twitter_authorization()
+        return TwitterConfig
 
-    def write(self, cr, uid, ids, vals, context=None):
-        res_id = super(twitter_config_settings, self).write(cr, uid, ids, vals, context=context)
+    @api.multi
+    def write(self, vals):
+        TwitterConfig = super(WebsiteTwitterConfig, self).write(vals)
         if vals.get('twitter_api_key') or vals.get('twitter_api_secret') or vals.get('twitter_screen_name'):
-            self._check_twitter_authorization(cr, uid, ids, context=context)
-        return res_id
+            self._check_twitter_authorization()
+        return TwitterConfig
--- addons/website_twitter/models/website_twitter_tweet.py
+++ addons/website_twitter/models/website_twitter_tweet.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
+from odoo import fields, models
 
 
-class WebsiteTwitterTweet(osv.osv):
-    _name = "website.twitter.tweet"
-    _description = "Twitter Tweets"
-    _columns = {
-        'website_id': fields.many2one('website', string="Website"),
-        'screen_name': fields.char("Screen Name"),
-        'tweet': fields.text('Tweets'),
+class WebsiteTwitterTweet(models.Model):
+    _name = 'website.twitter.tweet'
 
-        # Twitter IDs are 64-bit unsigned ints, so we need to store them in
-        # unlimited precision NUMERIC columns, which can be done with a
-        # float field. Used digits=(0,0) to indicate unlimited.
-        # Using VARCHAR would work too but would have sorting problems.  
-        'tweet_id': fields.float("Tweet ID", digits=(0,0)), # Twitter
-    }
+    website_id = fields.Many2one('website', string='Website')
+    screen_name = fields.Char(string='Screen Name')
+    tweet = fields.Text(string='Tweets')
+
+    # Twitter IDs are 64-bit unsigned ints, so we need to store them in
+    # unlimited precision NUMERIC columns, which can be done with a
+    # float field. Used digits=(0,0) to indicate unlimited.
+    # Using VARCHAR would work too but would have sorting problems.
+    tweet_id = fields.Float(string='Tweet ID', digits=(0, 0))  # Twitter
