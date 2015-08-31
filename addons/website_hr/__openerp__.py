4c96dd813e39bf79d1d7739a53bce63f5717ec85
rsi-odoo <rsi@odoo.com>
2015-08-31 14:08:00 +0530

    [MOV] website_hr: file renaming according to new api

--- addons/website_hr/__openerp__.py
+++ addons/website_hr/__openerp__.py
@@ -10,14 +10,14 @@ Our Team Page
         """,
     'depends': ['website', 'hr'],
     'demo': [
-        'data/website_hr_demo.xml',
+        'data/hr_employee_demo.xml',
     ],
     'data': [
         'data/website_hr_data.xml',
-        'views/website_hr.xml',
-        'views/website_hr_view.xml',
+        'views/website_hr_templates.xml',
+        'views/hr_employee_views.xml',
         'security/ir.model.access.csv',
-        'security/website_hr.xml',
+        'security/hr_employee_security.xml',
     ],
     'qweb': ['static/src/xml/*.xml'],
     'installable': True,
