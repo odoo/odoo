6ae2c5ef3b6c7d26ab19f228318d5ca1fcbf8dc0
rsi-odoo <rsi@odoo.com>
2015-08-31 14:37:47 +0530

    [MIG] website_hr: code migration to new api.

--- addons/website_hr/models/hr_employee.py
+++ addons/website_hr/models/hr_employee.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import osv, fields
+from odoo import api, fields, models
 
-
-class hr_employee(osv.osv):
+class HrEmployee(models.Model):
     _name = 'hr.employee'
     _inherit = ['hr.employee', 'website.published.mixin']
 
-    _columns = {
-        'public_info': fields.char('Public Info'),
-    }
+    public_info = fields.Char(string='Public Info')
 
-    def _website_url(self, cr, uid, ids, field_name, arg, context=None):
-        res = super(hr_employee, self)._website_url(cr, uid, ids, field_name, arg, context=context)
-        res.update({(employee_id, '/page/website.aboutus#team') for employee_id in ids})
+    @api.multi
+    def _website_url(self, field_name, arg):
+        res = super(HrEmployee, self)._website_url(field_name, arg)
+        res.update({(employee_id, '/page/website.aboutus#team') for employee_id in self.ids})
         return res
