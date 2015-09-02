724d2355e4a432e2834e019a48b7215ffe7a47ff
Prashant Panchal <ppa@odoo.com>
2015-09-02 11:11:00 +0530

    [MOVE] move res_currency and decimal_percision from produt file to two diffrent model of file

---
+++ addons/product/models/res_currency.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from openerp.osv import osv
+from openerp.tools.float_utils import float_compare
+
+
+class res_currency(osv.osv):
+    _inherit = 'res.currency'
+
+    def _check_main_currency_rounding(self, cr, uid, ids, context=None):
+        cr.execute('SELECT digits FROM decimal_precision WHERE name like %s',('Account',))
+        digits = cr.fetchone()
+        if digits and len(digits):
+            digits = digits[0]
+            main_currency = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.currency_id
+            for currency_id in ids:
+                if currency_id == main_currency.id:
+                    if float_compare(main_currency.rounding, 10 ** -digits, precision_digits=6) == -1:
+                        return False
+        return True
+
+    _constraints = [
+        (_check_main_currency_rounding, 'Error! You cannot define a rounding factor for the company\'s main currency that is smaller than the decimal precision of \'Account\'.', ['rounding']),
+    ]
