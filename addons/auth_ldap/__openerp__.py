64ac8caa060f7067adc8c7e3bd381a6f5350e0d8
Kinjal Mehta <kme@odoo.com>
2015-09-02 14:53:08 +0530

    [MOVE] auth_ldap: Move files in related models and views directory

--- addons/auth_ldap/__openerp__.py
+++ addons/auth_ldap/__openerp__.py
@@ -2,17 +2,15 @@
 
 {
     'name' : 'Authentication via LDAP',
-    'version' : '1.0',
     'depends' : ['base'],
     #'description': < auto-loaded from README file
     'category' : 'Extra Tools',
     'data' : [
-        'users_ldap_view.xml',
-        'user_ldap_installer.xml',
+        'views/res_company_views.xml',
+        'views/ldap_installer_views.xml',
         'security/ir.model.access.csv',
     ],
     'auto_install': False,
-    'installable': True,
     'external_dependencies' : {
         'python' : ['ldap'],
     }
