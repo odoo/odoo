a10ae1bfb8abaa22a98884f064b114d95205cbd8
Yannick Tivisse <yti@odoo.com>
2015-09-02 16:31:22 +0200

    [FIX] web_kanban: crash on project kanban loading if a task in in undefined stage kanban_normal cannot be loaded on the js side because 'Undefined' is not a stage, which was leading to an rpc call with null id

--- addons/web_kanban/static/src/js/kanban_widgets.js
+++ addons/web_kanban/static/src/js/kanban_widgets.js
@@ -114,7 +114,7 @@ var KanbanSelection = AbstractField.extend({
             var leg_opt = self.options && self.options.states_legend || null;
             if (leg_opt) {
                 var key = leg_opt[value.name];
-                var legend = self.parent.group_info[key];
+                var legend = self.parent.group_info && self.parent.group_info[key];
                 if (legend) {
                     value.state_name = legend;
                     value.tooltip = legend;
