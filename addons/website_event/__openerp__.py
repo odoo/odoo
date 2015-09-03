6aee28e6858b5cabb87dfbbb0271938b3954b856
Deep Bundela <dbu@odoo.com>
2015-09-03 12:40:46 +0530

    [MOV] website_event: reorganise module files
    - group models and views
    - remove useless 'doc/' directory
    - adapt module manifest

--- addons/website_event/__openerp__.py
+++ addons/website_event/__openerp__.py
@@ -12,15 +12,15 @@ Online Events
         """,
     'depends': ['website', 'website_partner', 'website_mail', 'event'],
     'data': [
-        'data/event_data.xml',
-        'views/website_event.xml',
-        'views/website_event_backend.xml',
+        'data/website_event_data.xml',
+        'views/website_event_templates.xml',
+        'views/website_event_views.xml',
         'security/ir.model.access.csv',
-        'security/website_event.xml',
+        'security/website_event_security.xml',
     ],
     'qweb': ['static/src/xml/*.xml'],
     'demo': [
-        'data/event_demo.xml'
+        'data/website_event_demo.xml'
     ],
     'installable': True,
     'application': True,
