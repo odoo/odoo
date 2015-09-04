bd0c6a2b242107be49df6b220da5218eceaf91e3
Xavier Morel <xmo@odoo.com>
2015-09-04 11:50:30 +0200

    [FIX] website_crm and website_hr_recruitment tours
    * run tours as public users since these screens should be accessible to
      and used by public users
    * fix access rights issues in website_crm tour (default value of team_id
      when creating a crm.lead requiring read access on crm.team, which the
      public user does not have)
    * remove duplicated crm.lead field (phone declared as char(size=64) then
      as char() a few lines below, removed first instance)

--- addons/website_crm/tests/test_website_crm.py
+++ addons/website_crm/tests/test_website_crm.py
@@ -4,7 +4,7 @@ import openerp.tests
 @openerp.tests.common.post_install(True)
 class TestWebsiteCrm(openerp.tests.HttpCase):
     def test_tour(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('website_crm_tour', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.website_crm_tour", login='admin')
+        self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('website_crm_tour', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.website_crm_tour")
 
         # need environment using the test cursor as it's not committed
         cr = self.registry.cursor()
