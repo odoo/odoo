108e424baf0d09c522668a9ed9261530e1216d04
qdp-odoo <qdp@odoo.com>
2015-09-07 17:12:43 +0200

    [IMP] account: auto_install localization module(s) if the country is known

--- addons/account/__openerp__.py
+++ addons/account/__openerp__.py
@@ -60,4 +60,5 @@ You could use this simplified accounting in case you work with an (external) acc
     'installable': True,
     'application': True,
     'auto_install': False,
+    'post_init_hook': '_auto_install_l10n',
 }
