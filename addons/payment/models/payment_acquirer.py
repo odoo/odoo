e178b42a9145fa657dcf64aadd12baa4ed95eda4
Damien Bouvy <dbo@odoo.com>
2015-09-08 16:30:12 +0200

    [FIX] payment: triggering the on_change to get partner details sometime put the partner_country_id to False; the default was there explicitly to avoid this kind of problem

--- addons/payment/models/payment_acquirer.py
+++ addons/payment/models/payment_acquirer.py
@@ -414,7 +414,7 @@ class PaymentTransaction(osv.Model):
                 'partner_zip': partner and partner.zip or False,
                 'partner_address': _partner_format_address(partner and partner.street or '', partner and partner.street2 or ''),
                 'partner_city': partner and partner.city or False,
-                'partner_country_id': partner and partner.country_id.id or False,
+                'partner_country_id': partner and partner.country_id.id or self._default_partner_country_id(cr, uid, context=context),
                 'partner_phone': partner and partner.phone or False,
             }}
         return {}
