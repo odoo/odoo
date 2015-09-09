99e717733ba78e4f630cfabb988aabf069070f10
Jainik Patel <jaa@odoo.com>
2015-09-09 16:17:48 +0530

    [MIG] hr_gamification: Migrated to new api.

--- addons/hr_gamification/wizard/gamification_badge_user_wizard.py
+++ addons/hr_gamification/wizard/gamification_badge_user_wizard.py
@@ -1,43 +1,31 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
-from openerp.exceptions import UserError
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
-class hr_grant_badge_wizard(osv.TransientModel):
-    _name = 'gamification.badge.user.wizard'
-    _inherit = ['gamification.badge.user.wizard']
 
-    _columns = {
-        'employee_id': fields.many2one("hr.employee", string='Employee', required=True),
-        'user_id': fields.related("employee_id", "user_id",
-                                  type="many2one", relation="res.users",
-                                  store=True, string='User')
-    }
+class GamificationBadgeUserWizard(models.TransientModel):
+    _inherit = 'gamification.badge.user.wizard'
 
-    def action_grant_badge(self, cr, uid, ids, context=None):
+    employee_id = fields.Many2one('hr.employee', string='Employee', required=True)
+    user_id = fields.Many2one('res.users', string='User', related='employee_id.user_id', store=True)
+
+    @api.multi
+    def action_grant_badge(self):
         """Wizard action for sending a badge to a chosen employee"""
-        if context is None:
-            context = {}
+        if not self.user_id:
+            raise UserError(_('You can send badges only to employees linked to a user.'))
 
-        badge_user_obj = self.pool.get('gamification.badge.user')
+        if self.env.uid == self.user_id.id:
+            raise UserError(_('You can not send a badge to yourself'))
 
-        for wiz in self.browse(cr, uid, ids, context=context):
-            if not wiz.user_id:
-                raise UserError(_('You can send badges only to employees linked to a user.'))
+        values = {
+            'user_id': self.user_id.id,
+            'sender_id': self.env.uid,
+            'badge_id': self.badge_id.id,
+            'employee_id': self.employee_id.id,
+            'comment': self.comment,
+        }
 
-            if uid == wiz.user_id.id:
-                raise UserError(_('You can not send a badge to yourself'))
-
-            values = {
-                'user_id': wiz.user_id.id,
-                'sender_id': uid,
-                'badge_id': wiz.badge_id.id,
-                'employee_id': wiz.employee_id.id,
-                'comment': wiz.comment,
-            }
-
-            badge_user = badge_user_obj.create(cr, uid, values, context=context)
-            result = badge_user_obj._send_badge(cr, uid, [badge_user], context=context)
-        return result
+        return self.env['gamification.badge.user'].create(values)._send_badge()
