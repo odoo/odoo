9b3f8246e1cdbee87fd0f5efe7c33a16239a7d69
qsm-odoo <qsm@odoo.com>
2015-09-14 14:50:42 +0200

    [REF] account_asset: widgetonbutton widget (LESS/JS/XML)
    The widget was still using gtk icons
    
    * Delete useless template (use JS+LESS)
    * JS Refactoring

--- addons/account_asset/static/src/js/account_asset.js
+++ addons/account_asset/static/src/js/account_asset.js
@@ -7,34 +7,26 @@ Details : called in list view with "<button name="create_move" type="object" wid
 odoo.define('account_asset.widget', function(require) {
 "use strict";
 
-// openerp.account_asset = function (instance) {
-    // var _t = instance.web._t,
-    //     _lt = instance.web._lt;
-    // var QWeb = instance.web.qweb;
-    // instance.web.account_asset = instance.web.account_asset || {};
-
 var core = require('web.core');
 var QWeb = core.qweb;
 var session = require('web.session');
-var list_widget_registry = core.list_widget_registry;
-var Column = list_widget_registry.get('field');
 
-var WidgetOnButton = Column.extend({
-    format: function (row_data, options) {
-        this._super(row_data, options);
+var _t = core._t;
+
+var WidgetOnButton = core.list_widget_registry.get('field').extend({
+    format: function(row_data, options) {
+        this._super.apply(this, arguments);
         this.has_value = !!row_data.move_check.value;
         this.parent_state = row_data.parent_state.value;
-        this.icon = this.has_value ? 'gtk-yes' : 'gtk-no'; // or STOCK_YES and STOCK_NO
-        this.string = this.has_value ? 'Posted' : 'Unposted';
-        var template = this.icon && 'ListView.row.buttonwidget';
-        return QWeb.render(template, {
-            widget: this,
-            prefix: session.prefix,
-            disabled: this.has_value,
-            invisible : true ? this.parent_state !== 'open' : false
-        });
+
+        return $('<div/>').append((this.parent_state === 'open')? $('<button/>', {
+            type: 'button',
+            title: (this.has_value)? _t('Posted') : _t('Unposted'),
+            disabled: !!this.has_value,
+            'class': 'btn btn-sm btn-link fa fa-circle o_widgetonbutton' + ((this.has_value)? ' o_has_value' : ''),
+        }) : '').html();
     },
 });
 
-list_widget_registry.add("button.widgetonbutton", WidgetOnButton);
+core.list_widget_registry.add("button.widgetonbutton", WidgetOnButton);
 });
