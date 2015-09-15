03cd9ea5ee84d941e32a292dc0e979f8c58babbe
Hitesh Trivedi <htr@openerp.com>
2015-09-15 18:06:54 +0530

    [MIG] Gamification : Re-organize file structure

--- addons/gamification/models/__init__.py
+++ addons/gamification/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import goal
-import challenge
-import res_users
-import badge
+import gamification_badge
+import gamification_challenge
+import gamification_goal
