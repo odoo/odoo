b3e762956654c3f34431f6b677add9e2815e6715
Jérome Maes <jem@openerp.com>
2015-09-15 15:32:05 +0200

    [FIX] website_event_track : menu items generation
    When uncheck 'show tracks on website' or 'tracks proposal' the menu of event page (on website) was not modified.

--- addons/website_event/models/event.py
+++ addons/website_event/models/event.py
@@ -35,7 +35,7 @@ class event(models.Model):
 
     show_menu = fields.Boolean('Dedicated Menu', compute='_get_show_menu', inverse='_set_show_menu',
                                help="Creates menus Introduction, Location and Register on the page "
-                                    " of the event on the website.")
+                                    " of the event on the website.", store=True)
     menu_id = fields.Many2one('website.menu', 'Event Menu')
 
     @api.one
