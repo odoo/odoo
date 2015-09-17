32c3f9e2214c04e6196696a1d60cbbfeaa9c3802
ptr <ptr@openerp.com>
2015-09-17 17:05:00 +0530

    [MIG] stock: StockPickingType, BarcodeRule model.

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -122,8 +122,8 @@ class StockLocation(models.Model):
 
     @api.model
     def create(self, default):
-        if not default.get('barcode', False):
-            default.update({'barcode': default.get('complete_name', False)})
+        if not default.get('barcode'):
+            default.update({'barcode': default.get('complete_name')})
         return super(StockLocation, self).create(default)
 
     @api.model
@@ -156,13 +156,13 @@ class StockLocation(models.Model):
             location = location.location_id
         return self._default_removal_strategy()
 
-    @api.multi
-    def get_warehouse(self):
+    @api.model
+    def get_warehouse(self, location):
         """
             Returns warehouse id of warehouse that contains location
             :param location: browse record (stock.location)
         """
-        whs = self.env["stock.warehouse"].search([('view_location_id.parent_left', '<=', self.parent_left), ('view_location_id.parent_right', '>=', self.parent_left)])
+        whs = self.env["stock.warehouse"].search([('view_location_id.parent_left', '<=', location.parent_left), ('view_location_id.parent_right', '>=', location.parent_left)])
         return whs and whs.ids[0] or False
 
 #----------------------------------------------------------
@@ -4637,34 +4637,35 @@ class stock_warehouse_orderpoint(osv.osv):
             return {'value': v, 'domain': d}
         return {'domain': {'product_uom': []}}
 
-class stock_picking_type(osv.osv):
+from openerp import fields, models, api
+class StockPickingType(models.Model):
     _name = "stock.picking.type"
     _description = "The picking type determines the picking view"
     _order = 'sequence'
 
-    def open_barcode_interface(self, cr, uid, ids, context=None):
-        final_url = "/stock/barcode/#action=stock.ui&picking_type_id=" + str(ids[0]) if len(ids) else '0'
+    @api.multi
+    def open_barcode_interface(self):
+        final_url = "/stock/barcode/#action=stock.ui&picking_type_id=" + str(self.ids[0]) if len(self.ids) else '0'
         return {'type': 'ir.actions.act_url', 'url': final_url, 'target': 'self'}
 
-    def _get_tristate_values(self, cr, uid, ids, field_name, arg, context=None):
-        picking_obj = self.pool.get('stock.picking')
-        res = {}
-        for picking_type_id in ids:
+    @api.multi
+    def _get_tristate_values(self):
+        for picking_type_id in self:
             #get last 10 pickings of this type
-            picking_ids = picking_obj.search(cr, uid, [('picking_type_id', '=', picking_type_id), ('state', '=', 'done')], order='date_done desc', limit=10, context=context)
+            picking_ids = self.env['stock.picking'].search([('picking_type_id', '=', picking_type_id.id), ('state', '=', 'done')], order='date_done desc', limit=10)
             tristates = []
-            for picking in picking_obj.browse(cr, uid, picking_ids, context=context):
+            for picking in picking_ids:
                 if picking.date_done > picking.date:
                     tristates.insert(0, {'tooltip': picking.name or '' + ": " + _('Late'), 'value': -1})
                 elif picking.backorder_id:
                     tristates.insert(0, {'tooltip': picking.name or '' + ": " + _('Backorder exists'), 'value': 0})
                 else:
                     tristates.insert(0, {'tooltip': picking.name or '' + ": " + _('OK'), 'value': 1})
-            res[picking_type_id] = json.dumps(tristates)
-        return res
+            picking_type_id.last_done_picking = json.dumps(tristates)
 
-    def _get_picking_count(self, cr, uid, ids, field_names, arg, context=None):
-        obj = self.pool.get('stock.picking')
+    @api.multi
+    def _get_picking_count(self):
+        obj = self.env['stock.picking']
         domains = {
             'count_picking_draft': [('state', '=', 'draft')],
             'count_picking_waiting': [('state', 'in', ('confirmed', 'waiting'))],
@@ -4675,81 +4676,78 @@ class stock_picking_type(osv.osv):
         }
         result = {}
         for field in domains:
-            data = obj.read_group(cr, uid, domains[field] +
-                [('state', 'not in', ('done', 'cancel')), ('picking_type_id', 'in', ids)],
-                ['picking_type_id'], ['picking_type_id'], context=context)
+            data = obj.read_group(domains[field] +
+                [('state', 'not in', ('done', 'cancel')), ('picking_type_id', 'in', self.ids)],
+                ['picking_type_id'], ['picking_type_id'])
             count = dict(map(lambda x: (x['picking_type_id'] and x['picking_type_id'][0], x['picking_type_id_count']), data))
-            for tid in ids:
+            for tid in self.ids:
                 result.setdefault(tid, {})[field] = count.get(tid, 0)
-        for tid in ids:
-            if result[tid]['count_picking']:
-                result[tid]['rate_picking_late'] = result[tid]['count_picking_late'] * 100 / result[tid]['count_picking']
-                result[tid]['rate_picking_backorders'] = result[tid]['count_picking_backorders'] * 100 / result[tid]['count_picking']
+        for tid in self:
+            if result[tid.id]['count_picking']:
+                tid.rate_picking_late = result[tid.id]['count_picking_late'] * 100 / result[tid.id]['count_picking']
+                tid.rate_picking_backorders = result[tid.id]['count_picking_backorders'] * 100 / result[tid.id]['count_picking']
             else:
-                result[tid]['rate_picking_late'] = 0
-                result[tid]['rate_picking_backorders'] = 0
+                tid.rate_picking_late = 0
+                tid.rate_picking_backorders = 0
+            tid.count_picking_draft = result[tid.id]['count_picking_draft']
+            tid.count_picking_waiting = result[tid.id]['count_picking_waiting']
+            tid.count_picking_ready = result[tid.id]['count_picking_ready']
+            tid.count_picking = result[tid.id]['count_picking']
+            tid. count_picking_late = result[tid.id]['count_picking_late']
+            tid.count_picking_backorders = result[tid.id]['count_picking_backorders']
+
+    @api.multi
+    def _get_action(self, action):
+        result = self.env.ref(action, raise_if_not_found=True)
+        result = result.read()[0]
+        if self:
+            result.display_name = self.display_name
         return result
 
-    def _get_action(self, cr, uid, ids, action, context=None):
-        mod_obj = self.pool.get('ir.model.data')
-        act_obj = self.pool.get('ir.actions.act_window')
-        result = mod_obj.xmlid_to_res_id(cr, uid, action, raise_if_not_found=True)
-        result = act_obj.read(cr, uid, [result], context=context)[0]
-        if ids:
-            picking_type = self.browse(cr, uid, ids[0], context=context)
-            result['display_name'] = picking_type.display_name
-        return result
+    @api.multi
+    def get_action_picking_tree_late(self):
+        return self._get_action('stock.action_picking_tree_late')
 
-    def get_action_picking_tree_late(self, cr, uid, ids, context=None):
-        return self._get_action(cr, uid, ids, 'stock.action_picking_tree_late', context=context)
+    @api.multi
+    def get_action_picking_tree_backorder(self):
+        return self._get_action('stock.action_picking_tree_backorder')
 
-    def get_action_picking_tree_backorder(self, cr, uid, ids, context=None):
-        return self._get_action(cr, uid, ids, 'stock.action_picking_tree_backorder', context=context)
+    @api.multi
+    def get_action_picking_tree_waiting(self):
+        return self._get_action('stock.action_picking_tree_waiting')
 
-    def get_action_picking_tree_waiting(self, cr, uid, ids, context=None):
-        return self._get_action(cr, uid, ids, 'stock.action_picking_tree_waiting', context=context)
+    @api.multi
+    def get_action_picking_tree_ready(self):
+        return self._get_action('stock.action_picking_tree_ready')
 
-    def get_action_picking_tree_ready(self, cr, uid, ids, context=None):
-        return self._get_action(cr, uid, ids, 'stock.action_picking_tree_ready', context=context)
-
-    def get_stock_picking_action_picking_type(self, cr, uid, ids, context=None):
-        return self._get_action(cr, uid, ids, 'stock.stock_picking_action_picking_type', context=context)
-
-
-    def onchange_picking_code(self, cr, uid, ids, picking_code=False):
-        if not picking_code:
+    @api.multi
+    @api.onchange('code')
+    def onchange_picking_code(self):
+        if not self.code:
             return False
-        
-        obj_data = self.pool.get('ir.model.data')
-        stock_loc = obj_data.xmlid_to_res_id(cr, uid, 'stock.stock_location_stock')
-        
-        result = {
-            'default_location_src_id': stock_loc,
-            'default_location_dest_id': stock_loc,
-        }
-        if picking_code == 'incoming':
-            result['default_location_src_id'] = obj_data.xmlid_to_res_id(cr, uid, 'stock.stock_location_suppliers')
-        elif picking_code == 'outgoing':
-            result['default_location_dest_id'] = obj_data.xmlid_to_res_id(cr, uid, 'stock.stock_location_customers')
-        return {'value': result}
+        stock_loc = self.env.ref('stock.stock_location_stock').id
+        self.default_location_src_id = stock_loc
+        self.default_location_dest_id = stock_loc
+        if self.code == 'incoming':
+            self.default_location_src_id = self.env.ref('stock.stock_location_suppliers')
+        elif self.code == 'outgoing':
+            self.default_location_dest_id = self.env.ref('stock.stock_location_customers')
 
-    def _get_name(self, cr, uid, ids, field_names, arg, context=None):
-        return dict(self.name_get(cr, uid, ids, context=context))
+    @api.multi
+    def _get_name(self):
+        return dict(self.name_get())
 
-    def name_get(self, cr, uid, ids, context=None):
+    @api.multi
+    def name_get(self):
         """Overides orm name_get method to display 'Warehouse_name: PickingType_name' """
-        if context is None:
-            context = {}
-        if not isinstance(ids, list):
-            ids = [ids]
         res = []
-        if not ids:
+        if not self.ids:
             return res
-        for record in self.browse(cr, uid, ids, context=context):
+        for record in self:
             name = record.name
             if record.warehouse_id:
-                name = record.warehouse_id.name + ': ' +name
-            if context.get('special_shortened_wh_name'):
+                name = record.warehouse_id.name + ': ' + name
+            if self._context.get('special_shortened_wh_name'):
                 if record.warehouse_id:
                     name = record.warehouse_id.name
                 else:
@@ -4757,74 +4755,50 @@ class stock_picking_type(osv.osv):
             res.append((record.id, name))
         return res
 
-    def _default_warehouse(self, cr, uid, context=None):
-        user = self.pool.get('res.users').browse(cr, uid, uid, context)
-        res = self.pool.get('stock.warehouse').search(cr, uid, [('company_id', '=', user.company_id.id)], limit=1, context=context)
-        return res and res[0] or False
+    @api.model
+    def _default_warehouse(self):
+        res = self.env['stock.warehouse'].search([('company_id', '=', self.env.user.company_id.id)], limit=1)
+        return res and res.ids[0] or False
 
-    _columns = {
-        'name': fields.char('Picking Type Name', translate=True, required=True),
-        'complete_name': fields.function(_get_name, type='char', string='Name'),
-        'color': fields.integer('Color'),
-        'sequence': fields.integer('Sequence', help="Used to order the 'All Operations' kanban view"),
-        'sequence_id': fields.many2one('ir.sequence', 'Reference Sequence', required=True),
-        'default_location_src_id': fields.many2one('stock.location', 'Default Source Location', help="This is the default source location when you create a picking manually with this picking type. It is possible however to change it or that the routes put another location. If it is empty, it will check for the supplier location on the partner. "),
-        'default_location_dest_id': fields.many2one('stock.location', 'Default Destination Location', help="This is the default destination location when you create a picking manually with this picking type. It is possible however to change it or that the routes put another location. If it is empty, it will check for the customer location on the partner. "),
-        'code': fields.selection([('incoming', 'Suppliers'), ('outgoing', 'Customers'), ('internal', 'Internal')], 'Type of Operation', required=True),
-        'return_picking_type_id': fields.many2one('stock.picking.type', 'Picking Type for Returns'),
-        'show_entire_packs': fields.boolean('Allow moving packs', help="If checked, this shows the packs to be moved as a whole in the Operations tab all the time, even if there was no entire pack reserved."),
-        'warehouse_id': fields.many2one('stock.warehouse', 'Warehouse', ondelete='cascade'),
-        'active': fields.boolean('Active'),
-        'use_create_lots': fields.boolean('Create New Lots', help="If this is checked only, it will suppose you want to create new Serial Numbers / Lots, so you can provide them in a text field. "),
-        'use_existing_lots': fields.boolean('Use Existing Lots', help="If this is checked, you will be able to choose the Serial Number / Lots. You can also decide to not put lots in this picking type.  This means it will create stock with no lot or not put a restriction on the lot taken. "),
+    name = fields.Char('Picking Type Name', translate=True, required=True)
+    complete_name = fields.Char(compute="_get_name", string='Name')
+    color = fields.Integer()
+    sequence = fields.Integer(help="Used to order the 'All Operations' kanban view")
+    sequence_id = fields.Many2one('ir.sequence', 'Reference Sequence', required=True)
+    default_location_src_id = fields.Many2one('stock.location', 'Default Source Location', help="This is the default source location when you create a picking manually with this picking type. It is possible however to change it or that the routes put another location. If it is empty, it will check for the supplier location on the partner. ")
+    default_location_dest_id = fields.Many2one('stock.location', 'Default Destination Location', help="This is the default destination location when you create a picking manually with this picking type. It is possible however to change it or that the routes put another location. If it is empty, it will check for the customer location on the partner. ")
+    code = fields.Selection([('incoming', 'Suppliers'), ('outgoing', 'Customers'), ('internal', 'Internal')], 'Type of Operation', required=True)
+    return_picking_type_id = fields.Many2one('stock.picking.type', 'Picking Type for Returns')
+    show_entire_packs = fields.Boolean('Allow moving packs', help="If checked, this shows the packs to be moved as a whole in the Operations tab all the time, even if there was no entire pack reserved.")
+    warehouse_id = fields.Many2one('stock.warehouse', 'Warehouse', ondelete='cascade', default=_default_warehouse)
+    active = fields.Boolean(default=True)
+    use_create_lots = fields.Boolean('Create New Lots', help="If this is checked only, it will suppose you want to create new Serial Numbers / Lots, so you can provide them in a text field. ")
+    use_existing_lots = fields.Boolean('Use Existing Lots', help="If this is checked, you will be able to choose the Serial Number / Lots. You can also decide to not put lots in this picking type.  This means it will create stock with no lot or not put a restriction on the lot taken. ")
+    # Statistics for the kanban view
+    last_done_picking = fields.Char(compute="_get_tristate_values", string='Last 10 Done Pickings')
+    count_picking_draft = fields.Integer(compute="_get_picking_count", multi='_get_picking_count')
+    count_picking_ready = fields.Integer(compute="_get_picking_count", multi='_get_picking_count')
+    count_picking = fields.Integer(compute="_get_picking_count", multi='_get_picking_count')
+    count_picking_waiting = fields.Integer(compute="_get_picking_count", multi='_get_picking_count')
+    count_picking_late = fields.Integer(compute="_get_picking_count", multi='_get_picking_count')
+    count_picking_backorders = fields.Integer(compute="_get_picking_count", multi='_get_picking_count')
+    rate_picking_late = fields.Integer(compute="_get_picking_count", multi='_get_picking_count')
+    rate_picking_backorders = fields.Integer(compute="_get_picking_count", multi='_get_picking_count')
+    # Barcode nomenclature
+    barcode_nomenclature_id = fields.Many2one('barcode.nomenclature', 'Barcode Nomenclature', help='A barcode nomenclature')
 
-        # Statistics for the kanban view
-        'last_done_picking': fields.function(_get_tristate_values,
-            type='char',
-            string='Last 10 Done Pickings'),
-
-        'count_picking_draft': fields.function(_get_picking_count,
-            type='integer', multi='_get_picking_count'),
-        'count_picking_ready': fields.function(_get_picking_count,
-            type='integer', multi='_get_picking_count'),
-        'count_picking': fields.function(_get_picking_count,
-            type='integer', multi='_get_picking_count'),
-        'count_picking_waiting': fields.function(_get_picking_count,
-            type='integer', multi='_get_picking_count'),
-        'count_picking_late': fields.function(_get_picking_count,
-            type='integer', multi='_get_picking_count'),
-        'count_picking_backorders': fields.function(_get_picking_count,
-            type='integer', multi='_get_picking_count'),
-
-        'rate_picking_late': fields.function(_get_picking_count,
-            type='integer', multi='_get_picking_count'),
-        'rate_picking_backorders': fields.function(_get_picking_count,
-            type='integer', multi='_get_picking_count'),
-
-        # Barcode nomenclature
-        'barcode_nomenclature_id':  fields.many2one('barcode.nomenclature','Barcode Nomenclature', help='A barcode nomenclature'),
-    }
-
-    _defaults = {
-        'warehouse_id': _default_warehouse,
-        'active': True,
-        'use_existing_lots': True,
-        'use_create_lots': True,
-    }
-
-class barcode_rule(models.Model):
+class BarcodeRule(models.Model):
     _inherit = 'barcode.rule'
 
     def _get_type_selection(self):
-        types = sets.Set(super(barcode_rule,self)._get_type_selection()) 
+        types = sets.Set(super(BarcodeRule, self)._get_type_selection())
         types.update([
-            ('weight','Weighted Product'),
-            ('location','Location'),
-            ('lot','Lot'),
-            ('package','Package')
+            ('weight', 'Weighted Product'),
+            ('location', 'Location'),
+            ('lot', 'Lot'),
+            ('package', 'Package')
         ])
         return list(types)
-
 
 class StockPackOperation(models.Model):
     _inherit = 'stock.pack.operation'
