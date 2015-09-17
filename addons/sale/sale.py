2850fcf992fe2035e93313de7a948808c9649318
Christophe Simonis <chs@odoo.com>
2015-09-17 13:02:28 +0200

    [FIX] sale: correct implementation of `_amount_all`
    `@api.multi` methods should iterate over all records in self.

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -22,15 +22,16 @@ class SaleOrder(models.Model):
 
     @api.depends('order_line.price_total')
     def _amount_all(self):
-        amount_untaxed = amount_tax = 0.0
-        for line in self.order_line:
-            amount_untaxed += line.price_subtotal
-            amount_tax += line.price_tax
-        self.update({
-            'amount_untaxed': self.pricelist_id.currency_id.round(amount_untaxed),
-            'amount_tax': self.pricelist_id.currency_id.round(amount_tax),
-            'amount_total': amount_untaxed + amount_tax,
-        })
+        for order in self:
+            amount_untaxed = amount_tax = 0.0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal
+                amount_tax += line.price_tax
+            order.update({
+                'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
+                'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
+                'amount_total': amount_untaxed + amount_tax,
+            })
 
     @api.depends('state', 'order_line.invoice_status')
     def _get_invoiced(self):
