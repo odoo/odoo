f4b44395a0aa1e70028b8b8676d2c0636b4299c2
Nicolas Martinelli <nim@odoo.com>
2015-09-17 14:19:59 +0200

    [FIX] purchase: correct implementation of `_amount_all`
    `@api.multi` methods should iterate over all records in self.

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -18,15 +18,16 @@ class PurchaseOrder(models.Model):
 
     @api.depends('order_line.price_total')
     def _amount_all(self):
-        amount_untaxed = amount_tax = 0.0
-        for line in self.order_line:
-            amount_untaxed += line.price_subtotal
-            amount_tax += line.price_tax
-        self.update({
-            'amount_untaxed': self.currency_id.round(amount_untaxed),
-            'amount_tax': self.currency_id.round(amount_tax),
-            'amount_total': amount_untaxed + amount_tax,
-        })
+        for order in self:
+            amount_untaxed = amount_tax = 0.0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal
+                amount_tax += line.price_tax
+            order.update({
+                'amount_untaxed': order.currency_id.round(amount_untaxed),
+                'amount_tax': order.currency_id.round(amount_tax),
+                'amount_total': amount_untaxed + amount_tax,
+            })
 
     @api.multi
     def _inverse_date_planned(self):
