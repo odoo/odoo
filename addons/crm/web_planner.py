b15775c73127b8a765f054019f3adb7d2506c746
Martin Trigaux <mat@odoo.com>
2015-09-22 16:29:40 +0200

    [FIX] crm: fault tolerant code
    If the sales teams 'Direct Sales' is deleted, it was not possible to load the planner.
    Do not crash if no record is found with that ID
    Fixes #8624

--- addons/crm/web_planner.py
+++ addons/crm/web_planner.py
@@ -14,11 +14,11 @@ class PlannerCrm(models.Model):
 
     @api.model
     def _prepare_planner_crm_data(self):
+        menu = self.env.ref('crm.menu_crm_opportunities', raise_if_not_found=False)
         # sudo is needed to avoid error message when current user's company != sale_department company
-        sales_team = self.sudo().env.ref('sales_team.team_sales_department')
-        values = {
-            'alias_domain': sales_team.alias_domain,
-            'alias_name': sales_team.alias_name,
-            'pipeline_menu_id': self.env.ref('crm.menu_crm_opportunities').id,
+        sales_team = self.sudo().env.ref('sales_team.team_sales_department', raise_if_not_found=False)
+        return {
+            'alias_domain': sales_team and sales_team.alias_domain or False,
+            'alias_name': sales_team and sales_team.alias_name or False,
+            'pipeline_menu_id': menu and menu.id or False
         }
-        return values
