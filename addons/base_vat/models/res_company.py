80f2f2af3306f034ec58915a29bb032367972948
Shivam Dudhat <sdu@odoo.com>
2015-09-23 16:00:34 +0530

    [MIG] base_vat: new API

--- addons/base_vat/models/res_company.py
+++ addons/base_vat/models/res_company.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
+from odoo import fields, models
 
-class res_company_vat (osv.osv):
+
+class ResCompany(models.Model):
     _inherit = 'res.company'
-    _columns = {
-        'vat_check_vies': fields.boolean('VIES VAT Check',
-                                         help="If checked, Partners VAT numbers will be fully validated against EU's VIES service "
-                                              "rather than via a simple format validation (checksum)."),
-    }
 
-    
+    vat_check_vies = fields.Boolean(
+        string='VIES VAT Check',
+        help="If checked, Partners VAT numbers will be fully validated against EU's VIES service "
+             "rather than via a simple format validation (checksum).")
