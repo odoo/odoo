149b7709fa2692181013070d84e2751438da3e10
Darshan Kalola <dka@openerp.com>
2015-09-24 14:43:25 +0530

    [MOVE]point_of_sale: re-structured module according to giudeline.

---
+++ addons/point_of_sale/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import account_bank_statement
+import point_of_sale
+import res_partner
+import res_config
+import res_users
