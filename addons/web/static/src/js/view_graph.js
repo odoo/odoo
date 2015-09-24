8cb1889baf5737458b1d977c948a9e3f8134c4fd
Nicolas Lempereur <nle@odoo.com>
2015-09-24 14:15:13 +0200

    [FIX] web: propagate context for graph view fields
    The fields loaded for the graph view used for measure and legend were
    not translated.
    
    opw-649918

--- addons/web/static/src/js/view_graph.js
+++ addons/web/static/src/js/view_graph.js
@@ -29,7 +29,7 @@ instance.web.GraphView = instance.web.View.extend({
         this.widget = undefined;
     },
     start: function () {
-        var load_fields = this.model.call('fields_get', [])
+        var load_fields = this.model.call('fields_get', [], {context: this.dataset.get_context()})
                 .then(this.prepare_fields.bind(this));
 
         return $.when(this._super(), load_fields).then(this.render_buttons.bind(this));
