2a5f07e0b919d4209c2cb72c6dfdc8b86cd5c7ff
Géry Debongnie <ged@odoo.com>
2015-09-30 14:14:30 +0200

    [REF] mail: last part of the chat refactoring (client side)
    Removal of old files and update of assets and imports
    
    Joint work with aab

--- addons/mail/__openerp__.py
+++ addons/mail/__openerp__.py
@@ -58,11 +58,12 @@ Main Features
     'installable': True,
     'application': True,
     'qweb': [
-        'static/src/xml/mail_thread.xml',
-        'static/src/xml/mail_chatter.xml',
-        'static/src/xml/mail_chat_common.xml',
-        'static/src/xml/mail_chat_backend.xml',
-        'static/src/xml/mail_followers.xml',
+        'static/src/xml/client_action.xml',
+        'static/src/xml/composer.xml',
+        'static/src/xml/chatter.xml',
+        'static/src/xml/systray.xml',
+        'static/src/xml/thread.xml',
+        'static/src/xml/chat_window.xml',
         'static/src/xml/announcement.xml',
     ],
 }
