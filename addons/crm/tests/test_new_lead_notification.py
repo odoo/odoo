17e562ecef6fd7833af39889a4d3e477c7c71eb2
Thibault Delavallée <tde@openerp.com>
2015-10-01 16:00:31 +0200

    [FIX][TESTS] mail: auto subscription, needaction
    - fixed computation of needaction flag (typo)
    - fixed order of auto subscription and message tracking: first subscribe then
      track. This way messages are send to newly added followers.
    - added tests in CRM to ensure this behavior works by updating the already
      existing test and using the mail test class

--- addons/crm/tests/test_new_lead_notification.py
+++ addons/crm/tests/test_new_lead_notification.py
@@ -1,43 +1,37 @@
 # -*- coding: utf-8 -*-
-import unittest
-from openerp.tests.common import TransactionCase
 
-class NewLeadNotificationTest(TransactionCase):
-    @unittest.skip("Broken in 9.0. auto_subscribe does not handle channels")
+from .common import TestCrm
+
+
+class NewLeadNotification(TestCrm):
+
     def test_new_lead_notification(self):
-        # Create a new user
-        groups = [
-            self.env.ref('base.group_user').id,
-            self.env.ref('base.group_sale_salesman').id,
-        ]
-        user = self.env["res.users"].create({
-            "name": __file__,
-            "login": __file__,
-            'groups_id': [(6, 0, groups)],
-        })
-
-        chan = self.env['mail.channel'].create({
-            'name': 'Follow leads',
-            'group_ids': [(6, 0, groups)],
-        })
-
-        # Subscribe him to sales department
-        team = self.env.ref("sales_team.team_sales_department")
+        """ Test newly create leads like from the website. People and channels
+        subscribed to the sales team shoud be notified. """
+        # subscribe a partner and a channel to the sales team with new lead subtype
         subtype = self.env.ref("crm.mt_salesteam_lead")
-        team.message_subscribe(channel_ids=[chan.id], subtype_ids=[subtype.id])
+        self.sales_team_1.message_subscribe(partner_ids=[self.user_salesman_all.partner_id.id], subtype_ids=[subtype.id])
+        self.sales_team_1.message_subscribe(channel_ids=[self.group_pigs.id], subtype_ids=[subtype.id])
 
         # Imitate what happens in the controller when somebody creates a new
         # lead from the website form
-        lead = (self.env["crm.lead"]
-                .with_context(mail_create_nosubscribe=True)
-                .sudo()
-                .create({"contact_name": "Somebody",
-                         "description": "Some question",
-                         "email_from": "somemail@example.com",
-                         "name": "Some subject",
-                         "partner_name": "Some company",
-                         "team_id": team.id,
-                         "phone": "+0000000000"}))
+        lead = self.env["crm.lead"].with_context(mail_create_nosubscribe=True).sudo().create({
+            "contact_name": "Somebody",
+            "description": "Some question",
+            "email_from": "somemail@example.com",
+            "name": "Some subject",
+            "partner_name": "Some company",
+            "team_id": self.sales_team_1.id,
+            "phone": "+0000000000"
+        })
+        # partner and channel should be auto subscribed
+        self.assertIn(self.user_salesman_all.partner_id, lead.message_partner_ids)
+        self.assertIn(self.group_pigs, lead.message_channel_ids)
+
+        msg = lead.message_ids[0]
+        self.assertIn(self.user_salesman_all.partner_id, msg.needaction_partner_ids)
+        self.assertIn(self.group_pigs, msg.channel_ids)
 
         # The user should have a new unread message
-        self.assertTrue(lead.sudo(user).message_unread)
+        lead_user = lead.sudo(self.user_salesman_all)
+        self.assertTrue(lead_user.message_needaction)
