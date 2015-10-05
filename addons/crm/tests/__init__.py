65646ec03ae2a1bff239f695f80e03c39be0ebd7
Ajay Patel <apa@openerp.com>
2015-10-05 19:03:24 +0530

    [IMP] crm: Convert yml test case into python test case.

--- addons/crm/tests/__init__.py
+++ addons/crm/tests/__init__.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import test_new_lead_notification
+import test_crm_access_group_users
+import test_crm_lead_find_stage
+import test_crm_lead_merge
+import test_crm_lead_message
+import test_lead2opportunity2win
+import test_lead2opportunity_assign_salesmen
