365ecece73be74a1cd1b8e92282238bcd31a867e
qdp-odoo <qdp@odoo.com>
2015-10-06 09:27:20 +0200

    [FIX] purchase: wrong valuation price on stock moves when a tax included in price was used (product were valuated with price included too)

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -474,6 +474,8 @@ class PurchaseOrderLine(models.Model):
         for line in self:
             order = line.order_id
             price_unit = line.price_unit
+            if line.taxes_id:
+                price_unit = line.taxes_id.compute_all(price_unit, currency=line.order_id.currency_id, quantity=line.product_qty)['total_excluded']
             if line.product_uom.id != line.product_id.uom_id.id:
                 price_unit *= line.product_uom.factor / line.product_id.uom_id.factor
             if order.currency_id != order.company_id.currency_id:
