77250b5b9a91c3e916d8d450730225a77d168df1
Ludovic Laffineur lla <lla@odoo.com>
2015-10-14 11:26:19 +0200

    [REM] website_partner: removing website_private unused

--- addons/website_partner/models/res_partner.py
+++ addons/website_partner/models/res_partner.py
@@ -8,8 +8,6 @@ class WebsiteResPartner(models.Model):
     _name = 'res.partner'
     _inherit = ['res.partner', 'website.seo.metadata', 'website.published.mixin']
 
-    website_private = fields.Boolean(string='Private Profile', compute='_compute_private',
-                                            inverse='_set_private', search='_search_private')
     website_description = fields.Html('Website Partner Full Description', strip_style=True)
     website_short_description = fields.Text('Website Partner Short Description')
     # hack to allow using plain browse record in qweb views
@@ -18,18 +16,6 @@ class WebsiteResPartner(models.Model):
     @api.one
     def _compute_get_ids(self):
         self.self = self.id
-
-    @api.multi
-    def _compute_private(self):
-        for partner in self:
-            partner.website_private = not partner.website_published
-
-    def _set_private(self):
-        for partner in self:
-            partner.website_published = not partner.website_private
-
-    def _search_private(self, operator, value):
-        return [('website_published', '=', not value)]
 
     @api.multi
     def _website_url(self, field_name, arg):
