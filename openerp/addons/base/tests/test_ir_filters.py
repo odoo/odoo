cbb902ac302dfb28c15355851f8a9fb2586c5b82
Xavier Morel <xmo@odoo.com>
2015-10-19 16:49:35 +0200

    [FIX] ir.filters tests
    get_filters now returns the serialization field, tests check for exact
    match on what get_filters return, so test assertions need the
    serialization stuff as well.

--- openerp/addons/base/tests/test_ir_filters.py
+++ openerp/addons/base/tests/test_ir_filters.py
@@ -34,10 +34,10 @@ class TestGetFilters(FiltersCase):
             self.cr, self.USER_ID, 'ir.filters')
 
         self.assertItemsEqual(map(noid, filters), [
-            dict(name='a', is_default=False, user_id=self.USER, domain='[]', context='{}', sort='[]'),
-            dict(name='b', is_default=False, user_id=self.USER, domain='[]', context='{}', sort='[]'),
-            dict(name='c', is_default=False, user_id=self.USER, domain='[]', context='{}', sort='[]'),
-            dict(name='d', is_default=False, user_id=self.USER, domain='[]', context='{}', sort='[]'),
+            dict(name='a', is_default=False, user_id=self.USER, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='b', is_default=False, user_id=self.USER, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='c', is_default=False, user_id=self.USER, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='d', is_default=False, user_id=self.USER, domain='[]', context='{}', sort='[]', serialization=False),
         ])
 
     def test_global_filters(self):
@@ -53,10 +53,10 @@ class TestGetFilters(FiltersCase):
             self.cr, self.USER_ID, 'ir.filters')
 
         self.assertItemsEqual(map(noid, filters), [
-            dict(name='a', is_default=False, user_id=False, domain='[]', context='{}', sort='[]'),
-            dict(name='b', is_default=False, user_id=False, domain='[]', context='{}', sort='[]'),
-            dict(name='c', is_default=False, user_id=False, domain='[]', context='{}', sort='[]'),
-            dict(name='d', is_default=False, user_id=False, domain='[]', context='{}', sort='[]'),
+            dict(name='a', is_default=False, user_id=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='b', is_default=False, user_id=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='c', is_default=False, user_id=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='d', is_default=False, user_id=False, domain='[]', context='{}', sort='[]', serialization=False),
         ])
 
     def test_no_third_party_filters(self):
@@ -71,8 +71,8 @@ class TestGetFilters(FiltersCase):
             self.cr, self.USER_ID, 'ir.filters')
 
         self.assertItemsEqual(map(noid, filters), [
-            dict(name='a', is_default=False, user_id=False, domain='[]', context='{}', sort='[]'),
-            dict(name='c', is_default=False, user_id=self.USER, domain='[]', context='{}', sort='[]'),
+            dict(name='a', is_default=False, user_id=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='c', is_default=False, user_id=self.USER, domain='[]', context='{}', sort='[]', serialization=False),
         ])
 
 class TestOwnDefaults(FiltersCase):
@@ -97,7 +97,7 @@ class TestOwnDefaults(FiltersCase):
 
         self.assertItemsEqual(map(noid, filters), [
             dict(name='a', user_id=self.USER, is_default=True,
-                 domain='[]', context='{}', sort='[]')
+                 domain='[]', context='{}', sort='[]', serialization=False)
         ])
 
     def test_new_filter_not_default(self):
@@ -121,9 +121,9 @@ class TestOwnDefaults(FiltersCase):
         filters = Filters.get_filters(self.cr, self.USER_ID, 'ir.filters')
 
         self.assertItemsEqual(map(noid, filters), [
-            dict(name='a', user_id=self.USER, is_default=False, domain='[]', context='{}', sort='[]'),
-            dict(name='b', user_id=self.USER, is_default=False, domain='[]', context='{}', sort='[]'),
-            dict(name='c', user_id=self.USER, is_default=True, domain='[]', context='{}', sort='[]'),
+            dict(name='a', user_id=self.USER, is_default=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='b', user_id=self.USER, is_default=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='c', user_id=self.USER, is_default=True, domain='[]', context='{}', sort='[]', serialization=False),
         ])
 
     def test_new_filter_existing_default(self):
@@ -147,9 +147,9 @@ class TestOwnDefaults(FiltersCase):
         filters = Filters.get_filters(self.cr, self.USER_ID, 'ir.filters')
 
         self.assertItemsEqual(map(noid, filters), [
-            dict(name='a', user_id=self.USER, is_default=False, domain='[]', context='{}', sort='[]'),
-            dict(name='b', user_id=self.USER, is_default=False, domain='[]', context='{}', sort='[]'),
-            dict(name='c', user_id=self.USER, is_default=True, domain='[]', context='{}', sort='[]'),
+            dict(name='a', user_id=self.USER, is_default=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='b', user_id=self.USER, is_default=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='c', user_id=self.USER, is_default=True, domain='[]', context='{}', sort='[]', serialization=False),
         ])
 
     def test_update_filter_set_default(self):
@@ -173,8 +173,8 @@ class TestOwnDefaults(FiltersCase):
         filters = Filters.get_filters(self.cr, self.USER_ID, 'ir.filters')
 
         self.assertItemsEqual(map(noid, filters), [
-            dict(name='a', user_id=self.USER, is_default=True, domain='[]', context='{}', sort='[]'),
-            dict(name='b', user_id=self.USER, is_default=False, domain='[]', context='{}', sort='[]'),
+            dict(name='a', user_id=self.USER, is_default=True, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='b', user_id=self.USER, is_default=False, domain='[]', context='{}', sort='[]', serialization=False),
         ])
 
 class TestGlobalDefaults(FiltersCase):
@@ -204,9 +204,9 @@ class TestGlobalDefaults(FiltersCase):
         filters = Filters.get_filters(self.cr, self.USER_ID, 'ir.filters')
 
         self.assertItemsEqual(map(noid, filters), [
-            dict(name='a', user_id=False, is_default=False, domain='[]', context='{}', sort='[]'),
-            dict(name='b', user_id=False, is_default=False, domain='[]', context='{}', sort='[]'),
-            dict(name='c', user_id=False, is_default=True, domain='[]', context='{}', sort='[]'),
+            dict(name='a', user_id=False, is_default=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='b', user_id=False, is_default=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='c', user_id=False, is_default=True, domain='[]', context='{}', sort='[]', serialization=False),
         ])
 
     def test_new_filter_existing_default(self):
@@ -272,6 +272,6 @@ class TestGlobalDefaults(FiltersCase):
         filters = Filters.get_filters(self.cr, self.USER_ID, 'ir.filters')
 
         self.assertItemsEqual(map(noid, filters), [
-            dict(name='a', user_id=False, is_default=False, domain='[]', context='{}', sort='[]'),
-            dict(name='b', user_id=False, is_default=True, domain='[]', context=context_value, sort='[]'),
+            dict(name='a', user_id=False, is_default=False, domain='[]', context='{}', sort='[]', serialization=False),
+            dict(name='b', user_id=False, is_default=True, domain='[]', context=context_value, sort='[]', serialization=False),
         ])
