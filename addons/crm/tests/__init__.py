e4aca67d9ede2e8a5653272a2a78fec088b79e30
fka-odoo <fka@openerp.com>
2015-11-17 19:13:03 +0530

    [ADD] crm: Added python test case for crm activity process

--- addons/crm/tests/__init__.py
+++ addons/crm/tests/__init__.py
@@ -3,6 +3,7 @@
 
 import test_new_lead_notification
 import test_crm_access_group_users
+import test_crm_lead_activity
 import test_crm_lead_find_stage
 import test_crm_lead_merge
 import test_crm_lead_message
