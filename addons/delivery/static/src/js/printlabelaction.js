f624b554fe32d55e7b903ebd528b3ae3814e1125
Mohammed Shekha <msh@openerp.com>
2015-11-17 19:59:41 +0530

    [IMP]web: close wizard when action kind of url is returned
    [FIX]delivery: Fixed the issue of reload current view when ir.actions.act_url is opened in blank window, call close callback when button is clicked and it returns act_url action, previously it was not handled

--- addons/delivery/static/src/js/printlabelaction.js
+++ addons/delivery/static/src/js/printlabelaction.js
@@ -4,26 +4,24 @@ var ActionManager = require('web.ActionManager');
 var framework = require('web.framework');
 
 ActionManager.include({
-
-    ir_actions_act_url: function (action) {
-    	var self = this;
-    
-    	if ('url' in action && Array.isArray(action.url)){
-	        if (action.target === 'self') {
-	            framework.redirect(action.url);
-	        } else {
-	            this.dialog_stop();
-	            _.each(action.url, function(url){ 
-					window.open(url, '_blank');
-				});
-	        }
-	        return $.when();
-	     }
-	    else {
-	        return self._super(action);
-    	}
-    },	
-
+    ir_actions_act_url: function (action, options) {
+        if ('url' in action && _.isArray(action.url)){
+            if (action.target === 'self') {
+                framework.redirect(action.url);
+            } else {
+                if (!this.dialog) {
+                    options.on_close();
+                }
+                this.dialog_stop();
+                _.each(action.url, function(url){ 
+                    window.open(url, '_blank');
+                });
+            }
+            return $.when();
+        } else {
+            return this._super(action);
+        }
+    },
 });
 
 });
