71f1577f21fff5581408821617ce18890b55850b
Thibault Delavallée <tde@openerp.com>
2015-11-18 18:11:52 +0100

    [FIX] mail, mail_channel: recipients of channels
    Mailing lists (mail.channel) should not send specific notification emails.
    Indeed there can be a lot of recipients and customizing each email can
    take time to compute. This leads to posting a message on a mail.channel
    being very slow.
    
    It is now possible for a model to customize the notification email
    recipients computation. The first use is to ensure that mail.channel
    encodes recipients using email_to instead of recipients_ids. This way
    less processing is performed on notification emails.
    
    This is a manual cherry-pick of commit 19d330e33e368d7773a3092d5451e6ee85531055
    that was not forard ported. Mail has indeed evolved since 8.0 and its
    forward port was not an easy task.
    
    This commit also includes the fix of mail.channel recipients. All are now
    considered as equal and receive a simple notification email. No distinction
    is done between partners and users. The notification emails for channels
    should be as simple as possible.

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -731,6 +731,17 @@ class MailThread(models.AbstractModel):
         res = dict()
         return res
 
+    @api.multi
+    def message_get_recipient_values(self, notif_message=None, recipient_ids=None):
+        """ Get specific notification recipient values to store on the notification
+        mail_mail. Basic method just set the recipient partners as mail_mail
+        recipients. Inherit this method to add custom behavior like using
+        recipient email_to to bypass the recipint_ids heuristics in the
+        mail sending mechanism. """
+        return {
+            'recipient_ids': [(4, pid) for pid in recipient_ids]
+        }
+
     # ------------------------------------------------------
     # Mail gateway
     # ------------------------------------------------------
