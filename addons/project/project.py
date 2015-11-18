e905b8d8f3098b54e75c722dad1f8ff17119c20f
sergio-incaser <sergio@incaser.es>
2015-11-18 11:59:19 +0100

    [IMP] project: Changes suggested by @mart-e.

--- addons/project/project.py
+++ addons/project/project.py
@@ -688,8 +688,7 @@ class task(osv.osv):
 
     def onchange_date_deadline(
             self, cr, uid, ids, date_end, date_deadline, context=None):
-        if not date_end or (date_end[:10] == self.browse(
-                cr, uid, ids, context=context).date_deadline):
+        if not date_end or (date_end[:10] == self.browse(cr, uid, ids, context=context).date_deadline):
             return {'value': {'date_end': date_deadline}}
 
     def duplicate_task(self, cr, uid, map_ids, context=None):
