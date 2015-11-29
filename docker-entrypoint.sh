354652271a5525fbef791fbab1611e0d297c5ab8
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-11-29 14:03:27 -0200

    prepara para maquina em producao

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 set -e
+
+
+echo "[options]" >> server.conf
 echo "admin_passwd = $ADMIN_PASSWORD" >> server.conf
+echo "xmlrpc_port = 8069" >> server.conf
+echo "$AWS_S3_KEY:$AWS_S3_SECRET" >> ~/.passwd-s3fs
+chmod 600 ~/.passwd-s3fs
+
 case "$1" in
 	--)
 		shift
-		exec openerp-server -c server.conf "$@"
+		mkdir ~/.local
+		s3fs -o use_cache=/tmp/s3  $AWS_S3_BUCKET ~/.local 
+		python2.7 /home/odoo/odoo/openerp-server -c server.conf "$@"
 		;;
 	-*)
-		exec openerp-server -c server.conf "$@"
+		mkdir ~/.local
+		s3fs -o use_cache=/tmp/s3 $AWS_S3_BUCKET ~/.local 
+		python2.7 /home/odoo/odoo/openerp-server -c server.conf "$@"
 		;;
 	*)
 		exec "$@"
