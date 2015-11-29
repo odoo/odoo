68c1484d5faef616483197e1d8bb6bc2344a5702
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-11-29 16:23:32 -0200

    change test environment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,7 @@ set -e
 echo "[options]" >> server.conf
 echo "admin_passwd = $ADMIN_PASSWORD" >> server.conf
 echo "xmlrpc_port = 8069" >> server.conf
+
 echo "$AWS_S3_KEY:$AWS_S3_SECRET" >> ~/.passwd-s3fs
 chmod 600 ~/.passwd-s3fs
 
@@ -19,10 +20,10 @@ case "$1" in
 		python2.7 /home/odoo/odoo/openerp-server -c server.conf "$@"
 		;;
 	-*)
+		shift
 		if [ ! -d "/home/odoo/.local" ]; then
 			mkdir ~/.local
 		fi
-		s3fs -o use_cache=/tmp/s3 $AWS_S3_BUCKET ~/.local 
 		python2.7 /home/odoo/odoo/openerp-server -c server.conf "$@"
 		;;
 	*)
