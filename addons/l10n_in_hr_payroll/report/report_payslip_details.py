119a79c866cf322ef39247fe00d441a664964c7d
pgu-odoo <pgu@odoo.com>
2015-11-30 12:40:00 +0530

    [IMP] l10n_in_hr_payroll : Converted code of report to new api to resolve the conflicts with hr_payroll

--- addons/l10n_in_hr_payroll/report/report_payslip_details.py
+++ addons/l10n_in_hr_payroll/report/report_payslip_details.py
@@ -1,20 +1,27 @@
 #-*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.report import report_sxw
-from openerp.osv import osv
-from openerp.addons.hr_payroll import report
+from odoo import api
+from odoo.addons.hr_payroll import report
+
+#while converting hr_payroll module into new api following error was occurring
+#   File "/home/odoo/runbot/extra/runbot/static/build/115894-9122-87dc2e/openerp/models.py", line 643, in _build_model
+#    model.__init__(pool, cr)
+#    TypeError: __init__() takes exactly 5 arguments (3 given)
+#to resolve this, converting the code into new api.
+
 
 class payslip_details_report_in(report.payslip_details_report.PayslipDetailsReport):
-
-    def __init__(self, cr, uid, name, context):
-        super(payslip_details_report_in, self).__init__(cr, uid, name, context)
-        self.localcontext.update({
-            'get_details_by_rule_category': self.get_details_by_rule_category,
-        })
-
-class wrapped_report_payslipdetailsin(osv.AbstractModel):
     _name = 'report.l10n_in_hr_payroll.report_payslipdetails'
-    _inherit = 'report.abstract_report'
-    _template = 'l10n_in_hr_payroll.report_payslipdetails'
-    _wrapped_report_class = payslip_details_report_in
+
+    @api.multi
+    def render_html(self, data=None):
+        records = self.env['hr.payslip'].browse(self.env.context.get('active_ids'))
+        docargs = {
+            'doc_ids': self.env.context.get('active_ids'),
+            'doc_model': report.model,
+            'docs': records,
+            'data': data,
+            'get_details_by_rule_category': self.get_details_by_rule_category,
+        }
+        return self.env['report'].render('l10n_in_hr_payroll.report_payslipdetails', docargs)
