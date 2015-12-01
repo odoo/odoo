727a70072092eb409690b1d9eb5f7eb1b7f300af
pvy <pvy@odoo.com>
2015-12-01 10:43:21 +0100

    [ADD] account: added the trust field to partners for followups

--- addons/account/models/partner.py
+++ addons/account/models/partner.py
@@ -441,6 +441,7 @@ class ResPartner(models.Model):
     invoice_ids = fields.One2many('account.invoice', 'partner_id', string='Invoices', readonly=True, copy=False)
     contract_ids = fields.One2many('account.analytic.account', 'partner_id', string='Contracts', readonly=True)
     bank_account_count = fields.Integer(compute='_compute_bank_count', string="Bank")
+    trust = fields.Selection([('good', 'Good Debtor'), ('normal', 'Normal Debtor'), ('bad', 'Bad Debtor')], string='Degree of trust you have in this debtor', default='normal', company_dependent=True)
 
     @api.multi
     def _compute_bank_count(self):
