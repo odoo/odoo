odoo/odoo

commit 35f3dea5d2641f88cbf61a3540851d2788db0055

[FIX] stock_dropshipping: do not raise a warning for dropshipping product
When selling a product with a dropshipping route, the available stock was
checked and a warning got raise if it was the case.
The _check_routing was not working properly as is called in a context of an
onchange and 'self' was the record in the database, not in the browser.

Just use check there is a dropshipping route on the product instead of checking
it on the routes of the sale order line (not reliable info anyway).

Add a todo for master to improve it to change the signature of the method.

opw 657760

addons/stock_dropshipping/stock_dropshipping.py
@@ -7,6 +7,7 @@ from openerp.exceptions import UserError
 class sale_order_line(models.Model):
     _inherit = 'sale.order.line'
 
+    # TODO @api.model as called from onchange
     @api.multi
     def _check_routing(self, product, warehouse):
         """ skip stock verification if the route goes from supplier to customer
@@ -14,12 +15,11 @@ class sale_order_line(models.Model):
         """
         res = super(sale_order_line, self)._check_routing(product, warehouse)
         if not res:
-            for line in self:
-                for pull_rule in line.route_id.pull_ids:
+            for route in product.route_ids:
+                for pull_rule in route.pull_ids:
                     if (pull_rule.picking_type_id.default_location_src_id.usage == 'supplier' and
                             pull_rule.picking_type_id.default_location_dest_id.usage == 'customer'):
-                        res = True
-                        break
+                        return True
         return res
 
 

commit 16e4cd15f0885de0148f46098173ea6c0a372401

[FIX] stock_dropshipping: multicompany filter
The multicompany rule on procurement.rule may not be working as the field
company_id is not displayed with no default value (so probably often empty).
This means that the user may have access to rules from another company from the
product but failing to read the stock.picking.type

To avoid regressions introduced by 35f3dea, make a search on stock.picking.type
to filter on the one the user really have access to.

opw 658141

addons/stock_dropshipping/stock_dropshipping.py
@@ -16,7 +16,11 @@ class sale_order_line(models.Model):
         res = super(sale_order_line, self)._check_routing(product, warehouse)
         if not res:
             for route in product.route_ids:
-                for pull_rule in route.pull_ids:
+                # multicompany rule on procurement rule may not be working as company_id
+                # field is not displayed and is probably not set
+                # search to benefit from stock.picking.type multicompany filter
+                picking_type_ids = self.env['stock.picking.type'].search([]).ids
+                for pull_rule in self.env['procurement.rule'].search([('id', 'in', route.pull_ids.ids), ('picking_type_id', 'in', picking_type_ids)]):
                     if (pull_rule.picking_type_id.default_location_src_id.usage == 'supplier' and
                             pull_rule.picking_type_id.default_location_dest_id.usage == 'customer'):
                         return True