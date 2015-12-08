f2a0fe2533e828f2b920194c25a6b6ec4192cd25
ptr <ptr@openerp.com>
2015-12-08 16:45:09 +0530

    [MIG] test case inventory and move.

--- addons/stock/tests/__init__.py
+++ addons/stock/tests/__init__.py
@@ -3,3 +3,5 @@
 from . import test_stock_flow
 from . import test_owner_available
 from . import test_resupply
+from . import test_inventory
+from . import test_move
---
+++ addons/stock/tests/test_inventory.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from openerp.tests.common import TransactionCase
+
+class TestInventory(TransactionCase):
+    def test_stock_inventory(self):
+        ProductObj = self.env['product.product']
+        InvObj = self.env['stock.inventory']
+        InvLineObj = self.env['stock.inventory.line']
+
+        inventory_product = ProductObj.create({
+                "name": "inventory prod",
+                "type": "product"
+            })
+
+        inventory_test0 = InvObj.create({
+                "name": "Test",
+                "filter": "product",
+                'product_id': inventory_product.id
+            })
+
+        inventory_test0.prepare_inventory()
+        assert len(inventory_test0.line_ids) == 0, "Wrong number of inventory lines."
+
+        inventory_testline0 = InvLineObj.create({
+            "inventory_id": inventory_test0.id,
+            "product_uom_id": self.env.ref('product.product_uom_unit').id,
+            "product_id": inventory_product.id,
+            "product_qty": 10,
+            "location_id": self.env.ref('stock.stock_location_14').id,
+        })
+
+        inventory_test0.action_done()
+
+        assert len(inventory_test0.move_ids) >= 1, "No move created for the inventory."
+        assert len(inventory_test0.move_ids[0].quant_ids) >= 1, "No quant created for this inventory"
+
+        product = inventory_product.with_context({'location': self.env.ref('stock.stock_location_14').id})
+        assert product.qty_available == 10, 'Expecting 10 products, got %.2f on location stock_location_14!' % (product.qty_available,)
+        product = inventory_product.with_context({'location': self.env.ref('stock.stock_location_stock').id})
+        assert product.qty_available == 10, 'Expecting 10 products, got %.2f on location stock_location_stock!' % (product.qty_available,)
+
+        product = inventory_product.with_context({'location': self.env.ref('stock.stock_location_components').id})
+        assert product.qty_available == 0, 'Expecting 0 products, got %.2f on location stock_location_components!' % (product.qty_available,)
+
+        inventory_test1 = InvObj.create({
+                'name': 'second test inventory',
+                'filter': 'product',
+                'product_id': inventory_product.id
+            })
+
+        inventory_test1.prepare_inventory()
+        assert inventory_test1.line_ids and len(inventory_test1.line_ids) == 1, "Wrong number of inventory lines."
+        assert inventory_test1.line_ids[0].product_qty == 10, "Wrong quantity in inventory line."
+        assert inventory_test1.line_ids[0].product_id.id == inventory_product.id, "Wrong product in inventory line."
+
+        inventory_test1.line_ids[0].write({'product_qty': 20})
+
+        inventory_test1.action_done()
+
+        product = inventory_product.with_context({'location': self.env.ref('stock.stock_location_14').id})
+        assert product.qty_available == 20, 'Expecting 20 products, got %.2f on location stock_location_14!' % (product.qty_available,)
