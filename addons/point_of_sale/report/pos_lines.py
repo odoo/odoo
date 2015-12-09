4e09fd5126ac6a418a386109a46624c21c506945
Darshan Kalola <dka@openerp.com>
2015-12-09 15:39:46 +0530

    [IMP]point_of_sale: improvements in reports  - Sales Detail' report:product code square brackets should display only if product having a code set on it.  - Sales Line report : display a multiple tax name separated by comma when product having more than one tax set on it.

--- addons/point_of_sale/report/pos_lines.py
+++ addons/point_of_sale/report/pos_lines.py
@@ -14,8 +14,6 @@ class pos_lines(report_sxw.rml_parse):
         self.localcontext.update({
             'time': time,
             'total_quantity': self.__total_quantity__,
-            'taxes':self.__taxes__,
-
         })
 
     def __total_quantity__(self, obj):
@@ -24,16 +22,6 @@ class pos_lines(report_sxw.rml_parse):
             tot += line.qty
         self.total = tot
         return self.total
-
-    def __taxes__(self, obj):
-        self.cr.execute ( " Select acct.name from pos_order as po " \
-                              " LEFT JOIN pos_order_line as pol ON po.id = pol.order_id " \
-                              " LEFT JOIN product_product as pp ON pol.product_id = pp.id"
-                              " LEFT JOIN product_taxes_rel as ptr ON pp.product_tmpl_id = ptr.prod_id " \
-                              " LEFT JOIN account_tax as acct ON acct.id = ptr.tax_id " \
-                              " WHERE pol.id = %s", (obj.id,))
-        res=self.cr.fetchone()[0]
-        return res
 
 
 class report_pos_lines(osv.AbstractModel):
