a815722d9c6f91743527f2fb062bc7ac2acf784e
Rutul Raval <rra@openerp.com>
2015-12-09 11:26:12 +0530

    [ADD] product: New entry in the app switcher / root menu: Products
        * This commit adds an easy access to products and variants from the app switcher / root menu, similar to what exists for contacts.
        * Move an action to view stockable products to stock, as it is only executed from stock.

--- addons/product/res_config.py
+++ addons/product/res_config.py
@@ -9,7 +9,10 @@ class base_config_settings(osv.TransientModel):
             help="Share your product to all companies defined in your instance.\n"
                  " * Checked : Product are visible for every company, even if a company is defined on the partner.\n"
                  " * Unchecked : Each company can see only its product (product where company is defined). Product not related to a company are visible for all companies."),
-        'group_product_variant': fields.boolean('Manage Product Variants', 
+        'group_product_variant': fields.selection([
+            (0, "No variants on products"),
+            (1, 'Products can have several attributes, defining variants (Example: size, color,...)')
+        ], "Product Variants",
             help='Work with product variant allows you to define some variant of the same products, an ease the product management in the ecommerce for example',
             implied_group='product.group_product_variant'),
     }
