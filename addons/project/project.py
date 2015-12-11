odoo/odoo

commit 1945a0fead5f04f8506238ee9d95644a441d0904

[IMP] project, project_issue : email cc set follower
Partner is created on the fly if it didn't exist yet.

addons/project/project.py
@@ -791,6 +791,13 @@ class task(osv.osv):
         aliases = self.pool['project.project'].message_get_reply_to(cr, uid, list(project_ids), default=default, context=context)
         return dict((task.id, aliases.get(task.project_id and task.project_id.id or 0, False)) for task in tasks)
 
+    def email_split(self, cr, uid, ids, msg, context=None):
+        email_list = tools.email_split((msg.get('to') or '') + ',' + (msg.get('cc') or ''))
+        # check left-part is not already an alias
+        task_ids = self.browse(cr, uid, ids, context=context)
+        aliases = [task.project_id.alias_name for task in task_ids if task.project_id]
+        return filter(lambda x: x.split('@')[0] not in aliases, email_list)
+
     def message_new(self, cr, uid, msg, custom_values=None, context=None):
         """ Override to updates the document according to the email. """
         if custom_values is None:
@@ -801,12 +808,10 @@ class task(osv.osv):
             'partner_id': msg.get('author_id', False)
         }
         defaults.update(custom_values)
+
         res = super(task, self).message_new(cr, uid, msg, custom_values=defaults, context=context)
-        email_list = tools.email_split((msg.get('to') or '') + ',' + (msg.get('cc') or ''))
-        new_task = self.browse(cr, uid, res, context=context)
-        if new_task.project_id and new_task.project_id.alias_name:  # check left-part is not already an alias
-            email_list = filter(lambda x: x.split('@')[0] != new_task.project_id.alias_name, email_list)
-        partner_ids = filter(lambda x: x, self._find_partner_from_emails(cr, uid, [], email_list, check_followers=False))
+        email_list = self.email_split(cr, uid, [res], msg, context=context)
+        partner_ids = self._find_partner_from_emails(cr, uid, [res], email_list, force_create=True, context=context)
         self.message_subscribe(cr, uid, [res], partner_ids, context=context)
         return res
 
@@ -828,6 +833,10 @@ class task(osv.osv):
                         update_vals[field] = float(res.group(2).lower())
                     except (ValueError, TypeError):
                         pass
+
+        email_list = self.email_split(cr, uid, ids, msg, context=context)
+        partner_ids = self._find_partner_from_emails(cr, uid, ids, email_list, force_create=True, context=context)
+        self.message_subscribe(cr, uid, ids, partner_ids, context=context)
         return super(task, self).message_update(cr, uid, ids, msg, update_vals=update_vals, context=context)
 
     def message_get_suggested_recipients(self, cr, uid, ids, context=None):

addons/project_issue/project_issue.py
@@ -368,6 +368,13 @@ class project_issue(osv.Model):
             pass
         return recipients
 
+    def email_split(self, cr, uid, ids, msg, context=None):
+        email_list = tools.email_split((msg.get('to') or '') + ',' + (msg.get('cc') or ''))
+        # check left-part is not already an alias
+        issue_ids = self.browse(cr, uid, ids, context=context)
+        aliases = [issue.project_id.alias_name for issue in issue_ids if issue.project_id]
+        return filter(lambda x: x.split('@')[0] not in aliases, email_list)
+
     def message_new(self, cr, uid, msg, custom_values=None, context=None):
         """ Overrides mail_thread message_new that is called by the mailgateway
             through message_process.
@@ -384,8 +391,20 @@ class project_issue(osv.Model):
             'user_id': False,
         }
         defaults.update(custom_values)
+
         res_id = super(project_issue, self).message_new(cr, uid, msg, custom_values=defaults, context=context)
+        email_list = self.email_split(cr, uid, [res_id], msg, context=context)
+        partner_ids = self._find_partner_from_emails(cr, uid, [res_id], email_list, force_create=True, context=context)
+        self.message_subscribe(cr, uid, [res_id], partner_ids, context=context)
         return res_id
+
+    def message_update(self, cr, uid, ids, msg, update_vals=None, context=None):
+        """ Override to update the issue according to the email. """
+
+        email_list = self.email_split(cr, uid, ids, msg, context=context)
+        partner_ids = self._find_partner_from_emails(cr, uid, ids, email_list, force_create=True, context=context)
+        self.message_subscribe(cr, uid, ids, partner_ids, context=context)
+        return super(project_issue, self).message_update(cr, uid, ids, msg, update_vals=update_vals, context=context)
 
     @api.cr_uid_ids_context
     @api.returns('mail.message', lambda value: value.id)

addons/mail/models/mail_thread.py
@@ -1392,16 +1392,18 @@ class MailThread(models.AbstractModel):
         return result
 
     @api.multi
-    def _find_partner_from_emails(self, emails, res_model=None, res_id=None, check_followers=True):
+    def _find_partner_from_emails(self, emails, res_model=None, res_id=None, check_followers=True, force_create=False):
         """ Utility method to find partners from email addresses. The rules are :
             1 - check in document (model | self, id) followers
             2 - try to find a matching partner that is also an user
             3 - try to find a matching partner
+            4 - create a new one if force_create = True
 
             :param list emails: list of email addresses
             :param string model: model to fetch related record; by default self
                 is used.
             :param boolean check_followers: check in document followers
+            :param boolean force_create: create a new partner if not found
         """
         if res_model is None:
             res_model = self._name
@@ -1449,6 +1451,8 @@ class MailThread(models.AbstractModel):
                     partners = Partner.search([('email', 'ilike', email_brackets)], limit=1)
                 if partners:
                     partner_id = partners[0].id
+            if not partner_id and force_create:
+                partner_id = self.env['res.partner'].name_create(contact)[0]
             partner_ids.append(partner_id)
         return partner_ids
 

addons/mail/models/res_partner.py
@@ -40,6 +40,7 @@ class Partner(models.Model):
     @api.model
     def _notify_prepare_template_context(self, message):
         # compute signature
+        signature = ""
         if message.author_id and message.author_id.user_ids and message.author_id.user_ids[0].signature:
             signature = message.author_id.user_ids[0].signature
         elif message.author_id:

commit 236ec3278ee42a0c2654cefde0440b3fe73abfdf

[FIX] project, issue: stop spamming partners
Commit 1945a0fead5f04f8506238ee9d95644a441d0904 introduced the automatic
addition of cc as followers of tasks and issues. However it seems to
automatically create partners if the address it not recognized. This
does not seem a good idea.

addons/project/project.py
@@ -806,7 +806,7 @@ class task(osv.osv):
 
         res = super(task, self).message_new(cr, uid, msg, custom_values=defaults, context=context)
         email_list = self.email_split(cr, uid, [res], msg, context=context)
-        partner_ids = self._find_partner_from_emails(cr, uid, [res], email_list, force_create=True, context=context)
+        partner_ids = filter(None, self._find_partner_from_emails(cr, uid, [res], email_list, force_create=False, context=context))
         self.message_subscribe(cr, uid, [res], partner_ids, context=context)
         return res
 
@@ -830,7 +830,7 @@ class task(osv.osv):
                         pass
 
         email_list = self.email_split(cr, uid, ids, msg, context=context)
-        partner_ids = self._find_partner_from_emails(cr, uid, ids, email_list, force_create=True, context=context)
+        partner_ids = filter(None, self._find_partner_from_emails(cr, uid, ids, email_list, force_create=False, context=context))
         self.message_subscribe(cr, uid, ids, partner_ids, context=context)
         return super(task, self).message_update(cr, uid, ids, msg, update_vals=update_vals, context=context)
 

addons/project_issue/project_issue.py
@@ -397,7 +397,7 @@ class project_issue(osv.Model):
 
         res_id = super(project_issue, self).message_new(cr, uid, msg, custom_values=defaults, context=context)
         email_list = self.email_split(cr, uid, [res_id], msg, context=context)
-        partner_ids = self._find_partner_from_emails(cr, uid, [res_id], email_list, force_create=True, context=context)
+        partner_ids = filter(None, self._find_partner_from_emails(cr, uid, [res_id], email_list, force_create=False, context=context))
         self.message_subscribe(cr, uid, [res_id], partner_ids, context=context)
         return res_id
 
@@ -405,7 +405,7 @@ class project_issue(osv.Model):
         """ Override to update the issue according to the email. """
 
         email_list = self.email_split(cr, uid, ids, msg, context=context)
-        partner_ids = self._find_partner_from_emails(cr, uid, ids, email_list, force_create=True, context=context)
+        partner_ids = filter(None, self._find_partner_from_emails(cr, uid, ids, email_list, force_create=False, context=context))
         self.message_subscribe(cr, uid, ids, partner_ids, context=context)
         return super(project_issue, self).message_update(cr, uid, ids, msg, update_vals=update_vals, context=context)
 