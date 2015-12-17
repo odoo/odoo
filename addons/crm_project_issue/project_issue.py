odoo/odoo

commit 3e8727e2aa987750d68b4a6f4e9e48572b43e001

[FIX] crm_project_issue: one more handrolled ref/xmlid_to_res_id

addons/crm_project_issue/project_issue.py
@@ -47,7 +47,8 @@ class crm_lead_to_project_issue_wizard(osv.TransientModel):
             # delete the lead
             Lead.unlink(cr, uid, [lead.id], context=None)
         # return the action to go to the form view of the new Issue
-        view_id = self.pool.get('ir.ui.view').search(cr, uid, [('model', '=', 'project.issue'), ('name', '=', 'project_issue_form_view')])
+        view_id = self.pool['ir.model.data'].xmlid_to_res_id(
+            cr, uid, 'project_issue.project_issue_form_view', context=context)
         return {
             'name': 'Issue created',
             'view_type': 'form',

commit a2e12b7a14f4be9179534d616b5ff69e38792dec

[FIX] crm_project_issue: remove unnecessary context from xmlid_to_res_id
Regression introduced with 3e8727e2aa987750d68b4a6f4e9e48572b43e001
opw-654063

addons/crm_project_issue/project_issue.py
@@ -48,7 +48,7 @@ class crm_lead_to_project_issue_wizard(osv.TransientModel):
             Lead.unlink(cr, uid, [lead.id], context=None)
         # return the action to go to the form view of the new Issue
         view_id = self.pool['ir.model.data'].xmlid_to_res_id(
-            cr, uid, 'project_issue.project_issue_form_view', context=context)
+            cr, uid, 'project_issue.project_issue_form_view')
         return {
             'name': 'Issue created',
             'view_type': 'form',