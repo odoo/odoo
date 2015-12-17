2434ac446292b73fa6e32ca871872e2914239cbf
Josse Colpaert <jco@odoo.com>
2015-12-17 17:36:13 +0100

    [FIX] mrp: When exploding for a phantom bom (kit), it should take care of service products in it too
    A Phantom BoM can have service products which can create tasks.
    Somehow the code did not use the _need_procurement function that was there before
    to check if it needs to create a task.

--- addons/mrp/stock.py
+++ addons/mrp/stock.py
@@ -62,7 +62,7 @@ class StockMove(osv.osv):
                     mid = move_obj.copy(cr, uid, move.id, default=valdef, context=context)
                     to_explode_again_ids.append(mid)
                 else:
-                    if product.type in ('consu','product'):
+                    if product._need_procurement():
                         valdef = {
                             'name': move.rule_id and move.rule_id.name or "/",
                             'origin': move.origin,
