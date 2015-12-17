3645eb14cd69c74067cb7798fcedc80384c81bd6
ptr <ptr@openerp.com>
2015-12-17 17:08:08 +0530

    revert commite.

--- addons/stock/inherit_stock_move.py
+++ addons/stock/inherit_stock_move.py
@@ -21,6 +21,34 @@ from openerp.exceptions import UserError
 class stock_move(osv.osv):
     _inherit = "stock.move"
 
+    @api.v7
+    def onchange_quantity(self, cr, uid, ids, product_id, product_qty, product_uom):
+        """ On change of product quantity finds UoM
+        @param product_id: Product id
+        @param product_qty: Changed Quantity of product
+        @param product_uom: Unit of measure of product
+        @return: Dictionary of values
+        """
+        warning = {}
+        result = {}
+
+        if (not product_id) or (product_qty <= 0.0):
+            result['product_qty'] = 0.0
+            return {'value': result}
+
+        product_obj = self.pool.get('product.product')
+        # Warn if the quantity was decreased
+        if ids:
+            for move in self.read(cr, uid, ids, ['product_qty']):
+                if product_qty < move['product_qty']:
+                    warning.update({
+                        'title': _('Information'),
+                        'message': _("By changing this quantity here, you accept the "
+                                "new quantity as complete: Odoo will not "
+                                "automatically generate a back order.")})
+                break
+        return {'warning': warning}
+
     def action_done(self, cr, uid, ids, context=None):
         """ Process completely the moves given as ids and if all moves are done, it will finish the picking.
         """
