63feb9987520452d3249483381374e81486c6c87
qsm-odoo <qsm@odoo.com>
2015-12-24 11:05:03 +0100

    [FIX] web: views in action dialog
    Opening a view (kanban or list) in a dialog thanks to a target=new
    action did not work anymore (only in community).
    
    The cause was the 'search_view' action flag which was set to false
    in case of a popup (preventing the view to search the records and so
    to display them). This commit sets this flag to true if the popup
    has to display a list/kanban view.
    
    Backport of aab enterprise work.

--- addons/web/static/src/js/action_manager.js
+++ addons/web/static/src/js/action_manager.js
@@ -609,7 +609,7 @@ var ActionManager = Widget.extend({
             var form = _.str.startsWith(action.view_mode, 'form');
             action.flags = _.defaults(action.flags || {}, {
                 views_switcher : !popup && !inline,
-                search_view : !popup && !inline,
+                search_view : !(popup && form) && !inline,
                 action_buttons : !popup && !inline,
                 sidebar : !popup && !inline,
                 pager : (!popup || !form) && !inline,
