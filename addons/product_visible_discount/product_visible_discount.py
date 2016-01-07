33c8948e8c0aaca2a24bbd2c2cd8789f99ada934
Nicolas Martinelli <nim@odoo.com>
2016-01-07 15:56:03 +0100

    [FIX] product_visible_discount: fix price with tax included
    _fix_tax_included_price should take new_list_price as parameter.
    Moreover, this should be done in both onchange methods.

--- addons/product_visible_discount/product_visible_discount.py
+++ addons/product_visible_discount/product_visible_discount.py
@@ -52,7 +52,7 @@ class sale_order_line(osv.osv):
                 list_price = line.order_id.pricelist_id.with_context(pricelist_context).price_rule_get(line.product_id.id, line.product_uom_qty or 1.0, line.order_id.partner_id)
 
                 new_list_price, currency_id = line.with_context(context_partner)._get_real_price_currency(line.product_id.id, list_price, line.product_uom_qty, line.product_uom.id, line.order_id.pricelist_id.id)
-                new_list_price = self.env['account.tax']._fix_tax_included_price(line.product_id.price, line.product_id.taxes_id, line.tax_id)
+                new_list_price = self.env['account.tax']._fix_tax_included_price(new_list_price, line.product_id.taxes_id, line.tax_id)
                 if line.order_id.pricelist_id.discount_policy == 'without_discount' and list_price[line.order_id.pricelist_id.id][0] != 0 and new_list_price != 0:
                     if line.product_id.company_id and line.order_id.pricelist_id.currency_id.id != line.product_id.company_id.currency_id.id:
                         # new_list_price is in company's currency while price in pricelist currency
@@ -81,6 +81,7 @@ class sale_order_line(osv.osv):
             pricelist_context = dict(context_partner, uom=self.product_uom.id, date=self.order_id.date_order)
             list_price = self.order_id.pricelist_id.with_context(pricelist_context).price_rule_get(self.product_id.id, self.product_uom_qty or 1.0, self.order_id.partner_id)
             new_list_price, currency_id = self.with_context(context_partner)._get_real_price_currency(self.product_id.id, list_price, self.product_uom_qty, self.product_uom.id, self.order_id.pricelist_id.id)
+            new_list_price = self.env['account.tax']._fix_tax_included_price(new_list_price, self.product_id.taxes_id, self.tax_id)
             if self.order_id.pricelist_id.discount_policy == 'without_discount' and list_price[self.order_id.pricelist_id.id][0] != 0 and new_list_price != 0:
                 if self.product_id.company_id and self.order_id.pricelist_id.currency_id.id != self.product_id.company_id.currency_id.id:
                     # new_list_price is in company's currency while price in pricelist currency
