497ee338c843d87f7a0531bcdf5041bbeffbc8cf
ptr <ptr@openerp.com>
2016-01-07 17:33:53 +0530

    [MIG] action_done method in stock_move.

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -2293,6 +2293,139 @@ class StockMove(models.Model):
         return res
 
     @api.multi
+    def action_done(self):
+        """ Process completely the moves given as ids and if all moves are done, it will finish the picking.
+        """
+        # context = context or {}
+        picking_obj = self.env["stock.picking"]
+        quant_obj = self.env["stock.quant"]
+        uom_obj = self.env["product.uom"]
+        todo = self.filtered(lambda move: move.state == "draft")
+        # todo = [move.id for move in self.browse(cr, uid, ids, context=context) if move.state == "draft"]
+        if todo:
+            self = self.browse(todo.action_confirm())
+        pickings = set()
+        procurement_ids = set()
+        #Search operations that are linked to the moves
+        operations = set()
+        move_qty = {}
+        for move in self:
+            move_qty[move.id] = move.product_qty
+            for link in move.linked_move_operation_ids:
+                operations.add(link.operation_id)
+
+        #Sort operations according to entire packages first, then package + lot, package only, lot only
+        operations = list(operations)
+        operations.sort(key=lambda x: ((x.package_id and not x.product_id) and -4 or 0) + (x.package_id and -2 or 0) + (x.pack_lot_ids and -1 or 0))
+
+        for ops in operations:
+            if ops.picking_id:
+                pickings.add(ops.picking_id.id)
+            main_domain = [('qty', '>', 0)]
+            entire_pack = False
+            if ops.product_id:
+                #If a product is given, the result is always put immediately in the result package (if it is False, they are without package)
+                quant_dest_package_id = ops.result_package_id.id
+            else:
+                # When a pack is moved entirely, the quants should not be written anything for the destination package
+                quant_dest_package_id = False
+                entire_pack = True
+            lot_qty = {}
+            tot_qty = 0.0
+            for pack_lot in ops.pack_lot_ids:
+                qty = uom_obj._compute_qty(ops.product_uom_id.id, pack_lot.qty, ops.product_id.uom_id.id)
+                lot_qty[pack_lot.lot_id.id] = qty
+                tot_qty += pack_lot.qty
+            if ops.pack_lot_ids and ops.product_id and float_compare(tot_qty, ops.product_qty, precision_rounding=ops.product_uom_id.rounding) != 0.0:
+                raise UserError(_('You have a difference between the quantity on the operation and the quantities specified for the lots. '))
+            quants_taken = []
+            false_quants = []
+            lot_move_qty = {}
+            for record in ops.linked_move_operation_ids:
+                move = record.move_id
+                self.check_tracking(move, ops)
+                preferred_domain = [('reservation_id', '=', move.id)]
+                fallback_domain = [('reservation_id', '=', False)]
+                fallback_domain2 = ['&', ('reservation_id', '!=', move.id), ('reservation_id', '!=', False)]
+                dom = main_domain
+                if not ops.pack_lot_ids:
+                    preferred_domain_list = [preferred_domain] + [fallback_domain] + [fallback_domain2]
+                    quants = quant_obj.quants_get_preferred_domain(record.qty, move, ops=ops, domain=dom,
+                                                        preferred_domain_list=preferred_domain_list)
+                    quant_obj.quants_move(quants, move, ops.location_dest_id, location_from=ops.location_id,
+                                          lot_id=False, owner_id=ops.owner_id.id, src_package_id=ops.package_id.id,
+                                          dest_package_id=quant_dest_package_id, entire_pack=entire_pack)
+                else:
+                    # Check what you can do with reserved quants already
+                    qty_on_link = record.qty
+                    rounding = ops.product_id.uom_id.rounding
+                    for reserved_quant in move.reserved_quant_ids:
+                        if not reserved_quant.lot_id:
+                            false_quants += [reserved_quant]
+                        elif float_compare(lot_qty.get(reserved_quant.lot_id.id, 0), 0, precision_rounding=rounding) > 0:
+                            if float_compare(lot_qty[reserved_quant.lot_id.id], reserved_quant.qty, precision_rounding=rounding) >= 0:
+                                lot_qty[reserved_quant.lot_id.id] -= reserved_quant.qty
+                                quants_taken += [(reserved_quant, reserved_quant.qty)]
+                                qty_on_link -= reserved_quant.qty
+                            else:
+                                quants_taken += [(reserved_quant, lot_qty[reserved_quant.lot_id.id])]
+                                lot_qty[reserved_quant.lot_id.id] = 0
+                                qty_on_link -= lot_qty[reserved_quant.lot_id.id]
+                    lot_move_qty[move.id] = qty_on_link
+
+                if not move_qty.get(move.id):
+                    raise UserError(_("The roundings of your Unit of Measures %s on the move vs. %s on the product don't allow to do these operations or you are not transferring the picking at once. ") % (move.product_uom.name, move.product_id.uom_id.name))
+                move_qty[move.id] -= record.qty
+
+            #Handle lots separately
+            if ops.pack_lot_ids:
+                self._move_quants_by_lot(ops, lot_qty, quants_taken, false_quants, lot_move_qty, quant_dest_package_id)
+
+            # Handle pack in pack
+            if not ops.product_id and ops.package_id and ops.result_package_id.id != ops.package_id.parent_id.id:
+                ops.package_id.sudo().write({'parent_id': ops.result_package_id.id})
+        #Check for remaining qtys and unreserve/check move_dest_id in
+        move_dest_ids = set()
+        for move in self:
+            move_qty_cmp = float_compare(move_qty[move.id], 0, precision_rounding=move.product_id.uom_id.rounding)
+            if move_qty_cmp > 0:  # (=In case no pack operations in picking)
+                main_domain = [('qty', '>', 0)]
+                preferred_domain = [('reservation_id', '=', move.id)]
+                fallback_domain = [('reservation_id', '=', False)]
+                fallback_domain2 = ['&', ('reservation_id', '!=', move.id), ('reservation_id', '!=', False)]
+                preferred_domain_list = [preferred_domain] + [fallback_domain] + [fallback_domain2]
+                self.check_tracking(move, False)
+                qty = move_qty[move.id]
+                quants = quant_obj.quants_get_preferred_domain(qty, move, domain=main_domain, preferred_domain_list=preferred_domain_list)
+                quant_obj.quants_move(quants, move, move.location_dest_id, lot_id=move.restrict_lot_id.id, owner_id=move.restrict_partner_id.id)
+
+            # If the move has a destination, add it to the list to reserve
+            if move.move_dest_id and move.move_dest_id.state in ('waiting', 'confirmed'):
+                move_dest_ids.add(move.move_dest_id.id)
+
+            if move.procurement_id:
+                procurement_ids.add(move.procurement_id.id)
+
+            #unreserve the quants and make them available for other operations/moves
+            quant_obj.quants_unreserve(move)
+        # Check the packages have been placed in the correct locations
+        self._check_package_from_moves()
+        #set the move as done
+        self.write({'state': 'done', 'date': time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)})
+        self.env['procurement.order'].check(list(procurement_ids))
+        #assign destination moves
+        if move_dest_ids:
+            self.browse(list(move_dest_ids)).action_assign()
+        #check picking state to set the date_done is needed
+        done_picking = []
+        for picking in picking_obj.browse(list(pickings)):
+            if picking.state == 'done' and not picking.date_done:
+                done_picking.append(picking.id)
+        if done_picking:
+            picking_obj.browse(done_picking).write({'date_done': time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)})
+        return True
+
+    @api.multi
     def _check_package_from_moves(self):
         pack_obj = self.env["stock.quant.package"]
         packs = set()
