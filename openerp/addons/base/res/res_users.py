3734dde6da37318a64ced6ccf5a16232d857bbbb
Bruno PLANCHER <bruno.plancher@gmail.com>
2016-01-08 11:50:20 +0100

    [IMP] res.users: improved calculation of implied_ids
    Notes:
    - Perform more verifivations before browsing the trans_implied_ids;
    - Usefull when an instance has many res.groups (more than 80), for example, when creating many groups to work as 'atomic' rights

--- openerp/addons/base/res/res_users.py
+++ openerp/addons/base/res/res_users.py
@@ -653,13 +653,19 @@ class groups_implied(osv.osv):
         return gid
 
     def write(self, cr, uid, ids, values, context=None):
+        originals = {}
+        for x in self.browse(cr, uid, ids, context=context):
+            originals.update({x.id: x})
         res = super(groups_implied, self).write(cr, uid, ids, values, context)
         if values.get('users') or values.get('implied_ids'):
             # add all implied groups (to all users of each group)
             for g in self.browse(cr, uid, ids):
-                gids = map(int, g.trans_implied_ids)
-                vals = {'users': [(4, u.id) for u in g.users]}
-                super(groups_implied, self).write(cr, uid, gids, vals, context)
+                origin = originals.get(g.id, False)
+                if origin and (origin.implied_ids != g.implied_ids or
+                               origin.users != g.users):
+                    gids = map(int, g.trans_implied_ids)
+                    vals = {'users': [(4, u.id) for u in g.users]}
+                    super(groups_implied, self).write(cr, uid, gids, vals, context)
         return res
 
 groups_implied()
