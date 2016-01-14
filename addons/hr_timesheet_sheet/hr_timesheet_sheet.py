6fd6ebd68b011a3a02c8ad4865084252676d23c9
Yannick Tivisse <yti@odoo.com>
2016-01-14 14:13:47 +0100

    [IMP] hr_timesheeet: Menus reorganisation + small improvements
    + Remove duplicated fields on res_company config view
    + Remove Timesheet range per 'Day' on res_company
    + Move Attendance information in the 'Attendances' notebook on timesheet view

--- addons/hr_timesheet_sheet/hr_timesheet_sheet.py
+++ addons/hr_timesheet_sheet/hr_timesheet_sheet.py
@@ -623,8 +623,7 @@ class hr_timesheet_sheet_sheet_account(osv.osv):
 class res_company(osv.osv):
     _inherit = 'res.company'
     _columns = {
-        'timesheet_range': fields.selection(
-            [('day','Day'),('week','Week'),('month','Month')], 'Timesheet range',
+        'timesheet_range': fields.selection([('week','Week'),('month','Month')], 'Timesheet range',
             help="Periodicity on which you validate your timesheets."),
         'timesheet_max_difference': fields.float('Timesheet allowed difference(Hours)',
             help="Allowed difference in hours between the sign in/out and the timesheet " \
