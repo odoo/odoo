94bd19b71b08d8c8ecd4af2de87ad2f5d2cd7e96
Thibault Delavallée <tde@openerp.com>
2016-01-15 16:44:29 +0100

    [IMP] project_issue: support archiving of columns for issues

--- addons/project_issue/models/project_task_type.py
+++ addons/project_issue/models/project_task_type.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import api, models
 
 
 class ProjectStage(models.Model):
@@ -10,3 +10,12 @@ class ProjectStage(models.Model):
     def _get_mail_template_id_domain(self):
         domain = super(ProjectStage, self)._get_mail_template_id_domain()
         return ['|'] + domain + [('model', '=', 'project.issue')]
+
+    @api.multi
+    def archive(self, archive, project_ids=None, archive_only_content=False):
+        if project_ids:
+            domain = [('project_id', 'in', project_ids), ('stage_id', 'in', self.ids)]
+        else:
+            domain = [('stage_id', 'in', self.ids)]
+        self.env['project.issue'].search(domain).write({'active': not archive})
+        return super(ProjectStage, self).archive(archive, project_ids=project_ids, archive_only_content=archive_only_content)
