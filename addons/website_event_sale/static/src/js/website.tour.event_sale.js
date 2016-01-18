24c41e3d31077746eba4cec90e5ff0f7e7b973f5
Olivier Dony <odo@openerp.com>
2016-01-18 16:16:21 +0100

    [FIX] website_event_sale: test updated for pricelist fix
    Rev. f9ee454d42b4c73cc387a1873e31bc3361b67517 fixed the pricelist
    application in website_event_sale in 8.0, but the 9.0 test
    contained a hardcoded price matching the old computation.
    The test failed when the fix was forward-ported to 9.0.

--- addons/website_event_sale/static/src/js/website.tour.event_sale.js
+++ addons/website_event_sale/static/src/js/website.tour.event_sale.js
@@ -83,8 +83,8 @@ base.ready().done(function () {
                 },
             },
             {
-                title:     "Check that the subtotal is 5,500.0", 
-                element:   '#order_total_untaxed .oe_currency_value:contains("5,500.0")',
+                title:     "Check that the subtotal is 3,856.96",
+                element:   '#order_total_untaxed .oe_currency_value:contains("3,856.96")',
             },
             {
                 title:     "Select `Wire Transfer` payment method",
