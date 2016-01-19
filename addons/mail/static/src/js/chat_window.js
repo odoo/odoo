302885b86077f0a76bd1b598bf7735b745f045a5
Aaron Bohy <aab@odoo.com>
2016-01-19 11:56:23 +0100

    [FIX] mail: various ui fixes
    - limit partner search to 10 in new message's chat window to prevent overflow
    - prevent negative needaction counters (may occur in multi-tab, with
      needactions received during an inactivity period)
    - remove <pre> tag when computing messages' preview for the messaging dropdown
      as they break the layout and add 'overflow: hidden' on o_mail_channel_preview
      just in case
    - rename 'New message' button in Inbox into 'Send mail' as it was confusing,
      and adapt .pot file accordingly
    - focus on composer when opening a chat window
    - break lines in threads to avoid horizontal scrollbar (credit goes to qsm)
    - correctly apply rules on <p> in threads
    
    opw-667235

--- addons/mail/static/src/js/chat_window.js
+++ addons/mail/static/src/js/chat_window.js
@@ -46,6 +46,8 @@ return Widget.extend({
 
         if (this.folded) {
             this.$el.css('height', HEIGHT_FOLDED);
+        } else {
+            this.focus_input();
         }
         var def = this.thread.replace(this.$('.o_chat_content'));
         return $.when(this._super(), def);
@@ -67,8 +69,14 @@ return Widget.extend({
         this.folded = _.isBoolean(fold) ? fold : !this.folded;
         if (!this.folded) {
             this.thread.scroll_to();
+            this.focus_input();
         }
         this.fold();
+    },
+    focus_input: function () {
+        if (!config.device.touch) {
+            this.$input.focus();
+        }
     },
     do_show: function () {
         this.is_hidden = false;
