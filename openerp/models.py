334febec127b84419315bb88b33f3b103e20f9bc
Goffin Simon <sig@odoo.com>
2016-01-21 12:47:48 +0100

    [FIX] models: `MissingError` when writing on recordset with duplicates
    When writing on a recordset with duplicates, the ORM raises a `MissingError`
    because the rowcount gives a difference with the injected ids.  The fix simply
    eliminates duplicates from ids.

--- openerp/models.py
+++ openerp/models.py
@@ -3896,7 +3896,7 @@ class BaseModel(object):
                 self._table, ','.join('"%s"=%s' % u[:2] for u in updates),
             )
             params = tuple(u[2] for u in updates if len(u) > 2)
-            for sub_ids in cr.split_for_in_conditions(ids):
+            for sub_ids in cr.split_for_in_conditions(set(ids)):
                 cr.execute(query, params + (sub_ids,))
                 if cr.rowcount != len(sub_ids):
                     raise MissingError(_('One of the records you are trying to modify has already been deleted (Document type: %s).') % self._description)
