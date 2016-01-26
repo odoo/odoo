15fe3d43b4b925a32b42eb46048a33987e10336b
Jeremy Kersten <jke@odoo.com>
2016-01-26 16:40:41 +0100

    [IMP] base: add required state for address
    Some address have required state.
    We add by default moreover 500 new states.
    
    We try also to merge duplicated states
    wich have the same country and code.
    
    Once merge is done, we try to had the unicity constraint
    to avoid futur duplicates.
    
    Merge code, should be removed in v11.

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -92,6 +92,11 @@ class Website(openerp.addons.web.controllers.main.Home):
 
         return request.render(page, values)
 
+    @http.route(['/website/country_infos/<model("res.country"):country>'], type='json', auth="public", methods=['POST'], website=True)
+    def country_infos(self, country, **kw):
+        fields = country.get_address_fields()
+        return dict(fields=fields, states=[(st.id, st.name, st.code) for st in country.state_ids], phone_code=country.phone_code)
+
     @http.route(['/robots.txt'], type='http', auth="public")
     def robots(self):
         return request.render('website.robots', {'url_root': request.httprequest.url_root}, mimetype='text/plain')
