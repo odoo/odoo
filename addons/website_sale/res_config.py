8979368b5779496ac57706b8d3e94ea982349bcc
Martin Geubelle <mge@odoo.com>
2016-02-04 17:45:34 +0100

    [ADD] website_sale: allow delivery in Settings

--- addons/website_sale/res_config.py
+++ addons/website_sale/res_config.py
@@ -6,6 +6,7 @@ class website_config_settings(models.TransientModel):
 
     salesperson_id = fields.Many2one('res.users', related='website_id.salesperson_id', string='Salesperson')
     salesteam_id = fields.Many2one('crm.team', related='website_id.salesteam_id', string='Sales Team')
+    module_website_sale_delivery = fields.Boolean("Add Delivery Costs to Online Sales")
     module_delivery_dhl = fields.Boolean("DHL integration")
     module_delivery_fedex = fields.Boolean("Fedex integration")
     module_delivery_temando = fields.Boolean("Temando integration")
