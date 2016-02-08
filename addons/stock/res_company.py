d2f8925452d9e3cc85f5280159feba122ebc181b
Pariket Trivedi <ptr@openerp.com>
2016-02-08 16:26:46 +0530

    [REF] stock: res_company model migrated into new api.

--- addons/stock/res_company.py
+++ addons/stock/res_company.py
@@ -1,45 +1,40 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
+from odoo import api, fields, models, _
 
-class res_company(osv.osv):
+
+class ResCompany(models.Model):
     _inherit = "res.company"
-    _columns = {
-        'propagation_minimum_delta': fields.integer('Minimum Delta for Propagation of a Date Change on moves linked together'),
-        'internal_transit_location_id': fields.many2one('stock.location', 'Internal Transit Location', help="Technical field used for resupply routes between warehouses that belong to this company", on_delete="restrict"),
-    }
 
-    def create_transit_location(self, cr, uid, company_id, company_name, context=None):
+    propagation_minimum_delta = fields.Integer(string='Minimum Delta for Propagation of a Date Change on moves linked together', defaulat=1)
+    internal_transit_location_id = fields.Many2one('stock.location', string='Internal Transit Location', help="Technical field used for resupply routes between warehouses that belong to this company", on_delete="restrict")
+
+    def create_transit_location(self):
         '''Create a transit location with company_id being the given company_id. This is needed
            in case of resuply routes between warehouses belonging to the same company, because
            we don't want to create accounting entries at that time.
         '''
-        data_obj = self.pool.get('ir.model.data')
         try:
-            parent_loc = data_obj.get_object_reference(cr, uid, 'stock', 'stock_location_locations')[1]
+            parent_loc = self.env.ref('stock.stock_location_locations').id
         except:
             parent_loc = False
         location_vals = {
-            'name': _('%s: Transit Location') % company_name,
+            'name': _('%s: Transit Location') % self.name,
             'usage': 'transit',
-            'company_id': company_id,
+            'company_id': self.id,
             'location_id': parent_loc,
         }
-        location_id = self.pool.get('stock.location').create(cr, uid, location_vals, context=context)
-        self.write(cr, uid, [company_id], {'internal_transit_location_id': location_id}, context=context)
+        location_id = self.env['stock.location'].create(location_vals)
+        self.write({'internal_transit_location_id': location_id.id})
 
-    def create(self, cr, uid, vals, context=None):
-        company_id = super(res_company, self).create(cr, uid, vals, context=context)
-        self.pool['stock.warehouse'].create(cr, uid, {
+    @api.model
+    def create(self, vals):
+        company_id = super(ResCompany, self).create(vals)
+        self.env['stock.warehouse'].create({
             'name': vals['name'],
             'code': vals['name'][:5],
-            'company_id': company_id,
-        }, context=context)
-        self.create_transit_location(cr, uid, company_id, vals['name'], context=context)
+            'company_id': company_id.id,
+        })
+        company_id.create_transit_location()
         return company_id
-
-    _defaults = {
-        'propagation_minimum_delta': 1,
-    }
