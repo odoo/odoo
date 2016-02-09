ffafeea174adaf625be287de9835538e5da5d900
Nicolas Lempereur <nle@odoo.com>
2016-02-09 10:43:00 +0100

    [FIX] website_crm: fix medium_id for website sent form
    In ed64790 `crm.crm_medium_website` was replaced by
    `utm.utm_medium_website`.
    
    There was one place where it was not reverberated so the medium_id was
    always unset in that instance (a crm.lead created from a website form).
    
    opw-668853
    
    note: equivalent of saas-6's 3ea3348

--- addons/website_crm/models/crm_lead.py
+++ addons/website_crm/models/crm_lead.py
@@ -4,5 +4,5 @@ class Lead(models.Model):
     _inherit = 'crm.lead'
 
     def website_form_input_filter(self, request, values):
-        values.setdefault('medium_id', request.registry['ir.model.data'].xmlid_to_res_id(request.cr, SUPERUSER_ID, 'crm.crm_medium_website'))
+        values.setdefault('medium_id', request.registry['ir.model.data'].xmlid_to_res_id(request.cr, SUPERUSER_ID, 'utm.utm_medium_website'))
         return values
