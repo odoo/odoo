c69731c1c360ba03ab9fada656d2d7b4ff472bec
Raphael Collet <rco@openerp.com>
2016-02-09 10:01:34 +0100

    [FIX] addons: fix usage of decorators `api.v7`/`api.v8`
    Specifically, when one API implementation calls the other one, it has to call
    the method *from the same class*.  Otherwise, overriding the method may result
    in an infinite recursion.  Consider:
    
        class A(Model):
            _name = 'stuff'
    
            @api.v8
            def foo(self):
                return 42
    
            @api.v7
            def foo(self, cr, uid, context=None):
                return self.browse(cr, uid, [], context).foo()
    
        class B(Model):
            _inherit = 'stuff'
    
            def foo(self, cr, uid, context=None):
                return super(B, self).foo(cr, uid, context=context) + 1
    
    and now call: `env['stuff'].foo()`.  This invokes `B.foo` (new-API), which
    calls `B.foo` (old-API), which calls `A.foo` (old-API), which calls `B.foo`
    (new-API) instead of `A.foo`!
    
    This issue would not be present if old-API `A.foo` was defined as:
    
            @api.v7
            def foo(self, cr, uid, context=None):
                return A.foo(self.browse(cr, uid, [], context))

--- addons/account/account_invoice.py
+++ addons/account/account_invoice.py
@@ -1209,7 +1209,7 @@ class account_invoice(models.Model):
     def pay_and_reconcile(self, cr, uid, ids, pay_amount, pay_account_id, period_id, pay_journal_id,
                           writeoff_acc_id, writeoff_period_id, writeoff_journal_id, context=None, name=''):
         recs = self.browse(cr, uid, ids, context)
-        return recs.pay_and_reconcile(pay_amount, pay_account_id, period_id, pay_journal_id,
+        return account_invoice.pay_and_reconcile(recs, pay_amount, pay_account_id, period_id, pay_journal_id,
                     writeoff_acc_id, writeoff_period_id, writeoff_journal_id, name=name)
 
 class account_invoice_line(models.Model):
@@ -1600,7 +1600,7 @@ class account_invoice_tax(models.Model):
     def compute(self, cr, uid, invoice_id, context=None):
         recs = self.browse(cr, uid, [], context)
         invoice = recs.env['account.invoice'].browse(invoice_id)
-        return recs.compute(invoice)
+        return account_invoice_tax.compute(recs, invoice)
 
     @api.model
     def move_line_get(self, invoice_id):
