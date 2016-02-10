973378c9ffb0c4393d8460a0850eaae8dc202ede
Pariket Trivedi <ptr@openerp.com>
2016-02-10 14:55:29 +0530

    [REF]stock: stock_pack_operation_lot model migrated into new api.

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -4554,68 +4554,55 @@ class stock_pack_operation(osv.osv):
         }
 
 
-class stock_pack_operation_lot(osv.osv):
+class StockPackOperationLot(models.Model):
     _name = "stock.pack.operation.lot"
     _description = "Specifies lot/serial number for pack operations that need it"
 
-    def _get_plus(self, cr, uid, ids, field_name, arg, context=None):
-        res = {}
-        for packlot in self.browse(cr, uid, ids, context=context):
+    @api.multi
+    def _get_plus(self):
+        for packlot in self:
             if packlot.operation_id.product_id.tracking == 'serial':
-                res[packlot.id] = (packlot.qty == 0.0)
+                packlot.plus_visible = (packlot.qty == 0.0)
             else:
-                res[packlot.id] = (packlot.qty_todo == 0.0) or (packlot.qty < packlot.qty_todo)
-        return res
+                packlot.plus_visible = (packlot.qty_todo == 0.0) or (packlot.qty < packlot.qty_todo)
 
-    _columns = {
-        'operation_id': fields.many2one('stock.pack.operation'),
-        'qty': fields.float('Done'),
-        'lot_id': fields.many2one('stock.production.lot', 'Lot/Serial Number'),
-        'lot_name': fields.char('Lot Name'),
-        'qty_todo': fields.float('To Do'),
-        'plus_visible': fields.function(_get_plus, type='boolean'),
-    }
+    operation_id = fields.Many2one('stock.pack.operation')
+    qty = fields.Float('Done', default=1.0)
+    lot_id = fields.Many2one('stock.production.lot', 'Lot/Serial Number')
+    lot_name = fields.Char()
+    qty_todo = fields.Float('To Do', default=0.0)
+    plus_visible = fields.Boolean(compute="_get_plus", default=True)
 
-    _defaults = {
-        'qty': lambda cr, uid, ids, c: 1.0,
-        'qty_todo': lambda cr, uid, ids, c: 0.0,
-        'plus_visible': True,
-    }
-
-    def _check_lot(self, cr, uid, ids, context=None):
-        for packlot in self.browse(cr, uid, ids, context=context):
+    @api.multi
+    @api.constrains('lot_id', 'lot_name')
+    def _check_lot(self):
+        for packlot in self:
             if not packlot.lot_name and not packlot.lot_id:
-                return False
+                raise UserError(_('Lot is required'))
         return True
 
-    _constraints = [
-        (_check_lot,
-            'Lot is required',
-            ['lot_id', 'lot_name']),
-    ]
-
     _sql_constraints = [
-        ('qty', 'CHECK(qty >= 0.0)','Quantity must be greater than or equal to 0.0!'),
+        ('qty', 'CHECK(qty >= 0.0)', 'Quantity must be greater than or equal to 0.0!'),
         ('uniq_lot_id', 'unique(operation_id, lot_id)', 'You have already mentioned this lot in another line'),
         ('uniq_lot_name', 'unique(operation_id, lot_name)', 'You have already mentioned this lot name in another line')]
 
-    def do_plus(self, cr, uid, ids, context=None):
-        pack_obj = self.pool['stock.pack.operation']
-        for packlot in self.browse(cr, uid, ids, context=context):
-            self.write(cr, uid, [packlot.id], {'qty': packlot.qty + 1}, context=context)
-        pack = self.browse(cr, uid, ids[0], context=context).operation_id
+    @api.multi
+    def do_plus(self):
+        for packlot in self:
+            packlot.write({'qty': packlot.qty + 1})
+        pack = self[0].operation_id
         qty_done = sum([x.qty for x in pack.pack_lot_ids])
-        pack_obj.write(cr, uid, [pack.id], {'qty_done': qty_done}, context=context)
-        return pack_obj.split_lot(cr, uid, [pack.id], context=context)
+        pack.write({'qty_done': qty_done})
+        return pack.split_lot()
 
-    def do_minus(self, cr, uid, ids, context=None):
-        pack_obj = self.pool['stock.pack.operation']
-        for packlot in self.browse(cr, uid, ids, context=context):
-            self.write(cr, uid, [packlot.id], {'qty': packlot.qty - 1}, context=context)
-        pack = self.browse(cr, uid, ids[0], context=context).operation_id
+    @api.multi
+    def do_minus(self):
+        for packlot in self:
+            packlot.write({'qty': packlot.qty - 1})
+        pack = self[0].operation_id
         qty_done = sum([x.qty for x in pack.pack_lot_ids])
-        pack_obj.write(cr, uid, [pack.id], {'qty_done': qty_done}, context=context)
-        return pack_obj.split_lot(cr, uid, [pack.id], context=context)
+        pack.write({'qty_done': qty_done})
+        return pack.split_lot()
 
 
 class StockMoveOperationLink(models.Model):
