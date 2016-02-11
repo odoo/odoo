acecd453899e3935d2ee1946ac85064bf44186ec
Pariket Trivedi <ptr@openerp.com>
2016-02-11 17:35:43 +0530

    [REF]stock: stock_incoterms model migrated into new api.

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -23,17 +23,14 @@ _logger = logging.getLogger(__name__)
 #----------------------------------------------------------
 # Incoterms
 #----------------------------------------------------------
-class stock_incoterms(osv.osv):
+from odoo import models, fields, api
+class StockIncoterms(models.Model):
     _name = "stock.incoterms"
     _description = "Incoterms"
-    _columns = {
-        'name': fields.char('Name', required=True, help="Incoterms are series of sales terms. They are used to divide transaction costs and responsibilities between buyer and seller and reflect state-of-the-art transportation practices."),
-        'code': fields.char('Code', size=3, required=True, help="Incoterm Standard Code"),
-        'active': fields.boolean('Active', help="By unchecking the active field, you may hide an INCOTERM you will not use."),
-    }
-    _defaults = {
-        'active': True,
-    }
+
+    name = fields.Char(required=True, help="Incoterms are series of sales terms. They are used to divide transaction costs and responsibilities between buyer and seller and reflect state-of-the-art transportation practices.")
+    code = fields.Char(required=True, help="Incoterm Standard Code")
+    active = fields.Boolean(default=True, help="By unchecking the active field, you may hide an INCOTERM you will not use.")
 
 #----------------------------------------------------------
 # Stock Location
