813f297ea0a141ff8bf5129febabb3ebbe16e625
Aaron Bohy <aab@odoo.com>
2016-02-11 16:07:41 +0100

    [IMP] mail: follower: remove an RPC when user is follower
    When loading a form_view with the follower widgets, an RPC was performed to get
    the followers info (name, email...), and if the user is one of the followers,
    a second RPC was done to retrieve the subtypes.
    
    This rev. batches those two RPCs. Also simplifies read_subscription_data() as
    it is now always called with a follower_id.
    
    A few code refactoring as well.

--- addons/mail/controllers/main.py
+++ addons/mail/controllers/main.py
@@ -37,36 +37,34 @@ class MailController(http.Controller):
         return True
 
     @http.route('/mail/read_followers', type='json', auth='user')
-    def read_followers(self, follower_ids):
-        result = []
+    def read_followers(self, follower_ids, res_model, res_id):
+        followers = []
         is_editable = request.env.user.has_group('base.group_no_one')
+        partner_id = request.env.user.partner_id
+        follower_id = None
         for follower in request.env['mail.followers'].browse(follower_ids):
-            result.append({
+            is_uid = partner_id == follower.partner_id
+            follower_id = follower.id if is_uid else follower_id
+            followers.append({
                 'id': follower.id,
                 'name': follower.partner_id.name or follower.channel_id.name,
                 'email': follower.partner_id.email if follower.partner_id else None,
                 'res_model': 'res.partner' if follower.partner_id else 'mail.channel',
                 'res_id': follower.partner_id.id or follower.channel_id.id,
                 'is_editable': is_editable,
-                'is_uid': request.env.user.partner_id == follower.partner_id,
+                'is_uid': is_uid,
             })
-        return result
+        return {
+            'followers': followers,
+            'subtypes': self.read_subscription_data(res_model, follower_id) if follower_id else None
+        }
 
     @http.route('/mail/read_subscription_data', type='json', auth='user')
-    def read_subscription_data(self, res_model, res_id, follower_id=None):
+    def read_subscription_data(self, res_model, follower_id):
         """ Computes:
             - message_subtype_data: data about document subtypes: which are
                 available, which are followed if any """
-        # find the document followers, update the data
-        followers = request.env['mail.followers']
-        if not follower_id:
-            followers = followers.search([
-                ('partner_id', '=', request.env.user.partner_id.id),
-                ('res_id', '=', res_id),
-                ('res_model', '=', res_model),
-            ])
-        else:
-            followers = followers.browse(follower_id)
+        followers = request.env['mail.followers'].browse(follower_id)
 
         # find current model subtypes, add them to a dictionary
         subtypes = request.env['mail.message.subtype'].search(['&', ('hidden', '=', False), '|', ('res_model', '=', res_model), ('res_model', '=', False)])
