8bee616ab2fda2355f972e91657a8a9a49f5887c
Pariket Trivedi <ptr@openerp.com>
2016-02-12 15:49:21 +0530

    [REF]stock: stock_production_lot model migrated into new api.

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -1681,38 +1681,30 @@ class stock_picking(models.Model):
         return package_id
 
 
-class stock_production_lot(osv.osv):
+class StockProductionLot(models.Model):
     _name = 'stock.production.lot'
     _inherit = ['mail.thread']
     _description = 'Lot/Serial'
-    _columns = {
-        'name': fields.char('Serial Number', required=True, help="Unique Serial Number"),
-        'ref': fields.char('Internal Reference', help="Internal reference number in case it differs from the manufacturer's serial number"),
-        'product_id': fields.many2one('product.product', 'Product', required=True, domain=[('type', 'in', ['product', 'consu'])]),
-        'quant_ids': fields.one2many('stock.quant', 'lot_id', 'Quants', readonly=True),
-        'create_date': fields.datetime('Creation Date'),
-    }
-    _defaults = {
-        'name': lambda x, y, z, c: x.pool.get('ir.sequence').next_by_code(y, z, 'stock.lot.serial'),
-        'product_id': lambda x, y, z, c: c.get('product_id', False),
-    }
+
+    name = fields.Char('Serial Number', required=True, default=lambda x: x.env['ir.sequence'].next_by_code('stock.lot.serial'), help="Unique Serial Number")
+    ref = fields.Char('Internal Reference', help="Internal reference number in case it differs from the manufacturer's serial number")
+    product_id = fields.Many2one('product.product', 'Product', required=True, domain=[('type', 'in', ['product', 'consu'])], default=lambda x: x._context.get('product_id'))
+    quant_ids = fields.One2many('stock.quant', 'lot_id', 'Quants', readonly=True)
+    create_date = fields.Datetime('Creation Date')
+
     _sql_constraints = [
         ('name_ref_uniq', 'unique (name, product_id)', 'The combination of serial number and product must be unique !'),
     ]
 
-    def action_traceability(self, cr, uid, ids, context=None):
+    @api.multi
+    def action_traceability(self):
         """ It traces the information of lots
         @param self: The object pointer.
-        @param cr: A database cursor
-        @param uid: ID of the user currently logged in
-        @param ids: List of IDs selected
-        @param context: A standard dictionary
         @return: A dictionary of values
         """
-        quant_obj = self.pool.get("stock.quant")
-        quants = quant_obj.search(cr, uid, [('lot_id', 'in', ids)], context=context)
+        quants = self.env["stock.quant"].search([('lot_id', 'in', self.ids)])
         moves = set()
-        for quant in quant_obj.browse(cr, uid, quants, context=context):
+        for quant in quants:
             moves |= {move.id for move in quant.history_ids}
         if moves:
             return {
@@ -1725,7 +1717,6 @@ class stock_production_lot(osv.osv):
                 'type': 'ir.actions.act_window',
                     }
         return False
-
 
 # ----------------------------------------------------
 # Move
