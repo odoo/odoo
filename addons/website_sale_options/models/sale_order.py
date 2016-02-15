008494e97757742dbc4bf13373645b7a47fbec00
Ravi Gadhia <rga@openerp.com>
2016-02-15 17:20:38 +0530

    [IMP] website_sale_options: new api migration

--- addons/website_sale_options/models/sale_order.py
+++ addons/website_sale_options/models/sale_order.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp import SUPERUSER_ID, api
-from openerp.osv import osv, orm, fields
-from openerp.tools.translate import _
+from odoo import api, fields, models, _
 
 
-class sale_order_line(osv.Model):
+class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
-    _columns = {
-        'linked_line_id': fields.many2one('sale.order.line', 'Linked Order Line', domain="[('order_id','!=',order_id)]", ondelete='cascade'),
-        'option_line_ids': fields.one2many('sale.order.line', 'linked_line_id', string='Options Linked'),
-    }
 
-class sale_order(osv.Model):
+    linked_line_id = fields.Many2one('sale.order.line', string='Linked Order Line', domain="[('order_id', '!=', order_id)]", ondelete='cascade')
+    option_line_ids = fields.One2many('sale.order.line', 'linked_line_id', string='Options Linked')
+
+class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     @api.multi
     def _cart_find_product_line(self, product_id=None, line_id=None, **kwargs):
         self.ensure_one()
-        lines = super(sale_order, self)._cart_find_product_line(product_id, line_id)
+        lines = super(SaleOrder, self)._cart_find_product_line(product_id, line_id)
         if line_id:
             return lines
         linked_line_id = kwargs.get('linked_line_id', False)
@@ -32,30 +30,31 @@ class sale_order(osv.Model):
             lines = lines.filtered(lambda line: not line.option_line_ids)
         return lines
 
-    def _cart_update(self, cr, uid, ids, product_id=None, line_id=None, add_qty=0, set_qty=0, context=None, **kwargs):
+    @api.multi
+    def _cart_update(self, product_id=None, line_id=None, add_qty=0, set_qty=0, **kwargs):
         """ Add or set product quantity, add_qty can be negative """
-        value = super(sale_order, self)._cart_update(cr, uid, ids, product_id, line_id, add_qty, set_qty, context=context, **kwargs)
+        value = super(SaleOrder, self)._cart_update(product_id, line_id, add_qty, set_qty, **kwargs)
 
-        sol = self.pool.get('sale.order.line')
-        line = sol.browse(cr, SUPERUSER_ID, value.get('line_id'), context=context)
+        SaleOrderLineSudo = self.env['sale.order.line'].sudo()
+        line = SaleOrderLineSudo.browse(value.get('line_id'))
 
         # link a product to the sale order
         if kwargs.get('linked_line_id'):
-            linked_line_id = sol.browse(cr, SUPERUSER_ID, kwargs['linked_line_id'], context=context)
+            linked_line = SaleOrderLineSudo.browse(kwargs['linked_line_id'])
             line.write({
-                    "name": _("%s\nOption for: %s") % (line.name, linked_line_id.product_id.name_get()[0][1]),
-                    "linked_line_id": linked_line_id.id
+                    "name": _("%s\nOption for: %s") % (line.name, linked_line.product_id.display_name),
+                    "linked_line_id": linked_line.id
                 })
 
         value['option_ids'] = set()
-        for so in self.browse(cr, uid, ids, context=context):
+        for so in self:
             # select all optional products linked to the updated line
-            option_line_ids = [l for l in so.order_line if l.linked_line_id.id == line.id]
+            option_lines = so.order_line.filtered(lambda l: l.linked_line_id.id == line.id)
 
             # update line
-            for option_line_id in option_line_ids:
-                super(sale_order, self)._cart_update(cr, uid, ids, option_line_id.product_id.id, option_line_id.id, add_qty, set_qty, context=context, **kwargs)
-                option_line_id.write({"name": _("%s\nOption for: %s") % (option_line_id.name, option_line_id.linked_line_id.product_id.name_get()[0][1])})
+            for option_line_id in option_lines:
+                super(SaleOrder, self)._cart_update(option_line_id.product_id.id, option_line_id.id, add_qty, set_qty, **kwargs)
+                option_line_id.write({"name": _("%s\nOption for: %s") % (option_line_id.name, option_line_id.linked_line_id.product_id.display_name)})
                 value['option_ids'].add(option_line_id.id)
 
         value['option_ids'] = list(value['option_ids'])
