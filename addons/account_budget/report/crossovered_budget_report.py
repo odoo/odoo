249402c4a33dcc63b41cc8e473ad97536c315892
Rashik Kalathiya <rka@odoo.com>
2016-02-16 12:58:36 +0530

    [IMP] account_budget, reports: migrate to new API and clean a code  - Removed useless perameter like ids,done,level from method funct()  - Add render_html method  - Removed funct_total() method

--- addons/account_budget/report/crossovered_budget_report.py
+++ addons/account_budget/report/crossovered_budget_report.py
@@ -1,179 +1,119 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import time
-from openerp.osv import osv
-from openerp.report import report_sxw
+from odoo import api, models
 
 
-class budget_report(report_sxw.rml_parse):
-    def __init__(self, cr, uid, name, context):
-        super(budget_report, self).__init__(cr, uid, name, context=context)
-        self.localcontext.update({
-            'funct': self.funct,
-            'funct_total': self.funct_total,
-            'time': time,
-        })
-        self.context = context
+class BudgetReport(models.AbstractModel):
+    _name = 'report.account_budget.report_crossoveredbudget'
 
-    def funct(self, object, form, ids=None, done=None, level=1):
-        if ids is None:
-            ids = {}
-        if not ids:
-            ids = self.ids
-        if not done:
-            done = {}
-        global tot
-        tot = {
-            'theo':0.00,
-            'pln':0.00,
-            'prac':0.00,
-            'perc':0.00
-        }
-        result = []
-
-        budgets = self.pool.get('crossovered.budget').browse(self.cr, self.uid, [object.id], self.context.copy())
-        c_b_lines_obj = self.pool.get('crossovered.budget.lines')
-        acc_analytic_obj = self.pool.get('account.analytic.account')
-        for budget_id in budgets:
-            res = {}
-            budget_lines = []
-            budget_ids = []
-            d_from = form['date_from']
-            d_to = form['date_to']
-
-            for line in budget_id.crossovered_budget_line:
-                budget_ids.append(line.id)
-
-            if not budget_ids:
-                return []
-
-            self.cr.execute('SELECT DISTINCT(analytic_account_id) FROM crossovered_budget_lines WHERE id = ANY(%s)',(budget_ids,))
-            an_ids = self.cr.fetchall()
-
-            context = {'wizard_date_from': d_from, 'wizard_date_to': d_to}
-            for i in range(0, len(an_ids)):
-                if not an_ids[i][0]:
-                    continue
-                analytic_name = acc_analytic_obj.browse(self.cr, self.uid, [an_ids[i][0]])
-                res={
+    def get_budget_lines(self, crossovered_budgets, form):
+        result = {}
+        for crossovered_budget in crossovered_budgets:
+            budget_lines = crossovered_budget.crossovered_budget_line
+            lines = []
+            budget_total = {
+                'tot_theo': 0.00,
+                'tot_pln': 0.00,
+                'tot_prac': 0.00,
+                'tot_perc': 0.00
+            }
+            if not budget_lines:
+                result.update({crossovered_budget.id: {'lines': lines, 'budget_total': budget_total}})
+                continue
+            for analytic_account in budget_lines.mapped('analytic_account_id'):
+                lines.append({
                     'b_id': '-1',
                     'a_id': '-1',
-                    'name': analytic_name[0].name,
+                    'name': analytic_account.name,
                     'status': 1,
                     'theo': 0.00,
                     'pln': 0.00,
                     'prac': 0.00,
                     'perc': 0.00
-                }
-                result.append(res)
-
-                line_ids = c_b_lines_obj.search(self.cr, self.uid, [('id', 'in', budget_ids), ('analytic_account_id','=',an_ids[i][0]), ('date_to', '>=', d_from), ('date_from', '<=', d_to)])
-                line_id = c_b_lines_obj.browse(self.cr, self.uid, line_ids)
+                })
                 tot_theo = tot_pln = tot_prac = tot_perc = 0.00
-
                 done_budget = []
-                for line in line_id:
-                    if line.id in budget_ids:
-                        theo = pract = 0.00
-                        theo = c_b_lines_obj._theo_amt(self.cr, self.uid, [line.id], context)[line.id]
-                        pract = c_b_lines_obj._prac_amt(self.cr, self.uid, [line.id], context)[line.id]
-                        if line.general_budget_id.id in done_budget:
-                            for record in result:
-                                if record['b_id'] == line.general_budget_id.id  and record['a_id'] == line.analytic_account_id.id:
-                                    record['theo'] += theo
-                                    record['pln'] += line.planned_amount
-                                    record['prac'] += pract
-                                    if record['theo'] <> 0.00:
-                                        perc = (record['prac'] / record['theo']) * 100
-                                    else:
-                                        perc = 0.00
-                                    record['perc'] = perc
-                                    tot_theo += theo
-                                    tot_pln += line.planned_amount
-                                    tot_prac += pract
-                                    tot_perc += perc
-                        else:
-                            if theo <> 0.00:
-                                perc = (pract / theo) * 100
-                            else:
-                                perc = 0.00
-                            res1 = {
-                                    'a_id': line.analytic_account_id.id,
-                                    'b_id': line.general_budget_id.id,
-                                    'name': line.general_budget_id.name,
-                                    'status': 2,
-                                    'theo': theo,
-                                    'pln': line.planned_amount,
-                                    'prac': pract,
-                                    'perc': perc,
-                            }
-                            tot_theo += theo
-                            tot_pln += line.planned_amount
-                            tot_prac += pract
-                            tot_perc += perc
-                            if form['report'] == 'analytic-full':
-                                result.append(res1)
-                                done_budget.append(line.general_budget_id.id)
+                for line in budget_lines.filtered(lambda rec: rec.analytic_account_id == analytic_account and rec.date_to >= form['date_from'] and rec.date_from <= form['date_to']):
+                    theo = pract = 0.00
+                    theo = line._theo_amt()[line.id]
+                    pract = line._prac_amt()[line.id]
+                    if line.general_budget_id.id in done_budget:
+                        for record in lines:
+                            if record['b_id'] == line.general_budget_id.id and record['a_id'] == line.analytic_account_id.id:
+                                record['theo'] += theo
+                                record['pln'] += line.planned_amount
+                                record['prac'] += pract
+                                if record['theo'] != 0.00:
+                                    perc = (record['prac'] / record['theo']) * 100
+                                else:
+                                    perc = 0.00
+                                record['perc'] = perc
+                                tot_theo += theo
+                                tot_pln += line.planned_amount
+                                tot_prac += pract
+                                tot_perc += perc
                     else:
-
-                        if line.general_budget_id.id in done_budget:
-                            continue
+                        if theo != 0.00:
+                            perc = (pract / theo) * 100
                         else:
-                            res1={
-                                    'a_id': line.analytic_account_id.id,
-                                    'b_id': line.general_budget_id.id,
-                                    'name': line.general_budget_id.name,
-                                    'status': 2,
-                                    'theo': 0.00,
-                                    'pln': 0.00,
-                                    'prac': 0.00,
-                                    'perc': 0.00
-                            }
-                            if form['report'] == 'analytic-full':
-                                result.append(res1)
-                                done_budget.append(line.general_budget_id.id)
+                            perc = 0.00
+                        res1 = {
+                                'a_id': line.analytic_account_id.id,
+                                'b_id': line.general_budget_id.id,
+                                'name': line.general_budget_id.name,
+                                'status': 2,
+                                'theo': theo,
+                                'pln': line.planned_amount,
+                                'prac': pract,
+                                'perc': perc,
+                        }
+                        tot_theo += theo
+                        tot_pln += line.planned_amount
+                        tot_prac += pract
+                        tot_perc += perc
+                        if form['report'] == 'analytic-full':
+                            lines.append(res1)
+                            done_budget.append(line.general_budget_id.id)
                 if tot_theo == 0.00:
                     tot_perc = 0.00
                 else:
                     tot_perc = float(tot_prac / tot_theo) * 100
                 if form['report'] == 'analytic-full':
-                    result[-(len(done_budget) +1)]['theo'] = tot_theo
-                    tot['theo'] += tot_theo
-                    result[-(len(done_budget) +1)]['pln'] = tot_pln
-                    tot['pln'] += tot_pln
-                    result[-(len(done_budget) +1)]['prac'] = tot_prac
-                    tot['prac'] += tot_prac
-                    result[-(len(done_budget) +1)]['perc'] = tot_perc
+                    lines[-(len(done_budget) + 1)]['theo'] = tot_theo
+                    budget_total['tot_theo'] += tot_theo
+                    lines[-(len(done_budget) + 1)]['pln'] = tot_pln
+                    budget_total['tot_pln'] += tot_pln
+                    lines[-(len(done_budget) + 1)]['prac'] = tot_prac
+                    budget_total['tot_prac'] += tot_prac
+                    lines[-(len(done_budget) + 1)]['perc'] = tot_perc
                 else:
-                    result[-1]['theo'] = tot_theo
-                    tot['theo'] += tot_theo
-                    result[-1]['pln'] = tot_pln
-                    tot['pln'] += tot_pln
-                    result[-1]['prac'] = tot_prac
-                    tot['prac'] += tot_prac
-                    result[-1]['perc'] = tot_perc
-            if tot['theo'] == 0.00:
-                tot['perc'] = 0.00
+                    lines[-1]['theo'] = tot_theo
+                    budget_total['tot_theo'] += tot_theo
+                    lines[-1]['pln'] = tot_pln
+                    budget_total['tot_pln'] += tot_pln
+                    lines[-1]['prac'] = tot_prac
+                    budget_total['tot_prac'] += tot_prac
+                    lines[-1]['perc'] = tot_perc
+            if budget_total['tot_theo'] == 0.00:
+                budget_total['tot_perc'] = 0.00
             else:
-                tot['perc'] = float(tot['prac'] / tot['theo']) * 100
+                budget_total['tot_perc'] = float(budget_total['tot_prac'] / budget_total['tot_theo']) * 100
+            result.update({crossovered_budget.id: {'lines': lines, 'budget_total': budget_total}})
         return result
 
-    def funct_total(self, form):
-        result = []
-        res = {}
-        res = {
-             'tot_theo': tot['theo'],
-             'tot_pln': tot['pln'],
-             'tot_prac': tot['prac'],
-             'tot_perc': tot['perc']
+    @api.multi
+    def render_html(self, data=None):
+        self.model = self.env.context.get('active_model')
+        docs = self.env[self.model].browse(self.env.context.get('active_ids'))
+        crossovered_budgets = self.env['crossovered.budget'].browse(data['form']['ids'])
+        result = self.get_budget_lines(crossovered_budgets, data['form'])
+        docargs = {
+            'doc_ids': self.ids,
+            'doc_model': self.model,
+            'docs': docs,
+            'crossoveredbudgets': result,
+            'date_from': data['form']['date_from'],
+            'date_to': data['form']['date_to']
         }
-        result.append(res)
-        return result
-
-
-class report_crossoveredbudget(osv.AbstractModel):
-    _name = 'report.account_budget.report_crossoveredbudget'
-    _inherit = 'report.abstract_report'
-    _template = 'account_budget.report_crossoveredbudget'
-    _wrapped_report_class = budget_report
+        return self.env['report'].render('account_budget.report_crossoveredbudget', docargs)
