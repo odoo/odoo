d35e912c11ff889f0fec3831a849ae8279779a42
Moisés López <moylop260@vauxoo.com>
2016-02-17 17:03:32 +0000

    [REF] stock: Avoid compute of qty virtual

--- addons/stock/product.py
+++ addons/stock/product.py
@@ -122,49 +122,59 @@ class product_product(osv.osv):
         context = context or {}
         field_names = field_names or []
 
+        virtual_compute_ok = True
+        # If only need qty available we can avoid get virtual qty
+        if [u'qty_available'] == field_names:
+            virtual_compute_ok = False
+
         domain_products = [('product_id', 'in', ids)]
         domain_quant, domain_move_in, domain_move_out = [], [], []
         domain_quant_loc, domain_move_in_loc, domain_move_out_loc = self._get_domain_locations(cr, uid, ids, context=context)
-        domain_move_in += self._get_domain_dates(cr, uid, ids, context=context) + [('state', 'not in', ('done', 'cancel', 'draft'))] + domain_products
-        domain_move_out += self._get_domain_dates(cr, uid, ids, context=context) + [('state', 'not in', ('done', 'cancel', 'draft'))] + domain_products
+        if virtual_compute_ok:
+            domain_move_in += self._get_domain_dates(cr, uid, ids, context=context) + [('state', 'not in', ('done', 'cancel', 'draft'))] + domain_products
+            domain_move_out += self._get_domain_dates(cr, uid, ids, context=context) + [('state', 'not in', ('done', 'cancel', 'draft'))] + domain_products
         domain_quant += domain_products
 
         if context.get('lot_id'):
             domain_quant.append(('lot_id', '=', context['lot_id']))
         if context.get('owner_id'):
             domain_quant.append(('owner_id', '=', context['owner_id']))
-            owner_domain = ('restrict_partner_id', '=', context['owner_id'])
-            domain_move_in.append(owner_domain)
-            domain_move_out.append(owner_domain)
+            if virtual_compute_ok:
+                owner_domain = ('restrict_partner_id', '=', context['owner_id'])
+                domain_move_in.append(owner_domain)
+                domain_move_out.append(owner_domain)
         if context.get('package_id'):
             domain_quant.append(('package_id', '=', context['package_id']))
 
-        domain_move_in += domain_move_in_loc
-        domain_move_out += domain_move_out_loc
-        moves_in = self.pool.get('stock.move').read_group(cr, uid, domain_move_in, ['product_id', 'product_qty'], ['product_id'], context=context)
-        moves_out = self.pool.get('stock.move').read_group(cr, uid, domain_move_out, ['product_id', 'product_qty'], ['product_id'], context=context)
+        if virtual_compute_ok:
+            domain_move_in += domain_move_in_loc
+            domain_move_out += domain_move_out_loc
+            moves_in = self.pool.get('stock.move').read_group(cr, uid, domain_move_in, ['product_id', 'product_qty'], ['product_id'], context=context)
+            moves_out = self.pool.get('stock.move').read_group(cr, uid, domain_move_out, ['product_id', 'product_qty'], ['product_id'], context=context)
 
         domain_quant += domain_quant_loc
         quants = self.pool.get('stock.quant').read_group(cr, uid, domain_quant, ['product_id', 'qty'], ['product_id'], context=context)
         quants = dict(map(lambda x: (x['product_id'][0], x['qty']), quants))
 
-        moves_in = dict(map(lambda x: (x['product_id'][0], x['product_qty']), moves_in))
-        moves_out = dict(map(lambda x: (x['product_id'][0], x['product_qty']), moves_out))
+        if virtual_compute_ok:
+            moves_in = dict(map(lambda x: (x['product_id'][0], x['product_qty']), moves_in))
+            moves_out = dict(map(lambda x: (x['product_id'][0], x['product_qty']), moves_out))
         res = {}
         ctx = context.copy()
         ctx.update({'prefetch_fields': False})
         for product in self.browse(cr, uid, ids, context=ctx):
             id = product.id
             qty_available = float_round(quants.get(id, 0.0), precision_rounding=product.uom_id.rounding)
-            incoming_qty = float_round(moves_in.get(id, 0.0), precision_rounding=product.uom_id.rounding)
-            outgoing_qty = float_round(moves_out.get(id, 0.0), precision_rounding=product.uom_id.rounding)
-            virtual_available = float_round(quants.get(id, 0.0) + moves_in.get(id, 0.0) - moves_out.get(id, 0.0), precision_rounding=product.uom_id.rounding)
-            res[id] = {
-                'qty_available': qty_available,
-                'incoming_qty': incoming_qty,
-                'outgoing_qty': outgoing_qty,
-                'virtual_available': virtual_available,
-            }
+            res[id] = {'qty_available': qty_available}
+            if virtual_compute_ok:
+                incoming_qty = float_round(moves_in.get(id, 0.0), precision_rounding=product.uom_id.rounding)
+                outgoing_qty = float_round(moves_out.get(id, 0.0), precision_rounding=product.uom_id.rounding)
+                virtual_available = float_round(quants.get(id, 0.0) + moves_in.get(id, 0.0) - moves_out.get(id, 0.0), precision_rounding=product.uom_id.rounding)
+                res[id].update({
+                    'incoming_qty': incoming_qty,
+                    'outgoing_qty': outgoing_qty,
+                    'virtual_available': virtual_available,
+                })
         return res
 
     def _search_product_quantity(self, cr, uid, obj, name, domain, context):
@@ -353,7 +363,7 @@ class product_template(osv.osv):
         var_ids = []
         for product in product_ids:
             var_ids += [p.id for p in product.product_variant_ids]
-        variant_available= self.pool['product.product']._product_available(cr, uid, var_ids, context=context)
+        variant_available= self.pool['product.product']._product_available(cr, uid, var_ids, name, context=context)
 
         for product in product_ids:
             qty_available = 0
