14402e16dc2b3a53e819b1f107f402403226cc0b
Hitesh Trivedi <htr@openerp.com>
2016-02-18 14:39:40 +0530

    [REF] Gamification : Migrate to new api

--- addons/gamification/models/res_users.py
+++ addons/gamification/models/res_users.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import osv
+from odoo import api, fields, models
 from challenge import MAX_VISIBILITY_RANKING
 
-class res_users_gamification_group(osv.Model):
+class ResUsersGamificationGroup(models.Model):
     """ Update of res.users class
         - if adding groups to an user, check gamification.challenge linked to
         this group, and the user. This is done by overriding the write method.
@@ -12,10 +12,11 @@ class res_users_gamification_group(osv.Model):
     _name = 'res.users'
     _inherit = ['res.users']
 
-    def get_serialised_gamification_summary(self, cr, uid, excluded_categories=None, context=None):
-        return self._serialised_goals_summary(cr, uid, user_id=uid, excluded_categories=excluded_categories, context=context)
+    def get_serialised_gamification_summary(self, excluded_categories=None):
+        # Calling with explicit `browse` instead of `self.env.user` as the latter is a sudo `browse_record`
+        return self.browse(self.env.uid)._serialised_goals_summary(excluded_categories=excluded_categories)
 
-    def _serialised_goals_summary(self, cr, uid, user_id, excluded_categories=None, context=None):
+    def _serialised_goals_summary(self, excluded_categories=None):
         """Return a serialised list of goals assigned to the user, grouped by challenge
         :excluded_categories: list of challenge categories to exclude in search
 
@@ -30,32 +31,27 @@ class res_users_gamification_group(osv.Model):
         ]
         """
         all_goals_info = []
-        challenge_obj = self.pool.get('gamification.challenge')
-        domain = [('user_ids', 'in', uid), ('state', '=', 'inprogress')]
+        domain = [('user_ids', 'in', self.env.uid), ('state', '=', 'inprogress')]
         if excluded_categories and isinstance(excluded_categories, list):
             domain.append(('category', 'not in', excluded_categories))
-        user = self.browse(cr, uid, uid, context=context)
-        challenge_ids = challenge_obj.search(cr, uid, domain, context=context)
-        for challenge in challenge_obj.browse(cr, uid, challenge_ids, context=context):
+        for challenge in self.env['gamification.challenge'].search(domain):
             # serialize goals info to be able to use it in javascript
-            lines = challenge_obj._get_serialized_challenge_lines(cr, uid, challenge, user_id, restrict_top=MAX_VISIBILITY_RANKING, context=context)
+            lines = challenge._get_serialized_challenge_lines(self.id, restrict_top=MAX_VISIBILITY_RANKING)
             if lines:
                 all_goals_info.append({
                     'id': challenge.id,
                     'name': challenge.name,
                     'visibility_mode': challenge.visibility_mode,
-                    'currency': user.company_id.currency_id.id,
+                    'currency': self.env.user.company_id.currency_id.id,
                     'lines': lines,
                 })
 
         return all_goals_info
 
-    def get_challenge_suggestions(self, cr, uid, context=None):
+    def get_challenge_suggestions(self):
         """Return the list of challenges suggested to the user"""
         challenge_info = []
-        challenge_obj = self.pool.get('gamification.challenge')
-        challenge_ids = challenge_obj.search(cr, uid, [('invited_user_ids', 'in', uid), ('state', '=', 'inprogress')], context=context)
-        for challenge in challenge_obj.browse(cr, uid, challenge_ids, context=context):
+        for challenge in self.env['gamification.challenge'].search([('invited_user_ids', 'in', self.env.uid), ('state', '=', 'inprogress')]):
             values = {
                 'id': challenge.id,
                 'name': challenge.name,
--- addons/gamification/wizard/update_goal.py
+++ addons/gamification/wizard/update_goal.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
+from odoo import api, fields, models
 
-class goal_manual_wizard(osv.TransientModel):
+class GoalManualWizard(models.TransientModel):
     """Wizard to update a manual goal"""
     _name = 'gamification.goal.wizard'
-    _columns = {
-        'goal_id': fields.many2one("gamification.goal", string='Goal', required=True),
-        'current': fields.float('Current'),
-    }
 
-    def action_update_current(self, cr, uid, ids, context=None):
+    goal_id = fields.Many2one("gamification.goal", string='Goal', required=True)
+    current = fields.Float()
+
+    @api.multi
+    def action_update_current(self):
         """Wizard action for updating the current value"""
 
-        goal_obj = self.pool.get('gamification.goal')
-
-        for wiz in self.browse(cr, uid, ids, context=context):
+        for wiz in self:
             towrite = {
                 'current': wiz.current,
                 'goal_id': wiz.goal_id.id,
                 'to_update': False,
             }
-            goal_obj.write(cr, uid, [wiz.goal_id.id], towrite, context=context)
-            goal_obj.update_goal(cr, uid, [wiz.goal_id.id], context=context)
+            wiz.goal_id.write(towrite)
+            wiz.goal_id.update_goal()
         return {}
