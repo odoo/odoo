796852bb3ae41edd779529193c9ab5e95f28a4c4
ptr <ptr@openerp.com>
2016-02-18 17:04:05 +0530

    [ADD]stock: convert test case yml to py for stock move.

--- addons/stock/tests/__init__.py
+++ addons/stock/tests/__init__.py
@@ -4,3 +4,4 @@ from . import test_stock_flow
 from . import test_owner_available
 from . import test_resupply
 from . import test_inventory
+from . import test_move
---
+++ addons/stock/tests/test_move.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+from openerp.tests.common import TransactionCase
+
+class TestMove(TransactionCase):
+    def test_stock_move(self):
+        ProductObj = self.env['product.product']
+        MoveObj = self.env['stock.move']
+
+        move_product = ProductObj.create({
+                'name': 'move prod',
+                'type': 'product'
+            })
+
+        move_test0 = MoveObj.create({
+            'name': 'Move Products',
+            'product_id': move_product.id,
+            'product_uom_qty': 5,
+            'product_uom': self.env.ref('product.product_uom_unit').id,
+            'location_id': self.env.ref('stock.stock_location_stock').id,
+            'location_dest_id': self.env.ref('stock.stock_location_customers').id,
+            })
+
+        move_test0.action_confirm()
+
+        product = move_product.with_context({'location': False})
+        self.assertEqual(product.qty_available, 0, 'Expecting 0 products in stock, got %.2f!' % (product.qty_available,))
+        self.assertEqual(product.virtual_available, -5.0, 'Expecting -5 products in virtual stock, got %.2f!' % (product.virtual_available,))
+
+        move_test0.action_done()
+
+        move_product.invalidate_cache()
+        product = move_product.with_context({'location': False})
+        self.assertEqual(product.qty_available, -5, 'Expecting -5 products in stock, got %.2f!' % (product.qty_available,))
+
+        product = move_product.with_context({'location': self.env.ref('stock.stock_location_customers').id})
+        self.assertEqual(product.qty_available, 5, 'Expecting 5 products in customer location, got %.2f!' % (product.qty_available,))
+
+        move_test2 = MoveObj.create({
+            'name': 'Move 15 Products',
+            'product_id': move_product.id,
+            'product_uom_qty': 15,
+            'product_uom': self.env.ref('product.product_uom_unit').id,
+            'location_id': self.env.ref('stock.stock_location_suppliers').id,
+            'location_dest_id': self.env.ref('stock.stock_location_stock').id,
+            })
+
+        move_test2.action_confirm()
+        move_test2.action_done()
+
+        product = move_product.with_context({'location': False})
+        self.assertEqual(product.qty_available, 10, 'Expecting 10 products in stock, got %.2f!' % (product.qty_available,))
+        self.assertEqual(product.virtual_available, 10.0, 'Expecting 10 products in virtual stock, got %.2f!' % (product.virtual_available,))
+
+        move_test1 = MoveObj.create({
+            'name': 'Move Products',
+            'product_id': move_product.id,
+            'product_uom_qty': 2,
+            'product_uom': self.env.ref('product.product_uom_unit').id,
+            'location_id': self.env.ref('stock.stock_location_stock').id,
+            'location_dest_id': self.env.ref('stock.stock_location_customers').id,
+            })
+
+        move_test1.action_confirm()
+
+        move_product.invalidate_cache()
+        product = move_product.with_context({'location': False})
+        self.assertEqual(product.qty_available, 10, 'Expecting 10 products in stock, got %.2f!' % (product.qty_available,))
+        self.assertEqual(product.virtual_available, 8.0, 'Expecting 8 products in virtual stock, got %.2f!' % (product.virtual_available,))
+
+        move_test1.action_done()
+
+        product.invalidate_cache()
+        product = move_product.with_context({'location': False})
+        self.assertEqual(product.qty_available, 8, 'Expecting 10 products in stock, got %.2f!' % (product.qty_available,))
+
+        product = move_product.with_context({'location': self.env.ref('stock.stock_location_customers').id})
+        self.assertEqual(product.qty_available, 7, 'Expecting 7 products in customer location, got %.2f!' % (product.qty_available,))
