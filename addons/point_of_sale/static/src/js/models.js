4e19b2f70c67e638189c216e8be56e9e707d3883
Joren Van Onder <jov@odoo.com>
2016-02-18 13:16:14 +0100

    [IMP] point_of_sale: automatically set customer fiscal position
    This makes it so that when you set a customer in the POS frontend it
    will automatically apply the fiscal position associated to the customer
    on the order.
    
    If the customer has no set fiscal position or if the fiscal position is
    not available in the POS nothing will happen.
    
    opw-668155

--- addons/point_of_sale/static/src/js/models.js
+++ addons/point_of_sale/static/src/js/models.js
@@ -182,7 +182,7 @@ exports.PosModel = Backbone.Model.extend({
         }
     },{
         model:  'res.partner',
-        fields: ['name','street','city','state_id','country_id','vat','phone','zip','mobile','email','barcode','write_date'],
+        fields: ['name','street','city','state_id','country_id','vat','phone','zip','mobile','email','barcode','write_date','property_account_position_id'],
         domain: [['customer','=',true]], 
         loaded: function(self,partners){
             self.partners = partners;
