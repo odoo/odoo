ff7c105cbd238f136d9283a8faa750b287748dfa
Josse Colpaert <jco@odoo.com>
2016-02-20 12:25:21 +0100

    [FIX] variable typo

--- addons/mrp_account/models/mrp_production.py
+++ addons/mrp_account/models/mrp_production.py
@@ -6,6 +6,21 @@ from openerp import api, models, _
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
+    @api.multi
+    def _cal_price(self, consumed_moves):
+        work_center_cost = 0
+        if self.work_order_ids:
+            duration = 0
+            for work_order in self.work_order_ids:
+                time_lines = work_order.time_ids.filtered(lambda x: x.state == 'done' and not x.used)
+                duration += sum(time_lines.mapped('duration'))
+                time_lines.write({'used' : True})
+            # Convert duration into hour and calculate total cost of work.
+            work_center_cost = (duration / 60) * self.workcenter_id.costs_hour
+        for move in self.move_finished_ids.filtered(lambda x: x.product_id.id == self.product_id.id and x.state not in ('done', 'cancel')):
+            if move.product_id.cost_method in ('real', 'average'):
+                move.price_unit = (sum([q.qty * q.cost for q in consumed_moves.mapped('quant_ids')]) + work_center_cost) / move.product_qty
+        return True
 
     def _costs_generate(self):
         """ Calculates total costs at the end of the production.
