8a2f4e7d3fc4abe4bf449123f8b9598bb4a48d14
Yannick Tivisse <yti@odoo.com>
2016-03-07 11:14:59 +0100

    [IMP] base: Merge group_light_multi_company into group_multi_company
    This distinction is useless and confusing.

--- addons/base_setup/res_config.py
+++ addons/base_setup/res_config.py
@@ -9,9 +9,9 @@ class base_config_settings(osv.osv_memory):
     _inherit = 'res.config.settings'
 
     _columns = {
-        'group_light_multi_company': fields.boolean('Manage multiple companies',
+        'group_multi_currency': fields.boolean('Manage multiple companies',
             help='Work in multi-company environments, with appropriate security access between companies.',
-            implied_group='base.group_light_multi_company'),
+            implied_group='base.group_multi_currency'),
         'module_share': fields.boolean('Allow documents sharing',
             help="""Share or embbed any screen of Odoo."""),
         'module_portal': fields.boolean('Activate the customer portal',
