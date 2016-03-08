449022dc4f5c55912129dc786da34d771e946e27
Antonio Espinosa <antespi@gmail.com>
2016-03-08 12:12:21 +0100

    [FIX] Calculate delivery product purchase price using grid

--- addons/delivery/delivery.py
+++ addons/delivery/delivery.py
@@ -203,6 +203,31 @@ class delivery_grid(osv.osv):
     }
     _order = 'sequence'
 
+    def get_cost(self, cr, uid, id, order, dt, context=None):
+        total = 0
+        weight = 0
+        volume = 0
+        quantity = 0
+        total_delivery = 0.0
+        product_uom_obj = self.pool.get('product.uom')
+        for line in order.order_line:
+            if line.state == 'cancel':
+                continue
+            if line.is_delivery:
+                total_delivery += line.price_subtotal + self.pool['sale.order']._amount_line_tax(cr, uid, line, context=context)
+            if not line.product_id or line.is_delivery:
+                continue
+            q = product_uom_obj._compute_qty(cr, uid, line.product_uom.id, line.product_uom_qty, line.product_id.uom_id.id)
+            weight += (line.product_id.weight or 0.0) * q
+            volume += (line.product_id.volume or 0.0) * q
+            quantity += q
+        total = (order.amount_total or 0.0) - total_delivery
+
+        ctx = context.copy()
+        ctx['date'] = order.date_order
+        total = self.pool['res.currency'].compute(cr, uid, order.currency_id.id, order.company_id.currency_id.id, total, context=ctx)
+        return self.get_cost_from_picking(cr, uid, id, total,weight, volume, quantity, context=context)
+
     def get_price(self, cr, uid, id, order, dt, context=None):
         total = 0
         weight = 0
@@ -227,6 +252,24 @@ class delivery_grid(osv.osv):
         ctx['date'] = order.date_order
         total = self.pool['res.currency'].compute(cr, uid, order.currency_id.id, order.company_id.currency_id.id, total, context=ctx)
         return self.get_price_from_picking(cr, uid, id, total,weight, volume, quantity, context=context)
+
+    def get_cost_from_picking(self, cr, uid, id, total, weight, volume, quantity, context=None):
+        grid = self.browse(cr, uid, id, context=context)
+        cost = 0.0
+        ok = False
+        price_dict = {'price': total, 'volume':volume, 'weight': weight, 'wv':volume*weight, 'quantity': quantity}
+        for line in grid.line_ids:
+            test = eval(line.type+line.operator+str(line.max_value), price_dict)
+            if test:
+                if line.price_type=='variable':
+                    cost = line.standard_price * price_dict[line.variable_factor]
+                else:
+                    cost = line.standard_price
+                ok = True
+                break
+        if not ok:
+            raise osv.except_osv(_("Unable to fetch delivery method!"), _("Selected product in the delivery method doesn't fulfill any of the delivery grid(s) criteria."))
+        return cost
 
     def get_price_from_picking(self, cr, uid, id, total, weight, volume, quantity, context=None):
         grid = self.browse(cr, uid, id, context=context)
