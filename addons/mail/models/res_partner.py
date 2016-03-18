19867b8f1bac379b1669013411c3d5a1f1730a70
Géry Debongnie <ged@odoo.com>
2016-03-18 12:21:59 +0100

    [FIX] mail: duplicating partners does not duplicate mail channels
    It could be a real problem when the duplicated user has access to all
    private conversation from the original user.

--- addons/mail/models/res_partner.py
+++ addons/mail/models/res_partner.py
@@ -27,7 +27,7 @@ class Partner(models.Model):
     opt_out = fields.Boolean(
         'Opt-Out', help="If opt-out is checked, this contact has refused to receive emails for mass mailing and marketing campaign. "
                         "Filter 'Available for Mass Mailing' allows users to filter the partners when performing mass mailing.")
-    channel_ids = fields.Many2many('mail.channel', 'mail_channel_partner', 'partner_id', 'channel_id', string='Channels')
+    channel_ids = fields.Many2many('mail.channel', 'mail_channel_partner', 'partner_id', 'channel_id', string='Channels', copy=False)
 
     @api.multi
     def message_get_suggested_recipients(self):
