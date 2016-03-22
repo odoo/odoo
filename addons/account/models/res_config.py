10846a6ff133d75361af18b15fec051a722bdc92
Cedric Snauwaert <csn@openerp.com>
2016-03-22 11:52:00 +0100

    [ADD] account_bank_import_statement_csv: add module to import csv bank statement

--- addons/account/models/res_config.py
+++ addons/account/models/res_config.py
@@ -140,6 +140,9 @@ class AccountConfigSettings(models.TransientModel):
     module_account_bank_statement_import_ofx = fields.Boolean("Import in .ofx format",
         help='Get your bank statements from your bank and import them in Odoo in the .OFX format.\n'
             '-This installs the module account_bank_statement_import_ofx.')
+    module_account_bank_statement_import_csv = fields.Boolean("Import in .csv format",
+        help='Get your bank statements from your bank and import them in Odoo in the .CSV format.\n'
+            '-This installs the module account_bank_statement_import_csv.')
 
 
     @api.model
