31e5389462bcfd57505aef74d888ec72456325fe
Thibault Delavallée <tde@openerp.com>
2016-03-22 13:08:45 +0100

    [MIGR] stock: barcode, settings, planner and res_*
    Those models are migrated to new API. No change is performed to code
    structure or functional structure. Some code cleaning may be performed when
    migrating to simplify the code. Other files may be updated due to the move
    towards the new API.

--- addons/stock/models/barcode.py
+++ addons/stock/models/barcode.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
-from odoo.tools.translate import _
+from odoo import models, _
 
 
-class barcode_rule(models.Model):
+class BarcodeRule(models.Model):
     _inherit = 'barcode.rule'
 
     def _get_type_selection(self):
-        types = set(super(barcode_rule, self)._get_type_selection())
+        types = set(super(BarcodeRule, self)._get_type_selection())
         types.update([
             ('weight', _('Weighted Product')),
             ('location', _('Location')),
--- addons/stock/models/web_planner.py
+++ addons/stock/models/web_planner.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
-from openerp import models
+
+from odoo import models
 
 
 class PlannerInventory(models.Model):
-
     _inherit = 'web.planner'
 
     def _get_planner_application(self):
         planner = super(PlannerInventory, self)._get_planner_application()
         planner.append(['planner_inventory', 'Inventory Planner'])
         return planner
-
-    def _prepare_planner_inventory_data(self):
-        # TDE note: void method kept (stable version), probably to remove in master
-        return {
-        }
