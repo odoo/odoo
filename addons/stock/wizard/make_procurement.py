e3b8fe202401e7a11938329373d94d7b60794088
Thibault Delavallée <tde@openerp.com>
2016-03-22 13:08:49 +0100

    [MIGR] stock: wizards
    Wizard models are migrated to new API. No change is performed to code
    structure or functional structure. Some code cleaning may be performed when
    migrating to simplify the code. Other files may be updated due to the move
    towards the new API.

--- addons/stock/wizard/make_procurement.py
+++ addons/stock/wizard/make_procurement.py
@@ -1,107 +1,85 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo import api, fields, models
 
-from openerp.osv import fields, osv
 
-
-class make_procurement(osv.osv_memory):
+class MakeProcurement(models.TransientModel):
     _name = 'make.procurement'
     _description = 'Make Procurements'
-    
-    def onchange_product_id(self, cr, uid, ids, prod_id, context=None):
-        product = self.pool.get('product.product').browse(cr, uid, prod_id, context=context)
-        return {'value': {
+
+    qty = fields.Float('Quantity', default=1.0, digits=(16, 2), required=True)
+    res_model = fields.Char('Res Model')
+    product_id = fields.Many2one('product.product', 'Product', required=True)
+    product_tmpl_id = fields.Many2one('product.template', 'Template', required=True)
+    product_variant_count = fields.Integer(string='Variant Number', related='product_tmpl_id.product_variant_count')
+    uom_id = fields.Many2one('product.uom', 'Unit of Measure', required=True)
+    warehouse_id = fields.Many2one('stock.warehouse', 'Warehouse', required=True)
+    date_planned = fields.Date('Planned Date', default=fields.Date.context_today, required=True)
+    route_ids = fields.Many2many('stock.location.route', string='Preferred Routes')
+
+    @api.model
+    def default_get(self, fields):
+        # TDE FIXME: clean this method, overly complicated (onchange ?)
+        res = super(MakeProcurement, self).default_get(fields)
+
+        if self.env.context.get('active_id') and self.env.context.get('active_model') == 'product.template':
+            product = self.env['product.product'].search([('product_tmpl_id', '=', self.env.context['active_id'])], limit=1)
+        elif self.env.context.get('active_id') and self.env.context.get('active_model') == 'product.product':
+            product = self.env['product.product'].browse(self.env.context['active_id'])
+        else:
+            product = self.env['product.product']
+        if 'product_id' in fields and not res.get('product_id') and product:
+            res['product_id'] = product.id
+        if 'product_tmpl_id' in fields and not res.get('product_tmpl_id') and product:
+            res['product_tmpl_id'] = product.product_tmpl_id.id
+        if 'uom_id' in fields and not res.get('uom_id') and product:
+            res['uom_id'] = product.uom_id.id
+        if 'warehouse_id' in fields and not res.get('warehouse_id'):
+            res['warehouse_id'] = self.env['stock.warehouse'].search([], limit=1).id
+        return res
+
+    def onchange_product_id_dict(self, product_id):
+        product = self.env['product.product'].browse(product_id)
+        return {
             'uom_id': product.uom_id.id,
             'product_tmpl_id': product.product_tmpl_id.id,
             'product_variant_count': product.product_tmpl_id.product_variant_count
-        }}
-    
-    _columns = {
-        'qty': fields.float('Quantity', digits=(16,2), required=True),
-        'res_model': fields.char('Res Model'),
-        'product_id': fields.many2one('product.product', 'Product', required=True),
-        'product_tmpl_id': fields.many2one('product.template', 'Template', required=True),
-        'product_variant_count': fields.related('product_tmpl_id', 'product_variant_count', type='integer', string='Variant Number'),
-        'uom_id': fields.many2one('product.uom', 'Unit of Measure', required=True),
-        'warehouse_id': fields.many2one('stock.warehouse', 'Warehouse', required=True),
-        'date_planned': fields.date('Planned Date', required=True),
-        'route_ids': fields.many2many('stock.location.route', string='Preferred Routes'),
-    }
+        }
 
-    _defaults = {
-        'date_planned': fields.date.context_today,
-        'qty': lambda *args: 1.0,
-    }
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        if self.product_id:
+            for key, value in self.onchange_product_id_dict(self.product_id.id).iteritems():
+                setattr(self, key, value)
 
-    def make_procurement(self, cr, uid, ids, context=None):
+    @api.model
+    def create(self, values):
+        if values.get('product_id'):
+            values.update(self.onchange_product_id_dict(values['product_id']))
+        return super(MakeProcurement, self).create(values)
+
+    @api.multi
+    def make_procurement(self):
         """ Creates procurement order for selected product. """
-        user = self.pool.get('res.users').browse(cr, uid, uid, context=context).login
-        wh_obj = self.pool.get('stock.warehouse')
-        procurement_obj = self.pool.get('procurement.order')
-        data_obj = self.pool.get('ir.model.data')
-
-        for proc in self.browse(cr, uid, ids, context=context):
-            wh = wh_obj.browse(cr, uid, proc.warehouse_id.id, context=context)
-            procure_id = procurement_obj.create(cr, uid, {
-                'name':'INT: '+str(user),
-                'date_planned': proc.date_planned,
-                'product_id': proc.product_id.id,
-                'product_qty': proc.qty,
-                'product_uom': proc.uom_id.id,
-                'warehouse_id': proc.warehouse_id.id,
-                'location_id': wh.lot_stock_id.id,
-                'company_id': wh.company_id.id,
-                'route_ids': [(6, 0, proc.route_ids.ids)],
-            })
-            procurement_obj.signal_workflow(cr, uid, [procure_id], 'button_confirm')
-
-        id2 = data_obj._get_id(cr, uid, 'procurement', 'procurement_tree_view')
-        id3 = data_obj._get_id(cr, uid, 'procurement', 'procurement_form_view')
-
-        if id2:
-            id2 = data_obj.browse(cr, uid, id2, context=context).res_id
-        if id3:
-            id3 = data_obj.browse(cr, uid, id3, context=context).res_id
-
+        ProcurementOrder = self.env['procurement.order']
+        for wizard in self:
+            procurement = ProcurementOrder.create({
+                'name': 'INT: %s' % (self.env.user.login),
+                'date_planned': wizard.date_planned,
+                'product_id': wizard.product_id.id,
+                'product_qty': wizard.qty,
+                'product_uom': wizard.uom_id.id,
+                'warehouse_id': wizard.warehouse_id.id,
+                'location_id': wizard.warehouse_id.lot_stock_id.id,
+                'company_id': wizard.warehouse_id.company_id.id,
+                'route_ids': [(6, 0, wizard.route_ids.ids)]})
+            procurement.signal_workflow('button_confirm')
         return {
             'view_type': 'form',
             'view_mode': 'tree,form',
             'res_model': 'procurement.order',
-            'res_id' : procure_id,
-            'views': [(id3,'form'),(id2,'tree')],
+            'res_id': procurement.id,
+            'views': [(False, 'form'), (False, 'tree')],
             'type': 'ir.actions.act_window',
-         }
-
-    def default_get(self, cr, uid, fields, context=None):
-        if context is None:
-            context = {}
-        record_id = context.get('active_id')
-
-        if context.get('active_model') == 'product.template':
-            product_ids = self.pool.get('product.product').search(cr, uid, [('product_tmpl_id', '=', context.get('active_id'))], context=context)
-            if product_ids:
-                record_id = product_ids[0]
-
-        res = super(make_procurement, self).default_get(cr, uid, fields, context=context)
-
-        if record_id and 'product_id' in fields:
-            proxy = self.pool.get('product.product')
-            product_ids = proxy.search(cr, uid, [('id', '=', record_id)], context=context, limit=1)
-            if product_ids:
-                product_id = product_ids[0]
-
-                product = self.pool.get('product.product').browse(cr, uid, product_id, context=context)
-                res['product_id'] = product.id
-                res['uom_id'] = product.uom_id.id
-
-        if 'warehouse_id' in fields:
-            warehouse_id = self.pool.get('stock.warehouse').search(cr, uid, [], context=context)
-            res['warehouse_id'] = warehouse_id[0] if warehouse_id else False
-
-        return res
-
-    def create(self, cr, uid, values, context=None):
-        if values.get('product_id'):
-            values.update(self.onchange_product_id(cr, uid, None, values['product_id'], context=context)['value'])
-        return super(make_procurement, self).create(cr, uid, values, context=context)
+        }
