ffc5b1058ad383484bb29e86e780645448579a49
David Monjoie <dmo@odoo.com>
2016-03-24 13:49:22 +0100

    [IMP] sale: improved pagebreak mechanism

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -422,21 +422,26 @@ class SaleOrder(models.Model):
             order.project_id = analytic
 
     @api.multi
-    def layout_categories(self):
+    def order_lines_layouted(self):
         """
-        Returns this order lines ordered by sale_layout_category sequence. Used to render the report.
+        Returns this order lines classified by sale_layout_category and separated in
+        pages according to the category pagebreaks. Used to render the report.
         """
         self.ensure_one()
-        grouped_lines = []
+        report_pages = [[]]
         for category, lines in groupby(self.order_line, lambda l: l.layout_category_id):
-            grouped_lines.append({
+            # If last added category induced a pagebreak, this one will be on a new page
+            if report_pages[-1] and report_pages[-1][-1]['pagebreak']:
+                report_pages.append([])
+            # Append category to current report page
+            report_pages[-1].append({
                 'name': category and category.name or 'Uncategorized',
                 'subtotal': category and category.subtotal,
                 'pagebreak': category and category.pagebreak,
                 'lines': list(lines)
             })
 
-        return grouped_lines
+        return report_pages
 
 
 class SaleOrderLine(models.Model):
@@ -837,21 +842,25 @@ class AccountInvoice(models.Model):
         return res
 
     @api.multi
-    def layout_categories(self):
+    def order_lines_layouted(self):
         """
         Returns this sale order lines ordered by sale_layout_category sequence. Used to render the report.
         """
         self.ensure_one()
-        grouped_lines = []
+        report_pages = [[]]
         for category, lines in groupby(self.invoice_line_ids, lambda l: l.layout_category_id):
-            grouped_lines.append({
+            # If last added category induced a pagebreak, this one will be on a new page
+            if report_pages[-1] and report_pages[-1][-1]['pagebreak']:
+                report_pages.append([])
+            # Append category to current report page
+            report_pages[-1].append({
                 'name': category and category.name or 'Uncategorized',
                 'subtotal': category and category.subtotal,
                 'pagebreak': category and category.pagebreak,
                 'lines': list(lines)
             })
 
-        return grouped_lines
+        return report_pages
 
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
