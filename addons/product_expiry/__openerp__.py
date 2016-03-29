71fae64534983977d3f46817cd7eac0f7cd1ba56
Srushti Patel <shp@odoo.com>
2016-03-29 11:11:55 +0530

    [MIG] product_expiry: migration to new api
    Thanks rgo-odoo for the _get_dates method and the removal of the context abuse.

--- addons/product_expiry/__openerp__.py
+++ addons/product_expiry/__openerp__.py
@@ -1,7 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name' : 'Products Expiration Date',
-    'version' : '1.0',
     'category' : 'Specific Industry Applications',
     'depends' : ['stock'],
     'demo' : ['data/product_expiry_demo.xml'],
@@ -18,10 +17,8 @@ Following dates can be tracked:
 
 Also implements the removal strategy First Expiry First Out (FEFO) widely used, for example, in food industries.
 """,
-    'data': ['views/product_expiry_view.xml',
+    'data': ['views/production_lot_views.xml',
              'views/product_template_views.xml',
              'views/stock_quant_views.xml',
              'data/product_expiry_data.xml'],
-    'auto_install': False,
-    'installable': True,
 }
--- addons/product_expiry/models/stock_quant.py
+++ addons/product_expiry/models/stock_quant.py
@@ -1,29 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import datetime
-
-import openerp
-from openerp.osv import fields, osv
+from odoo import api, fields, models
 
 
-class stock_quant(osv.osv):
+class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    def _get_quants(self, cr, uid, ids, context=None):
-        return self.pool.get('stock.quant').search(cr, uid, [('lot_id', 'in', ids)], context=context)
+    removal_date = fields.Datetime(related='lot_id.removal_date', store=True)
 
-    _columns = {
-        'removal_date': fields.related('lot_id', 'removal_date', type='datetime', string='Removal Date',
-            store={
-                'stock.quant': (lambda self, cr, uid, ids, ctx: ids, ['lot_id'], 20),
-                'stock.production.lot': (_get_quants, ['removal_date'], 20),
-            }),
-    }
-
-    def apply_removal_strategy(self, cr, uid, qty, move, ops=False, domain=None, removal_strategy='fifo', context=None):
+    @api.model
+    def apply_removal_strategy(self, qty, move, ops=False, domain=None, removal_strategy='fifo'):
         if removal_strategy == 'fefo':
-            order = 'removal_date, in_date, id'
-            return self._quants_get_order(cr, uid, qty, move, ops=ops, domain=domain, orderby=order, context=context)
-        return super(stock_quant, self).apply_removal_strategy(cr, uid, qty, move, ops=ops, domain=domain,
-                                                               removal_strategy=removal_strategy, context=context)
+            return self._quants_get_order(qty, move, ops=ops, domain=domain, orderby='removal_date, in_date, id')
+        return super(StockQuant, self).apply_removal_strategy(qty, move, ops=ops, domain=domain, removal_strategy=removal_strategy)
