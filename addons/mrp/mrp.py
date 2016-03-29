3c069d98fa9ccfb44f2b7899a819c8ba0697a877
scosist <scosist@asphaltzipper.com>
2016-03-28 19:06:14 -0600

    remove MO related changes to eliminate risk

--- addons/mrp/mrp.py
+++ addons/mrp/mrp.py
@@ -703,15 +703,13 @@ class mrp_production(osv.osv):
 
 
     def _prepare_lines(self, cr, uid, production, properties=None, context=None):
-        ctx = context and context.copy() or {}
-        ctx['bom_effectivity_date'] = production.date_planned
         # search BoM structure and route
         bom_obj = self.pool.get('mrp.bom')
         uom_obj = self.pool.get('product.uom')
         bom_point = production.bom_id
         bom_id = production.bom_id.id
         if not bom_point:
-            bom_id = bom_obj._bom_find(cr, uid, product_id=production.product_id.id, properties=properties, context=ctx)
+            bom_id = bom_obj._bom_find(cr, uid, product_id=production.product_id.id, properties=properties, context=context)
             if bom_id:
                 bom_point = bom_obj.browse(cr, uid, bom_id)
                 routing_id = bom_point.routing_id.id or False
@@ -723,7 +721,7 @@ class mrp_production(osv.osv):
         # get components and workcenter_lines from BoM structure
         factor = uom_obj._compute_qty(cr, uid, production.product_uom.id, production.product_qty, bom_point.product_uom.id)
         # product_lines, workcenter_lines
-        return bom_obj._bom_explode(cr, uid, bom_point, production.product_id, factor / bom_point.product_qty, properties, routing_id=production.routing_id.id, context=ctx)
+        return bom_obj._bom_explode(cr, uid, bom_point, production.product_id, factor / bom_point.product_qty, properties, routing_id=production.routing_id.id, context=context)
 
 
     def _action_compute_lines(self, cr, uid, ids, properties=None, context=None):
--- addons/mrp/procurement.py
+++ addons/mrp/procurement.py
@@ -45,11 +45,9 @@ class procurement_order(osv.osv):
         @return: True or False
         """
         for procurement in self.browse(cr, uid, ids, context=context):
-            ctx = context and context.copy() or {}
-            ctx['bom_effectivity_date'] = self._get_date_planned(cr, uid, procurement, context=context).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
             properties = [x.id for x in procurement.property_ids]
             bom_id = self.pool.get('mrp.bom')._bom_find(cr, uid, product_id=procurement.product_id.id,
-                                                        properties=properties, context=ctx)
+                                                        properties=properties, context=context)
             if not bom_id:
                 return False
         return True
@@ -69,12 +67,9 @@ class procurement_order(osv.osv):
             bom_id = procurement.bom_id.id
             routing_id = procurement.bom_id.routing_id.id
         else:
-            ctx = context and context.copy() or {}
-            ctx['bom_effectivity_date'] = newdate.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-            ctx['company_id'] = procurement.company_id.id
             properties = [x.id for x in procurement.property_ids]
             bom_id = bom_obj._bom_find(cr, uid, product_id=procurement.product_id.id,
-                                       properties=properties, context=ctx)
+                                       properties=properties, context=context)
             bom = bom_obj.browse(cr, uid, bom_id, context=context)
             routing_id = bom.routing_id.id
         return {
