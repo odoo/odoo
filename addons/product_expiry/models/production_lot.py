e5fcfd76b2bcadfb5c2d571aa28083b56c132f39
Srushti Patel <shp@odoo.com>
2016-03-29 11:11:55 +0530

    [MIG] product_expiry: migration to new api
    Thanks rgo-odoo for the _get_dates method and the removal of the context abuse.

--- addons/product_expiry/models/production_lot.py
+++ addons/product_expiry/models/production_lot.py
@@ -1,52 +1,47 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 import datetime
-
-import openerp
-from openerp.osv import fields, osv
+from odoo import api, fields, models
 
 
-class stock_production_lot(osv.osv):
+class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
 
-    def _get_date(dtype):
-        """Return a function to compute the limit date for this type"""
-        def calc_date(self, cr, uid, context=None):
-            """Compute the limit date for a given date"""
-            if context is None:
-                context = {}
-            if not context.get('product_id', False):
-                date = False
-            else:
-                product = openerp.registry(cr.dbname)['product.product'].browse(
-                    cr, uid, context['product_id'])
-                duration = getattr(product, dtype)
-                # set date to False when no expiry time specified on the product
-                date = duration and (datetime.datetime.today()
-                    + datetime.timedelta(days=duration))
-            return date and date.strftime('%Y-%m-%d %H:%M:%S') or False
-        return calc_date
+    life_date = fields.Datetime(string='End of Life Date',
+        help='This is the date on which the goods with this Serial Number may become dangerous and must not be consumed.')
+    use_date = fields.Datetime(string='Best before Date',
+        help='This is the date on which the goods with this Serial Number start deteriorating, without being dangerous yet.')
+    removal_date = fields.Datetime(string='Removal Date',
+        help='This is the date on which the goods with this Serial Number should be removed from the stock.')
+    alert_date = fields.Datetime(string='Alert Date',
+        help="This is the date on which an alert should be notified about the goods with this Serial Number.")
 
-    _columns = {
-        'life_date': fields.datetime('End of Life Date',
-            help='This is the date on which the goods with this Serial Number may become dangerous and must not be consumed.'),
-        'use_date': fields.datetime('Best before Date',
-            help='This is the date on which the goods with this Serial Number start deteriorating, without being dangerous yet.'),
-        'removal_date': fields.datetime('Removal Date',
-            help='This is the date on which the goods with this Serial Number should be removed from the stock.'),
-        'alert_date': fields.datetime('Alert Date',
-            help="This is the date on which an alert should be notified about the goods with this Serial Number."),
-    }
+    def _get_dates(self, product_id=None):
+        """Returns dates based on number of days configured in current lot's product."""
+        mapped_fields = {
+            'life_date': 'life_time',
+            'use_date': 'use_time',
+            'removal_date': 'removal_time',
+            'alert_date': 'alert_time'
+        }
+        res = dict.fromkeys(mapped_fields.keys(), False)
+        product = self.env['product.product'].browse(product_id) or self.product_id
+        if product:
+            for field in mapped_fields.keys():
+                duration = getattr(product, mapped_fields[field])
+                if duration:
+                    date = datetime.datetime.now() + datetime.timedelta(days=duration)
+                    res[field] = fields.Datetime.to_string(date)
+        return res
+
     # Assign dates according to products data
-    def create(self, cr, uid, vals, context=None):
-        context = dict(context or {})
-        context['product_id'] = vals.get('product_id', context.get('default_product_id'))
-        return super(stock_production_lot, self).create(cr, uid, vals, context=context)
+    @api.model
+    def create(self, vals):
+        vals.update(self._get_dates(vals.get('product_id')))
+        return super(StockProductionLot, self).create(vals)
 
-    _defaults = {
-        'life_date': _get_date('life_time'),
-        'use_date': _get_date('use_time'),
-        'removal_date': _get_date('removal_time'),
-        'alert_date': _get_date('alert_time'),
-    }
+    @api.onchange('product_id')
+    def _onchange_product(self):
+        dates_dict = self._get_dates()
+        for field, value in dates_dict.items():
+            setattr(self, field, value)
