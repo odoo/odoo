25e15c79dbcfab8b9e5706c99dda4d8c13f318a8
fka-odoo <fka@openerp.com>
2016-03-30 17:09:50 +0200

    [MIG] hr_contract : migrate the code to new api

--- addons/hr_contract/__openerp__.py
+++ addons/hr_contract/__openerp__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
 {
     'name': 'Employee Contracts',
     'version': '1.0',
@@ -21,9 +20,9 @@ You can assign several contracts per employee.
     'depends': ['base_action_rule', 'hr'],
     'data': [
         'security/ir.model.access.csv',
-        'views/hr_contract_views.xml',
         'data/hr_contract_data.xml',
         'views/base_action_rule_views.xml',
+        'views/hr_contract_views.xml',
     ],
     'demo': [],
     'test': ['test/test_hr_contract.yml'],
