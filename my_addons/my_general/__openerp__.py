57976ff5cf01a642187f0cc4d4236f2c6a626789
truongvinhtrinh <vinhtrinh02dn@gmail.com>
2016-03-30 22:21:58 +0700

    add my_general module

---
+++ my_addons/my_general/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Trinh General Customize',
+    'version': '1.0',
+    'category': 'Sales Management',
+    'sequence': 15,
+    'summary': 'Trinh general customize',
+    'description': """
+    To:
+    -- invisible invoicing_policy field for product form
+    """,
+    'website': 'https://www.odoo.com',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_view.xml',
+    ],
+    'demo': [],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
