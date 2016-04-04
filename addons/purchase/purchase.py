a0e04afe620aaf13788da90f1bcf5f8b1ab859f5
Nicolas Martinelli <nim@odoo.com>
2016-04-04 11:04:40 +0200

    [FIX] purchase: supplier currency
    When a supplier is added, the potential change of currency should be
    taken into account.
    
    opw-672862

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -351,13 +351,14 @@ class PurchaseOrder(models.Model):
             # Do not add a contact as a supplier
             partner = self.partner_id if not self.partner_id.parent_id else self.partner_id.parent_id
             if partner not in line.product_id.seller_ids.mapped('name') and len(line.product_id.seller_ids) <= 10:
+                currency = partner.property_purchase_currency_id or self.env.user.company_id.currency_id
                 supplierinfo = {
                     'name': partner.id,
                     'sequence': max(line.product_id.seller_ids.mapped('sequence')) + 1 if line.product_id.seller_ids else 1,
                     'product_uom': line.product_uom.id,
                     'min_qty': 0.0,
-                    'price': line.price_unit,
-                    'currency_id': partner_id.currency_id.id,
+                    'price': self.currency_id.compute(line.price_unit, currency),
+                    'currency_id': currency.id,
                     'delay': 0,
                 }
                 vals = {
