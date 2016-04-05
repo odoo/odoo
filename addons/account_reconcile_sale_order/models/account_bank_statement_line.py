3bb5408d9aa2e18b9ed208df030cf3b12eae00ec
pvy <pvy@odoo.com>
2016-04-05 16:51:34 +0200

    [IMP] account_reconcile_sale_order: Always make the user select the SO through a dialog

--- addons/account_reconcile_sale_order/models/account_bank_statement_line.py
+++ addons/account_reconcile_sale_order/models/account_bank_statement_line.py
@@ -2,8 +2,6 @@
 
 from openerp import models
 
-def matches_sale_order_payment_memo_pattern(string):
-    return string.lower().startswith('so') and string[2:].isdigit()
 
 class AccountBankStatementLine(models.Model):
     _inherit = "account.bank.statement.line"
@@ -13,7 +11,7 @@ class AccountBankStatementLine(models.Model):
             made to pay an open sales order.
         """
         # TOCHECK: allow it if the memo == the counterpart ref ? (so, if the SO was invoiced but is not yet in state done)
-        if self.find_matching_sale_order():
+        if self.find_matching_sale_orders():
             return None
         return super(AccountBankStatementLine, self)._get_move_lines_for_auto_reconcile()
 
@@ -22,33 +20,23 @@ class AccountBankStatementLine(models.Model):
             and use potential SO's invoices as reconciliation proposition.
         """
         ret = super(AccountBankStatementLine, self).get_data_for_single_reconciliation_widget(excluded_move_line_ids)
-        sale_order = self.find_matching_sale_order()
-        if sale_order:
-            if not self.partner_id:
-                self.partner_id = sale_order.partner_invoice_id
-            ret['sale_order_id'] = sale_order.id
-            ret['reconciliation_proposition'] = sale_order.get_move_lines_for_reconciliation_widget(self.id)
+        sale_orders = self.find_matching_sale_orders()
+        if sale_orders:
+            ret['sale_order_ids'] = sale_orders.ids
+            #ret['reconciliation_proposition'] = sale_order.get_move_lines_for_reconciliation_widget(self.id)
         return ret
 
-    def find_matching_sale_order(self):
-        """ Try to find a sales order this transaction was made to pay.
+    def find_matching_sale_orders(self):
+        """ Try to find sales orders this transaction was made to pay.
             Eg. a e-commerce user pays an order by wire transfer.
         """
         self.ensure_one()
-        if not matches_sale_order_payment_memo_pattern(self.name):
-            return None
 
         domain_so = [
             # TOCHECK: filter on payment acquirer ?
-            '&', ('state', 'in', ('sent', 'sale')),
-            '|', ('name', '=ilike', self.name),
-            ('client_order_ref', '=ilike', self.name)]
+            ('state', 'in', ('sent', 'sale'))]
         if self.partner_id:
             domain_so += [('partner_invoice_id', '=', self.partner_id.id)]
-        sale_order = self.env['sale.order'].search(domain_so)
+        sale_orders = self.env['sale.order'].search(domain_so)
 
-        # TOCHECK: could that happen or can we drop this code
-        if len(sale_order) != 1:
-            return None
-
-        return sale_order
+        return sale_orders
--- addons/account_reconcile_sale_order/static/src/js/account_reconciliation_widgets.js
+++ addons/account_reconcile_sale_order/static/src/js/account_reconciliation_widgets.js
@@ -2,10 +2,12 @@ odoo.define('account_reconcile_sale_order.reconciliation_custom', function (requ
 "use strict";
 
 var Model = require('web.Model');
+var form_common = require('web.form_common');
 var reconciliation_widgets = require('account.reconciliation');
 var core = require('web.core');
 var _ = require('_');
 var _t = core._t;
+var SelectCreateDialog = form_common.SelectCreateDialog;
 
 reconciliation_widgets.bankStatementReconciliationLine.include({
     events: _.defaults({
@@ -19,7 +21,7 @@ reconciliation_widgets.bankStatementReconciliationLine.include({
         // in its parent (the client action), so when the user wants to open a reconciliation's
         // matched sale order, we just reuse the action and change its res_id.
         var parent = this.getParent();
-        if (!parent.action_open_sale_order && this.init_data && this.init_data.sale_order_id) {
+        if (!parent.action_open_sale_order && this.init_data && this.init_data.sale_order_ids) {
             new Model("ir.actions.act_window")
                 .call("for_xml_id", ['account_reconcile_sale_order', 'action_sale_order'])
                 .done(function(action) { parent.action_open_sale_order = action })
@@ -31,32 +33,43 @@ reconciliation_widgets.bankStatementReconciliationLine.include({
         var self = this;
         // Prevent the widget from going to 'match' mode
         e.stopPropagation();
-        var action = this.getParent().action_open_sale_order;
-        action.res_id = this.init_data.sale_order_id;
-        // When the user comes back, after possibly having generated an invoice for the sale order,
-        // select the reconciliable move lines from the sale order's invoice(s) which are not already
-        // selected or selected elsewhere.
-        this.do_action(action, { on_reverse_breadcrumb: function() {
-            new Model("sale.order")
-                .call("get_move_lines_for_reconciliation_widget", [self.init_data.sale_order_id, self.st_line.id])
-                .then(function(lines) {
-                    if (lines.length === 0) {
-                        self.do_warn(_t("There is no open invoice for the sale order."));
-                        return;
-                    }
-                    // Filter out lines already selected (here or in another reconciliation)
-                    var excluded_ids = _.reduce(self.getParent().excluded_move_lines_ids, function(memo, val) { return memo.concat(val) }, []);
-                    lines = _.filter(lines, function(line) { return excluded_ids.indexOf(line.id) === -1 });
-                    // Remove from mv_lines_deselected the lines we are about to select
-                    var line_ids = _.map(lines, function(line) { return line.id });
-                    self.mv_lines_deselected = _.filter(self.mv_lines_deselected, function(line){ return line_ids.indexOf(line.id) === -1 });
-                    // Put the SO's reconciliable invoice lines in the selected lines, without duplicate
-                    var mv_lines_selected = _.uniq(_.union(self.get("mv_lines_selected"), lines), false, _.property('id'));
-                    _.each(lines, function(line) { self.decorateMoveLine(line) }, self);
-                    self.set("mv_lines_selected", mv_lines_selected);
+
+        new SelectCreateDialog(this, {
+            res_model: 'sale.order',
+            context: {search_default_partner_id: [this.init_data.line.partner_id], search_default_sent_or_sale: 1},
+            title: _t("Select a sale order"),
+            initial_view: "search",
+            disable_multiple_selection: true,
+            no_create: true,
+            on_selected: function(element_ids) {
+                var action = self.getParent().action_open_sale_order;
+                action.res_id = element_ids[0];
+                // When the user comes back, after possibly having generated an invoice for the sale order,
+                // select the reconciliable move lines from the sale order's invoice(s) which are not already
+                // selected or selected elsewhere.
+                self.do_action(action, { on_reverse_breadcrumb: function() {
+                    new Model("sale.order")
+                        .call("get_move_lines_for_reconciliation_widget", [element_ids[0], self.st_line.id])
+                        .then(function(lines) {
+                            if (lines.length === 0) {
+                                self.do_warn(_t("There is no open invoice for the sale order."));
+                                return;
+                            }
+                            // Filter out lines already selected (here or in another reconciliation)
+                            var excluded_ids = _.reduce(self.getParent().excluded_move_lines_ids, function(memo, val) { return memo.concat(val) }, []);
+                            lines = _.filter(lines, function(line) { return excluded_ids.indexOf(line.id) === -1 });
+                            // Remove from mv_lines_deselected the lines we are about to select
+                            var line_ids = _.map(lines, function(line) { return line.id });
+                            self.mv_lines_deselected = _.filter(self.mv_lines_deselected, function(line){ return line_ids.indexOf(line.id) === -1 });
+                            // Put the SO's reconciliable invoice lines in the selected lines, without duplicate
+                            var mv_lines_selected = _.uniq(_.union(self.get("mv_lines_selected"), lines), false, _.property('id'));
+                            _.each(lines, function(line) { self.decorateMoveLine(line) }, self);
+                            self.set("mv_lines_selected", mv_lines_selected);
+                        });
+                    },
                 });
-            },
-        });
+            }
+        }).open();
     },
 });
 
