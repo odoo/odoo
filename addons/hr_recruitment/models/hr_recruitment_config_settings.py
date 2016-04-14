884c35f552eb2efd856bc9431d677636d516daa6
Jérome Maes <jem@openerp.com>
2016-04-14 13:20:44 +0200

    [REM] fetchmail : remove auto magic field to configure fetchmail server
    This code was used to automatically configure a fetchmail server, according to
    a model. This followed a name convention, like 'module_', 'group_', ...
    
    This mechanism was replaced by mail.alias system, and it not used anymore
    by odoo modules.
    
    It is time to remove this old hacky stuffs : fetchmail.config.settings
    model is removed.

--- addons/hr_recruitment/models/hr_recruitment_config_settings.py
+++ addons/hr_recruitment/models/hr_recruitment_config_settings.py
@@ -6,7 +6,7 @@ from openerp import api, fields, models
 
 class RecruitmentSettings(models.TransientModel):
     _name = 'hr.recruitment.config.settings'
-    _inherit = ['res.config.settings', 'fetchmail.config.settings']
+    _inherit = ['res.config.settings']
 
     module_document = fields.Selection(selection=[
             (0, "Do not manage CVs and motivation letter"),
