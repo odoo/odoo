8203a4692ff9f65328685eae7d9e1d862b04e8a9
Vipul Bhatt <vbh@odoo.com>
2016-04-19 17:24:36 +0530

    [IMP] point_of_sale: Report performance issues 	- removed function call from the report, report rendered with ready data 	- tag <openerp> to <odoo>, some migration change

--- addons/point_of_sale/report/__init__.py
+++ addons/point_of_sale/report/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import account_statement
 import pos_invoice
 import pos_order_report
 import pos_receipt
