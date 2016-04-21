odoo/odoo

commit 8395f0d217e16df18fcc6f4309fe74b0dd8d50a2

[FIX] website_event: event dates within event timezone
The dates of an event on the website was always
within the user timezone. If not signed in,
it was within the public user timezone
(which is quite not user-friendly to change)

For this purpose, we weed get the context
from the record (the context used when using
`record.with_context`), and therefore
the QWeb field
`ir.qweb.field.datetime` is slightly altered,
to use the context from the record.

opw-675427

openerp/addons/base/ir/ir_qweb.py
@@ -712,12 +712,6 @@ class DateTimeConverter(osv.AbstractModel):
         lang = self.user_lang(cr, uid, context=context)
         locale = babel.Locale.parse(lang.code)
 
-        if isinstance(value, basestring):
-            value = datetime.datetime.strptime(
-                value, openerp.tools.DEFAULT_SERVER_DATETIME_FORMAT)
-        value = fields.datetime.context_timestamp(
-            cr, uid, timestamp=value, context=context)
-
         if options and 'format' in options:
             pattern = options['format']
         else:
@@ -728,6 +722,17 @@ class DateTimeConverter(osv.AbstractModel):
             pattern = pattern.replace(":ss", "").replace(":s", "")
 
         return babel.dates.format_datetime(value, format=pattern, locale=locale)
+
+    def record_to_html(self, cr, uid, field_name, record, options, context=None):
+        value = record[field_name]
+        if isinstance(value, basestring):
+            value = datetime.datetime.strptime(
+                value, openerp.tools.DEFAULT_SERVER_DATETIME_FORMAT)
+        value = fields.datetime.context_timestamp(
+            cr, uid, timestamp=value, context=record.env.context)
+        field = field = record._fields[field_name]
+        return self.value_to_html(
+            cr, uid, value, field, options=options, context=context)
 
 class TextConverter(osv.AbstractModel):
     _name = 'ir.qweb.field.text'

addons/website_event/views/website_event.xml
@@ -66,7 +66,7 @@
                                     </t>
                                 </div>
                                 <div>
-                                    <i class="fa fa-clock-o"></i> <span itemprop="startDate" t-field="event.date_begin" t-field-options='{"hide_seconds":"True"}'> </span> <i>to</i> <span itemprop="endDate" t-field="event.date_end" t-field-options='{"hide_seconds":"True"}'> </span>
+                                    <i class="fa fa-clock-o"></i> <span itemprop="startDate" t-field="event.with_context(tz=event.date_tz).date_begin" t-field-options='{"hide_seconds":"True"}'> </span> <i>to</i> <span itemprop="endDate" t-field="event.with_context(tz=event.date_tz).date_end" t-field-options='{"hide_seconds":"True"}'> </span>
                                 </div>
                                 <div itemprop="location" t-field="event.address_id" t-field-options='{
                                     "widget": "contact",
@@ -257,8 +257,8 @@
         <div itemscope="itemscope" itemtype="http://schema.org/Event" class="container">
             <h1 itemprop="name" class="text-center" t-field="event.name"></h1>
             <h4 class="text-center text-muted">
-                <i class="fa fa-clock-o"></i> <span itemprop="startDate" t-field="event.date_begin" t-field-options='{"hide_seconds":"True"}'/> to
-                <span itemprop="endDate" t-field="event.date_end" t-field-options='{"hide_seconds":"True"}'/>
+                <i class="fa fa-clock-o"></i> <span itemprop="startDate" t-field="event.with_context(tz=event.date_tz).date_begin" t-field-options='{"hide_seconds":"True"}'/> to
+                <span itemprop="endDate" t-field="event.with_context(tz=event.date_tz).date_end" t-field-options='{"hide_seconds":"True"}'/>
             </h4>
             <h4 class="text-center text-muted"
                 t-field="event.address_id" t-field-options='{
@@ -330,8 +330,8 @@
                     <h4>When</h4>
                 </div>
                 <div class="panel-body">
-                    <i class="fa fa-clock-o"></i> From <span t-field="event.date_begin" t-field-options='{"hide_seconds":"True"}'> </span><br/>
-                    <i class="fa fa-clock-o"></i> To <span t-field="event.date_end" t-field-options='{"hide_seconds":"True"}'> </span>
+                    <i class="fa fa-clock-o"></i> From <span t-field="event.with_context(tz=event.date_tz).date_begin" t-field-options='{"hide_seconds":"True"}'> </span><br/>
+                    <i class="fa fa-clock-o"></i> To <span t-field="event.with_context(tz=event.date_tz).date_end" t-field-options='{"hide_seconds":"True"}'> </span>
                 </div>
             </div>
 

commit 28e0a1771cb2a56308b2d83d0269af2da16198e6

[FIX] ir_qweb: regression of 8395f0d217e16df18fcc6f4309fe74b0dd8d50a2
This revision corrects a regression introduced by
8395f0d217e16df18fcc6f4309fe74b0dd8d50a2:
the possibility to call
`ir.qweb.field.datetime`.`value_to_html` with
a datetime string directly, instead of calling
`ir.qweb.field.datetime`.`record_to_html`
(which call `value_to_html` as well).

opw-675427

openerp/addons/base/ir/ir_qweb.py
@@ -712,6 +712,12 @@ class DateTimeConverter(osv.AbstractModel):
         lang = self.user_lang(cr, uid, context=context)
         locale = babel.Locale.parse(lang.code)
 
+        if isinstance(value, basestring):
+            value = datetime.datetime.strptime(
+                value, openerp.tools.DEFAULT_SERVER_DATETIME_FORMAT)
+        value = fields.datetime.context_timestamp(
+            cr, uid, timestamp=value, context=context)
+
         if options and 'format' in options:
             pattern = options['format']
         else:
@@ -724,15 +730,10 @@ class DateTimeConverter(osv.AbstractModel):
         return babel.dates.format_datetime(value, format=pattern, locale=locale)
 
     def record_to_html(self, cr, uid, field_name, record, options, context=None):
-        value = record[field_name]
-        if isinstance(value, basestring):
-            value = datetime.datetime.strptime(
-                value, openerp.tools.DEFAULT_SERVER_DATETIME_FORMAT)
-        value = fields.datetime.context_timestamp(
-            cr, uid, timestamp=value, context=record.env.context)
         field = field = record._fields[field_name]
+        value = record[field_name]
         return self.value_to_html(
-            cr, uid, value, field, options=options, context=context)
+            cr, uid, value, field, options=options, context=dict(context, **record.env.context))
 
 class TextConverter(osv.AbstractModel):
     _name = 'ir.qweb.field.text'