44480e3de8ce08e838ea2d2f7ae97386e44d5d9a
David Monjoie <dmo@odoo.com>
2016-04-21 12:40:10 +0200

    [REM] project_issue: removed dead on_change_template override
    This function was removed in 9.0 at 1e4498fef56406658b5ee8e6350b884b27c903ad.

--- addons/project_issue/models/account_analytic_account.py
+++ addons/project_issue/models/account_analytic_account.py
@@ -11,14 +11,6 @@ class AccountAnalyticAccount(models.Model):
     use_issues = fields.Boolean(string='Use Issues', help="Check this box to manage customer activities through this project")
 
     @api.multi
-    def on_change_template(self, template_id, date_start=False):
-        res = super(AccountAnalyticAccount, self).on_change_template(template_id, date_start=date_start)
-        if template_id and 'value' in res:
-            account_analytic_account = self.browse(template_id)
-            res['value']['use_issues'] = account_analytic_account.use_issues
-        return res
-
-    @api.multi
     def unlink(self):
         if self.env['project.issue'].search_count([('project_id.analytic_account_id', 'in', self.ids)]):
             raise UserError(_('Please remove existing issues in the project linked to the accounts you want to delete.'))
