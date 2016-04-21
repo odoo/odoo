5c545e32448ddcd46716ab1fc0d20610504ddd21
Joren Van Onder <jov@odoo.com>
2016-04-21 09:44:45 +0200

    [IMP] point_of_sale: rewrite frontend tests
    There used to be some acceptance tests but they have not actually been
    run since 8.0.
    
    These acceptance tests are very useful for the POS because the POS is a
    somewhat unusual module and it happens occasionally that a change in
    another module unintentionally breaks the POS.
    
    Another reason these tests are nice to have is because the POS
    duplicates a lot of calculations in JS because it's meant to be able to
    work offline (eg. calculation of taxes, orderline totals etc.). Subtle
    differences (eg. rounding) between the frontend JS calculations and the
    backend calculations make up the most common category of bugs in the
    POS. These frontend tests will make it easy to write tests for these
    scenario's. When phantom_js finishes we look through all the received
    pos.order records and see that all of them are paid. If there would be a
    difference between frontend and backend calculations the order would not
    have the paid status because the payment on the order would be different
    from the total. An example test for a previous bug has been included.
    
    This will also come in handy for testing the upcoming pricelist
    functionality that will be implemented in the POS.

---
+++ addons/point_of_sale/tests/test_frontend.py
@@ -0,0 +1,61 @@
+import odoo.tests
+from openerp.api import Environment
+
+class TestUi(odoo.tests.HttpCase):
+    def test_01_pos_basic_order(self):
+        cr = self.registry.cursor()
+        assert cr == self.registry.test_cr
+        env = Environment(cr, self.uid, {})
+
+        journal_obj = env['account.journal']
+        account_obj = env['account.account']
+        main_company = env.ref('base.main_company')
+        main_pos_config = env.ref('point_of_sale.pos_config_main')
+
+        account_receivable = account_obj.create({'code': 'X1012',
+                                                 'name': 'Account Receivable - Test',
+                                                 'user_type_id': env.ref('account.data_account_type_receivable').id,
+                                                 'reconcile': True})
+        field = self.env['ir.model.fields'].search([('name', '=', 'property_account_receivable_id'),
+                                                    ('model', '=', 'res.partner'),
+                                                    ('relation', '=', 'account.account')], limit=1)
+        env['ir.property'].create({'name': 'property_account_receivable_id',
+                                   'company_id': main_company.id,
+                                   'fields_id': field.id,
+                                   'value': 'account.account,' + str(account_receivable.id)})
+
+        # set the company currency to USD, otherwise it will assume
+        # euro's. this will cause issues as the sale journal is in
+        # USD, because of this all products would have a different
+        # price
+        main_company.currency_id = env.ref('base.USD')
+
+        test_sale_journal = journal_obj.create({'name': 'Sale Journal - Test',
+                                                'code': 'TSJ',
+                                                'type': 'sale',
+                                                'company_id': main_company.id})
+
+        main_pos_config.journal_id = test_sale_journal
+        main_pos_config.write({'journal_ids': [(0, 0, {'name': 'Cash Journal - Test',
+                                                       'code': 'TSC',
+                                                       'type': 'cash',
+                                                       'company_id': main_company.id,
+                                                       'journal_user': True})]})
+
+        # open a session, the /pos/web controller will redirect to it
+        main_pos_config.open_session_cb()
+
+        # needed because tests are run before the module is marked as
+        # installed. In js web will only load qweb coming from modules
+        # that are returned by the backend in module_boot. Without
+        # this you end up with js, css but no qweb.
+        env['ir.module.module'].search([('name', '=', 'point_of_sale')], limit=1).state = 'installed'
+        cr.release()
+
+        self.phantom_js("/pos/web",
+                        "odoo.__DEBUG__.services['web.Tour'].run('pos_basic_order', 'test')",
+                        "odoo.__DEBUG__.services['web.Tour'].tours.pos_basic_order",
+                        login="admin")
+
+        for order in env['pos.order'].search([]):
+            self.assertEqual(order.state, 'paid', "Validated order has payment of " + str(order.amount_paid) + " and total of " + str(order.amount_total))
