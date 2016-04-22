4789786b2d8ba150055dfe25695df4941115beeb
Jérome Maes <jem@openerp.com>
2016-04-22 14:30:34 +0200

    [MIG] note_pad : migration to new api

--- addons/note_pad/models/note.py
+++ addons/note_pad/models/note.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import osv, fields
-from openerp.tools.translate import _
+from odoo import fields, models
 
-class note_pad_note(osv.osv):
-    """ memo pad """
+
+class NotePad(models.Model):
 
     _name = 'note.note'
-    _inherit = ['pad.common','note.note']
+    _inherit = ['pad.common', 'note.note']
 
     _pad_fields = ['note_pad']
 
-    _columns = {
-        'note_pad_url': fields.char('Pad Url', pad_content_field='memo'),
-    }
+    note_pad_url = fields.Char('Pad Url', pad_content_field='memo')
