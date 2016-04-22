467ba386f45dba425271793b5ad37d3f41a5f009
Joren Van Onder <jov@odoo.com>
2016-04-22 09:54:09 +0200

    [IMP] point_of_sale: get rid of unused arguments to XmlReceipt template

--- addons/point_of_sale/static/src/js/screens.js
+++ addons/point_of_sale/static/src/js/screens.js
@@ -1390,8 +1390,6 @@ var ReceiptScreenWidget = ScreenWidget.extend({
     print_xml: function() {
         var env = {
             widget:  this,
-            pos:     this.pos,
-            order:   this.pos.get_order(),
             receipt: this.pos.get_order().export_for_printing(),
             paymentlines: this.pos.get_order().get_paymentlines()
         };
