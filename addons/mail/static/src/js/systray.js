1607c279b2c67c5f79998edf0dae67a6242344e8
Aaron Bohy <aab@odoo.com>
2016-04-28 11:00:33 +0200

    [FIX] mail: escape author name in messaging menu

--- addons/mail/static/src/js/systray.js
+++ addons/mail/static/src/js/systray.js
@@ -111,9 +111,7 @@ var MessagingMenu = Widget.extend({
 
             // Generate last message preview (inline message body and compute date to display)
             _.each(channels_preview, function (channel) {
-                channel.last_message_preview =
-                    channel.last_message.displayed_author + ': ' +
-                    chat_manager.get_message_body_preview(channel.last_message.body);
+                channel.last_message_preview = chat_manager.get_message_body_preview(channel.last_message.body);
                 if (channel.last_message.date.isSame(new Date(), 'd')) {  // today
                     channel.last_message_date = channel.last_message.date.format('LT');
                 } else {
