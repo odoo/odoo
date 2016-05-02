5a35ac8ac2de02b00e523911e349811bf99c0b5a
Thibault Delavallée <tde@openerp.com>
2016-05-02 18:32:36 +0200

    [MIGR] mrp_operations: new API

--- addons/mrp_operations/models/stock_move.py
+++ addons/mrp_operations/models/stock_move.py
@@ -1,23 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields
-from openerp.osv import osv
-import operator
-import time
-from datetime import datetime
-from openerp.tools.translate import _
-from openerp.exceptions import UserError
+from odoo import fields, models
 
-#----------------------------------------------------------
-# Work Centers
-#----------------------------------------------------------
-# capacity_hour : capacity per hour. default: 1.0.
-#          Eg: If 5 concurrent operations at one time: capacity = 5 (because 5 employees)
-# unit_per_cycle : how many units are produced for one cycle
 
-class stock_move(osv.osv):
+class StockMove(models.Model):
     _inherit = 'stock.move'
-    _columns = {
-        'move_dest_id_lines': fields.one2many('stock.move','move_dest_id', 'Children Moves')
-    }
+
+    move_dest_id_lines = fields.One2many('stock.move', 'move_dest_id', 'Children Moves')
