751ae44e6f630bfd00b263e43c535a5af1c6e14d
mtr-odoo <mtr@odoo.com>
2016-05-02 14:28:12 +0200

    [MOV] procurement: move and split

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -8,7 +8,7 @@ import time
 from odoo import api, fields, models, _
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.tools.float_utils import float_compare
-from odoo.addons.procurement import procurement
+from odoo.addons.procurement.models import procurement
 from odoo.exceptions import UserError
 
 
