94f01e5c95b0a38de79d01bab334d22bd2c0a2fb
Mitali Patel <mpa@odoo.com>
2016-05-02 13:27:11 +0200

    [MOV] mrp_repair: move and split

--- addons/mrp_repair/wizard/__init__.py
+++ addons/mrp_repair/wizard/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import cancel_repair
-import make_invoice
+import mrp_repair_cancel
+import mrp_repair_make_invoice
