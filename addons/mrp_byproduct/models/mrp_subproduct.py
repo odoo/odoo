4261eec8f47a54f99b331869e10ffa8dd375b890
Mitali Patel <mpa@odoo.com>
2016-05-02 17:23:40 +0200

    [MIGR] mrp_byproduct: new API

--- addons/mrp_byproduct/models/mrp_subproduct.py
+++ addons/mrp_byproduct/models/mrp_subproduct.py
@@ -1,47 +1,44 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields
-from openerp.osv import osv
-import openerp.addons.decimal_precision as dp
-from openerp.tools.translate import _
+from odoo import api, fields, models, _
+from odoo.addons import decimal_precision as dp
 
-class mrp_subproduct(osv.osv):
+
+class MrpSubProduct(models.Model):
     _name = 'mrp.subproduct'
     _description = 'Byproduct'
-    _columns={
-        'product_id': fields.many2one('product.product', 'Product', required=True),
-        'product_qty': fields.float('Product Qty', digits_compute=dp.get_precision('Product Unit of Measure'), required=True),
-        'product_uom': fields.many2one('product.uom', 'Product Unit of Measure', required=True),
-        'subproduct_type': fields.selection([('fixed','Fixed'),('variable','Variable')], 'Quantity Type', required=True, help="Define how the quantity of byproducts will be set on the production orders using this BoM.\
-  'Fixed' depicts a situation where the quantity of created byproduct is always equal to the quantity set on the BoM, regardless of how many are created in the production order.\
-  By opposition, 'Variable' means that the quantity will be computed as\
-    '(quantity of byproduct set on the BoM / quantity of manufactured product set on the BoM * quantity of manufactured product in the production order.)'"),
-        'bom_id': fields.many2one('mrp.bom', 'BoM', ondelete='cascade'),
-    }
-    _defaults={
-        'subproduct_type': 'variable',
-        'product_qty': lambda *a: 1.0,
-    }
 
-    def onchange_product_id(self, cr, uid, ids, product_id, context=None):
-        """ Changes UoM if product_id changes.
-        @param product_id: Changed product_id
-        @return: Dictionary of changed values
-        """
-        if product_id:
-            prod = self.pool.get('product.product').browse(cr, uid, product_id, context=context)
-            v = {'product_uom': prod.uom_id.id}
-            return {'value': v}
-        return {}
+    product_id = fields.Many2one('product.product', 'Product', required=True)
+    product_qty = fields.Float(
+        'Product Qty',
+        default=1.0, digits_compute=dp.get_precision('Product Unit of Measure'), required=True)
+    product_uom = fields.Many2one('product.uom', 'Product Unit of Measure', required=True)
+    subproduct_type = fields.Selection([
+        ('fixed', 'Fixed'),
+        ('variable', 'Variable')], string='Quantity Type',
+        default='variable', required=True,
+        help="Define how the quantity of byproducts will be set on the production orders using this BoM.\n"
+             "'Fixed' depicts a situation where the quantity of created byproduct is always equal to the "
+             " quantity set on the BoM, regardless of how many are created in the production order.\n"
+             "By opposition, 'Variable' means that the quantity will be computed as (quantity of byproduct "
+             "set on the BoM / quantity of manufactured product set on the BoM * quantity of manufactured "
+             "product in the production order.)")
+    bom_id = fields.Many2one('mrp.bom', 'BoM', ondelete='cascade')
 
-    def onchange_uom(self, cr, uid, ids, product_id, product_uom, context=None):
-        res = {'value':{}}
-        if not product_uom or not product_id:
-            return res
-        product = self.pool.get('product.product').browse(cr, uid, product_id, context=context)
-        uom = self.pool.get('product.uom').browse(cr, uid, product_uom, context=context)
-        if uom.category_id.id != product.uom_id.category_id.id:
-            res['warning'] = {'title': _('Warning'), 'message': _('The Product Unit of Measure you chose has a different category than in the product form.')}
-            res['value'].update({'product_uom': product.uom_id.id})
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        """ Changes UoM if product_id changes. """
+        if self.product_id:
+            self.product_uom = self.product_id.uom_id.id
+
+    @api.onchange('product_uom')
+    def onchange_uom(self):
+        res = {}
+        if self.product_uom and self.product_id and self.product_uom.category_id != self.product_id.uom_id.category_id:
+            res['warning'] = {
+                'title': _('Warning'),
+                'message': _('The Product Unit of Measure you chose has a different category than in the product form.')
+            }
+            self.product_uom = self.product_id.uom_id.id
         return res
