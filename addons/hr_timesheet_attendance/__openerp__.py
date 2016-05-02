ba5c7fe6cca1069e0703071f7fe139ac62f5fe4e
Xavier Morel <xmo@odoo.com>
2016-05-02 11:42:49 +0200

    [ADD] hr_timesheet_attendance report
    Provide a timesheet/attendance report independent from hr_timesheet_sheet.

---
+++ addons/hr_timesheet_attendance/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Timesheets/attendances reporting",
+    'description': """
+    """,
+    'category': 'Hidden',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['hr_timesheet', 'hr_attendance'],
+    'data': ['report/hr_timesheet_attendance_report_view.xml'],
+    'auto_install': True,
+}
