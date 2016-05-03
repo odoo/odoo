27f3ded16b800d8079bccfccf214c1ef8232ddad
Mitali Patel <mpa@odoo.com>
2016-05-03 10:18:29 +0200

    [MIGR] mrp_repair

--- addons/mrp_repair/wizard/mrp_repair_make_invoice.py
+++ addons/mrp_repair/wizard/mrp_repair_make_invoice.py
@@ -1,51 +1,36 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
+from odoo import api, fields, models
 
-class make_invoice(osv.osv_memory):
+
+class MakeInvoice(models.TransientModel):
     _name = 'mrp.repair.make_invoice'
     _description = 'Make Invoice'
 
-    _columns = {
-           'group': fields.boolean('Group by partner invoice address'),
-    }
+    group = fields.Boolean('Group by partner invoice address')
 
-    def make_invoices(self, cr, uid, ids, context=None):
-        """ Generates invoice(s) of selected records.
-        @param self: The object pointer.
-        @param cr: A database cursor
-        @param uid: ID of the user currently logged in
-        @param ids: List of IDs selected
-        @param context: A standard dictionary
-        @return: Loads the view of new invoice(s).
-        """
-        if context is None:
-            context = {}
-        inv = self.browse(cr, uid, ids[0], context=context)
-        order_obj = self.pool.get('mrp.repair')
-        mod_obj = self.pool.get('ir.model.data')
-        newinv = order_obj.action_invoice_create(cr, uid, context['active_ids'],
-                                                 group=inv.group,context=context)
+    @api.multi
+    def make_invoices(self):
+        if not self._context.get('active_ids'):
+            return {'type': 'ir.actions.act_window_close'}
+        new_invoice = {}
+        for wizard in self:
+            repairs = self.env['mrp.repair'].browse(self._context['active_ids'])
+            new_invoice = repairs.action_invoice_create(group=wizard.group)
 
-        # We have to trigger the workflow of the given repairs, otherwise they remain 'to be invoiced'.
-        # Note that the signal 'action_invoice_create' will trigger another call to the method 'action_invoice_create',
-        # but that second call will not do anything, since the repairs are already invoiced.
-        order_obj.signal_workflow(cr, uid, context['active_ids'], 'action_invoice_create')
-
-        form_res = mod_obj.get_object_reference(cr, uid, 'account', 'invoice_form')
-        form_id = form_res and form_res[1] or False
-        tree_res = mod_obj.get_object_reference(cr, uid, 'account', 'invoice_tree')
-        tree_id = tree_res and tree_res[1] or False
-
+            # We have to trigger the workflow of the given repairs, otherwise they remain 'to be invoiced'.
+            # Note that the signal 'action_invoice_create' will trigger another call to the method 'action_invoice_create',
+            # but that second call will not do anything, since the repairs are already invoiced.
+            repairs.signal_workflow('action_invoice_create')
         return {
-            'domain': [('id','in', newinv.values())],
+            'domain': [('id', 'in', new_invoice.values())],
             'name': 'Invoices',
             'view_type': 'form',
             'view_mode': 'tree,form',
             'res_model': 'account.invoice',
             'view_id': False,
-            'views': [(tree_id, 'tree'),(form_id, 'form')],
+            'views': [(self.env.ref('account.invoice_tree').id, 'tree'), (self.env.ref('account.invoice_form').id, 'form')],
             'context': "{'type':'out_invoice'}",
             'type': 'ir.actions.act_window'
         }
