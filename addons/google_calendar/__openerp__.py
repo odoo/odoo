Odew/odoo-1

[MOVE]google_calendar: moved models and data file in to models and data directory

addons/google_calendar/__openerp__.py
@@ -33,11 +33,11 @@ The module adds the possibility to synchronize Google Calendar with OpenERP
     'depends': ['google_account', 'calendar'],
     'qweb': ['static/src/xml/*.xml'],
     'data': [
-        'res_config_view.xml',
+        'views/res_config_view.xml',
         'security/ir.model.access.csv',
         'views/google_calendar.xml',
         'views/res_users.xml',
-        'google_calendar.xml',
+        'data/google_calendar.xml',
     ],
     'demo': [],
     'installable': True,

odoo/odoo

addons/google_calendar/__openerp__.py
@@ -14,11 +14,14 @@ The module adds the possibility to synchronize Google Calendar with Odoo
     'depends': ['google_account', 'calendar'],
     'qweb': ['static/src/xml/*.xml'],
     'data': [
-        'res_config_view.xml',
+        'data/google_calendar_data.xml',
+        'views/res_config_views.xml',
+        'views/google_calendar_templates.xml',
+        'views/res_users_views.xml',
         'security/ir.model.access.csv',
-        'views/google_calendar.xml',
+        'views/google_calendar_templates.xml',
         'views/res_users.xml',
-        'google_calendar.xml',
+        'data/google_calendar_data.xml',
     ],
     'demo': [],
     'installable': True,