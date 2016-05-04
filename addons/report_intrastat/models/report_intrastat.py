1fbb3eaad0051bc202a3f1cf7b2fb1aa19f0e7d8
Yannick Tivisse <yti@odoo.com>
2016-05-04 15:25:21 +0200

    [MIG] report_intrastat: Migrated to new api

--- addons/report_intrastat/models/report_intrastat.py
+++ addons/report_intrastat/models/report_intrastat.py
@@ -1,37 +1,24 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
-from openerp.tools.sql import drop_view_if_exists
-from openerp.addons.decimal_precision import decimal_precision as dp
+from odoo import fields, models
 
-
-class res_country(osv.osv):
-    _name = 'res.country'
+class res_country(models.Model):
     _inherit = 'res.country'
-    _columns = {
-        'intrastat': fields.boolean('Intrastat member'),
-    }
-    _defaults = {
-        'intrastat': lambda *a: False,
-    }
+
+    intrastat = fields.Boolean(string="Intrastat member")
 
 
-
-class report_intrastat_code(osv.osv):
+class ReportIntrastatCode(models.Model):
     _name = "report.intrastat.code"
     _description = "Intrastat code"
     _translate = False
-    _columns = {
-        'name': fields.char('Intrastat Code'),
-        'description': fields.char('Description'),
-    }
+
+    name = fields.Char(string='Intrastat Code')
+    description = fields.Char(string='Description')
 
 
-
-class product_template(osv.osv):
-    _name = "product.template"
+class ProductTemplate(models.Model):
     _inherit = "product.template"
-    _columns = {
-        'intrastat_id': fields.many2one('report.intrastat.code', 'Intrastat code'),
-    }
+
+    intrastat_id = fields.Many2one('report.intrastat.code', string='Intrastat code')
