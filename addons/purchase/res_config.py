0bf6e979c04010154c98ebdbd184b2c4d65a2dc9
Yannick Tivisse <yti@odoo.com>
2016-05-06 15:05:31 +0200

    [IMP] sale,purchase,stock,account: Don't display an empty 'warning' page
    And add a category hidden on the res.group definition

--- addons/purchase/res_config.py
+++ addons/purchase/res_config.py
@@ -34,10 +34,10 @@ class purchase_config_settings(osv.osv_memory):
             help="Calls for tenders are used when you want to generate requests for quotations to several vendors for a given set of products.\n"
                  "You can configure per product if you directly do a Request for Quotation "
                  "to one vendor or if you want a Call for Tenders to compare offers from several vendors."),
-        'group_warning': fields.selection([
+        'group_warning_purchase': fields.selection([
             (0, 'All the products and the customers can be used in purchase orders'),
             (1, 'An informative or blocking warning can be set on a product or a customer')
-            ], "Warning", implied_group='purchase.group_warning'),
+            ], "Warning", implied_group='purchase.group_warning_purchase'),
         'module_stock_dropshipping': fields.selection([
             (0, 'Suppliers always deliver to your warehouse(s)'),
             (1, "Allow suppliers to deliver directly to your customers")
