c760e17019463f585bb040cd5dfbaa2e0a77d71e
Keyur Gajjar <kga@odoo.com>
2016-05-09 12:19:38 +0530

    [IMP] hr_recruitment: specific search view for ir_attachments
    - Quick search on index_content field.
    - Removed unnecessary filters(My Document(s), url, binary)
    - Inherited ir.attachment's search view for that and bind
      it to actions of hr_recruitment.

--- addons/hr_recruitment/models/hr_job.py
+++ addons/hr_recruitment/models/hr_job.py
@@ -84,6 +84,7 @@ class Job(models.Model):
             'default_res_model': self._name,
             'default_res_id': self.ids[0]
         }
+        action['search_view_id'] = (self.env.ref('hr_recruitment.ir_attachment_view_search_inherit_hr_recruitment').id, )
         action['domain'] = ['|', '&', ('res_model', '=', 'hr.job'), ('res_id', 'in', self.ids), '&', ('res_model', '=', 'hr.applicant'), ('res_id', 'in', self.mapped('application_ids').ids)]
         return action
 
