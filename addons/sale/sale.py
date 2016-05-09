3dad21355c2963b4040b2116789957782504b20e
Damien Bouvy <dbo@odoo.com>
2016-05-09 11:58:49 +0200

    [FIX] sale: uom conversion for invoiced quantities
    When invoicing products from a SO, you may decide to change the uom
    of the invoice (if you invoiced only  a fraction of the product, for
    example). In that case, conversion is necessary to ensure that the
    "invoiced quantity" field on the SO line is correct.

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -529,9 +529,9 @@ class SaleOrderLine(models.Model):
             for invoice_line in line.invoice_lines:
                 if invoice_line.invoice_id.state != 'cancel':
                     if invoice_line.invoice_id.type == 'out_invoice':
-                        qty_invoiced += invoice_line.quantity
+                        qty_invoiced += self.env['product.uom']._compute_qty_obj(invoice_line.uom_id, invoice_line.quantity, line.product_uom)
                     elif invoice_line.invoice_id.type == 'out_refund':
-                        qty_invoiced -= invoice_line.quantity
+                        qty_invoiced -= self.env['product.uom']._compute_qty_obj(invoice_line.uom_id, invoice_line.quantity, line.product_uom)
             line.qty_invoiced = qty_invoiced
 
     @api.depends('price_subtotal', 'product_uom_qty')
