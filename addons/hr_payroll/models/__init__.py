7ab908d2383ccb77ef44fcdbd994340bc00cf2de
Ravi Gohil <rgo@odoo.com>
2016-05-10 15:04:04 +0530

    [MOV] hr_payroll: Restructured files according to new API guidelines (no splitting)
    [FIX] hr_payroll: Fixed wrong decorator.

--- addons/hr_payroll/models/__init__.py
+++ addons/hr_payroll/models/__init__.py
@@ -1,6 +1,5 @@
 #-*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import models
-import report
-import wizard
+import hr_payroll
+import res_config
