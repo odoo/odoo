13180b39fd9b6539e84e3c872e3c3e938b5a660f
Denis Ledoux <dle@odoo.com>
2016-05-10 16:05:47 +0200

    [FIX] google_calendar: do not copy user Google info
    If a user syncs his calendar with Google,
    and a second user is created in the database by copying this first
    user, the Google information of the first user was copied
    (The Google account to sync, the token to use, the last
    syncrhonization date, ...), which is obviously wrong.
    
    On calendar syncrhonization, which can be done
    manually or automatically with the according cron, all
    events of the first user were created a second time,
    as a second user synchronized the same Google Calendar.
    
    opw-674141

--- addons/google_calendar/google_calendar.py
+++ addons/google_calendar/google_calendar.py
@@ -987,12 +987,12 @@ class res_users(osv.Model):
     _inherit = 'res.users'
 
     _columns = {
-        'google_calendar_rtoken': fields.char('Refresh Token'),
-        'google_calendar_token': fields.char('User token'),
-        'google_calendar_token_validity': fields.datetime('Token Validity'),
-        'google_calendar_last_sync_date': fields.datetime('Last synchro date'),
+        'google_calendar_rtoken': fields.char('Refresh Token', copy=False),
+        'google_calendar_token': fields.char('User token', copy=False),
+        'google_calendar_token_validity': fields.datetime('Token Validity', copy=False),
+        'google_calendar_last_sync_date': fields.datetime('Last synchro date', copy=False),
         'google_calendar_cal_id': fields.char('Calendar ID', help='Last Calendar ID who has been synchronized. If it is changed, we remove \
-all links between GoogleID and Odoo Google Internal ID')
+all links between GoogleID and Odoo Google Internal ID', copy=False)
     }
 
 
