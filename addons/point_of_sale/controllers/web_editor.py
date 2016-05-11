3b8f6884d6811f12082db38d8bc972d94de2d1e5
Joren Van Onder <jov@odoo.com>
2016-05-11 15:33:22 +0200

    [TO_SQUASH] use ir.ui.view render_template instead of render
    4ddc32313996c8cf6e8e6dffd2253aad8a144ccf

--- addons/point_of_sale/controllers/web_editor.py
+++ addons/point_of_sale/controllers/web_editor.py
@@ -9,10 +9,10 @@ class WebEditorPointOfSale(Web_Editor):
     def get_field_text_html(self, model=None, res_id=None, field=None, callback=None, **kwargs):
         kwargs['snippets'] = '/point_of_sale/snippets'
         kwargs['template'] = 'point_of_sale.FieldTextHtml'
-        extra_head = request.registry["ir.ui.view"].render(request.cr, request.uid, 'point_of_sale.extra_head', None, context=request.context)
+        extra_head = request.registry["ir.ui.view"].render_template(request.cr, request.uid, 'point_of_sale.extra_head', None, context=request.context)
 
         return self.FieldTextHtml(model, res_id, field, callback, head=extra_head, **kwargs)
 
     @http.route(['/point_of_sale/snippets'], type='json', auth="user", website=True)
     def get_snippets(self):
-        return request.registry["ir.ui.view"].render(request.cr, request.uid, 'point_of_sale.customer_facing_display_snippets', None, context=request.context)
+        return request.registry["ir.ui.view"].render_template(request.cr, request.uid, 'point_of_sale.customer_facing_display_snippets', None, context=request.context)
