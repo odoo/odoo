5aa0baea8472e79094372c12be0ab1af57e93877
Jeremy Kersten <jke@odoo.com>
2016-05-13 16:25:16 +0200

    [FIX] website_link: fix zeroClipboard config
    ZeroClipboard lib has been moved from web to website_link but
    config file has been not updated.

--- addons/website_links/static/src/js/website_links.js
+++ addons/website_links/static/src/js/website_links.js
@@ -249,7 +249,7 @@ if(!$('.o_website_links_create_tracked_url').length) {
 
     base.ready().done(function() {
 
-        ZeroClipboard.config({swfPath: location.origin + "/web/static/lib/zeroclipboard/ZeroClipboard.swf" });
+        ZeroClipboard.config({swfPath: location.origin + "/website_links/static/lib/zeroclipboard/ZeroClipboard.swf" });
 
         // UTMS selects widgets
         var campaign_select = new SelectBox('utm.campaign');
