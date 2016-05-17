65192ea943a18305d75bfac4521daca3ca273df7
Géry Debongnie <ged@odoo.com>
2016-05-17 10:26:24 +0200

    [FIX] web: remove useless line
    the enable_button call should have been removed...

--- addons/web/static/src/js/views/form_view.js
+++ addons/web/static/src/js/views/form_view.js
@@ -746,7 +746,6 @@ var FormView = View.extend(common.FieldManagerMixin, {
                 self.to_view_mode();
                 core.bus.trigger('do_reload_needaction');
                 core.bus.trigger('form_view_saved', self);
-                self.enable_button();
             });
         }).always(function(){
             self.enable_button();
