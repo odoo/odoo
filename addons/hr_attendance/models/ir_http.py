8c32712e8f8b4f50d84e75e079ee7b4aace2cca9
Denis Vermylen (dve) <dve@odoo.com>
2016-05-19 12:16:09 +0200

    [IMP] hr_attendance: migration and revamp
    - Migration to new API
    - MODEL : hr.attendance now has check_in and check_out datetimes plus a computed field worked_hours, no more action (in/out) and no more reason.
              hr.employee has extra fields: pin and barcode and can print a badge containing that barcode.
    - the module contains : -> a menu "My Attendances" to check yourself in or out manually.
                            -> a menu "Manage Attendances" with 3 submenus:
                                  - "Attendances" where you can edit attendances
                                  - "Employees" where you have a kanban view of the employees (to see who's checked in or print badges)
                                  - "Kiosk Mode": fullscreen, made to be at the entrance of companies to allow
                                     all employees to check in and out, either with a barcode scanner to scan your badge
                                     or by selecting them in a kanban view and checking in or out manually with or without
                                     entering their PIN.  (enable/disable in Configuration)
                            -> a menu "Configuration" where you can change enable/disable PIN use.
    - GROUPS : -> HR manager (base.group_hr_manager): full access to the attendance module.
               -> HR officer (base.group_hr_user): full access except the "Configuration" menu.
               -> Manual Attendances (base.group_hr_attendance): only access to the attendance menu "My Attendance" to check in and out manually in the back-end
               -> Employee (base.group_user): has no access to the attendance module. (should only check in and out through the "Kiosk Mode" put at his disposal)
    - Views adaptation to the new fields, addition of a presence indicator (green/red dot) to employee kanban and form views.

--- addons/hr_attendance/models/ir_http.py
+++
@@ -1,9 +0,0 @@
-from openerp import models, service
-
-class Http(models.AbstractModel):
-    _inherit = 'ir.http'
-
-    def session_info(self):
-        result = super(Http, self).session_info()
-        result['is_employee'] = self.env.user.has_group('base.group_user')
-        return result
