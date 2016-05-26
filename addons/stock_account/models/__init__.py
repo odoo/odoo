6a824e4a5ef08125183060532393ff89cc63dc62
mdi-odoo <mdi@tinyerp.com>
2016-05-26 17:26:32 +0530

    [MOV] stock_account: reorganize module

---
+++ addons/stock_account/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import account_chart_template
+import account_invoice
+import product
+import stock
+import stock_config_settings
--- addons/stock_account/report/stock_history.py
+++ addons/stock_account/report/stock_history.py
@@ -4,38 +4,6 @@ from openerp import tools
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
 
-class wizard_valuation_history(osv.osv_memory):
-
-    _name = 'wizard.valuation.history'
-    _description = 'Wizard that opens the stock valuation history table'
-    _columns = {
-        'choose_date': fields.boolean('Inventory at Date'),
-        'date': fields.datetime('Date', required=True),
-    }
-
-    _defaults = {
-        'choose_date': False,
-        'date': fields.datetime.now,
-    }
-
-    def open_table(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        data = self.read(cr, uid, ids, context=context)[0]
-        ctx = context.copy()
-        ctx['history_date'] = data['date']
-        ctx['search_default_group_by_product'] = True
-        ctx['search_default_group_by_location'] = True
-        return {
-            'domain': "[('date', '<=', '" + data['date'] + "')]",
-            'name': _('Stock Value At Date'),
-            'view_type': 'form',
-            'view_mode': 'tree',
-            'res_model': 'stock.history',
-            'type': 'ir.actions.act_window',
-            'context': ctx,
-        }
-
 
 class stock_history(osv.osv):
     _name = 'stock.history'
