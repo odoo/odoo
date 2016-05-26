58245d4dcaf9ebbd61e11bfeaf37643eac542341
Géry Debongnie <ged@odoo.com>
2016-05-26 10:22:31 +0200

    [IMP] web: remove useless rpc by user_menu
    The UserMenu widget needs the user name, but it is available in the
    session informations.

--- addons/web/static/src/js/widgets/user_menu.js
+++ addons/web/static/src/js/widgets/user_menu.js
@@ -27,23 +27,18 @@ var UserMenu = Widget.extend({
         });
     },
     do_update: function () {
-        var self = this;
-        var $avatar = self.$('.oe_topbar_avatar');
-        $avatar.attr('src', $avatar.data('default-src'));
+        var $avatar = this.$('.oe_topbar_avatar');
         if (!session.uid) {
+            $avatar.attr('src', $avatar.data('default-src'));
             return $.when();
         }
-        var func = new Model("res.users").get_func("read");
-        return self.alive(func(session.uid, ["name"])).then(function(res) {
-            var topbar_name = res.name;
-            if(session.debug) {
-                topbar_name = _.str.sprintf("%s (%s)", topbar_name, session.db);
-            }
-            self.$('.oe_topbar_name').text(topbar_name);
-
-            var avatar_src = session.url('/web/image', {model:'res.users', field: 'image_small', id: session.uid});
-            $avatar.attr('src', avatar_src);
-        });
+        var topbar_name = session.name;
+        if(session.debug) {
+            topbar_name = _.str.sprintf("%s (%s)", topbar_name, session.db);
+        }
+        this.$('.oe_topbar_name').text(topbar_name);
+        var avatar_src = session.url('/web/image', {model:'res.users', field: 'image_small', id: session.uid});
+        $avatar.attr('src', avatar_src);
     },
     on_menu_documentation: function () {
         window.open('https://www.odoo.com/documentation/user', '_blank');
