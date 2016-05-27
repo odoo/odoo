a9d11f1175ff36f2ad295d08626c073e1317c533
Rutul Raval <rra@odoo.com>
2016-05-27 15:59:19 +0200

    [MIG] portal : migration to new api

--- addons/portal/__openerp__.py
+++ addons/portal/__openerp__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name' : 'Portal',
+    'name': 'Portal',
     'version': '1.0',
     'depends': [
         'base',
