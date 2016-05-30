84ae888adce2e9da16813dbbf22bbfe7a00421ed
Jérome Maes <jem@openerp.com>
2016-05-30 14:28:23 +0200

    [MIG] website_crm : migration to new api

--- addons/website_crm/tests/test_website_crm.py
+++ addons/website_crm/tests/test_website_crm.py
@@ -1,8 +1,14 @@
-from openerp.api import Environment
-import openerp.tests
-@openerp.tests.common.at_install(False)
-@openerp.tests.common.post_install(True)
-class TestWebsiteCrm(openerp.tests.HttpCase):
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.api import Environment
+import odoo.tests
+
+
+@odoo.tests.common.at_install(False)
+@odoo.tests.common.post_install(True)
+class TestWebsiteCrm(odoo.tests.HttpCase):
+
     def test_tour(self):
         self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('website_crm_tour', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.website_crm_tour")
 
