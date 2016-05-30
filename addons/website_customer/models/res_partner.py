3275a90ff0ee71292b32a6005d2b1a289a09154b
Jérome Maes <jem@openerp.com>
2016-05-30 15:55:50 +0200

    [MIG] website_customer : migraton to new api

--- addons/website_customer/models/res_partner.py
+++ addons/website_customer/models/res_partner.py
@@ -1,33 +1,29 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import osv, fields
+from odoo import api, fields, models
 
 
-class res_partner(osv.Model):
+class Partner(models.Model):
+
     _inherit = 'res.partner'
 
-    _columns = {
-        'website_tag_ids': fields.many2many('res.partner.tag', id1='partner_id', id2='tag_id', string='Website tags', oldname="tag_ids"),
-    }
+    website_tag_ids = fields.Many2many('res.partner.tag', 'res_partner_res_partner_tag_rel', 'partner_id', 'tag_id', string='Website tags')
 
 
-class res_partner_tags(osv.Model):
-    _description = 'Partner Tags - These tags can be used on website to find customers by sector, or ... '
+class Tags(models.Model):
+
     _name = 'res.partner.tag'
+    _description = 'Partner Tags - These tags can be used on website to find customers by sector, or ... '
     _inherit = 'website.published.mixin'
 
-    def get_selection_class(self, cr, uid, context=None):
+    @api.model
+    def get_selection_class(self):
         classname = ['default', 'primary', 'success', 'warning', 'danger']
         return [(x, str.title(x)) for x in classname]
 
-    _columns = {
-        'name': fields.char('Category Name', required=True, translate=True),
-        'partner_ids': fields.many2many('res.partner', 'res_partner_res_partner_tag_rel', id1='tag_id', id2='partner_id', string='Partners'),
-        'classname': fields.selection(get_selection_class, 'Class', help="Bootstrap class to customize the color", required=True),
-        'active': fields.boolean('Active'),
-    }
-    _defaults = {
-        'active': True,
-        'website_published': True,
-        'classname': 'default',
-    }
+    name = fields.Char('Category Name', required=True, translate=True)
+    partner_ids = fields.Many2many('res.partner', 'res_partner_res_partner_tag_rel', 'tag_id', 'partner_id', string='Partners')
+    classname = fields.Selection(get_selection_class, 'Class', default='default', help="Bootstrap class to customize the color", required=True)
+    active = fields.Boolean('Active', default=True)
+    website_published = fields.Boolean(default=True)
