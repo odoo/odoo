3de847afd8065a2d61bf66ee413ce00dfdf3e47b
Darshan Kalola <dka@openerp.com>
2016-05-30 12:12:46 +0200

    [MIG] pos_restaurant : migration to new api
    During migration, remove 'set_background_color' method, to directly use js api.

--- addons/pos_restaurant/models/pos_restaurant.py
+++ addons/pos_restaurant/models/pos_restaurant.py
@@ -1,86 +1,60 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import logging
+from odoo import api, fields, models
 
-import openerp
-from openerp import tools
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
 
-_logger = logging.getLogger(__name__)
+class RestaurantFloor(models.Model):
 
-class restaurant_floor(osv.osv):
     _name = 'restaurant.floor'
-    _columns = {
-        'name':             fields.char('Floor Name', required=True, help='An internal identification of the restaurant floor'),
-        'pos_config_id':    fields.many2one('pos.config','Point of Sale'),
-        'background_color': fields.char('Background Color', help='The background color of the floor layout, (must be specified in a html-compatible format)'),
-        'table_ids':        fields.one2many('restaurant.table','floor_id','Tables', help='The list of tables in this floor'),
-        'sequence':         fields.integer('Sequence',help='Used to sort Floors'),
-    }
 
-    _defaults = {
-        'sequence': 1,
-        'background_color': 'rgb(210, 210, 210)',
-    }
+    name = fields.Char('Floor Name', required=True, help='An internal identification of the restaurant floor')
+    pos_config_id = fields.Many2one('pos.config', string='Point of Sale')
+    background_image = fields.Binary('Background Image', attachment=True, help='A background image used to display a floor layout in the point of sale interface')
+    background_color = fields.Char('Background Color', help='The background color of the floor layout, (must be specified in a html-compatible format)', default='rgb(210, 210, 210)')
+    table_ids = fields.One2many('restaurant.table', 'floor_id', string='Tables', help='The list of tables in this floor')
+    sequence = fields.Integer('Sequence', help='Used to sort Floors', default=1)
 
-    def set_background_color(self, cr, uid, id, background, context=None):
-        self.write(cr, uid, [id], {'background_color': background}, context=context)
 
-class restaurant_table(osv.osv):
+class RestaurantTable(models.Model):
+
     _name = 'restaurant.table'
-    _columns = {
-        'name':         fields.char('Table Name', size=32, required=True, help='An internal identification of a table'),
-        'floor_id':     fields.many2one('restaurant.floor','Floor'),
-        'shape':        fields.selection([('square','Square'),('round','Round')],'Shape', required=True),
-        'position_h':   fields.float('Horizontal Position', help="The table's horizontal position from the left side to the table's center, in pixels"),
-        'position_v':   fields.float('Vertical Position', help="The table's vertical position from the top to the table's center, in pixels"),
-        'width':        fields.float('Width',   help="The table's width in pixels"),
-        'height':       fields.float('Height',  help="The table's height in pixels"),
-        'seats':        fields.integer('Seats', help="The default number of customer served at this table."),
-        'color':        fields.char('Color',    help="The table's color, expressed as a valid 'background' CSS property value"),
-        'active':       fields.boolean('Active',help='If false, the table is deactivated and will not be available in the point of sale'),
-    }
 
-    _defaults = {
-        'shape': 'square',
-        'seats': 1,
-        'position_h': 10,
-        'position_v': 10,
-        'height': 50,
-        'width':  50,
-        'active': True,
-    }
+    name = fields.Char('Table Name', required=True, help='An internal identification of a table')
+    floor_id = fields.Many2one('restaurant.floor', string='Floor')
+    shape = fields.Selection([('square', 'Square'), ('round', 'Round')], string='Shape', required=True, default='square')
+    position_h = fields.Float('Horizontal Position', default=10,
+        help="The table's horizontal position from the left side to the table's center, in pixels")
+    position_v = fields.Float('Vertical Position', default=10,
+        help="The table's vertical position from the top to the table's center, in pixels")
+    width = fields.Float('Width', default=50, help="The table's width in pixels")
+    height = fields.Float('Height', default=50, help="The table's height in pixels")
+    seats = fields.Integer('Seats', default=1, help="The default number of customer served at this table.")
+    color = fields.Char('Color', help="The table's color, expressed as a valid 'background' CSS property value")
+    active = fields.Boolean('Active', default=True, help='If false, the table is deactivated and will not be available in the point of sale')
 
-    def create_from_ui(self, cr, uid, table, context=None):
+    @api.model
+    def create_from_ui(self, table):
         """ create or modify a table from the point of sale UI.
             table contains the table's fields. If it contains an
             id, it will modify the existing table. It then
-            returns the id of the table.  """
+            returns the id of the table.
+        """
+        if table.get('floor_id'):
+            table['floor_id'] = table['floor_id'][0]
 
-        if table.get('floor_id',False):
-            floor_id = table['floor_id'][0]
-            table['floor_id'] = floor_id
-
-        if table.get('id',False):   # Modifiy existing table
-            table_id = table['id']
-            del table['id']
-            self.write(cr, uid, [table_id], table, context=context)
+        table_id = table.pop('id', False)
+        if table_id:
+            self.browse(table_id).write(table)
         else:
-            table_id = self.create(cr, uid, table, context=context)
-
+            table_id = self.create(table).id
         return table_id
 
-class restaurant_printer(osv.osv):
+
+class RestaurantPrinter(models.Model):
+
     _name = 'restaurant.printer'
 
-    _columns = {
-        'name' : fields.char('Printer Name', size=32, required=True, help='An internal identification of the printer'),
-        'proxy_ip': fields.char('Proxy IP Address', size=32, help="The IP Address or hostname of the Printer's hardware proxy"),
-        'product_categories_ids': fields.many2many('pos.category','printer_category_rel', 'printer_id','category_id',string='Printed Product Categories'),
-    }
-
-    _defaults = {
-        'name' : 'Printer',
-    }
+    name = fields.Char('Printer Name', required=True, default='Printer', help='An internal identification of the printer')
+    proxy_ip = fields.Char('Proxy IP Address', help="The IP Address or hostname of the Printer's hardware proxy")
+    product_categories_ids = fields.Many2many('pos.category', 'printer_category_rel', 'printer_id', 'category_id', string='Printed Product Categories')
