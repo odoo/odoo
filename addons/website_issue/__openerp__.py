f13d2f70c9aedba01ed25ca660c88a737a2a6faf
Jérome Maes <jem@openerp.com>
2016-05-30 17:11:20 +0200

    [MIG] website_issue : migrate to new api

--- addons/website_issue/__openerp__.py
+++ addons/website_issue/__openerp__.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'Issues Form',
     'category': 'Project',
@@ -8,7 +11,7 @@ Odoo Contact Form
 ====================
 
         """,
-    'depends': ['website_form','project_issue'],
+    'depends': ['website_form', 'project_issue'],
     'data': [
         'data/website_issue_data.xml',
     ],
