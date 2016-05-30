fda13eb017c168726b077633ee520ed2c0682a74
Jérome Maes <jem@openerp.com>
2016-05-30 17:10:17 +0200

    [MIG-MOV] website_hr_recruitment : finalize grouping models, and migrate xml and python to new api

--- addons/website_hr_recruitment/models/hr_recruitment.py
+++ addons/website_hr_recruitment/models/hr_recruitment.py
@@ -1,14 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from urlparse import urljoin
 from werkzeug import url_encode
 
-from openerp import models, fields, api
-from openerp.addons.website.models.website import slug
+from odoo import api, fields, models
+from odoo.addons.website.models.website import slug
 
-class HrRecruitmentSource(models.Model):
+
+class RecruitmentSource(models.Model):
     _inherit = 'hr.recruitment.source'
 
-    @api.depends('source_id', 'source_id.name', 'job_id')
+    url = fields.Char(compute='_compute_url', string='Url Parameters')
+
     @api.one
+    @api.depends('source_id', 'source_id.name', 'job_id')
     def _compute_url(self):
         base_url = self.env['ir.config_parameter'].get_param('web.base.url')
         for source in self:
@@ -20,4 +26,32 @@ class HrRecruitmentSource(models.Model):
                 })
             ))
 
-    url = fields.Char(compute='_compute_url', string='Url Parameters')
+
+class Applicant(models.Model):
+
+    _inherit = 'hr.applicant'
+
+    def website_form_input_filter(self, request, values):
+        if 'partner_name' in values:
+            values.setdefault('name', '%s\'s Application' % values['partner_name'])
+        return values
+
+
+class Job(models.Model):
+
+    _name = 'hr.job'
+    _inherit = ['hr.job', 'website.seo.metadata', 'website.published.mixin']
+
+    website_description = fields.Html('Website description', translate=True)
+
+    @api.multi
+    def _website_url(self, field_name, arg):
+        result = super(Job, self)._website_url(field_name, arg)
+        for job in self:
+            result[job.id] = "/jobs/detail/%s" % job.id
+        return result
+
+    @api.multi
+    def set_open(self):
+        self.write({'website_published': False})
+        return super(Job, self).set_open()
