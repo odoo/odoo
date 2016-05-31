ececa0a86568e4a5f8f2dfe927633c0ff2ef73a1
Jérome Maes <jem@openerp.com>
2016-05-31 10:53:37 +0200

    [MOV] website_membership: module directory organization

--- addons/website_membership/__openerp__.py
+++ addons/website_membership/__openerp__.py
@@ -10,11 +10,11 @@ Website for browsing Associations, Groups and Memberships
 """,
     'depends': ['website_partner', 'website_google_map', 'association', 'website_sale'],
     'data': [
-        'views/website_membership.xml',
+        'views/website_membership_templates.xml',
         'security/ir.model.access.csv',
         'security/website_membership.xml',
     ],
-    'demo': ['demo/membership.xml'],
+    'demo': ['data/membership_demo.xml'],
     'qweb': ['static/src/xml/*.xml'],
     'installable': True,
 }
