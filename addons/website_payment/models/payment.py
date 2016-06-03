b926b855a1ac456631a97d6ed5f603cb1c2cb2b2
keyur gajjar <kga@odoo.com>
2016-06-03 11:15:42 +0530

    [ADD]website_payment: boolean field needed to handle the ups cod type payments

--- addons/website_payment/models/payment.py
+++ addons/website_payment/models/payment.py
@@ -5,3 +5,23 @@ from odoo import api, fields, models
 class PaymentAcquirer(models.Model):
     _name = 'payment.acquirer'
     _inherit = ['payment.acquirer','website.published.mixin']
+
+    is_cod = fields.Boolean()
+
+    @api.model
+    def _get_acquirer_buttons(self, order, button_values, add_domain=None):
+        domain = [('website_published', '=', True), ('company_id', '=', order.company_id.id)]
+        if add_domain:
+            domain = domain + add_domain
+        acquirers = self.search(domain)
+        res = []
+        for acquirer in acquirers:
+            acquirer_button = acquirer.sudo().render(
+                '/',
+                order.amount_total,
+                order.pricelist_id.currency_id.id,
+                values=button_values
+            )
+            acquirer.button = acquirer_button
+            res.append(acquirer)
+        return res
