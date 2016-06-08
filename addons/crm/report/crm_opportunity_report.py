51594adaba5e7ebc7163d5c84b9ce7cfa9b43942
Jérome Maes <jem@openerp.com>
2016-06-08 16:29:03 +0200

    [FIX] crm, website_crm_partner_assign : fix dependencies due to moved files

--- addons/crm/report/crm_opportunity_report.py
+++ addons/crm/report/crm_opportunity_report.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.addons.crm import crm_stage
+from openerp.addons.crm.models import crm_stage
 from openerp.osv import fields, osv
 from openerp import tools
 
