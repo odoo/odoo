239be17472eb79b1f711f2874ea91c95897035ef
Gaurav Panchal <gan@openerp.com>
2016-06-09 19:09:52 +0530

    [MIG] project: migration to new api except project.py

--- addons/project/models/res_company.py
+++ addons/project/models/res_company.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
+from odoo import fields, models
 
-class res_company(osv.osv):
+
+class ResCompany(models.Model):
     _inherit = 'res.company'
-    _columns = {
-        'project_time_mode_id': fields.many2one('product.uom', 'Project Time Unit',
-            help='This will set the unit of measure used in projects and tasks.\n' \
-"If you use the timesheet linked to projects, don't " \
-"forget to setup the right unit of measure in your employees.",
-        ),
-    }
+
+    project_time_mode_id = fields.Many2one('product.uom', string='Project Time Unit',
+        help="This will set the unit of measure used in projects and tasks.\n"
+             "If you use the timesheet linked to projects, don't "
+             "forget to setup the right unit of measure in your employees.")
--- addons/project/models/web_planner.py
+++ addons/project/models/web_planner.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from openerp import models
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models
 
 
 class PlannerProject(models.Model):
