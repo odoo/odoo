d421c7449510ec813241ee2e50cab10a5e8bade1
Gaurav Panchal <gan@openerp.com>
2016-06-09 19:09:52 +0530

    [MIG] project: migration to new api except project.py

--- addons/project/models/res_config.py
+++ addons/project/models/res_config.py
@@ -1,36 +1,35 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp import SUPERUSER_ID
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
+from odoo import api, fields, models
 
-class project_configuration(osv.osv_memory):
+
+class ProjectConfiguration(models.TransientModel):
     _name = 'project.config.settings'
     _inherit = 'res.config.settings'
 
-    _columns = {
-        'module_pad': fields.selection([
-            (0, "Task description is a plain text"),
-            (1, "Collaborative rich text on task description")
-            ], "Pads",
-            help='Lets the company customize which Pad installation should be used to link to new pads '
-                 '(for example: http://ietherpad.com/).\n'
-                 '-This installs the module pad.'),
-        'module_rating_project': fields.selection([
-            (0, "No customer rating"),
-            (1, 'Allow activating customer rating on projects, at issue completion')
-            ], "Rating",
-            help="This allows customers to give rating on provided services"),
-        'generate_project_alias': fields.selection([
-            (0, "Do not create an email alias automatically"),
-            (1, "Automatically generate an email alias at the project creation")
-            ], "Project Alias",
-            help="Odoo will generate an email alias at the project creation from project name."),
-        'module_project_forecast': fields.boolean("Forecasts, planning and Gantt charts"),
-    }
+    module_pad = fields.Selection([
+        (0, "Task description is a plain text"),
+        (1, "Collaborative rich text on task description")
+        ], string="Pads",
+        help='Lets the company customize which Pad installation should be used to link to new pads '
+             '(for example: http://ietherpad.com/).\n'
+             '-This installs the module pad.')
+    module_rating_project = fields.Selection([
+        (0, "No customer rating"),
+        (1, 'Allow activating customer rating on projects, at issue completion')
+        ], string="Rating",
+        help="This allows customers to give rating on provided services")
+    generate_project_alias = fields.Selection([
+        (0, "Do not create an email alias automatically"),
+        (1, "Automatically generate an email alias at the project creation")
+        ], string="Project Alias",
+        help="Odoo will generate an email alias at the project creation from project name.")
+    module_project_forecast = fields.Boolean(string="Forecasts, planning and Gantt charts")
 
-    def set_default_generate_project_alias(self, cr, uid, ids, context=None):
-        config_value = self.browse(cr, uid, ids, context=context).generate_project_alias
-        check = self.pool['res.users'].has_group(cr, uid, 'base.group_system')
-        self.pool.get('ir.values').set_default(cr, check and SUPERUSER_ID or uid, 'project.config.settings', 'generate_project_alias', config_value)
+    @api.multi
+    def set_default_generate_project_alias(self):
+        check = self.env.user.has_group('base.group_system')
+        Values = check and self.env['ir.values'].sudo() or self.env['ir.values']
+        for config in self:
+            Values.set_default('project.config.settings', 'generate_project_alias', config.generate_project_alias)
