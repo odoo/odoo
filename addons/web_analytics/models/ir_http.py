b5ad7f9c7751b1d04a895c35092fe14f7aebb96a
Géry Debongnie <ged@odoo.com>
2016-06-15 10:30:23 +0200

    [REM] web_analytics
    Too many problems for not enough gain...

--- addons/web_analytics/models/ir_http.py
+++
@@ -1,9 +0,0 @@
-from openerp import models, service
-
-class Http(models.Model):
-    _inherit = 'ir.http'
-
-    def session_info(self):
-        result = super(Http, self).session_info()
-        result['version_info'] = service.common.exp_version()
-        return result
