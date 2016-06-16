caca9b307c02245d1b627ecfecca98834b52c305
Géry Debongnie <ged@odoo.com>
2016-06-16 13:44:21 +0200

    [REF] web: refactor and simplify web.core

--- addons/web/static/src/js/framework/translation.js
+++ addons/web/static/src/js/framework/translation.js
@@ -74,9 +74,42 @@ var TranslationDataBase = Class.extend(/** @lends instance.TranslationDataBase# 
 });
 
 var _t = new TranslationDataBase().build_translation_function();
+/**
+ * Lazy translation function, only performs the translation when actually
+ * printed (e.g. inserted into a template)
+ *
+ * Useful when defining translatable strings in code evaluated before the
+ * translation database is loaded, as class attributes or at the top-level of
+ * an OpenERP Web module
+ *
+ * @param {String} s string to translate
+ * @returns {Object} lazy translation object
+ */
 var _lt = function (s) {
     return {toString: function () { return _t(s); }};
 };
+
+/** Setup jQuery timeago */
+/*
+ * Strings in timeago are "composed" with prefixes, words and suffixes. This
+ * makes their detection by our translating system impossible. Use all literal
+ * strings we're using with a translation mark here so the extractor can do its
+ * job.
+ */
+{
+    _t('less than a minute ago');
+    _t('about a minute ago');
+    _t('%d minutes ago');
+    _t('about an hour ago');
+    _t('%d hours ago');
+    _t('a day ago');
+    _t('%d days ago');
+    _t('about a month ago');
+    _t('%d months ago');
+    _t('about a year ago');
+    _t('%d years ago');
+}
+
 
 return {
     _t: _t,
