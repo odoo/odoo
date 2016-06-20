7eb93f6139fcdc41bdb46eb49dbaab2da74c4484
Mitali Patel <mpa@odoo.com>
2016-06-20 18:05:24 +0530

    [IMP] hr: no more officer group,removed officer group and replaced hr user(officer) group by hr manager rights

--- addons/hr/tests/common.py
+++ addons/hr/tests/common.py
@@ -12,7 +12,6 @@ class TestHrCommon(common.TransactionCase):
         self.Users = self.env['res.users']
 
         self.group_hr_manager_id = self.ref('base.group_hr_manager')
-        self.group_hr_user_id = self.ref('base.group_hr_user')
         self.group_user_id = self.ref('base.group_user')
 
         # Will be used in various test cases of test_hr_flow
@@ -21,21 +20,13 @@ class TestHrCommon(common.TransactionCase):
         self.main_partner_id = self.ref('base.main_partner')
         self.rd_department_id = self.ref('hr.dep_rd')
 
-        # Creating three users and assigning each a group related to Human Resource Management
+        # Creating two users and assigning each a group related to Human Resource Management
         self.res_users_hr_manager = self.Users.create({
             'company_id': self.main_company_id,
             'name': 'HR manager',
             'login': 'hrm',
             'email': 'hrm@example.com',
             'groups_id': [(6, 0, [self.group_hr_manager_id])]
-        })
-
-        self.res_users_hr_officer = self.Users.create({
-            'company_id': self.main_company_id,
-            'name': 'HR Officer',
-            'login': 'hro',
-            'email': 'hro@example.com',
-            'groups_id': [(6, 0, [self.group_hr_user_id])]
         })
 
         self.res_users_employee = self.Users.create({
@@ -48,5 +39,5 @@ class TestHrCommon(common.TransactionCase):
 
         # Will be used to test the flow of jobs(i.e. opening the job position for "Developer" for
         # recruitment and closing it after recruitment done)
-        self.job_developer = self.env(user=self.res_users_hr_officer.id).ref('hr.job_developer')
-        self.employee_niv = self.env(user=self.res_users_hr_officer.id).ref('hr.employee_niv')
+        self.job_developer = self.env(user=self.res_users_hr_manager.id).ref('hr.job_developer')
+        self.employee_niv = self.env(user=self.res_users_hr_manager.id).ref('hr.employee_niv')
