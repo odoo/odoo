dae0157d7b99db163533172a466d5baabd4de638
Jérome Maes <jem@openerp.com>
2016-06-20 14:57:35 +0200

    [REF] calendar: rename 'class' field on calendar.event
    'class' is a reserved name in python. This will cause problem during new API migration.

--- addons/calendar/calendar.py
+++ addons/calendar/calendar.py
@@ -901,7 +901,7 @@ class calendar_event(osv.Model):
         'stop_datetime': fields.datetime('End Datetime', states={'done': [('readonly', True)]}, track_visibility='onchange'),  # old date_deadline
         'duration': fields.float('Duration', states={'done': [('readonly', True)]}),
         'description': fields.text('Description', states={'done': [('readonly', True)]}),
-        'class': fields.selection([('public', 'Everyone'), ('private', 'Only me'), ('confidential', 'Only internal users')], 'Privacy', states={'done': [('readonly', True)]}),
+        'privacy': fields.selection([('public', 'Everyone'), ('private', 'Only me'), ('confidential', 'Only internal users')], 'Privacy', states={'done': [('readonly', True)]}, oldname='class'),
         'location': fields.char('Location', help="Location of Event", track_visibility='onchange', states={'done': [('readonly', True)]}),
         'show_as': fields.selection([('free', 'Free'), ('busy', 'Busy')], 'Show Time as', states={'done': [('readonly', True)]}),
 
@@ -950,7 +950,7 @@ class calendar_event(osv.Model):
         'rrule_type': False,
         'allday': False,
         'state': 'draft',
-        'class': 'public',
+        'privacy': 'public',
         'show_as': 'busy',
         'month_by': 'date',
         'interval': 1,
@@ -1657,7 +1657,7 @@ class calendar_event(osv.Model):
         if context is None:
             context = {}
         fields2 = fields and fields[:] or None
-        EXTRAFIELDS = ('class', 'user_id', 'duration', 'allday', 'start', 'start_date', 'start_datetime', 'rrule')
+        EXTRAFIELDS = ('privacy', 'user_id', 'duration', 'allday', 'start', 'start_date', 'start_datetime', 'rrule')
         for f in EXTRAFIELDS:
             if fields and (f not in fields):
                 fields2.append(f)
@@ -1695,7 +1695,7 @@ class calendar_event(osv.Model):
                 user_id = type(r['user_id']) in (tuple, list) and r['user_id'][0] or r['user_id']
                 if user_id == uid:
                     continue
-            if r['class'] == 'private':
+            if r['privacy'] == 'private':
                 for f in r.keys():
                     recurrent_fields = self._get_recurrent_fields(cr, uid, context=context)
                     public_fields = list(set(recurrent_fields + ['id', 'allday', 'start', 'stop', 'display_start', 'display_stop', 'duration', 'user_id', 'state', 'interval', 'count', 'recurrent_id_date', 'rrule']))
