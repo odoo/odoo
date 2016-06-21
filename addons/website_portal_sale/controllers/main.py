936df38e1b9df837763e54dd0805e5c61e6b4451
Vipul Bhatt <vbh@odoo.com>
2016-06-21 16:10:00 +0530

    [IMP] website_*: generic filter and sort mecanism for portal documents
    website portal now provide mecanism to sort and filter the
    paginated document on the portal pages.
    All portal module now use this mecanism, like they do for
    'archive document' and 'pager'.
    
    A generic template was added in website_portal. To use
    it, renaming variables in method controller was required
    to make it as generic as possible.
    
    Some style cleaning was also done to make portal pages
    consistent.

--- addons/website_portal_sale/controllers/main.py
+++ addons/website_portal_sale/controllers/main.py
@@ -43,7 +43,7 @@ class website_account(website_account):
     #
 
     @http.route(['/my/quotes', '/my/quotes/page/<int:page>'], type='http', auth="user", website=True)
-    def portal_my_quotes(self, page=1, date_begin=None, date_end=None, **kw):
+    def portal_my_quotes(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
         values = self._prepare_portal_layout_values()
         partner = request.env.user.partner_id
         SaleOrder = request.env['sale.order']
@@ -52,6 +52,16 @@ class website_account(website_account):
             ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
             ('state', 'in', ['sent', 'cancel'])
         ]
+
+        searchbar_sortings = {
+            'date': {'label': _('Order Date'), 'order': 'date_order desc'},
+            'name': {'label': _('Name'), 'order': 'name'},
+        }
+
+        # default sortby order
+        if not sortby:
+            sortby = 'date'
+        sort_order = searchbar_sortings[sortby]['order']
 
         archive_groups = self._get_archive_groups('sale.order', domain)
         if date_begin and date_end:
@@ -62,13 +72,13 @@ class website_account(website_account):
         # make pager
         pager = request.website.pager(
             url="/my/quotes",
-            url_args={'date_begin': date_begin, 'date_end': date_end},
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
             total=quotation_count,
             page=page,
             step=self._items_per_page
         )
         # search the count to display, according to the pager data
-        quotations = SaleOrder.search(domain, limit=self._items_per_page, offset=pager['offset'])
+        quotations = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
 
         values.update({
             'date': date_begin,
@@ -76,11 +86,13 @@ class website_account(website_account):
             'pager': pager,
             'archive_groups': archive_groups,
             'default_url': '/my/quotes',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
         })
         return request.render("website_portal_sale.portal_my_quotations", values)
 
     @http.route(['/my/orders', '/my/orders/page/<int:page>'], type='http', auth="user", website=True)
-    def portal_my_orders(self, page=1, date_begin=None, date_end=None, **kw):
+    def portal_my_orders(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
         values = self._prepare_portal_layout_values()
         partner = request.env.user.partner_id
         SaleOrder = request.env['sale.order']
@@ -89,6 +101,16 @@ class website_account(website_account):
             ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
             ('state', 'in', ['sale', 'done'])
         ]
+
+        searchbar_sortings = {
+            'date': {'label': _('Order Date'), 'order': 'date_order desc'},
+            'name': {'label': _('Name'), 'order': 'name'},
+        }
+        # default sortby order
+        if not sortby:
+            sortby = 'date'
+        sort_order = searchbar_sortings[sortby]['order']
+
         archive_groups = self._get_archive_groups('sale.order', domain)
         if date_begin and date_end:
             domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
@@ -98,13 +120,13 @@ class website_account(website_account):
         # pager
         pager = request.website.pager(
             url="/my/orders",
-            url_args={'date_begin': date_begin, 'date_end': date_end},
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
             total=order_count,
             page=page,
             step=self._items_per_page
         )
         # content according to pager and archive selected
-        orders = SaleOrder.search(domain, limit=self._items_per_page, offset=pager['offset'])
+        orders = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
 
         values.update({
             'date': date_begin,
@@ -113,6 +135,8 @@ class website_account(website_account):
             'pager': pager,
             'archive_groups': archive_groups,
             'default_url': '/my/orders',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
         })
         return request.render("website_portal_sale.portal_my_orders", values)
 
@@ -135,7 +159,7 @@ class website_account(website_account):
     #
 
     @http.route(['/my/invoices', '/my/invoices/page/<int:page>'], type='http', auth="user", website=True)
-    def portal_my_invoices(self, page=1, date_begin=None, date_end=None, **kw):
+    def portal_my_invoices(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
         values = self._prepare_portal_layout_values()
         partner = request.env.user.partner_id
         AccountInvoice = request.env['account.invoice']
@@ -144,6 +168,18 @@ class website_account(website_account):
             ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
             ('state', 'in', ['open', 'paid', 'cancelled'])
         ]
+
+        searchbar_sortings = {
+            'date': {'label': _('Invoice Date'), 'order': 'date_invoice desc'},
+            'duedate': {'label': _('Due Date'), 'order': 'date_due desc'},
+            'name': {'label': _('Name'), 'order': 'name desc'},
+            'state': {'label': _('Status'), 'order': 'state'},
+        }
+        # default sort by order
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
+
         archive_groups = self._get_archive_groups('account.invoice', domain)
         if date_begin and date_end:
             domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
@@ -153,13 +189,13 @@ class website_account(website_account):
         # pager
         pager = request.website.pager(
             url="/my/invoices",
-            url_args={'date_begin': date_begin, 'date_end': date_end},
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
             total=invoice_count,
             page=page,
             step=self._items_per_page
         )
         # content according to pager and archive selected
-        invoices = AccountInvoice.search(domain, limit=self._items_per_page, offset=pager['offset'])
+        invoices = AccountInvoice.search(domain, order=order, limit=self._items_per_page, offset=pager['offset'])
         values.update({
             'date': date_begin,
             'invoices': invoices,
@@ -167,6 +203,8 @@ class website_account(website_account):
             'pager': pager,
             'archive_groups': archive_groups,
             'default_url': '/my/invoices',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
         })
         return request.render("website_portal_sale.portal_my_invoices", values)
 
