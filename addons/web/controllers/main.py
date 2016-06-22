2bc52a02df07a78d72f7b2963cc8680bf274adea
Christophe Simonis <chs@odoo.com>
2016-06-22 16:09:17 +0200

    [IMP] http: avoid saving sessions for auth='none' routes

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -129,6 +129,7 @@ def ensure_db(redirect='/web/database/selector'):
             query_string = iri_to_uri(r.query_string)
             url_redirect = url_redirect.replace(query=query_string)
         request.session.db = db
+        request.session.modified = True
         abort_and_redirect(url_redirect)
 
     # if db not provided, use the session one
