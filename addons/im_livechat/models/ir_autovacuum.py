7d64bd5ed2a6ddfc5245205073d390a747c84445
Aaron Bohy <aab@odoo.com>
2016-06-23 09:03:57 +0200

    [IMP] im_livechat: add gc of empty sessions in the autovacuum cron

---
+++ addons/im_livechat/models/ir_autovacuum.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models
+
+
+class AutoVacuum(models.AbstractModel):
+    _inherit = 'ir.autovacuum'
+
+    @api.model
+    def power_on(self, *args, **kwargs):
+        self.pool['mail.channel'].remove_empty_livechat_sessions()
+        return super(AutoVacuum, self).power_on(*args, **kwargs)
