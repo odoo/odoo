odoo/odoo

commit 32926e3851799157f805893cd0f6568bb5906212

[MIGR] base: migrate base/res to the new API
- `ir.property`, improve API of method `get_by_record`
- `res.bank`, `res.partner.bank`
- `res.company`
- `res.config`, `res.config.installer`, `res.config.settings`, fix usage of methods `get_default_`
- `res.country`, `res.country.group`, `res.country.state`
- `res.currency`, `res.currency.rate`
- `res.font`
- `res.lang`
- `res.partner`, `res.partner.category`, `res.partner.title`
- `res.request.link`
- `res.groups`, `res.users`, `res.users.log`, change password wizard

openerp/addons/base/res/res_currency.py
@@ -1,134 +1,113 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import json
+import math
 import re
 import time
-import math
 
-from openerp import api, fields as fields2
-from openerp import tools
-from openerp.osv import fields, osv
-from openerp.tools import float_round, float_is_zero, float_compare
-import json
+from odoo import api, fields, models, tools, _
 
 CURRENCY_DISPLAY_PATTERN = re.compile(r'(\w+)\s*(?:\((.*)\))?')
 
-class res_currency(osv.osv):
 
-    def _get_current_rate(self, cr, uid, ids, name, arg, context=None):
-        if context is None:
-            context = {}
-        res = {}
-
-        date = context.get('date') or fields2.Datetime.now()
-        company_id = context.get('company_id') or self.pool['res.users']._get_company(cr, uid, context=context)
-        for id in ids:
-            cr.execute("""SELECT rate FROM res_currency_rate 
-                           WHERE currency_id = %s
-                             AND name <= %s
-                             AND (company_id is null
-                                 OR company_id = %s)
-                        ORDER BY company_id, name desc LIMIT 1""",
-                       (id, date, company_id))
-            if cr.rowcount:
-                res[id] = cr.fetchone()[0]
-            else:
-                res[id] = 1
-        return res
-
-    def _decimal_places(self, cr, uid, ids, name, arg, context=None):
-        res = {}
-        for id in ids:
-            rounding = self.browse(cr, uid, id, context=context).rounding
-            rounding = (0 < rounding < 1) and rounding or 1
-            res[id] = int(math.ceil(math.log10(1 / rounding)))
-        return res
-
-    def _decimal_places(self, cr, uid, ids, name, arg, context=None):
-        res = {}
-        for currency in self.browse(cr, uid, ids, context=context):
-            if currency.rounding > 0 and currency.rounding < 1:
-                res[currency.id] = int(math.ceil(math.log10(1/currency.rounding)))
-            else:
-                res[currency.id] = 0
-        return res
-
+class Currency(models.Model):
     _name = "res.currency"
     _description = "Currency"
-    _columns = {
-        # Note: 'code' column was removed as of v6.0, the 'name' should now hold the ISO code.
-        'name': fields.char('Currency', size=3, required=True, help="Currency Code (ISO 4217)"),
-        'symbol': fields.char('Symbol', size=4, help="Currency sign, to be used when printing amounts."),
-        'rate': fields.function(_get_current_rate, string='Current Rate', digits=(12,6),
-            help='The rate of the currency to the currency of rate 1.'),
-        'rate_ids': fields.one2many('res.currency.rate', 'currency_id', 'Rates'),
-        'rounding': fields.float('Rounding Factor', digits=(12,6)),
-        'decimal_places': fields.function(_decimal_places, string='Decimal Places', type='integer'),
-        'active': fields.boolean('Active'),
-        'position': fields.selection([('after','After Amount'),('before','Before Amount')], 'Symbol Position', help="Determines where the currency symbol should be placed after or before the amount.")
-    }
-    _defaults = {
-        'active': 1,
-        'position' : 'after',
-        'rounding': 0.01,
-    }
+    _order = "name"
+
+    # Note: 'code' column was removed as of v6.0, the 'name' should now hold the ISO code.
+    name = fields.Char(string='Currency', size=3, required=True, help="Currency Code (ISO 4217)")
+    symbol = fields.Char(help="Currency sign, to be used when printing amounts.")
+    rate = fields.Float(compute='_compute_current_rate', string='Current Rate', digits=(12, 6),
+                        help='The rate of the currency to the currency of rate 1.')
+    rate_ids = fields.One2many('res.currency.rate', 'currency_id', string='Rates')
+    rounding = fields.Float(string='Rounding Factor', digits=(12, 6), default=0.01)
+    decimal_places = fields.Integer(compute='_compute_decimal_places')
+    active = fields.Boolean(default=True)
+    position = fields.Selection([('after', 'After Amount'), ('before', 'Before Amount')], default='after',
+        string='Symbol Position', help="Determines where the currency symbol should be placed after or before the amount.")
+    date = fields.Date(compute='_compute_date')
+
     _sql_constraints = [
         ('unique_name', 'unique (name)', 'The currency code must be unique!'),
     ]
-    _order = "name"
 
-    date = fields2.Date(compute='compute_date')
+    @api.multi
+    def _compute_current_rate(self):
+        date = self._context.get('date') or fields.Datetime.now()
+        company_id = self._context.get('company_id') or self.env.user._get_company().id
+        # the subquery selects the last rate before 'date' for the given currency/company
+        query = """SELECT c.id, (SELECT r.rate FROM res_currency_rate r
+                                  WHERE r.currency_id = c.id AND r.name <= %s
+                                    AND (r.company_id IS NULL OR r.company_id = %s)
+                               ORDER BY r.company_id, r.name DESC
+                                  LIMIT 1) AS rate
+                   FROM res_currency c
+                   WHERE c.id IN %s"""
+        self._cr.execute(query, (date, company_id, tuple(self.ids)))
+        currency_rates = dict(self._cr.fetchall())
+        for currency in self:
+            currency.rate = currency_rates.get(currency.id, 1.0)
 
-    @api.one
+    @api.multi
+    @api.depends('rounding')
+    def _compute_decimal_places(self):
+        for currency in self:
+            if 0 < currency.rounding < 1:
+                currency.decimal_places = int(math.ceil(math.log10(1/currency.rounding)))
+            else:
+                currency.decimal_places = 0
+
+    @api.multi
     @api.depends('rate_ids.name')
-    def compute_date(self):
-        self.date = self.rate_ids[:1].name
+    def _compute_date(self):
+        for currency in self:
+            currency.date = currency.rate_ids[:1].name
 
-    def name_search(self, cr, user, name='', args=None, operator='ilike', context=None, limit=100):
-        if not args:
-            args = []
-        results = super(res_currency,self)\
-            .name_search(cr, user, name, args, operator=operator, context=context, limit=limit)
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        results = super(Currency, self).name_search(name, args, operator=operator, limit=limit)
         if not results:
             name_match = CURRENCY_DISPLAY_PATTERN.match(name)
             if name_match:
-                results = super(res_currency,self)\
-                    .name_search(cr, user, name_match.group(1), args, operator=operator, context=context, limit=limit)
+                results = super(Currency, self).name_search(name_match.group(1), args, operator=operator, limit=limit)
         return results
 
-    def name_get(self, cr, uid, ids, context=None):
-        if not ids:
-            return []
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        reads = self.read(cr, uid, ids, ['name','symbol'], context=context, load='_classic_write')
-        return [(x['id'], tools.ustr(x['name'])) for x in reads]
+    @api.multi
+    def name_get(self):
+        return [(currency.id, tools.ustr(currency.name)) for currency in self]
 
-    def copy(self, cr, uid, id, default=None, context=None):
-        if context is None:
-            context = {}
-        if not default:
-            default = {}
-        default.update(name=_("%s (copy)")
-                       % (self.browse(cr, uid, id, context=context).name))
-        return super(res_currency, self).copy(
-            cr, uid, id, default=default, context=context)
+    @api.multi
+    def copy(self, default=None):
+        self.ensure_one()
+        default = dict(default or {}, name=_("%s (copy)") % self.name)
+        return super(Currency, self).copy(default=default)
 
-    @api.cr_uid_records
+    @api.v7
     def round(self, cr, uid, currency, amount):
-        """Return ``amount`` rounded  according to ``currency``'s
-           rounding rules.
+        return Currency.round(currency, amount)
 
-           :param Record currency: currency for which we are rounding
+    @api.v8
+    def round(self, amount):
+        """Return ``amount`` rounded  according to ``self``'s rounding rules.
+
            :param float amount: the amount to round
            :return: rounded float
-
-           With the new API, call it like: ``currency.round(amount)``.
         """
-        return float_round(amount, precision_rounding=currency.rounding)
+        # TODO: Need to check why it calls round() from sale.py, _amount_all() with *No* ID after below commits,
+        # https://github.com/odoo/odoo/commit/36ee1ad813204dcb91e9f5f20d746dff6f080ac2
+        # https://github.com/odoo/odoo/commit/0b6058c585d7d9a57bd7581b8211f20fca3ec3f7
+        # Removing self.ensure_one() will make few test cases to break of modules event_sale, sale_mrp and stock_dropshipping.
+        #self.ensure_one()
+        return tools.float_round(amount, precision_rounding=self.rounding)
 
-    @api.cr_uid_records
+    @api.v7
     def compare_amounts(self, cr, uid, currency, amount1, amount2):
+        return Currency.compare_amounts(currency, amount1, amount2)
+
+    @api.v8
+    def compare_amounts(self, amount1, amount2):
         """Compare ``amount1`` and ``amount2`` after rounding them according to the
            given currency's precision..
            An amount is considered lower/greater than another amount if their rounded
@@ -140,7 +119,6 @@ class res_currency(osv.osv):
            they respectively round to 0.01 and 0.0, even though
            0.006-0.002 = 0.004 which would be considered zero at 2 digits precision.
 
-           :param Record currency: currency for which we are rounding
            :param float amount1: first amount to compare
            :param float amount2: second amount to compare
            :return: (resp.) -1, 0 or 1, if ``amount1`` is (resp.) lower than,
@@ -149,45 +127,41 @@ class res_currency(osv.osv):
 
            With the new API, call it like: ``currency.compare_amounts(amount1, amount2)``.
         """
-        return float_compare(amount1, amount2, precision_rounding=currency.rounding)
+        return tools.float_compare(amount1, amount2, precision_rounding=self.rounding)
 
-    @api.cr_uid_records
+    @api.v7
     def is_zero(self, cr, uid, currency, amount):
-        """Returns true if ``amount`` is small enough to be treated as
-           zero according to ``currency``'s rounding rules.
+        return Currency.is_zero(currency, amount)
 
-           Warning: ``is_zero(amount1-amount2)`` is not always equivalent to 
+    @api.v8
+    def is_zero(self, amount):
+        """Returns true if ``amount`` is small enough to be treated as
+           zero according to current currency's rounding rules.
+           Warning: ``is_zero(amount1-amount2)`` is not always equivalent to
            ``compare_amounts(amount1,amount2) == 0``, as the former will round after
            computing the difference, while the latter will round before, giving
            different results for e.g. 0.006 and 0.002 at 2 digits precision.
 
-           :param Record currency: currency for which we are rounding
            :param float amount: amount to compare with currency's zero
 
            With the new API, call it like: ``currency.is_zero(amount)``.
         """
-        return float_is_zero(amount, precision_rounding=currency.rounding)
+        return tools.float_is_zero(amount, precision_rounding=self.rounding)
 
-    def _get_conversion_rate(self, cr, uid, from_currency, to_currency, context=None):
-        if context is None:
-            context = {}
-        ctx = context.copy()
-        from_currency = self.browse(cr, uid, from_currency.id, context=ctx)
-        to_currency = self.browse(cr, uid, to_currency.id, context=ctx)
-        return to_currency.rate/from_currency.rate
+    @api.model
+    def _get_conversion_rate(self, from_currency, to_currency):
+        from_currency = from_currency.with_env(self.env)
+        to_currency = to_currency.with_env(self.env)
+        return to_currency.rate / from_currency.rate
 
-    def _compute(self, cr, uid, from_currency, to_currency, from_amount, round=True, context=None):
-        if (to_currency.id == from_currency.id):
-            if round:
-                return self.round(cr, uid, to_currency, from_amount)
-            else:
-                return from_amount
+    @api.model
+    def _compute(self, from_currency, to_currency, from_amount, round=True):
+        if (to_currency == from_currency):
+            amount = to_currency.round(from_amount) if round else from_amount
         else:
-            rate = self._get_conversion_rate(cr, uid, from_currency, to_currency, context=context)
-            if round:
-                return self.round(cr, uid, to_currency, from_amount * rate)
-            else:
-                return from_amount * rate
+            rate = self._get_conversion_rate(from_currency, to_currency)
+            amount = to_currency.round(from_amount * rate) if round else from_amount * rate
+        return amount
 
     @api.v7
     def compute(self, cr, uid, from_currency_id, to_currency_id, from_amount,
@@ -215,23 +189,23 @@ class res_currency(osv.osv):
         # apply rounding
         return to_currency.round(to_amount) if round else to_amount
 
-    @api.v7
-    def get_format_currencies_js_function(self, cr, uid, context=None):
+    @api.model
+    def get_format_currencies_js_function(self):
         """ Returns a string that can be used to instanciate a javascript function that formats numbers as currencies.
             That function expects the number as first parameter and the currency id as second parameter.
             If the currency id parameter is false or undefined, the company currency is used.
         """
-        company_currency_id = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.currency_id.id
+        company_currency = self.env.user.with_env(self.env).company_id.currency_id
         function = ""
-        for row in self.search_read(cr, uid, domain=[], fields=['id', 'name', 'symbol', 'decimal_places', 'position'], context=context):
-            symbol = row['symbol'] or row['name']
-            format_number_str = "openerp.web.format_value(arguments[0], {type: 'float', digits: [69," + str(row['decimal_places']) + "]}, 0.00)"
-            if row['position'] == 'after':
-                return_str = "return " + format_number_str + " + '\\xA0' + " + json.dumps(symbol) + ";"
+        for currency in self.search([]):
+            symbol = currency.symbol or currency.name
+            format_number_str = "openerp.web.format_value(arguments[0], {type: 'float', digits: [69,%s]}, 0.00)" % currency.decimal_places
+            if currency.position == 'after':
+                return_str = "return %s + '\\xA0' + %s;" % (format_number_str, json.dumps(symbol))
             else:
-                return_str = "return " + json.dumps(symbol) + " + '\\xA0' + " + format_number_str + ";"
-            function += "if (arguments[1] === " + str(row['id']) + ") { " + return_str + " }"
-            if (row['id'] == company_currency_id):
+                return_str = "return %s + '\\xA0' + %s;" % (json.dumps(symbol), format_number_str)
+            function += "if (arguments[1] === %s) { %s }" % (currency.id, return_str)
+            if (currency == company_currency):
                 company_currency_format = return_str
         function = "if (arguments[1] === false || arguments[1] === undefined) {" + company_currency_format + " }" + function
         return function
@@ -254,30 +228,25 @@ class res_currency(osv.osv):
         """
 
 
-class res_currency_rate(osv.osv):
+class CurrencyRate(models.Model):
     _name = "res.currency.rate"
     _description = "Currency Rate"
-
-    _columns = {
-        'name': fields.datetime('Date', required=True, select=True),
-        'rate': fields.float('Rate', digits=(12, 6), help='The rate of the currency to the currency of rate 1'),
-        'currency_id': fields.many2one('res.currency', 'Currency', readonly=True),
-        'company_id': fields.many2one('res.company', 'Company')
-    }
-    _defaults = {
-        'name': lambda *a: time.strftime('%Y-%m-%d 00:00:00'),
-    }
     _order = "name desc"
 
-    def name_search(self, cr, user, name, args=None, operator='ilike', context=None, limit=80):
+    name = fields.Datetime(string='Date', required=True, index=True, default=fields.Date.today() + ' 00:00:00')
+    rate = fields.Float(digits=(12, 6), help='The rate of the currency to the currency of rate 1')
+    currency_id = fields.Many2one('res.currency', string='Currency', readonly=True)
+    company_id = fields.Many2one('res.company', string='Company')
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=80):
         if operator in ['=', '!=']:
             try:
                 date_format = '%Y-%m-%d'
-                if context.get('lang'):
-                    lang_obj = self.pool['res.lang']
-                    lang_ids = lang_obj.search(cr, user, [('code', '=', context['lang'])], context=context)
-                    if lang_ids:
-                        date_format = lang_obj.browse(cr, user, lang_ids[0], context=context).date_format
+                if self._context.get('lang'):
+                    langs = self.env['res.lang'].search([('code', '=', self._context['lang'])])
+                    if langs:
+                        date_format = langs.date_format
                 name = time.strftime('%Y-%m-%d', time.strptime(name, date_format))
             except ValueError:
                 try:
@@ -286,4 +255,4 @@ class res_currency_rate(osv.osv):
                     return []
                 name = ''
                 operator = 'ilike'
-        return super(res_currency_rate, self).name_search(cr, user, name, args=args, operator=operator, context=context, limit=limit)
+        return super(CurrencyRate, self).name_search(name, args=args, operator=operator, limit=limit)

addons/account/data/invoice_action_data.xml
@@ -21,7 +21,7 @@
 % set access_url = access_action['type'] == 'ir.actions.act_url' and access_action['url'] or '/report/pdf/account.report_invoice/' + str(object.id)
 
 <p>Dear
-% if object.partner_id.company_type == 'company' and object.child_ids:
+% if object.partner_id.is_company and object.child_ids:
     ${object.partner_id.child_ids[0].name}
 % else :
     ${object.partner_id.name}

addons/account/models/account_invoice.py
@@ -452,8 +452,8 @@ class AccountInvoice(models.Model):
                     res_dom = [('res_id', '=', 'res.partner,%s' % partner_id)]
                     rec_prop = prop.search(rec_dom + res_dom) or prop.search(rec_dom)
                     pay_prop = prop.search(pay_dom + res_dom) or prop.search(pay_dom)
-                    rec_account = rec_prop.get_by_record(rec_prop)
-                    pay_account = pay_prop.get_by_record(pay_prop)
+                    rec_account = rec_prop.get_by_record()
+                    pay_account = pay_prop.get_by_record()
                     if not rec_account and not pay_account:
                         action = self.env.ref('account.action_account_config')
                         msg = _('Cannot find a chart of accounts for this company, You should configure it. \nPlease go to Account Configuration.')

addons/account/views/report_overdue.xml
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-xs-5 col-xs-offset-7">
                     <span t-field="o.name"/><br/>
-                    <span t-raw="o._address_display(None, None)[o.id].replace('\n\n', '\n').replace('\n', '&lt;br&gt;')"/>
+                    <span t-raw="o.contact_address.replace('\n\n', '\n').replace('\n', '&lt;br&gt;')"/>
                     <span t-field="o.vat"/>
                 </div>
                 <p>

addons/base_setup/res_config.py
@@ -42,7 +42,7 @@ class base_config_settings(osv.osv_memory):
             'target': 'current',
         }
 
-    def get_default_company_share_partner(self, cr, uid, ids, fields, context=None):
+    def get_default_company_share_partner(self, cr, uid, fields, context=None):
         partner_rule = self.pool['ir.model.data'].xmlid_to_object(cr, uid, 'base.res_partner_rule', context=context)
         return {
             'company_share_partner': not bool(partner_rule.active)

addons/crm/crm_lead.py
@@ -9,7 +9,7 @@ from werkzeug import url_encode
 
 from openerp import SUPERUSER_ID
 from openerp import tools, api
-from openerp.addons.base.res.res_partner import format_address
+from openerp.addons.base.res.res_partner import FormatAddress
 from openerp.addons.crm import crm_stage
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
@@ -51,7 +51,7 @@ CRM_LEAD_FIELDS_TO_MERGE = ['name',
     'partner_name']
 
 
-class crm_lead(format_address, osv.osv):
+class crm_lead(FormatAddress, osv.osv):
     """ CRM Lead Case """
     _name = "crm.lead"
     _description = "Lead/Opportunity"

addons/crm/res_config.py
@@ -43,7 +43,7 @@ class crm_configuration(osv.TransientModel):
             alias_id = alias_ids and alias_ids[0] or False
         return alias_id
 
-    def get_default_generate_sales_team_alias(self, cr, uid, ids, context=None):
+    def get_default_generate_sales_team_alias(self, cr, uid, fields, context=None):
         return {'generate_sales_team_alias': self.pool['ir.values'].get_default(
             cr, uid, 'sales.config.settings', 'generate_sales_team_alias')}
 
@@ -51,7 +51,7 @@ class crm_configuration(osv.TransientModel):
         config_value = self.browse(cr, uid, ids, context=context).generate_sales_team_alias
         self.pool['ir.values'].set_default(cr, uid, 'sales.config.settings', 'generate_sales_team_alias', config_value)
 
-    def get_default_alias_prefix(self, cr, uid, ids, context=None):
+    def get_default_alias_prefix(self, cr, uid, fields, context=None):
         alias_name = False
         alias_id = self._find_default_lead_alias_id(cr, uid, context=context)
         if alias_id:

addons/google_calendar/res_config.py
@@ -10,16 +10,15 @@ class calendar_config_settings(osv.TransientModel):
         'server_uri': fields.char('URI for tuto')
     }
     
-    def set_calset(self,cr,uid,ids,context=None) :
+    def set_calset(self, cr, uid, ids, context=None) :
         params = self.pool['ir.config_parameter']
-        myself = self.browse(cr,uid,ids[0],context=context)
+        myself = self.browse(cr, uid, ids[0], context=context)
         params.set_param(cr, uid, 'google_calendar_client_id', (myself.cal_client_id or '').strip(), groups=['base.group_system'], context=None)
         params.set_param(cr, uid, 'google_calendar_client_secret', (myself.cal_client_secret or '').strip(), groups=['base.group_system'], context=None)
         
 
-    def get_default_all(self,cr,uid,ids,context=None):
+    def get_default_all(self, cr, uid, fields, context=None):
         params = self.pool.get('ir.config_parameter')
-        
         cal_client_id = params.get_param(cr, uid, 'google_calendar_client_id',default='',context=context)
         cal_client_secret = params.get_param(cr, uid, 'google_calendar_client_secret',default='',context=context)
         server_uri= "%s/google_account/authentication" % params.get_param(cr, uid, 'web.base.url',default="http://yourcompany.odoo.com",context=context)

commit f726d37575ac7dfc8c88424979d9125f8c9057ba

[FIX] base: rate with correct current user's company
Bug introduced in 32926e3.

As `self.env.user` is browsed with `sudo`, the result of this call
will always return the admin's current company and not the user's current
company.

opw-681809

openerp/addons/base/res/res_currency.py
@@ -36,7 +36,7 @@ class Currency(models.Model):
     @api.multi
     def _compute_current_rate(self):
         date = self._context.get('date') or fields.Datetime.now()
-        company_id = self._context.get('company_id') or self.env.user._get_company().id
+        company_id = self._context.get('company_id') or self.env['res.users']._get_company().id
         # the subquery selects the last rate before 'date' for the given currency/company
         query = """SELECT c.id, (SELECT r.rate FROM res_currency_rate r
                                   WHERE r.currency_id = c.id AND r.name <= %s