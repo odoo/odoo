6115d37b758d2068588076c215ac424c679f4a59
Simon Lejeune <sle@openerp.com>
2016-06-28 12:05:51 +0200

    [ADD] report: improve client action to allow sending `do_action` commands

--- addons/report/static/src/js/report.editor.js
+++ addons/report/static/src/js/report.editor.js
@@ -63,6 +63,29 @@ if (window.self !== window.top) {
             }
         }
     }, false);
+
+    // Allow to send commands to the webclient when the editor is disabled.
+    if (window.location.search.indexOf('enable_editor') === -1) {
+        // `do_action` command
+        $('[res-id][res-model][view-type]')
+            .wrap('<a/>')
+                .attr('href', '#')
+            .on('click', function (ev) {
+                ev.preventDefault();
+                var action = {
+                    'type': 'ir.actions.act_window',
+                    'view_type': $(this).attr('view-type'),
+                    'view_mode': $(this).attr('view-mode') || $(this).attr('view-type'),
+                    'res_id': Number($(this).attr('res-id')),
+                    'res_model': $(this).attr('res-model'),
+                    'views': [[$(this).attr('view-id') || false, $(this).attr('view-type')]],
+                };
+                window.parent.postMessage({
+                    'message': 'report:do_action',
+                    'action': action,
+                }, trusted_origin);
+            });
+    }
 }
 
 options.registry.many2one.include({
