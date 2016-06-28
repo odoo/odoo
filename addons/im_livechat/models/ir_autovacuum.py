2a3f37806f88e6a69ef53f7e54ac6cf2fb10c940
Christophe Simonis <chs@odoo.com>
2016-06-28 19:01:53 +0200

    [FIX] im_livechat: use new api when calling cleaning methods

--- addons/im_livechat/models/ir_autovacuum.py
+++ addons/im_livechat/models/ir_autovacuum.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from odoo import api, models
-
 
 class AutoVacuum(models.AbstractModel):
     _inherit = 'ir.autovacuum'
 
     @api.model
     def power_on(self, *args, **kwargs):
-        self.pool['mail.channel'].remove_empty_livechat_sessions()
-        self.pool['mail.channel.partner'].unpin_old_livechat_sessions()
+        self.env['mail.channel'].remove_empty_livechat_sessions()
+        self.env['mail.channel.partner'].unpin_old_livechat_sessions()
         return super(AutoVacuum, self).power_on(*args, **kwargs)
