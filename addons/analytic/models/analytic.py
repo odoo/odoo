9ae68521bff18bb6757fc323b17229abf0564755
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2016-06-30 16:15:01 +0200

    Improvements to performance for displaying analytic accounts and confirming invoices

--- addons/analytic/models/analytic.py
+++ addons/analytic/models/analytic.py
@@ -19,27 +19,25 @@ class account_analytic_account(models.Model):
 
     @api.multi
     def _compute_debit_credit_balance(self):
-        analytic_line_obj = self.env['account.analytic.line']
-        domain = [('account_id', 'in', self.mapped('id'))]
+        sql_query = ("""
+            SELECT account_id, SUM(amount)
+            FROM account_analytic_line
+            WHERE account_id IN %s
+            AND company_id = %s
+        """ % (tuple(self.ids), self.env.user.company_id.id))
         if self._context.get('from_date', False):
-            domain.append(('date', '>=', self._context['from_date']))
+            sql_query += (""" AND date >= %s""" % self._context['from_date'])
         if self._context.get('to_date', False):
-            domain.append(('date', '<=', self._context['to_date']))
-
-        account_amounts = analytic_line_obj.search_read(domain, ['account_id', 'amount'])
-        account_ids = set([line['account_id'][0] for line in account_amounts])
-        data_debit = {account_id: 0.0 for account_id in account_ids}
-        data_credit = {account_id: 0.0 for account_id in account_ids}
-        for account_amount in account_amounts:
-            if account_amount['amount'] < 0.0:
-                data_debit[account_amount['account_id'][0]] += account_amount['amount']
-            else:
-                data_credit[account_amount['account_id'][0]] += account_amount['amount']
+            sql_query += (""" AND date <= %s""" % self._context['to_date'])
+        sql_query += """ GROUP BY account_id"""
+        self.env.cr.execute(sql_query)
+        amounts = dict(self.env.cr.fetchall())
 
         for account in self:
-            account.debit = abs(data_debit.get(account.id, 0.0))
-            account.credit = data_credit.get(account.id, 0.0)
-            account.balance = account.credit - account.debit
+            amount = amounts.get(account.id, 0.0)
+            account.balance = amount
+            account.credit = amount > 0.0 and amount or 0.0
+            account.debit = amount < 0.0 and -amount or 0.0
 
     name = fields.Char(string='Analytic Account', index=True, required=True, track_visibility='onchange')
     code = fields.Char(string='Reference', index=True, track_visibility='onchange')
