22a5a5ad4a00750a91bab285a87f653a137f6fef
fwi-odoo <fwi@odoo.com>
2016-06-30 23:14:42 +0200

    [MOV] crm_partner_assign: merge into website_crm_partner_assign

--- addons/website_crm_partner_assign/models/crm_partner_assign.py
+++ addons/website_crm_partner_assign/models/crm_partner_assign.py
@@ -74,9 +74,9 @@ class crm_lead(osv.osv):
         'partner_assigned_id': fields.many2one('res.partner', 'Assigned Partner',track_visibility='onchange' , help="Partner this case has been forwarded/assigned to.", select=True),
         'partner_declined_ids': fields.many2many(
             'res.partner',
-            relation="crm_lead_declined_partner",
-            column1="lead_id",
-            column2="partner_id",
+            'crm_lead_declined_partner',
+            'lead_id',
+            'partner_id',
             string='Partner not interested'),
         'date_assign': fields.date('Assignation Date', help="Last date this case was forwarded/assigned to a partner"),
     }
@@ -98,6 +98,20 @@ class crm_lead(osv.osv):
                          'user_id' : user_id}
                    }
 
+    def assign_salesman_of_assigned_partner(self, cr, uid, ids, context=None):
+        salesmans_leads = {}
+        for lead in self.browse(cr, uid, ids, context=context):
+            if (lead.stage_id.probability > 0 and lead.stage_id.probability < 100) or lead.stage_id.sequence == 1: 
+                if lead.partner_assigned_id and lead.partner_assigned_id.user_id and lead.partner_assigned_id.user_id != lead.user_id:
+                    salesman_id = lead.partner_assigned_id.user_id.id
+                    if salesmans_leads.get(salesman_id):
+                        salesmans_leads[salesman_id].append(lead.id)
+                    else:
+                        salesmans_leads[salesman_id] = [lead.id]
+        for salesman_id, lead_ids in salesmans_leads.items():
+            salesteam_id = self.on_change_user(cr, uid, lead_ids, salesman_id, context=None)['value'].get('team_id')
+            self.write(cr, uid, lead_ids, {'user_id': salesman_id, 'team_id': salesteam_id}, context=context)
+
     def action_assign_partner(self, cr, uid, ids, context=None):
         return self.assign_partner(cr, uid, ids, partner_id=False, context=context)
 
@@ -111,7 +125,7 @@ class crm_lead(osv.osv):
             if not partner_id:
                 partner_id = partner_ids.get(lead.id, False)
             if not partner_id:
-                tag_to_add = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'website_crm_partner_assign', 'tag_portal_lead_partner_unavailable')[1]
+                tag_to_add = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'website_crm_partner_assign', 'tag_portal_lead_partner_unavailable')[1]       
                 self.write(cr, uid, [lead.id], {'tag_ids': [(4, tag_to_add, False)]}, context=context)
                 continue
             self.assign_geo_localize(cr, uid, [lead.id], lead.partner_latitude, lead.partner_longitude, context=context)
