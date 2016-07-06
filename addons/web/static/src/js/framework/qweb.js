odoo/odoo

commit 44543bca9394621a374263288869f8d21478f9af

[REF] web, *: move QWeb odoo init to web.QWeb
this js module centralizes all odoo initialization

addons/web/static/src/js/framework/qweb.js
@@ -1,0 +1,48 @@
+odoo.define('web.QWeb', function (require) {
+"use strict";
+
+var translation = require('web.translation');
+
+function QWeb(debug) {
+    var qweb = new QWeb2.Engine();
+    qweb.default_dict = {
+        '_' : _,
+        'JSON': JSON,
+        '_t' : translation._t,
+        '__debug__': debug,
+        'moment': function(date) { return new moment(date); },
+        'csrf_token': odoo.csrf_token,
+    };
+    qweb.debug = debug;
+    return qweb;
+}
+
+QWeb.prototype.preprocess_node = function() {
+    // Note that 'this' is the Qweb Node
+    switch (this.node.nodeType) {
+        case Node.TEXT_NODE:
+        case Node.CDATA_SECTION_NODE:
+            // Text and CDATAs
+            var translation = this.node.parentNode.attributes['t-translation'];
+            if (translation && translation.value === 'off') {
+                return;
+            }
+            var match = /^(\s*)([\s\S]+?)(\s*)$/.exec(this.node.data);
+            if (match) {
+                this.node.data = match[1] + _t(match[2]) + match[3];
+            }
+            break;
+        case Node.ELEMENT_NODE:
+            // Element
+            var attr, attrs = ['label', 'title', 'alt', 'placeholder'];
+            while ((attr = attrs.pop())) {
+                if (this.attributes[attr]) {
+                    this.attributes[attr] = _t(this.attributes[attr]);
+                }
+            }
+    }
+};
+
+return QWeb;
+
+});

addons/account/static/src/js/account_reconciliation_widgets.js
@@ -27,9 +27,6 @@ var _t = core._t;
 var QWeb = core.qweb;
 var bus = core.bus;
 
-var _ = require('_');
-var $ = require('$');
-
 function defaultIfUndef(variable, defaultValue) {
     return variable === undefined ? defaultValue : variable;
 }

addons/web/static/src/js/boot.js
@@ -36,11 +36,7 @@
     var job_deps = [];
     var job_deferred = [];
 
-    var services = Object.create({
-        qweb: new QWeb2.Engine(),
-        $: $,
-        _: _,
-    });
+    var services = Object.create({});
 
     var commentRegExp = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg;
     var cjsRequireRegExp = /[^.]\s*require\s*\(\s*["']([^'"\s]+)["']\s*\)/g;
@@ -270,5 +266,31 @@
         }, 4000);
     };
     $(log_when_loaded);
+
+
+    // IE patch
+    //-------------------------------------------------------------------------
+    if (typeof(console) === "undefined") {
+        // Even IE9 only exposes console object if debug window opened
+        window.console = {};
+        ('log error debug info warn assert clear dir dirxml trace group'
+            + ' groupCollapsed groupEnd time timeEnd profile profileEnd count'
+            + ' exception').split(/\s+/).forEach(function(property) {
+                console[property] = _.identity;
+        });
+    }
+
+    /**
+        Some hack to make placeholders work in ie9.
+    */
+    if (!('placeholder' in document.createElement('input'))) {
+        document.addEventListener("DOMNodeInserted",function(event){
+            var nodename =  event.target.nodeName.toLowerCase();
+            if ( nodename === "input" || nodename === "textarea" ) {
+                $(event.target).placeholder();
+            }
+        });
+    }
+
 
 })();

addons/web/static/src/js/framework/core.js
@@ -6,9 +6,7 @@ var mixins = require('web.mixins');
 var Registry = require('web.Registry');
 var translation = require('web.translation');
 
-var qweb = require('qweb');
-var _ = require('_');
-var $ = require('$');
+var QWeb = require('web.QWeb');
 
 var debug = $.deparam($.param.querystring()).debug !== undefined;
 
@@ -37,31 +35,6 @@ _.each('resize,scroll'.split(','), function(evtype) {
     });
 });
 
-// IE patch
-//-------------------------------------------------------------------------
-if (typeof(console) === "undefined") {
-    // Even IE9 only exposes console object if debug window opened
-    window.console = {};
-    ('log error debug info warn assert clear dir dirxml trace group'
-        + ' groupCollapsed groupEnd time timeEnd profile profileEnd count'
-        + ' exception').split(/\s+/).forEach(function(property) {
-            console[property] = _.identity;
-    });
-}
-
-/**
-    Some hack to make placeholders work in ie9.
-*/
-if (!('placeholder' in document.createElement('input'))) {    
-    document.addEventListener("DOMNodeInserted",function(event){
-        var nodename =  event.target.nodeName.toLowerCase();
-        if ( nodename === "input" || nodename == "textarea" ) {
-            $(event.target).placeholder();
-        }
-    });
-}
-
-
 
 /**
  * Lazy translation function, only performs the translation when actually
@@ -74,39 +47,9 @@ if (!('placeholder' in document.createElement('input'))) {
  * @param {String} s string to translate
  * @returns {Object} lazy translation object
  */
-qweb.debug = debug;
-_.extend(qweb.default_dict, {
-    '__debug__': debug,
-    'moment': function(date) { return new moment(date); },
-});
+var qweb = new QWeb(debug);
 
-qweb.default_dict.csrf_token = odoo.csrf_token;
 
-qweb.preprocess_node = function() {
-    // Note that 'this' is the Qweb Node
-    switch (this.node.nodeType) {
-        case Node.TEXT_NODE:
-        case Node.CDATA_SECTION_NODE:
-            // Text and CDATAs
-            var translation = this.node.parentNode.attributes['t-translation'];
-            if (translation && translation.value === 'off') {
-                return;
-            }
-            var match = /^(\s*)([\s\S]+?)(\s*)$/.exec(this.node.data);
-            if (match) {
-                this.node.data = match[1] + _t(match[2]) + match[3];
-            }
-            break;
-        case Node.ELEMENT_NODE:
-            // Element
-            var attr, attrs = ['label', 'title', 'alt', 'placeholder'];
-            while ((attr = attrs.pop())) {
-                if (this.attributes[attr]) {
-                    this.attributes[attr] = _t(this.attributes[attr]);
-                }
-            }
-    }
-};
 
 /** Setup jQuery timeago */
 /*

addons/web/static/src/js/framework/translation.js
@@ -3,8 +3,6 @@ odoo.define('web.translation', function (require) {
 "use strict";
 
 var Class = require('web.Class');
-var qweb = require('qweb');
-
 
 var TranslationDataBase = Class.extend(/** @lends instance.TranslationDataBase# */{
     init: function() {
@@ -80,12 +78,6 @@ var _lt = function (s) {
     return {toString: function () { return _t(s); }};
 };
 
-qweb.default_dict = {
-    '_' : _,
-    'JSON': JSON,
-    '_t' : _t,
-};
-
 return {
     _t: _t,
     _lt: _lt,
@@ -93,4 +85,3 @@ return {
 };
 
 });
-

addons/web/static/test/testing.js
@@ -10,11 +10,7 @@ odoo.testing.start_services = function () {
             deps: factory.deps,
         };
     });
-    var services = Object.create({
-        qweb: new QWeb2.Engine(),
-        $: $,
-        _: _,
-    });
+    var services = Object.create({});
   return odoo.process_jobs(jobs, services);
 };
 
@@ -95,7 +91,7 @@ odoo.define_section = function (name, section_deps) {
         QUnit.test(name, function (assert) {
             var services = odoo.testing.start_services();
             var deps = _.map(section_deps.concat(dep_names), function (name) { return services[name]; });
-            services.qweb.add_template(odoo.testing.templates);
+            services['web.core'].qweb.add_template(odoo.testing.templates);
             mock.clear();
             mock.interceptRPC(services['web.session']);
             var info = {
@@ -144,4 +140,3 @@ QUnit.moduleDone(function(result) {
     }
 
 });
-

addons/web/views/webclient_templates.xml
@@ -67,6 +67,7 @@
         <script type="text/javascript" src="/web/static/src/js/framework/model.js"></script>
         <script type="text/javascript" src="/web/static/src/js/framework/dom_utils.js"></script>
         <script type="text/javascript" src="/web/static/src/js/framework/utils.js"></script>
+        <script type="text/javascript" src="/web/static/src/js/framework/qweb.js"></script>
         <script type="text/javascript" src="/web/static/src/js/framework/core.js"></script>
         <script type="text/javascript" src="/web/static/src/js/framework/dialog.js"></script>
         <script type="text/javascript" src="/web/static/src/js/framework/local_storage.js"></script>

addons/web_editor/views/iframe.xml
@@ -41,8 +41,9 @@
                 if(window['odoo']) {
                     odoo.define('web.csrf', function (require) {
                         var token = "<t t-esc="request.csrf_token(None)"/>";
-                        require('web.core').csrf_token = token;
-                        require('qweb').default_dict.csrf_token = token;
+                        var core = require('web.core');
+                        core.csrf_token = token;
+                        core.qweb.default_dict.csrf_token = token;
                     });
                 }
             </script>

commit e1d5e0ae4040c93aeb7ecdc6a0350520d77061f1

[FIX] web: qweb: correctly set `preprocess_node` on qweb engine
issue: xml template are not translated anymore

rev 44543bca moved qweb code inside the `qweb.js` file and also changed
the behavior with the introduction of a `QWeb` factory returning instances
of qweb rendering engine. However, we have to set on the rendering engine
instance a `preprocess_node` function that will translate the qweb
templates.

The solution for this issue (also introduced with rev 44543bca) was
to set this function on the prototype of the QWeb factory, and it was
obviously not enough as it has to be attached to the rendering engine
instance.

In order to benefit from the fact that the `preprocess_node` function
is attached to the prototype of the QWeb factory, we could attach it to
the rendering engine instance with something like
`qweb.preprocessed_node = this.preprocessed_node`, however i find that
conceptually attaching a translate function to a factory does not make
sense, si i simply made `preprocess_node` a free function and i set it
on rendering engine instance.

addons/web/static/src/js/framework/qweb.js
@@ -16,10 +16,11 @@ function QWeb(debug, default_dict) {
         'csrf_token': odoo.csrf_token,
     });
     qweb.debug = debug;
+    qweb.preprocess_node = preprocess_node;
     return qweb;
 }
 
-QWeb.prototype.preprocess_node = function() {
+function preprocess_node() {
     // Note that 'this' is the Qweb Node
     switch (this.node.nodeType) {
         case Node.TEXT_NODE:
@@ -43,7 +44,7 @@ QWeb.prototype.preprocess_node = function() {
                 }
             }
     }
-};
+}
 
 return QWeb;
 