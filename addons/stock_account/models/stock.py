0d7c79f899921aa5af79b0ebabb26f802b1ae0ed
Thibault Delavallée <tde@openerp.com>
2016-07-07 10:55:15 +0200

    mergeme in migr

--- addons/stock_account/models/stock.py
+++ addons/stock_account/models/stock.py
@@ -10,7 +10,7 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-from odoo import new_fields
+from odoo import fields as new_fields
 
 
 class StockInventory(models.Model):
@@ -36,14 +36,14 @@ class StockInventory(models.Model):
 class StockLocation(models.Model):
     _inherit = "stock.location"
 
-    valuation_in_account_id = fields.Many2one(
+    valuation_in_account_id = new_fields.Many2one(
         'account.account', 'Stock Valuation Account (Incoming)',
         domain=[('internal_type', '=', 'other'), ('deprecated', '=', False)],
         help="Used for real-time inventory valuation. When set on a virtual location (non internal type), "
              "this account will be used to hold the value of products being moved from an internal location "
              "into this location, instead of the generic Stock Output Account set on the product. "
              "This has no effect for internal locations.")
-    valuation_out_account_id = fields.Many2one(
+    valuation_out_account_id = new_fields.Many2one(
         'account.account', 'Stock Valuation Account (Outgoing)',
         domain=[('internal_type', '=', 'other'), ('deprecated', '=', False)],
         help="Used for real-time inventory valuation. When set on a virtual location (non internal type), "
@@ -173,7 +173,7 @@ class StockQuant(models.Model):
         return res
 
     def _create_account_move_line(self, cr, uid, ids, move, credit_account_id, debit_account_id, journal_id, context=None):
-        quants = self.brows(cr, uid, ids, context=context)
+        quants = self.browse(cr, uid, ids, context=context)
         #group quants by cost
         quant_cost_qty = {}
         for quant in quants:
