93c678c18826b144ae2851f333dc3869dcb9b159
Thibault Delavallée <tde@openerp.com>
2016-07-07 13:43:38 +0200

    migr stock_account next IV

--- addons/stock_account/models/stock.py
+++ addons/stock_account/models/stock.py
@@ -216,43 +216,33 @@ class StockMove(models.Model):
                 ctx = dict(context or {}, force_company=move.company_id.id)
                 product_obj.write(cr, SUPERUSER_ID, [product.id], {'standard_price': new_std_price}, context=ctx)
 
-    def product_price_update_after_done(self, cr, uid, ids, context=None):
-        '''
-        This method adapts the price on the product when necessary
-        '''
-        for move in self.browse(cr, uid, ids, context=context):
-            #adapt standard price on outgoing moves if the product cost_method is 'real', so that a return
-            #or an inventory loss is made using the last value used for an outgoing valuation.
-            if move.product_id.cost_method == 'real' and move.location_dest_id.usage != 'internal':
-                #store the average price of the move on the move and product form
-                self._store_average_cost_price(cr, uid, [move.id], context=context)
+    @api.multi
+    def product_price_update_after_done(self):
+        ''' Adapt standard price on outgoing moves if the product cost_method is 'real', so that a
+        return or an inventory loss is made using the last value used for an outgoing valuation. '''
+        to_update_moves = self.filtered(lambda move: move.product_id.cost_method == 'real' and move.location_dest_id.usage != 'internal')
+        to_update_moves._store_average_cost_price()
 
-    def _store_average_cost_price(self, cr, uid, ids, context=None):
-        ''' move is a browe record '''
-        move = self.browse(cr, uid, ids, context=context)[0]
-        product_obj = self.pool.get('product.product')
-        if any([q.qty <= 0 for q in move.quant_ids]) or move.product_qty == 0:
-            #if there is a negative quant, the standard price shouldn't be updated
-            return
-        #Note: here we can't store a quant.cost directly as we may have moved out 2 units (1 unit to 5€ and 1 unit to 7€) and in case of a product return of 1 unit, we can't know which of the 2 costs has to be used (5€ or 7€?). So at that time, thanks to the average valuation price we are storing we will valuate it at 6€
-        average_valuation_price = 0.0
-        for q in move.quant_ids:
-            average_valuation_price += q.qty * q.cost
-        average_valuation_price = average_valuation_price / move.product_qty
-        # Write the standard price, as SUPERUSER_ID because a warehouse manager may not have the right to write on products
-        ctx = dict(context or {}, force_company=move.company_id.id)
-        product_obj.write(cr, SUPERUSER_ID, [move.product_id.id], {'standard_price': average_valuation_price}, context=ctx)
-        self.write(cr, uid, [move.id], {'price_unit': average_valuation_price}, context=context)
+    def _store_average_cost_price(self):
+        """ Store the average price of the move on the move and product form """
+        for move in self:
+            # product_obj = self.pool.get('product.product')
+            if any(q.qty <= 0 for q in move.quant_ids) or move.product_qty == 0:
+                # if there is a negative quant, the standard price shouldn't be updated
+                return
+            # Note: here we can't store a quant.cost directly as we may have moved out 2 units
+            # (1 unit to 5€ and 1 unit to 7€) and in case of a product return of 1 unit, we can't
+            # know which of the 2 costs has to be used (5€ or 7€?). So at that time, thanks to the
+            # average valuation price we are storing we will valuate it at 6€
+            valuation_price = sum(q.qty * q.cost for q in move.quant_ids)
+            average_valuation_price = valuation_price / move.product_qty
+
+            move.product_id.with_context(force_company=move.company_id.id).sudo().write({'standard_price': average_valuation_price})
+            move.write({'price_unit': average_valuation_price})
 
     def _get_accounting_data_for_valuation(self, cr, uid, ids, context=None):
-        """
-        Return the accounts and journal to use to post Journal Entries for the real-time
-        valuation of the quant.
-
-        :param context: context dictionary that can explicitly mention the company to consider via the 'force_company' key
-        :returns: journal_id, source account, destination account, valuation account
-        :raise: openerp.exceptions.UserError if any mandatory account or journal is not defined.
-        """
+        """ Return the accounts and journal to use to post Journal Entries for
+        the real-time valuation of the quant. """
         move = self.browse(cr, uid, ids, context=context)[0]
         product_obj = self.pool.get('product.template')
         accounts = product_obj.browse(cr, uid, move.product_id.product_tmpl_id.id, context).get_product_accounts()
