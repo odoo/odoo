232eea36ab98eb268f712b2083c637a7cb463e2c
Denis Vermylen (dve) <dve@odoo.com>
2016-07-08 11:30:12 +0200

    [MIG] website_quote: migrate to new api

--- addons/website_quote/models/product_template.py
+++ addons/website_quote/models/product_template.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import osv, fields
+from odoo import fields, models
 
-class product_template(osv.Model):
+
+class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    _columns = {
-        'website_description': fields.html('Description for the website'), # hack, if website_sale is not installed
-        'quote_description': fields.html('Description for the quote'),
-    }
+    website_description = fields.Html('Description for the website', sanitize=False) # hack, if website_sale is not installed
+    quote_description = fields.Html('Description for the quote', sanitize=False)
