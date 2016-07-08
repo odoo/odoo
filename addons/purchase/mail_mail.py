a32b3b220d5018017dac8dd5419843df25efa5a9
Thibault Delavallée <tde@openerp.com>
2016-07-08 12:20:37 +0200

    [REF] mail: clean mail_mail postprocess method having v7 and v8 implementation
    Old method postprocess_sent_message is removed. New api style method
    introduced when migrating the module is now used.

--- addons/purchase/mail_mail.py
+++ addons/purchase/mail_mail.py
@@ -5,9 +5,10 @@ class mail_mail(osv.Model):
     _name = 'mail.mail'
     _inherit = 'mail.mail'
 
-    def _postprocess_sent_message(self, cr, uid, mail, context=None, mail_sent=True):
-        if mail_sent and mail.model == 'purchase.order':
-            obj = self.pool.get('purchase.order').browse(cr, uid, mail.res_id, context=context)
-            if obj.state == 'draft':
-                self.pool.get('purchase.order').signal_workflow(cr, uid, [mail.res_id], 'send_rfq')
-        return super(mail_mail, self)._postprocess_sent_message(cr, uid, mail=mail, context=context, mail_sent=mail_sent)
+    def _postprocess_sent_message(self, cr, uid, ids, context=None, mail_sent=True):
+        for mail in self.browse(cr, uid, ids, context=context):
+            if mail_sent and mail.model == 'purchase.order':
+                obj = self.pool.get('purchase.order').browse(cr, uid, mail.res_id, context=context)
+                if obj.state == 'draft':
+                    self.pool.get('purchase.order').signal_workflow(cr, uid, [mail.res_id], 'send_rfq')
+        return super(mail_mail, self)._postprocess_sent_message(cr, uid, ids, context=context, mail_sent=mail_sent)
