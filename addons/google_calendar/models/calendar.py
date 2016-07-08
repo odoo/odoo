9b39275d859372ef384abd24cdcc4c116a26f075
Mohammed Shekha <msh@openerp.com>
2016-07-08 10:13:15 +0200

    [MIG] google_calendar: migrate to new api
    No behaviour changes, only migrate code and add
    some docstring.

--- addons/google_calendar/models/calendar.py
+++ addons/google_calendar/models/calendar.py
@@ -1,67 +1,63 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from datatime import datetime
-from openerp.osv import fields, osv
-
-import logging
-_logger = logging.getLogger(__name__)
+from odoo import api, fields, models
 
 
-class calendar_event(osv.Model):
+class Meeting(models.Model):
+
     _inherit = "calendar.event"
 
-    def get_fields_need_update_google(self, cr, uid, context=None):
-        recurrent_fields = self._get_recurrent_fields(cr, uid, context=context)
+    oe_update_date = fields.Datetime('Odoo Update Date')
+
+    @api.model
+    def get_fields_need_update_google(self):
+        recurrent_fields = self._get_recurrent_fields()
         return recurrent_fields + ['name', 'description', 'allday', 'start', 'date_end', 'stop',
-                                   'attendee_ids', 'alarm_ids', 'location', 'class', 'active',
+                                   'attendee_ids', 'alarm_ids', 'location', 'privacy', 'active',
                                    'start_date', 'start_datetime', 'stop_date', 'stop_datetime']
 
-    def write(self, cr, uid, ids, vals, context=None):
-        if context is None:
-            context = {}
-        sync_fields = set(self.get_fields_need_update_google(cr, uid, context))
-        if (set(vals.keys()) & sync_fields) and 'oe_update_date' not in vals.keys() and 'NewMeeting' not in context:
-            vals['oe_update_date'] = datetime.now()
+    @api.multi
+    def write(self, values):
+        sync_fields = set(self.get_fields_need_update_google())
+        if (set(values.keys()) and sync_fields) and 'oe_update_date' not in values.keys() and 'NewMeeting' not in self._context:
+            values['oe_update_date'] = fields.Datetime.now()
+        return super(Meeting, self).write(values)
 
-        return super(calendar_event, self).write(cr, uid, ids, vals, context=context)
-
-    def copy(self, cr, uid, id, default=None, context=None):
+    @api.multi
+    def copy(self, default=None):
         default = default or {}
         if default.get('write_type', False):
             del default['write_type']
         elif default.get('recurrent_id', False):
-            default['oe_update_date'] = datetime.now()
+            default['oe_update_date'] = fields.Datetime.now()
         else:
             default['oe_update_date'] = False
-        return super(calendar_event, self).copy(cr, uid, id, default, context)
+        return super(Meeting, self).copy(default)
 
-    def unlink(self, cr, uid, ids, can_be_deleted=False, context=None):
-        return super(calendar_event, self).unlink(cr, uid, ids, can_be_deleted=can_be_deleted, context=context)
-
-    _columns = {
-        'oe_update_date': fields.datetime('Odoo Update Date'),
-    }
+    @api.multi
+    def unlink(self, can_be_deleted=False):
+        return super(Meeting, self).unlink(can_be_deleted=can_be_deleted)
 
 
-class calendar_attendee(osv.Model):
+class Attendee(models.Model):
+
     _inherit = 'calendar.attendee'
 
-    _columns = {
-        'google_internal_event_id': fields.char('Google Calendar Event Id'),
-        'oe_synchro_date': fields.datetime('Odoo Synchro Date'),
-    }
-    _sql_constraints = [('google_id_uniq', 'unique(google_internal_event_id,partner_id,event_id)', 'Google ID should be unique!')]
+    google_internal_event_id = fields.Char('Google Calendar Event Id')
+    oe_synchro_date = fields.Datetime('Odoo Synchro Date')
 
-    def write(self, cr, uid, ids, vals, context=None):
-        if context is None:
-            context = {}
+    _sql_constraints = [
+        ('google_id_uniq', 'unique(google_internal_event_id,partner_id,event_id)', 'Google ID should be unique!')
+    ]
 
-        for id in ids:
-            ref = vals.get('event_id', self.browse(cr, uid, id, context=context).event_id.id)
+    @api.multi
+    def write(self, values):
+        for attendee in self:
+            meeting_id_to_update = values.get('event_id', attendee.event_id.id)
 
             # If attendees are updated, we need to specify that next synchro need an action
             # Except if it come from an update_from_google
-            if not context.get('curr_attendee', False) and not context.get('NewMeeting', False):
-                self.pool['calendar.event'].write(cr, uid, ref, {'oe_update_date': datetime.now()}, context)
-        return super(calendar_attendee, self).write(cr, uid, ids, vals, context=context)
+            if not self._context.get('curr_attendee', False) and not self._context.get('NewMeeting', False):
+                self.env['calendar.event'].browse(meeting_id_to_update).write({'oe_update_date': fields.Datetime.now()})
+        return super(Attendee, self).write(values)
--- addons/google_calendar/models/res_config.py
+++ addons/google_calendar/models/res_config.py
@@ -1,29 +1,24 @@
-from openerp.osv import fields, osv
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-class calendar_config_settings(osv.TransientModel):
+from odoo import fields, models
+
+
+class CalendarSettings(models.TransientModel):
+
     _inherit = 'base.config.settings'
-    
-    _columns = {
-        'google_cal_sync': fields.boolean("Show Tutorial"),
-        'cal_client_id': fields.char("Client_id"),
-        'cal_client_secret': fields.char("Client_key"),
-        'server_uri': fields.char('URI for tuto')
-    }
-    
-    def set_calset(self, cr, uid, ids, context=None) :
-        params = self.pool['ir.config_parameter']
-        myself = self.browse(cr, uid, ids[0], context=context)
-        params.set_param(cr, uid, 'google_calendar_client_id', (myself.cal_client_id or '').strip(), groups=['base.group_system'], context=None)
-        params.set_param(cr, uid, 'google_calendar_client_secret', (myself.cal_client_secret or '').strip(), groups=['base.group_system'], context=None)
-        
 
-    def get_default_all(self, cr, uid, fields, context=None):
-        params = self.pool.get('ir.config_parameter')
-        cal_client_id = params.get_param(cr, uid, 'google_calendar_client_id',default='',context=context)
-        cal_client_secret = params.get_param(cr, uid, 'google_calendar_client_secret',default='',context=context)
-        server_uri= "%s/google_account/authentication" % params.get_param(cr, uid, 'web.base.url',default="http://yourcompany.odoo.com",context=context)
-        return dict(cal_client_id=cal_client_id,cal_client_secret=cal_client_secret,server_uri=server_uri)
-        
-        
-        
-    
+    google_cal_sync = fields.Boolean("Show Tutorial")
+    cal_client_id = fields.Char("Client_id")
+    cal_client_secret = fields.Char("Client_key")
+    server_uri = fields.Char('URI for tuto')
+
+    def set_calset(self):
+        self.env['ir.config_parameter'].set_param('google_calendar_client_id', (self.cal_client_id or '').strip(), groups=['base.group_system'])
+        self.env['ir.config_parameter'].set_param('google_calendar_client_secret', (self.cal_client_secret or '').strip(), groups=['base.group_system'])
+
+    def get_default_all(self, fields):
+        cal_client_id = self.env['ir.config_parameter'].get_param('google_calendar_client_id', default='')
+        cal_client_secret = self.env['ir.config_parameter'].get_param('google_calendar_client_secret', default='')
+        server_uri = "%s/google_account/authentication" % self.env['ir.config_parameter'].get_param('web.base.url', default="http://yourcompany.odoo.com")
+        return dict(cal_client_id=cal_client_id, cal_client_secret=cal_client_secret, server_uri=server_uri)
