a4fae95d46e26aa9255a97dda63786a301029594
jeffery chen fan <jeffery9@gmail.com>
2016-07-10 11:54:49 +0800

    imporve the descriptions and fix the wrong account

--- addons/l10n_cn/__openerp__.py
+++ addons/l10n_cn/__openerp__.py
@@ -7,19 +7,17 @@
 # Copyright (C) 2008-now  开阖软件 jeff@osbzr.com    < PM and LTS >
 
 {
-    'name': '中国会计科目表',
+    'name': '中国本地化基础数据',
     'version': '1.8',
     'category': 'Localization',
     'author': 'www.openerp-china.org',
     'maintainer': 'jeff@osbzr.com',
     'website': 'http://openerp-china.org',
     'description': """
-
-    科目类型\会计科目表模板\增值税\辅助核算类别\管理会计凭证簿\财务会计凭证簿
-
-    添加中文省份数据
-
-    增加小企业会计科目表
+此模块包含本地化数据
+==============================
+1, 中国省份数据
+2, 科目类型
 
     """,
     'depends': ['base', 'account'],
