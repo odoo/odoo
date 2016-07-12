afebd52437ff68f042b06e3084efc52b375b8e61
Jérome Maes <jem@openerp.com>
2016-07-12 13:30:22 +0200

    [MIG] website_portal*: migration to new api
    Those modules were almost already migrated, this commit
    simply modified the import and the xml tags.

--- addons/website_portal_sale/models/sale_order.py
+++ addons/website_portal_sale/models/sale_order.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp import api, models
+from odoo import api, models
 
 
-class sale_order(models.Model):
+class SaleOrder(models.Model):
+
     _inherit = 'sale.order'
 
     @api.multi
@@ -13,7 +14,7 @@ class sale_order(models.Model):
         of the classic form view, redirect to the online quote if exists. """
         self.ensure_one()
         if self.state in ['draft', 'cancel']:
-            return super(sale_order, self).get_access_action()
+            return super(SaleOrder, self).get_access_action()
         return {
             'type': 'ir.actions.act_url',
             'url': '/my/orders/%s' % self.id,
