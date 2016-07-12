b755ee80a505f9641c9f547b6a372da6f41c0045
Thibault Delavallée <tde@openerp.com>
2016-07-12 15:55:30 +0200

    [MOV] sale_stock: reorganize module

---
+++ addons/sale_stock/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import account_invoice
+from . import procurement
+from . import product
+from . import res_company
+from . import sale_order
+from . import sale_config_settings
+from . import stock
