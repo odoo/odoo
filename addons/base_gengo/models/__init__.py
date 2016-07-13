9fd3c9e7f0b49d250e89f473aa564ce5e90cdf82
Yannick Tivisse <yti@odoo.com>
2016-07-13 13:24:40 +0200

    [MOV] base_gengo: Reorganized File Structure

--- addons/base_gengo/models/__init__.py
+++ addons/base_gengo/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import models
-import controller
-import wizard
+import ir_translation
+import res_company
