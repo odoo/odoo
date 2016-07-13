d321d0a762b040bc1dbc0081eda7130b22265346
Yannick Tivisse <yti@odoo.com>
2016-07-13 15:35:29 +0200

    [MIG] base_gengo: Migrated to new API

--- addons/base_gengo/models/res_company.py
+++ addons/base_gengo/models/res_company.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
+from odoo import fields, models
 
 
-class res_company(osv.Model):
-    _name = "res.company"
+class res_company(models.Model):
     _inherit = "res.company"
-    _columns = {
-           "gengo_private_key": fields.text("Gengo Private Key", copy=False, groups="base.group_system"),
-           "gengo_public_key": fields.text("Gengo Public Key", copy=False, groups="base.group_user"),
-           "gengo_comment": fields.text("Comments", help="This comment will be automatically be enclosed in each an every request sent to Gengo", groups="base.group_user"),
-           "gengo_auto_approve": fields.boolean("Auto Approve Translation ?", help="Jobs are Automatically Approved by Gengo.", groups="base.group_user"),
-           "gengo_sandbox": fields.boolean("Sandbox Mode", help="Check this box if you're using the sandbox mode of Gengo, mainly used for testing purpose."),
-    }
 
-    _defaults = {
-        "gengo_auto_approve": True,
-    }
+    gengo_private_key = fields.Text(string="Gengo Private Key", copy=False, groups="base.group_system")
+    gengo_public_key = fields.Text(string="Gengo Public Key", copy=False, groups="base.group_user")
+    gengo_comment = fields.Text(string="Comments", groups="base.group_user",
+      help="This comment will be automatically be enclosed in each an every request sent to Gengo")
+    gengo_auto_approve = fields.Boolean(string="Auto Approve Translation ?", groups="base.group_user", default=True,
+      help="Jobs are Automatically Approved by Gengo.")
+    gengo_sandbox = fields.Boolean(string="Sandbox Mode",
+      help="Check this box if you're using the sandbox mode of Gengo, mainly used for testing purpose.")
