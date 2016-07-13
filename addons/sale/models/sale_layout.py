aa95faf724a0396635c2bbfd6c319d88d961395d
fwi-odoo <fwi@odoo.com>
2016-07-13 15:58:22 +0200

    [MIG] sale: migrate models

--- addons/sale/models/sale_layout.py
+++ addons/sale/models/sale_layout.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import osv, fields
+from odoo import fields, models
 
 
-class SaleLayoutCategory(osv.Model):
+class SaleLayoutCategory(models.Model):
     _name = 'sale.layout_category'
     _order = 'sequence, id'
-    _columns = {
-        'name': fields.char('Name', required=True, translate=True),
-        'sequence': fields.integer('Sequence', required=True),
-        'subtotal': fields.boolean('Add subtotal'),
-        'pagebreak': fields.boolean('Add pagebreak')
-    }
 
-    _defaults = {
-        'subtotal': True,
-        'pagebreak': False,
-        'sequence': 10
-    }
+    name = fields.Char('Name', required=True, translate=True)
+    sequence = fields.Integer('Sequence', required=True, default=10)
+    subtotal = fields.Boolean('Add subtotal', default=True)
+    pagebreak = fields.Boolean('Add pagebreak')
