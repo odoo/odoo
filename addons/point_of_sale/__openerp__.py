a2a6936056e8bb67af1f52a6a18c32b775eeaedd
dut-odoo <dut@odoo.com>
2016-07-13 12:44:58 +0530

    [REF] point_of_sale: remove sale dependency of POS
    Stock account is sufficient for Point of Sale. No need to set sale_stock as
    dependency, as it installs Sales Management but is not required for running
    the POS.

--- addons/point_of_sale/__openerp__.py
+++ addons/point_of_sale/__openerp__.py
@@ -28,7 +28,7 @@ Main Features
 * Allows the user to create an invoice automatically
 * Refund previous sales
     """,
-    'depends': ['sale_stock', 'barcodes'],
+    'depends': ['stock_account', 'barcodes'],
     'data': [
         'security/point_of_sale_security.xml',
         'security/ir.model.access.csv',
