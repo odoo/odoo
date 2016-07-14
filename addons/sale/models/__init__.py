4fc03f8b874fe35181a5be6b050f1a1215390ff6
fwi-odoo <fwi@odoo.com>
2016-07-14 13:59:58 +0200

    [MOV] sale: split sale.py

--- addons/sale/models/__init__.py
+++ addons/sale/models/__init__.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import account_invoice
+import procurement
+import product_product
+import product_template
+import res_company
 import res_config
 import res_partner
 import sale
--- addons/sale/wizard/__init__.py
+++ addons/sale/wizard/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import mail_compose_message
 import sale_make_invoice_advance
