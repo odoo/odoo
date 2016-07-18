51632328f684c484415f45de251205e8aaee8b85
Ravi Patel <rap@odoo.com>
2016-07-18 18:14:49 +0530

    [IMP] prodcut_expiry: 'Product Alert Time' improvement
    - Hide life_time, use_time, removal_time and alert_time if tracking = no tracking
    - Change the tooltip of these fields(life_time, use_time, removal_time, alert_time)
    - Add filter 'Expiration Alert' on the Lot/Serial Number list view based on the lot/sn alert date
    - Add a tag 'Expiration alert' when the alert_date is reached.

--- addons/product_expiry/models/product_product.py
+++ addons/product_expiry/models/product_product.py
@@ -8,10 +8,10 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     life_time = fields.Integer(string='Product Life Time',
-        help='When a new a Serial Number is issued, this is the number of days before the goods may become dangerous and must not be consumed.')
+        help='Number of days before the goods may become dangerous and must not be consumed. It will be computed on the lot/serial number.')
     use_time = fields.Integer(string='Product Use Time',
-        help='When a new a Serial Number is issued, this is the number of days before the goods starts deteriorating, without being dangerous yet.')
+        help='Number of days before the goods starts deteriorating, without being dangerous yet. It will be computed on the lot/serial number.')
     removal_time = fields.Integer(string='Product Removal Time',
-        help='When a new a Serial Number is issued, this is the number of days before the goods should be removed from the stock.')
+        help='Number of days before the goods should be removed from the stock. It will be computed on the lot/serial number.')
     alert_time = fields.Integer(string='Product Alert Time',
-        help='When a new a Serial Number is issued, this is the number of days before an alert should be notified.')
+        help='Number of days before an alert should be raised on the lot/serial number.')
