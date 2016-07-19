29dd6ed8f06ff89d2864cab6d07ef0811d425822
fwi-odoo <fwi@odoo.com>
2016-07-19 10:18:25 +0200

    [MOV] account_check_printing: reorganize files according to guidelines

--- addons/account_check_printing/__openerp__.py
+++ addons/account_check_printing/__openerp__.py
@@ -10,13 +10,13 @@ It must be used as a dependency for modules that provide country-specific check 
 The check settings are located in the accounting journals configuration page.
     """,
     'website': 'https://www.odoo.com/page/accounting',
-    'depends' : ['account'],
+    'depends': ['account'],
     'data': [
-        'data/check_printing.xml',
-        'views/account_journal_dashboard_view.xml',
-        'views/account_journal_view.xml',
-        'views/account_payment_view.xml',
-        'wizard/print_pre_numbered_checks.xml'
+        'data/account_check_printing_data.xml',
+        'views/account_journal_dashboard_views.xml',
+        'views/account_journal_views.xml',
+        'views/account_payment_views.xml',
+        'wizard/print_prenumbered_checks_views.xml'
     ],
     'installable': True,
     'auto_install': False,
