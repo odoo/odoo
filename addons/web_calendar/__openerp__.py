d8b652d706fcd9b89eb95f70aebb16926d0c5deb
fwi-odoo <fwi@odoo.com>
2016-07-20 15:02:19 +0200

    [MOV][IMP] web_calendar: rename files according to guidelines, replace openerp and add license

--- addons/web_calendar/__openerp__.py
+++ addons/web_calendar/__openerp__.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'Web Calendar',
     'category': 'Hidden',
@@ -10,7 +13,7 @@ Odoo Web Calendar view.
     'version': '2.0',
     'depends': ['web'],
     'data' : [
-        'views/web_calendar.xml',
+        'views/web_calendar_templates.xml',
     ],
     'qweb': [
         'static/src/xml/*.xml',
