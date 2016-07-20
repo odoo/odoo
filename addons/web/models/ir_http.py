4ff34147d32f930b8fcb09bd692d9b5705f37827
Simon Lejeune <sle@openerp.com>
2016-07-20 12:23:58 +0200

    [FIX] web: session_info: introduce `is_superuser` key
    This is done in order to enable the tour service only for the
    superuser. Commit ce7a5904a47710 changed the meaning of `is_admin`
    to achieve this but broke the fact that some features of the debug
    manager are available to admin and not only to the superuser. Anyway
    a `is_admin` key should mean that the user is in the admin group.
    
    We could have checked client side that the uid is equal to 1 but
    adding a `is_superuser` key is more elegant and easier to grep.
    Also fix the only occurence of a uid check in the weblcient.
    
    This reverts commit ce7a5904a47710ef17172125b22b0dc356369804.

--- addons/web/models/ir_http.py
+++ addons/web/models/ir_http.py
@@ -21,7 +21,8 @@ class Http(models.AbstractModel):
         return {
             "session_id": request.session_id,
             "uid": request.session.uid,
-            "is_admin": request.env.uid == openerp.SUPERUSER_ID,
+            "is_admin": request.env.user.has_group('base.group_system'),
+            "is_superuser": request.env.user._is_superuser(),
             "user_context": request.session.get_context() if request.session.uid else {},
             "db": request.session.db,
             "server_version": version_info.get('server_version'),
