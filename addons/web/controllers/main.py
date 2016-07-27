65dcc7e0b921fef9783fd43166da51aedfb65931
Jairo Llopis <yajo.sk8@gmail.com>
2016-07-27 09:38:23 +0200

    [FIX][web] Detect image MIME.
    Odoo v8 lacks a MIME autodetect method, but backporting the most relevant parts from v9, we can make it detect the image type and serve the correct header, to allow some client-side manipulations to work fine.

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -1017,7 +1017,7 @@ class Binary(http.Controller):
         last_update = '__last_update'
         Model = request.registry[model]
         cr, uid, context = request.cr, request.uid, request.context
-        headers = [('Content-Type', 'image/png')]
+        headers = list()
         etag = request.httprequest.headers.get('If-None-Match')
         hashed_session = hashlib.md5(request.session_id).hexdigest()
         retag = hashed_session
@@ -1057,6 +1057,20 @@ class Binary(http.Controller):
             image_data = self.placeholder()
         headers.append(('ETag', retag))
         headers.append(('Content-Length', len(image_data)))
+
+        # Guess mime type
+        signatures = {
+            "image/jpeg": ['\xFF\xD8\xFF\xE0', '\xFF\xD8\xFF\xE2',
+                           '\xFF\xD8\xFF\xE3', '\xFF\xD8\xFF\xE1'],
+            "image/gif": ['GIF87a', 'GIF89a'],
+        }
+        mime = False
+        for key, matches in signatures.iteritems():
+            if any(image_data.startswith(match) for match in matches):
+                mime = key
+                break
+        headers.append(("Content-Type", mime or "image/png"))
+
         try:
             ncache = int(kw.get('cache'))
             headers.append(('Cache-Control', 'no-cache' if ncache == 0 else 'max-age=%s' % (ncache)))
