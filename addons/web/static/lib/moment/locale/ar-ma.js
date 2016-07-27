8cae6371687c125693c7c10b8b664af57c598cc0
kifcaliph <kifcaliph@hotmail.com>
2016-07-27 13:35:48 +0300

    Update ar-ma.js

--- addons/web/static/lib/moment/locale/ar-ma.js
+++ addons/web/static/lib/moment/locale/ar-ma.js
@@ -49,7 +49,7 @@
             yy : "%d سنوات"
         },
         week : {
-            dow : 6, // Saturday is the first day of the week.
+            dow : 1, // Saturday is the first day of the week.
             doy : 12  // The week that contains Jan 1st is the first week of the year.
         }
     });
