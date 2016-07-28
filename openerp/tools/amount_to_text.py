9219a7a70d9185a60b131d46ae6832d2b791eec8
Magno Costa - Akretion <magno.costa@akretion.com.br>
2016-07-28 11:44:17 -0300

    Change br to pt_BR to keep translations standards.

--- openerp/tools/amount_to_text.py
+++ openerp/tools/amount_to_text.py
@@ -161,50 +161,50 @@ def amount_to_text_nl(number, currency):
 # Brazilian Portuguese
 # -------------------------------------------------------------
 
-to_19_br = ('Zero', 'Um', 'Dois', u'Três', 'Quatro', 'Cinco', 'Seis', 'Sete',
-                'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze',
-                'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove')
-tens_br = ('Vinte', 'Trinta', 'Quarenta', 'Cinquenta',
-           'Sessenta', 'Setenta', 'Oitenta', 'Noventa')
-hundreds_br = ('Cem', 'Duzentos', 'Trezentos', 'Quatrocentos', 'Quinhentos',
-               'Seicentos', 'Setecentos', 'Oitocentos', 'Novecentos')
-denom_br = ('', 'Mil', u'Milhão', u'Bilhão', u'Trilhão', u'Quatrilhão',
-            u'Quintilhão', u'Sextilhão', u'Septilhão', u'Octilhão',
-            u'Nonilhão', u'Decilhão', u'Undecilhão', u'Duodecilhão',
-            u'Tredecilhão', u'Quattuordecilhão', u'Quindecilhão',
-            u'Sexdecilhão', u'Septendecillion', u'Octodecilhão',
-            u'Novendecilhão', u'Vigintilhão')
-denoms_br = ('', 'Mil', u'Milhões', u'Bilhões', u'Trilhões', u'Quatrilhões',
-             u'Quintilhões', u'Sextilhões', u'Septilhões', u'Octilhões',
-             u'Nonilhões', u'Decilhões', u'Undecilhões', u'Duodecilhões',
-             u'Tredecilhões', u'Quattuordecilhões', u'Quindecilhões',
-             u'Sexdecilhões', u'Septendecilliões', u'Octodecilhões',
-             u'Novendecilhões', u'Vigintilhões')
+to_19_pt_BR = ('Zero', 'Um', 'Dois', u'Três', 'Quatro', 'Cinco', 'Seis',
+               'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze',
+               'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito',
+               'Dezenove')
+tens_pt_BR = ('Vinte', 'Trinta', 'Quarenta', 'Cinquenta',
+              'Sessenta', 'Setenta', 'Oitenta', 'Noventa')
+hundreds_pt_BR = ('Cem', 'Duzentos', 'Trezentos', 'Quatrocentos', 'Quinhentos',
+                  'Seicentos', 'Setecentos', 'Oitocentos', 'Novecentos')
+denom_pt_BR = ('', 'Mil', u'Milhão', u'Bilhão', u'Trilhão', u'Quatrilhão',
+               u'Quintilhão', u'Sextilhão', u'Septilhão', u'Octilhão',
+               u'Nonilhão', u'Decilhão', u'Undecilhão', u'Duodecilhão',
+               u'Tredecilhão', u'Quattuordecilhão', u'Quindecilhão',
+               u'Sexdecilhão', u'Septendecillion', u'Octodecilhão',
+               u'Novendecilhão', u'Vigintilhão')
+denoms_pt_BR = ('', 'Mil', u'Milhões', u'Bilhões', u'Trilhões', u'Quatrilhões',
+                u'Quintilhões', u'Sextilhões', u'Septilhões', u'Octilhões',
+                u'Nonilhões', u'Decilhões', u'Undecilhões', u'Duodecilhões',
+                u'Tredecilhões', u'Quattuordecilhões', u'Quindecilhões',
+                u'Sexdecilhões', u'Septendecilliões', u'Octodecilhões',
+                u'Novendecilhões', u'Vigintilhões')
 
 
-def _convert_nn_br(val):
+def _convert_nn_pt_BR(val):
     """ convert a value < 100 to Brazilian Portuguese
     """
     if val < 20:
-        return to_19_br[val]
-    for (dcap, dval) in ((k, 20 + (10 * v)) for (v, k) in enumerate(tens_br)):
+        return to_19_pt_BR[val]
+    for (dcap, dval) in (
+            (k, 20 + (10 * v)) for (v, k) in enumerate(tens_pt_BR)):
         if dval + 10 > val:
             if val % 10:
-                return dcap + ' e ' + to_19_br[val % 10]
+                return dcap + ' e ' + to_19_pt_BR[val % 10]
             return dcap
 
 
-def _convert_nnn_br(val):
+def _convert_nnn_pt_BR(val):
     """ convert a value < 1000 to Brazilian Portuguese
         special cased because it is the level that kicks
         off the < 100 special case.  The rest are more general.
-        This also allows you to get strings in the form of
-        'forty-five hundred' if called directly.
     """
     word = ''
     if val > 0:
         for (dcap, dval) in (
-                (k, 100 + (100 * v)) for (v, k) in enumerate(hundreds_br)):
+                (k, 100 + (100 * v)) for (v, k) in enumerate(hundreds_pt_BR)):
             if dval + 100 > val:
                 (mod, rem) = (val % 100, val // 100)
                 if rem > 0:
@@ -212,24 +212,24 @@ def _convert_nnn_br(val):
                     if mod > 0:
                         word += ' e '
                 if mod > 0:
-                    word += _convert_nn_br(mod)
+                    word += _convert_nn_pt_BR(mod)
                 return word
 
 
 def brazil_number(val):
     if val < 100:
-        return _convert_nn_br(val)
+        return _convert_nn_pt_BR(val)
     if val < 1000:
-        return _convert_nnn_br(val)
-    for (didx, dval) in ((v - 1, 1000 ** v) for v in range(len(denom_br))):
+        return _convert_nnn_pt_BR(val)
+    for (didx, dval) in ((v - 1, 1000 ** v) for v in range(len(denom_pt_BR))):
         if dval > val:
             mod = 1000 ** didx
             l = val // mod
             r = val - (l * mod)
             if mod > 99999 and l > 2:
-                ret = _convert_nnn_br(l) + ' ' + denoms_br[didx]
+                ret = _convert_nnn_pt_BR(l) + ' ' + denoms_pt_BR[didx]
             else:
-                ret = _convert_nnn_br(l) + ' ' + denom_br[didx]
+                ret = _convert_nnn_pt_BR(l) + ' ' + denom_pt_BR[didx]
 
             if r > 0:
                 ret = ret + ', ' + brazil_number(r)
@@ -237,7 +237,7 @@ def brazil_number(val):
             return ret
 
 
-def amount_to_text_br(number, currency):
+def amount_to_text_pt_BR(number, currency):
     number = '%.2f' % number
     units_name = currency
     list = str(number).split('.')
@@ -257,7 +257,7 @@ def amount_to_text_br(number, currency):
 _translate_funcs = {
     'fr': amount_to_text_fr,
     'nl': amount_to_text_nl,
-    'br': amount_to_text_br,
+    'pt_BR': amount_to_text_pt_BR,
 }
 
 
