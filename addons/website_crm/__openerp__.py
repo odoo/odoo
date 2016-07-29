59174c56634acef0cce7be2f83a03f363be43a4c
Jeremy Kersten <jke@odoo.com>
2016-07-29 16:44:37 +0200

    [FIX] website_crm: update category
    When you come from front end -> install apps,
    the applications are filtered on category = website.
    This module should be in this list.

--- addons/website_crm/__openerp__.py
+++ addons/website_crm/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Contact Form',
-    'category': 'Sales',
+    'category': 'Website',
     'website': 'https://www.odoo.com/page/website-builder',
     'summary': 'Create Leads From Contact Form',
     'version': '2.0',
