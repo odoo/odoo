1d1e64427ad864acbf0567b447e079be7d89b1f0
fwi-odoo <fwi@odoo.com>
2016-08-01 16:06:00 +0200

    [MIG] website_blog: migrate py files

--- addons/website_blog/tests/__init__.py
+++ addons/website_blog/tests/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.addons.website_blog.tests import test_ui
-from openerp.addons.website_blog.tests import test_website_blog_flow
+from odoo.addons.website_blog.tests import test_ui
+from odoo.addons.website_blog.tests import test_website_blog_flow
