13e0cacc63e87b4fc3b11d249ad4a39bfa196cb8
Jitendra Prajapati <jpr@openerp.com>
2016-08-01 17:48:21 +0530

    [MIG] website_form_editor: Migrate to new api

--- tests/test_website_form_editor.py
+++ tests/test_website_form_editor.py
@@ -1,7 +1,12 @@
-import openerp.tests
-@openerp.tests.common.at_install(False)
-@openerp.tests.common.post_install(True)
-class TestWebsiteFormEditor(openerp.tests.HttpCase):
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# -*- coding: utf-8 -*-
+
+import odoo.tests
+
+
+@odoo.tests.common.at_install(False)
+@odoo.tests.common.post_install(True)
+class TestWebsiteFormEditor(odoo.tests.HttpCase):
     def test_tour(self):
         self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('website_form_editor_tour', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.website_form_editor_tour", login="admin")
         self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('website_form_editor_tour_submit', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.website_form_editor_tour_submit")
