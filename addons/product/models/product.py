e36db7a7afb5b36a75f715b314eb956a1cbc1046
fwi-odoo <fwi@odoo.com>
2016-08-03 10:33:49 +0200

    [IMP] product: only display attributes with multiple possible values

--- addons/product/models/product.py
+++ addons/product/models/product.py
@@ -376,7 +376,10 @@ class ProductProduct(models.Model):
 
         result = []
         for product in self.sudo():
-            variant = ", ".join([v.name for v in product.attribute_value_ids])
+            # display only the attributes with multiple possible values on the template
+            variable_attributes = product.attribute_line_ids.filtered(lambda l: len(l.value_ids) > 1).mapped('attribute_id')
+            variant = ", ".join([v.name for v in product.attribute_value_ids if v.attribute_id in variable_attributes])
+
             name = variant and "%s (%s)" % (product.name, variant) or product.name
             sellers = []
             if partner_ids:
