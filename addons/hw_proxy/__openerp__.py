56456359499963511dadd9e1a4c13ab530fc7812
Rohan Patel <rop@odoo.com>
2016-08-03 17:44:07 +0530

    [MIG] hw_proxy : Migrated to new API.

--- addons/hw_proxy/__openerp__.py
+++ addons/hw_proxy/__openerp__.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
 {
     'name': 'Hardware Proxy',
-    'version': '1.0',
     'category': 'Point Of Sale',
     'sequence': 6,
     'summary': 'Connect the Web Client to Hardware Peripherals',
@@ -19,9 +17,4 @@ This modules only contains the enabling framework. The actual devices drivers
 are found in other modules that must be installed separately. 
 
 """,
-    'depends': [],
-    'test': [
-    ],
-    'installable': True,
-    'auto_install': False,
 }
