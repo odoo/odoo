d74f09abb4176ebbbae1876e73d2461194d74512
Denis Vermylen (dve) <dve@odoo.com>
2016-08-04 16:46:16 +0200

    [FIX] hr_holidays: correct subscriptions on leaves
    followers were subscribed only when leaves were confirmed (from draft), but default
    state is 'confirm'. Thus only leaves that were reset to draft and re-confirmed
    were correctly followed. Now followers are subscribed in create and write when
    state is 'confirm'.

--- addons/hr_holidays/models/hr_holidays.py
+++ addons/hr_holidays/models/hr_holidays.py
@@ -355,6 +355,8 @@ class Holidays(models.Model):
         if not self._check_state_access_right(values):
             raise AccessError(_('You cannot set a leave request as \'%s\'. Contact a human resource manager.') % values.get('state'))
         holiday = super(Holidays, self.with_context(mail_create_nolog=True, mail_create_nosubscribe=True)).create(values)
+        if values.get('state') == 'confirm':
+            self.subscribe_followers()
         holiday.add_follower(employee_id)
         return holiday
 
@@ -364,6 +366,8 @@ class Holidays(models.Model):
         if not self._check_state_access_right(values):
             raise AccessError(_('You cannot set a leave request as \'%s\'. Contact a human resource manager.') % values.get('state'))
         result = super(Holidays, self).write(values)
+        if values.get('state') == 'confirm':
+            self.subscribe_followers()
         self.add_follower(employee_id)
         return result
 
@@ -376,6 +380,20 @@ class Holidays(models.Model):
     ####################################################
     # Business methods
     ####################################################
+
+    @api.multi
+    def subscribe_followers(self):
+        for holiday in self:
+            if holiday.department_id:
+                # Subscribe the followers of the department following the `confirmed` subtype
+                # It's done manually because `message_auto_subscribe` only works on fields for which
+                # the value is passed to the `create` or `write` methods,
+                # it doesn't work for related/computed fields
+                # This can be removed as soon as `department_id` on `hr.holidays` becomes a regular
+                # fields or as soon as `message_auto_subscribe` works with related/computed fields.
+                confirmed_subtype = self.env.ref('hr_holidays.mt_department_holidays_confirmed')
+                partner_ids = [follower.partner_id.id for follower in holiday.department_id.message_follower_ids if confirmed_subtype in follower.subtype_ids]
+                holiday.message_subscribe(partner_ids)
 
     @api.multi
     def _create_resource_leave(self):
@@ -418,17 +436,6 @@ class Holidays(models.Model):
     def action_confirm(self):
         if self.filtered(lambda holiday: holiday.state != 'draft'):
             raise UserError(_('Leave request must be in Draft state ("To Submit") in order to confirm it.'))
-        for holiday in self:
-            if holiday.department_id:
-                # Subscribe the followers of the department following the `confirmed` subtype
-                # It's done manually because `message_auto_subscribe` only works on fields for which
-                # the value is passed to the `create` or `write` methods,
-                # it doesn't work for related/computed fields
-                # This can be removed as soon as `department_id` on `hr.holidays` becomes a regular
-                # fields or as soon as `message_auto_subscribe` works with related/computed fields.
-                confirmed_subtype = self.env.ref('hr_holidays.mt_department_holidays_confirmed')
-                partner_ids = [follower.partner_id.id for follower in holiday.department_id.message_follower_ids if confirmed_subtype in follower.subtype_ids]
-                holiday.message_subscribe(partner_ids)
         return self.write({'state': 'confirm'})
 
     @api.multi
