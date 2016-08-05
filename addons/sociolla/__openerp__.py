c37501c17951c198c69cfbc57b1e8e11ce704541
yonghax <melfice_hax@hotmail.com>
2016-08-05 14:41:00 +0700

    [ADD] price_undiscounted on sale.order sale.order.line

--- addons/sociolla/__openerp__.py
+++ addons/sociolla/__openerp__.py
@@ -20,7 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'product'],
+    'depends': [
+        'base', 
+        'account', 
+        'product', 
+        'sale'
+    ],
 
     # always loaded
     'data': [
--- addons/sociolla/models/sale.py
+++ addons/sociolla/models/sale.py
@@ -9,6 +9,7 @@ class SaleOrder(models.Model):
     _inherit= "sale.order"
 
     discount_amount = fields.Monetary(string='Discount Amount', store=True, readonly=True, compute='_amount_all', track_visibility='always')
+    price_undiscounted = fields.Monetary(string='Undiscount Amount', store=True, readonly=True, compute='_amount_all', track_visibility='always')
 
     @api.depends('order_line.price_total')
     def _amount_all(self):
@@ -16,20 +17,26 @@ class SaleOrder(models.Model):
         Compute the total amounts of the SO, add compute discount
         """
         for order in self:
-            amount_untaxed = amount_tax = discount_amount = 0.0
+            amount_untaxed = amount_tax = discount_amount = price_undiscounted = 0.0
             for line in order.order_line:
                 amount_untaxed += line.price_subtotal
                 amount_tax += line.price_tax
+                price_undiscounted += line.price_undiscounted
+                discount_amount += line.discount_amount
+
             order.update({
                 'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
                 'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
                 'amount_total': amount_untaxed + amount_tax,
+                'discount_amount': discount_amount,
+                'price_undiscounted': price_undiscounted,
             })
 
 class SaleOrderLine(models.Model):
     _inherit="sale.order.line"
 
     discount_amount = fields.Monetary(string='Discount Amount', store=True, readonly=True, compute='_compute_amount', track_visibility='always')
+    price_undiscounted = fields.Monetary(string='Undiscount Amount', store=True, readonly=True, compute='_compute_amount', track_visibility='always')
 
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
     def _compute_amount(self):
@@ -38,10 +45,13 @@ class SaleOrderLine(models.Model):
         """
         for line in self:
             price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-            discount_amount = line.price_unit * (line.discount or 0.0) / 100.0
-            taxes = line.tax_id.compute_all(price, discount_amount, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_id)
+            price_undiscounted = round(line.product_uom_qty * line.price_unit) 
+            discount_amount = price_undiscounted * ((line.discount or 0.0) / 100.0)
+
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_id)
             line.update({
                 'discount_amount': discount_amount,
+                'price_undiscounted': price_undiscounted,
                 'price_tax': taxes['total_included'] - taxes['total_excluded'],
                 'price_total': taxes['total_included'],
                 'price_subtotal': taxes['total_excluded'],
