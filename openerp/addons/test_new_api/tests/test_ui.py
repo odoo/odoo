5828558137a431f7ee49b8dad3b1cc59fdb4184a
Christophe Matthieu <chm@odoo.com>
2016-08-09 16:10:21 +0200

    [IMP] test_new_api: convert widget_x2many tour to use web_tour

--- openerp/addons/test_new_api/tests/test_ui.py
+++ openerp/addons/test_new_api/tests/test_ui.py
@@ -1,10 +1,11 @@
-import openerp.tests
+import odoo.tests
 
-@openerp.tests.common.at_install(False)
-@openerp.tests.common.post_install(True)
-class TestUi(openerp.tests.HttpCase):
+class TestUi(odoo.tests.HttpCase):
+    post_install = True
+    at_install = False
+
     def test_01_admin_widget_x2many(self):
         self.phantom_js("/web#action=test_new_api.action_discussions",
-                        "odoo.__DEBUG__.services['web.Tour'].run('widget_x2many', 'test')",
-                        "odoo.__DEBUG__.services['web.Tour'].tours.widget_x2many",
-                        login="admin")
+                        "odoo.__DEBUG__.services['web_tour.tour'].run('widget_x2many', 100)",
+                        "odoo.__DEBUG__.services['web_tour.tour'].tours.widget_x2many.ready",
+                        login="admin", timeout=120)
