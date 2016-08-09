72c262e9ea51297337a93e28cd9e57689e4e08e3
Jairo Llopis <yajo.sk8@gmail.com>
2016-08-09 13:16:27 +0200

    [test_new_api] Tests that show weaknesses of ORM one2many fields
    Current Odoo ORM status gives no sane way to work with one2many fields, and these tests are written imitating several develop styles and situations:
    
    - Using `.new()` and adding those records with `|=` to a o2m field.
    - Same with `.create()`.
    - Using `[(5, 0, 0), (0, 0, {...}), ...]` style.

---
+++ openerp/addons/test_new_api/tests/test_cache.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from openerp.tests.common import TransactionCase
+
+
+class CacheCase(object):
+    def setUp(self):
+        super(CacheCase, self).setUp()
+        self.Line = self.env["test_new_api.multi.line"]
+        self.multi = self.env["test_new_api.multi"].create({
+            "name": "What is up?"
+        })
+
+    def test_unlink(self):
+        """Unlinking NewId removes it in cache and after invalidating it."""
+        self.multi.lines[-1].unlink()
+        self.assertEqual(len(self.multi.lines), 9)
+        self.multi.invalidate_cache()
+        self.assertEqual(len(self.multi.lines), 9)
+
+    def test_names_ok(self):
+        """Line names were saved fine."""
+        expected = set(map(str, range(10)))
+        self.assertEqual(expected, set(self.multi.lines.mapped("name")))
+
+    def test_modify(self):
+        """Editing a NewId removes it in cache and after invalidating it."""
+        self.multi.lines[-1].name = "hello!"
+        self.assertIn("hello!", self.multi.lines.mapped("name"))
+        self.multi.invalidate_cache()
+        self.assertIn("hello!", self.multi.lines.mapped("name"))
+
+
+class ORMNewStyleCacheCase(CacheCase, TransactionCase):
+    def setUp(self):
+        super(ORMNewStyleCacheCase, self).setUp()
+        for name in range(10):
+            self.multi.lines |= self.Line.new({"name": str(name)})
+
+
+class ORMCreateStyleCacheCase(CacheCase, TransactionCase):
+    def setUp(self):
+        super(ORMCreateStyleCacheCase, self).setUp()
+        for name in range(10):
+            self.multi.lines |= self.Line.create({"name": str(name)})
+
+
+class RPCStyleCacheCase(CacheCase, TransactionCase):
+    def setUp(self):
+        super(RPCStyleCacheCase, self).setUp()
+        new = [(5, False, False)]
+        for name in range(10):
+            new += [(0, False, {"name": str(name)})]
+        self.multi.lines = new
