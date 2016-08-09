a33e808ca63dffa30e7185f7325900e04bf337df
fwi-odoo <fwi@odoo.com>
2016-08-09 14:23:18 +0200

    [MOV] sale: re-organise directory structure according to new API guidelines

---
+++ addons/sale/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import res_partner
+import sale
+import sale_analytic
+import sale_config_settings
+import sale_layout
+import sales_team
