c1b2d74bc3eb8fed1a20fafbe7fdaa9377e388ad
Thibault Delavallée <tde@openerp.com>
2016-08-09 11:14:57 +0200

    [FIX] account, purchase: fix leftover select fields parameters

--- addons/account/models/account.py
+++ addons/account/models/account.py
@@ -232,7 +232,7 @@ class AccountJournal(models.Model):
 
     #groups_id = fields.Many2many('res.groups', 'account_journal_group_rel', 'journal_id', 'group_id', string='Groups')
     currency_id = fields.Many2one('res.currency', help='The currency used to enter statement', string="Currency", oldname='currency')
-    company_id = fields.Many2one('res.company', string='Company', required=True, index=1, default=lambda self: self.env.user.company_id,
+    company_id = fields.Many2one('res.company', string='Company', required=True, index=True, default=lambda self: self.env.user.company_id,
         help="Company related to this journal")
 
     refund_sequence = fields.Boolean(string='Dedicated Refund Sequence', help="Check this box if you don't want to share the same sequence for invoices and refunds made from this journal", default=False)
