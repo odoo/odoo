f3d715ca94f24f86ce39a4bc6873fe7576ef1860
fwi-odoo <fwi@odoo.com>
2016-08-09 15:00:16 +0200

    [SPLIT] sale: Split files according to related model

--- addons/sale/models/__init__.py
+++ addons/sale/models/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import account_config_settings
+import account_invoice
+import procurement_order
+import product_product
+import product_template
+import res_company
 import res_partner
 import sale
 import sale_analytic
