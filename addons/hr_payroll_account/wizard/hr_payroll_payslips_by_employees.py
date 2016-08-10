07c0814b560edef27b7cc1a5e91e5a5bab22e687
Richard Mathot <rim@odoo.com>
2016-08-10 13:39:23 +0200

    [FIX] hr_payroll_account: [] are not ()
    In memory of Mariner 1.

--- addons/hr_payroll_account/wizard/hr_payroll_payslips_by_employees.py
+++ addons/hr_payroll_account/wizard/hr_payroll_payslips_by_employees.py
@@ -10,5 +10,5 @@ class HrPayslipEmployees(models.TransientModel):
     def compute_sheet(self):
         journal_id = False
         if self.env.context.get('active_id'):
-            journal_id = self.env['hr.payslip.run'].browse(self.env.context.get['active_id']).journal_id.id
+            journal_id = self.env['hr.payslip.run'].browse(self.env.context.get('active_id')).journal_id.id
         return super(HrPayslipEmployees, self).with_context(journal_id=journal_id).compute_sheet()
