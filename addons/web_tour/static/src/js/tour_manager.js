03e75e06cdd518b76f90d48e2a1c9fe5b7a08630
Christophe Matthieu <chm@odoo.com>
2016-08-25 15:56:52 +0200

    [FIX] web_tour: better simulate text changes
    The change event has to be triggered when changing an input, a select
    and even another DOM element with contentEditable attribute.
    
    When changing an input, the "keydown" and "keyup" events also had to
    be triggered.

--- addons/web_tour/static/src/js/tour_manager.js
+++ addons/web_tour/static/src/js/tour_manager.js
@@ -63,7 +63,7 @@ var RunningTourActionHelper = core.Class.extend({
 
         text = text || "Test";
         if (values.consume_event === "input") {
-            values.$element.val(text).trigger("input");
+            values.$element.trigger("keydown").val(text).trigger("keyup").trigger("input");
         } else if (values.$element.is("select")) {
             values.$element.children("option")
                 .prop("selected", false).removeProp("selected")
@@ -73,6 +73,7 @@ var RunningTourActionHelper = core.Class.extend({
         } else {
             values.$element.text(text);
         }
+        values.$element.trigger("change");
     },
     _drag_and_drop: function (values, to) {
         var $to = $(to || document.body);
