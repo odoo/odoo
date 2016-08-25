0f8f68dfa75ea077fb8e5aaff0e11ea99b446303
Christophe Matthieu <chm@odoo.com>
2016-08-25 15:01:16 +0200

    [IMP] web_editor: convert tours to use web_tour.tour

--- addons/web_editor/tests/test_ui.py
+++ addons/web_editor/tests/test_ui.py
@@ -11,7 +11,7 @@ class TestUi(odoo.tests.HttpCase):
     at_install = False
 
     def test_01_admin_rte(self):
-        self.phantom_js("/web", "odoo.__DEBUG__.services['web.Tour'].run('rte', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.rte", login='admin')
+        self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('rte')", "odoo.__DEBUG__.services['web_tour.tour'].tours.rte.ready", login='admin')
 
     def test_02_admin_rte_inline(self):
-        self.phantom_js("/web", "odoo.__DEBUG__.services['web.Tour'].run('rte_inline', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.rte", login='admin')
+        self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('rte_inline')", "odoo.__DEBUG__.services['web_tour.tour'].tours.rte_inline.ready", login='admin')
