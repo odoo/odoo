d39fbea9ad5d8dc130171488297312a27cd347aa
Christophe Matthieu <chm@odoo.com>
2016-08-25 15:03:05 +0200

    [IMP] website_forum: convert tours to use web_tour.tour

--- addons/website_forum/tests/test_forum_process.py
+++ addons/website_forum/tests/test_forum_process.py
@@ -2,10 +2,11 @@
 
 import odoo.tests
 
-
-@odoo.tests.common.at_install(False)
-@odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
+
+    post_install = True
+    at_install = False
+
     def test_01_admin_forum_tour(self):
         self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('question')", "odoo.__DEBUG__.services['web_tour.tour'].tours.question.ready", login="admin")
 
@@ -15,4 +16,4 @@ class TestUi(odoo.tests.HttpCase):
             forum = env.ref('website_forum.forum_help')
             demo = env.ref('base.user_demo')
             demo.karma = forum.karma_post + 1
-        self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('forum_question', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.forum_question", login="demo")
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('forum_question')", "odoo.__DEBUG__.services['web_tour.tour'].tours.forum_question.ready", login="demo")
