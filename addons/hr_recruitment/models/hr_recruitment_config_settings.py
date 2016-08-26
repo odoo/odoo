8162cc8648ca724af381deea94fa1e1358c30e3d
Jérome Maes <jem@openerp.com>
2016-08-26 10:01:59 +0200

    [IMP] hr_recruitment: remove stupid option
    Disabling this resumes indexation option will uninstall
    hr_recruitment module, since it removes 'document', which
    is in the dependencies of recruitment.
    This makes no sense at all and should have been remove a
    long time ago.

--- addons/hr_recruitment/models/hr_recruitment_config_settings.py
+++ addons/hr_recruitment/models/hr_recruitment_config_settings.py
@@ -8,11 +8,6 @@ class RecruitmentSettings(models.TransientModel):
     _name = 'hr.recruitment.config.settings'
     _inherit = ['res.config.settings']
 
-    module_document = fields.Selection(selection=[
-        (0, "Do not manage CVs and motivation letter"),
-        (1, 'Allow the automatic indexation of resumes')
-        ], string='Resumes')
-
     module_hr_recruitment_survey = fields.Selection(selection=[
         (0, "Do not use interview forms"),
         (1, "Use interview forms during the recruitment process")
