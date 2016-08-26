d269eb0eb62d88e02c4fa33b84178d0e73d82ef1
Raphael Collet <rco@openerp.com>
2016-08-26 17:00:48 +0200

    [FIX] website: add missing module dependency `base_setup`

--- addons/website/__openerp__.py
+++ addons/website/__openerp__.py
@@ -13,7 +13,7 @@ Odoo Website CMS
 ===================
 
         """,
-    'depends': ['web', 'web_editor', 'web_planner'],
+    'depends': ['base_setup', 'web', 'web_editor', 'web_planner'],
     'installable': True,
     'data': [
         'data/website_data.xml',
