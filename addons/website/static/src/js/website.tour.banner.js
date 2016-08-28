c34346934c24e7ec87401a6505bcb1ab6c7f52a9
Fabien Pinckaers <fp@openerp.com>
2016-08-29 01:30:48 +0200

    [IMP] website: minitour to install contact form on contact us page (usability: people think there is no contact form)

--- addons/website/static/src/js/website.tour.banner.js
+++ addons/website/static/src/js/website.tour.banner.js
@@ -54,3 +54,27 @@ odoo.define("website.tour.banner", function (require) {
         position: "bottom",
     }]);
 });
+
+
+odoo.define("website.tour.contact", function (require) {
+    "use strict";
+
+    var core = require("web.core");
+    var tour = require("web_tour.tour");
+    var base = require("web_editor.base");
+    var _t = core._t;
+
+    tour.register("contact", {
+        url: "/page/contactus",
+        wait_for: base.ready(),
+    }, [{
+        trigger: "li#customize-menu",
+        content: _t("<b>Install a contact form</b> to improve this page."),
+        extra_trigger: "#o_contact_mail",
+        position: "bottom",
+    }, {
+        trigger: "li#install_apps",
+        content: _t("<b>Install new apps</b> to get more features. Let's install the <i>'Contact form'</i> app."),
+        position: "right",
+    }]);
+});
