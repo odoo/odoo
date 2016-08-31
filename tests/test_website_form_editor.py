faebb4a1e2802a5f90fc8b24dbabe72dd75f39ef
Christophe Matthieu <chm@odoo.com>
2016-08-31 16:05:27 +0200

    [IMP] website_form_editor: convert test to web_tour

--- tests/test_website_form_editor.py
+++ tests/test_website_form_editor.py
@@ -8,6 +8,6 @@ import odoo.tests
 @odoo.tests.common.post_install(True)
 class TestWebsiteFormEditor(odoo.tests.HttpCase):
     def test_tour(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('website_form_editor_tour', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.website_form_editor_tour", login="admin")
-        self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('website_form_editor_tour_submit', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.website_form_editor_tour_submit")
-        self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('website_form_editor_tour_results', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.website_form_editor_tour_results", login="admin")
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('website_form_editor_tour')", "odoo.__DEBUG__.services['web_tour.tour'].tours.website_form_editor_tour.ready", login="admin")
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('website_form_editor_tour_submit')", "odoo.__DEBUG__.services['web_tour.tour'].tours.website_form_editor_tour_submit.ready")
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('website_form_editor_tour_results')", "odoo.__DEBUG__.services['web_tour.tour'].tours.website_form_editor_tour_results.ready", login="admin")
