db0c2d54174fec820798df3aff2fe89f9adc7f08
Odoo en Ecuador <odoo-group-ecuador@users.noreply.github.com>
2016-08-31 11:10:40 -0500

    Delete __init__.py

--- l10n_ec_ats/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from . import tablas_model
-from . import pestana_sri
-from . import search_by_x_identificacion
-from . import account_invoice
-from . import account_tax
