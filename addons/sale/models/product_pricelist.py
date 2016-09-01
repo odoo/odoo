583521f39cd661b4c54bfad235499ab9b3d74419
Jeremy Kersten <jke@odoo.com>
2016-09-01 13:43:09 +0200

    [REF] sale: Merge product_visible_discount in sale module

--- addons/sale/models/product_pricelist.py
+++ addons/sale/models/product_pricelist.py
@@ -9,5 +9,5 @@ class ProductPricelist(models.Model):
 
     discount_policy = fields.Selection([
         ('with_discount', 'Discount included in the price'),
-        ('without_discount', 'Show discount in the sale order')],
+        ('without_discount', 'Show public price & discount to the customer')],
         default='with_discount')
