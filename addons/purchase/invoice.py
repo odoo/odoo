a57b10dbd534c9a2e4a691783d74902a68abfbd8
qdp-odoo <qdp@odoo.com>
2016-09-02 10:48:43 +0200

    [FIX] purchase: fix dropship use case in anglo saxon with perpetual valuation.
    OPW: 684742
    When using dropship+anglo-saxon+perpetual valuation, there is no journal move for the delivery to debit outgoing inventory (since the goods don't transit by an internal stock) but the sale does credit it so there was a build up in the holding account that has to be moved out manually. This was also reported in #12687.
    
    The solution implemented is to check if the invoice line is related to sale order lines having one of its procurement_ids with a purchase_line_id set. If yes, it means that it is a confirmed dropship and in that case we don't call to super (we don't create the cost of sale line).
    
    That means that:
    * If the procurement is in exception at the customer invoice time, the behavior will be as it is currently, but it's fine as you don't know how the procurement will be solved, and it'll be only at the beginning (once the config is done it shouldn't go in exception anymore). People will have to manually fix those amounts with a miscellaneous operation.
    * users in anglo saxon mode should not use the 'stock interim account (received)' on supplier invoices for dropshipped goods, but rather use the COGS directly (sounds to me logical, and that's actually why I wouldn't go for the solution to create the stock move entries every time even for the dropshipped goods. That, and the fact that it would pollute the accounting with useless moves)

--- addons/purchase/invoice.py
+++ addons/purchase/invoice.py
@@ -114,6 +114,15 @@ class AccountInvoice(models.Model):
         return res
 
     @api.model
+    def _anglo_saxon_sale_move_lines(self, i_line):
+        for sale_line in i_line.sale_line_ids:
+            for proc in sale_line.procurement_ids:
+                if proc.purchase_line_id:
+                    #if the invoice line is related to sale order lines having one of its procurement_ids with a purchase_line_id set, it means that it is a confirmed dropship and in that case we mustn't create the cost of sale line (because the product won't enter the stock)
+                    return []
+        return super(AccountInvoice, self)._anglo_saxon_sale_move_lines(i_line)
+
+    @api.model
     def _anglo_saxon_purchase_move_lines(self, i_line, res):
         """Return the additional move lines for purchase invoices and refunds.
 
