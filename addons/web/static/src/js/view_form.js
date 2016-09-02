2559b2cf4fc8312bda4f2ca68951ae3103b1d049
Nicolas Martinelli <nim@odoo.com>
2016-09-02 10:02:19 +0200

    [FIX] web: statusbar click
    When the statusbar is clicked, a `debounce` function prevents a
    doucle-click, and therefore making several `write` calls. On some status
    bars, clicking doesn't work anymore.
    
    The reason is because, in some mysterious cases, the event is propagated
    to the parent. The `currentTarget` is not the `li` element, but the
    parent `ul`. By setting the `immediate` argument to `true` (execute the
    first function instead of the last), this solves the issue.

--- addons/web/static/src/js/view_form.js
+++ addons/web/static/src/js/view_form.js
@@ -6178,7 +6178,7 @@ instance.web.form.FieldStatus = instance.web.form.AbstractField.extend({
                 });
             }
         }
-    }, 300),
+    }, 300, true),
 });
 
 instance.web.form.FieldMonetary = instance.web.form.FieldFloat.extend({
