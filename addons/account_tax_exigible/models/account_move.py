fffb5c0e0c5ab2f7ee3e8178422afe43f2e625b2
qdp-odoo <qdp@odoo.com>
2016-09-05 12:12:42 +0200

    [FIX] account_tax_exigible: fix missing imports statements...

--- addons/account_tax_exigible/models/account_move.py
+++ addons/account_tax_exigible/models/account_move.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp import fields, models, api
+from openerp import fields, models, api, _
+from openerp.exceptions import UserError
 
 
 class AccountMoveLine(models.Model):
