06524fec3bd0390d08f6db52e227987d4e3d92ef
Denis Vermylen (dve) <dve@odoo.com>
2016-09-05 11:04:03 +0200

    [IMP] website_sale: improve tooltips + strings on views
    Better tooltips for settings options:
        - Pricing Strategy: remove tooltip
        - Sales Price:
            - Specific prices per customer segment, currency, etc.: new pricing table available in product detail form (Sales tab).
            - Advanced pricing based on formulas (discounts, margins, rounding): apply price rules from a new *Pricelists* menu in Configuration.
        - Optional products: Installs *e-Commerce Optional Products*
    Better tooltips in product page:
      - Website Categories: Categories can be published on the Shop page (online catalog grid) to help customers find all the items within a category. To publish them, go to the Shop page, hit Customize and turn *Product Categories* on. A product can belong to several categories.
      - Suggested Products
      - Relabel the field on product detail form, web page + web customize menu: should be Alternative Products
      - new tooltip: Propose more expensive alternatives to your customers (upsell strategy). Those products show up on the product page.
      - Accessory Products: Accessories show up when the customer reviews the cart before paying (cross-sell strategy, e.g. for computers: mouse, keyboard, etc.). An algorithm figures out a list of accessories based on all the products added to cart.
      - Optional Products: Optional Products are suggested whenever the customer hits *Add to Cart* (cross-sell strategy, e.g. for computers: warranty, software, etc.).
      - Availability: Adds an availability status on the web product page.

--- addons/website_sale_options/models/product.py
+++ addons/website_sale_options/models/product.py
@@ -7,4 +7,7 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    optional_product_ids = fields.Many2many('product.template', 'product_optional_rel', 'src_id', 'dest_id', string='Optional Products', help="Products to propose when add to cart.")
+    optional_product_ids = fields.Many2many('product.template', 'product_optional_rel', 'src_id', 'dest_id',
+                                            string='Optional Products', help="Optional Products are suggested "
+                                            "whenever the customer hits *Add to Cart* (cross-sell strategy, "
+                                            "e.g. for computers: warranty, software, etc.).")
