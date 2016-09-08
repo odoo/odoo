41b62163eae2a2037c17b5f00ef08482398d5d04
Fabien Meghazi <fme@odoo.com>
2016-09-08 13:46:29 +0200

    [FIX] point_of_sale: the POS html bootstrap javascript not contain `db_info`

--- addons/point_of_sale/controllers/main.py
+++ addons/point_of_sale/controllers/main.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+import json
 import logging
 import werkzeug.utils
 
-from odoo import http
+from odoo import http, service
 from odoo.http import request
 
 _logger = logging.getLogger(__name__)
@@ -17,4 +18,11 @@ class PosController(http.Controller):
         if not pos_sessions:
             return werkzeug.utils.redirect('/web#action=point_of_sale.action_client_pos_menu')
         pos_sessions.login()
-        return request.render('point_of_sale.index')
+
+        version_info = service.common.exp_version()
+        db_info = {
+            'server_version': version_info.get('server_version'),
+            'server_version_info': version_info.get('server_version_info'),
+        }
+
+        return request.render('point_of_sale.index', qcontext={'db_info': json.dumps(db_info)})
