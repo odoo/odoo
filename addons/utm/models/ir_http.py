423d73ad900dfe43ebdce7c2d50c02a8e92f458d
Simon Lejeune <sle@openerp.com>
2016-09-15 11:39:18 +0200

    [FIX] utm: correct ir_http's _dispatch override
    When overriding dispatch, we have to make sure that we do the
    database operations before calling super, because we are not sure
    to have a valid database/env after (for example, in the case of a
    drop of a database).

--- addons/utm/models/ir_http.py
+++ addons/utm/models/ir_http.py
@@ -12,10 +12,16 @@ class IrHttp(models.AbstractModel):
 
     @classmethod
     def _dispatch(cls):
+        cookies_to_set = []
+        for var, dummy, cook in request.env['utm.mixin'].tracking_fields():
+            if var in request.params and request.httprequest.cookies.get(var) != request.params[var]:
+                cookies_to_set.append((cook, request.params[var], cls.get_utm_domain_cookies()))
+
         response = super(IrHttp, cls)._dispatch()
         if isinstance(response, Exception):
             return response
-        for var, dummy, cook in request.env['utm.mixin'].tracking_fields():
-            if var in request.params and request.httprequest.cookies.get(var) != request.params[var]:
-                response.set_cookie(cook, request.params[var], domain=cls.get_utm_domain_cookies())
+
+        for cookie_to_set in cookies_to_set:
+            response.set_cookie(cookie_to_set[0], cookie_to_set[1], domain=cookie_to_set[2])
+
         return response
