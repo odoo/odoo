f3c79300538f2e9df2d521ee7bcb2cae129d75d8
Nicolas Martinelli <nim@odoo.com>
2016-09-16 14:38:47 +0200

    [FIX] pad_project: initialize pad at task creation
    When a task is created automatically (e.g. during a SO confirmation when
    the product is configured as "Create task and track hours"), the pad is
    empty when the task is first displayed. It is necessary to edit the task
    and save it in order to be able to see the content.
    
    This is because the pad is normally initialized by the JS layer, at the
    first edition, thanks to a call to `pad_generate_url`. Therefore, a
    programmatic task creation won't initialize it.
    
    opw-685826

--- addons/pad_project/project_task.py
+++ addons/pad_project/project_task.py
@@ -8,3 +8,18 @@ class task(osv.osv):
     _columns = {
         'description_pad': fields.char('Pad URL', pad_content_field='description')
     }
+
+    def create(self, cr, uid, vals, context=None):
+        res = super(task, self).create(cr, uid, vals, context=context)
+        # In case the task is created programmatically, 'description_pad' is not filled in yet since
+        # it is normally initialized by the JS layer
+        if 'description_pad' not in vals:
+            ctx = dict(context or {})
+            ctx.update({
+                'model': 'project.task',
+                'field_name': 'description_pad',
+                'object_id': res,
+            })
+            description_pad = self.pad_generate_url(cr, uid, context=ctx)['url']
+            self.write(cr, uid, res, {'description_pad': description_pad}, context=context)
+        return res
