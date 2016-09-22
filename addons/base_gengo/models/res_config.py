633dd21b40c4f5f89b8d6806bb34a13c91511273
Laurent Smet <las@openerp.com>
2016-09-22 16:35:58 +0200

    [IMP] base_gengo: Move gengo parameters from company settings to general settings

---
+++ addons/base_gengo/models/res_config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models, fields
+
+
+class BaseConfigSettings(models.TransientModel):
+    _inherit = 'base.config.settings'
+
+    gengo_private_key = fields.Text(string="Gengo Private Key", related="company_id.gengo_private_key")
+    gengo_public_key = fields.Text(string="Gengo Public Key", related="company_id.gengo_public_key")
+    gengo_comment = fields.Text(string="Comments", related="company_id.gengo_comment",
+      help="This comment will be automatically be enclosed in each an every request sent to Gengo")
+    gengo_auto_approve = fields.Boolean(string="Auto Approve Translation ?", related="company_id.gengo_auto_approve",
+      help="Jobs are Automatically Approved by Gengo.")
+    gengo_sandbox = fields.Boolean(string="Sandbox Mode",
+      help="Check this box if you're using the sandbox mode of Gengo, mainly used for testing purpose.")
