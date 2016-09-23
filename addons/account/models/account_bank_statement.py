6e9d174330199cec1693f3cd3a5c302157e88e8c
qdp-odoo <qdp@odoo.com>
2016-09-23 14:57:26 +0200

    [IMP] account: deploy small usability improvements with great impact

--- addons/account/models/account_bank_statement.py
+++ addons/account/models/account_bank_statement.py
@@ -345,7 +345,7 @@ class AccountBankStatementLine(models.Model):
     _order = "statement_id desc, sequence"
     _inherit = ['ir.needaction_mixin']
 
-    name = fields.Char(string='Memo', required=True)
+    name = fields.Char(string='Label', required=True)
     date = fields.Date(required=True, default=lambda self: self._context.get('date', fields.Date.context_today(self)))
     amount = fields.Monetary(digits=0, currency_field='journal_currency_id')
     journal_currency_id = fields.Many2one('res.currency', related='statement_id.currency_id',
