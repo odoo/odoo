M4GiK/kamserp-v10

[FIX][IMP] various: unsudo mail/view controller and get_access_action
mail/view controller is a generic controller that redirects the user to a
given view, depending on the record and the user. Users may be redirected
to the backend form view, or to a website view. This is done notably by
calling get_access_action method that gives the action to perform (act_window
or url).

Previously this method was called using SUPERUSER. However it was therefore
impossible to know who the user was. This method is now called using the
current user. Various overrides of get_access_action have been updated to
add some logic and access rights check directly in the method allowing
more fine-grain behavior of the access action.

addons/website_portal_sale/models/sale_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
+from odoo import api, exceptions, models
 
 
 class SaleOrder(models.Model):
@@ -10,17 +10,25 @@ class SaleOrder(models.Model):
 
     @api.multi
     def get_access_action(self):
-        """ Override method that generated the link to access the document. Instead
-        of the classic form view, redirect to the online quote if exists. """
+        """ Instead of the classic form view, redirect to the online quote for
+        portal users that have access to a confirmed order. """
+        # TDE note: read access on sale order to portal users granted to followed sale orders
         self.ensure_one()
         if self.state in ['draft', 'cancel']:
             return super(SaleOrder, self).get_access_action()
-        return {
-            'type': 'ir.actions.act_url',
-            'url': '/my/orders/%s' % self.id,
-            'target': 'self',
-            'res_id': self.id,
-        }
+        if self.env.user.share:
+            try:
+                self.check_access_rule('read')
+            except exceptions.AccessError:
+                pass
+            else:
+                return {
+                    'type': 'ir.actions.act_url',
+                    'url': '/my/orders/%s' % self.id,
+                    'target': 'self',
+                    'res_id': self.id,
+                }
+        return super(SaleOrder, self).get_access_action()
 
     def _force_lines_to_invoice_policy_order(self):
         for line in self.order_line:
@@ -28,4 +36,3 @@ class SaleOrder(models.Model):
                 line.qty_to_invoice = line.product_uom_qty - line.qty_invoiced
             else:
                 line.qty_to_invoice = 0
-

addons/website_project_issue/models/project_issue.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
+from odoo import api, exceptions, models
 
 
 class Issue(models.Model):
@@ -10,15 +10,22 @@ class Issue(models.Model):
 
     @api.multi
     def get_access_action(self):
-        """ Override method that generated the link to access the document. Instead
-        of the classic form view, redirect to the post on the website directly """
+        """ Instead of the classic form view, redirect to website for portal users
+        that can read the issue. """
         self.ensure_one()
-        return {
-            'type': 'ir.actions.act_url',
-            'url': '/my/issues/%s' % self.id,
-            'target': 'self',
-            'res_id': self.id,
-        }
+        if self.env.user.share:
+            try:
+                self.check_access_rule('read')
+            except exceptions.AccessError:
+                pass
+            else:
+                return {
+                    'type': 'ir.actions.act_url',
+                    'url': '/my/issues/%s' % self.id,
+                    'target': 'self',
+                    'res_id': self.id,
+                }
+        return super(Issue, self).get_access_action()
 
     @api.multi
     def _notification_group_recipients(self, message, recipients, done_ids, group_data):

odoo/odoo

This is a backport of saas-13 commit 51c5cd7ff7c5dd752f05cbc8808f62fb08c8d259.

addons/website_portal_sale/models/sale_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp import api, models
+from openerp import api, exceptions, models
 
 
 class sale_order(models.Model):
@@ -9,17 +9,25 @@ class sale_order(models.Model):
 
     @api.multi
     def get_access_action(self):
-        """ Override method that generated the link to access the document. Instead
-        of the classic form view, redirect to the online quote if exists. """
+        """ Instead of the classic form view, redirect to the online quote for
+        portal users that have access to a confirmed order. """
+        # TDE note: read access on sale order to portal users granted to followed sale orders
         self.ensure_one()
         if self.state in ['draft', 'cancel']:
             return super(sale_order, self).get_access_action()
-        return {
-            'type': 'ir.actions.act_url',
-            'url': '/my/orders/%s' % self.id,
-            'target': 'self',
-            'res_id': self.id,
-        }
+        if self.env.user.share:
+            try:
+                self.check_access_rule('read')
+            except exceptions.AccessError:
+                pass
+            else:
+                return {
+                    'type': 'ir.actions.act_url',
+                    'url': '/my/orders/%s' % self.id,
+                    'target': 'self',
+                    'res_id': self.id,
+                }
+        return super(sale_order, self).get_access_action()
 
     def _force_lines_to_invoice_policy_order(self):
         for line in self.order_line:
@@ -27,4 +35,3 @@ class sale_order(models.Model):
                 line.qty_to_invoice = line.product_uom_qty - line.qty_invoiced
             else:
                 line.qty_to_invoice = 0
-

addons/website_project_issue/models/project_issue.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp import api, models
+from openerp import api, exceptions, models
 
 
 class Issue(models.Model):
@@ -10,15 +10,22 @@ class Issue(models.Model):
 
     @api.multi
     def get_access_action(self):
-        """ Override method that generated the link to access the document. Instead
-        of the classic form view, redirect to the post on the website directly """
+        """ Instead of the classic form view, redirect to website for portal users
+        that can read the issue. """
         self.ensure_one()
-        return {
-            'type': 'ir.actions.act_url',
-            'url': '/my/issues/%s' % self.id,
-            'target': 'self',
-            'res_id': self.id,
-        }
+        if self.env.user.share:
+            try:
+                self.check_access_rule('read')
+            except exceptions.AccessError:
+                pass
+            else:
+                return {
+                    'type': 'ir.actions.act_url',
+                    'url': '/my/issues/%s' % self.id,
+                    'target': 'self',
+                    'res_id': self.id,
+                }
+        return super(Issue, self).get_access_action()
 
     @api.multi
     def _notification_group_recipients(self, message, recipients, done_ids, group_data):