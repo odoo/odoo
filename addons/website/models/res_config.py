37ea415c9992321ba0a174a9a14e8dd6b627e644
Christophe Simonis <chs@odoo.com>
2016-09-27 13:21:41 +0200

    [FIX] website: remove duplicated field

--- addons/website/models/res_config.py
+++ addons/website/models/res_config.py
@@ -30,19 +30,18 @@ class WebsiteConfigSettings(models.TransientModel):
     cdn_activated = fields.Boolean('Use a Content Delivery Network (CDN)', related='website_id.cdn_activated')
     cdn_url = fields.Char(related='website_id.cdn_url')
     cdn_filters = fields.Text(related='website_id.cdn_filters')
-    module_website_form_editor = fields.Boolean("Form builder")
     module_website_form_editor = fields.Selection([
         (0, 'Use standard forms'),
         (1, 'Create and customize forms to generate emails, leads, issues and extra information in the checkout process (new snippet available)')
-        ], "Form Builder")
+    ], "Form Builder")
     module_website_version = fields.Selection([
         (0, 'No version management and A/B testing (easy)'),
         (1, 'Allow multiple versions of the same page (advanced)')
-        ], "A/B Testing")
+    ], "A/B Testing")
     favicon = fields.Binary('Favicon', related='website_id.favicon')
-    
+
     # Set as global config parameter since methods using it are not website-aware. To be changed
-    # when multi-website is implemented 
+    # when multi-website is implemented
     google_maps_api_key = fields.Char(string='Google Maps API Key')
 
     def set_google_maps_api_key(self):
