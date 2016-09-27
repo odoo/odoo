e03352db0bcc3a61c4560e820ea216dfb8fedea2
Thibault Delavallée <tde@odoo.com>
2016-09-27 12:34:45 +0200

    [REM] portal_sale: remove now void module

--- addons/portal_sale/models/account_invoice.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, models
-
-
-class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
--- addons/portal_sale/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, models
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
