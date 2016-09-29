32fc5e49d4c24311a038ce5953abf3b88f25b977
qdp-odoo <qdp@odoo.com>
2016-09-29 21:47:27 +0200

    [FIX] account_check_printing: fix flow of check.
    Use case fixed: create a draft supplier payment, tick it in list view and launch the action 'print check'. Then, on the pop up window cancel the operation => the state was badly set to 'sent'.

--- addons/account_check_printing/wizard/print_prenumbered_checks.py
+++ addons/account_check_printing/wizard/print_prenumbered_checks.py
@@ -14,6 +14,8 @@ class PrintPreNumberedChecks(models.TransientModel):
     def print_checks(self):
         check_number = self.next_check_number
         payments = self.env['account.payment'].browse(self.env.context['payment_ids'])
+        payments.filtered(lambda r: r.state == 'draft').post()
+        payments.filtered(lambda r: r.state != 'sent').write({'state': 'sent'})
         for payment in payments:
             payment.check_number = check_number
             check_number += 1
