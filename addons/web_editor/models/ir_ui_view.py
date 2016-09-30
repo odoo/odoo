e2fb1cb6ce0afe490a66c5b491a00b55b866118d
Jeremy Kersten <jke@odoo.com>
2016-09-30 15:05:37 +0200

    [FIX] web_editor: add missing import
    Thanks to my Linter...

--- addons/web_editor/models/ir_ui_view.py
+++ addons/web_editor/models/ir_ui_view.py
@@ -2,10 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import copy
+import logging
 from lxml import etree, html
 
 from odoo.exceptions import AccessError
 from odoo import api, models
+
+_logger = logging.getLogger(__name__)
 
 
 class IrUiView(models.Model):
