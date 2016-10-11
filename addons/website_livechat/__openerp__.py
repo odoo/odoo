789566a0d05d3832e8f9bc5670049afc636ae785
Olivier Dony <odo@openerp.com>
2016-10-11 10:37:34 +0200

    [FIX] website_livechat: make it auto-install
    Most people install the im_livechat module and expect it
    to be enabled automatically on their website.
    When it doesn't work they try to use the integration JS
    code and add it to their website layout, which does not
    work as expected.
    
    Setting it as auto-installed solves the issue.

--- addons/website_livechat/__openerp__.py
+++ addons/website_livechat/__openerp__.py
@@ -11,6 +11,7 @@ For website built with Odoo CMS, this module include a chat button on your Websi
     'author': 'OpenERP SA',
     'depends': ['website', 'im_livechat'],
     'installable': True,
+    'auto_install': True,
     'data': [
         'views/website_livechat.xml',
         'views/res_config.xml',
