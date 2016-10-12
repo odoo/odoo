a2ae219d4ee283b7e7af84a3e7fa3bb8e0a95f23
qsm-odoo <qsm@odoo.com>
2016-10-12 11:05:28 +0200

    [FIX] web_editor, website: fix customize_template_get
    Commit e11f23b701cd061ee2090e506e7ff71cc2ed8c33 split the website
    customize_template_get into web_editor and website ones. This split
    was not perfect and commit 20861b482a0f84c2946cc14a05648b87b95bdac4
    tried to make it work... but did not made it totally and added an
    indirect dependency to website module in web_editor...
    
    As the final result is wrong and inconsistent and that the previous
    9.0 implementation was already ugly, this was not possible to make
    a consistent full-stable fix.
    
    This commit tries to make a fix as stable as possible (the main method
    name did not change, the arguments did not change, their purpose is
    the same, ...) which makes the code work and be less ugly. However,
    the function returns are now slightly different (and JS depends on it).
    It returns only valid view information (not header stuff which is not
    related to anything and the inherit_id value is now a combination of
    the inherit view id and name). Also, a new private method has been
    created.
    
    Also, the method contained code which was deprecated since saas-6 but
    was never removed (and never broke anything "by chance").
    
    In master these methods will be refactored to make more sense.

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -262,10 +262,10 @@ class Website(Home):
 
     @http.route('/website/customize_template_get', type='json', auth='user', website=True)
     def customize_template_get(self, key, full=False, bundles=False):
-        """ Get inherit view's informations of the template ``key``. By default, only
-            :returns ``customize_show`` templates (which can be active or not), if
-                ``full=True`` returns inherit view's informations of the template ``key``.
-                ``bundles=True`` returns also the asset bundles
+        """ Get inherit view's informations of the template ``key``.
+            returns templates info (which can be active or not)
+            ``full=False`` returns only the customize_show template
+            ``bundles=True`` returns also the asset bundles
         """
         return request.env["ir.ui.view"].customize_template_get(key, full=full, bundles=bundles)
 
