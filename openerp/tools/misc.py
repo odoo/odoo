007607030a2c810dc7b134e926a8203b94babf84
开阖软件 Jeff Wang <jeff@osbzr.com>
2016-10-12 09:51:45 +0800

    [FIX] tools: keep keyword arguments in wrapper
    The patched add_sheet method lacks from the keyword argument cell_overwrite_ok
    that is present in original code.
    
    Closes #13755

--- openerp/tools/misc.py
+++ openerp/tools/misc.py
@@ -330,13 +330,13 @@ try:
     # add some sanitizations to respect the excel sheet name restrictions
     # as the sheet name is often translatable, can not control the input
     class PatchedWorkbook(xlwt.Workbook):
-        def add_sheet(self, name):
+        def add_sheet(self, name, cell_overwrite_ok=False):
             # invalid Excel character: []:*?/\
             name = re.sub(r'[\[\]:*?/\\]', '', name)
 
             # maximum size is 31 characters
             name = name[:31]
-            return super(PatchedWorkbook, self).add_sheet(name)
+            return super(PatchedWorkbook, self).add_sheet(name, cell_overwrite_ok=cell_overwrite_ok)
 
     xlwt.Workbook = PatchedWorkbook
 
