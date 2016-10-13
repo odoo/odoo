d2ce2eea325dc7303144fc82f95a0b4a5b6d9803
Gustavo Valverde <g.valverde02@gmail.com>
2016-10-13 02:54:43 -0400

    [FIX] Update CoA, Taxes, Fiscal Positions and Journals (IFRS Compatible)
    To comply with local and tax regulations:
     * The whole Charts of Accounts was changed
     * Most of the Journals has been preconfigured to comply with tax regulator
     * All the regulatory taxes were added and configured with group taxes
    
    To standardize the Dominican implementations:
     * An IFRS compatible CoA that does not major changes for most companies
     * A well structured CoA with a numbers logic
     * Use of the Odoo original flow for creating tax compliant invoice sequences

--- addons/l10n_do/__openerp__.py
+++ addons/l10n_do/__openerp__.py
@@ -1,40 +1,77 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# First author: Jose Ernesto Mendez <tecnologia@obsdr.com> (Open Business Solutions SRL.)
-# Copyright (c) 2012 -TODAY Open Business Solutions, SRL. (http://obsdr.com). All rights reserved.
-# This is a fork to upgrade to odoo 8.0
-# by Marcos Organizador de Negocios - Eneldo Serrata - www.marcos.org.do
+# Author: Gustavo Valverde. iterativo | Consultor de Odoo
+# Contributors: Edser Solis - iterativo
+
+# Copyright (c) 2016 - Present | Novum Ingenieria, SRL. - www.iterativo.do
+# All rights reserved.
 
 {
     'name': 'Dominican Republic - Accounting',
-    'version': '1.0',
+    'version': '2.0',
     'category': 'Localization',
     'description': """
-This is the base module to manage the accounting chart for Dominican Republic.
-==============================================================================
 
-* Chart of Accounts.
-* The Tax Code Chart for Domincan Republic
-* The main taxes used in Domincan Republic
-* Fiscal position for local """,
-    'author': 'Eneldo Serrata - Marcos Organizador de Negocios, SRL.',
-    'website': 'http://marcos.do',
-    'depends': ['account', 'base_iban'],
+Localization Module for Dominican Republic
+===========================================
+
+Catálogo de Cuentas e Impuestos para República Dominicana, Compatible para
+**Internacionalización** con **NIIF** y alineado a las normas y regulaciones
+de la Dirección General de Impuestos Internos (**DGII**).
+
+**Este módulo consiste de:**
+
+- Catálogo de Cuentas Estándar (alineado a DGII y NIIF)
+- Catálogo de Impuestos con la mayoría de Impuestos Preconfigurados
+        - ITBIS** para compras y ventas
+        - Retenciones de ITBIS
+        - Retenciones de ISR
+        - Grupos de Impuestos y Retenciones:
+                - Telecomunicaiones
+                - Proveedores de Materiales de Construcción
+                - Personas Físicas Proveedoras de Servicios
+        - Otros impuestos
+- Diarios Preconfigurados para manejo de la mayoría de los NCF
+        - Facturas con Valor Fiscal (para Ventas)
+        - Facturas para Consumidores Finales
+        - Notas de Débito y Crédito
+        - Registro de Proveedores Informales
+        - Registro de Gastos Menores
+        - Gubernamentales
+- Posiciones Fiscales para automatización de impuestos y retenciones
+        - Cambios de Impuestos a Exenciones (Ej. Ventas al Estado)
+        - Cambios de Impuestos a Retenciones (Ej. Compra Servicios al Exterior)
+        - Entre otros
+
+**Nota:**
+Este módulo no soporta NCF con Valor Fiscal para compras, ya que los
+mismos poseen un comportamiento diferente a los demás debido a que deben
+ser introducidos de forma manual, por lo que no han sido configurados a través
+de los diarios.
+
+    """,
+    'author': 'Gustavo Valverde - iterativo | Consultores de Odoo',
+    'website': 'http://iterativo.do',
+    'depends': ['account', 'purchase', 'sale', 'base_iban'],
     'data': [
-        # basic accounting data
-        'data/account_chart_template.xml',
+        # Journal Model
+        'data/ir_model_data.xml',
+        # Basic accounting data
+        'data/coa_template.xml',
+        'data/account_type.xml',
         'data/account.account.template.csv',
-        'data/set_account_on_chart_template.xml',
-        'data/account_account_tag.xml',
-        'data/account.tax.template.csv',
+        'data/tax_template.xml',
+        'data/account_defaults.xml',
+        # Country States
         'data/l10n_do_base_data.xml',
         # Adds fiscal position
-        'data/account.fiscal.position.template.csv',
-        'data/account.fiscal.position.tax.template.csv',
+        'data/fiscal_position_template.xml',
         # configuration wizard, views, reports...
         'data/account_chart_template.yml',
-    ],
+        # Journals and their sequences
+        'data/ir_sequence.xml',
+        'data/account_journal.xml',
+        ],
     'test': [],
     'demo': [],
     'installable': True,
