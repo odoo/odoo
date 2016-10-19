d8c88079d313a3c3912648e509743f71aa5f7126
Nicolas Martinelli <nim@odoo.com>
2016-10-19 11:01:03 +0200

    [FIX] purchase_requisition: unit price when quantity change
    On a PO linked to a purchase tender, changing the quantity on a PO line
    will trigger a recompute of the unit price. However, this recomputation
    will take into account the unit price of the supplier pricelist, not the
    unit price on the purchase tender.
    
    opw-690483

--- addons/purchase_requisition/purchase_requisition.py
+++ addons/purchase_requisition/purchase_requisition.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from datetime import datetime
+from openerp import api, models
 from openerp.osv import fields, osv
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from openerp.tools.translate import _
@@ -276,6 +277,24 @@ class purchase_order(osv.osv):
                         'procurement_id': po.requisition_id.procurement_id.id,
                         'move_dest_id': po.requisition_id.procurement_id.move_dest_id.id,
                     }, context=context)
+        return res
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    @api.onchange('product_qty', 'product_uom')
+    def _onchange_quantity(self):
+        res = super(PurchaseOrderLine, self)._onchange_quantity()
+        if self.order_id.requisition_id:
+            for line in self.order_id.requisition_id.line_ids:
+                if line.product_id == self.product_id:
+                    if line.product_uom_id != self.product_uom:
+                        self.price_unit = self.env['product.uom']._compute_price(
+                            line.product_uom_id.id, line.price_unit, to_uom_id=self.product_uom.id)
+                    else:
+                        self.price_unit = line.price_unit
+                    break
         return res
 
 
