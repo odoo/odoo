eb69e5e99a7e192caead15d166e7d67a6d447d58
mdi-odoo <mdi@tinyerp.com>
2016-10-21 11:33:49 +0530

    [IMP] sale*: Improve the settings form view

--- addons/website_quote/__manifest__.py
+++ addons/website_quote/__manifest__.py
@@ -20,6 +20,7 @@ Odoo Sale Quote Roller
         'views/sale_order_views.xml',
         'views/sale_quote_views.xml',
         'views/website_quote_templates.xml',
+        'views/sale_config_settings_views.xml',
         'security/ir.model.access.csv',
     ],
     'demo': [
