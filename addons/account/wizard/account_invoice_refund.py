85ed4bbc399b24d82401eeafb9869962fde37bcf
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2016-10-26 10:11:25 +0200

    [IMP] Add _prepare method for compute_refund

--- addons/account/wizard/account_invoice_refund.py
+++ addons/account/wizard/account_invoice_refund.py
@@ -36,19 +36,50 @@ class AccountInvoiceRefund(models.TransientModel):
         else:
             self.refund_only = False
 
+    @api.multi
+    def _prepare_compute_refund(self, inv, form, date, description):
+        inv_obj = self.env['account.invoice']
+        inv_line_obj = self.env['account.invoice.line']
+        inv_tax_obj = self.env['account.invoice.tax']
+        invoice = inv.read(
+            ['name', 'type', 'number', 'reference',
+             'comment', 'date_due', 'partner_id',
+             'partner_insite', 'partner_contact',
+             'partner_ref', 'payment_term_id', 'account_id',
+             'currency_id', 'invoice_line_ids', 'tax_line_ids',
+             'journal_id', 'date'])
+        invoice = invoice[0]
+        del invoice['id']
+        invoice_lines = inv_line_obj.browse(invoice['invoice_line_ids'])
+        invoice_lines = inv_obj.with_context(
+            mode='modify')._refund_cleanup_lines(invoice_lines)
+        tax_lines = inv_tax_obj.browse(invoice['tax_line_ids'])
+        tax_lines = inv_obj._refund_cleanup_lines(tax_lines)
+        invoice.update({
+            'type': inv.type,
+            'date_invoice': form.date_invoice,
+            'state': 'draft',
+            'number': False,
+            'invoice_line_ids': invoice_lines,
+            'tax_line_ids': tax_lines,
+            'date': date,
+            'name': description,
+            'origin': inv.origin,
+            'fiscal_position_id': inv.fiscal_position_id.id,
+        })
+        for field in ('partner_id', 'account_id', 'currency_id',
+                      'payment_term_id', 'journal_id'):
+            invoice[field] = invoice[field] and invoice[field][0]
+        return invoice
 
     @api.multi
     def compute_refund(self, mode='refund'):
         inv_obj = self.env['account.invoice']
-        inv_tax_obj = self.env['account.invoice.tax']
-        inv_line_obj = self.env['account.invoice.line']
         context = dict(self._context or {})
         xml_id = False
 
         for form in self:
             created_inv = []
-            date = False
-            description = False
             for inv in inv_obj.browse(context.get('active_ids')):
                 if inv.state in ['draft', 'proforma2', 'cancel']:
                     raise UserError(_('Cannot refund draft/proforma/cancelled invoice.'))
@@ -77,34 +108,7 @@ class AccountInvoiceRefund(models.TransientModel):
                             to_reconcile_lines += tmpline
                             to_reconcile_lines.reconcile()
                     if mode == 'modify':
-                        invoice = inv.read(
-                                    ['name', 'type', 'number', 'reference',
-                                    'comment', 'date_due', 'partner_id',
-                                    'partner_insite', 'partner_contact',
-                                    'partner_ref', 'payment_term_id', 'account_id',
-                                    'currency_id', 'invoice_line_ids', 'tax_line_ids',
-                                    'journal_id', 'date'])
-                        invoice = invoice[0]
-                        del invoice['id']
-                        invoice_lines = inv_line_obj.browse(invoice['invoice_line_ids'])
-                        invoice_lines = inv_obj.with_context(mode='modify')._refund_cleanup_lines(invoice_lines)
-                        tax_lines = inv_tax_obj.browse(invoice['tax_line_ids'])
-                        tax_lines = inv_obj._refund_cleanup_lines(tax_lines)
-                        invoice.update({
-                            'type': inv.type,
-                            'date_invoice': form.date_invoice,
-                            'state': 'draft',
-                            'number': False,
-                            'invoice_line_ids': invoice_lines,
-                            'tax_line_ids': tax_lines,
-                            'date': date,
-                            'name': description,
-                            'origin': inv.origin,
-                            'fiscal_position_id': inv.fiscal_position_id.id,
-                        })
-                        for field in ('partner_id', 'account_id', 'currency_id',
-                                         'payment_term_id', 'journal_id'):
-                                invoice[field] = invoice[field] and invoice[field][0]
+                        invoice = self._prepare_compute_refund(inv, form, date, description)
                         inv_refund = inv_obj.create(invoice)
                         if inv_refund.payment_term_id.id:
                             inv_refund._onchange_payment_term_date_invoice()
