382475826840a6757b35256a2e5e3ea2ae52be64
David Arnold <dar@devco.co>
2016-10-27 21:59:50 -0500

    [IMP] l10n_co: new niif chart of accounts After legal changes during 2015/2016, Colombia has intrduced IFRS standard. However, businesses are required to maintain a dual accounting still during 4 subsequent years (at least). The existing chart of account is not designed (well enough) to accomodate the new accounting standard smoothely. It furthermore has a set of taxes which is way too complex to handle and incomplete and therfore gives raise to false expectations.
    This is because the existing CoA goes into greate detail and comes with
    arround 2700 accounts. Combine this with the complexity of dual accounting
    and managment of this is impossible. The definition of the taxes has been based
    upon a no-localisation approach, and we have been trying to incorporate a
    comprehensive tax definition without touching code. As it shows, we cannot
    reach desired comprehensiveness with this approach. Yet people might get
    this impresison at first. It's wrong and harmful!
    
    This commit introduces a more lightweight CoA which is legal under the new law
    and which takes advantage of the inherent "multi-dimensionality" of an accounting
    software (eg. partner, currency and product all available on move line or otherwise
    constructable via transient models) to save on herarchic dimensionality (= the propensity
    to express every aspect of accounting in an herachic CoA fashion). Furthermore it
    eliminates tax definitions as we apply them in localisation modules in a more
    comprehensive and correct manner.
    
    This commit furthermore includes data for the new tags system according to our
    complete set of localisation modules, but generic enough for others to take into account.
    
    In order not to break existing choices, we introduced it as a parallel option as CoA.
    This ensures a smoother transition, but the idea would be to deprecate the old CoA at
    some point in time.
    
    Signed-off-by: David Arnold <dar@devco.co>

--- addons/l10n_co/__manifest__.py
+++ addons/l10n_co/__manifest__.py
@@ -1,30 +1,18 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Copyright (C) David Arnold (devCO).
-# Author        David Arnold (devCO), dar@devco.co
-# Co-Authors    Juan Pablo Aries (devCO), jpa@devco.co
-#               Hector Ivan Valencia Muñoz (TIX SAS)
-#               Nhomar Hernandez (Vauxoo)
-#               Humberto Ochoa (Vauxoo)
-
 {
-    'name': 'Colombian - Accounting',
-    'version': '0.8',
+    'name': 'Colombian Acounting - Common Base',
+    'version': '1.0',
     'category': 'Localization',
-    'description': 'Colombian Accounting and Tax Preconfiguration',
-    'author': 'David Arnold BA HSG (devCO)',
-    'depends': [
-        'account',
-        'base_vat',
-    ],
+    'description': 'Colombian Accounting Common Base for NIIF/Dual and OLD Charts',
+    'author': 'David Arnold (DevCO Colombia)',
+    'website': 'http://www.devco.co',
+    'depends': ['account'],
     'data': [
-        'data/account.account.tag.csv',
-        'data/l10n_co_chart_data.xml',
-        'data/account.account.template.csv',
-        'data/account_chart_template_data.xml',
-        'data/account.tax.template.csv',
         'data/account_tax_group_data.xml',
-        'data/account_chart_template_data.yml',
+        'data/account_type_data.xml',
+        'data/account_account_template_data.xml',
+        'data/menuitem_data.xml',
     ],
 }
