3a68ff005989cfc7358fe9913043a1319ec5a18c
Thibault Delavallée <tde@odoo.com>
2016-11-10 13:34:14 +0100

    [REF] resource: remove deprecated compatibility and rename methods
    Old methods are still present since a long time and are deprecated since
    saas-3. This compatibility layer adds unnecessary complexity to the module
    API. Those methods should now be removed. Some renaming is also performed
    to ease resource api understanding, notably to better differentiate
    internal methods from other methods.
    
    All addons are updated accordingly.

--- addons/mrp/models/mrp_workcenter.py
+++ addons/mrp/models/mrp_workcenter.py
@@ -206,7 +206,7 @@ class MrpWorkcenterProductivity(models.Model):
                 d2 = fields.Datetime.from_string(blocktime.date_end)
                 diff = d2 - d1
                 if (blocktime.loss_type not in ('productive', 'performance')) and blocktime.workcenter_id.calendar_id:
-                    r = blocktime.workcenter_id.calendar_id.get_working_hours(d1, d2, resource_id=blocktime.workcenter_id.resource_id.id)
+                    r = blocktime.workcenter_id.calendar_id.get_work_hours_count(d1, d2, resource_id=blocktime.workcenter_id.resource_id.id)
                     blocktime.duration = round(r * 60, 2)
                 else:
                     blocktime.duration = round(diff.total_seconds() / 60.0, 2)
