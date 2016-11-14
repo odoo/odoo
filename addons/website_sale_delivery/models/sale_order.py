8d36bf872a5c43f0d39e9e15b556082c3fac0f7e
Yannick Tivisse <yti@odoo.com>
2016-11-14 09:42:00 +0100

    Revert "[IMP] website_sale_delivery: Allow to (un)publish delivery methods from website"
    This reverts commit 6d9c59f348453a2c9026c4d9a5bb121790a7e189.
    
    This commit is breaking the enterprise/master branch because the method _get_delivery_methods takes 5 seconds to be executed with some delivery methods. Now, instead of having 1 delivery method displayed, the admin has all the delivery methods in order to be able to publish/unpublish them. As this method is called for each carrier, the phatom js tour takes more than 15 seconds to display the page /shop/confirm_order in the ecommerce, with is more than the timeout threshold.
    
    I revert this commit while waiting to find a proper solution.

--- addons/website_sale_delivery/models/sale_order.py
+++ addons/website_sale_delivery/models/sale_order.py
@@ -3,7 +3,6 @@
 import logging
 
 from odoo import api, fields, models, _
-from odoo.http import request
 from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
@@ -76,11 +75,7 @@ class SaleOrder(models.Model):
         # Following loop is done to avoid displaying delivery methods who are not available for this order
         # This can surely be done in a more efficient way, but at the moment, it mimics the way it's
         # done in delivery_set method of sale.py, from delivery module
-        user = request.env.user
-        if user.has_group('website.group_website_publisher') and user.has_group('sales_team.group_sale_manager'):
-            carrier_ids = DeliveryCarrier.search([]).ids
-        else:
-            carrier_ids = DeliveryCarrier.sudo().search(
+        carrier_ids = DeliveryCarrier.sudo().search(
             [('website_published', '=', True)]).ids
         for carrier_id in carrier_ids:
             carrier = DeliveryCarrier.browse(carrier_id)
