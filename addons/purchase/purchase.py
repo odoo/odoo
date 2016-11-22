odoo/odoo

commit 401a3cfaf6fd09f594e45c181e908900acba3777

[IMP] purchase: set scheduled date of lines
Commit a572b8115cdaa3a28f5c977a6006ddfa95647987 removes the inverse function of the `date_planned`. This
is necessary to avoid unexpected changes of the scheduled dates.
However, this removes the ability to set the scheduled dates of all
lines at once.

This commit introduces back the functionality thanks to a new button.
This way, the functionality remains without unexpected behavior.

Backport of rev: https://github.com/odoo/odoo/commit/2cf80e00752c68e443b0cd442ee50f7d6daaa455

opw-692873

addons/purchase/purchase.py
@@ -439,6 +439,11 @@ class PurchaseOrder(models.Model):
             result['res_id'] = self.invoice_ids.id
         return result
 
+    @api.multi
+    def action_set_date_planned(self):
+        for order in self:
+            order.order_line.update({'date_planned': order.date_planned})
+
 
 class PurchaseOrderLine(models.Model):
     _name = 'purchase.order.line'

addons/purchase/purchase_view.xml
@@ -249,7 +249,15 @@
                         <page string="Deliveries &amp; Invoices">
                             <group>
                                 <group>
-                                    <field name="date_planned"/>
+                                    <label for="date_planned"/>
+                                    <div>
+                                        <field name="date_planned" required="1" attrs="{'readonly': [('state', 'not in', ('draft', 'sent'))]}"/>
+                                        <button name="action_set_date_planned" type="object"
+                                            states="draft,sent"
+                                            string="Set date to all order lines"
+                                            help="This changes the scheduled date of all order lines to the given date"
+                                            class="fa fa-calendar o_icon_button oe_edit_only"/>
+                                    </div>
                                     <field name="picking_type_id" domain="[('code','=','incoming')]" options="{'no_create': True}" groups="stock.group_locations"/>
                                     <field name="dest_address_id" groups="stock.group_locations" attrs="{'invisible': [('default_location_dest_id_usage', '!=', 'customer')], 'required': [('default_location_dest_id_usage', '=', 'customer')]}"/>
                                     <field name="default_location_dest_id_usage" invisible="1"/>

commit 290601e28f1056d1d529374fbe65751900ab4974

[FIX] purchase: update order_lines date_planned
When we are trying to use the button 'set date to all order lines, we
get a traceback saying, that update cannot be done, because it ensure
that there's only one record. (After 9.0, there is no more this
'ensure_one').

This bug has been introduced in rev: https://github.com/odoo/odoo/commit/401a3cfaf6fd09f594e45c181e908900acba3777

To fix it, we iterate over the order lines, and do the update of each
line.

addons/purchase/purchase.py
@@ -442,7 +442,9 @@ class PurchaseOrder(models.Model):
     @api.multi
     def action_set_date_planned(self):
         for order in self:
-            order.order_line.update({'date_planned': order.date_planned})
+            #DO NOT FORWARD PORT
+            for line in order.order_line:
+                line.update({'date_planned': order.date_planned})
 
 
 class PurchaseOrderLine(models.Model):