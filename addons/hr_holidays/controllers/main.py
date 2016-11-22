e9ea081482473b78dfd56248a8fe17f6ad1ecc19
Thibault Delavallée <tde@odoo.com>
2016-11-22 16:59:44 +0100

    [FIX] hr_holidays: fix forward-ported code still using workflows
    Commit 3517cd46f70933292fe50f667b671a3d8292c7ea introduced in 9 some new
    controllers in hr holidays. They are used to activate some workflow signals
    through dedicated controllers.
    
    However hr holidays workflow have been removed meanwhile at commit
    6d4efba8f7dff46b6b1e0077ebe2a7cd01e9c711. This commit fixes the forward-port
    by correctling calling methods instead of trigerring workflow signals.

--- addons/hr_holidays/controllers/main.py
+++ addons/hr_holidays/controllers/main.py
@@ -12,7 +12,7 @@ class HrHolidaysController(http.Controller):
         comparison, record, redirect = MailController._check_token_and_record_or_redirect('hr.holidays', int(res_id), token)
         if comparison and record:
             try:
-                record.signal_workflow('validate')
+                record.action_validate()
             except Exception:
                 return MailController._redirect_to_messaging()
         return redirect
@@ -22,7 +22,7 @@ class HrHolidaysController(http.Controller):
         comparison, record, redirect = MailController._check_token_and_record_or_redirect('hr.holidays', int(res_id), token)
         if comparison and record:
             try:
-                record.signal_workflow('refuse')
+                record.action_refuse()
             except Exception:
                 return MailController._redirect_to_messaging()
         return redirect
