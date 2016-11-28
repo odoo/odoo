c468cfdd69c77a8ec3f940c4f62791bf15bb82d8
Fabien Pinckaers <fp@openerp.com>
2016-11-28 09:55:05 +0100

    [IMP] hr_attendance: remove unuseful JS code + clean view

--- addons/hr_attendance/static/src/js/presence_widgets.js
+++ addons/hr_attendance/static/src/js/presence_widgets.js
@@ -27,26 +27,6 @@ var FormPresenceIndicator = form_common.AbstractField.extend({
     },
 });
 
-var KanbanPresenceIndicator = kanban_widgets.AbstractField.extend({
-    init: function() {
-        this._super.apply(this, arguments);
-    },
-    start: function() {
-        this.display_field();
-        this.render_value(); // doesn't get called automatically in kanban_widgets.AbstractField
-        this.$el.tooltip({title: _t("employee presence<br/>green: checked in<br/>red: checked out"), trigger: 'hover'});
-        return this._super();
-    },
-    render_value: function() {
-        this.$('.oe_hr_attendance_status').toggleClass("oe_hr_attendance_status_green", this.field.raw_value == 'checked_in');
-        this.$('.oe_hr_attendance_status').toggleClass("oe_hr_attendance_status_red", this.field.raw_value == 'checked_out');
-    },
-    display_field: function() {
-        this.$el.html(QWeb.render("PresenceIndicator"));
-    },
-});
-
 core.form_widget_registry.add('hr_attendance_form_presence_indicator', FormPresenceIndicator);
-kanban_widgets.registry.add('hr_attendance_kanban_presence_indicator', KanbanPresenceIndicator);
 
 });
