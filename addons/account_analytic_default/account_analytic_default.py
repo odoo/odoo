bdf630f9b89a3d47c3826dac6fe9a04e6aa701d5
Nicolas Martinelli <nim@odoo.com>
2016-11-28 10:20:16 +0100

    [FIX] account_analytic_default: invoice from PO
    - Create a vendor bill
    - Add a PO thanks to the "Add Purchase Order"
    - The analytic accounts are not added on the lines
    
    opw-693313

--- addons/account_analytic_default/account_analytic_default.py
+++ addons/account_analytic_default/account_analytic_default.py
@@ -70,6 +70,14 @@ class account_invoice_line(osv.osv):
             self.account_analytic_id = False
         return res
 
+    def _set_additional_fields(self, invoice):
+        rec = self.env['account.analytic.default'].account_get(self.product_id.id, self.invoice_id.partner_id.id, self._uid,
+                                                               time.strftime('%Y-%m-%d'), company_id=self.company_id.id, context=self._context)
+        if rec:
+            self.account_analytic_id = rec.analytic_id.id
+        else:
+            self.account_analytic_id = False
+        super(account_invoice_line, self)._set_additional_fields(invoice)
 
 class stock_picking(osv.osv):
     _inherit = "stock.picking"
