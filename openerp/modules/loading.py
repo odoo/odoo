cbff8629fa0d0e7b1f4d2847b01d86893ba91b77
Christophe Simonis <chs@odoo.com>
2016-11-29 18:43:50 +0100

    [IMP] core: allow declaration of `end` migration scripts
    These `end` migration script will be executed when all modules have
    been updated and models setup.
    
    Also accelerate the search of migration scripts, speeding up registry
    loading.

--- openerp/modules/loading.py
+++ openerp/modules/loading.py
@@ -193,6 +193,8 @@ def load_module_graph(cr, graph, status=None, perform_checks=True, skip_modules=
             # Set new modules and dependencies
             modobj.write(cr, SUPERUSER_ID, [module_id], {'state': 'installed', 'latest_version': ver})
 
+            package.load_state = package.state
+            package.load_version = package.installed_version
             package.state = 'installed'
             for kind in ('init', 'demo', 'update'):
                 if hasattr(package, kind):
@@ -338,6 +340,11 @@ def load_modules(db, force_demo=False, status=None, update_module=False):
                     loaded_modules, update_module)
 
         registry.setup_models(cr)
+
+        # STEP 3.5: execute migration end-scripts
+        migrations = openerp.modules.migration.MigrationManager(cr, graph)
+        for package in graph:
+            migrations.migrate_module(package, 'end')
 
         # STEP 4: Finish and cleanup installations
         if processed_modules:
