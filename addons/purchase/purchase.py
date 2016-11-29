036ed86720395571d0ebb2d7e35d698e7c1fcd1a
Goffin Simon <sig@odoo.com>
2016-11-29 11:20:48 +0100

    [FIX] purchase: _get_stock_move_price_unit
    The price unit returned by the function _get_stock_move_price_unit
    must be in the purchase unit of measure of the product.
    
    Before the fix:
    
    - Set "Use anglo-saxon accounting" True at company settings.
    - Check that the "Account Properties" on product category are set (set a price difference account).
    - Set costing method = Average price, Inventory valuation = Perpetual (automated).
    - Create and confirm a PO (product purchased with unit of measure "Unit(s)" and purchase unit of measure "Dozen(s)")
    - Validate shipment (Receive the product)
    - Create and validate the supplier invoice
    - View the accounting journal entries
    
    Bug:
    
    In the invoice journal entry, there were:
    
    In the output account: debit=0.0 and credit=price unit of the PO line
    In the price difference account: debit= (11/12)*price unit of the PO line and credit=0.0
    In the input account: debit=(1/12)*price unit of the PO line and credit=0.0
    
    Expected behavior:
    
    In the output account: debit=0.0 and credit=price unit of the PO line
    In the input account: debit=price unit of the PO line and credit=0.0
    
    opw:694108

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -560,8 +560,8 @@ class PurchaseOrderLine(models.Model):
         price_unit = line.price_unit
         if line.taxes_id:
             price_unit = line.taxes_id.with_context(round=False).compute_all(price_unit, currency=line.order_id.currency_id, quantity=1.0)['total_excluded']
-        if line.product_uom.id != line.product_id.uom_id.id:
-            price_unit *= line.product_uom.factor / line.product_id.uom_id.factor
+        if line.product_uom.id != line.product_id.uom_po_id.id:
+            price_unit *= line.product_uom.factor / line.product_id.uom_po_id.factor
         if order.currency_id != order.company_id.currency_id:
             price_unit = order.currency_id.compute(price_unit, order.company_id.currency_id, round=False)
         return price_unit
