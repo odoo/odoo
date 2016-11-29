e0ccc7af6d0d5fa69d8d744581f91d60798a466d
Martin Trigaux <mat@odoo.com>
2016-11-29 15:15:14 +0100

    [FIX] sale: missing sale_line reference on refund
    When creating a refund, the many2many information was lost.
    The field invoice_lines on a sale.order.line only contained the first invoice,
    not the refund. This means that a line appears as fully invoiced while it could
    be refunded.

--- addons/sale/models/account_invoice.py
+++ addons/sale/models/account_invoice.py
@@ -55,12 +55,11 @@ class AccountInvoice(models.Model):
     @api.model
     def _refund_cleanup_lines(self, lines):
         result = super(AccountInvoice, self)._refund_cleanup_lines(lines)
-        if self.env.context.get('mode') == 'modify':
-            for i in xrange(0, len(lines)):
-                for name, field in lines[i]._fields.iteritems():
-                    if name == 'sale_line_ids':
-                        result[i][2][name] = [(6, 0, lines[i][name].ids)]
-                        lines[i][name] = False
+        for i in xrange(0, len(lines)):
+            for name, field in lines[i]._fields.iteritems():
+                if name == 'sale_line_ids':
+                    result[i][2][name] = [(6, 0, lines[i][name].ids)]
+                    lines[i][name] = False
         return result
 
     @api.multi
