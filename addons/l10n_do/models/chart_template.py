e0565e21404780c51aedad90e1cb1763ac71c520
Gustavo Valverde <g.valverde02@gmail.com>
2016-12-06 20:49:04 -0400

    [REF] Change new journal creation approach
    Using the new approach recommended by @qdp-odoo

--- addons/l10n_do/models/chart_template.py
+++ addons/l10n_do/models/chart_template.py
@@ -4,28 +4,6 @@
 from openerp import models, api, _
 
 
-class AccountChartTemplate(models.Model):
-    _inherit = "account.chart.template"
-
-    @api.multi
-    def _prepare_all_journals(
-            self, acc_template_ref, company, journals_dict=None):
-        res = super(AccountChartTemplate, self)._prepare_all_journals(
-            acc_template_ref, company, journals_dict=journals_dict)
-        if not self == self.env.ref('l10n_do.do_chart_template'):
-            return res
-        res.append(
-            {'name': _('Caja Chica'),
-             'type': 'cash',
-             'code': _('CSH2'),
-             'company_id': company.id,
-             'show_on_dashboard': True,
-             # 'sequence': 4,
-             },
-             )
-        return res
-
-
 class WizardMultiChartsAccounts(models.TransientModel):
     _inherit = 'wizard.multi.charts.accounts'
 
@@ -38,4 +16,19 @@ class WizardMultiChartsAccounts(models.TransientModel):
                 [('code', '=', '999999'), ('user_type_id', '=', self.env.ref(
                                      "account.data_unaffected_earnings").id)])
         account.write({'code': '61010100'})
+        return res
+
+    @api.model
+    def _get_default_bank_account_ids(self):
+        return [
+            {'acc_name': _('Cash'), 'account_type': 'cash'},
+            {'acc_name': _('Caja Chica'), 'account_type': 'cash'},
+            {'acc_name': _('Bank'), 'account_type': 'bank'}
+            ]
+
+    @api.model
+    def default_get(self, fields):
+        res = super(WizardMultiChartsAccounts, self).default_get(fields)
+        if 'bank_account_ids' in fields:
+            res.update({'bank_account_ids': self._get_default_bank_account_ids()})
         return res
