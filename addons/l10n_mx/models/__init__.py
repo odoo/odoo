9a0e3a11dbb30a86fc5e5f2b9b7a6876f1744ca1
Nhomar Hernandez <nhomar@vauxoo.com>
2016-12-09 13:46:23 +0100

    [IMP] l10n_mx: Completely refactored Mexican CoA Template. Courtesy of Vauxoo.
    * Added accounts provided following the structure provided by the SAT
    this accounts are only the minimal necessary ones, with the objective
    to set the first easiest engagement to new users and tags will represent the actual information
    from the document linked.
    
    * The name of the tag is the concatenation of tag.code + tag.name, because the account.tag
    model have not the code field.
    
    Note: we will need more accounts but some of them for special operations
    which are not basic at all, then it is better just propose the operational accounts
    once we understand a proper closing process.
    
    * Set the proper user-types on accounts in order to be able to use it with the official reports this CoA
    for operational purpose.
    
    * Added nature field and data in tags to allow set this value in electronic account report, and
    set this value with data pre loaded, we did not use user.type for this due to the fact that the same user type can have different nature (if it grows for credit or debit), I do not know if this field brakes the stable rule we can discuse about that.
    
    * Assign the account tags with in the Mexican CoA template installation, and with an onchange(account.code) when manually modifying/creating accounts.
    
    Rationale: If the code of the accounts follows the pattern 111.00.00 or 111-00-00 then it will
    set the tag 111.00 if the tag exists, if not then this will be ignored.
    
    * Generate an account tag by each second level account in SAT catalog, and assign to the corresponding account.

---
+++ addons/l10n_mx/models/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import chart_template
+from . import account
