6f84d832d7755174d0b7caa774bfc437406d8dae
Lucas Perais (lpe) <lpe@odoo.com>
2016-12-09 15:03:41 +0100

    [FIX] account: Nicely warn the user that no sequence is set when refund_sequence is True.
    Avoid a traceback when the refund_sequence is not set
    
    Closes #14666
    OPW #697292

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -123,7 +123,6 @@ class AccountMove(models.Model):
     def post(self):
         invoice = self._context.get('invoice', False)
         self._post_validate()
-
         for move in self:
             move.line_ids.create_analytic_lines()
             if move.name == '/':
@@ -137,7 +136,10 @@ class AccountMove(models.Model):
                         # If invoice is actually refund and journal has a refund_sequence then use that one or use the regular one
                         sequence = journal.sequence_id
                         if invoice and invoice.type in ['out_refund', 'in_refund'] and journal.refund_sequence:
+                            if not journal.refund_sequence_id:
+                                raise UserError(_('Please define a sequence for the refunds'))
                             sequence = journal.refund_sequence_id
+                                                            
                         new_name = sequence.with_context(ir_sequence_date=move.date).next_by_id()
                     else:
                         raise UserError(_('Please define a sequence on the journal.'))
