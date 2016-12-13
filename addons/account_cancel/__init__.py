8cf92b2ff6d64e7385b096e198b78c69dffeadb0
Kinjal Mehta <kme@odoo.com>
2016-12-13 16:27:06 +0530

    [IMP]account_cancel, l10n_fr: Due to inalterability of data in France accounting, block the installation of accunt_cancel module if l1on_fr has already installed and block the installation of l10n_fr module if account_cancel has already installed.

--- addons/account_cancel/__init__.py
+++ addons/account_cancel/__init__.py
@@ -2,3 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import models
+
+from odoo.exceptions import ValidationError
+
+from odoo import api, SUPERUSER_ID, _
+
+
+def _pre_install_account_cancel(cr):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    if env['ir.module.module'].search([('name', '=', 'l10n_fr'), ('state', '=', 'installed')]):
+        raise ValidationError(_('You cannot install Cancel Journal Entries module. \n To install this module, first you have to uninstall "France - Accounting" module.'))
