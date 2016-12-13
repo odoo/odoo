f226e66548d2edbddd25d0e19805dc2370a79d44
Juan Carlos del Valle <jc.ekinox@gmail.com>
2016-12-13 04:21:43 -0600

    [FIX] hr_expenses: add missing tax_ids
    * 300afa9 already fixed the bug of the account move for expenses with more than 1 taxes but the taxes were not used.
    Adding in `_prepare_move_line` to be included in the created move line.
    
    * added Contributor License Agreement
    
    Closes #12849

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -159,6 +159,7 @@ class HrExpense(models.Model):
             'amount_currency': line['price'] > 0 and abs(line.get('amount_currency')) or -abs(line.get('amount_currency')),
             'currency_id': line.get('currency_id'),
             'tax_line_id': line.get('tax_line_id'),
+            'tax_ids': line.get('tax_ids'),
             'ref': line.get('ref'),
             'quantity': line.get('quantity',1.00),
             'product_id': line.get('product_id'),
