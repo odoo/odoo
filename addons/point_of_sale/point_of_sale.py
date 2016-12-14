odoo/odoo

commit fe589bee7138ca5ca023fd793fc26b92382e6b2c

[FIX] point_of_sale: set pricelist in create
The pricelist is set by default as the one of the first opened session. It's not
necessary correct (if more than one session) and is not working for rescue
sessions. Always set one.

addons/point_of_sale/point_of_sale.py
@@ -763,6 +763,7 @@ class pos_order(osv.osv):
             # set name based on the sequence specified on the config
             session = self.pool['pos.session'].browse(cr, uid, values['session_id'], context=context)
             values['name'] = session.config_id.sequence_id._next()
+            values.setdefault('session_id', session.config_id.pricelist_id.id)
         else:
             # fallback on any pos.order sequence
             values['name'] = self.pool.get('ir.sequence').next_by_code(cr, uid, 'pos.order', context=context)

commit 1d2baac42ed08e2eae67099607d8991810aad303

[FIX] point_of_sale: correct typo in order pricelist selection
Typo introduced in fe589bee7138ca5ca023fd793fc26b92382e6b2c
prevented the selection of the correct non-default pricelist.

addons/point_of_sale/point_of_sale.py
@@ -932,7 +932,7 @@ class pos_order(osv.osv):
             # set name based on the sequence specified on the config
             session = self.pool['pos.session'].browse(cr, uid, values['session_id'], context=context)
             values['name'] = session.config_id.sequence_id._next()
-            values.setdefault('session_id', session.config_id.pricelist_id.id)
+            values.setdefault('pricelist_id', session.config_id.pricelist_id.id)
         else:
             # fallback on any pos.order sequence
             values['name'] = self.pool.get('ir.sequence').next_by_code(cr, uid, 'pos.order', context=context)