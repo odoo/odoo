b2bc70c341716aa7a819ae83171e7339550927e3
Gustavo Valverde <g.valverde02@gmail.com>
2016-12-13 21:21:00 -0400

    [REF] Update CoA, taxes, fiscal positions & journals
    To comply with local and tax regulations:
    - The whole Charts of Accounts was changed
    - Most of the Journals has been preconfigured to comply with tax regulator
    - All the regulatory taxes were added and configured with group taxes

---
+++ addons/l10n_do/models/chart_template.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 iterativo (https://www.iterativo.do) <info@iterativo.do>
+
+from openerp import models, api, _
+
+
+class WizardMultiChartsAccounts(models.TransientModel):
+    _inherit = 'wizard.multi.charts.accounts'
+
+    @api.model
+    def _get_default_bank_account_ids(self):
+        return [
+            {'acc_name': _('Cash'), 'account_type': 'cash'},
+            {'acc_name': _('Caja Chica'), 'account_type': 'cash'},
+            {'acc_name': _('Bank'), 'account_type': 'bank'}
+            ]
+
+    @api.model
+    def default_get(self, fields):
+        res = super(WizardMultiChartsAccounts, self).default_get(fields)
+        if 'bank_account_ids' in fields:
+            res.update({'bank_account_ids': self._get_default_bank_account_ids()})
+        return res
