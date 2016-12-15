ff039183b08138dc4185d4add08e00b3c17eb846
Rohan Patel <rop@odoo.com>
2016-12-15 18:09:31 +0530

    [IMP] sale,sale_salesman: move app description, rewrite the manifest file

--- addons/sale/__manifest__.py
+++ addons/sale/__manifest__.py
@@ -3,44 +3,11 @@
 
 {
     'name': 'Sales',
-    'version': '1.0',
-    'category': 'hidden',
-    'sequence': 15,
-    'summary': 'Quotations, Sales Orders, Invoicing',
+    'category': 'Sales',
+    'summary': 'Sale',
     'description': """
-Manage sales quotations and orders
-==================================
-
-This module allows you to manage your sales goals in an effective and efficient manner by keeping track of all sales orders and history.
-
-It handles the full sales workflow:
-
-* **Quotation** -> **Sales order** -> **Invoice**
-
-Preferences (only with Warehouse Management installed)
-------------------------------------------------------
-
-If you also installed the Warehouse Management, you can deal with the following preferences:
-
-* Shipping: Choice of delivery at once or partial delivery
-* Invoicing: choose how invoices will be paid
-* Incoterms: International Commercial terms
-
-You can choose flexible invoicing methods:
-
-* *On Demand*: Invoices are created manually from Sales Orders when needed
-* *On Delivery Order*: Invoices are generated from picking (delivery)
-* *Before Delivery*: A Draft invoice is created and must be paid before delivery
-
-With this module you can personnalize the sales order and invoice report with
-categories, subtotals or page-breaks.
-
-The Dashboard for the Sales Manager will include
-------------------------------------------------
-* My Quotations
-* Monthly Turnover (Graph)
+This module contains all the common features of Sales Management and eCommerce.
     """,
-    'website': 'https://www.odoo.com/page/crm',
     'depends': ['sales_team', 'account', 'procurement', 'report', 'web_tour'],
     'data': [
         'data/ir_sequence_data.xml',
