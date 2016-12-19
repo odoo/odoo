3ea2abbd29936facbdb09899568224c48bfe3eb7
Martin Trigaux <mat@odoo.com>
2016-12-19 16:06:35 +0100

    [FIX] purchase,mrp: match method signature
    To have the same as the main method in stock

--- addons/mrp/models/stock_warehouse.py
+++ addons/mrp/models/stock_warehouse.py
@@ -118,7 +118,7 @@ class StockWarehouse(models.Model):
         return routes
 
     @api.multi
-    def _update_name_and_code(self, name, code):
+    def _update_name_and_code(self, name=False, code=False):
         res = super(StockWarehouse, self)._update_name_and_code(name, code)
         # change the manufacture procurement rule name
         for warehouse in self:
