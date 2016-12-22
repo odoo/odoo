d4b2e16fd65d409c5983ea8199e6a51a3e3d7db9
Jeremy Kersten <jke@odoo.com>
2016-12-22 21:17:54 +0100

    [FIX] website: compress_html is obsolete
    This option is no more implemented since the refactoring of ir.qweb
    Gzip (in nginx) do the same and better...

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -178,7 +178,6 @@ class Website(models.Model):
     google_management_client_secret = fields.Char('Google Client Secret')
 
     user_id = fields.Many2one('res.users', string='Public User', default=lambda self: self.env.ref('base.public_user').id)
-    compress_html = fields.Boolean('Compress HTML') # TODO: REMOVE ME IN SAAS-14
     cdn_activated = fields.Boolean('Activate CDN for assets')
     cdn_url = fields.Char('CDN Base URL', default='')
     cdn_filters = fields.Text('CDN Filters', default=lambda s: '\n'.join(DEFAULT_CDN_FILTERS), help="URL matching those filters will be rewritten using the CDN Base URL")
