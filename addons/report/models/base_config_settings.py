869c6f49a03537c8fd1979d0e3644176819f77a8
Fabien Pinckaers <fp@openerp.com>
2016-12-24 16:47:29 -0800

    [IMP] report: config button to change template

--- addons/report/models/base_config_settings.py
+++ addons/report/models/base_config_settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, _
 
 
 class BaseConfigSettings(models.TransientModel):
@@ -24,3 +24,19 @@ class BaseConfigSettings(models.TransientModel):
         template = self.default_report_template
         if template=='standard': template='default'
         return self._prepare_report_view_action('report.report_template_'+template)
+
+    def change_report_template(self):
+        self.ensure_one()
+        template = self.env.ref('report.view_company_report_form', False)
+        return {
+            'name': _('Choose Your Report Layout'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_id': self.env.user.company_id.id,
+            'res_model': 'res.company',
+            'views': [(template.id, 'form')],
+            'view_id': template.id,
+            'target': 'new',
+        }
+
