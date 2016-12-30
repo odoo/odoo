06d0ee7dcf0314d5b8c7438e491c6a2a04a77585
Jose Suniaga [Vauxoo] <josemiguel@vauxoo.com>
2016-12-30 21:56:10 +0000

    [FIX] base_address_city: inherit partner form view to enforce cities
    was removed the inherited to function _fields_view_get_address,
    and inherit a xml view to use the field country_enforce_cities
    to force use of city_id instead city. Keeping up the current design.
    
    [FIX] base_address_extended: removed duplicated city field

--- addons/base_address_city/models/res_partner.py
+++ addons/base_address_city/models/res_partner.py
@@ -5,30 +5,6 @@ from lxml import etree
 
 from odoo import api, models, fields
 
-class FormatAddressMixin(models.AbstractModel):
-    _inherit = "format.address.mixin"
-
-    @api.model
-    def fields_view_get_address(self, arch):
-        arch = super(FormatAddressMixin, self).fields_view_get_address(arch)
-        #render the partner address accordingly to address_view_id
-        doc = etree.fromstring(arch)
-        for city_node in doc.xpath("//field[@name='city']"):
-            replacement_xml = """
-            <div>
-            <field name="country_enforce_cities" invisible="1"/>
-            <div attrs="{'invisible': [('country_enforce_cities', '=', False)]}">
-                <field name='city' attrs="{'invisible': ['|', ('city_id', '!=', False), ('city', '=', False)]}"/>
-                <field name='city_id'/>
-            </div>
-            </div>
-            """
-            city_id_node = etree.fromstring(replacement_xml)
-            city_node.getparent().replace(city_node, city_id_node)
-
-        arch = etree.tostring(doc)
-        return arch
-
 
 class Partner(models.Model):
     _inherit = 'res.partner'
