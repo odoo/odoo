6140e25902dc8b3203e19f9735ddfd0f977cb75f
David Arnold <dar@devco.co>
2017-01-07 20:32:29 -0500

    [IMP] adapt to updating use cas
    The `tax_template_ref` dictionary is actually necessary, if we want to provide for something like this:
    ```python
        def _get_account_vals(self, company, account_template, tax_ids, code_acc):
            val = super(UpdateChartTemplateInherit, self)._get_account_vals(
                self, company, account_template, tax_ids, code_acc)
            tax_ids_to_delete = []
            if account_template.tax_ids_delete:
                for tax in account_template.tax_ids_to_delete:
                    tax_ids_to_delete.append(tax_template_ref[tax.id])
            val['tax_ids'] = [(4, t, 0) for t in tax_ids] + [(3, t, 0) for t in tax_ids_to_delete]
    
            tag_ids_to_delete = account_template.tag_ids_to_delete
            val['tag_ids'] = [(4, t, 0) for t in tag_ids] + [(3, t, 0) for t in tag_ids_to_delete]
    
            return val
    ```

--- addons/account/models/chart_template.py
+++ addons/account/models/chart_template.py
@@ -350,10 +350,13 @@ class AccountChartTemplate(models.Model):
         new_xml_id = str(company.id)+'_'+template_xmlid.name
         return ir_model_data._update(model, template_xmlid.module, vals, xml_id=new_xml_id, store=True, noupdate=True, mode='init', res_id=False)
 
-    def _get_account_vals(self, company, account_template, tax_ids, code_acc):
+    def _get_account_vals(self, company, account_template, tax_ids, code_acc, tax_template_ref):
         """ This method generates a dictionnary of all the values for the account that will be created.
         """
         self.ensure_one()
+        tax_ids = []
+        for tax in account_template.tax_ids:
+            tax_ids.append(tax_template_ref[tax.id])
         val = {
                 'name': account_template.name,
                 'currency_id': account_template.currency_id and account_template.currency_id.id or False,
@@ -382,15 +385,11 @@ class AccountChartTemplate(models.Model):
         account_tmpl_obj = self.env['account.account.template']
         acc_template = account_tmpl_obj.search([('nocreate', '!=', True), ('chart_template_id', '=', self.id)], order='id')
         for account_template in acc_template:
-            tax_ids = []
-            for tax in account_template.tax_ids:
-                tax_ids.append(tax_template_ref[tax.id])
-
             code_main = account_template.code and len(account_template.code) or 0
             code_acc = account_template.code or ''
             if code_main > 0 and code_main <= code_digits:
                 code_acc = str(code_acc) + (str('0'*(code_digits-code_main)))
-            vals = self._get_account_vals(company, account_template, tax_ids, code_acc)
+            vals = self._get_account_vals(company, account_template, tax_ids, code_acc, tax_template_ref)
             new_account = self.create_record_with_xmlid(company, account_template, 'account.account', vals)
             acc_template_ref[account_template.id] = new_account
         return acc_template_ref
