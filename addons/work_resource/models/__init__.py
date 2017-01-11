0acdba990581e94cfceab5b057c7f5e98612600f
Thibault Delavallée <tde@odoo.com>
2017-01-11 17:45:13 +0100

    fix to commit in ref models I

--- addons/work_resource/models/__init__.py
+++ addons/work_resource/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import res_company
 import work_resource
+import res_company
 import work_mixins
