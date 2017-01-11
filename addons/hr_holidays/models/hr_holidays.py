36cf7d540087c2c614a52fdee6bf93e7b1b1d915
Thibault Delavallée <tde@odoo.com>
2017-01-11 14:47:30 +0100

    imp: hr_holidays: remove product stuff and use resource stuff

--- addons/hr_holidays/models/hr_holidays.py
+++ addons/hr_holidays/models/hr_holidays.py
@@ -280,13 +280,7 @@ class Holidays(models.Model):
 
         if employee_id:
             employee = self.env['hr.employee'].browse(employee_id)
-            resource = employee.resource_id
-            if resource and resource.calendar_id:
-                hours = resource.calendar_id.get_work_hours_count(from_dt, to_dt, resource_id=resource.id, compute_leaves=True)
-                uom_hour = resource.calendar_id.uom_id
-                uom_day = self.env.ref('product.product_uom_day')
-                if uom_hour and uom_day:
-                    return uom_hour._compute_quantity(hours, uom_day)
+            return employee.get_work_days_count(from_dt, to_dt)
 
         time_delta = to_dt - from_dt
         return math.ceil(time_delta.days + float(time_delta.seconds) / 86400)
