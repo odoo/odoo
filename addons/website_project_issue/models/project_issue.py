07f82d0d9d97e149095fb94fd4c81dc74f008b50
Jeremy Kersten <jke@odoo.com>
2017-01-12 17:40:29 +0100

    [IMP] website_project_issue: allow to see attachment from object in portal.
    Before this commit, only attachment linked to a message_ids was visible.
    So when you open the ticket, you don't have the 'main' attachments

--- addons/website_project_issue/models/project_issue.py
+++ addons/website_project_issue/models/project_issue.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, exceptions, models, fields
+from odoo import api, fields, exceptions, models
 
 
 class Issue(models.Model):
     _name = "project.issue"
     _inherit = ['project.issue']
 
+    attachment_ids = fields.One2many('ir.attachment', compute='_compute_attachment_ids', string="Main Attachments",
+                                     help="Attachment that don't come from message.")
     website_url = fields.Char('Website URL', compute='_compute_website_url', help='The full URL to access the document through the website.')
 
     def _compute_website_url(self):
         for issue in self:
             issue.website_url = '/my/issues/%s' % issue.id
+
+    def _compute_attachment_ids(self):
+        for issue in self:
+            attachment_ids = self.env['ir.attachment'].search([('res_id', '=', issue.id), ('res_model', '=', 'project.issue')]).ids
+            message_attachment_ids = self.mapped('message_ids.attachment_ids').ids # from mail_thread
+            issue.attachment_ids = list(set(attachment_ids) - set(message_attachment_ids))
 
     @api.multi
     def get_access_action(self):
