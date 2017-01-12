c32334a353882a5b1c2b82b434642d8105b856eb
Jeremy Kersten <jke@odoo.com>
2017-01-12 17:40:29 +0100

    [IMP] website_project_issue: allow to see attachment from main object.
    Before this commit, only attachment linked to a message_ids was visible.
    So when you open the ticket, you don't have the 'main' pictures

--- addons/website_project_issue/models/project_issue.py
+++ addons/website_project_issue/models/project_issue.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, exceptions, models, _
+from odoo import api, fields, exceptions, models, _
 
 
 class Issue(models.Model):
     _name = "project.issue"
     _inherit = ['project.issue']
+
+    main_attachment_ids =  fields.One2many('ir.attachment', compute='_compute_attachment_ids', string="Main Attachments",
+                                           help="Attachment that don't come from message.")
+    def _compute_attachment_ids(self):
+        for issue in self:
+            all_attach = self.env['ir.attachment'].search([('res_id', '=', issue.id), ('res_model', '=', 'project.issue')]).ids
+            message_attach_ids = []
+            for m in self.mapped('message_ids'):
+                message_attach_ids += m.attachment_ids.ids
+            issue.main_attachment_ids = list(set(all_attach) - set(message_attach_ids))
 
     @api.multi
     def get_access_action(self):
