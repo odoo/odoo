7f9cb6934fc611f25092a9df05415766ac1394f3
Denis Ledoux <dle@odoo.com>
2017-01-13 15:34:10 +0100

    [FIX] website_sale*: default company & warehouse for website orders
    By default, the default warehouse of a sale order is the first warehouse
    of the company of the user creating the sale order.
    
    In the case of a sale order created from the website,
    the user who is creating the sale order is the superuser,
    a.k.a the administrator.
    
    There was a big issue in the context of a multicompany database,
    with an administrator changing of company:
    the sale orders coming from the website would suddenly change
    of company, which is wrong.
    
    This was even more severe when the admin changed to a company
    which had no warehouse defined:
    It was then impossible to create the website cart, as no default warehouse
    was found, while the warehouse is a required field on the order.
    
    This revision ensures that the cart / sale order is always created
    under the website company
    
    opw-704557

---
+++ addons/website_sale_stock/models/website.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from openerp import SUPERUSER_ID
+from openerp.osv import orm
+
+
+class website(orm.Model):
+    _inherit = 'website'
+
+    def _prepare_sale_order_values(self, cr, uid, w, partner, context=None):
+        values = super(website, self)._prepare_sale_order_values(cr, uid, w, partner, context=context)
+        if values['company_id']:
+            warehouse_ids = self.pool['stock.warehouse'].search(cr, SUPERUSER_ID, [('company_id', '=', values['company_id'])], context=context)
+            if warehouse_ids:
+                values['warehouse_id'] = warehouse_ids[0]
+        return values
