d454d5d50f59b5ef3be7c8894b7a56932db3bc66
Laurent Mignon <laurent.mignon@acsone.eu>
2017-01-13 13:20:15 +0100

    [FIX] website: port of rev a98d60c The forward-port of the above revision missed the conversion to the new api

--- addons/website/models/ir_http.py
+++ addons/website/models/ir_http.py
@@ -371,7 +371,7 @@ class PageConverter(werkzeug.routing.PathConverter):
         query = query and query.startswith('website.') and query[8:] or query
         if query:
             domain += [('key', 'like', query)]
-        website_id = request.context.get('website_id') or request.registry['website'].search(cr, uid, [], limit=1)[0]
+        website_id = request.context.get('website_id') or request.env['website'].search([], limit=1).id
         domain += ['|', ('website_id', '=', website_id), ('website_id', '=', False)]
 
         views = View.search_read(domain, fields=['key', 'priority', 'write_date'], order='name')
