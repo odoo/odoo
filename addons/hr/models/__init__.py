c1526f20e3ea546618ad06af69e4fff8222ba692
Thibault Delavallée <tde@odoo.com>
2017-01-26 14:09:50 +0100

    [IMP] hr: add settings and add box to configure default company working hours

--- addons/hr/models/__init__.py
+++ addons/hr/models/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import hr
+import hr_config_settings
 import mail_alias
 import mail_thread
 import res_partner
--- addons/resource/__manifest__.py
+++ addons/resource/__manifest__.py
@@ -19,7 +19,6 @@ associated to every resource. It also manages the leaves of every resource.
         'security/ir.model.access.csv',
         'security/resource_security.xml',
         'views/resource_views.xml',
-        'views/res_company_views.xml',
     ],
     'demo': [
         'data/resource_demo.xml'
