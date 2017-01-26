fa9b4d54b0f8c54674bee3938945cf7d7b9fc966
Thibault Delavallée <tde@odoo.com>
2017-01-26 14:09:50 +0100

    [IMP] hr: add settings and add box to configure default company working hours

--- addons/hr/__manifest__.py
+++ addons/hr/__manifest__.py
@@ -38,6 +38,7 @@ You can manage:
         'security/ir.model.access.csv',
         'views/hr_views.xml',
         'views/hr_templates.xml',
+        'views/hr_config_settings_views.xml',
         'data/hr_data.xml',
     ],
     'demo': [
