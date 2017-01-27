48bf0a3856fd80304c2b30ffb31760db8c112656
Zakaria Makrelouf <z.makrelouf@gmail.com>
2017-01-27 11:12:57 +0100

    [FIX] l10n_be_invoice_bba: make selection labels translatable

--- addons/l10n_be_invoice_bba/invoice.py
+++ addons/l10n_be_invoice_bba/invoice.py
@@ -21,11 +21,13 @@ class account_invoice(osv.osv):
 
     @api.cr_uid_context
     def _get_reference_type(self, cursor, user, context=None):
-        """Add BBA Structured Communication Type and change labels from 'reference' into 'communication' """
+        """Add BBA Structured Communication Type and change
+        labels from 'reference' into 'communication' """
         res = super(account_invoice, self)._get_reference_type(cursor, user,
                 context=context)
-        res[[i for i,x in enumerate(res) if x[0] == 'none'][0]] = ('none', 'Free Communication')
-        res.append(('bba', 'BBA Structured Communication'))
+        res[[i for i,x in enumerate(res) if x[0] == 'none'][0]] = \
+            ('none', _('Free Communication'))
+        res.append(('bba', _('BBA Structured Communication')))
         #l_logger.warning('reference_type =  %s' %res )
         return res
 
