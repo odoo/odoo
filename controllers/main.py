183234c5b66d12ffa3e57b042dc06a34e60084d2
Yannick Tivisse <yti@odoo.com>
2017-01-31 13:03:43 +0100

    [FIX] website_sale_coupon: Allow public users to use coupons on website

--- controllers/main.py
+++ controllers/main.py
@@ -7,5 +7,5 @@ class Website_coupon(http.Controller):
     @http.route(['/shop/apply_coupon'], type='json', auth="public", website=True)
     def shop_apply_coupon(self, promo_code, **post):
         order = request.website.sale_get_order()
-        coupon_status = request.env['sale.coupon.apply.code'].apply_coupon(order, promo_code)
+        coupon_status = request.env['sale.coupon.apply.code'].sudo().apply_coupon(order, promo_code)
         return coupon_status
