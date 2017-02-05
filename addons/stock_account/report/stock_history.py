c87289bc6be5d1f0fa70754193871369618e5f4e
Florent de Labarre <florent.mirieu@gmail.com>
2017-02-05 22:01:37 +0100

    [FIX] In stock_history query

--- addons/stock_account/report/stock_history.py
+++ addons/stock_account/report/stock_history.py
@@ -79,7 +79,9 @@ class StockHistory(models.Model):
                 product_template_id,
                 SUM(quantity) as quantity,
                 date,
-                SUM(price_unit_on_quant * quantity) / SUM(quantity) as price_unit_on_quant,
+                CASE WHEN SUM(quantity)=0 THEN 0.0
+                    ELSE SUM(price_unit_on_quant * quantity) / SUM(quantity)
+                    END AS price_unit_on_quant,
                 source,
                 string_agg(DISTINCT serial_number, ', ' ORDER BY serial_number) AS serial_number
                 FROM
