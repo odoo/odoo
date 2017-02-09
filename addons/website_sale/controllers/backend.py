0a31e95d3787e789cfa40f9a1546170fbcfc13c4
Christophe Monniez <moc@odoo.com>
2017-02-09 11:05:31 +0530

    [IMP] website_sale: manage abandoned cart
    Purpose: Add button to see the number of
    abandoned carts and the ability to send
    a recovery email to those customers.
    
    On the abandoned carts menu, you can see
    the SO from the website where no reminder
    email has been sent.
    By selecting them, you can send an email
    to the customer to tell him there is still
    a cart waiting.
    Clicking on the link, he can reuse his old
    cart or merge it with his current one.
    
    An abandoned cart is a SO from a website
    sales team, and created after a treshold
    parametered by a 'delay' settings (number
    of hours to be considered as abandoned).

--- addons/website_sale/controllers/backend.py
+++ addons/website_sale/controllers/backend.py
@@ -86,10 +86,8 @@ class WebsiteSaleBackend(WebsiteBackend):
                 ('invoice_status', '=', 'to invoice'),
             ]),
             order_carts_abandoned_count=request.env['sale.order'].search_count(sale_order_domain + [
-                ('state', '=', 'draft'),
-                ('order_line', '!=', 'False'),
-                ('partner_id', '!=', request.env.ref('base.public_partner').id),
-                ('date_order', '<=', fields.Datetime.to_string(datetime.now() - timedelta(hours=1))),
+                ('is_abandoned_cart', '=', True),
+                ('cart_recovery_email_sent', '=', False)
             ]),
             payment_to_capture_count=request.env['payment.transaction'].search_count([
                 ('state', '=', 'authorized'),
