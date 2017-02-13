5f1a2ace0d882c41a595d93715bc85a8178bfc97
Florent de Labarre <florent.mirieu@gmail.com>
2017-02-13 17:23:59 +0100

    [FIX] name_search doesn't work in bom

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -334,7 +334,7 @@ class ProductTemplate(models.Model):
         while True:
             domain = templates and [('product_tmpl_id', 'not in', templates.ids)] or []
             args = args if args is not None else []
-            products_ns = Product.name_search(name, args+domain, operator=operator)
+            products_ns = Product.name_search(name, args+domain, operator=operator, limit=limit)
             products = Product.browse([x[0] for x in products_ns])
             templates |= products.mapped('product_tmpl_id')
             if (not products) or (len(templates) > limit):
