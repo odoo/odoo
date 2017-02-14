odoo/odoo

commit 2d23e824495063159fd0ca7feb51851ef74a9bac

[IMP] website: Make company logo editable from frontend
Purpose
=======

Clicking the company logo takes to the backend's company settings. Users get lost and don't know how to go back to the website.

Specs
=====

It should be a picture selection wizard. If possible the default picture should be the company logo if already set.

addons/website/static/src/js/website.snippets.editor.js
@@ -70,22 +70,6 @@ options.registry.company_data = options.Class.extend({
             });
         }
     },
-
-    on_focus: function () {
-        this._super.apply(this, arguments);
-
-        var proto = options.registry.company_data.prototype;
-
-        Dialog.confirm(null, _t("Do you want to edit the company data ?"), {
-            confirm_callback: function () {
-                editor.editor_bar.save_without_reload().then(function () {
-                    proto.__link_deferred.then(function (link) {
-                        window.location.href = link;
-                    });
-                });
-            },
-        });
-    },
 });
 
 options.registry.slider = options.Class.extend({

addons/website/views/website_templates.xml
@@ -215,7 +215,7 @@
 <template id="layout_logo_show" inherit_id="website.layout" customize_show="True" name="Show Logo">
     <xpath expr="//header//a[@class='navbar-brand']" position="replace">
         <a href="/" class="navbar-brand logo">
-            <img src="/logo.png" t-att-alt="'Logo of %s' % res_company.name" t-att-title="res_company.name"/>
+            <span t-field="res_company.logo" t-options="{'widget': 'image'}" t-att-alt="'Logo of %s' % res_company.name" t-att-title="res_company.name" />
         </a>
     </xpath>
 </template>

commit 26ce4e6c9c85653bad1fb5707f97b430780329e3

[FIX] website: restore company edition option
Commit 2d23e824495063159fd0ca7feb51851ef74a9bac broke the feature without mercy.

The mentioned commit introduces a nice improvement to allow the edition
of the company logo directly from frontend. However it also breaks the
option for non-logo parts (example: clicking on the address on the
contact us page). It also leaves most of the option code behind, which
makes useless code being executed for nothing.

This commit restores the company option the way it was and simply
deactivates it for the logo edition, which was the desired behavior.

addons/website/static/src/js/website.snippets.editor.js
@@ -70,6 +70,22 @@ options.registry.company_data = options.Class.extend({
             });
         }
     },
+
+    on_focus: function () {
+        this._super.apply(this, arguments);
+
+        var proto = options.registry.company_data.prototype;
+
+        Dialog.confirm(null, _t("Do you want to edit the company data ?"), {
+            confirm_callback: function () {
+                editor.editor_bar.save_without_reload().then(function () {
+                    proto.__link_deferred.then(function (link) {
+                        window.location.href = link;
+                    });
+                });
+            },
+        });
+    },
 });
 
 options.registry.slider = options.Class.extend({

addons/website/views/snippets.xml
@@ -874,7 +874,7 @@
          data-no-check="true"/>
 
     <div data-js="company_data"
-         data-selector="img[src='/logo.png'], [data-oe-expression='res_company.partner_id']"
+         data-selector="[data-oe-expression='res_company.partner_id']"
          data-no-check="true"/>
 </template>
 </odoo>