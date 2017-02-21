3f63197b6c21bb13af61b4e87fb1456127baa616
Goffin Simon <sig@odoo.com>
2017-02-21 17:25:44 +0100

    [FIX] sale_margin: uninstall_hook
    When uninstalling the module sale_margin, the view 'sale_report' was deleted
    because this view was overwritten  in this module.
    
    But the module sale still uses the 'sale_report' view so it raised an error
    each time this view was required.
    
    opw:709328

--- addons/sale_margin/__init__.py
+++ addons/sale_margin/__init__.py
@@ -1,4 +1,21 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from functools import partial
+import openerp
+from openerp import api, SUPERUSER_ID
 import sale_margin
 import report
+
+
+
+def uninstall_hook(cr, registry):
+	def recreate_view(dbname):
+		db_registry = openerp.modules.registry.RegistryManager.new(dbname)
+		with api.Environment.manage(), db_registry.cursor() as cr:
+			env = api.Environment(cr, SUPERUSER_ID, {})
+			if 'sale.report' in env:
+				env['ir.module.module'].search([('name', '=', 'sale_margin')]).state
+				env['sale.report'].init()
+
+
+	cr.after("commit", partial(recreate_view, cr.dbname))
