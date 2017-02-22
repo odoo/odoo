8a9ba0442095e3c0f8e7942dadeac33aa299d951
Aaron Bohy <aab@odoo.com>
2017-02-22 16:11:48 +0100

    [FIX] web: boot_tests: there should be no more missing modules

--- addons/web/static/test/boot_tests.js
+++ addons/web/static/test/boot_tests.js
@@ -10,8 +10,8 @@ odoo.__DEBUG__.didLogInfo.then(function() {
     QUnit.test('all modules are properly loaded', function(assert) {
         assert.expect(2);
 
-        assert.deepEqual(modulesInfo.missing, ['web.web_client'],
-            "only the web client should be missing");
+        assert.deepEqual(modulesInfo.missing, [],
+            "no js module should be missing");
         assert.deepEqual(modulesInfo.failed, [],
             "no js module should have failed");
     });
