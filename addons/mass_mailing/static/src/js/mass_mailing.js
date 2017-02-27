19e86c8388109eccf271e10c4efb6bea7cb82f60
qsm-odoo <qsm@odoo.com>
2017-02-27 17:35:58 +0100

    [IMP] *: remove deprecated view extensions
    * account, mass_mailing, sales_team
    
    These view extensions have already been ignored for around two years.
    The new views development is a great opportunity to remove them.

--- addons/mass_mailing/static/src/js/mass_mailing.js
+++ addons/mass_mailing/static/src/js/mass_mailing.js
@@ -3,7 +3,6 @@ odoo.define('mass_mailing.mass_mailing', function (require) {
 var FieldTextHtml = require('web_editor.backend').FieldTextHtml;
 var KanbanRecord = require('web.KanbanRecord');
 var KanbanColumn = require('web.KanbanColumn');
-var KanbanView = require('web.KanbanView');
 
 KanbanRecord.include({
     on_card_clicked: function (event) {
@@ -24,17 +23,8 @@ KanbanColumn.include({
     },
 });
 
-KanbanView.include({
-    on_groups_started: function() {
-        this._super.apply(this, arguments);
-        if (this.dataset.model === 'mail.mass_mailing') {
-            this.$el.find('.oe_kanban_draghandle').removeClass('oe_kanban_draghandle');
-        }
-    },
-});
-
 FieldTextHtml.include({
-    get_datarecord: function() {
+    get_datarecord: function () {
         /* Avoid extremely long URIs by whitelisting fields in the datarecord
         that get set as a get parameter */
         var datarecord = this._super();
--- addons/sales_team/static/src/js/sales_team.js
+++ addons/sales_team/static/src/js/sales_team.js
@@ -1,47 +1,10 @@
 odoo.define('sales_team.update_kanban', function (require) {
 "use strict";
 
-var KanbanView = require('web.KanbanView');
 var KanbanRecord = require('web.KanbanRecord');
-var data = require('web.data');
-var session = require('web.session');
-
-KanbanView.include({
-    crm_display_members_names: function() {
-        /*
-         * Set avatar title for members.
-         * In kanban views, many2many fields only return a list of ids.
-         * We can implement return value of m2m fields like [(1,"Adminstration"),...].
-         */
-        var self = this;
-        var members_ids = [];
-
-        // Collect members ids
-        self.$el.find('img[data-member_id]').each(function() {
-            members_ids.push($(this).data('member_id'));
-        });
-
-        // Find their matching names
-        var dataset = new data.DataSetSearch(self, 'res.users', session.context, [['id', 'in', _.uniq(members_ids)]]);
-        dataset.read_slice(['id', 'name']).done(function(result) {
-            _.each(result, function(v) {
-                // Set the proper value in the DOM
-                self.$el.find('img[data-member_id=' + v.id + ']').attr('title', v.name).tooltip();
-            });
-        });
-    },
-    on_groups_started: function() {
-        var self = this;
-        self._super.apply(self, arguments);
-
-        if (self.dataset.model === 'crm.team') {
-            self.crm_display_members_names();
-        }
-    },
-});
 
 KanbanRecord.include({
-    on_card_clicked: function() {
+    on_card_clicked: function () {
         if (this.model === 'crm.team') {
             this.$('.oe_kanban_crm_salesteams_list a').first().click();
         } else {
@@ -49,5 +12,4 @@ KanbanRecord.include({
         }
     },
 });
-
 });
