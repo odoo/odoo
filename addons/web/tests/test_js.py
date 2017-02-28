9b14290b2ad35bb1350c1b6e13b32742d8cccf53
Aaron Bohy <aab@odoo.com>
2017-02-28 11:57:05 +0100

    [FIX] web: execute WebSuite tests post install
    So that tests written in addons are executed as well.

--- addons/web/tests/test_js.py
+++ addons/web/tests/test_js.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import unittest
 import odoo.tests
 
+
 class WebSuite(odoo.tests.HttpCase):
+
+    post_install = True
+    at_install = False
+
     def test_01_js(self):
-        self.phantom_js('/web/tests?mod=web',"","", login='admin')
+        self.phantom_js('/web/tests?mod=web', "", "", login='admin')
