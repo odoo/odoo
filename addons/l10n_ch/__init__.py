1ba42a6f2958b7c49c3b827d587ed7b97e64df84
qdp-odoo <qdp@odoo.com>
2017-03-03 14:47:16 +0100

    [IMP] l10n_ch: add support for ISR/BVR generation and postal bank accounts
    The ISR is a payment slip report for account.invoice, that we automatically send and print with the regular invoice report (to ease usability, as these two reports usually go together).
    
    [FIX] l10n_ch: italian translation
    
    A field was missing for italian chart of account translation to work.

--- addons/l10n_ch/__init__.py
+++ addons/l10n_ch/__init__.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Author: Nicolas Bessi. Copyright Camptocamp SA
-# Financial contributors: Hasa SA, Open Net SA,
-#                         Prisme Solutions Informatique SA, Quod SA
-# Translation contributors: brain-tec AG, Agile Business Group
-
 from odoo import api, SUPERUSER_ID
 
 def load_translations(cr, registry):
---
+++ addons/l10n_ch/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import account_invoice
+import mail_template
+import res_bank
+import res_company
+import res_config
