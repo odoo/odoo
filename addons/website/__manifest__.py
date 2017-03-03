98007b73b0faf6e22e26fc831763dc78ccf080c5
Rohan Patel <rop@odoo.com>
2017-03-03 16:30:37 +0100

    [REF] website: merge Website and Website Admin menu items
    This commit merge the two website menu items in order to ease user flow
    inside Odoo. When clicking on the Website item a server action is called
    that decides to redirect the user either to the dashboard if the user is
    an admin or a website publisher, either to the website for other people.
    Overriding this method allows to define a specific behavior. Future commit
    will also redirect sales people to the website dashboard.
    
    Dashboard itself is customized to add a header used to display a website
    button. It will be used in future commits to add other new stuff to the
    dashboard.

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -17,18 +17,16 @@ Odoo Website CMS
     'installable': True,
     'data': [
         'data/website_data.xml',
-        'data/web_planner_data.xml',
         'security/website_security.xml',
         'security/ir.model.access.csv',
-
         'views/website_templates.xml',
         'views/website_navbar_templates.xml',
         'views/snippets.xml',
         'views/website_views.xml',
         'views/res_config_views.xml',
         'views/ir_actions_views.xml',
-
         'wizard/base_language_install_views.xml',
+        'data/web_planner_data.xml',
     ],
     'demo': [
         'data/website_demo.xml',
