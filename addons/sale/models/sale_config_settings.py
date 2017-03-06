daf7b8a7c2706d6350706230d037ae25d701751c
Yannick Tivisse <yti@odoo.com>
2017-03-06 13:21:20 +0100

    [FIX] sale: Correct the tax inclusion definition in the sales config
    The commit 2c4b6ec introduces a switch between the definions of tax included and excluded.
    
    As the default value wasn't coherent with the intended behavior, the commit b1125f4 tries to fix this.
    
    This commit is correct. Switch back the selection labels and set back the default value.

--- addons/sale/models/sale_config_settings.py
+++ addons/sale/models/sale_config_settings.py
@@ -49,9 +49,9 @@ class SaleConfiguration(models.TransientModel):
         implied_group='sale.group_show_price_total',
         group='base.group_portal,base.group_user,base.group_public')
     sale_show_tax = fields.Selection([
-        ('subtotal', 'Tax-Included Prices'),
-        ('total', 'Tax-Excluded Prices')], "Tax Display",
-        default='total',
+        ('subtotal', 'Tax-Excluded Prices'),
+        ('total', 'Tax-Included Prices')], "Tax Display",
+        default='subtotal',
         required=True)
     default_invoice_policy = fields.Selection([
         ('order', 'Ordered quantities'),
