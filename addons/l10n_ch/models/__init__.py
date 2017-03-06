799f82f8117d0369ab0ea6e7fc035ffec9408117
Olivier Colson <oco@odoo.com>
2017-03-06 16:40:33 +0100

    [IMP] l10n_ch
    Add a new button to generate the ISR and remove the "double generation" of reports on 'Print invoice' button. This modification brings the code back to its original version and hence fixes the javascript bugs that occured with less than 3 workers running on the server and with Google Chrome when triggering multiple downloads simultaneously.
    
    [ADD] account
    
    Add a identifier to the 'register payment' button in order to reference it more easily and add the 'Print ISR' next to it.
    
    [FIX] l10n_ch
    
    The module did not install itself anymore, an import was missing.
    
    Report generation was crashing due to a function's suppression.

--- addons/l10n_ch/models/__init__.py
+++ addons/l10n_ch/models/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import account_invoice
+import mail_compose_message
 import mail_template
 import res_bank
 import res_company
