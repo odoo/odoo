5421bb64ae41d35a7e8b3af041de399b4dfd3206
Dhaval Panchal <dpa@odoo.com>
2017-03-08 16:38:51 +0530

    [IMP] l10n_pt: add tax groups for Portugal accounting

--- addons/l10n_pt/__manifest__.py
+++ addons/l10n_pt/__manifest__.py
@@ -19,6 +19,7 @@
            'data/l10n_pt_chart_data.xml',
            'data/account_chart_template_data.xml',
            'data/account_fiscal_position_template_data.xml',
+           'data/account_data.xml',
            'data/account_tax_data.xml',
            'data/account_chart_template_data.yml',
            ],
