93eb4661be9683348a554c3a3b6756180dd03783
Aaron Bohy <aab@odoo.com>
2017-03-09 09:34:09 +0100

    [FIX] web_editor: various fixes and new tests
    - use 'nodeOptions' instead of 'options' in FieldTextHTML as
      'options' doesn't exist, it has been renamed into nodeOptions
      in AbstractField.
    - mock iframe route in test to prevent the test framework from
      displaying an error (unimplemented route), and so the test from
      being red.
    - test that the url given in the node options is correctly used.
    - remove useless and broken code introduced by last rebase.

--- addons/web_editor/static/tests/web_editor_tests.js
+++ addons/web_editor/static/tests/web_editor_tests.js
@@ -23,19 +23,29 @@ QUnit.module('web_editor', {
     }
 });
 
-QUnit.test('field html widget', function(assert) {
-    assert.expect(2);
+QUnit.test('field html widget', function (assert) {
+    assert.expect(6);
 
     var form = testUtils.createView({
         View: FormView,
         model: 'mass.mailing',
         data: this.data,
         arch: '<form string="Partners">' +
-                '<field name="body" widget="html_frame"/>' +
+                '<field name="body" widget="html_frame" options="{\'editor_url\': \'/test\'}"/>' +
             '</form>',
         res_id: 1,
         session: {user_context: {lang: "en_us"}},
-        manualDestroy: true,
+        mockRPC: function (route) {
+            if (_.str.startsWith(route, '/test')) {
+                // those tests will be executed twice, once in readonly and once in edit
+                assert.ok(route.search('model=mass.mailing') > 0,
+                    "the route should specify the correct model");
+                assert.ok(route.search('res_id=1') > 0,
+                    "the route should specify the correct id");
+                return $.when();
+            }
+            return this._super.apply(this, arguments);
+        },
     });
 
     assert.strictEqual(form.$('iframe').length, 1, "should have rendered an iframe without crashing");
