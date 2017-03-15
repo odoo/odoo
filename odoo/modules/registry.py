c9edb9dccbf09d0bd5fe17273f4f394b1f79c01c
Raphael Collet <rco@openerp.com>
2017-03-15 16:47:56 +0100

    [FIX] registry: use a weak dictionary for `model_cache` to avoid memory leaks
    The class attribute `model_cache` refers to model classes, which refer to their
    own registry.  This cache potentially keeps all past registries alive!

--- odoo/modules/registry.py
+++ odoo/modules/registry.py
@@ -7,6 +7,7 @@
 from collections import Mapping, defaultdict, deque
 from contextlib import closing
 from operator import attrgetter
+from weakref import WeakValueDictionary
 import logging
 import os
 import threading
@@ -29,6 +30,9 @@ class Registry(Mapping):
     """
     _lock = threading.RLock()
     _saved_lock = None
+
+    # a cache for model classes, indexed by their base classes
+    model_cache = WeakValueDictionary()
 
     @lazy_classproperty
     def registries(cls):
@@ -176,12 +180,6 @@ class Registry(Mapping):
     def __setitem__(self, model_name, model):
         """ Add or replace a model in the registry."""
         self.models[model_name] = model
-
-    @lazy_classproperty
-    def model_cache(cls):
-        """ A cache for model classes, indexed by their base classes. """
-        # we cache 256 classes per registry on average
-        return LRU(cls.registries.count * 256)
 
     @lazy_property
     def field_sequence(self):
