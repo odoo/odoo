6a8e59b65f67e205500bedbf74b20e019730c2be
Thibault Delavallée <tde@odoo.com>
2017-03-16 17:20:43 +0100

    [IMP] website_quote: define default order quote template coming in settings
    Using a real ir.values default that user can customize in settings
    is better for user experience than an hardcoded template using an
    xml_id. Betcha.

--- addons/website_quote/models/__init__.py
+++ addons/website_quote/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import sale_order
-from . import sale_quote
-from . import product_template
+from .import sale_order
+from .import sale_quote
+from .import product_template
+from .import sale_config_settings
