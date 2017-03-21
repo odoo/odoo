227f8ab116186f93967840274086b78ac67697f8
Jérome Maes <jem@openerp.com>
2017-03-21 09:55:04 +0100

    [IMP] sale_timesheet: introduce timesheet revenue
    This commit introduces the concept of timesheet
    revenue, aka how much the timesheet bring back
    money.
    If the timesheet is not invoiced yet, the computed
    revenue is theorical, based on the billable type
    (if your product is Time and Material, or Fixed Price).
    
    The formulae use at the timesheet line creation is
    
         If product on delivered quantity:
            timesheet hours * (SO Line Price) * (1- discount),
         elif product on ordered quantities & create task:
             min (
                timesheet hours * (SO Line unit price) * (1- discount),
                TOTAL SO - TOTAL INVOICED - sum(timesheet
                    revenues with invoice_id=False)
             )
         else: (non billable case)
            0
    
    When the timesheet is invoiced, the revenue is recomputed
    based on the prorata of the invoice amount : prorata of
    number of hours for 'time and material' product,
    prorata of the theorical revenue for 'fixed price' product.
    
    The timesheet revenue is expressed in the currency of the
    company (Monetary field, based on the company_currency_id field
    of account.analytic.line) to ease reporting.
    This feature normally can handle multi currency (different currency
    from SO, INV, and analytic account).

--- addons/sale_timesheet/models/__init__.py
+++ addons/sale_timesheet/models/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import account
+from . import account_invoice
 from . import hr
 from . import procurement
 from . import product
