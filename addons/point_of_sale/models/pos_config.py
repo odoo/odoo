3ed1b263a939ace7f25a6b422be057d31ecc1f6a
AZER <ezer1989@gmail.com>
2017-03-26 08:32:34 +0100

    Add sudo to journal in default_get for pos.config
    journal is not accessible for user from different company, 
    a security error is raised

--- addons/point_of_sale/models/pos_config.py
+++ addons/point_of_sale/models/pos_config.py
@@ -31,7 +31,7 @@ class PosConfig(models.Model):
 
     def _default_sale_journal(self):
         journal = self.env.ref('point_of_sale.pos_sale_journal', raise_if_not_found=False)
-        if journal and journal.company_id == self.env.user.company_id:
+        if journal and journal.sudo().company_id == self.env.user.company_id:
             return journal
         return self._default_invoice_journal()
 
