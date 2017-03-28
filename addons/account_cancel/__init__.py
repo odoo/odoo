60b812d9e236b46c5e0897012d58088b2cff86d4
Lucas Perais (lpe) <lpe@odoo.com>
2017-03-28 15:07:55 +0200

    Adapt account_cancel

--- addons/account_cancel/__init__.py
+++ addons/account_cancel/__init__.py
@@ -4,12 +4,13 @@
 import models
 from odoo import api, SUPERUSER_ID
 
-
+# FORWARD PORT NOTICE
+# In master as of March 2017, RCO-ODOO coded an exclusive field on modules to flag incompatibility
 def _check_incompatibility(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     wanted_states = ['installed', 'to upgrade', 'to install']
-
     l10n_fr_certification = env['ir.module.module'].search([('name', '=', 'l10n_fr_certification')])
+
     if l10n_fr_certification and l10n_fr_certification.state in wanted_states:
-        from odoo.addons.l10n_fr_certification import _deactivate_account_cancel_views
-        _deactivate_account_cancel_views(cr, registry)
+        from odoo.addons.l10n_fr_certification import _setup_inalterability
+        _setup_inalterability(cr, registry)
