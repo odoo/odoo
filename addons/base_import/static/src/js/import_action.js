03fabb0c4aefa57ee3a584f45b0af1d64ae01f48
Géry Debongnie <ged@odoo.com>
2017-03-31 15:27:08 +0200

    [REF] base_import: split import button code out of action

--- addons/base_import/static/src/js/import_action.js
+++ addons/base_import/static/src/js/import_action.js
@@ -3,10 +3,6 @@ odoo.define('base_import.import', function (require) {
 
 var ControlPanelMixin = require('web.ControlPanelMixin');
 var core = require('web.core');
-var KanbanController = require('web.KanbanController');
-var KanbanView = require('web.KanbanView');
-var ListController = require('web.ListController');
-var ListView = require('web.ListView');
 var session = require('web.session');
 var time = require('web.time');
 var Widget = require('web.Widget');
@@ -47,109 +43,6 @@ function jsonp(form, attributes, callback) {
     });
     $(form).ajaxSubmit(attributes);
 }
-
-// Mixins that enable the 'Import' feature
-var ImportViewMixin = {
-    init: function (viewInfo, params) {
-        var importEnabled = 'import_enabled' in params ? params.import_enabled : true;
-        // if true, the 'Import' button will be visible
-        this.controllerParams.importEnabled = importEnabled;
-    },
-};
-var ImportControllerMixin = {
-    init: function (parent, model, renderer, params) {
-        this.importEnabled = params.importEnabled;
-    },
-
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    /**
-     * Adds an event listener on the import button.
-     *
-     * @private
-     */
-    _bindImport: function () {
-        if (!this.$buttons) {
-            return;
-        }
-        var self = this;
-        this.$buttons.on('click', '.o_button_import', function () {
-            var state = self.model.get(self.handle, {raw: true});
-            self.do_action({
-                type: 'ir.actions.client',
-                tag: 'import',
-                params: {
-                    model: self.modelName,
-                    context: state.getContext(),
-                }
-            }, {
-                on_reverse_breadcrumb: self.reload.bind(self),
-            });
-        });
-    }
-};
-
-// Activate 'Import' feature on List views
-ListView.include({
-    init: function () {
-        this._super.apply(this, arguments);
-        ImportViewMixin.init.apply(this, arguments);
-    },
-});
-
-ListController.include({
-    init: function () {
-        this._super.apply(this, arguments);
-        ImportControllerMixin.init.apply(this, arguments);
-    },
-
-    //--------------------------------------------------------------------------
-    // Public
-    //--------------------------------------------------------------------------
-
-    /**
-     * Extends the renderButtons function of ListView by adding an event listener
-     * on the import button.
-     *
-     * @override
-     */
-    renderButtons: function () {
-        this._super.apply(this, arguments); // Sets this.$buttons
-        ImportControllerMixin._bindImport.call(this);
-    }
-});
-
-// Activate 'Import' feature on Kanban views
-KanbanView.include({
-    init: function () {
-        this._super.apply(this, arguments);
-        ImportViewMixin.init.apply(this, arguments);
-    },
-});
-
-KanbanController.include({
-    init: function () {
-        this._super.apply(this, arguments);
-        ImportControllerMixin.init.apply(this, arguments);
-    },
-
-    //--------------------------------------------------------------------------
-    // Public
-    //--------------------------------------------------------------------------
-
-    /**
-     * Extends the renderButtons function of ListView by adding an event listener
-     * on the import button.
-     *
-     * @override
-     */
-    renderButtons: function () {
-        this._super.apply(this, arguments); // Sets this.$buttons
-        ImportControllerMixin._bindImport.call(this);
-    }
-});
 
 var DataImport = Widget.extend(ControlPanelMixin, {
     template: 'ImportView',
