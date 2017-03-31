9ab421d99aaa8c683811f7e69d42965b3a398c5c
dbh <dbh@odoo.com>
2017-03-31 15:25:29 +0530

    [ADD] sale_management: Separate sales menu items from sale app
    Purpose
    =======
    
    Don't display sales app icon in Odoo homepage when installing ecommerce (only website should be visible)
    it's confusing to have sales icon  -> where do I manage my sales? In sales or in website? -> in website
    
    Specfication
    ============
    
    We need one new modules to add Sales menus to the homepage. Currently they come with "sale". So when installing "website_sale", we get them (because sale and account are dependencies of website_sale). We don't want that anymore. Along with that you need to replace modules in Apps menu:
    sales menu items must be in the new sale_management module, no longer in sale
    
    - create new app "sale_management"
    - Currently 3 app icons when installing ecommerce (Sales, Invoicing, Website) -> too many icons, where to start? -> GOAL: Only 2 icons > Website, Invoicing
    - new module to display Sales Icon (like account_accountant): "sale_management"
    - move menu items to sale_management
    - sale, sale_management: move app description, rewrite the manifest file
    - sale, sale_management: move tour from sale > sale_management
    - Changes in some dependency module "sale" > "sale_management" modules: event_sale, mrp_repair, pos_sale, report_intrastat, sale_crm, sale_expense, sale_stock, sale_timesheet, website_quote
    
    Menu items to move 'sale' --> 'sale_management'
    ===============================================
    
    Sales
    	Quotations
            Sales Orders
    Invoicing
    	Orders to invoice
            Orders to Upsell
    Catalog
    	Products
            Product Variants
            Pricelists
    Reporting
    	sales
    Configuration
    	Settings
            Products -Attributes, Attribute Values, Internal Categories

--- addons/sale_margin/__manifest__.py
+++ addons/sale_margin/__manifest__.py
@@ -12,7 +12,7 @@ This module adds the 'Margin' on sales order.
 This gives the profitability by calculating the difference between the Unit
 Price and Cost Price.
     """,
-    'depends':['sale'],
+    'depends': ['sale_management'],
     'demo':['data/sale_margin_demo.xml'],
     'data':['security/ir.model.access.csv','views/sale_margin_view.xml'],
     'uninstall_hook': "uninstall_hook",
