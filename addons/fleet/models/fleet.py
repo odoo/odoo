61a0f0afed4f3644bee5a1e216d24f5691079953
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2017-04-03 17:02:03 +0200

    [10.0][FIX] fleet: Fix error, access to self instead of iteration record

--- addons/fleet/models/fleet.py
+++ addons/fleet/models/fleet.py
@@ -213,11 +213,11 @@ class FleetVehicle(models.Model):
         LogContract = self.env['fleet.vehicle.log.contract']
         Cost = self.env['fleet.vehicle.cost']
         for record in self:
-            record.odometer_count = Odometer.search_count([('vehicle_id', '=', self.id)])
-            record.fuel_logs_count = LogFuel.search_count([('vehicle_id', '=', self.id)])
-            record.service_count = LogService.search_count([('vehicle_id', '=', self.id)])
-            record.contract_count = LogContract.search_count([('vehicle_id', '=', self.id)])
-            record.cost_count = Cost.search_count([('vehicle_id', '=', self.id), ('parent_id', '=', False)])
+            record.odometer_count = Odometer.search_count([('vehicle_id', '=', record.id)])
+            record.fuel_logs_count = LogFuel.search_count([('vehicle_id', '=', record.id)])
+            record.service_count = LogService.search_count([('vehicle_id', '=', record.id)])
+            record.contract_count = LogContract.search_count([('vehicle_id', '=', record.id)])
+            record.cost_count = Cost.search_count([('vehicle_id', '=', record.id), ('parent_id', '=', False)])
 
     @api.depends('log_contracts')
     def _compute_contract_reminder(self):
