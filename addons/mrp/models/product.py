6682364b69adcfb226c7d77d8815c570f825b94a
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2017-04-04 16:17:28 +0200

    [FIX] mrp: BoM count detection in case it is a single variant

--- addons/mrp/models/product.py
+++ addons/mrp/models/product.py
@@ -44,7 +44,12 @@ class ProductProduct(models.Model):
         read_group_res = self.env['mrp.bom'].read_group([('product_id', 'in', self.ids)], ['product_id'], ['product_id'])
         mapped_data = dict([(data['product_id'][0], data['product_id_count']) for data in read_group_res])
         for product in self:
-            product.bom_count = mapped_data.get(product.id, 0)
+            if product.product_tmpl_id.product_variant_count == 1:
+                bom_count = mapped_data.get(product.id, product.product_tmpl_id.bom_count)
+            else:
+                bom_count = mapped_data.get(product.id, 0)
+            product.bom_count = bom_count
+
 
     def _compute_mo_count(self):
         read_group_res = self.env['mrp.production'].read_group([('product_id', 'in', self.ids)], ['product_id'], ['product_id'])
