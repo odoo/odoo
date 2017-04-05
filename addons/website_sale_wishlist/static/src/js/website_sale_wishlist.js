ff8f50d3b355097a28f6d8fb14c11c3f244c2471
Jeremy Kersten <jke@odoo.com>
2017-04-05 14:41:56 +0200

    [IMP] website_sale_wishlist: improve wishlist after first users test
    - Update Cart Quantity when 'add to cart'
    - Change usability for b2b/b2c add/move to cart
    - Disable 'add to wishlist' if already in wish list
    - Wait the commit of 'Move to cart' of the last item
      before the redirect to avoid to get a fake empty cart.
    - Reset red notification after a small delay
    - Fix 'list of variants' optional feature (broken by commit 045a859)
    - Adjust animation to match cart/wishlist
    - Fix button cart / wishlist / comparison order
    - ...
    
    Courtesy of @adr-odoo for testing

--- addons/website_sale_wishlist/static/src/js/website_sale_wishlist.js
+++ addons/website_sale_wishlist/static/src/js/website_sale_wishlist.js
@@ -32,27 +32,40 @@ var ProductWishlist = Widget.extend({
         });
 
         if ($('.wishlist-section').length) {
-            $('.wishlist-section a.o_wish_rm').on('click', function (e){ self.wishlist_rm(e); });
-            $('.wishlist-section a.o_wish_add').on('click', function (e){ self.wishlist_add(e); });
-            $('.wishlist-section a.o_wish_mv').on('click', function (e){ self.wishlist_mv(e); });
+            $('.wishlist-section a.o_wish_rm').on('click', function (e){ self.wishlist_rm(e, false); });
+            $('.wishlist-section a.o_wish_add').on('click', function (e){ self.wishlist_add_or_mv(e); });
         }
+
+        $('.oe_website_sale').on('change', 'input.js_variant_change, select.js_variant_change, ul[data-attribute_value_ids]', function(ev) {
+            var $ul = $(ev.target).closest('.js_add_cart_variants');
+            var $parent = $ul.closest('.js_product');
+            var $product_id = $parent.find('.product_id').first();
+            var $el = $parent.find("[data-action='o_wishlist']");
+            if (!_.contains(self.wishlist_product_ids, parseInt($product_id.val(), 10))) {
+                $el.prop("disabled", false).removeClass('disabled').removeAttr('disabled');
+            }
+            else {
+                $el.prop("disabled", true).addClass('disabled').attr('disabled', 'disabled');
+            }
+        });
     },
     add_new_products:function($el, e){
         var self = this;
-        var product_id = $el.data('product-product-id');
-        if (e.currentTarget.classList.contains('o_add_wishlist_dyn')) {
-            product_id = parseInt($el.parent().find('.product_id').val());
-        }
         if (odoo.session_info.is_website_user){
             this.warning_not_logged();
         } else {
+            var product_id = $el.data('product-product-id');
+            if (e.currentTarget.classList.contains('o_add_wishlist_dyn')) {
+                product_id = parseInt($el.parent().find('.product_id').val());
+            }
             if (!_.contains(self.wishlist_product_ids, product_id)) {
                 return ajax.jsonRpc('/shop/wishlist/add', 'call', {
                     'product_id': product_id
                 }).then(function () {
                     self.wishlist_product_ids.push(product_id);
                     self.update_wishlist_view();
-                    website_sale_utils.animate_clone($('#my_wish'), $el.closest('form'), 10, 10);
+                    website_sale_utils.animate_clone($('#my_wish'), $el.closest('form'), 25, 40);
+                    $el.prop("disabled", true).addClass('disabled');
                 });
             }
         }
@@ -78,21 +91,29 @@ var ProductWishlist = Widget.extend({
             $('#my_wish').hide();
         }
     },
-    wishlist_rm: function(e){
+    wishlist_rm: function(e, deferred_redirect){
         var tr = $(e.currentTarget).parents('tr');
         var wish = tr.data('wish-id');
         var product = tr.data('product-id');
+        var self = this;
 
         new Model('product.wishlist')
             .call('write', [[wish], { active: false }, base.get_context()])
             .then(function(){
                 $(tr).hide();
             });
+
         this.wishlist_product_ids = _.without(this.wishlist_product_ids, product);
         if (this.wishlist_product_ids.length === 0) {
-            this.redirect_no_wish();
+            deferred_redirect = deferred_redirect ? deferred_redirect : $.Deferred();
+            deferred_redirect.then(function() {
+                self.redirect_no_wish();
+            });
         }
         this.update_wishlist_view();
+    },
+    wishlist_add_or_mv: function(e){
+        return $('#b2b_wish').is(':checked') ? this.wishlist_add(e) : this.wishlist_mv(e);
     },
     wishlist_add: function(e){
         var tr = $(e.currentTarget).parents('tr');
@@ -100,7 +121,7 @@ var ProductWishlist = Widget.extend({
 
         // can be hidden if empty
         $('#my_cart').removeClass('hidden');
-        website_sale_utils.animate_clone($('#my_cart'), tr, 0, 0);
+        website_sale_utils.animate_clone($('#my_cart'), tr, 25, 40);
         this.add_to_cart(product, tr.find('qty').val() || 1);
     },
     wishlist_mv: function(e){
@@ -108,15 +129,20 @@ var ProductWishlist = Widget.extend({
         var product = tr.data('product-id');
 
         $('#my_cart').removeClass('hidden');
-        website_sale_utils.animate_clone($('#my_cart'), tr, 0, 0);
-        this.add_to_cart(product, tr.find('qty').val() || 1);
-        this.wishlist_rm(e);
+        website_sale_utils.animate_clone($('#my_cart'), tr, 25, 40);
+        var adding_deffered = this.add_to_cart(product, tr.find('qty').val() || 1);
+        this.wishlist_rm(e, adding_deffered);
     },
     add_to_cart: function(product_id, qty_id) {
-        ajax.jsonRpc("/shop/cart/update_json", 'call', {
+        var add_to_cart = ajax.jsonRpc("/shop/cart/update_json", 'call', {
             'product_id': parseInt(product_id, 10),
             'add_qty': parseInt(qty_id, 10)
         });
+
+        add_to_cart.then(function(resp) {
+            $('.my_cart_quantity').text(resp.cart_quantity || '* 1');
+        });
+        return add_to_cart;
     },
     redirect_no_wish: function() {
         window.location = '/shop/cart';
