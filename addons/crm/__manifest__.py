4b5513cfd47c4692599a111db800d04ab5f6f62e
Thibault Delavallée <tde@odoo.com>
2017-04-06 13:46:43 +0200

    [REF] crm: use config parameter instead of ir values in settings
    Fields like `default_foo` still used ir.values although those are system-wide
    parameters and should therefore be stored in ir.config_parameter
    table.
    
    When no default_model is set on a field, don't prefix it by `default_`.
    
    Also, remove deprecated generation of alias on sales team.
    
    According to usability experts configuring a sales team alias should
    be done directly on the sales team view and not through an hidden
    and strangely configured settings.

--- addons/crm/__manifest__.py
+++ addons/crm/__manifest__.py
@@ -45,7 +45,6 @@ Dashboard for CRM will include:
 
         'data/crm_data.xml',
         'data/crm_stage_data.xml',
-        'data/sales_config_settings_data.xml',
         'data/crm_lead_data.xml',
         'data/web_planner_data.xml',
         'data/mail_template_data.xml',
