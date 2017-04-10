1f34c6703ca9e95954840d374e8463f0aaa20adf
Thibault Delavallée <tde@odoo.com>
2017-04-10 17:33:12 +0200

    [REM] crm: remove activity stmart button
    Because with new activities it does not make sense to have a button
    completely crm-related although activities are now cross apps.

--- addons/crm/models/res_partner.py
+++ addons/crm/models/res_partner.py
@@ -13,7 +13,6 @@ class Partner(models.Model):
     meeting_ids = fields.Many2many('calendar.event', 'calendar_event_res_partner_rel', 'res_partner_id', 'calendar_event_id', string='Meetings')
     opportunity_count = fields.Integer("Opportunity", compute='_compute_opportunity_count')
     meeting_count = fields.Integer("# Meetings", compute='_compute_meeting_count')
-    activities_count = fields.Integer("Activities", compute='_compute_activities_count')
 
     @api.multi
     def _compute_opportunity_count(self):
@@ -25,13 +24,6 @@ class Partner(models.Model):
     def _compute_meeting_count(self):
         for partner in self:
             partner.meeting_count = len(partner.meeting_ids)
-
-    @api.multi
-    def _compute_activities_count(self):
-        activity_data = self.env['crm.activity.report'].read_group([('partner_id', 'in', self.ids)], ['partner_id'], ['partner_id'])
-        mapped_data = {act['partner_id'][0]: act['partner_id_count'] for act in activity_data}
-        for partner in self:
-            partner.activities_count = mapped_data.get(partner.id, 0)
 
     @api.multi
     def schedule_meeting(self):
