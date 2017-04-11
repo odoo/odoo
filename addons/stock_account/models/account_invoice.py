c8a3e362a9a8d04b554a2889af66f9b2fde8b3fb
Olivier Colson <oco@odoo.com>
2017-04-11 17:06:21 +0200

    - correction of a small typo in function name

--- addons/stock_account/models/account_invoice.py
+++ addons/stock_account/models/account_invoice.py
@@ -115,7 +115,7 @@ class AccountInvoiceLine(models.Model):
 
     def get_invoice_line_account(self, type, product, fpos, company):
         if company.anglo_saxon_accounting and type in ('in_invoice', 'in_refund') and product and product.type == 'product':
-            accounts = product.product_tmpl_id._get_product_accounts(fiscal_pos=fpos)
+            accounts = product.product_tmpl_id.get_product_accounts(fiscal_pos=fpos)
             if accounts['stock_input']:
                 return accounts['stock_input']
         return super(AccountInvoiceLine, self).get_invoice_line_account(type, product, fpos, company)
