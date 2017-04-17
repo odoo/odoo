4c93eedfc6ebf2623f64183251f0747692749b94
Haresh Shyara <hsh@odoo.com>
2017-04-17 16:24:55 +0530

    [IMP] account, sale, l10n_ch, purchase: replace mail composer inheritance by message_post override
    Currently composer is inherited in those four modules to add a specific
    behavior when sending the invoice / quotation by email. Purpose is to
    mark it as sent, using a magic context key.
    
    This commit keeps the feature but changes the implementation. It is now
    done by overriding message_post which should be the entry point for all
    message-related features.

--- addons/l10n_ch/models/__init__.py
+++ addons/l10n_ch/models/__init__.py
@@ -3,7 +3,6 @@
 
 from . import res_config_settings
 from . import account_invoice
-from . import mail_compose_message
 from . import mail_template
 from . import res_bank
 from . import res_company
