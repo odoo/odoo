4c2a8db2134cb3cbd035307896fdc4f6e71d2a25
Christophe Matthieu <chm@odoo.com>
2017-04-18 11:42:28 +0200

    [FIX] web: prevent crash when the user doesn't have timezone

--- addons/web/models/ir_http.py
+++ addons/web/models/ir_http.py
@@ -26,7 +26,7 @@ class Http(models.AbstractModel):
         version_info = odoo.service.common.exp_version()
         return {
             "session_id": request.session.sid,
-            "tzOffset": datetime.datetime.now(pytz.timezone(self._context.get('tz', 'UTC'))).utcoffset().total_seconds() / 60,
+            "tzOffset": self._context.get('tz') and datetime.datetime.now(pytz.timezone(self._context['tz'])).utcoffset().total_seconds() / 60 or False,
             "uid": request.session.uid,
             "is_system": request.env.user._is_system(),
             "is_superuser": request.env.user._is_superuser(),
