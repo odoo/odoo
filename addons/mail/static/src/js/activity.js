5b41eb5c5341d1b27308665e53dcb6130f8bd84d
Mohammed Shekha <msh@openerp.com>
2017-04-25 18:28:25 +0530

    [IMP]mail: Added no_tabindex attribute on mail chatter, follower, activity widgets so that focus never comes through tab

--- addons/mail/static/src/js/activity.js
+++ addons/mail/static/src/js/activity.js
@@ -110,6 +110,7 @@ var AbstractActivityField = AbstractField.extend({
 // -----------------------------------------------------------------------------
 var Activity = AbstractActivityField.extend({
     className: 'o_mail_activity',
+    no_tabindex: true,
     events: {
         'click .o_activity_edit': '_onEditActivity',
         'click .o_activity_unlink': '_onUnlinkActivity',
--- addons/mail/static/src/js/chatter.js
+++ addons/mail/static/src/js/chatter.js
@@ -24,6 +24,7 @@ var QWeb = core.qweb;
 // the flickering when switching between records
 var Chatter = Widget.extend(chat_mixin, {
     template: 'mail.Chatter',
+    no_tabindex: true,
     custom_events: {
         reload_mail_fields: '_onReloadMailFields',
     },
