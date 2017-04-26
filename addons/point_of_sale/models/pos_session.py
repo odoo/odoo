3b2ad20d243fb62f7d5e0b03f8ec1b0dd7189c8e
Martin Trigaux <mat@odoo.com>
2017-04-26 12:03:09 +0200

    [FIX] point_of_sale: remove useless code
    Introduced at 36e7327f as a patch for stable and 1d2ca932 fixed in master.
    Maybe the capslock was not big enough, will use <blink/> next time.

--- addons/point_of_sale/models/pos_session.py
+++ addons/point_of_sale/models/pos_session.py
@@ -236,10 +236,6 @@ class PosSession(models.Model):
     @api.multi
     def action_pos_session_closing_control(self):
         for session in self:
-            #DO NOT FORWARD-PORT
-            if session.state == 'closing_control':
-                session.action_pos_session_close()
-                continue
             for statement in session.statement_ids:
                 if (statement != session.cash_register_id) and (statement.balance_end != statement.balance_end_real):
                     statement.write({'balance_end_real': statement.balance_end})
