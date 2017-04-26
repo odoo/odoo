a567b50035f73575c658ad6f87070713e12b4b74
Christophe Simonis <chs@odoo.com>
2017-04-26 17:55:16 +0200

    [IMP] test_pylint: check for badly written try/except
    Cherry-pick of 0ebe59f5d013c8d082a25cda243219a76508c10e

--- odoo/addons/test_pylint/tests/test_pylint.py
+++ odoo/addons/test_pylint/tests/test_pylint.py
@@ -8,12 +8,14 @@ except ImportError:
     pylint = None
 import subprocess
 from distutils.version import LooseVersion
+import os
 from os.path import join
 
 from odoo.tests.common import TransactionCase
 from odoo import tools
 from odoo.modules import get_modules, get_module_path
 
+HERE = os.path.dirname(os.path.realpath(__file__))
 
 _logger = logging.getLogger(__name__)
 
@@ -54,6 +56,8 @@ class TestPyLint(TransactionCase):
         'old-raise-syntax',
         'raising-string',
         'unpacking-in-except',
+
+        'no-comma-exception',
     ]
 
     BAD_FUNCTIONS = [
@@ -122,13 +126,21 @@ class TestPyLint(TransactionCase):
             '--enable=%s' % ','.join(self.ENABLED_CODES),
             '--reports=n',
             "--msg-template='{msg} ({msg_id}) at {path}:{line}'",
-            '--load-plugins=pylint.extensions.bad_builtin',
+            '--load-plugins=pylint.extensions.bad_builtin,_odoo_checkers',
             '--bad-functions=%s' % ','.join(self.BAD_FUNCTIONS),
             '--deprecated-modules=%s' % ','.join(self.BAD_MODULES)
         ]
 
+        pypath = HERE + os.pathsep + os.environ.get('PYTHONPATH', '')
+        env = dict(os.environ, PYTHONPATH=pypath)
         try:
-            process = subprocess.Popen(['pylint'] + options + paths, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            pylint_bin = tools.which('pylint')
+            process = subprocess.Popen(
+                [pylint_bin] + options + paths,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                env=env,
+            )
         except (OSError, IOError):
             self._skip_test('pylint executable not found in the path')
         else:
