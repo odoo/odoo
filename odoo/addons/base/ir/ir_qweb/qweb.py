effb462daba59bc9be1139039a71b094f0cca0e7
Christophe Matthieu <chm@odoo.com>
2017-04-26 16:28:36 +0200

    [IMP] web: extend qweb to allow dynamic templates in t-call
    Before this change the t-call can use only receive a literal as parameters:
    <t t-call="template.name17" />
    This call render the template 'template.name17' with the current context.
    
    Now, we can defined an expression with braces (same syntax that t-attf):
    <t t-call="template.name{{ 10 + 7 }}" />
    This call render the template 'template.name17' with the current context.
    
    This change allows for greater flexibility for applications using qweb.
    (The performance due to this change is unchanged during rendering time.)

--- odoo/addons/base/ir/ir_qweb/qweb.py
+++ odoo/addons/base/ir/ir_qweb/qweb.py
@@ -1475,7 +1475,7 @@ class QWeb(object):
                         ctx=ast.Load()
                     ),
                     args=[
-                        ast.Str(str(tmpl)),
+                        self._compile_format(str(tmpl)),
                         ast.Name(id=name_options, ctx=ast.Load()),
                     ],
                     keywords=[], starargs=None, kwargs=None
