93b751f58cc66892abbd1c308102474c55b66b33
Simon Lejeune <sle@openerp.com>
2017-04-26 13:50:44 +0200

    [REF] stock: adapt exising tests to new implementation

--- addons/stock/tests/common2.py
+++ addons/stock/tests/common2.py
@@ -85,11 +85,3 @@ class TestStockCommon(common.TestProductCommon):
         # Existing data
         cls.existing_inventories = cls.env['stock.inventory'].search([])
         cls.existing_quants = cls.env['stock.quant'].search([])
-
-        # Inventory
-        inventory_wizard = cls.env['stock.change.product.qty'].create({
-            'product_id': cls.product_1.id,
-            'new_quantity': 50.0,
-            'location_id': cls.warehouse_1.lot_stock_id.id,
-        })
-        inventory_wizard.change_product_qty()
