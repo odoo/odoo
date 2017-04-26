c7aeee9fbe38cf7d732386b5743385ff5672e137
Yannick Tivisse <yti@odoo.com>
2017-04-26 11:44:47 +0200

    [ADD] l10n_be_hr_payroll_fleet: Compute car related cost and advantages in belgian payroll
    - Compute the taxable ATN due to the company car (Depends on the acquisition date, car value, fuel type and CO2 emissions)
    - Compute the Total Depreciated Cost on the employer costs due to the depreciated recurring amount declared with the car purchase
    - Allow to retrieve these 2 different values on a vehicle.model in the case we want to have a default value for a new car
    - Modify name_get on a vehicle.model and the name compute method on a fleet.vehicle to display some useful information like the employer cost (by month) and the acquisition date
    - A new car may be purchase for an employee when the amount on unused car in the fleet exceeds X. Add a res_config view to configure that behavior
    - Add the company car mechanism (choose a car, select a new car from available models) on a hr.contract.

---
+++ addons/l10n_be_hr_payroll_fleet/models/hr_contract.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class HrContract(models.Model):
+    _inherit = 'hr.contract'
+
+    car_id = fields.Many2one('fleet.vehicle', string='Car',
+        domain=lambda self: self._get_available_cars_domain(),
+        default=lambda self: self.env['fleet.vehicle'].search([('driver_id', '=', self.employee_id.address_home_id.id)], limit=1))
+    car_atn = fields.Float(compute='_compute_car_atn_and_costs', string='ATN Company Car')
+    company_car_total_depreciated_cost = fields.Float(compute='_compute_car_atn_and_costs')
+    available_cars_amount = fields.Integer(compute='_compute_available_cars_amount', string='Number of available cars')
+    new_car = fields.Boolean('Request a new car')
+    new_car_model_id = fields.Many2one('fleet.vehicle.model', string="Model", domain=lambda self: self._get_possible_model_domain())
+    max_unused_cars = fields.Integer(compute='_compute_max_unused_cars')
+
+    @api.depends('car_id', 'new_car', 'new_car_model_id')
+    def _compute_car_atn_and_costs(self):
+        for contract in self:
+            if contract.car_id:
+                contract.car_atn = contract.car_id.atn
+                contract.company_car_total_depreciated_cost = contract.car_id.total_depreciated_cost
+            elif contract.new_car and contract.new_car_model_id:
+                contract.car_atn = contract.new_car_model_id.default_atn
+                contract.company_car_total_depreciated_cost = contract.new_car_model_id.default_total_depreciated_cost
+
+    @api.depends('name')
+    def _compute_available_cars_amount(self):
+        for contract in self:
+            contract.available_cars_amount = self.env['fleet.vehicle'].search_count([('driver_id', '=', False)])
+
+    @api.depends('name')
+    def _compute_max_unused_cars(self):
+        params = self.env['ir.config_parameter'].sudo()
+        max_unused_cars = params.get_param('l10n_be_hr_payroll_fleet.max_unused_cars', default=3)
+        for contract in self:
+            contract.max_unused_cars = int(max_unused_cars)
+
+    @api.onchange('employee_id')
+    def _onchange_employee_id(self):
+        super(HrContract, self)._onchange_employee_id()
+        self.car_id = self.env['fleet.vehicle'].search([('driver_id', '=', self.employee_id.address_home_id.id)], limit=1)
+        return {'domain': {'car_id': self._get_available_cars_domain()}}
+
+    def _get_available_cars_domain(self):
+        return ['|', ('driver_id', '=', False), ('driver_id', '=', self.employee_id.address_home_id.id)]
+
+    def _get_possible_model_domain(self):
+        return [('can_be_requested', '=', True)]
