5f9cfb2994de1d17f476c5fe504d300dab7de4fa
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2017-04-26 17:00:46 +0200

    [FIX] point_of_sale: backport of df3b04e + fix
    When a PoS order is closed, the payment and the sales journal item are
    not automatically reconciled. This potentially leaves a lot of
    unreconciled entries for a customer, which will appear in his follow-up
    report.
    
    This introduces an auto-reconciliation process upon order closing. The
    process restricts the reconciliation to an order individually, i.e. we
    don't try to cross-reconcile several orders for a customer.
    
    opw-697565
    
    Add missing sudo to allow a user without accounting rights to close a session
    and reconcile entries.
    
    Closes #16517

--- addons/point_of_sale/models/pos_session.py
+++ addons/point_of_sale/models/pos_session.py
@@ -27,6 +27,8 @@ class PosSession(models.Model):
                 if order.state not in ('paid'):
                     raise UserError(_("You cannot confirm all orders of this session, because they have not the 'paid' status"))
                 order.action_pos_order_done()
+            orders = session.order_ids.filtered(lambda order: order.state in ['invoiced', 'done'])
+            orders.sudo()._reconcile_payments()
 
     config_id = fields.Many2one(
         'pos.config', string='Point of Sale',
