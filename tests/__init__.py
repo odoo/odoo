59d1cacbcab51a0691134f16c3e0f05999633c56
xmo-odoo <xmo@odoo.com>
2017-04-28 09:07:28 +0200

    [FIX] implicitly relative imports

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import test_program_without_code_operations
-import test_program_with_code_operations
-import test_program_rules
-import test_program_rewards
+from . import test_program_without_code_operations
+from . import test_program_with_code_operations
+from . import test_program_rules
+from . import test_program_rewards
