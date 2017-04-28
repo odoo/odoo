c3ad67cef218368c45cdb29caf8a10d462d8cd68
xmo-odoo <xmo@odoo.com>
2017-04-28 09:07:28 +0200

    [FIX] implicitly relative imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import sale_coupon
-import sale_coupon_reward
-import sale_coupon_rules
-import sale_coupon_program
-import sale_order
+from . import sale_coupon
+from . import sale_coupon_reward
+from . import sale_coupon_rules
+from . import sale_coupon_program
+from . import sale_order
