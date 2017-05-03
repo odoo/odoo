a440403f4e9933a3a3f3fdf1ba8ca31ff54cabcf
Katherine Zaoral <kathy@vauxoo.com>
2017-05-03 21:31:20 +0000

    [FIX] Replace default_new_po_lead field with default_po_lead
    The field new_po_lead does not exist, the field po_lead does
    exists.

--- addons/purchase/models/stock_config_settings.py
+++ addons/purchase/models/stock_config_settings.py
@@ -8,4 +8,4 @@ class StockConfigSettings(models.TransientModel):
     _inherit = 'stock.config.settings'
 
     po_lead = fields.Float(related='company_id.po_lead', default=lambda self: self.env.user.company_id.po_lead)
-    default_new_po_lead = fields.Boolean(string="Security Lead Time for Purchase", default_model="stock.config.settings", help="Margin of error for vendor lead times. When the system generates Purchase Orders for reordering products,they will be scheduled that many days earlier to cope with unexpected vendor delays.")
+    default_po_lead = fields.Boolean(string="Security Lead Time for Purchase", default_model="stock.config.settings", help="Margin of error for vendor lead times. When the system generates Purchase Orders for reordering products,they will be scheduled that many days earlier to cope with unexpected vendor delays.")
