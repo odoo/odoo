30f8156f529f3a5b8fb0270bd189a1e9968b1a2a
amoyaux <arm@odoo.com>
2017-05-04 15:51:51 +0200

    [REF] mrp: ir.attachment handling
    Rev[0] and [1] introduced a versioning on product's attachments through an
    ECO creation or through a stat button on the product form view. These revisions
    added some fields on the ir attachment model: a many2one and a boolean field
    with a default value, and due to concerns over the migration of databases with a
    lot of ir.attachment records (like ours), we adapted the implentation in rev[2][3].
    
    We deemed reasonnable to remove the "active" field, as adding an active field not
    in the “base” module of the model could impact other modules that did not took into
    consideration this field. Also, this field only had a meaning when mrp_plm is
    installed, and ir.attachment is used in various places in contexts across Odoo.
    
    So, rev [2] and [3] changed the implementation of the versioning to only link
    ir.attachment to an ECO record, forgetting the functionality of archiving directly
    through the product form view stat button (without an ECO).
    
    To introduce back this behavior, we chose to create a model to handle the mrp
    attachments (named “mrp.document”) which inheritS ir.attachment. This way, we keep
    the behavior of ir.attachment and we do not alter the original table.
    
    This commit moved the priority field already set on ir.attachment to mrp.document
    and adds the active field that will be used in mrp_plm to archive/versioning purposes.
    
    linked commit:
    [0] https://github.com/odoo/enterprise/commit/99138b6711760a7562cb9559663aef6a4208a48f
    [1] https://github.com/odoo/enterprise/commit/4f88eb409776c1c4bc8f8d71d845b913c3604a54
    
    [2] https://github.com/odoo/enterprise/commit/7e1c73cfc84cd23b6ba87dab713c3078de16568d
    [3] https://github.com/odoo/enterprise/commit/ee1c4b29b8872dac9c525fcc6acdf80b5ef73131

--- addons/mrp/models/__init__.py
+++ addons/mrp/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import ir_attachment
+from . import mrp_document
 from . import mrp_config_settings
 from . import mrp_bom
 from . import mrp_message
