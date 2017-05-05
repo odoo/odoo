f156a5d60972bb5b17d0624a84b0e154cecb42dc
Olivier Colson <oco@odoo.com>
2017-05-05 15:00:26 +0200

    [FIX] account_accountant : dashboard compatible with new views
    Re-writing of account_accountant's dashboard in the new views API.

--- addons/account_accountant/static/src/js/account_dashboard.js
+++ addons/account_accountant/static/src/js/account_dashboard.js
@@ -2,48 +2,69 @@ odoo.define('account_accountant.dashboard', function (require) {
 "use strict";
 
 var core = require('web.core');
-var formats = require('web.formats');
-var Model = require('web.Model');
+var field_utils = require('web.field_utils');
+var KanbanView = require('web.KanbanView');
+var KanbanModel = require('web.KanbanModel');
+var KanbanRenderer = require('web.KanbanRenderer');
+var KanbanController = require('web.KanbanController');
 var session = require('web.session');
-var KanbanView = require('web_kanban.KanbanView');
-var data = require('web.data');
-var web_client = require('web.web_client');
+var view_registry = require('web.view_registry');
 
 var QWeb = core.qweb;
-
 var _t = core._t;
 var _lt = core._lt;
 
 const COMPANY_METHOD_TYPE = "company_object";
 
-var AccountDashboardView = KanbanView.extend({
-    display_name: _lt('Dashboard'),
-    icon: 'fa-dashboard',
-    searchview_hidden: true,
-    events: {
-        'click .account_accountant_dasboard_action': 'on_dashboard_action_clicked',
-    },
+var AccountDashboardRenderer = KanbanRenderer.extend({
+    events: _.extend({}, KanbanRenderer.prototype.events, {
+        'click .account_accountant_dashboard_action': 'onDashboardActionClicked',
+    }),
 
-    fetch_data: function() {
-        return new Model('account.journal')
-            .call('retrieve_account_dashboard', []);
-    },
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
 
-    render: function() {
-        var super_render = this._super;
+    /**
+     * Notifies the controller that the target has changed.
+     *
+     * @private
+     * @param {string} target_name the name of the changed target
+     * @param {string} value the new value
+     */
+    _notifyTargetChange: function (target_name, value) {
+        this.trigger_up('dashboard_edit_target', {
+            target_name: target_name,
+            target_value: value,
+        });
+    },
+  /**
+     * @override
+     * @private
+     * @returns {Deferred}
+     */
+    _render: function () {
         var self = this;
 
-        return this.fetch_data().then(function(result){
+        return this._super.apply(this, arguments).then(function () {
+            var values = self.state.dashboardValues;
             var account_dashboard = QWeb.render('account_accountant.AccountDashboard', {
                 widget: self,
-                values: result,
+                values: values,
             });
-            super_render.call(self);
-            $(account_dashboard).prependTo(self.$el);
+            self.$el.prepend(account_dashboard);
         });
     },
 
-    on_dashboard_action_clicked: function(ev) {
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {MouseEvent}
+     */
+    onDashboardActionClicked: function(e) {
         /* This functions allows the buttons of the setup bar to trigger Python
         * code defined in api.model functions, in res.company, and then to execute
         * the action returned by those.
@@ -51,30 +72,124 @@ var AccountDashboardView = KanbanView.extend({
         * run Python function 'name' of company, otherwise, it will directly execute
         * the action matching 'name'.
         */
-        ev.preventDefault();
-        var $action = $(ev.currentTarget);
+        e.preventDefault();
+        var self = this;
+        var $action = $(e.currentTarget);
         var name_attr = $action.attr('name');
         var type_attr = $action.attr('type');
         var action_context = $action.data('context');
 
         if(type_attr == COMPANY_METHOD_TYPE) {
-            new Model('res.company').call(name_attr, []).then(function(rslt_action) {
-                    web_client.action_manager.do_action(rslt_action, {
-                        additional_context: action_context
+           this._rpc({
+                    model: 'res.company',
+                    method: name_attr,
+                    args: [],
+                })
+                .then(
+                    function(rslt_action) {
+                        self.do_action(rslt_action, {
+                            action_context: action_context,
+                        })
                     });
-            });
         }
         else { //By default, we consider the content of 'name' as an action.
-            this.do_action(name_attr, {
-                    additional_context: action_context
+            self.do_action(name_attr, {
+                action_context: action_context,
             });
         }
     },
-
 });
 
-core.view_registry.add('account_accountant_dashboard', AccountDashboardView);
+var AccountDashboardModel = KanbanModel.extend({
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
 
-return AccountDashboardView;
+    /**
+     * @œverride
+     * @returns {Deferred}
+     */
+    load: function () {
+        return this._loadDashboard(this._super.apply(this, arguments));
+    },
+    /**
+     * @œverride
+     * @returns {Deferred}
+     */
+    reload: function () {
+        return this._loadDashboard(this._super.apply(this, arguments));
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @abstract
+     * @returns {Deferred -> Object} resolves to the required dashboard data
+     */
+    _fetchDashboardData: function () {
+        return $.when(this._rpc({
+                    model: 'account.journal',
+                    method: 'retrieve_account_dashboard',
+                    args: [],
+                }));
+    },
+    /**
+     * @private
+     * @param {Deferred} super_def a deferred that resolves with a dataPoint id
+     * @returns {Deferred -> string} resolves to the dataPoint id
+     */
+    _loadDashboard: function (super_def) {
+        var self = this;
+        var dashboard_def = this._fetchDashboardData();
+        return $.when(super_def, dashboard_def).then(function (id, dashboardValues) {
+            var dataPoint = self.localData[id];
+            dataPoint.dashboardValues = dashboardValues;
+            return id;
+        });
+    },
+});
+
+var AccountDashboardController = KanbanController.extend({
+    custom_events: _.extend({}, KanbanController.prototype.custom_events, {
+        dashboard_open_action: '_onDashboardOpenAction',
+    }),
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {OdooEvent} e
+     */
+    _onDashboardOpenAction: function (e) {
+        var action_name = e.data.action_name;
+        var action_context = e.data.action_context;
+        return this.do_action(action_name, {
+            additional_context: action_context,
+        });
+    },
+});
+
+var AccountDashboardView = KanbanView.extend({
+    config: _.extend({}, KanbanView.prototype.config, {
+        Model: AccountDashboardModel,
+        Renderer: AccountDashboardRenderer,
+        Controller: AccountDashboardController,
+    }),
+    display_name: _lt('Dashboard'),
+    icon: 'fa-dashboard',
+    searchview_hidden: true,
+});
+
+view_registry.add('account_accountant_dashboard', AccountDashboardView);
+
+return {
+    Model: AccountDashboardModel,
+    Renderer: AccountDashboardRenderer,
+    Controller: AccountDashboardController,
+};
 
 });
