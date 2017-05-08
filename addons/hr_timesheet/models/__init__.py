b0a74c60683cb92f81f54e2140bf173684c501b1
Jérome Maes <jem@openerp.com>
2017-05-08 15:39:07 +0200

    [MOV] hr_timesheet: reorder file according to guidelines

--- addons/hr_timesheet/models/__init__.py
+++ addons/hr_timesheet/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import models
-from . import report
+from . import hr_timesheet
+from . import res_config
+from . import project
