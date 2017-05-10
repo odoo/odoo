c88f1bc363d88182d7876e238e17e84ea19870ef
Dhawal Limbuwala <dli@odoo.com>
2017-05-10 11:22:21 +0530

    [FIX] account: fixed traceback when clicking on the send by email button clicked

--- addons/account/models/account_invoice.py
+++ addons/account/models/account_invoice.py
@@ -428,7 +428,7 @@ class AccountInvoice(models.Model):
             default_template_id=template and template.id or False,
             default_composition_mode='comment',
             mark_invoice_as_sent=True,
-            custom_layout="account.mail_template_data_notification_email_account_invoice"
+            custom_layout='account.mail_template_data_notification_email_account_invoice'
         )
         return {
             'name': _('Compose Email'),
