390b06043cc1c549310f431efad82e960dbb7ff8
Jérome Maes <jem@openerp.com>
2017-05-10 09:23:09 +0200

    [IMP] sale_timesheet: add test for revenue computation

--- addons/sale_timesheet/tests/__init__.py
+++ addons/sale_timesheet/tests/__init__.py
@@ -1,2 +1,6 @@
-from . import test_sale_timesheet
-from . import test_sale_service
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+#from . import test_sale_timesheet
+#from . import test_sale_service
+from . import test_timesheet_revenue
