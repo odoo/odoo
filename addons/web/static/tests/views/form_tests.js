odoo/odoo

commit 8f9362809e91ff7c2876ab464017abb13a4e0974

[FIX] web: FormView: notebook: activate first visible tab
In a notebook, tabs may have an invisible attrs. The first visible
tab should be active in any case. This wasn't working at the first
rendering of the view, because the invisible attrs was evaluated
for each tab, before processing the next ones, so the it couldn't
find a next tab to activate.

This fix ensures to process all tabs (i.e. generate their DOM
element) before evaluating their invisible attrs, and trying to
find which tab to activate.

addons/web/static/src/js/views/form/form_renderer.js
@@ -335,8 +335,6 @@ var FormRenderer = BasicRenderer.extend({
             });
             this._registerModifiers(node, this.state, $label, {
                 callback: function (element, modifiers, record) {
-                    var recordWidgets = self.allFieldWidgets[record.id];
-                    var widget = _.findWhere(recordWidgets, {name: fieldName});
                     element.$el.toggleClass('o_form_label_empty', !!(
                         record.data.id
                         && (modifiers.readonly || self.mode === 'readonly')
@@ -623,7 +621,11 @@ var FormRenderer = BasicRenderer.extend({
         var self = this;
         var $headers = $('<ul class="nav nav-tabs">');
         var $pages = $('<div class="tab-content nav nav-tabs">');
-        _.each(node.children, function (child, index) {
+        // renderedTabs is used to aggregate the generated $headers and $pages
+        // alongside their node, so that their modifiers can be registered once
+        // all tabs have been rendered, to ensure that the first visible tab
+        // is correctly activated
+        var renderedTabs = _.map(node.children, function (child, index) {
             var pageID = _.uniqueId('notebook_page_');
             var $header = self._renderTabHeader(child, pageID);
             var $page = self._renderTabPage(child, pageID);
@@ -632,20 +634,28 @@ var FormRenderer = BasicRenderer.extend({
                 $page.addClass('active');
             }
             self._handleAttributes($header, child);
-            self._registerModifiers(child, self.state, $header, {
-                callback: function (element, modifiers, record) {
+            $headers.append($header);
+            $pages.append($page);
+            return {
+                $header: $header,
+                $page: $page,
+                node: child,
+            };
+        });
+        // register the modifiers for each tab
+        _.each(renderedTabs, function (tab) {
+            self._registerModifiers(tab.node, self.state, tab.$header, {
+                callback: function (element, modifiers) {
                     // if the active tab is invisible, activate the first visible tab instead
                     if (modifiers.invisible && element.$el.hasClass('active')) {
                         element.$el.removeClass('active');
-                        $page.removeClass('active');
+                        tab.$page.removeClass('active');
                         var $firstVisibleTab = $headers.find('li:not(.o_form_invisible):first()');
                         $firstVisibleTab.addClass('active');
                         $pages.find($firstVisibleTab.find('a').attr('href')).addClass('active');
                     }
                 },
             });
-            $headers.append($header);
-            $pages.append($page);
         });
         return $('<div class="o_notebook">')
                 .data('name', node.attrs.name || '_default_')

addons/web/static/tests/views/form_tests.js
@@ -340,6 +340,37 @@ QUnit.module('Views', {
         form.destroy();
     });
 
+    QUnit.test('first notebook page invisible', function (assert) {
+        assert.expect(2);
+
+        var form = createView({
+            View: FormView,
+            model: 'partner',
+            data: this.data,
+            arch: '<form string="Partners">' +
+                    '<sheet>' +
+                        '<field name="product_id"/>' +
+                        '<notebook>' +
+                            '<page string="Foo" invisible="1">' +
+                                '<field name="foo"/>' +
+                            '</page>' +
+                            '<page string="Bar">' +
+                                '<field name="bar"/>' +
+                            '</page>' +
+                        '</notebook>' +
+                    '</sheet>' +
+                '</form>',
+            res_id: 1,
+        });
+
+        assert.ok(form.$('.o_notebook .nav li:first()').hasClass('o_form_invisible'),
+            'first tab should be invisible');
+        assert.ok(form.$('.o_notebook .nav li:nth(1)').hasClass('active'),
+            'second tab should be active');
+
+        form.destroy();
+    });
+
     QUnit.test('invisible attrs on group are re-evaluated on field change', function (assert) {
         assert.expect(2);
 

commit 23beee818a3efc1de9a39812af4f4069cb924094

[FIX] web: do not lose field/label pair on field rerendering
When a field widget switches between readonly and editable states, it is
fully rerendered. Labels have sometimes to get a specific class when
their associated field is (become) readonly and without any value. This
condition was evaluated on the initial widget and not on rerendered
ones.

Note that this commit only restores an already implemented behavior
broken by https://github.com/odoo-dev/odoo/commit/8f9362809e91ff7c2876ab464017abb13a4e0974
... but this time a test will prevent a new breaking

addons/web/static/src/js/views/form/form_renderer.js
@@ -370,15 +370,19 @@ var FormRenderer = BasicRenderer.extend({
     _renderInnerGroupField: function (node) {
         var self = this;
         var widget = this._renderFieldWidget(node, this.state);
+        var fieldName = node.attrs.name;
         var $tds = $('<td/>').append(widget.$el);
 
         if (node.attrs.nolabel !== '1') {
             var $labelTd = this._renderInnerGroupLabel(node, {
                 callback: function (element, modifiers, record) {
+                    var widgets = self.allFieldWidgets[record.id];
                     element.$el.toggleClass('o_form_label_empty', !!( // FIXME condition is evaluated twice (label AND widget...)
                         record.data.id
                         && (modifiers.readonly || self.mode === 'readonly')
-                        && !widget.isSet() // getting like this because it could have been re-rendered...
+                        // The widget could have been rerendered so we have to
+                        // search it in the list with the field name
+                        && !_.findWhere(widgets, {name: fieldName}).isSet()
                     ));
                 },
             });

addons/web/static/tests/views/form_tests.js
@@ -576,6 +576,53 @@ QUnit.module('Views', {
         form.destroy();
     });
 
+    QUnit.test('empty fields\' labels still get the empty class after widget rerender', function (assert) {
+        assert.expect(6);
+
+        this.data.partner.fields.foo.default = false; // no default value for this test
+        this.data.partner.records[1].foo = false;  // 1 is record with id=2
+        this.data.partner.records[1].int_field = false;  // 1 is record with id=2
+
+        var form = createView({
+            View: FormView,
+            model: 'partner',
+            data: this.data,
+            arch: '<form>' +
+                    '<sheet>' +
+                        '<group>' +
+                            '<field name="foo"/>' +
+                            '<field name="int_field" attrs="{\'readonly\': [[\'foo\', \'=\', \'readonly\']]}"/>' +
+                        '</group>' +
+                    '</sheet>' +
+                '</form>',
+            res_id: 2,
+        });
+
+        assert.strictEqual(form.$('.o_field_widget.o_field_empty').length, 2,
+            "should have 2 empty fields with correct class");
+        assert.strictEqual(form.$('.o_form_label_empty').length, 2,
+            "should have 2 muted labels (for the empty fieds) in readonly");
+
+        form.$buttons.find('.o_form_button_edit').click();
+
+        assert.strictEqual(form.$('.o_field_empty').length, 0,
+            "in edit mode, only empty readonly fields should have the o_field_empty class");
+        assert.strictEqual(form.$('.o_form_label_empty').length, 0,
+            "in edit mode, only labels associated to empty readonly fields should have the o_form_label_empty class");
+
+        form.$('input[name="foo"]').val("readonly").trigger("input"); // int_field is now rerendered as readonly
+        form.$('input[name="foo"]').val("edit").trigger("input"); // int_field is now rerendered as editable
+        form.$('input[name="int_field"]').val('1').trigger("input"); // int_field is now set
+        form.$('input[name="foo"]').val("readonly").trigger("input"); // int_field is now rerendered as readonly
+
+        assert.strictEqual(form.$('.o_field_empty').length, 0,
+            "there still should not be any empty class on fields as the readonly one is now set");
+        assert.strictEqual(form.$('.o_form_label_empty').length, 0,
+            "there still should not be any empty class on labels as the associated readonly field is now set");
+
+        form.destroy();
+    });
+
     QUnit.test('empty inner readonly fields don\'t have o_form_empty class in "create" mode', function (assert) {
         assert.expect(2);
 