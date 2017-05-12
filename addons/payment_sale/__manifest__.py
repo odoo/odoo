b78323ee44823b90888bae245d5e37220146d687
Thibault Delavallée <tde@odoo.com>
2017-05-12 15:09:06 +0200

    [ADD] payment_sale

---
+++ addons/payment_sale/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Payment - Sale',
+    'category': 'Sales',
+    'summary': 'Link Sales and Payment',
+    'version': '1.0',
+    'description': """Link Sales and Payment""",
+    'depends': ['payment', 'sale'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
