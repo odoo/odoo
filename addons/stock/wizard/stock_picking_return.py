28a18623360d52e196c7ae193cf643416ce42b7c
Simon Lejeune <sle@openerp.com>
2017-05-17 21:22:53 +0200

    [REF] stock: adapt returns to new implementation

--- addons/stock/wizard/stock_picking_return.py
+++ addons/stock/wizard/stock_picking_return.py
@@ -10,7 +10,7 @@ class ReturnPickingLine(models.TransientModel):
     _name = "stock.return.picking.line"
     _rec_name = 'product_id'
 
-    product_id = fields.Many2one('product.product', string="Product", required=True)
+    product_id = fields.Many2one('product.product', string="Product", required=True, domain="[('id', '=', product_id)]")
     quantity = fields.Float("Quantity", digits=dp.get_precision('Product Unit of Measure'), required=True)
     wizard_id = fields.Many2one('stock.return.picking', string="Wizard")
     move_id = fields.Many2one('stock.move', "Move")
@@ -35,7 +35,6 @@ class ReturnPicking(models.TransientModel):
             raise UserError("You may only return one picking at a time!")
         res = super(ReturnPicking, self).default_get(fields)
 
-        Quant = self.env['stock.quant']
         move_dest_exists = False
         product_return_moves = []
         picking = self.env['stock.picking'].browse(self.env.context.get('active_id'))
@@ -46,16 +45,10 @@ class ReturnPicking(models.TransientModel):
             for move in picking.move_lines:
                 if move.scrapped:
                     continue
-                if move.move_dest_id:
+                if move.move_dest_ids:
                     move_dest_exists = True
-                # Sum the quants in that location that can be returned (they should have been moved by the moves that were included in the returned picking)
-                quantity = sum(quant.qty for quant in Quant.search([
-                    ('history_ids', 'in', move.id),
-                    ('qty', '>', 0.0), ('location_id', 'child_of', move.location_dest_id.id)
-                ]).filtered(
-                    lambda quant: not quant.reservation_id or quant.reservation_id.origin_returned_move_id != move)
-                )
-                quantity = move.product_id.uom_id._compute_quantity(quantity, move.product_uom)
+                quantity = move.product_qty - sum(move.move_dest_ids.filtered(lambda m: m.state in ['partially_available', 'assigned', 'done']).\
+                                                  mapped('pack_operation_ids').mapped('product_qty'))
                 product_return_moves.append((0, 0, {'product_id': move.product_id.id, 'quantity': quantity, 'move_id': move.id}))
 
             if not product_return_moves:
@@ -77,22 +70,9 @@ class ReturnPicking(models.TransientModel):
 
     @api.multi
     def _create_returns(self):
-        return_moves = self.product_return_moves.mapped('move_id')
-        unreserve_moves = self.env['stock.move']
-        for move in return_moves:
-            to_check_moves = self.env['stock.move'] | move.move_dest_id
-            while to_check_moves:
-                current_move = to_check_moves[-1]
-                to_check_moves = to_check_moves[:-1]
-                if current_move.state not in ('done', 'cancel') and current_move.reserved_quant_ids:
-                    unreserve_moves |= current_move
-                split_move_ids = self.env['stock.move'].search([('split_from', '=', current_move.id)])
-                to_check_moves |= split_move_ids
-
-        if unreserve_moves:
-            unreserve_moves.do_unreserve()
-            # break the link between moves in order to be able to fix them later if needed
-            unreserve_moves.write({'move_orig_ids': False})
+        # TODO sle: the unreserve of the next moves could be less brutal
+        for return_move in self.product_return_moves.mapped('move_id'):
+            return_move.move_dest_ids.filtered(lambda m: m.state not in ('done', 'cancel')).do_unreserve()
 
         # create new picking for returned products
         picking_type_id = self.picking_id.picking_type_id.return_picking_type_id.id or self.picking_id.picking_type_id.id
@@ -106,21 +86,15 @@ class ReturnPicking(models.TransientModel):
         new_picking.message_post_with_view('mail.message_origin_link',
             values={'self': new_picking, 'origin': self.picking_id},
             subtype_id=self.env.ref('mail.mt_note').id)
-
         returned_lines = 0
         for return_line in self.product_return_moves:
             if not return_line.move_id:
                 raise UserError(_("You have manually created product lines, please delete them to proceed"))
             new_qty = return_line.quantity
+            # TODO sle: float_is_zero?
             if new_qty:
-                # The return of a return should be linked with the original's destination move if it was not cancelled
-                if return_line.move_id.origin_returned_move_id.move_dest_id.id and return_line.move_id.origin_returned_move_id.move_dest_id.state != 'cancel':
-                    move_dest_id = return_line.move_id.origin_returned_move_id.move_dest_id.id
-                else:
-                    move_dest_id = False
-
                 returned_lines += 1
-                return_line.move_id.copy({
+                r = return_line.move_id.copy({
                     'product_id': return_line.product_id.id,
                     'product_uom_qty': new_qty,
                     'picking_id': new_picking.id,
@@ -131,8 +105,8 @@ class ReturnPicking(models.TransientModel):
                     'warehouse_id': self.picking_id.picking_type_id.warehouse_id.id,
                     'origin_returned_move_id': return_line.move_id.id,
                     'procure_method': 'make_to_stock',
-                    'move_dest_id': move_dest_id,
                 })
+                r.write({'move_orig_ids': [(4, return_line.move_id.id, False)]})
 
         if not returned_lines:
             raise UserError(_("Please specify at least one non-zero quantity."))
