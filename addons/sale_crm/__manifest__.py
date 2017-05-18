0f92eed8a0d78824be6bd58937448a69035c8d4e
Yannick Tivisse <yti@odoo.com>
2017-05-18 13:07:03 +0200

    Revert "[ADD] sale_management: Separate sales menu items from sale app"
    This reverts commit 9ab421d99aaa8c683811f7e69d42965b3a398c5c.
    
    We should use the active field on the ir.ui.menu with the root menuitem instead of moving all the menuitems into a new module

--- addons/sale_crm/__manifest__.py
+++ addons/sale_crm/__manifest__.py
@@ -17,7 +17,7 @@ We suggest you to install this module, if you installed both the sale and the cr
 modules.
     """,
     'website': 'https://www.odoo.com/page/crm',
-    'depends': ['sale_management', 'crm'],
+    'depends': ['sale', 'crm'],
     'data': [
         'security/ir.model.access.csv',
         'views/partner_views.xml',
