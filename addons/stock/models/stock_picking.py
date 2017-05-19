66a9c3c5c0b91f9a5386cd6c835f2cc4fc603827
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2017-05-19 15:27:40 +0200

    [IMP] Add a hook to allow customizing the operations grouping on stock.picking

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -502,11 +502,24 @@ class Picking(models.Model):
         self.action_assign()
         self.do_prepare_partial()
 
+    @api.multi
+    def _get_pack_operation_grouping_key(self, product, quant, computed_putaway_location):
+        """ Compute the key used to group pack operations in a picking """
+        self.ensure_one()
+
+        _Mapping = namedtuple('Mapping', ('product', 'package', 'owner', 'location', 'location_dst_id'))
+        return _Mapping(
+            quant.product_id or product,
+            quant.package_id,
+            quant.owner_id or self.owner_id,
+            quant.location_id or self.location_id,
+            computed_putaway_location,
+        )
+
     def _prepare_pack_ops(self, quants, forced_qties):
         """ Prepare pack_operations, returns a list of dict to give at create """
         # TDE CLEANME: oh dear ...
         valid_quants = quants.filtered(lambda quant: quant.qty > 0)
-        _Mapping = namedtuple('Mapping', ('product', 'package', 'owner', 'location', 'location_dst_id'))
 
         all_products = valid_quants.mapped('product_id') | self.env['product.product'].browse(p.id for p in forced_qties.keys()) | self.move_lines.mapped('product_id')
         computed_putaway_locations = dict(
@@ -543,7 +556,7 @@ class Picking(models.Model):
         qtys_grouped = {}
         lots_grouped = {}
         for quant in valid_quants:
-            key = _Mapping(quant.product_id, quant.package_id, quant.owner_id, quant.location_id, computed_putaway_locations[quant.product_id])
+            key = self._get_pack_operation_grouping_key(quant.product_id, quant, computed_putaway_locations[quant.product_id])
             qtys_grouped.setdefault(key, 0.0)
             qtys_grouped[key] += quant.qty
             if quant.product_id.tracking != 'none' and quant.lot_id:
@@ -553,7 +566,7 @@ class Picking(models.Model):
         for product, qty in forced_qties.items():
             if qty <= 0.0:
                 continue
-            key = _Mapping(product, self.env['stock.quant.package'], self.owner_id, self.location_id, computed_putaway_locations[product])
+            key = self._get_pack_operation_grouping_key(product, self.env['stock.quant'], computed_putaway_locations[product])
             qtys_grouped.setdefault(key, 0.0)
             qtys_grouped[key] += qty
 
