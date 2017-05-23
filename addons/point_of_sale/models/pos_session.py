ea3a7fdfdc894a8dafb19668fac2193ac4a5603d
Goffin Simon <sig@odoo.com>
2017-05-23 17:38:38 +0200

    [FIX] point_of_sale: _confirm_orders
    To help users to know why they can't close their pos sessions when
    a Sales Journal is not set in the pos.config.
    
    opw:744758

--- addons/point_of_sale/models/pos_session.py
+++ addons/point_of_sale/models/pos_session.py
@@ -20,6 +20,8 @@ class PosSession(models.Model):
             orders = session.order_ids.filtered(lambda order: order.state == 'paid')
             journal_id = self.env['ir.config_parameter'].sudo().get_param(
                 'pos.closing.journal_id_%s' % company_id, default=session.config_id.journal_id.id)
+            if not journal_id:
+                raise UserError(_("You have to set a Sale Journal for the POS:%s") % (session.config_id.name,))
 
             move = self.env['pos.order'].with_context(force_company=company_id)._create_account_move(session.start_at, session.name, int(journal_id), company_id)
             orders.with_context(force_company=company_id)._create_account_move_line(session, move)
