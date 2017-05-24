0b8c4b8ec3168d1aae35e71c9c3e93fb39cb8ae5
Thibault Delavallée <tde@odoo.com>
2017-05-24 14:45:35 +0200

    [REF] payment, website_sale: make payment_transaction js code generic
    JS code managing transaction creation through acquirer 'Pay Now' form
    button is improved in order to be less website-sale dependant. It now
    takes parameters for access token and URL to call in Json. Class used
    to bind JS is now o_payment_acquirer_button in order to be more generic.
    
    eCommerce module is updated accordingly. Future commits will probably
    make Online Quote (website_quote) use it.

--- addons/website_sale/static/src/js/website_sale_payment.js
+++ addons/website_sale/static/src/js/website_sale_payment.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     // If option is enable
     if ($("#checkbox_cgv").length) {
       $("#checkbox_cgv").click(function() {
-        $("div.oe_sale_acquirer_button").find('input, button').prop("disabled", !this.checked);
+        $("div.o_payment_acquirer_button").find('input, button').prop("disabled", !this.checked);
       });
     }
 });
