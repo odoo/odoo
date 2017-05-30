fc6eb3b5eadb8583124baba5ef3d6b0d3a88b804
Jeremy Kersten <jke@odoo.com>
2017-05-30 16:42:54 +0200

    [FIX] web: remove bad check from jquery.blockUI
    This check is wrong since jquery 1.11 is < 1.2
    
    This commit closes #17271

--- addons/web/static/lib/jquery.blockUI/jquery.blockUI.js
+++ addons/web/static/lib/jquery.blockUI/jquery.blockUI.js
@@ -14,11 +14,6 @@
 
 ;(function($) {
 
-if (/1\.(0|1|2)\.(0|1|2)/.test($.fn.jquery) || /^1.1/.test($.fn.jquery)) {
-	alert('blockUI requires jQuery v1.2.3 or later!  You are using v' + $.fn.jquery);
-	return;
-}
-
 $.fn._fadeIn = $.fn.fadeIn;
 
 var noOp = function() {};
