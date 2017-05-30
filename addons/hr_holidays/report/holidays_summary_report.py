7dfd1f953baa035d342d5511c27bb26d115b144b
Florent de Labarre <florent.mirieu@gmail.com>
2017-05-30 22:32:45 +0200

    [IMP] optimize compute

--- addons/hr_holidays/report/holidays_summary_report.py
+++ addons/hr_holidays/report/holidays_summary_report.py
@@ -21,7 +21,7 @@ class HrHolidaySummaryReport(models.AbstractModel):
         res = []
         start_date = fields.Date.from_string(start_date)
         for x in range(0, 60):
-            color = '#ababab' if start_date.strftime('%w') in ('6','0') else '' # ('6','0') = (Saturday, Sunday)
+            color = '#ababab' if start_date.weekday() in (5,6,) else '' # (5,6) = (Saturday, Sunday)
             res.append({'day_str': start_date.strftime('%a'), 'day': start_date.day , 'color': color})
             start_date = start_date + relativedelta(days=1)
         return res
@@ -48,7 +48,7 @@ class HrHolidaySummaryReport(models.AbstractModel):
         for index in range(0, 60):
             current = start_date + timedelta(index)
             res.append({'day': current.day, 'color': ''})
-            if current.strftime('%w') in ('6','0') : # ('6','0') = (Saturday, Sunday)
+            if current.weekday() in (5,6,) : # (5,6) = (Saturday, Sunday)
                 res[index]['color'] = '#ababab'
         # count and get leave summary details.
         holiday_type = ['confirm','validate'] if holiday_type == 'both' else ['confirm'] if holiday_type == 'Confirmed' else ['validate']
