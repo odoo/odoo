c04c51f3d9db5443c15de6635d35df0c991e6e32
Lucas Perais (lpe) <lpe@odoo.com>
2017-06-06 14:41:58 +0200

    [FIX] point_of_sale: recompute bank statement balance before validating session
    Before this commit, the return flow in the pos would not work if closing the pos session intervened before the return payment registration.
    The balance of the the statements was not updated before going to the validate step.
    
    After this commit, we force the recomputing of the relevant fields (i.e. balance_end_real), to reflect reality and permitting validatioon afterwards
    
    Backport of a5f7eedead31ad650efb60e5a52a8e0450ebb181 (on #17438)
    Closes #17599
    OPW 743763

--- addons/point_of_sale/models/pos_session.py
+++ addons/point_of_sale/models/pos_session.py
@@ -238,13 +238,13 @@ class PosSession(models.Model):
     @api.multi
     def action_pos_session_closing_control(self):
         for session in self:
+            for statement in session.statement_ids:
+                if (statement != session.cash_register_id) and (statement.balance_end != statement.balance_end_real):
+                    statement.write({'balance_end_real': statement.balance_end})
             #DO NOT FORWARD-PORT
             if session.state == 'closing_control':
                 session.action_pos_session_close()
                 continue
-            for statement in session.statement_ids:
-                if (statement != session.cash_register_id) and (statement.balance_end != statement.balance_end_real):
-                    statement.write({'balance_end_real': statement.balance_end})
             session.write({'state': 'closing_control', 'stop_at': fields.Datetime.now()})
             if not session.config_id.cash_control:
                 session.action_pos_session_close()
