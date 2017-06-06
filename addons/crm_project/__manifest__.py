d281acc466bcd41aac95c9b633707ac56123c3b8
Yannick Tivisse <yti@odoo.com>
2017-06-06 15:10:58 +0200

    [MOV] crm_project(_issue): Rename module and move feature on project.task

--- addons/crm_project/__manifest__.py
+++ addons/crm_project/__manifest__.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Lead to Issue',
-    'summary': 'Create Issues from Leads',
+    'name': 'Lead to Tasks',
+    'summary': 'Create Tasks from Leads',
     'sequence': '19',
     'category': 'Project',
     'complexity': 'easy',
     'description': """
-Lead to Issues
-==============
+Lead to Tasks
+=============
 
-Link module to map leads to issues
+Link module to map leads to tasks
         """,
     'data': [
         'security/ir.model.access.csv',
-        'wizard/crm_lead2projectissue_wizard_view.xml',
+        'wizard/crm_lead_convert2task_views.xml',
         'views/crm_lead_views.xml'
     ],
-    'depends': ['crm', 'project_issue'],
+    'depends': ['crm', 'project'],
 }
