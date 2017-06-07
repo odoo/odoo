73482f162ca7a10c531af9f2002235e173d60375
jpr-odoo <jpr@openerp.com>
2017-06-07 15:25:10 +0530

    [REF] payment,payment_stripe: added a new widget for code readability and easy to expand the behaviour of payment method refactor stripe payment method as a widget

--- addons/payment/static/src/js/payment_transaction.js
+++ addons/payment/static/src/js/payment_transaction.js
@@ -1,81 +1,92 @@
 odoo.define('payment.transaction', function (require) {
 "use strict";
 
-var ajax = require('web.ajax');
+    var ajax = require('web.ajax');
+    var Widget = require('web.Widget');
 
-$(document).ready(function () {
-    // When choosing an acquirer, display its Pay Now button
-    var $payment = $("#payment_method");
-    $payment.on("click", "input[name='acquirer'], a.btn_payment_token", function (ev) {
-            var ico_off = 'fa-circle-o';
-            var ico_on = 'fa-dot-circle-o';
+    var PaymentMethod = Widget.extend({
+        events: {
+            'click input[name="acquirer"], a.btn_payment_token': 'pay_now',
+            'click button[type="submit"], button[name="submit"]': 'payment_transaction',
+        },
 
-            var payment_id = $(ev.currentTarget).val() || $(this).data('acquirer');
-            var token = $(ev.currentTarget).data('token') || '';
+        start: function() {
+            this._super.apply(this, arguments);
+            this.$("input[name='acquirer']:checked").click();
+        },
 
-            $("div.o_payment_acquirer_button[data-id='"+payment_id+"']", $payment).attr('data-token', token);
-            $("div.js_payment a.list-group-item").removeClass("list-group-item-info");
-            $('span.js_radio').switchClass(ico_on, ico_off, 0);
+        pay_now: function(event){
+            var self = this,
+                ico_off = 'fa-circle-o',
+                ico_on = 'fa-dot-circle-o';
+
+            var $acquirer_el = $(event.currentTarget),
+                payment_id = $acquirer_el.val() || $acquirer_el.data('acquirer'),
+                token = $acquirer_el.data('token') || '';
+
+            self.$('div.js_payment a.list-group-item').removeClass("list-group-item-info");
+            self.$('span.js_radio').switchClass(ico_on, ico_off, 0);
+
             if (token) {
-              $("div.o_payment_acquirer_button div.token_hide").hide();
-              $(ev.currentTarget).find('span.js_radio').switchClass(ico_off, ico_on, 0);
-              $(ev.currentTarget).parents('li').find('input').prop("checked", true);
-              $(ev.currentTarget).addClass("list-group-item-info");
+                self.$('div.o_payment_acquirer_button[data-id=' +payment_id+ ']').attr('data-token', token);
+                self.$('div.o_payment_acquirer_button div.token_hide').hide();
+                $acquirer_el.find('span.js_radio').switchClass(ico_off, ico_on, 0);
+                $acquirer_el.parents('li').find('input').prop("checked", true);
+                $acquirer_el.addClass("list-group-item-info");
+            } else {
+                self.$("div.o_payment_acquirer_button div.token_hide").show();
             }
-            else{
-              $("div.o_payment_acquirer_button div.token_hide").show();
+            self.$('div.o_payment_acquirer_button[data-id]').addClass("hidden");
+            self.$('div.o_payment_acquirer_button[data-id=' +payment_id+ ']').removeClass("hidden");
+        },
+
+        payment_transaction: function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            var self = this,
+                $acquirer_el = $(event.currentTarget),
+                $form = $acquirer_el.parents('form'),
+                acquirer = $acquirer_el.parents('div.o_payment_acquirer_button').first(),
+                tx_data = acquirer.data();
+
+            if (! tx_data.id || ! tx_data.callUrl) {
+                return false;
             }
-            $("div.o_payment_acquirer_button[data-id]", $payment).addClass("hidden");
-            $("div.o_payment_acquirer_button[data-id='"+payment_id+"']", $payment).removeClass("hidden");
 
-    })
-    .find("input[name='acquirer']:checked").click();
+            var params = {"tx_type": tx_data.txType || acquirer.find('input[name="odoo_save_token"]').is(':checked') ? 'form_save': 'form'},
+                acquirer_token = acquirer.attr('data-token'); // !=data
 
-    // When clicking on payment button: create the tx using json then continue to the acquirer
-    $payment.on("click", 'button[type="submit"], button[name="submit"]', function (ev) {
-      ev.preventDefault();
-      ev.stopPropagation();
-      var $form = $(ev.currentTarget).parents('form');
-      var acquirer = $(ev.currentTarget).parents('div.o_payment_acquirer_button').first();
-      var tx_data = acquirer.data();
-      if (! tx_data.id || ! tx_data.callUrl) {
-        return false;
-      }
-
-      var params = {"tx_type": tx_data.txType || acquirer.find('input[name="odoo_save_token"]').is(':checked') ? 'form_save': 'form'};
-      var acquirer_token = acquirer.attr('data-token'); // !=data
-      if (acquirer_token) {
-        params.token = acquirer_token;
-      }
-      if (tx_data.access_token) {
-        params.access_token = tx_data.access_token;
-      }
-      $form.off('submit');
-      ajax.jsonRpc(tx_data.callUrl, 'call', params).then(function (data) {
-          $(data).appendTo('body').submit();
-      });
-      return false;
+            if (acquirer_token) {
+                params.token = acquirer_token;
+            }
+            if (tx_data.access_token) {
+                params.access_token = tx_data.access_token;
+            }
+            $form.off('submit');
+            ajax.jsonRpc(tx_data.callUrl, 'call', params).then(function (data) {
+                $(data).appendTo('body').submit();
+            });
+            return false;
+        },
     });
 
-    $('div.oe_pay_token').on('click', 'a.js_btn_valid_tx', function() {
-      $('div.js_token_load').toggle();
-
-      var $form = $(this).parents('form');
-      ajax.jsonRpc($form.attr('action'), 'call', $.deparam($form.serialize())).then(function (data) {
-        if (data.url) {
-          window.location = data.url;
-        }
-        else {
-          $('div.js_token_load').toggle();
-          if (!data.success && data.error) {
-            $('div.oe_pay_token div.panel-body p').html(data.error + "<br/><br/>" + _('Retry ? '));
-            $('div.oe_pay_token div.panel-body').parents('div').removeClass('panel-info').addClass('panel-danger');
-          }
-        }
-      });
-
+    $(document).ready(function (){
+        $('div.o_pay_token').on('click', 'a.js_btn_valid_tx', function() {
+            $('div.js_token_load').toggle();
+            var $form = $(this).parents('form');
+            ajax.jsonRpc($form.attr('action'), 'call', $.deparam($form.serialize())).then(function (data) {
+                if (data.url) {
+                    window.location = data.url;
+                }
+                else {
+                    $('div.js_token_load').toggle();
+                    if (!data.success && data.error) {
+                        $('div.o_pay_token div.panel-body p').html(data.error + "<br/><br/>" + _('Retry ? '));
+                        $('div.o_pay_token div.panel-body').parents('div').removeClass('panel-info').addClass('panel-danger');
+                    }
+                }
+            });
+        });
     });
-
-});
-
+    return PaymentMethod;
 });
--- addons/payment_stripe/static/src/js/stripe.js
+++ addons/payment_stripe/static/src/js/stripe.js
@@ -1,80 +1,124 @@
 odoo.define('payment_stripe.stripe', function(require) {
     "use strict";
     var ajax = require('web.ajax');
-    var handler = StripeCheckout.configure({
-        key: $("input[name='stripe_key']").val(),
-        image: $("input[name='stripe_image']").val(),
-        locale: 'auto',
-        closed: function() {
-          if (!handler.isTokenGenerate) {
-                $('#pay_stripe')
-                    .removeAttr('disabled')
-                    .find('i').remove();
-          }
+    var Widget = require('web.Widget');
+
+    var StripePaymentMethod = Widget.extend({
+        events: {
+            'click #pay_stripe': 'pay_now',
         },
-        token: function(token, args) {
-            handler.isTokenGenerate = true;
-            ajax.jsonRpc("/payment/stripe/create_charge", 'call', {
-                tokenid: token.id,
-                email: token.email,
-                amount: $("input[name='amount']").val(),
-                acquirer_id: $("#acquirer_stripe").val(),
-                currency: $("input[name='currency']").val(),
-                invoice_num: $("input[name='invoice_num']").val(),
-                return_url: $("input[name='return_url']").val()
-            }).done(function(data){
-                handler.isTokenGenerate = false;
-                window.location.href = data;
+
+        init: function(){
+            this.handler = false;
+        },
+
+        start: function() {
+            this._super.apply(this, arguments);
+            var self = this;
+            self.handler = StripeCheckout.configure({
+                key: $("input[name='stripe_key']").val(),
+                image: $("input[name='stripe_image']").val(),
+                locale: 'auto',
+                closed: function() {
+                  if (!self.handler.isTokenGenerate) {
+                        $('#pay_stripe')
+                            .removeAttr('disabled')
+                            .find('i').remove();
+                  }
+                },
+                token: function(token, args) {
+                    self.handler.isTokenGenerate = true;
+                    ajax.jsonRpc("/payment/stripe/create_charge", 'call', {
+                        tokenid: token.id,
+                        email: token.email,
+                        amount: $("input[name='amount']").val(),
+                        acquirer_id: $("#acquirer_stripe").val(),
+                        currency: $("input[name='currency']").val(),
+                        invoice_num: $("input[name='invoice_num']").val(),
+                        return_url: $("input[name='return_url']").val()
+                    }).done(function(data){
+                        self.handler.isTokenGenerate = false;
+                        window.location.href = data;
+                    });
+                },
             });
         },
-    });
 
-    $('#pay_stripe').on('click', function(e) {
-        // Open Checkout with further options
-        if(!$(this).find('i').length)
-            $(this).append('<i class="fa fa-spinner fa-spin"/>');
-            $(this).attr('disabled','disabled');
+        pay_now: function(event){
+            var self = this,
+                $currentTarget = $(event.currentTarget),
+                $form = $currentTarget.parents('form');
 
-        var $form = $(e.currentTarget).parents('form');
-        var acquirer_id = $(e.currentTarget).closest('div.o_payment_acquirer_button,div.oe_quote_acquirer_button,div.o_website_payment_new_payment');
-        acquirer_id = acquirer_id.data('id') || acquirer_id.data('acquirer_id');
-        if (! acquirer_id) {
-            return false;
-        }
+            if(!$currentTarget.find('i').length)
+                $currentTarget.append('<i class="fa fa-spinner fa-spin"/>');
+                $currentTarget.attr('disabled','disabled');
 
-        var access_token = $("input[name='token']").val();
-        var so_id = $("input[name='return_url']").val().match(/quote\/([0-9]+)/) || undefined;
-        if (so_id) {
-            so_id = parseInt(so_id[1]);
-        }
+            var acquirer_id = $currentTarget.closest('div.o_payment_acquirer_button,div.o_website_payment_new_payment');
+            acquirer_id = acquirer_id.data('id') || acquirer_id.data('acquirer_id');
+            if (! acquirer_id) {
+                return false;
+            }
+            var params = {'form': $form};
+            event.preventDefault();
+            self.stripe_payment_transaction(acquirer_id, params);
+        },
 
-        e.preventDefault();
-        if ($('.o_website_payment').length !== 0) {
-            ajax.jsonRpc('/website_payment/transaction', 'call', {
-                    reference: $("input[name='invoice_num']").val(),
-                    amount: $("input[name='amount']").val(),
-                    currency_id: $("input[name='currency_id']").val(),
-                    acquirer_id: acquirer_id
-                })
-                handler.open({
-                    name: $("input[name='merchant']").val(),
-                    description: $("input[name='invoice_num']").val(),
-                    currency: $("input[name='currency']").val(),
-                    amount: $("input[name='amount']").val()*100
-                });
-        } else {
-            ajax.jsonRpc('/shop/payment/transaction/' + acquirer_id, 'call', {
-                    so_id: so_id,
+        stripe_payment_transaction: function(acquirer_id, params){
+            var self = this,
+                handler = self.handler,
+                so_token = $("input[name='token']").val(),
+                so_id = $("input[name='return_url']").val().match(/quote\/([0-9]+)/) || undefined,
+                access_token = $("input[name='access_token']").val(),
+                payment_request_id = $("input[name='return_url']").val().match(/payment\/([0-9]+)/) || undefined;
+            if (so_id) {
+                so_id = parseInt(so_id[1]);
+            }
+            if (payment_request_id) {
+                payment_request_id = parseInt(payment_request_id[1]);
+            }
+            if ($('#online_invoice_payment').length !== 0){
+                ajax.jsonRpc('/payment/transaction/' + acquirer_id, 'call', {
+                    payment_request_id: payment_request_id,
                     access_token: access_token
-                }, {'async': false}).then(function (data) {
-                $form.html(data);
-                handler.open({
-                    name: $("input[name='merchant']").val(),
-                    description: $("input[name='invoice_num']").val(),
-                    currency: $("input[name='currency']").val(),
-                    amount: $("input[name='amount']").val()*100
+                    }).then(function (data) {
+                    params.form.html(data);
+                    handler.open({
+                        name: $("input[name='merchant']").val(),
+                        description: $("input[name='invoice_num']").val(),
+                        currency: $("input[name='currency']").val(),
+                        amount: $("input[name='amount']").val()*100
+                    });
                 });
-            });
+            } else {
+                ajax.jsonRpc('/shop/payment/transaction/' + acquirer_id, 'call', {
+                        so_id: so_id,
+                        so_token: so_token
+                    }, {'async': false}).then(function (data) {
+                    params.form.html(data);
+                    handler.open({
+                        name: $("input[name='merchant']").val(),
+                        description: $("input[name='invoice_num']").val(),
+                        currency: $("input[name='currency']").val(),
+                        amount: $("input[name='amount']").val()*100
+                    });
+                });
+            }
+        },
+    });
+
+    $(document).ready(function () {
+        if($("#pay_stripe").length){
+            var stripe_payment = new StripePaymentMethod();
+            if($("#website_quote_payment").length){
+                stripe_payment.attachTo($("#website_quote_payment"));
+            }
+            if($("#website_sale_payment").length){
+                stripe_payment.attachTo($("#website_sale_payment"));
+            }
+            if($("#online_invoice_payment").length){
+                stripe_payment.attachTo($("#online_invoice_payment"));
+            }
         }
     });
+    return StripePaymentMethod;
 });
