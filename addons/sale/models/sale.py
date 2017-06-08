0c97c2c47f51a09ecd0efe571bf5ee4448c7c329
Haresh Shyara <hsh@odoo.com>
2017-06-08 16:06:12 +0530

    [IMP] sale: always update salesperson when changing partner
    Updating the partner of an SO imply
    
     * setting the new partner sales person if set;
     * setting myself as I am the person who updated the SO and is therefore
       in charge of managing it.

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -224,12 +224,11 @@ class SaleOrder(models.Model):
             'payment_term_id': self.partner_id.property_payment_term_id and self.partner_id.property_payment_term_id.id or False,
             'partner_invoice_id': addr['invoice'],
             'partner_shipping_id': addr['delivery'],
+            'user_id': self.partner_id.user_id.id or self.env.uid
         }
         if self.env['ir.config_parameter'].sudo().get_param('sale.use_sale_note') and self.env.user.company_id.sale_note:
             values['note'] = self.with_context(lang=self.partner_id.lang).env.user.company_id.sale_note
 
-        if self.partner_id.user_id:
-            values['user_id'] = self.partner_id.user_id.id
         if self.partner_id.team_id:
             values['team_id'] = self.partner_id.team_id.id
         self.update(values)
