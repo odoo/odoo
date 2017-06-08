7decc6fc397ed1d16a752f4f3198eae0ea8492cf
Thibault Delavallée <tde@odoo.com>
2017-06-08 10:28:30 +0200

    [FIX] sale_payment: move method in right module
    Commit 9882e2eba102a064b0657207915334a55d41d87e moved code from website_portal_sale to
    a new sale_payment module, in order to move code down and ease its use in
    variou addons. However a called method was forgotten in the process leading
    to some issues when using sale_payment. This commit fixes that.

--- addons/sale_payment/models/sale_order.py
+++ addons/sale_payment/models/sale_order.py
@@ -9,3 +9,10 @@ class SaleOrder(models.Model):
 
     payment_tx_id = fields.Many2one('payment.transaction', string='Last Transaction', copy=False)
     payment_acquirer_id = fields.Many2one('payment.acquirer', string='Payment Acquirer', related='payment_tx_id.acquirer_id', store=True)
+
+    def _force_lines_to_invoice_policy_order(self):
+        for line in self.order_line:
+            if self.state in ['sale', 'done']:
+                line.qty_to_invoice = line.product_uom_qty - line.qty_invoiced
+            else:
+                line.qty_to_invoice = 0
