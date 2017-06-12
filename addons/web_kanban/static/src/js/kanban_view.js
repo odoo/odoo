4d4a3e81f74c5bef6301d96a1b6e6fab01c0a50d
Martin Geubelle <mge@odoo.com>
2017-06-12 16:16:59 +0200

    [FIX] web_kanban: allow column creation on custom model
    This is a back-port of the commit https://github.com/odoo/odoo/commit/73bbb7a4d6d9f2cb20cf9cd7912e34922612ff11
    that had been applied in saas-16.
    
    As this has been considered as a bug, it should be fixed in previous versions.
    
    No need to forward-port it after saas-15 then.

--- addons/web_kanban/static/src/js/kanban_view.js
+++ addons/web_kanban/static/src/js/kanban_view.js
@@ -714,14 +714,15 @@ var KanbanView = View.extend({
     add_new_column: function (event) {
         var self = this;
         var model = new Model(this.relation, this.search_context);
-        model.call('create', [{name: event.data.value}], {
+        var name = event.data.value;
+        model.call('name_create', [name], {
             context: this.search_context,
-        }).then(function (id) {
+        }).then(function (result) {
             var dataset = new data.DataSetSearch(self, self.dataset.model, self.dataset.get_context(), []);
             var group_data = {
                 records: [],
                 title: event.data.value,
-                id: id,
+                id: result[0],
                 attributes: {folded: false},
                 dataset: dataset,
                 values: {},
