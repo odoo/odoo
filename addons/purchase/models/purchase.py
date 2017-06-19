004412749b83376221c7baec2f82718dd9bd954c
Florent de Labarre <florent.mirieu@gmail.com>
2017-06-19 14:06:32 +0200

    [IMP] purchase: clarify code
    The sum of list is confusing
    Work with recordsets directly
    
    Closes #17673

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -450,16 +450,15 @@ class PurchaseOrder(models.Model):
         result = action.read()[0]
 
         #override the context to get rid of the default filtering on operation type
-        result.pop('id', None)
         result['context'] = {}
-        pick_ids = sum([order.picking_ids.ids for order in self], [])
+        pick_ids = self.mapped('picking_ids')
         #choose the view_mode accordingly
         if len(pick_ids) > 1:
-            result['domain'] = "[('id','in',[" + ','.join(str(id) for id in pick_ids) + "])]"
+            result['domain'] = "[('id','in',%s])]" % (pick_ids.ids)
         elif len(pick_ids) == 1:
             res = self.env.ref('stock.view_picking_form', False)
             result['views'] = [(res and res.id or False, 'form')]
-            result['res_id'] = pick_ids and pick_ids[0] or False
+            result['res_id'] = pick_ids.id
         return result
 
     @api.multi
