a8c12e1fd0542726ada733b592f5196d256ec7e9
Goffin Simon <sig@odoo.com>
2017-06-27 13:14:01 +0200

    [FIX] sale: Updating dashboard_graph_model when uninstalling Sales
    Uninstalling Sales with CRM still installed lead to an error
    when clicking on CRM app because the dashboard_graph_model was not
    updated for each crm.team record.
    
    opw:748099

--- addons/sale/__init__.py
+++ addons/sale/__init__.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+
+from functools import partial
+import openerp
+from openerp import api, SUPERUSER_ID
+
 import models
 import report
 import wizard
+
+
+def uninstall_hook(cr, registry):
+    def update_dashboard_graph_model(dbname):
+        db_registry = openerp.modules.registry.RegistryManager.new(dbname)
+        with api.Environment.manage(), db_registry.cursor() as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+            if 'crm.team' in env:
+                recs = env['crm.team'].search([])
+                for rec in recs:
+                	rec._onchange_team_type()
+
+    cr.after("commit", partial(update_dashboard_graph_model, cr.dbname))
