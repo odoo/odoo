d2c30410a83074ab46ed47cad462d0ebf6baa05b
Christophe Simonis <chs@odoo.com>
2017-06-27 15:52:37 +0200

    [FIX] stock: correct import

--- addons/stock/models/stock_pack_operation.py
+++ addons/stock/models/stock_pack_operation.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models, _
 from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools.float_utils import float_round, float_compare
-from odoo.tools.compat import izip
+from odoo.tools.pycompat import izip
 
 
 class PackOperation(models.Model):
