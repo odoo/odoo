fe346fa021d1e762e14b09d1cdbcba6c74cde6f7
Yenthe V.G <yenthevg@gmail.com>
2017-06-28 10:36:02 +0200

    [REM] stock: remove duplicated module description
    The proper description is in static/description
    
    Closes #17873
    Fixes #3846

--- addons/stock/__manifest__.py
+++ addons/stock/__manifest__.py
@@ -5,32 +5,7 @@
     'name': 'Inventory Management',
     'version': '1.1',
     'summary': 'Inventory, Logistics, Warehousing',
-    'description': """
-Manage multi-warehouses, multi- and structured stock locations
-==============================================================
-
-The warehouse and inventory management is based on a hierarchical location structure, from warehouses to storage bins.
-The double entry inventory system allows you to manage customers, vendors as well as manufacturing inventories.
-
-Odoo has the capacity to manage lots and serial numbers ensuring compliance with the traceability requirements imposed by the majority of industries.
-
-Key Features
-------------
-* Moves history and planning,
-* Minimum stock rules
-* Support for barcodes
-* Rapid detection of mistakes through double entry system
-* Traceability (Serial Numbers, Packages, ...)
-
-Dashboard / Reports for Inventory Management will include:
-----------------------------------------------------------
-* Incoming Products (Graph)
-* Outgoing Products (Graph)
-* Procurement in Exception
-* Inventory Analysis
-* Last Product Inventories
-* Moves Analysis
-    """,
+    'description': "",
     'website': 'https://www.odoo.com/page/warehouse',
     'depends': ['product', 'procurement', 'barcodes', 'web_planner'],
     'category': 'Warehouse',
