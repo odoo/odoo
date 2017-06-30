aa5e531468b49dca76d659dd5e4bd27cf5faf5cb
Yenthe V.G <yenthevg@gmail.com>
2017-06-30 15:36:14 +0200

    [account] REM: duplicated description for account module

--- addons/account/__manifest__.py
+++ addons/account/__manifest__.py
@@ -5,13 +5,7 @@
     'version' : '1.1',
     'summary': 'Send Invoices and Track Payments',
     'sequence': 30,
-    'description': """
-Invoicing & Payments
-====================
-The specific and easy-to-use Invoicing system in Odoo allows you to keep track of your accounting, even when you are not an accountant. It provides an easy way to follow up on your vendors and customers.
-
-You could use this simplified accounting in case you work with an (external) account to keep your books, and you still want to keep track of payments. This module also offers you an easy method of registering payments, without having to encode complete abstracts of account.
-    """,
+    'description': "",
     'category': 'Accounting',
     'website': 'https://www.odoo.com/page/billing',
     'images' : ['images/accounts.jpeg','images/bank_statement.jpeg','images/cash_register.jpeg','images/chart_of_accounts.jpeg','images/customer_invoice.jpeg','images/journal_entries.jpeg'],
