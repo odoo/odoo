4322d96a0cad9eb67c74bb60ad260473cdb4891b
Jeremy Kersten <jke@odoo.com>
2017-07-03 09:57:21 +0200

    [FIX] website_portal_sale: be sure that form post a vat before to compare
    Typo in previous condition. 'vat' in partner is always True.

--- addons/website_portal_sale/controllers/main.py
+++ addons/website_portal_sale/controllers/main.py
@@ -197,7 +197,7 @@ class website_account(website_account):
         partner = request.env['res.users'].browse(request.uid).partner_id
         invoices = request.env['account.invoice'].sudo().search_count([('partner_id', '=', partner.id), ('state', 'not in', ['draft', 'cancel'])])
         if invoices:
-            if 'vat' in partner and (data['vat'] or False) != (partner.vat or False):
+            if 'vat' in data and (data['vat'] or False) != (partner.vat or False):
                 error['vat'] = 'error'
                 error_message.append(_('Changing VAT number is not allowed once invoices have been issued for your account. Please contact us directly for this operation.'))
             if 'name' in data and (data['name'] or False) != (partner.name or False):
