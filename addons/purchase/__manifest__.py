57f377588d8477a458011850584112b3ced99962
Yenthe V.G <yenthevg@gmail.com>
2017-07-05 12:53:16 +0200

    [REM] purchase: remove description in favour of index.html

--- addons/purchase/__manifest__.py
+++ addons/purchase/__manifest__.py
@@ -7,23 +7,7 @@
     'category': 'Purchases',
     'sequence': 60,
     'summary': 'Purchase Orders, Receipts, Vendor Bills',
-    'description': """
-Manage goods requirement by Purchase Orders easily
-==================================================
-
-Purchase management enables you to track your vendors' price quotations and convert them into purchase orders if necessary.
-Odoo has several methods of monitoring invoices and tracking the receipt of ordered goods. You can handle partial deliveries in Odoo, so you can keep track of items that are still to be delivered in your orders, and you can issue reminders automatically.
-
-Odoo's replenishment management rules enable the system to generate draft purchase orders automatically, or you can configure it to run a lean process driven entirely by current production needs.
-
-Dashboard / Reports for Purchase Management will include:
----------------------------------------------------------
-* Request for Quotations
-* Purchase Orders Waiting Approval
-* Monthly Purchases by Category
-* Receipt Analysis
-* Purchase Analysis
-    """,
+    'description': "",
     'website': 'https://www.odoo.com/page/purchase',
     'depends': ['stock_account'],
     'data': [
