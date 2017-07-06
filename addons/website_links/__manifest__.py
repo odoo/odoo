3988120c8a61365372fdd203b7ab72fe89096398
Fabien Pinckaers <fp@openerp.com>
2017-07-06 09:39:32 +0200

    [IMP] website_links: reverting latest commit: auto_install

--- addons/website_links/__manifest__.py
+++ addons/website_links/__manifest__.py
@@ -14,5 +14,5 @@ Website interface to create short and trackable URLs.
         'security/ir.model.access.csv',
     ],
     'qweb': ['static/src/xml/*.xml'],
-    'auto_install': False,
+    'auto_install': True,
 }
