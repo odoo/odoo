02d650bf80e8db0a2fa3d05af8dc2477a614790a
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2017-07-07 08:14:12 +0200

    [IMP] stock: only try to create if the lot is not existing yet

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -912,7 +912,9 @@ class Picking(models.Model):
         Lot = self.env['stock.production.lot']
         for pack_op_lot in self.mapped('pack_operation_ids').mapped('pack_lot_ids'):
             if not pack_op_lot.lot_id:
-                lot = Lot.create({'name': pack_op_lot.lot_name, 'product_id': pack_op_lot.operation_id.product_id.id})
+                lot = Lot.search([('name', '=', pack_op_lot.lot_name), ('product_id', '=', pack_op_lot.operation_id.product_id.id)])
+                if not lot:
+                    lot = Lot.create({'name': pack_op_lot.lot_name, 'product_id': pack_op_lot.operation_id.product_id.id})
                 pack_op_lot.write({'lot_id': lot.id})
         # TDE FIXME: this should not be done here
         self.mapped('pack_operation_ids').mapped('pack_lot_ids').filtered(lambda op_lot: op_lot.qty == 0.0).unlink()
