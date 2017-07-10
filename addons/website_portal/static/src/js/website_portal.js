c00aeea7f62572b00e46b5881feb375c9d1d582a
Thibault Delavallée <tde@odoo.com>
2017-07-10 16:05:39 +0200

    [IMP] portal: rename o_website_portal class to o_portal to simplify a bit various class names
    Indeed when adding suffixes for sub-classes this makes class names quite
    long.

--- addons/website_portal/static/src/js/website_portal.js
+++ addons/website_portal/static/src/js/website_portal.js
@@ -3,20 +3,20 @@ odoo.define('website_portal', function(require) {
     'use strict';
     require('website.website');
 
-    if(!$('.o_website_portal').length) {
-        return $.Deferred().reject("DOM doesn't contain '.o_website_portal'");
+    if(!$('.o_portal').length) {
+        return $.Deferred().reject("DOM doesn't contain '.o_portal'");
     }
 
-    if ($('.o_website_portal_details').length) {
+    if ($('.o_portal_details').length) {
         var state_options = $("select[name='state_id']:enabled option:not(:first)");
-        $('.o_website_portal_details').on('change', "select[name='country_id']", function () {
+        $('.o_portal_details').on('change', "select[name='country_id']", function () {
             var select = $("select[name='state_id']");
             state_options.detach();
             var displayed_state = state_options.filter("[data-country_id="+($(this).val() || 0)+"]");
             var nb = displayed_state.appendTo(select).show().size();
             select.parent().toggle(nb>=1);
         });
-        $('.o_website_portal_details').find("select[name='country_id']").change();
+        $('.o_portal_details').find("select[name='country_id']").change();
     }
 
     if ($('.o_portal_search_panel').length) {
