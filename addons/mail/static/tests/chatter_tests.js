4f83d3e52f1b640767d7ff78ca06cbeb01fb7660
Martin Geubelle <mge@odoo.com>
2017-07-11 17:18:19 +0200

    [FIX] mail: make many2many_tags_email widget work
    This widget hasn't been rewritten with the new views.
    
    The purpose of this widget is to open a popup for each invalid partners
    (without email) in a many2many to fill the email. If the email is not set, the
    partner will not appear in the tags and its id will not be sent (which should
    not be the role of a widget but that's the way it is).

--- addons/mail/static/tests/chatter_tests.js
+++ addons/mail/static/tests/chatter_tests.js
@@ -11,6 +11,7 @@ var testUtils = require('web.test_utils');
 
 var BasicComposer = Composers.BasicComposer;
 
+var createAsyncView = testUtils.createAsyncView;
 var createView = testUtils.createView;
 
 QUnit.module('mail', {}, function () {
@@ -860,6 +861,87 @@ QUnit.test('followers widget: do not display follower duplications', function (a
         "there should be 2 followers in the follower dropdown");
 
     form.destroy();
+});
+
+QUnit.module('FieldMany2ManyTagsEmail', {
+    beforeEach: function () {
+        this.data = {
+            partner: {
+                fields: {
+                    display_name: { string: "Displayed name", type: "char" },
+                    timmy: { string: "pokemon", type: "many2many", relation: 'partner_type'},
+                },
+                records: [{
+                    id: 1,
+                    display_name: "first record",
+                    timmy: [],
+                }],
+            },
+            partner_type: {
+                fields: {
+                    name: {string: "Partner Type", type: "char"},
+                    email: {string: "Email", type: "char"},
+                },
+                records: [
+                    {id: 12, display_name: "gold", email: 'coucou@petite.perruche'},
+                    {id: 14, display_name: "silver", email: ''},
+                ]
+            },
+        };
+    },
+});
+
+QUnit.test('fieldmany2many tags email', function (assert) {
+    assert.expect(11);
+    var done = assert.async();
+
+    this.data.partner.records[0].timmy = [12, 14];
+
+    // the modals need to be closed before the form view rendering
+    createAsyncView({
+        View: FormView,
+        model: 'partner',
+        data: this.data,
+        res_id: 1,
+        arch:'<form string="Partners">' +
+                '<sheet>' +
+                    '<field name="display_name"/>' +
+                    '<field name="timmy" widget="many2many_tags_email"/>' +
+                '</sheet>' +
+            '</form>',
+        viewOptions: {
+            mode: 'edit',
+        },
+        mockRPC: function (route, args) {
+            if (args.method ==='read' && args.model === 'partner_type') {
+                assert.step(args.args[0]);
+                assert.deepEqual(args.args[1] , ['display_name', 'email'], "should read the email");
+            }
+            return this._super.apply(this, arguments);
+        },
+        archs: {
+            'partner_type,false,form': '<form string="Types"><field name="display_name"/><field name="email"/></form>',
+        },
+    }).then(function (form) {
+        // should read it 3 times (1 with the form view, one with the form dialog and one after save)
+        assert.verifySteps([[12, 14], [14], [14]]);
+        assert.strictEqual(form.$('.o_field_many2manytags[name="timmy"] span.o_tag_color_10').length, 2,
+            "the second tag should be present");
+
+        form.destroy();
+        done();
+    });
+
+    assert.strictEqual($('.modal-body.o_act_window').length, 1,
+        "there should be one modal opened to edit the empty email");
+    assert.strictEqual($('.modal-body.o_act_window input[name="display_name"]').val(), "silver",
+        "the opened modal should be a form view dialog with the partner_type 14");
+    assert.strictEqual($('.modal-body.o_act_window input[name="email"]').length, 1,
+        "there should be an email field in the modal");
+
+    // set the email and save the modal (will render the form view)
+    $('.modal-body.o_act_window input[name="email"]').val('coucou@petite.perruche').trigger('input');
+    $('.modal-footer .btn-primary').click();
 });
 
 });
