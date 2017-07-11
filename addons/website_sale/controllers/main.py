90bec8511feaf97e1552b9e8b0b939d635c1f8da
Thibault Delavallée <tde@odoo.com>
2017-07-11 17:29:16 +0200

    [FIX] point_of_sale, website_sale: correctly handle tuple return value of render_qweb_pdf
    Fix related to 765b65c1df962e35ec1a3f8ee046db69efbcbd34 .

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -890,8 +890,8 @@ class WebsiteSale(http.Controller):
     def print_saleorder(self):
         sale_order_id = request.session.get('sale_last_order_id')
         if sale_order_id:
-            pdf = request.env.ref('sale.action_report_saleorder').sudo().render_qweb_pdf([sale_order_id])
-            pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', len(pdf))]
+            pdf, _ = request.env.ref('sale.action_report_saleorder').sudo().render_qweb_pdf([sale_order_id])
+            pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', u'%s' % len(pdf))]
             return request.make_response(pdf, headers=pdfhttpheaders)
         else:
             return request.redirect('/shop')
