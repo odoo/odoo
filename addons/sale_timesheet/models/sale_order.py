26cea16a437754d661c1e972015acf29eacb1683
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2017-07-12 09:21:50 +0200

    [IMP] Check if there is any sale line before compute analytic.

--- addons/sale_timesheet/models/sale_order.py
+++ addons/sale_timesheet/models/sale_order.py
@@ -141,6 +141,8 @@ class SaleOrderLine(models.Model):
     @api.multi
     def _compute_analytic(self, domain=None):
         if not domain:
+            if not self.ids:
+                return True
             # To filter on analyic lines linked to an expense
             expense_type_id = self.env.ref('account.data_account_type_expenses', raise_if_not_found=False)
             expense_type_id = expense_type_id and expense_type_id.id
