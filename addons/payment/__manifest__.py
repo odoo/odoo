0f251872c23694dbccccee95423ac8973a5be169
Thibault Delavallée <tde@odoo.com>
2017-07-14 15:04:52 +0200

    [ADD] account_payment: new bridge module between account and payment to enable portal payment

--- addons/payment/__manifest__.py
+++ addons/payment/__manifest__.py
@@ -13,7 +13,6 @@
         'views/payment_views.xml',
         'views/account_payment_views.xml',
         'views/payment_templates.xml',
-        'views/account_portal_templates.xml',
         'views/payment_assets.xml',
         'views/res_partner_views.xml',
         'security/ir.model.access.csv',
