eac9c6e5a59c4638d76e156b149533a7066c9b28
Kinjal Mehta <kme@odoo.com>
2017-07-17 16:43:27 +0530

    [ADD] web: use burger menu for medium screen devices.

--- addons/web/static/src/js/services/config.js
+++ addons/web/static/src/js/services/config.js
@@ -70,7 +70,7 @@ function _updateSizeProps() {
     var sc = _getSizeClass();
     if (sc !== config.device.size_class) {
         config.device.size_class = sc;
-        config.isMobile = config.device.size_class <= config.device.SIZES.XS;
+        config.isMobile = config.device.size_class <= config.device.SIZES.SM;
         core.bus.trigger('size_class', sc);
     }
 }
