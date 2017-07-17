e66ba3ce1ed7c7c79dc2965237d1e37c6e02940b
Thibault Delavallée <tde@odoo.com>
2017-07-17 16:46:53 +0200

    [CLN] website_payment: remove some unused variables in JS

--- addons/website_payment/static/src/js/payment_transaction.js
+++ addons/website_payment/static/src/js/payment_transaction.js
@@ -1,7 +1,6 @@
 odoo.define('website_payment.website_payment', function (require) {
 "use strict";
 
-var website = require('website.website');
 var ajax = require('web.ajax');
 
 if(!$('.o_website_payment').length) {
@@ -21,18 +20,6 @@ $payment.on("click", 'button[type="submit"],button[name="submit"]', function (ev
   ajax.jsonRpc('/website_payment/transaction/', 'call', data).then(function (result) {
     $form.submit();
   });
-
-  function getFormData($form){
-      var unindexed_array = $form.serializeArray();
-      var indexed_array = {};
-
-      $.map(unindexed_array, function(n, i){
-          indexed_array[n.name] = n.value;
-      });
-
-      return indexed_array;
-  }
 });
-
 
 });
