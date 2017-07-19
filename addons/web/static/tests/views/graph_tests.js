b63e22f50ea8cce8deaa7dcc0d1c5649d2d2880b
Jigar Patel <jat@openerp.com>
2017-07-19 14:18:12 +0530

    [FIX] web: Groupby not working on Pivot and Graph view
    The groupby property was wrongly set in some cases: if a graph/pivot view is defined as a groupby on field F, but there is another groupby key G in the context, it will take F instead of G (at initial load).

--- addons/web/static/tests/views/graph_tests.js
+++ addons/web/static/tests/views/graph_tests.js
@@ -344,6 +344,34 @@ QUnit.module('Views', {
             done();
         });
     });
+
+    QUnit.test('correctly use group_by key from the context', function (assert) {
+        var done = assert.async();
+        assert.expect(2);
+
+        var graph = createView({
+            View: GraphView,
+            model: 'foo',
+            data: this.data,
+            arch: '<graph><field name="product_id" /></graph>',
+            groupBy: ['color_id'],
+            viewOptions: {
+                context: {
+                    graph_measure: 'foo',
+                    graph_mode: 'line',
+                },
+            },
+        });
+        return concurrency.delay(0).then(function () {
+            assert.strictEqual(graph.$('text:contains(xphone)').length, 0,
+                        'should not contain a text element with product in legend');
+            assert.strictEqual(graph.$('text:contains(red)').length, 1,
+                        'should contain a text element with color in legend');
+            graph.destroy();
+            done();
+        });
+    });
+
 });
 
 });
