1b01b6e5bf17ab3567e3d3902b92ffda8cbc03b2
Thibault Delavallée <tde@odoo.com>
2017-07-19 10:45:32 +0200

    [ADD] web_routing: advanced routing for web module
    This module will contain website routing capabilities that are not
    necessarily linked to a website. As we don't want to put them directly
    in base to keep the core simple this module will be used to hold
    those features.

---
+++ addons/web_routing/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Web Routing',
+    'summary': 'Web Routing',
+    'sequence': '9100',
+    'category': 'Hidden',
+    'description': """
+Web Routing
+===========
+""",
+    'data': [
+    ],
+    'depends': ['web'],
+}
