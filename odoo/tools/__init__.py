3b1e28ce84b90211ce76bc11ecdf75ae4dc095a5
Richard Mathot <rim@odoo.com>
2017-07-24 16:54:42 +0200

    [REM] tools: old and unused lib pdf_utils

--- odoo/tools/__init__.py
+++ odoo/tools/__init__.py
@@ -12,7 +12,6 @@ from .graph import graph
 from .image import *
 from .amount_to_text import *
 from .amount_to_text_en import *
-from .pdf_utils import *
 from .yaml_import import *
 from .sql import *
 from .float_utils import *
