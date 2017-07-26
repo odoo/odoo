e1a1fc8e730c89faf99a4398dd3a71c4fca4046b
Pierre Rousseau <pro@odoo.com>
2017-07-26 15:44:09 +0200

    [IMP] l10n_sg: improve localisation
    Add unique_entity_number to company and partner to use them in the IRAS Audit File (l10n_sg_reports)
    Rename vat to GSTNo to granted to Singapore l10n
    Updates labels to l10n_sg_label
    Add description in manifest
    Add taxes fields in account.invoice.line

--- addons/l10n_sg/__init__.py
+++ addons/l10n_sg/__init__.py
@@ -3,7 +3,7 @@
 
 # Copyright (C) 2014 Tech Receptives (<http://techreceptives.com>).
 
-
 def _preserve_tag_on_taxes(cr, registry):
     from odoo.addons.account.models.chart_template import preserve_existing_tags_on_taxes
     preserve_existing_tags_on_taxes(cr, registry, 'l10n_sg')
+from . import models
