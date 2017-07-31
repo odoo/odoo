4de583fee6d41c55284f5e249c1ca6f5684cea4f
Fabien Meghazi <fme@odoo.com>
2017-07-31 10:55:30 +0200

    [WIP] forgot about http#db_filter()

--- odoo/http.py
+++ odoo/http.py
@@ -1491,6 +1491,11 @@ def db_filter(dbs, httprequest=None):
     if odoo.tools.config['dbfilter']:
         r = odoo.tools.config['dbfilter'].replace('%h', h).replace('%d', d)
         dbs = [i for i in dbs if re.match(r, i)]
+    else:
+        # In case --db-filter is not provided and --database is passed, Odoo will
+        # use the value of --database as a comma seperated list of exposed databases.
+        exposed_dbs = set(db.strip() for db in odoo.tools.config['db_name'].split(','))
+        dbs = list(exposed_dbs.intersection(dbs))
     return dbs
 
 def db_monodb(httprequest=None):
