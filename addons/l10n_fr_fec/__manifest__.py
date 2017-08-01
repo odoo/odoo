ec64aa084924c731604b02feb3ea761cd16b091e
Yannick Tivisse <yti@odoo.com>
2017-08-01 10:17:41 +0200

    [IMP] account: Split clearly the accounting & invoicing applications
    PURPOSE
    =======
    
    1) Have a clear split between the invoicing and the accounting apps
    2) Have a clear split between the community and the enterprise version (no accounting in community)
    
    SPECIFICATION
    =============
    
    (1) When only invoicing installed, we shouldn't see accounts (for community and enterprise)
    (a) Perpetual Inventory Valuation:
         - Hide the field inventory valuation from the product category
    (b) Taxes
    - No accounts
    - No cash basis option
    - No tax adjustment
    
    (2) Remove from Community, modules:
    - Accounting
    - Budgets
    - Assets
    
    (3) No account_report in community, keep PDF reports for
    - sale/purchase journal
    - Aged partner balance
    - Partner Ledger
    
    (4) Clean split between invoicing and accounting in enterprise
    (a) When installing invoicing, I shouldn't have access to accounting just by changing my access right to adviser
    (b) Menus (when only invoicing installed)
    - Sales
        - Customer Invoices
        - Customer Credit Notes
        - Payments
        - Customer Statements
        - Customers
        - Sellable Products
    - Purchases
        - Vendor Bills
        - Vendor Credit Notes
        - Payments
        - Vendors
        - Purchasable Products
    - Reporting
        - Partner Reports
            - Partner Ledger
            - Aged Receivable
            - Aged Payable
        - Audit Reports
            - Tax report
        - Management
            - Invoices
            - Product Margins
    - Configuration
        - Settings
        - Products
            - Products
        - Accounting
            - Taxes
            - Fiscal Positions
            - Bank Accounts
            - Journals
        - Management
            - Payment Terms
            - Follow-up levels
        - Payments
            - Payment Acquirers
    (c) Settings (when only invoicing installed)
    - Fiscal Localization
    - Taxes
        - Default Taxes
        - Rounding Method
        - TaxCloud connection
        - TaxCloud Categories
        - EU Digital Goods VAT
        - VIES VAT CHECK
    - Currencies
        - Main currency
        - Multi-currencies
    - Invoicing
        - Warnings
        - Docsaway
    - Customer Payments
        - Follow-up levels
        - Payment Followup
    - Bills Payment
    - Bank & Cash
    - Analytics
        - Margin
    - Automated Entries
    
    Remarks
    - Test with the different access rights

--- addons/l10n_fr_fec/__manifest__.py
+++ addons/l10n_fr_fec/__manifest__.py
@@ -9,7 +9,7 @@
     'summary': "Fichier d'Échange Informatisé (FEC) for France",
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.akretion.com',
-    'depends': ['l10n_fr', 'account_accountant'],
+    'depends': ['l10n_fr', 'account'],
     'data': [
         'wizard/account_fr_fec_view.xml',
     ],
