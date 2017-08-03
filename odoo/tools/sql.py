d0006976f3d7ed641d4b65ce5422127bcb29d5bf
Fabien Meghazi <fme@odoo.com>
2017-08-03 16:16:12 +0200

    add comment

--- odoo/tools/sql.py
+++ odoo/tools/sql.py
@@ -53,6 +53,9 @@ def table_columns(cr, tablename):
     """ Return a dict mapping column names to their configuration. The latter is
         a dict with the data from the table ``information_schema.columns``.
     """
+    # Do not select the field `character_octet_length` from `information_schema.columns`
+    # because specific access right restriction in the context of shared hosting might
+    # prevent a postgres user to read this field.
     query = '''SELECT column_name, udt_name, character_maximum_length, is_nullable
                FROM information_schema.columns WHERE table_name=%s'''
     cr.execute(query, (tablename,))
