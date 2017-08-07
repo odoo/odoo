20a524d5c7ea5f8bd4fa9971cfe93a945ec25aa2
Jérome Maes <jem@openerp.com>
2017-08-07 15:49:53 +0200

    [IMP] project_timesheet_holidays: ease leave type configuration
    A leave type now generate timesheet by default. So
    the internal project/task are taken from the leave
    type company (a default value for the company has
    been added).
    To allow the hr leave manager to select a project,
    he should be alble to read the analytic account
    (otherwise, crash when calling name_get).
    
    Setting the default internal project/task is advanced,
    so hidden in debug mode on timesheet config.

--- addons/project_timesheet_holidays/__manifest__.py
+++ addons/project_timesheet_holidays/__manifest__.py
@@ -17,8 +17,11 @@ on leaves. Project and task can be configured company-wide.
     'data': [
         'views/res_config_settings_views.xml',
         'views/hr_holidays_views.xml',
+        'security/ir.model.access.csv',
+
     ],
     'demo': [],
     'installable': True,
     'auto_install': True,
+    'post_init_hook': 'post_init',
 }
