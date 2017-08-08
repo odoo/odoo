cfdc76d758adbf8fded99a9d6c5771aa7e240139
dip-odoo <dip@odoo.com>
2017-08-08 13:58:39 +0200

    [IMP] base_vat: clearer labels for VIES check

--- addons/base_vat/models/account_config_settings.py
+++ addons/base_vat/models/account_config_settings.py
@@ -7,4 +7,4 @@ class AccountConfigSettings(models.TransientModel):
     _inherit = 'account.config.settings'
 
     vat_check_vies = fields.Boolean(related='company_id.vat_check_vies',
-        string='VIES VAT Check')
+        string='Verify VAT Numbers')
