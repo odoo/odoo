acf3cae9bfc13b24b221fa6f8a783e245001a7fe
Akash Bhavsar <abh@odoo.com>
2017-08-09 18:53:55 +0530

    [IMP] account : Code Imporvement

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -57,7 +57,7 @@ class account_abstract_payment(models.AbstractModel):
     @api.constrains('amount')
     def _check_amount(self):
         if self.amount < 0:
-            raise ValidationError(_('The payment amount must be strictly non-negative.'))
+            raise ValidationError(_('The payment amount cannot be negative.'))
 
     @api.multi
     @api.depends('payment_type', 'journal_id')
@@ -313,14 +313,11 @@ class account_payment(models.Model):
     def _onchange_amount(self):
         res = {}
         journal_type = ['bank', 'cash']
+        res['domain'] = {'journal_id': [('type', 'in', journal_type), self.payment_type == 'inbound' and ('at_least_one_inbound', '=', True) or ('at_least_one_outbound', '=', True)]}
         if self.amount == 0:
             journal_type.append('general')
             self.payment_difference_handling = 'reconcile'
-            self.journal_id = self.env['account.journal'].search([('type', '=', 'bank')], limit=1)
-        res['domain'] = {'journal_id': [
-            ('type', 'in', journal_type),
-            self.payment_type == 'inbound' and ('at_least_one_inbound', '=', True) or ('at_least_one_outbound', '=', True)
-        ]}
+            res['domain'] = {'journal_id': [('type', 'in', journal_type)]}
         return res
 
     @api.one
