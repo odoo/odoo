2bd4a1b01f13aaf9f18719c4ba9dc82db7e8063e
rar-odoo <rar@odoo.com>
2017-08-17 16:30:48 +0200

    [FIX] gamification: challenge winner (#18901)
    In the curent behaviour, the users of the challenge are ordered
    regardless of the goal definition (higher wins or lower wins).
    This impacts the report email with the podium of users and always puts higher on top.
    
    This fix changes the ordering depending on the goal definition type.

--- addons/gamification/models/challenge.py
+++ addons/gamification/models/challenge.py
@@ -503,8 +503,12 @@ class Challenge(models.Model):
 
             line_data['own_goal_id'] = False,
             line_data['goals'] = []
-
-            goals = Goals.search(domain, order="completeness desc, current desc")
+            if line.condition=='higher':
+                goals = Goals.search(domain, order="completeness desc, current desc")
+            else:
+                goals = Goals.search(domain, order="completeness desc, current asc")
+            import ipdb
+            ipdb.set_trace()
             for ranking, goal in enumerate(goals):
                 if user and goal.user_id == user:
                     line_data['own_goal_id'] = goal.id
