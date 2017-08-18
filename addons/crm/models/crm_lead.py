9b6f42e48a52552f1d2316856029385784897816
Dhawal Limbuwala <dli@odoo.com>
2017-08-18 16:43:12 +0200

    [IMP] crm,hr_recruitment,project: add 'Create and Edit' btn
    This commit adds a 'Create and Edit' button in the footer of the
    dialog opened when creating a record from the Kanban of several
    models. Clicking on this button creates the record and directly
    opens it in a more detailled form view in edit mode.

--- addons/crm/models/crm_lead.py
+++ addons/crm/models/crm_lead.py
@@ -421,6 +421,18 @@ class Lead(models.Model):
     def close_dialog(self):
         return {'type': 'ir.actions.act_window_close'}
 
+    @api.multi
+    def edit_dialog(self):
+        form_view = self.env.ref('crm.crm_case_form_view_oppor')
+        return {
+            'name': _('Opportunity'),
+            'res_model': 'crm.lead',
+            'res_id': self.id,
+            'views': [(form_view.id, 'form'),],
+            'type': 'ir.actions.act_window',
+            'target': 'inline'
+        }
+
     # ----------------------------------------
     # Business Methods
     # ----------------------------------------
