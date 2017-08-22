3c0cb264e107dffa5b2ae799be309ae92cd4b10b
Olivier Colson <oco@odoo.com>
2017-08-22 15:09:15 +0200

    qdp's review

--- odoo/tools/xml_utils.py
+++ odoo/tools/xml_utils.py
@@ -15,30 +15,33 @@ def check_with_xsd(tree_or_str, xsd_path):
         from odoo.exceptions import UserError
         raise UserError('\n'.join(str(e) for e in xml_errors.error_log))
 
-def xml_node_chain(first_parent_node, tags_list, str_value=None, return_node=None):
+def create_xml_node_chain(first_parent_node, nodes_list, last_node_value=None, return_node=None):
     """ Utility function for generating XML files nodes. Generates as a hierarchical
     chain of nodes (each node is the son of the previous one) the tags contained
-    in tags_list as children of node first_parent_node (previously created by
-    etree), setting the value of the last of these nodes to str_value if it is
+    in nodes_list as children of node first_parent_node (previously created by
+    etree), setting the value of the last of these nodes to last_node_value if it is
     specified. This function returns the created node whose tag is equal no
     return_node parameter, or None, if this parameter is None or does not
     match any node.
     """
     rslt = None
     current_node = first_parent_node
-    for tag in tags_list:
+    for tag in nodes_list:
         current_node = etree.SubElement(current_node, tag)
         if current_node.tag == return_node: #And so, cannot be None
             rslt = current_node
 
-    if str_value:
-        current_node.text = str_value
+    if last_node_value is not None:
+        current_node.text = last_node_value
 
     return rslt
 
-def xml_node(parent_node, tag, str_value=None):
+def create_xml_node(parent_node, node_name, node_value=None):
     """ Utility function for managing XML. It creates a new node with the specified
-    tag as a child of the previously generated parent_node node. It also gives it
-    str_value as its value, if this parameter is specified.
+    node_name as a child of given parent_node node and assigns it str_value as its
+    value.
+    :param parent_node: valid etree Element
+    :param node_name: string
+    :param node_value: string
     """
-    return xml_node_chain(parent_node, [tag], str_value, tag)
+    return create_xml_node_chain(parent_node, [node_name], node_value, node_name)
