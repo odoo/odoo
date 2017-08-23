7f180207b10108fb933a0e9b8bfe5d738b47823c
Haresh Shyara <hsh@odoo.com>
2017-08-23 18:57:48 +0530

    [IMP] stock: improve reception form view
    - Invisible 'Shipping Policy' and 'Priority' for Reception.
    - Invisible Operation Type when it's in the context.
    - Changed shipping policy 'Partial' to 'As soon as possible' and 'All at once' to 'When all products are ready'.

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -187,7 +187,7 @@ class Picking(models.Model):
         help="If this shipment was split, then this field links to the shipment which contains the already processed part.")
 
     move_type = fields.Selection([
-        ('direct', 'Partial'), ('one', 'All at once')], 'Shipping Policy',
+        ('direct', 'As soon as possible'), ('one', 'When all products are ready')], 'Shipping Policy',
         default='direct', required=True,
         states={'done': [('readonly', True)], 'cancel': [('readonly', True)]},
         help="It specifies goods to be deliver partially or all at once")
