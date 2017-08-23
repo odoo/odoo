18e93b4b799223d7526281e7c7764cbeb3fee280
qdp-odoo <qdp@odoo.com>
2017-08-23 10:06:41 +0200

    [REF] code review

--- odoo/tools/xml_utils.py
+++ odoo/tools/xml_utils.py
@@ -15,33 +15,28 @@ def check_with_xsd(tree_or_str, xsd_path):
         from odoo.exceptions import UserError
         raise UserError('\n'.join(str(e) for e in xml_errors.error_log))
 
-def create_xml_node_chain(first_parent_node, nodes_list, last_node_value=None, return_node=None):
+def create_xml_node_chain(first_parent_node, nodes_list, last_node_value=None):
     """ Utility function for generating XML files nodes. Generates as a hierarchical
-    chain of nodes (each node is the son of the previous one) the tags contained
-    in nodes_list as children of node first_parent_node (previously created by
-    etree), setting the value of the last of these nodes to last_node_value if it is
-    specified. This function returns the created node whose tag is equal no
-    return_node parameter, or None, if this parameter is None or does not
-    match any node.
+    chain of nodes (each new node being the son of the previous one) based on the tags
+    contained in `nodes_list`, under the given node `first_parent_node`.
+    It will also set the value of the last of these nodes to `last_node_value` if it is
+    specified. This function returns the list of created nodes.
     """
-    rslt = None
+    res = []
     current_node = first_parent_node
     for tag in nodes_list:
         current_node = etree.SubElement(current_node, tag)
-        if current_node.tag == return_node: #And so, cannot be None
-            rslt = current_node
+        res.append(current_node)
 
     if last_node_value is not None:
         current_node.text = last_node_value
-
-    return rslt
+    return res
 
 def create_xml_node(parent_node, node_name, node_value=None):
     """ Utility function for managing XML. It creates a new node with the specified
-    node_name as a child of given parent_node node and assigns it str_value as its
-    value.
+    `node_name` as a child of given `parent_node` and assigns it `node_value` as value.
     :param parent_node: valid etree Element
     :param node_name: string
     :param node_value: string
     """
-    return create_xml_node_chain(parent_node, [node_name], node_value, node_name)
+    return create_xml_node_chain(parent_node, [node_name], node_value, node_name)[0]
