af54c77a6e5081f015f59ff93dac70bbe60345d0
Olivier Colson <oco@odoo.com>
2017-08-25 11:36:47 +0200

    [IMP] account: add a test for the "inital balances" step of the setup bar

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -14,5 +14,6 @@ from . import test_payment
 from . import test_product_id_change
 from . import test_reconciliation
 from . import test_search
+from . import test_setup_bar
 from . import test_tax
 from . import test_templates_consistency
