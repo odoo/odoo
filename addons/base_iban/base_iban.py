6652d82351414e2dd92e464bb8e389a6bd48a4e1
Yuri Quintana Pacheco <yuriqp@gmail.com>
2017-08-25 10:23:10 +0200

    Update base_iban.py
    The iban validation was failing for the single string '1'.
    
    I brought the validation function used in newer odoo version, as well as the templates iban dictionary.

--- addons/base_iban/base_iban.py
+++ addons/base_iban/base_iban.py
@@ -18,44 +18,81 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+import re
 import string
 
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
 
 # Reference Examples of IBAN
-_ref_iban = { 'al':'ALkk BBBS SSSK CCCC CCCC CCCC CCCC', 'ad':'ADkk BBBB SSSS CCCC CCCC CCCC',
-'at':'ATkk BBBB BCCC CCCC CCCC', 'be': 'BEkk BBBC CCCC CCKK', 'ba': 'BAkk BBBS SSCC CCCC CCKK',
-'bg': 'BGkk BBBB SSSS DDCC CCCC CC', 'bh': 'BHkk BBBB SSSS SSSS SSSS SS',
-'cr': 'CRkk BBBC CCCC CCCC CCCC C',
-'hr': 'HRkk BBBB BBBC CCCC CCCC C', 'cy': 'CYkk BBBS SSSS CCCC CCCC CCCC CCCC',
-'cz': 'CZkk BBBB SSSS SSCC CCCC CCCC', 'dk': 'DKkk BBBB CCCC CCCC CC',
-'do': 'DOkk BBBB CCCC CCCC CCCC CCCC CCCC',
- 'ee': 'EEkk BBSS CCCC CCCC CCCK', 'fo': 'FOkk CCCC CCCC CCCC CC',
- 'fi': 'FIkk BBBB BBCC CCCC CK', 'fr': 'FRkk BBBB BGGG GGCC CCCC CCCC CKK',
- 'ge': 'GEkk BBCC CCCC CCCC CCCC CC', 'de': 'DEkk BBBB BBBB CCCC CCCC CC',
- 'gi': 'GIkk BBBB CCCC CCCC CCCC CCC', 'gr': 'GRkk BBBS SSSC CCCC CCCC CCCC CCC',
- 'gl': 'GLkk BBBB CCCC CCCC CC', 'hu': 'HUkk BBBS SSSC CCCC CCCC CCCC CCCC',
- 'is':'ISkk BBBB SSCC CCCC XXXX XXXX XX', 'ie': 'IEkk BBBB SSSS SSCC CCCC CC',
- 'il': 'ILkk BBBS SSCC CCCC CCCC CCC', 'it': 'ITkk KBBB BBSS SSSC CCCC CCCC CCC',
- 'kz': 'KZkk BBBC CCCC CCCC CCCC', 'kw': 'KWkk BBBB CCCC CCCC CCCC CCCC CCCC CC',
- 'lv': 'LVkk BBBB CCCC CCCC CCCC C',
-'lb': 'LBkk BBBB CCCC CCCC CCCC CCCC CCCC', 'li': 'LIkk BBBB BCCC CCCC CCCC C',
-'lt': 'LTkk BBBB BCCC CCCC CCCC', 'lu': 'LUkk BBBC CCCC CCCC CCCC' ,
-'mk': 'MKkk BBBC CCCC CCCC CKK', 'mt': 'MTkk BBBB SSSS SCCC CCCC CCCC CCCC CCC',
-'mr': 'MRkk BBBB BSSS SSCC CCCC CCCC CKK',
-'mu': 'MUkk BBBB BBSS CCCC CCCC CCCC CCCC CC', 'mc': 'MCkk BBBB BGGG GGCC CCCC CCCC CKK',
-'me': 'MEkk BBBC CCCC CCCC CCCC KK',
-'nl': 'NLkk BBBB CCCC CCCC CC', 'no': 'NOkk BBBB CCCC CCK',
-'pl':'PLkk BBBS SSSK CCCC CCCC CCCC CCCC',
-'pt': 'PTkk BBBB SSSS CCCC CCCC CCCK K', 'ro': 'ROkk BBBB CCCC CCCC CCCC CCCC',
-'sm': 'SMkk KBBB BBSS SSSC CCCC CCCC CCC', 'sa': 'SAkk BBCC CCCC CCCC CCCC CCCC',
-'rs': 'RSkk BBBC CCCC CCCC CCCC KK', 'sk': 'SKkk BBBB SSSS SSCC CCCC CCCC',
-'si': 'SIkk BBSS SCCC CCCC CKK', 'es': 'ESkk BBBB SSSS KKCC CCCC CCCC',
-'se': 'SEkk BBBB CCCC CCCC CCCC CCCC', 'ch': 'CHkk BBBB BCCC CCCC CCCC C',
-'tn': 'TNkk BBSS SCCC CCCC CCCC CCCC', 'tr': 'TRkk BBBB BRCC CCCC CCCC CCCC CC',
-'ae': 'AEkk BBBC CCCC CCCC CCCC CCC',
-'gb': 'GBkk BBBB SSSS SSCC CCCC CC',
+_ref_iban = { 
+    'ad': 'ADkk BBBB SSSS CCCC CCCC CCCC',  # Andorra
+    'ae': 'AEkk BBBC CCCC CCCC CCCC CCC',  # United Arab Emirates
+    'al': 'ALkk BBBS SSSK CCCC CCCC CCCC CCCC',  # Albania
+    'at': 'ATkk BBBB BCCC CCCC CCCC',  # Austria
+    'az': 'AZkk BBBB CCCC CCCC CCCC CCCC CCCC',  # Azerbaijan
+    'ba': 'BAkk BBBS SSCC CCCC CCKK',  # Bosnia and Herzegovina
+    'be': 'BEkk BBBC CCCC CCXX',  # Belgium
+    'bg': 'BGkk BBBB SSSS DDCC CCCC CC',  # Bulgaria
+    'bh': 'BHkk BBBB CCCC CCCC CCCC CC',  # Bahrain
+    'br': 'BRkk BBBB BBBB SSSS SCCC CCCC CCCT N',  # Brazil
+    'ch': 'CHkk BBBB BCCC CCCC CCCC C',  # Switzerland
+    'cr': 'CRkk BBBC CCCC CCCC CCCC C',  # Costa Rica
+    'cy': 'CYkk BBBS SSSS CCCC CCCC CCCC CCCC',  # Cyprus
+    'cz': 'CZkk BBBB SSSS SSCC CCCC CCCC',  # Czech Republic
+    'de': 'DEkk BBBB BBBB CCCC CCCC CC',  # Germany
+    'dk': 'DKkk BBBB CCCC CCCC CC',  # Denmark
+    'do': 'DOkk BBBB CCCC CCCC CCCC CCCC CCCC',  # Dominican Republic
+    'ee': 'EEkk BBSS CCCC CCCC CCCK',  # Estonia
+    'es': 'ESkk BBBB SSSS KKCC CCCC CCCC',  # Spain
+    'fi': 'FIkk BBBB BBCC CCCC CK',  # Finland
+    'fo': 'FOkk CCCC CCCC CCCC CC',  # Faroe Islands
+    'fr': 'FRkk BBBB BGGG GGCC CCCC CCCC CKK',  # France
+    'gb': 'GBkk BBBB SSSS SSCC CCCC CC',  # United Kingdom
+    'ge': 'GEkk BBCC CCCC CCCC CCCC CC',  # Georgia
+    'gi': 'GIkk BBBB CCCC CCCC CCCC CCC',  # Gibraltar
+    'gl': 'GLkk BBBB CCCC CCCC CC',  # Greenland
+    'gr': 'GRkk BBBS SSSC CCCC CCCC CCCC CCC',  # Greece
+    'gt': 'GTkk BBBB MMTT CCCC CCCC CCCC CCCC',  # Guatemala
+    'hr': 'HRkk BBBB BBBC CCCC CCCC C',  # Croatia
+    'hu': 'HUkk BBBS SSSC CCCC CCCC CCCC CCCC',  # Hungary
+    'ie': 'IEkk BBBB SSSS SSCC CCCC CC',  # Ireland
+    'il': 'ILkk BBBS SSCC CCCC CCCC CCC',  # Israel
+    'is': 'ISkk BBBB SSCC CCCC XXXX XXXX XX',  # Iceland
+    'it': 'ITkk KBBB BBSS SSSC CCCC CCCC CCC',  # Italy
+    'jo': 'JOkk BBBB NNNN CCCC CCCC CCCC CCCC CC',  # Jordan
+    'kw': 'KWkk BBBB CCCC CCCC CCCC CCCC CCCC CC',  # Kuwait
+    'kz': 'KZkk BBBC CCCC CCCC CCCC',  # Kazakhstan
+    'lb': 'LBkk BBBB CCCC CCCC CCCC CCCC CCCC',  # Lebanon
+    'li': 'LIkk BBBB BCCC CCCC CCCC C',  # Liechtenstein
+    'lt': 'LTkk BBBB BCCC CCCC CCCC',  # Lithuania
+    'lu': 'LUkk BBBC CCCC CCCC CCCC',  # Luxembourg
+    'lv': 'LVkk BBBB CCCC CCCC CCCC C',  # Latvia
+    'mc': 'MCkk BBBB BGGG GGCC CCCC CCCC CKK',  # Monaco
+    'md': 'MDkk BBCC CCCC CCCC CCCC CCCC',  # Moldova
+    'me': 'MEkk BBBC CCCC CCCC CCCC KK',  # Montenegro
+    'mk': 'MKkk BBBC CCCC CCCC CKK',  # Macedonia
+    'mr': 'MRkk BBBB BSSS SSCC CCCC CCCC CKK',  # Mauritania
+    'mt': 'MTkk BBBB SSSS SCCC CCCC CCCC CCCC CCC',  # Malta
+    'mu': 'MUkk BBBB BBSS CCCC CCCC CCCC CCCC CC',  # Mauritius
+    'nl': 'NLkk BBBB CCCC CCCC CC',  # Netherlands
+    'no': 'NOkk BBBB CCCC CCK',  # Norway
+    'pk': 'PKkk BBBB CCCC CCCC CCCC CCCC',  # Pakistan
+    'pl': 'PLkk BBBS SSSK CCCC CCCC CCCC CCCC',  # Poland
+    'ps': 'PSkk BBBB XXXX XXXX XCCC CCCC CCCC C',  # Palestinian
+    'pt': 'PTkk BBBB SSSS CCCC CCCC CCCK K',  # Portugal
+    'qa': 'QAkk BBBB CCCC CCCC CCCC CCCC CCCC C',  # Qatar
+    'ro': 'ROkk BBBB CCCC CCCC CCCC CCCC',  # Romania
+    'rs': 'RSkk BBBC CCCC CCCC CCCC KK',  # Serbia
+    'sa': 'SAkk BBCC CCCC CCCC CCCC CCCC',  # Saudi Arabia
+    'se': 'SEkk BBBB CCCC CCCC CCCC CCCC',  # Sweden
+    'si': 'SIkk BBSS SCCC CCCC CKK',  # Slovenia
+    'sk': 'SKkk BBBB SSSS SSCC CCCC CCCC',  # Slovakia
+    'sm': 'SMkk KBBB BBSS SSSC CCCC CCCC CCC',  # San Marino
+    'tn': 'TNkk BBSS SCCC CCCC CCCC CCCC',  # Tunisia
+    'tr': 'TRkk BBBB BRCC CCCC CCCC CCCC CC',  # Turkey
+    'vg': 'VGkk BBBB CCCC CCCC CCCC CCCC',  # Virgin Islands
+    'xk': 'XKkk BBBB CCCC CCCC CCCC',  # Kosovo
 }
 
 def _format_iban(iban_str):
@@ -68,6 +105,28 @@ def _format_iban(iban_str):
             if char.isalnum():
                 res += char.upper()
     return res
+
+def normalize_iban(iban):
+    return re.sub('[\W_]', '', iban or '')
+
+def validate_iban(iban):
+    iban = normalize_iban(iban)
+    if not iban:
+        return False
+
+    country_code = iban[:2].lower()
+
+    if country_code not in _ref_iban:
+        return False
+
+    iban_template = _ref_iban[country_code]
+    if len(iban) != len(iban_template.replace(' ', '')):
+        return False
+
+    check_chars = iban[4:] + iban[:4]
+    # BASE 36: 0..9,A..Z -> 0..35
+    digits = int(''.join(str(int(char, 36)) for char in check_chars))
+    return digits % 97 == 1
 
 def _pretty_iban(iban_str):
     "return iban_str in groups of four characters separated by a single space"
@@ -99,22 +158,7 @@ class res_partner_bank(osv.osv):
             @param iban: IBAN as string
             @return: True if IBAN is valid, False otherwise
         """
-        if not iban:
-            return False
-        iban = _format_iban(iban).lower()
-        if iban[:2] in _ref_iban and len(iban) != len(_format_iban(_ref_iban[iban[:2]])):
-            return False
-        #the four first digits have to be shifted to the end
-        iban = iban[4:] + iban[:4]
-        #letters have to be transformed into numbers (a = 10, b = 11, ...)
-        iban2 = ""
-        for char in iban:
-            if char.isalpha():
-                iban2 += str(ord(char)-87)
-            else:
-                iban2 += char
-        #iban is correct if modulo 97 == 1
-        return int(iban2) % 97 == 1
+        return validate_iban(iban)
 
     def check_iban(self, cr, uid, ids, context=None):
         '''
