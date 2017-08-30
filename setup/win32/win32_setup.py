615d62adc397a8452e8b35da638b4ddc63663f24
Christophe Monniez <moc@odoo.com>
2017-08-30 08:45:28 +0200

    [IMP] setup: add build support for py2exe 0.9.2.2
    Purpose: Odoo now supports python3. This commit adds the base
    support to build Odoo for win32 with python 3.4.
    
    A new win32 build image was created for nightly.

--- setup/win32/win32_setup.py
+++ setup/win32/win32_setup.py
@@ -17,14 +17,16 @@ def generate_files():
 
     files = []
     if os.name == 'nt':
-        files.append(("Microsoft.VC90.CRT", glob.glob('C:\Microsoft.VC90.CRT\*.*')))
+        files.append(('vcredist', ['c:\\vcredist_x86.exe', ]))
+    services_files = []
     for action, steps in actions.items():
         fname = action + '.bat'
-        files.append(fname)
+        services_files.append(fname)
         with open(fname, 'w') as fp:
             fp.write('@PATH=%WINDIR%\system32;%WINDIR%;%WINDIR%\System32\Wbem;.\n')
             for step in steps:
                 fp.write('@net %s %s\n' % (step, nt_service_name))
+    files.append(('services_files', services_files))
     return files
 
 setup(
