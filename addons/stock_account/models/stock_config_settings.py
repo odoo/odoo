b19f22cb975188866b0838f78ee878df036e7e56
Christophe Simonis <chs@odoo.com>
2017-08-31 00:28:01 +0200

    [FIX] stock_account: correct syntax error introduced during previous forward-port

--- addons/stock_account/models/stock_config_settings.py
+++ addons/stock_account/models/stock_config_settings.py
@@ -9,6 +9,3 @@ class StockConfigSettings(models.TransientModel):
 
     module_stock_landed_costs = fields.Boolean("Landed Costs",
         help="Affect landed costs on receipt operations and split them among products to update their cost price.")
-
-        if self.user_has_groups('stock.group_stock_manager') and self.user_has_groups('base.group_system'):
-            self = self.sudo()
