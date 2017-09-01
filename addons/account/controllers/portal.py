4b746b3b9b2e214ad05c193c9b846101ff6aa2fd
Thibault Delavallée <tde@odoo.com>
2017-09-01 13:13:45 +0200

    [REF] account: factorize portal code to ease future additions
     * get invoice access right check in its own method. Purpose is to allow
       its use in other methods and avoid writing same code several times;
     * factorize invoice customer view values computation. Purpose is to allow
       adding values when displaying the page through inheritance. For example
       to add available payment means in sale_payment;

--- addons/account/controllers/portal.py
+++ addons/account/controllers/portal.py
@@ -27,6 +27,26 @@ class PortalAccount(CustomerPortal):
     # My Invoices
     # ------------------------------------------------------------
 
+    def _invoice_check_access(self, invoice_id, access_token=None):
+        invoice = request.env['account.invoice'].browse([invoice_id])
+        invoice_sudo = invoice.sudo()
+        try:
+            invoice.check_access_rights('read')
+            invoice.check_access_rule('read')
+        except AccessError:
+            if not access_token or not consteq(invoice_sudo.access_token, access_token):
+                raise
+        return invoice_sudo
+
+    def _invoice_get_page_view_values(self, invoice, access_token, **kwargs):
+        values = {
+            'page_name': 'invoice',
+            'invoice': invoice,
+        }
+        if access_token:
+            values['no_breadcrumbs'] = True
+        return values
+
     @http.route(['/my/invoices', '/my/invoices/page/<int:page>'], type='http', auth="user", website=True)
     def portal_my_invoices(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
         values = self._prepare_portal_layout_values()
@@ -80,33 +100,24 @@ class PortalAccount(CustomerPortal):
 
     @http.route(['/my/invoices/<int:invoice_id>'], type='http', auth="public", website=True)
     def portal_my_invoice_detail(self, invoice_id, access_token=None, **kw):
-        invoice = request.env['account.invoice'].browse(invoice_id)
         try:
-            invoice.check_access_rights('read')
-            invoice.check_access_rule('read')
+            invoice_sudo = self._invoice_check_access(invoice_id, access_token)
         except AccessError:
-            if not access_token or not consteq(invoice.sudo().access_token, access_token):
-                return request.redirect('/my')
+            return request.redirect('/my')
 
-        values = {
-            'page_name': 'invoice',
-            'invoice': invoice.sudo(),
-        }
+        values = self._invoice_get_page_view_values(invoice_sudo, access_token, **kw)
         return request.render("account.portal_invoice_page", values)
 
     @http.route(['/my/invoices/pdf/<int:invoice_id>'], type='http', auth="public", website=True)
     def portal_my_invoice_report(self, invoice_id, access_token=None, **kw):
-        invoice = request.env['account.invoice'].browse(invoice_id)
         try:
-            invoice.check_access_rights('read')
-            invoice.check_access_rule('read')
+            invoice_sudo = self._invoice_check_access(invoice_id, access_token)
         except AccessError:
-            if not access_token or not consteq(invoice.sudo().access_token, access_token):
-                return request.redirect('/my')
+            return request.redirect('/my')
 
         # print report as sudo, since it require access to taxes, payment term, ... and portal
         # does not have those access rights.
-        pdf = request.env.ref('account.account_invoices').sudo().render_qweb_pdf([invoice_id])[0]
+        pdf = request.env.ref('account.account_invoices').sudo().render_qweb_pdf([invoice_sudo.id])[0]
         pdfhttpheaders = [
             ('Content-Type', 'application/pdf'),
             ('Content-Length', len(pdf)),
