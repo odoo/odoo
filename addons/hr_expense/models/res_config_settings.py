6b093df232947645c252137eef494152a8b96f63
Yannick Tivisse <yti@odoo.com>
2017-09-04 11:40:58 +0200

    [FIX] crm,hr_expense: Give 2 different names for alias prefix
    Purpose
    =======
    
    Now that the res.config model have been merged into a single model, the 2 alias prefixes should have different names to avoid conflicts.

--- addons/hr_expense/models/res_config_settings.py
+++ addons/hr_expense/models/res_config_settings.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    alias_prefix = fields.Char('Default Alias Name for Expenses')
+    expense_alias_prefix = fields.Char('Default Alias Name for Expenses')
     alias_domain = fields.Char('Alias Domain', default=lambda self: self.env["ir.config_parameter"].sudo().get_param("mail.catchall.domain"))
     use_mailgateway = fields.Boolean(string='Let your employees record expenses by email')
     module_sale = fields.Boolean(string="Customer Billing")
@@ -15,7 +15,7 @@ class ResConfigSettings(models.TransientModel):
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
         res.update(
-            alias_prefix=self.env.ref('hr_expense.mail_alias_expense').alias_name,
+            expense_alias_prefix=self.env.ref('hr_expense.mail_alias_expense').alias_name,
             use_mailgateway=self.env['ir.config_parameter'].sudo().get_param('hr_expense.use_mailgateway'),
         )
         return res
@@ -23,10 +23,10 @@ class ResConfigSettings(models.TransientModel):
     @api.multi
     def set_values(self):
         super(ResConfigSettings, self).set_values()
-        self.env.ref('hr_expense.mail_alias_expense').write({'alias_name': self.alias_prefix})
+        self.env.ref('hr_expense.mail_alias_expense').write({'alias_name': self.expense_alias_prefix})
         self.env['ir.config_parameter'].sudo().set_param('hr_expense.use_mailgateway', self.use_mailgateway)
 
     @api.onchange('use_mailgateway')
     def _onchange_use_mailgateway(self):
         if not self.use_mailgateway:
-            self.alias_prefix = False
+            self.expense_alias_prefix = False
