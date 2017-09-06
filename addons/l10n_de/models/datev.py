555a7848647341b3bb87f78cc8a34ac118ef0957
Cedric Snauwaert <csn@openerp.com>
2017-09-06 15:37:29 +0200

    pr comments

--- addons/l10n_de/models/datev.py
+++ addons/l10n_de/models/datev.py
@@ -3,14 +3,14 @@ from odoo import fields, models
 class AccountTaxTemplate(models.Model):
     _inherit = 'account.tax.template'
 
-    datev_correction_code = fields.Char(size=2)
+    l10n_de_datev_code = fields.Char(size=2)
 
     def _get_tax_vals(self, company, tax_template_to_tax):
     	vals = super(AccountTaxTemplate, self)._get_tax_vals(company, tax_template_to_tax)
-    	vals['datev_correction_code'] = self.datev_correction_code
+    	vals['l10n_de_datev_code'] = self.l10n_de_datev_code
     	return vals
 
 class AccountTax(models.Model):
 	_inherit = "account.tax"
 
-	datev_correction_code = fields.Char(size=2, help="2 digits code use by Datev")
+	l10n_de_datev_code = fields.Char(size=2, help="2 digits code use by Datev")
