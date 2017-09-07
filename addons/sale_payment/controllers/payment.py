5684c173012cfc5b0596d8cc82984b1a8c55ec71
Thibault Delavallée <tde@odoo.com>
2017-09-07 18:25:25 +0200

    [FIX] sale_payment: sudo tx creation as anonymous users may access it

--- addons/sale_payment/controllers/payment.py
+++ addons/sale_payment/controllers/payment.py
@@ -30,7 +30,7 @@ class PaymentPortal(http.Controller):
             return False
 
         token = request.env['payment.token'].sudo()  # currently no support of payment tokens
-        tx = request.env['payment.transaction']._check_or_create_sale_tx(
+        tx = request.env['payment.transaction'].sudo()._check_or_create_sale_tx(
             order_sudo,
             acquirer,
             payment_token=token,
