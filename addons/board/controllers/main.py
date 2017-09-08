ca496c21989d92b0e1d717faa4e834326188e546
David Monjoie <dmo@odoo.com>
2017-09-08 15:34:15 +0200

    [FIX] board: fix lxml import after P3 migration
    Encoding 'unicode' is not supported by xml.etree.Elementree.toString
    but it is supported by lxml.etree.toString

--- addons/board/controllers/main.py
+++ addons/board/controllers/main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from xml.etree import ElementTree
+from lxml import etree as ElementTree
 
 from odoo.http import Controller, route, request
 
