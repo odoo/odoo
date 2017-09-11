692f3c65fd6d1019ea597fffa2af4d7d384dbb56
Mitali Patel <mpa@odoo.com>
2017-09-11 18:03:47 +0530

    [IMP] web_settings_dashboard: qunit testcase code improvement

--- addons/web_settings_dashboard/static/tests/dashboard_tests.js
+++ addons/web_settings_dashboard/static/tests/dashboard_tests.js
@@ -3,20 +3,32 @@ odoo.define('web_settings_dashboard.dashboard_tests', function (require) {
 
 var testUtils = require('web.test_utils');
 var Dashboard = require('web_settings_dashboard');
-var Widget = require('web.Widget');
 
 QUnit.module('Settings', {
     beforeEach: function() {
-        this.dashboardData = {
-            'active_users': 1,
-            'pending_counts': 1,
-            'pending_users': [{'id': 1, 'email': 'xyz@odoo.com'}],
+        var self = this;
+        this.data = {
+            user: {
+                fields: {
+                    email: {string: 'Email', type: 'char'},
+                },
+                records: [
+                    {id: 1, email: 'chhagan@odoo.com'},
+                    {id: 2, email: 'magan@odoo.com'},
+                ]
+            },
         };
     }
 }, function () {
     QUnit.test('Dashboard: Invite new user', function (assert) {
-        assert.expect(1);
+        assert.expect(2);
+
         var self = this;
+        var dashboardData = {
+            'active_users': this.data.user.records.length,
+            'pending_counts': 0,
+            'pending_users': [],
+        };
 
         var parentDashboard = new Dashboard.Dashboard();
         testUtils.addMockEnvironment(parentDashboard, {
@@ -25,32 +37,47 @@ QUnit.module('Settings', {
                     return $.when({
                         'apps': {},
                         'share': {},
-                        'users_info': self.dashboardData,
+                        'users_info': dashboardData,
                     });
                 }
                 return this._super(route, args);
             },
         });
 
-        var invitationsDashboard = new Dashboard.DashboardInvitations(parentDashboard, this.dashboardData);
-        testUtils.addMockEnvironment(invitationsDashboard, {
+        this.invitationsDashboard = new Dashboard.DashboardInvitations(parentDashboard, dashboardData);
+        testUtils.addMockEnvironment(this.invitationsDashboard, {
             mockRPC: function (route, args) {
                 if (args.method === 'web_dashboard_create_users') {
-                    self.dashboardData.active_users++;
-                    self.dashboardData.pending_counts++;
-                    self.dashboardData.pending_users.push({'id': 2, 'email': args['args'][0][0]});
+                    var id = self.data.user.records.length + 1;
+                    var email = args['args'][0][0];
+                    var user = {'id': id, 'email': email};
+                    if (!_.contains(_.pluck(self.data.user.records, 'email'), email)) {
+                        dashboardData.active_users++;
+                        dashboardData.pending_counts++;
+                        dashboardData.pending_users.push(user);
+                        self.data.user.records.push(user);
+                    }
                     return $.when();
                 }
                 return this._super(route, args);
             },
         });
-        invitationsDashboard.appendTo($('#qunit-fixture'));
+        this.invitationsDashboard.appendTo($('#qunit-fixture'));
 
-        var inputBox = invitationsDashboard.$el.find('#user_emails').val('abc@odoo.com');
+        var inputBox = this.invitationsDashboard.$el.find('#user_emails');
         var keyPressEvent = $.Event('keypress', {'keyCode': 13});
+
+        inputBox.val('lagan@odoo.com');
         if(inputBox.trigger(keyPressEvent)) {
-            invitationsDashboard.$el.find('.o_web_settings_dashboard_invitations').trigger('click');
-            assert.strictEqual(invitationsDashboard.data['pending_users'].length, 2, 'New user created');
+            this.invitationsDashboard.$el.find('.o_web_settings_dashboard_invitations').trigger('click');
+            assert.strictEqual(this.invitationsDashboard.data['pending_counts'], 1, 'New user created');
+        }
+
+        // Check for already exist user
+        inputBox.val('magan@odoo.com');
+        if(inputBox.trigger(keyPressEvent)) {
+            this.invitationsDashboard.$el.find('.o_web_settings_dashboard_invitations').trigger('click');
+            assert.strictEqual(this.invitationsDashboard.data['pending_counts'], 1, 'User already created');
         }
     });
 
