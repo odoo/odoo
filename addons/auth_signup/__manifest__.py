67d1173bf381befc136d840e232514c2c9ef7e1b
Sanjay Jamod <sja@odoo.com>
2017-09-11 16:22:16 +0530

    [IMP] auth_signup: Disable 'Sign Up' button to prevent user from continuous clicking

--- addons/auth_signup/__manifest__.py
+++ addons/auth_signup/__manifest__.py
@@ -20,6 +20,7 @@ Allow users to sign up and reset their password
         'views/res_config_settings_views.xml',
         'views/res_users_views.xml',
         'views/auth_signup_login_templates.xml',
+        'views/auth_signup_assets.xml',
     ],
     'bootstrap': True,
 }
