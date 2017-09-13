6a0a915cc7f965189a85f3155539fb738b649b90
Joren Van Onder <jov@odoo.com>
2017-09-13 16:37:46 -0700

    [IMP] stock: ask confirmation when validating overprocessed moves

--- addons/stock/__manifest__.py
+++ addons/stock/__manifest__.py
@@ -42,6 +42,7 @@
         'wizard/stock_scheduler_compute_views.xml',
         'wizard/stock_immediate_transfer_views.xml',
         'wizard/stock_backorder_confirmation_views.xml',
+        'wizard/stock_overprocessed_transfer_views.xml',
 
         'views/res_partner_views.xml',
         'views/product_strategy_views.xml',
