ecc75092776ca11089772a970fba9638071322d9
Lucas Perais <lcsperais@gmail.com>
2017-09-14 09:56:07 +0200

    [FIX] account, invoice analysis: negative amounts for in_invoice-like
    Before this commit, the invoice analysis report summed the amount of invoices regardless of whether they were in or out,
    as if it were an absolute sum.
    
    This commit corrects the behavior back to v9's, where in_invoice have the minus sign and are hence subtracted
    
    opw 769409. Was PR  #19413

--- addons/account/report/account_invoice_report.py
+++ addons/account/report/account_invoice_report.py
@@ -114,7 +114,7 @@ class AccountInvoiceReport(models.Model):
                     ai.type, ai.state, pt.categ_id, ai.date_due, ai.account_id, ail.account_id AS account_line_id,
                     ai.partner_bank_id,
                     SUM ((invoice_type.sign * ail.quantity) / u.factor * u2.factor) AS product_qty,
-                    SUM(ail.price_subtotal_signed) AS price_total,
+                    SUM(ail.price_subtotal_signed * invoice_type.sign) AS price_total,
                     SUM(ABS(ail.price_subtotal_signed)) / CASE
                             WHEN SUM(ail.quantity / u.factor * u2.factor) <> 0::numeric
                                THEN SUM(ail.quantity / u.factor * u2.factor)
