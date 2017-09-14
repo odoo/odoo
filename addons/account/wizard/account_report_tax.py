436cf1498c39d9e4c6856ec992480e8c5813ea67
Prakash Prajapati <ppr@odoo.com>
2017-09-14 19:28:39 +0530

    [ADD] Account: PDf Tax Report Add Accrual Basis  Method

--- addons/account/wizard/account_report_tax.py
+++ addons/account/wizard/account_report_tax.py
@@ -8,10 +8,7 @@ class AccountTaxReport(models.TransientModel):
     _name = 'account.tax.report'
     _description = 'Tax Report'
 
-    options = fields.Selection([('accrual_basis', 'Accrual Basis Method'), ('cash_basis', 'Cash Basis Method')], default='accrual_basis', string='Options')
-
     def _print_report(self, data):
         data = self.pre_print_report(data)
-        data['form'].update(self.read(['options'])[0])
         records = self.env[data['model']].browse(data.get('ids', []))
         return self.env.ref('account.action_report_account_tax').report_action(records, data=data)
