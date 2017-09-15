7acab292f9f18c07b57cf135dd898e68f636a905
Olivier Colson <oco@odoo.com>
2017-09-15 13:45:18 +0200

    [IMP] l10n_es: improves localization package
    * PYMES, non-profit associations, complete (large enterprises) CoA
    * reviewed taxes, tax groups
    * reviewed tags, account types for reports in l10n_es_reports
    
    Thanks for the contribution and the original work, spanish community (esp. Pedro M. Baeza).
    
    Was task 30669. Was PR #19154

--- addons/l10n_es/__manifest__.py
+++ addons/l10n_es/__manifest__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Copyright (c) 2008-2010 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
-#                         Jordi Esteve <jesteve@zikzakmedia.com>
-# Copyright (c) 2012-2013, Grupo OPENTIA (<http://opentia.com>) Registered EU Trademark.
-#                         Dpto. Consultoría <consultoria@opentia.es>
-# Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                    Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
+# List of contributors:
+# Jordi Esteve <jesteve@zikzakmedia.com>
+# Dpto. Consultoría Grupo Opentia <consultoria@opentia.es>
+# Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Carlos Liébana <carlos.liebana@factorlibre.com>
+# Hugo Santos <hugo.santos@factorlibre.com>
+# Albert Cabedo <albert@gafic.com>
+# Olivier Colson <oco@odoo.com>
 
 {
     "name" : "Spain - Accounting (PGCE 2008)",
@@ -23,8 +25,9 @@ Spanish charts of accounts (PGCE 2008).
         * Spanish general chart of accounts 2008 for small and medium companies
         * Spanish general chart of accounts 2008 for associations
     * Defines templates for sale and purchase VAT
-    * Defines tax code templates
+    * Defines tax templates
     * Defines fiscal positions for spanish fiscal legislation
+    * Defines tax reports mod 111, 115 and 303
 """,
     "depends" : [
         "account",
@@ -32,13 +35,15 @@ Spanish charts of accounts (PGCE 2008).
         "base_vat",
     ],
     "data" : [
-        'data/account_account_type_data.xml',
-        'data/l10n_es_chart_data.xml',
-        'data/account_account_template_data.xml',
+        'data/account_chart_template_data.xml',
+        'data/account_account_template_common_data.xml',
+        'data/account_account_template_pymes_data.xml',
+        'data/account_account_template_assoc_data.xml',
+        'data/account_account_template_full_data.xml',
+        'data/account_chart_template_account_account_link.xml',
         'data/account_data.xml',
         'data/account_tax_data.xml',
         'data/account_fiscal_position_template_data.xml',
-        'data/account_chart_template_data.xml',
         'data/account_chart_template_data.yml',
     ],
 }
