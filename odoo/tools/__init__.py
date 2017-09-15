79038055f5fd5e24acc8578fa36fc3f6e4c32893
Mitali Patel <mpa@odoo.com>
2017-09-15 11:58:32 +0200

    [IMP] base, res.currency: amount_to_text() based on num2words library
    Old implementation of tools.amount_to_text() is now deprecated. The new one support more languages
    
    Was task 27215. Was PR #16531

--- odoo/tools/__init__.py
+++ odoo/tools/__init__.py
@@ -11,8 +11,6 @@ from .convert import *
 from .translate import *
 from .graph import graph
 from .image import *
-from .amount_to_text import *
-from .amount_to_text_en import *
 from .yaml_import import *
 from .sql import *
 from .float_utils import *
