3428f652ac3ce908fe5d73c64e773942d5a5e0c6
Mitali Patel <mpa@odoo.com>
2017-09-15 11:58:32 +0200

    [IMP] base, res.currency: amount_to_text() based on num2words library
    Old implementation of tools.amount_to_text() is now deprecated. The new one support more languages
    
    Was task 27215. Was PR #16531

--- odoo/addons/base/res/res_currency.py
+++ odoo/addons/base/res/res_currency.py
@@ -2,9 +2,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import json
+import logging
 import math
 import re
 import time
+
+try:
+    from num2words import num2words
+except ImportError:
+    logging.getLogger(__name__).warning("The num2words python library is not installed, l10n_mx_edi features won't be fully available.")
+    num2words = None
 
 from odoo import api, fields, models, tools, _
 
@@ -28,6 +35,8 @@ class Currency(models.Model):
     position = fields.Selection([('after', 'After Amount'), ('before', 'Before Amount')], default='after',
         string='Symbol Position', help="Determines where the currency symbol should be placed after or before the amount.")
     date = fields.Date(compute='_compute_date')
+    currency_unit_label = fields.Char(string="Currency Unit", help="Currency Unit Name")
+    currency_subunit_label = fields.Char(string="Currency Subunit", help="Currency Subunit Name")
 
     _sql_constraints = [
         ('unique_name', 'unique (name)', 'The currency code must be unique!'),
@@ -78,6 +87,34 @@ class Currency(models.Model):
     @api.multi
     def name_get(self):
         return [(currency.id, tools.ustr(currency.name)) for currency in self]
+
+    @api.multi
+    def amount_to_text(self, amount):
+        self.ensure_one()
+        def _num2words(number, lang):
+            try:
+                return num2words(number, lang=lang).title()
+            except NotImplementedError:
+                return num2words(number, lang='en').title()
+
+        if num2words is None:
+            logging.getLogger(__name__).warning("The library 'num2words' is missing, cannot render textual amounts.")
+            return ""
+
+        fractional_value, integer_value = math.modf(amount)
+        fractional_amount = round(abs(fractional_value), self.decimal_places) * (math.pow(10, self.decimal_places))
+        lang_code = self.env.context.get('lang') or self.env.user.lang
+        lang = self.env['res.lang'].search([('code', '=', lang_code)])
+        amount_words = tools.ustr('{amt_value} {amt_word}').format(
+                        amt_value=_num2words(int(integer_value), lang=lang.iso_code),
+                        amt_word=self.currency_unit_label,
+                        )
+        if not self.is_zero(fractional_value):
+            amount_words += ' ' + _('and') + tools.ustr(' {amt_value} {amt_word}').format(
+                        amt_value=_num2words(int(fractional_amount), lang=lang.iso_code),
+                        amt_word=self.currency_subunit_label,
+                        )
+        return amount_words
 
     @api.multi
     def round(self, amount):
