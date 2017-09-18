5553e5386a3ce1e2cc1752050353bcf3e5f89e51
Christophe Monniez <moc@odoo.com>
2017-09-18 16:09:48 +0200

    [IMP] Support for building Fedora26 rpm

--- setup/redhat/postinstall.sh
+++ setup/redhat/postinstall.sh
@@ -28,7 +28,7 @@ db_host = False
 db_port = False
 db_user = $ODOO_USER
 db_password = False
-addons_path = /usr/lib/python2.7/site-packages/odoo/addons
+addons_path = /usr/lib/python3.6/site-packages/odoo/addons
 " > $ODOO_CONFIGURATION_FILE
     chown $ODOO_USER:$ODOO_GROUP $ODOO_CONFIGURATION_FILE
     chmod 0640 $ODOO_CONFIGURATION_FILE
@@ -58,4 +58,3 @@ ExecStart=/usr/bin/odoo --config $ODOO_CONFIGURATION_FILE --logfile $ODOO_LOG_FI
 [Install]
 WantedBy=multi-user.target
 EOF
-easy_install pyPdf2 vatnumber pydot psycogreen suds ofxparse XlsxWriter
