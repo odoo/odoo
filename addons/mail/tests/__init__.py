f8118d8c8b872c82e712a2f5218e0b01f7b9cb34
Thibault Delavallée <tde@odoo.com>
2017-09-18 16:55:41 +0200

    [IMP] mail: tests: clean mail message tests
    Purpose is to lessen noise and make tests less low-level. Including :
    
     * move mail_mail model specific tests to its own file;
     * move feature specific tests to its own file;
     * add missing loggers to lessen noise;

--- addons/mail/tests/__init__.py
+++ addons/mail/tests/__init__.py
@@ -2,6 +2,7 @@
 
 from . import test_mail_followers
 from . import test_mail_message
+from . import test_mail_mail
 from . import test_mail_channel
 from . import test_mail_gateway
 from . import test_mail_template
