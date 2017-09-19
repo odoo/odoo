73bec3d60feaced7d285ad062021f7703fc9e31a
Cedric Snauwaert <csn@openerp.com>
2017-09-19 12:34:54 +0200

    [FIX] account: reconciliation, error when changing partner
    Traceback by pressing enter when changing partner which should not occur as keyUp event is
    in place for other M2O fields and not for the partner.
    
    In some case, if the line is balanced (marked as reconciled) and we change the user for one that does
    not have any move lines, we removed the selected proposition but we did not changed the line balance, resulting
    in a widget that is marked as inactive and no way to do something with it.

--- addons/account/static/src/js/reconciliation/reconciliation_renderer.js
+++ addons/account/static/src/js/reconciliation/reconciliation_renderer.js
@@ -600,6 +600,10 @@ var LineRenderer = Widget.extend(FieldManagerMixin, {
      * @param {keyup event} event
      */
     _onInputKeyup: function (event) {
+        var target_partner_id = $(event.target).parents('[name="partner_id"]');
+        if (target_partner_id.length === 1) {
+            return;
+        }
         if(event.keyCode === 13) {
             if (_.findWhere(this.model.lines, {mode: 'create'}).balance.amount) {
                 this._onCreateProposition();
