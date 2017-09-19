odoo/odoo

commit 65ce40b0553ec6f2a48aef07db5c448bf0cc3d0f

[IMP] web, various: apply kanban card color on header instead of whole card
Currently applying a color to a kanban item applies it to the whole
card. This creates usability issues as the content is not dynamically
updated to match the chosen color.

This commit proposes to apply the color only to the header and let the
card content standard. It helps designing cards that are always usable
and readable.

addons/mrp/models/stock_warehouse.py
@@ -58,7 +58,7 @@ class StockWarehouse(models.Model):
             wh_stock_loc = warehouse.lot_stock_id
             seq = seq_obj.search([('code', '=', 'mrp.production')], limit=1)
             other_pick_type = picking_type_obj.search([('warehouse_id', '=', warehouse.id)], order = 'sequence desc', limit=1)
-            color = other_pick_type and other_pick_type.color or 0
+            color = other_pick_type and other_pick_type.color or 1
             max_sequence = other_pick_type and other_pick_type.sequence or 0
             manu_type = picking_type_obj.create({
                 'name': _('Manufacturing'),

addons/stock/models/stock_warehouse.py
@@ -189,7 +189,7 @@ class Warehouse(models.Model):
         # choose the next available color for the operation types of this warehouse
         all_used_colors = [res['color'] for res in PickingType.search_read([('warehouse_id', '!=', False), ('color', '!=', False)], ['color'], order='color')]
         available_colors = [zef for zef in [0, 3, 4, 5, 6, 7, 8, 1, 2] if zef not in all_used_colors]
-        color = available_colors and available_colors[0] or 0
+        color = available_colors and available_colors[0] or 1
 
         # suit for each warehouse: reception, internal, pick, pack, ship
         max_sequence = PickingType.search_read([('sequence', '!=', False)], ['sequence'], limit=1, order='sequence desc')

addons/account/models/account.py
@@ -246,6 +246,7 @@ class AccountJournal(models.Model):
     bank_acc_number = fields.Char(related='bank_account_id.acc_number')
     bank_id = fields.Many2one('res.bank', related='bank_account_id.bank_id')
 
+    color = fields.Integer("Color Index", default=1)
     _sql_constraints = [
         ('code_company_uniq', 'unique (code, name, company_id)', 'The code and name of the journal must be unique per company !'),
     ]

addons/account/views/account_journal_dashboard_view.xml
@@ -7,11 +7,12 @@
                 <field name="id"/>
                 <field name="name"/>
                 <field name="type"/>
+                <field name="color"/>
                 <field name="show_on_dashboard"/>
                 <field name="kanban_dashboard"/>
                 <templates>
                     <t t-name="kanban-box">
-                        <div t-attf-class="#{kanban_color(0)}">
+                        <div>
                             <t t-value="JSON.parse(record.kanban_dashboard.raw_value)" t-set="dashboard"/>
                             <t t-value="record.type.raw_value" t-set="journal_type"/>
                             <t t-call="JournalTop"/>
@@ -35,7 +36,7 @@
                     </t>
 
                     <t t-name="JournalTop">
-                        <div class="o_kanban_card_header">
+                        <div t-attf-class="o_kanban_card_header #{kanban_color(record.color.raw_value)}">
                             <div class="o_kanban_card_header_title">
                                 <div class="o_primary">
                                     <a type="object" name="open_action"><field name="name"/></a>
@@ -197,6 +198,12 @@
                                 <div>
                                     <a type="object" name="open_action_with_context" context="{'action_name': 'action_validate_account_move', 'search_default_journal': True}">Post All Entries</a>
                                 </div>
+                            </div>
+                        </div>
+
+                        <div t-if="widget.editable" class="o_kanban_card_manage_settings row">
+                            <div class="col-xs-8">
+                                <ul class="oe_kanban_colorpicker" data-field="color"/>
                             </div>
                         </div>
 

addons/hr/models/hr.py
@@ -273,7 +273,7 @@ class Department(models.Model):
     member_ids = fields.One2many('hr.employee', 'department_id', string='Members', readonly=True)
     jobs_ids = fields.One2many('hr.job', 'department_id', string='Jobs')
     note = fields.Text('Note')
-    color = fields.Integer('Color Index')
+    color = fields.Integer('Color Index', default=1)
 
     @api.constrains('parent_id')
     def _check_parent_id(self):

addons/hr/views/hr_views.xml
@@ -512,8 +512,8 @@
                     <field name="color"/>
                     <templates>
                         <t t-name="kanban-box">
-                            <div t-attf-class="#{kanban_color(record.color.raw_value)}">
-                                <div class="o_kanban_card_header">
+                            <div>
+                                <div t-attf-class="o_kanban_card_header #{kanban_color(record.color.raw_value)}">
                                     <div class="o_kanban_card_header_title">
                                         <div class="o_primary"><field name="name"/></div>
                                         <div class="o_secondary"><field name="company_id"/></div>

addons/hr_recruitment/models/hr_job.py
@@ -25,7 +25,7 @@ class Job(models.Model):
     alias_id = fields.Many2one(
         'mail.alias', "Alias", ondelete="restrict", required=True,
         help="Email alias for this job position. New emails will automatically create new applicants for this job position.")
-    color = fields.Integer("Color Index")
+    color = fields.Integer("Color Index", default=1)
 
     def _compute_document_ids(self):
         applicants = self.mapped('application_ids').filtered(lambda self: not self.emp_id)

addons/hr_recruitment/views/hr_job_views.xml
@@ -25,8 +25,8 @@
                 <field name="user_id"/>
                 <templates>
                     <t t-name="kanban-box">
-                        <div t-attf-class="#{kanban_color(record.color.raw_value)}">
-                            <div class="o_kanban_card_header">
+                        <div>
+                            <div t-attf-class="o_kanban_card_header #{kanban_color(record.color.raw_value)}">
                                 <div class="o_kanban_card_header_title">
                                     <div class="o_primary"><t t-esc="record.name.value"/></div>
                                     <div t-if="record.alias_name.value and record.alias_domain.value and record.state.raw_value == 'recruit'" class="o_secondary o_job_alias">

commit 12d27781f3baab2594216044fe159bda9418be2a

[FIX] mrp, stock: picking type automatic colors
/!\ Should not be forward-ported in saas-18

Fix bug introduced by https://github.com/odoo/odoo/commit/65ce40b0553ec6f2a48aef07db5c448bf0cc3d0f:
When creating picking types, a color is automatically assigned. This
color is chosen to be the first available one, or by default 1 (since
mentioned commit, as '1' would be the default gray used in kanban
headers). Unfortunately, this was expressed as `color = first or 1`...
which will always be 1 since the very first available color is always 0.

addons/mrp/models/stock_warehouse.py
@@ -58,7 +58,7 @@ class StockWarehouse(models.Model):
             wh_stock_loc = warehouse.lot_stock_id
             seq = seq_obj.search([('code', '=', 'mrp.production')], limit=1)
             other_pick_type = picking_type_obj.search([('warehouse_id', '=', warehouse.id)], order = 'sequence desc', limit=1)
-            color = other_pick_type and other_pick_type.color or 1
+            color = other_pick_type.color if other_pick_type else 1
             max_sequence = other_pick_type and other_pick_type.sequence or 0
             manu_type = picking_type_obj.create({
                 'name': _('Manufacturing'),

addons/stock/models/stock_warehouse.py
@@ -188,8 +188,8 @@ class Warehouse(models.Model):
 
         # choose the next available color for the operation types of this warehouse
         all_used_colors = [res['color'] for res in PickingType.search_read([('warehouse_id', '!=', False), ('color', '!=', False)], ['color'], order='color')]
-        available_colors = [zef for zef in [0, 3, 4, 5, 6, 7, 8, 1, 2] if zef not in all_used_colors]
-        color = available_colors and available_colors[0] or 1
+        available_colors = [zef for zef in [1, 3, 4, 5, 6, 7, 8, 0, 2] if zef not in all_used_colors]
+        color = available_colors[0] if available_colors else 1
 
         # suit for each warehouse: reception, internal, pick, pack, ship
         max_sequence = PickingType.search_read([('sequence', '!=', False)], ['sequence'], limit=1, order='sequence desc')