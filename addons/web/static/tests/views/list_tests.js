c08f7acff8cb4a6698cd8b56d27ca986bded8b24
Deep Patel <dep@odoo.com>
2017-09-19 11:35:57 +0530

    [ADD] web: add test case save data when list sorting in edit mode in list view

--- addons/web/static/tests/views/list_tests.js
+++ addons/web/static/tests/views/list_tests.js
@@ -436,6 +436,26 @@ QUnit.module('Views', {
         list.destroy();
     });
 
+    QUnit.test('editable list view: save data when list sorting in edit mode', function (assert) {
+        assert.expect(1);
+
+        this.data.foo.fields.foo.sortable = true;
+
+        var list = createView({
+            View: ListView,
+            model: 'foo',
+            data: this.data,
+            arch: '<tree editable="bottom"><field name="foo"/></tree>',
+        });
+
+        list.$('td:not(.o_list_record_selector)').first().click();
+        list.$('input[name="foo"]').val('xyz').trigger('input');
+        list.$('.o_column_sortable').click();
+        list.$buttons.find('.o_list_button_save').click();
+        assert.strictEqual(list.$buttons.hasClass('o-editing'),false,"data saved");
+        list.destroy();
+    });
+
     QUnit.test('selection changes are triggered correctly', function (assert) {
         assert.expect(8);
 
