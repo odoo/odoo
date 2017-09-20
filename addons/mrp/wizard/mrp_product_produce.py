818ef1adfa328745186e71812eb907ee010cf40c
Josse Colpaert <jco@odoo.com>
2017-09-20 15:30:42 +0200

    [FIX] mrp: should not take into account done lines in produce wizard

--- addons/mrp/wizard/mrp_product_produce.py
+++ addons/mrp/wizard/mrp_product_produce.py
@@ -38,7 +38,7 @@ class MrpProductProduce(models.TransientModel):
                 lines = []
                 for move in production.move_raw_ids.filtered(lambda x: (x.product_id.tracking != 'none') and x.state not in ('done', 'cancel') and x.bom_line_id):
                     qty_to_consume = todo_quantity / move.bom_line_id.bom_id.product_qty * move.bom_line_id.product_qty
-                    for move_line in move.move_line_ids:
+                    for move_line in move.move_line_ids.filtered(lambda ml: not ml.lot_produced_id):
                         if float_compare(qty_to_consume, 0.0, precision_rounding=move.product_uom.rounding) < 0:
                             break
                         to_consume_in_line = min(qty_to_consume, move_line.product_uom_qty)
