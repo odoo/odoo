ac0ef48607062511632867f631e7cbc228487a2e
Josse Colpaert <jco@odoo.com>
2017-09-20 13:25:39 +0200

    [IMP] mrp: add a line in the produce wizard

--- addons/mrp/wizard/mrp_product_produce.py
+++ addons/mrp/wizard/mrp_product_produce.py
@@ -141,6 +141,34 @@ class MrpProductProduce(models.TransientModel):
 
         for pl in self.produce_line_ids:
             if pl.qty_done and pl.lot_id:
+                if not pl.move_id:
+                    # Find move_id that would match
+                    move_id = self.production_id.move_raw_ids.filtered(lambda x: x.product_id == pl.product_id and x.state not in ('done', 'cancel'))
+                    if move_id:
+                        pl.move_id = move_id
+                    else:
+                        # create a move and put it in there
+                        order = self.production_id
+                        pl.move_id = self.env['stock.move'].create({
+                                    'name': order.name,
+                                    'date': order.date_planned_start,
+                                    'date_expected': order.date_planned_start,
+                                    'product_id': pl.product_id.id,
+                                    'product_uom_qty': 0.0,
+                                    'product_uom': pl.product_uom_id.id,
+                                    'location_id': order.location_src_id.id,
+                                    'location_dest_id': self.product_id.property_stock_production.id,
+                                    'raw_material_production_id': order.id,
+                                    'company_id': order.company_id.id,
+                                    'operation_id': False,
+                                    'price_unit': pl.product_id.standard_price,
+                                    'procure_method': 'make_to_stock',
+                                    'origin': order.name,
+                                    'warehouse_id': False,
+                                    'group_id': order.procurement_group_id.id,
+                                    'propagate': order.propagate,
+                                    'unit_factor': 0.0,
+                                    'state': 'confirmed'})
                 ml = pl.move_id.move_line_ids.filtered(lambda ml: ml.lot_id == pl.lot_id and not ml.lot_produced_id)
                 if ml:
                     if (ml.qty_done + pl.qty_done) >= ml.product_uom_qty:
@@ -182,3 +210,7 @@ class MrpProductProduceLine(models.TransientModel):
     def _onchange_lot_id(self):
         if self.product_id.tracking == 'serial':
             self.qty_done = 1
+            
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        self.product_uom_id = self.product_id.uom_id.id
