90afc066ce699290f30aa30d4f7bbb4643dc0573
Rémi Rahir <rar@odoo.com>
2017-09-20 17:27:57 +0200

    [FIX] Website_sale_Stock: fix frontend js
    frontend js now requires dom to be ready.

--- addons/website_sale_stock/static/src/js/website_sale_stock.js
+++ addons/website_sale_stock/static/src/js/website_sale_stock.js
@@ -1,6 +1,7 @@
 odoo.define('website_sale_stock.website_sale', function(require) {
 'use strict';
 
+require('web.dom_ready');
 var base = require('web_editor.base');
 var ajax = require('web.ajax');
 var core = require('web.core');
