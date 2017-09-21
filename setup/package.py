3ef31bd9c9192bf5e7a8e5d66317d8fcd3db1747
Christophe Monniez <moc@odoo.com>
2017-09-21 17:07:20 +0200

    [FIX] seems that windows service does not like some chars

--- setup/package.py
+++ setup/package.py
@@ -213,7 +213,7 @@ class KVM(object):
 class KVMWinBuildExe(KVM):
     def run(self):
         with open(join(self.o.build_dir, 'setup/win32/Makefile.version'), 'w') as f:
-            f.write("VERSION=%s\n" % version)
+            f.write("VERSION=%s\n" % version.replace('~', '_'))
         with open(join(self.o.build_dir, 'setup/win32/Makefile.python'), 'w') as f:
             f.write("PYTHON_VERSION=%s\n" % self.o.vm_winxp_python_version.replace('.', ''))
         with open(join(self.o.build_dir, 'setup/win32/Makefile.servicename'), 'w') as f:
