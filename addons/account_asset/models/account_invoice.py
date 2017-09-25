de95e6f4a820b4aa46d4edfa2d666ea782712cb3
Goffin Simon <sig@odoo.com>
2017-09-25 10:50:39 +0200

    [FIX] account_asset: float division by zero
    The number of depreciations or the period length of your asset category cannot be null.
    
    opw:773790

--- addons/account_asset/models/account_invoice.py
+++ addons/account_asset/models/account_invoice.py
@@ -48,6 +48,8 @@ class AccountInvoiceLine(models.Model):
         self.asset_end_date = False
         cat = self.asset_category_id
         if cat:
+            if cat.method_number == 0 or cat.method_period == 0:
+                raise UserError(_('The number of depreciations or the period length of your asset category cannot be null.'))
             months = cat.method_number * cat.method_period
             if self.invoice_id.type in ['out_invoice', 'out_refund']:
                 self.asset_mrr = self.price_subtotal_signed / months
