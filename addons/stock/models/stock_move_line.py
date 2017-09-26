b812b5b625d0f848e3697c4363f1c5ad87e69df9
amoyaux <arm@odoo.com>
2017-09-26 11:25:46 +0200

    [FIX] stock: scrap move update scrap object
    Previously editing a done move line only update
    the quants to the right location. However the stock.scrap
    related object has not its qty updated.
    
    This commit modify the write of stock move line in order
    to also modify the stock scrap object to the correct quantity

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -286,6 +286,12 @@ class StockMoveLine(models.Model):
 
         res = super(StockMoveLine, self).write(vals)
 
+        # Update scrap object linked to move_lines to the new quantity.
+        if 'qty_done' in vals:
+            for move in self.mapped('move_id'):
+                if move.scrapped:
+                    move.scrap_ids.write({'scrap_qty': move.quantity_done})
+
         # As stock_account values according to a move's `product_uom_qty`, we consider that any
         # done stock move should have its `quantity_done` equals to its `product_uom_qty`, and
         # this is what move's `action_done` will do. So, we replicate the behavior here.
