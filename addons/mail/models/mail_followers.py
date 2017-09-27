934171ddb5ea4abddff43a8a6cafaeaa39349e5b
Raphael Collet <rco@odoo.com>
2017-09-27 16:58:19 +0200

    [FIX] mail: remove field res_model_id for performance reasons (#19744)

--- addons/mail/models/mail_followers.py
+++ addons/mail/models/mail_followers.py
@@ -18,12 +18,11 @@ class Followers(models.Model):
     _log_access = False
     _description = 'Document Followers'
 
-    res_model_id = fields.Many2one(
-        'ir.model', 'Related Document Model',
-        index=True, required=True, ondelete='cascade',
-        help='Model of the followed resource')
+    # Note. There is no integrity check on model names for performance reasons.
+    # However, followers of unlinked models are deleted by models themselves
+    # (see 'ir.model' inheritance).
     res_model = fields.Char(
-        'Related Document Model Name', index=True, readonly=True, related='res_model_id.model', compute_sudo=True, store=True)
+        'Related Document Model Name', required=True, index=True)
     res_id = fields.Integer(
         'Related Document ID', index=True, help='Id of the followed resource')
     partner_id = fields.Many2one(
@@ -40,7 +39,6 @@ class Followers(models.Model):
         :param force: if True, delete existing followers before creating new one
                       using the subtypes given in the parameters
         """
-        res_model_id = self.env['ir.model']._get(res_model).id
         force_mode = force or (all(partner_data.values()) and all(channel_data.values()))
         generic = []
         specific = {}
@@ -50,7 +48,7 @@ class Followers(models.Model):
 
         followers = self.sudo().search([
             '&',
-            '&', ('res_model_id', '=', res_model_id), ('res_id', 'in', res_ids),
+            '&', ('res_model', '=', res_model), ('res_id', 'in', res_ids),
             '|', ('partner_id', 'in', list(partner_data)), ('channel_id', 'in', list(channel_data))])
 
         if force_mode:
@@ -82,9 +80,9 @@ class Followers(models.Model):
         gen_new_pids = [pid for pid in partner_data if pid not in p_exist]
         gen_new_cids = [cid for cid in channel_data if cid not in c_exist]
         for pid in gen_new_pids:
-            generic.append([0, 0, {'res_model_id': res_model_id, 'partner_id': pid, 'subtype_ids': [(6, 0, partner_data.get(pid) or default_subtypes.ids)]}])
+            generic.append([0, 0, {'res_model': res_model, 'partner_id': pid, 'subtype_ids': [(6, 0, partner_data.get(pid) or default_subtypes.ids)]}])
         for cid in gen_new_cids:
-            generic.append([0, 0, {'res_model_id': res_model_id, 'channel_id': cid, 'subtype_ids': [(6, 0, channel_data.get(cid) or default_subtypes.ids)]}])
+            generic.append([0, 0, {'res_model': res_model, 'channel_id': cid, 'subtype_ids': [(6, 0, channel_data.get(cid) or default_subtypes.ids)]}])
 
         # create new followers, each document at a time because of existing followers to avoid erasing
         if not force_mode:
@@ -98,13 +96,13 @@ class Followers(models.Model):
                 # subscribe new followers
                 for new_pid in new_pids:
                     command.append((0, 0, {
-                        'res_model_id': res_model_id,
+                        'res_model': res_model,
                         'partner_id': new_pid,
                         'subtype_ids': [(6, 0, partner_data.get(new_pid) or default_subtypes.ids)],
                     }))
                 for new_cid in new_cids:
                     command.append((0, 0, {
-                        'res_model_id': res_model_id,
+                        'res_model': res_model,
                         'channel_id': new_cid,
                         'subtype_ids': [(6, 0, channel_data.get(new_cid) or default_subtypes.ids)],
                     }))
