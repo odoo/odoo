24d314808d0f834d6b3a6f4d1baef939e84e7e96
Jairo Llopis <yajo.sk8@gmail.com>
2017-09-29 13:01:57 +0200

    fixup! fixup! [IMP] website_sale_wishlist: Allow public users to wishlist

--- addons/website_sale_wishlist/__manifest__.py
+++ addons/website_sale_wishlist/__manifest__.py
@@ -9,6 +9,7 @@
     'version': '1.0',
     'depends': ['website_sale'],
     'data': [
+        'data/ir_cron.xml',
         'security/website_sale_wishlist_security.xml',
         'security/ir.model.access.csv',
         'views/website_sale_wishlist_template.xml',
