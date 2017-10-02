8f5ec59ef3cfbbf566f2351ac80bb59a32c2938a
Sébastien Beau <sebastien.beau@akretion.com>
2017-10-02 16:29:15 +0200

    IMP allowing convert_to_cache with list in base_sparse_field

--- addons/base_sparse_field/models/fields.py
+++ addons/base_sparse_field/models/fields.py
@@ -85,8 +85,8 @@ class Serialized(fields.Field):
         return json.dumps(value)
 
     def convert_to_cache(self, value, record, validate=True):
-        # cache format: dict
+        # cache format: dict / list
         value = value or {}
-        return value if isinstance(value, dict) else json.loads(value)
+        return value if isinstance(value, (dict, list)) else json.loads(value)
 
 fields.Serialized = Serialized
