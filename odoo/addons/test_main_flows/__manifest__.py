1a79883135bdaa25a5c0a42d359c999a4248ea1f
Yenthe V.G <yenthevg@gmail.com>
2017-10-03 08:45:09 +0200

    [FIX] test_main_flows: fix 'mobule' typo

--- odoo/addons/test_main_flows/__manifest__.py
+++ odoo/addons/test_main_flows/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '1.0',
     'category': 'Tools',
     'description': """
-This mobule will test the main workflow of Odoo.
+This module will test the main workflow of Odoo.
 It will install some main apps and will try to execute the most important actions.
 """,
     'depends': ['web_tour', 'crm', 'sale_timesheet', 'purchase', 'mrp', 'account'],
