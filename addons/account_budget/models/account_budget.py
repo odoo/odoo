489245a5ee90a1a1350302b8e4af1e7b7b340e99
Laurent Smet <las@openerp.com>
2017-10-03 14:28:32 +0200

    [FIX] account_budget: prevent having an account.budget.post without account_ids (#19860)
    Initially, the check was done in _compute_practical_amount.
    However, in case of no account_ids, an error is raised at each click until the account_ids are set.
    
    was PR #19860

--- addons/account_budget/models/account_budget.py
+++ addons/account_budget/models/account_budget.py
@@ -3,7 +3,7 @@
 
 from odoo import api, fields, models, _
 from odoo.tools import ustr
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 # ---------------------------------------------------------
 # Budgets
@@ -19,6 +19,26 @@ class AccountBudgetPost(models.Model):
     crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'general_budget_id', 'Budget Lines')
     company_id = fields.Many2one('res.company', 'Company', required=True,
         default=lambda self: self.env['res.company']._company_default_get('account.budget.post'))
+
+    def _check_account_ids(self, vals):
+        # Raise an error to prevent the account.budget.post to have not specified account_ids.
+        # This check is done on create because require=True doesn't work on Many2many fields.
+        if 'account_ids' in vals:
+            account_ids = self.resolve_2many_commands('account_ids', vals['account_ids'])
+        else:
+            account_ids = self.account_ids
+        if not account_ids:
+            raise ValidationError(_('The budget must have at least one account.'))
+
+    @api.model
+    def create(self, vals):
+        self._check_account_ids(vals)
+        return super(AccountBudgetPost, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        self._check_account_ids(vals)
+        return super(AccountBudgetPost, self).write(vals)
 
 
 class CrossoveredBudget(models.Model):
@@ -85,8 +105,6 @@ class CrossoveredBudgetLines(models.Model):
         for line in self:
             result = 0.0
             acc_ids = line.general_budget_id.account_ids.ids
-            if not acc_ids:
-                raise UserError(_("The Budget '%s' has no accounts!") % ustr(line.general_budget_id.name))
             date_to = self.env.context.get('wizard_date_to') or line.date_to
             date_from = self.env.context.get('wizard_date_from') or line.date_from
             if line.analytic_account_id.id:
