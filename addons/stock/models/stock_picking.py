46af955328ea6d21b562e8f8d7f2599386407578
Simon Lejeune <sle@openerp.com>
2017-10-05 13:57:52 +0200

    [FIX] stock: display `lot_name` in the detailed operations view
    If the user selected 'use_create_lots', he should see the text field and
    not the many2one. This improves the usability of the barcode scanning in
    a receipt, where the lots were not displayed (as it only set the
    lot_name).

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -294,6 +294,7 @@ class Picking(models.Model):
     # Used to search on pickings
     product_id = fields.Many2one('product.product', 'Product', related='move_lines.product_id')
     show_operations = fields.Boolean(compute='_compute_show_operations')
+    show_lots_text = fields.Boolean(compute='_compute_show_lots_text')
 
     _sql_constraints = [
         ('name_uniq', 'unique(name, company_id)', 'Reference must be unique per company!'),
@@ -312,6 +313,18 @@ class Picking(models.Model):
                     picking.show_operations = False
             else:
                 picking.show_operations = False
+
+    @api.depends('move_line_ids', 'picking_type_id.use_create_lots', 'picking_type_id.use_existing_lots', 'state')
+    def _compute_show_lots_text(self):
+        group_production_lot_enabled = self.user_has_groups('stock.group_production_lot')
+        for picking in self:
+            if not picking.move_line_ids:
+                picking.show_lots_text = False
+            elif group_production_lot_enabled and picking.picking_type_id.use_create_lots \
+                    and not picking.picking_type_id.use_existing_lots and picking.state != 'done':
+                picking.show_lots_text = True
+            else:
+                picking.show_lots_text = False
 
     @api.depends('move_type', 'move_lines.state', 'move_lines.picking_id')
     @api.one
