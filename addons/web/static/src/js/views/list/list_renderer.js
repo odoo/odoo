5b465611a009814ea1c70d967ce3dccca320f38c
Géry Debongnie <ged@odoo.com>
2017-10-05 15:55:10 +0200

    [REF] web: remove dead template/code/files
    We sometimes need to clean up stuff.  I am actually surprised that there
    were no more dead templates.  We should do the same for css/less.

--- addons/web/static/src/js/views/list/list_renderer.js
+++ addons/web/static/src/js/views/list/list_renderer.js
@@ -4,7 +4,6 @@ odoo.define('web.ListRenderer', function (require) {
 var BasicRenderer = require('web.BasicRenderer');
 var config = require('web.config');
 var core = require('web.core');
-var Dialog = require('web.Dialog');
 var dom = require('web.dom');
 var field_utils = require('web.field_utils');
 var Pager = require('web.Pager');
