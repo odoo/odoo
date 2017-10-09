fee135b686a51079fb2694d90306fe21c9a5c648
Stephan Rozendaal <stephan.rozendaal@gmail.com>
2017-10-09 21:25:05 +0200

    [IMP] http: set HTTPonly flag on session cookie
    This is a backport from commit f4d541e51ab on Odoo 11.0
    
    Original commit body:
    This will reinforce the framework against potential XSS exploits
    escalating to session hijack

--- openerp/http.py
+++ openerp/http.py
@@ -1617,7 +1617,7 @@ class Root(object):
         #   (the one using the cookie). That is a special feature of the Session Javascript class.
         # - It could allow session fixation attacks.
         if not explicit_session and hasattr(response, 'set_cookie'):
-            response.set_cookie('session_id', httprequest.session.sid, max_age=90 * 24 * 60 * 60)
+            response.set_cookie('session_id', httprequest.session.sid, max_age=90 * 24 * 60 * 60, httponly=True)
 
         return response
 
