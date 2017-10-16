74bbda5de2106fbd5f7866e5a4dd4dd37e182085
JoseMiguelRivero <jose.rivero@trescloud.com>
2017-10-16 11:44:40 -0500

    [FIX][account]: Codigo duplicado asientos descuadrados (#129)

--- addons/account/models/account_invoice.py
+++ addons/account/models/account_invoice.py
@@ -914,43 +914,7 @@ class AccountInvoice(models.Model):
             total, total_currency, iml = inv.with_context(ctx).compute_invoice_totals(company_currency, iml)
             #La siguiente linea fue creada por TresCloud
             iml = self._get_payable_accounts(total, total_currency, iml, ctx)
-            name = inv.name or '/'
-            if inv.payment_term_id:
-                totlines = inv.with_context(ctx).payment_term_id.with_context(currency_id=company_currency.id).compute(total, inv.date_invoice)[0]
-                res_amount_currency = total_currency
-                ctx['date'] = inv.date or inv.date_invoice
-                for i, t in enumerate(totlines):
-                    if inv.currency_id != company_currency:
-                        amount_currency = company_currency.with_context(ctx).compute(t[1], inv.currency_id)
-                    else:
-                        amount_currency = False
-
-                    # last line: add the diff
-                    res_amount_currency -= amount_currency or 0
-                    if i + 1 == len(totlines):
-                        amount_currency += res_amount_currency
-
-                    iml.append({
-                        'type': 'dest',
-                        'name': name,
-                        'price': t[1],
-                        'account_id': inv.account_id.id,
-                        'date_maturity': t[0],
-                        'amount_currency': diff_currency and amount_currency,
-                        'currency_id': diff_currency and inv.currency_id.id,
-                        'invoice_id': inv.id
-                    })
-            else:
-                iml.append({
-                    'type': 'dest',
-                    'name': name,
-                    'price': total,
-                    'account_id': inv.account_id.id,
-                    'date_maturity': inv.date_due,
-                    'amount_currency': diff_currency and total_currency,
-                    'currency_id': diff_currency and inv.currency_id.id,
-                    'invoice_id': inv.id
-                })
+            #El codigo removido en esa seccion esta en el metodo _get_payable_accounts
             part = self.env['res.partner']._find_accounting_partner(inv.partner_id)
             line = [(0, 0, self.line_get_convert(l, part.id)) for l in iml]
             line = inv.group_lines(iml, line)
