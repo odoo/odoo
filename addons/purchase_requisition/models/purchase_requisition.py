4f1cd90826cd142d94a8d3c3fb87e9559fa24bba
William Henrotin <whe@odoo.com>
2017-10-18 17:46:01 +0200

    [REF] purchase_requisition: sequence handling
    When creating a draft purchase agreement, a reference number was generated even if
    we do not click the 'Save' button.
    Now, the reference is filled with 'New' at the creation and overided with a proper
    reference number after clicking on 'Save'. Also, the prefix depends now of the
    requisition type. BOxxxxx for blanket orders, TExxxxx for call for tenders.

--- addons/purchase_requisition/models/purchase_requisition.py
+++ addons/purchase_requisition/models/purchase_requisition.py
@@ -45,7 +45,7 @@ class PurchaseRequisition(models.Model):
     def _get_type_id(self):
         return self.env['purchase.requisition.type'].search([], limit=1)
 
-    name = fields.Char(string='Agreement Reference', required=True, copy=False, default= lambda self: self.env['ir.sequence'].next_by_code('purchase.order.requisition'))
+    name = fields.Char(string='Agreement Reference', required=True, copy=False, default='New', readonly=True)
     origin = fields.Char(string='Source Document')
     order_count = fields.Integer(compute='_compute_orders_number', string='Number of Orders')
     vendor_id = fields.Many2one('res.partner', string="Vendor")
@@ -85,6 +85,7 @@ class PurchaseRequisition(models.Model):
                 requisition.is_quantity_copy = False
             else:
                 requisition.is_quantity_copy = True
+
 
     @api.multi
     @api.depends('purchase_ids')
