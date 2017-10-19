8ead5f9610b94d2e9e4305d6baf2b21d9729896f
Christophe Simonis <chs@odoo.com>
2017-10-19 21:03:00 +0200

    [FIX] sale_stock: do not mix indentation with tab & spaces

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -110,12 +110,12 @@ class SaleOrderLine(models.Model):
     @api.depends('product_id')
     def _compute_qty_delivered_updateable(self):
         # prefetch field before filtering
-    	self.mapped('product_id')
-   	# on consumable or stockable products, qty_delivered_updateable defaults
-	# to False; on other lines use the original computation
-	lines = self.filtered(lambda line: line.product_id.type not in ('consu', 'product'))
-    	lines = lines.with_prefetch(self._prefetch)
-    	super(SaleOrderLine, lines)._compute_qty_delivered_updateable()
+        self.mapped('product_id')
+        # on consumable or stockable products, qty_delivered_updateable defaults
+        # to False; on other lines use the original computation
+        lines = self.filtered(lambda line: line.product_id.type not in ('consu', 'product'))
+        lines = lines.with_prefetch(self._prefetch)
+        super(SaleOrderLine, lines)._compute_qty_delivered_updateable()
 
     @api.onchange('product_id')
     def _onchange_product_id_set_customer_lead(self):
