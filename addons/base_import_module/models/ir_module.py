627a82cad388b9a0d1fe00866646f42e26455a74
Adrian Torres <adt@odoo.com>
2017-10-27 15:27:41 +0200

    [FIX] base_import_module: display correct error
    Previous to this commit, when importing a studio module with unmet
    dependencies that were not studio the error message would just display
    the Studio is required to install Studio customizations.
    
    With this commit it should now correctly display the missing
    dependencies by checking if only web_studio is in the set of unmet
    dependencies
    
    opw-777642

--- addons/base_import_module/models/ir_module.py
+++ addons/base_import_module/models/ir_module.py
@@ -36,7 +36,8 @@ class IrModule(models.Model):
         unmet_dependencies = set(terp['depends']).difference(installed_mods)
 
         if unmet_dependencies:
-            if _is_studio_custom(path):
+            if (unmet_dependencies == set(['web_studio']) and
+                    _is_studio_custom(path)):
                 err = _("Studio customizations require Studio")
             else:
                 err = _("Unmet module dependencies: %s") % ', '.join(
