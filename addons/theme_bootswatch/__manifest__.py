5238595648c986650e7ed8878462afd159d62b42
Akash Bhavsar <abh@odoo.com>
2017-10-30 16:37:21 +0530

    [REF] theme_*: change category and summary of themes
    task-32528

--- addons/theme_bootswatch/__manifest__.py
+++ addons/theme_bootswatch/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Bootswatch Theme',
     'summary': 'Support for Bootswatch themes in master',
     'description': 'This theme module is exclusively for master to keep the support of Bootswatch themes which were previously part of the website module in 8.0.',
-    'category': 'Theme',
+    'category': 'Theme/Corporate',
     'sequence': 900,
     'version': '1.0',
     'depends': ['website'],
