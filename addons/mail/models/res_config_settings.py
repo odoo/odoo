7ef1e3654ec91f83931d938a37f5bee742e50026
Mitali Patel <mpa@odoo.com>
2017-10-30 17:11:36 +0530

    [IMP] mail: added new field to set the catchall in configuration setting

--- addons/mail/models/res_config_settings.py
+++ addons/mail/models/res_config_settings.py
@@ -16,16 +16,20 @@ class ResConfigSettings(models.TransientModel):
     fail_counter = fields.Integer('Fail Mail', readonly=True)
     alias_domain = fields.Char('Alias Domain', help="If you have setup a catch-all email domain redirected to "
                                "the Odoo server, enter the domain name here.")
+    catchall_name = fields.Char('Reply-to Catchall', help="When a contact replies to an email sent from Odoo, the default reply-to address is this generic address. This is used to route the message to the right discussion thread in Odoo and to the inbox of all its followers. The reply-to address can be also be changed for specific emails from the configuration of the email templates.")
+    catchall_domain = fields.Char('Catchall Domain', related='alias_domain')
 
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
+        IrConfigParameter = self.env["ir.config_parameter"]
 
         previous_date = datetime.datetime.now() - datetime.timedelta(days=30)
 
-        alias_domain = self.env["ir.config_parameter"].get_param("mail.catchall.domain", default=None)
+        alias_domain = IrConfigParameter.get_param("mail.catchall.domain", default=None)
+        catchall_name = IrConfigParameter.get_param("mail.catchall.alias")
         if alias_domain is None:
-            domain = self.env["ir.config_parameter"].get_param("web.base.url")
+            domain = IrConfigParameter.get_param("web.base.url")
             try:
                 alias_domain = urls.url_parse(domain).host
             except Exception:
@@ -36,10 +40,13 @@ class ResConfigSettings(models.TransientModel):
                 ('date', '>=', previous_date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)),
                 ('state', '=', 'exception')]),
             alias_domain=alias_domain or False,
+            catchall_name=catchall_name or False
         )
 
         return res
 
     def set_values(self):
         super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].set_param("mail.catchall.domain", self.alias_domain or '')
+        IrConfigParameter = self.env["ir.config_parameter"]
+        IrConfigParameter.set_param("mail.catchall.domain", self.alias_domain or '')
+        IrConfigParameter.set_param("mail.catchall.alias", self.catchall_name or '')
