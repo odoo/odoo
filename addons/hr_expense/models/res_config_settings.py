ab8f5ec95d41fdab501cb4c5b5b8e0218f337942
fda-odoo <fda@odoo.com>
2017-10-31 17:32:46 +0100

    [IMP] hr_expense: change tours text, labels, search field
    - subscribe responsibles of an expense to followers of that expense
    - hr_expense requires sale_expense instead of sale_management
    - remove one tour step and change texts of some others
    - remove header from expense line wizard
    - add tax field in form view for expense
    - change the display name of some fields and other titles.
    - remove the expense report and improve search bar for expenses in report.

--- addons/hr_expense/models/res_config_settings.py
+++ addons/hr_expense/models/res_config_settings.py
@@ -9,7 +9,7 @@ class ResConfigSettings(models.TransientModel):
     expense_alias_prefix = fields.Char('Default Alias Name for Expenses')
     use_mailgateway = fields.Boolean(string='Let your employees record expenses by email',
                                      config_parameter='hr_expense.use_mailgateway')
-    module_sale_management = fields.Boolean(string="Customer Billing")
+    module_sale_expense = fields.Boolean(string="Customer Billing")
 
     @api.model
     def get_values(self):
