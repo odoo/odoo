fae9617d5735d9b76995ed06090ff95de5e37cf6
Lucas Perais (lpe) <lpe@odoo.com>
2017-11-07 14:39:07 +0100

    Finishing off

--- addons/l10n_fr_certification/models/account_closure.py
+++ addons/l10n_fr_certification/models/account_closure.py
@@ -12,12 +12,12 @@ WRITE_MSG = _('Closures of receivable accounts are not meant to be written or de
 class AccountClosure(models.Model):
     _name = 'account.sale.closure'
 
+    name = fields.Char(help="Frequency and unique sequence number")
     company_id = fields.Many2one('res.company', string='Company', readonly=True)
     date_closure_stop = fields.Datetime(string="Closing Date", help='Date to which the values are computed', readonly=True)
     date_closure_start = fields.Datetime(string="Starting Date", help='Date from which the total interval is computed', readonly=True)
     frequency = fields.Selection(string='Interval of the closure', selection=[('daily', 'Daily'), ('monthly', 'Monthly'), ('annually', 'Annually')], readonly=True)
     total_interval = fields.Monetary(string="Period Total", help='Total in receivable accounts during the interval', readonly=True)
-    total_fiscal = fields.Monetary(string="Fiscal Year Cumulative Total", help='Total in receivable accounts since the beginning of the fiscal year', readonly=True)
     total_beginning = fields.Monetary(string="Cumulative Grand Total", help='Total in receivable accounts since the beginning of times', readonly=True)
     sequence_number = fields.Integer('Sequence number', readonly=True)
     move_ids = fields.Many2many('account.move', string='Journal entries that are included in the computation', readonly=True)
@@ -25,7 +25,9 @@ class AccountClosure(models.Model):
 
     def _build_query(self, company, date_start='', date_stop='', avoid_move_ids=[]):
         params = {'company_id': company.id}
-        query = '''SELECT m.write_date AS move_write_date, m.id AS move_id, debit-credit AS balance
+        query = '''SELECT m.write_date AS move_write_date,
+                          m.id AS move_id,
+                          debit-credit AS balance
             FROM account_move_line aml
             JOIN account_journal j ON aml.journal_id = j.id
             JOIN  (SELECT acc.id FROM account_account acc
@@ -60,13 +62,11 @@ class AccountClosure(models.Model):
 
         date_query_start = ''
         previous_move_ids = []
-        riding_fiscal_periods = interval_dates['riding_fiscal_periods']
 
         if previous_closure and previous_closure.move_ids:
             previous_date = previous_closure.move_ids.sorted(key=lambda m: m.write_date)[-1].write_date
             date_query_start = min(previous_date, interval_dates['interval_from'])
             previous_move_ids = previous_closure.move_ids.ids
-            riding_fiscal_periods = date_query_start < interval_dates['fiscal_from']
 
         query, params = self._build_query(company, date_query_start, interval_dates['date_stop'])
         self.env.cr.execute(query, params)
@@ -79,17 +79,7 @@ class AccountClosure(models.Model):
         total_interval = sum(aml['balance'] for aml in aml_interval)
         total_beginning = sum(aml['balance'] for aml in aml_beginning)
 
-        aml_fiscal = []
-        total_fiscal = 0
-        if riding_fiscal_periods:
-            aml_fiscal = filter(lambda aml: aml['move_write_date'] >= interval_dates['fiscal_from'], results)
-            total_fiscal = sum(aml['balance'] for aml in aml_fiscal)
-        else:
-            aml_fiscal = filter(lambda aml: aml['move_id'] not in previous_move_ids, results)
-            total_fiscal = previous_closure.total_fiscal + sum(aml['balance'] for aml in aml_fiscal)
-
         return {'total_interval': total_interval,
-                'total_fiscal': total_fiscal,
                 'total_beginning': previous_closure.total_beginning + total_beginning,
                 'move_ids': [(4, aml['move_id'], False) for aml in aml_interval],
                 'date_closure_stop': interval_dates['date_stop'],
@@ -100,7 +90,6 @@ class AccountClosure(models.Model):
             return datetime.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
 
         date_stop = datetime.utcnow()
-        fiscal_year_dates = company.compute_fiscalyear_dates(datetime(year=date_stop.year, month=date_stop.month, day=date_stop.day))
         interval_from = None
         if frequency == 'daily':
             interval_from = date_stop - timedelta(days=1)
@@ -113,9 +102,7 @@ class AccountClosure(models.Model):
             interval_from = date_stop.replace(year=year_target)
 
         return {'interval_from': time_to_string(interval_from),
-                'fiscal_from': time_to_string(fiscal_year_dates['date_from']),
-                'date_stop': time_to_string(date_stop),
-                'riding_fiscal_periods': interval_from < fiscal_year_dates['date_from']}
+                'date_stop': time_to_string(date_stop)}
 
     @api.multi
     def write(self, vals):
@@ -138,6 +125,7 @@ class AccountClosure(models.Model):
             values['company_id'] = company.id
             values['currency_id'] = company.currency_id.id
             values['sequence_number'] = new_sequence_number
+            values['name'] = frequency + ", " + str(new_sequence_number)
             account_closures |= account_closures.create(values)
 
         return account_closures
--- addons/l10n_fr_pos_cert/models/account_bank_statement.py
+++ addons/l10n_fr_pos_cert/models/account_bank_statement.py
@@ -8,8 +8,6 @@ BKN_NO_TOUCH = _('You cannot modify anything on a %s (name: %s) that was created
 class AccountBankStatement(models.Model):
     _inherit = 'account.bank.statement'
 
-    # Prevent modifying anything coming from the pos
-
     def unlink(self):
         for statement in self.filtered(lambda s: s.company_id._is_accounting_unalterable() and s.journal_id.journal_user):
             raise UserError(BKN_NO_TOUCH % (_('bank statement'), statement.name))
@@ -18,8 +16,6 @@ class AccountBankStatement(models.Model):
 
 class AccountBankStatementLine(models.Model):
     _inherit = 'account.bank.statement.line'
-
-    # Prevent modifying anything coming from the pos
 
     def unlink(self):
         for line in self.filtered(lambda s: s.company_id._is_accounting_unalterable() and s.journal_id.journal_user):
