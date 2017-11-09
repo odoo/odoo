52fd524aebc9fdd27a576ac1f5b69139d0a7e3d0
Simon Lejeune <sle@openerp.com>
2017-11-09 14:07:00 +0100

    [FIX] mrp, purchase_mrp: purchase_line_id also with kits
    When confirming a move for a kit, it is basically unlinked and replaced
    by moves for its component. The link to the purchase line was kept in
    v10, but since rev[1] it is not anymore. Make an helper to get the
    values and override it in purchase_mrp to add the purchase line link.
    
    [1] https://github.com/odoo/odoo/commit/ac1020a49270c1a9eb284b6a4fcf6d99807e74ee
    
    opw 779497

--- addons/purchase_mrp/models/purchase_mrp.py
+++ addons/purchase_mrp/models/purchase_mrp.py
@@ -28,3 +28,14 @@ class PurchaseOrderLine(models.Model):
                 return self.product_qty
             else:
                 return 0.0
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _prepare_phantom_move_values(self, bom_line, quantity):
+        vals = super(StockMove, self)._prepare_phantom_move_values(bom_line, quantity)
+        if self.purchase_line_id:
+            vals['purchase_line_id'] = self.purchase_line_id.id
+        return vals
+
