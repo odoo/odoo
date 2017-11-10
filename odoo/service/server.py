c68b41bff2d5421e48b990da90362264a3c16a81
Silvio Fernandez <silviofernandezmarin@gmail.com>
2017-11-11 00:48:05 +0100

    [FIX] server: avoid crash with gevent worker on windows
    Some people may want to use the gevent worker on Windows, even if
    multi-process mode is generally not supported on Windows.
    
    Fixes #17726
    Closes #20409

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -361,15 +361,15 @@ class GeventServer(CommonServer):
         import gevent
         from gevent.wsgi import WSGIServer
 
-        # Set process memory limit as an extra safeguard
-        _, hard = resource.getrlimit(resource.RLIMIT_AS)
-        resource.setrlimit(resource.RLIMIT_AS, (config['limit_memory_hard'], hard))
 
         if os.name == 'posix':
+            # Set process memory limit as an extra safeguard
+            _, hard = resource.getrlimit(resource.RLIMIT_AS)
+            resource.setrlimit(resource.RLIMIT_AS, (config['limit_memory_hard'], hard))
             signal.signal(signal.SIGQUIT, dumpstacks)
             signal.signal(signal.SIGUSR1, log_ormcache_stats)
-
-        gevent.spawn(self.watchdog)
+            gevent.spawn(self.watchdog)
+        
         self.httpd = WSGIServer((self.interface, self.port), self.app)
         _logger.info('Evented Service (longpolling) running on %s:%s', self.interface, self.port)
         try:
