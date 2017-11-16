89055c167ecbe4a948b167295e5517e0b3398642
jem-odoo <jem@openerp.com>
2017-11-16 14:00:57 +0100

    [IMP] sale_timesheet: add profitability report analysis
    This commit provide a real reporting tool
    for project profitability: graph and pivot
    views.
    You can now have a look at cost and revenues
    per project, so line, product, see which
    project manager brings the more money, which
    project costs many human time, ...
    
    The project overview is based on this report
    analysis to be consistent.
    
    However, a change of computation for 'amount
    invoiced' is introduced: we now based the amount
    on the price_reduce of the SO line, and not from
    the invoice lines. Also, once a draft invoice is
    generated, the quantity is considered as invoiced
    at the price of the SO line (no matter is extra
    discount happens when validating invoices).

--- addons/sale_timesheet/report/__init__.py
+++ addons/sale_timesheet/report/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import models
-from . import controllers
-from . import report
+from . import project_profitability_report_analysis
