b5ad7f38874c68bb40f35fb80e110099c2209109
Thibault Delavallée <tde@odoo.com>
2017-11-17 12:59:00 +0100

    [IMP] mail: add tool methods to manage activities directly from the mixin
    This commit adds tools methods related to activities in the mail.activity
    mixin. It gives to models inheriting from the activity mixin an easy-to-use
    API to schedule, unlink or mark activities as done. Purpose of those methods
    is to avoid having people manually managing activities in the code to hide
    the technical details of the activities model, notably access rights
    or activity types.
    
    A field is added on activity model to indicate they have been generated
    automatically. This way when rescheduling or unlinking based on some
    specific activity types we do not change user-created activities.
    
    Those methods include scheduling activities, changing their dates, marking
    them as done or unlinking them. Future commits will use those methods
    in various addons to automatically generate activities based on workflow
    we want to implement.
    
    This commit also adds tests for the newly added code. Future commits should
    probably have a look at activity security and add some tests cases to check
    it is correctly taken into account. It is considered a bit out of scope for
    this task.
    
    Thanks to @jem-odoo for its in-depth review of this commit. Well thanks for
    other commits also.

--- addons/test_mail/tests/test_mail_activity.py
+++ addons/test_mail/tests/test_mail_activity.py
@@ -14,7 +14,13 @@ class TestMailActivity(BaseFunctionalTest):
     @classmethod
     def setUpClass(cls):
         super(TestMailActivity, cls).setUpClass()
-        cls.test_record = cls.env['mail.test.activity'].create({'name': 'Test'})
+        cls.test_record = cls.env['mail.test.activity'].with_context(cls._quick_create_ctx).create({'name': 'Test'})
+        # reset ctx
+        cls.test_record = cls.test_record.with_context(
+            mail_create_nolog=False,
+            mail_create_nosubscribe=False,
+            mail_notrack=False
+        )
 
     def test_activity_flow_employee(self):
         with self.sudoAs('ernest'):
@@ -106,3 +112,67 @@ class TestMailActivity(BaseFunctionalTest):
             self.assertEqual(reminder.summary, 'Reminder')
             reminder.write({'note': 'Holidays', 'summary': 'Summary'})
             self.assertEqual(reminder.summary, 'Summary')
+
+    def test_activity_mixin(self):
+        today = date.today()
+        with self.sudoAs('ernest'):
+            self.assertEqual(self.test_record.env.user, self.user_employee)
+
+            # Test various scheduling of activities
+            act1 = self.test_record.activity_schedule(
+                'test_mail.mail_act_test_todo',
+                today + relativedelta(days=1),
+                user_id=self.user_admin.id)
+            self.assertEqual(act1.automated, True)
+
+            act_type = self.env.ref('test_mail.mail_act_test_todo')
+            self.assertEqual(self.test_record.activity_summary, act_type.summary)
+            self.assertEqual(self.test_record.activity_state, 'planned')
+            self.assertEqual(self.test_record.activity_user_id, self.user_admin)
+
+            act2 = self.test_record.activity_schedule(
+                'test_mail.mail_act_test_meeting',
+                today + relativedelta(days=-1))
+            self.assertEqual(self.test_record.activity_state, 'overdue')
+            self.assertEqual(self.test_record.activity_user_id, self.user_employee)
+
+            act3 = self.test_record.activity_schedule(
+                'test_mail.mail_act_test_todo',
+                today + relativedelta(days=3),
+                user_id=self.user_employee.id)
+            self.assertEqual(self.test_record.activity_state, 'overdue')
+            self.assertEqual(self.test_record.activity_user_id, self.user_employee)
+
+            self.assertEqual(self.test_record.activity_ids, act1 | act2 | act3)
+
+            # Perform todo activities for admin
+            self.test_record.activity_feedback(
+                ['test_mail.mail_act_test_todo'],
+                user_id=self.user_admin.id,
+                feedback='Test feedback',)
+            self.assertEqual(self.test_record.activity_ids, act2 | act3)
+
+            # Reschedule all activities, should update the record state
+            self.assertEqual(self.test_record.activity_state, 'overdue')
+            self.test_record.activity_reschedule(
+                ['test_mail.mail_act_test_meeting', 'test_mail.mail_act_test_todo'],
+                date_deadline=today + relativedelta(days=3)
+            )
+            self.assertEqual(self.test_record.activity_state, 'planned')
+
+            # Perform todo activities for remaining people
+            self.test_record.activity_feedback(
+                ['test_mail.mail_act_test_todo'],
+                feedback='Test feedback')
+
+            # Setting activities as done should delete them and post messages
+            self.assertEqual(self.test_record.activity_ids, act2)
+            self.assertEqual(len(self.test_record.message_ids), 2)
+            self.assertEqual(self.test_record.message_ids.mapped('subtype_id'), self.env.ref('mail.mt_activities'))
+
+            # Perform meeting activities
+            self.test_record.activity_unlink(['test_mail.mail_act_test_meeting'])
+
+            # Canceling activities should simply remove them
+            self.assertEqual(self.test_record.activity_ids, self.env['mail.activity'])
+            self.assertEqual(len(self.test_record.message_ids), 2)
