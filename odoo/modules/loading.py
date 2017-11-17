742a29f103671fcd62bf917a834780124e01c85f
Adrian Torres <adt@odoo.com>
2017-11-17 16:00:24 +0100

    Improve. Adapt. Overcome.

--- odoo/modules/loading.py
+++ odoo/modules/loading.py
@@ -433,7 +433,7 @@ def load_modules(db, force_demo=False, status=None, update_module=False):
         cr.close()
 
 
-def reset_modules_state(db):
+def reset_modules_state(db_name):
     """
     Resets modules flagged as "to x" to their original state
     """
@@ -443,6 +443,7 @@ def reset_modules_state(db):
     # installation/upgrade/uninstallation fails, which is the only known case
     # for which modules can stay marked as 'to %' for an indefinite amount
     # of time
+    db = odoo.sql_db.db_connect(db_name)
     with db.cursor() as cr:
         cr.execute(
             "UPDATE ir_module_module SET state='installed' WHERE state IN "
@@ -452,4 +453,3 @@ def reset_modules_state(db):
             "UPDATE ir_module_module SET state='uninstalled' WHERE "
             "state='to install'"
         )
-        cr.commit()
--- odoo/modules/registry.py
+++ odoo/modules/registry.py
@@ -82,7 +82,7 @@ class Registry(Mapping):
                     try:
                         odoo.modules.load_modules(registry._db, force_demo, status, update_module)
                     except Exception:
-                        odoo.modules.reset_modules_state(registry._db)
+                        odoo.modules.reset_modules_state(db_name)
                         raise
                 except Exception:
                     _logger.exception('Failed to load registry')
