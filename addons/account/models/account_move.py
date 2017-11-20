2159113927223ca3ade68e39cd2f144973647d4d
jem-odoo <jem@openerp.com>
2017-11-20 11:33:52 +0100

    [IMP] analytic,hr_timesheet: force partner from task or project
    Since partner is not required on analytic account, nor
    on AAL, we may have lines related to different partner (a 'support'
    project for instance, with task for different partner, the AAL will
    thus also have different partner). This use case is not possible with
    the field override in account.
    This commit will allow to do cost analysis by partner in the same
    analytic account.

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -289,7 +289,7 @@ class AccountMove(models.Model):
             if move.reverse_date and (not self.env.user.company_id.period_lock_date or move.reverse_date > self.env.user.company_id.period_lock_date):
                 date = move.reverse_date
             move.reverse_moves(date=date, auto=True)
-            
+
     @api.multi
     def action_view_reverse_entry(self):
         action = self.env.ref('account.action_move_journal_line').read()[0]
@@ -660,7 +660,7 @@ class AccountMoveLine(models.Model):
     @api.model
     def get_reconciliation_proposition(self, account_id, partner_id=False):
         """ Returns two lines whose amount are opposite """
-        
+
         target_currency = (self.currency_id and self.amount_currency) and self.currency_id or self.company_id.currency_id
         partner_id_condition = partner_id and 'AND a.partner_id = %(partner_id)s' or ''
 
@@ -826,7 +826,7 @@ class AccountMoveLine(models.Model):
                 amount_currency = line.amount_currency
 
             target_currency = target_currency or company_currency
-            
+
             ctx = context.copy()
             ctx.update({'date': target_date or line.date})
             # Use case:
@@ -835,13 +835,13 @@ class AccountMoveLine(models.Model):
             # 1)    25      0            0            NULL
             # 2)    17      0           25             EUR
             # 3)    33      0           25             YEN
-            # 
+            #
             # If we ask to see the information in the reconciliation widget in company currency, we want to see
             # The following information
             # 1) 25 USD (no currency information)
             # 2) 17 USD [25 EUR] (show 25 euro in currency information, in the little bill)
             # 3) 33 USD [25 YEN] (show 25 yen in currency information)
-            # 
+            #
             # If we ask to see the information in another currency than the company let's say EUR
             # 1) 35 EUR [25 USD]
             # 2) 25 EUR (no currency information)
@@ -1480,6 +1480,7 @@ class AccountMoveLine(models.Model):
             'ref': self.ref,
             'move_id': self.id,
             'user_id': self.invoice_id.user_id.id or self._uid,
+            'partner_id': self.partner_id.id,
         }
 
     @api.model
