9e0724088ba674b22c23d03c723d789e10101e77
Arnold Moyaux <arm@odoo.com>
2017-11-21 14:15:21 +0100

    [FIX] purchase: cancel rfq set destination move to mts
    Use case to reproduce:
    - Create a SO with a product MTO and confirm the PO
    -> PO -- Stock -- SO
    - Cancel the SO
    -> PO -- Stock    canceled SO
    - Set the quotation to draft and confirm it again
    -> PO  -- Stock
       RFQ -- Stock -- SO
    - If the user cancel the RFQ we will get this diagram:
    -> PO -- Stock
             Stock -- SO
    The user can't reserve the chain correctly. Also a new RFQ will be
    generated each time the user delete the existing one even if the PO
    already exists.
    
    It happens because the move keep the procure methode MTO although
    the moves origin and destination links are broken.
    
    This commit changes the move destination from purchase order
    line to MTS if the RFQ is deleted. This implies that RFQ are
    not automatically generated by the scheduler.

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -364,6 +364,15 @@ class PurchaseOrder(models.Model):
                 if inv and inv.state not in ('cancel', 'draft'):
                     raise UserError(_("Unable to cancel this purchase order. You must first cancel related vendor bills."))
 
+            # If the product is MTO, change the procure_method of the the closest move to purchase to MTS.
+            # The purpose is to link the po that the user will manually generate to the existing moves's chain.
+            if order.state in ('draft', 'sent', 'to approve'):
+                for order_line in order.order_line:
+                    if order_line.move_dest_ids:
+                        siblings_states = (order_line.move_dest_ids.mapped('move_orig_ids')).mapped('state')
+                        if all(state in ('done', 'cancel') for state in siblings_states):
+                            order_line.move_dest_ids.write({'procure_method': 'make_to_stock'})
+
             for pick in order.picking_ids.filtered(lambda r: r.state != 'cancel'):
                 pick.action_cancel()
 
