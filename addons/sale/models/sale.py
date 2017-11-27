4e1a9dd2ee618013a50254c12d45b976f49704d2
mreficent <miquel.raich@eficent.com>
2017-11-27 15:11:59 +0100

    [FIX] sale: fix typo
    Closes #21205

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -427,11 +427,11 @@ class SaleOrder(models.Model):
                     references[invoice] = references[invoice] | order
 
         if not invoices:
-            raise UserError(_('There is no invoicable line.'))
+            raise UserError(_('There is no invoiceable line.'))
 
         for invoice in invoices.values():
             if not invoice.invoice_line_ids:
-                raise UserError(_('There is no invoicable line.'))
+                raise UserError(_('There is no invoiceable line.'))
             # If invoice is negative, do a refund invoice instead
             if invoice.amount_untaxed < 0:
                 invoice.type = 'out_refund'
