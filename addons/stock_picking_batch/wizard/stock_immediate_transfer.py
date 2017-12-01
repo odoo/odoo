00b7b4f8b961c4f9fc51fa02035ac4cf4d401c86
Christophe Simonis <chs@odoo.com>
2017-12-01 10:49:32 +0100

    [FIX] stock{,_picking_batch}: correct public method return value

--- addons/stock_picking_batch/wizard/stock_immediate_transfer.py
+++ addons/stock_picking_batch/wizard/stock_immediate_transfer.py
@@ -11,10 +11,11 @@ class StockImmediateTransfer(models.TransientModel):
     def process(self):
         backorder_wizard_dict = super(StockImmediateTransfer, self).process()
         # If the immediate transfer wizard process all our picking but with some back order maybe needed we want to add the backorder already passed to the wizard.
-        if backorder_wizard_dict is not None:
+        if backorder_wizard_dict:
             backorder_wizard = self.env['stock.backorder.confirmation'].browse(backorder_wizard_dict.get('res_id', False))
             backorder_wizard.write({'pick_ids': [(4, p.id) for p in self.pick_to_backorder_ids]})
             return backorder_wizard_dict
         # If there is no backorder returned by the immediate transfer basic function we still wanted to process those manually given
         elif self.pick_to_backorder_ids:
             return self.pick_to_backorder_ids.action_generate_backorder_wizard()
+        return False
