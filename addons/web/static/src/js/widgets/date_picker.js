be5a862547a7dea675155a6865ad4bd52e4da372
Alexandre Kühn <aku@odoo.com>
2017-12-04 11:01:36 +0100

    [FIX] web,*: time format from user's language
    Before this fix, the chatter always showed dates of messages in military time format.
    
    This was not the expected behavior, as the user could have had customize
    the date and time format in the settings of the current language.
    
    Steps to reproduce the issue:
    
    1. Set Odoo in English
    2. Settings > Translations > Languages > English
       2.1. Set Date Format to %m/%d/%Y
       2.2. Set Time Format to %I:%M:%S %p
    3. CRM > (Select any opportunity with at least one message) > Mouse hover on elapsed time (e.g. "an hour ago")
    
    Expected:
    
    - It shows the customized time format (e.g. 11/07/2017 5:33:36 PM)
    
    Results:
    
    - It shows the military time format (e.g. Tue Nov 07 2017 17:33:36 GMT+0100)
    
    opw-781647

--- addons/web/static/src/js/widgets/date_picker.js
+++ addons/web/static/src/js/widgets/date_picker.js
@@ -26,7 +26,7 @@ var DateWidget = Widget.extend({
 
         this.name = parent.name;
         this.options = _.defaults(options || {}, {
-            format : time.strftime_to_moment_format((this.type_of_date === 'datetime')? (l10n.date_format + ' ' + l10n.time_format) : l10n.date_format),
+            format : this.type_of_date === 'datetime' ? time.getLangDatetimeFormat() : time.getLangDateFormat(),
             minDate: moment({ y: 1900 }),
             maxDate: moment().add(200, "y"),
             calendarWeeks: true,
