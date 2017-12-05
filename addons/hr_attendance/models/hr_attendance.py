0eddf83903d794066184fffa4ce3cf3914c8eb28
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-12-05 13:09:11 +0100

    [FIX] hr_attendance: Make department related field readonly
    Related fields that are not readonly are rewritten each time you save the
    associated record, provoking performance problems.
    
    Moreover, we don't want to allow an employee to change its department through
    an attendance record.
    
    Closes #21441

--- addons/hr_attendance/models/hr_attendance.py
+++ addons/hr_attendance/models/hr_attendance.py
@@ -16,7 +16,8 @@ class HrAttendance(models.Model):
         return self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
 
     employee_id = fields.Many2one('hr.employee', string="Employee", default=_default_employee, required=True, ondelete='cascade', index=True)
-    department_id = fields.Many2one('hr.department', string="Department", related="employee_id.department_id")
+    department_id = fields.Many2one('hr.department', string="Department", related="employee_id.department_id",
+        readonly=True)
     check_in = fields.Datetime(string="Check In", default=fields.Datetime.now, required=True)
     check_out = fields.Datetime(string="Check Out")
     worked_hours = fields.Float(string='Worked Hours', compute='_compute_worked_hours', store=True, readonly=True)
