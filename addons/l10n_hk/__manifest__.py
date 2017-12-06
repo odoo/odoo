504519e1110323c8470e0f60153f27b55bb27994
qdp-odoo <qdp@odoo.com>
2017-12-06 10:45:54 +0100

    [REM] l10n_hk: removed taxes

--- addons/l10n_hk/__manifest__.py
+++ addons/l10n_hk/__manifest__.py
@@ -11,7 +11,6 @@
     'depends': ['account'],
     'data': [
         'data/account_chart_template_data.xml',
-        'data/account_tax_template_data.xml',
         'data/l10n_hk_chart_data.xml',
         'data/account_chart_template_configure_data.xml',
     ],
