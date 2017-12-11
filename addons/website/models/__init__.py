13de846feac701ecbfdf15e3c2631edbe63621ce
Adrian Torres <adt@odoo.com>
2017-12-11 11:37:49 +0100

    Yet another booboo

--- addons/website/models/__init__.py
+++ addons/website/models/__init__.py
@@ -7,5 +7,6 @@ from . import ir_http
 from . import ir_qweb
 from . import website
 from . import ir_ui_view
+from . import res_company
 from . import res_partner
 from . import res_config_settings
---
+++ addons/website/models/res_company.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class Company(models.Model):
+    _inherit = "res.company"
+
+    @api.multi
+    def google_map_img(self, zoom=8, width=298, height=298):
+        partner = self.sudo().partner_id
+        return partner and partner.google_map_img(zoom, width, height) or None
+
+    @api.multi
+    def google_map_link(self, zoom=8):
+        partner = self.sudo().partner_id
+        return partner and partner.google_map_link(zoom) or None
