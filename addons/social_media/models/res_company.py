9a7fd72197799c0815f0d2e0ed4f895f674a053d
Adrian Torres <adt@odoo.com>
2017-12-12 14:56:59 +0000

    [ADD] social_media: new module for social media
    This technical dependency module adds settings to res_company for the
    company's social media pages.
    
    It is introduced to fix duplicated fields for res_company in website and
    mass_mailing, these two modules now require social_media as a dependency

--- addons/social_media/models/res_company.py
+++ addons/social_media/models/res_company.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
+
 
 class Company(models.Model):
     _inherit = "res.company"
 
-    # NB: Also defined in website
     social_twitter = fields.Char('Twitter Account')
     social_facebook = fields.Char('Facebook Account')
     social_github = fields.Char('GitHub Account')
