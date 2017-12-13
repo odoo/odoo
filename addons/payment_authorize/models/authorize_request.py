9794affd1ba4cf323786bd5fb15cdfa4ae6e02e0
fda-odoo <fda@odoo.com>
2017-12-13 16:07:59 +0100

    [FIX] Payment, payment_authorize: show an error if customer doesn't have a complete profil on authorize.net payment.
    If the user has no Zip code, country or city, authorize refuse the payment, but Odoo doens't show any error.
    The commit invite the user to log in in this case or to fill his missing information

--- addons/payment_authorize/models/authorize_request.py
+++ addons/payment_authorize/models/authorize_request.py
@@ -139,6 +139,11 @@ class AuthorizeAPI():
         etree.SubElement(payment_profile, "customerType").text = 'business' if partner.is_company else 'individual'
         billTo = etree.SubElement(payment_profile, "billTo")
         etree.SubElement(billTo, "address").text = (partner.street or '' + (partner.street2 if partner.street2 else '')) or None
+        
+        missing_fields = [partner._fields[field].string for field in ['city', 'country_id', 'zip'] if not partner[field]]
+        if missing_fields:
+            raise ValidationError({'missing_fields': missing_fields})
+        
         etree.SubElement(billTo, "city").text = partner.city
         etree.SubElement(billTo, "state").text = partner.state_id.name or None
         etree.SubElement(billTo, "zip").text = partner.zip
