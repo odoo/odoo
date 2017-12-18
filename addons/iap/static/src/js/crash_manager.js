4bac59b6f8d244901c66b4f305daf8c72c84fcec
Rémi Rahir <rar@odoo.com>
2017-12-18 16:52:19 +0100

    [FIX] iap: translation terms
    Added missing translation on iap error modal.

--- addons/iap/static/src/js/crash_manager.js
+++ addons/iap/static/src/js/crash_manager.js
@@ -37,7 +37,7 @@ CrashManager.include({
                     title: error_data.title || _t("Insufficient Balance"),
                     $content: content,
                     buttons: [
-                        {text: 'Buy credits at Odoo', classes : "btn-primary", click: function() {
+                        {text: _t('Buy credits at Odoo'), classes : "btn-primary", click: function() {
                             window.open(url, '_blank');
                         }, close:true},
                         {text: _t("Cancel"), close: true}
