af34d3b9ef2a3bbfcbbfefd8819a17e0bfbfd867
dip-odoo <dip@odoo.com>
2017-12-20 19:03:22 +0530

    [IMP] stock: validate inventory adjustment with product tracked
    When you have some product tracked in an inventory adjustment, the
    lot/serial number is not required, so when you confirm an inventory
    adjustment with product tracked for which you didn't provide serial/lot
    number you get a warning to inform you.
    
    TASK-30921

--- addons/stock/models/stock_inventory.py
+++ addons/stock/models/stock_inventory.py
@@ -4,7 +4,7 @@
 from odoo import api, fields, models, _
 from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError
-from odoo.tools import float_utils
+from odoo.tools import float_utils, float_compare
 
 
 class Inventory(models.Model):
@@ -161,6 +161,23 @@ class Inventory(models.Model):
     def action_reset_product_qty(self):
         self.mapped('line_ids').write({'product_qty': 0})
         return True
+
+    def action_validate(self):
+        inventory_lines = self.line_ids.filtered(lambda l: l.product_id.tracking in ['lot', 'serial'] and not l.prod_lot_id and l.theoretical_qty != l.product_qty)
+        lines = self.line_ids.filtered(lambda l: float_compare(l.product_qty, 1, precision_rounding=l.product_uom_id.rounding) > 0 and l.product_id.tracking == 'serial' and l.prod_lot_id)
+        if inventory_lines and not lines:
+            wiz_lines = [(0, 0, {'product_id': product.id, 'tracking': product.tracking}) for product in inventory_lines.mapped('product_id')]
+            wiz = self.env['stock.track.confirmation'].create({'inventory_id': self.id, 'tracking_line_ids': wiz_lines})
+            return {
+                    'name': _('Tracked Products in Inventory Adjustment'),
+                    'type': 'ir.actions.act_window',
+                    'view_mode': 'form',
+                    'res_model': 'stock.track.confirmation',
+                    'target': 'new',
+                    'res_id': wiz.id,
+                }
+        else:
+            self.action_done()
 
     def action_done(self):
         negative = next((line for line in self.mapped('line_ids') if line.product_qty < 0 and line.product_qty != line.theoretical_qty), False)
--- addons/stock/wizard/__init__.py
+++ addons/stock/wizard/__init__.py
@@ -10,3 +10,4 @@ from . import stock_overprocessed_transfer
 from . import stock_quantity_history
 from . import stock_warn_insufficient_qty
 from . import product_replenish
+from . import stock_track_confirmation
