c0769c753c4b4d5fa0b5a6a545f5f0141b71e83a
Nicolas Martinelli <nim@odoo.com>
2017-12-22 12:36:26 +0100

    [FIX] account: prevent traceback
    When changing the date of an account move, the method
    `_onchange_amount_currency` is called manually on a recordset. If the
    recordset length is > 1, it crashes.

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -518,7 +518,7 @@ class AccountMoveLine(models.Model):
         for line in self:
             amount = line.amount_currency
             if line.currency_id and line.currency_id != line.company_currency_id:
-                amount = self.currency_id.with_context(date=line.date).compute(amount, line.company_currency_id)
+                amount = line.currency_id.with_context(date=line.date).compute(amount, line.company_currency_id)
                 line.debit = amount > 0 and amount or 0.0
                 line.credit = amount < 0 and -amount or 0.0
 
