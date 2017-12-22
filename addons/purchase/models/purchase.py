5e100cb642729cb6a75a70bb941b3752d72e3fac
Benjamin Willig <benjamin.willig@acsone.eu>
2017-12-22 12:26:38 +0100

    [FIX] Allow to keep the state of a PO when logging a message
    Steps to reproduce the bug:
    
    - Create a new RFQ
    - When it's in draft state, log an internal note using the 'mail.compose.message' dialog.
    
    Bug:
    
    - RFQ is now in 'sent' state.
    
    The fix allows to inherit from mail_purchase_order_on_send method to change the base behavior.
    
    opw:801549

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -1165,10 +1165,14 @@ class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
     @api.multi
+    def mail_purchase_order_on_send(self):
+        if not self.filtered('subtype_id.internal'):
+            order = self.env['purchase.order'].browse(self._context['default_res_id'])
+            if order.state == 'draft':
+                order.state = 'sent'
+
+    @api.multi
     def send_mail(self, auto_commit=False):
         if self._context.get('default_model') == 'purchase.order' and self._context.get('default_res_id'):
-            if not self.filtered('subtype_id.internal'):
-                order = self.env['purchase.order'].browse([self._context['default_res_id']])
-                if order.state == 'draft':
-                    order.state = 'sent'
+            self.mail_purchase_order_on_send()
         return super(MailComposeMessage, self.with_context(mail_post_autofollow=True)).send_mail(auto_commit=auto_commit)
