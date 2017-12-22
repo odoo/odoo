5c4e6ef11336a625d2474fa48307a03b6110b35b
Adrian Torres <adt@odoo.com>
2017-12-22 10:37:29 +0100

    what the fuck

--- addons/mail/models/ir_model.py
+++ addons/mail/models/ir_model.py
@@ -8,7 +8,7 @@ class IrModel(models.Model):
     _inherit = 'ir.model'
 
     def unlink(self):
-            # Delete followers for models that will be unlinked.
-            query = "DELETE FROM mail_followers WHERE res_model IN %s"
-            self.env.cr.execute(query, [tuple(self.mapped('model'))])
-            return super(IrModel, self).unlink()
+        # Delete followers for models that will be unlinked.
+        query = "DELETE FROM mail_followers WHERE res_model IN %s"
+        self.env.cr.execute(query, [tuple(self.mapped('model'))])
+        return super(IrModel, self).unlink()
