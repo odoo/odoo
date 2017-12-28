b89fbfa2f86c594a15eb33f8c47fd717c5b76b45
Atchuthan, Sodexis <atchuthan@sodexis.com>
2017-12-28 15:34:41 +0530

    [FIX] Issue to resolve 'There was an error processing your payment: transaction issue.'

--- addons/sale_payment/models/payment.py
+++ addons/sale_payment/models/payment.py
@@ -63,6 +63,7 @@ class PaymentTransaction(models.Model):
             _logger.info('<%s> transaction authorized, auto-confirming order %s (ID %s)', self.acquirer_id.provider, self.sale_order_id.name, self.sale_order_id.id)
             if self.sale_order_id.state in ('draft', 'sent'):
                 self.sale_order_id.with_context(send_email=True).action_confirm()
+            return True
 
         if self.state == 'done':
             _logger.info('<%s> transaction completed, auto-confirming order %s (ID %s) and generating invoice', self.acquirer_id.provider, self.sale_order_id.name, self.sale_order_id.id)
