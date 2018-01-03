3f234cccb36cdb5781f8d7379518dfeec36cc1b2
Thibault Delavallée <tde@odoo.com>
2018-01-03 17:03:27 +0100

    [REF] mail: move message notify and author subscription in after_post hook method
    Purpose
     * move notify in message_post instead of always doing it at message
       creation
    
    Specifications
     * message create should only create a message
     * message post should be in charge of calling the notification process

--- addons/mail/models/mail_message.py
+++ addons/mail/models/mail_message.py
@@ -758,9 +758,6 @@ class Message(models.Model):
 
         message._invalidate_documents()
 
-        if not self.env.context.get('message_create_from_mail_mail'):
-            message._notify(force_send=self.env.context.get('mail_notify_force_send', True),
-                            user_signature=self.env.context.get('mail_notify_user_signature', True))
         return message
 
     @api.multi
