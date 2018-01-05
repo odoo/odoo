2511dfbb1c9c42d7dda2b2b3c3dbca88dc403099
Martin Trigaux <mat@odoo.com>
2018-01-05 17:27:58 +0100

    [IMP] base: do not generate translations for untranslatable languages
    The untranslatable langues were generating translation
    Followup and fixes #9479

--- odoo/addons/base/models/ir_translation.py
+++ odoo/addons/base/models/ir_translation.py
@@ -592,7 +592,7 @@ class IrTranslation(models.Model):
             query = """ INSERT INTO ir_translation (lang, type, name, res_id, src, value, module)
                         SELECT l.code, 'model', %(name)s, %(res_id)s, %(src)s, %(src)s, %(module)s
                         FROM res_lang l
-                        WHERE l.active AND NOT EXISTS (
+                        WHERE l.active AND l.translatable AND NOT EXISTS (
                             SELECT 1 FROM ir_translation
                             WHERE lang=l.code AND type='model' AND name=%(name)s AND res_id=%(res_id)s AND src=%(src)s
                         );
@@ -612,7 +612,7 @@ class IrTranslation(models.Model):
             query = """ INSERT INTO ir_translation (lang, type, name, res_id, src, value, module)
                         SELECT l.code, 'model', %(name)s, %(res_id)s, %(src)s, %(src)s, %(module)s
                         FROM res_lang l
-                        WHERE l.active AND l.code != 'en_US' AND NOT EXISTS (
+                        WHERE l.active AND l.translatable AND l.code != 'en_US' AND NOT EXISTS (
                             SELECT 1 FROM ir_translation
                             WHERE lang=l.code AND type='model' AND name=%(name)s AND res_id=%(res_id)s
                         );
