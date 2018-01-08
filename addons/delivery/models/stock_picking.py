52a85e57b9a6353c14abdbcd7e04d34214f151f5
Pierre Masereel <pim@odoo.com>
2018-01-08 16:25:09 +0100

    [IMP] stock: configurable product's weight UOM
    This commit adds the possibility to chose between pounds and kilograms
    to express the weight field on the product. This is done so that
    companies that worked internally in pounds do not have to convert their
    weights back to kilograms for a consistent computation of the rates in
    delivery.
    
    We complete the general settings with the selection of the weight's uom.
    Validating the setting will create an ir.config.parameter. A static
    method defined on product template will return the chosen UOM.
    
    We display "weight_uom_name" on the form view so that the UOM is not
    clickable/editable because we fear user would want to edit the UOM
    instead of changing it in the res config.
    
    We move the view definition allowing to fill the product's weight from
    stock to product to help the generation of the intrastat report without
    installing inventory.

--- addons/delivery/models/stock_picking.py
+++ addons/delivery/models/stock_picking.py
@@ -51,12 +51,6 @@ class StockMoveLine(models.Model):
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    def _default_uom(self):
-        weight_uom_id = self.env.ref('product.product_uom_kgm', raise_if_not_found=False)
-        if not weight_uom_id:
-            uom_categ_id = self.env.ref('product.product_uom_categ_kgm').id
-            weight_uom_id = self.env['product.uom'].search([('category_id', '=', uom_categ_id), ('factor', '=', 1)], limit=1)
-        return weight_uom_id
 
     @api.one
     @api.depends('move_line_ids')
@@ -89,7 +83,7 @@ class StockPicking(models.Model):
     weight = fields.Float(compute='_cal_weight', digits=dp.get_precision('Stock Weight'), store=True)
     carrier_tracking_ref = fields.Char(string='Tracking Reference', copy=False)
     carrier_tracking_url = fields.Char(string='Tracking URL', compute='_compute_carrier_tracking_url')
-    weight_uom_id = fields.Many2one('product.uom', string='Unit of Measure', required=True, readonly="1", help="Unit of measurement for Weight", default=_default_uom)
+    weight_uom_id = fields.Many2one('product.uom', string='Unit of Measure', compute='_compute_weight_uom_id', help="Unit of measurement for Weight")
     package_ids = fields.Many2many('stock.quant.package', compute='_compute_packages', string='Packages')
     weight_bulk = fields.Float('Bulk Weight', compute='_compute_bulk_weight')
     shipping_weight = fields.Float("Weight for Shipping", compute='_compute_shipping_weight')
@@ -98,6 +92,11 @@ class StockPicking(models.Model):
     def _compute_carrier_tracking_url(self):
         for picking in self:
             picking.carrier_tracking_url = picking.carrier_id.get_tracking_link(picking) if picking.carrier_id and picking.carrier_tracking_ref else False
+
+    def _compute_weight_uom_id(self):
+        weight_uom_id = self.env['product.template']._get_weight_uom_id_from_ir_config_parameter()
+        for picking in self:
+            picking.weight_uom_id = weight_uom_id
 
     @api.depends('move_lines')
     def _cal_weight(self):
