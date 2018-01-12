2d5f1dda19a95435632f52150f7214458e18cbe8
Haresh Shyara <hsh@odoo.com>
2018-01-12 12:54:43 +0530

    [IMP] stock, mrp, repair: get linked consumed or produced lines for this operation
    - When unfold down MO, Repair Order, Unbuild Order to display produced or consumed materials even product is trackable or not.
    - Normal products(not trackable) are directly displayed with source and destination not foldable.
    - Unfoldable line has no caret icon to unfold, so move it to margin-left 15px to make parellel.
    - Invisible source and destination location on head lines if product trackable.

--- addons/mrp/models/stock_traceability.py
+++ addons/mrp/models/stock_traceability.py
@@ -19,3 +19,12 @@ class MrpStockReport(models.TransientModel):
             res_id = move_line.move_id.consume_unbuild_id.id
             ref = move_line.move_id.consume_unbuild_id.name
         return res_model, res_id, ref
+
+    @api.model
+    def _get_linked_move_lines(self, move_line):
+        move_lines, is_used = super(MrpStockReport, self)._get_linked_move_lines(move_line)
+        if not move_lines:
+            move_lines = (move_line.move_id.consume_unbuild_id and move_line.produce_line_ids) or (move_line.move_id.production_id and move_line.consume_line_ids)
+        if not is_used:
+            is_used = (move_line.move_id.unbuild_id and move_line.consume_line_ids) or (move_line.move_id.raw_material_production_id and move_line.produce_line_ids)
+        return move_lines, is_used
