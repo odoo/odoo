ff155033c3e4abf8e0f604dd01e1fe59e7b928e9
Nicolas Lempereur <nle@odoo.com>
2018-01-15 16:24:52 +0100

    [FIX] website{,_slides}: display date in locale
    In 10.0 since b617ea69 some parts such as the date/datetime widget are
    adapted to the current locale and not just displayed in english.
    
    But the dates in the slides module are always in english because they
    don't wait for the locale to be loaded.
    
    To wait for the locale, this commit also adds a reference localeDef on
    the website.website module.
    
    note: this fix is only for [10.0,11.0[, another fix must be done in 11.0
    
    opw-804555
    closes #22241

--- addons/website_slides/static/src/js/slides.js
+++ addons/website_slides/static/src/js/slides.js
@@ -7,6 +7,7 @@ var core = require('web.core');
 var time = require('web.time');
 var Widget = require('web.Widget');
 var local_storage = require('web.local_storage');
+var website = require('website.website');
 
 var _t = core._t;
 var page_widgets = {};
@@ -15,19 +16,20 @@ $(document).ready(function () {
 
     var widget_parent = $('body');
 
-
-    $("timeago.timeago").each(function (index, el) {
-        var datetime = $(el).attr('datetime'),
-            datetime_obj = time.str_to_datetime(datetime),
-            // if presentation 7 days, 24 hours, 60 min, 60 second, 1000 millis old(one week)
-            // then return fix formate string else timeago
-            display_str = "";
-        if (datetime_obj && new Date().getTime() - datetime_obj.getTime() > 7 * 24 * 60 * 60 * 1000) {
-            display_str = datetime_obj.toDateString();
-        } else {
-            display_str = moment(datetime_obj).fromNow();
-        }
-        $(el).text(display_str);
+    website.localeDef.then(function () {
+        $("timeago.timeago").each(function (index, el) {
+            var datetime = $(el).attr('datetime'),
+                datetime_obj = time.str_to_datetime(datetime),
+                // if presentation 7 days, 24 hours, 60 min, 60 second, 1000 millis old(one week)
+                // then return fix formate string else timeago
+                display_str = "";
+            if (datetime_obj && new Date().getTime() - datetime_obj.getTime() > 7 * 24 * 60 * 60 * 1000) {
+                display_str = moment(datetime_obj).format('ll');
+            } else {
+                display_str = moment(datetime_obj).fromNow();
+            }
+            $(el).text(display_str);
+        });
     });
 
     // To prevent showing channel settings alert box once user closed it.
