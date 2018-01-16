ce91cf8f33a4cab2e5f266417bc37702482c73b2
Adrian Torres <adt@odoo.com>
2018-01-16 10:39:13 +0100

    [FIX] stock: remove existing demo data on reinstall
    Previous to this rev.:
    
        - Install stock w/ demo data
        - Uninstall stock
        - Reinstall stock
    
    Result: Boom! Installation crashes.
    
    Reason: The demo data installed with stock is not properly deleted after
    the module is uninstalled, which causes issues/conflicts upon
    reinstallation of the same demo data.
    
    Solution: Remove any demo data of stock models upon installation if any
    exist through a pre_init_hook, ideally this would be done by overriding
    the unlink() of the affected models / setting some ondelete='cascade',
    or even a uninstall_hook, however these solutions prove insufficient for
    databases that are currently affected by the problem.
    
    Fixes #22243

--- addons/stock/__init__.py
+++ addons/stock/__init__.py
@@ -5,3 +5,14 @@ import controllers
 import models
 import report
 import wizard
+
+
+from odoo import api, SUPERUSER_ID
+
+
+# TODO: Apply proper fix & remove in master
+def pre_init_hook(cr):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['ir.model.data'].search([
+        ('model', 'like', '%stock%')
+    ]).unlink()
