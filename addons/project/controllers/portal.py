30def14197cf1893fee12cac059d453ef72da3c8
Barad Mahendra <mba@openerp.com>
2018-01-17 10:52:12 +0530

    [FIX] Project: Portal project traceback
    if project privacy visibility is not portal then instead of traceback redirect to my portal view
    
    Task ID: 30985

--- addons/project/controllers/portal.py
+++ addons/project/controllers/portal.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 from operator import itemgetter
 
 from odoo import http, _
+from odoo.exceptions import AccessError
 from odoo.http import request
 from odoo.addons.portal.controllers.portal import get_records_pager, CustomerPortal, pager as portal_pager
 from odoo.tools import groupby as groupbyelem
@@ -73,6 +74,11 @@ class CustomerPortal(CustomerPortal):
     @http.route(['/my/project/<int:project_id>'], type='http', auth="user", website=True)
     def portal_my_project(self, project_id=None, **kw):
         project = request.env['project.project'].browse(project_id)
+        try:
+            project.check_access_rule('read')
+        except AccessError:
+            return request.redirect('/my')
+
         vals = {'project': project}
         history = request.session.get('my_projects_history', [])
         vals.update(get_records_pager(history, project))
@@ -180,6 +186,11 @@ class CustomerPortal(CustomerPortal):
     @http.route(['/my/task/<int:task_id>'], type='http', auth="user", website=True)
     def portal_my_task(self, task_id=None, **kw):
         task = request.env['project.task'].browse(task_id)
+        try:
+            task.check_access_rule('read')
+        except AccessError:
+            return request.redirect('/my')
+
         vals = {
             'task': task,
             'user': request.env.user
