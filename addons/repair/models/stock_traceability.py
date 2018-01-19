ac819909d00a4cae32532cc817ed316225e827a1
Haresh Shyara <hsh@odoo.com>
2018-01-19 17:15:24 +0530

    [IMP] stock, mrp, repair: optimization traceability report based on stock move lines
    - Optimized _lines() to fold/unfold mechanism and use stock move line in get lines.
    - Merged into single method (make_dict_move and make_dict_head) to get parent & child traceability result
      and use unfolfable to make line unfoldable.
    - Removed _get_produced_or_consumed_vals method and move mechanisam into _line method will do same for this.
    - Make some methods private.
    - Removed useless vals from dict.

--- addons/repair/models/stock_traceability.py
+++ addons/repair/models/stock_traceability.py
@@ -4,8 +4,8 @@ class MrpStockReport(models.TransientModel):
     _inherit = 'stock.traceability.report'
 
     @api.model
-    def get_links(self, move_line):
-        res_model, res_id, ref = super(MrpStockReport, self).get_links(move_line)
+    def _get_reference(self, move_line):
+        res_model, res_id, ref = super(MrpStockReport, self)._get_reference(move_line)
         if move_line.move_id.repair_id:
             res_model = 'repair.order'
             res_id = move_line.move_id.repair_id.id
