4b68a7b30afee019efb4c08365de79050aad08f8
Christophe Simonis <chs@odoo.com>
2018-01-22 19:08:57 +0100

    [FIX] hr_timesheet: add missing import

--- addons/hr_timesheet/models/project.py
+++ addons/hr_timesheet/models/project.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class Project(models.Model):
