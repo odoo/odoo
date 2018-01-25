0fb379bcb1d3bf9fc114c3367f1ce3f2e05cba39
Nicolas Martinelli <nim@odoo.com>
2018-01-25 13:53:28 +0100

    [FIX] purchase: change invoice date
    - Main currency: USD
    - Create a PO in EUR
    - Create an invoice from the PO
    - The invoice is in USD and the exchange rate is applied
    - Change the currency to EUR
    
    The conversion is not applied anymore.
    
    The original issue was that changing the date would overwrite any amount
    modified in the invoice. Actually, the dependency on the date is not
    really necessary. Indeed, this is supposed to help enconding invoices,
    but it is likely that:
    - If the PO is issued in a currency A, the bill received is in currency
      A.
      => no conversion necessary (solved from 11.1).
    - If the PO is issued in a currency A and the bill received is in
      currency B, the exchange rate used by the vendor is different from
      ours.
      => manual corrections are necessary
    
    Therefore, we remove the dependency on the date for the amount
    recomputation. However, we still use it in the onchange. This way:
    - the amount estimated should be close to the amount requested
    - we avoid unexpected recomputation of amounts
    
    Introduced with 836ff7e79f.
    
    opw-805926

--- addons/purchase/models/account_invoice.py
+++ addons/purchase/models/account_invoice.py
@@ -85,13 +85,11 @@ class AccountInvoice(models.Model):
         self.purchase_id = False
         return {}
 
-    @api.onchange('currency_id', 'date_invoice')
+    @api.onchange('currency_id')
     def _onchange_currency_id(self):
         if self.currency_id:
             for line in self.invoice_line_ids.filtered(lambda r: r.purchase_line_id):
-                if line.purchase_id.currency_id != self.currency_id:
-                    line.price_unit = line.purchase_id.currency_id.with_context(date=self.date_invoice)\
-                        .compute(line.purchase_line_id.price_unit, self.currency_id, round=False)
+                line.price_unit = line.purchase_id.currency_id.with_context(date=self.date_invoice).compute(line.purchase_line_id.price_unit, self.currency_id, round=False)
 
     @api.onchange('invoice_line_ids')
     def _onchange_origin(self):
