827f9cf7536598e1123fd010806f5e9b664620ec
Hetashree Chauhan <hch@odoo.com>
2018-01-29 12:08:13 +0530

    [ADD] stock: Report Forecasted Inventory
    Purpose of this task:
    Show forecasted inventory for all products.
    Before this commit we have to search forecasted inventory for each product individually.
    There is no way to check all the forecasted inventory in a single menu.
    
    After this commit:
    User can search for all forecasted inventory from the menu 'Forecasted Inventory'.
    Features added.
    1. Menu for Forecasted Inventory has been added.
    2. List and pivot view for Forecasted Inventory added.
    3. Default measures has been set to 'Quantity' and 'Cumulative Quantity'.
    4. Pivot view is grouped by product (vertical) and by weeks (horizontal).
    
    Why query changes required:
    User can search for all forecasted inventory from the menu 'Forecasted Inventory'.
    Features added.
    1. lines only those containing any stock move will be shown.
    2. Reference field added.
    3. We remove 'week' from the query, instead of we fetch all the data according to 'date_expected',
    and we create three group_bys (i.e. group by date, week and month)
    so it is easy for the user to check report according to day, week and month.
    
    This commit is related to task 36088.

--- addons/stock/report/report_stock_forecast.py
+++ addons/stock/report/report_stock_forecast.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, tools
 
@@ -14,49 +13,54 @@ class ReportStockForecat(models.Model):
     cumulative_quantity = fields.Float(string='Cumulative Quantity', readonly=True)
     quantity = fields.Float(readonly=True)
     company_id = fields.Many2one('res.company', string='Company', readonly=True)
+    reference = fields.Char('Reference')
 
     @api.model_cr
     def init(self):
         tools.drop_view_if_exists(self._cr, 'report_stock_forecast')
         self._cr.execute("""CREATE or REPLACE VIEW report_stock_forecast AS (SELECT
-        MIN(id) as id,
+        row_number() OVER (ORDER BY final.date,final.id) AS id,
         product_id as product_id,
-        date as date,
+        date(final.date) as date,
         sum(product_qty) AS quantity,
-        sum(sum(product_qty)) OVER (PARTITION BY product_id ORDER BY date) AS cumulative_quantity,
-        company_id
+        sum(sum(product_qty)) OVER (PARTITION BY final.product_id, final.company_id ORDER BY final.date) AS cumulative_quantity,
+        reference,
+        final.company_id
         FROM
         (SELECT
         MIN(id) as id,
         MAIN.product_id as product_id,
-        SUB.date as date,
+        date(SUB.date) as date,
         CASE WHEN MAIN.date = SUB.date THEN sum(MAIN.product_qty) ELSE 0 END as product_qty,
+        MAIN.reference as reference,
         MAIN.company_id as company_id
         FROM
         (SELECT
-            MIN(sq.id) as id,
-            sq.product_id,
-            date_trunc('week', to_date(to_char(CURRENT_DATE, 'YYYY/MM/DD'), 'YYYY/MM/DD')) as date,
+            MIN(-product.id) as id,
+            product.id as product_id,
+            date(CURRENT_DATE) as date,
             SUM(sq.quantity) AS product_qty,
+            'Starting Inventory' as reference,
             sq.company_id
             FROM
-            stock_quant as sq
+            product_product as product
             LEFT JOIN
-            product_product ON product_product.id = sq.product_id
+            stock_quant sq ON product.id = sq.product_id
             LEFT JOIN
             stock_location location_id ON sq.location_id = location_id.id
             WHERE
-            location_id.usage = 'internal'
-            GROUP BY date, sq.product_id, sq.company_id
+            location_id.usage = 'internal' or location_id.id is null
+            GROUP BY date, product.id, sq.company_id, sq.quantity
             UNION ALL
             SELECT
-            MIN(-sm.id) as id,
+            MIN(sm.id) as id,
             sm.product_id,
             CASE WHEN sm.date_expected > CURRENT_DATE
-            THEN date_trunc('week', to_date(to_char(sm.date_expected, 'YYYY/MM/DD'), 'YYYY/MM/DD'))
-            ELSE date_trunc('week', to_date(to_char(CURRENT_DATE, 'YYYY/MM/DD'), 'YYYY/MM/DD')) END
+            THEN date(sm.date_expected)
+            ELSE date(CURRENT_DATE) END
             AS date,
             SUM(sm.product_qty) AS product_qty,
+            sm.reference as reference,
             sm.company_id
             FROM
                stock_move as sm
@@ -69,16 +73,17 @@ class ReportStockForecat(models.Model):
             WHERE
             sm.state IN ('confirmed','partially_available','assigned','waiting') and
             source_location.usage != 'internal' and dest_location.usage = 'internal'
-            GROUP BY sm.date_expected,sm.product_id, sm.company_id
+            GROUP BY sm.date_expected,sm.product_id, sm.company_id, sm.reference
             UNION ALL
             SELECT
-                MIN(-sm.id) as id,
+                MIN(sm.id) as id,
                 sm.product_id,
                 CASE WHEN sm.date_expected > CURRENT_DATE
-                    THEN date_trunc('week', to_date(to_char(sm.date_expected, 'YYYY/MM/DD'), 'YYYY/MM/DD'))
-                    ELSE date_trunc('week', to_date(to_char(CURRENT_DATE, 'YYYY/MM/DD'), 'YYYY/MM/DD')) END
+                    THEN date(sm.date_expected)
+                    ELSE date(CURRENT_DATE) END
                 AS date,
                 SUM(-(sm.product_qty)) AS product_qty,
+                sm.reference AS reference,
                 sm.company_id
             FROM
                stock_move as sm
@@ -91,25 +96,74 @@ class ReportStockForecat(models.Model):
             WHERE
                 sm.state IN ('confirmed','partially_available','assigned','waiting') and
             source_location.usage = 'internal' and dest_location.usage != 'internal'
-            GROUP BY sm.date_expected,sm.product_id, sm.company_id)
+            GROUP BY sm.date_expected,sm.product_id, sm.company_id, sm.reference)
          as MAIN
      LEFT JOIN
-     (SELECT DISTINCT date
-      FROM
-      (
-             SELECT date_trunc('week', CURRENT_DATE) AS DATE
-             UNION ALL
-             SELECT date_trunc('week', to_date(to_char(sm.date_expected, 'YYYY/MM/DD'), 'YYYY/MM/DD')) AS date
-             FROM stock_move sm
-             LEFT JOIN
-             stock_location source_location ON sm.location_id = source_location.id
-             LEFT JOIN
-             stock_location dest_location ON sm.location_dest_id = dest_location.id
-             WHERE
-             sm.state IN ('confirmed','assigned','waiting') and sm.date_expected > CURRENT_DATE and
-             ((dest_location.usage = 'internal' AND source_location.usage != 'internal')
-              or (source_location.usage = 'internal' AND dest_location.usage != 'internal'))) AS DATE_SEARCH)
-             SUB ON (SUB.date IS NOT NULL)
-    GROUP BY MAIN.product_id,SUB.date, MAIN.date, MAIN.company_id
+    (SELECT DISTINCT date
+     FROM
+     (
+            SELECT date_trunc('day', CURRENT_DATE) AS DATE
+            UNION ALL
+            SELECT date(sm.date_expected) AS date
+            FROM stock_move sm
+            LEFT JOIN
+            stock_location source_location ON sm.location_id = source_location.id
+            LEFT JOIN
+            stock_location dest_location ON sm.location_dest_id = dest_location.id
+            WHERE
+            sm.state IN ('confirmed','assigned','waiting') and sm.date_expected > CURRENT_DATE and
+            ((dest_location.usage = 'internal' AND source_location.usage != 'internal')
+             or (source_location.usage = 'internal' AND dest_location.usage != 'internal'))) AS DATE_SEARCH)
+            SUB ON (SUB.date IS NOT NULL)
+    GROUP BY MAIN.product_id,SUB.date, MAIN.date, MAIN.company_id,MAIN.reference
     ) AS FINAL
-    GROUP BY product_id,date,company_id)""")
+    LEFT JOIN
+        stock_picking sp on final.reference=sp.name
+    Where final.product_qty != 0 or final.reference = 'Starting Inventory'
+    GROUP BY product_id,final.date,final.id,final.company_id,final.reference,sp.priority,sp.date,sp.id)""")
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        if not orderby:
+            orderby = 'date, id'  # Sort result by date_expected and id
+        if 'quantity' not in fields and 'cumulative_quantity' in fields:
+            fields.append('quantity')
+        res = super(ReportStockForecat, self).read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
+        list_index = []
+        lists = []
+        qty_for_final_line = []
+        cumulative_qty_final_line = []
+        res_list = {}
+        line_having_id = []
+        if 'cumulative_quantity' in fields:
+            for line in res:
+                if line.get('product_id'):
+                    index = res.index(line)
+                    if res_list.get(line['product_id']):
+                        res_list[line['product_id']].append(line['quantity'])  # Get cumulative quantity product wise
+                    else:
+                        res_list[line['product_id']] = [line['quantity']]
+                    line['cumulative_quantity'] = sum(res_list[line['product_id']])
+                    if line.get('reference') == 'Starting Inventory':
+                        if line.get('quantity'):
+                            line['cumulative_quantity'] = line['quantity']
+                        else:
+                            if index > 1 and line.get('__count') > 1:
+                                line['cumulative_quantity'] = res[index-1]['cumulative_quantity']
+                    else:
+                        if not line.get('reference'):
+                            list_index.append(res.index(line))
+                else:
+                        if line.get('quantity'):
+                            lists.append(line['quantity'])
+                        line['cumulative_quantity'] = sum(lists)
+        for line in res:
+            if line.get('id'):
+                line_having_id.append(line)
+            if not line.get('reference'):
+                qty_for_final_line.append(line['quantity'])
+                cumulative_qty_final_line.append(line['cumulative_quantity'])
+            if line_having_id:
+                line_having_id[0]['quantity'] = sum(qty_for_final_line)
+                line_having_id[0]['cumulative_quantity'] = sum(cumulative_qty_final_line)
+        return res
