6dfcda42cd7e664fcf746fb1385abfea7a049197
stefanorigano <sri@odoo.com>
2018-01-29 13:56:20 +0100

    [IMP] web: tables :hover state
    Properly style table’s rows ‘:hover’ state to let people understand
    that it’s clickable

--- addons/web/static/src/js/views/list/list_renderer.js
+++ addons/web/static/src/js/views/list/list_renderer.js
@@ -637,7 +637,7 @@ var ListRenderer = BasicRenderer.extend({
             return this._super();
         }
 
-        var $table = $('<table>').addClass('o_list_view table table-condensed table-striped');
+        var $table = $('<table>').addClass('o_list_view table table-condensed table-hover table-striped');
         this.$el
             .addClass('table-responsive')
             .append($table);
