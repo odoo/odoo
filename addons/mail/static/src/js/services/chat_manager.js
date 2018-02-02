05cc8be907ea7e28cf54670715c58e1b59b136c0
dbh <dbh@odoo.com>
2018-02-02 16:03:58 +0530

    [FIX] mail: correct chat window dropdown unread counter + hidden state
    Revision on recent JS refactoring: https://github.com/odoo/odoo/commit/a857e0351756a60a4716ca84208ce610cad0e854
    
    Intermediary changes have not been updated with final way of communicating
    with JS services. In particular, widgets must now explicitly call the
    service with this.call instead of relying on a mixin that hides the service call
    (e.g. the chat_mixin that has been removed)

--- addons/mail/static/src/js/services/chat_manager.js
+++ addons/mail/static/src/js/services/chat_manager.js
@@ -1634,7 +1634,7 @@ var ChatManager =  AbstractService.extend({
             web_client.set_title_part("_chat", tabTitle);
         }
 
-        this._sendNotification(web_client, title, content);
+        this.call('bus_service', 'sendNotification', web_client, title, content);
     },
     /**
      * Removes channel
