2f656666be63945889f396b2b9dadb62d4b5c1be
Olivier Colson <oco@odoo.com>
2018-02-08 17:02:09 +0100

    [FIX] l10n_ch: correctly use create and write to initialize ISR reference on bank journals
    Before that, the value was not written properly on the newly-created res.partner.bank object, through the related field.

--- addons/l10n_ch/models/account_journal.py
+++ addons/l10n_ch/models/account_journal.py
@@ -8,11 +8,32 @@ from odoo.exceptions import ValidationError
 from odoo.addons.base_iban.models.res_partner_bank import validate_iban
 from odoo.addons.base.models.res_bank import sanitize_account_number
 
+
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
     # For bank journals
     l10n_ch_postal = fields.Char(related='bank_account_id.l10n_ch_postal')
+
+    @api.model
+    def create(self, vals):
+        rslt = super(AccountJournal, self).create(vals)
+        # The call to super() creates the related bank_account_id field
+
+        if 'l10n_ch_postal' in vals:
+            rslt.l10n_ch_postal = vals['l10n_ch_postal']
+
+        return rslt
+
+    def write(self, vals):
+        rslt = super(AccountJournal, self).write(vals)
+        # The call to super() creates the related bank_account_id field if necessary
+
+        if 'l10n_ch_postal' in vals:
+            for record in self:
+                record.bank_account_id.l10n_ch_postal = vals['l10n_ch_postal']
+
+        return rslt
 
     @api.onchange('bank_acc_number')
     def _onchange_set_l10n_ch_postal(self):
