46a0d75be6f3eb3d170861a20673078a1d6bd230
Romain Derie <rde@odoo.com>
2018-02-08 15:48:50 +0100

    [FIX] web, web_editor: Better email validation with regex (JS)
    This commit improves link creation in web editor.
    
    Before this commit, anything with a @​ would be considered as valid email which
    is obviously not the case. It would then be prefixed by 'mailto:'.
    This was causing 2 issues:
      - URL with @​ symbol in it would have 'mailto:' added as prefix, like google
        maps coordinates.
        eg: mailto:http://www.google.com/maps/whatever/@​312211.22,321321
      - Incorrect emails would go through (eg: @​@​@​, ko@​k@​k@​, mail@​mail)
    
    Now, we will try to detect and identify valid emails from invalid emails from
    URLs. It will also handle 'mailto:' directly typed in the input.
    
    This commit comes with it's counterpart in enterprise https://github.com/odoo/enterprise/pull/1859
    
    This closes #11984, closes #22936
    
    Note that there is a WIP in master to use this new is_email function
    everywhere we try to validate emails. It will aim to uniformize email
    validation in odoo's javascript that are often performed with different mean
    here and there, some more permissive than others, some only checking for @​.

--- addons/web/static/src/js/framework/utils.js
+++ addons/web/static/src/js/framework/utils.js
@@ -418,6 +418,16 @@ function swap(array, elem1, elem2) {
     array[i1] = elem2;
 }
 
+function is_email(value, allow_mailto) {
+    // http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+    var re;
+    if (allow_mailto) {
+        re = /^(mailto:)?(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    } else {
+        re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    }
+    return re.test(value);
+}
 
 return {
     divmod: divmod,
@@ -445,6 +455,7 @@ return {
     xor: xor,
     DropMisordered: DropMisordered,
     swap: swap,
+    is_email: is_email,
 };
 
 });
