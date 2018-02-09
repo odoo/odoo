d0792e565962aee7b4e885cbe4c401f500fce671
RomainLibert <rli@odoo.com>
2018-02-09 16:01:51 +0100

    [IMP] hr_expense : Improve access rights of manager/officer
    Currently, officer can approve anyones leaves
    The distinction between the officer and manager doesn't make sense
    
      * Officer can now only see and approve expenses for which either are
      in his department or he is the manager
      * Officer cannot approve his own expenses anymore

--- addons/hr_expense/tests/common.py
+++ addons/hr_expense/tests/common.py
@@ -21,6 +21,7 @@ class TestExpenseCommon(TestAccountNoChartCommon):
         # create employee
         cls.employee = cls.env['hr.employee'].create({
             'name': 'Johnny Employee',
+            'user_id': cls.user_employee.id,
             'address_home_id': cls.user_employee.partner_id.id,
             'address_id': cls.user_employee.partner_id.id,
         })
@@ -55,4 +56,67 @@ class TestExpenseCommon(TestAccountNoChartCommon):
             'name': 'Second Expense for Johnny Employee',
             'employee_id': cls.employee.id,
             'journal_id': cls.journal.id,
+        })
+
+        Users = cls.env['res.users'].with_context(no_reset_password=True)
+
+        # Find Employee group
+        group_employee_id = cls.env.ref('base.group_user').id
+
+        cls.user_emp2 = Users.create({
+            'name': 'Superboy Employee',
+            'login': 'superboy',
+            'email': 'superboy@example.com',
+            'groups_id': [(6, 0, [group_employee_id])]
+        })
+
+        cls.user_officer = Users.create({
+            'name': 'Batman Officer',
+            'login': 'batman',
+            'email': 'batman.hero@example.com',
+            'groups_id': [(6, 0, [group_employee_id, cls.env.ref('hr_expense.group_hr_expense_user').id])]
+        })
+
+        cls.emp_emp2 = cls.env['hr.employee'].create({
+            'name': 'Superboy',
+            'user_id': cls.user_emp2.id,
+        })
+
+        cls.emp_officer = cls.env['hr.employee'].create({
+            'name': 'Batman',
+            'user_id': cls.user_officer.id,
+        })
+
+        cls.emp_manager = cls.env['hr.employee'].create({
+            'name': 'Superman',
+            'user_id': cls.user_manager.id,
+        })
+
+        cls.rd = cls.env['hr.department'].create({
+            'name': 'R&D',
+            'manager_id': cls.emp_officer.id,
+            'member_ids': [(6, 0, [cls.employee.id])],
+        })
+
+        cls.ps = cls.env['hr.department'].create({
+            'name': 'PS',
+            'manager_id': cls.emp_manager.id,
+            'member_ids': [(6, 0, [cls.emp_emp2.id])],
+        })
+
+        cls.uom_unit = cls.env.ref('uom.product_uom_unit').id
+        cls.uom_dozen = cls.env.ref('uom.product_uom_dozen').id
+
+        cls.product_1 = cls.env['product.product'].create({
+            'name': 'Batmobile repair',
+            'type': 'service',
+            'uom_id': cls.uom_unit,
+            'uom_po_id': cls.uom_unit,
+        })
+
+        cls.product_2 = cls.env['product.product'].create({
+            'name': 'Superboy costume washing',
+            'type': 'service',
+            'uom_id': cls.uom_unit,
+            'uom_po_id': cls.uom_unit,
         })
