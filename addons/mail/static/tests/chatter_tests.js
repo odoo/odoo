78d681d14f815d8f8ffc193568433cb2c4b6a75a
Mehul Patel <mep@odoo.com>
2018-02-09 18:26:54 +0530

    [IMP] mail: composer / chatter new followers email address tooltips
    Before this commit, many2many email tags showed the recipient's name
    and email address. Most of the time, the text was cut off in order to
    fit in the maximum length of a many2many tag, so the email address was
    usually not visible.
    
    With this commit, many2many email tags now show only the recipient's
    name. The recipient's email address is displayed when hovering on the
    tag.
    
    Task-ID 36677

--- addons/mail/static/tests/chatter_tests.js
+++ addons/mail/static/tests/chatter_tests.js
@@ -1336,7 +1336,6 @@ QUnit.module('FieldMany2ManyTagsEmail', {
 QUnit.test('fieldmany2many tags email', function (assert) {
     assert.expect(13);
     var done = assert.async();
-    var nameGottenIds = [[12], [12, 14]];
 
     this.data.partner.records[0].timmy = [12, 14];
 
@@ -1356,11 +1355,7 @@ QUnit.test('fieldmany2many tags email', function (assert) {
             mode: 'edit',
         },
         mockRPC: function (route, args) {
-            if (route === "/web/dataset/call_kw/partner_type/name_get") {
-                assert.deepEqual(args.args[0], nameGottenIds.shift(),
-                    "partner with email should be name_get'ed");
-            }
-            else if (args.method ==='read' && args.model === 'partner_type') {
+            if (args.method ==='read' && args.model === 'partner_type') {
                 assert.step(args.args[0]);
                 assert.deepEqual(args.args[1] , ['display_name', 'email'], "should read the email");
             }
@@ -1373,8 +1368,12 @@ QUnit.test('fieldmany2many tags email', function (assert) {
         // should read it 3 times (1 with the form view, one with the form dialog and one after save)
         assert.verifySteps([[12, 14], [14], [14]]);
         assert.strictEqual(form.$('.o_field_many2manytags[name="timmy"] span.o_tag_color_0').length, 2,
-            "the second tag should be present");
-
+            "two tags should be present");
+        var firstTag = form.$('.o_field_many2manytags[name="timmy"] span.o_tag_color_0').first();
+        assert.strictEqual(firstTag.find('.o_badge_text').text(), "gold",
+            "tag should only show display_name");
+        assert.strictEqual(firstTag.find('.o_badge_text').attr('title'), "coucou@petite.perruche",
+            "tag should show email address on mouse hover");
         form.destroy();
         done();
     });
