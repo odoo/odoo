odoo/odoo

commit c6e9f3c5e028a65adb6b2bdc399ed5e3f6bf8f8e

[FIX] web_editor: xml editor first level not shown
There is a slight visual inconsistency since: b4b7d1bf the xml/less
editor for first level of descendants were just prefixed by an invisible
space.

opw-807624
closes #22704

addons/web_editor/static/src/js/widgets/ace.js
@@ -742,7 +742,7 @@ var ViewEditor = Widget.extend({
 
             var text = data.text || '';
             if (!isSelected) {
-                text = Array($elem.data('level')).join('-') + ' ' + text;
+                text = Array($elem.data('level') + 1).join('-') + ' ' + text;
             }
             var $div = $('<div/>',  {
                 text: text,

commit d4ca69ef377d1abda74bc349a64c097bbe5b19e4

[FIX] web_editor: regression less editor c6e9f3c5e
In c6e9f3c5 a change was made to have the correct hierarchy levels, but
this can cause an issue when editing less files.

This is because we now had "undefined + 1" which gives NaN which breaks
Array constructor.

opw-814579
opw-815397
opw-814322
opw-815265
opw-814021
closes #23006

addons/web_editor/static/src/js/widgets/ace.js
@@ -742,7 +742,7 @@ var ViewEditor = Widget.extend({
 
             var text = data.text || '';
             if (!isSelected) {
-                text = Array($elem.data('level') + 1).join('-') + ' ' + text;
+                text = Array(($elem.data('level') || 0) + 1).join('-') + ' ' + text;
             }
             var $div = $('<div/>',  {
                 text: text,