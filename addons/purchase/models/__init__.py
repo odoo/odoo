8d4609a1db9ab39fd3542f65cc3a73cea65a823d
jem-odoo <jem@openerp.com>
2018-02-12 14:03:36 +0100

    [MOV] purchase: move file in purchase_stock

--- addons/purchase/models/__init__.py
+++ addons/purchase/models/__init__.py
@@ -3,10 +3,8 @@
 
 from . import account_invoice
 from . import purchase
-from . import procurement
 from . import product
 from . import res_company
 from . import res_config_settings
 from . import res_partner
-from . import stock
 from . import res_users
