1a4bd00807680b9a0ee1fb7ec5a67ab9fbde899a
Goffin Simon <sig@odoo.com>
2018-02-12 15:55:48 +0100

    [FIX]mrp, mrp_byproduct: Qty is not "produced" for Byproducts
    When producing a product as a BoM, the quantity for the byproduct was not increased in "produced".
    
    opw:814768

---
+++ addons/mrp_byproduct/models/mrp_workorder.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class MrpWorkorder(models.Model):
+	_inherit = 'mrp.workorder'
+
+	def _post_sub_lots_used(self):
+		super(MrpWorkorder, self)._post_sub_lots_used()
+		for sub_product in self.production_id.bom_id.sub_products:
+			quantity = self.qty_producing * sub_product.product_qty
+			production_move = self.production_id.move_finished_ids.filtered(lambda x: (x.product_id.id == sub_product.product_id.id) and (x.state not in ('done', 'cancel')))
+			if production_move.product_id.tracking != 'none':
+				move_lot = production_move.move_lot_ids.filtered(lambda x: x.lot_id.id == self.final_lot_id.id and x.product_id.id == sub_product.product_id.id)
+				if move_lot:
+					move_lot.quantity += quantity
+				else:
+					move_lot.create({'move_id': production_move.id,
+									 'lot_id': self.final_lot_id.id,
+									 'quantity': self.qty_producing,
+									 'quantity_done': self.qty_producing,
+									 'workorder_id': self.id,
+									 'product_id': sub_product.product_id.id,
+									 })
+			else:
+				production_move.quantity_done += quantity
