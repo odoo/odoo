f6f64db1e7014c2bf753e803c5b93066ed3f7868
XavierDo <xdo@odoo.com>
2018-02-13 14:20:16 +0100

    [IMP] im_livechat, mail: replace emoji with unicode.
    Remove the support to replace emojis by images. Most browser
    now support colored emoji or at least black and white emojis.
    The emojis list have been updated to ensure a correct support
    for most os/browser.
    Users have the possibility to install fonts localy if their os does not
    have one of the default emoji font installed.
    After default os emoji font, one of twemoji, emojione or noto color
    will be selected.
    The emoji list is now hardcoded in js in order to reduce the number of
    records in shortcodes.
    
    Also include a small fix for livechat to display message field when rating
    is bad.
    
    Task #36898
    PR #23689

--- addons/im_livechat/__manifest__.py
+++ addons/im_livechat/__manifest__.py
@@ -26,8 +26,7 @@ Help your customers with this chat, and analyse their feedback.
         "views/im_livechat_channel_views.xml",
         "views/im_livechat_channel_templates.xml",
         "report/im_livechat_report_channel_views.xml",
-        "report/im_livechat_report_operator_views.xml",
-        "data/im_livechat_channel_data.xml"
+        "report/im_livechat_report_operator_views.xml"
     ],
     'demo': [
         "data/im_livechat_channel_demo.xml",
