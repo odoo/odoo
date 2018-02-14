22f04b4023038e72fe74ff6d19c7ea4068c532cb
Alexandre Kühn <aku@odoo.com>
2018-02-14 14:47:03 +0100

    [FIX] mail: tests: improved mock_server mocked initMessaging
    Before this commit, if we wanted to add channels on rpc initMessaging,
    we had to mock this rpc completely.
    
    Now, with this commit, we can use data 'initMessaging', whose keys
    can overwrite the default mocked initMessaging.
    For instance, if this.data.initMessaging has the key 'channel_slots',
    then the mocked initMessaging rpc will return a new state for the
    existing channels to the chat_manager.
    
    This is needed in order to reduce the size of tests than need some custom
    initMessaging state, such as any test that needs at least one non-static
    channel (i.e. neither channel_inbox nor channel_starred).

--- addons/mail/static/tests/helpers/mock_server.js
+++ addons/mail/static/tests/helpers/mock_server.js
@@ -5,13 +5,31 @@ var MockServer = require('web.MockServer');
 
 MockServer.include({
     /**
+     * Param 'data' may have a key 'initMessaging' which contains
+     * a partial overwrite of the result from mockInitMessaging.
+     *
+     * Note: we must delete this key, so that this is not
+     * handled as a model definition.
+     *
+     * @override
+     * @param {Object} [data.initMessaging] 
+     */
+    init: function (data, options) {
+        if (data && data.initMessaging) {
+            this.initMessagingData = data.initMessaging;
+            delete data.initMessaging;
+        }
+        this._super.apply(this, arguments);
+    },
+
+    /**
      * Simulate the '/mail/init_messaging' route
      *
      * @private
      * @return {Object}
      */
     _mockInitMessaging: function () {
-        return {
+        return _.defaults(this.initMessagingData || {}, {
             'needaction_inbox_counter': 0,
             'starred_counter': 0,
             'channel_slots': [],
@@ -19,7 +37,7 @@ MockServer.include({
             'mention_partner_suggestions': [],
             'shortcodes': [],
             'menu_id': false,
-        };
+        });
     },
     /**
      * @override
