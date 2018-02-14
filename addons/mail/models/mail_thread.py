e32057e7894cb8d3dfa513c44d839bcf6eff67a8
Thibault Delavallée <tde@odoo.com>
2018-02-14 14:34:24 +0100

    [REF] mail: simplify parent fetch when posting a message
    1d57a09112e4def7c7ed9cc010f9d683c18ac1ea added a two-steps parent fetch when posting a message
    on a discussion thread. At the time this commit was done responsible were
    notified of the first thread message. It was helpful to notify them of the
    original email that created the thread instead of the "document created"
    generic message. Indeed for documents like leads or issues having the
    original email containing the customer question is interesting when being
    notified. This is why this commit was done.
    
    However since quite a long time responsible notifications have changed.
    They now receive an email notification telling they have been assigned on
    a document. This commit is not necessary anymore and we therefore remove
    it in order to clean a bit the message_post process.
    
    This commit globally reverts 1d57a09112e4def7c7ed9cc010f9d683c18ac1ea. Please note that @beledouxdenis
    agreed after a long thinking.
    
    This commit has a slight impact on mail performance tests, allowing to save
    one query in some more complex test case.

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -1862,10 +1862,8 @@ class MailThread(models.AbstractModel):
         # _mail_flat_thread: automatically set free messages to the first posted message
         MailMessage = self.env['mail.message']
         if self._mail_flat_thread and model and not parent_id and self.ids:
-            messages = MailMessage.search(['&', ('res_id', '=', self.ids[0]), ('model', '=', model), ('message_type', '=', 'email')], order="id ASC", limit=1)
-            if not messages:
-                messages = MailMessage.search(['&', ('res_id', '=', self.ids[0]), ('model', '=', model)], order="id ASC", limit=1)
-            parent_id = messages and messages[0].id or False
+            messages = MailMessage.search(['&', ('res_id', '=', self.ids[0]), ('model', '=', model)], order="id ASC", limit=1)
+            parent_id = messages.ids and messages.ids[0] or False
         # we want to set a parent: force to set the parent_id to the oldest ancestor, to avoid having more than 1 level of thread
         elif parent_id:
             messages = MailMessage.sudo().search([('id', '=', parent_id), ('parent_id', '!=', False)], limit=1)
