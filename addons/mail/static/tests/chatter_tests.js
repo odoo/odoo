c19809043727975c8360c244a21ecdcd9babc042
Thibault Delavallée <tde@odoo.com>
2018-02-16 15:05:45 +0100

    [REM] mail: remove o_mail_note class in chatter
    Purpose of this commit is to clean a bit various classes and conditions.
    
    First clean is the removal of o_mail_note class in activities less. It comes
    from first implementation of activity widget and is not used anymore in
    activity widgets rendering.
    
    Second clean is the removal of o_mail_note class when displaying messages.
    Only o_mail_discussion is used currently in chatter to make a different
    background depending on whether the message is a discussion or not. All
    we have to know to differentiate message display is whether it is a
    discussion done by an user or not.
    
    Tests have been updated to use o_mail_discussion class triggered by
    the message being a discussion, aka a comment posted by an user.

--- addons/mail/static/tests/chatter_tests.js
+++ addons/mail/static/tests/chatter_tests.js
@@ -463,8 +463,8 @@ QUnit.test('chatter: post, receive and star messages', function (assert) {
     assert.ok(form.$('.o_chatter_topbar .o_chatter_button_log_note').length,
         "log note button should be available");
     assert.strictEqual(form.$('.o_thread_message').length, 1, "thread should contain one message");
-    assert.ok(!form.$('.o_thread_message:first() .o_mail_note').length,
-        "the message shouldn't be a note");
+    assert.ok(form.$('.o_thread_message:first().o_mail_discussion').length,
+        "the message should be a discussion");
     assert.ok(form.$('.o_thread_message:first() .o_thread_message_core').text().indexOf('A message') >= 0,
         "the message's body should be correct");
     assert.ok(form.$('.o_thread_message:first() .o_mail_info').text().indexOf('John Doe') >= 0,
@@ -477,8 +477,8 @@ QUnit.test('chatter: post, receive and star messages', function (assert) {
     form.$('.oe_chatter .o_composer_button_send').click();
     assert.ok($('.oe_chatter .o_chat_composer').hasClass('o_hidden'), "chatter should be closed");
     assert.strictEqual(form.$('.o_thread_message').length, 2, "thread should contain two messages");
-    assert.ok(!form.$('.o_thread_message:first() .o_mail_note').length,
-        "the last message shouldn't be a note");
+    assert.ok(form.$('.o_thread_message:first().o_mail_discussion').length,
+        "the last message should be a discussion");
     assert.ok(form.$('.o_thread_message:first() .o_thread_message_core').text().indexOf('My first message') >= 0,
         "the message's body should be correct");
     assert.ok(form.$('.o_thread_message:first() .o_mail_info').text().indexOf('Me') >= 0,
@@ -491,8 +491,8 @@ QUnit.test('chatter: post, receive and star messages', function (assert) {
     form.$('.oe_chatter .o_composer_button_send').click();
     assert.ok($('.oe_chatter .o_chat_composer').hasClass('o_hidden'), "chatter should be closed");
     assert.strictEqual(form.$('.o_thread_message').length, 3, "thread should contain three messages");
-    assert.ok(form.$('.o_thread_message:first() .o_mail_note').length,
-        "the last message should be a note");
+    assert.ok(!form.$('.o_thread_message:first().o_mail_discussion').length,
+        "the last message should not be a discussion");
     assert.ok(form.$('.o_thread_message:first() .o_thread_message_core').text().indexOf('My first note') >= 0,
         "the message's body should be correct");
     assert.ok(form.$('.o_thread_message:first() .o_mail_info').text().indexOf('Me') >= 0,
