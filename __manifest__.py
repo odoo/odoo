dfb3bc933f383c09141018bbd23a9cf44519cf84
Priyanka Kakadiya <pka@odoo.com>
2018-02-16 15:46:57 +0530

    [REF] various: update manifests for better wording and names
    Time has come to update a bit applications name, summary and website links.
    Purpose is to have a website page correctly set for applications to be used
    in conjunction with the newly-introduced discover button.
    
    Names and summaries are updated to be more updated and shiny.
    
    This commit is related to task ID 47179 and 1861544. Related PR are #1834
    and #2365. First one is about classic applications while second one is about
    website applications.
    
    Co-Authored-By: Nimesh Jethva <nje@odoo.com>

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': "Website Sale Coupon",
-    'summary': """Allows to use discount coupons in ecommerce orders""",
-    'description': """Integrate coupons mechanism in ecommerce.""",
+    'name': "Coupons & Promotions for eCommerce",
+    'summary': """Use coupon & promotion programs in your eCommerce store""",
+    'description': """
+Create coupon and promotion codes to share in order to boost your sales (free products, discounts, etc.). Shoppers can use them in the eCommerce checkout.
+
+Coupon & promotion programs can be edited in the Catalog menu of the Website app.
+    """,
     'category': 'Website',
     'version': '1.0',
     'depends': ['website_sale', 'sale_coupon'],
