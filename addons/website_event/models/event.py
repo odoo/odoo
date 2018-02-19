57223240cf16354a4007b40e8c2b416c85fcc311
Goffin Simon <sig@odoo.com>
2018-02-19 16:22:51 +0100

    [FIX] website_event: Duplicating event with a menu
    Revert of 33e7c8d60c58c98d2f679b66abae5af7ef224726
    
    The right problem has been fixed with 1a8993e0cbcc7d9871a22602f1a14d5270540f64
    
    opw:813270

--- addons/website_event/models/event.py
+++ addons/website_event/models/event.py
@@ -32,7 +32,7 @@ class Event(models.Model):
     website_menu = fields.Boolean(
         'Dedicated Menu', compute='_compute_website_menu', inverse='_set_website_menu',
         help="Creates menus Introduction, Location and Register on the page "
-             " of the event on the website.", store=True, copy=False)
+             " of the event on the website.", store=True)
     menu_id = fields.Many2one('website.menu', 'Event Menu')
 
     def _compute_is_participating(self):
