9445d2f04d0decc29366aedea819b091f287f5e5
Alexandre Kühn <aku@odoo.com>
2018-02-20 15:26:31 +0100

    [FIX] web: hide nocontent helper in grouped kanban with no column
    Before this commit, the no content helper was displayed in grouped
    kanban without any column.
    
    It should be hidden, because the user expects to have a button to
    create a task, which is intentionally hidden in this case.
    (To sum up, the user is expected to create new columns before being
    prompted to create new tasks)

--- addons/web/static/src/js/views/kanban/kanban_renderer.js
+++ addons/web/static/src/js/views/kanban/kanban_renderer.js
@@ -350,7 +350,8 @@ var KanbanRenderer = BasicRenderer.extend({
             !remove &&
             !this._hasContent() &&
             !!this.noContentHelp &&
-            !(this.quickCreate && !this.quickCreate.folded);
+            !(this.quickCreate && !this.quickCreate.folded) &&
+            !this.state.isGroupedByM2ONoColumn;
 
         var $noContentHelper = this.$('.o_view_nocontent');
 
