4c6d7bb115f59358dfddac8434bc774695b04724
qsm-odoo <qsm@odoo.com>
2018-02-26 11:19:19 +0100

    [FIX] web_editor: stop crashing on applying fonts with standalone summernote
    Commit https://github.com/odoo/odoo/commit/20ed7d7f7d3f1292ac3618da4e285d010655e4c8?w=1#diff-6a334b274d95fb88a4fbc29b910fbb59R2120
    introduced a bug as it is using a function which is defined in another
    asset. This is fine for connected users but not for summernote-only
    users like public users on the forum.
    
    The reason the line was introduced is unsure so it is simply removed for
    now. The bug it was supposed to fix will be fixed later if it ever needs
    to be.

--- addons/web_editor/static/src/js/editor/summernote.js
+++ addons/web_editor/static/src/js/editor/summernote.js
@@ -2176,7 +2176,6 @@ $.summernote.pluginEvents.applyFont = function (event, editor, layoutInfo, color
     }
 
     // remove node without attributes (move content), and merge the same nodes
-    if (!dom.isImgFont(node)) {
      var className2, style, style2;
      for (i=0; i<nodes.length; i++) {
       node = nodes[i];
@@ -2217,7 +2216,6 @@ $.summernote.pluginEvents.applyFont = function (event, editor, layoutInfo, color
         }
       }
      }
-    }
 
     range.create(startPoint.node, startPoint.offset, endPoint.node, endPoint.offset).select();
 };
