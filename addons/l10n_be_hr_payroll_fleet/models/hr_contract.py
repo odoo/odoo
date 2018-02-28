5d8df3cd5e099579b391dd4de050b76838c7ce9b
Yannick Tivisse <yti@odoo.com>
2018-02-28 14:58:19 +0100

    [IMP] hr_contract: Track some fields modifications
    Purpose
    =======
    
    In contract, track fields (create a note, no need subtype. it is just for the history)
    - wage
    - fuel_card
    - meal_voucher_amount
    - representation_fees
    - additional_net_amount
    - retained_net_amount
    - commission_on_target
    - ip
    - car_id
    - internet
    - mobile
    - mobile_plus
    - final_yearly_ cost

--- addons/l10n_be_hr_payroll_fleet/models/hr_contract.py
+++ addons/l10n_be_hr_payroll_fleet/models/hr_contract.py
@@ -10,6 +10,7 @@ class HrContract(models.Model):
     car_id = fields.Many2one('fleet.vehicle', string='Company Car',
         domain=lambda self: self._get_available_cars_domain(),
         default=lambda self: self.env['fleet.vehicle'].search([('driver_id', '=', self.employee_id.address_home_id.id)], limit=1),
+        track_visibility="onchange",
         help="Employee's company car.")
     car_atn = fields.Float(compute='_compute_car_atn_and_costs', string='ATN Company Car', store=True)
     company_car_total_depreciated_cost = fields.Float(compute='_compute_car_atn_and_costs', store=True)
