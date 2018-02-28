7ac2fc420f36d9733373610acfd455b52df4679e
Nicolas Lempereur <nle@odoo.com>
2018-02-28 16:11:42 +0100

    [FIX] website_sale: update when changing quantity
    There is several issue happening when changing quantity:
    
    1) if website_sale_stock is installed, the displayed price is not
       updated when changing quantities.
    
    2) if the "Add to Cart" price is not updated, it erroneously displays
       the price multiplied by the quantity.
    
    3) when prices are updated, it only update attribute_value_ids.
    
    4) when variant are displayed as list view, visually the last list item
       is selected instead of the first, also changing quantity doesn't
       update price as it should be given pricelist.
    
    The first issue is caused by invalid JSON in data-attribute_value_ids
    attribute. The code modify the `.data('attribute_value_ids')` thinking
    it will be permanent. But if the value was invalid JSON we always parse
    it on the fly inside a temporary object so the object is never changed.
    
    The second issue is because originally, the "data-attribute_value_ids"
    inside "Add to Cart" was set wrongly (when rendering the view) because
    get_attribute_value_ids got the website_price multiplied by quantity.
    
    The third issue was even with website_sale_stock is not installed
    `.data('attribute_value_ids')` was only updated for one of the possible
    multiple ones in the page.
    
    With this change, we had json.dumps to ensure data-attribute_value_ids
    is good JSON, and we correct errors so it is fixed for instance only
    updating sources without updating website_sale and website_sale_options
    modules.
    
    note: when views are updated, this also solves a bigger issue when
    python2 is used (because attribute_value_ids contained u'text' strings.
    
    master equivalent of #23392 without js crutches for xml not updated
    
    opw-1816479
    closes #23467

--- addons/website_sale_options/static/src/js/website_sale.test.js
+++ addons/website_sale_options/static/src/js/website_sale.test.js
@@ -46,8 +46,101 @@ tour.register('shop_customize', {
             trigger: '.oe_product_cart a:contains("iPad")',
         },
         {
+            content: "open customize menu",
+            trigger: '#customize-menu > a',
+            extra_trigger: "#product_detail",
+        },
+        {
+            content: "click on 'List View of Variants'",
+            trigger: "#customize-menu a:contains(List View of Variants)",
+        },
+        {
+            content: "check page loaded after list of variant customization enabled",
+            trigger: '.js_product_change',
+            run: function () {}, // it's a check
+        },
+        {
+            content: "check price is 750 and set quantity to 2",
+            trigger: ".js_product:first input.quantity:propValue(1)",
+            extra_trigger: ".product_price .oe_price .oe_currency_value:containsExact(750.00)",
+            run: "text 2",
+        },
+        {
+            content: "verify pricelist based on quantity has effect",
+            trigger: ".product_price .oe_price .oe_currency_value:containsExact(600.00)",
+            run: function () {}, // it's a check
+        },
+        {
+            content: "check pricelit has been applied and switch to 32 GB variant",
+            trigger: ".js_product label:contains('32 GB')",
+        },
+        {
+            content: "verify that price has changed when changing variant",
+            trigger: ".product_price .oe_price .oe_currency_value:not(:containsExact(600.00))",
+            run: function () {}, // it's a check
+        },
+        {
+            content: "open customize menu",
+            trigger: '#customize-menu > a',
+        },
+        {
+            content: "remove 'List View of Variants'",
+            trigger: "#customize-menu a:contains(List View of Variants):has(input:checked)",
+        },
+        {
+            content: "check page loaded after list of variant customization disabled",
+            trigger: ".js_product:not(:has(.js_product_change))",
+            run: function () {}, // it's a check
+        },
+        {
+            content: "check price is 750 and set quantity to 2",
+            trigger: ".js_product:first input.quantity:propValue(1)",
+            extra_trigger: ".product_price .oe_price .oe_currency_value:containsExact(750.00)",
+            run: "text 2",
+        },
+        {
+            content: "verify pricelist based on quantity has effect",
+            trigger: ".product_price .oe_price .oe_currency_value:containsExact(600.00)",
+            run: function () {}, // it's a check
+        },
+        {
+            content: "switch to 32 GB variant",
+            trigger: ".js_product label:contains('32 GB')",
+        },
+        {
+            content: "verify that price has changed when changing variant",
+            trigger: ".product_price .oe_price .oe_currency_value:not(:containsExact(600.00))",
+            run: function () {}, // it's a check
+        },
+        {
+            content: "switch back to 16 GB variant",
+            trigger: ".js_product label:contains('16 GB')",
+        },
+        {
             content: "click on 'Add to Cart' button",
             trigger: "a:contains(Add to Cart)",
+        },
+        {
+            content: "price is lowered by pricelist and not multiplied by quantity",
+            trigger: "#product_confirmation .oe_price .oe_currency_value:containsExact(600.00)",
+            extra_trigger: "#product_confirmation input.quantity:propValue(2)",
+            run: function () {}, // it's a check
+        },
+        {
+            content: "set quantity to 1",
+            trigger: "#product_confirmation .js_add_cart_json .fa-minus",
+        },
+        {
+            content: "check that product page has been updated",
+            trigger: ".js_product:first input.quantity:propValue(1)",
+            extra_trigger: ".product_price .oe_price .oe_currency_value:containsExact(750.00)",
+            run: function () {}, // it's a check
+        },
+        {
+            content: "check that add to cart modal has been updated",
+            trigger: "#product_confirmation .oe_price .oe_currency_value:containsExact(750.00)",
+            extra_trigger: "#product_confirmation input.quantity:propValue(1)",
+            run: function () {}, // it's a check
         },
         {
             content: "add an optional Warranty",
