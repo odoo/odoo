5a34232870d876b9c7296e007ae0d29e2a3ff923
Thibault Delavallée <tde@odoo.com>
2018-03-01 11:00:01 +0100

    [FIX][IMP] project: include emails in customer portal
    Customer portal has a widget to display a chatter-like communication
    history. However only user comments are displayed. It seems interesting
    for documents like tasks that also include tickets and support to
    display incoming emails. Indeed most customers will use their email
    to discuss with the responsible.
    
    Done after feedback from internal migration.

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -467,6 +467,8 @@ class Task(models.Model):
     working_hours_close = fields.Float(compute='_compute_elapsed', string='Working hours to close', store=True, group_operator="avg")
     working_days_open = fields.Float(compute='_compute_elapsed', string='Working days to assign', store=True, group_operator="avg")
     working_days_close = fields.Float(compute='_compute_elapsed', string='Working days to close', store=True, group_operator="avg")
+    # customer portal: include comment and incoming emails in communication history
+    website_message_ids = fields.One2many(domain=lambda self: [('model', '=', self._name), ('message_type', 'in', ['email', 'comment'])])
 
     def _compute_attachment_ids(self):
         for task in self:
