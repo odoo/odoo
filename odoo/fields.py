odoo/odoo

commit 34c52e993834948661b5436fb6a4fae1c0e44928

[FIX] fields: performance of onchange on related fields
Optimize the copy of the cache being made by related fields to compute
themselves.  Instead of traversing records data in cache, follow the cache
structure itself and copy everything!

OPW 816125

odoo/fields.py
@@ -39,18 +39,7 @@ Default = object()                      # default value for __init__() methods
 
 def copy_cache(records, env):
     """ Recursively copy the cache of ``records`` to the environment ``env``. """
-    src, dst = records.env.cache, env.cache
-    todo, done = set(records), set()
-    while todo:
-        record = todo.pop()
-        if record not in done:
-            done.add(record)
-            target = record.with_env(env)
-            for field in src.get_fields(record):
-                value = src.get(record, field)
-                dst.set(target, field, value)
-                if value and field.type in ('many2one', 'one2many', 'many2many', 'reference'):
-                    todo.update(field.convert_to_record(value, record))
+    env.cache.copy(records, env)
 
 def first(records):
     """ Return the first record in ``records``, with the same prefetching. """

odoo/api.py
@@ -1013,6 +1013,17 @@ class Cache(object):
         ]
         return model.browse(ids)
 
+    def copy(self, records, env):
+        """ Copy the cache of ``records`` to ``env``. """
+        src = records
+        dst = records.with_env(env)
+        for field, field_cache in self._data.items():
+            src_key = field.cache_key(src)
+            dst_key = field.cache_key(dst)
+            for record_id, record_cache in field_cache.items():
+                if src_key in record_cache:
+                    record_cache[dst_key] = record_cache[src_key]
+
     def invalidate(self, spec=None):
         """ Invalidate the cache, partially or totally depending on ``spec``. """
         if spec is None:

commit b53c017052739f0735b9348ec4821f9a997db8e7

[FIX] fields: use `copy_cache` during onchanges only
This fixes the bug introduced in rev 34c52e993834948661b5436fb6a4fae1c0e44928
that optimizes method `copy_cache`.

odoo/fields.py
@@ -552,7 +552,7 @@ class Field(MetaField('DummyField', (object,), {})):
         # when related_sudo, bypass access rights checks when reading values
         others = records.sudo() if self.related_sudo else records
         # copy the cache of draft records into others' cache
-        if records.env != others.env:
+        if records.env.in_onchange and records.env != others.env:
             copy_cache(records - records.filtered('id'), others.env)
         #
         # Traverse fields one by one for all records, in order to take advantage

odoo/addons/test_new_api/tests/test_new_fields.py
@@ -651,16 +651,17 @@ class TestFields(common.TransactionCase):
         env = self.env(user=self.env.ref('base.user_demo'))
         self.assertEqual(env.user.login, "demo")
 
-        # create a new message as demo user
-        discussion = self.env.ref('test_new_api.discussion_0')
-        message = env['test_new_api.message'].new({'discussion': discussion})
-        self.assertEqual(message.discussion, discussion)
+        with self.env.do_in_onchange():
+            # create a new message as demo user
+            discussion = self.env.ref('test_new_api.discussion_0')
+            message = env['test_new_api.message'].new({'discussion': discussion})
+            self.assertEqual(message.discussion, discussion)
 
-        # read the related field discussion_name
-        self.assertEqual(message.discussion.env, env)
-        self.assertEqual(message.discussion_name, discussion.name)
-        with self.assertRaises(AccessError):
-            message.discussion.name
+            # read the related field discussion_name
+            self.assertEqual(message.discussion.env, env)
+            self.assertEqual(message.discussion_name, discussion.name)
+            with self.assertRaises(AccessError):
+                message.discussion.name
 
     @mute_logger('odoo.addons.base.ir.ir_model')
     def test_42_new_related(self):
@@ -673,14 +674,15 @@ class TestFields(common.TransactionCase):
         env = self.env(user=self.env.ref('base.user_demo'))
         self.assertEqual(env.user.login, "demo")
 
-        # create a new discussion and a new message as demo user
-        discussion = env['test_new_api.discussion'].new({'name': 'Stuff'})
-        message = env['test_new_api.message'].new({'discussion': discussion})
-        self.assertEqual(message.discussion, discussion)
+        with self.env.do_in_onchange():
+            # create a new discussion and a new message as demo user
+            discussion = env['test_new_api.discussion'].new({'name': 'Stuff'})
+            message = env['test_new_api.message'].new({'discussion': discussion})
+            self.assertEqual(message.discussion, discussion)
 
-        # read the related field discussion_name
-        self.assertNotEqual(message.sudo().env, message.env)
-        self.assertEqual(message.discussion_name, discussion.name)
+            # read the related field discussion_name
+            self.assertNotEqual(message.sudo().env, message.env)
+            self.assertEqual(message.discussion_name, discussion.name)
 
     def test_50_defaults(self):
         """ test default values. """