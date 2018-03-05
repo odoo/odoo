037cfc9f67f86bc05aa9b239d8e479fd07ebaff9
Nicolas Martinelli <nim@odoo.com>
2018-03-05 10:44:07 +0100

    [FIX] delivery: products not shared
    - Define 2 companies A and B. Delivery methods are linked to products in
      company A
    - Deactivate 'Share product to all companies'
    - Create a user B in Company B only
    - As user B, create a SO and open the drop-down list for delivery
      methods
    
    An access error is raised.
    
    Delivery methods should follow the same record rules than the products
    they are linked to.
    
    opw-1815490

--- addons/delivery/models/__init__.py
+++ addons/delivery/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import base_config_settings
 import delivery_carrier
 import delivery_price_rule
 import product_template
