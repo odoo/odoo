e51f00966262a59a6069f5bfafef9c86a4d42774
Pratima Gupta <pgu@odoo.com>
2018-03-05 18:18:58 +0530

    [IMP] event, website_event_track: make email templates bioutifoul
    Purpose of this commit is to enhance quality of templates proposed by Odoo
    and make them use notification layout when send by emails. Those emails
    are cleaner and more up to date compared to other emails.
    
    Including
      * registration and reminder templates that are send directly by email
        are improved to directly include the notification layout in the body
        itself;
      * improve layout of badge template;
      * use notification layout when sending badge;
      * improve layout of track confirmation template;
      * use notification layout when sending templates based on stage change;
    
    This commit is related to task ID 51122 (and PR #24052).

--- addons/website_event_track/models/event_track.py
+++ addons/website_event_track/models/event_track.py
@@ -139,7 +139,12 @@ class Track(models.Model):
         track = self[0]
         changes, tracking_value_ids = tracking[track.id]
         if 'stage_id' in changes and track.stage_id.mail_template_id:
-            res['stage_id'] = (track.stage_id.mail_template_id, {'composition_mode': 'mass_mail'})
+            res['stage_id'] = (track.stage_id.mail_template_id, {
+                'composition_mode': 'comment',
+                'auto_delete_message': True,
+                'subtype_id': self.env['ir.model.data'].xmlid_to_res_id('mail.mt_note'),
+                'notif_layout': 'mail.mail_notification_light'
+            })
         return res
 
     @api.multi
