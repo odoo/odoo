f48b754cef9e778423c51ee69dd19ee3e8611eb6
Denis Ledoux <dle@odoo.com>
2018-03-08 12:33:14 +0100

    [FIX] membership: missing triggers to recompute membership dates
    When a membership line has its dates changed
    (date_from, date_to, date_cancel)
    
    The membership start/stop dates on the member
    (res.partner) were not changed accordigly.
    
    opw-1823981

--- addons/membership/membership.py
+++ addons/membership/membership.py
@@ -346,7 +346,7 @@ class Partner(osv.osv):
                     string = 'Membership Start Date', type = 'date',
                     store = {
                         'account.invoice': (_get_invoice_partner, ['state'], 10),
-                        'membership.membership_line': (_get_partner_id, ['state'], 10, ),
+                        'membership.membership_line': (_get_partner_id, ['state', 'date_cancel', 'date_from'], 10, ),
                         'res.partner': (_get_partners, ['free_member', 'membership_state', 'associate_member'], 10)
                     }, help="Date from which membership becomes active."),
         'membership_stop': fields.function(
@@ -354,7 +354,7 @@ class Partner(osv.osv):
                     string = 'Membership End Date', type='date', multi='membership_stop',
                     store = {
                         'account.invoice': (_get_invoice_partner, ['state'], 10),
-                        'membership.membership_line': (_get_partner_id, ['state'], 10),
+                        'membership.membership_line': (_get_partner_id, ['state', 'date_cancel', 'date_to'], 10),
                         'res.partner': (_get_partners, ['free_member', 'membership_state', 'associate_member'], 10)
                     }, help="Date until which membership remains active."),
         'membership_cancel': fields.function(
@@ -362,7 +362,7 @@ class Partner(osv.osv):
                     string = 'Cancel Membership Date', type='date', multi='membership_cancel',
                     store = {
                         'account.invoice': (_get_invoice_partner, ['state'], 11),
-                        'membership.membership_line': (_get_partner_id, ['state'], 10),
+                        'membership.membership_line': (_get_partner_id, ['state', 'date_cancel'], 10),
                         'res.partner': (_get_partners, ['free_member', 'membership_state', 'associate_member'], 10)
                     }, help="Date on which membership has been cancelled"),
     }
