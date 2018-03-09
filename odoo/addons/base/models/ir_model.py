a9ca90041c75a89df92fd3581221115398371633
Adrian Torres <adt@odoo.com>
2018-03-09 15:44:39 +0100

    [IMP] fields: allow shared fields
    Given the following variables:
        * Module A
        * Module B
        * Model M
        * Field X of model M
    
    If module A defines M.X and is installed, an xmlid for this field is
    generated in the form of A.field_M_X
    
    Before this commit:
    
    If module B defines M.X as well and is installed after module A, no
    xmlid is generated.
    
    This means that if module A is uninstalled, the single xmlid pointing to
    M.X will be deleted and thus, the field itself will be deleted as well,
    therefore any views from module B referencing M.X will crash.
    
    After this commit:
    
    If module B (or any subsequent modules) define M.X, an xmlid will be
    generated in the form of B.field_M_X.
    
    If module A is uninstalled, the xmlid A.field_M_X will be deleted, but
    B.field_M_X will remain and thus the field itself won't be deleted.
    
    This system means that for a single actual field, there can be multiple
    xmlids, each xmlid sharing the same field, thus the name "shared
    fields".
    
    Task ID 38016

--- odoo/addons/base/models/ir_model.py
+++ odoo/addons/base/models/ir_model.py
@@ -746,39 +746,45 @@ class IrModelFields(models.Model):
         fields_data = self._existing_field_data(field.model_name)
         field_data = fields_data.get(field.name)
         params = self._reflect_field_params(field)
+        cr = self.env.cr
+        created = False
 
         if field_data is None:
-            cr = self.env.cr
-            # create an entry in this table
+            # does not exist, create an entry in this table
             query_insert(cr, self._table, params)
             record = self.browse(cr.fetchone())
             self.pool.post_init(record.modified, list(params))
-            # create a corresponding xml id
-            module = field._module or self._context.get('module')
-            if module:
-                model_name = field.model_name.replace('.', '_')
-                xmlid = 'field_%s__%s' % (model_name, field.name)
-                cr.execute(""" INSERT INTO ir_model_data (module, name, model, res_id, date_init, date_update)
-                               VALUES (%s, %s, %s, %s, (now() at time zone 'UTC'), (now() at time zone 'UTC')) """,
-                           (module, xmlid, record._name, record.id))
             # update fields_data (for recursive calls)
             fields_data[field.name] = dict(params, id=record.id)
-            return record
+            created = True
 
-        diff = {key for key, val in params.items() if field_data[key] != val}
-        if diff:
-            cr = self.env.cr
-            # update the entry in this table
+        elif any(field_data[key] != val for key, val in params.items()):
+            # exists, update the entry in this table
             query_update(cr, self._table, params, ['model', 'name'])
             record = self.browse(cr.fetchone())
-            self.pool.post_init(record.modified, diff)
+            names = [key for key, val in params.items() if field_data[key] != val]
+            self.pool.post_init(record.modified, names)
             # update fields_data (for recursive calls)
             field_data.update(params)
-            return record
 
         else:
-            # nothing to update, simply return the corresponding record
-            return self.browse(field_data['id'])
+            # exists, but nothing to update
+            record = self.browse(field_data['id'])
+
+        # generate xmlids if necessary, one per module defining the same field
+        module = self._context.get('module')
+        if module and (created or module in field._modules):
+            model_name = field.model_name.replace('.', '_')
+            xmlid = 'field_%s__%s' % (model_name, field.name)
+            cr.execute(
+                """
+                INSERT INTO ir_model_data (module, name, model, res_id, date_init, date_update)
+                SELECT %s, %s, %s, %s, (now() at time zone 'UTC'), (now() at time zone 'UTC')
+                WHERE NOT EXISTS (SELECT id FROM ir_model_data WHERE module=%s AND name=%s)
+                """, (module, xmlid, record._name, record.id, module, xmlid)
+            )
+
+        return record
 
     def _reflect_model(self, model):
         """ Reflect the given model's fields. """
