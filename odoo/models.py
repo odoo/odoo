e26baf75332e40f893040127f3313559219cb59e
Stefan Rijnhart <stefan@opener.am>
2018-03-09 15:08:10 +0100

    [IMP] Improve performance of mass reconciliations Consider the case of a payment order when reconciling the invoices with the amounts that are transferred to a clearing account. Every time a move line is reconciled, the creation of the reconciliation clears the cache of account.move.line's reconcile_id and reconcile_partial_id which leads to a new prefetching of the growing amount of move lines in the env's prefetch dictionary. This change only clears the cache of the records that are actually referred to in the new record's relation fields.

--- odoo/models.py
+++ odoo/models.py
@@ -3295,7 +3295,7 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
             # mark fields to recompute; do this before setting other fields,
             # because the latter can require the value of computed fields, e.g.,
             # a one2many checking constraints on records
-            self.modified(self._fields)
+            self.modified(self._fields, create=True)
 
             # set the value of non-column fields
             if other_fields:
@@ -4669,7 +4669,7 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
         self.env.cache.invalidate(spec)
 
     @api.multi
-    def modified(self, fnames):
+    def modified(self, fnames, create=False):
         """ Notify that fields have been modified on ``self``. This invalidates
             the cache, and prepares the recomputation of stored function fields
             (new-style fields only).
@@ -4685,7 +4685,17 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
             # invalidate mfield on self, and its inverses fields
             invalids.append((mfield, self._ids))
             for field in self._field_inverses[mfield]:
-                invalids.append((field, None))
+                if create:
+                    try:
+                        ids = self.mapped(mfield.name)._ids
+                        if ids:
+                            invalids.append((field, ids))
+                    except AccessError:
+                        # Caching should work regardless of access. As sudo will
+                        # not work in all cases, fallback on invalidating all
+                        invalids.append((field, None))
+                else:
+                    invalids.append((field, None))
             # group triggers by model and path to reduce the number of search()
             for field, path in self._field_triggers[mfield]:
                 triggers[(field.model_name, path)].add(field)
