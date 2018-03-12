291e37bd7cdfe0ee13e641d05b633fead5b8c8e4
len-odoo <len@odoo.com>
2018-03-12 15:51:57 +0100

    [FIX] stock, sale_stock, purchase: create quality checks for backorders
    There was no Quality check entry for backorders
    when the user did a partial shipment.
    We move the logic of the backorder creation to the _create_backorder function.
    This function was defined but not called anymore due to a refactoring.
    We clean some of its unnecessary overrides which were not used anymore.
    
    opw-1816845

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -1061,18 +1061,8 @@ class StockMove(models.Model):
             return moves_todo
 
         if picking:
-            moves_to_backorder = picking.move_lines.filtered(lambda x: x.state not in ('done', 'cancel'))
-            if moves_to_backorder:
-                backorder_picking = picking.copy({
-                        'name': '/',
-                        'move_lines': [],
-                        'move_line_ids': [],
-                        'backorder_id': picking.id
-                    })
-                picking.message_post(_('The backorder <a href=# data-oe-model=stock.picking data-oe-id=%d>%s</a> has been created.') % (backorder_picking.id, backorder_picking.name))
-                moves_to_backorder.write({'picking_id': backorder_picking.id})
-                moves_to_backorder.mapped('move_line_ids').write({'picking_id': backorder_picking.id})
-            moves_to_backorder._action_assign()
+            picking._create_backorder()
+
         return moves_todo
 
     def unlink(self):
