bed9f2ef7a15e57a5e183acb3ae5702a6cf04196
Romain Derie <rde@odoo.com>
2018-03-19 15:39:07 +0100

    [FIX] website_sale(_delivery): show prices according to B2B/B2C mode
    Before this commit:
    1. The delivery price on checkout would always be shown without the taxes.
       This would make the cart total unclear since it is not indicated that this
       price is tax excluded. You could only assume that this was the price the
       delivery would cost you (and not just the untaxed delivery cost which you
       can't even find the full price anyway since taxes in subtotal is the sum of
       all products taxes).
    
    2. The sale order line in payment step of checkout are always displayed with
       taxes.
    
    Now:
    We ensure that both 1. and 2. show the delivery price with or without taxes
    according to which mode (B2C or B2B) is enabled.
    
    This fixes #12872, closes #13592 and closes #23746

--- addons/website_sale_delivery/models/sale_order.py
+++ addons/website_sale_delivery/models/sale_order.py
@@ -27,7 +27,10 @@ class SaleOrder(models.Model):
     @api.depends('order_line.price_unit', 'order_line.tax_id', 'order_line.discount', 'order_line.product_uom_qty')
     def _compute_amount_delivery(self):
         for order in self:
-            order.amount_delivery = sum(order.order_line.filtered('is_delivery').mapped('price_subtotal'))
+            if self.env.user.has_group('sale.group_show_price_subtotal'):
+                order.amount_delivery = sum(order.order_line.filtered('is_delivery').mapped('price_subtotal'))
+            else:
+                order.amount_delivery = sum(order.order_line.filtered('is_delivery').mapped('price_total'))
 
     @api.depends('order_line.is_delivery')
     def _compute_has_delivery(self):
