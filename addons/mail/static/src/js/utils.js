fe75157661bdbfed663b0a25233275b5c2114a6d
Nicolas Martinelli <nim@odoo.com>
2018-03-22 09:07:28 +0100

    [FIX] mail: strip comments when inlining
    When an unread message contains conditional html statements, clicking on
    the discuss tray icon produces a JS traceback. This is due to
    `node.tagName` being undefined.
    
    When inlining, there is no need for these comments.
    
    opw-1829641

--- addons/mail/static/src/js/utils.js
+++ addons/mail/static/src/js/utils.js
@@ -92,6 +92,7 @@ function strip_html (node, transform_children) {
 
 function inline (node, transform_children) {
     if (node.nodeType === 3) return node.data;
+    if (node.nodeType === 8) return "";
     if (node.tagName === "BR") return " ";
     if (node.tagName.match(/^(A|P|DIV|PRE|BLOCKQUOTE)$/)) return transform_children();
     node.innerHTML = transform_children();
