9d1fafa050e18a02accd3787e8dfff608a0ec509
Kishan Gajjar <kig@odoo.com>
2018-03-22 18:03:09 +0530

    [IMP] web: Added a controller that set ids of current active_domain records in args

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -951,6 +951,19 @@ class DataSet(http.Controller):
     def call_kw(self, model, method, args, kwargs, path=None):
         return self._call_kw(model, method, args, kwargs)
 
+    @http.route('/web/dataset/call_kw_with_domain', type='json', auth="user")
+    def call_kw_with_domain(self, model, method, args, kwargs):
+        context = kwargs.get('context')
+        if context and context.get('active_domain') and model == context.get('active_model'):
+            ctx = {
+                'active_domain': context.get('active_domain'),
+                'active_model': context.get('active_model')
+            }
+            records = request.env[model].with_context(ctx).get_active_records()
+            # change ids to all active domain records ids
+            args[0] = records.ids
+        return self._call_kw(model, method, args, kwargs)
+
     @http.route('/web/dataset/call_button', type='json', auth="user")
     def call_button(self, model, method, args, domain_id=None, context_id=None):
         action = self._call_kw(model, method, args, {})
