9e47b4d4a364337489c75b6fc5ce232bf010cb3c
Florent de Labarre <florent.mirieu@gmail.com>
2018-03-23 21:40:12 +0000

    [FIX] sale_stock: show complete name of product
    In case of changing the quantity on hand of two variants of the same product,
    the two message created will have the same name (the name of the
    product.template).
    Show the name of variant instead.

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -346,5 +346,5 @@ class SaleOrderLine(models.Model):
             pickings = line.order_id.picking_ids.filtered(lambda p: p.state not in ('done', 'cancel'))
             for picking in pickings:
                 picking.message_post("The quantity of %s has been updated from %d to %d in %s" %
-                                      (line.product_id.name, line.product_uom_qty, values['product_uom_qty'], line.order_id.name))
+                                      (line.product_id.display_name, line.product_uom_qty, values['product_uom_qty'], line.order_id.name))
         super(SaleOrderLine, self)._update_line_quantity(values)
