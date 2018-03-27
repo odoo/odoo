8e95a81439fae881a69cac3943b256f8d8d6cae2
David Tran <david.tran@tvtmarine.com>
2018-03-27 16:59:31 +0530

    [IMP] l10n_vn: account type for accounts 21x
    They were all in type of non current assets which bring more diffcult in
    creating VAS compliance legal reports when those reports require data
    grouped by seperated account types.
    
    For example, balance sheet wants data grouped by fixed assets, fixed
    assets depeciation, etc. Without proper types, developers may need to
    search by account code prefixes which is not so convenient.
    
    This commit categorises accounts 21x into several types to serve
    Vietnamese legal reports better and offers ease for developers when
    creating such the reports.
    
    Also, this module is now:
    - fully in compliance with the Circular No.
    200/2014/TT-BTC dated 22 December 2014 by the Ministry of Finance which
    came into force on Jan 1, 2015
    - partially in compliance with the Circular 133/2016/TT-BTC dated Aug 26
    2016 by the Ministry of Finance which came into force on Jan 1, 2017.
    
    The following has been done:
    1. More common taxes added upon installation of the module.
    2. Set account groups so that Vietnamese accountants can use it the way
    of parent view account (like what was before Odoo 9). This brings peace
    to the accountants.
    3. New account groups data has been added to use in the similar way of
    parent view accounts before Odoo 9. For example, when account 111
    ledger/report is requested, accountants can now group all accounts
    111xxx using account tag 111.
    5. Fix a few wrong account declarations
    6. According to Vietnam law, sale and purchase journals must have a
    dedicated sequence for any refund.
    7. Use English instead of Vietnamese to bring ease for worldwide
    developers and foreigners doing business in Vietnam. Translation is
    added in next commit using l10n_multilang.

--- addons/l10n_vn/__init__.py
+++ addons/l10n_vn/__init__.py
@@ -3,7 +3,17 @@
 
 # This module is Copyright (c) 2009-2013 General Solutions (http://gscom.vn) All Rights Reserved.
 
+from . import models
+from odoo import api, SUPERUSER_ID
 
 def _preserve_tag_on_taxes(cr, registry):
     from odoo.addons.account.models.chart_template import preserve_existing_tags_on_taxes
     preserve_existing_tags_on_taxes(cr, registry, 'l10n_vn')
+
+def load_translations(env):
+    env.ref('l10n_vn.vn_template').process_coa_translations()
+
+def post_init(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    load_translations(env)
+    _preserve_tag_on_taxes(cr, registry)
--- addons/l10n_vn/__manifest__.py
+++ addons/l10n_vn/__manifest__.py
@@ -4,28 +4,46 @@
 
 {
     "name": "Vietnam - Accounting",
+    "summary": "Vietnam Chart of Accounts for Odoo",
     "version": "2.0",
-    "author": "General Solutions",
-    'website': 'http://gscom.vn',
+    "author": "General Solutions, Trobz, T.V.T Marine Automation (aka TVTMA)",
+    'website': 'http://gscom.vn, https://ma.tvtmarine.com',
     'category': 'Localization',
     "description": """
-This is the module to manage the accounting chart for Vietnam in Odoo.
-=========================================================================
+With the new development, the module is now:
+--------------------------------------------
+* fully in compliance with the Circular #200/2014/TT-BTC dated Dec 22, 2014 by the Ministry of Finance which came into force on Jan 1, 2015
+* partially in compliance with the Circular #133/2016/TT-BTC dated Aug 26, 2016 by the Ministry of Finance which came into force on Jan 1, 2017.
 
-This module applies to companies based in Vietnamese Accounting Standard (VAS)
-with Chart of account under Circular No. 200/2014/TT-BTC
+The following has been done and integrated
+------------------------------------------
+* More common taxes (e.g. import, export, special consumption, nature resource usage, etc)
+* Complete Chart of Accounts
+* Add one more field named code to the model account.account.tag so that Vietnamese accountants can use it the way of parent view account (like what was before Odoo 9). This bring peace to the accountants.
+* New account tags data has been added to use in the similar way of parent view accounts before Odoo 9. For example, accountant now can group all accounts 111xxx using account the tag 111.
+* Accounts now link to the tags having corresponding code. E.g. account 1111 and 1112 .... 111x have the same account tag of 111.
+* According to Vietnam law, sale and purchase journals must have a dedicated sequence for any refund.
+* Use English instead of Vietnamese to bring ease for worldwide developers and foreigners doing business in Vietnam. Translations for Vietnamese will be loaded upon loading Vietnamese and then installation of l10n_multilang module
 
-**Credits:**
-    - General Solutions.
-    - Trobz
+Known issues
+-------------
+* There are a few accounts conflicts between those two circular (e.g. 3385, 3386, etc) which can be handled manually in the meantime. Future development should allow admin to select an appropriate COA (either c200 or c133)
+
+Credits
+-------
+* General Solutions.
+* Trobz
+* ERPOnline
 """,
     "depends": [
         "account",
-        "base_iban"
+        "base_iban",
+        "l10n_multilang",
     ],
     "data": [
          'data/res.country.state.csv',
          'data/l10n_vn_chart_data.xml',
+         'data/account_group_data.xml',
          'data/account.account.template.csv',
          'data/l10n_vn_chart_post_data.xml',
          'data/account_data.xml',
@@ -33,5 +51,5 @@ with Chart of account under Circular No. 200/2014/TT-BTC
          'data/account_tax_data.xml',
          'data/account_chart_template_data.xml',
     ],
-    'post_init_hook': '_preserve_tag_on_taxes',
+    'post_init_hook': 'post_init',
 }
