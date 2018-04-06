f36cdbebd795b5001373aebf48fd612c44a6a2e8
tlemaire <thomas.lemaire@ndp-systemes.fr>
2018-04-06 11:02:51 +0200

    Separation of access rights on purchase orders and invoices

--- addons/purchase/partner.py
+++ addons/purchase/partner.py
@@ -8,12 +8,17 @@ class res_partner(models.Model):
     _inherit = 'res.partner'
 
     @api.multi
-    def _purchase_invoice_count(self):
+    def _purchase_order_count(self):
         PurchaseOrder = self.env['purchase.order']
-        Invoice = self.env['account.invoice']
         for partner in self:
             partner.purchase_order_count = PurchaseOrder.search_count([('partner_id', 'child_of', partner.id)])
-            partner.supplier_invoice_count = Invoice.search_count([('partner_id', 'child_of', partner.id), ('type', '=', 'in_invoice')])
+
+    @api.multi
+    def _purchase_invoice_count(self):
+        Invoice = self.env['account.invoice']
+        for partner in self:
+            partner.supplier_invoice_count = Invoice.search_count([('partner_id', 'child_of', partner.id),
+                                                                   ('type', '=', 'in_invoice')])
 
     @api.model
     def _commercial_fields(self):
@@ -22,5 +27,5 @@ class res_partner(models.Model):
     property_purchase_currency_id = fields.Many2one(
         'res.currency', string="Supplier Currency", company_dependent=True,
         help="This currency will be used, instead of the default one, for purchases from the current partner")
-    purchase_order_count = fields.Integer(compute='_purchase_invoice_count', string='# of Purchase Order')
+    purchase_order_count = fields.Integer(compute='_purchase_order_count', string='# of Purchase Order')
     supplier_invoice_count = fields.Integer(compute='_purchase_invoice_count', string='# Vendor Bills')
