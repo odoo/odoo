9d0adde13dd9ce1d27e40deff56a3e6024e0511b
Nicolas Martinelli <nim@odoo.com>
2018-04-06 15:03:37 +0200

    [FIX] l10n_fr_pos_cert: import `api`
    Following forward-port.

--- addons/l10n_fr_pos_cert/models/account_bank_statement.py
+++ addons/l10n_fr_pos_cert/models/account_bank_statement.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models
+from odoo import models, api
 from odoo.tools.translate import _
 from odoo.exceptions import UserError
 
