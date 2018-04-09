772bebede53ce75f4cc8682c8502920610d1d66f
jem-odoo <jem@openerp.com>
2018-04-09 16:46:57 +0200

    [MOV] project,hr_timesheet: move project_time_mode_id field
    'project_time_mode_id' is the field used to determined the UoM to
    use when logging timesheets. It is not used in project alone, but
    in timesheet. Moving it make sense, and does not cause any problems.
    For now, no UI allow the end user to change it: it is set with the
    Hours UoM (hardcoded).
    This is a technical move to prepare the feature that will allow
    people timesheeting in Days or Hours.
    
    Task #39079

--- addons/hr_timesheet/models/res_config_settings.py
+++ addons/hr_timesheet/models/res_config_settings.py
@@ -9,3 +9,8 @@ class ResConfigSettings(models.TransientModel):
 
     module_project_timesheet_synchro = fields.Boolean("Awesome Timesheet")
     module_project_timesheet_holidays = fields.Boolean("Leaves")
+    project_time_mode_id = fields.Many2one(
+        'uom.uom', related='company_id.project_time_mode_id', string='Project Time Unit',
+        help="This will set the unit of measure used in projects and tasks.\n"
+             "If you use the timesheet linked to projects, don't "
+             "forget to setup the right unit of measure in your employees.")
--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -2,7 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from datetime import timedelta
-from lxml import etree
 
 from odoo import api, fields, models, tools, SUPERUSER_ID, _
 from odoo.exceptions import UserError, AccessError, ValidationError
@@ -619,47 +618,6 @@ class Task(models.Model):
         for task in self:
             if not task._check_recursion():
                 raise ValidationError(_('Error! You cannot create recursive hierarchy of task(s).'))
-
-    # Override view according to the company definition
-    @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
-        # read uom as admin to avoid access rights issues, e.g. for portal/share users,
-        # this should be safe (no context passed to avoid side-effects)
-        obj_tm = self.env.user.company_id.project_time_mode_id
-        tm = obj_tm and obj_tm.name or 'Hours'
-
-        res = super(Task, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
-
-        # read uom as admin to avoid access rights issues, e.g. for portal/share users,
-        # this should be safe (no context passed to avoid side-effects)
-        obj_tm = self.env.user.company_id.project_time_mode_id
-        # using get_object to get translation value
-        uom_hour = self.env.ref('uom.product_uom_hour', False)
-        if not obj_tm or not uom_hour or obj_tm.id == uom_hour.id:
-            return res
-
-        eview = etree.fromstring(res['arch'])
-
-        # if the project_time_mode_id is not in hours (so in days), display it as a float field
-        def _check_rec(eview):
-            if eview.attrib.get('widget', '') == 'float_time':
-                eview.set('widget', 'float')
-            for child in eview:
-                _check_rec(child)
-            return True
-
-        _check_rec(eview)
-
-        res['arch'] = etree.tostring(eview, encoding='unicode')
-
-        # replace reference of 'Hours' to 'Day(s)'
-        for f in res['fields']:
-            # TODO this NOT work in different language than english
-            # the field 'Initially Planned Hours' should be replaced by 'Initially Planned Days'
-            # but string 'Initially Planned Days' is not available in translation
-            if 'Hours' in res['fields'][f]['string']:
-                res['fields'][f]['string'] = res['fields'][f]['string'].replace('Hours', obj_tm.name)
-        return res
 
     @api.model
     def get_empty_list_help(self, help):
