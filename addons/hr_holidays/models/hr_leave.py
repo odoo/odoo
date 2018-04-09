27e1d7780e19c2de3dd56012af0f5798a61b63fc
RomainLibert <rli@odoo.com>
2018-04-09 17:46:01 +0200

    [FIX] hr_holidays: allow edition of validated leaves
    Due to a record rule, it is not possible to add a follower to a
    validated leave, as we should be able to do it, we subscribe through
    sudo.

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -550,3 +550,10 @@ class HolidaysRequest(models.Model):
             })
 
         return [new_group] + groups
+
+    @api.multi
+    def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None, force=True):
+        # due to record rule can not allow to add follower and mention on validated leave so subscribe through sudo
+        if self.state in ['validate', 'validate1']:
+            return super(HolidaysRequest, self.sudo()).message_subscribe(partner_ids=partner_ids, channel_ids=channel_ids, subtype_ids=subtype_ids, force=force)
+        return super(HolidaysRequest, self).message_subscribe(partner_ids=partner_ids, channel_ids=channel_ids, subtype_ids=subtype_ids, force=force)
