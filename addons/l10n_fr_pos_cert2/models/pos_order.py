86e768f1d3023f5412aafb34436a77c6faa08831
Pierre Masereel <pim@odoo.com>
2018-04-11 13:21:29 +0200

    fixup! [ADD] l10n_fr_pos_cert2

--- addons/l10n_fr_pos_cert2/models/pos_order.py
+++ addons/l10n_fr_pos_cert2/models/pos_order.py
@@ -7,6 +7,7 @@ import time
 
 from openerp import models, api, _
 from openerp.tools import float_is_zero
+from openerp.exceptions import UserError
 
 
 class pos_order(models.Model):
@@ -33,7 +34,6 @@ class pos_order(models.Model):
     def save_ticket(self, order_data, html_ticket):
         order_id = self.env['pos.order'].search([('pos_reference', '=', order_data['name'])], limit=1)
         if order_data.get('l10n_fr_proforma'):
-            #USE fields.date from string
             name = '%s - %s - Proforma.posxml' % (order_id.display_name, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         else:
             name = '%s - %s - Receipt.posxml' % (order_id.display_name, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
@@ -49,32 +49,32 @@ class pos_order(models.Model):
         })
         return True
 
-    def create_from_ui(self, cr, uid, orders, context=None):
-
+    @api.model
+    def create_from_ui(self, orders):
         submitted_references = [o['data']['name'] for o in orders]
-        existing_orders = self.search_read(cr, uid, [('pos_reference', 'in', submitted_references)], ['pos_reference'], context=context)
+        existing_orders = self.search_read([('pos_reference', 'in', submitted_references)], ['pos_reference'])
         existing_references = set([o['pos_reference'] for o in existing_orders])
         orders_to_create = [o for o in orders if o['data']['name'] not in existing_references]
         orders_to_update = [o for o in orders if o['data']['name'] in existing_references]
 
-        order_ids = super(pos_order, self).create_from_ui(cr, uid, orders_to_create, context=context)
+        order_ids = super(pos_order, self).create_from_ui(orders_to_create)
 
         # duplicated from create_from_ui, but adapted to update existing records instead of creating new ones
         for order in orders_to_update:
             to_invoice = order['to_invoice']
             order = order['data']
             order_id = 0
-            for o in existing_orders:
-                if o['pos_reference'] == order['name']:
-                    order_id = o['id']
+            for existing_order in existing_orders:
+                if existing_order['pos_reference'] == order['name']:
+                    order_id = existing_order['id']
                     break
             if order.get('l10n_fr_proforma'):
-                self._update_order(cr, uid, order_id, order, context=context)
+                self._update_order(order_id, order)
             else:
                 if to_invoice:
-                    self._match_payment_to_invoice(cr, uid, order, context=context)
+                    self._match_payment_to_invoice(order)
 
-                self._update_order(cr, uid, order_id, order, context=context)
+                self._update_order(order_id, order)
                 order_ids.append(order_id)
 
                 # REMOVE ME IN 10.0
@@ -82,10 +82,10 @@ class pos_order(models.Model):
                 # As we'll use a non-stored computed field later, empty the cache
                 # ensure not computing this field for the full order list of the session
                 # which is a mess with big pos sessions (4000+ tickets)
-                self.pool['pos.order'].browse(cr, uid, [], context).env.invalidate_all()
-
+                self.env['pos.order'].browse([]).env.invalidate_all()
+                order = self.env['pos.order'].browse(order_id)
                 try:
-                    self.signal_workflow(cr, uid, [order_id], 'paid')
+                    order.signal_workflow('paid')
                 except psycopg2.OperationalError:
                     # do not hide transactional errors, the order(s) won't be saved!
                     raise
@@ -93,35 +93,32 @@ class pos_order(models.Model):
                     _logger.error('Could not fully process the POS Order: %s', tools.ustr(e))
 
                 if to_invoice:
-                    self.action_invoice(cr, uid, [order_id], context)
-                    order_obj = self.browse(cr, uid, order_id, context)
-                    self.pool['account.invoice'].signal_workflow(cr, SUPERUSER_ID, [order_obj.invoice_id.id],
-                                                                 'invoice_open')
+                    order.action_invoice()
+                    order.invoice_id.sudo().signal_workflow('invoice_open')
 
         return order_ids
 
-    #DO IN NEW API
-    def _update_order(self, cr, uid, order_id, order_data, context=None):
-        prec_acc = self.pool.get('decimal.precision').precision_get(cr, uid, 'Account')
-        session = self.pool.get('pos.session').browse(cr, uid, order_data['pos_session_id'], context=context)
+    @api.model
+    def _update_order(self, order_id, order_data):
+        prec_acc = self.env['decimal.precision'].precision_get('Account')
+        session = self.env['pos.session'].browse(order_data['pos_session_id'])
 
         if session.state == 'closing_control' or session.state == 'closed':
-            session_id = self._get_valid_session(cr, uid, order_data, context=context)
-            session = self.pool.get('pos.session').browse(cr, uid, session_id, context=context)
+            session_id = self._get_valid_session(order_data)
+            session = self.env['pos.session'].browse(session_id)
             order_data['pos_session_id'] = session_id
-        process_line = partial(self.pool['pos.order.line']._order_line_fields, cr, uid, context=context)
+        process_line = partial(self.env['pos.order.line']._order_line_fields)
         vals = {
-            'lines': [[5, 0, 0]]+[process_line(l) for l in order_data['lines']] if order_data['lines'] else False,
+            'lines': [[5, 0, 0]] + [process_line(l) for l in order_data['lines']] if order_data['lines'] else False,
             'session_id': order_data['pos_session_id'],
             'partner_id': order_data['partner_id'] or False,
             'fiscal_position_id': order_data['fiscal_position_id'],
         }
-        self.write(cr, uid, order_id, vals , context)
+        self.browse(order_id).write(vals)
         journal_ids = set()
         for payments in order_data['statement_ids']:
             if not float_is_zero(payments[2]['amount'], precision_digits=prec_acc):
-                self.add_payment(cr, uid, order_id, self._payment_fields(cr, uid, payments[2], context=context),
-                                 context=context)
+                self.add_payment(order_id, self._payment_fields(payments[2]))
             journal_ids.add(payments[2]['journal_id'])
 
         if session.sequence_number <= order_data['sequence_number']:
@@ -132,10 +129,10 @@ class pos_order(models.Model):
             cash_journal = session.cash_journal_id.id
             if not cash_journal:
                 # Select for change one of the cash journals used in this payment
-                cash_journal_ids = self.pool['account.journal'].search(cr, uid, [
+                cash_journal_ids = self.env['account.journal'].search([
                     ('type', '=', 'cash'),
                     ('id', 'in', list(journal_ids)),
-                ], limit=1, context=context)
+                ], limit=1)
                 if not cash_journal_ids:
                     # If none, select for change one of the cash journals of the POS
                     # This is used for example when a customer pays by credit card
@@ -145,9 +142,9 @@ class pos_order(models.Model):
                     if not cash_journal_ids:
                         raise UserError(_("No cash statement found for this session. Unable to record returned cash."))
                 cash_journal = cash_journal_ids[0]
-            self.add_payment(cr, uid, order_id, {
+            self.add_payment(order_id, {
                 'amount': -order_data['amount_return'],
                 'payment_date': time.strftime('%Y-%m-%d %H:%M:%S'),
                 'payment_name': _('return'),
-                'journal': cash_journal,
-            }, context=context)
+                'journal': cash_journal.id,
+            })
--- addons/l10n_fr_pos_cert2/static/src/js/models.js
+++ addons/l10n_fr_pos_cert2/static/src/js/models.js
@@ -11,19 +11,16 @@ var QWeb = core.qweb;
 
 var _t = core._t;
 
-
-var certification_deferred = null;
-
 var _super_order = models.Order;
 models.Order = models.Order.extend({
     export_for_printing: function () {
         var json = _super_order.prototype.export_for_printing.apply(this, arguments);
         json.l10n_fr_hash = this.l10n_fr_hash;
-        return json
+        return json;
     },
 
     export_as_JSON: function () {
-        var json = _super_order.prototype.export_as_JSON.apply(this,arguments);
+        var json = _super_order.prototype.export_as_JSON.apply(this, arguments);
         return _.extend(json, {
             'l10n_fr_hash': this.l10n_fr_hash,
             'l10n_fr_proforma': this.l10n_fr_proforma,
@@ -32,70 +29,18 @@ models.Order = models.Order.extend({
 
 });
 
-//to add if we don't want modifications of existing lines
-/*
-var orderline_super = models.Orderline.prototype;
-models.Orderline = models.Orderline.extend({
-    init_from_JSON: function (json) {
-        orderline_super.init_from_JSON.apply(this, arguments);
-        this.l10n_fr_proforma_finalized = json.l10n_fr_proforma_finalized;
-    },
-
-    export_as_JSON: function () {
-        var json = orderline_super.export_as_JSON.apply(this, arguments);
-
-        return _.extend(json, {
-            'l10n_fr_proforma_finalized': this.l10n_fr_proforma_finalized
-        });
-    },
-
-    set_discount: function (discount) {
-        if (this.l10n_fr_proforma_finalized) {
-            this._show_finalized_error();
-        } else {
-            orderline_super.set_discount.apply(this, arguments);
-        }
-    },
-
-    set_unit_price: function (price) {
-        if (this.l10n_fr_proforma_finalized) {
-            this._show_finalized_error();
-        } else {
-            orderline_super.set_unit_price.apply(this, arguments);
-        }
-    },
-
-    set_quantity: function(quantity){
-        if (this.l10n_fr_proforma_finalized) {
-            this._show_finalized_error();
-        } else {
-            orderline_super.set_quantity.apply(this, arguments);
-        }
-    },
-
-    _show_finalized_error: function () {
-        this.pos.gui.show_popup("error", {
-            'title': _t("Order error"),
-            'body':  _t("This orderline has already been finalized in a pro forma order and " +
-                "can no longer be modified. Please create a new line with eg. a negative quantity."),
-        });
-    },
-
-    can_be_merged_with: function (orderline, ignore_blackbox_finalized) {
-        if (this.l10n_fr_proforma_finalized || orderline.l10n_fr_proforma_finalized) {
-            return false;
-        } else {
-            return orderline_super.can_be_merged_with.apply(this, arguments);
-        }
-    },
-});
-*/
-
 var _super_pos = models.PosModel.prototype;
 models.PosModel = models.PosModel.extend({
+
+    initialize: function(session, attributes) {
+        this.certification_deferred = null;
+        return _super_pos.initialize.call(this, session, attributes);
+    },
+
     _save_to_server: function (orders, options) {
-        certification_deferred = new $.Deferred();
-        var order = this.get('selectedOrder');
+        this.certification_deferred = new $.Deferred();
+        var order = this.get_order();
+        var self = this;
         return _super_pos._save_to_server.apply(this, arguments).then(function (server_ids) {
             if (server_ids) {
                 if (server_ids.length > 0) {
@@ -111,19 +56,19 @@ models.PosModel = models.PosModel.extend({
                                 order.l10n_fr_hash = hash;
                             }
                         });
-                        certification_deferred.resolve(hash);
+                        self.certification_deferred.resolve(hash);
                         return server_ids;
                     }).fail(function (error, event) {
-                        certification_deferred.reject();
+                        self.certification_deferred.reject();
                         return server_ids;
                     });
                 }
-                certification_deferred.resolve(false);
+                self.certification_deferred.resolve(false);
                 return server_ids;
             }
-            certification_deferred.reject();
+            self.certification_deferred.reject();
         }, function error() {
-             certification_deferred.reject();
+           self.certification_deferred.reject();
         });
     },
 
@@ -133,7 +78,6 @@ models.PosModel = models.PosModel.extend({
             return this.push_order(old_order, undefined, true).then( function () {
                 // mark the lines that have been pro forma'd, because we won't allow to change them
                 old_order.get_orderlines().forEach(function (current, index, array) {
-                    current.l10n_fr_proforma_finalized = true;
                     current.trigger('change', current); // force export
                 });
             });
@@ -154,27 +98,28 @@ models.PosModel = models.PosModel.extend({
     },
 });
 
-var ReceiptScreenWidgetParent = screens.ReceiptScreenWidget;
-screens.ReceiptScreenWidget = screens.ReceiptScreenWidget.extend({
+
+screens.ReceiptScreenWidget.include({
     // Overload Function
     show: function(){
         var self = this;
-        certification_deferred.then(function success(hash) {
-                ReceiptScreenWidgetParent.prototype.show.apply(self, []);
+        var super_function = this._super;
+        this.pos.certification_deferred.then(function success(hash) {
+            super_function.apply(self);
         }, function error() {
-            self.pos.get('selectedOrder')._printed = true;
-            ReceiptScreenWidgetParent.prototype.show.apply(self, []);
+            self.pos.get_order()._printed = true;
+            super_function.apply(self);
             self.$('.pos-sale-ticket').hide();
             self.pos.gui.show_popup('confirm', {
                 title: _t('Connection required'),
                 body: _t('Can not print the bill because your point of sale is currently offline, do you want to retry?'),
                 confirm: function () {
-                    self.pos.get('selectedOrder').finalized = false;
-                    self.pos.get('selectedOrder')._printed = false;
+                    self.pos.get_order().finalized = false;
+                    self.pos.get_order()._printed = false;
                     self.gui.show_screen('payment');
                 },
                 cancel: function() {
-                    self.click_next()
+                    self.click_next();
                 }
             });
         });
@@ -190,7 +135,7 @@ screens.ReceiptScreenWidget = screens.ReceiptScreenWidget.extend({
         var receipt = QWeb.render('XmlReceipt',env);
         var posOrderModel = new Model('pos.order');
         posOrderModel.call('save_ticket', [this.pos.get_order().export_as_JSON(), receipt, {}]);
-        ReceiptScreenWidgetParent.prototype.print.apply(this, []);
+        this._super();
     }
 });
 
@@ -203,12 +148,12 @@ screens.ProductScreenWidget.include({
         var reprint_button = this.action_buttons['reprint'];
         if (reprint_button) {
             var super_reprint_click = reprint_button.button_click;
-            self = this;
+            var self = this;
             reprint_button.button_click = function () {
                 if (self.pos.old_receipt) {
                     self.pos.old_receipt = self.pos.old_receipt.replace(/(<div class="l10n_fr_hash">)(.*)(<\/div>)/, '$1DUPLICATA$3');
                 }
-                super_reprint_click.bind(self)()
+                super_reprint_click.bind(self)();
             }
         }
 
@@ -216,7 +161,7 @@ screens.ProductScreenWidget.include({
         if (print_bill_button) {
             var super_print_bill_button_click = print_bill_button.button_click;
             print_bill_button.button_click = function () {
-                self = this;
+                var self = this;
                 var order = self.pos.get_order();
                 if(order.get_orderlines().length > 0){
                     self.pos._l10n_fr_push_proforma().then(function () {
@@ -227,7 +172,7 @@ screens.ProductScreenWidget.include({
                         });
                         var posOrderModel = new Model('pos.order');
                         posOrderModel.call('save_ticket', [self.pos.get_order().export_as_JSON(), html_receipt, {}]);
-                        super_print_bill_button_click.bind(self)()
+                        super_print_bill_button_click.bind(self)();
                     });
                 }
             }
@@ -239,12 +184,9 @@ DB.include({
     // do not remove pro forma to keep them in localstorage after sent
     // to server and avoid losing it when the browser is closed
     remove_unpaid_order: function(order){
-        var orders = this.load('unpaid_orders',[]);
-        orders = _.filter(orders, function(o){
-            return (order.l10n_fr_proforma === true ||
-                    o.id !== order.uid);
-        });
-        this.save('unpaid_orders',orders);
+        if (! order.l10n_fr_proforma) {
+            this._super(order);
+        }
     },
 });
 
