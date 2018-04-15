bb24f3d996a07c242b5008dd7e3cb3d9a22cc505
Swapnesh Shah <swapneshshah111@gmail.com>
2018-04-15 13:07:26 +0530

    [IMP] mass_mailing: Use odoo instead of openerp
    Closes #24237

--- addons/mass_mailing/models/mail_mail.py
+++ addons/mass_mailing/models/mail_mail.py
@@ -6,7 +6,7 @@ import werkzeug.urls
 
 from odoo import api, fields, models, tools
 
-from openerp.addons.link_tracker.models.link_tracker import URL_REGEX
+from odoo.addons.link_tracker.models.link_tracker import URL_REGEX
 
 
 class MailMail(models.Model):
