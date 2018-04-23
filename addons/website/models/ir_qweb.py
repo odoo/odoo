5174b357602dae424134411d1fbba9ed6f5a50fd
Christophe Matthieu <chm@odoo.com>
2018-04-23 10:01:36 +0200

    [FIX] website: apply cdn for background-image

--- addons/website/models/ir_qweb.py
+++ addons/website/models/ir_qweb.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import re
 from collections import OrderedDict
 
 from odoo import models
 from odoo.http import request
+
+
+re_background_image = re.compile(r"(background-image\s*:\s*url\(\s*['\"]?\s*)([^)'\"]+)")
 
 
 class QWeb(models.AbstractModel):
@@ -40,5 +44,8 @@ class QWeb(models.AbstractModel):
         if name and name in atts:
             atts = OrderedDict(atts)
             atts[name] = website.get_cdn_url(atts[name])
+        if isinstance(atts.get('style'), str) and 'background-image' in atts['style']:
+            atts = OrderedDict(atts)
+            atts['style'] = re_background_image.sub(lambda m: '%s%s' % (m.group(1), website.get_cdn_url(m.group(2))), atts['style'])
 
         return atts
