08a1b6b51a395f2bd4c23fc076cca8e7019d92ef
Nicolas Martinelli <nim@odoo.com>
2018-04-25 09:07:23 +0200

    [FIX] account: reconciliation model with Tax 0
    - Create the following reconciliation model:
      Line 1 is 50% of amount, tax is 20%
      Line 2 is 50% of amount, tax is 0%
    - Use this reconciliation model to reconcile an amount of 1000.
    
    4 lines are created:
    - 2 lines for base amount (500)
    - 1 line for the 20% tax (100)
    - 1 line for the 0% tax (1000)
    
    Obviously, the last line is incorrect, and should be 0.
    
    opw-1837049

--- addons/account/static/src/js/reconciliation/reconciliation_model.js
+++ addons/account/static/src/js/reconciliation/reconciliation_model.js
@@ -964,6 +964,7 @@ var StatementModel = BasicModel.extend({
         var formatOptions = {
             currency_id: line.st_line.currency_id,
         };
+        var amount = values.amount !== undefined ? values.amount : line.balance.amount;
         var prop = {
             'id': _.uniqueId('createLine'),
             'label': values.label || line.st_line.name,
@@ -975,8 +976,7 @@ var StatementModel = BasicModel.extend({
             'debit': 0,
             'credit': 0,
             'base_amount': values.amount_type !== "percentage" ?
-                (values.amount || line.balance.amount) :
-                line.balance.amount * values.amount / 100,
+                (amount) : line.balance.amount * values.amount / 100,
             'percent': values.amount_type === "percentage" ? values.amount : null,
             'link': values.link,
             'display': true,
