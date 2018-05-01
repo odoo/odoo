d28f3778730188019ef0365e8a428dcfe16138e5
Siddarth Gajjar <sga@odoo.com>
2018-05-01 15:45:49 +0530

    [IMP] calendar: generic improvements
    We need the calendar view to be consistent in the usage of time formats
    currently the week column (on the far left) is not consistent with what
    we display in the meeting time slot.
    
    It is currently not ovious that you can add and view anybody's calendar
    so we need to add a placeholder.
    
    In order to create a meeting you have to click on create which is not
    explicit.
    
    Task #1838108
    Closes #24675

--- addons/web/static/src/js/views/calendar/calendar_renderer.js
+++ addons/web/static/src/js/views/calendar/calendar_renderer.js
@@ -78,6 +78,9 @@ var SidebarFilter = Widget.extend(FieldManagerMixin, {
                     {
                         mode: 'edit',
                         can_create: false,
+                        attrs: {
+                            'placeholder': _.str.sprintf(_t("Add %s"), self.title),
+                        },
                     });
             });
             defs.push(def);
@@ -388,7 +391,8 @@ return AbstractRenderer.extend({
                 if (!event.allDay) {
                     var start = event.r_start || event.start;
                     var end = event.r_end || event.end;
-                    display_hour = start.format('HH:mm') + ' - ' + end.format('HH:mm');
+                    var timeFormat = _t.database.parameters.time_format.search("%H") != -1 ? 'HH:mm': 'h:mma';
+                    display_hour = start.format(timeFormat) + ' - ' + end.format(timeFormat);
                     if (display_hour === '00:00 - 00:00') {
                         display_hour = _t('All day');
                     }
