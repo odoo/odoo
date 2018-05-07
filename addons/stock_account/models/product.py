67041f5b76ebb21c19b73874bd62a7b7ee4eb7cc
victor <victor@victor-U46E>
2018-05-07 18:05:56 -0500

    [IMP][product]: asiento contable en historico de precios de productos.

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -121,7 +121,7 @@ class ProductProduct(models.Model):
         locations = self.env['stock.location'].search([('usage', '=', 'internal'), ('company_id', '=', self.env.user.company_id.id)])
 
         product_accounts = {product.id: product.product_tmpl_id.get_product_accounts() for product in self}
-
+        ctx =  self._context.copy()
         for location in locations:
             for product in self.with_context(location=location.id, compute_child=False).filtered(lambda r: r.valuation == 'real_time'):
                 diff = product.standard_price - new_price
@@ -153,8 +153,11 @@ class ProductProduct(models.Model):
                     }
                     move = AccountMove.create(move_vals)
                     move.post()
-
-        self.write({'standard_price': new_price})
+                    #Enviamos por contexto el move para linkear con historia de costos del producto.
+                    ctx.update({
+                        'account_move_id': move.id
+                    })
+        self.with_context(ctx).write({'standard_price': new_price})
         return True
 
 
