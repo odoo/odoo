03d04f63915472f7ce5a8740b32443ce799cf9e1
Damien Bouvy <dbo@odoo.com>
2018-05-08 15:30:24 +0200

    Revert "[FIX] pos: fast closing pos session"
    This reverts commit 18a7c8cd3a874127d02699c181e51eff094c8d42.

--- addons/point_of_sale/models/pos_session.py
+++ addons/point_of_sale/models/pos_session.py
@@ -22,6 +22,7 @@ class PosSession(models.Model):
                 'pos.closing.journal_id_%s' % company_id, default=session.config_id.journal_id.id)
             if not journal_id:
                 raise UserError(_("You have to set a Sale Journal for the POS:%s") % (session.config_id.name,))
+
             move = self.env['pos.order'].with_context(force_company=company_id)._create_account_move(session.start_at, session.name, int(journal_id), company_id)
             orders.with_context(force_company=company_id)._create_account_move_line(session, move)
             for order in session.order_ids.filtered(lambda o: o.state not in ['done', 'invoiced']):
