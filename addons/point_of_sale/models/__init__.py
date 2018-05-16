220ac81fa6e8695dc68abd18997c16c6c919db0d
Mitali Patel <mpa@odoo.com>
2018-05-16 11:58:08 +0530

    [IMP] point_of_sale: implement digest KPIs / tips
    Purpose of this commit is to send POS sales details in digest emails. Those
    are added in the default digest template.
    
    This commit is linked to task ID 30655 and PR #18318.

--- addons/point_of_sale/models/__init__.py
+++ addons/point_of_sale/models/__init__.py
@@ -5,6 +5,7 @@ from . import account_bank_statement
 from . import account_journal
 from . import account_tax
 from . import barcode_rule
+from . import digest
 from . import pos_category
 from . import pos_config
 from . import pos_order
