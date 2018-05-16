0984b2aadb0156e1f742f0bf14f8d36b6d170f23
Yannick Tivisse <yti@odoo.com>
2018-05-16 15:11:42 +0200

    [FIX] l10n_be_hr_payroll: Compute correcty employer cost for warrants

--- addons/l10n_be_hr_payroll/models/l10n_be_hr_payroll.py
+++ addons/l10n_be_hr_payroll/models/l10n_be_hr_payroll.py
@@ -126,7 +126,7 @@ class HrContract(models.Model):
     @api.depends('commission_on_target')
     def _compute_warrants_cost(self):
         for contract in self:
-            contract.warrants_cost = contract.commission_on_target * 1.326 * 1.05 * 12.0
+            contract.warrants_cost = contract.commission_on_target * 1.326 / 1.05 * 12.0
             contract.warrant_value_employee = contract.commission_on_target * 1.326 * (1.00 - 0.535) * 12.0
 
     @api.depends('wage', 'fuel_card', 'representation_fees', 'transport_employer_cost',
