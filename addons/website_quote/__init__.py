e139fd9dcf11779cef3cb0278a315fe5f7625061
Joren Van Onder <jov@odoo.com>
2018-05-17 14:41:24 -0700

    [FIX] website_quote: install sale_payment together with website_quote
    Stable version of 2771bf8dc16f4166436ae24025a633485e97aa2d (reverted
    at 36a6a591f76094f9e44ca49cc7251ebd02edfa43) that doesn't cause the
    following errors:
    
    odoo.modules.graph: module website_quote: Unmet dependencies:
    sale_payment
    
    on existing databases without sale_payment installed.
    
    A downside of this approach is that sale_payment can be uninstalled
    without uninstalling website_quote. Adding an uninstall_hook to
    sale_payment could solve that, but it wouldn't warn the user
    website_quote would be removed in the "Uninstall module"
    wizard. Keeping in mind this is only a temporary hack for stable,
    keeping sale_payment installed in this case shouldn't cause any
    issues.
    
    opw-1847977

--- addons/website_quote/__init__.py
+++ addons/website_quote/__init__.py
@@ -3,3 +3,12 @@
 
 from . import controllers
 from . import models
+
+from odoo.api import Environment, SUPERUSER_ID
+
+def _install_sale_payment(cr, registry):
+    env = Environment(cr, SUPERUSER_ID, {})
+    env['ir.module.module'].search([
+        ('name', '=', 'sale_payment'),
+        ('state', '=', 'uninstalled'),
+    ]).button_install()
