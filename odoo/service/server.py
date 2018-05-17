17c67c93877a9ebb8b61986e8ae843799d8a8eef
Rod Schouteden <rod@schout-it.be>
2018-05-17 10:26:27 +0200

    compatibility gevent 1.3.0
    gevent 1.3.0 removed backward compatibility on gevent.wsgi.
    make the new path the default

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -363,7 +363,10 @@ class GeventServer(CommonServer):
 
     def start(self):
         import gevent
-        from gevent.wsgi import WSGIServer
+        try:
+            from gevent.pywsgi import WSGIServer
+        except:
+            from gevent.wsgi import WSGIServer
 
 
         if os.name == 'posix':
