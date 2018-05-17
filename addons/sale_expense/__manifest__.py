59636caebae77a527b1e62f85b6a741d4ef029d9
Gert Pellin <gpe@odoo.com>
2018-05-17 13:19:44 +0200

    [imp] sale_expense: Make expenses re-invoicing easier
    Change the data of the product Expenses : https://drive.google.com/a/odoo.com/file/d/1N0jW9QDQ35-5EY13fAsDIwuW8HHUXi-3/view?usp=drivesdk
    When creating a new expense product from the Expense app, the invoicing information should be Delivered and At cost by default  https://drive.google.com/a/odoo.com/file/d/1IoqYU8I5MBaeabQJOvQoO-NQl1RK2yPy/view?usp=drivesdk
    
    project : RD for Dummies
    task : [Expense] EXP product

--- addons/sale_expense/__manifest__.py
+++ addons/sale_expense/__manifest__.py
@@ -18,11 +18,12 @@ This module allow to reinvoice employee expense, by setting the SO directly on t
     'data': [
         'security/ir.model.access.csv',
         'security/sale_expense_security.xml',
+        'data/sale_expense_data.xml',
         'views/product_view.xml',
         'views/hr_expense_views.xml',
         'views/sale_order_views.xml',
     ],
-    'demo': ['sale_expense_demo.xml'],
+    'demo': ['data/sale_expense_demo.xml'],
     'test': [],
     'installable': True,
     'auto_install': True,
