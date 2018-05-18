df592133b5907c944c3d028bb4a5133c7b280024
Deep Patel <dep@odoo.com>
2018-05-18 13:26:25 +0200

    [ADD] crm_reveal: Automated lead generation based on Website visitor
    This module use odoo's IAP service to get lead data based on IP address.
    
    How this module works
    - Backend user will create rules for lead generation according to his/her need
    - When any visitors visits webpages, this module keep the IP address of the visitor
    - A cron job will call iap service to get lead data
    - IAP service will return lead data and lead is created
    
    Co-authored-by: Parth Gajjar <pga@odoo.com>

---
+++ addons/crm_reveal/models/crm_lead.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class Lead(models.Model):
+    _inherit = 'crm.lead'
+
+    reveal_ip = fields.Char(string='IP Address')
+    reveal_iap_credits = fields.Integer(string='IAP Credits')
+    reveal_rule_id = fields.Many2one('crm.reveal.rule', string='Lead Generation Rule', index=True)
+    reveal_id = fields.Char(string='Reveal ID', index=True)
