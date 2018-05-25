7f2a1f3a7ce758b035a6094a9c2bc2d028b1d221
Pierre Masereel <pim@odoo.com>
2018-05-25 12:00:51 +0200

    [TEST] base: child_of/parent_of with empty list or False value

--- odoo/addons/base/tests/test_expression.py
+++ odoo/addons/base/tests/test_expression.py
@@ -134,6 +134,14 @@ class TestExpression(TransactionCase):
         cats = Category.search([('id', 'child_of', categ_1.ids)])
         self.assertEqual(len(cats), 1)
 
+        # test hierarchical search in m2m with an empty list
+        cats = Category.search([('id', 'child_of', [])])
+        self.assertEqual(len(cats), 0)
+
+        # test hierarchical search in m2m with 'False' value
+        cats = Category.search([('id', 'child_of', False)])
+        self.assertEqual(len(cats), 20)
+
         # test hierarchical search in m2m with parent id (list of ids)
         cats = Category.search([('id', 'parent_of', categ_1.ids)])
         self.assertEqual(len(cats), 3)
@@ -153,6 +161,14 @@ class TestExpression(TransactionCase):
         # test hierarchical search in m2m with parent ids
         cats = Category.search([('id', 'parent_of', categ_root.ids)])
         self.assertEqual(len(cats), 1)
+
+        # test hierarchical search in m2m with an empty list
+        cats = Category.search([('id', 'parent_of', [])])
+        self.assertEqual(len(cats), 0)
+
+        # test hierarchical search in m2m with 'False' value
+        cats = Category.search([('id', 'parent_of', False)])
+        self.assertEqual(len(cats), 0)
 
     def test_10_equivalent_id(self):
         # equivalent queries
