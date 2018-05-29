4e5f826ce53b9e4cb73eea77935ea82e81ac66b9
Kinjal Mehta <kme@odoo.com>
2018-05-29 09:56:09 +0200

    [IMP] web: use burger menu for tablet in vertical mode
    Task #34853

--- addons/web/static/src/js/services/config.js
+++ addons/web/static/src/js/services/config.js
@@ -83,7 +83,7 @@ function _updateSizeProps() {
     var sc = _getSizeClass();
     if (sc !== config.device.size_class) {
         config.device.size_class = sc;
-        config.device.isMobile = config.device.size_class <= config.device.SIZES.XS;
+        config.device.isMobile = config.device.size_class <= config.device.SIZES.SM;
     }
 }
 
