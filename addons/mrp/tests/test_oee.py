b1b82f7ff0b2cce69b6cc5e5743710cfc481b6ba
cbu-odoo (@havelock.kujiu.org) <kujiu@kujiu.org>
2018-06-01 08:02:32 +0200

    [FIX] MRP test failed on working resource

--- addons/mrp/tests/test_oee.py
+++ addons/mrp/tests/test_oee.py
@@ -6,6 +6,7 @@ from odoo import fields
 from odoo.addons.mrp.tests.common import TestMrpCommon
 
 from odoo.tools.datetime import date, timedelta, datetime, time
+from unittest.mock import patch
 
 class TestOee(TestMrpCommon):
     def create_productivity_line(self, loss_reason, date_start=False, date_end=False):
@@ -19,50 +20,49 @@ class TestOee(TestMrpCommon):
 
     def test_wrokcenter_oee(self):
         """  Test case workcenter oee. """
-        day = date.today()
-        # Make the test work the weekend. It will fails due to workcenter working hours.
-        if day.weekday() in (5, 6):
-            day -= timedelta(days=2)
+        with patch.object(datetime, 'now', lambda tzinfo=None: datetime(2018, 10, 30, 10, 0)), \
+             patch.object(date, 'today', lambda tzinfo=None: date(2018, 10, 30)):
+            day = date.today()
 
-        tz = self.workcenter_1.resource_calendar_id.tz
+            tz = self.workcenter_1.resource_calendar_id.tz
 
-        def time_to_string_utc_datetime(time):
-            return datetime.combine(day, time, tzinfo=tz).astimezone('UTC')
+            def time_to_string_utc_datetime(time):
+                return datetime.combine(day, time, tzinfo=tz).astimezone('UTC')
 
-        start_time = time_to_string_utc_datetime(time(10, 43, 22))
-        end_time = time_to_string_utc_datetime(time(10, 56, 22))
-        # Productive time duration (13 min)
-        self.create_productivity_line(self.env.ref('mrp.block_reason7'), start_time, end_time)
+            start_time = time_to_string_utc_datetime(time(10, 43, 22))
+            end_time = time_to_string_utc_datetime(time(10, 56, 22))
+            # Productive time duration (13 min)
+            self.create_productivity_line(self.env.ref('mrp.block_reason7'), start_time, end_time)
 
-        # Material Availability time duration (1.52 min)
-        # Check working state is blocked or not.
-        start_time = datetime.combine(day, time(10, 47, 8))
-        workcenter_productivity_1 = self.create_productivity_line(self.env.ref('mrp.block_reason0'), start_time)
-        self.assertEqual(self.workcenter_1.working_state, 'blocked', "Wrong working state of workcenter.")
+            # Material Availability time duration (1.52 min)
+            # Check working state is blocked or not.
+            start_time = datetime.combine(day, time(10, 47, 8))
+            workcenter_productivity_1 = self.create_productivity_line(self.env.ref('mrp.block_reason0'), start_time)
+            self.assertEqual(self.workcenter_1.working_state, 'blocked', "Wrong working state of workcenter.")
 
-        # Check working state is normal or not.
-        end_time = datetime.combine(day, time(10, 48, 39))
-        workcenter_productivity_1.write({'date_end': end_time})
-        self.assertEqual(self.workcenter_1.working_state, 'normal', "Wrong working state of workcenter.")
+            # Check working state is normal or not.
+            end_time = datetime.combine(day, time(10, 48, 39))
+            workcenter_productivity_1.write({'date_end': end_time})
+            self.assertEqual(self.workcenter_1.working_state, 'normal', "Wrong working state of workcenter.")
 
-        # Process Defect time duration (1.33 min)
-        start_time = datetime.combine(day, time(10, 48, 38))
-        end_time = datetime.combine(day, time(10, 49, 58))
-        self.create_productivity_line(self.env.ref('mrp.block_reason5'), start_time, end_time)
-        # Reduced Speed time duration (3.0 min)
-        start_time = datetime.combine(day, time(10, 50, 22))
-        end_time = datetime.combine(day, time(10, 53, 22))
-        self.create_productivity_line(self.env.ref('mrp.block_reason4'), start_time, end_time)
+            # Process Defect time duration (1.33 min)
+            start_time = datetime.combine(day, time(10, 48, 38))
+            end_time = datetime.combine(day, time(10, 49, 58))
+            self.create_productivity_line(self.env.ref('mrp.block_reason5'), start_time, end_time)
+            # Reduced Speed time duration (3.0 min)
+            start_time = datetime.combine(day, time(10, 50, 22))
+            end_time = datetime.combine(day, time(10, 53, 22))
+            self.create_productivity_line(self.env.ref('mrp.block_reason4'), start_time, end_time)
 
-        # Block time : ( Process Defact (1.33 min) + Reduced Speed (3.0 min) + Material Availability (1.52 min)) = 5.85 min
-        blocked_time_in_hour = round(((1.33 + 3.0 + 1.52) / 60.0), 2)
-        # Productive time : Productive time duration (13 min)
-        productive_time_in_hour = round((13.0 / 60.0), 2)
+            # Block time : ( Process Defact (1.33 min) + Reduced Speed (3.0 min) + Material Availability (1.52 min)) = 5.85 min
+            blocked_time_in_hour = round(((1.33 + 3.0 + 1.52) / 60.0), 2)
+            # Productive time : Productive time duration (13 min)
+            productive_time_in_hour = round((13.0 / 60.0), 2)
 
-        # Check blocked time and productive time
-        self.assertEqual(self.workcenter_1.blocked_time, blocked_time_in_hour, "Wrong block time on workcenter.")
-        self.assertEqual(self.workcenter_1.productive_time, productive_time_in_hour, "Wrong productive time on workcenter.")
+            # Check blocked time and productive time
+            self.assertEqual(self.workcenter_1.blocked_time, blocked_time_in_hour, "Wrong block time on workcenter.")
+            self.assertEqual(self.workcenter_1.productive_time, productive_time_in_hour, "Wrong productive time on workcenter.")
 
-        # Check overall equipment effectiveness
-        computed_oee = round(((productive_time_in_hour * 100.0)/(productive_time_in_hour + blocked_time_in_hour)), 2)
-        self.assertEqual(self.workcenter_1.oee, computed_oee, "Wrong oee on workcenter.")
+            # Check overall equipment effectiveness
+            computed_oee = round(((productive_time_in_hour * 100.0)/(productive_time_in_hour + blocked_time_in_hour)), 2)
+            self.assertEqual(self.workcenter_1.oee, computed_oee, "Wrong oee on workcenter.")
