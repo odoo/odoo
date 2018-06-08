c3e421c3098e76e56e0cb021cd13bcbe741f66e7
qdp-odoo <qdp@odoo.com>
2018-06-08 17:11:02 +0200

    [REF] refactoring made during code review + usability of manual bank account creation

--- addons/account/wizard/setup_wizards.py
+++ addons/account/wizard/setup_wizards.py
@@ -52,17 +52,19 @@ class SetupBarBankConfigWizard(models.TransientModel):
     _inherits = {'res.partner.bank': 'res_partner_bank_id'}
     _name = 'account.setup.bank.manual.config'
 
-    setup_journal_link_creation = fields.Selection(selection=[('new', 'Create new journal'), ('link', 'Link to an existing journal')], default='new')
-    single_journal_id = fields.Many2one(string="Journal", comodel_name='account.journal', compute='compute_single_journal_id', inverse='set_single_journal_id')
-    single_journal_name = fields.Char(compute='compute_single_journal_related_data', inverse='set_single_journal_id', required=True)
-    single_journal_code = fields.Char(string="Code", required=True)
+    create_or_link_option = fields.Selection(selection=[('new', 'Create new journal'), ('link', 'Link to an existing journal')], default='new')
+    new_journal_name = fields.Char(compute='compute_new_journal_related_data', inverse='set_linked_journal_id', required=True, help='Will be used to name the Journal related to this bank account')
+    linked_journal_id = fields.Many2one(string="Journal", comodel_name='account.journal', compute='compute_linked_journal_id', inverse='set_linked_journal_id')
+    new_journal_code = fields.Char(string="Code", required=True, default=lambda self: self.env['account.journal'].get_next_bank_cash_default_code('bank', self.env['res.company']._company_default_get('account.journal').id))
 
-    wizard_acc_type = fields.Selection(string="Account Type", selection=lambda x: x.env['res.partner.bank'].get_supported_account_types(), compute='_compute_wizard_acc_type')
+    # field computing the type of the res.patrner.bank. It's behaves the same as a related res_part_bank_id.acc_type
+    # except we want to display  this information while the record isn't yet saved.
+    related_acc_type = fields.Selection(string="Account Type", selection=lambda x: x.env['res.partner.bank'].get_supported_account_types(), compute='_compute_related_acc_type')
 
     @api.depends('acc_number')
-    def _compute_wizard_acc_type(self):
+    def _compute_related_acc_type(self):
         for record in self:
-            record.wizard_acc_type = self.env['res.partner.bank'].retrieve_acc_type(record.acc_number)
+            record.related_acc_type = self.env['res.partner.bank'].retrieve_acc_type(record.acc_number)
 
     @api.model
     def create(self, vals):
@@ -73,34 +75,27 @@ class SetupBarBankConfigWizard(models.TransientModel):
         vals['partner_id'] = self.env.user.company_id.partner_id.id
         return super(SetupBarBankConfigWizard, self).create(vals)
 
-    @api.depends('single_journal_id')
-    def compute_single_journal_related_data(self):
+    @api.depends('linked_journal_id')
+    def compute_new_journal_related_data(self):
         for record in self:
-            if record.single_journal_id:
-                record.single_journal_name = record.single_journal_id.name
+            if record.linked_journal_id:
+                record.new_journal_name = record.linked_journal_id.name
 
     @api.depends('journal_id')  # Despite its name, journal_id is actually a One2many field
-    def compute_single_journal_id(self):
+    def compute_linked_journal_id(self):
         for record in self:
-            record.single_journal_id = record.journal_id and record.journal_id[0] or None
+            record.linked_journal_id = record.journal_id and record.journal_id[0] or None
 
-    @api.onchange('setup_journal_link_creation', 'single_journal_id')
-    def onchange_setup_journal_link_creation(self):
-        if self.single_journal_id and self.setup_journal_link_creation == 'link':
-            self.single_journal_code = self.single_journal_id.code
-        else:
-            self.single_journal_code = self.single_journal_code = self.env['account.journal'].get_next_bank_cash_default_code('bank', self.env['res.company']._company_default_get('account.journal').id)
-
-    def set_single_journal_id(self):
+    def set_linked_journal_id(self):
         """ Called when saving the wizard.
         """
         for record in self:
-            selected_journal = record.single_journal_id
-            if record.setup_journal_link_creation == 'new':
+            selected_journal = record.linked_journal_id
+            if record.create_or_link_option == 'new':
                 company = self.env['res.company']._company_default_get('account.journal')
                 selected_journal = self.env['account.journal'].create({
-                    'name': record.single_journal_name,
-                    'code': record.single_journal_code,
+                    'name': record.new_journal_name,
+                    'code': record.new_journal_code,
                     'type': 'bank',
                     'company_id': company.id,
                 })
