801f708bdfa3e6b28903fe6eadb4b157268321e7
Cedric Snauwaert <csn@openerp.com>
2018-06-08 14:48:37 +0200

    [FIX] l10n_de: when invoicing a line with an account that has a tax, ensure that line has tax of accounts

--- addons/l10n_de/models/datev.py
+++ addons/l10n_de/models/datev.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
 
 class AccountTaxTemplate(models.Model):
     _inherit = 'account.tax.template'
@@ -14,3 +16,36 @@ class AccountTax(models.Model):
     _inherit = "account.tax"
 
     l10n_de_datev_code = fields.Char(size=2, help="2 digits code use by Datev")
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    def write(self, vals):
+        if (vals.get('account_id') or vals.get('invoice_line_tax_ids')):
+            account_id = vals.get('account_id', False)
+            account = account_id and self.env['account.account'].browse(account_id) or self.account_id
+            account_name = account.name
+            account_tax = account.tax_ids.ids
+            if account_tax:
+                if vals.get('invoice_line_tax_ids'):
+                    invoice_line_tax = vals.get('invoice_line_tax_ids')[0][2]
+                else:
+                    invoice_line_tax = self.invoice_line_tax_ids
+                for tax in invoice_line_tax:
+                    if tax not in account_tax:
+                        tax_name = self.env['account.tax'].browse(tax).name
+                        raise UserError(_('Account %s does not authorize to have tax %s specified on the line.') % (tax_name, account_name))
+        return super(AccountInvoiceLine, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('invoice_line_tax_ids'):
+            account = self.env['account.account'].browse(vals.get('account_id'))
+            account_name = account.name
+            account_tax = account.tax_ids.ids
+            if account_tax:
+                for tax in vals.get('invoice_line_tax_ids')[0][2]:
+                    if tax not in account_tax:
+                        tax_name = self.env['account.tax'].browse(tax).name
+                        raise UserError(_('Account %s does not authorize to have tax %s specified on the line.') % (tax_name, account_name))
+        return super(AccountInvoiceLine, self).create(vals)
