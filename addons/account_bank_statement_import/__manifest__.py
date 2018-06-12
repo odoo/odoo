da8d4b5b99e3c103ded6ba7d3b21c5e1ed247f34
Yannick Vaucher <yvaucher@camptocamp.com>
2018-06-12 15:00:21 +0200

    [IMP] account_bank_statement_import: Update module description
    When using Community Edition you don't have access to any of the formats you
    will thus rely on odoo community to create types of import based on this
    generic module.

--- addons/account_bank_statement_import/__manifest__.py
+++ addons/account_bank_statement_import/__manifest__.py
@@ -4,7 +4,11 @@
     'category': 'Accounting',
     'version': '1.0',
     'depends': ['account'],
-    'description': """Generic Wizard to Import Bank Statements. Includes the import of files in .OFX format""",
+    'description': """Generic Wizard to Import Bank Statements.
+
+(This module does not include any type of import format.)
+
+OFX and QIF imports are available in Enterprise version.""",
     'data': [
         'account_bank_statement_import_view.xml',
         'account_import_tip_data.xml',
