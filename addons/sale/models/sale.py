88e4a8e4264854210b2504bcb4468c8ab4d6ad12
Nimesh Jethva <nje@odoo.com>
2018-06-18 17:01:26 +0530

    [IMP] sale, website_quote, website_sale: Make Some template beautiful.
    Purpose of this commit is to enhance quality of templates proposed by Odoo
    and make them use notification layout when send by emails. Those emails
    are cleaner and more up to date compared to other emails.
    
    Related Task ID : 1843171

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -516,7 +516,7 @@ class SaleOrder(models.Model):
             'default_template_id': template_id,
             'default_composition_mode': 'comment',
             'mark_so_as_sent': True,
-            'custom_layout': "mail.mail_notification_borders",
+            'custom_layout': "mail.mail_notification_light",
             'proforma': self.env.context.get('proforma', False),
             'force_email': True
         }
