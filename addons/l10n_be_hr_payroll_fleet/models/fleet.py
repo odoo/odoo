02dd74aadab597b2ce7e1e40b76d1f508520f51e
cbu-odoo (@havelock.kujiu.org) <kujiu@kujiu.org>
2018-06-18 10:57:03 +0200

    [REF] Update date and datetime fields in fleet

--- addons/l10n_be_hr_payroll_fleet/models/fleet.py
+++ addons/l10n_be_hr_payroll_fleet/models/fleet.py
@@ -5,7 +5,7 @@ import babel.dates
 
 from odoo import api, fields, models
 
-from odoo.fields import Datetime
+from odoo.fields import Datetime, Date
 
 
 class FleetVehicle(models.Model):
@@ -80,15 +80,15 @@ class FleetVehicle(models.Model):
     def _get_acquisition_date(self):
         self.ensure_one()
         return babel.dates.format_date(
-            date=Datetime.from_string(self.acquisition_date),
+            date=Date.from_string(self.acquisition_date),
             format='MMMM y',
             locale=self._context.get('lang') or 'en_US'
         )
 
     def _get_car_atn(self, acquisition_date, car_value, fuel_type, co2):
         # Compute the correction coefficient from the age of the car
-        now = Datetime.from_string(Datetime.now())
-        start = Datetime.from_string(acquisition_date)
+        now = Date.today()
+        start = Date.from_string(acquisition_date)
         if start:
             number_of_month = (now.year - start.year) * 12.0 + now.month - start.month + int(bool(now.day - start.day + 1))
             if number_of_month <= 12:
