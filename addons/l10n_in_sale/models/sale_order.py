c83ea064081c12aba37ea84fc35c65e5965965f0
Jigar Vaghela <jva@odoo.com>
2018-06-21 14:37:54 +0530

    [IMP] l10n_in: Improve Indian localization to supports GSTR-1 reports
    Purpose
    =======
    
    GST Invoice Report required to show tax amount product line wise.
    This Tax amount is divided in CESS and IGST or CGST and SGST.
    
    Improve GST Tax, Tax Groups and Tag.
    
    Reverse charge under GST
    As per GST rules tax will have to be paid directly by the receiver to the Government instead of the supplier.
    
    Reseller(E-commerce) under GST
    If you sale through ecommerce then need to specify ecommerce GSTIN.
    
    Import/Export goods under GST
    If you export the goods then need to specify
        -Export Type
        -Shipping bill number
        -Shipping bill date
        -Shipping port code
    
    Credit or Debit Note
    If u give Credit or Debit note then need to specify Refund reason.
    
    Place of Supply is the state of customer.
    
    Added GSTR reports
    
    GSTR Invoice report covers below GSTR-1 sections(this report is grouped by tax rate and journal entries
    B2B, B2CL, B2CS, EXP, CDNR and CDNUR
    
    GSTR payment report
    This report is covered under GSTIR-1 section called AT(advance payment) and ATADJ(advance payment adjustment)
    
    GSTR HSN report(it is based on the product HSN code)
    
    GSTR Exempted report(it is based on nil rated and exempt tax)
    This report is covered under GSTIR-1 section EXEMP
    
    Added demo data to to have GSTR-1 reports out of the box
    
    Solution
    ========
    
    fetched values of CESS, IGST, CGST and SGST amount in invoice report from tax groups
    
    added new taxes called "Nil Rated" and "Exempt" and also added it's related tax groups and accounts
    added tax tags related to GST tax percentage
    
    Set Reverse charge in negative tax
    
    Improved account move line creation grouping machenism:
    Now, move lines related to tax are grouped by product and uom instead of grouping them just by tax(this is for better reporting)

---
+++ addons/l10n_in_sale/models/sale_order.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    l10n_in_reseller_partner_id = fields.Many2one('res.partner',
+        string='Reseller', domain=[('vat', '!=', False)], states={'posted': [('readonly', True)]})
+
+    @api.multi
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals['l10n_in_reseller_partner_id'] = self.l10n_in_reseller_partner_id.id
+        return invoice_vals
