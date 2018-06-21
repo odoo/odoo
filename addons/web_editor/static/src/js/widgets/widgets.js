b84489b3c179a7285ff0e3ea6e8cdd63d0c6eb06
Deep Patel <dep@odoo.com>
2018-06-21 15:26:50 +0530

    [ADD] web_unsplash: Find free high-resolution images from Unsplash
    This commit:
    1. Adds settings to install the new `web_unsplash` enterprise module.
       See counterpart commit on enterprise for details.
    2. Export `ImageWidget` and `update_control_panel` to be able to override them.
    3. Add the unsplash access key rights to website designer. (see related OE
       commit)
    
    task-35060

--- addons/web_editor/static/src/js/widgets/widgets.js
+++ addons/web_editor/static/src/js/widgets/widgets.js
@@ -1103,6 +1103,7 @@ var MediaDialog = Dialog.extend({
     }),
     custom_events: _.extend({}, Dialog.prototype.custom_events || {}, {
         save_request: '_onSaveRequest',
+        update_control_panel: '_updateControlPanel',
     }),
 
     /**
@@ -1768,5 +1769,6 @@ return {
     MediaDialog: MediaDialog,
     LinkDialog: LinkDialog,
     CropImageDialog: CropImageDialog,
+    ImageWidget: ImageWidget,
 };
 });
--- addons/website/models/res_users.py
+++ addons/website/models/res_users.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResUsers(models.Model):
@@ -11,3 +11,10 @@ class ResUsers(models.Model):
         selection=lambda self: self._get_group_selection('base.module_category_website'),
         string='Website Editor', compute='_compute_groups_id', inverse='_inverse_groups_id',
         category_xml_id='base.module_category_website')
+
+    @api.multi
+    def _has_unsplash_key_rights(self):
+        self.ensure_one()
+        if self.has_group('website.group_website_designer'):
+            return True
+        return super(ResUsers, self)._has_unsplash_key_rights()
