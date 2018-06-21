41a6bb45431687cffa913d38c7b09c5a8ece8b97
Jigar Vaghela <jva@odoo.com>
2018-06-21 14:37:54 +0530

    [IMP] l10n_in: Improve Indian localization to supports GSTR-1 reports
    Purpose
    =======
    
    GST Invoice Report required to show tax amount product line wise.
    This Tax amount is divided in CESS and IGST or CGST and SGST.
    
    Improve GST Tax, Tax Groups and Tag.
    
    Reverse charge under GST
    As per GST rules tax will have to be paid directly by the receiver to the Government instead of the supplier.
    
    Reseller(E-commerce) under GST
    If you sale through ecommerce then need to specify ecommerce GSTIN.
    
    Import/Export goods under GST
    If you export the goods then need to specify
        -Export Type
        -Shipping bill number
        -Shipping bill date
        -Shipping port code
    
    Credit or Debit Note
    If u give Credit or Debit note then need to specify Refund reason.
    
    Place of Supply is the state of customer.
    
    Added GSTR reports
    
    GSTR Invoice report covers below GSTR-1 sections(this report is grouped by tax rate and journal entries
    B2B, B2CL, B2CS, EXP, CDNR and CDNUR
    
    GSTR payment report
    This report is covered under GSTIR-1 section called AT(advance payment) and ATADJ(advance payment adjustment)
    
    GSTR HSN report(it is based on the product HSN code)
    
    GSTR Exempted report(it is based on nil rated and exempt tax)
    This report is covered under GSTIR-1 section EXEMP
    
    Added demo data to to have GSTR-1 reports out of the box
    
    Solution
    ========
    
    fetched values of CESS, IGST, CGST and SGST amount in invoice report from tax groups
    
    added new taxes called "Nil Rated" and "Exempt" and also added it's related tax groups and accounts
    added tax tags related to GST tax percentage
    
    Set Reverse charge in negative tax
    
    Improved account move line creation grouping machenism:
    Now, move lines related to tax are grouped by product and uom instead of grouping them just by tax(this is for better reporting)

---
+++ addons/l10n_in/report/account_invoice_report.py
@@ -0,0 +1,310 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, tools
+
+
+class L10nInAccountInvoiceReport(models.Model):
+    _name = "l10n_in.account.invoice.report"
+    _description = "Account Invoice Statistics"
+    _auto = False
+    _order = 'date desc'
+
+    account_move_id = fields.Many2one('account.move', string="Account Move")
+    invoice_id = fields.Many2one('account.invoice', string="Invoice")
+    company_id = fields.Many2one('res.company', string="Company")
+    date = fields.Date(string="Accounting Date")
+    name = fields.Char(string="Invoice Number")
+    partner_id = fields.Many2one('res.partner', string="Customer")
+    is_reverse_charge = fields.Char("Reverse Charge")
+    l10n_in_export_type = fields.Selection([
+        ('regular', 'Regular'), ('deemed', 'Deemed'),
+        ('sale_from_bonded_wh', 'Sale from Bonded WH'),
+        ('export_with_igst', 'Export with IGST'),
+        ('sez_with_igst', 'SEZ with IGST payment'),
+        ('sez_without_igst', 'SEZ without IGST payment')])
+    journal_id = fields.Many2one('account.journal', string="Journal")
+    state = fields.Selection([('draft', 'Unposted'), ('posted', 'Posted')], string='Status')
+    igst_amount = fields.Float(string="IGST Amount")
+    cgst_amount = fields.Float(string="CGST Amount")
+    sgst_amount = fields.Float(string="SGST Amount")
+    cess_amount = fields.Float(string="Cess Amount")
+    price_total = fields.Float(string='Total Without Tax')
+    total = fields.Float(string="Invoice Total")
+    refund_invoice_id = fields.Many2one('account.invoice', string="Refund Invoice", help="From where this Refund is created")
+    shipping_bill_number = fields.Char(string="Shipping Bill Number")
+    shipping_bill_date = fields.Date(string="Shipping Bill Date")
+    shipping_port_code_id = fields.Many2one('l10n_in.port.code', string='Shipping port code')
+    ecommerce_partner_id = fields.Many2one('res.partner', string="E-commerce")
+    invoice_type = fields.Selection([
+        ('out_invoice', 'Customer Invoice'),
+        ('in_invoice', 'Vendor Bill'),
+        ('out_refund', 'Customer Credit Note'),
+        ('in_refund', 'Vendor Credit Note'),
+        ])
+    partner_vat = fields.Char(string="Customer GSTIN")
+    ecommerce_vat = fields.Char(string="E-commerce GSTIN")
+    tax_rate = fields.Float(string="Rate")
+    place_of_supply = fields.Char(string="Place of Supply")
+    is_pre_gst = fields.Char(string="Is Pre GST")
+    is_ecommerce = fields.Char(string="Is E-commerce")
+    b2cl_is_ecommerce = fields.Char(string="B2CL Is E-commerce")
+    b2cs_is_ecommerce = fields.Char(string="B2CS Is E-commerce")
+    supply_type = fields.Char(string="Supply Type")
+    export_type = fields.Char(string="Export Type")  # String from GSTR column.
+    refund_export_type = fields.Char(string="UR Type")  # String from GSTR column.
+    b2b_type = fields.Char(string="B2B Invoice Type")
+    refund_invoice_type = fields.Char(string="Document Type")
+    gst_format_date = fields.Char(string="Formated Date")
+    gst_format_refund_date = fields.Char(string="Formated Refund Date")
+    gst_format_shipping_bill_date = fields.Char(string="Formated Shipping Bill Date")
+    tax_id = fields.Many2one('account.tax', string="Tax")
+
+    def _select(self):
+        select_str = """
+            SELECT min(sub.id) as id,
+            sub.invoice_id,
+            sub.account_move_id,
+            sub.name,
+            sub.state,
+            sub.partner_id,
+            sub.date,
+            sub.l10n_in_export_type,
+            sub.ecommerce_partner_id,
+            sub.shipping_bill_number,
+            sub.shipping_bill_date,
+            sub.shipping_port_code_id,
+            sub.total,
+            sub.journal_id,
+            sub.company_id,
+            sub.invoice_type,
+            sub.refund_invoice_id,
+            sub.partner_vat,
+            sub.ecommerce_vat,
+            sub.tax_rate as tax_rate,
+            (CASE WHEN count(sub.is_reverse_charge) > 0
+                THEN 'Y'
+                ELSE 'N'
+                END) AS is_reverse_charge,
+            sub.place_of_supply,
+            sub.is_pre_gst,
+            sub.is_ecommerce,
+            sub.b2cl_is_ecommerce,
+            sub.b2cs_is_ecommerce,
+            sub.supply_type,
+            sub.export_type,
+            sub.refund_export_type,
+            sub.b2b_type,
+            sub.refund_invoice_type,
+            sub.gst_format_date,
+            sub.gst_format_refund_date,
+            sub.gst_format_shipping_bill_date,
+            sum(sub.igst_amount) * sub.amount_sign AS igst_amount,
+            sum(sub.cgst_amount) * sub.amount_sign AS cgst_amount,
+            sum(sub.sgst_amount) * sub.amount_sign AS sgst_amount,
+            avg(sub.cess_amount) * sub.amount_sign AS cess_amount,
+            sum(sub.price_total) AS price_total,
+            sub.tax_id
+        """
+        return select_str
+
+    def _sub_select(self):
+        sub_select_str = """
+            SELECT aml.id AS id,
+                aml.invoice_id,
+                aml.partner_id,
+                am.id AS account_move_id,
+                am.name,
+                am.state,
+                am.date,
+                ai.l10n_in_export_type AS l10n_in_export_type,
+                ai.l10n_in_reseller_partner_id AS ecommerce_partner_id,
+                ai.l10n_in_shipping_bill_number AS shipping_bill_number,
+                ai.l10n_in_shipping_bill_date AS shipping_bill_date,
+                ai.l10n_in_shipping_port_code_id AS shipping_port_code_id,
+                am.amount AS total,
+                am.journal_id,
+                aj.company_id,
+                ai.type AS invoice_type,
+                ai.refund_invoice_id AS refund_invoice_id,
+                p.vat AS partner_vat,
+                CASE WHEN rp.vat IS NULL THEN '' ELSE rp.vat END AS ecommerce_vat,
+                (CASE WHEN at.l10n_in_reverse_charge = True
+                    THEN True
+                    ELSE NULL
+                    END)  AS is_reverse_charge,
+                (CASE WHEN ps.l10n_in_tin IS NOT NULL
+                    THEN concat(ps.l10n_in_tin,'-',ps.name)
+                    WHEN ps.id IS NULL and cps.l10n_in_tin IS NOT NULL
+                    THEN concat(cps.l10n_in_tin,'-',cps.name)
+                    ELSE ''
+                    END) AS place_of_supply,
+                (CASE WHEN ai.type in ('out_refund', 'in_refund') and refund_ai.date <= to_date('2017-07-01', 'YYYY-MM-DD')
+                    THEN 'Y'
+                    ELSE 'N'
+                    END) as is_pre_gst,
+
+                (CASE WHEN ai.l10n_in_reseller_partner_id IS NOT NULL
+                    THEN 'Y'
+                    ELSE 'N'
+                    END) as is_ecommerce,
+                (CASE WHEN ai.l10n_in_reseller_partner_id IS NOT NULL
+                    THEN 'Y'
+                    ELSE 'N'
+                    END) as b2cl_is_ecommerce,
+                (CASE WHEN ai.l10n_in_reseller_partner_id IS NOT NULL
+                    THEN 'E'
+                    ELSE 'OE'
+                    END) as b2cs_is_ecommerce,
+                (CASE WHEN ps.id = cp.state_id or p.id IS NULL
+                    THEN 'Intra State'
+                    WHEN ps.id != cp.state_id and p.id IS NOT NULL
+                    THEN 'Inter State'
+                    END) AS supply_type,
+                (CASE WHEN ai.l10n_in_export_type in ('deemed', 'export_with_igst', 'sez_with_igst')
+                    THEN 'EXPWP'
+                    WHEN ai.l10n_in_export_type in ('sale_from_bonded_wh', 'sez_without_igst')
+                    THEN 'EXPWOP'
+                    ELSE ''
+                    END) AS export_type,
+                (CASE WHEN refund_ai.l10n_in_export_type in ('deemed', 'export_with_igst', 'sez_with_igst')
+                    THEN 'EXPWP'
+                    WHEN refund_ai.l10n_in_export_type in ('sale_from_bonded_wh', 'sez_without_igst')
+                    THEN 'EXPWOP'
+                    ELSE 'B2CL'
+                    END) AS refund_export_type,
+                (CASE WHEN ai.l10n_in_export_type = 'regular'
+                    THEN 'Regular'
+                    WHEN ai.l10n_in_export_type = 'deemed'
+                    THEN 'Deemed'
+                    WHEN ai.l10n_in_export_type = 'sale_from_bonded_wh'
+                    THEN 'Sale from Bonded WH'
+                    WHEN ai.l10n_in_export_type = 'export_with_igst'
+                    THEN 'Export with IGST'
+                    WHEN ai.l10n_in_export_type = 'sez_with_igst'
+                    THEN 'SEZ with IGST payment'
+                    WHEN ai.l10n_in_export_type = 'sez_without_igst'
+                    THEN 'SEZ without IGST payment'
+                    END) AS b2b_type,
+                (CASE WHEN ai.type = 'out_refund'
+                    THEN 'C'
+                    WHEN ai.type = 'in_refund'
+                    THEN 'D'
+                    ELSE ''
+                    END) as refund_invoice_type,
+                (CASE WHEN am.date IS NOT NULL
+                    THEN TO_CHAR(am.date, 'DD-MON-YYYY')
+                    ELSE ''
+                    END) as gst_format_date,
+                (CASE WHEN refund_ai.date IS NOT NULL
+                    THEN TO_CHAR(refund_ai.date, 'DD-MON-YYYY')
+                    ELSE ''
+                    END) as gst_format_refund_date,
+                (CASE WHEN ai.l10n_in_shipping_bill_date IS NOT NULL
+                    THEN TO_CHAR(ai.l10n_in_shipping_bill_date, 'DD-MON-YYYY')
+                    ELSE ''
+                    END) as gst_format_shipping_bill_date,
+                CASE WHEN at.tax_group_id IN
+                    (SELECT res_id FROM ir_model_data WHERE module='l10n_in' AND name='igst_group')
+                    THEN aml.balance
+                    ELSE 0
+                    END AS igst_amount,
+                CASE WHEN at.tax_group_id IN
+                    (SELECT res_id FROM ir_model_data WHERE module='l10n_in' AND name='cgst_group')
+                    THEN aml.balance
+                    ELSE 0
+                    END AS cgst_amount,
+                CASE WHEN at.tax_group_id IN
+                    (SELECT res_id FROM ir_model_data WHERE module='l10n_in' AND name='sgst_group')
+                    THEN aml.balance
+                    ELSE 0
+                    END AS sgst_amount,
+                (SELECT sum(temp_aml.balance) from account_move_line temp_aml JOIN account_tax temp_at ON temp_at.id = temp_aml.tax_line_id where temp_aml.move_id = aml.move_id and temp_aml.product_id = aml.product_id
+                    and temp_at.tax_group_id IN (SELECT res_id FROM ir_model_data WHERE module='l10n_in' AND name='cess_group')
+                    ) AS cess_amount,
+                CASE WHEN at.tax_group_id IN
+                    (SELECT res_id FROM ir_model_data WHERE module='l10n_in' AND name='sgst_group') OR at.l10n_in_reverse_charge = True
+                    THEN NULL
+                    ELSE (CASE WHEN aml.tax_base_amount <> 0 THEN aml.tax_base_amount ELSE NULL END)
+                    END AS price_total,
+                (CASE WHEN aj.type = 'sale' AND (ai.type IS NULL OR ai.type != 'out_refund') THEN -1 ELSE 1 END) AS amount_sign,
+                (CASE WHEN atr.parent_tax IS NOT NULL THEN atr.parent_tax
+                    ELSE at.id END) AS tax_id,
+                (CASE WHEN atr.parent_tax IS NOT NULL THEN parent_at.amount
+                    ELSE at.amount END) AS tax_rate
+        """
+        return sub_select_str
+
+    def _from(self):
+        from_str = """
+            FROM account_move_line aml
+                JOIN account_move am ON am.id = aml.move_id
+                JOIN account_journal aj ON aj.id = am.journal_id
+                JOIN res_company c ON c.id = aj.company_id
+                JOIN account_tax at ON at.id = aml.tax_line_id
+                LEFT JOIN res_partner cp ON cp.id = c.partner_id
+                LEFT JOIN res_country_state cps ON cps.id = cp.state_id
+                LEFT JOIN account_invoice ai ON ai.id = aml.invoice_id
+                LEFT JOIN account_invoice refund_ai ON refund_ai.id = ai.refund_invoice_id
+                LEFT JOIN res_partner p ON p.id = aml.partner_id
+                LEFT JOIN res_country_state ps ON ps.id = p.state_id
+                LEFT JOIN res_partner rp ON rp.id = ai.l10n_in_reseller_partner_id
+                LEFT JOIN account_tax_filiation_rel atr ON atr.child_tax = at.id
+                LEFT JOIN account_tax parent_at ON parent_at.id = atr.parent_tax
+                """
+        return from_str
+
+    def _where(self):
+        return """
+                WHERE am.state = 'posted'
+                    AND at.tax_group_id in (SELECT res_id FROM ir_model_data WHERE module='l10n_in' AND name in ('cgst_group','sgst_group','igst_group','gst_group'))
+        """
+
+    def _group_by(self):
+        group_by_str = """
+        GROUP BY sub.invoice_id,
+            sub.account_move_id,
+            sub.name,
+            sub.state,
+            sub.partner_id,
+            sub.date,
+            sub.l10n_in_export_type,
+            sub.ecommerce_partner_id,
+            sub.shipping_bill_number,
+            sub.shipping_bill_date,
+            sub.shipping_port_code_id,
+            sub.total,
+            sub.journal_id,
+            sub.company_id,
+            sub.invoice_type,
+            sub.refund_invoice_id,
+            sub.partner_vat,
+            sub.ecommerce_vat,
+            sub.place_of_supply,
+            sub.is_pre_gst,
+            sub.is_ecommerce,
+            sub.b2cl_is_ecommerce,
+            sub.b2cs_is_ecommerce,
+            sub.supply_type,
+            sub.export_type,
+            sub.refund_export_type,
+            sub.b2b_type,
+            sub.refund_invoice_type,
+            sub.gst_format_date,
+            sub.gst_format_refund_date,
+            sub.gst_format_shipping_bill_date,
+            sub.amount_sign,
+            sub.tax_id,
+            sub.tax_rate
+        """
+        return group_by_str
+
+    @api.model_cr
+    def init(self):
+        tools.drop_view_if_exists(self.env.cr, self._table)
+        self.env.cr.execute("""CREATE or REPLACE VIEW %s AS (
+            %s
+            FROM (
+                %s %s %s
+            ) AS sub %s)""" % (self._table, self._select(), self._sub_select(),
+                self._from(), self._where(), self._group_by()))
