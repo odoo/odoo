849565fc0c115a62a094f692c231dc6081f70bf9
Tejas Shahu <tsh@odoo.com>
2018-06-22 15:24:56 +0530

    [IMP]sale: Testcase improvement

--- addons/sale/tests/test_sale_order.py
+++ addons/sale/tests/test_sale_order.py
@@ -185,7 +185,7 @@ class TestSaleOrder(TestCommonSaleNoChart):
         so._create_analytic_account()
 
         company = self.env.ref('base.main_company')
-        journal = self.env['account.journal'].create({'name': 'Purchase Journal - Test', 'code': 'STPJ', 'type': 'purchase', 'company_id': company.id})
+        journal = self.env['account.journal'].create({'name': 'Purchase Journal - Testing', 'code': 'STPJ', 'type': 'purchase', 'company_id': company.id})
         invoice_vals = {
             'name': '',
             'type': 'in_invoice',
