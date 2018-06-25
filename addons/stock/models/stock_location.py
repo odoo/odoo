2cc7bc245baec6b2ce90220583f29a63fd3a5410
Arnold Moyaux <arm@odoo.com>
2018-06-25 16:13:46 +0200

    [FIX] stock: be able to update stock module (demo data)
    Due to commit 6840196d0c68d3096a15c7152588f068a5fc3e6d when a user
    with demo data try to update stock it will return the location
    constraint because the demo data try to update the supplier location
    with the usage supplier. However there is no check if the usage is
    really modify or just in values.
    
    This commit add a check in order to only select locations with a new
    usage or that effectively change the 'scrap location' switch.

--- addons/stock/models/stock_location.py
+++ addons/stock/models/stock_location.py
@@ -84,8 +84,12 @@ class Location(models.Model):
             if self.mapped('quant_ids'):
                 raise UserError(_("This location's usage cannot be changed to view as it contains products."))
         if 'usage' in values or 'scrap_location' in values:
+            modified_locations = self.filtered(
+                lambda l: l.usage != values.get('usage') or
+                l.scrap_location != values.get('scrap_location')
+            )
             reserved_quantities = self.env['stock.move.line'].search_count([
-                ('location_id', 'in', self.ids),
+                ('location_id', 'in', modified_locations.ids),
                 ('product_qty', '>', 0),
             ])
             if reserved_quantities:
