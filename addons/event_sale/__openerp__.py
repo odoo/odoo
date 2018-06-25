30fed0aac3036b9c5360bc349f5971a2281727f4
Lucas Perais (lpe) <lpe@odoo.com>
2018-06-25 16:55:20 +0200

    [FIX] event, event_sale, website_event: allow portal access to own registrations
    OPW 1859364

--- addons/event_sale/__openerp__.py
+++ addons/event_sale/__openerp__.py
@@ -26,6 +26,7 @@ this event.
         'event_sale_data.xml',
         'report/event_event_templates.xml',
         'security/ir.model.access.csv',
+        'security/event_security.xml',
         'wizard/event_edit_registration.xml',
     ],
     'demo': ['event_demo.xml'],
