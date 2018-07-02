7e4aae321d7df928681733257a631e5559fe5ab5
Cedric Snauwaert <csn@openerp.com>
2018-07-02 17:11:00 +0200

    [FIX] account: fix select partner problem in reconciliation widget
    Problem: create a bank statement with a line that has a positive amount
    and a line that has a negative amount. Both line does not have a partner
    Then click on reconcile to open the reconciliation widget, notice the
    message stating that we have to select a partner or choose a counterpart.
    
    Select a partner on both line, behavior is different for the line with
    credit > 0, the message still says "select partner or choose counterpart"
    
    This commit fixes this problem.

--- addons/account/static/src/js/reconciliation/reconciliation_model.js
+++ addons/account/static/src/js/reconciliation/reconciliation_model.js
@@ -849,7 +849,7 @@ var StatementModel = BasicModel.extend({
                 }) : false,
                 account_code: self.accounts[line.st_line.open_balance_account_id],
             };
-            line.balance.type = line.balance.amount_currency ? (line.balance.amount_currency > 0 && line.st_line.partner_id ? 0 : -1) : 1;
+            line.balance.type = line.balance.amount_currency ? (line.st_line.partner_id ? 0 : -1) : 1;
         });
     },
     /**
