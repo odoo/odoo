c0794649f348f035f969e2969ebc68a2da213a59
William Henrotin <whe@odoo.com>
2018-07-03 14:46:03 +0200

    [IMP] stock: stockable term rename
    "Stockable" term is not a proper english word. We change it
    to"Storable"This commit is related to task ID 38933
    
    PR:22085
    TASK:38933

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -49,7 +49,7 @@ class ProductTemplate(models.Model):
     type = fields.Selection([
         ('consu', _('Consumable')),
         ('service', _('Service'))], string='Product Type', default='consu', required=True,
-        help='A stockable product is a product for which you manage stock. The "Inventory" app has to be installed.\n'
+        help='A storable product is a product for which you manage stock. The "Inventory" app has to be installed.\n'
              'A consumable product, on the other hand, is a product for which stock is not managed.\n'
              'A service is a non-material product you provide.\n'
              'A digital content is a non-material product you sell online. The files attached to the products are the one that are sold on '
