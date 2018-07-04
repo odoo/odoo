6d23800eaa5aec16bb2ad2f45169e0358b906aa6
Christophe Matthieu <chm@odoo.com>
2018-07-04 16:24:48 +0200

    [FIX] mail: add access_token for image from base64 and cid

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -1770,7 +1770,7 @@ class MailThread(models.AbstractModel):
                     if not attachment:
                         attachment = fname_mapping.get(node.get('data-filename'), '')
                     if attachment:
-                        node.set('src', '/web/image/%s' % attachment.id)
+                        node.set('src', '/web/image/%s?access_token=%s' % (attachment.id, attachment.access_token))
                         postprocessed = True
             if postprocessed:
                 body = lxml.html.tostring(root, pretty_print=False, encoding='UTF-8')
