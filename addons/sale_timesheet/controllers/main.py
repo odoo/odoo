937c2fc70841e39031100662db74d793ce68a8ea
jem-odoo <jem@openerp.com>
2018-07-06 15:16:44 +0200

    [FIX] sale_timesheet: remaining calculation in plan project
    The computation is badly implemented. Wrong
    data struture used at the wrong moment.
    This commit fixes it: remaing hour in overview
    is SOLD qty - DONE qty (sum of timesheets).
    
    Task #1857652

--- addons/sale_timesheet/controllers/main.py
+++ addons/sale_timesheet/controllers/main.py
@@ -197,9 +197,8 @@ class SaleTimesheetController(http.Controller):
 
         # remaining computation of SO row, as Sold - Done (timesheet total)
         for sale_order_id, done_sold_vals in rows_sale_order_done_sold.items():
-            item = done_sold_vals.get(sale_order_id)
-            if item:
-                rows_sale_order[sale_order_id] = item['sold'] - item['done']
+            if sale_order_id in rows_sale_order:
+                rows_sale_order[sale_order_id][-1] = done_sold_vals['sold'] - done_sold_vals['done']
 
         # group rows SO, SOL and their related employee rows.
         timesheet_forecast_table_rows = []
