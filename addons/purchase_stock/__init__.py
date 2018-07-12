202b0515c839b27333808b9300fb7fb97531ddfa
Arnold Moyaux <arm@odoo.com>
2018-07-12 13:13:53 +0200

    [FIX] purchase_stock: post init hook for warehouses rules

--- addons/purchase_stock/__init__.py
+++ addons/purchase_stock/__init__.py
@@ -3,3 +3,16 @@
 
 from . import models
 from . import report
+
+from odoo import api, SUPERUSER_ID
+
+
+def _create_buy_rules(cr, registry):
+    """ This hook is used to add a default buy_pull_id on every warehouse. It is
+    necessary if the purchase_stock module is installed after some warehouses
+    were already created.
+    """
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    warehouse_ids = env['stock.warehouse'].search([('buy_pull_id', '=', False)])
+    for warehouse_id in warehouse_ids:
+        warehouse_id.buy_pull_id = warehouse_id._create_buy_rule()
