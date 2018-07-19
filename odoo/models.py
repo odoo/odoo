a81950f84f7611467349847b78137fe9c44b834b
Manuel Vázquez Acosta <manuel@merchise.org>
2018-07-19 14:24:35 -0400

    models: Make _revert_method robust in ongoing tests.
    If an addon, while testing, patches and reverts a method ``Model.method``,
    another addon's modifications of the same method can be ignored.
    
    This is because, while the addon is running the model's classes hierarchy in
    the registry looks like:
    
           BaseModel
              /
             /
            M1
             \
              \
              Model
    
    So the _patch_method finds the definition (with `getattr`) in the MRO, which
    is actually in class M1.  But _revert_method, modifies the Model class.
    
    Afterwards, when the second addon gets installed in the registry, the Model
    class `__bases__` attribute is modified in place, and the hierarchy looks
    like:
    
            BaseModel
              /   \
             /     \
            M1     M2
             \     /
              \   /
              Model
    
    But since the patched method is already in the class Model, and it most likely
    won't call the ``super()``, the modification in M2 will be lost while testing.
    
    Since the Model at the bottom of the hierarchy should not have any methods,
    the proposed solution is to delete the attribute, and only set it back if the
    original method is not what we find.

--- odoo/models.py
+++ odoo/models.py
@@ -4770,7 +4770,9 @@ class BaseModel(object):
             See :meth:`~._patch_method`.
         """
         method = getattr(cls, name)
-        setattr(cls, name, method.origin)
+        delattr(cls, name)
+        if method.origin is not getattr(cls, name, None):
+            setattr(cls, name, method.origin)
 
     #
     # Instance creation
