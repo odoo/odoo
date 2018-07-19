38fffe95a5234559cd59df14ad264c86739bb0be
Jigar Patel <jat@openerp.com>
2018-07-19 14:16:58 +0530

    [IMP] website_form: Allow user to change contact form's recipient email address
    Before this commit,
    By default, company email address is taken as recipient email address in contact form
    and there is no other way to edit it without changing company email address in community edition.
    (User can change email using form builder but it's in enterprise).
    
    After this commit,
    New field in website setting to set recipient email address without changing company email address.
    And user also change it by customize option of form itself in frontend.

--- addons/website_form/models/models.py
+++ addons/website_form/models/models.py
@@ -5,10 +5,19 @@ from odoo import models, fields, api
 from odoo.http import request
 
 
+class MailMail(models.Model):
+    _inherit = 'mail.mail'
+
+    def website_form_input_filter(self, request, values):
+        values['email_to'] = values.get('email_to') or request.website.contact_form_recipient_email
+        return values
+
+
 class website_form_config(models.Model):
     _inherit = 'website'
 
     website_form_enable_metadata = fields.Boolean('Technical data on contact form', help="You can choose to log technical data like IP, User Agent ,...")
+    contact_form_recipient_email = fields.Char(default=lambda self: self.get_current_website().company_id.email)
 
     def _website_form_last_record(self):
         if request and request.session.form_builder_model_model:
@@ -19,6 +28,7 @@ class website_form_config(models.Model):
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
     website_form_enable_metadata = fields.Boolean(related="website_id.website_form_enable_metadata", readonly=False)
+    contact_form_recipient_email = fields.Char(related='website_id.contact_form_recipient_email')
 
 
 class website_form_model(models.Model):
---
+++ addons/website_form/static/src/js/website_form_editor.js
@@ -0,0 +1,48 @@
+odoo.define('website_form_editor', function (require) {
+'use strict';
+
+var core = require('web.core');
+var Dialog = require('web.Dialog');
+var options = require('web_editor.snippets.options');
+
+var QWeb = core.qweb;
+var _t = core._t;
+
+options.registry.default_contact_form_editor = options.Class.extend({
+    //--------------------------------------------------------------------------
+    // Options
+    //--------------------------------------------------------------------------
+
+    /**
+     * Open dialog to change recipient email address.
+     */
+    changeRecipientEmail: function () {
+        var self = this;
+        var email = this.$target.find('[name="email_to"]').val();
+        if (!email) {
+            var $valueNode = this.$target.nearest('[data-for]');
+            var values = JSON.parse($valueNode.data('values').replace(/'/g, '"'));
+            email = values['email_to'];
+        }
+        new Dialog(this, {
+            size: 'medium',
+            title: _t("Change Recipient Email"),
+            $content: QWeb.render('website_form.default_contact_form_editor', {email: email}),
+            buttons: [
+                {
+                    text: _t("Save"),
+                    classes: 'btn-primary',
+                    close: true,
+                    click: function () {
+                        self.$target.find('[name="email_to"]').attr('value', this.$('input').val().trim());
+                    },
+                }, {
+                    text: _t("Cancel"),
+                    close: true,
+                },
+            ],
+        }).open();
+    }
+});
+
+});
