0a13dde432d1eeedb80a82e7aa14d0363112639e
Laurent Smet <las@openerp.com>
2018-07-23 12:26:53 +0200

    [REF] account_reports, *: security enhancements in reports
    * get_report_values() is now private
    * account_reports: make private most of the methods to avoid any leak
    * account_reports: ensure the options dict cannot be forged to leak any multi-company info
    * [FIX] account_reports: prevent inconsistencies between options and context
    
    Was task: 1823127
    Was PR #2399

--- report/sale_coupon_report.py
+++ report/sale_coupon_report.py
@@ -8,7 +8,7 @@ class CouponReport(models.AbstractModel):
     _name = 'report.sale_coupon.report_coupon'
 
     @api.model
-    def get_report_values(self, docids, data=None):
+    def _get_report_values(self, docids, data=None):
         docs = self.env['sale.coupon'].browse(docids)
         return {
             'doc_ids': docs.ids,
