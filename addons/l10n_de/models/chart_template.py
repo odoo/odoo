3ff5fea9339d0adc8c2f0e672d3a2b7820862899
Cédric Snauwaert <csn@openerp.com>
2018-07-24 17:09:28 +0200

    [ADD] l10n_de: din5008 type B report
    Specific report template used in Germany. Add a new report layout as well as
    a new paperformat.
    
    Was part of task: 35289
    Was part of PR #25472

--- addons/l10n_de/models/chart_template.py
+++ addons/l10n_de/models/chart_template.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import api, models, _
+from odoo import api, fields, models
 
 
 class WizardMultiChartsAccounts(models.TransientModel):
@@ -12,3 +12,16 @@ class WizardMultiChartsAccounts(models.TransientModel):
         existing_tags = [x[-1:] for x in res.get('tag_ids', [])]
         res['tag_ids'] = [(6, 0, existing_tags + [xml_id])]
         return res
+
+    # Write paperformat and report template used on company
+    @api.model
+    def execute(self):
+        res = super(WizardMultiChartsAccounts, self).execute()
+        if self.company_id.country_id.code == 'DE':
+            self.company_id.write({'external_report_layout': 'din5008', 'paperformat_id': self.env.ref('l10n_de.paperformat_euro_din').id})
+        return res
+
+class Company(models.Model):
+    _inherit = 'res.company'
+
+    external_report_layout = fields.Selection(selection_add=[('din5008', 'Din 5008')])
