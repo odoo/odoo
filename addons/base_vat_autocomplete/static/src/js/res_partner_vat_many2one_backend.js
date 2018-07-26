442fbe11add9fc5f5d87476ef0ce4f26a612ad12
TWA <twa@odoo.com>
2018-07-26 17:25:49 +0200

    [WIP][IMP] base_vat_autocomplete: Rework with async load
    Adapted the VIES validation for different sources of autocomplete.
    Added Create and Edit auto-filled
    Updated widget names with the bridge widget
    Adapter Model to return formatted data

--- addons/base_vat_autocomplete/static/src/js/res_partner_vat_many2one_backend.js
+++ addons/base_vat_autocomplete/static/src/js/res_partner_vat_many2one_backend.js
@@ -1,102 +1,125 @@
 odoo.define('base.res_partner_vat_many2one_backend', function (require) {
-// The goal of this file is to define a custom widget specific for partners
-// many2one to add a new create button when a VAT number is entered, by
-// fetching default data from VIES, if no company with matching data is found
-// in the current database.
+    "use strict";
 
-"use strict";
-var fieldRegistry = require('web.field_registry');
-var FieldMany2One = require('web.relational_fields').FieldMany2One;
-var core = require('web.core');
+    // The goal of this file is to define a custom widget specific for partners
+    // many2one to add a new create button when a VAT number is entered, by
+    // fetching default data from VIES, if no company with matching data is found
+    // in the current database.
 
-var _t = core._t;
+    var PartnerFieldMany2One = require('web.res_partner_many2one').PartnerFieldMany2One;
+    var concurrency = require('web.concurrency');
+    var core = require('web.core');
 
-var PartnerVatFieldMany2One = FieldMany2One.extend({
+    var _t = core._t;
 
-    // TODO: maybe override _quickCreate
+    var PartnerVatFieldMany2One = PartnerFieldMany2One.include({
+        /**
+         * @override
+         */
+        init: function () {
+            this._super.apply(this, arguments);
+            this._viesDropPrevious = new concurrency.DropPrevious();
+            this._addAutocompleteSource(this._searchViesExtraChoices, 'Searching VIES-VAT...', 10, this._validateViesSearchTerm);
+        },
 
-    /**
-     * @private
-     * @override
-     */
-    _search: function (search_val, additionalContext) {
-        var self = this;
-        var def = $.Deferred();
+        /**
+         * Sanitize search value by removing all not alphanumeric
+         *
+         * @param search_value
+         * @returns {string}
+         * @private
+         */
+        _sanitizeVAT: function(search_value){
+          return search_value ? search_value.replace(/[^A-Za-z0-9]/g, '') : '';
+        },
 
-        var context = _.extend(additionalContext, { 'query_vies': true});
+        /**
+         * Check if searched value is possibly a VAT : 2 first chars = alpha + min 5 numbers
+         *
+         * @param search_val
+         * @returns {boolean}
+         * @private
+         */
+        _validateViesSearchTerm: function(search_val) {
+            var str = this._sanitizeVAT(search_val);
+            var regex = /^[A-Za-z]{2}\d{5,}$/g;
 
-        var parent = this._super.apply(this, search_val, context);
+            return str && regex.test(str);
+        },
 
-        // TODO also intercept the M2ODialog to give it the proper context
+        /**
+         * Search VIES-VAT API to validate VAT number.
+         * If VAT number, return autocomplete result to Create and Edit with data from VIES
+         *
+         * @param search_val
+         * @returns {Promise<Array>}
+         * @private
+         */
+        _searchViesExtraChoices: function (search_val) {
+            var self = this;
+            var choices = [];
 
-        // TODO maybe code this better
-
-        parent.then(function (values) {
-            self._searchVat(search_val).then(function (result) {
-                if (result[0] && result[1]) {
-
-                    var context = {
-                        default_city: result[1].city,
-                        default_country_id: result[1].country_id,
-                        default_is_company: true,
-                        default_name: result[0],
-                        default_street: result[1].street,
-                        default_vat: search_val,
-                        default_zip: result[1].zip,
-                    };
-
-                    var companySpan = $('<span />').text(result[0]).html();
-
-                    var create_enabled = self.can_create && !self.nodeOptions.no_create;
-                    var raw_result = _.map(result, function (x) { return x[1]; });
-
-                    if (create_enabled && !self.nodeOptions.no_quick_create &&
-                        search_val.length > 0 && !_.contains(raw_result, search_val)
-                    ) {
-                        values.splice(-2, 1, {
-                            label: _.str.sprintf(_t('Create "<strong>%s</strong>"'), companySpan),
-                            action: self._quickCreate.bind(self, search_val),
-                            classname: 'o_m2o_dropdown_option'
-                        });
-                    }
-                    if (create_enabled && !self.nodeOptions.no_create_edit) {
-                        var createAndEditAction = function () {
-                            
-                            return self._searchCreatePopup("form", false, context);
-                        };
-                        values.splice(-1, 1, {
-                            label: _.str.sprintf(_t('Create and Edit "<strong>%s</strong>"'), companySpan),
-                            action: createAndEditAction,
-                            classname: 'o_m2o_dropdown_option',
-                        });
-                    }
-                }
-                def.resolve(values);
+            choices.push({
+                label: _t('Create and Edit from VIES-VAT :'),
             });
-        });
 
-        return def;
-    },
+            return self._searchViesVat(search_val).then(function (company_data) {
+                if(company_data){
+                    choices.push({
+                        label: company_data.name + ' ' + company_data.vat,
+                        value: company_data.vat,
+                        action: function() {
+                            self._createPartnerFromVies.call(self, company_data);
+                        },
+                        classname: 'o_m2o_dropdown_option'
+                    });
+                }
+                return choices;
+            });
+        },
 
-    _searchVat: function (search_val) {
-        var def = $.Deferred();
-        var context = this.record.getContext(this.recordParams);
+        /**
+         * Remote call to request VIES-VAT API
+         *
+         * @param search_val
+         * @returns {*|Promise}
+         * @private
+         */
+        _searchViesVat: function (search_val) {
+            var def = this._rpc({
+                model: this.field.relation,
+                method: "vies_vat_search",
+                kwargs: {
+                    context: this.record.getContext(this.recordParams),
+                    search_val: search_val,
+                }
+            });
+            this._viesDropPrevious.add(def);
+            return def;
+        },
 
-        this._rpc({
-            model: this.field.relation,
-            method: "vat_search",
-            kwargs: {
-                context: context,
-                search_val: search_val,
-            }
-        }).then(function (result) {
-            def.resolve(result);
-        });
+        /**
+         * Create a popup form with auto-filled values from VIES
+         *
+         * @param data
+         * @returns {*}
+         * @private
+         */
+        _createPartnerFromVies: function (data) {
+            var self = this;
+            self.$('input').val('');
 
-        return def;
-    }
+            var context = {
+                'default_is_company': true
+            };
+            _.each(data, function (val, key) {
+                context['default_' + key] = val && val.id ? val.id : val;
+            });
 
-});
+            return self._searchCreatePopup("form", false, context);
+        },
 
-fieldRegistry.add('res_partner_vat_many2one', PartnerVatFieldMany2One);
+    });
+
+    return PartnerVatFieldMany2One;
 });
