5c3660cf0cde4ad1b67247f95e48e790ce17fa88
Christophe Simonis <chs@odoo.com>
2018-07-26 13:15:44 +0200

    [FIX] base_vat_autocomplete: correct typo introduced by previous forward-port

--- addons/base_vat_autocomplete/models/res_partner.py
+++ addons/base_vat_autocomplete/models/res_partner.py
@@ -72,7 +72,7 @@ class ResPartner(models.Model):
         if len(lines) == 1:
             lines = [x.strip() for x in lines[0].split('   ') if x]
 
-        vals = partner._split_street_with_params(', '.join(lines.pop(0).rsplit(' ', 1)), '%(street_name)s, %(street_number)s/%(street_number2)s')
+        vals = self._split_street_with_params(', '.join(lines.pop(0).rsplit(' ', 1)), '%(street_name)s, %(street_number)s/%(street_number2)s')
         partner_address.update(vals)
 
         if len(lines) > 0:
