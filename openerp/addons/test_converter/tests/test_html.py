e0c07803c85d7103e5b94eaf6c94865ecc192eef
Nicolas Lempereur <nle@odoo.com>
2018-07-27 12:48:48 +0200

    [FIX] base: use -U+FEFF instead of U+2011 in -num
    In 5e6bb6566 an improvement was done so a numerical fields like '-500'
    was not breaked up over two lines after the hyphen.
    
    But it seems in a very particular case of printing PDF in a given
    combination of condition:
    
    - printing over wkhtmltopdf which itself uses an old version of webkit
    - particular font (issue happen with Arial but not "Segoe UI")
    - particular version of windows (windows server 2012, not on windows 10)
    
    the - character at the front of a monetary, float or integer field would
    be displayed as | erroneously.
    
    The problem is probably that the font system in the old webkit with
    given windows will not find the code point in the given font, and
    doesn't fallback correctly.
    
    This commit replace using the "NON-BREAKING HYPHEN (U+2011)" by using
    the "ZERO WIDTH NO-BREAK SPACE (U+FEFF)" which is an invisible character
    with no width that prevent splitting at its location.
    
    As a note, an alternative to this character is "WORD JOINER (U+2060)"
    that may be preferred, but it presents exactly the same issue (with
    -|500 instead of -{WORD JOINER}500) in the same environment.
    
    mentioned in https://www.odoo.com/forum/1/question/118653
    opw-1867842
    fixes #17093
    fixes #25840
    closes #26019

--- openerp/addons/test_converter/tests/test_html.py
+++ openerp/addons/test_converter/tests/test_html.py
@@ -68,7 +68,7 @@ class TestFloatExport(TestBasicExport):
         converter = self.get_converter('float')
 
         value = converter(-42.0)
-        self.assertEqual(value, u"\u201142.0")
+        self.assertEqual(value, u"-\N{ZERO WIDTH NO-BREAK SPACE}42.0")
 
         value = converter(42.0100)
         self.assertEqual(value, "42.01")
@@ -129,7 +129,7 @@ class TestCurrencyExport(TestExport):
             '<span data-oe-model="{obj._model._name}" data-oe-id="{obj.id}" '
                   'data-oe-field="value" data-oe-type="monetary" '
                   'data-oe-expression="obj.value">'
-                      u'<span class="oe_currency_value">\u20110.12</span>'
+                      u'<span class="oe_currency_value">-\N{ZERO WIDTH NO-BREAK SPACE}0.12</span>'
                       u'\N{NO-BREAK SPACE}{symbol}</span>'.format(
                 obj=obj,
                 symbol=currency.symbol.encode('utf-8')
