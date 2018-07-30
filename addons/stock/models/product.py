a0610c7cedbb9c0c78cfe6078246af567dea3aef
Simon Lejeune <sle@openerp.com>
2018-07-30 14:28:50 +0200

    [REF] stock: expose a method to get the theoretical quantity
    - use the `gather` method instead of calling search with a custom domain
    - expose the method so that the barcode client action is able to call it
      when a line is added without being backed by an inventory line (yet).

--- addons/stock/models/product.py
+++ addons/stock/models/product.py
@@ -362,6 +362,23 @@ class Product(models.Model):
         action['context'] = {'default_product_id': self.id}
         return action
 
+    @api.model
+    def get_theoretical_quantity(self, product_id, location_id, lot_id=None, package_id=None, owner_id=None, to_uom=None):
+        product_id = self.env['product.product'].browse(product_id)
+        product_id.check_access_rights('read')
+        product_id.check_access_rule('read')
+
+        location_id = self.env['stock.location'].browse(location_id)
+        lot_id = self.env['stock.production.lot'].browse(lot_id)
+        package_id = self.env['stock.quant.package'].browse(package_id)
+        owner_id = self.env['res.partner'].browse(owner_id)
+        to_uom = self.env['uom.uom'].browse(to_uom)
+        quants = self.env['stock.quant']._gather(product_id, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=True)
+        theoretical_quantity = sum([quant.quantity for quant in quants])
+        if to_uom and product_id.uom_id != to_uom:
+            theoretical_quantity = product_id.uom_id._compute_quantity(theoretical_quantity, to_uom)
+        return theoretical_quantity
+
     def write(self, values):
         res = super(Product, self).write(values)
         if 'active' in values and not values['active']:
