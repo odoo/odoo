5752a8643a9f07fec66082930608848f48101cf6
RomainLibert <rli@odoo.com>
2018-07-30 15:44:25 +0200

    [FIX] sale*,pos_sale: edit sale_report to fix an issue in enterprise
    This issue was introduced with the following commit
    https://github.com/odoo/odoo/commit/a4d6bff197b894f70eaecf5aa4e2088e5b014780
    
    The problem is that during the task #58961 we changed the API to
    generate the sale_report, but forgot to include the
    website_sale_dashboard sale_report, hence when accessing we had a
    traceback since this file was using the old API.
    
    Task #1869500
    Closes #26070

--- addons/sale_stock/report/sale_report.py
+++ addons/sale_stock/report/sale_report.py
@@ -9,7 +9,7 @@ class SaleReport(models.Model):
 
     warehouse_id = fields.Many2one('stock.warehouse', 'Warehouse', readonly=True)
 
-    def _query(self, with_clause='', fields={}, groupby=''):
+    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         fields['warehouse_id'] = ", s.warehouse_id as warehouse_id"
         groupby += ', s.warehouse_id'
-        return super(SaleReport, self)._query(with_clause, fields, groupby)
+        return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
