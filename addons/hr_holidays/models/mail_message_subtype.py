cf10dfe9d361f56cd0bc9a6bd11ad49b90fe8d05
TWA <twa@odoo.com>
2018-08-02 10:14:05 +0200

    [IMP] hr_holidays: Correction auto create res_model

--- addons/hr_holidays/models/mail_message_subtype.py
+++ addons/hr_holidays/models/mail_message_subtype.py
@@ -12,43 +12,37 @@ class MailMessageSubtype(models.Model):
 
     def _get_department_subtype(self):
         return self.search([
-            ['res_model', '=', self.res_model],
+            ['res_model', '=', 'hr.department'],
             ['parent_id', '=', self.id],
         ])
 
     def _update_department_subtype(self):
         subtype = self._get_department_subtype()
-
         if subtype:
             super(MailMessageSubtype, subtype).write({
                 'name': self.name,
+                'default': self.default,
             })
         else:
             subtype = super(MailMessageSubtype, self).create({
                 'name': self.name,
-                'res_model': self.res_model,
+                'res_model': 'hr.department',
                 'default': self.default or False,
                 'parent_id': self.id,
                 'relation_field': 'department_id',
-                'sequence': 8,
             })
-
         return subtype
 
     @api.model
     def create(self, vals):
         result = super(MailMessageSubtype, self).create(vals)
-
         if result.res_model in ['hr.leave', 'hr.leave.allocation']:
             result._update_department_subtype()
-
         return result
 
     @api.multi
     def write(self, vals):
         result = super(MailMessageSubtype, self).write(vals)
-
         if self.res_model in ['hr.leave', 'hr.leave.allocation']:
             self._update_department_subtype()
-
         return result
