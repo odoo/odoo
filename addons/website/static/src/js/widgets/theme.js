22c4311e283af95eafe1ba85ee1234bb8102110e
qsm-odoo <qsm@odoo.com>
2018-08-03 15:54:17 +0200

    [IMP] *: make customize dialog generic and improve options
    * web, web_editor, website, website_theme_install, portal,
      theme_default, theme_bootswatch
    
    The purpose of this task is to make the customize dialog as generic as
    possible, that is theme-independant:
    
    1) The design is now totally generic (Odoo visuals)
    2) The XML definition is form-view like. This allows themes to extend
       the dialog without any risk of breaking the style and also allows to
       not care about lots of technical details.
    3) New options have been included. Those were themes options that are
       now generic and which themes can simply adapt without touching the
       customize modal (navbar colors, footer color, navbar layout, fonts,
       body background, ...).
    
    The color palette can now also be customized with user colors.
    
    Using sass functionnalities, color palettes and fonts integration is now
    a lot better.
    
    Thanks to @qha-odoo for the original design.
    
    task-31677

--- addons/website/static/src/js/widgets/theme.js
+++ addons/website/static/src/js/widgets/theme.js
@@ -3,8 +3,10 @@ odoo.define('website.theme', function (require) {
 
 var config = require('web.config');
 var core = require('web.core');
+var ColorpickerDialog = require('web.colorpicker');
 var Dialog = require('web.Dialog');
 var weContext = require('web_editor.context');
+var widgets = require('web_editor.widget');
 var websiteNavbarData = require('website.navbar');
 
 var _t = core._t;
@@ -12,18 +14,26 @@ var _t = core._t;
 var templateDef = null;
 
 var ThemeCustomizeDialog = Dialog.extend({
+    xmlDependencies: (Dialog.prototype.xmlDependencies || [])
+        .concat(['/website/static/src/xml/website.editor.xml']),
+
     template: 'website.theme_customize',
     events: {
         'change [data-xmlid], [data-enable], [data-disable]': '_onChange',
+        'click .checked [data-xmlid], .checked [data-enable], .checked [data-disable]': '_onChange',
+        'click .o_theme_customize_color': '_onColorClick',
     },
 
     /**
      * @constructor
      */
     init: function (parent, options) {
+        options = options || {};
         this._super(parent, _.extend({
             title: _t("Customize this theme"),
-        }, options || {}));
+        }, options));
+
+        this.defaultTab = options.tab || 0;
     },
     /**
      * @override
@@ -46,7 +56,19 @@ var ThemeCustomizeDialog = Dialog.extend({
     start: function () {
         var self = this;
 
+        this._generateDialogHTML();
         this.$modal.addClass('o_theme_customize_modal');
+
+        // Enable the first option tab or the given default tab
+        var $tabs = this.$('[data-toggle="tab"]');
+        this.opened().then(function () {
+            $tabs.eq(self.defaultTab).tab('show');
+        });
+
+        // Hide the tab navigation if only one tab
+        if ($tabs.length <= 1) {
+            $tabs.closest('.nav').addClass('d-none');
+        }
 
         this.$inputs = this.$('[data-xmlid], [data-enable], [data-disable]');
 
@@ -60,6 +82,129 @@ var ThemeCustomizeDialog = Dialog.extend({
     // Private
     //--------------------------------------------------------------------------
 
+    /**
+     * @private
+     */
+    _generateDialogHTML: function () {
+        var $contents = this.$el.children('content');
+        if ($contents.length === 0) {
+            return;
+        }
+
+        $contents.remove();
+        this.$el.append(core.qweb.render('website.theme_customize_modal_layout'));
+        var $navLinksContainer = this.$('.nav');
+        var $navContents = this.$('.tab-content');
+
+        _.each($contents, function (content) {
+            var $content = $(content);
+
+            var contentID = _.uniqueId('content-');
+
+            // Build the nav tab for the content
+            $navLinksContainer.append($('<li/>', {
+                class: 'nav-item mb-1',
+            }).append($('<a/>', {
+                href: '#' + contentID,
+                class: 'nav-link',
+                'data-toggle': 'tab',
+                text: $content.attr('string'),
+            })));
+
+            // Build the tab pane for the content
+            var $navContent = $(core.qweb.render('website.theme_customize_modal_content', {
+                id: contentID,
+                title: $content.attr('title'),
+            }));
+            $navContents.append($navContent);
+            var $optionsContainer = $navContent.find('.o_options_container');
+
+            // Process content items
+            _processItems($content.children(), $optionsContainer);
+        });
+
+        this.$('[title]').tooltip();
+
+        function _processItems($items, $container) {
+            var optionsName = _.uniqueId('option-');
+
+            _.each($items, function (item) {
+                var $item = $(item);
+
+                switch (item.tagName) {
+                    case 'OPT':
+                        var colorPalette = $item.data('colorPalette');
+                        var icon = $item.data('icon');
+
+                        // Build the options template
+                        var $multiChoiceLabel = $(core.qweb.render('website.theme_customize_modal_option', {
+                            name: optionsName,
+                            id: $item.attr('id') || _.uniqueId('o_theme_customize_input_id_'),
+
+                            string: $item.attr('string'),
+                            icon: icon,
+                            color: $item.data('color'),
+                            font: $item.data('font'),
+
+                            colorPalette: colorPalette,
+
+                            xmlid: $item.data('xmlid'),
+                            enable: $item.data('enable'),
+                            disable: $item.data('disable'),
+                            reload: $item.data('reload'),
+                        }));
+
+                        if ($container.hasClass('form-row')) {
+                            var $col = $('<div/>', {class: (icon ? 'col-4' : (colorPalette ? 'col-12' : 'col-6'))});
+                            $col.append($multiChoiceLabel);
+                            $container.append($col);
+                        } else {
+                            $container.append($multiChoiceLabel);
+                        }
+                        break;
+
+                    case 'MORE':
+                        var collapseID = _.uniqueId('collapse-');
+
+                        var $col = $('<div/>', {
+                            class: 'col-12',
+                        }).appendTo($container);
+
+                        var string = $item.attr('string');
+                        if (string) {
+                            var $button = $('<button/>', {
+                                'type': 'button',
+                                class: 'btn btn-primary d-block mx-auto mt-3 collapsed',
+                                'data-toggle': 'collapse',
+                                'data-target': "#" + collapseID,
+                                text: string,
+                            });
+                            $col.append($button);
+                        }
+
+                        var $collapse = $('<div/>',{
+                            id: collapseID,
+                            class: 'collapse form-row justify-content-between mt-3',
+                            'data-depends': $item.data('depends'),
+                        });
+                        $col.append($collapse);
+
+                        _processItems($item.children(), $collapse);
+                        break;
+
+                    case 'LIST':
+                        var $listContainer = $('<div/>', {class: 'py-1 px-2 o_theme_customize_option_list'});
+                        var $col = $('<div/>', {
+                            class: 'col-6 mt-2',
+                            'data-depends': $item.data('depends'),
+                        }).append($('<h6/>', {text: $item.attr('string')}), $listContainer);
+                        $container.append($col);
+                        _processItems($item.children(), $listContainer);
+                        break;
+                }
+            });
+        }
+    },
     /**
      * @private
      */
@@ -113,10 +258,51 @@ var ThemeCustomizeDialog = Dialog.extend({
         return xmlIDs;
     },
     /**
-     * @abstract
      * @private
      */
-    _processChange: function ($inputs, event) {},
+    _processChange: function ($inputs) {
+        var self = this;
+        this.$modal.addClass('o_theme_customize_loading');
+
+        var bodyCustomImageXMLID = 'option_custom_body_image';
+        var $inputBodyCustomImage = $inputs.filter('[data-xmlid*="website.' + bodyCustomImageXMLID + '"]');
+        if (!$inputBodyCustomImage.length) {
+            return $.when();
+        }
+
+        var def = $.Deferred();
+        var $image = $('<img/>');
+        var editor = new widgets.MediaDialog(this, {onlyImages: true, firstFilters: ['background']}, null, $image[0]);
+
+        editor.on('save', this, function (media) { // TODO use scss customization instead (like for user colors)
+            var src = $(media).attr('src');
+            self._rpc({
+                model: 'ir.model.data',
+                method: 'get_object_reference',
+                args: ['website', bodyCustomImageXMLID],
+            }).then(function (data) {
+                return self._rpc({
+                    model: 'ir.ui.view',
+                    method: 'save',
+                    args: [
+                        data[1],
+                        '#wrapwrap { background-image: url("' + src + '"); }',
+                        '//style',
+                        weContext.get(),
+                    ],
+                });
+            }).then(function () {
+                def.resolve();
+            });
+        });
+        editor.on('cancel', this, function () {
+            def.resolve();
+        });
+
+        editor.open();
+
+        return def;
+    },
     /**
      * @private
      */
@@ -144,18 +330,33 @@ var ThemeCustomizeDialog = Dialog.extend({
             }
             $set.prop('checked', checked).closest('label').toggleClass('checked', checked);
         });
+
+        // Mark the collapsed section as visible if their dependencies are met
+        var $collapsedElements = this.$('[data-depends]');
+        _.each($collapsedElements, function (collapsed) {
+            var $collapsed = $(collapsed);
+            var enabled = true;
+            if (self._getInputs($collapsed.data('depends')).not(':checked').length) {
+                enabled = false;
+            }
+
+            if ($collapsed.is('.collapse')) {
+                $collapsed.collapse(enabled ? 'show' : 'hide');
+            } else {
+                $collapsed.toggleClass('d-none', !enabled);
+            }
+        });
     },
     /**
      * @private
      */
     _updateStyle: function (enable, disable, reload) {
-        this.$modal.addClass('loading');
-
         if (reload || config.debug === 'assets') {
             window.location.href = $.param.querystring('/website/theme_customize_reload', {
                 href: window.location.href,
                 enable: enable.join(','),
                 disable: disable.join(','),
+                tab: this.$('.nav-link.active').parent().index(),
             });
             return $.Deferred();
         }
@@ -168,28 +369,32 @@ var ThemeCustomizeDialog = Dialog.extend({
                 disable: disable,
                 get_bundle: true,
             },
-        }).then(function (bundleHTML) {
-            var frontendLinkSelector = 'link[href*=".assets_frontend"]';
-            var $links = $(frontendLinkSelector);
-            var $newLinks = $(bundleHTML).filter(frontendLinkSelector);
+        }).then(function (bundles) {
+            var defs = _.map(bundles, function (bundleContent, bundleName) {
+                var linkSelector = 'link[href*="' + bundleName + '"]';
+                var $links = $(linkSelector);
+                var $newLinks = $(bundleContent).filter(linkSelector);
 
-            var linksLoaded = $.Deferred();
-            var nbLoaded = 0;
-            $newLinks.on('load', function (e) {
-                if (++nbLoaded >= $newLinks.length) {
-                    linksLoaded.resolve();
-                }
-            });
-            $newLinks.on('error', function (e) {
-                linksLoaded.reject();
-                window.location.hash = 'theme=true';
-                window.location.reload();
+                var linksLoaded = $.Deferred();
+                var nbLoaded = 0;
+                $newLinks.on('load', function (e) {
+                    if (++nbLoaded >= $newLinks.length) {
+                        linksLoaded.resolve();
+                    }
+                });
+                $newLinks.on('error', function (e) {
+                    linksLoaded.reject();
+                    window.location.hash = 'theme=true';
+                    window.location.reload();
+                });
+                $links.last().after($newLinks);
+                return linksLoaded.then(function () {
+                    $links.remove();
+                });
             });
 
-            $links.last().after($newLinks);
-            return linksLoaded.then(function () {
-                $links.remove();
-                self.$modal.removeClass('loading');
+            return $.when.apply($, defs).then(function () {
+                self.$modal.removeClass('o_theme_customize_loading');
             });
         });
     },
@@ -206,7 +411,7 @@ var ThemeCustomizeDialog = Dialog.extend({
         var self = this;
 
         // Checkout the option that changed
-        var $option = $(ev.target).find('input').addBack('input');
+        var $option = $(ev.currentTarget);
         var $options = $option;
         var checked = $option.is(':checked');
 
@@ -231,12 +436,79 @@ var ThemeCustomizeDialog = Dialog.extend({
         this._setActive();
 
         // Update the style according to the whole set of options
-        self._processChange($options, ev);
-        self._updateStyle(
-            self._getXMLIDs($enable),
-            self._getXMLIDs($disable),
-            $option.data('reload') && window.location.href.match(new RegExp($option.data('reload')))
-        );
+        self._processChange($options).then(function () {
+            self._updateStyle(
+                self._getXMLIDs($enable),
+                self._getXMLIDs($disable),
+                $option.data('reload') && window.location.href.match(new RegExp($option.data('reload')))
+            );
+        });
+    },
+    /**
+     * @private
+     * @param {Event} ev
+     */
+    _onColorClick: function (ev) {
+        var self = this;
+        var $color = $(ev.currentTarget);
+        var colorName = $color.data('color');
+        var colorType = $color.data('colorType');
+
+        var colorpicker = new ColorpickerDialog(this, {
+            defaultColor: $color.find('span').css('background-color'),
+        });
+        colorpicker.on('colorpicker:saved', this, function (ev) {
+            ev.stopPropagation();
+
+            // TODO improve to be more efficient
+            self._rpc({
+                route: '/web_editor/get_assets_editor_resources',
+                params: {
+                    key: 'website.layout',
+                    get_views: false,
+                    get_scss: true,
+                    bundles: false,
+                    bundles_restriction: [],
+                },
+            }).then(function (data) {
+                var files = data.scss[0][1];
+                var file = _.find(files, function (file) {
+                    switch (colorType) {
+                        case 'theme':
+                            return file.url === '/website/static/src/scss/options/colors/user_theme_color_palette.scss';
+                        case 'typo':
+                            return file.url === '/website/static/src/scss/options/colors/user_color_palette_typo.scss';
+                    }
+                    return file.url === '/website/static/src/scss/options/colors/user_color_palette_' + colorName + '.scss';
+                });
+
+                var colors = {};
+                colors[colorName] = ev.data.hex;
+                if (colorName === 'primary') {
+                    colors['secondary'] = 'null';
+                    colors['gamma'] = 'null';
+                    colors['delta'] = 'null';
+                    colors['epsilon'] = 'null';
+                }
+
+                var updatedFileContent = file.arch;
+                _.each(colors, function (colorValue, colorName) {
+                    updatedFileContent = updatedFileContent.replace(new RegExp(colorName + ': (?:null|#[a-fA-F0-9]{6}),'), colorName + ': ' + colorValue + ',');
+                });
+
+                return self._rpc({
+                    route: '/web_editor/save_scss',
+                    params: {
+                        url: file.url,
+                        bundle_xmlid: 'web.assets_common',
+                        content: updatedFileContent,
+                    },
+                });
+            }).then(function () {
+                self.$('#' + $color.closest('.o_theme_customize_color_previews').data('depends')).click();
+            });
+        });
+        colorpicker.open();
     },
 });
 
@@ -254,7 +526,8 @@ var ThemeCustomizeMenu = websiteNavbarData.WebsiteNavbarActionWidget.extend({
     start: function () {
         var def;
         if ((window.location.hash || '').indexOf('theme=true') > 0) {
-            def = this._openThemeCustomizeDialog();
+            var tab = window.location.hash.match(/tab=(\d+)/);
+            def = this._openThemeCustomizeDialog(tab ? tab[1] : false);
             window.location.hash = '';
         }
         return $.when(this._super.apply(this, arguments), def);
@@ -268,10 +541,11 @@ var ThemeCustomizeMenu = websiteNavbarData.WebsiteNavbarActionWidget.extend({
      * Instantiates and opens the theme customization dialog.
      *
      * @private
+     * @param {string} tab
      * @returns {Deferred}
      */
-    _openThemeCustomizeDialog: function () {
-        return new ThemeCustomizeDialog(this).open();
+    _openThemeCustomizeDialog: function (tab) {
+        return new ThemeCustomizeDialog(this, {tab: tab}).open();
     },
 });
 
