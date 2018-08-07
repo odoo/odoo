3aa282ffa1c3e046d6d38d0cfea7408162a25383
Lucas Perais (lpe) <lpe@odoo.com>
2018-08-07 17:19:03 +0200

    [FIX] hw_escpos: only feed 4 lines when cutting
    otherwise it might fail

--- addons/hw_escpos/escpos/escpos.py
+++ addons/hw_escpos/escpos/escpos.py
@@ -881,7 +881,7 @@ class Escpos:
         """ Cut paper """
         # Fix the size between last line and cut
         # TODO: handle this with a line feed
-        self._raw("\n\n\n\n\n\n")
+        self._raw("\n\n\n\n")
         if mode.upper() == "PART":
             self._raw(PAPER_PART_CUT)
         else: # DEFAULT MODE: FULL CUT
