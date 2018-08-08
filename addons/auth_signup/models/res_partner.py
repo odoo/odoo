7c825a952d357a9aeac4f38de57c7433cde999ce
Jigar A <jam@odoo.com>
2018-08-08 11:57:48 -0700

    [FIX] signup: allow non-admin to invite portal users

--- addons/auth_signup/models/res_partner.py
+++ addons/auth_signup/models/res_partner.py
@@ -108,6 +108,7 @@ class ResPartner(models.Model):
         allow_signup = self.env['ir.config_parameter'].sudo().get_param('auth_signup.allow_uninvited', 'False').lower() == 'true'
         for partner in self:
             if allow_signup and not partner.user_ids:
+                partner = partner.sudo()
                 partner.signup_prepare()
                 res[partner.id]['auth_signup_token'] = partner.signup_token
             elif partner.user_ids:
