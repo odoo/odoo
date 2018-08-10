85bfc739fb351a1f782a57e3b3a56f20f05016c9
Yannick Tivisse <yti@odoo.com>
2018-08-10 13:46:37 +0200

    [IMP] base: Don't set customer=True on new users
    Purpose
    =======
    
    Create a user, then the partner is shown in Customers.
    
    Most of the time, user is not a customer.

--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -426,6 +426,7 @@ class Users(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         users = super(Users, self).create(vals_list)
+        users.write({'customer': False})
         for user in users:
             user.partner_id.active = user.active
             if user.partner_id.company_id:
