c0968e68eda55df624fb94016d036a2af598b10c
Jeremy Kersten <jke@odoo.com>
2018-08-12 23:17:24 +0200

    [ADD] website_theme_install: try to make theme multi website compliant
    From now, when you install a theme, it load the data from xml to template
    table theme [ir.ui.view|ir.attachment|website.page|website.menu].
    
    Data are only copied from this template table into the real table when you
    choose a theme on a website; Making them website_specific and with a link
    to the original to allow futur update.
    
    A special case is done to create theme.ir.ui.view when you are installing
    a theme, even if you continue to use template tag to create quick view.
    
    Co-authored-by: Derie Romain <rde@odoo.com>
    Co-authored-by: Kersten Jérémy <jke@odoo.com>

--- addons/website/models/ir_ui_view.py
+++ addons/website/models/ir_ui_view.py
@@ -23,11 +23,12 @@ class View(models.Model):
     website_id = fields.Many2one('website', ondelete='cascade', string="Website")
     page_ids = fields.One2many('website.page', 'view_id')
     first_page_id = fields.Many2one('website.page', string='Website Page', help='First page linked to this view', compute='_compute_first_page_id')
-    theme_id = fields.Many2one('ir.module.module')
+    # theme_id = fields.Many2one('ir.module.module')
 
-    @api.one
+    @api.multi
     def _compute_first_page_id(self):
-        self.first_page_id = self.env['website.page'].search([('view_id', '=', self.id)], limit=1)
+        for view in self:
+            view.first_page_id = self.env['website.page'].search([('view_id', '=', view.id)], limit=1)
 
     @api.multi
     def write(self, vals):
@@ -38,11 +39,6 @@ class View(models.Model):
         if not self._context.get('no_cow'):
             current_website_id = self._context.get('website_id')
             for view in self:
-                currently_updating = self._context.get('install_mode_data', {}).get('module', '')
-                if 'theme_' in currently_updating:
-                    current_website_id = False
-                    view = view._get_theme_specific_view(currently_updating)
-
                 # if generic view in multi-website context
                 if current_website_id and not view.website_id:
                     new_website_specific_view = view.copy({'website_id': current_website_id})
@@ -81,30 +77,10 @@ class View(models.Model):
         self.clear_caches()
         return result
 
-    @api.multi
-    def _get_theme_specific_view(self, theme_name):
-        self.ensure_one()
-        view = self
-        module_being_updated = self.env['ir.module.module'].search([('name', '=', theme_name)])
-        xml_id = self.env['ir.model.data'].search([('model', '=', 'ir.ui.view'), ('res_id', '=', view.id)])
-        if xml_id and xml_id.module != theme_name:
-            _logger.info('%s is updating view %s (ID: %s)', theme_name, xml_id.complete_name, view.id)
-
-            # check if a previously copied view for this theme already exists
-            theme_specific_view = self.env['ir.ui.view'].search([('key', '=', view.key), ('theme_id', '=', module_being_updated.id)])
-            if theme_specific_view:
-                view = theme_specific_view
-                _logger.info('diverting write to %s (ID: %s)', view.name, view.id)
-            else:
-                view = view.copy({'theme_id': module_being_updated.id})
-                _logger.info('created new theme-specific view %s (ID: %s)', view.name, view.id)
-
-        return view
-
     def _create_website_specific_pages_for_view(self, new_view, website):
         for page in self.page_ids:
             # create new pages for this view
-            new_page = page.copy({
+            page.copy({
                 'view_id': new_view.id,
             })
 
@@ -184,9 +160,6 @@ class View(models.Model):
         # we will prefer inactive website-specific views over active generic ones
         if current_website:
             domain = [leaf for leaf in domain if 'active' not in leaf]
-            if current_website.theme_ids:
-                theme_views_domain = [('theme_id', 'in', current_website.theme_ids.ids)]
-                website_views_domain = expression.OR([website_views_domain, theme_views_domain])
 
         return expression.AND([website_views_domain, domain])
 
@@ -207,10 +180,7 @@ class View(models.Model):
     def get_view_id(self, xml_id):
         if 'website_id' in self._context and not isinstance(xml_id, pycompat.integer_types):
             current_website = self.env['website'].browse(self._context.get('website_id'))
-            key_domain = [('key', '=', xml_id)]
-            theme_views_domain = [('theme_id', 'in', current_website.theme_ids.ids)]
-            website_views_domain = [('theme_id', '=', False)] + current_website.website_domain()
-            domain = expression.AND([expression.OR([theme_views_domain, website_views_domain]), key_domain])
+            domain = ['&', ('key', '=', xml_id)] + current_website.website_domain()
 
             view = self.search(domain, order='website_id', limit=1)
             if not view:
--- addons/website_theme_install/models/res_config_settings.py
+++ addons/website_theme_install/models/res_config_settings.py
@@ -1,11 +1,11 @@
 # coding: utf-8
-from odoo import api, fields, models
+from odoo import models
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    def website_install_theme(self):
+    def install_theme_on_current_website(self):
         self.website_id._force()
         return {
             'type': 'ir.actions.act_url',
