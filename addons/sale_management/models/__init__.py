43e1457a5bdd796deebbd51e324d03aaea43ca03
Sébastien Theys <seb@odoo.com>
2018-08-13 11:19:03 +0200

    fix

--- addons/sale_management/models/__init__.py
+++ addons/sale_management/models/__init__.py
@@ -2,6 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import digest
 from . import res_config_settings
-from . import sale_order
-from . import sale_quote
 from . import res_users
+from . import sale_order
+from . import sale_order_template
