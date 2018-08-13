b4af54f9c23b65abe50dde3dfb397510ec2fcee4
jem-odoo <jem@openerp.com>
2018-08-13 15:21:57 +0200

    [IMP] sale_timesheet: billing time at employee rate
    When a project become billable, the employee can now timesheet
    on a same task, at different rate.
    The project should be linked to a SO, and the mapping between
    the employee and the SO line will be done using 2 mecanisms:
    
    1/ task rate: the SO line set on the task is chosen for any employee
    timesheeting on the task.
    
    2/ employee rate: a map exists on the project, matching employee
    with an SO line from the SO of the project. If the employee is not in
    the map, the fallback will be the SO from the task, or the one from
    the project.
    This makes the SO line on the task always visible.
    
    The billable type of a project depends on if the map is filled
    or not. There is 2 ways of making a project billable:
    - project is created from SO confirmation (depending of product
    configuration)
    - through the wizard "Create Sales Order": a SO will be created
    and the project will be associated to it.
    
    The subtask should follow the configuration of its parent
    project (and not parent task).
    
    Task #34703
    Closes #26111

--- addons/sale_timesheet/models/sale_order.py
+++ addons/sale_timesheet/models/sale_order.py
@@ -222,7 +222,7 @@ class SaleOrderLine(models.Model):
             'analytic_account_id': account.id,
             'partner_id': self.order_id.partner_id.id,
             'sale_line_id': self.id,
-            'billable_type': 'task_rate',
+            'sale_order_id': self.order_id.id,
         }
         if self.product_id.project_template_id:
             values['name'] = "%s - %s" % (values['name'], self.product_id.project_template_id.name)
@@ -254,7 +254,6 @@ class SaleOrderLine(models.Model):
             'sale_line_id': self.id,
             'company_id': self.company_id.id,
             'user_id': False,  # force non assigned task, as created as sudo()
-            'billable_type': 'task_rate',
         }
 
     @api.multi
