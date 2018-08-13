83b08fec2942fdf75709bf43eaa9f4e3ebbfbaab
Sébastien Theys <seb@odoo.com>
2018-08-13 11:25:14 +0200

    fixes

--- addons/sale_quotation_builder/controllers/portal.py
+++ addons/sale_quotation_builder/controllers/portal.py
@@ -11,4 +11,4 @@ class CustomerPortal(CustomerPortal):
     @http.route(["/sale_quotation_builder/template/<model('sale.order.template'):template>"], type='http', auth="user", website=True)
     def sale_quotation_builder_template_view(self, template, **post):
         values = {'template': template}
-        return request.render('sale_design.so_template', values)
+        return request.render('sale_quotation_builder.so_template', values)
