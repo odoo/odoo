9aaa32b6f62a95f58d11c17aefb4da4027371223
Yannick Tivisse <yti@odoo.com>
2018-08-16 14:24:39 +0200

    [IMP] base: Better implementation of 8166eae

--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -425,8 +425,7 @@ class Users(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        users = super(Users, self).create(vals_list)
-        users.write({'customer': False})
+        users = super(Users, self.with_context(default_customer=False)).create(vals_list)
         for user in users:
             user.partner_id.active = user.active
             if user.partner_id.company_id:
