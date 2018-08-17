8e11dfc8b524b75722b4546043cf66c45419c461
Ravi Gadhia <rga@odoo.com>
2018-08-17 18:13:19 +0530

    [ADD] test_base module and move all base test files to test_base module

---
+++ odoo/addons/test_base/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'test_base',
+    'category': 'Tests',
+    'description': """A module to test orm, api, expression features.""",
+    'depends': ['base'],
+    'version': '0.0.1',
+}
