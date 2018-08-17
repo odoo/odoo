7ac4fdf5b2d08ba1eb7a2574e8159f4f74e29655
Adrien Dieudonne <adr@odoo.com>
2018-08-17 15:34:04 +0200

    [FIX] web: graph_renderer: check graph data before rendering
    Before this commit, we had two different errors when data was
    unavailable:
    The first one is generated by Odoo: "No data to display, try to[...]"
    The second one is generated by nvd3: "No Data Available".
    
    We always want the same message.
    So now, we always make some checks before rendering
    the graph by calling _render.

--- addons/web/static/src/js/views/graph/graph_renderer.js
+++ addons/web/static/src/js/views/graph/graph_renderer.js
@@ -56,7 +56,7 @@ return AbstractRenderer.extend({
     on_attach_callback: function () {
         this._super.apply(this, arguments);
         this.isInDOM = true;
-        this._renderGraph();
+        this._render();
     },
     /**
      * @override
