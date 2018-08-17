3ff99a38e6384c783d937799b9f9ec1a81f86a1d
Aaron Bohy <aab@odoo.com>
2018-08-17 13:39:29 +0200

    [FIX] web: fix evaluation context of the domain selector
    The domain was instantiated without being given any evaluation context.
    As a result, the uid variable was not defined,
    crashing the js if present in a user-defined filter.
    
    opw 1866852

--- addons/web/static/src/js/fields/basic_fields.js
+++ addons/web/static/src/js/fields/basic_fields.js
@@ -2264,19 +2264,18 @@ var FieldDomain = AbstractField.extend({
 
         // Convert char value to array value
         var value = this.value || "[]";
-        var domain = Domain.prototype.stringToArray(value);
 
         // Create the domain selector or change the value of the current one...
         var def;
         if (!this.domainSelector) {
-            this.domainSelector = new DomainSelector(this, this._domainModel, domain, {
+            this.domainSelector = new DomainSelector(this, this._domainModel, value, {
                 readonly: this.mode === "readonly" || this.inDialog,
                 filters: this.fsFilters,
                 debugMode: session.debug,
             });
             def = this.domainSelector.prependTo(this.$el);
         } else {
-            def = this.domainSelector.setDomain(domain);
+            def = this.domainSelector.setDomain(value);
         }
         // ... then replace the other content (matched records, etc)
         return def.then(this._replaceContent.bind(this));
