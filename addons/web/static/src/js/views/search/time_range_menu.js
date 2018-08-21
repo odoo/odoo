004b6e8b1c8d2d91105594e24fcea548ee756f87
Mathieu Duckerts-Antoine <dam@odoo.com>
2018-08-21 14:39:49 +0200

    [FIX] web: fix time range menu button style in mobile mode
    The search view in mobile mode has been redesigned and looks a bit different
    from the search view in desktop mode. This commit adapts the time range menu
    appearance in mobile mode.

--- addons/web/static/src/js/views/search/time_range_menu.js
+++ addons/web/static/src/js/views/search/time_range_menu.js
@@ -1,6 +1,7 @@
 odoo.define('web.TimeRangeMenu', function (require) {
 "use strict";
 
+var config = require('web.config');
 var core = require('web.core');
 var Domain = require('web.Domain');
 var TimeRangeMenuOptions = require('web.TimeRangeMenuOptions');
@@ -26,6 +27,8 @@ var TimeRangeMenu = Widget.extend({
      */
     init: function(parent, fields, configuration) {
         var self = this;
+        this.isMobile = config.device.isMobile;
+        this.symbol = this.isMobile ? 'fa fa-chevron-right float-right mt4' : 'caret';
         this._super(parent);
         this.dateFields = [];
         _.each(fields, function (field, name) {
