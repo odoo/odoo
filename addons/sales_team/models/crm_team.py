12729c1352c19bd18e28598d7a5528175309bd14
Nicolas Martinelli <nim@odoo.com>
2018-08-21 13:54:08 +0200

    [FIX] sales_team: default team is active
    Check the default team is active. Moreover, make the return object type
    consistent.
    
    opw-1876966

--- addons/sales_team/models/crm_team.py
+++ addons/sales_team/models/crm_team.py
@@ -37,8 +37,8 @@ class CrmTeam(models.Model):
                 try:
                     default_team_id.check_access_rule('read')
                 except AccessError:
-                    return False
-                if self.env.context.get('default_type') != 'lead' or default_team_id.use_leads:
+                    return self.env['crm.team']
+                if self.env.context.get('default_type') != 'lead' or default_team_id.use_leads and default_team_id.active:
                     team_id = default_team_id
         return team_id
 
