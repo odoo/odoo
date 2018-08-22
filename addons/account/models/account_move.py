82ac26f87e26ff760332e005b247774c4e2e3393
Luis Ernesto Torres Macías <luis_t@vauxoo.com>
2018-08-22 03:53:29 -0500

    [FIX] account: compare datetime.date to datetime.date
    Cannot compare datetime.date to '0000-00-00' as it's a string, must be a datetime.date

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -1550,7 +1550,7 @@ class AccountPartialReconcile(models.Model):
                                 'partner_id': line.partner_id.id,
                             })
         if newly_created_move:
-            if move_date > (self.company_id.period_lock_date or '0000-00-00') and newly_created_move.date != move_date:
+            if move_date > (self.company_id.period_lock_date or date.min) and newly_created_move.date != move_date:
                 # The move date should be the maximum date between payment and invoice (in case
                 # of payment in advance). However, we should make sure the move date is not
                 # recorded before the period lock date as the tax statement for this period is
