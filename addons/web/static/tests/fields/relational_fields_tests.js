odoo/odoo

commit bbbe176e2694c8c1167b88d21708b4bbe1acb8af

[FIX] web: properly resize textarea when its value changes
Before this commit, a textarea would not resize when its value changed if the event was different than "input" or "focus".

This commit fixes it by also listening to the "change" event, and by triggering it at appropriate times.

PR: #26430
Task: 1869469

addons/web/static/src/js/fields/basic_fields.js
@@ -1066,7 +1066,17 @@ var FieldText = InputField.extend(TranslatableFieldMixin, {
         }
         return this._super();
     },
-
+    /**
+     * Override to force a resize of the textarea when its value has changed
+     *
+     * @override
+     */
+    reset: function () {
+        var self = this;
+        return $.when(this._super.apply(this, arguments)).then(function () {
+            self.$input.trigger('change');
+        });
+    },
     //--------------------------------------------------------------------------
     // Handlers
     //--------------------------------------------------------------------------

addons/account/static/src/js/section_and_note_fields_backend.js
@@ -130,6 +130,20 @@ var SectionAndNoteFieldText = InputField.extend(TranslatableFieldMixin, {
         }
         return this._super.apply(this, arguments);
     },
+     /**
+     * Override to force a resize of the textarea when its value has changed
+     *
+     * @override
+     */
+    reset: function () {
+        if (!this.isSection) {
+            var self = this;
+            return $.when(this._super.apply(this, arguments)).then(function () {
+                self.$input.trigger('change');
+            });
+        }
+        return this._super.apply(this, arguments);
+    },
 
     //--------------------------------------------------------------------------
     // Handlers

addons/web/static/src/js/core/dom.js
@@ -91,7 +91,8 @@ return {
         minHeight = (options && options.min_height) || 50;
 
         $fixedTextarea = $('<textarea disabled>', {
-            class: $textarea[0].className,});
+            class: $textarea[0].className,
+        });
 
         var direction = _t.database.parameters.direction === 'rtl' ? 'right' : 'left';
         $fixedTextarea.css({
@@ -111,7 +112,7 @@ return {
         resize();
         $textarea.data("auto_resize", true);
 
-        $textarea.on('input focus', resize);
+        $textarea.on('input focus change', resize);
         if (options.parent) {
             core.bus.on('DOM_updated', options.parent, resize);
             core.bus.on('view_shown', options.parent, resize);

addons/web/static/tests/fields/basic_fields_tests.js
@@ -1487,6 +1487,42 @@ QUnit.module('basic_fields', {
         list.destroy();
     });
 
+    QUnit.test('text fields in edit mode should resize on reset', function (assert) {
+        assert.expect(1);
+
+        this.data.partner.fields.foo.type = 'text';
+
+        this.data.partner.onchanges = {
+            bar: function (obj) {
+                obj.foo = 'a\nb\nc\nd\ne\nf';
+            },
+        };
+
+        var form = createView({
+            View: FormView,
+            model: 'partner',
+            data: this.data,
+            arch: '<form string="Partners">' +
+                    '<field name="bar"/>' +
+                    '<field name="foo"/>' +
+                '</form>',
+            res_id: 1,
+        });
+
+        // edit the form
+        // trigger a textarea reset (through onchange) by clicking the box
+        // then check there is no scroll bar
+        form.$buttons.find('.o_form_button_edit').click();
+
+        form.$('div[name="bar"] input').click();
+
+        var $textarea = form.$('textarea:first');
+        assert.strictEqual($textarea.innerHeight(), $textarea[0].scrollHeight,
+            "textarea should not have a scroll bar");
+
+        form.destroy();
+    });
+
     QUnit.test('text field translatable', function (assert) {
         assert.expect(3);
 

commit 7039ffb91df61c4e7292d3fae4c093b3536d44bc

[FIX] web: prevent handle from crashing with text field
bbbe176e2694c8c1167b88d21708b4bbe1acb8af introduced a bug where we tried to trigger an event on an element that didn't exist. Indeed there is no input if it's not in edit mode.

This commit fixes it.

PR: none
Task: none

addons/web/static/src/js/fields/basic_fields.js
@@ -1079,7 +1079,9 @@ var FieldText = InputField.extend(TranslatableFieldMixin, {
     reset: function () {
         var self = this;
         return $.when(this._super.apply(this, arguments)).then(function () {
-            self.$input.trigger('change');
+            if (self.mode === 'edit') {
+                self.$input.trigger('change');
+            }
         });
     },
     //--------------------------------------------------------------------------

addons/web/static/tests/fields/relational_fields_tests.js
@@ -9491,7 +9491,7 @@ QUnit.module('relational_fields', {
         form.destroy();
     });
 
-    QUnit.test('one2many with sequence field, fetch name_get from empty list', function (assert) {
+    QUnit.test('one2many with sequence field, fetch name_get from empty list, field text', function (assert) {
         // There was a bug where a RPC would fail because no route was set.
         // The scenario is:
         // - create a new parent model, which has a one2many
@@ -9501,6 +9501,11 @@ QUnit.module('relational_fields', {
         // - reorder the lines with the handle
         // -> This will call a resequence, which calls a name_get.
         // -> With the bug that would fail, if it's ok the test will pass.
+
+        // This test will also make sure lists with
+        // FieldText (turtle_description) can be reordered with a handle.
+        // More specifically this will trigger a reset on a FieldText
+        // while the field is not in editable mode.
         assert.expect(4);
 
         this.data.turtle.fields.turtle_int.default = 10;
@@ -9521,6 +9526,7 @@ QUnit.module('relational_fields', {
                             '<field name="turtle_int" widget="handle"/>' +
                             '<field name="turtle_foo"/>' +
                             '<field name="not_required_product_id"/>' +
+                            '<field name="turtle_description" widget="text"/>' +
                         '</tree>' +
                     '</field>' +
                 '</form>',