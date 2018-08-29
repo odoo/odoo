edae8b4a8730a403f80ac3148e96e9a579f2336c
Christophe Matthieu <chm@odoo.com>
2018-08-29 13:48:25 +0200

    [FIX] web: Fixed the printing of reports.
    A new exception has been added to detect inconsistent reports. However, many are not structured as expected. Thus, the change consists mainly of checking only when the print has to be separated in part to attach it to the respective documents.
    
    We now take the page tag instead of the article, because the article tag is in the layout, so to separate the file, the t-call had to be inside the t-foreach (for the separation) and so possibly read the assets several times. Since all templates have the first tag (in the t-foreach) tag page, the choice is more sensible.
    
    The javascripts files used in readonly were taken out of the editing assets.
    
    A turn was made but not activated, it works locally, but on the runbot, an error is triggered when generating assets after choosing the settings.

--- addons/web/static/src/js/report/report.editor.js
+++ addons/web/static/src/js/report/report.editor.js
@@ -14,7 +14,6 @@ var trusted_origin = utils.build_origin(trusted_protocol, trusted_host);
 
 // Patch the editor's behavior when it is launched inside an iframe.
 if (window.self !== window.top) {
-    $(document.body).addClass('o_in_iframe'); //  in order to apply css rules
 
     // And now we chain some deferred to `save` and `cancel` in order to inform
     // the report's client action that the actions are done.
@@ -60,29 +59,6 @@ if (window.self !== window.top) {
             }
         }
     }, false);
-
-    // Allow to send commands to the webclient when the editor is disabled.
-    if (window.location.search.indexOf('enable_editor') === -1) {
-        // `do_action` command
-        $('[res-id][res-model][view-type]')
-            .wrap('<a/>')
-                .attr('href', '#')
-            .on('click', function (ev) {
-                ev.preventDefault();
-                var action = {
-                    'type': 'ir.actions.act_window',
-                    'view_type': $(this).attr('view-type'),
-                    'view_mode': $(this).attr('view-mode') || $(this).attr('view-type'),
-                    'res_id': Number($(this).attr('res-id')),
-                    'res_model': $(this).attr('res-model'),
-                    'views': [[$(this).attr('view-id') || false, $(this).attr('view-type')]],
-                };
-                window.parent.postMessage({
-                    'message': 'report:do_action',
-                    'action': action,
-                }, trusted_origin);
-            });
-    }
 }
 
 options.registry.many2one.include({
