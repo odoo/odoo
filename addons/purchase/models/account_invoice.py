90d9d94f97505c724734491f0ce2c686bfb5436f
Nicolas Martinelli <nim@odoo.com>
2018-08-29 15:48:02 +0200

    [FIX] purchase: keep vendor reference
    Create a PO and assign any string to the Vendor Reference field. Confirm
    and create an invoice.
    
    The Vendor Reference field on the invoice is empty, while it is expected
    to be filled in.
    
    opw-1879379

--- addons/purchase/models/account_invoice.py
+++ addons/purchase/models/account_invoice.py
@@ -101,8 +101,8 @@ class AccountInvoice(models.Model):
     @api.onchange('invoice_line_ids')
     def _onchange_origin(self):
         purchase_ids = self.invoice_line_ids.mapped('purchase_id')
-        if purchase_ids:
-            self.origin = ', '.join(purchase_ids.mapped('name'))
+        self.origin = ', '.join(purchase_ids.mapped('name'))
+        self.reference = ', '.join(purchase_ids.filtered('partner_ref').mapped('partner_ref'))
 
     @api.onchange('partner_id', 'company_id')
     def _onchange_partner_id(self):
