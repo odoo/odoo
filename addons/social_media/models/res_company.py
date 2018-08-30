288bcccc8e2e6b7e700abdfe9c377151c04c8cff
Yenthe666 <yenthevg@gmail.com>
2018-08-30 20:07:59 +0200

    [FIX] website, social_media, mass_mailing: add default value for missing media
    + add instagram
    
    This commit closes #26644 and closes #26649

--- addons/social_media/models/res_company.py
+++ addons/social_media/models/res_company.py
@@ -13,3 +13,4 @@ class Company(models.Model):
     social_linkedin = fields.Char('LinkedIn Account')
     social_youtube = fields.Char('Youtube Account')
     social_googleplus = fields.Char('Google+ Account')
+    social_instagram = fields.Char('Instagram Account')
