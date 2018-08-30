b31436b876a922050d9756a604f83cc053c9f2f7
Jeremy Kersten <jke@odoo.com>
2018-08-30 16:27:46 +0200

    [IMP] website: res_config_settings remove payment acquirer link
    New idea according FGI request is:
    A settings with nothing to configure (just a link), is useless in the
    res_config_settings view.

--- addons/website_payment/__manifest__.py
+++ addons/website_payment/__manifest__.py
@@ -14,7 +14,6 @@ This is a bridge module which integrates payment acquirers with Website app.
         'portal',
     ],
     'data': [
-        'views/res_config_settings_views.xml',
         'views/payment_acquirer.xml',
     ],
     'auto_install': False,
