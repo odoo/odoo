4e7fcf3e07619145d8460217b91e85709db1500d
Christophe Monniez <moc@odoo.com>
2018-08-30 13:41:14 +0200

    [FIX] web: wait web client to be ready before clickEverywhere test
    When starting the clickEverywhere test with enterprise modules
    installed, it happens that Odoo web client is not yet fully loaded.
    In this situation, the test does not find any app to test.
    
    With this commit, an attribute is added to odoo to check if the web
    client is fully loaded.

--- addons/web/tests/test_click_everywhere.py
+++ addons/web/tests/test_click_everywhere.py
@@ -7,11 +7,11 @@ import odoo.tests
 class TestMenusAdmin(odoo.tests.HttpCase):
 
     def test_01_click_everywhere_as_admin(self):
-        self.browser_js("/web", "odoo.__DEBUG__.services['web.clickEverywhere']();", "'web.clickEverywhere' in odoo.__DEBUG__.services", login="admin", timeout=1800)
+        self.browser_js("/web", "odoo.__DEBUG__.services['web.clickEverywhere']();", "odoo.isReady === true", login="admin", timeout=1800)
 
 
 @odoo.tests.tagged('click_all', 'post_install', '-at_install', '-standard')
 class TestMenusDemo(odoo.tests.HttpCase):
 
     def test_01_click_everywhere_as_demo(self):
-        self.browser_js("/web", "odoo.__DEBUG__.services['web.clickEverywhere']();", "'web.clickEverywhere' in odoo.__DEBUG__.services", login="demo", timeout=1800)
+        self.browser_js("/web", "odoo.__DEBUG__.services['web.clickEverywhere']();", "odoo.isReady === true", login="demo", timeout=1800)
