7b844b9c6fb71df378ae0c7ac5c834e473cd888d
Goffin Simon <sig@odoo.com>
2018-08-30 15:05:55 +0200

    [FIX] delivery: Error message for unavailable delivery methods
    With a fixed price delivery method, the error message was not displayed when
    the address doesn't match.
    
    Inspired from function base_on_rule_rate_shipment
    
    The error message was not clear enough.
    
    opw:1879204

--- addons/delivery/models/delivery_carrier.py
+++ addons/delivery/models/delivery_carrier.py
@@ -199,6 +199,12 @@ class DeliveryCarrier(models.Model):
             carrier.product_id.list_price = carrier.fixed_price
 
     def fixed_rate_shipment(self, order):
+        carrier = self._match_address(order.partner_shipping_id)
+        if not carrier:
+            return {'success': False,
+                    'price': 0.0,
+                    'error_message': _('Error: this delivery method is not available for this address.'),
+                    'warning_message': False}
         price = self.fixed_price
         if self.company_id.currency_id.id != order.currency_id.id:
             price = self.env['res.currency']._compute(self.company_id.currency_id, order.currency_id, price)
