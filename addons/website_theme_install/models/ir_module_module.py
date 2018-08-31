f39b31930a83e12391cedf30d170cad71111271b
Jeremy Kersten <jke@odoo.com>
2018-08-31 14:55:43 +0200

    [FIX] website_theme_install: reload registry when installing new theme

--- addons/website_theme_install/models/ir_module_module.py
+++ addons/website_theme_install/models/ir_module_module.py
@@ -145,6 +145,7 @@ class IrModuleModule(models.Model):
 
     @api.multi
     def button_choose_theme(self):
+        self.ensure_one()
         website = self.env['website'].get_current_website()
 
         # Unload previous theme
@@ -157,6 +158,9 @@ class IrModuleModule(models.Model):
         next_action = False
         if self.state != 'installed':
             next_action = self.button_immediate_install()
+            # reload registry to check if 'theme.utils'._<theme_name>_post_copy exists e.g.
+            self.env.reset()
+            self = self.env()[self._name].browse(self.id)
 
         # Copy new theme from template table to real table
         self._copy_theme_on_website(website)
