9b5909b4d7214fe347f779d5b405284a6e348198
Alexandre Kühn <aku@odoo.com>
2018-08-31 10:32:46 +0200

    [REF] note: rename app 'Productivity' to 'Notes'
    To stay consistent with the systray activity menu, which adds "Notes".
    
    Task-ID 1877502

--- addons/note/__manifest__.py
+++ addons/note/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Productivity',
+    'name': 'Notes',
     'version': '1.0',
     'category': 'Tools',
     'description': "",
