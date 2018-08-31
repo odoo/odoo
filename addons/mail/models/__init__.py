e27340715785187467e3b3385fb4682e580122b7
Martin Geubelle <mge@odoo.com>
2018-08-31 15:55:09 +0200

    [FIX] mail: consider `activity` as a valid view type
    The recently introduced `activity` view (see rev. odoo/odoo@1de9496 has not
    received all the consideration it deserves as a real view type (no RNG, not in
    the valid view `type`, no doc, etc.). It was currently only working based on a
    `_get_default_activity_view` method.
    
    Even if the RNG is currently really basic, its grammar could evolve in the future.
    
    This was causing an issue when opening Studio with this view as a default view
    with an invalid type was created.

--- addons/mail/models/__init__.py
+++ addons/mail/models/__init__.py
@@ -19,6 +19,8 @@ from . import res_users
 from . import res_company
 from . import res_config_settings
 from . import update
+from . import ir_action_act_window
 from . import ir_actions
 from . import ir_autovacuum
 from . import ir_model
+from . import ir_ui_view
