72c82b34d82787217bae639b053e1e450b6e9e65
William Henrotin <whe@odoo.com>
2018-09-04 16:38:23 +0200

    [FIX] purchase_requisition: datetime convertion
    Purchase agreements create request for quotation with prefilled field.
    The issue is:
    date_planned (on RfQ) is filled from schedule_date (on PA). The first
    one is a datetime object. The second one is a date object.
    This commit makes a right conversion between datetime and date at the
    RfQ creation. This issue was introduced by the datetime refactoring
    (#24717)
    
    Task id: 1878136

--- addons/purchase_requisition/models/purchase_requisition.py
+++ addons/purchase_requisition/models/purchase_requisition.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from datetime import datetime, time
 
 from odoo import api, fields, models, _
 from odoo.addons import decimal_precision as dp
@@ -273,6 +274,10 @@ class PurchaseRequisitionLine(models.Model):
     def _prepare_purchase_order_line(self, name, product_qty=0.0, price_unit=0.0, taxes_ids=False):
         self.ensure_one()
         requisition = self.requisition_id
+        if requisition.schedule_date:
+            date_planned = datetime.combine(requisition.schedule_date, time.min)
+        else:
+            date_planned = datetime.now()
         return {
             'name': name,
             'product_id': self.product_id.id,
@@ -280,7 +285,7 @@ class PurchaseRequisitionLine(models.Model):
             'product_qty': product_qty,
             'price_unit': price_unit,
             'taxes_id': [(6, 0, taxes_ids)],
-            'date_planned': requisition.schedule_date or fields.Datetime.now(),
+            'date_planned': date_planned,
             'account_analytic_id': self.account_analytic_id.id,
             'analytic_tag_ids': self.analytic_tag_ids.ids,
             'move_dest_ids': self.move_dest_id and [(4, self.move_dest_id.id)] or []
