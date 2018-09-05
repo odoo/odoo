bc4ff7cfb617ba2440be0bcd78019484b401c595
Guenzn <guenter.selbert@sewisoft.de>
2018-09-05 13:39:53 +0200

    [IMP] compute sales_count much faster with sql and don't return something

--- addons/sale/models/product_product.py
+++ addons/sale/models/product_product.py
@@ -7,17 +7,20 @@ from odoo import api, fields, models
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    @api.multi
-    def _sales_count(self):
-        r = {}
-        domain = [
-            ('state', 'in', ['sale', 'done']),
-            ('product_id', 'in', self.ids),
-        ]
-        for group in self.env['sale.report'].read_group(domain, ['product_id', 'product_uom_qty'], ['product_id']):
-            r[group['product_id'][0]] = group['product_uom_qty']
-        for product in self:
-            product.sales_count = r.get(product.id, 0)
-        return r
-
     sales_count = fields.Integer(compute='_sales_count', string='# Sales')
+
+    def _sales_count(self):
+        self._cr.execute("""
+        SELECT  l.product_id,
+        sum(l.product_uom_qty / u.factor * u2.factor) AS product_uom_qty                    
+        FROM sale_order_line l
+           JOIN sale_order s ON l.order_id = s.id     
+           LEFT JOIN product_product p ON l.product_id = p.id
+           LEFT JOIN product_template t ON p.product_tmpl_id = t.id
+           LEFT JOIN product_uom u ON u.id = l.product_uom
+           LEFT JOIN product_uom u2 ON u2.id = t.uom_id
+        WHERE s.state in ('done', 'sale') and product_id in ({product_ids})
+        GROUP BY l.product_id""".format(product_ids=", ".join(str(i) for i in self.ids)))
+        product_qty = {r[0]: r[1] for r in self._cr.fetchall()}
+        for product in self:
+            product.sales_count = product_qty.get(product.id, 0)
