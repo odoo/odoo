4671328e975463274c1de859fe29a884d16363a5
Toufik Benjaa <tbe@odoo.com>
2018-09-05 20:36:16 +0200

    [FIX] sale_payment: create invoice using transaction's company
    - When creating an invoice created after a payment, we need to force the
      company to the transaction's company.
      This avoid to create move lines with different companies (Administrator's
      company because of the payment processing done by the admin and the
      sale order/transaction company).

--- addons/sale_payment/models/payment.py
+++ addons/sale_payment/models/payment.py
@@ -69,6 +69,9 @@ class PaymentTransaction(models.Model):
 
         if self.env['ir.config_parameter'].sudo().get_param('website_sale.automatic_invoice'):
             for trans in self.filtered(lambda t: t.sale_order_ids):
+                ctx_company = {'company_id': self.acquirer_id.company_id.id,
+                            'force_company': self.acquirer_id.company_id.id}
+                trans = trans.with_context(ctx_company)
                 trans.sale_order_ids._force_lines_to_invoice_policy_order()
                 invoices = trans.sale_order_ids.action_invoice_create()
                 trans.invoice_ids = [(6, 0, invoices)]
