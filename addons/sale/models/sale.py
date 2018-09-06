a7f938bdc250e11c689939fbfb06a692930aefe8
Nicolas Martinelli <nim@odoo.com>
2018-09-06 13:10:04 +0200

    [FIX] sale: prevent confirmation of SO
    Prevent the confirmation of a SO in state 'cancel' or 'done'.
    
    opw-1881333

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -547,10 +547,17 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_confirm(self):
+        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+            raise UserError(_(
+                'It is not allowed to confirm an order in the following states: %s'
+            ) % (', '.join(self._get_forbidden_state_confirm())))
         self._action_confirm()
         if self.env['ir.config_parameter'].sudo().get_param('sale.auto_done_setting'):
             self.action_done()
         return True
+
+    def _get_forbidden_state_confirm(self):
+        return {'done', 'cancel'}
 
     @api.multi
     def _create_analytic_account(self, prefix=None):
