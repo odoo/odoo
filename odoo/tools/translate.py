5020f41b9586502ad2edf790bc87af4726e3157d
Christophe Simonis <chs@odoo.com>
2018-09-07 17:24:10 +0200

    Revert "[FIX] tools: skip bad translations"
    This actually broke some valid use cases (catch by new translations tests)
    
    This reverts commit 428fbd0381385f9b7cec83a1a2b9240e192abc21.

--- odoo/tools/translate.py
+++ odoo/tools/translate.py
@@ -1101,14 +1101,10 @@ def trans_load_data(cr, fileobj, fileformat, lang, lang_name=None, verbose=True,
 
             # discard the target from the POT targets.
             src = dic['src']
-            target = pot_targets.get(src)
-            if not target or (dic['type'], dic['name'], dic['res_id']) not in target.targets:
-                _logger.info("Translation '%s' (%s, %s, %s) not found in reference pot, skipping",
-                    src[:60], dic['type'], dic['name'], dic['res_id'])
-                return
-
-            target.value = dic['value']
-            target.targets.discard((dic['type'], dic['name'], dic['type'] != 'code' and dic['res_id'] or 0))
+            if src in pot_targets:
+                target = pot_targets[src]
+                target.value = dic['value']
+                target.targets.discard((dic['type'], dic['name'], dic['type'] != 'code' and dic['res_id'] or 0))
 
             # This would skip terms that fail to specify a res_id
             res_id = dic['res_id']
