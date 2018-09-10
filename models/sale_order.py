9c0245c180faa9c47fba1144b118d5f4bdb954f0
Romain Derie <rde@odoo.com>
2018-09-10 18:18:46 +0200

    [FIX] (website_)sale_coupon: `_compute_tax_id` with tax on reward line
    It should get tax from the SOL and not the SO's product as the tax is applied
    on the SOL only (the reward product has no tax by default).

--- models/sale_order.py
+++ models/sale_order.py
@@ -362,3 +362,16 @@ class SaleOrderLine(models.Model):
                 line.order_id.code_promo_program_id -= related_program
                 related_program_lines |= line.order_id.order_line.filtered(lambda l: l.product_id.id == related_program.discount_line_product_id.id) - line
         return super(SaleOrderLine, self | related_program_lines).unlink()
+
+    @api.multi
+    def _compute_tax_id(self):
+        reward_lines = self.filtered('is_reward_line')
+        super(SaleOrderLine, self - reward_lines)._compute_tax_id()
+        # Discount reward line is split per tax, the discount is set on the line but not on the product
+        # as the product is the generic discount line.
+        # In case of a free product, retrieving the tax on the line instead of the product won't affect the behavior.
+        for line in reward_lines:
+            fpos = line.order_id.fiscal_position_id or line.order_id.partner_id.property_account_position_id
+            # If company_id is set, always filter taxes by the company
+            taxes = line.tax_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
+            line.tax_id = fpos.map_tax(taxes, line.product_id, line.order_id.partner_shipping_id) if fpos else taxes
