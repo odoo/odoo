009bf39af6431990b1fb0a179b8c752a984c62e7
Martin Trigaux <mat@odoo.com>
2018-09-11 16:58:32 +0200

    [REF] point_of_sale: clean dead code

--- addons/point_of_sale/static/src/js/models.js
+++ addons/point_of_sale/static/src/js/models.js
@@ -353,8 +353,7 @@ exports.PosModel = Backbone.Model.extend({
         },
     },{
         model:  'product.product',
-        // todo remove list_price in master, it is unused
-        fields: ['display_name', 'list_price', 'lst_price', 'standard_price', 'categ_id', 'pos_categ_id', 'taxes_id',
+        fields: ['display_name', 'lst_price', 'standard_price', 'categ_id', 'pos_categ_id', 'taxes_id',
                  'barcode', 'default_code', 'to_weight', 'uom_id', 'description_sale', 'description',
                  'product_tmpl_id','tracking'],
         order:  _.map(['sequence','default_code','name'], function (name) { return {name: name}; }),
