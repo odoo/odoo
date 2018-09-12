4ae2ffc094c263ba75b6e117bc2949bba4154a8e
svs-odoo <svs@odoo.com>
2018-09-12 12:11:24 +0200

    [FIX] web: avoid annoying xdg-open prompt
    Due to the route beginning with 'test: ', it was interpreted like a
    protocol and on some OS (ubuntu for example), a xdg-open prompt pops up.
    So, the window looses focus and some tests will fail because of that.

--- addons/web/static/tests/chrome/action_manager_tests.js
+++ addons/web/static/tests/chrome/action_manager_tests.js
@@ -1906,7 +1906,7 @@ QUnit.module('ActionManager', {
             start: function () {
                 var self = this;
                 return this._super.apply(this, arguments).then(function () {
-                    self.iframe.src = 'test: ' + self.iframe.getAttribute('src');
+                    self.iframe.src = 'test ' + self.iframe.getAttribute('src');
                 });
             }
         });
@@ -1921,7 +1921,7 @@ QUnit.module('ActionManager', {
                 if (route === '/report/check_wkhtmltopdf') {
                     return $.when('broken');
                 }
-                if (route === 'test: /report/html/some_report') {
+                if (route === 'test /report/html/some_report') {
                     return $.when();
                 }
                 return this._super.apply(this, arguments);
@@ -1946,7 +1946,7 @@ QUnit.module('ActionManager', {
             '/web/action/load',
             '/report/check_wkhtmltopdf',
             'notification',
-            'test: /report/html/some_report', // report client action's iframe
+            'test /report/html/some_report', // report client action's iframe
         ]);
 
         actionManager.destroy();
