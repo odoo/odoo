4a1ad6f5365707ad58b2a2387860f35386ad9eaf
Sébastien Theys <seb@odoo.com>
2018-09-12 15:13:01 +0200

    [IMP] various: rename electronic payment to online payment
    To be consistent with "online signature".
    
    PR: #26801
    task-1876864

--- addons/sale/models/res_config_settings.py
+++ addons/sale/models/res_config_settings.py
@@ -18,7 +18,7 @@ class ResConfigSettings(models.TransientModel):
     use_quotation_validity_days = fields.Boolean("Default Quotation Validity", config_parameter='sale.use_quotation_validity_days')
     group_warning_sale = fields.Boolean("Sale Order Warnings", implied_group='sale.group_warning_sale')
     portal_confirmation_sign = fields.Boolean(related='company_id.portal_confirmation_sign', string='Online Signature')
-    portal_confirmation_pay = fields.Boolean(related='company_id.portal_confirmation_pay', string='Electronic Payment')
+    portal_confirmation_pay = fields.Boolean(related='company_id.portal_confirmation_pay', string='Online Payment')
     group_sale_delivery_address = fields.Boolean("Customer Addresses", implied_group='sale.group_delivery_invoice_address')
     multi_sales_price = fields.Boolean("Multiple Sales Prices per Product")
     multi_sales_price_method = fields.Selection([
