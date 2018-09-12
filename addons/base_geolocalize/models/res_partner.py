2095d3f20f9fec103fba25f85d0629cc9d6f4b02
Emanuel Cino <ecino@compassion.ch>
2018-09-12 12:47:43 +0200

    [FIX] base_geolocalize allow to use Openstreetmap instead of Google
    - Change API provider to prevent invalid API calls
    - Move geo_find methods into base.geocoder object to ease inheritance
    - Add system parameter to allow use of another provider
    
    fixes #x26924
    fixes #x6929

--- addons/base_geolocalize/models/res_partner.py
+++ addons/base_geolocalize/models/res_partner.py
@@ -1,53 +1,4 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-import json
-import logging
-
-import requests
-
-from odoo import api, fields, models, tools, _
-from odoo.exceptions import UserError
-
-_logger = logging.getLogger(__name__)
-
-
-def geo_find(addr, apikey=False):
-    if not addr:
-        return None
-
-    if not apikey:
-        raise UserError(_('''API key for GeoCoding (Places) required.\n
-                          Save this key in System Parameters with key: google.api_key_geocode, value: <your api key>
-                          Visit https://developers.google.com/maps/documentation/geocoding/get-api-key for more information.
-                          '''))
-
-    url = "https://maps.googleapis.com/maps/api/geocode/json"
-    try:
-        result = requests.get(url, params={'sensor': 'false', 'address': addr, 'key': apikey}).json()
-    except Exception as e:
-        raise UserError(_('Cannot contact geolocation servers. Please make sure that your Internet connection is up and running (%s).') % e)
-
-    if result['status'] != 'OK':
-        if result.get('error_message'):
-            _logger.error(result['error_message'])
-        return None
-
-    try:
-        geo = result['results'][0]['geometry']['location']
-        return float(geo['lat']), float(geo['lng'])
-    except (KeyError, ValueError):
-        return None
-
-
-def geo_query_address(street=None, zip=None, city=None, state=None, country=None):
-    if country and ',' in country and (country.endswith(' of') or country.endswith(' of the')):
-        # put country qualifier in front, otherwise GMap gives wrong results,
-        # e.g. 'Congo, Democratic Republic of the' => 'Democratic Republic of the Congo'
-        country = '{1} {0}'.format(*country.split(',', 1))
-    return tools.ustr(', '.join(
-        field for field in [street, ("%s %s" % (zip or '', city or '')).strip(), state, country]
-        if field
-    ))
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -57,26 +8,25 @@ class ResPartner(models.Model):
     partner_longitude = fields.Float(string='Geo Longitude', digits=(16, 5))
     date_localization = fields.Date(string='Geolocation Date')
 
-    @classmethod
-    def _geo_localize(cls, apikey, street='', zip='', city='', state='', country=''):
-        search = geo_query_address(street=street, zip=zip, city=city, state=state, country=country)
-        result = geo_find(search, apikey)
-        if result is None:
-            search = geo_query_address(city=city, state=state, country=country)
-            result = geo_find(search, apikey)
-        return result
-
     @api.multi
     def geo_localize(self):
         # We need country names in English below
-        apikey = self.env['ir.config_parameter'].sudo().get_param('google.api_key_geocode')
+        geo_obj = self.env['base.geocoder']
         for partner in self.with_context(lang='en_US'):
-            result = partner._geo_localize(apikey,
-                                           partner.street,
-                                           partner.zip,
-                                           partner.city,
-                                           partner.state_id.name,
-                                           partner.country_id.name)
+            result = geo_obj.geo_find(geo_obj.geo_query_address(
+                street=partner.street,
+                zip=partner.zip,
+                city=partner.city,
+                state=partner.state_id.name,
+                country=partner.country_id.name
+            ))
+            if result is None:
+                result = geo_obj.geo_find(geo_obj.geo_query_address(
+                    city=partner.city,
+                    state=partner.state_id.name,
+                    country=partner.country_id.name
+                ))
+
             if result:
                 partner.write({
                     'partner_latitude': result[0],
--- addons/website_crm_partner_assign/models/crm_lead.py
+++ addons/website_crm_partner_assign/models/crm_lead.py
@@ -81,15 +81,26 @@ class CrmLead(models.Model):
                 'partner_longitude': longitude
             })
             return True
+        geo_obj = self.env['base.geocoder']
         # Don't pass context to browse()! We need country name in english below
         for lead in self:
             if lead.partner_latitude and lead.partner_longitude:
                 continue
             if lead.country_id:
-                apikey = self.env['ir.config_parameter'].sudo().get_param('google.api_key_geocode')
-                result = self.env['res.partner']._geo_localize(apikey,
-                                                               lead.street, lead.zip, lead.city,
-                                                               lead.state_id.name, lead.country_id.name)
+                result = geo_obj.geo_find(geo_obj.geo_query_address(
+                    street=lead.street,
+                    zip=lead.zip,
+                    city=lead.city,
+                    state=lead.state_id.name,
+                    country=lead.country_id.name))
+
+                if result is None:
+                    result = geo_obj.geo_find(geo_obj.geo_query_address(
+                        city=lead.city,
+                        state=lead.state_id.name,
+                        country=lead.country_id.name
+                    ))
+
                 if result:
                     lead.write({
                         'partner_latitude': result[0],
