0dc7598eba33f8d29ef7e98e0d26606f7c2bee06
Aaron Bohy <aab@odoo.com>
2018-09-13 14:03:59 +0200

    [FIX] *: adapt calls to _is_superuser
    Rev. 97e13d1 added a second admin user (id 2), the 'human' one, on
    top of the 'technical' one (id 1, also known as the superuser).
    Basically, all calls to _is_superuser() should have been changed to
    _is_admin(). They weren't. As a consequence, some features that
    were previously available for the admin weren't anymore (e.g.
    tours).
    
    This rev. replaces all calls to _is_superuser() by calls to
    _is_admin().

--- addons/web_tour/models/ir_http.py
+++ addons/web_tour/models/ir_http.py
@@ -10,6 +10,6 @@ class Http(models.AbstractModel):
 
     def session_info(self):
         result = super(Http, self).session_info()
-        if result['is_superuser']:
+        if result['is_admin']:
             result['web_tours'] = request.env['web_tour.tour'].get_consumed_tours()
         return result
