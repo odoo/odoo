4a57d2619bc4f625eed0929ce90d44a1d9494aa2
Jeremy Kersten <jke@odoo.com>
2018-09-13 12:32:05 +0200

    [FIX] website[_theme_install]: fix website form view
    Before this commit, you had 'choose a theme' into the form view.
    It should be only visible in modal
    
    task-1878245

--- addons/website_theme_install/models/res_config_settings.py
+++ addons/website_theme_install/models/res_config_settings.py
@@ -9,3 +9,8 @@ class ResConfigSettings(models.TransientModel):
         self.website_id._force()
         action = self.env.ref('website_theme_install.theme_install_kanban_action')
         return action.read()[0]
+
+    def action_website_create_new(self):
+        res = super(ResConfigSettings, self).action_website_create_new()
+        res['view_id'] = self.env.ref('website_theme_install.view_website_form_view_themes_modal').id
+        return res
