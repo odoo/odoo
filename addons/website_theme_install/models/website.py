46557360d4293e8bf704f8b02195ce536464fa3b
Jeremy Kersten <jke@odoo.com>
2018-09-13 08:49:44 +0200

    [IMP] website(_theme_install): keep breadcrumb when you choose theme
    Before this commit, we return an act_url, in this case the breadcrumb is not
    build and the user cannot retruen to the settings in one click.
    
    task-1878245

--- addons/website_theme_install/models/website.py
+++ addons/website_theme_install/models/website.py
@@ -11,8 +11,5 @@ class Website(models.Model):
     @api.model
     def create_and_redirect_to_theme(self, vals):
         self.browse(vals)._force()
-        return {
-            'type': 'ir.actions.act_url',
-            'url': '/web#action=website_theme_install.theme_install_kanban_action',
-            'target': 'self',
-        }
+        action = self.env.ref('website_theme_install.theme_install_kanban_action')
+        return action.read()[0]
