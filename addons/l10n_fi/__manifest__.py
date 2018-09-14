919372e478f34389ba4b4536304c2dc4f6235ef1
Miku Laitinen <miku.laitinen@gmail.com>
2018-09-14 20:07:19 +0300

    [ADD] Added Finnish localization modules

---
+++ addons/l10n_fi/__manifest__.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) RockIT Oy 2014-
+# Copyright (C) Avoin.Systems 2014-
+# Authors:
+#   Miku Laitinen (Avoin.Systems)
+#   Mikko Närjänen (Avoin.Systems)
+#   Toni Pesola (Avoin.Systems)
+#   Mikko Salmela (RockIT Oy)
+
+{
+    "name": "Finland - Accounting",
+    "category": "Localization",
+    "version": "1.0.0",
+    "author": "RockIt Oy & Avoin.Systems",
+    "depends": [
+        "account",
+        "base_vat",
+        "base_iban",
+        "l10n_multilang",
+    ],
+    "data": [
+        "data/account_account_tag_data.xml",
+    ],
+}
