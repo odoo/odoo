59ff50cb95298fc467ae6e2a2c3184136b4dd686
Raphael Collet <rco@odoo.com>
2018-09-14 11:15:05 +0200

    [FIX] product: partner property_pricelist load in batch for pos
    Reimplement the compute method of the field to make it work in batch.

--- addons/product/models/res_partner.py
+++ addons/product/models/res_partner.py
@@ -8,17 +8,18 @@ class Partner(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
 
+    # NOT A REAL PROPERTY !!!!
     property_product_pricelist = fields.Many2one(
         'product.pricelist', 'Sale Pricelist', compute='_compute_product_pricelist',
-        inverse="_inverse_product_pricelist", company_dependent=False,  # NOT A REAL PROPERTY
+        inverse="_inverse_product_pricelist", company_dependent=False,
         help="This pricelist will be used, instead of the default one, for sales to the current partner")
 
     @api.multi
     @api.depends('country_id')
     def _compute_product_pricelist(self):
+        res = self.env['product.pricelist']._get_partner_pricelist_multi(self.ids)
         for p in self:
-            if not isinstance(p.id, models.NewId):  # if not onchange
-                p.property_product_pricelist = self.env['product.pricelist']._get_partner_pricelist(p.id)
+            p.property_product_pricelist = res.get(p.id)
 
     @api.one
     def _inverse_product_pricelist(self):
