65a20819447536d07c0a5287a60b7526a428400e
Antoine Guenet (AGE) <age@odoo.com>
2018-09-17 09:15:37 +0200

    temp: start writing new tour

--- addons/project/static/src/js/tour_portal.js
+++ addons/project/static/src/js/tour_portal.js
@@ -3,7 +3,8 @@ odoo.define('project.portal_tours', function (require) {
     
     var tour = require('web_tour.tour');
     
-    tour.register('portal_project_tour', {
+    // ADMIN
+    tour.register('portal_project_tour_admin', {
         test: true,
         url: '/my/project/2',
     },
@@ -34,6 +35,61 @@ odoo.define('project.portal_tours', function (require) {
             }, {
                 content: "Change that task's stage",
                 trigger: 'iframe button:contains("To Do")',
+            }, {
+                content: "Open the chatter composer",
+                trigger: 'iframe button:contains("Send message")',
+            }, {
+                content: "Once the composer is open, send a message on the chatter",
+                trigger: 'iframe .o_composer_input textarea:first',
+                extra_trigger: 'iframe .o_thread_composer',
+                run: function (actions) {
+                    actions.text("Test message");
+                    actions.auto('iframe .o_composer_send button:contains("Send")');
+                },
+            }, {
+                content: "Check that the chatter contains the message",
+                trigger: 'iframe .o_thread_message_content:contains("Test message")',
+            }, {
+                content: "Click on the project's name from within the form",
+                trigger: 'iframe .o_group a[href*="/my/project"]:contains("Office Design"):first',
+            }, {
+                content: "Once the Kanban view is open again, check that the breadcrumb shows the name of the project",
+                extra_trigger: 'iframe .o_kanban_view',
+                trigger: '.breadcrumb-item:contains("Office Design")',
+            },
+        ],
+    );
+
+    // UNLOGGED, ACCESS TOKEN
+    tour.register('portal_project_tour_unlogged_accesstoken', {
+        test: true,
+        url: '/web',
+    },
+        [
+            {
+                content: "Open the Project module",
+                trigger: '.o_app[data-menu-xmlid="project.menu_main_pm"], .oe_menu_toggler[data-menu-xmlid="project.menu_main_pm"]',
+            }, {
+                content: "Open the edit form for the first project ('Office Design')",
+                trigger: '.o_kanban_record:first .o_kanban_manage_toggle_button',
+                run: function (actions) {
+                    actions.auto('.oe_kanban_action.oe_kanban_action_a:contains("Edit"):first');
+                },
+            }, {
+                content: "Copy the Public Link",
+                trigger: '.o_btn_char_copy',
+            },
+            {
+                content: "Check that the breadcrumb shows the name of the project",
+                trigger: '.breadcrumb-item:contains("Office Design")',
+            }, {
+                content: "Once the iframe is loaded, open a task's form view",
+                extra_trigger: 'iframe .o_view_controller',
+                trigger: 'iframe .o_kanban_record:contains("Meeting Room Furnitures"):first',
+            }, {
+                content: "Check that the selected task was opened",
+                extra_trigger: 'iframe .o_form_view',
+                trigger: 'iframe .o_task_name:contains("Meeting Room Furnitures")',
             }, {
                 content: "Open the chatter composer",
                 trigger: 'iframe button:contains("Send message")',
--- addons/project/tests/test_project_ui.py
+++ addons/project/tests/test_project_ui.py
@@ -9,7 +9,12 @@ class TestUi(odoo.tests.HttpCase):
     def test_01_project_tour(self):
         self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('project_tour')", "odoo.__DEBUG__.services['web_tour.tour'].tours.project_tour.ready", login="admin")
     
-    def test_02_project_portal_tour(self):
-        self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('portal_project_tour')",
-                        "odoo.__DEBUG__.services['web_tour.tour'].tours.portal_project_tour.ready",
+    def test_02_project_portal_tour_admin(self):
+        self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('portal_project_tour_admin')",
+                        "odoo.__DEBUG__.services['web_tour.tour'].tours.portal_project_tour_admin.ready",
                         login="admin", timeout=180)
+    
+    # def test_03_project_portal_tour_unlogged_accesstoken(self):
+    #     self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('portal_project_tour_unlogged_accesstoken')",
+    #                     "odoo.__DEBUG__.services['web_tour.tour'].tours.portal_project_tour_unlogged_accesstoken.ready",
+    #                     login=None, timeout=180)
