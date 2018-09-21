f517d9a9a0e2c80201ec0e7f2d8a10d79d337f15
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2018-09-21 13:41:07 +0200

    [IMP] stock_dropshipping: linking with SO to keep PO lines in sync
    The route drop shipping is not yet smartly linked with the PO triggered
    to do the direct route to the customer, especially when you change
    quantities on the SO after confirming it. It will result in wrong
    quantities in the PO lines which is a no go.

--- addons/stock_dropshipping/models/purchase.py
+++ addons/stock_dropshipping/models/purchase.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, fields
+from odoo import api, models, fields, _
+from odoo.exceptions import UserError
 
 
 class PurchaseOrderLine(models.Model):
@@ -16,6 +17,39 @@ class PurchaseOrderLine(models.Model):
             re['sale_line_id'] = self.sale_line_id.id
         return res
 
+    def _merge_in_existing_line(self, product_id, product_qty, product_uom, location_id, name, origin, values):
+        """ This function purpose is to be override with the purpose to forbide _run_buy  method
+        to merge a new po line in an existing one.
+        """
+        if values.get('sale_line_id') and self.sale_line_id and self.sale_line_id.id != values.get('sale_line_id'):
+            return False
+        return super(PurchaseOrderLine, self)._merge_in_existing_line(product_id, product_qty, product_uom, location_id, name, origin, values)
+
+    @api.multi
+    def unlink(self):
+        is_dropshipping = self.mapped('order_id').mapped('picking_type_id') == self.env.ref('stock_dropshipping.picking_type_dropship')
+        for line in self:
+            if line.sale_line_id and is_dropshipping:
+                raise UserError(
+                    _('Purchase Order Line %s (Qty: %s) in %s was created through the route dropshipping. '
+                      'As deleting this line might create inconsistencies, please adapt changes for quantities through the correspondent line in Sale Order %s' % (line.name, line.product_qty, line.order_id.name, line.sale_line_id.order_id.name))
+                )
+        return super(PurchaseOrderLine, self).unlink()
+
+    @api.multi
+    def write(self, vals):
+        if 'product_qty' in vals:
+            is_dropshipping = self.mapped('order_id').mapped('picking_type_id') == self.env.ref('stock_dropshipping.picking_type_dropship')
+            for line in self:
+                if line.sale_line_id and is_dropshipping and vals.get('product_qty') != line.sale_line_id.product_uom_qty:
+                    raise UserError(
+                        _('Purchase Order Line %s (Qty: %s) in %s was created through the route dropshipping. '
+                          'Please adapt changes for quantities through the correspondent line in Sale Order %s' % (line.name, line.product_qty, line.order_id.name, line.sale_line_id.order_id.name))
+                    )
+
+        res = super(PurchaseOrderLine, self).write(vals)
+        return res
+
 
 class ProcurementRule(models.Model):
     _inherit = 'procurement.rule'
@@ -26,3 +60,27 @@ class ProcurementRule(models.Model):
         res['sale_line_id'] = values.get('sale_line_id', False)
         return res
 
+    def _update_purchase_order_line(self, product_id, product_qty, product_uom, values, line, partner):
+        if values.get('sale_line_id'):
+            sale_line = self.env['sale.order.line'].browse(values.get('sale_line_id'))
+            po_line = self.env['purchase.order.line'].search([
+                ('sale_line_id', '=', values.get('sale_line_id')),
+                ('order_id.state', 'not in', ['cancel', 'done']),
+                ('product_id', '=', product_id.id)
+            ])
+            if po_line:
+                product_qty = sale_line.product_qty - po_line.product_qty
+        res = super(ProcurementRule, self)._update_purchase_order_line(product_id, product_qty, product_uom, values, line, partner)
+        return res
+
+    def _make_po_get_domain(self, values, partner):
+        domain = super(ProcurementRule, self)._make_po_get_domain(values, partner)
+        if values.get('sale_line_id'):
+            domain_new = tuple()
+            for term in domain:
+                if term != ('state', '=', 'draft'):
+                    domain_new += (tuple(term),)
+                else:
+                    domain_new += (('state', 'not in', ('cancel', 'done')),)
+            domain = domain_new
+        return domain
--- addons/stock_dropshipping/models/sale.py
+++ addons/stock_dropshipping/models/sale.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, fields
+from odoo import api, models, fields, _
+from odoo.exceptions import UserError
 
 
 class SaleOrderLine(models.Model):
@@ -11,13 +12,53 @@ class SaleOrderLine(models.Model):
 
     @api.multi
     def _get_qty_procurement(self):
+        self.ensure_one()
+        qty = 0.0
         # People without purchase rights should be able to do this operation
         purchase_lines_sudo = self.sudo().purchase_line_ids
         if not self.move_ids.filtered(lambda r: r.state != 'cancel') and purchase_lines_sudo.filtered(lambda r: r.state != 'cancel'):
-            qty = 0.0
-            for po_line in purchase_lines_sudo.filtered(lambda r: r.state != 'cancel'):
-                qty += po_line.product_uom._compute_quantity(po_line.product_qty, self.product_uom, rounding_method='HALF-UP')
-            return qty
+            purchase_lines = purchase_lines_sudo.filtered(lambda r: r.state != 'cancel')
+            if purchase_lines:
+                for po_line in purchase_lines:
+                    qty += po_line.product_uom._compute_quantity(po_line.product_qty, self.product_uom, rounding_method='HALF-UP')
+                return qty
+            else:
+                return super(SaleOrderLine, self)._get_qty_procurement()
         else:
             return super(SaleOrderLine, self)._get_qty_procurement()
+
+    @api.model
+    def create(self, vals):
+        if 'product_uom_qty' in vals and vals.get('order_id'):
+            order = self.env['sale.order'].browse(vals.get('order_id'))
+            existing_line = order.order_line.filtered(
+                lambda line: line.product_id.id == vals.get('product_id')
+            )
+            if existing_line:
+                po_line = self.env['purchase.order.line'].search([
+                    ('sale_line_id', 'in', existing_line.ids),
+                    ('order_id.state', 'not in', ['cancel', 'done']),
+                    ('product_id', '=', vals.get('product_id'))
+                ])
+                if po_line:
+                    raise UserError(
+                        _('Please reuse the order line with the same product for drop shipping instead of creating a new line which can not be linked!')
+                    )
+        res = super(SaleOrderLine, self).create(vals)
+        return res
+
+    @api.multi
+    def write(self, vals):
+        if 'product_uom_qty' in vals:
+            for line in self:
+                po_line = self.env['purchase.order.line'].search([
+                    ('sale_line_id', '=', line.id),
+                    ('order_id.state', 'not in', ['cancel', 'done']),
+                    ('product_id', '=', line.product_id.id)
+                ])
+                if po_line:
+                    line.move_ids = po_line.move_ids
+
+        res = super(SaleOrderLine, self).write(vals)
+        return res
 
