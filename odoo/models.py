9e3a2eff61e00343308dbc847ee8e1fb545fce22
Yannick Tivisse <yti@odoo.com>
2018-09-21 13:24:16 +0200

    [IMP] models.py: Log a warning if no '_description' is set on model

--- odoo/models.py
+++ odoo/models.py
@@ -519,6 +519,8 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
         for base in reversed(cls.__bases__):
             if not getattr(base, 'pool', None):
                 # the following attributes are not taken from model classes
+                if not base._inherit and not base._description:
+                    _logger.warning("The model %s has no _description", cls._name)
                 cls._description = base._description or cls._description
                 cls._table = base._table or cls._table
                 cls._sequence = base._sequence or cls._sequence
