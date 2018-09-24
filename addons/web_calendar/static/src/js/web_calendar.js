1aa38c0ed5141ed82278b729126de24acea98c98
Guenzn <guenter.selbert@sewisoft.de>
2018-09-24 20:30:02 +0200

    [FIX] show correct datetime format including timezone

--- addons/web_calendar/static/src/js/web_calendar.js
+++ addons/web_calendar/static/src/js/web_calendar.js
@@ -560,13 +560,12 @@ var CalendarView = View.extend({
                     }
                     else if (value instanceof Array) {
                         temp_ret[fieldname] = value[1]; // no name_get to make
-                    }
-                    else if (_.contains(["date", "datetime"], self.fields[fieldname].type)) {
-                        temp_ret[fieldname] = formats.format_value(value, self.fields[fieldname]);
-                    }
-                    else {
+                    }else {
                         throw new Error("Incomplete data received from dataset for record " + evt.id);
                     }
+                }
+                else if (_.contains(["date", "datetime"], self.fields[fieldname].type)) {
+                    temp_ret[fieldname] = formats.format_value(value, self.fields[fieldname]);
                 }
                 else if (_.contains(["one2many","many2many"], self.fields[fieldname].type)) {
                     if (value === false) {
