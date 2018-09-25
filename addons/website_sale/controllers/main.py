9b1d6965cdb15bb25903a205c722e49c626c8a87
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2018-09-25 06:46:07 +0200

    [FIX] website_sale: sort order must be a string
    Otherwise we will hit a ValueError in _generate_order_by_inner which
    results in a HTTP Code 500 <- nope we can not accept this even if the
    parameter was wrongly used

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -171,7 +171,10 @@ class WebsiteSale(http.Controller):
     def _get_search_order(self, post):
         # OrderBy will be parsed in orm and so no direct sql injection
         # id is added to be sure that order is a unique sort key
-        return 'website_published desc,%s , id desc' % post.get('order', 'website_sequence desc')
+        order_value = post.get('order')
+        if not isinstance(order_value, str):
+            order_value = 'website_sequence desc'
+        return 'website_published desc,%s , id desc' % order_value
 
     def _get_search_domain(self, search, category, attrib_values):
         domain = request.website.sale_product_domain()
