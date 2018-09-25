8b957b7e85c297a3ac394535a10c477b1d3cbd0d
Josse Colpaert <jco@odoo.com>
2018-09-25 18:43:00 +0200

    [IMP] hw_*: updated web interface for IoT Box
    We changed the naming of posbox to IoT Box everywhere
    and renewed the web interface using jinja2 templates in
    separate html files.
    
    A separate configuration for the server and the IoT Box name
    was added and it shows drivers for both  pos and iot.
    
    The interface also needs a step to step wizard that is launched
    when the access point is active for simplified configuration.
    
    Thanks to qle and Deep Patel.

--- addons/hw_posbox_homepage/__manifest__.py
+++ addons/hw_posbox_homepage/__manifest__.py
@@ -2,17 +2,17 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'PosBox Homepage',
+    'name': 'IoT Box Homepage',
     'category': 'Point of Sale',
     'sequence': 6,
     'website': 'https://www.odoo.com/page/point-of-sale-hardware',
-    'summary': 'A homepage for the PosBox',
+    'summary': 'A homepage for the IoT Box',
     'description': """
-PosBox Homepage
-===============
+IoT Box Homepage
+================
 
 This module overrides Odoo web interface to display a simple
-Homepage that explains what's the posbox and show the status,
+Homepage that explains what's the iotbox and show the status,
 and where to find documentation.
 
 If you activate this module, you won't be able to access the 
