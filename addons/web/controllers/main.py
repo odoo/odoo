8b01f0fac33925fb5699ba8ec10e792660e51d65
xmo-odoo <xmo@odoo.com>
2018-09-27 13:30:59 +0200

    [FIX] web: actually update the session token
    Also clear cache.

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -521,7 +521,8 @@ class Home(http.Controller):
         uid = request.env.user.id
         if request.env.user._is_system():
             uid = request.session.uid = odoo.SUPERUSER_ID
-            security.compute_session_token(request.session, request.env)
+            request.env['res.users']._invalidate_session_cache()
+            request.session.session_token = security.compute_session_token(request.session, request.env)
 
         return http.redirect_with_hash(self._login_redirect(uid))
 
