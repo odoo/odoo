486cb017ad1d882671db0249fc726bb37a0ec603
Xavier Morel <xmo@odoo.com>
2018-10-02 20:32:40 +0200

    [ADD] auth_password_policy
    Hard-checks password length according to configured rule (default: at
    least 8 characters).
    
    Closes #22351

---
+++ addons/auth_password_policy/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': "Password Policy",
+    "summary": "Implements basic password policy configuration & check",
+    'depends': ['base_setup', 'auth_crypt'],
+    'data': ['views/res_config_settings_views.xml']
+}
