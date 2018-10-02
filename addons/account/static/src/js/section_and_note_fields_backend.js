odoo/odoo

commit 20ae0013077ece8f0e1398b616005367d966bc1f

[IMP] product,(website_)sale: Product configurator bug fix
Targets commit a25dda0bba1f3ef19dcae62eee771f91ee018016

Purpose
=======

[Follow up JCR testing]

Various bug fixes including:

- optional products dont show when they have variants configured on them
- when no variant is created, you cant choose the product one first time in the ecommerce
- when in debug, attribute and attribute value menu is double
- fix design of default picture in cart summary / product comparison
- fix missing href tag on product comparison "add to cart" buttons
- products with the "Can be sold" box unchecked should not appear in the product configurator

addons/account/static/src/js/section_and_note_fields_backend.js
@@ -91,6 +91,7 @@ var SectionAndNoteListRenderer = ListRenderer.extend({
         this.unselectRow().then(function () {
             var context = ev.currentTarget.dataset.context;
 
+            var pricelistId = self._getPricelistId();
             if (context && pyUtils.py_eval(context).open_product_configurator){
                 self._rpc({
                     model: 'ir.model.data',
@@ -102,7 +103,10 @@ var SectionAndNoteListRenderer = ListRenderer.extend({
                         type: 'ir.actions.act_window',
                         res_model: 'sale.product.configurator',
                         views: [[res_id, 'form']],
-                        target: 'new'
+                        target: 'new',
+                        context: {
+                            'default_pricelist_id': pricelistId
+                        }
                     }, {
                         on_close: function (products) {
                             if (products && products !== 'special'){
@@ -122,6 +126,20 @@ var SectionAndNoteListRenderer = ListRenderer.extend({
                 self.trigger_up('add_record', {context: context && [context]}); // TODO write a test, the deferred was not considered
             }
         });
+    },
+
+    /**
+     * Will try to get the pricelist_id value from the parent sale_order form
+     *
+     * @private
+     * @returns {integer} pricelist_id's id
+     */
+    _getPricelistId: function () {
+        var saleOrderForm = this.getParent() && this.getParent().getParent();
+        var stateData = saleOrderForm && saleOrderForm.state && saleOrderForm.state.data;
+        var pricelist_id = stateData.pricelist_id && stateData.pricelist_id.data;
+
+        return pricelist_id.id;
     },
 
     /**

addons/product/views/product_views.xml
@@ -11,7 +11,7 @@
                 <header>
                     <button string="Configure Variants" type="action"
                         name="%(product_attribute_value_action)d"
-                        attrs="{'invisible': [('attribute_line_ids', '=', False)]}"
+                        attrs="{'invisible': [('attribute_line_ids', '&lt;=', 0)]}"
                         groups="product.group_product_variant"/>
                 </header>
                 <sheet>

addons/sale/controllers/product_configurator.py
@@ -60,7 +60,7 @@ class ProductConfiguratorController(http.Controller):
 
         has_optional_products = False
         for optional_product in product.optional_product_ids:
-            if optional_product.get_filtered_variants(product):
+            if optional_product.has_dynamic_attributes() or optional_product.get_filtered_variants(product):
                 has_optional_products = True
                 break
 

addons/sale/models/sale.py
@@ -159,7 +159,6 @@ class SaleOrder(models.Model):
     partner_shipping_id = fields.Many2one('res.partner', string='Delivery Address', readonly=True, required=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)], 'sale': [('readonly', False)]}, help="Delivery address for current sales order.")
 
     pricelist_id = fields.Many2one('product.pricelist', string='Pricelist', required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, help="Pricelist for current sales order.")
-    pricelist_id_id = fields.Integer(string="Pricelist ID", related='pricelist_id.id', readonly=True)
     currency_id = fields.Many2one("res.currency", related='pricelist_id.currency_id', string="Currency", readonly=True, required=True)
     analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account', readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, help="The analytic account related to a sales order.", copy=False, oldname='project_id')
 

addons/sale/static/src/js/product_configurator_controller.js
@@ -56,7 +56,7 @@ var ProductConfiguratorFormController = FormController.extend({
             route: '/product_configurator/configure',
             params: {
                 product_id: event.data.changes.product_template_id.id,
-                pricelist_id: $('.js_sale_order_pricelist_id').html()
+                pricelist_id: this.renderer.pricelistId
             }
         }).then(function (configurator) {
             self.renderer.renderConfigurator(configurator);
@@ -87,19 +87,12 @@ var ProductConfiguratorFormController = FormController.extend({
         ];
 
         var productId = parseInt($modal.find(productSelector.join(', ')).first().val(), 10);
-        var productReady = $.Deferred();
-        if (productId){
-            productReady.resolve(productId);
-        } else {
-            productReady = this._rpc({
-                model: 'product.template',
-                method: 'create_product_variant',
-                args: [
-                    $modal.find('.product_template_id').val(),
-                    JSON.stringify(self.renderer.getSelectedVariantValues($modal))
-                ],
-            });
-        }
+        var productReady = this.renderer.selectOrCreateProduct(
+            $modal,
+            productId,
+            $modal.find('.product_template_id').val(),
+            false
+        );
 
         productReady.done(function (productId){
             $modal.find(productSelector.join(', ')).val(productId);
@@ -126,7 +119,7 @@ var ProductConfiguratorFormController = FormController.extend({
 
             self.optionalProductsModal = new OptionalProductsModal($('body'), {
                 rootProduct: self.rootProduct,
-                pricelistId: $('.js_sale_order_pricelist_id').html(),
+                pricelistId: self.renderer.pricelistId,
                 okButtonText: _t('Confirm'),
                 cancelButtonText: _t('Back'),
                 title: _t('Configure')

addons/sale/static/src/js/product_configurator_mixin.js
@@ -269,6 +269,43 @@ var ProductConfiguratorMixin = {
         return values.concat(unchangedValues);
     },
 
+    /**
+     * Will return a deferred:
+     * - If the product already exists, immediately resolves it with the product_id
+     * - If the product does not exist yet ("dynamic" variant creation), this method will
+     *   create the product first and then resolve the deferred with the created product's id
+     *
+     * @param {$.Element} $container the container to look into
+     * @param {integer} productId the product id
+     * @param {integer} productTemplateId the corresponding product template id
+     * @param {boolean} useAjax wether the rpc call should be done using ajax.jsonRpc or using _rpc
+     * @returns {$.Deferred} the deferred that will be resolved with a {integer} productId
+     */
+    selectOrCreateProduct: function ($container, productId, productTemplateId, useAjax) {
+        var self = this;
+        var productReady = $.Deferred();
+        if (productId && productId !== '0'){
+            productReady.resolve(productId);
+        } else {
+            var params = {
+                model: 'product.template',
+                method: 'create_product_variant',
+                args: [
+                    productTemplateId,
+                    JSON.stringify(self.getSelectedVariantValues($container))
+                ]
+            };
+
+            if (useAjax) {
+                productReady = ajax.jsonRpc('/web/dataset/call', 'call', params);
+            } else {
+                productReady = this._rpc(params);
+            }
+        }
+
+        return productReady;
+    },
+
     //--------------------------------------------------------------------------
     // Private
     //--------------------------------------------------------------------------

addons/sale/static/src/js/product_configurator_modal.js
@@ -345,20 +345,12 @@ var OptionalProductsModal = Dialog.extend(ServicesMixin, ProductConfiguratorMixi
             $main_product.after($parent);
         }
 
-        var productId = $parent.find('.product_id').val();
-        var productReady = $.Deferred();
-        if (productId && productId !== '0'){
-            productReady.resolve(productId);
-        } else {
-            productReady = ajax.jsonRpc('/web/dataset/call', 'call', {
-                model: 'product.template',
-                method: 'create_product_variant',
-                args: [
-                    productTemplateId,
-                    JSON.stringify(self.getSelectedVariantValues($parent))
-                ]
-            });
-        }
+        var productReady = this.selectOrCreateProduct(
+            $parent,
+            $parent.find('.product_id').val(),
+            productTemplateId,
+            true
+        );
 
         productReady.done(function (productId){
             $parent.find('.product_id').val(productId);
@@ -376,9 +368,12 @@ var OptionalProductsModal = Dialog.extend(ServicesMixin, ProductConfiguratorMixi
                 if ($addedItem.find(".product_template_id").length > 0) {
                     // we need to map this product with it's optional products to be able to:
                     // - remove them from the cart if the parent product is removed
-                    // - place the optional products under their parent in the interface when they're added to the cart
-                    optionalProductsMap[productTemplateId] = $addedItem.find(".product_template_id").map(function () {
-                        return $(this).val();
+                    // - place the optional products under their parent in
+                    //   the interface when they're added to the cart
+                    optionalProductsMap[productTemplateId] = $addedItem
+                        .find(".product_template_id")
+                        .map(function () {
+                            return $(this).val();
                     }).get();
                 }
 

addons/sale/static/src/js/product_configurator_renderer.js
@@ -10,8 +10,7 @@ var ProductConfiguratorFormRenderer = FormRenderer.extend(ProductConfiguratorMix
      */
     init: function (){
         this._super.apply(this, arguments);
-
-        this.pricelistId = $('.js_sale_order_pricelist_id').html();
+        this.pricelistId = this.state.context.default_pricelist_id || 0;
     },
     /**
      * @override

commit fb400d35163437e5c41890d0fdef4834df8f7319

[FIX] account: allow creating a new line on an invoice without crashing if not pricelist is defined
This bug had been introduced in https://github.com/odoo/odoo/commit/20ae0013077ece8f0e1398b616005367d966bc1f .

addons/account/static/src/js/section_and_note_fields_backend.js
@@ -137,9 +137,9 @@ var SectionAndNoteListRenderer = ListRenderer.extend({
     _getPricelistId: function () {
         var saleOrderForm = this.getParent() && this.getParent().getParent();
         var stateData = saleOrderForm && saleOrderForm.state && saleOrderForm.state.data;
-        var pricelist_id = stateData.pricelist_id && stateData.pricelist_id.data;
+        var pricelist_id = stateData.pricelist_id && stateData.pricelist_id.data && stateData.pricelist_id.data.id;
 
-        return pricelist_id.id;
+        return pricelist_id;
     },
 
     /**