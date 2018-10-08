18bffd060bddf6341fc3d2caef3e97ab6cb7ca33
asa-odoo <asa@odoo.com>
2018-10-08 15:49:44 +0530

    [FIX] point_of_sale,stock: Change demo data uom
    delivery: fix test data

--- addons/delivery/tests/test_delivery_stock_move.py
+++ addons/delivery/tests/test_delivery_stock_move.py
@@ -17,7 +17,7 @@ class StockMoveInvoice(AccountingTestCase):
         self.pricelist_id = self.env.ref('product.list0')
         self.product_11 = self.env.ref('product.product_product_11')
         self.product_cable_management_box = self.env.ref('stock.product_cable_management_box')
-        self.product_uom_kgm = self.env.ref('uom.product_uom_kgm')
+        self.product_uom_unit = self.env.ref('uom.product_uom_unit')
         self.normal_delivery = self.env.ref('delivery.normal_delivery_carrier')
 
     def test_01_delivery_stock_move(self):
@@ -32,10 +32,10 @@ class StockMoveInvoice(AccountingTestCase):
             'partner_shipping_id': self.partner_18.id,
             'pricelist_id': self.pricelist_id.id,
             'order_line': [(0, 0, {
-                'name': 'Ice Cream',
+                'name': 'Cable Management Box',
                 'product_id': self.product_cable_management_box.id,
                 'product_uom_qty': 2,
-                'product_uom': self.product_uom_kgm.id,
+                'product_uom': self.product_uom_unit.id,
                 'price_unit': 750.00,
             })],
         })
