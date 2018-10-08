e27f6e40897412db220e81ccdd3c35221a484002
William Henrotin <whe@odoo.com>
2018-10-08 12:01:49 +0000

    [FIX] mrp: remove multiness in non multi method
    The method button_start begins by an ensure_one() but loop on self too. 
    This commit removes to loop for cleaner code.
    
    Task : 1850640
    
    closes odoo/odoo#27532

--- addons/mrp/models/mrp_workorder.py
+++ addons/mrp/models/mrp_workorder.py
@@ -420,20 +420,19 @@ class MrpWorkorder(models.Model):
             loss_id = self.env['mrp.workcenter.productivity.loss'].search([('loss_type','=','performance')], limit=1)
             if not len(loss_id):
                 raise UserError(_("You need to define at least one productivity loss in the category 'Performance'. Create one from the Manufacturing app, menu: Configuration / Productivity Losses."))
-        for workorder in self:
-            if workorder.production_id.state != 'progress':
-                workorder.production_id.write({
-                    'state': 'progress',
-                    'date_start': datetime.now(),
-                })
-            timeline.create({
-                'workorder_id': workorder.id,
-                'workcenter_id': workorder.workcenter_id.id,
-                'description': _('Time Tracking: ')+self.env.user.name,
-                'loss_id': loss_id[0].id,
+        if self.production_id.state != 'progress':
+            self.production_id.write({
+                'state': 'progress',
                 'date_start': datetime.now(),
-                'user_id': self.env.user.id
             })
+        timeline.create({
+            'workorder_id': self.id,
+            'workcenter_id': self.workcenter_id.id,
+            'description': _('Time Tracking: ')+self.env.user.name,
+            'loss_id': loss_id[0].id,
+            'date_start': datetime.now(),
+            'user_id': self.env.user.id
+        })
         return self.write({'state': 'progress',
                     'date_start': datetime.now(),
         })
