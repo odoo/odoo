ff40f82be3bbd1c19d3efecfa73e28d3d6dc6308
Alexandre Kühn <aku@odoo.com>
2018-10-09 09:38:50 +0000

    [FIX] web: `self.menu.change_menu_section` is not a function
    Revision on https://github.com/odoo/odoo/commit/872461c8cc8ddfa779410dfde92f7b6361c58a4d
    
    The commit above changed the menu in community, so that it
    looks like enterprise. To do so, it copies the menu management
    from the enterprise repository to the community repository.
    
    Some code about the home menu has been removed, since it is replaced
    with the app menu in community. However, some functions have been
    removed by accident, such as `change_menu_section`.
    
    Closes #27436

--- addons/web/static/src/js/chrome/menu.js
+++ addons/web/static/src/js/chrome/menu.js
@@ -79,6 +79,32 @@ var Menu = Widget.extend({
 
         return this._super.apply(this, arguments);
     },
+    change_menu_section: function (primary_menu_id) {
+        if (!this.$menu_sections[primary_menu_id]) {
+            return; // unknown menu_id
+        }
+
+        if (this.current_primary_menu === primary_menu_id) {
+            return; // already in that menu
+        }
+
+        if (this.current_primary_menu) {
+            this.$menu_sections[this.current_primary_menu].detach();
+        }
+
+        // Get back the application name
+        for (var i = 0; i < this.menu_data.children.length; i++) {
+            if (this.menu_data.children[i].id === primary_menu_id) {
+                this.$menu_brand_placeholder.text(this.menu_data.children[i].name);
+                break;
+            }
+        }
+
+        this.$menu_sections[primary_menu_id].appendTo(this.$section_placeholder);
+        this.current_primary_menu = primary_menu_id;
+
+        core.bus.trigger('resize');
+    },
     _trigger_menu_clicked: function (menu_id, action_id) {
         this.trigger_up('menu_clicked', {
             id: menu_id,
