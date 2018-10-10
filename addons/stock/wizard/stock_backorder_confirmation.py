9764f04a3c33ca1d69dc5bf75b1eb2b02767ef6a
arbaes <baesarnaud@gmail.com>
2018-10-10 13:33:50 +0000

    [IMP] stock: backorder created only when needed
    Before this commit, when the user chose to not make a backorder, a backorder was created then immediatly canceled.
    This behaviour is now changed as in such a case the backorder is simply not created anymore

--- addons/stock/wizard/stock_backorder_confirmation.py
+++ addons/stock/wizard/stock_backorder_confirmation.py
@@ -20,12 +20,7 @@ class StockBackorderConfirmation(models.TransientModel):
                     if float_compare(move.product_uom_qty, move.quantity_done, move.product_uom.rounding) > 0:
                         moves_to_log[move] = (move.quantity_done, move.product_uom_qty)
                 pick_id._log_less_quantities_than_expected(moves_to_log)
-        self.pick_ids.action_done()
-        if cancel_backorder:
-            for pick_id in self.pick_ids:
-                backorder_pick = self.env['stock.picking'].search([('backorder_id', '=', pick_id.id)])
-                backorder_pick.action_cancel()
-                pick_id.message_post(body=_("Back order <em>%s</em> <b>cancelled</b>.") % (backorder_pick.name))
+        self.pick_ids.with_context({'cancel_backorder': cancel_backorder}).action_done()
 
     def process(self):
         self._process()
