feb336a2034acc469fae188eb8e00fcc44503214
Pierre Masereel <pim@odoo.com>
2018-10-11 13:21:39 +0000

    [REF] stock: remove unused location usage
    The stock location usage 'procurement' is not used anymore in stock, and
    we remove it as it is not used either for the stock and valuation of
    stock.
    
    TASK-ID: 38469

--- addons/stock/models/stock_location.py
+++ addons/stock/models/stock_location.py
@@ -33,7 +33,6 @@ class Location(models.Model):
         ('internal', 'Internal Location'),
         ('customer', 'Customer Location'),
         ('inventory', 'Inventory Loss'),
-        ('procurement', 'Procurement'),
         ('production', 'Production'),
         ('transit', 'Transit Location')], string='Location Type',
         default='internal', index=True, required=True,
@@ -42,7 +41,6 @@ class Location(models.Model):
              "\n* Internal Location: Physical locations inside your own warehouses,"
              "\n* Customer Location: Virtual location representing the destination location for products sent to your customers"
              "\n* Inventory Loss: Virtual location serving as counterpart for inventory operations used to correct stock levels (Physical inventories)"
-             "\n* Procurement: Virtual location serving as temporary counterpart for procurement operations when the source (vendor or production) is not known yet. This location should be empty when the procurement scheduler has finished running."
              "\n* Production: Virtual counterpart location for production operations: this location consumes the raw material and produces finished products"
              "\n* Transit Location: Counterpart location that should be used in inter-company or inter-warehouses operations")
     location_id = fields.Many2one(
