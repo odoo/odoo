2122138cb8237ff1fa0dcecccfce5922fd21e52d
Rémi Rahir <rar@odoo.com>
2018-10-17 14:59:57 +0200

    [FIX] snailmail: instable behaviour on print
    Since commit b6a981405e1c211b3d5377ff53a2bb7847225d6f we require a
    certain key in the context to apply the custom snailmail report layout.
    This was badly managed and would eventually confilt inside the modal
    that links snailmail and the mail composer.
    
    This fix stabilises the biehaviour by properly propagating the context
    key.

--- addons/snailmail/models/ir_actions_report.py
+++ addons/snailmail/models/ir_actions_report.py
@@ -7,14 +7,6 @@ class IrActionsReport(models.Model):
     _inherit = 'ir.actions.report'
 
     @api.model
-    def _get_rendering_context(self, docids, data):
-        res = super(IrActionsReport, self)._get_rendering_context(docids, data)
-        #  Add snailmail_layout if specified in the context
-        if self.env.context.get('snailmail_layout'):
-            res.update({'snailmail_layout': True})
-        return res
-
-    @api.model
     def get_paperformat(self):
         # force the right format (euro/A4) when sending letters, only if we are not using the l10n_DE layout
         res = super(IrActionsReport, self).get_paperformat()
