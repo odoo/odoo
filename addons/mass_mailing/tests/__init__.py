96fbc1a95773ded0da26900f9621c1c12e777492
Thibault Delavallée <tde@odoo.com>
2018-10-17 13:06:22 +0000

    [MOV] (test_)mass_mailing: move not mass-mailing models specific tests
    Purpose of this commit is to move tests not completely specific to some
    mass mailing models to test_mass_mailing. Purpose is to allow to write
    more in-depth tests based on existing ones by using test models instead
    of some standard odoo ones.
    
    This commit is linked to task ID 1889703 and PR #27526.

--- addons/mass_mailing/tests/__init__.py
+++ addons/mass_mailing/tests/__init__.py
@@ -2,7 +2,3 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_mass_mailing_list_merge
-from . import test_mass_mailing_content
-from . import test_mail_auto_blacklist
-from . import test_mass_mailing
-from . import test_blacklisted_channel_partner
