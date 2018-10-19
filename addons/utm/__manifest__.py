65f9840fd2cb69ace065d50d2b9c6d2aebd703ef
Thibault Delavallée <tde@odoo.com>
2018-10-19 12:55:40 +0000

    [MOV] utm: organize module structure according to guidelines
    This module holds old code and is still not updated according to guidelines.
    Let us update a bit the naming in this module to have readable code.
    
    This commit is linked to task ID 1896681 and PR #27975. No functional change
    should occur with this commit as it contains only renaming / move.

--- addons/utm/__manifest__.py
+++ addons/utm/__manifest__.py
@@ -1,16 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'UTM Trackers',
     'category': 'Hidden',
     'description': """
-Enable UTM trackers in shared links.
-=====================================================
-        """,
+Enable management of UTM trackers: campaign, medium, source.
+""",
     'version': '1.0',
     'depends': ['base'],
     'data': [
+        'data/utm_data.xml',
+        'views/utm_views.xml',
         'security/ir.model.access.csv',
-        'views/utm.xml',
-        'data/utm_data.xml'
     ],
     'demo': [],
     'auto_install': False,
