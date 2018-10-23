d5ca268438fb60cd825c1b2545670d1ed5d3d0f7
Antoine Prieels (ANP) <anp@odoo.com>
2018-10-23 08:37:03 +0000

    [IMP] snailmail: Change order of settings and default values
    Task #1892473

--- addons/snailmail/models/res_company.py
+++ addons/snailmail/models/res_company.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class Company(models.Model):
     _inherit = "res.company"
 
-    snailmail_color = fields.Boolean(string='Color', default=False)
+    snailmail_color = fields.Boolean(string='Color', default=True)
     snailmail_duplex = fields.Boolean(string='Both sides', default=False)
