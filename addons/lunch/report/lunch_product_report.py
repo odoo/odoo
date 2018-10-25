f6fd798f6bad3c15fe816ed345de047c77fbf623
RomainLibert <rli@odoo.com>
2018-10-25 16:43:19 +0200

    [IMP] lunch: automatize lunch orders
    We would like to automatize lunch as currently all orders are manually ordered and validated,
    and a cash move has to be manually created each time you want to credit the wallet of some user.
    
    1/ In this commit we add a new widget that comes with the kanban view
       and will allow to take orders in a friendly manner.
    2/ We also add a lunch.supplier model this also allows you to configure an
       automatic ordering via email.
    3/ Introduces new lunch.location model to replace res.partner on suppliers
    4/ New mail template for automatic ordering
    5/ Add the concept of new products (mark a product as new until some date)
    6/ The toppings are now defined by product category
    7/ Lunch.order removed, lunch.order.line renamed into lunch.order
    8/ New SQL report to get the wallet content (aggregate of lunch.cashmove and lunch.order)
    9/ New setting to allow negative wallet (aka overdraft)
    10/ Application now remembers what notes and toppings you last ordered with your product
    11/ You can change your location using the widget on the kanban view
    12/ Edition of the order can be done until the order is arrived
    
    TaskID: 1856876
    TaskID: 1916105

---
+++ addons/lunch/report/lunch_product_report.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, tools
+from odoo.osv import expression
+
+
+class LunchProductReport(models.Model):
+    _name = "lunch.product.report"
+    _description = 'Product report'
+    _auto = False
+    _order = 'is_favorite desc, is_new desc, last_order_date asc, product_id asc'
+
+    id = fields.Integer('ID')
+    product_id = fields.Integer('Product ID')
+    name = fields.Char('Product Name')
+    category_id = fields.Many2one('lunch.product.category', 'Product Category')
+    description = fields.Text('Description')
+    price = fields.Float('Price')
+    supplier_id = fields.Many2one('lunch.supplier', 'Vendor')
+    company_id = fields.Many2one('res.company')
+    currency_id = fields.Many2one('res.currency', related='company_id.currency_id')
+    is_favorite = fields.Boolean('Favorite')
+    user_id = fields.Many2one('res.users')
+    is_new = fields.Boolean('New')
+    active = fields.Boolean('Active')
+    last_order_date = fields.Date('Last Order Date')
+
+    # This field is used only for searching
+    is_available_at = fields.Many2one('lunch.location', 'Product Availability', compute='_compute_is_available_at', search='_search_is_available_at')
+
+    def _compute_is_available_at(self):
+        """
+            Is available_at is always false when browsing it
+            this field is there only to search (see _search_is_available_at)
+        """
+        for product in self:
+            product.is_available_at = False
+
+    def _search_is_available_at(self, operator, value):
+        supported_operators = ['in', 'not in', '=', '!=']
+
+        if not operator in supported_operators:
+            return expression.TRUE_DOMAIN
+
+        if isinstance(value, int):
+            value = [value]
+
+        if operator in expression.NEGATIVE_TERM_OPERATORS:
+            return expression.AND([[('supplier_id.available_location_ids', 'not in', value)], [('supplier_id.available_location_ids', '!=', False)]])
+
+        return expression.OR([[('supplier_id.available_location_ids', 'in', value)], [('supplier_id.available_location_ids', '=', False)]])
+
+    @api.multi
+    def write(self, values):
+        user_id = self.env.user.id
+        if 'is_favorite' in values:
+            for report in self:
+                if values['is_favorite']:
+                    self.env.cr.execute('''INSERT INTO lunch_product_favorite_user_rel(product_id, user_id) VALUES (%s, %s)''',
+                                (report.product_id, user_id))
+                else:
+                    self.env.cr.execute('''DELETE FROM lunch_product_favorite_user_rel WHERE product_id=%s AND user_id=%s''',
+                                (report.product_id, user_id))
+
+    def init(self):
+        tools.drop_view_if_exists(self._cr, self._table)
+
+        self._cr.execute("""
+            CREATE or REPLACE view %s AS (
+                SELECT
+                    row_number() over () AS id,
+                    product.id AS product_id,
+                    product.name,
+                    product.category_id,
+                    product.description,
+                    product.price,
+                    product.supplier_id,
+                    product.company_id,
+                    product.active,
+                    users.id AS user_id,
+                    fav.user_id IS NOT NULL AS is_favorite,
+                    product.new_until >= current_date AS is_new,
+                    (SELECT date FROM lunch_order WHERE product_id = product.id AND user_id = users.id AND date <= current_date ORDER BY date DESC LIMIT 1) AS last_order_date
+                FROM lunch_product product
+                RIGHT JOIN res_users users ON users.company_id = product.company_id -- multi company
+                LEFT JOIN lunch_product_favorite_user_rel fav ON fav.product_id = product.id AND fav.user_id = users.id
+                JOIN res_groups_users_rel groups ON groups.uid = users.id -- only generate for internal users
+                WHERE users.active AND product.active AND groups.gid = %%s --only take into account active products and users
+            );
+        """ % self._table, (self.env.ref('base.group_user').id,))
---
+++ addons/lunch/static/src/js/lunch_kanban_renderer.js
@@ -0,0 +1,29 @@
+odoo.define('lunch.LunchKanbanRenderer', function (require) {
+"use strict";
+
+/**
+ * This file defines the Renderer for the Lunch Kanban view, which is an
+ * override of the KanbanRenderer.
+ */
+
+var LunchKanbanRecord = require('lunch.LunchKanbanRecord');
+
+var KanbanRenderer = require('web.KanbanRenderer');
+
+var LunchKanbanRenderer = KanbanRenderer.extend({
+    config: _.extend({}, KanbanRenderer.prototype.config, {
+        KanbanRecord: LunchKanbanRecord,
+    }),
+
+    /**
+     * @override
+     */
+    start: function () {
+        this.$el.addClass('o_lunch_kanban_view position-relative align-content-start flex-grow-1 flex-shrink-1');
+        return this._super.apply(this, arguments);
+    },
+});
+
+return LunchKanbanRenderer;
+
+});
