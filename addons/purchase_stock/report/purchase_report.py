b5cea7e2b6908c5c8ab051f296e5e4c85e64c9fe
Hetashree Chauhan <hch@odoo.com>
2018-10-30 06:34:50 +0000

    [IMP] purchase: purchase report improvement
    Modify purchase report in order to make it consistent with
    sales report.
    For the RFQ it will show the Order Date and for the Purchase order
    menu it will display the Confirmation Date.
    
    Also rewrite a bit the SQL view in order to compute every purchase
    order in the company currency.
    
    Technicaly rewrite the SQL alias in order to have a report more
    readable in the future.
    
    Task ID : 1857130.
    
    closes odoo/odoo#28248
    
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>

--- addons/purchase_stock/report/purchase_report.py
+++ addons/purchase_stock/report/purchase_report.py
@@ -13,7 +13,7 @@ class PurchaseReport(models.Model):
         return super(PurchaseReport, self)._select() + ", spt.warehouse_id as picking_type_id"
 
     def _from(self):
-        return super(PurchaseReport, self)._from() + " left join stock_picking_type spt on (spt.id=s.picking_type_id)"
+        return super(PurchaseReport, self)._from() + " left join stock_picking_type spt on (spt.id=po.picking_type_id)"
 
     def _group_by(self):
         return super(PurchaseReport, self)._group_by() + ", spt.warehouse_id"
