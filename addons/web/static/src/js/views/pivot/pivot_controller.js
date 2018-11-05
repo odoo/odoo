7618f84beb3e3252f0e62de0650d4bcb33398b29
Mathieu Duckerts-Antoine <dam@odoo.com>
2018-11-05 13:17:46 +0000

    [FIX] web: sort pivot rows in comparison mode
    Before this commit, it was not possible to sort
    the rows of a pivot view while in comparison
    mode. This fix makes it now possible. Note that
    in comparison mode, a click on a header of type
    "measure" will sort the rows according to the
    column values coming from the data of interest.
    
    closes odoo/odoo#28411

--- addons/web/static/src/js/views/pivot/pivot_controller.js
+++ addons/web/static/src/js/views/pivot/pivot_controller.js
@@ -336,7 +336,8 @@ var PivotController = AbstractController.extend({
         var col_id = $target.data('id');
         var measure = $target.data('measure');
         var isAscending = $target.hasClass('o_pivot_measure_row_sorted_asc');
-        this.model.sortRows(col_id, measure, isAscending);
+        var dataType = $target.data('data_type');
+        this.model.sortRows(col_id, measure, isAscending, dataType);
         this.update({}, {reload: false});
     },
     /**
