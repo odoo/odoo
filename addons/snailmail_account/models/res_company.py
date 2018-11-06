9df4e270cd15acc0029ec9a90bad4b41f06a4ebc
Nicolas Martinelli <nim@odoo.com>
2018-11-06 14:01:07 +0000

    [FIX] snailmail_account: do not send by default
    Since most of clients don't have any credit, deactivate this by default.
    
    opw-1906566
    
    closes odoo/odoo#28438

--- addons/snailmail_account/models/res_company.py
+++ addons/snailmail_account/models/res_company.py
@@ -7,4 +7,4 @@ from odoo import fields, models
 class Company(models.Model):
     _inherit = "res.company"
 
-    invoice_is_snailmail = fields.Boolean(string='Send by Letter by default', default=True)
+    invoice_is_snailmail = fields.Boolean(string='Send by Letter by default', default=False)
