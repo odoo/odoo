6b95bb6a61bba88500af912912635df02ba3ae0d
Pierre Masereel <pim@odoo.com>
2018-11-07 16:09:40 +0000

    [IMP] stock: inventory loss location for each company
    Each company should have its own inventory location to allow specific
    account on it.
    
    TASK-ID: 1882269

--- addons/stock/models/res_company.py
+++ addons/stock/models/res_company.py
@@ -17,7 +17,6 @@ class Company(models.Model):
            in case of resuply routes between warehouses belonging to the same company, because
            we don't want to create accounting entries at that time.
         '''
-        # TDE FIXME: called in data - should be done in init ??
         parent_location = self.env.ref('stock.stock_location_locations', raise_if_not_found=False)
         for company in self:
             location = self.env['stock.location'].create({
@@ -34,11 +33,38 @@ class Company(models.Model):
                 'property_stock_supplier': location.id,
             })
 
+    def _create_inventory_loss_location(self):
+        parent_location = self.env.ref('stock.stock_location_locations_virtual', raise_if_not_found=False)
+        inventory_loss_product_template_field = self.env['ir.model.fields'].search([('model','=','product.template'),('name','=','property_stock_inventory')])
+        for company in self:
+            inventory_loss_location = self.env['stock.location'].create({
+                'name': '%s: Inventory adjustment' % company.name,
+                'usage': 'inventory',
+                'location_id': parent_location.id,
+                'company_id': company.id,
+            })
+            self.env['ir.property'].create({
+                'name': 'property_stock_inventory_%s' % company.name,
+                'fields_id': inventory_loss_product_template_field.id,
+                'company_id': company.id,
+                'value': 'stock.location,%d' % inventory_loss_location.id,
+            })
+
+    @api.model
+    def create_missing_inventory_loss_location(self):
+        company_ids  = self.env['res.company'].search([])
+        inventory_loss_product_template_field = self.env['ir.model.fields'].search([('model','=','product.template'),('name','=','property_stock_inventory')])
+        companies_having_property = self.env['ir.property'].search([('fields_id', '=', inventory_loss_product_template_field.id)]).mapped('company_id')
+        company_without_property = company_ids - companies_having_property
+        for company in company_without_property:
+            company._create_inventory_loss_location()
+
     @api.model
     def create(self, vals):
         company = super(Company, self).create(vals)
 
         company.create_transit_location()
+        company.sudo()._create_inventory_loss_location()
         # mutli-company rules prevents creating warehouse and sub-locations
         self.env['stock.warehouse'].check_access_rights('create')
         self.env['stock.warehouse'].sudo().create({'name': company.name, 'code': company.name[:5], 'company_id': company.id, 'partner_id': company.partner_id.id})
