055e1b8324b09261d3e19fb31bfdd9b84db78b42
Christophe Monniez <moc@odoo.com>
2018-11-08 08:50:33 +0000

    [REM] packaging: remove useless code
    PKG-INFO is generated during the setup phase and is not needed anymore.
    Also this file contains outdated informations.
    
    py2exe is not used anymore to build the windows installer.
    
    closes odoo/odoo#28494

--- setup.py
+++ setup.py
@@ -1,124 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import re
-from glob import glob
 from setuptools import find_packages, setup
 from os.path import join, dirname
 
 
 exec(open(join(dirname(__file__), 'odoo', 'release.py'), 'rb').read())  # Load release variables
 lib_name = 'odoo'
-
-
-def py2exe_datafiles():
-    data_files = {}
-    data_files['Microsoft.VC90.CRT'] = glob('C:\Microsoft.VC90.CRT\*.*')
-
-    for root, dirnames, filenames in os.walk('odoo'):
-        for filename in filenames:
-            if not re.match(r'.*(\.pyc|\.pyo|\~)$', filename):
-                data_files.setdefault(root, []).append(join(root, filename))
-
-    import babel
-    data_files['babel/localedata'] = glob(join(dirname(babel.__file__), 'localedata', '*'))
-    others = ['global.dat', 'numbers.py', 'support.py', 'plural.py']
-    data_files['babel'] = [join(dirname(babel.__file__), f) for f in others]
-    others = ['frontend.py', 'mofile.py']
-    data_files['babel/messages'] = [join(dirname(babel.__file__), 'messages', f) for f in others]
-
-    import pytz
-    tzdir = dirname(pytz.__file__)
-    for root, _, filenames in os.walk(join(tzdir, 'zoneinfo')):
-        base = join('pytz', root[len(tzdir) + 1:])
-        data_files[base] = [join(root, f) for f in filenames]
-
-    import docutils
-    import passlib
-    import reportlab
-    import requests
-    data_mapping = ((docutils, 'docutils'),
-                    (passlib, 'passlib'),
-                    (reportlab, 'reportlab'),
-                    (requests, 'requests'))
-
-    for mod, datadir in data_mapping:
-        basedir = dirname(mod.__file__)
-        for root, _, filenames in os.walk(basedir):
-            base = join(datadir, root[len(basedir) + 1:])
-            data_files[base] = [join(root, f)
-                                for f in filenames
-                                if not f.endswith(('.py', '.pyc', '.pyo'))]
-
-    return list(data_files.items())
-
-
-def py2exe_options():
-    if os.name == 'nt':
-        import py2exe
-        return {
-            'console': [
-                {'script': 'odoo-bin', 'icon_resources': [
-                    (1, join('setup', 'win32', 'static', 'pixmaps', 'openerp-icon.ico'))
-                ]},
-            ],
-            'options': {
-                'py2exe': {
-                    'skip_archive': 1,
-                    'optimize': 0,  # Keep the assert running as the integrated tests rely on them.
-                    'dist_dir': 'dist',
-                    'packages': [
-                        'asynchat', 'asyncore',
-                        'BeautifulSoup',
-                        'commands',
-                        'dateutil',
-                        'decimal',
-                        'decorator',
-                        'docutils',
-                        'email',
-                        'encodings',
-                        'HTMLParser',
-                        'imaplib',
-                        'jinja2',
-                        'lxml', 'lxml._elementpath', 'lxml.builder', 'lxml.etree', 'lxml.objectify',
-                        'mako',
-                        'markupsafe',
-                        'mock',
-                        'ofxparse',
-                        'odoo',
-                        'passlib',
-                        'PIL',
-                        'poplib',
-                        'psutil',
-                        'pychart',
-                        'pydot',
-                        'pyparsing',
-                        'PyPDF2',
-                        'pytz',
-                        'reportlab',
-                        'requests',
-                        'select',
-                        'smtplib',
-                        'suds',
-                        'uuid',
-                        'vatnumber',
-                        'vobject',
-                        'win32service', 'win32serviceutil',
-                        'xlrd',
-                        'xlsxwriter',
-                        'xlwt',
-                        'xml', 'xml.dom',
-                        'yaml',
-                    ],
-                    'excludes': ['Tkconstants', 'Tkinter', 'tcl'],
-                }
-            },
-            'data_files': py2exe_datafiles()
-        }
-    else:
-        return {}
-
 
 setup(
     name='odoo',
@@ -176,5 +64,4 @@ setup(
     tests_require=[
         'mock',
     ],
-    **py2exe_options()
 )
