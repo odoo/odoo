2a82dfe50bbbe818f264dee2f6c7c6d654e980a6
Nicolas Lempereur <nle@odoo.com>
2018-11-08 12:20:09 +0000

    [FIX] mail: enable composer after message sent
    a27f3e6f18 disabled send button on composer sent.
    
    But this is only needed for chatter composer, other composers directly
    send the message and are not refreshed asynchronously.
    
    So for other composer, the send button worked one time but then was
    disabled and the only way to sent was ENTER (on basic composer) or
    CTRL+ENTER (on extended composer).
    
    With this fix, disabling the button is done only for chatter composer.
    
    11.0 backport of 12.0 #28474
    
    issue found when checking opw-1904029
    closes #28506

--- addons/mail/static/src/js/composer.js
+++ addons/mail/static/src/js/composer.js
@@ -479,13 +479,10 @@ var BasicComposer = Widget.extend(chat_mixin, {
     },
 
     send_message: function () {
-        var $button = this.$('.o_composer_button_send');
-
         if (this.is_empty() || !this.do_check_attachment_upload()) {
             return;
         }
 
-        $button.prop('disabled', true);
         clearTimeout(this.canned_timeout);
         var self = this;
         this.preprocess_message().then(function (message) {
