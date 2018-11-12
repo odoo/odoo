8000638ade5438e6cb8e754e9bc45ab148546922
Antoine Prieels (ANP) <anp@odoo.com>
2018-11-12 10:54:33 +0000

    [IMP] iap: Add full div to Web Settings Dashboard
    The IAP link in Web Settings Dashboard used to be a simple text link in DashboardApps, now we've created a full div
    
    closes odoo/odoo#28566

--- addons/iap/static/src/js/dashboard.js
+++ addons/iap/static/src/js/dashboard.js
@@ -2,23 +2,41 @@ odoo.define('iap.Dashboard', function (require) {
 "use strict";
 
 var Dashboard = require('web_settings_dashboard');
+var Widget = require('web.Widget');
 
 Dashboard.Dashboard.include({
     /**
      * @override
      */
-    load_apps: function (data) {
-        var _super = this._super.bind(this);
+    init: function () {
+        this._super.apply(this, arguments);
+        return this.all_dashboards.push('iap');
+    },
+            
+    load_iap: function (data) {
+        var self = this;
         return this._rpc({
             model:  'iap.account',
             method: 'get_account_url',
             args: [],
             kwargs: {},
         }).then(function (url) {
-            data.apps.url = url;
-            return _super(data);
+            data.iap = {};
+            data.iap.url = url;
+            return new DashboardIAP(self, data.iap).replace(self.$('.o_web_settings_dashboard_iap'));
         });
     },
+});
+
+var DashboardIAP = Widget.extend({
+    template: 'iap.web_settings_dashboard_iap',
+
+    init: function(parent, data) {
+        this.data = data;
+        this.parent = parent;
+        return this._super.apply(this, arguments);
+    },
+
 });
 
 });
