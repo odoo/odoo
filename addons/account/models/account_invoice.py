6d6a06f49d6e9cb011c668328f5035c843440f4d
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2018-11-13 14:37:22 +0100

    [FIX] account: wrong default journal based on type available in vals
    As the journal is mostly set in the Odoo world the _default_journal
    method is not needed during create, but in case you only have
    partner_id, account_id and type in vals you will have retrieved a wrong
    journal in case you have an incoming invoice.

--- addons/account/models/account_invoice.py
+++ addons/account/models/account_invoice.py
@@ -447,6 +447,9 @@ class AccountInvoice(models.Model):
 
     @api.model
     def create(self, vals):
+        if not vals.get('journal_id') and vals.get('type'):
+            vals['journal_id'] = self.with_context(type=vals.get('type'))._default_journal().id
+
         onchanges = {
             '_onchange_partner_id': ['account_id', 'payment_term_id', 'fiscal_position_id', 'partner_bank_id'],
             '_onchange_journal_id': ['currency_id'],
