7dd7a5f8b7a96cd3a2eb1b475609e4bc8c946533
Thibault Delavallée <tde@odoo.com>
2018-11-14 13:43:10 +0000

    [MOV] survey: rename files according to guidelines
    Purpose of this commit is to rename some files before going into some more
    work. No split or cleaning is done here, only preparing by doing some pure
    renaming.
    
    No functional behavior change should occur with this commit. Only move
    has been performed.
    
    This commit is linked to task ID 1903617 and PR #28166.

--- addons/survey/models/__init__.py
+++ addons/survey/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import survey
+from . import survey_survey
 from . import ir_autovacuum
