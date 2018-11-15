fb8e562756d0438997d4452a6b95b1a420fb734b
Swapnesh Shah <swapneshshah111@gmail.com>
2018-11-15 16:45:01 +0000

    [FIX] sale: Fix _compute_type_name to work in batch
    Closes #28713
    
    closes odoo/odoo#28742

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -259,7 +259,7 @@ class SaleOrder(models.Model):
     @api.depends('state')
     def _compute_type_name(self):
         for record in self:
-            record.type_name = _('Quotation') if self.state in ('draft', 'sent', 'cancel') else _('Sales Order')
+            record.type_name = _('Quotation') if record.state in ('draft', 'sent', 'cancel') else _('Sales Order')
 
     @api.multi
     def unlink(self):
