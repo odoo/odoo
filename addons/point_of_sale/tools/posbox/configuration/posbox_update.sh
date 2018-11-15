a61db487d4e34b353353e6231bcc5eaf52f97116
Josse Colpaert <jco@odoo.com>
2018-11-15 10:17:50 +0100

    [FIX] point_of_sale: IoT Box image update fix
    The update does not work anymore on the IoT Box,
    probably because the structure of the branch is
    a bit different now.  It starts to download everything
    instead.
    
    We did not use 'git archive' because this does not work
    on GitHub and downloading a zip would always have everything
    inside.  This way, we keep the original method, even with
    diverged branches.

--- addons/point_of_sale/tools/posbox/configuration/posbox_update.sh
+++ addons/point_of_sale/tools/posbox/configuration/posbox_update.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 sudo mount -o remount,rw /
-sudo git --work-tree=/home/pi/odoo/ --git-dir=/home/pi/odoo/.git pull
+
+# As we got an error of diverged branches in the case of git pull,
+# we fetch and checkout the remote branch, delete the local branch and
+# set the remote branch to be the new original one
+sudo git --work-tree=/home/pi/odoo/ --git-dir=/home/pi/odoo/.git fetch --depth=1
+sudo git --work-tree=/home/pi/odoo/ --git-dir=/home/pi/odoo/.git checkout origin/12.0
+sudo git --work-tree=/home/pi/odoo/ --git-dir=/home/pi/odoo/.git branch -D 12.0
+sudo git --work-tree=/home/pi/odoo/ --git-dir=/home/pi/odoo/.git checkout -b 12.0
 sudo mount -o remount,ro /
 (sleep 5 && sudo reboot) &
