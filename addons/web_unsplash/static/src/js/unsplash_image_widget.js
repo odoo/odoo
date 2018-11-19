8b1a05d9e09dca317ca38422d75f459a7e1784aa
Romain Derie <rde@odoo.com>
2018-11-19 14:36:31 +0000

    [FIX] web_unsplash: don't expose client_id in network
    Unsplash CEO asked us to stop exposing our key client side (even if it was
    only in the browser network and previously allowed by their API team leader).
    
    After multiple exchange, it has been negociated that:
    1. Our non saas user won't be able to apply for a production key and should
       prefix they Unsplash application name by 'Odoo:'. Their key will remain in
       test mode.
       Indeed, Unsplash won't be able to review every Odoo application and ensure
       it respects the API terms and it is a real application.
    2. Our saas user will query Unsplash with a dedicated enterprise key
    3. The documentation should explain it all
    
    Task-1911345

--- addons/web_unsplash/static/src/js/unsplash_image_widget.js
+++ addons/web_unsplash/static/src/js/unsplash_image_widget.js
@@ -88,10 +88,6 @@ ImageWidget.include({
                 res_id: self.options.res_id,
             }
         }).then(function (images) {
-            for (var img in self._unsplash.selectedImages) {
-                self.unsplashAPI.notifyDownload(self._unsplash.selectedImages[img].download_url);
-            }
-
             _.each(images, function (image) {
                 image.src = image.url;
                 image.isDocument = !(/gif|jpe|jpg|png/.test(image.mimetype));
@@ -140,7 +136,7 @@ ImageWidget.include({
             self.$('.unsplash_img_container').html(QWeb.render('web_unsplash.dialog.image.content', { rows: rows }));
             self._highlightSelectedImages();
         }).fail(function (err) {
-            self.$('.unsplash_img_container').html(QWeb.render('web_unsplash.dialog.error.content', err));
+            self.$('.unsplash_img_container').html(QWeb.render('web_unsplash.dialog.error.content', { status: err }));
         }).always(function () {
             self._toggleAttachmentContaines(false);
         });
