d5cd85b93ce9eec732c50de63944a795fcfc064f
Lucas Perais (lpe) <lpe@odoo.com>
2018-11-20 14:27:14 +0000

    [FIX] base: res_partner: clarify string of commercial_partner_country_id
    To avoid confusion in the filters and elsewhere
    
    OPW 1910654
    
    closes odoo/odoo#28848

--- odoo/addons/base/res/res_partner.py
+++ odoo/addons/base/res/res_partner.py
@@ -218,7 +218,8 @@ class Partner(models.Model):
     # technical field used for managing commercial fields
     commercial_partner_id = fields.Many2one('res.partner', compute='_compute_commercial_partner',
                                              string='Commercial Entity', store=True, index=True)
-    commercial_partner_country_id = fields.Many2one('res.country', related='commercial_partner_id.country_id', store=True)
+    commercial_partner_country_id = fields.Many2one('res.country', related='commercial_partner_id.country_id', store=True,
+        string="Commercial Entity's Country")
     commercial_company_name = fields.Char('Company Name Entity', compute='_compute_commercial_company_name',
                                           store=True)
     company_name = fields.Char('Company Name')
