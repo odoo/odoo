f43b6bc90c8e7b0ee94fc5a6bd10fdce0d227df6
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-11-21 16:50:11 +0100

    [FIX] sale: Take into account sales order date for sequence
    **Steps to reproduce**
    
    * Define "Sales Order" sequence for using date ranges checking "Use subsequences per date_range".
    * Change sequence prefix to "SO/%(year)s/".
    * Create a sales order with order date in 2019 (a different year than current one).
    
    **Current behavior**
    
    Got a sales order with 2018 (current year) number.
    
    **Expected behavior**
    
    Got a sales order with 2019 number.

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -259,10 +259,16 @@ class SaleOrder(models.Model):
     @api.model
     def create(self, vals):
         if vals.get('name', _('New')) == _('New'):
-            if 'company_id' in vals:
-                vals['name'] = self.env['ir.sequence'].with_context(force_company=vals['company_id']).next_by_code('sale.order') or _('New')
-            else:
-                vals['name'] = self.env['ir.sequence'].next_by_code('sale.order') or _('New')
+            context = self.env.context.copy()
+            if vals.get('company_id'):
+                context['force_company'] = vals['company_id']
+            if vals.get('date_order'):
+                # This convoluted form is needed because the passed value might not be normalized
+                context['ir_sequence_date'] = self._fields['date_order'].convert_to_cache(vals['date_order'], False, validate=False)[:10]
+            Sequence = self.env['ir.sequence']
+            if context:
+                Sequence = Sequence.with_context(context)
+            vals['name'] = Sequence.next_by_code('sale.order') or _('New')
 
         # Makes sure partner_invoice_id', 'partner_shipping_id' and 'pricelist_id' are defined
         if any(f not in vals for f in ['partner_invoice_id', 'partner_shipping_id', 'pricelist_id']):
