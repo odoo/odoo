d012bbd0e81da0d3d3039415855ea50dc185a7e3
arbaes <arb@odoo.com>
2018-11-22 14:39:39 +0000

    [IMP] mrp: quantity_done on kits in immediate transfers
    With this commit, when a kit is processed trough an immediate transfer,
    the quantity done set on the kit is now propagated to its components.
    
    TaskID: 1896772
    
    closes odoo/odoo#28998

--- addons/mrp/tests/test_stock.py
+++ addons/mrp/tests/test_stock.py
@@ -102,3 +102,176 @@ class TestWarehouse(common.TestMrpCommon):
 
 #        scrap_move = production_3.move_raw_ids.filtered(lambda x: x.product_id == self.product_2 and x.scrapped)
 #        self.assertTrue(scrap_move, "There are no any scrap move created for production order.")
+
+
+class TestKitPicking(common.TestMrpCommon):
+    def setUp(self):
+        super(TestKitPicking, self).setUp()
+
+        def create_product(name):
+            p = Form(self.env['product.product'])
+            p.name = name
+            p.type = 'product'
+            return p.save()
+
+        # Create a kit 'kit_parent' :
+        # ---------------------------
+        #
+        # kit_parent --|- kit_2 x2 --|- component_d x1
+        #              |             |- kit_1 x2 -------|- component_a   x2
+        #              |                                |- component_b   x1
+        #              |                                |- component_c   x3
+        #              |
+        #              |- kit_3 x1 --|- component_f x1
+        #              |             |- component_g x2
+        #              |
+        #              |- component_e x1
+        # Creating all components
+        component_a = create_product('Comp A')
+        component_b = create_product('Comp B')
+        component_c = create_product('Comp C')
+        component_d = create_product('Comp D')
+        component_e = create_product('Comp E')
+        component_f = create_product('Comp F')
+        component_g = create_product('Comp G')
+        # Creating all kits
+        kit_1 = create_product('Kit 1')
+        kit_2 = create_product('Kit 2')
+        kit_3 = create_product('kit 3')
+        self.kit_parent = create_product('Kit Parent')
+        # Linking the kits and the components via some 'phantom' BoMs
+        bom_kit_1 = self.env['mrp.bom'].create({
+            'product_tmpl_id': kit_1.product_tmpl_id.id,
+            'product_qty': 1.0,
+            'type': 'phantom'})
+        BomLine = self.env['mrp.bom.line']
+        BomLine.create({
+            'product_id': component_a.id,
+            'product_qty': 2.0,
+            'bom_id': bom_kit_1.id})
+        BomLine.create({
+            'product_id': component_b.id,
+            'product_qty': 1.0,
+            'bom_id': bom_kit_1.id})
+        BomLine.create({
+            'product_id': component_c.id,
+            'product_qty': 3.0,
+            'bom_id': bom_kit_1.id})
+        bom_kit_2 = self.env['mrp.bom'].create({
+            'product_tmpl_id': kit_2.product_tmpl_id.id,
+            'product_qty': 1.0,
+            'type': 'phantom'})
+        BomLine.create({
+            'product_id': component_d.id,
+            'product_qty': 1.0,
+            'bom_id': bom_kit_2.id})
+        BomLine.create({
+            'product_id': kit_1.id,
+            'product_qty': 2.0,
+            'bom_id': bom_kit_2.id})
+        bom_kit_parent = self.env['mrp.bom'].create({
+            'product_tmpl_id': self.kit_parent.product_tmpl_id.id,
+            'product_qty': 1.0,
+            'type': 'phantom'})
+        BomLine.create({
+            'product_id': component_e.id,
+            'product_qty': 1.0,
+            'bom_id': bom_kit_parent.id})
+        BomLine.create({
+            'product_id': kit_2.id,
+            'product_qty': 2.0,
+            'bom_id': bom_kit_parent.id})
+        bom_kit_3 = self.env['mrp.bom'].create({
+            'product_tmpl_id': kit_3.product_tmpl_id.id,
+            'product_qty': 1.0,
+            'type': 'phantom'})
+        BomLine.create({
+            'product_id': component_f.id,
+            'product_qty': 1.0,
+            'bom_id': bom_kit_3.id})
+        BomLine.create({
+            'product_id': component_g.id,
+            'product_qty': 2.0,
+            'bom_id': bom_kit_3.id})
+        BomLine.create({
+            'product_id': kit_3.id,
+            'product_qty': 1.0,
+            'bom_id': bom_kit_parent.id})
+
+        # We create an 'immediate transfer' receipt for x3 kit_parent
+        self.test_partner = self.env['res.partner'].create({
+            'name': 'Notthat Guyagain',
+            'supplier': True,
+            'customer': True,
+        })
+        self.test_supplier = self.env['stock.location'].create({
+            'name': 'supplier',
+            'usage': 'supplier',
+            'location_id': self.env.ref('stock.stock_location_stock').id,
+        })
+
+        self.expected_quantities = {
+            component_a: 24,
+            component_b: 12,
+            component_c: 36,
+            component_d: 6,
+            component_e: 3,
+            component_f: 3,
+            component_g: 6
+        }
+
+    def test_kit_immediate_transfer(self):
+        """ Make sure a kit is split in the corrects quantity_done by components in case of an
+        immediate transfer.
+        """
+        picking = self.env['stock.picking'].create({
+            'location_id': self.test_supplier.id,
+            'location_dest_id': self.warehouse_1.wh_input_stock_loc_id.id,
+            'partner_id': self.test_partner.id,
+            'picking_type_id': self.env.ref('stock.picking_type_in').id,
+            'immediate_transfer': True
+        })
+        move_receipt_1 = self.env['stock.move'].create({
+            'name': self.kit_parent.name,
+            'product_id': self.kit_parent.id,
+            'quantity_done': 3,
+            'product_uom': self.kit_parent.uom_id.id,
+            'picking_id': picking.id,
+            'picking_type_id': self.env.ref('stock.picking_type_in').id,
+            'location_id':  self.test_supplier.id,
+            'location_dest_id': self.warehouse_1.wh_input_stock_loc_id.id,
+        })
+        picking.button_validate()
+
+        # We check that the picking has the correct quantities after its move were splitted.
+        self.assertEquals(len(picking.move_lines), 7)
+        for move_line in picking.move_lines:
+            self.assertEquals(move_line.quantity_done, self.expected_quantities[move_line.product_id])
+
+    def test_kit_planned_transfer(self):
+        """ Make sure a kit is split in the corrects product_qty by components in case of a
+        planned transfer.
+        """
+        picking = self.env['stock.picking'].create({
+            'location_id': self.test_supplier.id,
+            'location_dest_id': self.warehouse_1.wh_input_stock_loc_id.id,
+            'partner_id': self.test_partner.id,
+            'picking_type_id': self.env.ref('stock.picking_type_in').id,
+            'immediate_transfer': False,
+        })
+        move_receipt_1 = self.env['stock.move'].create({
+            'name': self.kit_parent.name,
+            'product_id': self.kit_parent.id,
+            'product_uom_qty': 3,
+            'product_uom': self.kit_parent.uom_id.id,
+            'picking_id': picking.id,
+            'picking_type_id': self.env.ref('stock.picking_type_in').id,
+            'location_id':  self.test_supplier.id,
+            'location_dest_id': self.warehouse_1.wh_input_stock_loc_id.id,
+        })
+        picking.action_confirm()
+
+        # We check that the picking has the correct quantities after its move were splitted.
+        self.assertEquals(len(picking.move_lines), 7)
+        for move_line in picking.move_lines:
+            self.assertEquals(move_line.product_qty, self.expected_quantities[move_line.product_id])
