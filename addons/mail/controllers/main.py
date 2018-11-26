6d32d2d1b3427afad2bfa1c2631521cae61c1342
ThanhDodeurOdoo <tso@odoo.com>
2018-11-26 11:08:01 +0100

    [REF] base, *: refactor binary_content
    *: tools, web, website, website_forum, mail, im_livechat
    
    This commit refactors ir_http to make it more readable
    and flexible.
    
    Move the resize function of web/image to odoo.tools
    
    Co-authored-by: XavierDo <xdo@odoo.com>
    Co-authored-by: Antony Lesuisse <al@openerp.com>
    
    closes: #28563
    task: #1908896

--- addons/mail/controllers/main.py
+++ addons/mail/controllers/main.py
@@ -9,7 +9,6 @@ import werkzeug
 from werkzeug import url_encode
 
 from odoo import api, http, registry, SUPERUSER_ID, _
-from odoo.addons.web.controllers.main import binary_content
 from odoo.exceptions import AccessError
 from odoo.http import request
 from odoo.tools import consteq
@@ -216,7 +215,8 @@ class MailController(http.Controller):
                 # if the current user has access to the document, get the partner avatar as sudo()
                 request.env[res_model].browse(res_id).check_access_rule('read')
                 if partner_id in request.env[res_model].browse(res_id).sudo().exists().message_ids.mapped('author_id').ids:
-                    status, headers, _content = binary_content(model='res.partner', id=partner_id, field='image_medium', default_mimetype='image/png', env=request.env(user=SUPERUSER_ID))
+                    status, headers, _content = request.env['ir.http'].sudo().binary_content(
+                        model='res.partner', id=partner_id, field='image_medium', default_mimetype='image/png')
                     # binary content return an empty string and not a placeholder if obj[field] is False
                     if _content != '':
                         content = _content
--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -36,7 +36,7 @@ import odoo
 import odoo.modules.registry
 from odoo.api import call_kw, Environment
 from odoo.modules import get_resource_path
-from odoo.tools import crop_image, topological_sort, html_escape, pycompat
+from odoo.tools import limited_image_resize, topological_sort, html_escape, pycompat
 from odoo.tools.mimetypes import guess_mimetype
 from odoo.tools.translate import _
 from odoo.tools.misc import str2bool, xlwt, file_open
@@ -423,16 +423,6 @@ def xml2json_from_elementtree(el, preserve_whitespaces=False):
             kids.append(kid.tail)
     res["children"] = kids
     return res
-
-def binary_content(xmlid=None, model='ir.attachment', id=None, field='datas', unique=False,
-                   filename=None, filename_field='datas_fname', download=False, mimetype=None,
-                   default_mimetype='application/octet-stream', related_id=None, access_mode=None, access_token=None,
-                   env=None):
-    return request.registry['ir.http'].binary_content(
-        xmlid=xmlid, model=model, id=id, field=field, unique=unique, filename=filename,
-        filename_field=filename_field, download=download, mimetype=mimetype,
-        default_mimetype=default_mimetype, related_id=related_id, access_mode=access_mode, access_token=access_token,
-        env=env)
 
 #----------------------------------------------------------
 # Odoo Web web Controllers
@@ -1029,18 +1019,14 @@ class Binary(http.Controller):
         '/web/content/<string:model>/<int:id>/<string:field>/<string:filename>'], type='http', auth="public")
     def content_common(self, xmlid=None, model='ir.attachment', id=None, field='datas',
                        filename=None, filename_field='datas_fname', unique=None, mimetype=None,
-                       download=None, data=None, token=None, access_token=None, related_id=None, access_mode=None,
-                       **kw):
-        status, headers, content = binary_content(
+                       download=None, data=None, token=None, access_token=None, **kw):
+
+        status, headers, content = request.env['ir.http'].binary_content(
             xmlid=xmlid, model=model, id=id, field=field, unique=unique, filename=filename,
-            filename_field=filename_field, download=download, mimetype=mimetype,
-            access_token=access_token, related_id=related_id, access_mode=access_mode)
-        if status == 304:
-            response = werkzeug.wrappers.Response(status=status, headers=headers)
-        elif status == 301:
-            return werkzeug.utils.redirect(content, code=301)
-        elif status != 200:
-            response = request.not_found()
+            filename_field=filename_field, download=download, mimetype=mimetype, access_token=access_token)
+
+        if status != 200:
+            return request.env['ir.http']._response_by_status(status, headers, content)
         else:
             content_base64 = base64.b64decode(content)
             headers.append(('Content-Length', len(content_base64)))
@@ -1068,39 +1054,18 @@ class Binary(http.Controller):
         '/web/image/<int:id>-<string:unique>/<int:width>x<int:height>/<string:filename>'], type='http', auth="public")
     def content_image(self, xmlid=None, model='ir.attachment', id=None, field='datas',
                       filename_field='datas_fname', unique=None, filename=None, mimetype=None,
-                      download=None, width=0, height=0, crop=False, related_id=None, access_mode=None,
-                      access_token=None, avoid_if_small=False, upper_limit=False, signature=False):
-        status, headers, content = binary_content(
+                      download=None, width=0, height=0, crop=False, access_token=None, avoid_if_small=False,
+                      upper_limit=False, **kw):
+        status, headers, content = request.env['ir.http'].binary_content(
             xmlid=xmlid, model=model, id=id, field=field, unique=unique, filename=filename,
             filename_field=filename_field, download=download, mimetype=mimetype,
-            default_mimetype='image/png', related_id=related_id, access_mode=access_mode, access_token=access_token)
-        if status == 304:
-            return werkzeug.wrappers.Response(status=304, headers=headers)
-        elif status == 301:
-            return werkzeug.utils.redirect(content, code=301)
-        elif status != 200 and download:
-            return request.not_found()
+            default_mimetype='image/png', access_token=access_token)
 
-        if headers and dict(headers).get('Content-Type', '') == 'image/svg+xml':  # we shan't resize svg images
-            height = 0
-            width = 0
-        else:
-            height = int(height or 0)
-            width = int(width or 0)
+        if status != 200 and download:
+            return request.env['ir.http']._response_by_status(status, headers, content)
 
-        if crop and (width or height):
-            content = crop_image(content, type='center', size=(width, height), ratio=(1, 1))
-
-        elif content and (width or height):
-            if not upper_limit:
-                # resize maximum 500*500
-                if width > 500:
-                    width = 500
-                if height > 500:
-                    height = 500
-            content = odoo.tools.image_resize_image(base64_source=content, size=(width or None, height or None),
-                                                    encoding='base64', upper_limit=upper_limit,
-                                                    avoid_if_small=avoid_if_small)
+        content = limited_image_resize(
+            content, width=width, height=height, crop=crop, upper_limit=upper_limit, avoid_if_small=avoid_if_small)
 
         if content:
             image_base64 = base64.b64decode(content)
