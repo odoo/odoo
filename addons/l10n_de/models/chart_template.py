5e9ed18ef3590579dde4bfbc09f8fa983c96c389
Olivier Colson <oco@odoo.com>
2018-11-26 09:51:07 +0000

    [FIX] l10n_de, l10n_mx, l10n_nl: isolate account.tag creation by country
    Before that, the specific account tags created in l10n modules were not only assigned when the company was in the right country.
    
    closes odoo/odoo#29030

--- addons/l10n_de/models/chart_template.py
+++ addons/l10n_de/models/chart_template.py
@@ -8,9 +8,10 @@ class AccountChartTemplate(models.Model):
     @api.model
     def _prepare_transfer_account_for_direct_creation(self, name, company):
         res = super(AccountChartTemplate, self)._prepare_transfer_account_for_direct_creation(name, company)
-        xml_id = self.env.ref('l10n_de.tag_de_asset_bs_B_III_2').id
-        res.setdefault('tag_ids', [])
-        res['tag_ids'].append((4, xml_id))
+        if company.country_id.code == 'DE':
+            xml_id = self.env.ref('l10n_de.tag_de_asset_bs_B_III_2').id
+            res.setdefault('tag_ids', [])
+            res['tag_ids'].append((4, xml_id))
         return res
 
     # Write paperformat and report template used on company
