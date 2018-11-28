a4221ac650f6d6fa62e667964d36d622d94caa93
Lucas Perais (lpe) <lpe@odoo.com>
2018-11-28 15:26:48 +0000

    [FIX] base,web_editor: get_view_id should return singleton
    Because of 3f948cd48423aa61b5dd133c424f085861a9c47a
    and since the key of an ir.ui.view cannot be unique
    (multiwebsite), duplicating views made several parts of Odoo crash
    
    After this commit, we limit to one the views that are searched with their key
    Responsibility is given to the user to change the priority and/or the key
    in order to predict which view will be taken
    
    OPW 1913335,1913100,1913186,1913112,1913240
    
    closes odoo/odoo#29105

--- addons/web_editor/models/ir_ui_view.py
+++ addons/web_editor/models/ir_ui_view.py
@@ -202,7 +202,7 @@ class IrUiView(models.Model):
     @api.model
     def _view_obj(self, view_id):
         if isinstance(view_id, pycompat.string_types):
-            return self.search([('key', '=', view_id)]) or self.env.ref(view_id)
+            return self.search([('key', '=', view_id)], limit=1) or self.env.ref(view_id)
         elif isinstance(view_id, pycompat.integer_types):
             return self.browse(view_id)
         # assume it's already a view object (WTF?)
