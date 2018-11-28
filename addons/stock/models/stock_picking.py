3e33b3ae02f82047a59a6dd1a916e610515f9575
Nimesh Jethva <nje@odoo.com>
2018-11-28 11:03:12 +0000

    [REF] stock: visibility of the scrap button/moves
    * do not display the scrap button in receipt (we don't want to allow
    products not coming from your stock)
    * no scrap button when any picking is done
    * don't display the scrap moves, the user needs to go to the stat button
    
    Task ID 1913275
    
    closes odoo/odoo#29097

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -640,7 +640,7 @@ class Picking(models.Model):
                                 move_ids_without_package |= move
                         else:
                             move_ids_without_package |= move
-            picking.move_ids_without_package = move_ids_without_package
+            picking.move_ids_without_package = move_ids_without_package.filtered(lambda ml: ml.scrapped == False)
 
     def _set_move_without_package(self):
         self.move_lines |= self.move_ids_without_package
