ffdfadb8147309f3436dae0ad9d720b9020f3b05
Lucas Perais (lpe) <lpe@odoo.com>
2018-11-30 15:39:30 +0000

    [FIX] account: randomize invoice reference if no match
    Take explanation of
    commit ab6043a
    but this time, have a suffix with a flavor of %(year)s
    
    Before this commit, validating the invoice crashes
    
    After this commit it doesn't.
    
    OPW 1912954
    
    closes odoo/odoo#29160

--- addons/account/models/account_invoice.py
+++ addons/account/models/account_invoice.py
@@ -397,7 +397,12 @@ class AccountInvoice(models.Model):
         if self.company_id.invoice_reference_type == 'invoice_number':
             seq_suffix = self.journal_id.sequence_id.suffix or ''
             regex_number = '.*?([0-9]+)%s$' % seq_suffix
-            identification_number = int(re.match(regex_number, self.number).group(1))
+            exact_match = re.match(regex_number, self.number)
+            if exact_match:
+                identification_number = int(exact_match.group(1))
+            else:
+                ran_num = str(uuid.uuid4().int)
+                identification_number = int(ran_num[:5] + ran_num[-5:])
             prefix = self.number
         else:
             #self.company_id.invoice_reference_type == 'partner'
