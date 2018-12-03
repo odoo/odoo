f867f9a51f1121d74bf3435b03ed53a730f05942
Juhil Somaiya <jso@odoo.com>
2018-12-03 17:44:24 +0530

    [FIX] manufacturing: bom structure and cost report
    	- Before this commit, BOM structure and cost report only prints quantity 1 though we change quantity value
    	- After this commit, it will print dynamic quantity value
    
    This commit is related to issue: https://github.com/odoo/odoo/issues/27549

--- addons/mrp/report/mrp_report_bom_structure.py
+++ addons/mrp/report/mrp_report_bom_structure.py
@@ -14,13 +14,13 @@ class ReportBomStructure(models.AbstractModel):
         docs = []
         for bom_id in docids:
             bom = self.env['mrp.bom'].browse(bom_id)
-            variant = data and data.get('variant')
+            variant = data and data.get('variant') and int(data.get('variant'))
             candidates = variant and self.env['product.product'].browse(variant) or bom.product_tmpl_id.product_variant_ids
             for product_variant_id in candidates:
                 if data and data.get('childs'):
                     doc = self._get_pdf_line(bom_id, product_id=product_variant_id, qty=float(data.get('quantity')), child_bom_ids=json.loads(data.get('childs')))
                 else:
-                    doc = self._get_pdf_line(bom_id, product_id=product_variant_id, unfolded=True)
+                    doc = self._get_pdf_line(bom_id, product_id=product_variant_id, qty=float(data.get('quantity')), unfolded=True)
                 doc['report_type'] = 'pdf'
                 doc['report_structure'] = data and data.get('report_type') or 'all'
                 docs.append(doc)
@@ -28,7 +28,7 @@ class ReportBomStructure(models.AbstractModel):
                 if data and data.get('childs'):
                     doc = self._get_pdf_line(bom_id, qty=float(data.get('quantity')), child_bom_ids=json.loads(data.get('childs')))
                 else:
-                    doc = self._get_pdf_line(bom_id, unfolded=True)
+                    doc = self._get_pdf_line(bom_id, qty=float(data.get('quantity')), unfolded=True)
                 doc['report_type'] = 'pdf'
                 doc['report_structure'] = data and data.get('report_type') or 'all'
                 docs.append(doc)
