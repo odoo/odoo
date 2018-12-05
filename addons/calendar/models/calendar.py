8fe1af3ab6c2f0b5a650a5e5c12e002412fb934f
luisiniguezh <luis.iniguezh@gmail.com>
2018-12-05 15:27:34 -0600

    removing unnecessary call to api_fields

--- addons/calendar/models/calendar.py
+++ addons/calendar/models/calendar.py
@@ -1552,7 +1552,7 @@ class Meeting(models.Model):
     @api.model
     def get_time_dst_delta(self, id, tz, record):
         master_id = self.browse(id)
-        master_dst = tz.localize(api_fields.Datetime.from_string(master_id.start_datetime)).dst()
+        master_dst = tz.localize(fields.Datetime.from_string(master_id.start_datetime)).dst()
         current_dst = tz.localize(datetime.strptime(record['id'].split('-')[1], '%Y%m%d%H%M%S')).dst()
         if master_dst and not current_dst:
             delta = timedelta(hours=1)
