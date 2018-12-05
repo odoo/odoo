28e9d20d98adcdd76028d538c2cad1474627be8c
Thibault Delavallée <tde@odoo.com>
2018-12-05 10:51:42 +0100

    [FIX] survey: fix base URLs for survey tours tests
    Currently surveys tours are launched starting from / . However on this page
    survey assets are not launched meaning ready code for survey tours is never
    true.
    
    Correctly setting base URL to be on a page loading survey assets unlocks the
    ready code computation and allow to launch tests from command line notably.
    
    This commit is related to task ID 1915356 and PR #29264.

--- addons/survey/tests/test_survey_ui.py
+++ addons/survey/tests/test_survey_ui.py
@@ -1,14 +1,15 @@
-import odoo.tests
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import odoo.tests
+
 
 @odoo.tests.common.tagged('post_install','-at_install')
 class TestUi(odoo.tests.HttpCase):
 
     def test_01_admin_survey_tour(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready", login="admin")
+        self.phantom_js("/survey/start/user-feedback-form-1/phantom", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready", login="admin")
 
     def test_02_demo_survey_tour(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready", login="demo")
+        self.phantom_js("/survey/start/user-feedback-form-1/phantom", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready", login="demo")
 
     def test_03_public_survey_tour(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready")
+        self.phantom_js("/survey/start/user-feedback-form-1/phantom", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready")
