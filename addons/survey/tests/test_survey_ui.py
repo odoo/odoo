c1a6d711ed5b057dc48c3ef0e28b329718db29a8
Thibault Delavallée <tde@odoo.com>
2018-12-05 18:04:21 +0100

    [IMP] survey: improve demo survey
    Purpose of this commit is to provide a new simpler survey as demo data in
    survey application. Current survey is about Odoo use which is not coherent
    with the demo data set used currently in Odoo. It is now a survey about
    ecommerce use with an example of each different questions.
    
    Survey tour is updated accordingly. It now uses a standard routing instead
    of the test route. Indeed purpose of tests is to use real life use case
    not corner case like testing capabilities of survey application.
    
    This commit is linked to task ID 1912228 and PR #29303.

--- addons/survey/tests/test_survey_ui.py
+++ addons/survey/tests/test_survey_ui.py
@@ -6,10 +6,10 @@ import odoo.tests
 class TestUi(odoo.tests.HttpCase):
 
     def test_01_admin_survey_tour(self):
-        self.phantom_js("/survey/start/user-feedback-form-1/phantom", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready", login="admin")
+        self.phantom_js("/survey/start/user-feedback-form-1", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready", login="admin")
 
     def test_02_demo_survey_tour(self):
-        self.phantom_js("/survey/start/user-feedback-form-1/phantom", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready", login="demo")
+        self.phantom_js("/survey/start/user-feedback-form-1", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready", login="demo")
 
     def test_03_public_survey_tour(self):
-        self.phantom_js("/survey/start/user-feedback-form-1/phantom", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready")
+        self.phantom_js("/survey/start/user-feedback-form-1", "odoo.__DEBUG__.services['web_tour.tour'].run('test_survey')", "odoo.__DEBUG__.services['web_tour.tour'].tours.test_survey.ready")
