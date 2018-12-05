7b636f33423fedfec29861c0ad5d3a9b4346994d
Cedric Snauwaert <csn@openerp.com>
2018-12-05 12:15:38 +0000

    [FIX] account: manual reconciliation with writeoff and currency was wrong
    When trying to reconcile the following line with the manual reconciliation widget
    (company currency in EUR)
    
    Debit Credit Amount_currency Date        Currency
    13.11    0          0        10/26/2018  /
    0       34.16      -39       11/09/2018  USD
    
    And a write off with the following value:
    Debit: 21.05
    
    The write-off created was not with the correct debit and the entries were not totally reconciled.
    
    The reason being that the method was converting the 21.05 with the USD rate of today and changed the debit.
    
    We should never convert debit/credit as debit/credit in the manual reconciliation widget is always displayed in company currency. However if all entries are with the same currency, the writeoff should also have that currency set.
    This commit fixes this behavior.

--- addons/account/models/reconciliation_widget.py
+++ addons/account/models/reconciliation_widget.py
@@ -698,13 +698,15 @@ class AccountReconciliation(models.AbstractModel):
         # Create writeoff move lines
         if len(new_mv_line_dicts) > 0:
             company_currency = account_move_line[0].account_id.company_id.currency_id
-            company = account_move_line[0].account_id.company_id
-            date = fields.Date.today()
-            writeoff_currency = account_move_line[0].currency_id or company_currency
+            same_currency = False
+            currencies = list(set([aml.currency_id or company_currency for aml in account_move_line]))
+            if len(currencies) == 1 and currencies[0] != company_currency:
+                same_currency = True
+            # We don't have to convert debit/credit to currency as all values in the reconciliation widget are displayed in company currency
+            # If all the lines are in the same currency, create writeoff entry with same currency also
             for mv_line_dict in new_mv_line_dicts:
-                if writeoff_currency != company_currency:
-                    mv_line_dict['debit'] = writeoff_currency._convert(mv_line_dict['debit'], company_currency, company, date)
-                    mv_line_dict['credit'] = writeoff_currency._convert(mv_line_dict['credit'], company_currency, company, date)
+                if not same_currency:
+                    mv_line_dict['amount_currency'] = False
                 writeoff_lines += account_move_line._create_writeoff([mv_line_dict])
 
             (account_move_line + writeoff_lines).reconcile()
