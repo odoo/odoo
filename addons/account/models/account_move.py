2f2fe03ec3c710462ecab96f8899c33f11854a1d
qdp-odoo <qdp@odoo.com>
2018-12-05 13:58:46 +0000

    [REF] account: refactoring of the invoice validation
    Better split of invoice_validate() and action_move_create() functions: the later now only deals with the account.move creation while the former assigns missing values (dates) and gets its number. The move now always receives its name from the invoice, which means there's no need anymore of the 'invoice' arg in account.move post() method.
    
    We now also put the origin of the invoice in the move ref, in order to allow searching on that reference in the bank statement reconciliation widget.
    
    closes odoo/odoo#29343

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -282,7 +282,7 @@ class AccountMove(models.Model):
         return res
 
     @api.multi
-    def post(self, invoice=False):
+    def post(self):
         self._post_validate()
         for move in self:
             move.line_ids.create_analytic_lines()
@@ -290,20 +290,11 @@ class AccountMove(models.Model):
                 new_name = False
                 journal = move.journal_id
 
-                if invoice and invoice.move_name and invoice.move_name != '/':
-                    new_name = invoice.move_name
+                if journal.sequence_id:
+                    sequence = journal.sequence_id
+                    new_name = sequence.with_context(ir_sequence_date=move.date).next_by_id()
                 else:
-                    if journal.sequence_id:
-                        # If invoice is actually refund and journal has a refund_sequence then use that one or use the regular one
-                        sequence = journal.sequence_id
-                        if invoice and invoice.type in ['out_refund', 'in_refund'] and journal.refund_sequence:
-                            if not journal.refund_sequence_id:
-                                raise UserError(_('Please define a sequence for the credit notes'))
-                            sequence = journal.refund_sequence_id
-
-                        new_name = sequence.with_context(ir_sequence_date=move.date).next_by_id()
-                    else:
-                        raise UserError(_('Please define a sequence on the journal.'))
+                    raise UserError(_('Please define a sequence on the journal.'))
 
                 if new_name:
                     move.name = new_name
