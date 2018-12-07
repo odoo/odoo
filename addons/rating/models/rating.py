d86dcafea6d6a4a36783c989df6ae44098587eab
Denis Ledoux <dle@odoo.com>
2018-12-07 14:26:44 +0100

    [FIX] project, rating: project percentage satisfaction compute performance
    This is basically a backport of revision
    47479401cb3f685788d76abe01d3d2bc21ff11e3
    to gain the performance on the project
    percentage satisaction computation
    
    but without introducing the new parent mixin,
    which would not be a stable change.

--- addons/rating/models/rating.py
+++ addons/rating/models/rating.py
@@ -3,6 +3,8 @@
 import base64
 import uuid
 
+from datetime import timedelta
+
 from odoo import api, fields, models, tools, _
 
 from odoo.modules.module import get_resource_path
@@ -352,3 +354,31 @@ class RatingMixin(models.AbstractModel):
         for rate in data['repartition']:
             result['percent'][rate] = (data['repartition'][rate] * 100) / data['total'] if data['total'] > 0 else 0
         return result
+
+    @api.model
+    def _compute_parent_rating_percentage_satisfaction(self, parent_records, rating_satisfaction_days=None):
+        # build domain and fetch data
+        domain = [('parent_res_model', '=', parent_records._name), ('parent_res_id', 'in', parent_records.ids), ('rating', '>=', 1), ('consumed', '=', True)]
+        if rating_satisfaction_days:
+            domain += [('create_date', '>=', fields.Datetime.to_string(fields.datetime.now() - timedelta(days=rating_satisfaction_days)))]
+        data = self.env['rating.rating'].read_group(domain, ['parent_res_id', 'rating'], ['parent_res_id', 'rating'], lazy=False)
+
+        # get repartition of grades per parent id
+        default_grades = {'great': 0, 'okay': 0, 'bad': 0}
+        grades_per_parent = dict((parent_id, dict(default_grades)) for parent_id in parent_records.ids)  # map: {parent_id: {'great': 0, 'bad': 0, 'ok': 0}}
+        for item in data:
+            parent_id = item['parent_res_id']
+            rating = item['rating']
+            if rating >= RATING_LIMIT_SATISFIED:
+                grades_per_parent[parent_id]['great'] += item['__count']
+            elif rating > RATING_LIMIT_OK:
+                grades_per_parent[parent_id]['okay'] += item['__count']
+            else:
+                grades_per_parent[parent_id]['bad'] += item['__count']
+
+        # compute percentage per parent
+        res = {}
+        for record in parent_records:
+            repartition = grades_per_parent.get(record.id)
+            res[record.id] = repartition['great'] * 100 / sum(repartition.values()) if sum(repartition.values()) else -1
+        return res
