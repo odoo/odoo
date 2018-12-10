88bd53b36574c6ab8bf42c7d9dd7fdad4afb8b80
Denis Ledoux <dle@odoo.com>
2018-12-10 17:10:36 +0100

    [IMP] web: read_progress_bar, use read group when possible
    Considering the nature of the query,
    which basically does a field sum (an aggregate) on a bunch of records
    (e.g. ALL your assigned leads, or all leads if you removed the "My leads" filter)
    it's more efficient to use read_group (at lot more efficient).
    
    It wasn't, because `read_group` cannot be used on fields
    that aren't stored, and the main subjet to have created
    `read_progress_bar` was to aggregate the `activity_state`
    of the records, which is not a stored field.
    
    Therefore, `search_read` has been used,
    and then the aggregate was done in post-process.
    
    This revision aims to use `read_group` when possible,
    to make this method more efficient when possible
    (e.g. for the `kanban_state` field of `project.task`)
    
    Then, either `activity_state` should be stored,
    to allow the read_group, either its `read_group`
    should be overriden to allow it, by doing
    pure sql for instance.

--- addons/web/models/models.py
+++ addons/web/models/models.py
@@ -25,11 +25,28 @@ class Base(models.AbstractModel):
         :return a dictionnary mapping group_by values to dictionnaries mapping
                 progress bar field values to the related number of records
         """
+        gb = group_by.partition(':')[0]
+        field_type = self._fields[gb].type
+        if field_type == 'selection':
+            selection_labels = dict(self.fields_get()[gb]['selection'])
+
+        progress_bar_field = self._fields[progress_bar['field']].base_field
+        if progress_bar_field.store and progress_bar_field.column_type:
+            groups = self.read_group(domain, [progress_bar['field']], [group_by, progress_bar['field']], lazy=False)
+            data = {}
+            for g in groups:
+                if g[progress_bar['field']] in progress_bar['colors']:
+                    group_by_value = g[group_by]
+                    if field_type == 'selection':
+                        group_by_value = selection_labels[group_by_value]
+                    if type(group_by_value) == tuple:
+                        group_by_value = group_by_value[1]._value
+                    data.setdefault(group_by_value, {key: 0 for key in progress_bar['colors']})[g[progress_bar['field']]] = g['__count']
+            return data
 
         # Workaround to match read_group's infrastructure
         # TO DO in master: harmonize this function and readgroup to allow factorization
         group_by_modifier = group_by.partition(':')[2] or 'month'
-        group_by = group_by.partition(':')[0]
         display_date_formats = {
             'day': 'dd MMM yyyy',
             'week': "'W'w YYYY",
@@ -37,15 +54,12 @@ class Base(models.AbstractModel):
             'quarter': 'QQQ yyyy',
             'year': 'yyyy'}
 
-        records_values = self.search_read(domain or [], [progress_bar['field'], group_by])
+        records_values = self.search_read(domain or [], [progress_bar['field'], gb])
 
         data = {}
-        field_type = self._fields[group_by].type
-        if field_type == 'selection':
-            selection_labels = dict(self.fields_get()[group_by]['selection'])
 
         for record_values in records_values:
-            group_by_value = record_values[group_by]
+            group_by_value = record_values[gb]
 
             # Again, imitating what _read_group_format_result and _read_group_prepare_data do
             if group_by_value and field_type in ['date', 'datetime']:
