257e77ed6906043f5a608ec36b00cd75ec19b265
David Beguin <dbe@odoo.com>
2018-12-11 15:30:36 +0100

    [IMP] sale_crm : review quotations and sales smart buttons on opportunity
    various improvements and fixes:
        - View quotations was incorrect
            -> should be real quotation view and not SO view
        - Cotation count was incorrect
            -> Should only include Quotations with state='draft' or state='sent'
        - SO button should be visible if number of SO > 0 and not the total amount
    
    Task ID : 1908622
    Closes PR #29461

--- addons/sale_crm/models/crm_lead.py
+++ addons/sale_crm/models/crm_lead.py
@@ -9,24 +9,28 @@ from odoo import api, fields, models
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    sale_amount_total = fields.Monetary(compute='_compute_sale_amount_total', string="Sum of Orders", help="Untaxed Total of Confirmed Orders", currency_field='company_currency')
-    sale_number = fields.Integer(compute='_compute_sale_amount_total', string="Number of Quotations")
+    sale_amount_total = fields.Monetary(compute='_compute_sale_data', string="Sum of Orders", help="Untaxed Total of Confirmed Orders", currency_field='company_currency')
+    quotation_count = fields.Integer(compute='_compute_sale_data', string="Number of Quotations")
+    sale_order_count = fields.Integer(compute='_compute_sale_data', string="Number of Sale Orders")
     order_ids = fields.One2many('sale.order', 'opportunity_id', string='Orders')
 
-    @api.depends('order_ids')
-    def _compute_sale_amount_total(self):
+    @api.depends('order_ids.state', 'order_ids.currency_id', 'order_ids.amount_untaxed', 'order_ids.date_order', 'order_ids.company_id')
+    def _compute_sale_data(self):
         for lead in self:
             total = 0.0
-            nbr = 0
+            quotation_cnt = 0
+            sale_order_cnt = 0
             company_currency = lead.company_currency or self.env.user.company_id.currency_id
             for order in lead.order_ids:
-                if order.state in ('draft', 'sent', 'sale'):
-                    nbr += 1
+                if order.state in ('draft', 'sent'):
+                    quotation_cnt += 1
                 if order.state not in ('draft', 'sent', 'cancel'):
+                    sale_order_cnt += 1
                     total += order.currency_id._convert(
                         order.amount_untaxed, company_currency, order.company_id, order.date_order or fields.Date.today())
             lead.sale_amount_total = total
-            lead.sale_number = nbr
+            lead.quotation_count = quotation_cnt
+            lead.sale_order_count = sale_order_cnt
 
     @api.model
     def retrieve_sales_dashboard(self):
