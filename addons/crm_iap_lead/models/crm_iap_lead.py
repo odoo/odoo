8c8833e234fda2092720e48d46cba19aa3f490d0
Louis Baudoux <lba@odoo.com>
2018-12-17 16:19:11 +0100

    [WIP] lead mining

---
+++ addons/crm_iap_lead/models/crm_iap_lead.py
@@ -0,0 +1,63 @@
+from odoo import api, fields, models
+
+
+class IndustryTag(models.Model):
+    """ Industry Tags of Acquisition Rules """
+    _name = 'crm.iap.lead.industry'
+    _description = 'Industry Tag'
+
+    name = fields.Char(string='Tag Name', required=True, translate=True)
+    reveal_id = fields.Char(required=True)
+    color = fields.Integer(string='Color Index')
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', 'Tag name already exists!'),
+    ]
+
+
+class PeopleRole(models.Model):
+    """ CRM Reveal People Roles for People """
+    _name = 'crm.iap.lead.role'
+    _description = 'People Role'
+
+    name = fields.Char(string='Role Name', required=True, translate=True)
+    reveal_id = fields.Char(required=True)
+    color = fields.Integer(string='Color Index')
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', 'Role name already exists!'),
+    ]
+
+    @api.depends('name')
+    def name_get(self):
+        return [(role.id, role.name.replace('_', ' ').title()) for role in self]
+
+
+class PeopleSeniority(models.Model):
+    """ Seniority for People Rules """
+    _name = 'crm.iap.lead.seniority'
+    _description = 'People Seniority'
+
+    name = fields.Char(string='Name', required=True, translate=True)
+    reveal_id = fields.Char(required=True)
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', 'Name already exists!'),
+    ]
+
+    @api.depends('name')
+    def name_get(self):
+        return [(seniority.id, seniority.name.replace('_', ' ').title()) for seniority in self]
+
+
+class TechnologyTag(models.Model):
+    """ Technology Tags of Clearbit """
+    _name = 'crm.iap.lead.technology'
+    _description = 'Technology Tag'
+
+    name = fields.Char(string='Name', required=True, translate=True)
+    tech_tag = fields.Char(string="Tech Tag", required=True)
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', 'Technology tag name already exists!'),
+    ]
--- addons/crm_iap_lead/models/crm_lead.py
+++ addons/crm_iap_lead/models/crm_lead.py
@@ -7,7 +7,5 @@ from odoo import fields, models
 class Lead(models.Model):
     _inherit = 'crm.lead'
 
-    reveal_ip = fields.Char(string='IP Address')
-    reveal_iap_credits = fields.Integer(string='IAP Credits')
-    reveal_rule_id = fields.Many2one('crm.reveal.rule', string='Lead Generation Rule', index=True)
     reveal_id = fields.Char(string='Reveal ID', index=True)
+    lead_mining_request_id = fields.Many2one('crm.iap.lead.request', string='Lead Mining Request', index=True)
