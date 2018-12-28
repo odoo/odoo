765075ff475e0120d1c3f5564f2259c34d2386cc
Yannick Tivisse <yti@odoo.com>
2018-12-28 11:24:41 +0000

    [FIX] hr_holidays: Fix 6035112
    1/ self.date_from on a for loop on self won't work
    2/ When calling get_work_hours_count with self.date_from=False or self.date_to=False,
       this will lead to a traceback when looking the timezone on the date.
    
    Closes #29697

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -363,8 +363,11 @@ class HolidaysRequest(models.Model):
     def _compute_number_of_hours_display(self):
         for holiday in self:
             calendar = holiday.employee_id.resource_calendar_id or self.env.user.company_id.resource_calendar_id
-            number_of_hours = calendar.get_work_hours_count(self.date_from, self.date_to)
-            holiday.number_of_hours_display = number_of_hours or (holiday.number_of_days * HOURS_PER_DAY)
+            if holiday.date_from and holiday.date_to:
+                number_of_hours = calendar.get_work_hours_count(holiday.date_from, holiday.date_to)
+                holiday.number_of_hours_display = number_of_hours or (holiday.number_of_days * HOURS_PER_DAY)
+            else:
+                holiday.number_of_hours_display = 0
 
     @api.multi
     @api.depends('state', 'employee_id', 'department_id')
