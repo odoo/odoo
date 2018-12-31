db14a37cd6ec4c5db300057be8e1a6d97ba0a615
Lucas Lefèvre <lul@odoo.com>
2018-12-31 13:31:21 +0000

    [FIX] hr_fleet: Fix access right on employee form
    Purpose
    ======
    
    Since commit 80db9b4 a user can't access an employee form if he is neither Fleet User nor Manager.
    => Access error: Document model: fleet.vehicle.assignation.log
    The reason is the computation of the number of vehicule logs.
    A regular user doesn't have access rights on this model.
    
    This commit fixes this.
    
    closes odoo/odoo#29835

--- addons/hr_fleet/models/employee.py
+++ addons/hr_fleet/models/employee.py
@@ -24,7 +24,7 @@ class Employee(models.Model):
 
     def _compute_employee_cars_count(self):
         driver_ids = (self.mapped('user_id.partner_id') | self.mapped('address_home_id')).ids
-        fleet_data = self.env['fleet.vehicle.assignation.log'].read_group(
+        fleet_data = self.env['fleet.vehicle.assignation.log'].sudo().read_group(
             domain=[('driver_id', 'in', driver_ids)], fields=['driver_id'], groupby=['driver_id'])
         mapped_data = dict([(m['driver_id'][0], m['driver_id_count']) for m in fleet_data])
         for employee in self:
