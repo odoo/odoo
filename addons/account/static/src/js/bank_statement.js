da6f4fd3b25f7e92604ecf5f61336ec11159228c
wan <wan@odoo.com>
2019-01-04 11:03:56 +0100

    [FIX] account: hide import button only on bank journals OPW 1920290 While v11 allows users the choice of creating or importing cash statements, v12 only allows users to create cash statements.
    This behavior was changed by OPW 1861619 and #93c92c72e84c8802af4c698ba0a681c25113cec4 but had some side effect on other journals. This commit fixes it

--- addons/account/static/src/js/bank_statement.js
+++ addons/account/static/src/js/bank_statement.js
@@ -7,7 +7,12 @@ odoo.define('account.bank_statement', function(require) {
     var includeDict = {
         renderButtons: function () {
             this._super.apply(this, arguments);
-            if (this.modelName === "account.bank.statement") {
+            if (this.modelName === "account.bank.statement" && 
+                this.searchView &&
+                this.searchView.action.env.context &&
+                this.searchView.action.env.context.journal_type &&
+                this.searchView.action.env.context.journal_type == "bank" ||
+                false) {
                 this.$buttons.find('button.o_button_import').hide();
             }
         }
