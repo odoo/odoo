f8193ae930e9e86dabf39db5a5817b0919c7089a
Swapnesh Shah <swapneshshah111@gmail.com>
2019-01-08 00:02:03 +0530

    [FIX] mrp_byproduct: Don't allow same product as byproduct

--- addons/mrp_byproduct/models/mrp_bom.py
+++ addons/mrp_byproduct/models/mrp_bom.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class MrpBom(models.Model):
@@ -10,3 +11,13 @@ class MrpBom(models.Model):
     _inherit = 'mrp.bom'
 
     sub_products = fields.One2many('mrp.subproduct', 'bom_id', 'Byproducts', copy=True)
+
+    @api.constrains('product_id', 'product_tmpl_id', 'sub_products')
+    def _check_byproduct_recursion(self):
+        for bom in self:
+            if bom.product_id:
+                if bom.sub_products.filtered(lambda x: x.product_id == bom.product_id):
+                    raise ValidationError(_('Byproduct %s should not be same as BoM product.') % bom.display_name)
+            else:
+                if bom.sub_products.filtered(lambda x: x.product_id.product_tmpl_id == bom.product_tmpl_id):
+                    raise ValidationError(_('Byproduct product %s should not be same as BoM product.') % bom.display_name)
