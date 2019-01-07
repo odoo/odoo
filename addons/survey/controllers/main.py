85117165875add7437fa0878692ae6d37b4d8273
Thibault Delavallée <tde@odoo.com>
2019-01-07 15:25:52 +0100

    [FIX] survey: allow debug parameter in testing route
    Adding kwargs in test route of surveys allow notably to avoid crashing when
    working in debug mode and testing surveys.

--- addons/survey/controllers/main.py
+++ addons/survey/controllers/main.py
@@ -49,7 +49,7 @@ class Survey(http.Controller):
         return None
 
     @http.route('/survey/test/<model("survey.survey"):survey>', type='http', auth='user', website=True)
-    def survey_test(self, survey, token=None):
+    def survey_test(self, survey, token=None, **kwargs):
         """ Test mode for surveys: create a test answer, only for managers or officers
         testing their surveys """
         if request.env.user.has_group('survey.group_survey_manager') or \
