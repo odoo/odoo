10384c689a27aae2101c60b6f3673ecf0810cfdb
qsm-odoo <qsm@odoo.com>
2019-01-07 17:38:11 +0000

    [IMP] website, *: optimize scss customizations
    * web_editor
    
    This commit's goal is to optimize the delay to make a scss customization
    thanks to the website customize dialog. Before this commit, the JS code
    took advantage of existing web_editor routes but this was far from
    efficient as this was done:
    
    1) PY: Load all scss files used in the page
    2) JS: Find the one we want to customize and adapt the scss content
    3) PY: Create the scss customization with the new content
    
    (So two RPC (including one very slow) and content building in
    javascript)
    
    After this commit, this is done:
    
    1) PY: Load the scss file being customized, adapt its content according
       to the new values and save the customization
    
    (So the whole logic is in python, requiring only one small RPC)
    
    This commit also take the opportunity to review the web_editor ace
    editor loading code (the logic is shared with website customizations).
    
    PR https://github.com/odoo/odoo/pull/29624
    task-1904244

--- addons/website/models/assets.py
+++ addons/website/models/assets.py
@@ -1,38 +1,55 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo.addons.web_editor.controllers.main import Web_Editor
-from odoo.http import request
+
+from odoo import models
 
 
-class Web_Editor(Web_Editor):
+class Assets(models.AbstractModel):
+    _inherit = 'web_editor.assets'
 
-    def _get_view_fields_to_read(self):
-        res = super(Web_Editor, self)._get_view_fields_to_read()
+    def get_view_fields_to_read(self):
+        res = super(Assets, self).get_view_fields_to_read()
         res.append('website_id')
         return res
 
-    def save_scss_or_js_view_hook(self):
-        res = super(Web_Editor, self).save_scss_or_js_view_hook()
-
-        website = request.env['website'].get_current_website()
-        if website:
-            res['website_id'] = website.id
-        return res
-
-    def save_scss_or_js_attachment_hook(self):
-        res = super(Web_Editor, self).save_scss_or_js_attachment_hook()
-
-        website = request.env['website'].get_current_website()
-        if website:
-            res['website_id'] = website.id
-        return res
-
-    def get_custom_attachment(self, custom_url, op='='):
-        website = request.env['website'].get_current_website()
-        res = super(Web_Editor, self).get_custom_attachment(custom_url, op=op)
+    def _get_custom_attachment(self, custom_url, op='='):
+        """
+        See web_editor.Assets._get_custom_attachment
+        Extend to only return the attachments related to the current website.
+        """
+        website = self.env['website'].get_current_website()
+        res = super(Assets, self)._get_custom_attachment(custom_url, op=op)
         return res.with_context(website_id=website.id).filtered(lambda x: not x.website_id or x.website_id == website)
 
-    def get_custom_view(self, custom_url, op='='):
-        website = request.env['website'].get_current_website()
-        res = super(Web_Editor, self).get_custom_view(custom_url, op=op)
+    def _get_custom_view(self, custom_url, op='='):
+        """
+        See web_editor.Assets._get_custom_view
+        Extend to only return the views related to the current website.
+        """
+        website = self.env['website'].get_current_website()
+        res = super(Assets, self)._get_custom_view(custom_url, op=op)
         return res.with_context(website_id=website.id).filter_duplicate()
+
+    def _save_asset_attachment_hook(self):
+        """
+        See web_editor.Assets._save_asset_attachment_hook
+        Extend to add website ID at attachment creation.
+        """
+        res = super(Assets, self)._save_asset_attachment_hook()
+
+        website = self.env['website'].get_current_website()
+        if website:
+            res['website_id'] = website.id
+        return res
+
+    def _save_asset_view_hook(self):
+        """
+        See web_editor.Assets._save_asset_view_hook
+        Extend to add website ID at view creation.
+        """
+        res = super(Assets, self)._save_asset_view_hook()
+
+        website = self.env['website'].get_current_website()
+        if website:
+            res['website_id'] = website.id
+        return res
