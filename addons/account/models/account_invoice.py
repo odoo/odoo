dc3e3d699b171b8c5c76c210618eca6dd13e7ab1
Jorge Pinna Puissant <jpp@odoo.com>
2019-01-08 10:48:20 +0000

    [FIX] account: error in Base amount tax opw-1917605
    Before this commit, when accounting is set to global rounding, the base
    amount per tax is not correctly calculated.
    
    Now, the Base amount per tax is calculated adding the rounded lines.
    
    To test this issue, you need to set to global rounding and create a
    customer invoice with the following lines :
    line 1 = Qty:40; unit price:2.27; tax:15%; discount: 10%
    line 2 = Qty:21; unit price:2.77; tax:15%; discount: 10%
    line 3 = Qty:21; unit price:2.77; tax:15%; discount: 10%
    
    When the invoce is printed, you would notice that the amount is 186.43
    and it should be 186.42.
    
    closes odoo/odoo#30021

--- addons/account/models/account_invoice.py
+++ addons/account/models/account_invoice.py
@@ -642,6 +642,7 @@ class AccountInvoice(models.Model):
     @api.multi
     def get_taxes_values(self):
         tax_grouped = {}
+        round_curr = self.currency_id.round
         for line in self.invoice_line_ids:
             price_unit = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
             taxes = line.invoice_line_tax_ids.compute_all(price_unit, self.currency_id, line.quantity, line.product_id, self.partner_id)['taxes']
@@ -651,9 +652,10 @@ class AccountInvoice(models.Model):
 
                 if key not in tax_grouped:
                     tax_grouped[key] = val
+                    tax_grouped[key]['base'] = round_curr(val['base'])
                 else:
                     tax_grouped[key]['amount'] += val['amount']
-                    tax_grouped[key]['base'] += val['base']
+                    tax_grouped[key]['base'] += round_curr(val['base'])
         return tax_grouped
 
     @api.multi
