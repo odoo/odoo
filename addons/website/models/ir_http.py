ec1f6f0a6a1dc2e17df9c00cf535a186ff17a433
Jeremy Kersten <jke@odoo.com>
2019-01-22 16:02:56 +0100

    [FIX] website: missing replace from type to redirect_type
    website.redirect feature is broken since commit 2924b77
    
    related to odoo/odoo#27599 and odoo/odoo#28112

--- addons/website/models/ir_http.py
+++ addons/website/models/ir_http.py
@@ -164,7 +164,7 @@ class Http(models.AbstractModel):
 
         redirect = cls._serve_redirect()
         if redirect:
-            return request.redirect(_build_url_w_params(redirect.url_to, request.params), code=redirect.type)
+            return request.redirect(_build_url_w_params(redirect.url_to, request.params), code=redirect.redirect_type)
 
         return False
 
