0baddee0d5825e7edb3b9efeeb786b658e6f4111
Nicolas Martinelli <nim@odoo.com>
2019-01-22 09:06:44 +0000

    [FIX] stock_account: Average Cost
    - Create a product where the associated product category has Costing
      Method "Average Cost (AVCO)"
    - Create a PO. Order:
      2 units at 40
      4 units at 35
    - Receive the picking
    
    The cost of the product is set at 35 (or 40, depending on the order of
    the lines in the PO).
    
    Since we are in a situation where the available quantity is zero, we
    enter in the `if float_is_zero...` condition, where the `qty_done` is
    not set.
    
    We should set it in all cases.
    
    opw-1907328
    
    closes odoo/odoo#30422

--- addons/stock_account/models/stock.py
+++ addons/stock_account/models/stock.py
@@ -381,7 +381,7 @@ class StockMove(models.Model):
             product_tot_qty_available = move.product_id.qty_available + tmpl_dict[move.product_id.id]
             rounding = move.product_id.uom_id.rounding
 
-            qty_done = 0.0
+            qty_done = move.product_uom._compute_quantity(move.quantity_done, move.product_id.uom_id)
             if float_is_zero(product_tot_qty_available, precision_rounding=rounding):
                 new_std_price = move._get_price_unit()
             elif float_is_zero(product_tot_qty_available + move.product_qty, precision_rounding=rounding) or \
@@ -390,7 +390,6 @@ class StockMove(models.Model):
             else:
                 # Get the standard price
                 amount_unit = std_price_update.get((move.company_id.id, move.product_id.id)) or move.product_id.standard_price
-                qty_done = move.product_uom._compute_quantity(move.quantity_done, move.product_id.uom_id)
                 qty = forced_qty or qty_done
                 new_std_price = ((amount_unit * product_tot_qty_available) + (move._get_price_unit() * qty)) / (product_tot_qty_available + qty_done)
 
