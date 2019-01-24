ce6e66cef930a30d9a62988a16f9f8319cb9f25c
Nicolas Lempereur <nle@odoo.com>
2019-01-24 15:59:20 +0000

    [FIX] web: bar chart no crazy with 2 group by
    In 3d2ca8104e4 some change were introduced to the graph view.
    
    But part of it had no sense when displaying data over 2 group by.
    
    eg. if the first level has 2 groups (2018, 2019) and each second level 2
        groups (Done,Cancelled), we could have:
         2018/Done: 0
         2018/Cancelled: 5
         2019/Done: 8
         2019/Cancelled: 9
        this gives us two main groups [2018: 5] / [2019: 17], the code
        remove the 2018 because [2018/Done] is 0 so we have something odd
        with columns not in order, and in stacked bar chart some part of
        a bar at the wrong offset
    
    opw-1932517
    closes #30529

--- addons/web/tests/__init__.py
+++ addons/web/tests/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import test_image
 from . import test_js
 from . import test_menu
 from . import test_serving_base
