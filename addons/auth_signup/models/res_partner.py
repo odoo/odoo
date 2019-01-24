162b8b325205d89ee0be03448116afe6fd6903f6
Thibault Delavallée <tde@odoo.com>
2019-01-24 14:22:10 +0100

    [IMP] auth_signup: allow direct URL redirection instead of action when computing signup URL
    Purpose of this commit is to improve the redirection done after signup.
    Currently everything is tailored to create an Odoo action (action, model,
    res_id, ...). This action is used to forge an URL that is the redirection
    URL after signup. In some cases you would like to directly give the redirection
    URL when no Odoo standard action is involved.
    
    An example of use is the redirection when subscribing to take a survey. We
    want customers to be back on survey that is a frontend URL, not a backend
    URL linked to an action.
    
    This commit is linked to task ID 1932508 and PR #30508.

--- addons/auth_signup/models/res_partner.py
+++ addons/auth_signup/models/res_partner.py
@@ -48,7 +48,7 @@ class ResPartner(models.Model):
             partner.signup_url = result.get(partner.id, False)
 
     @api.multi
-    def _get_signup_url_for_action(self, action=None, view_type=None, menu_id=None, res_id=None, model=None):
+    def _get_signup_url_for_action(self, url=None, action=None, view_type=None, menu_id=None, res_id=None, model=None):
         """ generate a signup url for the given partner ids and action, possibly overriding
             the url state components (menu_id, id, view_type) """
 
@@ -73,23 +73,26 @@ class ResPartner(models.Model):
             else:
                 continue        # no signup token, no user, thus no signup url!
 
-            fragment = dict()
-            base = '/web#'
-            if action == '/mail/view':
-                base = '/mail/view?'
-            elif action:
-                fragment['action'] = action
-            if view_type:
-                fragment['view_type'] = view_type
-            if menu_id:
-                fragment['menu_id'] = menu_id
-            if model:
-                fragment['model'] = model
-            if res_id:
-                fragment['res_id'] = res_id
+            if url:
+                query['redirect'] = url
+            else:
+                fragment = dict()
+                base = '/web#'
+                if action == '/mail/view':
+                    base = '/mail/view?'
+                elif action:
+                    fragment['action'] = action
+                if view_type:
+                    fragment['view_type'] = view_type
+                if menu_id:
+                    fragment['menu_id'] = menu_id
+                if model:
+                    fragment['model'] = model
+                if res_id:
+                    fragment['res_id'] = res_id
 
-            if fragment:
-                query['redirect'] = base + werkzeug.urls.url_encode(fragment)
+                if fragment:
+                    query['redirect'] = base + werkzeug.urls.url_encode(fragment)
 
             res[partner.id] = werkzeug.urls.url_join(base_url, "/web/%s?%s" % (route, werkzeug.urls.url_encode(query)))
         return res
