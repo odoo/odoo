1fa590143e5e34da76cb90d3a66360994c447868
Jeremy Kersten <jke@odoo.com>
2019-01-25 15:15:55 +0100

    [FIX] website_forum: fix intro msg

--- addons/website_forum/models/forum.py
+++ addons/website_forum/models/forum.py
@@ -54,14 +54,20 @@ class Forum(models.Model):
     welcome_message = fields.Html(
         'Welcome Message',
         translate=True,
-        default = """<section class="bg-info" style="height: 168px;"><div class="container">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <h1 class="text-center" style="text-align: left;">Welcome!</h1>
-                                <p class="text-muted text-center" style="text-align: left;">This community is for professionals and enthusiasts of our products and services. Share and discuss the best content and new marketing ideas, build your professional profile and become a better marketer together.</p>
-                            </div>
-                            <div class="col-lg-12">
-                                <a href="#" class="js_close_intro">Hide Intro</a>    <a class="btn btn-primary forum_register_url" href="/web/login">Register</a> </div>
+        default="""<section class="bg-info shadow">
+                        <div class="container py-5">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <h1 class="text-center">Welcome!</h1>
+                                    <p class="text-400 text-center">
+                                        This community is for professionals and enthusiasts of our products and services.
+                                        <br/>Share and discuss the best content and new marketing ideas, build your professional profile and become a better marketer together.
+                                    </p>
+                                </div>
+                                <div class="col text-center mt-3">
+                                    <a href="#" class="js_close_intro btn btn-outline-light">Hide Intro</a>
+                                    <a class="btn btn-light forum_register_url" href="/web/login">Register</a>
+                                </div>
                             </div>
                         </div>
                     </section>""")
