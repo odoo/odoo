d8f156ae7333e9e7ce712ee50d4898443c61a610
David Beguin <dbe@odoo.com>
2019-01-28 10:33:44 +0000

    [IMP] website_slides : use website_profile page template and adds website_slides specific information
    Adds slides ands slide.channels specific information (courses) into the website_profile template page.
    
    As there can be more than one slide.channel,the profile page shows everything linked to every slide.channel,
    except if the channel_id is given in url arguments.
    
    Adds the 'Go to eLearning' button in the 'new rank reached' mail to encourage the users to continue
    to be active on the eLearning platform as well, to gain more karma point and improve there rank.
    
    Task ID : 1922159
    PR #30988

--- addons/website_slides/__manifest__.py
+++ addons/website_slides/__manifest__.py
@@ -33,6 +33,7 @@ Share and Publish Videos, Presentations and Documents'
         'views/slide_channel_tag_views.xml',
         'views/slide_channel_invite_views.xml',
         'views/website_slides_menu_views.xml',
+        'views/website_slides_templates_profile.xml',
         'data/website_slides_ir_data.xml',
         'data/mail_data.xml',
         'data/slide_data.xml',
