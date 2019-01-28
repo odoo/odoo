6c2a9482cc3b4a2b7e0acab99e6eb9d659dcd950
Adrian Torres <adt@odoo.com>
2019-01-28 12:03:26 +0100

    [FIX] crm: update dashboard_graph_model when uninstalling sale
    Complement of a8c12e1fd0542726ada733b592f5196d256ec7e9
    opw-1925437

--- addons/crm/__init__.py
+++ addons/crm/__init__.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from functools import partial
+import odoo
+from odoo import api, SUPERUSER_ID
+
 import controllers
 import models
 import report
 import wizard
+
+
+def uninstall_hook(cr, registry):
+    def update_dashboard_graph_model(dbname):
+        db_registry = odoo.modules.registry.RegistryManager.new(dbname)
+        with api.Environment.manage(), db_registry.cursor() as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+            if 'crm.team' in env:
+                vals = [v[0] for v in env['crm.team']._fields['dashboard_graph_model'].selection]
+                recs = env['crm.team'].search([('dashboard_graph_model', 'not in', vals)])
+                recs.write({'dashboard_graph_model': None})
+    cr.after("commit", partial(update_dashboard_graph_model, cr.dbname))
