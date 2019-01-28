c363115f9cc362eb3966ada0dd00a36fd69a5a50
Adrian Torres <adt@odoo.com>
2019-01-28 15:50:55 +0100

    [FIX] website: properly unlink website_id ir.model.field
    It is not properly unlinked because it is a m2o required with
    ondelete='set null'
    
    Fixes #25078

--- addons/website/__init__.py
+++ addons/website/__init__.py
@@ -4,3 +4,19 @@
 from . import controllers
 from . import models
 from . import wizard
+
+import odoo
+from odoo import api, SUPERUSER_ID
+from functools import partial
+
+
+def uninstall_hook(cr, registry):
+    def rem_website_id_null(dbname):
+        db_registry = odoo.modules.registry.Registry.new(dbname)
+        with api.Environment.manage(), db_registry.cursor() as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+            env['ir.model.fields'].search([
+                ('name', '=', 'website_id'),
+                ('model', '=', 'res.config.settings'),
+            ]).unlink()
+    cr.after('commit', partial(rem_website_id_null, cr.dbname))
