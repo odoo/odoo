37db6396557b4ef09824bdd453ee833cd0a3f658
Thibault Delavallée <tde@odoo.com>
2019-01-31 11:15:57 +0100

    [FIX] sale: fix wording of link when sending a quotation by email
    Since commit 34c47eecd30bdf66bc057bf285a51cff956cd0e8 wording of link when
    sending a quotation by email is broken. Indeed has document in draft is
    not correctly considered as having to be signed and/or paid the link is
    not labelled correctly.
    
    This commit fixes it. Parameter is also renamed to be more understandable.
    This commit is linked to task ID 1910532 and PR #29849 .

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -738,12 +738,12 @@ class SaleOrder(models.Model):
 
         return report_pages
 
-    def has_to_be_signed(self, also_in_draft=False):
-        return (self.state == 'sent' or (self.state == 'draft' and also_in_draft)) and not self.is_expired and self.require_signature and not self.signature
+    def has_to_be_signed(self, include_draft=False):
+        return (self.state == 'sent' or (self.state == 'draft' and include_draft)) and not self.is_expired and self.require_signature and not self.signature
 
-    def has_to_be_paid(self, also_in_draft=False):
+    def has_to_be_paid(self, include_draft=False):
         transaction = self.get_portal_last_transaction()
-        return (self.state == 'sent' or (self.state == 'draft' and also_in_draft)) and not self.is_expired and self.require_payment and transaction.state != 'done' and self.amount_total
+        return (self.state == 'sent' or (self.state == 'draft' and include_draft)) and not self.is_expired and self.require_payment and transaction.state != 'done' and self.amount_total
 
     @api.multi
     def _notify_get_groups(self, message, groups):
