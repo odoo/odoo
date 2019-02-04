a9f5ece25ac60115a52d10c262578f5c551d723b
jbm-odoo <jbm@odoo.com>
2019-02-04 10:05:36 +0100

    [IMP] l10n_be_hr_payroll: Manage employee departure
    Compute notice duration and if the notice duration is not respected,
    a report about termination fees is generated.
    At the end of the notice duration, we can generate a certificate
    about time off not taken and an other about time off for the next
    year.
    
    TaskID: 1914556

--- addons/l10n_be_hr_payroll/wizard/__init__.py
+++ addons/l10n_be_hr_payroll/wizard/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import hr_payroll_employee_departure_holiday_attest
+from . import hr_payroll_employee_departure_notice
 from . import l10n_be_individual_account_wizard
