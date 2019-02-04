odoo/odoo

commit ff0b02eae37a58d90cc99b5830f1b65100f398be

[FIX] *: fix a bunch of incorrectly documented docstring
Fix a bunch of ill-documented/incomplete/incorrect method docs
Without this, the automatic doc generation does not work.

addons/web/static/src/js/chrome/search_filters.js
@@ -96,7 +96,7 @@ var ExtendedSearchProposition = Widget.extend(/** @lends instance.web.search.Ext
     },
 });
 
-ExtendedSearchProposition.Field = Widget.extend({
+var Field = Widget.extend({
     init: function (parent, field) {
         this._super(parent);
         this.field = field;
@@ -153,7 +153,7 @@ ExtendedSearchProposition.Field = Widget.extend({
     }
 });
 
-ExtendedSearchProposition.Char = ExtendedSearchProposition.Field.extend({
+var Char = Field.extend({
     tagName: 'input',
     className: 'o_input',
     attributes: {
@@ -172,7 +172,7 @@ ExtendedSearchProposition.Char = ExtendedSearchProposition.Field.extend({
     }
 });
 
-ExtendedSearchProposition.DateTime = ExtendedSearchProposition.Field.extend({
+var DateTime = Field.extend({
     tagName: 'span',
     attributes: {
         type: 'datetime'
@@ -241,7 +241,7 @@ ExtendedSearchProposition.DateTime = ExtendedSearchProposition.Field.extend({
     },
 });
 
-ExtendedSearchProposition.Date = ExtendedSearchProposition.DateTime.extend({
+var Date = DateTime.extend({
     attributes: {
         type: 'date'
     },
@@ -250,7 +250,7 @@ ExtendedSearchProposition.Date = ExtendedSearchProposition.DateTime.extend({
     },
 });
 
-ExtendedSearchProposition.Integer = ExtendedSearchProposition.Field.extend({
+var Integer = Field.extend({
     tagName: 'input',
     className: 'o_input',
     attributes: {
@@ -280,11 +280,11 @@ ExtendedSearchProposition.Integer = ExtendedSearchProposition.Field.extend({
     }
 });
 
-ExtendedSearchProposition.Id = ExtendedSearchProposition.Integer.extend({
+var Id = Integer.extend({
     operators: [{value: "=", text: _lt("is")}]
 });
 
-ExtendedSearchProposition.Float = ExtendedSearchProposition.Field.extend({
+var Float = Field.extend({
     template: 'SearchView.extended_search.proposition.float',
     operators: [
         {value: "=", text: _lt("is equal to")},
@@ -313,7 +313,7 @@ ExtendedSearchProposition.Float = ExtendedSearchProposition.Field.extend({
     }
 });
 
-ExtendedSearchProposition.Selection = ExtendedSearchProposition.Field.extend({
+var Selection = Field.extend({
     template: 'SearchView.extended_search.proposition.selection',
     operators: [
         {value: "=", text: _lt("is")},
@@ -331,7 +331,7 @@ ExtendedSearchProposition.Selection = ExtendedSearchProposition.Field.extend({
     }
 });
 
-ExtendedSearchProposition.Boolean = ExtendedSearchProposition.Field.extend({
+var Boolean = Field.extend({
     tagName: 'span',
     operators: [
         {value: "=", text: _lt("is true")},
@@ -347,19 +347,19 @@ ExtendedSearchProposition.Boolean = ExtendedSearchProposition.Field.extend({
 });
 
 core.search_filters_registry
-    .add('char', ExtendedSearchProposition.Char)
-    .add('text', ExtendedSearchProposition.Char)
-    .add('one2many', ExtendedSearchProposition.Char)
-    .add('many2one', ExtendedSearchProposition.Char)
-    .add('many2many', ExtendedSearchProposition.Char)
-    .add('datetime', ExtendedSearchProposition.DateTime)
-    .add('date', ExtendedSearchProposition.Date)
-    .add('integer', ExtendedSearchProposition.Integer)
-    .add('float', ExtendedSearchProposition.Float)
-    .add('monetary', ExtendedSearchProposition.Float)
-    .add('boolean', ExtendedSearchProposition.Boolean)
-    .add('selection', ExtendedSearchProposition.Selection)
-    .add('id', ExtendedSearchProposition.Id);
+    .add('char', Char)
+    .add('text', Char)
+    .add('one2many', Char)
+    .add('many2one', Char)
+    .add('many2many', Char)
+    .add('datetime', DateTime)
+    .add('date', Date)
+    .add('integer', Integer)
+    .add('float', Float)
+    .add('monetary', Float)
+    .add('boolean', Boolean)
+    .add('selection', Selection)
+    .add('id', Id);
 
 return {
     ExtendedSearchProposition: ExtendedSearchProposition

addons/account/static/src/js/account_dashboard_setup_bar.js
@@ -124,7 +124,7 @@ var AccountSetupBarModel = KanbanModel.extend({
 
     /**
      * @abstract
-     * @returns {Deferred -> Object} resolves to the required dashboard data
+     * @returns {Deferred<Object>} resolves to the required dashboard data
      */
     _fetchDashboardData: function () {
         return $.when(this._rpc({
@@ -136,7 +136,7 @@ var AccountSetupBarModel = KanbanModel.extend({
     /**
      * @private
      * @param {Deferred} super_def a deferred that resolves with a dataPoint id
-     * @returns {Deferred -> string} resolves to the dataPoint id
+     * @returns {Deferred<string>} resolves to the dataPoint id
      */
     _loadDashboard: function (super_def) {
         var self = this;

addons/account/static/src/js/reconciliation/reconciliation_model.js
@@ -12,7 +12,7 @@ var _t = core._t;
  * Model use to fetch, format and update 'account.bank.statement' and
  * 'account.bank.statement.line' datas allowing reconciliation
  *
- * The statement internal structure:
+ * The statement internal structure::
  *
  *  {
  *      valuenow: integer
@@ -25,7 +25,7 @@ var _t = core._t;
  *      accounts: {id: code}
  *  }
  *
- * The internal structure of each line is:
+ * The internal structure of each line is::
  *
  *   {
  *      balance: {
@@ -114,7 +114,7 @@ var StatementModel = BasicModel.extend({
      * account type
      *
      * @param {string} handle
-     * @param {number} mv_lines id
+     * @param {number} mv_line_id
      * @returns {Deferred}
      */
     addProposition: function (handle, mv_line_id) {
@@ -167,13 +167,15 @@ var StatementModel = BasicModel.extend({
      * change the mode line ('inactive', 'match', 'create'), and fetch the new
      * matched lines or prepare to create a new line
      *
-     * 'match': display the matched lines, the user can select the lines to
-     *   apply there as proposition
-     * 'create': display fields and quick create button to create a new
-     *   proposition for the reconciliation
+     * ``match``
+     *   display the matched lines, the user can select the lines to apply
+     *   there as proposition
+     * ``create``
+     *   display fields and quick create button to create a new proposition
+     *   for the reconciliation
      *
      * @param {string} handle
-     * @param {string} mode
+     * @param {'inactive' | 'match' | 'create'} mode
      * @returns {Deferred}
      */
     changeMode: function (handle, mode) {
@@ -316,7 +318,7 @@ var StatementModel = BasicModel.extend({
     /**
      * get the line data for this handle
      *
-     * @param {OdooEvent} event
+     * @param {Object} handle
      * @returns {Object}
      */
     getLine: function (handle) {
@@ -324,6 +326,7 @@ var StatementModel = BasicModel.extend({
     },
     /**
      * load data from
+     *
      * - 'account.bank.statement' fetch the line id and bank_statement_id info
      * - 'account.reconcile.model'  fetch all reconcile model (for quick add)
      * - 'account.account' fetch all account code
@@ -430,7 +433,7 @@ var StatementModel = BasicModel.extend({
      * 'amount_type'
      *
      * @param {string} handle
-     * @param {integer} reconcile model id
+     * @param {integer} reconcileModelId
      * @returns {Deferred}
      */
     quickCreateProposition: function (handle, reconcileModelId) {
@@ -460,7 +463,7 @@ var StatementModel = BasicModel.extend({
      * Remove a proposition and switch to an active mode ('create' or 'match')
      *
      * @param {string} handle
-     * @param {number} proposition id (move line id)
+     * @param {number} id (move line id)
      * @returns {Deferred}
      */
     removeProposition: function (handle, id) {
@@ -519,7 +522,7 @@ var StatementModel = BasicModel.extend({
      * Then the total is recomputed to have 100%.
      *
      * @param {string} handle
-     * @param {number[]} context.statement_ids
+     * @param {*} values
      * @returns {Deferred}
      */
     updateProposition: function (handle, values) {
@@ -636,7 +639,7 @@ var StatementModel = BasicModel.extend({
      * stop the editable proposition line and remove it if it's invalid then
      * compute the line
      *
-     * @see '_computeLine'
+     * See :func:`_computeLine`
      *
      * @private
      * @param {string} handle
@@ -679,7 +682,7 @@ var StatementModel = BasicModel.extend({
      * Check the taxes server side for each updated propositions with tax_id
      *
      * @private
-     * @param {Object}
+     * @param {Object} line
      * @returns {Deferred}
      */
     _computeLine: function (line) {
@@ -771,7 +774,7 @@ var StatementModel = BasicModel.extend({
      *
      * @private
      * @param {string} handle
-     * @param {integer} reconcile model id
+     * @param {integer} reconcileModelId
      */
     _computeReconcileModels: function (handle, reconcileModelId) {
         var line = this.getLine(handle);
@@ -788,8 +791,7 @@ var StatementModel = BasicModel.extend({
      * format a name_get into an object {id, display_name}, idempotent
      *
      * @private
-     * @param {Object|Array} data or name_get
-     * @param {Object|false} {id, display_name}
+     * @param {Object|Array} [value] data or name_get
      */
     _formatNameGet: function (value) {
         return value ? (value.id ? value : {'id': value[0], 'display_name': value[1]}) : false;
@@ -798,8 +800,8 @@ var StatementModel = BasicModel.extend({
      * Format each propositions (amount, label, account_id)
      *
      * @private
-     * @param {Object}
-     * @param {Object[]}
+     * @param {Object} line
+     * @param {Object[]} props
      */
     _formatLineProposition: function (line, props) {
         var self = this;
@@ -818,7 +820,7 @@ var StatementModel = BasicModel.extend({
      * @see '_computeLine'
      *
      * @private
-     * @param {Object[]}
+     * @param {Object[]} lines
      * @returns {Deferred}
      */
     _formatLine: function (lines) {
@@ -845,7 +847,7 @@ var StatementModel = BasicModel.extend({
      *
      * @private
      * @param {string} handle
-     * @param {Object[]}
+     * @param {Object[]} mv_lines
      * @returns {Deferred}
      */
     _formatMoveLine: function (handle, mv_lines) {
@@ -869,8 +871,8 @@ var StatementModel = BasicModel.extend({
      * base_amount with the decimal number from the currency
      *
      * @private
-     * @param {Object}
-     * @param {Object}
+     * @param {Object} line
+     * @param {Object} values
      * @returns {Object}
      */
     _formatQuickCreate: function (line, values) {
@@ -980,7 +982,7 @@ var StatementModel = BasicModel.extend({
      *
      * @private
      * @param {object} line
-     * @param {object} proposition
+     * @param {object} prop
      * @returns {object}
      */
     _formatToProcessReconciliation: function (line, prop) {
@@ -1216,7 +1218,7 @@ var ManualModel = StatementModel.extend({
      * 
      * @override
      * @private
-     * @param {Object}
+     * @param {Object} line
      * @returns {Deferred}
      */
     _computeLine: function (line) {
@@ -1237,8 +1239,8 @@ var ManualModel = StatementModel.extend({
      * @see '_computeLine'
      *
      * @private
-     * @param {string} 'customers', 'suppliers' or 'accounts'
-     * @param {Object}
+     * @param {'customers' | 'suppliers' | 'accounts'} type
+     * @param {Object} data
      * @returns {Deferred}
      */
     _formatLine: function (type, data) {
@@ -1264,8 +1266,8 @@ var ManualModel = StatementModel.extend({
      * 
      * @override
      * @private
-     * @param {Object}
-     * @param {Object}
+     * @param {Object} line
+     * @param {Object} props
      */
     _formatLineProposition: function (line, props) {
         var self = this;

addons/barcodes/static/src/js/barcode_events.js
@@ -182,9 +182,12 @@ var BarcodeEvents = core.Class.extend(mixins.PropertiesMixin, {
 });
 
 return {
-    // Singleton that emits barcode_scanned events on core.bus
+    /** Singleton that emits barcode_scanned events on core.bus */
     BarcodeEvents: new BarcodeEvents(),
-    // List of barcode prefixes that are reserved for internal purposes
+    /**
+     * List of barcode prefixes that are reserved for internal purposes
+     * @type Array
+     */
     ReservedBarcodePrefixes: ['O-CMD'],
 };
 

addons/bus/static/src/js/bus.js
@@ -136,12 +136,13 @@ bus.Bus = Widget.extend({
  * one in the list of open tabs. This one start polling for the other. When a notification is recieved from the poll, it
  * is signaling through the localStorage too.
  *
- * localStorage used keys are :
- *      - bus.channels : shared public channel list to listen during the poll
- *      - bus.options : shared options
- *      - bus.notification : the received notifications from the last poll
- *      - bus.tab_list : list of opened tab ids
- *      - bus.tab_master : generated id of the master tab
+ * localStorage used keys are:
+ *
+ * - bus.channels : shared public channel list to listen during the poll
+ * - bus.options : shared options
+ * - bus.notification : the received notifications from the last poll
+ * - bus.tab_list : list of opened tab ids
+ * - bus.tab_master : generated id of the master tab
  */
 var CrossTabBus = bus.Bus.extend({
     init: function(){

addons/mail/static/src/js/activity.js
@@ -13,7 +13,7 @@ var _t = core._t;
 
 /**
  * Set the 'label_delay' entry in activity data according to the deadline date
- * @param {Array} list of activity Object
+ * @param {Array} activities list of activity Object
  * @return {Array} : list of modified activity Object
  */
 var setDelayLabel = function(activities){

addons/mail/static/src/js/chatter_composer.js
@@ -89,7 +89,7 @@ var ChatterComposer = composer.BasicComposer.extend({
 
     /**
      * Get the list of selected suggested partners
-     * @returns Array() : list of 'recipient' selected partners (may not be created in db)
+     * @returns {Array} list of 'recipient' selected partners (may not be created in db)
      **/
     get_checked_suggested_partners: function () {
         var self = this;
@@ -106,8 +106,8 @@ var ChatterComposer = composer.BasicComposer.extend({
     /**
      * Check the additional partners (not necessary registered partners), and open a popup form view
      * for the ones who informations is missing.
-     * @param Array : list of 'recipient' partners to complete informations or validate
-     * @returns Deferred resolved with the list of checked suggested partners (real partner)
+     * @param {Array} checked_suggested_partners list of 'recipient' partners to complete informations or validate
+     * @returns {Deferred} resolved with the list of checked suggested partners (real partner)
      **/
     check_suggested_partners: function (checked_suggested_partners) {
         var self = this;

addons/mail/static/src/js/document_viewer.js
@@ -34,7 +34,7 @@ var DocumentViewer = Widget.extend({
      * Documents that are not of type image or video are filtered out.
      *
      * @override
-     * @param {Array[Object]} attachments list of attachments
+     * @param {Array<Object>} attachments list of attachments
      * @param {integer} activeAttachmentID
      */
     init: function (parent, attachments, activeAttachmentID) {

commit 2849a6d2a60c81f949b4d25663608c7ab8359f18

[FIX] search_filters: broke a thing in ff0b02e
As part of fixing the JS docs (docstrings & references & ...), the
various search filters were de-namespaced (aka
ExtendedSearchProposition.Integer = ... -> var Integer = ...).

This had the side-effect of making a `new Date` refer to the extended
search Date field instead of the global Date object, which went
unnoticed because *that* had been wrapped in a moment() call, and
moment() apparently doesn't mind being given garbage, but it would also
create an "unbound" and thus never destroyed widget.

Remove the `new Date`, calling moment directly has pretty much the same
effect, moment objects created from date objects just have an extra
field (a cache for the date object I guess).

closes odoo/odoo#30791

addons/web/static/src/js/chrome/search_filters.js
@@ -238,7 +238,7 @@ var DateTime = Field.extend({
     _create_new_widget: function (name) {
         this[name] = new (this._get_widget_class())(this);
         return this[name].appendTo(this.$el).then((function () {
-            this[name].setValue(moment(new Date()));
+            this[name].setValue(moment());
         }).bind(this));
     },
     _get_widget_class: function () {