0099195ec67695560eab59addf4c226c89d76d7d
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2019-02-06 08:48:30 +0200

    Added copyright

--- addons/l10n_ua/__manifest__.py
+++ addons/l10n_ua/__manifest__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+# Copyright (C) 2019 Bohdan Lisnenko <bohdan.lisnenko@erp.co.ua>, ERP Ukraine
 
 {
     'name': 'Ukraine - Accounting',
