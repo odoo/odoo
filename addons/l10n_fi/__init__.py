76b5e01969e877f66370ec41ac5acb2c141f0409
Elmeri Niemelä <niemela.elmeri@gmail.com>
2019-02-08 18:03:14 +0530

    [ADD] l10n_fi: Finnish localization modules

---
+++ addons/l10n_fi/__init__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    ODOO Addon module by Sprintit Ltd
+#    Copyright (C) 2018 Sprintit Ltd (<http://sprintit.fi>).
+#
+#    Part of Odoo. See LICENSE file for full copyright and licensing details.
+#
+##############################################################################
