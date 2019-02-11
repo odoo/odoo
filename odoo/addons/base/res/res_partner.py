b5137bba1889237757cec064e96865405f092175
Nans Lefebvre <len@odoo.com>
2019-02-11 15:01:27 +0100

    [FIX] base: fix res_partner name_get for multi-company
    Let the database be in multi-company with unshared contact catalog.
    Let F be a filter "('partner_id', 'not ilike', 'string')" on invoices.
    If there is a contact which parent belongs to another company,
    then this parent cannot be read.
    Thus the name_get on partner can create an access error.
    As a result applying the filter F would fail and return no results.
    
    opw 1933862

--- odoo/addons/base/res/res_partner.py
+++ odoo/addons/base/res/res_partner.py
@@ -585,7 +585,7 @@ class Partner(models.Model, FormatAddress):
                 if not name and partner.type in ['invoice', 'delivery', 'other']:
                     name = dict(self.fields_get(['type'])['type']['selection'])[partner.type]
                 if not partner.is_company:
-                    name = "%s, %s" % (partner.commercial_company_name or partner.parent_id.name, name)
+                    name = "%s, %s" % (partner.commercial_company_name or partner.sudo().parent_id.name, name)
             if self._context.get('show_address_only'):
                 name = partner._display_address(without_company=True)
             if self._context.get('show_address'):
