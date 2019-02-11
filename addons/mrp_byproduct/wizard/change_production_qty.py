8f748aea63a9d3a61404516014bff8587231e27d
Arnold Moyaux <arm@odoo.com>
2019-02-11 13:47:41 +0100

    [DEL] mrp_byproduct: remove module

--- addons/mrp_byproduct/wizard/change_production_qty.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, models
-
-
-class ChangeProductionQty(models.TransientModel):
-    _inherit = 'change.production.qty'
-
