4b2c73b0e06b0e17486fa0b3a53f516101f9e7e8
Arnold Moyaux <arm@odoo.com>
2019-02-11 12:47:41 +0000

    [DEL] mrp_byproduct: remove module

--- addons/mrp_byproduct/models/mrp_subproduct.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, fields, models, _
-from odoo.addons import decimal_precision as dp
-
