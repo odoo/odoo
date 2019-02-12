11874133bdd12ef1ef39f848008d90d7055213c0
Lucas Lefèvre <lul@odoo.com>
2019-02-12 15:29:55 +0100

    [ADD] hr_skills: Introduce a new module for employee resumé and skills
    Purpose
    =======
    
    Consultancy companies need resumé and skills of their consultants.
    For big projects, they often need to send them to their customers.
    These informations are also useful to statistics.
    
    Specification
    =============
    
    New models
    ----------
    
    1/ `hr.resume.line.type`
    Types of resumé lines. e.g. *Experience*, *Education*, *Hobbies*
    
    2/ `hr.resume.line`
    It is a line in the resumé of an employee.
    
    3/ `hr.skill`
    Name of a skill. e.g *French*, *Python*, *Piano*
    
    4/ `hr.skill.type`
    Skills can belongs to a particular type. A skill type has skill levels associated.
    e.g. *Languages*, *Dev*, *Music*
    
    5/ `hr.skill.level`
    Levels available for a particular skill type. Each level has a label
    and a progress (between 0 and 100) associated.
    e.g. *Intermediary (20%)*, *Advanced (85%)*, *Expert (100%)*
    
    6/ `hr.employee.skill`
    These are skills which employees have. It links an employee with a particular skill
    and level.
    e.g. Mitchell has an *Intermediary* level in *Python*
    
    Access Rights
    -------------
    
    Only a `hr_user` can create/edit `hr.resume.line.type`, `hr.skill`, `hr.skill.level`, `hr.skill.type`.
    If employees are allowed to edit their infos (setting), they can also create/edit `hr.resume.line`,
    `hr.employee.skill` for themselves.
    
    UI
    --
    Resumé lines are displayed, grouped by type, in a new 'Resumé' tab in the employee
    form. Resumé lines can be reordered (handle widget)
    
    Emloyee skills are displayed in the Resumé tab, grouped by skill type.

--- addons/hr/models/res_config_settings.py
+++ addons/hr/models/res_config_settings.py
@@ -11,6 +11,7 @@ class ResConfigSettings(models.TransientModel):
         related='company_id.resource_calendar_id', readonly=False)
     module_hr_org_chart = fields.Boolean(string="Show Organizational Chart")
     module_hr_presence = fields.Boolean(string="Control presence of employees")
+    module_hr_skills = fields.Boolean(string="Employee Skills and Resumé")
     hr_presence_control_login = fields.Boolean(string="According to the system login (User status on chat)", config_parameter='hr.hr_presence_control_login')
     hr_presence_control_email = fields.Boolean(string="According to the amount of sent emails", config_parameter='hr.hr_presence_control_email')
     hr_presence_control_ip = fields.Boolean(string="According to the IP address", config_parameter='hr.hr_presence_control_ip')
