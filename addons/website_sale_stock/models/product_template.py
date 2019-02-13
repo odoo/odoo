f2d4b118d039d746a76a36986ad8fbcb12599677
Sébastien Theys <seb@odoo.com>
2019-02-13 16:25:56 +0000

    [FIX] sale,website_sale: fix list view of variants price compute
    Before this commit, the list view of variant didn't use `_get_combination_info`
    so the displayed price was not taking into account important settings such as
    B2B/B2C and currency convert.

--- addons/website_sale_stock/models/product_template.py
+++ addons/website_sale_stock/models/product_template.py
@@ -17,8 +17,10 @@ class ProductTemplate(models.Model):
     custom_message = fields.Text(string='Custom Message', default='')
 
     @api.multi
-    def _get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False, reference_product=False):
-        combination_info = super(ProductTemplate, self)._get_combination_info(combination, product_id, add_qty, pricelist, reference_product)
+    def _get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False, reference_product=False, only_template=False):
+        combination_info = super(ProductTemplate, self)._get_combination_info(
+            combination=combination, product_id=product_id, add_qty=add_qty, pricelist=pricelist,
+            parent_combination=reference_product, only_template=only_template)
 
         if combination_info['product_id']:
             product = self.env['product.product'].sudo().browse(combination_info['product_id'])
