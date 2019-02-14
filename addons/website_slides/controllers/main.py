a39257259884a17478d8085abdc6486b530155e3
David Beguin <dbe@odoo.com>
2019-02-14 11:33:50 +0100

    [IMP] website_slides : show completed and ongoing course on profile
    Two new sections are added in the profile page : Completed courses and Followed courses
    Thoses sections replace the former 'Courses' section.
    
    Use span instead of b tag in profile page
    
    Task ID : 1922159
    PR #31109

--- addons/website_slides/controllers/main.py
+++ addons/website_slides/controllers/main.py
@@ -571,13 +571,15 @@ class WebsiteSlides(WebsiteProfile):
         return channels
 
     def _prepare_user_slides_profile(self, user):
-        courses = request.env['slide.channel.partner'].sudo().search([('partner_id', '=', user.partner_id.id)]).mapped('channel_id')
+        courses = request.env['slide.channel.partner'].sudo().search([('partner_id', '=', user.partner_id.id)])
+        courses_completed = courses.filtered(lambda c: c.completed)
+        courses_ongoing = courses - courses_completed
         values = {
             'uid': request.env.user.id,
             'user': user,
             'main_object': user,
-            'courses': courses,
-            'count_courses': len(courses),
+            'courses_completed': courses_completed,
+            'courses_ongoing': courses_ongoing,
             'is_profile_page': True,
             'badge_category': 'slides',
         }
