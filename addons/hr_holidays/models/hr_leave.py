81092e6faa567526d94f8a67fa7804948353a862
Toufik Ben Jaa <tbe@odoo.com>
2019-02-14 14:53:30 +0100

    [FIX] hr_holidays: allow holidays officer to approve leaves
    - Holidays officer were not allowed to approve leaves.
    - Employee's manager should be able to approve leaves even if it is not
      its leaves manager.

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -731,15 +731,15 @@ class HolidaysRequest(models.Model):
         """ Check if target state is achievable. """
         current_employee = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
         is_team_leader = self.env.user.has_group('hr_holidays.group_hr_holidays_team_leader')
-        is_manager = self.env.user.has_group('hr_holidays.group_hr_holidays_manager')
+        is_officer = self.env.user.has_group('hr_holidays.group_hr_holidays_user')
         for holiday in self:
             val_type = holiday.holiday_status_id.validation_type
             if state == 'confirm':
                 continue
 
             if state == 'draft':
-                if holiday.employee_id != current_employee and not is_manager:
-                    raise UserError(_('Only a Leave Manager can reset other people leaves.'))
+                if holiday.employee_id != current_employee and not is_officer:
+                    raise UserError(_('Only a Leave Officer or Manager can reset other people leaves.'))
                 continue
 
             if not is_team_leader:
@@ -749,17 +749,16 @@ class HolidaysRequest(models.Model):
                 # use ir.rule based first access check: department, members, ... (see security.xml)
                 holiday.check_access_rule('write')
 
-            if holiday.employee_id == current_employee and not is_manager:
-                raise UserError(_('Only a Leave Manager can approve its own requests.'))
+            if holiday.employee_id == current_employee and not is_officer:
+                raise UserError(_('Only a Leave Officer or Manager can approve its own requests.'))
 
             if (state == 'validate1' and val_type == 'both') or (state == 'validate' and val_type == 'manager'):
                 manager = holiday.employee_id.parent_id or holiday.employee_id.department_id.manager_id
-                if (manager and manager != current_employee) and not self.env.user.has_group('hr_holidays.group_hr_holidays_manager'):
-                    raise UserError(_('You must be either %s\'s manager or Leave manager to approve this leave') % (holiday.employee_id.name))
+                if (manager and manager != current_employee) and not is_officer:
+                    raise UserError(_('You must be either %s\'s manager or Leave Officer / Manager to approve this leave') % (holiday.employee_id.name))
 
-            if state == 'validate' and val_type == 'both':
-                if not self.env.user.has_group('hr_holidays.group_hr_holidays_manager'):
-                    raise UserError(_('Only an Leave Manager can apply the second approval on leave requests.'))
+            if state == 'validate' and val_type == 'both' and not is_officer:
+                raise UserError(_('Only an Leave Officer / Manager can apply the second approval on leave requests.'))
 
     # ------------------------------------------------------------
     # Activity methods
