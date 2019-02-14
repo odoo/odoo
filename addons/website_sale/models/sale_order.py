1d220734aba765dcf2ae35b4c39cf456d2d0f8ad
Toufik Ben Jaa <tbe@odoo.com>
2019-02-14 10:25:06 +0000

    [FIX] website_sale: avoid to consider quotations as eCommerce carts
    - An eCommerce cart is a quotation linked to a website.
    - The feature to consider a cart as abandonned wasn't correctly checking
      if the quotation is linked to a website and flag regular quotation as
      abandonned carts.
    
      This causes issues since Sales may wish to send the quotation by mail,
      but the button is hidden and replaced by the button "send recovery
      mail" which send an email with a specific email template.
    
    closes odoo/odoo#31103

--- addons/website_sale/models/sale_order.py
+++ addons/website_sale/models/sale_order.py
@@ -43,11 +43,17 @@ class SaleOrder(models.Model):
     @api.multi
     @api.depends('website_id', 'date_order', 'order_line', 'state', 'partner_id')
     def _compute_abandoned_cart(self):
-        abandoned_delay = self.website_id and self.website_id.cart_abandoned_delay or 1.0
-        abandoned_datetime = datetime.utcnow() - relativedelta(hours=abandoned_delay)
         for order in self:
-            domain = order.date_order and order.date_order <= abandoned_datetime and order.state == 'draft' and order.partner_id.id != self.env.ref('base.public_partner').id and order.order_line
-            order.is_abandoned_cart = bool(domain)
+            # a quotation can be considered as an abandonned cart if it is linked to a website,
+            # is in the 'draft' state and has an expiration date
+            if order.website_id and order.state == 'draft' and order.date_order:
+                public_partner_id = order.website_id.user_id.partner_id
+                # by default the expiration date is 1 hour if not specified on the website configuration
+                abandoned_delay = order.website_id.cart_abandoned_delay or 1.0
+                abandoned_datetime = datetime.utcnow() - relativedelta(hours=abandoned_delay)
+                order.is_abandoned_cart = bool(order.date_order <= abandoned_datetime and order.partner_id != public_partner_id and order.order_line)
+            else:
+                order.is_abandoned_cart = False
 
     def _search_abandoned_cart(self, operator, value):
         abandoned_delay = self.website_id and self.website_id.cart_abandoned_delay or 1.0
