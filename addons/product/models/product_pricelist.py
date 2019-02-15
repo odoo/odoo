64dcce4c18795894d8ebdddd623cdfb082cc4202
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2019-02-15 15:57:43 +0100

    [FIX] product: public price must be converted to pricelist currency
    It seems that the final conversion is missed in case there is no
    suitable rule, but needs to be done anyway in order to get the correct
    price in some cases like retrieving the public price

--- addons/product/models/product_pricelist.py
+++ addons/product/models/product_pricelist.py
@@ -240,8 +240,8 @@ class Pricelist(models.Model):
                     suitable_rule = rule
                 break
             # Final price conversion into pricelist currency
-            if suitable_rule and suitable_rule.compute_price != 'fixed' and suitable_rule.base != 'pricelist':
-                if suitable_rule.base == 'standard_price':
+            if not suitable_rule or suitable_rule and suitable_rule.compute_price != 'fixed' and suitable_rule.base != 'pricelist':
+                if suitable_rule and suitable_rule.base == 'standard_price':
                     # The cost of the product is always in the company currency
                     price = product.cost_currency_id.compute(price, self.currency_id, round=False)
                 else:
