576fcae9ebf4bc8429362cd25c36dc41c76fdfca
Jinal Patel <jip@odoo.com>
2019-02-15 14:14:22 +0530

    [IMP] stock: assign someone to picking
    purpose is allow to assign picking to users
    - add field responsible on the picking that allows to select a user
    - Now we assign responsible to several picking at once
    - add default filter 'My Transfers' and 'Unassigned Transfers'
    
    Related task : 1938107

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -265,6 +265,10 @@ class Picking(models.Model):
         default=lambda self: self.env['res.company']._company_default_get('stock.picking'),
         index=True, required=True,
         states={'done': [('readonly', True)], 'cancel': [('readonly', True)]})
+    user_id = fields.Many2one(
+        'res.users', 'Responsible', tracking=True,
+        domain=lambda self: [('groups_id', 'in', self.env.ref('stock.group_stock_user').id)],
+        default=lambda self: self.env.user)
 
     move_line_ids = fields.One2many('stock.move.line', 'picking_id', 'Operations')
     move_line_ids_without_package = fields.One2many('stock.move.line', 'picking_id', 'Operations without package', domain=['|',('package_level_id', '=', False), ('picking_type_entire_packs', '=', False)])
