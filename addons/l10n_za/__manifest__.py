1ddd53c37f9f0a8ec7e3ae124ba2e1e2dc49d0ff
Habib Ayob <habib@paradigmdigital.co.za>
2019-02-17 18:48:17 +0200

    [IMP] l10n_za: replace unused yml file with xml function call. Minor chart data changes.

--- addons/l10n_za/__manifest__.py
+++ addons/l10n_za/__manifest__.py
@@ -22,7 +22,7 @@ This is the latest basic South African localisation necessary to run Odoo in ZA:
         'data/l10n_za_chart_data.xml',
         'data/account_template.xml',
         'data/account.tax.template.csv',
-        'data/account_chart_template.yml',
+        'data/account_chart_template_configure_data.xml',
         
     ],
 }
