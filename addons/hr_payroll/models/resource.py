980cbb25161383e01265aaf06c3e4697af8d0084
Yannick Tivisse <yti@odoo.com>
2019-02-18 12:46:42 +0000

    [FIX] hr_payroll: Refine access to benefit types for officers/managers

--- addons/hr_payroll/models/resource.py
+++ addons/hr_payroll/models/resource.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from odoo import models, fields, api
-from odoo.osv.expression import AND, OR
 
 
 class ResourceCalendar(models.Model):
