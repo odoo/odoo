f43faad5a3b317f3bfc05da84820bd0963e751b8
Thibault Delavallée <tde@odoo.com>
2019-02-18 12:09:26 +0000

    [REF] website_slide: rename statistics fields on channel and category models
    Purpose is to reduce model naming complexity. Currently you have: slide_type
    <type>, and related statistics on channel and category nbr_<type>s with a
    trailing and error-prone s. Let us remove the trailing s to reduce complexity
    of code reading. Some code cleaning in statistics computation is also done.
    
    Commit linked to task ID 1941250 and PR #31272.

--- addons/website_slides/controllers/main.py
+++ addons/website_slides/controllers/main.py
@@ -119,7 +119,7 @@ class WebsiteSlides(WebsiteProfile):
             domain = expression.AND([domain, [('tag_ids', 'in', tags.ids)]])
 
         if slide_type and 'nbr_%ss' % slide_type in request.env['slide.channel']:
-            domain = expression.AND([domain, [('nbr_%ss' % slide_type, '>', 0)]])
+            domain = expression.AND([domain, [('nbr_%s' % slide_type, '>', 0)]])
         return domain
 
     def _prepare_channel_values(self, **kwargs):
