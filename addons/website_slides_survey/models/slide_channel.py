e5f3056d96d6cf5918f6d9f6dc5a6619b4d0c55b
Thibault Delavallée <tde@odoo.com>
2019-02-18 12:09:26 +0000

    [REF] website_slide: rename statistics fields on channel and category models
    Purpose is to reduce model naming complexity. Currently you have: slide_type
    <type>, and related statistics on channel and category nbr_<type>s with a
    trailing and error-prone s. Let us remove the trailing s to reduce complexity
    of code reading. Some code cleaning in statistics computation is also done.
    
    Commit linked to task ID 1941250 and PR #31272.

--- addons/website_slides_survey/models/slide_channel.py
+++ addons/website_slides_survey/models/slide_channel.py
@@ -7,26 +7,10 @@ from odoo import fields, models
 class Channel(models.Model):
     _inherit = 'slide.channel'
 
-    nbr_certifications = fields.Integer("Number of Certifications", compute='_compute_slides_statistics', store=True)
-
-    def _compute_slides_statistics_type(self, read_group_res):
-        result = super(Channel, self)._compute_slides_statistics_type(read_group_res)
-        for cid in result:
-            result[cid]['nbr_certifications'] = 0
-        for res_group in read_group_res:
-            cid = res_group['channel_id'][0]
-            result[cid]['nbr_certifications'] += res_group.get('slide_type', '') == 'certification' and res_group['__count'] or 0
-        return result
+    nbr_certification = fields.Integer("Number of Certifications", compute='_compute_slides_statistics', store=True)
 
 
 class Category(models.Model):
     _inherit = 'slide.category'
 
-    nbr_certifications = fields.Integer("Number of Certifications", compute='_count_presentations', store=True)
-
-    def _extract_count_presentations_type(self, result, record_id):
-        statistics = super(Category, self)._extract_count_presentations_type(result, record_id)
-        statistics['nbr_certifications'] = result[record_id].get('certification', 0)
-        statistics['total_slides'] += statistics['nbr_certifications']
-
-        return statistics
+    nbr_certification = fields.Integer("Number of Certifications", compute='_count_presentations', store=True)
