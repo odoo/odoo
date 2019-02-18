92034d1c19cfbc3c54147f858a790d525cfa97c5
Christophe Matthieu <chm@odoo.com>
2019-02-18 09:42:38 +0000

    [FIX] web_editor: speeder edition of field char on website_blog
    Before this fix, the customize button was updated with each character,
    which slowed down the fast insertion of characters and used resources
    unnecessarily.

--- addons/website/static/src/js/editor/wysiwyg_multizone.js
+++ addons/website/static/src/js/editor/wysiwyg_multizone.js
@@ -89,7 +89,7 @@ var WysiwygMultizone = Wysiwyg.extend({
                 }
             }
             if (ev.key.length === 1) {
-                this._onChange();
+                this._onChangeThrottled();
             }
         },
         'click .note-editable': function (ev) {
@@ -123,6 +123,7 @@ var WysiwygMultizone = Wysiwyg.extend({
         options.addDropSelector = ':o_editable';
         this.savingMutex = new concurrency.Mutex();
         this._super(parent, options);
+        this._onChangeThrottled = _.throttle(this._onChange.bind(this), 300);
     },
     /**
      * Prevent some default features for the editable area.
