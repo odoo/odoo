860ba62feaf28c828012f1b828f8b046d8371bb7
David Beguin <dbe@odoo.com>
2019-02-20 10:24:23 +0100

    [FIX] website_profile : give always access to user to his own profile
    The main condition to be able to see a profile is that the user we want to
    see the profile is website_published.
    But if a new user sign up and wants to access his own profile, he must
    get access to his own profile, even if he is (not yet) website_published.
    
    Task ID : 1941250
    
    Also linked with tasks :
    1902304 : New eLearning Master Task
    1922159 : website_profile (subtask)
    
    PR #31287

--- addons/website_profile/controllers/main.py
+++ addons/website_profile/controllers/main.py
@@ -45,6 +45,9 @@ class WebsiteProfile(http.Controller):
 
     def _check_user_profile_access(self, user_id):
         user_sudo = request.env['res.users'].sudo().browse(user_id)
+        # User can access - no matter what - his own profile
+        if user_sudo.id == request.env.user.id:
+            return user_sudo
         if user_sudo.karma == 0 or not user_sudo.website_published or \
             (user_sudo.id != request.session.uid and request.env.user.karma < request.website.karma_profile_min):
             return False
