7282812cd61efe1adb78080cce8177eb45e71d0c
Aurélien Warnon <awa@odoo.com>
2019-02-21 09:14:23 +0100

    [REF] website_slides: refactor the slide fullscreen side bar widget
    Purpose
    =======
    
    This commit includes various improvement on the slides fullscreen side bar widget:
    
    - linting
    - better selectors (avoid global)
    - "attachTo" instead of "appendTo"

--- addons/website_slides/static/src/js/slides_course_sidebar_list.js
+++ addons/website_slides/static/src/js/slides_course_sidebar_list.js
@@ -1,34 +1,38 @@
 odoo.define('website_slides.sidebar', function (require) {
-    'use strict';
-    var sAnimations = require('website.content.snippets.animation');
-    var core = require('web.core');
-    var Widget = require('web.Widget');
+'use strict';
 
-    var SideBar = Widget.extend({
-        init: function (el){
-            this._super.apply(this,arguments);
-        },
-        start: function (){
-            $('.o_wslides_fullscreen_toggle_sidebar').click(function (ev){
-                ev.preventDefault();
-                $(ev.currentTarget).toggleClass('active');
-                $('.o_wslides_fullscreen_sidebar').toggleClass('o_wslides_fullscreen_sidebar_hidden');
-                $('.o_wslides_fullscreen_player').toggleClass('o_wslides_fullscreen_player_no_sidebar')
-            })
-            return this._super.apply(this, arguments);
-        },
-    });
+var sAnimations = require('website.content.snippets.animation');
+var Widget = require('web.Widget');
 
-    sAnimations.registry.websiteSlidesSidebarList = Widget.extend({
-        selector: '.o_wslides_fullscreen_toggle_sidebar',
-        // xmlDependencies: ['/website_slides/static/src/xml/website_slides.xml'],
-        init: function (el){
-            this._super.apply(this, arguments);
-        },
-        start: function (){
-            this._super.apply(this, arguments);
-            var sideBar = new SideBar(this);
-            sideBar.appendTo(".oe_js_side_bar_list");
-        }
-    });
+var SideBar = Widget.extend({
+    events: {
+        'click .o_wslides_fullscreen_toggle_sidebar': '_onClickToggleSideBar'
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    _onClickToggleSideBar: function (ev) {
+        ev.preventDefault();
+        $(ev.currentTarget).toggleClass('active');
+        this.$('.o_wslides_fullscreen_sidebar').toggleClass('o_wslides_fullscreen_sidebar_hidden');
+        this.$('.o_wslides_fullscreen_player').toggleClass('o_wslides_fullscreen_player_no_sidebar');
+    }
+});
+
+sAnimations.registry.websiteSlidesSidebarList = sAnimations.Class.extend({
+    selector: '.o_wslides_fullscreen_toggle_sidebar',
+    start: function (){
+        this._super.apply(this, arguments);
+        var sideBar = new SideBar(this);
+        sideBar.attachTo(".o_wslides");
+    }
+});
+
+return {
+    sideBar: SideBar,
+    websiteSlidesSidebarList: sAnimations.registry.websiteSlidesSidebarList
+};
+
 });
