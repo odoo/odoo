06e5f6b58590222a89af18512db45c1cf5081542
Xavier Morel <xmo@odoo.com>
2019-02-21 11:20:49 +0100

    [FIX] core: handling of o2m record deletion in SSF
    The SSF would properly mark its own records as deleted, but it would
    not properly handle deletion requests coming from an onchange, and it
    would not necessarily convert DELETE_ALL commands (5) into the proper
    sequence of individual (2)s matching existing records.

--- odoo/addons/test_new_api/tests/test_ui.py
+++ odoo/addons/test_new_api/tests/test_ui.py
@@ -5,6 +5,13 @@ import odoo.tests
 class TestUi(odoo.tests.HttpCase):
 
     def test_01_admin_widget_x2many(self):
+        # FIXME: breaks if too many children of base.menu_tests
+
+        # This tour turns out to be quite sensible to the number of items in
+        # the base.menu_tests: it's specifically sequenced to be lower (after)
+        # the default, but doesn't account for the fact that it could
+        # "fall off" into the "o_extra_menu_items" section if the window is
+        # too small or there are too many items preceding it in the tests menu
         self.phantom_js("/web#action=test_new_api.action_discussions",
                         "odoo.__DEBUG__.services['web_tour.tour'].run('widget_x2many', 100)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.widget_x2many.ready",
