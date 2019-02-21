a7dbecd6f4ac9bd8f0ccb15cee1cc3623580ce7c
Parth Choksi <pch@odoo.com>
2019-02-21 05:05:40 +0000

    [IMP] auth_signup : send a reminder for pending users
    Purpose of this commit is to send a mail template to the user
    who invited other user(s) in Odoo.
    
    It informs the user about the details of user(s) who have been
    invited by him but have still not registered after the given days
    of creation (default set to -> 5days)
    
    task-1912449
    Closes #31582
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/auth_signup/__manifest__.py
+++ addons/auth_signup/__manifest__.py
@@ -17,6 +17,7 @@ Allow users to sign up and reset their password
     ],
     'data': [
         'data/auth_signup_data.xml',
+        'data/ir_cron_data.xml',
         'views/res_config_settings_views.xml',
         'views/res_users_views.xml',
         'views/auth_signup_login_templates.xml',
