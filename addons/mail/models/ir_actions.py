22ba9e70478f1cb41e253a230f12febdb717bb26
Lucas Perais (lpe) <lpe@odoo.com>
2019-02-25 16:03:18 +0000

    [FIX] base_automation, mail: automated action on computed fields
    Together a6db9a3b2502e3c153e755cea4d331c65423fb4b and
    4fd14fe9127360ee602a667634535043375c8f15
    corrected the triggering of automated actions at *each* recompute of a field
    
    It is problematic since, maybe, the field that should trigger an action
    *is* a computed one. It often happens with "state"-like fields
    
    This commit aims at applying the logic of those two commits,
    except in the case we do want the action to be triggered
    even in a recompute case
    
    OPW 1935727
    
    closes odoo/odoo#31422
    
    Signed-off-by: "Lucas Perais (lpe)" <lpe@odoo.com>

--- addons/mail/models/ir_actions.py
+++ addons/mail/models/ir_actions.py
@@ -80,18 +80,27 @@ class ServerActions(models.Model):
     def _is_recompute(self, action):
         """When an activity is set on update of a record,
         update might be triggered many times by recomputes.
-        When need to know it to skip these steps."""
+        When need to know it to skip these steps.
+        Except if the computed field is supposed to trigger the action
+        """
         records = self.env[action.model_id.model].browse(
             self._context.get('active_ids', self._context.get('active_id')))
         old_values = action._context.get('old_values')
         if old_values:
-            field_names = list(old_values[records[0].id])
-            field = records._fields[field_names[0]]
-            # Pick an arbitrary field; if it is marked to be recomputed,
-            # it means we are in an extraneous write triggered by the recompute.
-            # In this case, we should not create a new activity.
-            if records._recompute_check(field):
-                return True
+            domain_post = action._context.get('domain_post')
+            tracked_fields = []
+            if domain_post:
+                for leaf in domain_post:
+                    if isinstance(leaf, (tuple, list)):
+                        tracked_fields.append(leaf[0])
+            fields_to_check = [field for record, field_names in old_values.items() for field in field_names if field not in tracked_fields]
+            if fields_to_check:
+                field = records._fields[fields_to_check[0]]
+                # Pick an arbitrary field; if it is marked to be recomputed,
+                # it means we are in an extraneous write triggered by the recompute.
+                # In this case, we should not create a new activity.
+                if records._recompute_check(field):
+                    return True
         return False
 
     @api.model
