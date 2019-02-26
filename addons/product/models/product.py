b837cc51c061f321bb9e294d0ef0bc38669b6cdc
Nicolas Martinelli <nim@odoo.com>
2019-02-26 08:22:33 +0000

    [IMP] product: minimal quantity
    User might be confused by the help: they think the vendor cannot sell
    the product below this quantity, which is not what the field depicts.
    
    Improve the label and the help of the `min_qty` field to avoid
    confusion.
    
    opw-1943511
    
    closes odoo/odoo#31414

--- addons/product/models/product.py
+++ addons/product/models/product.py
@@ -695,8 +695,8 @@ class SupplierInfo(models.Model):
         related='product_tmpl_id.uom_po_id',
         help="This comes from the product form.")
     min_qty = fields.Float(
-        'Minimal Quantity', default=0.0, required=True,
-        help="The minimal quantity to purchase from this vendor, expressed in the vendor Product Unit of Measure if not any, in the default unit of measure of the product otherwise.")
+        'Quantity', default=0.0, required=True,
+        help="The quantity to purchase from this vendor to benefit from the price, expressed in the vendor Product Unit of Measure if not any, in the default unit of measure of the product otherwise.")
     price = fields.Float(
         'Price', default=0.0, digits=dp.get_precision('Product Price'),
         required=True, help="The price to purchase a product")
