06baea8069c0e01f4d44a7196b3fdd203d28be20
Jigar Vaghela <jva@odoo.com>
2019-02-27 15:06:38 +0530

    [IMP] l10n_lu: load_translations on post init hook.

--- addons/l10n_lu/__init__.py
+++ addons/l10n_lu/__init__.py
@@ -3,6 +3,17 @@
 
 from . import models
 
+from odoo import api, SUPERUSER_ID
+
+
+def load_translations(env):
+    env.ref('l10n_lu.lu_2011_chart_1').process_coa_translations()
+
 def _preserve_tag_on_taxes(cr, registry):
     from odoo.addons.account.models.chart_template import preserve_existing_tags_on_taxes
     preserve_existing_tags_on_taxes(cr, registry, 'l10n_lu')
+
+def post_init(cr, registry):
+    _preserve_tag_on_taxes(cr, registry)
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    load_translations(env)
