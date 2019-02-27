bdc0b4b892dc17c0a51e454740633edc26f3de6d
Pierre Paridans <app@odoo.com>
2019-02-27 11:53:47 +0000

    [FIX] lunch: make it work on mobile
    This module didn't worked on mobile: user cannot select the products or
    see their cart.
    
    This commit reworks the LunchKanbanWidget template and introduces a
    dedicated template for mobile. This template introduces a button at the
    bottom of the screen to toggle the "cart" widget on mobile (hiding it by
    default).
    
    It tries to make the best use of the existing Bootstrap classes allowing
    to have a more responsive layout and reducing the custom CSS required by
    this module (both in desktop and in mobile).
    
    This commit adds a decent JS tests-suite (mobile & desktop) to this
    module as the existing one left substential parts of the UI untested
    (opening wizard when clicking on a kanban record, cart's lines content,
    clear cart button, widget structure and content based on the state...).
    Those tests are also less based on DOM nodes order/tag names and use
    instead dedicated classnames.
    
    It also modifies the LunchKanbanRecord click handling to had a dedicated
    event handler instead of overriding the default _onGlobalClick() and
    make it more testable.
    
    Task ID: 1945032

--- addons/lunch/static/src/js/lunch_kanban_record.js
+++ addons/lunch/static/src/js/lunch_kanban_record.js
@@ -1,22 +1,36 @@
 odoo.define('lunch.LunchKanbanRecord', function (require) {
-"use strict";
+    "use strict";
 
-/**
- * This file defines the KanbanRecord for the Lunch Kanban view.
- */
+    /**
+     * This file defines the KanbanRecord for the Lunch Kanban view.
+     */
 
-var KanbanRecord = require('web.KanbanRecord');
+    var KanbanRecord = require('web.KanbanRecord');
 
-var LunchKanbanRecord = KanbanRecord.extend({
-    _onGlobalClick: function (ev) {
-        ev.preventDefault();
-        // ignore clicks on oe_kanban_action elements
-        if (!$(ev.target).hasClass('oe_kanban_action')) {
-            this.trigger_up('open_wizard', {productId: this.recordData.product_id});
-        }
-    },
-});
+    var LunchKanbanRecord = KanbanRecord.extend({
+        events: _.extend({}, KanbanRecord.prototype.events, {
+            'click': '_onSelectRecord',
+        }),
 
-return LunchKanbanRecord;
+        //--------------------------------------------------------------------------
+        // Handlers
+        //--------------------------------------------------------------------------
 
-});
+        /**
+         * Open the add product wizard
+         *
+         * @private
+         * @param {MouseEvent} ev Click event
+         */
+        _onSelectRecord: function (ev) {
+            ev.preventDefault();
+            // ignore clicks on oe_kanban_action elements
+            if (!$(ev.target).hasClass('oe_kanban_action')) {
+                this.trigger_up('open_wizard', {productId: this.recordData.product_id});
+            }
+        },
+    });
+
+    return LunchKanbanRecord;
+
+    });
