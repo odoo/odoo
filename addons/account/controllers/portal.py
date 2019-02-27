6a63ddfbf367a3c9928776dcc41a3c384b6037b9
Laurent Smet <las@odoo.com>
2019-02-27 13:37:45 +0000

    [FIX] account: Fix portal in account without account_payment
    'transaction_ids' is a field defined in 'account_payment' but used in 'account'.
    Then, the portal generates a traceback with only 'account' installed.

--- addons/account/controllers/portal.py
+++ addons/account/controllers/portal.py
@@ -3,7 +3,6 @@
 
 from odoo import http, _
 from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
-from odoo.addons.payment.controllers.portal import PaymentProcessing
 from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
 
@@ -86,7 +85,6 @@ class PortalAccount(CustomerPortal):
             return self._show_report(model=invoice_sudo, report_type=report_type, report_ref='account.account_invoices', download=download)
 
         values = self._invoice_get_page_view_values(invoice_sudo, access_token, **kw)
-        PaymentProcessing.remove_payment_transaction(invoice_sudo.transaction_ids)
         return request.render("account.portal_invoice_page", values)
 
     # ------------------------------------------------------------
