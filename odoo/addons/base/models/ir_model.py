c8ad39f293b3d40d7795fa81cbff6de7b6d96a7a
Raphael Collet <rco@odoo.com>
2019-02-27 11:38:50 +0100

    [FIX] base: copied views are delete too early
    Install Invoicing (account) and uninstall it. It results to a an error
    because some views required by `payment.acquirer` are deleted before
    the acquirers.
    
    The problem is due to the way copied views are deleted, since 1388b7f
    they are removed before the module uninstallation. The related commit
    faced a similar problem where copied views were deleted too late
    during a module uninstallation.
    
    The two problems are revealing that the copied views have to be removed
    as part of the module uninstallation, more precisely after all records
    refering to them has been deleted but before the schemas has been
    cleaned.
    
    opw-1943286

--- odoo/addons/base/models/ir_model.py
+++ odoo/addons/base/models/ir_model.py
@@ -1591,9 +1591,17 @@ class IrModelData(models.Model):
 
         # Remove non-model records first, then model fields, and finish with models
         undeletable += unlink_if_refcount(item for item in to_unlink if item[0] not in ('ir.model', 'ir.model.fields', 'ir.model.constraint'))
+
+        # Remove copied views. This must happen after removing all records from
+        # the modules to remove, otherwise ondelete='restrict' may prevent the
+        # deletion of some view. This must also happen before cleaning up the
+        # database schema, otherwise some dependent fields may no longer exist
+        # in database.
+        modules = self.env['ir.module.module'].search([('name', 'in', modules_to_remove)])
+        modules._remove_copied_views()
+
         undeletable += unlink_if_refcount(item for item in to_unlink if item[0] == 'ir.model.constraint')
 
-        modules = self.env['ir.module.module'].search([('name', 'in', modules_to_remove)])
         constraints = self.env['ir.model.constraint'].search([('module', 'in', modules.ids)])
         constraints._module_data_uninstall()
 
