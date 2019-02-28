10c30690937e4914fbd24afb2e28d750cf1430d6
wan <wan@odoo.com>
2019-02-28 10:03:34 +0000

    [ADD] l10n_be_invoice_bba: update reference fields Task 1920906 remove the l10n_be_structured_comm field random/date references will not be longer used adapt for the last changes in account
    closes odoo/odoo#29858
    
    Signed-off-by: Quentin De Paoli (qdp) <qdp@openerp.com>

---
+++ addons/l10n_be_invoice_bba/models/account_journal.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import fields, models
+
+
+class AccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+    invoice_reference_model = fields.Selection(selection_add=[('be', 'Belgium')])
