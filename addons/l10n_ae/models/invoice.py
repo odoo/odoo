683cd7e6e09ca67d9180ca58534ad2f76de783bb
Ravi Gohil <rgo@odoo.com>
2019-03-01 19:43:16 +0530

    some more improvements

--- addons/l10n_ae/models/invoice.py
+++ addons/l10n_ae/models/invoice.py
@@ -9,8 +9,9 @@ class AccountInvoice(models.Model):
 
     # Technical field to show rate on invoice report
     l10n_ae_invoice_rate = fields.Float(compute='_compute_rate', string='Rate on Invoice Date', digits=(12, 6))
+    l10n_ae_amount_tax_signed = fields.Monetary(compute='_compute_rate', string='TaxAmount in AED', currency_field='company_currency_id')
 
-    @api.depends('date_invoice')
+    @api.depends('date_invoice', 'amount_tax')
     def _compute_rate(self):
         for invoice in self:
             rate = self.env['res.currency']._get_conversion_rate(invoice.currency_id,
@@ -18,3 +19,6 @@ class AccountInvoice(models.Model):
                 invoice.date_invoice or fields.Date.today()
             )
             invoice.l10n_ae_invoice_rate = rate
+            amount_tax_signed = invoice.currency_id._convert(invoice.amount_tax, invoice.company_id.currency_id,
+                 invoice.company_id, invoice.date_invoice or fields.Date.today())
+            invoice.l10n_ae_amount_tax_signed = amount_tax_signed
