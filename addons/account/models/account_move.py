05525388b91dd6b0345d7807ee865bea91517346
Moisés López <moylop260@vauxoo.com>
2019-03-02 20:21:09 +0000

    [REF] account: Faster account.update_check

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -1212,16 +1212,26 @@ class AccountMoveLine(models.Model):
     @api.multi
     def _update_check(self):
         """ Raise Warning to cause rollback if the move is posted, some entries are reconciled or the move is older than the lock date"""
-        move_ids = set()
-        for line in self:
-            err_msg = _('Move name (id): %s (%s)') % (line.move_id.name, str(line.move_id.id))
-            if line.move_id.state != 'draft':
+        query = """SELECT am.id, am.state, aml.reconciled, am.name
+        FROM account_move_line aml
+        INNER JOIN account_move am
+           ON am.id = aml.move_id
+        WHERE (am.state != 'draft'
+               OR (aml.reconciled IS TRUE
+                   AND NOT (aml.debit = 0 AND aml.credit = 0)))
+              AND aml.id IN %s
+        LIMIT 1
+        """
+        self.env.cr.execute(query, (tuple(self.ids),))
+        res = self.env.cr.dictfetchone()
+        if res:
+            err_msg = _('Move name (id): %s (%s)') % (res.get('name'), str(res.get('id')))
+            if res['state'] != 'draft':
                 raise UserError(_('You cannot do this modification on a posted journal entry, you can just change some non legal fields. You must revert the journal entry to cancel it.\n%s.') % err_msg)
-            if line.reconciled and not (line.debit == 0 and line.credit == 0):
+            if res['reconciled']:
                 raise UserError(_('You cannot do this modification on a reconciled entry. You can just change some non legal fields or you must unreconcile first.\n%s.') % err_msg)
-            if line.move_id.id not in move_ids:
-                move_ids.add(line.move_id.id)
-        self.env['account.move'].browse(list(move_ids))._check_lock_date()
+
+        self.mapped('move_id')._check_lock_date()
         return True
 
     ####################################################
