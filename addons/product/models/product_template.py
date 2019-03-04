06e981c0eb270f68a2856ebca50ee3b0a7bec250
Janusz Harkot <jh@blueice.pl>
2019-03-04 15:10:18 +0100

    allow management of volume decimal precision

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -79,7 +79,7 @@ class ProductTemplate(models.Model):
         help = "Cost used for stock valuation in standard price and as a first price to set in average/FIFO.")
 
     volume = fields.Float(
-        'Volume', compute='_compute_volume', inverse='_set_volume',
+        'Volume', compute='_compute_volume', inverse='_set_volume', digits=dp.get_precision('Stock Volume'),
         help="The volume in m3.", store=True)
     weight = fields.Float(
         'Weight', compute='_compute_weight', digits=dp.get_precision('Stock Weight'),
