62a29dba2c51ed498e605744bf92383017734fa6
Martin Trigaux <mat@odoo.com>
2019-03-05 10:05:47 +0000

    [REV] models: do not erase master version
    This commit has been backported in 11.0 [1] and then reverted [2],
    because it introduced a regression in web_studio, discovered at opw-1947380
    
    As no forward-port has been done between the backport and the revert,
    the forward-port of the revert also contains the patch itself, resulting
    in a noop.
    
    Actually revert 06d73eab623e58427a7387879d242c320dc5d399 in branch `12.0`.
    
    [1] d855e3911fa40472f471439c5b3d78e3f7f493a8
    [2] 1748b4671816a5b29f349978c2536eb6018a1bf0
    
    closes odoo/odoo#31637
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- odoo/models.py
+++ odoo/models.py
@@ -3361,7 +3361,7 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
                 _logger.warning('Field %s is deprecated: %s', field, field.deprecated)
 
             if field.column_type:
-                if single_lang or not (has_translation and field.translate):
+                if single_lang or not (has_translation and field.translate is True):
                     # val is not a translation: update the table
                     val = field.convert_to_column(val, self, vals)
                     columns.append((name, field.column_format, val))
