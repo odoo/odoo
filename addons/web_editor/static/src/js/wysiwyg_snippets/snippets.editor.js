6456f13360eac7da23d085a15ddabb53d6f75c23
Antoine Guenet <age@odoo.com>
2019-03-08 15:05:30 +0100

    [FIX] web_editor: update the handle on click input node in website edit
    When clicking on a snippet containing an input node on Firfeox, the
    handle wouldn't appear as expected. This is due to trying JQuery (but
    not only) trying to modify properties that are restricted on Firefox but
    not on Chrome. The error message still appears from time to time due to
    Summernote using `document.getSelection`, which is problematic with
    Firefox. This will be fixed with the next version of the editor. In the
    meantime the direct effect of that error (the handle issue) is fixed
    with this commit.
    
    The standard target (`Event.target`) is now looked for in first position
    instead of non-standard 'Event.srcElement` (IE alias for `Event.target`)
    then `Event.originalTarget` (Mozilla-specific) and finally
    `Event.target` (standard). `Event.originalTarget` is restricted on
    Firefox so it was giving the same cryptic error as mentioned above
    ("Error: Permission denied to access property", which generally points
    to same-origin policy violations).
    
    Ref:
    - https://developer.mozilla.org/en-US/docs/Web/API/Event/srcElement
    - https://developer.mozilla.org/en-US/docs/Web/API/Event/originalTarget
    - https://developer.mozilla.org/en-US/docs/Web/API/Event/target
    - https://developer.mozilla.org/en-US/docs/Web/API/Window/getSelection#Related_objects
    - https://developer.mozilla.org/en-US/docs/web/javascript/reference/errors/property_access_denied

--- addons/web_editor/static/src/js/wysiwyg_snippets/snippets.editor.js
+++ addons/web_editor/static/src/js/wysiwyg_snippets/snippets.editor.js
@@ -605,7 +605,7 @@ var SnippetsMenu = Widget.extend({
         // Active snippet editor on click in the page
         var lastClickedElement;
         this.$document.on('click.snippets_menu', '*', function (ev) {
-            var srcElement = ev.srcElement || (ev.originalEvent && (ev.originalEvent.originalTarget || ev.originalEvent.target) || ev.target);
+            var srcElement = ev.target || (ev.originalEvent && (ev.originalEvent.target || ev.originalEvent.originalTarget)) || ev.srcElement;
             if (lastClickedElement === srcElement || !srcElement) {
                 return;
             }
