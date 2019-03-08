ece194e8f05cdeca89dc63dea76b717a38e25098
Graeme Gellatly <gdgellatly@gmail.com>
2019-03-08 13:52:04 +1300

    Remove individual m2m indexes and replace with a single mirrored composite

--- odoo/fields.py
+++ odoo/fields.py
@@ -2648,8 +2648,7 @@ class Many2many(_RelationalMulti):
                                       "{id2}" INTEGER NOT NULL,
                                       PRIMARY KEY("{id1}","{id2}"));
                 COMMENT ON TABLE "{rel}" IS %s;
-                CREATE INDEX ON "{rel}" ("{id1}");
-                CREATE INDEX ON "{rel}" ("{id2}")
+                CREATE INDEX ON "{rel}" ("{id2}","{id1}");
             """.format(rel=self.relation, id1=self.column1, id2=self.column2)
             cr.execute(query, ['RELATION BETWEEN %s AND %s' % (model._table, comodel._table)])
             _schema.debug("Create table %r: m2m relation between %r and %r", self.relation, model._table, comodel._table)
