2d970942bae15a6e5378a05deec18295435c235f
Romain Derie <rde@odoo.com>
2019-03-12 10:06:55 +0000

    [FIX] website_sale: limit pricelist to it's website if set
    Before this commit:
    1. Non Public User would get the main company pricelist returned when calling
       `get_current_pricelist`. As this is saved in an `ir.property`
       (`property_product_pricelist`) that is not website dependant, that pricelist
       would always be returned on every website.
       This is a problem if that pricelist is website limited, you should not be
       able to use it on other websites.
    2. Public User would not have a valid pricelist on other website than the
       default one. Indeed, every pricelists are restricted that default website.
       It would result in prices shown as "0" (without currency symbol) on the shop
    3. Pricelists set as selectable without a website_id set would not be
       selectable
    4. Public User would not get a fallback pricelist when reading its
       `property_product_pricelist`. Indeed, the public user's partner is not
       active and since the refactoring of this field compute method to be a multi,
       it would end up doing a search() instead of a browse(). Thus, it would find
       a pricelist for the public user but it would not set it.
    
    This commit:
    1. We override and check that pricelists returnes by the `ir.property` are
       indeed available on the current website.
    2. Remove the website restriction on `Public Pricelist` so other websites have
       at least one pricelist available.
    3. `pricelist_ids` on `website` now return all available pricelists including
       the ones that are generic (no website set)
    4. We search for inactive partners too.
    
    Part of this commit fixes part of 27861
    Github-25109

--- addons/product/models/product_pricelist.py
+++ addons/product/models/product_pricelist.py
@@ -314,34 +314,53 @@ class Pricelist(models.Model):
         res = self._get_partner_pricelist_multi([partner_id], company_id)
         return res[partner_id].id
 
+    def _get_partner_pricelist_multi_search_domain_hook(self):
+        return []
+
+    def _get_partner_pricelist_multi_filter_hook(self):
+        return self
+
     def _get_partner_pricelist_multi(self, partner_ids, company_id=None):
         """ Retrieve the applicable pricelist for given partners in a given company.
+
+            It will return the first found pricelist in this order:
+            First, the pricelist of the specific property (res_id set), this one
+                   is created when saving a pricelist on the partner form view.
+            Else, it will return the pricelist of the partner country group
+            Else, it will return the generic property (res_id not set), this one
+                  is created on the company creation.
+            Else, it will return the first available pricelist
 
             :param company_id: if passed, used for looking up properties,
                 instead of current user's company
             :return: a dict {partner_id: pricelist}
         """
-        Partner = self.env['res.partner']
+        # `partner_ids` might be ID from inactive uers. We should use active_test
+        # as we will do a search() later (real case for website public user).
+        Partner = self.env['res.partner'].with_context(active_test=False)
+
         Property = self.env['ir.property'].with_context(force_company=company_id or self.env.user.company_id.id)
         Pricelist = self.env['product.pricelist']
+        pl_domain = self._get_partner_pricelist_multi_search_domain_hook()
 
-        # retrieve values of property
+        # if no specific property, try to find a fitting pricelist
         result = Property.get_multi('property_product_pricelist', Partner._name, partner_ids)
 
-        remaining_partner_ids = [pid for pid, val in result.items() if not val]
+        remaining_partner_ids = [pid for pid, val in result.items() if not val or
+                                 not val._get_partner_pricelist_multi_filter_hook()]
         if remaining_partner_ids:
             # get fallback pricelist when no pricelist for a given country
             pl_fallback = (
-                Pricelist.search([('country_group_ids', '=', False)], limit=1) or
+                Pricelist.search(pl_domain + [('country_group_ids', '=', False)], limit=1) or
                 Property.get('property_product_pricelist', 'res.partner') or
-                Pricelist.search([], limit=1)
+                Pricelist.search(pl_domain, limit=1)
             )
             # group partners by country, and find a pricelist for each country
             domain = [('id', 'in', remaining_partner_ids)]
             groups = Partner.read_group(domain, ['country_id'], ['country_id'])
             for group in groups:
                 country_id = group['country_id'] and group['country_id'][0]
-                pl = Pricelist.search([('country_group_ids.country_ids', '=', country_id)], limit=1)
+                pl = Pricelist.search(pl_domain + [('country_group_ids.country_ids', '=', country_id)], limit=1)
                 pl = pl or pl_fallback
                 for pid in Partner.search(group['__domain']).ids:
                     result[pid] = pl
