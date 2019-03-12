95eb13a056923b0209eb5998879fff69033327d4
Jigar Patel <jat@openerp.com>
2019-03-12 16:45:26 +0530

    [IMP] calendar: allow a user to accept/decline meeting from calendar view popover

--- addons/calendar/models/calendar.py
+++ addons/calendar/models/calendar.py
@@ -1781,3 +1781,12 @@ class Meeting(models.Model):
             if 'UNTIL' not in rule_str and 'COUNT' not in rule_str:
                 rule_str += ';COUNT=100'
         return rule_str
+
+    def change_attendee_status(self, status):
+        attendee = self.attendee_ids.filtered(lambda x: x.partner_id == self.env.user.partner_id)
+        if status == 'accepted':
+            return attendee.do_accept()
+        elif status == 'declined':
+            return attendee.do_decline()
+        else:
+            return attendee.do_tentative()
