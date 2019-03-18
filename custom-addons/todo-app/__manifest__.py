de3b1718058a9953b41de4d3134d306f9389d0cc
Nguyen Huu Dat (FHM.AVI) <DatNH4@fsoft.com.vn>
2019-03-18 09:56:49 +0700

    add new demo module

---
+++ custom-addons/todo-app/__manifest__.py
@@ -0,0 +1,9 @@
+{
+ 'name': 'To-Do Application',
+ 'description': 'Manage personal to-do tasks.',
+ 'author': 'Daniel Reis',
+ 'depends': ['base'],
+ 'application': True,
+ 'installable': False,
+ 'auto_install': False,
+}
