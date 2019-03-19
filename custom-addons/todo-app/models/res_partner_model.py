8208db9aaf1b5a029c2befda9323f07917997ad5
Nguyen Huu Dat (FHM.AVI) <DatNH4@fsoft.com.vn>
2019-03-19 09:44:27 +0700

    edit init

--- custom-addons/todo-app/models/res_partner_model.py
+++ custom-addons/todo-app/models/res_partner_model.py
@@ -2,6 +2,4 @@ from odoo import fields, models
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    todo_ids= fields.Many2many(
-    'todo.task',
-    string="To-do Teams")
+    todo_ids= fields.Many2many('todo.task', string = "To-do Teams")
