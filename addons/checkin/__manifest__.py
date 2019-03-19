8cec9c59e4bc704eda9e120036ff39dc406ea03a
Nguyen Huu Dat (FHM.AVI) <DatNH4@fsoft.com.vn>
2019-03-19 14:15:09 +0700

    edit2

--- addons/checkin/__manifest__.py
+++ addons/checkin/__manifest__.py
@@ -3,6 +3,9 @@
  'description': 'Manage check-in history',
  'author': 'Dat Nguyen',
  'depends': ['base'],
+ 'data': [
+        'checkin_menu.xml'
+    ],
  'installable': True,
  'application': True,
  'auto_install': False,
--- addons/hr/__manifest__.py
+++ addons/hr/__manifest__.py
@@ -28,7 +28,6 @@
         'views/hr_templates.xml',
         'views/res_config_settings_views.xml',
         'data/hr_data.xml',
-        'checkin_menu.xml',
     ],
     'demo': [
         'data/hr_demo.xml'
