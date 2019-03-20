6d7b1b11135ac4c947ca1bf418dd5cc0e3a693d3
Wolfgang Taferner <wolfgang.taferner@wt-io-it.at>
2019-03-20 06:19:44 +0000

    [REF] l10n_at: complete refactoring
    As the available Austrian Localization was very basic and migrated for
    years without any contribution or consolidation, a complete refactoring
    was contributed by WT-IO-IT GmbH to move forward.
    
    closes odoo/odoo#35642
    
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- addons/l10n_at/__manifest__.py
+++ addons/l10n_at/__manifest__.py
@@ -1,23 +1,45 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Copyright (C) conexus.at
+# Copyright (c) 2015 WT-IO-IT GmbH (https://www.wt-io-it.at)
+#                    Mag. Wolfgang Taferner <wolfgang.taferner@wt-io-it.at>
+
+# List of contributors:
+# Mag. Wolfgang Taferner <wolfgang.taferner@wt-io-it.at>
+# Josse Colpaert <jco@odoo.com>
 
 {
-    'name': 'Austria - Accounting',
-    'version': '2.0',
-    'author': 'conexus.at',
-    'website': 'http://www.conexus.at',
-    'category': 'Localization',
-    'depends': ['account'],
-    'description': """
-This module provides the standard Accounting Chart for Austria which is based on the Template from BMF.gv.at.
-============================================================================================================= 
-Please keep in mind that you should review and adapt it with your Accountant, before using it in a live Environment.
-""",
-    'data': ['data/l10n_at_chart_data.xml',
-             'data/account_data.xml',
-             'data/account_tax_report_data.xml',
-             'data/account_tax_data.xml',
-             'data/account_chart_template_data.xml'],
+    "name": "Austria - Accounting",
+    "version": "2.0",
+    "author": "WT-IO-IT GmbH, Wolfgang Taferner",
+    "website": "https://www.wt-io-it.at",
+    "category": "Localization",
+    'summary': "Austrian Standardized Charts & Tax",
+    "description": """
+   
+Austrian charts of accounts (Einheitskontenrahmen 2010).
+==========================================================
+
+    * Defines the following chart of account templates:
+        * Austrian General Chart of accounts 2010
+    * Defines templates for sale and purchase VAT
+    * Defines tax templates
+    * Defines fiscal positions for Austrian fiscal legislation
+    * Defines tax reports U1/U30
+ 
+    """,
+    "depends": [
+        "account",
+        "base_iban",
+        "base_vat",
+    ],
+    "data": [
+        'data/account_account_tag.xml',
+        'data/account_account_template.xml',
+        'data/account_chart_template.xml',
+        'data/account_tax_report_data.xml',
+        'data/account_tax_template.xml',
+        'data/account_fiscal_position_template.xml',
+        'data/account_chart_template_configure_data.xml',
+    ],
 }
