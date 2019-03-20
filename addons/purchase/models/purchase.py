fd8569f072e7a65738ec6bb89b8988106589bd57
Priyanka Kakadiya <pka@odoo.com>
2019-03-20 17:23:51 +0530

    [FIX] purchase: fix error when creating new quotation of purchase
    - in purchase there are 3 fields that computes by '_amount_all', so it is called 3 times. but, round method has 'ensure_one' therefore it gives singleton error.
    
    - So, removed rounding as there is need of rounding explicitly since these are monetary fields which will be rounded automatically.
    
    Related to Issue: 1948645

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -27,8 +27,8 @@ class PurchaseOrder(models.Model):
                 amount_untaxed += line.price_subtotal
                 amount_tax += line.price_tax
             order.update({
-                'amount_untaxed': order.currency_id.round(amount_untaxed),
-                'amount_tax': order.currency_id.round(amount_tax),
+                'amount_untaxed': amount_untaxed,
+                'amount_tax': amount_tax,
                 'amount_total': amount_untaxed + amount_tax,
             })
 
