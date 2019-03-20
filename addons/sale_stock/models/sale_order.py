a8697541fd9f083cf3fc9b6b8d9d10c2b5db6a39
jem-odoo <jem@openerp.com>
2019-03-20 09:42:38 +0100

    [IMP] sale: make confirmation_date required for confirmed SO
    Somehow, when a sales order is confirmed, its confirmation date might not be set (or it is
    possible to remove it). This makes no sense as we expect it to be set when the SO is confirmed.
    This commit adds an SQL constraint to ensure the confirmation date is required when the SO is
    confirmed or done.
    To populate this column, the confirmation can be deduced from the mail.message, with the confirmed
    subtype.
    
    Task-1930506

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -48,7 +48,7 @@ class SaleOrder(models.Model):
         super(SaleOrder, self)._compute_expected_date()
         for order in self:
             dates_list = []
-            confirm_date = fields.Datetime.from_string((order.confirmation_date or order.write_date) if order.state == 'sale' else fields.Datetime.now())
+            confirm_date = fields.Datetime.from_string(order.confirmation_date if order.state in ['sale', 'done'] else fields.Datetime.now())
             for line in order.order_line.filtered(lambda x: x.state != 'cancel' and not x._is_delivery()):
                 dt = confirm_date + timedelta(days=line.customer_lead or 0.0)
                 dates_list.append(dt)
@@ -301,7 +301,7 @@ class SaleOrderLine(models.Model):
 
     @api.onchange('product_uom_qty')
     def _onchange_product_uom_qty(self):
-        # When modifying a one2many, _origin doesn't guarantee that its values will be the ones 
+        # When modifying a one2many, _origin doesn't guarantee that its values will be the ones
         # in database. Hence, we need to explicitly read them from there.
         if self._origin:
             product_uom_qty_origin = self._origin.read(["product_uom_qty"])[0]["product_uom_qty"]
