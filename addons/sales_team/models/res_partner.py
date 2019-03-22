aaacaedee94de5df8d1b8f1330d5136627551779
mara1 <raul.martin@braintec-group.com>
2019-03-22 13:09:25 +0100

    Bugfix for Sales Team Missing team_id in Partner view, when it's defined in the .py file Adding some logic to assign a default value when changing the Salesperson

--- addons/sales_team/models/res_partner.py
+++ addons/sales_team/models/res_partner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ResPartner(models.Model):
@@ -10,3 +10,8 @@ class ResPartner(models.Model):
     team_id = fields.Many2one(
         'crm.team', 'Sales Team',
         help='If set, this Sales Team will be used for sales and assignations related to this partner')
+
+    @api.onchange('user_id')
+    def _onchange_user_id(self):
+        if self.user_id and not self.team_id:
+            self.team_id = self.user_id.sale_team_id
