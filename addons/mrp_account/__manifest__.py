f51f83b79f52841906d0f0af0251073d7c4ab7ba
Arnaud Baes <arb@odoo.com>
2019-03-27 17:48:13 +0100

    [MOV] mrp_account
    The module is moved from the enterprise repository as it
    was *without* the bom cost report.
    
    task 1831382

---
+++ addons/mrp_account/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Accounting - MRP',
+    'version': '1.0',
+    'category': 'Manufacturing',
+    'summary': 'Analytic accounting in Manufacturing',
+    'description': """
+Analytic Accounting in MRP
+==========================
+
+* Cost structure report
+""",
+    'website': 'https://www.odoo.com/page/manufacturing',
+    'depends': ['mrp', 'stock_account'],
+    'installable': True,
+    'auto_install': True,
+}
