5a0fad4e0a49a052d27bd388343a8a07041676d4
Thibault Delavallée <tde@odoo.com>
2019-03-27 11:23:14 +0100

    [FIX] website_forum: remove not working channel_ids given to composer
    Mail composer does not support channels. Indeed as it can be used to post
    a message or send an email it only supports partners. Previously field
    channel_ids given to the composer was used only in website_forum. However
    it is not used and therefore simplest way of solving it is to remove it.
    
    Anyway allowing to have channels followers of forum tags and being notified
    of new publications is probably more spammy than really necessary. Especially
    that it never worked.
    
    Commit linked to task ID 1907153 and PR #28464

--- addons/website_forum/models/forum.py
+++ addons/website_forum/models/forum.py
@@ -482,21 +482,18 @@ class Post(models.Model):
     def post_notification(self):
         for post in self:
             tag_partners = post.tag_ids.mapped('message_partner_ids')
-            tag_channels = post.tag_ids.mapped('message_channel_ids')
 
             if post.state == 'active' and post.parent_id:
                 post.parent_id.message_post_with_view(
                     'website_forum.forum_post_template_new_answer',
                     subject=_('Re: %s') % post.parent_id.name,
                     partner_ids=[(4, p.id) for p in tag_partners],
-                    channel_ids=[(4, c.id) for c in tag_channels],
                     subtype_id=self.env['ir.model.data'].xmlid_to_res_id('website_forum.mt_answer_new'))
             elif post.state == 'active' and not post.parent_id:
                 post.message_post_with_view(
                     'website_forum.forum_post_template_new_question',
                     subject=post.name,
                     partner_ids=[(4, p.id) for p in tag_partners],
-                    channel_ids=[(4, c.id) for c in tag_channels],
                     subtype_id=self.env['ir.model.data'].xmlid_to_res_id('website_forum.mt_question_new'))
             elif post.state == 'pending' and not post.parent_id:
                 # TDE FIXME: in master, you should probably use a subtype;
