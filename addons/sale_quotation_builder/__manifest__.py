6150339adb6fa77606a34d01c1755b9e1f7361d8
Victor Feyens <vfe@odoo.com>
2019-03-27 14:49:00 +0100

    [MOV] quotation_builder : default quotation template
    The default quotation template should be a demo data and shouldn't be
    set as default in the settings anymore.
    W.r.t the task 1847939, the default quotation template is misleading.
    Indeed, the default quotation_confirmation_percentage from the company
    for the sale orders is overriden by the value from the default template
    when creating a new sale_order.

--- addons/sale_quotation_builder/__manifest__.py
+++ addons/sale_quotation_builder/__manifest__.py
@@ -10,11 +10,13 @@
     'description': "Design great quotation templates with building blocks to significantly boost your success rate.",
     'depends': ['website', 'sale_management', 'website_mail'],
     'data': [
-        'data/sale_order_template_data.xml',
         'views/sale_portal_templates.xml',
         'views/sale_order_template_views.xml',
         'views/res_config_settings_views.xml',
         'views/sale_order_views.xml',
+    ],
+    'demo': [
+        'data/sale_order_template_demo.xml',
     ],
     'installable': True,
 }
