070a56b047748ce087e06920c785733cf9a76227
Ravi Gohil <rgo@odoo.com>
2019-04-01 15:03:32 +0530

    improvements (reworked on #26475) - all accounts will be created from csv - moved few taxes' definition from xml to csv - updated account csv file by reexporting account template records into csv - renamed files as per odoo standards - removed unused account.chart.template.csv - account_template.xml to be removed from next commit to see the diff of account movement from other xml file to itself

--- addons/l10n_za/__manifest__.py
+++ addons/l10n_za/__manifest__.py
@@ -13,16 +13,14 @@ This is the latest basic South African localisation necessary to run Odoo in ZA:
     - SARS VAT Ready Structure""",
     'author': 'Paradigm Digital',
     'website': 'https://www.paradigmdigital.co.za',
-    'depends': ['account', 
-                'base_vat'
-    ],
+    'depends': ['account', 'base_vat'],
     'data': [
         'data/account.account.tag.csv',
         'data/account.tax.group.csv',
-        'data/l10n_za_chart_data.xml',
-        'data/account_template.xml',
+        'data/account_chart_template_data.xml',
+        'data/account.account.template.csv',
         'data/account.tax.template.csv',
+        'data/account_chart_template_post_data.xml',
         'data/account_chart_template_configure_data.xml',
-        
     ],
 }
