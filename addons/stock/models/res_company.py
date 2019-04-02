92b74fae3717697f76cadc4f750fb50b9dc38035
Simon Lejeune <sle@openerp.com>
2019-04-02 16:29:27 +0200

    fixup! [REF] stock: locations per company creations

--- addons/stock/models/res_company.py
+++ addons/stock/models/res_company.py
@@ -131,7 +131,7 @@ class Company(models.Model):
         self._create_transit_location()
         self._create_inventory_loss_location()
         self._create_production_location()
-        self.._create_scrap_location()
+        self._create_scrap_location()
 
     @api.model
     def create(self, vals):
