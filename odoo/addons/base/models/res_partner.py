7ef45d1f772277dce993bf2976409eb8883fe421
Fabien Pinckaers <fp@openerp.com>
2019-04-02 15:54:10 +0200

    [WIP] avoid recursive implementation of function field

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -284,13 +284,13 @@ class Partner(models.Model):
     def _compute_get_ids(self):
         self.self = self.id
 
-    @api.depends('is_company', 'parent_id.commercial_partner_id')
+    @api.depends('is_company', 'parent_id')
     def _compute_commercial_partner(self):
         for partner in self:
-            if partner.is_company or not partner.parent_id:
-                partner.commercial_partner_id = partner
-            else:
-                partner.commercial_partner_id = partner.parent_id.commercial_partner_id
+            p = partner
+            while not (p.is_company or not p.parent_id):
+                p = p.parent_id
+            partner.commercial_partner_id = p
 
     @api.depends('company_name', 'parent_id.is_company', 'commercial_partner_id.name')
     def _compute_commercial_company_name(self):
