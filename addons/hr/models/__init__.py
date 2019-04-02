b2b26f1ff7e4dcf7d0c0ccc36b249045372329c7
RomainLibert <rli@odoo.com>
2019-04-02 11:03:25 +0200

    [REF] hr: split hr.py and hr_views.xml into model related files
    This is much better for code readability, and in our coding guidelines

--- addons/hr/models/__init__.py
+++ addons/hr/models/__init__.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import hr
-from . import res_config_settings
+from . import hr_employee
+from . import hr_employee_category
+from . import hr_department
+from . import hr_job
+from . import hr_plan
 from . import mail_alias
 from . import mail_channel
+from . import res_config_settings
 from . import res_partner
 from . import res_users
-from . import hr_plan
