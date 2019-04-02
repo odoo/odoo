1b8b76cf7a805aa57d2bf15b3ecfe023c0694933
Sébastien Theys <seb@odoo.com>
2019-04-02 08:45:41 +0000

    [IMP] tests, *: add start_tour helper
    Enterprise counterpart of https://github.com/odoo/odoo/pull/32316
    
    closes odoo/enterprise#3987
    
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

--- tests/test_website_form_editor.py
+++ tests/test_website_form_editor.py
@@ -7,6 +7,6 @@ import odoo.tests
 @odoo.tests.tagged('post_install','-at_install')
 class TestWebsiteFormEditor(odoo.tests.HttpCase):
     def test_tour(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('website_form_editor_tour')", "odoo.__DEBUG__.services['web_tour.tour'].tours.website_form_editor_tour.ready", login="admin")
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('website_form_editor_tour_submit')", "odoo.__DEBUG__.services['web_tour.tour'].tours.website_form_editor_tour_submit.ready")
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('website_form_editor_tour_results')", "odoo.__DEBUG__.services['web_tour.tour'].tours.website_form_editor_tour_results.ready", login="admin")
+        self.start_tour("/", 'website_form_editor_tour', login="admin")
+        self.start_tour("/", 'website_form_editor_tour_submit')
+        self.start_tour("/", 'website_form_editor_tour_results', login="admin")
