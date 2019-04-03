e63501be4f46331c9af1622e229f8bedb5a9a774
Nans Lefebvre <len@odoo.com>
2019-04-03 16:00:33 +0200

    [FIX] mail: reload the record when uploading attachments if post_refresh is set to always
    When post_refresh is set to 'always', the displayed record is entirely reloaded
    each time a message is posted in the chatter.
    However no such thing exists for when attachments are uploaded.
    
    This is of use e.g. when in the view there are fields that have attributes that
    depend on computed fields, themselves dependent on the record's attachments.
    
    One such case is the OCR.
    
    opw 1950403

--- addons/mail/static/src/js/chatter.js
+++ addons/mail/static/src/js/chatter.js
@@ -81,6 +81,7 @@ var Chatter = Widget.extend({
             var nodeOptions = fieldsInfo[mailFields.mail_thread].options || {};
             this.hasLogButton = options.display_log_button || nodeOptions.display_log_button;
             this.postRefresh = nodeOptions.post_refresh || 'never';
+            this.reloadOnUploadAttachment = this.postRefresh === 'always';
         }
     },
     /**
@@ -572,6 +573,9 @@ var Chatter = Widget.extend({
      * @private
      */
     _onReloadAttachmentBox: function () {
+        if (this.reloadOnUploadAttachment) {
+            this.trigger_up('reload');
+        }
         this._reloadAttachmentBox();
     },
     /**
