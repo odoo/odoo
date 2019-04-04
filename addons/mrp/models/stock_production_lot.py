7556938fc712b2a45b915129caa16588d6d0c1da
William Henrotin <whe@odoo.com>
2019-04-04 09:33:20 +0000

    [IMP] mrp: multiple finished product lots
    This commit allows the user in charge of manufacturing products to
    split his production into different lots if this production is done via
    workorders. Using a lot in a workorder will adds constraint to all the
    other ones. It will also prefil the final lot field and matching quantity
    to produce for futur workorders.
    
    This commit uses workorder lines (in another one2many as standard workorder_line_ids)
    to register lot/serial number and produced quantity. Prefill lots and
    quantities are all read from those lines
    
    Task : 1891864

--- addons/mrp/models/stock_production_lot.py
+++ addons/mrp/models/stock_production_lot.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models, _
+from odoo import models, _
 from odoo.exceptions import UserError
 
 
 class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
-
-    use_next_on_work_order_id = fields.Many2one('mrp.workorder',
-        string="Next Work Order to Use",
-        help='Technical: used to figure out default serial number on work orders')
 
     def _check_create(self):
         active_mo_id = self.env.context.get('active_mo_id')
