7d498135a563c42ce55855ce0ad0e9371bd269e0
Colinliz <lizheng02@inspur.com>
2019-04-08 17:29:09 +0800

    [FIX] l10n_cn_small_business: Modify Small And Medium-sized Enterprises Accounting Chart Steps to reproduce the bug: - Add some child account and modify some parent account according to 'Accounting Standards for 2017 Small and Medium-sized Enterprises' - Delete included and excluded tax: 17% 11% 3% Signed-off-by: Colinliz <lizheng02@inspur.com>

--- addons/l10n_cn_small_business/__manifest__.py
+++ addons/l10n_cn_small_business/__manifest__.py
@@ -5,14 +5,15 @@
 #                         维智众源 oldrev@gmail.com  <states data>
 # Copyright (C) 2012-2012 南京盈通 ccdos@intoerp.com <small business chart>
 # Copyright (C) 2008-now  开阖软件 jeff@osbzr.com    < PM and LTS >
+# Copyright (C) 2018-now  浪潮PS Cloud pscloud@inspur.com    < PM and LTS >
 
 {
     'name': 'China - Small Business CoA',
-    'version': '1.8',
+    'version': '12.0.1.0',
     'category': 'Localization',
-    'author': 'www.openerp-china.org',
-    'maintainer': 'jeff@osbzr.com',
-    'website': 'http://openerp-china.org',
+    'author': 'www.mypscloud.com',
+    'maintainer': 'pscloud@inspur.com',
+    'website': 'www.mypscloud.com/',
     'description': """
 
     科目类型\会计科目表模板\增值税\辅助核算类别\管理会计凭证簿\财务会计凭证簿
@@ -30,5 +31,4 @@
         'data/account_tax_template_data.xml',
         'data/account_chart_template_data.xml',
     ],
-    'auto_install': True
 }
