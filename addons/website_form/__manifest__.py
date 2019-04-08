02a71d5c0f1b9d3c20bb00df195c292019ee4957
Jeremy Kersten <jke@odoo.com>
2019-04-08 07:07:52 +0000

    [MRG] website_form[_editor]: merge modules together
    closes odoo/odoo#32491
    
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- addons/website_form/__manifest__.py
+++ addons/website_form/__manifest__.py
@@ -1,13 +1,19 @@
 {
-    'name': 'Generic Form Controller',
-    'category': 'Hidden',
-    'summary': 'Generic controller for web forms',
-    'description': 'Generic controller for web forms',
+    'name': 'Website Form',
+    'category': 'Website',
+    'summary': 'Build custom web forms',
+    'description': """
+        Customize and create your own web forms.
+        This module adds a new building block in the website builder in order to build new forms from scratch in any website page.
+    """,
     'version': '1.0',
     'depends': ['website', 'mail'],
     'data': [
+        'data/mail_mail_data.xml',
         'views/assets.xml',
+        'views/ir_model_views.xml',
         'views/res_config_view.xml',
+        'views/snippets.xml',
     ],
     'installable': True,
     'auto_install': False,
