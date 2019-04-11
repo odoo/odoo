89cbe3e79b01d78ea093ea842b2f3becdc9ad76e
Nans Lefebvre <len@odoo.com>
2019-04-11 12:16:31 +0200

    [FIX] account: better message on user group error
    The validation error introduced in 552da8dc can pop up in contexts where
    it is not expected;
    e.g. not in editing user rights but while upgrading a module.
    Therefore we make it more explicit, giving the user a way to resolve the error.

--- addons/account/models/res_users.py
+++ addons/account/models/res_users.py
@@ -12,7 +12,15 @@ class Users(models.Model):
     @api.constrains('groups_id')
     def _check_one_user_type(self):
         super(Users, self)._check_one_user_type()
-        for user in self:
-            if (user.user_has_groups('account.group_show_line_subtotals_tax_included') and
-                    user.user_has_groups('account.group_show_line_subtotals_tax_excluded')):
-                raise ValidationError(_('A user cannot have both Tax B2B and Tax B2C'))
+
+        users_with_both_groups = self.filtered(lambda user:
+            user.user_has_groups('account.group_show_line_subtotals_tax_included') and
+            user.user_has_groups('account.group_show_line_subtotals_tax_excluded')
+        )
+        if users_with_both_groups:
+            names = ", ".join(users_with_both_groups.mapped('name'))
+            raise ValidationError(_("A user cannot have both Tax B2B and Tax B2C.\n"
+                                    "Problematic user(s): %s\n"
+                                    "You should go in General Settings, and choose to display Product Prices\n"
+                                    "either in 'Tax-Included' or in 'Tax-Excluded' mode\n"
+                                    "(or switch twice the mode if you are already in the desired one).") % names)
