c457ffe0bb049374c5c69e3f6b86d59595c7ba01
Martin Trigaux <mat@odoo.com>
2019-04-11 06:45:55 +0000

    [IMP] web: unify the use of GROUPABLE_TYPES
    cf discussion on #31789
    
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- addons/web/static/src/js/views/graph/graph_view.js
+++ addons/web/static/src/js/views/graph/graph_view.js
@@ -16,8 +16,8 @@ var GraphRenderer = require('web.GraphRenderer');
 var _t = core._t;
 var _lt = core._lt;
 
-var GROUPABLE_TYPES =
-    ['many2one', 'char', 'boolean', 'selection', 'date', 'datetime'];
+var controlPanelViewParameters = require('web.controlPanelViewParameters');
+var GROUPABLE_TYPES = controlPanelViewParameters.GROUPABLE_TYPES;
 
 var GraphView = AbstractView.extend({
     display_name: _lt('Graph'),
--- addons/web/static/src/js/views/pivot/pivot_view.js
+++ addons/web/static/src/js/views/pivot/pivot_view.js
@@ -16,8 +16,8 @@ var PivotRenderer = require('web.PivotRenderer');
 var _t = core._t;
 var _lt = core._lt;
 
-var GROUPABLE_TYPES =
-    ['many2one', 'char', 'boolean', 'selection', 'date', 'datetime'];
+var controlPanelViewParameters = require('web.controlPanelViewParameters');
+var GROUPABLE_TYPES = controlPanelViewParameters.GROUPABLE_TYPES;
 
 var PivotView = AbstractView.extend({
     display_name: _lt('Pivot'),
