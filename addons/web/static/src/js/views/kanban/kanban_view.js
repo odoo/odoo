2eb76c2a960cb7af54ac53d7e7bd36fb7e6c020a
Pierre Paridans <app@odoo.com>
2019-04-12 13:49:15 +0000

    [IMP] lunch,web: don't disable SearchPanel on mobile
    SearchPanel widget in kanban views has been introduced in commit
    https://github.com/odoo/odoo/pull/30517/commits/25c9355c02485e85914430188f3281246fc54cda.
    But this feature was disabled for mobile.
    
    This commit removes unneeded differentiation between desktop and mobile
    for the SearchPanel.
    
    It also adapt related tests to reflect this change.
    
    Task ID: 1917277

--- addons/web/static/src/js/views/kanban/kanban_view.js
+++ addons/web/static/src/js/views/kanban/kanban_view.js
@@ -188,9 +188,7 @@ var KanbanView = BasicView.extend({
      */
     _processNode: function (node, fv) {
         if (node.tag === 'searchpanel') {
-            if (!config.device.isMobile) {
-                this._processSearchPanelNode(node, fv);
-            }
+            this._processSearchPanelNode(node, fv);
             return false;
         }
         return this._super.apply(this, arguments);
