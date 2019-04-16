e27802637f0b5c109d90afac706230e59867e199
Shreya Modi <smo@odoo.com>
2019-04-16 15:04:30 +0530

    [IMP] mrp: should no show manufacturing order in the scrap

--- addons/stock/wizard/stock_warn_insufficient_qty.py
+++ addons/stock/wizard/stock_warn_insufficient_qty.py
@@ -36,5 +36,7 @@ class StockWarnInsufficientQtyScrap(models.TransientModel):
         return self.scrap_id.do_scrap()
 
     def action_cancel(self):
+        seq = self.env.ref('stock.sequence_stock_scrap')
+        seq.number_next_actual -= 1
         # FIXME in master: we should not have created the scrap in a first place
         return self.scrap_id.sudo().unlink()
