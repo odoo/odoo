d08c286c7701a7cd22a46ba4cfa27cac8b26ba98
Florent de Labarre <florent.mirieu@gmail.com>
2019-04-16 22:45:33 +0200

    WIP

--- addons/stock/models/stock_inventory.py
+++ addons/stock/models/stock_inventory.py
@@ -190,12 +190,16 @@ class Inventory(models.Model):
             'state': 'draft'
         })
 
+ 
     def action_start(self):
-        for inventory in self.filtered(lambda x: x.state not in ('done','cancel')):
-            vals = {'state': 'confirm', 'date': fields.Datetime.now()}
-            if (inventory.filter != 'partial') and not inventory.line_ids:
-                vals.update({'line_ids': [(0, 0, line_values) for line_values in inventory._get_inventory_lines_values()]})
-            inventory.write(vals)
+        with self.env.norecompute():
+            for inventory in self.filtered(lambda x: x.state not in ('done', 'cancel')):
+                if (inventory.filter != 'partial') and not inventory.line_ids:
+                    for val in inventory._get_inventory_lines_values():
+                        val['inventory_id'] = inventory.id
+                        self.env['stock.inventory.line'].create(val)
+                inventory.write({'state': 'confirm', 'date': fields.Datetime.now()})
+        self.recompute()
         return True
 
     def action_inventory_line_tree(self):
@@ -210,67 +214,77 @@ class Inventory(models.Model):
         }
         return action
 
-    def _get_inventory_lines_values(self):
+def _get_inventory_lines_values(self):
         # TDE CLEANME: is sql really necessary ? I don't think so
         locations = self.env['stock.location'].search([('id', 'child_of', [self.location_id.id])])
-        domain = ' location_id in %s AND active = TRUE'
+        domain = ' sq.location_id in %s AND pp.active = TRUE'
         args = (tuple(locations.ids),)
 
         vals = []
         Product = self.env['product.product']
-        # Empty recordset of products available in stock_quants
-        quant_products = self.env['product.product']
         # Empty recordset of products to filter
         products_to_filter = self.env['product.product']
 
         # case 0: Filter on company
         if self.company_id:
-            domain += ' AND company_id = %s'
+            domain += ' AND sq.company_id = %s'
             args += (self.company_id.id,)
-        
-        #case 1: Filter on One owner only or One product for a specific owner
+
+        # case 1: Filter on One owner only or One product for a specific owner
         if self.partner_id:
-            domain += ' AND owner_id = %s'
+            domain += ' AND sq.owner_id = %s'
             args += (self.partner_id.id,)
-        #case 2: Filter on One Lot/Serial Number
+        # case 2: Filter on One Lot/Serial Number
         if self.lot_id:
-            domain += ' AND lot_id = %s'
+            domain += ' AND sq.lot_id = %s'
             args += (self.lot_id.id,)
-        #case 3: Filter on One product
+        # case 3: Filter on One product
         if self.product_id:
-            domain += ' AND product_id = %s'
+            domain += ' AND sq.product_id = %s'
             args += (self.product_id.id,)
             products_to_filter |= self.product_id
-        #case 4: Filter on A Pack
+        # case 4: Filter on A Pack
         if self.package_id:
-            domain += ' AND package_id = %s'
+            domain += ' AND sq.package_id = %s'
             args += (self.package_id.id,)
-        #case 5: Filter on One product category + Exahausted Products
+        # case 5: Filter on One product category + Exahausted Products
         if self.category_id:
             categ_products = Product.search([('categ_id', '=', self.category_id.id)])
-            domain += ' AND product_id = ANY (%s)'
+            domain += ' AND sq.product_id = ANY (%s)'
             args += (categ_products.ids,)
             products_to_filter |= categ_products
 
-        self.env.cr.execute("""SELECT product_id, sum(quantity) as product_qty, location_id, lot_id as prod_lot_id, package_id, owner_id as partner_id
-            FROM stock_quant
-            LEFT JOIN product_product
-            ON product_product.id = stock_quant.product_id
+        self.env.cr.execute(
+            """SELECT
+                sq.product_id,
+                sum(sq.quantity) as product_qty,
+                sq.location_id,
+                sq.lot_id as prod_lot_id,
+                sq.package_id,
+                sq.owner_id as partner_id,
+                MIN(pt.uom_id) as product_uom_id
+            FROM stock_quant AS sq
+            LEFT JOIN product_product AS pp ON pp.id = sq.product_id
+            LEFT JOIN product_template AS pt ON pt.id = pp.product_tmpl_id
             WHERE %s
-            GROUP BY product_id, location_id, lot_id, package_id, partner_id """ % domain, args)
+            GROUP BY sq.product_id, sq.location_id, sq.lot_id, sq.package_id, sq.owner_id """ % domain, args)
 
+        quant_product_ids = []
         for product_data in self.env.cr.dictfetchall():
             # replace the None the dictionary by False, because falsy values are tested later on
             for void_field in [item[0] for item in product_data.items() if item[1] is None]:
                 product_data[void_field] = False
             product_data['theoretical_qty'] = product_data['product_qty']
             if product_data['product_id']:
-                product_data['product_uom_id'] = Product.browse(product_data['product_id']).uom_id.id
-                quant_products |= Product.browse(product_data['product_id'])
+                quant_product_ids.append(product_data['product_id'])
             vals.append(product_data)
         if self.exhausted:
+            quant_products = Product.browse(quant_product_ids)
             exhausted_vals = self._get_exhausted_inventory_line(products_to_filter, quant_products)
             vals.extend(exhausted_vals)
+        
+        #prefetch type, to win time during the _check_product_id(self) on stock.inventory.line
+        Product.browse(quant_product_ids).read(['type'])
         return vals
 
     def _get_exhausted_inventory_line(self, products, quant_products):
@@ -287,10 +301,13 @@ class Inventory(models.Model):
         else:
             exhausted_domain += [('id', 'not in', quant_products.ids)]
         exhausted_products = self.env['product.product'].search(exhausted_domain)
-        for product in exhausted_products:
+        
+        #prefetch type, to win time during the _check_product_id(self) on stock.inventory.line
+        exhausted_products.read(['type'])
+        for product_id in exhausted_products.ids:
             vals.append({
                 'inventory_id': self.id,
-                'product_id': product.id,
+                'product_id': product_id,
                 'location_id': self.location_id.id,
             })
         return vals
