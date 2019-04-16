07680342de7c0d78fc2646daf8b1eefc43c7604c
svs-odoo <svs@odoo.com>
2019-04-16 14:09:07 +0200

    [MOV] mrp_account: merge with mrp_bom_cost
    Merges mrp_bom_cost into mrp_account since they have both the same
    depedences.
    
    Task #1964809

--- addons/mrp_account/__manifest__.py
+++ addons/mrp_account/__manifest__.py
@@ -11,9 +11,17 @@ Analytic Accounting in MRP
 ==========================
 
 * Cost structure report
+
+Also, allows to compute the cost of the product based on its BoM, using the costs of its components and work center operations.
+It adds a button on the product itself but also an action in the list view of the products.
+If the automated inventory valuation is active, the necessary accounting entries will be created.
+
 """,
     'website': 'https://www.odoo.com/page/manufacturing',
     'depends': ['mrp', 'stock_account'],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "data": ["views/product_views.xml"],
     'installable': True,
     'auto_install': True,
 }
