e0b33e280e6f9739e5015f936674456dfd0f8bcd
MaxLarue <mla@odoo.com>
2019-04-17 12:36:01 +0000

    [IMP] sale_timesheet: overview usability and styling
    In order to make the overview more understandable, we provide some modifications:
    
    - remove some value when they are zero ("No task found", ...)
    - the whole middle column (percentages) is merged inside the hours one
    - provide some tooltips
    - renaming titles
    - ...
    
    To do so and to avoid leaving empty lines or doing complicated template branching, the
    above table is split in two different tables, so that we can remove lines independently.
    
    We also added a new category in the overview, concerning timesheets that are related to a cancelled so. This meant tweaking the project_overview plan_prepare_value method, in order to have the new numbers calculated and keep the others consitent.
    
    Task-1968121
    
    closes odoo/odoo#33015
    
    Signed-off-by: Jérome Maes (jem) <jem@openerp.com>

--- addons/sale_timesheet/models/account.py
+++ addons/sale_timesheet/models/account.py
@@ -15,9 +15,9 @@ class AccountAnalyticLine(models.Model):
         return expression.OR([domain, [('qty_delivered_method', '=', 'timesheet')]])
 
     timesheet_invoice_type = fields.Selection([
-        ('billable_time', 'Billable Time'),
-        ('billable_fixed', 'Billable Fixed'),
-        ('non_billable', 'Non Billable'),
+        ('billable_time', 'Billed on Timesheets'),
+        ('billable_fixed', 'Billed at a Fixed price'),
+        ('non_billable', 'Non Billable Tasks'),
         ('non_billable_project', 'No task found')], string="Billable Type", compute='_compute_timesheet_invoice_type', compute_sudo=True, store=True, readonly=True)
     timesheet_invoice_id = fields.Many2one('account.invoice', string="Invoice", readonly=True, copy=False, help="Invoice created from the timesheet")
 
