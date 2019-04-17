fdeccca22e89ea5b5bcf4d6677536b7edfe2e655
Raphael Collet <rco@odoo.com>
2019-04-17 10:46:29 +0200

    [REF] api: remove deprecated env.in_onchange

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -757,7 +757,7 @@ class HolidaysRequest(models.Model):
         current_employee = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
         is_officer = self.env.user.has_group('hr_holidays.group_hr_holidays_user')
         is_manager = self.env.user.has_group('hr_holidays.group_hr_holidays_manager')
-        if self.env.in_onchange:
+        if not all(self._ids):
             return
         for holiday in self:
             val_type = holiday.holiday_status_id.validation_type
