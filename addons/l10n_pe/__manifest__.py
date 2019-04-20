cc1219cc18769d0867a991278a3108e9248c46da
Nhomar G Hernandez m <nhomar@vauxoo.com>
2019-04-20 08:40:25 -0500

    [REF] l10n_pe: Current CoA for Perú is  unusable.
    - Moved parent accounts as groups.
    - Set minimal set of accounts to work with the most basic
      set of features in odoo accounting related.
    - Prepare coding relatable with the official PCGE.
    - Cleanup the taxes in order to use the correct accounts.
    - Clean the author (this module is nothing like the original one).
    - Removed account types, those should be into the correct module.
      they are not necessary **yet** to create the reports due to the
      fact that they do not depend on the account types but in the
      accounts themselves on the PCGE and they will be anoying for
      multi-localization environments.

--- addons/l10n_pe/__manifest__.py
+++ addons/l10n_pe/__manifest__.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-# Copyright (c) 2011 Cubic ERP - Teradata SAC. (http://cubicerp.com).
-
 {
     'name': 'Peru - Accounting',
-    'description': """
-Peruvian accounting chart and tax localization. According the PCGE 2010.
-========================================================================
-
-Plan contable peruano e impuestos de acuerdo a disposiciones vigentes de la
-SUNAT 2011 (PCGE 2010).
-
-    """,
-    'author': ['Cubic ERP'],
+    "version": "2.0",
+    'summary': "PCGE Simplified",
     'category': 'Localization',
+    'description': """
+Minimal set of accounts to start to work in Perú.
+=================================================
+
+The usage of this CoA must refer to the official documentation on MEF.
+
+https://www.mef.gob.pe/contenidos/conta_publ/documentac/VERSION_MODIFICADA_PCG_EMPRESARIAL.pdf 
+    """,
     'depends': ['account'],
+    'author': ['Vauxoo, Odoo'],
     'data': [
+        'data/account_types.xml',
         'data/l10n_pe_chart_data.xml',
+        'data/account.group.csv',
         'data/account.account.template.csv',
         'data/l10n_pe_chart_post_data.xml',
-        'data/account_data.xml',
         'data/account_tax_data.xml',
         'data/account_chart_template_data.xml',
     ],
