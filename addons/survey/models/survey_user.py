067ceabc59ab8cc587f06d2ded026a70a5cfed9f
Aurélien Warnon <awa@odoo.com>
2019-04-23 12:05:29 +0000

    [REF] survey: add a "_mark_done" method to improve answers state management
    As other modules than survey (namely hr_skills) now need a hook to add specific
    behavior when a survey answer is marked as 'done', we removed the '_send_certification'
    method and replaced it with a '_mark_done' method.
    
    It will make it easier to override this method as it has a more generic meaning.
    
    closes odoo/odoo#32880
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/survey/models/survey_user.py
+++ addons/survey/models/survey_user.py
@@ -137,11 +137,14 @@ class SurveyUserInput(models.Model):
                 > user_input.start_datetime + relativedelta(minutes=user_input.survey_id.time_limit)
 
     @api.multi
-    def _send_certification(self):
-        """ Will send the certification email with attached document if
+    def _mark_done(self):
+        """ This method will:
+        1. mark the state as 'done'
+        2. send the certification email with attached document if
         - The survey is a certification
         - It has a certification_mail_template_id set
         - The user succeeded the test """
+        self.write({'state': 'done'})
         for user_input in self:
             if user_input.survey_id.certificate and user_input.quizz_passed and user_input.survey_id.certification_mail_template_id:
                 user_input.survey_id.certification_mail_template_id.send_mail(user_input.id, notif_layout="mail.mail_notification_light")
