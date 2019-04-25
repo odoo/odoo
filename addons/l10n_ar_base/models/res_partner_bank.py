e2b365072282aebcd477d4a6ddec661a98409042
Katherine Zaoral <kz@adhoc.com.ar>
2019-04-25 13:56:07 -0300

    [FIX] l10n_ar_base: use pycompat for 12.0 since zip is not supported
    When moving to version 13 this change can be reverted

--- addons/l10n_ar_base/models/res_partner_bank.py
+++ addons/l10n_ar_base/models/res_partner_bank.py
@@ -3,6 +3,7 @@
 # directory
 ##############################################################################
 from odoo import fields, models, api, _
+from odoo.tools import pycompat
 from odoo.exceptions import ValidationError
 
 
@@ -26,6 +27,9 @@ class ResPartnerBank(models.Model):
     def is_valid_cbu(self):
         """ Ensure that the given CBU number is a valid number
         """
+        # TODO change pycompat.izip() to zip() when we get to version 13.0
+        # taking into account what was commented here
+        # https://github.com/odoo/odoo/pull/16811
         self.ensure_one()
         cbu = self.l10n_ar_cbu
         if type(cbu) == int:
@@ -33,12 +37,13 @@ class ResPartnerBank(models.Model):
         cbu = cbu.strip()
         if len(cbu) != 22:
             return False
-        s1 = sum(int(a) * b for a, b in zip(cbu[0:7], (7, 1, 3, 9, 7, 1, 3)))
+        s1 = sum(int(a) * b for a, b in pycompat.izip(
+            cbu[0:7], (7, 1, 3, 9, 7, 1, 3)))
         d1 = (10 - s1) % 10
         if d1 != int(cbu[7]):
             return False
         s2 = sum(int(a) * b
-                 for a, b in zip(cbu[8:-1],
+                 for a, b in pycompat.izip(cbu[8:-1],
                                  (3, 9, 7, 1, 3, 9, 7, 1, 3, 9, 7, 1, 3)))
         d2 = (10 - s2) % 10
         if d2 != int(cbu[-1]):
