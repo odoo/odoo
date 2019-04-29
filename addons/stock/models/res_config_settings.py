15716642e1975b4f2f29916011a4be29804cda88
Arnaud Baes <arb@odoo.com>
2019-04-29 15:19:06 +0000

    [IMP] sale_stock: Show SN/LN on invoices
    With a settings added in the stock settings, it is now possible to
    show the SN/LN for every product in an invoice.
    
    To achieve this, we fetch every `stock_move_line` from the related SO,
    then we keep only the returned and delivered moves. We then filter those
    move lines using a range of dates, the starting date being the date of the last
    invoice related to the SO, the ending date being the current date. Finally,
    we compute de quantities delivered with every related sn/ln in order to show
    them on the invoice.
    
    TaskID: 1923220
    
    closes odoo/odoo#33022
    
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- addons/stock/models/res_config_settings.py
+++ addons/stock/models/res_config_settings.py
@@ -16,7 +16,7 @@ class ResConfigSettings(models.TransientModel):
         help="Track following dates on lots & serial numbers: best before, removal, end of life, alert. \n Such dates are set automatically at lot/serial number creation based on values set on the product (in days).")
     group_stock_production_lot = fields.Boolean("Lots & Serial Numbers",
         implied_group='stock.group_production_lot')
-    group_lot_on_delivery_slip = fields.Boolean("Display Lots & Serial Numbers",
+    group_lot_on_delivery_slip = fields.Boolean("Display Lots & Serial Numbers on Delivery Slips",
         implied_group='stock.group_lot_on_delivery_slip')
     group_stock_tracking_lot = fields.Boolean("Delivery Packages",
         implied_group='stock.group_tracking_lot')
