dc8ccebaffb672bb9a6a70cfe81464e4b5c25e2a
sbbaro <sbb@odoo.com>
2019-04-30 13:36:35 +0200

    [FIX] account.invoice message origin error
    The message to be posted in the chatter when an invoice
    related to a purchase order was modified was triggering an error
    because of a problem in the sintax of the formatting.
    
    opw 1971223

--- addons/purchase/models/account_invoice.py
+++ addons/purchase/models/account_invoice.py
@@ -269,7 +269,7 @@ class AccountInvoice(models.Model):
             #To get all po reference when updating invoice line or adding purchase order reference from vendor bill.
             purchase = (purchase_old | purchase_new) - (purchase_old & purchase_new)
             if purchase:
-                message = _("This vendor bill has been modified from: %s") % (",".join(["<a href=# data-oe-model=purchase.order data-oe-id="+str(order.id)+">"+order.name+"</a>" for order in purchase]))
+                message = _("This vendor bill has been modified from: %s" % (",".join(["<a href=# data-oe-model=purchase.order data-oe-id="+str(order.id)+">"+order.name+"</a>" for order in purchase])))
                 invoice.message_post(body=message)
         return result
 
