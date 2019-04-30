07e97089d1d6159468797a572d0ccde8568d26b0
qmo-odoo <qmo@odoo.com>
2019-04-30 08:14:44 +0000

    [FIX] website_slides_survey: fix some certifications issues in eLearning frontend
    This commit fixes a few issues with courses which have certification
    
     * fix links to already passed certifications: currently it crashed, now it
       redirects to the certification end screen;
     * "Begin your certification now" green box link now has same behaviour
       as links in the list view. It redirects to either the start certification
       screen or the first slide in fullscreen mode. Moreover once the user has
       started the course (at least one completed slide) this box disappears as
       it is not really useful anymore;
    
    Some dead code is removed.
    
    Commit linked to task ID 1978530 (fixes linked to internal testing).

--- addons/website_slides_survey/static/src/js/slides_course_fullscreen_player.js
+++ addons/website_slides_survey/static/src/js/slides_course_fullscreen_player.js
@@ -9,15 +9,7 @@ Fullscreen.include({
     xmlDependencies: (Fullscreen.prototype.xmlDependencies || []).concat(
         ["/website_slides_survey/static/src/xml/website_slides_fullscreen.xml"]
     ),
-    _preprocessSlideData: function (slidesDataList){
-        slidesDataList = this._super.apply(this, arguments);
-        slidesDataList.forEach(function (slideData, index){
-            if (slideData.type === "certification"){
-                slideData.certificationUrl = '/slides_survey/slide/get_certification_url?slide_id=' + slideData.id;
-            }
-        });
-        return slidesDataList;
-    },
+
     /**
      * Extend the _renderSlide method so that slides of type "certification"
      * are also taken into account and rendered correctly
