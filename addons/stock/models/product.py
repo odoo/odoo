odoo/odoo

commit 15ea753a0fdd04b5966f0bd5e62b969c22664555

[FIX] stock: show product qty on searched location
The location_id and warehouse_id fields on product.template are just
dummy field intended to add something in the context that will be used
to compute the data being rendered (eg. for location, the quantity
displayed is the quantity of the product in the searched location).

But the feature was broken at a point, and has been solved in 11.0 with
7c7b0992734.

This solution was not implemented in 10.0 up to saas-15 since this is
not acceptable for a stable version.

This changeset is only for 10.0 up to saas-15 to implement the fix
differently.

opw-1945417
closes #31475

addons/stock/models/product.py
@@ -4,6 +4,7 @@
 from odoo import api, fields, models, _
 from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError
+from odoo.osv import expression
 from odoo.tools.float_utils import float_round
 from datetime import datetime
 import operator as py_operator
@@ -370,6 +371,15 @@ class Product(models.Model):
     def action_view_routes(self):
         return self.mapped('product_tmpl_id').action_view_routes()
 
+    @api.model
+    def search(self, domain=None, *args, **kwargs):
+        # ONLY FOR 10.0 UP TO SAAS-15
+        # ignore dummy fields used for search context²
+        for index in range(len(domain or [])):
+            if domain[index][0] in ('location_id', 'warehouse_id'):
+                domain[index] = expression.TRUE_LEAF
+        return super(Product, self).search(domain, *args, **kwargs)
+
     @api.multi
     def write(self, values):
         res = super(Product, self).write(values)
@@ -504,6 +514,15 @@ class ProductTemplate(models.Model):
     @api.onchange('tracking')
     def onchange_tracking(self):
         return self.mapped('product_variant_ids').onchange_tracking()
+
+    @api.model
+    def search(self, domain=None, *args, **kwargs):
+        # ONLY FOR 10.0 UP TO SAAS-15
+        # ignore dummy fields used for search context
+        for index in range(len(domain or [])):
+            if domain[index][0] in ('location_id', 'warehouse_id'):
+                domain[index] = expression.TRUE_LEAF
+        return super(ProductTemplate, self).search(domain, *args, **kwargs)
 
     @api.multi
     def write(self, vals):

commit 08425316d311d4569d5d5634248fc1fd18d6a7f5

[FIX] stock: rewrite search override style for RPC
Putting *args and **kwargs confuses the function `odoo.api.split_context`.
This causes that a call to `search` via XMLRPC gets the wrong argument: takes
the first argument of ``args`` as the context.

Example which ignores the `count=True`::

```
api.call_kw(self.env['product.product'], 'search', (
    [], False, False, None, True, {'lang': 'en_US'}
), {})
```

would ignore the `count=True` and use the wrong argument for context.

Issue introduced in commit 15ea753a.

Since:

`def search(self, model=None, *args, **kwargs)`

is equivalent to:

`def search(self, args, offset=0, limit=None, order=None, count=False):`

because the call to the super will anyway call the parent method, we can
change the method arguments.

note: this is only for [10.0,saas-14] where 15ea753a is.

closes #33147

Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>


Co-authored-by: Nicolas Lempereur <nle-odoo@users.noreply.github.com>

addons/stock/models/product.py
@@ -372,13 +372,13 @@ class Product(models.Model):
         return self.mapped('product_tmpl_id').action_view_routes()
 
     @api.model
-    def search(self, domain=None, *args, **kwargs):
+    def search(self, args, offset=0, limit=None, order=None, count=False):
         # ONLY FOR 10.0 UP TO SAAS-15
         # ignore dummy fields used for search context²
-        for index in range(len(domain or [])):
-            if domain[index][0] in ('location_id', 'warehouse_id'):
-                domain[index] = expression.TRUE_LEAF
-        return super(Product, self).search(domain, *args, **kwargs)
+        for index in range(len(args or [])):
+            if args[index][0] in ('location_id', 'warehouse_id'):
+                args[index] = expression.TRUE_LEAF
+        return super(Product, self).search(args, offset=offset, limit=limit, order=order, count=count)
 
     @api.multi
     def write(self, values):
@@ -516,13 +516,13 @@ class ProductTemplate(models.Model):
         return self.mapped('product_variant_ids').onchange_tracking()
 
     @api.model
-    def search(self, domain=None, *args, **kwargs):
+    def search(self, args, offset=0, limit=None, order=None, count=False):
         # ONLY FOR 10.0 UP TO SAAS-15
         # ignore dummy fields used for search context
-        for index in range(len(domain or [])):
-            if domain[index][0] in ('location_id', 'warehouse_id'):
-                domain[index] = expression.TRUE_LEAF
-        return super(ProductTemplate, self).search(domain, *args, **kwargs)
+        for index in range(len(args or [])):
+            if args[index][0] in ('location_id', 'warehouse_id'):
+                args[index] = expression.TRUE_LEAF
+        return super(ProductTemplate, self).search(args, offset=offset, limit=limit, order=order, count=count)
 
     @api.multi
     def write(self, vals):