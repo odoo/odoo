5d6b78b2c2817f12467e7e3f8df36872b517f5de
Ravi Gohil <rgo@odoo.com>
2019-05-07 11:56:46 +0530

    fixes and improvements

--- addons/l10n_il/__manifest__.py
+++ addons/l10n_il/__manifest__.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Copyright (C) 2014 InnOpen Group Kft (<http://www.innopen.eu>).
-
 {
     'name': 'Israel - Accounting',
-    'version': '2.0',
+    'version': '1.0',
     'category': 'Localization',
     'description': """
 This is the latest basic Israelian localisation necessary to run Odoo in IL:
-===========================================================================
+============================================================================
 
-This module consists :
-
+This module consists:
  - Generic Israelian chart of accounts
  - Israelian taxes
  """,
@@ -24,7 +21,7 @@ This module consists :
         'data/account.account.template.csv',
         'data/account_data.xml',
         'data/account_tax_template_data.xml',
-        'data/l10n_il_chart_post_data.xml',
+        'data/account_chart_template_post_data.xml',
         'data/account_chart_template_configure_data.xml',
     ],
 }
