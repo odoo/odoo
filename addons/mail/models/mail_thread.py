b7c1e941b02adf5010d107806fa4bac9604c11d6
Jairo Llopis <yajo.sk8@gmail.com>
2019-05-08 08:20:18 +0000

    [IMP] mail: allow auto_commit parameter with templated message posts
    The underlying `mail.compose.message` model has a useful `auto_commit`
    argument when sending emails, which is unavailable when posting messages with
    template through message_post_with_template.
    
    This patch simply exposes the preexisting feature, to ease developments that
    need it.
    
    closes odoo/odoo#37783
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -1923,7 +1923,7 @@ class MailThread(models.AbstractModel):
             kwargs['body'] = rendered_template
             record.message_post_with_template(False, **kwargs)
 
-    def message_post_with_template(self, template_id, email_layout_xmlid=None, **kwargs):
+    def message_post_with_template(self, template_id, email_layout_xmlid=None, auto_commit=False, **kwargs):
         """ Helper method to send a mail with a template
             :param template_id : the id of the template to render to create the body of the message
             :param **kwargs : parameter to create a mail.compose.message woaerd (which inherit from mail.message)
@@ -1952,7 +1952,7 @@ class MailThread(models.AbstractModel):
         if template_id:
             update_values = composer.onchange_template_id(template_id, kwargs['composition_mode'], self._name, res_id)['value']
             composer.write(update_values)
-        return composer.send_mail()
+        return composer.send_mail(auto_commit=auto_commit)
 
     def message_notify(self, partner_ids=False, parent_id=False, model=False, res_id=False,
                        author_id=None, email_from=None, body='', subject=False, **kwargs):
