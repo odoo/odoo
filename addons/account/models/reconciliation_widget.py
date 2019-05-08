0543597af91b248bbed86a05b3e6ae46c66770aa
Nicolas Martinelli <nim@odoo.com>
2019-05-08 09:59:20 +0000

    [FIX] account: reconciliation multi-company
    - Create 2 companies C1 and C2
    - Do not share partners between companies
    - Create a partner P1 in C1:
    Name: PartnerA
    AccountNumber: 1234
    - Create a partner P2 in C2:
    Name: PartnerA
    AccountNumber: 1234
    - Create an invoice in C2 for P2, validate and pay
    - Add the fields `account_number` and `partner_name` to the bank
    statement lines for convenience.
    - Create a bank statement with a line:
    Partner Name: PartnerA
    Account Number: 1234
    - Run the following query (on which no record rule is applied for bank
    statement reconciliation):
    
    ```sql
    SELECT
    st_line.id                          AS id,
    COALESCE(p1.id,p2.id,p3.id)         AS partner_id,
    p1.id AS p1,
    p2.id AS p2,
    p3.id AS p3
    FROM account_bank_statement_line st_line
    LEFT JOIN res_partner_bank bank ON bank.id = st_line.bank_account_id OR bank.acc_number = st_line.account_number
    LEFT JOIN res_partner p1 ON st_line.partner_id=p1.id
    LEFT JOIN res_partner p2 ON bank.partner_id=p2.id
    LEFT JOIN res_partner p3 ON p3.name ILIKE st_line.partner_name
    WHERE st_line.id IN (XX);
    ```
    
    The result is:
    
    |       id |   partner_id|      p1|    p2|    p3|
    |----------|-------------|--------|------|------|
    |        XX|           P1|    null|    P1|    P1|
    |        XX|           P2|    null|    P2|    P1|
    
    P1 should not appear in any of the results. In some configuration, P1
    will be selected for matching and will trigger an access error.
    
    Commit 908a11172289b097988ae5846be97646e02b6652 introduced the record
    rules for `p3`, but was wrongly set at the end of the query. We
    introduce the rules for `p3` as well as `bank`, so the result is now:
    
    |       id |   partner_id|      p1|    p2|    p3|
    |----------|-------------|--------|------|------|
    |        XX|           P2|    null|    P2|  null|
    
    Which is the expected result.
    
    Fixes #32954
    opw-1974649
    
    closes odoo/odoo#33245
    
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/account/models/reconciliation_widget.py
+++ addons/account/models/reconciliation_widget.py
@@ -79,28 +79,43 @@ class AccountReconciliation(models.AbstractModel):
 
     @api.model
     def _get_bank_statement_line_partners(self, st_lines):
+        params = []
+
+        # Add the res.partner.ban's IR rules. In case partners are not shared between companies,
+        # identical bank accounts may exist in a company we don't have access to.
+        ir_rules_query = self.env['res.partner.bank']._where_calc([])
+        self.env['res.partner.bank']._apply_ir_rules(ir_rules_query, 'read')
+        from_clause, where_clause, where_clause_params = ir_rules_query.get_sql()
+        if where_clause:
+            where_bank = ('AND %s' % where_clause).replace('res_partner_bank', 'bank')
+            params += where_clause_params
+        else:
+            where_bank = ''
+
+        # Add the res.partner's IR rules. In case partners are not shared between companies,
+        # identical partners may exist in a company we don't have access to.
+        ir_rules_query = self.env['res.partner']._where_calc([])
+        self.env['res.partner']._apply_ir_rules(ir_rules_query, 'read')
+        from_clause, where_clause, where_clause_params = ir_rules_query.get_sql()
+        if where_clause:
+            where_partner = ('AND %s' % where_clause).replace('res_partner', 'p3')
+            params += where_clause_params
+        else:
+            where_partner = ''
+
         query = '''
             SELECT
                 st_line.id                          AS id,
                 COALESCE(p1.id,p2.id,p3.id)         AS partner_id
             FROM account_bank_statement_line st_line
-            LEFT JOIN res_partner_bank bank         ON bank.id = st_line.bank_account_id OR bank.acc_number = st_line.account_number
-            LEFT JOIN res_partner p1 ON st_line.partner_id=p1.id
-            LEFT JOIN res_partner p2 ON bank.partner_id=p2.id
-            LEFT JOIN res_partner p3 ON p3.name ILIKE st_line.partner_name
-            WHERE st_line.id IN %s
         '''
-        params = [tuple(st_lines.ids)]
+        query += 'LEFT JOIN res_partner_bank bank ON bank.id = st_line.bank_account_id OR bank.acc_number = st_line.account_number %s\n' % (where_bank)
+        query += 'LEFT JOIN res_partner p1 ON st_line.partner_id=p1.id \n'
+        query += 'LEFT JOIN res_partner p2 ON bank.partner_id=p2.id \n'
+        query += 'LEFT JOIN res_partner p3 ON p3.name ILIKE st_line.partner_name %s\n' % (where_partner)
+        query += 'WHERE st_line.id IN %s'
 
-        # Add the res.partner's IR rules to the WHERE clause. In case partners are not shared
-        # between companies, identical partners may exist in a company we don't have access to.
-        ir_rules_query = self.env['res.partner']._where_calc([])
-        self.env['res.partner']._apply_ir_rules(ir_rules_query, 'read')
-        from_clause, where_clause, where_clause_params = ir_rules_query.get_sql()
-        where_p3 = (" AND %s" % where_clause).replace('res_partner', 'p3') if where_clause else ''
-        if where_p3:
-            query += where_p3
-            params += where_clause_params
+        params += [tuple(st_lines.ids)]
 
         self._cr.execute(query, params)
 
