16c5890c96a551fdd4e91d74aa1426d9ed356f59
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-09 09:43:41 -0300

    [REF] l10n_latam_documents: rename module from l10n_latam_documents to l10n_latam_document

--- addons/l10n_latam_document/__manifest__.py
+++ addons/l10n_latam_document/__manifest__.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    "name": "Latam Documents",
+    "name": "Latam Document",
     "version": "12.0.1.0.0",
     "author": "ADHOC SA",
     "category": "Localization",
