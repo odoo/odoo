8b3b14cc536a53b2507ce38b22983e903b840df9
Mitali Patel <mpa@odoo.com>
2019-05-09 11:37:26 +0200

    [IMP] sale, website_sale : improve emails sent on confirmation and payment
    Purpose of this commit is to send the order and payment details email to user
    when they purchase any order from ecommerce or when do the payment from
    portal. Currently it was sending quotation mail when the payment is processed.
    Now it send an email for quotation, and one email for sale confirmation that
    may contain more detailed content if coming from website (eCommerce).
    
    In sale
    
     * create new confirmation template and set the default template in sales
       settings;
     * when payment enabled in sales settings then display template option to send
       default confirmation mail when payment is processed or order signed;
     * add tool method to find the right template to use when sending the
       quotation / SO by email;
     * ensure email is sent everytime SO is confirmed;
    
    In website_sale
    
     * add option in website setting in order, to set the confirmation mail
       template;
     * ensure email is sent everytime SO is confirmed;
    
    Related to task ID 1873634
    Linked to PR #28781

--- addons/sale/tests/test_sale_signature.py
+++ addons/sale/tests/test_sale_signature.py
@@ -22,6 +22,8 @@ class TestSaleSignature(odoo.tests.HttpCase):
         })
 
         # must be sent to the user so he can see it
-        sales_order.force_quotation_send()
+        email_act = sales_order.action_quotation_send()
+        email_ctx = email_act.get('context', {})
+        sales_order.with_context(**email_ctx).message_post_with_template(email_ctx.get('default_template_id'))
 
         self.start_tour("/", 'sale_signature', login="portal")
