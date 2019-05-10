c8c8413e3e86e2abc51ade48f11320de961e5e50
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-05-10 17:19:55 +0000

    [IMP] mail: enforce kw args for message_{post/notify/log}
    Since there are message_post overrides using the form `def message_post(self,
    **kwargs)` in some modules, this method is intended to be invoked with keyword
    arguments only.
    
    This commit enforces this behavior. Calls such as `message_post("body")` will
    fail regardless of which addon is installed, forcing users to use
    `message_post(body="body")`.
    
    It also fixes a message_post override in hr, and applies the same
    mechanism to message_notify, and _message_log.
    
    closes odoo/odoo#33306
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -1764,7 +1764,7 @@ class MailThread(models.AbstractModel):
         return return_values
 
     @api.returns('mail.message', lambda value: value.id)
-    def message_post(self,
+    def message_post(self, *,
                      body='', subject=None, message_type='notification',
                      email_from=None, author_id=None, parent_id=False,
                      subtype_id=False, subtype=None, partner_ids=None, channel_ids=None,
@@ -1954,7 +1954,8 @@ class MailThread(models.AbstractModel):
             composer.write(update_values)
         return composer.send_mail(auto_commit=auto_commit)
 
-    def message_notify(self, partner_ids=False, parent_id=False, model=False, res_id=False,
+    def message_notify(self, *,
+                       partner_ids=False, parent_id=False, model=False, res_id=False,
                        author_id=None, email_from=None, body='', subject=False, **kwargs):
         """ Shortcut allowing to notify partners of messages that shouldn't be 
         displayed on a document. It pushes notifications on inbox or by email depending
@@ -1997,7 +1998,7 @@ class MailThread(models.AbstractModel):
         MailThread._notify_thread(new_message, values, **notif_kwargs)
         return new_message
 
-    def _message_log(self, body='', author_id=None, email_from=None, subject=False, message_type='notification', **kwargs):
+    def _message_log(self, *, body='', author_id=None, email_from=None, subject=False, message_type='notification', **kwargs):
         """ Shortcut allowing to post note on a document. It does not perform
         any notification and pre-computes some values to have a short code
         as optimized as possible. This method is private as it does not check
