0cd4e191a0acde93af302e486026fd6a6b2afc29
Ankita Raval <anr@odoo.com>
2019-05-13 10:56:10 +0530

    [IMP] mrp : Crash when products of type service in BoM
    Before Commit:
    If a product type service in a bom, create a manufacturing order
    of product's bom they returns empty recordset because if product
    type is not storable and consumable is returns empty object of
    stock move else they returns the data dictionary.
    
    After Commit:
    If product type service in a bom, create a manufacturing order
    we returns the empty dictionary rather than empty object of stock move
    
    Add a testcases
    1) to check manufacturing order is generated of product
       type of service in bom or not.
    2) to check manufacturing order is generate from sale
       order of product type sevice in bom or not.
    
    Task ID - 1981790

--- addons/mrp/tests/test_order.py
+++ addons/mrp/tests/test_order.py
@@ -942,3 +942,36 @@ class TestMrpOrder(TestMrpCommon):
         self.assertEqual(product_produce.workorder_line_ids[0].product_uom_id, unit, 'Should be the product uom so "unit"')
         self.assertEqual(product_produce.workorder_line_ids[1].qty_to_consume, 1, 'Should be 1 unit since the tracking is serial and quantity 2.')
         self.assertEqual(product_produce.workorder_line_ids[1].product_uom_id, unit, 'should be the product uom so "unit"')
+
+    def test_product_type_service_1(self):
+        # Create finished product
+        finished_product = self.env['product.product'].create({
+            'name': 'Geyser',
+            'type': 'product',
+        })
+
+        # Create service type product
+        product_raw = self.env['product.product'].create({
+            'name': 'raw Geyser',
+            'type': 'service',
+        })
+
+        # Create bom for finish product
+        bom = self.env['mrp.bom'].create({
+            'product_id': finished_product.id,
+            'product_tmpl_id': finished_product.product_tmpl_id.id,
+            'product_uom_id': self.env.ref('uom.product_uom_unit').id,
+            'product_qty': 1.0,
+            'type': 'normal',
+            'bom_line_ids': [(5, 0), (0, 0, {'product_id': product_raw.id})]
+        })
+
+        mo_form = Form(self.env['mrp.production'])
+        mo_form.product_id = finished_product
+        mo_form.bom_id = bom
+        mo_form.product_uom_id = self.env.ref('uom.product_uom_unit')
+        mo_form.product_qty = 1
+        mo = mo_form.save()
+
+        # Check Mo is created or not
+        self.assertTrue(mo, "Mo is created")
