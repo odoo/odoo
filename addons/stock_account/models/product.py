6fde345caece824f9b6dde13e7693bd4d03ed703
Simon Lejeune <sle@openerp.com>
2019-05-15 15:24:50 +0000

    [REF] stock_account: fifo "normal" cost method with valuation layers
    We don't implement fifo negative handling in this commit.
    We make sure we run fifo in average mode to get a proper remaining_qty
    field.
    `_run_fifo` is now a method on product (since it could be user outside
    of a stock move context). It is really similar to its ancestor on the
    stock move, with a few float_is_zero and compare more and a missing
    remaing_value.
    
    We move the valuation sanity check into a proper helper,
    _sanity_check_for_valuation.
    
    We complete the _create_correction_svl method with accounting entries.
    
    task-1875873

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -98,11 +98,11 @@ class ProductProduct(models.Model):
             'unit_cost': unit_cost,
             'quantity': quantity,
         }
-        if self.cost_method in ('average'):
+        if self.cost_method in ('average', 'fifo'):
             vals['remaining_qty'] = quantity
         return vals
 
-    def _prepare_out_svl_vals(self, quantity):
+    def _prepare_out_svl_vals(self, quantity, company):
         """Prepare the values for a stock valuation layer created by a delivery.
 
         :param quantity: the quantity to value, expressed in `self.uom_id`
@@ -118,6 +118,11 @@ class ProductProduct(models.Model):
             'unit_cost': self.standard_price,
             'quantity': quantity,
         }
+        if self.cost_method in ('average', 'fifo'):
+            fifo_vals = self._run_fifo(abs(quantity), company)
+            vals['remaining_qty'] = fifo_vals.get('remaining_qty')
+            if self.cost_method == 'fifo':
+                vals.update(fifo_vals)
         return vals
 
     def _change_standard_price(self, new_price, counterpart_account_id=False):
@@ -197,6 +202,58 @@ class ProductProduct(models.Model):
 
         # Actually update the standard price.
         self.write({'standard_price': new_price})
+
+    def _run_fifo(self, quantity, company):
+        self.ensure_one()
+
+        # Find back incoming stock valuation layers (called candidates here) to value `quantity`.
+        qty_to_take_on_candidates = quantity
+        candidates = self.env['stock.valuation.layer'].search([
+            ('product_id', '=', self.id),
+            ('remaining_qty', '>', 0),
+            ('company_id', '=', company.id),
+        ])
+        new_standard_price = 0
+        tmp_value = 0  # to accumulate the value taken on the candidates
+        for candidate in candidates:
+            new_standard_price = candidate.unit_cost
+            qty_taken_on_candidate = min(qty_to_take_on_candidates, candidate.remaining_qty)
+
+            value_taken_on_candidate = qty_taken_on_candidate * candidate.unit_cost
+            candidate_vals = {
+                'remaining_qty': candidate.remaining_qty - qty_taken_on_candidate,
+            }
+            candidate.write(candidate_vals)
+
+            qty_to_take_on_candidates -= qty_taken_on_candidate
+            tmp_value += value_taken_on_candidate
+            if float_is_zero(qty_to_take_on_candidates, precision_rounding=self.uom_id.rounding):
+                break
+
+        # Update the standard price with the price of the last used candidate, if any.
+        if new_standard_price and self.cost_method == 'fifo':
+            self.sudo().with_context(force_company=company.id).standard_price = new_standard_price
+
+        # If there's still quantity to value but we're out of candidates, we fall in the
+        # negative stock use case. We chose to value the out move at the price of the
+        # last out and a correction entry will be made once `_fifo_vacuum` is called.
+        vals = {}
+        if float_is_zero(qty_to_take_on_candidates, precision_rounding=self.uom_id.rounding):
+            vals = {
+                'value': -tmp_value,
+                'unit_cost': tmp_value / quantity,
+            }
+        else:
+            assert qty_to_take_on_candidates > 0
+            last_fifo_price = new_standard_price or self.standard_price
+            negative_stock_value = last_fifo_price * -qty_to_take_on_candidates
+            tmp_value += abs(negative_stock_value)
+            vals = {
+                'remaining_qty': -qty_to_take_on_candidates,
+                'value': -tmp_value,
+                'unit_cost': last_fifo_price,
+            }
+        return vals
 
     # -------------------------------------------------------------------------
     # Miscellaneous
