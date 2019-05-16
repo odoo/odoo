7657cbf61cc448194c83b02bcaaaa2823fa2722a
rhe <rhe@odoo.com>
2019-05-16 08:15:53 +0000

    [FIX] sale: Adapt sales count on product
    When the sales count on the product form doesn't take into account the
    sales done in point of sale.
    
    So we've extend the list of done states to take the ones from point of
    sale.
    
    TASK-ID: 1985493
    
    closes odoo/odoo#33422
    
    Signed-off-by: pimodoo <pimodoo@users.noreply.github.com>

--- addons/sale/models/product_product.py
+++ addons/sale/models/product_product.py
@@ -26,12 +26,15 @@ class ProductProduct(models.Model):
         r = {}
         if not self.user_has_groups('sales_team.group_sale_salesman'):
             return r
+        date_from = fields.Datetime.to_string(fields.datetime.combine(fields.datetime.now() - timedelta(days=365),
+                                                                      time.min))
 
-        date_from = fields.Datetime.to_string(fields.datetime.now() - timedelta(days=365))
+        done_states = self.env['sale.report']._get_done_states()
+
         domain = [
-            ('state', 'in', ['sale', 'done']),
+            ('state', 'in', done_states),
             ('product_id', 'in', self.ids),
-            ('date', '>', date_from)
+            ('date', '>=', date_from),
         ]
         for group in self.env['sale.report'].read_group(domain, ['product_id', 'product_uom_qty'], ['product_id']):
             r[group['product_id'][0]] = group['product_uom_qty']
