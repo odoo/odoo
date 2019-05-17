753ab1ac2a54e92d10017c4d94f54a38d3d5b063
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-17 18:46:29 -0300

    [REM] test_l10n_ar: remove not needed code for the test module.

--- addons/test_l10n_ar/__init__.py
+++ addons/test_l10n_ar/__init__.py
@@ -11,15 +11,6 @@ def install_l10n_ar(cr, registry):
         ('name', '=', 'l10n_ar'), ('state', '=', 'uninstalled')])
     module_ids.sudo().button_install()
 
-def set_user_company(cr, registry):
-    _logger.info('Set user company to main company to avoid unit test errors')
-    env = Environment(cr, SUPERUSER_ID, {})
-    users = env.ref('base.user_root')
-    users |= env.ref('base.user_admin')
-    users |= env.ref('base.user_demo')
-    users.write({'company_id': env.ref('base.main_company').id})
-
 def post_init_hook(cr, registry):
     _logger.info('Post init hook initialized')
     install_l10n_ar(cr, registry)
-    set_user_company(cr, registry)
