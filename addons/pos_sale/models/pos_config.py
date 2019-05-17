2bffd654518a72ecc4c9df07f9565e4789f4b195
rhe <rhe@odoo.com>
2019-05-17 13:19:26 +0000

    [IMP] point_of_sale: improve multi company
    Since the company is not stored anymore on the user and is in the
    environement, some parts of the code that was based on the current user
    company cannot work anymore.
    
    We've adapted the code to take the company from the relevent documents
    to avoid having inconsistencies through the different documents
    generated in the point of sale.
    
    TASK-ID: 1997613
    
    closes odoo/odoo#33556
    
    Signed-off-by: pimodoo <pimodoo@users.noreply.github.com>

--- addons/pos_sale/models/pos_config.py
+++ addons/pos_sale/models/pos_config.py
@@ -9,13 +9,10 @@ class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     def _get_default_pos_team(self):
-        try:
-            team = self.env.ref('sales_team.pos_sales_team', raise_if_not_found=False)
-            return team if team.active else None
-        except AccessError:
-            return None 
+        team = self.env['crm.team'].search(['|', ('company_id', '=', self.env.company.id), ('company_id', '=', False)], limit=1)
+        return team
 
     crm_team_id = fields.Many2one(
         'crm.team', string="Sales Team",
-        help="This Point of sale's sales will be related to this Sales Team.", 
+        help="This Point of sale's sales will be related to this Sales Team.",
         default=_get_default_pos_team)
