bd19a8dc382c932b1e1c5280ecd6d5dfda910b39
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-05-17 12:54:48 -0300

    [FIX] cbu validation and type selection

--- addons/l10n_ar_base/models/res_partner_bank.py
+++ addons/l10n_ar_base/models/res_partner_bank.py
@@ -1,20 +1,15 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models, api, _
-from odoo.tools import pycompat
-from odoo.exceptions import ValidationError
+from odoo import models, api, _
+import stdnum.ar.cbu
+
+def validate_cbu(cbu):
+    return stdnum.ar.cbu.validate(cbu)
 
 
 class ResPartnerBank(models.Model):
 
     _inherit = 'res.partner.bank'
-
-    @api.constrains('acc_number')
-    def check_cbu(self):
-        for rec in self.filtered(lambda x: x.acc_type == 'cbu'):
-            if rec.acc_number and not rec.is_valid_cbu():
-                raise ValidationError(
-                    _('The CBU "%s" is not valid') % rec.acc_number)
 
     @api.model
     def _get_supported_account_types(self):
@@ -24,29 +19,10 @@ class ResPartnerBank(models.Model):
         res.append(('cbu', _('CBU')))
         return res
 
-    @api.multi
-    def is_valid_cbu(self):
-        """ Ensure that the given CBU number is a valid number
-        """
-        # TODO change pycompat.izip() to zip() when we get to version 13.0
-        # taking into account what was commented here
-        # https://github.com/odoo/odoo/pull/16811
-        self.ensure_one()
-        cbu = self.acc_number
-        if type(cbu) == int:
-            cbu = "%022d" % cbu
-        cbu = cbu.strip()
-        if len(cbu) != 22:
-            return False
-        s1 = sum(int(a) * b for a, b in pycompat.izip(
-            cbu[0:7], (7, 1, 3, 9, 7, 1, 3)))
-        d1 = (10 - s1) % 10
-        if d1 != int(cbu[7]):
-            return False
-        s2 = sum(int(a) * b
-                 for a, b in pycompat.izip(cbu[8:-1],
-                                 (3, 9, 7, 1, 3, 9, 7, 1, 3, 9, 7, 1, 3)))
-        d2 = (10 - s2) % 10
-        if d2 != int(cbu[-1]):
-            return False
-        return True
+    @api.model
+    def retrieve_acc_type(self, acc_number):
+        try:
+            validate_cbu(acc_number)
+            return 'cbu'
+        except Exception:
+            return super(ResPartnerBank, self).retrieve_acc_type(acc_number)
