62978bb0193ac5f4f7bb50ad8a6203086dc3b7ca
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-17 10:12:43 -0300

    [ADD] test_l10n_ar: update main user company to avoid unit tests errors

--- addons/test_l10n_ar/__init__.py
+++ addons/test_l10n_ar/__init__.py
@@ -4,14 +4,21 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-def update_companies_country(cr, registry):
+def install_l10n_ar(cr, registry):
     _logger.info('Force installation l10n_ar module')
     env = Environment(cr, SUPERUSER_ID, {})
     module_ids = env['ir.module.module'].search([
         ('name', '=', 'l10n_ar'), ('state', '=', 'uninstalled')])
     module_ids.sudo().button_install()
 
+def set_user_company(cr, registry):
+    _logger.info('Set user company to main company to avoid unit test errors')
+    users = env.ref('base.user_root')
+    users =| env.ref('base.user_admin')
+    users =| env.ref('base.user_demo')
+    users.write({'company_id': env.ref('base.main_company')})
 
 def post_init_hook(cr, registry):
     _logger.info('Post init hook initialized')
-    update_companies_country(cr, registry)
+    install_l10n_ar(cr, registry)
+    set_user_company(cr, registry)
