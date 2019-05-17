35a1fe9538ab0273365fbad677b946e421992b68
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-05-17 12:55:29 -0300

    [REF] clean fiscal positions

--- addons/l10n_ar/models/account_fiscal_position_template.py
+++ addons/l10n_ar/models/account_fiscal_position_template.py
@@ -8,6 +8,5 @@ class AccountFiscalPositionTemplate(models.Model):
 
     l10n_ar_afip_code = fields.Char(
         'AFIP Code',
-        help='This code will be used on electronic invoice and citi '
-        'reports',
+        help='This code will be used on citi reports',
     )
