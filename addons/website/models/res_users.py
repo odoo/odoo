e76d301324ce812e0fcb0ecb7ec1eefd15af049d
Romain Derie <rde@odoo.com>
2019-05-21 10:01:58 +0000

    [FIX] website: restore the add of website_id on user at signup
    Since 1f4cde927a the website_id was not set anymore.
    
    Indeed, before this commit website_id was the field from the inherits, thus
    when writing on website_id from res.user, we would write on the partner field.
    
    But with 1f4cde927a the website_id was explicitely overrided on res.users as a
    related to res.partner (its inherits) with readonly=True.
    
    Thus, writting website_id on res.users was not working anymore.
    
    closes odoo/odoo#33521
    
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- addons/website/models/res_users.py
+++ addons/website/models/res_users.py
@@ -2,8 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 import logging
 
-from odoo import api, fields, models, tools, _
-from odoo.exceptions import ValidationError
+from odoo import api, fields, models, tools
 from odoo.http import request
 
 _logger = logging.getLogger(__name__)
@@ -12,8 +11,8 @@ _logger = logging.getLogger(__name__)
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    website_id = fields.Many2one('website', related='partner_id.website_id', store=True, related_sudo=False, readonly=True)
-    
+    website_id = fields.Many2one('website', related='partner_id.website_id', store=True, related_sudo=False, readonly=False)
+
     _sql_constraints = [
         # Partial constraint, complemented by unique index (see below). Still
         # useful to keep because it provides a proper error message when a
@@ -51,7 +50,7 @@ class ResUsers(models.Model):
     @api.model_cr_context
     def _auto_init(self):
         result = super(ResUsers, self)._auto_init()
-        # Use unique index to implement unique constraint per website, even if website_id is null 
+        # Use unique index to implement unique constraint per website, even if website_id is null
         # (not possible using a constraint)
         tools.create_unique_index(self._cr, 'res_users_login_key_unique_website_index',
             self._table, ['login', 'COALESCE(website_id,-1)'])
