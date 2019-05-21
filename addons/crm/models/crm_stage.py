c4c1d7b1aff42f4d0e1ba59147c368e770d4999c
Patrick Hoste <pko@odoo.com>
2019-05-21 09:23:14 +0200

    [REM] crm, project: remove the legend_priority fields
    Purpose of this commit is to remove of the legend_priority field in the crm
    and project addons since they are not used anywhere.
    
    Those were initially introduced at dd343c86330467ba6cfffb62e22b643a004fb764 . Purpose was to be able
    to customize displayed label of priority field used in kanban view in
    replacement of standard string. However kanban view does not support it
    anymore since whatever web refactoring and this feature will not be
    reintroduced. Let us therefore remove the fields.
    
    Linked to task ID 1935620

--- addons/crm/models/crm_stage.py
+++ addons/crm/models/crm_stage.py
@@ -38,8 +38,6 @@ class Stage(models.Model):
     requirements = fields.Text('Requirements', help="Enter here the internal requirements for this stage (ex: Offer sent to customer). It will appear as a tooltip over the stage's name.")
     team_id = fields.Many2one('crm.team', string='Sales Team', ondelete='set null',
         help='Specific team that uses this stage. Other teams will not be able to see or use this stage.')
-    legend_priority = fields.Text('Priority Management Explanation', translate=True,
-        help='Explanation text to help users using the star and priority mechanism on stages or issues that are in this stage.')
     fold = fields.Boolean('Folded in Pipeline',
         help='This stage is folded in the kanban view when there are no records in that stage to display.')
 
