d73f90f0558244532a4233ee9cfa612b989d208d
Prakash Prajapati <ppr@odoo.com>
2019-05-21 05:32:31 +0000

    [REM] project: remove notes field from project.task model.
    The notes field is not used anywhere so removed it.
    
    task-1986384
    
    Closes #33514
    
    Signed-off-by: Jérome Maes (jem) <jem@openerp.com>

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -467,7 +467,6 @@ class Task(models.Model):
         index=True,
         tracking=True,
         change_default=True)
-    notes = fields.Text(string='Notes')
     planned_hours = fields.Float("Planned Hours", help='It is the time planned to achieve the task. If this document has sub-tasks, it means the time needed to achieve this tasks and its childs.',tracking=True)
     subtask_planned_hours = fields.Float("Subtasks", compute='_compute_subtask_planned_hours', help="Computed using sum of hours planned of all subtasks created from main task. Usually these hours are less or equal to the Planned Hours (of main task).")
     user_id = fields.Many2one('res.users',
