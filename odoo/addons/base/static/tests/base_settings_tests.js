298b8b3d4cc75769ec30dffbe66b89c46917aafc
Mitali Patel <mpa@odoo.com>
2019-05-22 17:52:24 +0530

    [IMP] base: qunit test case for settings view
    Purpose of this commit is write qunit test
    case for apply settings first before open
    any action when setting is true and it will
    show a link to configure the object
    
    task-1921574

--- odoo/addons/base/static/tests/base_settings_tests.js
+++ odoo/addons/base/static/tests/base_settings_tests.js
@@ -3,11 +3,14 @@ odoo.define('base.settings_tests', function (require) {
 
 var testUtils = require('web.test_utils');
 var view_registry = require('web.view_registry');
+var AbstractStorageService = require('web.AbstractStorageService');
+var RamStorage = require('web.RamStorage');
 
 var createView = testUtils.createView;
 var BaseSettingsView = view_registry.get('base_settings');
 var createActionManager = testUtils.createActionManager;
 
+var LocalStorageServiceMock;
 
 QUnit.module('base_settings_tests', {
     beforeEach: function () {
@@ -19,6 +22,7 @@ QUnit.module('base_settings_tests', {
                 },
             },
         };
+        LocalStorageServiceMock = AbstractStorageService.extend({storage: new RamStorage()});
     }
 }, function () {
 
@@ -138,6 +142,71 @@ QUnit.module('base_settings_tests', {
             'default_get', // when we come back, we want to restart from scratch
         ]);
 
+        actionManager.destroy();
+    });
+
+    QUnit.test('save the data before open any action in settings view', async function (assert) {
+        assert.expect(7);
+
+        var actions = [{
+            id: 1,
+            name: 'Settings view',
+            res_model: 'project',
+            type: 'ir.actions.act_window',
+            views: [[1, 'form']],
+        }, {
+            id: 4,
+            name: 'Other action',
+            res_model: 'project',
+            type: 'ir.actions.act_window',
+            views: [[2, 'list']],
+        }];
+        var archs = {
+            'project,1,form': '<form string="Settings" js_class="base_settings">' +
+                    '<header>' +
+                        '<button string="Save" type="object" name="execute"/>' +
+                    '</header>' +
+                    '<div class="app_settings_block" string="CRM" data-key="crm">' +
+                        '<button name="4" string="Execute action" type="action"/>' +
+                    '</div>' +
+                '</form>',
+            'project,2,list': '<tree><field name="foo"/></tree>',
+            'project,false,search': '<search></search>',
+        };
+
+        var actionManager = await createActionManager({
+            actions: actions,
+            archs: archs,
+            data: this.data,
+            services: {
+                local_storage: LocalStorageServiceMock,
+            },
+            mockRPC: function (route, args) {
+                if (route === '/web/dataset/call_button') {
+                    return Promise.resolve();
+                }
+                if (args.method) {
+                    assert.step(args.method);
+                }
+                return this._super.apply(this, arguments);
+            },
+        });
+        testUtils.mock.intercept(actionManager, 'execute_action', function (event) {
+            assert.step(event.data.action_data.name);
+        });
+
+        await actionManager.doAction(1);
+        await testUtils.nextTick();
+        await testUtils.dom.click(actionManager.$('button[name="4"]'));
+        var local_storage_data = actionManager.call('local_storage', 'getItem', 'buttonClickedEvent');
+        assert.strictEqual(local_storage_data.attrs.name, "4" , "Should store data which having button name 4 in localstorage");
+        assert.verifySteps([
+            'load_views', // initial setting action
+            'default_get', // this is a setting view => create new record
+            'create', // when we click on action button => save
+            'read', // with save, we have a reload... (not necessary actually)
+            'execute', // save record before open action
+        ]);
         actionManager.destroy();
     });
 
