47ab4b50d35af8745b32e6fea1604226a07b492a
Kevin Baptiste <kba@odoo.com>
2019-05-23 10:28:32 +0200

    [IMP] hr_holidays: removed moved test

--- addons/hr_holidays/tests/__init__.py
+++ addons/hr_holidays/tests/__init__.py
@@ -4,7 +4,6 @@
 from . import test_access_rights
 from . import test_holidays_flow
 from . import test_hr_leave_type
-from . import test_accrual_allocations
 from . import test_change_department
 from . import test_leave_requests
 from . import test_out_of_office
