281fac8956eeb684a28d5bf7f5cd94082f6986c9
Pierre Rousseau <pro@odoo.com>
2019-05-24 12:12:44 +0200

    [IMP] sms: add SMS template and its support in SMS composer
    Purpose of this commit is to provide users template to use when sending
    SMS. It is based on what already exists for mail templates. This commit
    
      * adds templates for SMS
    
        * users can now create template for SMS Text messages similar to mail
          templates;
        * jinja syntax is supported for body like mail templates, which is why
          templates are linked to a given model;
    
      * improves the SMS composer
    
        * templates are supported in composer like the message composer in
          advanced debug mode because people don't understand how it works;
        * composer is extended to support notably mass SMS without posting messages;
    
    Templates will also be used in a near future in mass sms sending (mass
    mailing application improvement) and in marketing automation (for enterprise).
    Tests are updated and added to ensure feature works.
    
    Related to task 1922163
    Linked to PR #33510
    
    Co-Authored-By: Thibault Delavallee <tde@odoo.com>
    Co-Authored-By: Pierre Rousseau <pro@odoo.com>

--- addons/sms/models/mail_thread.py
+++ addons/sms/models/mail_thread.py
@@ -73,6 +73,14 @@ class MailThread(models.AbstractModel):
                     result[record.id] = {'partner': partner, 'sanitized': False, 'number': partner.mobile or partner.phone}
         return result
 
+    def _message_sms_using_template(self, partner_ids=False, sms_template_xmlid=False, sms_template_fallback='', put_in_queue=False):
+        self.ensure_one()
+        template = self.env.ref(sms_template_xmlid) if sms_template_xmlid else False
+        if template:
+            return self._message_sms(sms_template_fallback, partner_ids=partner_ids)
+        else:
+            return self._message_sms(sms_template_fallback, partner_ids=partner_ids)
+
     def _message_sms(self, body, subtype_id=False, partner_ids=False, number_field=False, numbers=False):
         """ Main method to post a message on a record using SMS-based notification
         method.
