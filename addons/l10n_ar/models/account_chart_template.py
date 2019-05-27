be49b06e444840beff9d2dd91c06f217b80d6491
Katherine Zaoral <kz@adhoc.com.ar>
2019-05-27 11:36:08 -0300

    fixup! [ADD] l10n_ar: Use self.env.ref() instead comparing country_id.code

--- addons/l10n_ar/models/account_chart_template.py
+++ addons/l10n_ar/models/account_chart_template.py
@@ -41,6 +41,6 @@ class AccountChartTemplate(models.Model):
             AccountChartTemplate, self)._prepare_all_journals(
             acc_template_ref, company, journals_dict=journals_dict)
 
-        if company.country_id.code == 'AR':
+        if company.country_id == self.env.ref('base.ar'):
             res = [item for item in res if item.get('type') == 'sale']
         return res
