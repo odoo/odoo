8266a5e70adff0e5623c6fe8e3e80e34e96e877e
Lucas Perais (lpe) <lpe@odoo.com>
2019-05-31 17:03:12 +0200

    Tour render report

--- addons/account/static/tests/account_dashboard_setup_bar_tests.js
+++ addons/account/static/tests/account_dashboard_setup_bar_tests.js
@@ -8,6 +8,7 @@ var _t = core._t;
 
 tour.register('account_dashboard_setup_tour', {
     test: true,
+    url: '/web',
 }, [tour.STEPS.SHOW_APPS_MENU_ITEM,
     {
         trigger: '.o_app[data-menu-xmlid="account.menu_finance"]',
@@ -38,4 +39,42 @@ tour.register('account_dashboard_setup_tour', {
     }
 ]);
 
+tour.register('account_render_report', {
+    url: '/web',
+}, [tour.STEPS.SHOW_APPS_MENU_ITEM,
+    {
+        trigger: '.o_app[data-menu-xmlid="account.menu_finance"]',
+        position: 'bottom',
+    }, {
+        content:    "wait web client",
+        trigger:    ".o_onboarding_container",
+        extra_trigger: ".breadcrumb",
+        run: function () {},
+    }, {
+        trigger: '.o_data_row:contains("'+ _t('Open') +'"):first',
+    }, {
+        trigger: '.o_control_panel button:contains("' + _t('Print') + '")',
+    }, {
+        trigger: '.o_control_panel div.o_dropdown_menu a:contains("'+ _t('Invoices without Payment')+ '")',
+    }, {
+        content: 'Wait for the report to render',
+        trigger: 'iframe body.container',
+    }, {
+        trigger: 'iframe .o_report_layout_standard h2',
+        content: 'Primary color is correct',
+        run: function () {
+            if (this.$anchor.css('color') !== "rgb(18, 52, 86)") {
+                throw new Error(_('The primary color should be the one set on the company.'));
+            }
+        },
+    }, {
+        trigger: 'iframe .o_report_layout_standard #informations div strong',
+        content: 'Secondary color is correct',
+        run: function () {
+            if (this.$anchor.css('color') !== "rgb(120, 145, 1)") {
+                throw new Error(_('The secondary color should be the one set on the company.'));
+            }
+        },
+    }
+]);
 });
--- addons/account/tests/test_account_onboarding.py
+++ addons/account/tests/test_account_onboarding.py
@@ -8,3 +8,16 @@ class TestAccountDocumentLayout(HttpCase):
         company = self.env.company_id
         company.account_onboarding_invoice_layout_state = 'not_done'
         self.start_tour("/web", 'account_dashboard_setup_tour', login='admin')
+
+    def test_render_account_document_layout(self):
+        company = self.env.company_id
+        report_layout = self.env.ref('web.report_layout_standard')
+        company.write({
+            'primary_color': '#123456',
+            'secondary_color': '#789101',
+            'external_report_layout_id': report_layout.view_id.id,
+        })
+        self.env.ref('account.account_invoices_without_payment').write({
+            'report_type': 'qweb-html',
+        })
+        self.start_tour("/web", 'account_render_report', login='admin', timeout=200)
