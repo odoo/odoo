e9be0bb17842a66445e5cf54fc849c4ac7ca73e5
qsm-odoo <qsm@odoo.com>
2019-06-03 12:35:21 +0000

    [REF] web, *: share common parts of main frontend layouts
    * http_routing, portal, rating, survey, website, website_survey,
    website_slides_survey
    
    Before this commit, the final base layout of website was a fully
    overridden layout of the one in portal, which was somehow a duplicated
    one of the login one in web, which... so lots of duplicated code.
    
    This commit is a first step towards a better organization:
    
    1) The web app defines a frontend layout (to include base frontend
    assets), with a base company logo as header.
    
    2) The portal app modifies that layout in place to include the base
    header, footer, ... It also uses a primary extension of it for
    portal pages.
    
    The survey app simply uses the above layout instead of defining its
    own (by primary extension to include its own assets for its own
    pages)
    
    Same goes for the rating app and pages.
    
    3) The website app modifies that layout in place to include the UI
    assets, to add website UI, ... This allows to create frontend apps
    which do not depend on website, with a non duplicated layout that
    will be automatically adapted if website is ever installed (this
    therefore allows to get rid of website_survey definitely)
    
    This commit also fixes the session info system and the translation URL
    on the frontend side to not require to redefine the whole session_info
    for portal, website, ... Now the frontend session_info is defined in web
    and http_routing extends it to add translation informations, then
    website extends it again to add its own elements (not to redefine them
    all as before). Note: before, http_routing defined the translation route
    but only portal was adding it in its layout...
    This is an adaptation of the work that was done with commit
    https://github.com/odoo/odoo/commit/99821fdcf89aa66ac9561a972c6823135ebf65c0
    
    Note 1: Many frontend but non-website apps (not only survey / rating)
    could probably use this too but this would be the topic of another task.
    
    Note 2: survey currently depends on http_routing but does not add itself
    in the list of frontend apps to translate, it probably should.
    
    Note 3: web_editor does currently not depend on http_routing but does
    add itself in the list of frontend apps to translate, it thus uses a
    function it does not really depend on... to check after its work-in-
    progress refactoring.
    
    task-1961045
    
    closes odoo/odoo#33825
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/portal/models/ir_http.py
+++ addons/portal/models/ir_http.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-import hashlib
-import json
 
-from odoo import api, fields, models
+from odoo import models
 from odoo.osv import expression
-from odoo.http import request
 
 
 class IrHttp(models.AbstractModel):
@@ -15,27 +12,3 @@ class IrHttp(models.AbstractModel):
     def _get_translation_frontend_modules_domain(cls):
         domain = super(IrHttp, cls)._get_translation_frontend_modules_domain()
         return expression.OR([domain, [('name', '=', 'portal')]])
-
-    @api.model
-    def get_portal_session_info(self):
-        # see http_routing/main.py
-        Modules = request.env['ir.module.module'].sudo()
-        IrHttp = request.env['ir.http'].sudo()
-        domain = IrHttp._get_translation_frontend_modules_domain()
-        modules = Modules.search(
-            expression.AND([domain, [('state', '=', 'installed')]])
-        ).mapped('name')
-        user_context = request.session.get_context() if request.session.uid else {}
-        lang = user_context.get('lang')
-        translations, _ = request.env['ir.translation'].get_translations_for_webclient(modules, lang)
-        return {
-            'is_admin': self.env.user._is_admin(),
-            'is_system': self.env.user._is_system(),
-            'is_website_user': self.env.user._is_public(),
-            'user_id': self.env.user.id,
-            'is_frontend': True,
-            'translationURL': '/website/translations',
-            'cache_hashes': {
-                'translations': hashlib.sha1(json.dumps(translations, sort_keys=True).encode()).hexdigest(),
-            },
-        }
--- addons/rating/controllers/main.py
+++ addons/rating/controllers/main.py
@@ -10,13 +10,13 @@ from odoo.tools.translate import _
 
 class Rating(http.Controller):
 
-    @http.route('/rating/<string:token>/<int:rate>', type='http', auth="public")
+    @http.route('/rating/<string:token>/<int:rate>', type='http', auth="public", website=True)
     def open_rating(self, token, rate, **kwargs):
         assert rate in (1, 5, 10), "Incorrect rating"
         rating = request.env['rating.rating'].sudo().search([('access_token', '=', token)])
         if not rating:
             return request.not_found()
-        rate_names={
+        rate_names = {
             5: _("not satisfied"),
             1: _("highly dissatisfied"),
             10: _("satisfied")
@@ -28,7 +28,7 @@ class Rating(http.Controller):
             'rate_name': rate_names[rate], 'rate': rate
         })
 
-    @http.route(['/rating/<string:token>/<int:rate>/submit_feedback'], type="http", auth="public", methods=['post'])
+    @http.route(['/rating/<string:token>/<int:rate>/submit_feedback'], type="http", auth="public", methods=['post'], website=True)
     def submit_rating(self, token, rate, **kwargs):
         rating = request.env['rating.rating'].sudo().search([('access_token', '=', token)])
         if not rating:
