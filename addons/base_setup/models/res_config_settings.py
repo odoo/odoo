6f230916bdd8a38ff1ccf5e788f6f2e72efbb327
Yannick Tivisse <yti@odoo.com>
2019-06-04 14:08:47 +0200

    [IMP] base_setup: Remove group_multi_company on res.config
    A user is supposed to be put in the group_multi_company group when the
    he is allowed in several companies. Activating the feature for all the users
    is not necessary.

--- addons/base_setup/models/res_config_settings.py
+++ addons/base_setup/models/res_config_settings.py
@@ -8,7 +8,6 @@ class ResConfigSettings(models.TransientModel):
 
     _inherit = 'res.config.settings'
 
-    group_multi_company = fields.Boolean("Manage multiple companies", implied_group='base.group_multi_company')
     company_id = fields.Many2one('res.company', string='Company', required=True,
         default=lambda self: self.env.company)
     user_default_rights = fields.Boolean(
