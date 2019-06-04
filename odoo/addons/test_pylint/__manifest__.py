53f6afd2eaf56344b483c2e0ae59cdc2859af423
Christophe Monniez <moc@odoo.com>
2019-06-04 14:24:29 +0200

    [FIX] test_lint, test_pylint: reintroduce an empty test_pylint module
    Since test_pylint was moved into the test_lint module in commit
    b27077bb, this could break Odoo instances where a module has test_pylint in its dependency.
    
    With this commit, the test_pylint module is coming back as an empty
    module.

---
+++ odoo/addons/test_pylint/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'test-eval',
+    'version': '0.1',
+    'category': 'Tests',
+    'description': """Deprecated empty module in favor of test-lint.""",
+    'maintainer': 'Odoo SA',
+    'depends': ['base'],
+    'installable': True,
+    'auto_install': False,
+}
