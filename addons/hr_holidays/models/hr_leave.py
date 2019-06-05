6d34a19ba8caf287d2973a1b1fd1430b39d5612d
RomainLibert <rli@odoo.com>
2019-06-05 10:00:12 +0000

    [FIX] hr_holidays: fix multi company issue in onchange
    If you are logged into a company that differs from the company of your
    employee, you won't be able to read the informations on that employee,
    and you will have a crash.
    Instead of taking the employee linked to the current user, we use search
    in order to first apply the ir.rules hence we ensure that we will be
    able to read the needed informations.
    
    closes odoo/odoo#33903
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -339,7 +339,7 @@ class HolidaysRequest(models.Model):
     def _onchange_type(self):
         if self.holiday_type == 'employee':
             if not self.employee_id:
-                self.employee_id = self.env.user.employee_ids[:1].id
+                self.employee_id = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1).id
             self.mode_company_id = False
             self.category_id = False
         elif self.holiday_type == 'company':
@@ -352,7 +352,7 @@ class HolidaysRequest(models.Model):
             self.mode_company_id = False
             self.category_id = False
             if not self.department_id:
-                self.department_id = self.env.user.employee_ids[:1].department_id.id
+                self.department_id = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1).department_id.id
         elif self.holiday_type == 'category':
             self.employee_id = False
             self.mode_company_id = False
