40acd8cf1823adc16eb1779a0a54825f7d691270
Xavier Morel <xmo@odoo.com>
2019-06-05 11:03:06 +0200

    [FIX] portal, website: login redirection
    * migrate website to overriding web_login less (still needed to flag it
      as website-enabled) and use _login_redirect for its login redirection
      override needs
    * modify portal and website to not replace / shortcut the redirection
      workflow, so it's possible to override those properly if / as
      necessary

--- addons/portal/controllers/web.py
+++ addons/portal/controllers/web.py
@@ -16,5 +16,5 @@ class Home(Home):
 
     def _login_redirect(self, uid, redirect=None):
         if not redirect and not request.env['res.users'].sudo().browse(uid).has_group('base.group_user'):
-            return '/my'
+            redirect = '/my'
         return super(Home, self)._login_redirect(uid, redirect=redirect)
