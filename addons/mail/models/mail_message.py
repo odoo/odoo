7053e8b9a66e0e71a9ff21d10a846868c0f05439
Nicolas Martinelli <nim@odoo.com>
2019-06-05 08:53:46 +0200

    [IMP] mail, website_mail, base: extra info on error messages
    Display extra information on error messages:
    - Access error based on ACLs: user, fields (if applies)
    - Access error based on record rules: user, ids
    - Missing error: model, operation, user, ids
    
    Co-authored-by: jev-odoo <jev@odoo.com>

--- addons/mail/models/mail_message.py
+++ addons/mail/models/mail_message.py
@@ -582,8 +582,9 @@ class Message(models.Model):
                                 WHERE message.message_type = %%s AND (message.subtype_id IS NULL OR subtype.internal IS TRUE) AND message.id = ANY (%%s)''' % (self._table), ('comment', self.ids,))
             if self._cr.fetchall():
                 raise AccessError(
-                    _('The requested operation cannot be completed due to security restrictions. Please contact your system administrator.\n\n(Document type: %s, Operation: %s)') %
-                    (self._description, operation))
+                    _('The requested operation cannot be completed due to security restrictions. Please contact your system administrator.\n\n(Document type: %s, Operation: %s)') % (self._description, operation)
+                    + ' - ({} {}, {} {})'.format(_('Records:'), self.ids[:6], _('User:'), self._uid)
+                )
 
         # Read mail_message.ids to have their values
         message_values = dict((res_id, {}) for res_id in self.ids)
@@ -682,8 +683,9 @@ class Message(models.Model):
         if not other_ids:
             return
         raise AccessError(
-            _('The requested operation cannot be completed due to security restrictions. Please contact your system administrator.\n\n(Document type: %s, Operation: %s)') %
-            (self._description, operation))
+            _('The requested operation cannot be completed due to security restrictions. Please contact your system administrator.\n\n(Document type: %s, Operation: %s)') % (self._description, operation)
+            + ' - ({} {}, {} {})'.format(_('Records:'), list(other_ids)[:6], _('User:'), self._uid)
+        )
 
     @api.model
     def _get_record_name(self, values):
