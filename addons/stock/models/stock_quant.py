871d78ef3319d3ce2870e63dedbede865dd5774d
svs-odoo <svs@odoo.com>
2019-06-06 06:38:27 +0000

    [IMP] stock: change empty quant message
    Rewrites the help message displayed when no quant to make it accurier.
    
    Task #2006337

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -525,9 +525,10 @@ class StockQuant(models.Model):
             'context': self.env.context,
             'domain': domain or [],
             'help': """
-                <p class="o_view_nocontent_empty_folder">No stock on hand</p>
+                <p class="o_view_nocontent_empty_folder">No Stock On Hand</p>
                 <p>This analysis gives you an overview on the current stock
-                level of your products.</p>
+                level of your products.<br/>
+                Click on create to update the quantity on hand</p>
                 """
         }
 
