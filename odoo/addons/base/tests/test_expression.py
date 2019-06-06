fd8a810bd136e90007c90a79c17385c9374f1f0f
Xavier Morel <xmo@odoo.com>
2019-06-06 15:46:37 +0200

    [FIX] core: != / not ilike failing to match null records
    As odoo makes little difference, one would expect `(field, !=, 'foo')`
    or `(field, 'not ilike', 'foo')` to have the same behaviour when field is
    empty, and provide the opposite result as the "positive" operators.
    
    However because NULL <OP> <value> returns NULL which is generally
    assimilated to false, the sections above would fail to match records
    whose field is null (they would match records whose field is *empty*
    though).
    
    Fix this by wrapping the operation in a coalesce falling back
    to (hopefully) the correct value if the result is null.
    
    See also: odoo/odoo#22879

--- odoo/addons/base/tests/test_expression.py
+++ odoo/addons/base/tests/test_expression.py
@@ -1015,3 +1015,27 @@ class TestAutoJoin(TransactionCase):
         # Test produced queries
         self.assertEqual(len(self.query_list), 1,
             "_auto_join on: ('child_ids.state_id.country_id.code', 'like', '..') number of queries incorrect")
+
+    def test_nullfields(self):
+        obj1 = self.env['res.bank'].create({'name': 'c0'})
+        obj2 = self.env['res.bank'].create({'name': 'c1', 'city': 'Ljósálfaheimr'})
+        obj3 = self.env['res.bank'].create({'name': 'c2', 'city': 'York'})
+        obj4 = self.env['res.bank'].create({'name': 'c3', 'city': 'Springfield'})
+
+        self.assertEqual(
+            self.env['res.bank'].search([
+                ('id', 'in', (obj1 | obj2 | obj3 | obj4).ids),
+                ('city', '!=', 'York'),
+            ]),
+            (obj1 | obj2 | obj4),
+            "Should have returned all banks whose city is not York"
+        )
+
+        self.assertEqual(
+            self.env['res.bank'].search([
+                ('id', 'in', (obj1 | obj2 | obj3 | obj4).ids),
+                ('city', 'not ilike', 'field'),
+            ]),
+            (obj1 | obj2 | obj3),
+            "Should have returned all banks whose city doesn't contain field"
+        )
