358192695bf2cda266206f39fe62f5f999dfb793
Romain Derie <rde@odoo.com>
2019-06-07 09:41:07 +0000

    [IMP] base, http, web: do not start tests in debug mode
    It seems to be a better solution to keep starting tests without being in debug
    mode to avoid having new fields and menus appearing.
    
    This commit simply drop the debug mode in test mode and retrieve the test
    assets by checking if test mode is enabled directly.
    
    closes odoo/odoo#34012
    
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- addons/stock/static/tests/tours/route.js
+++ addons/stock/static/tests/tours/route.js
@@ -5,7 +5,7 @@ var tour = require('web_tour.tour');
 
 tour.register('stock', {
     test: true,
-    url: '/web#action=stock.action_stock_config_settings',
+    url: '/web?debug=1#action=stock.action_stock_config_settings',
     },
      [
         {
