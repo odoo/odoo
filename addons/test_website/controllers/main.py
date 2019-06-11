bf9fb0b98e4f7b886bdda30d1800c94993e1b4ae
fja-odoo <fja@odoo.com>
2019-06-11 14:26:53 +0200

    [IMP] test_website: add test tour for the website errors
    The purpose of these tests is to verify that errors informations
    are displayed correctly on the website.
    
    Part of https://github.com/odoo/odoo/pull/32132
    task-1894820

--- addons/test_website/controllers/main.py
+++ addons/test_website/controllers/main.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import json
+import werkzeug
+
 from odoo import http
 from odoo.http import request
 from odoo.addons.portal.controllers.web import Home
-import json
+from odoo.exceptions import UserError, ValidationError, AccessError, MissingError, AccessError, AccessDenied
 
 
 class WebsiteTest(Home):
 
-    @http.route('/test_view', type='http', auth="public", website=True)
-    def test_view(self, **kw):
+    @http.route('/test_view', type='http', auth='public', website=True)
+    def test_view(self, **kwargs):
         return request.render('test_website.test_view')
 
     @http.route('/ignore_args/converteronly/<string:a>/', type='http', auth="public", website=True)
@@ -40,3 +43,55 @@ class WebsiteTest(Home):
     @http.route('/multi_company_website', type='http', auth="public", website=True)
     def test_company_context(self):
         return request.make_response(json.dumps(request.context.get('allowed_company_ids')))
+
+    @http.route('/test_error_view', type='http', auth='public', website=True)
+    def test_error_view(self, **kwargs):
+        return request.render('test_website.test_error_view')
+
+    @http.route('/test_user_error_http', type='http', auth='public', website=True)
+    def test_user_error_http(self, **kwargs):
+        raise UserError("This is a user http test")
+
+    @http.route('/test_user_error_json', type='json', auth='public', website=True)
+    def test_user_error_json(self, **kwargs):
+        raise UserError("This is a user rpc test")
+
+    @http.route('/test_validation_error_http', type='http', auth='public', website=True)
+    def test_validation_error_http(self, **kwargs):
+        raise ValidationError("This is a validation http test")
+
+    @http.route('/test_validation_error_json', type='json', auth='public', website=True)
+    def test_validation_error_json(self, **kwargs):
+        raise ValidationError("This is a validation rpc test")
+
+    @http.route('/test_access_error_json', type='json', auth='public', website=True)
+    def test_access_error_json(self, **kwargs):
+        raise AccessError("This is an access rpc test")
+
+    @http.route('/test_access_error_http', type='http', auth='public', website=True)
+    def test_access_error_http(self, **kwargs):
+        raise AccessError("This is an access http test")
+
+    @http.route('/test_missing_error_json', type='json', auth='public', website=True)
+    def test_missing_error_json(self, **kwargs):
+        raise MissingError("This is a missing rpc test")
+
+    @http.route('/test_missing_error_http', type='http', auth='public', website=True)
+    def test_missing_error_http(self, **kwargs):
+        raise MissingError("This is a missing http test")
+
+    @http.route('/test_internal_error_json', type='json', auth='public', website=True)
+    def test_internal_error_json(self, **kwargs):
+        raise werkzeug.exceptions.InternalServerError()
+
+    @http.route('/test_internal_error_http', type='http', auth='public', website=True)
+    def test_internal_error_http(self, **kwargs):
+        raise werkzeug.exceptions.InternalServerError()
+
+    @http.route('/test_access_denied_json', type='json', auth='public', website=True)
+    def test_denied_error_json(self, **kwargs):
+        raise AccessDenied("This is an access denied rpc test")
+
+    @http.route('/test_access_denied_http', type='http', auth='public', website=True)
+    def test_denied_error_http(self, **kwargs):
+        raise AccessDenied("This is an access denied http test")
