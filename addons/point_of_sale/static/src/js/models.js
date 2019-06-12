odoo/odoo

commit 9e908175307611c0ad224de1d00370d90511e9e6

[FIX] point_of_sale : display discount on ticket
According to the setting set on the pricelist

More the discount_policy field from sale to product to be usable in point of
sale, even without sale installed

Task id: 1853454

closes odoo/odoo#27310

addons/point_of_sale/static/src/js/models.js
@@ -294,7 +294,7 @@ exports.PosModel = Backbone.Model.extend({
         loaded: function(self, locations){ self.shop = locations[0]; },
     },{
         model:  'product.pricelist',
-        fields: ['name', 'display_name'],
+        fields: ['name', 'display_name', 'discount_policy'],
         domain: function(self) { return [['id', 'in', self.config.available_pricelist_ids]]; },
         loaded: function(self, pricelists){
             _.map(pricelists, function (pricelist) { pricelist.items = []; });
@@ -1556,6 +1556,9 @@ exports.Orderline = Backbone.Model.extend({
             discount:           this.get_discount(),
             product_name:       this.get_product().display_name,
             product_name_wrapped: this.generate_wrapped_product_name(),
+            price_lst:          this.get_lst_price(),
+            display_discount_policy:    this.display_discount_policy(),
+            price_display_one:  this.get_display_price_one(),
             price_display :     this.get_display_price(),
             price_with_tax :    this.get_price_with_tax(),
             price_without_tax:  this.get_price_without_tax(),
@@ -1619,6 +1622,28 @@ exports.Orderline = Backbone.Model.extend({
     get_base_price:    function(){
         var rounding = this.pos.currency.rounding;
         return round_pr(this.get_unit_price() * this.get_quantity() * (1 - this.get_discount()/100), rounding);
+    },
+    get_display_price_one: function(){
+        var rounding = this.pos.currency.rounding;
+        var price_unit = this.get_unit_price() * (1.0 - (this.get_discount() / 100.0));
+        if (this.pos.config.iface_tax_included !== 'total') {
+            return round_pr(price_unit, rounding);
+        } else {
+            var product =  this.get_product();
+            var taxes_ids = product.taxes_id;
+            var taxes =  this.pos.taxes;
+            var product_taxes = [];
+
+            _(taxes_ids).each(function(el){
+                product_taxes.push(_.detect(taxes, function(t){
+                    return t.id === el;
+                }));
+            });
+
+            var all_taxes = this.compute_all(product_taxes, price_unit, 1, this.pos.currency.rounding);
+
+            return round_pr(all_taxes.total_included * (1 - this.get_discount()/100), rounding);
+        }
     },
     get_display_price: function(){
         if (this.pos.config.iface_tax_included === 'total') {
@@ -1789,6 +1814,12 @@ exports.Orderline = Backbone.Model.extend({
             "tax": taxtotal,
             "taxDetails": taxdetail,
         };
+    },
+    display_discount_policy: function(){
+        return this.order.pricelist.discount_policy;
+    },
+    get_lst_price: function(){
+        return this.product.lst_price;
     },
 });
 
@@ -2460,7 +2491,9 @@ exports.Order = Backbone.Model.extend({
     },
     get_total_discount: function() {
         return round_pr(this.orderlines.reduce((function(sum, orderLine) {
-            return sum + (orderLine.get_unit_price() * (orderLine.get_discount()/100) * orderLine.get_quantity());
+            sum += (orderLine.get_unit_price() * (orderLine.get_discount()/100) * orderLine.get_quantity());
+            sum += ((orderLine.get_lst_price() - orderLine.get_unit_price()) * orderLine.get_quantity());
+            return sum;
         }), 0), this.pos.currency.rounding);
     },
     get_total_tax: function() {

addons/point_of_sale/static/src/xml/pos.xml
@@ -775,19 +775,44 @@
 
             <div class='orderlines' line-ratio='0.6'>
                 <t t-foreach='receipt.orderlines' t-as='line'>
-                    <t t-set='simple' t-value='line.discount === 0 and line.unit_name === "Unit(s)" and line.quantity === 1' />
+                    <t t-set='simple' t-value='(line.discount === 0 and line.unit_name === "Unit(s)" and line.quantity === 1 and !discount_to_show and !(line.display_discount_policy == "without_discount" &amp;&amp; line.price != line.price_lst))' />
                     <t t-if='simple'>
                         <line>
                             <left><t t-esc='line.product_name_wrapped[0]' /></left>
                             <right><value t-att-value-decimals='pos.currency.decimals'><t t-esc='line.price_display' /></value></right>
                         </line>
-                        <t t-call="XmlReceiptWrappedProductNameLines"/>
                     </t>
+                    <t t-call="XmlReceiptWrappedProductNameLines"/>
                     <t t-if='!simple'>
                         <line><left><t t-esc='line.product_name_wrapped[0]' /></left></line>
                         <t t-call="XmlReceiptWrappedProductNameLines"/>
+                        <t t-if="line.display_discount_policy == 'without_discount' &amp;&amp; line.price != line.price_lst">
+                            <line indent='1'>
+                                <left>
+                                    <value t-att-value-decimals='pos.dp["Product Price"]'>
+                                        <t t-esc="line.price_lst" />
+                                    </value> ->
+                                    <value t-att-value-decimals='pos.dp["Product Price"]'>
+                                        <t t-esc="line.price" />
+                                    </value>
+                                </left>
+                            </line>
+                        </t>
+                        <t t-else='line.discount !== 0'>
+                            <line indent='1'>
+                                <left>
+                                    <value t-att-value-decimals='pos.dp["Product Price"]'>
+                                        <t t-esc="line.price_lst" />
+                                    </value>
+                                </left>
+                            </line>
+                        </t>
                         <t t-if='line.discount !== 0'>
-                            <line indent='1'><left>Discount: <t t-esc='line.discount' />%</left></line>
+                            <line indent='1'>
+                                <left>
+                                    Discount: <t t-esc='line.discount' />%
+                                </left>
+                            </line>
                         </t>
                         <line indent='1'>
                             <left>
@@ -799,7 +824,7 @@
                                 </t>
                                 x
                                 <value t-att-value-decimals='pos.dp["Product Price"]'>
-                                    <t t-esc='line.price' />
+                                    <t t-esc='line.price_display_one' />
                                 </value>
                             </left>
                             <right>
@@ -1269,7 +1294,7 @@
                 <t t-if="line.get_product().tracking!=='none'">
                     <i class="oe_link_icon fa fa-list oe_icon line-lot-icon oe_green" t-if="line.has_valid_product_lot()"
                        aria-label="Valid product lot" role="img" title="Valid product lot"/>
-                    <i class="oe_link_icon fa fa-list oe_icon line-lot-icon oe_red" t-if="not(line.has_valid_product_lot())"
+                    <i class="oe_link_icon fa fa-list oe_icon line-lot-icon oe_red" t-if="!line.has_valid_product_lot()"
                        aria-label="Invalid product lot" role="img" title="Invalid product lot"/>
                 </t>
             </span>
@@ -1284,7 +1309,16 @@
                         </em>
                         <t t-esc="line.get_unit().name" />
                         at
-                        <t t-esc="widget.format_currency(line.get_unit_display_price(),'Product Price')" />
+                        <t t-if="line.display_discount_policy() == 'without_discount' &amp;&amp; 
+                            line.get_unit_display_price() != line.get_lst_price()">
+                            <s>
+                                <t t-esc="widget.format_currency(line.get_lst_price(),'Product Price')" />
+                            </s>
+                            <t t-esc="widget.format_currency(line.get_unit_display_price(),'Product Price')" />
+                        </t>
+                        <t t-else="">
+                            <t t-esc="widget.format_currency(line.get_unit_display_price(),'Product Price')" />
+                        </t>
                         /
                         <t t-esc="line.get_unit().name" />
                     </li>
@@ -1472,6 +1506,15 @@
                 <tr t-foreach="orderlines" t-as="orderline">
                     <td>
                         <t t-esc="orderline.get_product().display_name"/>
+                        <t t-if="orderline.display_discount_policy() == 'without_discount' &amp;&amp; 
+                            orderline.get_unit_display_price() != orderline.get_lst_price()">
+                            <div class="pos-disc-font">
+                                <s>
+                                    <t t-esc="widget.format_currency(orderline.get_lst_price(),'Product Price')"/>
+                                </s> ->
+                                <t t-esc="widget.format_currency(orderline.get_unit_display_price(),'Product Price')"/>
+                            </div>
+                        </t>
                          <t t-if="orderline.get_discount() > 0">
                             <div class="pos-disc-font">
                                 With a <t t-esc="orderline.get_discount()"/>% discount

addons/product/models/product_pricelist.py
@@ -37,6 +37,11 @@ class Pricelist(models.Model):
     country_group_ids = fields.Many2many('res.country.group', 'res_country_group_pricelist_rel',
                                          'pricelist_id', 'res_country_group_id', string='Country Groups')
 
+    discount_policy = fields.Selection([
+        ('with_discount', 'Discount included in the price'),
+        ('without_discount', 'Show public price & discount to the customer')],
+        default='with_discount')
+
     @api.multi
     def name_get(self):
         return [(pricelist.id, '%s (%s)' % (pricelist.name, pricelist.currency_id.name)) for pricelist in self]

addons/sale/models/__init__.py
@@ -3,7 +3,6 @@
 
 from . import analytic
 from . import account_invoice
-from . import product_pricelist
 from . import product_product
 from . import product_template
 from . import res_company

addons/sale/models/product_pricelist.py
@@ -1,13 +1,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import fields, models
-
-
-class ProductPricelist(models.Model):
-    _inherit = 'product.pricelist'
-
-    discount_policy = fields.Selection([
-        ('with_discount', 'Discount included in the price'),
-        ('without_discount', 'Show public price & discount to the customer')],
-        default='with_discount')

commit 4bcede565b5b03a5f16740f50f6829ff2afba55a

[FIX] point_of_sale: discount applied twice
The discount is applied two times when 'tax_included == total'. So we've
cleaned the computation method by using the unit_display_price that on
which we are computing the discount price.

This wrong behavior has been introduced in https://github.com/odoo/odoo/commit/9e908175307611c0ad224de1d00370d90511e9e6

TASK-ID: 1978564

addons/point_of_sale/static/src/js/models.js
@@ -1636,25 +1636,8 @@ exports.Orderline = Backbone.Model.extend({
     },
     get_display_price_one: function(){
         var rounding = this.pos.currency.rounding;
-        var price_unit = this.get_unit_price() * (1.0 - (this.get_discount() / 100.0));
-        if (this.pos.config.iface_tax_included !== 'total') {
-            return round_pr(price_unit, rounding);
-        } else {
-            var product =  this.get_product();
-            var taxes_ids = product.taxes_id;
-            var taxes =  this.pos.taxes;
-            var product_taxes = [];
-
-            _(taxes_ids).each(function(el){
-                product_taxes.push(_.detect(taxes, function(t){
-                    return t.id === el;
-                }));
-            });
-
-            var all_taxes = this.compute_all(product_taxes, price_unit, 1, this.pos.currency.rounding);
-
-            return round_pr(all_taxes.total_included * (1 - this.get_discount()/100), rounding);
-        }
+        var unit_display_price=this.get_unit_display_price();
+        return round_pr(unit_display_price * (1.0 - (this.get_discount() / 100.0)), rounding);
     },
     get_display_price: function(){
         if (this.pos.config.iface_tax_included === 'total') {