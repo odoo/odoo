4c7f236fb547579ceceee307e5b96e36b6011cc8
Pierre Rousseau <pro@odoo.com>
2019-06-12 06:48:28 +0000

    [FIX] snailmail: re-render report when using snailmail
    If the option 'Reload from attachment' is checked for a report, it will be generated from the existing attachments.
    However, for snailmail, we have to force the re-rendering to apply specific css rules to match the layout of a A4 letter.
    
    Task-ID: 2008896
    
    closes odoo/odoo#34053
    
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/snailmail/models/ir_actions_report.py
+++ addons/snailmail/models/ir_actions_report.py
@@ -6,6 +6,14 @@ from odoo import models, fields, api, _
 class IrActionsReport(models.Model):
     _inherit = 'ir.actions.report'
 
+    @api.multi
+    def retrieve_attachment(self, record):
+        # Override this method in order to force to re-render the pdf in case of
+        # using snailmail
+        if self.env.context.get('snailmail_layout'):
+            return False
+        return super(IrActionsReport, self).retrieve_attachment(record)
+
     @api.model
     def get_paperformat(self):
         # force the right format (euro/A4) when sending letters, only if we are not using the l10n_DE layout
