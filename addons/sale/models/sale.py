00d352431507a69c6240396e1e436fe203ae4d40
Nicolas Martinelli <nim@odoo.com>
2019-06-14 10:28:13 +0200

    [FIX] sale: no company
    - Activate multi-company
    - In a SO, remove the company (the field is not mandatory)
    
    The currency rate computation crashes.
    
    The computation expects a company in order to compute the rate.
    
    If there is no company set, we set the rate to 1.0. It is worth
    stressing that if both multi-company and multi-currency are activated, a
    company is necessary for reporting consistency.
    
    Fixes #34066
    opw-2009044

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -217,6 +217,8 @@ class SaleOrder(models.Model):
     @api.depends('pricelist_id', 'date_order', 'company_id')
     def _compute_currency_rate(self):
         for order in self:
+            if not order.company_id:
+                order.currency_rate = 1.0
             order.currency_rate = self.env['res.currency']._get_conversion_rate(order.company_id.currency_id, order.currency_id, order.company_id, order.date_order)
 
     def _compute_access_url(self):
