2f8cc64a6edbdcd0a009a2cce31d8e2acc40b011
qdp-odoo <qdp@odoo.com>
2019-06-17 16:45:56 +0200

    [WIP] account: refactoring made during code review

--- addons/account/models/ir_actions_report.py
+++ addons/account/models/ir_actions_report.py
@@ -9,13 +9,11 @@ class IrActionsReport(models.Model):
     @api.multi
     def retrieve_attachment(self, record):
         # get the original invoice through the message_main_attachment_id field of the record
-        if self.report_name == 'account.report_original_vendor_bill':
-            if record.message_main_attachment_id.mimetype == 'application/pdf':
+        if self.report_name == 'account.report_original_vendor_bill' and record.message_main_attachment_id:
+            if record.message_main_attachment_id.mimetype == 'application/pdf' or \
+               record.message_main_attachment_id.mimetype.startswith('image'):
                 return record.message_main_attachment_id
-            else:
-                return False
-        else:
-            return super(IrActionsReport, self).retrieve_attachment(record)
+        return super(IrActionsReport, self).retrieve_attachment(record)
 
     @api.multi
     def _post_pdf(self, save_in_attachment, pdf_content=None, res_ids=None):
@@ -23,6 +21,6 @@ class IrActionsReport(models.Model):
         if self.report_name == 'account.report_original_vendor_bill':
             pdf_content = None
             res_ids = None
-        if not save_in_attachment:
-            raise UserError(_("No original vendor bills could be found for any of the selected vendor bills."))
+            if not save_in_attachment:
+                raise UserError(_("No original vendor bills could be found for any of the selected vendor bills."))
         return super(IrActionsReport, self)._post_pdf(save_in_attachment, pdf_content=pdf_content, res_ids=res_ids)
