e0ba3fcdb9126bd3ab9ec0729031beea3fd92aea
lbs-odoo <lbs@odoo.com>
2019-06-17 11:14:38 +0000

    [ADD] hr_work_entry: Move work entries mechanism into its own module
    Purpose
    =======
    
    We would like to re-use the work entries in the attendance module.
    
    As hr_attendance is not dependant on hr_payroll, we move the whole
    mechanism into its own module.
    
    This could be reused too into others modules, like forecasting or whatever.
    
    TaskID: 1904850
    
    closes odoo/odoo#34434
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ addons/hr_work_entry/__manifest__.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Work Entries',
+    'category': 'Human Resources/Employees',
+    'sequence': 39,
+    'summary': 'Manages work entries',
+    'description': "",
+    'installable': True,
+    'depends': [
+        'hr',
+    ],
+    'data': [
+        'security/hr_work_entry_security.xml',
+        'security/ir.model.access.csv',
+        'data/hr_work_entry_data.xml',
+        'views/hr_work_entry_views.xml',
+    ],
+    'qweb': [
+        "static/src/xml/work_entry_templates.xml",
+    ],
+}
