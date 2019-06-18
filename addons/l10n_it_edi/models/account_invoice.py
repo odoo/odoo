f722ef0cb079ab2368d4f4075e4c42c0ab9c376a
wan <wan@odoo.com>
2019-06-18 14:43:07 +0000

    [IMP] account, _facturx, l10n[be,it]_edi: remove account.invoice.import.wizard Since the wizard is not shown anymore (skipped to go right to file selection), we can remove it. Small refactoring had to be done for l10n_it in order to upload files with multiple invoices inside and to detect format
    closes odoo/odoo#34145
    
    Signed-off-by: Quentin De Paoli (qdp) <qdp@openerp.com>

--- addons/l10n_it_edi/models/account_invoice.py
+++ addons/l10n_it_edi/models/account_invoice.py
@@ -290,23 +290,22 @@ class AccountMove(models.Model):
                 )
             self.l10n_it_send_state = 'invalid'
 
-    def _import_xml_invoice(self, content, attachment):
+    def _import_xml_invoice(self, tree):
         ''' Extract invoice values from the E-Invoice xml tree passed as parameter.
 
         :param content: The tree of the xml file.
         :return: A dictionary containing account.invoice values to create/update it.
         '''
 
-        try:
-            tree = etree.fromstring(content)
-        except:
-            _logger.info('Error during decoding XML file')
-            return self.env['account.move']
-
         invoices = self.env['account.move']
+        multi = False
 
         # possible to have multiple invoices in the case of an invoice batch, the batch itself is repeated for every invoice of the batch
         for body_tree in tree.xpath('//FatturaElettronicaBody', namespaces=tree.nsmap):
+            if multi:
+                # make sure all the iterations create a new invoice record (except the first which could have already created one)
+                self = self.env['account.move']
+            multi = True
 
             elements = tree.xpath('//DatiGeneraliDocumento/TipoDocumento', namespaces=tree.nsmap)
             if elements and elements[0].text and elements[0].text == 'TD01':
@@ -654,7 +653,8 @@ class AccountMove(models.Model):
                     })
 
                     # default_res_id is had to context to avoid facturx to import his content
-                    new_invoice.with_context(default_res_id=new_invoice.id).message_post(
+                    # no_new_invoice to prevent from looping on the message_post that would create a new invoice without it
+                    new_invoice.with_context(no_new_invoice=True, default_res_id=new_invoice.id).message_post(
                         body=(_("Attachment from XML")),
                         attachment_ids=[attachment_64.id]
                     )
@@ -662,10 +662,6 @@ class AccountMove(models.Model):
             for message in message_to_log:
                 new_invoice.message_post(body=message)
 
-            if attachment:
-                new_invoice.l10n_it_einvoice_name = attachment.name
-                attachment.write({'res_model': 'account.move', 'res_id': new_invoice.id})
-                new_invoice.message_post(attachment_ids=[attachment.id])
             invoices += new_invoice
         return invoices
 
@@ -694,6 +690,20 @@ class AccountMove(models.Model):
                 if text:
                     output_str += "<li>%s: %s</li>" % (element.tag, text)
         return output_str + "</ul>"
+
+    @api.model
+    def _get_xml_decoders(self):
+        # Override
+        ubl_decoders = [('Italy', self._detect_italy_edi, self._import_xml_invoice)]
+        return super(AccountMove, self)._get_xml_decoders() + ubl_decoders
+
+    @api.model
+    def _detect_italy_edi(self, tree, file_name):
+        # Quick check the file name looks like the one of an Italian EDI XML.
+        flag = re.search("([A-Z]{2}[A-Za-z0-9]{2,28}_[A-Za-z0-9]{0,5}.(xml.p7m|xml))", file_name)
+        error = None
+
+        return {'flag': flag, 'error': error}
 
 class AccountTax(models.Model):
     _name = "account.tax"
@@ -730,18 +740,3 @@ class AccountTax(models.Model):
                     raise ValidationError("If the tax has exoneration, you must enter a kind of exoneration, a law reference and the amount of the tax must be 0.0.")
                 if tax.l10n_it_kind_exoneration == 'N6' and tax.l10n_it_vat_due_date == 'S':
                     raise UserError(_("'Scissione dei pagamenti' is not compatible with exoneration of kind 'N6'"))
-
-class ImportInvoiceImportWizard(models.TransientModel):
-    _name = 'account.invoice.import.wizard'
-    _inherit = 'account.invoice.import.wizard'
-
-    def _create_invoice_from_file(self, attachment):
-        if attachment.mimetype == 'application/xml' and re.search("([A-Z]{2}[A-Za-z0-9]{2,28}_[A-Za-z0-9]{0,5}.(xml.p7m|xml))", attachment.name):
-            if self.env['account.move'].search([('l10n_it_einvoice_name', '=', attachment.name)], limit=1):
-                # invoice already exist
-                raise UserError(_('E-invoice already exist: %s') % attachment.name)
-            self = self.with_context(default_journal_id= self.journal_id.id)
-            invoice = self.env['account.move']._import_xml_invoice(base64.decodestring(attachment.datas), attachment)
-        else:
-            invoice = super(ImportInvoiceImportWizard, self)._create_invoice_from_file(attachment)
-        return invoice
