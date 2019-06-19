9017d9e9320e5ebb3d4892e670e40f7fe5521468
Christophe Monniez <moc@odoo.com>
2019-06-19 16:34:57 +0200

    m

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -637,12 +637,12 @@ class ChromeBrowser():
         # wait for remaining screenshots
         while self.screenshot_ids:
             self.catch_next_event() 
-        self._websocket_send('Page.stopLoading')
+        self.catch_next_event(ids={self._websocket_send('Page.stopLoading')}, ignore_errors=True)
         self._logger.info('Deleting cookies and clearing local storage')
         self.catch_next_event(ids={self._websocket_send('Network.clearBrowserCookies')}, ignore_errors=True)
         self.catch_next_event(ids={self._websocket_send('Runtime.evaluate', params={'expression': 'localStorage.clear()'})}, ignore_errors=True)
         self.navigate_to('about:blank', wait_stop=True, ignore_errors=True)
-        self._websocket_send('Runtime.DiscardConsoleEntries')
+        self.catch_next_event(ids={self._websocket_send('Runtime.discardConsoleEntries')}, ignore_errors=True)
         self._encode_screencast()
 
     # main loop
