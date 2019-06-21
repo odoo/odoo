9a3572a5c5f7684d0c2d4bd49319394f9af2b6ec
Martin Trigaux <mat@odoo.com>
2019-06-21 13:37:59 +0200

    [FIX] hr_payroll: unify the field labels
    The field module_account_accountant is called "Account" in account module
    but was changed to "Account Accountant" in hr_payroll, making the source of
    a translated record to change based on the module installed (which can lead
    to confused translators or broken universes)
    
    Terms export was unified at #34296

--- addons/hr_payroll/models/res_config_settings.py
+++ addons/hr_payroll/models/res_config_settings.py
@@ -7,7 +7,7 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    module_account_accountant = fields.Boolean(string='Account Accountant')
+    module_account_accountant = fields.Boolean(string='Account')
     module_l10n_fr_hr_payroll = fields.Boolean(string='French Payroll')
     module_l10n_be_hr_payroll = fields.Boolean(string='Belgium Payroll')
     module_l10n_in_hr_payroll = fields.Boolean(string='Indian Payroll')
