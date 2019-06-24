d2f087558ef099490d151013456222f56b4427f2
Kishan Gajjar <kig@odoo.com>
2019-06-24 09:40:37 +0000

    [FIX] web_editor: restore broken VideoDialog options
    - To enable loop on a video, only setting the 'loop' URL parameter
    to 1 will not work anymore. We also need to set the playlist parameter
    to the video ID.
    
    - Sound is no longer allowed in autoplay because of new autoplay
    policies of most browsers. To make autoplay work, the video must be
    explicitely muted.
    
    closes odoo/odoo#34323
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/web_editor/static/src/js/widgets/widgets.js
+++ addons/web_editor/static/src/js/widgets/widgets.js
@@ -795,7 +795,7 @@ var VideoDialog = Widget.extend({
             return {errorCode: 0};
         }
 
-        var autoplay = options.autoplay ? '?autoplay=1' : '?autoplay=0';
+        var autoplay = options.autoplay ? '?autoplay=1&mute=1' : '?autoplay=0';
 
         if (ytMatch && ytMatch[2].length === 11) {
             $video.attr('src', '//www.youtube' + (ytMatch[1] || '') + '.com/embed/' + ytMatch[2] + autoplay);
@@ -824,7 +824,8 @@ var VideoDialog = Widget.extend({
             $video.attr('src', $video.attr('src') + '&rel=0');
         }
         if (options.loop && (ytMatch || vimMatch)) {
-            $video.attr('src', $video.attr('src') + '&loop=1');
+            var videoSrc = _.str.sprintf('%s&loop=1', $video.attr('src'));
+            $video.attr('src', ytMatch ? _.str.sprintf('%s&playlist=%s', videoSrc, ytMatch[2]) : videoSrc);
         }
         if (options.hide_controls && (ytMatch || dmMatch)) {
             $video.attr('src', $video.attr('src') + '&controls=0');
