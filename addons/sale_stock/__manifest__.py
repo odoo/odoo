shaneebkhan/android-app

[IMP] sale_stock: added wizard to display stock information
Before this commit,
Warning was raised from SO if there are not enough quantities
available in stock.

After this commit,
Information about the product stock can be seen in an
information popup if the SO is in the draft state.
User can get to know about the Quantity on Hand, Quantity
Available and Quantity Forecasted on scheduled date.

task-2003485

addons/sale_stock/__manifest__.py
@@ -29,10 +29,12 @@ Preferences
         'views/sale_stock_portal_template.xml',
         'views/stock_production_lot_views.xml',
         'views/report_invoice.xml',
+        'views/stock_information_template.xml',
         'report/sale_order_report_templates.xml',
         'report/stock_report_deliveryslip.xml',
         'data/sale_stock_data.xml',
         'wizard/stock_rules_report_views.xml',
+        'wizard/storable_product_qty_wizard.xml',
     ],
     'demo': ['data/sale_order_demo.xml'],
     'installable': True,

odoo/odoo

addons/sale_stock/__manifest__.py
@@ -29,10 +29,12 @@ Preferences
         'views/sale_stock_portal_template.xml',
         'views/stock_production_lot_views.xml',
         'views/report_invoice.xml',
+        'views/stock_information_template.xml',
         'report/sale_order_report_templates.xml',
         'report/stock_report_deliveryslip.xml',
         'data/sale_stock_data.xml',
         'wizard/stock_rules_report_views.xml',
+        'wizard/product_qty_wizard.xml',
     ],
     'demo': ['data/sale_order_demo.xml'],
     'installable': True,