09d2451d1eec275a43804033b00f646f98fb38d3
Florent de Labarre <florent.mirieu@gmail.com>
2019-06-26 21:08:08 +0000

    [REF] stock: unlink of inventory adjustment allow deletion of draft and cancelled inventory adjustment
    closes odoo/odoo#34469
    
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- addons/stock/models/stock_inventory.py
+++ addons/stock/models/stock_inventory.py
@@ -75,8 +75,8 @@ class Inventory(models.Model):
 
     def unlink(self):
         for inventory in self:
-            if inventory.state == 'done':
-                raise UserError(_('You cannot delete a validated inventory adjustement.'))
+            if inventory.state not in ('draft', 'cancel'):
+                raise UserError(_('You can only delete a draft inventory adjustment. If the inventory adjustment is not done, you can cancel it.'))
         return super(Inventory, self).unlink()
 
     def action_validate(self):
