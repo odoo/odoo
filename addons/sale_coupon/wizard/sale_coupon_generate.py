7933d759d9b5844fac94ae4da921a1fe17423ad0
mgh-odoo <mgh@odoo.com>
2019-06-26 15:33:15 +0530

    [IMP] sale_coupon: coupon layout and usability improve
    some layout fixed and confusing string of promo_applicability option is
    changed to 'Send a Coupon' form 'Apply On Next Order'.
    
    Task-ID: 2027296
    Closes #34620

--- addons/sale_coupon/wizard/sale_coupon_generate.py
+++ addons/sale_coupon/wizard/sale_coupon_generate.py
@@ -3,6 +3,7 @@
 from odoo import api, fields, models
 
 import ast
+from odoo.osv import expression
 
 
 class SaleCouponGenerate(models.TransientModel):
@@ -15,6 +16,7 @@ class SaleCouponGenerate(models.TransientModel):
         ('nbr_customer', 'Number of Selected Customers')
         ], default='nbr_coupon')
     partners_domain = fields.Char(string="Customer", default='[]')
+    has_partner_email = fields.Boolean(compute='_compute_has_partner_email')
 
     def generate_coupon(self):
         """Generates the number of coupons entered in wizard field nbr_coupons
@@ -29,9 +31,16 @@ class SaleCouponGenerate(models.TransientModel):
 
         if self.generation_type == 'nbr_customer' and self.partners_domain:
             for partner in self.env['res.partner'].search(ast.literal_eval(self.partners_domain)):
-                vals.update({'partner_id': partner.id})
+                vals.update({'partner_id': partner.id, 'state': 'sent' if partner.email else 'new'})
                 coupon = self.env['sale.coupon'].create(vals)
                 subject = '%s, a coupon has been generated for you' % (partner.name)
                 template = self.env.ref('sale_coupon.mail_template_sale_coupon', raise_if_not_found=False)
                 if template:
-                    template.send_mail(coupon.id, email_values={'email_to': partner.email, 'email_from': self.env.user.email or '', 'subject': subject,})
+                    email_values = {'email_to': partner.email, 'email_from': self.env.user.email or '', 'subject': subject}
+                    template.send_mail(coupon.id, email_values=email_values, notif_layout='mail.mail_notification_light')
+
+    @api.depends('partners_domain')
+    def _compute_has_partner_email(self):
+        for record in self:
+            domain = expression.AND([safe_eval(record.partners_domain), [('email', '=', False)]])
+            record.has_partner_email = self.env['res.partner'].search_count(domain) == 0
