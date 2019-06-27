8fd742ed0e58fcd0bd1dfd42f36be0f9a690d6b5
Meghna Jaswani <mja@odoo.com>
2019-06-27 11:55:52 +0530

    [IMP] l10n_il: Added new taxes and fields for withholding tax report pcn856
    [IMP] l10n_il: Changed the name of the field of model res.company
    
    [IMP] l10n_il : Added new model to define two new fields i.e holding tax reason and ITA branch
    
    [IMP] l10n_il: Improvements in declaration of tax groups for withholding taxes
    
    [IMP] l10n_il: Created withholding tax as group of two taxes i.e positive and negative tax.Hence,the negative tax is the withheld tax that can be reported
    
    [IMP] l10n_il: Added data for withholding tax reason & code

--- addons/l10n_il/__manifest__.py
+++ addons/l10n_il/__manifest__.py
@@ -17,11 +17,16 @@ This module consists:
     'website': 'http://www.odoo.com/accounting',
     'depends': ['base', 'account'],
     'data': [
+        'security/ir.model.access.csv',
         'data/account_chart_template_data.xml',
         'data/account.account.template.csv',
         'data/account_data.xml',
         'data/account_tax_template_data.xml',
         'data/account_chart_template_post_data.xml',
         'data/account_chart_template_configure_data.xml',
+        'data/withholding_tax_data.xml',
+        'views/res_partner_view.xml',
+        'views/res_company_view.xml',
+        'views/l10n_il_withh_tax_report_data_view.xml'
     ],
 }
