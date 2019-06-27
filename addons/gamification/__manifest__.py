bfbc7c6a184b2b507b1bf4ad778430bc2f3a0e7f
Patrick Hoste <pko@odoo.com>
2019-06-27 07:54:04 +0000

    [IMP] gamification: track karma change on users
    PURPOSE
    
    Allow karma gain tracking enabling notably display of top users based on
    weekly / monthly gain in website profile.
    
    SPECIFCIATIONS
    
    Each time a user gains karma a record is created in the gamification karma
    tracking model. Scheduled activity runs to consolidate the records into
    monthly gain records to avoid having crowdy table and unnecessary noise
    in karma gain.
    
    This model is made private and only accessible through some dedicated
    compute methods / controllers used in website profile.
    
    In website profile module buttons are added to see users ranking based
    on their total karma (like before) but also by last week and last month
    gains (using the newly introduced tracking model).
    
    LINKS
    
    Task ID 2003505
    PR #34594

--- addons/gamification/__manifest__.py
+++ addons/gamification/__manifest__.py
@@ -33,9 +33,11 @@ Both goals and badges are flexibles and can be adapted to a large range of modul
         'data/gamification_karma_rank_data.xml',
         'views/gamification.xml',
         'views/gamification_karma_rank_views.xml',
+        'views/gamification_karma_tracking_views.xml',
         'views/mail_templates.xml'
     ],
     'demo': [
-        'data/gamification_karma_rank_demo.xml'
+        'data/gamification_karma_rank_demo.xml',
+        'data/gamification_karma_tracking_demo.xml',
     ],
 }
