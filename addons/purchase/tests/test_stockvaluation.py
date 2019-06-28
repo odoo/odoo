18a3322b91ae8e77105a5149ffb8fbad17d4a085
Nicolas Martinelli <nim@odoo.com>
2019-06-28 12:07:41 +0000

    [FIX] purchase_stock: average cost
    - Create 2 rates:
    2019-05-01: 1.0
    2019-06-01: 0.5
    - Set-up a product A in AVCO
    - Create a PO for A on 2019-05-15
    - Receive the product after 2019-06-01
    
    The AML are created with a rate of 0.5, but the product cost is updated
    with the rate of 1.0.
    
    This is because the stock move date is used when it is still assigned,
    and not done yet. As described in:
    
    https://github.com/odoo/odoo/blob/59ea5443dc634fe33e5e5a32d3f7c619e7fbf05c/addons/stock/models/stock_move.py#L36
    
    The date is the scheduled date until the move is done, then it is the
    date of the actual move processing.
    
    We change the date to today's date, since it is the value used to update
    the move date:
    
    https://github.com/odoo/odoo/blob/59ea5443dc634fe33e5e5a32d3f7c619e7fbf05c/addons/stock/models/stock_move.py#L1143
    
    opw-2006368
    
    closes odoo/odoo#34446
    
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- addons/purchase/tests/test_stockvaluation.py
+++ addons/purchase/tests/test_stockvaluation.py
@@ -719,12 +719,12 @@ class TestStockValuationWithCOA(AccountingTestCase):
             'company_id': company.id,
         })
 
-        # To allow testing validation of PO
+        # To allow testing validation of PO and Delivery
+        today = date_po
         def _today(*args, **kwargs):
-            return date_po
-        # To allow testing validation of Delivery
+            return today
         def _now(*args, **kwargs):
-            return date_delivery + ' 01:00:00'
+            return today + ' 01:00:00'
 
         patchers = [
             patch('odoo.fields.Date.context_today', _today),
@@ -753,13 +753,17 @@ class TestStockValuationWithCOA(AccountingTestCase):
 
         line_product_avg = po.order_line.filtered(lambda l: l.product_id == product_avg)
 
+        today = date_delivery
         picking = po.picking_ids
         (picking.move_lines
             .filtered(lambda l: l.purchase_line_id == line_product_avg)
             .write({'quantity_done': 1.0}))
 
         picking.button_validate()
+        # 1 Units received at rate 0.7 = 42.86
+        self.assertAlmostEqual(product_avg.standard_price, 42.86)
 
+        today = date_invoice
         inv = self.env['account.invoice'].create({
             'type': 'in_invoice',
             'date_invoice': date_invoice,
