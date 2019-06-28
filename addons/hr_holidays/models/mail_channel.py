962f52dee7ac542a5f56a42b59d457198d96916d
Varun Raval <var@odoo.com>
2019-06-28 15:57:51 +0530

    [FIX] hr_holidays: display consecutive leave date on out of office message
    Purpose of the commit is when a when a user takes leave for a longer
    period, will probably use different  types of leaves and also arrange
    leave based on holidays.
    
    so we should check all the leaves which are taken by a user and if
    leaves are consecutive leaves(excluding the holidays) then we will
    show the 'Out of office' until the last consecutive date.
    
    task-2002525
    Closes: 34448

--- addons/hr_holidays/models/mail_channel.py
+++ addons/hr_holidays/models/mail_channel.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from datetime import timedelta
+
 from odoo import api, fields, models
 
 
@@ -13,16 +15,34 @@ class Channel(models.Model):
         partners_on_leave = [partner_id for partner_id in direct_partners.ids if 'leave' in partner_infos[partner_id]['im_status']]
         if partners_on_leave:
             now = fields.Datetime.now()
-            self.env.cr.execute('''SELECT res_users.partner_id as partner_id, hr_leave.out_of_office_message as out_of_office_message, hr_leave.date_to as date_to
+            self.env.cr.execute('''SELECT res_users.employee_id as employee_id, res_users.partner_id as partner_id, hr_leave.out_of_office_message as out_of_office_message, hr_leave.date_to as date_to, hr_leave.date_from as date_from
                                 FROM res_users
                                 JOIN hr_leave ON hr_leave.user_id = res_users.id
                                 AND hr_leave.state not in ('cancel', 'refuse')
                                 AND res_users.active = 't'
-                                AND hr_leave.date_from <= %s
                                 AND hr_leave.date_to >= %s
-                                AND res_users.partner_id in %s''', (now, now, tuple(partners_on_leave)))
-            out_of_office_infos = dict(((res['partner_id'], res) for res in self.env.cr.dictfetchall()))
+                                AND res_users.partner_id in %s ORDER BY date_to asc''', (now, tuple(partners_on_leave)))
+            out_of_office_infos = {}
+            for res in self.env.cr.dictfetchall():
+                if out_of_office_infos.get(res['partner_id']):
+                    out_of_office_infos[res['partner_id']].append(res)
+                else:
+                    out_of_office_infos[res['partner_id']] = [res]
             for partner_id, out_of_office_info in out_of_office_infos.items():
-                partner_infos[partner_id]['out_of_office_date_end'] = out_of_office_info['date_to']
-                partner_infos[partner_id]['out_of_office_message'] = out_of_office_info['out_of_office_message']
+                first_leave = out_of_office_info[0]
+                partner_infos[partner_id]['out_of_office_message'] = first_leave['out_of_office_message']
+                partner_infos[partner_id]['out_of_office_date_end'] = first_leave['date_to']
+                if first_leave.get('employee_id'):
+                    employee_id = self.env['hr.employee'].browse(first_leave.get('employee_id'))
+                    resource_calendar_id = employee_id.resource_calendar_id
+                    working_days = resource_calendar_id.mapped('attendance_ids.dayofweek')
+                    days = 1
+                    for next_leave in out_of_office_info:
+                        while str((first_leave['date_to'].date() + timedelta(days=days)).weekday()) not in working_days:
+                            days += 1
+                        if next_leave['date_from'].date() == first_leave['date_to'].date() + timedelta(days=days):
+                            days = 1
+                            partner_infos[partner_id]['out_of_office_date_end'] = next_leave['date_to']
+                            first_leave = next_leave
+
         return partner_infos
