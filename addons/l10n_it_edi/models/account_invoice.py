7d7b8222488f113e37f2a3c40bfaf36c8a1419e3
Varun Raval <var@odoo.com>
2019-07-02 04:58:06 +0000

    [IMP] l10n_it_edi: tax representative should have vat number
    If Company Tax Representative of the seller is not having a VAT number,
    which is mandatory, system won't allow to validate the invoice.
    
    closes odoo/odoo#34511
    
    Task: 1971512
    Closes: #34511
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- addons/l10n_it_edi/models/account_invoice.py
+++ addons/l10n_it_edi/models/account_invoice.py
@@ -108,6 +108,9 @@ class AccountMove(models.Model):
         if not seller.country_id:
             raise UserError(_("%s must have a country.") % (seller.display_name))
 
+        if seller.l10n_it_has_tax_representative and not seller.l10n_it_tax_representative_partner_id.vat:
+            raise UserError(_("Tax representative partner %s of %s must have a tax number.") % (seller.l10n_it_tax_representative_partner_id.display_name, seller.display_name))
+
         # <1.4.1>
         if not buyer.vat and not buyer.l10n_it_codice_fiscale and buyer.country_id.code == 'IT':
             raise UserError(_("The buyer, %s, or his company must have either a VAT number either a tax code (Codice Fiscale).") % (buyer.display_name))
