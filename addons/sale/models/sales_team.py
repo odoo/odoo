odoo/odoo

commit beaa30a3d1843de43a45f419bfbc1bfa7613a920

[IMP/REF] accounting-pocalypse yeaaahh
This commit merges the following models
 * account.invoice and account.move
 * account.invoice.line and account.move.line
 * account.voucher and account.move
 * account.voucher.line and account.move.line

It was the opportunity for a big cleanup of the code, so it also restructures the whole account module, its different models/fields, the tests etc. for a better world and a better code readability.

==== Rationale ====
The rationale of this huge change is that we want journal entries / invoices to be easily edited, and changes reflected in the other model. It's a HUGE feature and very strategic for the fiduciary companies. For example, changing the account of a journal entry needs to be automatically reflected on the related invoice.

The same reasoning applies to sale/purchase vouchers.

==== Changes made in features =====
When creating an invoice, you are now creating a journal entry directly.
--> The object account.invoice no longer exists.
In the same fashion when creating an invoice line, you're now adding journal items directly in the journal entry representing the invoice. If this invoice line has some tax, it may create additional journal items as well.
--> The models account.invoice.line & account.invoice.tax no longer exist

Identically, when creating a sale/purchase receipt with its lines, you are now creating a journal entry directly and there's no more usability difference between encoding a receipt or an invoice.
--> The object account.voucher no longer exists.
--> The object account.voucher.line no longer exists.
--> The whole account_voucher module no longer exists.

Positive side-effects coming from these changes are
* draft invoices/bills/sale or purchase receipts now create a draft accounting entry. Validate these objects now simply post its journal entry. That means that draft invoices/bills/sale or purchase receipt can straightforwardly be included in reporting or budgets.

addons/sale/models/sales_team.py
@@ -65,16 +65,32 @@ class CrmTeam(models.Model):
 
     @api.multi
     def _compute_invoiced(self):
-        invoice_data = self.env['account.invoice'].read_group([
-            ('state', 'in', ['open', 'in_payment', 'paid']),
-            ('team_id', 'in', self.ids),
-            ('date', '<=', date.today()),
-            ('date', '>=', date.today().replace(day=1)),
-            ('type', 'in', ['out_invoice', 'out_refund']),
-        ], ['amount_untaxed_signed', 'team_id'], ['team_id'])
-        data_map = { datum['team_id'][0]: datum['amount_untaxed_signed'] for datum in invoice_data}
+        if not self:
+            return
+
+        query = '''
+            SELECT
+                move.team_id         AS team_id,
+                SUM(line.balance)    AS amount_untaxed_signed
+            FROM account_move move
+            LEFT JOIN account_move_line line ON line.move_id = move.id
+            INNER JOIN account_account_type account_type ON account_type.id = line.user_type_id 
+            WHERE move.type IN ('out_invoice', 'out_refund', 'in_invoice', 'in_refund')
+            AND move.invoice_payment_state IN ('in_payment', 'paid')
+            AND move.state = 'posted'
+            AND move.team_id IN %s
+            AND move.date BETWEEN %s AND %s
+            AND line.tax_line_id IS NULL
+            AND line.display_type IS NULL
+            AND account_type.type NOT IN ('receivable', 'payable')
+        '''
+        today = fields.Date.today()
+        params = [tuple(self.ids), fields.Date.to_string(today), fields.Date.to_string(today.replace(day=1))]
+        self._cr.execute(query, params)
+
+        data_map = dict((v[0], v[1]) for v in self._cr.fetchall())
         for team in self:
-            team.invoiced = data_map.get(team.id,0.0)
+            team.invoiced = data_map.get(team.id, 0.0)
     
     def _graph_get_model(self):
         if self._context.get('in_sales_app'):

commit 2337d4115bc53fd8be574480dcf432283432bd78

[FIX] sale: traceback in sales team dashboard
* Sales > Orders > Sales Team > traceback with error 'user_type_id' isn't defined on account.move.line.
* Also the move.team_id has to appear in some group by because we're using an aggregate function.

Both bugs have been introduced by beaa30a (aka accounting-pocalypse)

closes odoo/odoo#34570

Signed-off-by: Quentin De Paoli (qdp) <qdp@openerp.com>

addons/sale/models/sales_team.py
@@ -74,7 +74,6 @@ class CrmTeam(models.Model):
                 SUM(line.balance)    AS amount_untaxed_signed
             FROM account_move move
             LEFT JOIN account_move_line line ON line.move_id = move.id
-            INNER JOIN account_account_type account_type ON account_type.id = line.user_type_id 
             WHERE move.type IN ('out_invoice', 'out_refund', 'in_invoice', 'in_refund')
             AND move.invoice_payment_state IN ('in_payment', 'paid')
             AND move.state = 'posted'
@@ -82,7 +81,8 @@ class CrmTeam(models.Model):
             AND move.date BETWEEN %s AND %s
             AND line.tax_line_id IS NULL
             AND line.display_type IS NULL
-            AND account_type.type NOT IN ('receivable', 'payable')
+            AND line.account_internal_type NOT IN ('receivable', 'payable')
+            GROUP BY move.team_id
         '''
         today = fields.Date.today()
         params = [tuple(self.ids), fields.Date.to_string(today), fields.Date.to_string(today.replace(day=1))]