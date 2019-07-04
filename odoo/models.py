shaneebkhan/android-app

[FIX] mail, hr: bypass user_has_groups() in superuser mode

odoo/models.py
@@ -2715,7 +2715,7 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
         for fname, field in self._fields.items():
             if allfields and fname not in allfields:
                 continue
-            if field.groups and not self.user_has_groups(field.groups):
+            if field.groups and not self.env.sumode and not self.user_has_groups(field.groups):
                 continue
 
             description = field.get_description(self.env)

odoo/odoo

odoo/models.py
@@ -2703,7 +2703,7 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
         for fname, field in self._fields.items():
             if allfields and fname not in allfields:
                 continue
-            if field.groups and not self.user_has_groups(field.groups):
+            if field.groups and not self.env.su and not self.user_has_groups(field.groups):
                 continue
 
             description = field.get_description(self.env)