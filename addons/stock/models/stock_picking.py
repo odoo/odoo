39b49f8c52c0379dd2c872ffa737fdf26d4a270d
Arnaud Baes <arb@odoo.com>
2019-07-04 15:57:17 +0200

    [REF] stock: cleaning action_done() on pickings
    Removing outdated comments, updating the docstring and some variable names.

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -566,53 +566,37 @@ class Picking(models.Model):
         return True
 
     def action_done(self):
-        """Changes picking state to done by processing the Stock Moves of the Picking
+        """ Link undone move_lines without move to an existing one
+        with the same product if there's one, else create a new one.
+        Then set the pickings states as done by processing theirs Stock Moves.
 
-        Normally that happens when the button "Done" is pressed on a Picking view.
         @return: True
         """
-        # TDE FIXME: remove decorator when migration the remaining
         todo_moves = self.mapped('move_lines').filtered(lambda self: self.state in ['draft', 'waiting', 'partially_available', 'assigned', 'confirmed'])
-        # Check if there are ops not linked to moves yet
         for pick in self:
-            # # Explode manually added packages
-            # for ops in pick.move_line_ids.filtered(lambda x: not x.move_id and not x.product_id):
-            #     for quant in ops.package_id.quant_ids: #Or use get_content for multiple levels
-            #         self.move_line_ids.create({'product_id': quant.product_id.id,
-            #                                    'package_id': quant.package_id.id,
-            #                                    'result_package_id': ops.result_package_id,
-            #                                    'lot_id': quant.lot_id.id,
-            #                                    'owner_id': quant.owner_id.id,
-            #                                    'product_uom_id': quant.product_id.uom_id.id,
-            #                                    'product_qty': quant.qty,
-            #                                    'qty_done': quant.qty,
-            #                                    'location_id': quant.location_id.id, # Could be ops too
-            #                                    'location_dest_id': ops.location_dest_id.id,
-            #                                    'picking_id': pick.id
-            #                                    }) # Might change first element
-            # # Link existing moves or add moves when no one is related
-            for ops in pick.move_line_ids.filtered(lambda x: not x.move_id):
+            # Check if there are move_lines not linked to moves yet
+            for move_line in pick.move_line_ids.filtered(lambda x: not x.move_id):
                 # Search move with this product
-                moves = pick.move_lines.filtered(lambda x: x.product_id == ops.product_id)
+                moves = pick.move_lines.filtered(lambda x: x.product_id == move_line.product_id)
                 moves = sorted(moves, key=lambda m: m.quantity_done < m.product_qty, reverse=True)
                 if moves:
-                    ops.move_id = moves[0].id
+                    move_line.move_id = moves[0].id
                 else:
                     new_move = self.env['stock.move'].create({
-                                                    'name': _('New Move:') + ops.product_id.display_name,
-                                                    'product_id': ops.product_id.id,
-                                                    'product_uom_qty': ops.qty_done,
-                                                    'product_uom': ops.product_uom_id.id,
-                                                    'description_picking': ops.description_picking,
-                                                    'location_id': pick.location_id.id,
-                                                    'location_dest_id': pick.location_dest_id.id,
-                                                    'picking_id': pick.id,
-                                                    'picking_type_id': pick.picking_type_id.id,
-                                                   })
-                    ops.move_id = new_move.id
+                        'name': _('New Move:') + move_line.product_id.display_name,
+                        'product_id': move_line.product_id.id,
+                        'product_uom_qty': move_line.qty_done,
+                        'product_uom': move_line.product_uom_id.id,
+                        'description_picking': move_line.description_picking,
+                        'location_id': pick.location_id.id,
+                        'location_dest_id': pick.location_dest_id.id,
+                        'picking_id': pick.id,
+                        'picking_type_id': pick.picking_type_id.id,
+                    })
+                    move_line.move_id = new_move.id
                     new_move._action_confirm()
                     todo_moves |= new_move
-                    #'qty_done': ops.qty_done})
+
         todo_moves._action_done(cancel_backorder=self.env.context.get('cancel_backorder'))
         self.write({'date_done': fields.Datetime.now()})
         return True
