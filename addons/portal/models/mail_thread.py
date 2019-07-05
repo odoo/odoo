a258340eb1b6953d94b58823ebafa5e944a32a36
Julien Castiaux <juc@odoo.com>
2019-07-05 10:40:59 +0200

    [FIX] portal: show emails in portal chatter
    Create a quotation and send an email to the customer, let the customer
    reply by email, as a portal user go on the portal view of that
    quotation, he does not see the emails.
    
    opw-2008653

--- addons/portal/models/mail_thread.py
+++ addons/portal/models/mail_thread.py
@@ -10,5 +10,5 @@ class MailThread(models.AbstractModel):
     _mail_post_token_field = 'access_token' # token field for external posts, to be overridden
 
     website_message_ids = fields.One2many('mail.message', 'res_id', string='Website Messages',
-        domain=lambda self: [('model', '=', self._name), ('message_type', '=', 'comment')], auto_join=True,
+        domain=lambda self: [('model', '=', self._name), '|', ('message_type', '=', 'comment'), ('message_type', '=', 'email')], auto_join=True,
         help="Website communication history")
