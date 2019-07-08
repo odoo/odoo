dbb10c443b7d62d3eea1d647ff5429bdeddd1573
Damien Bouvy <dbo@odoo.com>
2019-07-08 14:49:37 +0200

    [FIX] account_payment: accountpocalypse changes
    Leftover fieldname from the good ol' days

--- addons/account_payment/controllers/portal.py
+++ addons/account_payment/controllers/portal.py
@@ -11,7 +11,7 @@ class PortalAccount(PortalAccount):
     def _invoice_get_page_view_values(self, invoice, access_token, **kwargs):
         values = super(PortalAccount, self)._invoice_get_page_view_values(invoice, access_token, **kwargs)
         acquirers_sudo = request.env['payment.acquirer'].sudo()._get_available_acquirers(partner=invoice.partner_id, company=invoice.company_id)
-        payment_vals = acquirers_sudo._get_payment_form_values(invoice.partner_id, amount=invoice.residual, currency=invoice.currency_id)
+        payment_vals = acquirers_sudo._get_payment_form_values(invoice.partner_id, amount=invoice.amount_residual, currency=invoice.currency_id)
         values.update(payment_vals)
         # if the current user is connected we set partner_id to his partner otherwise we set it as the invoice partner
         # we do this to force the creation of payment tokens to the correct partner and avoid token linked to the public user
