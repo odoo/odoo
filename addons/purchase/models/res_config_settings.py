19d6174e19662811d215c68657a97931cc5e4332
Victor Feyens <vfe@odoo.com>
2019-07-08 15:06:08 +0200

    [ADD] (sale/purchase)_product_matrix

--- addons/purchase/models/res_config_settings.py
+++ addons/purchase/models/res_config_settings.py
@@ -25,6 +25,7 @@ class ResConfigSettings(models.TransientModel):
         implied_group="purchase.group_manage_vendor_price")
     module_account_3way_match = fields.Boolean("3-way matching: purchases, receptions and bills")
     module_purchase_requisition = fields.Boolean("Purchase Agreements")
+    module_purchase_product_matrix = fields.Boolean("Variants Grid Entry")
     po_lead = fields.Float(related='company_id.po_lead', readonly=False)
     use_po_lead = fields.Boolean(
         string="Security Lead Time for Purchase",
