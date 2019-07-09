162d0fab0fa7fa86c41ba8fdd0625b58542e041a
Thibault Delavallée <tde@odoo.com>
2019-07-09 14:02:17 +0000

    [ADD] event_sms: allow to schedule SMS in event communication
    PURPOSE
    
    Allow the user to schedule SMS communication on events
    
    SPECIFICATION
    
    In event.event form view:
      * rename tab 'Email Schedule' to 'Communication'
      * add a selection field at the beginning of event_mail_ids with label 'Send'
        and values 'Email' and 'SMS Text Message' (required with default value
        'Email');
      * add support of SMS template and SMS sending in event communication;
    
    When sending SMS on event registrations, keep a log of what has been sent.
    Use mobile or phone field of event registration as both may contain valid
    values, like partner model.
    
    Add SMS template data
      * Name: Event: Registration (see data)
      * Name: Event: Reminder (see data)
    
    Improve registration model
      * add a mobile field to the attendees form view: https://nimb.ws/p0VvyS
    
    Technically this task uses the newly-introduced API methods allowing to
    send and schedule SMS.
    
    LINKS
    
    Task 1922187
    Part of PR #34705

--- addons/event_sale/wizard/event_edit_registration.py
+++ addons/event_sale/wizard/event_edit_registration.py
@@ -33,6 +33,7 @@ class RegistrationEditor(models.TransientModel):
                     'name': reg.name,
                     'email': reg.email,
                     'phone': reg.phone,
+                    'mobile': reg.mobile,
                     'sale_order_line_id': so_line.id,
                 })
             for count in range(int(so_line.product_uom_qty) - len(existing_registrations)):
@@ -71,6 +72,7 @@ class RegistrationEditorLine(models.TransientModel):
     event_ticket_id = fields.Many2one('event.event.ticket', string='Event Ticket')
     email = fields.Char(string='Email')
     phone = fields.Char(string='Phone')
+    mobile = fields.Char(string='Mobile')
     name = fields.Char(string='Name', index=True)
 
     def get_registration_data(self):
@@ -81,6 +83,7 @@ class RegistrationEditorLine(models.TransientModel):
             'partner_id': self.editor_id.sale_order_id.partner_id.id,
             'name': self.name or self.editor_id.sale_order_id.partner_id.name,
             'phone': self.phone or self.editor_id.sale_order_id.partner_id.phone,
+            'mobile': self.mobile or self.editor_id.sale_order_id.partner_id.mobile,
             'email': self.email or self.editor_id.sale_order_id.partner_id.email,
             'origin': self.editor_id.sale_order_id.name,
             'sale_order_id': self.editor_id.sale_order_id.id,
