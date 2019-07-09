d561868bfa406b5ab22fd2719ec4426a773f4935
Mathieu Duckerts-Antoine <dam@odoo.com>
2019-07-09 09:40:09 +0000

    [IMP] web: review date filters and groupbys functioning
    This commit modifies the functioning of date filters
    in the control panel Filters and Group By submenus.
    
    For a filter with date attribute set, the list of
    proposed options is shorter but allows a bigger set of
    domains to be quickly created (through combinations of the options).
    The resulting list of domains is different of the previous one:
    domains like "Today" or "Last Week" are left aside but many others
    (and their unions) are now accessible.
    The domains generated in that way are no more dynamic but it is still possible
    to save dynamic filters in general.
    
    This commit also reintroduces the multi selection of options for groupbys of type
    date/datetime in the groupby menu.
    
    Task ID: 2028787

--- addons/web/static/src/js/views/control_panel/search/filter_menu.js
+++ addons/web/static/src/js/views/control_panel/search/filter_menu.js
@@ -6,7 +6,6 @@ var core = require('web.core');
 var Domain = require('web.Domain');
 var DropdownMenu = require('web.DropdownMenu');
 var search_filters = require('web.search_filters');
-var time = require('web.time');
 
 var _t = core._t;
 var QWeb = core.qweb;
@@ -109,37 +108,6 @@ var FilterMenu = DropdownMenu.extend({
         if (this.generatorMenuIsOpen && !this.propositions.length) {
             this._appendProposition();
         }
-    },
-    /**
-     * @override
-     * @private
-     */
-    _renderMenuItems: function () {
-        var self= this;
-        this._super.apply(this, arguments);
-        // the following code adds tooltip on date options in order
-        // to alert the user of the meaning of intervals
-        var $options = this.$('.o_item_option');
-        $options.each(function () {
-            var $option = $(this);
-            $option.tooltip({
-                delay: { show: 500, hide: 0 },
-                title: function () {
-                    var itemId = $option.attr('data-item_id');
-                    var optionId = $option.attr('data-option_id');
-                    var fieldName = _.findWhere(self.items, {id: itemId}).fieldName;
-                    var domain = Domain.prototype.constructDomain(fieldName, optionId, 'date', true);
-                    var evaluatedDomain = Domain.prototype.stringToArray(domain);
-                    var dateFormat = time.getLangDateFormat();
-                    var dateStart = moment(evaluatedDomain[1][2], "YYYY-MM-DD", 'en').format(dateFormat);
-                    var dateEnd = moment(evaluatedDomain[2][2], "YYYY-MM-DD", 'en').format(dateFormat);
-                    if (optionId === 'today' || optionId === 'yesterday') {
-                        return dateStart;
-                    }
-                    return _.str.sprintf(_t('From %s To %s'), dateStart, dateEnd);
-                }
-            });
-        });
     },
     /**
      * Hide and display the submenu which allows adding custom filters.
