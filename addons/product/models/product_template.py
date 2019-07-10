a88c00cbe266dca56e03d0fdfd03198c8514ac6c
Victor Feyens <vfe@odoo.com>
2019-07-10 14:57:43 +0200

    [MOV] sale -> product : product variant creation based on combination of product.template.attribute.value

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -2,7 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import itertools
-
+import logging
+_logger = logging.getLogger(__name__)
 
 from odoo import api, fields, models, tools, _
 from odoo.exceptions import ValidationError, RedirectWarning, UserError
@@ -830,6 +831,51 @@ class ProductTemplate(models.Model):
         filtered_combination = combination._without_no_variant_attributes()
         attribute_values = filtered_combination.mapped('product_attribute_value_id')
         return self.env['product.product'].browse(self._get_variant_id_for_combination(attribute_values))
+
+    def _create_product_variant(self, combination, log_warning=False):
+        """ Create if necessary and possible and return the product variant
+        matching the given combination for this template.
+
+        It is possible to create only if the template has dynamic attributes
+        and the combination itself is possible.
+
+        :param combination: the combination for which to get or create variant.
+            The combination must contain all necessary attributes, including
+            those of type no_variant. Indeed even though those attributes won't
+            be included in the variant if newly created, they are needed when
+            checking if the combination is possible.
+        :type combination: recordset of `product.template.attribute.value`
+
+        :param log_warning: whether a warning should be logged on fail
+        :type log_warning: bool
+
+        :return: the product variant matching the combination or none
+        :rtype: recordset of `product.product`
+        """
+        self.ensure_one()
+
+        Product = self.env['product.product']
+
+        product_variant = self._get_variant_for_combination(combination)
+        if product_variant:
+            return product_variant
+
+        if not self.has_dynamic_attributes():
+            if log_warning:
+                _logger.warning('The user #%s tried to create a variant for the non-dynamic product %s.' % (self.env.user.id, self.id))
+            return Product
+
+        if not self._is_combination_possible(combination):
+            if log_warning:
+                _logger.warning('The user #%s tried to create an invalid variant for the product %s.' % (self.env.user.id, self.id))
+            return Product
+
+        attribute_values = combination.mapped('product_attribute_value_id')._without_no_variant_attributes()
+
+        return Product.sudo().create({
+            'product_tmpl_id': self.id,
+            'attribute_value_ids': [(6, 0, attribute_values.ids)]
+        })
 
     @tools.ormcache('self.id', 'attribute_values')
     def _get_variant_id_for_combination(self, attribute_values):
--- addons/sale/models/product_template.py
+++ addons/sale/models/product_template.py
@@ -59,51 +59,6 @@ class ProductTemplate(models.Model):
         }
         return action
 
-    def _create_product_variant(self, combination, log_warning=False):
-        """ Create if necessary and possible and return the product variant
-        matching the given combination for this template.
-
-        It is possible to create only if the template has dynamic attributes
-        and the combination itself is possible.
-
-        :param combination: the combination for which to get or create variant.
-            The combination must contain all necessary attributes, including
-            those of type no_variant. Indeed even though those attributes won't
-            be included in the variant if newly created, they are needed when
-            checking if the combination is possible.
-        :type combination: recordset of `product.template.attribute.value`
-
-        :param log_warning: whether a warning should be logged on fail
-        :type log_warning: bool
-
-        :return: the product variant matching the combination or none
-        :rtype: recordset of `product.product`
-        """
-        self.ensure_one()
-
-        Product = self.env['product.product']
-
-        product_variant = self._get_variant_for_combination(combination)
-        if product_variant:
-            return product_variant
-
-        if not self.has_dynamic_attributes():
-            if log_warning:
-                _logger.warning('The user #%s tried to create a variant for the non-dynamic product %s.' % (self.env.user.id, self.id))
-            return Product
-
-        if not self._is_combination_possible(combination):
-            if log_warning:
-                _logger.warning('The user #%s tried to create an invalid variant for the product %s.' % (self.env.user.id, self.id))
-            return Product
-
-        attribute_values = combination.mapped('product_attribute_value_id')._without_no_variant_attributes()
-
-        return Product.sudo().create({
-            'product_tmpl_id': self.id,
-            'attribute_value_ids': [(6, 0, attribute_values.ids)]
-        })
-
     def create_product_variant(self, product_template_attribute_value_ids):
         """ Create if necessary and possible and return the id of the product
         variant matching the given combination for this template.
