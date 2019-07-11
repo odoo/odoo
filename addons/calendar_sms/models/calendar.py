shaneebkhan/android-app

[IMP] calendar_sms, hr_presence: use sms.template and its API when sending SMS
Purpose of this commit is to include SMS templates and the new sms messaging
API and methods in calendar_sms and hr_presence. Templates allow people to
customize the content of sent SMS. New SMS messaging API allows to track
notification status when sending SMS.

Related to task 1922163
Linked to PR #33510

addons/calendar_sms/models/calendar.py
@@ -20,8 +20,12 @@ class CalendarEvent(models.Model):
     def _do_sms_reminder(self):
         """ Send an SMS text reminder to attendees that haven't declined the event """
         for event in self:
-            sms_msg = _("Event reminder: %s on %s.") % (event.name, event.start_datetime or event.start_date)
-            event._message_sms(sms_msg)
+            event._message_sms_using_template(
+                partner_ids=self._get_default_sms_recipients().ids,
+                sms_template_xmlid='calendar_sms.sms_template_data_calendar_reminder',
+                sms_template_fallback=_("Event reminder: %s on %s.") % (event.name, event.start_datetime or event.start_date),
+                put_in_queue=False
+            )
 
 
 class CalendarAlarm(models.Model):

odoo/odoo

addons/calendar_sms/models/calendar.py
@@ -20,8 +20,12 @@ class CalendarEvent(models.Model):
     def _do_sms_reminder(self):
         """ Send an SMS text reminder to attendees that haven't declined the event """
         for event in self:
-            sms_msg = _("Event reminder: %s on %s.") % (event.name, event.start_datetime or event.start_date)
-            event._message_sms(sms_msg)
+            event._message_sms_with_template(
+                template_xmlid='calendar_sms.sms_template_data_calendar_reminder',
+                template_fallback=_("Event reminder: %s on %s.") % (event.name, event.start_datetime or event.start_date),
+                partner_ids=self._sms_get_default_partners().ids,
+                put_in_queue=False
+            )
 
 
 class CalendarAlarm(models.Model):