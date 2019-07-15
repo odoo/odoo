b3e7fd040b934ee1a60ddaf8c866af3e787e484c
Thibault Delavallée <tde@odoo.com>
2019-07-15 09:18:22 +0000

    [MOV] mass_mailing: reorganize module according to guidelines
    PURPOSE
    
    Mass mailing is currently a bit messy. As SMS will be added as a way to notify
    people in mass through SMS let us take this opportunity to somehow clean this
    application: organization, light code cleaning, model renaming.
    
    SPECIFICATIONS
    
    Guidelines: re-organize model files and split them according to main
    models. Also reorganize views.
    
    Done in this commit
    
      * split big python file by main models;
      * split views according to python files;
      * move statistics report to /report;
      * merge some exploded view files (assets, snippets, application menus);
    
    LINKS
    
    Task ID 2037906
    Preparing task ID 1997464 (SMS addition in mass mailing)
    PR #34938

--- addons/mass_mailing/models/__init__.py
+++ addons/mass_mailing/models/__init__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import link_tracker
+from . import mass_mailing_contact
+from . import mass_mailing_list
 from . import mass_mailing
 from . import mass_mailing_stats
 from . import mail_mail
 from . import mail_template
 from . import mail_thread
 from . import res_config_settings
-from . import mass_mailing_report
-from . import link_tracker
