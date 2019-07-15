dc95bee2b39bf77d51bab1044ea4dfb3b99100b4
Thibault Delavallée <tde@odoo.com>
2019-07-15 11:18:22 +0200

    [MOV] mass_mailing: split models

--- addons/mass_mailing/models/__init__.py
+++ addons/mass_mailing/models/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import mass_mailing_contact
+from . import mass_mailing_list
 from . import mass_mailing
 from . import mass_mailing_stats
 from . import mail_mail
