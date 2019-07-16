553d0819ecdf56b4ddf12e1a3f8f81f9b74d92ec
Arnold Moyaux <arm@odoo.com>
2019-07-16 08:08:46 +0000

    [DEL] stock, mrp: remove report forecast
    It will be replaced by an easier report that will
    dispaly the cumulative forecast and the planned in/out
    moves.

--- addons/mrp/report/__init__.py
+++ addons/mrp/report/__init__.py
@@ -2,5 +2,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import mrp_report_bom_structure
-from . import report_stock_forecast
 from . import report_stock_rule
