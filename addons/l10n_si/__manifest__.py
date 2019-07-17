ead1e3c2d163ad79c5af32fae902dda603dbbd3a
Naglis Jonaitis <naglis@mailbox.org>
2019-07-17 21:56:11 +0000

    [FIX] l10n_si: fix typo in manifest key
    closes odoo/odoo#34962
    
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- addons/l10n_si/__manifest__.py
+++ addons/l10n_si/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Slovenian - Accounting",
-    "versiom": "1.1",
+    "version": "1.1",
     "author": "Mentis d.o.o.",
     "website": "http://www.mentis.si",
     'category': 'Localization',
