396a3f196eee8db1b537603e5b3adbc09325c071
Kishan Gajjar <kig@odoo.com>
2019-07-17 10:07:40 +0000

    [FIX] web_editor: allow autoplay for Vimeo videos
    X-original-commit: 1a87fcc8a39cff99358b91718994ff14967d39bf

--- addons/web_editor/static/src/js/widgets/widgets.js
+++ addons/web_editor/static/src/js/widgets/widgets.js
@@ -949,7 +949,7 @@ var VideoWidget = MediaWidget.extend({
             $video.attr('src', vinMatch[0] + '/embed/simple');
             videoType = 'vin';
         } else if (vimMatch && vimMatch[3].length) {
-            $video.attr('src', '//player.vimeo.com/video/' + vimMatch[3] + autoplay);
+            $video.attr('src', '//player.vimeo.com/video/' + vimMatch[3] + autoplay.replace('mute', 'muted'));
             videoType = 'vim';
         } else if (dmMatch && dmMatch[2].length) {
             var just_id = dmMatch[2].replace('video/','');
