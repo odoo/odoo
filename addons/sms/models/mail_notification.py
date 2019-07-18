67211e54298201a5e27878e9604a07e7df1bea7f
Denis Ledoux <dle@odoo.com>
2019-07-18 14:39:38 +0200

    fixes performance test_mail

--- addons/sms/models/mail_notification.py
+++ addons/sms/models/mail_notification.py
@@ -16,3 +16,19 @@ class Notification(models.Model):
         ('sms_credit', 'Insufficient Credit'),
         ('sms_server', 'Server Error')]
     )
+    # DLE P152: test_complex_message_post and many others
+    # Though this is an interesting case:
+    # On sms.resend.recipient, partner_id depends on mail.notification.partner_id as it is defined as a related
+    # fields.Many2one('res.partner', 'Partner', related='notification_id.res_partner_id', readonly=True)
+    # The below is to add an inverse to the many2one sms.resend.recipient.notification_id
+    # so it maintains a list of sms.resend.recipient.partner_id to invalidate
+    # when modifying the partner_id of mail.notification. Otherwise the ORM has to do a plain search to find the sms.resend.recipient
+    # which have as notification id the one that has been modified
+    # Though:
+    #  - We already discussed the matter it would rather be more interesting to do this automatically for these cases,
+    #    with a kind of virtual one2many the developer/user doesn't know about,
+    #    so the developer doesnt have to create it himself for this technical/performance reason
+    #  - In this case, the modified happens on the creation of the mail.notification, and has it has just been created it
+    #    would be quite safe to assume there was no `sms.resend.recipient` and we could have avoided the search,
+    #    even without this virtual/technical one2many.
+    sms_resend_recipient_ids = fields.One2many('sms.resend.recipient', 'notification_id', strong='SMS recipients to resend')
