6eddf26459c53eff74bb49b68561e8ad1461aabe
svs-odoo <svs@odoo.com>
2019-07-19 14:19:30 +0200

    [FIX] stock: inventory at date for single product
    On stock list view of a single product, when user click on "Inventory at
    Date" button, the displayed report shows all products.
    This commit makes the report only shows the current product.
    
    Task #2039731

--- addons/stock/wizard/stock_quantity_history.py
+++ addons/stock/wizard/stock_quantity_history.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models, _
+from odoo.osv import expression
 
 
 class StockQuantityHistory(models.TransientModel):
@@ -15,6 +16,13 @@ class StockQuantityHistory(models.TransientModel):
     def open_at_date(self):
         tree_view_id = self.env.ref('stock.view_stock_product_tree').id
         form_view_id = self.env.ref('stock.product_form_view_procurement_button').id
+        domain = [('type', '=', 'product')]
+        product_id = self.env.context.get('product_id', False)
+        product_tmpl_id = self.env.context.get('product_tmpl_id', False)
+        if product_id:
+            domain = expression.AND([domain, [('id', '=', product_id)]])
+        elif product_tmpl_id:
+            domain = expression.AND([domain, [('product_tmpl_id', '=', product_tmpl_id)]])
         # We pass `to_date` in the context so that `qty_available` will be computed across
         # moves until date.
         action = {
@@ -23,7 +31,7 @@ class StockQuantityHistory(models.TransientModel):
             'view_mode': 'tree,form',
             'name': _('Products'),
             'res_model': 'product.product',
-            'domain': "[('type', '=', 'product')]",
+            'domain': domain,
             'context': dict(self.env.context, to_date=self.inventory_datetime),
         }
         return action
