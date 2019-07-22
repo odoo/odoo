ee10b19f67827f9e05382ee25cd6021f68ddf10d
Jérémy Hennecart <jeh@odoo.com>
2019-07-22 13:08:55 +0000

    [REF] website_sale_slides: improve integration into eLeaning application
    PURPOSE
    
    eLearning should have its own application in manager. Indeed this is becoming
    a big application and having it embedded inside Website application is not
    enough anymore. It should also hold updated and easy-to-use menus,
    navigation, actions and views in backend.
    
    SPECIFICATIONS
    
    Improve course form view: add a revenue stat button displaying the total
    of sale order linked to the course's product.
    
    Add in slide.channel kanban card
      * Add "Sales       150.00$" below "Watch Time" if type "on payment". Same
        info than the smart/stat button on the course itself. Click on the amount
        sends to Reporting Revenues (same than on the stat btn)
    
    Add menu entry inside Reporting
        * Revenues: graph view on sale.report model showing revenues of eLearning
          courses.
            Area chart, X: month Y: €, Color: course
    
    LINKS
    
    Task 1978729
    PR #35061
    
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>
    Co-Authored-By: Jérémy Hennecart <jeh@odoo.com>

--- addons/website_sale_slides/models/slide_channel.py
+++ addons/website_sale_slides/models/slide_channel.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import api, fields, models
 
 
 class Channel(models.Model):
@@ -9,10 +9,31 @@ class Channel(models.Model):
 
     enroll = fields.Selection(selection_add=[('payment', 'On payment')])
     product_id = fields.Many2one('product.product', 'Product', index=True)
+    product_sale_revenues = fields.Float(
+        string='Total revenues', compute='_compute_product_sale_revenues',
+        groups="sales_team.group_sale_salesman")
 
     _sql_constraints = [
         ('product_id_check', "CHECK( enroll!='payment' OR product_id IS NOT NULL )", "Product is required for on payment channels.")
     ]
+
+    @api.depends('product_id')
+    def _compute_product_sale_revenues(self):
+        domain = [
+            ('state', 'in', self.env['sale.report']._get_done_states()),
+            ('product_id', 'in', self.product_id.ids),
+        ]
+        rg_data = dict(
+            (item['product_id'][0], item['price_total'])
+            for item in self.env['sale.report'].read_group(domain, ['product_id', 'price_total'], ['product_id'])
+        )
+        for channel in self:
+            channel.product_sale_revenues = rg_data.get(channel.product_id.id, 0)
+
+    def action_view_sales(self):
+        action = self.env.ref('website_sale_slides.sale_report_action_slides').read()[0]
+        action['domain'] = [('product_id', 'in', self.product_id.ids)]
+        return action
 
     def _filter_add_members(self, target_partners, **member_values):
         """ Overridden to add 'payment' channels to the filtered channels. People
