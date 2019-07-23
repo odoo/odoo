e446a5409d1a2152e6140a0bb3437b28fb121532
Pratima Gupta <pgu@odoo.com>
2019-07-23 05:52:36 +0000

    [IMP] stock: hide from and to locations from receipt and delivery
    In this commit -
    In detailed operations wizard, from and to locations should be hidden when -
    
    1) Operation type is of 'Vendor' (From location)
    2) Operation type is of 'Customer' (To location)
    
    task-2036654
    
    closes odoo/odoo#35062
    
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -519,8 +519,8 @@ class StockMove(models.Model):
                 self.env.context,
                 show_lots_m2o=self.has_tracking != 'none' and (picking_type_id.use_existing_lots or self.state == 'done' or self.origin_returned_move_id.id),  # able to create lots, whatever the value of ` use_create_lots`.
                 show_lots_text=self.has_tracking != 'none' and picking_type_id.use_create_lots and not picking_type_id.use_existing_lots and self.state != 'done' and not self.origin_returned_move_id.id,
-                show_source_location=self.location_id.child_ids and self.picking_type_id.code != 'incoming',
-                show_destination_location=self.location_dest_id.child_ids and self.picking_type_id.code != 'outgoing',
+                show_source_location=self.picking_type_id.code != 'incoming',
+                show_destination_location=self.picking_type_id.code != 'outgoing',
                 show_package=not self.location_id.usage == 'supplier',
                 show_reserved_quantity=self.state != 'done' and not self.picking_id.immediate_transfer and self.picking_type_id.code != 'incoming'
             ),
