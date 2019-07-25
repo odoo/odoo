17013c1d704685cd38a792a63ddbb196b1db3f9f
Susana <susana@blancomartin.cl>
2019-07-26 01:20:47 +0200

    Removed l10n_cl_base module

--- addons/l10n_cl_base/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-# from . import l10n_cl_county
-from . import res_company
-from . import res_country
-from . import res_currency
-from . import res_partner
-from . import res_partner_bank
-from . import res_state
-
