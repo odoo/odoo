3cc517501c6ee542f942ea630f96fc411bf44505
David Beguin <dbe@odoo.com>
2019-07-26 15:32:46 +0200

    test added on visitor creation on treacked page

---
+++ addons/website/tests/test_website_visitor.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+from odoo.tests import HttpCase
+
+
+class WebsiteVisitorTests(HttpCase):
+    def test_create_visitor_on_tracked_page(self):
+        Page = self.env['website.page']
+        View = self.env['ir.ui.view']
+        Visitor = self.env['website.visitor']
+        base_view = View.create({
+            'name': 'Base',
+            'type': 'qweb',
+            'arch': '''<t name="Homepage" t-name="website.base_view">
+                        <t t-call="website.layout">
+                            I am a generic page
+                        </t>
+                    </t>''',
+            'key': 'test.base_view',
+        })
+        untracked_page = Page.create({
+            'view_id': base_view.id,
+            'url': '/untracked_page_1',
+            'website_published': True,
+        })
+        tracked_page = Page.create({
+            'view_id': base_view.id,
+            'url': '/tracked_page_1',
+            'website_published': True,
+            'track': True,
+        })
+
+        self.assertEqual(len(Visitor.search([])), 0, "No visitor at the moment")
+        self.url_open(untracked_page.url)
+        self.assertEqual(len(Visitor.search([])), 0, "No visitor created after visiting an untracked page")
+        self.url_open(tracked_page.url)
+        self.assertEqual(len(Visitor.search([])), 1, "A visitor should be created after visiting a tracked page")
