5d58767cddb599f702337caca2fb1bc282a15296
Daniel Blanco <daniel@blancomartin.cl>
2019-07-27 15:12:58 -0400

    [FIX] clean code from res_state

--- addons/l10n_cl_base/models/res_state.py
+++ addons/l10n_cl_base/models/res_state.py
@@ -33,10 +33,6 @@ class ResState(models.Model):
     _name = 'res.country.state'
     _inherit = 'res.country.state'
 
-    # code = fields.Char(
-    #     'State Code', size=32, help='The state code.\n', required=True)
-    # complete_name = fields.Char(
-    #     string='Complete Name')
     parent_id = fields.Many2one(
         'res.country.state', 'Parent State', index=True,
         domain="[('type', '=', 'view'), ('id', '!=', id)]")
