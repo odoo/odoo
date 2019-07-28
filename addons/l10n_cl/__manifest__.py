2e5f849be610130cc973e40d31b67aac4958379d
Daniel Blanco <daniel@blancomartin.cl>
2019-07-28 15:09:09 -0400

    [WIP] account_tax_report_data.xml

--- addons/l10n_cl/__manifest__.py
+++ addons/l10n_cl/__manifest__.py
@@ -15,6 +15,7 @@ Plan contable chileno e impuestos de acuerdo a disposiciones vigentes
         'l10n_latam_document',
         ],
     'data': [
+        'data/account_tax_report_data.xml',
         'data/l10n_cl_chart_data.xml',
         'data/account_tax_group_data.xml',
         'data/account_tax_tags_data.xml',
