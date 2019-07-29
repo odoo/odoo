303b1bda5fe7bc7d554ca55d93ceae1f1f0b4e98
Meghna Jaswani <mja@odoo.com>
2019-07-29 16:34:06 +0530

    [IMP] l10n_il : Added new model to define two new fields i.e holding tax reason and ITA branch

--- addons/l10n_il/models/res_partner.py
+++ addons/l10n_il/models/res_partner.py
@@ -9,4 +9,3 @@ class ResPartner(models.Model):
 
     l10n_il_income_tax_id_number = fields.Char(string='IncomeTax ID')
     l10n_il_registry_number = fields.Char(string='Registry Number')
-    l10n_il_withh_tax_reason = fields.Selection([('06', 'Payments'), ('07', 'Payments to a foreign person or a foreign company done by an Israeli enterprise'), ('18', 'Dividend payments')], required=True, default='06', string='WHT Reason', readonly=False)
