b58e7e873f3988b16cbbdd5264e643ffc04fca1e
Lucas Perais (lpe) <lpe@odoo.com>
2019-07-30 12:02:27 +0000

    [FIX] sale: import order line multicompany
    - Admin (U1) user, Demo (U2) with Sales Manager rights
    - Create parent company A and child company AA, U2 has access to A (U1 to both)
    - Create 2 taxes, T1 for A, T2 for AA
    - For product P, add tax T1 in company A, and T2 in company AA
    - U2 creates a new SO in company A with no lines, export Order Lines/Product
    - U2 edits the file to add product P
    - U2 imports the file
    
    Before this commit, the sale order's line had the taxes for bot company
    
    After this commit, it only has the taxes for the company of the sale order
    
    OPW 2036453
    
    closes odoo/odoo#35294
    
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -838,7 +838,8 @@ class SaleOrderLine(models.Model):
         for line in self:
             fpos = line.order_id.fiscal_position_id or line.order_id.partner_id.property_account_position_id
             # If company_id is set, always filter taxes by the company
-            taxes = line.product_id.taxes_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
+            line_company_id = line.company_id or line.order_id.company_id
+            taxes = line.product_id.taxes_id.filtered(lambda r: not line_company_id or r.company_id == line_company_id)
             line.tax_id = fpos.map_tax(taxes, line.product_id, line.order_id.partner_shipping_id) if fpos else taxes
 
     @api.model
