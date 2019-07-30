d2bd3fb2825cc7667b00f23929d71784a4b55869
Simon Lejeune <sle@openerp.com>
2019-07-30 08:49:06 +0000

    [REF] stock, stock_account: no 'at date' on layers from picking
    When accessing the stock valuation layers of a done picking through the
    stat button "Valuation", do not display the inventory at date button. We
    guard the visibility of this button through a context key named
    "no_at_date".
    
    task-59011

--- addons/stock_account/models/stock_picking.py
+++ addons/stock_account/models/stock_picking.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from ast import literal_eval
+
 from odoo import models
 
 
@@ -11,5 +13,8 @@ class StockPicking(models.Model):
         self.ensure_one()
         domain = [('id', 'in', self.move_lines.stock_valuation_layer_ids.ids)]
         action = self.env.ref('stock_account.stock_valuation_layer_action').read()[0]
-        return dict(action, domain=domain)
+        context = literal_eval(action['context'])
+        context.update(self.env.context)
+        context['not_at_date'] = True
+        return dict(action, domain=domain, context=context)
 
