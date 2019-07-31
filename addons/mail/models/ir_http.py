4da64b3f3c8dff88d8e0e01c65374590cc0bf4f7
mgh-odoo <mgh@odoo.com>
2019-07-31 12:51:10 +0000

    [IMP] mail: email channels duplicated notifications
    This commit prevents sending a notification within Odoo when a
    message is posted from a mailing channel to a user whose notification
    management preference is set to "Handle by Emails".
    
    Task-ID 2033302
    
    closes odoo/odoo#36000
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/models/ir_http.py
+++ addons/mail/models/ir_http.py
@@ -13,4 +13,5 @@ class IrHttp(models.AbstractModel):
         result = super(IrHttp, self).session_info()
         if self.env.user.has_group('base.group_user'):
             result['out_of_office_message'] = user.out_of_office_message
+            result['notification_type'] = user.notification_type
         return result
