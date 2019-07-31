30873e1b59b475098177330d9a907eaadba7651a
Naglis Jonaitis <naglis@mailbox.org>
2019-07-31 20:04:09 +0300

    [IMP] *: fix typos in field definitions
    * account, crm, hr, im_livechat, l10n_ch, l10n_it_edi, mail,
      mass_mailing, project, stock, base
    
    - `String` -> `string`;
    - `defaut` -> `default`;
    - `defaults` -> `default`;
    - `reandonly` -> `readonly`;
    - removed redundant `placeholder` attribute for `Char` field;
    - removed redundant `size` attribute for `Float` and `Integer` fields;
    - changed to use `Selection` instead of `Char` for a field having a
      defined `selection`.

--- addons/stock/wizard/product_replenish.py
+++ addons/stock/wizard/product_replenish.py
@@ -12,7 +12,7 @@ class ProductReplenish(models.TransientModel):
     _description = 'Product Replenish'
 
     product_id = fields.Many2one('product.product', string='Product', required=True)
-    product_tmpl_id = fields.Many2one('product.template', String='Product Template', required=True)
+    product_tmpl_id = fields.Many2one('product.template', string='Product Template', required=True)
     product_has_variants = fields.Boolean('Has variants', default=False, required=True)
     product_uom_category_id = fields.Many2one('uom.category', related='product_id.uom_id.category_id', readonly=True, required=True)
     product_uom_id = fields.Many2one('uom.uom', string='Unity of measure', required=True)
