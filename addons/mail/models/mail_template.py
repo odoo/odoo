ed8ab220a6c1e7e8809f0778b26d274ab7f11c61
Jinal Patel <jip@odoo.com>
2019-08-02 17:00:24 +0530

    [IMP] mail: add default email templates by companies
    Purpose of the task is default template on composer should be
    company wise.
    
    so added company_id field and use that field to get the default
    template on object.
    
    task-2032716

--- addons/mail/models/mail_template.py
+++ addons/mail/models/mail_template.py
@@ -149,6 +149,7 @@ class MailTemplate(models.Model):
     null_value = fields.Char('Default Value', help="Optional value to use if the target field is empty")
     copyvalue = fields.Char('Placeholder Expression', help="Final placeholder expression, to be copy-pasted in the desired template field.")
     scheduled_date = fields.Char('Scheduled Date', help="If set, the queue manager will send the email after the date. If not set, the email will be send as soon as possible. Jinja2 placeholders may be used.")
+    company_id = fields.Many2one('res.company', string="Company")
 
     @api.onchange('model_id')
     def onchange_model_id(self):
