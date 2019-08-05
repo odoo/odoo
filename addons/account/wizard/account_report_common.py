7388ddce87ec407fd7df3ffbe5b50d12aa905f1f
Jigar Vaghela <jva@odoo.com>
2019-08-05 15:20:26 +0530

    [IMP] account: extendable method and views set id in view so extend in other modules account report comman method check report add extendable read parameter

--- addons/account/wizard/account_report_common.py
+++ addons/account/wizard/account_report_common.py
@@ -36,12 +36,13 @@ class AccountCommonReport(models.TransientModel):
     def _print_report(self, data):
         raise NotImplementedError()
 
-    def check_report(self):
+    def check_report(self, fields=[]):
         self.ensure_one()
         data = {}
+        fields = ['date_from', 'date_to', 'journal_ids', 'target_move', 'company_id'] + fields
         data['ids'] = self.env.context.get('active_ids', [])
         data['model'] = self.env.context.get('active_model', 'ir.ui.menu')
-        data['form'] = self.read(['date_from', 'date_to', 'journal_ids', 'target_move', 'company_id'])[0]
+        data['form'] = self.read(fields)[0]
         used_context = self._build_contexts(data)
         data['form']['used_context'] = dict(used_context, lang=self.env.context.get('lang') or 'en_US')
         return self.with_context(discard_logo_check=True)._print_report(data)
