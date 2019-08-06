b57c2e1aef60a8a207798c8bdcd62e199977736f
Arnaud Baes <arb@odoo.com>
2019-08-06 14:31:34 +0200

    [IMP] stock,product_expiry: lot views cleanup

--- addons/product_expiry/models/production_lot.py
+++ addons/product_expiry/models/production_lot.py
@@ -12,7 +12,7 @@ class StockProductionLot(models.Model):
     use_date = fields.Datetime(string='Best before Date',
         help='This is the date on which the goods with this Serial Number start deteriorating, without being dangerous yet.')
     removal_date = fields.Datetime(string='Removal Date',
-        help='This is the date on which the goods with this Serial Number should be removed from the stock.')
+        help='This is the date on which the goods with this Serial Number should be removed from the stock. This date will be used in FEFO removal strategy.')
     alert_date = fields.Datetime(string='Alert Date',
         help='Date to determine the expired lots and serial numbers using the filter "Expiration Alerts".')
     product_expiry_alert = fields.Boolean(compute='_compute_product_expiry_alert', help="The Alert Date has been reached.")
