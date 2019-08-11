b60320e46dbf4a526f7905d4c65597fcf4968487
Susana <susana@blancomartin.cl>
2019-08-11 16:24:53 +0200

    [IMP] Cleaning code

--- addons/l10n_cl/models/account_move.py
+++ addons/l10n_cl/models/account_move.py
@@ -32,21 +32,3 @@ class AccountMove(models.Model):
                     ('id', 'in', sequences.l10n_latam_document_type_id.ids)
                 ]
         return domain
-
-    # def _get_account_move_type(self):
-    #     if self.type:
-    #         return self.type
-    #     return self._context.get('default_type')
-    #
-    # def post(self):
-    #     internal_type = self._get_account_move_type()
-    #     for rec in self.filtered(lambda x: x.l10n_latam_use_documents and not x.l10n_latam_document_number and
-    #                              self.journal_id.company_id.country_id == self.env.ref('base.cl') and
-    #                              internal_type in ['out_invoice', 'out_refund']):
-    #         if internal_type == 'out_invoice':
-    #             sequence = self.journal_id.sequence_number_next
-    #         else:
-    #             sequence = self.journal_id.refund_sequence_number_next
-    #         rec.l10n_latam_document_number = sequence
-    #         rec._get_sequence().next_by_id(sequence_date=rec.date)
-    #     return super().post()
