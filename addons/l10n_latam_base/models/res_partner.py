f99d4927f6bd44a5fbbad0badb8d1999ba25e780
Katherine Zaoral <kz@adhoc.com.ar>
2019-08-12 19:52:15 +0000

    [ADD] l10n_latam_base: new module to manage multiple identification types
    In Latin American countries, every partner needs
    to be identified even if he does not have the usual
    VAT number (whatever it is called) e.g. you can also
    be identified by your identity card.
    
    We chose to put those types also in the vat field,
    but to be clear what it is, we added a selection field
    before the vat field which is VAT by default, but
    is e.g. RUT (VAT of Argentina) when you choose Argentina
    as country.

---
+++ addons/l10n_latam_base/models/res_partner.py
@@ -0,0 +1,28 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    l10n_latam_identification_type_id = fields.Many2one('l10n_latam.identification.type',
+        string="Identification Type", index=True, auto_join=True,
+        default=lambda self: self.env.ref('l10n_latam_base.it_vat', raise_if_not_found=False),
+        help="The type of identification")
+    vat = fields.Char(string='Identification Number', help="Identification Number for selected type")
+
+    @api.model
+    def _commercial_fields(self):
+        return super()._commercial_fields() + ['l10n_latam_identification_type_id']
+
+    @api.constrains('vat', 'l10n_latam_identification_type_id')
+    def check_vat(self):
+        with_vat = self.filtered(lambda x: x.l10n_latam_identification_type_id.is_vat)
+        return super(ResPartner, with_vat).check_vat()
+
+    @api.onchange('country_id')
+    def _onchange_country(self):
+        country = self.country_id or self.company_id.country_id or self.env.company.country_id
+        self.l10n_latam_identification_type_id = self.env['l10n_latam.identification.type'].search(
+            [('country_id', '=', country.id), ('is_vat', '=', True)]) or self.env.ref('l10n_latam_base.it_vat', raise_if_not_found=False)
