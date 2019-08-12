f9944276d42c2704bea6f519e78d8f0b63283f3f
Katherine Zaoral <kz@adhoc.com.ar>
2019-08-12 19:53:31 +0000

    [IMP] l10n_ar: complete refactor of Argentinian localization
    Now, it depends on l10n_latam_base for the identification
    types and on l10n_latam_invoice_document for the invoice
    numbering.
    
    It is the AFIP responsibility on the partner that
    determines the document type.
    
    There are also 3 demo companies for the most important
    responsibility types for yourself.

---
+++ addons/l10n_ar/models/account_fiscal_position.py
@@ -0,0 +1,26 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import fields, models
+
+
+class AccountFiscalPosition(models.Model):
+
+    _inherit = 'account.fiscal.position'
+
+    l10n_ar_afip_responsibility_type_ids = fields.Many2many(
+        'l10n_ar.afip.responsibility.type', 'l10n_ar_afip_reponsibility_type_fiscal_pos_rel',
+        string='AFIP Responsibility Types', help='List of AFIP responsibilities where this fiscal position '
+        'should be auto-detected')
+
+    def get_fiscal_position(self, partner_id, delivery_id=None):
+        """ Take into account the partner afip responsibility in order to auto-detect the fiscal position """
+        company = self.env['res.company'].browse(self._context.get('force_company', self.env.user.company_id.id))
+        if company.country_id == self.env.ref('base.ar'):
+            domain = [
+                ('auto_apply', '=', True),
+                ('l10n_ar_afip_responsibility_type_ids', '=', self.env['res.partner'].browse(
+                    partner_id).l10n_ar_afip_responsibility_type_id.id),
+            ]
+            if self.env.context.get('force_company'):
+                domain.append(('company_id', '=', self.env.context.get('force_company')))
+            return self.search(domain, limit=1).id
+        return super().get_fiscal_position(partner_id, delivery_id=delivery_id)
