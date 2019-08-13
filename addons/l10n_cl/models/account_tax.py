de8e00b5ffec2027c208ce9ff387f28dfe843e10
Daniel Blanco <daniel@blancomartin.cl>
2019-08-13 09:49:25 -0400

    [FIX] correct __init__.py to include account_tax l10n_cl_sii_code

--- addons/l10n_cl/models/account_tax.py
+++ addons/l10n_cl/models/account_tax.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
+# For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class AccountTax(models.Model):
