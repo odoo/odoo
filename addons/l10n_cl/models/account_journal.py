370779254ea24c531e2f9c6c85ae6f556b4e3f03
Daniel Blanco <daniel@blancomartin.cl>
2019-08-13 22:36:22 -0400

    [IMP] let just one condition and remove reduntant returns

--- addons/l10n_cl/models/account_journal.py
+++ addons/l10n_cl/models/account_journal.py
@@ -4,37 +4,25 @@ from odoo import fields, models, api
 
 
 class AccountJournal(models.Model):
-
     _inherit = "account.journal"
 
-    l10n_cl_sequence_ids = fields.Many2many('ir.sequence', relation='l10n_cl_journal_sequence_rel',
-                                            column1='journal_id', column2='sequence_id', string='Sequences',
-                                            domain="[('l10n_latam_document_type_id', '!=', False)]")
+    l10n_cl_sequence_ids = fields.Many2many(
+        'ir.sequence', 'l10n_cl_journal_sequence_rel', 'journal_id', 'sequence_id', string='Sequences',
+        domain="[('l10n_latam_document_type_id', '!=', False)]")
 
     def create_document_sequences(self):
         self.ensure_one()
-        if self.company_id.country_id != self.env.ref('base.cl'):
-            return True
-
-        if (self.env['ir.sequence'].search([('l10n_latam_document_type_id', '!=', False)]) and
-                not self.env.context.get('manual_creation', False)):
-            return True
-
-        if not self.type == 'sale' or not self.l10n_latam_use_documents:
-            return False
-
+        if (self.company_id.country_id != self.env.ref('base.cl')) or ((self.env['ir.sequence'].search(
+                [('l10n_latam_document_type_id', '!=', False)]) and not self.env.context.get(
+                'manual_creation', False))) or not self.type == 'sale' or not self.l10n_latam_use_documents:
+            return
         internal_types = ['invoice', 'debit_note', 'credit_note']
-        domain = [
-            ('country_id.code', '=', 'CL'),
-            ('internal_type', 'in', internal_types),
-            ('active', '=', True)
-        ]
-
+        domain = [('country_id.code', '=', 'CL'), ('internal_type', 'in', internal_types), ('active', '=', True)]
         documents = self.env['l10n_latam.document.type'].search(domain)
         for document in documents:
             sequence = self.env['ir.sequence'].create(document.get_document_sequence_vals(self))
             self.update({'l10n_cl_sequence_ids': [(4, {sequence.id})]})
-        return True
+        return
 
     @api.model
     def create(self, values):
@@ -42,6 +30,3 @@ class AccountJournal(models.Model):
         res = super().create(values)
         res.create_document_sequences()
         return res
-
-
-
