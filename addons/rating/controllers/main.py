020417863c57aa35ab0e99e20a38d24346ef66cf
ryv-odoo <ryv@odoo.com>
2019-08-14 15:16:51 +0200

    [REF] rating, various: improve rating.rating ACLs
    PURPOSE
    
    Rating model should be available only for internal users. External users
    access them only through dedicated routes or controllers using sudo and/or
    granting access through tokens. Therefore simplifying ACLs should be feasible.
    
    SPECIFICATIONS
    
    Remove access to rating.rating for public and portal users. Only employees
    can access it, with full access given to system admins.
    
    Update various functional flows to use sudo() and check that access is
    verified before using sudo.
    
    Impacted modules
    
      * rating / mail: add groups on some rating related fields as only
        internal users should access them now;
      * rating / mail: set some statistics fields using compute_sudo as their
        value should be accessible for external people even without access to
        the underlying rating.rating records;
      * project: makes some use of rating and has to be updated, notably for
        the public rating page;
      * website_{livechat, rating, slides}: add sudo in public routes as access
        is already granted;
      * website_slides: set statistics field using compute_sudo as their
        value should be accessible for external people;
    
    TASK ID 2053096
    PR #36592

--- addons/rating/controllers/main.py
+++ addons/rating/controllers/main.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import werkzeug
-
 from odoo import http
 from odoo.http import request
 from odoo.tools.translate import _
