8438a743093197a25f0a6aa6363b21f540d9ce64
Arnold Moyaux <arm@odoo.com>
2019-08-14 17:16:32 +0200

    [REF] mrp_subcontracting: views v2
    - nothing tracked
    The "register components" button shouldn't appear, the receipt looks normal.
    - finished products tracked
    The "register components" button shouldn't appear, the receipt looks normal.
    - finished products not tracked and components tracked
    The "register components" button appear and the burger redirects to "Produce" wizard. The user should record all the productions before being able to access SML through the burger wizard. He cannot force any SML for subcontracted products with tracked components before having recorded all the productions.
    - finished products and components tracked
    The "register components" button appear and the burger redirects to "Produce" wizard. The user should record all the productions before being able to access SML through the burger wizard. He cannot force any SML for subcontracted products with tracked components before having recorded all the productions.

--- addons/mrp_subcontracting/models/stock_warehouse.py
+++ addons/mrp_subcontracting/models/stock_warehouse.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
+from odoo import fields, models, _
 
 
 class StockWarehouse(models.Model):
@@ -136,4 +136,3 @@ class StockWarehouse(models.Model):
 
     def _get_subcontracting_location(self):
         return self.company_id.subcontracting_location_id
-
