e7661e313e4eec67e9ce2853ec5dd71a83890264
Nhomar Hernández <nhomar@vauxoo.com>
2019-08-14 06:49:13 -0500

    [IMP] l10n_pe: Welcome top new version, preparing for l10n_pe_edi

---
+++ addons/l10n_pe/models/__init__.py
@@ -0,0 +1,9 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from . import account_tax
+from . import account_move
+from . import chart_template
+from . import l10n_latam_identification_type
+from . import res_partner
+from . import res_city_district
+from . import res_city
+
