a93a73d7d9d1e8ebf1e00f9eac81fa16417e39fd
Meghna Jaswani <mja@odoo.com>
2019-08-14 15:00:52 +0530

    [REM] l10n_il: Removed the unnecessary fields which were not required

--- addons/l10n_il/__manifest__.py
+++ addons/l10n_il/__manifest__.py
@@ -17,15 +17,11 @@ This module consists:
     'website': 'http://www.odoo.com/accounting',
     'depends': ['base', 'account'],
     'data': [
-        'security/ir.model.access.csv',
         'data/account_chart_template_data.xml',
         'data/account.account.template.csv',
         'data/account_data.xml',
         'data/account_tax_template_data.xml',
         'data/account_chart_template_post_data.xml',
         'data/account_chart_template_configure_data.xml',
-        'views/res_partner_view.xml',
-        'views/res_company_view.xml',
-        'views/l10n_il_withh_tax_report_data_view.xml'
     ],
 }
--- addons/l10n_il/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-# Copyright (C) 2014 Tech Receptives (<http://techreceptives.com>).
-
-from . import res_partner
-from . import res_company
-from . import l10n_il_withh_tax_report_data
