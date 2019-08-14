db43fb096c467a6f160a929039b9eb3f17715077
Nicolas Martinelli <nim@odoo.com>
2019-08-14 16:57:12 +0200

    [FIX] account: assert_balanced in multi-company/multi-currency
    The issue arise in such a specific use case that fixing it in stable
    isn't worth the risk.
    
    This reverts commit 50774af70d1cdb39636b9b5e633c5d3a7473646b.

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -380,23 +380,16 @@ class AccountMove(models.Model):
     def assert_balanced(self):
         if not self.ids:
             return True
+        prec = self.env.user.company_id.currency_id.decimal_places
 
-        # /!\ As this method is called in create / write, we can't make the assumption the computed stored fields
-        # are already done. Then, this query MUST NOT depend of computed stored fields (e.g. balance).
-        # It happens as the ORM makes the create with the 'no_recompute' statement.
-        self._cr.execute('''
-            SELECT line.move_id
-            FROM account_move_line line
-            JOIN account_move move ON move.id = line.move_id
-            JOIN account_journal journal ON journal.id = move.journal_id
-            JOIN res_company company ON company.id = journal.company_id
-            JOIN res_currency currency ON currency.id = company.currency_id
-            WHERE line.move_id IN %s
-            GROUP BY line.move_id, currency.decimal_places
-            HAVING ROUND(SUM(debit - credit), currency.decimal_places) != 0.0;
-        ''', [tuple(self.ids)])
-
-        if self._cr.fetchone():
+        self._cr.execute("""\
+            SELECT      move_id
+            FROM        account_move_line
+            WHERE       move_id in %s
+            GROUP BY    move_id
+            HAVING      abs(sum(debit) - sum(credit)) > %s
+            """, (tuple(self.ids), 10 ** (-max(5, prec))))
+        if len(self._cr.fetchall()) != 0:
             raise UserError(_("Cannot create unbalanced journal entry."))
         return True
 
