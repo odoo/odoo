6650a517cb9100271df9f97d8cba149d591f49f1
Victor Feyens <vfe@odoo.com>
2019-08-16 13:10:08 +0200

    [IMP] sale: product views cleanup

--- addons/sale/__manifest__.py
+++ addons/sale/__manifest__.py
@@ -33,7 +33,6 @@ This module contains all the common features of Sales Management and eCommerce.
         'views/res_config_settings_views.xml',
         'views/payment_views.xml',
         'views/product_attribute_views.xml',
-        'views/product_views.xml',
         'views/utm_campaign_views.xml',
         'wizard/sale_order_cancel_views.xml',
         'wizard/sale_payment_link_views.xml',
