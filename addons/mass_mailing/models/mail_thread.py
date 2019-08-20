e8c55024f8965b2dd649e16df25ae1b957e20690
Thibault Delavallée <tde@odoo.com>
2019-08-20 11:49:47 +0000

    [IMP] mail: remove unnecessary decorators
    Purpose of this commit is to lessen complexity of stack chain in mail main
    methods. Removing decorators is a good start for this.
    
    LINKS
    
    Task 1919267
    PR #30597

--- addons/mass_mailing/models/mail_thread.py
+++ addons/mass_mailing/models/mail_thread.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import logging
-import re
 import datetime
 
-from odoo import api, models, tools, fields
-from odoo.tools import decode_smtp_header, decode_message_header
+from odoo import api, models, fields
+from odoo.tools import decode_smtp_header
 
-_logger = logging.getLogger(__name__)
 BLACKLIST_MAX_BOUNCED_LIMIT = 5
 
 
@@ -17,7 +14,7 @@ class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
     @api.model
-    def message_route_process(self, message, message_dict, routes):
+    def _message_route_process(self, message, message_dict, routes):
         """ Override to update the parent mailing traces. The parent is found
         by using the References header of the incoming message and looking for
         matching message_id in mailing.trace. """
@@ -25,7 +22,7 @@ class MailThread(models.AbstractModel):
             message_ids = [x.strip() for x in decode_smtp_header(message['References']).split()]
             self.env['mailing.trace'].set_opened(mail_message_ids=message_ids)
             self.env['mailing.trace'].set_replied(mail_message_ids=message_ids)
-        return super(MailThread, self).message_route_process(message, message_dict, routes)
+        return super(MailThread, self)._message_route_process(message, message_dict, routes)
 
     def message_post_with_template(self, template_id, **kwargs):
         # avoid having message send through `message_post*` methods being implicitly considered as
