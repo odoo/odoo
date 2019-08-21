f17ae0fbd32c1045c6557a34df24d8fc9ac44e62
Laurent Smet <las@odoo.com>
2019-08-21 13:12:26 +0200

    [FIX] account: Fix cancelling a payment
    This commit fixes a wrong condition since the new "cancel" state has been introduced in
    journal entries.
    
    issue: https://github.com/odoo/odoo/issues/35362#issuecomment-520641052

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -2809,7 +2809,7 @@ class AccountMoveLine(models.Model):
         move_ids = set()
         for line in self:
             err_msg = _('Move name (id): %s (%s)') % (line.move_id.name, str(line.move_id.id))
-            if line.move_id.state != 'draft':
+            if line.move_id.state == 'posted':
                 raise UserError(_('You cannot do this modification on a posted journal entry, you can just change some non legal fields. You must revert the journal entry to cancel it.\n%s.') % err_msg)
             if line.reconciled and not (line.debit == 0 and line.credit == 0):
                 raise UserError(_('You cannot do this modification on a reconciled entry. You can just change some non legal fields or you must unreconcile first.\n%s.') % err_msg)
