10594d9f08dd57918d0ac16fdcd1c4894872d099
Joren Van Onder <jov@odoo.com>
2019-08-21 14:11:42 -0700

    [ADD] pos_adyen: integrate Adyen's Terminal API
    This allows Adyen's POS terminals to be used in the POS. The
    architecture is as follows:
    
    Odoo <--> Adyen server <--> Adyen payment terminal
    
    It means we don't have to communicate directly with the
    terminal. Instead we send HTTP requests to Adyen's API and they take
    care of the rest. Therefore this integration doesn't require an
    IOTBox.
    
    Unfortunately this module is more complicated than it needed to be due
    to Adyen's API not supporting CORS. Because of this we can't directly
    call their API from JS. Instead we need to proxy requests through the
    Odoo backend. This complicates things further because this means we
    can't use their synchronous API. With this API an HTTP request remains
    open for the entire duration of a transaction. We can't afford to
    block an HTTP worker this long. So this implements their asynchronous
    API which calls us back when a transaction completes.
    
    In addition to the regular sale requests we also poll the status of
    the terminal using DiagnosisRequests while transactions are in
    progress. This is to handle cases where a payment terminal loses
    connection after a payment transaction is initiated. It also handles
    cases where a terminal was recently disconnected (<1 min). In this
    last case Adyen will process the payment request as usual, without
    notifying us the terminal is offline. It's important to notify the POS
    user to check the terminal, otherwise he'll have no idea something is
    wrong. We store the DiagnosisResponse's unique ID and every time the
    POS requests a status update we launch a new DiagnosisRequest. When we
    no longer receive DiagnosisResponse ID updates we can notify the user
    of this in the POS.
    
    To test this locally use something like ngrok so Adyen can call you
    back.
    
    API docs: https://docs.adyen.com/point-of-sale
    
    https://www.odoo.com/web#id=1981799&model=project.task&view_type=form&menu_id=

---
+++ addons/pos_adyen/models/pos_payment_method.py
@@ -0,0 +1,104 @@
+# coding: utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import json
+import logging
+import pprint
+import random
+import requests
+import string
+
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+_logger = logging.getLogger(__name__)
+
+class PosPaymentMethod(models.Model):
+    _inherit = 'pos.payment.method'
+
+    use_payment_terminal = fields.Selection(selection_add=[('adyen', 'Adyen')])
+    adyen_api_key = fields.Char(string="Adyen API key", help='Used when connecting to Adyen: https://docs.adyen.com/user-management/how-to-get-the-api-key/#description', copy=False)
+    adyen_terminal_identifier = fields.Char(help='[Terminal model]-[Serial number], for example: P400Plus-123456789', copy=False)
+    adyen_test_mode = fields.Boolean(help='Run transactions in the test environment.')
+    adyen_latest_response = fields.Char(help='Technical field used to buffer the latest asynchronous notification from Adyen.', copy=False, groups='base.group_erp_manager')
+    adyen_latest_diagnosis = fields.Char(help='Technical field used to determine if the terminal is still connected.', copy=False, groups='base.group_erp_manager')
+
+    @api.constrains('adyen_terminal_identifier')
+    def _check_adyen_terminal_identifier(self):
+        for payment_method in self:
+            existing_payment_method = self.search([('id', '!=', payment_method.id),
+                                                   ('adyen_terminal_identifier', '=', payment_method.adyen_terminal_identifier)],
+                                                  limit=1)
+            if existing_payment_method:
+                raise ValidationError(_('Terminal %s is already used on payment method %s.')
+                                      % (payment_method.adyen_terminal_identifier, existing_payment_method.display_name))
+
+    def _is_write_forbidden(self, fields):
+        whitelisted_fields = set(('adyen_latest_response', 'adyen_latest_diagnosis'))
+        return super(PosPaymentMethod, self)._is_write_forbidden(fields - whitelisted_fields)
+
+    def _adyen_diagnosis_request_data(self, pos_config_name):
+        service_id = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        return {
+            "SaleToPOIRequest": {
+                "MessageHeader": {
+                    "ProtocolVersion": "3.0",
+                    "MessageClass": "Service",
+                    "MessageCategory": "Diagnosis",
+                    "MessageType": "Request",
+                    "ServiceID": service_id,
+                    "SaleID": pos_config_name,
+                    "POIID": self.adyen_terminal_identifier
+                },
+                "DiagnosisRequest": {
+                    "HostDiagnosisFlag": False
+                }
+            }
+        }
+
+    def get_latest_adyen_status(self, pos_config_name):
+        self.ensure_one()
+        latest_response = self.sudo().adyen_latest_response
+        latest_response = json.loads(latest_response) if latest_response else False
+        self.sudo().adyen_latest_response = ''  # avoid handling old responses multiple times
+
+        # Poll the status of the terminal if there's no new
+        # notification we received. This is done so we can quickly
+        # notify the user if the terminal is no longer reachable due
+        # to connectivity issues.
+        if not latest_response:
+            self.proxy_adyen_request(self._adyen_diagnosis_request_data(pos_config_name))
+
+        return {
+            'latest_response': latest_response,
+            'last_received_diagnosis_id': self.adyen_latest_diagnosis,
+        }
+
+    def proxy_adyen_request(self, data):
+        ''' Necessary because Adyen's endpoints don't have CORS enabled '''
+        self.ensure_one()
+        TIMEOUT = 10
+        endpoint = 'https://terminal-api-live.adyen.com/async'
+        if self.adyen_test_mode:
+            endpoint = 'https://terminal-api-test.adyen.com/async'
+
+        _logger.info('request to adyen\n%s', pprint.pformat(data))
+        headers = {
+            'x-api-key': self.adyen_api_key,
+            'Content-Type': 'application/json'
+        }
+        req = requests.post(endpoint, data=json.dumps(data), headers=headers, timeout=TIMEOUT)
+        _logger.info('response from adyen (HTTP status %s):\n%s', req.status_code, req.text)
+
+        # Authentication error doesn't return JSON
+        if req.status_code == 401:
+            return {
+                'error': {
+                    'status_code': req.status_code,
+                    'message': req.text
+                }
+            }
+
+        if req.text == 'ok':
+            return True
+
+        return req.json()
