c55daccc1b9659393f785e87acf883856201dbdc
Lucas Perais (lpe) <lpe@odoo.com>
2019-08-21 15:33:13 +0000

    [FIX] lunch: lunch search panel right domain on start
    Open the Lunch kanban view
    
    Before this commit, the counts for each product were the counts
    across all location
    This was before the location domain from the lunch widget was not sent
    
    After this commit, the counts displayed on startup are the ones
    of the right user location
    
    OPW 2053044
    
    closes odoo/odoo#35976
    
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/lunch/static/src/js/lunch_kanban_model.js
+++ addons/lunch/static/src/js/lunch_kanban_model.js
@@ -12,6 +12,7 @@ var LunchKanbanModel = KanbanModel.extend({
     init: function () {
         this.locationId = false;
         this.userId = false;
+        this._promInitLocation = null;
 
         this._super.apply(this, arguments);
     },
@@ -26,15 +27,23 @@ var LunchKanbanModel = KanbanModel.extend({
     getCurrentLocationId: function () {
         return this.locationId;
     },
+    /**
+     * @return {Promise} resolved with the location domain
+     */
+    getLocationDomain: function () {
+        var self = this;
+        return this._initUserLocation().then(function () {
+            return [self._buildLocationDomainLeaf()];
+        });
+    },
     load: function () {
         var self = this;
         var args = arguments;
         var _super = this._super;
 
-        return this._getUserLocation().then(function (locationId) {
+        return this._initUserLocation().then(function () {
             var params = args[0];
-            self.locationId = locationId;
-            self._addOrUpdate(params.domain, ['is_available_at', 'in', [locationId]]);
+            self._addOrUpdate(params.domain, self._buildLocationDomainLeaf());
 
             return _super.apply(self, args);
         });
@@ -42,7 +51,7 @@ var LunchKanbanModel = KanbanModel.extend({
     reload: function (id, options) {
         var domain = options && options.domain || this.localData[id].domain;
 
-        this._addOrUpdate(domain, ['is_available_at', 'in', [this.locationId]]);
+        this._addOrUpdate(domain, this._buildLocationDomainLeaf());
         options = _.extend(options, {domain: domain});
 
         return this._super.apply(this, arguments);
@@ -66,6 +75,18 @@ var LunchKanbanModel = KanbanModel.extend({
 
         return domain;
     },
+    /**
+     * Builds the domain leaf corresponding to the current user's location
+     *
+     * @private
+     * @return {Array}
+     */
+    _buildLocationDomainLeaf: function () {
+        var locationId = this.getCurrentLocationId();
+        if (locationId) {
+            return ['is_available_at', 'in', [locationId]];
+        }
+    },
     _getUserLocation: function () {
         return this._rpc({
             route: '/lunch/user_location_get',
@@ -74,18 +95,34 @@ var LunchKanbanModel = KanbanModel.extend({
             },
         });
     },
+    /**
+     * Gets the user location once.
+     * Can be triggered from anywhere
+     * Useful to inject the location domain in the search panel
+     *
+     * @private
+     * @return {Promise}
+     */
+    _initUserLocation: function () {
+        var self = this;
+        if (!this._promInitLocation) {
+            this._promInitLocation = new Promise(function (resolve) {
+                self._getUserLocation().then(function (locationId) {
+                    self.locationId = locationId;
+                    resolve();
+                });
+            });
+        }
+        return this._promInitLocation;
+    },
     _updateLocation: function (locationId) {
         this.locationId = locationId;
         return Promise.resolve();
     },
     _updateUser: function (userId) {
-        var self = this;
-
         this.userId = userId;
-
-        return this._getUserLocation().then(function (locationId) {
-            self.locationId = locationId;
-        });
+        this._promInitLocation = null;
+        return this._initUserLocation();
     }
 });
 
