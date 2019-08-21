7044f2116b5d5ffac835a162dfe74c1981821d7e
Thibault Delavallée <tde@odoo.com>
2019-08-21 09:11:38 +0000

    [IMP] website mail: remove useless public channel data
    PURPOSE
    
    Fix frontend rating and clean a bit module organization
    
    SPECIFICATIONS
    
    Remove useless mail.channel data as it makes noise without any added value.
    
    LINKS
    
    Task 2057301
    PR #35870

--- addons/website_mail/__manifest__.py
+++ addons/website_mail/__manifest__.py
@@ -13,7 +13,6 @@ It is responsible of comments moderation for published documents (forum, slides,
     'depends': ['website', 'mail'],
     'data': [
         'views/website_mail_templates.xml',
-        'data/mail_channel_data.xml',
         'security/website_mail_security.xml',
     ],
     'qweb': [
