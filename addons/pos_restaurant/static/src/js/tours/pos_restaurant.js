6ca7ed6ef1862e765fe4c400cbd76b9cbee8064f
Pierre Masereel <pim@odoo.com>
2019-08-22 14:39:31 +0200

    [FIX] pos_restaurant: remove verify_sync when opening table
    When we open a table, the option verify sync is called, and doesn't
    always work because of a race condition when we chack the status of
    connecting, and the icon that has already changed.
    
    As it doesn't really check anything because we check in python that the
    orders are synchronised, we removed it.

--- addons/pos_restaurant/static/src/js/tours/pos_restaurant.js
+++ addons/pos_restaurant/static/src/js/tours/pos_restaurant.js
@@ -30,12 +30,7 @@ odoo.define('pos_reataurant.tour.synchronized_table_management', function (requi
             run: function() {
                 var orders = $('.order-sequence');
                 if (orders.length === order_count) {
-                    var fail = orders.toArray().some(function(order){
-                        return !order.firstChild.data.includes('S-');
-                    });
-                    if (fail) {
-                        throw "sync failed";
-                    }
+                    return
                 } else {
                     throw "sync failed";
                 }
@@ -92,7 +87,6 @@ odoo.define('pos_reataurant.tour.synchronized_table_management', function (requi
             trigger: '.label:contains(' + table_id +')',
             run: 'click',
         }];
-        steps = steps.concat(verify_sync());
         if (order_count !== null){
             steps = steps.concat(verify_orders_synced(order_count));
         }
