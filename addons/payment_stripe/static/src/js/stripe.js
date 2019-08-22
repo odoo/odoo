31a706817a1081bb9c9b5554d3da4a7d4f8e0315
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2019-08-22 04:09:41 +0000

    [FIX] payment_stripe: backwards compatibility IE 11
    closes odoo/odoo#35944
    
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- addons/payment_stripe/static/src/js/stripe.js
+++ addons/payment_stripe/static/src/js/stripe.js
@@ -130,14 +130,14 @@ odoo.define('payment_stripe.stripe', function(require) {
             }).then(function (data) {
                 try { provider_form[0].innerHTML = data; } catch (e) {};
             });
-        } else if (window.location.href.includes("/my/orders/")) {
+        } else if (window.location.href.indexOf("/my/orders/") !== -1) {
             var create_tx = ajax.jsonRpc('/pay/sale/' + so_id + '/form_tx/', 'call', {
                 access_token: access_token,
                 acquirer_id: acquirer_id
             }).then(function (data) {
                 try { provider_form.innerHTML = data; } catch (e) {};
             });
-        } else if (window.location.href.includes("/my/invoices/")) {
+        } else if (window.location.href.indexOf("/my/invoices/") !== -1) {
             var create_tx = ajax.jsonRpc('/invoice/pay/' + invoice_id + '/form_tx/', 'call', {
                 access_token: access_token,
                 acquirer_id: acquirer_id
