5bf175f3bd46e643adaae7dc6d78f0efd20ce703
Kevin Baptiste <kba@odoo.com>
2019-08-22 07:33:20 +0000

    [IMP] hr_holidays: reintroduce out of office (back2basics)
    TaskID: 2057430
    
    closes odoo/odoo#35952
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_holidays/models/__init__.py
+++ addons/hr_holidays/models/__init__.py
@@ -7,6 +7,7 @@ from . import hr_department
 from . import hr_leave
 from . import hr_leave_allocation
 from . import hr_leave_type
+from . import mail_channel
 from . import mail_message_subtype
 from . import res_partner
 from . import res_users
