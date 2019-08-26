fcb678f290996e777e986e38d851ae76a5586d45
Thibault Delavallée <tde@odoo.com>
2019-08-26 13:23:38 +0000

    [FIX] payment: fix error management in payment form
    PURPOSE
    
    Test frontend and UI tools of eLearning.
    
    SPECIFICATIONS
    
    Recent commit f0cbeed41915502bcb36e15a0420a5a59c4f04b5 introduced a tool method to parse errors and ease
    their management. A variable typo was done by mistake, fixed in this commit.
    
    LINKS
    
    Task ID 1937768
    
    closes odoo/odoo#36085
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/payment_authorize/static/src/js/payment_form.js
+++ addons/payment_authorize/static/src/js/payment_form.js
@@ -78,7 +78,7 @@ PaymentForm.include({
                 self.displayError(
                     _t('Server Error'),
                     _t("We are not able to add your payment method at the moment.") +
-                        this._parseError(error)
+                        self._parseError(error)
                 );
             });
         };
