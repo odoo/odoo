2ca96230c27915fec27b5d9740e8a8843990939d
wan <wan@odoo.com>
2019-08-26 11:10:58 +0000

    [IMP] account: acrual expenses/revenues
    =======
    Purpose
    =======
    Accruals are revenues and expenses that are incurred during an accounting period for which no invoices or payments were received or made.
    
    => We suggest a wizard allowing recognizing the Expense/Revenue in a different period keeping tax return in the invoice period.
    
    Example:
    A vendor bill received in February which was for the December consumption.
    You should recognize the expense in December but the tax return is still due to February.
    
    1) Regular Vendor Bill in february
    Account          D          C
    600 ACHAT    10000
    411 TVA       2100
    440 FOUR                12100
    
    2) Action opening a wizard that allows specifying when we have to move the expense.
    - Targeted date, accrued account (expense, revenue, taxes, ...)
    - Action from account.move.line ?
    
    3) Wizard result
    a) The expense account is replaced by the accrued expense account in the original vendor bill journal entry (february)
    Compte           D          C
    444 FAE      10000
    411 TVA       2100
    440 FOUR                12100
    
    b) A new accrual journal entry is created for the original expense account in december
    Compte           D          C
    600 ACHAT    10000
    444 FAE                 10000
    
    Was task 2006617
    
    closes odoo/odoo#36053
    
    Signed-off-by: Quentin De Paoli (qdp) <qdp@openerp.com>

--- addons/account/wizard/__init__.py
+++ addons/account/wizard/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 
+from . import account_accrual_accounting
 from . import account_unreconcile
 from . import account_validate_account_move
 from . import pos_box
