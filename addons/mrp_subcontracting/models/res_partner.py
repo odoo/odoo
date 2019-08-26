fd6f3935ca2eab48544b99199cc4b090ec57ac23
Jinal Patel <jip@odoo.com>
2019-08-26 11:00:19 +0530

    [FIX] mrp_subcontracting: fix traceback while creating subcontractor
    In this commit, added value 'subcontractor' for selection
    field type of 'res.partner'.
    By this commit, traceback has been fixed.
    
    task-2058434

--- addons/mrp_subcontracting/models/res_partner.py
+++ addons/mrp_subcontracting/models/res_partner.py
@@ -7,6 +7,7 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    type = fields.Selection(selection_add=[('subcontractor', 'Subcontractor')])
     property_stock_subcontractor = fields.Many2one(
         'stock.location', string="Subcontractor Location", company_dependent=True,
         help="The stock location used as source and destination when sending\
