odoo/odoo

commit ee03fc37704b637dd0e34a5497b555318e616011

[FIX] sql_db: flush in savepoint, following odoo/odoo@9920f20e4c7753bc17bea71dea3a90f7de687196
closes odoo/odoo#35986

Signed-off-by: Raphael Collet (rco) <rco@openerp.com>

odoo/sql_db.py
@@ -413,10 +413,17 @@ class Cursor(object):
     def savepoint(self):
         """context manager entering in a new savepoint"""
         name = uuid.uuid1().hex
+        env = next((e for e in Environment.envs if e.cr is self), None)
+        if env is not None:
+            env['base'].flush()
         self.execute('SAVEPOINT "%s"' % name)
         try:
             yield
+            if env is not None:
+                env['base'].flush()
         except Exception:
+            if env is not None:
+                env.clear()
             self.execute('ROLLBACK TO SAVEPOINT "%s"' % name)
             raise
         else:

commit 8b494e5ceb6bcc237b3cf05e4f83ced34b2d8b0a

[FIX] sql_db: revert ee03fc37704
ee03fc37704 introduced a non deterministic bug by choosing an
environment in a set, so the environment is not always the good one.

odoo/sql_db.py
@@ -413,17 +413,10 @@ class Cursor(object):
     def savepoint(self):
         """context manager entering in a new savepoint"""
         name = uuid.uuid1().hex
-        env = next((e for e in Environment.envs if e.cr is self), None)
-        if env is not None:
-            env['base'].flush()
         self.execute('SAVEPOINT "%s"' % name)
         try:
             yield
-            if env is not None:
-                env['base'].flush()
         except Exception:
-            if env is not None:
-                env.clear()
             self.execute('ROLLBACK TO SAVEPOINT "%s"' % name)
             raise
         else: