906480093a75e54ddf7d212b61c1d10d7f9c55ca
Aaron Bohy <aab@odoo.com>
2019-08-27 08:42:37 +0000

    [FIX] web: 'create' in context and non inline subviews
    Let's assume an action with 'create': 0 in the context, and a non
    inline subview in the form view. Before this rev., the 'create': 0
    was applied to both the form view and the subview.
    
    This rev. applies the same logic as what the server does for inline
    subviews: the rule is applied to the subview iff its model is the
    same as the one of the form view (with key 'base_model_name' set
    in the context).
    
    Task 2035653
    
    closes odoo/odoo#36109
    
    Signed-off-by: Géry Debongnie (ged) <ged@openerp.com>

--- addons/web/static/src/js/views/form/form_view.js
+++ addons/web/static/src/js/views/form/form_view.js
@@ -102,6 +102,10 @@ var FormView = BasicView.extend({
                     var refinedContext = _.pick(self.loadParams.context, function (value, key) {
                         return key.indexOf('_view_ref') === -1;
                     });
+                    // Specify the main model to prevent access rights defined in the context
+                    // (e.g. create: 0) to apply to subviews. We use here the same logic as
+                    // the one applied by the server for inline views.
+                    refinedContext.base_model_name = self.controllerParams.modelName;
                     defs.push(parent.loadViews(
                             field.relation,
                             new Context(context, self.userContext, refinedContext).eval(),
