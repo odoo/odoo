3d6e91789968719c726420554d25b25f81282a55
Nans Lefebvre <len@odoo.com>
2019-08-27 06:42:16 +0000

    [FIX] stock: error at application re-install
    Fine-tuning of 1da058f8edd, for the same use-case.
    The _valid_barcode method is here to bypass the barcode_company_uniq constraint.
    The constraint is put on barcode/company couples;
    however if checked without a given 'company_id' in the values,
    which ends up as False, then the check is actually not the correct one:
    the company_id is going to be filled later by the default_get.
    
    Instead we use the value given by the default_get, consistently with the create.
    
    The same error could affect _create_missing_locations,
    since it gives an explicit value to company_id that was not passed in values.
    We give it to make sure that the company is consistent there to.
    
    opw 2058888
    
    closes odoo/odoo#36102
    
    Signed-off-by: Nans Lefebvre (len) <len@odoo.com>

--- addons/stock/models/stock_warehouse.py
+++ addons/stock/models/stock_warehouse.py
@@ -441,7 +441,7 @@ class Warehouse(models.Model):
         delivery_steps = vals.get('delivery_steps', def_values['delivery_steps'])
         code = vals.get('code') or self.code
         code = code.replace(' ', '').upper()
-        company_id = vals.get('company_id', self.company_id.id)
+        company_id = vals.get('company_id', self.default_get(['company_id'])['company_id'])
         sub_locations = {
             'lot_stock_id': {
                 'name': _('Stock'),
@@ -490,12 +490,14 @@ class Warehouse(models.Model):
         avoid mistakes during picking types and rules creation.
         """
         for warehouse in self:
+            company_id = vals.get('company_id', warehouse.company_id.id)
+            vals.update(company_id=company_id)
             sub_locations = warehouse._get_locations_values(vals)
             missing_location = {}
             for location, location_values in sub_locations.items():
                 if not warehouse[location] and location not in vals:
                     location_values['location_id'] = vals.get('view_location_id', warehouse.view_location_id.id)
-                    location_values['company_id'] = vals.get('company_id', warehouse.company_id.id)
+                    location_values['company_id'] = company_id
                     missing_location[location] = self.env['stock.location'].create(location_values).id
             if missing_location:
                 warehouse.write(missing_location)
