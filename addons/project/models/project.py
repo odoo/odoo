d7b56e9f4e9ac2efc1f9c18e40ea652a4881d11f
Geoffroy Larue <gla@odoo.com>
2019-08-29 12:32:55 +0200

    [IMP] project: project form view
    Move and relabel the Privacy field and its various options to improve usability.
    
    Task-2047389      Closes #36360

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -195,16 +195,16 @@ class Project(models.Model):
         help="Internal email associated with this project. Incoming emails are automatically synchronized "
              "with Tasks (or optionally Issues if the Issue Tracker module is installed).")
     privacy_visibility = fields.Selection([
-            ('followers', 'Visible by invited employees'),
-            ('employees', 'Visible by employees'),
-            ('portal', 'Visible by portal users and employees'),
+            ('followers', 'Invited employees'),
+            ('employees', 'All employees'),
+            ('portal', 'Portal users and all employees'),
         ],
-        string='Privacy', required=True,
+        string='Visibility', required=True,
         default='portal',
         help="Defines the visibility of the tasks of the project:\n"
-                "- Visible by invited employees: employees may only see the followed project and tasks.\n"
-                "- Visible by employees: employees may see all project and tasks.\n"
-                "- Visible by portal users and employees: employees may see everything."
+                "- Invited employees: employees may only see the followed project and tasks.\n"
+                "- All employees: employees may see all project and tasks.\n"
+                "- Portal users and all employees: employees may see everything."
                 "   Portal users may see project and tasks followed by.\n"
                 "   them or by someone of their company.")
     doc_count = fields.Integer(compute='_compute_attached_docs_count', string="Number of documents attached")
