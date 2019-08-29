3b3d35e8b91fb8881be848665349828c64642e03
Adrien Dieudonne <adr@odoo.com>
2019-08-29 12:10:07 +0000

    [IMP] web: SearchView: Allow user to quickly search on mobile
    Currently the search panel is nice for a search with filters but
    that was not 100% efficient for simple research without filters.
    
    The Search is now integrated in the form view, with the ability
    to open the Search panel to apply filters or group by.
    
    Task ID : 1924760

--- addons/web/static/tests/views/mobile_control_panel_tests.js
+++ addons/web/static/tests/views/mobile_control_panel_tests.js
@@ -39,8 +39,61 @@ QUnit.module('Mobile Search view Screen', {
         };
     },
 }, function () {
-    QUnit.test('can activate a filter with mobile search view', async function (assert) {
-        assert.expect(3);
+    QUnit.test('can activate a filter with mobile search view in quick search mode', async function (assert) {
+        assert.expect(7);
+
+        var searchRPCFlag = false;
+
+        var actionManager = await createActionManager({
+            actions: this.actions,
+            archs: this.archs,
+            data: this.data,
+            mockRPC: function (route, args) {
+                if (searchRPCFlag) {
+                    assert.deepEqual(args.domain, [['foo', 'ilike', 'A']],
+                        "domain should have been properly transferred to list view");
+                }
+                return this._super.apply(this, arguments);
+            },
+        });
+
+        await actionManager.doAction(1);
+
+        assert.strictEqual($('button.o_enable_searchview.fa-search').length, 1,
+            "should display a button to open the searchview");
+        assert.deepEqual($('.o_searchview_input_container:visible').length, 0,
+            "Quick search input should be hidden");
+
+        // open the search view
+        await testUtils.dom.click($('button.o_enable_searchview'));
+        assert.deepEqual($('.o_toggle_searchview_full:visible').length, 1,
+            "should display a button to expand the searchview");
+        assert.deepEqual($('.o_searchview_input_container:visible').length, 1,
+            "Quick search input should now be visible");
+
+        searchRPCFlag = true;
+
+        // use quick search input
+        $('.o_searchview_input')
+            .val("A")
+            .trigger($.Event('keypress', { which: 65, keyCode: 65 }));
+        await testUtils.nextTick();
+        actionManager.$('.o_searchview_input')
+            .trigger($.Event('keydown', { which: $.ui.keyCode.ENTER, keyCode: $.ui.keyCode.ENTER }));
+        await testUtils.nextTick();
+
+        // close quick search
+        await testUtils.dom.click($('button.o_enable_searchview.fa-close'));
+        assert.deepEqual($('.o_toggle_searchview_full:visible').length, 0,
+            "Expand icon shoud be hidden");
+        assert.deepEqual($('.o_searchview_input_container:visible').length, 0,
+            "Quick search input should be hidden");
+
+        actionManager.destroy();
+    });
+
+    QUnit.test('can activate a filter with mobile search view in full screen mode', async function (assert) {
+        assert.expect(4);
 
         var filterActiveFlag = false;
 
@@ -61,19 +114,24 @@ QUnit.module('Mobile Search view Screen', {
 
         assert.ok(!$('.o_mobile_search').is(':visible'),
             'mobile search view is not visible');
+
         // open the search view
         await testUtils.dom.click($('button.o_enable_searchview'));
-
+        // open it in full screen
+        await testUtils.dom.click($('.o_toggle_searchview_full'));
         assert.ok($('.o_mobile_search').is(':visible'),
             'mobile search view is visible');
 
         // open filter sub menu
         await testUtils.dom.click($('button.o_dropdown_toggler_btn').first());
-
         filterActiveFlag = true;
-
         // click on Active filter
         await testUtils.dom.click($('.o_filters_menu a:contains(Active)'));
+
+        // closing search view
+        await testUtils.dom.click($('.o_mobile_search_close'));
+        assert.ok(!$('.o_mobile_search').is(':visible'),
+            'mobile search view is not visible');
 
         actionManager.destroy();
     });
