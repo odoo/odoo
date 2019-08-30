7b1093d9de79de5ca3a43b8107a9570476f1e521
Thibault Delavallée <tde@odoo.com>
2019-08-30 15:56:53 +0200

    [FIX] (website_)rating: synchronize rating feedback with message body
    Purpose is to set review message on both message and rating so that reviews
    are correctly displayed in eLearning backend as well as correctly displayed
    in chatter frontend.
    
    Task ID 2058595
    PR #36281

--- addons/website_rating/controllers/portal_chatter.py
+++ addons/website_rating/controllers/portal_chatter.py
@@ -11,8 +11,14 @@ class PortalChatter(PortalChatter):
 
     def _portal_post_filter_params(self):
         fields = super(PortalChatter, self)._portal_post_filter_params()
-        fields.append('rating_value')
+        fields += ['rating_value', 'rating_feedback']
         return fields
+
+    @http.route()
+    def portal_chatter_post(self, res_model, res_id, message, redirect=None, attachment_ids='', attachment_tokens='', **kwargs):
+        if kwargs.get('rating_value'):
+            kwargs['rating_feedback'] = kwargs.pop('rating_feedback', message)
+        return super(PortalChatter, self).portal_chatter_post(res_model, res_id, message, redirect=redirect, attachment_ids=attachment_ids, attachment_tokens=attachment_tokens, **kwargs)
 
     @http.route()
     def portal_chatter_init(self, res_model, res_id, domain=False, limit=False, **kwargs):
