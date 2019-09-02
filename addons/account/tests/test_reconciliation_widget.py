f4ffaafad524e34b10f70ac77a6a57d7a451c944
wan <wan@odoo.com>
2019-09-02 12:35:27 +0200

    [FIX] account: prevent multiple event concurency * When clicking on the partner m2o selection field before having clicked on the bank statement line, 2 events occured:   * the bank statement line expanded   * the partner selection showed
      This could lead to unwanted behavior as the renderer could update
      -> Stop the propagation of a click on .o_field_widget to prevent
      flickering and possible update of the statement line widget
    
    * Always recompute the create line as sometimes the value is not correct
      when removing a proposition

--- addons/account/tests/test_reconciliation_widget.py
+++ addons/account/tests/test_reconciliation_widget.py
@@ -7,7 +7,7 @@ from odoo.addons.account.tests.test_reconciliation import TestReconciliation
 _logger = logging.getLogger(__name__)
 
 
-@odoo.tests.tagged('post_install', '-at_install',  '-standard', 'to_fix')
+@odoo.tests.tagged('post_install', '-at_install')
 @odoo.tests.common.at_install(False)
 @odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
@@ -20,7 +20,7 @@ class TestUi(odoo.tests.HttpCase):
 
         # To be able to test reconciliation, admin user must have access to accounting features, so we give him the right group for that
         self.env.ref('base.user_admin').write({'groups_id': [(4, self.env.ref('account.group_account_user').id)]})
-        
+
         payload = {'action':'bank_statement_reconciliation_view', 'statement_line_ids[]': bank_stmt_line.ids}
         prep = requests.models.PreparedRequest()
         prep.prepare_url(url="http://localhost/web#", params=payload)
