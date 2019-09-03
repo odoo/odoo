d18a38c29596ce4c896f0b6b44c50a757b939594
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-09-03 15:22:11 +0200

    Extract methods from StockQuant._update_reserved_quantity()
    In order to allow more advanced behaviours from extension addons.
    For instance, an addon to reserve full packaging (e.g. a pallet) before
    considering the removal strategy
    (https://github.com/OCA/stock-logistics-warehouse/pull/695).
    
    As this special strategy is based on the ordered quantity, it cannot be added to
    the _gather() or _get_removal_strategy() methods which do not know the
    quantity. The reservation happens in an override of _update_reserved_quantity(),
    and many lines of code have to be duplicated. Extracting these 2 methods
    will allow to reduce duplication.

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -401,6 +401,61 @@ class StockQuant(models.Model):
             })
         return self._get_available_quantity(product_id, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=False, allow_negative=True), fields.Datetime.from_string(in_date)
 
+    def _update_reserved_get_available(
+        self,
+        product_id,
+        location_id,
+        quantity,
+        lot_id=None,
+        package_id=None,
+        owner_id=None,
+        strict=False,
+    ):
+        """Get available quants when updating reservation for product"""
+        rounding = product_id.uom_id.rounding
+        available_quantity = 0.
+        if float_compare(quantity, 0, precision_rounding=rounding) > 0:
+            # if we want to reserve
+            available_quantity = self._get_available_quantity(product_id, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
+            if float_compare(quantity, available_quantity, precision_rounding=rounding) > 0:
+                raise UserError(_('It is not possible to reserve more products of %s than you have in stock.') % product_id.display_name)
+        elif float_compare(quantity, 0, precision_rounding=rounding) < 0:
+            # if we want to unreserve
+            available_quantity = sum(self.mapped('reserved_quantity'))
+            if float_compare(abs(quantity), available_quantity, precision_rounding=rounding) > 0:
+                raise UserError(_('It is not possible to unreserve more products of %s than you have in stock.') % product_id.display_name)
+        return available_quantity
+
+    def _reserve_quant(self, quantity, available_quantity):
+        """Reserve a quantity on a quant.
+
+        Return the adjusted quantity, the still available quantity after
+        the reservation occurred and the reserved quantity for this quant.
+
+        :param quantity: the total quantity required for a move, maybe not
+        entirely reserved by the current quant
+        :param available_quantity: total available quantity for the product
+        (across all quants)
+        """
+        rounding = self.product_id.uom_id.rounding
+        reserved = 0.
+        if float_compare(quantity, 0, precision_rounding=rounding) > 0:
+            max_quantity_on_quant = self.quantity - self.reserved_quantity
+            if float_compare(max_quantity_on_quant, 0, precision_rounding=rounding) <= 0:
+                return quantity, available_quantity, 0
+            max_quantity_on_quant = min(max_quantity_on_quant, quantity)
+            self.reserved_quantity += max_quantity_on_quant
+            reserved = max_quantity_on_quant
+            quantity -= max_quantity_on_quant
+            available_quantity -= max_quantity_on_quant
+        else:
+            max_quantity_on_quant = min(self.reserved_quantity, abs(quantity))
+            self.reserved_quantity -= max_quantity_on_quant
+            reserved = -max_quantity_on_quant
+            quantity += max_quantity_on_quant
+            available_quantity += max_quantity_on_quant
+        return quantity, available_quantity, reserved
+
     @api.model
     def _update_reserved_quantity(self, product_id, location_id, quantity, lot_id=None, package_id=None, owner_id=None, strict=False):
         """ Increase the reserved quantity, i.e. increase `reserved_quantity` for the set of quants
@@ -417,37 +472,27 @@ class StockQuant(models.Model):
         self = self.sudo()
         rounding = product_id.uom_id.rounding
         quants = self._gather(product_id, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
+
         reserved_quants = []
 
-        if float_compare(quantity, 0, precision_rounding=rounding) > 0:
-            # if we want to reserve
-            available_quantity = self._get_available_quantity(product_id, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
-            if float_compare(quantity, available_quantity, precision_rounding=rounding) > 0:
-                raise UserError(_('It is not possible to reserve more products of %s than you have in stock.') % product_id.display_name)
-        elif float_compare(quantity, 0, precision_rounding=rounding) < 0:
-            # if we want to unreserve
-            available_quantity = sum(quants.mapped('reserved_quantity'))
-            if float_compare(abs(quantity), available_quantity, precision_rounding=rounding) > 0:
-                raise UserError(_('It is not possible to unreserve more products of %s than you have in stock.') % product_id.display_name)
-        else:
+        available_quantity = quants._update_reserved_get_available(
+            product_id,
+            location_id,
+            quantity,
+            lot_id=lot_id,
+            package_id=package_id,
+            owner_id=owner_id,
+            strict=strict,
+        )
+        if not available_quantity:
             return reserved_quants
 
         for quant in quants:
-            if float_compare(quantity, 0, precision_rounding=rounding) > 0:
-                max_quantity_on_quant = quant.quantity - quant.reserved_quantity
-                if float_compare(max_quantity_on_quant, 0, precision_rounding=rounding) <= 0:
-                    continue
-                max_quantity_on_quant = min(max_quantity_on_quant, quantity)
-                quant.reserved_quantity += max_quantity_on_quant
-                reserved_quants.append((quant, max_quantity_on_quant))
-                quantity -= max_quantity_on_quant
-                available_quantity -= max_quantity_on_quant
-            else:
-                max_quantity_on_quant = min(quant.reserved_quantity, abs(quantity))
-                quant.reserved_quantity -= max_quantity_on_quant
-                reserved_quants.append((quant, -max_quantity_on_quant))
-                quantity += max_quantity_on_quant
-                available_quantity += max_quantity_on_quant
+            (quantity, available_quantity, reserved) = quant._reserve_quant(
+                quantity, available_quantity,
+            )
+            if reserved:
+                reserved_quants.append((quant, reserved))
 
             if float_is_zero(quantity, precision_rounding=rounding) or float_is_zero(available_quantity, precision_rounding=rounding):
                 break
