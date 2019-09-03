c4eba6f5b150a0e881b7fb9b935ef1a0c3154bbf
laa <laa@odoo.com>
2019-09-03 07:09:11 +0000

    [FIX] website_slides: correctly set sequence of newly added content
    Purpose of this commit is to add new content at last position in frontend.
    Indeed backend uses last position in embedded list view, frontend should
    do the same. Moreover when having a lot of content, having everything at
    the bottom of the screen at the same position of Add content / section
    buttons makes sens.
    
    Specifications
    
      * when adding a new category: add it as last category;
      * when adding a new slide in a category: add it as last slide of the
        category;
    
    Specific case of slide without category
    
      * when adding a new slide without category: add it as last slide of the
        channel (display it as last) but keep it on top in backend view
        (display as first);
    
    Improve content list display in frontend of eLearning
    
      * contents without section are now at the bottom;
      * fixed alignment of buttons icon (Add Content and Add Section)
      * fixed broken design because of title too long
    
    Task 2049640 (website slides back2basics and fiximp)
    Task 2061672 (new content bug report)
    
    Co-Authored-By: Michaël Mattiello <mcm@odoo.com>
    Co-Authored-By: Alexis Lacroix <laa@odoo.com>

--- addons/website_slides/models/slide_channel.py
+++ addons/website_slides/models/slide_channel.py
@@ -523,16 +523,7 @@ class Channel(models.Model):
         all_slides = self.env['slide.slide'].sudo().search(base_domain, order=order)
         category_data = []
 
-        # First add uncategorized slides
-        uncategorized_slides = all_slides.filtered(lambda slide: not slide.category_id)
-        if uncategorized_slides or force_void:
-            category_data.append({
-                'category': False, 'id': False,
-                'name': _('Uncategorized'), 'slug_name': _('Uncategorized'),
-                'total_slides': len(uncategorized_slides),
-                'slides': uncategorized_slides[(offset or 0):(offset + limit or len(uncategorized_slides))],
-            })
-        # Then all categories by natural order
+        # First add all categories by natural order
         for category in all_categories:
             category_slides = all_slides.filtered(lambda slide: slide.category_id == category)
             if not category_slides and not force_void:
@@ -542,6 +533,15 @@ class Channel(models.Model):
                 'name': category.name, 'slug_name': slug(category),
                 'total_slides': len(category_slides),
                 'slides': category_slides[(offset or 0):(limit + offset or len(category_slides))],
+            })
+        # Then add uncategorized slides
+        uncategorized_slides = all_slides.filtered(lambda slide: not slide.category_id)
+        if uncategorized_slides or force_void:
+            category_data.append({
+                'category': False, 'id': False,
+                'name': _('Uncategorized'), 'slug_name': _('Uncategorized'),
+                'total_slides': len(uncategorized_slides),
+                'slides': uncategorized_slides[(offset or 0):(offset + limit or len(uncategorized_slides))],
             })
         return category_data
 
