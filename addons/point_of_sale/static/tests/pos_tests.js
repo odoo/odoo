1a570feb9ecab41b1a6172696aa57d9d8ca58858
Pierre Paridans <app@odoo.com>
2019-09-05 12:02:25 +0200

    [WIP] point_of_sale: tests

--- addons/point_of_sale/static/tests/pos_tests.js
+++ addons/point_of_sale/static/tests/pos_tests.js
@@ -2,7 +2,8 @@ odoo.define("point_of_sale.PointOfSaleTests", function (require) {
 "use strict";
 
 const { dom } = require("web.test_utils");
-const { createPointOfSale, isPointOfSaleLoaded, loadPointOfSale } = require("point_of_sale.test_utils");
+const { createPointOfSale, isPointOfSaleLoaded, loadPointOfSale, getPointOfSaleInstance } = require("point_of_sale.test_utils");
+const { Orderline, Product } = require("point_of_sale.models");
 
 QUnit.module("Point of Sale");
 
@@ -37,8 +38,12 @@ QUnit.module("pos.ui", {
                 records: [],
             },
             "uom.uom": {
-                fields: {},
-                records: [],
+                fields: {
+                    name: { string: "Name", type: "char" },
+                },
+                records: [
+                    { id: 1, name: "Units" },
+                ],
             },
             "res.partner": {
                 fields: {},
@@ -79,13 +84,17 @@ QUnit.module("pos.ui", {
                     pricelist_id: { string: "Default pricelist", type: "many2one", relation: "product.pricelist" },
                     currency_id: { string: "Currency", type: "many2one", relation: "res.currency" },
                     company_id: { string: "Company", type: "many2one", relation: "res.company" },
+                    available_pricelist_ids: { string: "Available Pricelists", type: "many2many", relation: "product.pricelist"},
                 },
                 records: [
-                    { id: 1, pricelist_id: 1, currency_id: 1, company_id: 1 },
+                    { id: 1, pricelist_id: 1, currency_id: 1, company_id: 1, available_pricelist_ids: [1] },
                 ],
             },
             "product.pricelist": {
-                fields: {},
+                fields: {
+                    discount_policy: {string: "Discount Policy", type: "selection"},
+                    items: { string: "Items", type: "many2one", relation: "product.pricelist.item" },
+                },
                 records: [
                     { id: 1 },
                 ],
@@ -108,9 +117,35 @@ QUnit.module("pos.ui", {
                 fields: {},
                 records: [],
             },
-            "product.product": {
+            "product.template": {
                 fields: {},
                 records: [],
+            },
+            "product.product": {
+                fields: {
+                    name: { string: "Name", type: "char" },
+                    taxes_id: { string: "Taxes", type: "many2many", relation: "account.tax" },
+                    sale_ok: { string: "Can be Sold", type: "boolean", default: true },
+                    available_in_pos: { string: "Available in POS", type: "boolean" },
+                    company_id: { string: "Company", type: "many2one", relation: "res.company" },
+                    lst_price: { string: "Public Price", type: "float" },
+                    standard_price: { string: "Cost", type: "float" },
+                    categ_id: { string: "Product Category", type: "many2one", relation: "product.category" },
+                    pos_categ_id: { string: "POS Category", type: "many2one", relation: "pos.category" },
+                    barcode: { string: "Barcode", type: "char" },
+                    default_code: { string: "Internal Reference", type: "char" },
+                    to_weight: { string: "To Weight with Scale", type: "boolean" },
+                    uom_id: { string: "Unit of Measure", type: "many2one", relation: "uom.uom" },
+                    description_sale: { string: "Sales Description", type: "char" },
+                    description: { string: "Description", type: "char" },
+                    product_tmpl_id: { string: "Product Template", type: "many2one", relation: "product.template" },
+                    tracking: { string: "Tracking", type: "selection", default: "none" },
+                },
+                records: [
+                    { id: 1, name: "Chair", available_in_pos: true, company_id: 1, uom_id: 1 },
+                    { id: 2, name: "Table", available_in_pos: true, company_id: 1, uom_id: 1 },
+                    { id: 3, name: "Pencil", company_id: 1, uom_id: 1 },
+                ],
             },
             "account.bank.statement": {
                 fields: {},
@@ -215,6 +250,207 @@ QUnit.module("pos.ui", {
             await dom.click(pos.$(orderSelector).find(".deleteorder-button"));
             assert.containsOnce(pos.$(orderSelector), ".orders > .order-button",
                 "should have 1 order when clicking on delete order button");
+
+            pos.destroy();
+        });
+    });
+
+    QUnit.module("ProductScreenWidget", function () {
+        QUnit.test("basic rendering", async function (assert) {
+            assert.expect(7);
+
+            const pos = await loadPointOfSale({
+                data: this.data,
+                session: this.session,
+            });
+
+            const leftPanel = ".product-screen .leftpane";
+
+            assert.containsOnce(pos, leftPanel,
+                "should have a left panel");
+            assert.containsOnce(pos.$(leftPanel), ".order",
+                "left panel should have an Order");
+            assert.containsOnce(pos.$(leftPanel), ".actionpad",
+                "left panel should have an Actionpad");
+            assert.containsOnce(pos.$(leftPanel), ".numpad",
+                "left panel should have a Numpad");
+
+            const rightPanel = ".product-screen .rightpane";
+            assert.containsOnce(pos, rightPanel,
+                "should have a right panel");
+            assert.containsOnce(pos.$(rightPanel), ".rightpane-header",
+                "should have ProductCategories header");
+            assert.containsOnce(pos.$(rightPanel), ".product-list",
+                "should have a ProductList");
+
+            pos.destroy();
+        });
+    });
+
+    QUnit.module("OrderWidget", function () {
+        QUnit.test("basic rendering", async function (assert) {
+            assert.expect(2);
+
+            const pos = await loadPointOfSale({
+                data: this.data,
+                session: this.session,
+            });
+
+            const instance = getPointOfSaleInstance(pos);
+
+            const order = instance.pos.get_order();
+            const products = instance.pos.db.get_product_by_category(0);
+            const orderline1 = new Orderline({}, {pos: instance.pos, order, product: new Product({}, products[0])});
+            order.add_orderline(orderline1);
+            const orderline2 = new Orderline({}, {pos: instance.pos, order, product: new Product({}, products[1])});
+            order.add_orderline(orderline2);
+
+            const orderContainer = ".product-screen .order-container";
+
+            assert.containsOnce(pos.$(orderContainer), ".orderlines",
+                "should have an orderline list");
+            assert.containsN(pos.$(orderContainer), ".orderlines .orderline", 2,
+                "should have 2 orderlines");
+
+            pos.destroy();
+        });
+
+        QUnit.test("empty order", async function (assert) {
+            assert.expect(3);
+
+            const pos = await loadPointOfSale({
+                data: this.data,
+                session: this.session,
+            });
+
+            const orderContainer = ".product-screen .order-container";
+
+            assert.containsNone(pos.$(orderContainer), ".orderlines",
+                "shouldn't have any orderline list");
+            assert.containsOnce(pos.$(orderContainer), ".order-empty",
+                "should have an empty order splashscreen");
+            assert.strictEqual(pos.$(orderContainer).find(".order-empty").text().trim(), "Your shopping cart is empty");
+
+            pos.destroy();
+        });
+    });
+
+    QUnit.module("ActionpadWidget", function () {
+        QUnit.test("basic rendering", async function (assert) {
+            assert.expect(3);
+
+            const pos = await loadPointOfSale({
+                data: this.data,
+                session: this.session,
+            });
+
+            const actionpadContainer = ".product-screen .actionpad";
+
+            assert.containsOnce(pos, actionpadContainer,
+                "should have an Actionpad");
+            assert.containsOnce(pos.$(actionpadContainer), ".set-customer",
+                "should have a button to choose customer");
+            assert.containsOnce(pos.$(actionpadContainer), ".pay",
+                "should have a button to process payment");
+
+            pos.destroy();
+        });
+    });
+
+    QUnit.module("NumpadWidget", function () {
+        QUnit.test("basic rendering", async function (assert) {
+            assert.expect(8);
+
+            const pos = await loadPointOfSale({
+                data: this.data,
+                session: this.session,
+            });
+
+            const numpadContainer = ".product-screen .numpad";
+
+            assert.containsOnce(pos, numpadContainer,
+                "should have a Numpad");
+            assert.containsN(pos.$(numpadContainer), ".input-button.number-char", 11,
+                "shoud have 11 'numeric' buttons (including the decimal mark)");
+            assert.containsN(pos.$(numpadContainer), ".mode-button", 3,
+                "should have 3 'mode' buttons");
+            assert.containsOnce(pos.$(numpadContainer), ".mode-button[data-mode='quantity']",
+                "should have a button to change quantity");
+            assert.containsOnce(pos.$(numpadContainer), ".mode-button[data-mode='discount']",
+                "should have a button to change discount");
+            assert.containsOnce(pos.$(numpadContainer), ".mode-button[data-mode='price']",
+                "should have a button to change price");
+            assert.containsOnce(pos.$(numpadContainer), ".input-button.numpad-minus",
+                "shoud have a 'sign' button");
+            assert.containsOnce(pos.$(numpadContainer), ".input-button.numpad-backspace",
+                "shoud have a 'backspace' button");
+
+            pos.destroy();
+        });
+    });
+
+    QUnit.module("ProductCategoriesWidget", function () {
+        QUnit.test("basic rendering", async function (assert) {
+            assert.expect(5);
+
+            const pos = await loadPointOfSale({
+                data: this.data,
+                session: this.session,
+            });
+
+            const headerContainer = ".product-screen .rightpane-header";
+
+            assert.containsOnce(pos, headerContainer,
+                "should have ProductCategories");
+            assert.containsOnce(pos.$(headerContainer), ".breadcrumbs",
+                "should have a categories breadcrumb");
+            assert.containsOnce(pos.$(headerContainer), ".breadcrumbs .breadcrumb-button.breadcrumb-home",
+                "should have a 'home' categories' button");
+            assert.containsOnce(pos.$(headerContainer), ".searchbox",
+                "should have a categories search field");
+            assert.containsOnce(pos.$(headerContainer), ".searchbox .search-clear.right",
+                "should have a clear search field button");
+
+            pos.destroy();
+        });
+    });
+
+    QUnit.module("ProductListWidget", function () {
+        QUnit.test("basic rendering", async function (assert) {
+            assert.expect(2);
+
+            const pos = await loadPointOfSale({
+                data: this.data,
+                session: this.session,
+            });
+
+            const listContainer = ".product-screen .product-list-container";
+
+            assert.containsOnce(pos.$(listContainer), ".product-list",
+                "should have a products list");
+            assert.containsN(pos.$(listContainer), ".product-list .product", 2,
+                "should have 2 products listed");
+
+            pos.destroy();
+        });
+
+        QUnit.test("empty product list", async function (assert) {
+            assert.expect(3);
+
+            this.data["product.product"].records = [];
+
+            const pos = await loadPointOfSale({
+                data: this.data,
+                session: this.session,
+            });
+
+            const listContainer = ".product-screen .product-list-container";
+
+            assert.containsNone(pos.$(listContainer), ".product-list",
+                "shouldn't have any products list");
+            assert.containsOnce(pos.$(listContainer), ".product-list-empty",
+                "should have an empty product list splashscreen");
+            assert.strictEqual(pos.$(listContainer).find(".product-list-empty").text().trim(), "There are no products in this category.");
 
             pos.destroy();
         });
