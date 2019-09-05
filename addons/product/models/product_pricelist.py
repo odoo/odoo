e7af1a8c7e5d78a5d32dcaabcf1e507cf01cb704
Nans Lefebvre <len@odoo.com>
2019-09-05 10:54:23 +0200

    [FIX] product: pricelists with temporary discounts
    Create a pricelist with a temporary discount on some product only for today.
    Go to the shop. The discount is applied on the product.
    Add the product to the cart.
    Go to the cart page (review order): the price does not use the discount.
    
    Because the query getting the pricelist item is some handwritten SQL,
    it only works for dates, and not e.g. in the case of datetimes.
    So the pricelist item encoding the discount is not retrieved, and thus not used.
    
    It turns out e.g. in _website_product_id_change it gives a datetime in context,
    explaining why sometimes the discount is applied, and sometimes not.
    
    We use fields.Date.to_date as it should work on all date formats we shall pass.
    
    Co-written with @sbbaro
    
    opw 2054881

--- addons/product/models/product_pricelist.py
+++ addons/product/models/product_pricelist.py
@@ -109,6 +109,7 @@ class Pricelist(models.Model):
         self.ensure_one()
         if not date:
             date = self._context.get('date') or fields.Date.context_today(self)
+        date = fields.Date.to_date(date)  # handwritten query below does not work with non-dates (e.g. datetimes)
         if not uom_id and self._context.get('uom'):
             uom_id = self._context['uom']
         if uom_id:
