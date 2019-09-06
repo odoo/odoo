a523918e67273e1192f15b149c74a3e6a976f357
Damien Bouvy <dbo@odoo.com>
2019-09-06 13:58:13 +0000

    [ADD] payment_stripe_sca: Stripe API changes following PSD2
    With the PSD2 directive coming into effect on September 14th,
    was had no choice but to switch our Stripe integration to use
    newer APIs that support Strong Customer Authentication (SCA for short).
    
    This meant switching from the old stripe.js implementation for
    'redirection' flow to the Stripe Checkout API and from the
    Charge API to the newer SetupIntent API for s2s flows.
    
    This new modules introduce these changes in a non-intrusive way,
    this means that installing this module is *not* required for
    all customers (e.g. instances only accepting payment to and
    from countries that are not part of the EU), that installing it
    will support SCA flows and that uninstallation leaves you with
    a working instance as well. Essentially a backport of 9f5f2fc
    in a seperate module.
    
    Task 2039100

---
+++ addons/payment_stripe_sca/controllers/main.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
+import pprint
+import werkzeug
+
+from odoo.http import route, request
+from odoo.addons.payment_stripe.controllers.main import StripeController
+
+_logger = logging.getLogger(__name__)
+
+
+class StripeControllerSCA(StripeController):
+    _success_url = '/payment/stripe/success'
+    _cancel_url = '/payment/stripe/cancel'
+
+    @route(['/payment/stripe/success', '/payment/stripe/cancel'], type='http', auth='public')
+    def stripe_success(self, **kwargs):
+        request.env['payment.transaction'].sudo().form_feedback(kwargs, 'stripe')
+        return werkzeug.utils.redirect('/payment/process')
+
+    @route(['/payment/stripe/s2s/create_json_3ds'], type='json', auth='public', csrf=False)
+    def stripe_s2s_create_json_3ds(self, verify_validity=False, **kwargs):
+        if not kwargs.get('partner_id'):
+            kwargs = dict(kwargs, partner_id=request.env.user.partner_id.id)
+        token = request.env['payment.acquirer'].browse(int(kwargs.get('acquirer_id'))).s2s_process(kwargs)
+
+        if not token:
+            res = {
+                'result': False,
+            }
+            return res
+
+        res = {
+            'result': True,
+            'id': token.id,
+            'short_name': token.short_name,
+            '3d_secure': False,
+            'verified': False,
+        }
+
+        if verify_validity != False:
+            token.validate()
+            res['verified'] = token.verified
+
+        return res
+
+    @route('/payment/stripe/s2s/create_setup_intent', type='json', auth='public', csrf=False)
+    def stripe_s2s_create_setup_intent(self, acquirer_id, **kwargs):
+        acquirer = request.env['payment.acquirer'].browse(int(acquirer_id))
+        res = acquirer._create_setup_intent(kwargs)
+        return res.get('client_secret')
+
+
+    @route('/payment/stripe/s2s/process_payment_intent', type='json')
+    def stripe_s2s_process_payment_intent(self, **post):
+        return request.env['payment.transaction'].sudo().form_feedback(post, 'stripe')
+
+    
+    # These routes are deprecated, let's remove them for security's sake
+    def stripe_s2s_create_json(self, **post):
+        raise werkzeug.exceptions.NotFound()
+    
+    def stripe_s2s_create(self, **post):
+        raise werkzeug.exceptions.NotFound()
+
+    def stripe_create_charge(self, **post):
+        raise werkzeug.exceptions.NotFound()
