78f3d15984024f71e7a67240f4723afe7632e3b8
svs-odoo <svs@odoo.com>
2019-09-09 08:21:10 +0000

    [IMP] product_expiry: add use_expiration_date
    Adds new product field, `use_expiration_date`, to know if user wants to
    use expiration date on product. Only accessible when product is tracked.
    
    Also, changes fields' name:
        - product.template:     life_time becomes expiration_time
        - stock.production.lot: life_date becomes expiration_date
    
    Task #1938656

--- addons/product_expiry/models/product_product.py
+++ addons/product_expiry/models/product_product.py
@@ -7,7 +7,10 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    life_time = fields.Integer(string='Product Life Time',
+    use_expiration_date = fields.Boolean(string='Expiration Date',
+        help='When this box is ticked, you have the possibility to specify dates to manage'
+        ' product expiration, on the product and on the corresponding lot/serial numbers')
+    expiration_time = fields.Integer(string='Product Life Time',
         help='Number of days before the goods may become dangerous and must not be consumed. It will be computed on the lot/serial number.')
     use_time = fields.Integer(string='Product Use Time',
         help='Number of days before the goods starts deteriorating, without being dangerous yet. It will be computed using the lot/serial number.')
