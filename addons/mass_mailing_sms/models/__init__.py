f1edb62e8e81c62625a846be0df343caa4f840a8
qmo-odoo <qmo@odoo.com>
2019-09-19 11:34:03 +0000

    [MOV] mass_mailing_sms,link_tracker: Move convert_links_text to link_tracker
    This method being needed in the new social app, it made sense to move it to
    the link_tracker module to make it available for mass_mailing_sms and social
    to avoid unnecessary duplicated code.
    
    LINKS:
    
    closes odoo/odoo#34973
    
    Taskid: 2045210
    Pr: 37143
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mass_mailing_sms/models/__init__.py
+++ addons/mass_mailing_sms/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import link_tracker
 from . import mailing_contact
 from . import mailing_list
 from . import mailing_mailing
