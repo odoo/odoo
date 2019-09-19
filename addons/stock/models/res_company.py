5ad757cadd99474b5f68b3977729a2fadfd125a6
Victor Feyens <vfe@odoo.com>
2019-09-19 09:47:20 +0000

    [IMP] stock: location creation on company creation.

--- addons/stock/models/res_company.py
+++ addons/stock/models/res_company.py
@@ -80,8 +80,8 @@ class Company(models.Model):
             })
 
     def _create_scrap_location(self):
+        parent_location = self.env.ref('stock.stock_location_locations_virtual', raise_if_not_found=False)
         for company in self:
-            parent_location = self.env.ref('stock.stock_location_locations_virtual', raise_if_not_found=False)
             scrap_location = self.env['stock.location'].create({
                 'name': 'Scrap',
                 'usage': 'inventory',
