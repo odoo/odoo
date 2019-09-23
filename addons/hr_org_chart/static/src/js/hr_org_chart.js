85d9fba11ae537132c993a6ee3d37c20be1deeb6
Toufik Ben Jaa <tbe@odoo.com>
2019-09-23 09:36:11 +0000

    [FIX] hr{_org_chart}: multi-company access issues on employees
    - Avoid to crash when trying to render an employee's org chart
      when in a different company than yours.
    - Retrieve the context from the current environment instead of
      the `_check_employee` function parameters.
    
    closes odoo/odoo#37251
    
    Signed-off-by: Romain Libert (rli) <rli@odoo.com>

--- addons/hr_org_chart/static/src/js/hr_org_chart.js
+++ addons/hr_org_chart/static/src/js/hr_org_chart.js
@@ -84,6 +84,9 @@ var FieldOrgChart = AbstractField.extend({
 
         var self = this;
         return this._getOrgData().then(function (orgData) {
+            if(_.isEmpty(orgData)) {
+                return false;
+            }
             orgData.view_employee_id = self.recordData.id;
             self.$el.html(QWeb.render("hr_org_chart", orgData));
             self.$('[data-toggle="popover"]').each(function () {
