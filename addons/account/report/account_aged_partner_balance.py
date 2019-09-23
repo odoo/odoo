a38e2db100ecb6f1b5fd326f305acfa10c535544
Priyanka Kakadiya <pka@odoo.com>
2019-09-23 17:21:05 +0530

    [FIX] account: removed unused accounting report
    Before this commit:
    when try to open or duplicate Aged Partner Balance report with the studio it
    gives traceback.
    
    As deprecated pdf reports are removed by this commit:
    e51b4f6
    'Aged Partner Balance' report is removed from manifest but was not removed
    report. So, it is giving traceback.
    
    After this commit:
    'Aged Partner Balance' report is removed, so it will not be shown in the reports.
    
    task-2070274

--- addons/account/report/account_aged_partner_balance.py
+++ addons/account/report/account_aged_partner_balance.py
@@ -228,35 +228,3 @@ class ReportAgedPartnerBalance(models.AbstractModel):
             if at_least_one_amount or (self._context.get('include_nullified_amount') and lines[partner['partner_id']]):
                 res.append(values)
         return res, total, lines
-
-    @api.model
-    def _get_report_values(self, docids, data=None):
-        if not data.get('form') or not self.env.context.get('active_model') or not self.env.context.get('active_id'):
-            raise UserError(_("Form content is missing, this report cannot be printed."))
-
-        total = []
-        model = self.env.context.get('active_model')
-        docs = self.env[model].browse(self.env.context.get('active_id'))
-
-        target_move = data['form'].get('target_move', 'all')
-        date_from = fields.Date.from_string(data['form'].get('date_from')) or fields.Date.today()
-
-        if data['form']['result_selection'] == 'customer':
-            account_type = ['receivable']
-        elif data['form']['result_selection'] == 'supplier':
-            account_type = ['payable']
-        else:
-            account_type = ['payable', 'receivable']
-
-        movelines, total, dummy = self._get_partner_move_lines(account_type, date_from, target_move, data['form']['period_length'])
-        return {
-            'doc_ids': self.ids,
-            'doc_model': model,
-            'data': data['form'],
-            'docs': docs,
-            'time': time,
-            'get_partner_lines': movelines,
-            'get_direction': total,
-            'company_id': self.env['res.company'].browse(
-                data['form']['company_id'][0]),
-        }
