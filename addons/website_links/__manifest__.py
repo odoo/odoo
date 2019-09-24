403bf9e76f92b9e8bb021a3c186704362edaa7b0
qmo-odoo <qmo@odoo.com>
2019-09-24 08:46:10 +0000

    [IMP] website_links, link_tracker: Add action buttons on tree view
    This commit adds two buttons on the link.tracker tree view:
      - Visit Page : Redirects the user to the visited page
      - Statistics : Redirects to the page statistics
                     (with the '+' at the end of the shortened url)
    In this commit we also add some padding to the graph of the page statistics
    
    LINKS
    
    closes odoo/odoo#37378
    
    Taskid: 2075858
    Pr: #37344
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_links/__manifest__.py
+++ addons/website_links/__manifest__.py
@@ -9,6 +9,7 @@ Those trackers can be used in Google Analytics to track clicks and visitors, or 
     'version': '1.0',
     'depends': ['website', 'link_tracker'],
     'data': [
+        'views/link_tracker_views.xml',
         'views/website_links_template.xml',
         'views/website_links_graphs.xml',
         'security/ir.model.access.csv',
