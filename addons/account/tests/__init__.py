5b9306013ca279808c1aefa542f30a602c55bfd8
Nans Lefebvre <len@odoo.com>
2019-09-24 17:06:30 +0000

    [FIX] account: round debit and credit of account move lines at creation
    This fix is similar to 8020033; both are workarounds for the bug (limitation)
    introduced by 9d87d15db6dd40a5a0162fb126c725a2f0a40414.
    In this case, the currency_field is a related field, and these fields are only
    computed after scalar values are written in database.
    As a result, this value is not rounded at creation.
    
    It is a problem since typically accounting reports are written directly in SQL,
    and thus reuse the values directly.
    Anyway even in the ORM the convert_to_cache optimised this away using the
    assumption that 'the value should have been rounded in convert_to_column'.
    It follows that in the report, errors can be accumulated
    to the point where the report is not balanced.
    This needs to be fixed in database by going through every existing line.
    
    Of note: the limitation that was readily acknowledged has not been worked around
    anywhere, in fact almost every single currency_field is a related one.
    Also of note, commit afef71d6b should "Always round monetary values in database"
    while it explicitly checked for the presence of the currency_field in the values
    and while not introducing a mechanism to make sure it would always be in the
    values.
    
    opw 2066849
    
    closes odoo/odoo#37461
    
    X-original-commit: 9b0d430144c1dcd155941a35e44fb7831ed7b7e6
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -7,6 +7,7 @@ from . import test_account_move_tax_lock_date
 from . import test_account_supplier_invoice
 from . import test_account_validate_account_move
 from . import test_account_invoice_rounding
+from . import test_account_move_rounding
 from . import test_bank_statement_reconciliation
 from . import test_fiscal_position
 from . import test_invoice_onchange
