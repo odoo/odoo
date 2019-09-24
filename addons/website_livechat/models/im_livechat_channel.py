bc6bec5ee1a5fdc74c1d2d94ee587c33ecf46b1a
David Beguin <dbe@odoo.com>
2019-09-24 11:28:20 +0200

    [FIX] website_livechat, im_livechat : use visitor display name instead of name
    As since 5b9a81a2c6948ecfbd8f8ed3cad1b068fc4231d1
    website.visitor.name can be null
    if the visitor is not linked to a lead or a res.partner,
    we cannot use the name anymore to build the name of the mail.channel, etc..
    Using display_name will always return the correct value,
    and will include the numbering of the visitor,
    in order to identify easily the chat windows
    (if operator is speaking with multi visitor at the same time)
    
    Task ID: 2076190
    PR #37340

--- addons/website_livechat/models/im_livechat_channel.py
+++ addons/website_livechat/models/im_livechat_channel.py
@@ -16,7 +16,7 @@ class ImLivechatChannel(models.Model):
                 'livechat_active': True
             })
             if not user_id:
-                mail_channel_vals['anonymous_name'] = visitor_sudo.name + (' (%s)' % visitor_sudo.country_id.name if visitor_sudo.country_id else '')
+                mail_channel_vals['anonymous_name'] = visitor_sudo.display_name + (' (%s)' % visitor_sudo.country_id.name if visitor_sudo.country_id else '')
             # As chat requested by the visitor, delete the chat requested by an operator if any to avoid conflicts between two flows
             chat_request_channel = self.env['mail.channel'].sudo().search([('livechat_visitor_id', '=', visitor_sudo.id), ('livechat_active', '=', True)])
             for mail_channel in chat_request_channel:
