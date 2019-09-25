687854718be8b0e704a6987bbb6d29c02f804ee2
Mantavya Gajjar <mga@openerp.com>
2019-09-25 21:34:31 +0200

    [FIX]: changes as per the review [TODO]: make a merge proposals based on the feedback

--- addons/l10n_in/models/account.py
+++ addons/l10n_in/models/account.py
@@ -12,7 +12,6 @@ class AccountJournal(models.Model):
     l10n_in_import_export = fields.Boolean("Import/Export", help="Tick this if this journal is use for Import/Export Under Indian GST.")
     l10n_in_gstin_partner_id = fields.Many2one('res.partner', string="GSTIN", ondelete="restrict", help="GSTIN related to this journal. If empty then consider as company GSTIN.")
 
-    @api.depends('name', 'state')
     def name_get(self):
         result = []
         for journal in self:
