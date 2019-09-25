de84975b0907b1dbfc3613ddc56fbcc9f93844c6
Thibault Delavallée <tde@odoo.com>
2019-09-25 09:39:51 +0000

    [IMP] event_sms: allow event managers to manage sms templates
    PURPOSE
    
    Allow people to edit sms templates. Limit that rights to some main
    application managers.
    
    SPECIFICATIONS
    
    SMS Template access rights
    GROUP-------------R-W-C-D-Note
    Internal User-----X
    ​​​Admin / Settings--X-X-X-X
    Stock Manager-----X-X-X-stock.picking
    Sales Manager-----X-X-X-crm.lead, res.partner
    Event Manager-----X-X-X-event.registration
    Sub Manager-------X-X-X-sale.subscription, res.partner
    MarkAut Manager---X-X-X-no limit
    Account Manager---X-X-X-res.partner (followup)
    
    Other groups
      * Online Appointment NO TEMPLATE USED
      * Accounting Manager NO TEMPLATE USED
      * SMS Marketing NO TEMPLATE USED
      * Studio Automated Action Need Technical Settings Anyway
      * Scheduled Actions Need Technical Settings Anyway
      * Server Action Need Technical Settings Anyway
    
    LINKS
    
    Task 2076366 (send now)
    Task 2067873 (template access)
    PR #37298
    PR odoo/enterprise#5750

--- addons/event_sms/__manifest__.py
+++ addons/event_sms/__manifest__.py
@@ -11,6 +11,8 @@
         'data/sms_data.xml',
         'views/event_views.xml',
         'views/event_mail_views.xml',
+        'security/ir.model.access.csv',
+        'security/sms_security.xml',
     ],
     'demo': [
     ],
