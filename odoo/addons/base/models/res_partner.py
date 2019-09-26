62430fb42549d34d9fe68c13c77c42348ff2806e
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-09-26 15:20:19 +0200

    [IMP] base: Add contact format field on res.country
    In switzerland a contact person is always written on a new line.
    Therefore we would like to be able to have the following display on the reports:
    Company Name
    Contact person
    Street Nr
    Zip City
    COUNTRY
    
    However, it is not possible to do a proper extension, as the format string is hardcoded in _get_name function.
    With this new field on res.country, we're able to define a specific contact for each country that needs it.

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -680,12 +680,12 @@ class Partner(models.Model):
         """ Utility method to allow name_get to be overrided without re-browse the partner """
         partner = self
         name = partner.name or ''
-
         if partner.company_name or partner.parent_id:
             if not name and partner.type in ['invoice', 'delivery', 'other']:
                 name = dict(self.fields_get(['type'])['type']['selection'])[partner.type]
             if not partner.is_company:
-                name = "%s, %s" % (partner.commercial_company_name or partner.parent_id.name, name)
+                contact_format = partner.country_id.contact_format or partner.parent_id.country_id.contact_format or "%s, %s"
+                name = contact_format % (partner.commercial_company_name or partner.parent_id.name, name)
         if self._context.get('show_address_only'):
             name = partner._display_address(without_company=True)
         if self._context.get('show_address'):
