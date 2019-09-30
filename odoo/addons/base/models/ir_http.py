73a37bedbfb6debe052460981b8c923ffe4fc4ee
Nans Lefebvre <len@odoo.com>
2019-09-30 12:38:34 +0000

    [FIX] ir_http: return a 404 if binary content cannot be decoded
    Backport of a68f7e6e72dbfd44e14c2f293fa6dc6aa402308a
    
    Commit 5a9e1af64acc has the unfortunate side-effect of crashing early
    if for any reason the content cannot be decoded.
    However, simply ignoring that the content cannot be decoded is no better idea:
    some functions pipe the result to decoding functions that crash the same.
    The resulting traceback pollutes the log with uninformative message such as:
    binascii.Error: Incorrect padding 5 0.002 0.016
    
    In case the content cannot be decoded (data corruption, or simply missing file)
    we return a clean 404 instead, which is morally almost equivalent,
    and is clean even from functions that depend on binary_content.
    
    opw 2117114
    
    closes odoo/odoo#39806
    
    X-original-commit: e923a9654a3f22bff84e79f2d03a568b5f9985bd
    Signed-off-by: Nans Lefebvre (len) <len@odoo.com>

--- odoo/addons/base/models/ir_http.py
+++ odoo/addons/base/models/ir_http.py
@@ -362,7 +362,11 @@ class IrHttp(models.AbstractModel):
                 attach_mimetype = env['ir.attachment'].search_read(domain=[('res_model', '=', model), ('res_id', '=', id), ('res_field', '=', field)], fields=['mimetype'], limit=1)
                 mimetype = attach_mimetype and attach_mimetype[0]['mimetype']
             if not mimetype:
-                mimetype = guess_mimetype(base64.b64decode(content), default=default_mimetype)
+                try:
+                    decoded_content = base64.b64decode(content)
+                except base64.binascii.Error:  # if we could not decode it, no need to pass it down: it would crash elsewhere...
+                    return (404, [], None)
+                mimetype = guess_mimetype(decoded_content, default=default_mimetype)
 
         # extension
         _, existing_extension = os.path.splitext(filename)
