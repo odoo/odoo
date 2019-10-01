0467cab22511ebb037131f937d8cf5e872f496fd
Christophe Simonis <chs@odoo.com>
2019-10-01 16:03:59 +0000

    [FIX] l10n_*: accurate module version
    closes odoo/odoo#37732
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- addons/l10n_ar/__manifest__.py
+++ addons/l10n_ar/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Argentinian Accounting',
-    'version': '13.0.1.0.0',
+    'version': "3.0",
     'description': """
 Functional
 ----------
