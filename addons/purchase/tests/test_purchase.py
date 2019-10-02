5487df74e14363cf4c60ae9ab80ce46cfea67de5
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-10-02 10:49:27 +0000

    [IMP] purchase: Take into account purchase order date for sequence
    **Steps to reproduce**
    
    * Define "Purchase Order" sequence for using date ranges checking "Use subsequences per date_range".
    * Change sequence prefix to "PO/%(year)s/".
    * Create a purchase order with order date in 2020 (a different year than current one).
    
    **Current behavior**
    
    Got a purchase order with 2019 (current year) number.
    
    **Expected behavior**
    
    Got a purchase order with 2020 number.
    
    closes odoo/odoo#38468
    
    X-original-commit: ce423e6adb6659a1d5885bad158c59b882cb4565
    Signed-off-by: Damien Bouvy (dbo) <dbo@odoo.com>

--- addons/purchase/tests/test_purchase.py
+++ addons/purchase/tests/test_purchase.py
@@ -92,3 +92,26 @@ class TestPurchase(SavepointCase):
                 po_line.date_planned = orig_date_planned
         po.save()
 
+    def test_purchase_order_sequence(self):
+        PurchaseOrder = self.env['purchase.order'].with_context(tracking_disable=True)
+        company = self.env.user.company_id
+        self.env['ir.sequence'].search([
+            ('code', '=', 'purchase.order'),
+        ]).write({
+            'use_date_range': True, 'prefix': 'PO/%(range_year)s/',
+        })
+        vals = {
+            'partner_id': self.vendor.id,
+            'company_id': company.id,
+            'currency_id': company.currency_id.id,
+            'date_order': '2019-01-01',
+        }
+        purchase_order = PurchaseOrder.create(vals.copy())
+        self.assertTrue(purchase_order.name.startswith('PO/2019/'))
+        vals['date_order'] = '2020-01-01'
+        purchase_order = PurchaseOrder.create(vals.copy())
+        self.assertTrue(purchase_order.name.startswith('PO/2020/'))
+        # In EU/BXL tz, this is actually already 01/01/2020
+        vals['date_order'] = '2019-12-31 23:30:00'
+        purchase_order = PurchaseOrder.with_context(tz='Europe/Brussels').create(vals.copy())
+        self.assertTrue(purchase_order.name.startswith('PO/2020/'))
