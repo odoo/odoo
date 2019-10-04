a26e71a938f086dc88c55b791b8cbde98c51cb82
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-10-04 10:37:54 +0200

    [FIX] purchase_stock: Use hook method
    This uses the hook method introduced in ce1f1b6 for consistency when used on modules
    that modify this behavior.

--- addons/purchase_stock/models/purchase.py
+++ addons/purchase_stock/models/purchase.py
@@ -290,7 +290,7 @@ class PurchaseOrderLine(models.Model):
         self.ensure_one()
         line = self[0]
         order = line.order_id
-        price_unit = line.price_unit
+        price_unit = line._prepare_compute_all_values()['price_unit']
         if line.taxes_id:
             price_unit = line.taxes_id.with_context(round=False).compute_all(
                 price_unit, currency=line.order_id.currency_id, quantity=1.0, product=line.product_id, partner=line.order_id.partner_id
