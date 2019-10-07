af2867791d161ddc1af2dfd5040391c299b6d0d1
Jeremy Kersten <jke@odoo.com>
2019-10-07 08:56:02 +0000

    [FIX] website: allow authenticate in json multidb
    Before this commit launch a server with --db-filter that match at least 2 dbs name
    Try to authenticate
    
    You will have an error request is unbound when you try to access request.env
    
    Now we retrieve the user from self instead of the request.
    
    New test to ensure rpc authentication is tested.
    
    Related to commit 245ef4b1
    
    closes odoo/odoo#38969
    
    X-original-commit: 4b3400c430bec7539aada0619fd203978daca2d8
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

---
+++ addons/test_website/static/tests/tours/json_auth.js
@@ -0,0 +1,26 @@
+odoo.define('test_website.json_auth', function (require) {
+'use strict';
+
+var tour = require('web_tour.tour');
+var session = require('web.session')
+
+tour.register('test_json_auth', {
+    test: true,
+}, [{
+    trigger: 'body',
+    run: async function () {
+        await session.rpc('/test_get_dbname').then( function (result){
+            return session.rpc("/web/session/authenticate", {
+                db: result,
+                login: 'admin',
+                password: 'admin'
+            });
+        });
+        window.location.href = window.location.origin;
+    },
+}, {
+    trigger: 'span:contains(Mitchell Admin)',
+    run: function () {},
+}
+]);
+});
--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -1255,10 +1255,11 @@ class WebsiteSale(http.Controller):
     @http.route('/shop/products/recently_viewed_update', type='json', auth='public', website=True)
     def products_recently_viewed_update(self, product_id, **kwargs):
         res = {}
-        visitor_sudo = request.env['website.visitor']._get_visitor_from_request_or_create()
-        if request.httprequest.cookies.get('visitor_uuid', '') != visitor_sudo.access_token:
-            res['visitor_uuid'] = visitor_sudo.access_token
-        visitor_sudo._add_viewed_product(product_id)
+        visitor_sudo = request.env['website.visitor']._get_visitor_from_request(force_create=True)
+        if visitor_sudo:
+            if request.httprequest.cookies.get('visitor_uuid', '') != visitor_sudo.access_token:
+                res['visitor_uuid'] = visitor_sudo.access_token
+            visitor_sudo._add_viewed_product(product_id)
         return res
 
     @http.route('/shop/products/recently_viewed_delete', type='json', auth='public', website=True)
