718e647017fd8a64a982e76f3939ca56192879ee
Florimond Husquinet (fhu) <fhu@odoo.com>
2019-10-07 13:24:08 +0000

    [FIX] base: impossible to change contact name format
    - Create a sales order for a company with a contact name.
    - Click the Preview button.
    - The company and contact names appear on the same line, only separated by a
    comma.
    
    Before this commit:
    
    it's not possible to easily override this behavior.
    
    After this commit:
    
    it's possible to override `Partner._get_contact_name` to change the format of
    contact name.
    
    Beware that this method is used in `_get_name` which is called from various
    places. A check on the context may be necessary.
    
    closes odoo/odoo#38100
    
    Opw: 2077294
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -676,6 +676,9 @@ class Partner(models.Model):
                 'target': 'new',
                 'flags': {'form': {'action_buttons': True}}}
 
+    def _get_contact_name(self, partner, name):
+        return "%s, %s" % (partner.commercial_company_name or partner.parent_id.name, name)
+
     def _get_name(self):
         """ Utility method to allow name_get to be overrided without re-browse the partner """
         partner = self
@@ -685,7 +688,7 @@ class Partner(models.Model):
             if not name and partner.type in ['invoice', 'delivery', 'other']:
                 name = dict(self.fields_get(['type'])['type']['selection'])[partner.type]
             if not partner.is_company:
-                name = "%s, %s" % (partner.commercial_company_name or partner.parent_id.name, name)
+                name = self._get_contact_name(partner, name)
         if self._context.get('show_address_only'):
             name = partner._display_address(without_company=True)
         if self._context.get('show_address'):
