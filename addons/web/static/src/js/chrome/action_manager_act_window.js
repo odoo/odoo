0d408aae52bc7803304b912da1ebc800a5eb5a2f
Romain Derie <rde@odoo.com>
2019-10-08 09:47:26 +0000

    [REM] web: remove unused controller /apps/<app>
    This controller never really worked in the last 3 versions.
    It was fixed in 11.3 with 8f69a39515d but broken with refactoring in 12.0 at a
    higher level with 872461c8cc8d.
    
    It was even worse in 13.0 as it was leading to a traceback: `view_type` field
    got removed with fad8d1e85e498 but this controller was still reading that
    field.
    
    closes odoo/odoo#38355
    
    X-original-commit: a5c4e262449fef03d05d5250e4523832db5fbf00
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- addons/web/static/src/js/chrome/action_manager_act_window.js
+++ addons/web/static/src/js/chrome/action_manager_act_window.js
@@ -58,11 +58,8 @@ ActionManager.include({
      * @param {string} [state.view_type]
      */
     loadState: function (state) {
-        var callersArguments = arguments;
         var _super = this._super.bind(this);
-        var self = this;
         var action;
-        var def;
         var options = {
             clear_breadcrumbs: true,
             pushState: false,
@@ -117,20 +114,12 @@ ActionManager.include({
                 action = lastAction;
                 options.viewType = state.view_type;
             }
-        } else if (state.sa) {
-            def = this._rpc({
-                route: '/web/session/get_session_action',
-                params: {key: state.sa},
-            }).then(function (sessionAction) {
-                action = sessionAction;
-            });
         }
-        return Promise.all([def]).then(function () {
-            if (action) {
-                return self.doAction(action, options);
-            }
-            return _super.apply(self, callersArguments);
-        });
+        if (action) {
+            return this.doAction(action, options);
+        }
+        return _super.apply(this, arguments);
+
     },
 
     //--------------------------------------------------------------------------
