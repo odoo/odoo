fcf7a395e805a844bfc74795519841a6be88d10b
Arnold Moyaux <arm@odoo.com>
2019-10-08 14:02:30 +0000

    [FIX] mrp_subcontracting: correct type for view registry
    When the action is called by a javascript function the view
    is not correctly rendered because the tree type does not exist
    in the registry but the correct type is list.

--- addons/mrp_subcontracting/models/stock_move.py
+++ addons/mrp_subcontracting/models/stock_move.py
@@ -88,7 +88,7 @@ class StockMove(models.Model):
             'name': _('Raw Materials for %s') % (self.product_id.display_name),
             'type': 'ir.actions.act_window',
             'res_model': 'stock.move',
-            'views': [(tree_view.id, 'tree'), (form_view.id, 'form')],
+            'views': [(tree_view.id, 'list'), (form_view.id, 'form')],
             'target': 'current',
             'domain': [('id', 'in', moves.ids)],
         }
