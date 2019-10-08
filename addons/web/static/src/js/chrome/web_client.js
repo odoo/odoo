odoo/odoo

commit 19eacf7d23c9413de4430a3422b5ed74b37ef242

[IMP] web: new navbar in community
The navbar in community now looks more like the navbar in enterprise.

Here is a sum-up of the changes in community:

- The navbar in community is now in blue, instead of black.
- No more app list that almost fills the whole navbar: there is
  now an "Apps Menu" in the left-most side of the navbar.
- No more sidebar for the menus inside an app: those menus are
  now located in the middle part of the navbar.

The navbar in community does not support mobile (< 768px width),
but it should still somewhat usable on most mobile devices.
As for the website navbar, it has been unchanged and should still
support mobile layout.

Task-ID 1878649

addons/web/static/src/js/chrome/web_client.js
@@ -6,149 +6,164 @@ var config = require('web.config');
 var core = require('web.core');
 var data_manager = require('web.data_manager');
 var dom = require('web.dom');
-var framework = require('web.framework');
 var Menu = require('web.Menu');
 var session = require('web.session');
-var SystrayMenu = require('web.SystrayMenu');
-var UserMenu = require('web.UserMenu');
 
 return AbstractWebClient.extend({
-    events: _.extend({}, AbstractWebClient.prototype.events, {
-        'click .oe_logo_edit_admin': 'logo_edit',
-        'click .oe_logo img': function(ev) {
-            ev.preventDefault();
-            return this.clear_uncommitted_changes().then(function() {
-                framework.redirect("/web" + (config.debug ? "?debug" : ""));
-            });
-        },
+    custom_events: _.extend({}, AbstractWebClient.prototype.custom_events, {
+        app_clicked: 'on_app_clicked',
+        menu_clicked: 'on_menu_clicked',
     }),
-    show_application: function() {
-        var self = this;
-
-        this.toggle_bars(true);
-        this.set_title();
-        this.update_logo();
-
-        // Menu is rendered server-side thus we don't want the widget to create any dom
-        this.menu = new Menu(this);
-        this.menu.setElement(this.$el.parents().find('.oe_application_menu_placeholder'));
-        this.menu.on('menu_click', this, this.on_menu_action);
-
-        // Create the user menu (rendered client-side)
-        this.user_menu = new UserMenu(this);
-        var $user_menu_placeholder = $('body').find('.oe_user_menu_placeholder').show();
-        var user_menu_loaded = this.user_menu.appendTo($user_menu_placeholder);
-
-        // Create the systray menu (rendered server-side)
-        this.systray_menu = new SystrayMenu(this);
-        this.systray_menu.setElement(this.$el.parents().find('.oe_systray'));
-        var systray_menu_loaded = this.systray_menu.start();
-
-        // Start the menu once both systray and user menus are rendered
-        // to prevent overflows while loading
-        return $.when(systray_menu_loaded, user_menu_loaded).then(function() {
-            self.bind_hashchange();
-            self.menu.$el.removeClass('d-none');
-            self.menu.start();
+    start: function () {
+        core.bus.on('change_menu_section', this, function (menuID) {
+            this.do_push_state(_.extend($.bbq.getState(), {
+                menu_id: menuID,
+            }));
         });
 
+        return this._super.apply(this, arguments);
     },
-    toggle_bars: function(value) {
-        this.$('tr:has(td.navbar),.oe_leftbar').toggle(value);
-    },
-    update_logo: function(reload) {
-        var company = session.company_id;
-        var img = session.url('/web/binary/company_logo' + '?db=' + session.db + (company ? '&company=' + company : ''));
-        this.$('.o_sub_menu_logo img').attr('src', '').attr('src', img + (reload ? "&t=" + Date.now() : ''));
-        this.$('.oe_logo_edit').toggleClass('oe_logo_edit_admin', session.is_superuser);
-    },
-    logo_edit: function(ev) {
+    bind_events: function () {
         var self = this;
-        ev.preventDefault();
-        this._rpc({
-                model: 'res.users',
-                method: 'read',
-                args: [[session.uid], ['company_id']],
+        this._super.apply(this, arguments);
+
+        /*
+            Small patch to allow having a link with a href towards an anchor. Since odoo use hashtag
+            to represent the current state of the view, we can't easily distinguish between a link
+            towards an anchor and a link towards anoter view/state. If we want to navigate towards an
+            anchor, we must not change the hash of the url otherwise we will be redirected to the app
+            switcher instead.
+            To check if we have an anchor, first check if we have an href attributes starting with #.
+            Try to find a element in the DOM using JQuery selector.
+            If we have a match, it means that it is probably a link to an anchor, so we jump to that anchor.
+        */
+        this.$el.on('click', 'a', function (ev) {
+            var disable_anchor = ev.target.attributes.disable_anchor;
+            if (disable_anchor && disable_anchor.value === "true") {
+                return;
+            }
+
+            var href = ev.target.attributes.href;
+            if (href) {
+                if (href.value[0] === '#' && href.value.length > 1) {
+                    if (self.$("[id='"+href.value.substr(1)+"']").length) {
+                        ev.preventDefault();
+                        self.trigger_up('scrollTo', {'selector': href.value});
+                    }
+                }
+            }
+        });
+    },
+    load_menus: function () {
+        return this._rpc({
+                model: 'ir.ui.menu',
+                method: 'load_menus',
+                args: [config.debug],
+                context: session.user_context,
             })
-            .then(function(data) {
-                self._rpc({
-                        route: '/web/action/load',
-                        params: { action_id: 'base.action_res_company_form' },
-                    })
-                    .done(function(result) {
-                        result.res_id = data[0].company_id[0];
-                        result.target = "new";
-                        result.views = [[false, 'form']];
-                        result.flags = {
-                            action_buttons: true,
-                            headless: true,
-                        };
-                        self.action_manager.doAction(result, {
-                            on_close: self.update_logo.bind(self, true),
-                        });
-                    });
+            .then(function (menuData) {
+                // Compute action_id if not defined on a top menu item
+                for (var i = 0; i < menuData.children.length; i++) {
+                    var child = menuData.children[i];
+                    if (child.action === false) {
+                        while (child.children && child.children.length) {
+                            child = child.children[0];
+                            if (child.action) {
+                                menuData.children[i].action = child.action;
+                                break;
+                            }
+                        }
+                    }
+                }
+                return menuData;
             });
-        return false;
     },
-    bind_hashchange: function() {
+    show_application: function () {
         var self = this;
-        $(window).bind('hashchange', this.on_hashchange);
-        var didHashChanged = false;
-        $(window).one('hashchange', function () {
-            didHashChanged = true;
-        });
+        this.set_title();
 
-        var state = $.bbq.getState(true);
-        if (_.isEmpty(state) || state.action === "login") {
-            self.menu.is_bound.done(function() {
-                self._rpc({
+        return this.instanciate_menu_widgets().then(function () {
+            $(window).bind('hashchange', self.on_hashchange);
+
+            // If the url's state is empty, we execute the user's home action if there is one (we
+            // show the first app if not)
+            if (_.isEmpty($.bbq.getState(true))) {
+                return self._rpc({
                         model: 'res.users',
                         method: 'read',
-                        args: [[session.uid], ['action_id']],
+                        args: [session.uid, ["action_id"]],
                     })
-                    .done(function(result) {
-                        var amDef = self.action_manager.dp.current_def;
-                        if (didHashChanged || (amDef && amDef.state() === 'pending')) {
-                            return;
-                        }
+                    .then(function (result) {
                         var data = result[0];
-                        if(data.action_id) {
-                            self.action_manager.doAction(data.action_id[0]);
-                            self.menu.open_action(data.action_id[0]);
+                        if (data.action_id) {
+                            return self.do_action(data.action_id[0]).then(function () {
+                                self.menu.change_menu_section(self.menu.action_id_to_primary_menu_id(data.action_id[0]));
+                            });
                         } else {
-                            var first_menu_id = self.menu.$el.find("a:first").data("menu");
-                            if(first_menu_id) {
-                                self.menu.menu_click(first_menu_id);
+                            self.menu.openFirstApp();
+                        }
+                    });
+            } else {
+                return self.on_hashchange();
+            }
+        });
+    },
+
+    instanciate_menu_widgets: function () {
+        var self = this;
+        var defs = [];
+        return this.load_menus().then(function (menuData) {
+            self.menu_data = menuData;
+
+            // Here, we instanciate every menu widgets and we immediately append them into dummy
+            // document fragments, so that their `start` method are executed before inserting them
+            // into the DOM.
+            if (self.menu) {
+                self.menu.destroy();
+            }
+            self.menu = new Menu(self, menuData);
+            defs.push(self.menu.prependTo(self.$el));
+            return $.when.apply($, defs);
+        });
+    },
+
+    // --------------------------------------------------------------
+    // URL state handling
+    // --------------------------------------------------------------
+    on_hashchange: function (event) {
+        if (this._ignore_hashchange) {
+            this._ignore_hashchange = false;
+            return $.when();
+        }
+
+        var self = this;
+        return this.clear_uncommitted_changes().then(function () {
+            var stringstate = $.bbq.getState(false);
+            if (!_.isEqual(self._current_state, stringstate)) {
+                var state = $.bbq.getState(true);
+                if (state.action || (state.model && (state.view_type || state.id))) {
+                    return self.action_manager.loadState(state, !!self._current_state).then(function () {
+                        if (state.menu_id) {
+                            if (state.menu_id !== self.menu.current_primary_menu) {
+                                core.bus.trigger('change_menu_section', state.menu_id);
+                            }
+                        } else {
+                            var action = self.action_manager.getCurrentAction();
+                            if (action) {
+                                var menu_id = self.menu.action_id_to_primary_menu_id(action.id);
+                                if (menu_id) {
+                                    core.bus.trigger('change_menu_section', menu_id);
+                                }
                             }
                         }
                     });
-            });
-        } else {
-            $(window).trigger('hashchange');
-        }
-    },
-    on_hashchange: function(event) {
-        if (this._ignore_hashchange) {
-            this._ignore_hashchange = false;
-            return;
-        }
-
-        var self = this;
-        this.clear_uncommitted_changes().then(function () {
-            var stringstate = event.getState(false);
-            if (!_.isEqual(self._current_state, stringstate)) {
-                var state = event.getState(true);
-                if(!state.action && state.menu_id) {
-                    self.menu.is_bound.done(function() {
-                        self.menu.menu_click(state.menu_id);
+                } else if (state.menu_id) {
+                    var action_id = self.menu.menu_id_to_action_id(state.menu_id);
+                    return self.do_action(action_id, {clear_breadcrumbs: true}).then(function () {
+                        core.bus.trigger('change_menu_section', state.menu_id);
                     });
                 } else {
-                    self.action_manager.loadState(state, !!self._current_state).then(function () {
-                        var action = self.action_manager.getCurrentAction();
-                        if (action) {
-                            self.menu.open_action(action.id, state.menu_id);
-                        }
-                    });
+                    self.menu.openFirstApp();
                 }
             }
             self._current_state = stringstate;
@@ -159,17 +174,88 @@ return AbstractWebClient.extend({
             }
         });
     },
-    on_menu_action: function(options) {
-        this.action_manager.doAction(options.action_id, {
-            clear_breadcrumbs: true,
-            action_menu_id: options.id,
-        });
+
+    // --------------------------------------------------------------
+    // Menu handling
+    // --------------------------------------------------------------
+    on_app_clicked: function (ev) {
+        var self = this;
+        return this.menu_dm.add(data_manager.load_action(ev.data.action_id))
+            .then(function (result) {
+                return self.action_mutex.exec(function () {
+                    var completed = $.Deferred();
+                    var options = _.extend({}, ev.data.options, {
+                        clear_breadcrumbs: true,
+                        action_menu_id: ev.data.menu_id,
+                    });
+                    $.when(self._openMenu(result, options)).fail(function () {
+                        completed.resolve();
+                    }).done(function () {
+                        self._on_app_clicked_done(ev)
+                            .then(completed.resolve.bind(completed))
+                            .fail(completed.reject.bind(completed));
+                    });
+                    setTimeout(function () {
+                        completed.resolve();
+                    }, 2000);
+                    return completed;
+                });
+            });
     },
-    toggle_fullscreen: function(fullscreen) {
-        this._super(fullscreen);
-        if (!fullscreen) {
-            core.bus.trigger('resize');
-        }
+    _on_app_clicked_done: function (ev) {
+        core.bus.trigger('change_menu_section', ev.data.menu_id);
+        return $.Deferred().resolve();
+    },
+    on_menu_clicked: function (ev) {
+        var self = this;
+        return this.menu_dm.add(data_manager.load_action(ev.data.action_id))
+            .then(function (result) {
+                return self.action_mutex.exec(function () {
+                    var completed = $.Deferred();
+                    $.when(self._openMenu(result, {
+                        clear_breadcrumbs: true,
+                    })).always(function () {
+                        completed.resolve();
+                    });
+
+                    setTimeout(function () {
+                        completed.resolve();
+                    }, 2000);
+
+                    return completed;
+                });
+            }).always(function () {
+                self.$el.removeClass('o_mobile_menu_opened');
+            });
+    },
+    /**
+     * Open the action linked to a menu.
+     * This function is mostly used to allow override in other modules.
+     *
+     * @private
+     * @param {Object} action
+     * @param {Object} options
+     * @returns {Deferred}
+     */
+    _openMenu: function (action, options) {
+        return this.do_action(action, options);
+    },
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * Returns the left and top scroll positions of the main scrolling area
+     * (i.e. the action manager in desktop).
+     *
+     * @returns {Object} with keys left and top
+     */
+    getScrollPosition: function () {
+        return {
+            left: this.action_manager.el.scrollLeft,
+            top: this.action_manager.el.scrollTop,
+        };
     },
 
     //--------------------------------------------------------------------------
@@ -178,29 +264,22 @@ return AbstractWebClient.extend({
 
     /**
      * @override
+     * @private
      */
     _onGetScrollPosition: function (ev) {
-        ev.data.callback({
-            left: this.action_manager.el.scrollLeft,
-            top: this.action_manager.el.scrollTop,
-        });
+        ev.data.callback(this.getScrollPosition());
     },
     /**
      * @override
+     * @private
      */
     _onScrollTo: function (ev) {
-        var offset;
-        if (ev.data.selector) {
+        var offset = {top: ev.data.top, left: ev.data.left || 0};
+        if (!offset.top) {
             offset = dom.getPosition(document.querySelector(ev.data.selector));
-            // substract the position of the ActionManager as it is the
-            // scrolling element
-            var actionManagerOffset = dom.getPosition(this.action_manager.el);
-            offset.left -= actionManagerOffset.left;
-            offset.top -= actionManagerOffset.top;
-        } else {
-            offset = {top: ev.data.top || 0, left: ev.data.left || 0};
+            // Substract the position of the action_manager as it is the scrolling part
+            offset.top -= dom.getPosition(this.action_manager.el).top;
         }
-
         this.action_manager.el.scrollTop = offset.top;
         this.action_manager.el.scrollLeft = offset.left;
     },

addons/web/__manifest__.py
@@ -24,6 +24,7 @@ This module provides the core of the Odoo Web Client.
     'qweb': [
         "static/src/xml/base.xml",
         "static/src/xml/kanban.xml",
+        "static/src/xml/menu.xml",
         "static/src/xml/rainbow_man.xml",
         "static/src/xml/report.xml",
         "static/src/xml/web_calendar.xml",

addons/web/static/src/js/chrome/apps_menu.js
@@ -1,0 +1,104 @@
+odoo.define('web.AppsMenu', function (require) {
+"use strict";
+
+var Widget = require('web.Widget');
+
+var AppsMenu = Widget.extend({
+    template: 'AppsMenu',
+    events: {
+        'click .o_app': '_onAppsMenuItemClicked',
+    },
+    /**
+     * @override
+     * @param {web.Widget} parent
+     * @param {Object} menuData
+     * @param {Object[]} menuData.children
+     */
+    init: function (parent, menuData) {
+        this._super.apply(this, arguments);
+        this._activeApp = undefined;
+        this._apps = _.map(menuData.children, function (appMenuData) {
+            return {
+                actionID: parseInt(appMenuData.action.split(',')[1]),
+                menuID: appMenuData.id,
+                name: appMenuData.name,
+                xmlID: appMenuData.xmlid,
+            };
+        });
+    },
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * @returns {Object}
+     */
+    getActiveApp: function () {
+        return this._activeApp || {};
+    },
+    /**
+     * @returns {Object[]}
+     */
+    getApps: function () {
+        return this._apps;
+    },
+    /**
+     * Open the given app
+     */
+    openApp: function () {},
+    /**
+     * Open the first app in the list of apps
+     */
+    openFirstApp: function () {
+        var firstApp = this._apps[0];
+        this._openApp(firstApp);
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {Object} app
+     */
+    _openApp: function (app) {
+        this._setActiveApp(app);
+        this.trigger_up('app_clicked', {
+            action_id: app.actionID,
+            menu_id: app.menuID,
+        });
+    },
+    /**
+     * @private
+     * @param {Object} app
+     */
+    _setActiveApp: function (app) {
+        this._activeApp = app;
+        this.renderElement();
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Called when clicking on an item in the apps menu.
+     *
+     * @private
+     * @param {MouseEvent} ev
+     */
+    _onAppsMenuItemClicked: function (ev) {
+        var $target = $(ev.currentTarget);
+        var actionID = $target.data('action-id');
+        var menuID = $target.data('menu-id');
+        var app = _.findWhere(this._apps, { actionID: actionID, menuID: menuID });
+        this._openApp(app);
+    },
+
+});
+
+return AppsMenu;
+
+});

commit 0bc44afd8242397a9f1b478b18ba7ab847318c39

[FIX] web: restore state session action dispatching at loadState
e9350993ca0 restored the /apps/<app> controller and introduced  a test.

Despite the test still working, the feature was broken at higher level with
19eacf7d23c94 refactoring.
Indeed, loadState was not called anymore even if state.sa existed.

Enterprise counterpart https://github.com/odoo/enterprise/pull/5974

closes odoo/odoo#38206

Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

addons/web/static/src/js/chrome/web_client.js
@@ -141,7 +141,7 @@ return AbstractWebClient.extend({
             var stringstate = $.bbq.getState(false);
             if (!_.isEqual(self._current_state, stringstate)) {
                 var state = $.bbq.getState(true);
-                if (state.action || (state.model && (state.view_type || state.id))) {
+                if (state.sa || state.action || (state.model && (state.view_type || state.id))) {
                     return self.action_manager.loadState(state, !!self._current_state).then(function () {
                         if (state.menu_id) {
                             if (state.menu_id !== self.menu.current_primary_menu) {