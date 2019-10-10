1455e130c94cfffcb91edb68786cdc3492654476
svs-odoo <svs@odoo.com>
2019-10-10 13:36:12 +0000

    [IMP] mrp_product_expiry: wo confirmation
    With product_expiry, when a workorder is done, we check if there is
    expired lot in components. If it is, we ask confirmation to the user.
    
    Task #1938656
    
    closes odoo/odoo#36680
    
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- addons/mrp_product_expiry/models/mrp_workorder.py
+++ addons/mrp_product_expiry/models/mrp_workorder.py
@@ -4,16 +4,16 @@
 from odoo import models
 
 
-class MrpProductProduce(models.TransientModel):
-    _inherit = 'mrp.product.produce'
+class MrpWorkorder(models.Model):
+    _inherit = 'mrp.workorder'
 
-    def do_produce(self):
+    def record_production(self):
         confirm_expired_lots = self._check_expired_lots()
         if confirm_expired_lots:
             return confirm_expired_lots
-        return super(MrpProductProduce, self).do_produce()
+        return super(MrpWorkorder, self).record_production()
 
     def _get_expired_context(self, expired_lot_ids):
-        context = super(MrpProductProduce, self)._get_expired_context(expired_lot_ids)
-        context['default_produce_id'] = self.id
+        context = super(MrpWorkorder, self)._get_expired_context(expired_lot_ids)
+        context['default_workorder_id'] = self.id
         return context
