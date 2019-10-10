afad149eb8a542b39e9508a458d7aeb43aeb60ed
Joseph Caburnay <jcb@odoo.com>
2019-10-10 11:45:35 +0000

    [REF] *_sale_coupon: create coupon module
    This refactoring extracts parts of `sale_coupon` module to create a more
    generic module called `coupon`. This new module can be extended in
    different sales modules to implement functionalities specific to modules
    that extend it.

--- addons/sale_coupon_delivery/models/sale_coupon.py
+++ addons/sale_coupon_delivery/models/sale_coupon.py
@@ -5,10 +5,10 @@
 from odoo import models, _
 
 
-class SaleCoupon(models.Model):
-    _inherit = "sale.coupon"
+class Coupon(models.Model):
+    _inherit = "coupon.coupon"
 
     def _check_coupon_code(self, order):
         if self.program_id.reward_type == 'free_shipping' and not order.order_line.filtered(lambda line: line.is_delivery):
             return {'error': _('The shipping costs are not in the order lines.')}
-        return super(SaleCoupon, self)._check_coupon_code(order)
+        return super(Coupon, self)._check_coupon_code(order)
--- addons/sale_coupon_delivery/models/sale_coupon_reward.py
+++ addons/sale_coupon_delivery/models/sale_coupon_reward.py
@@ -4,15 +4,15 @@
 from odoo import fields, models, _
 
 
-class SaleCouponReward(models.Model):
-    _inherit = 'sale.coupon.reward'
-    _description = "Sales Coupon Reward"
+class CouponReward(models.Model):
+    _inherit = 'coupon.reward'
+    _description = "Coupon Reward"
 
     reward_type = fields.Selection(selection_add=[('free_shipping', 'Free Shipping')])
 
     def name_get(self):
         result = []
-        reward_names = super(SaleCouponReward, self).name_get()
+        reward_names = super(CouponReward, self).name_get()
         free_shipping_reward_ids = self.filtered(lambda reward: reward.reward_type == 'free_shipping').ids
         for res in reward_names:
             result.append((res[0], res[0] in free_shipping_reward_ids and _("Free Shipping") or res[1]))
