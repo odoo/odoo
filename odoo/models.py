0bfb695b4f3f773531f2a0266b0d6a64954bbde6
Victor Feyens <vfe@odoo.com>
2019-10-11 16:40:46 +0000

    [ADD] ORM: with_company

--- odoo/models.py
+++ odoo/models.py
@@ -5011,6 +5011,39 @@ Record ids: %(records)s
         """
         return self.with_env(self.env(user=user, su=False))
 
+    def with_company(self, company):
+        """ with_company(company)
+
+        Return a new version of this recordset with a modified context, such that::
+            result.env.company = company
+            result.env.companies = self.env.companies | company
+
+        :param company:
+        :type company: :class:`~odoo.addons.base.models.res_company` or int
+
+        .. warning::
+
+            When using an unauthorized company for current user,
+            accessing the company(ies) on the environment may trigger
+            an AccessError if not done in a sudoed environment.
+        """
+        if not company:
+            # With company = None/False/0/[]/empty recordset: keep current environment
+            return self
+
+        company_id = int(company)
+        allowed_company_ids = self.env.context.get('allowed_company_ids', [])
+        if allowed_company_ids and company_id == allowed_company_ids[0]:
+            return self
+        # Copy the allowed_company_ids list
+        # to avoid modifying the context of the current environment.
+        allowed_company_ids = list(allowed_company_ids)
+        if company_id in allowed_company_ids:
+            allowed_company_ids.remove(company_id)
+        allowed_company_ids.insert(0, company_id)
+
+        return self.with_context(allowed_company_ids=allowed_company_ids)
+
     def with_context(self, *args, **kwargs):
         """ with_context([context][, **overrides]) -> records
 
