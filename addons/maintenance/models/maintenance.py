61661a3ec53fa884919924095476f119ef16c54c
Yenthe666 <yenthevg@gmail.com>
2019-10-13 10:50:57 +0000

    [FIX] maintenance: don't copy stage
    When duplicating maintenance request, the stage should be reset to the
    first one (which is +/- what the default does).
    
    Fixes odoo/odoo#38558
    
    closes odoo/odoo#38573
    
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- addons/maintenance/models/maintenance.py
+++ addons/maintenance/models/maintenance.py
@@ -291,7 +291,7 @@ class MaintenanceRequest(models.Model):
                                    ondelete='restrict', index=True)
     user_id = fields.Many2one('res.users', string='Technician', track_visibility='onchange', oldname='technician_user_id')
     stage_id = fields.Many2one('maintenance.stage', string='Stage', ondelete='restrict', track_visibility='onchange',
-                               group_expand='_read_group_stage_ids', default=_default_stage)
+                               group_expand='_read_group_stage_ids', default=_default_stage, copy=False)
     priority = fields.Selection([('0', 'Very Low'), ('1', 'Low'), ('2', 'Normal'), ('3', 'High')], string='Priority')
     color = fields.Integer('Color Index')
     close_date = fields.Date('Close Date', help="Date the maintenance was finished. ")
