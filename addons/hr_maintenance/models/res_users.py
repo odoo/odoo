667b682133ffbe80ffd82f69b0f3a2aa2732db05
Lucas Lefèvre <lul@odoo.com>
2019-10-14 08:22:48 +0000

    [FIX] hr_maintenance: Display equipment count of employee
    In the employee form view and in the employee profile, the stat button
    displaying the equipment count is wrong.
    It counts the equipment owned by the user, not equipments assigned to the
    employee.
    
    closes odoo/odoo#38889
    
    X-original-commit: 07bd725c97519a82cc76f3069614ac70b49495a3
    Signed-off-by: lul-odoo <LucasLefevre@users.noreply.github.com>

--- addons/hr_maintenance/models/res_users.py
+++ addons/hr_maintenance/models/res_users.py
@@ -4,13 +4,8 @@ from odoo import api, models, fields
 class Users(models.Model):
     _inherit = 'res.users'
 
-    equipment_ids = fields.One2many('maintenance.equipment', 'owner_user_id')
-    equipment_count = fields.Integer('Equipments', compute='_compute_equipment_count')
-
-    @api.depends('equipment_ids')
-    def _compute_equipment_count(self):
-        for user in self:
-            user.equipment_count = len(user.equipment_ids)
+    equipment_ids = fields.One2many('maintenance.equipment', 'owner_user_id', string="Managed Equipments")
+    equipment_count = fields.Integer(related='employee_id.equipment_count', string="Assigned Equipments")
 
     def __init__(self, pool, cr):
         """ Override of __init__ to add access rights.
@@ -26,4 +21,10 @@ class Users(models.Model):
 class Employee(models.Model):
     _inherit = 'hr.employee'
 
-    equipment_count = fields.Integer(related='user_id.equipment_count')
+    equipment_ids = fields.One2many('maintenance.equipment', 'employee_id')
+    equipment_count = fields.Integer('Equipments', compute='_compute_equipment_count')
+
+    @api.depends('equipment_ids')
+    def _compute_equipment_count(self):
+        for employee in self:
+            employee.equipment_count = len(employee.equipment_ids)
