5d86d1847c8e947e6ab9e58eb3c4d8cb52e3e3b3
Nans Lefebvre <len@odoo.com>
2019-10-15 16:49:25 +0200

    [FIX] fields, test_access_rights: get currency field in sudo
    This commits fixes user duplication.
    
    Suppose there is a monetary field, with a currency_field that is a related.
    When going through model's _create, we do:
    `if field.type in ('one2many', 'many2many'):
        self.env.cache.set(record, field, ())`
    knowing that thse values are false, with the intent to clean them later:
    `for record, field in cachetoclear`
    However, when setting the scalar values, we go through:
    `accessing cache_value = field.convert_to_cache(value, record)`
    In the case of the monetary field, this depends on another field value
    (the currency_field). If it is a related, we can access its value.
    However, at this point, if we check any access rights, we might use the value
    of a relational in cache for which the value is incorrectly set to False.
    
    In the case of the user, this is what happens: it inherits its currency_id
    from partner, as well as its debit_limit which depends on it.
    When the access if checked, company_ids is set to False in cache.
    So when going through the rule 'user rule', which checks that the company_ids
    intersects with the env.companies.ids, the result is always False.
    
    In some way this is essentially hiding the problem, but the true fix is
    probably not feasible in stable.
    
    opw 2086661

--- odoo/addons/test_access_rights/models.py
+++ odoo/addons/test_access_rights/models.py
@@ -51,3 +51,18 @@ class FakeTicket(models.Model):
 
     name = fields.Char()
     message_partner_ids = fields.Many2many(comodel_name='res.partner')
+
+
+class ResPartner(models.Model):
+    """User inherits partner, so we are implicitly adding these fields to User
+       This essentially reproduces the (sad) situation introduced by account.
+    """
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    currency_id = fields.Many2one('res.currency', compute='_get_company_currency', readonly=True)
+    monetary = fields.Monetary()  # implicitly depends on currency_id as currency_field
+
+    def _get_company_currency(self):
+        for partner in self:
+            partner.currency_id = partner.sudo().company_id.currency_id
