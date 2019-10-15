876edcc35b972595dc7b62e993260afe342f858a
uso-odoo <uso@odoo.com>
2019-10-15 11:54:17 +0530

    [FIX] apps: double scrollbar in 'Updates'
    Fixed the double scrollbar issue in 'Apps>Updates'
    
    task-2082253

--- addons/web/static/src/js/apps.js
+++ addons/web/static/src/js/apps.js
@@ -114,7 +114,7 @@ var Apps = Widget.extend({
                 qs.debug = session.debug;
             }
             var u = $.param.querystring(client.origin + "/apps/embed/client", qs);
-            var css = {width: '100%', height: '750px'};
+            var css = {width: '100%', height: '750px', position: 'fixed'};
             self.$ifr = $('<iframe>').attr('src', u);
 
             self.uniq = _.uniqueId('apps');
