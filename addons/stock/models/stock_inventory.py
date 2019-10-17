20459045b0d627d296f6689cb373458a536131ac
Reinaldo Ramos <reinaldo.ramos@arxi.pt>
2019-10-17 17:12:12 +0100

    [IMP] stock: Split _get_inventory_lines_values method to simplify domain customization

--- addons/stock/models/stock_inventory.py
+++ addons/stock/models/stock_inventory.py
@@ -230,11 +230,46 @@ class Inventory(models.Model):
         }
         return action
 
-    def _get_inventory_lines_values(self):
-        # TDE CLEANME: is sql really necessary ? I don't think so
+    def _get_domain_args(self):
+
         locations = self.env['stock.location'].search([('id', 'child_of', [self.location_id.id])])
         domain = ' location_id in %s AND quantity != 0 AND active = TRUE'
         args = (tuple(locations.ids),)
+        Product = self.env['product.product']
+        products_to_filter = self.env['product.product']
+        # case 0: Filter on company
+        if self.company_id:
+            domain += ' AND company_id = %s'
+            args += (self.company_id.id,)
+
+        # case 1: Filter on One owner only or One product for a specific owner
+        if self.partner_id:
+            domain += ' AND owner_id = %s'
+            args += (self.partner_id.id,)
+        # case 2: Filter on One Lot/Serial Number
+        if self.lot_id:
+            domain += ' AND lot_id = %s'
+            args += (self.lot_id.id,)
+        # case 3: Filter on One product
+        if self.product_id:
+            domain += ' AND product_id = %s'
+            args += (self.product_id.id,)
+            products_to_filter |= self.product_id
+        # case 4: Filter on A Pack
+        if self.package_id:
+            domain += ' AND package_id = %s'
+            args += (self.package_id.id,)
+        # case 5: Filter on One product category + Exahausted Products
+        if self.category_id:
+            categ_products = Product.search([('categ_id', 'child_of', self.category_id.id)])
+            domain += ' AND product_id = ANY (%s)'
+            args += (categ_products.ids,)
+            products_to_filter |= categ_products
+        return domain, args
+
+    def _get_inventory_lines_values(self):
+        # TDE CLEANME: is sql really necessary ? I don't think so
+        locations = self.env['stock.location'].search([('id', 'child_of', [self.location_id.id])])
 
         vals = []
         Product = self.env['product.product']
@@ -243,34 +278,7 @@ class Inventory(models.Model):
         # Empty recordset of products to filter
         products_to_filter = self.env['product.product']
 
-        # case 0: Filter on company
-        if self.company_id:
-            domain += ' AND company_id = %s'
-            args += (self.company_id.id,)
-
-        #case 1: Filter on One owner only or One product for a specific owner
-        if self.partner_id:
-            domain += ' AND owner_id = %s'
-            args += (self.partner_id.id,)
-        #case 2: Filter on One Lot/Serial Number
-        if self.lot_id:
-            domain += ' AND lot_id = %s'
-            args += (self.lot_id.id,)
-        #case 3: Filter on One product
-        if self.product_id:
-            domain += ' AND product_id = %s'
-            args += (self.product_id.id,)
-            products_to_filter |= self.product_id
-        #case 4: Filter on A Pack
-        if self.package_id:
-            domain += ' AND package_id = %s'
-            args += (self.package_id.id,)
-        #case 5: Filter on One product category + Exahausted Products
-        if self.category_id:
-            categ_products = Product.search([('categ_id', 'child_of', self.category_id.id)])
-            domain += ' AND product_id = ANY (%s)'
-            args += (categ_products.ids,)
-            products_to_filter |= categ_products
+        domain, args = self._get_domain_args()
 
         self.env.cr.execute("""SELECT product_id, sum(quantity) as product_qty, location_id, lot_id as prod_lot_id, package_id, owner_id as partner_id
             FROM stock_quant
