d3ca227276236d10c33ad7ce6c6747a57874b17d
Colinliz <lizheng02@inspur.com>
2019-10-17 10:31:42 +0000

    [FIX] l10n_cn_small_business: Modify Small And Medium-sized Enterprises COA
    Changes
    - Modify some account according to 'Accounting Standards for 2017
      Small and Medium-sized Enterprises'
    - Add account translation into .po file
    
    closes odoo/odoo#38935
    
    Signed-off-by: Colinliz <lizheng02@inspur.com>
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- addons/l10n_cn_small_business/__manifest__.py
+++ addons/l10n_cn_small_business/__manifest__.py
@@ -5,14 +5,15 @@
 #                         维智众源 oldrev@gmail.com  <states data>
 # Copyright (C) 2012-2012 南京盈通 ccdos@intoerp.com <small business chart>
 # Copyright (C) 2008-now  开阖软件 jeff@osbzr.com    < PM and LTS >
+# Copyright (C) 2019-now  浪潮PS Cloud lizheng02@inspur.com    < PM and LTS >
 
 {
     'name': 'China - Small Business CoA',
-    'version': '1.8',
+    'version': '2.0',
     'category': 'Localization',
-    'author': 'www.openerp-china.org',
-    'maintainer': 'jeff@osbzr.com',
-    'website': 'http://openerp-china.org',
+    'author': 'www.openerp-china.org,lizheng02@inspur.com',
+    'maintainer': 'lizheng02@inspur.com',
+    'website': 'http://www.mypscloud.com',
     'description': """
 
     科目类型\会计科目表模板\增值税\辅助核算类别\管理会计凭证簿\财务会计凭证簿
@@ -20,6 +21,8 @@
     添加中文省份数据
 
     增加小企业会计科目表
+
+    修改小企业会计科目表
 
     """,
     'depends': ['l10n_cn'],
