fc52ed7b193b6510dfabd2434ae441b33da1a7bb
Sébastien Theys <seb@odoo.com>
2019-10-21 18:41:43 +0200

    simplify code of partner_im_status_icon

--- addons/mail/static/src/js/owl/component/partner_im_status_icon.js
+++ addons/mail/static/src/js/owl/component/partner_im_status_icon.js
@@ -15,6 +15,37 @@ class PartnerImStatusIcon extends owl.Component {
             };
         });
     }
+
+    get statusClass() {
+        switch (this.storeProps.partner.im_status) {
+            case 'online':
+                return 'bg-success';
+            case 'away':
+                return 'bg-warning';
+            case 'offline':
+                return 'bg-500';
+            default:
+                return 'bg-500';
+        }
+    }
+
+    get statusTitle() {
+        return _.str.sprintf(this._getStatusTitle(), _.escape(this.storeProps.partner.name));
+    }
+
+    _getStatusTitle() {
+        switch (this.storeProps.partner.im_status) {
+            case 'online':
+                return this.env._t("%s is online");
+            case 'away':
+                return this.env._t("%s is idle");
+            case 'offline':
+                return this.env._t("%s is offline");
+            default:
+                return this.env._t("The status of %s is unknown");
+        }
+    }
+
 }
 
 PartnerImStatusIcon.template = 'mail.component.PartnerImStatusIcon';
