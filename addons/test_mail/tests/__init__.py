8b231f9ec9b1e0c7b74702ae45be706396559985
Thibault Delavallée <tde@odoo.com>
2019-10-21 14:51:32 +0200

    [IMP] test_mail: add new mail composer tests for its internals
    Purpose is to have some low level tests for composer itself allowing to
    ensure its behavior. We add tests notably about default values computation
    in mass mailing mode as well as template_id change and values synchronization
    in composer model.
    
    In a near future composer will probably be improved (use computed fields,
    rewrite part of its logic to improve performances). Having tests done before
    those tasks ensure we notice every change that might happen.
    
    Task ID-2390314
    PR odoo/odoo#62061
    
    X-original-commit: def98d2375a8a906092293f0aa1ce03bf948d2d3

--- addons/test_mail/tests/__init__.py
+++ addons/test_mail/tests/__init__.py
@@ -3,6 +3,7 @@
 from . import test_invite
 from . import test_ir_actions
 from . import test_mail_activity
+from . import test_mail_composer
 from . import test_mail_followers
 from . import test_mail_message
 from . import test_mail_mail
