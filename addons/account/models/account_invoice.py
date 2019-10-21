4be03eaeafabdcc19d95fbd51d9844abe733f2b5
Lucas Perais (lpe) <lpe@odoo.com>
2019-10-21 13:49:05 +0200

    FIX Invoice assign outstanding:
    Do not fill in the amount_currency of aml when it is empty
    at the moment we are assigning a payment (which can be a credit note...)

--- addons/account/models/account_invoice.py
+++ addons/account/models/account_invoice.py
@@ -1095,11 +1095,6 @@ class AccountInvoice(models.Model):
     def assign_outstanding_credit(self, credit_aml_id):
         self.ensure_one()
         credit_aml = self.env['account.move.line'].browse(credit_aml_id)
-        if not credit_aml.currency_id and self.currency_id != self.company_id.currency_id:
-            amount_currency = self.company_id.currency_id._convert(credit_aml.balance, self.currency_id, self.company_id, credit_aml.date or fields.Date.today())
-            credit_aml.with_context(allow_amount_currency=True, check_move_validity=False).write({
-                'amount_currency': amount_currency,
-                'currency_id': self.currency_id.id})
         if credit_aml.payment_id:
             credit_aml.payment_id.write({'invoice_ids': [(4, self.id, None)]})
         return self.register_payment(credit_aml)
