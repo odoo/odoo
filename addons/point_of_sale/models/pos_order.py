a905e3bc644a43951c46279178b498dc31ab8796
jev-odoo <jev@odoo.com>
2019-10-21 11:48:09 +0200

    [FIX] point_of_sale: adapt _create_account_move_line for inheritance
    This commit intend to allow better inheritance in the
    account_move_line_creation_process
    
    The new _prepare_account_move_and_lines method only prepare
    the move and the lines but does not write and post them.
    
    This is done by the former _create_account_move_line method.
    
    Needed to fix pos_sessions with unbalanced journal entries
    caused by round globally option with taxes included in price
    
    It is a backport of https://github.com/odoo/odoo/commit/52d68984459210cc1a1da039707eb47ae937da07
    
    X-original-commit: 99a924ae4358880a7c325679d2cab56ea678477c

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -239,7 +239,7 @@ class PosOrder(models.Model):
         inv_line.update(price_unit=line.price_unit, discount=line.discount, name=inv_name)
         return InvoiceLine.sudo().create(inv_line)
 
-    def _create_account_move_line(self, session=None, move=None):
+    def _prepare_account_move_and_lines(self, session=None, move=None):
         def _flatten_tax_and_children(taxes, group_done=None):
             children = self.env['account.tax']
             if group_done is None:
@@ -460,6 +460,17 @@ class PosOrder(models.Model):
 
         if self and order.company_id.anglo_saxon_accounting:
             add_anglosaxon_lines(grouped_data)
+
+        return {
+            'grouped_data': grouped_data,
+            'move': move,
+        }
+
+    def _create_account_move_line(self, session=None, move=None):
+        vals = self._prepare_account_move_and_lines(session, move)
+
+        grouped_data = vals['grouped_data']
+        move = vals['move']
 
         all_lines = []
         for group_key, group_data in grouped_data.items():
