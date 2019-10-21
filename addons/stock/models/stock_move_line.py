8084f3658c663ce09456638e02c951b55785dba2
Simon Lejeune <sle@openerp.com>
2019-10-21 12:25:08 +0000

    [REF] stock: wording
    This is not related to the batch picking task but was found while
    testing it.
    
    task-2069646

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -15,7 +15,7 @@ class StockMoveLine(models.Model):
     _order = "result_package_id desc, id"
 
     picking_id = fields.Many2one(
-        'stock.picking', 'Stock Picking', auto_join=True,
+        'stock.picking', 'Transfer', auto_join=True,
         check_company=True,
         help='The stock operation where the packing has been made')
     move_id = fields.Many2one(
