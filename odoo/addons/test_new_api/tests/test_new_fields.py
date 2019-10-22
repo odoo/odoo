odoo/odoo

commit 291a0e99d137142bd24addd1054c1357ddd7125a

[FIX] fields: ensure Binary are never recomputed with "bin_size"
The compute methods are always expecting the real value and not the bin_size.

The solution is to always compute with `bin_size=False`, and then manually
compute the `bin_size` and set it on the cache of `bin_size=True`.

PR: #38292

Co-authored-by Sébastien Theys <seb@odoo.com>

odoo/addons/test_new_api/models.py
@@ -517,6 +517,15 @@ class ComputeOnchange(models.Model):
                 record.baz = record.foo
 
 
+class ModelBinary(models.Model):
+    _name = 'test_new_api.model_binary'
+    _description = 'Test Image field'
+
+    binary = fields.Binary()
+    binary_related_store = fields.Binary("Binary Related Store", related='binary', store=True, readonly=False)
+    binary_related_no_store = fields.Binary("Binary Related No Store", related='binary', store=False, readonly=False)
+
+
 class ModelImage(models.Model):
     _name = 'test_new_api.model_image'
     _description = 'Test Image field'

odoo/fields.py
@@ -3,15 +3,13 @@
 
 """ High-level objects for fields. """
 
-from collections import OrderedDict, defaultdict
+from collections import defaultdict
 from datetime import date, datetime, time
-from dateutil.relativedelta import relativedelta
-from functools import partial
 from operator import attrgetter
 import itertools
 import logging
 import base64
-
+import binascii
 import pytz
 
 try:
@@ -28,6 +26,7 @@ from .tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
 from .tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
 from .tools.translate import html_translate, _
 from .tools.mimetypes import guess_mimetype
+from odoo.exceptions import CacheMiss
 
 DATE_LENGTH = len(date.today().strftime(DATE_FORMAT))
 DATETIME_LENGTH = len(datetime.now().strftime(DATETIME_FORMAT))
@@ -1903,6 +1902,32 @@ class Binary(Field):
         if isinstance(value, _BINARY):
             return bytes(value)
         return False if value is None else value
+
+    def compute_value(self, records):
+        bin_size_name = 'bin_size_' + self.name
+        if records.env.context.get('bin_size') or records.env.context.get(bin_size_name):
+            # always compute without bin_size
+            records_no_bin_size = records.with_context(**{'bin_size': False, bin_size_name: False})
+            super().compute_value(records_no_bin_size)
+            # manually update the bin_size cache
+            cache = records.env.cache
+            for record_no_bin_size, record in zip(records_no_bin_size, records):
+                try:
+                    value = cache.get(record_no_bin_size, self)
+                    try:
+                        value = base64.b64decode(value)
+                    except (TypeError, binascii.Error):
+                        pass
+                    try:
+                        value = human_size(len(value))
+                    except (TypeError):
+                        pass
+                    cache_value = self.convert_to_cache(value, record)
+                    cache.set(record, self, cache_value)
+                except CacheMiss:
+                    pass
+        else:
+            super().compute_value(records)
 
     def read(self, records):
         # values are stored in attachments, retrieve them

odoo/addons/test_new_api/tests/test_new_fields.py
@@ -1497,6 +1497,64 @@ class TestFields(common.TransactionCase):
         self.assertEqual(Image.open(io.BytesIO(base64.b64decode(record.image))).size, (2000, 4000))
         self.assertEqual(Image.open(io.BytesIO(base64.b64decode(record.image_256))).size, (128, 256))
 
+        # test bin_size
+        record_bin_size = record.with_context(bin_size=True)
+        self.assertEqual(record_bin_size.image, b'31.54 Kb')
+        self.assertEqual(record_bin_size.image_512, b'1.02 Kb')
+        self.assertEqual(record_bin_size.image_256, b'424.00 bytes')
+
+    def test_95_binary_bin_size(self):
+        binary_value = base64.b64encode(b'content')
+        binary_size = b'7.00 bytes'
+
+        def assertBinaryValue(record, value):
+            for field in ('binary', 'binary_related_store', 'binary_related_no_store'):
+                self.assertEqual(record[field], value)
+
+        # created, flushed, and first read without context
+        record = self.env['test_new_api.model_binary'].create({'binary': binary_value})
+        record.flush()
+        record.invalidate_cache()
+        record_no_bin_size = record.with_context(bin_size=False)
+        record_bin_size = record.with_context(bin_size=True)
+
+        assertBinaryValue(record, binary_value)
+        assertBinaryValue(record_no_bin_size, binary_value)
+        assertBinaryValue(record_bin_size, binary_size)
+
+        # created, flushed, and first read with bin_size=False
+        record_no_bin_size = self.env['test_new_api.model_binary'].with_context(bin_size=False).create({'binary': binary_value})
+        record_no_bin_size.flush()
+        record_no_bin_size.invalidate_cache()
+        record = self.env['test_new_api.model_binary'].browse(record.id)
+        record_bin_size = record.with_context(bin_size=True)
+
+        assertBinaryValue(record_no_bin_size, binary_value)
+        assertBinaryValue(record, binary_value)
+        assertBinaryValue(record_bin_size, binary_size)
+
+        # created, flushed, and first read with bin_size=True
+        record_bin_size = self.env['test_new_api.model_binary'].with_context(bin_size=True).create({'binary': binary_value})
+        record_bin_size.flush()
+        record_bin_size.invalidate_cache()
+        record = self.env['test_new_api.model_binary'].browse(record.id)
+        record_no_bin_size = record.with_context(bin_size=False)
+
+        assertBinaryValue(record_bin_size, binary_size)
+        assertBinaryValue(record_no_bin_size, binary_value)
+        assertBinaryValue(record, binary_value)
+
+        # created without context and flushed with bin_size
+        record = self.env['test_new_api.model_binary'].create({'binary': binary_value})
+        record_no_bin_size = record.with_context(bin_size=False)
+        record_bin_size = record.with_context(bin_size=True)
+        record_bin_size.flush()
+        record_bin_size.invalidate_cache()
+
+        assertBinaryValue(record, binary_value)
+        assertBinaryValue(record_no_bin_size, binary_value)
+        assertBinaryValue(record_bin_size, binary_size)
+
 
 class TestX2many(common.TransactionCase):
     def test_definition_many2many(self):

commit 852ee32e3419c210a3844b0f943af8dd2bbdae88

[FIX] fields: 'bin_size' must have no effect on arbitrary binary field values
This fixes a regression introduced by 291a0e99d137142bd24addd1054c1357ddd7125a.

closes odoo/odoo#39159

Signed-off-by: Raphael Collet (rco) <rco@openerp.com>

odoo/addons/test_new_api/models.py
@@ -525,6 +525,13 @@ class ModelBinary(models.Model):
     binary = fields.Binary()
     binary_related_store = fields.Binary("Binary Related Store", related='binary', store=True, readonly=False)
     binary_related_no_store = fields.Binary("Binary Related No Store", related='binary', store=False, readonly=False)
+    binary_computed = fields.Binary(compute='_compute_binary')
+
+    @api.depends('binary')
+    def _compute_binary(self):
+        # arbitrary value: 'bin_size' must have no effect
+        for record in self:
+            record.binary_computed = [(record.id, bool(record.binary))]
 
 
 class ModelImage(models.Model):

odoo/fields.py
@@ -1923,7 +1923,8 @@ class Binary(Field):
                     except (TypeError, binascii.Error):
                         pass
                     try:
-                        value = human_size(len(value))
+                        if isinstance(value, (bytes, _BINARY)):
+                            value = human_size(len(value))
                     except (TypeError):
                         pass
                     cache_value = self.convert_to_cache(value, record)

odoo/addons/test_new_api/tests/test_new_fields.py
@@ -1598,6 +1598,18 @@ class TestFields(common.TransactionCase):
         assertBinaryValue(record_no_bin_size, binary_value)
         assertBinaryValue(record_bin_size, binary_size)
 
+        # check computed binary field with arbitrary Python value
+        record = self.env['test_new_api.model_binary'].create({})
+        record.flush()
+        record.invalidate_cache()
+        record_no_bin_size = record.with_context(bin_size=False)
+        record_bin_size = record.with_context(bin_size=True)
+
+        expected_value = [(record.id, False)]
+        self.assertEqual(record.binary_computed, expected_value)
+        self.assertEqual(record_no_bin_size.binary_computed, expected_value)
+        self.assertEqual(record_bin_size.binary_computed, expected_value)
+
 
 class TestX2many(common.TransactionCase):
     def test_definition_many2many(self):