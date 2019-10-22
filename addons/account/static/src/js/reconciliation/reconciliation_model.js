odoo/odoo

commit 68d71b8eafa3227269de12ba3c64b9860fbcf63c

[FIX] account: reconciliation widget fetch correct reconciliation models
Have a reconciliation model that applies to some particular journals
And that will match some of those journals' lines
The field journal should be filled too
(this field is only the default journal that a proposition will be filled with)

Open the reconciliation widget on a given journal, within the list of
possible journal (from the account dashboard)

Before this commit there was a crashed. This was caused by the fact that
one of the lines that should be displayed had a reconcile model that was not fetched beforehand

After this commit, there is no crash as all relevant reconcile
models are fetched

Note that, on reconcile models, the field journal_id is just there to prefill
the propositions it will create

corrects commit e5c8071484c883bf78478a39ef2120bcd8f2442d

OPW 2084942

closes odoo/odoo#38726

Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

addons/account/static/src/js/reconciliation/reconciliation_model.js
@@ -382,7 +382,7 @@ var StatementModel = BasicModel.extend({
             domainReconcile.push(['company_id', 'in', context.company_ids]);
         }
         if (context && context.active_model === 'account.journal' && context.active_ids) {
-            domainReconcile.push(['journal_id', 'in', [false].concat(context.active_ids)]);
+            domainReconcile.push(['match_journal_ids', 'in', [false].concat(context.active_ids)]);
         }
         var def_reconcileModel = this._loadReconciliationModel({domainReconcile: domainReconcile});
         var def_account = this._rpc({

addons/account/static/tests/reconciliation_tests.js
@@ -1292,6 +1292,35 @@ QUnit.module('account', {
         clientAction.destroy();
     });
 
+    QUnit.test('Reconciliation fetch correct reconciliation models', function (assert) {
+        assert.expect(1);
+
+        _.extend(this.params.options.context, {
+            active_model: 'account.journal', // On account dashboard, click "Reconcile" on a journal
+            active_ids: [1,2], // Active journals
+            company_ids: [3,4], // Active companies
+        });
+
+        var clientAction = new ReconciliationClientAction.StatementAction(null, this.params.options);
+
+        testUtils.addMockEnvironment(clientAction, {
+            data: this.params.data,
+            mockRPC: function (route, args) {
+                if (args.model === 'account.reconcile.model' && args.method === 'search_read') {
+                    assert.deepEqual(
+                        args.kwargs.domain,
+                        [['company_id', 'in', [3,4]], ['match_journal_ids', 'in', [false, 1, 2]]],
+                        'The domain to get reconcile models should contain the right fields and values'
+                    );
+                }
+                return this._super.apply(this, arguments);
+            }
+        });
+        clientAction.appendTo($('#qunit-fixture'));
+
+        clientAction.destroy();
+    });
+
     QUnit.test('Reconciliation manual', function (assert) {
         assert.expect(13);
 

commit 93411358da3cc5580604a48b22edafea2353df1e

[FIX] account: missformed domain reconciliation model
Commit https://github.com/odoo/odoo/commit/68d71b8eafa3227269de12ba3c64b9860fbcf63c
introduced a wrong domain
Falsy m2m must be explicited and put outside of the "in [ids]" leaf

This commit corrects it

OPW 2090865
OPW 2091449
OPW 2091778
OPW 2091778

closes odoo/odoo#39208

Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

addons/account/static/src/js/reconciliation/reconciliation_model.js
@@ -382,7 +382,9 @@ var StatementModel = BasicModel.extend({
             domainReconcile.push(['company_id', 'in', context.company_ids]);
         }
         if (context && context.active_model === 'account.journal' && context.active_ids) {
-            domainReconcile.push(['match_journal_ids', 'in', [false].concat(context.active_ids)]);
+            domainReconcile.push('|');
+            domainReconcile.push(['match_journal_ids', '=', false]);
+            domainReconcile.push(['match_journal_ids', 'in', context.active_ids]);
         }
         var def_reconcileModel = this._loadReconciliationModel({domainReconcile: domainReconcile});
         var def_account = this._rpc({

addons/account/static/tests/reconciliation_tests.js
@@ -1308,8 +1308,12 @@ QUnit.module('account', {
             mockRPC: function (route, args) {
                 if (args.model === 'account.reconcile.model' && args.method === 'search_read') {
                     assert.deepEqual(
-                        args.kwargs.domain,
-                        [['company_id', 'in', [3,4]], ['match_journal_ids', 'in', [false, 1, 2]]],
+                        args.kwargs.domain, [
+                            ['company_id', 'in', [3,4]],
+                            '|',
+                            ['match_journal_ids', '=', false],
+                            ['match_journal_ids', 'in', [1, 2]],
+                        ],
                         'The domain to get reconcile models should contain the right fields and values'
                     );
                 }