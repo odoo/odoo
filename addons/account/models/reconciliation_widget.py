d8e61d39b37154444ac48afbf7d54aa654719e55
Jorge Pinna Puissant <jpp@odoo.com>
2019-10-24 10:49:00 +0200

    [FIX] account: KeyError when transaction retry on reconciliation
    To reproduce this issue, a transaction retry must be forced:
    — Put a breakpoint in a line of the function process_bank_statement_line
        in addons/account/models/reconciliation_widget.py;
    — Open the bank statement reconciliation;
    — Click in validate;
    — When the code is in pause;
    — Open psql and modify a line of the database that affects the current
        reconciliation, for example you can change the partner_id of the
        table account_bank_statement_line for the line that is modified in
        https://github.com/odoo/odoo/blob/3e2d968c55542426746b287c556b5fb05d59fdcb/addons/account/models/reconciliation_widget.py#L35;
    — Continue the process;
    — This will generate an 'ERROR: could not serialize access due to
        concurrent update' and a transaction retry.
    
    Before this commit, a KeyError is raised, this occurs because
    counterpart_aml_id was deleted from the dictionary in the first passage,
    then when in the second passage triggered by the 'retry transaction',
    counterpart_aml_id doesn't exist any more in the dictionary.
    
    Now, the data is copied and there is no problem when the 'retry
    transaction' is called.
    
    Fixes #23816
    opw-2085822

--- addons/account/models/reconciliation_widget.py
+++ addons/account/models/reconciliation_widget.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import copy
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 from odoo.osv import expression
@@ -31,7 +32,7 @@ class AccountReconciliation(models.AbstractModel):
         AccountMoveLine = self.env['account.move.line']
         ctx = dict(self._context, force_price_include=False)
 
-        for st_line, datum in pycompat.izip(st_lines, data):
+        for st_line, datum in pycompat.izip(st_lines, copy.deepcopy(data)):
             payment_aml_rec = AccountMoveLine.browse(datum.get('payment_aml_ids', []))
 
             for aml_dict in datum.get('counterpart_aml_dicts', []):
