odoo/odoo

commit 7e2b0ebe7996c9fa35f75949b7fc8cd5a58d7c36

[FIX] web_editor, *: fix the theme colorpicker
*: website

Inherited colorpicker were not used due to the context missing the
website_id and the method call being used instead of call_kw.

task-2092386

closes odoo/odoo#39282

Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

addons/website/static/src/js/utils.js
@@ -3,8 +3,18 @@ odoo.define('website.utils', function (require) {
 
 var ajax = require('web.ajax');
 var core = require('web.core');
+var weContext = require('web_editor.context');
 
 var qweb = core.qweb;
+
+//TODO Remove me !
+var weGetContext = weContext.get;
+weContext.get = function (context) {
+    var html = document.documentElement;
+    return _.extend({
+        website_id: html.getAttribute('data-website-id') | 0,
+    }, weGetContext(context), context);
+};
 
 /**
  * Allows to load anchors from a page.

addons/web_editor/static/src/js/editor/rte.summernote.js
@@ -14,7 +14,7 @@ var weWidgets = require('wysiwyg.widgets');
 var QWeb = core.qweb;
 var _t = core._t;
 
-ajax.jsonRpc('/web/dataset/call', 'call', {
+ajax.jsonRpc('/web/dataset/call_kw', 'call', {
     'model': 'ir.ui.view',
     'method': 'read_template',
     'args': ['web_editor.colorpicker'],

commit 508b605949a1f2006444c66dc22fc0bfb89376d0

[FIX] website, *: restore website js loading
* web_editor

Commit https://github.com/odoo/odoo/commit/7e2b0ebe7996c9fa35f75949b7fc8cd5a58d7c36
broke the website JS loading as it added a dependency to a lazy loaded
JS module which was not even always lazy loaded.

A test so that this does not happens anymore will be made in another
commit.

Related to task-2092386

addons/web_editor/static/src/js/base.js
@@ -1,6 +1,8 @@
 odoo.define('web_editor.base', function (require) {
 'use strict';
 
+// TODO this should be re-removed as soon as possible.
+
 var ajax = require('web.ajax');
 var session = require('web.session');
 
@@ -126,10 +128,21 @@ return {
 odoo.define('web_editor.context', function (require) {
 'use strict';
 
+// TODO this should be re-removed as soon as possible.
+
 function getContext(context) {
     var html = document.documentElement;
     return _.extend({
         lang: (html.getAttribute('lang') || 'en_US').replace('-', '_'),
+
+        // Unfortunately this is a mention of 'website' in 'web_editor' as there
+        // was no other way to do it as this was restored in a stable version.
+        // Indeed, the editor is currently using this context at the root of JS
+        // module, so there is no way for website to hook itself before
+        // web_editor uses it (without a risky refactoring of web_editor in
+        // stable). As mentioned above, the editor should not use this context
+        // anymore anyway (this was restored by the saas-12.2 editor revert).
+        'website_id': html.getAttribute('data-website-id') | 0,
     }, context || {});
 }
 function getExtraContext(context) {
@@ -151,6 +164,8 @@ return {
 
 odoo.define('web_editor.ready', function (require) {
 'use strict';
+
+// TODO this should be re-removed as soon as possible.
 
 var base = require('web_editor.base');
 

addons/website/static/src/js/utils.js
@@ -3,18 +3,8 @@ odoo.define('website.utils', function (require) {
 
 var ajax = require('web.ajax');
 var core = require('web.core');
-var weContext = require('web_editor.context');
 
 var qweb = core.qweb;
-
-//TODO Remove me !
-var weGetContext = weContext.get;
-weContext.get = function (context) {
-    var html = document.documentElement;
-    return _.extend({
-        website_id: html.getAttribute('data-website-id') | 0,
-    }, weGetContext(context), context);
-};
 
 /**
  * Allows to load anchors from a page.