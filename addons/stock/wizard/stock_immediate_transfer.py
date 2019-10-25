ff276c6484b138982f185ec9c832f2dbf25baaef
Simon Lejeune <sle@openerp.com>
2019-10-25 14:41:30 +0000

    [REF] stock: immediate and backorder wizard multi
    followup of rev [0]
    
    If these wizards are called on multiple pickings, display the list of
    the pickings that could be impacted and allow to select which one should
    be impacted.
    
    We also adapt the sanity checks at the start of `button_validte` in
    order to specify the concerned pickings if needed. We do not enable the
    multi behavior for batch at the moment, so it's only enabled for the
    validate multi in the list view.
    
    [0] 6ab4b0d4967490ed1ee0559ec296da6637705125
    
    task-2069646
    
    closes odoo/odoo#41475
    
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- addons/stock/wizard/stock_immediate_transfer.py
+++ addons/stock/wizard/stock_immediate_transfer.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models, _
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError
+
+
+class StockImmediateTransferLine(models.TransientModel):
+    _name = 'stock.immediate.transfer.line'
+    _description = 'Immediate Transfer Line'
+
+    immediate_transfer_id = fields.Many2one('stock.immediate.transfer', 'Immediate Transfer', required=True)
+    picking_id = fields.Many2one('stock.picking', 'Transfer', required=True)
+    to_immediate = fields.Boolean('To Process')
 
 
 class StockImmediateTransfer(models.TransientModel):
@@ -10,9 +19,31 @@ class StockImmediateTransfer(models.TransientModel):
     _description = 'Immediate Transfer'
 
     pick_ids = fields.Many2many('stock.picking', 'stock_picking_transfer_rel')
+    show_transfers = fields.Boolean()
+    immediate_transfer_line_ids = fields.One2many(
+        'stock.immediate.transfer.line',
+        'immediate_transfer_id',
+        string="Immediate Transfer Lines")
+
+    @api.model
+    def default_get(self, fields):
+        res = super().default_get(fields)
+        if 'immediate_transfer_line_ids' in fields:
+            if self.env.context.get('default_pick_ids'):
+                res['pick_ids'] = self.env.context['default_pick_ids']
+                res['immediate_transfer_line_ids'] = [(0, 0, {'to_immediate': True, 'picking_id': pick_id[1]}) for pick_id in res['pick_ids']]
+        return res
 
     def process(self):
-        for picking in self.pick_ids:
+        pickings_to_do = self.env['stock.picking']
+        pickings_not_to_do = self.env['stock.picking']
+        for line in self.immediate_transfer_line_ids:
+            if line.to_immediate is True:
+                pickings_to_do |= line.picking_id
+            else:
+                pickings_not_to_do |= line.picking_id
+
+        for picking in pickings_to_do:
             # If still in draft => confirm and assign
             if picking.state == 'draft':
                 picking.action_confirm()
@@ -26,15 +57,8 @@ class StockImmediateTransfer(models.TransientModel):
 
         pickings_to_validate = self.env.context.get('button_validate_picking_ids')
         if pickings_to_validate:
-            return self.env['stock.picking'].browse(pickings_to_validate).with_context(skip_immediate=True).button_validate()
-        return True
-
-    def process_no_immediate(self):
-        # Remove `self.pick_ids` from `button_validate_picking_ids` and call `button_validate` with
-        # the subset (if any).
-        pickings_to_validate = self.env['stock.picking'].browse(self.env.context.get('button_validate_picking_ids'))
-        pickings_to_validate = pickings_to_validate - self.pick_ids
-        if pickings_to_validate:
+            pickings_to_validate = self.env['stock.picking'].browse(pickings_to_validate)
+            pickings_to_validate = pickings_to_validate - pickings_not_to_do
             return pickings_to_validate.with_context(skip_immediate=True).button_validate()
         return True
 
