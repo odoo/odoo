5633a5bb1b5a2cf7faaf3c6653f453045e5e4136
William Henrotin <whe@odoo.com>
2019-10-28 11:58:27 +0100

    [FIX] stock: split move line in extra move
    _action_done() creates extra stock move when quantity_done is superior
    to initial demand. This extra move should be, after confirmation, either
    merge into the original stock_move, either stay extra but the move lines
    still linked to the first one should be shared accordingly to the quantity
    done. The fix 561b3461a020021d911999cb41154f158b069a1d adapted the code
    for MRP to split the move lines but neither those cases are applied in
    MRP because the extra move has no picking_id field.
    
    condition :
      ```if merge_into_self and extra_move.picking_id:```
    will therefore be false as well as
      ```if not merge_into_self:```
    
    To be sure the two extra move management cases are complementary, this
    Commit keeps make sure the conditions are mutualy exclusive.
    
    opw: 2087864

--- addons/mrp/tests/test_order.py
+++ addons/mrp/tests/test_order.py
@@ -351,6 +351,33 @@ class TestMrpOrder(TestMrpCommon):
         self.assertEqual(production.routing_id.id, False, 'The routing field should be empty on the mo')
         self.assertEqual(production.move_raw_ids[0].location_id.id, self.warehouse_1.wh_input_stock_loc_id.id, 'Raw moves start location should have altered.')
 
+    def test_split_move_line(self):
+        """ Consume more component quantity than the initial demand.
+        It should create extra move and share the quantity between the two stock
+        moves """
+        mo, _, p_final, p1, p2 = self.generate_mo(qty_base_1=10, qty_final=1, qty_base_2=1)
+        mo.action_assign()
+        produce_form = Form(self.env['mrp.product.produce'].with_context({
+            'active_id': mo.id,
+            'active_ids': [mo.id],
+        }))
+        for i in range(len(produce_form.produce_line_ids)):
+            with produce_form.produce_line_ids.edit(i) as line:
+                line.qty_done += 1
+        product_produce = produce_form.save()
+        product_produce.do_produce()
+        self.assertEqual(len(mo.move_raw_ids), 2)
+        self.assertEqual(len(mo.move_raw_ids.mapped('move_line_ids')), 2)
+        self.assertEqual(mo.move_raw_ids[0].move_line_ids.mapped('qty_done'), [2])
+        self.assertEqual(mo.move_raw_ids[1].move_line_ids.mapped('qty_done'), [11])
+        self.assertEqual(mo.move_raw_ids[0].quantity_done, 2)
+        self.assertEqual(mo.move_raw_ids[1].quantity_done, 11)
+        mo.button_mark_done()
+        self.assertEqual(len(mo.move_raw_ids), 4)
+        self.assertEqual(len(mo.move_raw_ids.mapped('move_line_ids')), 4)
+        self.assertEqual(mo.move_raw_ids.mapped('quantity_done'), [1, 10, 1, 1])
+        self.assertEqual(mo.move_raw_ids.mapped('move_line_ids.qty_done'), [1, 10, 1, 1])
+
     def test_multiple_post_inventory(self):
         """ Check the consumed quants of the produced quants when intermediate calls to `post_inventory` during a MO."""
 
