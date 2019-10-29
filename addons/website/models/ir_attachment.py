db215257ce24ea56e7a937d61deae13747d9a679
Moisés López <moylop260@vauxoo.com>
2019-10-29 18:21:35 +0000

    [REF] website: Speedup website (when a theme is installed)
    After install a website theme the webpage is slower
    
    It is because a new query is executed to get website attachments (1)
    
    This query is so slow and it is executed each time the website is opened
    
    Running the following query:
        EXPLAIN (ANALYZE, VERBOSE, BUFFERS)
        SELECT id FROM ir_attachment WHERE active=True AND key = 'SOMETHING' AND website=1
    
    The result before index:
        Seq Scan on ir_attachment ...
        Execution Time: 21.320 ms
    
    The result after index (key, website):
        Bitmap Heap Scan on ir_attachment ...
        Execution Time: 0.111 ms
    
    192x faster
    
    The website must be as faster as possible so this index make sense
    
    (1) https://github.com/odoo/odoo/blob/d00a832952a0b9d2276a1f057ee10152b0c7f5bc/addons/website/models/ir_http.py#L332
    
    X-original-commit: 61f49f52f3e655e91209790ebff99992c5b68a0c

--- addons/website/models/ir_attachment.py
+++ addons/website/models/ir_attachment.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import logging
-from odoo import fields, models, api
+from odoo import fields, models, api, tools
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
 _logger = logging.getLogger(__name__)
@@ -34,3 +34,19 @@ class Attachment(models.Model):
         extra_domain = (extra_domain or []) + website.website_domain()
         order = ('website_id, %s' % order) if order else 'website_id'
         return super(Attachment, self).get_serve_attachment(url, extra_domain, extra_fields, order)
+<<<<<<< HEAD
+=======
+
+    @api.model
+    def get_attachment_by_key(self, key, extra_domain=None, order=None):
+        website = self.env['website'].get_current_website()
+        extra_domain = (extra_domain or []) + website.website_domain()
+        order = ('website_id, %s' % order) if order else 'website_id'
+        return super(Attachment, self).get_attachment_by_key(key, extra_domain, order)
+
+    def init(self):
+        res = super(Attachment, self).init()
+        # ir_http._xmlid_to_obj is using this index for multi-website
+        tools.create_index(self._cr, 'ir_attachment_key_website_idx', self._table, ['key', 'website_id'])
+        return res
+>>>>>>> b187c3ec9f0... temp
