3c90d09be35036183ea23b7eec937004f4c4ec41
Yannick Tivisse <yti@odoo.com>
2019-11-04 16:47:03 +0100

    [IMP] survey: Adapt tests to work with/without demo data

--- addons/survey/static/tests/tours/certification_failure.js
+++ addons/survey/static/tests/tours/certification_failure.js
@@ -82,7 +82,7 @@ var lastSteps = [{
 
 tour.register('test_certification_failure', {
     test: true,
-    url: '/survey/start/4ead4bc8-b8f2-4760-a682-1fde8ddb95ac'
+    url: '/survey/start/4ead4bc8-b8f2-4760-a682-1fde8daaaaac'
 }, [].concat(failSteps, retrySteps, failSteps, lastSteps));
 
 });
