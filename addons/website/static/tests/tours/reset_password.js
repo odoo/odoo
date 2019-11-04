091175bb301abc8ee3299f1755a3a99ede2c9368
Yannick Tivisse <yti@odoo.com>
2019-11-04 16:50:30 +0100

    [IMP] website: Adapt tests to work with/without demo data

--- addons/website/static/tests/tours/reset_password.js
+++ addons/website/static/tests/tours/reset_password.js
@@ -44,14 +44,14 @@ tour.register('website_reset_password', {
             });
             // Set a website on "Admin" partner to test the URL of the website.
             var def3 = rpc.query({
-                model: 'res.partner',
-                method: 'name_search',
-                kwargs: {'name': 'Mitchell Admin', 'args': [['company_id', '=', 1]]},
-            }).then(function (res) {
+                model: 'ir.model.data',
+                method: 'xmlid_to_res_id',
+                args: ['base.partner_admin'],
+            }).then(function (res_id) {
                 return rpc.query({
                     'model': 'res.partner',
                     'method': 'write',
-                    'args': [[res[0][0]], {
+                    'args': [[res_id], {
                         'website_id': 1,
                     }],
                 });
