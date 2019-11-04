ce096917a4b186fbe567e03a992e04bcb3698e1f
Adrian Torres <adt@odoo.com>
2019-11-04 08:49:47 +0000

    [IMP] tests: partially backport classCleanups from CPython 3.8
    This commit partially backports bpo-24412, which allows the definition of
    class cleanups (addClassCleanup) and module cleanups (omitted),
    similar to instance cleanups (addCleanup).
    
    This is useful for tests that override unittest's setUpClass and
    could crash during its execution: If this happens, it is possible that a
    bunch of crap is left in the database or even worse, the cursor becomes
    completely fucked; Thanks to the addClassCleanup, we can undo the damage
    done by the setUpClass.
    
    Another benefit is that it is called unconditionally after tearDownClass
    is called, so it can also be called as a replacement and/or safer
    tearDownClass.

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -20,6 +20,7 @@ import unittest
 import psutil
 import werkzeug.serving
 from werkzeug.debug import DebuggedApplication
+from odoo.tests.common import OdooSuite
 
 if os.name == 'posix':
     # Unix only for workers
@@ -1130,7 +1131,7 @@ def load_test_file_py(registry, test_file):
             for mod_mod in get_test_modules(mod):
                 mod_path, _ = os.path.splitext(getattr(mod_mod, '__file__', ''))
                 if test_path == mod_path:
-                    suite = unittest.TestSuite()
+                    suite = OdooSuite()
                     for t in unittest.TestLoader().loadTestsFromModule(mod_mod):
                         suite.addTest(t)
                     _logger.log(logging.INFO, 'running tests %s.', mod_mod.__name__)
