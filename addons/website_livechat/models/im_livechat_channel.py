190dab8144b5dd46bb2492093d0d8d3abafb19b4
David Beguin <dbe@odoo.com>
2019-11-06 16:27:36 +0000

    [IMP] website_livechat : add various tests on livechat and visitors
    Adds Python tests and javascripts tours on livechat (website and visitor
    integration). Because breaking livechat every two days in rush periods
    (or even not) is getting quite annoying.
    
    Those tests are testing :
    
    - The client side flow (open livechat, send messages,
      send rating and close the livechat session)
    - The channel and message author naming, visitor page view history
    - Chat request flow (complete chat request flow, open empty operator's
      chat request and cancel due to visitor's new chat session)
    
    Task ID : 2079087
    PR #40052
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_livechat/models/im_livechat_channel.py
+++ addons/website_livechat/models/im_livechat_channel.py
@@ -15,6 +15,7 @@ class ImLivechatChannel(models.Model):
             if not user_id:
                 mail_channel_vals['anonymous_name'] = visitor_sudo.display_name + (' (%s)' % visitor_sudo.country_id.name if visitor_sudo.country_id else '')
             # As chat requested by the visitor, delete the chat requested by an operator if any to avoid conflicts between two flows
+            # TODO DBE : Move this into the proper method (open or init mail channel)
             chat_request_channel = self.env['mail.channel'].sudo().search([('livechat_visitor_id', '=', visitor_sudo.id), ('livechat_active', '=', True)])
             for mail_channel in chat_request_channel:
                 mail_channel._close_livechat_session(cancel=True, speaking_with=operator.name)
