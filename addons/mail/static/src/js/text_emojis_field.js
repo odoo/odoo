c42cd134c4e46ea4618fad25f640c813e918cf03
Debauche Stéphane <std@odoo.com>
2019-11-08 09:51:21 +0000

    [MOV] mail, social: move emojis mixin and field from social to mail
    PURPOSE
    
    Allow to use the text field with integrated emojis support in other apps
    notably mass mailing
    
    SPECIFICATIONS
    
    Move emojis mixin and FieldTextEmojis field definition from social to mail
    application. Rename emojis namespace accordingly.
    
    LINKS
    
    Task #2117238
    PR #40045
    Enterprise PR odoo/enterprise#6630

---
+++ addons/mail/static/src/js/text_emojis_field.js
@@ -0,0 +1,129 @@
+odoo.define('mail.text_emojis_field', function (require) {
+"use strict";
+
+var basicFields = require('web.basic_fields');
+var core = require('web.core');
+var emojis = require('mail.emojis');
+var registry = require('web.field_registry');
+var MailEmojisMixin = require('mail.emoji_mixin');
+var _onEmojiClickMixin = MailEmojisMixin._onEmojiClick;
+var QWeb = core.qweb;
+
+var FieldTextEmojis = basicFields.FieldText.extend(MailEmojisMixin, {
+    /**
+     * @override
+     * @private
+     */
+    init: function () {
+        this._super.apply(this, arguments);
+        this._updatePreview =_.throttle(this._updatePreview, 1000, {leading: false});
+        this.emojis = emojis;
+    },
+
+    /**
+     * This will add an emoji button that shows the emojis selection dropdown.
+     *
+     * We use 'on_attach_callback' because we need the element to be attached to the form first.
+     * That's because the $emojisIcon element needs to be rendered outside of this $el
+     * (which is an textarea, that can't 'contain' any other elements).
+     *
+     * @override
+     */
+    on_attach_callback: function () {
+        var self = this;
+
+        if (!this.$emojisIcon) {
+            this.$emojisIcon = $(QWeb.render(
+                'mail.EmojisDropdown', {
+                    widget: this,
+                    emojisDropdownId: 'emojis_dropdown'
+                }
+            ));
+            this.$emojisIcon.find('.o_mail_emoji').on('click', function (ev) {
+                self._onEmojiClick(ev);
+                self._isDirty = true;
+            });
+            this.$el.after(this.$emojisIcon);
+        }
+
+        if (this.mode === 'edit') {
+            this.$emojisIcon.show();
+        } else {
+            this.$emojisIcon.hide();
+        }
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     * @private
+     */
+    _render: function () {
+        this._super.apply(this, arguments);
+
+        if (this.mode !== 'edit') {
+            this.$el.html(this._formatText(this.$el.text()));
+        }
+    },
+
+    /**
+     * Overridden because we need to add the Emoji to the input AND trigger
+     * the 'change' event to refresh the various post previews.
+     *
+     * @override
+     * @private
+     */
+    _onEmojiClick: function () {
+        _onEmojiClickMixin.apply(this, arguments);
+        this.$input.trigger('change');
+    },
+
+    /**
+     *
+     * By default, the 'change' event is only triggered when the textarea is blurred.
+     *
+     * We override this method because we want to update the various post previews while
+     * the user is typing his message (and not only on blur).
+     *
+     * @override
+     * @private
+     */
+    _onKeydown: function () {
+        this._super.apply(this, arguments);
+        this._updatePreview();
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Used by MailEmojisMixin, check its document for more info.
+     *
+     * @param {$.Element} $emoji
+     * @private
+     */
+    _getTargetTextArea($emoji) {
+        return this.$el;
+    },
+
+    /**
+     * Triggers the 'change' event to refresh the various post previews.
+     * This method is throttled to run at most once every second.
+     * (to avoid spamming the server while the user is typing his message)
+     *
+     * @private
+     */
+    _updatePreview: function () {
+        this.$input.trigger('change');
+    }
+};
+
+registry.add('text_emojis', FieldTextEmojis);
+
+return FieldTextEmojis;
+
+});
