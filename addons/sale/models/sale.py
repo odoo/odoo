827074e0110be72762e5044ea586f4308b688298
Victor Feyens <vfe@odoo.com>
2019-11-12 17:49:35 +0100

    [IMP] sale: do not set pricelist on create if not set.

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -464,13 +464,12 @@ class SaleOrder(models.Model):
                 seq_date = fields.Datetime.context_timestamp(self, fields.Datetime.to_datetime(vals['date_order']))
             vals['name'] = self.env['ir.sequence'].next_by_code('sale.order', sequence_date=seq_date) or _('New')
 
-        # Makes sure partner_invoice_id', 'partner_shipping_id' and 'pricelist_id' are defined
-        if any(f not in vals for f in ['partner_invoice_id', 'partner_shipping_id', 'pricelist_id']):
+        # Makes sure partner_invoice_id', 'partner_shipping_id' are defined
+        if any(f not in vals for f in ['partner_invoice_id', 'partner_shipping_id']):
             partner = self.env['res.partner'].browse(vals.get('partner_id'))
             addr = partner.address_get(['delivery', 'invoice'])
             vals['partner_invoice_id'] = vals.setdefault('partner_invoice_id', addr['invoice'])
             vals['partner_shipping_id'] = vals.setdefault('partner_shipping_id', addr['delivery'])
-            vals['pricelist_id'] = vals.setdefault('pricelist_id', partner.property_product_pricelist.id)
         result = super(SaleOrder, self).create(vals)
         return result
 
