84a35ff0bf25659184a3beea814b1648c266cf30
Benjamin Vray <bvr@odoo.com>
2019-11-13 15:49:10 +0100

    [IMP] website: add templates for footer
    Also remove all t-fields from footers to have only static contents in
    footers. For social links, a controller is added so that a "static url"
    /website/social/xxx always redirect to the correct set URL for the given
    xxx social network.
    
    Part of https://github.com/odoo/odoo/pull/38950
    task-2087641
    
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/website/models/theme_models.py
+++ addons/website/models/theme_models.py
@@ -162,6 +162,7 @@ class Theme(models.AbstractModel):
                 'navbar-font-number': 'null',
                 'buttons-font-number': 'null',
                 'header-template': 'null',
+                'footer-template': 'null',
             }
         )
 
@@ -169,6 +170,13 @@ class Theme(models.AbstractModel):
         self.disable_view('website.template_header_hamburger')
         self.disable_view('website.template_header_hamburger_left')
         self.disable_view('website.template_header_navbar_text_center')
+
+        # Reinitialize footer templates
+        self.disable_view('website.template_footer_logo_about_us_below')
+        self.disable_view('website.template_footer_links_address_logo')
+        self.disable_view('website.template_footer_name_logo_links_about_us')
+        self.disable_view('website.template_footer_logo_only')
+        self.disable_view('website.template_footer_address_logo')
 
         # Call specific theme post copy
         theme_post_copy = '_%s_post_copy' % mod.name
