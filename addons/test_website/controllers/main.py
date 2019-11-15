de25a3ea6a12593b28914bff6813003a5f6118b5
Jeremy Kersten <jke@odoo.com>
2019-11-15 08:19:43 +0000

    [FIX] website: match get/post in is_multilang_url
    Old heuristic is not more True:
    Force to check method to POST. Odoo uses methods : ['POST'] and ['GET', 'POST']
    
    We have some controller that only allow 'GET' method, so we need to check GET
    also when we try to know if an url is multilang or not.
    
    This commit fix case where a controller '/test' only allow GET and you were in
    another language that the default, in this case, the rendered url in qweb was
    /get instead of /<lang>/get.
    
    Closes #37223
    
    closes odoo/odoo#40398
    
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- addons/test_website/controllers/main.py
+++ addons/test_website/controllers/main.py
@@ -11,3 +11,19 @@ class WebsiteTest(Home):
     @http.route('/test_view', type='http', auth="public", website=True, sitemap=False)
     def test_view(self, **kw):
         return request.render('test_website.test_view')
+
+    @http.route(['/get'], type='http', auth="public", methods=['GET'], website=True)
+    def get_method(self, **kw):
+        return request.make_response('get')
+
+    @http.route(['/post'], type='http', auth="public", methods=['POST'], website=True)
+    def post_method(self, **kw):
+        return request.make_response('post')
+
+    @http.route(['/get_post'], type='http', auth="public", methods=['GET', 'POST'], website=True)
+    def get_post_method(self, **kw):
+        return request.make_response('get_post')
+
+    @http.route(['/get_post_nomultilang'], type='http', auth="public", methods=['GET', 'POST'], website=True, multilang=False)
+    def get_post_method_no_multilang(self, **kw):
+        return request.make_response('get_post_nomultilang')
