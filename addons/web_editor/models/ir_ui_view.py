0ec61d1347457fb5b0188042353bf3f7d6cc7cc3
Nicolas Lempereur <nle@odoo.com>
2019-11-15 13:38:07 +0000

    [FIX] web_editor: update attribute of edition container
    In most use case, when we edit the website we have a situation such as:
    
    ```
    <div id="wrap" branding-attributes="...">
        <section class="mycontent">hi!</section>
    </div>
    ```
    
    When we modify a part, we replace all child nodes of the branded
    element.
    
    But if we had more complex content such as:
    
    ```
    <div id="wrap">
        <section class="mycontent" branding-attributes="...">ho!</section>
        <t t-call-assets="web.assets_common" t-js="false" t-css="false"/>
    </div>
    ```
    
    we have a t-call inside the div#wrap, so branding is distributed to
    child that could have attribute modified (eg. changing background).
    
    Then if `<section/>` node is saved, the possibly modified attributes
    are lost.
    
    Without the change, the added test fails with:
    
     '<div class="nice">hoi</div>' not found in '...<div>hoi</div>...' :
     saved element attributes are saved excluding branding ones
    
    opw-2122947
    closes #40345
    
    closes odoo/odoo#40830
    
    X-original-commit: 71b20a24c40ae9daa10076ab0fd656b043819cff
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- addons/web_editor/models/ir_ui_view.py
+++ addons/web_editor/models/ir_ui_view.py
@@ -10,6 +10,8 @@ from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
+EDITING_ATTRIBUTES = ['data-oe-model', 'data-oe-id', 'data-oe-field', 'data-oe-xpath', 'data-note-id']
+
 
 class IrUiView(models.Model):
     _inherit = 'ir.ui.view'
@@ -132,6 +134,11 @@ class IrUiView(models.Model):
         # Note: after a standard edition, the tail *must not* be replaced
         if replace_tail:
             root.tail = replacement.tail
+        # update attributes
+        root.attrib.clear()
+        root.attrib.update(replacement.attrib)
+        for attribute in EDITING_ATTRIBUTES:
+            root.attrib.pop(attribute, None)
         # replace all children
         del root[:]
         for child in replacement:
