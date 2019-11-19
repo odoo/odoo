9be6900868a46e0f5b708074126dacc05d37cc64
William Henrotin <whe@odoo.com>
2019-11-19 15:06:28 +0000

    [FIX] stock: remove dynamic domain on return wizard

--- addons/mrp_subcontracting/wizard/stock_picking_return.py
+++ addons/mrp_subcontracting/wizard/stock_picking_return.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
-from odoo.osv.expression import OR
+from odoo import api, models, fields
+
 
 class ReturnPicking(models.TransientModel):
     _inherit = 'stock.return.picking'
 
+    subcontract_location_id = fields.Many2one('stock.location', compute='_compute_subcontract_location_id')
+
+    @api.depends('picking_id')
+    def _compute_subcontract_location_id(self):
+        for record in self:
+            record.subcontract_location_id = record.picking_id.partner_id.with_company(
+                record.picking_id.company_id
+            ).property_stock_subcontractor
+
     @api.onchange('picking_id')
     def _onchange_picking_id(self):
         res = super(ReturnPicking, self)._onchange_picking_id()
-        if not any(self.product_return_moves.filtered(lambda r: r.quantity > 0).move_id.mapped('is_subcontract')):
-            return res
-        subcontract_location = self.picking_id.partner_id.with_company(self.picking_id.company_id).property_stock_subcontractor
-        self.location_id = subcontract_location.id
-        domain_location = OR([
-            ['|', ('id', '=', self.original_location_id.id), ('return_location', '=', True)],
-            [('id', '=', subcontract_location.id)]
-        ])
-        if not res:
-            res = {'domain': {'location_id': domain_location}}
-        else:
-            res['domain'] = {'location_id': domain_location}
+        if any(self.product_return_moves.filtered(lambda r: r.quantity > 0).move_id.mapped('is_subcontract')):
+            self.location_id = self.picking_id.partner_id.with_company(self.picking_id.company_id).property_stock_subcontractor
         return res
 
     def _prepare_move_default_values(self, return_line, new_picking):
