7059b177ee3108afe951dc279d3db4054eba4115
Lucas Lefèvre <lul@odoo.com>
2019-11-19 09:25:24 +0000

    [IMP] project: Following a private project no longer required
    =======
    Purpose
    =======
    
    Currently for a user to be allowed to see a private project they must follow the
    project. This means that they will be added as a follower of all new tasks in the
    project which causes them to receive 1000000 notifications, especially if there are a
    lot of tasks in a project.
    
    ==============
    Specifications
    ==============
    
    1) Add an Employee m2m field on project next to 'Invited Employees'
        - only visible if the 'Invited Employees' option is selected
        - the project should only be visible to employees selected there (regardless if they
          are followers or not)
    2) Add an Employee m2m field on tasks below the 'Email cc' one in debug mode
        - only visible if the 'Invited Employees' option is selected on the related project
        - should be pre-filled with what is set on the project
        - the task should be visible to employees selected there (regardless if they are
          followers or not)
    3) Add a Portal users m2m field on project next to 'Portal users and all employees'
        - only visible if the 'Portal users and all employees' option is selected
        - the project should only be visible to portal users selected there (regardless if
          they are followers or not)
        - rename the option into 'Invited portal users and all employees'
    4) Add a Portal users m2m field on tasks below the 'Email cc' one in debug mode
        - only visible if the 'Portal users and all employees' option is selected
        - should be pre-filled with what is set on the project
        - the task should be visible to portal users selected there (regardless if they are
          followers or not)
    5) Following a project/task should not grant the user/employee the ability to see the
       project/task
    
    Task 2031527
    
    closes odoo/odoo#40505
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_timesheet/models/hr_timesheet.py
+++ addons/hr_timesheet/models/hr_timesheet.py
@@ -25,7 +25,7 @@ class AccountAnalyticLine(models.Model):
         domain = [('allow_timesheets', '=', True)]
         if not self.user_has_groups('hr_timesheet.group_timesheet_manager'):
             return expression.AND([domain,
-                ['|', ('privacy_visibility', '!=', 'followers'), ('message_partner_ids', 'in', [self.env.user.partner_id.id])]
+                ['|', ('privacy_visibility', '!=', 'followers'), ('allowed_internal_user_ids', 'in', self.env.user.ids)]
             ])
         return domain
 
@@ -36,7 +36,7 @@ class AccountAnalyticLine(models.Model):
 
     def _domain_task_id(self):
         if not self.user_has_groups('hr_timesheet.group_hr_timesheet_approver'):
-            return ['|', ('privacy_visibility', '!=', 'followers'), ('message_partner_ids', 'in', [self.env.user.partner_id.id])]
+            return ['|', ('privacy_visibility', '!=', 'followers'), ('allowed_user_ids', 'in', self.env.user.ids)]
         return []
 
     task_id = fields.Many2one(
