e4909327446ce674d56a2935bc47bb93e02a0ecf
William Henrotin <whe@odoo.com>
2019-11-19 16:06:28 +0100

    [FIX] stock: remove dynamic domain on return wizard

--- addons/mrp_subcontracting/__manifest__.py
+++ addons/mrp_subcontracting/__manifest__.py
@@ -17,6 +17,7 @@
         'views/stock_move_views.xml',
         'views/stock_picking_views.xml',
         'views/supplier_info_views.xml',
+        'wizard/stock_picking_return_views.xml',
     ],
     'demo': [
         'data/mrp_subcontracting_demo.xml',
