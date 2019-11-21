b1acfee6343a464f22bbcd8e0f2d422ab186f1fe
mightyjol (jhk) <jhk@odoo.com>
2019-11-21 15:43:45 +0100

    [FIX]base_import: added warning dialog on import error
    previous behavior:
    non-admin user would see a parsing error when trying to import
    .xlsx files due to wrong mimetype deduction (considered svg)
    
    current behavior:
    user sees a clear error with instructions to avoid further
    issues (convert to csv)
    
    opw-2124608

--- addons/base_import/controllers/main.py
+++ addons/base_import/controllers/main.py
@@ -3,10 +3,10 @@
 
 import json
 
-from odoo import http
+from odoo import http, _
 from odoo.http import request
 from odoo.tools import misc
-
+from odoo.exceptions import UserError
 
 class ImportController(http.Controller):
 
@@ -14,10 +14,18 @@ class ImportController(http.Controller):
     def set_file(self, file, import_id, jsonp='callback'):
         import_id = int(import_id)
 
-        written = request.env['base_import.import'].browse(import_id).write({
-            'file': file.read(),
-            'file_name': file.filename,
-            'file_type': file.content_type,
-        })
+        try:
+            written = request.env['base_import.import'].browse(import_id).write({
+                'file': file.read(),
+                'file_name': file.filename,
+                'file_type': file.content_type,
+            })
+        except UserError as e:
+            title = _("User Error")
+            content = _(".xlsx are not allowed unless you have administrator rights. You can convert your file in .xls or .csv to avoid this issue.")
+            return """odoo.define(function (require) {
+                var web_client = require('web.web_client');
+                web_client.do_warn("%s", "%s", true);
+                });""" % (title, content)
 
         return 'window.top.%s(%s)' % (misc.html_escape(jsonp), json.dumps({'result': written}))
