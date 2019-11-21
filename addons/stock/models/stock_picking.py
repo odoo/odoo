7ead452b59ca68bcd4992ec283bd7eb588a28974
Simon Lejeune <sle@openerp.com>
2019-11-21 15:53:23 +0100

    [REF] stock: always display lot_id on return pickings

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -393,11 +393,13 @@ class Picking(models.Model):
             else:
                 picking.show_operations = False
 
-    @api.depends('move_line_ids', 'picking_type_id.use_create_lots', 'picking_type_id.use_existing_lots', 'state')
+    @api.depends('move_line_ids', 'move_lines', 'picking_type_id.use_create_lots', 'picking_type_id.use_existing_lots', 'state')
     def _compute_show_lots_text(self):
         group_production_lot_enabled = self.user_has_groups('stock.group_production_lot')
         for picking in self:
-            if not picking.move_line_ids:
+            if picking.move_lines.origin_returned_move_id:
+                picking.show_lots_text = False
+            elif not picking.move_line_ids:
                 picking.show_lots_text = False
             elif group_production_lot_enabled and picking.picking_type_id.use_create_lots \
                     and not picking.picking_type_id.use_existing_lots and picking.state != 'done':
