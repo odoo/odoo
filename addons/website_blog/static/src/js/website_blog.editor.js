a339e5d9d3bf6e9f4c6012843c463258a8cc7c90
Nicolas Lempereur <nle@odoo.com>
2019-11-22 16:37:22 +0000

    [FIX] website_blog: translate on blog no remove cover
    When you modify a translation on the blog post cover, we would save the
    blog post cover (which is intended in non-translation-editing mode) but
    the cover data had not been set (since it is not needed) so when saving
    empty data all cover properties are removed and the cover seems removed.
    
    opw-2126191
    closes #40658
    
    closes odoo/odoo#40733
    
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- addons/website_blog/static/src/js/website_blog.editor.js
+++ addons/website_blog/static/src/js/website_blog.editor.js
@@ -85,8 +85,12 @@ rte.Class.include({
     /**
      * @override
      */
-    _saveElement: function ($el, context) {
+    _saveElement: function ($el, context, withLang) {
         var defs = [this._super.apply(this, arguments)];
+        // do not save cover in translation mode
+        if (withLang) {
+            return defs[0];
+        }
         // TODO the o_dirty class is not put on the right element for blog cover
         // edition. For some strange reason, it was forcly put (even if not
         // dirty) in <= saas-16 but this is not the case anymore.
