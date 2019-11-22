e43a56fb67219e867942a27868bc14aa73cee202
William Henrotin <whe@odoo.com>
2019-11-22 15:36:38 +0000

    [FIX] mrp: split test
    This commit split test_product_produce_6 in two tests. The
    original used a `with self.assertRaise` in the middle of the
    test. The issue of this command is that it do not rollback the
    transaction. An error is catched but a part of code is commited.
    Continuing the test after the `with self.assertRaise` will be
    done on non wanted data
    
    closes odoo/odoo#40956
    
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>

--- addons/mrp/tests/test_order.py
+++ addons/mrp/tests/test_order.py
@@ -746,8 +746,7 @@ class TestMrpOrder(TestMrpCommon):
         """ Plan 5 finished products, reserve and produce 3. Post the current production.
         Simulate an unlock and edit and, on the opened moves, set the consumed quantity
         to 3. Now, try to update the quantity to produce to 3. It should fail since there
-        are consumed quantities. Unlock and edit, remove the consumed quantities and
-        update the quantity to produce to 3."""
+        are consumed quantities"""
         self.stock_location = self.env.ref('stock.stock_location_stock')
         mo, bom, p_final, p1, p2 = self.generate_mo()
         self.assertEqual(len(mo), 1, 'MO should have been created')
@@ -778,7 +777,34 @@ class TestMrpOrder(TestMrpCommon):
         with self.assertRaises(UserError):
             update_quantity_wizard.change_prod_qty()
 
-        mo.move_raw_ids.filtered(lambda m: m.state != 'done')[0].quantity_done = 0
+    def test_product_produce_6bis(self):
+        """ Plan 5 finished products, reserve and produce 3. Post the current production.
+        update the quantity to produce to 3. It should unlink the extra reserved moves"""
+        self.stock_location = self.env.ref('stock.stock_location_stock')
+        mo, bom, p_final, p1, p2 = self.generate_mo()
+        self.assertEqual(len(mo), 1, 'MO should have been created')
+
+        self.env['stock.quant']._update_available_quantity(p1, self.stock_location, 20)
+
+        self.env['stock.quant']._update_available_quantity(p2, self.stock_location, 5)
+        mo.action_assign()
+
+        produce_wizard = self.env['mrp.product.produce'].with_context({
+            'active_id': mo.id,
+            'active_ids': [mo.id],
+        }).create({
+            'product_qty': 3.0,
+        })
+        produce_wizard._onchange_product_qty()
+        produce_wizard.do_produce()
+
+        mo.post_inventory()
+        self.assertEqual(len(mo.move_raw_ids), 4)
+
+        update_quantity_wizard = self.env['change.production.qty'].create({
+            'mo_id': mo.id,
+            'product_qty': 3,
+        })
         update_quantity_wizard.change_prod_qty()
 
         self.assertEqual(len(mo.move_raw_ids), 2)
