86b2a332464b60480b88e7252eac9c346965e2bb
Barad Mahendra <mba@openerp.com>
2019-11-25 11:24:45 +0000

    [IMP] (hr, sale)_timesheet, pad(_project): improve project general UX
    Purpose fo the task is to improve the UX of timesheet, project and task.
    
    Done the chages for below point:
      - improve the stage demo data to unified the project stage
      - on the task timesheet 'sub-tasks hours spent' should be clickable
    and on click, it will display the list of subtask timesheet.
      - display in warning tasks for which Remaining Hours < 0 if Planned Hours > 0 in task list view
      - improve the settings of collabrative pad and planning and project form view
    
    Task-2129052
    
    closes odoo/odoo#41092
    
    Closes: #41092
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/project/models/res_config_settings.py
+++ addons/project/models/res_config_settings.py
@@ -7,7 +7,7 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    module_project_forecast = fields.Boolean(string="Forecasts")
+    module_project_forecast = fields.Boolean(string="Planning")
     module_hr_timesheet = fields.Boolean(string="Task Logs")
     group_subtask_project = fields.Boolean("Sub-tasks", implied_group="project.group_subtask_project")
     group_project_rating = fields.Boolean("Use Rating on Project", implied_group='project.group_project_rating')
