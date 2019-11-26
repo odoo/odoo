6618063d24947894c111cd02434706a3e7bc7f7f
Nans Lefebvre <len@odoo.com>
2019-11-26 15:57:04 +0100

    [TEST] account: add performance test
    A simple test to easily check performance of the reconciliation.
    
    opw 2121587
    
    Co-authored by: François Honoré <francois.honore@acsone.eu>

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -27,3 +27,4 @@ from . import test_reconciliation_matching_rules
 from . import test_account_onboarding
 from . import test_portal_attachment
 from . import test_tax_report
+from . import test_reconciliation_heavy_load
