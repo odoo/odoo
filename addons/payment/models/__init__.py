65dc9472ddd6ded466cf472aa0b18685df40e44f
Jeremy Kersten <jke@odoo.com>
2019-11-26 15:07:57 +0000

    [FIX] website: need to load payment translation too.
    Closes #40826
    
    X-original-commit: 5397cedd0be3067aa226dbe3dde054054004434f

--- addons/payment/models/__init__.py
+++ addons/payment/models/__init__.py
@@ -6,4 +6,7 @@ from . import res_partner
 from . import account_payment
 from . import chart_template
 from . import ir_http
+<<<<<<< HEAD
 from . import res_company
+=======
+>>>>>>> 8f3304ce601... temp
--- addons/payment/models/ir_http.py
+++ addons/payment/models/ir_http.py
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+=======
+# coding: utf-8
+>>>>>>> 8f3304ce601... temp
 from odoo import models
 from odoo.osv import expression
 
@@ -11,4 +15,8 @@ class IrHttp(models.AbstractModel):
     @classmethod
     def _get_translation_frontend_modules_domain(cls):
         domain = super(IrHttp, cls)._get_translation_frontend_modules_domain()
+<<<<<<< HEAD
         return expression.OR([domain, [('name', '=', 'payment')]])
+=======
+        return expression.OR([domain, [('name', '=ilike', 'payment%')]])
+>>>>>>> 8f3304ce601... temp
