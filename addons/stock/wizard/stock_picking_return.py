f64c5fdf69dc85f1680f68e6018a4a2bd577a098
Simon Lejeune <sle@openerp.com>
2019-11-26 16:19:53 +0100

    pedantic

--- addons/stock/wizard/stock_picking_return.py
+++ addons/stock/wizard/stock_picking_return.py
@@ -82,10 +82,20 @@ class ReturnPicking(models.TransientModel):
             self.location_id = location_id
 
     def _get_returnable_quantity(self, stock_move):
+        """Return the quantities of `stock_move`  still returnable.
+
+        When computing the quantities to return, this method handles the existing returns, returns
+        of returns, and so on.
+
+        :param stock_move: `stock.move` record to return
+        :return: a dict of the quantities of `stock_move` stille returnable, indexed by
+            `stock.production.lot` and `stock.quant.package`
+        :rtype: dict
+        """
         moves = stock_move
         return_moves = stock_move.returned_move_ids
         while return_moves:
-            moves += return_moves
+            moves |= return_moves
             return_moves = return_moves.returned_move_ids
         move_lines = moves.filtered(lambda m: m.state != 'cancel' and not m.scrapped).move_line_ids
 
@@ -94,12 +104,12 @@ class ReturnPicking(models.TransientModel):
         move_lines_out = move_lines - move_lines_in
 
         qty_per_lot_and_package = defaultdict(lambda: 0)
-        for ml in move_lines_out:
-            qty = ml.product_uom_id._compute_quantity(ml.qty_done, ml.product_id.uom_id)
-            qty_per_lot_and_package[(ml.lot_id, ml.package_id)] -= qty
-        for ml in move_lines_in:
-            qty = ml.product_uom_id._compute_quantity(ml.qty_done, ml.product_id.uom_id)
-            qty_per_lot_and_package[(ml.lot_id, ml.result_package_id)] += qty
+        for move_line in move_lines_out:
+            qty = move_line.product_uom_id._compute_quantity(move_line.qty_done, move_line.product_id.uom_id)
+            qty_per_lot_and_package[(move_line.lot_id, move_line.package_id)] -= qty
+        for move_line in move_lines_in:
+            qty = move_line.product_uom_id._compute_quantity(move_line.qty_done, move_line.product_id.uom_id)
+            qty_per_lot_and_package[(move_line.lot_id, move_line.result_package_id)] += qty
         return qty_per_lot_and_package
 
     def _prepare_stock_return_picking_line_vals_from_move(self, stock_move):
