edf04f317c2f1abcaed5deb178777cb5b40d0112
Vishnu Thuletiya <vtu@odoo.com>
2019-11-27 06:11:46 +0000

    [IMP] project: improve the project kanban and task breadcrumb
    Purpose of the task is, when some one remove the default filter
    at that time kanban view is really a mess so when go though the
    project always apply the domain of current project instead
    of filter.
    
    The breadcrumb should be the name of the project instead of name
    of the action(tasks) so pass display_name from method to change the
    breadcrum when view the task from kanban view.
    
    closes odoo/odoo#40922
    
    Task: 2124377
    Closes: #40922
    Related: odoo/enterprise#6903
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -362,11 +362,10 @@ class Project(models.Model):
         not_fav_projects.write({'favorite_user_ids': [(4, self.env.uid)]})
         favorite_projects.write({'favorite_user_ids': [(3, self.env.uid)]})
 
-    def open_tasks(self):
-        ctx = dict(self._context)
-        ctx.update({'search_default_project_id': self.id})
-        action = self.env['ir.actions.act_window'].for_xml_id('project', 'act_project_project_2_project_task_all')
-        return dict(action, context=ctx)
+    def action_view_tasks(self):
+        action = self.with_context(active_id=self.id, active_ids=self.ids).env.ref('project.act_project_project_2_project_task_all').read()[0]
+        action['display_name'] = self.name
+        return action
 
     def action_view_account_analytic_line(self):
         """ return the action to see all the analytic lines of the project's analytic account """
