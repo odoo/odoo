5c793e018c58d1fed62770848ff55614a0929667
Kevin Baptiste <kba@odoo.com>
2019-11-28 08:54:00 +0100

    [FIX] pad_project: prevent crash if no default project_id
    Task creation was crashing when there was no project selected in the
    kanban's quickcreate window.
    
    TaskID: 2146564

--- addons/pad_project/models/project.py
+++ addons/pad_project/models/project.py
@@ -15,7 +15,7 @@ class ProjectTask(models.Model):
     @api.model
     def create(self, vals):
         # When using quick create, the project_id is in the context, not in the vals
-        project_id = vals.get('project_id', False) or self.default_get(['project_id'])['project_id']
+        project_id = vals.get('project_id', False) or self.default_get(['project_id']).get('project_id', False)
         if not self.env['project.project'].browse(project_id).use_pads:
             self = self.with_context(pad_no_create=True)
         return super(ProjectTask, self).create(vals)
