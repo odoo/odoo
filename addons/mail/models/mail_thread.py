0fb70f3d1b110039c61885a87d101d7673fc28d5
Thibault Delavallée <tde@odoo.com>
2019-11-28 13:37:09 +0100

    [FIX] mail: remove constraint on domain when checking aliases and bounce
    Purpose of this commit is to lessen side effects of 3aa26ed078f46345ffff4cd7136309ccfc600f28 (forward
    port of e40f3ccad627af5f74be4fe9f73c201447cb6eb1 ) by removing the check on domain when checking
    recipients for matching alias / bounce / catchall.
    
    Indeed there are existing deployments where people do email forwarding without
    having strong assertions on email domain. In that case alias configuration
    may be broken which is not acceptable. Limit check of domains is technically
    True but its side effect may break valid use case, including email forwarding.
    
    In this commit we therefore remove the constraint on domain check when
    computing email_to and all_recipients localparts. Other part of the original
    fix (multi recipients check for bounce and catchall is kept.

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -997,11 +997,9 @@ class MailThread(models.AbstractModel):
         Alias, dest_aliases = self.env['mail.alias'], self.env['mail.alias']
         catchall_alias = self.env['ir.config_parameter'].sudo().get_param("mail.catchall.alias")
         bounce_alias = self.env['ir.config_parameter'].sudo().get_param("mail.bounce.alias")
-        alias_domain = self.env['ir.config_parameter'].sudo().get_param("mail.catchall.domain")
         fallback_model = model
 
         # get email.message.Message variables for future processing
-        local_hostname = socket.gethostname()
         message_id = message.get('Message-Id')
 
         # compute references to find if message is a reply to an existing thread
@@ -1017,7 +1015,6 @@ class MailThread(models.AbstractModel):
         email_to_localparts = [
             e.split('@', 1)[0].lower()
             for e in (tools.email_split(email_to) or [''])
-            if not alias_domain or e.endswith('@%s' % alias_domain)
         ]
 
         # Delivered-To is a safe bet in most modern MTAs, but we have to fallback on To + Cc values
@@ -1031,7 +1028,6 @@ class MailThread(models.AbstractModel):
         rcpt_tos_localparts = [
             e.split('@')[0].lower()
             for e in tools.email_split(rcpt_tos)
-            if not alias_domain or e.endswith('@%s' % alias_domain)
         ]
 
         # 0. Verify whether this is a bounced email and use it to collect bounce data and update notifications for customers
