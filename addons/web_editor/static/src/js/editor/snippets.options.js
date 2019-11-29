odoo/odoo

commit ff1c35513aecef5fbc30f7c2f3834450e719714e

[IMP] web: update to fontawesome 4.7.0 to 5.11.2
FontAwesome 5 introduced new names for some icons as described on
https://fontawesome.com/how-to-use/on-the-web/setup/upgrading-from-version-4#name-changes

This commit replaces the old names to the new ones.

closes odoo/odoo#35826

Taskid: 2050241
Related: odoo/enterprise#5180
Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

addons/web_editor/static/src/scss/wysiwyg_snippets.scss
@@ -227,7 +227,8 @@ body.editor_enable.editor_has_snippets {
                 width: 1em;
                 right: 0.5em;
                 text-align: center;
-                font-family: FontAwesome;
+                font-family: 'Font Awesome 5 Free';
+                font-weight: 900;
             }
         }
 
@@ -515,7 +516,7 @@ body.editor_enable.editor_has_snippets {
                     display: flex;
                     justify-content: center;
                     align-items: center;
-                    font-family: FontAwesome !important;
+                    font-family: 'Font Awesome 5 Free' !important;
                     color: $o-we-color-success;
                 }
 
@@ -679,7 +680,7 @@ body.editor_enable.editor_has_snippets {
                     border: solid 1px darken($o-we-handles-accent-color, 10%);
                     line-height: $o-we-handles-btn-size - 2;
                     font-size: 14px;
-                    font-family: FontAwesome;
+                    font-family: 'Font Awesome 5 Free';
                     background-color: darken($o-we-handles-accent-color, 20%);
                     color: white;
                 }
@@ -696,12 +697,14 @@ body.editor_enable.editor_has_snippets {
 
                 &.w:after,
                 &.e:after {
-                    content: "\f07e";
+                    content: "\f337";
+                    font-weight: 900;
                 }
 
                 &.s:after,
                 &.n:after {
-                    content: "\f07d";
+                    content: "\f338";
+                    font-weight: 900;
                 }
 
                 &.o_handle_start {
@@ -709,11 +712,13 @@ body.editor_enable.editor_has_snippets {
                     &.w:after,
                     &.e:after {
                         content: '\f061';
+                        font-weight: 900;
                     }
 
                     &.n:after,
                     &.s:after {
                         content: '\f063';
+                        font-weight: 900;
                     }
                 }
 
@@ -722,11 +727,13 @@ body.editor_enable.editor_has_snippets {
                     &.w:after,
                     &.e:after {
                         content: '\f060';
+                        font-weight: 900;
                     }
 
                     &.n:after,
                     &.s:after {
                         content: '\f062';
+                        font-weight: 900;
                     }
                 }
 
@@ -771,7 +778,7 @@ body.editor_enable.editor_has_snippets {
                 &::before, &::after {
                     content: '\f141';
                     display: block;
-                    font-family: FontAwesome;
+                    font-family: 'Font Awesome 5 Free';
                     font-size: 0.8em;
                     line-height: 0.35em;
                 }

addons/website/static/src/js/editor/snippets.options.js
@@ -1678,7 +1678,7 @@ options.registry.anchor = options.Class.extend({
             buttons.push({
                 text: _t("Remove"),
                 classes: 'btn-link ml-auto',
-                icon: 'fa-trash',
+                icon: 'fa-trash-alt',
                 close: true,
                 click: function () {
                     self._setAnchorName();

commit 0b5f1fbead827363f70deb8359805af94cb3eb94

[FIX] web_editor: restore fa icons used in editor following lib update
Commit https://github.com/odoo/odoo/commit/ff1c35513aecef5fbc30f7c2f3834450e719714e
broke some editor icons.

closes odoo/odoo#41097

Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

addons/web_editor/static/src/js/editor/snippets.options.js
@@ -1294,7 +1294,7 @@ registry.background = SnippetOption.extend({
         editBgEl.dataset.chooseImage = 'true';
         editBgEl.dataset.noPreview = 'true';
         const iconEl = document.createElement('i');
-        iconEl.classList.add('fa', 'fa-fw', 'fa-pencil-square-o');
+        iconEl.classList.add('fa', 'fa-fw', 'fa-edit');
         this.editBgTextEl = document.createElement('span');
         editBgEl.appendChild(this.editBgTextEl);
         editBgEl.appendChild(iconEl);

addons/web_editor/static/src/scss/wysiwyg_snippets.scss
@@ -189,7 +189,7 @@ body.editor_enable.editor_has_snippets {
             white-space: nowrap;
             text-align: left;
 
-            &.fa {
+            &.fa, &.fas, &.fab, &.far {
                 display: inline-block;
                 margin: 0 0 0 0.3em;
                 padding: 0;
@@ -311,7 +311,7 @@ body.editor_enable.editor_has_snippets {
                             margin-left: 0.5em;
                         }
                     }
-                    > .fa {
+                    > .fa, > .fas, > .fab, > .far {
                         flex: 0 0 auto;
                     }
                 }
@@ -413,7 +413,7 @@ body.editor_enable.editor_has_snippets {
                 display: flex;
                 align-items: center;
 
-                > .fa {
+                > .fa, > .fas, > .fab, > .far {
                     margin-left: auto;
                 }
             }
@@ -517,6 +517,7 @@ body.editor_enable.editor_has_snippets {
                     justify-content: center;
                     align-items: center;
                     font-family: 'Font Awesome 5 Free' !important;
+                    font-weight: 900;
                     color: $o-we-color-success;
                 }
 
@@ -681,6 +682,7 @@ body.editor_enable.editor_has_snippets {
                     line-height: $o-we-handles-btn-size - 2;
                     font-size: 14px;
                     font-family: 'Font Awesome 5 Free';
+                    font-weight: 900;
                     background-color: darken($o-we-handles-accent-color, 20%);
                     color: white;
                 }
@@ -779,6 +781,7 @@ body.editor_enable.editor_has_snippets {
                     content: '\f141';
                     display: block;
                     font-family: 'Font Awesome 5 Free';
+                    font-weight: 900;
                     font-size: 0.8em;
                     line-height: 0.35em;
                 }