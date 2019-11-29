91aa6cf5628af7ad60d85f39a26165b1027827a0
Parth Choksi <pch@odoo.com>
2019-11-29 12:18:47 +0000

    [IMP] web: better positionning of daterangepicker
    Before this commit, the daterangepicker always opened below the input,
    even when there was not enough space to display it. Moreover, when the
    user scrolled while the picker was opened, it wasn't stucked to the
    input, so the picker was displayed at a random position after the
    scroll.
    
    With this commit, we compute the available space above the picker
    when opening it, and if there is enough space, we display it above,
    otherwise, we display it below. Moreover, we automatically close
    the picker when the user scrolls.
    
    task-2117229
    
    closes odoo/odoo#42161
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Co-authored-by: Mohammed Shekha <msh@odoo.com>

--- addons/web/static/src/js/libs/daterangepicker.js
+++ addons/web/static/src/js/libs/daterangepicker.js
@@ -11,4 +11,14 @@ daterangepicker.prototype.clickDate = function (ev) {
     }
 };
 
+/**
+ * Override to open up or down based on top/bottom space in window.
+ */
+const moveFunction = daterangepicker.prototype.move;
+daterangepicker.prototype.move = function () {
+    const offset = this.element.offset();
+    this.drops = this.container.height() < offset.top ? 'up' : 'down';
+    moveFunction.apply(this, arguments);
+};
+
 });
