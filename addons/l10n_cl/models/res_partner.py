a45e9759fcff8cbb3ef7e1ecbaf5ed6a969a2045
Daniel Blanco <daniel@blancomartin.cl>
2019-12-02 12:34:52 +0000

    [FIX] l10n_cl: Several fixes and improvements for l10n_cl [FIX] l10n_cl: format RUT/VAT as used in Chile (99999999-X) [FIX] l10n_cl: prevent errors with domain of available documents in invoice view. [FIX] l10n_cl: allow to select document types when partner is empty [ADD] l10n_cl: add ILA taxes (purchase and sale) and assign account to taxes [ADD] l10n_cl: add fiscal position exempt for purchases and sales [IMP] l10n_cl: configure accounts used in inventory valuation [FIX] l10n_cl prevent to put unneeded sales documents in sales journal [FIX] l10n_cl limit revision of document types and partner constrains only for sale and purchase journals [FIX] l10n_cl: add extra validation to know wether there is a latam_document_type_code selected [ADD] l10n_cl: add document type '801' Orden de compra [WIP] add tag to costo de ventas account [FIX] l10n_cl: fiscal position templates: fix supermarket tax [FIX] l10n_cl: change order for boletas [WIP] l10n_cl: fix the compute document types method [FIX] l10n_cl: prevent expected singleton error (checked in tests) in post [I18N] l10n_cl: add new translations [I18N] l10n_cl: reduce size of taxes names and abbreviation for invoices (probably more work to add here) [FIX] l10n_cl: remove countries from accounts [FIX] l10n_cl: remove recalc of domain when journal changes [FIX] l10n_cl: remove sale constraint
    X-original-commit: d5c830702bf8e9c716415b6c4fed1f73911bad12

--- addons/l10n_cl/models/res_partner.py
+++ addons/l10n_cl/models/res_partner.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import stdnum
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
@@ -21,3 +22,34 @@ class ResPartner(models.Model):
              '2 - Fees Receipt Issuer (Applies to suppliers who issue fees receipt)\n'
              '3 - End consumer (only receipts)\n'
              '4 - Foreigner')
+
+    def _format_vat_cl(self, values):
+        identification_types = [self.env.ref('l10n_latam_base.it_vat').id, self.env.ref('l10n_cl.it_RUT').id,
+                                self.env.ref('l10n_cl.it_RUN').id]
+        partner_country_is_chile = (values.get('country_id') == self.env.ref('base.cl').id) or (
+                    values.get('l10n_latam_identification_type_id') and
+                    self.env['l10n_latam.identification.type'].browse(
+                        values.get('l10n_latam_identification_type_id')).country_id == self.env.ref('base.cl'))
+        if partner_country_is_chile and \
+                values.get('l10n_latam_identification_type_id') in identification_types and values.get('vat'):
+            return stdnum.util.get_cc_module('cl', 'vat').format(values['vat']).replace('.', '').replace(
+                'CL', '').upper()
+        else:
+            return values['vat']
+
+    @api.model
+    def create(self, values):
+        if values.get('vat'):
+            values['vat'] = self._format_vat_cl(values)
+        return super().create(values)
+
+    def write(self, values):
+        for record in self:
+            vat_values = {
+                'vat': values.get('vat', record.vat),
+                'l10n_latam_identification_type_id': values.get(
+                    'l10n_latam_identification_type_id', record.l10n_latam_identification_type_id.id),
+                'country_id': values.get('country_id', record.country_id.id)
+            }
+            values['vat'] = self._format_vat_cl(vat_values)
+        return super().write(values)
