10abb8d0dd6329019e871a7d0aa454691df3cd5e
Xavier Morel <xmo@odoo.com>
2019-12-04 12:42:18 +0000

    [IMP] core: reporting of browser logs / errors during setup
    Sink handling of JS logging, exceptions and websocket timeouts so
    calls other than _wait_code_ok handle them somewhat properly: the
    issue fixed by odoo/odoo#41231 passed because it occurred during
    module loading, which happens during initial page loading (browser_js
    > navigate_to > _websocket_wait_event), which ignored logs (and
    exceptions though here it's a console.error log), and as a result
    reported no failure (and would simply miss that specific test as well
    as every test following it).
    
    Also since ChromeBrowser treats console.error as an exception,
    important messages should be logged atomically. Merge two consecutive
    console.error into a single one at the loading of modules so we don't
    just get an exception "error while loading foo.bar" without any of the
    useful details.
    
    That ChromeBrowser treats console.error as exception is also why the
    new method gets a flag (to suppress this behaviour): in the case of
    two console.error, upon encountering the first it's treated as an
    error so we try to take a screenshot, which goes through the messages
    in order to get the screenshot response, which encounters the second
    console.error, which gets treated as an exception, which hides the
    first error.
    
    Instead, screenshotting (and more generally _websocket_wait_id) should
    treat console.error as a regular logging call, probably.
    
    Also run JS tests in debug=assets for easier debugging (ha!) and
    improve formatting of exception object when receiving an exception:
    * if we can get a description on an `exception` remote object just
      print that, it's formatted to show the exception type, message &
      traceback
    * otherwise format the garbage that is an "ExceptionDetails" object

--- odoo/addons/base/tests/test_http_case.py
+++ odoo/addons/base/tests/test_http_case.py
@@ -14,16 +14,16 @@ class TestHttpCase(HttpCase):
             with patch('odoo.tests.common.ChromeBrowser.take_screenshot', return_value=None):
                 self.browser_js(url_path='about:blank', code=code)
         # second line must contains error message
-        self.assertEqual(error_catcher.exception.args[0].split('\n', 1)[1], "test error message")
+        self.assertEqual(error_catcher.exception.args[0].splitlines()[1], "test error message")
 
     def test_console_error_object(self):
         with self.assertRaises(AssertionError) as error_catcher:
-            code = "console.error(TypeError('test error ' + 'message'))"
+            code = "console.error(TypeError('test error message'))"
             with patch('odoo.tests.common.ChromeBrowser.take_screenshot', return_value=None):
                 self.browser_js(url_path='about:blank', code=code)
         # second line must contains error message
-        self.assertEqual(error_catcher.exception.args[0].split('\n', 1)[1],
-        'TypeError: test error message\n    at <anonymous>:1:15')
+        self.assertEqual(error_catcher.exception.args[0].splitlines()[1:3],
+        ['TypeError: test error message', '    at <anonymous>:1:15'])
 
     def test_console_log_object(self):
         logger = logging.getLogger('odoo')
@@ -36,10 +36,10 @@ class TestHttpCase(HttpCase):
             self.browser_js(url_path='about:blank', code=code)
         console_log_count = 0
         for log in log_catcher.output:
-            if 'console log' in log:
-                text = log.split('console log: ', 1)[1]
+            if '.browser:' in log:
+                text = log.split('.browser:', 1)[1]
                 if text == 'test successful':
                     continue
-                self.assertEqual(log.split('console log: ', 1)[1], "Object\n{custom:Object, value:1, description:'dummy'}")
+                self.assertEqual(text, "Object(custom=Object, value=1, description='dummy')")
                 console_log_count +=1
         self.assertEqual(console_log_count, 1)
