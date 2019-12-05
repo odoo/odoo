8260a93548b44ed561fb5b79213b51a453d8a44e
qsm-odoo <qsm@odoo.com>
2019-12-05 14:27:59 +0000

    [REF] web_editor: review user value widgets scss
    Part of https://github.com/odoo/odoo/pull/41402

--- addons/website_blog/static/src/js/tours/website_blog.js
+++ addons/website_blog/static/src/js/tours/website_blog.js
@@ -23,7 +23,7 @@ odoo.define("website_blog.tour", function (require) {
         position: "top",
         run: "text",
     }, {
-        trigger: "we-button:containsExact(" + _t("Change Cover") + "):visible",
+        trigger: "we-button:containsExactText(" + _t("Change Cover") + ")",
         extra_trigger: "#wrap div[data-oe-expression=\"blog_post.name\"]:not(:containsExact(\"\"))",
         content: _t("Set a blog post <b>cover</b>."),
         position: "right",
