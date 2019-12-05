26c3eb0553a40a3fafc9f04c55d60e931d116e99
Josse Colpaert <jco@odoo.com>
2019-12-05 21:16:13 +0000

    [REF] account: added method for processing extra fields in refund wizard
    If we add an extra field in the refund wizard, e.g.
    there was no good method to inherit in order to put it on
    the refund before the refund was posted in case of the
    cancel or modify case.
    
    That is why we put this code in a separate method, so
    we can still modify the refund with the information from
    the wizard before being posted.  (as it is e.g. sent through
    the api when posted)
    
    closes odoo/odoo#41478
    
    Related: odoo/enterprise#7026
    Signed-off-by: Quentin De Paoli (qdp) <qdp@openerp.com>

--- addons/account/wizard/account_invoice_refund.py
+++ addons/account/wizard/account_invoice_refund.py
@@ -36,6 +36,17 @@ class AccountInvoiceRefund(models.TransientModel):
         else:
             self.refund_only = False
 
+    def _get_refund(self, inv, mode):
+        self.ensure_one()
+        if inv.state in ['draft', 'cancel']:
+            raise UserError(_('Cannot create credit note for the draft/cancelled invoice.'))
+        if inv.reconciled and mode in ('cancel', 'modify'):
+            raise UserError(_(
+                'Cannot create a credit note for the invoice which is already reconciled, invoice should be unreconciled first, then only you can add credit note for this invoice.'))
+
+        date = self.date or False
+        description = self.description or inv.name
+        return inv.refund(self.date_invoice, date, description, inv.journal_id.id)
 
     @api.multi
     def compute_refund(self, mode='refund'):
@@ -50,15 +61,7 @@ class AccountInvoiceRefund(models.TransientModel):
             date = False
             description = False
             for inv in inv_obj.browse(context.get('active_ids')):
-                if inv.state in ['draft', 'cancel']:
-                    raise UserError(_('Cannot create credit note for the draft/cancelled invoice.'))
-                if inv.reconciled and mode in ('cancel', 'modify'):
-                    raise UserError(_('Cannot create a credit note for the invoice which is already reconciled, invoice should be unreconciled first, then only you can add credit note for this invoice.'))
-
-                date = form.date or False
-                description = form.description or inv.name
-                refund = inv.refund(form.date_invoice, date, description, inv.journal_id.id)
-
+                refund = form._get_refund(inv, mode)
                 created_inv.append(refund.id)
                 if mode in ('cancel', 'modify'):
                     movelines = inv.move_id.line_ids
