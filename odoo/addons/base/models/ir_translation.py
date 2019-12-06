cf7182209e647d301e311327ce3f5d93e85b29e0
Nans Lefebvre <len@odoo.com>
2019-12-06 13:05:48 +0000

    [FIX] ir_translation: correct rights check in _set_ids
    Suppose you have a record rA of model A, and a record rB of model B
    which has a related B.v on field A.x, that is stored and translatable.
    Suppose also that user U can write on A, but not on B because of ACLs.
    
    Let U write on rA.x. Then it recomputes the value of rB.v.
    Since this directly calls the _write, we check the record rules but not
    the ACLs, so U wrote rA.x and rB.v successfully.
    
    Now let U be in another language L.
    This means that we go trough translations, which calls _set_ids.
    If the translation already exists, then the update is done in SQL so no
    access rights are checked.
    However if translations do not exist, the translations are created via
    the ORM and thus check both record rules and ACLs.
    Therefore the operation is forbidden, since by hypothesis U cannot write
    on B.
    
    We can skip the check in this case, since we come from either a create
    or a write, so the ACLs should already be checked on the original models
    as intended (in the case of B, not fully).
    Furthermore it removes the incoherency with the update case.
    
    opw 2145738
    
    closes odoo/odoo#41508
    
    X-original-commit: e6bf027362afb8fd1db19a44989a5df01ece82e5
    Signed-off-by: Nans Lefebvre (len) <len@odoo.com>

--- odoo/addons/base/models/ir_translation.py
+++ odoo/addons/base/models/ir_translation.py
@@ -324,7 +324,7 @@ class IrTranslation(models.Model):
 
         # create missing translations
         for res_id in set(ids) - set(existing_ids):
-            self.create({
+            self.sudo().create({
                 'lang': lang,
                 'type': tt,
                 'name': name,
