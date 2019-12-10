d05d508c187e78ae40cdf5b5a1dd207f0c83dbea
Kevin Baptiste <kba@odoo.com>
2019-12-10 15:18:54 +0100

    [FIX] fields.py: correctly identify svg file
    Before this commit, files in open document format were wrongly
    identified as svg, thus preventing non-system user from uploading them.
    
    TaskID: 2154029

--- odoo/fields.py
+++ odoo/fields.py
@@ -1810,7 +1810,7 @@ class Binary(Field):
             return None
         # Detect if the binary content is an SVG for restricting its upload
         # only to system users.
-        if value[:1] == b'P':  # Fast detection of first 6 bits of '<' (0x3C)
+        if value[:1] in (b'P', 'P') and value[:4] != b'PK\x03\x04':  # Fast detection of first 6 bits of '<' (0x3C)
             decoded_value = base64.b64decode(value)
             # Full mimetype detection
             if (guess_mimetype(decoded_value).startswith('image/svg') and
