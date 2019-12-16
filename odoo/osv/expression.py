b8481e0ea1718916f764836f435e21880ba69164
Nicolas Lempereur <nle@odoo.com>
2019-12-16 17:22:07 +0000

    [FIX] expression.py: avoid using TRUE_DOMAIN/FALSE_DOMAIN
    Ensure that expression.OR and expression.AND and some other
    expression.py methods do not propagate or rely on TRUE_DOMAIN and
    FALSE_DOMAIN that may be muted on some instance.
    
    For example, if we did:
    
      self.search(expression.OR([]))
    
    then in the search method we do something like:
    
      received_domain.append(('res_field', '=', False))
    
    before this commit, FALSE_DOMAIN would be altered for any succeeding code
    that try to use it in `[(0, '=', 1), ('res_field', '=', 'False')]`.
    
    Without the changeset, the added test would fail with:
    
        [(1, '=', 1), ('id', '=', 1)] != [(1, '=', 1)]
        [(0, '=', 1), ('id', '=', 1)] != [(0, '=', 1)]
        [(0, '=', 1), ('id', '=', 1)] != [(0, '=', 1)]
        [(1, '=', 1), ('id', '=', 1)] != [(1, '=', 1)]
        [(1, '=', 1), ('id', '=', 1)] != [(1, '=', 1)]
    
    note: another commit referenced in #41968 should make the TRUE_DOMAIN
    and FALSE_DOMAIN immutable.
    
    related to work on opw-2154448
    closes #42107
    
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- odoo/osv/expression.py
+++ odoo/osv/expression.py
@@ -189,7 +189,7 @@ def normalize_domain(domain):
     """
     assert isinstance(domain, (list, tuple)), "Domains to normalize must have a 'domain' form: a list or tuple of domain components"
     if not domain:
-        return TRUE_DOMAIN
+        return [TRUE_LEAF]
     result = []
     expected = 1                            # expected number of expressions
     op_arity = {NOT_OPERATOR: 1, AND_OPERATOR: 2, OR_OPERATOR: 2}
@@ -264,12 +264,12 @@ def combine(operator, unit, zero, domains):
 
 def AND(domains):
     """AND([D1,D2,...]) returns a domain representing D1 and D2 and ... """
-    return combine(AND_OPERATOR, TRUE_DOMAIN, FALSE_DOMAIN, domains)
+    return combine(AND_OPERATOR, [TRUE_LEAF], [FALSE_LEAF], domains)
 
 
 def OR(domains):
     """OR([D1,D2,...]) returns a domain representing D1 or D2 or ... """
-    return combine(OR_OPERATOR, FALSE_DOMAIN, TRUE_DOMAIN, domains)
+    return combine(OR_OPERATOR, [FALSE_LEAF], [TRUE_LEAF], domains)
 
 
 def distribute_not(domain):
@@ -741,7 +741,7 @@ class expression(object):
                 either as a range using the parent_left/right tree lookup fields
                 (when available), or as an expanded [(left,in,child_ids)] """
             if not ids:
-                return FALSE_DOMAIN
+                return [FALSE_LEAF]
             if left_model._parent_store and (not left_model.pool._init) and (not context.get('defer_parent_store_computation')):
                 # TODO: Improve where joins are implemented for many with '.', replace by:
                 # doms += ['&',(prefix+'.parent_left','<',rec.parent_right),(prefix+'.parent_left','>=',rec.parent_left)]
