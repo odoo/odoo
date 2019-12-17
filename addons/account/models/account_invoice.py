ed7c56979cb30bea0113a4d922106bca750f2655
Goffin Simon <sig@odoo.com>
2019-12-17 10:32:30 +0000

    [FIX] account: Wrong unit price with included tax and fiscal position
    Steps to reproduce the bug:
    
    - Let's consider a sale included tax T1 of 10% and a sale excluded tax T2 of 0%
    - Let's consider a product P with T1 and a sale price of 110€
    - Let's consider a fiscal position FP that mappes T1 to T2
    - Let's consider a customer C with FP as fiscal position
    - Create a customer invoice for C
    - Add P on the first line and T1 is replaced by T2
    
    Bug:
    
    The unit price of P was still 110€ instead of 100€ because the included tax was not removed from the base
    price of P.
    
    opw:2150564
    
    closes odoo/odoo#41991
    
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- addons/account/models/account_invoice.py
+++ addons/account/models/account_invoice.py
@@ -1888,12 +1888,8 @@ class AccountInvoiceLine(models.Model):
             self.price_unit = 0.0
 
         if self.product_id and self.uom_id:
-            if self.invoice_id.type in ('in_invoice', 'in_refund'):
-                price_unit = self.product_id.standard_price
-            else:
-                price_unit = self.product_id.lst_price
-            self.price_unit = self.product_id.uom_id._compute_price(price_unit, self.uom_id)
-            self._set_currency()
+            self._set_taxes()
+            self.price_unit = self.product_id.uom_id._compute_price(self.price_unit, self.uom_id)
 
             if self.product_id.uom_id.category_id.id != self.uom_id.category_id.id:
                 warning = {
