885f24f4ffe400abea7efc8012a4a81d222b5607
LiLEE <lilee115@users.noreply.github.com>
2019-12-17 11:03:22 +0000

    [FIX] website_sale_stock: allow override of context
    closes odoo/odoo#41997
    
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- addons/website_sale_stock/controllers/variant.py
+++ addons/website_sale_stock/controllers/variant.py
@@ -8,5 +8,6 @@ from odoo.addons.website_sale.controllers.variant import WebsiteSaleVariantContr
 class WebsiteSaleStockVariantController(WebsiteSaleVariantController):
     @http.route()
     def get_combination_info_website(self, product_template_id, product_id, combination, add_qty, **kw):
-        kw['context'] = {'website_sale_stock_get_quantity': True}
+        kw['context'] = kw.get('context', {})
+        kw['context'].update(website_sale_stock_get_quantity=True)
         return super(WebsiteSaleStockVariantController, self).get_combination_info_website(product_template_id, product_id, combination, add_qty, **kw)
