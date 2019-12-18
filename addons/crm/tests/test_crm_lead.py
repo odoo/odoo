1744cf9a0d492abfe2249cc8b67dc89dd4387c6a
Thibault Delavallée <tde@odoo.com>
2019-12-18 14:28:02 +0000

    [REF] crm: clean lead common test class
    Purpose of this commit is to clean a bit crm tests, notably lessening data
    created in tests and removing / improving some low level tests. As we are
    about to remove onchange / default and replace them by editable computed
    fields it is easier to clean some tests and data beforehand.
    
    This commit is a preliminary cleaning coming from task 2088565 about CRM
    onchange to compute methods update.
    
    LINKS
    
    Task ID 2088565
    Community PR #42344
    Enterprise PR odoo/enterprise#7403
    
    Related: odoo/enterprise#7403
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/crm/tests/test_crm_lead.py
+++ addons/crm/tests/test_crm_lead.py
@@ -1,88 +1,99 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from .common import TestCrmCommon
-from odoo.modules.module import get_module_resource
+from odoo.addons.crm.tests.common import TestCrmCommon, INCOMING_EMAIL
+from odoo.tests.common import users
 
 
 class TestCRMLead(TestCrmCommon):
 
-    def test_crm_lead_cancel(self):
-        # I set a new sales team giving access rights of salesman.
-        team = self.env['crm.team'].with_user(self.crm_salemanager).create({'name': "Phone Marketing"})
-        lead = self.crm_case_1
-        lead.with_user(self.crm_salemanager).write({'team_id': team.id})
-        # Salesmananger check unqualified lead
-        self.assertEqual(lead.stage_id.sequence, 1, 'Lead is in new stage')
-
-    def test_crm_lead_copy(self):
-        # I make duplicate the Lead
-        self.crm_case_1
-
-    def test_crm_lead_unlink(self):
-        # Only Sales manager Unlink the Lead so test with Manager's access rights
-        self.crm_case_1.with_user(self.crm_salemanager).unlink()
-
-    def test_find_stage(self):
-        # I create a new lead
+    @users('user_sales_manager')
+    def test_crm_lead_creation_partner(self):
         lead = self.env['crm.lead'].create({
-            'type': "lead",
-            'name': "Test lead new",
-            'partner_id': self.res_partner_1.id,
-            'description': "This is the description of the test new lead.",
-            'team_id': self.env.ref("sales_team.team_sales_department").id
+            'name': 'TestLead',
+            'contact_name': 'Raoulette TestContact',
+            'email_from': '"Raoulette TestContact" <raoulette@test.example.com>',
         })
-        # I change type from lead to opportunity
-        lead.convert_opportunity(self.res_partner_2.id)
-        # I check default stage of opportunity
-        self.assertLessEqual(lead.stage_id.sequence, 1, "Default stage of lead is incorrect!")
+        self.assertEqual(lead.type, 'lead')
+        self.assertEqual(lead.user_id, self.user_sales_manager)
+        self.assertEqual(lead.team_id, self.sales_team_1)
+        self.assertEqual(lead.stage_id, self.stage_team1_1)
+        self.assertEqual(lead.contact_name, 'Raoulette TestContact')
+        self.assertEqual(lead.email_from, '"Raoulette TestContact" <raoulette@test.example.com>')
 
-        # Now I change the stage of opportunity to won.
+        # update to a partner, should udpate address
+        lead.write({'partner_id': self.contact_1.id})
+        # self.assertEqual(lead.partner_name, self.contact_company_1.name)
+        # self.assertEqual(lead.contact_name, self.contact_1.name)
+        # self.assertEqual(lead.email_from, self.contact_1.email)
+        # self.assertEqual(lead.street, self.contact_1.street)
+        # self.assertEqual(lead.city, self.contact_1.city)
+        # self.assertEqual(lead.zip, self.contact_1.zip)
+        # self.assertEqual(lead.country_id, self.contact_1.country_id)
+
+    @users('user_sales_manager')
+    def test_crm_lead_stages(self):
+        lead = self.lead_1.with_user(self.env.user)
+        self.assertEqual(lead.team_id, self.sales_team_1)
+
+        lead.convert_opportunity(self.contact_1.id)
+        self.assertEqual(lead.team_id, self.sales_team_1)
+
         lead.action_set_won()
-        # I check stage of opp should won, after change stage.
-        stage_id = lead._stage_find(domain=[('is_won', '=', True)])
-        self.assertEqual(stage_id, lead.stage_id, "Stage of opportunity is incorrect!")
+        self.assertEqual(lead.probability, 100.0)
+        self.assertEqual(lead.stage_id, self.stage_team1_won)
 
-    def test_crm_lead_message(self):
-        # Give the access rights of Salesman to communicate with customer
-        # Customer interested in our product, so he sends request by email to get more details.
-        # Mail script will fetch his request from mail server. Then I process that mail after read EML file.
-        request_file = open(get_module_resource('crm', 'tests', 'customer_request.eml'), 'rb')
-        request_message = request_file.read()
-        self.env['mail.thread'].with_user(self.crm_salesman).message_process('crm.lead', request_message)
-
-        # After getting the mail, I check details of new lead of that customer
-        lead = self.env['crm.lead'].with_user(self.crm_salesman).search([('email_normalized', '=', 'info@customer.com')], limit=1)
-        self.assertTrue(lead.ids, 'Fail to create merge opportunity wizard')
-        self.assertFalse(lead.partner_id, 'Customer should be a new one')
-        self.assertEqual(lead.name, 'Fournir votre devis avec le meilleur prix.', 'Subject does not match')
-
-        # I reply his request with welcome message.
-        # TODO revert mail.mail to mail.compose.message (conversion to customer should be automatic).
-        lead = self.env['crm.lead'].search([('email_normalized', '=', 'info@customer.com')], limit=1)
-        mail = self.env['mail.compose.message'].with_context(active_model='crm.lead', active_id=lead.id).create({
-            'body': "Merci de votre intérêt pour notre produit, nous vous contacterons bientôt. Bien à vous",
-            'email_from': 'sales@mycompany.com'
+    @users('user_sales_manager')
+    def test_crm_team_alias(self):
+        new_team = self.env['crm.team'].create({
+            'name': 'TestAlias',
+            'use_leads': True,
+            'use_opportunities': True,
+            'alias_name': 'test.alias'
         })
-        try:
-            mail.send_mail()
-        except:
-            pass
+        self.assertEqual(new_team.alias_id.alias_name, 'test.alias')
+        self.assertEqual(new_team.alias_name, 'test.alias')
 
-        # Now, I convert him into customer and put him into regular customer list
-        lead = self.env['crm.lead'].search([('email_normalized', '=', 'info@customer.com')], limit=1)
-        lead.handle_partner_assignation()
+        new_team.write({
+            'use_leads': False,
+            'use_opportunities': False,
+        })
+        # self.assertFalse(new_team.alias_id.alias_name)
+        # self.assertFalse(new_team.alias_name)
+
+    def test_mailgateway(self):
+        new_lead = self.format_and_process(
+            INCOMING_EMAIL,
+            'unknown.sender@test.example.com',
+            '%s@%s' % (self.sales_team_1.alias_name, self.alias_domain),
+            subject='Delivery cost inquiry',
+            target_model='crm.lead',
+        )
+        self.assertEqual(new_lead.email_from, 'unknown.sender@test.example.com')
+        self.assertFalse(new_lead.partner_id)
+        self.assertEqual(new_lead.name, 'Delivery cost inquiry')
+
+        message = new_lead.with_user(self.user_sales_manager).message_post(
+            body='Here is my offer !',
+            subtype_xmlid='mail.mt_comment')
+        self.assertEqual(message.author_id, self.user_sales_manager.partner_id)
+
+        new_partner_id = new_lead.handle_partner_assignation()[new_lead.id]
+        new_partner = self.env['res.partner'].with_user(self.user_sales_manager).browse(new_partner_id)
+        self.assertEqual(new_partner.email, 'unknown.sender@test.example.com')
+        self.assertEqual(new_partner.team_id, self.sales_team_1)
 
     def test_crm_lead_merge(self):
-        # During a mixed merge (involving leads and opps), data should be handled a certain way following their type (m2o, m2m, text, ...)  Start by creating two leads and an opp and giving the rights of Sales manager.
-        default_stage_id = self.stage_lead1.id
-        LeadSalesmanager = self.env['crm.lead'].with_user(self.crm_salemanager)
+        # During a mixed merge (involving leads and opps), data should be handled a certain way following their type
+        # (m2o, m2m, text, ...)  Start by creating two leads and an opp and giving the rights of Sales manager.
+        default_stage_id = self.stage_team1_1.id
+        LeadSalesmanager = self.env['crm.lead'].with_user(self.user_sales_manager)
 
         # TEST CASE 1
         test_crm_opp_01 = LeadSalesmanager.create({
             'type': 'opportunity',
             'name': 'Test opportunity 1',
-            'partner_id': self.res_partner_3.id,
+            'partner_id': self.contact_2.id,
             'stage_id': default_stage_id,
             'description': 'This is the description of the test opp 1.'
         })
@@ -90,7 +101,7 @@ class TestCRMLead(TestCrmCommon):
         test_crm_lead_01 = LeadSalesmanager.create({
             'type': 'lead',
             'name': 'Test lead first',
-            'partner_id': self.res_partner_1.id,
+            'partner_id': self.contact_1.id,
             'stage_id': default_stage_id,
             'description': 'This is the description of the test lead first.'
         })
@@ -98,7 +109,7 @@ class TestCRMLead(TestCrmCommon):
         test_crm_lead_02 = LeadSalesmanager.create({
             'type': 'lead',
             'name': 'Test lead second',
-            'partner_id': self.res_partner_1.id,
+            'partner_id': self.contact_1.id,
             'stage_id': default_stage_id,
             'description': 'This is the description of the test lead second.'
         })
@@ -107,11 +118,11 @@ class TestCRMLead(TestCrmCommon):
         additionnal_context = {'active_model': 'crm.lead', 'active_ids': lead_ids, 'active_id': lead_ids[0]}
 
         # I create a merge wizard and merge the leads and opp together in the first item of the list.
-        merge_opp_wizard_01 = self.env['crm.merge.opportunity'].with_user(self.crm_salemanager).with_context(**additionnal_context).create({})
+        merge_opp_wizard_01 = self.env['crm.merge.opportunity'].with_user(self.user_sales_manager).with_context(**additionnal_context).create({})
         merge_opp_wizard_01.action_merge()
 
         # I check for the resulting merged opp (based on name and partner)
-        merged_lead = self.env['crm.lead'].search([('name', '=', 'Test opportunity 1'), ('partner_id', '=', self.res_partner_3.id)], limit=1)
+        merged_lead = self.env['crm.lead'].search([('name', '=', 'Test opportunity 1'), ('partner_id', '=', self.contact_2.id)], limit=1)
         self.assertTrue(merged_lead, 'Fail to create merge opportunity wizard')
         self.assertEqual(merged_lead.description, 'This is the description of the test opp 1.\n\nThis is the description of the test lead first.\n\nThis is the description of the test lead second.', 'Description mismatch: when merging leads/opps with different text values, these values should get concatenated and separated with line returns')
         self.assertEqual(merged_lead.type, 'opportunity', 'Type mismatch: when at least one opp in involved in the merge, the result should be a new opp (instead of %s)' % merged_lead.type)
@@ -125,14 +136,14 @@ class TestCRMLead(TestCrmCommon):
         test_crm_lead_03 = LeadSalesmanager.create({
             'type': 'lead',
             'name': 'Test lead 3',
-            'partner_id': self.res_partner_1.id,
+            'partner_id': self.contact_1.id,
             'stage_id': default_stage_id
         })
 
         test_crm_lead_04 = LeadSalesmanager.create({
             'type': 'lead',
             'name': 'Test lead 4',
-            'partner_id': self.res_partner_1.id,
+            'partner_id': self.contact_1.id,
             'stage_id': default_stage_id
         })
 
@@ -140,13 +151,13 @@ class TestCRMLead(TestCrmCommon):
         additionnal_context = {'active_model': 'crm.lead', 'active_ids': lead_ids, 'active_id': lead_ids[0]}
 
         # I create a merge wizard and merge the leads together.
-        merge_opp_wizard_02 = self.env['crm.merge.opportunity'].with_user(self.crm_salemanager).with_context(**additionnal_context).create({})
+        merge_opp_wizard_02 = self.env['crm.merge.opportunity'].with_user(self.user_sales_manager).with_context(**additionnal_context).create({})
         merge_opp_wizard_02.action_merge()
 
         # I check for the resulting merged lead (based on name and partner)
-        merged_lead = self.env['crm.lead'].search([('name', '=', 'Test lead 3'), ('partner_id', '=', self.res_partner_1.id)], limit=1)
+        merged_lead = self.env['crm.lead'].search([('name', '=', 'Test lead 3'), ('partner_id', '=', self.contact_1.id)], limit=1)
         self.assertTrue(merged_lead, 'Fail to create merge opportunity wizard')
-        self.assertEqual(merged_lead.partner_id.id, self.res_partner_1.id, 'Partner mismatch')
+        self.assertEqual(merged_lead.partner_id.id, self.contact_1.id, 'Partner mismatch')
         self.assertEqual(merged_lead.type, 'lead', 'Type mismatch: when leads get merged together, the result should be a new lead (instead of %s)' % merged_lead.type)
         self.assertFalse(test_crm_lead_04.exists(), 'This tailing lead (id %s) should not exist anymore' % test_crm_lead_04.id)
 
@@ -155,14 +166,14 @@ class TestCRMLead(TestCrmCommon):
         test_crm_opp_02 = LeadSalesmanager.create({
             'type': 'opportunity',
             'name': 'Test opportunity 2',
-            'partner_id': self.res_partner_3.id,
+            'partner_id': self.contact_2.id,
             'stage_id': default_stage_id
         })
 
         test_crm_opp_03 = LeadSalesmanager.create({
             'type': 'opportunity',
             'name': 'Test opportunity 3',
-            'partner_id': self.res_partner_3.id,
+            'partner_id': self.contact_2.id,
             'stage_id': default_stage_id
         })
 
@@ -170,41 +181,11 @@ class TestCRMLead(TestCrmCommon):
         additionnal_context = {'active_model': 'crm.lead', 'active_ids': opportunity_ids, 'active_id': opportunity_ids[0]}
 
         # I create a merge wizard and merge the opps together.
-        merge_opp_wizard_03 = self.env['crm.merge.opportunity'].with_user(self.crm_salemanager).with_context(**additionnal_context).create({})
+        merge_opp_wizard_03 = self.env['crm.merge.opportunity'].with_user(self.user_sales_manager).with_context(**additionnal_context).create({})
         merge_opp_wizard_03.action_merge()
 
-        merged_opportunity = self.env['crm.lead'].search([('name', '=', 'Test opportunity 2'), ('partner_id', '=', self.res_partner_3.id)], limit=1)
+        merged_opportunity = self.env['crm.lead'].search([('name', '=', 'Test opportunity 2'), ('partner_id', '=', self.contact_2.id)], limit=1)
         self.assertTrue(merged_opportunity, 'Fail to create merge opportunity wizard')
-        self.assertEqual(merged_opportunity.partner_id.id, self.res_partner_3.id, 'Partner mismatch')
+        self.assertEqual(merged_opportunity.partner_id.id, self.contact_2.id, 'Partner mismatch')
         self.assertEqual(merged_opportunity.type, 'opportunity', 'Type mismatch: when opps get merged together, the result should be a new opp (instead of %s)' % merged_opportunity.type)
         self.assertFalse(test_crm_opp_03.exists(), 'This tailing opp (id %s) should not exist anymore' % test_crm_opp_03.id)
-
-    def test_lead_won(self):
-        """
-        As there are multiple ways to set a lead as won (by action_set_won or moving the lead to a won stage)
-        The logic behind the set as won is now in the write override to be executed
-        each time the new stage is a won stage.
-        """
-        # Create leads at different stage
-        leads_to_create = []
-        for i in range(3):
-            for x in range(3):
-                leads_to_create.append({
-                    'type': "lead",
-                    'name': "Test lead new " + str(x),
-                    'partner_id': self.res_partner_1.id,
-                    'description': "This is the description of the test new lead.",
-                    'team_id': self.env.ref("sales_team.team_sales_department").id,
-                    'stage_id': self.env.ref("crm.stage_lead%s" % (str(i + 1))).id,
-                })
-        leads = self.env['crm.lead'].create(leads_to_create)
-        # Set leads as won (stage = won)
-        leads[:4].write({'stage_id': self.env.ref("crm.stage_lead4").id})
-        # Mark leads as won (via action)
-        leads[4:].action_set_won()
-        # Check if every single lead has the correct probability + correct stage
-        index = 0
-        for lead in leads:
-            self.assertEqual(lead.stage_id.id, self.env.ref("crm.stage_lead4").id, 'Stage must be "Won"')
-            self.assertEqual(lead.probability, 100, 'Probability of a won lead must be = 100%')
-            index += 1
