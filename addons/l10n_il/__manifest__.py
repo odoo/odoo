3da152aa6ea33dcac7afdd82be32d28fc745226d
Meghna Jaswani <mja@odoo.com>
2019-12-19 12:51:13 +0530

    [IMP] l10n_il: Self Invoice Corrections based on review

--- addons/l10n_il/__manifest__.py
+++ addons/l10n_il/__manifest__.py
@@ -23,7 +23,6 @@ This module consists:
         'data/account_tax_template_data.xml',
         'data/account_fiscal_position_templates_data.xml',
         'data/account_chart_template_post_data.xml',
-        'data/account_chart_template_configure_data.xml',
-        'views/res_partner_view.xml'
+        'data/account_chart_template_configure_data.xml'
     ],
 }
--- addons/l10n_il/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    l10n_il_self_invoice = fields.Boolean(string='Self Invoice', help="Set this to true to when self invoice needs to be created")
