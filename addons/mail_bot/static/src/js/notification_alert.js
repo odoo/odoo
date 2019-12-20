74a427698a8f7e345ae87f5ffdd6e03a4d11b83c
Adrien Dieudonne <adr@odoo.com>
2019-12-20 13:12:56 +0100

    [FIX] mail_bot: hide notification alert in "my profile" on mobile

--- addons/mail_bot/static/src/js/notification_alert.js
+++ addons/mail_bot/static/src/js/notification_alert.js
@@ -1,6 +1,7 @@
 odoo.define('mail.NotificationAlert', function (require) {
 "use strict";
 
+var mobile = require('web_mobile.rpc');
 var Widget = require('web.Widget');
 var widgetRegistry = require('web.widget_registry');
 
@@ -14,6 +15,10 @@ var NotificationAlert = Widget.extend({
     */
    init: function () {
       this._super.apply(this, arguments);
+      // No need to check browser notif because we use native notification on mobile.
+      if (mobile.methods.getFCMKey) {
+         return;
+      }
       var hasRequest = this.call('mailbot_service', 'isRequestingForNativeNotifications');
       this.isNotificationBlocked = window.Notification && window.Notification.permission !== "granted" && !hasRequest;
    },
