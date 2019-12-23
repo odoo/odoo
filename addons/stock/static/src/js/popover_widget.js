5cafacf28e3069249a57db52878cd92d53561006
Rémy Voet (ryv) <ryv@odoo.com>
2019-12-23 17:30:00 +0100

    [IMP] stock: add widget popover
    Add a popover widget linked to a Char field containing
    a JSON string. It used to display flexible content in a Bootstrap
    popover which appears when the user have mouse over or focus on the
    information icon/button.
    
    task-2082261

---
+++ addons/stock/static/src/js/popover_widget.js
@@ -0,0 +1,56 @@
+odoo.define('stock.popover_widget', function (require) {
+'use strict';
+
+var AbstractField = require('web.AbstractField');
+var core = require('web.core');
+var QWeb = core.qweb;
+var fieldRegistry = require('web.field_registry');
+
+/**
+ * Widget Popover for JSON field, by default render a simple html message
+ * {
+ *  'msg': '<CONTENT OF THE POPOVER>',
+ *  'color': '<COLOR CLASS OF ICON>' (optionnal),
+ *  'title': '<TITLE OF POPOVER>' (optionnal),
+ *  'popoverTemplate': '<TEMPLATE OF THE TEMPLATE>' (optionnal)
+ * }
+ */
+var PopoverWidgetField = AbstractField.extend({
+    supportedFieldTypes: ['char'],
+    buttonTemplape: 'stock.popoverButton',
+    popoverTemplate: 'stock.popoverContent',
+    trigger: 'hover focus',
+    placement: 'top',
+    html: true,
+
+    _render: function () {
+        var value = JSON.parse(this.value);
+        if (!value) {
+            this.$el.html('');
+            return;
+        }
+        this.$el.css('max-width', '17px');
+        this.$el.html(QWeb.render(this.buttonTemplape, value));
+        this.$el.find('a').prop('special_click', true);
+        this.$popover = $(QWeb.render(value.popoverTemplate || this.popoverTemplate, value));
+        this.$el.find('a').popover({
+            content: this.$popover,
+            html: this.html,
+            placement: this.placement,
+            title: value.title || this.title,
+            trigger: this.trigger,
+            delay: {'show': 0, 'hide': 100},
+        });
+    },
+
+    destroy: function () {
+        this.$el.find('a').popover('dispose');
+        this._super.apply(this, arguments);
+    },
+
+});
+
+fieldRegistry.add('popover_widget', PopoverWidgetField);
+
+return PopoverWidgetField;
+});
