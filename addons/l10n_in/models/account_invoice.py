3790ee783f3e00ae9932ccd86ef4e16bcb6c54e1
Goffin Simon <sig@odoo.com>
2019-12-23 13:25:42 +0000

    [FIX] l10n_in: Traceback error when printing credit note
    Steps to reproduce the bug:
    
    - Install l10n_in
    - Set your company in India with INR currency
    - Create a credit note
    - Try to print it without payment
    
    Bug:
    
    A traceback was raised.
    
    opw:2160098
    
    X-original-commit: 9a3093625f39456a9f3bd4c0616bc0300e4b55f2

--- addons/l10n_in/models/account_invoice.py
+++ addons/l10n_in/models/account_invoice.py
@@ -29,6 +29,43 @@ class AccountMove(models.Model):
     l10n_in_reseller_partner_id = fields.Many2one('res.partner', 'Reseller', domain=[('vat', '!=', False)], help="Only Registered Reseller", readonly=True, states={'draft': [('readonly', False)]})
     l10n_in_partner_vat = fields.Char(related="partner_id.vat", readonly=True)
 
+<<<<<<< HEAD
+=======
+    def _get_report_base_filename(self):
+        self.ensure_one()
+        if self.company_id.country_id.code != 'IN':
+            return super(AccountInvoice, self)._get_report_base_filename()
+        return self.type == 'out_invoice' and self.state == 'draft' and _('Draft %s') % (self.journal_id.name) or \
+            self.type == 'out_invoice' and self.state in ('open','in_payment','paid') and '%s - %s' % (self.journal_id.name, self.number) or \
+            self.type == 'out_refund' and self.state == 'draft' and _('Credit Note') or \
+            self.type == 'out_refund' and _('Credit Note - %s') % (self.number) or \
+            self.type == 'in_invoice' and self.state == 'draft' and _('Vendor Bill') or \
+            self.type == 'in_invoice' and self.state in ('open','in_payment','paid') and _('Vendor Bill - %s') % (self.number) or \
+            self.type == 'in_refund' and self.state == 'draft' and _('Vendor Credit Note') or \
+            self.type == 'in_refund' and _('Vendor Credit Note - %s') % (self.number)
+
+    @api.multi
+    def _invoice_line_tax_values(self):
+        self.ensure_one()
+        tax_datas = {}
+        Tag = self.env['account.account.tag']
+        repart_field = '%s_repartition_line_ids' % ('invoice' if self.type in ('in_invoice', 'out_invoice') else 'refund')
+
+        for line in self.mapped('invoice_line_ids'):
+            price_unit = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            tax_lines = line.invoice_line_tax_ids.compute_all(price_unit, line.invoice_id.currency_id, line.quantity, line.product_id, line.invoice_id.partner_id, self.type in ('in_refund', 'out_refund'))['taxes']
+            for t_line in tax_lines:
+                x2m_res = line.invoice_line_tax_ids[repart_field].resolve_2many_commands(field_name='tag_ids', commands=t_line['tag_ids'])
+                t_line['tag_ids'] = [tag['id'] for tag in x2m_res]
+                t_line['report_line_ids'] = Tag.browse(t_line['tag_ids']).mapped('tax_report_line_ids').ids
+            tax_datas[line.id] = tax_lines
+        return tax_datas
+
+    def inv_line_characteristic_hashcode(self, invoice_line):
+        res = super(AccountInvoice, self).inv_line_characteristic_hashcode(invoice_line)
+        return res + "-%s" %(invoice_line.get('product_uom_id', 'False'))
+
+>>>>>>> 244b2a72aa9... temp
     @api.model
     def _get_tax_grouping_key_from_tax_line(self, tax_line):
         # OVERRIDE to group taxes also by product.
