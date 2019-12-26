5eec92d28be71764b0b839baf0795829c0a400e4
Romain Derie <rde@odoo.com>
2019-12-26 11:16:22 +0000

    [IMP] website_mail_channel: review Discussion Group snippet
    Part of Inner Snippets review task. Left panelize the discussion group
    snippet.
    
    Part of https://github.com/odoo/odoo/pull/42937
    task-2088157
    task-2152191

--- addons/website_mail_channel/static/src/js/website_mail_channel.editor.js
+++ addons/website_mail_channel/static/src/js/website_mail_channel.editor.js
@@ -8,32 +8,68 @@ var wUtils = require('website.utils');
 var _t = core._t;
 
 options.registry.subscribe = options.Class.extend({
-    select_mailing_list: function (previewMode, value) {
-        var self = this;
-        return wUtils.prompt({
-            id: "editor_new_subscribe_button",
-            window_title: _t("Add a Subscribe Button"),
-            select: _t("Discussion List"),
-            init: function (field) {
-                return self._rpc({
-                    model: 'mail.channel',
-                    method: 'name_search',
-                    args: ['', [['public', '=', 'public']]],
-                });
-            },
-        }).then(function (result) {
-            self.$target.attr("data-id", result.val);
-        });
-    },
-    onBuilt: function () {
-        var self = this;
-        this._super();
-        this.select_mailing_list("click").guardedCatch(function () {
-            self.getParent().removeSnippet();
-        });
-    },
+    /**
+     * @override
+     */
     cleanForSave: function () {
         this.$target.addClass('d-none');
+    },
+
+    //--------------------------------------------------------------------------
+    // Options
+    //--------------------------------------------------------------------------
+
+    /**
+     * Creates a new mail.channel through a modal prompt.
+     *
+     * @see this.selectClass for parameters
+     */
+    createChannel: function (previewMode, widgetValue, params) {
+        var self = this;
+        return wUtils.prompt({
+            id: "editor_new_mail_channel_subscribe",
+            window_title: _t("New Mail Channel"),
+            input: _t("Name"),
+        }).then(function (result) {
+            var name = result.val;
+            if (!name) {
+                return;
+            }
+            return self._rpc({
+                model: 'mail.channel',
+                method: 'create',
+                args: [{
+                    name: name,
+                    public: 'public',
+                }],
+            }).then(function (id) {
+                self.$target.attr("data-id", id);
+                return self._rerenderXML();
+            });
+        });
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    _renderCustomXML: function (uiFragment) {
+        return this._rpc({
+            model: 'mail.channel',
+            method: 'name_search',
+            args: ['', [['public', '=', 'public']]],
+        }).then(channels => {
+            const menuEl = uiFragment.querySelector('.select_discussion_list');
+            for (const channel of channels) {
+                const el = document.createElement('we-button');
+                el.dataset.selectDataAttribute = channel[0];
+                el.textContent = channel[1];
+                menuEl.appendChild(el);
+            }
+        });
     },
 });
 });
