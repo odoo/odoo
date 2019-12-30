60c5d5731523a8129c00586e09be55a999de5794
qmo-odoo <qmo@odoo.com>
2019-12-30 09:17:23 +0100

    [FIX] website_slides: fix slide reordering in frontend
    This commit fixes the reordering of slides on upload or drag and drop
    in frontend. This issue was due to the fact that in a previous commit
    (c4eba6f) uncategorized slides were moved to the bottom of the list,
    which broke the reordering after drag and drop.
    
    Indeed we use the dom order itself to reorder the slides with a simple rpc
    call to dataset/resequence. Instead we revert part of the previous fix
    to display uncategorized slides at the top. It allows to be coherent with
    resequence and with backend behavior that displays uncategorized at the
    top of the list.
    
    Another issue was that, on slide upload, in the controller, we used
    the category_id on the slide (which seems to not always be correct at
    this stage) instead of the one given by the javascript. This caused the slide
    to not be uploaded in the right section.
    
    This commit also adds some margin to the categories in frontend for training
    courses. Otherwise uncategorized slides are too close to the first category.
    
    Task ID 2090927

--- addons/website_slides/tests/test_slide_utils.py
+++ addons/website_slides/tests/test_slide_utils.py
@@ -5,6 +5,25 @@ from odoo.addons.website_slides.tests import common as slides_common
 from odoo.tests.common import users
 
 
+class TestSlidesManagement(slides_common.SlidesCase):
+
+    @users('user_publisher')
+    def test_get_categorized_slides(self):
+        new_category = self.env['slide.slide'].create({
+            'name': 'Cooking Tips for Cooking Humans',
+            'channel_id': self.channel.id,
+            'is_category': True,
+            'sequence': 5,
+        })
+        order = self.env['slide.slide']._order_by_strategy['sequence']
+        categorized_slides = self.channel._get_categorized_slides([], order)
+        self.assertEquals(categorized_slides[0]['category'], False)
+        self.assertEquals(categorized_slides[1]['category'], self.category)
+        self.assertEquals(categorized_slides[1]['total_slides'], 2)
+        self.assertEquals(categorized_slides[2]['total_slides'], 0)
+        self.assertEquals(categorized_slides[2]['category'], new_category)
+
+
 class TestSequencing(slides_common.SlidesCase):
 
     @users('user_publisher')
