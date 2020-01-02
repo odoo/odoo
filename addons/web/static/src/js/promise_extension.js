38cb0c13058e90e4f59388656d50651289185cca
qsm-odoo <qsm@odoo.com>
2020-01-02 12:10:33 +0100

    [FIX] web: properly implement guardedCatch
    The implementation we currently had for 'guardedCatch' was always
    generating an error in Firefox (and not in Chrome) and was considered
    as a Firefox mistake. Once again, I wanted to prove that wrong.
    
    In the end, after investigation:
    
    - Odoo code is supposed to catch 'unhandledrejection' events to cancel
      them so that no error is logged in the console.
    
    - Old firefox (when native promises started being used in Odoo) never
      triggered the 'unhandledrejection' event (that's why we added a
      polyfill... which was recently removed because it generated errors in
      Microsoft Edge). So no rejection is logging an error... except when
      the polyfill was there (then all rejections were logging an error).
    
    - New firefox does trigger the 'unhandledrejection' event in all correct
      cases... but no code is able to cancel them, so the console log always
      appears.
    
    - Chrome allows to cancel the 'unhandledrejection' events but seems to
      only trigger them in "asynchronous code". Indeed, see the standard
      javascript following lines:
    
          Promise.reject();
          Promise.reject().catch(() => Promise.reject());
    
      Chrome seems inconsistant since both lines creates and returns an
      unhandled rejected promise but only the second one triggers an
      'unhandledrejection' (that we can cancel).
    
    In any case, this commit solves the problem at the source:
    `guardedCatch` should mark the original promise as handled and not
    generate a new unhandled rejected promise in the case the rejection
    handler is called, but instead returning the original
    handled-and-rejected one.
    
    Part of https://github.com/odoo/odoo/pull/42798
    task-1964486

--- addons/web/static/src/js/promise_extension.js
+++ addons/web/static/src/js/promise_extension.js
@@ -1,22 +1,29 @@
 /**
  * This file adds a 'guardedCatch' function to the Promise API. This function
- * has to be used when we don't want to swallow real errors (crashes), like
- * 'catch' does (i.e. basically all the time in Odoo). We only execute the
- * 'onRejected' handler if the rejection's reason is not an Error, and we always
- * return a rejected Promise to let the rejection bubble up (and trigger the
- * 'unhandledrejection' event).
+ * has to be used to not catch real errors (crashes), like 'catch' does. The
+ * 'onRejected' handler is only executed if the rejection's reason is not an
+ * Error object, otherwise we let the original rejection reason propagate.
+ *
+ * Note: we also want the method to behave like 'fail' of jQuery:
+ * prom.guardedCatch(1).then(2) should only call (1) if prom is rejected as
+ * .fail() returned the original promise.
  */
-
-(function () {
-    var _catch = Promise.prototype.catch;
-    Promise.prototype.guardedCatch = function (onRejected) {
-        return _catch.call(this, function (reason) {
-            if (!reason || !(reason instanceof Error)) {
-                if (onRejected) {
-                    onRejected.call(this, reason);
-                }
-            }
+Promise.prototype.guardedCatch = function (onRejected) {
+    const self = this;
+    return this.catch(function (reason) {
+        if (typeof onRejected !== 'function' || reason instanceof Error) {
+            // In the case the method was not given a proper rejection handler
+            // or that the original promise was rejected because of a common
+            // JS error, we let the rejection propagate by re-triggering it...
             return Promise.reject(reason);
-        });
-    };
-})();
+        }
+        // ... otherwise the jQuery 'fail' method's behavior is desired: execute
+        // the rejection handler, ignores its result and async components and
+        // propagate the original promise result (ideally should return the
+        // original promise itself, but not possible using native promises) but
+        // keep the fact that it has been handled (do not return a new
+        // *unhandled* rejected promise).
+        onRejected.call(this, reason);
+        return self;
+    });
+};
