f3f639157fc00c54fa7c5f6dccc818483f7a315d
Helly kapatel <hpl@odoo.com>
2020-01-02 13:40:07 +0000

    [IMP] l10n_vn: Update COA and COA name convert to english
    Add translation(po file) in Vietnamese(vi_VN)
    
    task: 2054159

--- addons/l10n_vn/__init__.py
+++ addons/l10n_vn/__init__.py
@@ -3,6 +3,13 @@
 
 # This module is Copyright (c) 2009-2013 General Solutions (http://gscom.vn) All Rights Reserved.
 
+from odoo import api, SUPERUSER_ID
+
+
+def _post_init_hook(cr, registry):
+    _preserve_tag_on_taxes(cr, registry)
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('l10n_vn.vn_template').process_coa_translations()
 
 def _preserve_tag_on_taxes(cr, registry):
     from odoo.addons.account.models.chart_template import preserve_existing_tags_on_taxes
