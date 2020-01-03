d160997c9515dd6178da83fcb1daebad0086cb86
Thibault Delavallée <tde@odoo.com>
2020-01-03 12:04:34 +0000

    [MOV] base_address_extended: reorganize python code
    Purpose is to reorganize code according to guidelines and split the main
    badly named python file, to understand that 3 models are impacted: res.company,
    res.country and res.partner.
    
    Task ID 2158302
    PR odoo/odoo#42678

--- addons/base_address_extended/models/__init__.py
+++ addons/base_address_extended/models/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import base_address_extended
+from . import res_company
+from . import res_country
+from . import res_partner
