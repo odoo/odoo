311b04b5ca3af6946dfbe32a143905a239d55eb0
Thibault Delavallée <tde@odoo.com>
2020-01-06 17:32:24 +0100

    [IMP] crm: clean and improve tests for lead2opportunity converters
    PURPOSE
    
    As crm will soon evolve (onchange -> compute, code improvements, addition of
    new features) cleaning and improving tests is necessary to help avoid issues.
    
    SPECIFICATIONS
    
    In this commit we add tests for the lead 2 opportunity converters. Indeed
    it is strange that such a critical wizard has so few tests. Notably the
    use of single-lead / multi-lead mode is more tested, allowing to see that
    code is somewhat oldish.
    
    LINKS
    
    Side effect of Task ID 2056759 (remove crm.partner.binding mixin)
    Side effect of Task ID 2088565 (crm onchange -> compute)
    Community PR #43127
    Enterprise PR odoo/enterprise#7656

--- addons/crm/tests/__init__.py
+++ addons/crm/tests/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from . import test_crm_lead
-from . import test_new_lead_notification
-from . import test_lead2opportunity
+from . import test_crm_lead_notification
+from . import test_crm_lead_convert
 from . import test_crm_activity
 from . import test_crm_ui
 from . import test_crm_pls
