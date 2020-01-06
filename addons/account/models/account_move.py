6d989939e7f9772109e7196485b6b0eaaf4dd4fd
wan <wan@odoo.com>
2020-01-06 16:38:58 +0100

    [IMP] account: preview in accrual wizard
    Task 2168064
    Show a preview of what will be done when using the accrual wizard

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -2419,6 +2419,20 @@ class AccountMove(models.Model):
         action['res_id'] = self.copy().id
         return action
 
+    @api.model
+    def _move_dict_to_preview_vals(self, move_vals, currency_id=None):
+        preview_vals = {
+            'group_name': "%s, %s" % (format_date(self.env, move_vals['date']) or _('[Not set]'), move_vals['ref']),
+            'items_vals': move_vals['line_ids'],
+        }
+        for line in preview_vals['items_vals']:
+            if 'partner_id' in line[2]:
+                line[2]['partner_id'] = self.env['res.partner'].browse(line[2]['partner_id']).display_name
+            line[2]['account_id'] = self.env['account.account'].browse(line[2]['account_id']).display_name or _('Destination Account')
+            line[2]['debit'] = currency_id and formatLang(self.env, line[2]['debit'], currency_obj=currency_id) or line[2]['debit']
+            line[2]['credit'] = currency_id and formatLang(self.env, line[2]['credit'], currency_obj=currency_id) or line[2]['debit']
+        return preview_vals
+
 
 class AccountMoveLine(models.Model):
     _name = "account.move.line"
