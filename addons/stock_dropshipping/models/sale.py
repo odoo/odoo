44ae14ed6c2c91bc76425515d3ffdb97df8132df
Rémy Voet (ryv) <ryv@odoo.com>
2020-01-07 16:41:15 +0100

    [ADD] sale_purchase_stock: link between PO<->SO in case of MTO
    In order to improve the navigation between of SO and PO in the case of
    MTO, add a direct link between PO<->SO. When the SO is confirmed
    (with storable product(s) with MTO + buy),
    PO is/are generated, in this case, add a stat button on each model form
    to avoid to manually search the related SO with the source field (name).
    
    task-1913392

--- addons/stock_dropshipping/models/sale.py
+++ addons/stock_dropshipping/models/sale.py
@@ -1,24 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, fields
+from odoo import models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
-
-    purchase_line_ids = fields.One2many('purchase.order.line', 'sale_line_id')
-
-    def _get_qty_procurement(self, previous_product_uom_qty):
-        # People without purchase rights should be able to do this operation
-        purchase_lines_sudo = self.sudo().purchase_line_ids
-        if purchase_lines_sudo.filtered(lambda r: r.state != 'cancel'):
-            qty = 0.0
-            for po_line in purchase_lines_sudo.filtered(lambda r: r.state != 'cancel'):
-                qty += po_line.product_uom._compute_quantity(po_line.product_qty, self.product_uom, rounding_method='HALF-UP')
-            return qty
-        else:
-            return super(SaleOrderLine, self)._get_qty_procurement(previous_product_uom_qty=previous_product_uom_qty)
+    _inherit = 'sale.order.line'
 
     def _compute_is_mto(self):
         super(SaleOrderLine, self)._compute_is_mto()
@@ -31,3 +18,14 @@ class SaleOrderLine(models.Model):
                         pull_rule.picking_type_id.sudo().default_location_dest_id.usage == 'customer':
                     line.is_mto = True
                     break
+
+    def _get_qty_procurement(self, previous_product_uom_qty):
+        # People without purchase rights should be able to do this operation
+        purchase_lines_sudo = self.sudo().purchase_line_ids
+        if purchase_lines_sudo.filtered(lambda r: r.state != 'cancel'):
+            qty = 0.0
+            for po_line in purchase_lines_sudo.filtered(lambda r: r.state != 'cancel'):
+                qty += po_line.product_uom._compute_quantity(po_line.product_qty, self.product_uom, rounding_method='HALF-UP')
+            return qty
+        else:
+            return super(SaleOrderLine, self)._get_qty_procurement(previous_product_uom_qty=previous_product_uom_qty)
