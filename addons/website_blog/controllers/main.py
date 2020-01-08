de6ba203b8eb53f26afbdf33969ca494a7b33af7
Jason Van Malder <jvm@odoo.com>
2020-01-08 07:38:12 +0000

    [FIX] website_blog: fix last update change on visit
    Issue
    
        - Install Blog
        - Create a post
        - Remember the last update date
        - Visit the post
        - Check the last update date
    
        After visiting the post, the last update date changed.
    
    Cause
    
        In the web_blog main.py controller, we increase the visit
        count with a write, so the write_date is updated.
    
    Solution
    
        Keep the old write_date by forcing it.
    
    OPW-2166289
    
    closes odoo/odoo#42954
    
    X-original-commit: 16843c63b97cda746972526f8933bf25b435d585
    Signed-off-by: Jason Van Malder <jvm-odoo@users.noreply.github.com>

--- addons/website_blog/controllers/main.py
+++ addons/website_blog/controllers/main.py
@@ -265,6 +265,7 @@ class WebsiteBlog(http.Controller):
             # Increase counter
             blog_post.sudo().write({
                 'visits': blog_post.visits + 1,
+                'write_date': blog_post.write_date,
             })
         return response
 
