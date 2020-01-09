a1d6c6f2a182dd36fd634024b71bf6388f8250ef
yhu-odoo <yhu@odoo.com>
2020-01-09 09:49:39 +0100

    [FIX] delivery: field name 'length' is reserved in JS
    length is a reserved attribute in JavaScript, and may cause problems
    when returning the object to the JS framework.
    This commit is to rename the field.
    
    PR
    Task 2152050

--- addons/delivery/models/product_packaging.py
+++ addons/delivery/models/product_packaging.py
@@ -9,7 +9,7 @@ class ProductPackaging(models.Model):
 
     height = fields.Integer('Height')
     width = fields.Integer('Width')
-    length = fields.Integer('Length')
+    packaging_length = fields.Integer('Length')
     max_weight = fields.Float('Max Weight', help='Maximum weight shippable in this packaging')
     shipper_package_code = fields.Char('Package Code')
     package_carrier_type = fields.Selection([('none', 'No carrier integration')], string='Carrier', default='none')
@@ -17,7 +17,7 @@ class ProductPackaging(models.Model):
     _sql_constraints = [
         ('positive_height', 'CHECK(height>=0)', 'Height must be positive'),
         ('positive_width', 'CHECK(width>=0)', 'Width must be positive'),
-        ('positive_length', 'CHECK(length>=0)', 'Length must be positive'),
+        ('positive_length', 'CHECK(packaging_length>=0)', 'Length must be positive'),
         ('positive_max_weight', 'CHECK(max_weight>=0.0)', 'Max Weight must be positive'),
     ]
 
