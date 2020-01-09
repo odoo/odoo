28faf4085e4042546a17decb23f38a88e9f5efa7
Simon Lejeune <sle@openerp.com>
2020-01-09 09:52:02 +0000

    [REF] stock_landed_costs: domain on transfers
    The previous domain was too naive: a receipt is not always valued
    (inter-warehouse transfer) and even a receipt that should be valued
    isn't always (receive goods you do not own, change your config from
    manual to automated and then apply the lc on an old receipt, etc.)
    
    Enforce a domain making sure the transfer is valued.
    
    related opw-2165697
    task-2169844
    
    closes odoo/odoo#43410
    
    X-original-commit: c6aa61b22a18c3f110baa7ed3c97e275b21e5329
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- addons/stock_landed_costs/models/stock_landed_cost.py
+++ addons/stock_landed_costs/models/stock_landed_cost.py
@@ -45,6 +45,7 @@ class LandedCost(models.Model):
     picking_ids = fields.Many2many(
         'stock.picking', string='Transfers',
         copy=False, states={'done': [('readonly', True)]})
+    allowed_picking_ids = fields.Many2many('stock.picking', compute='_compute_allowed_picking_ids')
     cost_lines = fields.One2many(
         'stock.landed.cost.lines', 'cost_id', 'Cost Lines',
         copy=True, states={'done': [('readonly', True)]})
@@ -78,6 +79,18 @@ class LandedCost(models.Model):
     def _compute_total_amount(self):
         for cost in self:
             cost.amount_total = sum(line.price_unit for line in cost.cost_lines)
+
+    @api.depends('company_id')
+    def _compute_allowed_picking_ids(self):
+        self.env.cr.execute("""SELECT sm.picking_id, sm.company_id
+                                 FROM stock_move AS sm
+                           INNER JOIN stock_valuation_layer AS svl ON svl.stock_move_id = sm.id
+                                WHERE sm.picking_id IS NOT NULL""")
+        valued_picking_ids_per_company = defaultdict(list)
+        for res in self.env.cr.fetchall():
+            valued_picking_ids_per_company[res[1]].append(res[0])
+        for cost in self:
+            cost.allowed_picking_ids = valued_picking_ids_per_company[cost.company_id.id]
 
     @api.model
     def create(self, vals):
