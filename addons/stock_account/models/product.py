c62dea6a7ce6b63f708831707811e922cd6bddd0
Ujas Dubal <udu@odoo.com>
2020-01-13 10:01:46 +0000

    [FIX] stock_account: Added default type for 'account.move' line.
    This commit will fixed the error when product has some stock quants
    and product category with 'Automated' valuation method, is set to that product.
    
    ==============================
    
    Steps to product the above mentioned error:
    1. Create product with any category which has 'Manual' valuation method.
    2. Update 'On Hand' quantity for the same product.
    3. Create product category with valuation method set to 'Automated'.
    4. Now set the above product category to the same product which you created in step 1.
    5. And Click on the save button.
    
    ==============================
    
    Also edit the test case.
    
    closes odoo/odoo#43721
    
    Task: 2155804
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -470,6 +470,7 @@ class ProductProduct(models.Model):
                     'credit': abs(value),
                     'product_id': product.id,
                 })],
+                'type': 'entry',
             }
             move_vals_list.append(move_vals)
         return move_vals_list
@@ -505,6 +506,7 @@ class ProductProduct(models.Model):
                     'credit': abs(value),
                     'product_id': product.id,
                 })],
+                'type': 'entry',
             }
             move_vals_list.append(move_vals)
         return move_vals_list
