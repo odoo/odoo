982d195963b3d010c5aa69a90ef3d6ea5899453d
Jorge Pinna Puissant <jpp@odoo.com>
2020-01-16 13:30:55 +0000

    [FIX] website_sale_digital: search domain for attachment count
    Before this commit, the search domain returns no results.
    
    Fine-tuning of 77077ca1db2c998cedcb0c41f12accc65c8304e3
    
    opw-2172563
    
    X-original-commit: f3d178c329eeb4d840c24801247fc5da39df998c

--- addons/website_sale_digital/models/product.py
+++ addons/website_sale_digital/models/product.py
@@ -40,8 +40,8 @@ class Product(models.Model):
         for product in self:
             product.attachment_count = self.env['ir.attachment'].search_count([
                 '|',
-                ('res_model', '=', 'product.template'), ('res_id', '=', product.product_tmpl_id.id), ('product_downloadable', '=', True),
-                ('res_model', '=', 'product.product'), ('res_id', '=', product.id), ('product_downloadable', '=', True)])
+                '&', '&', ('res_model', '=', 'product.template'), ('res_id', '=', product.product_tmpl_id.id), ('product_downloadable', '=', True),
+                '&', '&', ('res_model', '=', 'product.product'), ('res_id', '=', product.id), ('product_downloadable', '=', True)])
 
     def action_open_attachments(self):
         self.ensure_one()
