36e4ff2c4caa1c9b704c7304228ffb9384840b39
Yannick Tivisse <yti@odoo.com>
2020-01-16 11:25:59 +0000

    [FIX] mail: Clean residual context keys on _message_track_post_template
    Purpose
    =======
    
    Clean the context to get rid of residual default_* keys
    that could cause issues afterward during the mail.message
    generation. Example: 'default_parent_id' would refer to
    the parent_id of the current record that was used during
    its creation, but could refer to wrong parent message id,
    leading to a traceback in case the related message_id
    doesn't exist
    
    Taskid: 2176445
    X-original-commit: 266eeb9373e4350e82d6c9c7d81ecd3c7148e0ed

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -575,6 +575,14 @@ class MailThread(models.AbstractModel):
     def _message_track_post_template(self, tracking):
         if not any(change for rec_id, (change, tracking_value_ids) in tracking.items()):
             return True
+        # Clean the context to get rid of residual default_* keys
+        # that could cause issues afterward during the mail.message
+        # generation. Example: 'default_parent_id' would refer to
+        # the parent_id of the current record that was used during
+        # its creation, but could refer to wrong parent message id,
+        # leading to a traceback in case the related message_id
+        # doesn't exist
+        self = self.with_context(clean_context(self._context))
         templates = self._track_template(tracking)
         for field_name, (template, post_kwargs) in templates.items():
             if not template:
