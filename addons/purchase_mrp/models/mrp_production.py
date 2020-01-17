9d98c43581e2579f43b35541b43264866dede5a5
Simon Lejeune <sle@openerp.com>
2020-01-17 16:00:51 +0000

    [REF] purchase_stock: push the extra quantity
    With a product configured as buy on order and a warehouse configured as
    receipt in two steps, if the user increases the quantity on a po line
    generated by a sale order before confirming it, the system will send all
    the quantity to input but only the ordered quantity to customer. The
    issue is that the "extra quantity" will stay in input.
    
    We fix this issue by creating a new move with the extra quantity to the
    input location so that push rules will send the extra quantity to stock
    while the ordered quantity will be sent to the customer.
    
    There was also an issue when incrementing the quantity on the po line
    after confirmation if the po line was the result of a reordering rule:
    only a move from supplier to the location of the reordering rule was
    created.
    
    This commit also introduces a change of semantic:
    `created_purchase_line_id` is cleared after confirming the RFQ. This
    allows to merge more in `_merge_moves`.
    
    task-1981355
    
    closes odoo/odoo#43545
    
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- addons/purchase_mrp/models/mrp_production.py
+++ addons/purchase_mrp/models/mrp_production.py
@@ -12,14 +12,15 @@ class MrpProduction(models.Model):
         compute='_compute_purchase_order_count',
         groups='purchase.group_purchase_user')
 
-    @api.depends('procurement_group_id.stock_move_ids.created_purchase_line_id.order_id')
+    @api.depends('procurement_group_id.stock_move_ids.created_purchase_line_id.order_id', 'procurement_group_id.stock_move_ids.move_orig_ids.purchase_line_id.order_id')
     def _compute_purchase_order_count(self):
         for production in self:
-            production.purchase_order_count = len(production.procurement_group_id.stock_move_ids.created_purchase_line_id.order_id)
+            production.purchase_order_count = len(production.procurement_group_id.stock_move_ids.created_purchase_line_id.order_id |
+                                                  production.procurement_group_id.stock_move_ids.move_orig_ids.purchase_line_id.order_id)
 
     def action_view_purchase_orders(self):
         self.ensure_one()
-        purchase_order_ids = self.procurement_group_id.stock_move_ids.created_purchase_line_id.order_id.ids
+        purchase_order_ids = (self.procurement_group_id.stock_move_ids.created_purchase_line_id.order_id | self.procurement_group_id.stock_move_ids.move_orig_ids.purchase_line_id.order_id).ids
         action = {
             'res_model': 'purchase.order',
             'type': 'ir.actions.act_window',
