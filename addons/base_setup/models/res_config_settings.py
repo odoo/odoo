3c8b1432cbdc95ff31ee422bcc88130372b0fe0d
wan <wan@odoo.com>
2020-01-21 11:48:05 +0000

    [IMP] inter_company_rules: split sale/purchase and account
    Task 2080625
    There are now 2 modules:
    * account_inter_company_rules
    * sale_purchase_inter_company_rules
    
    This allows to use the feature without having to install sale and
    purchase.
    
    closes odoo/odoo#43039
    
    Related: odoo/enterprise#7636
    Signed-off-by: Quentin De Paoli (qdp) <qdp@openerp.com>

--- addons/base_setup/models/res_config_settings.py
+++ addons/base_setup/models/res_config_settings.py
@@ -24,7 +24,7 @@ class ResConfigSettings(models.TransientModel):
     module_auth_oauth = fields.Boolean("Use external authentication providers (OAuth)")
     module_auth_ldap = fields.Boolean("LDAP Authentication")
     module_base_gengo = fields.Boolean("Translate Your Website with Gengo")
-    module_inter_company_rules = fields.Boolean("Manage Inter Company")
+    module_account_inter_company_rules = fields.Boolean("Manage Inter Company")
     module_pad = fields.Boolean("Collaborative Pads")
     module_voip = fields.Boolean("Asterisk (VoIP)")
     module_web_unsplash = fields.Boolean("Unsplash Image Library")
