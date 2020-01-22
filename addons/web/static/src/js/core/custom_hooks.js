a3e24efc19e68dc838f645413cd392a64a9e3d8b
Aaron Bohy <aab@odoo.com>
2020-01-22 14:27:17 +0000

    [REF] web: remove custom useExternalListener hook
    This hook is now available in owl directly, so we remove its custom
    definition in Odoo, and adapt the code using it.

--- addons/web/static/src/js/core/custom_hooks.js
+++ addons/web/static/src/js/core/custom_hooks.js
@@ -1,4 +1,4 @@
-odoo.define('web.custom_hooks', function (require) {
+odoo.define('web.custom_hooks', function () {
     "use strict";
 
     const { Component, hooks } = owl;
@@ -37,33 +37,7 @@ odoo.define('web.custom_hooks', function (require) {
         };
     }
 
-    /**
-     * When component needs to listen to DOM Events on element(s) that is not part of his hierarchy, we can use
-     * `useExternalListener` hook.
-     * It will correctly add and remove the event listener.
-     *
-     * Example:
-     *  a menu needs to listen to the click on window to be closed automatically
-     *
-     * Usage:
-     *  in the constructor of the OWL component that needs to be notified,
-     *  `useExternalListener(window, 'click', this._doSomething);` listen to the click event on window and call
-     *  `this._doSomething` function of the component when the click happened
-     *
-     * @param {EventTarget} target
-     * @param {string} eventName
-     * @param {Function} handler
-     * @param {(Object|boolean)} [eventParams]
-     */
-    function useExternalListener(target, eventName, handler, eventParams) {
-        const boundHandler = handler.bind(Component.current);
-
-        onMounted(() => target.addEventListener(eventName, boundHandler, eventParams));
-        onWillUnmount(() => target.removeEventListener(eventName, boundHandler, eventParams));
-    }
-
     return {
         useFocusOnUpdate,
-        useExternalListener,
     };
 });
