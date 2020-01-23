98e01fee1ae3067cbf4794f8e2413047efb5ba15
jbm-odoo <jbm@odoo.com>
2020-01-23 08:56:00 +0000

    [FIX] stock_account: Avoid divide by 0
    closes odoo/odoo#43188
    
    Related: odoo/enterprise#7676
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -605,6 +605,9 @@ class ProductProduct(models.Model):
         """
         self.ensure_one()
 
+        if not qty_to_invoice:
+            return 0
+
         candidates = stock_moves\
             .sudo()\
             .mapped('stock_valuation_layer_ids')\
