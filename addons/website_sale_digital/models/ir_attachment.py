07a5af87d887ab8006d5e09a9346ffcc5cff0a7b
KARARA Mohamed <kararamohamed90@gmail.com>
2020-01-25 20:03:09 +0100

    [FIX] website_sale_digital: download file without having access Improper access control in ir.attachment for the public user, allows remote authenticated attackers to access sensitive information via crafted RPC requests

--- addons/website_sale_digital/models/ir_attachment.py
+++ addons/website_sale_digital/models/ir_attachment.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api, _
+from odoo.exceptions import AccessError
 
 
 class Attachment(models.Model):
@@ -9,3 +10,13 @@ class Attachment(models.Model):
     _inherit = ['ir.attachment']
 
     product_downloadable = fields.Boolean("Downloadable from product portal", default=False)
+
+    @api.model
+    def check(self, mode, values=None):
+        if self:
+            self._cr.execute('SELECT product_downloadable FROM ir_attachment WHERE id IN %s', [tuple(self.ids)])
+            for product_downloadable, in self._cr.fetchall():
+                if product_downloadable and not (self.env.user._is_admin() or self.env.user.has_group('base.group_user')):
+                    raise AccessError(_("Sorry, you are not allowed to access this document."))
+
+        super(Attachment, self).check(mode, values)
