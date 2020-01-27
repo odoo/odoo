78a7a4cf9a7596f5cea89a28bee880d60b9b0628
Samuel Degueldre <sad@odoo.com>
2020-01-27 14:22:13 +0000

    [FIX] website_blog: fix next post loading animation
    Previously, when clicking on the next post at the bottom of a blog post,
    a spinning loader circle would always appear in the bottom left of the
    screen in an incorrect position. This commit removes that spinner.
    
    This commit also adds support for the half-screen cover size, which
    until now would animate as though it was full screen. All in all this
    just makes the transition animation much more seamless, which was the
    point of having it in the first place.
    
    task-2166790
    
    closes odoo/odoo#44198
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website_blog/static/src/js/website_blog.js
+++ addons/website_blog/static/src/js/website_blog.js
@@ -32,10 +32,13 @@ publicWidget.registry.websiteBlog = publicWidget.Widget.extend({
         var self = this;
         var $el = $(ev.currentTarget);
         var nexInfo = $el.find('#o_wblog_next_post_info').data();
-
-        $el.css('height', $(window).height())
-           .find('.o_record_cover_container').addClass(nexInfo.size + ' ' + nexInfo.text).end()
+        $el.find('.o_record_cover_container').addClass(nexInfo.size + ' ' + nexInfo.text).end()
            .find('.o_wblog_toggle').toggleClass('d-none');
+        // Appending a placeholder so that the cover can scroll to the top of the
+        // screen, regardless of its height.
+        const placeholder = document.createElement('div');
+        placeholder.style.minHeight = '100vh';
+        this.$('#o_wblog_next_container').append(placeholder);
 
         // Use _.defer to calculate the 'offset()'' only after that size classes
         // have been applyed and that $el has been resized.
