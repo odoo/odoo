89b3c7ca73a732738cdc02324465a03e82d05593
jbw <jbw@odoo.com>
2020-01-27 13:00:27 +0100

    [IMP] hr_expense enable attachment preview in journal view
    Adding preview of expenses attachment to journal view.
    
    When creating an expense, files can be attached (e.g. scanned receipts).
    Those attachment files where not displayed in "Accounting > Accounting > Journals > Purchases" move_line list.
    They now appear in the document preview.
    
    Task = 2176443

--- addons/hr_expense/models/account_move_line.py
+++ addons/hr_expense/models/account_move_line.py
@@ -10,6 +10,12 @@ class AccountMoveLine(models.Model):
 
     expense_id = fields.Many2one('hr.expense', string='Expense', copy=False, help="Expense where the move line come from")
 
+    def _get_attachment_domains(self):
+        attachment_domains = super(AccountMoveLine, self)._get_attachment_domains()
+        if self.expense_id:
+            attachment_domains.append([('res_model', '=', 'hr.expense'), ('res_id', '=', self.expense_id.id)])
+        return attachment_domains
+
     def reconcile(self, writeoff_acc_id=False, writeoff_journal_id=False):
         res = super(AccountMoveLine, self).reconcile(writeoff_acc_id=writeoff_acc_id, writeoff_journal_id=writeoff_journal_id)
         account_move_ids = [l.move_id.id for l in self if float_compare(l.move_id._get_cash_basis_matched_percentage(), 1, precision_digits=5) == 0]
