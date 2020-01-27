a5d28af1cc660175a9bf8839dcb3b050e5571b3f
Robin Heinz <rhe@odoo.com>
2020-01-27 14:16:56 +0000

    [FIX] point_of_sale: sale details from frontend.
    Before when we tried to print the sale report ticket from the front end, there was a traceback.
    Now use the correct values to prevent the traceback.
    
    closes odoo/odoo#44043
    
    X-original-commit: c5da82b7e4ec2310f13594508e22328139466147
    Signed-off-by: pimodoo <pimodoo@users.noreply.github.com>

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -729,7 +729,7 @@ class ReportSaleDetails(models.AbstractModel):
         domain = [('state', 'in', ['paid','invoiced','done'])]
 
         if (session_ids):
-            domain = AND([domain, [('session_id', 'in', session_ids.ids)]])
+            domain = AND([domain, [('session_id', 'in', session_ids)]])
         else:
             if date_start:
                 date_start = fields.Datetime.from_string(date_start)
@@ -754,7 +754,7 @@ class ReportSaleDetails(models.AbstractModel):
             ])
 
             if config_ids:
-                domain = AND([domain, [('config_id', 'in', config_ids.ids)]])
+                domain = AND([domain, [('config_id', 'in', config_ids)]])
 
         orders = self.env['pos.order'].search(domain)
 
@@ -821,7 +821,7 @@ class ReportSaleDetails(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         data = dict(data or {})
         configs = self.env['pos.config'].browse(data['config_ids'])
-        data.update(self.get_sale_details(data['date_start'], data['date_stop'], configs))
+        data.update(self.get_sale_details(data['date_start'], data['date_stop'], configs.ids))
         return data
 
 class AccountCashRounding(models.Model):
