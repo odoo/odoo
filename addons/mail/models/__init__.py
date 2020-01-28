a1b01751b558ac3f85478e3f1ea273f950083894
Siddarth Gajjar <sga@odoo.com>
2020-01-28 10:45:46 +0000

    [IMP] mail: prevent from re-using aliases
    PURPOSE
    
    Make aliases usage more unique, prevent re-using bounce or catchall aliases
    and remove auto-uniqueness of aliases.
    
    SPECIFICATIONS
    
    Before this commit, user could create an email alias having same name as
    catchall/bounce email alias, which should not happen. Also, while creating
    or duplicating alias, if the same name was already available, a new unique
    name was generated by adding a sequence number to existing name.
    
    This behavior is not considered as a good one as it magically creates aliases
    different from what user expects. User could even not own the newly-created
    alias, leading to a broken mail gateway.
    
    In this commit we improve that behavior by ensuring that no duplicate alias
    name should be entered while creation / updation for both catcall/bounce and
    mail alias. Also, while duplicating an alias, name will now be blank by default
    to force user to enter the name. Finally when creating an alias an error is
    raised if the name is already taken.
    
    Task ID 2160070

--- addons/mail/models/__init__.py
+++ addons/mail/models/__init__.py
@@ -26,6 +26,7 @@ from . import ir_action_act_window
 from . import ir_actions
 from . import ir_attachment
 from . import ir_autovacuum
+from . import ir_config_parameter
 from . import ir_http
 from . import ir_model
 from . import ir_model_fields
