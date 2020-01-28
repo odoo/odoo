7ef76144ee6e7bd8e67f9869889f5a65caf26d2a
Dharmraj Jhala <dja@odoo.com>
2020-01-28 12:00:48 +0000

    [IMP] sms: improve SMS preview without existing record behavior and code
    This commit clean-ups the code for model `sms.template.preview` which
    mainly includes the following changes :
    
     * remove 'res_id' field and use only the reference field instead;
     * remove inheritance from `sms.template` as we only need a body field
       computed based on record and template;
     * convert onchange of lang / record to compute field for body;
     * fix 'No Records' warning which previously appeared even if there were
       records to be previewed (by adding compute field 'no_records');
    
    LINKS
    
    TaskID 2180132
    Community PR odoo/odoo#44104
    Upgrade PR odoo/upgrade#763
    
    Related: odoo/upgrade#763
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/sms/wizard/sms_template_preview.py
+++ addons/sms/wizard/sms_template_preview.py
@@ -5,7 +5,6 @@ from odoo import api, fields, models
 
 
 class SMSTemplatePreview(models.TransientModel):
-    _inherit = "sms.template"
     _name = "sms.template.preview"
     _description = "SMS Template Preview"
 
@@ -21,36 +20,31 @@ class SMSTemplatePreview(models.TransientModel):
     @api.model
     def default_get(self, fields):
         result = super(SMSTemplatePreview, self).default_get(fields)
-        sms_template = self._context.get('default_sms_template_id') and self.env['sms.template'].browse(self._context['default_sms_template_id']) or False
-        if sms_template and not result.get('res_id'):
-            result['res_id'] = self.env[sms_template.model].search([], limit=1)
+        sms_template_id = self.env.context.get('default_sms_template_id')
+        if not sms_template_id or 'resource_ref' not in fields:
+            return result
+        sms_template = self.env['sms.template'].browse(sms_template_id)
+        res = self.env[sms_template.model_id.model].search([], limit=1)
+        if res:
+            result['resource_ref'] = '%s,%s' % (sms_template.model_id.model, res.id)
         return result
 
     sms_template_id = fields.Many2one('sms.template') # NOTE This should probably be required
 
     lang = fields.Selection(_selection_languages, string='Template Preview Language')
     model_id = fields.Many2one('ir.model', related="sms_template_id.model_id")
-    res_id = fields.Integer(string='Record ID')
-    resource_ref = fields.Reference(string='Record reference', selection='_selection_target_model', compute='_compute_resource_ref', inverse='_inverse_resource_ref')
+    body = fields.Char('Body', compute='_compute_sms_template_fields')
+    resource_ref = fields.Reference(string='Record reference', selection='_selection_target_model')
+    no_record = fields.Boolean('No Record', compute='_compute_no_record')
 
-    @api.depends('model_id', 'res_id')
-    def _compute_resource_ref(self):
+    @api.depends('model_id')
+    def _compute_no_record(self):
         for preview in self:
-            if preview.model_id:
-                preview.resource_ref = '%s,%s' % (preview.model_id.model, preview.res_id or 0)
-            else:
-                preview.resource_ref = False
+            preview.no_record = (self.env[preview.model_id.model].search_count([]) == 0) if preview.model_id else True
 
-    def _inverse_resource_ref(self):
-        for preview in self:
-            if preview.resource_ref:
-                preview.res_id = preview.resource_ref.id
-
-    @api.onchange('lang', 'resource_ref')
-    def on_change_resource_ref(self):
-        # Update res_id and body depending of the resource_ref
-        if self.resource_ref:
-            self.res_id = self.resource_ref.id
-        if self.sms_template_id:
-            template = self.sms_template_id.with_context(lang=self.lang)
-            self.body = template._render_template(template.body, template.model, self.res_id or 0)
+    @api.depends('lang', 'resource_ref')
+    def _compute_sms_template_fields(self):
+        for wizard in self:
+            # Update body depending of the resource_ref
+            sms_template = wizard.sms_template_id.with_context(lang=wizard.lang)
+            wizard.body = sms_template._render_template(sms_template.body, sms_template.model, wizard.resource_ref.id) if wizard.resource_ref else sms_template.body
