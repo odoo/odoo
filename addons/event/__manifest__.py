f7e1238fedf59bc35b2bfbd6cede9b9714e5d395
Thibault Delavallée <tde@odoo.com>
2020-01-28 11:32:29 +0000

    [MOV][IMP] event: improve some views and event main menus definitions
    PURPOSE
    
    This commit is part of ticket model support directly in event application.
    
    SPECIFICATIONS
    
    In this commit we move menu definitions in their own file. It eases views
    split and move, as otherwise there are dependencies between files that are
    complicated to solve. Better to have menus defined in a main file, and updating
    them at will afterwards.
    
    We also improve menus sequences and groups, as some were missing or badly
    defined.
    
    Registration tree view is slightly updated to include phone and mobile numbers
    as those are used notably for communication. They are hidden by default
    like email.
    
    Finally a small tweak of event kanban view is performed, notably to have
    a better display with dependencies installed. We ensure all cards have the
    same height.
    
    LINKS
    
    Task ID 2177281
    Community PR #43488
    
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>
    Co-Authored-By: Stéphane Debauche <std@odoo.com>

--- addons/event/__manifest__.py
+++ addons/event/__manifest__.py
@@ -21,6 +21,7 @@ Key Features
     'data': [
         'security/event_security.xml',
         'security/ir.model.access.csv',
+        'views/event_menu_views.xml',
         'views/event_views.xml',
         'views/event_stage_views.xml',
         'report/event_event_templates.xml',
