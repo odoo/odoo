8eedd339de073141ee2ff2af7388e4809a7076e0
Nicolas Martinelli <nim@odoo.com>
2020-01-29 12:54:54 +0100

    [FIX] stock: remove zero quant
    Partial backport of c0d7a05730dca242ad6 in order to remove quants with
    quantity of zero after merging.
    
    opw-2181684
    
    X-original-commit: ee95e3b2fdfa06409b1f74680e830f0bd3bb90a9

--- addons/stock/models/procurement.py
+++ addons/stock/models/procurement.py
@@ -249,6 +249,7 @@ class ProcurementGroup(models.Model):
 
         # Merge duplicated quants
         self.env['stock.quant']._merge_quants()
+        self.env['stock.quant']._unlink_zero_quants()
         if use_new_cursor:
             self._cr.commit()
 
