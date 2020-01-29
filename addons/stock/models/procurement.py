0f71cb10281f3a951cc5545f01d4213000962f12
Nicolas Martinelli <nim@odoo.com>
2020-01-29 12:54:54 +0100

    [FIX] stock: remove zero quant
    Partial backport of c0d7a05730dca242ad6 in order to remove quants with
    quantity of zero after merging.
    
    opw-2181684

--- addons/stock/models/procurement.py
+++ addons/stock/models/procurement.py
@@ -259,6 +259,7 @@ class ProcurementGroup(models.Model):
 
         # Merge duplicated quants
         self.env['stock.quant']._merge_quants()
+        self.env['stock.quant']._unlink_zero_quants()
         if use_new_cursor:
             self._cr.commit()
 
--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -293,6 +293,22 @@ class StockQuant(models.Model):
         return reserved_quants
 
     @api.model
+    def _unlink_zero_quants(self):
+        """ _update_available_quantity may leave quants with no
+        quantity and no reserved_quantity. It used to directly unlink
+        these zero quants but this proved to hurt the performance as
+        this method is often called in batch and each unlink invalidate
+        the cache. We defer the calls to unlink in this method.
+        """
+        precision_digits = max(6, self.env.ref('product.decimal_product_uom').digits * 2)
+        # Use a select instead of ORM search for UoM robustness.
+        query = """SELECT id FROM stock_quant WHERE round(quantity::numeric, %s) = 0 AND round(reserved_quantity::numeric, %s) = 0;"""
+        params = (precision_digits, precision_digits)
+        self.env.cr.execute(query, params)
+        quant_ids = self.env['stock.quant'].browse([quant['id'] for quant in self.env.cr.dictfetchall()])
+        quant_ids.sudo().unlink()
+
+    @api.model
     def _merge_quants(self):
         """ In a situation where one transaction is updating a quant via
         `_update_available_quantity` and another concurrent one calls this function with the same
