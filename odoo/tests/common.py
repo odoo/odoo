94a60a039e03c4daffbda8f4e53e7344374ca503
Xavier Morel <xmo@odoo.com>
2020-01-30 13:30:34 +0000

    [IMP] core: only show JS stacktrace for exceptions & console.trace
    When revamping the message fetching from the headless browser, since
    stacktraces are available (by default) on console.error and
    console.warning events I assumed it could / would be useful to show
    them in the Python-level log. And they *were* quite useful during the
    original fixing stage.
    
    However they turn out not to be very useful day-to-day:
    
    * they add a lot of noise and lead to the error message itself being
      lost in a big block of red / logging.error
    * when a tour fails (which is the vast majority of the failures) the
      JS stacktrace always points to the same location in the tour
      manager (the one which goes "this step never succeeded") which is
      completely useless
    * aside from being bundled, normal JS code (where the stacktrace could
      be useful) doesn't generally use console.warn or console.error, it's
      going to straight blow up with an exception in which case we always
      get a stacktrace
    
    Leave the stacktrace formatting for console.trace as that's pretty
    much the only point of using this instead of console.log.
    
    closes odoo/odoo#44343
    
    X-original-commit: 34500853f39c9557852cb83cbc274f59d803922f
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -1103,7 +1103,7 @@ class ChromeBrowser():
 
     LINE_PATTERN = '\tat %(functionName)s (%(url)s:%(lineNumber)d:%(columnNumber)d)\n'
     def _format_stack(self, logrecord):
-        if logrecord['type'] not in ('error', 'trace', 'warning'):
+        if logrecord['type'] not in ['trace']:
             return
 
         trace = logrecord.get('stackTrace')
