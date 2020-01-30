57b386d651e8b5638da0613c625efdea49116fa3
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2020-01-30 13:15:00 +0100

    [FIX] point_of_sale: send receipt ticket as attachment
    Open POS, make an order, on the payment screen select email, select a
    customer, pay and validate.
    
    The receipt will be not an attachment, but a code image. The side effect
    is the visualization dependant on the viewer (email client, webmail,
    etc.)
    
    opw-2179303

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -652,12 +652,24 @@ class PosOrder(models.Model):
         orders = self.browse(order_ids) if order_ids else self
 
         message = _("<p>Dear %s,<br/>Here is your electronic ticket for the %s. </p>") % (client['name'], name)
+
+        filename = 'Receipt-' + name + '.jpg'
+        receipt = self.env['ir.attachment'].create({
+            'name': filename,
+            'type': 'binary',
+            'datas': ticket,
+            'res_model': 'pos_order',
+            'res_id': orders.id,
+            'store_fname': filename,
+            'mimetype': 'image/jpeg',
+        })
         template_data = {
             'subject': _('Receipt %s') % name,
-            'body_html': message + '<img src="data:image/jpeg;base64,%s"/>' % ticket,
+            'body_html': message,
             'author_id': self.env.user.partner_id.id,
             'email_from': self.env.company.email or self.env.user.email_formatted,
-            'email_to': client['email']
+            'email_to': client['email'],
+            'attachment_ids': [(4, receipt.id, 0)]
         }
 
         if orders.mapped('account_move'):
@@ -669,10 +681,10 @@ class PosOrder(models.Model):
                 'datas': base64.b64encode(report[0]),
                 'store_fname': filename,
                 'res_model': 'account.move',
-                'res_id': order_ids[0],
+                'res_id': orders.account_move.id,
                 'mimetype': 'application/x-pdf'
             })
-            template_data['attachment_ids'] = attachment
+            template_data['attachment_ids'] += [(4, attachment.id, 0)]
 
         mail = self.env['mail.mail'].create(template_data)
         mail.send()
