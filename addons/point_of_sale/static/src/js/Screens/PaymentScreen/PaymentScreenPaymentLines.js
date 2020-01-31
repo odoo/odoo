09cfde14bd086bd190a8c34a157557fa363d0438
Pierre Masereel <pim@odoo.com>
2020-01-31 13:08:42 +0000

    [IMP] point_of_sale: responsive UI
    The point of sale user interface has been adapted to work on mobile
    devices.
    
    We modified the design of payment lines to look like payment methods and
    stack them together in the mobile view.
    
    When you had a cash method, the amount was not preffilled during
    payment, now we always prefill the amount to keep it more simple.
    
    closes odoo/odoo#51891
    
    Task-id: 1887609
    Related: odoo/upgrade#1334
    Signed-off-by: pimodoo <pimodoo@users.noreply.github.com>

--- addons/point_of_sale/static/src/js/Screens/PaymentScreen/PaymentScreenPaymentLines.js
+++ addons/point_of_sale/static/src/js/Screens/PaymentScreen/PaymentScreenPaymentLines.js
@@ -8,22 +8,6 @@ odoo.define('point_of_sale.PaymentScreenPaymentLines', function(require) {
         formatLineAmount(paymentline) {
             return this.env.pos.format_currency_no_symbol(paymentline.get_amount());
         }
-        get changeText() {
-            return this.env.pos.format_currency(this.currentOrder.get_change());
-        }
-        get totalDueText() {
-            return this.env.pos.format_currency(
-                this.currentOrder.get_total_with_tax() + this.currentOrder.get_rounding_applied()
-            );
-        }
-        get remainingText() {
-            return this.env.pos.format_currency(
-                this.currentOrder.get_due() > 0 ? this.currentOrder.get_due() : 0
-            );
-        }
-        get currentOrder() {
-            return this.env.pos.get_order();
-        }
         selectedLineClass(line) {
             return { 'payment-terminal': line.get_payment_status() };
         }
