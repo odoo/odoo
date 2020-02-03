cb3ea72d6f33a20ed65790cbbfc380f9f6073fa4
Nicolas Martinelli <nim@odoo.com>
2020-02-03 11:16:48 +0000

    [FIX] account: do not subscribe commercial partner
    - Create a partner which is a company
    - Add a contact
    - Create an invoice for the contact, post it
    
    Both the contact and the company are set as followers of the invoice.
    
    Only the contact should be, there is no reason to make the company
    follower too.
    
    opw-2179084
    
    closes odoo/odoo#44624
    
    X-original-commit: daedd9eb402e5dd3c6a700a93ee58022266ec95b
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -2114,7 +2114,7 @@ class AccountMove(models.Model):
             if move.auto_post and move.date > fields.Date.today():
                 raise UserError(_("This move is configured to be auto-posted on {}".format(move.date.strftime(get_lang(self.env).date_format))))
 
-            move.message_subscribe([p.id for p in [move.partner_id, move.commercial_partner_id] if p not in move.sudo().message_partner_ids])
+            move.message_subscribe([p.id for p in [move.partner_id] if p not in move.sudo().message_partner_ids])
 
             to_write = {'state': 'posted'}
 
