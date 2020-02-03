1cf11fd6c0d72024d987eadf2b3431824887fc5a
Sébastien Theys <seb@odoo.com>
2020-02-03 13:05:11 +0000

    [FIX] website_sale: prevent issue with test shop_list_view_b2c
    It very rarely happens that the test is failing due to the following error:
    
    `Tour shop_list_view_b2c failed at step
    .oe_product_cart .oe_currency_value:contains("825.00")`
    
    The value found in that case is 750 instead of 825, which indicates that the b2c
    setting was not correctly activated.
    
    To exclude the RPC themselves from the equation, it is decided here to move the
    setting change directly in the Python.
    
    closes odoo/odoo#44492
    
    Signed-off-by: Romain Derie <rdeodoo@users.noreply.github.com>

--- addons/website_sale/tests/test_customize.py
+++ addons/website_sale/tests/test_customize.py
@@ -214,4 +214,10 @@ class TestUi(odoo.tests.HttpCase):
         self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('tour_shop_no_variant_attribute')", "odoo.__DEBUG__.services['web_tour.tour'].tours.tour_shop_no_variant_attribute.ready", login="demo")
 
     def test_06_admin_list_view_b2c(self):
+        # activate b2c
+        config = self.env['res.config.settings'].create({})
+        config.show_line_subtotals_tax_selection = "tax_included"
+        config._onchange_sale_tax()
+        config.execute()
+
         self.browser_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_list_view_b2c')", "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_list_view_b2c.ready", login="admin")
