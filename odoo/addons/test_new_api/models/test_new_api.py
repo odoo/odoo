604b6a22db8eddfb37f9be31d3826394e25bd86c
Sébastien Theys <seb@odoo.com>
2020-02-04 11:33:50 +0000

    [FIX] core: fix _flush_search() when _order depends on m2o
    closes odoo/odoo#46316
    
    X-original-commit: a1ccb709b1d6c85227e0f6ca034872202bcfa0a2
    Signed-off-by: Raphael Collet (rco) <rco@openerp.com>
    Co-authored-by: Raphael Collet <rco@odoo.com>
    Co-authored-by: Sébastien Theys <seb@odoo.com>

--- odoo/addons/test_new_api/models/test_new_api.py
+++ odoo/addons/test_new_api/models/test_new_api.py
@@ -816,3 +816,20 @@ class ModelParentM2o(models.Model):
 
     name = fields.Char('Name')
     child_ids = fields.One2many('test_new_api.model_child_m2o', 'parent_id', string="Children")
+
+
+class Country(models.Model):
+    _name = 'test_new_api.country'
+    _description = 'Country, ordered by name'
+    _order = 'name, id'
+
+    name = fields.Char()
+
+
+class City(models.Model):
+    _name = 'test_new_api.city'
+    _description = 'City, ordered by country then name'
+    _order = 'country_id, name, id'
+
+    name = fields.Char()
+    country_id = fields.Many2one('test_new_api.country')
