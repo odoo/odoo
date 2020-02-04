3781f5a53736546f50134151798aefa2ae1bbab8
Debauche Stéphane <std@odoo.com>
2020-02-04 08:06:28 +0000

    [FIX] website_event(_questions): correctly parse frontend question answers
    When user register to an event from website, they can answer questions
    specific to a given registration or to all registrations. However they
    are not correctly parsed and not saved.
    
    Task 2188857
    PR #44545
    
    X-original-commit: e27d9b5c4f234f6ce64456c2f52e7cfb0331332f

--- addons/website_event_questions/controllers/main.py
+++ addons/website_event_questions/controllers/main.py
@@ -9,10 +9,20 @@ class WebsiteEvent(WebsiteEventController):
     def _process_attendees_form(self, event, form_details):
         ''' Process data posted from the attendee details form. '''
         registrations = super(WebsiteEvent, self)._process_attendees_form(event, form_details)
+
         for registration in registrations:
-            answer_ids = []
-            for key, value in registration.items():
-                if key.startswith('answer_ids-'):
-                    answer_ids.append([4, int(value)])
-            registration['answer_ids'] = answer_ids
+            registration['answer_ids'] = []
+
+        general_answer_ids = []
+        for key, value in form_details.items():
+            if 'answer_ids' in key:
+                index = int(key.split('-')[0])
+                if not index:
+                    general_answer_ids.append([4, int(value)])
+                else:
+                    registrations[index-1]['answer_ids'].append([4, int(value)])
+
+        for registration in registrations:
+            registration['answer_ids'].extend(general_answer_ids)
+
         return registrations
