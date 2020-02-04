5952928b42de348c91cc61c9615fbb49e9df4146
Adrian Torres <adt@odoo.com>
2020-02-04 11:57:37 +0000

    [REM] *: remove various unused import shims
    Before this commit, a lot of leftover import shims existed in the
    codebase for py2-py3 compatibility, these are no longer needed since
    Odoo 13.0+ doesn't support Python 2 anymore and is (finally) in EOL.
    
    With this commit, these shims are dropped, making the code cleaner,
    easier to read and with one less dependency.
    
    Queue -> queue -> py2-py3 compatibility
    xmlrpclib -> xmlrpc.client -> py2-py3 compatibility
    ConfigParser -> configparser -> py2-py3 compatibility
    itertools.izip_longest -> itertools.zip_longest -> py2-py3 compatibility
    urllib -> urllib.request -> py2-py3 compatibility
    __builtins__ -> builtins -> py2-py3 compatibility
    _winreg -> winreg -> py2-py3 compatibility
    
    mock -> unittest.mock -> merged into CPython
    
    The debian/fedora packages and requirements.txt have been updated accordingly
    
    closes odoo/odoo#44601
    
    Related: odoo/enterprise#8141
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/addons/test_new_api/tests/test_onchange.py
+++ odoo/addons/test_new_api/tests/test_onchange.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
+from unittest.mock import patch
 
 from odoo.addons.base.tests.common import SavepointCaseWithUserDemo
 from odoo.tests import common
