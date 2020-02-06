20df3588dcd3fd0b6b0c6dbb95941cceea853740
fja-odoo <fja@odoo.com>
2020-02-06 17:14:54 +0000

    [FIX] website_form, *: add submit btn position option
    * = website_crm, website_hr_recruitment, website_sale
    
    It is now possible to change the position of the submit button via
    the left panel. Indeed switching to a non-horizontal form left the
    submit button aligned with the labels which thus appeared broken.
    
    Part of https://github.com/odoo/odoo/pull/44907
    task-2191409
    
    X-original-commit: 60b4c8d04006736be573189accd0dccaafafa651

--- addons/website_form/static/src/snippets/s_website_form/options.js
+++ addons/website_form/static/src/snippets/s_website_form/options.js
@@ -33,13 +33,10 @@ const FormEditor = options.Class.extend({
         }
         // Set selection as records to avoid added conplexity
         if (field.type === 'selection') {
-            field.records = [];
-            field.selection.forEach(el => {
-                field.records.push({
-                    id: el[0],
-                    display_name: el[1],
-                });
-            });
+            field.records = field.selection.map(el => ({
+                id: el[0],
+                display_name: el[1],
+            }));
         } else if (field.relation && field.relation !== 'ir.attachment') {
             field.records = await this._rpc({
                 model: field.relation,
@@ -86,9 +83,8 @@ const FormEditor = options.Class.extend({
      * @returns {Object}
      */
     _getDefaultFormat: function () {
-        const formOptions = this.el.closest('.o_we_customize_panel');
         return {
-            labelWidth: formOptions && formOptions.querySelector('we-customizeblock-options [data-select-style] input').value + 'px',
+            labelWidth: this.$target[0].querySelector('.s_website_form_label').style.width,
             labelPosition: 'left',
             multiPosition: 'horizontal',
             requiredMark: this._isRequiredMark(),
