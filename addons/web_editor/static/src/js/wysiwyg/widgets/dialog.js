fec6ac26021105f3242b8993d5eea7b5d15e3206
Krupa Patel <kup@odoo.com>
2020-02-06 11:18:20 +0000

    [FIX] web_editor: add focus in url input of link dialog
    Before this commit, when clicking on the link button after focusing a
    link, the first input of the link dialog was hidden (label), and the
    second input (URL) was not focused.
    
    In this commit, we focus the first *visible* input so users are able to
    directly copy-paste URL.
    
    task-2083209
    
    closes odoo/odoo#44931
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/widgets/dialog.js
+++ addons/web_editor/static/src/js/wysiwyg/widgets/dialog.js
@@ -36,7 +36,7 @@ var SummernoteDialog = Dialog.extend({
 
         var self = this;
         this.opened(function () {
-            self.$('input:first').focus();
+            self.$('input:visible:first').focus();
             self.$el.closest('.modal').addClass('o_web_editor_dialog');
             self.$el.closest('.modal').on('hidden.bs.modal', self.options.onClose);
         });
