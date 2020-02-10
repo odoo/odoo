579825a6f19b23dc44cc00b3a306ddc9006e2995
David Beguin <dbe@odoo.com>
2020-02-10 15:35:20 +0100

    [IMP] survey: add conditional questions demo data
    Purpose
    =======
    
    This commit adds demo data to illustrate the conditional questions
    configuration. Users will be able to choose among 4 themes. Depending on their
    choice(s), the survey will continue by displaying only the questions related
    to the selected theme(s).
    
    Link
    ====
    
    Task ID: 23509
    PR #44950

--- addons/survey/__manifest__.py
+++ addons/survey/__manifest__.py
@@ -49,6 +49,7 @@ sent mails with personal token for the invitation of the survey.
         'data/survey_demo_certification.xml',
         'data/survey_demo_quiz.xml',
         'data/survey_demo_quiz_userinput.xml',
+        'data/survey_demo_conditional.xml',
         'data/survey.user_input.line.csv'
     ],
     'installable': True,
