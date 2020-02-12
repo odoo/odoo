ec3428900c59d79bd60990c8d357b35336a419bf
jerome hanke (jhk) <jhk@odoo.com>
2020-02-12 12:51:40 +0100

    [FIX] mail_bot: odoobot waits 1 week between notification requests
    Steps to reproduce:
    - install mail_bot
    - wait for odoobot to ask you if you want to receive notifications
    - do not select "grant" or "deny", click the closing cross
    
    Previous behavior:
    Odoobot would consider that you denied the request
    
    Current behavior:
    Odoobot waits 1 week and will ask you again
    
    opw-2186299
    
    X-original-commit: b7254ecaeee7f8fa564ca2059f119f5d12404af6

--- addons/mail_bot/static/src/js/mailbot_service.js
+++ addons/mail_bot/static/src/js/mailbot_service.js
@@ -4,6 +4,7 @@ odoo.define('mail_bot.MailBotService', function (require) {
 var AbstractService = require('web.AbstractService');
 var core = require('web.core');
 var session = require('web.session');
+var localStorage = require('web.local_storage');
 
 var _t = core._t;
 
@@ -13,7 +14,11 @@ var MailBotService =  AbstractService.extend({
      */
     start: function () {
         this._hasRequest = (window.Notification && window.Notification.permission === "default") || false;
+        var lastRequestTime = localStorage.getItem('odoobot_notification_last_request_time');
         if ('odoobot_initialized' in session && ! session.odoobot_initialized) {
+            this._showOdoobotTimeout();
+        }
+        else if (lastRequestTime !== null && (new Date().getTime() - parseInt(lastRequestTime) > 7*24*60*60*1000)) {
             this._showOdoobotTimeout();
         }
     },
