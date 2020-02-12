9d69bf49c5ca38ce588275f7fe984d7d3488f6f3
Rémy Baranx (bar) <bar@odoo.com>
2020-02-12 10:46:56 +0000

    [IMP] project_timesheet_holidays : convert onchange to stored-editable computed fields.
    The 'timesheet_generate' and 'timesheet_task_id' fields have been set as
    stored-editable computed fields.
    
    The condition to compute the timesheet_generat field has changed to be
    coherent with the _check_timesheet_generate constraint.
    
    Keep the timesheet_generate field without its default value (True) to avoid
    raising a ValidationError through the _check_timesheet_generate constraint
    before the timesheet_generate field has been computed.

--- addons/project_timesheet_holidays/models/res_config_settings.py
+++ addons/project_timesheet_holidays/models/res_config_settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models,api
 
 
 class ResConfigSettings(models.TransientModel):
@@ -9,3 +9,13 @@ class ResConfigSettings(models.TransientModel):
 
     leave_timesheet_project_id = fields.Many2one(related='company_id.leave_timesheet_project_id', string="Internal Project", domain="[('company_id', '=', company_id)]", readonly=False)
     leave_timesheet_task_id = fields.Many2one(related='company_id.leave_timesheet_task_id', string="Time Off Task", domain="[('company_id', '=', company_id)]", readonly=False)
+
+    @api.onchange('leave_timesheet_project_id')
+    def _onchange_timesheet_project_id(self):
+        if self.leave_timesheet_project_id != self.leave_timesheet_task_id.project_id:
+            self.leave_timesheet_task_id = False
+
+    @api.onchange('leave_timesheet_task_id')
+    def _onchange_timesheet_task_id(self):
+        if self.leave_timesheet_task_id:
+            self.leave_timesheet_project_id = self.leave_timesheet_task_id.project_id
