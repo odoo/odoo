52ac7f2ddb5c27a177f9bd0c088f2a8a08ab676f
Thibault Delavallée <tde@odoo.com>
2020-02-13 12:59:47 +0000

    [FIX] website(_crm): fix email and phone computation of visitor
    Currently there are several limitations in visitor email and phone
    computation, notably phone of partner is not taken into account (only mobile)
    and computation does not work in website_crm if there are no leads.
    
    In this commit we
    
      * support both mobile and phone fields of partner. Indeed generally all
        SMS flows check for those two fields and not only for mobile;
      * replace the SQL by standard ORM computation. As those fields are not stored
        and displayed in list views ORM should be capable of managing them;
      * avoid crash when creating visitors through the UI (no ids -> don't go
        into raw SQL);
      * correctly keep partner-based values when no lead is linked to the visitor;
    
    Tests are also added in order to ensure behavior.
    
    Task ID 2196869
    
    closes odoo/odoo#48369
    
    X-original-commit: 02ceb42e204be2b40b334b4e554eef6dfaffd228
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website/models/website_visitor.py
+++ addons/website/models/website_visitor.py
@@ -70,16 +70,16 @@ class WebsiteVisitor(models.Model):
             (record.name or _('Website Visitor #%s') % record.id)
         ) for record in self]
 
-    @api.depends('partner_id.email_normalized', 'partner_id.mobile')
+    @api.depends('partner_id.email_normalized', 'partner_id.mobile', 'partner_id.phone')
     def _compute_email_phone(self):
         results = self.env['res.partner'].search_read(
             [('id', 'in', self.partner_id.ids)],
-            ['id', 'email_normalized', 'mobile'],
+            ['id', 'email_normalized', 'mobile', 'phone'],
         )
         mapped_data = {
             result['id']: {
                 'email_normalized': result['email_normalized'],
-                'mobile': result['mobile']
+                'mobile': result['mobile'] if result['mobile'] else result['phone']
             } for result in results
         }
 
