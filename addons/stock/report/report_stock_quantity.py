odoo/odoo

commit d7da1b733b4cece73d26805e6371499026bed0cd

[FIX] stock: Correct forecast quantity with view location
Usecase to reproduce:
- Set the lot_stock_id as a view location
- Create a PO and validate it

-> The forecast quantity on stat button is correct but it's missing in
the forecast quantity report.

It happens because the condition for stock_move is based on valuation
strategy. However it's not correct since it's not True that a product
is in the stock if it's valuated (as subcontracting, owner, ...)

In order to fix it uses the warehouse in order to define if a move is in
or out (the same way than the _get_domain_locations on product).

addons/stock/report/report_stock_quantity.py
@@ -30,18 +30,18 @@ SELECT
     m.id,
     product_id,
     CASE
-        WHEN (ls.usage = 'internal' OR ls.usage = 'transit' AND ls.company_id IS NOT NULL) AND ld.usage != 'internal' THEN 'out'
-        WHEN ls.usage != 'internal' AND (ld.usage = 'internal' OR ld.usage = 'transit' AND ld.company_id IS NOT NULL) THEN 'in'
+        WHEN (whs.id IS NOT NULL AND whd.id IS NULL) OR ls.usage = 'transit' THEN 'out'
+        WHEN (whs.id IS NULL AND whd.id IS NOT NULL) OR ld.usage = 'transit' THEN 'in'
     END AS state,
     date_expected::date AS date,
     CASE
-        WHEN (ls.usage = 'internal' OR ls.usage = 'transit' AND ls.company_id IS NOT NULL) AND ld.usage != 'internal' THEN -product_qty
-        WHEN ls.usage != 'internal' AND (ld.usage = 'internal' OR ld.usage = 'transit' AND ld.company_id IS NOT NULL) THEN product_qty
+        WHEN (whs.id IS NOT NULL AND whd.id IS NULL) OR ls.usage = 'transit' THEN -product_qty
+        WHEN (whs.id IS NULL AND whd.id IS NOT NULL) OR ld.usage = 'transit' THEN product_qty
     END AS product_qty,
     m.company_id,
     CASE
-        WHEN (ls.usage = 'internal' OR ls.usage = 'transit' AND ls.company_id IS NOT NULL) AND ld.usage != 'internal' THEN whs.id
-        WHEN ls.usage != 'internal' AND (ld.usage = 'internal' OR ld.usage = 'transit' AND ld.company_id IS NOT NULL) THEN whd.id
+        WHEN (whs.id IS NOT NULL AND whd.id IS NULL) OR ls.usage = 'transit' THEN whs.id
+        WHEN (whs.id IS NULL AND whd.id IS NOT NULL) OR ld.usage = 'transit' THEN whd.id
     END AS warehouse_id
 FROM
     stock_move m
@@ -53,9 +53,8 @@ LEFT JOIN product_product pp on pp.id=m.product_id
 LEFT JOIN product_template pt on pt.id=pp.product_tmpl_id
 WHERE
     pt.type = 'product' AND
-    product_qty != 0 AND (
-    (ls.usage = 'internal' OR ls.usage = 'transit' AND ls.company_id IS NOT NULL) AND ld.usage != 'internal' OR
-    ls.usage != 'internal' AND (ld.usage = 'internal' OR ld.usage = 'transit' AND ld.company_id IS NOT NULL)) AND
+    product_qty != 0 AND
+    (whs.id IS NULL or whd.id IS NULL OR whs.id != whd.id) AND
     m.state NOT IN ('cancel', 'draft', 'done')
 UNION
 SELECT
@@ -89,15 +88,15 @@ SELECT
         ELSE date::date - interval '1 day'
     END, '1 day'::interval)::date date,
     CASE
-        WHEN (ls.usage = 'internal' OR ls.usage = 'transit' AND ls.company_id IS NOT NULL) AND ld.usage != 'internal' AND m.state = 'done' THEN product_qty
-        WHEN ls.usage != 'internal' AND (ld.usage = 'internal' OR ld.usage = 'transit' AND ld.company_id IS NOT NULL) AND m.state = 'done' THEN -product_qty
-        WHEN (ls.usage = 'internal' OR ls.usage = 'transit' AND ls.company_id IS NOT NULL) AND ld.usage != 'internal' THEN -product_qty
-        WHEN ls.usage != 'internal' AND (ld.usage = 'internal' OR ld.usage = 'transit' AND ld.company_id IS NOT NULL) THEN product_qty
+        WHEN (whs.id IS NOT NULL AND whd.id IS NULL) OR ls.usage = 'transit' AND m.state = 'done' THEN product_qty
+        WHEN (whs.id IS NULL AND whd.id IS NOT NULL) OR ld.usage = 'transit'  AND m.state = 'done' THEN -product_qty
+        WHEN (whs.id IS NOT NULL AND whd.id IS NULL) OR ls.usage = 'transit' THEN -product_qty
+        WHEN (whs.id IS NULL AND whd.id IS NOT NULL) OR ld.usage = 'transit' THEN product_qty
     END AS product_qty,
     m.company_id,
     CASE
-        WHEN (ls.usage = 'internal' OR ls.usage = 'transit' AND ls.company_id IS NOT NULL) AND ld.usage != 'internal' THEN whs.id
-        WHEN ls.usage != 'internal' AND (ld.usage = 'internal' OR ld.usage = 'transit' AND ld.company_id IS NOT NULL) THEN whd.id
+        WHEN (whs.id IS NOT NULL AND whd.id IS NULL) OR ls.usage = 'transit' THEN whs.id
+        WHEN (whs.id IS NULL AND whd.id IS NOT NULL) OR ld.usage = 'transit' THEN whd.id
     END AS warehouse_id
 FROM
     stock_move m
@@ -109,9 +108,8 @@ LEFT JOIN product_product pp on pp.id=m.product_id
 LEFT JOIN product_template pt on pt.id=pp.product_tmpl_id
 WHERE
     pt.type = 'product' AND
-    product_qty != 0 AND (
-    (ls.usage = 'internal' OR ls.usage = 'transit' AND ls.company_id IS NOT NULL) AND ld.usage != 'internal' OR
-    ls.usage != 'internal' AND (ld.usage = 'internal' OR ld.usage = 'transit' AND ld.company_id IS NOT NULL)) AND
+    product_qty != 0 AND
+    (whs.id IS NULL or whd.id IS NULL OR whs.id != whd.id) AND
     m.state NOT IN ('cancel', 'draft')
 );
 """

commit 620df11e7c510d693285ce265ea2d3a4dab48a54

[FIX] stock: forecast report
- have 3 units in stock
- confirm a receipt of 2 units
- the forecast is 1 instead of 5

The bug was introduced by rev d7da1b733b4cece73d26805e6371499026bed0cd

addons/stock/report/report_stock_quantity.py
@@ -88,8 +88,8 @@ SELECT
         ELSE date::date - interval '1 day'
     END, '1 day'::interval)::date date,
     CASE
-        WHEN (whs.id IS NOT NULL AND whd.id IS NULL) OR ls.usage = 'transit' AND m.state = 'done' THEN product_qty
-        WHEN (whs.id IS NULL AND whd.id IS NOT NULL) OR ld.usage = 'transit'  AND m.state = 'done' THEN -product_qty
+        WHEN ((whs.id IS NOT NULL AND whd.id IS NULL) OR ls.usage = 'transit') AND m.state = 'done' THEN product_qty
+        WHEN ((whs.id IS NULL AND whd.id IS NOT NULL) OR ld.usage = 'transit') AND m.state = 'done' THEN -product_qty
         WHEN (whs.id IS NOT NULL AND whd.id IS NULL) OR ls.usage = 'transit' THEN -product_qty
         WHEN (whs.id IS NULL AND whd.id IS NOT NULL) OR ld.usage = 'transit' THEN product_qty
     END AS product_qty,