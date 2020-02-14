b08665217e27ba9ae6f4e9339bbe2e5666e04f77
Patrick Hoste <pko@odoo.com>
2020-02-14 12:55:55 +0000

    [IMP] (website_)event: add new tour to demonstrate new features
    PURPOSE
    
    Create a tour for Events to introduce new users to this freshly
    revamped application.
    
    SPECIFICATIONS
    
    In 'website_event' module the tour steps extends the 'event' tour steps
    
    LINKS
    
    Task ID : 2180175
    PR : #45612
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ addons/website_event/static/src/js/tours/event_tour.js
@@ -0,0 +1,57 @@
+odoo.define('website_event.event_steps', function (require) {
+"use strict";
+
+var core = require('web.core');
+var _t = core._t;
+
+var EventAdditionalTourSteps = require('event.event_steps');
+
+EventAdditionalTourSteps.include({
+
+    init: function() {
+        this._super.apply(this, arguments);
+    },
+
+    _get_website_event_steps: function () {
+        this._super.apply(this, arguments);
+        return [{
+                trigger: 'button[name="is_published"]',
+                extra_trigger: 'div.o_form_buttons_view:not(.o_hidden)',
+                content: _t("Use this <b>shortcut</b> to easily access your event web page."),
+                position: 'bottom',
+            }, {
+                trigger: 'li#edit-page-menu a',
+                content: _t("With the Edit button, you can <b>customize</b> the web page visitors will see when registrating."),
+                position: 'bottom',
+            }, {
+                trigger: 'div[name="Image - Text"] .oe_snippet_thumbnail',
+                content: _t("<b>Drag and Drop</b> this snippet below the event title."),
+                position: 'bottom',
+                run: 'drag_and_drop #o_wevent_event_main_col',
+            }, {
+                trigger: '#web_editor-top-edit button[data-action="save"]',
+                content: _t("Don't forget to click <b>save</b> when you're done."),
+                position: 'bottom',
+            }, {
+                trigger: 'label.js_publish_btn',
+                content: _t("Looking great! Let's now <b>publish</b> this page so that it becomes <b>visible</b> on your website!"),
+                position: 'bottom',
+            }, {
+                trigger: 'a.css_edit_dynamic',
+                extra_trigger: 'div.o_notification_manager:empty',
+                content: _t("Want to change your event configuration? Let's go back to the event form."),
+                position: 'bottom',
+                run: function (actions) {
+                    actions.click('div.dropdown-menu a#edit-in-backend');
+                },
+            }, {
+                trigger: 'a#edit-in-backend',
+                content: _t("This shortcut will bring you right back to the event form."),
+                position: 'bottom'
+            }];
+    }
+});
+
+return EventAdditionalTourSteps;
+
+});
