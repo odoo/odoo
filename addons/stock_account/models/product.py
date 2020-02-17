99252f3d93ca68077e0169c580eaecb6fc8ce8fd
Tiffany Chang (tic) <tic@odoo.com>
2020-02-17 15:03:54 +0000

    [IMP] stock/mrp: Update UX
    This commit updates product, stock and manufacturing views for improved
    usability. Handles view only changes of overall usability improvement
    task. Specifically spec numbers:
    
    1. Add "Run Scheduler" (from stock) to the manufacturing app under operations
    5. Make "detailed operations" wizard modal have more intitutive footer
       buttons (i.e. don't say "Confirm"/"Cancel" when changes aren't
       possible.
    8. Rearranging of vendor pricelist view
    9. Updating of tooltip content for "property_stock_account_input_categ_id"
    
    Task: 2041853

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -627,9 +627,8 @@ class ProductCategory(models.Model):
     property_stock_account_input_categ_id = fields.Many2one(
         'account.account', 'Stock Input Account', company_dependent=True,
         domain="[('company_id', '=', allowed_company_ids[0]), ('deprecated', '=', False)]", check_company=True,
-        help="""When doing automated inventory valuation, counterpart journal items for all incoming stock moves will be posted in this account,
-                unless there is a specific valuation account set on the source location. This is the default value for all products in this category.
-                It can also directly be set on each product.""")
+        help="""Counterpart journal items for all incoming stock moves will be posted in this account, unless there is a specific valuation account
+                set on the source location. This is the default value for all products in this category. It can also directly be set on each product.""")
     property_stock_account_output_categ_id = fields.Many2one(
         'account.account', 'Stock Output Account', company_dependent=True,
         domain="[('company_id', '=', allowed_company_ids[0]), ('deprecated', '=', False)]", check_company=True,
