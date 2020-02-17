b10c760259c4282ee7189d960544889dd740560d
Nicolas Martinelli <nim@odoo.com>
2020-02-17 13:02:57 +0000

    [FIX] mrp: MTSO as default
    Set the MTSO as default for Manufacturing, similarly to `mto_pull_id`.
    
    X-original-commit: 473e3ee607d318d624bb0117d14603af3ce4ecea

--- addons/mrp/models/stock_warehouse.py
+++ addons/mrp/models/stock_warehouse.py
@@ -126,7 +126,7 @@ class StockWarehouse(models.Model):
             'manufacture_mto_pull_id': {
                 'depends': ['manufacture_steps', 'manufacture_to_resupply'],
                 'create_values': {
-                    'procure_method': 'make_to_order',
+                    'procure_method': 'mts_else_mto',
                     'company_id': self.company_id.id,
                     'action': 'pull',
                     'auto': 'manual',
