99e8d320ceba05a5f95d217f78231a68340add41
aab-odoo <aab@odoo.com>
2020-02-18 10:04:18 +0100

    [FIX] do not deploy services in tests

--- addons/web/static/tests/main_tests.js
+++ addons/web/static/tests/main_tests.js
@@ -3,6 +3,20 @@ odoo.define('web.web_client', async function (require) {
     "use strict";
 
     const session = require("web.session");
+    const { bus } = require('web.core');
+
+    // listen to unhandled rejected promises, and when the rejection is not due
+    // to a crash, prevent the browser from displaying an 'unhandledrejection'
+    // error in the console, which would make tests crash on each Promise.reject()
+    // something similar is done by the CrashManagerService, but by default, it
+    // isn't deployed by default in tests
+    bus.on('crash_manager_unhandledrejection', this, function (ev) {
+        if (!ev.reason || !(ev.reason instanceof Error)) {
+            ev.stopPropagation();
+            ev.stopImmediatePropagation();
+            ev.preventDefault();
+        }
+    });
 
     owl.config.mode = "dev";
 
