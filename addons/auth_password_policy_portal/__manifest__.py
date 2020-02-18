b6116af9d03c5640d49adfea868d72940487e21a
Xavier Morel <xmo@odoo.com>
2020-02-18 09:29:28 +0000

    [IMP] portal: /my/security page
    * allows portal users to update their password
    * and manage their API keys
    * any anything technical / security related we may need to add in the
      future
    
    Also bridge module for the password meter (auth password policy).

---
+++ addons/auth_password_policy_portal/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': "Password Policy support for Signup",
+    'depends': ['auth_password_policy', 'portal'],
+    'category': 'Tools',
+    'auto_install': True,
+    'data': ['views/templates.xml']
+}
