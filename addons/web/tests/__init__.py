8cdae519fa6dbc138e1c4acd4c6383dc98e44e14
Michell Stuttgart <michellstut@gmail.com>
2020-02-18 16:00:26 -0300

    [FIX] Comenta codigos de ui incompativeis com o novo tema

--- addons/web/tests/__init__.py
+++ addons/web/tests/__init__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_image
-from . import test_js
+# from . import test_js
 from . import test_menu
 from . import test_serving_base
 from . import test_click_everywhere
