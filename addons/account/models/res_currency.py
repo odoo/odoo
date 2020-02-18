51fd4a178e8c14e6addefef16ba582eeee9e853a
Laurent Smet <las@odoo.com>
2020-02-18 13:51:57 +0000

    [FIX] account: Fix traceback when creating a new currency.
    Introduced by:
    https://github.com/odoo/odoo/commit/8d0499cc59971acbc1de74af2ce4d25f0a15019d
    
    closes odoo/odoo#45688
    
    X-original-commit: 12b17a20586c6b3c1069879557f5a169d5d85667
    Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

--- addons/account/models/res_currency.py
+++ addons/account/models/res_currency.py
@@ -15,7 +15,8 @@ class ResCurrency(models.Model):
     @api.depends('rounding')
     def _compute_display_rounding_warning(self):
         for record in self:
-            record.display_rounding_warning = record._origin.rounding != record.rounding \
+            record.display_rounding_warning = record.id \
+                                              and record._origin.rounding != record.rounding \
                                               and record._origin._has_accounting_entries()
 
     def write(self, vals):
