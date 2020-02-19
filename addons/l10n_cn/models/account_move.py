106fdd0426a36ae8317288cc68139a34e665898f
yhu-odoo <yhu@odoo.com>
2020-02-19 14:54:39 +0100

    [IMP] l10n_cn: add print option "Voucher"
    Add an additional report called "Voucher/记帐凭证" to print option in
    account.move form view.
    
    A library "cn2an" are required to conver a number to "amount in
    words/大写金额" for Chinese financial usage.
    
    Task 2179744
    
    X-original-commit: 0f6fdce76809af7e965f672970d74594a337f6fb

--- addons/l10n_cn/models/account_move.py
+++ addons/l10n_cn/models/account_move.py
@@ -3,7 +3,12 @@
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+from odoo.osv import expression
 
+try:
+    from cn2an import an2cn
+except ImportError:
+    an2cn = None
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -15,3 +20,24 @@ class AccountMove(models.Model):
         for record in self:
             if record.fapiao and (len(record.fapiao) != 8 or not record.fapiao.isdecimal()):
                 raise ValidationError(_("Fapiao number is an 8-digit number. Please enter a correct one."))
+
+    @api.model
+    def check_cn2an(self):
+        return an2cn
+
+    @api.model
+    def _convert_to_amount_in_word(self, number):
+        """Convert number to ``amount in words`` for Chinese financial usage."""
+        if not self.check_cn2an():
+            return None
+        return an2cn(number, 'rmb')
+
+    def _count_attachments(self):
+        domains = [[('res_model', '=', 'account.move'), ('res_id', '=', self.id)]]
+        statement_ids = self.line_ids.mapped('statement_id')
+        payment_ids = self.line_ids.mapped('payment_id')
+        if statement_ids:
+            domains.append([('res_model', '=', 'account.bank.statement'), ('res_id', 'in', statement_ids)])
+        if payment_ids:
+            domains.append([('res_model', '=', 'account.payment'), ('res_id', 'in', payment_ids)])
+        return self.env['ir.attachment'].search_count(expression.OR(domains))
