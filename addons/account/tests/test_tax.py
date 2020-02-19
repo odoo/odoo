b658040171a8625d442ee336056520da57826317
Nicolas Martinelli <nim@odoo.com>
2020-02-19 12:08:17 +0100

    [FIX] account: error on tax rounding
    - Active 'Round Globally'
    - Create the tax:
      Amount: 21 %
      Tax Included
    - Create the following invoice:
      Line 1: qty 1.0, price 11.90
      Line 2: qty 1.0, price 2.80
    
    The Taxes amount is 2.56 while it should be 2.55. Indeed, it should be:
    
    2.0652893 + 0.4859504 = 2.5512397 => 2.55
    
    This is because the `base` amount used in the tax computation is rounded
    to the currency decimal.
    
    opw-2181486
    
    X-original-commit: febb2355b6961a25cf059e2da6a3fd16c48d5bff

--- addons/account/tests/test_tax.py
+++ addons/account/tests/test_tax.py
@@ -106,9 +106,20 @@ class TestTax(AccountTestUsers):
                 }),
             ],
         })
+
+        self.tax_21_percent = self.tax_model.create({
+            'name': "test_rounding_methods_1",
+            'amount_type': 'percent',
+            'amount': 21,
+        })
+
         self.bank_journal = self.env['account.journal'].search([('type', '=', 'bank'), ('company_id', '=', self.account_manager.company_id.id)])[0]
         self.bank_account = self.bank_journal.default_debit_account_id
         self.expense_account = self.env['account.account'].search([('user_type_id.type', '=', 'payable')], limit=1) #Should be done by onchange later
+
+        # Ensure the rounding method is 'round_per_line' to avoid issue when launching the tests on
+        # an existing DB.
+        self.env.user.company_id.tax_calculation_rounding_method = 'round_per_line'
 
     def _check_compute_all_results(self, total_included, total_excluded, taxes, res):
         self.assertAlmostEqual(res['total_included'], total_included)
@@ -639,4 +650,38 @@ class TestTax(AccountTestUsers):
                 # ---------------
             ],
             tax.compute_all(-1.0)
+        )
+
+    def test_rounding_tax_included_round_globally_01(self):
+        ''' Test the rounding of a 21% price included tax in an invoice having 11.90 and 2.80 as lines.
+        The decimal precision is set to 2.
+        '''
+        self.tax_21_percent.price_include = True
+        self.tax_21_percent.company_id.currency_id.rounding = 0.01
+        self.tax_21_percent.company_id.tax_calculation_rounding_method = 'round_globally'
+
+        res1 = self.tax_21_percent.compute_all(11.90)
+        self._check_compute_all_results(
+            11.90,      # 'total_included'
+            9.83,       # 'total_excluded'
+            [
+                # base , amount
+                # ---------------
+                (9.83471067, 2.0652893),
+                # ---------------
+            ],
+            res1
+        )
+
+        res2 = self.tax_21_percent.compute_all(2.80)
+        self._check_compute_all_results(
+            2.80,      # 'total_included'
+            2.31,      # 'total_excluded'
+            [
+                # base , amount
+                # ---------------
+                (2.3140496,  0.4859504),
+                # ---------------
+            ],
+            res2
         )
