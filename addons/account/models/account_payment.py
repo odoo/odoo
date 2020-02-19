dacbd93b3aaba61d2128e07d9d75bf1cb37a2504
oco-odoo <oco@odoo.com>
2020-02-19 08:55:55 +0000

    [FIX] account, hr_expense: incorrect forward-port
    Incorrect forward-port of e09798098679f made in 51d49517a8b5af2d1c85d9c6.
    
    closes odoo/odoo#45775
    
    X-original-commit: 099d0d6785b85dd67ef091fdaef084713fafb3c0
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -186,7 +186,7 @@ class account_payment(models.Model):
         won't be displayed but some modules might change that, depending on the payment type."""
         for payment in self:
             payment.show_partner_bank_account = payment.payment_method_code in self._get_method_codes_using_bank_account()
-            payment.require_partner_bank_account = payment.payment_method_code in self._get_method_codes_needing_bank_account()
+            payment.require_partner_bank_account = payment.state == 'draft' and payment.payment_method_code in self._get_method_codes_needing_bank_account()
 
     @api.depends('payment_type', 'journal_id')
     def _compute_hide_payment_method(self):
