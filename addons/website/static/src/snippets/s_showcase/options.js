4dbd6b8d11fced0bf9672d9e997eec0b463591a0
qsm-odoo <qsm@odoo.com>
2020-02-20 10:01:13 +0000

    [IMP] website: improve edition of Showcase snippet
    Also no more scss required for the snippet.
    
    Part of https://github.com/odoo/odoo/pull/46773
    task-2191381
    
    Co-authored-by: Cocographique <nmi@odoo.com>

---
+++ addons/website/static/src/snippets/s_showcase/options.js
@@ -0,0 +1,18 @@
+odoo.define('website.s_showcase_options', function (require) {
+'use strict';
+
+const options = require('web_editor.snippets.options');
+
+options.registry.Showcase = options.Class.extend({
+    /**
+     * @override
+     */
+    onMove: function () {
+        const $showcaseCol = this.$target.parent().closest('.row > div');
+        const isLeftCol = $showcaseCol.index() <= 0;
+        const $title = this.$target.children('.s_showcase_title');
+        $title.toggleClass('flex-lg-row-reverse', isLeftCol);
+        $title.find('.s_showcase_icon').toggleClass('mr-lg-0 ml-3', isLeftCol);
+    },
+});
+});
