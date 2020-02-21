4bca34eb6617be858fe0209a0f50f92cd13338a4
jvm-odoo <jvm@odoo.com>
2020-02-21 13:04:55 +0000

    [FIX] mail: attachments in the message when marking activity as done
    Fine tuning of 1e411e3a6d
    - It moves the attachment in the message when we mark an activity as
      done.
    - It adds a test
    
    OPW-2196668
    
    closes odoo/odoo#45938
    
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/mail/models/mail_activity.py
+++ addons/mail/models/mail_activity.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from collections import defaultdict
 from datetime import date, datetime, timedelta
 import pytz
 
@@ -241,6 +242,19 @@ class MailActivity(models.Model):
         message = self.env['mail.message']
         if feedback:
             self.write(dict(feedback=feedback))
+
+        # Search for all attachments linked to the activities we are about to unlink. This way, we
+        # can link them to the message posted and prevent their deletion.
+        attachments = self.env['ir.attachment'].search_read([
+            ('res_model', '=', self._name),
+            ('res_id', 'in', self.ids),
+        ], ['id', 'res_id'])
+
+        activity_attachments = defaultdict(list)
+        for attachment in attachments:
+            activity_id = attachment['res_id']
+            activity_attachments[activity_id].append(attachment['id'])
+
         for activity in self:
             record = self.env[activity.res_model].browse(activity.res_id)
             record.message_post_with_view(
@@ -249,11 +263,19 @@ class MailActivity(models.Model):
                 subtype_id=self.env.ref('mail.mt_activities').id,
                 mail_activity_type_id=activity.activity_type_id.id,
             )
-            message |= record.message_ids[0]
 
-            # Removing attachments res_id to prevent unlinking it
-            # We still want them in the message
-            self.env['ir.attachment'].search([('res_model', '=', self._name), ('res_id', 'in', self.ids)]).write({'res_id': False})
+            # Moving the attachments in the message
+            # TODO: Fix void res_id on attachment when you create an activity with an image
+            # directly, see route /web_editor/attachment/add
+            activity_message = record.message_ids[0]
+            message_attachments = self.env['ir.attachment'].browse(activity_attachments[activity.id])
+            message_attachments.write({
+                'res_id': activity_message.id,
+                'res_model': activity_message._name,
+            })
+            activity_message.attachment_ids = message_attachments
+            message |= activity_message
+
         self.unlink()
         return message.ids and message.ids[0] or False
 
