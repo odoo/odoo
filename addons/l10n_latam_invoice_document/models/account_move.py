ad50e8614b6e6afe5adc7bfef77884995135fc21
Christophe Monniez <moc@odoo.com>
2020-02-21 13:11:16 +0100

    [FIX] account, l10n_latam_invoice_document: fix move_type typo
    Two typos were introduced with d675dbaa4c71.

--- addons/l10n_latam_invoice_document/models/account_move.py
+++ addons/l10n_latam_invoice_document/models/account_move.py
@@ -98,7 +98,7 @@ class AccountMove(models.Model):
     def _check_invoice_type_document_type(self):
         for rec in self.filtered('l10n_latam_document_type_id.internal_type'):
             internal_type = rec.l10n_latam_document_type_id.internal_type
-            invoice_type = rec.tmove_ype
+            invoice_type = rec.move_type
             if internal_type in ['debit_note', 'invoice'] and invoice_type in ['out_refund', 'in_refund']:
                 raise ValidationError(_('You can not use a %s document type with a refund invoice') % internal_type)
             elif internal_type == 'credit_note' and invoice_type in ['out_invoice', 'in_invoice']:
