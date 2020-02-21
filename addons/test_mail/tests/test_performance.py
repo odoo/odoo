c6b7feb9712f4e7bd9e5517fe2a16b319ec63a15
jvm-odoo <jvm@odoo.com>
2020-02-21 13:04:55 +0000

    [FIX] mail: attachments in the message when marking activity as done
    Fine tuning of 1e411e3a6d
    - It moves the attachment in the message when we mark an activity as
      done.
    - It adds a test
    
    OPW-2196668
    
    closes odoo/odoo#46820
    
    X-original-commit: 2ad6cb4feb01dbb394ab50ce9f8c1fd6da30bf8c
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/test_mail/tests/test_performance.py
+++ addons/test_mail/tests/test_performance.py
@@ -182,7 +182,7 @@ class TestAdvMailPerformance(TransactionCase):
             #voip module read activity_type during create leading to one less query in enterprise on action_feedback
             category = activity.activity_type_id.category
 
-        with self.assertQueryCount(__system__=24, emp=47):  # com runbot: 25 - 46 // test_mail only: 24 - 45
+        with self.assertQueryCount(__system__=25, emp=47):  # com runbot: 26 - 46 // test_mail only: 25 - 45
             activity.action_feedback(feedback='Zizisse Done !')
 
     @users('__system__', 'emp')
@@ -199,7 +199,7 @@ class TestAdvMailPerformance(TransactionCase):
 
         record.write({'name': 'Dupe write'})
 
-        with self.assertQueryCount(__system__=26, emp=49):  # com runbot: 27 - 86 // test_mail only: 26 - 47
+        with self.assertQueryCount(__system__=27, emp=49):  # com runbot: 28 - 86 // test_mail only: 27 - 47
             record.action_close('Dupe feedback')
 
         self.assertEqual(record.activity_ids, self.env['mail.activity'])
