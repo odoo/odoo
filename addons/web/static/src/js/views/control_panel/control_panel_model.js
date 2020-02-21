e516c239d12d9836b864731ecf800a11e136138a
Mohammed Shekha <msh@odoo.com>
2020-02-21 10:32:14 +0000

    [FIX] web: do not use groupbys in views not groupable
    Before this commit: Some problems occur with not groupable views (views for
    which searchMenuTypes does not contain 'groupBy'):
    
        - The searchbar still displays autocomplete suggestions for groupby filters
        defined in the search arch while selecting one of them would cause a crash;
        - If the action has a default groupby (set in Studio for instance) then it
        is still applied and causes crashes.
        - Those problems happen in the documents kanban view for instance.
    
    After this commit: If a view is not groupable,
    
        - The groupby filters coming from the search arch are not displayed at all
        in the interface;
        - A default groupby is never applied;
        - The search query generated in the control panel has groupBy = [].
    
    X-original-commit: 254cb801af893615ecbef51f05898e4190c32d29

--- addons/web/static/src/js/views/control_panel/control_panel_model.js
+++ addons/web/static/src/js/views/control_panel/control_panel_model.js
@@ -257,7 +257,7 @@ var ControlPanelModel = mvc.Model.extend({
             facets: facets,
             filterFields: filterFields,
             filters: filters,
-            groupBys: groupBys,
+            groupBys: this.searchMenuTypes.includes('groupBy') ? groupBys : [],
             timeRanges: timeRanges,
             favorites: favorites,
             groups: this.groups,
@@ -307,7 +307,7 @@ var ControlPanelModel = mvc.Model.extend({
         return {
             context: context,
             domain: results.domain,
-            groupBy: groupBy,
+            groupBy: this.searchMenuTypes.includes('groupBy') ? groupBy : [],
             orderedBy: this._getOrderedBy(),
         };
     },
