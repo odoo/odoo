8f925001b122d13b797fc0f99858038dead317d2
Florent de Labarre <florent.mirieu@gmail.com>
2020-02-23 11:31:27 +0100

    [IMP] stock_dropshipping: direct access to purchase
    When you create a sale order with some line with dropshipping, you not a direct access to the purchase generared.

--- addons/stock_dropshipping/models/sale.py
+++ addons/stock_dropshipping/models/sale.py
@@ -4,6 +4,33 @@
 from odoo import models
 
 
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    purchase_dropshipping_count = fields.Integer(compute='_get_purchase_dropshipping_count',
+                                                 groups='purchase.group_purchase_user')
+
+    def _get_purchase_dropshipping_count(self):
+        for sale in self:
+            sale.purchase_dropshipping_count = self.env['purchase.order'].search_count(
+                [('order_line', 'in', sale.order_line.mapped('purchase_line_ids').ids)])
+
+    def action_view_purchase_dropshipping(self):
+        self.ensure_one()
+        purchases = self.env['purchase.order'].search(
+            [('order_line', 'in', self.order_line.mapped('purchase_line_ids').ids)])
+        return {
+            'name': _('Dropshipping Purchase'),
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_model': 'purchase.order',
+            'type': 'ir.actions.act_window',
+            'domain': [('id', 'in', purchases.ids)],
+            'target': 'current',
+            'context': self.env.context,
+        }
+
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
@@ -25,7 +52,8 @@ class SaleOrderLine(models.Model):
         if purchase_lines_sudo.filtered(lambda r: r.state != 'cancel'):
             qty = 0.0
             for po_line in purchase_lines_sudo.filtered(lambda r: r.state != 'cancel'):
-                qty += po_line.product_uom._compute_quantity(po_line.product_qty, self.product_uom, rounding_method='HALF-UP')
+                qty += po_line.product_uom._compute_quantity(po_line.product_qty,
+                                                             self.product_uom, rounding_method='HALF-UP')
             return qty
         else:
             return super(SaleOrderLine, self)._get_qty_procurement(previous_product_uom_qty=previous_product_uom_qty)
