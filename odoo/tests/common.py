77ee56a107f31d2d592598460df16998001d2868
Xavier Morel <xmo@odoo.com>
2020-02-24 08:26:00 +0000

    [IMP] core: logging of array remoteobjects
    The old path would just return e.g. `Array(2)` for an array of size 2,
    which is not very useful when trying to see if an array's contents are
    relevant to an issue.
    
    Turns out arrays are serialized pretty much exactly like objects
    without a subtype, just with property names being indices instead of
    keys. So add a case which formats such remoteobjects as
    array-literal-ish.
    
    closes odoo/odoo#46056
    
    X-original-commit: 6d5639024fa0f7501efe836e5571cd45bb46b123
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -1107,23 +1107,30 @@ class ChromeBrowser():
         """ attempts to make a CDT RemoteObject comprehensible
         """
         objtype = arg['type']
-        klass = arg.get('className', '')
         subtype = arg.get('subtype')
         if objtype == 'undefined':
             # the undefined remoteobject is literally just {type: undefined}...
             return 'undefined'
-        elif objtype != 'object' or subtype:
+        elif objtype != 'object' or subtype not in (None, 'array'):
             # value is the json representation for json object
             # otherwise fallback on the description which is "a string
             # representation of the object" e.g. the traceback for errors, the
             # source for functions, ... finally fallback on the entire arg mess
             return arg.get('value', arg.get('description', arg))
-
+        elif subtype == 'array':
+            # apparently value is *not* the JSON representation for arrays
+            # instead it's just Array(3) which is useless, however the preview
+            # properties are the same as object which is useful (just ignore the
+            # name which is the index)
+            return '[%s]' % ', '.join(
+                repr(p['value']) if p['type'] == 'string' else str(p['value'])
+                for p in arg.get('preview', {}).get('properties', [])
+            )
         # all that's left is type=object, subtype=None aka custom or
         # non-standard objects, print as TypeName(param=val, ...), sadly because
         # of the way Odoo widgets are created they all appear as Class(...)
         return '%s(%s)' % (
-            klass or objtype,
+            arg.get('className') or 'object',
             ', '.join(
                 '%s=%s' % (p['name'], repr(p['value']) if p['type'] == 'string' else p['value'])
                 for p in arg.get('preview', {}).get('properties', [])
