66c409917a6d837c87f33a9686b69fffde841466
Sébastien Theys <seb@odoo.com>
2020-02-25 13:35:29 +0100

    [IMP] test_mail, test_mass_mailing: adapt query count
    To see impact of following commit.

--- addons/test_mass_mailing/tests/test_performance.py
+++ addons/test_mass_mailing/tests/test_performance.py
@@ -50,7 +50,7 @@ class TestMassMailPerformance(TestMassMailPerformanceBase):
             'mailing_domain': [('id', 'in', self.mm_recs.ids)],
         })
 
-        with self.assertQueryCount(__system__=2261, marketing=2261):
+        with self.assertQueryCount(__system__=2161, marketing=2161):
             mailing.action_send_mail()
 
         self.assertEqual(mailing.sent, 50)
@@ -89,7 +89,7 @@ class TestMassMailBlPerformance(TestMassMailPerformanceBase):
             'mailing_domain': [('id', 'in', self.mm_recs.ids)],
         })
 
-        with self.assertQueryCount(__system__=2636, marketing=2636):
+        with self.assertQueryCount(__system__=2512, marketing=2512):
             mailing.action_send_mail()
 
         self.assertEqual(mailing.sent, 50)
