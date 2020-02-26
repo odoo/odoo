fc9dc53d09437ef6b6ba3fd6854ec21ce0a14373
jbm-odoo <jbm@odoo.com>
2020-02-26 17:03:28 +0100

    [IMP] tools: rotate PDF
    Task ID 1251

--- odoo/tools/pdf.py
+++ odoo/tools/pdf.py
@@ -19,3 +19,21 @@ def merge_pdf(pdf_data):
     merged_pdf = _buffer.getvalue()
     _buffer.close()
     return merged_pdf
+
+
+def rotate_pdf(pdf):
+    ''' Rotate clockwise PDF (90°)
+    :param pdf: a PDF to rotate
+    :return: a PDF rotated
+    '''
+    writer = PdfFileWriter()
+    reader = PdfFileReader(io.BytesIO(pdf), strict=False)
+    for page in range(0, reader.getNumPages()):
+        page = reader.getPage(page)
+        page.rotateClockwise(90)
+        writer.addPage(page)
+    _buffer = io.BytesIO()
+    writer.write(_buffer)
+    out_pdf = _buffer.getvalue()
+    _buffer.close()
+    return out_pdf
