025f6758f57ec6b7d3135b80a90f27707c5146b8
Jigar Vaghela <jva@odoo.com>
2020-02-26 08:50:51 +0000

    [FIX] l10n_vn: remove _preserve_tag_on_taxes because of no more use in the current version
    closes odoo/odoo#43096
    
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- addons/l10n_vn/__init__.py
+++ addons/l10n_vn/__init__.py
@@ -7,10 +7,5 @@ from odoo import api, SUPERUSER_ID
 
 
 def _post_init_hook(cr, registry):
-    _preserve_tag_on_taxes(cr, registry)
     env = api.Environment(cr, SUPERUSER_ID, {})
     env.ref('l10n_vn.vn_template').process_coa_translations()
-
-def _preserve_tag_on_taxes(cr, registry):
-    from odoo.addons.account.models.chart_template import preserve_existing_tags_on_taxes
-    preserve_existing_tags_on_taxes(cr, registry, 'l10n_vn')
