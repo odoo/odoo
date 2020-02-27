86d5d60ad8f11c04f7cd5f91ebb2298669557f2c
Humberto Arocha <hbto@vauxoo.com>
2020-02-27 12:29:25 +0000

    [FIX] account: extract code into method to allow changing the date of the tax
    Before this commit, when two lines belonging to a period that is already
    closed a cash basis entry is created at the present date. There are
    localizations that could not allow creating the cash basis entry in a
    date other than the maximum between the Journal Items being reconciled.
    
    Now, the localizations are able to override and chose the date without
    having to override the whole 'create_tax_cash_basis_entry' method.
    
    opw-2195016
    
    closes odoo/odoo#46434
    
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -1782,6 +1782,14 @@ class AccountPartialReconcile(models.Model):
     def _get_amount_tax_cash_basis(self, amount, line):
         return line.company_id.currency_id.round(amount)
 
+    def _set_tax_cash_basis_entry_date(self, move_date, newly_created_move):
+        if move_date > (self.company_id.period_lock_date or '0000-00-00') and newly_created_move.date != move_date:
+            # The move date should be the maximum date between payment and invoice (in case
+            # of payment in advance). However, we should make sure the move date is not
+            # recorded before the period lock date as the tax statement for this period is
+            # probably already sent to the estate.
+            newly_created_move.write({'date': move_date})
+
     def create_tax_cash_basis_entry(self, percentage_before_rec):
         self.ensure_one()
         move_date = self.debit_move_id.date
@@ -1873,12 +1881,7 @@ class AccountPartialReconcile(models.Model):
                                 })
         self.recompute()
         if newly_created_move:
-            if move_date > (self.company_id.period_lock_date or '0000-00-00') and newly_created_move.date != move_date:
-                # The move date should be the maximum date between payment and invoice (in case
-                # of payment in advance). However, we should make sure the move date is not
-                # recorded before the period lock date as the tax statement for this period is
-                # probably already sent to the estate.
-                newly_created_move.write({'date': move_date})
+            self._set_tax_cash_basis_entry_date(move_date, newly_created_move)
             # post move
             newly_created_move.post()
 
