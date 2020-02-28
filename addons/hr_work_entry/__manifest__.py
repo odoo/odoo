1a0aea6b19b1faf905a5afdedba5bbd951d7cd11
qho <qho@odoo.com>
2020-02-28 14:10:39 +0100

    [IMP]Resource: resource_calendar default_get, and global time off
    Before this commit, all new created calendar attandances used "40 hours" as default template.
    
    With this commit, new generated calendar attandance will use the company's default calendar as the template
    
    This commit also link specific personal calendar with the company global time off.
    
    TaskId: 2181651

--- addons/hr_work_entry/__manifest__.py
+++ addons/hr_work_entry/__manifest__.py
@@ -16,6 +16,7 @@
         'security/ir.model.access.csv',
         'data/hr_work_entry_data.xml',
         'views/hr_work_entry_views.xml',
+        'views/resource_views.xml'
     ],
     'qweb': [
         "static/src/xml/work_entry_templates.xml",
