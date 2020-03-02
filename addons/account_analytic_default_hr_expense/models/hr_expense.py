e3d4c5e1a392af7383137e6ed74b9b8f117bf067
Adrian Torres <adt@odoo.com>
2020-03-02 13:10:05 +0000

    [FIX] base: handle custom non-stored m2m
    Before this commit, one could create a custom, non-stored m2m field
    through the web interface BUT the deletion of the custom field would be
    impossible after saving the form.
    
    This is due to the fact that the relation would not be stored in DB
    since the field is store=False, therefore the value of the relation
    would equal to None, and since the unlinking mechanism assumes that
    the value is never None, it tries to drop a table of name None, which
    does not exist.
    
    closes odoo/odoo#46890
    
    X-original-commit: cdd1439a380810569db80d984c8cb80d213b1bf3
    Signed-off-by: Adrian Torres (adt) <adt@odoo.com>

--- addons/account_analytic_default_hr_expense/models/hr_expense.py
+++ addons/account_analytic_default_hr_expense/models/hr_expense.py
@@ -11,6 +11,6 @@ class HrExpense(models.Model):
     def _onchange_product_id(self):
         res = super(HrExpense, self)._onchange_product_id()
         rec = self.env['account.analytic.default'].account_get(self.product_id.id, company_id=self.company_id.id, date=self.date)
-        self.analytic_account_id = rec.analytic_id.id
-        self.analytic_tag_ids = rec.analytic_tag_ids.ids
+        self.analytic_account_id = self.analytic_account_id or rec.analytic_id.id
+        self.analytic_tag_ids = self.analytic_tag_ids or rec.analytic_tag_ids.ids
         return res
