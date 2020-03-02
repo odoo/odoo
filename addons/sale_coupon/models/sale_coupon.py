be6acad988fb0b3b6270bfb0d71952b5feb6ad1a
Swapnesh Shah <swapneshshah111@gmail.com>
2020-03-02 23:14:14 +0530

    [FIX] sale_coupon: Delete Coupons on deleting Related Program
    There is no Point to Keep Coupons which is not linked to any Coupon Program.
    
    X-original-commit: 870eb3fd36fe74eb10b9e8bac157fac3f5582d0d

--- addons/sale_coupon/models/sale_coupon.py
+++ addons/sale_coupon/models/sale_coupon.py
@@ -35,7 +35,7 @@ class SaleCoupon(models.Model):
         ('cancel', 'Cancelled')
     ], required=True, default='new')
     partner_id = fields.Many2one('res.partner', "For Customer")
-    program_id = fields.Many2one('sale.coupon.program', "Program")
+    program_id = fields.Many2one('sale.coupon.program', "Program", required=True, ondelete="cascade")
     order_id = fields.Many2one('sale.order', 'Order Reference', readonly=True,
         help="The sales order from which coupon is generated")
     sales_order_id = fields.Many2one('sale.order', 'Used in', readonly=True,
