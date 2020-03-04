f6b019fcfdd35a693e8af43ae38d10e31e96284a
Rémy Voet (ryv) <ryv@odoo.com>
2020-03-04 10:25:57 +0000

    [FIX] stock_account: fix valuate empty stock.
    To reproduce, product A in AVCO:
    - Buy 2 product A at 1.00 $
    - Buy 1 product A at 1.01 $
    - Sell 3 product A.
    - The stock valuation won't be correct: still remain 0.01 in
    stock without quantity and the svl related to the sell
    have a value of 3.00 (instead of 3.01)
    
    To fix:
    In case of AVCO, add the rounding error value to the out
    stock move layer.
    
    closes odoo/odoo#46850
    
    Signed-off-by: William Henrotin <Whenrow@users.noreply.github.com>

--- addons/stock_account/tests/test_stockvaluation.py
+++ addons/stock_account/tests/test_stockvaluation.py
@@ -2000,7 +2000,7 @@ class TestStockValuation(SavepointCase):
         move5.move_line_ids.qty_done = 30.0
         move5._action_done()
 
-        self.assertEqual(move5.stock_valuation_layer_ids.value, -477.6)
+        self.assertEqual(move5.stock_valuation_layer_ids.value, -477.5)
 
         # Receives 10 units but assign them to an owner, the valuation should not be impacted.
         move6 = self.env['stock.move'].create({
@@ -2019,6 +2019,24 @@ class TestStockValuation(SavepointCase):
         move6._action_done()
 
         self.assertEqual(move6.stock_valuation_layer_ids.value, 0)
+
+        # Sale 50 units @ $19.50 per unit (no stock anymore)
+        move7 = self.env['stock.move'].create({
+            'name': '50 units @ $19.50 per unit',
+            'location_id': self.stock_location.id,
+            'location_dest_id': self.customer_location.id,
+            'product_id': self.product1.id,
+            'product_uom': self.uom_unit.id,
+            'product_uom_qty': 50.0,
+        })
+        move7._action_confirm()
+        move7._action_assign()
+        move7.move_line_ids.qty_done = 50.0
+        move7._action_done()
+
+        self.assertEqual(move7.stock_valuation_layer_ids.value, -796.0)
+        self.assertAlmostEqual(self.product1.quantity_svl, 0.0)
+        self.assertAlmostEqual(self.product1.value_svl, 0.0)
 
     def test_average_perpetual_2(self):
         self.product1.categ_id.property_cost_method = 'average'
