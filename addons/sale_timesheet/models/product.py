85e2cf23b9b557c9355d7e51067b35bacbbb3747
qho <qho@odoo.com>
2020-03-04 09:21:16 +0100

    [FIX]sale_timesheet: Avoid an empty service policy
    Before this commit, when a product switched to type `service`, the service invoicing policy was left as empty value
    
    This commit fix this problem. The default value will be "Ordered quantities" (ordered_timesheet)
    
    TaskId: 2205594

--- addons/sale_timesheet/models/product.py
+++ addons/sale_timesheet/models/product.py
@@ -36,7 +36,7 @@ class ProductTemplate(models.Model):
             policy = None
             if product.invoice_policy == 'delivery':
                 policy = 'delivered_manual' if product.service_type == 'manual' else 'delivered_timesheet'
-            elif product.invoice_policy == 'order' and product.service_type == 'timesheet':
+            elif product.invoice_policy == 'order' and (product.service_type == 'timesheet' or product.type == 'service'):
                 policy = 'ordered_timesheet'
             product.service_policy = policy
 
@@ -83,6 +83,8 @@ class ProductTemplate(models.Model):
         if self.type == 'service' and not self.invoice_policy:
             self.invoice_policy = 'order'
             self.service_type = 'timesheet'
+        elif self.type == 'service' and self.invoice_policy == 'order':
+            self.service_policy = 'ordered_timesheet'
         elif self.type == 'consu' and not self.invoice_policy and self.service_policy == 'ordered_timesheet':
             self.invoice_policy = 'order'
 
