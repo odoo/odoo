0f33670461dd16b53701b2cad11d24b2a9820cb2
qmo-odoo <qmo@odoo.com>
2020-03-05 08:27:21 +0100

    [IMP] mass_mailing: Add an hour to schedule time if user picks today
    Purpose of this commit is to avoid having a user error when he sets
    the schedule time as today.
    
    To do this, we decided to add an extra hour in case the schedule time is
    lower or equal to today's date.
    
    If adding an extra hour causes the schedule_date to be pushed to the next day,
    we set the time as 23:59:59 instead. That way, we make sure that
    the user won't think that a bug happened.
    
    LINKS:
    
    TaskID: 2206153
    PR: #47054
    
    wip

--- addons/mass_mailing/wizard/mailing_mailing_schedule_date.py
+++ addons/mass_mailing/wizard/mailing_mailing_schedule_date.py
@@ -3,6 +3,8 @@
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+from datetime import datetime, timedelta
+from pytz import timezone, UTC
 
 
 class MailingMailingScheduleDate(models.TransientModel):
@@ -11,6 +13,20 @@ class MailingMailingScheduleDate(models.TransientModel):
 
     schedule_date = fields.Datetime(string='Scheduled for')
     mass_mailing_id = fields.Many2one('mailing.mailing', required=True, ondelete='cascade')
+
+    @api.onchange('schedule_date')
+    def _onchange_schedule_date(self):
+        user_tz = timezone(self.env.user.tz)
+        today = datetime.now().astimezone(user_tz)
+        for mailing_schedule_date in self:
+            if mailing_schedule_date.schedule_date:
+                schedule_date_user_tz = mailing_schedule_date.schedule_date.astimezone(user_tz)
+                if schedule_date_user_tz <= today:
+                    temp_schedule_date = schedule_date_user_tz + timedelta(hours=1)
+                    if temp_schedule_date.day > mailing_schedule_date.schedule_date.day or temp_schedule_date.day == 1:
+                        mailing_schedule_date.schedule_date = schedule_date_user_tz.replace(minute=59, second=59).astimezone(UTC).replace(tzinfo=None)
+                    else:
+                        mailing_schedule_date.schedule_date = temp_schedule_date.astimezone(UTC).replace(tzinfo=None)
 
     @api.constrains('schedule_date')
     def _check_schedule_date(self):
