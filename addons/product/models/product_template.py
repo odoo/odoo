7a0d52c3ca6f364996a1afcacf06a717b9141aa7
Simon Lejeune <sle@openerp.com>
2020-03-05 10:24:39 +0000

    [FIX] product: expand group on categories only when needed
    Issue: on main product's kanban, if you group by categories, all
    categories (even the empty ones) appear. This is not desired.
    
    We only wanted this group_expand logic when using the products stat
    button on the product category form view.
    
    closes odoo/odoo#46966
    
    X-original-commit: e3f1254e5b28aa801f137835d871427786fb3428
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -29,7 +29,7 @@ class ProductTemplate(models.Model):
 
     def _read_group_categ_id(self, categories, domain, order):
         category_ids = self.env.context.get('default_categ_id')
-        if not category_ids:
+        if not category_ids and self.env.context.get('group_expand'):
             category_ids = categories._search([], order=order, access_rights_uid=SUPERUSER_ID)
         return categories.browse(category_ids)
 
