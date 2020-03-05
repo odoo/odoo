7baa11fde0f1b6eda8dd3040720861cb7cbad49b
Arnold Moyaux <arm@odoo.com>
2020-03-05 09:57:59 +0000

    [FIX] mrp: modify linked move remove quantity done
    - create product comp1
    - create product finished1
    - created bom: 1 comp1 for 1 finished1
    - activate PBM
    - 100 units of comp1 in stock
    - create an MO for 5
    - on the pbm, deliver 3 and no backorder
    - on the mo, check availability, produce 3
    - on the pbm, unlock, change delivered qty from 3 to 5
    
    On the mo, the raw move is reserved to 5 units but the 3 units
    used in the first produce are removed.
    
    It happens because editing the quantity or initial on a move
    will try to reserve the next moves. In order to reserved,
    _do_unreserve will destroy the stock.move.line and _action_assign
    (call just after) will recreate them in order to regenerate the
    reservation. In the process the qty_done on the stock.move.line will
    be loss. We want to avoid this behavior in mrp so we call
    _decrease_reserved_quantity from mrp that will keep stock.move.line
    with quantity done.
    
    opw-2206472
    
    closes odoo/odoo#47617
    
    X-original-commit: c6cf5841015c87163f8ea16dd75232e5cb855df6
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -685,7 +685,6 @@ class MrpProduction(models.Model):
         move = self.move_raw_ids.filtered(lambda x: x.bom_line_id.id == bom_line.id and x.state not in ('done', 'cancel'))
         if move:
             old_qty = move[0].product_uom_qty
-            remaining_qty = move[0].raw_material_production_id.product_qty - move[0].raw_material_production_id.qty_produced
             if quantity > 0:
                 move[0].write({'product_uom_qty': quantity})
                 move[0]._action_assign()
