a4ea3fb94790b21b92cf05b840d3150223f85123
fja-odoo <fja@odoo.com>
2020-03-06 15:09:44 +0000

    [FIX] web_editor: fix laggy option sections
    Some option sections are feeling really laggy (especialy the theme ones
    we now have in master/13.2). This was due to the "preview" activation of
    the snippet being triggered on mouseover and not on mouseenter.
    
    Introduced by: https://github.com/odoo/odoo/pull/36068
    
    task-2198177
    
    X-original-commit: e3196e08f1e071a10b56dc651aab6e41072e29c6

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -424,7 +424,7 @@ var SnippetEditor = Widget.extend({
                 node.parentNode.removeChild(node);
             }
         });
-        $optionsSection.on('mouseover', this._onOptionsSectionMouseOver.bind(this));
+        $optionsSection.on('mouseenter', this._onOptionsSectionMouseOver.bind(this));
         $optionsSection.on('mouseleave', this._onOptionsSectionMouseLeave.bind(this));
         $optionsSection.on('click', 'we-title > span', this._onOptionsSectionClick.bind(this));
         $optionsSection.on('click', '.oe_snippet_clone', this._onCloneClick.bind(this));
