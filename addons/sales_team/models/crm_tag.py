648f1b38434588e213cbf38bc197d583750980a8
vho <vho@odoo.com>
2020-03-12 10:35:49 +0000

    [IMP] sales_team, web: add and improve color widget to improve UX on crm.tag
    The color picker help the user to choose a color instead of picking it
    by id.
    
    The title and arial-label "no-color" default value are removed from the
    picker_preview because they are not accurate. Instead name of currently set
    color is used.
    
    Task ID 2128166
    
    closes odoo/odoo#47474
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/sales_team/models/crm_tag.py
+++ addons/sales_team/models/crm_tag.py
@@ -14,7 +14,7 @@ class Tag(models.Model):
         return randint(1, 11)
 
     name = fields.Char('Tag Name', required=True, translate=True)
-    color = fields.Integer('Color Index', default=_get_default_color)
+    color = fields.Integer('Color', default=_get_default_color)
 
     _sql_constraints = [
         ('name_uniq', 'unique (name)', "Tag name already exists !"),
