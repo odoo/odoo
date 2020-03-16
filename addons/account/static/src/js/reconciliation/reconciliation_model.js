db561c7b3625076ad648f3e907fbf2a4e62eabb3
Quentin Nicolay <qni@odoo.com>
2020-03-16 17:05:24 +0100

    [FIX] account: "load more" button in manual reconciliation
    Status : For the moment, the "load more" button skip some lines each time we use it.
    The splice is not used correctly. For example, on a array of 75, we see the first
    30 lines. The splice function cuts the array in two : The first 30 lines on one and last
    45 lines in an other. After the cut, we say that an index is at 30 to split on the load more.
    When we click on "Load more", we check the second array. But, as we use the index at 30 for
    the splice function, we skip the first 30 lines of the second array (with only 45 lines so we
    needed to take the first 30 lines) and only show the last 15 lines.
    
    Expected : When we click on "load more" button, we need to respect the pagerIndex
    and not remove elements.
    
    Fix : Changing the way we use the splice. Using slice instead of splice.
    
    opw-2171734
    closes #47719
    
    X-original-commit: 5d9d5d0367eafe2d759fdafa84b0e8f3783cf72c
    Co-authored-by: Nicolas Lempereur <nle@odoo.com>

--- addons/account/static/src/js/reconciliation/reconciliation_model.js
+++ addons/account/static/src/js/reconciliation/reconciliation_model.js
@@ -1248,7 +1248,7 @@ var ManualModel = StatementModel.extend({
                             self.manualLines = result;
                             self.valuenow = 0;
                             self.valuemax = Object.keys(self.manualLines).length;
-                            var lines = self.manualLines.splice(0, self.defaultDisplayQty);
+                            var lines = self.manualLines.slice(0, self.defaultDisplayQty);
                             self.pagerIndex = lines.length;
                             return self.loadData(lines);
                         });
@@ -1263,7 +1263,7 @@ var ManualModel = StatementModel.extend({
                             self.manualLines = result;
                             self.valuenow = 0;
                             self.valuemax = Object.keys(self.manualLines).length;
-                            var lines = self.manualLines.splice(0, self.defaultDisplayQty);
+                            var lines = self.manualLines.slice(0, self.defaultDisplayQty);
                             self.pagerIndex = lines.length;
                             return self.loadData(lines);
                         });
@@ -1285,7 +1285,7 @@ var ManualModel = StatementModel.extend({
                             self.manualLines = [].concat(result.accounts, result.customers, result.suppliers)
                             self.valuenow = 0;
                             self.valuemax = Object.keys(self.manualLines).length;
-                            var lines = self.manualLines.splice(0, self.defaultDisplayQty);
+                            var lines = self.manualLines.slice(0, self.defaultDisplayQty);
                             self.pagerIndex = lines.length;
                             return self.loadData(lines);
                         });
@@ -1302,7 +1302,7 @@ var ManualModel = StatementModel.extend({
         if (qty === undefined) {
             qty = this.defaultDisplayQty;
         }
-        var lines = this.manualLines.splice(this.pagerIndex, qty);
+        var lines = this.manualLines.slice(this.pagerIndex, this.pagerIndex + qty);
         this.pagerIndex += qty;
         return this.loadData(lines);
     },
