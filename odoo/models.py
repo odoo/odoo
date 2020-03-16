6ccae2912f24569b9d2b4fded2daeb8c5617502d
Xavier Morel <xmo@odoo.com>
2020-03-16 08:22:28 +0000

    [IMP] core: cutoff imports after some number of errors
    Importing 100 records and getting 100 errors (often the same every
    time because a required field was not mapped) is not super useful and
    spams the logs a lot.
    
    Cutting off at one point seems useful.
    
    Note: integrates warnings-related fix from #47972 reported by Grzegorz
    Marczyński in #47936.
    
    closes odoo/odoo#48095
    
    X-original-commit: 6b05f1d13c862dd66e89f35607d072315be80995
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/models.py
+++ odoo/models.py
@@ -987,8 +987,9 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
             except Exception:
                 pass
 
+            errors = 0
             # try again, this time record by record
-            for rec_data in data_list:
+            for i, rec_data in enumerate(data_list, 1):
                 try:
                     with cr.savepoint():
                         rec = self._load_records([rec_data], mode == 'update')
@@ -1001,6 +1002,7 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
                     messages.append(dict(info, type='error', **PGERROR_TO_OE[e.pgcode](self, fg, info, e)))
                     # Failed to write, log to messages, rollback savepoint (to
                     # avoid broken transaction) and keep going
+                    errors += 1
                 except Exception as e:
                     _logger.debug("Error while loading record", exc_info=True)
                     info = rec_data['info']
@@ -1009,6 +1011,13 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
                     messages.append(dict(info, type='error', message=message, moreinfo=moreinfo))
                     # Failed for some reason, perhaps due to invalid data supplied,
                     # rollback savepoint and keep going
+                    errors += 1
+                if errors >= 10 and (errors >= i / 10):
+                    messages.append({
+                        'type': 'warning',
+                        'message': _(u"Found more than 10 errors and more than one error per 10 records, interrupted to avoid showing too many errors.")
+                    })
+                    break
 
         # make 'flush' available to the methods below, in the case where XMLID
         # resolution fails, for instance
