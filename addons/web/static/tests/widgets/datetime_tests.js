ed300693b93215dd28c8637ff01975bf4933f15f
Mohammed Shekha <msh@odoo.com>
2020-03-17 14:42:13 +0530

    [IMP] Improvement

--- addons/web/static/tests/widgets/datetime_tests.js
+++ addons/web/static/tests/widgets/datetime_tests.js
@@ -1,38 +1,49 @@
 odoo.define('web.datetime_tests', function (require) {
 "use strict";
 
-var datepicker = require("web.datepicker");
-var testUtils = require("web.test_utils");
+const datepicker = require("web.datepicker");
+const testUtils = require("web.test_utils");
 
 QUnit.module('DatePicker', {}, function () {
 
     QUnit.module('DateTimeWidget', {}, function () {
 
-        QUnit.test("in date time widget, changing a date and then clicking on input", async function (assert) {
-            assert.expect(2);
+        QUnit.test("date time widget, selecting a date and clicking on input to close datepicker", async function (assert) {
+            assert.expect(4);
 
-            var $target = $("#qunit-fixture");
-
-            var parent = testUtils.createParent({});
-            var dateTimeWidget = new datepicker.DateTimeWidget(parent);
-
+            let counter = 0;
             testUtils.mock.patch(datepicker.DateTimeWidget, {
                 _setValueFromUi: function () {
-                    assert.ok(true, "should have triggered _setValueFromUi");
+                    counter++;
                     return this._super.apply(this, arguments);
                 },
             });
 
+            const $target = $("#qunit-fixture");
+
+            const parent = testUtils.createParent({});
+            const dateTimeWidget = new datepicker.DateTimeWidget(parent);
             await dateTimeWidget.appendTo($target);
+
             // opening the datepicker
             await testUtils.dom.openDatepicker($target.find('.o_datepicker'));
+
+            assert.strictEqual($('.bootstrap-datetimepicker-widget:visible').length, 1,
+                "datepicker should be opened");
+            assert.strictEqual(counter, 0, "counter should be 0");
             // selecting today's date (no date has been selected yet)
-            testUtils.dom.click(document.querySelector(".datepicker-days > table > tbody > tr > td.day.today"));
+            testUtils.dom.click($('.day:contains(22)'));
             // now click on the input element, this should invoke _setValueFromUi() 2 times
             testUtils.dom.click(document.querySelector(".o_datepicker_input"));
+            assert.strictEqual(counter, 2, "counter should be 0");
 
-            testUtils.mock.unpatch(datepicker.DateTimeWidget);
+            // re-open datepicker
+            testUtils.dom.openDatepicker($target.find('.o_datepicker'));
+            assert.strictEqual($('.day.active').text(), '22',
+                "datepicker should be highlight with 22nd day of month");
+
             dateTimeWidget.destroy();
+            testUtils.mock.unpatch(datepicker.DateTimeWidget);
         });
     });
 });
