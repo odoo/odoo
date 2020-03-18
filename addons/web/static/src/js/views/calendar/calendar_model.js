8619841a7465d8eb3ee852b85ca256274a10935d
Ivan Sokolov <jzx115@gmail.com>
2020-03-18 20:59:08 +0300

    [FIX] web: x2many fields fields for calendar color
    Steps to reproduce:
    - create calendar view with x2many field in 'color' definition option (e.g. color="other_user_ids")
    
    Previous behavior:
    Error is triggered
    
    Current behavior:
    Filters rendered and working properly

--- addons/web/static/src/js/views/calendar/calendar_model.js
+++ addons/web/static/src/js/views/calendar/calendar_model.js
@@ -578,85 +578,91 @@ return AbstractModel.extend({
      * @param {any} events
      * @returns {Deferred}
      */
-    _loadRecordsToFilters: function (element, events) {
-        var self = this;
-        var new_filters = {};
-        var to_read = {};
+   _loadRecordsToFilters: function (element, events) {
+            var self = this;
+            var new_filters = {};
+            var to_read = {};
 
-        _.each(this.data.filters, function (filter, fieldName) {
-            var field = self.fields[fieldName];
+            _.each(this.data.filters, function (filter, fieldName) {
+                var field = self.fields[fieldName];
 
-            new_filters[fieldName] = filter;
-            if (filter.write_model) {
-                if (field.relation === self.model_color) {
-                    _.each(filter.filters, function (f) {
-                        f.color_index = f.value;
+                new_filters[fieldName] = filter;
+                if (filter.write_model) {
+                    if (field.relation === self.model_color) {
+                        _.each(filter.filters, function (f) {
+                            f.color_index = f.value;
+                        });
+                    }
+                    return;
+                }
+
+                _.each(filter.filters, function (filter) {
+                    filter.display = !filter.active;
+                });
+
+                var fs = [];
+                var undefined_fs = [];
+                _.each(events, function (event) {
+                    var data =  event.record[fieldName];
+                    var x2many;
+                    if (!_.contains(['many2many', 'one2many'], field.type)) {
+                        data = [data];
+                        x2many = false;
+                    } else {
+                        to_read[field.relation] = (to_read[field.relation] || []).concat(data);
+                        x2many = true;
+                    }
+                    _.each(data, function (_value) {
+                        var value = _.isArray(_value) ? _value[0] : _value;
+                        var f = {
+                            'color_index': self.model_color === (field.relation || element.model) ? value : false,
+                            'value': value,
+                            'label': x2many? false: fieldUtils.format[field.type](_value, field) || _t("Undefined"),
+                            'avatar_model': field.relation || element.model,
+                        };
+                        // if field used as color does not have value then push filter in undefined_fs,
+                        // such filters should come last in filter list with Undefined string, later merge it with fs
+                        value ? fs.push(f) : undefined_fs.push(f);
                     });
-                }
-                return;
-            }
-
-            _.each(filter.filters, function (filter) {
-                filter.display = !filter.active;
-            });
-
-            var fs = [];
-            var undefined_fs = [];
-            _.each(events, function (event) {
-                var data =  event.record[fieldName];
-                if (!_.contains(['many2many', 'one2many'], field.type)) {
-                    data = [data];
-                } else {
-                    to_read[field.relation] = (to_read[field.relation] || []).concat(data);
-                }
-                _.each(data, function (_value) {
-                    var value = _.isArray(_value) ? _value[0] : _value;
-                    var f = {
-                        'color_index': self.model_color === (field.relation || element.model) ? value : false,
-                        'value': value,
-                        'label': fieldUtils.format[field.type](_value, field) || _t("Undefined"),
-                        'avatar_model': field.relation || element.model,
-                    };
-                    // if field used as color does not have value then push filter in undefined_fs,
-                    // such filters should come last in filter list with Undefined string, later merge it with fs
-                    value ? fs.push(f) : undefined_fs.push(f);
+                });
+                _.each(_.union(fs, undefined_fs), function (f) {
+                    var f1 = _.findWhere(filter.filters, f);
+                    if (f1) {
+                        f1.display = true;
+                    } else {
+                        f.display = f.active = true;
+                        filter.filters.push(f);
+                    }
                 });
             });
-            _.each(_.union(fs, undefined_fs), function (f) {
-                var f1 = _.findWhere(filter.filters, f);
-                if (f1) {
-                    f1.display = true;
-                } else {
-                    f.display = f.active = true;
-                    filter.filters.push(f);
-                }
-            });
-        });
 
-        var defs = [];
-        _.each(to_read, function (ids, model) {
-            defs.push(self._rpc({
+            var defs = [];
+            _.each(to_read, function (ids, model) {
+                defs.push(self._rpc({
                     model: model,
                     method: 'name_get',
                     args: [_.uniq(ids)],
                 })
-                .then(function (res) {
-                    to_read[model] = _.object(res);
-                }));
-        });
-        return $.when.apply($, defs).then(function () {
-            _.each(self.data.filters, function (filter) {
-                if (filter.write_model) {
-                    return;
-                }
-                if (filter.filters.length && (filter.filters[0].avatar_model in to_read)) {
-                    _.each(filter.filters, function (f) {
-                        f.label = to_read[f.avatar_model][f.value];
-                    });
-                }
+                    .then(function (res) {
+                        to_read[model] = _.object(res);
+                    }));
             });
-        });
-    },
+            return $.when.apply($, defs).then(function () {
+                _.each(self.data.filters, function (filter) {
+                    if (filter.write_model) {
+                        return;
+                    }
+                    if (filter.filters.length && (filter.filters[0].avatar_model in to_read)) {
+                        _.each(filter.filters, function (f) {
+                            var new_label = to_read[f.avatar_model][f.value];
+                            if (typeof new_label !== typeof undefined && new_label !== false){
+                                f.label = to_read[f.avatar_model][f.value];
+                            }
+                        });
+                    }
+                });
+            });
+        },
     /**
      * parse the server values to javascript framwork
      *
