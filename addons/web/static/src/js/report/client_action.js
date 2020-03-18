42927e8ef355358cd5498608d9cc1df074b7c20c
Aaron Bohy <aab@odoo.com>
2020-03-18 15:57:08 +0000

    [FIX] web: report client action: mount control panel
    Before this commit, the 'mounted' hook of the control panel in the
    report client action was never called, because of a missing call
    to '_super'. As a consequence, the 'Print' button wasn't displayed.
    
    closes odoo/odoo#47940
    
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/web/static/src/js/report/client_action.js
+++ addons/web/static/src/js/report/client_action.js
@@ -37,20 +37,16 @@ var ReportAction = AbstractAction.extend({
     start: function () {
         var self = this;
         this.iframe = this.$('iframe')[0];
+        this.$buttons = $(QWeb.render('report.client_action.ControlButtons', {}));
+        this.$buttons.on('click', '.o_report_print', this.on_click_print);
+        this.controlPanelProps.cp_content = {
+            $buttons: this.$buttons,
+        };
         return Promise.all([this._super.apply(this, arguments), session.is_bound]).then(async function () {
             var web_base_url = session['web.base.url'];
             var trusted_host = utils.get_host_from_url(web_base_url);
             var trusted_protocol = utils.get_protocol_from_url(web_base_url);
             self.trusted_origin = utils.build_origin(trusted_protocol, trusted_host);
-
-            self.$buttons = $(QWeb.render('report.client_action.ControlButtons', {}));
-            self.$buttons.on('click', '.o_report_print', self.on_click_print);
-
-            await self.updateControlPanel({
-                cp_content: {
-                    $buttons: self.$buttons,
-                },
-            });
 
             // Load the report in the iframe. Note that we use a relative URL.
             self.iframe.src = self.report_url;
@@ -71,10 +67,12 @@ var ReportAction = AbstractAction.extend({
         // messages and we can only filter them by their origin, so we chose to ignore the
         // messages that do not come from `web.base.url`.
         $(window).on('message', this, this.on_message_received);
+        this._super();
     },
 
     on_detach_callback: function () {
         $(window).off('message', this.on_message_received);
+        this._super();
     },
 
     /**
