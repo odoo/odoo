62a2c5357aeb3d11538fd7f678669708f695e680
Nicolas Lempereur <nle@odoo.com>
2020-03-19 14:15:46 +0000

    [FIX] website: menu translation on website
    Currently, the menu are only translated for installed language when we
    create a new website.
    
    When we create a new menu (eg. by installing a module) or install a new
    language we will only translate menu without website_id set, so the menu
    are not translated.
    
    With this changeset, we try to match translation of menu without
    website_id to menu with website_id when translations are updated:
    
    - when a language is installed/updated
    - when a module is installed/updated
    
    Without the changeset, the added test would fail with:
    
    - "Menu in english" != "Menu en français"
      Load translation add missing translation from template menu
    
    - "Menu in french" != "Menu en français"
      Load translation with overwriting update existing menu from template
    
    fixes #43365
    opw-2209864
    closes #48031
    
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- addons/website/models/__init__.py
+++ addons/website/models/__init__.py
@@ -8,6 +8,7 @@ from . import ir_qweb
 from . import ir_qweb_fields
 from . import website
 from . import ir_rule
+from . import ir_translation
 from . import ir_ui_view
 from . import res_company
 from . import res_partner
