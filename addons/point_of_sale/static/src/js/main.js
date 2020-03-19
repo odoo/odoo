aa481ef728755d0121e3e1f226b6d032ff40dbda
Lucas Perais (lpe) <lpe@odoo.com>
2020-03-19 14:02:33 +0100

    POS has very very very very lite actionManager capabilities

--- addons/point_of_sale/static/src/js/main.js
+++ addons/point_of_sale/static/src/js/main.js
@@ -1,4 +1,3 @@
-
 odoo.define('point_of_sale.main', function (require) {
     "use strict";
 
@@ -8,31 +7,18 @@ odoo.define('point_of_sale.main', function (require) {
      * that should not need the whole infrastructure
      * of the backend
      */
-    const chrome = require('point_of_sale.chrome');
-    const { ComponentAdapter } = require('web.OwlCompatibility');
-
     const env = require('web.env');
+    const PosRoot = require('point_of_sale.owlPosRoot');
     const session = require("web.session");
 
     owl.config.mode = env.isDebug() ? "dev" : "prod";
     owl.Component.env = env;
 
-    class PosRoot extends owl.Component {
-        mounted() {
-            odoo.isReady = true;
-            this.env.bus.trigger('web-client-mounted');
-            super.mounted();
-        }
-    };
-    PosRoot.template = owl.tags.xml`
-        <t t-component="ComponentAdapter" Component="props.Component" t-att-class="'o_action_manager'"/>
-    `;
-    PosRoot.components = { ComponentAdapter };
     /**
      * Add the owl templates to the environment and start the web client.
      */
     async function startPOS() {
-        const posRoot = new PosRoot(null, {Component: chrome.Chrome});
+        const posRoot = new PosRoot(null);
         await session.is_bound;
         env.qweb.addTemplates(session.owlTemplates);
 
@@ -40,9 +26,4 @@ odoo.define('point_of_sale.main', function (require) {
         posRoot.mount(document.body);
     }
     startPOS();
-});
-
-odoo.define('root.widget', function (require) {
-    "use strict";
-    return null;
 });
---
+++ addons/point_of_sale/static/src/js/owl_pos_root.js
@@ -0,0 +1,28 @@
+odoo.define('point_of_sale.owlPosRoot', function (require) {
+    "use strict";
+
+    const ActionManager = require('web.ActionManager');
+    const chrome = require('point_of_sale.chrome');
+    const { ComponentAdapter } = require('web.OwlCompatibility');
+
+    class PosRoot extends owl.Component {
+        constructor() {
+            super();
+            // Pos needs to be able to print Invoices
+            // this is the SOLE purpose of this actionManager
+            this.actionManager = new ActionManager(this.env);
+            this.PosChrome = chrome.Chrome;
+        }
+        mounted() {
+            odoo.isReady = true;
+            this.env.bus.trigger('web-client-mounted');
+            super.mounted();
+        }
+    };
+    PosRoot.template = owl.tags.xml`
+        <t t-component="ComponentAdapter" Component="PosChrome" t-att-class="'o_action_manager'"/>
+    `;
+    PosRoot.components = { ComponentAdapter };
+
+    return PosRoot;
+});
