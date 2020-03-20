d61e720fa4a38884e97ef08b466b5961fbbaf650
Aaron Bohy <aab@odoo.com>
2020-03-20 12:14:25 +0100

    [FIX] sale,*: product_configurator: can drag&drop row
    *sale_product_configurator
    
    Since commit 50bf830, a crash occurred when a row containing a
    many2one displayed with widget 'product_configurator' was dragged
    and dropped.
    
    For instance, open a quotation with at least two rows, and drag and
    drop one of them.
    
    Issue reported on task 2206228
    
    X-original-commit: d9c35b40b7df588f9e81142a2b272d9c7da06e04

--- addons/sale/static/src/js/product_configurator_widget.js
+++ addons/sale/static/src/js/product_configurator_widget.js
@@ -116,7 +116,7 @@ var ProductConfiguratorWidget = relationalFields.FieldMany2One.extend({
      */
     reset: async function (record, ev) {
         await this._super(...arguments);
-        if (ev.target === this) {
+        if (ev && ev.target === this) {
             if (ev.data.changes && !ev.data.preventProductIdCheck && ev.data.changes.product_template_id) {
                 this._onTemplateChange(record.data.product_template_id.data.id, ev.data.dataPointID);
             } else if (ev.data.changes && ev.data.changes.product_id) {
