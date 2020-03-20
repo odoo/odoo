485516d796223ade826b332e7c7ae8f7d725cfc6
fja-odoo <fja@odoo.com>
2020-03-20 15:50:06 +0100

    [FIX] web_editor: revert datepicker font awesome 5
    datetimepicker today button used a font awesome 5 icon.
    Font Awesome 5 was reverted to 4.7 but not this line.
    
    introduced by: 699890d3f0b9d5e4421ffb9b423a8dfa67ad469e
    
    task-2211899

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -1254,7 +1254,6 @@ const DatetimePickerUserValueWidget = InputUserValueWidget.extend({
             defaultDate: moment().format(),
             icons: {
                 close: 'fa fa-check primary',
-                today: 'far fa-calendar-check',
             },
             locale: moment.locale(),
             format: time.getLangDatetimeFormat(),
