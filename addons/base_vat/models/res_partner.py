9ebc6f2bfd9868398bb55bb782338beab9afcb2e
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2020-03-20 09:14:18 +0000

    [FIX] base_vat: skip compacting if not vat of the country
    Create a new contact with:
    - Name [DEMO]
    - VAT RORO790707I47
    - Country Mexico
    
    An error inform the user that the vat is not valid, this happens because
    the vat number is recognized as romanian and compacted before passing
    to actual vat checking. Skip the check if the country of the new
    partner does not match what is detected on the vat, assuming the user
    know what he/she is doing
    
    opw-2218491
    
    closes odoo/odoo#48080
    
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/base_vat/models/res_partner.py
+++ addons/base_vat/models/res_partner.py
@@ -463,8 +463,11 @@ class ResPartner(models.Model):
     def default_compact(self, vat):
         return vat
 
-    def _fix_vat_number(self, vat):
+    def _fix_vat_number(self, vat, country_id):
+        code = self.env['res.country'].browse(country_id).code if country_id else False
         vat_country, vat_number = self._split_vat(vat)
+        if code and code.lower() != vat_country:
+            return vat
         check_func_name = 'compact_' + vat_country
         check_func = globals().get(check_func_name) or getattr(self, 'default_compact')
         vat_number = check_func(vat_number)
@@ -473,11 +476,13 @@ class ResPartner(models.Model):
     @api.model
     def create(self, values):
         if values.get('vat'):
-            values['vat'] = self._fix_vat_number(values['vat'])
+            country_id = values.get('country_id')
+            values['vat'] = self._fix_vat_number(values['vat'], country_id)
         return super(ResPartner, self).create(values)
 
     def write(self, values):
         if values.get('vat'):
-            values['vat'] = self._fix_vat_number(values['vat'])
+            country_id = values.get('country_id', self.country_id.id)
+            values['vat'] = self._fix_vat_number(values['vat'], country_id)
         return super(ResPartner, self).write(values)
-        
+
