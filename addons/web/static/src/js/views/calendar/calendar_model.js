5ce4800e38b5692c2ed93c272ac6671ea747b3a8
Romeo Fragomeli <rfr@odoo.com>
2020-03-20 13:04:43 +0000

    [FIX] web: don't show all_day slot when no all_day mapping is present
    Before this commit, it was possible to create/move an event in the "all
    day" slot of FullCalendar even when the mapping field wasn't set.
    
    With this commit, when the "all day" mapping is not present, the "all
    day" slot of FullCalendar becomes hidden.
    
    Ref:
    https://fullcalendar.io/docs/allDaySlot
    
    Task ID: 2183249

--- addons/web/static/src/js/views/calendar/calendar_model.js
+++ addons/web/static/src/js/views/calendar/calendar_model.js
@@ -457,6 +457,7 @@ return AbstractModel.extend({
             dayNamesShort: moment.weekdaysShort(),
             firstDay: this.week_start,
             slotLabelFormat: _t.database.parameters.time_format.search("%H") !== -1 ? format24Hour : format12Hour,
+            allDaySlot: this.mapping.all_day,
         };
     },
     /**
