8badb4b5b346a2bd4cf9eada4e5a2dac2dc42d74
Thomas Carlier <thc@odoo.com>
2020-03-23 10:39:58 +0000

    [FIX] hr_expense: remove the expense_extract options from hr_expense
    before this commit:
    - the selection field with OCR options was in hr_expense module
    
    after this commit:
    - the selection field is now in expense_extract module
    
    closes odoo/odoo#48617
    
    X-original-commit: 9440139105825d4efa5fda23149c60179870f9f3
    Related: odoo/enterprise#9553
    Signed-off-by: Christophe Simonis <chs@odoo.com>
    Signed-off-by: Thomas Carlier <Explicitapps@users.noreply.github.com>

--- addons/hr_expense/models/res_config_settings.py
+++ addons/hr_expense/models/res_config_settings.py
@@ -12,8 +12,6 @@ class ResConfigSettings(models.TransientModel):
 
     module_hr_payroll_expense = fields.Boolean(string='Reimburse Expenses in Payslip')
     module_hr_expense_extract = fields.Boolean(string='Send bills to OCR to generate expenses')
-    expense_extract_show_ocr_option_selection = fields.Selection(related='company_id.expense_extract_show_ocr_option_selection',
-        string='Expense processing option', readonly=False)
 
 
     @api.model
