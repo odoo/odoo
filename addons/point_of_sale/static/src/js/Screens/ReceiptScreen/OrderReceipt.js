2e82d414e1177c0efc8237c38456d0e5aec2bbaa
Joseph Caburnay <jcb@odoo.com>
2020-03-24 15:15:50 +0100

    reactive OrderReceipt

--- addons/point_of_sale/static/src/js/Screens/ReceiptScreen/OrderReceipt.js
+++ addons/point_of_sale/static/src/js/Screens/ReceiptScreen/OrderReceipt.js
@@ -2,22 +2,29 @@ odoo.define('point_of_sale.OrderReceipt', function(require) {
     'use strict';
 
     const { PosComponent } = require('point_of_sale.PosComponent');
-    const {
-        WrappedProductNameLines,
-    } = require('point_of_sale.WrappedProductNameLines');
+    const { WrappedProductNameLines } = require('point_of_sale.WrappedProductNameLines');
     const Registry = require('point_of_sale.ComponentsRegistry');
 
     class OrderReceipt extends PosComponent {
         static template = 'OrderReceipt';
         constructor() {
             super(...arguments);
-            this.order = this.props.order;
-            const _receiptEnv = this.order.getOrderReceiptEnv();
-            this.receipt = _receiptEnv.receipt;
-            this.orderlines = _receiptEnv.orderlines;
-            this.paymentlines = _receiptEnv.paymentlines;
-            this.isTaxIncluded =
-                Math.abs(this.receipt.subtotal - this.receipt.total_with_tax) <= 0.000001;
+            this.receiptEnv = this.props.order.getOrderReceiptEnv();
+        }
+        get receipt() {
+            return this.receiptEnv.receipt;
+        }
+        get orderlines() {
+            return this.receiptEnv.orderlines;
+        }
+        get paymentlines() {
+            return this.receiptEnv.paymentlines;
+        }
+        get isTaxIncluded() {
+            return Math.abs(this.receipt.subtotal - this.receipt.total_with_tax) <= 0.000001;
+        }
+        willUpdateProps(nextProps) {
+            this.receiptEnv = nextProps.order.getOrderReceiptEnv();
         }
         isSimple(line) {
             return (
@@ -31,7 +38,7 @@ odoo.define('point_of_sale.OrderReceipt', function(require) {
             );
         }
     }
-    OrderReceipt.components = { WrappedProductNameLines }
+    OrderReceipt.components = { WrappedProductNameLines };
 
     Registry.add('OrderReceipt', OrderReceipt);
 
