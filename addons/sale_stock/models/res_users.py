91f33a47a33a2713b205ad877520927555656403
Victor Feyens <vfe@odoo.com>
2020-03-25 14:56:47 +0000

    [FIX] sale_stock: default warehouse_id
    Finetuning of
    https://github.com/odoo/odoo/commit/7736b59e840a6117ba337d3b0e969323ce7cde10
    :
    
    self.user_id is always an empty recordset in a default function call,
    use self.env.user instead, it is the default value for user_id.
    
    The _init_column override is needed to ensure the new property field on
    res_users isn't read at module installation, because its ir.model.fields
    doesn't exist already, leading to a wrong sql query in ir_property.
    
    closes odoo/odoo#48378
    
    X-original-commit: 6c6bd018c761535f973781319090116d7eea60ad
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale_stock/models/res_users.py
+++ addons/sale_stock/models/res_users.py
@@ -12,6 +12,8 @@ class Users(models.Model):
     def _get_default_warehouse_id(self):
         if self.property_warehouse_id:
             return self.property_warehouse_id
+        # !!! Any change to the following search domain should probably
+        # be also applied in sale_stock/models/sale_order.py/_init_column.
         return self.env['stock.warehouse'].search([('company_id', '=', self.env.company.id)], limit=1)
 
     def __init__(self, pool, cr):
