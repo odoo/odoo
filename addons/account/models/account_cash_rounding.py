5c85fe12a37474acddcc40fb775ad7ff456b3183
Nicolas Martinelli <nim@odoo.com>
2020-03-26 15:12:04 +0000

    [FIX] account, pos_cash_rounding: company_id on cash rounding
    - Create 2 companies C1 and C2
    - Create a cash rounding method for C2, i.e. use an account of C2
    - In the POS configuration of C1, choose the created cash rounding
      method
    
    When closing the POS, entries will be booked in C1 and C2 => error.
    
    We define a `company_id` field which is a related to the `account`
    company.
    
    opw-2223150
    opw-2217718
    
    closes odoo/odoo#48632
    
    X-original-commit: a1feee5db2d6509c0af8f59812a8e8ce5b23c662
    Signed-off-by: pimodoo <pimodoo@users.noreply.github.com>
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- addons/account/models/account_cash_rounding.py
+++ addons/account/models/account_cash_rounding.py
@@ -25,6 +25,7 @@ class AccountCashRounding(models.Model):
     rounding_method = fields.Selection(string='Rounding Method', required=True,
         selection=[('UP', 'UP'), ('DOWN', 'DOWN'), ('HALF-UP', 'HALF-UP')],
         default='HALF-UP', help='The tie-breaking rule used for float rounding operations')
+    company_id = fields.Many2one('res.company', related='profit_account_id.company_id')
 
     def round(self, amount):
         """Compute the rounding on the amount passed as parameter.
