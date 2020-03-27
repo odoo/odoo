a017e17bcafd6f3f765900e2eb98a905cc2c4a11
Aurélien Warnon <awa@odoo.com>
2020-03-27 11:40:09 +0100

    [IMP] website_event_questions: allow 'free text' questions
    PURPOSE
    
    Allow "free text" questions in the event questions asked to attendees, in
    addition to the existing "selection" type.
    e.g: "What are your allergies?"
    
    SPECS
    
    The implementation is heavily based on the modelisation we chose for the survey
    module since the features become similar in terms of question types and inputs.
    
    - Introduce a 'question_type' that is either 'simple_choice'
      (= previous behavior) or 'text_box' that will contain the attendee text
      answer.
    - Rename the event.answer model to event.question.answer to make it more
      obvious that it contains answer "suggestions" to a question of type
      'simple_choice'.
    - Introduce a 'event.user_input.line' that is an answer to a specific question
      for a specific registration.
      It works very similarly to 'survey.user_input.line' but it's not contained in
      a 'user_input' model, it's directly linked to the registration using a
      'user_input_line_ids' o2m field.
    
    The frontend view has been adapted accordingly to display either a selection
    input or a textarea based on the question type.
    The frontend orm processing now takes in both answer types (id of suggested
    answer or text input).
    
    On the backend side, views have been adapted to display event questions and
    attendee answers in a more convenient and logical manner.
    
    LINKS
    
    PR #45497
    Upgrade PR odoo/upgrade#799
    Task ID 2189529

--- addons/website_event_questions/tests/common.py
+++ addons/website_event_questions/tests/common.py
@@ -12,6 +12,7 @@ class TestEventQuestionCommon(TestEventCommon):
 
         cls.event_question_1 = cls.env['event.question'].create({
             'title': 'Question1',
+            'question_type': 'simple_choice',
             'event_type_id': cls.event_type_complex.id,
             'once_per_order': False,
             'answer_ids': [
@@ -21,11 +22,18 @@ class TestEventQuestionCommon(TestEventCommon):
         })
         cls.event_question_2 = cls.env['event.question'].create({
             'title': 'Question2',
+            'question_type': 'simple_choice',
             'event_type_id': cls.event_type_complex.id,
             'once_per_order': True,
             'answer_ids': [
                 (0, 0, {'name': 'Q2-Answer1'}),
                 (0, 0, {'name': 'Q2-Answer2'})
             ],
+        })
+        cls.event_question_3 = cls.env['event.question'].create({
+            'title': 'Question3',
+            'question_type': 'text_box',
+            'event_type_id': cls.event_type_complex.id,
+            'once_per_order': True,
         })
         cls.event_type_complex.write({'use_questions': True})
