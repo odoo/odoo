bc03acfe44d5db52fe7bbd9eec4b58e56ee158bf
alt-odoo <alt@odoo.com>
2020-03-27 13:49:03 +0100

    [FIX] account: wrong journal for cash basis entry
    In some situations, the context can contain a default journal id. When creating the cash
    basis entry, we are correctly retrieving the Cash Basis Journal for the account.move, but
    as we do not explicitly set the journal on the account.move.line also, it is using the
    context default value for journal_id (usually the customer invoices journal). As journal_id
    on account.move.line is a related field to journal_id of account.move, we are wrongly
    updating it. We should populate journal_id on account.move.line explicitly to avoid this.
    
    X-original-commit: ef702ff43c79b6554252f7d485a5c7abd5131587

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -1442,6 +1442,7 @@ class AccountPartialReconcile(models.Model):
         return {
             'name': line.name,
             'account_id': account_id,
+            'journal_id': new_move.journal_id.id,
             'tax_exigible': True,
             'tax_ids': [(6, 0, [tax])],
             'move_id': new_move.id,
@@ -1507,6 +1508,7 @@ class AccountPartialReconcile(models.Model):
                                 'currency_id': line.currency_id.id,
                                 'move_id': newly_created_move.id,
                                 'partner_id': line.partner_id.id,
+                                'journal_id': newly_created_move.journal_id.id,
                             })
                             # Group by cash basis account and tax
                             self.env['account.move.line'].with_context(check_move_validity=False).create({
@@ -1522,6 +1524,7 @@ class AccountPartialReconcile(models.Model):
                                 'currency_id': line.currency_id.id,
                                 'move_id': newly_created_move.id,
                                 'partner_id': line.partner_id.id,
+                                'journal_id': newly_created_move.journal_id.id,
                             })
                             if line.account_id.reconcile:
                                 #setting the account to allow reconciliation will help to fix rounding errors
