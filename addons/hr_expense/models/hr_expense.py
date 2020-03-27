27056955db94f11006860b7acc58fabdc0f2163f
Florent de Labarre <florent.mirieu@gmail.com>
2020-03-27 14:11:17 +0000

    [FIX] hr_expense: unit_amount erase when set product_id
    When an expense is create from email, if the product is not set, when you want set the product it is erase the unit price.
    
    closes odoo/odoo#50394
    
    X-original-commit: 74736914bb4d39947523f3dcd5e9d9455f4ee768
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -152,7 +152,8 @@ class HrExpense(models.Model):
         if self.product_id:
             if not self.name:
                 self.name = self.product_id.display_name or ''
-            self.unit_amount = self.product_id.price_compute('standard_price')[self.product_id.id]
+            if not self.attachment_number or (self.attachment_number and not self.unit_amount):
+                self.unit_amount = self.product_id.price_compute('standard_price')[self.product_id.id]
             self.product_uom_id = self.product_id.uom_id
             self.tax_ids = self.product_id.supplier_taxes_id.filtered(lambda tax: tax.company_id == self.company_id)  # taxes only from the same company
             account = self.product_id.product_tmpl_id.with_company(self.company_id)._get_product_accounts()['expense']
