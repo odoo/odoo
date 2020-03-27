5afeb6326b856bbae327c284ead4add1a7607566
lbs-odoo <lbs@odoo.com>
2020-03-27 15:46:44 +0100

    [IMP] sale_expense : add condition on expense_policy display
    The 'Re-Invoice Expenses' (expense_policy) can be visible only if 'Can be Expensed' is true.
    
    TASK#2169344

--- addons/sale_expense/models/product_template.py
+++ addons/sale_expense/models/product_template.py
@@ -11,10 +11,14 @@ class ProductTemplate(models.Model):
         visibility = self.user_has_groups('hr_expense.group_hr_expense_user')
         return visibility or super(ProductTemplate, self)._default_visible_expense_policy()
 
+    @api.depends('can_be_expensed')
     def _compute_visible_expense_policy(self):
-        super(ProductTemplate, self)._compute_visible_expense_policy()
+        expense_products = self.filtered(lambda p: p.can_be_expensed)
+        for product_template in self - expense_products:
+            product_template.visible_expense_policy = False
 
+        super(ProductTemplate, expense_products)._compute_visible_expense_policy()
         visibility = self.user_has_groups('hr_expense.group_hr_expense_user')
-        for product_template in self:
+        for product_template in expense_products:
             if not product_template.visible_expense_policy:
                 product_template.visible_expense_policy = visibility
