309291bc9fa49585646d94a672afe21dee715521
Thibault Delavallée <tde@odoo.com>
2020-03-30 11:23:15 +0000

    [MOV] mail: split alias model from its mixin
    Purpose is to ease module understanding.
    
    Task ID 2160070

--- addons/mail/models/__init__.py
+++ addons/mail/models/__init__.py
@@ -4,6 +4,7 @@
 from . import mail_message_subtype
 from . import mail_tracking_value
 from . import mail_alias
+from . import mail_alias_mixin
 from . import mail_followers
 from . import mail_notification
 from . import mail_render_mixin
