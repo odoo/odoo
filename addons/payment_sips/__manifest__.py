bea543f821ec60741554986a49b3c3d4c70787a7
Adrian Torres <adt@odoo.com>
2020-03-30 08:39:00 +0000

    [FIX] payment_*: reset payment acquirers to default on uninstall
    The `payment` module introduces a certain amount of payment acquirers,
    each one corresponding to a `payment_` module.
    
    When a `payment_` module is installed, this data is updated so that
    payments done with the corresponding acquirer change in behaviour using
    the provider installed by the `payment_` module.
    
    When a `payment_` module is uninstalled, this data should be reset to
    default, more especifically the `view_template_id` and the `provider`
    fields of `payment.acquirer`.
    
    This was not possible before this commit, and more importantly it would
    make the uninstallation of such `payment_` module impossible as the
    `view_template_id` is a required m2o ondelete='set null', which will
    make the registry crash. Even if the former wasn't a problem, the
    provider field would remain set to a non-existing selection option,
    which would make the registry crash (eventually, when checking a record
    with such a selection option).
    
    With this commit, we reset these fields to their default value upon
    module uninstall.
    
    In 13, the issue with `view_template_id` should be fixed, as required
    m2o that are ondelete='set null' are no longer possible. As for the
    provider Selection field, a fix should arrive in master soon.
    
    opw-2225333
    
    closes odoo/odoo#48778
    
    X-original-commit: 999a1312412cb12a8978efb39d5bd76a34d3a69d
    Signed-off-by: Raphael Collet (rco) <rco@openerp.com>
    Signed-off-by: Adrian Torres (adt) <adt@odoo.com>

--- addons/payment_sips/__manifest__.py
+++ addons/payment_sips/__manifest__.py
@@ -21,4 +21,5 @@ payments acquirer using Worldline SIPS.""",
     ],
     'installable': True,
     'post_init_hook': 'create_missing_journal_for_acquirers',
+    'uninstall_hook': 'uninstall_hook',
 }
