72af06ab90bf0f38ec36d5340d852cf79cf32a63
lejeune quentin <qle@odoo.com>
2020-03-31 15:11:50 +0000

    [FIX] point_of_sale: Fix package for access point on IoT
    When we build a new box we make a update && upgrade of the raspbian image.
    But it upgrade "firmware-brcm80211" from version "1:20190114+rpt4" to "1:20190114+rpt5"
    With this update and when Odoo run on the box the "wifi access point" can't be activated
    
    With this commit we unabled the update of this package and keep the "1:20190114+rpt5"
    
    closes odoo/odoo#48684
    
    Signed-off-by: Quentin Lejeune (qle) <qle@odoo.com>

--- addons/point_of_sale/tools/posbox/overwrite_before_init/etc/init_posbox_image.sh
+++ addons/point_of_sale/tools/posbox/overwrite_before_init/etc/init_posbox_image.sh
@@ -20,6 +20,8 @@ echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
 locale-gen
 source ~/.bashrc
 
+apt-mark hold firmware-brcm80211
+# upgrade firmware-brcm80211 broke access point on rpi4
 apt-get update && apt-get -y upgrade
 # Do not be too fast to upgrade to more recent firmware and kernel than 4.38
 # Firmware 4.44 seems to prevent the LED mechanism from working
