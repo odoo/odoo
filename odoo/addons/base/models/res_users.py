b7380095b2ccc9152a6f029d8caadd11d12b6f3c
Raphael Collet <rco@odoo.com>
2020-04-02 08:45:54 +0200

    [IMP] ir.ui.view: call _update_user_groups_view() once per install
    This saves 1.5% of the total installation time.

--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -18,6 +18,7 @@ from lxml.builder import E
 import passlib.context
 
 from odoo import api, fields, models, tools, SUPERUSER_ID, _
+from odoo.addons.base.models.ir_model import MODULE_UNINSTALL_FLAG
 from odoo.exceptions import AccessDenied, AccessError, UserError, ValidationError
 from odoo.http import request
 from odoo.osv import expression
@@ -1085,6 +1086,10 @@ class GroupsView(models.Model):
         """ Modify the view with xmlid ``base.user_groups_view``, which inherits
             the user form view, and introduces the reified group fields.
         """
+        if self._context.get('install_filename') or self._context.get(MODULE_UNINSTALL_FLAG):
+            # skip now; it will be updated once at the end of (un)installation
+            return
+
         # remove the language to avoid translations, it will be handled at the view level
         self = self.with_context(lang=None)
 
