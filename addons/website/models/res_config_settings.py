cac6882b6c8d4615fef0e62a14f85da948736655
fja-odoo <fja@odoo.com>
2020-04-06 13:23:06 +0200

    [FIX] website: fix res config settings related
    Writing on a related field in the inverse method does not write on the
    field it is related to (website_id).
    
    The issue in the settings panel was that unchecking an option did not
    deactivate it. Also when an option was deactivated, the fields were not
    reseted.
    
    task-2232185

--- addons/website/models/res_config_settings.py
+++ addons/website/models/res_config_settings.py
@@ -27,9 +27,21 @@ class ResConfigSettings(models.TransientModel):
     specific_user_account = fields.Boolean(related='website_id.specific_user_account', readonly=False,
                                            help='Are newly created user accounts website specific')
 
-    google_analytics_key = fields.Char('Google Analytics Key', related='website_id.google_analytics_key', readonly=False)
-    google_management_client_id = fields.Char('Google Client ID', related='website_id.google_management_client_id', readonly=False)
-    google_management_client_secret = fields.Char('Google Client Secret', related='website_id.google_management_client_secret', readonly=False)
+    google_analytics_key = fields.Char('Google Analytics Key', compute="_compute_google_analytics_key", store=True, readonly=False)
+    google_management_client_id = fields.Char('Google Client ID', compute="_compute_google_management_client_id", store=True, readonly=False)
+    google_management_client_secret = fields.Char('Google Client Secret', compute="_compute_google_management_client_secret", store=True, readonly=False)
+
+    @api.depends('website_id')
+    def _compute_google_analytics_key(self):
+        self.google_analytics_key = self.website_id.google_analytics_key
+
+    @api.depends('website_id')
+    def _compute_google_management_client_id(self):
+        self.google_management_client_id = self.website_id.google_management_client_id
+
+    @api.depends('website_id')
+    def _compute_google_management_client_secret(self):
+        self.google_management_client_secret = self.website_id.google_management_client_secret
 
     cdn_activated = fields.Boolean(related='website_id.cdn_activated', readonly=False)
     cdn_url = fields.Char(related='website_id.cdn_url', readonly=False)
@@ -39,34 +51,53 @@ class ResConfigSettings(models.TransientModel):
     auth_signup_uninvited = fields.Selection(compute="_compute_auth_signup",
         inverse="_set_auth_signup")
 
-    social_twitter = fields.Char(related='website_id.social_twitter', readonly=False)
-    social_facebook = fields.Char(related='website_id.social_facebook', readonly=False)
-    social_github = fields.Char(related='website_id.social_github', readonly=False)
-    social_linkedin = fields.Char(related='website_id.social_linkedin', readonly=False)
-    social_youtube = fields.Char(related='website_id.social_youtube', readonly=False)
-    social_instagram = fields.Char(related='website_id.social_instagram', readonly=False)
+    social_twitter = fields.Char(compute="_compute_social_twitter", store=True, readonly=False)
+    social_facebook = fields.Char(compute="_compute_social_facebook", store=True, readonly=False)
+    social_github = fields.Char(compute="_compute_social_github", store=True, readonly=False)
+    social_linkedin = fields.Char(compute="_compute_social_linkedin", store=True, readonly=False)
+    social_youtube = fields.Char(compute="_compute_social_youtube", store=True, readonly=False)
+    social_instagram = fields.Char(compute="_compute_social_instagram", store=True, readonly=False)
+
+    @api.depends('website_id')
+    def _compute_social_twitter(self):
+        self.social_twitter = self.website_id.social_twitter
+
+    @api.depends('website_id')
+    def _compute_social_facebook(self):
+        self.social_facebook = self.website_id.social_facebook
+
+    @api.depends('website_id')
+    def _compute_social_github(self):
+        self.social_github = self.website_id.social_github
+
+    @api.depends('website_id')
+    def _compute_social_linkedin(self):
+        self.social_linkedin = self.website_id.social_linkedin
+
+    @api.depends('website_id')
+    def _compute_social_youtube(self):
+        self.social_youtube = self.website_id.social_youtube
+
+    @api.depends('website_id')
+    def _compute_social_instagram(self):
+        self.social_instagram = self.website_id.social_instagram
 
     @api.depends('website_id', 'social_twitter', 'social_facebook', 'social_github', 'social_linkedin', 'social_youtube', 'social_instagram')
-    def has_social_network(self):
+    def _compute_has_social_network(self):
         self.has_social_network = self.social_twitter or self.social_facebook or self.social_github \
             or self.social_linkedin or self.social_youtube or self.social_instagram
 
-    def inverse_has_social_network(self):
-        if not self.has_social_network:
-            self.social_twitter = ''
-            self.social_facebook = ''
-            self.social_github = ''
-            self.social_linkedin = ''
-            self.social_youtube = ''
-            self.social_instagram = ''
-
-    has_social_network = fields.Boolean("Configure Social Network", compute=has_social_network, inverse=inverse_has_social_network)
+    has_social_network = fields.Boolean("Configure Social Network", compute='_compute_has_social_network', store=True, readonly=False)
 
     favicon = fields.Binary('Favicon', related='website_id.favicon', readonly=False)
     social_default_image = fields.Binary('Default Social Share Image', related='website_id.social_default_image', readonly=False)
 
-    google_maps_api_key = fields.Char(related='website_id.google_maps_api_key', readonly=False)
+    google_maps_api_key = fields.Char(compute="_compute_google_maps_api_key", store=True, readonly=False)
     group_multi_website = fields.Boolean("Multi-website", implied_group="website.group_multi_website")
+
+    @api.depends('website_id')
+    def _compute_google_maps_api_key(self):
+        self.google_maps_api_key = self.website_id.google_maps_api_key
 
     @api.depends('website_id.auth_signup_uninvited')
     def _compute_auth_signup(self):
@@ -78,34 +109,20 @@ class ResConfigSettings(models.TransientModel):
             config.website_id.auth_signup_uninvited = config.auth_signup_uninvited
 
     @api.depends('website_id')
-    def has_google_analytics(self):
+    def _compute_has_google_analytics(self):
         self.has_google_analytics = bool(self.google_analytics_key)
 
     @api.depends('website_id')
-    def has_google_analytics_dashboard(self):
+    def _compute_has_google_analytics_dashboard(self):
         self.has_google_analytics_dashboard = bool(self.google_management_client_id)
 
     @api.depends('website_id')
-    def has_google_maps(self):
+    def _compute_has_google_maps(self):
         self.has_google_maps = bool(self.google_maps_api_key)
 
-    def inverse_has_google_analytics(self):
-        if not self.has_google_analytics:
-            self.has_google_analytics_dashboard = False
-            self.google_analytics_key = False
-
-    def inverse_has_google_maps(self):
-        if not self.has_google_maps:
-            self.google_maps_api_key = False
-
-    def inverse_has_google_analytics_dashboard(self):
-        if not self.has_google_analytics_dashboard:
-            self.google_management_client_id = False
-            self.google_management_client_secret = False
-
-    has_google_analytics = fields.Boolean("Google Analytics", compute=has_google_analytics, inverse=inverse_has_google_analytics)
-    has_google_analytics_dashboard = fields.Boolean("Google Analytics Dashboard", compute=has_google_analytics_dashboard, inverse=inverse_has_google_analytics_dashboard)
-    has_google_maps = fields.Boolean("Google Maps", compute=has_google_maps, inverse=inverse_has_google_maps)
+    has_google_analytics = fields.Boolean("Google Analytics", compute='_compute_has_google_analytics', store=True, readonly=False)
+    has_google_analytics_dashboard = fields.Boolean("Google Analytics Dashboard", compute='_compute_has_google_analytics_dashboard', store=True, readonly=False)
+    has_google_maps = fields.Boolean("Google Maps", compute='_compute_has_google_maps', store=True, readonly=False)
 
     @api.onchange('language_ids')
     def _onchange_language_ids(self):
@@ -123,8 +140,36 @@ class ResConfigSettings(models.TransientModel):
             config.website_language_count = len(self.language_ids)
 
     def set_values(self):
-        super(ResConfigSettings, self).set_values()
+        super().set_values()
+        if not self.has_social_network:
+            self.social_twitter = ''
+            self.social_facebook = ''
+            self.social_github = ''
+            self.social_linkedin = ''
+            self.social_youtube = ''
+            self.social_instagram = ''
+        self.website_id.social_twitter = self.social_twitter
+        self.website_id.social_facebook = self.social_facebook
+        self.website_id.social_github = self.social_github
+        self.website_id.social_linkedin = self.social_linkedin
+        self.website_id.social_youtube = self.social_youtube
+        self.website_id.social_instagram = self.social_instagram
 
+        if not self.has_google_analytics:
+            self.has_google_analytics_dashboard = False
+            self.google_analytics_key = False
+        self.website_id.has_google_analytics_dashboard = self.has_google_analytics_dashboard
+        self.website_id.google_analytics_key = self.google_analytics_key
+
+        if not self.has_google_maps:
+            self.google_maps_api_key = False
+        self.website_id.google_maps_api_key = self.google_maps_api_key
+
+        if not self.has_google_analytics_dashboard:
+            self.google_management_client_id = False
+            self.google_management_client_secret = False
+        self.website_id.google_management_client_id = self.google_management_client_id
+        self.website_id.google_management_client_secret = self.google_management_client_secret
     def open_template_user(self):
         action = self.env.ref('base.action_res_users').read()[0]
         action['res_id'] = literal_eval(self.env['ir.config_parameter'].sudo().get_param('base.template_portal_user_id', 'False'))
