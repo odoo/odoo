cogitoweb/OCB

[FIX] core: calling read_group / name_search over xmlrpc
Also non-browser jsonrpc (as it goes through a similar process): for
internal performance reasons, name_search and read_group have been
converted to a *lazy* name_get, so the "display name" is not
unnecessarily computed.

However this is an issue for the RPC endpoints (/xmlrpc and /jsonrpc)
as they have no support for `lazy` and thus tend to blow up and / or
do the wrong thing when trying to output a lazy:

* xmlrpc has no way to handle lazy at all and straight blows up
* jsonrpc falls back to `json_default` so they try to stringify the
  lazy, which might have worked except

*Problematically* both endpoints delegate the actual work to
`dispatch_rpc` which handles dispatching between various services and
ultimately creates a *new* cursor before calling model
methods (`object` service and `execute`/`execute_kw`).

This means by the time the result is serialized to be output, the
lazy's cursor has long been closed, and thus any access to an
unevaluated `lazy` errors out when trying to fetch the underlying
item.

This also means we can't just add a hook to serialize the lazy
in the xmlrpc marshaller, though we do have to do that. We *also* (for
both xmlrpc and jsonrpc) have to force evluation of lazy values before
our cursor is closed, meaning it has to be done right after the method
is invoked, iterating the entire response.

Related to task 2170343

closes odoo/odoo#47801

Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

odoo/addons/test_performance/tests/test_performance.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 import json
 
 from odoo.tests.common import TransactionCase, users, warmup
-from odoo.tools import pycompat
+from odoo.tools import pycompat, json_default
 
 
 class TestPerformance(TransactionCase):
@@ -141,4 +141,4 @@ class TestPerformance(TransactionCase):
                 self.assertEqual(res['value'], exp['value'])
         # now serialize to json, which should force evaluation
         with self.assertQueryCount(__system__=1, demo=1):
-            json.dumps(result)
+            json.dumps(result, default=json_default)

odoo/odoo

X-original-commit: ac78a9fab9109f63bcdc149556e1010cdcb6e417

odoo/addons/test_performance/tests/test_performance.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 import json
 
 from odoo.tests.common import TransactionCase, users, warmup
-from odoo.tools import mute_logger
+from odoo.tools import mute_logger, json_default
 
 
 class TestPerformance(TransactionCase):
@@ -406,4 +406,4 @@ class TestPerformance(TransactionCase):
                 self.assertEqual(res['value'], exp['value'])
         # now serialize to json, which should force evaluation
         with self.assertQueryCount(__system__=1, demo=1):
-            json.dumps(result)
+            json.dumps(result, default=json_default)