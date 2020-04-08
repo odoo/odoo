edbe6f3390c00f8bbdc6135b0654835d82909f9d
Nicolas Martinelli <nim@odoo.com>
2020-04-08 14:22:49 +0000

    [FIX] purchase: invoice status
    Filter out section and notes from the invoice status computation.
    
    opw-2233550
    
    X-original-commit: 6406ad719018316cb90cdb49ff7d810c45057813

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -40,9 +40,18 @@ class PurchaseOrder(models.Model):
                 order.invoice_status = 'no'
                 continue
 
-            if any(not float_is_zero(line.qty_to_invoice, precision_digits=precision) for line in order.order_line):
+            if any(
+                not float_is_zero(line.qty_to_invoice, precision_digits=precision)
+                for line in order.order_line.filtered(lambda l: not l.display_type)
+            ):
                 order.invoice_status = 'to invoice'
-            elif all(float_is_zero(line.qty_to_invoice, precision_digits=precision) for line in order.order_line) and order.invoice_ids:
+            elif (
+                all(
+                    float_is_zero(line.qty_to_invoice, precision_digits=precision)
+                    for line in order.order_line.filtered(lambda l: not l.display_type)
+                )
+                and order.invoice_ids
+            ):
                 order.invoice_status = 'invoiced'
             else:
                 order.invoice_status = 'no'
