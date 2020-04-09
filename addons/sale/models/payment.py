92c9cc16a2f5e9dd945ae26c025fb9b6e5553408
Florent de Labarre <florent.mirieu@gmail.com>
2020-04-09 12:00:53 +0000

    [FIX] sale: use rounding of currency of order
    closes odoo/odoo#59402
    
    X-original-commit: 6c68a9b6af19ed1149b2a6651bce3ca5e36a6c8b
    Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

--- addons/sale/models/payment.py
+++ addons/sale/models/payment.py
@@ -74,7 +74,7 @@ class PaymentTransaction(models.Model):
     def _check_amount_and_confirm_order(self):
         self.ensure_one()
         for order in self.sale_order_ids.filtered(lambda so: so.state in ('draft', 'sent')):
-            if float_compare(self.amount, order.amount_total, 2) == 0:
+            if order.currency_id.compare_amounts(self.amount, order.amount_total) == 0:
                 order.with_context(send_email=True).action_confirm()
             else:
                 _logger.warning(
