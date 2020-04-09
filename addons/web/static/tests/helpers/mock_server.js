f5f0ca843d98bcbaf0fd8ad63a31a3cacf390168
Julien Mougenot <jum@odoo.com>
2020-04-09 07:42:20 +0000

    [IMP] *: enable_counters with false as default
    In the search panel, the attribute disable_counters with default False
    has been changed to enable_counters with default False.

--- addons/web/static/tests/helpers/mock_server.js
+++ addons/web/static/tests/helpers/mock_server.js
@@ -627,7 +627,7 @@ var MockServer = Class.extend({
     _mockSearchPanelRange: function (model, fieldName, kwargs = {}) {
         const field = this.data[model].fields[fieldName];
         let localCounters = {};
-        if (field.type !== 'many2many' && !kwargs.disable_counters) {
+        if (field.type !== 'many2many' && kwargs.enable_counters) {
             localCounters = this._mockSearchPanelLocalCounters(model, fieldName, kwargs);
         }
         if (['many2one', 'many2many'].includes(field.type)) {
@@ -690,7 +690,7 @@ var MockServer = Class.extend({
         }
         const rangeKwargs = Object.assign({ comodelFieldNames }, kwargs);
         const valuesRange = this._mockSearchPanelRange(model, fieldName, rangeKwargs);
-        if (parentName && !kwargs.disable_counters) {
+        if (parentName && kwargs.enable_counters) {
             this._mockSearchPanelGlobalCounters(valuesRange, parentName);
         }
         return {
@@ -748,7 +748,7 @@ var MockServer = Class.extend({
         }
         const rangeKwargs = Object.assign({ comodelFieldNames }, kwargs);
         const valuesRange = this._mockSearchPanelRange(model, fieldName, rangeKwargs);
-        if (field.type === 'many2many' && !kwargs.disable_counters) {
+        if (field.type === 'many2many' && kwargs.enable_counters) {
             const modelDomain = Domain.prototype.normalizeArray([
                 ...(kwargs.search_domain || []),
                 ...(kwargs.category_domain || []),
