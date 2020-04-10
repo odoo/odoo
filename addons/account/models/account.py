odoo/odoo

commit beaa30a3d1843de43a45f419bfbc1bfa7613a920

[IMP/REF] accounting-pocalypse yeaaahh
This commit merges the following models
 * account.invoice and account.move
 * account.invoice.line and account.move.line
 * account.voucher and account.move
 * account.voucher.line and account.move.line

It was the opportunity for a big cleanup of the code, so it also restructures the whole account module, its different models/fields, the tests etc. for a better world and a better code readability.

==== Rationale ====
The rationale of this huge change is that we want journal entries / invoices to be easily edited, and changes reflected in the other model. It's a HUGE feature and very strategic for the fiduciary companies. For example, changing the account of a journal entry needs to be automatically reflected on the related invoice.

The same reasoning applies to sale/purchase vouchers.

==== Changes made in features =====
When creating an invoice, you are now creating a journal entry directly.
--> The object account.invoice no longer exists.
In the same fashion when creating an invoice line, you're now adding journal items directly in the journal entry representing the invoice. If this invoice line has some tax, it may create additional journal items as well.
--> The models account.invoice.line & account.invoice.tax no longer exist

Identically, when creating a sale/purchase receipt with its lines, you are now creating a journal entry directly and there's no more usability difference between encoding a receipt or an invoice.
--> The object account.voucher no longer exists.
--> The object account.voucher.line no longer exists.
--> The whole account_voucher module no longer exists.

Positive side-effects coming from these changes are
* draft invoices/bills/sale or purchase receipts now create a draft accounting entry. Validate these objects now simply post its journal entry. That means that draft invoices/bills/sale or purchase receipt can straightforwardly be included in reporting or budgets.

addons/account/models/account.py
@@ -553,8 +553,6 @@ class AccountJournal(models.Model):
         domain=[('deprecated', '=', False)], help="It acts as a default account for debit amount")
     update_posted = fields.Boolean(string='Allow Cancelling Entries',
         help="Check this box if you want to allow the cancellation the entries related to this journal or of the invoice related to this journal")
-    group_invoice_lines = fields.Boolean(string='Group Invoice Lines',
-        help="If this box is checked, the system will try to group the accounting lines when generating them from invoices.")
     sequence_id = fields.Many2one('ir.sequence', string='Entry Sequence',
         help="This field contains the information related to the numbering of the journal entries of this journal.", required=True, copy=False)
     refund_sequence_id = fields.Many2one('ir.sequence', string='Credit Note Entry Sequence',
@@ -738,7 +736,7 @@ class AccountJournal(models.Model):
         if self.alias_id:
             self.alias_id.write(alias_values)
         else:
-            self.alias_id = self.env['mail.alias'].with_context(alias_model_name='account.invoice',
+            self.alias_id = self.env['mail.alias'].with_context(alias_model_name='account.move',
                 alias_parent_model_name='account.journal').create(alias_values)
 
         if vals.get('alias_name'):
@@ -1179,17 +1177,6 @@ class AccountTax(models.Model):
         if self.price_include:
             self.include_base_amount = True
 
-    def get_grouping_key(self, invoice_tax_val):
-        """ Returns a string that will be used to group account.invoice.tax sharing the same properties"""
-        self.ensure_one()
-        return str(invoice_tax_val['tax_id']) + '-' + \
-               str(invoice_tax_val.get('tax_repartition_line_id')) + '-' + \
-               str(invoice_tax_val['account_id']) + '-' + \
-               str(invoice_tax_val['account_analytic_id']) + '-' + \
-               str(invoice_tax_val.get('analytic_tag_ids', [])) + '-' + \
-               str(invoice_tax_val.get('tax_ids') or []) + '-' + \
-               str(invoice_tax_val.get('tag_ids') or [])
-
     def _compute_amount(self, base_amount, price_unit, quantity=1.0, product=None, partner=None):
         """ Returns the amount of a single tax. base_amount is the actual amount on which the tax is applied, which is
             price_unit * quantity eventually affected by previous taxes (if tax is include_base_amount XOR price_include)
@@ -1210,7 +1197,7 @@ class AccountTax(models.Model):
             else:
                 return quantity * self.amount
 
-        price_include = self._context['force_price_include'] if 'force_price_include' in self._context else self.price_include
+        price_include = self._context.get('force_price_include', self.price_include)
 
         # base * (1 + tax_amount) = new_base
         if self.amount_type == 'percent' and not price_include:
@@ -1254,11 +1241,14 @@ class AccountTax(models.Model):
         return rep_lines.filtered(lambda x: x.repartition_type == repartition_type).mapped('tag_ids')
 
     @api.multi
-    def compute_all(self, price_unit, currency=None, quantity=1.0, product=None, partner=None, is_refund=False):
+    def compute_all(self, price_unit, currency=None, quantity=1.0, product=None, partner=None, is_refund=False, handle_price_include=True):
         """ Returns all information required to apply taxes (in self + their children in case of a tax group).
             We consider the sequence of the parent for group of taxes.
                 Eg. considering letters as taxes and alphabetic order as sequence :
                 [G, B([A, D, F]), E, C] will be computed as [A, D, F, C, E, G]
+
+            'handle_price_include' is used when we need to ignore all tax included in price. If False, it means the
+            amount passed to this method will be considered as the base of all computations.
 
         RETURN: {
             'total_excluded': 0.0,    # Total without taxes
@@ -1362,28 +1352,29 @@ class AccountTax(models.Model):
         incl_fixed_amount = incl_percent_amount = incl_division_amount = 0
         # Store the tax amounts we compute while searching for the total_excluded
         cached_tax_amounts = {}
-        for tax in reversed(taxes):
-            if tax.include_base_amount:
-                base = recompute_base(base, incl_fixed_amount, incl_percent_amount, incl_division_amount)
-                incl_fixed_amount = incl_percent_amount = incl_division_amount = 0
-                store_included_tax_total = True
-            if tax.price_include:
-                if tax.amount_type == 'percent':
-                    incl_percent_amount += tax.amount
-                elif tax.amount_type == 'division':
-                    incl_division_amount += tax.amount
-                elif tax.amount_type == 'fixed':
-                    incl_fixed_amount += quantity * tax.amount
-                else:
-                    # tax.amount_type == other (python)
-                    tax_amount = tax._compute_amount(base, price_unit, quantity, product, partner)
-                    incl_fixed_amount += tax_amount
-                    # Avoid unecessary re-computation
-                    cached_tax_amounts[i] = tax_amount
-                if store_included_tax_total:
-                    total_included_checkpoints[i] = base
-                    store_included_tax_total = False
-            i -= 1
+        if handle_price_include:
+            for tax in reversed(taxes):
+                if tax.include_base_amount:
+                    base = recompute_base(base, incl_fixed_amount, incl_percent_amount, incl_division_amount)
+                    incl_fixed_amount = incl_percent_amount = incl_division_amount = 0
+                    store_included_tax_total = True
+                if tax.price_include:
+                    if tax.amount_type == 'percent':
+                        incl_percent_amount += tax.amount
+                    elif tax.amount_type == 'division':
+                        incl_division_amount += tax.amount
+                    elif tax.amount_type == 'fixed':
+                        incl_fixed_amount += quantity * tax.amount
+                    else:
+                        # tax.amount_type == other (python)
+                        tax_amount = tax._compute_amount(base, price_unit, quantity, product, partner)
+                        incl_fixed_amount += tax_amount
+                        # Avoid unecessary re-computation
+                        cached_tax_amounts[i] = tax_amount
+                    if store_included_tax_total:
+                        total_included_checkpoints[i] = base
+                        store_included_tax_total = False
+                i -= 1
 
         total_excluded = recompute_base(base, incl_fixed_amount, incl_percent_amount, incl_division_amount)
 
@@ -1438,8 +1429,8 @@ class AccountTax(models.Model):
                     'price_include': tax.price_include,
                     'tax_exigibility': tax.tax_exigibility,
                     'tax_repartition_line_id': repartition_line.id,
-                    'tag_ids': [(6, False, (repartition_line.tag_ids + subsequent_tags).ids)],
-                    'tax_ids': [(6, False, subsequent_taxes.ids)]
+                    'tag_ids': (repartition_line.tag_ids + subsequent_tags).ids,
+                    'tax_ids': subsequent_taxes.ids,
                 })
 
                 total_amount += line_amount

addons/account/views/account_portal_templates.xml
@@ -6,7 +6,7 @@
                 <t t-else="">Invoices &amp; Bills</t>
             </li>
             <li t-if="invoice" class="breadcrumb-item active">
-                <t t-esc="invoice.number" t-if="invoice.number"/>
+                <t t-esc="invoice.name" t-if="invoice.name"/>
                 <t t-else=""><em>Draft Invoice</em></t>
             </li>
         </xpath>
@@ -46,13 +46,13 @@
                 <t t-foreach="invoices" t-as="invoice">
                     <tr>
                         <td>
-                            <a t-att-href="invoice.get_portal_url()" t-att-title="invoice.number">
-                                <t t-esc="invoice.number" t-if="invoice.number"/>
+                            <a t-att-href="invoice.get_portal_url()" t-att-title="invoice.name">
+                                <t t-esc="invoice.name" t-if="invoice.name"/>
                                 <em t-else="">Draft Invoice</em>
                             </a>
                         </td>
-                        <td><span t-field="invoice.date_invoice"/></td>
-                        <td class='d-none d-md-table-cell'><span t-field="invoice.date_due"/></td>
+                        <td><span t-field="invoice.invoice_date"/></td>
+                        <td class='d-none d-md-table-cell'><span t-field="invoice.invoice_date_due"/></td>
                         <td class="tx_status">
                             <t t-if="invoice.state == 'open'">
                                 <span class="badge badge-pill badge-info"><i class="fa fa-fw fa-clock-o" aria-label="Opened" title="Opened" role="img"></i><span class="d-none d-md-inline"> Waiting for Payment</span></span>
@@ -64,7 +64,7 @@
                                 <span class="badge badge-pill badge-warning"><i class="fa fa-fw fa-remove" aria-label="Cancelled" title="Cancelled" role="img"></i><span class="d-none d-md-inline"> Cancelled</span></span>
                             </t>
                         </td>
-                        <td class="text-right"><span t-esc="-invoice.residual if invoice.type == 'out_refund' else invoice.residual" t-options='{"widget": "monetary", "display_currency": invoice.currency_id}'/></td>
+                        <td class="text-right"><span t-esc="-invoice.amount_residual if invoice.type == 'out_refund' else invoice.amount_residual" t-options='{"widget": "monetary", "display_currency": invoice.currency_id}'/></td>
                     </tr>
                 </t>
             </tbody>
@@ -76,7 +76,7 @@
         <xpath expr="//div[hasclass('o_portal_sidebar')]" position="inside">
             <t t-set="o_portal_fullwidth_alert" groups="sales_team.group_sale_salesman">
                 <t t-call="portal.portal_back_in_edit_mode">
-                    <t t-set="backend_url" t-value="'/web#return_label=Website&amp;model=%s&amp;id=%s&amp;action=%s&amp;view_type=form' % (invoice._name, invoice.id, invoice.env.ref('account.action_invoice_tree1').id)"/>
+                    <t t-set="backend_url" t-value="'/web#return_label=Website&amp;model=%s&amp;id=%s&amp;action=%s&amp;view_type=form' % (invoice._name, invoice.id, invoice.env.ref('account.action_move_out_invoice_type').id)"/>
                 </t>
             </t>
 
@@ -86,10 +86,10 @@
                     <t t-set="classes" t-value="'col-lg-auto d-print-none'"/>
                     <t t-set="title">
                         <h2 class="mb-0">
-                            <b t-if="invoice.residual > 0" t-field="invoice.residual"/>
+                            <b t-if="invoice.amount_residual > 0" t-field="invoice.amount_residual"/>
                             <b t-else="1" t-field="invoice.amount_total"/>
                         </h2>
-                        <div class="small" t-if="invoice.state != 'paid' and invoice.type == 'out_invoice'"><i class="fa fa-clock-o"/><span class="o_portal_sidebar_timeago ml4" t-att-datetime="invoice.date_due"/></div>
+                        <div class="small" t-if="invoice.state != 'paid' and invoice.type == 'out_invoice'"><i class="fa fa-clock-o"/><span class="o_portal_sidebar_timeago ml4" t-att-datetime="invoice.invoice_date_due"/></div>
                     </t>
 
                     <t t-set="entries">
@@ -104,15 +104,15 @@
                                     </div>
                                 </div>
                             </li>
-                            <li t-if="invoice.user_id" class="list-group-item flex-grow-1">
+                            <li t-if="invoice.invoice_user_id" class="list-group-item flex-grow-1">
                                 <div class="small mb-1"><strong class="text-muted">Salesperson</strong></div>
                                 <div class="row">
                                     <div class="col flex-grow-0 pr-2">
-                                        <img t-if="invoice.user_id.image" class="rounded-circle mt-1 o_portal_contact_img" t-att-src="image_data_uri(invoice.user_id.image)" alt="Contact"/>
+                                        <img t-if="invoice.invoice_user_id.image" class="rounded-circle mt-1 o_portal_contact_img" t-att-src="image_data_uri(invoice.invoice_user_id.image)" alt="Contact"/>
                                         <img t-else="" class="rounded-circle mt-1 o_portal_contact_img" src="/web/static/src/img/user_menu_avatar.png" alt="Contact"/>
                                     </div>
                                     <div class="col pl-0">
-                                        <span t-field="invoice.user_id" t-options='{"widget": "contact", "fields": ["name", "phone"], "no_marker": True}'/>
+                                        <span t-field="invoice.invoice_user_id" t-options='{"widget": "contact", "fields": ["name", "phone"], "no_marker": True}'/>
                                         <a data-toggle="modal" href="#" data-target="#invoice_chatter" class="small"><i class="fa fa-fw fa-comments"/><b>Send message</b></a>
                                     </div>
                                 </div>

addons/gamification_sale_crm/data/gamification_sale_crm_data.xml
@@ -9,9 +9,9 @@
             <field name="computation_mode">sum</field>
             <field name="monetary">True</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
-            <field name="field_id" eval="ref('account.field_account_invoice_report__price_total')" />
-            <field name="field_date_id" eval="ref('account.field_account_invoice_report__date')" />
-            <field name="domain">[('state','!=','cancel'),('user_id','=',user.id),('type','=','out_invoice')]</field>
+            <field name="field_id" eval="ref('account.field_account_invoice_report__price_subtotal')" />
+            <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
+            <field name="domain">[('state','!=','cancel'),('invoice_user_id','=',user.id),('type','=','out_invoice')]</field>
         </record>
 
         <record model="gamification.goal.definition" id="definition_crm_nbr_new_leads">
@@ -35,7 +35,7 @@
             <field name="field_id" eval="ref('crm.field_crm_lead__day_close')" />
             <field name="field_date_id" eval="ref('crm.field_crm_lead__date_closed')" />
             <field name="domain">[('user_id','=',user.id),('type', '=', 'lead')]</field>
-        </record> 
+        </record>
 
         <record model="gamification.goal.definition" id="definition_crm_lead_delay_close">
             <field name="name">Days to Close a Deal</field>
@@ -47,7 +47,7 @@
             <field name="field_id" eval="ref('crm.field_crm_lead__day_open')" />
             <field name="field_date_id" eval="ref('crm.field_crm_lead__date_open')" />
             <field name="domain">[('user_id','=',user.id)]</field>
-        </record> 
+        </record>
 
 
         <record model="gamification.goal.definition" id="definition_crm_nbr_new_opportunities">
@@ -76,8 +76,8 @@
             <field name="computation_mode">count</field>
             <field name="suffix">orders</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
-            <field name="field_date_id" eval="ref('account.field_account_invoice_report__date')" />
-            <field name="domain">[('state','=','paid'),('user_id','=',user.id),('type','=','out_invoice')]</field>
+            <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
+            <field name="domain">[('state','=','paid'),('invoice_user_id','=',user.id),('type','=','out_invoice')]</field>
         </record>
         <record model="gamification.goal.definition" id="definition_crm_tot_paid_sale_order">
             <field name="name">Total Paid Sales Orders</field>
@@ -85,9 +85,9 @@
             <field name="computation_mode">count</field>
             <field name="monetary">True</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
-            <field name="field_id" eval="ref('account.field_account_invoice_report__price_total')" />
-            <field name="field_date_id" eval="ref('account.field_account_invoice_report__date')" />
-            <field name="domain">[('state','=','paid'),('user_id','=',user.id),('type','=','out_invoice')]</field>
+            <field name="field_id" eval="ref('account.field_account_invoice_report__price_subtotal')" />
+            <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
+            <field name="domain">[('state','=','paid'),('invoice_user_id','=',user.id),('type','=','out_invoice')]</field>
         </record>
 
 
@@ -98,8 +98,8 @@
             <field name="condition">lower</field>
             <field name="suffix">invoices</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
-            <field name="field_date_id" eval="ref('account.field_account_invoice_report__date')" />
-            <field name="domain">[('state','!=','cancel'),('user_id','=',user.id),('type','=','out_refund')]</field>
+            <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
+            <field name="domain">[('state','!=','cancel'),('invoice_user_id','=',user.id),('type','=','out_refund')]</field>
         </record>
         <record model="gamification.goal.definition" id="definition_crm_tot_customer_refunds">
             <field name="name">Total Customer Credit Notes</field>
@@ -108,9 +108,9 @@
             <field name="condition">higher</field>
             <field name="monetary">True</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
-            <field name="field_id" eval="ref('account.field_account_invoice_report__price_total')" />
-            <field name="field_date_id" eval="ref('account.field_account_invoice_report__date')" />
-            <field name="domain">[('state','!=','cancel'),('user_id','=',user.id),('type','=','out_refund')]</field>
+            <field name="field_id" eval="ref('account.field_account_invoice_report__price_subtotal')" />
+            <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
+            <field name="domain">[('state','!=','cancel'),('invoice_user_id','=',user.id),('type','=','out_refund')]</field>
         </record>
 
 

addons/l10n_in/views/report_invoice.xml
@@ -9,15 +9,15 @@
                 <t t-set="tag_sgst_id" t-value="env.ref('l10n_in.sgst_tag_tax').id"/>
                 <t t-set="tag_igst_id" t-value="env.ref('l10n_in.igst_tag_tax').id"/>
                 <t t-set="tag_cess_id" t-value="env.ref('l10n_in.cess_tag_tax').id"/>
-                <t t-set="tax_map" t-value="o.tax_line_ids.mapped('tax_id.tag_ids.id')"/>
+                <t t-set="tax_map" t-value="o.invoice_line_ids.mapped('tax_ids.tag_ids.id')"/>
                 <t t-set="is_igst" t-value="tag_igst_id in tax_map"/>
                 <t t-set="is_cgst" t-value="tag_cgst_id in tax_map"/>
                 <t t-set="is_cess" t-value="tag_cess_id in tax_map"/>
-                <t t-set="tax_datas" t-value="o._invoice_line_tax_values()"/>
+                <t t-set="tax_datas" t-value="o.line_ids.filtered('tax_line_id')"/>
             </t>
         </xpath>
 
-        <xpath expr="//p[@t-if='o.comment']" position="before">
+        <xpath expr="//p[@t-if='o.narration']" position="before">
             <t t-if="o.company_id.country_id.code == 'IN'">
                 <p id="total_in_words" class="mb16">
                     <strong>Total (In Words): </strong>
@@ -82,7 +82,7 @@
                 <span t-if="o.type == 'out_refund'">Credit Note</span>
                 <span t-if="o.type == 'in_refund'">Vendor Credit Note</span>
                 <span t-if="o.type == 'in_invoice'">Vendor Bill</span>
-                <span t-field="o.number"/>
+                <span t-field="o.name"/>
             </h2>
         </xpath>
 

commit 1f2928a530a136b80d9513a89a0a7d302e5db0b6

[FIX] account.invoice: leftovers of the state / invoice_payment_state refactoring
this fixes bugs introduced by
https://github.com/odoo/odoo/commit/beaa30a3d1843de43a45f419bfbc1bfa7613a920

Signed-off-by: Csaba Tóth <csaba.toth@i3rendszerhaz.hu>

addons/account/models/account.py
@@ -980,7 +980,7 @@ class AccountJournal(models.Model):
             draft_moves = self.env['account.move'].search([('journal_id', 'in', self.ids), ('state', '=', 'draft')])
             pending_payments = draft_moves.mapped('line_ids.payment_id')
             pending_payments.mapped('move_line_ids.move_id').post()
-            pending_payments.mapped('reconciled_invoice_ids').filtered(lambda x: x.state == 'in_payment').write({'state': 'paid'})
+            pending_payments.mapped('reconciled_invoice_ids').filtered(lambda x: x.invoice_payment_state == 'in_payment').write({'invoice_payment_state': 'paid'})
         for record in self:
             if record.restrict_mode_hash_table and not record.secure_sequence_id:
                 record._create_secure_sequence(['secure_sequence_id'])
@@ -1357,9 +1357,9 @@ class AccountTax(models.Model):
             JOIN account_tax tax ON tax.id = line.tax_line_id
             WHERE line.tax_line_id IN %s
             AND line.company_id != tax.company_id
-            
+
             UNION ALL
-            
+
             SELECT line.id
             FROM account_move_line_account_tax_rel tax_rel
             JOIN account_tax tax ON tax.id = tax_rel.account_tax_id

addons/account/views/account_portal_templates.xml
@@ -6,7 +6,7 @@
                 <t t-else="">Invoices &amp; Bills</t>
             </li>
             <li t-if="invoice" class="breadcrumb-item active">
-                <t t-esc="invoice.name" t-if="invoice.name"/>
+                <t t-esc="invoice.name" t-if="invoice.name != '/'"/>
                 <t t-else=""><em>Draft Invoice</em></t>
             </li>
         </xpath>
@@ -47,17 +47,17 @@
                     <tr>
                         <td>
                             <a t-att-href="invoice.get_portal_url()" t-att-title="invoice.name">
-                                <t t-esc="invoice.name" t-if="invoice.name"/>
+                                <t t-esc="invoice.name" t-if="invoice.name != '/'"/>
                                 <em t-else="">Draft Invoice</em>
                             </a>
                         </td>
                         <td><span t-field="invoice.invoice_date"/></td>
                         <td class='d-none d-md-table-cell'><span t-field="invoice.invoice_date_due"/></td>
                         <td class="tx_status">
-                            <t t-if="invoice.state == 'open'">
+                            <t t-if="invoice.state == 'posted' and invoice.invoice_payment_state != 'paid'">
                                 <span class="badge badge-pill badge-info"><i class="fa fa-fw fa-clock-o" aria-label="Opened" title="Opened" role="img"></i><span class="d-none d-md-inline"> Waiting for Payment</span></span>
                             </t>
-                            <t t-if="invoice.state == 'paid'">
+                            <t t-if="invoice.state == 'posted' and invoice.invoice_payment_state == 'paid'">
                                 <span class="badge badge-pill badge-success"><i class="fa fa-fw fa-check" aria-label="Paid" title="Paid" role="img"></i><span class="d-none d-md-inline"> Paid</span></span>
                             </t>
                             <t t-if="invoice.state == 'cancel'">
@@ -89,7 +89,7 @@
                             <b t-if="invoice.amount_residual > 0" t-field="invoice.amount_residual"/>
                             <b t-else="1" t-field="invoice.amount_total"/>
                         </h2>
-                        <div class="small" t-if="invoice.state != 'paid' and invoice.type == 'out_invoice'"><i class="fa fa-clock-o"/><span class="o_portal_sidebar_timeago ml4" t-att-datetime="invoice.invoice_date_due"/></div>
+                        <div class="small" t-if="invoice.invoice_payment_state != 'paid' and invoice.type == 'out_invoice'"><i class="fa fa-clock-o"/><span class="o_portal_sidebar_timeago ml4" t-att-datetime="invoice.invoice_date_due"/></div>
                     </t>
 
                     <t t-set="entries">

addons/gamification_sale_crm/data/gamification_sale_crm_data.xml
@@ -77,7 +77,7 @@
             <field name="suffix">orders</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
             <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
-            <field name="domain">[('state','=','paid'),('invoice_user_id','=',user.id),('type','=','out_invoice')]</field>
+            <field name="domain">[('invoice_payment_state','=','paid'),('invoice_user_id','=',user.id),('type','=','out_invoice')]</field>
         </record>
         <record model="gamification.goal.definition" id="definition_crm_tot_paid_sale_order">
             <field name="name">Total Paid Sales Orders</field>
@@ -87,7 +87,7 @@
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
             <field name="field_id" eval="ref('account.field_account_invoice_report__price_subtotal')" />
             <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
-            <field name="domain">[('state','=','paid'),('invoice_user_id','=',user.id),('type','=','out_invoice')]</field>
+            <field name="domain">[('invoice_payment_state','=','paid'),('invoice_user_id','=',user.id),('type','=','out_invoice')]</field>
         </record>
 
 

addons/l10n_in/views/report_invoice.xml
@@ -26,7 +26,7 @@
 
         <xpath expr="//h2" position="replace">
             <h2>
-                <span t-if="o.type == 'out_invoice' and o.state in ('open', 'in_payment', 'paid')" t-field="o.journal_id.name"/>
+                <span t-if="o.type == 'out_invoice' and o.state == 'open'" t-field="o.journal_id.name"/>
                 <span t-if="o.type == 'out_invoice' and o.state == 'draft'">Draft <span t-field="o.journal_id.name"/></span>
                 <span t-if="o.type == 'out_invoice' and o.state == 'cancel'">Cancelled <span t-field="o.journal_id.name"/></span>
                 <span t-if="o.type == 'out_refund'">Credit Note</span>

addons/website_crm_partner_assign/report/crm_partner_report.py
@@ -54,5 +54,5 @@ class CrmPartnerReportAssign(models.Model):
                 FROM
                     res_partner p
                     left join account_invoice_report i
-                        on (i.partner_id=p.id and i.type in ('out_invoice','out_refund') and i.state in ('open','in_payment','paid'))
+                        on (i.partner_id=p.id and i.type in ('out_invoice','out_refund') and i.state='open')
             )""")