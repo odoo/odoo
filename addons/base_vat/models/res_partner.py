c3379312d9c887780b7e62fa2079e1e42d2bf89e
alt-odoo <alt@odoo.com>
2020-04-10 14:13:03 +0000

    [FIX] base_vat: no vat check if multiple countries
    We should only check the consistency between the vat number and country code in
    case we have one country involved in the recordset.
    
    closes odoo/odoo#49530
    
    X-original-commit: 23771ef7fcadfc0022d278c2e3436a7befc9e5b8
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/base_vat/models/res_partner.py
+++ addons/base_vat/models/res_partner.py
@@ -489,7 +489,7 @@ class ResPartner(models.Model):
         return super(ResPartner, self).create(values)
 
     def write(self, values):
-        if values.get('vat'):
+        if values.get('vat') and len(self.mapped('country_id')) == 1:
             country_id = values.get('country_id', self.country_id.id)
             values['vat'] = self._fix_vat_number(values['vat'], country_id)
         return super(ResPartner, self).write(values)
