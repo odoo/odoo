odoo/odoo

commit 5019ec001d6e4f3423b74e7e365b85fcf5f76a24

[IMP/REF] accounting-pocalypse yeaaahh
This commit merges the following models
 * account.invoice and account.move
 * account.invoice.line and account.move.line
 * account.voucher and account.move
 * account.voucher.line and account.move.line

It was the opportunity for a big cleanup of the code, so it also restructures the whole account module, its different models/fields, the tests etc. for a better world and a better code readability.

==== Rationale ====
The rationale of this huge change is that we want journal entries / invoices to be easily edited, and changes reflected in the other model. It's a HUGE feature and very strategic for the fiduciary companies. For example, changing the account of a journal entry needs to be automatically reflected on the related invoice.

The same reasoning applies to sale/purchase vouchers.

==== Changes made in features =====
When creating an invoice, you are now creating a journal entry directly.
--> The object account.invoice no longer exists.
In the same fashion when creating an invoice line, you're now adding journal items directly in the journal entry representing the invoice. If this invoice line has some tax, it may create additional journal items as well.
--> The models account.invoice.line & account.invoice.tax no longer exist

Identically, when creating a sale/purchase receipt with its lines, you are now creating a journal entry directly and there's no more usability difference between encoding a receipt or an invoice.
--> The object account.voucher no longer exists.
--> The object account.voucher.line no longer exists.
--> The whole account_voucher module no longer exists.

Positive side-effects coming from these changes are
* draft invoices/bills/sale or purchase receipts now create a draft accounting entry. Validate these objects now simply post its journal entry. That means that draft invoices/bills/sale or purchase receipt can straightforwardly be included in reporting or budgets.

addons/account/views/account_portal_templates.xml
@@ -6,7 +6,7 @@
                 <t t-else="">Invoices &amp; Bills</t>
             </li>
             <li t-if="invoice" class="breadcrumb-item active">
-                <t t-esc="invoice.number" t-if="invoice.number"/>
+                <t t-esc="invoice.name" t-if="invoice.name"/>
                 <t t-else=""><em>Draft Invoice</em></t>
             </li>
         </xpath>
@@ -46,13 +46,13 @@
                 <t t-foreach="invoices" t-as="invoice">
                     <tr>
                         <td>
-                            <a t-att-href="invoice.get_portal_url()" t-att-title="invoice.number">
-                                <t t-esc="invoice.number" t-if="invoice.number"/>
+                            <a t-att-href="invoice.get_portal_url()" t-att-title="invoice.name">
+                                <t t-esc="invoice.name" t-if="invoice.name"/>
                                 <em t-else="">Draft Invoice</em>
                             </a>
                         </td>
-                        <td><span t-field="invoice.date_invoice"/></td>
-                        <td class='d-none d-md-table-cell'><span t-field="invoice.date_due"/></td>
+                        <td><span t-field="invoice.invoice_date"/></td>
+                        <td class='d-none d-md-table-cell'><span t-field="invoice.invoice_date_due"/></td>
                         <td class="tx_status">
                             <t t-if="invoice.state == 'open'">
                                 <span class="badge badge-pill badge-info"><i class="fa fa-fw fa-clock-o" aria-label="Opened" title="Opened" role="img"></i><span class="d-none d-md-inline"> Waiting for Payment</span></span>
@@ -64,7 +64,7 @@
                                 <span class="badge badge-pill badge-warning"><i class="fa fa-fw fa-remove" aria-label="Cancelled" title="Cancelled" role="img"></i><span class="d-none d-md-inline"> Cancelled</span></span>
                             </t>
                         </td>
-                        <td class="text-right"><span t-esc="-invoice.residual if invoice.type == 'out_refund' else invoice.residual" t-options='{"widget": "monetary", "display_currency": invoice.currency_id}'/></td>
+                        <td class="text-right"><span t-esc="-invoice.amount_residual if invoice.type == 'out_refund' else invoice.amount_residual" t-options='{"widget": "monetary", "display_currency": invoice.currency_id}'/></td>
                     </tr>
                 </t>
             </tbody>
@@ -76,7 +76,7 @@
         <xpath expr="//div[hasclass('o_portal_sidebar')]" position="inside">
             <t t-set="o_portal_fullwidth_alert" groups="sales_team.group_sale_salesman">
                 <t t-call="portal.portal_back_in_edit_mode">
-                    <t t-set="backend_url" t-value="'/web#return_label=Website&amp;model=%s&amp;id=%s&amp;action=%s&amp;view_type=form' % (invoice._name, invoice.id, invoice.env.ref('account.action_invoice_tree1').id)"/>
+                    <t t-set="backend_url" t-value="'/web#return_label=Website&amp;model=%s&amp;id=%s&amp;action=%s&amp;view_type=form' % (invoice._name, invoice.id, invoice.env.ref('account.action_move_out_invoice_type').id)"/>
                 </t>
             </t>
 
@@ -86,10 +86,10 @@
                     <t t-set="classes" t-value="'col-lg-auto d-print-none'"/>
                     <t t-set="title">
                         <h2 class="mb-0">
-                            <b t-if="invoice.residual > 0" t-field="invoice.residual"/>
+                            <b t-if="invoice.amount_residual > 0" t-field="invoice.amount_residual"/>
                             <b t-else="1" t-field="invoice.amount_total"/>
                         </h2>
-                        <div class="small" t-if="invoice.state != 'paid' and invoice.type == 'out_invoice'"><i class="fa fa-clock-o"/><span class="o_portal_sidebar_timeago ml4" t-att-datetime="invoice.date_due"/></div>
+                        <div class="small" t-if="invoice.state != 'paid' and invoice.type == 'out_invoice'"><i class="fa fa-clock-o"/><span class="o_portal_sidebar_timeago ml4" t-att-datetime="invoice.invoice_date_due"/></div>
                     </t>
 
                     <t t-set="entries">
@@ -104,15 +104,15 @@
                                     </div>
                                 </div>
                             </li>
-                            <li t-if="invoice.user_id" class="list-group-item flex-grow-1">
+                            <li t-if="invoice.invoice_user_id" class="list-group-item flex-grow-1">
                                 <div class="small mb-1"><strong class="text-muted">Salesperson</strong></div>
                                 <div class="row">
                                     <div class="col flex-grow-0 pr-2">
-                                        <img t-if="invoice.user_id.image" class="rounded-circle mt-1 o_portal_contact_img" t-att-src="image_data_uri(invoice.user_id.image)" alt="Contact"/>
+                                        <img t-if="invoice.invoice_user_id.image" class="rounded-circle mt-1 o_portal_contact_img" t-att-src="image_data_uri(invoice.invoice_user_id.image)" alt="Contact"/>
                                         <img t-else="" class="rounded-circle mt-1 o_portal_contact_img" src="/web/static/src/img/user_menu_avatar.png" alt="Contact"/>
                                     </div>
                                     <div class="col pl-0">
-                                        <span t-field="invoice.user_id" t-options='{"widget": "contact", "fields": ["name", "phone"], "no_marker": True}'/>
+                                        <span t-field="invoice.invoice_user_id" t-options='{"widget": "contact", "fields": ["name", "phone"], "no_marker": True}'/>
                                         <a data-toggle="modal" href="#" data-target="#invoice_chatter" class="small"><i class="fa fa-fw fa-comments"/><b>Send message</b></a>
                                     </div>
                                 </div>

addons/gamification_sale_crm/data/gamification_sale_crm_data.xml
@@ -9,9 +9,9 @@
             <field name="computation_mode">sum</field>
             <field name="monetary">True</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
-            <field name="field_id" eval="ref('account.field_account_invoice_report__price_total')" />
-            <field name="field_date_id" eval="ref('account.field_account_invoice_report__date')" />
-            <field name="domain">[('state','!=','cancel'),('user_id','=',user.id),('type','=','out_invoice')]</field>
+            <field name="field_id" eval="ref('account.field_account_invoice_report__price_subtotal')" />
+            <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
+            <field name="domain">[('state','!=','cancel'),('invoice_user_id','=',user.id),('type','=','out_invoice')]</field>
         </record>
 
         <record model="gamification.goal.definition" id="definition_crm_nbr_new_leads">
@@ -35,7 +35,7 @@
             <field name="field_id" eval="ref('crm.field_crm_lead__day_close')" />
             <field name="field_date_id" eval="ref('crm.field_crm_lead__date_closed')" />
             <field name="domain">[('user_id','=',user.id),('type', '=', 'lead')]</field>
-        </record> 
+        </record>
 
         <record model="gamification.goal.definition" id="definition_crm_lead_delay_close">
             <field name="name">Days to Close a Deal</field>
@@ -47,7 +47,7 @@
             <field name="field_id" eval="ref('crm.field_crm_lead__day_open')" />
             <field name="field_date_id" eval="ref('crm.field_crm_lead__date_open')" />
             <field name="domain">[('user_id','=',user.id)]</field>
-        </record> 
+        </record>
 
 
         <record model="gamification.goal.definition" id="definition_crm_nbr_new_opportunities">
@@ -76,8 +76,8 @@
             <field name="computation_mode">count</field>
             <field name="suffix">orders</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
-            <field name="field_date_id" eval="ref('account.field_account_invoice_report__date')" />
-            <field name="domain">[('state','=','paid'),('user_id','=',user.id),('type','=','out_invoice')]</field>
+            <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
+            <field name="domain">[('state','=','paid'),('invoice_user_id','=',user.id),('type','=','out_invoice')]</field>
         </record>
         <record model="gamification.goal.definition" id="definition_crm_tot_paid_sale_order">
             <field name="name">Total Paid Sales Orders</field>
@@ -85,9 +85,9 @@
             <field name="computation_mode">count</field>
             <field name="monetary">True</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
-            <field name="field_id" eval="ref('account.field_account_invoice_report__price_total')" />
-            <field name="field_date_id" eval="ref('account.field_account_invoice_report__date')" />
-            <field name="domain">[('state','=','paid'),('user_id','=',user.id),('type','=','out_invoice')]</field>
+            <field name="field_id" eval="ref('account.field_account_invoice_report__price_subtotal')" />
+            <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
+            <field name="domain">[('state','=','paid'),('invoice_user_id','=',user.id),('type','=','out_invoice')]</field>
         </record>
 
 
@@ -98,8 +98,8 @@
             <field name="condition">lower</field>
             <field name="suffix">invoices</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
-            <field name="field_date_id" eval="ref('account.field_account_invoice_report__date')" />
-            <field name="domain">[('state','!=','cancel'),('user_id','=',user.id),('type','=','out_refund')]</field>
+            <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
+            <field name="domain">[('state','!=','cancel'),('invoice_user_id','=',user.id),('type','=','out_refund')]</field>
         </record>
         <record model="gamification.goal.definition" id="definition_crm_tot_customer_refunds">
             <field name="name">Total Customer Credit Notes</field>
@@ -108,9 +108,9 @@
             <field name="condition">higher</field>
             <field name="monetary">True</field>
             <field name="model_id" eval="ref('account.model_account_invoice_report')" />
-            <field name="field_id" eval="ref('account.field_account_invoice_report__price_total')" />
-            <field name="field_date_id" eval="ref('account.field_account_invoice_report__date')" />
-            <field name="domain">[('state','!=','cancel'),('user_id','=',user.id),('type','=','out_refund')]</field>
+            <field name="field_id" eval="ref('account.field_account_invoice_report__price_subtotal')" />
+            <field name="field_date_id" eval="ref('account.field_account_invoice_report__invoice_date')" />
+            <field name="domain">[('state','!=','cancel'),('invoice_user_id','=',user.id),('type','=','out_refund')]</field>
         </record>
 
 

addons/l10n_in/views/report_invoice.xml
@@ -9,15 +9,15 @@
                 <t t-set="tag_sgst_id" t-value="env.ref('l10n_in.sgst_tag_tax').id"/>
                 <t t-set="tag_igst_id" t-value="env.ref('l10n_in.igst_tag_tax').id"/>
                 <t t-set="tag_cess_id" t-value="env.ref('l10n_in.cess_tag_tax').id"/>
-                <t t-set="tax_map" t-value="o.tax_line_ids.mapped('tax_id.tag_ids.id')"/>
+                <t t-set="tax_map" t-value="o.invoice_line_ids.mapped('tax_ids.tag_ids.id')"/>
                 <t t-set="is_igst" t-value="tag_igst_id in tax_map"/>
                 <t t-set="is_cgst" t-value="tag_cgst_id in tax_map"/>
                 <t t-set="is_cess" t-value="tag_cess_id in tax_map"/>
-                <t t-set="tax_datas" t-value="o._invoice_line_tax_values()"/>
+                <t t-set="tax_datas" t-value="o.line_ids.filtered('tax_line_id')"/>
             </t>
         </xpath>
 
-        <xpath expr="//p[@t-if='o.comment']" position="before">
+        <xpath expr="//p[@t-if='o.narration']" position="before">
             <t t-if="o.company_id.country_id.code == 'IN'">
                 <p id="total_in_words" class="mb16">
                     <strong>Total (In Words): </strong>
@@ -82,7 +82,7 @@
                 <span t-if="o.type == 'out_refund'">Credit Note</span>
                 <span t-if="o.type == 'in_refund'">Vendor Credit Note</span>
                 <span t-if="o.type == 'in_invoice'">Vendor Bill</span>
-                <span t-field="o.number"/>
+                <span t-field="o.name"/>
             </h2>
         </xpath>
 

addons/website_crm_partner_assign/report/crm_partner_report.py
@@ -24,7 +24,7 @@ class CrmPartnerReportAssign(models.Model):
     date = fields.Date('Invoice Account Date', readonly=True)
 
     _depends = {
-        'account.invoice.report': ['date', 'partner_id', 'price_total', 'state', 'type'],
+        'account.invoice.report': ['invoice_date', 'partner_id', 'price_subtotal', 'state', 'type'],
         'crm.lead': ['partner_assigned_id'],
         'res.partner': ['activation', 'country_id', 'date_partnership', 'date_review',
                         'grade_id', 'parent_id', 'team_id', 'user_id'],
@@ -50,8 +50,8 @@ class CrmPartnerReportAssign(models.Model):
                     p.user_id,
                     p.team_id,
                     (SELECT count(id) FROM crm_lead WHERE partner_assigned_id=p.id) AS nbr_opportunities,
-                    i.price_total as turnover,
-                    i.date
+                    i.price_subtotal as turnover,
+                    i.invoice_date
                 FROM
                     res_partner p
                     left join account_invoice_report i

commit 9f6f0403b7edf343918dc58558cf1a77803d53c1

[FIX] account.invoice: leftovers of the state / invoice_payment_state refactoring
this fixes bugs introduced by
https://github.com/odoo/odoo/commit/5019ec001d6e4f3423b74e7e365b85fcf5f76a24

in saas-13.2, the field invoice_payment_state was renamed into payment_state by
https://github.com/odoo/odoo/commit/1b3185b008fede2df5ca85dc4442e1bd0ff5efa7

in saas-13.4, the feature post_at of journal have been removed by
https://github.com/odoo/odoo/commit/ffc0a22fd034f0555b94fd6528444bdc92b68e84

closes odoo/odoo#49569

X-original-commit: 2001c413ee142c50ca6549cd5dff9fc0f9f1637b
Signed-off-by: Csaba Tóth <csaba.toth@i3rendszerhaz.hu>
Signed-off-by: Quentin De Paoli (qdp) <qdp@openerp.com>

addons/account/views/account_portal_templates.xml
@@ -6,7 +6,7 @@
                 <t t-else="">Invoices &amp; Bills</t>
             </li>
             <li t-if="invoice" class="breadcrumb-item active">
-                <t t-esc="invoice.name" t-if="invoice.name"/>
+                <t t-esc="invoice.name" t-if="invoice.name != '/'"/>
                 <t t-else=""><em>Draft Invoice</em></t>
             </li>
         </xpath>
@@ -47,17 +47,17 @@
                     <tr>
                         <td>
                             <a t-att-href="invoice.get_portal_url()" t-att-title="invoice.name">
-                                <t t-esc="invoice.name" t-if="invoice.name"/>
+                                <t t-esc="invoice.name" t-if="invoice.name != '/'"/>
                                 <em t-else="">Draft Invoice</em>
                             </a>
                         </td>
                         <td><span t-field="invoice.invoice_date"/></td>
                         <td class='d-none d-md-table-cell'><span t-field="invoice.invoice_date_due"/></td>
                         <td class="tx_status">
-                            <t t-if="invoice.state == 'open'">
+                            <t t-if="invoice.state == 'posted' and invoice.payment_state != 'paid'">
                                 <span class="badge badge-pill badge-info"><i class="fa fa-fw fa-clock-o" aria-label="Opened" title="Opened" role="img"></i><span class="d-none d-md-inline"> Waiting for Payment</span></span>
                             </t>
-                            <t t-if="invoice.state == 'paid'">
+                            <t t-if="invoice.state == 'posted' and invoice.payment_state == 'paid'">
                                 <span class="badge badge-pill badge-success"><i class="fa fa-fw fa-check" aria-label="Paid" title="Paid" role="img"></i><span class="d-none d-md-inline"> Paid</span></span>
                             </t>
                             <t t-if="invoice.state == 'cancel'">
@@ -89,7 +89,7 @@
                             <b t-if="invoice.amount_residual > 0" t-field="invoice.amount_residual"/>
                             <b t-else="1" t-field="invoice.amount_total"/>
                         </h2>
-                        <div class="small" t-if="invoice.state != 'paid' and invoice.move_type == 'out_invoice'"><i class="fa fa-clock-o"/><span class="o_portal_sidebar_timeago ml4" t-att-datetime="invoice.invoice_date_due"/></div>
+                        <div class="small" t-if="invoice.payment_state != 'paid' and invoice.move_type == 'out_invoice'"><i class="fa fa-clock-o"/><span class="o_portal_sidebar_timeago ml4" t-att-datetime="invoice.invoice_date_due"/></div>
                     </t>
 
                     <t t-set="entries">

addons/gamification_sale_crm/data/gamification_sale_crm_data.xml
@@ -76,7 +76,7 @@
             <field name="suffix">orders</field>
             <field name="model_id" ref="account.model_account_invoice_report"/>
             <field name="field_date_id" ref="account.field_account_invoice_report__invoice_date"/>
-            <field name="domain">[('state','=','paid'),('invoice_user_id','=',user.id),('move_type','=','out_invoice')]</field>
+            <field name="domain">[('payment_state','=','paid'),('invoice_user_id','=',user.id),('move_type','=','out_invoice')]</field>
         </record>
         <record model="gamification.goal.definition" id="definition_crm_tot_paid_sale_order">
             <field name="name">Total Paid Sales Orders</field>
@@ -86,7 +86,7 @@
             <field name="model_id" ref="account.model_account_invoice_report"/>
             <field name="field_id" ref="account.field_account_invoice_report__price_subtotal"/>
             <field name="field_date_id" ref="account.field_account_invoice_report__invoice_date"/>
-            <field name="domain">[('state','=','paid'),('invoice_user_id','=',user.id),('move_type','=','out_invoice')]</field>
+            <field name="domain">[('payment_state','=','paid'),('invoice_user_id','=',user.id),('move_type','=','out_invoice')]</field>
         </record>
 
 

addons/l10n_in/views/report_invoice.xml
@@ -26,7 +26,7 @@
 
         <xpath expr="//h2" position="replace">
             <h2>
-                <span t-if="o.move_type == 'out_invoice' and o.state in ('open', 'in_payment', 'paid')" t-field="o.journal_id.name"/>
+                <span t-if="o.move_type == 'out_invoice' and o.state == 'open'" t-field="o.journal_id.name"/>
                 <span t-if="o.move_type == 'out_invoice' and o.state == 'draft'">Draft <span t-field="o.journal_id.name"/></span>
                 <span t-if="o.move_type == 'out_invoice' and o.state == 'cancel'">Cancelled <span t-field="o.journal_id.name"/></span>
                 <span t-if="o.move_type == 'out_refund'">Credit Note</span>

addons/website_crm_partner_assign/report/crm_partner_report.py
@@ -54,5 +54,5 @@ class CrmPartnerReportAssign(models.Model):
                 FROM
                     res_partner p
                     left join account_invoice_report i
-                        on (i.partner_id=p.id and i.move_type in ('out_invoice','out_refund') and i.state in ('open','in_payment','paid'))
+                        on (i.partner_id=p.id and i.move_type in ('out_invoice','out_refund') and i.state='open')
             )""")