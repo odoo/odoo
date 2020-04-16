1a6b07748d858376c03900400b97cd02135e3b70
Patrick Hoste <pko@odoo.com>
2020-04-16 13:32:29 +0000

    [IMP] website_slides: publish content when publishing slide
    Purpose of this commit is to avoid de-synchronization where content could
    be unpublished whereas slide was published.
    
    Task ID 2239840
    
    closes odoo/odoo#49908
    
    X-original-commit: d0f4a3e85eab63e335766a823185fd6ada6e07b4
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_slides/models/__init__.py
+++ addons/website_slides/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import ir_http
 from . import gamification_challenge
 from . import slide_slide
 from . import slide_question
