06b6ca0f317869c5f68c7e7db5532b2a45cb92d2
jvm-odoo <jvm@odoo.com>
2020-04-17 06:53:08 +0000

    [FIX] board: fix creating wrong controllers
    Issue
    
    	- Install Dashboard & Sales
    	- Add a graph & a list to the dashboard
    	- Go in the dashboard
    
    	Rendering ok
    
    	- Click on a list item
    	- Go back with the breadcrumb
    
    	Traceback
    
    Cause
    
    	When going back with the breadcrumb
    	we retrieve the previous view controller.
    
    	https://github.com/odoo/odoo/blob/9400059073d7401789eb1a60d6a0ec6f58dfc66b/addons/web/static/src/js/chrome/action_manager_act_window.js#L457
    
    	By restoring the board controller, we
    	restore all the subcontrollers too.
    
    	But the actual board renderer is re-creating
    	controllers. This creates inconsistencies
    	with the view.
    
    Solution
    
    	Remove the restored controllers
    
    OPW-2232713
    
    closes odoo/odoo#49728
    
    X-original-commit: 4f3d818052d689da762d8afbf1789daaa3d39821
    Signed-off-by: Jason Van Malder (jvm) <jvm@odoo.com>

--- addons/board/static/src/js/board_view.js
+++ addons/board/static/src/js/board_view.js
@@ -341,6 +341,7 @@ var BoardRenderer = FormRenderer.extend({
         });
 
         var $html = $('<div>').append($(QWeb.render('DashBoard', {node: node, isMobile: config.device.isMobile})));
+        this._boardSubcontrollers = []; // dashboard controllers are reset on re-render
 
         // render each view
         _.each(this.actionsDescr, function (action) {
