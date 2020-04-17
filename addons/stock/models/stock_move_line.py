8352e2e414114bf4fc1434ab4fc6ae56537bce57
Tiffany Chang (tic) <tic@odoo.com>
2020-04-17 13:36:19 +0200

    [IMP] stock, delivery: aggregate delivery slip product lines
    This commit makes it so move_lines with the same product+description+uom
    are grouped together ONLY WHEN serial numbers/lots are not to be
    printed. This is the case when the setting "Display Lots & Serial
    Numbers on Delivery Slips" is not active or when there are no
    lots/serial numbers assigned in the picking. This is only applicable for
    stock.pickings that are 'state=Done'.
    
    In order to implement this, some clean up of lots/serial numbers printing
    was also done.
    
    This completes subsection 2 of overall Improve Delivery Slip task.
    Task: 2008609

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -605,3 +605,32 @@ class StockMoveLine(models.Model):
     def _should_bypass_reservation(self, location):
         self.ensure_one()
         return location.should_bypass_reservation() or self.product_id.type != 'product'
+
+    def _get_aggregated_product_quantities(self):
+        """ Returns a dictionary of products (key = id+name+description+uom) and corresponding values of interest.
+
+        Allows aggregation of data across separate move lines for the same product. This is expected to be useful
+        in things such as delivery reports. Dict key is made as a combination of values we expect to want to group
+        the products by (i.e. so data is not lost). This function purposely ignores lots/SNs because these are
+        expected to already be properly grouped by line.
+
+        returns: dictionary {product_id+name+description+uom: {product, name, description, qty_done, product_uom}, ...}
+        """
+        aggregated_move_lines = {}
+        for move_line in self:
+            name = move_line.product_id.display_name
+            description = move_line.move_id.description_picking
+            if description == name or description == move_line.product_id.name:
+                description = False
+            uom = move_line.product_uom_id
+            line_key = str(move_line.product_id.id) + "_" + name + (description or "") + "uom " + str(uom.id)
+
+            if line_key not in aggregated_move_lines:
+                aggregated_move_lines[line_key] = {'name': name,
+                                                   'description': description,
+                                                   'qty_done': move_line.qty_done,
+                                                   'product_uom': uom.name,
+                                                   'product': move_line.product_id}
+            else:
+                aggregated_move_lines[line_key]['qty_done'] += move_line.qty_done
+        return aggregated_move_lines
