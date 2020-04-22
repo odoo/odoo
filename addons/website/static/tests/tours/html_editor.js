52c23e01e3592126b4b4d6bde75e25594f203a99
DramixDw <rve@odoo.com>
2020-04-22 20:40:45 +0000

    [REM] website,*: remove About us page and references
    Remove the default about us page because it could be easily recreated and
    doesn't add a lot of value to the website.
    
    task-2189613

--- addons/website/static/tests/tours/html_editor.js
+++ addons/website/static/tests/tours/html_editor.js
@@ -5,7 +5,7 @@ var tour = require('web_tour.tour');
 
 tour.register('html_editor_multiple_templates', {
     test: true,
-    url: '/aboutus',
+    url: '/generic',
 },
     [
         // 1. Edit the page through Edit Mode, it will COW the view
@@ -24,7 +24,7 @@ tour.register('html_editor_multiple_templates', {
             extra_trigger: '#oe_structure_test_ui.o_dirty',
             trigger: "#web_editor-top-edit button[data-action=save]",
         },
-        // 2. Edit generic aboutus view
+        // 2. Edit generic view
         {
             content: "open customize menu",
             extra_trigger: "body:not(.editor_enable)",
@@ -35,8 +35,8 @@ tour.register('html_editor_multiple_templates', {
             trigger: '#html_editor',
         },
         {
-            content: "add something in the aboutus view",
-            trigger: 'div.ace_line .ace_xml:contains("aboutus")',
+            content: "add something in the generic view",
+            trigger: 'div.ace_line .ace_xml:contains("Generic")',
             run: function () {
                 ace.edit('ace-view-editor').getSession().insert({row: 3, column: 1}, '<p>somenewcontent</p>\n');
             },
@@ -52,7 +52,7 @@ tour.register('html_editor_multiple_templates', {
         },
         {
             content: "add something in the oe_structure specific view",
-            extra_trigger: '#ace-view-id:contains("website.aboutus_oe_structure_test_ui")', // If no xml_id it should show key
+            extra_trigger: '#ace-view-id:contains("test.generic_view_oe_structure_test_ui")', // If no xml_id it should show key
             trigger: 'div.ace_line .ace_xml:contains("s_cover")',
             run: function () {
                 ace.edit('ace-view-editor').getSession().insert({row: 2, column: 1}, '<p>anothernewcontent</p>\n');
@@ -74,7 +74,7 @@ tour.register('html_editor_multiple_templates', {
 
 tour.register('test_html_editor_scss', {
     test: true,
-    url: '/aboutus',
+    url: '/contactus',
 },
     [
         // 1. Open Html Editor and select a scss file
--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -61,24 +61,39 @@ class TestUiHtmlEditor(odoo.tests.HttpCase):
     def test_html_editor_multiple_templates(self):
         Website = self.env['website']
         View = self.env['ir.ui.view']
-        generic_aboutus = Website.viewref('website.aboutus')
+        Page = self.env['website.page']
+        
+        self.generic_view = View.create({
+            'name': 'Generic',
+            'type': 'qweb',
+            'arch': '''
+                <div>content</div>
+            ''',
+            'key': 'test.generic_view',
+        })
+
+        self.generic_page = Page.create({
+            'view_id': self.generic_view.id,
+            'url': '/generic',
+        })
+
+        generic_page = Website.viewref('test.generic_view')
         # Use an empty page layout with oe_structure id for this test
         oe_structure_layout = '''
-            <t name="About us" t-name="website.aboutus">
+            <t name="Generic" t-name="test.generic_view">
                 <t t-call="website.layout">
-                    <p>aboutus</p>
                     <div id="oe_structure_test_ui" class="oe_structure oe_empty"/>
                 </t>
             </t>
         '''
-        generic_aboutus.arch = oe_structure_layout
+        generic_page.arch = oe_structure_layout
         self.start_tour("/", 'html_editor_multiple_templates', login='admin')
-        self.assertEqual(View.search_count([('key', '=', 'website.aboutus')]), 2, "Aboutus view should have been COW'd")
-        self.assertTrue(generic_aboutus.arch == oe_structure_layout, "Generic Aboutus view should be untouched")
-        self.assertEqual(len(generic_aboutus.inherit_children_ids.filtered(lambda v: 'oe_structure' in v.name)), 0, "oe_structure view should have been deleted when aboutus was COW")
-        specific_aboutus = Website.with_context(website_id=1).viewref('website.aboutus')
-        self.assertTrue(specific_aboutus.arch != oe_structure_layout, "Specific Aboutus view should have been changed")
-        self.assertEqual(len(specific_aboutus.inherit_children_ids.filtered(lambda v: 'oe_structure' in v.name)), 1, "oe_structure view should have been created on the specific tree")
+        self.assertEqual(View.search_count([('key', '=', 'test.generic_view')]), 2, "homepage view should have been COW'd")
+        self.assertTrue(generic_page.arch == oe_structure_layout, "Generic homepage view should be untouched")
+        self.assertEqual(len(generic_page.inherit_children_ids.filtered(lambda v: 'oe_structure' in v.name)), 0, "oe_structure view should have been deleted when aboutus was COW")
+        specific_page = Website.with_context(website_id=1).viewref('test.generic_view')
+        self.assertTrue(specific_page.arch != oe_structure_layout, "Specific homepage view should have been changed")
+        self.assertEqual(len(specific_page.inherit_children_ids.filtered(lambda v: 'oe_structure' in v.name)), 1, "oe_structure view should have been created on the specific tree")
 
     def test_html_editor_scss(self):
         self.start_tour("/", 'test_html_editor_scss', login='admin')
