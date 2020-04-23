d57e50008141881dcf3bf7b40033767ac6a9655a
Mathieu Duckerts-Antoine <dam@odoo.com>
2020-04-23 10:03:54 +0000

    [FIX] web: keep server group order in pivot
    In the pivot view, groups comming from many read_group are assemble
    to create the header hierarchies and the table.
    
    Groups returned by the read_group may be already sorted by the server
    using the order set on the model for instance and that order should
    be reflected in the header hierarchies (and in the table).
    It is important to keep that order since it might not be possible
    to recreate it in the interface. Even if possible it might not be a
    good idea for performance reasons.
    
    Before this commit:
    The groups are added in a JS object, whose keys are the group ids
    (when grouping on a many2one at least). Then, to generate the table,
    we iterate over the keys of that object, but those keys are sorted
    according to the numerical order (not the insertion order),
    because keys are kind of numeric.
    See https://stackoverflow.com/questions/5525795/does-javascript-guarantee-object-property-order.
    
    After this commit:
    Instead of a JS object, we use a Map for which the insertion order
    is used (official spec).
    
    Task ID: 2243416
    
    closes odoo/odoo#50551
    
    X-original-commit: 1a9a85c8b8d0f47c3c6f3878cd6d9401ddc3ad75
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/pivot_tests.js
+++ addons/web/static/tests/views/pivot_tests.js
@@ -1393,9 +1393,9 @@ QUnit.module('Views', {
         await testUtils.dom.click(pivot.$('.o_pivot_field_menu .dropdown-item[data-field="customer"]'));
 
         values = [
-            "29", "2", "1", "32",
+            "29", "1", "2", "32",
             "12",           "12",
-            "17", "2", "1", "20",
+            "17", "1", "2", "20",
         ];
         assert.strictEqual(getCurrentValues(pivot), values.join(','));
 
@@ -1404,10 +1404,10 @@ QUnit.module('Views', {
         await testUtils.dom.click(pivot.$('.o_pivot_field_menu .dropdown-item[data-field="other_product_id"]'));
 
         values = [
-            "29", "2", "1", "32",
+            "29", "1", "2", "32",
             "12",           "12",
-            "17", "2", "1", "20",
-            "17", "2", "1", "20",
+            "17", "1", "2", "20",
+            "17", "1", "2", "20",
         ];
         assert.strictEqual(getCurrentValues(pivot), values.join(','));
 
@@ -1421,11 +1421,11 @@ QUnit.module('Views', {
         // sanity check of what the table should look like if all groups are
         // expanded, to ensure that the former asserts are pertinent
         values = [
-            "12", "17", "2", "1", "32",
+            "12", "17", "1", "2", "32",
             "12",                 "12",
             "12",                 "12",
-                  "17", "2", "1", "20",
-                  "17", "2", "1", "20",
+                  "17", "1", "2", "20",
+                  "17", "1", "2", "20",
         ];
         assert.strictEqual(getCurrentValues(pivot), values.join(','));
 
@@ -2716,6 +2716,52 @@ QUnit.module('Views', {
         await testUtils.dom.click(pivot.$('th.o_pivot_header_cell_closed')[1]);
 
         assert.containsOnce(pivot, '.o_pivot_field_menu', 'Only one dropdown should be displayed at a time');
+
+        pivot.destroy();
+    });
+
+    QUnit.test('Server order is kept by default', async function (assert) {
+        assert.expect(1);
+
+        let isSecondReadGroup = false;
+
+        var pivot = await createView({
+            View: PivotView,
+            model: "partner",
+            data: this.data,
+            arch: '<pivot>' +
+                    '<field name="customer" type="row"/>' +
+                    '<field name="foo" type="measure"/>' +
+                '</pivot>',
+            mockRPC: function (route, args) {
+                if (args.method === 'read_group' && isSecondReadGroup) {
+                    return Promise.resolve([
+                        {
+                            customer: [2, 'Second'],
+                            foo: 18,
+                            __count: 2,
+                            __domain :[["customer", "=", 2]],
+                        },
+                        {
+                            customer: [1, 'First'],
+                            foo: 14,
+                            __count: 2,
+                            __domain :[["customer", "=", 1]],
+                        }
+                    ]);
+                }
+                var result = this._super.apply(this, arguments);
+                isSecondReadGroup = true;
+                return result;
+            },
+        });
+
+        const values = [
+            "32", // Total Value
+            "18", // Second
+            "14", // First
+        ];
+        assert.strictEqual(getCurrentValues(pivot), values.join());
 
         pivot.destroy();
     });
