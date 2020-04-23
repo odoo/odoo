2fe62ef80a2030a9ef15abe57dab629e65b1ae18
Yannick Tivisse <yti@odoo.com>
2020-04-23 09:17:51 +0200

    [IMP] hr_timesheet: Make unit_amount multi-editable on timesheets
    TaskID: 2169102

--- addons/account/models/account_analytic_line.py
+++ addons/account/models/account_analytic_line.py
@@ -56,32 +56,49 @@ class AccountAnalyticLine(models.Model):
     _description = 'Analytic Line'
 
     product_id = fields.Many2one('product.product', string='Product', check_company=True)
-    general_account_id = fields.Many2one('account.account', string='Financial Account', ondelete='restrict', readonly=True,
-                                         related='move_id.account_id', store=True, domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
-                                         compute_sudo=True)
+    general_account_id = fields.Many2one(
+        'account.account', string='Financial Account', ondelete='restrict', readonly=False,
+        related='move_id.account_id', store=True, compute='_compute_general_account_id',
+        domain="[('deprecated', '=', False), ('company_id', '=', company_id)]", compute_sudo=True)
     move_id = fields.Many2one('account.move.line', string='Journal Item', ondelete='cascade', index=True, check_company=True)
     code = fields.Char(size=8)
     ref = fields.Char(string='Ref.')
 
-    @api.onchange('product_id', 'product_uom_id', 'unit_amount', 'currency_id')
-    def on_change_unit_amount(self):
-        if not self.product_id:
-            return {}
+    amount = fields.Monetary(compute='_compute_unit_amount', store=True, readonly=False)
+    product_uom_id = fields.Many2one(compute='_compute_product_uom_id', store=True, readonly=False)
 
-        result = 0.0
-        prod_accounts = self.product_id.product_tmpl_id.with_company(self.company_id)._get_product_accounts()
-        unit = self.product_uom_id
-        account = prod_accounts['expense']
-        if not unit or self.product_id.uom_po_id.category_id.id != unit.category_id.id:
-            unit = self.product_id.uom_po_id
+    @api.depends('product_id', 'company_id')
+    def _compute_general_account_id(self):
+        for line in self:
+            if not line.product_id:
+                continue
+            prod_accounts = line.product_id.product_tmpl_id.with_company(line.company_id)._get_product_accounts()
+            line.general_account_id = prod_accounts['expense']
 
-        # Compute based on pricetype
-        amount_unit = self.product_id.price_compute('standard_price', uom=unit)[self.product_id.id]
-        amount = amount_unit * self.unit_amount or 0.0
-        result = (self.currency_id.round(amount) if self.currency_id else round(amount, 2)) * -1
-        self.amount = result
-        self.general_account_id = account
-        self.product_uom_id = unit
+    @api.depends('product_id', 'product_uom_id')
+    def _compute_product_uom_id(self):
+        for line in self:
+            if not line.product_id:
+                continue
+            unit = line.product_uom_id
+            if not unit or line.product_id.uom_po_id.category_id.id != unit.category_id.id:
+                line.product_uom_id = line.product_id.uom_po_id
+
+    @api.depends('product_id', 'product_uom_id', 'unit_amount', 'currency_id')
+    def _compute_amount(self):
+        for line in self:
+            if not line.product_id:
+                continue
+
+            unit = line.product_uom_id
+            if not unit or line.product_id.uom_po_id.category_id.id != unit.category_id.id:
+                unit = line.product_id.uom_po_id
+
+            # Compute based on pricetype
+            amount_unit = line.product_id.price_compute('standard_price', uom=unit)[line.product_id.id]
+            amount = amount_unit * line.unit_amount or 0.0
+            result = (line.currency_id.round(amount) if line.currency_id else round(amount, 2)) * -1
+            line.amount = result
 
     @api.model
     def view_header_get(self, view_id, view_type):
