0333f0abb4c1285595ffe57d10e1a7002ae53cca
qmo-odoo <qmo@odoo.com>
2020-04-26 11:29:52 +0000

    [ADD] {google,fetchmail}_gmail: OAuth for gmail servers
    Purpose
    =======
    Less secured apps are no longer supported by google, therefore, we need
    to transition to the OAuth2 authentication system.
    
    Specifications
    ==============
    1. User will need to fill their Gmail API credentials in the main
       settings page
    2. Then, in the incoming / outgoing mail server form view, they will
       need to tick the Gmail support checkbox
    3. A link will be available to be redirected to Gmail and accept the
       permission
    4. The user can now copy / paste the authorization code in Odoo, set
       his email as "login" and then send / receive emails with Gmail
    
    Task-2170676
    
    closes odoo/odoo#83424
    
    X-original-commit: ff223afc5300b2421b8ce935bb468f503c938c5d
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ addons/fetchmail_gmail/models/fetchmail_server.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models, _
+from odoo.exceptions import UserError
+
+
+class FetchmailServer(models.Model):
+    _name = 'fetchmail.server'
+    _inherit = ['fetchmail.server', 'google.gmail.mixin']
+
+    @api.constrains('use_google_gmail_service', 'server_type')
+    def _check_use_google_gmail_service(self):
+        if any(server.use_google_gmail_service and server.server_type != 'imap' for server in self):
+            raise UserError(_('Gmail authentication only supports IMAP server type.'))
+
+    @api.onchange('use_google_gmail_service')
+    def _onchange_use_google_gmail_service(self):
+        """Set the default configuration for a IMAP Gmail server."""
+        if self.use_google_gmail_service:
+            self.server = 'imap.gmail.com'
+            self.server_type = 'imap'
+            self.is_ssl = True
+            self.port = 993
+        else:
+            self.server_type = 'pop'
+            self.is_ssl = False
+            self.google_gmail_authorization_code = False
+            self.google_gmail_refresh_token = False
+            self.google_gmail_access_token = False
+            self.google_gmail_access_token_expiration = False
+
+    def _imap_login(self, connection):
+        """Authenticate the IMAP connection.
+
+        If the mail server is Gmail, we use the OAuth2 authentication protocol.
+        """
+        self.ensure_one()
+        if self.use_google_gmail_service:
+            auth_string = self._generate_oauth2_string(self.user, self.google_gmail_refresh_token)
+            connection.authenticate('XOAUTH2', lambda x: auth_string)
+            connection.select('INBOX')
+        else:
+            super(FetchmailServer, self)._imap_login(connection)
--- odoo/addons/base/models/ir_mail_server.py
+++ odoo/addons/base/models/ir_mail_server.py
@@ -226,7 +226,10 @@ class IrMailServer(models.Model):
         elif not host:
             mail_server, smtp_from = self.sudo()._find_mail_server(smtp_from)
 
+        if not mail_server:
+            mail_server = self.env['ir.mail_server']
         ssl_context = None
+
         if mail_server:
             smtp_server = mail_server.smtp_host
             smtp_port = mail_server.smtp_port
@@ -314,7 +317,7 @@ class IrMailServer(models.Model):
             local, at, domain = smtp_user.rpartition('@')
             if at:
                 smtp_user = local + at + idna.encode(domain).decode('ascii')
-            connection.login(smtp_user, smtp_password or '')
+            mail_server._smtp_login(connection, smtp_user, smtp_password or '')
 
         # Some methods of SMTP don't check whether EHLO/HELO was sent.
         # Anyway, as it may have been sent by login(), all subsequent usages should consider this command as sent.
@@ -326,6 +329,18 @@ class IrMailServer(models.Model):
         connection.smtp_from = smtp_from
 
         return connection
+
+    def _smtp_login(self, connection, smtp_user, smtp_password):
+        """Authenticate the SMTP connection.
+
+        Can be overridden in other module for different authentication methods.Can be
+        called on the model itself or on a singleton.
+
+        :param connection: The SMTP connection to authenticate
+        :param smtp_user: The user to used for the authentication
+        :param smtp_password: The password to used for the authentication
+        """
+        connection.login(smtp_user, smtp_password)
 
     def build_email(self, email_from, email_to, subject, body, email_cc=None, email_bcc=None, reply_to=False,
                     attachments=None, message_id=None, references=None, object_id=False, subtype='plain', headers=None,
