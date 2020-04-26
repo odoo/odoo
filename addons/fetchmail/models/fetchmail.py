8ea5655f1918f4924eb6c6413c723a651ccd0848
qmo-odoo <qmo@odoo.com>
2020-04-26 13:29:52 +0200

    [ADD] {google,fetchmail}_gmail: OAuth for gmail servers
    Purpose
    =======
    Less secured apps are no longer supported by google, therefore, we need
    to transition to the OAuth2 authentication system.
    
    Specifications
    ==============
    1. User will need to fill their Gmail API credentials in the main
       settings page
    2. Then, in the incoming / outgoing mail server form view, they will
       need to tick the Gmail support checkbox
    3. A link will be available to be redirected to Gmail and accept the
       permission
    4. The user can now copy / paste the authorization code in Odoo, set
       his email as "login" and then send / receive emails with Gmail
    
    Task-2170676

--- addons/fetchmail/models/fetchmail.py
+++ addons/fetchmail/models/fetchmail.py
@@ -103,7 +103,7 @@ odoo_mailgate: "|/path/to/odoo-mailgate.py --host=localhost -u %(uid)d -p PASSWO
                 connection = IMAP4_SSL(self.server, int(self.port))
             else:
                 connection = IMAP4(self.server, int(self.port))
-            connection.login(self.user, self.password)
+            self._imap_login(connection)
         elif self.server_type == 'pop':
             if self.is_ssl:
                 connection = POP3_SSL(self.server, int(self.port))
@@ -116,6 +116,16 @@ odoo_mailgate: "|/path/to/odoo-mailgate.py --host=localhost -u %(uid)d -p PASSWO
         # Add timeout on socket
         connection.sock.settimeout(MAIL_TIMEOUT)
         return connection
+
+    def _imap_login(self, connection):
+        """Authenticate the IMAP connection.
+
+        Can be overridden in other module for different authentication methods.
+
+        :param connection: The IMAP connection to authenticate
+        """
+        self.ensure_one()
+        connection.login(self.user, self.password)
 
     def button_confirm_login(self):
         for server in self:
