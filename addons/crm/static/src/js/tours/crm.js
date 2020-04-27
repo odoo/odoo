e77de52502fa96fa86d1b55e4bced9275bf90140
Pierre Collinet (pic) <pic@odoo.com>
2020-04-27 12:47:28 +0200

    [FIX] crm: fix error during test of crm.js
    create new opportunity during the onboarding tour of the crm module

--- addons/crm/static/src/js/tours/crm.js
+++ addons/crm/static/src/js/tours/crm.js
@@ -11,26 +11,28 @@ tour.register('crm_tour', {
 }, [tour.STEPS.SHOW_APPS_MENU_ITEM, {
     trigger: '.o_app[data-menu-xmlid="crm.crm_menu_root"]',
     content: _t("Ready to boost your sales? Let’s have a look at your <b>Pipeline</b>."),
-    position: 'bottom'
+    position: 'bottom',
 }, {
     trigger: '.o-kanban-button-new',
+    extra_trigger: '.o_opportunity_kanban',
     content: _t("<b>Create your first opportunity</b>"),
-    position: 'bottom'
+    position: 'bottom',
 }, {
-    trigger: '.o_kanban_quick_create .o_field_widget[name="partner_id"]',
-    content: _t("Look for a Contact or create a new one.Tip : Did you know you can search by VAT Number as well?"),
-    position: 'right'
+    trigger: ".o_kanban_quick_create input:first",
+    content: _t('Look for a Contact or create a new one. Tip : Did you know you can search by VAT Number as well?'),
+    position: "right",
+}, {
+    trigger: ".o_kanban_quick_create input[name='name']",
+    content: _t("<b>Choose a name</b> for your opportunity, example: <i>'Need a new website'</i>"),
+    position: "right",
 }, {
     trigger: '.o_kanban_quick_create .o_field_monetary[name="planned_revenue"]',
     content: _t("Define here the Expected Revenue of this Opportunity."),
-    position: 'right'
+    position: 'right',
 }, {
     trigger: '.o_kanban_quick_create .o_kanban_add',
     content: _t("Now, <b>add your Opportunity</b> to your Pipeline."),
     position: 'bottom',
-    run: function (actions) {
-        actions.auto('.o_kanban_quick_create .o_kanban_cancel')
-    }
 }, {
     trigger: ".o_opportunity_kanban .o_kanban_group:first-child .o_kanban_record:last-child",
     content: _t("<b>Drag &amp; drop opportunities</b> between columns as you progress in your sales cycle."),
@@ -40,12 +42,12 @@ tour.register('crm_tour', {
     trigger: ".o_kanban_record:not(.o_updating) .o_activity_color_default",
     extra_trigger: ".o_opportunity_kanban",
     content: _t("Looks like nothing is planned. :(Tip : Schedule activities to keep track of everything you have to do!"),
-    position: "bottom"
+    position: "bottom",
 }, {
     trigger: ".o_schedule_activity",
     extra_trigger: ".o_opportunity_kanban",
     content: _t("Let's schedule an activity."),
-    position: "bottom"
+    position: "bottom",
 }, {
     trigger: '.modal-body .o_field_char[name="summary"]',
     extra_trigger: ".o_opportunity_kanban",
