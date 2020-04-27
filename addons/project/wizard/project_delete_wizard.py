a6406e339825afe71cc27b9667293db503a57d57
Kevin Baptiste <kba@odoo.com>
2020-04-27 06:50:59 +0000

    [IMP] project: hide Archive button on delete wizard
    ... when all the projects are already archived.
    
    closes odoo/odoo#48728
    
    Taskid: 2228556
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/project/wizard/project_delete_wizard.py
+++ addons/project/wizard/project_delete_wizard.py
@@ -10,6 +10,11 @@ class ProjectDelete(models.TransientModel):
 
     project_ids = fields.Many2many('project.project', string='Projects')
     task_count = fields.Integer(compute='_compute_task_count')
+    projects_archived = fields.Boolean(compute='_compute_projects_archived')
+
+    def _compute_projects_archived(self):
+        for wizard in self.with_context(active_test=False):
+            wizard.projects_archived = all([not p.active for p in wizard.project_ids])
 
     def _compute_task_count(self):
         for wizard in self:
@@ -17,8 +22,7 @@ class ProjectDelete(models.TransientModel):
 
     def action_archive(self):
         self.project_ids.write({'active': False})
-        return self.env.ref('project.open_view_project_all').read()[0]
 
     def confirm_delete(self):
         self.with_context(active_test=False).project_ids.unlink()
-        return self.env.ref('project.open_view_project_all').read()[0]
+        return self.env.ref('project.open_view_project_all_config').read()[0]
