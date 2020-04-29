aad9797260d7b01cb36b85465ed97c9030a5e52e
Yannick Tivisse <yti@odoo.com>
2020-04-29 14:00:02 +0200

    [MOV] base: Move passwords wizard into their own files in base

---
+++ odoo/addons/base/wizard/change_password_user.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models, _
+from odoo.exceptions import UserError
+
+
+class ChangePasswordUser(models.TransientModel):
+    """ A model to configure users in the change password wizard. """
+    _name = 'change.password.user'
+    _description = 'User, Change Password Wizard'
+
+    wizard_id = fields.Many2one('change.password.wizard', string='Wizard', required=True, ondelete='cascade')
+    user_id = fields.Many2one('res.users', string='User', required=True, ondelete='cascade')
+    user_login = fields.Char(string='User Login', readonly=True)
+    new_passwd = fields.Char(string='New Password', default='')
+
+    def change_password_button(self):
+        for line in self:
+            if not line.new_passwd:
+                raise UserError(_("Before clicking on 'Change Password', you have to write a new password."))
+            line.user_id.write({'password': line.new_passwd})
+        # don't keep temporary passwords in the database longer than necessary
+        self.write({'new_passwd': False})
