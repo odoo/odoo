9084b8064065cc34d35f95474c32ed68518b1e24
Didier <did@odoo.com>
2020-05-04 11:48:06 +0200

    Ordering message in the tree view

--- addons/im_chatbot/models/message.py
+++ addons/im_chatbot/models/message.py
@@ -6,8 +6,10 @@ from odoo import models, fields, api
 class Message(models.Model):
     _name = "im_chatbot.message"
     _description = "Message from the chatbot"
+    _order = "sequence, id"
 
     name = fields.Char(string="Message")
+    sequence = fields.Integer(string="Sequence", default=10)
     answer_type = fields.Selection(
         [("selection", "Selection"), ("input", "User input")], required=True
     )
