17c21f81784d0dd9395e189203c18982427e942c
pka-odoo <pka@odoo.com>
2020-05-04 11:09:23 +0200

    [IMP] base_import,*: move Import button to Favorite menu
    *account,crm_iap_lead,hr_expense
    
    Part of task 2234547
    
    Co-authored-by: Mohammed Shekha <msh@odoo.com>
    Co-authored-by: Aaron Bohy <aab@odoo.com>

--- addons/base_import/static/tests/import_buttons_tests.js
+++ addons/base_import/static/tests/import_buttons_tests.js
@@ -1,14 +1,15 @@
 odoo.define('web.base_import_tests', function (require) {
 "use strict";
 
-var KanbanView = require('web.KanbanView');
-var ListView = require('web.ListView');
-var testUtils = require('web.test_utils');
+const KanbanView = require('web.KanbanView');
+const ListView = require('web.ListView');
+const PivotView = require('web.PivotView');
+const testUtils = require('web.test_utils');
 
-var createView = testUtils.createView;
+const createView = testUtils.createView;
 
 QUnit.module('Base Import Tests', {
-    beforeEach: function() {
+    beforeEach: function () {
         this.data = {
             foo: {
                 fields: {
@@ -22,119 +23,148 @@ QUnit.module('Base Import Tests', {
     }
 });
 
-QUnit.test('add import button in list', async function(assert) {
+QUnit.test('import in favorite dropdown in list', async function (assert) {
     assert.expect(2);
 
-    var list = await createView({
+    const list = await createView({
         View: ListView,
         model: 'foo',
         data: this.data,
         arch: '<tree><field name="foo"/></tree>',
     });
 
-    assert.isVisible(list.$buttons.find('.o_button_import:contains(Import)'),
-        "should have a visible Import button");
-
-    testUtils.mock.intercept(list, 'do_action', function() {
+    testUtils.mock.intercept(list, 'do_action', function () {
         assert.ok(true, "should have triggered a do_action");
     });
 
-    await testUtils.dom.click(list.$buttons.find('.o_button_import:contains(Import)'));
+    await testUtils.dom.click(list.$('.o_favorite_menu button'));
+    assert.containsOnce(list, '.o_import_menu');
+
+    await testUtils.dom.click(list.$('.o_import_menu button'));
+
     list.destroy();
 });
 
-QUnit.test('list without import button', async function(assert) {
+QUnit.test('import favorite dropdown item should not in list with create="0"', async function (assert) {
     assert.expect(1);
 
-    var list = await createView({
+    const list = await createView({
         View: ListView,
         model: 'foo',
         data: this.data,
-        arch: '<tree><field name="foo"/></tree>',
-        viewOptions: {
-            import_enabled: false,
-        }
+        arch: '<tree create="0"><field name="foo"/></tree>',
     });
 
-    assert.containsNone(list.$buttons, '.o_button_import', 'should not have an Import button');
+    await testUtils.dom.click(list.$('.o_favorite_menu button'));
+    assert.containsNone(list, '.o_import_menu');
+
     list.destroy();
 });
 
-QUnit.test('add import button in kanban', async function(assert) {
-    assert.expect(2);
-
-    var kanban = await createView({
-        View: KanbanView,
-        model: 'foo',
-        data: this.data,
-        arch: '<kanban><templates><t t-name="kanban-box">' +
-                    '<div>' +
-                    '<field name="foo"/>' +
-                    '</div>' +
-                '</t></templates></kanban>',
-    });
-
-    assert.isVisible(kanban.$buttons.find('.o_button_import:contains(Import)'),
-        "should have a visible Import button");
-
-    testUtils.mock.intercept(kanban, 'do_action', function() {
-        assert.ok(true, "should have triggered a do_action");
-    });
-
-    await testUtils.dom.click(kanban.$buttons.find('.o_button_import:contains(Import)'));
-    kanban.destroy();
-});
-
-QUnit.test('kanban without import button', async function(assert) {
+QUnit.test('import favorite dropdown item should not in list with import="0"', async function (assert) {
     assert.expect(1);
 
-    var kanban = await createView({
-        View: KanbanView,
-        model: 'foo',
-        data: this.data,
-        arch: '<kanban><templates><t t-name="kanban-box">' +
-                    '<div>' +
-                    '<field name="foo"/>' +
-                    '</div>' +
-                '</t></templates></kanban>',
-        viewOptions: {
-            import_enabled: false,
-        }
-    });
-
-    assert.containsNone(kanban.$buttons, '.o_button_import', "should not have an Import button");
-    kanban.destroy();
-});
-
-QUnit.test('import attrs in list views', async function (assert) {
-    assert.expect(1);
-
-    var list = await createView({
+    const list = await createView({
         View: ListView,
         model: 'foo',
         data: this.data,
         arch: '<tree import="0"><field name="foo"/></tree>',
     });
 
-    assert.containsNone(list.$buttons, '.o_button_import');
+    await testUtils.dom.click(list.$('.o_favorite_menu button'));
+    assert.containsNone(list, '.o_import_menu');
+
     list.destroy();
 });
 
-QUnit.test('import attrs in kanban views', async function (assert) {
-    assert.expect(1);
+QUnit.test('import in favorite dropdown in kanban', async function (assert) {
+    assert.expect(2);
 
-    var kanban = await createView({
+    const kanban = await createView({
         View: KanbanView,
         model: 'foo',
         data: this.data,
-        arch: '<kanban import="0">' +
-                '<templates><t t-name="kanban-box">' +
-                    '<div><field name="foo"/></div>' +
-                '</t></templates>' +
-            '</kanban>',
+        arch: `<kanban>
+                <templates>
+                    <t t-name="kanban-box">
+                        <div><field name="foo"/></div>
+                    </t>
+                </templates>
+            </kanban>`,
     });
 
-    assert.containsNone(kanban.$buttons, '.o_button_import');
+    testUtils.mock.intercept(kanban, 'do_action', function () {
+        assert.ok(true, "should have triggered a do_action");
+    });
+
+    await testUtils.dom.click(kanban.$('.o_favorite_menu button'));
+    assert.containsOnce(kanban, '.o_import_menu');
+
+    await testUtils.dom.click(kanban.$('.o_import_menu button'));
+
     kanban.destroy();
 });
+
+QUnit.test('import favorite dropdown item should not in list with create="0"', async function (assert) {
+    assert.expect(1);
+
+    const kanban = await createView({
+        View: KanbanView,
+        model: 'foo',
+        data: this.data,
+        arch: `<kanban create="0">
+                <templates>
+                    <t t-name="kanban-box">
+                        <div><field name="foo"/></div>
+                    </t>
+                </templates>
+            </kanban>`,
+    });
+
+    await testUtils.dom.click(kanban.$('.o_favorite_menu button'));
+    assert.containsNone(kanban, '.o_import_menu');
+
+    kanban.destroy();
+});
+
+QUnit.test('import dropdown favorite should not in kanban with import="0"', async function (assert) {
+    assert.expect(1);
+
+    const kanban = await createView({
+        View: KanbanView,
+        model: 'foo',
+        data: this.data,
+        arch: `<kanban import="0">
+                <templates>
+                    <t t-name="kanban-box">
+                        <div><field name="foo"/></div>
+                    </t>
+                </templates>
+            </kanban>`,
+    });
+
+    await testUtils.dom.click(kanban.$('.o_favorite_menu button'));
+    assert.containsNone(kanban, '.o_import_menu');
+
+    kanban.destroy();
+});
+
+QUnit.test('import should not available in favorite dropdown in pivot (other than kanban or list)', async function (assert) {
+    assert.expect(1);
+
+    this.data.foo.fields.foobar = { string: "Fubar", type: "integer", group_operator: 'sum' };
+
+    const pivot = await createView({
+        View: PivotView,
+        model: 'foo',
+        data: this.data,
+        arch: '<pivot><field name="foobar" type="measure"/></pivot>',
+    });
+
+    await testUtils.dom.click(pivot.$('.o_favorite_menu button'));
+    assert.containsNone(pivot, '.o_import_menu');
+
+    pivot.destroy();
+});
+
 });
