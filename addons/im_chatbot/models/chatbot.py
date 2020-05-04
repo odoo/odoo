231c2fdb289269e34aa411ba4a226b28a9c357d9
Didier <did@odoo.com>
2020-05-04 16:30:03 +0200

    Display chatbot number on the channel form

--- addons/im_chatbot/models/chatbot.py
+++ addons/im_chatbot/models/chatbot.py
@@ -13,3 +13,15 @@ class ChatBot(models.Model):
     message_ids = fields.One2many("im_chatbot.message", "chatbot_id")
 
     channel_ids = fields.Many2many("im_livechat.channel")
+class ImLivechatChannel(models.Model):
+    _name = "im_livechat.channel"
+    _inherit = "im_livechat.channel"
+
+    chatbot_ids = fields.Many2many("im_chatbot.chatbot")
+
+    nb_chatbot = fields.Integer(compute="_compute_nb_chatbot")
+
+    @api.depends("chatbot_ids")
+    def _compute_nb_chatbot(self):
+        for channel in self:
+            channel.nb_chatbot = len(channel.chatbot_ids)
