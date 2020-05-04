698fcbd6d059fa0c4fc157fd7b3b1d9d2664c3c8
Didier <did@odoo.com>
2020-05-04 10:44:30 +0200

    Name string and answer_type as required

--- addons/im_chatbot/models/message.py
+++ addons/im_chatbot/models/message.py
@@ -7,7 +7,7 @@ class Message(models.Model):
     _name = "im_chatbot.message"
     _description = "Message from the chatbot"
 
-    name = fields.Char()
-    answer_type = fields.Selection([("selection", "selection")])
+    name = fields.Char(string="Message")
+    answer_type = fields.Selection([("selection", "Selection")], required=True)
     chatbot_id = fields.Many2one("im_chatbot.chatbot", index=True)
     answer_ids = fields.Many2many("im_chatbot.answer")
