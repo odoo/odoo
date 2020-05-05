9dfe09775fcbf773ae9e13dd44c9b5021f9f8c79
David Beguin <dbe@odoo.com>
2020-05-05 07:54:04 +0000

    [FIX] survey: redirect on login page on survey start if not logged in.
    Before this commit, when sharing a survey 'Login required' to a non logged in
    user, the redirection was made to the home page.
    The reason is: when the survey is not started yet, no user input is already
    created for this survey and this user. So if no user_input has been created,
    yet, that means that the user has never started the survey before and user
    need first to login before continuing.
    
    After this commit, we redirect to the loggin page.
    
    Task ID: 2246430
    
    closes odoo/odoo#58965
    
    X-original-commit: a309d889ddf6e03a6701e921258cfa9338800fbb
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: David Beguin <dbeguin@users.noreply.github.com>

--- addons/survey/controllers/main.py
+++ addons/survey/controllers/main.py
@@ -125,15 +125,18 @@ class Survey(http.Controller):
             return request.render("survey.survey_void_content", {'survey': survey_sudo, 'answer': answer_sudo})
         elif error_key == 'survey_closed' and access_data['can_answer']:
             return request.render("survey.survey_closed_expired", {'survey': survey_sudo})
-        elif error_key == 'survey_auth' and answer_sudo.access_token:
-            if answer_sudo.partner_id and (answer_sudo.partner_id.user_ids or survey_sudo.users_can_signup):
-                if answer_sudo.partner_id.user_ids:
-                    answer_sudo.partner_id.signup_cancel()
+        elif error_key == 'survey_auth':
+            if not answer_sudo:  # survey is not even started
+                redirect_url = '/web/login?redirect=/survey/start/%s' % survey_sudo.access_token
+            elif answer_sudo.access_token:  # survey is started but user is not logged in anymore.
+                if answer_sudo.partner_id and (answer_sudo.partner_id.user_ids or survey_sudo.users_can_signup):
+                    if answer_sudo.partner_id.user_ids:
+                        answer_sudo.partner_id.signup_cancel()
+                    else:
+                        answer_sudo.partner_id.signup_prepare(expiration=fields.Datetime.now() + relativedelta(days=1))
+                    redirect_url = answer_sudo.partner_id._get_signup_url_for_action(url='/survey/start/%s?answer_token=%s' % (survey_sudo.access_token, answer_sudo.access_token))[answer_sudo.partner_id.id]
                 else:
-                    answer_sudo.partner_id.signup_prepare(expiration=fields.Datetime.now() + relativedelta(days=1))
-                redirect_url = answer_sudo.partner_id._get_signup_url_for_action(url='/survey/start/%s?answer_token=%s' % (survey_sudo.access_token, answer_sudo.access_token))[answer_sudo.partner_id.id]
-            else:
-                redirect_url = '/web/login?redirect=%s' % ('/survey/start/%s?answer_token=%s' % (survey_sudo.access_token, answer_sudo.access_token))
+                    redirect_url = '/web/login?redirect=%s' % ('/survey/start/%s?answer_token=%s' % (survey_sudo.access_token, answer_sudo.access_token))
             return request.render("survey.survey_auth_required", {'survey': survey_sudo, 'redirect_url': redirect_url})
         elif error_key == 'answer_deadline' and answer_sudo.access_token:
             return request.render("survey.survey_closed_expired", {'survey': survey_sudo})
