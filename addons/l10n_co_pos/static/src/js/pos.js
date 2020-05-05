c0ba1b0828a53f43e8acd4a1507768ca669ee756
Joseph Caburnay <jcb@odoo.com>
2020-05-05 07:56:47 +0000

    [REF] l10n_co_pos: migration to OWL framework

--- addons/l10n_co_pos/static/src/js/pos.js
+++ addons/l10n_co_pos/static/src/js/pos.js
@@ -2,9 +2,6 @@ odoo.define('l10n_co_pos.pos', function (require) {
 "use strict";
 
 var models = require('point_of_sale.models');
-var screens = require('point_of_sale.screens');
-var session = require('web.session');
-var rpc = require('web.rpc');
 
 models.PosModel = models.PosModel.extend({
     is_colombian_country: function () {
@@ -30,35 +27,6 @@ models.Order = models.Order.extend({
         result = Boolean(result || this.pos.is_colombian_country());
         return result;
     }
-});
-
-screens.PaymentScreenWidget.include({
-    post_push_order_resolve: function (order, server_ids) {
-        if (this.pos.is_colombian_country()) {
-            var _super = this._super;
-            var args = arguments;
-            var self = this;
-            return new Promise (function (resolve, reject) {
-                rpc.query({
-                    model: 'pos.order',
-                    method: 'search_read',
-                    domain: [['id', 'in', server_ids]],
-                    fields: ['name'],
-                    context: session.user_context,
-                }).then(function (result) {
-                    order.set_l10n_co_dian(result[0].name || false);
-                }).finally(function () {
-                    _super.apply(self, args).then(function () {
-                        resolve();
-                    }).catch(function (error) {
-                        reject(error);
-                    });
-                });
-            });
-        } else {
-            return this._super(order, server_ids);
-        }
-    },
 });
 
 });
