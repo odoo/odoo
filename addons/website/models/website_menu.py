49f2b004a096c7667fc8ec752a8460f15bcb4110
Nicolas Lempereur <nle@odoo.com>
2020-05-05 15:11:06 +0000

    [FIX] website: changing menu sequence clear menu cache
    As of 02e01dce51 the website menu is prefetched for the user and saved
    in cache.
    
    If the sequence of a menu is changed (eg. with the "Edit Menu" modal on
    website), the cache was not cleared so it seemed like this did not work.
    
    With this changeset, changing a menu sequence will clear ormcache.
    
    opw-2244908
    closes #50683
    
    closes odoo/odoo#50703
    
    X-original-commit: 8da3276e8b641e615e4c436b4f0f79139d158220
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- addons/website/models/website_menu.py
+++ addons/website/models/website_menu.py
@@ -94,7 +94,7 @@ class Menu(models.Model):
 
     def write(self, values):
         res = super().write(values)
-        if 'website_id' in values or 'group_ids' in values:
+        if 'website_id' in values or 'group_ids' in values or 'sequence' in values:
             self.clear_caches()
         return res
 
