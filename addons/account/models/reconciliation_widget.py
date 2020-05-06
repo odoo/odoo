odoo/odoo

commit db4b3c174189d67b270521428437c38f9705f002

[FIX] account: unkown partner reconciliation from aged report
Create a journal entry with accounts payable receivable in the journal
lines and no partner. Reach out Aged payable/receivable report. Click on
the unkown partner and then reconcile. Nothing will be shown in the
reconciliation widget.

This occur because the function which gather the lines do not take into
account the possibility of an empty partners list

opw-2242031

closes odoo/odoo#50726

Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

addons/account/models/reconciliation_widget.py
@@ -317,7 +317,7 @@ class AccountReconciliation(models.AbstractModel):
         # show entries that are not reconciled with other partner. Asking for a specific partner on a specific account
         # is never done.
         accounts_data = []
-        if not partner_ids:
+        if not any(partner_ids):
             accounts_data = self.get_data_for_manual_reconciliation('account', account_ids)
         return {
             'customers': self.get_data_for_manual_reconciliation('partner', partner_ids, 'receivable'),

commit 729ebc1a39eb68540afe3839647fa48c42b4b7dd

[FIX] account: avoid traceback on reconciliation
Commit db4b3c1 introduced a traceback when reconciliation view is opened for
manual reconciliation because partner_ids can be a list or None

addons/account/models/reconciliation_widget.py
@@ -317,7 +317,7 @@ class AccountReconciliation(models.AbstractModel):
         # show entries that are not reconciled with other partner. Asking for a specific partner on a specific account
         # is never done.
         accounts_data = []
-        if not any(partner_ids):
+        if not partner_ids or not any(partner_ids):
             accounts_data = self.get_data_for_manual_reconciliation('account', account_ids)
         return {
             'customers': self.get_data_for_manual_reconciliation('partner', partner_ids, 'receivable'),