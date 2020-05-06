498f43c9dc919fa15963e1b81fd7e7a42633034f
Goffin Simon <sig@odoo.com>
2020-05-06 12:45:47 +0200

    [FIX] sales_team: Portal users suggested as Salesperson
    Steps to reproduce the bug:
    
    - Create a portal user U
    - Go to Contact app and create a new contact
    - In the page Sales & Purchases, select a salesperson
    
    Bug:
    
    It was possible to select U even if U was not in the group group_sale_salesman
    
    Same behavior as field user_id on model 'sale.order'
    
    opw:2246954

--- addons/sales_team/models/res_partner.py
+++ addons/sales_team/models/res_partner.py
@@ -10,3 +10,9 @@ class ResPartner(models.Model):
     team_id = fields.Many2one(
         'crm.team', 'Sales Team',
         help='If set, this Sales Team will be used for sales and assignations related to this partner')
+    salesman_user_ids = fields.One2many(comodel_name='res.users', compute='_compute_salesman_user_ids')
+
+
+    def _compute_salesman_user_ids(self):
+        for record in self:
+            record.salesman_user_ids = self.env.ref('sales_team.group_sale_salesman').users
