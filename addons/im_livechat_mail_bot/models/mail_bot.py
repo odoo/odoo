b91151a71b9f3e3433e09178aa3e992f57830084
dmonzonis <mod@odoo.com>
2020-05-07 07:09:43 +0000

    [IMP] mail_bot: improve OdooBot workflow
    This commit attempts to improve the workflow of the OdooBot onboarding
    tutorial, as well as the answer given by the bot while idle.
    
    These changes come from watching how users interact with OdooBot. To
    improve the user's experience, the following changes have been made:
    
    1. Move the attachment to the end of the tour, as it is where most
       people just close the bot.
    2. Remove the quotation marks around commands that OdooBot tells the
       user to type, like "/" or ":)", as some users try to actually type
       the quotation marks too. Instead, use a light grey background around
       the command that the user should type.
    3. Some users ask OdooBot questions, but the responses they get are
       useless. To solve this, use a message linking to the documentation or the
       videos when:
        - The user gives wrong answers twice for the same stage of the tour
        - The user talks again when the tour is completed
        - There's a question mark in the answer
    
    To achieve this a "failed" state field is added on user model, allowing to
    distinguish state in the bot workflow from state of answers (failed / not
    failed).
    
    Task ID: 2233014
    PR #49661

--- addons/im_livechat_mail_bot/models/mail_bot.py
+++ addons/im_livechat_mail_bot/models/mail_bot.py
@@ -10,13 +10,16 @@ class MailBot(models.AbstractModel):
     def _get_answer(self, record, body, values, command):
         odoobot_state = self.env.user.odoobot_state
         if self._is_bot_in_private_channel(record):
-            if odoobot_state == "onboarding_ping" and self._is_bot_pinged(values):
+            if odoobot_state == "onboarding_attachement" and values.get("attachment_ids"):
+                self.env.user.odoobot_failed = False
                 self.env.user.odoobot_state = "onboarding_canned"
-                return _("That's me! 🎉<br/>Try to type \":\" to use canned responses.")
+                return _("That's me! 🎉<br/>Try typing <span class=\"o_odoobot_command\">:</span> to use canned responses.")
             elif odoobot_state == "onboarding_canned" and values.get("canned_response_ids"):
+                self.env.user.odoobot_failed = False
                 self.env.user.odoobot_state = "idle"
                 return _("Good, you can customize canned responses in the live chat application.<br/><br/><b>It's the end of this overview</b>, enjoy discovering Odoo!")
-            #repeat question if needed
-            elif odoobot_state == 'onboarding_canned':
-                return _("Not sure wat you are doing. Please press : and wait for the propositions. Select one of them and press enter.")
+            # repeat question if needed
+            elif odoobot_state == 'onboarding_canned' and not self._is_help_requested(body):
+                self.env.user.odoobot_failed = True
+                return _("Not sure what you are doing. Please, type <span class=\"o_odoobot_command\">:</span> and wait for the propositions. Select one of them and press enter.")
         return super(MailBot, self)._get_answer(record, body, values, command)
