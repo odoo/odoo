0aa8dd8a923d5f1b42673caa83dd7c6d5522cfc0
Nicolas Martinelli <nim@odoo.com>
2020-05-07 07:44:01 +0000

    [FIX] point_of_sale, stock_sms: do not send mail and SMS
    Complement of PR #50720 to aovid sending email as well.
    
    opw-2250185
    
    closes odoo/odoo#50814
    
    closes odoo/odoo#51042
    
    X-original-commit: 64ef9d74753ddf532cb4fc3d9769cd4913a2d9d7
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/point_of_sale/models/stock_picking.py
+++ addons/point_of_sale/models/stock_picking.py
@@ -131,3 +131,8 @@ class StockPicking(models.Model):
                             self.env['stock.move.line'].create(ml_vals)
             else:
                 current_move.quantity_done = abs(sum(order_lines.mapped('qty')))
+
+    def _send_confirmation_email(self):
+        # Avoid sending Mail/SMS for POS deliveries
+        pickings = self.filtered(lambda p: p.picking_type_id != p.picking_type_id.warehouse_id.pos_type_id)
+        return super(StockPicking, pickings)._send_confirmation_email()
