ff180fe5917c61cf9e0f5e87019fc1512d0fc79b
Ipsita Borisagar <ibo@odoo.com>
2020-05-08 18:33:41 +0530

    [IMP] web: remove prompt from kanban view
    Before this commit, if we schedule more than one activity and then click on
    small clock icon from systray shows "Load more...(x remaining)" prompt into
    kanban view which is meaningless.
    
    After this commit, if there are more than one scheduled activity it doesn't show
    the prompt as we can see in v12.0 and v11.0. It shows that prompt only when the
    len(records) >= limit in `web_search_read`.
    
    Task-id: 2238562
    PR https://github.com/odoo/odoo/pull/51251

--- addons/web/models/models.py
+++ addons/web/models/models.py
@@ -38,7 +38,7 @@ class Base(models.AbstractModel):
                 'length': 0,
                 'records': []
             }
-        if limit and (len(records) == limit or self.env.context.get('force_search_count')):
+        if limit and (len(records) >= limit and self.env.context.get('force_search_count')):
             length = self.search_count(domain)
         else:
             length = len(records) + offset
