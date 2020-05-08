0c02ff54c356a4cefcab68130f6a4c8db2f98840
Nicolas Martinelli <nim@odoo.com>
2020-05-08 12:09:26 +0000

    [FIX] purchase_stock: receipt with return
    - Create a PO, order 5 items
    - Validate Receipt with 5 items
    - Return 1 item to Vendor
    - Update Order Quantity on PO, from 5 to 8
    
    A picking of 3 units is created.
    
    If the same workflow is used in Sales, a picking of 4 units would be
    created, which is the expected behavior.
    
    We adapt the logic of Sales [1] to Purchase.
    
    [1] https://github.com/odoo/odoo/commit/8a3284685856b6980e3b620abdbbb3e2c79f57c4
    
    opw-2233826
    
    closes odoo/odoo#50919
    
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- addons/purchase_stock/models/purchase.py
+++ addons/purchase_stock/models/purchase.py
@@ -366,7 +366,10 @@ class PurchaseOrderLine(models.Model):
             return res
         qty = 0.0
         price_unit = self._get_stock_move_price_unit()
-        for move in self.move_ids.filtered(lambda x: x.state != 'cancel' and not x.location_dest_id.usage == "supplier"):
+        outgoing_moves, incoming_moves = self._get_outgoing_incoming_moves()
+        for move in outgoing_moves:
+            qty -= move.product_uom._compute_quantity(move.product_uom_qty, self.product_uom, rounding_method='HALF-UP')
+        for move in incoming_moves:
             qty += move.product_uom._compute_quantity(move.product_uom_qty, self.product_uom, rounding_method='HALF-UP')
         template = {
             # truncate to 2000 to avoid triggering index limit error
@@ -419,3 +422,16 @@ class PurchaseOrderLine(models.Model):
         """
         lines = self.filtered(lambda l: l.propagate_date == values['propagate_date'] and l.propagate_date_minimum_delta == values['propagate_date_minimum_delta'] and l.propagate_cancel == values['propagate_cancel'])
         return lines and lines[0] or self.env['purchase.order.line']
+
+    def _get_outgoing_incoming_moves(self):
+        outgoing_moves = self.env['stock.move']
+        incoming_moves = self.env['stock.move']
+
+        for move in self.move_ids.filtered(lambda r: r.state != 'cancel' and not r.scrapped and self.product_id == r.product_id):
+            if move.location_dest_id.usage == "supplier" and move.to_refund:
+                outgoing_moves |= move
+            elif move.location_dest_id.usage != "supplier":
+                if not move.origin_returned_move_id or (move.origin_returned_move_id and move.to_refund):
+                    incoming_moves |= move
+
+        return outgoing_moves, incoming_moves
