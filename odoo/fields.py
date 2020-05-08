469ca96531ba8a1249f7ee2375bc099ab0f668c0
Raphael Collet <rco@odoo.com>
2020-05-08 10:36:08 +0200

    [FIX] core: partial unlink in recordset
    The following loop crashes on the second iteration:
    
        for record in records:
    	record.display_name     # access non-stored computed field
            record.unlink()         # delete record
    
    The cache has been invalidated by `unlink` on the first iteration, so
    the field is computed.  The computation is done on the whole recordset,
    which fails with a `MissingError` because of the first record.  The fix
    is to retry the recomputation on the single record in this case.
    
    X-original-commit: 7f33ebf528b1415452ce549e3329c3fe89be0ad8

--- odoo/fields.py
+++ odoo/fields.py
@@ -914,7 +914,7 @@ class Field(MetaField('DummyField', (object,), {})):
             recs = record if self.recursive else env.records_to_compute(self)
             try:
                 self.compute_value(recs)
-            except AccessError:
+            except (AccessError, MissingError):
                 self.compute_value(record)
 
         try:
@@ -949,7 +949,7 @@ class Field(MetaField('DummyField', (object,), {})):
                     recs = record if self.recursive else record._in_cache_without(self)
                     try:
                         self.compute_value(recs)
-                    except AccessError:
+                    except (AccessError, MissingError):
                         self.compute_value(record)
                     value = env.cache.get(record, self)
 
