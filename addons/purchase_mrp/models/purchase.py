5f01219047cf26883b6b3515470332f9b4c13674
Rémy Voet (ryv) <ryv@odoo.com>
2020-05-08 15:13:21 +0200

    [FIX] purchase_mrp: fix count MO in case of MTO
    task-2241471

--- addons/purchase_mrp/models/purchase.py
+++ addons/purchase_mrp/models/purchase.py
@@ -15,11 +15,12 @@ class PurchaseOrder(models.Model):
     @api.depends('order_line.move_dest_ids.group_id.mrp_production_ids')
     def _compute_mrp_production_count(self):
         for purchase in self:
-            purchase.mrp_production_count = len(purchase.order_line.move_dest_ids.group_id.mrp_production_ids)
+            purchase.mrp_production_count = len(purchase.order_line.move_dest_ids.group_id.mrp_production_ids |
+                                                purchase.order_line.move_ids.move_dest_ids.group_id.mrp_production_ids)
 
     def action_view_mrp_productions(self):
         self.ensure_one()
-        mrp_production_ids = self.order_line.move_dest_ids.group_id.mrp_production_ids.ids
+        mrp_production_ids = (self.order_line.move_dest_ids.group_id.mrp_production_ids | self.order_line.move_ids.move_dest_ids.group_id.mrp_production_ids).ids 
         action = {
             'res_model': 'mrp.production',
             'type': 'ir.actions.act_window',
