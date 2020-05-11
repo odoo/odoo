b70818fd758ed646d709063c85566e7fb9897566
Siddarth Gajjar <sga@odoo.com>
2020-05-11 11:13:52 +0000

    [REF] {website_}mass_mailing: display social links in mass mailing templates
    PURPOSE
    
    Using Email Marketing or Marketing Automation without website installed will
    not populate the links for your social media accounts.
    
    SPECIFICATIONS
    
    Move social_links template directly into mass_mailing and use company
    information available through social_media module as default values, then
    overridden by website information.
    
    TaskID 2036094
    PR #51025
    Enterprise PR odoo/enterprise#10497
    
    Related: odoo/upgrade#1212
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mass_mailing/__manifest__.py
+++ addons/mass_mailing/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Email Marketing',
     'summary': 'Design, send and track emails',
     'description': "",
-    'version': '2.1',
+    'version': '2.2',
     'sequence': 110,
     'website': 'https://www.odoo.com/page/mailing',
     'category': 'Marketing/Email Marketing',
