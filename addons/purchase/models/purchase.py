b5ab7e8d52f8ecd396c4d57356d01594be90b76c
Nicolas Martinelli <nim@odoo.com>
2020-05-13 15:54:13 +0000

    [FIX] purchase: invoice status
    1. Create products A & B with 'Control Policy' set to 'On received
       quantities'
    1. Create a PO with 2 products A & B
    2. Validate
       => status is `no`
    3. Receive Product A, create a backorder for product B
       => status is `to invoice`
    4. Create a the bill for A
       => status is `no`
    5. Change the ordered quantity of B to 0
       => status is `no`
    
    Before 15330712d2ea3c16c57, the status was `invoiced`, which is more
    logical. Indeed, If we ordered nothing, there is nothing expected to be
    received, so the PO can be considered fully billed.
    
    Revert the fix seems it turns out it wasn't even fixing the use case...
    
    Note that it would make things much easier to define a field
    `invoice_status` on the PO line, as already done on the SO line.
    
    opw-2253926
    
    X-original-commit: 072526408943afc4549ac679f027dde9aeed0149

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -49,15 +49,17 @@ class PurchaseOrder(models.Model):
                 for line in order.order_line.filtered(lambda l: not l.display_type)
             ):
                 order.invoice_status = 'to invoice'
-            elif all(
-                (line.product_qty if line.product_id.purchase_method == 'purchase' else line.qty_received)
-                and float_compare(
-                    line.qty_invoiced,
-                    line.product_qty if line.product_id.purchase_method == 'purchase' else line.qty_received,
-                    precision_digits=precision,
+            elif (
+                all(
+                    float_compare(
+                        line.qty_invoiced,
+                        line.product_qty if line.product_id.purchase_method == "purchase" else line.qty_received,
+                        precision_digits=precision,
+                    )
+                    >= 0
+                    for line in order.order_line.filtered(lambda l: not l.display_type)
                 )
-                >= 0
-                for line in order.order_line.filtered(lambda l: not l.display_type)
+                and order.invoice_ids
             ):
                 order.invoice_status = 'invoiced'
             else:
