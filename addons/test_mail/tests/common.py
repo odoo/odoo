5e56b2fdf8ddbcfc3ca5bc3f0f16705c8d2f24e1
Thibault Delavallée <tde@odoo.com>
2020-05-14 10:33:52 +0000

    [FIX] mass_mailing: correctly consider replies for traces
    Consider an email going through the mail gateway. Its headers References
    and In-Reply-To are used in mail gateway to find if it is an answer to an
    existing discussion thread.
    
    Notably in mass mailing it is used to set mailing traces (called statistics
    before 13.0) as opened and replied. However currently only the References
    header is used.
    
    In this commit we now use References or In-Reply-To like what is done in
    mail gateway.
    
    Task ID 2257717
    PR #51332
    
    Forward-port-of: #51319
    Forward-port-of: #51247
    X-original-commit: 9921e38692e7dc4547afc34acc395da340eca410

--- addons/test_mail/tests/common.py
+++ addons/test_mail/tests/common.py
@@ -341,6 +341,39 @@ class MockEmails(common.SingleTransactionCase):
         self.env['mail.thread'].with_context(mail_channel_noautofollow=True).message_process(model, mail)
         return self.env[target_model].search([(target_field, '=', subject)])
 
+    def gateway_reply_wrecord(self, template, record, use_in_reply_to=True):
+        """ Simulate a reply through the mail gateway. Usage: giving a record,
+        find an email sent to him and use its message-ID to simulate a reply.
+
+        Some noise is added in References just to test some robustness. """
+        email = self._find_sent_email_wrecord(record)
+
+        if use_in_reply_to:
+            extra = 'In-Reply-To:\r\n\t%s\n' % email['message_id']
+        else:
+            disturbing_other_msg_id = '<123456.654321@another.host.com>'
+            extra = 'References:\r\n\t%s\n\r%s' % (email['message_id'], disturbing_other_msg_id)
+
+        return self.format_and_process(
+            template, email_from=email['email_to'][0], to=email['reply_to'],
+            subject='Re: %s' % email['subject'],
+            extra=extra,
+            msg_id='<123456.%s.%d@test.example.com>' % (record._name, record.id),
+            target_model=record._name,
+            target_field=record._rec_name,
+        )
+
+    def _find_sent_email_wrecord(self, record):
+        """ Helper to find in outgoing emails (see build_email) an email linked to
+        a given record. It has been introduced with a fix for mass mailing and is
+        not meant to be used widely, proper tools are available in later versions. """
+        for mail in self._mails:
+            if mail['object_id'] == '%d-%s' % (record.id, record._name):
+                break
+        else:
+            raise AssertionError('Sent email not found for record %s' % record)
+        return mail
+
 
 @tagged('moderation')
 class Moderation(MockEmails, BaseFunctionalTest):
