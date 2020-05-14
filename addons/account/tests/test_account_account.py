62b324e8442751a63d88f0b01dbbf80e0c5ecd74
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-05-14 07:22:00 +0000

    [FIX] account: Fix UPDATE in _toggle_reconcile_to_false
    As _toggle_reconcile_to_true updates the amount_residual on unreconciled
    move lines linked to the account, _toggle_reconcile_to_false must
    set the amount_residual to 0.
    However as the SQL query is broken the update fails and move lines
    supposed to have 0 as amount_residual still have the value.
    
    closes odoo/odoo#52533
    
    X-original-commit: 8e32134cedce688c0fa33d1c51c5570d63f14efa
    Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

--- addons/account/tests/test_account_account.py
+++ addons/account/tests/test_account_account.py
@@ -27,3 +27,102 @@ class TestAccountAccount(AccountTestInvoicingCommon):
 
         with self.assertRaises(UserError), self.cr.savepoint():
             self.company_data['default_account_revenue'].company_id = self.company_data_2['company']
+
+    def test_toggle_reconcile(self):
+        ''' Test the feature when the user sets an account as reconcile/not reconcile with existing journal entries. '''
+        account = self.company_data['default_account_revenue']
+
+        move = self.env['account.move'].create({
+            'move_type': 'entry',
+            'date': '2019-01-01',
+            'line_ids': [
+                (0, 0, {
+                    'account_id': account.id,
+                    'currency_id': self.currency_data['currency'].id,
+                    'debit': 100.0,
+                    'credit': 0.0,
+                    'amount_currency': 200.0,
+                }),
+                (0, 0, {
+                    'account_id': account.id,
+                    'currency_id': self.currency_data['currency'].id,
+                    'debit': 0.0,
+                    'credit': 100.0,
+                    'amount_currency': -200.0,
+                }),
+            ],
+        })
+        move.post()
+
+        self.assertRecordValues(move.line_ids, [
+            {'reconciled': False, 'amount_residual': 0.0, 'amount_residual_currency': 0.0},
+            {'reconciled': False, 'amount_residual': 0.0, 'amount_residual_currency': 0.0},
+        ])
+
+        # Set the account as reconcile and fully reconcile something.
+        account.reconcile = True
+        self.env['account.move.line'].invalidate_cache()
+
+        self.assertRecordValues(move.line_ids, [
+            {'reconciled': False, 'amount_residual': 100.0, 'amount_residual_currency': 200.0},
+            {'reconciled': False, 'amount_residual': -100.0, 'amount_residual_currency': -200.0},
+        ])
+
+        move.line_ids.reconcile()
+        self.assertRecordValues(move.line_ids, [
+            {'reconciled': True, 'amount_residual': 0.0, 'amount_residual_currency': 0.0},
+            {'reconciled': True, 'amount_residual': 0.0, 'amount_residual_currency': 0.0},
+        ])
+
+        # Set back to a not reconcile account and check the journal items.
+        move.line_ids.remove_move_reconcile()
+        account.reconcile = False
+        self.env['account.move.line'].invalidate_cache()
+
+        self.assertRecordValues(move.line_ids, [
+            {'reconciled': False, 'amount_residual': 0.0, 'amount_residual_currency': 0.0},
+            {'reconciled': False, 'amount_residual': 0.0, 'amount_residual_currency': 0.0},
+        ])
+
+    def test_toggle_reconcile_with_partials(self):
+        ''' Test the feature when the user sets an account as reconcile/not reconcile with partial reconciliation. '''
+        account = self.company_data['default_account_revenue']
+
+        move = self.env['account.move'].create({
+            'move_type': 'entry',
+            'date': '2019-01-01',
+            'line_ids': [
+                (0, 0, {
+                    'account_id': account.id,
+                    'currency_id': self.currency_data['currency'].id,
+                    'debit': 100.0,
+                    'credit': 0.0,
+                    'amount_currency': 200.0,
+                }),
+                (0, 0, {
+                    'account_id': account.id,
+                    'currency_id': self.currency_data['currency'].id,
+                    'debit': 0.0,
+                    'credit': 50.0,
+                    'amount_currency': -100.0,
+                }),
+                (0, 0, {
+                    'account_id': self.company_data['default_account_expense'].id,
+                    'currency_id': self.currency_data['currency'].id,
+                    'debit': 0.0,
+                    'credit': 50.0,
+                    'amount_currency': -100.0,
+                }),
+            ],
+        })
+        move.post()
+
+        # Set the account as reconcile and partially reconcile something.
+        account.reconcile = True
+        self.env['account.move.line'].invalidate_cache()
+
+        move.line_ids.filtered(lambda line: line.account_id == account).reconcile()
+
+        # Try to set the account as a not-reconcile one.
+        with self.assertRaises(UserError), self.cr.savepoint():
+            account.reconcile = False
