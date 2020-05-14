57ec9049e2d1fe6043b2a4b6660cc83b480a4cef
qsm-odoo <qsm@odoo.com>
2020-05-14 22:44:28 +0000

    [MOV] web: rename colorpicker_dialog.xml file
    The ColorpickerDialog will become a widget, this commit is only about
    renaming the related XML file.
    
    Part of https://github.com/odoo/odoo/pull/46088
    task-2195313

--- addons/web/static/src/js/widgets/colorpicker.js
+++ addons/web/static/src/js/widgets/colorpicker.js
@@ -9,7 +9,7 @@ var _t = core._t;
 
 var ColorpickerDialog = Dialog.extend({
     xmlDependencies: (Dialog.prototype.xmlDependencies || [])
-        .concat(['/web/static/src/xml/colorpicker_dialog.xml']),
+        .concat(['/web/static/src/xml/colorpicker.xml']),
     template: 'ColorpickerDialog',
     events: _.extend({}, Dialog.prototype.events || {}, {
         'mousedown .o_color_pick_area': '_onMouseDownPicker',
