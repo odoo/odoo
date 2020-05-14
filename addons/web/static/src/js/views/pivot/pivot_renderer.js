15b11ed8f6583d8972e9420daf4d2ce8c148592b
Aaron Bohy <aab@odoo.com>
2020-05-14 14:48:06 +0200

    [FIX] web: pivot no content helper
    This commit reverts [1], which fixed the no content
    helper message of the pivot view, by moving the message
    definition in the JS.
    
    With Owl components, one must always use the translation
    function in the env (env._t), and no longer import it from
    'web.core'. Moreover, there is no valid reason to define the
    message in JS here, except that there was a bug in owl, that
    has been fixed by [2] (merged in Odoo by the previous commit).
    
    [1] https://github.com/odoo/odoo/commit/c7cc672aba669175280d619ad39b14daefd7733f
    [2] https://github.com/odoo/owl/commit/2af8cccd65424ea75fbaa9f0a83c6974be4b1b5d

--- addons/web/static/src/js/views/pivot/pivot_renderer.js
+++ addons/web/static/src/js/views/pivot/pivot_renderer.js
@@ -3,8 +3,6 @@ odoo.define('web.PivotRenderer', function (require) {
 
     const OwlAbstractRenderer = require('web.AbstractRendererOwl');
     const field_utils = require('web.field_utils');
-    const core = require('web.core');
-    const _t = core._t;
 
     const { useExternalListener, useState, onMounted, onPatched } = owl.hooks;
 
@@ -50,9 +48,6 @@ odoo.define('web.PivotRenderer', function (require) {
 
             if (!this.env.device.isMobile) {
                 useExternalListener(window, 'click', this._resetState);
-            }
-            if (!props.hasData || !props.measures.length) {
-                this.description = _t("Try to add some records, or make sure that there is at least one measure and no active filter in the search bar.");
             }
         }
 
