e9b667403e382991baf22a29a41e643bc0124ff7
Jorge Pinna Puissant <jpp@odoo.com>
2020-05-15 14:01:05 +0000

    [FIX] hr: activity type configuration on hr plans
    - Employees > Configuration > Planning Types and ensure the activity
        type "To Do" is set to scheduled date several days after previous
        activity (default should be 5 days);
    - Check that onboarding Plan has at least 2 activities with activity
        type "To Do";
    - Click launch plan.
    
    Before this commit, the activity type configuration is ignored and all
    activities are due tomorrow.
    
    Now, the activities are due taking into account the activity type
    configuration.
    
    opw-2255586
    
    X-original-commit: bf8a9af1ac549be830bec28c88eb1c565b591941

--- addons/hr/wizard/hr_plan_wizard.py
+++ addons/hr/wizard/hr_plan_wizard.py
@@ -24,12 +24,13 @@ class HrPlanWizard(models.TransientModel):
             responsible = activity_type.get_responsible_id(self.employee_id)
 
             if self.env['hr.employee'].with_user(responsible).check_access_rights('read', raise_exception=False):
-                self.employee_id.activity_schedule(
+                activity = self.employee_id.activity_schedule(
                     activity_type_id=activity_type.activity_type_id.id,
                     summary=activity_type.summary,
                     note=activity_type.note,
                     user_id=responsible.id
                 )
+                activity._onchange_activity_type_id()
 
         return {
             'type': 'ir.actions.act_window',
