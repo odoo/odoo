c799b644828df78306aaa0f8c5a97097c966ae14
Cocographique <nmi@odoo.com>
2020-05-18 23:09:38 +0000

    [IMP] website, *: improve "Blockquote" and "Quotes Carousel" snippets
    *: web_editor
    
    Part of https://github.com/odoo/odoo/pull/52698
    task-2210730

--- addons/website/static/src/snippets/s_blockquote/options.js
+++ addons/website/static/src/snippets/s_blockquote/options.js
@@ -10,43 +10,34 @@ options.registry.Blockquote = options.Class.extend({
     //--------------------------------------------------------------------------
 
     /**
-     * Changes the position of the progressbar text.
+     * Change blockquote design.
      *
      * @see this.selectClass for parameters
      */
     display: function (previewMode, widgetValue, params) {
-        this.$target.find('.s_blockquote_icon')
-            .toggleClass('d-none', widgetValue !== 'classic');
 
-        const $content = this.$target.find('.s_blockquote_content');
-        $content.find('.quote_char').remove();
+        // Classic
+        this.$target.find('.s_blockquote_avatar').toggleClass('d-none', widgetValue !== 'classic');
+
+        // Cover
+        const $blockquote = this.$target.find('.s_blockquote_content');
         if (widgetValue === 'cover') {
-            this.$target.find('.s_blockquote_content > p').before(
-                $('<fa/>').addClass('quote_char fa fa-quote-left font-italic')
-            );
-        }
-
-        // Text style
-        $content.toggleClass('text-center', widgetValue === 'cover');
-        $content.toggleClass('font-italic', widgetValue !== 'classic');
-
-        // Bg Img
-        if (widgetValue === 'cover') {
-            this.$target.css({'background-image': "url('/web/image/website.s_parallax_default_image')"});
-            this.$target.css({'background-position': "50%"});
-            $content.css({'background-color': "rgba(0, 0, 0, 0.5)"});
+            $blockquote.css({"background-image": "url('/web/image/website.s_blockquote_cover_default_image')"});
+            $blockquote.css({"background-position": "50% 50%"});
+            $blockquote.addClass('oe_img_bg');
+            if (this.$target.find('.s_blockquote_filter').length == 0) {
+                $blockquote.wrapInner('<div class="s_blockquote_filter bg-white-50"/>');
+            }
         } else {
-            this.$target.css({'background-image': "unset"});
-            $content.css({'background-color': "unset"});
+            $blockquote.css({"background-image": ""});
+            $blockquote.css({"background-position": ""});
+            $blockquote.removeClass('oe_img_bg');
+            $blockquote.find('.s_blockquote_filter').contents().unwrap();
         }
 
-        // Blockquote Footer
-        const $footer = this.$target.find('footer');
-        $footer.toggleClass('text-white', widgetValue === 'cover');
-        this.$target.toggleClass('text-white', widgetValue === 'cover');
-        $footer.toggleClass('d-none', widgetValue === 'minimalist');
-        this.$target.find('.s_blockquote_avatar')
-            .toggleClass('d-none', widgetValue !== 'classic');
+        // Minimalist
+        this.$target.find('.s_blockquote_icon').toggleClass('d-none', widgetValue === 'minimalist');
+        this.$target.find('footer').toggleClass('d-none', widgetValue === 'minimalist');
     },
 });
 });
