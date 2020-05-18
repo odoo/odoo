a6be65755c14a7382b485a797ae41a15ee912019
Nicolas Martinelli <nim@odoo.com>
2020-05-18 14:55:22 +0000

    [FIX] account: prevent traceback if no matching ids
    In graph view, `ids` might be `False`, leading to crashes.
    
    opw-2248350
    
    X-original-commit: 0f43ca475d297f43f6ed510a53ad5ac81c6e95de

--- addons/account/report/account_invoice_report.py
+++ addons/account/report/account_invoice_report.py
@@ -210,12 +210,53 @@ class ReportInvoiceWithoutPayment(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         docs = self.env['account.move'].browse(docids)
 
+<<<<<<< HEAD
         qr_code_urls = {}
         for invoice in docs:
             if invoice.display_qr_code:
                 new_code_url = invoice.generate_qr_code()
                 if new_code_url:
                     qr_code_urls[invoice.id] = new_code_url
+=======
+        # First we get the structure of the results. The results won't be correct in multi-currency,
+        # but we need this result structure.
+        # By adding 'ids:array_agg(id)' to the fields, we will be able to map the results of the
+        # second step in the structure of the first step.
+        result_ref = super(AccountInvoiceReport, self).read_group(
+            domain, fields + ['ids:array_agg(id)'], groupby, offset, limit, orderby, lazy
+        )
+
+        # In mono-currency, the results are correct, so we don't need the second step.
+        if len(self.env.companies.mapped('currency_id')) <= 1:
+            return result_ref
+
+        # Reset all fields needing recomputation.
+        for res_ref in result_ref:
+            for field in {'amount_total', 'price_average', 'price_subtotal', 'residual'} & set(res_ref):
+                res_ref[field] = 0.0
+
+        # Then we perform another read_group, but this time we group by 'currency_id'. This way, we
+        # are able to convert in batch in the current company currency.
+        # During the process, we fill in the result structure we got in the previous step. To make
+        # the mapping, we use the aggregated ids.
+        result = super(AccountInvoiceReport, self).read_group(
+            domain, fields + ['ids:array_agg(id)'], set(groupby) | {'currency_id'}, offset, limit, orderby, lazy
+        )
+        for res in result:
+            if res.get('currency_id') and self.env.company.currency_id.id != res['currency_id'][0]:
+                for field in {'amount_total', 'price_average', 'price_subtotal', 'residual'} & set(res):
+                    res[field] = self.env.company.currency_id.round((res[field] or 0.0) * get_rate(res['currency_id'][0]))
+            # Since the size of result_ref should be resonable, it should be fine to loop inside a
+            # loop.
+            for res_ref in result_ref:
+                if res.get('ids') and res_ref.get('ids') and set(res['ids']) <= set(res_ref['ids']):
+                    for field in {'amount_total', 'price_subtotal', 'residual'} & set(res_ref):
+                        res_ref[field] += res[field]
+                    for field in {'price_average'} & set(res_ref):
+                        res_ref[field] = (res_ref[field] + res[field]) / 2 if res_ref[field] else res[field]
+
+        return result_ref
+>>>>>>> 9326670862d... temp
 
         return {
             'doc_ids': docids,
