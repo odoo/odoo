3b5216f102decd6a7bd710c406bcba34cd97e378
Yannick Tivisse <yti@odoo.com>
2020-05-19 12:25:55 +0200

    [FIX] hr_holidays: Create leave in batch

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -630,51 +630,54 @@ class HolidaysRequest(models.Model):
             # Is probably handled via ir.rule
             raise AccessError(_('You don\'t have the rights to apply second approval on a leave request'))
 
-    @api.model
-    def create(self, values):
+    @api.model_create_multi
+    def create(self, vals_list):
         """ Override to avoid automatic logging of creation """
-        if not self._context.get('leave_fast_create'):
-            employee_id = values.get('employee_id', False)
-            leave_type_id = values.get('holiday_status_id')
-            leave_type = self.env['hr.leave.type'].browse(leave_type_id)
-            # Handle automatic department_id
-            if not values.get('department_id'):
-                values.update({'department_id': self.env['hr.employee'].browse(employee_id).department_id.id})
+        for values in vals_list:
+            if not self._context.get('leave_fast_create'):
+                employee_id = values.get('employee_id', False)
+                leave_type_id = values.get('holiday_status_id')
+                leave_type = self.env['hr.leave.type'].browse(leave_type_id)
+                # Handle automatic department_id
+                if not values.get('department_id'):
+                    values.update({'department_id': self.env['hr.employee'].browse(employee_id).department_id.id})
 
-            # Handle no_validation
-            if leave_type.validation_type == 'no_validation':
-                values.update({'state': 'confirm'})
+                # Handle no_validation
+                if leave_type.validation_type == 'no_validation':
+                    values.update({'state': 'confirm'})
 
-            # Handle double validation
-            if leave_type.validation_type == 'both':
-                self._check_double_validation_rules(employee_id, values.get('state', False))
+                # Handle double validation
+                if leave_type.validation_type == 'both':
+                    self._check_double_validation_rules(employee_id, values.get('state', False))
 
-        holiday = super(HolidaysRequest, self.with_context(mail_create_nosubscribe=True)).create(values)
-        if self._context.get('import_file'):
-            holiday._onchange_leave_dates()
-        if not self._context.get('leave_fast_create'):
-            # FIXME remove these, as they should not be needed
-            if employee_id:
-                holiday.with_user(SUPERUSER_ID)._sync_employee_details()
-            if 'number_of_days' not in values and ('date_from' in values or 'date_to' in values):
-                holiday.with_user(SUPERUSER_ID)._onchange_leave_dates()
+        holidays = super(HolidaysRequest, self.with_context(mail_create_nosubscribe=True)).create(vals_list)
 
-            # Everything that is done here must be done using sudo because we might
-            # have different create and write rights
-            # eg : holidays_user can create a leave request with validation_type = 'manager' for someone else
-            # but they can only write on it if they are leave_manager_id
-            holiday_sudo = holiday.sudo()
-            holiday_sudo.add_follower(employee_id)
-            if holiday.validation_type == 'manager':
-                holiday_sudo.message_subscribe(partner_ids=holiday.employee_id.leave_manager_id.partner_id.ids)
-            if leave_type.validation_type == 'no_validation':
-                # Automatic validation should be done in sudo, because user might not have the rights to do it by himself
-                holiday_sudo.action_validate()
-                holiday_sudo.message_subscribe(partner_ids=[holiday_sudo._get_responsible_for_approval().partner_id.id])
-                holiday_sudo.message_post(body=_("The time off has been automatically approved"), subtype="mt_comment") # Message from OdooBot (sudo)
-            elif not self._context.get('import_file'):
-                holiday_sudo.activity_update()
-        return holiday
+        for holiday in holidays:
+            if self._context.get('import_file'):
+                holiday._onchange_leave_dates()
+            if not self._context.get('leave_fast_create'):
+                # FIXME remove these, as they should not be needed
+                if employee_id:
+                    holiday.with_user(SUPERUSER_ID)._sync_employee_details()
+                if 'number_of_days' not in values and ('date_from' in values or 'date_to' in values):
+                    holiday.with_user(SUPERUSER_ID)._onchange_leave_dates()
+
+                # Everything that is done here must be done using sudo because we might
+                # have different create and write rights
+                # eg : holidays_user can create a leave request with validation_type = 'manager' for someone else
+                # but they can only write on it if they are leave_manager_id
+                holiday_sudo = holiday.sudo()
+                holiday_sudo.add_follower(employee_id)
+                if holiday.validation_type == 'manager':
+                    holiday_sudo.message_subscribe(partner_ids=holiday.employee_id.leave_manager_id.partner_id.ids)
+                if leave_type.validation_type == 'no_validation':
+                    # Automatic validation should be done in sudo, because user might not have the rights to do it by himself
+                    holiday_sudo.action_validate()
+                    holiday_sudo.message_subscribe(partner_ids=[holiday_sudo._get_responsible_for_approval().partner_id.id])
+                    holiday_sudo.message_post(body=_("The time off has been automatically approved"), subtype="mt_comment") # Message from OdooBot (sudo)
+                elif not self._context.get('import_file'):
+                    holiday_sudo.activity_update()
+        return holidays
 
     def _read(self, fields):
         fields = set(fields)
@@ -969,9 +972,9 @@ class HolidaysRequest(models.Model):
                         e_to_approve.append(employee)
                     if state == 'validate':
                         e_to_validate.append(employee)
-                split_leaves.filtered(lambda l: l.employee_id.id in e_to_confirm + e_to_approve + e_to_validate).action_confirm()
-                split_leaves.filtered(lambda l: l.employee_id.id in e_to_approve).action_approve()
-                split_leaves.filtered(lambda l: l.employee_id.id in e_to_validate).action_validate()
+                split_leaves.filtered(lambda l: l.employee_id in e_to_confirm + e_to_approve + e_to_validate).action_confirm()
+                split_leaves.filtered(lambda l: l.employee_id in e_to_approve).action_approve()
+                split_leaves.filtered(lambda l: l.employee_id in e_to_validate).action_validate()
 
             values = [holiday._prepare_holiday_values(employee) for employee in employees]
             leaves = self.env['hr.leave'].with_context(
