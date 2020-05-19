b9c87408637455c7b4b15b20581885049a5636da
Martin Trigaux <mat@odoo.com>
2020-05-19 11:37:17 +0200

    [IMP] base: do not verify ACL on ir.property
    Users have no longer access to unlink if not system. Property are
    accessed via sudo so ACL check is no longer relevant.

--- odoo/addons/base/models/ir_property.py
+++ odoo/addons/base/models/ir_property.py
@@ -360,8 +360,6 @@ class Property(models.Model):
             if value == default_value:
                 # avoid prop.unlink(), as it clears the record cache that can
                 # contain the value of other properties to set on record!
-                prop.check_access_rights('unlink')
-                prop.check_access_rule('unlink')
                 self._cr.execute("DELETE FROM ir_property WHERE id=%s", [prop.id])
             elif value != clean(prop.get_by_record()):
                 prop.write({'value': value})
