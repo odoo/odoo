63a8fdd9db1112d496f549d3c44b24aea8e908d9
Jeremy Kersten <jke@odoo.com>
2020-05-19 16:17:36 +0000

    [IMP] website_forum: store the forum_count
    Cannot be a one2many, since website_id=False means that it is for all websites

--- addons/website_forum/models/website.py
+++ addons/website_forum/models/website.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, _
+from odoo import models, fields, api, _
 from odoo.addons.http_routing.models.ir_http import url_for
 
 
 class Website(models.Model):
     _inherit = 'website'
 
-    def _compute_field_forums_count(self):
-        Forum = self.env['forum.forum']
-        for website in self:
-            website.forums_count = Forum.search_count(website.website_domain())
+    @api.model
+    def get_default_forum_count(self):
+        self.forums_count = self.env['forum.forum'].search_count(self.website_domain())
 
-    forums_count = fields.Integer(compute=_compute_field_forums_count)
+    forums_count = fields.Integer(readonly=True, default=get_default_forum_count)
 
     def get_suggested_controllers(self):
         suggested_controllers = super(Website, self).get_suggested_controllers()
