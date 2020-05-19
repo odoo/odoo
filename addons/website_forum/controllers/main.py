52a0406a2c71621ce4c5fc550b9a8a5f695b29bf
Jeremy Kersten <jke@odoo.com>
2020-05-19 12:26:11 +0000

    [IMP] website_forum: delay the set_viewed
    Try to make the update the latest possible, to minimize risk of concurrent
    update.
    
    Since it is not critical to loose a view, add the old value in where clause, to
    try to decrease the risk to alter at the same time a same row.

--- addons/website_forum/controllers/main.py
+++ addons/website_forum/controllers/main.py
@@ -235,8 +235,6 @@ class WebsiteForum(WebsiteProfile):
         if question.state == 'pending' and user.karma < forum.karma_post and question.create_uid != user:
             raise werkzeug.exceptions.NotFound()
 
-        # increment view counter
-        question.sudo().set_viewed()
         if question.parent_id:
             redirect_url = "/forum/%s/question/%s" % (slug(forum), slug(question.parent_id))
             return werkzeug.utils.redirect(redirect_url, 301)
@@ -252,6 +250,10 @@ class WebsiteForum(WebsiteProfile):
         })
         if (request.httprequest.referrer or "").startswith(request.httprequest.url_root):
             values['back_button_url'] = request.httprequest.referrer
+
+        # increment view counter
+        question.sudo().set_viewed()
+
         return request.render("website_forum.post_description_full", values)
 
     @http.route('/forum/<model("forum.forum"):forum>/question/<model("forum.post"):question>/toggle_favourite', type='json', auth="user", methods=['POST'], website=True)
