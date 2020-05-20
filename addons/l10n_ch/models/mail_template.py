0fcadf8b14b58cadc9192b1d8f901c315f2e7827
oco-odoo <oco@odoo.com>
2020-05-20 13:31:31 +0000

    [FIX] l10n_ch: QR code: raise error when the data needed for the QR-code are misconfigured
    Just like for account module with standard QR-code generation, this had to be done this way in order to enable raising errors while generating the QR-code (in case of missing data, for example). If we just called the function within the template, it would display a nasty traceback telling Q-web failed instead of just reporting a clean error message to the user.
    
    Before this commit, build_qr_code_url was called in silent mode from the template ; so it did not return any QR-code URL when an error occured, but did not raise anything either. So the user ended up with an invoice with an empty square where the QR-code should have been; without any additional explanation.
    
    closes odoo/odoo#51340
    
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>
    Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

--- addons/l10n_ch/models/mail_template.py
+++ addons/l10n_ch/models/mail_template.py
@@ -27,31 +27,25 @@ class MailTemplate(models.Model):
 
         if self.model == 'account.move':
             for record in self.env[self.model].browse(res_ids):
-                if record.l10n_ch_isr_valid:
-                    # We add an attachment containing the ISR
-                    inv_print_name = self._render_field('report_name', record.ids, compute_lang=True)[record.id]
-
-            if related_model._name == 'account.move':
-
-                template = res_ids_to_templates[res_id]
-                inv_print_name = self._render_template(template.report_name, template.model, res_id)
+                inv_print_name = self._render_field('report_name', record.ids, compute_lang=True)[record.id]
                 new_attachments = []
 
-                if related_model.l10n_ch_isr_valid:
+                if record.l10n_ch_isr_valid:
                     # We add an attachment containing the ISR
                     isr_report_name = 'ISR-' + inv_print_name + '.pdf'
-                    isr_pdf = self.env.ref('l10n_ch.l10n_ch_isr_report').render_qweb_pdf([res_id])[0]
+                    isr_pdf = self.env.ref('l10n_ch.l10n_ch_isr_report').render_qweb_pdf(record.ids)[0]
                     isr_pdf = base64.b64encode(isr_pdf)
                     new_attachments.append((isr_report_name, isr_pdf))
 
-                if related_model.can_generate_qr_bill():
+                if record.can_generate_qr_bill():
                     # We add an attachment containing the QR-bill
                     qr_report_name = 'QR-bill-' + inv_print_name + '.pdf'
-                    qr_pdf = self.env.ref('l10n_ch.l10n_ch_qr_report').render_qweb_pdf([res_id])[0]
+                    qr_pdf = self.env.ref('l10n_ch.l10n_ch_qr_report').render_qweb_pdf(record.ids)[0]
                     qr_pdf = base64.b64encode(qr_pdf)
                     new_attachments.append((qr_report_name, qr_pdf))
 
-                attachments_list = multi_mode and result[res_id].get('attachments', False) or result.get('attachments', False)
+                record_dict = multi_mode and result[record.id] or result
+                attachments_list = record_dict.get('attachments', False)
                 if attachments_list:
                     attachments_list.extend(new_attachments)
                 else:
