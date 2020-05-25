b12c8a864191142d8b4b7d205237d20eb1043ec5
DramixDw <rve@odoo.com>
2020-05-25 17:50:39 +0200

    [IMP] website_sale: /shop/cart/update as POST with csrf
    The /shop/cart/update was accepting both GET and POST request. Therefore,
    we couldn't have csrf on the route meaning any website could change your
    cart if the route was known.
    
    task-2263776

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -416,7 +416,7 @@ class WebsiteSale(http.Controller):
 
         return request.render("website_sale.cart", values)
 
-    @http.route(['/shop/cart/update'], type='http', auth="public", methods=['GET', 'POST'], website=True, csrf=False)
+    @http.route(['/shop/cart/update'], type='http', auth="public", methods=['POST'], website=True)
     def cart_update(self, product_id, add_qty=1, set_qty=0, **kw):
         """This route is called when adding a product to cart (no options)."""
         sale_order = request.website.sale_get_order(force_create=True)
