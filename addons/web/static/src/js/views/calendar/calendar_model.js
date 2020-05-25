79d6d77a690b38f6cb908f073d6b4f8c3afd3128
Julien Castiaux <juc@odoo.com>
2020-05-25 12:56:48 +0200

    [i18n] calendar: missing term
    Closes #26017

--- addons/web/static/src/js/views/calendar/calendar_model.js
+++ addons/web/static/src/js/views/calendar/calendar_model.js
@@ -444,6 +444,7 @@ return AbstractModel.extend({
             dayNamesShort: moment.weekdaysShort(),
             firstDay: this.week_start,
             slotLabelFormat: _t.database.parameters.time_format.search("%H") != -1 ? 'H:mm': 'h(:mm)a',
+            eventLimitText: _t("more"),
         };
     },
     /**
