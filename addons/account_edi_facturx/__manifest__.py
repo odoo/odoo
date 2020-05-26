a4490b3daf828c0cf559d756c30f5271c6c77e3c
Benjamin Frantzen (bfr) <bfr@odoo.com>
2020-05-26 07:46:18 +0000

    [ADD] account_edi: generic module to manage EDI (factur-x)
    - account_edi is the base module to manage EDIs. It manages the generic worflow of import/export and defines the methods for import/export that should be overridden by each types of EDI (in different modules).
    - On export, the files are added as attachment and can be embeded into pdf (format specific).
    - On export, the exported format are set on the journal to allow different settings based on journal (different companies, countries, etc).
    - On import, depending on the import method (from message_post or upload button in invoice tree view) a new invoice is created or an existing one is updated (only allowed draft invoices with no invoice lines).
    - Adapted factur-x

---
+++ addons/account_edi_facturx/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Import/Export invoices with Factur-X',
+    'version' : '1.0',
+    'category': 'Accounting/Accounting',
+    'depends' : ['account_edi'],
+    'data': [
+        'data/account_edi_data.xml',
+        'data/facturx_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
