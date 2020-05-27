1e4146b91a7a309faad43376dba7fe574db26943
Julien Castiaux <juc@odoo.com>
2020-05-27 12:41:12 +0000

    [FIX] mail: safe unicode smtp address decoding
    Send an email from a [RFC 2047] encoded email address whose decoded
    display-name contains a coma, e.g. `"Doé, John" <john@example.com>` to
    Odoo. Odoo parses the [RFC 2047] email address but doesn't re-quote the
    display-name. The decoded address is `Doé, John <john@example.com>`.
    
    According to [RFC 5322], unquoted coma is the character used as address
    separator. Using that very address to bounce the email to other
    recipients thus send a message to two separated mailboxes: `Doé` and
    `John <john@example.com>`. The first address is invalid and discarded,
    the second one is incomplete.
    
    This commit introduce a new SMTP header decoding function dedicated to
    parse addresses. It makes sure all [RFC 2047] encoded display-names are
    safely re-quoted upon decoding. Although some addresses don't need to be
    re-quoted (like `John Doé <john@example.com>`), there is no problem
    quoting them anyway (and the implementation is easier).
    
    opw-2246004
    [RFC 2047]: https://tools.ietf.org/html/rfc2047
    [RFC 53322]: https://tools.ietf.org/html/rfc5322#section-3.4
    
    closes odoo/odoo#51985
    
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- odoo/tools/mail.py
+++ odoo/tools/mail.py
@@ -533,20 +533,32 @@ def email_references(references):
     return (ref_match, model, thread_id, hostname, is_private)
 
 # was mail_message.decode()
-def decode_smtp_header(smtp_header):
+def decode_smtp_header(smtp_header, quoted=False):
     """Returns unicode() string conversion of the given encoded smtp header
     text. email.header decode_header method return a decoded string and its
-    charset for each decoded par of the header. This method unicodes the
-    decoded header and join them in a complete string. """
+    charset for each decoded part of the header. This method unicodes the
+    decoded header and join them in a complete string.
+
+    :param bool quoted: when True, encoded words in the header will be turned into RFC822
+        quoted-strings after decoding, which is appropriate for address headers
+    """
     if isinstance(smtp_header, Header):
         smtp_header = ustr(smtp_header)
     if smtp_header:
-        text = decode_header(smtp_header.replace('\r', ''))
-        # The joining space will not be needed as of Python 3.3
-        # See https://github.com/python/cpython/commit/07ea53cb218812404cdbde820647ce6e4b2d0f8e
-        sep = ' ' if pycompat.PY2 else ''
-        return sep.join([ustr(x[0], x[1]) for x in text])
-    return u''
+        pairs = decode_header(smtp_header.replace('\r', ''))
+        tokens = []
+        for token, enc in pairs:
+            token = ustr(token, enc)
+            if enc and quoted:
+                # re-quote the encoded word to form an RFC822 quoted-string
+                token = email_addr_escapes_re.sub(r'\\\g<0>', token)
+                tokens.append('"%s"' % token)
+            else:
+                # plain word
+                tokens.append(token)
+        return ''.join(tokens)
+    return ''
+
 
 # was mail_thread.decode_header()
 def decode_message_header(message, header, separator=' '):
