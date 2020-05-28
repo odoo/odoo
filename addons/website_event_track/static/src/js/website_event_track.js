7bc8d23ce3c18d3d349c75e6547d5d058c58f2af
Aurélien Warnon <awa@odoo.com>
2020-05-28 13:47:52 +0200

    [IMP] website_event_track: improve track calendar view and demo data
    This commit aims to slightly improve the agenda view for better usability:
    Better search, tags support, ...
    
    It also includes a new demo event to showcase the "online" features:
    Youtube streams, tracks suggestions, ...
    
    This new demo event is a bit special since it needs to create tracks occurring
    at the moment of the module installation in order to be able to see "live"
    tracks during the module demonstration.
    
    Task 2252655

--- addons/website_event_track/static/src/js/website_event_track.js
+++ addons/website_event_track/static/src/js/website_event_track.js
@@ -2,11 +2,14 @@ odoo.define('website_event_track.website_event_track', function (require) {
 'use strict';
 
 var publicWidget = require('web.public.widget');
+var core = require('web.core');
+var _t = core._t;
 
 publicWidget.registry.websiteEventTrack = publicWidget.Widget.extend({
     selector: '.o_wevent_event',
     events: {
         'input #event_track_search': '_onEventTrackSearchInput',
+        'click .o_wevent_track_tag': '_onEventTagClick',
     },
 
     //--------------------------------------------------------------------------
@@ -23,10 +26,26 @@ publicWidget.registry.websiteEventTrack = publicWidget.Widget.extend({
         var text = $(ev.currentTarget).val();
         var filter = _.str.sprintf(':containsLike(%s)', text);
 
-        $('#search_summary').removeClass('invisible');
+        this.$('#search_summary').removeClass('invisible');
         var $tracks = $('.event_track');
-        $('#search_number').text($tracks.filter(filter).length);
-        $tracks.removeClass('invisible').not(filter).addClass('invisible');
+        this.$('#search_count').text(_.str.sprintf(_t('%s found'), $tracks.filter(filter).length));
+        $tracks.removeClass('o_wevent_track_invisible').not(filter).addClass('o_wevent_track_invisible');
+    },
+
+    /**
+     *
+     * Allows to easily search tracks while staying on the agenda page.
+     *
+     * @private
+     * @param {MouseEvent} ev
+     */
+    _onEventTagClick: function (ev) {
+        this.$('#event_track_search').val(
+            $(ev.currentTarget).find('span').text()
+        ).trigger('input');
     },
 });
+
+return publicWidget.registry.websiteEventTrack;
+
 });
