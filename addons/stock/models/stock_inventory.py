cc329f35ad5cad1c0ee20d1ff317ff60ca80f19d
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-05-28 14:25:42 +0000

    [FIX] stock: avoid ambiguous query
    In case a third-party module adds `company_id` on `product.product`, the
    SQL query becomes ambiguous and crashes.
    
    Closes #52024
    
    X-original-commit: 0bf2a1606b233cb2db83a1cc35de243a3c431922
    Co-authored-by: Nicolas Martinelli <nim@odoo.com>

--- addons/stock/models/stock_inventory.py
+++ addons/stock/models/stock_inventory.py
@@ -220,6 +220,7 @@ class Inventory(models.Model):
         if self.location_ids:
             domain_loc = [('id', 'child_of', self.location_ids.ids)]
         else:
+<<<<<<< HEAD
             domain_loc = [('company_id', '=', self.company_id.id), ('usage', 'in', ['internal', 'transit'])]
         locations_ids = [l['id'] for l in self.env['stock.location'].search_read(domain_loc, ['id'])]
 
@@ -304,6 +305,47 @@ class Inventory(models.Model):
             vals.append(line_values)
         if self.exhausted:
             vals += self._get_exhausted_inventory_lines_vals({(l['product_id'], l['location_id']) for l in vals})
+=======
+            locations = self.env['stock.location'].search([('company_id', '=', self.company_id.id), ('usage', 'in', ['internal', 'transit'])])
+        domain = ' sq.location_id in %s AND sq.quantity != 0 AND pp.active'
+        args = (tuple(locations.ids),)
+
+        vals = []
+        Product = self.env['product.product']
+        # Empty recordset of products available in stock_quants
+        quant_products = self.env['product.product']
+
+        # If inventory by company
+        if self.company_id:
+            domain += ' AND sq.company_id = %s'
+            args += (self.company_id.id,)
+        if self.product_ids:
+            domain += ' AND sq.product_id in %s'
+            args += (tuple(self.product_ids.ids),)
+
+        self.env['stock.quant'].flush(['company_id', 'product_id', 'quantity', 'location_id', 'lot_id', 'package_id', 'owner_id'])
+        self.env['product.product'].flush(['active'])
+        self.env.cr.execute("""SELECT sq.product_id, sum(sq.quantity) as product_qty, sq.location_id, sq.lot_id as prod_lot_id, sq.package_id, sq.owner_id as partner_id
+            FROM stock_quant sq
+            LEFT JOIN product_product pp
+            ON pp.id = sq.product_id
+            WHERE %s
+            GROUP BY sq.product_id, sq.location_id, sq.lot_id, sq.package_id, sq.owner_id """ % domain, args)
+
+        for product_data in self.env.cr.dictfetchall():
+            product_data['company_id'] = self.company_id.id
+            product_data['inventory_id'] = self.id
+            # replace the None the dictionary by False, because falsy values are tested later on
+            for void_field in [item[0] for item in product_data.items() if item[1] is None]:
+                product_data[void_field] = False
+            product_data['theoretical_qty'] = product_data['product_qty']
+            if self.prefill_counted_quantity == 'zero':
+                product_data['product_qty'] = 0
+            if product_data['product_id']:
+                product_data['product_uom_id'] = Product.browse(product_data['product_id']).uom_id.id
+                quant_products |= Product.browse(product_data['product_id'])
+            vals.append(product_data)
+>>>>>>> 36ab9f8ca2d... temp
         return vals
 
 
