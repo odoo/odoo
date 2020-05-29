31d19a8f522206ec89c5712e25d12529b586f5b1
Rémy Voet (ryv) <ryv@odoo.com>
2020-05-29 09:44:48 +0200

    [IMP] mrp_account: prevent grouping slv from MO
    Prevent the group by product in the stock valuation layer when
    we coming from a MO form via the button "Stock Valution".
    
    task-2241471

--- addons/mrp_account/models/mrp_production.py
+++ addons/mrp_account/models/mrp_production.py
@@ -83,4 +83,5 @@ class MrpProduction(models.Model):
         context = literal_eval(action['context'])
         context.update(self.env.context)
         context['no_at_date'] = True
+        context['search_default_group_by_product_id'] = False
         return dict(action, domain=domain, context=context)
