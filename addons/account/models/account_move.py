830d6e9f8c9197f7a7c3f6b814765caca7cd17db
Florent de Labarre <florent.mirieu@gmail.com>
2020-05-29 12:04:44 +0000

    [FIX] account: impossible to re-post if post_at_bank_rec
    X-original-commit: 2d45844315656e4ea5c9008fc2fd800466210d82

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -2226,7 +2226,7 @@ class AccountMove(models.Model):
         return action
 
     def action_post(self):
-        if self.mapped('line_ids.payment_id') and any(post_at == 'bank_rec' for post_at in self.mapped('journal_id.post_at')):
+        if self.filtered(lambda x: x.journal_id.post_at == 'bank_rec').mapped('line_ids.payment_id').filtered(lambda x: x.state != 'reconciled'):
             raise UserError(_("A payment journal entry generated in a journal configured to post entries only when payments are reconciled with a bank statement cannot be manually posted. Those will be posted automatically after performing the bank reconciliation."))
         return self.post()
 
