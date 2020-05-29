9bcb00d1e5069f82ed8c80dde9f2e84af41e9389
Florent de Labarre <florent.mirieu@gmail.com>
2020-05-29 12:04:44 +0000

    [FIX] account: impossible to re-post if post_at_bank_rec
    closes odoo/odoo#52165
    
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -353,9 +353,8 @@ class AccountMove(models.Model):
 
     @api.multi
     def action_post(self):
-        if self.mapped('line_ids.payment_id'):
-            if any(self.mapped('journal_id.post_at_bank_rec')):
-                raise UserError(_("A payment journal entry generated in a journal configured to post entries only when payments are reconciled with a bank statement cannot be manually posted. Those will be posted automatically after performing the bank reconciliation."))
+        if self.filtered(lambda x: x.journal_id.post_at_bank_rec).mapped('line_ids.payment_id').filtered(lambda x: x.state != 'reconciled'):
+            raise UserError(_("A payment journal entry generated in a journal configured to post entries only when payments are reconciled with a bank statement cannot be manually posted. Those will be posted automatically after performing the bank reconciliation."))
         return self.post()
 
     @api.multi
