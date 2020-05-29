3e09df4dd6b4a21979d1d24a3fe988b16fdfe15f
Damien Bouvy <dbo@odoo.com>
2020-05-29 12:22:10 +0200

    fixup! [IMP] pos(_hr): convert setups to classmethod for HttpCase test classes

--- addons/point_of_sale/tests/test_js.py
+++ addons/point_of_sale/tests/test_js.py
@@ -7,10 +7,11 @@ import odoo.tests
 
 @odoo.tests.tagged("post_install", "-at_install")
 class WebSuite(odoo.tests.HttpCase):
-    def setUp(self):
-        super().setUp()
-        env = self.env(user=self.env.ref('base.user_admin'))
-        self.main_pos_config = env.ref('point_of_sale.pos_config_main')
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        env = cls.env(user=cls.env.ref('base.user_admin'))
+        cls.main_pos_config = env.ref('point_of_sale.pos_config_main')
 
     def test_pos_js(self):
         # open a session, the /pos/web controller will redirect to it
