8f0bb16fa1b7847b1d779e53827738940d4d5370
jpr-odoo <jpr@openerp.com>
2020-06-01 12:08:09 +0000

    [IMP] website: add min & max option for chart axis value
    This commit introduces min & max input options for the chart values of
    the axis. It will be visible only for the line and bar chart.
    
    - When min and max values are same, we fix this by changing the min
      value to zero and multiply provided value by 2 to set max value.
    
    - When min value greater than max value, we swap the values.
    
    - When the min value is given and the max value is not:
        - If the min value is smaller than greatest value of the chart, then
          the max value is set to the greatest value of the chart.
        - Otherwise, the given minimum value is used as maximum value and
          the minimum is set to 0.
    
    - When only the max value is given then the minimum value is set to 0.
    
    task-2250766
    
    closes odoo/odoo#52221
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Co-authored-by: dwa-odoo <dwa@odoo.com>

--- addons/website/static/src/snippets/s_chart/options.js
+++ addons/website/static/src/snippets/s_chart/options.js
@@ -118,6 +118,9 @@ options.registry.InnerChart = options.Class.extend({
             this._setDefaultSelectedInput();
             await this._reloadGraph();
         }
+        if (params.attributeName === 'minValue' || params.attributeName === 'maxValue') {
+            this._computeTicksMinMaxValue();
+        }
     },
 
     //--------------------------------------------------------------------------
@@ -147,6 +150,44 @@ options.registry.InnerChart = options.Class.extend({
             }
         }
         return this._super(...arguments);
+    },
+    /**
+     * Maintains the gap between the scale axis for the auto fit behavior if we
+     * used min/max config.
+     *
+     * @private
+     */
+    _computeTicksMinMaxValue() {
+        const dataset = this.$target[0].dataset;
+        let minValue = parseInt(dataset.minValue);
+        let maxValue = parseInt(dataset.maxValue);
+        if (!isNaN(maxValue)) {
+            // Reverse min max values when min value is greater than max value
+            if (maxValue < minValue) {
+                maxValue = minValue;
+                minValue = parseInt(dataset.maxValue);
+            } else if (maxValue === minValue) {
+                // If min value and max value are same for positive and negative
+                // number
+                minValue < 0 ? (maxValue = 0, minValue = 2 * minValue) : (minValue = 0, maxValue = 2 * maxValue);
+            }
+        } else {
+            // Find max value from each row/column data
+            const datasets = JSON.parse(dataset.data).datasets || [];
+            const dataValue = _.flatten(datasets.map(el => el.data.map(data => {
+                return !isNaN(parseInt(data)) ? parseInt(data) : 0;
+            })));
+            // When max value is not given and min value is greater than chart
+            // data values
+            if (minValue >= Math.max(...dataValue)) {
+                maxValue = minValue;
+                minValue = 0;
+            }
+        }
+        this.$target.attr({
+            'data-ticks-min': minValue,
+            'data-ticks-max': maxValue,
+        });
     },
     /**
      * Sets and reloads the data on the canvas if it has changed.
