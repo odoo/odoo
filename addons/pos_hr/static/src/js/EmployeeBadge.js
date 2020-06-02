cd6b3cf40ad187a064a800ac3036f1ad56a2500f
Joseph Caburnay <jcb@odoo.com>
2020-06-02 09:33:55 +0200

    fix flickering when mouse over

--- addons/pos_hr/static/src/js/EmployeeBadge.js
+++ addons/pos_hr/static/src/js/EmployeeBadge.js
@@ -13,7 +13,13 @@ odoo.define('pos_hr.EmployeeBadge', function (require) {
             const { selectEmployee } = useSelectEmployee();
             this.selectEmployee = selectEmployee;
             useListener('click', this._selectCashier);
-            this.state = useState({ isMouseOver: false });
+            this.state = useState({ isMouseEnter: false });
+            // We use the following state variables to assure the following behavior:
+            //  When the mouse pointer enters this component's element, the element's
+            //  width is set to whichever width is larger when the text is 'Select User'
+            //  vs when the text is '<Employee name>'.
+            this._widthBeforeMouseEnter = 0;
+            this._usePreviousWidth = false;
         }
         mounted() {
             this.env.pos.on('change:cashier', this.render, this);
@@ -21,11 +27,29 @@ odoo.define('pos_hr.EmployeeBadge', function (require) {
         willUnmount() {
             this.env.pos.off('change:cashier', null, this);
         }
+        willPatch() {
+            if (this._usePreviousWidth) {
+                this._widthBeforeMouseEnter = getComputedStyle(this.el).getPropertyValue(
+                    'width'
+                );
+            }
+        }
+        patched() {
+            if (this._usePreviousWidth) {
+                const newWidth = getComputedStyle(this.el).getPropertyValue('width');
+                if (parseFloat(newWidth) < parseFloat(this._widthBeforeMouseEnter)) {
+                    this.el.style.width = this._widthBeforeMouseEnter;
+                }
+                this._usePreviousWidth = false;
+            } else {
+                this.el.style.width = 'auto';
+            }
+        }
         get cashier() {
             return this.env.pos.get_cashier();
         }
         get text() {
-            if (this.state.isMouseOver) {
+            if (this.state.isMouseEnter) {
                 return this._onMouseOverText();
             } else {
                 return this.cashier ? this.cashier.name : '';
@@ -52,6 +76,13 @@ odoo.define('pos_hr.EmployeeBadge', function (require) {
             if (employee) {
                 this.env.pos.set_cashier(employee);
             }
+        }
+        onMouseEnter() {
+            this._usePreviousWidth = true;
+            this.state.isMouseEnter = true;
+        }
+        onMouseLeave() {
+            this.state.isMouseEnter = false;
         }
     }
     EmployeeBadge.template = 'EmployeeBadge';
