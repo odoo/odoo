c73c6dc5e373b7eb4e4aeef4210ade685328d03e
Laurent Smet <las@odoo.com>
2020-06-05 08:30:44 +0200

    [FIX] account: Fix broken 'test_invoice_taxes' in l10n builds

--- addons/account/tests/test_account_move_partner_count.py
+++ addons/account/tests/test_account_move_partner_count.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from odoo.addons.account.tests.account_test_savepoint import AccountTestInvoicingCommon
 
 from odoo.tests import tagged
