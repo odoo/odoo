d9fa88b091cb86558ef66c2a23a17d064a0c9423
jvm-odoo <jvm@odoo.com>
2020-06-05 07:47:15 +0000

    [FIX] web_editor: fix duplicated pasted image summernote
    Issue
    
            - Windows > Chrome or Firefox
            - Any chatter > Open full screen
            - Paste any image
    
            The image is pasted twice
    
    Cause
    
            Summernote adds the image but
            the browser event adds it too
    
    Solution
    
    	I applied the fix that summernote did
    	summernote/summernote/commit/6e432941af17c5fee0877d9ff54b6ace65fbdb4b
    
    OPW-2267712
    
    X-original-commit: 5298027b7a6450895ee4f377a22a7e34d9a63d6c

--- addons/web_editor/static/lib/summernote/summernote.js
+++ addons/web_editor/static/lib/summernote/summernote.js
@@ -4479,6 +4479,7 @@ var Clipboard = /** @class */ (function () {
             var item = lists.head(clipboardData.items);
             if (item.kind === 'file' && item.type.indexOf('image/') !== -1) {
                 this.context.invoke('editor.insertImagesOrCallback', [item.getAsFile()]);
+                event.preventDefault();
             }
             this.context.invoke('editor.afterCommand');
         }
