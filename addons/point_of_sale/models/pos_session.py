80e8ecfdfafc7826eb2b20887f5f52e77cbed647
Nicolas Martinelli <nim@odoo.com>
2020-06-08 15:49:59 +0200

    [FIX] point_of_sale: close session with accountg rights
    - Create a user with only Point Of Sale User rights
    - As this user, open a POS session, create a POS order
    - Validate and Close the session
    
    An `AccessError` is raised because the user has no Accounting access
    rights.
    
    A POS user should be allowed to close a POS session, as it was the case
    in 12.0.
    
    opw-2251699

--- addons/point_of_sale/models/pos_session.py
+++ addons/point_of_sale/models/pos_session.py
@@ -283,9 +283,19 @@ class PosSession(models.Model):
     def _validate_session(self):
         self.ensure_one()
         self._check_if_no_draft_orders()
-        self._create_account_move()
+        # Users without any accounting rights won't be able to create the journal entry. If this
+        # case, switch to sudo for creation and posting.
+        sudo = False
+        if (
+            not self.env['account.move'].check_access_rights('create', raise_exception=False)
+            and self.user_has_groups('point_of_sale.group_pos_user')
+        ):
+            sudo = True
+            self.sudo()._create_account_move()
+        else:
+            self._create_account_move()
         if self.move_id.line_ids:
-            self.move_id.post()
+            self.move_id.post() if not sudo else self.move_id.sudo().post()
             # Set the uninvoiced orders' state to 'done'
             self.env['pos.order'].search([('session_id', '=', self.id), ('state', '=', 'paid')]).write({'state': 'done'})
         else:
