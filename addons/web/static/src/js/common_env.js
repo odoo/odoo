af85bb5e13b9c97d022a721bd782a37d718e4496
Aaron Bohy <aab@odoo.com>
2020-06-08 11:50:24 +0000

    [IMP] web,*: do not deploy services in tests
    *mail,point_of_sale,web_editor,website
    
    In the tests, required services are deployed for each test
    independently. There is no need to have, in additon, all services
    deployed globally. Worse, it could conflict and lead to unexpected
    results.
    
    This commit ensures services are no longer deployed globally in
    tests. It turns the module 'web.env' into a declarative module with
    no side-effect, by moving the service deployment to main.js, which
    isn't added to the tests page.
    
    Task 2287397
    
    closes odoo/odoo#53817
    
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/web/static/src/js/common_env.js
+++ addons/web/static/src/js/common_env.js
@@ -19,7 +19,7 @@ odoo.define("web.commonEnv", function (require) {
 
     const { jsonRpc } = require("web.ajax");
     const { device, isDebug } = require("web.config");
-    const { bus, serviceRegistry } = require("web.core");
+    const { bus } = require("web.core");
     const rpc = require("web.rpc");
     const session = require("web.session");
     const { _t } = require("web.translation");
@@ -96,41 +96,6 @@ odoo.define("web.commonEnv", function (require) {
         },
         session,
     };
-
-
-    // Deploy services in the env (specializations of AbstractService registered
-    // into the serviceRegistry)
-    const UndeployedServices = Object.assign({}, serviceRegistry.map);
-    function _deployServices() {
-        let done = false;
-        while (!done) {
-            // find a service with no missing dependency
-            const serviceName = Object.keys(UndeployedServices).find(serviceName => {
-                const Service = UndeployedServices[serviceName];
-                return Service.prototype.dependencies.every(depName => {
-                    return env.services[depName];
-                });
-            });
-            if (serviceName) {
-                const Service = UndeployedServices[serviceName];
-                const service = new Service(env);
-                env.services[serviceName] = service;
-                delete UndeployedServices[serviceName];
-                service.start();
-            } else {
-                done = true;
-            }
-        }
-    }
-    serviceRegistry.onAdd((serviceName, Service) => {
-        if (serviceName in env.services || serviceName in UndeployedServices) {
-            throw new Error(`Service ${serviceName} is already loaded.`);
-        }
-        UndeployedServices[serviceName] = Service;
-        _deployServices();
-    });
-    _deployServices();
-
 
     return env;
 });
--- addons/web_editor/static/tests/field_html_tests.js
+++ addons/web_editor/static/tests/field_html_tests.js
@@ -176,6 +176,13 @@ QUnit.module('web_editor', {}, function () {
                     '</form>',
                 res_id: 1,
             });
+
+            // Summernote needs a RootWidget to set as parent of the ColorPaletteWidget. In the
+            // tests, there is no RootWidget, so we set it here to the parent of the form view, which
+            // can act as RootWidget, as it will honor rpc requests correctly (to the MockServer).
+            const rootWidget = odoo.__DEBUG__.services['root.widget'];
+            odoo.__DEBUG__.services['root.widget'] = form.getParent();
+
             await testUtils.form.clickEdit(form);
             var $field = form.$('.oe_form_field[name="body"]');
 
@@ -233,6 +240,7 @@ QUnit.module('web_editor', {}, function () {
                 '<p>t<font style="background-color: rgb(0, 255, 255);">oto t</font><font style="" class="bg-o-color-3">oto&nbsp;</font><font class="bg-o-color-3" style="">to</font>to</p><p>tata</p>',
                 "should have rendered the field correctly in edit");
 
+            odoo.__DEBUG__.services['root.widget'] = rootWidget;
             form.destroy();
         });
 
