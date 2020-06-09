e280855d0e189584e00d363899cd44c177172987
Nicolas Martinelli <nim@odoo.com>
2020-06-09 12:46:41 +0000

    [FIX] repair: operation translation
    Translate the repair fees description in the language of the partner, as
    done for repair lines.
    
    opw-2274120
    
    closes odoo/odoo#52715
    
    X-original-commit: c881e3fe2a8b3db43fa9613a52b996fdfab21392
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/repair/models/repair.py
+++ addons/repair/models/repair.py
@@ -726,7 +726,10 @@ class RepairFee(models.Model):
                 fp = self.env['account.fiscal.position'].browse(fp_id)
             self.tax_id = fp.map_tax(self.product_id.taxes_id, self.product_id, partner).ids
         if self.product_id:
-            self.name = self.product_id.display_name
+            if partner:
+                self.name = self.product_id.with_context(lang=partner.lang).display_name
+            else:
+                self.name = self.product_id.display_name
             self.product_uom = self.product_id.uom_id.id
             if self.product_id.description_sale:
                 self.name += '\n' + self.product_id.description_sale
