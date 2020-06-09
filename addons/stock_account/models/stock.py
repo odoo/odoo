f53c4b671e324b01a440474f4e823b4e3c756a5f
Nicolas Martinelli <nim@odoo.com>
2020-06-09 09:51:09 +0000

    [FIX] stock_account: return value in `_run_valuation`
    1. Create a product P with the following
       Stockable
       Tracked by serial number
       FIFO category
    2. Create a PO for the product
    3. Purchase at 6 pcs at 100 unit price
    4. Confirm the PO
    5. Add some serial numbers to the picking and validate it
    6. Check the product's value in stock in accounting:
       => one entry with Debit 600
    7. Create a delivery picking in stock
       Deliver 3 of the product
       Choose some serial numbers and process the picking
    8. Check the stock value as before
       => one entry with Credit 300
    9. Unlock the delivery picking from step 7.
       Set the quantity of the first line from 1 to 0
       Add a new line, selecting one of the serial numbers not selected
    10. Save and lock the picking
    
    There are 2 new accounting entries:
    - One with reference Correction of WH/OUT/XXXXXX (modification of past
      move), Debit 100
    - Another with reference WH/OUT/XXXXXX, Credit 200 and Quantity -3
    
    The second entry is incorrect, it should be a Credit of 100 and a
    Quantity of -1.
    
    The amount is incorrect since `_run_valuation` doesn't return any value
    in this specific use case. On the other hand, the quantity on the move
    line is not transferred to the account move creation. Hence, it falls
    back on the stock move quantity.
    
    opw-2261861
    
    closes odoo/odoo#52673
    
    closes odoo/odoo#52723
    
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/stock_account/models/stock.py
+++ addons/stock_account/models/stock.py
@@ -71,7 +71,7 @@ class StockMoveLine(models.Model):
             if move.state == 'done':
                 correction_value = move._run_valuation(line.qty_done)
                 if move.product_id.valuation == 'real_time' and (move._is_in() or move._is_out()):
-                    move.with_context(force_valuation_amount=correction_value)._account_entry_move()
+                    move.with_context(force_valuation_amount=correction_value, forced_quantity=line.qty_done)._account_entry_move()
         return lines
 
     @api.multi
@@ -369,7 +369,7 @@ class StockMove(models.Model):
             valued_quantity = 0
             for valued_move_line in valued_move_lines:
                 valued_quantity += valued_move_line.product_uom_id._compute_quantity(valued_move_line.qty_done, self.product_id.uom_id)
-            self.env['stock.move']._run_fifo(self, quantity=quantity)
+            value_to_return = self.env['stock.move']._run_fifo(self, quantity=quantity)
             if self.product_id.cost_method in ['standard', 'average']:
                 curr_rounding = self.company_id.currency_id.rounding
                 value = -float_round(self.product_id.standard_price * (valued_quantity if quantity is None else quantity), precision_rounding=curr_rounding)
