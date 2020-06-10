0ab39688cdc17fe740fab0e1465d81c639050b46
Goffin Simon <sig@odoo.com>
2020-06-10 10:42:21 +0000

    [FIX] odoo: Traceback when creating a new contact
    Steps to reproduce the bug:
    
    - Let's consider a new instance with default installed language 'en_US'
    - Install CRM
    - Activate a second language (e.g. en_GB)
    - Set that language in all users
    - Inactivate default language 'en_US'
    - Reset the language of your current user (no value)
    - Go to contact and try to create a new one
    
    Bug:
    
    A traceback was raised because the lang en_US did not exist.
    
    opw:2267711
    
    closes odoo/odoo#53064
    
    X-original-commit: c43647f085a7f62c9c81db6553be6a6e402943d0
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- odoo/http.py
+++ odoo/http.py
@@ -1071,7 +1071,7 @@ class OpenERPSession(sessions.Session):
         if lang in babel.core.LOCALE_ALIASES:
             lang = babel.core.LOCALE_ALIASES[lang]
 
-        context['lang'] = lang or 'en_US'
+        context['lang'] = lang
 
     def save_action(self, action):
         """
