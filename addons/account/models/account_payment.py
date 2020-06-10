a16d3bade0c65a547e71b223b821e0ba43d2d46f
Xavier ALT <xal@odoo.com>
2020-06-10 23:32:18 +0000

    [FIX] account: fix payment posting when user encoding one for another company
    Consider a multi-company database with user A allowed on multiple
    companies (ex: YourCompany (USD), Belgian Company (EUR))
    
    - user A switch to "YourCompany"
    - Go to Accounting / Sales / Documents / Payment
    - Create a new payment, with:
    
      * Payment Type = "Received Money"
      * Partner Type = "Customer"
      * Partner = "Agrolait"
      * Payment Journal = "Bank (EUR)"
        (i.e. journal of "Belgiam Company")
      * Payment Amount = "200" "EUR"
    
    - Click on "Confirm"
    
    => An error is raised "Cannot create moves for different companies."
    
    This commit ensure that when creating the payment journal entry, the
    `destination account` is always computed relative to the payment's
    company and not the current company of the user.
    
    OPW-2192639
    
    closes odoo/odoo#53029
    
    X-original-commit: 0bcf35186bb209e8e75178f32f5baeed959c35e0
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>
    Signed-off-by: Xavier ALT <xavieralt@users.noreply.github.com>

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -519,15 +519,16 @@ class account_payment(models.Model):
                 raise UserError(_('There is no Transfer Account defined in the accounting settings. Please define one to be able to confirm this transfer.'))
             self.destination_account_id = self.company_id.transfer_account_id.id
         elif self.partner_id:
+            partner = self.partner_id.with_context(force_company=self.company_id.id)
             if self.partner_type == 'customer':
-                self.destination_account_id = self.partner_id.property_account_receivable_id.id
+                self.destination_account_id = partner.property_account_receivable_id.id
             else:
-                self.destination_account_id = self.partner_id.property_account_payable_id.id
+                self.destination_account_id = partner.property_account_payable_id.id
         elif self.partner_type == 'customer':
-            default_account = self.env['ir.property'].get('property_account_receivable_id', 'res.partner')
+            default_account = self.env['ir.property'].with_context(force_company=self.company_id.id).get('property_account_receivable_id', 'res.partner')
             self.destination_account_id = default_account.id
         elif self.partner_type == 'supplier':
-            default_account = self.env['ir.property'].get('property_account_payable_id', 'res.partner')
+            default_account = self.env['ir.property'].with_context(force_company=self.company_id.id).get('property_account_payable_id', 'res.partner')
             self.destination_account_id = default_account.id
 
     @api.depends('move_line_ids.matched_debit_ids', 'move_line_ids.matched_credit_ids')
