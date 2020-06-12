17ed3e1a49ff40e41189d514e0b610f2fbfca094
mreficent <miquel.raich@eficent.com>
2020-06-12 16:00:36 +0200

    [IMP] models: error message in _read_group_raw
    Avoid to debug to find which field is not stored.

--- odoo/models.py
+++ odoo/models.py
@@ -2291,7 +2291,7 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
         for gb in groupby_fields:
             assert gb in self._fields, "Unknown field %r in 'groupby'" % gb
             gb_field = self._fields[gb].base_field
-            assert gb_field.store and gb_field.column_type, "Fields in 'groupby' must be regular database-persisted fields (no function or related fields), or function fields with store=True"
+            assert gb_field.store and gb_field.column_type, "Field %r in 'groupby' must be regular database-persisted fields (no function or related fields), or function fields with store=True" % gb
 
         aggregated_fields = []
         select_terms = []
