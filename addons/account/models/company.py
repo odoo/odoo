odoo/odoo

commit c6880460aa7c7b11622c6a3c03320314e441edcc

[IMP] account: tax report: add the possibility to use a different country than the company's for the tax report
Some companies have their office in a country, and sell and pay taxes in another one. Before this commit, they couldn't use Odoo's tax report, as the country set on the res.company than the one to be used with their taxes. It is now possible to do that, using a config parameter.

closes odoo/odoo#52405

X-original-commit: 436376f7b4428160457bbba2f3d0021fe77fc051
Related: odoo/enterprise#10944
Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>
Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

addons/account/models/company.py
@@ -589,3 +589,16 @@ class ResCompany(models.Model):
             results_by_journal['results'].append(rslt)
 
         return results_by_journal
+
+    def get_fiscal_country(self):
+        """ Returns the country to be used for this company's tax report.
+        By default, it'll be the one from the address; but a config parameter
+        may be used for each company to customize that behavior.
+        """
+        self.ensure_one()
+
+        fiscal_country_key = 'account_fiscal_country_%s' % self.id
+        forced_country_code = self.env['ir.config_parameter'].get_param(fiscal_country_key)
+        forced_country = forced_country_code and self.env['res.country'].search([('code', 'ilike', forced_country_code)], limit=1)
+
+        return forced_country or self.country_id

commit b4f757cf4f798bf36df9491d74ce4f7b69a41e7a

[FIX] account: access rights issue on tax report
- The commit https://github.com/odoo/odoo/commit/c6880460aa7c7b11622c6a3c03320314e441edcc
  introduced an issue where when the method `get_fiscal_country` is called
  without settings privilege an access error is thrown.

  Read operations should be done in `sudo` on `ir.config_parameter`
  records.

addons/account/models/company.py
@@ -598,7 +598,7 @@ class ResCompany(models.Model):
         self.ensure_one()
 
         fiscal_country_key = 'account_fiscal_country_%s' % self.id
-        forced_country_code = self.env['ir.config_parameter'].get_param(fiscal_country_key)
+        forced_country_code = self.env['ir.config_parameter'].sudo().get_param(fiscal_country_key)
         forced_country = forced_country_code and self.env['res.country'].search([('code', 'ilike', forced_country_code)], limit=1)
 
         return forced_country or self.country_id