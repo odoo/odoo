cogitoweb/OCB

[FIX] website: load specific view translation
When a view is:

- a specific view (duplicated for a specific website)
- inherited by a new view that is translated

the inheriting view will also be duplicated, but the translation will
only be created for the generic version and not the specific ones.

With this changeset, we duplicate translation of arch_db terms of the
generic view onto matching specific views.

Without the change, added test fails with:

  AssertionError: '<div>hello</div>' != '<div>hi</div>'
  loading module translation copy translation from base to specific view

fixes #51579
opw-2261278
closes #52451

Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

addons/website/tests/test_views.py
@@ -948,6 +948,33 @@ class TestCowViewSaving(common.TransactionCase):
         finally:
             View.pool._init = original_pool_init
 
+    def test_specific_view_translation(self):
+        Translation = self.env['ir.translation']
+
+        Translation.insert_missing(self.base_view._fields['arch_db'],  self.base_view)
+        translation = Translation.search([
+            ('res_id', '=', self.base_view.id), ('name', '=', 'ir.ui.view,arch_db')
+        ])
+        translation.value = 'hello'
+        translation.module = 'website'
+
+        self.base_view.with_context(website_id=1).write({'active': True})
+        specific_view = self.base_view._get_specific_views() - self.base_view
+
+        self.assertEquals(specific_view.with_context(lang='en_US').arch, '<div>hello</div>',
+            "copy on write (COW) also copy existing translations")
+
+        translation.value = 'hi'
+        self.assertEquals(specific_view.with_context(lang='en_US').arch, '<div>hello</div>',
+            "updating translation of base view doesn't update specific view")
+
+        Translation.with_context(overwrite=True).load_module_terms(['website'], ['en_US'])
+
+        specific_view.invalidate_cache(['arch_db', 'arch'])
+        self.assertEquals(specific_view.with_context(lang='en_US').arch, '<div>hi</div>',
+            "loading module translation copy translation from base to specific view")
+
+
 class Crawler(HttpCase):
     def setUp(self):
         super(Crawler, self).setUp()

odoo/odoo

X-original-commit: 989d58d26f8803b40c1411cb97b0171b05d274d7

addons/website/tests/test_views.py
@@ -945,6 +945,33 @@ class TestCowViewSaving(common.TransactionCase):
         finally:
             View.pool._init = original_pool_init
 
+    def test_specific_view_translation(self):
+        Translation = self.env['ir.translation']
+
+        Translation.insert_missing(self.base_view._fields['arch_db'],  self.base_view)
+        translation = Translation.search([
+            ('res_id', '=', self.base_view.id), ('name', '=', 'ir.ui.view,arch_db')
+        ])
+        translation.value = 'hello'
+        translation.module = 'website'
+
+        self.base_view.with_context(website_id=1).write({'active': True})
+        specific_view = self.base_view._get_specific_views() - self.base_view
+
+        self.assertEqual(specific_view.with_context(lang='en_US').arch, '<div>hello</div>',
+            "copy on write (COW) also copy existing translations")
+
+        translation.value = 'hi'
+        self.assertEqual(specific_view.with_context(lang='en_US').arch, '<div>hello</div>',
+            "updating translation of base view doesn't update specific view")
+
+        Translation._load_module_terms(['website'], ['en_US'], overwrite=True)
+
+        specific_view.invalidate_cache(['arch_db', 'arch'])
+        self.assertEqual(specific_view.with_context(lang='en_US').arch, '<div>hi</div>',
+            "loading module translation copy translation from base to specific view")
+
+
 @tagged('-at_install', 'post_install')
 class Crawler(HttpCase):
     def setUp(self):