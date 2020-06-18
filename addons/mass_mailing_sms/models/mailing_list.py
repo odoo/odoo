91d50ab07487a983633caca35e2610a5fe54ed50
Aurélien Warnon <awa@odoo.com>
2020-06-18 11:29:22 +0200

    [IMP] mass_mailing[_sms]: pimp mailing list views & demo data
    PURPOSE
    
    Improves various views related to the mass mailing lists to help the user to
    check the "health" of its lists at a glance.
    
    SPECS
    
    Introduce statistic fields on mailing.list:
    - Total number of contacts (replaces the previous number of valid emails)
    - Number of valid email contacts
    - Number of valid SMS contacts
    - Number of mailings sent using the list
    - Percentage (and total count) of opted-out contacts
    - Percentage (and total count) of blacklisted contacts
    - Percentage of contacts having at least one bounced message
    
    The statistics are shown on the kanban and also on the form view where they are
    used to quickly reach the associated mailings / contacts.
    Demo data were slightly adapted to show more interesting demos on views.
    
    On a technical point of view, the various counts of contacts are made in a
    single query using CASE WHEN syntax.
    We need some entry points in the query to be able to dynamically add fields and
    joins in the mass_mailing_sms app, but it's better than copy/pasting multiple
    times a very similar query.
    
    LINKS
    
    Task 2182622
    UPG PR odoo/upgrade#1990

--- addons/mass_mailing_sms/models/mailing_list.py
+++ addons/mass_mailing_sms/models/mailing_list.py
@@ -1,36 +1,53 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import fields, models
 
 
 class MailingList(models.Model):
     _inherit = 'mailing.list'
 
-    def _compute_contact_nbr(self):
-        if self.env.context.get('mailing_sms') and self.ids:
-            self.env.cr.execute('''
-select list_id, count(*)
-from mailing_contact_list_rel r
-left join mailing_contact c on (r.contact_id=c.id)
-left join phone_blacklist bl on c.phone_sanitized = bl.number and bl.active
-where
-    list_id in %s
-    AND COALESCE(r.opt_out,FALSE) = FALSE
-    AND c.phone_sanitized IS NOT NULL
-    AND bl.id IS NULL
-group by list_id''', (tuple(self.ids), ))
-            data = dict(self.env.cr.fetchall())
-            for mailing_list in self:
-                mailing_list.contact_nbr = data.get(mailing_list.id, 0)
-            return
-        return super(MailingList, self)._compute_contact_nbr()
+    contact_count_sms = fields.Integer(compute="_compute_mailing_list_statistics", string="SMS Contacts")
 
-    def action_view_contacts(self):
+    def action_view_mailings(self):
         if self.env.context.get('mailing_sms'):
-            action = self.env["ir.actions.actions"]._for_xml_id("mass_mailing_sms.mailing_contact_action_sms")
-            action['domain'] = [('list_ids', 'in', self.ids)]
-            context = dict(self.env.context, search_default_filter_valid_sms_recipient=1, default_list_ids=self.ids)
-            action['context'] = context
+            action = self.env["ir.actions.actions"]._for_xml_id('mass_mailing_sms.mailing_mailing_action_sms')
+            action['domain'] = [('id', 'in', self.mailing_ids.ids)]
+            action['context'] = {
+                'default_mailing_type': 'sms',
+                'default_contact_list_ids': self.ids,
+                'mailing_sms': True
+            }
             return action
-        return super(MailingList, self).action_view_contacts()
+        else:
+            return super(MailingList, self).action_view_mailings()
+
+    def action_view_contacts_sms(self):
+        action = self.action_view_contacts()
+        action['context'] = dict(action.get('context', {}), search_default_filter_valid_sms_recipient=1)
+        return action
+
+    def _get_contact_statistics_fields(self):
+        """ See super method docstring for more info.
+        Adds:
+        - contact_count_sms:           all valid sms
+        - contact_count_blacklisted:   override the dict entry to add SMS blacklist condition """
+
+        values = super(MailingList, self)._get_contact_statistics_fields()
+        values.update({
+            'contact_count_sms': '''
+                SUM(CASE WHEN
+                    (c.phone_sanitized IS NOT NULL
+                    AND COALESCE(r.opt_out,FALSE) = FALSE
+                    AND bl_sms.id IS NULL)
+                THEN 1 ELSE 0 END) AS contact_count_sms''',
+            'contact_count_blacklisted': f'''
+                SUM(CASE WHEN (bl.id IS NOT NULL OR bl_sms.id IS NOT NULL)
+                THEN 1 ELSE 0 END) AS contact_count_blacklisted'''
+        })
+        return values
+
+    def _get_contact_statistics_joins(self):
+        return super(MailingList, self)._get_contact_statistics_joins() + '''
+            LEFT JOIN phone_blacklist bl_sms ON c.phone_sanitized = bl_sms.number and bl_sms.active
+        '''
