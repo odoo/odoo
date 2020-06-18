7edeea48a66ec09889b37eabc409a24e8d4b2791
Katherine Zaoral <kz@adhoc.com.ar>
2020-06-18 19:57:26 +0000

    [IMP] l10n_ar: Argentinean invoice report
    Create custom report for Argentinean to follow legal requirements that
    will be suitable for pre-printed invoice, oline invoices and a base
    template for the electronic invoice reports:
    
    * create new custom templates for the report, one for the invoice and
      another for the invoice with payments report
    * extend l10n_latam_invoice_document new generic method to be able to
      print custom invoice report for Argentinean.
    * update demo data to show differences on the custom layouts
    * Fix l10n_ar_afip_start_date field string to match the same as the
      one in the report.
    * update translations
    
    X-original-commit: c4bb2556cc26bf69e02a8c8f28d68bf3cf0a15dc

--- addons/l10n_ar/models/account_move.py
+++ addons/l10n_ar/models/account_move.py
@@ -214,3 +214,13 @@ class AccountMove(models.Model):
                 param['l10n_latam_document_type_ids'] = tuple(self.l10n_latam_document_type_id.search(
                     [('l10n_ar_letter', '=', self.l10n_latam_document_type_id.l10n_ar_letter)]).ids)
         return where_string, param
+
+    def _get_name_invoice_report(self, report_xml_id):
+        self.ensure_one()
+        if self.l10n_latam_use_documents and self.company_id.country_id.code == 'AR':
+            custom_report = {
+                'account.report_invoice_document_with_payments': 'l10n_ar.report_invoice_document_with_payments',
+                'account.report_invoice_document': 'l10n_ar.report_invoice_document',
+            }
+            return custom_report.get(report_xml_id) or report_xml_id
+        return super()._get_name_invoice_report(report_xml_id)
