7c1dfb806a1d4312292b2885d4b0b9eb0a8d5d25
stefanorigano (SRI) <sri@odoo.com>
2020-06-19 08:55:41 +0000

    [IMP] website, *: add off-canvas and other hamburger menu options
    *: web, portal
    
    Part of https://github.com/odoo/odoo/pull/56427
    task-2264627
    
    X-original-commit: 89bf0d4170f42b40cd46915c7d0160c4818f3b8f
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/website/models/theme_models.py
+++ addons/website/models/theme_models.py
@@ -184,8 +184,8 @@ class Theme(models.AbstractModel):
 
         # Reinitialize header templates
         self.disable_view('website.template_header_hamburger')
-        self.disable_view('website.template_header_hamburger_left')
-        self.disable_view('website.template_header_navbar_text_center')
+        self.disable_view('website.template_header_vertical')
+        self.disable_view('website.option_header_hamburger_center_logo')
 
         # Reinitialize footer templates
         self.enable_view('website.footer_custom')
