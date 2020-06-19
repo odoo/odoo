15517f94f4eaec1d7247e1c5404f5797ff915a9c
stefanorigano (SRI) <sri@odoo.com>
2020-06-19 10:55:41 +0200

    [IMP] website, *: add off-canvas and other hamburger menu options
    *: portal
    
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/website/static/src/js/editor/snippets.options.js
+++ addons/website/static/src/js/editor/snippets.options.js
@@ -1668,6 +1668,15 @@ options.registry.collapse = options.Class.extend({
 
 options.registry.Header = options.registry.BackgroundImage.extend({
 
+    /**
+     * @override
+     */
+    async updateUI() {
+        await this._super(...arguments);
+        const isHamburger = this.$('.navbar-toggler').is(':visible');
+        this.$el.find('#hamburger_opt_label').toggleClass('d-none', isHamburger);
+    },
+
     //--------------------------------------------------------------------------
     // Private
     //--------------------------------------------------------------------------
