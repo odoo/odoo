ff4e4e4f00f65f834c74ed732595a87248ee6633
Joseph Caburnay <jcb@odoo.com>
2020-06-22 11:44:36 +0200

    [FIX] point_of_sale,pos_*: indeterministic tour tests failures

--- addons/pos_restaurant/static/tests/tours/ControlButtons.tour.js
+++ addons/pos_restaurant/static/tests/tours/ControlButtons.tour.js
@@ -16,16 +16,16 @@ odoo.define('pos_restaurant.tour.ControlButtons', function (require) {
 
     // Test TransferOrderButton
     FloorScreen.do.clickTable('T2');
-    ProductScreen.exec.addOrderline('Water', '5.0', '2.0');
+    ProductScreen.exec.addOrderline('Water', '5', '2', '10.0');
     ProductScreen.do.clickTransferButton();
     FloorScreen.do.clickTable('T4');
-    ProductScreen.do.clickOrderline('Water', '5.0', '2.0');
+    ProductScreen.do.clickOrderline('Water', '5', '2');
     Chrome.do.backToFloor();
     FloorScreen.do.clickTable('T2');
     ProductScreen.check.orderIsEmpty();
     Chrome.do.backToFloor();
     FloorScreen.do.clickTable('T4');
-    ProductScreen.do.clickOrderline('Water', '5.0', '2.0');
+    ProductScreen.do.clickOrderline('Water', '5', '2');
 
     // Test SplitBillButton
     ProductScreen.do.clickSplitBillButton();
@@ -36,9 +36,8 @@ odoo.define('pos_restaurant.tour.ControlButtons', function (require) {
     TextAreaPopup.check.isShown();
     TextAreaPopup.do.inputText('test note');
     TextAreaPopup.do.clickConfirm();
-    ProductScreen.check.orderlineHasNote('Water', '5.0', 'test note');
-    ProductScreen.exec.addOrderline('Water', '8', '1.1');
-    ProductScreen.check.selectedOrderlineHas('Water', '8', '8.80');
+    ProductScreen.check.orderlineHasNote('Water', '5', 'test note');
+    ProductScreen.exec.addOrderline('Water', '8', '1', '8.0');
 
     // Test PrintBillButton
     ProductScreen.do.clickPrintBillButton();
