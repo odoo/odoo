f51dba10080b37b65ed245893b17717392f9aa20
Thibault Delavallée <tde@odoo.com>
2020-06-24 17:02:12 +0200

    [IMP] website_event_track: prepare some views updates
    RATIONALE
    
    Events are sometimes completely online, trying to gather a community. In this
    merge we improve Event application to better support full-online events:
    better tracks management, wishlists, streaming, chat rooms, revamped
    design, ... Purpose is to improve a lot user experience when being online.
    
    PURPOSE
    
    Add some missing bits to ease inheritance and new menus / views addition
    for Online Event support.
    
    MERGEUP: event(_track): prepare views: resequence menus + standard view improvement
    
    + [IMP] event: improve tag views
    
    + add anchors
    
    LINKS
    
    Task ID 2252655 (Main Online Event task)
    Task ID 2283796 (Event B2Basics))

--- addons/website_event_track/__manifest__.py
+++ addons/website_event_track/__manifest__.py
@@ -17,6 +17,7 @@
         'views/event_track_views.xml',
         'views/event_sponsor_views.xml',
         'views/event_views.xml',
+        'views/event_menus.xml',
     ],
     'demo': [
         'data/res_partner_demo.xml',
