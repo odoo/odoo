3488109e64760c75bb51305770346122a107108b
Thibault Delavallée <tde@odoo.com>
2020-06-24 14:07:13 +0200

    [IMP] website_event(_track)_online: improve registration flow
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge we
    improve Event application to better support full-online events with improved
    tracks, wishlists, chat rooms, .
    
    PURPOSE
    
    Improve the overall registration flow. In addition to already improved features
    (email, standard pages), some improvements are done here specifically for
    Online feature.
    
    SPECIFICATIONS: REGISTER MENU
    
    Add a register button to the right that can be removed through an option on
    the event itself. Currently register menu is lost within other event menus.
    In this commit we add a CTA to the right lf event naviguation in order to show
    it to customers. If current user has registered this button is not displayed.
    
    This is done in addition to already existing register menu. This one can be
    tweaked through frontend menu edition. CTA has another purpose, bringing
    people to register through a visible CTA.
    
    SPECIFICATIONS: REGISTRATION CONFIMATION
    
    At the end of registration process, add a link to the talks. It allows to
    send people directly to the talk page in order to complete their wishlist.
    
    SPECIFICATIONS: IS PARTICIPATING
    
    Make "is participating" field functionally working for public users. It is now
    computed based on visitor. If a visitor is available check its registrations
    in order to determine if current user has registered to the event.
    
    Remove support of 'email' based computation of "is participating" as this is
    quite unsafe: partner and visitor should give results interesting enough.h.
    
    LINKS
    
    Community PR #53540
    Enterprise PR odoo/enterprise#11384
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283796 (Event B2Basics / Registration Flow)
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

--- addons/website_event_online/__manifest__.py
+++ addons/website_event_online/__manifest__.py
@@ -18,9 +18,11 @@
         'views/event_event_views.xml',
         'views/event_registration_views.xml',
         'views/website_visitor_views.xml',
+        'views/event_templates_event.xml',
     ],
     'demo': [
         'data/website_visitor_demo.xml',
+        'data/event_demo.xml',
         'data/event_registration_demo.xml',
     ],
     'application': False,
