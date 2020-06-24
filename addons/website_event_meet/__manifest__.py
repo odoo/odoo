e4fd523b17f5bd49202ca1321f458ba54a00e5b6
Thibault Delavallée <tde@odoo.com>
2020-06-24 08:19:26 +0000

    [ADD] website_event_*: add Online Event support base modules
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge we
    improve Event application to better support full-online events with improved
    tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    Add base modules for some incoming Online Event features.
    
    SPECIFICATIONS
    
    This commit simply adds modules to be completed while working on new Online
    Event feature. This is mainly done to ease future work as Online Event was
    developed in a stable release, leading to some restrictions for pre-merging
    and organizing commits.
    
    Containing :
    
      * website_jitsi: allow to manage jitsi-based chat rooms, with widget to
        integrate into website pages;
      * website_event_online: website_event upgrades required to support online
        event;
      * website_event_track_online: website_event_track upgrades required to
        support online tracks;
      * website_event_meet: chat and discussions rooms online, allowing to gather
        a community online in an event;
      * website_event_track_exhibitor: sponsors -> exhibitors upgrade with chat
        capabilities, new list and page views;
      * website_event_track_session: new session-based tracks display and
        management, new list and page views;
      * website_event_track_live: support youtube and streaming on tracks, as well
        as improved next video suggestion;
      * website_event_track_quiz: add gamification tools on tracks. Allow to gain
        points with quiz on talks;
    
    As well as some bridge modules
    
      * website_event_meet_quiz: integrate leaderboard in community rooms;
      * website_event_track_live_quiz: integrate quiz and streaming;
    
    Future commits will fill those modules
    
    LINKS
    
    Community PR #53540
    Enterprise PR odoo/enterprise#11384
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283796 (Event B2Basics / Registration Flow)
    Task ID-2299857 (Event Design Review)
    Task ID-2284043 (Visitor-based track wishlist)
    Task ID-2283869 (Notify attendees by push)
    Task ID-2283738 (Sponsors -> Exhibitors upgrade)
    Task ID-2283735 (Talks -> Session upgrade)
    Task ID-2283771 (Quizzes on Talks, Leaderboard)
    Task ID-2283742 (Community gathering on chat rooms)
    Task ID-2291344 (Integrate Twitter Wall)
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Quentin Mourier <qmo@odoo.com>
    Co-Authored-By: Stéphane Debauche <std@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

---
+++ addons/website_event_meet/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Event Meeting / Rooms',
+    'category': 'Marketing/Events',
+    'sequence': 1002,
+    'version': '1.0',
+    'summary': 'Event: meeting and chat rooms',
+    'website': 'https://www.odoo.com/page/events',
+    'description': "",
+    'depends': [
+        'website_event_online',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': False,
+    'installable': True,
+}
---
+++ addons/website_jitsi/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    "name": "Website Jitsi",
+    'category': 'Hidden',
+    'version': '1.0',
+    "summary": "Create Jitsi room on website.",
+    'website': 'https://www.odoo.com/page/events',
+    "description": "Create Jitsi room on website.",
+    "depends": [
+        "website"
+    ],
+    "data": [
+    ],
+    'application': False,
+}
