d0c69c61cc8f0d17fe3fd6bd4b8e131012441183
Thibault Delavallée <tde@odoo.com>
2020-06-24 10:19:26 +0200

    [ADD] website_event_*: add Online Event support base modules
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge
    we improve Event application to better support full-online events with
    improved tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    SPECIFICATIONS
    
    This commit simply adds modules to be completed while working on new Online
    Event feature. This is mainly done to ease future work as Online Event was
    developed in a stable release, leading to some restrictions for pre-merging
    and organizing commits.
    
    Containing
    
      * gamification_quiz: quizzes to add gamification tools to your modules. It
        will be used to ask questions after talks;
      * website_event_online: preparatory stuff to support online event;
      * website_event_meet: chat and discussions rooms online;
      * website_event_track_online: preparatory stuff to support online tracks;
      * website_event_track_exhibitor: exhibitors on event;
      * website_event_track_session: live sessions on tracks;
      * website_event_track_live: youtube / live support tracks;
      * website_event_track_quiz: quiz on tracks;
    
    LINKS
    
    Task ID 2252655 (Main Online Event task)
    Task ID 2283796 (Event B2Basics)
    Task ID 2284043 (Visitor-based track wishlist)
    Task ID 2283738 (Sponsors -> Exhibitors upgrade)
    Task ID 2283735 (Tracks -> Sessions upgrade)
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

---
+++ addons/website_event_track_session/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Event Live Sessions',
+    'category': 'Marketing/Events',
+    'sequence': 1005,
+    'version': '1.0',
+    'summary': 'Event: live session on tracks',
+    'website': 'https://www.odoo.com/page/events',
+    'description': "",
+    'depends': [
+        'website_event_track_online',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': False,
+    'installable': True,
+}
