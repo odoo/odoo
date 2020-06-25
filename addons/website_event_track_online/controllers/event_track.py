b1b126c3a5cc86b52bd485190e71ac138ff61c1b
David Beguin <dbe@odoo.com>
2020-06-25 10:05:18 +0200

    [IMP] website_event_track_online : add wishlist mechanism on event tracks
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge we
    improve Event application to better support full-online events with improved
    tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    Allow visitors and logged users to wishlist tracks and link tracks to visitors.
    Purpose is to improve engagement and promote tracks before an event.
    
    SPECIFICATIONS: TRACK WISHLIST
    
    Add a link between visitors or partners, and tracks. This link is done in a new
    many2many relationship build on event.track.visitor new model. It is used to
    store the wishlist flag. That way even public users can wishlist a track
    using their visitor.
    
    At login, information is propagated to their partner. It means that wishlist
    are saved if customer logs in or registers.
    
    We also add a widget for frontend pages. It allows to toggle the wishlist flag
    on tracks. It is based on visitor and/or current user, leading to a complete
    flow even for anonymous people.
    
    We allow to set some tracks as "keynotes". Those are considered as wishlisted
    by default. In order to avoid storing too much information, we do not create an
    entry in the wishlist table for each attendee. We simply consider all event
    attendees to have wishlisted this track by default. In order to be able to mute
    a keynote, a blacklist mechanism is implemented. Using the same relationship
    table between visitors or partners and tracks, a flag is added that blacklists
    the track.
    
    Fields are added on visitor allowing to dynamically build a domain to find
    visitors that wishlisted a track (or did not blacklist list in case of default
    wishlist). This could be used for example to automatically contact attendees
    before a track, like what is done in enterprise counterpart of this merge.
    
    LINKS
    
    Community PR #53540
    Enterprise PR odoo/enterprise#11384
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283796 (Event B2Basics / Registration Flow
    Task ID-2284043 (Visitor-based track wishlist)
    Task ID-2283869 (Notify attendees by push)
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

---
+++ addons/website_event_track_online/controllers/event_track.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from werkzeug.exceptions import Forbidden, NotFound
+
+from odoo import exceptions, http
+from odoo.http import request
+
+
+class WebsiteEventTrackController(http.Controller):
+
+    def _fetch_track(self, track_id, allow_is_accepted=False):
+        track = request.env['event.track'].browse(track_id).exists()
+        if not track:
+            raise NotFound()
+        try:
+            track.check_access_rights('read')
+            track.check_access_rule('read')
+        except exceptions.AccessError:
+            track_sudo = track.sudo()
+            if allow_is_accepted and track_sudo.is_accepted:
+                track = track_sudo
+            else:
+                raise Forbidden()
+
+        event = track.event_id
+        # JSON RPC have no website in requests
+        if hasattr(request, 'website_id') and not event.can_access_from_current_website():
+            raise NotFound()
+        try:
+            event.check_access_rights('read')
+            event.check_access_rule('read')
+        except exceptions.AccessError:
+            raise Forbidden()
+
+        return track
+
+    @http.route("/event/track/toggle_reminder", type="json", auth="public", website=True)
+    def track_reminder_toggle(self, track_id, set_reminder_on):
+        """ Set a reminder a track for current visitor. Track visitor is created or updated
+        if it already exists. Exception made if un-wishlisting and no track_visitor
+        record found (should not happen unless manually done).
+
+        :param boolean set_reminder_on:
+          If True, set as a wishlist, otherwise un-wishlist track;
+          If the track is a Key Track (wishlisted_by_default):
+            if set_reminder_on = False, blacklist the track_partner
+            otherwise, un-blacklist the track_partner
+        """
+        track = self._fetch_track(track_id, allow_is_accepted=True)
+        force_create = set_reminder_on or track.wishlisted_by_default
+        event_track_partner = track._get_event_track_visitors(force_create=force_create)
+        visitor_sudo = event_track_partner.visitor_id
+
+        if not track.wishlisted_by_default:
+            if not event_track_partner or event_track_partner.is_wishlisted == set_reminder_on:  # ignore if new state = old state
+                return {'error': 'ignored'}
+            event_track_partner.is_wishlisted = set_reminder_on
+        else:
+            if not event_track_partner or event_track_partner.is_blacklisted != set_reminder_on:  # ignore if new state = old state
+                return {'error': 'ignored'}
+            event_track_partner.is_blacklisted = not set_reminder_on
+
+        result = {'reminderOn': set_reminder_on}
+        if request.httprequest.cookies.get('visitor_uuid', '') != visitor_sudo.access_token:
+            result['visitor_uuid'] = visitor_sudo.access_token
+
+        return result
