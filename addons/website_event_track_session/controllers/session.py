cdbe78fafda7392ce1fbe317441034ffb3a61cd9
Thibault Delavallée <tde@odoo.com>
2020-06-25 09:11:27 +0200

    [ADD] website_event_track_session: new session mode for tracks
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbea@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>
    
    [IMP] website_event_track_session: apply display-timer widget on tracks CTA button
    
    This commit applies the display-timer widget to the tracks CTA buttons, if any.
    A Pre timer is configured to annonounce the start of the talk, if the visitor
    visit the page of the talk a bit to soon.
    The main timer depends from the track.website_cta_start_remaining.
    At the end of the main timer, the CTA button is displayed to the visitor.

---
+++ addons/website_event_track_session/controllers/session.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from ast import literal_eval
+from werkzeug.exceptions import Forbidden, NotFound
+
+from odoo import exceptions, http
+from odoo.addons.website_event_track.controllers.main import WebsiteEventTrackController
+from odoo.http import request
+from odoo.osv import expression
+from odoo.tools import is_html_empty
+
+
+class WebsiteEventSessionController(WebsiteEventTrackController):
+
+    def _get_event_tracks_base_domain(self, event):
+        search_domain_base = [
+            ('event_id', '=', event.id),
+        ]
+        if not request.env.user.has_group('event.group_event_user'):
+            search_domain_base = expression.AND([search_domain_base, [('is_published', '=', True)]])
+        return search_domain_base
+
+    # ------------------------------------------------------------
+    # MAIN PAGE
+    # ------------------------------------------------------------
+
+    @http.route([
+        '''/event/<model("event.event"):event>/track''',
+        '''/event/<model("event.event"):event>/track/tag/<model("event.track.tag"):tag>'''
+    ], type='http', auth="public", website=True, sitemap=False)
+    def event_tracks(self, event, tag=None, **searches):
+        if not event.can_access_from_current_website():
+            raise NotFound()
+
+        return request.render(
+            "website_event_track.tracks",
+            self._event_tracks_get_values(event, tag=tag, **searches)
+        )
+
+    def _event_tracks_get_values(self, event, tag=None, **searches):
+        # init and process search terms
+        searches.setdefault('search', '')
+        searches.setdefault('tags', '')
+        search_domain = self._get_event_tracks_base_domain(event)
+
+        # search on content
+        if searches.get('search'):
+            search_domain = expression.AND([
+                search_domain,
+                [('name', 'ilike', searches['search'])]
+            ])
+
+        # search on tags
+        search_tags = self._get_search_tags(searches['tags'])
+        if not search_tags and tag:  # backward compatibility
+            search_tags = tag
+        if search_tags:
+            # Example: You filter on age: 10-12 and activity: football.
+            # Doing it this way allows to only get events who are tagged "age: 10-12" AND "activity: football".
+            # Add another tag "age: 12-15" to the search and it would fetch the ones who are tagged:
+            # ("age: 10-12" OR "age: 12-15") AND "activity: football
+            grouped_tags = dict()
+            for search_tag in search_tags:
+                grouped_tags.setdefault(search_tag.category_id, list()).append(search_tag)
+            search_domain_items = [
+                [('tag_ids', 'in', [tag.id for tag in grouped_tags[group]])]
+                for group in grouped_tags
+            ]
+            search_domain = expression.AND([
+                search_domain,
+                *search_domain_items
+            ])
+
+        # fetch data to display
+        event = event.with_context(tz=event.date_tz or 'UTC')
+        tracks = request.env['event.track'].search(search_domain, order='date asc')
+        tag_categories = request.env['event.track.tag.category'].sudo().search([])
+
+        # organize categories for display: live, soon, ...
+        tracks_live = tracks.filtered(lambda track: track.is_track_live)
+        tracks_soon = tracks.filtered(lambda track: not track.is_track_live and track.is_track_soon)
+        tracks = tracks.sorted(lambda track: track.is_track_done)
+
+        # return rendering values
+        return {
+            # event information
+            'event': event,
+            'main_object': event,
+            # tracks display information
+            'tracks': tracks,
+            'tracks_live': tracks_live,
+            'tracks_soon': tracks_soon,
+            # search information
+            'searches': searches,
+            'search_key': searches['search'],
+            'search_tags': search_tags,
+            'tag_categories': tag_categories,
+            # environment
+            'is_html_empty': is_html_empty,
+            'hostname': request.httprequest.host.split(':')[0],
+        }
+
+    # ------------------------------------------------------------
+    # FRONTEND FORM
+    # ------------------------------------------------------------
+
+    @http.route(['/event/<model("event.event"):event>/track/<model("event.track"):track>'], type='http', auth="public", website=True, sitemap=False)
+    def event_track(self, event, track, **options):
+        if not event.can_access_from_current_website():
+            raise NotFound()
+
+        try:
+            track.check_access_rule('read')
+        except exceptions.AccessError:
+            raise Forbidden()
+        track = track.sudo()
+
+        return request.render(
+            "website_event_track_session.event_track_main",
+            self._event_track_get_values(event, track, **options)
+        )
+
+    def _event_track_get_values(self, event, track, **options):
+        # search for tracks list
+        search_domain_base = self._get_event_tracks_base_domain(event)
+        search_domain_base = expression.AND([
+            search_domain_base,
+            [('id', '!=', track.id)]
+        ])
+        tracks_other = request.env['event.track'].sudo().search(search_domain_base)
+
+        option_widescreen = options.get('widescreen', False)
+        option_widescreen = bool(option_widescreen) if option_widescreen != '0' else False
+
+        return {
+            # event information
+            'event': event,
+            'main_object': track,
+            'track': track,
+            # sidebar
+            'tracks_other': tracks_other,
+            # options
+            'option_widescreen': option_widescreen,
+            # environment
+            'is_html_empty': is_html_empty,
+            'hostname': request.httprequest.host.split(':')[0],
+        }
+
+    # ------------------------------------------------------------
+    # TOOLS
+    # ------------------------------------------------------------
+
+    def _get_search_tags(self, tag_search):
+        # TDE FIXME: make me generic (slides, event, ...)
+        try:
+            tag_ids = literal_eval(tag_search)
+        except Exception:
+            tags = request.env['event.track.tag'].sudo()
+        else:
+            # perform a search to filter on existing / valid tags implicitly
+            tags = request.env['event.track.tag'].sudo().search([('id', 'in', tag_ids)])
+        return tags
---
+++ addons/website_event_track_session/models/event_track.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from datetime import timedelta
+from pytz import utc
+
+from odoo import api, fields, models
+
+
+class EventTrack(models.Model):
+    _inherit = "event.track"
+
+    # Call to action
+    website_cta = fields.Boolean('CTA Button')
+    website_cta_title = fields.Char('CTA Title')
+    website_cta_url = fields.Char('CTA Url')
+    website_cta_delay = fields.Integer('CTA Delay')
+    # time information for track
+    is_track_live = fields.Boolean(
+        'Is Track Live', compute='_compute_track_time_data',
+        help="Track has started and is ongoing")
+    is_track_soon = fields.Boolean(
+        'Is Track Soon', compute='_compute_track_time_data',
+        help="Track will begin soon")
+    is_track_upcoming = fields.Boolean(
+        'Is Track Upcoming', compute='_compute_track_time_data',
+        help="Track is not yet started")
+    is_track_done = fields.Boolean(
+        'Is Track Done', compute='_compute_track_time_data',
+        help="Track is finished")
+    track_start_remaining = fields.Integer(
+        'Minutes before track starts', compute='_compute_track_time_data',
+        help="Remaining time before track starts (seconds)")
+    track_start_relative = fields.Integer(
+        'Minutes compare to track start', compute='_compute_track_time_data',
+        help="Relative time compared to track start (seconds)")
+    # time information for CTA
+    is_website_cta_live = fields.Boolean(
+        'Is CTA Live', compute='_compute_cta_time_data',
+        help="CTA button is available")
+    website_cta_start_remaining = fields.Integer(
+        'Minutes before CTA starts', compute='_compute_cta_time_data',
+        help="Remaining time before CTA starts (seconds)")
+
+    @api.depends('date', 'date_end')
+    def _compute_track_time_data(self):
+        """ Compute start and remaining time for track itself. Do everything in
+        UTC as we compute only time deltas here. """
+        now_utc = utc.localize(fields.Datetime.now().replace(microsecond=0))
+        for track in self:
+            date_begin_utc = utc.localize(track.date, is_dst=False)
+            date_end_utc = utc.localize(track.date_end, is_dst=False)
+            track.is_track_live = date_begin_utc <= now_utc < date_end_utc
+            track.is_track_soon = (date_begin_utc - now_utc).total_seconds() < 30*60 if date_begin_utc > now_utc else False
+            track.is_track_upcoming = date_begin_utc > now_utc
+            track.is_track_done = date_end_utc <= now_utc
+            if date_begin_utc >= now_utc:
+                track.track_start_relative = int((date_begin_utc - now_utc).total_seconds())
+                track.track_start_remaining = track.track_start_relative
+            else:
+                track.track_start_relative = int((now_utc - date_begin_utc).total_seconds())
+                track.track_start_remaining = 0
+
+    @api.depends('date', 'date_end', 'website_cta', 'website_cta_delay')
+    def _compute_cta_time_data(self):
+        """ Compute start and remaining time for track itself. Do everything in
+        UTC as we compute only time deltas here. """
+        now_utc = utc.localize(fields.Datetime.now().replace(microsecond=0))
+        for track in self:
+            if not track.website_cta:
+                track.is_cta_live = track.website_cta_start_remaining = False
+                continue
+
+            date_begin_utc = utc.localize(track.date, is_dst=False) + timedelta(minutes=track.website_cta_delay or 0)
+            date_end_utc = utc.localize(track.date_end, is_dst=False)
+            track.is_cta_live = date_begin_utc <= now_utc <= date_end_utc
+            if date_begin_utc >= now_utc:
+                td = date_begin_utc - now_utc
+                track.website_cta_start_remaining = int(td.total_seconds())
+            else:
+                track.website_cta_start_remaining = 0
