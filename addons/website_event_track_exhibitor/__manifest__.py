0567e9fde44d7ef8b6bddab35a72e90f18460706
Aurélien Warnon <awa@odoo.com>
2020-06-25 15:23:23 +0000

    [ADD] website_event_track_exhibitor: new sponsor display and management
    ATIONALE
    
    Events are sometimes held online, gathering a community. In this merge we
    improve Event application to better support full-online events with improved
    tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    Purpose of this commit is to completely redesign the talks pages and frontend
    flow. Notably better showcase live or upcoming talks, have a usable tracks list,
    provide reworked page view with tracks suggestions, ...
    
    SPECIFICATIONS
    
    Mockups are given in https://docs.google.com/presentation/d/1VSxp2luUvp9xLFq9d1Wy-6IC0mP0NA8vLmh44WE5PCs/edit#slide=id.g891af57392_0_84
    
    Main ideas are
    
      * based on sponsor: exhibitors are upgraded sponsor with chat capabilities;
      * new pages: view for all exhibitors and view for a specific exhibitor;
      * list view: display a cards-based view of exhibitors, ordered by sponsorship
        level. This page allows to filter exhibitors and search on them;
      * page view: new page view of an exhibitor. Two tabs: main content holds chat
        (jitsi-based) and information. Aside tab contains suggestions of exhibitors
        based on a given heuristics;
    
    SPECIFICATIONS: EXHIBITOR
    
    Use sponsor model. Upgrade it with a boolean flag telling it is an exhibitor.
    By default exhibitors have a chat room configured, using the previously-added
    chat.room model using jitsi. Add a catchphrase (slogan) as well as opening
    hour support (hours when jitsi is available for customers).
    
    SPECIFICATIONS: LIST VIEW
    
    Display a cards-based view of exhibitors. Display basic informations: cover
    picture, title, catchphrase, country. When clicking on an exhibitor, behavior
    depends on configuration
    
      * before the event: redirect to /register (if not registered) or display a
        popup telling it is not started yet;
      * during the event: during meeting hours, open page view of exhibitor with
        jitsi-based chat room opened by default (if defined). Outside of opening
        hours, display a popul telling it is closed;
      * after event: display page view of exhibitor, jitsi is closed;
    
    SPECIFICATIONS: PAGE VIEW
    
    Display a main content tab, with exhibitor description, contact information.
    If defined, display the jitsi room. This is the default behavior for exhibitors.
    
    Display an aside tab, with exhibitors suggestions following this heuristics
      * currently opened;
      * same country;
      * same language;
      * finally a random to have an "equivalent wave" randomly given;
    
    In mobile, main content should take the whole screen and aside should be
    collapsed. That way we put emphasis on main content.
    
    LINKS
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283738 (Sponsors -> Exhibitors upgrade)
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

--- addons/website_event_track_exhibitor/__manifest__.py
+++ addons/website_event_track_exhibitor/__manifest__.py
@@ -15,8 +15,18 @@
         'website_jitsi',
     ],
     'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/assets.xml',
+        'views/event_sponsor_views.xml',
+        'views/event_event_views.xml',
+        'views/event_exhibitor_templates_list.xml',
+        'views/event_exhibitor_templates_page.xml',
+        'views/event_type_views.xml',
     ],
     'demo': [
+        'data/event_demo.xml',
+        'data/event_sponsor_demo.xml',
     ],
     'application': False,
     'installable': True,
