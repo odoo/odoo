cc46b78f80dcb8c8601fbb89aa26ba75b512b055
David Beguin <dbe@odoo.com>
2020-06-25 10:05:18 +0200

    [IMP] website_event_track_online : add wishlist mechanism on event tracks
    RATIONALE
    
    PURPOSE
    
    SPECIFICATIONS
    
    This adds a wishlist mechanism on event tracks. Logged in visitors
    (=res.partner) can add some talks to their wishlist in order to prepare the
    event for which they are registrered to.
    
    Some talks can be set as wishlisted by default. Those talks won't be
    wishlistable but the visitors but they will appear as starred on the list
    of talks. It's not possible to unstar those talks.
    
    An intermediate model have been introduced to manage talks participants:
    event.track.partner
    
    This model will be useful not only for the wishlist but also for other type of
    participation to the talk, like taking a quiz linked to that talk, etc.
    (talk's quiz will be added in later commit)
    
    This commit is part of the event online refactoring.
    
    mergeup: track online visitor prout
    
    In this commit, the wishlist widget is reworked to use the concept of reminder
    instead of wishlist properly speaking.
    Users need to be able to un-wishlist key tracks (that are wishlisted by
    default). In order to do that, is_blacklisted field have been added on
    track_partner model.
    
    When the user un-wishlist a key track via the now called reminder widget,
    is_blacklisted is set to True for that track_partner. The user won't receive
    reminder anymore for this track.
    For normal track, when the user set a reminder, is_wishlisted is set to true,
    like it was done before.
    
    LINKS
    
    Task ID 2252655 (master Online Event task)
    Task ID 2284043 (introduce track wishlist)
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

---
+++ addons/website_event_track_online/static/src/js/event_track_reminder.js
@@ -0,0 +1,82 @@
+odoo.define('website_event_track.website_event_track_reminder', function (require) {
+'use strict';
+
+var core = require('web.core');
+var _t = core._t;
+var publicWidget = require('web.public.widget');
+
+publicWidget.registry.websiteEventTrackReminder = publicWidget.Widget.extend({
+    selector: '.o_wetrack_js_reminder',
+    events: {
+        'click': '_onReminderToggleClick',
+    },
+
+    /**
+     * @override
+     */
+    init: function () {
+        this._super.apply(this, arguments);
+        this._onReminderToggleClick = _.debounce(this._onReminderToggleClick, 500, true);
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //-------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {Event} ev
+     */
+    _onReminderToggleClick: function (ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+        var self = this;
+        var $trackLink = $(ev.currentTarget).find('i');
+
+        if (this.reminderOn === undefined) {
+            this.reminderOn = $trackLink.data('reminderOn');
+        }
+
+        this._rpc({
+            route: '/event/track/toggle_reminder',
+            params: {
+                track_id: $trackLink.data('trackId'),
+                set_reminder_on: !this.reminderOn
+            },
+        }).then(function (result) {
+            if (result.error && result.error === 'ignored') {
+                self.displayNotification({
+                    type: 'info',
+                    title: _t('Error'),
+                    message: _.str.sprintf(_t('Unknown issue, please retry')),
+                });
+            } else {
+                self.reminderOn = result.reminderOn;
+                var reminderText = self.reminderOn ? _t('Reminder On') : _t('Set a Reminder');
+                self.$('.o_wetrack_js_reminder_text').text(reminderText);
+                self._updateDisplay();
+                var message = self.reminderOn ? _t('Talk added to your wishlist') : _t('Talk removed from your wishlist');
+                self.displayNotification({
+                    type: 'info',
+                    title: message
+                });
+            }
+        });
+    },
+
+    _updateDisplay: function () {
+        var $trackLink = this.$el.find('i');
+        if (this.reminderOn) {
+            $trackLink.addClass('fa-bell').removeClass('fa-bell-o');
+            $trackLink.attr('title', _t('Reminder On'));
+        } else {
+            $trackLink.addClass('fa-bell-o').removeClass('fa-bell');
+            $trackLink.attr('title', _t('Set a reminder'));
+        }
+    },
+
+});
+
+return publicWidget.registry.websiteEventTrackReminder;
+
+});
