d7e6a4255402eef5158d1dc5b576f954d3b458e7
Katherine Zaoral <kz@adhoc.com.ar>
2020-06-26 12:27:59 -0300

    [ADD] l10n_ar_website_sale: new module
    New module that add argentinean fields to the ecommerce checkout form
    
     * Add fields to the checkout form
       * identification type field
       * afip responsibility type field
     * Add logic to validate the new fields
     * take into account the identification type when validating the VAT in the checkout form
     * add validation of the combinitation between identification type and afip resposibility fields
     * Change label name for the vat field, to show "Number" instead of "VAT number".
     * add demo date to display an argentinean website and show products with taxes
     * add module translations

---
+++ addons/l10n_ar_website_sale/__manifest__.py
@@ -0,0 +1,21 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Argentinean Website',
+    'version': '1.0',
+    'category': 'Localization',
+    'sequence': 14,
+    'author': 'Odoo, ADHOC SA',
+    'description': """Be able to see Identification Type and AFIP Responsibility in ecommerce checkout form.""",
+    'depends': [
+        'website_sale',
+        'l10n_ar',
+    ],
+    'data': [
+        'data/ir_model_fields.xml',
+        'views/templates.xml',
+        'demo/website_demo.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
