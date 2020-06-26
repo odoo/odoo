369c42a61b27420f753b52bbbe0304e09c29c5fb
Nasreddin (bon) <bon@odoo.com>
2020-06-26 14:51:52 +0000

    [FIX] sale: Duplicating a Sales Order with Custom Attributes
    Issue
    
    	- Turn on Sales Product Configurator
    	- Create a product template with an attribute and an attribute value defined as "is custom".
    	- Create a new quote, add the template, and configure the product, entering a custom value.
    	- Save.
    	- Duplicate the Quote
    
    	Custom value missing.
    
    Cause
    
    	Custom value (field product_custom_attribute_value_ids) not duplicated.
    
    Solution
    
    	Add attribute 'copy=True' to field product_custom_attribute_value_ids.
    
    opw-2279933
    
    closes odoo/odoo#53796
    
    X-original-commit: 7ea6798fb8b8b0249f56cf4bc5cab627438cd636
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>
    Signed-off-by: bon-odoo <nboulif@users.noreply.github.com>

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -1143,7 +1143,7 @@ class SaleOrderLine(models.Model):
     product_updatable = fields.Boolean(compute='_compute_product_updatable', string='Can Edit Product', readonly=True, default=True)
     product_uom_qty = fields.Float(string='Ordered Quantity', digits=dp.get_precision('Product Unit of Measure'), required=True, default=1.0)
     product_uom = fields.Many2one('uom.uom', string='Unit of Measure')
-    product_custom_attribute_value_ids = fields.One2many('product.attribute.custom.value', 'sale_order_line_id', string='User entered custom product attribute values')
+    product_custom_attribute_value_ids = fields.One2many('product.attribute.custom.value', 'sale_order_line_id', string='User entered custom product attribute values', copy=True)
 
     # M2M holding the values of product.attribute with create_variant field set to 'no_variant'
     # It allows keeping track of the extra_price associated to those attribute values and add them to the SO line description
