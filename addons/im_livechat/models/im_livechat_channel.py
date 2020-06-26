e88575ac239165ac15efac207e8db64d7ba08201
Thibault Francois <tfr@openerp.com>
2020-06-26 12:09:21 +0000

    [FIX] im_livechat: performance issue with _compute_nbr_channel
    filtered(lambda c: c.channel_message_ids)
    take more than 2 minutes to get executed with a regular user
    due to access_right check for every single message.
    
    Solution:
    use the previous read_group with the domain channel_message_ids != False
    which is equivalent
    
    closes odoo/odoo#54318
    
    X-original-commit: b4dcfc3e273c40c53cbf778152cf38fabb39a096
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/im_livechat/models/im_livechat_channel.py
+++ addons/im_livechat/models/im_livechat_channel.py
@@ -74,10 +74,10 @@ class ImLivechatChannel(models.Model):
 
     @api.depends('channel_ids')
     def _compute_nbr_channel(self):
-        channels = self.env['mail.channel'].search([('livechat_channel_id', 'in', self.ids)])
-        channel_count = dict.fromkeys(self.ids, 0)
-        for channel in channels.filtered(lambda c: c.channel_message_ids):
-            channel_count[channel.livechat_channel_id.id] += 1
+        data = self.env['mail.channel'].read_group([
+            ('livechat_channel_id', 'in', self._ids),
+            ('channel_message_ids', '!=', False)], ['__count'], ['livechat_channel_id'], lazy=False)
+        channel_count = {x['livechat_channel_id'][0]: x['__count'] for x in data}
         for record in self:
             record.nbr_channel = channel_count.get(record.id, 0)
 
