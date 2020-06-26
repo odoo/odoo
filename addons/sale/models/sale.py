1ddbdafd5932f71f97a84a6e2a0c1c3ecf4db8ed
Damien Bouvy <dbo@odoo.com>
2020-06-26 12:37:59 +0000

    [FIX] sale: set and propagate payment type in payment flow
    The payment.transaction type field allows filtering some potential
    payment methods down the line of a transaction being created; e.g. if a
    subscription product is present in a quote, the payment's type should be
    `form_save` to save the payment token for future use on the subscription
    that will be created at confirmation.
    
    The transaction that got created by sale flows did not set the type of
    the transaction correctly, and this value was not propagated to the
    rendering values of the acquirer. It now is.
    
    This is necessary as part of task 2275051, where we need to know which
    payment methods to enable in Stripe depending on a need for tokenization
    or not (since some methods are not tokenizable).
    
    closes odoo/odoo#54426
    
    X-original-commit: 09dbbd77d83332c48f62b1c36c3b33ddbfa3a4eb
    Signed-off-by: Damien Bouvy (dbo) <dbo@odoo.com>

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -972,6 +972,7 @@ Reason(s) of this behavior could be:
             'currency_id': currency.id,
             'partner_id': partner.id,
             'sale_order_ids': [(6, 0, self.ids)],
+            'type': self[0]._get_payment_type(),
         })
 
         transaction = self.env['payment.transaction'].create(vals)
