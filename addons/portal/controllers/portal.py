9e0457953af1464b7b7178f3cd508fa01b057edc
Katherine Zaoral <kz@adhoc.com.ar>
2020-06-26 12:27:14 -0300

    [IMP] portal: separate vat validation into a new method

--- addons/portal/controllers/portal.py
+++ addons/portal/controllers/portal.py
@@ -295,6 +295,20 @@ class CustomerPortal(Controller):
             error_message.append(_('Invalid Email! Please enter a valid email address.'))
 
         # vat validation
+        error, error_message = self._vat_validation(data, error, error_message)
+
+        # error message for empty required fields
+        if [err for err in error.values() if err == 'missing']:
+            error_message.append(_('Some required fields are empty.'))
+
+        unknown = [k for k in data if k not in self.MANDATORY_BILLING_FIELDS + self.OPTIONAL_BILLING_FIELDS]
+        if unknown:
+            error['common'] = 'Unknown field'
+            error_message.append("Unknown field '%s'" % ','.join(unknown))
+
+        return error, error_message
+
+    def _vat_validation(self, data, error, error_message):
         partner = request.env.user.partner_id
         if data.get("vat") and partner and partner.vat != data.get("vat"):
             if partner.can_edit_vat():
@@ -312,16 +326,6 @@ class CustomerPortal(Controller):
                         error["vat"] = 'error'
             else:
                 error_message.append(_('Changing VAT number is not allowed once document(s) have been issued for your account. Please contact us directly for this operation.'))
-
-        # error message for empty required fields
-        if [err for err in error.values() if err == 'missing']:
-            error_message.append(_('Some required fields are empty.'))
-
-        unknown = [k for k in data if k not in self.MANDATORY_BILLING_FIELDS + self.OPTIONAL_BILLING_FIELDS]
-        if unknown:
-            error['common'] = 'Unknown field'
-            error_message.append("Unknown field '%s'" % ','.join(unknown))
-
         return error, error_message
 
     def _document_check_access(self, model_name, document_id, access_token=None):
