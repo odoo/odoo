cfb0ee9eef9ce6a542db3b5a3e4863f0917fd083
Cocographique <nmi@odoo.com>
2020-06-26 15:19:19 +0000

    [IMP] website: review and improve footer template choices
    Part of https://github.com/odoo/odoo/pull/56427
    task-2264627
    
    X-original-commit: 34ed29cbac80180982c9acc86c1412433bb88919

--- addons/website/models/theme_models.py
+++ addons/website/models/theme_models.py
@@ -189,11 +189,13 @@ class Theme(models.AbstractModel):
 
         # Reinitialize footer templates
         self.enable_view('website.footer_custom')
-        self.disable_view('website.template_footer_logo_about_us_below')
-        self.disable_view('website.template_footer_links_address_logo')
-        self.disable_view('website.template_footer_name_logo_links_about_us')
-        self.disable_view('website.template_footer_logo_only')
-        self.disable_view('website.template_footer_address_logo')
+        self.disable_view('website.template_footer_descriptive')
+        self.disable_view('website.template_footer_centered')
+        self.disable_view('website.template_footer_links')
+        self.disable_view('website.template_footer_minimalist')
+        self.disable_view('website.template_footer_contact')
+        self.disable_view('website.template_footer_call_to_action')
+        self.disable_view('website.template_footer_headline')
 
         # Reinitialize footer scrolltop template
         self.disable_view('website.option_footer_scrolltop')
