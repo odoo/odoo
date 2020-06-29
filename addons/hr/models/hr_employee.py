2050bd95bf6773f0fd3174d13843f763641e00ab
Yannick Tivisse <yti@odoo.com>
2020-06-29 12:18:41 +0000

    [FIX] hr: Don't link another menu_id when lauching an onboarding plan
    closes odoo/odoo#53799
    
    Taskid: 2219194
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr/models/hr_employee.py
+++ addons/hr/models/hr_employee.py
@@ -243,7 +243,12 @@ class HrEmployeePrivate(models.Model):
             vals.update(self._sync_user(user))
             vals['name'] = vals.get('name', user.name)
         employee = super(HrEmployeePrivate, self).create(vals)
-        url = '/web#%s' % url_encode({'action': 'hr.plan_wizard_action', 'active_id': employee.id, 'active_model': 'hr.employee'})
+        url = '/web#%s' % url_encode({
+            'action': 'hr.plan_wizard_action',
+            'active_id': employee.id,
+            'active_model': 'hr.employee',
+            'menu_id': self.env.ref('hr.menu_hr_root').id,
+        })
         employee._message_log(body=_('<b>Congratulations!</b> May I recommend you to setup an <a href="%s">onboarding plan?</a>') % (url))
         if employee.department_id:
             self.env['mail.channel'].sudo().search([
