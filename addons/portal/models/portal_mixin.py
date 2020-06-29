7c2e8bfdd9381266d12d21376142adc82b9f2b34
Xavier Morel <xmo@odoo.com>
2020-06-29 08:19:09 +0200

    [IMP] mail: make portal mixin extend mail thread
    The portal mixin overrides _notify_get_groups which comes from
    mail.thread despite not extending it. It's not a huge issue as that's
    a mail.thread hook so nothing untowards would happen, but looking at
    the various users of mail.thread and discussing with tde:
    
    * more or less every model which uses portal mixin also uses
      mail.thread
    * the only model which doesn't do so is `sign.request.item` which
      doesn't have a specific reason *not* to use mail.thread
    * according to tde: "pour le bien portal devrait imposer mail thread"

--- addons/portal/models/portal_mixin.py
+++ addons/portal/models/portal_mixin.py
@@ -7,6 +7,7 @@ from odoo import api, exceptions, fields, models, _
 
 class PortalMixin(models.AbstractModel):
     _name = "portal.mixin"
+    _inherit = "mail.thread"
     _description = 'Portal Mixin'
 
     access_url = fields.Char(
