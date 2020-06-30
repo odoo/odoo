9a16d650fae5a77abf0d262f59dc91e031845982
Aurélien Warnon <awa@odoo.com>
2020-06-30 12:29:03 +0000

    [ADD] website_event_track_live: configure Youtube videos / streams on event.tracks
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge we
    improve Event application to better support full-online events with improved
    tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    Allow to include a youtube video / live stream on event tracks and update the
    interface to show "online" capabilities.
    
    SPECIFICATIONS
    
    This commit improves the existing event.tracks to include the possibility to
    configure a Youtube live stream or video.
    
    The frontend view of the track was adapted accordingly to show the stream /
    video instead of the track description when available.
    
    At the end of the video, the track view will automatically suggest a next track
    to the attendee in order to keep him looping through interesting tracks.
    Those suggestions are based on an algorithm that tries to select the track that
    would be the most interesting to this attendee based on his wishlist, similar
    tags with current track, same room, ...
    
    In addition, we automatically fallback to a Youtube thumbnail for tracks
    website image when available.
    
    We support two youtube modes
    
      * streaming: start and end of video is detected based on youtube API;
      * replay: boolean flag on track, telling the video is a static one, allowing
        to know by advance the end of video, and to pre-compute thumbnail and give
        early access if necessary;
    
    KNOWN LIMITATIONS
    
    Tracks do not hold any protection for payment-based tickets. Everything that
    is published is available using standard ACLs. Content protection based on
    tickets is currently not supported.
    
    LINKS
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283735 (Talks -> Session upgrade)
    Task ID-2283729 (Live/Youtube Tracks)
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

--- addons/website_event_track_live/__manifest__.py
+++ addons/website_event_track_live/__manifest__.py
@@ -15,8 +15,16 @@
         'website_event_track_session',
     ],
     'data': [
+        'views/assets.xml',
+        'views/event_track_templates_list.xml',
+        'views/event_track_templates_page.xml',
+        'views/event_track_views.xml',
     ],
     'demo': [
+        'data/event_track_demo.xml'
+    ],
+    'qweb': [
+        'static/src/xml/website_event_track_live_templates.xml'
     ],
     'application': False,
     'installable': True,
