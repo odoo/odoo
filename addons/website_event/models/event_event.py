4b7e08f8063eb6d9a3d08d6c69a246003f9f924f
Thibault Delavallée <tde@odoo.com>
2020-07-01 15:13:36 +0200

    [REF] website_event(_track): ease inheritance of event menus and pages management
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge
    we improve Event application to better support full-online events with
    improved tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    New and improved pages are about to land for online event. However current
    menus and pages management is hard to tweak, especially when dealing with
    the stable policy of 13.3 .
    
    SPECIFICATIONS
    
    Refactor some code, notably
    
      * code dealing with menus to activate and de-activate that is duplicated
        for each kind of menu;
      * allow to track and link a menu_type through inheritance in create_menu
        notably to support website.event.menu model available in Track app and
        unfortunately not in website_event;
      * make a somehow generic way of getting fields dealing with menu items
        through simple inheritance;
    
    Behavior in website_event and website_event_track should be the same. Behavior
    will be tweaked in other modules through inheritance.. In future commits some
    menus and pages will be tweaked a bit, notably about visitor tracking or page
    renaming.
    
    LINKS
    
    Task ID 2252655 (Main Online Event task)
    Task ID 2284043 (Visitor-based track wishlist)

--- addons/website_event/models/event_event.py
+++ addons/website_event/models/event_event.py
@@ -33,8 +33,8 @@ class Event(models.Model):
     # website
     website_published = fields.Boolean(tracking=True)
     website_menu = fields.Boolean(
-        string='Dedicated Menu',
-        compute='_compute_from_event_type', readonly=False, store=True,
+        string='Website Menu',
+        compute='_compute_website_menu', readonly=False, store=True,
         help="Creates menus Introduction, Location and Register on the page "
              "of the event on the website.")
     menu_id = fields.Many2one('website.menu', 'Event Menu', copy=False)
@@ -57,15 +57,14 @@ class Event(models.Model):
                 event.website_url = '/event/%s' % slug(event)
 
     @api.depends('event_type_id')
-    def _compute_from_event_type(self):
+    def _compute_website_menu(self):
         """ Also ensure a value for website_menu as it is a trigger notably for
         track related menus. """
-        super(Event, self)._compute_from_event_type()
         for event in self:
-            if not event.event_type_id and not event.website_menu:
-                event.website_menu = False
-            elif event.event_type_id:
+            if event.event_type_id and event.event_type_id != event._origin.event_type_id:
                 event.website_menu = event.event_type_id.website_menu
+            elif not event.website_menu:
+                event.website_menu = False
 
     @api.model
     def create(self, vals):
@@ -74,12 +73,10 @@ class Event(models.Model):
         return res
 
     def write(self, vals):
-        menu_activated = self.filtered(lambda event: event.website_menu)
-        menu_deactivated = self.filtered(lambda event: not event.website_menu)
+        split_tocheck = self._split_for_menu_update()
         res = super(Event, self).write(vals)
-        menu_to_deactivate = menu_activated.filtered(lambda event: not event.website_menu)
-        menu_to_activate = menu_deactivated.filtered(lambda event: event.website_menu)
-        (menu_to_activate | menu_to_deactivate)._update_website_menus()
+        split_to_update = self._get_events_for_menu_update(split_tocheck)
+        self._update_website_menus(split_to_update=split_to_update)
         return res
 
     # ------------------------------------------------------------
@@ -89,9 +86,48 @@ class Event(models.Model):
     def toggle_website_menu(self, val):
         self.website_menu = val
 
+    def _get_menu_update_fields(self):
+        """" Return a list of fields triggering a split of menu to activate /
+        menu to de-activate. Due to saas-13.3 improvement of menu management
+        this is done using side-methods to ease inheritance.
+
+        :return list: list of fields, each of which triggering a menu update
+          like website_menu, website_track, ... """
+        return ['website_menu']
+
+    def _split_for_menu_update(self):
+        """ For each field linked to a menu, get the set of events having this
+        menu activated and de-activated. Purpose is to find those whose value
+        changed and update the underlying menus. """
+        split_tocheck = dict()
+        for fname in self._get_menu_update_fields():
+            activated = self.filtered(lambda event: event[fname])
+            split_tocheck[fname] = {
+                'activated': activated,
+                'deactivated': self - activated,
+            }
+        return split_tocheck
+
+    def _get_events_for_menu_update(self, split_result):
+        """ For each field linked to a menu, get the set of events requiring
+        this menu to be activated or de-activated based on previous recorded
+        value. """
+        split_to_update = dict()
+        for fname in self._get_menu_update_fields():
+            split_to_update[fname] = self.env['event.event']
+            split_to_update[fname] |= split_result[fname]['activated'].filtered(lambda event: not event[fname])
+            split_to_update[fname] |= split_result[fname]['deactivated'].filtered(lambda event: event[fname])
+        return split_to_update
+
     def _get_menu_entries(self):
         """ Method returning menu entries to display on the website view of the
-        event, possibly depending on some options in inheriting modules. """
+        event, possibly depending on some options in inheriting modules.
+
+        Each menu entry is a tuple containing :
+          * name: menu item name
+          * url: if set, url to a route (do not use xml_id in that case);
+          * xml_id: template linked to the page (do not use url in that case);
+        """
         self.ensure_one()
         return [
             (_('Introduction'), False, 'website_event.template_intro'),
@@ -99,7 +135,8 @@ class Event(models.Model):
             (_('Register'), '/event/%s/register' % slug(self), False),
         ]
 
-    def _update_website_menus(self):
+    def _update_website_menus(self, split_to_update=None):
+        """ Synchronize event configuration and its menu entries for frontend. """
         for event in self:
             if event.menu_id and not event.website_menu:
                 event.menu_id.unlink()
@@ -109,19 +146,25 @@ class Event(models.Model):
                 for sequence, (name, url, xml_id) in enumerate(event._get_menu_entries()):
                     event._create_menu(sequence, name, url, xml_id)
 
-    def _create_menu(self, sequence, name, url, xml_id):
+    def _create_menu(self, sequence, name, url, xml_id, menu_type=False, force_track=False):
+        """ If url: create a website menu. Tracking visitors is decided on the
+        template used by this route. If xml_id: create a new page, then link
+        it to a menu.
+
+        :param menu_type: type of menu. Mainly used for inheritance purpose
+          allowing more fine-grain tuning of menus. """
         if not url:
             self.env['ir.ui.view'].search([('name', '=', name + ' ' + self.name)]).unlink()
-            newpath = self.env['website'].sudo().new_page(name + ' ' + self.name, template=xml_id, ispage=False)['url']
+            newpath = self.env['website'].sudo().new_page(name + ' ' + self.name, template=xml_id, ispage=False, force_track=force_track)['url']
             url = "/event/" + slug(self) + "/page/" + newpath[1:]
-        menu = self.env['website.menu'].sudo().create({
+        website_menu = self.env['website.menu'].sudo().create({
             'name': name,
             'url': url,
             'parent_id': self.menu_id.id,
             'sequence': sequence,
             'website_id': self.website_id.id,
         })
-        return menu
+        return website_menu
 
     # ------------------------------------------------------------
     # TOOLS
