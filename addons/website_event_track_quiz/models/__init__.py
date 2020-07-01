a0093de549b8b07877df5dfd49c5023ce783ebfe
qmo-odoo <qmo@odoo.com>
2020-07-01 10:45:10 +0000

    [ADD] website_event_track_quiz: add quiz feature on event.track
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge we
    improve Event application to better support full-online events with improved
    tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    Allow to configure a quiz on event tracks, much like eLearning.
    The quiz is taken at the end of the live stream / track video.
    (Or when the talk is 'live' of 'done').
    
    SPECIFICATIONS
    
    Users can now configure an event.quiz on event.track.
    
    The quiz is a simple collection of event.quiz.questions that in turn contain
    a few event.quiz.answers.
    Only one of the answer is considered as correct and should be configured with
    "awarded_points" greater than 0.
    
    On the event.track frontend page, attendees can take the quiz when the track
    is "live" (or "done") and earn the total amount of configured awarded_points.
    Each quiz can only be taken once (if you fail, you can't try again).
    
    Attendees are then ranked in a leaderboard that is displayed in the "Community"
    section of the event frontend menus.
    The leaderboard allows to see your own position as well as basic search for
    other attendees.
    The points earned during the event are event-specific and will not be shared
    with other instances of event.event.
    
    Points as well as quiz completion are stored in the relationship between a
    visitor and a track (event.track.visitor).
    
    The models are very much based on a similar feature in website_slides, even
    though the implementation in event is not karma based.
    We will probably have to merge the features later into a base gamification_quiz
    module.
    
    LINKS
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283771 (Quizzes on Talks, Leaderboard)
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Quentin Mourier <qmo@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

--- addons/website_event_track_quiz/models/__init__.py
+++ addons/website_event_track_quiz/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import models
-from . import controllers
+from . import event_track
+from . import event_track_visitor
+from . import event_quiz
