446677e8f09b2ec9a5cba5492372d211ced1c9eb
Jorge Pinna Puissant <jpp@odoo.com>
2020-07-02 16:13:25 +0200

    [FIX] mail: avoid changing the user assigned to the activity
    Before this commit, when the activity type is changed, the user assigned
    to is also restarted by default.
    
    Now, if a user is already assigned, the modification of the activity
    type will not change the assigned user.
    
    opw-2265631

--- addons/mail/models/mail_activity.py
+++ addons/mail/models/mail_activity.py
@@ -218,7 +218,7 @@ class MailActivity(models.Model):
             if self.activity_type_id.delay_from == 'previous_activity' and 'activity_previous_deadline' in self.env.context:
                 base = fields.Date.from_string(self.env.context.get('activity_previous_deadline'))
             self.date_deadline = base + relativedelta(**{self.activity_type_id.delay_unit: self.activity_type_id.delay_count})
-            self.user_id = self.activity_type_id.default_user_id or self.env.user
+            self.user_id = self.user_id or self.activity_type_id.default_user_id or self.env.user
             if self.activity_type_id.default_description:
                 self.note = self.activity_type_id.default_description
 
