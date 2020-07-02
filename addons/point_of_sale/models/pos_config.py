dba5937d8b3e5ef753b5bd4cd74f532f5ee4f001
Nicolas Martinelli <nim@odoo.com>
2020-07-02 12:01:17 +0200

    [FIX] point_of_sale: do not block if no COA
    In case the COA is imported in the system, the `chart_template_id` of
    the company remains empty while the accounting is usable. However, in
    this case it's impossible to open a POS session.
    
    We leave the warning in the POS config but avoid blocking the user.
    
    opw-2286640

--- addons/point_of_sale/models/pos_config.py
+++ addons/point_of_sale/models/pos_config.py
@@ -548,9 +548,6 @@ class PosConfig(models.Model):
         """
         self.ensure_one()
         if not self.current_session_id:
-            if not self.company_has_template:
-                raise UserError(_("A Chart of Accounts is not yet installed in your current company. Please install a "
-                                  "Chart of Accounts through the Invoicing/Accounting settings before launching a PoS session." ))
             self._check_company_journal()
             self._check_company_invoice_journal()
             self._check_company_payment()
