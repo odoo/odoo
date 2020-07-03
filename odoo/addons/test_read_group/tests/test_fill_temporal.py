770853e2905acd15524baa08d360572d98cc3b34
Nicolas Lempereur <nle@odoo.com>
2020-07-03 13:40:53 +0000

    [FIX] models.py: group by date with DST change
    When we group by date with DST change within a range, we could get a
    reocrd inside two date range grouping, or inside no grouping.
    
    This is because we computed range just with [+ 1 month], so we possibly
    had these ranges (in UTC):
    
    - October 2019 : [('datetime', '>=', '2019-10-01 02:00:00')
                      ('datetime', '<', '2019-11-01 02:00:00')]
    
    - November 2019 : [('datetime', '>=', '2019-11-01 01:00:00')
                       ('datetime', '<', '2019-12-01 01:00:00')]
    
    So a record on 2019-11-01 01:30:00 would be both inside October and
    November.
    
    This happen because the DST is removed on happen on 27 October 2019 and
    this was not taken into account when computing the end of the range.
    
    With this changeset, for the given example aboth, we will have:
    
    - October 2019 : [('datetime', '>=', '2019-10-01 02:00:00')
                      ('datetime', '<', '2019-11-01 01:00:00')]
    
    Added test without the change fails with "AssertionError: Lists differ"
    because:
    
    - "Q1 2019" finished on 17:00:00 instead of 16:00:00
    - "Q3 2019" finished on 16:00:00 instead of 17:00:00
    
    opw-2278829
    closes #54056
    
    closes odoo/odoo#54186
    
    X-original-commit: df9de06d260b313f0db61f47b7ebdcdd1c9c6cf6
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- odoo/addons/test_read_group/tests/test_fill_temporal.py
+++ odoo/addons/test_read_group/tests/test_fill_temporal.py
@@ -531,6 +531,50 @@ class TestFillTemporal(common.TransactionCase):
 
         self.assertEqual(groups, expected)
 
+    def test_quarter_with_timezones(self):
+        """Test quarter with timezones.
+
+        We group year by quarter and check that it is consistent with timezone.
+        """
+        self.Model.create({'datetime': '2019-01-01 03:30:00', 'value': 2})
+        self.Model.create({'datetime': '2019-12-30 22:30:00', 'value': 3})
+
+        expected = [{
+            '__domain': ['&',
+                ('datetime', '>=', '2018-12-31 17:00:00'),
+                ('datetime', '<', '2019-03-31 16:00:00')],
+            'datetime:quarter': 'Q1 2019',
+            'datetime_count': 1,
+            'value': 2
+        }, {
+            '__domain': ['&',
+                       ('datetime', '>=', '2019-03-31 16:00:00'),
+                       ('datetime', '<', '2019-06-30 16:00:00')],
+            'datetime:quarter': 'Q2 2019',
+            'datetime_count': 0,
+            'value': False
+        }, {
+            '__domain': ['&',
+                       ('datetime', '>=', '2019-06-30 16:00:00'),
+                       ('datetime', '<', '2019-09-30 17:00:00')],
+            'datetime:quarter': 'Q3 2019',
+            'datetime_count': 0,
+            'value': False
+        }, {
+            '__domain': ['&',
+                       ('datetime', '>=', '2019-09-30 17:00:00'),
+                       ('datetime', '<', '2019-12-31 17:00:00')],
+            'datetime:quarter': 'Q4 2019',
+            'datetime_count': 1,
+            'value': 3
+        }]
+
+        model_fill = self.Model.with_context(tz='Asia/Hovd', fill_temporal=True)
+        groups = model_fill.read_group([], fields=['datetime', 'value'],
+                                       groupby=['datetime:quarter'])
+
+        self.assertEqual(groups, expected)
+
     def test_egde_fx_tz(self):
         """We test if different edge effect by using a different timezone from the user context
 
