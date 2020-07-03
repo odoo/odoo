99c0388ad516c7a3d4a95add355f9f82d7c84169
Kevin Baptiste <kba@odoo.com>
2020-07-03 12:09:47 +0000

    [IMP] fleet: usability improvements
     - Fix the display of the licence plat on the kanban view
     - Store the Driver on fleet.vehicle.log.services to remember who was
     the driver at the time the service was done.
     - Rename 'Informations' to 'Information'
     - Rename 'Driver' to 'Current Driver' on the contract
    
    closes odoo/odoo#54051
    
    Taskid: 2289808
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/fleet/models/fleet_vehicle_cost.py
+++ addons/fleet/models/fleet_vehicle_cost.py
@@ -33,7 +33,7 @@ class FleetVehicleLogContract(models.Model):
         help='Date when the coverage of the contract expirates (by default, one year after begin date)')
     days_left = fields.Integer(compute='_compute_days_left', string='Warning Date')
     insurer_id = fields.Many2one('res.partner', 'Vendor')
-    purchaser_id = fields.Many2one(related='vehicle_id.driver_id', string='Driver')
+    purchaser_id = fields.Many2one(related='vehicle_id.driver_id', string='Current Driver')
     ins_ref = fields.Char('Reference', size=64, copy=False)
     state = fields.Selection([
         ('futur', 'Incoming'),
