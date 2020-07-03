ade149b09fb1af67255d5ba5e4927215af3faa5f
Nicolas Lempereur <nle@odoo.com>
2020-07-03 13:40:53 +0000

    [FIX] models.py: group by date with DST change
    When we group by date with DST change within a range, we could get a
    reocrd inside two date range grouping, or inside no grouping.
    
    This is because we computed range just with [+ 1 month], so we possibly
    had these ranges (in UTC):
    
    - October 2019 : [('datetime', '>=', '2019-10-01 02:00:00')
                      ('datetime', '<', '2019-11-01 02:00:00')]
    
    - November 2019 : [('datetime', '>=', '2019-11-01 01:00:00')
                       ('datetime', '<', '2019-12-01 01:00:00')]
    
    So a record on 2019-11-01 01:30:00 would be both inside October and
    November.
    
    This happen because the DST is removed on happen on 27 October 2019 and
    this was not taken into account when computing the end of the range.
    
    With this changeset, for the given example aboth, we will have:
    
    - October 2019 : [('datetime', '>=', '2019-10-01 02:00:00')
                      ('datetime', '<', '2019-11-01 01:00:00')]
    
    Added test without the change fails with "AssertionError: Lists differ"
    because:
    
    - "Q1 2019" finished on 17:00:00 instead of 16:00:00
    - "Q3 2019" finished on 16:00:00 instead of 17:00:00
    
    opw-2278829
    closes #54056
    
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- odoo/models.py
+++ odoo/models.py
@@ -2019,6 +2019,8 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
                     if gb['tz_convert']:
                         tzinfo = range_start.tzinfo
                         range_start = range_start.astimezone(pytz.utc)
+                        # take into account possible hour change between start and end
+                        range_end = tzinfo.localize(range_end.replace(tzinfo=None))
                         range_end = range_end.astimezone(pytz.utc)
 
                     range_start = range_start.strftime(fmt)
