d80bd823afae2047f1a020d522da6486487cfbac
Juan Manuel Romero <jmanuelromero93@gmail.com>
2020-07-04 19:40:13 -0500

    Modulo para chat por whatsapp

---
+++ custom-addons/whatsapp_chat/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Whatsapp Chat',
+    'description': 'Shows a fixed whatsapp button on the website pages',
+    'author': 'Juan Romero',
+    'depends': ['base'],
+    'application': True,
+    'data': [
+        'views/footer.xml',
+        'views/assets.xml'
+    ]
+}
