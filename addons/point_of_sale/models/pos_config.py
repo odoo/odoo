odoo/odoo

commit 393e77df633d1ca05d75ee1b18cc85aa2ff984d9

[REF] point_of_sale: post_install_pos_localisation
Refactoring this method to make it extensible. After this refactoring, we
can now define methods that can apply defaults to the company's pos.config
records during the installation of a localisation.

To do so, we hook methods to the setup_defaults method. The hooked methods
take pos.config records (self) and a company. The pos.config records belong
to the given company.

addons/point_of_sale/models/pos_config.py
@@ -613,45 +613,53 @@ class PosConfig(models.Model):
     # is installed, or if POS is installed on database having companies that already have
     # a localisation installed
     @api.model
-    def post_install_pos_localisation(self):
-        self.assign_payment_journals()
-        self.generate_pos_journal()
-
-    @api.model
-    def assign_payment_journals(self, companies=False):
+    def post_install_pos_localisation(self, companies=False):
         self = self.sudo()
         if not companies:
             companies = self.env['res.company'].search([])
-        for company in companies:
-            if company.chart_template_id:
-                cash_journal = self.env['account.journal'].search([('company_id', '=', company.id), ('type', '=', 'cash')], limit=1)
-                pos_receivable_account = company.account_default_pos_receivable_account_id
-                payment_methods = self.env['pos.payment.method']
-                if cash_journal:
-                    payment_methods |= payment_methods.create({
-                        'name': _('Cash'),
-                        'receivable_account_id': pos_receivable_account.id,
-                        'is_cash_count': True,
-                        'cash_journal_id': cash_journal.id,
-                        'company_id': company.id,
-                    })
+        for company in companies.filtered('chart_template_id'):
+            pos_configs = self.search([('company_id', '=', company.id)])
+            pos_configs.setup_defaults(company)
+
+    def setup_defaults(self, company):
+        """Extend this method to customize the existing pos.config of the company during the installation
+        of a localisation.
+        
+        :param self pos.config: pos.config records present in the company during the installation of localisation.
+        :param company res.company: the single company where the pos.config defaults will be setup.
+        """
+        self.assign_payment_journals(company)
+        self.generate_pos_journal(company)
+
+    def assign_payment_journals(self, company):
+        for pos_config in self:
+            if pos_config.payment_method_ids:
+                continue
+            cash_journal = self.env['account.journal'].search([('company_id', '=', company.id), ('type', '=', 'cash')], limit=1)
+            pos_receivable_account = company.account_default_pos_receivable_account_id
+            payment_methods = self.env['pos.payment.method']
+            if cash_journal:
                 payment_methods |= payment_methods.create({
-                    'name': _('Bank'),
+                    'name': _('Cash'),
                     'receivable_account_id': pos_receivable_account.id,
-                    'is_cash_count': False,
+                    'is_cash_count': True,
+                    'cash_journal_id': cash_journal.id,
                     'company_id': company.id,
                 })
-                existing_pos_config = self.env['pos.config'].search([('company_id', '=', company.id), ('payment_method_ids', '=', False)])
-                existing_pos_config.write({'payment_method_ids': [(6, 0, payment_methods.ids)]})
+            payment_methods |= payment_methods.create({
+                'name': _('Bank'),
+                'receivable_account_id': pos_receivable_account.id,
+                'is_cash_count': False,
+                'company_id': company.id,
+            })
+            pos_config.write({'payment_method_ids': [(6, 0, payment_methods.ids)]})
 
-    @api.model
-    def generate_pos_journal(self, companies=False):
-        self = self.sudo()
-        if not companies:
-            companies = self.env['res.company'].search([])
-        for company in companies:
+    def generate_pos_journal(self, company):
+        for pos_config in self:
+            if pos_config.journal_id:
+                continue
             pos_journal = self.env['account.journal'].search([('company_id', '=', company.id), ('code', '=', 'POSS')])
-            if company.chart_template_id and not pos_journal:
+            if not pos_journal:
                 pos_journal = self.env['account.journal'].create({
                     'type': 'sale',
                     'name': 'Point of Sale',
@@ -659,5 +667,4 @@ class PosConfig(models.Model):
                     'company_id': company.id,
                     'sequence': 20
                 })
-                existing_pos_config = self.env['pos.config'].search([('company_id', '=', company.id), ('journal_id', '=', False)])
-                existing_pos_config.write({'journal_id': pos_journal.id})
+            pos_config.write({'journal_id': pos_journal.id})

addons/point_of_sale/models/chart_template.py
@@ -5,20 +5,6 @@ from odoo import api, models
 class AccountChartTemplate(models.Model):
     _inherit = 'account.chart.template'
 
-    def _create_bank_journals(self, company, acc_template_ref):
-        """
-        Add the payment journals to the existing pos config
-        """
-        journals = super(AccountChartTemplate, self)._create_bank_journals(company, acc_template_ref)
-        self.env['pos.config'].assign_payment_journals(companies=company)
-        return journals
-
-    @api.model
-    def generate_journals(self, acc_template_ref, company, journals_dict=None):
-        res = super(AccountChartTemplate, self).generate_journals(acc_template_ref, company, journals_dict)
-        self.env['pos.config'].generate_pos_journal(companies=company)
-        return res
-
     def _load(self, sale_tax_rate, purchase_tax_rate, company):
         """Remove the payment methods that are created for the company before installing the chart of accounts.
 
@@ -27,4 +13,6 @@ class AccountChartTemplate(models.Model):
         deleted during the loading of chart of accounts.
         """
         self.env['pos.payment.method'].search([('company_id', '=', company.id)]).unlink()
-        return super(AccountChartTemplate, self)._load(sale_tax_rate, purchase_tax_rate, company)
+        result = super(AccountChartTemplate, self)._load(sale_tax_rate, purchase_tax_rate, company)
+        self.env['pos.config'].post_install_pos_localisation(companies=company)
+        return result

commit a0f57b921c1a9ab35a7d65ca227514316d72dade

[FIX] point_of_sale: duplicated payment methods
To reproduce the issue, install pos_restaurant directly without going
thru point_of_sale module (allow demo). Check payment methods and see
the duplicates.

This issue is introduced in 393e77df633d1ca05d75ee1b18cc85aa2ff984d9.
It is caused by repeatedly creating a set of payment methods for each
pos.config.

The fix is to create a set of payment methods once, then assign them to
pos.config's that don't have payment methods yet.

Included in this fix is taking out the queries inside loops because the
queries do not depend on the loop object.

addons/point_of_sale/models/pos_config.py
@@ -625,7 +625,7 @@ class PosConfig(models.Model):
     def setup_defaults(self, company):
         """Extend this method to customize the existing pos.config of the company during the installation
         of a localisation.
-        
+
         :param self pos.config: pos.config records present in the company during the installation of localisation.
         :param company res.company: the single company where the pos.config defaults will be setup.
         """
@@ -634,47 +634,44 @@ class PosConfig(models.Model):
         self.setup_invoice_journal(company)
 
     def assign_payment_journals(self, company):
-        for pos_config in self:
-            if pos_config.payment_method_ids:
-                continue
-            cash_journal = self.env['account.journal'].search([('company_id', '=', company.id), ('type', '=', 'cash')], limit=1)
-            pos_receivable_account = company.account_default_pos_receivable_account_id
-            payment_methods = self.env['pos.payment.method']
-            if cash_journal:
-                payment_methods |= payment_methods.create({
-                    'name': _('Cash'),
-                    'receivable_account_id': pos_receivable_account.id,
-                    'is_cash_count': True,
-                    'cash_journal_id': cash_journal.id,
-                    'company_id': company.id,
-                })
+        cash_journal = self.env['account.journal'].search([('company_id', '=', company.id), ('type', '=', 'cash')], limit=1)
+        pos_receivable_account = company.account_default_pos_receivable_account_id
+        payment_methods = self.env['pos.payment.method']
+        if cash_journal:
             payment_methods |= payment_methods.create({
-                'name': _('Bank'),
+                'name': _('Cash'),
                 'receivable_account_id': pos_receivable_account.id,
-                'is_cash_count': False,
+                'is_cash_count': True,
+                'cash_journal_id': cash_journal.id,
                 'company_id': company.id,
             })
+        payment_methods |= payment_methods.create({
+            'name': _('Bank'),
+            'receivable_account_id': pos_receivable_account.id,
+            'is_cash_count': False,
+            'company_id': company.id,
+        })
+        for pos_config in self.filtered(lambda config: not config.payment_method_ids):
             pos_config.write({'payment_method_ids': [(6, 0, payment_methods.ids)]})
 
     def generate_pos_journal(self, company):
+        existing_pos_journal = self.env['account.journal'].search([('company_id', '=', company.id), ('code', '=', 'POSS')])
+        default_pos_journal = existing_pos_journal or self.env['account.journal'].create({
+            'type': 'sale',
+            'name': 'Point of Sale',
+            'code': 'POSS',
+            'company_id': company.id,
+            'sequence': 20
+        })
         for pos_config in self:
             if pos_config.journal_id:
                 continue
-            pos_journal = self.env['account.journal'].search([('company_id', '=', company.id), ('code', '=', 'POSS')])
-            if not pos_journal:
-                pos_journal = self.env['account.journal'].create({
-                    'type': 'sale',
-                    'name': 'Point of Sale',
-                    'code': 'POSS',
-                    'company_id': company.id,
-                    'sequence': 20
-                })
-            pos_config.write({'journal_id': pos_journal.id})
+            pos_config.write({'journal_id': default_pos_journal.id})
 
     def setup_invoice_journal(self, company):
-        for pos_config in self:
-            invoice_journal_id = pos_config.invoice_journal_id or self.env['account.journal'].search([('type', '=', 'sale'), ('company_id', '=', company.id)], limit=1)
-            if invoice_journal_id:
-                pos_config.write({'invoice_journal_id': invoice_journal_id.id})
+        default_invoice_journal = self.env['account.journal'].search([('type', '=', 'sale'), ('company_id', '=', company.id)], limit=1)
+        for pos_config in self.filtered(lambda config: not config.invoice_journal_id):
+            if default_invoice_journal:
+                pos_config.write({'invoice_journal_id': default_invoice_journal.id})
             else:
                 pos_config.write({'module_account': False})