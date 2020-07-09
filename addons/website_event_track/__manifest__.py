a03554db912f72325709232acf0e83a18a488cb1
Thibault Delavallée <tde@odoo.com>
2020-07-09 13:09:37 +0200

    [IMP] website_event_track: add views for website menu model

--- addons/website_event_track/__manifest__.py
+++ addons/website_event_track/__manifest__.py
@@ -17,6 +17,7 @@
         'views/event_track_views.xml',
         'views/event_sponsor_views.xml',
         'views/event_views.xml',
+        'views/website_event_menu_views.xml',
         'views/event_menus.xml',
     ],
     'demo': [
