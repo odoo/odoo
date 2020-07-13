96da7a8d4e82fdeabe04c87dfded6a89216faad1
Thibault Delavallée <tde@odoo.com>
2020-07-13 15:10:10 +0000

    [ADD] test_event_full: add a test module containing a whole event-online flow
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge we
    improve Event application to better support full-online events with improved
    tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    Have a module to test all those new features.
    
    SPECIFICATIONS
    
    Do as much tests as possible, which will probably be around "no time". At
    least have a base module allowing to add them after main merge.
    
    LINKS
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283796 (Event B2Basics / Registration Flow)
    Task ID-2299857 (Event Design Review)
    Task ID-2284043 (Visitor-based track wishlist)
    Task ID-2283869 (Notify attendees by push)
    Task ID-2283738 (Sponsors -> Exhibitors upgrade)
    Task ID-2283735 (Talks -> Session upgrade)
    Task ID-2283729 (Live/Youtube Tracks)
    Task ID-2283771 (Quizzes on Talks, Leaderboard)
    Task ID-2283742 (Community gathering on chat rooms)
    Task ID-2291344 (Integrate Twitter Wall)

---
+++ addons/test_event_full/tests/test_wevent_register.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import tests
+from odoo.addons.test_event_full.tests.common import TestWEventCommon
+from odoo.tests.common import HOST
+
+
+@tests.common.tagged('post_install', '-at_install')
+class TestWEventRegister(TestWEventCommon):
+
+    def test_register(self):
+        self.browser_js(
+            '/event',
+            'odoo.__DEBUG__.services["web_tour.tour"].run("wevent_register", 100)',
+            'odoo.__DEBUG__.services["web_tour.tour"].tours.wevent_register.ready',
+            login=None
+        )
