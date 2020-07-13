4d0b2d328658ec79d9ce7815646e8cb34ee8e8f7
Thibault Delavallée <tde@odoo.com>
2020-07-13 17:10:10 +0200

    [ADD] test_event_full: complete me update me

---
+++ addons/test_event_full/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Test Full Event Flow',
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """
+This module will test the main event flows of Odoo, both frontend and backend.
+It installs sale capabilities, front-end flow, eCommerce, questions, ...
+""",
+    'depends': [
+        'website_event_online',
+        'website_event_questions',
+        'website_event_meet',
+        'website_event_sale',
+        'website_event_track_online',
+        'website_event_track_exhibitor',
+        'website_event_track_session',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'demo': [
+        'data/event_demo.xml',
+    ],
+    'installable': True,
+}
---
+++ addons/test_event_full/static/src/js/tours/wevent_register_tour.js
@@ -0,0 +1,51 @@
+odoo.define('test_event_full.tour.register', function (require) {
+"use strict";
+
+var tour = require('web_tour.tour');
+
+/**
+ * The purpose of this tour is to check the whole certification flow:
+ *
+ * -> student (= demo user) checks 'on payment' course content
+ * -> clicks on "buy course"
+ * -> is redirected to webshop on the product page
+ * -> buys the course
+ * -> fails 3 times, exhausting his attempts
+ * -> is removed to the members of the course
+ * -> buys the course again
+ * -> succeeds the certification
+ * -> has the course marked as completed
+ * -> has the certification in his user profile
+ *
+ */
+
+
+var initTourSteps = [{
+    content: 'Go on Online TestEvent page',
+    trigger: 'a[href*="/event"]:contains("Online TestEvent"):first',
+}];
+
+var browseSessionsSteps = [{
+    content: 'Browse Sessions',
+    trigger: 'a:contains("Sessions")',
+}, {
+    content: 'Go on "Main Gathering" talk',
+    trigger: 'a:contains("Main Gathering")',
+}];
+
+var registerSteps = [{
+    content: 'Go on Register',
+    trigger: 'li.btn-primary a:contains("Register")',
+}];
+
+tour.register('wevent_register', {
+    url: '/event',
+    test: true
+}, [].concat(
+        initTourSteps,
+        browseSessionsSteps,
+        registerSteps
+    )
+);
+
+});
