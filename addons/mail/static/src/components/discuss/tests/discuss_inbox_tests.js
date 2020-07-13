48e4c5cfa67cd63c5ca06f05a49e7678e61237d5
Xavier Dubuc <xdu@odoo.com>
2020-07-13 14:56:55 +0200

    [FIX] mail: show subject of the message in inbox if set
    task-2280146

--- addons/mail/static/src/components/discuss/tests/discuss_inbox_tests.js
+++ addons/mail/static/src/components/discuss/tests/discuss_inbox_tests.js
@@ -411,6 +411,99 @@ QUnit.test('"reply to" composer should send message if message replied to is not
     assert.verifySteps(['message_post']);
 });
 
+QUnit.test('show subject of message in inbox', async function (assert) {
+    assert.expect(3);
+
+    this.data['mail.message'].records = [{
+        author_id: [7, "Demo"],
+        body: "<p>Test</p>",
+        date: "2019-04-20 11:00:00",
+        id: 100,
+        is_discussion: false,
+        is_notification: false,
+        message_type: 'comment',
+        needaction: true,
+        needaction_partner_ids: [3],
+        model: 'mail.channel',
+        res_id: 20,
+        subject: "Salutations, voyageur",
+    }];
+
+    await this.start({
+        env: {
+            session: {
+                name: 'Admin',
+                partner_display_name: 'Your Company, Admin',
+                partner_id: 3,
+                uid: 2,
+            },
+        },
+    });
+    assert.containsOnce(
+        document.body,
+        '.o_Message',
+        "should display a single message"
+    );
+    assert.containsOnce(
+        document.body,
+        '.o_Message_subject',
+        "should display subject of the message"
+    );
+    assert.strictEqual(
+        document.querySelector('.o_Message_subject').textContent,
+        "Subject: Salutations, voyageur",
+        "Subject of the message should be 'Salutations, voyageur'"
+    )
+});
+
+QUnit.test('show subject of message in history', async function (assert) {
+    assert.expect(3);
+
+    this.data['mail.message'].records = [{
+        author_id: [7, "Demo"],
+        body: "<p>Test</p>",
+        date: "2019-04-20 11:00:00",
+        history_partner_ids: [3],
+        id: 100,
+        message_type: 'comment',
+        model: 'project.task',
+        record_name: "Refactoring",
+        res_id: 20,
+        subject: "Salutations, voyageur",
+    }];
+
+    await this.start({
+        discuss: {
+            params: {
+                default_active_id: 'mail.box_history',
+            },
+        },
+        env: {
+            session: {
+                name: 'Admin',
+                partner_display_name: 'Your Company, Admin',
+                partner_id: 3,
+                uid: 2,
+            },
+        },
+    });
+    assert.containsOnce(
+        document.body,
+        '.o_Message',
+        "should display a single message"
+    );
+    assert.containsOnce(
+        document.body,
+        '.o_Message_subject',
+        "should display subject of the message"
+    );
+    assert.strictEqual(
+        document.querySelector('.o_Message_subject').textContent,
+        "Subject: Salutations, voyageur",
+        "Subject of the message should be 'Salutations, voyageur'"
+    )
+});
+
 });
 });
 });
