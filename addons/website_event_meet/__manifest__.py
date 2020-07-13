d1aa15b81b36a11f95679aec18c4687caf782b11
std-odoo <std@odoo.com>
2020-07-13 12:59:07 +0200

    [ADD] website_event_meet: allow visitors to meet at online events
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge we
    improve Event application to better support full-online events with improved
    tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    Allow the website visitors to meet at online events using chat rooms. Those are
    based on jitsi rooms previously added.
    
    SPECIFICATIONS
    
    Define a new model holding discussion rooms, with their target and a publish
    management. Inherit from chat.room mixin to enable jitsi-based chat.
    
    Any website visitor can create a meeting room based on Jitsi. Using some
    information (name, summary, room size, languyage) anyone can launch a new
    discussion topic. This will make a link with a new jitsi room enabling people
    to chat. Visitor-community rooms creation can be turned off on event itself.
    
    Meeting rooms can be pinned by the event managers to choose which rooms are
    displayed on top. Managers can also duplicate the full meeting rooms if a topic
    is popular, or publish/unpublish rooms like usual website records.
    
    SPECIFICATIONS: LIST VIEW
    
    Display a card-based list view of meeting rooms. They can be filtered by
    language. When a meeting room reach the maximum number of participants it is
    not visible anymore for standard visitors.
    
    The meeting rooms unpinned are cleaned after a certain period of time if there
    is no activity in it usign the autovacuum cron. That way we keep a clean view
    on the frontend and we keep the important (pinned) meeting rooms.
    
    SPECIFICATIONS: PAGE VIEW
    
    Use the same main content / aside panel display as exhibitors / tracks, to
    display the jitsi in the main content. Left panel proposes suggestions of
    rooms, based on current one.
    
    When a visitor join a meeting room, we fill the jitsi username with the name
    he entered during his registration.
    
    LINKS
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283742 (Community gathering on chat rooms)
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Stéphane Debauche <std@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

--- addons/website_event_meet/__manifest__.py
+++ addons/website_event_meet/__manifest__.py
@@ -11,11 +11,23 @@
     'website': 'https://www.odoo.com/page/events',
     'description': "",
     'depends': [
+        # STD-TODO: remove 'website_event_track_online' we just need it to be
+        # able to use 'website.event.menu' which is defined in 'website_event_track'
+        # as well as even form view defined in track_online
         'website_event_online',
+        'website_event_track_online',
+        'website_jitsi',
     ],
+    'demo': ['data/website_event_meet_demo.xml'],
     'data': [
-    ],
-    'demo': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/assets.xml',
+        'views/event_meet_templates_list.xml',
+        'views/event_meet_templates_page.xml',
+        'views/event_meeting_room_views.xml',
+        'views/event_event_views.xml',
+        'views/event_type_views.xml',
     ],
     'application': False,
     'installable': True,
