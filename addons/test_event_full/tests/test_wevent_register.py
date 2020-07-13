5ad2295a5d7ff0d706eda3a4fb998e5302886ba9
Thibault Delavallée <tde@odoo.com>
2020-07-13 17:10:10 +0200

    [ADD] test_event_full: complete me update me

---
+++ addons/test_event_full/tests/test_wevent_register.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import tests
+from odoo.addons.test_event_full.tests.common import TestWEventCommon
+
+
+@tests.common.tagged('post_install', '-at_install')
+class TestWEventRegister(TestWEventCommon):
+
+    def test_register(self):
+        self.browser_js(
+            '/event',
+            'odoo.__DEBUG__.services["web_tour.tour"].run("wevent_register")',
+            'odoo.__DEBUG__.services["web_tour.tour"].tours.wevent_register.ready',
+            login=None
+        )
