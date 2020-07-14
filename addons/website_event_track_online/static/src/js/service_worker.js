62fed929a184b16f8f9beea7f0e65c9ccb745308
Pierre Paridans <app@odoo.com>
2020-07-14 14:02:21 +0000

    [IMP] website_event_track_online: add PWA support
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge we
    improve Event application to better support full-online events with improved
    tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    Add PWA support. This will improve adoption and use of our Events application.
    
    LINKS
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283796 (Event B2Basics / Registration Flow)
    Task ID-1896623 (PWA Support)

---
+++ addons/website_event_track_online/static/src/js/service_worker.js
@@ -0,0 +1,228 @@
+importScripts("/website_event_track_online/static/lib/idb-keyval/idb-keyval.js");
+
+const PREFIX = "odoo-event";
+const SYNCABLE_ROUTES = ["/event/track/toggle_reminder"];
+const CACHABLE_ROUTES = ["/web/webclient/version_info"];
+
+const { Store, set, get, del } = idbKeyval;
+const pendingRequestsQueueName = `${PREFIX}-pending-requests`;
+const cacheName = `${PREFIX}-cache`;
+const syncStore = new Store(`${PREFIX}-sync-db`, `${PREFIX}-sync-store`);
+const cacheStore = new Store(`${PREFIX}-cache-db`, `${PREFIX}-cache-store`);
+
+/**
+ *
+ * @param {string} url
+ * @returns {string}
+ */
+const urlPathname = (url) => new URL(url).pathname;
+
+/**
+ *
+ * @param {Array} whitelist
+ * @returns {Function}
+ */
+const canHandleRoutes = (whitelist) => (url) => whitelist.includes(urlPathname(url));
+
+/**
+ *
+ * @param {Request} request
+ * @returns {boolean}
+ */
+const isGET = (request) => request.method === "GET";
+
+/**
+ *
+ * @returns {Function}
+ */
+const isSyncableURL = canHandleRoutes(SYNCABLE_ROUTES);
+
+/**
+ *
+ * @returns {Function}
+ */
+const isCachableURL = canHandleRoutes(CACHABLE_ROUTES);
+
+/**
+ *
+ * @param {Request} req
+ * @returns {Promise<Object>}
+ */
+const serializeRequest = async (req) => ({
+    url: req.url,
+    method: req.method,
+    headers: Object.fromEntries(req.headers.entries()),
+    body: await req.text(),
+    mode: req.mode,
+    credentials: req.credentials,
+    cache: req.cache,
+    redirect: req.redirect,
+    referrer: req.referrer,
+    integrity: req.integrity,
+});
+
+/**
+ *
+ * @param {Object} requestData
+ * @returns {Request}
+ */
+const deserializeRequest = (requestData) => {
+    const { url } = requestData;
+    delete requestData.url;
+    return new Request(url, requestData);
+};
+
+/**
+ *
+ * @param {Response} res
+ * @returns {Promise<Object>}
+ */
+const serializeResponse = async (res) => ({
+    body: await res.text(),
+    status: res.status,
+    statusText: res.statusText,
+    headers: Object.fromEntries(res.headers.entries()),
+});
+
+/**
+ *
+ * @param {Object} responseData
+ * @returns {Response}
+ */
+const deserializeResponse = (responseData) => {
+    const { body } = responseData;
+    delete responseData.body;
+    return new Response(body, responseData);
+};
+
+/**
+ *
+ * @param {Object} serializedRequest
+ * @returns {string}
+ */
+const buildCacheKey = ({ url, body: { method, params } }) =>
+    JSON.stringify({
+        url,
+        method,
+        params,
+    });
+
+/**
+ *
+ * @returns {int}
+ */
+const uniqueRequestId = () => Math.floor(Math.random() * 1000 * 1000 * 1000);
+
+/**
+ *
+ * @returns {Response}
+ */
+const buildEmptyResponse = () => new Response(JSON.stringify({ jsonrpc: "2.0", id: uniqueRequestId(), result: {} }));
+
+/**
+ *
+ * @param {Request} request
+ * @param {Response} response
+ * @returns {Promise}
+ */
+const cacheRequest = async (request, response) => {
+    if (isGET(request)) {
+        const cache = await caches.open(cacheName);
+        await cache.put(request, response.clone());
+    } else if (isCachableURL(request.url)) {
+        const serializedResponse = await serializeResponse(response.clone());
+        await set(buildCacheKey(serializedRequest), serializedResponse, cacheStore);
+    }
+};
+
+/**
+ *
+ * @param {Request} request
+ * @returns {boolean}
+ */
+const isCachableRequest = (request) => isGET(request) || isCachableURL(request.url);
+
+/**
+ *
+ * @param {Request} request
+ * @returns {Promise<Response|null>}
+ */
+const matchCache = async (request) => {
+    if (isGET(request)) {
+        const cache = await caches.open(cacheName);
+        return await cache.match(request);
+    }
+    if (isCachableURL(request.url)) {
+        const serializedRequest = await serializeRequest(requestCopy);
+        const cachedResponse = await get(buildCacheKey(serializedRequest), cacheStore);
+        if (cachedResponse) {
+            return deserializeResponse(cachedResponse);
+        }
+    }
+    return null;
+};
+
+/**
+ *
+ * @param {FetchEvent} param0
+ * @returns {Promise<Response>}
+ */
+const processFetchEvent = async ({ request }) => {
+    const requestCopy = request.clone();
+    let response;
+    try {
+        response = await fetch(request);
+        await cacheRequest(request, response);
+    } catch (requestError) {
+        if (isCachableRequest(requestCopy)) {
+            response = await matchCache(requestCopy);
+        } else if (isSyncableURL(requestCopy.url)) {
+            const pendingRequests = (await get(pendingRequestsQueueName, syncStore)) || [];
+            const serializedRequest = await serializeRequest(requestCopy);
+            await set(pendingRequestsQueueName, [...pendingRequests, serializedRequest], syncStore);
+            if (self.registration.sync) {
+                await self.registration.sync.register(pendingRequestsQueueName).catch((err) => {
+                    console.warn("Cannot use BackgroundSync", err);
+                    throw requestError;
+                });
+            }
+            return buildEmptyResponse();
+        } else {
+            console.warn(`Offline ${requestCopy.method} request currently not supported`, requestCopy);
+        }
+
+        if (!response) {
+            throw requestError;
+        }
+    }
+    return response;
+};
+
+/**
+ *
+ * @returns {Promise}
+ */
+const processPendingRequests = async () => {
+    const pendingRequests = (await get(pendingRequestsQueueName, syncStore)) || [];
+    if (!pendingRequests.length) {
+        console.info("Nothing to sync!");
+        return;
+    }
+    let pendingRequest;
+    while ((pendingRequest = pendingRequests.shift())) {
+        const request = deserializeRequest(pendingRequest);
+        await fetch(request);
+        await set(pendingRequestsQueueName, pendingRequests, syncStore);
+    }
+};
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(processFetchEvent(event));
+});
+
+self.addEventListener("sync", (event) => {
+    console.info(`Syncing pending requests...`);
+    if (event.tag === pendingRequestsQueueName) {
+        event.waitUntil(processPendingRequests());
+    }
+});
