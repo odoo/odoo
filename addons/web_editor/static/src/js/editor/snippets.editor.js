84b67f2c3393da05441bf1e9165171f6275ecfa1
qsm-odoo <qsm@odoo.com>
2020-07-16 18:50:36 +0200

    [FIX] web_editor: fix small memory leak of destroyed snippet editors
    In some complex cases, some snippet editors were destroyed but were
    still referenced in memory.
    
    E.g.
    
    1) Reorder a snippet gallery image
    2) The code is implemented in such a way that reordering create a new
       wrapping column for each image and moves the images in them.
    3) This result in having the snippet editor linked to the old columns
       to be explicitely destroyed and removed from memory, but not the ones
       linked to the images as they are still in the DOM (just moved).
    4) ... but the snippet editors linked to the images are children of
       the snippet editors linked to the columns. They are thus implicitely
       destroyed... but not removed from memory.
    
    Part of https://github.com/odoo/odoo/pull/54608
    
    X-original-commit: e4bf37c7fc892cdba57f0acac8e10f5698184e67

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -303,6 +303,10 @@ var SnippetEditor = Widget.extend({
      * @override
      */
     destroy: function () {
+        // Before actually destroying a snippet editor, notify the parent
+        // about it so that it can update its list of alived snippet editors.
+        this.trigger_up('snippet_editor_destroyed');
+
         this._super(...arguments);
         if (this.draggableComponent) {
             this.draggableComponent.unsetDraggable();
@@ -991,6 +995,7 @@ var SnippetsMenu = Widget.extend({
         'go_to_parent': '_onGoToParent',
         'remove_snippet': '_onRemoveSnippet',
         'snippet_edition_request': '_onSnippetEditionRequest',
+        'snippet_editor_destroyed': '_onSnippetEditorDestroyed',
         'snippet_removed': '_onSnippetRemoved',
         'snippet_cloned': '_onSnippetCloned',
         'snippet_option_visibility_update': '_onSnippetOptionVisibilityUpdate',
@@ -1263,16 +1268,15 @@ var SnippetsMenu = Widget.extend({
      * Updates the cover dimensions of the current snippet editor.
      */
     updateCurrentSnippetEditorOverlay: function () {
-        this.snippetEditors = this.snippetEditors.filter(snippetEditor => {
+        for (const snippetEditor of this.snippetEditors) {
             if (snippetEditor.$target.closest('body').length) {
                 snippetEditor.cover();
-                return true;
+                continue;
             }
             // Destroy options whose $target are not in the DOM anymore but
             // only do it once all options executions are done.
             this._mutex.exec(() => snippetEditor.destroy());
-            return false;
-        });
+        }
     },
 
     //--------------------------------------------------------------------------
@@ -2393,6 +2397,15 @@ var SnippetsMenu = Widget.extend({
      */
     _onSnippetEditionRequest: function (ev) {
         this._mutex.exec(ev.data.exec);
+    },
+    /**
+     * @private
+     * @param {OdooEvent} ev
+     */
+    _onSnippetEditorDestroyed(ev) {
+        ev.stopPropagation();
+        const index = this.snippetEditors.indexOf(ev.target);
+        this.snippetEditors.splice(index, 1);
     },
     /**
      * @private
