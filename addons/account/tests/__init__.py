3ad65201c0f04de748c7ab56ba13b44a71c38eb9
Laurent Smet <las@odoo.com>
2020-07-17 15:42:11 +0200

    [IMP] account: Remove test_control_journal_account dependency to AccountTestCommon
    l10n runbot builds are all failing when running at least one test depending of AccountTestCommon because it:
    - doesn't create a sandboxed testing environnement to manage the multi-currency, multi-company, the default company's currency, the exchange rates...
    - doesn't setup a testing user then all tests are done using the superuser.
    - doesn't provide a fully setup chart of accounts: exchange difference journal is not set, accounts have bad types, etc...
    - is run sometimes at-install.
    
    --task: 2301180

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -17,7 +17,6 @@ from . import test_account_move_partner_count
 from . import test_account_move_rounding
 from . import test_account_invoice_report
 from . import test_account_journal_dashboard
-from . import test_control_journal_account
 from . import test_fiscal_position
 from . import test_reconciliation
 from . import test_search
