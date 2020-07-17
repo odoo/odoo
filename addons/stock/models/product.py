f50d80810f657ef8a6e8d048a3db83e2f34c1ad6
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2020-07-17 10:52:21 +0200

    [FIX] stock,sale_mrp: Inventory adjustment not possible
    1) Create a stockable product > Add some quantities on hand
    2) Edit this product > set it as a consumable or service
    3) Create an inventory adjustment on all products
    
    Error will raise "Something went wrong! You can only adjust storable products.
    
    This occur because when switching product type the available quantity
    was not cleared, so it will get in the next inventory, but only storable
    products can have inventory. So the product should to have no
    quantity left before switching type
    
    opw-2300478
    
    X-original-commit: be17e31307070498ba9ac7dc594443730caa7404

--- addons/stock/models/product.py
+++ addons/stock/models/product.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 from odoo import _, api, fields, models, SUPERUSER_ID
 from odoo.exceptions import UserError
 from odoo.osv import expression
+from odoo.tools import pycompat,float_is_zero
 from odoo.tools.float_utils import float_round
 
 OPERATORS = {
@@ -733,6 +734,8 @@ class ProductTemplate(models.Model):
             ])
             if existing_move_lines:
                 raise UserError(_("You can not change the type of a product that is currently reserved on a stock move. If you need to change the type, you should first unreserve the stock move."))
+        if 'type' in vals and vals['type'] != 'product' and any(p.type == 'product' and not float_is_zero(p.qty_available, precision_rounding=p.uom_id.rounding) for p in self):
+            raise UserError(_("Available quantity should be set to zero before changing type"))
         return super(ProductTemplate, self).write(vals)
 
     # Be aware that the exact same function exists in product.product
