dbd26179ae912367de4b9442db1deff4f6d9b282
Florent de Labarre <florent.mirieu@gmail.com>
2020-07-20 17:44:00 +0000

    [FIX] stock : singleton error (fine-tuning bf2f1bf)
    Fine tuning of bf2f1bf
    
    Can raise if write with multi records.
    
    opw-2303446
    
    X-original-commit: 70a19d21427d82840036ae301470b34ef922be54

--- addons/stock/models/product.py
+++ addons/stock/models/product.py
@@ -624,6 +624,11 @@ class ProductTemplate(models.Model):
             ])
             if existing_move_lines:
                 raise UserError(_("You can not change the type of a product that is currently reserved on a stock move. If you need to change the type, you should first unreserve the stock move."))
+<<<<<<< HEAD
+=======
+        if 'type' in vals and vals['type'] != 'product' and self.filtered(lambda p: p.type == 'product' and not float_is_zero(p.qty_available, precision_rounding=p.uom_id.rounding)):
+            raise UserError(_("Available quantity should be set to zero before changing type"))
+>>>>>>> 153f74e9b94... temp
         return super(ProductTemplate, self).write(vals)
 
     def action_update_quantity_on_hand(self):
