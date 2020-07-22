ae2f0c6218e0f83aa77d2c997db9940cbda54fe2
Katherine Zaoral <kz@adhoc.com.ar>
2020-07-22 15:26:21 +0000

    [REF] l10n_cl: Add custom report
    Since we have remove the template in l10n_latam_invoice_document we apply here
    the same template just to be able to maintain the same report in the stable
    version for chile. We only adapt the way that the custom report is shown to
    match with the new logic added in l10n_latam_document
    
    closes odoo/odoo#50182
    
    closes odoo/odoo#55482
    
    Related: odoo/enterprise#10203
    Related: odoo/enterprise#12230
    X-original-commit: 93a165933555d6c92bd887ac1e6304f65f020a8e
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- addons/l10n_cl/models/account_move.py
+++ addons/l10n_cl/models/account_move.py
@@ -114,3 +114,13 @@ class AccountMove(models.Model):
             where_string += " AND l10n_latam_document_type_id = %(l10n_latam_document_type_id)s "
             param['l10n_latam_document_type_id'] = self.l10n_latam_document_type_id.id or 0
         return where_string, param
+
+    def _get_name_invoice_report(self, report_xml_id):
+        self.ensure_one()
+        if self.l10n_latam_use_documents and self.company_id.country_id.code == 'CL':
+            custom_report = {
+                'account.report_invoice_document_with_payments': 'l10n_cl.report_invoice_document_with_payments',
+                'account.report_invoice_document': 'l10n_cl.report_invoice_document',
+            }
+            return custom_report.get(report_xml_id) or report_xml_id
+        return super()._get_name_invoice_report(report_xml_id)
