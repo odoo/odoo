93a165933555d6c92bd887ac1e6304f65f020a8e
Katherine Zaoral <kz@adhoc.com.ar>
2020-07-22 15:26:21 +0000

    [REF] l10n_cl: Add custom report
    Since we have remove the template in l10n_latam_invoice_document we apply here
    the same template just to be able to maintain the same report in the stable
    version for chile. We only adapt the way that the custom report is shown to
    match with the new logic added in l10n_latam_document
    
    closes odoo/odoo#50182
    
    closes odoo/odoo#54925
    
    Related: odoo/enterprise#10203
    Related: odoo/enterprise#12022
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- addons/l10n_cl/__manifest__.py
+++ addons/l10n_cl/__manifest__.py
@@ -25,6 +25,7 @@ Plan contable chileno e impuestos de acuerdo a disposiciones vigentes
         'views/ir_sequence_view.xml',
         'views/res_bank_view.xml',
         'views/res_country_view.xml',
+        'views/report_invoice.xml',
         'views/res_partner.xml',
         'views/res_config_settings_view.xml',
         'data/l10n_cl_chart_data.xml',
