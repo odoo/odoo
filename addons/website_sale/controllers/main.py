50f82dd3d34b4acd1f0bf197bcb099c737a94b5a
Katherine Zaoral <kz@adhoc.com.ar>
2020-07-22 10:30:34 -0300

    [IMP] website_sale: display vat error message
    Show error_message when there is a problem with the check_vat showing
    the information about the error we found out.

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -568,8 +568,9 @@ class WebsiteSale(http.Controller):
             partner_dummy = Partner.new(self._get_vat_validation_fields(data))
             try:
                 partner_dummy.check_vat()
-            except ValidationError:
+            except ValidationError as exception:
                 error["vat"] = 'error'
+                error_message.append(exception.name)
 
         if [err for err in error.values() if err == 'missing']:
             error_message.append(_('Some required fields are empty.'))
