d1b6ce73e2cb904de8521213e80969f2cacaed4b
Adam Heinz <amh@metricwise.net>
2020-07-23 14:05:53 -0400

    Add url_open(**kwargs) to reach requests.get/post(**kwargs).

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -1122,13 +1122,13 @@ class HttpCase(TransactionCase):
         self.opener = requests.Session()
         self.opener.cookies['session_id'] = self.session_id
 
-    def url_open(self, url, data=None, files=None, timeout=10, headers=None):
+    def url_open(self, url, data=None, files=None, timeout=10, headers=None, **kwargs):
         self.env['base'].flush()
         if url.startswith('/'):
             url = "http://%s:%s%s" % (HOST, PORT, url)
         if data or files:
-            return self.opener.post(url, data=data, files=files, timeout=timeout, headers=headers)
-        return self.opener.get(url, timeout=timeout, headers=headers)
+            return self.opener.post(url, data=data, files=files, timeout=timeout, headers=headers, **kwargs)
+        return self.opener.get(url, timeout=timeout, headers=headers, **kwargs)
 
     def _wait_remaining_requests(self, timeout=10):
 
