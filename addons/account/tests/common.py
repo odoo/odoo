825cabf9f70dcbb759f763038231ce1dda5ebc7b
Laurent Smet <las@odoo.com>
2020-07-24 10:13:04 +0200

    [IMP] account: Remove AccountTestUsersCommon testing class
    l10n runbot builds are all failing when running at least one test depending of AccountTestCommon because it:
    - doesn't create a sandboxed testing environnement to manage the multi-currency, multi-company, the default company's currency, the exchange rates...
    - doesn't setup a testing user then all tests are done using the superuser.
    - doesn't provide a fully setup chart of accounts: exchange difference journal is not set, accounts have bad types, etc...
    - is run sometimes at-install.
    
    --task: 2301180

--- addons/account/tests/common.py
+++ addons/account/tests/common.py
@@ -283,41 +283,6 @@ class AccountTestCommon(SavepointCase):
         cls.company.currency_exchange_journal_id = cls.currency_diff_journal.id
 
 
-class AccountTestUsersCommon(AccountTestCommon):
-
-    """Tests for diffrent type of user 'Accountant/Adviser' and added groups"""
-
-    @classmethod
-    def setUpClass(cls):
-        super(AccountTestUsersCommon, cls).setUpClass()
-        cls.res_user_model = cls.env['res.users']
-        cls.main_company = cls.env.ref('base.main_company')
-        cls.main_partner = cls.env.ref('base.main_partner')
-        cls.main_bank = cls.env.ref('base.res_bank_1')
-        res_users_account_user = cls.env.ref('account.group_account_invoice')
-        res_users_account_manager = cls.env.ref('account.group_account_manager')
-        partner_manager = cls.env.ref('base.group_partner_manager')
-        cls.tax_model = cls.env['account.tax']
-        cls.account_model = cls.env['account.account']
-        cls.account_type_model = cls.env['account.account.type']
-        cls.currency_euro = cls.env.ref('base.EUR')
-
-        cls.account_user = cls.res_user_model.with_context({'no_reset_password': True}).create(dict(
-            name="Accountant",
-            company_id=cls.main_company.id,
-            login="acc",
-            email="accountuser@yourcompany.com",
-            groups_id=[(6, 0, [res_users_account_user.id, partner_manager.id])]
-        ))
-        cls.account_manager = cls.res_user_model.with_context({'no_reset_password': True}).create(dict(
-            name="Adviser",
-            company_id=cls.main_company.id,
-            login="fm",
-            email="accountmanager@yourcompany.com",
-            groups_id=[(6, 0, [res_users_account_manager.id, partner_manager.id])]
-        ))
-
-
 class AccountTestNoChartCommon(SavepointCaseWithUserDemo):
     """ Some tests required to be executed at module installation, and not 'post install', like moslty
         of accounting tests, since a chart of account is required
