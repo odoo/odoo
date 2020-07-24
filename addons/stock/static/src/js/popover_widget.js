9d3d87d1aa6be68f0bc51307cae14bbfd71f8c4e
Priyanka Kakadiya <pka@odoo.com>
2020-07-24 15:41:16 +0530

    [IMP] stock: resolve conflict chnages

--- addons/stock/static/src/js/popover_widget.js
+++ addons/stock/static/src/js/popover_widget.js
@@ -2,6 +2,8 @@ odoo.define('stock.popover_widget', function (require) {
     'use strict';
 
     const AbstractFieldOwl = require('web.AbstractFieldOwl');
+    const Context = require('web.Context');
+    const data_manager = require('web.data_manager');
     const fieldRegistryOwl = require('web.field_registry_owl');
 
     class PopoverWidgetField extends AbstractFieldOwl {
@@ -13,6 +15,9 @@ odoo.define('stock.popover_widget', function (require) {
             this.color = 'text-primary';
             this.icon = 'fa-info-circle';
             this.json_value = JSON.parse(this.value);
+            if (this.json_value.trigger) {
+                delete this.json_value.trigger;
+            }
             Object.assign(this, this.json_value);
         }
 
@@ -34,6 +39,20 @@ odoo.define('stock.popover_widget', function (require) {
 
         get popoverContentTemplate() {
             return this.json_value.popoverTemplate || this.popoverTemplate;
+        }
+
+        async _openForecast(ev) {
+            const action = await data_manager.load_action('stock.report_stock_quantity_action_product');
+            const additional_context = {
+                grid_anchor: this.recordData.delivery_date_grid,
+                search_default_warehouse_id: [this.recordData.warehouse_id.data.id],
+                search_default_below_warehouse: false
+            };
+            action.context = new Context(action.context, additional_context);
+            action.domain = [
+                ['product_id', '=', this.recordData.product_id.data.id]
+            ];
+            return this.trigger('do-action', {action: action});
         }
 
     }
