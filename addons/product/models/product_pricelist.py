b3d39d4e7d9aa4553daea9e738321aa395ca075b
Pafnow <pafnow@hotmail.com>
2020-07-24 09:52:44 +0700

    [FIX] typo on function name of product.pricelist.item
     _onchane_rule_content > _onchange_rule_content

--- addons/product/models/product_pricelist.py
+++ addons/product/models/product_pricelist.py
@@ -556,7 +556,7 @@ class PricelistItem(models.Model):
                 item.product_id = None
 
     @api.onchange('product_id', 'product_tmpl_id', 'categ_id')
-    def _onchane_rule_content(self):
+    def _onchange_rule_content(self):
         if not self.user_has_groups('product.group_sale_pricelist') and not self.env.context.get('default_applied_on', False):
             # If advanced pricelists are disabled (applied_on field is not visible)
             # AND we aren't coming from a specific product template/variant.
