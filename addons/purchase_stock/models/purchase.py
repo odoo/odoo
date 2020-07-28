c454a2d19097b524544553f68d63603e31382fca
yhu-odoo <yhu@odoo.com>
2020-07-28 14:18:05 +0200

    [FIX] purchase_stock: set propagate_date according to buy rule
    Before this commit, when we manually create a PO line, the
    propagate_date will always be false. So the change on scheduled date
    will never pass to corresponding move.
    
    After this commit, when create a PO line, if no propagate_date for it,
    we find if the product has a route with a buy rule. If yes, use the
    setting on its first buy rule, if no, set the default to be true.
    
    Task 2298950
    PR #55155

--- addons/purchase_stock/models/purchase.py
+++ addons/purchase_stock/models/purchase.py
@@ -338,6 +338,24 @@ class PurchaseOrderLine(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         lines = super(PurchaseOrderLine, self).create(vals_list)
+
+        # when no "propagate_date", find if the product has a route with a buy rule,
+        # if yes, use the setting on the rule, if no, set "True" as default.
+        for line, vals in zip(lines, vals_list):
+            if 'propagate_date' not in vals:
+                buy_rules = line.product_id.route_ids.rule_ids.filtered(lambda r: r.action == "buy")
+                if buy_rules:
+                    rule = min(buy_rules, key=lambda r: (r.route_id.sequence, r.sequence))
+                    line.write({
+                        'propagate_date': rule.propagate_date,
+                        'propagate_date_minimum_delta': rule.propagate_date_minimum_delta,
+                    })
+                else:
+                    line.write({
+                        'propagate_date': True,
+                        'propagate_date_minimum_delta': 0,
+                    })
+
         lines.filtered(lambda l: l.order_id.state == 'purchase')._create_or_update_picking()
         return lines
 
