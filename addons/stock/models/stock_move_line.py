8fd18e72a90b4763ac430b9ce7edda452f410b33
Miquel Raïch <miquel.raich@forgeflow.com>
2020-07-29 13:01:28 +0200

    [FIX] stock: quant in_date should be the date the product arrives to the warehouse

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -490,6 +490,9 @@ class StockMoveLine(models.Model):
                         taken_from_untracked_qty = min(untracked_qty, abs(quantity))
                         Quant._update_available_quantity(ml.product_id, ml.location_id, -taken_from_untracked_qty, lot_id=False, package_id=ml.package_id, owner_id=ml.owner_id)
                         Quant._update_available_quantity(ml.product_id, ml.location_id, taken_from_untracked_qty, lot_id=ml.lot_id, package_id=ml.package_id, owner_id=ml.owner_id)
+                # we don't want the in_date if it's an incoming move from a supplier
+                if ml.location_id.usage == "supplier":
+                    in_date = False
                 Quant._update_available_quantity(ml.product_id, ml.location_dest_id, quantity, lot_id=ml.lot_id, package_id=ml.result_package_id, owner_id=ml.owner_id, in_date=in_date)
             ml_ids_to_ignore.add(ml.id)
         # Reset the reserved quantity as we just moved it to the destination location.
