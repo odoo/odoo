e8ce2693d77e8a69c62d1e714013a30dbb093979
oco-odoo <oco@odoo.com>
2020-07-30 10:47:41 +0000

    [FIX] account, point_of_sale: correct tax_audit computation for pos returns
    https://github.com/odoo/enterprise/pull/12091 changed the way the tax report computes its totals wit account moves resulting for return operations in pos. This commit does the same on tax_audit field, so that it stays consistent with the report.
    
    closes odoo/odoo#55193
    
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>

---
+++ addons/point_of_sale/models/account_move.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    def _get_refund_tax_audit_condition(self, aml):
+        # Overridden so that the returns can be detected as credit notes by the tax audit computation
+        rslt = super()._get_refund_tax_audit_condition(aml)
+
+        if aml.invoice_id:
+            # We don't need to check the pos orders for this move lines if an invoice
+            # is linked to it ; we know that the invoice type tells us whether it's a refund
+            return rslt
+
+        pos_orders_count = self.env['pos.order'].search_count([('account_move', '=', aml.move_id)])
+        return rslt or (pos_orders_count and aml.debit > 0)
