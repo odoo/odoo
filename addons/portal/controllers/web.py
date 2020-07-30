e3566f59aa33155b6bf93af13bcb73d5d3e4e44a
Romeo Fragomeli <rfr@odoo.com>
2020-07-30 15:33:34 +0200

    [FIX] portal: Avoid error at login with portal user on mobile
    Before this commit, some errors were shown to the end user when
    he tried to login to the mobile app with a portal user.
    The problem is the user was stuck to a blank page and wasn't
    able to logout even after reloading the app.
    
    Note that even if portal users aren't supposed to access to the
    backend, it's quite annoying to not be able to go back.
    
    After this commit, portal users are automatically redirected to
    the portal page ("/my") if they try go to "/web". This is the same
    behavior as when portal users login via "/web/login".
    By doing this we avoid having custom code in the native apps
    because we already avoid any login to the app if we are out of "/web".
    
    Note: this commit doesn't cover all cases as portal users are defined in
    the "base" module and the portal page is set in the "portal" module.
    So this this fix will not work if "portal" is not installed and if you
    manually created a portal user (only possible in debug mode).
    This is unlikely and we therefore consider that this fix covers the
    majority of cases while being the safest.
    
    Task ID: 2266024
    opw-2281168
    
    X-original-commit: 29cbba73a71990e76f497ee892ce16fc7fc53d9f

--- addons/portal/controllers/web.py
+++ addons/portal/controllers/web.py
@@ -18,3 +18,9 @@ class Home(Home):
         if not redirect and not request.env['res.users'].sudo().browse(uid).has_group('base.group_user'):
             return '/my'
         return super(Home, self)._login_redirect(uid, redirect=redirect)
+
+    @http.route('/web', type='http', auth="none")
+    def web_client(self, s_action=None, **kw):
+        if request.session.uid and not request.env['res.users'].sudo().browse(request.session.uid).has_group('base.group_user'):
+            return http.local_redirect('/my', query=request.params, keep_hash=True)
+        return super(Home, self).web_client(s_action, **kw)
