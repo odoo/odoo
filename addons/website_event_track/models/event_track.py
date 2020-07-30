b6f0b706b175c2745aed301c36d28718987bfc66
Thibault Delavallée <tde@odoo.com>
2020-07-30 14:53:04 +0000

    [REF] website_event_track: set color back on track
    Partial revert of 08bcc53856200af8918a75fde470c8ecfac66732
    
    closes odoo/odoo#55213
    
    Related: odoo/upgrade#1535
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_event_track/models/event_track.py
+++ addons/website_event_track/models/event_track.py
@@ -81,7 +81,7 @@ class Track(models.Model):
     duration = fields.Float('Duration', default=1.5, help="Track duration in hours.")
     location_id = fields.Many2one('event.track.location', 'Location')
     event_id = fields.Many2one('event.event', 'Event', required=True)
-    color = fields.Integer('Color', related="stage_id.color")
+    color = fields.Integer('Color')
     priority = fields.Selection([
         ('0', 'Low'), ('1', 'Medium'),
         ('2', 'High'), ('3', 'Highest')],
