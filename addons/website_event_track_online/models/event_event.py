4f0bb3c19fe3867b3a0f94584cdab05287c7fb45
qmo-odoo <qmo@odoo.com>
2020-07-30 11:00:11 +0200

    [IMP] website_event_track_online: add a (void) community menu and page
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge we
    improve Event application to better support full-online events with improved
    tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    Prepare community addition (both quiz-based and room-based) by adding
    a community link and page management.
    
    SPECIFICATIONS
    
    In order to show the leaderboard and the meeting rooms on the same page we
    have to put a page and a route at the base of two upcoming modules: meet and
    quiz. The only common ancestor of those two will be website_event_track_online
    who gains a field for the community menu.
    
    It currently does nothing and leads to a void page. Both meet and quiz modules
    will add their own logic: meeting rooms or leaderboard. Having bothi installed
    displays rooms with a small leaderboard.
    
    See next commits for more details on those modules.
    
    LINKS
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283771 (Quizzes on Talks, Leaderboard)
    Task ID-2283742 (Community gathering on chat rooms)
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Quentin Mourier <qmo@odoo.com>
    Co-Authored-By: Stéphane Debauche <std@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

--- addons/website_event_track_online/models/event_event.py
+++ addons/website_event_track_online/models/event_event.py
@@ -1,24 +1,64 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, _
+from odoo import models, fields, api, _
 from odoo.addons.http_routing.models.ir_http import slug
 
 
 class EventEvent(models.Model):
     _inherit = "event.event"
 
+    community_menu = fields.Boolean(
+        "Community Menu", compute="_compute_community_menu",
+        readonly=False, store=True,
+        help="Display community tab on website")
+    community_menu_ids = fields.One2many(
+        "website.event.menu", "event_id", string="Event Community Menus",
+        domain=[("menu_type", "=", "community")])
+
+    @api.depends("event_type_id", "website_menu", "community_menu")
+    def _compute_community_menu(self):
+        """ At type onchange: synchronize. At website_menu update: synchronize. """
+        for event in self:
+            if event.event_type_id and event.event_type_id != event._origin.event_type_id:
+                event.community_menu = event.event_type_id.community_menu
+            elif event.website_menu and event.website_menu != event._origin.website_menu or not event.community_menu:
+                event.community_menu = True
+            elif not event.website_menu:
+                event.community_menu = False
+
     # ------------------------------------------------------------
     # WEBSITE MENU MANAGEMENT
     # ------------------------------------------------------------
 
     # OVERRIDES: ADD SEQUENCE
+
+    def _get_menu_update_fields(self):
+        update_fields = super(EventEvent, self)._get_menu_update_fields()
+        update_fields += ['community_menu']
+        return update_fields
+
+    def _update_website_menus(self, menus_update_by_field=None):
+        super(EventEvent, self)._update_website_menus(menus_update_by_field=menus_update_by_field)
+        for event in self:
+            if not menus_update_by_field or event in menus_update_by_field.get('community_menu'):
+                event._update_website_menu_entry('community_menu', 'community_menu_ids', '_get_community_menu_entries')
+
+    def _get_menu_type_field_matching(self):
+        res = super(EventEvent, self)._get_menu_type_field_matching()
+        res['community'] = 'community_menu'
+        return res
+
+    def _get_community_menu_entries(self):
+        self.ensure_one()
+        return [(_('Community'), '/event/%s/community' % slug(self), False, 80, 'community')]
+
     def _get_track_menu_entries(self):
         """ Remove agenda as this is now managed separately """
         self.ensure_one()
         return [
             (_('Talks'), '/event/%s/track' % slug(self), False, 10, 'track'),
-            (_('Agenda'), '/event/%s/agenda' % slug(self), False, 70, 'track')
+            (_('Agenda'), '/event/%s/agenda' % slug(self), False, 70, False)
         ]
 
     def _get_track_proposal_menu_entries(self):
