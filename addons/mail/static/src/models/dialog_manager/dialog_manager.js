9216bdcb6f0bbb20900a33bbf7283e6e4fda1b2c
Sébastien Theys <seb@odoo.com>
2020-08-05 14:03:05 +0000

    [FIX] mail: clean up JS relations by storing records
    Remove the support and the conversion from record to localId and back from
    localId to record.
    
    Part of task-2308312
    
    closes odoo/odoo#55481
    
    X-original-commit: fc5e8be4e65518770a281f0dffae07854d20073a
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/models/dialog_manager/dialog_manager.js
+++ addons/mail/static/src/models/dialog_manager/dialog_manager.js
@@ -2,7 +2,7 @@ odoo.define('mail/static/src/models/dialog_manager/dialog_manager.js', function 
 'use strict';
 
 const { registerNewModel } = require('mail/static/src/model/model_core.js');
-const { attr, one2many } = require('mail/static/src/model/model_field.js');
+const { one2many } = require('mail/static/src/model/model_field.js');
 
 function factory(dependencies) {
 
@@ -13,21 +13,10 @@ function factory(dependencies) {
         //----------------------------------------------------------------------
 
         /**
-         * @param {mail.dialog} dialog
-         */
-        close(dialog) {
-            this.unregister(dialog);
-            dialog.delete();
-        }
-
-        /**
          * @param {string} modelName
          * @param {Object} [recordData]
          */
         open(modelName, recordData) {
-            const dialog = this.env.models['mail.dialog'].create({
-                manager: [['link', this]],
-            });
             if (!modelName) {
                 throw new Error("Dialog should have a link to a model");
             }
@@ -36,53 +25,17 @@ function factory(dependencies) {
                 throw new Error(`No model exists with name ${modelName}`);
             }
             const record = Model.create(recordData);
-            dialog.update({ record: [['link', record]] });
-            this.update({ _ordered: this._ordered.concat([dialog.localId]) });
+            const dialog = this.env.models['mail.dialog'].create({
+                manager: [['link', this]],
+                record: [['link', record]],
+            });
             return dialog;
         }
 
-        /**
-         * @param {mail.dialog} dialog
-         */
-        unregister(dialog) {
-            if (!this.allOrdered.includes(dialog)) {
-                return;
-            }
-            this.update({
-                _ordered: this._ordered.filter(
-                    dialogLocalId => dialogLocalId !== dialog.localId
-                ),
-                dialogs: [['unlink', dialog]],
-            });
-        }
-
-        //----------------------------------------------------------------------
-        // Private
-        //----------------------------------------------------------------------
-
-        /**
-         * FIXME: dependent on implementation that uses arbitrary order in relations!!
-         *
-         * @private
-         * @returns {mail.dialog}
-         */
-        _computeAllOrdered() {
-            return [['replace', this._ordered.map(dialogLocalId =>
-                this.env.models['mail.dialog'].get(dialogLocalId)
-            )]];
-        }
     }
 
     DialogManager.fields = {
-        _ordered: attr({ default: [] }),
-        // FIXME: dependent on implementation that uses arbitrary order in relations!!
-        allOrdered: one2many('mail.dialog', {
-            compute: '_computeAllOrdered',
-            dependencies: [
-                '_ordered',
-                'dialogs',
-            ],
-        }),
+        // FIXME: dependent on implementation that uses insert order in relations!!
         dialogs: one2many('mail.dialog', {
             inverse: 'manager',
             isCausal: true,
