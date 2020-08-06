ff9a01788fc8f7101a9c7c1f397e7bb09dec1b72
Victor Feyens <vfe@odoo.com>
2020-08-06 17:00:53 +0000

    [FIX] stock_dropshipping: test_lifo_price
    Because the picking_type of the new move was defined as 
    show_operations=True, the quantity_done field on the lines was readonly.

--- addons/stock_dropshipping/tests/test_lifo_price.py
+++ addons/stock_dropshipping/tests/test_lifo_price.py
@@ -77,6 +77,7 @@ class TestLifoPrice(ValuationReconciliationTestCommon):
         purchase_order_lifo2.picking_ids[0].button_validate()
 
         # Let us send some goods
+        self.company_data['default_warehouse'].out_type_id.show_operations = False
         out_form = Form(self.env['stock.picking'])
         out_form.picking_type_id = self.company_data['default_warehouse'].out_type_id
         out_form.immediate_transfer = True
