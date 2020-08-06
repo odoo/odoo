e29c3537b3a5061c372a71db0695a9afe8fbe557
Sébastien Theys <seb@odoo.com>
2020-08-06 11:28:57 +0200

    [FIX] mail: process result of init messaging asynchronously
    Part of task-2308312

--- addons/mail/static/src/utils/test_utils.js
+++ addons/mail/static/src/utils/test_utils.js
@@ -67,9 +67,9 @@ function _useChatWindow(callbacks) {
         destroy: prevDestroy,
     } = callbacks;
     return Object.assign({}, callbacks, {
-        mount: prevMount.concat(() => {
+        mount: prevMount.concat(async () => {
             // trigger mounting of chat window manager
-            Component.env.services['chat_window']._onWebClientReady();
+            await Component.env.services['chat_window']._onWebClientReady();
         }),
         destroy: prevDestroy.concat(() => {
             Component.env.services['chat_window'].destroy();
@@ -92,9 +92,9 @@ function _useDialog(callbacks) {
         destroy: prevDestroy,
     } = callbacks;
     return Object.assign({}, callbacks, {
-        mount: prevMount.concat(() => {
+        mount: prevMount.concat(async () => {
             // trigger mounting of dialog manager
-            Component.env.services['dialog']._onWebClientReady();
+            await Component.env.services['dialog']._onWebClientReady();
         }),
         destroy: prevDestroy.concat(() => {
             Component.env.services['dialog'].destroy();
@@ -138,7 +138,7 @@ function _useDiscuss(callbacks) {
             discussWidget = new DiscussWidget(widget, state.discussData);
             await discussWidget.appendTo($(selector));
             if (state.autoOpenDiscuss) {
-                discussWidget.on_attach_callback();
+                await discussWidget.on_attach_callback();
             }
         }),
         return: prevReturn.concat(result => {
@@ -166,7 +166,7 @@ function _useMessagingMenu(callbacks) {
         mount: prevMount.concat(async ({ selector, widget }) => {
             messagingMenuWidget = new MessagingMenuWidget(widget, {});
             await messagingMenuWidget.appendTo($(selector));
-            messagingMenuWidget.on_attach_callback();
+            await messagingMenuWidget.on_attach_callback();
         }),
         return: prevReturn.concat(result => {
             Object.assign(result, { messagingMenuWidget });
