e22f7a4b85f582c79b216625493da17c3ba2754f
Thibault Delavallée <tde@odoo.com>
2020-08-07 11:07:13 +0000

    [FIX] website_event(_track/_online): improve and synchronize tags behavior
    PURPOSE
    
    Improve and make color management coherent in event models. Notably consider
    colorless tags as being used for internal management only, and not displayed
    in frontend.
    
    SPECIFICATIONS
    
    Ensure tags without color are not displayed in frontend for both event and
    track models (event main page, track display, agenda). Also ensure a default
    random color is given when creating tags so that they are displayed by
    default.
    
    When a category used as a dropdown menu has no tags to display hide it in
    order to have void categories in filtering section.
    
    LINKS
    
    Related to testing of EventOnline
    Task ID-2314778
    Fwd port of PR #55625
    PR #55642

--- addons/website_event_track/models/event_track.py
+++ addons/website_event_track/models/event_track.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from datetime import timedelta
 from random import randint
 
 from odoo import api, fields, models
 from odoo.tools.translate import _, html_translate
 from odoo.addons.http_routing.models.ir_http import slug
-from datetime import timedelta
 
 
 class TrackTag(models.Model):
@@ -14,13 +14,13 @@ class TrackTag(models.Model):
     _description = 'Event Track Tag'
     _order = 'name'
 
-    def _get_default_color(self):
+    def _default_color(self):
         return randint(1, 11)
 
     name = fields.Char('Tag Name', required=True)
     track_ids = fields.Many2many('event.track', string='Tracks')
     color = fields.Integer(
-        string='Color Index', default=_get_default_color,
+        string='Color Index', default=lambda self: self._default_color(),
         help="Note that colorless tags won't be available on the website.")
 
     _sql_constraints = [
