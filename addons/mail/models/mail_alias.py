6390d63c8861010560fb5a49d144e3811edbb506
Victor Feyens <vfe@odoo.com>
2020-08-07 15:37:37 +0200

    [FIX] *: adapt python constraints to support multi recordsets
    Otherwise, they may crash or be incorrectly verified when
    creating/updating recordsets containing multiple records.
    
    Also improve the constraint error messages to be more detailed.
    
    When possible, use a sql constraint instead to speed up the records
    validation.
    
    Task Id: 2328664
    COM PR: https://github.com/odoo/odoo/pull/55525
    ENT PR: https://github.com/odoo/enterprise/pull/12250

--- addons/mail/models/mail_alias.py
+++ addons/mail/models/mail_alias.py
@@ -87,8 +87,12 @@ class Alias(models.Model):
             local-part. Quoted-string and internationnal characters are
             to be rejected. See rfc5322 sections 3.4.1 and 3.2.3
         """
-        if self.alias_name and not dot_atom_text.match(self.alias_name):
-            raise ValidationError(_("You cannot use anything else than unaccented latin characters in the alias address."))
+        for alias in self:
+            if alias.alias_name and not dot_atom_text.match(alias.alias_name):
+                raise ValidationError(_(
+                    "You cannot use anything else than unaccented latin characters in the alias address (%s).",
+                    alias.alias_name,
+                ))
 
     def _compute_alias_domain(self):
         alias_domain = self._default_alias_domain()
