e1c4ed3ee8e5786f70f17c5949205682b4c8a2b5
Katherine Zaoral <kz@adhoc.com.ar>
2020-08-10 14:35:41 +0000

    [IMP] l10n_ar: rename module
    To make the match with the new name standard for localization modules
    
    closes odoo/odoo#55704
    
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- addons/l10n_ar/__manifest__.py
+++ addons/l10n_ar/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Argentinian Accounting',
+    'name': 'Argentina - Accounting',
     'version': "3.0",
     'description': """
 Functional
