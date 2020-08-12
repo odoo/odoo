0d5bc219ae9ae485b9ef3bafa5499fe7bfb19099
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2020-08-12 16:42:21 +0200

    [FIX] purchase_stock,sale_stock: account right for automated inventory valuation
    - Have a DEMO user, with just inventory user permission.
    - Have a product category CAT on which Inventory Valuation is Automated
    - Create a new purchase order
    - Add vendor and two products from CAT with some quantities.
    - Confirm the order
    - With DEMO, go to the newly created receipt.
    - Edit the quantities to receive only the first product and create a back order.
    - Return to the admin user and go to the same purchase order
    - Create a new bill, remove the line with 0 quantity (might not be necessary) and validate.
    - With DEMO, go to the inventory app and go to the previously created back order receipt.
    - Edit the quantity to receive fully.
    - Now hit validate
    
    An access error raise because permission to read the invoice (bill) is required
    while it should not be the case since all the flow is in stock app.
    
    opw-2308399

--- addons/sale_stock/models/stock.py
+++ addons/sale_stock/models/stock.py
@@ -31,7 +31,7 @@ class StockMove(models.Model):
         related to this stock move.
         """
         rslt = super(StockMove, self)._get_related_invoices()
-        invoices = self.mapped('picking_id.sale_id.invoice_ids').filtered(lambda x: x.state not in ('draft', 'cancel'))
+        invoices = self.mapped('picking_id.sale_id.invoice_ids').filtered(lambda x: x.sudo().state not in ('draft', 'cancel'))
         rslt += invoices
         #rslt += invoices.mapped('refund_invoice_ids')
         return rslt
