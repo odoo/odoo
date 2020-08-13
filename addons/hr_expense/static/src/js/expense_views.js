95f5e5c7777a18545d633a98a8eee004c33eaf38
Yannick Tivisse <yti@odoo.com>
2020-08-13 11:25:21 +0200

    [IMP] hr_expense: Improve global UX (back2basics)
    TaskID: 2303951

--- addons/hr_expense/static/src/js/expense_views.js
+++ addons/hr_expense/static/src/js/expense_views.js
@@ -3,13 +3,16 @@ odoo.define('hr_expense.expenses.tree', function (require) {
     var DocumentUploadMixin = require('hr_expense.documents.upload.mixin');
     var KanbanController = require('web.KanbanController');
     var KanbanView = require('web.KanbanView');
+    var PivotView = require('web.PivotView');
     var ListController = require('web.ListController');
     var ListView = require('web.ListView');
     var viewRegistry = require('web.view_registry');
     var core = require('web.core');
     var ListRenderer = require('web.ListRenderer');
     var KanbanRenderer = require('web.KanbanRenderer');
+    var PivotRenderer = require('web.PivotRenderer');
     var session = require('web.session');
+    const config = require('web.config');
 
     var QWeb = core.qweb;
 
@@ -20,6 +23,38 @@ odoo.define('hr_expense.expenses.tree', function (require) {
             'change .o_expense_documents_upload .o_form_binary_form': '_onAddAttachment',
         }),
     });
+
+    const ExpenseQRCodeMixin = {
+        async _renderView() {
+            const self = this;
+            await this._super(...arguments);
+            const google_url = "https://play.google.com/store/apps/details?id=com.odoo.mobile";
+            const apple_url = "https://apps.apple.com/be/app/odoo/id1272543640";
+            const action_desktop = {
+                name: 'Download our App',
+                type: 'ir.actions.client',
+                tag: 'expense_qr_code_modal',
+                params: {'url': "https://apps.apple.com/be/app/odoo/id1272543640"},
+                target: 'new',
+            };
+            this.$el.find('img.o_expense_apple_store').on('click', function(event) {
+                event.preventDefault();
+                if (!config.device.isMobile) {
+                    self.do_action(_.extend(action_desktop, {params: {'url': apple_url}}));
+                } else {
+                    self.do_action({type: 'ir.actions.act_url', url: apple_url});
+                }
+            });
+            this.$el.find('img.o_expense_google_store').on('click', function(event) {
+                event.preventDefault();
+                if (!config.device.isMobile) {
+                    self.do_action(_.extend(action_desktop, {params: {'url': google_url}}));
+                } else {
+                    self.do_action({type: 'ir.actions.act_url', url: google_url});
+                }
+            });
+        },
+    };
 
     const ExpenseDashboardMixin = {
         _render: async function () {
@@ -50,9 +85,9 @@ odoo.define('hr_expense.expenses.tree', function (require) {
             }
             return value;
         }
-    }
+    };
 
-    var ExpenseListRenderer = ListRenderer.extend(ExpenseDashboardMixin);
+    var ExpenseListRenderer = ListRenderer.extend(ExpenseDashboardMixin, ExpenseQRCodeMixin);
 
     var ExpensesListViewDashboardUpload = ListView.extend({
         config: _.extend({}, ListView.prototype.config, {
@@ -75,7 +110,7 @@ odoo.define('hr_expense.expenses.tree', function (require) {
         }),
     });
 
-    var ExpenseKanbanRenderer = KanbanRenderer.extend(ExpenseDashboardMixin);
+    var ExpenseKanbanRenderer = KanbanRenderer.extend(ExpenseDashboardMixin, ExpenseQRCodeMixin);
 
     var ExpensesKanbanView = KanbanView.extend({
         config: _.extend({}, KanbanView.prototype.config, {
