166f636e64c8b9b5aff8c7ad0cbdd83ec5280207
Benjamin Vray <bvr@odoo.com>
2020-08-13 11:55:07 +0200

    [FIX] website: fix add slide carousel snippet
    Before this commit, when adding a new slide, the data-target of the new
    indicator was wrong because we were using the slide ID instead of the
    carousel ID.
    
    task-2318526

--- addons/website/static/src/js/editor/snippets.options.js
+++ addons/website/static/src/js/editor/snippets.options.js
@@ -1065,7 +1065,7 @@ options.registry.CarouselItem = options.Class.extend({
         const $items = this.$carousel.find('.carousel-item');
         this.$controls.removeClass('d-none');
         this.$indicators.append($('<li>', {
-            'data-target': '#' + this.$target.attr('id'),
+            'data-target': '#' + this.$carousel.attr('id'),
             'data-slide-to': $items.length,
         }));
         this.$indicators.append(' ');
