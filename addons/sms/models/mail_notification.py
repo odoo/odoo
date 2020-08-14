45499daba28526a82318e5cb528c6bc99fca0b09
xO-Tx <mou@odoo.com>
2020-08-14 13:04:00 +0200

    [IMP] sms: override cron activity to notify admins about sms failures
    This commit attempts to override "cron_notify_admins()" method and add notifications for failed sms messages to prevent modularity issues.
    
    task-2049528

--- addons/sms/models/mail_notification.py
+++ addons/sms/models/mail_notification.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api, tools, _
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 
 
 class MailNotification(models.Model):
@@ -19,3 +21,44 @@ class MailNotification(models.Model):
         ('sms_server', 'Server Error'),
         ('sms_acc', 'Unregistered Account')
     ])
+
+    @api.model
+    def _cron_notify_admins(self):
+        super()._cron_notify_admins()
+        cron = self.env.ref('mail.ir_cron_mail_notify_administrators')
+        previous_date = fields.Datetime.now() - relativedelta(**{cron.interval_type: cron.interval_number})
+        # Count failed SMS
+        failed_sms_counter = self.env['sms.sms'].sudo().search_count([
+            ('create_date', '>=', previous_date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)),
+            ('state', '=', 'error')])
+        # Send notifications about failed SMS (_notify_admins methods in daily cron job should set a repeat_delay of 1 day)
+        if failed_sms_counter:
+            sms_messages = {
+                "sms__failure": lambda failed_sms_counter, mail_sms_notifications_window_action: (
+                    _('Odoo has been unable to send SMS'),
+                    _(
+                        "%(failed_sms_counter)d SMS(s) could not be sent due to technical issues.\n"
+                        "Please check whether recipient's phone number exists and if your have enaugh IAP credits.\n"
+                        "To recharge your IAP credits please go to your 'IAP Portal' [Settings app > Odoo IAP > View my Services].\n"
+                        "From there, you can view your current balance, recharge your credits, review your consumption and "
+                        "set another reminder (by email) to when credits are low.\n"
+                        "You can check SMS notifications for more details about failed SMS messages (an error message should help you diagnose the issue.)"
+                    ) % {
+                        'failed_sms_counter': failed_sms_counter,
+                    },
+                    {
+                        "related_doc": 'https://www.odoo.com/documentation/user/online/general/in_app_purchase/in_app_purchase.html',
+                        "related_view_url": "/web#action=%(mail_sms_notifications_window_action)s&amp;view_type=list" % {
+                            'mail_sms_notifications_window_action': mail_sms_notifications_window_action
+                        },
+                        "related_view_text": "go to sms notifications"
+                    }
+                )
+            }
+            self._notify_admins(
+                *self._get_admin_notification('sms__failure', sms_messages)(
+                    failed_sms_counter,
+                    self.env.ref('mail.mail_notification_action').id
+                ),
+                repeat_delay=timedelta(days=1)
+            )
