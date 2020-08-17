2c5ce41c423e6ab92053b1f082086035bdc151f8
yhu-odoo <yhu@odoo.com>
2020-08-17 08:45:28 +0000

    [IMP] uom: change default rounding to two
    Change the default rouning digits of all UoMs to be two, also change the
    decimal.precision of UoM to be two. Adapt all the tests.
    Also to avoid hardcoded digits in `should_consume_qty` widget.
    
    PR #56000
    
    X-original-commit: 460ec0402a2352a5b179d81935f7230f6bc97cb7

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -102,7 +102,7 @@ class MrpProduction(models.Model):
     lot_producing_id = fields.Many2one(
         'stock.production.lot', string='Lot/Serial Number', copy=False,
         domain="[('product_id', '=', product_id), ('company_id', '=', company_id)]", check_company=True)
-    qty_producing = fields.Float(string="Quantity Producing", copy=False)
+    qty_producing = fields.Float(string="Quantity Producing", digits='Product Unit of Measure', copy=False)
     product_uom_category_id = fields.Many2one(related='product_id.uom_id.category_id')
     product_uom_qty = fields.Float(string='Total Quantity', compute='_compute_product_uom_qty', store=True)
     picking_type_id = fields.Many2one(
