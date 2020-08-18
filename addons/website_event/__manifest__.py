a91c36ce1746d84abb5805fc0c99b2e5d2627b08
Thibault Delavallée <tde@odoo.com>
2020-08-18 15:36:34 +0200

    [MOV] website_event: reorganize module content
    PURPOSE
    
    Clean organization and models linked to Event Online feature introduced in
    semi stable saas-13.3 at odoo/odoo@981f95bbf46658eda9f2d33751b9310cc67d86aa and odoo/enterprise@14722028ae0773c3710ea48ce48ff44703e83b5a
    
    Also clean website event menu not being available in website event but only
    in website event track, which implies some extra-code to manage it.
    
    In short: merge website_event_online in website_event, website_event_track
    _(online/session) in website_event_track.
    
    RATIONALE
    
    _online modules have been added to extend content of website_event and
    website_event_track without having any impact on those module. First step
    of cleaning is to move this content directly in base module.
    
    track_session module is mainly a rewrite of track module. Second step of
    cleaning is to move its content directly in website_event_track.
    
    SPECIFICATIONS
    
    Quickly reorganize some bits of code in website_event module to ease future
    integration of code from _online sub-modules. It also helps understanding
    and finding its way through the module.
    
    Split python files by model: notably event.type outside of event.event file
    and code.
    
    Reorganize templates by main use
    
      * _list: list view of events;
      * _page: page view of events, either event itself or registration flow
        (_page_registration);
      * move side templates in side files;
    
    LINKS
    
    Task ID-2319779
    COM odoo/odoo#56067
    ENT odoo/enterprise#12520
    UPG odoo/upgrade#1654

--- addons/website_event/__manifest__.py
+++ addons/website_event/__manifest__.py
@@ -1,23 +1,33 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
     'name': 'Events',
+    'version': '1.1',
     'category': 'Marketing/Events',
     'sequence': 140,
     'summary': 'Publish events, sell tickets',
     'website': 'https://www.odoo.com/page/events',
     'description': "",
-    'depends': ['website', 'website_partner', 'website_mail', 'event'],
+    'depends': [
+        'event',
+        'website',
+        'website_partner',
+        'website_mail',
+    ],
     'data': [
         'data/event_data.xml',
         'views/assets.xml',
         'views/res_config_settings_views.xml',
         'views/event_snippets.xml',
-        'views/event_templates.xml',
-        'views/event_templates_misc_pages.xml',
-        'views/event_templates_registration.xml',
+        'views/event_templates_list.xml',
+        'views/event_templates_page.xml',
+        'views/event_templates_page_registration.xml',
+        'views/event_templates_page_misc.xml',
+        'views/event_templates_widgets.xml',
         'views/website_templates.xml',
-        'views/event_views.xml',
+        'views/event_event_views.xml',
+        'views/event_type_views.xml',
         'security/ir.model.access.csv',
         'security/event_security.xml',
     ],
