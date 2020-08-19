e87d151b57dfc2ee1a758be652a5e664cc765014
Robin Heinz <rhe@odoo.com>
2020-08-19 11:57:35 +0200

    [IMP] point_of_sale: cashbox opening popup.
    Move the cashbox opening control from the left panel of the product screen widget to a popup that is raised at the opening of the session if its state is: opening_control.
    
    task-id: 2321973

--- addons/point_of_sale/static/src/js/Screens/ProductScreen/CashBoxOpening.js
+++ addons/point_of_sale/static/src/js/Screens/ProductScreen/CashBoxOpening.js
@@ -1,42 +1,71 @@
-odoo.define('point_of_sale.CashBoxOpening', function(require) {
+odoo.define('point_of_sale.CashOpeningPopup', function(require) {
     'use strict';
 
+    const { useState, useRef} = owl.hooks;
+    const { useListener } = require('web.custom_hooks');
+    const NumberBuffer = require('point_of_sale.NumberBuffer');
     const PosComponent = require('point_of_sale.PosComponent');
+    const AbstractAwaitablePopup = require('point_of_sale.AbstractAwaitablePopup');
     const Registries = require('point_of_sale.Registries');
-    const { Gui } = require('point_of_sale.Gui');
 
-    class CashBoxOpening extends PosComponent {
+
+    class CashOpeningPopup extends AbstractAwaitablePopup {
         constructor() {
             super(...arguments);
-            this.changes = {};
-            this.defaultValue = this.env.pos.bank_statement.balance_start || 0;
-            this.symbol = this.env.pos.currency.symbol;
+            this.cashBoxValue = this.env.pos.bank_statement.balance_start || 0;;
+            this.currency = this.env.pos.currency;
+            this.state = useState({
+                notes: "",
+            });
+            useListener('numpad-click-input', this._updateCashAmount);
+            useListener('update-cash', this._updateCashAmount);
+            this.inputRef = useRef('input');
+            NumberBuffer.use({
+                nonKeyboardInputEvent: 'numpad-click-input',
+                triggerAtInput: 'update-cash',
+                useWithBarcode: false,
+            });
         }
-        captureChange(event) {
-            this.changes[event.target.name] = event.target.value;
-        }
+
         startSession() {
-            let cashOpening = this.changes.cashBoxValue? this.changes.cashBoxValue: this.defaultValue;
-            if(isNaN(cashOpening)) {
-                Gui.showPopup('ErrorPopup',{
-                    'title': 'Wrong value',
-                    'body':  'Please insert a correct value.',
-                });
+            if(!parseFloat(this.cashBoxValue))
                 return;
-            }
-            this.env.pos.bank_statement.balance_start = cashOpening;
+            this.env.pos.bank_statement.balance_start = parseFloat(this.cashBoxValue);
             this.env.pos.pos_session.state = 'opened';
-            this.props.cashControl.cashControl = false;
             this.rpc({
-                    model: 'pos.session',
+                   model: 'pos.session',
                     method: 'set_cashbox_pos',
-                    args: [this.env.pos.pos_session.id, cashOpening, this.changes.notes],
+                    args: [this.env.pos.pos_session.id, parseFloat(this.cashBoxValue), this.state.notes],
                 });
+            this.trigger('close-popup');
         }
+
+        sendInput(value) {
+            this.trigger('numpad-click-input', { value });
+        }
+
+        async _updateCashAmount(event) {
+            let value = event.detail.value ? event.detail.value : event.detail.key
+            if(value !== "Backspace") {
+                if(this.cashBoxValue === 0) {
+                    this.cashBoxValue = value !== "."? value: this.cashBoxValue + value;
+                } else {
+                    this.cashBoxValue += value;
+                }
+            } else {
+                if(this.cashBoxValue.length > 1) {
+                    this.cashBoxValue = this.cashBoxValue.substring(0, this.cashBoxValue.length -1)
+                } else {
+                    this.cashBoxValue = 0;
+                }
+            }
+            this.render();
+        }
+
     }
-    CashBoxOpening.template = 'CashBoxOpening';
 
-    Registries.Component.add(CashBoxOpening);
+    CashOpeningPopup.template = 'CashOpeningPopup';
+    Registries.Component.add(CashOpeningPopup);
 
-    return CashBoxOpening;
+    return CashOpeningPopup;
 });
