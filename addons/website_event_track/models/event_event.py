0fc9e8c6760c525295b261c13be462ae0b1f0349
Thibault Delavallée <tde@odoo.com>
2020-08-19 12:43:33 +0000

    [MOV] website_event_track: reorganize module content
    PURPOSE
    
    Clean organization and models linked to Event Online feature introduced in
    semi stable saas-13.3 at odoo/odoo@981f95bbf46658eda9f2d33751b9310cc67d86aa and odoo/enterprise@14722028ae0773c3710ea48ce48ff44703e83b5a
    
    Also clean website event menu not being available in website event but only
    in website event track, which implies some extra-code to manage it.
    
    In short: merge website_event_online in website_event, website_event_track
    _(online/session) in website_event_track.
    
    RATIONALE
    
    _online modules have been added to extend content of website_event and
    website_event_track without having any impact on those module. First step
    of cleaning is to move this content directly in base module.
    
    track_session module is mainly a rewrite of track module. Second step of
    cleaning is to move its content directly in website_event_trackit.
    
    SPECIFICATIONS
    
    Quickly reorganize some bits of code in website_event_track module to ease
    future integration of code from _online sub-modules. It also helps
    understanding and finding its way through the module.
    
    Split python files by model: notably event.track sub models inside their own
    file to better follow future modifications.
    
    Reorganize templates by main use
    
      * _agenda: agenda view of tracks;
      * _list: views of tracks, list-based;
      * _page: a specific view of track, page-based;
      * move side templates in side files;
      * reorganize fields and add separators as this model will soon gain a lot
        of fields;
      * reorganize controllers to separate them by main use;
    
    LINKS
    
    Task ID-2319779
    COM odoo/odoo#56067
    ENT odoo/enterprise#12520
    UPG odoo/upgrade#1654

--- addons/website_event_track/models/event_event.py
+++ addons/website_event_track/models/event_event.py
@@ -5,24 +5,6 @@ from odoo import api, fields, models, _
 from odoo.addons.http_routing.models.ir_http import slug
 
 
-class EventType(models.Model):
-    _inherit = 'event.type'
-
-    website_track = fields.Boolean(
-        string='Tracks on Website', compute='_compute_website_menu_data',
-        readonly=False, store=True)
-    website_track_proposal = fields.Boolean(
-        string='Tracks Proposals on Website', compute='_compute_website_menu_data',
-        readonly=False, store=True)
-
-    @api.depends('website_menu')
-    def _compute_website_menu_data(self):
-        """ Simply activate or de-activate all menus at once. """
-        for event_type in self:
-            event_type.website_track = event_type.website_menu
-            event_type.website_track_proposal = event_type.website_menu
-
-
 class Event(models.Model):
     _inherit = "event.event"
 
