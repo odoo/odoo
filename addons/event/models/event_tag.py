e7d6773a72469c33960353c54ef59a3dc8091559
Thibault Delavallée <tde@odoo.com>
2020-08-19 10:04:46 +0000

    [MOV] (website_)event(_online): move tag color field to event
    PURPOSE
    
    Clean organization and models linked to Event Online feature introduced in
    semi stable saas-13.3 at odoo/odoo@981f95bbf46658eda9f2d33751b9310cc67d86aa and odoo/enterprise@14722028ae0773c3710ea48ce48ff44703e83b5a
    
    Also clean website event menu not being available in website event but only
    in website event track, which implies some extra-code to manage it.
    
    In short: merge website_event_online in website_event, website_event_track
    _(online/session) in website_event_track.
    
    RATIONALE
    
    _online modules have been added to extend content of website_event and
    website_event_track without having any impact on those module. First step
    of cleaning is to move this content directly in base module.
    
    track_session module is mainly a rewrite of track module. Second step of
    cleaning is to move its content directly in website_event_track.
    
    SPECIFICATIONS
    
    Move event.tag color field from website event online to event. This field is
    part of standard event models.
    
    LINKS
    
    Task ID-2319779
    COM odoo/odoo#56067
    ENT odoo/enterprise#12520
    UPG odoo/upgrade#1654

--- addons/event/models/event_tag.py
+++ addons/event/models/event_tag.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import _, api, fields, models
+from random import randint
+
+from odoo import api, fields, models
 
 
 class EventTagCategory(models.Model):
@@ -18,9 +20,15 @@ class EventTag(models.Model):
     _description = "Event Tag"
     _order = "sequence"
 
+    def _default_color(self):
+        return randint(1, 11)
+
     name = fields.Char("Name", required=True, translate=True)
     sequence = fields.Integer('Sequence', default=0)
     category_id = fields.Many2one("event.tag.category", string="Category", required=True, ondelete='cascade')
+    color = fields.Integer(
+        string='Color Index', default=lambda self: self._default_color(),
+        help='Tag color. No color means no display in kanban or front-end, to distinguish internal tags from public categorization tags.')
 
     def name_get(self):
         return [(tag.id, "%s: %s" % (tag.category_id.name, tag.name)) for tag in self]
