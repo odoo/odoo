766c7c3840332c6c104d831af5d7723e054a3d85
Jorge Luis Juarez <contacto@jorgejuarez.net>
2020-08-20 02:48:10 +0000

    [IMP] purchase: add hook method
    Add a method containing the information to create the supplier information
    
    closes odoo/odoo#56171
    
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -409,6 +409,17 @@ class PurchaseOrder(models.Model):
     def button_done(self):
         self.write({'state': 'done', 'priority': '0'})
 
+    def _prepare_supplier_info(self, partner, line, price, currency):
+        # Prepare supplierinfo data when adding a product
+        return {
+            'name': partner.id,
+            'sequence': max(line.product_id.seller_ids.mapped('sequence')) + 1 if line.product_id.seller_ids else 1,
+            'min_qty': 0.0,
+            'price': price,
+            'currency_id': currency.id,
+            'delay': 0,
+        }
+
     def _add_supplier_to_product(self):
         # Add the partner in the supplier list of the product if the supplier is not registered for
         # this product. We limit to 10 the number of suppliers for a product to avoid the mess that
@@ -425,14 +436,7 @@ class PurchaseOrder(models.Model):
                     default_uom = line.product_id.product_tmpl_id.uom_po_id
                     price = line.product_uom._compute_price(price, default_uom)
 
-                supplierinfo = {
-                    'name': partner.id,
-                    'sequence': max(line.product_id.seller_ids.mapped('sequence')) + 1 if line.product_id.seller_ids else 1,
-                    'min_qty': 0.0,
-                    'price': price,
-                    'currency_id': currency.id,
-                    'delay': 0,
-                }
+                supplierinfo = self._prepare_supplier_info(partner, line, price, currency)
                 # In case the order partner is a contact address, a new supplierinfo is created on
                 # the parent company. In this case, we keep the product name and code.
                 seller = line.product_id._select_seller(
