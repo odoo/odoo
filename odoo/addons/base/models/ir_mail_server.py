9ecc9d7e2bf194a4a88c136ba303d89bcb88c6ce
asa-odoo <asa@odoo.com>
2020-08-20 12:29:48 +0000

    [IMP] base: improve notification of successful mail server connection
    This commit improves the notification displayed when we configure
    the mail server and click 'Test Connection' button. It removes the
    title of the notification, improves the message string and changes
    the notification type to 'success' instead of default 'warning'.
    
    Task Id : 2321763
    PR #56213
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- odoo/addons/base/models/ir_mail_server.py
+++ odoo/addons/base/models/ir_mail_server.py
@@ -153,14 +153,13 @@ class IrMailServer(models.Model):
                     # ignored, just a consequence of the previous exception
                     pass
 
-        title = _("Connection Test Succeeded!")
-        message = _("Everything seems properly set up!")
+        message = _("Connection Test Successful!")
         return {
             'type': 'ir.actions.client',
             'tag': 'display_notification',
             'params': {
-                'title': title,
                 'message': message,
+                'type': 'success',
                 'sticky': False,
             }
         }
