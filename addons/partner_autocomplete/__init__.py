5236bb1f7f579b17c4714cf637ff621b78b265f4
David Beguin <dbe@odoo.com>
2020-08-21 11:52:02 +0200

    [IMP] partner_autocomplete: enrich base company after DB install
    In order to make a wow effect right after DB install, we enrich the base
    company based on the given email address at install.
    
    To do so, we use the partner_autocomplete service from IAP, using 1 of the
    free credits that are offered to new clients on saas plateform.
    
    Only the fields that are not filled yet are enriched.
    
    Task ID: 2322455

--- addons/partner_autocomplete/__init__.py
+++ addons/partner_autocomplete/__init__.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from . import models
+from odoo import api, SUPERUSER_ID
+
+
+def enrich_base_company(cr, registry):
+    """ Will attempt to enrich the base company if sufficient information & credits are available.
+    If the enrichment does not work, ignore the error as this is not a critical process. """
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.company._enrich()
--- addons/partner_autocomplete/models/res_company.py
+++ addons/partner_autocomplete/models/res_company.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+import json
+from urllib.parse import urlparse
+
+from odoo import fields, models, tools, _
+
 
 class ResCompany(models.Model):
     _name = 'res.company'
@@ -12,3 +16,58 @@ class ResCompany(models.Model):
     def _inverse_partner_gid(self):
         for company in self:
             company.partner_id.partner_gid = company.partner_gid
+
+    def _enrich(self):
+        """
+        This method calls the partner autocomplete service from IAP to enrich the partner related fields
+        of the company.
+        """
+        self.ensure_one()
+
+        company_domain = self._get_company_domain()
+        if not company_domain:
+            return {'error': _("Could not extract company domain. Impossible to enrich the company.")}
+
+        company_data = self.env['res.partner'].enrich_company(company_domain, False, self.vat)
+        if company_data.get('error'):
+            return {'error': company_data.get('error_message', _('Unkown reason'))}
+
+        for field in ['state_id', 'country_id']:
+            company_data[field] = company_data.get(field, {}).get('id')
+
+        additional_data = company_data.pop('additional_info', False)
+
+        # Keep only truthy values that are not already set on the target partner
+        company_data = {field: value for field, value in company_data.items()
+                        if field in self.partner_id._fields and value and not self.partner_id[field]}
+
+        self.partner_id.write(company_data)
+        if additional_data:
+            template_values = json.loads(additional_data)
+            template_values['flavor_text'] = _("Company auto-completed by Odoo Partner Autocomplete Service")
+            self.partner_id.message_post_with_view(
+                'iap_mail.enrich_company',
+                values=template_values,
+                subtype_id=self.env.ref('mail.mt_note').id,
+            )
+        return {}
+
+    def _get_company_domain(self):
+        """ Extract the company domain to be used by IAP services.
+        The domain is extracted from the website or the email information.
+        e.g:
+            - www.info.proximus.be -> proximus.be
+            - info@proximus.be -> proximus.be """
+        self.ensure_one()
+
+        if self.website:
+            company_domain = urlparse(self.website).netloc
+            if '.' in company_domain:
+                return '.'.join(company_domain.split('.')[-2:])  # remove subdomains
+
+        if self.email:
+            normalized_email = tools.email_normalize(self.email)
+            if normalized_email:
+                return normalized_email.split('@')[1]
+
+        return False
