cf26d60a529b5c7875d5f6978f45112617c5615c
Dominik Rubo <dru@odoo.com>
2020-08-21 11:46:28 +0200

    [IMP] project: enable config-selected features in projects
    This change ensures that when the user activates optional features of the
    project app, they are enabled in existing projects as well as newly created
    projects automatically, which is likely what the user expects.
    
    For most optional features, this behavior was already in place. It was not in
    place yet for the sub-tasks and timesheets features, which this change fixes.
    
    Two added tests check the behavior described above for features that are
    activated through a `group_*` field on `res.config.settings` (sub-tasks,
    recurring tasks and ratings). The tests do not cover features that are
    activated through a `module_*` field, as such fields require the installation
    of further modules and we cannot simulate this.
    
    Task 2297054
    
    X-original-commit: 8dd0edae898a532dbe7e4f18c9118b9b68e57900
    Co-authored-by: Kevin Baptiste <50135801+kba-odoo@users.noreply.github.com>

--- addons/project/tests/__init__.py
+++ addons/project/tests/__init__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from . import test_project_base, test_project_flow, test_access_rights, test_project_recurrence, test_project_ui
+from . import test_access_rights
+from . import test_project_base
+from . import test_project_config
+from . import test_project_flow
+from . import test_project_recurrence
+from . import test_project_ui
 from . import test_multicompany
