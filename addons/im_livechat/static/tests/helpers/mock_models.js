c9a334bb1c9b5b1df843e3d8b5a5ab042e86680c
Sébastien Theys <seb@odoo.com>
2020-08-24 12:37:47 +0200

    [FIX] mail, im_livechat: make livechat work properly
    - fix traceback on receiving first livechat message due to bad computation of
      correspondent
    - fix missing [0] in return of country name_get in channel_info
    - fix visitor/correspondent computation to work when chatting with oneself
    - reintroduce livechat ordered by last message in discuss sidebar
    - improve mock server to make these fixes possible to test
    - introduce corresponding tests
    
    PR: #56243
    task-2323670

--- addons/im_livechat/static/tests/helpers/mock_models.js
+++ addons/im_livechat/static/tests/helpers/mock_models.js
@@ -15,9 +15,23 @@ MockModels.patch('im_livechat/static/tests/helpers/mock_models.js', T =>
          */
         static generateData() {
             const data = super.generateData(...arguments);
+            Object.assign(data, {
+                'im_livechat.channel': {
+                    fields: {
+                        user_ids: { string: "Operators", type: 'many2many', relation: 'res.users' }
+                    },
+                    records: [],
+                }
+            });
             Object.assign(data['mail.channel'].fields, {
-                // fake type, not on Python models but is the result of a formatter
-                livechat_visitor: { string: 'livechat_visitor', type: 'Object', default: false },
+                anonymous_name: { string: "Anonymous Name", type: 'char' },
+                country_id: { string: "Country", type: 'many2one', relation: 'res.country' },
+                livechat_active: { string: "Is livechat ongoing?", type: 'boolean', default: false },
+                livechat_channel_id: { string: "Channel", type: 'many2one', relation: 'im_livechat.channel' },
+                livechat_operator_id: { string: "Operator", type: 'many2one', relation: 'res.partner' },
+            });
+            Object.assign(data['res.users'].fields, {
+                livechat_username: { string: 'Livechat Username', type: 'string' },
             });
             return data;
         }
