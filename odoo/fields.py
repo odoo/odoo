325e68900595896e1b9bbc8b8cdfc536d97fd956
Victor Feyens <vfe@odoo.com>
2020-08-25 16:03:57 +0200

    [FIX] fields: do not warn correct field behavior
    When a selection field is specified as related through model 
    inheritance, we shouldn't log the old selection choice as an error.
    
    It should only be logged if the selection/selection_add is specified in 
    a later inheritance, after the field was changed to a related compute.

--- odoo/fields.py
+++ odoo/fields.py
@@ -2390,12 +2390,15 @@ class Selection(Field):
         # determine selection (applying 'selection_add' extensions)
         values = None
         labels = {}
+        previous = None
 
         for field in self._base_fields:
             # We cannot use field.selection or field.selection_add here
             # because those attributes are overridden by ``_setup_attrs``.
             if 'selection' in field.args:
-                if self.related:
+                if previous and 'related' in previous.args:
+                    # Only log 'useless' selection attribute when it is defined AFTER
+                    # the field was defined as compute related, not before ...
                     _logger.warning("%s: selection attribute will be ignored as the field is related", self)
                 selection = field.args['selection']
                 if isinstance(selection, list):
@@ -2411,7 +2414,9 @@ class Selection(Field):
                     self.ondelete = None
 
             if 'selection_add' in field.args:
-                if self.related:
+                if previous and 'related' in previous.args:
+                    # Only log 'useless' selection attribute when it is defined AFTER
+                    # the field was defined as compute related, not before ...
                     _logger.warning("%s: selection_add attribute will be ignored as the field is related", self)
                 selection_add = field.args['selection_add']
                 assert isinstance(selection_add, list), \
@@ -2454,6 +2459,7 @@ class Selection(Field):
                 labels.update(kv for kv in selection_add if len(kv) == 2)
                 self.ondelete.update(ondelete)
 
+            previous = field
         if values is not None:
             self.selection = [(value, labels[value]) for value in values]
 
