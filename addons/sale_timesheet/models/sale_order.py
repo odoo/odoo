ef10da87fd58a4e6e8334096206806ec23bd8922
jbm-odoo <jbm@odoo.com>
2020-08-27 09:18:46 +0200

    [FIX] *_timesheet: Timesheet duration
    Before this commit:
    https://github.com/odoo/odoo/pull/55507/commits/67a8177064953abf0f03e77238ce5d24149ffdaf introduces a bug:
    the ratio used to compute the display of timesheet duration is always 1.
    
    After this commit:
    The ratio is correctly computed.
    The timesheet total duration is computed by ignoring the units of
    measure because timesheets are always expressed in hours and the
    widget already makes the conversion.
    
    TaskID 2326216

--- addons/sale_timesheet/models/sale_order.py
+++ addons/sale_timesheet/models/sale_order.py
@@ -65,14 +65,7 @@ class SaleOrder(models.Model):
     @api.depends('timesheet_ids', 'company_id.timesheet_encode_uom_id')
     def _compute_timesheet_total_duration(self):
         for sale_order in self:
-            duration_list = []
-            for timesheet in sale_order.timesheet_ids:
-                timesheet_uom = timesheet.product_uom_id or timesheet.company_id.project_time_mode_id
-                if timesheet_uom != sale_order.timesheet_encode_uom_id and timesheet_uom.category_id == sale_order.timesheet_encode_uom_id.category_id:
-                    duration_list.append(timesheet_uom._compute_quantity(timesheet.unit_amount, sale_order.timesheet_encode_uom_id))
-                else:
-                    duration_list.append(timesheet.unit_amount)
-            sale_order.timesheet_total_duration = sum(duration_list)
+            sale_order.timesheet_total_duration = sum(sale_order.timesheet_ids.mapped('unit_amount'))
 
     @api.onchange('project_id')
     def _onchange_project_id(self):
