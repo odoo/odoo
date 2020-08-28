38c03e68a27c4716af4ab9f7e3bedb465284e5ec
Raphael Collet <rco@odoo.com>
2020-08-28 16:11:21 +0200

    [FIX] core: cursor hooks API and implementation
    Python 3.8 changed the equality rules for bound methods to be based on
    the *identity* of the receiver (`__self__`) rather than its *equality*.
    This means that in 3.7, methods from different instances will compare
    (and hash) equal, thereby landing in the same map "slot", but that isn't
    the case in 3.8.
    
    While it's usually not relevant, it's an issue for `GroupCalls` which is
    indexed by a function: in 3.7, that being a method from recordsets
    comparing equal will deduplicate them, but not anymore in 3.8, leading
    to duplicated callbacks (exactly the thing GroupCalls aims to avoid).
    
    Also, the API of `GroupCalls` turned out to be unusual and weird.  The
    bug above is fixed by using a plain list for callbacks, thereby avoiding
    comparisons between registered functions.  The API is now:
    
        callbacks.add(func)     # add func to callbacks
        callbacks.run()         # run all callbacks in addition order
        callbacks.clear()       # remove all callbacks
    
    In order to handle aggregated data, the `callbacks` object provides a
    dictionary `callbacks.data` that any callback function can freely use.
    For the sake of consistency, the `callbacks.data` dict is automatically
    cleared upon execution of callbacks.
    
    Discovered by @william-andre
    
    Related to odoo#56583
    
    References:
    
    * https://bugs.python.org/issue1617161
    * python/cpython#7848
    * https://docs.python.org/3/whatsnew/changelog.html#python-3-8-0-alpha-1
      (no direct link because individual entries are not linkable, look for
      bpo-1617161)

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -375,12 +375,12 @@ class BaseCase(TreeCase, MetaCase('DummyCase', (object,), {})):
                 expected = counters.get(login, default)
                 if flush:
                     self.env.user.flush()
-                    self.env.cr.precommit()
+                    self.env.cr.precommit.run()
                 count0 = self.cr.sql_log_count
                 yield
                 if flush:
                     self.env.user.flush()
-                    self.env.cr.precommit()
+                    self.env.cr.precommit.run()
                 count = self.cr.sql_log_count - count0
                 if count != expected:
                     # add some info on caller to allow semi-automatic update of query count
@@ -399,11 +399,11 @@ class BaseCase(TreeCase, MetaCase('DummyCase', (object,), {})):
             # same operations, otherwise the caches might not be ready!
             if flush:
                 self.env.user.flush()
-                self.env.cr.precommit()
+                self.env.cr.precommit.run()
             yield
             if flush:
                 self.env.user.flush()
-                self.env.cr.precommit()
+                self.env.cr.precommit.run()
 
     def assertRecordValues(self, records, expected_values):
         ''' Compare a recordset with a list of dictionaries representing the expected results.
