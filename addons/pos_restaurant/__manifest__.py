a84d559645e7890390cfc21847169158254bb726
Antoine Prieels <anp@odoo.com>
2020-08-28 11:43:11 +0000

    [IMP] pos_restaurant: Open tabs Usability
    Improve usability of open tabs in the POS.
    
    closes odoo/odoo#56785
    
    X-original-commit: bbe80dcb30c1f5f028bf05cd14a514c627f4ee92
    Signed-off-by: pimodoo <pimodoo@users.noreply.github.com>
    Signed-off-by: Antoine Prieëls <aprieels@users.noreply.github.com>

--- addons/pos_restaurant/__manifest__.py
+++ addons/pos_restaurant/__manifest__.py
@@ -42,6 +42,7 @@ This module adds several features to the Point of Sale that are specific to rest
         'static/src/xml/Screens/SplitBillScreen/SplitOrderline.xml',
         'static/src/xml/Screens/ReceiptScreen/OrderReceipt.xml',
         'static/src/xml/Screens/ProductScreen/Orderline.xml',
+        'static/src/xml/Screens/PaymentScreen/PaymentScreen.xml',
         'static/src/xml/Screens/PaymentScreen/PaymentScreenElectronicPayment.xml',
         'static/src/xml/Screens/FloorScreen/FloorScreen.xml',
         'static/src/xml/Screens/FloorScreen/EditBar.xml',
