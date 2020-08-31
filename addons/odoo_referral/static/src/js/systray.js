d30e9f96ef1a8dafe8c93d9161dae3c5b7c1c170
Mohammed Shekha <msh@odoo.com>
2020-08-31 19:41:22 +0530

    [REF] convert odoo referral systray icon to owl
    In order to prepare the future, we want to convert everything in Owl.
    This commit converts the Odoo Referral Icon displayed in Systray into owl.
    
    task-2328747

--- addons/odoo_referral/static/src/js/systray.js
+++ addons/odoo_referral/static/src/js/systray.js
@@ -1,61 +1,66 @@
 odoo.define('systray.systray_odoo_referral', function (require) {
     "use strict";
-    var core = require('web.core');
-    var localStorage = require('web.local_storage');
-    var SystrayMenu = require('web.SystrayMenu');
-    var Widget = require('web.Widget');
 
-    var _t = core._t;
-    var ActionMenu = Widget.extend({
-        template: 'systray_odoo_referral.gift_icon',
-        events: {
-            'click .gift_icon': 'onclick_gifticon',
-        },
+    const localStorage = require('web.local_storage');
+    const SystrayMenu = require('web.SystrayMenu');
 
-        start: function (parent) {
-            var self = this;
-            var lastFetch = localStorage.getItem('odoo_referral.updates_last_fetch');
-            var updatesCount = localStorage.getItem('odoo_referral.updates_count');
-            var hasClicked = localStorage.getItem('odoo_referral.has_clicked');
-            if (hasClicked && (!lastFetch || Date(parseInt(lastFetch)) < Date(new Date().getTime() - (24 * 60 * 60 * 1000)))) {
-                this._rpc({
-                    model: 'res.users',
-                    method: 'get_referral_updates_count_for_current_user'
-                }, {
-                    shadow: true,
-                }).then(function (count) {
-                    localStorage.setItem('odoo_referral.updates_last_fetch', Date.now());
-                    localStorage.setItem('odoo_referral.updates_count', count);
-                    if (count > 0) {
-                        self.$('.o_notification_counter').text(count);
+    class ActionMenu extends owl.Component {
+
+        async mounted(parent) {
+            const lastFetch = localStorage.getItem("odoo_referral.updates_last_fetch");
+            const updatesCount = localStorage.getItem("odoo_referral.updates_count");
+            const hasClicked = localStorage.getItem("odoo_referral.has_clicked");
+            if (
+                hasClicked &&
+                (!lastFetch ||
+                    Date(parseInt(lastFetch)) < Date(new Date().getTime() - 24 * 60 * 60 * 1000))
+            ) {
+                const count = await this.env.services.rpc(
+                    {
+                        model: "res.users",
+                        method: "get_referral_updates_count_for_current_user",
+                    },
+                    {
+                        shadow: true,
                     }
-                });
+                );
+                localStorage.setItem("odoo_referral.updates_last_fetch", Date.now());
+                localStorage.setItem("odoo_referral.updates_count", count);
+                if (count > 0) {
+                    this.el.querySelector(".o_notification_counter").innerText = count;
+                }
             } else {
                 if (updatesCount && updatesCount > 0) {
-                    self.$('.o_notification_counter').text(updatesCount);
+                    this.el.querySelector(".o_notification_counter").innerText = updatesCount;
                 }
             }
-            return this._super.apply(this, arguments);
-        },
+            return super.mounted(...arguments);
+        }
 
-        onclick_gifticon: function () {
-            var self = this;
-            localStorage.setItem('odoo_referral.updates_count', 0);
-            this._rpc({
-                route: '/odoo_referral/go/'
-            }).then(function (result) {
-                localStorage.setItem('odoo_referral.has_clicked', 1);
-                self.$('.o_notification_counter').text('');
-                var w = window.open(result.link, '_blank', 'noreferrer noopener');
-                if (!w || w.closed || typeof w.closed === 'undefined') {
-                    var message = _t('A popup window has been blocked. You ' +
-                                 'may need to change your browser settings to allow ' +
-                                 'popup windows for this page.');
-                    self.do_warn(false, message, true);
-                }
+        async onClickGiftIcon() {
+            localStorage.setItem("odoo_referral.updates_count", 0);
+            const result = await this.env.services.rpc({
+                route: "/odoo_referral/go/",
             });
-        },
-    });
+            localStorage.setItem("odoo_referral.has_clicked", 1);
+            this.el.querySelector(".o_notification_counter").innerText = "";
+            const w = window.open(result.link, "_blank", "noreferrer noopener");
+            if (!w || w.closed || typeof w.closed === "undefined") {
+                const message = this.env._t(
+                    "A popup window has been blocked. You " +
+                        "may need to change your browser settings to allow " +
+                        "popup windows for this page."
+                );
+                this.env.services.notification.notify({
+                    type: "danger",
+                    message: message,
+                    sticky: true,
+                });
+            }
+        }
+    }
+
+    ActionMenu.template = "systray_odoo_referral.gift_icon";
 
     SystrayMenu.Items.push(ActionMenu);
     return ActionMenu;
