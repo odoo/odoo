4b79e16d9659d544b5965eafbf8d899c12d1fca5
Martin Trigaux <mat@odoo.com>
2020-08-31 08:08:21 +0000

    [FIX] base: keep evaluation context unchanged
    Before this commit, the evaluation context was forced with sudo(False)
    as the call to _render_qweb_html was made with sudo(True). The sudo
    was removed to prevent rendering a report on any object.
    
    This was problematic for reports such as sale.order portal page where
    the public user does not have access to the record but uses a custom
    verification mechanism using an access_token field and then calling
    _render with sudo.
    
    This commit moves the sudo call to keep the _get_evaluation_context
    with the same environment and allow to call _render with sudo
    
    Add tests with sale portal building the report as low-priviledge user
    
    closes odoo/odoo#56890
    
    X-original-commit: 57665a02f7b4cd113f8f40ec47c58a518fe8669c
    Related: odoo/enterprise#12845
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- odoo/addons/base/models/ir_actions_report.py
+++ odoo/addons/base/models/ir_actions_report.py
@@ -694,13 +694,13 @@ class IrActionsReport(models.Model):
             data = {}
         data.setdefault('report_type', 'pdf')
 
-        # access the report details with sudo() but evaluation context as sudo(False)
+        # access the report details with sudo() but evaluation context as current user
         self_sudo = self.sudo()
 
         # In case of test environment without enough workers to perform calls to wkhtmltopdf,
         # fallback to render_html.
         if (tools.config['test_enable'] or tools.config['test_file']) and not self.env.context.get('force_report_rendering'):
-            return self_sudo._render_qweb_html(res_ids, data=data)
+            return self._render_qweb_html(res_ids, data=data)
 
         # As the assets are generated during the same transaction as the rendering of the
         # templates calling them, there is a scenario where the assets are unreachable: when
@@ -791,7 +791,7 @@ class IrActionsReport(models.Model):
             data = {}
         data.setdefault('report_type', 'text')
         data = self._get_rendering_context(docids, data)
-        return self._render_template(self.report_name, data), 'text'
+        return self._render_template(self.sudo().report_name, data), 'text'
 
     @api.model
     def _render_qweb_html(self, docids, data=None):
@@ -801,7 +801,7 @@ class IrActionsReport(models.Model):
             data = {}
         data.setdefault('report_type', 'html')
         data = self._get_rendering_context(docids, data)
-        return self._render_template(self.report_name, data), 'html'
+        return self._render_template(self.sudo().report_name, data), 'html'
 
     @api.model
     def _get_rendering_context_model(self):
@@ -810,23 +810,23 @@ class IrActionsReport(models.Model):
 
     @api.model
     def _get_rendering_context(self, docids, data):
+        # access the report details with sudo() but evaluation context as current user
+        self_sudo = self.sudo()
+
         # If the report is using a custom model to render its html, we must use it.
         # Otherwise, fallback on the generic html rendering.
-        report_model = self._get_rendering_context_model()
+        report_model = self_sudo._get_rendering_context_model()
 
         data = data and dict(data) or {}
 
 
         if report_model is not None:
-            # _render_ may be executed in sudo but evaluation context as real user
-            report_model = report_model.sudo(False)
             data.update(report_model._get_report_values(docids, data=data))
         else:
-            # _render_ may be executed in sudo but evaluation context as real user
-            docs = self.env[self.model].sudo(False).browse(docids)
+            docs = self.env[self_sudo.model].browse(docids)
             data.update({
                 'doc_ids': docids,
-                'doc_model': self.model,
+                'doc_model': self_sudo.model,
                 'docs': docs,
             })
         return data
