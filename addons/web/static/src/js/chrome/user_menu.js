583bbf981fc8a7d226c5242f616921e8043f9dff
Mohammed Shekha <msh@odoo.com>
2020-09-01 13:42:45 +0530

    [REF] web: convert usemenu of systray to owl
    In order to prepare the future, we want to convert everything in Owl.
    This commit converts the Usermenu displayed in Systray into owl.
    
    task-2329195

--- addons/web/static/src/js/chrome/user_menu.js
+++ addons/web/static/src/js/chrome/user_menu.js
@@ -1,132 +1,152 @@
 odoo.define('web.UserMenu', function (require) {
-"use strict";
-
-/**
- * This widget is appended by the webclient to the right of the navbar.
- * It displays the avatar and the name of the logged user (and optionally the
- * db name, in debug mode).
- * If clicked, it opens a dropdown allowing the user to perform actions like
- * editing its preferences, accessing the documentation, logging out...
- */
-
-var config = require('web.config');
-var core = require('web.core');
-var framework = require('web.framework');
-var Dialog = require('web.Dialog');
-var Widget = require('web.Widget');
-
-var _t = core._t;
-var QWeb = core.qweb;
-
-var UserMenu = Widget.extend({
-    template: 'UserMenu',
+    "use strict";
 
     /**
-     * @override
-     * @returns {Promise}
+     * This widget is appended by the webclient to the right of the navbar.
+     * It displays the avatar and the name of the logged user (and optionally the
+     * db name, in debug mode).
+     * If clicked, it opens a dropdown allowing the user to perform actions like
+     * editing its preferences, accessing the documentation, logging out...
      */
-    start: function () {
-        var self = this;
-        var session = this.getSession();
-        this.$el.on('click', '[data-menu]', function (ev) {
-            ev.preventDefault();
-            var menu = $(this).data('menu');
-            self['_onMenu' + menu.charAt(0).toUpperCase() + menu.slice(1)]();
-        });
-        return this._super.apply(this, arguments).then(function () {
-            var $avatar = self.$('.oe_topbar_avatar');
+
+    const config = require('web.config');
+    const Dialog = require("web.OwlDialog");
+    const framework = require('web.framework');
+    const patchMixin = require("web.patchMixin");
+
+    const { useState } = owl.hooks;
+
+    class UserMenu extends owl.Component {
+        constructor() {
+            super(...arguments);
+            this.state = useState({ showShortcuts: false });
+        }
+        /**
+         * @override
+         * @returns {Promise}
+         */
+        mounted() {
+            const session = this.env.session;
+            super.mounted(...arguments);
+            const avatar = this.el.querySelector(".oe_topbar_avatar");
             if (!session.uid) {
-                $avatar.attr('src', $avatar.data('default-src'));
+                avatar && avatar.setAttribute("src", avatar.getAttribute("data-default-src"));
                 return Promise.resolve();
             }
-            var topbar_name = session.name;
+            let topbarName = session.name;
             if (config.isDebug()) {
-                topbar_name = _.str.sprintf("%s (%s)", topbar_name, session.db);
+                topbarName = `${topbarName} (${session.db})`;
             }
-            self.$('.oe_topbar_name').text(topbar_name);
-            var avatar_src = session.url('/web/image', {
-                model:'res.users',
-                field: 'image_128',
+            const topbarElement = this.el.querySelector(".oe_topbar_name");
+            if (topbarElement) {
+                topbarElement.innerText = topbarName;
+            }
+            const avatarSrc = session.url("/web/image", {
+                model: "res.users",
+                field: "image_128",
                 id: session.uid,
             });
-            $avatar.attr('src', avatar_src);
-        });
-    },
+            if (avatar) {
+                avatar.setAttribute("src", avatarSrc);
+            }
+        }
 
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
+        //--------------------------------------------------------------------------
+        // Public
+        //--------------------------------------------------------------------------
 
-    /**
-     * @private
-     */
-    _onMenuAccount: function () {
-        var self = this;
-        this.trigger_up('clear_uncommitted_changes', {
-            callback: function () {
-                self._rpc({route: '/web/session/account'})
-                    .then(function (url) {
-                        framework.redirect(url);
-                    })
-                    .guardedCatch(function (result, ev){
-                        ev.preventDefault();
-                        framework.redirect('https://accounts.odoo.com/account');
-                    });
-            },
-        });
-    },
-    /**
-     * @private
-     */
-    _onMenuDocumentation: function () {
-        window.open('https://www.odoo.com/documentation/user', '_blank');
-    },
-    /**
-     * @private
-     */
-    _onMenuLogout: function () {
-        this.trigger_up('clear_uncommitted_changes', {
-            callback: this.do_action.bind(this, 'logout'),
-        });
-    },
-    /**
-     * @private
-     */
-    _onMenuSettings: function () {
-        var self = this;
-        var session = this.getSession();
-        this.trigger_up('clear_uncommitted_changes', {
-            callback: function () {
-                self._rpc({
-                        model: "res.users",
-                        method: "action_get"
-                    })
-                    .then(function (result) {
-                        result.res_id = session.uid;
-                        self.do_action(result);
-                    });
-            },
-        });
-    },
-    /**
-     * @private
-     */
-    _onMenuSupport: function () {
-        window.open('https://www.odoo.com/buy', '_blank');
-    },
-    /**
-     * @private
-     */
-    _onMenuShortcuts: function() {
-        new Dialog(this, {
-            size: 'large',
-            dialogClass: 'o_act_window',
-            title: _t("Keyboard Shortcuts"),
-            $content: $(QWeb.render("UserMenu.shortcuts"))
-        }).open();
-    },
-});
+        doAction(params) {
+            return this.trigger("do-action", Object.assign({}, params));
+        }
 
-return UserMenu;
+        //--------------------------------------------------------------------------
+        // Handlers
+        //--------------------------------------------------------------------------
+
+        /**
+         * @private
+         * @param {MouseEvent} ev
+         */
+        _onClickMenu(ev) {
+            const menu = ev.target.getAttribute("data-menu");
+            this["_onMenu" + menu.charAt(0).toUpperCase() + menu.slice(1)]();
+        }
+        /**
+         * Closes shortcut dialog
+         *
+         * @private
+         */
+        _onCloseShortcuts() {
+            this.state.showShortcuts = false;
+        }
+        /**
+         * @private
+         */
+        _onMenuAccount() {
+            this.trigger("clear_uncommitted_changes", {
+                callback: () => {
+                    this.env.services
+                        .rpc({ route: "/web/session/account" })
+                        .then(function (url) {
+                            framework.redirect(url);
+                        })
+                        .guardedCatch(function (result, ev) {
+                            ev.preventDefault();
+                            framework.redirect("https://accounts.odoo.com/account");
+                        });
+                },
+            });
+        }
+        /**
+         * @private
+         */
+        _onMenuDocumentation() {
+            window.open("https://www.odoo.com/documentation/user", "_blank");
+        }
+        /**
+         * @private
+         */
+        _onMenuLogout() {
+            this.trigger("clear_uncommitted_changes", {
+                callback: this.doAction.bind(this, { action: "logout" }),
+            });
+        }
+        /**
+         * @private
+         */
+        _onMenuSettings() {
+            const session = this.env.session;
+            this.trigger("clear_uncommitted_changes", {
+                callback: () => {
+                    this.env.services
+                        .rpc({
+                            model: "res.users",
+                            method: "action_get",
+                        })
+                        .then((result) => {
+                            result.res_id = session.uid;
+                            this.doAction({ action: result });
+                        });
+                },
+            });
+        }
+        /**
+         * @private
+         */
+        _onMenuSupport() {
+            window.open("https://www.odoo.com/buy", "_blank");
+        }
+        /**
+         * @private
+         */
+        _onMenuShortcuts() {
+            this.state.showShortcuts = true;
+        }
+    }
+
+    UserMenu.template = 'UserMenu';
+    UserMenu.components = { Dialog }
+
+    return patchMixin(UserMenu);
 
 });
--- addons/web/static/tests/chrome/user_menu_tests.js
+++ addons/web/static/tests/chrome/user_menu_tests.js
@@ -1,9 +1,10 @@
 odoo.define('web.user_menu_tests', function (require) {
 "use strict";
 
-var testUtils = require('web.test_utils');
-var UserMenu = require('web.UserMenu');
-var Widget = require('web.Widget');
+const testUtils = require('web.test_utils');
+const UserMenu = require('web.UserMenu');
+
+const { createComponent } = testUtils;
 
 QUnit.module('chrome', {}, function () {
     QUnit.module('UserMenu');
@@ -11,21 +12,17 @@ QUnit.module('chrome', {}, function () {
     QUnit.test('basic rendering', async function (assert) {
         assert.expect(3);
 
-        var parent = new Widget();
-
-        await testUtils.mock.addMockEnvironment(parent, {});
-        var userMenu = new UserMenu(parent);
-        await userMenu.appendTo($('body'));
+        const userMenu = await createComponent(UserMenu, {});
+        await userMenu.mount(document.querySelector("body"));
 
         assert.strictEqual($('.o_user_menu').length, 1,
             "should have a user menu in the DOM");
-        assert.hasClass(userMenu.$el,'o_user_menu',
+        assert.hasClass(userMenu.el, 'o_user_menu',
             "user menu in DOM should be from user menu widget instantiation");
         assert.containsOnce(userMenu, '.dropdown-item[data-menu="shortcuts"]',
             "should have a 'Shortcuts' item");
 
         userMenu.destroy();
-        parent.destroy();
     });
 });
 
