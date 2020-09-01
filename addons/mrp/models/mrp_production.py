d87b66b93ec72ee546e6371874109de0c134cc5c
Rémy Voet (ryv) <ryv@odoo.com>
2020-09-01 16:31:15 +0200

    [FIX] *stock*: fix date_deadline
    date_deadline of the stock move has been reviewed:
    - Now, it represents the datetime when the operation must
    be done to be in time for the uppest document (SO/PO/MO/RR).
    - At the creation of stock move the date_deadline is computed in
    the same way than the sheduled date expected that it is not include
    security days (inverse the logic for PO).
    - When we update the delivery date (or lead time) of SO (or PO).
    The deadline of related stock move is updated and the delta is propagate
    between origin and dest moves.
    
    task-2324039

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -316,7 +316,7 @@ class MrpProduction(models.Model):
 
     def _set_date_deadline(self):
         for production in self:
-            (self.move_raw_ids | self.move_finished_ids).date_deadline = production.date_deadline
+            production.move_finished_ids.date_deadline = production.date_deadline
 
     def _compute_is_planned(self):
         for production in self:
@@ -708,7 +708,7 @@ class MrpProduction(models.Model):
                 if production.is_partially_planned:
                     raise UserError(_('You cannot move a manufacturing order once it has a planned workorder, move related workorder(s) instead.'))
             if vals.get('date_planned_start'):
-                production.move_raw_ids.write({'date': production.date_planned_start})
+                production.move_raw_ids.write({'date': production.date_planned_start, 'date_deadline': production.date_planned_start})
             if vals.get('date_planned_finished'):
                 production.move_finished_ids.write({'date': production.date_planned_finished})
             if any(field in ['move_raw_ids', 'move_finished_ids', 'workorder_ids'] for field in vals) and production.state != 'draft':
@@ -719,10 +719,12 @@ class MrpProduction(models.Model):
                         'group_id': production.procurement_group_id.id,
                         'reference': production.name,
                         'date': production.date_planned_start,
+                        'date_deadline': production.date_planned_start
                     })
                     production.move_finished_ids.filtered(lambda move: move.additional and move.date > production.date_planned_finished).write({
                         'reference': production.name,
                         'date': production.date_planned_finished,
+                        'date_deadline': production.date_deadline
                     })
                 production._autoconfirm_production()
                 if production in production_to_replan:
@@ -881,7 +883,7 @@ class MrpProduction(models.Model):
             'sequence': bom_line.sequence if bom_line else 10,
             'name': self.name,
             'date': self.date_planned_start,
-            'date_deadline': self.date_deadline,
+            'date_deadline': self.date_planned_start,
             'bom_line_id': bom_line.id if bom_line else False,
             'picking_type_id': self.picking_type_id.id,
             'product_id': product_id.id,
