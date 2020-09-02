120e627d6eb74f36e59430e1d246ccb9a0302409
Arnaud Joset <arj@odoo.com>
2020-09-02 14:45:34 +0000

    [IMP] pad: save pas configuration in ir.config_parameter
    Before this commit, the pad key and server were saved on the company. It was an historical decision whose reasons no longer applies.
    The "Root url" called later "URL template" was company dependant but it no longer exists.
    
    closes odoo/odoo#57014
    
    Taskid: 2326514
    Related: odoo/upgrade#1739
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/pad/models/res_config_settings.py
+++ addons/pad/models/res_config_settings.py
@@ -7,5 +7,5 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    pad_server = fields.Char(related='company_id.pad_server', string="Pad Server", readonly=False)
-    pad_key = fields.Char(related='company_id.pad_key', string="Pad API Key", readonly=False)
+    pad_server = fields.Char(config_parameter='pad.pad_server', string="Pad Server")
+    pad_key = fields.Char(config_parameter='pad.pad_key', string="Pad API Key")
