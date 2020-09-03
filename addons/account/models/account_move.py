8c79d088e0d7d9f74ba8350224140b9d1973f46c
Goffin Simon <sig@odoo.com>
2020-09-03 14:53:29 +0000

    [FIX] account: Payment term date not saved
    Steps to reproduce the bug:
    
    - Create a customer invoice I for customer C with no payment term and no invoice date
    - Set a payment term date to a date D in the futur
    - Validate I
    
    Bug:
    
    The payment term date was changed to today as the invoice date instead of keeping D
    
    opw:2328668
    
    closes odoo/odoo#57048
    
    Related: odoo/enterprise#13097
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -303,7 +303,7 @@ class AccountMove(models.Model):
     @api.onchange('invoice_date')
     def _onchange_invoice_date(self):
         if self.invoice_date:
-            if not self.invoice_payment_term_id:
+            if not self.invoice_payment_term_id and (not self.invoice_date_due or self.invoice_date_due < self.invoice_date):
                 self.invoice_date_due = self.invoice_date
             self.date = self.invoice_date
             self._onchange_currency()
