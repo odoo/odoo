6e8b06e1c71463e3accbce8a4992aa397d2c1148
Aaron Bohy <aab@odoo.com>
2020-09-03 07:20:20 +0000

    [FIX] web: list: do not call on_attach_callback at reload
    Before this commit, the ListRenderer's on_attach_callback hook was
    called each time the view was reloaded, even though the renderer
    was already in the DOM. As a consequence, sub components were
    mounted several times, which could lead to issues.
    
    For instance, go to Inventory > Operations > Transfers, create a
    new one. Change the Operation Type to a Receipt, and change again
    to a Delivery: it crashed.
    
    Task 2315149
    Closes #56701
    
    X-original-commit: 78f361fb5382e4c7aed9a4e705e20f8d8757300d

--- addons/web/static/src/js/views/list/list_renderer.js
+++ addons/web/static/src/js/views/list/list_renderer.js
@@ -1090,10 +1090,10 @@ var ListRenderer = BasicRenderer.extend({
 
         // append the table (if any) to the main element
         if (tableWrapper) {
-            dom.append(this.el, tableWrapper, {
-                callbacks: [{ widget: this }],
-                in_DOM: document.body.contains(this.el),
-            });
+            this.el.appendChild(tableWrapper);
+            if (document.body.contains(this.el)) {
+                this.pagers.forEach(pager => pager.on_attach_callback());
+            }
             if (this.optionalColumns.length) {
                 this.el.classList.add('o_list_optional_columns');
                 this.$('table').append(
