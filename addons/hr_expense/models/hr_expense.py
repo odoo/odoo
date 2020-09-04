d5512f772153027d3359dd063d8114b8e7c97170
jdoutreloux <jud@odoo.com>
2020-09-04 11:24:15 +0000

    [FIX] hr_expense : fix error when posting entries from an expense report
    When a manager who does not have admin rights to the Employee app tries to post accounting entries
    from an expense report it generates an error.
    
    Task-2299286

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -351,9 +351,9 @@ class HrExpense(models.Model):
                 raise UserError(_("No credit account found for the %s journal, please configure one.") % (self.sheet_id.bank_journal_id.name))
             account_dest = self.sheet_id.bank_journal_id.default_credit_account_id.id
         else:
-            if not self.employee_id.address_home_id:
+            if not self.employee_id.sudo().address_home_id:
                 raise UserError(_("No Home Address found for the employee %s, please configure one.") % (self.employee_id.name))
-            partner = self.employee_id.address_home_id.with_context(force_company=self.company_id.id)
+            partner = self.employee_id.sudo().address_home_id.with_context(force_company=self.company_id.id)
             account_dest = partner.property_account_payable_id.id or partner.parent_id.property_account_payable_id.id
         return account_dest
 
