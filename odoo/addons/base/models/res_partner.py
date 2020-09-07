41f796f56ea1de21d66597f3ebec79286fa97971
Kamesh Patel <kat@odoo.com>
2020-09-07 16:28:17 +0530

    [FIX] project: search on archived partners fix
    Before this commit:
    
    When the search task by archived partners,
    will not returning respective tasks.
    
    After this commit:
    
    When the search task by archived partners,
    will return respective tasks.
    
    LINKS
    
    task-2326885

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -734,8 +734,7 @@ class Partner(models.Model):
         """ Override search() to always show inactive children when searching via ``child_of`` operator. The ORM will
         always call search() with a simple domain of the form [('parent_id', 'in', [ids])]. """
         # a special ``domain`` is set on the ``child_ids`` o2m to bypass this logic, as it uses similar domain expressions
-        if len(args) == 1 and len(args[0]) == 3 and args[0][:2] == ('parent_id','in') \
-                and args[0][2] != [False]:
+        if len(args) == 1 and len(args[0]) == 3 and args[0][:2] == ('parent_id', 'in'):
             self = self.with_context(active_test=False)
         return super(Partner, self)._search(args, offset=offset, limit=limit, order=order,
                                             count=count, access_rights_uid=access_rights_uid)
