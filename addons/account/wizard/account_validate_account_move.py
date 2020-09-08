a15fe237ef054b575c8c795e9eb20b2d771f69e9
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-08 11:37:32 +0200

    [FIX] account: Allow to validate from journal entries menu
    **Steps to reproduce**
    
    * Enable developer mode and "Show Full Accounting Features"
    * Go to *Invoicing > Accounting > Miscellaneous > Journal Entries*.
    * Create a new record and fill it.
    * Go back to the list.
    * Select the recently created record (or any not posted).
    * Click on *Action > Post entries*.
    * Click on "Post Journal Entries" button on the appearing popup.
    
    **Current behavior**
    
    Error: "There are no journal items in the draft state to post."
    
    **Expected behavior**
    
    Journal entry is posted.
    
    The problem is a recent change in 3d40e05, that switches the way selected moves
    are recognized, for accomodating other use case. With this code, both are valid.

--- addons/account/wizard/account_validate_account_move.py
+++ addons/account/wizard/account_validate_account_move.py
@@ -8,8 +8,13 @@ class ValidateAccountMove(models.TransientModel):
 
     def validate_move(self):
         context = dict(self._context or {})
-        moves = self.env['account.move'].search([('journal_id', '=', context.get('active_id')),
-                                                 ('state', '=', 'draft')]).filtered('line_ids')
+        if context.get("active_model") == "account.move":
+            moves = self.env["account.move"].browse(context.get('active_ids'))
+        elif context.get("active_model") == "account.journal":
+            moves = self.env['account.move'].search([('journal_id', '=', context.get('active_id')),
+                                                    ('state', '=', 'draft')]).filtered('line_ids')
+        else:
+            moves = self.env["account.move"]
         if not moves:
             raise UserError(_('There are no journal items in the draft state to post.'))
         moves.post()
