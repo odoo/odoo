91506f578e5b3681e838a123711fcea155de0da3
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-09-09 10:50:16 -0300

    [FIX] purchase, sale: tax selection on lines
    The `company_id` field is mandatory on `purchase.order` and
    `sale.order`:
    
    https://github.com/odoo/odoo/blob/644fdef9f950a11bc275865eb16d5a409eac0b02/addons/purchase/models/purchase.py#L133
    https://github.com/odoo/odoo/blob/644fdef9f950a11bc275865eb16d5a409eac0b02/addons/sale/models/sale.py#L226
    
    Therefore, we should apply the same logic than the `account.move`:
    
    https://github.com/odoo/odoo/blob/644fdef9f950a11bc275865eb16d5a409eac0b02/addons/account/models/account_move.py#L2744
    
    Closes #57366
    opw-2336501

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -1096,8 +1096,8 @@ class SaleOrderLine(models.Model):
     def _compute_tax_id(self):
         for line in self:
             fpos = line.order_id.fiscal_position_id or line.order_id.partner_id.property_account_position_id
-            # If company_id is set, always filter taxes by the company
-            taxes = line.product_id.taxes_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
+            # If company_id is set in the order, always filter taxes by the company
+            taxes = line.product_id.taxes_id.filtered(lambda r: r.company_id == line.order_id.company_id)
             line.tax_id = fpos.map_tax(taxes, line.product_id, line.order_id.partner_shipping_id) if fpos else taxes
 
     @api.model
