3fa6827e15b2a7cf63dbc352f3f7c77b4e1699b6
Aurélien Warnon <awa@odoo.com>
2020-09-10 10:24:35 +0200

    [IMP] event_sale: speed up event price_subtotal computation
    This commit improves the performance of the 'sale_price_subtotal' field of
    event.event by refactoring the way it's computed.
    
    The previous naive implementation looped on sale.order.lines linked to the
    event and performed a currency conversion for every single line. This leads
    up to thousands of requests to fetch the conversion rate and sum up everything
    on a real production database like Odoo.com .
    
    This new implementation groups the amounts by currency and performs a single
    conversion per currency (factored by the number of events in 'self'). It has
    the drawback of using 'today' to fetch conversion rates instead of the actual
    SO date like previous implementation.
    
    We (aka: all main POs as this decision was discussed) consider that as an
    acceptable trade-off to increase performances.
    
    Task ID-2335287
    
    X-original-commit: a4c4b03a09ffc08842f44b5759e3c7298cfbd915

--- addons/event_sale/models/event_event.py
+++ addons/event_sale/models/event_event.py
@@ -22,15 +22,58 @@ class Event(models.Model):
                  'sale_order_lines_ids.price_subtotal', 'sale_order_lines_ids.currency_id',
                  'sale_order_lines_ids.company_id', 'sale_order_lines_ids.order_id.date_order')
     def _compute_sale_price_subtotal(self):
+        """ Takes all the sale.order.lines related to this event and converts amounts
+        from the currency of the sale order to the currency of the event company.
+
+        To avoid extra overhead, we use conversion rates as of 'today'.
+        Meaning we have a number that can change over time, but using the conversion rates
+        at the time of the related sale.order would mean thousands of extra requests as we would
+        have to do one conversion per sale.order (and a sale.order is created every time
+        we sell a single event ticket). """
+        date_now = fields.Datetime.now()
+        sale_price_by_event = {}
+        if self.ids:
+            event_subtotals = self.env['sale.order.line'].read_group(
+                [('event_id', 'in', self.ids),
+                 ('price_subtotal', '!=', 0)],
+                ['event_id', 'currency_id', 'price_subtotal:sum'],
+                ['event_id', 'currency_id'],
+                lazy=False
+            )
+
+            company_by_event = {
+                event._origin.id or event.id: event.company_id
+                for event in self
+            }
+
+            currency_by_event = {
+                event._origin.id or event.id: event.currency_id
+                for event in self
+            }
+
+            currency_by_id = {
+                currency.id: currency
+                for currency in self.env['res.currency'].browse(
+                    [event_subtotal['currency_id'][0] for event_subtotal in event_subtotals]
+                )
+            }
+
+            for event_subtotal in event_subtotals:
+                price_subtotal = event_subtotal['price_subtotal']
+                event_id = event_subtotal['event_id'][0]
+                currency_id = event_subtotal['currency_id'][0]
+                sale_price = currency_by_event[event_id]._convert(
+                    price_subtotal,
+                    currency_by_id[currency_id],
+                    company_by_event[event_id],
+                    date_now)
+                if event_id in sale_price_by_event:
+                    sale_price_by_event[event_id] += sale_price
+                else:
+                    sale_price_by_event[event_id] = sale_price
+
         for event in self:
-            event.sale_price_subtotal = sum([
-                event.currency_id._convert(
-                    sale_order_line_id.price_subtotal,
-                    sale_order_line_id.currency_id,
-                    sale_order_line_id.company_id,
-                    sale_order_line_id.order_id.date_order)
-                for sale_order_line_id in event.sale_order_lines_ids
-            ])
+            event.sale_price_subtotal = sale_price_by_event.get(event._origin.id or event.id, 0)
 
     def action_view_linked_orders(self):
         """ Redirects to the orders linked to the current events """
