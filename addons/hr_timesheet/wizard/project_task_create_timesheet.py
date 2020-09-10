e5b1fd358c3496f2c34ec65a9f59fbfb29a8b87f
Prakash Prajapati <ppr@odoo.com>
2020-09-10 16:34:02 +0530

    [ADD] hr_timesheet: update the time spend in the account analytic line when the timer is running
    When the timer is started from the account analytic line and stops the
    timer from the project task, it will update to time spend.
    
    Task-Id: 2276015

--- addons/hr_timesheet/wizard/project_task_create_timesheet.py
+++ addons/hr_timesheet/wizard/project_task_create_timesheet.py
@@ -20,16 +20,31 @@ class ProjectTaskCreateTimesheet(models.TransientModel):
     )
 
     def save_timesheet(self):
-        values = {
-            'task_id': self.task_id.id,
-            'project_id': self.task_id.project_id.id,
-            'date': fields.Date.context_today(self),
-            'name': self.description,
-            'user_id': self.env.uid,
-            'unit_amount': self.time_spent,
-        }
-        self.task_id.user_timer_id.unlink()
-        return self.env['account.analytic.line'].create(values)
+        timer = self.env['timer.timer'].search([
+            ('user_id', '=', self.env.user.id),
+            ('res_model', '=', 'account.analytic.line'),
+            ('res_id', 'in', self.task_id.timesheet_ids.ids),
+            ('timer_start', '!=', False),
+            ('timer_pause', '=', False)
+        ], limit=1)
+        """ When the timer started from the timesheet, the analytic line is already created so in that case just stop the timer and
+        update the amount instead of creating the new line.
+        """
+        if timer:
+            analytic_line = self.env[timer.res_model].search([('id', '=', timer.res_id)])
+            self.task_id.user_timer_id.unlink()
+            return analytic_line.write({'unit_amount': self.time_spent});
+        else:
+            values = {
+                'task_id': self.task_id.id,
+                'project_id': self.task_id.project_id.id,
+                'date': fields.Date.context_today(self),
+                'name': self.description,
+                'user_id': self.env.uid,
+                'unit_amount': self.time_spent,
+            }
+            self.task_id.user_timer_id.unlink()
+            return self.env['account.analytic.line'].create(values)
 
     def action_delete_timesheet(self):
         self.task_id.user_timer_id.unlink()
