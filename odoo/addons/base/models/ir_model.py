24987cd89edcbc271f9e036e2f4513a4275c2f51
Raphael Collet <rco@odoo.com>
2020-09-15 12:27:17 +0000

    [IMP] core: prevent deletion of a group with ACLs or record rules
    This simply makes it less easy to screw up a database by preventing all
    users to do their daily tasks.
    
    closes odoo/odoo#57776
    
    X-original-commit: 665a795fc1b199f4f17cc9f1257d1b36b6276e6f
    Signed-off-by: Raphael Collet (rco) <rco@openerp.com>
    Co-authored-by: Florent de Labarre <florent.mirieu@gmail.com>

--- odoo/addons/base/models/ir_model.py
+++ odoo/addons/base/models/ir_model.py
@@ -1624,7 +1624,7 @@ class IrModelAccess(models.Model):
     name = fields.Char(required=True, index=True)
     active = fields.Boolean(default=True, help='If you uncheck the active field, it will disable the ACL without deleting it (if you delete a native ACL, it will be re-created when you reload the module).')
     model_id = fields.Many2one('ir.model', string='Model', required=True, index=True, ondelete='cascade')
-    group_id = fields.Many2one('res.groups', string='Group', ondelete='cascade', index=True)
+    group_id = fields.Many2one('res.groups', string='Group', ondelete='restrict', index=True)
     perm_read = fields.Boolean(string='Read Access')
     perm_write = fields.Boolean(string='Write Access')
     perm_create = fields.Boolean(string='Create Access')
