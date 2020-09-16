edb98f3766af663ad5ebf9ed458900b57e13cb62
yhu-odoo <yhu@odoo.com>
2020-09-16 13:34:04 +0200

    [FIX] *: adapt access to ir.actions.* records
    Missing part of commit 6156f98288b16f6c84d36bacf16d5a66a661abb3
    
    Fix issue #57780

--- addons/purchase_stock/models/purchase.py
+++ addons/purchase_stock/models/purchase.py
@@ -126,8 +126,7 @@ class PurchaseOrder(models.Model):
     def action_view_picking(self):
         """ This function returns an action that display existing picking orders of given purchase order ids. When only one found, show the picking immediately.
         """
-        action = self.env.ref('stock.action_picking_tree_all')
-        result = action.read()[0]
+        result = self.env["ir.actions.actions"]._for_xml_id('stock.action_picking_tree_all')
         # override the context to get rid of the default filtering on operation type
         result['context'] = {'default_partner_id': self.partner_id.id, 'default_origin': self.name, 'default_picking_type_id': self.picking_type_id.id}
         pick_ids = self.mapped('picking_ids')
