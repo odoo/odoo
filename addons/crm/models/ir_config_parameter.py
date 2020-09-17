47919cc291e708a533b6f3f3fa5d1f777a970ff9
Raphael Collet <rco@odoo.com>
2020-09-17 14:54:47 +0200

    add deobfuscating comments

--- addons/crm/models/ir_config_parameter.py
+++ addons/crm/models/ir_config_parameter.py
@@ -10,6 +10,7 @@ class IrConfigParameter(models.Model):
     def write(self, vals):
         result = super().write(vals)
         if any(record.key == "crm.pls_fields" for record in self):
+            # force the registry to update crm.lead.probability's depends
             self.flush()
             self.env.registry.setup_models(self.env.cr)
         return result
@@ -18,6 +19,7 @@ class IrConfigParameter(models.Model):
     def create(self, vals_list):
         records = super().create(vals_list)
         if any(record.key == "crm.pls_fields" for record in records):
+            # force the registry to update crm.lead.probability's depends
             self.flush()
             self.env.registry.setup_models(self.env.cr)
         return records
