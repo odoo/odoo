add71e94b5ced64da0050ce15dd2387337086a7b
Didier (did) <did@odoo.com>
2020-09-23 17:17:37 +0000

    [FIX] mail: compute display_name for partner and user
    task-2322135
    
    closes odoo/odoo#58374
    
    X-original-commit: d9bdbde95c3694ecdaccd58b5475631137c22e93
    Related: odoo/enterprise#13478
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Co-authored-by: Sébastien Theys <seb@odoo.com>

--- addons/mail/static/src/models/activity/activity.js
+++ addons/mail/static/src/models/activity/activity.js
@@ -94,7 +94,7 @@ function factory(dependencies) {
                     data2.creator = [
                         ['insert', {
                             id: data.create_uid[0],
-                            partnerDisplayName: data.create_uid[1],
+                            display_name: data.create_uid[1],
                         }],
                     ];
                 }
@@ -109,7 +109,7 @@ function factory(dependencies) {
                     data2.assignee = [
                         ['insert', {
                             id: data.user_id[0],
-                            partnerDisplayName: data.user_id[1],
+                            display_name: data.user_id[1],
                         }],
                     ];
                 }
