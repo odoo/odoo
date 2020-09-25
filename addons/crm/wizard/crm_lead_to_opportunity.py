182fa38d7f28a92e7171c4ee3a42c8f73217b765
Thibault Francois <tfr@openerp.com>
2020-09-25 13:02:48 +0000

    [FIX] crm: don't bypass access right to find partner:
    Problem
    -------
    
    Sales people can have restriction on partner they can see
    Private addresses, multi company, ....
    
    When they convert a lead to opportunity, it's currently possible
    that the wizard will find and link a partner that the current user
    cannot see.
    
    Solution
    --------
    Field that are now computed store field, that were previously
    normal field with onchange, should not be computed as sudo
    to respect the record rule
    
    closes odoo/odoo#59517
    
    X-original-commit: af9db1d2aa605d46b5b5dae3d63533367c431f33
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/crm/wizard/crm_lead_to_opportunity.py
+++ addons/crm/wizard/crm_lead_to_opportunity.py
@@ -24,25 +24,25 @@ class Lead2OpportunityPartner(models.TransientModel):
     name = fields.Selection([
         ('convert', 'Convert to opportunity'),
         ('merge', 'Merge with existing opportunities')
-    ], 'Conversion Action', compute='_compute_name', readonly=False, store=True)
+    ], 'Conversion Action', compute='_compute_name', readonly=False, store=True, compute_sudo=False)
     action = fields.Selection([
         ('create', 'Create a new customer'),
         ('exist', 'Link to an existing customer'),
         ('nothing', 'Do not link to a customer')
-    ], string='Related Customer', compute='_compute_action', readonly=False, store=True)
+    ], string='Related Customer', compute='_compute_action', readonly=False, store=True, compute_sudo=False)
     lead_id = fields.Many2one('crm.lead', 'Associated Lead', required=True)
     duplicated_lead_ids = fields.Many2many(
         'crm.lead', string='Opportunities', context={'active_test': False},
-        compute='_compute_duplicated_lead_ids', readonly=False, store=True)
+        compute='_compute_duplicated_lead_ids', readonly=False, store=True, compute_sudo=False)
     partner_id = fields.Many2one(
         'res.partner', 'Customer',
-        compute='_compute_partner_id', readonly=False, store=True)
+        compute='_compute_partner_id', readonly=False, store=True, compute_sudo=False)
     user_id = fields.Many2one(
         'res.users', 'Salesperson',
-        compute='_compute_user_id', readonly=False, store=True)
+        compute='_compute_user_id', readonly=False, store=True, compute_sudo=False)
     team_id = fields.Many2one(
         'crm.team', 'Sales Team',
-        compute='_compute_team_id', readonly=False, store=True)
+        compute='_compute_team_id', readonly=False, store=True, compute_sudo=False)
     force_assignment = fields.Boolean(
         'Force assignment', default=True,
         help='If checked, forces salesman to be updated on updated opportunities even if already set.')
