64bbe87d9107de9af7ea69cf322912f4e97c84b6
Rémi Rahir <rar@odoo.com>
2020-09-25 10:56:20 +0000

    [FIX] crm_iap_lead_enrich: improve iap calls robustness
    In its current state, the code contacting the iap service was prone to
    fail in case of huge batches of leads being processed due to 2 factors:
    - the call could timeout;
    - any processed lead that was modified somewhere else in the meantime
    would trigger a serialization failure and ultimately make the whole
    batch fail.
    
    This commit ensures that we process the leads by smaller batches and
    that we avoid serialization failures by acquiring a lock on the current
    batch.
    
    We now also avoid useless calls to iap when an email address is linked
    to well-known email providers to alleviate the payload and speed up the
    server's reponse.
    
    closes odoo/odoo#59190
    
    X-original-commit: 0a898e16948b38a0029937c7c2eb6e2a38fd9a8a
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/crm_iap_lead_enrich/tests/test_lead_enrich.py
+++ addons/crm_iap_lead_enrich/tests/test_lead_enrich.py
@@ -11,6 +11,7 @@ class TestLeadEnrich(TestCrmCommon, MockIAPEnrich):
     @classmethod
     def setUpClass(cls):
         super(TestLeadEnrich, cls).setUpClass()
+        cls.registry.enter_test_mode(cls.cr)
 
         cls.leads = cls.env['crm.lead']
         for x in range(0, 4):
@@ -18,6 +19,11 @@ class TestLeadEnrich(TestCrmCommon, MockIAPEnrich):
                 'name': 'Test %s' % x,
                 'email_from': 'test_mail_%s@example.com' % x
             })
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.registry.leave_test_mode()
+        super().tearDownClass()
 
     @users('user_sales_manager')
     def test_enrich_internals(self):
