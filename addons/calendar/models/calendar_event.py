6af16a025cc20fb3623f67a45e858d876103975b
Swapnesh Shah <swapneshshah111@gmail.com>
2020-09-28 23:36:21 +0530

    [FIX] calendar: compute stop when needed
    Before this commit there would be trackback (TypeError: unsupported operand type(s) for +: 'bool' and 'datetime.timedelta') when you will try to remove field `Starting At`
    
    With this commit, We will compute `stop` if `start` is present.

--- addons/calendar/models/calendar_event.py
+++ addons/calendar/models/calendar_event.py
@@ -328,7 +328,8 @@ class Meeting(models.Model):
         for event in self:
             # Round the duration (in hours) to the minute to avoid weird situations where the event
             # stops at 4:19:59, later displayed as 4:19.
-            event.stop = event.start + timedelta(minutes=round((event.duration or 1.0) * 60))
+            if event.start:
+                event.stop = event.start + timedelta(minutes=round((event.duration or 1.0) * 60))
             if event.allday:
                 event.stop -= timedelta(seconds=1)
 
