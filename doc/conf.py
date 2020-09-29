a7afdc19810ee8f700343868e318a195b56d1f98
Martin Trigaux <mat@odoo.com>
2020-09-29 12:10:07 +0000

    [FIX] doc: update version
    This commit lands in master, the version switcher was displayed as
    "14.0" instead of "master" when published to
    https://www.odoo.com/documentation/master
    
    closes odoo/odoo#58816
    
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- doc/conf.py
+++ doc/conf.py
@@ -54,9 +54,9 @@ copyright = u'Odoo S.A.'
 # built documents.
 #
 # The short X.Y version.
-version = '14.0'
+version = 'master'
 # The full version, including alpha/beta/rc tags.
-release = '14.0'
+release = 'master'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
