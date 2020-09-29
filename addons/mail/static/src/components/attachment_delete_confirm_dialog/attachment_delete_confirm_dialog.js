4de23039410b489a7bba30033204478a0a9f9f4b
Sébastien Theys <seb@odoo.com>
2020-09-29 23:04:51 +0200

    [IMP] mail: add files in preparation of task-2282322
    The task will require adding new files which we don't want to do in stable.
    
    X-original-commit: 5580a0e4e44744d13ec9bed03f2116c043c611b1

---
+++ addons/mail/static/src/components/attachment_delete_confirm_dialog/attachment_delete_confirm_dialog.js
@@ -0,0 +1,6 @@
+odoo.define('mail/static/src/components/attachment_delete_confirm_dialog/attachment_delete_confirm_dialog.js', function (require) {
+'use strict';
+
+// TODO FIXME file to be completed in task-2282322
+
+});
