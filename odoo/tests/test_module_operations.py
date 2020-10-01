c4ebb5fcbfaa92683339168d1005a1079bef21de
Nicolas Martinelli <nim@odoo.com>
2020-10-01 17:08:44 +0200

    [FIX] base_setup, website, tests: remove Gengo references
    The Gengo modules were removed with:
    https://github.com/odoo/odoo/commit/b38b72e456a
    https://github.com/odoo/odoo/commit/9b1f0962baa
    
    But there are still references to it.
    
    opw-2349904
    
    X-original-commit: a30edba504aa1b62aa02092112a55bca65cf9a0c

--- odoo/tests/test_module_operations.py
+++ odoo/tests/test_module_operations.py
@@ -17,7 +17,7 @@ import odoo.tests.loader
 _logger = logging.getLogger('odoo.tests.test_module_operations')
 
 BLACKLIST = {
-    'auth_ldap', 'document_ftp', 'base_gengo', 'website_gengo', 'website_instantclick', 'pad',
+    'auth_ldap', 'document_ftp', 'website_instantclick', 'pad',
     'pad_project', 'note_pad', 'pos_cache', 'pos_blackbox_be', 'payment_test',
 }
 IGNORE = ('hw_', 'theme_', 'l10n_', 'test_', 'payment_')
