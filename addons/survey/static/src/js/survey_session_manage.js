23a41ff58f244e20c017f8e306772267242212d8
David Beguin <dbe@odoo.com>
2020-10-02 07:19:03 +0000

    [FIX] survey: let Chart.js handle the session graph max height automatically
    Before this commit, the graph y scale in the survey manager screen was based on
    the attendees count. But is some attendees do not answer the question, the bars
    never reach the maximum height of the graph, and bars can stay really small.
    
    Example: 10 attendees - 4 answers - A: 1 / B: 1 / C: 2
    
    10 |
       |
       |
       |       |
    0  |_|__|__|_
         A  B  C
    
    After this commit, the max height of the graph is not set on the graph. So the
    chart lib will handle itself the height and will adapt automatically the bars
    height in function of the data given to the graph. Bars will always take full
    height space, relatively to the count of the most voted answer.
    
    Example: 10 attendees - 4 answers - A: 1 / B: 1 / C: 2
    
    2  |       |
       |       |
       | |  |  |
    0  |_|__|__|_
         A  B  C
    
    Task ID: 2351455
    
    closes odoo/odoo#59777
    
    X-original-commit: ac64af033f0bbaa698aee424a5cab85fd9b967d2
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/survey/static/src/js/survey_session_manage.js
+++ addons/survey/static/src/js/survey_session_manage.js
@@ -385,7 +385,7 @@ publicWidget.registry.SurveySessionManage = publicWidget.Widget.extend({
                 self.attendeesCount = questionResults.attendees_count;
 
                 if (self.resultsChart && questionResults.question_statistics_graph) {
-                    self.resultsChart.updateChart(JSON.parse(questionResults.question_statistics_graph), self.attendeesCount);
+                    self.resultsChart.updateChart(JSON.parse(questionResults.question_statistics_graph));
                 } else if (self.textAnswers) {
                     self.textAnswers.updateTextAnswers(questionResults.input_line_values);
                 }
@@ -452,7 +452,6 @@ publicWidget.registry.SurveySessionManage = publicWidget.Widget.extend({
 
         if (!this.isStartScreen && this.showBarChart) {
             this.resultsChart = new SurveySessionChart(this, {
-                attendeesCount: this.attendeesCount,
                 questionType: this.$el.data('questionType'),
                 answersValidity: this.$el.data('answersValidity'),
                 hasCorrectAnswers: this.hasCorrectAnswers,
