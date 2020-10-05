odoo/odoo

commit f9ef2c8cd980ecb9eee22d4cc43d5291953471ef

[FIX] account: Fix automatic wizard issues
When coming from the dashboard (dashboard => Bills to pay => Bill)
The context contains information about the purchase journal and not the
move line, causing an issue in the wizard. This change will make sure
the context always contains the necessary move line information

Task id #2344722

closes odoo/odoo#58316

Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

addons/account/models/account_move.py
@@ -4664,7 +4664,12 @@ class AccountMoveLine(models.Model):
 
     def action_automatic_entry(self):
         [action] = self.env.ref('account.account_automatic_entry_wizard_action').read()
-        action['context'] = self.env.context
+        # Force the values of the move line in the context to avoid issues
+        ctx = dict(self.env.context)
+        ctx['active_id'] = self.id
+        ctx['active_ids'] = self.ids
+        ctx['active_model'] = 'account.move.line'
+        action['context'] = ctx
         return action
 
     @api.model

commit 587c1d0f9bddd7bcc3d8d165d6d3073387dfe73f

[FIX] account: Fix traceback when opening auto entries with multilines
Introduced by:
https://github.com/odoo/odoo/commit/f9ef2c8cd980ecb9eee22d4cc43d5291953471ef

`self.id` is making an `ensure_one` raising a traceback when trying to open the wizard with more than one selected line.

closes odoo/odoo#59078

Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

addons/account/models/account_move.py
@@ -4666,7 +4666,7 @@ class AccountMoveLine(models.Model):
         [action] = self.env.ref('account.account_automatic_entry_wizard_action').read()
         # Force the values of the move line in the context to avoid issues
         ctx = dict(self.env.context)
-        ctx['active_id'] = self.id
+        ctx.pop('active_id', None)
         ctx['active_ids'] = self.ids
         ctx['active_model'] = 'account.move.line'
         action['context'] = ctx