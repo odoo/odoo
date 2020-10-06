08741655d80dcbb64928e372a1739f74c029e944
Jorge Pinna Puissant <jpp@odoo.com>
2020-10-06 08:31:42 +0000

    [FIX] project: access token on task
    - Create a new Project with 'Portal user and all employees' as
      visibility;
    - Add a portal user as customer;
    - Create a new task in the Project, without assigning the task to
      anybody;
    - Connect to the portal as the portal user;
    - Send a message on the task.
    
    Before this commit, an Error 403 forbidden was raised, this error occurs
    because the task don't have an access token created, this token will be
    created when an internal user send a message on the task.
    
    Now, the access token is created if the project has a 'Portal user and
    all employees' visibility, and the portal user can post a message on
    the task.
    
    opw-2345070
    
    closes odoo/odoo#59268
    
    X-original-commit: 8a68fac8b246a3e2435e670d9fb2bfb91cc9f83f
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -761,6 +761,8 @@ class Task(models.Model):
             vals.update(self.update_date_end(vals['stage_id']))
             vals['date_last_stage_update'] = fields.Datetime.now()
         task = super(Task, self.with_context(context)).create(vals)
+        if task.project_id.privacy_visibility == 'portal':
+            task._portal_ensure_token()
         return task
 
     def write(self, vals):
