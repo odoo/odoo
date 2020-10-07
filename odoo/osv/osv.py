c667c8242a2648e69c299830472c65642ef73039
Julien Castiaux <juc@odoo.com>
2020-10-07 18:14:40 +0200

    [FIX] base: fix exception retro-compatibility-aid
    Restore the old signature of except_orm.
    
    opw-2354525
    opw-2355980

--- odoo/osv/osv.py
+++ odoo/osv/osv.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from ..exceptions import UserError
+from ..exceptions import except_orm
 from ..models import Model, TransientModel, AbstractModel
 
 # Deprecated, kept for backward compatibility.
-except_osv = UserError
+except_osv = except_orm
 
 # Deprecated, kept for backward compatibility.
 osv = Model
