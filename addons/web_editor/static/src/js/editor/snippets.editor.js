3178f24f8f1c120dc4a97e90324d62ca4f8a3b91
Siddarth Gajjar <sga@odoo.com>
2020-10-09 11:06:54 +0000

    [FIX] web_editor: fix drag & drop with restricted editor access rights
    task-2345098
    
    closes odoo/odoo#79851
    
    X-original-commit: cd051046c191f9f4cd6df4627319931720ceb257
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2330,7 +2330,11 @@ var SnippetsMenu = Widget.extend({
                     return dragSnip;
                 },
                 start: function () {
+                    const prom = new Promise(resolve => dragAndDropResolve = () => resolve());
+                    self._mutex.exec(() => prom);
+
                     self.options.wysiwyg.odooEditor.automaticStepUnactive();
+
                     self.$el.find('.oe_snippet_thumbnail').addClass('o_we_already_dragging');
                     self.options.wysiwyg.odooEditor.observerUnactive('dragAndDropCreateSnippet');
 
@@ -2404,9 +2408,6 @@ var SnippetsMenu = Widget.extend({
                     self.draggableComponent.$scrollTarget.on('scroll.scrolling_element', function () {
                         self.$el.trigger('scroll');
                     });
-
-                    const prom = new Promise(resolve => dragAndDropResolve = () => resolve());
-                    self._mutex.exec(() => prom);
                 },
                 stop: async function (ev, ui) {
                     const doc = self.options.wysiwyg.odooEditor.document;
