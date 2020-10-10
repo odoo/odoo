83e108c80772e306de235a628dd3f2776c96adc5
Swapnesh Shah <swapneshshah111@gmail.com>
2020-10-10 10:29:39 +0000

    [FIX] mrp: compute duration when needed
    Before this commit there would be trackback (TypeError: unsupported operand type(s) for -: 'datetime.datetime' and 'NoneType') when you will try to remove field Start date as we are using the field to compute duration.
    
    With this commit, we compute duration when Both fields are present.
    
    closes odoo/odoo#70482
    
    X-original-commit: f29597fce37350f1c05d98341b959809bbb160b2
    Signed-off-by: Rémy Voet <ryv-odoo@users.noreply.github.com>

--- addons/stock/wizard/stock_picking_return.py
+++ addons/stock/wizard/stock_picking_return.py
@@ -110,6 +110,16 @@ class ReturnPicking(models.TransientModel):
         }
         return vals
 
+    def _prepare_picking_default_values(self):
+        return {
+            'move_lines': [],
+            'picking_type_id': self.picking_id.picking_type_id.return_picking_type_id.id or self.picking_id.picking_type_id.id,
+            'state': 'draft',
+            'origin': _("Return of %s") % self.picking_id.name,
+            'location_id': self.picking_id.location_dest_id.id,
+            'location_dest_id': self.location_id.id
+        }
+
     def _create_returns(self):
         # TODO sle: the unreserve of the next moves could be less brutal
         for return_move in self.product_return_moves.mapped('move_id'):
@@ -117,13 +127,7 @@ class ReturnPicking(models.TransientModel):
 
         # create new picking for returned products
         picking_type_id = self.picking_id.picking_type_id.return_picking_type_id.id or self.picking_id.picking_type_id.id
-        new_picking = self.picking_id.copy({
-            'move_lines': [],
-            'picking_type_id': picking_type_id,
-            'state': 'draft',
-            'origin': _("Return of %s", self.picking_id.name),
-            'location_id': self.picking_id.location_dest_id.id,
-            'location_dest_id': self.location_id.id})
+        new_picking = self.picking_id.copy(self._prepare_picking_default_values())
         new_picking.message_post_with_view('mail.message_origin_link',
             values={'self': new_picking, 'origin': self.picking_id},
             subtype_id=self.env.ref('mail.mt_note').id)
