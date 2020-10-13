a9996630999614dcfacba2ce265f4e48f531d0c1
Raphael Collet <rco@odoo.com>
2020-10-13 12:01:34 +0000

    [FIX] core: assignment of binary field on new record with origin
    The assignment of a binary field on a new record with origin did
    actually modify the attachment storing the value of the field on the
    origin record!  The fix is simply to avoid messing up with attachments
    when updating new records.
    
    closes odoo/odoo#59909
    
    X-original-commit: 6778c4c10fa68c46bde6fae176f547f905e9c02f
    Signed-off-by: Raphael Collet (rco) <rco@openerp.com>

--- odoo/addons/test_new_api/tests/test_new_fields.py
+++ odoo/addons/test_new_api/tests/test_new_fields.py
@@ -1946,6 +1946,10 @@ class TestFields(TransactionCaseWithUserDemo):
         self.assertEqual(record.image, image_h)
         self.assertEqual(new_record.image, image_h)
 
+        # assignment to new record with origin should not do any query
+        with self.assertQueryCount(0):
+            new_record.image = image_w
+
     def test_95_binary_bin_size(self):
         binary_value = base64.b64encode(b'content')
         binary_size = b'7.00 bytes'
