eeb028b3d4753a69b3a1a55a7e53a47732f24eb8
Jeremy Kersten <jke@odoo.com>
2020-10-14 17:56:30 +0200

    [FIX] website: remove searchbar from kanban theme
    X-original-commit: c10cc2cfb46d34e6a119859dc1bc34ce8229c1da

--- addons/website/static/src/js/theme_preview_kanban.js
+++ addons/website/static/src/js/theme_preview_kanban.js
@@ -14,7 +14,11 @@ var ThemePreviewKanbanController = KanbanController.extend(ThemePreviewControlle
      */
     start: async function () {
         await this._super(...arguments);
+
+        // hide pager
         this.el.classList.add('o_view_kanban_theme_preview_controller');
+
+        // update breacrumb
         const websiteLink = Object.assign(document.createElement('a'), {
             className: 'btn btn-secondary ml-3',
             href: '/',
@@ -44,7 +48,7 @@ var ThemePreviewKanbanController = KanbanController.extend(ThemePreviewControlle
 });
 
 var ThemePreviewKanbanView = KanbanView.extend({
-    searchMenuTypes: [],
+    withSearchBar: false,  // hide searchBar
 
     config: _.extend({}, KanbanView.prototype.config, {
         Controller: ThemePreviewKanbanController,
