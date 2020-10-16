22ca63f5d3d3a0b80b3ecdd59c50089ac520a3c0
qsm-odoo <qsm@odoo.com>
2020-10-16 08:55:01 +0000

    [FIX] mass_mailing: make better link dialog for mass mailing
    Primary and link buttons are "hacked" by mailing themes scss. We thus
    have to fix their preview if possible (as they are previewed as standard
    bootstrap while they actually have a different look depending on the
    mailing theme).
    
    Note: this commit is obviously a big hack which calls for lots of
    improvements in master.
    
    FIXME: this only partially work due to the new editor, it has to be
    fixed as soon as the master becomes stable again...
    
    Part of https://github.com/odoo/odoo/pull/60308
    opw-2360756
    
    X-original-commit: 7bd7bc2513b72f5ffc006758c4bf4eb42ae6ed67

---
+++ addons/mass_mailing/static/src/js/mass_mailing_link_dialog_fix.js
@@ -0,0 +1,79 @@
+
+odoo.define('mass_mailing.fix.LinkDialog', function (require) {
+'use strict';
+
+const LinkDialog = require('wysiwyg.widgets.LinkDialog');
+
+/**
+ * Primary and link buttons are "hacked" by mailing themes scss. We thus
+ * have to fix their preview if possible.
+ */
+LinkDialog.include({
+    /**
+     * @override
+     */
+    start() {
+        const ret = this._super(...arguments);
+        const mailWrapperEl = this.__editorEditable.querySelector('.o_mail_wrapper');
+        if (!mailWrapperEl) {
+            return ret;
+        }
+
+        this.opened().then(() => {
+            // Ugly hack to show the real color for link and primary which
+            // depend on the mailing themes. Note: the hack is not enough as
+            // the mailing theme changes those colors in some environment,
+            // sometimes (for example 'btn-primary in this snippet looks like
+            // that')... we'll consider this a limitation until a master
+            // refactoring of those mailing themes.
+            this.__realMMColors = {};
+            const $previewArea = $('<div/>').addClass('o_mail_snippet_general');
+            $(mailWrapperEl).closest('.o_layout').append($previewArea);
+            _.each(['link', 'primary', 'secondary'], type => {
+                const $el = $('<a href="#" class="btn btn-' + type + '"/>');
+                $el.appendTo($previewArea);
+                this.__realMMColors[type] = {
+                    'border-color': $el.css('border-top-color'),
+                    'background-color': $el.css('background-color'),
+                    'color': $el.css('color'),
+                };
+                $el.remove();
+
+                this.$('.form-group .o_btn_preview.btn-' + type)
+                    .css(_.pick(this.__realMMColors[type], 'background-color', 'color'));
+            });
+            $previewArea.remove();
+
+            this._adaptPreview();
+        });
+
+        return ret;
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * FIXME review once the new editor is fixed as the hack does only partially
+     * work in there...
+     *
+     * @override
+     */
+    _adaptPreview() {
+        this._super(...arguments);
+        if (this.__realMMColors) {
+            var $preview = this.$("#link-preview a:first");
+            $preview.css('border-color', '');
+            $preview.css('background-color', '');
+            $preview.css('color', '');
+            _.each(['link', 'primary', 'secondary'], type => {
+                if ($preview.hasClass('btn-' + type) || type === 'link' && !$preview.hasClass('btn')) {
+                    $preview.css(this.__realMMColors[type]);
+                }
+            });
+        }
+    },
+});
+
+});
