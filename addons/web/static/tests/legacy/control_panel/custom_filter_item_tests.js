b2a29fa49060bd8b3289fb5d1683eb9f632a0863
Mohammed Shekha <msh@odoo.com>
2020-10-16 10:45:44 +0000

    [FIX] web: custom filter on selection field shows DB value
    before this commit: custom filter on selection field shows database value while
    it should display user freidnly value.
    
    after this commit: custom filter on selection field will display UI value of
    selection attribute instead of database value.
    
    task-2363112
    
    closes odoo/odoo#78181
    
    X-original-commit: 0f268ba36b302df9f247c746db64987a966c1dec
    Signed-off-by: FrancoisGe <fge@odoo.com>

--- addons/web/static/tests/legacy/control_panel/custom_filter_item_tests.js
+++ addons/web/static/tests/legacy/control_panel/custom_filter_item_tests.js
@@ -179,7 +179,7 @@ odoo.define('web.filter_menu_generator_tests', function (require) {
 
             // Default value
             expectedFilters = [{
-                description: 'Color is "black"',
+                description: 'Color is "Black"',
                 domain: '[["color","=","black"]]',
                 type: 'filter',
             }];
@@ -189,7 +189,7 @@ odoo.define('web.filter_menu_generator_tests', function (require) {
 
             // Updated value
             expectedFilters = [{
-                description: 'Color is "white"',
+                description: 'Color is "White"',
                 domain: '[["color","=","white"]]',
                 type: 'filter',
             }];
