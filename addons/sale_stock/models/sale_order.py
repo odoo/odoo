8a034f0496805fe5990ee24ccf92f37adc7d5c3b
Tiffany Chang (tic) <tic@odoo.com>
2020-10-20 15:23:44 +0000

    [REM] procurement_jit, (sale_)stock: replace procurement jit logic
    Instead of having a module handle the procurement_jit, we now
    incorporate it directly within sale_stock._action_launch_stock_rule and
    make it dependent on the new stock.picking.type reservation_method option
    rather than applying to all deliveries.
    
    Note that only picking scheduler trigger has been moved to sale_stock,
    since the action_assign portion of its moves are now handled directly in
    stock.move.
    
    Also we add a new test to ensure new reservation_method logic works
    as expected with sale orders.
    
    Task: 2359317
    Upgrade PR: odoo/upgrade#1868

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -585,6 +585,14 @@ class SaleOrderLine(models.Model):
                 line.name, line.order_id.name, line.order_id.company_id, values))
         if procurements:
             self.env['procurement.group'].run(procurements)
+
+        # This next block is currently needed only because the scheduler trigger is done by picking confirmation rather than stock.move confirmation
+        orders = self.mapped('order_id')
+        for order in orders:
+            pickings_to_confirm = order.picking_ids.filtered(lambda p: p.state not in ['cancel', 'done'])
+            if pickings_to_confirm:
+                # Trigger the Scheduler for Pickings
+                pickings_to_confirm.action_confirm()
         return True
 
     def _check_package(self):
