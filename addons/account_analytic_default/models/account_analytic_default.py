ad15d82c4d2cc6d364b928cd6cef7a1e08e0eea7
Goffin Simon <sig@odoo.com>
2020-10-21 12:33:39 +0200

    [FIX] account: Auto-complete with analytic account
    Steps to reproduce the bug:
    
    - Create a vendor bill VB1 with one line L1
    - Set an analytic account AA on L1
    - Post VB1
    - Create a new vendor bill VB2
    - Set VB1 on the field Auto-complete
    
    Bug:
    
    The created line L2 on VB2 had no analytic account
    
    PS: In 12.0, the analytic account was kept when using Auto-complete field
    
    opw:2359432
    
    X-original-commit: 454429d4ae6dbf4730dfb8c9c46ecf35dfd4b503
    Co-authored-by: smetl <las@odoo.com>

--- addons/account_analytic_default/models/account_analytic_default.py
+++ addons/account_analytic_default/models/account_analytic_default.py
@@ -69,8 +69,8 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     # Overload of fields defined in account
-    analytic_account_id = fields.Many2one(compute="_compute_analytic_account", store=True, readonly=False)
-    analytic_tag_ids = fields.Many2many(compute="_compute_analytic_account", store=True, readonly=False)
+    analytic_account_id = fields.Many2one(compute="_compute_analytic_account", store=True, readonly=False, copy=True)
+    analytic_tag_ids = fields.Many2many(compute="_compute_analytic_account", store=True, readonly=False, copy=True)
 
     @api.depends('product_id', 'account_id', 'partner_id', 'date_maturity')
     def _compute_analytic_account(self):
@@ -88,9 +88,3 @@ class AccountMoveLine(models.Model):
             if rec and not record.exclude_from_invoice_tab:
                 record.analytic_account_id = rec.analytic_id
                 record.analytic_tag_ids = rec.analytic_tag_ids
-
-    def _copy_data_extend_business_fields(self, values):
-        # OVERRIDE to copy the 'analytic_account_id' if set
-        super(AccountMoveLine, self)._copy_data_extend_business_fields(values)
-        if self.analytic_account_id:
-            values['analytic_account_id'] = self.analytic_account_id.id
