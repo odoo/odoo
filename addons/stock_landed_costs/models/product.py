2bee9201f838ea41bec12ea770deb927c19c12f9
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2020-10-21 14:55:19 +0200

    [FIX] stock_landed_costs: check landed costs activation conditions
    Steps to reproduce:
    1. Activate landed costs
    2. Create a product that in the purchase tab has active 'Is a landed
    cost'
    3. Add this product to a purchase order
    4. Confirm the PO, create the bill and create landed costs entry
    
    According to the documentation
    https://www.odoo.com/documentation/user/14.0/inventory/management/reporting/integrating_landed_costs.html
    
    'Landed costs can only be applied to products with a FIFO or AVCO
    costing method and an automated inventory valuation (which requires the
    accounting application to be installed).'
    
    This flow needs correction since the user is able to activate landed
    costs without automated inventory valuation
    
    opw-2332763

--- addons/stock_landed_costs/models/product.py
+++ addons/stock_landed_costs/models/product.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
@@ -9,3 +10,10 @@ class ProductTemplate(models.Model):
 
     landed_cost_ok = fields.Boolean('Is a Landed Cost', help='Indicates whether the product is a landed cost.')
 
+    @api.constrains('landed_cost_ok')
+    def _check_landed_cost_ok(self):
+        for tmpl in self:
+            if tmpl.landed_cost_ok and not (tmpl.type == 'service' and \
+                tmpl.categ_id.property_valuation == 'real_time' and \
+                tmpl.categ_id.property_cost_method in ('fifo','average')):
+                    raise ValidationError(_('Landed costs can only be applied to products with a FIFO or AVCO costing method and an automated inventory valuation (which requires the accounting application to be installed).'))
