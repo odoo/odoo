d8c5e6cc01be85e4833c65ba73eb4cf806e2c888
Nicolas Martinelli <nim@odoo.com>
2020-10-21 11:19:00 +0000

    [FIX] stock: procurement company
    The forward-port #60325 highlighted an issue regarding the default
    company of a procurement. It is set as the user company, which may not
    make sense when the method is called with sudo. More generally, the
    default company should be the company of the location we are searching
    rules in.
    
    X-original-commit: 7f25f9dca6c90836168c8de4fe97af1d5a40e925

--- addons/stock/models/stock_rule.py
+++ addons/stock/models/stock_rule.py
@@ -303,6 +303,7 @@ class ProcurementGroup(models.Model):
         In order to be able to find a suitable location that provide the product
         it will search among stock.rule.
         """
+<<<<<<< HEAD
         actions_to_run = defaultdict(list)
         errors = []
         for procurement in procurements:
@@ -331,6 +332,19 @@ class ProcurementGroup(models.Model):
 
         if errors:
             raise UserError('\n'.join(errors))
+=======
+        values.setdefault('company_id', location_id.company_id)
+        values.setdefault('priority', '1')
+        values.setdefault('date_planned', fields.Datetime.now())
+        rule = self._get_rule(product_id, location_id, values)
+        if not rule:
+            raise UserError(_('No procurement rule found in location "%s" for product "%s".\n Check routes configuration.') % (location_id.display_name, product_id.display_name))
+        action = 'pull' if rule.action == 'pull_push' else rule.action
+        if hasattr(rule, '_run_%s' % action):
+            getattr(rule, '_run_%s' % action)(product_id, product_qty, product_uom, location_id, name, origin, values)
+        else:
+            _logger.error("The method _run_%s doesn't exist on the procument rules" % action)
+>>>>>>> 13ffc8d9044... temp
         return True
 
     @api.model
