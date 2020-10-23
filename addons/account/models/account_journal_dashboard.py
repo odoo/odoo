b78051b785053e16a0ced1ee7152c5f88f139b37
Ravi Gohil <rgo@odoo.com>
2020-10-23 11:04:59 +0530

    [FIX] account: fix amounts of sales/purchases journals on dashboard
    Issue1:
    Create an invoice with 100EUR and make a payment of 20EUR,
    'Awaiting Payments' on dashboard shows 100EUR instead of 80EUR.
    
    Issue2:
    - Activate EUR/USD with EUR as company currency
    - Set USD rate to 0.75 for 20th Oct, 2020 and 0.5 for 23rd Oct, 2020
    - Create a bill dated 20 Oct, 2020 of 75USD(i.e. 100EUR) and validate it
    Dashboard will show 150EUR instead of 100EUR while move related to that
    bill will be correctly set to 100EUR.
    
    Related: 2357491

--- addons/account/models/account_journal_dashboard.py
+++ addons/account/models/account_journal_dashboard.py
@@ -194,7 +194,7 @@ class account_journal(models.Model):
             query_results_drafts = self.env.cr.dictfetchall()
 
             today = datetime.today()
-            query = """SELECT amount_total, currency_id AS currency, type FROM account_invoice WHERE journal_id = %s AND date < %s AND state = 'open';"""
+            query = """SELECT amount_total, currency_id AS currency, type, date_invoice, company_id FROM account_invoice WHERE journal_id = %s AND date < %s AND state = 'open';"""
             self.env.cr.execute(query, (self.id, today))
             late_query_results = self.env.cr.dictfetchall()
             (number_waiting, sum_waiting) = self._count_results_and_sum_amounts(query_results_to_pay, currency)
@@ -224,7 +224,7 @@ class account_journal(models.Model):
         data as its first element, and the arguments dictionary to use to run
         it as its second.
         """
-        return ("""SELECT state, amount_total, currency_id AS currency, type
+        return ("""SELECT state, residual as amount_total, currency_id AS currency, type, date_invoice, company_id
                   FROM account_invoice
                   WHERE journal_id = %(journal_id)s AND state = 'open';""", {'journal_id':self.id})
 
@@ -234,7 +234,7 @@ class account_journal(models.Model):
         gather the bills in draft state data, and the arguments
         dictionary to use to run it as its second.
         """
-        return ("""SELECT state, amount_total, currency_id AS currency, type
+        return ("""SELECT state, amount_total, currency_id AS currency, type, date_invoice, company_id
                   FROM account_invoice
                   WHERE journal_id = %(journal_id)s AND state = 'draft';""", {'journal_id':self.id})
 
@@ -249,7 +249,9 @@ class account_journal(models.Model):
             rslt_count += 1
 
             type_factor = result.get('type') in ('in_refund', 'out_refund') and -1 or 1
-            rslt_sum += type_factor * cur.compute(result.get('amount_total'), target_currency)
+            date = result.get('date_invoice') or fields.Date.today()
+            company_id = result.get('company_id') or self.env.user.company_id.id
+            rslt_sum += type_factor * cur.with_context(date=date, company_id=company_id).compute(result.get('amount_total'), target_currency)
         return (rslt_count, rslt_sum)
 
     @api.multi
