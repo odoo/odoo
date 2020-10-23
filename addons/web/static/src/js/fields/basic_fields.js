ce6d1689825212b86e74e394f6b06e36ab69bdc5
wan <wan@odoo.com>
2020-10-23 17:30:52 +0200

    [FIX] web: increase file upload to 128Mb
    Also make this a configurable value, from the server.
    
    This is the limit of the SAAS servers.

--- addons/web/static/src/js/fields/basic_fields.js
+++ addons/web/static/src/js/fields/basic_fields.js
@@ -1699,7 +1699,7 @@ var AbstractFieldBinary = AbstractField.extend({
         this._super.apply(this, arguments);
         this.fields = record.fields;
         this.useFileAPI = !!window.FileReader;
-        this.max_upload_size = 25 * 1024 * 1024; // 25Mo
+        this.max_upload_size = session.max_file_upload_size || 128 * 1024 * 1024;
         if (!this.useFileAPI) {
             var self = this;
             this.fileupload_id = _.uniqueId('o_fileupload');
