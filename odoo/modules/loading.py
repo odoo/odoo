a07d0d4791ef661be9dd068855d1f3cdd7805ab8
Raphael Collet <rco@odoo.com>
2020-10-26 15:35:40 +0000

    [FIX] core: reinstall hooks after setting up models in registry
    Before this patch, adding a field on a custom model discards all
    automated actions on that model.  The explanation is relatively simple.
    When models are set up in the registry, the classes of custom models are
    dropped then recreated.  Given that automated actions are implemented as
    monkey-patches on model classes, the setup of models simply loses those
    monkey-patches, which explains why they stop working on custom models.
    
    The fix introduces an `_unregister_hook()` method, that is expected to
    clean up what has been done in `_register_hook()`.  When the registry is
    ready (i.e., not being loaded), the setup of models first invokes
    `_unregister_hook()` on models, proceeds with the setup, and finally
    invokes `_register_hook()` to reinstall the hooks.
    
    OPW 2362308
    
    X-original-commit: ac970380deaafb8542670951ed63dc11ffaf6b6c

--- odoo/modules/loading.py
+++ odoo/modules/loading.py
@@ -556,6 +556,10 @@ def load_modules(db, force_demo=False, status=None, update_module=False):
             _logger.error('At least one test failed when loading the modules.')
 
         # STEP 8: call _register_hook on every model
+        # This is done *exactly once* when the registry is being loaded. See the
+        # management of those hooks in `Registry.setup_models`: all the calls to
+        # setup_models() done here do not mess up with hooks, as registry.ready
+        # is False.
         env = api.Environment(cr, SUPERUSER_ID, {})
         for model in env.values():
             model._register_hook()
