691c14c07089218bed52fb37807c315918bbcd1c
Nicolas Martinelli <nim@odoo.com>
2020-10-26 10:10:33 +0000

    [FIX] account, purchase, sale: portal access error
    - Install account / purchase / sale
    - Ceate an internal user without any access rights
    - Go to `/my`
    
    A 500 error is raised because of an AccessError.
    
    When the user has no access rights to any of the mentioned applications,
    the `search` call returns an AccessError.
    
    We prevent the access error and return 0 as a fallback.
    
    opw-2367559
    
    closes odoo/odoo#60849
    
    X-original-commit: 54ef98c613219aba3bda41817f7767261b8092d2
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/purchase/controllers/portal.py
+++ addons/purchase/controllers/portal.py
@@ -22,7 +22,7 @@ class CustomerPortal(portal.CustomerPortal):
         if 'purchase_count' in counters:
             values['purchase_count'] = request.env['purchase.order'].search_count([
                 ('state', 'in', ['purchase', 'done', 'cancel'])
-            ])
+            ]) if request.env['purchase.order'].check_access_rights('read', raise_exception=False) else 0
         return values
 
     def _purchase_order_get_page_view_values(self, order, access_token, **kwargs):
