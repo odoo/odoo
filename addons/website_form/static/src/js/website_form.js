6578997a5114a60e397cffb2c8a8e4a678803168
Nicolas Galler <ngl@odoo.com>
2020-10-26 08:49:51 +0000

    [FIX] website_form: prevent double clicking send
    Behavior prior to the commit:
    
    A user can double click the "Send" button when submitting a form on the
    website.  Because the event handler is removed on the first click, the
    second submission uses the browser default behavior and posts the form
    to an incorrect URL, resulting in a 404 error for the user.
    
    Behavior after the commit:
    
    The button is disabled after the first click.
    
    Note:
    
    Using `.attr('disabled')` is sufficient both to disable the button and
    to give the disabled visual on all browsers since IE9, but we keep
    `.addClass('disabled')` and `.off()` for cases where the clickable is not a
    `button` but a link.
    
    opw-2356402
    
    closes odoo/odoo#60840
    
    Signed-off-by: Nicolas Galler <nicocrm@users.noreply.github.com>

--- addons/website_form/static/src/js/website_form.js
+++ addons/website_form/static/src/js/website_form.js
@@ -62,7 +62,10 @@ odoo.define('website_form.animation', function (require) {
 
         send: function (e) {
             e.preventDefault();  // Prevent the default submit behavior
-            this.$target.find('.o_website_form_send').off().addClass('disabled');  // Prevent users from crazy clicking
+            this.$target.find('.o_website_form_send')
+                .off('click')
+                .addClass('disabled')
+                .attr('disabled', 'disabled');  // Prevent users from crazy clicking
 
             var self = this;
 
@@ -235,7 +238,12 @@ odoo.define('website_form.animation', function (require) {
         update_status: function (status) {
             var self = this;
             if (status !== 'success') {  // Restore send button behavior if result is an error
-                this.$target.find('.o_website_form_send').on('click',function (e) {self.send(e);}).removeClass('disabled');
+                this.$target.find('.o_website_form_send')
+                    .removeClass('disabled')
+                    .removeAttr('disabled')
+                    .on('click', function (e) {
+                        self.send(e);
+                    });
             }
             var $result = this.$('#o_website_form_result');
             this.templates_loaded.done(function () {
