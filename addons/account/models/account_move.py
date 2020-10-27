7b9819a1f478e3ff8a22b3913429f48da5bdd23e
Nathan de Pryck <nde@odoo.com>
2020-10-27 17:12:23 +0100

    [FIX] account: use _for_xml_id to read action
    Use of '_for_xml_id' instead of read in order to avoid
    access right issue on 'ir.actions.act_window'.

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -4671,7 +4671,7 @@ class AccountMoveLine(models.Model):
         return ids
 
     def open_reconcile_view(self):
-        [action] = self.env.ref('account.action_account_moves_all_a').read()
+        action = self.env['ir.actions.act_window']._for_xml_id('account.action_account_moves_all_a')
         ids = self._reconciled_lines()
         action['domain'] = [('id', 'in', ids)]
         return action
