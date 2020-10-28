dfce8a614be9e2091f4561ff65ca9592239165cd
Daniel Duque <daniel.duque@factorlibre.com>
2020-10-28 10:38:01 +0000

    [FIX] core: missing default values with inherits
    Consider some model A that "_inherits" from model B, which itself
    "_inherits" from model C.  Also consider a field F on model C, which is
    inherited by both models B and A.  Now create a record from model A,
    with a given record for model C, no record for model B, and a default
    value for field F.  This should create a record in A, connected to a new
    record in B, connected to the given record in C, and the default value
    should be ignored, as a record from C is given.
    
    Before this commit, the given record in C is modified with the default
    value for F.  The default value is considered because no record is given
    for B, and that value is used to modify the given record in C.  The fix
    consists in discarding default values by considering potential ancestor
    records when parent records are not given.
    
    closes odoo/odoo#60898
    
    Signed-off-by: Raphael Collet (rco) <rco@openerp.com>
    Co-authored-by: Raphael Collet <rco@odoo.com>

--- odoo/addons/test_inherits/models.py
+++ odoo/addons/test_inherits/models.py
@@ -9,6 +9,7 @@ class Unit(models.Model):
     name = fields.Char('Name', required=True)
     state = fields.Selection([('a', 'A'), ('b', 'B')], string='State')
     surname = fields.Char(compute='_compute_surname')
+    size = fields.Integer()
 
     @api.one
     @api.depends('name')
@@ -25,6 +26,7 @@ class Box(models.Model):
     unit_id = fields.Many2one('test.unit', 'Unit', required=True,
                               ondelete='cascade')
     field_in_box = fields.Char('Field1')
+    size = fields.Integer()
 
 
 # We add a third level of _inherits
