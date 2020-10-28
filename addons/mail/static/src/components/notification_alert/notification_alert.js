b270666a17cf7b3697566201cfff0d8e0ed78a98
Ipsita Borisagar <ibo@odoo.com>
2020-10-28 17:25:25 +0530

    [IMP] mail: improve preferences warning
    PURPOSE
    
    Once the push notifications have been blocked, message in
    the preferences is not straightforward enough for users to allow them.
    
    SPECIFICATION
    
    User can easily copy current browser setting's URL by copy
    button in the message.
    
    LINKS
    
    PR #47919
    Task-2188584

--- addons/mail/static/src/components/notification_alert/notification_alert.js
+++ addons/mail/static/src/components/notification_alert/notification_alert.js
@@ -17,8 +17,31 @@ class NotificationAlert extends Component {
             return {
                 isMessagingInitialized,
                 isNotificationBlocked: this.isNotificationBlocked,
+                settingUrl: this.isNotificationBlocked ? this.settingUrl : false,
+                settingPath: this.isNotificationBlocked ? this.settingPath : false,
             };
         });
+    }
+
+    mounted() {
+        super.mounted();
+        if (this.isNotificationBlocked && this.settingUrl) {
+            const $clipboardBtn = $(this.el.querySelector('.o_NotificationAlert_urlClipboardButton')).tooltip({
+                title: this.env._t("Copied!"),
+                trigger: 'manual',
+                placement: 'right',
+            });
+            const clipboard = new ClipboardJS($clipboardBtn[0], {
+                text: () => this.settingUrl,
+                container: this.el,
+            });
+            clipboard.on('success', () => {
+                setTimeout(() => {
+                    $clipboardBtn.tooltip('show');
+                    setTimeout(() => $clipboardBtn.tooltip('hide'), 800);
+                });
+            });
+        }
     }
 
     //--------------------------------------------------------------------------
@@ -39,7 +62,23 @@ class NotificationAlert extends Component {
             !this.env.messaging.isNotificationPermissionDefault()
         );
     }
-
+    /**
+     * @returns {string}
+     */
+    get settingPath() {
+        const isBrowserSafari = navigator.userAgent.includes('Safari') && navigator.userAgent.search('Chrome') < 0;
+        return isBrowserSafari ? this.env._t("(path:Safari > Preferences > Website > Notifications)") : '';
+    }
+    /**
+     * @returns {string|undefined}
+     */
+    get settingUrl() {
+        if (navigator.userAgent.includes('Chrome')) {
+            return "chrome://settings/content/siteDetails?site=https%3A%2F%2F" + window.location.host;
+        } else if (navigator.userAgent.includes('Firefox')) {
+            return "about:preferences#privacy";
+        }
+    }
 }
 
 Object.assign(NotificationAlert, {
