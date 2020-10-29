de11bdb4cc1b5fcd98dc748b5216f744240c424f
Achraf (abz) <abz@odoo.com>
2020-10-29 09:09:07 +0000

    [FIX] account: Reconcile payment from the manual reconciliation
    Issue
    
        - Install "Accounting"
        - Go to "Configuration/Reconciliation Models"
        - Create new reconciliation model "Test" with "620000 Bank Fees" account
        - Go to "Customers/Payments" and try to reconcile a payment via "Manual Operations"
            by clicking on "Test" button
    Cause
    
        This instruction "st_line.company_currency_id.is_zero(residual_balance)"
        needs at least one element in st_line
    
    Solution
    
        Skip this instruction if "st_line" is empty
    
    opw-2370034
    
    closes odoo/odoo#61045
    
    X-original-commit: 161a6c494a412097468fca214817c42f022d29e2
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>
    Signed-off-by: Achraf <abz-odoo@users.noreply.github.com>

--- addons/account/models/account_reconcile_model.py
+++ addons/account/models/account_reconcile_model.py
@@ -317,7 +317,8 @@ class AccountReconcileModel(models.Model):
         lines_vals_list = []
 
         for line in self.line_ids:
-            if not line.account_id or st_line.company_currency_id.is_zero(residual_balance):
+            currency_id = st_line.currency_id or st_line.journal_id.currency_id or self.company_id.currency_id 
+            if not line.account_id or currency_id.is_zero(residual_balance):
                 return []
 
             if line.amount_type == 'percentage':
