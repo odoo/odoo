d26e9a4c4a08d7535d36311a5f811ff6af4c1084
Jeremy Kersten <jke@odoo.com>
2020-11-03 16:11:37 +0100

    [FIX] google_recaptcha: remove autoinstall on google_recaptcha
    You can enable it in base_setup when you need it.
    
    Depends of base_setup and not ir_http or website because you need to
    be able to enable it for login form e.g.

--- addons/google_recaptcha/models/res_config_settings.py
+++ addons/google_recaptcha/models/res_config_settings.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-import requests
-
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
@@ -10,4 +8,10 @@ class ResConfigSettings(models.TransientModel):
 
     recaptcha_public_key = fields.Char("Site Key", config_parameter='recaptcha_public_key', groups='base.group_system')
     recaptcha_private_key = fields.Char("Secret Key", config_parameter='recaptcha_private_key', groups='base.group_system')
-    recaptcha_min_score = fields.Float("Minimum score", config_parameter='recaptcha_min_score', groups='base.group_system', default="0.5", help="Should be between 0.0 and 1.0")
+    recaptcha_min_score = fields.Float(
+        "Minimum score",
+        config_parameter='recaptcha_min_score',
+        groups='base.group_system',
+        default="0.5",
+        help="Should be between 0.0 and 1.0.\n1.0 is very likely a good interaction, 0.0 is very likely a bot"
+    )
