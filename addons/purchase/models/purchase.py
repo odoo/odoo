b9b76d0f4422eb8e5eb2a5170f11fe00a418784c
svs-odoo <svs@odoo.com>
2020-11-03 09:43:51 +0100

    [REM] purchase: remove unused import

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -6,10 +6,10 @@ from itertools import groupby
 from pytz import timezone, UTC
 from werkzeug.urls import url_encode
 
-from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo import api, fields, models, _
 from odoo.osv import expression
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
-from odoo.tools.float_utils import float_compare, float_is_zero
+from odoo.tools.float_utils import float_is_zero
 from odoo.exceptions import AccessError, UserError, ValidationError
 from odoo.tools.misc import formatLang, get_lang
 
