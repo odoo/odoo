b15d20d71323b391b83035d862aa2b78f89dcbc6
Victor Feyens <vfe@odoo.com>
2020-11-03 12:13:05 +0000

    [IMP] crm_*, sales_team: support batch creation
    See merge commit for more details.
    
    Task ID-2330149
    COM PR odoo/odoo#61246
    ENT PR odoo/enterprise#14561

--- addons/sales_team/models/crm_team.py
+++ addons/sales_team/models/crm_team.py
@@ -85,12 +85,11 @@ class CrmTeam(models.Model):
     # CRUD
     # ------------------------------------------------------------
 
-    @api.model
-    def create(self, values):
-        team = super(CrmTeam, self.with_context(mail_create_nosubscribe=True)).create(values)
-        if values.get('member_ids'):
-            team._add_members_to_favorites()
-        return team
+    @api.model_create_multi
+    def create(self, vals_list):
+        teams = super(CrmTeam, self.with_context(mail_create_nosubscribe=True)).create(vals_list)
+        teams.filtered(lambda t: t.member_ids)._add_members_to_favorites()
+        return teams
 
     def write(self, values):
         res = super(CrmTeam, self).write(values)
