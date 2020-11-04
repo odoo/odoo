4cb4dee8def986865a0fec0d3589865ef44c9720
Xavier Morel <xmo@odoo.com>
2020-11-04 12:32:20 +0100

    [FIX] account: access error on an action button

--- addons/account/models/account_journal_dashboard.py
+++ addons/account/models/account_journal_dashboard.py
@@ -520,9 +520,7 @@ class account_journal(models.Model):
             ctx.update(search_default_journal_id=self.id)
             del ctx['search_default_journal']  # otherwise it will do a useless groupby in bank statements
         ctx.pop('group_by', None)
-        ir_model_obj = self.env['ir.model.data']
-        model, action_id = ir_model_obj.get_object_reference('account', action_name)
-        [action] = self.env[model].browse(action_id).read()
+        action = self.env['ir.actions.actions']._for_xml_id(f'account.{action_name}')
         action['context'] = ctx
         if ctx.get('use_domain', False):
             action['domain'] = isinstance(ctx['use_domain'], list) and ctx['use_domain'] or ['|', ('journal_id', '=', self.id), ('journal_id', '=', False)]
