0297ab3133d6d729427dd63eb789ea29041a12b4
Swapnesh Shah <swapneshshah111@gmail.com>
2020-11-05 23:32:28 +0530

    [IMP] account: remove orphan repartition lines
    When a tax is deleted,
    remove the `account.tax.repartition.line` linked to it as well.
    
    Fixes #60603

--- addons/account/models/account_tax.py
+++ addons/account/models/account_tax.py
@@ -628,10 +628,10 @@ class AccountTaxRepartitionLine(models.Model):
         help="Account on which to post the tax amount")
     tag_ids = fields.Many2many(string="Tax Grids", comodel_name='account.account.tag', domain=[('applicability', '=', 'taxes')], copy=True)
     invoice_tax_id = fields.Many2one(comodel_name='account.tax',
-        check_company=True,
+        check_company=True, ondelete='cascade',
         help="The tax set to apply this distribution on invoices. Mutually exclusive with refund_tax_id")
     refund_tax_id = fields.Many2one(comodel_name='account.tax',
-        check_company=True,
+        check_company=True, ondelete='cascade',
         help="The tax set to apply this distribution on refund invoices. Mutually exclusive with invoice_tax_id")
     tax_id = fields.Many2one(comodel_name='account.tax', compute='_compute_tax_id')
     tax_fiscal_country_id = fields.Many2one(string="Fiscal Country", comodel_name='res.country', related='company_id.account_tax_fiscal_country_id', help="Technical field used to restrict tags domain in form view.")
