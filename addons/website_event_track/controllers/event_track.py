1d22e600e670b568563934d74f3df0a414b6e7ce
Noe Antoine <nan@odoo.com>
2020-11-05 14:21:30 +0100

    [REF] website_event_track: Track Proposal Form Revamp (GDPR)
    Distinguish and display to the speaker which info is collected by the form
    for internal use vs external dissemination. The contact details one wants to
    share with the event manager (Private details : contact_phone, contact_email)
    could indeed be different from the ones shared to attendees (partner_phone,
    partner_email,...). The form in both back-end and front-end is made more
    detailed, including more fields. Previously the form could have several
    speakers, now only one.
    
    Front-End
    
    - Track proposal form changed, now includes tags, contact / speaker info...
    - Track name and description are mandatory fields
    - User can select (but not create) several track tags from those existing.
      This allows categorizing the track in pre-defined categories with format
      "tag category : tag name". It requires a select2 widget,
      implemented in new file website_event_track_proposal_add_tag.js.
    - New tickable section "contact me through different contact" not displayed
      if the checkbox is not ticked. If the section is ticked, then the info set
      there (contact_name, contact_phone, contact_email) will be added on a new
      contact with id partner_id set on the track.
    - In both cases (checkbox on/off), if the contact/partner email is the
      same as logged user's, uses its partner on form. No contact creation needed.
    - Improved / dynamic error display on form submission. If the form is valid,
      it resets after submission.
    - New widget in website_event_track_proposal.js:
        - The partner_name is propagated on contact_name on input but editable.
        - When the optional section is checked:
    	- The contact_name is made required.
    	- The user must at least enter a contact phone or an email. The email
              is normalized but any phone format is accepted, since the choice of
              country is not resolved (e.g. geoip not always relevant). Could be
              improved with international phone number widget, see COM PR #34725
    
    Back-end: following changes are done to ease contact creation and form completion
    from back-end, as well as reaching the speaker from chatter
    
    - On the form:
        - If created on the fly from M2O (entering a name and using "create ..."),
          the new partner will use default values contact_phone and contact_email.
        - Once the contact is set (partner_id), contact_phone and contact_email
          are set to readonly since they change according to the partner.
        - When setting or changing the partner: this will fill all the form fields
          with new available partner data to ease the flow, but only the empty ones
          (this prevents losing previously entered information).
          If partner is a company, company name is set to the name of partner.
        - All fields are editable in the speaker section.
        - Exception thrown if no contact mean available when supposed to.
    - On the chatter, about contact creation and message subscription:
        - From contact creation through suggested contacts.
    	- If the partner is set but is not in the followers, it is suggested
    	- If no partner is set, then there is at maximum one suggested contact
    	  from track data: using contact_email if there is one, partner_email
    	  otherwise. This priority serves main commit purpose. The user can
              create and edit a corresponding contact if the address is not linked
    	  to a partner. It will then be searched for and set on track.
    
    Modifies tests in test_track_partner_sync. Before, partner fields on track
    would be erased by customer's ones. Now, they are updated only if empty.
    Contact fields are erased by customer's ones if set. tests changed accordingly.
    
    Task ID - 2329406
    COM PR odoo/odoo#60847
    UPG PR odoo/upgrade#2346

--- addons/website_event_track/controllers/event_track.py
+++ addons/website_event_track/controllers/event_track.py
@@ -11,7 +11,7 @@ import babel.dates
 import base64
 import pytz
 
-from odoo import exceptions, http, fields, _
+from odoo import exceptions, http, fields, tools, _
 from odoo.http import request
 from odoo.osv import expression
 from odoo.tools import is_html_empty, plaintext2html
@@ -392,30 +392,67 @@ class EventTrackController(http.Controller):
 
     @http.route(['''/event/<model("event.event"):event>/track_proposal/post'''], type='http', auth="public", methods=['POST'], website=True)
     def event_track_proposal_post(self, event, **post):
-        tags = []
-        for tag in event.allowed_track_tag_ids:
-            if post.get('tag_' + str(tag.id)):
-                tags.append(tag.id)
+        if not event.can_access_from_current_website():
+            raise NotFound()
 
-        track = request.env['event.track'].sudo().create({
+        # Only accept existing tag indices. Use search instead of browse + exists:
+        # this prevents users to register colorless tags if not allowed to (ACL).
+        input_tag_indices = [int(tag_id) for tag_id in post['tags'].split(',') if tag_id]
+        valid_tag_indices = request.env['event.track.tag'].search([('id', 'in', input_tag_indices)]).ids
+
+        contact = request.env['res.partner']
+        visitor_partner = request.env['website.visitor']._get_visitor_from_request().partner_id
+        # Contact name is required. Therefore, empty contacts are not considered here. At least one of contact_phone
+        # and contact_email must be filled. Email is verified. If the post tries to create contact with no valid entry,
+        # raise exception. If normalized email is the same as logged partner, use its partner_id on track instead.
+        # This prevents contact duplication. Otherwise, create new contact with contact additional info of post.
+        if post.get('add_contact_information'):
+            valid_contact_email = tools.email_normalize(post.get('contact_email'))
+            # Here, the phone is not formatted. To format it, one needs a country. Based on a country, from geoip for instance.
+            # The problem is that one could propose a track in country A with phone number of country B. Validity is therefore
+            # quite tricky. We accept any format of contact_phone. Could be improved with select country phone widget.
+            if valid_contact_email or post.get('contact_phone'):
+                if visitor_partner and valid_contact_email == visitor_partner.email_normalized:
+                    contact = visitor_partner
+                else:
+                    contact = request.env['res.partner'].sudo().create({
+                        'email': valid_contact_email,
+                        'name': post.get('contact_name'),
+                        'phone': post.get('contact_phone'),
+                    })
+            else:
+                raise exceptions.ValidationError(_("Format Error : please enter a valid contact phone or contact email."))
+        # If the speaker email is the same as logged user's, then also uses its partner on track, same as above.
+        else:
+            valid_speaker_email = tools.email_normalize(post['partner_email'])
+            if visitor_partner and valid_speaker_email == visitor_partner.email_normalized:
+                contact = visitor_partner
+
+        track = request.env['event.track'].with_context({'mail_create_nosubscribe': True}).sudo().create({
             'name': post['track_name'],
+            'partner_id': contact.id,
             'partner_name': post['partner_name'],
-            'partner_email': post['email_from'],
-            'partner_phone': post['phone'],
-            'partner_biography': plaintext2html(post['biography']),
+            'partner_email': post['partner_email'],
+            'partner_phone': post['partner_phone'],
+            'partner_function': post['partner_function'],
+            'contact_phone': contact.phone,
+            'contact_email': contact.email,
             'event_id': event.id,
-            'tag_ids': [(6, 0, tags)],
-            'user_id': False,
+            'tag_ids': [(6, 0, valid_tag_indices)],
             'description': plaintext2html(post['description']),
-            'image': base64.b64encode(post['image'].read()) if post.get('image') else False
+            'user_id': False,
+            'image': base64.b64encode(post['image'].read()) if post.get('image') else False,
         })
+
         if request.env.user != request.website.user_id:
             track.sudo().message_subscribe(partner_ids=request.env.user.partner_id.ids)
-        else:
-            partner = request.env['res.partner'].sudo().search([('email', '=', post['email_from'])])
-            if partner:
-                track.sudo().message_subscribe(partner_ids=partner.ids)
-        return request.render("website_event_track.event_track_proposal", {'track': track, 'event': event})
+
+        return request.render("website_event_track.event_track_proposal", {'track': track, 'event': event, 'main_object': event})
+
+    # ACL : This route is necessary since rpc search_read method in js is not accessible to all users (e.g. public user).
+    @http.route(['''/event/track_tag/search_read'''], type='json', auth="public", website=True)
+    def website_event_track_fetch_tags(self, domain, fields):
+        return request.env['event.track.tag'].search_read(domain, fields)
 
     # ------------------------------------------------------------
     # TOOLS
