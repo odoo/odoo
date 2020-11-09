8e395cf3f4437178bffcfac304c4f351d80190f9
wan <wan@odoo.com>
2020-11-09 11:26:03 +0000

    [FIX] point_of_sale,pos_hr,pos_coupon: adapt tests for localization builds
    Part of task 2124952
    
    By using AccountTestInvoicingHttpCommon, we have a setup independant
    from the demo datas. This also prevents running tests with incompatible
    localization: the tests for l10n with another currency than USD failed
    because of the demo datas preventing the change of currency.

--- addons/pos_hr/tests/test_frontend.py
+++ addons/pos_hr/tests/test_frontend.py
@@ -11,19 +11,28 @@ from odoo.addons.point_of_sale.tests.test_frontend import TestPointOfSaleHttpCom
 
 
 class TestPosHrHttpCommon(TestPointOfSaleHttpCommon):
-    def setUp(self):
-        super().setUp()
-        self.main_pos_config.write({"module_pos_hr": True})
+    @classmethod
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass(chart_template_ref=chart_template_ref)
+
+        cls.env.user.groups_id += cls.env.ref('hr.group_hr_user')
+
+        cls.main_pos_config.write({"module_pos_hr": True})
 
         # Admin employee
-        self.env.ref("hr.employee_admin").write(
-            {"name": "Mitchell Admin", "pin": False}
-        )
+        admin = cls.env.ref("hr.employee_admin").sudo().copy({
+            "company_id": cls.env.company.id,
+            "user_id": cls.env.user.id,
+            "name": "Mitchell Admin",
+            "pin": False,
+        })
 
         # User employee
-        emp1 = self.env.ref("hr.employee_han")
+        emp1 = cls.env.ref("hr.employee_han").sudo().copy({
+            "company_id": cls.env.company.id,
+        })
         emp1_user = new_test_user(
-            self.env,
+            cls.env,
             login="emp1_user",
             groups="base.group_user",
             name="Pos Employee1",
@@ -32,10 +41,13 @@ class TestPosHrHttpCommon(TestPointOfSaleHttpCommon):
         emp1.write({"name": "Pos Employee1", "pin": "2580", "user_id": emp1_user.id})
 
         # Non-user employee
-        emp2 = self.env.ref("hr.employee_jve")
+        emp2 = cls.env.ref("hr.employee_jve").sudo().copy({
+            "company_id": cls.env.company.id,
+        })
         emp2.write({"name": "Pos Employee2", "pin": "1234"})
+        (admin + emp1 + emp2).company_id = cls.env.company
 
-        with Form(self.main_pos_config) as config:
+        with Form(cls.main_pos_config) as config:
             config.employee_ids.add(emp1)
             config.employee_ids.add(emp2)
 
@@ -49,5 +61,5 @@ class TestUi(TestPosHrHttpCommon):
         self.start_tour(
             "/pos/ui?config_id=%d" % self.main_pos_config.id,
             "PosHrTour",
-            login="admin",
+            login="accountman",
         )
