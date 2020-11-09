71a3d15039cb45cba62665a26296761b77d607fe
Debauche Stéphane <std@odoo.com>
2020-11-09 17:38:43 +0100

    [IMP] base: make datetime coherent with dateutil
    As we do not need all the datetime module, we want to restrict the
    functions (as we do for dateutil). Also centralise handling of this
    and apply it by default, no reason for there to be divergences.
    
    Task 2179629

--- addons/mail/models/mail_template.py
+++ addons/mail/models/mail_template.py
@@ -97,7 +97,7 @@ try:
         'str': str,
         'quote': urls.url_quote,
         'urlencode': urls.url_encode,
-        'datetime': datetime,
+        'datetime': tools.wrap_module(datetime, []),
         'len': len,
         'abs': abs,
         'min': min,
