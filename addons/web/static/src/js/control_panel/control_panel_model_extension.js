283759bf338abf53509bb250012c283b478dd03c
Mohammed Shekha <msh@odoo.com>
2020-11-10 16:39:44 +0530

    [IMP] web: add customGroup property when custom group created
    before this commit: it is technically difficult which groups are coming from
    <group> tag and which groups are coming from custom group, as there is no
    such property to differentiate.
    
    after this commit: when createNewGroupBy method is called it will create custom
    group and will add customGroup: true on newly created groups sol later it can
    be differntiated with this property.
    
    task-2376505

--- addons/web/static/src/js/control_panel/control_panel_model_extension.js
+++ addons/web/static/src/js/control_panel/control_panel_model_extension.js
@@ -323,6 +323,7 @@ odoo.define("web/static/src/js/control_panel/control_panel_model_extension.js", 
                 groupNumber,
                 id: filterId,
                 type: 'groupBy',
+                customGroup: true,
             };
             this.state.filters[filterId] = filter;
             if (['date', 'datetime'].includes(field.type)) {
