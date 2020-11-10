ae368bd0decf53217c30d97a4547d495f81b9f19
Thanh Dodeur <tso@odoo.com>
2020-11-10 06:25:22 +0000

    [FIX] mail: prevents image loading errors
    Before this commit, an event listener was still active after the removal
    of the attachment viewer, which caused the the onLoad function to be
    called on an removed component if the component was unmounted before
    the image was loaded. This commit fixes this issue by adding the
    eventListener through the owl template.
    
    Additionally, when a new image was loading, the old image was kept in
    the dom until the end of the loading of the new image, this commit
    also fixes this issue by adding a t-key on the image element.
    
    part of task-2363522
    
    closes odoo/odoo#63452
    
    X-original-commit: 31f71f500b88aa3eb0688ab990d0c5f00676c71e
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/components/attachment/attachment_tests.js
+++ addons/mail/static/src/components/attachment/attachment_tests.js
@@ -630,6 +630,43 @@ QUnit.test('clicking on the delete attachment button multiple times should do th
     );
 });
 
+QUnit.test('[technical] does not crash when the viewer is closed before image load', async function (assert) {
+    /**
+     * When images are displayed using `src` attribute for the 1st time, it fetches the resource.
+     * In this case, images are actually displayed (fully fetched and rendered on screen) when
+     * `<image>` intercepts `load` event.
+     *
+     * Current code needs to be aware of load state of image, to display spinner when loading
+     * and actual image when loaded. This test asserts no crash from mishandling image becoming
+     * loaded from being viewed for 1st time, but viewer being closed while image is loading.
+     */
+    assert.expect(1);
+
+    await this.start({ hasDialog: true });
+    const attachment = this.env.models['mail.attachment'].create({
+        filename: "test.png",
+        id: 750,
+        mimetype: 'image/png',
+        name: "test.png",
+    });
+    await this.createAttachmentComponent(attachment);
+    await afterNextRender(() => document.querySelector('.o_Attachment_image').click());
+    const imageEl = document.querySelector('.o_AttachmentViewer_viewImage');
+    await afterNextRender(() =>
+        document.querySelector('.o_AttachmentViewer_headerItemButtonClose').click()
+    );
+    // Simulate image becoming loaded.
+    let successfulLoad;
+    try {
+        imageEl.dispatchEvent(new Event('load', { bubbles: true }));
+        successfulLoad = true;
+    } catch (err) {
+        successfulLoad = false;
+    } finally {
+        assert.ok(successfulLoad, 'should not crash when the image is loaded');
+    }
+});
+
 });
 });
 });
