61f946483175d03771509b5581abcb08f820aaca
Benoit Socias <bso@odoo.com>
2020-11-12 13:03:44 +0000

    [FIX] website_sale: find abandoned cart using specific delay per website
    Before this commit the search for abandoned carts was always using a delay of
    one hour if no ebsite in the context.
    
    After this commit the search for abandoned carts uses the specific cart
    abandoned delay configured for each website and the related public partner_id.
    
    This commit closes #59709, #54932
    
    closes odoo/odoo#61671
    
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- addons/website_sale/tests/__init__.py
+++ addons/website_sale/tests/__init__.py
@@ -4,6 +4,7 @@
 from . import test_customize
 from . import test_sale_process
 from . import test_sitemap
+from . import test_website_sale_cart_abandoned
 from . import test_website_sale_cart_recovery
 from . import test_website_sale_mail
 from . import test_website_sale_pricelist
