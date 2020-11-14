443517005639aaab0c5859d26b161848f7c5cf00
Xavier Morel <xmo@odoo.com>
2020-11-14 17:15:53 +0100

    [IMP] mail: better error reporting to follow

--- addons/mail/models/mail_followers.py
+++ addons/mail/models/mail_followers.py
@@ -48,7 +48,7 @@ class Followers(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        res = super(Followers, self).create(vals_list)
+        res = super(Followers, self).create(vals_list)._check_rights()
         res._invalidate_documents()
         return res
 
@@ -57,6 +57,7 @@ class Followers(models.Model):
         if 'res_model' in vals or 'res_id' in vals:
             self._invalidate_documents()
         res = super(Followers, self).write(vals)
+        self._check_rights()
         if any(x in vals for x in ['res_model', 'res_id', 'partner_id']):
             self._invalidate_documents()
         return res
@@ -65,6 +66,21 @@ class Followers(models.Model):
     def unlink(self):
         self._invalidate_documents()
         return super(Followers, self).unlink()
+
+    def _check_rights(self):
+        user_partner = self.env.user.partner_id
+        for record in self:
+            obj = self.env[record.res_model].browse(record.res_id)
+            if record.channel_id or record.partner_id != user_partner:
+                obj.check_access_rights('write')
+                obj.check_access_rule('write')
+                subject = record.channel_id or record.partner_id
+                subject.check_access_rights('read')
+                subject.check_access_rule('read ')
+            else:
+                obj.check_access_rights('read')
+                obj.check_access_rule('read')
+        return self
 
     _sql_constraints = [
         ('mail_followers_res_partner_res_model_id_uniq', 'unique(res_model,res_id,partner_id)', 'Error, a partner cannot follow twice the same object.'),
