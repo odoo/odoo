839e0ebb0c991cc82a200dd2e2a301d21a5fa878
Florent de Labarre <florent.mirieu@gmail.com>
2020-11-15 10:45:46 +0100

    [FIX] various: respect mail_post_autofollow previous values when adding it
    Purpose of this commit is to avoid forcing mail_post_autofollow to True when
    it is set to False. It eases inheritance and custom behavior.
    
    Task-2612911
    PR odoo/odoo#60792

--- addons/mail/models/mail_activity.py
+++ addons/mail/models/mail_activity.py
@@ -1082,7 +1082,7 @@ class MailActivityMixin(models.AbstractModel):
         template = self.env['mail.template'].browse(template_id).exists()
         if not template:
             return False
-        for record in self.with_context(mail_post_autofollow=True):
+        for record in self.with_context(mail_post_autofollow=self.env.context.get('mail_post_autofollow', True)):
             record.message_post_with_template(
                 template_id,
                 composition_mode='comment'
