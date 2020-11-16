c7a1754009a4c36f175c60ff35246cb52d6ba57d
Jacky (trj) <trj@odoo.com>
2020-11-16 13:55:29 +0100

    manifest update

--- addons/l10n_de_pos_cert/__manifest__.py
+++ addons/l10n_de_pos_cert/__manifest__.py
@@ -1,31 +1,27 @@
 # -*- coding: utf-8 -*-
+
 {
     'name': "l10n_de_pos_cert",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        Germany TSS Regulation
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        This module brings the technical requirement for the new Germany regulation with the Technical Security System by using a cloud-based solution with Fiskaly.
+        
+        Install this if you are using the Point of Sale app in Germany.    
+        
+    """,
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'category': 'Accounting/Localizations/Point of Sale',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['l10n_de', 'point_of_sale'],
     'installable': True,
     'auto_install': True,
     'application': False,
 
-    # always loaded
     'data': [
         'data/ir_config_param.xml',
         'views/l10n_de_pos_cert_templates.xml',
