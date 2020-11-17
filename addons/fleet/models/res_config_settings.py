eb590f9dce52da700fb6d7267c2e03fb9a2b3804
Goffin Simon <sig@odoo.com>
2020-11-17 09:23:02 +0100

    [FIX] fleet: Module fleet_account doesn't exist
    The module fleet_account has been replaced by account_disallowed_expenses_fleet
    in https://github.com/odoo/odoo/commit/db9da432b7aedcd1bc944e23e59459c9c92801b7
    
    opw:2382169

--- addons/fleet/models/res_config_settings.py
+++ addons/fleet/models/res_config_settings.py
@@ -8,4 +8,4 @@ class ResConfigSettings(models.TransientModel):
     _inherit = ['res.config.settings']
 
     delay_alert_contract = fields.Integer(string='Delay alert contract outdated', default=30, config_parameter='hr_fleet.delay_alert_contract')
-    module_fleet_account = fields.Boolean(string="Analytic Accounting Fleet")
+
