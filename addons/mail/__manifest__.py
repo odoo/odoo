9d16ebf907b45edff27b59eabf1d20e0d78eff4a
Thibault Delavallée <tde@odoo.com>
2020-11-17 14:10:19 +0000

    [IMP] mail: remove default blank template used in contextual actions
    Mail holds a blank template that adds no real value. Its purpose was to
    give a base for parner managers to perform a mass mailing on leads. Using a
    template and its action menu entry it was possible to launch a mail composer
    in mass mail mode on leads.
    
    However template itself is not necessary. Same goal can be achieved with
    a standard action. As template was blank and creating noise we can remove
    it and simplify available templates.
    
    LINKS
    
    Task ID-2375767
    COM PR odoo/odoo#61814
    ENT PR odoo/enterprise#14775
    UPG PR odoo/upgrade#1936

--- addons/mail/__manifest__.py
+++ addons/mail/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Discuss',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Productivity/Discuss',
     'sequence': 145,
     'summary': 'Chat, mail gateway and private channels',
