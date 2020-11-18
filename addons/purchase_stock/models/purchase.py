0b1f43aefa219216981933301861658bb4e2838b
Florent de Labarre <florent.mirieu@gmail.com>
2020-11-18 17:50:19 +0100

    [FIX] purchase_stock : use limit
    Don't need to put in cache all picking_type.
    Use limit = 1

--- addons/purchase_stock/models/purchase.py
+++ addons/purchase_stock/models/purchase.py
@@ -195,10 +195,10 @@ class PurchaseOrder(models.Model):
 
     @api.model
     def _get_picking_type(self, company_id):
-        picking_type = self.env['stock.picking.type'].search([('code', '=', 'incoming'), ('warehouse_id.company_id', '=', company_id)])
+        picking_type = self.env['stock.picking.type'].search([('code', '=', 'incoming'), ('warehouse_id.company_id', '=', company_id)], limit=1)
         if not picking_type:
-            picking_type = self.env['stock.picking.type'].search([('code', '=', 'incoming'), ('warehouse_id', '=', False)])
-        return picking_type[:1]
+            picking_type = self.env['stock.picking.type'].search([('code', '=', 'incoming'), ('warehouse_id', '=', False)], limit=1)
+        return picking_type
 
     def _prepare_picking(self):
         if not self.group_id:
