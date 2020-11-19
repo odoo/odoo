68694bdd2ba141edc85acd2b3560c0cf6ea3659c
Debauche Stéphane <std@odoo.com>
2020-11-19 18:53:52 +0100

    [FIX] mail: make the current user member of the created channel
    Forward port fixes done in stable versions and not correclty forward ported
    into master at merge time.
    
    ORIGINAL COMMIT
    
    Purpose
    =======
    Before, by default, if a user create a channel, he will not be member
    of this channel.
    
    After, the current user will always be member of the new channel.
    
    LINKS
    
    Task ID-2421795
    COM PR odoo/odoo#63677
    X-Original-commit odoo/odoo@eda542c82f84d7b5589846691b9cb6b7f1021947
    X-Original-Task ID-1963414

--- addons/mail/tests/__init__.py
+++ addons/mail/tests/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_mail_channel
+from . import test_mail_channel_partner
 from . import test_mail_full_composer
 from . import test_res_partner
 from . import test_update_notification
