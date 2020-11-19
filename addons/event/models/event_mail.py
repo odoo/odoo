9f25b5d5748a05b4c372cc995f4e6afdc2714a04
Paul Morelle <pmo@odoo.com>
2020-11-19 17:30:21 +0100

    [FIX] event: avoid too large prefetching of event.mail
    The prefetching engine wants to prefetch all the registrations to all
    the events of all the schedulers, which may make a lot of data.
    
    If the autocommit is on, then all this prefetched data is discarded at
    each loop.
    
    This commit limits the prefetching to each scheduler, so that we avoid
    prefetching the data unnecessarily.
    
    X-original-commit: 904a46f28cb64e3bd51c40dc364b256d434d1c93

--- addons/event/models/event_mail.py
+++ addons/event/models/event_mail.py
@@ -169,7 +169,8 @@ You receive this email because you are:
         for scheduler in schedulers:
             try:
                 with self.env.cr.savepoint():
-                    scheduler.execute()
+                    # Prevent a mega prefetch of the registration ids of all the events of all the schedulers
+                    self.browse(scheduler.id).execute()
             except Exception as e:
                 _logger.exception(e)
                 self.invalidate_cache()
