f06d12cff759c11ba68f51af2601823ea8bc626a
Sébastien Theys <seb@odoo.com>
2020-11-20 17:05:53 +0100

    [FIX] mail: avoid empty seen indicator
    `MessageSeenIndicator` sub-component should not be rendered if the thread does
    not support seen indicators.
    
    The issue is manifesting because the otherwise empty indicator has a margin,
    which is adding more space than expected before the following element.
    
    X-original-commit: 4e0e8a415ae2175cd68e514cff33b2bffeb99db7

--- addons/mail/static/src/components/message/message_tests.js
+++ addons/mail/static/src/components/message/message_tests.js
@@ -343,6 +343,7 @@ QUnit.test("'channel_fetch' notification received is correctly handled", async f
         display_name: "Demo User",
     });
     const thread = this.env.models['mail.thread'].create({
+        channel_type: 'chat',
         id: 11,
         members: [
             [['link', currentPartner]],
@@ -404,6 +405,7 @@ QUnit.test("'channel_seen' notification received is correctly handled", async fu
         display_name: "Demo User",
     });
     const thread = this.env.models['mail.thread'].create({
+        channel_type: 'chat',
         id: 11,
         members: [
             [['link', currentPartner]],
@@ -464,6 +466,7 @@ QUnit.test("'channel_fetch' notification then 'channel_seen' received  are corre
         display_name: "Demo User",
     });
     const thread = this.env.models['mail.thread'].create({
+        channel_type: 'chat',
         id: 11,
         members: [
             [['link', currentPartner]],
@@ -541,6 +544,7 @@ QUnit.test('do not show messaging seen indicator if not authored by me', async f
         display_name: "Demo User"
     });
     const thread = this.env.models['mail.thread'].create({
+        channel_type: 'chat',
         id: 11,
         partnerSeenInfos: [['create', [
             {
@@ -589,6 +593,7 @@ QUnit.test('do not show messaging seen indicator if before last seen by all mess
         display_name: "Demo User",
     });
     const thread = this.env.models['mail.thread'].create({
+        channel_type: 'chat',
         id: 11,
         messageSeenIndicators: [['insert', {
             channelId: 11,
@@ -656,6 +661,7 @@ QUnit.test('only show messaging seen indicator if authored by me, after last see
         display_name: "Demo User"
     });
     const thread = this.env.models['mail.thread'].create({
+        channel_type: 'chat',
         id: 11,
         partnerSeenInfos: [['create', [
             {
