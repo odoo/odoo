2713ee8af957766d7f40851a91c7a473d8b4587b
Thibault Delavallée <tde@odoo.com>
2020-11-20 09:09:23 +0000

    [FIX] test_mail_full: fix overlapping class names
    It is better to use another naming than re-using class names. Somehow it
    seems sometimes we have MRO issues.
    
    Task ID-2390314
    PR odoo/odoo#62384
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/test_mail_full/tests/test_sms_server_actions.py
+++ addons/test_mail_full/tests/test_sms_server_actions.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.addons.test_mail_full.tests.common import TestMailFullCommon, TestRecipients
+from odoo.addons.test_mail_full.tests.common import TestMailFullCommon, TestMailFullRecipients
 
 
-class TestServerAction(TestMailFullCommon, TestRecipients):
+class TestServerAction(TestMailFullCommon, TestMailFullRecipients):
 
     @classmethod
     def setUpClass(cls):
