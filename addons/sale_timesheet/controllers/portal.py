47738f1015dbfcf38db9df401511cd73526fd10f
Laurent Stukkens (LTU) <ltu@odoo.com>
2020-11-20 18:20:33 +0100

    [IMP] sale_timesheet: adapt timesheet sections in portal
    Prior to this commit:
    
        * The portal timesheet section was only visible in invoices when
          invoicing policy was set to delivery.
        * The portal timesheet section was not visible on orders.
    
    After this commit:
    
        * The portal timesheet section will be displayed for both delivery and order invoicing
          policies.
        * The portal timesheet section will be available for orders.
    
    task-2388500

--- addons/sale_timesheet/controllers/portal.py
+++ addons/sale_timesheet/controllers/portal.py
@@ -12,7 +12,29 @@ class PortalAccount(portal.PortalAccount):
     def _invoice_get_page_view_values(self, invoice, access_token, **kwargs):
         values = super(PortalAccount, self)._invoice_get_page_view_values(invoice, access_token, **kwargs)
         domain = request.env['account.analytic.line']._timesheet_get_portal_domain()
-        domain = expression.AND([domain, [('timesheet_invoice_id', '=', invoice.id)]])
+        domain = expression.AND([
+            domain,
+            request.env['account.analytic.line']._timesheet_get_sale_domain(
+                invoice.mapped('line_ids.sale_line_ids'),
+                request.env['account.move'].browse([invoice.id])
+            )
+        ])
+        values['timesheets'] = request.env['account.analytic.line'].sudo().search(domain)
+        values['is_uom_day'] = request.env['account.analytic.line'].sudo()._is_timesheet_encode_uom_day()
+        return values
+
+
+class CustomerPortal(portal.CustomerPortal):
+    def _order_get_page_view_values(self, order, access_token, **kwargs):
+        values = super(CustomerPortal, self)._order_get_page_view_values(order, access_token, **kwargs)
+        domain = request.env['account.analytic.line']._timesheet_get_portal_domain()
+        domain = expression.AND([
+            domain,
+            request.env['account.analytic.line']._timesheet_get_sale_domain(
+                order.mapped('order_line'),
+                order.invoice_ids
+            )
+        ])
         values['timesheets'] = request.env['account.analytic.line'].sudo().search(domain)
         values['is_uom_day'] = request.env['account.analytic.line'].sudo()._is_timesheet_encode_uom_day()
         return values
