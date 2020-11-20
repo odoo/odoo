a9cc39babf3dc5c7da923c5ed5534792f5df2a53
Laurent Stukkens (LTU) <ltu@odoo.com>
2020-11-20 17:20:33 +0000

    [IMP] sale_timesheet: adapt timesheet sections in portal
    Prior to this commit:
    
        * The portal timesheet section was only visible in invoices when
          invoicing policy was set to delivery.
        * The portal timesheet section was not visible on orders.
    
    After this commit:
    
        * The portal timesheet section will be displayed for both delivery and order invoicing
          policies.
        * The portal timesheet section will be available for orders.
    
    task-2388500
    
    closes odoo/odoo#62516
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/sale_timesheet/models/account.py
+++ addons/sale_timesheet/models/account.py
@@ -131,7 +131,20 @@ class AccountAnalyticLine(models.Model):
             thus there is no meaning of showing invoice with ordered quantity.
         """
         domain = super(AccountAnalyticLine, self)._timesheet_get_portal_domain()
-        return expression.AND([domain, [('timesheet_invoice_type', 'in', ['billable_time', 'non_billable'])]])
+        return expression.AND([domain, [('timesheet_invoice_type', 'in', ['billable_time', 'non_billable', 'billable_fixed'])]])
+
+    @api.model
+    def _timesheet_get_sale_domain(self, order_lines_ids, invoice_ids):
+        return [
+            '|',
+            '&',
+            ('timesheet_invoice_id', 'in', invoice_ids.ids),
+            #TODO Question to Reviewer: non_billable was part of domain in _timesheet_get_portal_domain so I kept it here, does this make sense?
+            ('timesheet_invoice_type', 'in', ['billable_time', 'non_billable']),
+            '&',
+            ('timesheet_invoice_type', '=', 'billable_fixed'),
+            ('so_line', 'in', order_lines_ids.ids)
+        ]
 
     def _get_timesheets_to_merge(self):
         res = super(AccountAnalyticLine, self)._get_timesheets_to_merge()
