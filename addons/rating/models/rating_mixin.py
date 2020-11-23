58553165881686a2c0f6912f05e5637763329752
adekock11 <adekock11@gmail.com>
2020-11-23 12:47:05 +0200

    [FIX] rating: When doing group-by or pivot views, it makes sense to average the rating values rather than the default sum

--- addons/rating/models/rating_mixin.py
+++ addons/rating/models/rating_mixin.py
@@ -53,7 +53,7 @@ class RatingMixin(models.AbstractModel):
     _description = "Rating Mixin"
 
     rating_ids = fields.One2many('rating.rating', 'res_id', string='Rating', groups='base.group_user', domain=lambda self: [('res_model', '=', self._name)], auto_join=True)
-    rating_last_value = fields.Float('Rating Last Value', groups='base.group_user', compute='_compute_rating_last_value', compute_sudo=True, store=True)
+    rating_last_value = fields.Float('Rating Last Value', groups='base.group_user', compute='_compute_rating_last_value', compute_sudo=True, store=True, group_operator='avg')
     rating_last_feedback = fields.Text('Rating Last Feedback', groups='base.group_user', related='rating_ids.feedback')
     rating_last_image = fields.Binary('Rating Last Image', groups='base.group_user', related='rating_ids.rating_image')
     rating_count = fields.Integer('Rating count', compute="_compute_rating_stats", compute_sudo=True)
