8b00a22fac329eccc752404bbf2d4e348d36e862
svs-odoo <svs@odoo.com>
2020-11-24 09:46:52 +0000

    mrp,stock: rename quality_control_worksheet
    Add option to install Quality Control / QC Worksheets from stock
    settings.
    
    task-2281266

--- addons/mrp/models/res_config_settings.py
+++ addons/mrp/models/res_config_settings.py
@@ -15,7 +15,7 @@ class ResConfigSettings(models.TransientModel):
     module_mrp_plm = fields.Boolean("Product Lifecycle Management (PLM)")
     module_mrp_workorder = fields.Boolean("Work Orders")
     module_quality_control = fields.Boolean("Quality")
-    module_quality_worksheet = fields.Boolean("Quality Worksheet")
+    module_quality_control_worksheet = fields.Boolean("Quality Worksheet")
     module_mrp_subcontracting = fields.Boolean("Subcontracting")
     group_mrp_routings = fields.Boolean("MRP Work Orders",
         implied_group='mrp.group_mrp_routings')
