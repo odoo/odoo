b181d83eec0ce9acaacd057f1cc94d25d8e43b08
Pierre Masereel <pim@odoo.com>
2020-11-26 15:52:03 +0100

    [FIX] point_of_sale: handle picking type without lot usage
    When you are configuring the picking type used in the point of sale to
    not use existing lots and not create lots, the POS still ask you to
    encode a lot for product tracked, and the picking created don't use
    existing lots in stock because no reservation is made when a picking is
    valaidated from point of sale.
    
    As we want to reserve only in this specific case, we are now checking
    the options on the picking type to reserve or not product tracked.
    
    We are also not asking anymore for lot or serial number if picking type
    is configured that way.
    
    OPW-2377008
    
    X-original-commit: f9dd1d88d0d24914028464cc12b69ff0878346f8

--- addons/point_of_sale/models/stock_picking.py
+++ addons/point_of_sale/models/stock_picking.py
@@ -3,7 +3,7 @@
 
 from odoo import api,fields, models
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools import float_is_zero
+from odoo.tools import float_is_zero, float_compare
 
 from itertools import groupby
 
@@ -94,49 +94,63 @@ class StockPicking(models.Model):
             )
             confirmed_moves = current_move._action_confirm()
             for move in confirmed_moves:
-                if first_line.product_id == move.product_id and first_line.product_id.tracking != 'none' and (self.picking_type_id.use_existing_lots or self.picking_type_id.use_create_lots):
-                    for line in order_lines:
-                        sum_of_lots = 0
-                        for lot in line.pack_lot_ids.filtered(lambda l: l.lot_name):
-                            if line.product_id.tracking == 'serial':
-                                qty = 1
-                            else:
-                                qty = abs(line.qty)
-                            ml_vals = move._prepare_move_line_vals()
-                            ml_vals.update({'qty_done':qty})
-                            if self.picking_type_id.use_existing_lots:
-                                existing_lot = self.env['stock.production.lot'].search([
-                                    ('company_id', '=', self.company_id.id),
-                                    ('product_id', '=', line.product_id.id),
-                                    ('name', '=', lot.lot_name)
-                                ])
-                                if not existing_lot and self.picking_type_id.use_create_lots:
-                                    existing_lot = self.env['stock.production.lot'].create({
-                                        'company_id': self.company_id.id,
-                                        'product_id': line.product_id.id,
-                                        'name': lot.lot_name,
-                                    })
-                                ml_vals.update({
-                                    'lot_id': existing_lot.id,
-                                })
-                            else:
-                                ml_vals.update({
-                                    'lot_name': lot.lot_name,
-                                })
-                            self.env['stock.move.line'].create(ml_vals)
-                            sum_of_lots += qty
-                        if abs(line.qty) != sum_of_lots:
-                            difference_qty = abs(line.qty) - sum_of_lots
-                            ml_vals = current_move._prepare_move_line_vals()
-                            if line.product_id.tracking == 'serial':
-                                ml_vals.update({'qty_done': 1})
-                                for i in range(int(difference_qty)):
+                if first_line.product_id == move.product_id:
+                    if first_line.product_id.tracking != 'none':
+                        if self.picking_type_id.use_existing_lots or self.picking_type_id.use_create_lots:
+                            for line in order_lines:
+                                sum_of_lots = 0
+                                for lot in line.pack_lot_ids.filtered(lambda l: l.lot_name):
+                                    if line.product_id.tracking == 'serial':
+                                        qty = 1
+                                    else:
+                                        qty = abs(line.qty)
+                                    ml_vals = move._prepare_move_line_vals()
+                                    ml_vals.update({'qty_done':qty})
+                                    if self.picking_type_id.use_existing_lots:
+                                        existing_lot = self.env['stock.production.lot'].search([
+                                            ('company_id', '=', self.company_id.id),
+                                            ('product_id', '=', line.product_id.id),
+                                            ('name', '=', lot.lot_name)
+                                        ])
+                                        if not existing_lot and self.picking_type_id.use_create_lots:
+                                            existing_lot = self.env['stock.production.lot'].create({
+                                                'company_id': self.company_id.id,
+                                                'product_id': line.product_id.id,
+                                                'name': lot.lot_name,
+                                            })
+                                        ml_vals.update({
+                                            'lot_id': existing_lot.id,
+                                        })
+                                    else:
+                                        ml_vals.update({
+                                            'lot_name': lot.lot_name,
+                                        })
                                     self.env['stock.move.line'].create(ml_vals)
-                            else:
-                                ml_vals.update({'qty_done': difference_qty})
+                                    sum_of_lots += qty
+                                if abs(line.qty) != sum_of_lots:
+                                    difference_qty = abs(line.qty) - sum_of_lots
+                                    ml_vals = current_move._prepare_move_line_vals()
+                                    if line.product_id.tracking == 'serial':
+                                        ml_vals.update({'qty_done': 1})
+                                        for i in range(int(difference_qty)):
+                                            self.env['stock.move.line'].create(ml_vals)
+                                    else:
+                                        ml_vals.update({'qty_done': difference_qty})
+                                        self.env['stock.move.line'].create(ml_vals)
+                        else:
+                            move._action_assign()
+                            sum_of_lots = 0
+                            for move_line in move.move_line_ids:
+                                move_line.qty_done = move_line.product_uom_qty
+                                sum_of_lots += move_line.product_uom_qty
+                            if float_compare(move.product_uom_qty, move.quantity_done, precision_rounding=move.product_uom.rounding) > 0:
+                                remaining_qty = move.product_uom_qty - move.quantity_done
+                                ml_vals = move._prepare_move_line_vals()
+                                ml_vals.update({'qty_done':remaining_qty})
                                 self.env['stock.move.line'].create(ml_vals)
-                else:
-                    move.quantity_done = move.product_uom_qty
+
+                    else:
+                        move.quantity_done = move.product_uom_qty
 
     def _send_confirmation_email(self):
         # Avoid sending Mail/SMS for POS deliveries
