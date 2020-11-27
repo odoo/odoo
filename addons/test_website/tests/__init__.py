3ac583850a1c72942153bf050dde10de8cd9feec
Benoit Socias <bso@odoo.com>
2020-11-27 15:42:23 +0000

    [FIX] web_editor, test_website: will fix the deletion of custom snippets
    This commit is a manual forward port of #62555. The tour test needed to
    be adapted because the navigation flow is slightly different: upon
    custom snippet creation, the editor page used to be saved and reloaded
    in 14.0 but this is not the case anymore.
    
    Before this commit the deletion of custom snippets failed because the
    button click event got intercepted by the drag'n'drop mechanism.
    The currentTarget of the event was used in an asynchronous call where it
    had already been replaced by the jQuery's event bubbling mechanism when
    invoking the other handler.
    
    After this commit the deletion of custom snippets works again and a test
    tour is introduced to make sure it does not get broken again
    
    task-2405854
    Manual forward port of #62555, identical to #63108
    
    closes odoo/odoo#63388
    
    X-original-commit: 4de0519d5b54634604e22b32d91a099695378088
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- addons/test_website/tests/__init__.py
+++ addons/test_website/tests/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_controller_args
+from . import test_custom_snippet
 from . import test_error
 from . import test_is_multilang
 from . import test_multi_company
