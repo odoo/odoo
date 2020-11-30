56810703f2831561dcd9efc784b40d026b92066d
Laurent Stukkens (LTU) <ltu@odoo.com>
2020-11-30 18:51:36 +0100

    [IMP] sale_timesheet: remaining hours in task sales_line_id
    Prior to this commit:
    
        - The sale_line_id name_get shows the product name.
    
    After this commit:
    
        - In the task Form view, the SOL also displays the remaining quantity for
          products which have a service_policy set to 'ordered_timesheet'.
    
    task-2409761

--- addons/sale_timesheet/models/project.py
+++ addons/sale_timesheet/models/project.py
@@ -231,6 +231,28 @@ class ProjectTask(models.Model):
 
     # TODO: [XBO] remove me in master
     non_allow_billable = fields.Boolean("Non-Billable", help="Your timesheets linked to this task will not be billed.")
+    remaining_hours_so = fields.Float('Remaining Hours on SO', compute='_compute_remaining_hours_so')
+    remaining_hours_available = fields.Boolean(related="sale_line_id.remaining_hours_available")
+
+    @api.depends('sale_line_id', 'timesheet_ids', 'timesheet_ids.unit_amount')
+    def _compute_remaining_hours_so(self):
+        # TODO This is not yet perfectly working as timesheet.so_line stick to its old value although changed
+        #      in the task From View.
+        timesheets = self.timesheet_ids.filtered(lambda t: t.task_id.sale_line_id in (t.so_line, t._origin.so_line) and t.so_line.remaining_hours_available)
+
+        mapped_remaining_hours = {task._origin.id: task.sale_line_id and task.sale_line_id.remaining_hours or 0.0 for task in self}
+        uom_hour = self.env.ref('uom.product_uom_hour')
+        for timesheet in timesheets:
+            delta = 0
+            if timesheet._origin.so_line == timesheet.task_id.sale_line_id:
+                delta += timesheet._origin.unit_amount
+            if timesheet.so_line == timesheet.task_id.sale_line_id:
+                delta -= timesheet.unit_amount
+            if delta:
+                mapped_remaining_hours[timesheet.task_id._origin.id] += timesheet.so_line.product_uom._compute_quantity(delta, uom_hour)
+
+        for task in self:
+            task.remaining_hours_so = mapped_remaining_hours[task._origin.id]
 
     @api.depends(
         'allow_billable', 'allow_timesheets', 'sale_order_id')
