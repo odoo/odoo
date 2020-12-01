4e2e91717fa0f4c207b903e4bf8c3db24c5616d0
Mohammed Shekha <msh@odoo.com>
2020-12-01 06:04:15 +0000

    [IMP] web: list multi-edit: discard, cancel then save
    In a list view that allows the multi edition, select several
    records, edit a field, and 'click' on Discard. In the confirm
    dialog, click on 'Cancel'. The row is still in edition and the
    changes are still there. However, before this commit, clicking
    on 'Save' did nothing.
    
    The reason is that the 'Save' button is fake. Typically, clicking
    on it triggers a 'field_changed' event, thus saving the selected
    records. In this above scenario, the changes have already been
    notified, so nothing happens when the user clicks on 'Save'.
    
    This commit fixes the issue by storing the last 'field_changed'
    event, and re-triggering it when necessary. This isn't the cleaner
    way to handle that scenario, but it is the simpler, so we go for
    it in stable. In master, it would be nice to rework the multi
    edition feature with this scenario in mind.
    
    task-2376233
    
    closes odoo/odoo#66045
    
    X-original-commit: 18ed619baeda06f071779da00177beee01f8a896
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/list_tests.js
+++ addons/web/static/tests/views/list_tests.js
@@ -7856,6 +7856,50 @@ QUnit.module('Views', {
         list.destroy();
     });
 
+    QUnit.test('editable list view: click Discard, Cancel discard dialog and then Save in multi edition', async function (assert) {
+        assert.expect(5);
+
+        const list = await createView({
+            arch: `
+                <tree editable="top" multi_edit="1">
+                    <field name="foo"/>
+                </tree>`,
+            data: this.data,
+            model: 'foo',
+            View: ListView,
+        });
+
+        // select two records
+        await testUtils.dom.click(list.$('.o_data_row:eq(0) .o_list_record_selector input'));
+        await testUtils.dom.click(list.$('.o_data_row:eq(1) .o_list_record_selector input'));
+
+        await testUtils.dom.click(list.$('.o_data_row:first() .o_data_cell:first()'));
+        list.$('.o_data_row:first() .o_data_cell:first() input').val("oof");
+
+        const $discardButton = list.$buttons.find('.o_list_button_discard');
+
+        // Simulates an actual click (event chain is: mousedown > change > blur > focus > mouseup > click)
+        await testUtils.dom.triggerEvents($discardButton, ['mousedown']);
+        await testUtils.dom.triggerEvents(list.$('.o_data_row:first() .o_data_cell:first() input'),
+            ['change', 'blur', 'focusout']);
+        await testUtils.dom.triggerEvents($discardButton, ['focus']);
+        $discardButton[0].dispatchEvent(new MouseEvent('mouseup'));
+        await testUtils.dom.click($discardButton);
+
+        assert.ok($('.modal').text().includes("Warning"), "Modal should ask to discard changes");
+        await testUtils.dom.click($('.modal .btn:contains(Cancel)'));
+        assert.hasClass(list.$('.o_data_row:first'), 'o_selected_row',
+            "the first row should still be selected");
+
+        await testUtils.dom.click($('.o_list_button_save'));
+        assert.containsOnce(document.body, '.modal');
+        await testUtils.dom.click($('.modal .btn-primary'));
+        assert.containsNone(list, '.o_selected_row');
+        assert.strictEqual(list.$('.o_data_row .o_data_cell').text(), "oofoofgnapblip");
+
+        list.destroy();
+    });
+
     QUnit.test('editable list view: multi edition with readonly modifiers', async function (assert) {
         assert.expect(5);
 
