85ff73a71976b9460e27c78069830d17e2c626ef
Xavier BOL (xbo) <xbo@odoo.com>
2020-12-01 16:07:43 +0100

    [IMP] sale_timesheet: split the check in two methods
    This commit splits the _check_sale_line_in_project_map method in two
    methods to easily override the condition in the constrains (useful to
    accept the tickets where no SOL is defined in project linked in helpdesk
    team)
    
    task-2409761

--- addons/sale_timesheet/models/account.py
+++ addons/sale_timesheet/models/account.py
@@ -55,11 +55,13 @@ class AccountAnalyticLine(models.Model):
             else:
                 self.so_line = False
 
+    def _check_timesheet_can_be_billed(self):
+        return self.so_line in self.project_id.mapped('sale_line_employee_ids.sale_line_id') | self.task_id.sale_line_id | self.project_id.sale_line_id
+
     @api.constrains('so_line', 'project_id')
     def _check_sale_line_in_project_map(self):
-        for timesheet in self.filtered(lambda t: t.project_id and t.so_line):  # billed timesheet
-            if timesheet.so_line not in timesheet.project_id.mapped('sale_line_employee_ids.sale_line_id') | timesheet.task_id.sale_line_id | timesheet.project_id.sale_line_id:
-                raise ValidationError(_("This timesheet line cannot be billed: there is no Sale Order Item defined on the task, nor on the project. Please define one to save your timesheet line."))
+        if not all(t._check_timesheet_can_be_billed() for t in self.filtered(lambda t: t.project_id and t.so_line)):
+            raise ValidationError(_("This timesheet line cannot be billed: there is no Sale Order Item defined on the task, nor on the project. Please define one to save your timesheet line."))
 
     def write(self, values):
         # prevent to update invoiced timesheets if one line is of type delivery
