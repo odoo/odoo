d56fdc014270ea29fa5b9e35695a9be4391dd987
Benjamin Frantzen (bfr) <bfr@odoo.com>
2020-12-02 11:14:31 +0000

    [FIX] l10n_be_edi: fix domain on search product when importing UBL
    EAN13 is not a field, barcode is the correct field
    
    closes odoo/odoo#62798
    
    X-original-commit: b0e3f711be86e587b0ea1c7671bfab6c7bf10894
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>
    Signed-off-by: bfr-o <bfr-o@users.noreply.github.com>

--- addons/account_edi_ubl/models/account_edi_format.py
+++ addons/account_edi_ubl/models/account_edi_format.py
@@ -144,7 +144,7 @@ class AccountEdiFormat(models.Model):
                     elements = eline.xpath('cac:Item/cac:StandardItemIdentification/cbc:ID[@schemeID=\'GTIN\']', namespaces=namespaces)
                     if elements:
                         product_ean13 = elements[0].text
-                        domains.append([('ean13', '=', product_ean13)])
+                        domains.append([('barcode', '=', product_ean13)])
                     if domains:
                         product = self.env['product.product'].search(expression.OR(domains), limit=1)
                         if product:
