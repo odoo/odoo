ab7ec8c30b3f34fb4484f9b7591530b112cbd33a
Xavier Dubuc <xdu@odoo.com>
2020-12-02 09:32:47 +0000

    [FIX] mail: split attachments list to improve style
    There is now two lists of attachments : first one with images and second
    one with non-images. Attachment viewer allows to navigate through
    previews of all viewable attachments in the order they're shown.
    
    task-2272587
    task-2401325
    
    closes odoo/odoo#62719
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/components/message/message.js
+++ addons/mail/static/src/components/message/message.js
@@ -156,13 +156,6 @@ class Message extends Component {
     }
 
     /**
-     * @returns {mail.attachment[]}
-     */
-    get imageAttachments() {
-        return this.message.attachments.filter(attachment => attachment.fileType === 'image');
-    }
-
-    /**
      * Tell whether the bottom of this message is visible or not.
      *
      * @param {Object} param0
@@ -222,14 +215,6 @@ class Message extends Component {
     get message() {
         return this.env.models['mail.message'].get(this.props.messageLocalId);
     }
-
-    /**
-     * @returns {mail.attachment[]}
-     */
-    get nonImageAttachments() {
-        return this.message.attachments.filter(attachment => attachment.fileType !== 'image');
-    }
-
     /**
      * @returns {string}
      */
