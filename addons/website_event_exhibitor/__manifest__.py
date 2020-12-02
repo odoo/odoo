8c4ce772e7b90dfdffbad6435abc505c68e537fd
Thibault Delavallée <tde@odoo.com>
2020-12-02 09:23:09 +0000

    [ADD] website_event_exhibitor: add a module to handle sponsors and (online) exhibitors
    RATIONALE
    
    Events often have sponsors displayed on their front page. This should be
    a real standalone feature of Online Event application. However currently it
    cannot be used without tracks managements
    
    PURPOSE
    
    Purpose of this task is to allow to use and display sponsors on an Event front
    page without using tracks. Moreover online and chat capabilities should be
    part of a sponsor feature, located within website_event_exhibitor module.
    
    SPECIFICATIONS
    
    Create a void module ``website_event_exhibitor`` that will soon host
    
      * sponsor model from ``website_event_track``;
      * all exhibitor feature from ``website_event_track_exhibitor``;
      * without any dependency on tracks;
    
    When installing website_event_exhibitor people will gain access to all features
    related to sponsoring, from the simple cards to a complete jitsi-based chat
    capability on frontend.
    
    LINKS
    
    Task ID-2326433
    COM PR #61781
    ENT pr odoo/enterprise#14752
    UPG PR odoo/upgrade#1931

---
+++ addons/website_event_exhibitor/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Event Exhibitors',
+    'category': 'Marketing/Events',
+    'sequence': 1004,
+    'version': '1.0',
+    'summary': 'Event: manage sponsors and exhibitors',
+    'website': 'https://www.odoo.com/page/events',
+    'description': "",
+    'depends': [
+        'website_event',
+        'website_jitsi',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': False,
+    'installable': True,
+}
