37ebf84e6fe4647661207f1a9edc1578baf24931
Alexandre Kühn <aku@odoo.com>
2020-12-03 13:32:50 +0000

    [FIX] mail: ignore partner notification with unsupported `type`
    Task-2411800
    
    closes odoo/odoo#62930
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/models/messaging_notification_handler/messaging_notification_handler.js
+++ addons/mail/static/src/models/messaging_notification_handler/messaging_notification_handler.js
@@ -445,7 +445,7 @@ function factory(dependencies) {
                 return this._handleNotificationPartnerUnsubscribe(data.id);
             } else if (type === 'user_connection') {
                 return this._handleNotificationPartnerUserConnection(data);
-            } else {
+            } else if (!type) {
                 return this._handleNotificationPartnerChannel(data);
             }
         }
