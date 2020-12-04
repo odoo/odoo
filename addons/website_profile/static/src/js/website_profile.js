c1deab4303a41f79cabf6264c4e27418f3d09175
Benjamin Vray <bvr@odoo.com>
2020-12-04 10:44:48 +0100

    [FIX] website_forum, website_profile: disable editor toolbar options
    Before this commit, the inline style added with some options of the
    editor toolbar when editing a textarea was removed due to the
    strip_style of the content field.
    
    Since we cannot remove this strip_style for security reasons, we can
    only disable options that add inline style.
    
    This commit closes #34288
    
    X-original-commit: 74e4114e20186e31343af12a673fa3e4bc4eac4a

--- addons/website_profile/static/src/js/website_profile.js
+++ addons/website_profile/static/src/js/website_profile.js
@@ -60,13 +60,26 @@ publicWidget.registry.websiteProfileEditor = publicWidget.Widget.extend({
             return def;
         }
 
+        // Warning: Do not activate any option that adds inline style.
+        // Because the style is deleted after save.
+        var toolbar = [
+            ['style', ['style']],
+            ['font', ['bold', 'italic', 'underline', 'clear']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture']],
+            ['history', ['undo', 'redo']],
+        ];
+
         var $textarea = this.$('textarea.o_wysiwyg_loader');
         var loadProm = wysiwygLoader.load(this, $textarea[0], {
+            toolbar: toolbar,
             recordInfo: {
                 context: this._getContext(),
                 res_model: 'res.users',
                 res_id: parseInt(this.$('input[name=user_id]').val()),
             },
+            disableResizeImage: true,
         }).then(wysiwyg => {
             this._wysiwyg = wysiwyg;
         });
