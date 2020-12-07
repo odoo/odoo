136f90e6946688fa916af5be0f2064db6dba12dd
Nasreddin (bon) <bon@odoo.com>
2020-12-07 10:48:05 +0100

    [FIX] website_crm: Traceback on select form builder after uninstall website_crm
    Issue
    
    	- Install 'website_sale' and 'website_crm' modules
    	- Uninstall 'website_crm'
    	- Go to "Contact us" page
    	- Open Editor and click on form to edit it
    	- On sidebar, click on "Change Form Parameters"
    	- Change "Action" to "Create an Opportunity"
    
    	Traceback is raised.
    
    Cause
    
    	'website_form_access' field of 'crm.model_crm_lead' model is set to 'True' and therefore
    	display the choice in the selection menu of form builder.
    
    Solution
    
    	Add an uninstall hook to set 'website_form_access' to 'False'.
    
    opw-2393241

--- addons/website_crm/__init__.py
+++ addons/website_crm/__init__.py
@@ -3,3 +3,10 @@
 
 from . import controllers
 from . import models
+
+from odoo import api, SUPERUSER_ID
+
+def uninstall_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    model = env['ir.model'].search([('website_form_key', '=', "create_lead")])
+    model.write({'website_form_access' : False})
