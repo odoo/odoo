6f07db9fcdabd8768c4b2a625b01b531aaef38db
Ivan Yelizariev <iel@odoo.com>
2020-12-07 12:35:49 +0000

    [FIX] pos_cache: make it compatible with onboarding tool
    STEPS:
    * install pos_cache in clean database without demo
    * open pos
    * click OK on popup "Would you like to load demo data"
    
    closes odoo/odoo#63103
    
    Before: error, products are not loaded even after refreshing
    After: no errors, products are loaded
    X-original-commit: e669ea8a6a79fdf7e309b3191c9648c60179d195
    Signed-off-by: pimodoo <pimodoo@users.noreply.github.com>

--- addons/pos_cache/static/src/js/pos_cache.js
+++ addons/pos_cache/static/src/js/pos_cache.js
@@ -21,6 +21,7 @@ models.PosModel = models.PosModel.extend({
         // uncached way, so get rid of it.
         if (product_index !== -1) {
             this.models.splice(product_index, 1);
+            this.product_model = product_model;
         }
         return posmodel_super.load_server_data.apply(this, arguments).then(function () {
           // Give both the fields and domain to pos_cache in the
@@ -28,8 +29,8 @@ models.PosModel = models.PosModel.extend({
           // values in the backend and they automatically stay in
           // sync with whatever is defined (and maybe extended by
           // other modules) in js.
-          var product_fields =  typeof product_model.fields === 'function'  ? product_model.fields(self)  : product_model.fields;
-          var product_domain =  typeof product_model.domain === 'function'  ? product_model.domain(self)  : product_model.domain;
+          var product_fields =  typeof self.product_model.fields === 'function'  ? self.product_model.fields(self)  : self.product_model.fields;
+          var product_domain =  typeof self.product_model.domain === 'function'  ? self.product_model.domain(self)  : self.product_model.domain;
             var records = rpc.query({
                     model: 'pos.config',
                     method: 'get_products_from_cache',
