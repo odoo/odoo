7b00585291a48afc9fd5f75acc35bb82e93f4eea
Kevin Baptiste <kba@odoo.com>
2020-12-07 10:16:14 +0000

    [IMP] microsoft_account: allow configuration of endpoints
    The endpoints were hardcoded in the application code. This commit aims
    at making them overridable with system parameters:
    
     - `microsoft_account.auth_endpoint` defaults to
        https://login.microsoftonline.com/common/oauth2/v2.0/authorize
    
     - `microsoft_account.token_endpoint` defaults to
        https://login.microsoftonline.com/common/oauth2/v2.0/token
    
    This change allows customers to restrict the tenant the API targets[0].
    
    [0] https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-v2-protocols#endpoints
    
    closes odoo/odoo#62945
    
    Taskid: 2413197
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/microsoft_account/models/microsoft_service.py
+++ addons/microsoft_account/models/microsoft_service.py
@@ -14,8 +14,8 @@ _logger = logging.getLogger(__name__)
 
 TIMEOUT = 20
 
-MICROSOFT_AUTH_ENDPOINT = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
-MICROSOFT_TOKEN_ENDPOINT = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
+DEFAULT_MICROSOFT_AUTH_ENDPOINT = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
+DEFAULT_MICROSOFT_TOKEN_ENDPOINT = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
 
 
 class MicrosoftService(models.AbstractModel):
@@ -24,6 +24,14 @@ class MicrosoftService(models.AbstractModel):
 
     def _get_calendar_scope(self):
         return 'offline_access openid Calendars.ReadWrite'
+
+    @api.model
+    def _get_auth_endpoint(self):
+        return self.env["ir.config_parameter"].sudo().get_param('microsoft_account.auth_endpoint', DEFAULT_MICROSOFT_AUTH_ENDPOINT)
+
+    @api.model
+    def _get_token_endpoint(self):
+        return self.env["ir.config_parameter"].sudo().get_param('microsoft_account.token_endpoint', DEFAULT_MICROSOFT_TOKEN_ENDPOINT)
 
     @api.model
     def generate_refresh_token(self, service, authorization_code):
@@ -49,7 +57,7 @@ class MicrosoftService(models.AbstractModel):
             'grant_type': "refresh_token"
         }
         try:
-            req = requests.post(MICROSOFT_TOKEN_ENDPOINT, data=data, headers=headers, timeout=TIMEOUT)
+            req = requests.post(self._get_token_endpoint(), data=data, headers=headers, timeout=TIMEOUT)
             req.raise_for_status()
             content = req.json()
         except IOError:
@@ -82,7 +90,7 @@ class MicrosoftService(models.AbstractModel):
             'prompt': 'consent',
             'access_type': 'offline'
         })
-        return "%s?%s" % (MICROSOFT_AUTH_ENDPOINT, encoded_params)
+        return "%s?%s" % (self._get_auth_endpoint(), encoded_params)
 
     @api.model
     def _get_microsoft_tokens(self, authorize_code, service):
@@ -105,7 +113,7 @@ class MicrosoftService(models.AbstractModel):
             'redirect_uri': base_url + '/microsoft_account/authentication'
         }
         try:
-            dummy, response, dummy = self._do_request(MICROSOFT_TOKEN_ENDPOINT, params=data, headers=headers, method='POST', preuri='')
+            dummy, response, dummy = self._do_request(self._get_token_endpoint(), params=data, headers=headers, method='POST', preuri='')
             access_token = response.get('access_token')
             refresh_token = response.get('refresh_token')
             ttl = response.get('expires_in')
