e21e012491a0cd60398000d92124e60504570134
Xavier BOL (xbo) <xbo@odoo.com>
2020-12-10 18:34:47 +0100

    [IMP] {hr,sale}_timesheet: review portal timesheets
    This commit improves the portal view containing the timesheets list
    (this view is available in the '/my/timesheets' route.
    
    In sale_timesheet module, we
      * add new group by and a search on SOL,
      * add an SOL field on the left of the unit_amount one,
      * change the group by by default, to group the timesheets by SOL
      * display the following information in the group: 'x Hours ordered, x
    Remaining', when the timesheets are grouped by SOL. However, the 'x
    Remaining' part should only be visible if the number of timesheets is
    greater than 0.
      * move the total number of hours in the group itself and align it to
    the right.
      * change the number of items displayed per page, 100 items instead of
    20.
      * reduce the size of the font in tbody tag (0.8rem instead of
    0.875rem)
      * display the content of the field on hover, it's usefull when the
    field it's cropped.
      * Add 'View Timesheets' button in portal SO: this new button button
    called 'View timesheets' is displayed next the name of the product of
    a SO in Sale Order Portal. it is only visible if the number of
    timehseets is greater than 0 for this SOL. The button redirects the user
    to /my/timesheets page with a filter on the corresponding SOL + a group
    by SOL.
    
    task-2409761

--- addons/sale_timesheet/controllers/portal.py
+++ addons/sale_timesheet/controllers/portal.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo import http, _
 from odoo.http import request
 from odoo.osv import expression
 
 from odoo.addons.account.controllers import portal
+from odoo.addons.hr_timesheet.controllers.portal import TimesheetCustomerPortal
 
 
 class PortalAccount(portal.PortalAccount):
@@ -38,3 +40,33 @@ class CustomerPortal(portal.CustomerPortal):
         values['timesheets'] = request.env['account.analytic.line'].sudo().search(domain)
         values['is_uom_day'] = request.env['account.analytic.line'].sudo()._is_timesheet_encode_uom_day()
         return values
+
+
+class SaleTimesheetCustomerPortal(TimesheetCustomerPortal):
+
+    def _get_searchbar_inputs(self):
+        searchbar_inputs = super()._get_searchbar_inputs()
+        searchbar_inputs.update(sol={'input': 'sol', 'label': _('Search in Sales Order Item')}, sol_id={'input': 'sol_id', 'label': _('Search in Sales Order Item ID')})
+        return searchbar_inputs
+
+    def _get_searchbar_groupby(self):
+        searchbar_groupby = super()._get_searchbar_groupby()
+        searchbar_groupby.update(sol={'input': 'sol', 'label': _('Sales Order Item')})
+        return searchbar_groupby
+
+    def _get_search_domain(self, search_in, search):
+        search_domain = super()._get_search_domain(search_in, search)
+        if search_in in ('sol', 'all'):
+            search_domain = expression.OR([search_domain, [('so_line', 'ilike', search)]])
+        if search_in == 'sol_id':
+            search_domain = expression.OR([search_domain, [('so_line.id', '=', search)]])
+        return search_domain
+
+    def _get_groupby_mapping(self):
+        groupby_mapping = super()._get_groupby_mapping()
+        groupby_mapping.update(sol='so_line')
+        return groupby_mapping
+
+    @http.route(['/my/timesheets', '/my/timesheets/page/<int:page>'], type='http', auth="user", website=True)
+    def portal_my_timesheets(self, page=1, sortby=None, filterby=None, search=None, search_in='all', groupby='sol', **kw):
+        return super().portal_my_timesheets(page, sortby, filterby, search, search_in, groupby, **kw)
