c88944227b63ac61e27fde606cc560603a0fe695
Xavier Morel <xmo@odoo.com>
2020-12-11 10:12:13 +0000

    [IMP] website: mockify test
    instead of using ad-hoc weirds
    
    X-original-commit: 7a42cc19492ada223678b6b67fc85fb121155e2b

--- addons/website/tests/test_qweb.py
+++ addons/website/tests/test_qweb.py
@@ -4,7 +4,7 @@
 from lxml import etree
 import re
 
-from odoo import tools
+from odoo import http, tools
 from odoo.addons.base.tests.common import TransactionCaseWithUserDemo
 from odoo.addons.website.tools import MockRequest
 from odoo.modules.module import get_module_resource
@@ -150,15 +150,12 @@ class TestQwebProcessAtt(TransactionCase):
             self._test_att('/my-page', {'href': '/fr/my-page'})
 
     def test_process_att_url_crap(self):
-        with MockRequest(self.env, website=self.website) as request:
+        with MockRequest(self.env, website=self.website):
+            match = http.root.get_db_router.return_value.bind.return_value.match
             # #{fragment} is stripped from URL when testing route
             self._test_att('/x#y?z', {'href': '/x#y?z'})
-            self.assertEqual(
-                request.httprequest.app._log_call[-1],
-                (('/x',), {'method': 'POST', 'query_args': None})
-            )
+            match.assert_called_with('/x', method='POST', query_args=None)
+
+            match.reset_calls()
             self._test_att('/x?y#z', {'href': '/x?y#z'})
-            self.assertEqual(
-                request.httprequest.app._log_call[-1],
-                (('/x',), {'method': 'POST', 'query_args': 'y'})
-            )
+            match.assert_called_with('/x', method='POST', query_args='y')
