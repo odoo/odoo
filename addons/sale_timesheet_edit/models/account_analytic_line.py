efba0148a843e35cdcdfe3228f718d8fe8634f56
Xavier BOL (xbo) <xbo@odoo.com>
2020-12-14 11:19:57 +0100

    [FIX] sale_timesheet: determine SOL only if allow_billable=True
    Before this commit, even if the allow_billable=False in the current
    project, we search a SOL for task and timesheets.
    
    This commit determines the SOL in tasks and timesheets only if the
    allow_billable=True in the linked project.
    
    task-2409761

--- addons/sale_timesheet_edit/models/account_analytic_line.py
+++ addons/sale_timesheet_edit/models/account_analytic_line.py
@@ -10,7 +10,7 @@ class AccountAnalyticLine(models.Model):
 
     is_so_line_edited = fields.Boolean()
 
-    @api.depends('task_id.sale_line_id', 'project_id.sale_line_id', 'employee_id')
+    @api.depends('task_id.sale_line_id', 'project_id.sale_line_id', 'project_id.allow_billable', 'employee_id')
     def _compute_so_line(self):
         super(AccountAnalyticLine, self.filtered(lambda t: not t.is_so_line_edited))._compute_so_line()
 
