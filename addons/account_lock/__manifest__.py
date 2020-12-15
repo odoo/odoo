ea517235bc51ca5e7359ca041fa97b86e1025090
Nicolas (vin) <vin@odoo.com>
2020-12-15 12:48:58 +0000

    [IMP] account_lock: Update the module description
    In order to clarify the purpose of the account_lock module, slightly
    update its description.
    
    Task id #2382740
    
    closes odoo/odoo#64559
    
    X-original-commit: f15188907a63d3f50c80c8254e60b9f31a666c55
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>

--- addons/account_lock/__manifest__.py
+++ addons/account_lock/__manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Irreversible Lock Date',
-    'version' : '1.0',
+    'name': 'Irreversible Lock Date',
+    'version': '1.0',
     'category': 'Accounting/Accounting',
     'description': """
     Make the lock date irreversible:
 
-    * You cannot define stricter conditions on advisors than on users. Then, the lock date on advisor must be set before the lock date for users.
-    * You cannot lock a period that is not finished yet. Then, the lock date for advisors must be set before the last day of the previous month.
-    * The new lock date for advisors must be set after the previous lock date.
+    * You cannot set stricter restrictions on advisors than on users. Therefore, the All Users Lock Date must be anterior (or equal) to the Invoice/Bills Lock Date.
+    * You cannot lock a period that has not yet ended. Therefore, the All Users Lock Date must be anterior (or equal) to the last day of the previous month.
+    * Any new All Users Lock Date must be posterior (or equal) to the previous one.
     """,
-    'depends' : ['account'],
+    'depends': ['account'],
     'data': [],
 }
