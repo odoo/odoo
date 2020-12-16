0ffd8614ed753629bfe24bf62d791a79507f9004
Simon Genin (ges) <ges@odoo.com>
2020-12-16 13:28:11 +0100

    wip

---
+++ odoo/addons/base/transpiler/codes/classes.js
@@ -0,0 +1,9 @@
+export default class Nice {}
+
+class Vehicule {}
+
+export class Car extends Vehicule {}
+
+export class Boat extends Vehicule {}
+
+export const Ferrari = class Ferrari extends Car {};
---
+++ odoo/addons/base/transpiler/codes/import.odoo.js
@@ -0,0 +1,14 @@
+odoo.define('@tests/import', function (require) {
+  let __exports = {};
+
+  const { Dialog }  = require("@tests/Dialog");
+  const { Dialog, Notification }  = require("@tests/Dialog");
+  const { Dialog, Notification }  = require("@tests/Dialog");
+  const { Dialog, Notification }  = require("@tests/Dialog");
+  const { Dialog, Notification }  = require("@tests/Dialog");
+  const { Dialog, Notification }  = require("@tests/Dialog");
+  const DialogModel = require("@tests/Dialog").__default;
+  const  DialogModel = require("@tests/Dialog").__default;
+
+  return __exports;
+});
