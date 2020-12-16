cbb91363571a9e321815502ef31d13cb4563868d
Victor Feyens <vfe@odoo.com>
2020-12-16 11:38:44 +0100

    fixup

--- addons/lunch/__manifest__.py
+++ addons/lunch/__manifest__.py
@@ -24,7 +24,7 @@ If you want to save your employees' time and avoid them to always have coins in 
     'data': [
         'security/lunch_security.xml',
         'security/ir.model.access.csv',
-        'data/ir_cron.xml'
+        'data/ir_cron.xml',
         'report/lunch_cashmove_report_views.xml',
         'views/lunch_templates.xml',
         'views/lunch_alert_views.xml',
