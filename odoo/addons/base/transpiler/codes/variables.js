17c2700960fb334e4b37ec7bcbde0829e23722c1
Simon Genin (ges) <ges@odoo.com>
2020-12-16 16:05:52 +0100

    wip

---
+++ odoo/addons/base/transpiler/codes/variables.js
@@ -0,0 +1,17 @@
+/**
+ * Not supported:
+ * export const x = 4, y = 12;
+ * export { a as aa, b as bb };
+ */
+
+export const v = 5;
+
+const a = 12;
+const b = 15;
+
+export { a, b };
+
+export default 100;
+
+export default a;
+
---
+++ odoo/addons/base/transpiler/codes/variables.odoo.js
@@ -0,0 +1,16 @@
+odoo.define('@tests/variables', function (require) {
+  let __exports = {};
+
+  const v = __exports.v = 5;
+
+  const a = 12;
+  const b = 15;
+
+  __exports = Object.assign(__exports, { a, b});
+
+  __exports.__default = 100;
+
+  __exports.__default = a;
+
+  return __exports;
+});
