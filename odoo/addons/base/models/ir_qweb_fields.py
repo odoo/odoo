47d91eac31a7551843d2db7f47f398cee188f95e
Carlos Dauden <carlos.dauden@tecnativa.com>
2020-12-17 21:24:56 +0100

    [IMP] base: Compute context_timestamp when use widget date with datetime field to get day correctly

--- odoo/addons/base/models/ir_qweb_fields.py
+++ odoo/addons/base/models/ir_qweb_fields.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import base64
+import datetime
 import re
 from collections import OrderedDict
 from io import BytesIO
@@ -211,6 +212,8 @@ class DateConverter(models.AbstractModel):
 
     @api.model
     def value_to_html(self, value, options):
+        if isinstance(value, datetime.datetime):
+            value = fields.Datetime.context_timestamp(self, value)
         return format_date(self.env, value, date_format=options.get('format'))
 
 
