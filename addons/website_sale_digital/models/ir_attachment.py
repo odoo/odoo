a3e90c4a59eabd3c15ca504dd2d1f3fc61a2bda8
Romain Derie <rde@odoo.com>
2020-12-18 15:46:25 +0000

    [FIX] website_sale_digital: error if no records in check on attachment
    Since f9c00ced9, the code might throw an error if self is an empty recordset,
    as `id IN ()` will crash in psql.
    
    X-original-commit: 711b64d63934034b2e0c5eeb6da32c4162434a6b

--- addons/website_sale_digital/models/ir_attachment.py
+++ addons/website_sale_digital/models/ir_attachment.py
@@ -9,3 +9,16 @@ class Attachment(models.Model):
     _inherit = ['ir.attachment']
 
     product_downloadable = fields.Boolean("Downloadable from product portal", default=False)
+<<<<<<< HEAD
+=======
+
+    @api.model
+    def check(self, mode, values=None):
+        super().check(mode, values=values)
+        if mode == 'read' and self and not self.env.user.has_group('base.group_user'):
+            self._cr.execute('SELECT 1 FROM ir_attachment WHERE product_downloadable AND id IN %s', [tuple(self.ids)])
+            if self._cr.rowcount:
+                raise AccessError(_("Sorry, you are not allowed to access this document."))
+
+        super(Attachment, self).check(mode, values)
+>>>>>>> 7429b9b5f2d... temp
