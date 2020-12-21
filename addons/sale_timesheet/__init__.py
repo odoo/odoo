9d8d861428029db4a3a9fc24571ceb48def91ecb
Ivan Yelizariev <iel@odoo.com>
2020-12-21 11:51:57 +0100

    [FIX] sale_timesheet: restore ir.rule on uninstalling
    STEPS:
    * install sale_timesheet
    * uninstall sale_timesheet
    
    BEFORE: error on reading account.analytic.line:
    
        ValueError: Invalid field account.analytic.line.project_id in
        leaf ('project_id', '=', False)
    
    AFTER: rule domain is restored to original value
    
    ---
    
    opw-2418498

--- addons/sale_timesheet/__init__.py
+++ addons/sale_timesheet/__init__.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, SUPERUSER_ID
+
 from . import controllers
 from . import models
 from . import wizard
 from . import report
+
+
+def uninstall_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    module_domain = "('project_id', '=', False)"
+    original_domain = "(1, '=', 1)"
+    rule = env.ref('account.account_analytic_line_rule_billing_user', raise_if_not_found=False)
+    if rule and rule.domain_force:
+        rule.domain_force = rule.domain_force.replace(module_domain, original_domain)
