5ab1fd1a5fed5e0fc73910060eeb345c75b98925
Julien Mougenot <jum@odoo.com>
2020-12-22 16:59:46 +0000

    [FIX] web,mail: Call on_attach_callback when appending content
    Before this commit, the form renderer would call "on_attach_callback"
    only when the whole _render process was over. The problem was that this
    process is asynchronous and this callback should be invoked as soon as
    the widgets are appended.
    
    As such, any asynchronous operation is performed before appending the
    content in a new function "__renderView".
    
    Task 2346540
    
    closes odoo/odoo#65991
    
    X-original-commit: 953fb092636b9919aaa6c6a6cfaffd003ba951d5
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/js/views/basic/basic_renderer.js
+++ addons/web/static/src/js/views/basic/basic_renderer.js
@@ -684,7 +684,7 @@ var BasicRenderer = AbstractRenderer.extend(WidgetAdapterMixin, {
         if (this._isInDom) {
             for (const handle in this.allFieldWidgets) {
                 this.allFieldWidgets[handle].forEach(widget => {
-                    if (widget.on_attach_callback) {
+                    if (!utils.isComponent(widget.constructor) && widget.on_attach_callback) {
                         widget.on_attach_callback();
                     }
                 });
@@ -694,6 +694,8 @@ var BasicRenderer = AbstractRenderer.extend(WidgetAdapterMixin, {
                     widget.on_attach_callback();
                 }
             });
+            // call on_attach_callback on child components (including field components)
+            WidgetAdapterMixin.on_attach_callback.call(this);
         }
     },
     /**
