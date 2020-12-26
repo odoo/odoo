a555942e025727321531f0bdf7beb5dec15706d0
Swapnesh Shah <swapneshshah111@gmail.com>
2020-12-26 15:18:13 +0530

    [FIX] sale_stock: log exception activity only once
    Steps to reproduce the bug:
    
    - Create SO --> Process Picking
    - Cancel SO
    
    Bug:
    
    There are dupliciate activities `exception_on_so` added on Picking.
    
    
    With this Commit:
    
    Log activity only once on Cancelling Sale order.
    
    
    Fixes: #63796

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -213,7 +213,7 @@ class SaleOrder(models.Model):
                 sale_order_lines_quantities = {order_line: (order_line.product_uom_qty, 0) for order_line in sale_order.order_line}
                 documents = self.env['stock.picking']._log_activity_get_documents(sale_order_lines_quantities, 'move_ids', 'UP')
         self.picking_ids.filtered(lambda p: p.state != 'done').action_cancel()
-        if documents:
+        if documents and self._context.get('disable_cancel_warning'):
             filtered_documents = {}
             for (parent, responsible), rendering_context in documents.items():
                 if parent._name == 'stock.picking':
