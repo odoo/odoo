2db5b00750c24f70126877aec74ca95c061e6e3c
Nicolas Galler <ngl@odoo.com>
2020-12-31 11:30:43 +0100

    [FIX] stock: run scheduler actions as super user
    In `run_scheduler`, we are running certain functions as super user to
    avoid inter company and access rights issues.  This commit adds a
    `sudo()` call to the `action_assign()` call, in case some objects
    related to the retrieved moves are not accessible.
    
    In addition a commit call is moved up to apply after the
    `procure_orderpoint_confirm` call (as it was, it was applied after the
    `_action_assign` loop, but there is in fact already a commit in that
    loop, this lead to a somewhat random behavior where the replenish rules
    were sometimes rolled back and sometimes not, depending on whether the
    first iteration of the loop errored out or not)
    
    opw-2394706
    
    X-original-commit: 7a2c23c738daa23ab469f6a780bb7df69a25bec0

--- addons/stock/models/stock_rule.py
+++ addons/stock/models/stock_rule.py
@@ -400,18 +400,17 @@ class ProcurementGroup(models.Model):
     def _run_scheduler_tasks(self, use_new_cursor=False, company_id=False):
         # Minimum stock rules
         self.sudo()._procure_orderpoint_confirm(use_new_cursor=use_new_cursor, company_id=company_id)
+        if use_new_cursor:
+            self._cr.commit()
 
         # Search all confirmed stock_moves and try to assign them
         domain = self._get_moves_to_assign_domain(company_id)
         moves_to_assign = self.env['stock.move'].search(domain, limit=None,
             order='priority desc, date_expected asc')
         for moves_chunk in split_every(100, moves_to_assign.ids):
-            self.env['stock.move'].browse(moves_chunk)._action_assign()
+            self.env['stock.move'].browse(moves_chunk).sudo()._action_assign()
             if use_new_cursor:
                 self._cr.commit()
-
-        if use_new_cursor:
-            self._cr.commit()
 
         # Merge duplicated quants
         self.env['stock.quant']._quant_tasks()
