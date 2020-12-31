004fd334ff07cdbc71362dd114acf02adf561bd9
Goffin Simon <sig@odoo.com>
2020-12-31 12:53:32 +0000

    [FIX] point_of_sale, pos_sale: Field crm_team_id not defined
    Fine tuning of https://github.com/odoo/odoo/commit/61e5389236ad767827ef2fe039aa78b20848298e
    
    opw:2418806
    
    closes odoo/odoo#63956
    
    X-original-commit: 277c24599c8a9d6e9af2075d16c505c200643163
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- addons/pos_sale/models/pos_order.py
+++ addons/pos_sale/models/pos_order.py
@@ -22,7 +22,7 @@ class PosOrder(models.Model):
             date_order = order.date_order or fields.Datetime.now()
             order.currency_rate = self.env['res.currency']._get_conversion_rate(order.company_id.currency_id, order.pricelist_id.currency_id, order.company_id, date_order)
 
-    def _prepare_invoice(self):
-        invoice_vals = super(PosOrder, self)._prepare_invoice()
+    def _prepare_invoice_vals(self):
+        invoice_vals = super(PosOrder, self)._prepare_invoice_vals()
         invoice_vals['team_id'] = self.crm_team_id
         return invoice_vals
