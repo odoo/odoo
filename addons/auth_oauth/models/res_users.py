1cca2de3d14193c7ab29d2ad18d57665125c186f
Temur <tvibliani@users.noreply.github.com>
2021-01-04 06:35:11 +0400

    Update Oauth 2.0 Access Token placement to the "Bearer" option
    - Limit support to the "Bearer" placement option for the Oauth 2.0
      Access Tokens in the relevant endpoint calls from `auth_oauth` module.
    - Do NOT change DB layout (removed "access_token_location" field,
      which was introduced by the previous commit).

--- addons/auth_oauth/models/res_users.py
+++ addons/auth_oauth/models/res_users.py
@@ -24,22 +24,18 @@ class ResUsers(models.Model):
     ]
 
     @api.model
-    def _auth_oauth_rpc(self, endpoint, access_token, token_location):
-        if token_location == "bearer":
-            return requests.get(endpoint, headers={'Authorization': 'Bearer %s' % access_token}).json()
-        if token_location == "oauth":
-            return requests.get(endpoint, headers={'Authorization': 'OAuth %s' % access_token}).json()
-        return requests.get(endpoint, params={'access_token': access_token}).json()
+    def _auth_oauth_rpc(self, endpoint, access_token):
+        return requests.get(endpoint, headers={'Authorization': 'Bearer %s' % access_token}).json()
 
     @api.model
     def _auth_oauth_validate(self, provider, access_token):
         """ return the validation data corresponding to the access token """
         oauth_provider = self.env['auth.oauth.provider'].browse(provider)
-        validation = self._auth_oauth_rpc(oauth_provider.validation_endpoint, access_token, oauth_provider.access_token_location)
+        validation = self._auth_oauth_rpc(oauth_provider.validation_endpoint, access_token)
         if validation.get("error"):
             raise Exception(validation['error'])
         if oauth_provider.data_endpoint:
-            data = self._auth_oauth_rpc(oauth_provider.data_endpoint, access_token, oauth_provider.access_token_location)
+            data = self._auth_oauth_rpc(oauth_provider.data_endpoint, access_token)
             validation.update(data)
         return validation
 
