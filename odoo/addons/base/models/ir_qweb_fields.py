e5246902afc30cab373451c14b42dd37124f72d1
Nasreddin (bon) <bon@odoo.com>
2021-01-06 17:17:24 +0100

    [FIX] misc: Babel; handle 'kur' (kurdish) locale
    Issue
    
    	- add a new language with locale code KUR (for Kurdish)
    	- print any report with a datetime on it (RFQ for example)
    
    Cause
    
    	Babel (version < 2.7.0) does not handle locale "KUR".
    
    Solution
    
    	If wrong locale or not managed by Babel, try to fallback
    	on server default locale.
    	If still wrong locale or not managed, then fallback on "en_US" as locale.
    
    opw-2416482

--- odoo/addons/base/models/ir_qweb_fields.py
+++ odoo/addons/base/models/ir_qweb_fields.py
@@ -10,6 +10,7 @@ from lxml import etree
 import math
 
 from odoo.tools import html_escape as escape, posix_to_ldml, safe_eval, float_utils, format_date, pycompat
+from odoo.tools.misc import babel_locale_parse
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -232,7 +233,7 @@ class DateTimeConverter(models.AbstractModel):
         if not value:
             return ''
         lang = self.user_lang()
-        locale = babel.Locale.parse(lang.code)
+        locale = babel_locale_parse(lang.code)
 
         if isinstance(value, pycompat.string_types):
             value = fields.Datetime.from_string(value)
@@ -521,7 +522,7 @@ class DurationConverter(models.AbstractModel):
     def value_to_html(self, value, options):
         units = dict(TIMEDELTA_UNITS)
 
-        locale = babel.Locale.parse(self.user_lang().code)
+        locale = babel_locale_parse(self.user_lang().code)
         factor = units[options.get('unit', 'second')]
         round_to = units[options.get('round', 'second')]
 
@@ -574,7 +575,7 @@ class RelativeDatetimeConverter(models.AbstractModel):
 
     @api.model
     def value_to_html(self, value, options):
-        locale = babel.Locale.parse(self.user_lang().code)
+        locale = babel_locale_parse(self.user_lang().code)
 
         if isinstance(value, pycompat.string_types):
             value = fields.Datetime.from_string(value)
