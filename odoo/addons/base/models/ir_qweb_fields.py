49390b7a80bdf2f1a41f92a00b4b7bd7a482f41d
Nasreddin (bon) <bon@odoo.com>
2021-01-06 16:17:24 +0000

    [FIX] misc: Babel; handle 'kur' (kurdish) locale
    Issue
    
    	- add a new language with locale code KUR (for Kurdish)
    	- print any report with a datetime on it (RFQ for example)
    
    Cause
    
    	Babel (version < 2.7.0) does not handle locale "KUR".
    
    Solution
    
    	If wrong locale or not managed by Babel, try to fallback
    	on server default locale.
    	If still wrong locale or not managed, then fallback on "en_US" as locale.
    
    opw-2416482
    
    X-original-commit: e6ccdb397792db62c3b08d96435b3c1667c1aa9d

--- odoo/addons/base/models/ir_qweb_fields.py
+++ odoo/addons/base/models/ir_qweb_fields.py
@@ -11,7 +11,7 @@ from lxml import etree
 import math
 
 from odoo.tools import html_escape as escape, posix_to_ldml, float_utils, format_date, format_duration, pycompat
-from odoo.tools.misc import get_lang
+from odoo.tools.misc import get_lang, babel_locale_parse
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -236,7 +236,7 @@ class DateTimeConverter(models.AbstractModel):
         options = options or {}
 
         lang = self.user_lang()
-        locale = babel.Locale.parse(lang.code)
+        locale = babel_locale_parse(lang.code)
         format_func = babel.dates.format_datetime
         if isinstance(value, str):
             value = fields.Datetime.from_string(value)
@@ -559,7 +559,7 @@ class DurationConverter(models.AbstractModel):
     def value_to_html(self, value, options):
         units = {unit: duration for unit, label, duration in TIMEDELTA_UNITS}
 
-        locale = babel.Locale.parse(self.user_lang().code)
+        locale = babel_locale_parse(self.user_lang().code)
         factor = units[options.get('unit', 'second')]
         round_to = units[options.get('round', 'second')]
 
@@ -617,7 +617,7 @@ class RelativeDatetimeConverter(models.AbstractModel):
 
     @api.model
     def value_to_html(self, value, options):
-        locale = babel.Locale.parse(self.user_lang().code)
+        locale = babel_locale_parse(self.user_lang().code)
 
         if isinstance(value, str):
             value = fields.Datetime.from_string(value)
