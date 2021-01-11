3af11f0b6db0efb6c912b25bcd04d2cbe9a07d57
Jeremy Kersten <jke@odoo.com>
2021-01-11 19:27:02 +0000

    [FIX] test_website: add tests for website.rewrite
    Basic tests & tests of the 3 fixes of #64328
    
    Be sure that we have a beautiful error page and not a black/white error
    Be sure that slug_matching with 308 redirect works as expected and don't
    raise a RequestUID exception.
    
    closes odoo/odoo#64889
    
    Co-authored-by: "Romain Derie <rde@odoo.com>"
    Co-authored-by: "Jeremy Kersten <jke@odoo.com>"

--- addons/test_website/tests/__init__.py
+++ addons/test_website/tests/__init__.py
@@ -7,6 +7,7 @@ from . import test_error
 from . import test_is_multilang
 from . import test_multi_company
 from . import test_performance
+from . import test_redirect
 from . import test_reset_views
 from . import test_session
 from . import test_views_during_module_operation
