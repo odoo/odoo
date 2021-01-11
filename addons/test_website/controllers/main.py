72444fb1f8ce032da6019e1be290854701da7413
Jeremy Kersten <jke@odoo.com>
2021-01-11 19:27:02 +0000

    [FIX] test_website: add tests for website.rewrite
    Basic tests & tests of the 3 fixes of #64328
    
    Be sure that we have a beautiful error page and not a black/white error
    Be sure that slug_matching with 308 redirect works as expected and don't
    raise a RequestUID exception.
    
    closes odoo/odoo#64889
    
    X-original-commit: 3af11f0b6db0efb6c912b25bcd04d2cbe9a07d57
    Co-authored-by: "Romain Derie <rde@odoo.com>"
    Co-authored-by: "Jeremy Kersten <jke@odoo.com>"

--- addons/test_website/controllers/main.py
+++ addons/test_website/controllers/main.py
@@ -125,3 +125,12 @@ class WebsiteTest(Home):
     @http.route(['/empty_controller_test'], type='http', auth='public', website=True, multilang=False, sitemap=False)
     def empty_controller_test(self, **kw):
         return 'Basic Controller Content'
+
+    # Test Redirects
+    @http.route(['/test_website/country/<model("res.country"):country>'], type='http', auth="public", website=True, sitemap=False)
+    def test_model_converter_country(self, country, **kw):
+        return request.render('test_website.test_redirect_view', {'country': country})
+
+    @http.route(['/test_website/200/<model("test.model"):rec>'], type='http', auth="public", website=True, sitemap=False)
+    def test_model_converter_seoname(self, rec, **kw):
+        return request.make_response('ok')
