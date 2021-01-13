0cedf13f280a2bfd39b826485a12d8e0066d0825
nie <nie@odoo.com>
2021-01-13 10:00:43 +0000

    [FIX] mrp: ensure serial numbers are used once
    Steps:
    - Install mrp_workorder
    - Go to MRP > Master Data > Products
    - Create a Product (1)
      - Inventory
        - Route: Manufacture
        - Tracking: By Unique Serial Number
    - Create a Bill of Material for (1)
      - Routing:
        - Create a new routing with two operations:
          - Start Next Operation: Once some products are processed
          - Duration Computation: Set duration manually
      - Add a new component
        - Product Type: Storable Product
    - Go to Operations > Manufacturing Orders
    - Create a new one:
      - Product: (1)
      - Quantity to Produce: 5
    - Click Mark as ToDo > Plan > Work Orders
    - Click the first work order
    - Process it
    - Open the web console
    - Enter this: `odoo.__DEBUG__.services['web.core'].bus.trigger('barcode_scanned', "BBBAR", $(".o_web_client")[0]);`
    - Click Record Production
    - Repeat the two last points
    
    Bug:
    Using the same serial number is authorized.
    
    Explanation:
    We have to use two operations because, if it's the last one,
    `_update_finished_move()` ensures all the products have a unique serial
    number as seen here:
    https://github.com/odoo/odoo/blob/a5ac0fad7d1e288ac1995369a9a64c2e54125c27/addons/mrp/models/mrp_abstract_workorder.py#L259-L264
    
    When trying to add the second serial number manually, we get the
    following error upon creation: "The combination of serial number and
    product must be unique across a company !". However, this error does not
    prevent the user from clicking Record Production and adding a duplicate.
    The same applies to scanned serial numbers. They bypass the creation
    check and directly enter the code in the field.
    
    This commit prevents clicking Record Production if the serial number has
    already been used in the manufacturing order. This way, both manual and
    scanned serial numbers are taken care of. Adding empty serial numbers
    is authorized as stated in this test:
    https://github.com/odoo/odoo/blob/fe1c381b67ff78d48bb50fd731717324cf03a719/addons/mrp/tests/test_workorder_operation.py#L765-L771
    
    opw:2366412
    
    X-original-commit: a3e8ff615690f3d4fcd362cd83c5403be259c092

--- addons/mrp/models/mrp_workorder.py
+++ addons/mrp/models/mrp_workorder.py
@@ -456,11 +456,75 @@ class MrpWorkorder(models.Model):
                         'workorder_id': workorders_by_bom[production.bom_id][-1:].id
                     })
 
+<<<<<<< HEAD
             for workorders in workorders_by_bom.values():
                 if workorders[0].state == 'pending':
                     workorders[0].state = 'ready'
                 for workorder in workorders:
                     workorder._start_nextworkorder()
+=======
+        self.ensure_one()
+        self._check_company()
+        if float_compare(self.qty_producing, 0, precision_rounding=self.product_uom_id.rounding) <= 0:
+            raise UserError(_('Please set the quantity you are currently producing. It should be different from zero.'))
+
+        # Ensure serial numbers are used once
+        if self.product_id.tracking == 'serial' and self.finished_lot_id:
+            line = self.finished_workorder_line_ids.filtered(
+                lambda line: line.lot_id.id == self.finished_lot_id.id
+            )
+            if line:
+                raise UserError(_('You cannot produce the same serial number twice.'))
+
+        # If last work order, then post lots used
+        if not self.next_work_order_id:
+            self._update_finished_move()
+
+        # Transfer quantities from temporary to final move line or make them final
+        self._update_moves()
+
+        # Transfer lot (if present) and quantity produced to a finished workorder line
+        if self.product_tracking != 'none':
+            self._create_or_update_finished_line()
+
+        # Update workorder quantity produced
+        self.qty_produced += self.qty_producing
+
+        # Suggest a finished lot on the next workorder
+        if self.next_work_order_id and self.product_tracking != 'none' and (not self.next_work_order_id.finished_lot_id or self.next_work_order_id.finished_lot_id == self.finished_lot_id):
+            self.next_work_order_id._defaults_from_finished_workorder_line(self.finished_workorder_line_ids)
+            # As we may have changed the quantity to produce on the next workorder,
+            # make sure to update its wokorder lines
+            self.next_work_order_id._apply_update_workorder_lines()
+
+        # One a piece is produced, you can launch the next work order
+        self._start_nextworkorder()
+
+        # Test if the production is done
+        rounding = self.production_id.product_uom_id.rounding
+        production_qty = self._get_real_uom_qty(self.qty_production)
+        if float_compare(self.qty_produced, production_qty, precision_rounding=rounding) < 0:
+            previous_wo = self.env['mrp.workorder']
+            if self.product_tracking != 'none':
+                previous_wo = self.env['mrp.workorder'].search([
+                    ('next_work_order_id', '=', self.id)
+                ])
+            candidate_found_in_previous_wo = False
+            if previous_wo:
+                candidate_found_in_previous_wo = self._defaults_from_finished_workorder_line(previous_wo.finished_workorder_line_ids)
+            if not candidate_found_in_previous_wo:
+                # self is the first workorder
+                self.qty_producing = self.qty_remaining
+                self.finished_lot_id = False
+                if self.product_tracking == 'serial':
+                    self.qty_producing = 1
+
+            self._apply_update_workorder_lines()
+        else:
+            self.qty_producing = 0
+            self.button_finish()
+        return True
+>>>>>>> 25bc25f42df... temp
 
     def _get_byproduct_move_to_update(self):
         return self.production_id.move_finished_ids.filtered(lambda x: (x.product_id.id != self.production_id.product_id.id) and (x.state not in ('done', 'cancel')))
