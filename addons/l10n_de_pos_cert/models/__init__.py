ae1c6ea37179f4a018ecabb36f8c4411dd7b0d4f
Jacky (trj) <trj@odoo.com>
2021-01-14 16:26:07 +0100

    Remove module from the community version

--- addons/l10n_de_pos_cert/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import pos_config
-from . import pos_download_dsfinvk_export
-from . import pos_dsfinvk_export
-from . import pos_order
-from . import pos_session
-from . import res_company
