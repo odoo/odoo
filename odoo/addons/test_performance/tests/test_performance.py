907be2788aebf7386efb9cb4875e537c3505bbb6
Raphael Collet <rco@odoo.com>
2021-01-15 12:22:20 +0000

    [IMP] core: avoid flushing fields to read
    The idea is to avoid flushing the fields to fetch in method _read().
    This delays UPDATE queries, and makes the prefetching mechanism simpler
    and more effective.
    
    We do this by not overwriting the cache values by the values fetched
    from database.  This simple idea allows to fetch more fields and more
    records without having to care about pending computations and updates.
    But it requires the cache consistency to be much more strict, because
    nothing will "fix" the cache inconsistencies "by chance".  And it also
    requires pending updates to be present in cache.
    
    Part-of: odoo/odoo#66938

--- odoo/addons/test_performance/tests/test_performance.py
+++ odoo/addons/test_performance/tests/test_performance.py
@@ -455,17 +455,12 @@ class TestPerformance(SavepointCaseWithUserDemo):
         with self.assertQueries([], flush=False):
             records[1].value = 42
 
-        # fetching 'name' prefetches all fields except 'value_pc' on all records
-        # (because 'value_pc' must be computed); reading those fields causes
-        # field 'value' to be flushed first
+        # fetching 'name' prefetches all fields on all records
         queries = [
-            ''' UPDATE "test_performance_base"
-                SET "value"=%s, "write_date"=%s, "write_uid"=%s
-                WHERE id IN %s
-            ''',
             ''' SELECT "test_performance_base"."id" AS "id",
                        "test_performance_base"."name" AS "name",
                        "test_performance_base"."value" AS "value",
+                       "test_performance_base"."value_pc" AS "value_pc",
                        "test_performance_base"."partner_id" AS "partner_id",
                        "test_performance_base"."total" AS "total",
                        "test_performance_base"."create_uid" AS "create_uid",
@@ -482,24 +477,7 @@ class TestPerformance(SavepointCaseWithUserDemo):
         with self.assertQueries([], flush=False):
             result_value = [record.value for record in records]
 
-        # fetching 'value_pc' (missing from above) prefetches all fields on all
-        # records except the one to compute
-        queries = [
-            ''' SELECT "test_performance_base"."id" AS "id",
-                       "test_performance_base"."name" AS "name",
-                       "test_performance_base"."value" AS "value",
-                       "test_performance_base"."partner_id" AS "partner_id",
-                       "test_performance_base"."total" AS "total",
-                       "test_performance_base"."create_uid" AS "create_uid",
-                       "test_performance_base"."create_date" AS "create_date",
-                       "test_performance_base"."write_uid" AS "write_uid",
-                       "test_performance_base"."write_date" AS "write_date",
-                       "test_performance_base"."value_pc" AS "value_pc"
-                FROM "test_performance_base"
-                WHERE "test_performance_base".id IN %s
-            ''',
-        ]
-        with self.assertQueries(queries, flush=False):
+        with self.assertQueries([], flush=False):
             result_value_pc = [record.value_pc for record in records]
 
         result = list(zip(result_name, result_value, result_value_pc))
