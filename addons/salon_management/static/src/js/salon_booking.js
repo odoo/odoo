b45b1a6b66c95f95bbe5b10aea200d17e3e41a0b
yinmazatin <yinmazatin21@gmail.com>
2021-01-18 17:20:45 +0700

    customized the color

--- addons/salon_management/static/src/js/salon_booking.js
+++ addons/salon_management/static/src/js/salon_booking.js
@@ -62,13 +62,13 @@ odoo.define('salon_management.website_salon_booking_system', function(require) {
                                 '</span></td><td><span>' + order_details[x]['orders'][i]['start_time_only'] +
                                 '</span></td><td><span>' + order_details[x]['orders'][i]['end_time_only'] + '</span></td></tr>'
                         }
-                        order += '<div class="col-lg-4 s_title pt16 pb16"><div style="height: 200px!important; text-align: center;' +
+                        order += '<div class="col-lg-4 s_title pt16 pb16 row"><div style="height: 200px!important; text-align: center;' +
                             'border: 1px solid #666;padding: 15px 0px;box-shadow: 7px 8px 5px #888888;background-color:#389D38;border-radius:10px;color:#fff;margin-bottom: 10px;">' +
                             '<span style="font-size: 15px;">' + chair_name + '</span>' +
                             '<br/><a style="color:#fff;font-size:15px;">Booked Details</a>' +
                             '<div id="style-2" style="overflow-y:scroll;height:105px;padding-right:25px;padding-left:25px;margin-right:10px;">' +
-                            '<table class="table"><th style="font-size:11px;">Booked No.</th><th style="font-size:11px;">Start Time</th>' +
-                            '<th style="font-size:11px;">End Time</th><div><tbody style="font-size: 10px;">' +
+                            '<table class="table"><th style="font-size:11px;color:#fff;">Booked No.</th><th style="font-size:11px;color:#fff;">Start Time</th>' +
+                            '<th style="font-size:12px;color:#fff;">End Time</th><div><tbody style="font-size:12px;color:#fff;">' +
                             lines + '</tbody></div></table></div></div></div>'
 
                     }
