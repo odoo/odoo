8c5bfd351ad254c62e31dbe3b8dd9333171bc5d9
Kevin Baptiste <kba@odoo.com>
2021-01-21 13:05:21 +0000

    [IMP] hr_attendance: show own attendances
    Users with the rights 'Manual Attendance' get (read-only) access to their
    own attendances.
    
    closes odoo/odoo#66934
    
    Taskid: 2438863
    Related: odoo/upgrade#2216
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ addons/hr_attendance/models/ir_ui_menu.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models
+
+
+class IrUiMenu(models.Model):
+    _inherit = 'ir.ui.menu'
+
+    def _load_menus_blacklist(self):
+        res = super()._load_menus_blacklist()
+        if self.env.user.has_group('hr_attendance.group_hr_attendance_user'):
+            res.append(self.env.ref('hr_attendance.menu_hr_attendance_attendances_overview').id)
+        return res
