a406894e3b42ee1eb058e335cef32249bb48bdc3
Arnold Moyaux <arm@odoo.com>
2021-01-26 11:13:22 +0000

    [FIX] stock: couldn't unreserve mixed tracking stock
    - Install stock
    - Go to Inventory > Configuration > Settings and enable "Lots" and "Storage Locations"
    - Create a Product tracked By Lots (i.e. Product X)
    - Go to Inventory > Operations > Inventory Adjustments
    - Create an Inventory Adjustment for Product X:
    
         Product   |   Location   |   Lot/SN   |   Real Quantity
     -------------------------------------------------------------
        Product X  |   WH/Stock   |   LOT 01   |       20
        Product X  |   WH/Stock   |            |       10
    
    - Validate Inventory
    - Go to Inventory > Operations > Transfers and create one:
      * Source Location: WH/Stock
      * Destination Location: WH/Stock/Shelf1
      * Operation Type: Internal Transfers
      * Operations:
        [Product: Product X, Initial Demand: 25]
    - Save Transfer, Mark As Todo and Check availability
    - Click on list icon of Operation line for Product X to display Detailed Operations
    - 20 units of LOT 01 and 5 units without lot have been reserved
    - Set LOT 01 for the 5 reserved units without lot and confirm
    - Open Detailed Operations again
    - There are now 20 units of LOT 01 and 5 units of LOT 01
    - Remove the row with 5 units and confirm
    - Check availability and open Detailed Operation
    - There is now only a row with 25 reserved units of LOT 01
    - Unreserve
    The following errror is raised:
    "It is not possible to unreserve more products of P than you have in stock."
    
    It happens because the system is not able to manage quants with lots and
    wihtout lots at the same time. When modifying the move line to 25
    reserved units. It's composed of 20 quants with lot and 5 quants without
    lot. And when unreserving it will check if there is a quants with 25
    units with the lot and if it's not found 25 units without lot. But never
    25 units of quants with lots and without lots.
    
    opw-2419444
    
    Close #64497
    
    closes odoo/odoo#66029
    
    X-original-commit: 217c76b11e6c2bb5bf861284343c87b4ca241da1
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -452,14 +452,14 @@ class StockQuant(models.Model):
         domain = [('product_id', '=', product_id.id)]
         if not strict:
             if lot_id:
-                domain = expression.AND([[('lot_id', '=', lot_id.id)], domain])
+                domain = expression.AND([['|', ('lot_id', '=', lot_id.id), ('lot_id', '=', False)], domain])
             if package_id:
                 domain = expression.AND([[('package_id', '=', package_id.id)], domain])
             if owner_id:
                 domain = expression.AND([[('owner_id', '=', owner_id.id)], domain])
             domain = expression.AND([[('location_id', 'child_of', location_id.id)], domain])
         else:
-            domain = expression.AND([[('lot_id', '=', lot_id and lot_id.id or False)], domain])
+            domain = expression.AND([['|', ('lot_id', '=', lot_id.id), ('lot_id', '=', False)] if lot_id else [('lot_id', '=', False)], domain])
             domain = expression.AND([[('package_id', '=', package_id and package_id.id or False)], domain])
             domain = expression.AND([[('owner_id', '=', owner_id and owner_id.id or False)], domain])
             domain = expression.AND([[('location_id', '=', location_id.id)], domain])
@@ -611,6 +611,8 @@ class StockQuant(models.Model):
         """
         self = self.sudo()
         quants = self._gather(product_id, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=True)
+        if lot_id and quantity > 0:
+            quants = quants.filtered(lambda q: q.lot_id)
 
         incoming_dates = [d for d in quants.mapped('in_date') if d]
         if in_date:
@@ -667,7 +669,7 @@ class StockQuant(models.Model):
 
         if float_compare(quantity, 0, precision_rounding=rounding) > 0:
             # if we want to reserve
-            available_quantity = self._get_available_quantity(product_id, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
+            available_quantity = sum(quants.filtered(lambda q: float_compare(q.quantity, 0, precision_rounding=rounding) > 0).mapped('quantity')) - sum(quants.mapped('reserved_quantity'))
             if float_compare(quantity, available_quantity, precision_rounding=rounding) > 0:
                 raise UserError(_('It is not possible to reserve more products of %s than you have in stock.', product_id.display_name))
         elif float_compare(quantity, 0, precision_rounding=rounding) < 0:
