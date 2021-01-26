4b4d5545bad3d683184bdca484b9e5eb799e3f36
Goffin Simon <sig@odoo.com>
2021-01-26 14:25:45 +0000

    [FIX] hr_holidays: Leaves in Multi companies
    Steps to reproduce the bug:
    
    - Let's consider 3 companies C1, C2, C3 where C1 and C2 are childs of C3
    - Let's consider 3 employees E1, E2, E3 respectively in C1, C2, C3
    - Create a user U with administrator access rights in Leaves and U is linked to E3
    - Log as U in C3
    - Create a Leave Type LT with no company
    - Create a leave LT  LT1 for E1 and LT2 for E2
    - Change your current company to C2
    - Click on LT1
    
    Bug:
    
    An access right error was raised
    
    opw:2431572
    
    X-original-commit: 6f587ab563a98becf6a8a38beaa1615b8834a7e9

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -428,14 +428,13 @@ class HolidaysRequest(models.Model):
 
     def _get_calendar(self):
         self.ensure_one()
-        return self.employee_id.resource_calendar_id or self.env.user.company_id.resource_calendar_id
+        return self.employee_id.sudo().resource_calendar_id or self.env.user.company_id.resource_calendar_id
 
     @api.multi
     @api.depends('number_of_days')
     def _compute_number_of_hours_display(self):
         for holiday in self:
             calendar = holiday._get_calendar()
-            if holiday.date_from and holiday.date_to:
                 number_of_hours = calendar.get_work_hours_count(holiday.date_from, holiday.date_to)
                 holiday.number_of_hours_display = number_of_hours or (holiday.number_of_days * HOURS_PER_DAY)
             else:
@@ -556,7 +555,7 @@ class HolidaysRequest(models.Model):
                 elif leave.holiday_type == 'category':
                     target = leave.category_id.name
                 else:
-                    target = leave.employee_id.name
+                    target = leave.employee_id.sudo().name
                 if leave.leave_type_request_unit == 'hour':
                     res.append(
                         (leave.id,
