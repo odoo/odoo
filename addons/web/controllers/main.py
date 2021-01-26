e7ee615b32c64550d762ec31ac985998d620019f
Xavier Morel <xmo@odoo.com>
2021-01-26 09:05:35 +0100

    [IMP] http: avoid cycle on request

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -108,9 +108,8 @@ def serialize_exception(f):
     return wrap
 
 def abort_and_redirect(url):
-    r = request.httprequest
     response = request.redirect(url, 302)
-    response = r.app.get_response(r, response, explicit_session=False)
+    response = http.root.get_response(request.httprequest, response, explicit_session=False)
     werkzeug.exceptions.abort(response)
 
 def ensure_db(redirect='/web/database/selector'):
@@ -1032,7 +1031,7 @@ class Proxy(http.Controller):
             from werkzeug.wrappers import BaseResponse
             base_url = request.httprequest.base_url
             query_string = request.httprequest.query_string
-            client = Client(request.httprequest.app, BaseResponse)
+            client = Client(http.root, BaseResponse)
             headers = {'X-Openerp-Session-Id': request.session.sid}
             return client.post('/' + path, base_url=base_url, query_string=query_string,
                                headers=headers, data=data)
