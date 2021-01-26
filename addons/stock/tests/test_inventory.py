d5b03c6330ffcd6f4f537d074044d14dd4aa21c7
Arnold Moyaux <arm@odoo.com>
2021-01-26 11:13:22 +0000

    [FIX] stock: couldn't unreserve mixed tracking stock
    - Install stock
    - Go to Inventory > Configuration > Settings and enable "Lots" and "Storage Locations"
    - Create a Product tracked By Lots (i.e. Product X)
    - Go to Inventory > Operations > Inventory Adjustments
    - Create an Inventory Adjustment for Product X:
    
         Product   |   Location   |   Lot/SN   |   Real Quantity
     -------------------------------------------------------------
        Product X  |   WH/Stock   |   LOT 01   |       20
        Product X  |   WH/Stock   |            |       10
    
    - Validate Inventory
    - Go to Inventory > Operations > Transfers and create one:
      * Source Location: WH/Stock
      * Destination Location: WH/Stock/Shelf1
      * Operation Type: Internal Transfers
      * Operations:
        [Product: Product X, Initial Demand: 25]
    - Save Transfer, Mark As Todo and Check availability
    - Click on list icon of Operation line for Product X to display Detailed Operations
    - 20 units of LOT 01 and 5 units without lot have been reserved
    - Set LOT 01 for the 5 reserved units without lot and confirm
    - Open Detailed Operations again
    - There are now 20 units of LOT 01 and 5 units of LOT 01
    - Remove the row with 5 units and confirm
    - Check availability and open Detailed Operation
    - There is now only a row with 25 reserved units of LOT 01
    - Unreserve
    The following errror is raised:
    "It is not possible to unreserve more products of P than you have in stock."
    
    It happens because the system is not able to manage quants with lots and
    wihtout lots at the same time. When modifying the move line to 25
    reserved units. It's composed of 20 quants with lot and 5 quants without
    lot. And when unreserving it will check if there is a quants with 25
    units with the lot and if it's not found 25 units without lot. But never
    25 units of quants with lots and without lots.
    
    opw-2419444
    
    Close #64497
    
    closes odoo/odoo#66029
    
    closes odoo/odoo#93141
    
    X-original-commit: 83d55d8ed8b8b5a5232e3cdfc09abf5285f18ed7
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/stock/tests/test_inventory.py
+++ addons/stock/tests/test_inventory.py
@@ -155,10 +155,10 @@ class TestInventory(TransactionCase):
         stock_confirmation_wizard.action_confirm()
 
         # check
-        self.assertEqual(self.env['stock.quant']._get_available_quantity(self.product2, self.stock_location, lot_id=lot1, strict=True), 1.0)
+        self.assertEqual(self.env['stock.quant']._get_available_quantity(self.product2, self.stock_location, lot_id=lot1, strict=True), 11.0)
         self.assertEqual(self.env['stock.quant']._get_available_quantity(self.product2, self.stock_location, strict=True), 10.0)
         self.assertEqual(self.env['stock.quant']._get_available_quantity(self.product2, self.stock_location), 11.0)
-        self.assertEqual(len(self.env['stock.quant']._gather(self.product2, self.stock_location, lot_id=lot1, strict=True)), 1.0)
+        self.assertEqual(len(self.env['stock.quant']._gather(self.product2, self.stock_location, lot_id=lot1, strict=True).filtered(lambda q: q.lot_id)), 1.0)
         self.assertEqual(len(self.env['stock.quant']._gather(self.product2, self.stock_location, strict=True)), 1.0)
         self.assertEqual(len(self.env['stock.quant']._gather(self.product2, self.stock_location)), 2.0)
 
