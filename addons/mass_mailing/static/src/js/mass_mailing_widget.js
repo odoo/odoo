0b9cefa252428353330e5910f8a9540c303a3841
Nicolas Lempereur <nle@odoo.com>
2021-01-27 16:36:06 +0100

    [FIX] mass_mailing, web_editor: send mail to outlook and keep image size
    Office Outlook doesn't take into account the [style] attribute, and it
    doesn't manage width/height in percent value.
    
    This commit set width/heigth attribute in pixels and also always set the
    width/height in style attribute (keeping them in the original unit and
    setting them to auto if not present).
    
    This is a reintroduction of 2015 commit bdf58adb465b
    
    opw-2447756
    
    X-original-commit: c51762a790d6fefd97e6c818bfaa9307180dd5b5

--- addons/mass_mailing/static/src/js/mass_mailing_widget.js
+++ addons/mass_mailing/static/src/js/mass_mailing_widget.js
@@ -73,6 +73,15 @@ var MassMailingFieldHtml = FieldHtml.extend({
                 convertInline.fontToImg($editable);
                 convertInline.classToStyle($editable);
 
+                // fix outlook image rendering bug
+                _.each(['width', 'height'], function(attribute) {
+                    $editable.find('img[style*="width"], img[style*="height"]').attr(attribute, function(){
+                        return $(this)[attribute]();
+                    }).css(attribute, function(){
+                        return $(this).get(0).style[attribute] || 'auto';
+                    });
+                });
+
                 self.trigger_up('field_changed', {
                     dataPointID: self.dataPointID,
                     changes: _.object([fieldName], [self._unWrap($editable.html())])
