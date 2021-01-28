8104ca806b40e4ff5c8f6d1f17f8892836b5fd3b
oco-odoo <oco@odoo.com>
2021-01-28 14:47:53 +0100

    [REM] l10n_ch_qr_iban: merge module into l10n_ch
    This module had been introduced in stable to add a field to store the QR-IBAN in case it differed from the regular account number. We now merge it into the original module and make it so that the only way to define a QR-IBAN is to populate this field (no more magic with account number).

--- addons/l10n_ch/models/account_invoice.py
+++ addons/l10n_ch/models/account_invoice.py
@@ -119,9 +119,7 @@ class AccountMove(models.Model):
                 (3) 1: control digit for identification number and reference
         """
         for record in self:
-            has_qriban = record.partner_bank_id and record.partner_bank_id._is_qr_iban() or False
-            isr_subscription = record.l10n_ch_isr_subscription
-            if (has_qriban or isr_subscription) and record.name:
+            if (record.partner_bank_id.l10n_ch_qr_iban or record.l10n_ch_isr_subscription) and record.name:
                 id_number = record._get_isrb_id_number()
                 if id_number:
                     id_number = id_number.zfill(l10n_ch_ISR_ID_NUM_LENGTH)
@@ -246,7 +244,7 @@ class AccountMove(models.Model):
         for inv in self:
             if inv.move_type == 'in_invoice' and inv.company_id.account_fiscal_country_id.code == "CH":
                 partner_bank = inv.partner_bank_id
-                needs_isr_ref = partner_bank._is_qr_iban() or partner_bank._is_isr_issuer()
+                needs_isr_ref = partner_bank.l10n_ch_qr_iban or partner_bank._is_isr_issuer()
                 if needs_isr_ref and not inv._has_isr_ref():
                     inv.l10n_ch_isr_needs_fixing = True
                     continue
