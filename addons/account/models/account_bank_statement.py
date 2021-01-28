92ded34638f8ad6d04f0649852c301af09302fcb
Laurent Smet <las@odoo.com>
2021-01-28 12:18:52 +0000

    [FIX] account: Fix 'is_reconciled' flag for zero-amount statement line
    When the suspense account is not reconcilable, 'suspense_lines.reconciled' always gives 'False' leading to a not reconciled statement line.
    
    closes odoo/odoo#65285
    
    X-original-commit: 73ddcfbcc58b7a6542ced4bd8b20dd67c9f446ee
    Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

--- addons/account/models/account_bank_statement.py
+++ addons/account/models/account_bank_statement.py
@@ -785,19 +785,6 @@ class AccountBankStatementLine(models.Model):
         for st_line in self:
             liquidity_lines, suspense_lines, other_lines = st_line._seek_for_lines()
 
-            # Compute is_reconciled
-            if not st_line.id:
-                # New record: The journal items are not yet there.
-                st_line.is_reconciled = False
-            elif suspense_lines:
-                # In case of the statement line comes from an older version, it could have a residual amount of zero.
-                st_line.is_reconciled = all(suspense_line.reconciled for suspense_line in suspense_lines)
-            elif st_line.currency_id.is_zero(st_line.amount):
-                st_line.is_reconciled = True
-            else:
-                # The journal entry seems reconciled.
-                st_line.is_reconciled = True
-
             # Compute residual amount
             if st_line.to_check:
                 st_line.amount_residual = -st_line.amount_currency if st_line.foreign_currency_id else -st_line.amount
@@ -805,6 +792,19 @@ class AccountBankStatementLine(models.Model):
                 st_line.amount_residual = sum(suspense_lines.mapped('amount_residual_currency'))
             else:
                 st_line.amount_residual = sum(suspense_lines.mapped('amount_currency'))
+
+            # Compute is_reconciled
+            if not st_line.id:
+                # New record: The journal items are not yet there.
+                st_line.is_reconciled = False
+            elif suspense_lines:
+                # In case of the statement line comes from an older version, it could have a residual amount of zero.
+                st_line.is_reconciled = suspense_lines.currency_id.is_zero(st_line.amount_residual)
+            elif st_line.currency_id.is_zero(st_line.amount):
+                st_line.is_reconciled = True
+            else:
+                # The journal entry seems reconciled.
+                st_line.is_reconciled = True
 
     # -------------------------------------------------------------------------
     # CONSTRAINT METHODS
