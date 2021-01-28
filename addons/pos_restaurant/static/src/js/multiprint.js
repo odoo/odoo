odoo/odoo

commit 129de2971950a4d306469e19ef419ecbc2c60ab0

[FIX] pos_restaurant: Use order printers with HTTPS
The protocol and port to use when printing orders were hardcoded. This
resulted in errors when using the POS in HTTPS as the browser blocked
non-HTTPS requests on the page.

closes odoo/odoo#64814

Taskid: 2413240
Signed-off-by: Quentin Lejeune (qle) <qle@odoo.com>

addons/pos_restaurant/static/src/js/multiprint.js
@@ -60,9 +60,9 @@ models.load_models({
             if(active_printers[printers[i].id]){
                 var url = printers[i].proxy_ip || '';
                 if(url.indexOf('//') < 0){
-                    url = 'http://'+url;
+                    url = window.location.protocol + '//' + url;
                 }
-                if(url.indexOf(':',url.indexOf('//')+2) < 0){
+                if(url.indexOf(':',url.indexOf('//')+2) < 0 && window.location.protocol === 'http'){
                     url = url+':8069';
                 }
                 var printer = new Printer(self,{url:url});

commit 068ea5c937f61a53f4125466bb95121be64d1e57

[FIX] pos_restaurant: Kitchen Printer fails with HTTP
Since 129de2971950a4d306469e19ef419ecbc2c60ab0, the port was no longer
added to the URL of the box when using HTTP.

addons/pos_restaurant/static/src/js/multiprint.js
@@ -62,7 +62,7 @@ models.load_models({
                 if(url.indexOf('//') < 0){
                     url = window.location.protocol + '//' + url;
                 }
-                if(url.indexOf(':',url.indexOf('//')+2) < 0 && window.location.protocol === 'http'){
+                if(url.indexOf(':',url.indexOf('//')+2) < 0 && window.location.protocol === 'http:'){
                     url = url+':8069';
                 }
                 var printer = new Printer(self,{url:url});