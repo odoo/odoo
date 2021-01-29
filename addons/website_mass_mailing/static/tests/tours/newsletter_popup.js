4eae1588b8d99d0ac865df8f5d85f3eea44e937b
qsm-odoo <qsm@odoo.com>
2021-01-29 11:27:02 +0000

    [FIX] website_mass_mailing, *: add test for newsletter popup
    *: website
    
    This is the forward-port of a stable dependency fix, needed as it
    includes a test. The dependency was properly fixed with [1].
    
    [1]: https://github.com/odoo/odoo/commit/c4687e661c89b3fcc1904c9b10cb2d23e73c9abf
    
    closes odoo/odoo#65530
    
    X-original-commit: 76e1495b0d5eab8fc03b6daf51dbc1c148c7de53
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

---
+++ addons/website_mass_mailing/static/tests/tours/newsletter_popup.js
@@ -0,0 +1,81 @@
+odoo.define("website_mass_mailing.tour.newsletter_popup_edition", function (require) {
+"use strict";
+
+const tour = require('web_tour.tour');
+const wTourUtils = require('website.tour_utils');
+
+tour.register('newsletter_popup_edition', {
+    test: true,
+    url: '/?enable_editor=1',
+}, [
+    wTourUtils.dragNDrop({
+        id: 's_newsletter_subscribe_popup',
+        name: 'Newsletter Popup',
+    }),
+    {
+        content: "Confirm newsletter choice",
+        trigger: '.modal-footer .btn-primary',
+    },
+    {
+        content: "Check the modal is opened for edition",
+        trigger: '.o_newsletter_popup .modal:visible',
+        in_modal: false,
+        run: () => null,
+    },
+    wTourUtils.clickOnSave(),
+    {
+        content: "Check the modal has been saved, closed",
+        trigger: '.o_newsletter_popup',
+        extra_trigger: 'body:not(.editor_enable)',
+        run: function (actions) {
+            const $modal = this.$anchor.find('.modal');
+            if ($modal.is(':visible')) {
+                console.error('Modal is still opened...');
+            }
+        },
+    },
+]);
+});
+
+odoo.define("website_mass_mailing.tour.newsletter_popup_use", function (require) {
+"use strict";
+
+const tour = require('web_tour.tour');
+
+tour.register('newsletter_popup_use', {
+    test: true,
+    url: '/',
+}, [
+    {
+        content: "Check the modal is not yet opened and force it opened",
+        trigger: '.o_newsletter_popup',
+        run: function (actions) {
+            const $modal = this.$anchor.find('.modal');
+            if ($modal.is(':visible')) {
+                console.error('Modal is already opened...');
+            }
+            $(document).trigger('mouseleave');
+        },
+    },
+    {
+        content: "Check the modal is now opened and enter text in the subscribe input",
+        trigger: '.o_newsletter_popup .modal input',
+        in_modal: false,
+        run: 'text hello@world.com',
+    },
+    {
+        content: "Subscribe",
+        trigger: '.modal-dialog .btn-primary',
+    },
+    {
+        content: "Check the modal is now closed",
+        trigger: '.o_newsletter_popup',
+        run: function (actions) {
+            const $modal = this.$anchor.find('.modal');
+            if ($modal.is(':visible')) {
+                console.error('Modal is still opened...');
+            }
+        },
+    }
+]);
+});
