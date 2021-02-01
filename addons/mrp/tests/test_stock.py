0333ae704ef4c3c8a70eeb6d727b90eef96e8fac
Adrien Widart <awt@odoo.com>
2021-02-01 13:38:49 +0000

    [FIX] mrp: force kit to be consumable product
    Kits must be consumable products, they might never be in stock.
    Setting kits as storable products can create some issues if the user
    creates some reordering rules.
    
    Because kits might never be in stock, it's impossible to fullfil the
    quantity of a reordering rule: asking a mini/maxi quantity on a kit
    makes no sense.
    
    Odoo will continuously propose to order more to reach the quantity
    necessary for the kit, but will never reach the asked qty, as it's a
    kit, not a manufactured product.
    
    OPW-2448878
    
    X-original-commit: 3d34d58388ab362bf8e69cc2e75adbdfda3331b2

--- addons/mrp/tests/test_stock.py
+++ addons/mrp/tests/test_stock.py
@@ -214,10 +214,10 @@ class TestKitPicking(common.TestMrpCommon):
     def setUp(self):
         super(TestKitPicking, self).setUp()
 
-        def create_product(name):
+        def create_product(name, type='product'):
             p = Form(self.env['product.product'])
             p.name = name
-            p.type = 'product'
+            p.type = type
             return p.save()
 
         # Create a kit 'kit_parent' :
@@ -241,10 +241,10 @@ class TestKitPicking(common.TestMrpCommon):
         component_f = create_product('Comp F')
         component_g = create_product('Comp G')
         # Creating all kits
-        kit_1 = create_product('Kit 1')
-        kit_2 = create_product('Kit 2')
-        kit_3 = create_product('kit 3')
-        self.kit_parent = create_product('Kit Parent')
+        kit_1 = create_product('Kit 1', type='consu')
+        kit_2 = create_product('Kit 2', type='consu')
+        kit_3 = create_product('kit 3', type='consu')
+        self.kit_parent = create_product('Kit Parent', type='consu')
         # Linking the kits and the components via some 'phantom' BoMs
         bom_kit_1 = self.env['mrp.bom'].create({
             'product_tmpl_id': kit_1.product_tmpl_id.id,
