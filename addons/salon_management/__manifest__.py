fec84693597b9d03f7325077494657394989f52f
zatin <yinmazatin21@gmail.com>
2021-02-01 13:14:22 +0700

    Update the index html about module description

--- addons/salon_management/__manifest__.py
+++ addons/salon_management/__manifest__.py
@@ -1,21 +1,8 @@
 # -*- coding: utf-8 -*-
 ###################################################################################
 #
-#    Cybrosys Technologies Pvt. Ltd.
-#    Copyright (C) 2020-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#
-#    This program is free software: you can modify
-#    it under the terms of the GNU Affero General Public License (AGPL) as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#    A2A Digtial 
+#    Copyright (C) 2021-TODAY A2A Digital (<https://www.a2adigital.com>).
 #
 ###################################################################################
 {
@@ -48,7 +35,7 @@
              'wizard/salon_booking_payment.xml',
              'templates/salon_booking_templates.xml',
              ],
-    'images': ['static/description/banner.png'],
+    'images': ['static/description/banner.jpg'],
     'license': 'AGPL-3',
     'installable': True,
     'application': True,
