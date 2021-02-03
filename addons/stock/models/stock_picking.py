ccc07104cf99f412a656f70cd0ef8430079f7100
nie <nie@odoo.com>
2021-02-03 15:21:46 +0000

    [FIX] stock: use returned moves when confirming
    Steps:
    - Install mrp, stock
    - Go to Inventory > Configuration > Operation Types
    - Edit Delivery Orders:
      - Show Detailed Operations: Checked
    - Go to Master Data > Products
    - Create two products (1) (2)
    - Click (1)
    - Click the "Bill of Material" smart button
    - Create a BoM:
      - BoM Type: Kit
      - Component: (2)
    - Go to Overview
    - Click Delivery Orders
    - Create a Delivery Order:
      - Add (1) with an initial demand of 1
    - Mark as ToDo
    - Click Edit:
      - Detailed Operations tab:
        - Add (1) with a done quantity of 1
    - Validate and create a Backorder
    
    Bug:
    Record does not exist or has been deleted.
    (Record: stock.move(60,), User: 2)
    
    Explanation:
    When confirming a `stock.picking` with `mrp` installed, the action goes
    through `action_explode()`.
    https://github.com/odoo/odoo/blob/d9ea1d5f054f05197581acf67c8a9c1e6acb8d02/addons/mrp/models/stock_move.py#L154
    This action splits the components of the product with a Kit (`phantom`)
    BoM instead of manufacturing it. When everything is split, the
    `stock.move` unlinks itself because it has been replaced with its
    components. `action_explode()` then returns the new components. Trying
    to call a method on the unlinked move will result in an error.
    
    This commit replaces the move with the newly generated component moves.
    
    This is safe to do in `stock` because the non-overridden
    `_action_confirm()` returns the move it was called on as seen here:
    https://github.com/odoo/odoo/blob/d9ea1d5f054f05197581acf67c8a9c1e6acb8d02/addons/stock/models/stock_move.py#L790
    
    opw:2450016
    
    X-original-commit: 67b52df6211fdc174de8d830a40c37e6b0e6016d

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -747,6 +747,7 @@ class Picking(models.Model):
         self._check_company()
 
         todo_moves = self.mapped('move_lines').filtered(lambda self: self.state in ['draft', 'waiting', 'partially_available', 'assigned', 'confirmed'])
+<<<<<<< HEAD
         for picking in self:
             if picking.owner_id:
                 picking.move_lines.write({'restrict_partner_id': picking.owner_id.id})
@@ -759,6 +760,49 @@ class Picking(models.Model):
         done_incoming_moves._trigger_assign()
 
         self._send_confirmation_email()
+=======
+        # Check if there are ops not linked to moves yet
+        for pick in self:
+            # # Explode manually added packages
+            # for ops in pick.move_line_ids.filtered(lambda x: not x.move_id and not x.product_id):
+            #     for quant in ops.package_id.quant_ids: #Or use get_content for multiple levels
+            #         self.move_line_ids.create({'product_id': quant.product_id.id,
+            #                                    'package_id': quant.package_id.id,
+            #                                    'result_package_id': ops.result_package_id,
+            #                                    'lot_id': quant.lot_id.id,
+            #                                    'owner_id': quant.owner_id.id,
+            #                                    'product_uom_id': quant.product_id.uom_id.id,
+            #                                    'product_qty': quant.qty,
+            #                                    'qty_done': quant.qty,
+            #                                    'location_id': quant.location_id.id, # Could be ops too
+            #                                    'location_dest_id': ops.location_dest_id.id,
+            #                                    'picking_id': pick.id
+            #                                    }) # Might change first element
+            # # Link existing moves or add moves when no one is related
+            for ops in pick.move_line_ids.filtered(lambda x: not x.move_id):
+                # Search move with this product
+                moves = pick.move_lines.filtered(lambda x: x.product_id == ops.product_id)
+                moves = sorted(moves, key=lambda m: m.quantity_done < m.product_qty, reverse=True)
+                if moves:
+                    ops.move_id = moves[0].id
+                else:
+                    new_move = self.env['stock.move'].create({
+                                                    'name': _('New Move:') + ops.product_id.display_name,
+                                                    'product_id': ops.product_id.id,
+                                                    'product_uom_qty': ops.qty_done,
+                                                    'product_uom': ops.product_uom_id.id,
+                                                    'location_id': pick.location_id.id,
+                                                    'location_dest_id': pick.location_dest_id.id,
+                                                    'picking_id': pick.id,
+                                                    'picking_type_id': pick.picking_type_id.id,
+                                                   })
+                    ops.move_id = new_move.id
+                    new_move = new_move._action_confirm()
+                    todo_moves |= new_move
+                    #'qty_done': ops.qty_done})
+        todo_moves._action_done()
+        self.write({'date_done': fields.Datetime.now()})
+>>>>>>> 3a00bbbe87e... temp
         return True
 
     def _send_confirmation_email(self):
