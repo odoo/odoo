1556fc3c56775fc8cbaf6571886c97efde5355d1
jbw <jbw@odoo.com>
2021-02-03 15:01:41 +0000

    [IMP] account: get default payment ref format with country
    Set the payment reference standard to the one of the country of the
    company by default.
    
    This is a backport of task 2297165. Mimicking it's bahviour without
    merging the l10n_be_invoice_bba module into the l10n_be module.
    
    Task: 2440462
    X-original-commit: d68fb5f72d69d0382392afd7407f94520857f764

--- addons/l10n_be/models/__init__.py
+++ addons/l10n_be/models/__init__.py
@@ -2,5 +2,10 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import account_chart_template
+<<<<<<< HEAD
 from . import account_journal
 from . import account_move
+=======
+from . import account_invoice
+from . import account_journal
+>>>>>>> 80add7b1185... temp
