80f05addf48dd57da9ae78f39116a9b23f60529a
Martin Trigaux <mat@odoo.com>
2021-02-04 09:40:00 +0000

    [FIX] mail: fallback on English message
    In case of bad translation that doesn't contain the placeholder
    
    Context:
    A bad Japanese translation was inserted
    
      #. module: mail
      #: code:addons/mail/models/mail_thread.py:0
      #, python-format
      msgid "Create new %(document)s"
      msgstr "新しい%(ドキュメント)を作成する"
    
    The translation has been corrected but use the 14.0 syntax of _ method
    to include placeholders and fallback on the English source term in
    case of bad translation
    cf odoo/odoo#52155
    
    closes odoo/odoo#65517
    
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -377,26 +377,24 @@ class MailThread(models.AbstractModel):
             email_link = "<a href='mailto:%(email)s'>%(email)s</a>" % {'email': alias.display_name}
             if nothing_here:
                 return "<p class='o_view_nocontent_smiling_face'>%(dyn_help)s</p>" % {
-                    'dyn_help': _("Add a new %(document)s or send an email to %(email_link)s") % {
-                        'document': document_name,
-                        'email_link': email_link
-                    }
+                    'dyn_help': _("Add a new %(document)s or send an email to %(email_link)s",
+                        document=document_name,
+                        email_link=email_link,
+                    )
                 }
             # do not add alias two times if it was added previously
             if "oe_view_nocontent_alias" not in help:
                 return "%(static_help)s<p class='oe_view_nocontent_alias'>%(dyn_help)s</p>" % {
                     'static_help': help,
-                    'dyn_help': _("Create new %(document)s by sending an email to %(email_link)s") %  {
-                        'document': document_name,
-                        'email_link': email_link,
-                    }
+                    'dyn_help': _("Create new %(document)s by sending an email to %(email_link)s",
+                        document=document_name,
+                        email_link=email_link,
+                    )
                 }
 
         if nothing_here:
             return "<p class='o_view_nocontent_smiling_face'>%(dyn_help)s</p>" % {
-                'dyn_help': _("Create new %(document)s") % {
-                    'document': document_name,
-                }
+                'dyn_help': _("Create new %(document)s", document=document_name),
             }
 
         return help
