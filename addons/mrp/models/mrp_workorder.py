98302b3a526c3bddeb8d3c2f76c1d99834262b7b
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2021-02-05 10:47:15 +0000

    [FIX] mrp: avoid blocking MO after post inventory
    Fine tuning of 074a2c9c9f0415898d099914f023c8f3c320cf8b
    
    The fix was applying to a specific case while we need a generic solution
    for a chain of workorder where we don't know how the quantity will be
    processed. In a generic case is more effective to consider the workorders
    which have not been finished (or canceled) to detect how much quantity
    is still left to process
    
    opw-2415073
    
    X-original-commit: 072d86ef35e5fe4a4f200cf9abee08d85cfa4525

--- addons/mrp/models/mrp_workorder.py
+++ addons/mrp/models/mrp_workorder.py
@@ -465,6 +465,55 @@ class MrpWorkorder(models.Model):
     def _get_byproduct_move_to_update(self):
         return self.production_id.move_finished_ids.filtered(lambda x: (x.product_id.id != self.production_id.product_id.id) and (x.state not in ('done', 'cancel')))
 
+<<<<<<< HEAD
+=======
+    def _create_or_update_finished_line(self):
+        """
+        1. Check that the final lot and the quantity producing is valid regarding
+            other workorders of this production
+        2. Save final lot and quantity producing to suggest on next workorder
+        """
+        self.ensure_one()
+        final_lot_quantity = self._get_real_uom_qty(self.qty_production)
+        rounding = self.product_uom_id.rounding
+        # Get the max quantity possible for current lot in other workorders, avoid
+        # considering already completed steps up to the current one
+        for workorder in (self.production_id.workorder_ids.filtered(lambda wo: not (wo.id < self.id and wo.state in ('done','cancel'))) - self):
+            # We add the remaining quantity to the produced quantity for the
+            # current lot. For 5 finished products: if in the first wo it
+            # creates 4 lot A and 1 lot B and in the second it create 3 lot A
+            # and it remains 2 units to product, it could produce 5 lot A.
+            # In this case we select 4 since it would conflict with the first
+            # workorder otherwise.
+            line = workorder.finished_workorder_line_ids.filtered(lambda line: line.lot_id == self.finished_lot_id)
+            line_without_lot = workorder.finished_workorder_line_ids.filtered(lambda line: line.product_id == workorder.product_id and not line.lot_id)
+            quantity_remaining = workorder.qty_remaining + line_without_lot.qty_done
+            quantity = line.qty_done + quantity_remaining
+            if line and float_compare(quantity, final_lot_quantity, precision_rounding=rounding) <= 0:
+                final_lot_quantity = quantity
+            elif float_compare(quantity_remaining, final_lot_quantity, precision_rounding=rounding) < 0:
+                final_lot_quantity = quantity_remaining
+
+        # final lot line for this lot on this workorder.
+        current_lot_lines = self.finished_workorder_line_ids.filtered(lambda line: line.lot_id == self.finished_lot_id)
+
+        # this lot has already been produced
+        if float_compare(final_lot_quantity, current_lot_lines.qty_done + self.qty_producing, precision_rounding=rounding) < 0:
+            raise UserError(_('You have produced %s %s of lot %s in the previous workorder. You are trying to produce %s in this one') %
+                (final_lot_quantity, self.product_id.uom_id.name, self.finished_lot_id.name, current_lot_lines.qty_done + self.qty_producing))
+
+        # Update workorder line that regiter final lot created
+        if not current_lot_lines:
+            current_lot_lines = self.env['mrp.workorder.line'].create({
+                'finished_workorder_id': self.id,
+                'product_id': self.product_id.id,
+                'lot_id': self.finished_lot_id.id,
+                'qty_done': self.qty_producing,
+            })
+        else:
+            current_lot_lines.qty_done += self.qty_producing
+
+>>>>>>> ea68827b16a... temp
     def _start_nextworkorder(self):
         if self.state == 'done' and self.next_work_order_id.state == 'pending':
             self.next_work_order_id.state = 'ready'
