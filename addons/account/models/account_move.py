6eebd35c2078bdbb79953163fbad53fa995e1d1d
wan <wan@odoo.com>
2021-02-08 17:08:12 +0100

    [FIX] account: don't return recordset on action_post
    Fixes #65706
    
    X-original-commit: 398751954ef1735bfb2757328189cf57f8beeb1c

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -2545,7 +2545,8 @@ class AccountMove(models.Model):
         return action
 
     def action_post(self):
-        return self._post(soft=False)
+        self._post(soft=False)
+        return False
 
     def js_assign_outstanding_line(self, line_id):
         ''' Called by the 'payment' widget to reconcile a suggested journal item to the present
