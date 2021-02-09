094e2a0b36f8305ed3af472cc72b15bf0171bdf7
Lucas Lefèvre <lul@odoo.com>
2021-02-09 16:42:00 +0000

    [FIX] google_calendar: events sync issues
    Before this commit:
    
    The google synchronization dis not properly synced event in some conditions. Some use cases were not properly tested.
    
    Recurrent events were regularly badly synchronized with Google.
    
    Several issues occured:
     - events not follow recurrence were duplicated on both Google or Odoo and sometimes deleted when the recurrence was reapplied.
     - the base_event (first event of the recurrence) was duplicated
     - miscalculation of the event google_id when they were part of a recurrence but did not followed the rrule.
     - improper data handling from google. Odoo objets were created with incorrect values
     - attendee state was not properly sync from Odoo to Google
     - whole recurrence deletions from google were not properly sync in Odoo
     - when time fields of a recurrence were modified on Google, modifications were ignored on Odoo
     - Event timezone were not properly saved on the recurrency. (Odoo saves it on the recurrency and Google on the event)
     - Odoo considered that all public events are writable bu Odoo users. That would trigger errors as Google implement an access right model on public events
     - lack of tests
     - a lot of weird behaviors resulting from these problems.
    
    Taskid: 2456498
    Opw: 2299834
    X-original-commit: 374d3c7f95fca9832afb0d34d710f28dbadcdfa6
    Co-authored-by: Lucas Lefèvre <lul@odoo.com>
    Co-authored-by: Arnaud Joset <arj@odoo.com>

--- addons/google_calendar/utils/google_event.py
+++ addons/google_calendar/utils/google_event.py
@@ -4,6 +4,7 @@ from odoo.api import model
 from odoo.tools import email_normalize
 from odoo.tools.sql import existing_tables
 import pytz
+import logging
 from typing import Iterator, Mapping
 from collections import abc
 from dateutil.parser import parse
@@ -11,6 +12,8 @@ from dateutil.relativedelta import relativedelta
 
 
 from odoo import _
+
+_logger = logging.getLogger(__name__)
 
 
 class GoogleEvent(abc.Set):
@@ -79,7 +82,7 @@ class GoogleEvent(abc.Set):
         """Returns the Odoo id stored in the Google Event metadata.
         This id might not actually exists in the database.
         """
-        properties = self.extendedProperties and self.extendedProperties.get('shared', {}) or {}
+        properties = self.extendedProperties and (self.extendedProperties.get('shared', {}) or self.extendedProperties.get('private', {})) or {}
         o_id = properties.get('%s_odoo_id' % dbname)
         if o_id:
             return int(o_id)
@@ -88,15 +91,15 @@ class GoogleEvent(abc.Set):
         ids = tuple(e._odoo_id for e in self if e._odoo_id)
         if len(ids) == len(self):
             return ids
-        found = self._load_odoo_ids_from_db(env)
+        model = self._get_model(env)
+        found = self._load_odoo_ids_from_db(env, model)
         unsure = self - found
         if unsure:
-            unsure._load_odoo_ids_from_metadata(env)
+            unsure._load_odoo_ids_from_metadata(env, model)
 
         return tuple(e._odoo_id for e in self)
 
-    def _load_odoo_ids_from_metadata(self, env):
-        model = self._get_model(env)
+    def _load_odoo_ids_from_metadata(self, env, model):
         unsure_odoo_ids = tuple(e._meta_odoo_id(env.cr.dbname) for e in self)
         odoo_events = model.browse(_id for _id in unsure_odoo_ids if _id)
 
@@ -110,8 +113,7 @@ class GoogleEvent(abc.Set):
             if odoo_id in o_ids:
                 e._events[e.id]['_odoo_id'] = odoo_id
 
-    def _load_odoo_ids_from_db(self, env):
-        model = self._get_model(env)
+    def _load_odoo_ids_from_db(self, env, model):
         odoo_events = model.with_context(active_test=False)._from_google_ids(self.ids)
         mapping = {e.google_id: e.id for e in odoo_events}  # {google_id: odoo_id}
         existing_google_ids = odoo_events.mapped('google_id')
@@ -153,7 +155,17 @@ class GoogleEvent(abc.Set):
     def filter(self, func) -> 'GoogleEvent':
         return GoogleEvent(e for e in self if func(e))
 
+    def clear_type_ambiguity(self, env):
+        ambiguous_events = self.filter(GoogleEvent._is_type_ambiguous)
+        recurrences = ambiguous_events._load_odoo_ids_from_db(env, env['calendar.recurrence'])
+        for recurrence in recurrences:
+            self._events[recurrence.id]['recurrence'] = True
+        for event in ambiguous_events - recurrences:
+            self._events[event.id]['recurrence'] = False
+
     def is_recurrence(self):
+        if self._is_type_ambiguous():
+            _logger.warning("Ambiguous event type: cannot accurately tell whether a cancelled event is a recurrence or not")
         return bool(self.recurrence)
 
     def is_recurrent(self):
@@ -175,6 +187,12 @@ class GoogleEvent(abc.Set):
         events.odoo_ids(env)
 
         return self.filter(lambda e: e._odoo_id)
+
+    def _is_type_ambiguous(self):
+        """For cancelled events/recurrences, Google only send the id and
+        the cancelled status. There is no way to know if it was a recurrence
+        or simple event."""
+        return self.is_cancelled() and 'recurrence' not in self._events[self.id]
 
     def _get_model(self, env):
         if all(e.is_recurrence() for e in self):
