35c54ed51e7c043debb54608f5392b77344aefa7
Yannick Tivisse <yti@odoo.com>
2021-02-09 11:08:34 +0100

    [IMP] hr: Add work location number field
    Could be useful, for example in the DMFA declaration

--- addons/hr/models/hr_work_location.py
+++ addons/hr/models/hr_work_location.py
@@ -13,3 +13,4 @@ class WorkLocation(models.Model):
     name = fields.Char(string="Work Location", required=True)
     company_id = fields.Many2one('res.company', required=True, default=lambda self: self.env.company)
     address_id = fields.Many2one('res.partner', required=True, string="Work Address", domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    location_number = fields.Char()
