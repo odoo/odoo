dcfc8b7896f079e8e4474392807d9c53cb7e94f3
Lucas Lefèvre <lul@odoo.com>
2021-02-09 16:42:00 +0000

    [FIX] google_calendar: events sync issues
    Before this commit:
    
    The google synchronization dis not properly synced event in some conditions. Some use cases were not properly tested.
    
    Recurrent events were regularly badly synchronized with Google.
    
    Several issues occured:
     - events not follow recurrence were duplicated on both Google or Odoo and sometimes deleted when the recurrence was reapplied.
     - the base_event (first event of the recurrence) was duplicated
     - miscalculation of the event google_id when they were part of a recurrence but did not followed the rrule.
     - improper data handling from google. Odoo objets were created with incorrect values
     - attendee state was not properly sync from Odoo to Google
     - whole recurrence deletions from google were not properly sync in Odoo
     - when time fields of a recurrence were modified on Google, modifications were ignored on Odoo
     - Event timezone were not properly saved on the recurrency. (Odoo saves it on the recurrency and Google on the event)
     - Odoo considered that all public events are writable bu Odoo users. That would trigger errors as Google implement an access right model on public events
     - lack of tests
     - a lot of weird behaviors resulting from these problems.
    
    closes odoo/odoo#68332
    
    Taskid: 2456498
    Opw: 2299834
    X-original-commit: 374d3c7f95fca9832afb0d34d710f28dbadcdfa6
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Co-authored-by: Lucas Lefèvre <lul@odoo.com>
    Co-authored-by: Arnaud Joset <arj@odoo.com>

--- addons/google_calendar/models/calendar_attendee.py
+++ addons/google_calendar/models/calendar_attendee.py
@@ -4,6 +4,7 @@
 from odoo import models
 
 from odoo.addons.google_calendar.models.google_sync import google_calendar_token
+from odoo.addons.google_calendar.utils.google_calendar import GoogleCalendarService
 
 class Attendee(models.Model):
     _name = 'calendar.attendee'
@@ -17,3 +18,11 @@ class Attendee(models.Model):
         with google_calendar_token(self.env.user.sudo()) as token:
             if not token:
                 super()._send_mail_to_attendees(template_xmlid, force_send, ignore_recurrence)
+
+    def write(self, vals):
+        res = super().write(vals)
+        if vals.get('state'):
+            # When the state is changed, the corresponding event must be sync with google
+            google_service = GoogleCalendarService(self.env['google.service'])
+            self.event_id.filtered('google_id')._sync_odoo2google(google_service)
+        return res
