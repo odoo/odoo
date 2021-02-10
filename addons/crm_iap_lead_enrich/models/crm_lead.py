da523d204243a29787cef374b0f608106064c09c
ijas ahammed <ija@odoo.com>
2021-02-10 06:24:32 +0000

    [FIX] crm_iap_lead_enrich: show 'Enrich' button only if email is valid
    With this commit, the 'Enrich' button on lead now appears only after the
    email is set and valid, which previously appeared even if no email is set
    or if the value is not a valid email.
    
    Task ID-2393253
    COM PR odoo/odoo#64075

--- addons/crm_iap_lead_enrich/models/crm_lead.py
+++ addons/crm_iap_lead_enrich/models/crm_lead.py
@@ -24,7 +24,7 @@ class Lead(models.Model):
             self.show_enrich_button = False
             return
         for lead in self:
-            if not lead.active or not lead.email_from or lead.iap_enrich_done or lead.reveal_id or lead.probability == 100:
+            if not lead.active or not lead.email_from or lead.email_state == 'incorrect' or lead.iap_enrich_done or lead.reveal_id or lead.probability == 100:
                 lead.show_enrich_button = False
             else:
                 lead.show_enrich_button = True
