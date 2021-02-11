17b3dbc00043a7d2f489cd94d4255909ee3f866b
Anh Thao Pham (pta) <pta@odoo.com>
2021-02-11 09:40:00 +0000

    [FIX] mail: prevent use of private addresses for followers, recipients,...
    - Connect with Admin
    - Go to Contacts, edit himself by adding a Private Address
    - Create an Internal User without "Access to Private Addresses" right (i.e. User X)
    - Go to any app implementing chatter (i.e. Sales)
    - Create a SO
    - Add the created Private Address as follower
    - Make sure User X can access the record (i.e. Sales: Administrator)
    - Connect with User X and open the SO
    An Access Error is raised while trying to fetch data about the followers.
    
    This commit prevents to:
    - add a private address as follower of a record
    - add a private address as Recipient in full composer
    - propose private addresses when adding a mention to a partner
    
    opw-2428936
    
    Task-id: 2463622
    X-original-commit: 20536e1bbeb641539c0de44364f8376e7cef651b

--- addons/mail/wizard/invite.py
+++ addons/mail/wizard/invite.py
@@ -39,7 +39,8 @@ class Invite(models.TransientModel):
 
     res_model = fields.Char('Related Document Model', required=True, index=True, help='Model of the followed resource')
     res_id = fields.Integer('Related Document ID', index=True, help='Id of the followed resource')
-    partner_ids = fields.Many2many('res.partner', string='Recipients', help="List of partners that will be added as follower of the current document.")
+    partner_ids = fields.Many2many('res.partner', string='Recipients', help="List of partners that will be added as follower of the current document.",
+                                   domain=[('type', '!=', 'private')])
     message = fields.Html('Message')
     send_mail = fields.Boolean('Send Email', default=True, help="If checked, the partners will receive an email warning they have been added in the document's followers.")
 
