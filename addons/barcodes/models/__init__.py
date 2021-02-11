d1abf31fb4f5b51182e2f7da595a822bbe606069
svs-odoo <svs@odoo.com>
2021-02-11 14:57:41 +0000

    [MOV] barcodes: split `barcodes.py`
    Separates `barcode.nomenclature` and `barcode.rule` in their own file.
    
    task-1968113
    
    Co-authored-by: ryv-odoo<ryv@odoo.com>

--- addons/barcodes/models/__init__.py
+++ addons/barcodes/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from . import barcodes
 from . import barcode_events_mixin
+from . import barcode_nomenclature
+from . import barcode_rule
 from . import ir_http
 from . import res_company
