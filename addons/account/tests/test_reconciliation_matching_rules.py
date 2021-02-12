de7435f13ddd91295640a01367eb27ec2a782579
Laurent Smet <las@odoo.com>
2021-02-12 09:13:40 +0000

    [FIX] account: Set memo to ref if not payment_reference
    Issue
    
    	- Install "Accounting" module
    	- Create a new bill :
    	- Set vendor
    	- Set no payment reference
    	- Set a bill reference
    	- Add products to bill
    	- Confirm and click on Register Payment
    
    	Memo field has no value.
    
    Cause
    
    	The memo field is based on lines name.
    	Lines name is computed in `_onchange_payment_reference`
    	and depends only on payment_referecence.
    
    Solution
    
    	If no `payment_reference`, fallback on `ref` to set line name.
    
    opw-2440389
    
    closes odoo/odoo#66491
    
    Co-author: nboulif <bon@odoo.com>
    X-original-commit: 7a7dc212af7279544cffba362bce7daa4fc00865
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>
    Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

--- addons/account/tests/test_reconciliation_matching_rules.py
+++ addons/account/tests/test_reconciliation_matching_rules.py
@@ -211,7 +211,7 @@ class TestReconciliationMatchingRules(AccountTestInvoicingCommon):
         self.rule_1.match_text_location_note = False
         self._check_statement_matching(self.rule_1, {
             self.bank_line_3.id: {'aml_ids': [self.invoice_line_5.id], 'model': self.rule_1, 'partner': self.bank_line_3.partner_id},
-            self.bank_line_4.id: {'aml_ids': [self.invoice_line_7.id], 'model': self.rule_1, 'partner': self.bank_line_4.partner_id},
+            self.bank_line_4.id: {'aml_ids': [self.invoice_line_6.id], 'model': self.rule_1, 'partner': self.bank_line_4.partner_id},
             self.bank_line_5.id: {'aml_ids': [self.invoice_line_6.id], 'model': self.rule_1, 'partner': self.bank_line_5.partner_id},
         }, statements=self.bank_st_2)
 
@@ -220,7 +220,7 @@ class TestReconciliationMatchingRules(AccountTestInvoicingCommon):
         self.rule_1.match_text_location_note = True
         self._check_statement_matching(self.rule_1, {
             self.bank_line_3.id: {'aml_ids': [self.invoice_line_6.id], 'model': self.rule_1, 'partner': self.bank_line_3.partner_id},
-            self.bank_line_4.id: {'aml_ids': [self.invoice_line_7.id], 'model': self.rule_1, 'partner': self.bank_line_4.partner_id},
+            self.bank_line_4.id: {'aml_ids': [self.invoice_line_6.id], 'model': self.rule_1, 'partner': self.bank_line_4.partner_id},
             self.bank_line_5.id: {'aml_ids': [self.invoice_line_5.id], 'model': self.rule_1, 'partner': self.bank_line_5.partner_id},
         }, statements=self.bank_st_2)
 
@@ -229,8 +229,8 @@ class TestReconciliationMatchingRules(AccountTestInvoicingCommon):
         self.rule_1.match_text_location_note = False
         self._check_statement_matching(self.rule_1, {
             self.bank_line_3.id: {'aml_ids': [self.invoice_line_5.id], 'model': self.rule_1, 'partner': self.bank_line_3.partner_id},
-            self.bank_line_4.id: {'aml_ids': [self.invoice_line_7.id], 'model': self.rule_1, 'partner': self.bank_line_4.partner_id},
-            self.bank_line_5.id: {'aml_ids': [self.invoice_line_7.id], 'model': self.rule_1, 'partner': self.bank_line_5.partner_id},
+            self.bank_line_4.id: {'aml_ids': [self.invoice_line_6.id], 'model': self.rule_1, 'partner': self.bank_line_4.partner_id},
+            self.bank_line_5.id: {'aml_ids': [self.invoice_line_6.id], 'model': self.rule_1, 'partner': self.bank_line_5.partner_id},
         }, statements=self.bank_st_2)
 
         self.rule_1.match_text_location_label = True
@@ -238,8 +238,8 @@ class TestReconciliationMatchingRules(AccountTestInvoicingCommon):
         self.rule_1.match_text_location_note = True
         self._check_statement_matching(self.rule_1, {
             self.bank_line_3.id: {'aml_ids': [self.invoice_line_6.id], 'model': self.rule_1, 'partner': self.bank_line_3.partner_id},
-            self.bank_line_4.id: {'aml_ids': [self.invoice_line_7.id], 'model': self.rule_1, 'partner': self.bank_line_4.partner_id},
-            self.bank_line_5.id: {'aml_ids': [self.invoice_line_7.id], 'model': self.rule_1, 'partner': self.bank_line_5.partner_id},
+            self.bank_line_4.id: {'aml_ids': [self.invoice_line_6.id], 'model': self.rule_1, 'partner': self.bank_line_4.partner_id},
+            self.bank_line_5.id: {'aml_ids': [self.invoice_line_6.id], 'model': self.rule_1, 'partner': self.bank_line_5.partner_id},
         }, statements=self.bank_st_2)
 
         self.rule_1.match_text_location_label = False
