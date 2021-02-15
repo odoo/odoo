863662ffe018320574561d284b9d7ddeb9e02ae9
Xavier BOL (xbo) <xbo@odoo.com>
2021-02-15 10:06:42 +0100

    [IMP] sale_project: not filter SOLs if SO defined in project
    Before this commit, when the pricing type of the project is either
    project rate or employee rate, the list of possible SOLs in sale_line_id
    is filtered to have only the SOLs in the SO.
    
    This commit removes this filter because we just want to use the SOL set
    in the project as a default value.
    
    task-2442683
    
    closes odoo/odoo#66181

--- addons/sale_project/models/project.py
+++ addons/sale_project/models/project.py
@@ -53,7 +53,7 @@ class ProjectTask(models.Model):
 
     sale_order_id = fields.Many2one('sale.order', 'Sales Order', help="Sales order to which the task is linked.")
     sale_line_id = fields.Many2one(
-        'sale.order.line', 'Sales Order Item', domain="[('is_service', '=', True), ('order_partner_id', 'child_of', commercial_partner_id), ('is_expense', '=', False), ('state', 'in', ['sale', 'done']), ('order_id', '=?', project_sale_order_id)]",
+        'sale.order.line', 'Sales Order Item', domain="[('is_service', '=', True), ('order_partner_id', 'child_of', commercial_partner_id), ('is_expense', '=', False), ('state', 'in', ['sale', 'done'])]",
         compute='_compute_sale_line', store=True, readonly=False, copy=False,
         help="Sales order item to which the project is linked. Link the timesheet entry to the sales order item defined on the project. "
         "Only applies on tasks without sale order item defined, and if the employee is not in the 'Employee/Sales Order Item Mapping' of the project.")
