5e0611f4d46e05371ea4749425fa4173736f5c41
Simon Genin (ges) <ges@odoo.com>
2021-02-15 14:11:19 +0000

    [FIX] tsconfig: add missing command name attribute
    closes odoo/odoo#66180
    
    Signed-off-by: Géry Debongnie (ged) <ged@openerp.com>

--- odoo/cli/tsconfig.py
+++ odoo/cli/tsconfig.py
@@ -12,6 +12,9 @@ from odoo.modules.module import MANIFEST_NAMES
 class TSConfig(Command):
     """Generates tsconfig files for javascript code"""
 
+    def __init__(self):
+        self.command_name = "tsconfig"
+
     def get_module_list(self, path):
         return [
             mod.split(os.path.sep)[-2]
