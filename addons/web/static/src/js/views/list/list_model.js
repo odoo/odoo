6156f7b6ac17c983af8f601e2463f76e382df5d7
Aaron Bohy <aab@odoo.com>
2021-02-16 14:35:44 +0000

    [FIX] web: list: correctly evaluate groupby modifier with m2o
    Let's assume a <groupby> node in a list view containing a button
    with an invisible modifier referencing a many2one field, e.g.
    
    ```
     <tree>
       <groupby>
         <field name="m2o" invisible="1"/>
         <button string="do it" attrs="{'invisible': [('m2o', '=',
    False)]}"/>
       </groupby>
     </tree>
    ```
    
    Before this commit, the m2o field was correctly read, but it's
    value wasn't processed by the model, so the modifier wasn't
    correctly evaluated.
    
    Issue reported on PR https://github.com/odoo/odoo/pull/65316/
    
    closes odoo/odoo#66287
    
    X-original-commit: bbaeeb76bbef4ea371d3db5821af91bf1fe9ac4f
    Signed-off-by: Géry Debongnie (ged) <ged@openerp.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/js/views/list/list_model.js
+++ addons/web/static/src/js/views/list/list_model.js
@@ -166,6 +166,7 @@ odoo.define('web.ListModel', function (require) {
                         viewType: 'groupby',
                     });
                     dp.groupData = groupDp.id;
+                    self._parseServerData(groupFields, groupDp, groupDp.data);
                 });
             });
         },
