26203f194aec19e0a5847dc60657137a7699fb09
Noe Antoine <nan@odoo.com>
2021-02-17 11:28:34 +0000

    [IMP] website_crm: propagate logged partner on lead from contact us form
    Reduce friction submitting a CRM contact us form while linking the
    lead to the portal contact.This facilitates the flow generating leads
    from the contact us form.
    
    The partner_id of the lead created from form is set to the one of the
    logged visitor if model is crm.lead and there is a logged_visitor and
    if the prefilled email and phone number of form match the logged partner's
    (otherwise it would change the partner when setting it on lead
    
    In details : there must be an email, the same in input and in partner,
    after normalization. Only then:
    - If there are both a phone in values and a phone on partner,
      propagate partner on lead only if they are the same.
    - Else, that is if there is no phone in partner or no phone in values,
      propagate partner on lead. If no phone in input, it will not propagate.
      on the model. If no phone in partner, then if there is one in input, it
      will write that additional info on partner, completing its profile.
    
    Concerning phone comparison: extending current logic, the numbers are
    still formatted before comparison. That is:
    - Before this comit, the form value was formatted according to country in
    record or geoip. Now, if there is no country_id in record, uses the logged
    visitor partner's country or env company country, if there is a logged user.
    Finally, it uses session geoip as last option. (changes in _get_country())
    - The partner's number is formatted through its model function _phone_format,
    according to the partner country (or env company).
    - Both formattings are done in international format.
    
    Adds tours to check that partner is linked to lead only if phone and email
    are not changed from prefilled data (from logged visitor's partner on form.
    
    Task-ID : 2241639
    PR : #61126
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_crm/tests/test_website_crm.py
+++ addons/website_crm/tests/test_website_crm.py
@@ -16,3 +16,22 @@ class TestWebsiteCrm(odoo.tests.HttpCase):
         self.assertEqual(record.contact_name, 'John Smith')
         self.assertEqual(record.email_from, 'john@smith.com')
         self.assertEqual(record.partner_name, 'Odoo S.A.')
+
+    def test_catch_logged_partner_info_tour(self):
+        user_login = 'admin'
+        user_partner = self.env['res.users'].search([('login', '=', user_login)]).partner_id
+        partner_email = user_partner.email
+        partner_phone = user_partner.phone
+
+        # no edit on prefilled data from logged partner : propagate partner_id on created lead
+        self.start_tour("/", "website_crm_catch_logged_partner_info_tour", login=user_login)
+        created_lead = self.env['crm.lead'].search([('description', '=', '### TOUR DATA PREFILL ###')])
+        self.assertEqual(created_lead.partner_id, user_partner)
+
+        # edited contact us partner info : do not propagate partner_id on lead
+        self.start_tour("/", "website_crm_tour", login=user_login)
+        created_lead = self.env['crm.lead'].search([('description', '=', '### TOUR DATA ###')])
+        self.assertFalse(created_lead.partner_id)
+        # check partner has not been changed
+        self.assertEqual(user_partner.email, partner_email)
+        self.assertEqual(user_partner.phone, partner_phone)
