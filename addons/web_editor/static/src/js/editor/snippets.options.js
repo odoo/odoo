7728066615342b7aa8d1500a74811f770e395be9
Nicolas Lempereur <nle@odoo.com>
2021-02-19 14:10:58 +0000

    [FIX] web_editor: no error background position + ctrl Z
    Scenario:
    
    - start editing background position
    - press TAB key
    - press CTRL + Z
    
    => traceback
    
    Why:
    
    Restoring a snapshot with CTRL Z can send a click event with target not
    wrapped in a jQuery object which was not expected by the code.
    
    reported in https://github.com/odoo/odoo/pull/66462#issuecomment-782025417
    
    opw-2423445
    
    closes odoo/odoo#66540
    
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -4455,7 +4455,7 @@ registry.BackgroundPosition = SnippetOptionWidget.extend({
      * @private
      */
     _onDocumentClicked: function (ev) {
-        if (!ev.target.closest('.o_we_background_position_overlay')) {
+        if (!$(ev.target).closest('.o_we_background_position_overlay')) {
             this._toggleBgOverlay(false);
         }
     },
