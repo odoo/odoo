12b51f93d58f0ec98d8d5c4476fde55f2bcbaa1c
Thibault Delavallée <tde@odoo.com>
2021-02-19 12:25:55 +0100

    [MOV] mail, im_livechat: split models related to mail channel and reorder some fields definitions
    RATIONALE
    
    Channel model is a mail.thread enabled model behaving strangely with followers,
    notifications and discuss. Its code should however be simplified to be more
    self contained and avoid unwanted side effects on other models.
    
    SPECIFICATIONS
    
    Purpose is to prepare modifications in models by doing a preparatory cleaning
    of some fields and model definitions. Notably
    
      * move mail.channel.partner im_livechat override in its own file;
      * slightly re-order member-related fields on channel or mail.channel.partner
        models, and split long lines;
    
    This commit should not change anything functionally as it contains is only some
    code move and reordering.
    
    LINKS
    
    Task ID-2070632 (main task)
    Task ID-2419762 (followup task)
    COM PR odoo/odoo#62859
    ENT PR odoo/enterprise#15172
    UPG PR odoo/upgrade#2005

--- addons/mail/models/mail_channel.py
+++ addons/mail/models/mail_channel.py
@@ -49,9 +49,15 @@ class Channel(models.Model):
     email_send = fields.Boolean('Send messages by email', default=False)
     image_128 = fields.Image("Image", max_width=128, max_height=128, default=_get_default_image)
     # members (depends=['...'] is for `test_mail/tests/common.py`, class Moderation, `setUpClass`)
-    channel_last_seen_partner_ids = fields.One2many('mail.channel.partner', 'channel_id', string='Last Seen', depends=['channel_partner_ids'])
-    channel_partner_ids = fields.Many2many('res.partner', 'mail_channel_partner', 'channel_id', 'partner_id', string='Listeners', depends=['channel_last_seen_partner_ids'])
-    channel_message_ids = fields.Many2many('mail.message', 'mail_message_mail_channel_rel')
+    channel_partner_ids = fields.Many2many(
+        'res.partner', 'mail_channel_partner', 'channel_id', 'partner_id',
+        string='Listeners', depends=['channel_last_seen_partner_ids'])
+    channel_last_seen_partner_ids = fields.One2many(
+        'mail.channel.partner', 'channel_id', string='Last Seen',
+        depends=['channel_partner_ids'])
+    channel_message_ids = fields.Many2many(
+        'mail.message', 'mail_message_mail_channel_rel',
+        string='Channel Messages')
     is_member = fields.Boolean('Is a member', compute='_compute_is_member')
     is_subscribed = fields.Boolean(
         'Is Subscribed', compute='_compute_is_subscribed')
