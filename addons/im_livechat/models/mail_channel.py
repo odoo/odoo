74dc9795f5b9c1712bb171e8ca2e965acd832165
Thibault Delavallée <tde@odoo.com>
2021-02-19 11:25:55 +0000

    [MOV] mail, im_livechat: split models related to mail channel and reorder some fields definitions
    RATIONALE
    
    Channel model is a mail.thread enabled model behaving strangely with followers,
    notifications and discuss. Its code should however be simplified to be more
    self contained and avoid unwanted side effects on other models.
    
    SPECIFICATIONS
    
    Purpose is to prepare modifications in models by doing a preparatory cleaning
    of some fields and model definitions. Notably
    
      * move mail.channel.partner im_livechat override in its own file;
      * slightly re-order member-related fields on channel or mail.channel.partner
        models, and split long lines;
    
    This commit should not change anything functionally as it contains is only some
    code move and reordering.
    
    LINKS
    
    Task ID-2070632 (main task)
    Task ID-2419762 (followup task)
    COM PR odoo/odoo#62859
    ENT PR odoo/enterprise#15172
    UPG PR odoo/upgrade#2005

--- addons/im_livechat/models/mail_channel.py
+++ addons/im_livechat/models/mail_channel.py
@@ -3,24 +3,6 @@
 
 from odoo import api, fields, models, _
 
-class ChannelPartner(models.Model):
-    _inherit = 'mail.channel.partner'
-
-    @api.autovacuum
-    def _gc_unpin_livechat_sessions(self):
-        """ Unpin livechat sessions with no activity for at least one day to
-            clean the operator's interface """
-        self.env.cr.execute("""
-            UPDATE mail_channel_partner
-            SET is_pinned = false
-            WHERE id in (
-                SELECT cp.id FROM mail_channel_partner cp
-                INNER JOIN mail_channel c on c.id = cp.channel_id
-                WHERE c.channel_type = 'livechat' AND cp.is_pinned is true AND
-                    cp.write_date < current_timestamp - interval '1 day'
-            )
-        """)
-
 
 class MailChannel(models.Model):
     """ Chat Session
