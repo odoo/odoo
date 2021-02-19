800f9c499a720e77f799246382687e44c18f13cd
bit-odoo <bit@odoo.com>
2021-02-19 04:40:25 +0000

    [FW][FIX] mail, various: show only my activities shown through systray
    Before this commit, suppose we have a scenario like below
    
      Task-A:
        Activity-1:
          name: Email ( Today )
          Assigned to: User-1
      Task-B:
        Activity-1:
          name: Email ( Today )
          assigned to: User-2
        Activity-2:
          name: Call ( Due in 3 Days )
          assigned to: User-1
    
    When User-1 goes through the systray 'Today' filter shortcut he gets both
    Task-A and Task-B in the list instead of only Task-A. Indeed currently
    activities are not filtered based on current user with its deadlines.
    
    However purpose of systray is to indicate activities current user has to
    perform instead of global activities.
    
    After this commit activities will be filtered based on deadlines as well as the
    current user. In order to achieve this behavior we needed to pass a domain like
    [
       ('activity_ids.date_deadline','=', fields.Date.today()),
       ('activity_ids.user_id','=', 1)
    ]
    
    And for that purpose we introduced a non-stored compute field with a search
    method.
    
    Task ID-2438822
    COM PR odoo/odoo#72219
    
    X-original-commit: f4eaf4d8fb2f97240201104dcd4fc7e2674bce02

--- addons/test_mail/tests/test_mail_activity.py
+++ addons/test_mail/tests/test_mail_activity.py
@@ -576,3 +576,31 @@ class TestActivityMixin(TestActivityCommon):
             self.assertEqual(origin_1_activity_1.state, 'planned')
             result = self.env['mail.test.activity'].search([('activity_state', '=', 'today')])
             self.assertNotIn(origin_1, result, 'The activity state miss calculated during the search')
+
+    @mute_logger('odoo.addons.mail.models.mail_mail')
+    def test_my_activity_flow_employee(self):
+        Activity = self.env['mail.activity']
+        Activity.create({
+            'activity_type_id': self.env.ref('test_mail.mail_act_test_todo').id,
+            'res_model_id': self.env.ref('test_mail.model_mail_test_activity').id,
+            'res_id': self.test_record.id,
+            'user_id': self.user_admin.id,
+        })
+        Activity.create({
+            'activity_type_id': self.env.ref('test_mail.mail_act_test_call').id,
+            'date_deadline': date.today() + relativedelta(days=1),
+            'res_model_id': self.env.ref('test_mail.model_mail_test_activity').id,
+            'res_id': self.test_record.id,
+            'user_id': self.user_employee.id,
+        })
+
+        test_record_1 = self.env['mail.test.activity'].with_context(self._test_context).create({'name': 'Test 1'})
+        Activity.create({
+            'activity_type_id': self.env.ref('test_mail.mail_act_test_todo').id,
+            'res_model_id': self.env.ref('test_mail.model_mail_test_activity').id,
+            'res_id': test_record_1.id,
+            'user_id': self.user_employee.id,
+        })
+        with self.with_user('employee'):
+            record = self.env['mail.test.activity'].search([('my_activity_date_deadline', '=', datetime.today())])
+            self.assertEqual(test_record_1, record)
