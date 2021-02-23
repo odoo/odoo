4746523a2c03c3b5c2ff262e67ae8b9f8afafa12
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2021-02-23 07:31:37 +0000

    [FIX] hr_recruitment: display recruitment analysis when no data
    Have an empty database
    Install Recruitment
    Employee>Departments>Administration(3 dots)>Recruitments
    
    Missing record message raise
    This occur because of an access to a record that may not exists
    
    opw-2431929
    
    closes odoo/odoo#66764
    
    X-original-commit: 911aec8465a4a796297a465e96b3033d1c415628
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>
    Signed-off-by: agr-odoo <agr-odoo@users.noreply.github.com>

--- addons/hr_recruitment/models/hr_recruitment.py
+++ addons/hr_recruitment/models/hr_recruitment.py
@@ -309,7 +309,7 @@ class Applicant(models.Model):
         return res
 
     def get_empty_list_help(self, help):
-        if 'active_id' in  self.env.context:
+        if 'active_id' in self.env.context and self.env.context.get('active_model') == 'hr.job':
             alias_id = self.env['hr.job'].browse(self.env.context['active_id']).alias_id
         else:
             alias_id = False
