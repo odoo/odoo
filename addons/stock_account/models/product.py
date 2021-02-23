f45110e9b59c8faefcd9e0a0c428a58b93e42a5d
Goffin Simon <sig@odoo.com>
2021-02-23 08:58:37 +0000

    [FIX] stock_account: Update standard price from import
    Fine tuning of c2f834d9528576a77fb21902a2338b88ddf361d5
    
    PS: The diff value was always 0 when computing:
    
    diff = new_price - product.standard_price
    
    because the standard_price was already updated
    
    opw:2424292
    
    closes odoo/odoo#66655
    
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -182,12 +182,12 @@ class ProductProduct(models.Model):
         return vals
 
     def write(self, vals):
-        res = super(ProductProduct, self).write(vals)
         if self.env.context.get('import_file') and not self.env.context.get('import_standard_price'):
             if 'standard_price' in vals:
                 for product in self:
                     counterpart_account_id = product.property_account_expense_id.id or product.categ_id.property_account_expense_categ_id.id
                     product.with_context(import_standard_price=True)._change_standard_price(vals['standard_price'], counterpart_account_id)
+        res = super(ProductProduct, self).write(vals)
         return res
 
     def _change_standard_price(self, new_price, counterpart_account_id=False):
