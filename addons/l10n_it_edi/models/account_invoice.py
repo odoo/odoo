3c31bceca4b409f06df37aa2fabcb985b87e7ca2
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2021-02-23 10:34:52 +0000

    [FIX] l10n_it_edi: generate xml for invoices with 0% tax only
    Have an invoice with single line and 0% tax on it
    As the tax has 0 amount, no related moves are created,
    the e-invoice export will fail the formal compliance check as it is
    missing a section
    
    opw-2461496
    
    closes odoo/odoo#66960
    
    X-original-commit: 71cc7212b1c1abad5d37e29f3a1f33f6e6cd2a81
    Signed-off-by: agr-odoo <agr-odoo@users.noreply.github.com>

--- addons/l10n_it_edi/models/account_invoice.py
+++ addons/l10n_it_edi/models/account_invoice.py
@@ -223,6 +223,13 @@ class AccountMove(models.Model):
         pdf = base64.b64encode(pdf)
         pdf_name = re.sub(r'\W+', '', self.name) + '.pdf'
 
+        # tax map for 0% taxes which have no tax_line_id
+        tax_map = dict()
+        for line in self.line_ids:
+            for tax in line.tax_ids:
+                if tax.amount == 0.0:
+                    tax_map[tax] = tax_map.get(tax, 0.0) + line.price_subtotal
+
         # Create file content.
         template_values = {
             'record': self,
@@ -240,6 +247,7 @@ class AccountMove(models.Model):
             'document_type': document_type,
             'pdf': pdf,
             'pdf_name': pdf_name,
+            'tax_map': tax_map,
         }
         content = self.env.ref('l10n_it_edi.account_invoice_it_FatturaPA_export')._render(template_values)
         return content
