abfe38b09798ff5f671ca2ea729917e2120b9b89
Christophe Monniez <moc@odoo.com>
2021-02-23 13:24:08 +0000

    [FIX] tests: test support for modules without `__init__.py`
    Now that a test exists to enforce `__init__.py` in odoo module, we must
    ensure that we keep the feature.
    
    For that reason, a white-list system is added in `test_dunderinit` and a
    test is added in order to be sure that a module without `__init__.py` can
    be exists.
    
    This this is made of a fake module `test_data_module` without
     `__init__.py` and a test module which ensure the fake module was
    installed.
    
    closes odoo/odoo#66651
    
    Related: odoo/enterprise#16596
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

--- odoo/addons/test_lint/tests/test_dunderinit.py
+++ odoo/addons/test_lint/tests/test_dunderinit.py
@@ -9,13 +9,15 @@ from . import lint_case
 
 _logger = logging.getLogger(__name__)
 
+# whitelist that allow data modules only
+WHITELIST = ['test_data_module', ]
 
 class TestDunderinit(lint_case.LintCase):
 
     def test_dunderinit(self):
         """ Test that __init__.py exists in Odoo modules, otherwise they won't get packaged"""
 
-        modules_list = get_modules()
+        modules_list = [mod for mod in get_modules() if mod not in WHITELIST]
         for mod in modules_list:
             dunderinit_path = Path(get_module_path(mod)) / '__init__.py'
             self.assertTrue(dunderinit_path.is_file(), "Missing `__init__.py ` in module %s" % mod)
