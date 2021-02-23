65aa95493c933501491b061e7801d3b79389240e
Christophe Monniez <moc@odoo.com>
2021-02-23 13:24:08 +0000

    [FIX] tests: test support for modules without `__init__.py`
    Now that a test exists to enforce `__init__.py` in odoo module, we must
    ensure that we keep the feature.
    
    For that reason, a white-list system is added in `test_dunderinit` and a
    test is added in order to be sure that a module without `__init__.py` can
    be exists.
    
    This this is made of a fake module `test_data_module` without
     `__init__.py` and a test module which ensure the fake module was
    installed.
    
    closes odoo/odoo#66735
    
    X-original-commit: 506755ef8ced187c0f5cd2fa600e7bb1e1fc0fe3
    Related: odoo/enterprise#16623
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

---
+++ odoo/addons/test_data_module_install/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'test installation of data module',
+    'description': 'Test data module (see test_data_module) installation',
+    'version': '0.0.1',
+    'category': 'Hidden/Tests',
+    'sequence': 10,
+}
