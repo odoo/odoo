7c48f260fbdeda265c7edaaab3fbd540a4d5892d
Francois (fge) <fge@odoo.com>
2021-02-26 14:07:57 +0000

    [FIX] base: @odoo-module alias ending with **/
    When you define an @odoo-module with an alias without any space between
    the alias and the **/, the alias contains the **/.
    The purpose of this commit is to no longer add the **/ to the alias.
    
    Example:
    /** @odoo-module alias=web.base**/
    
    Before:
      odoo.define(`web.base**/`, function(require) {
    After:
      odoo.define(`web.base`, function(require) {
    
    closes odoo/odoo#66929
    
    Signed-off-by: Géry Debongnie (ged) <ged@openerp.com>

--- odoo/addons/test_assetsbundle/tests/test_js_transpiler_regex.py
+++ odoo/addons/test_assetsbundle/tests/test_js_transpiler_regex.py
@@ -19,17 +19,21 @@ class TestJsTranspiler(TransactionCase):
             '// @odoo-module alias=web.test',
             '/* @odoo-module  alias=web.test',
             '/** @odoo-module  alias=web.test',
+            '/** @odoo-module  alias=web.test**/',
             '/* @odoo-module  alias=web.test ',
             '/* @odoo-module alias=web.test default=false',
             '/* @odoo-module alias=web.test default=false ',
+            '/* @odoo-module alias=web.test default=false**/',
         ]
 
         for case in cases:
             assert ODOO_MODULE_RE.match(case), "URL_RE is failing... >%s<" % case
             if "alias" in case:
                 assert ODOO_MODULE_RE.match(case).groupdict().get('alias'), "URL_RE is failing for alias... >%s<" % case
+                assert ODOO_MODULE_RE.match(case).groupdict().get('alias') == "web.test", "URL_RE does not get the right alias for ... >%s<" % case
             if "default" in case:
                 assert ODOO_MODULE_RE.match(case).groupdict().get('default'), "URL_RE is failing for default... >%s<" % case
+                assert ODOO_MODULE_RE.match(case).groupdict().get('default') == "false", "URL_RE does not get the right default for ... >%s<" % case
 
     def test_incorrect_ODOO_MODULE_RE(self):
         cases = [
