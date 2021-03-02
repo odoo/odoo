645228f15fbeb7f84436b58eb194e3220068947e
Nikunj Ladava <nla@odoo.com>
2021-03-02 17:47:46 +0530

    [IMP] crm: propagate partner's company_name to lead partner_name
    When setting a partner on a lead if we select the individual customer that
    is not a company with company_name being set, propagate it to partner_name
    on lead.
    
    Task ID-2446969
    PR #65846

--- addons/crm/tests/test_crm_lead.py
+++ addons/crm/tests/test_crm_lead.py
@@ -139,6 +139,28 @@ class TestCRMLead(TestCrmCommon):
         self.assertEqual(lead.state_id, empty_partner.state_id, "State should be sync from the Partner")
         self.assertEqual(lead.country_id, empty_partner.country_id, "Country should be sync from the Partner")
 
+    def test_crm_lead_creation_partner_company(self):
+        """ Test lead / partner synchronization involving company details """
+        # Test that partner_name (company name) is the partner name if partner is company
+        lead = self.env['crm.lead'].create({
+            'name': 'TestLead',
+            'partner_id': self.contact_company.id,
+        })
+        self.assertEqual(lead.contact_name, False,
+                         "Lead contact name should be Falsy when dealing with companies")
+        self.assertEqual(lead.partner_name, self.contact_company.name,
+                         "Lead company name should be set to partner name if partner is a company")
+        # Test that partner_name (company name) is the partner company name if partner is an individual
+        self.contact_company.write({'is_company': False})
+        lead = self.env['crm.lead'].create({
+            'name': 'TestLead',
+            'partner_id': self.contact_company.id,
+        })
+        self.assertEqual(lead.contact_name, self.contact_company.name,
+                         "Lead contact name should be set to partner name if partner is not a company")
+        self.assertEqual(lead.partner_name, self.contact_company.company_name,
+                         "Lead company name should be set to company name if partner is not a company")
+
     def test_crm_lead_creation_partner_no_address(self):
         """ Test that an empty address on partner does not void its lead values """
         empty_partner = self.env['res.partner'].create({
