1e00857fe70b51439b15c27457c4b46cd208f551
Jeremy Kersten <jke@odoo.com>
2021-03-02 16:05:51 +0100

    [FIX] website_sale: we don't support rounding uom in ecommerce
    We only support int, following #c9edca7896e3096289936f5399a82f08a59b60ce
    
    opw-2456796
    
    X-original-commit: a05a34366f6dc8c70b39c15c3441beaf6bbb72d2

--- addons/website_sale/models/sale_order.py
+++ addons/website_sale/models/sale_order.py
@@ -149,12 +149,12 @@ class SaleOrder(models.Model):
 
         try:
             if add_qty:
-                add_qty = float(add_qty)
+                add_qty = int(add_qty)
         except ValueError:
             add_qty = 1
         try:
             if set_qty:
-                set_qty = float(set_qty)
+                set_qty = int(set_qty)
         except ValueError:
             set_qty = 0
         quantity = 0
