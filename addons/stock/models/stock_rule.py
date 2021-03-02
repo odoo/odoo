d91343f9f6826185e66c52e835addb5632d71820
William Henrotin <whe@odoo.com>
2021-03-02 16:23:01 +0000

    [FIX] stock: pass on change from move to move line
    The run_push() method may change the location_dest_id of the stock move
    but not the associated stock move line. That can produce a
    de-synchronisation between stock move and stock quants as those are
    updated by the stock move lines.
    This commit ensures the location_dest_id of the stock move is also set
    on the move lines in run_push()
    
    opw : 2427301
    
    closes odoo/odoo#67451
    
    X-original-commit: 7dafde71c0591be74f9d433976243342b1b3e635
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>
    Signed-off-by: William Henrotin <Whenrow@users.noreply.github.com>

--- addons/stock/models/stock_rule.py
+++ addons/stock/models/stock_rule.py
@@ -169,6 +169,10 @@ class StockRule(models.Model):
         if self.auto == 'transparent':
             old_dest_location = move.location_dest_id
             move.write({'date': new_date, 'location_dest_id': self.location_id.id})
+            # make sure the location_dest_id is consistent with the move line location dest
+            if move.move_line_ids:
+                move.move_line_ids.location_dest_id = move.location_dest_id._get_putaway_strategy(move.product_id) or move.location_dest_id
+
             # avoid looping if a push rule is not well configured; otherwise call again push_apply to see if a next step is defined
             if self.location_id != old_dest_location:
                 # TDE FIXME: should probably be done in the move model IMO
