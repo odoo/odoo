e5215adea16c1822711a96cc740f752e91d1909b
Kevin Baptiste <kba@odoo.com>
2021-03-03 15:18:05 +0100

    wip

--- addons/hr_holidays_attendance/models/hr_leave_type.py
+++ addons/hr_holidays_attendance/models/hr_leave_type.py
@@ -11,3 +11,12 @@ class HRLeaveType(models.Model):
     overtime_deductible = fields.Boolean("Deduct Extra Hours", default=False,
                                          help="""Once a time off of this type is approved, extra hours in attendances
                                          will be deducted.""")
+
+    def get_employees_days(self, employee_ids):
+        res = super().get_employees_days(employee_ids)
+        deductibles = self.env['hr.leave.type'].search([('overtime_deductible', '=', True), ('allocation_type', '=', 'no')]).ids
+        for employee_id, allocations in res.items():
+            for allocation_id in allocations:
+                if allocation_id in deductibles:
+                    res[employee_id][allocation_id]['virtual_remaining_leaves'] = self.env['hr.employee'].sudo().browse(employee_id).total_overtime
+        return res
