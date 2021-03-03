2e111db6c58b95e9e46178fe436904599679cb8c
Arnaud (arg) <arg@odoo.com>
2021-03-03 10:21:24 +0100

    m

---
+++ addons/website_sale_stock_wishlist/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Sale Stock Wishlist",
+    'summary': """
+        Bridge module for website_sale_stock and website_sale_wishlist""",
+    'description': """
+         Bridge module to advise shopper's to add product to their wishlist if stock is empty and wishlist enabled""",
+    'category': 'Hidden',
+    'version': '0.1',
+    'auto_install': True,
+
+    'depends': [
+        'website_sale_stock',
+        'website_sale_wishlist'
+    ],
+
+    'data': [
+        'views/website_sale_stock_wishlist_template.xml',
+    ],
+}
