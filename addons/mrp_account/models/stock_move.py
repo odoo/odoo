4d4e122a56b9d5bf2d97c6a3e9afe066a848b19b
yhu-odoo <yhu@odoo.com>
2021-03-05 10:56:16 +0000

    [IMP](stock,mrp)_account: use location valuation account
    Previous fix f056254d1c5153467b10d9581214ff6d1348be76 didn't take the
    situation when production location has its own valuation accounts into
    consideration. Fix it in this commit.
    
    opw-2476417
    
    closes odoo/odoo#71064
    
    X-original-commit: 60e83f03b871605b7aa281503269f66ac3ea940b
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>
    Signed-off-by: Yuchen Huang <yhu-odoo@users.noreply.github.com>

--- addons/mrp_account/models/stock_move.py
+++ addons/mrp_account/models/stock_move.py
@@ -7,6 +7,18 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def _is_returned(self, valued_type):
-        if self.unbuild_id and self.unbuild_id.mo_id:   # unbuilding a MO
+        if self.unbuild_id:
             return True
         return super()._is_returned(valued_type)
+
+    def _get_src_account(self, accounts_data):
+        if not self.unbuild_id:
+            return super()._get_src_account(accounts_data)
+        else:
+            return self.location_dest_id.valuation_out_account_id.id or accounts_data['stock_input'].id
+
+    def _get_dest_account(self, accounts_data):
+        if not self.unbuild_id:
+            return super()._get_dest_account(accounts_data)
+        else:
+            return self.location_id.valuation_in_account_id.id or accounts_data['stock_output'].id
