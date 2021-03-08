e1c3b507ec01f6f152a1bfb9ec174982271b407c
Florent de Labarre <florent.mirieu@gmail.com>
2021-03-08 13:31:14 +0100

    [FIX] website_sale : return in loop

--- addons/website_sale/models/sale_order.py
+++ addons/website_sale/models/sale_order.py
@@ -289,19 +289,21 @@ class SaleOrder(models.Model):
 
     def _cart_accessories(self):
         """ Suggest accessories based on 'Accessory Products' of products in cart """
-        for order in self:
-            products = order.website_order_line.mapped('product_id')
-            accessory_products = self.env['product.product']
-            for line in order.website_order_line.filtered(lambda l: l.product_id):
-                combination = line.product_id.product_template_attribute_value_ids + line.product_no_variant_attribute_value_ids
-                accessory_products |= line.product_id.accessory_product_ids.filtered(lambda product:
-                    product.website_published and
-                    product not in products and
-                    product._is_variant_possible(parent_combination=combination) and
-                    (product.company_id == line.company_id or not product.company_id)
-                )
+        if not self:
+            return
+        self.ensure_one()
+        products = self.website_order_line.mapped('product_id')
+        accessory_products = self.env['product.product']
+        for line in self.website_order_line.filtered(lambda l: l.product_id):
+            combination = line.product_id.product_template_attribute_value_ids + line.product_no_variant_attribute_value_ids
+            accessory_products |= line.product_id.accessory_product_ids.filtered(lambda product:
+                product.website_published and
+                product not in products and
+                product._is_variant_possible(parent_combination=combination) and
+                (product.company_id == line.company_id or not product.company_id)
+            )
 
-            return random.sample(accessory_products, len(accessory_products))
+        return random.sample(accessory_products, len(accessory_products))
 
     def action_recovery_email_send(self):
         for order in self:
