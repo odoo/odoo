3fe9ba9e79cb1687fc1b8d7450d17711e34574d7
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:12:53 +0100

    [REF] assets: pos_mercury

--- addons/pos_mercury/__manifest__.py
+++ addons/pos_mercury/__manifest__.py
@@ -25,7 +25,6 @@ following:
     'data': [
         'data/pos_mercury_data.xml',
         'security/ir.model.access.csv',
-        'views/pos_mercury_templates.xml',
         'views/pos_mercury_views.xml',
         'views/pos_mercury_transaction_templates.xml',
         'views/pos_config_setting_views.xml',
@@ -33,11 +32,29 @@ following:
     'demo': [
         'data/pos_mercury_demo.xml',
     ],
-    'qweb': [
-        'static/src/xml/OrderReceipt.xml',
-        'static/src/xml/PaymentScreenPaymentLines.xml',
-        'static/src/xml/PaymentTransactionPopup.xml',
-    ],
     'installable': True,
     'auto_install': False,
+    'assets': {
+        'point_of_sale.assets': [
+            # inside .
+            'pos_mercury/static/src/js/pos_mercury.js',
+            # inside .
+            'pos_mercury/static/src/js/OrderReceipt.js',
+            # inside .
+            'pos_mercury/static/src/js/PaymentScreen.js',
+            # inside .
+            'pos_mercury/static/src/js/PaymentScreenPaymentLines.js',
+            # inside .
+            'pos_mercury/static/src/js/PaymentTransactionPopup.js',
+            # inside .
+            'pos_mercury/static/src/js/ProductScreen.js',
+            # inside .
+            'pos_mercury/static/src/css/pos_mercury.css',
+        ],
+        'web.assets_qweb': [
+            'pos_mercury/static/src/xml/OrderReceipt.xml',
+            'pos_mercury/static/src/xml/PaymentScreenPaymentLines.xml',
+            'pos_mercury/static/src/xml/PaymentTransactionPopup.xml',
+        ],
+    }
 }
