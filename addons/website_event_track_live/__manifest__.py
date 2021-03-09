8e401bf771f9f5e272079ab71fab51fad5741217
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:19:21 +0100

    [REF] assets: website_event_track_live

--- addons/website_event_track_live/__manifest__.py
+++ addons/website_event_track_live/__manifest__.py
@@ -14,7 +14,6 @@
         'website_event_track',
     ],
     'data': [
-        'views/assets.xml',
         'views/event_track_templates_list.xml',
         'views/event_track_templates_page.xml',
         'views/event_track_views.xml',
@@ -22,9 +21,19 @@
     'demo': [
         'data/event_track_demo.xml'
     ],
-    'qweb': [
-        'static/src/xml/website_event_track_live_templates.xml'
-    ],
     'application': False,
     'installable': True,
+    'assets': {
+        'web.assets_frontend': [
+            # after //link[last()]
+            'website_event_track_live/static/src/scss/website_event_track_live.scss',
+            # after //script[last()]
+            'website_event_track_live/static/src/js/website_event_track_suggestion.js',
+            # after //script[last()]
+            'website_event_track_live/static/src/js/website_event_track_live.js',
+        ],
+        'web.assets_qweb': [
+            'website_event_track_live/static/src/xml/website_event_track_live_templates.xml',
+        ],
+    }
 }
