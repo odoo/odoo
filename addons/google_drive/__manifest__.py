e338d951fd3ec5d6b70b8c48e47e8e417e97ba18
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:03:06 +0100

    [REF] assets: google_drive

--- addons/google_drive/__manifest__.py
+++ addons/google_drive/__manifest__.py
@@ -11,10 +11,8 @@
         'security/ir.model.access.csv',
         'data/google_drive_data.xml',
         'views/google_drive_views.xml',
-        'views/google_drive_templates.xml',
         'views/res_config_settings_views.xml',
     ],
-    'qweb': ['static/src/xml/*.xml'],
     'demo': [
         'data/google_drive_demo.xml'
     ],
@@ -25,5 +23,22 @@ Integrate google document to Odoo record.
 
 This module allows you to integrate google documents to any of your Odoo record quickly and easily using OAuth 2.0 for Installed Applications,
 You can configure your google Authorization Code from Settings > Configuration > General Settings by clicking on "Generate Google Authorization Code"
-"""
+""",
+    'assets': {
+        'web.assets_backend': [
+            # inside .
+            'google_drive/static/src/css/google_drive.css',
+            # inside .
+            'google_drive/static/src/js/gdrive.js',
+        ],
+        'web.qunit_suite_tests': [
+            # inside .
+            'google_drive/static/tests/mock_server.js',
+            # inside .
+            'google_drive/static/tests/gdrive_test.js',
+        ],
+        'web.assets_qweb': [
+            'google_drive/static/src/xml/*.xml',
+        ],
+    }
 }
