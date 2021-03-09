60ac2d1ac365ca8749fa4c8f74f8dafb13de5f7e
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:00:36 +0100

    [REF] assets: barcodes

--- addons/barcodes/__manifest__.py
+++ addons/barcodes/__manifest__.py
@@ -8,9 +8,28 @@
         'data/barcodes_data.xml',
         'views/barcodes_view.xml',
         'security/ir.model.access.csv',
-        'views/barcodes_templates.xml',
-    ],
+        ],
     'installable': True,
     'auto_install': False,
     'post_init_hook': '_assign_default_nomeclature_id',
+    'assets': {
+        'web.assets_backend': [
+            # inside .
+            'barcodes/static/src/js/barcode_parser.js',
+            # inside .
+            'barcodes/static/src/js/barcode_events.js',
+            # inside .
+            'barcodes/static/src/js/barcode_form_view.js',
+            # inside .
+            'barcodes/static/src/js/barcode_field.js',
+        ],
+        'web.qunit_suite_tests': [
+            # inside .
+            'barcodes/static/tests/barcode_tests.js',
+        ],
+        'web.qunit_mobile_suite_tests': [
+            # inside .
+            'barcodes/static/tests/barcode_mobile_tests.js',
+        ],
+    }
 }
