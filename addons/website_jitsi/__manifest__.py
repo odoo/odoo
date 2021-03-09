4445cbcff64615639eec59d2cb4c5e5de4f0150c
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:19:52 +0100

    [REF] assets: website_jitsi

--- addons/website_jitsi/__manifest__.py
+++ addons/website_jitsi/__manifest__.py
@@ -13,11 +13,18 @@
         "website"
     ],
     "data": [
-        'views/assets.xml',
         'views/chat_room_templates.xml',
         'views/chat_room_views.xml',
         'views/res_config_settings.xml',
         'security/ir.model.access.csv',
     ],
     'application': False,
+    'assets': {
+        'web.assets_frontend': [
+            # after //link[last()]
+            'website_jitsi/static/src/css/chat_room.css',
+            # after //script[last()]
+            'website_jitsi/static/src/js/chat_room.js',
+        ],
+    }
 }
