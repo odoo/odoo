ce0a3b8d4b563fc6b99dc2b62ab1858b70102b30
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:12:43 +0100

    [REF] assets: pos_hr

--- addons/pos_hr/__manifest__.py
+++ addons/pos_hr/__manifest__.py
@@ -15,16 +15,39 @@ The actual till still requires one user but an unlimited number of employees can
 
     'data': [
         'views/pos_config.xml',
-        'views/point_of_sale.xml',
         'views/pos_order_view.xml',
         'views/pos_order_report_view.xml',
     ],
     'installable': True,
     'auto_install': True,
-    'qweb': [
-        'static/src/xml/HeaderLockButton.xml',
-        'static/src/xml/Chrome.xml',
-        'static/src/xml/CashierName.xml',
-        'static/src/xml/LoginScreen.xml',
-    ],
+    'assets': {
+        'point_of_sale.assets': [
+            # inside .
+            'pos_hr/static/src/css/pos.css',
+            # inside .
+            'pos_hr/static/src/js/models.js',
+            # inside .
+            'pos_hr/static/src/js/useSelectEmployee.js',
+            # inside .
+            'pos_hr/static/src/js/Chrome.js',
+            # inside .
+            'pos_hr/static/src/js/HeaderLockButton.js',
+            # inside .
+            'pos_hr/static/src/js/CashierName.js',
+            # inside .
+            'pos_hr/static/src/js/LoginScreen.js',
+        ],
+        'web.assets_tests': [
+            # inside .
+            'pos_hr/static/tests/tours/PosHrTourMethods.js',
+            # inside .
+            'pos_hr/static/tests/tours/PosHrTour.js',
+        ],
+        'web.assets_qweb': [
+            'pos_hr/static/src/xml/HeaderLockButton.xml',
+            'pos_hr/static/src/xml/Chrome.xml',
+            'pos_hr/static/src/xml/CashierName.xml',
+            'pos_hr/static/src/xml/LoginScreen.xml',
+        ],
+    }
 }
