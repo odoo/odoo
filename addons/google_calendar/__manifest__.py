cd90148456d89a79abc625c08b42f466e73fffe2
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:03:03 +0100

    [REF] assets: google_calendar

--- addons/google_calendar/__manifest__.py
+++ addons/google_calendar/__manifest__.py
@@ -7,7 +7,6 @@
     'category': 'Productivity',
     'description': "",
     'depends': ['google_account', 'calendar'],
-    'qweb': ['static/src/xml/*.xml'],
     'data': [
         'data/google_calendar_data.xml',
         'security/ir.model.access.csv',
@@ -15,9 +14,31 @@
         'views/res_config_settings_views.xml',
         'views/res_users_views.xml',
         'views/google_calendar_views.xml',
-        'views/google_calendar_templates.xml',
-    ],
+        ],
     'demo': [],
     'installable': True,
     'auto_install': False,
+    'assets': {
+        'web.assets_backend': [
+            # inside .
+            'google_calendar/static/src/js/google_calendar_popover.js',
+            # inside .
+            'google_calendar/static/src/js/google_calendar.js',
+            # after link[last()]
+            'google_calendar/static/src/scss/google_calendar.scss',
+        ],
+        'web.qunit_suite_tests': [
+            # inside .
+            'google_calendar/static/tests/mock_server.js',
+            # inside .
+            'google_calendar/static/tests/google_calendar_tests.js',
+        ],
+        'web.qunit_mobile_suite_tests': [
+            # inside .
+            'google_calendar/static/tests/mock_server.js',
+        ],
+        'web.assets_qweb': [
+            'google_calendar/static/src/xml/*.xml',
+        ],
+    }
 }
