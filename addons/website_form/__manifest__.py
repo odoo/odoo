15cb4a604722b7867a881e46e15aca7018afe19e
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:19:31 +0100

    [REF] assets: website_form

--- addons/website_form/__manifest__.py
+++ addons/website_form/__manifest__.py
@@ -9,8 +9,8 @@
     'version': '1.0',
     'depends': ['website', 'mail', 'google_recaptcha'],
     'data': [
+        'data/ir_asset.xml',
         'data/mail_mail_data.xml',
-        'views/assets.xml',
         'views/ir_model_views.xml',
         'views/snippets/snippets.xml',
         'views/snippets/s_website_form.xml',
@@ -18,4 +18,24 @@
     ],
     'installable': True,
     'auto_install': True,
+    'assets': {
+        'web.assets_frontend': [
+            # after //link[last()]
+            'website_form/static/src/snippets/s_website_form/001.scss',
+            # after //script[last()]
+            'website_form/static/src/snippets/s_website_form/000.js',
+        ],
+        'website.assets_editor': [
+            # inside .
+            'website_form/static/src/scss/wysiwyg_snippets.scss',
+            # inside .
+            'website_form/static/src/snippets/s_website_form/options.js',
+            # inside .
+            'website_form/static/src/js/website_form_editor_registry.js',
+        ],
+        'web.assets_tests': [
+            # inside .
+            'website_form/static/tests/tours/website_form_editor.js',
+        ],
+    }
 }
