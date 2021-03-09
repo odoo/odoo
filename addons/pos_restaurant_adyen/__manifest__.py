9fb05e6b9060d487bcb4bdd0bbb6b8e43ee54496
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:12:58 +0100

    [REF] assets: pos_restaurant_adyen

--- addons/pos_restaurant_adyen/__manifest__.py
+++ addons/pos_restaurant_adyen/__manifest__.py
@@ -11,7 +11,14 @@
     'depends': ['pos_adyen', 'pos_restaurant', 'payment_adyen'],
     'data': [
         'views/pos_payment_method_views.xml',
-        'views/point_of_sale_assets.xml',
-    ],
+        ],
     'auto-install': True,
+    'assets': {
+        'point_of_sale.assets': [
+            # inside .
+            'pos_restaurant_adyen/static/src/js/models.js',
+            # inside .
+            'pos_restaurant_adyen/static/src/js/payment_adyen.js',
+        ],
+    }
 }
