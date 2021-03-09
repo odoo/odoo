8f12d7dfd92a0d43dd0bfcaf5f0c139cc3199579
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:13:20 +0100

    [REF] assets: project

--- addons/project/__manifest__.py
+++ addons/project/__manifest__.py
@@ -32,7 +32,6 @@
         'views/res_partner_views.xml',
         'views/res_config_settings_views.xml',
         'views/mail_activity_views.xml',
-        'views/project_assets.xml',
         'views/project_portal_templates.xml',
         'data/ir_cron_data.xml',
         'data/mail_data.xml',
@@ -41,10 +40,40 @@
         'wizard/project_task_type_delete_views.xml',
     ],
     'demo': ['data/project_demo.xml'],
-    'qweb': ['static/src/xml/project_templates.xml'],
     'test': [
     ],
     'installable': True,
     'auto_install': False,
     'application': True,
+    'assets': {
+        'web.assets_backend': [
+            # inside .
+            'project/static/src/css/project.css',
+            # inside .
+            'project/static/src/js/project_form.js',
+            # inside .
+            'project/static/src/js/project_kanban.js',
+            # inside .
+            'project/static/src/js/project_list.js',
+            # inside .
+            'project/static/src/js/project_rating_reporting.js',
+            # inside .
+            'project/static/src/js/project_task_kanban_examples.js',
+            # inside .
+            'project/static/src/js/tours/project.js',
+            # inside .
+            'project/static/src/js/project_calendar.js',
+            # inside .
+            'project/static/src/scss/project_dashboard.scss',
+        ],
+        'web.assets_frontend': [
+            # inside .
+            'project/static/src/scss/portal_rating.scss',
+            # inside .
+            'project/static/src/js/portal_rating.js',
+        ],
+        'web.assets_qweb': [
+            'project/static/src/xml/project_templates.xml',
+        ],
+    }
 }
