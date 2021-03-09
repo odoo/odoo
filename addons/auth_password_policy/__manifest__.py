2a1532c0dc2e8748fed9edddb4c6cbc0e1816672
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:00:25 +0100

    [REF] assets: auth_password_policy

--- addons/auth_password_policy/__manifest__.py
+++ addons/auth_password_policy/__manifest__.py
@@ -5,8 +5,21 @@
     'depends': ['base_setup', 'web'],
     'data': [
         'data/defaults.xml',
-        'views/assets.xml',
         'views/res_users.xml',
         'views/res_config_settings_views.xml',
-    ]
+    ],
+    'assets': {
+        'web.assets_backend': [
+            # inside .
+            'auth_password_policy/static/src/js/password_field.js',
+            # inside .
+            'auth_password_policy/static/src/js/change_password.js',
+        ],
+        'web.assets_common': [
+            # inside .
+            'auth_password_policy/static/src/js/password_gauge.js',
+            # inside .
+            'auth_password_policy/static/src/css/password_field.css',
+        ],
+    }
 }
