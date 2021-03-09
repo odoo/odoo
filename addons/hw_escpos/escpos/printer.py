a6c738321cd02fe19fae1a68cba69a3946f34e53
Danny <ddejong@therp.nl>
2021-03-09 11:13:59 +0100

    [FIX] Fixed a usb timeout bug for the ESCPOS printer in hw_escpos. The code would hang indefinitally if the usb device doesn't respond.

--- addons/hw_escpos/escpos/printer.py
+++ addons/hw_escpos/escpos/printer.py
@@ -102,7 +102,7 @@ class Usb(Escpos):
             maxiterate += 1
             if maxiterate > 10000:
                 raise NoStatusError()
-            r = self.device.read(self.in_ep, 20, self.interface).tolist()
+            r = self.device.read(self.in_ep, 20).tolist()
             while len(r):
                 rep = r.pop()
         return rep
