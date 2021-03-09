eaf479579168a55086c9e044c280c9b7ef83ce2d
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:00:51 +0100

    [REF] assets: base_iban

--- addons/base_iban/__manifest__.py
+++ addons/base_iban/__manifest__.py
@@ -12,9 +12,20 @@ with a single statement.
     """,
     'depends': ['account', 'web'],
     'data': [
-        'views/templates.xml',
         'views/partner_view.xml',
         'views/setup_wizards_view.xml'
     ],
     'demo': ['data/res_partner_bank_demo.xml'],
+    'assets': {
+        'web.assets_backend': [
+            # inside .
+            'base_iban/static/src/js/iban_widget.js',
+            # inside .
+            'base_iban/static/src/scss/iban_widget_view.scss',
+        ],
+        'web.qunit_suite_tests': [
+            # inside .
+            'base_iban/static/src/tests/iban_widget_test.js',
+        ],
+    }
 }
