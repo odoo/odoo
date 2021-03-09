f58ec9732010c9a93cfd03669f60cea53ab1e565
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:00:27 +0100

    [REF] assets: auth_password_policy_portal

--- addons/auth_password_policy_portal/__manifest__.py
+++ addons/auth_password_policy_portal/__manifest__.py
@@ -3,5 +3,11 @@
     'depends': ['auth_password_policy', 'portal'],
     'category': 'Tools',
     'auto_install': True,
-    'data': ['views/templates.xml']
+    'data': ['views/templates.xml'],
+    'assets': {
+        'web.assets_frontend': [
+            # inside .
+            'auth_password_policy_portal/static/src/scss/portal_policy.scss',
+        ],
+    }
 }
