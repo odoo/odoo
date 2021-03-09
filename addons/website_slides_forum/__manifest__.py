4d93553aca95298091cda8a1a1f6435f34d9d989
Simon Genin (ges) <ges@odoo.com>
2021-03-09 14:20:47 +0100

    [REF] assets: website_slides_forum

--- addons/website_slides_forum/__manifest__.py
+++ addons/website_slides_forum/__manifest__.py
@@ -17,7 +17,6 @@
         'views/forum_views.xml',
         'views/slide_channel_views.xml',
         'views/website_slides_menu_views.xml',
-        'views/assets.xml',
         'views/website_slides_templates.xml',
         'views/website_slides_forum_templates.xml'
     ],
@@ -25,4 +24,10 @@
         'data/slide_channel_demo.xml',
     ],
     'auto_install': True,
+    'assets': {
+        'web.assets_frontend': [
+            # after //script[last()]
+            'website_slides_forum/static/src/js/website_slides_forum.editor.js',
+        ],
+    }
 }
