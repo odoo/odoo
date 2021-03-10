4ee97ea4c6d6b2b8001a7e29602d7c6007b74ac3
Hendrik <hendrikwidjaja25@gmail.com>
2021-03-10 17:42:57 +0000

    [FIX] stock_account: Fix shadowed 'product' variable in anglo-saxon lines
    closes odoo/odoo#68373
    
    X-original-commit: bdace2f538718a521926f0c5d073c05c40e98fac
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>
    Signed-off-by: Hendrik <hendrikw25@users.noreply.github.com>

--- addons/stock_account/models/account_move.py
+++ addons/stock_account/models/account_move.py
@@ -186,12 +186,12 @@ class AccountMove(models.Model):
                 continue
 
             products = product or move.mapped('invoice_line_ids.product_id')
-            for product in products:
-                if product.valuation != 'real_time':
+            for prod in products:
+                if prod.valuation != 'real_time':
                     continue
 
                 # We first get the invoices move lines (taking the invoice and the previous ones into account)...
-                product_accounts = product.product_tmpl_id._get_product_accounts()
+                product_accounts = prod.product_tmpl_id._get_product_accounts()
                 if move.is_sale_document():
                     product_interim_account = product_accounts['stock_output']
                 else:
@@ -200,10 +200,10 @@ class AccountMove(models.Model):
                 if product_interim_account.reconcile:
                     # Search for anglo-saxon lines linked to the product in the journal entry.
                     product_account_moves = move.line_ids.filtered(
-                        lambda line: line.product_id == product and line.account_id == product_interim_account and not line.reconciled)
+                        lambda line: line.product_id == prod and line.account_id == product_interim_account and not line.reconciled)
 
                     # Search for anglo-saxon lines linked to the product in the stock moves.
-                    product_stock_moves = stock_moves.filtered(lambda stock_move: stock_move.product_id == product)
+                    product_stock_moves = stock_moves.filtered(lambda stock_move: stock_move.product_id == prod)
                     product_account_moves += product_stock_moves.mapped('account_move_ids.line_ids')\
                         .filtered(lambda line: line.account_id == product_interim_account and not line.reconciled)
 
