dff61a77085d3431eeb0165bac8c5814b53ebb45
qmo-odoo <qmo@odoo.com>
2021-03-17 14:56:18 +0100

    [IMP] calendar: Add tracking on active field
    This commit is needed for the enterprise PR linked below.
    It will be needed to notify followers if a calendar event
    gets cancelled (archived)
    
    LINKS:
    TaskID: #2223519
    Community PR: odoo/odoo#68022
    Enterprise PR:odoo/enterprise#17141

--- addons/calendar/models/calendar_event.py
+++ addons/calendar/models/calendar_event.py
@@ -122,6 +122,7 @@ class Meeting(models.Model):
     # filtering
     active = fields.Boolean(
         'Active', default=True,
+        tracking=True,
         help="If the active field is set to false, it will allow you to hide the event alarm information without removing it.")
     categ_ids = fields.Many2many(
         'calendar.event.type', 'meeting_category_rel', 'event_id', 'type_id', 'Tags')
