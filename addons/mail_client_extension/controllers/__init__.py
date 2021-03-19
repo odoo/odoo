7c4a2d4691b261ae7b2fb1fe4ef9c3a10aa2cce2
nounoubensebia <neb@odoo.com>
2021-03-19 10:32:14 +0000

    [MOV] mail_client_extension: split authentication and main controller code
    This commit splits the code located in the mail_client_extension module into
    two separate files, an "authentication.py" file containing authentication
    related code and a "mail_client_extension.py" file containing everything else.
    
    Purpose is to better organize the code and to avoid having a bloated main
    controller.
    
    This commit does not change anything functionally, it only moves and refactors
    code.
    
    Task-2382870
    
    UPG-PR: https://github.com/odoo/upgrade/pull/2156
    COM-PR: https://github.com/odoo/odoo/pull/66139

--- addons/mail_client_extension/controllers/__init__.py
+++ addons/mail_client_extension/controllers/__init__.py
@@ -1,2 +1,5 @@
-# -*- coding: utf-8 -*
-from . import main
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import authenticate
+from . import mail_plugin
