3c267d2aae5e77bd6010132a8f55d3f1a87812af
Aurélien Warnon <awa@odoo.com>
2021-03-19 09:26:40 +0000

    [IMP] crm_iap_lead: allow ordering crm.iap.lead.industry with a sequence
    This commit simply adds a sequence field to the crm.iap.lead.industry model and
    re-orders the records imported in the CSV file.
    
    This re-ordering is performed to show preferred records to the user, having
    some diversified industry choices at the top that provide interesting results.
    
    Nothing should change functionally.
    
    UPG PR odoo/upgrade#2288
    Task-2471703

--- addons/crm_iap_lead/models/crm_iap_lead_industry.py
+++ addons/crm_iap_lead/models/crm_iap_lead_industry.py
@@ -8,11 +8,13 @@ class CrmIapLeadIndustry(models.Model):
     """ Industry Tags of Acquisition Rules """
     _name = 'crm.iap.lead.industry'
     _description = 'CRM IAP Lead Industry'
+    _order = 'sequence,id'
 
     name = fields.Char(string='Industry', required=True, translate=True)
     reveal_ids = fields.Char(required=True,
         help="The list of reveal_ids for this industry, separated with ','")
     color = fields.Integer(string='Color Index')
+    sequence = fields.Integer('Sequence')
 
     _sql_constraints = [
         ('name_uniq', 'unique (name)', 'Industry name already exists!'),
