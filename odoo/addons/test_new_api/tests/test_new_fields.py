c7ddb795c1afd1d9453a98d63709fb62b4bdf8d4
Raphael Collet <rco@odoo.com>
2021-03-19 11:57:59 +0000

    [REF] core: add method get_currency_field() on Monetary to retrieve it
    This replaces the setup of the attribute 'currency_field', which depends
    on the presence of other fields on the model, and may therefore vary
    from one registry to another.  This is necessary to make monetary fields
    shareable across registries.

--- odoo/addons/test_new_api/tests/test_new_fields.py
+++ odoo/addons/test_new_api/tests/test_new_fields.py
@@ -2042,22 +2042,26 @@ class TestFields(TransactionCaseWithUserDemo):
     def test_93_monetary_related(self):
         """ Check the currency field on related monetary fields. """
         # check base field
-        field = self.env['test_new_api.monetary_base']._fields['amount']
-        self.assertEqual(field.currency_field, 'base_currency_id')
+        model = self.env['test_new_api.monetary_base']
+        field = model._fields['amount']
+        self.assertEqual(field.get_currency_field(model), 'base_currency_id')
 
         # related fields must use the field 'currency_id' or 'x_currency_id'
-        field = self.env['test_new_api.monetary_related']._fields['amount']
+        model = self.env['test_new_api.monetary_related']
+        field = model._fields['amount']
         self.assertEqual(field.related, ('monetary_id', 'amount'))
-        self.assertEqual(field.currency_field, 'currency_id')
+        self.assertEqual(field.get_currency_field(model), 'currency_id')
 
-        field = self.env['test_new_api.monetary_custom']._fields['x_amount']
+        model = self.env['test_new_api.monetary_custom']
+        field = model._fields['x_amount']
         self.assertEqual(field.related, ('monetary_id', 'amount'))
-        self.assertEqual(field.currency_field, 'x_currency_id')
+        self.assertEqual(field.get_currency_field(model), 'x_currency_id')
 
         # inherited field must use the same field as its parent field
-        field = self.env['test_new_api.monetary_inherits']._fields['amount']
+        model = self.env['test_new_api.monetary_inherits']
+        field = model._fields['amount']
         self.assertEqual(field.related, ('monetary_id', 'amount'))
-        self.assertEqual(field.currency_field, 'base_currency_id')
+        self.assertEqual(field.get_currency_field(model), 'base_currency_id')
 
     def test_94_image(self):
         f = io.BytesIO()
