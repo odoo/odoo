7a23a9562093663c7d513b993a073f6cd44827f9
Goffin Simon <sig@odoo.com>
2021-03-24 09:53:59 +0000

    [FIX] purchase: not possible to link to purchase order
    Steps to reproduce the bug:
    
    - Create a bill
    - Click on Auto complete
    
    Bug:
    
    A traceback was raised.
    
    opw:2490125
    
    X-original-commit: 81a8be7d115ffcfac0126c252811381a482bd6a9

--- addons/purchase/report/purchase_bill.py
+++ addons/purchase/report/purchase_bill.py
@@ -52,3 +52,15 @@ class PurchaseBillUnion(models.Model):
             name += ': ' + formatLang(self.env, amount, monetary=True, currency_obj=doc.currency_id)
             result.append((doc.id, name))
         return result
+<<<<<<< HEAD
+=======
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+        domain = []
+        if name:
+            domain = ['|', ('name', operator, name), ('reference', operator, name)]
+        purchase_bills_union_ids = self._search(expression.AND([domain, args]), limit=limit, access_rights_uid=name_get_uid)
+        return purchase_bills_union_ids
+>>>>>>> e8e99ef2d2a... temp
