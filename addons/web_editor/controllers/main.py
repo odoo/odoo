603ace1966a58f9513988b8f793e44aee23d6572
Romain Derie <rde@odoo.com>
2021-03-24 17:25:24 +0000

    [IMP] web_editor: add error support for too big image files
    ValueError is raised when image resolution is too big (over 45e6[1]).
    
    [1]: https://github.com/odoo/odoo/blame/4c91e895a98642f10bd1d2d46819705edc38f638/odoo/tools/image.py#L47
    
    Part of https://github.com/odoo/odoo/pull/65828
    task-2345082
    
    closes odoo/odoo#65828
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/web_editor/controllers/main.py
+++ addons/web_editor/controllers/main.py
@@ -175,6 +175,8 @@ class Web_Editor(http.Controller):
                 # considered as an image by the brower file input, but not
                 # recognized as such by PIL, eg .webp
                 return {'error': format_error_msg}
+            except ValueError as e:
+                return {'error': e.args[0]}
 
         self._clean_context()
         attachment = self._attachment_create(name=name, data=data, res_id=res_id, res_model=res_model)
