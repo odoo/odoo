2369fa7f267e2b96a723c189c97896fc8c2693f7
Gorash <chm@odoo.com>
2021-03-25 17:33:17 +0100

    imp perf => cache for _get_fpos_by_region

--- addons/account/models/partner.py
+++ addons/account/models/partner.py
@@ -6,7 +6,7 @@ import logging
 
 from psycopg2 import sql, DatabaseError
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models, _, tools
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.exceptions import ValidationError
 from odoo.addons.base.models.res_partner import WARNING_MESSAGE, WARNING_HELP
@@ -105,6 +105,7 @@ class AccountFiscalPosition(models.Model):
         zip_to = vals.get('zip_to')
         if zip_from and zip_to:
             vals['zip_from'], vals['zip_to'] = self._convert_zip_values(zip_from, zip_to)
+        self.clear_caches()
         return super(AccountFiscalPosition, self).create(vals)
 
     def write(self, vals):
@@ -113,9 +114,11 @@ class AccountFiscalPosition(models.Model):
         if zip_from or zip_to:
             for rec in self:
                 vals['zip_from'], vals['zip_to'] = self._convert_zip_values(zip_from or rec.zip_from, zip_to or rec.zip_to)
+        self.clear_caches()
         return super(AccountFiscalPosition, self).write(vals)
 
     @api.model
+    @tools.cache('self.env.company.id', 'country_id', 'state_id', 'zipcode', 'vat_required')
     def _get_fpos_by_region(self, country_id=False, state_id=False, zipcode=False, vat_required=False):
         if not country_id:
             return False
