f4318a27eeb630d15c8bbe80cbb37ccbc5f904b7
Barad Mahendra <mba@openerp.com>
2021-03-25 18:37:46 +0530

    [IMP] base,sale_(project): generic improvements for project
    Currently, empty help content are not properly added so in this
    task add the proper help content on several menu and add module
    field service in community.
    
    So in this commit done the below changes:
     - Task form view: change the tooltip of the sale_line_id field
     - Change the empty help content in below menu:
       - reporting > tasks analysis
       - Configuration > Stages
       - Project > Reporting > Project Costs and Revenues
     - base: The industry_fsm module should show up under Apps in community,
    with the usual 'upgrade' button
    
    TaskID: 2465012

--- addons/sale_project/models/project.py
+++ addons/sale_project/models/project.py
@@ -49,8 +49,7 @@ class ProjectTask(models.Model):
     sale_line_id = fields.Many2one(
         'sale.order.line', 'Sales Order Item', domain="[('is_service', '=', True), ('order_partner_id', 'child_of', commercial_partner_id), ('is_expense', '=', False), ('state', 'in', ['sale', 'done'])]",
         compute='_compute_sale_line', recursive=True, store=True, readonly=False, copy=False,
-        help="Sales order item to which the project is linked. Link the timesheet entry to the sales order item defined on the project. "
-        "Only applies on tasks without sale order item defined, and if the employee is not in the 'Employee/Sales Order Item Mapping' of the project.")
+        help="Sales Order Item to which the time spent on this task will be added, in order to be invoiced to your customer.")
     project_sale_order_id = fields.Many2one('sale.order', string="Project's sale order", related='project_id.sale_order_id')
     invoice_count = fields.Integer("Number of invoices", related='sale_order_id.invoice_count')
     task_to_invoice = fields.Boolean("To invoice", compute='_compute_task_to_invoice', search='_search_task_to_invoice', groups='sales_team.group_sale_salesman_all_leads')
