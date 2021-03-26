02c018f777773146f5652eac06ae951f49041939
Arnaud Joset <arj@odoo.com>
2021-03-26 18:03:40 +0100

    [REF] payment_alipay: migrate Alipay to the new payment API
    See the merge commit for more details.
    
    task-2333027

--- addons/payment_alipay/__init__.py
+++ addons/payment_alipay/__init__.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import controllers
 from . import models
-from odoo.addons.payment.models.payment_acquirer import create_missing_journal_for_acquirers
+
+from odoo.addons.payment import reset_payment_acquirer
+from odoo.addons.payment.models.payment_acquirer import create_missing_journals  # post-init hook
+
+
+def uninstall_hook(cr, registry):
+    reset_payment_acquirer(cr, registry, 'alipay')
