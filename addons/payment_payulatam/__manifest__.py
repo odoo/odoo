ffee5a914b3944214089728ee114fd4f54d597ca
Kevin Baptiste <kba@odoo.com>
2021-03-26 18:30:52 +0100

    [REF] payment_payulatam: migrate PayULatam to the new payment API
    See the merge commit for more details.
    
    task-2333038

--- addons/payment_payulatam/__manifest__.py
+++ addons/payment_payulatam/__manifest__.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
     'name': 'PayuLatam Payment Acquirer',
+    'version': '2.0',
     'category': 'Accounting/Payment Acquirers',
     'sequence': 370,
     'summary': 'Payment Acquirer: PayuLatam Implementation',
@@ -14,5 +14,6 @@
         'data/payment_acquirer_data.xml',
     ],
     'application': True,
-    'post_init_hook': 'create_missing_journal_for_acquirers',
+    'post_init_hook': 'create_missing_journals',
+    'uninstall_hook': 'uninstall_hook',
 }
