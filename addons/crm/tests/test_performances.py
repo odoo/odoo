1b6a1d1567eebae52f113a83f23c39a7fd224c32
Thibault Francois <tfr@openerp.com>
2021-03-29 14:46:33 +0200

    [IMP] crm: change in assignation process and limit
    PURPOSE
    
    All unassigned leads should be assigned to teams as soon as possible to ease
    lead analysis.
    
    Purpose of assign thresholds is to ensure sales people receive at least this
    amount of leads within 30 days, counting lost and won leads. Giving them leads
    regularly is also one goal of automatic assign.
    
    SPECIFICATIONS: TEAM ALLOCATION
    
    Team assignment has to be updated as we may have team domains that overlap.
    We therefore remove maximum number of leads to allocate to teams. Instead all
    available unassigned leads are allocated within teams.
    
      - Solution: assign all available leads and not a count based on team's
        capacity. This notably reverts the main goal of odoo/odoo@6df2f0cfc0309badfd58471e413ece695d629009
        (see odoo/odoo#48422)
    
    This assignment process is done proportionally to the team capacity. It is
    computed as the sum of each member's maximum assignment counter. This means
    that with a team having twice as much sale capacity than another team sharing
    the same domain: first team should receive about 2/3 of leads while the second
    one should receive the remaining 1/3.
    
      - Solution: assign lead one by one. Choose a team randomly using a weighted
        random algorithm, based on team's members capacity.
    
    SPECIFICATIONS: MEMBER ASSIGN
    
    Counting every lead whatever its state may lead to an inconvenient situation.
    Moreover salespersons may opt-out from assign by setting their max capacity
    to 0, for example when going on holidays.
    
    When doing that lead assignment is not smooth and getting back to a full
    pipe may take several days. To solve that issue a compensation is added in
    assignment quota done to sales people. Salespersons having few leads will
    get a boost in assign as soon as they get back in assign process. When being
    near maximum compensation is nearing 0 and daily quota is given.
    
    PERFORMANCE
    
    During team assignation, assigning lead one by one may cause performance issue.
    Since PLS is computed at each flush, so we want to flush after a bunch of lead.
    To solve that we obviously need to not commit at each assignation but
    also avoid to search for duplicate at each assignation, that why the
    search for duplicates is done at the beginning of the process and stored
    in memory.
    
    LINKS
    
    Task ID-2444908 (assign fixes)
    Task ID-2489951 (assign process improvements)
    COM PR odoo/odoo#70172
    
    X-original-commit: 83f72711e7affa8b0ad997b9ee262326e9577d54

--- addons/crm/tests/test_performances.py
+++ addons/crm/tests/test_performances.py
@@ -34,7 +34,7 @@ class TestLeadAssignPerf(TestLeadAssignCommon):
             lead_type='lead',
             user_ids=[False],
             partner_ids=[self.contact_1.id, self.contact_2.id, False, False, False],
-            count=50
+            count=200
         )
         # commit probability and related fields
         leads.flush()
@@ -50,34 +50,24 @@ class TestLeadAssignPerf(TestLeadAssignCommon):
         leads.flush()
 
         with self.with_user('user_sales_manager'):
-            with self.assertQueryCount(user_sales_manager=483):  # crm only: ??
+            with self.assertQueryCount(user_sales_manager=1333):  # crm only: ??
                 self.env['crm.team'].browse(self.sales_teams.ids)._action_assign_leads(work_days=2)
 
         # teams assign
-        # due to duplicate management keeping master team, we may not ensure leads to be
-        # fulfilling their original team volume
         leads = self.env['crm.lead'].search([('id', 'in', leads.ids)])  # ensure order
         leads_st1 = leads.filtered_domain([('team_id', '=', self.sales_team_1.id)])
         leads_stc = leads.filtered_domain([('team_id', '=', self.sales_team_convert.id)])
-        self.assertEqual(len(leads_st1), 10)  # 2 * 2 * 75 / 30.0
-        self.assertEqual(len(leads_stc), 12)  # 2 * 2 * 90 / 30.0
+        self.assertLessEqual(len(leads_st1), 128)
+        self.assertLessEqual(len(leads_stc), 96)
+        self.assertEqual(len(leads_st1) + len(leads_stc), len(leads))  # Make sure all lead are assigned
 
         # salespersons assign
         self.members.invalidate_cache(fnames=['lead_month_count'])
-        self.assertMemberAssign(self.sales_team_1_m1, 3)  # 45 max on 2 days
-        self.assertMemberAssign(self.sales_team_1_m2, 1)  # 15 max on 2 days
-        self.assertMemberAssign(self.sales_team_1_m3, 1)  # 15 max on 2 days
-        self.assertMemberAssign(self.sales_team_convert_m1, 2)  # 30 max on 15
-        self.assertMemberAssign(self.sales_team_convert_m2, 4)  # 60 max on 15
-
-        # run a second round to finish leads
-        with self.with_user('user_sales_manager'):
-            with self.assertQueryCount(user_sales_manager=137):  # crm only: ??
-                self.env['crm.team'].browse(self.sales_teams.ids)._action_assign_leads(work_days=2)
-
-        # teams assign: everything should be done due to duplicates
-        leads = self.env['crm.lead'].search([('id', 'in', leads.ids)])  # ensure order
-        self.assertEqual(len(leads.filtered_domain([('team_id', '=', False)])), 0)
+        self.assertMemberAssign(self.sales_team_1_m1, 11)  # 45 max on 2 days (3) + compensation (8.4)
+        self.assertMemberAssign(self.sales_team_1_m2, 4)  # 15 max on 2 days (1) + compensation (2.8)
+        self.assertMemberAssign(self.sales_team_1_m3, 4)  # 15 max on 2 days (1) + compensation (2.8)
+        self.assertMemberAssign(self.sales_team_convert_m1, 8)  # 30 max on 15 (2) + compensation (5.6)
+        self.assertMemberAssign(self.sales_team_convert_m2, 15)  # 60 max on 15 (4) + compsantion (11.2)
 
     @mute_logger('odoo.models.unlink', 'odoo.addons.crm.models.crm_team', 'odoo.addons.crm.models.crm_team_member')
     def test_assign_perf_no_duplicates(self):
@@ -85,7 +75,7 @@ class TestLeadAssignPerf(TestLeadAssignCommon):
             lead_type='lead',
             user_ids=[False],
             partner_ids=[False],
-            count=50
+            count=100
         )
         # commit probability and related fields
         leads.flush()
@@ -99,23 +89,24 @@ class TestLeadAssignPerf(TestLeadAssignCommon):
                 lead.probability = (idx + 1) * 10 * ((int(lead.priority) + 1) / 2)
 
         with self.with_user('user_sales_manager'):
-            with self.assertQueryCount(user_sales_manager=209):  # crm only: 209 (seems reproducible)
+            with self.assertQueryCount(user_sales_manager=580):  # crm only: 580 (seems reproducible)
                 self.env['crm.team'].browse(self.sales_teams.ids)._action_assign_leads(work_days=2)
 
         # teams assign
         leads = self.env['crm.lead'].search([('id', 'in', leads.ids)])  # ensure order
         leads_st1 = leads.filtered_domain([('team_id', '=', self.sales_team_1.id)])
         leads_stc = leads.filtered_domain([('team_id', '=', self.sales_team_convert.id)])
-        self.assertEqual(len(leads_st1), 10)  # 2 * 2 * 75 / 30.0
-        self.assertEqual(len(leads_stc), 12)  # 2 * 2 * 90 / 30.0
+        self.assertEqual(len(leads_st1) + len(leads_stc), 100)  # 2 * 2 * 75 / 30.0
+        self.assertLessEqual(len(leads_st1), 100)  # 2 * 2 * 75 / 30.0
+        self.assertLessEqual(len(leads_stc), 66)  # 2 * 2 * 90 / 30.0
 
         # salespersons assign
         self.members.invalidate_cache(fnames=['lead_month_count'])
-        self.assertMemberAssign(self.sales_team_1_m1, 3)  # 45 max on 2 days
-        self.assertMemberAssign(self.sales_team_1_m2, 1)  # 15 max on 2 days
-        self.assertMemberAssign(self.sales_team_1_m3, 1)  # 15 max on 2 days
-        self.assertMemberAssign(self.sales_team_convert_m1, 2)  # 30 max on 15
-        self.assertMemberAssign(self.sales_team_convert_m2, 4)  # 60 max on 15
+        self.assertMemberAssign(self.sales_team_1_m1, 11)  # 45 max on 2 days (3) + compensation (8.4)
+        self.assertMemberAssign(self.sales_team_1_m2, 4)  # 15 max on 2 days (1) + compensation (2.8)
+        self.assertMemberAssign(self.sales_team_1_m3, 4)  # 15 max on 2 days (1) + compensation (2.8)
+        self.assertMemberAssign(self.sales_team_convert_m1, 8)  # 30 max on 15 (2) + compensation (5.6)
+        self.assertMemberAssign(self.sales_team_convert_m2, 15)  # 60 max on 15 (4) + compsantion (11.2)
 
     @mute_logger('odoo.models.unlink', 'odoo.addons.crm.models.crm_team', 'odoo.addons.crm.models.crm_team_member')
     def test_assign_perf_populated(self):
@@ -180,9 +171,15 @@ class TestLeadAssignPerf(TestLeadAssignCommon):
         leads.flush()
 
         with self.with_user('user_sales_manager'):
-            with self.assertQueryCount(user_sales_manager=6293):  # crm only:  ??
+            with self.assertQueryCount(user_sales_manager=5384):  # crm only: ??
                 self.env['crm.team'].browse(sales_teams.ids)._action_assign_leads(work_days=30)
 
+        # teams assign
+        leads = self.env['crm.lead'].search([('id', 'in', leads.ids)])
+        self.assertEqual(leads.team_id, sales_teams)
+        self.assertEqual(leads.user_id, sales_teams.member_ids)
+
+        # salespersons assign
         self.members.invalidate_cache(fnames=['lead_month_count'])
         self.assertMemberAssign(self.sales_team_1_m1, 45)  # 45 max on one month
         self.assertMemberAssign(self.sales_team_1_m2, 15)  # 15 max on one month
