45c3426fffc5a5da4fe516143277d2ae9e543995
Thibault Delavallée <tde@odoo.com>
2021-03-30 07:49:26 +0000

    [FIX] sms: ensure group system has full rights on sms.template model
    As no global rule is defined for system, people belonging to both system
    and a functional group may be limited in their rights about sms templates.
    
    For example install event_sms -> admin is member of system and event manager
    groups. He cannot edit templates other than related to event.
    
    With this commit members of system may write, create or unlink all templates
    independently from their functinal groups.
    
    Task ID-2495426
    Followup of odoo/odoo#64626
    
    closes odoo/odoo#68530
    
    X-original-commit: e0c1563993da918a0fbc2e31a13f3fe6a7ea2916
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/sms/__manifest__.py
+++ addons/sms/__manifest__.py
@@ -32,6 +32,7 @@ The service is provided by the In App Purchase Odoo platform.
         'views/sms_sms_views.xml',
         'views/sms_template_views.xml',
         'security/ir.model.access.csv',
+        'security/sms_security.xml',
     ],
     'demo': [
         'data/sms_demo.xml',
