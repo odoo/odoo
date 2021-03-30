25d2562928c88e972e9777232e63700a22702ecd
Romain Estievenart <res@odoo.com>
2021-03-30 13:49:23 +0200

    [IMP] pos: Improve status-buttons UX on small screen
    - Show user avatar logo on desktop and mobile.
    We are now able to know wich employee is currently connected on mobile.
    The user name is still shown on desktop but didn't appear for small device.
    
    - Add "sign-out" icon on desktop and mobile.
    
    - Remove label strings like "Close", "Orders" and the username to
      gain some space.
    
    - Remove "close_button" unused class
    
    Task ID: 2453679

--- addons/point_of_sale/static/src/js/ChromeWidgets/CashierName.js
+++ addons/point_of_sale/static/src/js/ChromeWidgets/CashierName.js
@@ -7,12 +7,13 @@ odoo.define('point_of_sale.CashierName', function(require) {
     // Previously UsernameWidget
     class CashierName extends PosComponent {
         get username() {
-            const cashier = this.env.pos.get_cashier();
-            if (cashier) {
-                return cashier.name;
-            } else {
-                return '';
-            }
+            const {name} = this.env.pos.get_cashier();
+            return name ? name : '';
+        }
+        get avatar() {
+            const {user_id} = this.env.pos.get_cashier();
+            const id = user_id && user_id.length ? user_id[0] : -1;
+            return `/web/image/res.users/${id}/avatar_128`;
         }
     }
     CashierName.template = 'CashierName';
