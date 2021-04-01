c8b12102510eff59510122b74f5c5981381f7afb
Aaron Bohy <aab@odoo.com>
2021-04-01 16:48:14 +0200

    [REM] R.I.P. wowl

--- addons/wowl/tests/test_wowl_js.py
+++
@@ -1,8 +0,0 @@
-import odoo.tests
-
-
-@odoo.tests.tagged('post_install', '-at_install', 'lpe')
-class WebSuite(odoo.tests.HttpCase):
-
-    def test_wowl(self):
-        self.browser_js('/wowl/tests', "", "", login='admin', timeout=1800)
