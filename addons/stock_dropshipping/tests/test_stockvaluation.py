49f39040523a27707671577182d1950ae131cd9f
wan <wan@odoo.com>
2021-04-02 12:33:24 +0200

    [FIX] account: never set default for invoice_date
    For customer invoices:
    * do not set default date because if you prepare an invoice (and it gets
      a default date), then validate it the next day, the date will be wrong
    * set the date when posting if it wasn't set, because why not?
    
    For vendor bills:
    * do not set default date because you rarely encode a bill at the bill
      date. Forcing the user to enter it reduces risks of user error
      (duplicated vendor bill)
    * do not set it when posting, same reason.
    
    opw-2492862
    Related #68368
    Closes #68367

--- addons/stock_dropshipping/tests/test_stockvaluation.py
+++ addons/stock_dropshipping/tests/test_stockvaluation.py
@@ -69,6 +69,7 @@ class TestStockValuation(ValuationReconciliationTestCommon):
         move_form = Form(self.env['account.move'].with_context(default_move_type='in_invoice'))
         move_form.partner_id = vendor1
         move_form.purchase_id = self.purchase_order1
+        move_form.invoice_date = move_form.date
         for i in range(len(self.purchase_order1.order_line)):
             with move_form.invoice_line_ids.edit(i) as line_form:
                 line_form.tax_ids.clear()
