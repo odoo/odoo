2412fe1018ffd1568a9cf2cbde5f61bfb045762a
xO-Tx <mou@odoo.com>
2021-04-06 14:39:31 +0000

    [FIX] website_sale,*: fix shop_buy_product tour
    *: website_sale_product_configurator
    
    The goal of this commit is to fix the tour : "shop_buy_product"
    by adding the "goToCart" step to work with the new "add to cart"
    scenarios (adding products in the same page is default behaviour).
    
    closes odoo/odoo#68889
    
    X-original-commit: ccc1584cd5ee9156e96e373997765b94831a828d
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- addons/website_sale/static/tests/tours/website_sale_buy.js
+++ addons/website_sale/static/tests/tours/website_sale_buy.js
@@ -2,6 +2,7 @@ odoo.define('website_sale.tour', function (require) {
 'use strict';
 
 var tour = require("web_tour.tour");
+const tourUtils = require('website_sale.tour_utils');
 
 tour.register('shop_buy_product', {
     test: true,
@@ -37,6 +38,7 @@ tour.register('shop_buy_product', {
             extra_trigger: 'label:contains(Steel) input:propChecked',
             trigger: '#product_detail form[action^="/shop/cart/update"] #add_to_cart',
         },
+            tourUtils.goToCart(),
         {
             content: "add suggested",
             extra_trigger: '#wrap:not(:has(#cart_products:contains("Storage Box")))',
