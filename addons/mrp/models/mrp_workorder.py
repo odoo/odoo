b4f98f6fcea076a4627ea83d40f838c0bd588c50
Pratima Gupta <pgu@odoo.com>
2021-04-08 17:56:02 +0530

    [IMP] mrp: suggest the qty to produce instead of 0 in workorders
    Before this commit, if any workorder is stared to produce, qty
    producing was 0. After this commit, if product tracking is none
    then system will suggest the remaining qty of workorder to produce
    when user is start it.
    
    TaskId - 2480775

--- addons/mrp/models/mrp_workorder.py
+++ addons/mrp/models/mrp_workorder.py
@@ -509,6 +509,8 @@ class MrpWorkorder(models.Model):
 
         if self.product_tracking == 'serial':
             self.qty_producing = 1.0
+        else:
+            self.qty_producing = self.qty_remaining
 
         self.env['mrp.workcenter.productivity'].create(
             self._prepare_timeline_vals(self.duration, datetime.now())
