3c97c42a23773b90a50c582c88fd5f6e2829e4a9
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-04-09 14:47:15 -0300

    [IMP] account_check: first pseudo working version

---
+++ addons/account_check/models/account_check_operation.py
@@ -0,0 +1,99 @@
+from odoo import fields, models, _, api
+from odoo.exceptions import UserError, ValidationError
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class AccountCheckOperation(models.Model):
+
+    _name = 'account.check.operation'
+    _description = 'account.check.operation'
+    _rec_name = 'operation'
+    _order = 'date desc, id desc'
+
+    date = fields.Date(default=fields.Date.context_today, required=True, index=True)
+    check_id = fields.Many2one(
+        'account.check', 'Check', required=True, ondelete='cascade', auto_join=True, index=True)
+    operation = fields.Selection([
+        # from payments
+        ('holding', 'Receive'),
+        ('deposited', 'Deposit'),
+        ('selled', 'Sell'),
+        ('delivered', 'Deliver'),
+        # usado para hacer transferencias internas, es lo mismo que delivered
+        # (endosado) pero no queremos confundir con terminos, a la larga lo
+        # volvemos a poner en holding
+        ('transfered', 'Transfer'),
+        ('handed', 'Hand'),
+        ('withdrawed', 'Withdrawal'),
+        # from checks
+        ('reclaimed', 'Claim'),
+        ('rejected', 'Rejection'),
+        ('debited', 'Debit'),
+        ('returned', 'Return'),
+        # al final no vamos a implemnetar esto ya que habria que hacer muchas
+        # cosas hasta actualizar el asiento, mejor se vuelve atras y se
+        # vuelve a generar deuda y listo, igualmente lo dejamos por si se
+        # quiere usar de manera manual
+        ('changed', 'Change'),
+        ('cancel', 'Cancel'),
+    ],
+        required=True,
+        index=True,
+    )
+    origin_name = fields.Char(
+        compute='_compute_origin_name'
+    )
+    origin = fields.Reference(
+        string='Origin Document',
+        selection='_reference_models')
+    partner_id = fields.Many2one(
+        'res.partner',
+        string='Partner',
+    )
+    notes = fields.Text(
+    )
+
+    def unlink(self):
+        for rec in self:
+            if rec.origin:
+                raise ValidationError(_(
+                    'You can not delete a check operation that has an origin.'
+                    '\nYou can delete the origin reference and unlink after.'))
+        return super(AccountCheckOperation, self).unlink()
+
+    @api.depends('origin')
+    def _compute_origin_name(self):
+        """
+        We add this computed method because an error on tree view displaying
+        reference field when destiny record is deleted.
+        As said in this post (last answer) we should use name_get instead of
+        display_name
+        https://www.odoo.com/es_ES/forum/ayuda-1/question/
+        how-to-override-name-get-method-in-new-api-61228
+        """
+        for rec in self:
+            try:
+                if rec.origin:
+                    _id, name = rec.origin.name_get()[0]
+                    origin_name = name
+                    # origin_name = rec.origin.display_name
+                else:
+                    origin_name = False
+            except Exception as e:
+                _logger.exception(
+                    "Compute origin on checks exception: %s" % e)
+                # if we can get origin we clean it
+                rec.write({'origin': False})
+                origin_name = False
+            rec.origin_name = origin_name
+
+    @api.model
+    def _reference_models(self):
+        return [
+            ('account.payment', 'Payment'),
+            ('account.check', 'Check'),
+            ('account.move', 'Journal Entry'),
+            ('account.move.line', 'Journal Item'),
+            ('account.bank.statement.line', 'Statement Line'),
+        ]
