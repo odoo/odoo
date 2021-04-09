d78e48238677c1456ac78d6b3792fb4a5fb8d716
Matthieu Stockbauer <tsm@odoo.com>
2021-04-09 10:45:55 +0200

    [IMP] website, website_form : contactus made static

--- addons/website_form/__manifest__.py
+++ addons/website_form/__manifest__.py
@@ -15,9 +15,13 @@
         'views/snippets/snippets.xml',
         'views/snippets/s_website_form.xml',
         'views/website_form_templates.xml',
+        'data/website_form_data.xml',
     ],
     'installable': True,
-    'auto_install': True,
+    'auto_install': [
+        'website',
+        'mail',
+    ],
     'assets': {
         'website.assets_editor': [
             'website_form/static/src/snippets/s_website_form/options.js',
