e6c4158bd83d46a35e8a6adcd043422f82807596
Bruno Boi <boi@odoo.com>
2021-04-13 15:52:29 +0200

    [FIX] cli: typeRoots must contains only folders

--- odoo/cli/tsconfig.py
+++ odoo/cli/tsconfig.py
@@ -64,7 +64,7 @@ class TSConfig(Command):
                 "checkJs": True,
                 "allowJs": True,
                 "noEmit": True,
-                "typeRoots": ["community/addons/web/static/types/*.d.ts"],
+                "typeRoots": ["community/addons/web/static/types"],  # TODO make this path dynamic
                 "paths": self.generate_imports(modules)
             }, "exclude": self.generate_excludes()
         }
