33917ef5c943e3968f48d288a8609c944e4a89a2
William Henrotin <whe@odoo.com>
2021-04-13 10:33:17 +0200

    [FIX] stock_account: get unit_cost in sudo mode
    Commit ad5ac19218b4c529c651da951acdb93e1ac1227d adds a sudo() in the
    test condition but not inside of the condition.
    
    Close #69144

--- addons/stock_account/models/stock_move.py
+++ addons/stock_account/models/stock_move.py
@@ -34,7 +34,7 @@ class StockMove(models.Model):
         price_unit = self.price_unit
         # If the move is a return, use the original move's price unit.
         if self.origin_returned_move_id and self.origin_returned_move_id.sudo().stock_valuation_layer_ids:
-            price_unit = self.origin_returned_move_id.stock_valuation_layer_ids[-1].unit_cost
+            price_unit = self.origin_returned_move_id.sudo().stock_valuation_layer_ids[-1].unit_cost
         return not self.company_id.currency_id.is_zero(price_unit) and price_unit or self.product_id.standard_price
 
     @api.model
