b0c5bd414ff8f23e3437cbbbe2a1307343a9764f
William Henrotin <whe@odoo.com>
2021-04-13 08:33:17 +0000

    [FIX] stock_account: get unit_cost in sudo mode
    Commit ad5ac19218b4c529c651da951acdb93e1ac1227d adds a sudo() in the
    test condition but not inside of the condition.
    
    Close #69144
    
    closes odoo/odoo#69231
    
    Signed-off-by: William Henrotin <Whenrow@users.noreply.github.com>

--- addons/stock_account/tests/test_stockvaluation.py
+++ addons/stock_account/tests/test_stockvaluation.py
@@ -910,7 +910,7 @@ class TestStockValuation(SavepointCase):
         stock_return_picking_action = stock_return_picking.create_returns()
         return_pick = self.env['stock.picking'].browse(stock_return_picking_action['res_id'])
         return_pick.move_lines[0].move_line_ids[0].qty_done = 1.0
-        return_pick.action_done()
+        return_pick.with_user(self.inventory_user).action_done()
 
         self.assertEqual(self.product1.standard_price, 16)
 
