08409d9739b2355cd672d7357985a98dc3e6e2bf
std-odoo <std@odoo.com>
2021-04-14 07:23:06 +0000

    [IMP] mail: allow to force the FROM headers of the outgoing mail server
    Purpose
    =======
    We want to be able to force the FROM headers when we sent email in
    SMTP. So we can avoid the emails to be considered as spam.
    
    Specifications
    ==============
    This is done with 2 system parameters.
    
    If the system parameter `mail.force.smtp.from` is set we encapsulate all
    outgoing email from with the given value.
    
    If the previous system parameter is not set and if both
    `mail.dynamic.smtp.from` and `mail.catchall.domain` are set, we
    encapsulate the FROM only if the domain of the email is not the same as
    the domain of the catchall parameter.
    
    Otherwise we do not encapsulate the email (same behavior as before this
    commit).
    
    Task 2367946
    See odoo/odoo/pull/61853
    
    closes odoo/odoo#69218
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- odoo/tools/mail.py
+++ odoo/tools/mail.py
@@ -533,6 +533,19 @@ def email_references(references):
             is_private = True
     return (ref_match, model, thread_id, hostname, is_private)
 
+
+def email_domain_extract(email):
+    """Return the domain of the given email."""
+    if not email:
+        return
+
+    email_split = getaddresses([email])
+    if not email_split or not email_split[0]:
+        return
+
+    _, _, domain = email_split[0][1].rpartition('@')
+    return domain
+
 # was mail_message.decode()
 def decode_smtp_header(smtp_header, quoted=False):
     """Returns unicode() string conversion of the given encoded smtp header
@@ -595,3 +608,30 @@ def formataddr(pair, charset='utf-8'):
                 name=email_addr_escapes_re.sub(r'\\\g<0>', name),
                 addr=address)
     return address
+
+
+def encapsulate_email(old_email, new_email):
+    """Change the FROM of the message and use the old one as name.
+
+    e.g.
+    * Old From: "Admin" <admin@gmail.com>
+    * New From: notifications@odoo.com
+    * Output:   "Admin (admin@gmail.com)" <notifications@odoo.com>
+    """
+    old_email_split = getaddresses([old_email])
+    if not old_email_split or not old_email_split[0]:
+        return old_email
+
+    new_email_split = getaddresses([new_email])
+    if not new_email_split or not new_email_split[0]:
+        return
+
+    if old_email_split[0][0]:
+        name_part = '%s (%s)' % old_email_split[0]
+    else:
+        name_part = old_email_split[0][1]
+
+    return formataddr((
+        name_part,
+        new_email_split[0][1],
+    ))
