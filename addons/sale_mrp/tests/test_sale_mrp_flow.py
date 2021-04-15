5467389393103f18f88a7a1d93725240323ce6fe
Goffin Simon <sig@odoo.com>
2021-04-15 09:09:31 +0000

    [FIX] account: Impossible to create account type
    Steps to reproduce the bug:
    
    - Go to Accounting>Journals and create new record
    - In advanced settings tab, use 'Allowed Account Types' field to create a new one
    
    Bug:
    
    Form for account type does not have required internal_group
    
    Introduced by https://github.com/odoo/odoo/commit/5aeec0c3dcd8aa20293167073f3f4f5cec323b74
    
    opw:2497616
    
    closes odoo/odoo#69455
    
    X-original-commit: bacfea994ce7232af33d7e5525a5b56d5cba5871
    Related: odoo/enterprise#22335
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/sale_mrp/tests/test_sale_mrp_flow.py
+++ addons/sale_mrp/tests/test_sale_mrp_flow.py
@@ -581,7 +581,7 @@ class TestSaleMrpFlow(ValuationReconciliationTestCommon):
         self.company.anglo_saxon_accounting = True
         self.partner = self.env['res.partner'].create({'name': 'My Test Partner'})
         self.category = self.env.ref('product.product_category_1').copy({'name': 'Test category','property_valuation': 'real_time', 'property_cost_method': 'fifo'})
-        account_type = self.env['account.account.type'].create({'name': 'RCV type', 'type': 'other', 'internal_group': 'asset'})
+        account_type = self.env['account.account.type'].sudo().create({'name': 'RCV type', 'type': 'other', 'internal_group': 'asset'})
         self.account_receiv = self.env['account.account'].create({'name': 'Receivable', 'code': 'RCV00' , 'user_type_id': account_type.id, 'reconcile': True})
         account_expense = self.env['account.account'].create({'name': 'Expense', 'code': 'EXP00' , 'user_type_id': account_type.id, 'reconcile': True})
         account_output = self.env['account.account'].create({'name': 'Output', 'code': 'OUT00' , 'user_type_id': account_type.id, 'reconcile': True})
@@ -1645,7 +1645,7 @@ class TestSaleMrpFlow(ValuationReconciliationTestCommon):
         self.company.anglo_saxon_accounting = True
         self.partner = self.env['res.partner'].create({'name': 'My Test Partner'})
         self.category = self.env.ref('product.product_category_1').copy({'name': 'Test category', 'property_valuation': 'real_time', 'property_cost_method': 'fifo'})
-        account_type = self.env['account.account.type'].create({'name': 'RCV type', 'type': 'other', 'internal_group': 'asset'})
+        account_type = self.env['account.account.type'].sudo().create({'name': 'RCV type', 'type': 'other', 'internal_group': 'asset'})
         self.account_receiv = self.env['account.account'].create({'name': 'Receivable', 'code': 'RCV00', 'user_type_id': account_type.id, 'reconcile': True})
         account_expense = self.env['account.account'].create({'name': 'Expense', 'code': 'EXP00', 'user_type_id': account_type.id, 'reconcile': True})
         account_output = self.env['account.account'].create({'name': 'Output', 'code': 'OUT00', 'user_type_id': account_type.id, 'reconcile': True})
