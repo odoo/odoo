5f22270ee149ad249b29dfb2ff19562a99c5928d
Adrien Widart <awt@odoo.com>
2021-04-15 08:08:07 +0000

    [FIX] purchase_stock: consider product unit price precision
    When changing the product price precision, this can lead to incorrect
    stock valuations.
    
    To reproduce the error:
    (Enable debug mode)
    1. Go to Settings > Technical > Database Strucutre > Decimal Accuracy
    2. Edit Product Price:
        - Digits: 4
    3. Create a Product Category PC:
        - Costing Method: FIFO
    4. Create a Product P:
        - Product Type: Storable Product
        - Product Category: PC
    5. Create a RfQ with product P:
        - Quantity: 1000
        - Unit Price: 0.035
    6. Confirm Order, Receive Products, Validate
    7. Click on Valuation
    
    Error: The total value is equal to $40 instead of $35. The calculation
    was done after rounding the unit price: $0.035 becomes $0.04, then
    1000*0.04=$40.
    
    When confirming the RfQ, a stock move is created. To do so, the method
    `_get_stock_move_price_unit` is called. When validating the delivery, it
    recomputes the unit price thanks to method `_get_price_unit`. In both
    situation, and if the line has taxes, the method `compute_all` is called
    like this:
    ```python
    price_unit = line.taxes_id.with_context(round=False).compute_all(price_unit,
    	currency=line.order_id.currency_id, quantity=1.0)['total_void']
    ```
    But here is the problem. In this method, total amount is computed with
    this line:
    ```python
    base = currency.round(price_unit * quantity)
    ```
    However, `quantity` is equal to 1 and the multiplication is rounded
    using the currency precision. As a result, `base` is equal to $0.04.
    Then, all computations will use this value and will be incorrect.
    
    This fix applies the real quantity so `base` will have the correct
    value:
    ```
    base = currency.round(price_unit * quantity)
         = currency.round(0.035 * 1000)
         = 35
    ```
    
    OPW-2472192
    
    closes odoo/odoo#70269
    
    X-original-commit: 77b7a23c3c759b33e26bf91faf14a10f02449a91
    Signed-off-by: William Henrotin <Whenrow@users.noreply.github.com>
    Signed-off-by: Adrien Widart <adwid@users.noreply.github.com>

--- addons/purchase_stock/models/stock.py
+++ addons/purchase_stock/models/stock.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
+from odoo.tools.float_utils import float_round
 
 
 class StockPicking(models.Model):
@@ -36,11 +37,14 @@ class StockMove(models.Model):
         """ Returns the unit price for the move"""
         self.ensure_one()
         if self.purchase_line_id and self.product_id.id == self.purchase_line_id.product_id.id:
+            price_unit_prec = self.env['decimal.precision'].precision_get('Product Price')
             line = self.purchase_line_id
             order = line.order_id
             price_unit = line.price_unit
             if line.taxes_id:
-                price_unit = line.taxes_id.with_context(round=False).compute_all(price_unit, currency=line.order_id.currency_id, quantity=1.0)['total_void']
+                qty = line.product_qty or 1
+                price_unit = line.taxes_id.with_context(round=False).compute_all(price_unit, currency=line.order_id.currency_id, quantity=qty)['total_void']
+                price_unit = float_round(price_unit / qty, precision_digits=price_unit_prec)
             if line.product_uom.id != line.product_id.uom_id.id:
                 price_unit *= line.product_uom.factor / line.product_id.uom_id.factor
             if order.currency_id != order.company_id.currency_id:
