bc86a101a8496fba36eeeed67673ad2fca0c3619
Thibault Libioulle <tle@odoo.com>
2021-04-16 12:52:39 +0000

    [IMP] project: add tests for subtasks
    This commit adds tests concerning PR #62577.
    
    Tests also address errors in PR #68647 and #69170.
    
    PR #70576
    
    task-2388034
    task-2522066
    
    X-original-commit: b76e277f03c881ce6fd4d92fc8c8fb3e0ab704ea

--- addons/project/tests/test_project_flow.py
+++ addons/project/tests/test_project_flow.py
@@ -276,3 +276,27 @@ class TestProjectFlow(TestProjectCommon):
         self.assertEqual(rating_good.parent_res_id, self.project_goats.id)
         self.assertEqual(self.project_goats.rating_percentage_satisfaction, 50)
         self.assertEqual(self.project_pigs.rating_percentage_satisfaction, -1)
+
+    def test_task_with_no_project(self):
+        """
+            With this test, we want to make sure the fact that a task has no project doesn't affect the entire
+            behaviours of projects.
+
+            1) Try to compute every field of a task which has no project.
+            2) Try to compute every field of a project and assert it isn't affected by this use case.
+        """
+        task_without_project = self.env['project.task'].with_context({'mail_create_nolog': True}).create({
+            'name': 'Test task without project'
+        })
+
+        for field in task_without_project._fields.keys():
+            try:
+                task_without_project[field]
+            except Exception as e:
+                raise AssertionError("Error raised unexpectedly while computing a field of the task ! Exception : " + e.args[0])
+
+        for field in self.project_pigs._fields.keys():
+            try:
+                self.project_pigs[field]
+            except Exception as e:
+                raise AssertionError("Error raised unexpectedly while computing a field of the project ! Exception : " + e.args[0])
