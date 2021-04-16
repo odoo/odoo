4d19c5bbbe0752c96397429e61d723105b94a7ca
Francois (fge) <fge@odoo.com>
2021-04-16 15:09:08 +0200

    [FIX] web: initial rpcs are done twice on /web/webclient/load_menus/

--- addons/web/static/src/services/menu_service.js
+++ addons/web/static/src/services/menu_service.js
@@ -11,6 +11,8 @@ function makeFetchLoadMenus() {
   return async function fetchLoadMenus(reload) {
     if (reload) {
       loadMenusHash = new Date().getTime().toString();
+    } else if (odoo.loadMenusPromise) {
+        return odoo.loadMenusPromise;
     }
     const res = await browser.fetch(`${loadMenusUrl}/${loadMenusHash}`);
     if (!res.ok) {
