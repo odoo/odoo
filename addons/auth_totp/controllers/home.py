111b0615687e5bda8af3ae34a2f2cd69d398a075
Arnaud (arg) <arg@odoo.com>
2021-04-19 18:13:53 +0200

    [IMP]auth_totp: 2FA Trusted Devices
    + Added the 'Remember this Device' feature
    
    Added '2FA Trusted Devices' feature to allow users to remember their device to bypass the 2FA for the next connections.

--- addons/auth_totp/controllers/home.py
+++ addons/auth_totp/controllers/home.py
@@ -21,7 +21,17 @@ class Home(odoo.addons.web.controllers.main.Home):
             return http.redirect_with_hash('/web/login')
 
         error = None
-        if request.httprequest.method == 'POST':
+        if request.httprequest.method == 'GET':
+            user = request.env['res.users'].browse(request.session.pre_uid)
+            cookies = request.httprequest.cookies
+            if 'trusted_device' in cookies.keys():
+                key = cookies['trusted_device']
+                checked_credentials = request.env['res.users.apikeys']._check_credentials(scope='Trusted Device', key=key)
+                if checked_credentials == user.id:
+                    request.session.finalize()
+                    return http.redirect_with_hash(self._login_redirect(request.session.uid, redirect=redirect))
+
+        elif request.httprequest.method == 'POST':
             user = request.env['res.users'].browse(request.session.pre_uid)
             try:
                 with user._assert_can_auth():
@@ -32,7 +42,18 @@ class Home(odoo.addons.web.controllers.main.Home):
                 error = _("Invalid authentication code format.")
             else:
                 request.session.finalize()
-                return http.redirect_with_hash(self._login_redirect(request.session.uid, redirect=redirect))
+                returning_session = http.redirect_with_hash(self._login_redirect(request.session.uid, redirect=redirect))
+                if 'remember' in kwargs.keys() and kwargs['remember'] == 'on':
+                    client_info = {
+                        'browser': request.httprequest.user_agent.browser.capitalize(),
+                        'platform': request.httprequest.user_agent.platform.capitalize(),
+                        'location': request.httprequest.remote_addr,
+                    }
+                    name = '{browser}({platform}) - {location}'.format(**client_info)
+                    key = request.env['res.users.apikeys']._generate('Trusted Device', name)
+                    print(key)
+                    returning_session.headers.add('Set-Cookie', f'trusted_device={key}; Path=/; Max-Age={90 * 60 * 60 * 24}; HttpOnly')  # Max-Age = 90 days;
+                return returning_session
 
         return request.render('auth_totp.auth_totp_form', {
             'error': error,
