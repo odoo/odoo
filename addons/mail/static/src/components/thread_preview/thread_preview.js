5780dd70d47ac64483ae01e8062ac9ad826078fc
Zelong Lin <zel@odoo.com>
2021-04-20 14:50:14 +0200

    [IMP] stage changed improved in systray

--- addons/mail/static/src/components/thread_preview/thread_preview.js
+++ addons/mail/static/src/components/thread_preview/thread_preview.js
@@ -31,8 +31,6 @@ class ThreadPreview extends Component {
             if (lastMessage) {
                 lastMessageAuthor = lastMessage.author;
             }
-            console.log(lastMessageAuthor)
-            console.log(lastMessage)
             return {
                 isDeviceMobile: this.env.messaging.device.isMobile,
                 lastMessage: lastMessage ? lastMessage.__state : undefined,
