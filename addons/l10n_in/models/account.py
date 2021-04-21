83860e79030da51f1686ac10c6c820d468e827cc
Ivan Yelizariev <iel@odoo.com>
2021-04-21 11:28:39 +0200

    [REM] l10n_in: delete dead code
    both methods don't exist anymore and not used

--- addons/l10n_in/models/account.py
+++ addons/l10n_in/models/account.py
@@ -27,29 +27,7 @@ class AccountJournal(models.Model):
         return list(result_dict.items())
 
 
-class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
-
-    @api.depends('move_id.line_ids', 'move_id.line_ids.tax_line_id', 'move_id.line_ids.debit', 'move_id.line_ids.credit')
-    def _compute_tax_base_amount(self):
-        aml = self.filtered(lambda l: l.company_id.account_fiscal_country_id.code == 'IN' and l.tax_line_id  and l.product_id)
-        for move_line in aml:
-            base_lines = move_line.move_id.line_ids.filtered(lambda line: move_line.tax_line_id in line.tax_ids and move_line.product_id == line.product_id)
-            move_line.tax_base_amount = abs(sum(base_lines.mapped('balance')))
-        remaining_aml = self - aml
-        if remaining_aml:
-            return super(AccountMoveLine, remaining_aml)._compute_tax_base_amount()
-
-
 class AccountTax(models.Model):
     _inherit = 'account.tax'
 
     l10n_in_reverse_charge = fields.Boolean("Reverse charge", help="Tick this if this tax is reverse charge. Only for Indian accounting")
-
-    def get_grouping_key(self, invoice_tax_val):
-        """ Returns a string that will be used to group account.invoice.tax sharing the same properties"""
-        key = super(AccountTax, self).get_grouping_key(invoice_tax_val)
-        if self.company_id.account_fiscal_country_id.code == 'IN':
-            key += "-%s-%s"% (invoice_tax_val.get('l10n_in_product_id', False),
-                invoice_tax_val.get('l10n_in_uom_id', False))
-        return key
