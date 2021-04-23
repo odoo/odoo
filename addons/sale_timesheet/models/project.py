2480e9593808126ade2537cc2ae40b85080ba441
Laurent Stukkens (LTU) <ltu@odoo.com>
2021-04-23 16:06:23 +0000

    [FIX] sale_timesheet: prevent multicompany issue on SOL
    Steps to reproduce:
    
            - Create an SO for a prepaid service in company A and confirm it
            - Create a project and a task in company B and select the customer linked to the SO you previously created
            - Unselect company A from the multi-company widget; only display the records from company B
            - Timesheet on the task you previously created
    
    Observed behavior:
    
            - The SOL set by default belongs to company A, thus generating an access right error when trying to timesheet on the task
    
    Expected behavior:
    
            - The SOL set by default should belong to the same company as the task
    
    task-2515259
    
    closes odoo/odoo#69783
    
    Related: odoo/enterprise#17921
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/sale_timesheet/models/project.py
+++ addons/sale_timesheet/models/project.py
@@ -349,7 +349,7 @@ class ProjectTask(models.Model):
         self.ensure_one()
         if not self.commercial_partner_id or not self.allow_billable:
             return False
-        domain = [('is_service', '=', True), ('order_partner_id', 'child_of', self.commercial_partner_id.id), ('is_expense', '=', False), ('state', 'in', ['sale', 'done'])]
+        domain = [('company_id', '=', self.company_id.id), ('is_service', '=', True), ('order_partner_id', 'child_of', self.commercial_partner_id.id), ('is_expense', '=', False), ('state', 'in', ['sale', 'done'])]
         if self.project_id.bill_type == 'customer_project' and self.project_sale_order_id:
             domain.append(('order_id', '=?', self.project_sale_order_id.id))
         sale_lines = self.env['sale.order.line'].search(domain)
