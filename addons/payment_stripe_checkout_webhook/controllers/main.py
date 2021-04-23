443da3aa32efba2805159e6496c0b55fb1f48b2a
Adrien Horgnies <aho@odoo.com>
2021-04-23 13:08:09 +0000

    [IMP] payment_stripe: support webhooks
    Allow configuring a webhook in Stripe to send s2s notifications to Odoo
    when a Checkout payment is completed. Note that SetupIntent and
    PaymentIntent events are not listened to, since they are handled 'live'
    with the customer actively present; the main use case for Stripe
    webhooks is a Checkout session that gets interrupted before the customer
    is redirected to Odoo (e.g. network loss, browser crash, closing the
    tab, etc.).
    
    The webhook should be configured to send its events to
    <base_url>/payment/stripe/webhook and should only subscribe to
    checkout.session.completed events to avoid spamming the Odoo server with
    useless notifications.
    
    opw-2488452
    opw-2451463
    opw-2449738
    
    BACKPORT of commit: dc4f6ad04f8bf3800c650d878b08e44775b95628
    
    Should not be merged beyond 14.0 (14.0 excluded)
    
    closes odoo/odoo#71520
    
    X-original-commit: 7a8aca1df1edf3db36302617b3e7605bf7bdadaf
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

---
+++ addons/payment_stripe_checkout_webhook/controllers/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import json
+import logging
+
+from odoo.http import request
+
+from odoo import http
+
+_logger = logging.getLogger(__name__)
+
+
+class StripeController(http.Controller):
+
+    @http.route('/payment/stripe/webhook', type='json', auth='public', csrf=False)
+    def stripe_webhook(self, **kwargs):
+        request.env['payment.acquirer'].sudo()._handle_stripe_webhook(kwargs)
+        return 'OK'
