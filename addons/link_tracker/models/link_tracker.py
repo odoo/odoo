a2068f9696917d7fa4848bc09d05eced84d0cc2f
Krzysztof Magusiak <chris.magusiak@gmail.com>
2021-04-26 15:33:23 +0000

    [FIX] link_tracker: Compute URL with char tracking fields
    Make link tracker handle other types than many2one for tracking fields.
    Currently, when a field of type char is returned, an error occurs.
    
    odoo/addons/utm/models/utm_mixin.py already handles other types than
    many2one for tracking_fields(). This aligns to that behaviour in the
    link tracker.
    
    opw-2494686
    
    closes odoo/odoo#69859
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/link_tracker/models/link_tracker.py
+++ addons/link_tracker/models/link_tracker.py
@@ -83,8 +83,11 @@ class LinkTracker(models.Model):
         for tracker in self:
             parsed = urls.url_parse(tracker.url)
             utms = {}
-            for key, field, cook in self.env['utm.mixin'].tracking_fields():
-                attr = getattr(tracker, field).name
+            for key, field_name, cook in self.env['utm.mixin'].tracking_fields():
+                field = self._fields[field_name]
+                attr = getattr(tracker, field_name)
+                if field.type == 'many2one':
+                    attr = attr.name
                 if attr:
                     utms[key] = attr
             utms.update(parsed.decode_query())
