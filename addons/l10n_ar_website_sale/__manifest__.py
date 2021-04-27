46aede993585a9d0c418e8bbc2371b2e49859886
Valentino <vbe@odoo.com>
2021-04-27 22:34:32 +0000

    [IMP] l10n_ar: Update eCommerce module displayed name
    Feedback received: change the name from 'Argentinean Website' to 'Argentinean eCommerce'  in order to stay consistent with the functional name of stadand Apps
    
    closes odoo/odoo#70361
    
    X-original-commit: 7afb8e3193615d54f9e1b40a453eaf7f6c456dad
    Signed-off-by: Josse Colpaert <jco@openerp.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/l10n_ar_website_sale/__manifest__.py
+++ addons/l10n_ar_website_sale/__manifest__.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Argentinean Website',
+    'name': 'Argentinean eCommerce',
     'version': '1.0',
     'category': 'Accounting/Localizations/Website',
     'sequence': 14,
