36fe5d5cf91d69b280f8f7e53765739736351c66
Jorge Pinna Puissant <jpp@odoo.com>
2021-04-28 13:30:37 +0000

    [IMP] test_lint: Add ESLint to the test suite
    This commit allows using ESLint on the code JS to disallow undeclared
    variables (no-undef) and disallow specific global variables
    (no-restricted-globals) (error, event and self).
    
    Co-authored-by: Samuel Degueldre <sad@odoo.com>

--- odoo/addons/test_lint/tests/__init__.py
+++ odoo/addons/test_lint/tests/__init__.py
@@ -1,6 +1,7 @@
 from . import test_pylint
 from . import test_pofile
 from . import test_ecmascript
+from . import test_eslint
 from . import test_markers
 from . import test_onchange_domains
 from . import test_dunderinit
