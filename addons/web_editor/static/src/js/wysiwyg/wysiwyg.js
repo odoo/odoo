756783665e07d59bef57880c0dce72ff259b6a60
Sébastien Geelen (sge) <sge@odoo.com>
2021-04-29 17:00:55 +0200

    [FIX] web_editor: fix icon replacement when using toolbar replace button

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -591,6 +591,14 @@ const Wysiwyg = Widget.extend({
      */
     openMediaDialog(params = {}) {
         const sel = this.odooEditor.document.getSelection();
+        const fontawesomeIcon = getInSelection(this.odooEditor.document, '.fa');
+        if (fontawesomeIcon && sel.toString().trim() === "") {
+            params.node = $(fontawesomeIcon);
+            // save layouting classes from icons to not break the page if you edit an icon
+            params.htmlClass = [...fontawesomeIcon.classList].filter((className) => {
+                return !className.startsWith('fa') || faZoomClassRegex.test(className);
+            }).join(' ');
+        }
         if (!sel.rangeCount) {
             return;
         }
