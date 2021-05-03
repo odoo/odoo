2bac8ffceaf9328af2d3d1fe81a22106234b22aa
Nicolas (vin) <vin@odoo.com>
2021-05-03 07:49:25 +0000

    [IMP] l10_fr_reports: carry over tax grid 27 to tax grid 22
    Using the new carryover feature, improve the french tax report to properly carryover the values from the tax grid 27 to the tax grid 22 of the next period.
    
    Also slightly improve the carryover itself
    to support total lines, and disallow to
    carry over to lines from another report.
    
    Task id #2452451
    
    closes odoo/odoo#69887
    
    Related: odoo/upgrade#2708
    Related: odoo/enterprise#17953
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>

--- addons/account/models/account_tax_carryover_line.py
+++ addons/account/models/account_tax_carryover_line.py
@@ -2,6 +2,7 @@ from odoo import api, fields, models, _
 
 from odoo.exceptions import ValidationError
 
+
 class AccountTaxCarryoverLine(models.Model):
     _name = 'account.tax.carryover.line'
     _description = 'Tax carryover line'
@@ -9,17 +10,26 @@ class AccountTaxCarryoverLine(models.Model):
     name = fields.Char(required=True)
     amount = fields.Float(required=True, default=0.0)
     date = fields.Date(required=True, default=fields.Date.context_today, readonly=True)
-    tax_report_line_id = fields.Many2one(comodel_name='account.tax.report.line', string="Tax report line")
-    tax_report_country_id = fields.Many2one(related='tax_report_line_id.report_id.country_id')
-    company_id = fields.Many2one(comodel_name='res.company', string='Company',
-                                 required=True,
-                                 default=lambda self: self.env.company)
-    foreign_vat_fiscal_position_id = fields.Many2one(comodel_name='account.fiscal.position',
-                                                     string='Fiscal position',
-                                                     help="The foreign fiscal position for which this carryover is made.",
-                                                     domain="[('company_id', '=', company_id), "
-                                                            "('country_id', '=', tax_report_country_id), "
-                                                            "('foreign_vat', '!=', False)]")
+    tax_report_line_id = fields.Many2one(
+        comodel_name='account.tax.report.line',
+        string="Tax report line",
+        domain="[('report_id', '=', tax_report_id)]",
+    )
+    tax_report_id = fields.Many2one(related='tax_report_line_id.report_id')
+    tax_report_country_id = fields.Many2one(related='tax_report_id.country_id')
+    company_id = fields.Many2one(
+        comodel_name='res.company', string='Company',
+        required=True,
+        default=lambda self: self.env.company,
+    )
+    foreign_vat_fiscal_position_id = fields.Many2one(
+        comodel_name='account.fiscal.position',
+        string="Fiscal position",
+        help="The foreign fiscal position for which this carryover is made.",
+        domain="[('company_id', '=', company_id), "
+               "('country_id', '=', tax_report_country_id), "
+               "('foreign_vat', '!=', False)]",
+    )
 
     @api.constrains('foreign_vat_fiscal_position_id')
     def _check_fiscal_position(self):
