34b5b0e86f0c1f89179b9cf1a2dd083d51512b41
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2021-05-05 10:49:26 +0000

    [FIX] repair: correctly group invoices
    Have 2 repair records with the same partner_id, ready to be invoiced
    In list view select both records and click Action>Create invoices
    In the wizard check 'Group by partner invoice address', create invoices.
    
    Separate invoices will be created, but while grouping 1 invoice should
    be created joining the 2 repairs
    
    opw-2476539
    
    closes odoo/odoo#70434
    
    X-original-commit: 8f6e2fa1096f57ace6c84ae6083ab77ee37a69a1
    Signed-off-by: agr-odoo <agr-odoo@users.noreply.github.com>

--- addons/repair/models/repair.py
+++ addons/repair/models/repair.py
@@ -332,7 +332,7 @@ class Repair(models.Model):
             if not journal:
                 raise UserError(_('Please define an accounting sales journal for the company %s (%s).') % (company.name, company.id))
 
-            if (partner_invoice.id, currency.id) not in grouped_invoices_vals:
+            if (partner_invoice.id, currency.id, company.id) not in grouped_invoices_vals:
                 grouped_invoices_vals[(partner_invoice.id, currency.id, company.id)] = []
             current_invoices_list = grouped_invoices_vals[(partner_invoice.id, currency.id, company.id)]
 
