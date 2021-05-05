a642d55821f8f5f15d076a20b21a12a05e46ee12
mreficent <miquel.raich@eficent.com>
2021-05-05 14:52:17 +0000

    [FIX] stock,mrp: don't unarchive operations from archived warehouses
    If you modify an archived warehouse, it may happen that the associated operations get unarchived. Operations should be active only if their warehouse is active.
    
    closes odoo/odoo#70607
    
    X-original-commit: 63167ac8ae6ac08fa54a133ed9089dd872c7d73c
    Signed-off-by: William Henrotin <Whenrow@users.noreply.github.com>

--- addons/mrp/models/stock_warehouse.py
+++ addons/mrp/models/stock_warehouse.py
@@ -259,15 +259,15 @@ class StockWarehouse(models.Model):
         data = super(StockWarehouse, self)._get_picking_type_update_values()
         data.update({
             'pbm_type_id': {
-                'active': self.manufacture_to_resupply and self.manufacture_steps in ('pbm', 'pbm_sam'),
+                'active': self.manufacture_to_resupply and self.manufacture_steps in ('pbm', 'pbm_sam') and self.active,
                 'barcode': self.code.replace(" ", "").upper() + "-PC",
             },
             'sam_type_id': {
-                'active': self.manufacture_to_resupply and self.manufacture_steps == 'pbm_sam',
+                'active': self.manufacture_to_resupply and self.manufacture_steps == 'pbm_sam' and self.active,
                 'barcode': self.code.replace(" ", "").upper() + "-SFP",
             },
             'manu_type_id': {
-                'active': self.manufacture_to_resupply,
+                'active': self.manufacture_to_resupply and self.active,
                 'default_location_src_id': self.manufacture_steps in ('pbm', 'pbm_sam') and self.pbm_loc_id.id or self.lot_stock_id.id,
                 'default_location_dest_id': self.manufacture_steps == 'pbm_sam' and self.sam_loc_id.id or self.lot_stock_id.id,
             },
