d978b9e1c0f8e4a66de1b4ececbb97f77a1718d7
Thibault Delavallée <tde@odoo.com>
2021-05-06 13:36:52 +0000

    [IMP] various: update query counters according to latest runbot run
    Base: seems related fields take only 1 extra query instead of 3
    
    Crm: assignment seems to have been slightly improved. Some randomness still
    happens.
    
    Hr Holidays: seems it was further improved beyond space frontier
    
    Hr Work Entry Holidays: seems it was further improved
    
    Test mail: those tests were a bit random, seems random is gone (hopefully)
    
    Test mail full: updated local counters
    
    Test mass mailing: seems we gained one query, updated local counters
    
    closes odoo/odoo#70504
    
    X-original-commit: 8c215b3d60929166b3e60e2a4dfb2a6c70439046
    Related: odoo/enterprise#18194
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/hr_work_entry_holidays/tests/test_performance.py
+++ addons/hr_work_entry_holidays/tests/test_performance.py
@@ -31,7 +31,7 @@ class TestWorkEntryHolidaysPerformance(TestWorkEntryHolidaysBase):
         self.richard_emp.generate_work_entries(date(2018, 1, 1), date(2018, 1, 2))
         leave = self.create_leave(datetime(2018, 1, 1, 7, 0), datetime(2018, 1, 1, 18, 0))
 
-        with self.assertQueryCount(__system__=94, admin=99):
+        with self.assertQueryCount(__system__=94, admin=95):
             leave.action_validate()
         leave.action_refuse()
 
@@ -40,14 +40,14 @@ class TestWorkEntryHolidaysPerformance(TestWorkEntryHolidaysBase):
     def test_performance_leave_write(self):
         leave = self.create_leave(datetime(2018, 1, 1, 7, 0), datetime(2018, 1, 1, 18, 0))
 
-        with self.assertQueryCount(__system__=24, admin=24):
+        with self.assertQueryCount(__system__=20, admin=20):
             leave.date_to = datetime(2018, 1, 1, 19, 0)
         leave.action_refuse()
 
     @users('__system__', 'admin')
     @warmup
     def test_performance_leave_create(self):
-        with self.assertQueryCount(__system__=28, admin=59):
+        with self.assertQueryCount(__system__=22, admin=23):
             leave = self.create_leave(datetime(2018, 1, 1, 7, 0), datetime(2018, 1, 1, 18, 0))
         leave.action_refuse()
 
@@ -56,7 +56,7 @@ class TestWorkEntryHolidaysPerformance(TestWorkEntryHolidaysBase):
     def test_performance_leave_confirm(self):
         leave = self.create_leave(datetime(2018, 1, 1, 7, 0), datetime(2018, 1, 1, 18, 0))
         leave.action_draft()
-        with self.assertQueryCount(__system__=22, admin=55):
+        with self.assertQueryCount(__system__=20, admin=21):
             leave.action_confirm()
         leave.state = 'cancel'
 
@@ -108,7 +108,7 @@ class TestWorkEntryHolidaysPerformancesBigData(TestWorkEntryHolidaysBase):
     def test_work_entries_generation_perf(self):
         # Test Case 7: Try to generate work entries for
         # a hundred employees over a month
-        with self.assertQueryCount(__system__=11522, admin=11522):
+        with self.assertQueryCount(__system__=11319, admin=11522):
             work_entries = self.contracts._generate_work_entries(date(2020, 7, 1), date(2020, 8, 31))
 
         # Original work entries to generate when we don't adapt date_generated_from and
