4db958989e9b5476399cf1b27ebcae3aeaa2ce45
Nicolas (vin) <vin@odoo.com>
2021-05-07 08:27:18 +0000

    [IMP] account: Extract payment methods into their own file
    The account payment file being quite big already,
    and the payment methods being modified in another
    task (2414749) we'll extract them into their own
    file to make it easier to work with them
    
    closes odoo/odoo#70937
    
    Related: odoo/enterprise#18367
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -4,17 +4,6 @@ from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
 
-class AccountPaymentMethod(models.Model):
-    _name = "account.payment.method"
-    _description = "Payment Methods"
-    _order = 'sequence'
-
-    name = fields.Char(required=True, translate=True)
-    code = fields.Char(required=True)  # For internal identification
-    payment_type = fields.Selection([('inbound', 'Inbound'), ('outbound', 'Outbound')], required=True)
-    sequence = fields.Integer(help='Used to order Methods in the form view', default=10)
-
-
 class AccountPayment(models.Model):
     _name = "account.payment"
     _inherits = {'account.move': 'move_id'}
