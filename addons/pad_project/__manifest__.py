59f333c6a845afe1347cfe18cc017e8e95b5fc90
Cedric Prieels (cpr) <cpr@odoo.com>
2021-05-10 15:13:12 +0200

    [IMP] project: improve globally the UX
    The UX of the 4 main views (kanban, form, list, portal) of the project app has been globally improved.
    In particular, different icons color/style were changed along with several strings, and the order of a few elements was revised to improve readability.
    
    task-2508723
    See odoo/enterprise#18387

--- addons/pad_project/__manifest__.py
+++ addons/pad_project/__manifest__.py
@@ -15,7 +15,6 @@ This module adds a PAD in all project form views.
     'data': [
         'views/res_config_settings_views.xml',
         'views/project_views.xml',
-        'views/project_portal_templates.xml',
         ],
     'auto_install': True,
     'assets': {
