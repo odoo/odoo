3fe56b8ab6d4c662790e06651e30ebb25ceafb64
Raphael Collet <rco@odoo.com>
2021-05-12 14:02:33 +0000

    [FIX] core: computed editable one2many field with a domain
    Manually adding a line should not trigger the computation of the field,
    just like modifying the line fields that occur in the field's domain.
    In other words, the dependencies of the field should be limited to the
    ones declared on field or its compute method.
    
    X-original-commit: 1c39814bd729cc08882f1545c7d88b0592e63f9a

--- odoo/addons/test_new_api/models/test_new_api.py
+++ odoo/addons/test_new_api/models/test_new_api.py
@@ -692,6 +692,40 @@ class ComputeUnassigned(models.Model):
                 record.bares = record.foo
 
 
+class ComputeOne2many(models.Model):
+    _name = 'test_new_api.one2many'
+    _description = "A computed editable one2many field with a domain"
+
+    name = fields.Char()
+    line_ids = fields.One2many(
+        'test_new_api.one2many.line', 'container_id',
+        compute='_compute_line_ids', store=True, readonly=False,
+        domain=[('count', '>', 0)],
+    )
+
+    @api.depends('name')
+    def _compute_line_ids(self):
+        # increment counter of line with the same name, or create a new line
+        for record in self:
+            if not record.name:
+                continue
+            for line in record.line_ids:
+                if line.name == record.name:
+                    line.count += 1
+                    break
+            else:
+                record.line_ids = [(0, 0, {'name': record.name})]
+
+
+class ComputeOne2manyLine(models.Model):
+    _name = 'test_new_api.one2many.line'
+    _description = "Line of a computed one2many"
+
+    name = fields.Char()
+    count = fields.Integer(default=1)
+    container_id = fields.Many2one('test_new_api.one2many', required=True)
+
+
 class ModelBinary(models.Model):
     _name = 'test_new_api.model_binary'
     _description = 'Test Image field'
