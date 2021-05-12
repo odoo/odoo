f916fc47fa84e7ad5dd1a48322045ae5376d3e0b
Yannick Tivisse <yti@odoo.com>
2021-05-12 14:03:33 +0000

    [FIX] hr_payroll: Recompute work entries/payslips on contract update
    closes odoo/odoo#70814
    
    Forward-port-of: #18075
    X-original-commit: ee398e1abe44caca8bab74f27614dabd9b45590e
    Related: odoo/enterprise#18322
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_work_entry_contract/wizard/hr_work_entry_regeneration_wizard.py
+++ addons/hr_work_entry_contract/wizard/hr_work_entry_regeneration_wizard.py
@@ -83,15 +83,22 @@ class HrWorkEntryRegenerationWizard(models.TransientModel):
 
     def regenerate_work_entries(self):
         self.ensure_one()
-        if not self.valid:
-            raise ValidationError(_("In order to regenerate the work entries, you need to provide the wizard with an employee_id, a date_from and a date_to. In addition to that, the time interval defined by date_from and date_to must not contain any validated work entries."))
+        if not self.env.context.get('work_entry_skip_validation'):
+            if not self.valid:
+                raise ValidationError(_("In order to regenerate the work entries, you need to provide the wizard with an employee_id, a date_from and a date_to. In addition to that, the time interval defined by date_from and date_to must not contain any validated work entries."))
 
-        if self.date_from < self.earliest_available_date or self.date_to > self.latest_available_date:
-            raise ValidationError(_("The from date must be >= '%(earliest_available_date)s' and the to date must be <= '%(latest_available_date)s', which correspond to the generated work entries time interval.", earliest_available_date=self._date_to_string(self.earliest_available_date), latest_available_date=self._date_to_string(self.latest_available_date)))
-        work_entries = self.env['hr.work.entry'].search([('employee_id', '=', self.employee_id.id),
-                                                         ('date_stop', '>=', self.date_from),
-                                                         ('date_start', '<=', self.date_to)])
+            if self.date_from < self.earliest_available_date or self.date_to > self.latest_available_date:
+                raise ValidationError(_("The from date must be >= '%(earliest_available_date)s' and the to date must be <= '%(latest_available_date)s', which correspond to the generated work entries time interval.", earliest_available_date=self._date_to_string(self.earliest_available_date), latest_available_date=self._date_to_string(self.latest_available_date)))
+
+        date_from = max(self.date_from, self.earliest_available_date)
+        date_to = min(self.date_to, self.latest_available_date)
+        work_entries = self.env['hr.work.entry'].search([
+            ('employee_id', '=', self.employee_id.id),
+            ('date_stop', '>=', date_from),
+            ('date_start', '<=', date_to),
+            ('state', '!=', 'validated')])
+
         work_entries.write({'active': False})
-        self.employee_id.generate_work_entries(self.date_from, self.date_to, True)
+        self.employee_id.generate_work_entries(date_from, date_to, True)
         action = self.env["ir.actions.actions"]._for_xml_id('hr_work_entry.hr_work_entry_action')
         return action
