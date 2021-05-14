3d1d159b8880568d32e3977657cbafbaee9d5b6a
Arthur Detroux (ard) <ard@odoo.com>
2021-05-14 08:50:30 +0000

    [FIX] mass_mailing: make editor perform cleanForSave on save
    Before this commit, when editing a mass_mailing template, on save,
    the editor would not tell the snippet menu to perform a cleanForSave
    which it should normally do for the option to properly prepare to save.
    
    Bug discovered while working on task-2327045
    Ideally, would need to be backported but requires a different fix in the
    old editor.
    
    closes odoo/odoo#72346
    
    X-original-commit: 639b6a55586107bd3d855d322c1102df101eb079
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/mass_mailing/static/src/js/mass_mailing_widget.js
+++ addons/mass_mailing/static/src/js/mass_mailing_widget.js
@@ -48,7 +48,7 @@ var MassMailingFieldHtml = FieldHtml.extend({
      *
      * @override
      */
-    commitChanges: function () {
+    commitChanges: async function () {
         var self = this;
         if (this.mode === 'readonly' || !this.isRendered) {
             return this._super();
@@ -60,7 +60,9 @@ var MassMailingFieldHtml = FieldHtml.extend({
         }
 
         var $editable = this.wysiwyg.getEditable();
-
+        if (this.wysiwyg.snippetsMenu) {
+            await this.wysiwyg.snippetsMenu.cleanForSave();
+        }
         return this.wysiwyg.saveModifiedImages(this.$content).then(function () {
             self._isDirty = self.wysiwyg.isDirty();
 
