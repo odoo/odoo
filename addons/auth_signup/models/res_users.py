e8b12644f84c1bdfc07c1cd1892a5d5d1e7edfa6
Stéphane Debauche <std@odoo.com>
2021-05-17 13:54:50 +0000

    [IMP] auth_signup: do not write on the sign_up template
    Remove arbitrary write on template used in signup process. Instead of writing
    on template if some values are not correct compared to the current flow
    we force values given to ``send_mail``. It allows to let people tune the
    template while still having a working auth signup process.
    
    Task ID-2534550 (Template usage improvement)
    Task ID-2477164 (Composer mixin)
    COM PR odoo/odoo#70889
    ENT PR odoo/enterprise#18352

--- addons/auth_signup/models/res_users.py
+++ addons/auth_signup/models/res_users.py
@@ -187,15 +187,14 @@ class ResUsers(models.Model):
             template = self.env.ref('auth_signup.reset_password_email')
         assert template._name == 'mail.template'
 
-        template_values = {
+        email_values = {
             'email_to': '${object.email|safe}',
             'email_cc': False,
             'auto_delete': True,
-            'partner_to': False,
+            'recipient_ids': [],
+            'partner_ids': [],
             'scheduled_date': False,
         }
-        if any(template[field] != value for (field, value) in template_values.items()):
-            template.write(template_values)
 
         for user in self:
             if not user.email:
@@ -203,7 +202,7 @@ class ResUsers(models.Model):
             # TDE FIXME: make this template technical (qweb)
             with self.env.cr.savepoint():
                 force_send = not(self.env.context.get('import_file', False))
-                template.send_mail(user.id, force_send=force_send, raise_exception=True)
+                template.send_mail(user.id, force_send=force_send, raise_exception=True, email_values=email_values)
             _logger.info("Password reset email sent for user <%s> to <%s>", user.login, user.email)
 
     def send_unregistered_user_reminder(self, after_days=5):
