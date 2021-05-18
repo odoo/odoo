1a4a030637938b9a5eff89fed8ada4eb1bb2397b
Victor Feyens <vfe@odoo.com>
2021-05-18 15:54:46 +0000

    [MOV] sale: split payment.py (2)

--- addons/sale/models/payment_transaction.py
+++ addons/sale/models/payment_transaction.py
@@ -10,17 +10,6 @@ from odoo.tools import float_compare
 _logger = logging.getLogger(__name__)
 
 
-class PaymentAcquirer(models.Model):
-    _inherit = 'payment.acquirer'
-
-    so_reference_type = fields.Selection(string='Communication',
-        selection=[
-            ('so_name', 'Based on Document Reference'),
-            ('partner', 'Based on Customer ID')], default='so_name',
-        help='You can set here the communication type that will appear on sales orders.'
-             'The communication will be given to the customer when they choose the payment method.')
-
-
 class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
 
