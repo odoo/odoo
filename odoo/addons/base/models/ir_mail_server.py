719bf8048d118677e6a89904c3c831f2d38872ed
std-odoo <std@odoo.com>
2021-05-19 09:57:39 +0200

    [REM] base: revert the rewriting of the FROM of the emails sent
    Purpose
    =======
    Revert the commit a757cab8578174860a57a7b34267e33d1a50a1c7
    because the rewriting of the FROM of the emails sent will
    be done in a smarter way in master.
    
    Task-2367946
    odoo/odoo#61853
    odoo/upgrade#1903

--- odoo/addons/base/models/ir_mail_server.py
+++ odoo/addons/base/models/ir_mail_server.py
@@ -305,12 +305,7 @@ class IrMailServer(models.Model):
         if references:
             msg['references'] = references
         msg['Subject'] = subject
-
-        email_from, return_path = self._get_email_from(email_from)
         msg['From'] = email_from
-        if return_path:
-            headers.setdefault('Return-Path', return_path)
-
         del msg['Reply-To']
         msg['Reply-To'] = reply_to or email_from
         msg['To'] = email_to
@@ -337,35 +332,6 @@ class IrMailServer(models.Model):
                 maintype, subtype = mime.split('/') if mime and '/' in mime else ('application', 'octet-stream')
                 msg.add_attachment(fcontent, maintype, subtype, filename=fname)
         return msg
-
-    def _get_email_from(self, email_from):
-        """Logic which determines which email to use when sending the email.
-
-        - If the system parameter `mail.force.smtp.from` is set we encapsulate all
-          outgoing email from
-        - If the previous system parameter is not set and if both `mail.dynamic.smtp.from`
-          and `mail.catchall.domain` are set, we encapsulate the FROM only if the domain
-          of the email is not the same as the domain of the catchall parameter
-        - Otherwise we do not encapsulate the email and given email_from is used as is
-
-        :param email_from: The initial FROM headers
-        :return: The FROM to used in the headers and optionally the Return-Path
-        """
-        force_smtp_from = self.env['ir.config_parameter'].sudo().get_param('mail.force.smtp.from')
-        dynamic_smtp_from = self.env['ir.config_parameter'].sudo().get_param('mail.dynamic.smtp.from')
-        catchall_domain = self.env['ir.config_parameter'].sudo().get_param('mail.catchall.domain')
-
-        if force_smtp_from:
-            rfc2822_force_smtp_from = extract_rfc2822_addresses(force_smtp_from)
-            rfc2822_force_smtp_from = rfc2822_force_smtp_from[0] if rfc2822_force_smtp_from else None
-            return encapsulate_email(email_from, force_smtp_from), rfc2822_force_smtp_from
-
-        elif dynamic_smtp_from and catchall_domain and email_domain_extract(email_from) != catchall_domain:
-            rfc2822_dynamic_smtp_from = extract_rfc2822_addresses(dynamic_smtp_from)
-            rfc2822_dynamic_smtp_from = rfc2822_dynamic_smtp_from[0] if rfc2822_dynamic_smtp_from else None
-            return encapsulate_email(email_from, dynamic_smtp_from), rfc2822_dynamic_smtp_from
-
-        return email_from, None
 
     @api.model
     def _get_default_bounce_address(self):
