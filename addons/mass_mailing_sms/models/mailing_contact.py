5d4208d44d9044a769a43130df94f9a4967f318e
Thibault Delavallée <tde@odoo.com>
2021-05-20 14:40:56 +0000

    [FIX] sms, various: fix override of sms and phone fields methods
    There are two mixin involved in SMS notifications
    
      * mail.thread: standard SMS sending capabilities;
      * mail.thread.phone: advanced use with support of blacklist;
    
    Both require to define fields to use when searching for phone numbers
    (generally phone and/or mobile). Those are ``_sms_get_number_fields`` for
    mail.thread standard implementation (in SMS module) and ``_phone_get_number
    _fields`` for mail.thread.phone advanced implementation (in phone_validation
    module).
    
    In this commit we make mail.thread.phone act like a more advanced version
    of mail.thread by making _sms_get_number_fields use result of _phone_get_number
    _fields, and not the inverse. It allows to remove some overrides notably in
    CRM having to define twice fields.
    
    Task ID-2528169
    
    closes odoo/odoo#71111
    
    Related: odoo/enterprise#18428
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mass_mailing_sms/models/mailing_contact.py
+++ addons/mass_mailing_sms/models/mailing_contact.py
@@ -10,6 +10,5 @@ class MailingContact(models.Model):
 
     mobile = fields.Char(string='Mobile')
 
-    def _sms_get_number_fields(self):
-        # TDE note: should override _phone_get_number_fields but ok as sms is in dependencies
+    def _phone_get_number_fields(self):
         return ['mobile']
