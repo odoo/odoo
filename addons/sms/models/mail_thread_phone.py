38090d2ab51f5b5fd37a04cfed03131ab78f373d
Thibault Delavallée <tde@odoo.com>
2021-05-20 14:40:56 +0000

    [FIX] sms, various: fix override of sms and phone fields methods
    There are two mixin involved in SMS notifications
    
      * mail.thread: standard SMS sending capabilities;
      * mail.thread.phone: advanced use with support of blacklist;
    
    Both require to define fields to use when searching for phone numbers
    (generally phone and/or mobile). Those are ``_sms_get_number_fields`` for
    mail.thread standard implementation (in SMS module) and ``_phone_get_number
    _fields`` for mail.thread.phone advanced implementation (in phone_validation
    module).
    
    In this commit we make mail.thread.phone act like a more advanced version
    of mail.thread by making _sms_get_number_fields use result of _phone_get_number
    _fields, and not the inverse. It allows to remove some overrides notably in
    CRM having to define twice fields.
    
    Task ID-2528169
    
    closes odoo/odoo#71111
    
    Related: odoo/enterprise#18428
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/sms/models/mail_thread_phone.py
+++ addons/sms/models/mail_thread_phone.py
@@ -7,10 +7,10 @@ from odoo import models
 class PhoneMixin(models.AbstractModel):
     _inherit = 'mail.thread.phone'
 
-    def _phone_get_number_fields(self):
-        """ Add fields coming from sms implementation. """
-        sms_fields = self._sms_get_number_fields()
-        res = super(PhoneMixin, self)._phone_get_number_fields()
-        for fname in (f for f in res if f not in sms_fields):
-            sms_fields.append(fname)
-        return sms_fields
+    def _sms_get_number_fields(self):
+        """ Add fields coming from mail.thread.phone implementation. """
+        phone_fields = self._phone_get_number_fields()
+        sms_fields = super(PhoneMixin, self)._sms_get_number_fields()
+        for fname in (f for f in sms_fields if f not in phone_fields):
+            phone_fields.append(fname)
+        return phone_fields
