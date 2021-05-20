fcd9e45df0a9d666472fab130293d52d4258f5ad
Jacky (trj) <trj@odoo.com>
2021-05-20 10:01:28 +0200

    [FIX] point_of_sale: random failure when reprint receipt Because the `receiptHtml` was taken from a component which was mounted outside of the DOM, it was hardly ever destroyed for some unknown reasons and the `el.outerHTML` could not be taken to create the receipt.

--- addons/point_of_sale/static/src/js/Screens/OrderManagementScreen/ControlButtons/ReprintReceiptButton.js
+++ addons/point_of_sale/static/src/js/Screens/OrderManagementScreen/ControlButtons/ReprintReceiptButton.js
@@ -6,7 +6,6 @@ odoo.define('point_of_sale.ReprintReceiptButton', function (require) {
     const PosComponent = require('point_of_sale.PosComponent');
     const OrderManagementScreen = require('point_of_sale.OrderManagementScreen');
     const Registries = require('point_of_sale.Registries');
-    const OrderReceipt = require('point_of_sale.OrderReceipt');
     const contexts = require('point_of_sale.PosContext');
 
     class ReprintReceiptButton extends PosComponent {
@@ -20,10 +19,7 @@ odoo.define('point_of_sale.ReprintReceiptButton', function (require) {
             if (!order) return;
 
             if (this.env.pos.proxy.printer) {
-                const fixture = document.createElement('div');
-                const orderReceipt = new (Registries.Component.get(OrderReceipt))(this, { order });
-                await orderReceipt.mount(fixture);
-                const receiptHtml = orderReceipt.el.outerHTML;
+                const receiptHtml = this.props.hiddenOrderReceipt.el.outerHTML;
                 const printResult = await this.env.pos.proxy.printer.print_receipt(receiptHtml);
                 if (!printResult.successful) {
                     this.showTempScreen('ReprintReceiptScreen', { order: order });
