a35700251a164ba520c9645832534738e420c985
Nikunj Ladava <nla@odoo.com>
2021-05-20 10:08:14 +0000

    [REM] l10n_cl: remove sequence from journal
    - latam document type was used in l10n_cl_sequence_ids and used the same
    table in l10n_cl_journal_ids but it was never used any where
    
    task- 2464212
    
    closes odoo/odoo#71095
    
    Related: odoo/upgrade#2478
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- addons/l10n_cl/__manifest__.py
+++ addons/l10n_cl/__manifest__.py
@@ -22,7 +22,6 @@ Plan contable chileno e impuestos de acuerdo a disposiciones vigentes
     'data': [
         'views/account_move_view.xml',
         'views/account_tax_view.xml',
-        'views/ir_sequence_view.xml',
         'views/res_bank_view.xml',
         'views/res_country_view.xml',
         'views/report_invoice.xml',
