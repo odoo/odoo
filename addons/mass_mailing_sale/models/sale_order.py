ec5640e7ed78c46d9345ddceeffd537790488020
Thibault Delavallée <tde@odoo.com>
2021-05-21 09:42:59 +0200

    [IMP] mass_mailing_{event/event_track/sale}: add default mailing domains
    When performing an email or SMS mailing on functional models a default domain
    is a good start to avoid sending notifications to dead records, aka
    
      * canceled registrations;
      * canceled tracks;
      * canceled sale orders;
    
    Task ID-2431217
    COM PR odoo#67322
    ENT PR odoo/enterprise#16876
    UPG PR odoo/upgrade#2236

--- addons/mass_mailing_sale/models/sale_order.py
+++ addons/mass_mailing_sale/models/sale_order.py
@@ -7,3 +7,7 @@ from odoo import models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     _mailing_enabled = True
+
+    def _mailing_get_default_domain(self, mailing):
+        """ Exclude by default canceled orders when performing a mass mailing. """
+        return [('state', '!=', 'cancel')]
