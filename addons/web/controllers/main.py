c3439d01bf4e765f5b7d5056af2b0927949cdba1
David Beguin <dbe@odoo.com>
2021-05-21 13:16:19 +0000

    [IMP] web: display toaster instead of dialog when change_password goes wrong
    This commit is part of the 2 factor authentication revamp.
    The purpose is to avoid to display error modals when it's possible to only
    display a notification toaser.
    
    Task-2487630
    
    Part-of: odoo/odoo#71142

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -1200,21 +1200,21 @@ class Session(http.Controller):
         old_password, new_password,confirm_password = operator.itemgetter('old_pwd', 'new_password','confirm_pwd')(
             {f['name']: f['value'] for f in fields})
         if not (old_password.strip() and new_password.strip() and confirm_password.strip()):
-            return {'error':_('You cannot leave any password empty.'),'title': _('Change Password')}
+            return {'error': _('You cannot leave any password empty.')}
         if new_password != confirm_password:
-            return {'error': _('The new password and its confirmation must be identical.'),'title': _('Change Password')}
+            return {'error': _('The new password and its confirmation must be identical.')}
 
         msg = _("Error, password not changed !")
         try:
             if request.env['res.users'].change_password(old_password, new_password):
-                return {'new_password':new_password}
+                return {'new_password': new_password}
         except AccessDenied as e:
             msg = e.args[0]
             if msg == AccessDenied().args[0]:
                 msg = _('The old password you provided is incorrect, your password was not changed.')
         except UserError as e:
             msg = e.args[0]
-        return {'title': _('Change Password'), 'error': msg}
+        return {'error': msg}
 
     @http.route('/web/session/get_lang_list', type='json', auth="none")
     def get_lang_list(self):
