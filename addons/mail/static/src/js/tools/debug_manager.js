2a1288c722872d5f9507c8b5e91952477a1eff2b
Julien Mougenot <jum@odoo.com>
2021-05-25 14:42:26 +0000

    [IMP] mail: adapt debug manager include

--- addons/mail/static/src/js/tools/debug_manager.js
+++ addons/mail/static/src/js/tools/debug_manager.js
@@ -1,30 +1,37 @@
-// /** @odoo-module **/
+/** @odoo-module **/
 
-// import core from 'web.core';
-// import DebugManager from 'web.DebugManager.Backend';
+import { registry } from "@web/core/registry";
 
-// var _t = core._t;
-// /**
-//  * adds a new method available for the debug manager, called by the "Manage Messages" button.
-//  *
-//  */
-// DebugManager.include({
-//     getMailMessages: function () {
-//         var selectedIDs = this._controller.getSelectedIds();
-//         if (!selectedIDs.length) {
-//             console.warn(_t("No message available"));
-//             return;
-//         }
-//         this.do_action({
-//             res_model: 'mail.message',
-//             name: _t('Manage Messages'),
-//             views: [[false, 'list'], [false, 'form']],
-//             type: 'ir.actions.act_window',
-//             domain: [['res_id', '=', selectedIDs[0]], ['model', '=', this._controller.modelName]],
-//             context: {
-//                 default_res_model: this._controller.modelName,
-//                 default_res_id: selectedIDs[0],
-//             },
-//         });
-//     },
-// });
+export function manageMessages({ action, component, env }) {
+    const selectedIds = component.widget.getSelectedIds();
+    if (!selectedIds.length) {
+        return null; // No record
+    }
+    const description = env._t("Manage Messages");
+    return {
+        type: "item",
+        description,
+        callback: () => {
+            env.services.action.doAction({
+                res_model: "mail.message",
+                name: description,
+                views: [
+                    [false, "list"],
+                    [false, "form"],
+                ],
+                type: "ir.actions.act_window",
+                domain: [
+                    ["res_id", "=", selectedIds[0]],
+                    ["model", "=", action.res_model],
+                ],
+                context: {
+                    default_res_model: action.res_model,
+                    default_res_id: selectedIds[0],
+                },
+            });
+        },
+        sequence: 325,
+    };
+}
+
+registry.category("debug").category("form").add("mail.manageMessages", manageMessages);
--- addons/mail/static/tests/tools/debug_manager_tests.js
+++ addons/mail/static/tests/tools/debug_manager_tests.js
@@ -1,62 +1,70 @@
 /** @odoo-module **/
 
-import testUtils from 'web.test_utils';
+import { manageMessages } from "@mail/js/tools/debug_manager";
+import { click, legacyExtraNextTick } from "@web/../tests/helpers/utils";
+import {
+    createWebClient,
+    doAction,
+    getActionManagerTestConfig,
+} from "@web/../tests/webclient/actions/helpers";
+import { debugService } from "@web/core/debug/debug_service";
+import { registry } from "@web/core/registry";
 
-var createDebugManager = testUtils.createDebugManager;
+QUnit.module("DebugMenu");
 
-QUnit.module('Mail DebugManager', {}, function () {
+QUnit.test("Manage Messages", async function (assert) {
+    assert.expect(6);
 
-    QUnit.skip("Manage Messages", async function (assert) {
-        assert.expect(3);
-
-        var debugManager = await createDebugManager({
-            intercepts: {
-                do_action: function (event) {
-                    assert.deepEqual(event.data.action, {
-                      context: {
-                        default_res_model: "testModel",
-                        default_res_id: 5,
-                      },
-                        res_model: 'mail.message',
-                        name: "Manage Messages",
-                        views: [[false, 'list'], [false, 'form']],
-                        type: 'ir.actions.act_window',
-                        domain: [['res_id', '=', 5], ['model', '=', 'testModel']],
-                    });
-                },
-            },
-        });
-
-        await debugManager.appendTo($('#qunit-fixture'));
-
-        // Simulate update debug manager from web client
-        var action = {
-            views: [{
-                displayName: "Form",
-                fieldsView: {
-                    view_id: 1,
-                },
-                type: "form",
-            }],
-        };
-        var view = {
-            viewType: "form",
-            getSelectedIds: function () {
-                return [5];
-            },
-            modelName: 'testModel',
-        };
-        await testUtils.nextTick();
-        await debugManager.update('action', action, view);
-
-        var $messageMenu = debugManager.$('a[data-action=getMailMessages]');
-        assert.strictEqual($messageMenu.length, 1, "should have Manage Message menu item");
-        assert.strictEqual($messageMenu.text().trim(), "Manage Messages",
-            "should have correct menu item text");
-
-        await testUtils.dom.click(debugManager.$('> a')); // open dropdown
-        await testUtils.dom.click($messageMenu);
-
-        debugManager.destroy();
+    const testConfig = Object.assign(getActionManagerTestConfig(), {
+        debug: "1",
     });
+
+    // Add fake "mail.message" model and arch
+    testConfig.serverData.models["mail.message"] = {
+        fields: { name: { string: "Name", type: "char" } },
+        records: [],
+    };
+    Object.assign(testConfig.serverData.views, {
+        "mail.message,false,list": `<tree/>`,
+        "mail.message,false,form": `<form/>`,
+        "mail.message,false,search": `<search/>`,
+    });
+
+    // Activate debug service with "Manage Message" item
+    registry.category("services").add("debug", debugService);
+    registry.category("debug").category("form").add("manageMessages", manageMessages);
+
+    async function mockRPC(route, args) {
+        if (args.method === "check_access_rights") {
+            return true;
+        }
+        if (route === "/web/dataset/search_read" && args.model === "mail.message") {
+            assert.strictEqual(args.context.default_res_id, 5);
+            assert.strictEqual(args.context.default_res_model, "partner");
+            assert.deepEqual(args.domain, ["&", ["res_id", "=", 5], ["model", "=", "partner"]]);
+        }
+    }
+
+    const wc = await createWebClient({ testConfig, mockRPC });
+    await doAction(wc, 3, { viewType: "form", resId: 5 });
+    await legacyExtraNextTick();
+    await click(wc.el, ".o_debug_manager .o_dropdown_toggler");
+
+    const dropdownItems = wc.el.querySelectorAll(
+        ".o_debug_manager .o_dropdown_menu .o_dropdown_item"
+    );
+    assert.strictEqual(dropdownItems.length, 1);
+    assert.strictEqual(
+        dropdownItems[0].innerText.trim(),
+        "Manage Messages",
+        "should have correct menu item text"
+    );
+
+    await click(dropdownItems[0]);
+    await legacyExtraNextTick();
+
+    assert.strictEqual(
+        wc.el.querySelector(".breadcrumb-item.active").innerText.trim(),
+        "Manage Messages"
+    );
 });
