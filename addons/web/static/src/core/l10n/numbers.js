4c3acc5b6d370715c900e2f8366676823858f9ab
Mathieu Duckerts-Antoine <dam@odoo.com>
2021-05-26 11:07:58 +0000

    [IMP] web: allow to parse more numbers
    The present commit reintroduces several number parsers that were previously defined in web.field_utils:
    parseFloatTime, parseInteger, parseMonetary, and parsePercentage. Some minor simplifications in their API
    have been brought. Notably, the "field" parameter has been removed. Indeed, we expect those parsers
    to be used in more specific contexts than before so that the advantage of having a common API (value, field, options)
    has become less important.

--- addons/web/static/src/core/l10n/numbers.js
+++ addons/web/static/src/core/l10n/numbers.js
@@ -1,8 +1,10 @@
 /** @odoo-module **/
 
-import { escapeRegExp, intersperse, sprintf } from "../utils/strings";
+import { escapeRegExp, intersperse } from "../utils/strings";
 import { localization } from "./localization";
 import { _lt } from "./translation";
+
+class InvalidNumberError extends Error {}
 
 // -----------------------------------------------------------------------------
 // Helpers
@@ -32,7 +34,7 @@ function insertThousandsSep(num, thousandsSep = ",", grouping = [3, 0]) {
  * @param {Object} options - additional options
  * @param {string|RegExp} [options.thousandsSep] - the thousands separator used in the value
  * @param {string|RegExp} [options.decimalPoint] - the decimal point used in the value
- * @returns number
+ * @returns {number}
  */
 function parseNumber(value, options = {}) {
     // a number can have the thousand separator multiple times. ex: 1,000,000.00
@@ -57,7 +59,7 @@ function parseNumber(value, options = {}) {
  * @param {number[]} [options.grouping]
  *   array of relative offsets at which to insert `thousandsSep`.
  *   See `numbers.insertThousandsSep` method.
- * @returns string
+ * @returns {string}
  */
 export function formatFloat(value, options = {}) {
     if (value === false) {
@@ -72,11 +74,10 @@ export function formatFloat(value, options = {}) {
 }
 
 /**
- * Parse a more readable formated string representing a float to a float
+ * Try to extract a float from a string. The localization is considered in the process.
  *
- * @param {string} value the formatted number
- * @returns float
- * @throws Error if the value can't be parsed properly
+ * @param {string} value
+ * @returns {number} a float
  */
 export function parseFloat(value) {
     let thousandsSepRegex = new RegExp(escapeRegExp(localization.thousandsSep), "g");
@@ -86,9 +87,102 @@ export function parseFloat(value) {
         decimalPoint: decimalPointRegex,
     });
     if (isNaN(parsed)) {
-        throw new Error(sprintf(_lt("'%s' is not a correct float").toString(), value));
+        throw new InvalidNumberError(`"${value}" is not a correct number`);
     }
     return parsed;
+}
+
+/**
+ * Try to extract a float time from a string. The localization is considered in the process.
+ * The float time can have two formats: float or integer:integer.
+ *
+ * @param {string} value
+ * @returns {number} a float
+ */
+export function parseFloatTime(value) {
+    let sign = 1;
+    if (value[0] === "-") {
+        value = value.slice(1);
+        sign = -1;
+    }
+    const values = value.split(":");
+    if (values.length > 2) {
+        throw new InvalidNumberError(`"${value}" is not a correct number`);
+    }
+    if (values.length === 1) {
+        return sign * parseFloat(value);
+    }
+    const hours = parseInteger(values[0]);
+    const minutes = parseInteger(values[1]);
+    return sign * (hours + minutes / 60);
+}
+
+/**
+ * Try to extract an integer from a string. The localization is considered in the process.
+ *
+ * @param {string} value
+ * @returns {number} an integer
+ */
+export function parseInteger(value) {
+    let thousandsSepRegex = new RegExp(escapeRegExp(localization.thousandsSep), "g");
+    let decimalPointRegex = new RegExp(escapeRegExp(localization.decimalPoint), "g");
+    const parsed = parseNumber(value, {
+        thousandsSep: thousandsSepRegex,
+        decimalPoint: decimalPointRegex,
+    });
+    if (!Number.isInteger(parsed)) {
+        throw new InvalidNumberError(`"${value}" is not a correct number`);
+    }
+    return parsed;
+}
+
+/**
+ * Try to extract a monetary value from a string. The localization is considered in the process.
+ * The monetary value can have the formats sym$&nbsp;float, float$&nbsp;sym or float
+ * where $&nbsp; is a non breaking space and sym is a currency symbol.
+ * If a symbol is found it must correspond to the default currency symbol or to the
+ * symbol of the currency whose id is passed in options.
+ *
+ * @param {string} value
+ * @param {Object} [options={}]
+ * @param {number} [options.currencyId]
+ * @returns {number} float
+ */
+export function parseMonetary(value, options = {}) {
+    let values = value.split("&nbsp;");
+    if (values.length === 1) {
+        return parseFloat(value);
+    }
+    let currency;
+    if (options.currencyId) {
+        currency = odoo.session_info.currencies[options.currencyId];
+    } else {
+        currency = Object.values(odoo.session_info.currencies)[0];
+    }
+    const symbolIndex = values.findIndex((v) => v === currency.symbol);
+    if (symbolIndex === -1) {
+        throw new InvalidNumberError(`"${value}" is not a correct number`);
+    }
+    values.splice(symbolIndex, 1);
+    if (values.length !== 1) {
+        throw new InvalidNumberError(`"${value}" is not a correct number`);
+    }
+    return parseFloat(values[0]);
+}
+
+/**
+ * Try to extract a float from a string and unconvert it with a conversion factor of 100.
+ * The localization is considered in the process.
+ * The percentage can have two formats: float or float%.
+ *
+ * @param {string} value
+ * @returns {number} float
+ */
+export function parsePercentage(value) {
+    if (value[value.length - 1] === "%") {
+        value = value.slice(0, value.length - 1);
+    }
+    return parseFloat(value) / 100;
 }
 
 /**
@@ -102,7 +196,7 @@ export function parseFloat(value) {
  *    the minimum number of digits to preserve when switching to another
  *    level of thousands (e.g. with a value of '2', 4321 will still be
  *    represented as 4321 otherwise it will be down to one digit (4k))
- * @returns string
+ * @returns {string}
  */
 export function humanNumber(number, options = { decimals: 0, minDigits: 1 }) {
     number = Math.round(number);
