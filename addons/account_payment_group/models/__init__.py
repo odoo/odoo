f707463ebef3e87dce7b5c26210e1237e5cfc23a
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-05-27 09:59:47 -0300

    [ADD] account_payment_group

---
+++ addons/account_payment_group/models/__init__.py
@@ -0,0 +1,8 @@
+from . import account_payment
+from . import account_payment_group
+from . import account_move
+# from . import account_move_line
+# from . import res_company
+# from . import account_journal_dashboard
+# from . import account_bank_statement_line
+# from . import payment_transaction
