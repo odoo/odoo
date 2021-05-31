20c5bddd5902a96fe8d0c27cd308d77c4771b3f7
Michael Mattiello (mcm) <mcm@odoo.com>
2021-05-31 13:22:56 +0000

    [IMP] web: improve error management
    - Error handlers have been simplified, handlers don't returns functions
    anymore and take 3 params: env, uncaughtError and originalError.
    - Source maps have been reintroduced.
    - The original error message and name are now concatenated to
    the "wrapper" error ones.
    
    closes odoo-dev/odoo#895
    
    Related: odoo-dev/enterprise#154
    Signed-off-by: Géry Debongnie (ged) <ged@openerp.com>

--- addons/web/static/src/core/errors/error_service.js
+++ addons/web/static/src/core/errors/error_service.js
@@ -1,9 +1,9 @@
 /** @odoo-module **/
 
 import { browser } from "../browser/browser";
-import { isBrowserChrome } from "../browser/feature_detection";
 import { _lt } from "../l10n/translation";
 import { registry } from "../registry";
+import { annotateTraceback, formatTraceback, getErrorTechnicalName } from "./error_utils";
 
 /**
  * Uncaught Errors have 4 properties:
@@ -14,41 +14,70 @@ import { registry } from "../registry";
  *      necessarily an error (for ex, if some code does throw "boom")
  */
 export class UncaughtError extends Error {
-    constructor(message, name) {
+    constructor(message) {
         super(message);
-        this.name = name || "UncaughtError";
-        this.originalError = null;
+        this.name = getErrorTechnicalName(this);
         this.traceback = null;
     }
 }
 
 export class UncaughtClientError extends UncaughtError {
     constructor(message = _lt("Uncaught Javascript Error")) {
-        super(message, "UncaughtClientError");
+        super(message);
     }
 }
 
 export class UncaughtPromiseError extends UncaughtError {
     constructor(message = _lt("Uncaught Promise")) {
-        super(message, "UncaughtPromiseError");
+        super(message);
         this.unhandledRejectionEvent = null;
     }
 }
 
 export class UncaughtCorsError extends UncaughtError {
     constructor(message = _lt("Uncaught CORS Error")) {
-        super(message, "UncaughtCorsError");
+        super(message);
+    }
+}
+
+/**
+ * @param {UncaughtError} uncaughtError
+ * @param {Error} originalError
+ * @returns {string}
+ */
+function combineErrorNames(uncaughtError, originalError) {
+    const originalErrorName = getErrorTechnicalName(originalError);
+    const uncaughtErrorName = getErrorTechnicalName(uncaughtError);
+    if (originalErrorName === Error.name) {
+        return uncaughtErrorName;
+    } else {
+        return `${uncaughtErrorName} > ${originalErrorName}`;
+    }
+}
+
+/**
+ * @param {import("../../env").OdooEnv} env
+ * @param {UncaughtError} uncaughtError
+ * @param {Error} originalError
+ * @returns {Promise<void>}
+ */
+async function completeUncaughtError(env, uncaughtError, originalError) {
+    uncaughtError.name = combineErrorNames(uncaughtError, originalError);
+    if (env.debug.includes("assets")) {
+        uncaughtError.traceback = await annotateTraceback(originalError);
+    } else {
+        uncaughtError.traceback = formatTraceback(originalError);
+    }
+    if (originalError.message) {
+        uncaughtError.message = `${uncaughtError.message} > ${originalError.message}`;
     }
 }
 
 export const errorService = {
     start(env) {
-        const handlers = registry
-            .category("error_handlers")
-            .getAll()
-            .map((builder) => builder(env));
+        const handlers = registry.category("error_handlers").getAll();
 
-        function handleError(error, retry = true) {
+        function handleError(error, originalError, retry = true) {
             const services = env.services;
             if (!services.dialog || !services.notification || !services.rpc) {
                 // here, the environment is not ready to provide feedback to the user.
@@ -56,25 +85,25 @@ export const errorService = {
                 // recover.
                 if (retry) {
                     browser.setTimeout(() => {
-                        handleError(error, false);
+                        handleError(error, originalError, false);
                     }, 1000);
                 }
                 return;
             }
             for (let handler of handlers) {
-                if (handler(error, env)) {
+                if (handler(env, error, originalError)) {
                     break;
                 }
             }
             env.bus.trigger("ERROR_DISPATCHED", error);
         }
 
-        window.addEventListener("error", (ev) => {
-            const { colno, error: eventError, filename, lineno, message } = ev;
-            let err;
+        window.addEventListener("error", async (ev) => {
+            const { colno, error: originalError, filename, lineno, message } = ev;
+            let uncaughtError;
             if (!filename && !lineno && !colno) {
-                err = new UncaughtCorsError();
-                err.traceback = env._t(
+                uncaughtError = new UncaughtCorsError();
+                uncaughtError.traceback = env._t(
                     `Unknown CORS error\n\n` +
                         `An unknown CORS error occured.\n` +
                         `The error probably originates from a JavaScript file served from a different origin.\n` +
@@ -82,37 +111,23 @@ export const errorService = {
                 );
             } else {
                 // ignore Chrome video internal error: https://crbug.com/809574
-                if (!eventError && message === "ResizeObserver loop limit exceeded") {
+                if (!originalError && message === "ResizeObserver loop limit exceeded") {
                     return;
                 }
-                let stack = eventError ? eventError.stack : "";
-                if (!isBrowserChrome()) {
-                    // transforms the stack into a chromium stack
-                    // Chromium stack example:
-                    // Error: Mock: Can't write value
-                    //     _onOpenFormView@http://localhost:8069/web/content/425-baf33f1/web.assets.js:1064:30
-                    //     ...
-                    stack = `${message}\n${stack}`.replace(/\n/g, "\n    ");
-                }
-                err = new UncaughtClientError();
-                err.originalError = eventError;
-                err.traceback = `${message}\n\n${filename}:${lineno}\n${env._t(
-                    "Traceback"
-                )}:\n${stack}`;
+                uncaughtError = new UncaughtClientError();
+                await completeUncaughtError(env, uncaughtError, originalError);
             }
-            handleError(err);
+            handleError(uncaughtError, originalError);
         });
 
-        window.addEventListener("unhandledrejection", (ev) => {
-            const uncaughtError = ev.reason;
-            const error = new UncaughtPromiseError();
-            error.unhandledRejectionEvent = ev;
-            error.originalError = uncaughtError;
-            if (uncaughtError instanceof Error) {
-                error.message = uncaughtError.message;
-                error.traceback = uncaughtError.stack; // todo: do same computation as regular errors
+        window.addEventListener("unhandledrejection", async (ev) => {
+            const originalError = ev.reason;
+            const uncaughtError = new UncaughtPromiseError();
+            uncaughtError.unhandledRejectionEvent = ev;
+            if (originalError instanceof Error) {
+                await completeUncaughtError(env, uncaughtError, originalError);
             }
-            handleError(error);
+            handleError(uncaughtError, originalError);
         });
     },
 };
