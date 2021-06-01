b2fedca4196c1079a9fbd78704e97e97f9340a14
Robin Heinz <rhe@odoo.com>
2021-06-01 11:40:33 +0200

    [FIX] point_of_sale: Fix order management reprint infinite recursive loop
    The were few times where we ended up with a infinite recursion while clicking on an order in the order management screen.
    This lead to a browser stuck.
    Now we don't use a temporary screen anymore and use the same mechanism of the receiptScreen to print the order.
    
    X-original-commit: 2ea13121cf82c4c9b069363cfd2ef2857fc8ccaf

--- addons/point_of_sale/static/src/js/Screens/OrderManagementScreen/ControlButtons/ReprintReceiptButton.js
+++ addons/point_of_sale/static/src/js/Screens/OrderManagementScreen/ControlButtons/ReprintReceiptButton.js
@@ -6,7 +6,6 @@ odoo.define('point_of_sale.ReprintReceiptButton', function (require) {
     const PosComponent = require('point_of_sale.PosComponent');
     const OrderManagementScreen = require('point_of_sale.OrderManagementScreen');
     const Registries = require('point_of_sale.Registries');
-    const OrderReceipt = require('point_of_sale.OrderReceipt');
     const contexts = require('point_of_sale.PosContext');
 
     class ReprintReceiptButton extends PosComponent {
@@ -19,18 +18,7 @@ odoo.define('point_of_sale.ReprintReceiptButton', function (require) {
             const order = this.orderManagementContext.selectedOrder;
             if (!order) return;
 
-            if (this.env.pos.proxy.printer) {
-                const fixture = document.createElement('div');
-                const orderReceipt = new (Registries.Component.get(OrderReceipt))(this, { order });
-                await orderReceipt.mount(fixture);
-                const receiptHtml = orderReceipt.el.outerHTML;
-                const printResult = await this.env.pos.proxy.printer.print_receipt(receiptHtml);
-                if (!printResult.successful) {
-                    this.showTempScreen('ReprintReceiptScreen', { order: order });
-                }
-            } else {
-                this.showTempScreen('ReprintReceiptScreen', { order: order });
-            }
+            this.showScreen('ReprintReceiptScreen', { order: order });
         }
     }
     ReprintReceiptButton.template = 'ReprintReceiptButton';
