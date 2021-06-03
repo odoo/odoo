7e81134c712cc659e6b006d4b6de63725aa5bc1f
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2021-06-03 10:19:47 +0000

    [FIX] account: fix Average Price measure
    Open Accounting>Reporting>Invoices
    Add measure 'Average Price'
    
    The reported amount will be wrong, as it will not consider the quantity,
    making an average of the price subtotal
    
    opw-2522621
    
    closes odoo/odoo#71664
    
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>

--- addons/account/report/account_invoice_report.py
+++ addons/account/report/account_invoice_report.py
@@ -107,7 +107,11 @@ class AccountInvoiceReport(models.Model):
                 line.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) * (CASE WHEN move.type IN ('in_invoice','out_refund','in_receipt') THEN -1 ELSE 1 END)
                                                                             AS quantity,
                 -line.balance                                               AS price_subtotal,
-                -line.balance / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0)
+                -COALESCE(line.balance
+                   / NULLIF(line.quantity, 0.0)
+                   / NULLIF(COALESCE(uom_line.factor, 1), 0.0)
+                   / NULLIF(COALESCE(uom_template.factor, 1), 0.0),
+                   0.0)
                                                                             AS price_average,
                 COALESCE(partner.country_id, commercial_partner.country_id) AS country_id,
                 1                                                           AS nbr_lines
