3a4a1cd24325eb5c9a456a0967072760b78d6bc2
Rémy Voet (ryv) <ryv@odoo.com>
2021-06-04 12:24:39 +0000

    [IMP] base: add warning if Chrome not installed
    For test tours.

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -846,6 +846,7 @@ class ChromeBrowser():
             # TODO: handle windows platform: https://stackoverflow.com/a/40674915
             pass
 
+        self._logger.warning("Chrome executable not found")
         raise unittest.SkipTest("Chrome executable not found")
 
     def _spawn_chrome(self, cmd):
