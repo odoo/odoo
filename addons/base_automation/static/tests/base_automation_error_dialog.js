3016ece358724454ac394a8f33121ef38a9f36cc
Samuel Degueldre <sad@odoo.com>
2021-06-07 11:23:01 +0000

    [REF] base_automation: convert error dialog to wowl
    closes odoo-dev/odoo#924
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/base_automation/static/tests/base_automation_error_dialog.js
+++ addons/base_automation/static/tests/base_automation_error_dialog.js
@@ -1,75 +1,130 @@
-odoo.define('base_automation.BaseAutomatioErrorDialogTests', function (require) {
-'use strict';
+/** @odoo-modules */
 
-    const CrashManager = require('web.CrashManager').CrashManager;
-    const session = require('web.session');
-    const makeTestEnvironment = require("web.test_env");
+import session from "web.session";
+import { registry } from "@web/core/registry";
+import { errorService } from "@web/core/errors/error_service";
+import { dialogService } from "@web/core/dialog/dialog_service";
+import { notificationService } from "@web/core/notifications/notification_service";
+import { uiService } from "@web/core/ui_service";
+import { hotkeyService } from "@web/webclient/hotkeys/hotkey_service";
+import { registerCleanup } from "@web/../tests/helpers/cleanup";
+import { makeTestEnv } from "@web/../tests/helpers/mock_env";
+import { makeFakeRPCService, makeFakeLocalizationService } from "@web/../tests/helpers/mock_services";
+import { RPCError } from "@web/core/network/rpc_service";
 
-    QUnit.module('base_automation', {}, function () {
+import { BaseAutomationErrorDialog } from "../src/js/base_automation_error_dialog";
 
-        QUnit.module('Error Dialog');
+const serviceRegistry = registry.category("services");
 
-        QUnit.test('Error due to an automated action', async function (assert) {
-            assert.expect(4);
+QUnit.module("base_automation", {}, function () {
 
-            let baseAutomationName = 'Test base automation error dialog';
-            let error = {
-                type: 'Odoo Client Error',
-                message: 'Message',
-                data: {
-                    debug: 'Traceback',
-                    context: {
-                        exception_class: 'base_automation',
-                        base_automation: {
-                            id: 1,
-                            name: baseAutomationName,
-                        },
-                    },
-                },
+    let unhandledRejectionCb;
+    QUnit.module("Error Dialog", {
+        async beforeEach() {
+            serviceRegistry.add("dialog", dialogService);
+            serviceRegistry.add("ui", uiService);
+            serviceRegistry.add("error", errorService);
+            serviceRegistry.add("hotkey", hotkeyService);
+            serviceRegistry.add("localization", makeFakeLocalizationService());
+            serviceRegistry.add("action", { start: () => {} });
+            serviceRegistry.add("orm", { start: () => {} });
+            serviceRegistry.add("user", { start: () => ({ isAdmin: true }) });
+            registry.category("error_dialogs").add("base_automation", BaseAutomationErrorDialog);
+            // Both of these are unused but required for the error service to call error handlers
+            serviceRegistry.add("notification", notificationService);
+            serviceRegistry.add("rpc", makeFakeRPCService());
+            const windowAddEventListener = window.addEventListener;
+            window.addEventListener = (type, cb) => {
+                if (type === "unhandledrejection") {
+                    unhandledRejectionCb = cb;
+                }
             };
-            // Force the user session to be admin, to display the disable and edit action buttons,
-            // then reset back to the origin value after the test.
-            let isAdmin = session.is_admin;
-            session.is_admin = true;
+            registerCleanup(() => {
+                window.addEventListener = windowAddEventListener;
+            });
+        },
+    });
 
-            let crashManager = new CrashManager();
-            crashManager.env = makeTestEnvironment();
-            let dialog = crashManager.show_error(error);
+    QUnit.test("Error due to an automated action", async function (assert) {
+        assert.expect(5);
 
-            await dialog._opened;
-
-            assert.containsOnce(document.body, '.modal .o_clipboard_button');
-            assert.containsOnce(document.body, '.modal .o_disable_action_button');
-            assert.containsOnce(document.body, '.modal .o_edit_action_button');
-            assert.ok(dialog.$el.text().indexOf(baseAutomationName) !== -1);
-
-            session.is_admin = isAdmin;
-            crashManager.destroy();
+        const error = new RPCError();
+        const errorContext = {
+            exception_class: "base_automation",
+            base_automation: {
+                id: 1,
+                name: "Test base automation error dialog",
+            },
+        };
+        Object.assign(error, {
+            subType: "Odoo Client Error",
+            message: "Message",
+            data: {
+                debug: "Traceback",
+                context: errorContext,
+            },
+            exceptionName: errorContext.exception_class,
         });
 
-        QUnit.test('Error not due to an automated action', async function (assert) {
-            assert.expect(3);
+        const env = await makeTestEnv();
+        const dialogOpened = new Promise((resolve) => {
+            env.services.dialog.bus.on("ADD", null, async ({ class: dialogClass, props }) => {
+                assert.equal(props.data.context, errorContext, "Received the correct error context");
+                assert.equal(dialogClass, BaseAutomationErrorDialog, "Should open the correct dialog class");
+                const fixture = document.getElementById("qunit-fixture");
+                const dialogContainer = document.createElement('div');
+                dialogContainer.classList.add('o_dialog_container');
+                fixture.appendChild(dialogContainer);
+                await owl.mount(dialogClass, { target: fixture, props, env });
+                assert.containsOnce(dialogContainer, '.modal .fa-clipboard');
+                assert.containsOnce(dialogContainer, '.modal .o_disable_action_button');
+                assert.containsOnce(dialogContainer, '.modal .o_edit_action_button');
+                resolve();
+            });
+        });
+        const errorEvent = new PromiseRejectionEvent("error", { reason: {
+            message: error,
+            legacy: true,
+            event: $.Event(),
+        }, promise: null });
+        await unhandledRejectionCb(errorEvent);
+        await dialogOpened;
+    });
 
-            let error = {
-                type: 'Odoo Client Error',
-                message: 'Message',
-                data: {
-                    debug: 'Traceback',
-                },
-            };
-            let crashManager = new CrashManager();
-            crashManager.env = makeTestEnvironment();
-            let dialog = crashManager.show_error(error);
+    QUnit.test("Error not due to an automated action", async function (assert) {
+        assert.expect(4);
 
-            await dialog._opened;
-
-            assert.containsOnce(document.body, '.modal .o_clipboard_button');
-            assert.containsNone(document.body, '.modal .o_disable_action_button');
-            assert.containsNone(document.body, '.modal .o_edit_action_button');
-
-            crashManager.destroy();
+        const error = new RPCError();
+        Object.assign(error, {
+            subType: "Odoo Client Error",
+            message: "Message",
+            data: {
+                debug: "Traceback",
+            },
         });
 
+        const env = await makeTestEnv();
+        const dialogOpened = new Promise((resolve) => {
+            env.services.dialog.bus.on("ADD", null, async ({ class: dialogClass, props }) => {
+                assert.notEqual(dialogClass, BaseAutomationErrorDialog, "Should not open base_automation dialog on other errors");
+                const fixture = document.getElementById("qunit-fixture");
+                const dialogContainer = document.createElement('div');
+                dialogContainer.classList.add('o_dialog_container');
+                fixture.appendChild(dialogContainer);
+                await owl.mount(dialogClass, { target: fixture, props, env });
+                assert.containsOnce(dialogContainer, '.modal .fa-clipboard');
+                assert.containsNone(dialogContainer, '.modal .o_disable_action_button');
+                assert.containsNone(dialogContainer, '.modal .o_edit_action_button');
+                resolve();
+            });
+        });
+        const errorEvent = new PromiseRejectionEvent("error", { reason: {
+            message: error,
+            legacy: true,
+            event: $.Event(),
+        }, promise: null });
+        await unhandledRejectionCb(errorEvent);
+        await dialogOpened;
     });
 
 });
