86c639cb1408d1f3004296008aa8d2e5bf759aa4
Thibault Delavallée <tde@odoo.com>
2021-06-07 06:17:06 +0000

    [IMP] website_event_exhibitor: improve sponsor type management
    Choose lowest sponsor type as default when creating a new sponsor. Purpose
    is to avoid being blocked by required fields. Choosing the lowest available
    sponsor type allows to ease discovering the feature without breaking any
    feature.
    
    Also set a default sequence when creating a sponsor type to avoid jump
    effect on list view: always send new sponsor types at the end.
    
    Also set default ribbon as no ribbon to avoid a "void" entry in ribbon style
    when creating new sponsor types.
    
    Task ID-2558875
    PR odoo/odoo#71787

--- addons/website_event_exhibitor/models/event_sponsor_type.py
+++ addons/website_event_exhibitor/models/event_sponsor_type.py
@@ -9,10 +9,12 @@ class SponsorType(models.Model):
     _description = 'Event Sponsor Level'
     _order = "sequence"
 
+    def _default_sequence(self):
+        return (self.search([], order="sequence desc", limit=1).sequence or 0) + 1
+
     name = fields.Char('Sponsor Level', required=True, translate=True)
-    sequence = fields.Integer('Sequence')
-    display_ribbon_style = fields.Selection([
-        ('no_ribbon', 'No Ribbon'),
-        ('Gold', 'Gold'),
-        ('Silver', 'Silver'),
-        ('Bronze', 'Bronze')], string='Ribbon Style')
+    sequence = fields.Integer('Sequence', default=_default_sequence)
+    display_ribbon_style = fields.Selection(
+        [('no_ribbon', 'No Ribbon'), ('Gold', 'Gold'),
+         ('Silver', 'Silver'), ('Bronze', 'Bronze')],
+        string='Ribbon Style', default='no_ribbon')
