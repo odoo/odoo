618562802efd59c801dff658c0b674768c1c9d35
Xavier Morel <xmo@odoo.com>
2021-06-07 11:29:02 +0000

    [IMP] [sale_]product_matrix: remove t-raw
    Using `sale_product_matrix`'s tour as `product_matrix` doesn't
    actually have any test for this.
    
    Remove t-raw from the product_matrix(.extra_price) template, by moving
    the formatting to the client side (unclear why it was done by the
    server in the first place).
    
    Also fixes a bug where a negative price_extra would have *two* `-`
    signs: one before the currency, and one after the currency: the price
    being formatted should be abs'd to avoid a negation being generated by
    the monetary formatting. Also uses non-breaking spaces everywhere
    where the server-side formatting mixed breaking and non-breaking
    spaces. Keeps from the original the peculiarity that the extra price
    should always have a sign positioned before prefix currency signs.
    
    Also changes the calling conventions of `product_matrix.extra_price`:
    instead of being called with a formatted `price` it's now called with
    the entire cell object.

--- addons/product_matrix/tests/common.py
+++ addons/product_matrix/tests/common.py
@@ -44,3 +44,8 @@ class TestMatrixCommon(common.HttpCase):
                 'value_ids': [(6, 0, attribute.value_ids.ids)]
             }) for attribute in product_attributes],
         })
+        def get_ptav(pav_name):
+            return self.env['product.template.attribute.value']\
+                .search([('product_attribute_value_id.name', '=', pav_name)])
+        get_ptav('PAV12').price_extra = 50
+        get_ptav('PAV31').price_extra = -25
