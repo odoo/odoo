e5fc198c0bec5faaed4b21e654a6baa9a950cbe0
Didier (did) <did@odoo.com>
2021-06-10 14:28:35 +0200

    [FIX] mail: compute the threadView scroll position inside the model

--- addons/mail/static/src/models/message_list_view/message_list_view.js
+++ addons/mail/static/src/models/message_list_view/message_list_view.js
@@ -6,11 +6,89 @@ import { attr, one } from '@mail/model/model_field';
 registerModel({
     name: 'MessageListView',
     identifyingFields: ['threadViewOwner'],
+    recordMethods: {
+        /**
+         * @private
+         * @returns {boolean}
+         */
+        _computeIsAtBottom() {
+            return (
+                this.scrollTop >= this.scrollHeight - this.clientHeight - this.endThreshold
+            );
+        },
+        /***
+         * @private
+         * @returns {boolean}
+         */
+        _computeIsAtEnd() {
+            if (this.threadViewOwner.order === 'asc') {
+                return this.isAtBottom;
+            }
+            return this.isAtTop;
+        },
+        /**
+         * @private
+         * @returns {boolean}
+         */
+        _computeIsAtStart() {
+            if (this.threadViewOwner.order === 'asc') {
+                return this.isAtTop;
+            }
+            return this.isAtBottom;
+        },
+        /**
+         * @private
+         * @returns {boolean}
+         */
+        _computeIsAtTop() {
+            return this.scrollTop <= this.endThreshold;
+        },
+    },
     fields: {
+        clientHeight: attr(),
         /**
          * States the OWL component of this message list view
          */
         component: attr(),
+        /**
+         * The margin that we use to detect that the scrollbar is a the end of
+         * the threadView.
+         */
+        endThreshold: attr({
+            default: 30,
+        }),
+        /**
+         * States whether the message list scroll position is at the bottom
+         * of the message list.
+         */
+        isAtBottom: attr({
+            compute: '_computeIsAtBottom',
+        }),
+        /**
+         * States whether the message list scroll position is at the end of
+         * the message list. Depending of the message list order, this could be
+         * the top or the bottom.
+         */
+        isAtEnd: attr({
+            compute: '_computeIsAtEnd',
+        }),
+        /**
+         * States whether the message list scroll position is at the start
+         * of the message list. Depending of the message list order, this could
+         * be the top or the bottom.
+         */
+        isAtStart: attr({
+            compute: '_computeIsAtStart',
+        }),
+        /**
+         * States whether the message list scroll position is at the top of
+         * the message list.
+         */
+        isAtTop: attr({
+            compute: '_computeIsAtTop',
+        }),
+        scrollHeight: attr(),
+        scrollTop: attr(),
         threadViewOwner: one('ThreadView', {
             inverse: 'messageListView',
             readonly: true,
