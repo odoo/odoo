0ee4e627a8342127ccdd147fb1bf5376f9375da1
AaronHForgeFlow <ahenriquez@eficent.com>
2021-06-10 16:29:59 +0200

    [FIX] sale_mrp: stock_account_get_anglo_saxon_price_unit on multicompany environments

--- addons/sale_mrp/models/account_move.py
+++ addons/sale_mrp/models/account_move.py
@@ -11,8 +11,8 @@ class AccountMoveLine(models.Model):
 
         so_line = self.sale_line_ids and self.sale_line_ids[-1] or False
         if so_line:
-            bom = so_line.product_id.product_tmpl_id.bom_ids and so_line.product_id.product_tmpl_id.bom_ids[0]
-            if bom.type == 'phantom':
+            bom = so_line.product_id.product_tmpl_id.bom_ids.filtered(lambda b: not b.company_id or b.company_id == so_line.company_id)[:1]
+            if bom and bom.type == 'phantom':
                 qty_to_invoice = self.product_uom_id._compute_quantity(self.quantity, self.product_id.uom_id)
                 qty_invoiced = sum([x.product_uom_id._compute_quantity(x.quantity, x.product_id.uom_id) for x in so_line.invoice_lines if x.move_id.state == 'posted'])
                 moves = so_line.move_ids
