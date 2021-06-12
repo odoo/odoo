e42ca291edfc15171b5bc1ae9b5a1738c08e39f4
Géry Debongnie <ged@odoo.com>
2021-06-12 09:15:04 +0000

    [REF] web: refactor notification system
    This commit does some improvements on notification system:
    
    - move notification handling logic in service
    - remove use of __destroy
    - simplify types
    
    closes odoo-dev/odoo#955
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/core/notifications/notification_container.js
+++ addons/web/static/src/core/notifications/notification_container.js
@@ -1,62 +1,25 @@
 /** @odoo-module **/
 
-import { Notification as NotificationComponent } from "./notification";
+import { Notification } from "./notification";
 
 const { Component, tags } = owl;
 
 export class NotificationContainer extends Component {
     setup() {
-        this.notifications = [];
-        this.props.bus.on("ADD", this, this.add);
-        this.props.bus.on("REMOVE", this, this.remove);
-    }
-    __destroy() {
-        this.props.bus.off("ADD", this, this.add);
-        this.props.bus.off("REMOVE", this, this.remove);
-        super.__destroy();
-    }
-
-    /**
-     * @param {Object} params
-     * @param {number} params.id
-     * @param {{
-     *      name: string;
-     *      icon?: string;
-     *      primary?: boolean;
-     *      onClick: () => void
-     * }[]} params.buttons
-     * @param {string} params.className
-     * @param {string} params.message
-     * @param {boolean} params.messageIsHtml
-     * @param {() => void} params.onClose
-     * @param {string} params.title
-     * @param {"warning" | "danger" | "success" | "info"} params.type
-     */
-    add(params) {
-        const { id, ...props } = params;
-        this.notifications.push({ id, props });
-        this.render();
-    }
-    /**
-     * @param {number} id
-     */
-    remove(id) {
-        const index = this.notifications.findIndex((n) => n.id === id);
-        if (index > -1) {
-            this.notifications.splice(index, 1);
-            this.render();
-        }
+        // this works, but then this component cannot be unmounted, then
+        // remounted. would need a destroyed hook different from willunmount
+        this.props.bus.on("UPDATE", this, this.render);
     }
 }
 
 NotificationContainer.template = tags.xml`
     <div class="o_notification_manager">
-        <t t-foreach="notifications" t-as="notification" t-key="notification.id">
-            <NotificationComponent
+        <t t-foreach="props.notifications" t-as="notification" t-key="notification.id">
+            <Notification
                 t-props="notification.props"
                 t-transition="o_notification_fade"
-                t-on-close="remove(notification.id)"
+                t-on-close="notification.close()"
             />
         </t>
     </div>`;
-NotificationContainer.components = { NotificationComponent };
+NotificationContainer.components = { Notification };
