11be071799d3d447d12e99f4379aefe0d85b688e
Christophe Monniez <moc@odoo.com>
2021-06-14 16:12:36 +0200

    [IMP] iap, various: implement _neutralize method
    An overridable model method was added in a previous commit in order to
    neutralize a database.
    
    This commit adds the implementation of this method on the `iap.account` model.
    A _get_iap_config_paremeters method is also added on this model.
    Each iap service must override this method to extend the list of
    `ir.config_parameter` that need to be neutralized.

--- addons/partner_autocomplete/models/__init__.py
+++ addons/partner_autocomplete/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import iap_account
 from . import iap_autocomplete_api
 from . import ir_http
 from . import res_partner
