a81942cf7efc6ec75b0fb1d30e243fc7a5b6f529
William Braeckman <wbr@odoo.com>
2021-06-14 09:56:43 +0200

    [MOV] hr_fleet: Split model files according to coding guidelines
    Applies the same changes as 6747402a96d3bd6013aa9e0ec29bca17c8ffc4a9 to
    hr_fleet
    
    Task ID: 2415309

--- addons/hr_fleet/models/__init__.py
+++ addons/hr_fleet/models/__init__.py
@@ -6,4 +6,6 @@ from . import res_users
 from . import fleet_vehicle_assignation_log
 from . import fleet_category
 from . import fleet_vehicle
-from . import fleet_vehicle_cost
+from . import fleet_vehicle_log_contract
+from . import fleet_vehicle_log_services
+from . import fleet_vehicle_odometer
