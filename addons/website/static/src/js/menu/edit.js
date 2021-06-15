f81f06b3b11618c28673843809c964f3b9bda907
Sylvain Francis <syf@odoo.com>
2021-06-15 13:43:29 +0000

    [FIX] website: blog post text editable
    Since f17506676 if when editing a page there was several elements
    matching:
    
        .oe_structure.oe_empty, [data-oe-type="html"]
    
    If at least one was empty we would not be able to edit any of them.
    
    opw-2572176
    opw-2572151
    opw-2571217
    
    closes odoo/odoo#72172
    
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- addons/website/static/src/js/menu/edit.js
+++ addons/website/static/src/js/menu/edit.js
@@ -135,7 +135,7 @@ var EditPageMenu = websiteNavbarData.WebsiteNavbarActionWidget.extend({
         const $target = this._targetForEdition();
         const $skeleton = $target.find('.oe_structure.oe_empty, [data-oe-type="html"]');
         this.$editorMessageElements = $skeleton.not('[data-editor-message]').attr('data-editor-message', _t('DRAG BUILDING BLOCKS HERE'));
-        $skeleton.attr('contentEditable', !$skeleton.is(':empty'));
+        $skeleton.attr('contenteditable', function () { return !$(this).is(':empty'); });
     },
     /**
      * Returns the target for edition.
