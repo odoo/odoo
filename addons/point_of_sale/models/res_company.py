0336646faf7c224cdf81c62ba153f5871c39309e
Fabrice Henrion <fhe@odoo.com>
2021-06-16 13:04:52 +0000

    [IMP] point_of_sale: rephrase
    closes odoo/odoo#72232
    
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- addons/point_of_sale/models/res_company.py
+++ addons/point_of_sale/models/res_company.py
@@ -7,7 +7,7 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     point_of_sale_update_stock_quantities = fields.Selection([
-            ('closing', 'At the session closing (advised)'),
+            ('closing', 'At the session closing (recommended)'),
             ('real', 'In real time'),
             ], default='closing', string="Update quantities in stock",
             help="At the session closing: A picking is created for the entire session when it's closed\n In real time: Each order sent to the server create its own picking")
