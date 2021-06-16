2f7584e520aaba9da63a50b9bed4e100bb51871d
Thibault Delavallée <tde@odoo.com>
2021-06-16 09:54:52 +0000

    [IMP] test_mail: add tests for ``_find_partner_from_emails``
    See odoo/odoo#71047
    
    closes odoo/odoo#73357
    
    X-original-commit: b774cb09f8a44ce23213e80acc7138f96034c15e
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/test_mail/tests/__init__.py
+++ addons/test_mail/tests/__init__.py
@@ -9,8 +9,9 @@ from . import test_mail_followers
 from . import test_mail_message
 from . import test_mail_mail
 from . import test_mail_gateway
-from . import test_mail_template
 from . import test_mail_thread_internals
+from . import test_mail_template
+from . import test_mail_tools
 from . import test_message_management
 from . import test_message_post
 from . import test_message_track
