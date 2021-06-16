9868ad8765ea39ff10ced84ef7c655153f6be60f
Esam Hussein <esam.n.hussein@gmail.com>
2021-06-16 12:22:31 +0300

    [FIX] point_of_sale: receipt printing available for offline mode

--- addons/point_of_sale/static/src/js/Screens/PaymentScreen/PaymentScreen.js
+++ addons/point_of_sale/static/src/js/Screens/PaymentScreen/PaymentScreen.js
@@ -178,8 +178,7 @@ odoo.define('point_of_sale.PaymentScreen', function (require) {
                     syncedOrderBackendIds = await this.env.pos.push_single_order(this.currentOrder);
                 }
             } catch (error) {
-                //  exception for "XmlHttpRequestError"
-                if (error.code != -32098)
+                if (error.code == 700)
                     this.error = true;
                 if (error instanceof Error) {
                     throw error;
