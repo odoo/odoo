aad3e012488eb58a99764fafe081829cfc86e1b2
Younn Olivier <yol@odoo.com>
2021-06-17 15:45:40 +0200

    [FIX] web_editor: fix snippets custom colors display on color palette
    After the new editor was merged, custom event 'request_editable'
    triggered by the ColorPaletteWidget to retrieve custom colors from the
    editable was not processed.
    
    Now, we pass the editable in the options when we init the
    ColorPaletteWidget.
    
    task-2476601

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -1418,6 +1418,7 @@ const ColorpickerUserValueWidget = SelectUserValueWidget.extend({
     _renderColorPalette: function () {
         const options = {
             selectedColor: this._value,
+            $editable: this.$target.closest('.o_editable'),
         };
         if (this.options.dataAttributes.excluded) {
             options.excluded = this.options.dataAttributes.excluded.replace(/ /g, '').split(',');
