37f808b89fad2248d18d212c5bb6540469f17f20
AdrienHorgnies <aho@odoo.com>
2021-06-18 11:36:12 +0200

    [FIX] payment_stripe_checkout_webhook: use jsonrequest over kwargs
    Stripe is not sending JSONRPC but plain JSON;
     kwargs is empty, we need to use request.jsonrequest instead.
    
     Bug was introduced by a1ff4dc25534467e138cdb26f5b52df7dd33b79c

--- addons/payment_stripe_checkout_webhook/controllers/main.py
+++ addons/payment_stripe_checkout_webhook/controllers/main.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import json
 import logging
 
 from odoo.http import request
@@ -13,5 +12,5 @@ class StripeController(http.Controller):
 
     @http.route('/payment/stripe/webhook', type='json', auth='public', csrf=False)
     def stripe_webhook(self, **kwargs):
-        request.env['payment.acquirer'].sudo()._handle_stripe_webhook(kwargs)
+        request.env['payment.acquirer'].sudo()._handle_stripe_webhook(request.jsonrequest)
         return 'OK'
