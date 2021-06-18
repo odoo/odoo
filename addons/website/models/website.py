f4e5ee462e25cdd0051e7bb5675de71304029502
Romain Derie <rde@odoo.com>
2021-06-18 14:34:38 +0000

    [FIX] website: safely access menu recordset
    Introduced with 7fb016f9966b
    [0] access could crash, recordset could be empty after `filtered`.
    
    Step to reproduce:
      - Add admin group to website top level menu (Website > Debug > Menu)
      - Try to access frontend as non admin
    
    opw-2573763
    task-2574346
    
    X-original-commit: 9aaa792f48fc74fe759e74c8ceef48496e766309

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -151,9 +151,12 @@ class Website(models.Model):
                 # don't add child menu if parent is forbidden
                 if menu.parent_id and menu.parent_id in menus:
                     menu.parent_id._cache['child_id'] += (menu.id,)
+
             # prefetch every website.page and ir.ui.view at once
             menus.mapped('is_visible')
-            website.menu_id = menus and menus.filtered(lambda m: not m.parent_id)[0].id or False
+
+            top_menus = menus.filtered(lambda m: not m.parent_id)
+            website.menu_id = top_menus and top_menus[0].id or False
 
     # self.env.uid for ir.rule groups on menu
     @tools.ormcache('self.env.uid', 'self.id')
