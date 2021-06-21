bd79c42ffa916e6711a3a02b092e136151ca0fa6
Mahmoud Habboush <62886470+mhabboush@users.noreply.github.com>
2021-06-21 12:47:18 +0300

    [FIX] revert previous attempt to fix qty
    The whole division is inside the NULLIF, so dividing by uom_template.factor will become multiplication

--- addons/account/report/account_invoice_report.py
+++ addons/account/report/account_invoice_report.py
@@ -95,7 +95,7 @@ class AccountInvoiceReport(models.Model):
                 move.invoice_date_due,
                 uom_template.id                                             AS product_uom_id,
                 template.categ_id                                           AS product_categ_id,
-                line.quantity / NULLIF(COALESCE(uom_line.factor, 1) * COALESCE(uom_template.factor, 1), 0.0) * (CASE WHEN move.move_type IN ('in_invoice','out_refund','in_receipt') THEN -1 ELSE 1 END)
+                line.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) * (CASE WHEN move.move_type IN ('in_invoice','out_refund','in_receipt') THEN -1 ELSE 1 END)
                                                                             AS quantity,
                 -line.balance * currency_table.rate                         AS price_subtotal,
                 -COALESCE(line.balance
