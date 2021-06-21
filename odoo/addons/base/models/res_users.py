odoo/odoo

commit d8c5cc1335806d379134866262840fcafb05b643

[IMP] account: add a readonly group
Task 2092079
Accounting firms that want to give access to their customers avoiding
mistakes and risks will love this profile that can't do anything
wrong... Maybe as well as companies auditors..?

closes odoo/odoo#39860

Related: odoo/enterprise#6576
Signed-off-by: Quentin De Paoli (qdp) <qdp@openerp.com>

odoo/addons/base/models/res_users.py
@@ -1190,6 +1190,11 @@ class GroupsView(models.Model):
                 return (app, 'selection', gs.sorted('id'), category_name)
             # determine sequence order: a group appears after its implied groups
             order = {g: len(g.trans_implied_ids & gs) for g in gs}
+            # We want a selection for Accounting too. Auditor and Invoice are both
+            # children of Accountant, but the two of them make a full accountant
+            # so it makes no sense to have checkboxes.
+            if app.xml_id == 'base.module_category_accounting_accounting':
+                return (app, 'selection', gs.sorted(key=order.get), category_name)
             # check whether order is total, i.e., sequence orders are distinct
             if len(set(order.values())) == len(gs):
                 return (app, 'selection', gs.sorted(key=order.get), category_name)

commit 4849d1f09e4d2dd5c4fcf628a6d26fdbc11aefea

[FIX] core: accounting groups field in user form view
This is a followup of https://github.com/odoo/odoo/pull/63423.

The source of the bug is the special case introduced for accounting
groups in d8c5cc1335806d379134866262840fcafb05b643.  The groups in that
selection field are not totally ordered, and the non-ordered elements
are de facto ordered by name, which is a translated field!  In the
example above, the English version uses the field name
`sel_groups_22_23_24_25` while the Dutch version uses the field name
`sel_groups_23_22_24_25`.  Because the first one is used in the form
view, and it is not found in the model's documented fields (which uses
the second one), the field is discarded from the view.

The first fix was modifying method fields_get() to return all possible
field names for that selection field.  But this triggers memory errors
when you duplicate the groups that are partially ordered: having N such
groups generates N! permutations, which does not scale with N.

So, instead of generating all possible field names, we generate the
exact field name used in the user form view, i.e., the one using no
translations for the group names.

closes odoo/odoo#72463

Signed-off-by: Raphael Collet (rco) <rco@openerp.com>

odoo/addons/base/models/res_users.py
@@ -1450,36 +1450,28 @@ class UsersView(models.Model):
 
                 # FIXME: in Accounting, the groups in the selection are not
                 # totally ordered, and their order therefore partially depends
-                # on their name, which is translated!  So we generate all the
-                # possible field names according to the partial order.
-                gs_list = [gs]
+                # on their name, which is translated!  However, the group name
+                # used in the "user groups view" corresponds to the group order
+                # without translations.
+                field_name_gs = gs
                 if app.xml_id == 'base.module_category_accounting_accounting':
-                    # ranks = {0: [A, B], 2: [C], 3: [D]}
-                    ranks = defaultdict(list)
-                    for g in gs:
-                        ranks[len(g.trans_implied_ids & gs)].append(g)
-                    # perms = [[AB, BA], [C], [D]]
-                    perms = [
-                        [Group.concat(*perm) for perm in itertools.permutations(rank)]
-                        for k, rank in sorted(ranks.items())
-                    ]
-                    # gs_list = [ABCD, BACD]
-                    gs_list = [Group.concat(*perm) for perm in itertools.product(*perms)]
+                    # put field_name_gs in the same order as in the user form view
+                    order = {g: len(g.trans_implied_ids & gs) for g in gs}
+                    field_name_gs = gs.with_context(lang=None).sorted('name').sorted(order.get)
 
-                for gs in gs_list:
-                    field_name = name_selection_groups(gs.ids)
-                    if allfields and field_name not in allfields:
-                        continue
-                    # selection group field
-                    tips = ['%s: %s' % (g.name, g.comment) for g in gs if g.comment]
-                    res[field_name] = {
-                        'type': 'selection',
-                        'string': app.name or _('Other'),
-                        'selection': selection_vals + [(g.id, g.name) for g in gs],
-                        'help': '\n'.join(tips),
-                        'exportable': False,
-                        'selectable': False,
-                    }
+                field_name = name_selection_groups(field_name_gs.ids)
+                if allfields and field_name not in allfields:
+                    continue
+                # selection group field
+                tips = ['%s: %s' % (g.name, g.comment) for g in gs if g.comment]
+                res[field_name] = {
+                    'type': 'selection',
+                    'string': app.name or _('Other'),
+                    'selection': selection_vals + [(g.id, g.name) for g in gs],
+                    'help': '\n'.join(tips),
+                    'exportable': False,
+                    'selectable': False,
+                }
             else:
                 # boolean group fields
                 for g in gs: