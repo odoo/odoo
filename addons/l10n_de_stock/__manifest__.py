9f7b55210ab15f49df3be9eeaf7217afd89ff495
wan <wan@odoo.com>
2021-06-22 14:08:58 +0200

    [FIX] l10n_de: review DIN5008 format
    X-original-commit: bfa7c9e78037cd0abda3e8f0a9a519a600988e9c

---
+++ addons/l10n_de_stock/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Germany - Stock',
+    'category': 'Accounting/Localizations',
+    'depends': [
+        'l10n_de',
+        'stock',
+    ],
+    'auto_install': True,
+}
