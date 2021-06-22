b6cc62dbd5654ad82deea9deae6c252fcd4e5142
wan <wan@odoo.com>
2021-06-22 12:08:58 +0000

    [FIX] l10n_de: review DIN5008 format
    closes odoo/odoo#72548
    
    X-original-commit: bfa7c9e78037cd0abda3e8f0a9a519a600988e9c
    Signed-off-by: Quentin De Paoli (qdp) <qdp@openerp.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

---
+++ addons/l10n_de_repair/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Germany - Repair',
+    'category': 'Accounting/Localizations',
+    'depends': [
+        'l10n_de',
+        'repair',
+    ],
+    'auto_install': True,
+}
