a130e685ab048602e003da4be10601ea916eccaf
D3na <ahmadjd94@gmail.com>
2021-06-23 20:48:26 +0300

    :wrench: fix issue with default company for user

--- addons/aumet/models/product.py
+++ addons/aumet/models/product.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     scientific_name = fields.Many2one('aumet.scientific_name', string='Scientific Name', required=False)
+
+    @api.model
+    def create(self, vals_list):
+        vals_list["company_id"] = self.env.company.id
+        return super(ProductTemplate, self).create(vals_list)
