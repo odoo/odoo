63b040ed38db54f0477d650828873690b6c2f0ef
Thibault Delavallée <tde@odoo.com>
2021-06-23 11:21:53 +0200

    [IMP] website_event: create website menu for all events menus
    In website_event some event-specific frontend menus are not linked to
    ``website.event.menu`` like track or exhibitor sub-modules menus. This
    comes from initial implementation of ``website.event.menu`` that was
    available only for ``website_event_track``.
    
    Using ``website.event.menu`` eases menu management as it allows to have
    an object making a link between the event and the website menu. Notably
    when checking / unchecking in backend submenus it eases management.
    It also eases management when people manually edit menus from frontend
    as otherwise we have to manually manage ``ir.ui.view`` based on some
    naming manipulation.
    
    Task ID-2577079
    See odoo/odoo#72411

--- addons/website_event/tests/test_event_internals.py
+++ addons/website_event/tests/test_event_internals.py
@@ -11,7 +11,7 @@ from odoo.tests.common import users
 class TestEventWebsite(TestWebsiteEventCommon):
 
     @users('user_eventmanager')
-    def test_menu_create(self):
+    def test_menu_management(self):
         event = self.env['event.event'].create({
             'name': 'TestEvent',
             'date_begin': fields.Datetime.to_string(datetime.today() + timedelta(days=1)),
@@ -19,7 +19,33 @@ class TestEventWebsite(TestWebsiteEventCommon):
             'website_menu': True,
             'community_menu': False,
         })
-        self._assert_website_menus(event)
+        self.assertTrue(event.website_menu)
+        self.assertTrue(event.introduction_menu)
+        self.assertTrue(event.location_menu)
+        self.assertTrue(event.register_menu)
+        self.assertFalse(event.community_menu)
+        self._assert_website_menus(event, ['Introduction', 'Location', 'Register'], menus_out=['Community'])
+
+        event.community_menu = True
+        self._assert_website_menus(event, ['Introduction', 'Location', 'Register', 'Community'])
+
+        # test create without any requested menus
+        event = self.env['event.event'].create({
+            'name': 'TestEvent',
+            'date_begin': fields.Datetime.to_string(datetime.today() + timedelta(days=1)),
+            'date_end': fields.Datetime.to_string(datetime.today() + timedelta(days=15)),
+            'website_menu': False,
+        })
+        self.assertFalse(event.website_menu)
+        self.assertFalse(event.introduction_menu)
+        self.assertFalse(event.location_menu)
+        self.assertFalse(event.register_menu)
+        self.assertFalse(event.community_menu)
+        self.assertFalse(event.menu_id)
+
+        # test update of website_menu triggering 3 sub menus
+        event.write({'website_menu': True})
+        self._assert_website_menus(event, ['Introduction', 'Location', 'Register'], menus_out=['Community'])
 
     @users('user_event_web_manager')
     def test_menu_management_frontend(self):
@@ -30,18 +56,14 @@ class TestEventWebsite(TestWebsiteEventCommon):
             'website_menu': True,
             'community_menu': False,
         })
+        self._assert_website_menus(event, ['Introduction', 'Location', 'Register'], menus_out=['Community'])
+
+        # simulate menu removal from frontend: aka unlinking a menu
+        event.menu_id.child_id.filtered(lambda menu: menu.name == 'Introduction').unlink()
+
         self.assertTrue(event.website_menu)
-        self._assert_website_menus(event)
+        self._assert_website_menus(event, ['Location', 'Register'], menus_out=['Introduction', 'Community'])
 
-        introduction_menu = event.menu_id.child_id.filtered(lambda menu: menu.name == 'Introduction')
-        introduction_menu.unlink()
-
-        self.assertTrue(event.website_menu)
-        self._assert_website_menus(event, set(['Location', 'Register']))
-
-    @users('user_eventmanager')
-    def test_menu_update(self):
-        event = self.env['event.event'].browse(self.event_0.id)
-        self.assertFalse(event.menu_id)
-        event.website_menu = True
-        self._assert_website_menus(event)
+        # re-created from backend
+        event.introduction_menu = True
+        self._assert_website_menus(event, ['Introduction', 'Location', 'Register'], menus_out=['Community'])
