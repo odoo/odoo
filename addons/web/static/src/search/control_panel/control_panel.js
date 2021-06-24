44860aad3869f1bd4d97ce727dd8414b23f1e79b
Mathieu Duckerts-Antoine <dam@odoo.com>
2021-06-24 11:03:10 +0200

    [IMP] web: search infrastructure
    Co-authored-by: Aaron Bohy <aab@odoo.com>
    Co-authored-by: Bruno Boi <boi@odoo.com>
    Co-authored-by: Géry Debongnie <ged@odoo.com>
    Co-authored-by: Mathieu Duckerts-Antoine <dam@odoo.com>
    Co-authored-by: Jorge Pinna Puissant <jpp@odoo.com>
    Co-authored-by: Stefano Rigano <sri@odoo.com>

--- addons/web/static/src/search/control_panel/control_panel.js
+++ addons/web/static/src/search/control_panel/control_panel.js
@@ -1,8 +1,20 @@
 /** @odoo-module **/
 
+import { ComparisonMenu } from "../comparison_menu/comparison_menu";
+import { FavoriteMenu } from "../favorite_menu/favorite_menu";
+import { FilterMenu } from "../filter_menu/filter_menu";
+import { GroupByMenu } from "../group_by_menu/group_by_menu";
+import { SearchBar } from "../search_bar/search_bar";
 import { useService } from "@web/core/utils/hooks";
 
 const { Component } = owl;
+
+const MAPPING = {
+    filter: FilterMenu,
+    groupBy: GroupByMenu,
+    comparison: ComparisonMenu,
+    favorite: FavoriteMenu,
+};
 
 export class ControlPanel extends Component {
     setup() {
@@ -28,6 +40,24 @@ export class ControlPanel extends Component {
     }
 
     /**
+     * @returns {Component[]}
+     */
+    get searchMenus() {
+        const searchMenus = [];
+        for (const key of this.env.searchModel.searchMenuTypes) {
+            // look in display instead?
+            if (
+                key === "comparison" &&
+                this.env.searchModel.getSearchItems((i) => i.type === "comparison").length === 0
+            ) {
+                continue;
+            }
+            searchMenus.push({ Component: MAPPING[key], key });
+        }
+        return searchMenus;
+    }
+
+    /**
      * Called when an element of the breadcrumbs is clicked.
      *
      * @param {string} jsId
@@ -46,7 +76,27 @@ export class ControlPanel extends Component {
     }
 }
 
+ControlPanel.components = { ComparisonMenu, FavoriteMenu, FilterMenu, GroupByMenu, SearchBar };
 ControlPanel.template = "web.ControlPanel";
+// ControlPanel.props = {
+//     breadcrumbs: { type: Array, element: { jsId: String, name: String }, optional: true },
+//     display: { type: Object, optional: true },
+//     displayName: { type: String, optional: true },
+//     viewSwitcherEntries: {
+//         type: Array,
+//         element: {
+//             type: Object,
+//             shape: {
+//                 active: { type: Boolean, optional: true },
+//                 icon: String,
+//                 multiRecord: { type: Boolean, optional: true },
+//                 name: [Object, String],
+//                 type: String,
+//             },
+//         },
+//         optional: true,
+//     },
+// };
 ControlPanel.defaultProps = {
     breadcrumbs: [],
     display: {},
--- addons/web/static/tests/search/control_panel.js
+++ addons/web/static/tests/search/control_panel.js
@@ -1,108 +1,117 @@
 /** @odoo-module **/
 
-import { actionService } from "@web/webclient/actions/action_service";
 import { click } from "@web/../tests/helpers/utils";
 import { ControlPanel } from "@web/search/control_panel/control_panel";
-import { getFixture } from "@web/../tests/helpers/utils";
-import { makeTestEnv } from "@web/../tests/helpers/mock_env";
-import { notificationService } from "@web/core/notifications/notification_service";
-import { patchWithCleanup } from "@web/../tests/helpers/utils";
-import { registerCleanup } from "@web/../tests/helpers/cleanup";
-import { registry } from "@web/core/registry";
+import { makeWithSearch, setupControlPanelServiceRegistry } from "./helpers";
 
-const serviceRegistry = registry.category("services");
-
-const { mount } = owl;
-
-/**
- * @param {MakeViewParams} params
- * @returns {owl.Component}
- */
-export async function makeControlPanel(params = {}) {
-    const serverData = params.serverData;
-    const mockRPC = params.mockRPC;
-    const props = Object.assign({}, params);
-    delete props.serverData;
-    delete props.mockRPC;
-
-    const env = await makeTestEnv({ serverData, mockRPC });
-    const target = getFixture();
-
-    const controlPanel = await mount(ControlPanel, { env, props, target });
-
-    registerCleanup(() => controlPanel.destroy());
-
-    return controlPanel;
-}
-
-QUnit.module("ControlPanel", {
-    async beforeEach() {
-        serviceRegistry.add("action", actionService);
-        serviceRegistry.add("notification", notificationService);
-    },
-});
-
-QUnit.test("simple rendering", async (assert) => {
-    const controlPanel = await makeControlPanel({
-        display: {
-            "top-right": false,
-        },
-    });
-    assert.containsOnce(controlPanel, ".o_cp_top");
-    assert.containsOnce(controlPanel, ".o_cp_top_left");
-    assert.containsNone(controlPanel, ".o_cp_top_right");
-    assert.containsOnce(controlPanel, ".o_cp_bottom");
-    assert.containsOnce(controlPanel, ".o_cp_bottom_left");
-    assert.containsOnce(controlPanel, ".o_cp_bottom_right");
-
-    assert.containsNone(controlPanel, ".o_cp_switch_buttons");
-
-    assert.containsOnce(controlPanel, ".breadcrumb");
-    assert.containsOnce(controlPanel, ".breadcrumb li.breadcrumb-item");
-    assert.strictEqual(controlPanel.el.querySelector("li.breadcrumb-item").innerText, "Unnamed");
-});
-
-QUnit.test("breadcrumbs prop", async (assert) => {
-    const controlPanel = await makeControlPanel({
-        breadcrumbs: [{ jsId: "controller_7", name: "Previous" }],
-        displayName: "Current",
+let serverData;
+QUnit.module("Search", (hooks) => {
+    hooks.beforeEach(async () => {
+        serverData = {
+            models: {
+                foo: {
+                    fields: {},
+                },
+            },
+            views: {
+                "foo,false,search": `<search/>`,
+            },
+        };
+        setupControlPanelServiceRegistry();
     });
 
-    assert.containsN(controlPanel, ".breadcrumb li.breadcrumb-item", 2);
-    const breadcrumbItems = controlPanel.el.querySelectorAll("li.breadcrumb-item");
-    assert.strictEqual(breadcrumbItems[0].innerText, "Previous");
-    assert.hasClass(breadcrumbItems[1], "active");
-    assert.strictEqual(breadcrumbItems[1].innerText, "Current");
+    QUnit.module("ControlPanel");
 
-    controlPanel.env.services.action.restore = (jsId) => {
-        assert.step(jsId);
-    };
+    QUnit.test("simple rendering", async (assert) => {
+        const controlPanel = await makeWithSearch(
+            { serverData },
+            {
+                resModel: "foo",
+                Component: ControlPanel,
+                componentProps: {
+                    display: {
+                        "top-right": false,
+                    },
+                },
+                searchMenuTypes: [],
+            }
+        );
 
-    await click(breadcrumbItems[0]);
-    assert.verifySteps(["controller_7"]);
-});
+        assert.containsOnce(controlPanel, ".o_cp_top");
+        assert.containsOnce(controlPanel, ".o_cp_top_left");
+        assert.containsNone(controlPanel, ".o_cp_top_right");
+        assert.containsOnce(controlPanel, ".o_cp_bottom");
+        assert.containsOnce(controlPanel, ".o_cp_bottom_left");
+        assert.containsOnce(controlPanel, ".o_cp_bottom_right");
 
-QUnit.test("viewSwitcherEntries prop", async (assert) => {
-    const controlPanel = await makeControlPanel({
-        viewSwitcherEntries: [
-            { type: "list", active: true, icon: "fa-list-ul", name: "List" },
-            { type: "kanban", icon: "fa-th-large", name: "Kanban" },
-        ],
+        assert.containsNone(controlPanel, ".o_cp_switch_buttons");
+
+        assert.containsOnce(controlPanel, ".breadcrumb");
+        assert.containsOnce(controlPanel, ".breadcrumb li.breadcrumb-item");
+        assert.strictEqual(
+            controlPanel.el.querySelector("li.breadcrumb-item").innerText,
+            "Unnamed"
+        );
     });
 
-    assert.containsOnce(controlPanel, ".o_cp_switch_buttons");
-    assert.containsN(controlPanel, ".o_switch_view", 2);
-    const views = controlPanel.el.querySelectorAll(".o_switch_view");
+    QUnit.test("breadcrumbs prop", async (assert) => {
+        const controlPanel = await makeWithSearch(
+            { serverData },
+            {
+                resModel: "foo",
+                Component: ControlPanel,
+                componentProps: {
+                    breadcrumbs: [{ jsId: "controller_7", name: "Previous" }],
+                    displayName: "Current",
+                },
+                searchMenuTypes: [],
+            }
+        );
 
-    assert.strictEqual(views[0].getAttribute("data-tooltip"), "List");
-    assert.hasClass(views[0], "active");
-    assert.strictEqual(views[1].getAttribute("data-tooltip"), "Kanban");
-    assert.hasClass(views[1], "fa-th-large");
+        assert.containsN(controlPanel, ".breadcrumb li.breadcrumb-item", 2);
+        const breadcrumbItems = controlPanel.el.querySelectorAll("li.breadcrumb-item");
+        assert.strictEqual(breadcrumbItems[0].innerText, "Previous");
+        assert.hasClass(breadcrumbItems[1], "active");
+        assert.strictEqual(breadcrumbItems[1].innerText, "Current");
 
-    controlPanel.env.services.action.switchView = (viewType) => {
-        assert.step(viewType);
-    };
+        controlPanel.env.services.action.restore = (jsId) => {
+            assert.step(jsId);
+        };
 
-    await click(views[1]);
-    assert.verifySteps(["kanban"]);
+        await click(breadcrumbItems[0]);
+        assert.verifySteps(["controller_7"]);
+    });
+
+    QUnit.test("viewSwitcherEntries prop", async (assert) => {
+        const controlPanel = await makeWithSearch(
+            { serverData },
+            {
+                resModel: "foo",
+                Component: ControlPanel,
+                componentProps: {
+                    viewSwitcherEntries: [
+                        { type: "list", active: true, icon: "fa-list-ul", name: "List" },
+                        { type: "kanban", icon: "fa-th-large", name: "Kanban" },
+                    ],
+                },
+                searchMenuTypes: [],
+            }
+        );
+
+        assert.containsOnce(controlPanel, ".o_cp_switch_buttons");
+        assert.containsN(controlPanel, ".o_switch_view", 2);
+        const views = controlPanel.el.querySelectorAll(".o_switch_view");
+
+        assert.strictEqual(views[0].getAttribute("data-tooltip"), "List");
+        assert.hasClass(views[0], "active");
+        assert.strictEqual(views[1].getAttribute("data-tooltip"), "Kanban");
+        assert.hasClass(views[1], "fa-th-large");
+
+        controlPanel.env.services.action.switchView = (viewType) => {
+            assert.step(viewType);
+        };
+
+        await click(views[1]);
+        assert.verifySteps(["kanban"]);
+    });
 });
