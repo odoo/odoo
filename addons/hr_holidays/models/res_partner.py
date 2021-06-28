a1fd738e58d60d1d0248afcc58eb3c563dbacafa
Thanh Dodeur <tso@odoo.com>
2021-06-28 14:11:53 +0200

    temp: move out of office date to mail_partner_format

--- addons/hr_holidays/models/res_partner.py
+++ addons/hr_holidays/models/res_partner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
+from odoo import api, models, fields
 
 
 class ResPartner(models.Model):
@@ -18,6 +18,22 @@ class ResPartner(models.Model):
                     partner.im_status = 'leave_away'
                 else:
                     partner.im_status = 'leave_offline'
+
+    def mail_partner_format(self):
+        # TODO temporary, shouldn't do the query for each partner
+        res = super(ResPartner, self).mail_partner_format()
+        res.setdefault('out_of_office_date_end', False)
+        if 'leave' in self.im_status:
+            now = fields.Datetime.now()
+            leave = self.env['hr.leave'].sudo().search([
+                ('user_id.partner_id', '=', self.id),
+                ('state', 'not in', ['cancel', 'refuse']),
+                ('date_from', '<=', now),
+                ('date_to', '>=', now),
+            ], limit=1)
+            if leave:
+                res['out_of_office_date_end'] = leave[0].date_to
+        return res
 
     @api.model
     def _get_on_leave_ids(self):
