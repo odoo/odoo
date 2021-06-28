350ac7abc114885ab1a8e884f643228b59ae2280
Pierre Masereel <pim@odoo.com>
2021-06-28 11:41:41 +0200

    [IMP] pos_sale: when a downpayment is made show a popup to choose the percentage

--- addons/pos_sale/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
+++ addons/pos_sale/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
@@ -1,6 +1,8 @@
 odoo.define('pos_sale.SaleOrderManagementScreen', function (require) {
     'use strict';
 
+    const { sprintf } = require('web.utils');
+    const { parse } = require('web.field_utils');
     const { useContext } = owl.hooks;
     const { useListener } = require('web.custom_hooks');
     const ControlButtonsMixin = require('point_of_sale.ControlButtonsMixin');
@@ -193,19 +195,29 @@ odoo.define('pos_sale.SaleOrderManagementScreen', function (require) {
                         'product_uom_qty': lines[i].product_uom_qty,
                         'price_unit': lines[i].price_unit,
                         'total': lines[i].price_total,
-                    }
+                    };
                 }
+
+                let down_payment = sale_order.amount_total;
+                const { confirmed, payload } = await this.showPopup('NumberPopup', {
+                    title: sprintf(this.env._t("Percentage of %s"), this.env.pos.format_currency(sale_order.amount_total)),
+                    startingValue: 0,
+                });
+                if (confirmed){
+                    down_payment = sale_order.amount_total * parse.float(payload) / 100;
+                }
+
 
                 let new_line = new models.Orderline({}, {
                     pos: this.env.pos,
                     order: this.env.pos.get_order(),
                     product: this.env.pos.db.get_product_by_id(this.env.pos.config.down_payment_product_id[0]),
-                    price: sale_order.amount_total,
+                    price: down_payment,
                     price_manually_set: true,
                     sale_order_origin_id: clickedOrder,
                     down_payment_details: tab,
                 });
-                new_line.set_unit_price(sale_order.amount_total);
+                new_line.set_unit_price(down_payment);
                 this.env.pos.get_order().add_orderline(new_line);
               }
 
