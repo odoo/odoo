439ea50f8a96d5d061a83d0ed0463a7deb0292f8
william-andre <wan@odoo.com>
2021-06-29 13:20:06 +0000

    [FIX] account: get accounting date from invoicing date for vendor bills
    In the first days of the month, many of the previous month's invoices
    could still to be posted since the lock date is not yet set to the last
    day of the previous month. To avoid the user having to modify the
    default accounting date on each such bill, detect whether the conditions
    allow the invoice to be posted on the very last day of the oldest
    possible month, and then adapt the accounting date to the last day of
    that month.
    
    task-2575560
    
    closes odoo/odoo#73701
    
    X-original-commit: 2c5c1a4478505688f35e47ca20e7977b134d0fdb
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/tests/test_account_move_out_invoice.py
+++ addons/account/tests/test_account_move_out_invoice.py
@@ -142,6 +142,18 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
             self.term_line_vals_1,
         ], self.move_vals)
 
+    @freeze_time('2020-01-15')
+    def test_out_invoice_onchange_invoice_date(self):
+        for tax_date, invoice_date, accounting_date in [
+            ('2019-03-31', '2019-05-12', '2020-01-15'),
+            ('2019-03-31', '2019-02-10', '2020-01-15'),
+            ('2019-05-31', '2019-06-15', '2020-01-15'),
+        ]:
+            self.invoice.company_id.tax_lock_date = tax_date
+            with Form(self.invoice) as move_form:
+                move_form.invoice_date = invoice_date
+            self.assertEqual(self.invoice.date, fields.Date.to_date(accounting_date))
+
     def test_out_invoice_line_onchange_product_1(self):
         move_form = Form(self.invoice)
         with move_form.invoice_line_ids.edit(0) as line_form:
