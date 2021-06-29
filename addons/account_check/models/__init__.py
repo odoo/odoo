5b8e996b7484ba6639914204ffadfaad75fb45e6
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-06-29 17:51:39 -0300

    [DEL] account_check: unused files

--- addons/account_check/models/__init__.py
+++ addons/account_check/models/__init__.py
@@ -1,7 +1,3 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
 from . import account_move
 from . import account_payment
 from . import account_chart_template
