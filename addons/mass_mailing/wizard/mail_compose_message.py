08ceddfea2a0462245cd8721aae0e87d3f25fb8f
Thibault Delavallée <tde@odoo.com>
2021-07-02 13:11:10 +0200

    [MOV][REF] mail: add failure type on mail.mail model and move computation in mail
    RATIONALE
    
    Currently most of mail related failure types are computed in mass mailing.
    This is mainly due to historical reasons. We could move some of this
    computation directly at mail level and store failure information directly
    in mail.mail records.
    
    SPECIFICATIONS
    
    Overall purpose is to get near SMS implementation where base composer prepare
    more advanced pre computation: blacklist, optout, seen list.
    
    Detect and store failure types directly on mail.mail: blacklist, optout,
    duplicates, missing email, wrong email, ...
    
    State computation is moved from mass mailing to mail. It is also improved as
    currently everything is based on "first recipient found". This is globally
    valid for mass mailing who uses default recipients (customer). However when
    dealing with generic mailing this is not true anymore.
    
    We choose to store and update status on mail.mail records only when having
    a single recipient. When several recipients are defined on a given mail.mail
    we cannot really decide a status prior to sending it and skip this computation.
    
    Mass mailing override now uses information from mail.mail to update its linked
    traces as computation is now delegated to base composer model.
    
    LINKS
    
    Task ID-2377974
    Community PR odoo#61467
    Enterprise PR odoo/enterprise#14633
    Upgrade PR odoo/upgrade#1907

--- addons/mass_mailing/wizard/mail_compose_message.py
+++ addons/mass_mailing/wizard/mail_compose_message.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, tools
-from odoo.tools import email_re
+from odoo import fields, models
 
 
 class MailComposeMessage(models.TransientModel):
@@ -40,70 +39,27 @@ class MailComposeMessage(models.TransientModel):
                     'attachment_ids': [(6, 0, self.attachment_ids.ids)],
                 })
 
-            # Preprocess res.partners to batch-fetch from db
-            # if recipient_ids is present, it means they are partners
-            # (the only object to fill get_default_recipient this way)
-            recipient_partners_ids = []
-            read_partners = {}
-            for res_id in res_ids:
-                mail_values = res[res_id]
-                if mail_values.get('recipient_ids'):
-                    # recipient_ids is a list of x2m command tuples at this point
-                    recipient_partners_ids.append(mail_values.get('recipient_ids')[0][1])
-            read_partners = self.env['res.partner'].browse(recipient_partners_ids)
-
-            partners_email = {p.id: p.email for p in read_partners}
-
-            opt_out_list = self._context.get('mass_mailing_opt_out_list')
-            seen_list = self._context.get('mass_mailing_seen_list')
+            recipients_info = self._process_recipient_values(res)
             mass_mail_layout = self.env.ref('mass_mailing.mass_mailing_mail_layout', raise_if_not_found=False)
             for res_id in res_ids:
+                mail_values = res[res_id]
                 if mail_values.get('body_html') and mass_mail_layout:
                     mail_values['body_html'] = mass_mail_layout._render({'body': mail_values['body_html']}, engine='ir.qweb', minimal_qcontext=True)
-
-                mail_values = res[res_id]
-                if mail_values.get('email_to'):
-                    mail_to = mail_values['email_to']
-                else:
-                    partner_id = (mail_values.get('recipient_ids') or [(False, '')])[0][1]
-                    mail_to = partners_email.get(partner_id)
-                mail_to_normalized = tools.email_normalize(mail_to)
-
-                # prevent sending to blocked addresses that were included by mistake
-                # blacklisted or optout or duplicate -> cancel
-                failure_type = False
-                if mail_values.get('state') == 'cancel':
-                    failure_type = 'mail_bl'
-                elif opt_out_list and mail_to in opt_out_list:
-                    mail_values['state'] = 'cancel'
-                    failure_type = 'mail_optout'
-                elif seen_list and mail_to in seen_list:
-                    mail_values['state'] = 'cancel'
-                    failure_type = 'mail_dup'
-                # void of falsey values -> error
-                elif not mail_to:
-                    mail_values['state'] = 'cancel'
-                    failure_type = 'mail_email_missing'
-                elif not mail_to_normalized or not email_re.findall(mail_to):
-                    mail_values['state'] = 'cancel'
-                    failure_type = "mail_email_invalid"
-                elif seen_list is not None:
-                    seen_list.add(mail_to)
 
                 trace_vals = {
                     'model': self.model,
                     'res_id': res_id,
                     'mass_mailing_id': mass_mailing.id,
                     # if mail_to is void, keep falsy values to allow searching / debugging traces
-                    'email': mail_to or mail_values.get('email_to'),
+                    'email': recipients_info[res_id]['mail_to'][0] if recipients_info[res_id]['mail_to'] else '',
                 }
                 # propagate failed states to trace when still-born
                 if mail_values.get('state') == 'cancel':
                     trace_vals['trace_status'] = 'cancel'
                 elif mail_values.get('state') == 'exception':
                     trace_vals['trace_status'] = 'error'
-                if failure_type:
-                    trace_vals['failure_type'] = failure_type
+                if mail_values.get('failure_type'):
+                    trace_vals['failure_type'] = mail_values['failure_type']
 
                 mail_values.update({
                     'mailing_id': mass_mailing.id,
