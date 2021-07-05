b0c5664e1221c50891f7b2c8d380a7306cdebcb3
Ivan Yelizariev <iel@odoo.com>
2021-07-05 14:49:19 +0200

    [FIX] mail: speed up read_progress_bar
    This commit fixes the performance issue in getting statistics for
    ``activity_state`` (colored clock icon for overdue/today/planned) in
    CRM.  The query has been tested for several years on a large database
    (Odoo's own production database).
    
    Performance test on 29 K crm.lead records (activity_state):
    
    With a filter for 10 records:
    
    ```
    | measurement        | before | after |
    |--------------------+--------+-------|
    | number of queries  |     25 |     5 |
    | query time, ms     |     12 |    95 | (*)
    | remaining time, ms |     32 |     7 |
    ```
    
    All records:
    
    ```
    | measurement        | before | after |
    |--------------------+--------+-------|
    | number of queries  |   1326 |     5 |
    | query time, ms     |   1739 |   129 |
    | remaining time, ms |  47934 |    17 |
    ```
    
    As we can see in the last results, the time went from almost 50 seconds
    (not responsive at all) to 150 milliseconds (responsive).  The time
    increase in (*) may be caused by imperfect measurements, which are raw
    and not averaged measures.
    
    Co-authored-by: Nicolas Seinlet <nse@odoo.com>
    
    ---
    
    opw-2346901
    task-1915411

--- addons/mail/models/mail_activity.py
+++ addons/mail/models/mail_activity.py
@@ -696,6 +696,73 @@ class MailActivityMixin(models.AbstractModel):
         ).unlink()
         return result
 
+    def _read_progress_bar(self, domain, group_by, progress_bar):
+        group_by_fname = group_by.partition(':')[0]
+        if not (progress_bar['field'] == 'activity_state' and self._fields[group_by_fname].store):
+            return super()._read_progress_bar(domain, group_by, progress_bar)
+
+        # optimization for 'activity_state'
+
+        # explicitly check access rights, since we bypass the ORM
+        self.check_access_rights('read')
+        query = self._where_calc(domain)
+        self._apply_ir_rules(query, 'read')
+        gb = group_by.partition(':')[0]
+        annotated_groupbys = [
+            self._read_group_process_groupby(gb, query)
+            for gb in [group_by, 'activity_state']
+        ]
+        groupby_dict = {gb['groupby']: gb for gb in annotated_groupbys}
+        for gb in annotated_groupbys:
+            if gb['field'] == 'activity_state':
+                gb['qualified_field'] = '"_last_activity_state"."activity_state"'
+        groupby_terms, orderby_terms = self._read_group_prepare('activity_state', [], annotated_groupbys, query)
+        select_terms = [
+            '%s as "%s"' % (gb['qualified_field'], gb['groupby'])
+            for gb in annotated_groupbys
+        ]
+        from_clause, where_clause, where_params = query.get_sql()
+        tz = self._context.get('tz') or self.env.user.tz or 'UTC'
+        select_query = """
+            SELECT 1 AS id, count(*) AS "__count", {fields}
+            FROM {from_clause}
+            JOIN (
+                SELECT res_id,
+                CASE
+                    WHEN min(date_deadline - (now() AT TIME ZONE COALESCE(res_partner.tz, %s))::date) > 0 THEN 'planned'
+                    WHEN min(date_deadline - (now() AT TIME ZONE COALESCE(res_partner.tz, %s))::date) < 0 THEN 'overdue'
+                    WHEN min(date_deadline - (now() AT TIME ZONE COALESCE(res_partner.tz, %s))::date) = 0 THEN 'today'
+                    ELSE null
+                END AS activity_state
+                FROM mail_activity
+                JOIN res_users ON (res_users.id = mail_activity.user_id)
+                JOIN res_partner ON (res_partner.id = res_users.partner_id)
+                WHERE res_model = '{model}'
+                GROUP BY res_id
+            ) AS "_last_activity_state" ON ("{table}".id = "_last_activity_state".res_id)
+            WHERE {where_clause}
+            GROUP BY {group_by}
+        """.format(
+            fields=', '.join(select_terms),
+            from_clause=from_clause,
+            model=self._name,
+            table=self._table,
+            where_clause=where_clause or '1=1',
+            group_by=', '.join(groupby_terms),
+        )
+        self.env.cr.execute(select_query, [tz] * 3 + where_params)
+        fetched_data = self.env.cr.dictfetchall()
+        self._read_group_resolve_many2one_fields(fetched_data, annotated_groupbys)
+        data = [
+            {key: self._read_group_prepare_data(key, val, groupby_dict)
+             for key, val in row.items()}
+            for row in fetched_data
+        ]
+        return [
+            self._read_group_format_result(vals, annotated_groupbys, [group_by], domain)
+            for vals in data
+        ]
+
     @api.multi
     def toggle_active(self):
         """ Before archiving the record we should also remove its ongoing
