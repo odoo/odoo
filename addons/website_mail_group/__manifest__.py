8b814947d30e9aba25cc3200be369b4fa2e15d71
std-odoo <std@odoo.com>
2021-07-05 08:56:12 +0000

    [ADD] website_mail_group: re-add the <mail.group> website snippets
    Purpose
    =======
    
    This snippets was originally in the "Website Mail Channel" module,
    but it has been removed.
    
    The snippet allow the user to subscribe to a specific mailing list.
    
    If the current user has no access to the mailing list (because of ACL
    rule) the widget will be invisible for him.
    
    Links
    =====
    
    Task-2510267
    See odoo/pull/71599
    See odoo/enterprise/pull/19296
    See odoo/upgrade/pull/2600

---
+++ addons/website_mail_group/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Website Mail Group",
+    'summary': "Add a website snippet for the mail groups.",
+    'description': "",
+    'version': '1.0',
+    'depends': ['mail_group', 'website'],
+    'auto_install': True,
+    'data': [
+        'views/snippets/s_group.xml',
+        'views/snippets/snippets.xml',
+    ],
+    'assets': {
+        'website.assets_wysiwyg': [
+            'website_mail_group/static/src/snippets/s_group/options.js',
+        ],
+        'web.assets_frontend': [
+            'website_mail_group/static/src/snippets/s_group/000.js',
+        ],
+    },
+}
