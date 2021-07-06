e01060578d4e292c6424f3df8f125b01d943f55b
Adrien Widart <awt@odoo.com>
2021-07-06 12:32:18 +0000

    [FIX] sale_stock: set invoice_status value when delivery is canceled
    Suppose the option "Lock Confirmed Sales" enabled and a product with the
    invoicing policy set to "Delivered quantities". When cancelling the
    delivery of such a product, the invoice status of the associated SO
    should be 'Nothing to Invoice'
    
    To reproduce the error:
    1. In Settings, enable "Lock Confirmed Sales"
    2. Create a product P:
        - Type: Consumable
        - Invoicing policy: Delivered quantities
    3. Create a SO with one P
    4. Confirm the SO
    5. Cancel the SO's delivery
    6. Check the invoice status (`invoice_status`)
        - 12.0: Either by modifying the view, or by adding the field with
    studio or directly via PSQL
        - 13.0+: The field can be enabled thanks to the options of the tree
    view
    
    Error: The invoice status is "Fully Invoiced" but the product hasn't be
    delivered and no invoice has been created
    
    OPW-2464343
    
    closes odoo/odoo#73417
    
    X-original-commit: 9bedda1835830f829b565f35dcd67562ce4bbf54
    Signed-off-by: William Henrotin <Whenrow@users.noreply.github.com>
    Signed-off-by: Adrien Widart <adwid@users.noreply.github.com>

--- addons/sale_stock/tests/test_sale_stock.py
+++ addons/sale_stock/tests/test_sale_stock.py
@@ -737,3 +737,30 @@ class TestSaleStock(TestSale):
         return_picking.button_validate()
         # Checks the delivery amount (must still be 10).
         self.assertEqual(sale_order.order_line.qty_delivered, 10)
+
+    def test_13_cancel_delivery(self):
+        """
+        Suppose the option "Lock Confirmed Sales" enabled and a product with the invoicing policy set to "Delivered
+        quantities". When cancelling the delivery of such a product, the invoice status of the associated SO should be
+        'Nothing to Invoice'
+        """
+        self.env['ir.config_parameter'].set_param('sale.auto_done_setting', True)
+
+        product = self.products['prod_del']
+        so = self.env['sale.order'].create({
+            'partner_id': self.partner.id,
+            'partner_invoice_id': self.partner.id,
+            'partner_shipping_id': self.partner.id,
+            'order_line': [(0, 0, {
+                'name': product.name,
+                'product_id': product.id,
+                'product_uom_qty': 2,
+                'product_uom': product.uom_id.id,
+                'price_unit': product.list_price
+            })],
+            'pricelist_id': self.env.ref('product.list0').id,
+        })
+        so.action_confirm()
+        so.picking_ids.action_cancel()
+
+        self.assertEqual(so.invoice_status, 'no')
