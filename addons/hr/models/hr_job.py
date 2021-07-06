odoo/odoo

commit 446d906f508880eb787debbec3c23cfd081bb045

[IMP] hr: improves field type from text to html
Replace text fields to html fields as we have our own 'OdooEditor'.
Indeed, it gives more options to users in the way they format their
content without weighting too much on the UI
(tools appear on demand and not by default).

Models -> Fields

1) fleet.vehicle -> description
2) fleet.vehicle.log.contract -> notes
3) hr.job -> description
4) hr.contract -> note
5) hr.leave.allocation -> notes
6) hr.applicant -> description
7) lunch.product -> description
8) lunch.order -> product_description
9) lunch.product.report -> description

Task Id: 2499504

addons/hr/models/hr_job.py
@@ -20,7 +20,7 @@ class Job(models.Model):
     no_of_hired_employee = fields.Integer(string='Hired Employees', copy=False,
         help='Number of hired employees for this job position during recruitment phase.')
     employee_ids = fields.One2many('hr.employee', 'job_id', string='Employees', groups='base.group_user')
-    description = fields.Text(string='Job Description')
+    description = fields.Html(string='Job Description')
     requirements = fields.Text('Requirements')
     department_id = fields.Many2one('hr.department', string='Department', domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)

addons/website_hr_recruitment/views/website_hr_recruitment_templates.xml
@@ -52,10 +52,10 @@
                                     <h5 t-if="job.no_of_recruitment &gt; 1">
                                         <t t-esc="job.no_of_recruitment"/> open positions
                                     </h5>
-                                    <p t-if="editable"
+                                    <div t-if="editable"
                                        t-field="job.description"
                                        class="mt16 mb0 css_non_editable_mode_hidden"/>
-                                    <p t-esc="len((job.description or '').split(' ')) > 35 and '%s ...' % ' '.join(job.description.split(' ')[:35]) or job.description"
+                                    <div t-esc="len((job.description or '').split(' ')) > 35 and '%s ...' % ' '.join(job.description.split(' ')[:35]) or job.description"
                                         class="mt16 mb0 css_editable_mode_hidden"
                                     />
                                     <div class="o_job_infos mt16">

addons/hr_recruitment/models/hr_recruitment.py
@@ -99,7 +99,7 @@ class Applicant(models.Model):
 
     name = fields.Char("Subject / Application Name", required=True, help="Email subject for applications sent via email")
     active = fields.Boolean("Active", default=True, help="If the active field is set to false, it will allow you to hide the case without removing it.")
-    description = fields.Text("Description")
+    description = fields.Html("Description")
     email_from = fields.Char("Email", size=128, help="Applicant email", compute='_compute_partner_phone_email',
         inverse='_inverse_partner_email', store=True)
     probability = fields.Float("Probability")

addons/fleet/models/fleet_vehicle.py
@@ -18,7 +18,7 @@ class FleetVehicle(models.Model):
         return state if state and state.id else False
 
     name = fields.Char(compute="_compute_vehicle_name", store=True)
-    description = fields.Text("Vehicle Description")
+    description = fields.Html("Vehicle Description")
     active = fields.Boolean('Active', default=True, tracking=True)
     company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company)
     currency_id = fields.Many2one('res.currency', related='company_id.currency_id')

addons/fleet/models/fleet_vehicle_cost.py
@@ -44,7 +44,7 @@ class FleetVehicleLogContract(models.Model):
         help='Choose whether the contract is still valid or not',
         tracking=True,
         copy=False)
-    notes = fields.Text('Terms and Conditions', help='Write here all supplementary information relative to this contract', copy=False)
+    notes = fields.Html('Terms and Conditions', help='Write here all supplementary information relative to this contract', copy=False)
     cost_generated = fields.Monetary('Recurring Cost')
     cost_frequency = fields.Selection([
         ('no', 'No'),

addons/hr_contract/models/hr_contract.py
@@ -33,7 +33,7 @@ class Contract(models.Model):
         default=lambda self: self.env.company.resource_calendar_id.id, copy=False, index=True,
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
     wage = fields.Monetary('Wage', required=True, tracking=True, help="Employee's monthly gross wage.")
-    notes = fields.Text('Notes')
+    notes = fields.Html('Notes')
     state = fields.Selection([
         ('draft', 'New'),
         ('open', 'Running'),

addons/hr_holidays/models/hr_leave_allocation.py
@@ -66,7 +66,7 @@ class HolidaysAllocation(models.Model):
         'hr.employee', compute='_compute_from_holiday_type', store=True, string='Employee', index=True, readonly=False, ondelete="restrict", tracking=True,
         states={'cancel': [('readonly', True)], 'refuse': [('readonly', True)], 'validate1': [('readonly', True)], 'validate': [('readonly', True)]})
     manager_id = fields.Many2one('hr.employee', compute='_compute_from_employee_id', store=True, string='Manager')
-    notes = fields.Text('Reasons', readonly=True, states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]})
+    notes = fields.Html('Reasons', readonly=True, states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]})
     # duration
     number_of_days = fields.Float(
         'Number of Days', compute='_compute_from_holiday_status_id', store=True, readonly=False, tracking=True, default=1,

addons/lunch/models/lunch_order.py
@@ -41,7 +41,7 @@ class LunchOrder(models.Model):
 
     display_toppings = fields.Text('Extras', compute='_compute_display_toppings', store=True)
 
-    product_description = fields.Text('Description', related='product_id.description')
+    product_description = fields.Html('Description', related='product_id.description')
     topping_label_1 = fields.Char(related='product_id.category_id.topping_label_1')
     topping_label_2 = fields.Char(related='product_id.category_id.topping_label_2')
     topping_label_3 = fields.Char(related='product_id.category_id.topping_label_3')

commit 3b2755406505f80e7c3f8b118edcd0a65f2e89c7

[IMP] hr,website_hr_recruitment: improve job views
Multiple improvement with job views:
 - Add a sequence field for jobs
 - Display 'Open Positions' on the website event if there is only 1 open
   position
 - Display whether the website is published on both the list and the
   form views of hr.job
 - Fix an issue introduced with 446d906f508880eb787debbec3c23cfd081bb045
   where html field would be cut and html tags would be displayed
Minor change:
 - Add an sql constraint on the estimated number of recruitment to be
   positive

Task ID: 2581595

closes odoo/odoo#72769

Related: odoo/enterprise#19264
Related: odoo/upgrade#2585
Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

addons/hr/models/hr_job.py
@@ -9,8 +9,10 @@ class Job(models.Model):
     _name = "hr.job"
     _description = "Job Position"
     _inherit = ['mail.thread']
+    _order = 'sequence'
 
     name = fields.Char(string='Job Position', required=True, index=True, translate=True)
+    sequence = fields.Integer(default=10)
     expected_employees = fields.Integer(compute='_compute_employees', string='Total Forecasted Employees', store=True,
         help='Expected number of employees for this job position after new recruitment.')
     no_of_employee = fields.Integer(compute='_compute_employees', string="Current Number of Employees", store=True,
@@ -31,6 +33,7 @@ class Job(models.Model):
 
     _sql_constraints = [
         ('name_company_uniq', 'unique(name, company_id, department_id)', 'The name of the job position must be unique per department in company!'),
+        ('no_of_recruitment_positive', 'CHECK(no_of_recruitment >= 0)', 'The expected number of new employees must be positive.')
     ]
 
     @api.depends('no_of_recruitment', 'employee_ids.job_id', 'employee_ids.active')

addons/hr/views/hr_job_views.xml
@@ -50,6 +50,7 @@
             <field name="model">hr.job</field>
             <field name="arch" type="xml">
                 <tree string="Job" sample="1">
+                    <field name="sequence" widget="handle"/>
                     <field name="name"/>
                     <field name="department_id"/>
                     <field name="no_of_employee"/>

addons/hr_recruitment/models/hr_job.py
@@ -8,7 +8,7 @@ from odoo import api, fields, models, _
 class Job(models.Model):
     _name = "hr.job"
     _inherit = ["mail.alias.mixin", "hr.job"]
-    _order = "state desc, name asc"
+    _order = "sequence, state desc, name asc"
 
     @api.model
     def _default_address_id(self):

addons/website_hr_recruitment/controllers/main.py
@@ -30,7 +30,7 @@ class WebsiteHrRecruitment(http.Controller):
 
         # List jobs available to current UID
         domain = request.website.website_domain()
-        job_ids = Jobs.search(domain, order="is_published desc, no_of_recruitment desc").ids
+        job_ids = Jobs.search(domain, order="is_published desc, sequence, no_of_recruitment desc").ids
         # Browse jobs as superuser, because address is restricted
         jobs = Jobs.sudo().browse(job_ids)
 

addons/website_hr_recruitment/models/hr_recruitment.py
@@ -51,6 +51,7 @@ class Job(models.Model):
         default_description = self.env["ir.model.data"].xmlid_to_object("website_hr_recruitment.default_website_description")
         return (default_description._render() if default_description else "")
 
+    website_published = fields.Boolean(help='Set if the application is published on the website of the company.')
     website_description = fields.Html('Website description', translate=html_translate, sanitize_attributes=False, default=_get_default_website_description, prefetch=False, sanitize_form=False)
 
     def _compute_website_url(self):

addons/website_hr_recruitment/static/src/scss/website_hr_recruitment.scss
@@ -20,4 +20,20 @@
             }
         }
     }
+    .o_website_hr_recruitment_job_description {
+        //The ellipsis may not be supported on all platforms, the text will just break for them
+        max-height: 60px; //Limit to 3 lines
+        line-height: 20px;
+        -webkit-line-clamp: 3;
+        -moz-line-clamp: 3;
+        -ms-line-clamp: 3;
+        line-clamp: 3;
+        word-break: break-word;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -moz-box-orient: vertical;
+        -ms-box-orient: vertical;
+        box-orient: vertical;
+        overflow: hidden;
+    }
 }

addons/website_hr_recruitment/views/hr_recruitment_views.xml
@@ -35,6 +35,9 @@
             <div name="button_box" position="inside">
                 <field name="is_published" widget="website_redirect_button"/>
             </div>
+            <xpath expr="//field[@name='no_of_recruitment']" position="after">
+                <field name="website_published" string="Is Published"/>
+            </xpath>
         </field>
     </record>
 
@@ -57,6 +60,9 @@
             <field name="department_id" position="after">
                 <field name="website_id" groups="website.group_multi_website"/>
             </field>
+            <xpath expr="//field[@name='state']" position="after">
+                <field name="website_published" string="Published"/>
+            </xpath>
         </field>
     </record>
 </odoo>

addons/website_hr_recruitment/views/website_hr_recruitment_templates.xml
@@ -49,14 +49,14 @@
                                     <h3 class="text-secondary mt0 mb4">
                                         <span t-field="job.name"/>
                                     </h3>
-                                    <h5 t-if="job.no_of_recruitment &gt; 1">
+                                    <h5 t-if="job.no_of_recruitment &gt;= 1">
                                         <t t-esc="job.no_of_recruitment"/> open positions
                                     </h5>
                                     <div t-if="editable"
                                        t-field="job.description"
                                        class="mt16 mb0 css_non_editable_mode_hidden"/>
-                                    <div t-esc="len((job.description or '').split(' ')) > 35 and '%s ...' % ' '.join(job.description.split(' ')[:35]) or job.description"
-                                        class="mt16 mb0 css_editable_mode_hidden"
+                                    <div t-esc="job.description or ''"
+                                        class="mt16 mb0 css_editable_mode_hidden o_website_hr_recruitment_job_description"
                                     />
                                     <div class="o_job_infos mt16">
                                         <span t-field="job.address_id" t-options='{