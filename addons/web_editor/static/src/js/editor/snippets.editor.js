c90ce06124402509835efdfbe7a068c47d216652
xO-Tx <mou@odoo.com>
2021-07-06 16:55:35 +0200

    [FIX] web_editor: fix shapes snippet option
    - Exclude '.o_we_pager_header' from click behaviour so clicking on the
      shape dropdown does not close it if an user misclicks next to a pager
      button.
    
    - Add borders on navigation buttons in shapes option to improve
      visibility.
    
    - Cut the last visible option in the shapes list, this way the user
      will understand that he can scroll.
    
    Part of https://github.com/odoo/odoo/pull/69857
    task-2431484
    
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -1061,7 +1061,7 @@ var SnippetsMenu = Widget.extend({
             });
 
             var $target = $(srcElement);
-            if (!$target.closest('we-button, we-toggler, .o_we_color_preview').length) {
+            if (!$target.closest('we-button, we-toggler, we-select, .o_we_color_preview').length) {
                 this._closeWidgets();
             }
             if (!$target.closest('body > *').length) {
