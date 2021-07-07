484ba72801b58a1334ad60125dc022a973d36d8a
Thibault Delavallée <tde@odoo.com>
2021-07-07 06:51:36 +0000

    [IMP] website_event_*: simplify menu definition code
    Now that all menus are managed the same way using a menu type and
    clear definitions we can set all menu type definitions to override
    _get_website_menu_entries. This is just some code cleaning and
    has no functional impact.
    
    Task-2577079
    PR odoo#72411

--- addons/website_event_exhibitor/models/event_event.py
+++ addons/website_event_exhibitor/models/event_event.py
@@ -49,13 +49,15 @@ class EventEvent(models.Model):
         super(EventEvent, self)._update_website_menus(menus_update_by_field=menus_update_by_field)
         for event in self:
             if event.menu_id and (not menus_update_by_field or event in menus_update_by_field.get('exhibitor_menu')):
-                event._update_website_menu_entry('exhibitor_menu', 'exhibitor_menu_ids', '_get_exhibitor_menu_entries')
+                event._update_website_menu_entry('exhibitor_menu', 'exhibitor_menu_ids', 'exhibitor')
 
     def _get_menu_type_field_matching(self):
         res = super(EventEvent, self)._get_menu_type_field_matching()
         res['exhibitor'] = 'exhibitor_menu'
         return res
 
-    def _get_exhibitor_menu_entries(self):
+    def _get_website_menu_entries(self):
         self.ensure_one()
-        return [(_('Exhibitors'), '/event/%s/exhibitors' % slug(self), False, 60, 'exhibitor')]
+        return super(EventEvent, self)._get_website_menu_entries() + [
+            (_('Exhibitors'), '/event/%s/exhibitors' % slug(self), False, 60, 'exhibitor')
+        ]
