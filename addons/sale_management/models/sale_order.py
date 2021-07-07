bfdb207c9f681eb410ca35fe9c4232d6370952cb
Hubert (huvw) <huvw@odoo.com>
2021-07-07 13:55:04 +0000

    [FIX] sale_management: Pricelist singleton error while choosing optional product
    Backport of b6ced2e13e328539b086067a836d1e0fba3990b7
    
    opw-2517635
    
    closes odoo/odoo#73320
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale_management/models/sale_order.py
+++ addons/sale_management/models/sale_order.py
@@ -181,7 +181,8 @@ class SaleOrderOption(models.Model):
         new_sol = self.env['sale.order.line'].new(values)
         new_sol._onchange_discount()
         self.discount = new_sol.discount
-        self.price_unit = new_sol._get_display_price(product)
+        if self.order_id.pricelist_id and self.order_id.partner_id:
+            self.price_unit = new_sol._get_display_price(product)
         return {'domain': domain}
 
     @api.multi
