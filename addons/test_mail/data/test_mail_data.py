8af1210a6261ee1c3cdadb12315d966fa5e4d378
Jairo Llopis <jairo.llopis@tecnativa.com>
2021-07-08 10:12:41 +0000

    [FW][IMP] test_mail: test models with type do not mess with attachment types
    Purpose of this commit is to highlight an issue that may happens easily with
    `crm` that is made generic here within `test_mail`.
    
    `crm` alters the context when creating a new record adding in this case
    `default_type` to it][1]. The returned record contains that altered context.
    his results in other records created from it trying to assign that same default
    value for `type`. This is a very common name for fields, and happens to exist
    in `ir.attachment` too.
    
    If you create an alias for incoming leads in your DB with default values
    `{"type": "lead"}` (something very common) and then an email comes to that
    alias that contains an inlined base64 image, the attachment creation process
    would simply fail.
    
    Obtained error is ``ValueError: Wrong value for ir.attachment.type: 'lead'`` .
    
    [1]: https://github.com/odoo/odoo/blob/272602193f5647f7f2270ed6ec68777625a139dd/addons/crm/models/crm_lead.py#L310-L311
    
    X-original-commit: 99434b2e8528c10fcc9cb6860765e0ddcaa364c8
    Part-of: odoo/odoo#77005
    Co-authored-by: Thibault Delavallee <tde@odoo.com>

--- addons/test_mail/data/test_mail_data.py
+++ addons/test_mail/data/test_mail_data.py
@@ -43,6 +43,50 @@ Content-Transfer-Encoding: quoted-printable
 ------=_Part_4200734_24778174.1344608186754--
 """
 
+MAIL_TEMPLATE_EXTRA_HTML = """Return-Path: <whatever-2a840@postmaster.twitter.com>
+To: {to}
+cc: {cc}
+Received: by mail1.openerp.com (Postfix, from userid 10002)
+    id 5DF9ABFB2A; Fri, 10 Aug 2012 16:16:39 +0200 (CEST)
+From: {email_from}
+Subject: {subject}
+MIME-Version: 1.0
+Content-Type: multipart/alternative;
+    boundary="----=_Part_4200734_24778174.1344608186754"
+Date: Fri, 10 Aug 2012 14:16:26 +0000
+Message-ID: {msg_id}
+{extra}
+------=_Part_4200734_24778174.1344608186754
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: quoted-printable
+
+Please call me as soon as possible this afternoon!
+
+--
+Sylvie
+------=_Part_4200734_24778174.1344608186754
+Content-Type: text/html; charset=utf-8
+Content-Transfer-Encoding: quoted-printable
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+ <head>=20
+  <meta http-equiv=3D"Content-Type" content=3D"text/html; charset=3Dutf-8" />
+ </head>=20
+ <body style=3D"margin: 0; padding: 0; background: #ffffff;-webkit-text-size-adjust: 100%;">=20
+
+  <p>Please call me as soon as possible this afternoon!</p>
+  {extra_html}
+
+  <p>--<br/>
+     Sylvie
+  <p>
+ </body>
+</html>
+------=_Part_4200734_24778174.1344608186754--
+"""
+
+
 MAIL_TEMPLATE_PLAINTEXT = """Return-Path: <whatever-2a840@postmaster.twitter.com>
 To: {to}
 Received: by mail1.openerp.com (Postfix, from userid 10002)
