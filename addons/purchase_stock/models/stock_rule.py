fed64bb0a63acbf47f9ec4883d95fa1ff1096bca
Swapnesh Shah <swapneshshah111@gmail.com>
2021-07-11 07:58:28 +0000

    [FIX] purchase_stock: allow non admin users to replenish product
    Before this commit, Non admin user was not allowed to Replenish product
    with `Buy` route as access to `ir.config_parameter` is allowed to Admin users only.
    
    With this commit, we are passing `sudo()` to get `purchase_stock.delta_days_merge`
    from `ir.config_parameter`.
    
    Fixes #73400
    
    X-original-commit: 1b03087524080f1a9751b1561cd8891466e72367

--- addons/purchase_stock/models/stock_rule.py
+++ addons/purchase_stock/models/stock_rule.py
@@ -310,6 +310,10 @@ class StockRule(models.Model):
         )
         if values.get('orderpoint_id'):
             procurement_date = fields.Date.to_date(values['date_planned']) - relativedelta(days=int(values['supplier'].delay) + company_id.po_lead)
+<<<<<<< HEAD
+=======
+            delta_days = int(self.env['ir.config_parameter'].sudo().get_param('purchase_stock.delta_days_merge') or 0)
+>>>>>>> adc0c6ba8a8... temp
             domain += (
                 ('date_order', '<=', datetime.combine(procurement_date, datetime.max.time())),
                 ('date_order', '>=', datetime.combine(procurement_date, datetime.min.time()))
