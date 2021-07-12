19970106f73b9c28d109927995674695489d1f73
sal-odoo <sal@odoo.com>
2021-07-12 10:05:04 +0400

    [ADD] add l10n_ae_pos

---
+++ addons/l10n_ae_pos/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'U.A.E. - Point of Sale',
+    'author': 'Odoo PS',
+    'category': 'Accounting/Localizations/Point of Sale',
+    'description': """
+United Arab Emirates POS Localization
+=======================================================
+    """,
+    'depends': ['point_of_sale'],
+    'qweb': ['static/src/xml/Screens/ReceiptScreen/OrderReceipt.xml'],
+}
