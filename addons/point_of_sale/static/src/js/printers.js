d0e05dbe44bddcd66de955c799d2aa8e77195db4
Jacky (trj) <trj@odoo.com>
2021-07-12 09:52:49 +0000

    [FIX] point_of_sale: characters overlap in the receipt with html2canvas
    Some characters were overlapped in the printed receipt or receipt sent by email because the library html2canvas doesn't handle well the css wrapping (e.g. in the German localization) by default.
    
    X-original-commit: 13644cbdc298c3ca6b4a6faf2ad9bce96dc80bfe

--- addons/point_of_sale/static/src/js/printers.js
+++ addons/point_of_sale/static/src/js/printers.js
@@ -101,7 +101,8 @@ var PrinterMixin = {
                 onrendered: function (canvas) {
                     $('.pos-receipt-print').empty();
                     resolve(self.process_canvas(canvas));
-                }
+                },
+                letterRendering: true,
             })
         });
         return promise;
