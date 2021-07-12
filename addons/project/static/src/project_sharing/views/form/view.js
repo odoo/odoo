cb01fe6c902a524d185007278987074aa5046a3c
Xavier BOL (xbo) <xbo@odoo.com>
2021-07-12 11:31:56 +0200

    [IMP] project: add chatter in project sharing form view
    Before this commit, the chatter is not available in form view of project
    sharing even for portal user. Since the portal views, we have a chatter
    for the portal users, we need a chatter for those users in project
    sharing.
    
    This commit integrates the portal chatter in the form view for only the
    portal users.
    
    task-2379518
    
    closes #73341

--- addons/project/static/src/project_sharing/views/form/view.js
+++ addons/project/static/src/project_sharing/views/form/view.js
@@ -2,9 +2,11 @@
 
 import FormView from 'web.FormView';
 import Controller from './controller';
+import Renderer from './renderer';
 
 export default FormView.extend({
     config: _.extend({}, FormView.prototype.config, {
         Controller,
+        Renderer,
     }),
 });
