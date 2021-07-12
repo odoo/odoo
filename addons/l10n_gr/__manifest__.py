bcb1bd22125c4e31f3b8d7489310e0e2a0964736
Paolo (pgi) <pgi@odoo.com>
2021-07-12 12:50:19 +0000

    [FIX] l10n_*: Localization tax group localization data normalization
    "account_tax_group.xml" and "account_data.xml" data files
    have been renamed to "account_tax_group_data.xml"
    when containing only tax groups, for compliance with the standard.
    
    AE, AR, AT, BE, BO, BR, CA, CN, CR, CZ,
    DE SKR03, DE SKR04, DO, ES, FI, FR, GR,
    GT, HN, IL, IN, LT, MA, MX, NL, NO, PA,
    PL, PT, RO, SG, SY, TH, TR, UA, UY, VE,
    VN.
    
    Part-of: odoo/odoo#77295

--- addons/l10n_gr/__manifest__.py
+++ addons/l10n_gr/__manifest__.py
@@ -23,7 +23,7 @@ Greek accounting chart and localization.
               'data/l10n_gr_chart_data.xml',
               'data/account.account.template.csv',
               'data/account_chart_template_data.xml',
-              'data/account_data.xml',
+              'data/account_tax_group_data.xml',
               'data/account_tax_report_data.xml',
               'data/account_tax_data.xml',
               'data/account_chart_template_configure_data.xml'
--- addons/l10n_tr/__manifest__.py
+++ addons/l10n_tr/__manifest__.py
@@ -22,7 +22,7 @@ Bu modül kurulduktan sonra, Muhasebe yapılandırma sihirbazı çalışır
         'data/l10n_tr_chart_data.xml',
         'data/account.account.template.csv',
         'data/l10n_tr_chart_post_data.xml',
-        'data/account_data.xml',
+        'data/account_tax_group_data.xml',
         'data/account_tax_template_data.xml',
         'data/account_chart_template_data.xml',
     ],
