a56fdf48f92d4096adc17302cdd43d71e99642c3
Benjamin Frantzen (bfr) <bfr@odoo.com>
2021-07-12 09:31:25 +0000

    [FIX] account_edi_ubl: convert narration to text before setting it in the xml.
    closes odoo/odoo#73818
    
    X-original-commit: 20490dc384310c1de4ec5fe02f90af030cc18ca7
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>
    Signed-off-by: bfr-o <bfr-o@users.noreply.github.com>

--- addons/l10n_be_edi/tests/test_ubl.py
+++ addons/l10n_be_edi/tests/test_ubl.py
@@ -76,7 +76,6 @@ class TestL10nBeEdi(AccountEdiTestCommon):
                 <ID>INV/2017/00001</ID>
                 <IssueDate>2017-01-01</IssueDate>
                 <InvoiceTypeCode>380</InvoiceTypeCode>
-                <Note></Note>
                 <DocumentCurrencyCode>Gol</DocumentCurrencyCode>
                 <AccountingSupplierParty>
                     <Party>
