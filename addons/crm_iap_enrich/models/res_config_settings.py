aed28b43289bd57b4e39bf64fcee726ec526dd19
Thibault Delavallée <tde@odoo.com>
2021-07-12 11:30:19 +0200

    [MOV] crm_iap_enrich: rename ``crm_iap_lead_enrich`` module
    PURPOSE
    
    Perform a global renaming / cleaning of IAP features often added or merged
    with minimal review. Time to cleanup !
    
    SPECIFICATIONS
    
    Rename ``crm_iap_lead_enrich`` to ``crm_iap_enrich`` . Lead naming is not
    really required as it is a bridge build on ``crm``.
    
    LINKS
    
    Task-2630969
    Prepares Task-2600047 (code improvements and cleaning)
    COM PR odoo/odoo#75514
    ENT PR odoo/enterprise#20424
    UPG PR odoo/upgrade#2770

--- addons/crm_iap_enrich/models/res_config_settings.py
+++ addons/crm_iap_enrich/models/res_config_settings.py
@@ -10,12 +10,12 @@ class ResConfigSettings(models.TransientModel):
     @api.model
     def get_values(self):
         values = super(ResConfigSettings, self).get_values()
-        cron = self.sudo().with_context(active_test=False).env.ref('crm_iap_lead_enrich.ir_cron_lead_enrichment', raise_if_not_found=False)
+        cron = self.sudo().with_context(active_test=False).env.ref('crm_iap_enrich.ir_cron_lead_enrichment', raise_if_not_found=False)
         values['lead_enrich_auto'] = 'auto' if cron and cron.active else 'manual'
         return values
 
     def set_values(self):
         super(ResConfigSettings, self).set_values()
-        cron = self.sudo().with_context(active_test=False).env.ref('crm_iap_lead_enrich.ir_cron_lead_enrichment', raise_if_not_found=False)
+        cron = self.sudo().with_context(active_test=False).env.ref('crm_iap_enrich.ir_cron_lead_enrichment', raise_if_not_found=False)
         if cron:
             cron.active = self.lead_enrich_auto == 'auto'
