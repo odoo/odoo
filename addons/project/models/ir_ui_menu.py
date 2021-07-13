c9ce091c358eabb000f0ce7e5404fe64d8c30f76
William Braeckman <wbr@odoo.com>
2021-07-13 15:06:12 +0000

    [IMP] project: add project stages
    Adds new optional stages for the project.project model.
    
    Previously projects would stay dormant when they were finished, we can
    now define stages for those projects to determine where a stage is in
    it's lifecycle.
    
    Also adds some small changes to the project itself, such as adding tags.
    
    Closes odoo/odoo#73648
    
    Task ID: 2581575
    
    Related: odoo/upgrade#2646
    Signed-off-by: LTU-Odoo <IT-Ideas@users.noreply.github.com>

---
+++ addons/project/models/ir_ui_menu.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models
+
+
+class IrUiMenu(models.Model):
+    _inherit = 'ir.ui.menu'
+
+    def _load_menus_blacklist(self):
+        res = super()._load_menus_blacklist()
+        if self.env.user.has_group('project.group_project_stages'):
+            res.append(self.env.ref('project.menu_projects').id)
+        return res
