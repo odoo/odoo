4a0372b506d1e44003a1ec4793d3097936c3a608
Nicolas Bayet <nby@odoo.com>
2021-07-14 14:29:02 +0000

    [FIX] web_editor: paste multiline
    Inserting text that contain multiline does not work.
    One "\n" is removed.
    Two "\n" become only one.
    
    Task ID: 2602881
    
    closes odoo/odoo#73835
    
    X-original-commit: 89450137653526d9947bc21410cc4163e8b9b440
    Signed-off-by: Antoine Guenet <Zinston@users.noreply.github.com>

--- addons/web_editor/static/lib/odoo-editor/src/commands/commands.js
+++ addons/web_editor/static/lib/odoo-editor/src/commands/commands.js
@@ -94,7 +94,7 @@ function insert(editor, data, isText = true) {
         } else {
             startNode.after(nodeToInsert);
         }
-        if (isShrunkBlock(startNode)) {
+        if (startNode.tagName !== 'BR' && isShrunkBlock(startNode)) {
             startNode.remove();
         }
         startNode = nodeToInsert;
