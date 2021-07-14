8ad2e92f2800570d0d98504b82f6e79954a560be
Sébastien Theys <seb@odoo.com>
2021-07-14 10:45:44 +0000

    [IMP] mail: remove static mention suggestions
    Returning all the users at init does not scale. Mention suggestion code is good
    enough now that this is no longer necessary.
    
    closes odoo/odoo#73689
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/controllers/main.py
+++ addons/mail/controllers/main.py
@@ -261,7 +261,6 @@ class MailController(http.Controller):
             'channel_slots': request.env['mail.channel'].channel_fetch_slot(),
             'mail_failures': request.env['mail.message'].message_fetch_failed(),
             'commands': request.env['mail.channel'].get_mention_commands(),
-            'mention_partner_suggestions': request.env['res.partner'].get_static_mention_suggestions(),
             'shortcodes': request.env['mail.shortcode'].sudo().search_read([], ['source', 'substitution', 'description']),
             'menu_id': request.env['ir.model.data'].xmlid_to_res_id('mail.menu_root_discuss'),
             'partner_root': request.env.ref('base.partner_root').sudo().mail_partner_format(),
