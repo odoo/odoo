e2fdac55f6a66b733f4966d5f8a1d2f2f4fd8796
Laurent Smet <las@odoo.com>
2021-07-15 07:49:52 +0000

    [FIX] payment: Fix payment access rights for not admin users
    The journal needs to access the payment acquirers to determine with payment method is still available.
    Since the acquirers can't be accessed only for admin users, an accountant doesn't any right to read them, and then, an access error was raised when opening the journal form view.
    
    Also, the 'payment.acquirer' was accessed from the 'account' module instead of being overridden in 'payment'.
    
    closes odoo/odoo#73761
    
    X-original-commit: 7b4580c3756657c902eceb9840984dde76429dcd
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>

--- addons/payment/models/account_payment_method.py
+++ addons/payment/models/account_payment_method.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
+from odoo.osv import expression
 
 
 class AccountPaymentMethodLine(models.Model):
@@ -18,7 +19,7 @@ class AccountPaymentMethodLine(models.Model):
 
     @api.depends('payment_method_id')
     def _compute_payment_acquirer_id(self):
-        acquirers = self.env['payment.acquirer'].search([
+        acquirers = self.env['payment.acquirer'].sudo().search([
             ('provider', 'in', self.mapped('code')),
             ('company_id', 'in', self.journal_id.company_id.ids),
         ])
@@ -27,6 +28,19 @@ class AccountPaymentMethodLine(models.Model):
             code = line.payment_method_id.code
             company = line.journal_id.company_id
             line.payment_acquirer_id = acquirers_map.get((code, company), False)
+
+    def _get_payment_method_domain(self):
+        # OVERRIDE
+        domain = super()._get_payment_method_domain()
+        information = self._get_payment_method_information().get(self.code)
+
+        unique = information.get('mode') == 'unique'
+        if unique:
+            company_ids = self.env['payment.acquirer'].sudo().search([('provider', '=', self.code)]).mapped('company_id')
+            if company_ids:
+                domain = expression.AND([domain, [('company_id', 'in', company_ids.ids)]])
+
+        return domain
 
     def action_open_acquirer_form(self):
         self.ensure_one()
