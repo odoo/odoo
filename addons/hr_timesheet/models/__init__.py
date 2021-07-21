55b795ee5a87bc8d1b6e6a6617f4c5b82c1a0565
bbh-odoo <bbh@odoo.com>
2021-07-21 11:46:24 +0000

    [IMP] hr_timesheet: improved  access rights
    before this commit:
    The approver can access all timesheets which is not coherent.
    
    after this commit:
    improved the labelling of the timesheet app's roles, modified the menu according
    to role of User.Aprrover now can access his own timesheets only by click only
    available option  'My timesheets'.
    
    TaskID - 2531321
    
    closes odoo/odoo#74242
    
    Related: odoo/enterprise#19858
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_timesheet/models/__init__.py
+++ addons/hr_timesheet/models/__init__.py
@@ -4,6 +4,7 @@
 from . import hr_employee
 from . import hr_timesheet
 from . import ir_http
+from . import ir_ui_menu
 from . import res_company
 from . import res_config_settings
 from . import project
