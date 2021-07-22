fefd2cfd992f1b26915aff43744b9266a4df3b9f
Thibault Delavallée <tde@odoo.com>
2021-07-22 14:05:20 +0200

    [REF] crm_iap_mine: remove now unused crm.iap.lead.helpers model
    SPECIFICATIONS
    
    Content has now been moved into iap core models as tools methods. This
    custom model can now be safely removed.
    
    LINKS

--- addons/crm_iap_mine/models/__init__.py
+++ addons/crm_iap_mine/models/__init__.py
@@ -2,7 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import crm_lead
-from . import crm_iap_lead_helpers
 from . import crm_iap_lead_industry
 from . import crm_iap_lead_role
 from . import crm_iap_lead_seniority
