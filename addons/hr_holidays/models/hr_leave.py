7577ddedd05be5ffbd0b5b2f28fd3ab414e205bf
Nasreddin Boulif (bon) <bon@odoo.com>
2021-07-23 14:33:13 +0200

    [FIX] hr_holidays: Allow overlapping if not employee
    Issue:
    
      When creating a leave there is an error when you use by "employee tag"
      but it works when you use "by employee".
    
    Solution:
    
      cherry-pick of:
      https://github.com/odoo/odoo/commit/394eedc1e4b6b9871b360d6ab168005c2731d3ff
      https://github.com/odoo/odoo/commit/c89c0e70da1e09c43eee8e76c7094dcc114c451b
    
    opw-2525086

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -452,9 +452,9 @@ class HolidaysRequest(models.Model):
             else:
                 holiday.can_approve = True
 
-    @api.constrains('date_from', 'date_to')
+    @api.constrains('date_from', 'date_to', 'state', 'employee_id')
     def _check_date(self):
-        for holiday in self:
+        for holiday in self.filtered('employee_id'):
             domain = [
                 ('date_from', '<', holiday.date_to),
                 ('date_to', '>', holiday.date_from),
