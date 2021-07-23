c38870c023218cde46d4fdada29e019010564002
Leonardo Pavan Rocha <lpr@odoo.com>
2021-07-23 12:54:17 +0000

    [IMP] base,hr: add tests for avatar mixin
    In task 2404630 the avatar feature was created and no tests were written due to
    the iminent freeze. This PR implements tests for the avatar feature, such as
    avatar generation and placeholder logic.
    
    task-2578233
    
    closes odoo/odoo#79688
    
    X-original-commit: 400b3fd93266f88832446c8f7422f6e14b958115
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -13,6 +13,7 @@ from . import test_float
 from . import test_func
 from . import test_http_case
 from . import test_image
+from . import test_avatar_mixin
 from . import test_ir_actions
 from . import test_ir_attachment
 from . import test_ir_cron
