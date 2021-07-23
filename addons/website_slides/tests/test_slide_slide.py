fa7451544d017e3f8b50c4b1c9091d60db822dad
Munaf Khan <mub@odoo.com>
2021-07-23 13:17:29 +0000

    [IMP] website_slides: improve lesson design in documentation
    Currently, in the documentation type of courses, card layout for the lessons
    is broken because there isn't enough space to display everything.
    
    To fix this, this commit moves the 'Completed' badge (which is in the card
    footer) and other tags at the end of the card-body. It formats likes and
    dislikes using a decimalized formatting (ex, 10000 will be displayed
    as 10k).
    
    But that improvement leads to another trouble. Say there are '1k' likes and
    user upvotes the lesson. There is no indication whether or not the vote was
    recorded. To fix this, we will now use different icons based on the user vote
    
      - The thumbs-up icon will be filled if user has liked the lesson and it will
        be hollow otherwise;
      - Likewise, the thumbs-down icon will be filled if user has disliked the
        lesson, and it will be hollow otherwise;
    
    Clicking on either of the button, if the vote is recorded successfully, icon
    and user vote will be toggled for better indication. Note that before this
    commit, if user was performing the same action (for example upvoting again)
    a warning popover used to appear, which isn't the case anymore because now
    the vote will simply be toggled. The mechanism is somewhat similar to youtube
    (you can also directly dislike the already liked content and visa-versa).
    
    The same mechanism is also applied for likes/dislikes in the footer of each
    lesson's page. And, on the same page under 'Statistics' tab, they are simply
    formatted like everywhere else.
    
    Apart from that, this commit makes other small UI changes listed
    as below:
    
      - We now display three slides per category instead of four, giving the
        cards a bit more space;
      - On the lessons' page, we move the likes/dislikes right after description
        which previously was at bottom right of the page;
      - Likes / Rating is now always displayed the same in lessen view whatever
        the channel type (displaying ratings / comments / likes);
    
    Also see odoo/upgrade#2854
    
    Task-2607416
    
    Part-of: odoo/odoo#74171
    Co-authored-by: Munaf Bahelim <mub@odoo.com>
    Co-authored-by: Thibault Delavallée <tde@odoo.com>

--- addons/website_slides/tests/test_slide_slide.py
+++ addons/website_slides/tests/test_slide_slide.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import psycopg2
+
 from odoo.addons.website_slides.tests import common as slides_common
+from odoo.tests.common import users
+from odoo.tools import mute_logger
 
 
-class TestFromURL(slides_common.SlidesCase):
+class TestSlideInternals(slides_common.SlidesCase):
+
+    @mute_logger('odoo.sql_db')
+    @users('user_manager')
+    def test_slide_create_vote_constraint(self):
+        # test vote value must be 1, 0 and -1.
+        with self.assertRaises(psycopg2.errors.CheckViolation), self.cr.savepoint():
+            self.env['slide.slide.partner'].create({
+                'slide_id': self.slide.id,
+                'channel_id': self.channel.id,
+                'partner_id': self.user_manager.partner_id.id,
+                'vote': 2,
+            })
+
+class TestVideoFromURL(slides_common.SlidesCase):
     def test_video_youtube(self):
         youtube_urls = {
             'W0JQcpGLSFw': [
