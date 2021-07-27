533580c326d8831aadc8d6161cb2295c8ad166a3
Ivan Yelizariev <iel@odoo.com>
2021-07-27 17:46:08 +0200

    [IMP] base: check access rights in batch
    This commit optimizes `_visible_menu_ids` by checking access rights in batch.
    It saves about 200-300 ms. The cache is not needed since it's result is already
    cached in `load_menus' method.
    
    This commit also removes lang key from ormcache decorator for `.check` method.
    It was not needed even without this refactoring. See deleted comments for details

--- odoo/addons/base/models/ir_ui_menu.py
+++ odoo/addons/base/models/ir_ui_menu.py
@@ -73,7 +73,6 @@ class IrUiMenu(models.Model):
             raise ValidationError(_('Error! You cannot create recursive menus.'))
 
     @api.model
-    @tools.ormcache('frozenset(self.env.user.groups_id.ids)', 'debug')
     def _visible_menu_ids(self, debug=False):
         """ Return the ids of the menu items visible to the user. """
         # retrieve all menus, and determine which ones are visible
@@ -109,16 +108,22 @@ class IrUiMenu(models.Model):
             'ir.actions.server': 'model_name',
         }
 
+        all_models = []
         # performance trick: determine the ids to prefetch by type
         prefetch_ids = defaultdict(list)
         for action in action_menus.mapped('action'):
             prefetch_ids[action._name].append(action.id)
+            model_name = action._name in MODEL_BY_TYPE and action[MODEL_BY_TYPE[action._name]]
+            if model_name:
+                all_models.append(model_name)
+
+        check_multi = access.check_multi(all_models, 'read')
 
         for menu in action_menus:
             action = menu.action
             action = action.with_prefetch(prefetch_ids[action._name])
             model_name = action._name in MODEL_BY_TYPE and action[MODEL_BY_TYPE[action._name]]
-            if not model_name or access.check(model_name, 'read', False):
+            if not model_name or access.check(model_name, 'read', False, check_multi):
                 # make menu visible, and its folder ancestors, too
                 visible += menu
                 menu = menu.parent_id
