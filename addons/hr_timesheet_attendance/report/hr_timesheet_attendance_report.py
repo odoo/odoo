74efe493cca433630839d12dd0724c3adaa1fbbc
bbh-odoo <bbh@odoo.com>
2021-07-28 16:27:06 +0530

    [IMP] hr_timesheet,sale_timesheet: improved reporting features
    Purpose of the commit is to improve the timsheets's report
    of the timesheet app.
    
    So in this commit, done the below changes:
    - timesheets by billing type report:
     - add default group by date month > billable type.
     - removed undefine value from report
     - improve helper the to: "No data yet! \n Track your timesheets by
        billing type and make sure your time is billable."
     - added grid, kanban and list view
    - list view opened through the graph/pivot view: change the name of the action to 'Timesheets'
    - improved name of the report to 'Timesheet by employee'
    - improved helper of Timesheet by project report
    - Timesheet / Attendance report:
     - disabled list opening after clicking graph view.
     - added a group by Employee on report ,
     - modified pivot view according to company of user.
     - added default measure is total difference' and the default group by
       is date > month for report
    
    TaskId: 253134

--- addons/hr_timesheet_attendance/report/hr_timesheet_attendance_report.py
+++ addons/hr_timesheet_attendance/report/hr_timesheet_attendance_report.py
@@ -14,6 +14,7 @@ class TimesheetAttendance(models.Model):
     total_timesheet = fields.Float()
     total_attendance = fields.Float()
     total_difference = fields.Float()
+    company_id = fields.Many2one('res.company', string='Company', readonly=True)
 
     def init(self):
         tools.drop_view_if_exists(self.env.cr, self._table)
@@ -22,6 +23,7 @@ class TimesheetAttendance(models.Model):
                 max(id) AS id,
                 t.user_id,
                 t.date,
+                t.company_id,
                 coalesce(sum(t.attendance), 0) AS total_attendance,
                 coalesce(sum(t.timesheet), 0) AS total_timesheet,
                 coalesce(sum(t.attendance), 0) - coalesce(sum(t.timesheet), 0) as total_difference
@@ -31,7 +33,8 @@ class TimesheetAttendance(models.Model):
                     resource_resource.user_id AS user_id,
                     hr_attendance.worked_hours AS attendance,
                     NULL AS timesheet,
-                    hr_attendance.check_in::date AS date
+                    hr_attendance.check_in::date AS date,
+                    resource_resource.company_id as company_id
                 FROM hr_attendance
                 LEFT JOIN hr_employee ON hr_employee.id = hr_attendance.employee_id
                 LEFT JOIN resource_resource on resource_resource.id = hr_employee.resource_id
@@ -41,11 +44,12 @@ class TimesheetAttendance(models.Model):
                     ts.user_id AS user_id,
                     NULL AS attendance,
                     ts.unit_amount AS timesheet,
-                    ts.date AS date
+                    ts.date AS date,
+                    NULL AS company_id
                 FROM account_analytic_line AS ts
                 WHERE ts.project_id IS NOT NULL
             ) AS t
-            GROUP BY t.user_id, t.date
+            GROUP BY t.user_id, t.date, t.company_id
             ORDER BY t.date
         )
         """ % self._table)
