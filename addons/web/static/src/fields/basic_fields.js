506d8bcbb6b1035986fb79972861e633a9acb8ed
Julien Mougenot <jum@odoo.com>
2021-07-29 17:45:33 +0200

    [WIP] Kanban view

--- addons/web/static/src/fields/basic_fields.js
+++ addons/web/static/src/fields/basic_fields.js
@@ -1,5 +1,7 @@
 /** @odoo-module **/
 import { registry } from "@web/core/registry";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { _lt } from "../core/l10n/translation";
 
 const { Component } = owl;
 const fieldRegistry = registry.category("fields");
@@ -20,14 +22,30 @@ export class FieldChar extends Component {
 fieldRegistry.add("char", FieldChar);
 
 // -----------------------------------------------------------------------------
+// FieldBoolean
+// -----------------------------------------------------------------------------
+
+export class FieldBoolean extends Component {
+    setup() {
+        this.record = this.props.record;
+        this.data = Boolean(this.record.data[this.props.name]);
+    }
+}
+
+FieldBoolean.template = "web.FieldBoolean";
+FieldBoolean.components = { CheckBox };
+
+fieldRegistry.add("boolean", FieldBoolean);
+
+// -----------------------------------------------------------------------------
 // FieldImage
 // -----------------------------------------------------------------------------
 
-const fileTypeMagicWordMap = {
+export const fileTypeMagicWordMap = {
     "/": "jpg",
     R: "gif",
     i: "png",
-    P: "svg+xml"
+    P: "svg+xml",
 };
 export class FieldImage extends Component {
     static template = "web.FieldImage";
@@ -60,3 +78,32 @@ export class FieldSelection extends Component {
 }
 
 fieldRegistry.add("selection", FieldSelection);
+
+// -----------------------------------------------------------------------------
+// FieldColorPicker
+// -----------------------------------------------------------------------------
+
+export class FieldColorPicker extends Component {
+    setup() {
+        this.record = this.props.record;
+        this.data = this.record.data[this.props.name] || "";
+    }
+}
+
+FieldColorPicker.template = "web.FieldChar";
+FieldColorPicker.RECORD_COLORS = [
+    _lt("No color"),
+    _lt("Red"),
+    _lt("Orange"),
+    _lt("Yellow"),
+    _lt("Light blue"),
+    _lt("Dark purple"),
+    _lt("Salmon pink"),
+    _lt("Medium blue"),
+    _lt("Dark blue"),
+    _lt("Fushia"),
+    _lt("Green"),
+    _lt("Purple"),
+];
+
+fieldRegistry.add("color_picker", FieldColorPicker);
--- addons/web/static/src/fields/relational_fields.js
+++ addons/web/static/src/fields/relational_fields.js
@@ -13,3 +13,14 @@ export class FieldMany2one extends Component {
 }
 
 registry.category("fields").add("many2one", FieldMany2one);
+
+export class FieldMany2ManyTags extends Component {
+    static template = "web.FieldMany2ManyTags";
+
+    setup() {
+        const data = this.props.record.data[this.props.name];
+        this.data = data ? data[1] : "";
+    }
+}
+
+registry.category("fields").add("many2many_tags", FieldMany2ManyTags);
