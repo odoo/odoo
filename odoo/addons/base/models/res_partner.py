f13624be1a924bc6c0aab0c59b7e305c8821c281
Thibault Delavallée <tde@odoo.com>
2021-07-30 09:42:42 +0200

    [IMP] base: avoid double formatting in partner 'email_formatted' field
    PURPOSE
    
    Be defensive when dealing with email fields, notably when having multi-emails
    or email field containing an already-formatted email.
    
    SPECIFICATIONS
    
    Main fix in this commit: fix multiple nested formatting in 'email_formatted'
    computation for <res.partner>. Other use cases are mainly left untouched as
    we let users deal with their input. In summary :
    
      * double format: if email already holds a formatted email, we should not use
        it to compute email_formatted, like
    
          name: Name / email: 'Format' <email@domain.com>
          -> before '"Name" <"Name" <email@domain.com>>"
          -> after '"Name" <email@domain.com>''
    
      * multi emails: sometimes this field is used to hold several addresses
        like email1@domain.com, email2@domain.com. We currently let this value
        globally untouched by extracting emails and joining them, as we do not
        expect email_formatted to be a list of emails. Extractin emails allows
        to filter out extra text stored in email field, like
    
          name: Name / email: text, email1@domain.com, email2@domain.com
          -> before: "Name" <text, email1@domain.com, email2@domain.com>
          -> after: "Name" <email1@domain.com,email2@domain.com>
    
      * invalid email: if something is wrong, better keep it in email_formatted
        than harcoding "False". Indeed this eases management and understanding
        of failures at mail.mail, mail.notification and mailing.trace level. This
        behavior does not change as it was already implemented like that even if
        not sure it was intended;
    
    Task-2612945 (Mail: Defensive email formatting)
    
    X-original-commit: 7f92286a6840f088f107fa47ed33d499217911e3

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -503,11 +503,36 @@ class Partner(models.Model):
 
     @api.depends('name', 'email')
     def _compute_email_formatted(self):
+        """ Compute formatted email for partner, using formataddr. Be defensive
+        in computation, notably
+
+          * double format: if email already holds a formatted email like
+            'Name' <email@domain.com> we should not use it as it to compute
+            email formatted like "Name <'Name' <email@domain.com>>";
+          * multi emails: sometimes this field is used to hold several addresses
+            like email1@domain.com, email2@domain.com. We currently let this value
+            untouched, but remove any formatting from multi emails;
+          * invalid email: if something is wrong, keep it in email_formatted as
+            this eases management and understanding of failures at mail.mail,
+            mail.notification and mailing.trace level;
+          * void email: email_formatted is False, as we cannot do anything with
+            it;
+        """
+        self.email_formatted = False
         for partner in self:
-            if partner.email:
-                partner.email_formatted = tools.formataddr((partner.name or u"False", partner.email or u"False"))
-            else:
-                partner.email_formatted = ''
+            emails_normalized = tools.email_normalize_all(partner.email)
+            if emails_normalized:
+                # note: multi-email input leads to invalid email like "Name" <email1, email2>
+                # but this is current behavior in Odoo 14+ and some servers allow it
+                partner.email_formatted = tools.formataddr((
+                    partner.name or u"False",
+                    ','.join(emails_normalized)
+                ))
+            elif partner.email:
+                partner.email_formatted = tools.formataddr((
+                    partner.name or u"False",
+                    partner.email
+                ))
 
     @api.depends('is_company')
     def _compute_company_type(self):
