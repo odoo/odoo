20a970ead8df9d0384fca7d3399c068ae25bd671
Lucas Perais (lpe) <lpe@odoo.com>
2021-07-30 14:27:36 +0200

    [FIX] form button box improve dropdown and style
    Moved style from form_view.scss to button_bux.scss

--- addons/web/static/src/views/form/button_box/button_box.js
+++ addons/web/static/src/views/form/button_box/button_box.js
@@ -1,15 +1,36 @@
 /** @odoo-module  */
 import { useEffect } from "@web/core/effect_hook";
-import { browser } from "@web/core/browser/browser";
+import { useService } from "@web/core/service_hook";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+
+const DEFAULT_MAX_BUTTONS = 7;
+
+class ButtonBoxDropdown extends Dropdown {
+    setup() {
+        super.setup();
+        useEffect(
+            () => this.props.redrawButtons(this.state.open),
+            () => [this.state.open]
+        );
+    }
+}
+ButtonBoxDropdown.props = Object.assign(Object.create(Dropdown.props), {
+    redrawButtons: { type: Function },
+});
 
 export class ButtonBox extends owl.Component {
     setup() {
+        const ui = useService("ui");
+        const getMaxButtons = () => {
+            return [2, 2, 2, 4][ui.size] || DEFAULT_MAX_BUTTONS;
+        };
+
         const dropdown = owl.hooks.useRef("dropdown");
 
-        const maxButtonsInBox = 3;
-
         let visibleButtons, dropDownButtons;
+
         useEffect(() => {
+            const maxButtonsInBox = 1; //getMaxButtons();
             visibleButtons = [];
             dropDownButtons = [];
             const children = this.el.children;
@@ -29,22 +50,25 @@ export class ButtonBox extends owl.Component {
                 }
             }
             dropdown.el.classList.toggle("o_hidden", !dropDownButtons.length);
+            this.el.classList.toggle("o-full", dropDownButtons.length);
+            this.el.classList.toggle("o-not-full", !dropDownButtons.length);
         });
 
-        let timeoutId;
-        this.beforeOpenDropdown = () => {
-            browser.clearTimeout(timeoutId);
-            timeoutId = browser.setTimeout(() => {
-                const children = dropdown.el.querySelector(".o_dropdown_menu").children;
-                for (let index = 0; index < children.length; index++) {
-                    const elem = children[index];
-                    elem.classList.remove("o_hidden");
-                    if (index in visibleButtons) {
-                        elem.classList.add("o_hidden");
-                    }
+        this.dropdownRedrawButtons = (isOpen) => {
+            if (!isOpen) {
+                return;
+            }
+            const dropdownMenu = dropdown.el.querySelector(".o_dropdown_menu");
+            const children = dropdownMenu.children;
+            for (let index = 0; index < children.length; index++) {
+                const elem = children[index];
+                elem.classList.remove("o_hidden");
+                if (index in visibleButtons) {
+                    elem.classList.add("o_hidden");
                 }
-            });
+            }
         };
     }
 }
 ButtonBox.template = "web.Form.ButtonBox";
+ButtonBox.components = { ButtonBoxDropdown };
