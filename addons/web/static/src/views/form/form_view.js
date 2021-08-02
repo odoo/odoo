70edf044fce49562329b010b8fcab15240c5e6c5
Julien Mougenot <jum@odoo.com>
2021-08-02 18:54:17 +0200

    Load relational data

--- addons/web/static/src/views/form/form_view.js
+++ addons/web/static/src/views/form/form_view.js
@@ -1,11 +1,12 @@
 /* @odoo-module */
 
-import { registry } from "@web/core/registry";
-import { XMLParser } from "@web/core/utils/xml";
-import { ControlPanel } from "@web/search/control_panel/control_panel";
-import { Pager, usePager } from "@web/search/pager/pager";
-import { useModel } from "@web/views/helpers/model";
 import { useDebugMenu } from "../../core/debug/debug_menu";
+import { registry } from "../../core/registry";
+import { XMLParser } from "../../core/utils/xml";
+import { ControlPanel } from "../../search/control_panel/control_panel";
+import { Pager, usePager } from "../../search/pager/pager";
+import { useModel } from "../../views/helpers/model";
+import { FieldParser } from "../helpers/view_utils";
 import { RelationalModel } from "../relational_model";
 import { FormRenderer } from "./form_renderer";
 import { useViewButtons } from "@web/views/view_button/hook";
@@ -13,36 +14,35 @@ import { useViewButtons } from "@web/views/view_button/hook";
 // -----------------------------------------------------------------------------
 
 class FormArchParser extends XMLParser {
-    parse(arch) {
-        const _fields = new Set();
-        _fields.add("display_name");
+    parse(arch, fields) {
         const xmlDoc = this.parseXML(arch);
+        const fieldParser = new FieldParser(fields);
         this.visitXML(xmlDoc, (node) => {
             if (node.tagName === "field") {
-                _fields.add(node.getAttribute("name"));
+                fieldParser.addField(node);
             }
         });
-        return { fields: [..._fields], arch, xmlDoc };
+        return {
+            arch,
+            xmlDoc,
+            fields: ["display_name", ...fieldParser.getFields()],
+            relations: fieldParser.getRelations(),
+        };
     }
 }
 
 // -----------------------------------------------------------------------------
 
 class FormView extends owl.Component {
-    static type = "form";
-    static display_name = "Form";
-    static multiRecord = false;
-    static template = `web.FormView`;
-    static components = { ControlPanel, FormRenderer, Pager };
-
     setup() {
         useDebugMenu("view", { component: this });
-        this.archInfo = new FormArchParser().parse(this.props.arch);
+        this.archInfo = new FormArchParser().parse(this.props.arch, this.props.fields);
         this.model = useModel(RelationalModel, {
             resModel: this.props.resModel,
             resId: this.props.resId,
             resIds: this.props.resIds,
             fields: this.props.fields,
+            relations: this.archInfo.relations,
             activeFields: this.archInfo.fields,
         });
         this.pagerProps = usePager(this.model, this.props.resId, this.props.resIds);
@@ -50,5 +50,11 @@ class FormView extends owl.Component {
         useViewButtons(this.model);
     }
 }
+
+FormView.type = "form";
+FormView.display_name = "Form";
+FormView.multiRecord = false;
+FormView.template = `web.FormView`;
+FormView.components = { ControlPanel, FormRenderer, Pager };
 
 registry.category("views").add("form", FormView);
