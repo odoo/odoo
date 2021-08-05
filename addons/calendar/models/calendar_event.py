a8b4f8f6c009d8abd7210973005fd755ac3bc573
Adrien Widart <awt@odoo.com>
2021-08-05 08:50:03 +0000

    [FIX] crm: invite partners on quick creation of meeting
    When creating a meeting from a partner/lead, the invitations won't be
    created/sent
    
    To reproduce the error:
    1. Open a lead
    2. Click on Meeting
    3. Create a meeting
        - On meeting creation, directly click on "Create", not "Edit"
    
    Error: No invitation has been sent. When editing the created event,
    there isn't any invitation on Invitations tab. Same error will happen
    when opening the form of a customer instead of a lead (step 1)
    
    For an attendee to be created, the partners associated with the meeting
    must be explicitly listed in the creation values:
    https://github.com/odoo/odoo/blob/3e20e68f0790a0b0f3b5c4d43f59f235b7d20fef/addons/calendar/models/calendar_event.py#L710-L713
    However, in the above use case, the partners identifiers are given
    through the context. This explains why the attendees are not created.
    
    OPW-2531496
    
    closes odoo/odoo#75046
    
    X-original-commit: 365459804b4f9a59abf4b6e90c1100f3d0a9eb9a
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Arnaud Joset <arj-odoo@users.noreply.github.com>

--- addons/calendar/models/calendar_event.py
+++ addons/calendar/models/calendar_event.py
@@ -87,6 +87,12 @@ class Meeting(models.Model):
         return partners
 
     @api.model
+    def _default_attendee_ids(self):
+        """ Automatically add the current partner when creating an event if there is none
+        (happens when we quickcreate an event)"""
+        return self._attendees_values([(4, self.env.user.partner_id.id)])
+
+    @api.model
     def _default_videocall_location(self):
         if self.env.context.get('calendar_no_videocall'):
             return False
@@ -159,7 +165,7 @@ class Meeting(models.Model):
     message_ids = fields.One2many(auto_join=False)
     # attendees
     attendee_ids = fields.One2many(
-        'calendar.attendee', 'event_id', 'Participant')
+        'calendar.attendee', 'event_id', 'Participant', default=_default_attendee_ids)
     attendee_status = fields.Selection(
         Attendee.STATE_SELECTION, string='Attendee Status', compute='_compute_attendee')
     partner_ids = fields.Many2many(
@@ -385,10 +391,8 @@ class Meeting(models.Model):
                     activity_vals['user_id'] = user_id
                 values['activity_ids'] = [(0, 0, activity_vals)]
 
-        # Automatically add the current partner when creating an event if there is none (happens when we quickcreate an event)
-        self_partner_id = [(4, self.env.user.partner_id.id)]
         vals_list = [
-            dict(vals, attendee_ids=self._attendees_values(vals.get('partner_ids', self_partner_id)))
+            dict(vals, attendee_ids=self._attendees_values(vals['partner_ids'])) if 'partner_ids' in vals else vals
             for vals in vals_list
         ]
         recurrence_fields = self._get_recurrent_fields()
