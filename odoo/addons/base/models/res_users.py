8024a6e369b79abb1cbd52840fd66eaec66c07a3
Raphael Collet <rco@openerp.com>
2021-08-05 15:26:53 +0200

    [IMP] base: don't try authenticating inactive users

--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -669,6 +669,8 @@ class Users(models.Model):
         try:
             self = api.Environment(cr, uid, {})[cls._name]
             with self._assert_can_auth():
+                if not self.env.user.active:
+                    raise AccessDenied()
                 self._check_credentials(passwd)
                 cls.__uid_cache[db][uid] = passwd
         finally:
