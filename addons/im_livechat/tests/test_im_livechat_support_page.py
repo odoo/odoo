2d3dbe92176b5ac78c271439ee85d2976294275a
Samuel Degueldre <sad@odoo.com>
2021-08-06 07:12:42 +0000

    [IMP] tests: allow browser_js ready code to be a promise and await it
    closes odoo/odoo#74789
    
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

--- addons/im_livechat/tests/test_im_livechat_support_page.py
+++ addons/im_livechat/tests/test_im_livechat_support_page.py
@@ -8,21 +8,6 @@ from odoo.tests import HttpCase
 class TestImLivechatSupportPage(HttpCase):
     def test_load_modules(self):
         """Checks that all javascript modules load correctly on the livechat support page"""
-        test_ready = """
-            if (!window.testSetupHasRun) {
-                window.testSetupHasRun = true;
-                const setReady = () => odoo.__DEBUG__.didLogInfo.then(() => {
-                    window.testIsReady = true;
-                });
-                if (document.readyState === "complete") {
-                    setReady();
-                } else {
-                    window.addEventListener("load", setReady);
-                }
-            }
-            window.testIsReady;
-        """
-
         check_js_modules = """
             const { missing, failed, unloaded } = odoo.__DEBUG__.jsModules;
             if ([missing, failed, unloaded].some(arr => arr.length)) {
@@ -31,4 +16,4 @@ class TestImLivechatSupportPage(HttpCase):
                 console.log("test successful");
             }
         """
-        self.browser_js("/im_livechat/support/1", code=check_js_modules, ready=test_ready)
+        self.browser_js("/im_livechat/support/1", code=check_js_modules, ready="odoo.__DEBUG__.didLogInfo")
