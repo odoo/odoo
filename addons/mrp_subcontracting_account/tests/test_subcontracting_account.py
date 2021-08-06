6aa8b7dd67e5db48b869279d768e81774dd90dc7
Rémy Voet (ryv) <ryv@odoo.com>
2021-08-06 11:33:06 +0000

    [IMP] mrp_subcontracting: improve mrp bom report
    - The "BoM Structure & Cost" takes now in account
    the subcontracting cost.
    The subcontracting cost depends of the best supplier info in product
    and subcontractor register in the BoM.
    - Also add this subcontracting cost (to the cost field) when we click on
    "Compute Price from BoM" in the product view.
    
    task-2486811
    
    Part-of: odoo/odoo#75041

--- addons/mrp_subcontracting_account/tests/test_subcontracting_account.py
+++ addons/mrp_subcontracting_account/tests/test_subcontracting_account.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.addons.mrp_subcontracting.tests.common import TestMrpSubcontractingCommon
+from odoo import Command
 from odoo.tests.common import Form
+from odoo.tools.float_utils import float_round, float_compare
+
+from odoo.addons.mrp_subcontracting.tests.common import TestMrpSubcontractingCommon
+from odoo.addons.mrp_account.tests.test_bom_price import TestBomPrice
 
 class TestAccountSubcontractingFlows(TestMrpSubcontractingCommon):
     def test_subcontracting_account_flow_1(self):
@@ -132,3 +136,58 @@ class TestAccountSubcontractingFlows(TestMrpSubcontractingCommon):
         self.assertEqual(len(f_layers), 4)
         for layer in f_layers:
             self.assertEqual(layer.value, 100 + 50)
+
+class TestBomPriceSubcontracting(TestBomPrice):
+
+    def test_01_compute_price_subcontracting_cost(self):
+        """Test calculation of bom cost with subcontracting."""
+        self.table_head.uom_po_id = self.dozen
+        partner = self.env['res.partner'].create({
+            'name': 'A name can be a Many2one...'
+        })
+        (self.bom_1 | self.bom_2).write({
+            'type': 'subcontract',
+            'subcontractor_ids': [Command.link(partner.id)]
+        })
+        suppliers = self.env['product.supplierinfo'].create([
+            {
+                'name': partner.id,
+                'product_tmpl_id': self.dining_table.product_tmpl_id.id,
+                'price': 150.0,
+            }, {
+                'name': partner.id,
+                'product_tmpl_id': self.table_head.product_tmpl_id.id,
+                'price': 120.0,  # 10 by Unit because uom_po_id is in dozen
+            }
+        ])
+        self.assertEqual(suppliers.mapped('is_subcontractor'), [True, True])
+
+        # -----------------------------------------------------------------
+        # Cost of BoM (Dining Table 1 Unit)
+        # -----------------------------------------------------------------
+        # Component Cost =  Table Head     1 Unit * 300 = 300 (478.75 from it's components)
+        #                   Screw          5 Unit *  10 =  50
+        #                   Leg            4 Unit *  25 = 100
+        #                   Glass          1 Unit * 100 = 100
+        #                   Subcontracting 1 Unit * 150 = 150
+        # Total = 700 [878.75 if components of Table Head considered] (for 1 Unit)
+        # -----------------------------------------------------------------
+        self.assertEqual(self.dining_table.standard_price, 1000, "Initial price of the Product should be 1000")
+        self.dining_table.button_bom_cost()
+        self.assertEqual(float_round(self.dining_table.standard_price, precision_digits=2), 700.0, "After computing price from BoM price should be 700")
+
+        # Cost of BoM (Table Head 1 Dozen)
+        # -----------------------------------------------------------------
+        # Component Cost =  Plywood Sheet   12 Unit * 200 = 2400
+        #                   Bolt            60 Unit *  10 =  600
+        #                   Colour          12 Unit * 100 = 1200
+        #                   Corner Slide    57 Unit * 25  = 1425
+        #                   Subcontracting  1 Dozen * 120 =  120
+        #                                           Total = 5745
+        #                          1 Unit price (5745/12) =  478.75
+        # -----------------------------------------------------------------
+
+        self.assertEqual(self.table_head.standard_price, 300, "Initial price of the Product should be 300")
+        self.Product.browse([self.dining_table.id, self.table_head.id]).action_bom_cost()
+        self.assertEqual(float_compare(self.table_head.standard_price, 478.75, precision_digits=2), 0, "After computing price from BoM price should be 878.75")
+        self.assertEqual(float_compare(self.dining_table.standard_price, 878.75, precision_digits=2), 0, "After computing price from BoM price should be 878.75")
