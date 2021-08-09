bee8c61df2559df81bab91e0478a0e111c7b1a54
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-08-09 11:41:46 -0300

    [IMP] third checks: display check name

--- addons/l10n_ar_third_check/models/account_journal.py
+++ addons/l10n_ar_third_check/models/account_journal.py
@@ -12,5 +12,4 @@ class AccountJournal(models.Model):
     def _default_inbound_payment_methods(self):
         if self._context.get('third_checks_journal'):
             return self.env.ref('l10n_ar_third_check.account_payment_method_new_third_checks') + self.env.ref('l10n_ar_third_check.account_payment_method_in_third_checks')
-
         return super()._default_inbound_payment_methods()
