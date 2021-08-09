c840ef4c73a4ce7b1860b1cd6c9d79addd0ece4a
Younn Olivier <yol@odoo.com>
2021-08-09 11:56:21 +0000

    [IMP] website, *: use widgets to display mega menu templates
    *: web_editor
    
    The masonry templates selection SnippetOption has been abstracted so
    that mega menus content can also be replaced with template content.
    
    Part of https://github.com/odoo/odoo/pull/71143
    task-2502208
    
    Part-of: odoo/odoo#71143

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -6605,6 +6605,111 @@ registry.MultipleItems = SnippetOptionWidget.extend({
     _removeItemCallback($target) {},
 });
 
+registry.SelectTemplate = SnippetOptionWidget.extend({
+    custom_events: Object.assign({}, SnippetOptionWidget.prototype.custom_events, {
+        'user_value_widget_opening': '_onWidgetOpening',
+    }),
+
+    /**
+     * @constructor
+     */
+    init() {
+        this._super(...arguments);
+        this.containerSelector = '';
+        this.selectTemplateWidgetName = '';
+    },
+    /**
+     * @constructor
+     */
+    async start() {
+        this.containerEl = this.containerSelector ? this.$target.find(this.containerSelector)[0] : this.$target[0];
+        this._templates = {};
+        return this._super(...arguments);
+    },
+
+    //--------------------------------------------------------------------------
+    // Options
+    //--------------------------------------------------------------------------
+
+    /**
+     * Changes the masonry snippet layout.
+     *
+     * @see this.selectClass for parameters
+     */
+    async selectTemplate(previewMode, widgetValue, params) {
+        await this._templatesLoading;
+
+        if (previewMode === 'reset') {
+            if (!this.beforePreviewNodes) {
+                // FIXME should not be necessary: only needed because we have a
+                // strange 'reset' sent after a non-preview
+                return;
+            }
+
+            // Empty the container and restore the original content
+            while (this.containerEl.lastChild) {
+                this.containerEl.removeChild(this.containerEl.lastChild);
+            }
+            for (const node of this.beforePreviewNodes) {
+                this.containerEl.appendChild(node);
+            }
+            this.beforePreviewNodes = null;
+            return;
+        }
+
+        if (!this.beforePreviewNodes) {
+            // We are about the apply a template on non-previewed content,
+            // save that content's nodes.
+            this.beforePreviewNodes = [...this.containerEl.childNodes];
+        }
+        // Empty the container and add the template content
+        while (this.containerEl.lastChild) {
+            this.containerEl.removeChild(this.containerEl.lastChild);
+        }
+        this.containerEl.insertAdjacentHTML('beforeend', this._templates[widgetValue]);
+
+        if (!previewMode) {
+            // The original content to keep saved has to be retrieved just
+            // before the preview (if we save it now, we might miss other items
+            // added by other options or custo).
+            this.beforePreviewNodes = null;
+        }
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {OdooEvent} ev
+     */
+    _onWidgetOpening(ev) {
+        if (this._templatesLoading || ev.target.getName() !== this.selectTemplateWidgetName) {
+            return;
+        }
+        const templateParams = ev.target.getMethodsParams('selectTemplate');
+        const proms = templateParams.possibleValues.map(async xmlid => {
+            if (!xmlid) {
+                return;
+            }
+            // TODO should be better and retrieve all rendering in one RPC (but
+            // those ~10 RPC are only done once per edit mode if the option is
+            // opened, so I guess this is acceptable).
+            this._templates[xmlid] = await this._rpc({
+                model: 'ir.ui.view',
+                method: 'render_public_asset',
+                args: [`${xmlid}`, {}],
+                kwargs: {
+                    context: this.options.context,
+                },
+            });
+        });
+        this._templatesLoading = Promise.all(proms);
+    },
+});
+
+
 return {
     SnippetOptionWidget: SnippetOptionWidget,
     snippetOptionRegistry: registry,
