2324bbdba36e100fe47df3ba4a5d87fbb4c867af
Sébastien Theys <seb@odoo.com>
2021-08-11 16:21:20 +0000

    [IMP] mail, *: batch channels at init messaging
    * = im_livechat, test_discuss_full
    
    Less code, less queries.
    
    Part of task-2622462

--- addons/im_livechat/models/res_partner.py
+++ addons/im_livechat/models/res_partner.py
@@ -35,13 +35,12 @@ class Partners(models.Model):
         return result
 
     def _get_channels_as_member(self):
-        values = super()._get_channels_as_member()
-        livechat_channels = self.env['mail.channel'].search([
+        channels = super()._get_channels_as_member()
+        channels |= self.env['mail.channel'].search([
             ('channel_type', '=', 'livechat'),
             ('channel_last_seen_partner_ids', 'in', self.env['mail.channel.partner'].sudo()._search([
                 ('partner_id', '=', self.id),
                 ('is_pinned', '=', True),
             ])),
         ])
-        values['channel_livechat'] = livechat_channels.channel_info()
-        return values
+        return channels
--- addons/im_livechat/static/src/models/messaging_initializer/messaging_initializer.js
+++ addons/im_livechat/static/src/models/messaging_initializer/messaging_initializer.js
@@ -1,33 +1,9 @@
 /** @odoo-module **/
 
 import { registerInstancePatchModel } from '@mail/model/model_core';
-import { executeGracefully } from '@mail/utils/utils';
 import { insert } from '@mail/model/model_field_command';
 
 registerInstancePatchModel('mail.messaging_initializer', 'im_livechat/static/src/models/messaging_initializer/messaging_initializer.js', {
-
-    //----------------------------------------------------------------------
-    // Private
-    //----------------------------------------------------------------------
-
-    /**
-     * @override
-     * @param {Object[]} [param0.channel_livechat=[]]
-     */
-    async _initChannels(initMessagingData) {
-        await this.async(() => this._super(initMessagingData));
-        const { channel_livechat = [] } = initMessagingData;
-        return executeGracefully(channel_livechat.map(data => () => {
-            const channel = this.env.models['mail.thread'].insert(
-                this.env.models['mail.thread'].convertData(data),
-            );
-            // flux specific: channels received at init have to be
-            // considered pinned. task-2284357
-            if (!channel.isPinned) {
-                channel.pin();
-            }
-        }));
-    },
     /**
      * @override
      */
