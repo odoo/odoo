e7450bee8f018a5334d9628235824f2fc6f32cec
Aurélien (avd) <avd@odoo.com>
2021-08-12 11:59:59 +0000

    [FIX] stock: fix stock_picking compute state
    Fix error when retrieving _get_relevant_state_among_moves()
    for each picking in stock_picking._compute_state.
    
    closes odoo/odoo#75019
    
    Signed-off-by: Rémy Voet <ryv-odoo@users.noreply.github.com>

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -439,7 +439,7 @@ class Picking(models.Model):
             elif picking_moves_state_map[picking.id]['all_cancel_done']:
                 picking.state = 'done'
             else:
-                relevant_move_state = self.env['stock.move'].browse(picking_move_lines[picking_id.id])._get_relevant_state_among_moves()
+                relevant_move_state = self.env['stock.move'].browse(picking_move_lines[picking.id])._get_relevant_state_among_moves()
                 if picking.immediate_transfer and relevant_move_state not in ('draft', 'cancel', 'done'):
                     picking.state = 'assigned'
                 elif relevant_move_state == 'partially_available':
