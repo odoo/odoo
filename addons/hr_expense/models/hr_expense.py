13cfef755a2448035532875f727fcbf561bf96f7
Touati Djamel (otd) <otd@odoo.com>
2021-08-13 16:08:05 +0200

    [FIX] hr_expense: fix the approval of multiple expense sheet
    Steps to reproduce the bug:
    - Go to Expenses > Expense Reports to approve
    - Select expense reports with different managers
    - Click on the Action drop-down button > Approve report
    
    Problem:
    Traceback is triggered because we try to access `"self.user_id"` (manager) of expenses as a single element,
    While we have several `”user_id”`
    
    Solution:
    - Filter the `"hr.expense.sheet"` which must be approved
    - Approve each expense separately
    
    opw-2622837

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -944,8 +944,7 @@ class HrExpenseSheet(models.Model):
 
             if not self.env.user in current_managers and not self.user_has_groups('hr_expense.group_hr_expense_user') and self.employee_id.expense_manager_id != self.env.user:
                 raise UserError(_("You can only approve your department expenses"))
-
-        responsible_id = self.user_id.id or self.env.user.id    
+        
         notification = {
             'type': 'ir.actions.client',
             'tag': 'display_notification',
@@ -955,14 +954,17 @@ class HrExpenseSheet(models.Model):
                 'sticky': False,  #True/False will display for few seconds if false
             },
         }
-        sheet_to_approve = self.filtered(lambda s: s.state in ['submit', 'draft'])
-        if sheet_to_approve:
-            notification['params'].update({
-                'title': _('The expense reports were successfully approved.'),
-                'type': 'success',
-                'next': {'type': 'ir.actions.act_window_close'},
-            })
-            sheet_to_approve.write({'state': 'approve', 'user_id': responsible_id})
+        filtered_sheet = self.filtered(lambda s: s.state in ['submit', 'draft'])
+        if not filtered_sheet:
+            return notification
+        for sheet in filtered_sheet:
+            sheet.write({'state': 'approve', 'user_id': sheet.user_id.id or self.env.user.id})
+        notification['params'].update({
+            'title': _('The expense reports were successfully approved.'),
+            'type': 'success',
+            'next': {'type': 'ir.actions.act_window_close'},
+        })
+            
         self.activity_update()
         return notification
 
