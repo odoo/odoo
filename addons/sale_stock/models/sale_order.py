c9cefd8b4e11a636aa3bd3c4a7dd43048db5a853
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2021-08-16 15:46:38 +0000

    [FIX] sale_stock: fix effective picking date
    - Create a sale order that generates picking and confirm it.
    - Validate the picking  when the local DATE and the GMT DATE
    are different (i.e. GMT-4, between local 20:00:00 and 23:59:59)
    
    Effective Date will be different in the SO and the picking
    
    opw-2559308
    
    closes odoo/odoo#75357
    
    X-original-commit: 0411dda6f2d1216b6c8b727523e7557cfbad7e91
    Signed-off-by: agr-odoo <agr-odoo@users.noreply.github.com>

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -41,7 +41,7 @@ class SaleOrder(models.Model):
         for order in self:
             pickings = order.picking_ids.filtered(lambda x: x.state == 'done' and x.location_dest_id.usage == 'customer')
             dates_list = [date for date in pickings.mapped('date_done') if date]
-            order.effective_date = dates_list and min(dates_list).date()
+            order.effective_date = dates_list and fields.Date.context_today(order, min(dates_list))
 
     @api.depends('picking_policy')
     def _compute_expected_date(self):
