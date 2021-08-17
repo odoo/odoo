a47d7039869308db81c49059569e93a2f06f1fad
Rémy Voet (ryv) <ryv@odoo.com>
2021-08-17 09:22:39 +0000

    [FIX] mrp_subcontracting: avoid byproduct
    We cannot have byproduct in case of subcontracting,
    raise a Error if we try (like operation). Also remove the
    it from the subcontracted form (also clean empty space of it).
    
    task-2486811
    
    Part-of: odoo/odoo#75041

--- addons/mrp_subcontracting/models/mrp_bom.py
+++ addons/mrp_subcontracting/models/mrp_bom.py
@@ -21,7 +21,7 @@ class MrpBom(models.Model):
         else:
             return self.env['mrp.bom']
 
-    @api.constrains('operation_ids', 'type')
+    @api.constrains('operation_ids', 'byproduct_ids', 'type')
     def _check_subcontracting_no_operation(self):
-        if self.filtered_domain([('type', '=', 'subcontract'), ('operation_ids', '!=', False)]):
-            raise ValidationError(_('You can not set a Bill of Material with operations as subcontracting.'))
+        if self.filtered_domain([('type', '=', 'subcontract'), '|', ('operation_ids', '!=', False), ('byproduct_ids', '!=', False)]):
+            raise ValidationError(_('You can not set a Bill of Material with operations or by-product line as subcontracting.'))
