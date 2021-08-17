da98426bbe93491bbc082da404c78293958ac632
Lucas Perais (lpe) <lpe@odoo.com>
2021-08-17 14:03:58 +0000

    [REF] web: introduce useViewArch hook
    Part-of: odoo-dev/odoo#1018

--- addons/web/static/src/views/helpers/view_hook.js
+++ addons/web/static/src/views/helpers/view_hook.js
@@ -2,6 +2,7 @@
 
 import { useDebugCategory } from "@web/core/debug/debug_context";
 import { useSetupAction } from "@web/webclient/actions/action_hook";
+import { registry } from "@web/core/registry";
 
 const { useComponent } = owl.hooks;
 
@@ -9,4 +10,29 @@ export function useSetupView(params) {
     const component = useComponent();
     useDebugCategory("view", { component });
     useSetupAction(params);
+}
+
+export function useViewArch(arch, params = {}) {
+    const CATEGORY = "__processed_archs__";
+
+    arch = arch.trim();
+    const processedRegistry = registry.category(CATEGORY);
+
+    let processedArch;
+    if (!processedRegistry.contains(arch)) {
+        processedArch = {};
+        processedRegistry.add(arch, processedArch);
+    } else {
+        processedArch = processedRegistry.get(arch);
+    }
+
+    const { compile, extract } = params;
+    if (!("template" in processedArch) && compile) {
+        processedArch.template = owl.tags.xml`${compile(arch)}`;
+    }
+    if (!("extracted" in processedArch) && extract) {
+        processedArch.extracted = extract(arch);
+    }
+
+    return processedArch;
 }
