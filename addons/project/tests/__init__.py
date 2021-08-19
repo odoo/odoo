e9268f307879231a1ae9d17e7e1d717e0661ab35
Xavier BOL (xbo) <xbo@odoo.com>
2021-08-19 14:36:35 +0200

    [IMP] project: add unit test for project sharing feature
    task-2379518
    
    closes #73341

--- addons/project/tests/__init__.py
+++ addons/project/tests/__init__.py
@@ -6,6 +6,7 @@ from . import test_project_base
 from . import test_project_config
 from . import test_project_flow
 from . import test_project_recurrence
+from . import test_project_sharing
 from . import test_project_subtasks
 from . import test_project_ui
 from . import test_project_update_access_rights
