010727183d70e5ed35eb69f6de57aa4ec52dcff8
Florent de Labarre <florent.mirieu@gmail.com>
2021-08-23 10:07:16 +0000

    [FIX] mail: markallasread doesn't care of domain
    - Go to discuss
    - add a search in searchbar
    - click on Mark All Read
    --> Issue all messages are mark all read. The domain is not care
    
    X-original-commit: 1f8a0e7807f96ee857ebe25c040396ed3b6a906a

--- addons/mail/static/src/widgets/discuss/discuss.js
+++ addons/mail/static/src/widgets/discuss/discuss.js
@@ -128,4 +128,221 @@ export const DiscussWidget = AbstractAction.extend({
             type: 'rainbow_man',
         });
     },
+<<<<<<< HEAD
+=======
+    /**
+     * @private
+     */
+    _updateControlPanel() {
+        // Invite
+        if (this._shouldHaveInviteButton()) {
+            this.$buttons.find('.o_invite').removeClass('o_hidden');
+        } else {
+            this.$buttons.find('.o_invite').addClass('o_hidden');
+        }
+        // Mark All Read
+        if (
+            this.discuss.threadView &&
+            this.discuss.thread &&
+            this.discuss.thread === this.env.messaging.inbox
+        ) {
+            this.$buttons
+                .find('.o_widget_Discuss_controlPanelButtonMarkAllRead')
+                .removeClass('o_hidden')
+                .prop('disabled', this.discuss.threadView.messages.length === 0);
+        } else {
+            this.$buttons
+                .find('.o_widget_Discuss_controlPanelButtonMarkAllRead')
+                .addClass('o_hidden');
+        }
+        // Unstar All
+        if (
+            this.discuss.threadView &&
+            this.discuss.thread &&
+            this.discuss.thread === this.env.messaging.starred
+        ) {
+            this.$buttons
+                .find('.o_unstar_all')
+                .removeClass('o_hidden')
+                .prop('disabled', this.discuss.threadView.messages.length === 0);
+        } else {
+            this.$buttons
+                .find('.o_unstar_all')
+                .addClass('o_hidden');
+        }
+        // Mobile: Add channel
+        if (
+            this.env.messaging.device.isMobile &&
+            this.discuss.activeMobileNavbarTabId === 'channel'
+        ) {
+            this.$buttons
+                .find('.o_mobile_new_channel')
+                .removeClass('o_hidden');
+        } else {
+            this.$buttons
+                .find('.o_mobile_new_channel')
+                .addClass('o_hidden');
+        }
+        // Mobile: Add message
+        if (
+            this.env.messaging.device.isMobile &&
+            this.discuss.activeMobileNavbarTabId === 'chat'
+        ) {
+            this.$buttons
+                .find('.o_mobile_new_message')
+                .removeClass('o_hidden');
+        } else {
+            this.$buttons
+                .find('.o_mobile_new_message')
+                .addClass('o_hidden');
+        }
+        // Select All & Unselect All
+        const $selectAll = this.$buttons.find('.o_widget_Discuss_controlPanelButtonSelectAll');
+        const $unselectAll = this.$buttons.find('.o_widget_Discuss_controlPanelButtonUnselectAll');
+
+        if (
+            this.discuss.threadView &&
+            (
+                this.discuss.threadView.checkedMessages.length > 0 ||
+                this.discuss.threadView.uncheckedMessages.length > 0
+            )
+        ) {
+            $selectAll.removeClass('o_hidden');
+            $selectAll.toggleClass('disabled', this.discuss.threadView.uncheckedMessages.length === 0);
+            $unselectAll.removeClass('o_hidden');
+            $unselectAll.toggleClass('disabled', this.discuss.threadView.checkedMessages.length === 0);
+        } else {
+            $selectAll.addClass('o_hidden');
+            $selectAll.addClass('disabled');
+            $unselectAll.addClass('o_hidden');
+            $unselectAll.addClass('disabled');
+        }
+
+        // Moderation Actions
+        const $moderationButtons = this.$buttons.find('.o_widget_Discuss_controlPanelButtonModeration');
+        if (
+            this.discuss.threadView &&
+            this.discuss.threadView.checkedMessages.length > 0 &&
+            this.discuss.threadView.checkedMessages.filter(
+                message => !message.isModeratedByCurrentPartner
+            ).length === 0
+        ) {
+            $moderationButtons.removeClass('o_hidden');
+        } else {
+            $moderationButtons.addClass('o_hidden');
+        }
+
+        let title;
+        if (this.env.messaging.device.isMobile || !this.discuss.thread) {
+            title = this.env._t("Discuss");
+        } else {
+            const prefix =
+                this.discuss.thread.channel_type === 'channel' &&
+                this.discuss.thread.public !== 'private'
+                ? '#'
+                : '';
+            title = `${prefix}${this.discuss.thread.displayName}`;
+        }
+
+        this.updateControlPanel({
+            cp_content: {
+                $buttons: this.$buttons,
+            },
+            title,
+        });
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     */
+    _onClickInvite() {
+        new InvitePartnerDialog(this, {
+            activeThreadLocalId: this.discuss.thread.localId,
+            messagingEnv: this.env,
+        }).open();
+    },
+    /**
+     * @private
+     */
+    _onClickMarkAllAsRead() {
+        const domain = this.discuss.stringifiedDomain ? JSON.parse(this.discuss.stringifiedDomain) : undefined;
+        this.env.models['mail.message'].markAllAsRead(domain);
+    },
+    /**
+     * @private
+     */
+    _onClickMobileNewChannel() {
+        this.discuss.update({ isAddingChannel: true });
+    },
+    /**
+     * @private
+     */
+    _onClickMobileNewMessage() {
+        this.discuss.update({ isAddingChat: true });
+    },
+    /**
+     * @private
+     */
+    _onClickModerationAccept() {
+        this.env.models['mail.message'].moderate(
+            this.discuss.threadView.checkedMessages,
+            'accept'
+        );
+    },
+    /**
+     * @private
+     */
+    _onClickModerationDiscard() {
+        this.discuss.update({ hasModerationDiscardDialog: true });
+    },
+    /**
+     * @private
+     */
+    _onClickModerationReject() {
+        this.discuss.update({ hasModerationRejectDialog: true });
+    },
+    /**
+     * @private
+     */
+    _onClickSelectAll() {
+        this.env.models['mail.message'].checkAll(
+            this.discuss.thread,
+            this.discuss.stringifiedDomain
+        );
+    },
+    /**
+     * @private
+     */
+    _onClickUnselectAll() {
+        this.env.models['mail.message'].uncheckAll(
+            this.discuss.thread,
+            this.discuss.stringifiedDomain
+        );
+    },
+    /**
+     * @private
+     */
+    _onClickUnstarAll() {
+        this.env.models['mail.message'].unstarAll();
+    },
+    /**
+     * @private
+     * @param {Object} searchQuery
+     */
+    _onSearch: function (searchQuery) {
+        this.discuss.update({
+            stringifiedDomain: JSON.stringify(searchQuery.domain),
+        });
+    },
+});
+
+action_registry.add('mail.widgets.discuss', DiscussWidget);
+
+return DiscussWidget;
+
+>>>>>>> d27b99f0000... temp
 });
