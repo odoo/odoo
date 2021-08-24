686eac3f57a625c35bc05dc797b437cb658360f1
Touati Djamel (otd) <otd@odoo.com>
2021-08-24 16:24:39 +0200

    [ADD]l10n_de_reports_datev, account: change incorrect field in German tax report
    Steps to reproduce the bug:
    - Install German accounting
    - Create a German company and select it in the selector
    - Go to accounting > Reporting > Tax report
    - Click on "Export (XML)”
    
    Problem:
    The field `company_registery` is used in` Steuernummer` instead of `l10n_de_stnr`.
    The `l10n_de_stnr` field is defined in the` l10n_de_pos_cert` module,
    so the user will have to install the point of sale module if we use this field.
    
    Solution:
    It seems more correct to define this field in German accounting and use it in order to avoid unnecessary module installation to customers.
    
    Partial-backport of : https://github.com/odoo/odoo/commit/d2bd6d69d647777fffe51c22cbb324e6d27a950f
    
    Partial-backport of: https://github.com/odoo/odoo/pull/72590/commits/8cc7452ec5b229e4be08a6af74da5098e07d16bc
    
    opw-2560974

---
+++ addons/l10n_de_reports_datev/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Germany - Accounting Reports DATEV',
+    'category': 'Localization',
+    'description': """
+        Accounting reports DATEV for Germany
+    """,
+    'depends': [
+        'l10n_de'
+    ],
+    'data': [
+        'views/res_company_views.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
