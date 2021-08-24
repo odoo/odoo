3f02ef04d9f61861957cc1579356ba61163ab6cd
Nicolas Lempereur <nle@odoo.com>
2021-08-24 14:31:42 +0000

    [FIX] account_fleet: export multiple fleet_vehicle moves
    The compute field for fleet_vehicle().account_move_ids can be using
    self.id where self might be a recordset with more than one record.
    
    opw-2630226
    
    closes odoo/odoo#75541
    
    X-original-commit: c5e14bee56d2f36499088e217e86df3be0fb0fb0
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- addons/account_fleet/models/fleet_vehicle.py
+++ addons/account_fleet/models/fleet_vehicle.py
@@ -17,7 +17,7 @@ class FleetVehicle(models.Model):
             return
 
         for vehicle in self:
-            vehicle.account_move_ids = self.env['account.move.line'].search([('vehicle_id', '=', self.id), ('move_id.state', '!=', 'cancel')]).move_id
+            vehicle.account_move_ids = self.env['account.move.line'].search([('vehicle_id', '=', vehicle.id), ('move_id.state', '!=', 'cancel')]).move_id
             vehicle.bill_count = len(vehicle.account_move_ids)
 
     def action_view_bills(self):
