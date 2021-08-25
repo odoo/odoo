b70b431e2cc9cef0a7ed86c25c2cc9b419f3d97f
Jigar Vaghela <jva@odoo.com>
2021-08-25 13:05:53 +0530

    [IMP] l10n_in*: improvement in Indian localizations
    Remove l10n_in_gstin from the account.invoice because as per rule input tax credit goes to Bill to party in any case
    Add tax,group and report line of Non-GST
    Add STATE CESS report line to differentiate state cess
    
    Add Other Country state for specify place of supply
    we decided to create multi-company in case of multi GSTIN so we hide Multi GSTIN feature
    It's complicated to create a company base on this and move record so we hide it instead of remove.
    
    We just need a place of supply in the Invoice because there are many complex cases so we can't get it automatically in all cases like
    
    (a) where the supply involves movement of goods, whether by the supplier or
    the recipient or by any other person, the place of supply of such goods shall be the
    location of the goods at the time at which the movement of goods terminates for
    delivery to the recipient;
    
    (b) where the goods are delivered by the supplier to a recipient or any other
    person on the direction of a third person, whether acting as an agent or otherwise,
    before or during movement of goods, either by way of transfer of documents of title
    to the goods or otherwise, it shall be deemed that the said third person has received
    the goods and the place of supply of such goods shall be the principal place of business
    of such person;
    
    (c) where the supply does not involve movement of goods, whether by the
    supplier or the recipient, the place of supply shall be the location of such goods at the
    time of the delivery to the recipient;
    
    (d) where the goods are assembled or installed at site, the place of supply shall
    be the place of such installation or assembly;
    
    (e) where the goods are supplied on board a conveyance, including a vessel, an
    aircraft, a train or a motor vehicle, the place of supply shall be the location at which
    such goods are taken on board.
    
    for more details check CHAPTER V here https://www.cbic.gov.in/resources//htdocs-cbec/gst/igst-act.pdf;jsessionid=63D695F612B9972934E8131415FEBA28

--- addons/l10n_in/models/account_invoice.py
+++ addons/l10n_in/models/account_invoice.py
@@ -27,7 +27,7 @@ class AccountMove(models.Model):
     l10n_in_state_id = fields.Many2one(
         "res.country.state",
         string="Place of supply",
-        domain=[("country.code", "=", "IN")],
+        domain=[("country_id.code", "=", "IN")],
         compute="_compute_l10n_in_state_id",
         store=True,
     )
@@ -38,37 +38,27 @@ class AccountMove(models.Model):
     l10n_in_shipping_port_code_id = fields.Many2one('l10n_in.port.code', 'Port code', states={'draft': [('readonly', False)]})
     l10n_in_reseller_partner_id = fields.Many2one('res.partner', 'Reseller', domain=[('vat', '!=', False)], help="Only Registered Reseller", readonly=True, states={'draft': [('readonly', False)]})
 
+    @api.depends("partner_id", "journal_id")
+    def _compute_l10n_in_state_id(self):
+        for move in self:
+            l10n_in_state_id = False
+            if move.country_code == "IN" and move.journal_id.type == "sale" and move.partner_id:
+                company_unit_partner = (
+                    move.journal_id.l10n_in_gstin_partner_id
+                    or move.journal_id.company_id.partner_id
+                )
+                l10n_in_state_id = move.partner_id.state_id.country_id.code == "IN" and move.partner_id.state_id or False
+                # still state is not set then assumed that transaction is local like PoS so set state of company
+                if not l10n_in_state_id and not move.partner_id.country_id:
+                    l10n_in_state_id = company_unit_partner.state_id
+            move.l10n_in_state_id = l10n_in_state_id
+
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
         """Use journal type to define document type because not miss state in any entry including POS entry"""
         if self.country_code == 'IN':
             self.l10n_in_gst_treatment = self.partner_id.l10n_in_gst_treatment
         return super()._onchange_partner_id()
-
-    @api.depends("partner_id", "journal_id", "l10n_in_gst_treatment")
-    def _compute_l10n_in_state_id(self):
-        for move in self:
-            l10n_in_state_id = False
-            if move.country_code == "IN" and move.journal_id.type == "sale":
-                company_unit_partner = (
-                    move.journal_id.l10n_in_gstin_partner_id
-                    or move.journal_id.company_id.partner_id
-                )
-                l10n_in_state_id = self._l10n_in_get_indian_state(move.partner_id)
-                # still state is not set then assumed that transaction is local like PoS so set state of company
-                if not l10n_in_state_id and not move.partner_id.country_id:
-                    l10n_in_state_id = company_unit_partner.state_id
-            move.l10n_in_state_id = l10n_in_state_id
-
-    @api.model
-    def _l10n_in_get_indian_state(self, partner):
-        """In tax return filing, If customer is not Indian in that case place of supply is must set to Other Territory.
-        So we set Other Territory in l10n_in_state_id when customer(partner) is not Indian
-        State is big role under GST because tax type is depend on.for more information check this https://www.cbic.gov.in/resources//htdocs-cbec/gst/Integrated%20goods%20&%20Services.pdf"""
-        indian_state = False
-        if partner.state_id and partner.state_id.country_id.code == "IN":
-            indian_state = partner.state_id
-        return indian_state
 
     def _post(self, soft=True):
         """Use journal type to define document type because not miss state in any entry including POS entry"""
@@ -102,11 +92,7 @@ class AccountMove(models.Model):
                         name=gst_treatment_name_mapping.get(move.l10n_in_gst_treatment),
                     )
                 )
-            if not move.l10n_in_state_id and move.l10n_in_gst_treatment and move.l10n_in_gst_treatment != 'overseas' and move.move_type in [
-                'out_invoice',
-                'out_refund',
-                'out_receipt'
-            ]:
+            if not move.l10n_in_state_id and move.l10n_in_gst_treatment and move.l10n_in_gst_treatment != 'overseas' and move.is_sale_document(include_receipts=True):
                 raise ValidationError(
                     _(
                         "Place of supply is required under GST Treatment %(name)s",
