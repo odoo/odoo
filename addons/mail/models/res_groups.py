0456dfd06fb1859322b89faf73e0ed1116b99eef
Thibault Delavallée <tde@odoo.com>
2021-08-25 12:17:51 +0000

    [FIX] mail: fix wrong override of write
    Somehow a context parameter was still given to write. This should not happen
    in new API (Odoo v8).
    
    Task-2631873
    PR odoo/odoo#75571

--- addons/mail/models/res_groups.py
+++ addons/mail/models/res_groups.py
@@ -13,11 +13,11 @@ class ResGroups(models.Model):
     _inherit = 'res.groups'
     _description = 'Access Groups'
 
-    def write(self, vals, context=None):
-        write_res = super(ResGroups, self).write(vals)
+    def write(self, vals):
+        res = super(ResGroups, self).write(vals)
         if vals.get('users'):
             # form: {'group_ids': [(3, 10), (3, 3), (4, 10), (4, 3)]} or {'group_ids': [(6, 0, [ids]}
             user_ids = [command[1] for command in vals['users'] if command[0] == 4]
             user_ids += [id for command in vals['users'] if command[0] == 6 for id in command[2]]
             self.env['mail.channel'].search([('group_ids', 'in', self._ids)])._subscribe_users_automatically()
-        return write_res
+        return res
