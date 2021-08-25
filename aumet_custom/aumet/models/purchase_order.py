26f9dd744193630d233036a677550590ff09094c
D3na <ahmadjd94@gmail.com>
2021-08-25 16:24:46 +0300

    :wrench: add custom module directory

--- aumet_custom/aumet/models/purchase_order.py
+++ aumet_custom/aumet/models/purchase_order.py
@@ -25,31 +25,33 @@ class PurchaseOrder(models.Model):
         pass
 
     def write(self, vals):
+        state_of_po = vals.get('state', False)
+        if state_of_po != "cancel":
 
-        for i in self.order_line:
-            if not self.env.user.marketplace_token:
-                raise ValidationError(f'Make sure to have your Marketplace token in your user settings')
+            for i in self.order_line:
+                if not self.env.user.marketplace_token:
+                    raise ValidationError(f'Make sure to have your Marketplace token in your user settings')
 
-            if i.product_id.is_marketplace_item:
-                item_add_line_result = cart.CartAPI.add_item_to_cart(
-                    self.env.user.marketplace_token,
-                    self.env.user.marketplace_pharmacy_id,
-                    i.product_id.marketplace_product.marketplace_id,
-                    i.product_uom_qty,
-                    i.bonus,
-                    i.payment_method.marketplace_payment_method_id
-                )
-                print("@!#!@#!@#!")
-                print(item_add_line_result.json()["message"])
-                error_code = ErrorHelper.get_status_code(item_add_line_result.json()["message"])
+                if i.product_id.is_marketplace_item:
+                    item_add_line_result = cart.CartAPI.add_item_to_cart(
+                        self.env.user.marketplace_token,
+                        self.env.user.marketplace_pharmacy_id,
+                        i.product_id.marketplace_product.marketplace_id,
+                        i.product_uom_qty,
+                        i.bonus,
+                        i.payment_method.marketplace_payment_method_id
+                    )
+                    print("@!#!@#!@#!")
+                    print(item_add_line_result.json()["message"])
+                    error_code = ErrorHelper.get_status_code(item_add_line_result.json()["message"])
 
-                if error_code == 409:
-                    valid_methods = self.get_payment_methods(i.product_id.marketplace_product.marketplace_id)
-                    raise ValidationError(
-                        f'Invalid Payment method for order line {i.product_id.name}, valid methods are {valid_methods}')
-                elif error_code == 408:
-                    raise ValidationError(
-                        f'your Marketplace cart seems to be full, please try emptying it before placing order')
+                    if error_code == 409:
+                        valid_methods = self.get_payment_methods(i.product_id.marketplace_product.marketplace_id)
+                        raise ValidationError(
+                            f'Invalid Payment method for order line {i.product_id.name}, valid methods are {valid_methods}')
+                    elif error_code == 408:
+                        raise ValidationError(
+                            f'your Marketplace cart seems to be full, please try emptying it before placing order')
 
         return super(PurchaseOrder, self).write(vals)
 
