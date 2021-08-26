4bc97e8c0f5834944894fa8a53a39c7277e93005
D3na <ahmadjd94@gmail.com>
2021-08-26 15:54:34 +0300

    :wrench: add flexi pharmayc

--- aumet_custom/product_scientific_name/models/scientific_name.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-
-class ProductScientific(models.Model):
-    _name = "product.scientific.name"
-    
-    name = fields.Char(string="Name",required=True)
