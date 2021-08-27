e981e1e7dc0dbdf14b391a14365aa7d559fa494f
Yannick Tivisse <yti@odoo.com>
2021-08-27 13:22:46 +0000

    [FIX] hr_contract: Fix multi-company issue on api.constrains
    Purpose
    =======
    
    Avoid mismatch between contracts from different companies and clarify
    error message.
    
    closes odoo/odoo#75553
    
    Related: odoo/enterprise#19647
    Related: odoo/upgrade#2777
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_contract/models/hr_contract.py
+++ addons/hr_contract/models/hr_contract.py
@@ -108,6 +108,7 @@ class Contract(models.Model):
             domain = [
                 ('id', '!=', contract.id),
                 ('employee_id', '=', contract.employee_id.id),
+                ('company_id', '=', contract.company_id.id),
                 '|',
                     ('state', 'in', ['open', 'close']),
                     '&',
@@ -124,7 +125,12 @@ class Contract(models.Model):
 
             domain = expression.AND([domain, start_domain, end_domain])
             if self.search_count(domain):
-                raise ValidationError(_('An employee can only have one contract at the same time. (Excluding Draft and Cancelled contracts)'))
+                raise ValidationError(
+                    _(
+                        'An employee can only have one contract at the same time. (Excluding Draft and Cancelled contracts).\n\nEmployee: %(employee_name)s',
+                        employee_name=contract.employee_id.name
+                    )
+                )
 
     @api.constrains('date_start', 'date_end')
     def _check_dates(self):
