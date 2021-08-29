c0e325ecf5d225e2491cfb28f0d1cc3a080f9997
ahmadjd94 <ahmadjd94@gmail.com>
2021-08-29 14:21:52 +0300

    add installation script

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,4 +1,7 @@
+sudo apt-get install libsasl2-dev python-dev libldap2-dev libssl-dev
 sudo apt-get install gcc -y
 sudo apt-get install g++ -y
-sudo apt-get install python-dev -y
+sudo apt-get install python3-dev -y
+virtualenv -p python3 venv
+source venv/bin/activate
 pip3 install -r requirements.txt
