9fe299b86dd2e747f7af905a59a7b59f5011e6f2
Christopher Ormaza <cormaza@opa-consulting.com>
2021-08-31 16:53:31 -0500

    FIX: description

--- addons/l10n_ec/models/l10n_ec_sri_payment.py
+++ addons/l10n_ec/models/l10n_ec_sri_payment.py
@@ -7,6 +7,7 @@ from odoo import fields, models
 class SriPayment(models.Model):
 
     _name = "l10n_ec.sri.payment"
+    _description = "SRI Payment Method"
 
     name = fields.Char("Name")
     code = fields.Char("Code")
