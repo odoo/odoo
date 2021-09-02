e8d0473b3b1fe2832e913339b9e6a696f4d5d77c
Cedric Prieels (cpr) <cpr@odoo.com>
2021-09-02 12:50:04 +0200

    [IMP] hr_timesheet: remove activities from project update
    This commit removes the activities from the project update default
    description.
    
    This commit prepares the removal of the project overview feature and is
    a part of the global project update description improvement.
    
    PR : #72736
    See odoo/upgrade#2706
    
    task-2545084

--- addons/hr_timesheet/__manifest__.py
+++ addons/hr_timesheet/__manifest__.py
@@ -30,7 +30,6 @@ up a management by affair.
         'views/res_config_settings_views.xml',
         'views/project_views.xml',
         'views/project_portal_templates.xml',
-        'views/project_update_templates.xml',
         'views/hr_timesheet_portal_templates.xml',
         'report/hr_timesheet_report_view.xml',
         'report/project_report_view.xml',
