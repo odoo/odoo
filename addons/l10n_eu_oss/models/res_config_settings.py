448f36f602a00ae3bb8b80e9a7fd2ae53ddb7f9a
oco-odoo <oco@odoo.com>
2021-09-02 19:25:33 +0200

    [MOV] l10n_eu_oss: new name for l10n_eu_service
    This module is now used to support OSS taxes, which replaced MOSS a few months ago. This renaming follows the changes introduced in stable by https://github.com/odoo/odoo/commit/0e7c289003a0a69075188dbfaea724919dac32a4 .
    
    [IMP] l10n_eu_oss: remove dead code from former l10n_eu_service feature
    
    [IMP] account, product, l10n_eu_oss: community adjustements to support enterprise's OSS report
    
    The following changes where introduced in order to support the features implemented in the new l10n_eu_oss_reports module:
    
    - account.tag objects applicable on taxes are no longer required to have a country_id. This way, we allow using tags in generic reports (i.e. the ones available for multiple countries) as well.
    
    - add account_tag_is on product.template and product.category: this fields are m2m to account.account.tag applicable on taxes. When making an entry from this product, these tags will be set on all the tax move lines created for it. We use it in OSS to differentiate between imported and non-imported goods in the report.
    
    - always display country_id on fiscal positions' form view: OSS taxes require to be used in some fiscal position with a country_id set, so that we can infer the country they are mapping the vat from. We make that change in the view so that users are not obliged to create auto-applied fiscal positions to fix the OSS taxes they created manually, and can directly set the country on the fiscal position.
    
    Task 2591541

--- addons/l10n_eu_oss/models/res_config_settings.py
+++ addons/l10n_eu_oss/models/res_config_settings.py
@@ -7,13 +7,13 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    l10n_eu_services_eu_country = fields.Boolean('Is European country?', compute='_compute_l10n_eu_services_european_country')
+    l10n_eu_oss_eu_country = fields.Boolean('Is European country?', compute='_compute_l10n_eu_oss_european_country')
 
     def refresh_eu_tax_mapping(self):
         self.env.companies._map_eu_taxes()
 
     @api.depends('company_id')
-    def _compute_l10n_eu_services_european_country(self):
+    def _compute_l10n_eu_oss_european_country(self):
         european_countries = self.env.ref('base.europe').country_ids
         for record in self:
-            record.l10n_eu_services_eu_country = record.company_id.account_fiscal_country_id in european_countries
+            record.l10n_eu_oss_eu_country = record.company_id.account_fiscal_country_id in european_countries
