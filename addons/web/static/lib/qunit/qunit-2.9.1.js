bd516609628f49c472d8faadba1670415bbb2856
Xavier Morel <xmo@odoo.com>
2021-09-03 12:30:53 +0000

    [IMP] web: don't check for bad dom after test on failure
    Currently, if a test fails there's very high chances the DOM will not
    be clean, because qunit doesn't have good cleanup APIs. This means a
    test failure will always be prefixed by a dump of the page DOM which
    is just confusing and makes it hard to find the actual error.
    
    This change updates the OdooAfterTestHook handler to:
    
    * Receive the `Test` itself, instead of adding more information to the
      synthetic object it currently receives.
    * Only show the leftover DOM if the test otherwise succeeded.
    * Do the entire reporting via `pushFailure`, rather than having
      duplication between an explicit `console.error` and a test failure.
    * Update the QUnit.log handler to
      - not show `undefined`
      - try to improve readability by removing the bracketing and quoting
    
    closes odoo/odoo#75948
    
    X-original-commit: 8bceac8032f48ae77e8a2c0c038f3bc26489e0ac
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- addons/web/static/lib/qunit/qunit-2.9.1.js
+++ addons/web/static/lib/qunit/qunit-2.9.1.js
@@ -2979,6 +2979,7 @@
   }
 
   Test.prototype = {
+  	get moduleName() { return this.module.name; },
   	before: function before() {
   		var _this = this;
 
@@ -3145,7 +3146,7 @@
   			this.pushFailure("Expected at least one assertion, but none were run - call " + "expect(0) to accept zero assertions.", this.stack);
   		}
 
-        emit("OdooAfterTestHook", { moduleName: this.module.name, testName: this.testName }); // Odoo customization
+        emit("OdooAfterTestHook", this); // Odoo customization
 
   		var i,
   		    module = this.module,
