6f1c5c3227ab5da650724e8d50ba38027695dd87
Thibault Delavallée <tde@odoo.com>
2021-09-06 13:51:05 +0200

    [IMP] mail, note: correctly name thread compute methods
    Just cleaning those method names and removing unnecessary definition override
    in note.
    
    Task-2638444
    PR odoo/odoo#76005

--- addons/note/models/note.py
+++ addons/note/models/note.py
@@ -52,10 +52,8 @@ class Note(models.Model):
     date_done = fields.Date('Date done')
     color = fields.Integer(string='Color Index')
     tag_ids = fields.Many2many('note.tag', 'note_tags_rel', 'note_id', 'tag_id', string='Tags')
-    message_partner_ids = fields.Many2many(
-        comodel_name='res.partner', string='Followers (Partners)',
-        compute='_get_followers', search='_search_follower_partners',
-        compute_sudo=True)
+    # modifying property of ``mail.thread`` field
+    message_partner_ids = fields.Many2many(compute_sudo=True)
 
     @api.depends('memo')
     def _compute_name(self):
