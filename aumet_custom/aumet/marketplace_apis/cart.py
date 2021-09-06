b5a5212240b8890f3f75b9c51662abdd7033b029
ahmadjd94 <ahmadjd94@gmail.com>
2021-09-06 11:15:04 +0300

    revert changes

--- aumet_custom/aumet/marketplace_apis/cart.py
+++ aumet_custom/aumet/marketplace_apis/cart.py
@@ -1,19 +1,32 @@
 import json
+import logging
+
 import requests
+
+from odoo.exceptions import ValidationError
+from odoo.tools import config
+
+
+def check_token(func):
+    def func_wrapper(cls, token=None, *args, **kwargs):
+        if token:
+            return func(cls, token, *args, *kwargs)
+
+        raise ValidationError("Please make sure your user has a marketplace assigned user from users settings")
+
+    return func_wrapper
+
+
+_logger = logging.getLogger(__name__)
 
 
 class CartAPI:
+    marketplace_host = config.get("marketplace_host")
 
     @classmethod
-    def get_all_product_details(self):
-        url = "https://dev-mpapi.aumet.tech/v1/pharmacy/products?limit=1000"
-
-        payload = {}
-        headers = {
-            'Cookie': 'PHPSESSID=vbqvsfdhpl5l3acdk54hu3ecoi'
-        }
-        token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjI2MSwidXNlckVtYWlsIjoiYS50YXllaCtwaGFybWEyQGF1bWV0LmNvbSIsImZ1bGxOYW1lIjoiQWJlZCBQaGFybWFjeSIsImV4cCI6MjI2MTE4OTI4OX0.RMkjAEgkVEDBrDlMl2aFvZwzPk79QlCy7bNy8zW_dDw"
-
+    @check_token
+    def get_all_product_details(cls, token):
+        url = f"{cls.marketplace_host}/v1/pharmacy/products?limit=1000"
         response = requests.post(url, headers={
             'Content-Type': 'application/json',
             'x-user-lang': 'en',
@@ -21,10 +34,12 @@ class CartAPI:
             'x-session-id': '123',
             'Cookie': 'PHPSESSID=adl0oj5l20ufa78t4ij2s7nl91',
             "x-access-token": token
-        }, data=payload)
+        })
+
     @classmethod
-    def get_product_details(cls, product_id, token):
-        url = f"https://dev-mpapi.aumet.tech/v1/pharmacy/products/{product_id}"
+    @check_token
+    def get_product_details(cls, token, product_id):
+        url = f"{cls.marketplace_host}/v1/pharmacy/products/{product_id}"
 
         payload = {}
 
@@ -40,14 +55,18 @@ class CartAPI:
         return response.json()
 
     @classmethod
+    @check_token
     def add_item_to_cart(cls, token, buyer_id, product_marketplace_id, bonus_amount, amount, payment_method):
-        url = "https://dev-mpapi.aumet.tech/v1/pharmacy/cart/product"
+        url = f"{cls.marketplace_host}/v1/pharmacy/cart/product"
         payload = json.dumps({
             "entityProductId": product_marketplace_id,
             "quantity": amount+bonus_amount,
             "entityId": buyer_id,
             "paymentMethodId": payment_method
         })
+
+        _logger.info("request outgoing to marketplace")
+        _logger.info(payload)
 
         response = requests.post(url, headers={
             'Content-Type': 'application/json',
@@ -61,8 +80,9 @@ class CartAPI:
         return response
 
     @classmethod
-    def get_disr_details(cls,token,dist_id):
-        url = f"https://dev-mpapi.aumet.tech/v1/distributor/{dist_id}/details"
+    @check_token
+    def get_disr_details(cls, token, dist_id):
+        url = f"{cls.marketplace_host}/v1/distributor/{dist_id}/details"
 
         payload = {}
 
