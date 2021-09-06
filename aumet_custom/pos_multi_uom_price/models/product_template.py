9371849a9fc7f7f31c1c7a28e857b00063347c67
wk96 <89963300+wk96@users.noreply.github.com>
2021-09-06 14:52:20 +0300

    Add files via upload

---
+++ aumet_custom/pos_multi_uom_price/models/product_template.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, _
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+    
+    multi_uom_price_id = fields.One2many('product.multi.uom.price', 'product_id', _("UOM price"))
