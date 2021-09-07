828d5157a52b6cbffee3cc142b6808a9f7ea939c
Cedric Prieels (cpr) <cpr@odoo.com>
2021-09-07 13:47:32 +0000

    [FIX] project{,_mrp}: fix project update stat buttons
    We are copying 3 stats buttons from the project
    view form (manufacturing orders, work orders and bills of materials)
     and we add them to the project update page.
    
    See task-2638359
    
    closes odoo/odoo#76421
    
    X-original-commit: 4572e710a5e755c0c75c95a6d033e44f0a2799f8
    Related: odoo/enterprise#20811
    Signed-off-by: LTU-Odoo <IT-Ideas@users.noreply.github.com>

--- addons/project_mrp/models/project.py
+++ addons/project_mrp/models/project.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, _
 
 
 class Project(models.Model):
@@ -13,12 +13,57 @@ class Project(models.Model):
 
     def action_view_mrp_production(self):
         self.ensure_one()
-        return self.analytic_account_id.action_view_mrp_production()
+        action = self.analytic_account_id.action_view_mrp_production()
+        if self.production_count > 1:
+            action["view_mode"] = 'tree,form,kanban,calendar,pivot,graph'
+        return action
 
     def action_view_mrp_bom(self):
         self.ensure_one()
-        return self.analytic_account_id.action_view_mrp_bom()
+        action = self.analytic_account_id.action_view_mrp_bom()
+        if self.bom_count > 1:
+            action['view_mode'] = 'tree,form,kanban'
+        return action
 
     def action_view_workorder(self):
         self.ensure_one()
-        return self.analytic_account_id.action_view_workorder()
+        action = self.analytic_account_id.action_view_workorder()
+        if self.workorder_count > 1:
+            action['view_mode'] = 'tree,form,kanban,calendar,pivot,graph,gantt'
+        return action
+
+    # ----------------------------
+    #  Project Updates
+    # ----------------------------
+
+    def _get_stat_buttons(self):
+        buttons = super(Project, self)._get_stat_buttons()
+        if self.user_has_groups('mrp.group_mrp_user'):
+            buttons.extend([{
+                'icon': 'wrench',
+                'text': _('Manufacturing Orders'),
+                'number': self.production_count,
+                'action_type': 'object',
+                'action': 'action_view_mrp_production',
+                'show': self.production_count > 0,
+                'sequence': 19,
+            },
+            {
+                'icon': 'cog',
+                'text': _('Work Orders'),
+                'number': self.workorder_count,
+                'action_type': 'object',
+                'action': 'action_view_workorder',
+                'show': self.workorder_count > 0,
+                'sequence': 20,
+            },
+            {
+                'icon': 'flask',
+                'text': _('Bills of Materials'),
+                'number': self.bom_count,
+                'action_type': 'object',
+                'action': 'action_view_mrp_bom',
+                'show': self.bom_count > 0,
+                'sequence': 21,
+            }])
+        return buttons
