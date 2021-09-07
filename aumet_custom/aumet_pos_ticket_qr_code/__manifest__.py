0e72bf19b93271548d8f596f35d0161d64711a6b
walaa <w.khaled@aumet.com>
2021-09-07 16:21:06 +0300

    add modules

---
+++ aumet_custom/aumet_pos_ticket_qr_code/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Aumet Pos Ticket QR Code ',
+    'version': '14.0.1.0.0',
+    'description': """
+        QR Code Add Receipt POS
+    """,
+    'depends': ['point_of_sale','pos_receipt_backend'],
+    'data': [
+        'views/PosConfig.xml'
+    ],
+    'demo': [
+    ],
+    'qweb': [
+        'static/src/xml/Screens/ReceiptScreen/OrderReceipt.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
