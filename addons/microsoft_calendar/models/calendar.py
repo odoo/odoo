7720ee1d9af35accabc3896c5001d32faf435a4f
Arnaud Joset <arj@odoo.com>
2021-09-07 14:03:51 +0200

    [FIX] microsoft_calendar: rename sync range ICP parameter
    Before this commit, the ICP used to get the date range of events to sync was improperly named.

--- addons/microsoft_calendar/models/calendar.py
+++ addons/microsoft_calendar/models/calendar.py
@@ -71,7 +71,7 @@ class Meeting(models.Model):
     def _get_microsoft_sync_domain(self):
         # in case of full sync, limit to a range of 1y in past and 1y in the future by default
         ICP = self.env['ir.config_parameter'].sudo()
-        day_range = int(ICP.get_param('google_calendar.sync.range_days', default=365))
+        day_range = int(ICP.get_param('microsoft_calendar.sync.range_days', default=365))
         lower_bound = fields.Datetime.subtract(fields.Datetime.now(), days=day_range)
         upper_bound = fields.Datetime.add(fields.Datetime.now(), days=day_range)
         return [
