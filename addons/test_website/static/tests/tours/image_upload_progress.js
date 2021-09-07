06f924ce7f11bcc76c97e104c271b999ccc911fe
Romain Derie <rde@odoo.com>
2021-09-07 20:20:08 +0200

    [FIX] web_editor, web_unsplash, test_website: make unsplash work
    Since cfe079221cd, unsplash is not working anymore as that commit did not adapt
    the unsplash code to that change.
    
    task-2581567
    
    X-original-commit: 9a4723628e868836c7bbe8bec31ec9b6ce3f2554

--- addons/test_website/static/tests/tours/image_upload_progress.js
+++ addons/test_website/static/tests/tours/image_upload_progress.js
@@ -3,28 +3,30 @@ odoo.define('test_website.image_upload_progress', function (require) {
 
 const tour = require('web_tour.tour');
 
+const setupSteps = [{
+    content: "enter edit mode",
+    trigger: "a[data-action=edit]"
+}, {
+    content: "drop a snippet",
+    trigger: "#oe_snippets .oe_snippet[name='Text - Image'] .oe_snippet_thumbnail:not(.o_we_already_dragging)",
+    extra_trigger: "body.editor_enable.editor_has_snippets",
+    moveTrigger: ".oe_drop_zone",
+    run: "drag_and_drop #wrap",
+}, {
+    content: "drop a snippet",
+    trigger: "#oe_snippets .oe_snippet[name='Image Gallery'] .oe_snippet_thumbnail:not(.o_we_already_dragging)",
+    extra_trigger: "body.editor_enable.editor_has_snippets",
+    moveTrigger: ".oe_drop_zone",
+    run: "drag_and_drop #wrap",
+}];
+
 const formatErrorMsg = "format is not supported. Try with: .gif, .jpe, .jpeg, .jpg, .png, .svg";
 
 tour.register('test_image_upload_progress', {
     url: '/test_image_progress',
     test: true
 }, [
-    {
-        content: "enter edit mode",
-        trigger: "a[data-action=edit]"
-    }, {
-        content: "drop a snippet",
-        trigger: "#oe_snippets .oe_snippet[name='Text - Image'] .oe_snippet_thumbnail:not(.o_we_already_dragging)",
-        extra_trigger: "body.editor_enable.editor_has_snippets",
-        moveTrigger: ".oe_drop_zone",
-        run: "drag_and_drop #wrap",
-    }, {
-        content: "drop a snippet",
-        trigger: "#oe_snippets .oe_snippet[name='Image Gallery'] .oe_snippet_thumbnail:not(.o_we_already_dragging)",
-        extra_trigger: "body.editor_enable.editor_has_snippets",
-        moveTrigger: ".oe_drop_zone",
-        run: "drag_and_drop #wrap",
-    },
+    ...setupSteps,
     // 1. Check multi image upload
     {
         content: "click on dropped snippet",
@@ -155,6 +157,43 @@ tour.register('test_image_upload_progress', {
                 console.error("There should be one noficiation toaster opened, and only one.");
             }
         }
+    },
+]);
+
+
+tour.register('test_image_upload_progress_unsplash', {
+    url: '/test_image_progress',
+    test: true
+}, [
+    ...setupSteps,
+    // 1. Check multi image upload
+    {
+        content: "click on dropped snippet",
+        trigger: "#wrap .s_image_gallery .img",
+    }, {
+        content: "click on replace media to open image dialog",
+        trigger: 'we-customizeblock-option [data-replace-media]',
+    }, {
+        content: "search 'fox' images",
+        trigger: ".o_we_search",
+        run: "text fox",
+    }, {
+        content: "click on unsplash result", // note that unsplash is mocked
+        trigger: ".o_unsplash_attachment_cell"
+    }, {
+        content: "check that the upload progress bar is correctly shown",
+        // ensure it is there so we are sure next step actually test something
+        extra_trigger: '.o_notification_close',
+        trigger: ".o_we_progressbar:contains('fox'):contains('File has been uploaded')",
+        in_modal: false,
+        run: function () {}, // it's a check
+    }, {
+        content: "notification should close after 3 seconds",
+        trigger: 'body:not(:has(.o_notification_close))',
+        in_modal: false,
+    }, {
+        content: "unsplash image (mocked to logo) should have been used",
+        trigger: "#wrap .s_image_gallery .img[data-original-src^='/unsplash/HQqIOc8oYro/fox']",
     },
 ]);
 
