242b1caa6543fcbf35591d5424a880a7a7969f83
Mathieu Duckerts-Antoine <dam@odoo.com>
2021-09-07 06:22:58 +0000

    [IMP] web,*: new GraphView component
    *hr_timesheet,project,stock
    
    Part-of: odoo/odoo#73311
    Co-authored-by: Mathieu Duckerts-Antoine <dam@odoo.com>
    Co-authored-by: Julien Mougenot <jum@odoo.com>
    Co-authored-by: Jorge Pinna Puissant <jpp@odoo.com>
    Co-authored-by: Stefano Rigano <sri@odoo.com>

--- addons/project/static/src/js/project_rating_reporting.js
+++ addons/project/static/src/js/project_rating_reporting.js
@@ -1,8 +1,6 @@
 /** @odoo-module **/
 
 import { _t } from 'web.core';
-import GraphController from 'web.GraphController';
-import GraphView from 'web.GraphView';
 import PivotController from 'web.PivotController';
 import PivotView from 'web.PivotView';
 import viewRegistry from 'web.view_registry';
@@ -34,29 +32,3 @@ const ProjectPivotView = PivotView.extend({
 });
 
 viewRegistry.add('project_rating_pivot', ProjectPivotView);
-
-const ProjectGraphController = GraphController.extend({
-    /**
-     * @override
-     */
-    init: function () {
-        this._super.apply(this, arguments);
-        _.each(this.measures, measure => {
-            if (measure.fieldName === 'res_id') {
-                measure.description = _t('Task');
-            } else if (measure.fieldName === 'parent_res_id') {
-                measure.description = _t('Project');
-            } else if (measure.fieldName === 'rating') {
-                measure.description = _t('Rating Value (/5)');
-            }
-        });
-    },
-});
-
-const ProjectGraphView = GraphView.extend({
-    config: Object.assign({}, GraphView.prototype.config, {
-        Controller: ProjectGraphController,
-    }),
-});
-
-viewRegistry.add('project_rating_graph', ProjectGraphView);
