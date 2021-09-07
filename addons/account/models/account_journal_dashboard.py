b0bd9aac00be4059c89710cfb6fde3327eecd975
Habib (ayh) <ayh@odoo.com>
2021-09-07 16:14:50 +0200

    [IMP] account: simplify invoice payment status filters
    Modify search filters on the invoice views:
    Open -> Unpaid (not_paid & partial)
    Closed -> Paid (in_payment & paid)
    Overdue -> Overdue
    
    [FIX] journal kanban view "late invoices/bills" should not include due date = today
    
    Task 2635664
    
    X-original-commit: fa15c73a087bd6b5a81aa448df2e0a8654213246

--- addons/account/models/account_journal_dashboard.py
+++ addons/account/models/account_journal_dashboard.py
@@ -368,7 +368,7 @@ class account_journal(models.Model):
                 company_id
             FROM account_move move
             WHERE journal_id = %(journal_id)s
-            AND invoice_date_due <= %(today)s
+            AND invoice_date_due < %(today)s
             AND state = 'posted'
             AND payment_state in ('not_paid', 'partial')
             AND move_type IN ('out_invoice', 'out_refund', 'in_invoice', 'in_refund', 'out_receipt', 'in_receipt');
