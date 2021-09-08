bce68c33041fae7a8dec05e12869b077493bfd07
walaa <w.khaled@aumet.com>
2021-09-08 10:22:37 +0300

    add cash control pos

---
+++ aumet_custom/aumet_pos_cash_control/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': " Aumet Pos cash control""",
+    'summary': """Pos Automatic cash control""",
+    'description': """Pos Automatic cash control""",
+    'website': "",
+    'category': 'Point of Sale',
+    'version': '14.0.0.0',
+    'depends': ['point_of_sale'],
+    'data': [
+    ],
+
+    'qweb': [
+    ],
+    "auto_install": False,
+    "installable": True,
+}
