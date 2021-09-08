df5359447ce478b036d176ac426a9d2782bb10ae
Lucas Perais (lpe) <lpe@odoo.com>
2021-09-08 15:57:25 +0000

    [FIX] web: list_editable in FieldOne2Many: onSuccess at the right time
    When changing a value for a o2m (list) within a form, the _setValue
    returned a Promise that did not take into account the real rendering of
    the list renderer.
    
    The use case at hand is:
    - enable product configurator
    - have a product with options
    - add that product in a SO
    - add the option as well through the product configurator
    - confirm the configurator
    
    The configurator triggers an add_record event, with an onSuccess callback
    
    Before this commit, the onSuccess callback was called too early, when the previous
    rendering operations were not finished yet.
    
    After this commit, the onSuccess is executed when the list renderer is really ready
    
    opw-2580044
    
    closes odoo/odoo#79441
    
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/web/static/tests/fields/relational_fields/field_one2many_tests.js
+++ addons/web/static/tests/fields/relational_fields/field_one2many_tests.js
@@ -14,6 +14,9 @@ var relationalFields = require('web.relational_fields');
 var testUtils = require('web.test_utils');
 var fieldUtils = require('web.field_utils');
 
+const AbstractFieldOwl = require('web.AbstractFieldOwl');
+const fieldRegistryOwl = require('web.field_registry_owl');
+
 const cpHelpers = testUtils.controlPanel;
 var createView = testUtils.createView;
 const { FieldOne2Many } = relationalFields;
@@ -9953,6 +9956,69 @@ QUnit.module('fields', {}, function () {
             ], "should have saved the updated turtle_int sequence");
 
             form.destroy();
+        });
+
+        QUnit.test("add_record in an o2m with an OWL field: wait mounted before success", async function (assert) {
+            assert.expect(7);
+
+            let testInst = 0;
+            class TestField extends AbstractFieldOwl {
+                setup() {
+                    super.setup();
+                    const ID = testInst++;
+                    owl.hooks.onMounted(() => {
+                        assert.step(`mounted ${ID}`);
+                    });
+
+                    owl.hooks.onWillUnmount(() => {
+                        assert.step(`willUnmount ${ID}`);
+                    });
+                }
+                activate() {
+                    return true;
+                }
+            }
+
+            TestField.template = owl.tags.xml`<span>test</span>`;
+            fieldRegistryOwl.add('test_field', TestField);
+
+            const def = testUtils.makeTestPromise();
+            const form = await createView({
+                View: FormView,
+                model: 'partner',
+                debug: 1,
+                data: this.data,
+                arch: `<form>
+                        <field name="p">
+                            <tree editable="bottom">
+                                <field name="name" widget="test_field"/>
+                            </tree>
+                        </field>
+                    </form>`,
+                viewOptions: {
+                    mode: 'edit',
+                },
+            });
+
+            const list = form.renderer.allFieldWidgets[form.handle][0];
+
+            list.trigger_up('add_record', {
+                context: [{
+                    default_name: 'this is a test',
+                }],
+                allowWarning: true,
+                forceEditable: 'bottom',
+                onSuccess: function () {
+                    assert.step("onSuccess");
+                    def.resolve();
+                }
+            });
+
+            await testUtils.nextTick();
+            await def;
+            assert.verifySteps(["mounted 0", "willUnmount 0", "mounted 1", "onSuccess"]);
+            form.destroy();
+            assert.verifySteps(["willUnmount 1"]);
         });
     });
 });
