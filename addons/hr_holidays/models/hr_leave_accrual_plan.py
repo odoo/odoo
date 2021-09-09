ed7f1c7c5ee3d021dd7383e645c963b914b75e1e
Kevin Baptiste <kba@odoo.com>
2021-09-09 10:46:17 +0200

    [FIX] hr_holidays: accrual levels should be copied
    The accrual levels were not copied when an Accrual Plan was duplicated.
    
    TaskID: 2639094

--- addons/hr_holidays/models/hr_leave_accrual_plan.py
+++ addons/hr_holidays/models/hr_leave_accrual_plan.py
@@ -11,7 +11,7 @@ class AccrualPlan(models.Model):
     name = fields.Char('Name', required=True)
     time_off_type_id = fields.Many2one('hr.leave.type', string="Time Off Type")
     employees_count = fields.Integer("Employees", compute='_compute_employee_count')
-    level_ids = fields.One2many('hr.leave.accrual.level', 'accrual_plan_id')
+    level_ids = fields.One2many('hr.leave.accrual.level', 'accrual_plan_id', copy=True)
     allocation_ids = fields.One2many('hr.leave.allocation', 'accrual_plan_id')
     transition_mode = fields.Selection([
         ('immediately', 'Immediately'),
@@ -41,3 +41,9 @@ class AccrualPlan(models.Model):
             'res_model': 'hr.employee',
             'domain': [('id', 'in', self.allocation_ids.employee_id.ids)],
         }
+
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        default = dict(default or {},
+                       name=_("%s (copy)", self.name))
+        return super().copy(default=default)
