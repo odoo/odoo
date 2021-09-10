e830968efec87dce3fbc1f42224b631e14e4f9ed
Aaron Bohy <aab@odoo.com>
2021-09-10 14:31:52 +0000

    [REF] web: views: factorize sample data logic
    This commit factorizes the sample data logic to the Model and
    useModel hook. That way, nothing as to be done in concrete models
    anymore.
    
    This commit also ensures that searchParams given to the view are a
    deep copy of what relies in the SearchModel. Before this commit,
    the PivotModel cloned them in its "load" method. It is now done
    in the SearchModel for every views.
    
    X-original-commit: 0f43b7994b6963644cea5b694ba6b07df7af8989
    Part-of: odoo/odoo#76576

--- addons/web/static/src/views/helpers/model.js
+++ addons/web/static/src/views/helpers/model.js
@@ -2,6 +2,8 @@
 
 import { SEARCH_KEYS } from "@web/search/with_search/with_search";
 import { useBus, useService } from "@web/core/utils/hooks";
+import { buildSampleORM } from "@web/views/helpers/sample_server";
+import { useSetupView } from "@web/views/helpers/view_hook";
 
 const { core, hooks } = owl;
 const { EventBus } = core;
@@ -19,6 +21,8 @@ export class Model extends EventBus {
     constructor(env, params, services) {
         super();
         this.env = env;
+        this.orm = services.orm;
+        this.useSampleModel = false; // will be set to true by the "useModel" hook if necessary
         this.setup(params, services);
     }
 
@@ -31,7 +35,20 @@ export class Model extends EventBus {
     /**
      * @param {SearchParams} searchParams
      */
-    load(/* searchParams */) {}
+    async load(/* searchParams */) {}
+
+    /**
+     * This function is meant to be overriden by models that want to implement
+     * the sample data feature. It should return true iff the last loaded state
+     * actually contains data. If not, another load will be done (if the sample
+     * feature is enabled) with the orm service substituted by another using the
+     * SampleServer, to have sample data to display instead of an empty screen.
+     *
+     * @returns {boolean}
+     */
+    hasData() {
+        return true;
+    }
 
     notify() {
         this.trigger("update");
@@ -68,18 +85,51 @@ export function useModel(ModelClass, params, options = {}) {
     for (const key of ModelClass.services) {
         services[key] = useService(key);
     }
+    services.orm = services.orm || useService("orm");
+
     const model = new ModelClass(component.env, params, services);
     useBus(model, "update", options.onUpdate || component.render);
 
+    const globalState = component.props.globalState || {};
+    let useSampleModel = Boolean(
+        "useSampleModel" in globalState
+            ? globalState.useSampleModel
+            : component.props.useSampleModel
+    );
+    model.useSampleModel = useSampleModel;
+    const orm = model.orm;
+    let sampleORM = globalState.sampleORM;
+    const user = useService("user");
+    async function load(props) {
+        model.orm = orm;
+        const searchParams = getSearchParams(props);
+        await model.load(searchParams);
+        if (useSampleModel && !model.hasData()) {
+            sampleORM =
+                sampleORM || buildSampleORM(component.props.resModel, component.props.fields, user);
+            model.orm = sampleORM;
+            await model.load(searchParams);
+        } else {
+            useSampleModel = false;
+        }
+        model.useSampleModel = useSampleModel;
+    }
     onWillStart(() => {
-        const searchParams = getSearchParams(component.props);
-        return model.load(searchParams);
+        return load(component.props);
+    });
+    onWillUpdateProps((nextProps) => {
+        useSampleModel = false;
+        return load(nextProps);
     });
 
-    onWillUpdateProps((nextProps) => {
-        const searchParams = getSearchParams(nextProps);
-        searchParams.useSampleModel = false;
-        return model.load(searchParams);
+    useSetupView({
+        exportGlobalState() {
+            return {
+                sampleORM,
+                useSampleModel: model.useSampleModel,
+            };
+        },
     });
+
     return model;
 }
