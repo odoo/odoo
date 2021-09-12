7b24b2510f9f9eb8f9026f70360196cfe1895098
ahmadjd94 <ahmadjd94@gmail.com>
2021-09-12 15:43:24 +0300

    remove old modules

--- aumet_custom/pos_multi_uom_price/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import product_multi_uom_price
-from . import product_template
-from . import pos_order
-from . import stock_picking
--- aumet_custom/pos_multi_uom_price/models/product_template.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, _
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    
-    multi_uom_price_id = fields.One2many('product.multi.uom.price', 'product_id', _("UOM price"))
