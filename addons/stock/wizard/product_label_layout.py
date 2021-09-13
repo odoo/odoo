odoo/odoo

commit d9f45ba6941939b3d4b40beb5abbc330be84d695

[IMP] product,stock{_picking_batch}: product labels revamp
This commit removes the pdf and zpl reports for product templates and
variants. Instead, labels are printed via a new wizard allowing to
specify the label quantity, format and optional text.

There are a bunch of usual retail label sizes in pdf or zpl code

Task : 2501730

closes odoo/odoo#69690

Related: odoo/enterprise#20246
Related: odoo/upgrade#2542
Signed-off-by: William Henrotin <Whenrow@users.noreply.github.com>

addons/stock/wizard/product_label_layout.py
@@ -1,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from collections import defaultdict
+from odoo import fields, models
+
+
+class ProductLabelLayout(models.TransientModel):
+    _inherit = 'product.label.layout'
+
+    move_line_ids = fields.Many2many('stock.move.line')
+    picking_quantity = fields.Selection([
+        ('picking', 'Transfer Quantities'),
+        ('custom', 'Custom')], string="Quantity to print", default='custom')
+    print_format = fields.Selection(selection_add=[
+        ('zpl', 'ZPL Labels'),
+        ('zplxprice', 'ZPL Labels with price')
+    ], ondelete={'zpl': 'set default', 'zplxprice': 'set default'})
+
+    def _prepare_report_data(self):
+        xml_id, data = super()._prepare_report_data()
+
+        if 'zpl' in self.print_format:
+            xml_id = 'stock.label_product_product'
+
+        if self.picking_quantity == 'picking' and self.move_line_ids:
+            qties = defaultdict(int)
+            uom_unit = self.env.ref('uom.product_uom_categ_unit', raise_if_not_found=False)
+            for line in self.move_line_ids:
+                if line.product_uom_id.category_id == uom_unit:
+                    qties[line.product_id.id] += line.qty_done
+            # Pass only products with some quantity done to the report
+            data['quantity_by_product'] = {p: int(q) for p, q in qties.items() if q}
+        return xml_id, data

addons/stock_picking_batch/models/stock_picking_batch.py
@@ -233,6 +233,22 @@ class StockPickingBatch(models.Model):
         action['context'] = {'default_picking_ids': self.picking_ids.ids}
         return action
 
+    def action_open_label_layout(self):
+        view = self.env.ref('product.product_label_layout_form')
+        return {
+            'name': _('Choose Labels Layout'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'product.label.layout',
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
+            'target': 'new',
+            'context': {
+                'default_product_ids': self.move_lines.product_id.ids,
+                'default_move_line_ids': self.move_line_ids.ids,
+                'default_picking_quantity': 'picking'},
+        }
+
     # -------------------------------------------------------------------------
     # Miscellaneous
     # -------------------------------------------------------------------------

addons/product/wizard/product_label_layout.py
@@ -1,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class ProductLabelLayout(models.TransientModel):
+    _name = 'product.label.layout'
+    _description = 'Choose the sheet layout to print the labels'
+
+    print_format = fields.Selection([
+        ('dymo', 'Dymo'),
+        ('2x7xprice', '2 x 7 with price'),
+        ('4x7xprice', '4 x 7 with price'),
+        ('4x12', '4 x 12'),
+        ('4x12xprice', '4 x 12 with price')], string="Format", default='2x7xprice', required=True)
+    custom_quantity = fields.Integer('Quantity', default=1, required=True)
+    product_ids = fields.Many2many('product.product')
+    product_tmpl_ids = fields.Many2many('product.template')
+    extra_html = fields.Html('Extra Content', default='')
+    rows = fields.Integer(compute='_compute_dimensions')
+    columns = fields.Integer(compute='_compute_dimensions')
+
+    @api.depends('print_format')
+    def _compute_dimensions(self):
+        for wizard in self:
+            if 'x' in wizard.print_format:
+                columns, rows = wizard.print_format.split('x')[:2]
+                wizard.columns = int(columns)
+                wizard.rows = int(rows)
+            else:
+                wizard.columns, wizard.rows = 1, 1
+
+    def _prepare_report_data(self):
+        if self.custom_quantity <= 0:
+            raise UserError(_('You need to set a positive quantity.'))
+
+        # Get layout grid
+        if self.print_format == 'dymo':
+            xml_id = 'product.report_product_template_label_dymo'
+        elif 'x' in self.print_format:
+            xml_id = 'product.report_product_template_label'
+        else:
+            xml_id = ''
+
+        active_model = ''
+        if self.product_tmpl_ids:
+            products = self.product_tmpl_ids.ids
+            active_model = 'product.template'
+        elif self.product_ids:
+            products = self.product_ids.ids
+            active_model = 'product.product'
+
+        # Build data to pass to the report
+        data = {
+            'picking_quantity': self.picking_quantity,
+            'active_model': active_model,
+            'quantity_by_product': {p: self.custom_quantity for p in products},
+            'layout_wizard': self.id,
+            'price_included': 'xprice' in self.print_format,
+        }
+        return xml_id, data
+
+    def process(self):
+        self.ensure_one()
+        xml_id, data = self._prepare_report_data()
+        if not xml_id:
+            raise UserError(_('Unable to find report template for %s format', self.print_format))
+        return self.env.ref(xml_id).report_action(None, data=data)

addons/product/__init__.py
@@ -4,3 +4,4 @@
 from . import models
 from . import report
 from . import populate
+from . import wizard

addons/product/__manifest__.py
@@ -30,6 +30,7 @@ Print product labels with barcode.
         'data/product_data.xml',
         'security/product_security.xml',
         'security/ir.model.access.csv',
+        'wizard/product_label_layout_views.xml',
         'views/res_config_settings_views.xml',
         'views/product_attribute_views.xml',
         'views/product_views.xml',
@@ -40,7 +41,7 @@ Print product labels with barcode.
         'report/product_product_templates.xml',
         'report/product_template_templates.xml',
         'report/product_packaging.xml',
-        'report/product_pricelist_report_templates.xml'
+        'report/product_pricelist_report_templates.xml',
     ],
     'demo': [
         'data/product_demo.xml',
@@ -50,6 +51,9 @@ Print product labels with barcode.
     'assets': {
         'web.assets_backend': [
             'product/static/src/js/**/*',
+        ],
+        'web.report_assets_common': [
+            'product/static/src/scss/report_label_sheet.scss',
         ],
         'web.qunit_suite_tests': [
             'product/static/tests/**/*',

addons/product/models/product.py
@@ -596,6 +596,11 @@ class ProductProduct(models.Model):
             )
         return super().view_header_get(view_id, view_type)
 
+    def action_open_label_layout(self):
+        action = self.env['ir.actions.act_window']._for_xml_id('product.action_open_label_layout')
+        action['context'] = {'default_product_ids': self.ids}
+        return action
+
     def open_pricelist_rules(self):
         self.ensure_one()
         domain = ['|',

addons/product/models/product_template.py
@@ -528,6 +528,11 @@ class ProductTemplate(models.Model):
             '', args=[('id', 'in', list(searched_ids))],
             operator='ilike', limit=limit, name_get_uid=name_get_uid)
 
+    def action_open_label_layout(self):
+        action = self.env['ir.actions.act_window']._for_xml_id('product.action_open_label_layout')
+        action['context'] = {'default_product_tmpl_ids': self.ids}
+        return action
+
     def open_pricelist_rules(self):
         self.ensure_one()
         domain = ['|',

addons/product/report/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import product_label_report
 from . import product_pricelist_report

commit efd151ac984dd4dbf3ebf67d87c7fd4f2e607e30

[FIX] prpduct,stock: fixes on product labels
This commit fix some issue introduced with d9f45ba69419.

addons/stock/wizard/product_label_layout.py
@@ -11,7 +11,7 @@ class ProductLabelLayout(models.TransientModel):
     move_line_ids = fields.Many2many('stock.move.line')
     picking_quantity = fields.Selection([
         ('picking', 'Transfer Quantities'),
-        ('custom', 'Custom')], string="Quantity to print", default='custom')
+        ('custom', 'Custom')], string="Quantity to print", required=True, default='custom')
     print_format = fields.Selection(selection_add=[
         ('zpl', 'ZPL Labels'),
         ('zplxprice', 'ZPL Labels with price')

addons/stock_picking_batch/models/stock_picking_batch.py
@@ -249,7 +249,7 @@ class StockPickingBatch(models.Model):
         return action
 
     def action_open_label_layout(self):
-        view = self.env.ref('product.product_label_layout_form')
+        view = self.env.ref('stock.product_label_layout_form_picking')
         return {
             'name': _('Choose Labels Layout'),
             'type': 'ir.actions.act_window',
@@ -259,7 +259,7 @@ class StockPickingBatch(models.Model):
             'view_id': view.id,
             'target': 'new',
             'context': {
-                'default_product_ids': self.move_lines.product_id.ids,
+                'default_product_ids': self.move_line_ids.product_id.ids,
                 'default_move_line_ids': self.move_line_ids.ids,
                 'default_picking_quantity': 'picking'},
         }