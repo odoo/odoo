a03de8bc38439cfb1f6e1d1ca5d8a81bc111f5df
John Laterre (jol) <jol@odoo.com>
2021-09-13 17:07:43 +0200

    [ADD] l10n_din5008: isolate din 5008 from l10n_de
    DIN 5008 is not specific to Germany.
    It also applies to Switzerland, Austria (& Lichtenstein).
    
    The goal is to make DIN independent from l10n_de,
    for Switzerland and Austria.
    
    Task: 2613993

--- addons/l10n_at/models/__init__.py
+++ addons/l10n_at/models/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import datev
 from . import chart_template
-from . import ir_actions_report
-from . import res_company
