6f08752fc2e052eed25ada4d0c5419f6508c71fd
Hubert Van de Walle (huvw) <huvw@odoo.com>
2021-09-13 10:06:20 +0000

    [FIX] web: select all feature now works as intended
    Steps to follow
    
      - Go to the variant tab of a product
      - Add an attribute and click search more
      - Select the checkbox at the top to select all records from the current page
      - Click on Select all
      - Click on Select
      -> Only the records from the first page are added
    
    Cause of the issue
    
      `getSelectedRecords` doesn't return the record from all pages
      but only from the current one
    
    Solution
    
      If the domain is selected, use all the records
    
    opw-2586877
    
    closes odoo/odoo#77228
    
    X-original-commit: 963ccf1b4f3bb82c974af2ac0e47344c0ce060ac
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Hubert Van De Walle <hubvd@users.noreply.github.com>

--- addons/web/static/tests/legacy/views/view_dialogs_tests.js
+++ addons/web/static/tests/legacy/views/view_dialogs_tests.js
@@ -584,6 +584,40 @@ QUnit.module('Views', {
         parent.destroy();
     });
 
+    QUnit.test('SelectCreateDialog calls on_selected with every record matching the domain', async function (assert) {
+        assert.expect(1);
+
+        const parent = await createParent({
+            data: this.data,
+            archs: {
+                'partner,false,list':
+                    '<tree limit="2" string="Partner">' +
+                        '<field name="display_name"/>' +
+                        '<field name="foo"/>' +
+                    '</tree>',
+                'partner,false,search':
+                    '<search>' +
+                        '<field name="foo"/>' +
+                    '</search>',
+            },
+            session: {},
+        });
+
+        new dialogs.SelectCreateDialog(parent, {
+            res_model: 'partner',
+            on_selected: function(records) {
+                assert.equal(records.length, 3)
+            }
+        }).open();
+        await testUtils.nextTick();
+
+        await testUtils.dom.click($('thead .o_list_record_selector input'));
+        await testUtils.dom.click($('.o_list_selection_box .o_list_select_domain'));
+        await testUtils.dom.click($('.modal .o_select_button'));
+
+        parent.destroy();
+    });
+
     QUnit.test('propagate can_create onto the search popup o2m', async function (assert) {
         assert.expect(4);
 
