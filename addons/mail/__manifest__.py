67d551ea5df0d4f95f31afb69a0cf5fde5a5bd9e
Thibault Delavallée <tde@odoo.com>
2021-09-13 12:20:00 +0000

    [REM] mail: remove unused notification template
    RIP ``mail_notification_borders``. You were a joyful comrade.
    
    Task-2621326 (Mail: add 'view' button in 'light notification template')
    Related to odoo/upgrade#2829
    
    Part-of: odoo/odoo#76418

--- addons/mail/__manifest__.py
+++ addons/mail/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Discuss',
-    'version': '1.5',
+    'version': '1.6',
     'category': 'Productivity/Discuss',
     'sequence': 145,
     'summary': 'Chat, mail gateway and private channels',
