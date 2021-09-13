50f7636ee01fe5b2961d908874aef701427cdb73
David Beguin <dbe@odoo.com>
2021-09-13 10:25:32 +0200

    [FIX] auth_totp, auth_totp_mail: fix misplaced test of 2FA invite button
    Since mail dependancy has been extracted into a bridge module auth_totp_mail,
    the test about "Invite to use 2FA" is misplaced in the wrong module.
    
    This commit moves the test on 2FA invite button to the bridge module and fixes
    the test on auth_totp module to use another indicator that 2FA has been disabled
    for the currently tested user (see test file).
    
    Task-2645206
    Parent Task-2638538
    COM PR: odoo/odoo#76381

--- addons/auth_totp/static/tests/totp_flow.js
+++ addons/auth_totp/static/tests/totp_flow.js
@@ -242,6 +242,6 @@ trigger: 'div:contains("enter your password")',
     trigger: "a.nav-link:contains(Account Security)",
 }, {
     content: "check that demo user has been de-totp'd",
-    trigger: "button:contains(Invite to use 2FA)",
+    trigger: "button[name=action_totp_enable_wizard]",
 }])
 });
