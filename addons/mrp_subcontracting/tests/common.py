eaa32a8b4d817f347253a902414432242d5cba7a
Rémy Voet (ryv) <ryv@odoo.com>
2021-09-13 17:42:07 +0200

    Clean test: TestMrpSubcontractingCommons

--- addons/mrp_subcontracting/tests/common.py
+++ addons/mrp_subcontracting/tests/common.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo import Command
 from odoo.tests.common import Form, TransactionCase
 
 class TestMrpSubcontractingCommon(TransactionCase):
@@ -8,54 +9,38 @@ class TestMrpSubcontractingCommon(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestMrpSubcontractingCommon, cls).setUpClass()
-        # 1: Create a subcontracting partner
-        main_partner = cls.env['res.partner'].create({'name': 'main_partner'})
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.warehouse = cls.env.ref('stock.warehouse0')
+
+        # Create a subcontracting partner
         cls.subcontractor_partner1 = cls.env['res.partner'].create({
             'name': 'subcontractor_partner',
-            'parent_id': main_partner.id,
             'company_id': cls.env.ref('base.main_company').id,
         })
 
-        # 2. Create a BOM of subcontracting type
-        cls.comp1 = cls.env['product.product'].create({
-            'name': 'Component1',
-            'type': 'product',
-            'categ_id': cls.env.ref('product.product_category_all').id,
-        })
-        cls.comp2 = cls.env['product.product'].create({
-            'name': 'Component2',
-            'type': 'product',
-            'categ_id': cls.env.ref('product.product_category_all').id,
-        })
-        cls.finished = cls.env['product.product'].create({
-            'name': 'finished',
-            'type': 'product',
-            'categ_id': cls.env.ref('product.product_category_all').id,
-        })
-        bom_form = Form(cls.env['mrp.bom'])
-        bom_form.type = 'subcontract'
-        bom_form.consumption = 'strict'
-        bom_form.product_tmpl_id = cls.finished.product_tmpl_id
-        bom_form.subcontractor_ids.add(cls.subcontractor_partner1)
-        with bom_form.bom_line_ids.new() as bom_line:
-            bom_line.product_id = cls.comp1
-            bom_line.product_qty = 1
-        with bom_form.bom_line_ids.new() as bom_line:
-            bom_line.product_id = cls.comp2
-            bom_line.product_qty = 1
-        cls.bom = bom_form.save()
-
-        # Create a BoM for cls.comp2
-        cls.comp2comp = cls.env['product.product'].create({
-            'name': 'component for Component2',
-            'type': 'product',
-            'categ_id': cls.env.ref('product.product_category_all').id,
-        })
-        bom_form = Form(cls.env['mrp.bom'])
-        bom_form.product_tmpl_id = cls.comp2.product_tmpl_id
-        with bom_form.bom_line_ids.new() as bom_line:
-            bom_line.product_id = cls.comp2comp
-            bom_line.product_qty = 1
-        cls.comp2_bom = bom_form.save()
-
-        cls.warehouse = cls.env['stock.warehouse'].search([], limit=1)
+        categ_all = cls.env.ref('product.product_category_all')
+        # Create products for BoM
+        cls.finished, cls.comp1, cls.comp2, cls.comp2comp = cls.env['product.product'].create([
+            {'name': 'Finished  ', 'type': 'product', 'categ_id': categ_all.id},
+            {'name': 'Component1', 'type': 'product', 'categ_id': categ_all.id},
+            {'name': 'Component2', 'type': 'product', 'categ_id': categ_all.id},
+            {'name': 'Component for Component2', 'type': 'product', 'categ_id': categ_all.id},
+        ])
+        # 3. Create a subcontracted BoM and a Normal BoM for one component
+        cls.bom, cls.comp2_bom = cls.env['mrp.bom'].create([
+            {
+                'type': 'subcontract',
+                'consumption': 'strict',
+                'product_tmpl_id': cls.finished.product_tmpl_id.id,
+                'subcontractor_ids': [Command.link(cls.subcontractor_partner1.id)],
+                'bom_line_ids': [
+                    Command.create({'product_id': cls.comp1.id, 'product_qty': 1}),
+                    Command.create({'product_id': cls.comp2.id, 'product_qty': 1}),
+                ]
+            }, {
+                'product_tmpl_id': cls.comp2.product_tmpl_id.id,
+                'bom_line_ids': [
+                    Command.create({'product_id': cls.comp2comp.id, 'product_qty': 1}),
+                ]
+            }
+        ])
