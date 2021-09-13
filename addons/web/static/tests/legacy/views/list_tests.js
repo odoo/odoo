odoo/odoo

commit 3dd1c0f87ede01469018f2fadad77b23c789cab8

[FIX] web: undefined group name for group by date:<interval>
Previously, grouping a list by a non-default time interval (e.g.
'creation date:week') yielded a grouped list of records with undefined
group names.

addons/web/static/src/js/views/list/list_renderer.js
@@ -377,6 +377,10 @@ var ListRenderer = BasicRenderer.extend({
             $cells.unshift($('<td>'));
         }
         var name = group.value === undefined ? _t('Undefined') : group.value;
+        var groupBy = this.state.groupedBy[groupLevel];
+        if (group.fields[groupBy.split(':')[0]].type !== 'boolean') {
+            name = name || _t('Undefined');
+        }
         var $th = $('<th>')
                     .addClass('o_group_name')
                     .text(name + ' (' + group.count + ')');

addons/web/static/tests/views/list_tests.js
@@ -2466,6 +2466,23 @@ QUnit.module('Views', {
 
         list.destroy();
     });
+
+    QUnit.test('list grouped by date:month', function (assert) {
+        assert.expect(1);
+
+        var list = createView({
+            View: ListView,
+            model: 'foo',
+            data: this.data,
+            arch: '<tree><field name="date"/></tree>',
+            groupBy: ['date:month'],
+        });
+
+        assert.strictEqual(list.$('tbody').text(), "January 2017 (1)Undefined (3)",
+            "the group names should be correct");
+
+        list.destroy();
+    });
 });
 
 });

addons/web/static/src/js/views/basic/basic_model.js
@@ -84,8 +84,8 @@ odoo.define('web.BasicModel', function (require) {
 
 var AbstractModel = require('web.AbstractModel');
 var concurrency = require('web.concurrency');
-var core = require('web.core');
 var Context = require('web.Context');
+var core = require('web.core');
 var Domain = require('web.Domain');
 var fieldUtils = require('web.field_utils');
 var session = require('web.session');
@@ -3087,7 +3087,8 @@ var BasicModel = AbstractModel.extend({
                 lazy: true,
             })
             .then(function (groups) {
-                var rawGroupBy = list.groupedBy[0].split(':')[0];
+                var groupByField = list.groupedBy[0];
+                var rawGroupBy = groupByField.split(':')[0];
                 var previousGroups = _.map(list.data, function (groupID) {
                     return self.localData[groupID];
                 });
@@ -3098,13 +3099,13 @@ var BasicModel = AbstractModel.extend({
                 _.each(groups, function (group) {
                     var aggregateValues = {};
                     _.each(group, function (value, key) {
-                        if (_.contains(fields, key) && key !== list.groupedBy[0]) {
+                        if (_.contains(fields, key) && key !== groupByField) {
                             aggregateValues[key] = value;
                         }
                     });
                     // When a view is grouped, we need to display the name of each group in
                     // the 'title'.
-                    var value = group[rawGroupBy];
+                    var value = group[groupByField];
                     if (list.fields[rawGroupBy].type === "selection") {
                         var choice = _.find(list.fields[rawGroupBy].selection, function (c) {
                             return c[0] === value;

addons/web/static/tests/helpers/mock_server.js
@@ -632,7 +632,9 @@ var MockServer = Class.extend({
             var fieldName = groupByField.split(':')[0];
             var aggregateFunction = groupByField.split(':')[1] || 'month';
             if (fields[fieldName].type === 'date') {
-                if (aggregateFunction === 'day') {
+                if (!val) {
+                    return false;
+                } else if (aggregateFunction === 'day') {
                     return moment(val).format('YYYY-MM-DD');
                 } else {
                     return moment(val).format('MMMM YYYY');

commit af053446a05685b8ced5a0c03a9a8f64f556d0b5

[FIX] web: falsy group names in list view
When a group has a falsy value (e.g. if grouped by an integer field having a "0" group), make sure to display its proper name instead of "Undefined".
Off course, boolean fields are still showing "false" name if it is the case.

This issue was introduced with 3dd1c0f

addons/web/static/src/legacy/js/views/list/list_renderer.js
@@ -714,11 +714,12 @@ var ListRenderer = BasicRenderer.extend({
     _renderGroupRow: function (group, groupLevel) {
         var cells = [];
 
-        var name = group.value === undefined ? _t('Undefined') : group.value;
-        var groupBy = this.state.groupedBy[groupLevel];
-        if (group.fields[groupBy.split(':')[0]].type !== 'boolean') {
-            name = name || _t('Undefined');
-        }
+        const groupBy = this.state.groupedBy[groupLevel];
+        const groupByFieldName = groupBy.split(':')[0];
+        const groupByField = group.fields[groupByFieldName];
+        const name = groupByField.type === "boolean"
+            ? (group.value === undefined ? _t('Undefined') : group.value)
+            : (group.value === undefined || group.value === false ? _t('Undefined') : group.value);
         var $th = $('<th>')
             .addClass('o_group_name')
             .attr('tabindex', -1)

addons/web/static/tests/legacy/views/list_tests.js
@@ -9895,7 +9895,7 @@ QUnit.module('Views', {
     });
 
     QUnit.test('grouped lists with expand attribute and a lot of groups', async function (assert) {
-        assert.expect(8);
+        assert.expect(10);
 
         for (var i = 0; i < 15; i++) {
             this.data.foo.records.push({foo: 'record ' + i, int_field: i});
@@ -9916,13 +9916,42 @@ QUnit.module('Views', {
         });
 
         assert.containsN(list, '.o_group_header', 10); // page 1
-        assert.containsN(list, '.o_data_row', 11); // one group contains two records
+        assert.containsN(list, '.o_data_row', 12); // two groups contains two records
         assert.containsOnce(list, '.o_pager'); // has a pager
+
+        assert.deepEqual(
+            [...list.el.querySelectorAll(".o_group_name")].map((el) => el.innerText),
+            [
+                "10 (2)",
+                "9 (2)",
+                "17 (1)",
+                "-4 (1)",
+                "0 (1)",
+                "1 (1)",
+                "2 (1)",
+                "3 (1)",
+                "4 (1)",
+                "5 (1)",
+            ]
+        );
 
         await cpHelpers.pagerNext(list); // switch to page 2
 
         assert.containsN(list, '.o_group_header', 7); // page 2
         assert.containsN(list, '.o_data_row', 7);
+
+        assert.deepEqual(
+            [...list.el.querySelectorAll(".o_group_name")].map((el) => el.innerText),
+            [
+                "6 (1)",
+                "7 (1)",
+                "8 (1)",
+                "11 (1)",
+                "12 (1)",
+                "13 (1)",
+                "14 (1)",
+            ]
+        );
 
         assert.verifySteps([
             'web_read_group', // read_group page 1