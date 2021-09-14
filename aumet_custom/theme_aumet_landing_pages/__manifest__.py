f02f091c8647c44d9d30270bf6bf228a5081d6aa
ihab-shhadat <ikshhadat17@gmail.com>
2021-09-14 14:09:06 +0300

    Landing page

---
+++ aumet_custom/theme_aumet_landing_pages/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Landing Page Theme',
+    'description': 'Aumet Pharmacy Theme',
+    'summary': 'Aumet Pharmacy Theme',
+    'category': 'Theme/Corporate',
+    'version': '1.0',
+    'author': 'Ihab Shhadat',
+    'depends': ['base', 'website', 'website_crm'],
+    'data': [
+        'views/footer.xml',
+        'views/header.xml',
+        'views/contact_us.xml',
+        'views/views.xml',
+        'views/snippets/index/index_headline.xml',
+        'views/snippets/index/index_header.xml',
+        'views/snippets/index/index_partner.xml',
+        'views/snippets/index/index_partner_item.xml',
+        'views/snippets/index/index_service.xml',
+        'views/snippets/index/index_service_item.xml',
+        'views/snippets/index/index_testmonial.xml',
+        'views/snippets/index/index_video.xml',
+
+    ],
+    'images': [
+        'static/description/banner.png',
+        'static/description/splash_screenshot.png'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
