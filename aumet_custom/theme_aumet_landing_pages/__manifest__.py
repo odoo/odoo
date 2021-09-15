0ce464e069dd5a6ba7ef9ecc7b62084147542aa9
ihab-shhadat <ikshhadat17@gmail.com>
2021-09-15 11:25:47 +0300

    Landing page

--- aumet_custom/theme_aumet_landing_pages/__manifest__.py
+++ aumet_custom/theme_aumet_landing_pages/__manifest__.py
@@ -8,17 +8,17 @@
     'depends': ['base', 'website', 'website_crm'],
     'data': [
         'views/footer.xml',
-        'views/header.xml',
         'views/contact_us.xml',
+        'views/header.xml',
         'views/views.xml',
         'views/snippets/index/index_headline.xml',
-        'views/snippets/index/index_header.xml',
         'views/snippets/index/index_partner.xml',
         'views/snippets/index/index_partner_item.xml',
         'views/snippets/index/index_service.xml',
         'views/snippets/index/index_service_item.xml',
         'views/snippets/index/index_testmonial.xml',
         'views/snippets/index/index_video.xml',
+        'views/snippets/index/index_plan.xml',
 
     ],
     'images': [
