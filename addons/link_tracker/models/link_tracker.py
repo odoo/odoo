4af4d0445553d6bd936d1b52282f8c7e5dc42ce7
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2021-09-15 08:25:16 +0000

    [FIX] link_tracker,website_links: use specific site url
    - Create 3 websites and set a website domain for each
    - Install the website_links module
    - Go to Website > Configuration > Settings
    - Select the website 3
    - Go to website
    - Menu Promote > Link tracking
    
    The URL field uses the domain you are logged into the database
    rather than the domain of the active website
    
    The expected behavior is:
    With an ICP set to URL1, a website 1 set to URL2 and a website 2 set to
    URL3, going to the link tracker website page from URL3 but with website
    1 selected should:
    
    Show the current website domain as placeholder of the input -> URL2
    Create link for that current website the user selected -> URL2
    
    opw-2638494
    
    closes odoo/odoo#77705
    
    X-original-commit: d094d76354d6f46db3dc7f8e716b5f778ca26569
    Signed-off-by: agr-odoo <agr-odoo@users.noreply.github.com>

--- addons/link_tracker/models/link_tracker.py
+++ addons/link_tracker/models/link_tracker.py
@@ -69,7 +69,7 @@ class LinkTracker(models.Model):
     @api.depends('code')
     def _compute_short_url(self):
         for tracker in self:
-            tracker.short_url = urls.url_join(tracker.get_base_url(), '/r/%(code)s' % {'code': tracker.code})
+            tracker.short_url = urls.url_join(tracker.short_url_host, '%(code)s' % {'code': tracker.code})
 
     def _compute_short_url_host(self):
         for tracker in self:
