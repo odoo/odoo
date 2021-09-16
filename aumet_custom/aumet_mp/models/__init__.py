c5c47783805f2faa44874bad27a380c2a57f0366
ahmadjd94 <ahmadjd94@gmail.com>
2021-09-16 10:14:09 +0300

    remove redundant module

--- aumet_custom/aumet_mp/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import aumet_marketplace
-from . import res_company
-from . import marketplace_product
-from . import partner
-from . import product
-from . import purchase
-from . import payment_method
