510a498aa71f117975a08ca3d3df6217e3b8a74d
Hubert Van de Walle (huvw) <huvw@odoo.com>
2021-09-16 12:58:44 +0000

    [FIX] web: export empty relational field in xlsx as empty string
    Steps to follow
    
      - Go to the contact app for example
      - Export some records with a relational field in the import-compatible xlsx format
      - Reimport the downloaded file
      - Odoo can't find matching records with a name False
    
    Cause of the issue
    
      xlsxwriter will write a boolean false value for empty relations
    
    Solution
    
      If a field is relational, write an empty string instead
    
    opw-2643705
    
    closes odoo/odoo#82771
    
    X-original-commit: 43dbeccd1fb845f08f5bc48562f40a848d0a64d2
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/models.py
+++ odoo/models.py
@@ -999,9 +999,9 @@ class BaseModel(metaclass=MetaModel):
 
                             if name == 'id':
                                 xml_ids = [xid for _, xid in value.__ensure_xml_id()]
-                                current[index] = ','.join(xml_ids) or False
+                                current[index] = ','.join(xml_ids)
                             else:
-                                current[index] = field.convert_to_export(value, record) or False
+                                current[index] = field.convert_to_export(value, record)
                             continue
 
                         lines2 = value._export_rows(fields2, _is_toplevel_call=False)
@@ -1013,7 +1013,7 @@ class BaseModel(metaclass=MetaModel):
                             # append the other lines at the end
                             lines += lines2[1:]
                         else:
-                            current[i] = False
+                            current[i] = ''
 
         # if any xid should be exported, only do so at toplevel
         if _is_toplevel_call and any(f[-1] == 'id' for f in fields):
