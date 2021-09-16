008ed3e77733a7fa524da246ce27c056260203cb
Francois (fge) <fge@odoo.com>
2021-09-16 08:28:24 +0000

    [FIX] website: missing dependency in debugMenuService
    DebugMenu depends on services:
    - location => env._t
    - command => useCommand hook
    This is add by dca9ca6464ccac8ed5e6b22e445706d16b7ec2ed.
    
    Part-of: odoo/odoo#76642

--- addons/website/static/src/js/menu/debug_menu.js
+++ addons/website/static/src/js/menu/debug_menu.js
@@ -4,7 +4,7 @@ import { DebugMenu } from "@web/core/debug/debug_menu";
 import { createDebugContext } from "@web/core/debug/debug_context";
 
 const debugMenuService = {
-    dependencies: ["orm"],
+    dependencies: ["command", "localization", "orm"],
     start(env) {
         Object.assign(env, createDebugContext(env, { categories: ["default"] }));
         const systray = document.querySelector('.o_menu_systray');
