ba619ce02719d3d5f978e6b26b29a56d92e41359
Kevin Baptiste <kba@odoo.com>
2021-09-16 12:35:32 +0000

    [IMP] hr_holidays: allow users to cancel Time Off
    A user without Time Off rights is now allowed to cancel their own
    validated time off when:
        - It's not yet started;
        - No work-entries has been generated for it yet.
    
    The time off is archived and the days taken reallocated.
    
    closes odoo/odoo#76722
    
    Taskid: 2643713
    Related: odoo/upgrade#2843
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_holidays/static/src/js/time_off_calendar/time_off_calendar_controller.js
+++ addons/hr_holidays/static/src/js/time_off_calendar/time_off_calendar_controller.js
@@ -58,20 +58,50 @@ export const TimeOffCalendarController = CalendarController.extend({
         const timeoffData = event.target.state.data.find((timeOff) => timeOff.id === eventId);
         // Take the record associated to the timeoffData and read its state to hide or not the Delete button
         const timeoffState = timeoffData && timeoffData.record.state;
-        if(timeoffState && !['validate', 'refuse'].includes(timeoffState)) {
+        const canCancel = timeoffData && timeoffData.record.can_cancel;
+        if(canCancel || timeoffState && !['validate', 'refuse'].includes(timeoffState)) {
             options.deletable = true;
             options.removeButtonText = _t("Delete");
-            options.on_remove = () => {
-                Dialog.confirm(self, _t("Are you sure you want to delete this record ?"), {
-                    confirm_callback: () => {
-                        self.model.deleteRecords(self._getEventId(event), self.modelName).then(() => {
-                            self.reload();
-                        });
-                    }
-                });
-            };
+            options.on_remove = () => this._deleteLeave(timeoffData.record);
         }
         return options;
+    },
+
+    _onDeleteRecord(ev) {
+        this._deleteLeave(ev.data.event.record);
+    },
+
+    _deleteLeave(timeoff) {
+        const canCancel = timeoff.can_cancel;
+        if (canCancel) {
+            const context = Object.assign({}, this.context, {
+                default_leave_id: timeoff.id,
+            });
+            const cancel_dialog = new dialogs.FormViewDialog(this, {
+                title: _t('Delete Confirmation'),
+                res_model: 'hr.holidays.cancel.leave',
+                context: context,
+            });
+            cancel_dialog.on('execute_action', this, (ev) => {
+                const action_name = ev.data.action_data.name || ev.data.action_data.special;
+                const event_data = _.clone(ev.data);
+                if (action_name === 'action_cancel_leave') {
+                    ev.stopPropagation();
+                    this.trigger_up('execute_action', event_data);
+                    setTimeout(() => this.reload(), 300);
+                    cancel_dialog.close();
+                }
+            });
+            cancel_dialog.open();
+        } else {
+            Dialog.confirm(this, _t("Are you sure you want to delete this record ?"), {
+                confirm_callback: () => {
+                    this.model.deleteRecords(timeoff.id, this.modelName).then(() => {
+                        this.reload();
+                    });
+                }
+            });
+        }
     },
 
     //--------------------------------------------------------------------------
