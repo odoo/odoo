e239502227326634c8d6e290af4932a0a0099341
Aaron Bohy <aab@odoo.com>
2021-09-17 11:03:35 +0000

    [IMP] web: add Race concurrency primitive
    This commit adds a new concurrency primitive inspired by
    Promise.race, except that it is dynamic (promises can be added to
    a Race instance anytime).
    
    X-original-commit: 4a487524d69ac7ede68296ea4e1f51e1b769d642
    Part-of: odoo/odoo#76778

--- addons/web/static/src/core/utils/concurrency.js
+++ addons/web/static/src/core/utils/concurrency.js
@@ -117,3 +117,47 @@ export class Mutex {
         return this._unlockedProm || Promise.resolve();
     }
 }
+
+/**
+ * Race is a class designed to manage concurrency problems inspired by
+ * Promise.race(), except that it is dynamic in the sense that promises can be
+ * added anytime to a Race instance. When a promise is added, it returns another
+ * promise which resolves as soon as a promise, among all added promises, is
+ * resolved. The race is thus over. From that point, a new race will begin the
+ * next time a promise will be added.
+ */
+export class Race {
+    constructor() {
+        this.currentProm = null;
+        this.currentPromResolver = null;
+    }
+    /**
+     * Register a new promise. If there is an ongoing race, the promise is added
+     * to that race. Otherwise, it starts a new race. The returned promise
+     * resolves as soon as the race is over, with the value of the first resolved
+     * promise added to the race.
+     *
+     * @param {Promise} promise
+     * @returns {Promise
+     */
+    add(promise) {
+        if (!this.currentProm) {
+            this.currentProm = new Promise((resolve) => {
+                this.currentPromResolver = (value) => {
+                    this.currentProm = null;
+                    this.currentPromResolver = null;
+                    resolve(value);
+                };
+            });
+        }
+        promise.then(this.currentPromResolver);
+        return this.currentProm;
+    }
+    /**
+     * @returns {Promise|null} promise resolved as soon as the race is over, or
+     *   null if there is no race ongoing)
+     */
+    getCurrentProm() {
+        return this.currentProm;
+    }
+}
