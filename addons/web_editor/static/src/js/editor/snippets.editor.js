ea2a40afb96147e83c897a0376e187d8c957e66a
qsm-odoo <qsm@odoo.com>
2021-09-20 09:33:06 +0000

    [FIX] web_editor: properly await all async parts for onRemove/onClone
    Even though onRemove and onClone cannot be async themselves (yet?), it
    is called via a trigger_up of `call_for_each_child_snippet` so that each
    part of the snippet being removed / cloned has their onRemove / onClone
    called. Doing that way, some SnippetEditor instances may have to be
    created and it can be an async operation... the problem is that our code
    was only awaiting the first of those instanciations instead of all of
    them.
    
    closes odoo/odoo#76853
    
    X-original-commit: 64ce5a7bff933ad93c0618b0db5be5b3bcb09834
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -315,8 +315,8 @@ var SnippetEditor = Widget.extend({
                     for (var i in editor.styles) {
                         editor.styles[i].onRemove();
                     }
-                    resolve();
                 },
+                onSuccess: resolve,
             });
         });
 
@@ -518,8 +518,8 @@ var SnippetEditor = Widget.extend({
                             isCurrent: ($snippet.is($clone)),
                         });
                     }
-                    resolve();
                 },
+                onSuccess: resolve,
             });
         });
         this.trigger_up('snippet_cloned', {$target: $clone, $origin: this.$target});
@@ -2325,7 +2325,10 @@ var SnippetsMenu = Widget.extend({
      * @param {OdooEvent} ev
      */
     _onCallForEachChildSnippet: function (ev) {
-        this._callForEachChildSnippet(ev.data.$snippet, ev.data.callback);
+        const prom = this._callForEachChildSnippet(ev.data.$snippet, ev.data.callback);
+        if (ev.data.onSuccess) {
+            prom.then(() => ev.data.onSuccess());
+        }
     },
     /**
      * Called when the overlay dimensions/positions should be recomputed.
