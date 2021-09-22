6dcf27fff03d376defa92fc619804c97faa2ee07
svs-odoo <svs@odoo.com>
2021-09-22 14:54:57 +0000

    [FIX] product_expiry: lot expiration_date
    This commit adresses to two issues:
    
    Since 0c83542a93dde8dcaead04fadb7db95cf627a8b4, the move line's `expiration_date`
    set by the user is always overrided at the creation of the move which
    means the user has to modify it after the move was created.
    
    To fix that, the compute doesn't override the `expiration_date` if there
    is already one set.
    Therefore, the onchange on the product who was removed is re-added to
    force to recompute the `expiration_date` if the move line's product is
    changed and it has already an `expiration_date` set.
    
    closes odoo/odoo#78189
    
    X-original-commit: db2881c4b6dc02b68ad61c1be8464d874f9115db
    Signed-off-by: William Henrotin <Whenrow@users.noreply.github.com>
    Signed-off-by: Steve Van Essche <svs-odoo@users.noreply.github.com>

--- addons/product_expiry/tests/test_stock_production_lot.py
+++ addons/product_expiry/tests/test_stock_production_lot.py
@@ -293,12 +293,12 @@ class TestStockProductionLot(TestStockCommon):
         receipt.action_confirm()
 
         # Defines a date during the receipt.
-        move = receipt.move_ids_without_package[0]
-        line = move.move_line_ids[0]
-        self.assertEqual(move.use_expiration_date, True)
-        line.lot_name = 'Apple Box #2'
-        line.expiration_date = expiration_date
-        line.qty_done = 4
+        move_form = Form(receipt.move_ids_without_package, view="stock.view_stock_move_operations")
+        with move_form.move_line_ids.new() as line:
+            line.lot_name = 'Apple Box #2'
+            line.expiration_date = expiration_date
+            line.qty_done = 4
+        move = move_form.save()
 
         receipt._action_done()
         # Get back the lot created when the picking was done...
