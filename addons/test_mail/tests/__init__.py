f21777805d37cbb5b0374507ae27694e0c556c1d
Thibault Delavallée <tde@odoo.com>
2021-09-22 15:12:18 +0000

    [IMP] test_mail: add test for reply_to computation on records
    Purpose is to assert current behavior as this may change in master. Two
    kind of tests are added: performance and multi company. This is done in
    stable to better see evolution of those tests in various stables.
    
    Prepares Task-36879 (MultiCompany Aliases)
    
    X-Original-Commit: odoo/odoo@04efd28a626b694a9460986fbb69e19a52d8b039

--- addons/test_mail/tests/__init__.py
+++ addons/test_mail/tests/__init__.py
@@ -9,6 +9,7 @@ from . import test_mail_followers
 from . import test_mail_message
 from . import test_mail_mail
 from . import test_mail_gateway
+from . import test_mail_multicompany
 from . import test_mail_thread_internals
 from . import test_mail_template
 from . import test_mail_template_preview
