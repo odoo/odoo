7611de196c5298485ed94cc9357bde1e1d253c18
Touati Djamel (otd) <otd@odoo.com>
2021-09-22 09:49:54 +0000

    [FIX] purchase_stock, purchase_mrp: adjust the qty of purchased kit correctly
    Steps to reproduce the bug:
    - Create a BOM kit for “product K”  with:
        - 2 * “product A”
        - 1 * “product B”
    - Create a PO for 1 unit of “product K” > confirm
    - A receipt delivery with 2 units of “product A” and 1 unit of B will be created
    - Modify the ordered Qty to 2 units of “Product K”
    
    Problem:
    The receipt delivery will not be updated correctly (4 units of product A and 3 of product B)
    because the `"_prepare_stock_moves"` function computed the previous quantity wrong based on the moves quantities
    since the moves are for products A and B, not product F.(do not take into account the products in kit)
    
    Solution:
    For kit products, do not calculate from the `"stock.move"`, calculate the difference between the quantity before and after the change
    
    opw-2645719
    
    closes odoo/odoo#77706
    
    X-original-commit: ffd4fe8a2b0b25b398118dfc5ac08d007c256882
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>
    Signed-off-by: Djamel Touati <DjamelTouati@users.noreply.github.com>

--- addons/purchase_stock/models/purchase.py
+++ addons/purchase_stock/models/purchase.py
@@ -351,9 +351,11 @@ class PurchaseOrderLine(models.Model):
         if values.get('date_planned'):
             new_date = fields.Datetime.to_datetime(values['date_planned'])
             self.filtered(lambda l: not l.display_type)._update_move_date_deadline(new_date)
+        lines = self.filtered(lambda l: l.order_id.state == 'purchase')
+        previous_product_qty = {line.id: line.product_uom_qty for line in lines}
         result = super(PurchaseOrderLine, self).write(values)
         if 'product_qty' in values:
-            self.filtered(lambda l: l.order_id.state == 'purchase')._create_or_update_picking()
+            lines.with_context(previous_product_qty=previous_product_qty)._create_or_update_picking()
         return result
 
     def action_product_forecast_report(self):
@@ -446,13 +448,8 @@ class PurchaseOrderLine(models.Model):
         if self.product_id.type not in ['product', 'consu']:
             return res
 
-        qty = 0.0
         price_unit = self._get_stock_move_price_unit()
-        outgoing_moves, incoming_moves = self._get_outgoing_incoming_moves()
-        for move in outgoing_moves:
-            qty -= move.product_uom._compute_quantity(move.product_uom_qty, self.product_uom, rounding_method='HALF-UP')
-        for move in incoming_moves:
-            qty += move.product_uom._compute_quantity(move.product_uom_qty, self.product_uom, rounding_method='HALF-UP')
+        qty = self._get_qty_procurement()
 
         move_dests = self.move_dest_ids
         if not move_dests:
@@ -477,6 +474,16 @@ class PurchaseOrderLine(models.Model):
             extra_move_vals['move_dest_ids'] = False  # don't attach
             res.append(extra_move_vals)
         return res
+
+    def _get_qty_procurement(self):
+        self.ensure_one()
+        qty = 0.0
+        outgoing_moves, incoming_moves = self._get_outgoing_incoming_moves()
+        for move in outgoing_moves:
+            qty -= move.product_uom._compute_quantity(move.product_uom_qty, self.product_uom, rounding_method='HALF-UP')
+        for move in incoming_moves:
+            qty += move.product_uom._compute_quantity(move.product_uom_qty, self.product_uom, rounding_method='HALF-UP')
+        return qty
 
     def _check_orderpoint_picking_type(self):
         warehouse_loc = self.order_id.picking_type_id.warehouse_id.view_location_id
