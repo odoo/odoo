77ae007082e0d8602ad66165be200b40135af836
Alexandre Kühn <aku@odoo.com>
2021-09-22 08:37:37 +0000

    [FIX] mail: remove webRTC component excessive getters
    X-original-commit: 9bf4b6c29207b97368f27098df3cfbe79e2b5f0f
    Part-of: odoo/odoo#77107

--- addons/mail/static/src/components/rtc_invitations/rtc_invitations.js
+++ addons/mail/static/src/components/rtc_invitations/rtc_invitations.js
@@ -4,20 +4,7 @@ import { registerMessagingComponent } from '@mail/utils/messaging_component';
 
 const { Component } = owl;
 
-export class RtcInvitations extends Component {
-
-    //--------------------------------------------------------------------------
-    // Getters / Setters
-    //--------------------------------------------------------------------------
-
-    /**
-     * @returns {mail.thread[]}
-     */
-    get threads() {
-        return this.messaging && this.messaging.ringingThreads;
-    }
-
-}
+export class RtcInvitations extends Component {}
 
 Object.assign(RtcInvitations, {
     props: {},
