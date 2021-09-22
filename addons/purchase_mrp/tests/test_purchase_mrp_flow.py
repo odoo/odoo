c264b6fb208a4ad3cd7331b5806bb4712d79d44f
Touati Djamel (otd) <otd@odoo.com>
2021-09-22 09:49:54 +0000

    [FIX] purchase_stock, purchase_mrp: adjust the qty of purchased kit correctly
    Steps to reproduce the bug:
    - Create a BOM kit for “product K”  with:
        - 2 * “product A”
        - 1 * “product B”
    - Create a PO for 1 unit of “product K” > confirm
    - A receipt delivery with 2 units of “product A” and 1 unit of B will be created
    - Modify the ordered Qty to 2 units of “Product K”
    
    Problem:
    The receipt delivery will not be updated correctly (4 units of product A and 3 of product B)
    because the `"_prepare_stock_moves"` function computed the previous quantity wrong based on the moves quantities
    since the moves are for products A and B, not product F.(do not take into account the products in kit)
    
    Solution:
    For kit products, do not calculate from the `"stock.move"`, calculate the difference between the quantity before and after the change
    
    opw-2645719
    
    X-original-commit: 30305b4a4293700a24d8445a8af0ade1cd3398f6

--- addons/purchase_mrp/tests/test_purchase_mrp_flow.py
+++ addons/purchase_mrp/tests/test_purchase_mrp_flow.py
@@ -3,6 +3,7 @@
 
 from odoo.tests.common import Form, TransactionCase
 from odoo.tests import Form
+from odoo import fields
 
 
 class TestSaleMrpFlow(TransactionCase):
@@ -484,4 +485,27 @@ class TestSaleMrpFlow(TransactionCase):
         ]).order_id
         self.assertTrue(purchase)
         self.assertEqual(purchase.order_line.product_qty, 5)
+
+    def test_01_purchase_mrp_kit_qty_change(self):
+        self.partner = self.env.ref('base.res_partner_1')
+
+        # Create a PO with one unit of the kit product
+        self.po = self.env['purchase.order'].create({
+            'partner_id': self.partner.id,
+            'order_line': [(0, 0, {'name': self.kit_1.name, 'product_id': self.kit_1.id, 'product_qty': 1, 'product_uom': self.kit_1.uom_id.id, 'price_unit': 60.0, 'date_planned': fields.Datetime.now()})],
+        })
+        # Validate the PO
+        self.po.button_confirm()
+
+        # Check the component qty in the created picking
+        self.assertEqual(self.po.picking_ids.move_ids_without_package[0].product_uom_qty,2, "The quantity of components must be created according to the BOM")
+        self.assertEqual(self.po.picking_ids.move_ids_without_package[1].product_uom_qty,1, "The quantity of components must be created according to the BOM")
+        self.assertEqual(self.po.picking_ids.move_ids_without_package[2].product_uom_qty,3, "The quantity of components must be created according to the BOM")
+
+        # Update the kit quantity in the PO
+        self.po.order_line[0].product_qty = 2
+        # Check the component qty after the update
+        self.assertEqual(self.po.picking_ids.move_ids_without_package[0].product_uom_qty,4, "The amount of the kit components must be updated when changing the quantity of the kit.")
+        self.assertEqual(self.po.picking_ids.move_ids_without_package[1].product_uom_qty,2, "The amount of the kit components must be updated when changing the quantity of the kit.")
+        self.assertEqual(self.po.picking_ids.move_ids_without_package[2].product_uom_qty,6, "The amount of the kit components must be updated when changing the quantity of the kit.")
 
