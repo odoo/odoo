c92b3657f659ca21cd5e0a1a2a2121dee28c25ee
Hoang Tran <thhoang.tr@gmail.com>
2021-09-23 14:12:27 +0000

    [FIX] stock: avoid write on move when create return line
    When creating a wizard `stock.return.picking` and call
    `_onchange_picking_id`, the creation of return picking line will perform
    a write on move linked with it, due to `uom_id` being set as related with
    `readonly=False`. Set `readonly` to `True` to avoid it
    
    closes odoo/odoo#77100
    
    Signed-off-by: Tiffany Chang <tic@odoo.com>

--- addons/stock/tests/__init__.py
+++ addons/stock/tests/__init__.py
@@ -14,6 +14,7 @@ from . import test_packing_neg
 from . import test_proc_rule
 from . import test_wise_operator
 from . import test_report
+from . import test_stock_return_picking
 
 # TODO enable this test: error on runbot to create asset and open the iframe
 # from . import test_ui
