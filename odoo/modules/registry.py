cdbd315f827c410f2f39ab956a890ff014108916
Raphael Collet <rco@odoo.com>
2021-09-23 14:44:37 +0000

    [FIX] registry: make invalidation flags thread-specific
    The registry attributes 'registry_invalidated' and 'cache_invalidated'
    are used to flag that the current request has modified the registry or
    invalidated the ormcache, respectively.  This provides a simple yet
    efficient way to signal registry changes or cache invalidations to other
    workers.
    
    However, those flags were not meant to be used with multi-threaded
    workers.  For instance, a thread may signal registry changes that are
    actually made by another thread.  It can also happen that a thread
    changes the registry, which makes another thread crash (like a thread
    modifying a dict while another one iterates over it), and the latter
    will reset the registry to its original state because it misinterprets
    the registry changes as its own changes.
    
    The situation can even get worse, making threads crash in cascade and
    eventually leaving the registry in an inconsistent state.  When this
    happens, the worker is broken and has to be manually restarted.
    
    The fix consists in making those flags thread-specific.  This does not
    prevent thread crashing because of concurrent changes, but at least it
    avoids leaving the worker in a broken state.
    
    closes odoo/odoo#77178
    
    Signed-off-by: Raphael Collet (rco) <rco@openerp.com>

--- odoo/modules/registry.py
+++ odoo/modules/registry.py
@@ -137,8 +137,7 @@ class Registry(Mapping):
         self.cache_sequence = None
 
         # Flags indicating invalidation of the registry or the cache.
-        self.registry_invalidated = False
-        self.cache_invalidated = False
+        self._invalidation_flags = threading.local()
 
         with closing(self.cursor()) as cr:
             has_unaccent = odoo.modules.db.has_unaccent(cr)
@@ -362,6 +361,24 @@ class Registry(Mapping):
         """
         for model in self.models.values():
             model.clear_caches()
+
+    @property
+    def registry_invalidated(self):
+        """ Determine whether the current thread has modified the registry. """
+        return getattr(self._invalidation_flags, 'registry', False)
+
+    @registry_invalidated.setter
+    def registry_invalidated(self, value):
+        self._invalidation_flags.registry = value
+
+    @property
+    def cache_invalidated(self):
+        """ Determine whether the current thread has modified the cache. """
+        return getattr(self._invalidation_flags, 'cache', False)
+
+    @cache_invalidated.setter
+    def cache_invalidated(self, value):
+        self._invalidation_flags.cache = value
 
     def setup_signaling(self):
         """ Setup the inter-process signaling on this registry. """
