df3a0826230774944d52ac8dcdfd369b7e9caf63
Samk13 <samarbid13@gmail.com>
2021-09-27 00:49:55 +0200

    setup custom theme

---
+++ addons/theme_tutorial/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Sam theme',
+    'description': 'A description for your theme.',
+    'version': '1.0',
+    'author': 'Sam Arbid',
+    'category': 'Theme/Creative',
+
+    'depends': ['website'],
+    'data': [
+    ],
+}
