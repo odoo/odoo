f271510d99ca8ea3b9f2570306575715a2c712ee
Romain Derie <rde@odoo.com>
2021-09-27 17:28:13 +0000

    [FIX] web: remove the command palette from the frontend
    There is multiple issue with the palette in the frontend:
    
    First, the command palette is bound to CTRL+K, which need to be bound to link
    creation as this is an universal keybind, as CTRL+P CTRL+F are, as any other
    edition app in the world does.
    (Note that link creation is not only for edit mode but also for end user, eg
    forum post)
    History of the keybind:
    1. Command palette was introduced with CTRL+K
    2. The keybind was changed to CTRL+M to restore CTRL+K for link edition, see
       2e396628e216
    3. The keybind change was reverted and command back to CTRL+K, as CTRL+M can't
       be used on MAC
    
    Second, the command palette do not correctly work in the frontend, the menu are
    not opened when an entry is selected, and the submenu are not shown in the palette
    when the menu was opened before opening the palette.
    
    Lastly, the CTRL+K in frontend does not only open the command palette but it
    also create a link in the background of the palette being opened.. which
    result in very bad usability..
    The one expecting a link has to deal with a palette.
    The one expecting a palette has had a link created without really noticing it.
    
    task-2659885
    
    closes odoo/odoo#77879
    
    X-original-commit: 6135b04738a58564592f438364f5a147ce9c471f
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website/static/src/js/menu/debug_menu.js
+++ addons/website/static/src/js/menu/debug_menu.js
@@ -1,16 +1,16 @@
 /** @odoo-module */
 import { registry } from "@web/core/registry";
-import { DebugMenu } from "@web/core/debug/debug_menu";
+import { DebugMenuBasic } from "@web/core/debug/debug_menu_basic";
 import { createDebugContext } from "@web/core/debug/debug_context";
 
 const debugMenuService = {
-    dependencies: ["command", "localization", "orm"],
+    dependencies: ["localization", "orm"],
     start(env) {
         if (env.debug) {
             const systray = document.querySelector('.o_menu_systray');
             if (systray) {
                 Object.assign(env, createDebugContext(env, {categories: ["default"]}));
-                owl.mount(DebugMenu, {
+                owl.mount(DebugMenuBasic, {
                     target: systray,
                     position: 'first-child',
                     env,
