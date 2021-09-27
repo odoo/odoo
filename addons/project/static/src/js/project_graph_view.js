fdfd8b35b91bc42664ff4c01bf75d51354116c44
Julien Mougenot <jum@odoo.com>
2021-09-27 09:12:51 +0200

    [REF] *: adapt modules to action and view contexts
    This commit handles the adaptation of any component using the search
    model's 'action' and/or 'view' key, now replaced by the environment
    keys 'actionContext' and 'viewContext'.
    
    Affected modules are:
    - base_import
    - board
    - crm
    - google_spreadsheet
    - project
    
    X-original-commit: 40f1ae87e1436192a70a8a7432eb72d379cfa6dc

--- addons/project/static/src/js/project_graph_view.js
+++ addons/project/static/src/js/project_graph_view.js
@@ -1,12 +1,12 @@
 /** @odoo-module **/
 
-import { GraphView } from "@web/views/graph/graph_view";
 import { ProjectControlPanel } from "@project/project_control_panel/project_control_panel";
 import { registry } from "@web/core/registry";
+import { GraphView } from "@web/views/graph/graph_view";
 
 const viewRegistry = registry.category("views");
 
 class ProjectGraphView extends GraphView {}
-ProjectGraphView.components = { ...GraphView.components, ControlPanel: ProjectControlPanel };
+ProjectGraphView.ControlPanel = ProjectControlPanel;
 
 viewRegistry.add("project_graph", ProjectGraphView);
