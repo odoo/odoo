1395e0e597df3b10b7f988b2e1512adad12c8a92
Xavier BOL (xbo) <xbo@odoo.com>
2021-09-28 14:19:15 +0200

    [FIX] project: remove 'Share with all users' checkbox in project sharing
    Before this commit, the portal user could see the checkbox in search
    view to share his favorite item with all users. This action is not
    authorize in project sharing feature and should be removed.
    
    This commit removes this checkbox to avoid the portal user to share his
    favorite item with others users.
    
    task-2633229
    
    closes #77156
    
    X-original-commit: 19b38074dd8bfe4d8347db31f946beeb2850d453

--- addons/project/__manifest__.py
+++ addons/project/__manifest__.py
@@ -115,6 +115,7 @@
             'web/static/src/legacy/js/public/public_widget.js',
             'portal/static/src/js/portal_chatter.js',
             'portal/static/src/js/portal_composer.js',
+            'project/static/src/project_sharing/search/favorite_menu/custom_favorite_item.xml',
             'project/static/src/project_sharing/**/*.js',
             'project/static/src/scss/project_sharing/*',
             'web/static/src/start.js',
