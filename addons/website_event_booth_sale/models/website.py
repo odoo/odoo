a08d7ff472b0f418dba01ebb2079a967383dc995
Patrick Hoste <pko@odoo.com>
2021-09-28 13:41:51 +0000

    [FIX] website_event_booth_sale: fix typo type to detailed_type
    PURPOSE
    
    A typo has been made with the following commit:
    https://github.com/odoo/odoo/commit/73c4c9233e6aa48a7df1ea3e5476395622c54e43
    This typo made the test unreachable so this commit corrects it.
    
    LINKS
    
    Task-2635461
    PR: odoo/odoo#77081

--- addons/website_event_booth_sale/models/website.py
+++ addons/website_event_booth_sale/models/website.py
@@ -8,4 +8,4 @@ class Website(models.Model):
     _inherit = 'website'
 
     def sale_product_domain(self):
-        return ['&'] + super(Website, self).sale_product_domain() + [('type', '!=', 'event_booth')]
+        return ['&'] + super(Website, self).sale_product_domain() + [('detailed_type', '!=', 'event_booth')]
