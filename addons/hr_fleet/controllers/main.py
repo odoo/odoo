819e82cb173346558f4019a1fe50b059cd545979
Chong Wang (cwg) <cwg@odoo.com>
2021-09-28 12:44:19 +0200

    change hr_fleet

--- addons/hr_fleet/controllers/main.py
+++ addons/hr_fleet/controllers/main.py
@@ -4,11 +4,12 @@
 import base64
 import io
 
-from PyPDF2 import  PdfFileReader, PdfFileWriter
 from reportlab.pdfgen import canvas
+from pikepdf import Page
 
 from odoo import _
 from odoo.http import request, route, Controller
+from odoo.tools.pdf import OdooPdf
 
 
 
@@ -28,7 +29,7 @@ class HrFleet(Controller):
             ('res_model', '=', 'fleet.vehicle.assignation.log'),
             ('res_id', 'in', car_assignation_logs.ids)], order='create_date')
 
-        writer = PdfFileWriter()
+        writer = OdooPdf.new()
 
         font = "Helvetica"
         normal_font_size = 14
@@ -36,41 +37,39 @@ class HrFleet(Controller):
         for document in doc_list:
             car_line_doc = request.env['fleet.vehicle.assignation.log'].browse(document.res_id)
             try:
-                reader = PdfFileReader(io.BytesIO(base64.b64decode(document.datas)), strict=False, overwriteWarnings=False)
+                with OdooPdf.open(io.BytesIO(base64.b64decode(document.datas))) as reader, io.BytesIO() as header:
+
+                    width = float(abs(reader.pages[0].MediaBox[2] - reader.pages[0].MediaBox[0]))
+                    height = float(abs(reader.pages[0].MediaBox[3] - reader.pages[0].MediaBox[1]))
+
+                    can = canvas.Canvas(header)
+                    can.setFont(font, normal_font_size)
+                    can.setFillColorRGB(1, 0, 0)
+
+                    car_name = car_line_doc.vehicle_id.display_name
+                    date_start = car_line_doc.date_start
+                    date_end = car_line_doc.date_end or '...'
+
+                    text_to_print = _(
+                        "%(car_name)s (driven from: %(date_start)s to %(date_end)s)",
+                        car_name=car_name,
+                        date_start=date_start,
+                        date_end=date_end
+                    )
+                    can.drawCentredString(width / 2, height - normal_font_size, text_to_print)
+                    can.save()
+                    header_pdf = OdooPdf.open(header)
+
+                    for page in reader.pages:
+                        page.add_overlay(Page(header_pdf.pages[0]))
+                    writer.pages.extend(reader.pages)
+
             except Exception:
                 continue
 
-            width = float(reader.getPage(0).mediaBox.getUpperRight_x())
-            height = float(reader.getPage(0).mediaBox.getUpperRight_y())
-
-            header = io.BytesIO()
-            can = canvas.Canvas(header)
-            can.setFont(font, normal_font_size)
-            can.setFillColorRGB(1, 0, 0)
-
-            car_name = car_line_doc.vehicle_id.display_name
-            date_start = car_line_doc.date_start
-            date_end = car_line_doc.date_end or '...'
-
-            text_to_print = _(
-                "%(car_name)s (driven from: %(date_start)s to %(date_end)s)",
-                car_name=car_name,
-                date_start=date_start,
-                date_end=date_end
-            )
-            can.drawCentredString(width / 2, height - normal_font_size, text_to_print)
-            can.save()
-            header_pdf = PdfFileReader(header, overwriteWarnings=False)
-
-            for page_number in range(0, reader.getNumPages()):
-                page = reader.getPage(page_number)
-                page.mergePage(header_pdf.getPage(0))
-                writer.addPage(page)
-
-        _buffer = io.BytesIO()
-        writer.write(_buffer)
-        merged_pdf = _buffer.getvalue()
-        _buffer.close()
+        with io.BytesIO() as _buffer:
+            writer.save(_buffer)
+            merged_pdf = _buffer.getvalue()
 
         pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', len(merged_pdf))]
 
