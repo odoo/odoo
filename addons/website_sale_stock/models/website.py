6866b0e3588ba5853392253c8916ce39bde20af3
Romain Derie <rde@odoo.com>
2021-09-28 17:17:59 +0000

    [FIX] website_sale(_stock): remove dead code
    When the `or` was introduced [1], the company_id was not required [2].
    The required was added later somewhere between 11.0 and 12.0 [3].
    
    Reaching the `or` condition was therefore not possible anymore.
    Extracted from https://github.com/odoo/odoo/pull/77308
    
    [1]: https://github.com/odoo/odoo/blame/10.0/addons/website_sale/models/sale_order.py#L350
    [2]: https://github.com/odoo/odoo/blame/10.0/addons/website/models/website.py#L165
    [3]: https://github.com/odoo/odoo/commit/3d1218f84d787e38d6358e8f5b4c26d899264df6
    
    closes odoo/odoo#77882
    
    X-original-commit: c33ba174d0bd534794e3daa7dc99f84140ba6d22
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website_sale_stock/models/website.py
+++ addons/website_sale_stock/models/website.py
@@ -10,13 +10,12 @@ class Website(models.Model):
     def _prepare_sale_order_values(self, partner, pricelist):
         self.ensure_one()
         values = super(Website, self)._prepare_sale_order_values(partner, pricelist)
-        if values['company_id']:
-            warehouse_id = (
-                self.warehouse_id and self.warehouse_id.id or
-                self.env['ir.default'].get('sale.order', 'warehouse_id', company_id=values.get('company_id')) or
-                self.env['ir.default'].get('sale.order', 'warehouse_id') or
-                self.env['stock.warehouse'].sudo().search([('company_id', '=', values['company_id'])], limit=1).id
-            )
-            if warehouse_id:
-                values['warehouse_id'] = warehouse_id
+        warehouse_id = (
+            self.warehouse_id and self.warehouse_id.id or
+            self.env['ir.default'].get('sale.order', 'warehouse_id', company_id=self.company_id.id) or
+            self.env['ir.default'].get('sale.order', 'warehouse_id') or
+            self.env['stock.warehouse'].sudo().search([('company_id', '=', self.company_id.id)], limit=1).id
+        )
+        if warehouse_id:
+            values['warehouse_id'] = warehouse_id
         return values
