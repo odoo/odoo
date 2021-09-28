cf582372cd24f78b88bb04ee1437af3462eaf4d3
Nicolas (vin) <vin@odoo.com>
2021-09-28 07:26:48 +0000

    [IMP] account: Log changes on bank statements
    With bank synchronization, having information about changes done by
    the user on the bank statements can be useful when looking into issues.
    
    We'll now log changes in the starting/ending balance as well as the
    state of the statement to keep better track of what is happening on
    the statements.
    
    Task id #2647224
    
    closes odoo/odoo#77782
    
    X-original-commit: 5416b9ef27b12b0d68dc88ed640cde907b6432c7
    Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>

--- addons/account/models/account_bank_statement.py
+++ addons/account/models/account_bank_statement.py
@@ -214,9 +214,9 @@ class AccountBankStatement(models.Model):
     reference = fields.Char(string='External Reference', states={'open': [('readonly', False)]}, copy=False, readonly=True, help="Used to hold the reference of the external mean that created this statement (name of imported file, reference of online synchronization...)")
     date = fields.Date(required=True, states={'confirm': [('readonly', True)]}, index=True, copy=False, default=fields.Date.context_today)
     date_done = fields.Datetime(string="Closed On")
-    balance_start = fields.Monetary(string='Starting Balance', states={'confirm': [('readonly', True)]}, compute='_compute_starting_balance', readonly=False, store=True)
-    balance_end_real = fields.Monetary('Ending Balance', states={'confirm': [('readonly', True)]}, compute='_compute_ending_balance', readonly=False, store=True)
-    state = fields.Selection(string='Status', required=True, readonly=True, copy=False, selection=[
+    balance_start = fields.Monetary(string='Starting Balance', states={'confirm': [('readonly', True)]}, compute='_compute_starting_balance', readonly=False, store=True, tracking=True)
+    balance_end_real = fields.Monetary('Ending Balance', states={'confirm': [('readonly', True)]}, compute='_compute_ending_balance', readonly=False, store=True, tracking=True)
+    state = fields.Selection(string='Status', required=True, readonly=True, copy=False, tracking=True, selection=[
             ('open', 'New'),
             ('posted', 'Processing'),
             ('confirm', 'Validated'),
