ac0d7b5de4bb5a88c65bbefcf7eb99c52fea4651
Borja Riera <briera@gsmep.com>
2021-09-30 18:51:09 +0200

    odoo custom addons - módulo prova

---
+++ odoo_custom_addons/prova/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "prova",
+
+    'summary': """
+        Add customer field on partner""",
+
+    'description': """
+         Add customer field on partner
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
