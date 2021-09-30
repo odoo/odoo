56167deddd793bb3fe5b628bd85f78bf945ffb7e
Romain Derie <rde@odoo.com>
2021-09-30 15:41:54 +0000

    [FIX] website_sale_stock: always select a warehouse
    In multi-warehouses, the available quantities shown on product page is not the
    same one used to compute the max quantity that can be bought in the checkout.
    
    Indeed, a sale order has a required `warehouse_id` field, and the quantity that
    can be bought is computed based on that warehouse.
    But if the website has no warehouse set to it (in settings), the shown quantity
    on product page will be computed based on all available warehouses.
    
    It will then show an error on payment step, if the user has selected more
    quantities than there is available on the warehouse used for his order.
    
    Considering the structure of `sale.order`, displaying the quantities in
    several warehouses does not make sense.
    
    To reproduce the issue:
    1. In Settings:
        - Enable 'Multi-Warehouses'
        - Make sure the option Website > Inventory > Warehouse in undefined
    2. Create a second warehouse WH02
    3. Update the quantity of "Storage Box":
        - 18 in first WH (should already be present)
        - 12 in WH02
    4. Edit "Storage Box":
        - eCommerce > Availability: "Show inventory on website and prevent
    sales if not enough stock"
    5. On the eShop, go on "Storage Box"
        - Note that there are 30 units available
    6. Add 22 "Storage Box" to the cart
    7. Process checkout
    
    Error: When trying to pay, a Server Error is raised: "We are not able to
    redirect you to the payment form. You ask for 22.0 products but only
    18.0 is available." This is confusing since the available quantity on
    the product page is 30
    
    opw-2630804
    
    X-original-commit: ee2d1c31bb0f6ee7c52b84b6c9ee5ca2d8e70e03
    Part-of: odoo/odoo#78045
    Co-authored-by: Adrien Widart <awt@odoo.com>
    Co-authored-by: Romain Derie <rde@odoo.com>

--- addons/website_sale_stock/models/website.py
+++ addons/website_sale_stock/models/website.py
@@ -10,12 +10,16 @@ class Website(models.Model):
     def _prepare_sale_order_values(self, partner, pricelist):
         self.ensure_one()
         values = super(Website, self)._prepare_sale_order_values(partner, pricelist)
-        warehouse_id = (
+        if values['company_id']:
+            warehouse_id = self._get_warehouse_available()
+            if warehouse_id:
+                values['warehouse_id'] = warehouse_id
+        return values
+
+    def _get_warehouse_available(self):
+        return (
             self.warehouse_id and self.warehouse_id.id or
             self.env['ir.default'].get('sale.order', 'warehouse_id', company_id=self.company_id.id) or
             self.env['ir.default'].get('sale.order', 'warehouse_id') or
             self.env['stock.warehouse'].sudo().search([('company_id', '=', self.company_id.id)], limit=1).id
         )
-        if warehouse_id:
-            values['warehouse_id'] = warehouse_id
-        return values
