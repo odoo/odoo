c8363b7bb74ace4e77b593efc3a9104143240141
std-odoo <std@odoo.com>
2021-09-30 10:53:36 +0000

    [FIX] link_tracker: add a system parameter to disabled the tracking
    Bug
    ===
    If you send a URL (which will be transformed into a link tracker) to
    an external website (e.g. in mass mailing, social, ...), the UTM values
    will be added in the GET parameters of the URL.
    
    The external website might crash if those parameters are not supported
    by it.
    
    We want to add a system parameter `link_tracker.no_external_tracking`
    to be able to not add those UTM values in the redirected URL.
    
    This is done only for external website, because it will never be an
    issue if the URL redirect to Odoo.
    
    Task-2657413
    
    closes odoo/odoo#78594
    
    X-original-commit: 0b3538e50b4a5c3d9b699d5b2349cb5188d88c88
    Signed-off-by: awa-odoo <awa-odoo@users.noreply.github.com>

--- addons/link_tracker/models/link_tracker.py
+++ addons/link_tracker/models/link_tracker.py
@@ -82,8 +82,21 @@ class LinkTracker(models.Model):
 
     @api.depends('url')
     def _compute_redirected_url(self):
+        """Compute the URL to which we will redirect the user.
+
+        By default, add UTM values as GET parameters. But if the system parameter
+        `link_tracker.no_external_tracking` is set, we add the UTM values in the URL
+        *only* for URLs that redirect to the local website (base URL).
+        """
+        no_external_tracking = self.env['ir.config_parameter'].sudo().get_param('link_tracker.no_external_tracking')
+
         for tracker in self:
+            base_domain = urls.url_parse(tracker.get_base_url()).netloc
             parsed = urls.url_parse(tracker.url)
+            if no_external_tracking and parsed.netloc and parsed.netloc != base_domain:
+                tracker.redirected_url = parsed.to_url()
+                continue
+
             utms = {}
             for key, field_name, cook in self.env['utm.mixin'].tracking_fields():
                 field = self._fields[field_name]
