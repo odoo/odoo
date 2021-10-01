a08b384144e6758ef72a8c4c346dd48a7c341677
William Braeckman <wbr@odoo.com>
2021-10-01 10:26:10 +0000

    [ADD] l10n_fr_hr_holidays: add french time off for part timers
    Adds 2 new modules:
    l10n_fr_hr_holidays
    l10n_fr_hr_work_entry_holidays
    
    This commit adds modules allowing to compute accurately
    the number of days off taken by a french part-timer.
    
    Both modules will automatically install when hr_holidays is installed and
    at least one of the companies is located in France
    
    In France, when taking a time off using a different calendar from the
    company's calendar you have to count the days as if you were taking time
    off in the company calendar.
    
    For example let's say you have an employee working only monday through
    wednesday, if that employee were to take a time off ending with
    wednesday afternoon, the time off would count up until friday (if the
    company calendar if monday->friday), so that a whole week is always 5
    days regardless of the employee calendar.
    
    This is also the case for 'holes' in the employee calendar, for example
    a calendar where one would work monday, wednesday and friday only.
    
    l10n_fr_work_entry_holidays:
    By default time off only generate work entries for intersections between
    time off and attendance period, so that when taking a time off for a
    whole month you wouldn't get work entries on the weekend if you normally
    don't work that day.
    
    But since l10n_fr_hr_holidays computes the number of days it would
    normally cost with french rules. We also have to make sure that the work
    entries are generated appropriately. Essentially this modules adds the
    logic required to fill the holes needed using the company calendar
    instead of the employee one.
    
    OTHER:
    Fixes a redundant call to super function in hr.leave._get_number_of_days,
    improving its performance a bit
    
    task-2602897
    
    closes odoo/odoo#82637
    
    Related: odoo/enterprise#25805
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Co-authored-by: Bertrand Dossogne <bedo@odoo.com>

--- addons/hr_holidays/__manifest__.py
+++ addons/hr_holidays/__manifest__.py
@@ -82,5 +82,6 @@ A synchronization with an internal agenda (Meetings of the CRM module) is also p
             '/hr_holidays/static/tests/tours/**/*'
         ],
     },
+    'post_init_hook': '_hr_holiday_post_init',
     'license': 'LGPL-3',
 }
