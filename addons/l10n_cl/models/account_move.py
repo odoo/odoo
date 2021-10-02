03b620e61b8010200a17fa57d7dea39ef7b85a99
Daniel Blanco <daniel@blancomartin.cl>
2021-10-02 01:15:57 +0000

    [I18N] l10n_cl: relax the condition for registering entries in purchase journal, in order to allow to record expenses without warnings.
    X-original-commit: c1e7b1ad2156752e6d2878c3650683f2e57f8a1d

--- addons/l10n_cl/models/account_move.py
+++ addons/l10n_cl/models/account_move.py
@@ -85,10 +85,6 @@ class AccountMove(models.Model):
                 if tax_payer_type == '4' or country_id.code != "CL":
                     raise ValidationError(_('You need a journal without the use of documents for foreign '
                                             'suppliers'))
-            if rec.journal_id.type == 'purchase' and not rec.journal_id.l10n_latam_use_documents:
-                if tax_payer_type != '4':
-                    raise ValidationError(_('This supplier should be defined as foreigner tax payer type and '
-                                            'the country should be different from Chile to register purchases.'))
 
     @api.onchange('journal_id')
     def _l10n_cl_onchange_journal(self):
