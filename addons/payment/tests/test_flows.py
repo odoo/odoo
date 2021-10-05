odoo/odoo

commit 7fccbac004628093da49016f75370a84bba49465

[IMP] payment, website_payment: perform the donation payment
This commit adds /donation/* routes to perform the donation:
- /pay: shows the form with the contact details and donation details
- /transaction: creates the partner_id if missing and executes the
transaction
- /confirm: displays the payment success page

PR-63133

task-2398403

Part-of: odoo/odoo#63133

addons/payment/controllers/portal.py
@@ -142,7 +142,10 @@ class PaymentPortal(portal.CustomerPortal):
             'invoice_id': invoice_id,
             **self._get_custom_rendering_context_values(**kwargs),
         }
-        return request.render('payment.pay', rendering_context)
+        return request.render(self._get_payment_page_template_xmlid(**kwargs), rendering_context)
+
+    def _get_payment_page_template_xmlid(self, **kwargs):
+        return 'payment.pay'
 
     @http.route('/my/payment_method', type='http', methods=['GET'], auth='user', website=True)
     def payment_method(self, **kwargs):
@@ -205,16 +208,9 @@ class PaymentPortal(portal.CustomerPortal):
         tx_sudo = self._create_transaction(
             amount=amount, currency_id=currency_id, partner_id=partner_id, **kwargs
         )
-
-        # The generic landing route require the tx id and access token to be provided, since there
-        # is no document to rely on. The access token is recomputed in case we are dealing with a
-        # validation transaction (acquirer-specific amount and currency).
-        access_token = payment_utils.generate_access_token(
-            tx_sudo.partner_id.id, tx_sudo.amount, tx_sudo.currency_id.id
-        )
-        tx_sudo.landing_route = f'{tx_sudo.landing_route}' \
-                                f'?tx_id={tx_sudo.id}&access_token={access_token}'
-
+        if tx_sudo.operation == 'validation':
+            # The amount and currency have been chosen for the validation, recompute the access token
+            self._update_landing_route(tx_sudo)
         return tx_sudo._get_processing_values()
 
     def _create_transaction(
@@ -307,6 +303,24 @@ class PaymentPortal(portal.CustomerPortal):
         PaymentPostProcessing.monitor_transactions(tx_sudo)
 
         return tx_sudo
+
+    @staticmethod
+    def _update_landing_route(tx_sudo):
+        """ Recompute the access token stored in the landing route of validation transactions.
+
+        The generic landing route require the tx id and access token to be provided, since there is
+        no document to rely on. The access token is recomputed in case we are dealing with a
+        validation transaction (acquirer-specific amount and currency).
+
+        :param recordset tx_sudo: The transaction whose landing routes to update, as a
+                                  `payment.transaction` record.
+        :return: None
+        """
+        access_token = payment_utils.generate_access_token(
+            tx_sudo.partner_id.id, tx_sudo.amount, tx_sudo.currency_id.id
+        )
+        tx_sudo.landing_route = f'{tx_sudo.landing_route}' \
+                                f'?tx_id={tx_sudo.id}&access_token={access_token}'
 
     @http.route('/payment/confirmation', type='http', methods=['GET'], auth='public', website=True)
     def payment_confirm(self, tx_id, access_token, **kwargs):

addons/website_payment/controllers/portal.py
@@ -1,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http, _
+from odoo.exceptions import ValidationError
+from odoo.http import request
+from odoo.tools.json import scriptsafe as json_safe
+
+from odoo.addons.payment import utils as payment_utils
+from odoo.addons.payment.controllers import portal as payment_portal
+
+
+class PaymentPortal(payment_portal.PaymentPortal):
+    @http.route('/donation/pay', type='http', methods=['GET', 'POST'], auth='public', website=True, sitemap=False)
+    def donation_pay(self, **kwargs):
+        """ Behaves like PaymentPortal.payment_pay but for donation
+
+        :param dict kwargs: As the parameters of in payment_pay, with the additional:
+            - str donation_options: The options settled in the donation snippet
+            - str donation_descriptions: The descriptions for all prefilled amounts
+        :return: The rendered donation form
+        :rtype: str
+        :raise: werkzeug.exceptions.NotFound if the access token is invalid
+        """
+        kwargs['is_donation'] = True
+        kwargs['currency_id'] = int(kwargs.get('currency', request.env.company.currency_id.id))
+        kwargs['amount'] = float(kwargs.get('amount', 25))
+        kwargs['donation_options'] = kwargs.get('donation_options', json_safe.dumps(dict(customAmount="freeAmount")))
+
+        if request.env.user._is_public():
+            kwargs['partner_id'] = request.env.user.partner_id.id
+            kwargs['access_token'] = payment_utils.generate_access_token(kwargs['partner_id'], kwargs['amount'], kwargs['currency_id'])
+
+        return self.payment_pay(**kwargs)
+
+    @http.route('/donation/transaction/<minimum_amount>', type='json', auth='public', website=True, sitemap=False)
+    def donation_transaction(self, amount, currency_id, partner_id, access_token, minimum_amount=0, **kwargs):
+        if float(amount) < float(minimum_amount):
+            raise ValidationError(_('Donation amount must be at least %.2f.', float(minimum_amount)))
+        use_public_partner = request.env.user._is_public() or not partner_id
+        if use_public_partner:
+            details = kwargs['partner_details']
+            if not details.get('name'):
+                raise ValidationError(_('Name is required.'))
+            if not details.get('email'):
+                raise ValidationError(_('Email is required.'))
+            if not details.get('country_id'):
+                raise ValidationError(_('Country is required.'))
+            partner_id = request.website.user_id.partner_id.id
+            del kwargs['partner_details']
+
+        kwargs.pop('custom_create_values', None)  # Don't allow passing arbitrary create values
+        tx_sudo = self._create_transaction(
+            amount=amount, currency_id=currency_id, partner_id=partner_id, **kwargs
+        )
+        tx_sudo.is_donation = True
+        if use_public_partner:
+            tx_sudo.update({
+                'partner_name': details['name'],
+                'partner_email': details['email'],
+                'partner_country_id': details['country_id'],
+            })
+        elif not tx_sudo.partner_country_id:
+            tx_sudo.partner_country_id = kwargs['partner_details']['country_id']
+        self._update_landing_route(tx_sudo)
+
+        # Send a notification to warn that a donation has been made
+        recipient_email = kwargs['donation_recipient_email']
+        comment = kwargs['donation_comment']
+        tx_sudo._send_donation_email(True, comment, recipient_email)
+
+        return tx_sudo._get_processing_values()
+
+    def _get_custom_rendering_context_values(self, donation_options=None, donation_descriptions=None, is_donation=False, **kwargs):
+        rendering_context = super()._get_custom_rendering_context_values(**kwargs)
+        if is_donation:
+            user_sudo = request.env.user
+            logged_in = not user_sudo._is_public()
+            # If the user is logged in, take their partner rather than the partner set in the params.
+            # This is something that we want, since security rules are based on the partner, and created
+            # tokens should not be assigned to the public user. This should have no impact on the
+            # transaction itself besides making reconciliation possibly more difficult (e.g. The
+            # transaction and invoice partners are different).
+            partner_sudo = user_sudo.partner_id
+            partner_details = {}
+            countries = request.env['res.country']
+            if logged_in:
+                partner_details = {
+                    'name': partner_sudo.name,
+                    'email': partner_sudo.email,
+                    'country_id': partner_sudo.country_id.id,
+                }
+
+            countries = request.env['res.country'].sudo().search([])
+            descriptions = request.httprequest.form.getlist('donation_descriptions')
+
+            donation_options = json_safe.loads(donation_options) if donation_options else {}
+            donation_amounts = json_safe.loads(donation_options.get('donationAmounts', '[]'))
+
+            rendering_context.update({
+                'is_donation': True,
+                'partner': partner_sudo,
+                'transaction_route': '/donation/transaction/%s' % donation_options.get('minimumAmount', 0),
+                'partner_details': partner_details,
+                'error': {},
+                'countries': countries,
+                'donation_options': donation_options,
+                'donation_amounts': donation_amounts,
+                'donation_descriptions': descriptions,
+            })
+        return rendering_context
+
+    def _get_payment_page_template_xmlid(self, **kwargs):
+        if kwargs.get('is_donation'):
+            return 'website_payment.donation_pay'
+        return super()._get_payment_page_template_xmlid(**kwargs)

addons/website_payment/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import controllers
 from . import models

addons/website_payment/__manifest__.py
@@ -14,7 +14,9 @@ This is a bridge module that adds multi-website support for payment acquirers.
         'portal',
     ],
     'data': [
+        'data/donation_data.xml',
         'views/payment_acquirer.xml',
+        'views/donation_templates.xml',
         'views/snippets/snippets.xml',
         'views/snippets/s_donation.xml',
     ],
@@ -22,6 +24,10 @@ This is a bridge module that adds multi-website support for payment acquirers.
     'assets': {
         'website.assets_wysiwyg': [
             'website_payment/static/src/snippets/s_donation/options.js',
+        ],
+        'web.assets_frontend': [
+            'website_payment/static/src/js/website_payment_donation.js',
+            'website_payment/static/src/js/website_payment_form.js',
         ],
     },
     'license': 'LGPL-3',

addons/website_payment/controllers/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import account_payment
-from . import payment_acquirer
-from . import payment_transaction
+from . import portal

addons/website_payment/data/donation_data.xml
@@ -1,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<odoo>
+    <data noupdate="1">
+        <record id="mail_template_donation" model="mail.template">
+            <field name="name">Donation</field>
+            <field name="model_id" ref="payment.model_payment_transaction" />
+            <field name="lang">${object.partner_id.lang}</field>
+        </record>
+    </data>
+
+    <template id="website_payment.donation_mail_body" name="Donation mail">
+        <table border="0" cellpadding="0" style="background-color: white; padding: 0px; border-collapse:separate;">
+            <tr style="height: 48px;"><td valign="top"><span style="font-size: 24px; font-weight: bold;">
+                <t t-if="is_internal_notification">Donation notification</t>
+                <t t-else="">Donation</t>
+            </span></td></tr>
+            <t t-if="not is_internal_notification">
+                <tr><td valign="top">
+                    Dear <t t-out="tx.partner_name"/>,
+                </td></tr>
+                <tr><td valign="top">
+                    <div style="margin: 16px 0px 16px 0px;">
+                        Thank you for your donation of <span t-out="tx.amount" t-options="{'widget': 'monetary', 'display_currency': tx.currency_id}"/> made on <t t-out="tx.create_date" t-options="{'widget': 'date'}"/>.
+                        <br/>
+                        We appreciate your support for our organization as such.
+                        <br/>
+                        Regards.
+                    </div>
+                </td></tr>
+            </t>
+            <tr><td valign="top">
+                <div style="margin: 16px 0px 16px 0px;">
+                    <table border="0" cellpadding="0" cellspacing="5" width="100%">
+                        <tr>
+                            <td><b>Donor Name:</b></td>
+                            <td><t t-out="tx.partner_name"/></td>
+                        </tr>
+                        <tr>
+                            <td><b>Donor Email:</b></td>
+                            <td><t t-out="tx.partner_email"/></td>
+                        </tr>
+                        <tr>
+                            <td><b>Donation Date:</b></td>
+                            <td><t t-out="tx.create_date.date()"/></td>
+                        </tr>
+                        <tr>
+                            <td><b>Amount(<t t-out="tx.currency_id.symbol"/>):</b></td>
+                            <td><t t-out="tx.amount"/></td>
+                        </tr>
+                        <tr t-if="is_internal_notification and comment">
+                            <td><b>Comment:</b></td>
+                            <td><t t-out="comment"/></td>
+                        </tr>
+                        <tr>
+                            <td><b>Payment Method:</b></td>
+                            <td><t t-out="tx.provider"/></td>
+                        </tr>
+                        <tr>
+                            <td><b>Payment ID:</b></td>
+                            <td><t t-out="tx.reference"/></td>
+                        </tr>
+                    </table>
+                </div>
+            </td></tr>
+        </table>
+    </template>
+</odoo>

addons/website_payment/models/account_payment.py
@@ -1,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    is_donation = fields.Boolean(string="Is Donation", related="payment_transaction_id.is_donation", help="Is the payment a donation")

addons/website_payment/models/payment_transaction.py
@@ -1,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import _, fields, models
+
+
+class PaymentTransaction(models.Model):
+    _inherit = "payment.transaction"
+
+    is_donation = fields.Boolean(string="Is donation", help="Is the payment a donation")
+
+    def _finalize_post_processing(self):
+        super()._finalize_post_processing()
+        for tx in self.filtered('is_donation'):
+            tx._send_donation_email()
+            msg = [_('Payment received from donation with following details:')]
+            for field in ['company_id', 'partner_id', 'partner_name', 'partner_country_id', 'partner_email']:
+                field_name = tx._fields[field].string
+                value = tx[field]
+                if value:
+                    if 'name' in value:
+                        value = value.name
+                    msg.append('<br/>- %s: %s' % (field_name, value))
+            tx.payment_id._message_log(body=''.join(msg))
+
+    def _send_donation_email(self, is_internal_notification=False, comment=None, recipient_email=None):
+        self.ensure_one()
+        if is_internal_notification or self.state == 'done':
+            subject = _('A donation has been made on your website') if is_internal_notification else _('Donation confirmation')
+            body = self.env.ref('website_payment.donation_mail_body')._render({
+                'is_internal_notification': is_internal_notification,
+                'tx': self,
+                'comment': comment,
+            }, engine='ir.qweb', minimal_qcontext=True)
+            self.env.ref('website_payment.mail_template_donation').send_mail(
+                self.id, notif_layout="mail.mail_notification_light",
+                force_send=True,
+                email_values={
+                    'email_to': recipient_email if is_internal_notification else self.partner_email,
+                    'email_from': self.company_id.email_formatted,
+                    'author_id': self.partner_id.id,
+                    'subject': subject,
+                    'body_html': body,
+                },
+            )

commit d53f9f631a6d3ca8b87b1b7c7d890412eb0b0b19

[FIX] payment: add req param to the landing route
When a payment is made, add required parameters to the landing route.

This issue was introduced with 7fccbac

task-2645216

Co-authored-by: Victor Feyens<vfe@odoo.com>

addons/payment/controllers/portal.py
@@ -208,9 +208,7 @@ class PaymentPortal(portal.CustomerPortal):
         tx_sudo = self._create_transaction(
             amount=amount, currency_id=currency_id, partner_id=partner_id, **kwargs
         )
-        if tx_sudo.operation == 'validation':
-            # The amount and currency have been chosen for the validation, recompute the access token
-            self._update_landing_route(tx_sudo)
+        self._update_landing_route(tx_sudo, access_token)  # Add the required parameters to the route
         return tx_sudo._get_processing_values()
 
     def _create_transaction(
@@ -305,20 +303,22 @@ class PaymentPortal(portal.CustomerPortal):
         return tx_sudo
 
     @staticmethod
-    def _update_landing_route(tx_sudo):
-        """ Recompute the access token stored in the landing route of validation transactions.
+    def _update_landing_route(tx_sudo, access_token):
+        """ Add the mandatory parameters to the route and recompute the access token if needed.
 
-        The generic landing route require the tx id and access token to be provided, since there is
+        The generic landing route requires the tx id and access token to be provided since there is
         no document to rely on. The access token is recomputed in case we are dealing with a
         validation transaction (acquirer-specific amount and currency).
 
         :param recordset tx_sudo: The transaction whose landing routes to update, as a
                                   `payment.transaction` record.
+        :param str access_token: The access token used to authenticate the partner
         :return: None
         """
-        access_token = payment_utils.generate_access_token(
-            tx_sudo.partner_id.id, tx_sudo.amount, tx_sudo.currency_id.id
-        )
+        if tx_sudo.operation == 'validation':
+            access_token = payment_utils.generate_access_token(
+                tx_sudo.partner_id.id, tx_sudo.amount, tx_sudo.currency_id.id
+            )
         tx_sudo.landing_route = f'{tx_sudo.landing_route}' \
                                 f'?tx_id={tx_sudo.id}&access_token={access_token}'
 

addons/website_payment/controllers/portal.py
@@ -75,7 +75,7 @@ class PaymentPortal(payment_portal.PaymentPortal):
             })
         elif not tx_sudo.partner_country_id:
             tx_sudo.partner_country_id = kwargs['partner_details']['country_id']
-        self._update_landing_route(tx_sudo)
+        self._update_landing_route(tx_sudo, access_token)
 
         # Send a notification to warn that a donation has been made
         recipient_email = kwargs['donation_recipient_email']

addons/payment/tests/test_flows.py
@@ -59,12 +59,17 @@ class TestFlows(PaymentCommon, PaymentHttpCommon):
         self.assertEqual(tx_sudo.currency_id.id, self.currency.id)
         self.assertEqual(tx_sudo.partner_id.id, self.partner.id)
         self.assertEqual(tx_sudo.reference, self.reference)
+
         # processing_values == given values
         self.assertEqual(processing_values['acquirer_id'], self.acquirer.id)
         self.assertEqual(processing_values['amount'], self.amount)
         self.assertEqual(processing_values['currency_id'], self.currency.id)
         self.assertEqual(processing_values['partner_id'], self.partner.id)
         self.assertEqual(processing_values['reference'], self.reference)
+
+        # Verify computed values not provided, but added during the flow
+        self.assertIn("tx_id=", tx_sudo.landing_route)
+        self.assertIn("access_token=", tx_sudo.landing_route)
 
         if flow == 'redirect':
             # In redirect flow, we verify the rendering of the dummy test form