odoo/odoo

commit 2a7436d83abe178bd21c0694edc9825f5d70d65f

[FIX] web_editor: properly display the state of list in the toolbar
When selecting text in a list, the toolbar should show that a list is
selected, and which type. It however failed to do that, which is fixed
with this commit.

task-2638422

closes odoo/odoo#77690

Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -1422,6 +1422,7 @@ export class OdooEditor extends EventTarget {
             }
         }
         this.updateColorpickerLabels();
+        const listUIClasses = {UL: 'fa-list-ul', OL: 'fa-list-ol', CL: 'fa-tasks'};
         const block = closestBlock(sel.anchorNode);
         for (const [style, tag, isList] of [
             ['paragraph', 'P', false],
@@ -1446,6 +1447,15 @@ export class OdooEditor extends EventTarget {
                     : block.tagName === tag;
                 button.classList.toggle('active', isActive);
             }
+        }
+        const listMode = getListMode(block.parentElement);
+        const listDropdownButton = this.toolbar.querySelector('#listDropdownButton');
+        if (listDropdownButton) {
+            if (listMode) {
+                listDropdownButton.classList.remove('fa-list-ul', 'fa-list-ol', 'fa-tasks');
+                listDropdownButton.classList.add(listUIClasses[listMode]);
+            }
+            listDropdownButton.closest('button').classList.toggle('active', block.tagName === 'LI');
         }
         const linkNode = getInSelection(this.document, 'a');
         const linkButton = this.toolbar.querySelector('#createLink');

addons/web_editor/static/src/xml/editor.xml
@@ -118,7 +118,7 @@
                 <button type="button" class="btn dropdown-toggle"
                     data-toggle="dropdown" title="Toggle List" tabindex="-1"
                     data-original-title="List" aria-expanded="false">
-                    <i id="justifyDropdownButton" class="fa fa-list-ul fa-fw"></i>
+                    <i id="listDropdownButton" class="fa fa-list-ul fa-fw"></i>
                 </button>
                 <div class="dropdown-menu">
                     <div class="btn-group">

commit 5905097beb12f22c0935e1da022f7dae99d81887

[FIX] web_editor: fix traceback when clicking outside any snippet
This bug was introduced by
https://github.com/odoo/odoo/commit/2a7436d83abe178bd21c0694edc9825f5d70d65f.

addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -1448,14 +1448,16 @@ export class OdooEditor extends EventTarget {
                 button.classList.toggle('active', isActive);
             }
         }
-        const listMode = getListMode(block.parentElement);
-        const listDropdownButton = this.toolbar.querySelector('#listDropdownButton');
-        if (listDropdownButton) {
-            if (listMode) {
-                listDropdownButton.classList.remove('fa-list-ul', 'fa-list-ol', 'fa-tasks');
-                listDropdownButton.classList.add(listUIClasses[listMode]);
+        if (block) {
+            const listMode = getListMode(block.parentElement);
+            const listDropdownButton = this.toolbar.querySelector('#listDropdownButton');
+            if (listDropdownButton) {
+                if (listMode) {
+                    listDropdownButton.classList.remove('fa-list-ul', 'fa-list-ol', 'fa-tasks');
+                    listDropdownButton.classList.add(listUIClasses[listMode]);
+                }
+                listDropdownButton.closest('button').classList.toggle('active', block.tagName === 'LI');
             }
-            listDropdownButton.closest('button').classList.toggle('active', block.tagName === 'LI');
         }
         const linkNode = getInSelection(this.document, 'a');
         const linkButton = this.toolbar.querySelector('#createLink');