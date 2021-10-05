4184f11aa122959f46ad16046e5ecea691b2ba53
Aurélien Warnon <awa@odoo.com>
2021-10-05 12:52:20 +0000

    [MOV] website_slides: move slide.embed model into its own file
    As we are going to do some rework on the slide.embed feature, let's put the
    model into its own python file.
    
    Task-2654009
    
    Part-of: odoo/odoo#77736

--- addons/website_slides/models/slide_slide.py
+++ addons/website_slides/models/slide_slide.py
@@ -56,30 +56,6 @@ class SlidePartnerRelation(models.Model):
             ('partner_id', 'in', self.partner_id.ids),
         ])._recompute_completion()
 
-class EmbeddedSlide(models.Model):
-    """ Embedding in third party websites. Track view count, generate statistics. """
-    _name = 'slide.embed'
-    _description = 'Embedded Slides View Counter'
-    _rec_name = 'slide_id'
-
-    slide_id = fields.Many2one('slide.slide', string="Presentation", required=True, index=True)
-    url = fields.Char('Third Party Website URL', required=True)
-    count_views = fields.Integer('# Views', default=1)
-
-    def _add_embed_url(self, slide_id, url):
-        baseurl = urls.url_parse(url).netloc
-        if not baseurl:
-            return 0
-        embeds = self.search([('url', '=', baseurl), ('slide_id', '=', int(slide_id))], limit=1)
-        if embeds:
-            embeds.count_views += 1
-        else:
-            embeds = self.create({
-                'slide_id': slide_id,
-                'url': baseurl,
-            })
-        return embeds.count_views
-
 
 class SlideTag(models.Model):
     """ Tag to search slides across channels. """
