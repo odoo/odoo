acc95ec204baa1dddbe292c379a1768fe1deccbf
Victor Feyens <vfe@odoo.com>
2021-10-05 14:03:41 +0000

    [FIX] *: target v15.0 documentation with doc links
    Part-of: odoo/odoo#77872

--- addons/web/static/src/webclient/user_menu/user_menu_items.js
+++ addons/web/static/src/webclient/user_menu/user_menu_items.js
@@ -7,7 +7,7 @@ import { _lt } from "../../core/l10n/translation";
 import { session } from "@web/session";
 
 function documentationItem(env) {
-    const documentationURL = "https://www.odoo.com/documentation/14.0";
+    const documentationURL = "https://www.odoo.com/documentation/15.0";
     return {
         type: "item",
         id: "documentation",
--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -981,7 +981,7 @@ class Users(models.Model):
                     "and *might* be a proxy. If your Odoo is behind a proxy, "
                     "it may be mis-configured. Check that you are running "
                     "Odoo in Proxy Mode and that the proxy is properly configured, see "
-                    "https://www.odoo.com/documentation/14.0/administration/deployment/deploy.html#https for details.",
+                    "https://www.odoo.com/documentation/15.0/administration/install/deploy.html#https for details.",
                     source
                 )
             raise AccessDenied(_("Too many login failures, please wait a bit before trying again."))
