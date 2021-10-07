62df2870968547da761a6af1ac52b5ecc3b0c3f5
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2021-10-07 19:31:18 +0000

    [FIX] sale: use correct company when retrieving price
    Have two taxes included in price in two different companies
    Have a product with both taxes
    Have the 2 companies selected
    Make a SO and add the product
    
    The price on the SO will add both taxes rather than just 1
    
    opw-2657752
    
    closes odoo/odoo#78032
    
    Signed-off-by: agr-odoo <agr-odoo@users.noreply.github.com>

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -1601,7 +1601,8 @@ class SaleOrderLine(models.Model):
         self._compute_tax_id()
 
         if self.order_id.pricelist_id and self.order_id.partner_id:
-            vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+            company = self.company_id or self.order_id.company_id
+            vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, company)
         self.update(vals)
 
         title = False
@@ -1634,7 +1635,8 @@ class SaleOrderLine(models.Model):
                 uom=self.product_uom.id,
                 fiscal_position=self.env.context.get('fiscal_position')
             )
-            self.price_unit = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+            company = self.company_id or self.order_id.company_id
+            self.price_unit = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, company)
 
     def name_get(self):
         result = []
