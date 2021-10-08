aa2d0abb1b2a94a4df50e97453f29b33c6a33308
JF Aubert <ajf@odoo.com>
2021-10-08 09:48:17 +0000

    [FIX] mrp_subcontracting: Allow to create backorder for tracked products
    This commit fixes the 'You need to supply a Lot/Serial Number...' message
    when validating a receipt with backorder for subcontracted tracked products.
    
    closes odoo/odoo#79121
    
    Task: 2604664
    X-original-commit: 2ab8f5cc681d45323e540ca57f85168fd587cf02
    Signed-off-by: Tiffany Chang <tic@odoo.com>

--- addons/mrp_subcontracting/models/mrp_production.py
+++ addons/mrp_subcontracting/models/mrp_production.py
@@ -41,6 +41,8 @@ class MrpProduction(models.Model):
             raise UserError(_("This MO isn't related to a subcontracted move"))
         if float_is_zero(self.qty_producing, precision_rounding=self.product_uom_id.rounding):
             return {'type': 'ir.actions.act_window_close'}
+        if self.product_tracking != 'none' and not self.lot_producing_id:
+            raise UserError(_('You must enter a serial number for %s') % self.product_id.name)
         for sml in self.move_raw_ids.move_line_ids:
             if sml.tracking != 'none' and not sml.lot_id:
                 raise UserError(_('You must enter a serial number for each line of %s') % sml.product_id.name)
@@ -65,6 +67,11 @@ class MrpProduction(models.Model):
             action = self._get_subcontract_move().filtered(lambda m: m.state not in ('done', 'cancel'))._action_record_components()
             action['res_id'] = backorder.id
             return action
+        return {'type': 'ir.actions.act_window_close'}
+
+    def action_subcontracting_discard_remaining_components(self):
+        self.ensure_one()
+        self.qty_producing = 0
         return {'type': 'ir.actions.act_window_close'}
 
     def _pre_button_mark_done(self):
@@ -141,7 +148,7 @@ class MrpProduction(models.Model):
                 return False
             if not all(line.lot_id for line in mo.move_raw_ids.filtered(lambda sm: sm.has_tracking != 'none').move_line_ids):
                 return False
-            if mo.product_id.tracking != 'none' and not mo.lot_producing_id:
+            if mo.product_tracking != 'none' and not mo.lot_producing_id:
                 return False
             return True
 
