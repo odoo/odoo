7d7fd21f991845b7c2ac8acfd65e5c4e7a787687
Martin Trigaux <mat@odoo.com>
2021-10-08 06:23:21 +0000

    [FIX] mass_mailing: avoid Expected singleton error
    When writing on more than one record (e.g. via
    _process_mass_mailing_queue cron)
    
    X-original-commit: b18614bf99df47de77650f431e2062b8d2eacd06
    Part-of: odoo/odoo#78259

--- addons/mass_mailing/models/mailing.py
+++ addons/mass_mailing/models/mailing.py
@@ -405,9 +405,10 @@ class MassMailing(models.Model):
 
         result = super(MassMailing, self).write(values)
 
-        if self.ab_testing_schedule_datetime:
+        if any(self.mapped('ab_testing_schedule_datetime')):
+            schedule_date = min(m.ab_testing_schedule_datetime for m in self if m.ab_testing_schedule_datetime)
             ab_testing_cron = self.env.ref('mass_mailing.ir_cron_mass_mailing_ab_testing').sudo()
-            ab_testing_cron._trigger(at=self.ab_testing_schedule_datetime)
+            ab_testing_cron._trigger(at=schedule_date)
 
         return result
 
