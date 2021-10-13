968be6be5530bc79d6f93b1ef7a02c6f46122ebe
Pierre Paridans <app@odoo.com>
2021-10-13 14:44:23 +0000

    [IMP] web: feature detection impl. normalization
    This commit normalizes the different feature/browser detection modules'
    implementations to avoid difference between "newer" implementation and
    legacy ones.
    
    Its main goal is to ease future maintainability by having only one
    single source of truth for feature detection and, also ensure fixes
    applied to one version are applied everywhere.
    
    closes odoo/odoo#79499
    
    X-original-commit: 3b90e15983edcb79e6bd6e45685926cd4dfaae6b
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- addons/web/static/src/core/browser/feature_detection.js
+++ addons/web/static/src/core/browser/feature_detection.js
@@ -15,26 +15,35 @@ export function isBrowserChrome() {
     return browser.navigator.userAgent.includes("Chrome");
 }
 
+export function isAndroid() {
+    return /Android/i.test(browser.navigator.userAgent);
+}
+
+export function isIOS() {
+    return /(iPad|iPhone|iPod)/i.test(browser.navigator.userAgent) ||
+        (browser.navigator.platform === 'MacIntel' && maxTouchPoints() > 1);
+}
+
+export function isOtherMobileOS() {
+    return /(webOS|BlackBerry|Windows Phone)/i.test(browser.navigator.userAgent);
+}
+
 export function isMacOS() {
     return Boolean(browser.navigator.userAgent.match(/Mac/i));
 }
 
 export function isMobileOS() {
-    return Boolean(
-        browser.navigator.userAgent.match(/Android/i) ||
-            browser.navigator.userAgent.match(/webOS/i) ||
-            browser.navigator.userAgent.match(/iPhone/i) ||
-            browser.navigator.userAgent.match(/iPad/i) ||
-            browser.navigator.userAgent.match(/iPod/i) ||
-            browser.navigator.userAgent.match(/BlackBerry/i) ||
-            browser.navigator.userAgent.match(/Windows Phone/i)
-    );
+    return isAndroid() || isIOS() || isOtherMobileOS();
 }
 
 export function isIosApp() {
-    return navigator.userAgent.match(/OdooMobile \(iOS\)/i);
+    return /OdooMobile \(iOS\)/i.test(browser.navigator.userAgent);
 }
 
 export function hasTouch() {
     return "ontouchstart" in window || "onmsgesturechange" in window;
+}
+
+export function maxTouchPoints() {
+    return browser.navigator.maxTouchPoints || 1;
 }
