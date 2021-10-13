8d3c1f21518ba0d709357bef5fde2a1c215b590a
Valentin Chevalier <vcr@odoo.com>
2021-10-13 18:12:50 +0200

    [FIX] payment,sale: access error in payment link
    When a user was opening the payment_link_wizard and tried to select a
    preferred payment acquirer, he was getting an AccessError telling that
    he was not allowed to access 'payment.acquirer' records.
    
    Fix PR #69334 (task-2504225)
    
    task-2666881
    
    X-original-commit: 2ff580c073ac105219582dbf2108a1437645c9b5

--- addons/sale/wizard/sale_payment_link.py
+++ addons/sale/wizard/sale_payment_link.py
@@ -24,16 +24,17 @@ class PaymentLinkWizard(models.TransientModel):
             })
         return res
 
-    @api.depends('company_id', 'partner_id', 'currency_id')
-    def _compute_available_acquirer_ids(self):
-        sale_links = self.filtered(lambda link: link.res_model == 'sale.order')
-        super(PaymentLinkWizard, self-sale_links)._compute_available_acquirer_ids()
-        for link in sale_links:
-            link.available_acquirer_ids = link.env['payment.acquirer']._get_compatible_acquirers(
-                company_id=link.company_id.id,
-                partner_id=link.partner_id.id,
-                currency_id=link.currency_id.id,
-                sale_order_id=link.res_id)
+    def _get_payment_acquirer_available(self, res_model, res_id, **kwargs):
+        """ Select and return the acquirers matching the criteria.
+
+        :param str res_model: active model
+        :param int res_id: id of 'active_model' record
+        :return: The compatible acquirers
+        :rtype: recordset of `payment.acquirer`
+        """
+        if res_model == 'sale.order':
+            kwargs['sale_order_id'] = res_id
+        return super()._get_payment_acquirer_available(**kwargs)
 
     def _generate_link(self):
         """ Override of payment to add the sale_order_id in the link. """
@@ -46,7 +47,7 @@ class PaymentLinkWizard(models.TransientModel):
                                     f'?reference={urls.url_quote(payment_link.description)}' \
                                     f'&amount={payment_link.amount}' \
                                     f'&sale_order_id={payment_link.res_id}' \
-                                    f'{"&acquirer_id=" + str(payment_link.acquirer_id.id) if payment_link.acquirer_id else "" }' \
+                                    f'{"&acquirer_id=" + str(payment_link.payment_acquirer_selection) if payment_link.payment_acquirer_selection != "all" else "" }' \
                                     f'&access_token={payment_link.access_token}'
                 # Order-related fields are retrieved in the controller
             else:
