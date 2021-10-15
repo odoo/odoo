7997ce62db1dbe956bcd3f42b9a40bf5fa27f845
David (dafr) <dafr@odoo.com>
2021-10-15 07:41:07 +0000

    [FIX] sale_mrp: filter variant BOMs for anglo_saxon price_unit
    When invoicing a product with multiple variants and with anglo_saxon_accounting, the COGS would be the wrong amount.
    The function _stock_account_get_anglo_saxon_price_unit() was selecting the first BOM among all variant BOMs.
    Using the variant_bom_ids when existing prevent this situation.
    
    OPW-2662863
    
    closes odoo/odoo#78654
    
    X-original-commit: c07d005ae8153745c67527bbf97749e6473e4d41
    Signed-off-by: William Henrotin <Whenrow@users.noreply.github.com>
    Signed-off-by: DavidFesquet <DavidFesquet@users.noreply.github.com>

--- addons/sale_mrp/models/account_move.py
+++ addons/sale_mrp/models/account_move.py
@@ -11,7 +11,9 @@ class AccountMoveLine(models.Model):
 
         so_line = self.sale_line_ids and self.sale_line_ids[-1] or False
         if so_line:
-            bom = so_line.product_id.product_tmpl_id.bom_ids.filtered(lambda b: not b.company_id or b.company_id == so_line.company_id)[:1]
+            bom = (so_line.product_id.variant_bom_ids or so_line.product_id.product_tmpl_id.bom_ids).filtered(
+                lambda b: not b.company_id or b.company_id == so_line.company_id
+            )[:1]
             if bom and bom.type == 'phantom':
                 qty_to_invoice = self.product_uom_id._compute_quantity(self.quantity, self.product_id.uom_id)
                 qty_invoiced = sum([x.product_uom_id._compute_quantity(x.quantity, x.product_id.uom_id) for x in so_line.invoice_lines if x.move_id.state == 'posted'])
