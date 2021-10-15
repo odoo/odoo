638ffb30d10a42cc8255affed7da837fcd8018e3
roen-odoo <roen@odoo.com>
2021-10-15 12:07:41 +0000

    [FIX] sale,sale_project : Avoid sending multiple email on upsell
    Expected behaviour:
    When you create a sale order with a project associated. When the sale order change state
    from non upsell to upsell sales person should receive one email.
    
    Current behaviour:
    The sales person receive an email for every entry in the timesheet after the sale order goes
    from non upsell to upsell.
    
    Step to reproduce:
    Create a sale order setup to create a project and task and the invoicing policy is ordered quantities.
    
    Add a timesheet entry which takes it past the ordered quantity.
    
    Create the invoice for the order.
    
    The state of the line and order goes to upsell, and the upsell email is sent to the salesperson.
    
    Add another line in the timesheet and the email is sent again to the salesperson
    
    opw-2581428
    
    closes odoo/odoo#79287
    
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -511,11 +511,13 @@ class SaleOrder(models.Model):
         return result
 
     def _compute_field_value(self, field):
+        if field.name == 'invoice_status':
+            filtered_self = self.filtered(lambda so: so.user_id and so._origin.invoice_status != 'upselling')
         super()._compute_field_value(field)
         if field.name != 'invoice_status' or self.env.context.get('mail_activity_automation_skip'):
             return
 
-        filtered_self = self.filtered(lambda so: so.user_id and so.invoice_status == 'upselling')
+        filtered_self = filtered_self.filtered(lambda so: so.invoice_status == 'upselling')
         if not filtered_self:
             return
 
