4923bbb641777df1b092484250c3ad2e06e9a76f
Thomas Josse <thjo@odoo.com>
2021-10-15 13:20:07 +0000

    [IMP] rating: changes the conversion of the ratings
    Purpose
    =======
    
    This commit changes a conversion variable
    `RATING_LIMIT_SATISFIED` in order to follow a new conversion:
    > 5/4 := Satisfied
    > 3 := Okay
    > 2/1 := Dissatisfied
    
    Specifications
    ==============
    
    This commit only changes the variable in order to limit the
    modifications made to the `rating` module/models.
    
    This is done in order to change the faces displayed for each review/rating
    without impacting the statistics. The ratings/reviews will have the same values
    as before but will only get a different conversion if higher than 4.
    
    task-2597345
    
    See odoo/enterprise#20480
    See odoo/upgrade#2784
    
    Part-of: odoo/odoo#75646

--- addons/rating/models/rating.py
+++ addons/rating/models/rating.py
@@ -7,7 +7,7 @@ from odoo import api, fields, models
 
 from odoo.modules.module import get_resource_path
 
-RATING_LIMIT_SATISFIED = 5
+RATING_LIMIT_SATISFIED = 4
 RATING_LIMIT_OK = 3
 RATING_LIMIT_MIN = 1
 
