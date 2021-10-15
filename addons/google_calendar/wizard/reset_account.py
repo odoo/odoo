c794673e83d85bbe17bc02a95ea20dfaf3588aff
Daniel Blanco <daniel@blancomartin.cl>
2021-10-15 11:56:19 -0300

    [I18N] google_calendar: fix call to _set_auth_tokens method

--- addons/google_calendar/wizard/reset_account.py
+++ addons/google_calendar/wizard/reset_account.py
@@ -45,7 +45,7 @@ class ResetGoogleAccount(models.TransientModel):
                 'need_sync': True,
             })
 
-        self.user_id._set_auth_tokens(False, False, 0)
+        self._set_auth_tokens(False, False, 0)
         self.user_id.write({
             'google_calendar_sync_token': False,
             'google_calendar_cal_id': False,
