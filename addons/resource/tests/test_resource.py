74624f43ee56c5c059eb0c043f2fa509308a57d1
Thibault Libioulle <tle@odoo.com>
2021-10-15 14:46:01 +0200

    [REF] resource,hr{,_contract}: obtain calendar validity per resource
    This commit adds methods to easily obtain calendar validity per resource
    for a given search period and get the work intervals per resource,
    taking into account the validity of their calendars.
    
    In hr, the create and departure date of an employee are taken into
    account to compute the calendars validity.
    
    In hr_contract, it looks for every contract of human resources with
    employee of type student or employee existing in the search period and
    consider the specific calendar of the contract as valid during the
    contract-lifetime.
    
    PR: #77362
    
    task-2646630

--- addons/resource/tests/test_resource.py
+++ addons/resource/tests/test_resource.py
@@ -6,7 +6,7 @@ from pytz import timezone, utc
 
 from odoo import fields
 from odoo.exceptions import ValidationError
-from odoo.addons.resource.models.resource import Intervals
+from odoo.addons.resource.models.resource import Intervals, sum_intervals
 from odoo.addons.resource.tests.common import TestResourceCommon
 from odoo.tests.common import TransactionCase
 
@@ -1148,3 +1148,72 @@ class TestTimezones(TestResourceCommon):
             (date(2018, 4, 12), 8),
             (date(2018, 4, 13), 8),
         ])
+
+class TestResource(TestResourceCommon):
+
+    def test_calendars_validity_within_period(self):
+        calendars = self.jean.resource_id._get_calendars_validity_within_period(
+            utc.localize(datetime(2021, 7, 1, 8, 0, 0)),
+            utc.localize(datetime(2021, 7, 30, 17, 0, 0)),
+        )
+        interval = Intervals([(
+            utc.localize(datetime(2021, 7, 1, 8, 0, 0)),
+            utc.localize(datetime(2021, 7, 30, 17, 0, 0)),
+            self.env['resource.calendar.attendance']
+        )])
+
+        self.assertEqual(1, len(calendars), "The dict returned by calendars validity should only have 1 entry")
+        self.assertEqual(1, len(calendars[self.jean.resource_id.id]), "Jean should only have one calendar")
+        jean_entry = calendars[self.jean.resource_id.id]
+        jean_calendar = next(iter(jean_entry))
+        self.assertEqual(self.jean.resource_calendar_id, jean_calendar, "It should be Jean's Calendar")
+        self.assertFalse(jean_entry[jean_calendar] - interval, "Interval should cover all calendar's validity")
+        self.assertFalse(interval - jean_entry[jean_calendar], "Calendar validity should cover all interval")
+
+        calendars = self.env['resource.resource']._get_calendars_validity_within_period(
+            utc.localize(datetime(2021, 7, 1, 8, 0, 0)),
+            utc.localize(datetime(2021, 7, 30, 17, 0, 0)),
+        )
+        self.assertEqual(1, len(calendars), "The dict returned by calendars validity should only have 1 entry")
+        self.assertEqual(1, len(calendars[False]), "False (default) should only have one calendar")
+        false_entry = calendars[False]
+        false_calendar = next(iter(false_entry))
+        self.assertEqual(self.env.company.resource_calendar_id, false_calendar, "It should be company calendar Calendar")
+        self.assertFalse(false_entry[false_calendar] - interval, "Interval should cover all calendar's validity")
+        self.assertFalse(interval - false_entry[false_calendar], "Calendar validity should cover all interval")
+
+    def test_performance(self):
+        calendars = [self.calendar_jean, self.calendar_john, self.calendar_jules, self.calendar_patel]
+        calendars_len = len(calendars)
+        self.resources_test = self.env['resource.test'].create([{
+            'name': 'resource ' + str(i),
+            'resource_calendar_id': calendars[i % calendars_len].id,
+        } for i in range(0, 50)])
+
+        start = utc.localize(datetime(2021, 7, 7, 12, 0, 0))
+        end = utc.localize(datetime(2021, 7, 16, 23, 59, 59))
+        with self.assertQueryCount(12):
+            work_intervals, _ = self.resources_test.resource_id._get_valid_work_intervals(start, end)
+
+        self.assertEqual(len(work_intervals), 50)
+
+    def test_get_valid_work_intervals(self):
+        start = utc.localize(datetime(2021, 7, 7, 12, 0, 0))
+        end = utc.localize(datetime(2021, 7, 16, 23, 59, 59))
+        work_intervals, _ = self.jean.resource_id._get_valid_work_intervals(start, end)
+        sum_work_intervals = sum_intervals(work_intervals[self.jean.resource_id.id])
+        self.assertEqual(58, sum_work_intervals, "Sum of the work intervals for the resource jean should be 40h+18h = 58h")
+
+    def test_get_valid_work_intervals_calendars_only(self):
+        calendars = [self.calendar_jean, self.calendar_john, self.calendar_jules, self.calendar_patel]
+        start = utc.localize(datetime(2021, 7, 7, 12, 0, 0))
+        end = utc.localize(datetime(2021, 7, 16, 23, 59, 59))
+        _, calendars_intervals = self.env['resource.resource']._get_valid_work_intervals(start, end, calendars)
+        sum_work_intervals_jean = sum_intervals(calendars_intervals[self.calendar_jean.id])
+        self.assertEqual(58, sum_work_intervals_jean, "Sum of the work intervals for the calendar of jean should be 40h+18h = 58h")
+        sum_work_intervals_john = sum_intervals(calendars_intervals[self.calendar_john.id])
+        self.assertEqual(26 - 1 / 3600, sum_work_intervals_john, "Sum of the work intervals for the calendar of john should be 20h+6h-1s = 25h59m59s")
+        sum_work_intervals_jules = sum_intervals(calendars_intervals[self.calendar_jules.id])
+        self.assertEqual(31, sum_work_intervals_jules, "Sum of the work intervals for the calendar of jules should be Wodd:15h+Wpair:16h = 31h")
+        sum_work_intervals_patel = sum_intervals(calendars_intervals[self.calendar_patel.id])
+        self.assertEqual(49, sum_work_intervals_patel, "Sum of the work intervals for the calendar of patel should be 14+35h = 49h")
