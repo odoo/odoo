200a16ee15c0f2f2d07367eee984aaedccd26a6f
Pierrot (prro) <prro@odoo.com>
2021-10-18 15:22:00 +0000

    [FIX] [hr,sale]_timesheet: update timesheet's partner from Task
    How to reproduce the problem:
    - Install hr_timesheet and create a project.
    - Create a task in the project, and assign a Partner as the Customer
    - In the Timesheet tab of the Task, add a new entry. Save the Task.
    - Edit the task again and change the Customer. Save again.
    -> the partner on the Timesheet entry did not change.
    
    With sale_timesheet, the Sales Order Item of the timesheet entry can
    be updated from the Sales Order Item of the Task, but it isn't the case
    with the partner, which is not logical.
    
    Solution : Now, the partner_id is computed and will automatically be
    updated from the Task's Customer/partner_id. If sale_timesheet is
    installed, it will first filter out the invoiced timesheet entry:
    we don't want to change an already invoiced entry.
    
    opw-2616784
    
    closes odoo/odoo#78790
    
    X-original-commit: ccf6973b394d045a90a8bec64c06439b9dc41305
    Signed-off-by: LTU-Odoo <IT-Ideas@users.noreply.github.com>
    Signed-off-by: prro-odoo <proose@users.noreply.github.com>

--- addons/sale_timesheet/models/account.py
+++ addons/sale_timesheet/models/account.py
@@ -47,6 +47,10 @@ class AccountAnalyticLine(models.Model):
     def _compute_so_line(self):
         for timesheet in self.filtered(lambda t: not t.is_so_line_edited and t._is_not_billed()):  # Get only the timesheets are not yet invoiced
             timesheet.so_line = timesheet.project_id.allow_billable and timesheet._timesheet_determine_sale_line()
+    
+    @api.depends('timesheet_invoice_id.state')
+    def _compute_partner_id(self):
+        super(AccountAnalyticLine, self.filtered(lambda t: t._is_not_billed()))._compute_partner_id()
 
     def _is_not_billed(self):
         self.ensure_one()
