9551765fddbff1b5b9fa26c0ae6523ae0eae8003
Aaron Bohy <aab@odoo.com>
2021-10-18 13:33:49 +0200

    [WIP] work on grouped list view

--- addons/web/static/src/views/list/list_renderer.js
+++ addons/web/static/src/views/list/list_renderer.js
@@ -4,18 +4,19 @@ import { CheckBox } from "@web/core/checkbox/checkbox";
 import { Field } from "@web/fields/field";
 
 const { Component } = owl;
-const { useSubEnv } = owl.hooks;
 
 export class ListRenderer extends Component {
     setup() {
         this.fields = this.props.fields;
         this.columns = this.props.info.columns;
-
-        useSubEnv({ model: this.props.record.model });
     }
 
     openRecord(record) {
         this.props.openRecord(record);
+    }
+
+    toggleGroup(group) {
+        group.toggle();
     }
 }
 
