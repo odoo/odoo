eb2c75b5c1db05325e6057df3a7d691bfce29522
William Braeckman <wbr@odoo.com>
2021-10-19 15:35:48 +0000

    [IMP] lunch: add possibility to notify users on lunch arrival
    There was no way for the lunch manager to notify users that their meal
    had arrived prior to this commit besides manually sending a message to
    the concerned users.
    There now is a dedicated button after the order has been confirmed by
    the lunch manager to send notifications.
    
    TaskId-2587476
    
    closes odoo/odoo#78649
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/lunch/models/res_config_settings.py
+++ addons/lunch/models/res_config_settings.py
@@ -9,3 +9,4 @@ class ResConfigSettings(models.TransientModel):
 
     currency_id = fields.Many2one('res.currency', related='company_id.currency_id')
     company_lunch_minimum_threshold = fields.Float(string="Maximum Allowed Overdraft", readonly=False, related='company_id.lunch_minimum_threshold')
+    company_lunch_notify_message = fields.Html(string="Lunch notification message", readonly=False, related="company_id.lunch_notify_message")
