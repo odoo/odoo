7d80475777d99fe4cce964086155ce66ef8fd7bf
Josse Colpaert <jco@odoo.com>
2021-10-20 10:26:39 +0200

    [IMP] l10n_se

--- addons/l10n_se/__manifest__.py
+++ addons/l10n_se/__manifest__.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     "name": "Swedish - Accounting",
     "version": "1.0",
-    "author": "XCLUDE",
+    "summary": """Swedish chart of account EU BAS2020""",
+    "description": """
+This is the module to manage the accounting chart for Sweden in Odoo.
+==============================================================================
+
+Install some swedish chart of accounts.
+    - Merge with XCLUDE CoA
+    - Upgraded to EU BAS 2020 for Aktiebolag K2
+
+    """,
+    "author": "XCLUDE, Linserv AB",
+    "website": "https://www.linserv.se",
     "category": "Localization",
     "depends": ["account", "base_vat"],
     "data": [
-        "data/account_chart_template_before_accounts.xml",
+        "data/account_chart_template.xml",
         "data/account.account.template.csv",
-        "data/account_chart_template_after_accounts.xml",
+        "data/account_chart_template_post_data.xml",
         "data/account_tax_group.xml",
         "data/account_tax_report_data.xml",
         "data/account_tax_template.xml",
@@ -17,6 +29,8 @@
         "data/account_fiscal_position_account_template.xml",
         "data/account_fiscal_position_tax_template.xml",
         "data/account_chart_template_configuration.xml",
+        "data/menuitem_data.xml",
     ],
     'license': 'LGPL-3',
- }
+    "installable": True,
+}
