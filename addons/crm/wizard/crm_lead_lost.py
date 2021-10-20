a830ceaaf9ef72002027afa25cbb5b1575a28ee3
Thibault Delavallée <tde@odoo.com>
2021-10-20 07:14:08 +0000

    [IMP] crm: allow to log a feedback when marking a lead as lost
    PURPOSE
    
    Allow sales reps to add a closing note to their lead while it is being marked
    as lost. This is currently already done by a lot of sales reps but manually
    with the "Log a Note" button.
    
    SPECIFICATIONS
    
    In ``Lost Reason`` model: add a new html field allowing to log a note on the
    lost leads. Below the m2o, add an Extra Comment field where users can add a
    "closing note". When the wizard is submitted, log this message as a note on
    selected records.
    
    Add tests, allowing to test both the wizard and this new feature.
    
    Task-2671709
    
    Part-of: odoo/odoo#78648

--- addons/crm/wizard/crm_lead_lost.py
+++ addons/crm/wizard/crm_lead_lost.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import fields, models, _
+from odoo.tools.mail import is_html_empty
 
 
 class CrmLeadLost(models.TransientModel):
@@ -8,7 +10,20 @@ class CrmLeadLost(models.TransientModel):
     _description = 'Get Lost Reason'
 
     lost_reason_id = fields.Many2one('crm.lost.reason', 'Lost Reason')
+    lost_feedback = fields.Html(
+        'Closing Note', sanitize=True,
+        help="Closing note logged in leads discussion history."
+    )
 
     def action_lost_reason_apply(self):
+        self.ensure_one()
         leads = self.env['crm.lead'].browse(self.env.context.get('active_ids'))
-        return leads.action_set_lost(lost_reason=self.lost_reason_id.id)
+        res = leads.action_set_lost(lost_reason=self.lost_reason_id.id)
+        if not is_html_empty(self.lost_feedback):
+            leads._message_log_batch(
+                bodies=dict(
+                    (lead.id, self.lost_feedback)
+                    for lead in leads),
+                subject=False,
+            )
+        return res
