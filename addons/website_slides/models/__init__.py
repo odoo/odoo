d41a63f01a5f44481ae0eb36d842b98e1e110236
Thomas Josse <thjo@odoo.com>
2021-10-22 08:51:54 +0200

    [ADD] website_slides: merging slide.slide.link into resource
    Purpose
    =======
    This commit is used to merge 2 models into 1: `slide.slide.resource`
    and `slide.slide.link`
    
    Specifications
    ==============
    These models are merged in order to create a unique table for the
    *Additional Resources* of a Content.
    
    In order to do that, we merged the models into `slide.slide.resource`
    that will contain either a link or a file.
    
    task-2597345
    
    See odoo/enterprise#20480
    See odoo/upgrade#2784

--- addons/website_slides/models/__init__.py
+++ addons/website_slides/models/__init__.py
@@ -7,6 +7,7 @@ from . import slide_slide
 from . import slide_question
 from . import slide_channel
 from . import slide_channel_tag
+from . import slide_slide_resource
 from . import res_config_settings
 from . import website
 from . import res_users
