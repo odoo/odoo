04f09503b2cecb7ce7aeee5153b5c916b71ccc79
xO-Tx <mou@odoo.com>
2021-10-22 12:48:49 +0000

    [FIX] web, website: fix initAutoMoreMenu on user navbar
    The current code in 'initAutoMoreMenu' doesn't work properly
    on navbar after the update on dropdown in [1]. Small tweaks
    were added in this commit to fix that.
    
    [1]: 84715436d87bb05b421bc9ccaacda67d07571690
    
    task-2618929
    
    X-original-commit: 29b9280c44534d2f210dbcea83e0049365949f48
    Part-of: odoo/odoo#78959

--- addons/website/static/src/js/menu/navbar.js
+++ addons/website/static/src/js/menu/navbar.js
@@ -40,13 +40,13 @@ var WebsiteNavbar = publicWidget.RootWidget.extend({
      */
     start: function () {
         var self = this;
-        initAutoMoreMenu(this.el.querySelector('ul.o_menu_sections'), {
+        initAutoMoreMenu(this.el.querySelector('.o_menu_sections'), {
             maxWidth: function () {
                 // The navbar contains different elements in community and
                 // enterprise, so we check for both of them here only
                 return self.$el.width()
                     - (self.$('.o_menu_systray').outerWidth(true) || 0)
-                    - (self.$('ul#oe_applications').outerWidth(true) || 0)
+                    - (self.$('#oe_applications').outerWidth(true) || 0)
                     - (self.$('.o_menu_toggle').outerWidth(true) || 0)
                     - (self.$('.o_menu_brand').outerWidth(true) || 0);
             },
