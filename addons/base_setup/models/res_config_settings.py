0728da84f4d83b5a1f8a3fde14a9a94548443187
Thibault Delavallée <tde@odoo.com>
2021-10-25 15:32:01 +0200

    [IMP] base_setup, mail, hr: allow to configure company alias domains
    PURPOSE
    
    Allow alias domains to be multiple, notably to be used in a multi company
    environment where each company has its own alias domain.
    
    SPECIFICATIONS
    
    Update settings to configure your company's current alias domain instead
    of the global 'alias.catchall.domain' configuration parameter. As domain
    configuration is globally done per company, this is now a related on the
    company. Advanced configuration and management can still be done manually
    in settings.
    
    Field 'external_email_server_default' triggering display of mail configuration
    is moved to mail. It is defined in 'base_setup' but used only in mail hence
    moving the field to be coherent.
    
    Task-36879 (Mail: Support Multi Domains Aliases)
    
    Part-of: odoo/odoo#76734

--- addons/base_setup/models/res_config_settings.py
+++ addons/base_setup/models/res_config_settings.py
@@ -15,9 +15,6 @@ class ResConfigSettings(models.TransientModel):
     user_default_rights = fields.Boolean(
         "Default Access Rights",
         config_parameter='base_setup.default_user_rights')
-    external_email_server_default = fields.Boolean(
-        "Use Custom Email Servers",
-        config_parameter='base_setup.default_external_email_server')
     module_base_import = fields.Boolean("Allow users to import data from CSV/XLS/XLSX/ODS files")
     module_google_calendar = fields.Boolean(
         string='Allow the users to synchronize their calendar  with Google Calendar')
