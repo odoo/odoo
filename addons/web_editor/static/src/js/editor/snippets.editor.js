ca1225f5f7361dbf263e3ec9cf8b2411e13efd2c
Nicolas Bayet <nby@odoo.com>
2021-10-26 08:37:44 +0000

    [FIX] web_editor, website: only save dirty element
    Before this commit, when inserting a new snippet or moving a snippet
    that was already in the page would temporarily create hooks (`we-hook`)
    in the page. The insertion of thoses hooks would trigger mutation
    in the dirty observer (observer defined in edit.js) and mark all
    oe_structure for which a hook was temporarily inserted.
    Thus, creating unnecessary views in the backend.
    
    Now, the code skip the mutations of the drag and drop hooks.
    
    Task-2567752
    
    X-original-commit: 1b80fe9b56acea09a6784691561df65fe6349f50

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -834,6 +834,7 @@ var SnippetEditor = Widget.extend({
      * @private
      */
     _onDragAndDropStart: function () {
+        this.wysiwyg.odooEditor.observerUnactive('dragAndDropMoveSnippet');
         this.trigger_up('drag_and_drop_start');
         this.options.wysiwyg.odooEditor.automaticStepUnactive();
         var self = this;
@@ -947,6 +948,7 @@ var SnippetEditor = Widget.extend({
         this.$body.removeClass('move-important');
         $clone.remove();
 
+        this.wysiwyg.odooEditor.observerActive('dragAndDropMoveSnippet');
         if (this.dropped) {
             if (prev) {
                 this.$target.insertAfter(prev);
@@ -2319,6 +2321,7 @@ var SnippetsMenu = Widget.extend({
                 start: function () {
                     self.options.wysiwyg.odooEditor.automaticStepUnactive();
                     self.$el.find('.oe_snippet_thumbnail').addClass('o_we_already_dragging');
+                    self.options.wysiwyg.odooEditor.observerUnactive('dragAndDropCreateSnippet');
 
                     dropped = false;
                     $snippet = $(this);
@@ -2421,6 +2424,7 @@ var SnippetsMenu = Widget.extend({
                     }
 
                     self.getEditableArea().find('.oe_drop_zone').droppable('destroy').remove();
+                    self.options.wysiwyg.odooEditor.observerActive('dragAndDropCreateSnippet');
 
                     if (dropped) {
                         var prev = $toInsert.first()[0].previousSibling;
@@ -2439,7 +2443,12 @@ var SnippetsMenu = Widget.extend({
                         }
 
                         var $target = $toInsert;
+
+
+                        self.options.wysiwyg.odooEditor.observerUnactive('dragAndDropCreateSnippet');
                         await self._scrollToSnippet($target, self.$scrollable);
+                        self.options.wysiwyg.odooEditor.observerActive('dragAndDropCreateSnippet');
+
 
                         _.defer(async function () {
                             // Free the mutex now to allow following operations
