27e194f86b9824bc2fc638b6e90b3c82b6082334
Rémy Voet (ryv) <ryv@odoo.com>
2021-10-27 10:53:56 +0000

    [IMP] base: add company name for record rule error
    In debug mode, when a company ir.rule raise a exception
    for one or multiple records, a feedback message is
    display with limited records information.
    Add company name to these information to help the user to change
    his current company to the correct one directly.
    
    task-2628865
    
    Part-of: odoo/odoo#79073

--- odoo/addons/base/models/ir_rule.py
+++ odoo/addons/base/models/ir_rule.py
@@ -230,12 +230,7 @@ class IrRule(models.Model):
         resolution_info = _("Contact your administrator to request access if necessary.")
 
         if not self.env.user.has_group('base.group_no_one') or not self.env.user.has_group('base.group_user'):
-            msg = """{operation_error}
-
-{resolution_info}""".format(
-                operation_error=operation_error,
-                resolution_info=resolution_info)
-            return AccessError(msg)
+            return AccessError(f"{operation_error}\n\n{resolution_info}")
 
         # This extended AccessError is only displayed in debug mode.
         # Note that by default, public and portal users do not have
@@ -243,35 +238,32 @@ class IrRule(models.Model):
         # so it is relatively safe here to include the list of rules and record names.
         rules = self._get_failing(records, mode=operation).sudo()
 
-        records_description = ', '.join(['%s (id=%s)' % (rec.display_name, rec.id) for rec in records[:6].sudo()])
+        records_sudo = records[:6].sudo()
+        company_related = any('company_id' in (r.domain_force or '') for r in rules)
+
+        def get_record_description(rec):
+            # If the user has access to the company of the record, add this
+            # information in the description to help them to change company
+            if company_related and 'company_id' in rec and rec.company_id in self.env.user.company_ids:
+                return f'{rec.display_name} (id={rec.id}, company={rec.company_id.display_name})'
+            return f'{rec.display_name} (id={rec.id})'
+
+        records_description = ', '.join(get_record_description(rec) for rec in records_sudo)
         failing_records = _("Records: %s", records_description)
 
-        user_description = '%s (id=%s)' % (self.env.user.name, self.env.user.id)
+        user_description = f'{self.env.user.name} (id={self.env.user.id})'
         failing_user = _("User: %s", user_description)
 
-        rules_description = '\n'.join('- %s' % rule.name for rule in rules)
+        rules_description = '\n'.join(f'- {rule.name}' for rule in rules)
         failing_rules = _("This restriction is due to the following rules:\n%s", rules_description)
-        if any('company_id' in (r.domain_force or []) for r in rules):
+        if company_related:
             failing_rules += "\n\n" + _('Note: this might be a multi-company issue.')
-
-        msg = """{operation_error}
-
-{failing_records}
-{failing_user}
-
-{failing_rules}
-
-{resolution_info}""".format(
-                operation_error=operation_error,
-                failing_records=failing_records,
-                failing_user=failing_user,
-                failing_rules=failing_rules,
-                resolution_info=resolution_info)
 
         # clean up the cache of records prefetched with display_name above
         for record in records[:6]:
             record._cache.clear()
 
+        msg = f"{operation_error}\n\n{failing_records}\n{failing_user}\n\n{failing_rules}\n\n{resolution_info}"
         return AccessError(msg)
 
 
