ccf3f8e7d50a1a14fb764ea2ecbb1d94d76b02a4
Rémy Baranx (bar) <bar@odoo.com>
2021-11-02 15:12:45 +0100

    [FIX] calendar: email calendar event attendees
    When clicking on the "email" button in the `calendar.event`
    form view, the `email_to' field should be filled with the list
    of attendee email addresses separated by a comma.
    
    opw-2667016
    
    X-original-commit: 87e913b892c2518a716fceb247b57ab8c955a07f

--- addons/calendar/models/calendar_event.py
+++ addons/calendar/models/calendar_event.py
@@ -691,6 +691,11 @@ class Meeting(models.Model):
     # MAILING
     # ------------------------------------------------------------
 
+    def _get_attendee_emails(self):
+        """ Get comma-separated attendee email addresses. """
+        self.ensure_one()
+        return ",".join([e for e in self.attendee_ids.mapped("email") if e])
+
     def _sync_activities(self, fields):
         # update activities
         for event in self:
