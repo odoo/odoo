7bebed6bf0b4bc316ebfcee02a5449e58dd8d329
Yannick Tivisse <yti@odoo.com>
2021-11-02 12:53:30 +0100

    [IMP] sale: Convert product_uom_change into compute method

--- addons/sale/tests/test_onchange.py
+++ addons/sale/tests/test_onchange.py
@@ -158,12 +158,10 @@ class TestOnchangeProductId(TransactionCase):
 
         # force compute uom and prices
         order_line.product_id_change()
-        order_line.product_uom_change()
         order_line._onchange_discount()
         self.assertEqual(order_line.price_subtotal, 90, "Christmas discount pricelist rule not applied")
         self.assertEqual(order_line.discount, 10, "Christmas discount not equalt to 10%")
         order_line.product_uom = new_uom
-        order_line.product_uom_change()
         order_line._onchange_discount()
         self.assertEqual(order_line.price_subtotal, 900, "Christmas discount pricelist rule not applied")
         self.assertEqual(order_line.discount, 10, "Christmas discount not equalt to 10%")
@@ -275,7 +273,6 @@ class TestOnchangeProductId(TransactionCase):
         order_line.product_id_change()
         self.assertEqual(order_line.price_unit, 180, "First pricelist rule not applied")
         order_line.product_uom = new_uom
-        order_line.product_uom_change()
         self.assertEqual(order_line.price_unit, 1800, "First pricelist rule not applied")
 
     def test_sale_warnings(self):
