8e2a1c728c71bb888be0271c31fcc9aca6e70490
Laurent Stukkens (LTU) <ltu@odoo.com>
2021-11-02 09:50:16 +0000

    [FIX] sale_timesheet: prevent deterministic test fail
    Prioir to this commit:
    test_project_update_description_profitability was failing when started
    just before midnight the last day of the month as template_values['profitability']['month']
    was taking taking that current month. today being calculated after, was taking the next month
    value as passed midnigh and next month.
    
    After this commit:
    today is calculated prior the call to _get_template_values and freezegun is used
    in order to ensure that the date remains the same in the method.
    
    task-2682172
    
    X-original-commit: 3e4d12be3dd441d87a65b637da556d4c8ef82260

--- addons/sale_timesheet/tests/test_project_update.py
+++ addons/sale_timesheet/tests/test_project_update.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+<<<<<<< HEAD
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+=======
+
+from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
+>>>>>>> af15942e8cf... temp
 from freezegun import freeze_time
 
 from odoo.tests import tagged
@@ -9,8 +15,24 @@ from odoo.addons.project.tests.test_project_update_flow import TestProjectUpdate
 class TestProjectUpdateSaleTimesheet(TestProjectUpdate):
 
     def test_project_update_description_profitability(self):
+<<<<<<< HEAD
         self.project_pigs.allow_billable = True
         template_values = self.env['project.update']._get_template_values(self.project_pigs)
+=======
+        today = date.today()
+        with freeze_time(today + timedelta(hours=12)):
+            template_values = self.env['project.update']._get_template_values(self.project_pigs)
+
+        self.assertEqual(template_values['profitability']['month'], today.strftime('%B %Y'),
+                         "The month used in the template should be well defined")
+        self.assertEqual(template_values['profitability']['previous_month'], (today + relativedelta(months=-1)).strftime('%B'),
+                         "The previous month used in the template should be well defined")
+        self.assertTrue(template_values['profitability']['is_timesheet_uom_hour'], "Default timesheet uom is hour.")
+        self.assertEqual(template_values['profitability']['timesheet_uom'], "hours", "Default timesheet uom should be displayed as 'hours'")
+        self.assertEqual(template_values['profitability']['timesheet_unit_amount'], '0', "Timesheet unit amount used in the template should be defined.")
+        self.assertEqual(template_values['profitability']['previous_timesheet_unit_amount'], '0', "Previous Timesheet unit amount used in the template should be defined.")
+        self.assertEqual(template_values['profitability']['timesheet_trend'], "0", "Timesheet trend used in the template should be well defined.")
+>>>>>>> af15942e8cf... temp
         self.assertEqual(template_values['profitability']['costs'], "$\xa00.00", "Project costs used in the template should be well defined")
         self.assertEqual(template_values['profitability']['revenues'], "$\xa00.00", "Project revenues used in the template should be well defined")
         self.assertEqual(template_values['profitability']['margin'], 0, "Margin used in the template should be well defined")
