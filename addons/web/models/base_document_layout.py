887f8dd993d9b9518da4c30be29a0e2dc69f589a
Wolfgang Taferner <wolfgang.taferner@wt-io-it.at>
2021-11-04 14:29:59 +0100

    [FIX] web: incorrect object in render_values

--- addons/web/models/base_document_layout.py
+++ addons/web/models/base_document_layout.py
@@ -119,7 +119,7 @@ class BaseDocumentLayout(models.TransientModel):
                     wizard_with_logo = wizard
                 preview_css = self._get_css_for_preview(styles, wizard_with_logo.id)
                 ir_ui_view = wizard_with_logo.env['ir.ui.view']
-                wizard.preview = ir_ui_view._render_template('web.report_invoice_wizard_preview', {'company': wizard_with_logo, 'preview_css': preview_css})
+                wizard.preview = ir_ui_view._render_template('web.report_invoice_wizard_preview', {'company': wizard_with_logo.company_id, 'preview_css': preview_css})
             else:
                 wizard.preview = False
 
