48c680562b395a54b305afd0784b18b111671e82
Ivan Yelizariev <iel@odoo.com>
2021-11-08 11:31:09 +0100

    [ADD] l10n_kz
    task-2693102

---
+++ addons/l10n_kz/README.rst
@@ -0,0 +1,20 @@
+===========================
+ Kazakhstan - localization
+===========================
+
+Below are references to legal docs related to Kazakh accounting.
+
+Chart of accounts
+=================
+
+Name of the legal doc about Chart of Accounts: Приказ Министра финансов Республики Казахстан от 23 мая 2007 года N 185 "Об утверждении Типового плана счетов бухгалтерского учета". Зарегистрирован в Министерстве юстиции Республики Казахстан 29 июня 2007 года N 4771.
+
+https://adilet.zan.kz/rus/docs/V070004771_
+
+Tax Reports
+===========
+
+Name of the legal doc about Tax Reports: Приказ Министра финансов Республики
+Казахстан от 25 декабря 2014 года № 587 "Об утверждении форм налоговой отчетности и правил их составления"
+
+https://adilet.zan.kz/rus/docs/V14E0010156
---
+++ addons/l10n_kz/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    "name": "Kazakhstan - Accounting",
+    "version": "1.0.0",
+    "category": "Accounting/Localizations/Account Charts",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        # Chart of Accounts
+        "data/account_chart_template_data.xml",
+        #"data/account_account_tag_data.xml",
+        "data/account_account_template_data.xml",
+        "data/account.group.template.csv",
+
+        # Taxes
+        "data/account_tax_group_data.xml",
+        #"data/account_tax_report_data.xml",
+        "data/account_tax_template_data.xml",
+        #"data/account_fiscal_position_template_data.xml",
+
+        "data/account_chart_post_data.xml",
+        "data/account_chart_template_try_loading.xml",
+
+        "data/menuitem_data.xml",
+
+    ],
+    "demo": [
+        "demo/demo_company.xml",
+    ]
+}
