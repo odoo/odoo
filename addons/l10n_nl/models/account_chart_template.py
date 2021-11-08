11b4e50faeeab7d43e2c3be358f1833445bafc74
Ivan Yelizariev <iel@odoo.com>
2021-11-08 14:50:49 +0000

    [REM] account,l10n_*: delete dead code about transfer account
    `_prepare_transfer_account_for_direct_creation` is not used since https://github.com/odoo/odoo/commit/bcd1536f7fa2916bbc3470155ec9858535ece6f8
    
    closes odoo/odoo#79520
    
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/l10n_nl/models/account_chart_template.py
+++ addons/l10n_nl/models/account_chart_template.py
@@ -14,12 +14,3 @@ class AccountChartTemplate(models.Model):
             if account:
                 account.tag_ids = [(4, self.env.ref('l10n_nl.account_tag_12').id)]
         return res
-
-    @api.model
-    def _prepare_transfer_account_for_direct_creation(self, name, company):
-        res = super(AccountChartTemplate, self)._prepare_transfer_account_for_direct_creation(name, company)
-        if company.account_fiscal_country_id.code == 'NL':
-            xml_id = self.env.ref('l10n_nl.account_tag_25').id
-            res.setdefault('tag_ids', [])
-            res['tag_ids'].append((4, xml_id))
-        return res
