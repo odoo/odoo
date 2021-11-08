76ad2e2f6bbe08d073b9b8862870a8d0c43f0956
Laurent Stukkens (LTU) <ltu@odoo.com>
2021-11-08 09:02:35 +0000

    [FIX] sale: prevent useless filtered in _compute_field_value
    This commit improves the fix made in e73e201.
    The improvement will be added in the forward port starting from saas-14.4.
    
    opw-2581428
    
    closes odoo/odoo#79467
    
    Signed-off-by: Xavier <xbo@odoo.com>

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -511,7 +511,7 @@ class SaleOrder(models.Model):
         return result
 
     def _compute_field_value(self, field):
-        if field.name == 'invoice_status':
+        if field.name == 'invoice_status' and not self.env.context.get('mail_activity_automation_skip'):
             filtered_self = self.filtered(lambda so: so.user_id and so._origin.invoice_status != 'upselling')
         super()._compute_field_value(field)
         if field.name != 'invoice_status' or self.env.context.get('mail_activity_automation_skip'):
