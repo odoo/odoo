00937f6b7bd12191f6655cfd749d12d2558da0a0
Fabio Barbero <faba@odoo.com>
2021-11-08 16:06:55 +0100

    [FIX] digest: ease unsubscribe
    Purpose of this commit is to ease usage of unsubscribe button for periodic
    digests for non-admins. Users land on page that says "You have unsubscribed
    from ..." and not on digest form view anymore.
    
    It is done using a public route with a token, allowing to unsubscribe even
    when being not logged, for example when receiving digest emails that are
    unwanted.
    
    Task-2641394 (Digest emails sending improvement)
    Task-2582128 (Digest onboarding and usage improvement)
    
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

--- addons/digest/controllers/portal.py
+++ addons/digest/controllers/portal.py
@@ -1,21 +1,34 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from werkzeug.exceptions import Forbidden
+from werkzeug.exceptions import Forbidden, NotFound
 from werkzeug.urls import url_encode
 
 from odoo import _
 from odoo.http import Controller, request, route
+from odoo.tools import consteq
 
 
 class DigestController(Controller):
 
-    @route('/digest/<int:digest_id>/unsubscribe', type='http', website=True, auth='user')
-    def digest_unsubscribe(self, digest_id):
-        digest = request.env['digest.digest'].browse(digest_id).exists()
-        digest.action_unsubcribe()
+    @route('/digest/<int:digest_id>/unsubscribe', type='http', website=True, auth='public')
+    def digest_unsubscribe(self, digest_id, token=None, user_id=None):
+        digest_sudo = request.env['digest.digest'].sudo().browse(digest_id).exists()
+
+        # new route parameters
+        if digest_sudo and token and user_id:
+            correct_token = digest_sudo._get_unsubscribe_token(int(user_id))
+            if not consteq(correct_token, token):
+                raise NotFound()
+            digest_sudo._action_unsubscribe_users(request.env['res.users'].sudo().browse(int(user_id)))
+        # old route was given without any token or user_id but only for auth users
+        elif digest_sudo and not token and not user_id and not request.env.user.share:
+            digest_sudo.action_unsubcribe()
+        else:
+            raise NotFound()
+
         return request.render('digest.portal_digest_unsubscribed', {
-            'digest': digest,
+            'digest': digest_sudo,
         })
 
     @route('/digest/<int:digest_id>/set_periodicity', type='http', website=True, auth='user')
