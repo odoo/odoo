69c6ffb5a298eff9bfbbc5002f8c9b85edb1c5f6
Horacio Tellez <hote@odoo.com>
2021-11-09 14:13:23 +0100

    [FIX] payment: hide the refund button on payments created from a refund
    task-2685480
    
    X-original-commit: 02eba891cb8cd58985837090f2b60cf263bce065

--- addons/payment/models/account_payment.py
+++ addons/payment/models/account_payment.py
@@ -44,7 +44,8 @@ class AccountPayment(models.Model):
 
     def _compute_amount_available_for_refund(self):
         for payment in self:
-            if payment.payment_transaction_id.sudo().acquirer_id.support_refund:
+            tx = payment.payment_transaction_id
+            if tx.acquirer_id.sudo().support_refund and tx.operation != 'refund':
                 # Only consider refund transactions that are confirmed by summing the amounts of
                 # payments linked to such refund transactions. Indeed, should a refund transaction
                 # be stuck forever in a transient state (due to webhook failure, for example), the
