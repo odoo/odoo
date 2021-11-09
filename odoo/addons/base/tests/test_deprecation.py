fbfbf1756beed52c45c1956742bda27ad1fe2113
Rémy Voet (ryv) <ryv@odoo.com>
2021-11-09 14:04:42 +0100

    [REM] core: remove useless view_init
    The method view_init() is a hook called from default_get(). We can get
    the same result by overriding default_get directly.

---
+++ odoo/addons/base/tests/test_deprecation.py
@@ -0,0 +1,26 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import inspect
+
+from odoo.tests.common import TransactionCase, tagged
+
+DEPRECATED_MODEL_ATTRIBUTES = [
+    'view_init',
+]
+
+
+@tagged('-at_install', 'post_install', 'deprecation')
+class TestModelDeprecations(TransactionCase):
+
+    def test_model_attributes(self):
+        for model_name, Model in self.registry.items():
+            for attr in DEPRECATED_MODEL_ATTRIBUTES:
+                with self.subTest(model=model_name, attr=attr):
+                    value = getattr(Model, attr, None)
+                    if value is None:
+                        continue
+                    msg = f"Deprecated method/attribute {model_name}.{attr}"
+                    module = inspect.getmodule(value)
+                    if module:
+                        msg += f" in {module}"
+                    self.fail(msg)
