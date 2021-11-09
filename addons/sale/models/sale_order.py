28809489b7e6b940690436a6cd5152ef1babf57c
Yannick Tivisse <yti@odoo.com>
2021-11-09 13:40:41 +0100

    [IMP] sale: Remove useless pre-precessing on SOL creation
    This is not needed anymore since the last ORM changes from this PR
    with the pre_compute attribute.

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -548,13 +548,6 @@ class SaleOrder(models.Model):
                 vals['name'] = self.env['ir.sequence'].next_by_code(
                     'sale.order', sequence_date=seq_date) or _('New')
 
-            # Makes sure partner_invoice_id', 'partner_shipping_id' and 'pricelist_id' are defined
-            if any(f not in vals for f in ['partner_invoice_id', 'partner_shipping_id', 'pricelist_id']):
-                partner = self.env['res.partner'].browse(vals.get('partner_id'))
-                addr = partner.address_get(['delivery', 'invoice'])
-                vals['partner_invoice_id'] = vals.setdefault('partner_invoice_id', addr['invoice'])
-                vals['partner_shipping_id'] = vals.setdefault('partner_shipping_id', addr['delivery'])
-                vals['pricelist_id'] = vals.setdefault('pricelist_id', partner.property_product_pricelist.id)
         return super(SaleOrder, self).create(vals_list)
 
     def _compute_field_value(self, field):
