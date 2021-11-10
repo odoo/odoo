e6057a1b4ed5a83a534e4eb4c4b4ec1b16926f47
Thibault Delavallée <tde@odoo.com>
2021-11-10 14:20:20 +0000

    [FIX] mass_mailing: fix mail related values of statistics emails
    When mailings have no responsible, KPIs emails are broken as there is no
    from and to on the mail. This leads to mails being invalid and set in
    exception.
    
    Moreover currently author and from / to of those D+1 emails are not coherent
    as author is current user (generally odoobot as this is generated through
    a cron) while from and to are based on mailing responsible.
    
    In this commit we make statistics emails more coherent
      * when a responsible is set on the mailing: author, from and to are linked
        to the responsible;
      * when there is no responsible, current user is set as it may be sent by
        regular people;
      * reply-to is set to company email as it is often the case with 'marketing'
        emails;
    
    Task-2686586 (Repair mailing statistics email)
    
    X-original-commit: 95a21ca16f560cf4341c547f6bc909d3261acd1b
    Part-of: odoo/odoo#79877

--- addons/test_mail_full/tests/test_mass_mailing_statistics.py
+++ addons/test_mail_full/tests/test_mass_mailing_statistics.py
@@ -7,7 +7,7 @@ from odoo.addons.mail.tests.common import mail_new_test_user
 from odoo.addons.test_mail_full.tests.test_mass_sms import TestMassSMSCommon
 from odoo.tests.common import users
 from odoo.tests import tagged
-from odoo.tools import formataddr, mute_logger
+from odoo.tools import mute_logger
 
 
 @tagged('digest', 'mass_mailing')
@@ -53,10 +53,10 @@ class TestMailingStatistics(TestMassSMSCommon):
         self.assertEqual(len(self._new_mails), 1, "Mailing: a mail should have been created for statistics")
         mail = self._new_mails[0]
         # test email values
-        self.assertEqual(mail.author_id, self.user_marketing.partner_id)
+        self.assertEqual(mail.author_id, self.user_marketing_2.partner_id)
         self.assertEqual(mail.email_from, self.user_marketing_2.email_formatted)
         self.assertEqual(mail.email_to, self.user_marketing_2.email_formatted)
-        self.assertEqual(mail.reply_to, formataddr((self.company_admin.name, '%s@%s' % (self.alias_catchall, self.alias_domain))))
+        self.assertEqual(mail.reply_to, self.company_admin.partner_id.email_formatted)
         self.assertEqual(mail.state, 'outgoing')
         # test body content: KPIs
         body_html = html.fromstring(mail.body_html)
