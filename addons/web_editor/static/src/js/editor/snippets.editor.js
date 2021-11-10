8087bd00e8f8462a48d8dbedfa4b9185a3f7d1e7
Nicolas Bayet <nby@odoo.com>
2021-11-10 13:26:47 +0000

    [FIX] web_editor: allow to move snippets
    As an oversight of 2185e89e3f2a15b98b9cb8c3371a6031f9a6f055, it became
    impossible to move snippets.
    
    Task-2688281
    
    closes odoo/odoo#79648
    
    X-original-commit: f76ba944c516a0377cec324f564df604e52c2ffd
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Antoine Guenet <age@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -838,7 +838,7 @@ var SnippetEditor = Widget.extend({
      * @private
      */
     _onDragAndDropStart: function () {
-        this.wysiwyg.odooEditor.observerUnactive('dragAndDropMoveSnippet');
+        this.options.wysiwyg.odooEditor.observerUnactive('dragAndDropMoveSnippet');
         this.trigger_up('drag_and_drop_start');
         this.options.wysiwyg.odooEditor.automaticStepUnactive();
         var self = this;
@@ -952,7 +952,7 @@ var SnippetEditor = Widget.extend({
         this.$body.removeClass('move-important');
         $clone.remove();
 
-        this.wysiwyg.odooEditor.observerActive('dragAndDropMoveSnippet');
+        this.options.wysiwyg.odooEditor.observerActive('dragAndDropMoveSnippet');
         if (this.dropped) {
             if (prev) {
                 this.$target.insertAfter(prev);
