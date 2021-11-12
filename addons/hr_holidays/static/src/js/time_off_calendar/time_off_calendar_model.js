23da302c21a0e274ec01075e1a50194220e0d21e
Philippe Wauthy <phwa@odoo.com>
2021-11-12 15:23:32 +0000

    [FIX] hr_holidays: round the number of days of a time off
    When the user is set in another timezone than the context timezone, the first or last work interval retrieved to calculate the number of days
    between two dates is starting later or ending earlier depending on the user timezone.
    The number of days displayed between two dates is therefore not an integer.
    
    e.g.
    A time off taken between 08/06/2021 and 10/06/2021 (timezone in the context is Europe/Brussels) will give:
    - Number of days: 3 days if the user is in Europe/Brussels
    - Number of days: 2.69 if the user is in Asia/Calcutta (first work interval starting later)
    - Number of days: 2.5 if the user is in US/Pacific (last work interval ending earlier)
    
    task-2646655
    
    closes odoo/odoo#77036
    
    Related: odoo/upgrade#3486
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_holidays/static/src/js/time_off_calendar/time_off_calendar_model.js
+++ addons/hr_holidays/static/src/js/time_off_calendar/time_off_calendar_model.js
@@ -2,10 +2,18 @@
 
 import CalendarModel from "web.CalendarModel";
 
-
 export const TimeOffCalendarModel = CalendarModel.extend({
     _getFilterDomain: function() {
         const company_domain = [['user_id.company_id', 'in', this.data.context.allowed_company_ids]];
         return this._super().concat(company_domain);
+    },
+
+    calendarEventToRecord(event) {
+        const data = this._super(...arguments);
+        if (event.allDay) {
+            data.date_from = data.date_from.utc().startOf('day');
+            data.date_to = data.date_to.utc().endOf('day');
+        }
+        return data;
     },
 });
