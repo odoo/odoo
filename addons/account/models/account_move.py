96c610a275f693d5f0a6c7379f796fb2467f874b
Thibault Delavallée <tde@odoo.com>
2021-11-16 17:05:43 +0100

    [IMP] mail: rename tracking methods and clean dead code
    Purpose is to have tracking methods beginning with ``_track``, indicating
    those are tool methods used for tracking. It helps organizing the mail thread
    file and having short but precise method names.
    
    We also remove the usage of ``mail_track_log_only`` context key that is not
    used anymore in the code. It allows to shorten a bit the code and make it
    easier to read.
    
    Task-2671709

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -2319,7 +2319,7 @@ class AccountMove(models.Model):
 
         if not self.is_invoice(include_receipts=True):
             if self.payment_id and 'state' in init_values:
-                self.payment_id.message_track(['state'], {self.payment_id.id: init_values})
+                self.payment_id._message_track(['state'], {self.payment_id.id: init_values})
             return super(AccountMove, self)._track_subtype(init_values)
 
         if 'payment_state' in init_values and self.payment_state == 'paid':
