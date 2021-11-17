d6db5ed17fb0b1ff93f0e9dae022ab1ddedaa8f7
Aaron Bohy <aab@odoo.com>
2021-11-17 15:14:32 +0100

    unskip push state tests

--- addons/web/static/tests/webclient/actions/push_state_tests.js
+++ addons/web/static/tests/webclient/actions/push_state_tests.js
@@ -8,6 +8,7 @@ import {
     click,
     getFixture,
     legacyExtraNextTick,
+    makeDeferred,
     nextTick,
     patchWithCleanup,
 } from "../../helpers/utils";
@@ -175,7 +176,7 @@ QUnit.module("ActionManager", (hooks) => {
         assert.containsOnce(target, ".modal .test_client_action");
     });
 
-    QUnit.skip("properly push state", async function (assert) {
+    QUnit.test("properly push state", async function (assert) {
         assert.expect(3);
         const webClient = await createWebClient({ serverData });
         await doAction(webClient, 4);
@@ -191,7 +192,7 @@ QUnit.module("ActionManager", (hooks) => {
             view_type: "list",
         });
         await testUtils.dom.click($(target).find("tr.o_data_row:first"));
-        await legacyExtraNextTick();
+        await nextTick();
         assert.deepEqual(webClient.env.services.router.current.hash, {
             action: 8,
             model: "pony",
@@ -200,22 +201,22 @@ QUnit.module("ActionManager", (hooks) => {
         });
     });
 
-    QUnit.skip("push state after action is loaded, not before", async function (assert) {
+    QUnit.test("push state after action is loaded, not before", async function (assert) {
         assert.expect(2);
-        const def = testUtils.makeTestPromise();
-        const mockRPC = async function (route) {
-            if (route === "/web/dataset/search_read") {
+        const def = makeDeferred();
+        const mockRPC = async function (route, args) {
+            if (args.method === "web_search_read") {
                 await def;
             }
         };
         const webClient = await createWebClient({ serverData, mockRPC });
         doAction(webClient, 4);
-        await testUtils.nextTick();
-        await legacyExtraNextTick();
+        await nextTick();
+        await nextTick();
         assert.deepEqual(webClient.env.services.router.current.hash, {});
         def.resolve();
-        await testUtils.nextTick();
-        await legacyExtraNextTick();
+        await nextTick();
+        await nextTick();
         assert.deepEqual(webClient.env.services.router.current.hash, {
             action: 4,
             model: "partner",
