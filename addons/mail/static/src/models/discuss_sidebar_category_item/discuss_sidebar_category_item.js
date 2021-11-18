580f6d54a2ccf473974f20fb416e797ccb65b26a
Alexandre Kühn <aku@odoo.com>
2021-11-18 17:32:17 +0000

    [IMP] mail: remove editabletext component
    It was no longer used.
    
    closes odoo/odoo#80063
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/models/discuss_sidebar_category_item/discuss_sidebar_category_item.js
+++ addons/mail/static/src/models/discuss_sidebar_category_item/discuss_sidebar_category_item.js
@@ -133,9 +133,6 @@ function factory(dependencies) {
          * @param {MouseEvent} ev
          */
         onClick(ev) {
-            if (isEventHandled(ev, 'EditableText.click')) {
-                return;
-            }
             this.channel.open();
         }
 
