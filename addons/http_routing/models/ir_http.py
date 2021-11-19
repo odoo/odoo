84d2f5b57ccf3dbcefebdbc795ab1872bc1504b9
Jeremy Kersten <jke@odoo.com>
2021-11-19 13:34:06 +0000

    [FIX] http_routing: don't remove trailing / during redirect
    Before this commit, since we promote the use of route without trailing / for
    best SEO (fee0113), we remove the trailing / during a redirect to avoid an
    extra request.
    
    Unfortunately, it will break some route with trailing / in case of multi lang.
    
    So we remove this optimization, it will increase potentially number of http
    request before to get the final url, but it will allow to continue to support
    trailing slash in v15.
    
    This commit partially revert the commit ae35117
    
    closes odoo/odoo#80109
    
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/http_routing/models/ir_http.py
+++ addons/http_routing/models/ir_http.py
@@ -167,11 +167,7 @@ def url_lang(path_or_uri, lang_code=None):
             elif lang_url_code != default_lg.url_code or force_lang:
                 ps.insert(1, lang_url_code)
 
-            # remove trailing /
-            # ['', fr', ''] => /fr/ instead of /fr
-            if ps[-1] == '':
-                ps.pop(-1)
-            location = (u'/'.join(ps) or u'/') + sep + qs
+            location = u'/'.join(ps) + sep + qs
     return location
 
 
