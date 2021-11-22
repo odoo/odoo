af6abf816cbc5156c549b334d29a9754d81afcde
Thibault Delavallée <tde@odoo.com>
2021-11-22 16:57:56 +0000

    [IMP] crm, test_mail: flush tracking in tests involving tracking
    Tracking is generated using commit hooks, meaning they are really sent when
    the commit ends. This is done to ease values aggregation and accumulation
    through various record updates. In some tests we therefore have to manually
    flush the tracking, otherwise it is not created and posted. Notably tests about
    lead lost wizard were not flushing and therefore not generated the tracking
    messages. Those tests will be improved in future commits, cleaning them is
    therefore a necessary step.
    
    We also specifically add some tracking flush in mail performance tests. This
    is to be sure we measure impact of a write and tracking separately from
    previous transactions. Seems everything is working as intended as warmup and
    query counters already perform flushes. Here we simply flush at the end
    of the setup to be sure all base is clean before starting tests.
    
    Task-2671709
    
    Part-of: odoo/odoo#78648

--- addons/crm/tests/test_crm_lead_lost.py
+++ addons/crm/tests/test_crm_lead_lost.py
@@ -6,7 +6,7 @@ from odoo.exceptions import AccessError
 from odoo.tests.common import tagged, users
 
 
-@tagged('lead_manage')
+@tagged('lead_manage', 'lead_lost')
 class TestLeadConvert(crm_common.TestCrmCommon):
 
     @classmethod
@@ -22,9 +22,11 @@ class TestLeadConvert(crm_common.TestCrmCommon):
             'user_id': self.user_sales_salesman.id,
             'probability': 32,
         })
+        self.flush_tracking()
 
         lead = self.lead_1.with_user(self.env.user)
         self.assertEqual(lead.probability, 32)
+        self.assertEqual(len(lead.message_ids), 2, 'Should have tracked new responsible')
 
         lost_wizard = self.env['crm.lead.lost'].with_context({
             'active_ids': lead.ids,
@@ -33,11 +35,14 @@ class TestLeadConvert(crm_common.TestCrmCommon):
         })
 
         lost_wizard.action_lost_reason_apply()
+        self.flush_tracking()
 
         self.assertEqual(lead.probability, 0)
         self.assertEqual(lead.automated_probability, 0)
         self.assertFalse(lead.active)
         self.assertEqual(lead.lost_reason, self.lost_reason)  # TDE FIXME: should be called lost_reason_id non didjou
+        # check messages
+        self.assertEqual(len(lead.message_ids), 3, 'Should have logged a tracking message for lost lead with reason')
 
     @users('user_sales_salesman')
     def test_lead_lost_crm_rights(self):
