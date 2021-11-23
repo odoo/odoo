cfb81d0bbd8e090929a825718357ece8d30e6d21
Adrien Widart <awt@odoo.com>
2021-11-23 10:31:47 +0000

    [FIX] sale_stock: let public users open the DO
    From the portal view of a SO, when a public/portal user tries to open
    the delivery order, he will have an error
    
    To reproduce the issue:
    (Need sale_management. Use demo data)
    1. Create & Confirm a SO:
        - Customer: Joel Willis
        - Add some products
    2. Log in as portal (i.e., Joel Willis)
    3. Open the SO
    4. Open the Delivery Order
    
    Error: It leads to a 403 error page.
    
    When rendering the template, the `sudo` flag is forced to False. Since,
    in this case, this is a public controller that uses a token, we can use
    `SUPERUSER_ID` (see [1] for more details)
    
    [1] 61b4b6777d64218f94c51ec839c187fdc7b2034f
    
    OPW-2689053
    
    closes odoo/odoo#80308
    
    X-original-commit: cb098f6348ddc85edfef06c284c74c8af44ca7ba
    Signed-off-by: Arnold Moyaux <arm@odoo.com>

--- addons/sale_stock/tests/__init__.py
+++ addons/sale_stock/tests/__init__.py
@@ -9,3 +9,4 @@ from . import test_sale_stock_report
 from . import test_sale_order_dates
 from . import test_sale_stock_multicompany
 from . import test_sale_stock_accrued_entries
+from . import test_sale_stock_access_rights
