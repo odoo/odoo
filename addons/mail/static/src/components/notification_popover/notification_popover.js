22809e96060c7a1034b83e318077a61acc31ef43
Alexandre Kühn <aku@odoo.com>
2021-11-23 15:17:15 +0000

    [IMP] mail: remove NotificationPopover/notificationLocalIds deep props
    In preparation for using OWL v2 in discuss code.
    (shouldUpdateBasedOnProps will be removed)
    
    Task-2695743
    
    closes odoo/odoo#80616
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/components/notification_popover/notification_popover.js
+++ addons/mail/static/src/components/notification_popover/notification_popover.js
@@ -7,65 +7,19 @@ const { Component } = owl;
 export class NotificationPopover extends Component {
 
     /**
-     * @returns {string}
+     * @returns {mail.message_view}
      */
-    get iconClass() {
-        switch (this.notification.notification_status) {
-            case 'sent':
-                return 'fa fa-check';
-            case 'bounce':
-                return 'fa fa-exclamation';
-            case 'exception':
-                return 'fa fa-exclamation';
-            case 'ready':
-                return 'fa fa-send-o';
-            case 'canceled':
-                return 'fa fa-trash-o';
-        }
-        return '';
-    }
-
-    /**
-     * @returns {string}
-     */
-    get iconTitle() {
-        switch (this.notification.notification_status) {
-            case 'sent':
-                return this.env._t("Sent");
-            case 'bounce':
-                return this.env._t("Bounced");
-            case 'exception':
-                return this.env._t("Error");
-            case 'ready':
-                return this.env._t("Ready");
-            case 'canceled':
-                return this.env._t("Canceled");
-        }
-        return '';
-    }
-
-    /**
-     * @returns {mail.notification[]}
-     */
-    get notifications() {
-        if (!this.messaging) {
-            return [];
-        }
-        return this.props.notificationLocalIds.map(
-            notificationLocalId => this.messaging.models['mail.notification'].get(notificationLocalId)
-        );
+    get messageView() {
+        return this.messaging && this.messaging.models['mail.message_view'].get(this.props.messageViewLocalId);
     }
 
 }
 
 Object.assign(NotificationPopover, {
     props: {
-        notificationLocalIds: {
-            type: Array,
-            element: String,
-        },
+        messageViewLocalId: String,
     },
     template: 'mail.NotificationPopover',
 });
 
-registerMessagingComponent(NotificationPopover, { propsCompareDepth: { notificationLocalIds: 1 } });
+registerMessagingComponent(NotificationPopover);
