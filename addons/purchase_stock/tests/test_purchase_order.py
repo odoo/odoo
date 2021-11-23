48f17f0e61059e4e8ac5b73ef03517fb997db61c
William Henrotin <whe@odoo.com>
2021-11-23 10:54:46 +0000

    [FIX] purchase_stock: create and confirm the same day
    Testing purchase order creation assumes the whole test is done the same
    day. The assert could failed if the test is run right before midnight
    and end the day after.
    
    This commit ensure the time is frozen during all the tests about
    purchase order creation.
    
    closes odoo/odoo#80257
    
    X-original-commit: 438e47cdc87fc0f13c61a1ede29a190120dea117
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/purchase_stock/tests/test_purchase_order.py
+++ addons/purchase_stock/tests/test_purchase_order.py
@@ -2,12 +2,14 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 import re
 from datetime import datetime, timedelta
+from freezegun import freeze_time
 
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.addons.stock_account.tests.test_anglo_saxon_valuation_reconciliation_common import ValuationReconciliationTestCommon
 from odoo.tests import Form, tagged
 
 
+@freeze_time("2021-01-14 09:12:15")
 @tagged('post_install', '-at_install')
 class TestPurchaseOrder(ValuationReconciliationTestCommon):
 
