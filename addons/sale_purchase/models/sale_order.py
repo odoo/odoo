fe46954e1ffb2b036ec624b3f88a5f1c4a9172ff
William Henrotin <whe@odoo.com>
2021-11-24 10:42:44 +0000

    [FIX] sale_purchase: change deprecated method
    The currency.compute method is deprecated. We use _convert instead
    
    closes odoo/odoo#80336
    
    Related: odoo/enterprise#22469
    Signed-off-by: Arnold Moyaux <arm@odoo.com>

--- addons/sale_purchase/models/sale_order.py
+++ addons/sale_purchase/models/sale_order.py
@@ -239,7 +239,7 @@ class SaleOrderLine(models.Model):
         if supplierinfo:
             price_unit = self.env['account.tax'].sudo()._fix_tax_included_price_company(supplierinfo.price, self.product_id.supplier_taxes_id, taxes, self.company_id)
             if purchase_order.currency_id and supplierinfo.currency_id != purchase_order.currency_id:
-                price_unit = supplierinfo.currency_id.compute(price_unit, purchase_order.currency_id)
+                price_unit = supplierinfo.currency_id._convert(price_unit, purchase_order.currency_id, purchase_order.company_id, fields.datetime.today())
 
         return {
             'name': '[%s] %s' % (self.product_id.default_code, self.name) if self.product_id.default_code else self.name,
