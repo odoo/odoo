585f25ccdbb591b9ab1355bad206d9ec433caf15
Xavier-Do <xdo@odoo.com>
2021-11-25 11:21:06 +0100

    [IMP] tests: avoid double logs on build page
    Since auto-retry, real errors will lead to doubled errors on runbot.
    This can be noisy and hard to understand.
    
    This commit will help with this by using a lower log level on the first
    execution. Log 25 are kepts.

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -45,6 +45,7 @@ from . import test_res_partner
 from . import test_res_partner_bank
 from . import test_res_users
 from . import test_reports
+from . import test_test_retry
 from . import test_test_suite
 from . import test_tests_tags
 from . import test_form_create
