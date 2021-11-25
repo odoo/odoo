341ad3f4dd575f7207d9bd16adac35276e86290f
roen-odoo <roen@odoo.com>
2021-11-25 13:27:35 +0000

    [FIX] point_of_sale, pos_coupon, pos_hr, pos_restaurant : Check COA not used
    Check COA parameter in the open_session_cb was actually never used
    
    opw-2691615
    
    closes odoo/odoo#80410
    
    Related: odoo/enterprise#22522
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/point_of_sale/tests/test_point_of_sale_flow.py
+++ addons/point_of_sale/tests/test_point_of_sale_flow.py
@@ -22,7 +22,7 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
         return untax, sum(tax.get('amount', 0.0) for tax in res['taxes'])
 
     def test_order_refund(self):
-        self.pos_config.open_session_cb(check_coa=False)
+        self.pos_config.open_session_cb()
         current_session = self.pos_config.current_session_id
         # I create a new PoS order with 2 lines
         order = self.PosOrder.create({
@@ -194,7 +194,7 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
         """
 
         # I click on create a new session button
-        self.pos_config.open_session_cb(check_coa=False)
+        self.pos_config.open_session_cb()
         current_session = self.pos_config.current_session_id
 
         # I create a PoS order with 2 units of PCSC234 at 450 EUR
@@ -482,7 +482,7 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
 
     def test_order_to_invoice(self):
 
-        self.pos_config.open_session_cb(check_coa=False)
+        self.pos_config.open_session_cb()
         current_session = self.pos_config.current_session_id
 
         untax1, atax1 = self.compute_tax(self.product3, 450*0.95, 2)
@@ -598,7 +598,7 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
         """
 
         # I click on create a new session button
-        self.pos_config.open_session_cb(check_coa=False)
+        self.pos_config.open_session_cb()
 
         current_session = self.pos_config.current_session_id
         num_starting_orders = len(current_session.order_ids)
@@ -778,7 +778,7 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
         })
 
         # I click on create a new session button
-        eur_config.open_session_cb(check_coa=False)
+        eur_config.open_session_cb()
         current_session = eur_config.current_session_id
 
         # I create a PoS order with 2 units of PCSC234 at 450 EUR (Tax Incl)
@@ -877,7 +877,7 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
             self.assertAlmostEqual(a, b)
 
     def test_order_to_invoice_no_tax(self):
-        self.pos_config.open_session_cb(check_coa=False)
+        self.pos_config.open_session_cb()
         current_session = self.pos_config.current_session_id
 
         # I create a new PoS order with 2 units of PC1 at 450 EUR (Tax Incl) and 3 units of PCSC349 at 300 EUR. (Tax Excl)
@@ -957,7 +957,7 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
         })
 
         # sell product thru pos
-        self.pos_config.open_session_cb(check_coa=False)
+        self.pos_config.open_session_cb()
         pos_session = self.pos_config.current_session_id
         untax, atax = self.compute_tax(product5, 10.0)
         product5_order = {'data':
