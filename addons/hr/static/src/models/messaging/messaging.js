05f7871e5f441e245f2ad05a867e5b0e48c7b8c5
Louis Wicket (wil) <wil@odoo.com>
2021-11-29 18:11:18 +0100

    [IMP] hr: rename employee model
    Rename javascript model 'hr.employee' to 'Employee' in order to
    distinguish javascript model from python model.
    
    Part of task-2701674.

--- addons/hr/static/src/models/messaging/messaging.js
+++ addons/hr/static/src/models/messaging/messaging.js
@@ -11,7 +11,7 @@ patchRecordMethods('mail.messaging', {
      */
     async getChat({ employeeId }) {
         if (employeeId) {
-            const employee = this.messaging.models['hr.employee'].insert({ id: employeeId });
+            const employee = this.messaging.models['Employee'].insert({ id: employeeId });
             return employee.getChat();
         }
         return this._super(...arguments);
@@ -21,7 +21,7 @@ patchRecordMethods('mail.messaging', {
      */
     async openProfile({ id, model }) {
         if (model === 'hr.employee' || model === 'hr.employee.public') {
-            const employee = this.messaging.models['hr.employee'].insert({ id });
+            const employee = this.messaging.models['Employee'].insert({ id });
             return employee.openProfile();
         }
         return this._super(...arguments);
