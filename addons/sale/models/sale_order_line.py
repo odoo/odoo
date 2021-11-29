0f2edc94bf12d6728ba2f5ec9d9bdd490a1d4077
Yannick Tivisse <yti@odoo.com>
2021-11-29 17:57:52 +0100

    [IMP] sale: Use a cache to compute fiscal positions

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -139,9 +139,12 @@ class SaleOrderLine(models.Model):
 
     @api.depends('product_id')
     def _compute_tax_id(self):
+        cached_fpos = {}
         for line in self:
             line = line.with_company(line.company_id)
-            fpos = line.order_id.fiscal_position_id or line.order_id.fiscal_position_id._get_fiscal_position(line.order_partner_id)
+            if line.order_id not in cached_fpos:
+                cached_fpos[line.order_id] = line.order_id.fiscal_position_id or line.order_id.fiscal_position_id._get_fiscal_position(line.order_partner_id)
+            fpos = cached_fpos[line.order_id]
             # If company_id is set, always filter taxes by the company
             taxes = line.product_id.taxes_id.filtered(lambda t: t.company_id == line.env.company)
             line.tax_id = fpos.map_tax(taxes)
