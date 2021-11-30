ca82b5c0dadbe779ad828e2c65f0b663d311768c
Zelong Lin <zel@odoo.com>
2021-11-30 14:57:54 +0100

    [FIX] discuss: clicking on attachment download closes current rtc call
    Using <a> with 'download' attribute to download the attachment.
    Prevent calling .navigate() interrupting the current call.
    
    task-2692597

--- addons/mail/static/src/components/attachment_viewer/attachment_viewer.js
+++ addons/mail/static/src/components/attachment_viewer/attachment_viewer.js
@@ -136,8 +136,7 @@ export class AttachmentViewer extends Component {
      * @private
      */
     _download() {
-        const id = this.attachmentViewer.attachment.id;
-        this.env.services.navigate(`/web/content/ir.attachment/${id}/datas`, { download: true });
+        this.attachmentViewer.attachment.download();
     }
 
     /**
