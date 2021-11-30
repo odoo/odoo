6aafe1303ef1554c2c1c4aceb87bb3d117fbd9fa
Thibault Francois <tfr@odoo.com>
2021-11-30 10:43:20 +0000

    [FIX] base{_import_module}: cloc should not count generated code
    * Studio generate computed field "count" when the user add a button
    in the button box. Since those lines are written by studio with drag and drop
    it should not be counted for the maintenance subscription.
    
    We can detect them because field created by the user in studio start with x_studio
    and fields created outside studio does not have studio_customization
    xml_id
    
    * Add test to make no standard module introduce customization in the
    database during the install the will be counted by cloc
    
    see https://github.com/odoo/enterprise/pull/22664
    
    closes odoo/odoo#85707
    
    X-original-commit: abdce539fc6d9346fd0d7d6c49b4da04ba4d5bdd
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- odoo/addons/base/tests/test_cloc.py
+++ odoo/addons/base/tests/test_cloc.py
@@ -2,7 +2,7 @@
 import sys
 
 from odoo.tools import cloc
-from odoo.tests.common import TransactionCase
+from odoo.tests import TransactionCase, tagged
 
 XML_TEST = """<!-- Comment -->
 <?xml version="1.0" encoding="UTF-8"?>
@@ -79,7 +79,54 @@ function() {
 }
 '''
 
-class TestCloc(TransactionCase):
+class TestClocCustomization(TransactionCase):
+    def create_xml_id(self, name, res_id, module='studio_customization'):
+        self.env['ir.model.data'].create({
+            'name': name,
+            'model': 'ir.model.fields',
+            'res_id': res_id,
+            'module': module,
+        })
+
+    def create_field(self, name):
+        return self.env['ir.model.fields'].with_context(studio=True).create({
+            'name': name,
+            'field_description': name,
+            'model': 'res.partner',
+            'model_id': self.env.ref('base.model_res_partner').id,
+            'ttype': 'integer',
+            'store': False,
+            'compute': "for rec in self: rec['x_invoice_count'] = 10",
+        })
+
+    def test_ignore_auto_generated_computed_field(self):
+        """
+            Check that we count custom fields with no module or studio not auto generated
+            Having an xml_id but no existing module is consider as not belonging to a module
+        """
+        f1 = self.create_field('x_invoice_count')
+        self.create_xml_id('invoice_count', f1.id)
+        cl = cloc.Cloc()
+        cl.count_customization(self.env)
+        self.assertEqual(cl.code.get('odoo/studio', 0), 0, 'Studio auto generated count field should not be counted in cloc')
+        f2 = self.create_field('x_studio_custom_field')
+        self.create_xml_id('studio_custom', f2.id)
+        cl = cloc.Cloc()
+        cl.count_customization(self.env)
+        self.assertEqual(cl.code.get('odoo/studio', 0), 1, 'Count other studio computed field')
+        f3 = self.create_field('x_custom_field')
+        cl = cloc.Cloc()
+        cl.count_customization(self.env)
+        self.assertEqual(cl.code.get('odoo/studio', 0), 2, 'Count fields without xml_id')
+        f4 = self.create_field('x_custom_field_export')
+        self.create_xml_id('studio_custom', f4.id, '__export__')
+        cl = cloc.Cloc()
+        cl.count_customization(self.env)
+        self.assertEqual(cl.code.get('odoo/studio', 0), 3, 'Count fields with xml_id but without module')
+
+
+class TestClocParser(TransactionCase):
+
     def test_parser(self):
         cl = cloc.Cloc()
         xml_count = cl.parse_xml(XML_TEST)
@@ -96,3 +143,16 @@ class TestCloc(TransactionCase):
             self.assertEqual(py_count, (8, 16))
         js_count = cl.parse_js(JS_TEST)
         self.assertEqual(js_count, (10, 17))
+
+
+@tagged('post_install', '-at_install')
+class TestClocStdNoCusto(TransactionCase):
+
+    def test_no_custo_install(self):
+        """
+            Make sure after the installation of module
+            no database customization is counted
+        """
+        cl = cloc.Cloc()
+        cl.count_customization(self.env)
+        self.assertEqual(cl.code.get('odoo/studio', 0), 0, 'Module should not generate customization in database')
