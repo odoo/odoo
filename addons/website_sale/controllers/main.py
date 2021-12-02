8ff6664c9dc98b7282c57b2859745bf1030aabc9
Martin Trigaux <mat@odoo.com>
2021-12-02 16:36:24 +0100

    [FIX] website_sale: allow public user to print cart
    When the public user is connected, allow him to print the report of
    the open cart.
    Since 61b4b6777d64 the sudo is not enough but with_user(1) is
    required.

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -1105,7 +1105,7 @@ class WebsiteSale(http.Controller):
     def print_saleorder(self, **kwargs):
         sale_order_id = request.session.get('sale_last_order_id')
         if sale_order_id:
-            pdf, _ = request.env.ref('sale.action_report_saleorder').sudo()._render_qweb_pdf([sale_order_id])
+            pdf, _ = request.env.ref('sale.action_report_saleorder').with_user(SUPERUSER_ID)._render_qweb_pdf([sale_order_id])
             pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', u'%s' % len(pdf))]
             return request.make_response(pdf, headers=pdfhttpheaders)
         else:
