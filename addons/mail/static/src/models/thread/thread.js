e53dffd1cb89275b12663859d3544693f074f420
Sébastien Theys <seb@odoo.com>
2021-12-06 18:30:30 +0100

    [FIX] mail: improve performance of ringing thread
    Avoid iterating over all channels every time a new channel is added.
    
    Part of task-2702450

--- addons/mail/static/src/models/thread/thread.js
+++ addons/mail/static/src/models/thread/thread.js
@@ -1647,6 +1647,17 @@ function factory(dependencies) {
 
         /**
          * @private
+         * @returns {FieldCommand}
+         */
+        _computeMessagingAsRingingThread() {
+            if (this.rtcInvitingSession) {
+                return replace(this.messaging);
+            }
+            return clear();
+        }
+
+        /**
+         * @private
          * @returns {mail.message[]}
          */
         _computeNeedactionMessagesAsOriginThread() {
@@ -2300,6 +2311,11 @@ function factory(dependencies) {
         messageSeenIndicators: one2many('mail.message_seen_indicator', {
             inverse: 'thread',
             isCausal: true,
+        }),
+        messagingAsRingingThread: many2one('mail.messaging', {
+            compute: '_computeMessagingAsRingingThread',
+            inverse: 'ringingThreads',
+            readonly: true,
         }),
         model: attr({
             readonly: true,
