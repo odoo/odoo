0bdbe95e0655e948adc451da8fb55b6d1da7a7a5
Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>
2021-12-06 15:44:35 +0100

    [IMP] l10n_pt: add tax report for Portugal
    X-original-commit: c4d66cebfd08614618dbd0e052b85bca81a90c79

--- addons/l10n_pt/__manifest__.py
+++ addons/l10n_pt/__manifest__.py
@@ -20,6 +20,7 @@
            'data/account_chart_template_data.xml',
            'data/account_fiscal_position_template_data.xml',
            'data/account_tax_group_data.xml',
+           'data/account_tax_report.xml',
            'data/account_tax_data.xml',
            'data/account_chart_template_configure_data.xml',
            ],
