8cf5db7d7aa2ee65e5024b664baeffbfbb2739d2
Sébastien Theys <seb@odoo.com>
2021-12-07 18:02:10 +0100

    [FIX] mail: rename ambiguous get method in model manager
    To ease debugging in general, avoid having multiple methods with the same name,
    especially in the same file.
    
    This is especially useful when the method name appears in a stack trace, for
    example in the performance tool.
    
    Part of task-2702450
    
    X-original-commit: c3673e63064c7f331754ef0e9d02cbbe7b1927f4

--- addons/mail/static/src/model/model_manager.js
+++ addons/mail/static/src/model/model_manager.js
@@ -676,7 +676,7 @@ export class ModelManager {
             __values: {},
         });
         const record = !this.env.isDebug() ? nonProxyRecord : new Proxy(nonProxyRecord, {
-            get: function (record, prop) {
+            get: function getFromProxy(record, prop) {
                 if (
                     !Model.__fieldMap[prop] &&
                     !['_super', 'then'].includes(prop) &&
@@ -1223,7 +1223,7 @@ export class ModelManager {
             // Add field accessors.
             for (const field of Model.__fieldList) {
                 Object.defineProperty(Model.prototype, field.fieldName, {
-                    get() { // this is bound to record
+                    get: function getFieldValue() { // this is bound to record
                         for (const listener of this.modelManager._listeners) {
                             this.modelManager._localIdsObservedByListener.get(listener).add(this.localId);
                             listener.lastObservedLocalIds.add(this.localId);
