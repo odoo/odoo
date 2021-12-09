f18cd32a936829d8a059db0d259189503ee5317f
Romain Derie <rde@odoo.com>
2021-12-09 18:48:51 +0000

    [FIX] theme_*: enable footer/header template correctly
    Some theme are enabling an header template. When doing so, they also disable
    the default header template.
    But this is not enough, as the user could have changed that template and it is
    not the default one anymore.
    
    Then, updating that theme (UX, CLI, Migration) will raise a traceback.
    
    Step to reproduce:
      - Create a website and install theme avantgarde
      - Enter edit mode and select Magazine header template
      At that point, update the theme, either:
      - Through the UI, on theme switch screen, click on Update
      - Through CLI, just run a `-u theme_avantgarde`
    -> A traceback will be raised about an xpath error, as both the magazine header
       template and the hamburger header template are active at the same time. Only
       one template is supposed to be activated.
    
    The issue also impact migration, as migrated website can't be accessed due to
    the xpath error on rendering.
    
    Theme being impacted (at least): Avantgarde, Graphene & Nano
    
    Note that when installing one of those themes for the first time on a website,
    the error won't occur as `_reset_default_config()` will be called through
    `_theme_remove()`.
    
    Note that this fix will ensure the correct template is set (and all others are
    disabled), but the scss variable won't be correctly set (as it would be if that
    template change was done through the right panel).
    This is not that much of a problem (considering what it solves), any later
    change from the user through the right panel will solve that mismatch.
    
    Fixes https://github.com/odoo/upgrade/pull/3048
    task-2593407
    opw-2680866
    opw-2685951
    opw-2685124
    opw-2679040
    
    Part-of: odoo/odoo#81181

--- addons/website/models/theme_models.py
+++ addons/website/models/theme_models.py
@@ -192,6 +192,32 @@ class Theme(models.AbstractModel):
     _description = 'Theme Utils'
     _auto = False
 
+    _header_templates = [
+        'website.template_header_hamburger',
+        'website.template_header_vertical',
+        'website.template_header_sidebar',
+        'website.template_header_slogan',
+        'website.template_header_contact',
+        'website.template_header_boxed',
+        'website.template_header_centered_logo',
+        'website.template_header_image',
+        'website.template_header_hamburger_full',
+        'website.template_header_magazine',
+        # Default one, keep it last
+        'website.template_header_default',
+    ]
+    _footer_templates = [
+        'website.template_footer_descriptive',
+        'website.template_footer_centered',
+        'website.template_footer_links',
+        'website.template_footer_minimalist',
+        'website.template_footer_contact',
+        'website.template_footer_call_to_action',
+        'website.template_footer_headline',
+        # Default one, keep it last
+        'website.footer_custom',
+    ]
+
     def _post_copy(self, mod):
         # Call specific theme post copy
         theme_post_copy = '_%s_post_copy' % mod.name
@@ -225,27 +251,14 @@ class Theme(models.AbstractModel):
         self.disable_asset('Ripple effect JS')
 
         # Reinitialize header templates
-        self.disable_view('website.template_header_hamburger')
-        self.disable_view('website.template_header_vertical')
-        self.disable_view('website.template_header_sidebar')
-        self.disable_view('website.template_header_slogan')
-        self.disable_view('website.template_header_contact')
-        self.disable_view('website.template_header_boxed')
-        self.disable_view('website.template_header_centered_logo')
-        self.disable_view('website.template_header_image')
-        self.disable_view('website.template_header_hamburger_full')
-        self.disable_view('website.template_header_magazine')
-        self.enable_view('website.template_header_default')
+        for view in self._header_templates[:-1]:
+            self.disable_view(view)
+        self.enable_view(self._header_templates[-1])
 
         # Reinitialize footer templates
-        self.disable_view('website.template_footer_descriptive')
-        self.disable_view('website.template_footer_centered')
-        self.disable_view('website.template_footer_links')
-        self.disable_view('website.template_footer_minimalist')
-        self.disable_view('website.template_footer_contact')
-        self.disable_view('website.template_footer_call_to_action')
-        self.disable_view('website.template_footer_headline')
-        self.enable_view('website.footer_custom')
+        for view in self._footer_templates[:-1]:
+            self.disable_view(view)
+        self.enable_view(self._footer_templates[-1])
 
         # Reinitialize footer scrolltop template
         self.disable_view('website.option_footer_scrolltop')
@@ -301,6 +314,12 @@ class Theme(models.AbstractModel):
 
     @api.model
     def enable_view(self, xml_id):
+        if xml_id in self._header_templates:
+            for view in self._header_templates:
+                self.disable_view(view)
+        elif xml_id in self._footer_templates:
+            for view in self._footer_templates:
+                self.disable_view(view)
         self._toggle_view(xml_id, True)
 
     @api.model
