e7b94fd19f289d5560ea39ef6eaeb70dd2155b00
Arthur Gossuin (goa) <goa@odoo.com>
2021-12-09 08:37:07 +0000

    [IMP] delivery: add product_template.country_of_origin field
    The origin of goods is required for the commercial invoice generation with
    delivery providers. Until then we were using the warehouse country, but the
    origin of goods is a different kind of field, as explained in this link:
    [https://ec.europa.eu/taxation_customs/customs-4/international-affairs/origin-goods_en]( https://ec.europa.eu/taxation_customs/customs-4/international-affairs/origin-goods_en)
    
    This commit is part of more important upcoming changes brought in Odoo
    enterprise, especially with Fedex, Easypost, DHL and UPS commercial
    invoices.
    
    task-2701428
    
    closes odoo/odoo#80900
    
    Signed-off-by: Arnold Moyaux <arm@odoo.com>

--- addons/delivery/models/product_template.py
+++ addons/delivery/models/product_template.py
@@ -11,3 +11,9 @@ class ProductTemplate(models.Model):
         string="HS Code",
         help="Standardized code for international shipping and goods declaration. At the moment, only used for the FedEx shipping provider.",
     )
+    country_of_origin = fields.Many2one(
+        'res.country',
+        'Origin of Goods',
+        help="Rules of origin determine where goods originate, i.e. not where they have been shipped from, but where they have been produced or manufactured.\n"
+             "As such, the ‘origin’ is the 'economic nationality' of goods traded in commerce.",
+    )
