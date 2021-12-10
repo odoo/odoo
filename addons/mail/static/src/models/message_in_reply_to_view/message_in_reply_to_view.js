e70e7dd548158ec0a405bfe099c5c1fde0c13617
Didier (did) <did@odoo.com>
2021-12-10 11:54:16 +0100

    [FIX] mail: use a placeholder when replying to empty message
    Before the PR, when replying to an empty (only attachment) nothing was display
    in the message reply preview. This PR introduce a placeholder to handle this
    case.
    
    Also taking to opportunity to add missing translations.
    
    task-2664815

--- addons/mail/static/src/models/message_in_reply_to_view/message_in_reply_to_view.js
+++ addons/mail/static/src/models/message_in_reply_to_view/message_in_reply_to_view.js
@@ -42,9 +42,39 @@ function factory(dependencies) {
             threadView.addComponentHint('highlight-reply', parentMessageView);
         }
 
+        /**
+         * @private
+         * @returns {boolean}
+         */
+        _computeHasAttachmentBackLink() {
+            const parentMessage = this.messageView.message.parentMessage;
+            return parentMessage.isBodyEmpty && parentMessage.hasAttachments;
+        }
+
+        /**
+         * @private
+         * @returns {boolean}
+         */
+        _computeHasBodyBackLink() {
+            return !this.messageView.message.parentMessage.isBodyEmpty;
+        }
+
     }
 
     MessageInReplyToView.fields = {
+        /**
+         * Determines if the reply has a back link to an attachment only
+         * message.
+         */
+        hasAttachmentBackLink: attr({
+            compute: '_computeHasAttachmentBackLink',
+        }),
+        /**
+         * Determines if the reply has a back link to a non-empty body.
+         */
+        hasBodyBackLink: attr({
+            compute: '_computeHasBodyBackLink',
+        }),
         messageView: one2one('mail.message_view', {
             inverse: 'messageInReplyToView',
             readonly: true,
