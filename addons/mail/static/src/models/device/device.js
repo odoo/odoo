d7d5d522c257fedb78d4a8cd0652df6af9088a66
Louis Wicket (wil) <wil@odoo.com>
2021-12-13 15:01:57 +0000

    [IMP] mail: rename device model
    Rename javascript model `mail.device` to `Device` in order to
    distinguish javascript models from python models.
    
    Part of task-2701674.
    
    closes odoo/odoo#81332
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/models/device/device.js
+++ addons/mail/static/src/models/device/device.js
@@ -4,7 +4,7 @@ import { registerModel } from '@mail/model/model_core';
 import { attr } from '@mail/model/model_field';
 
 registerModel({
-    name: 'mail.device',
+    name: 'Device',
     identifyingFields: ['messaging'],
     lifecycleHooks: {
         _created() {
