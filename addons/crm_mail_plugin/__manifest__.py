8b6f438b7b816544fc3d4b67f5c9ebe9b382bcd1
Julien Castiaux <juc@odoo.com>
2021-12-13 08:16:24 +0000

    [FIX] *mail_plugin: unused i18n path in manifest
    *: mail_plugin, crm_mail_plugin, project_mail_plugin
    
    The code that is discovering translatable strings is defined in
    [translate.py]. It does lookup in all python files and in all javascript
    files defined under 'static/src/'.
    
    The top-level `web.assets_backend` directive in the manifest is useless
    and have been removed.
    
    [translate.py]: https://github.com/odoo/odoo/blob/8b468e1f7a3dfeb2bcfa83bff55c4b5adf6b2212/odoo/tools/translate.py#L1138-L1147
    
    Part-of: odoo/odoo#79977

--- addons/crm_mail_plugin/__manifest__.py
+++ addons/crm_mail_plugin/__manifest__.py
@@ -17,9 +17,6 @@
         'views/crm_mail_plugin_lead.xml',
         'views/crm_lead_views.xml'
     ],
-    'web.assets_backend': [
-        'crm_mail_plugin/static/src/to_translate/**/*',
-    ],
     'installable': True,
     'application': False,
     'auto_install': True,
