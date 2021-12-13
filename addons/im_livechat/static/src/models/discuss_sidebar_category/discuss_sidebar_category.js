55d00016477329d31d69eb54bf84c0a29325788a
Louis Wicket (wil) <wil@odoo.com>
2021-12-13 15:30:05 +0000

    [IMP] im_livechat, mail: rename discuss sidebar category model
    Rename javascript model `mail.discuss_sidebar_category` to
    `DiscussSidebarCategory` in order to distinguish javascript models from
    python models.
    
    Part of task-2701674.
    
    closes odoo/odoo#81345
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/src/models/discuss_sidebar_category/discuss_sidebar_category.js
+++ addons/im_livechat/static/src/models/discuss_sidebar_category/discuss_sidebar_category.js
@@ -5,13 +5,13 @@ import { one2one } from '@mail/model/model_field';
 // ensure that the model definition is loaded before the patch
 import '@mail/models/discuss_sidebar_category/discuss_sidebar_category';
 
-addFields('mail.discuss_sidebar_category', {
+addFields('DiscussSidebarCategory', {
     discussAsLivechat: one2one('Discuss', {
         inverse: 'categoryLivechat',
         readonly: true,
     }),
 });
 
-patchIdentifyingFields('mail.discuss_sidebar_category', identifyingFields => {
+patchIdentifyingFields('DiscussSidebarCategory', identifyingFields => {
     identifyingFields[0].push('discussAsLivechat');
 });
