17d608ade368d423a01718c5aed5399c3682932c
roen-odoo <roen@odoo.com>
2021-12-17 12:43:22 +0000

    [FIX] sale: Payment had wrong sales team
    Current behavior :
    When creating payment from invoice, the payment had the wrong sales team id
    
    Steps to reproduce :
    - Create atleast 2 invoices with different sales team and same partner
    - Register payments for every invoice
    - Go in payments list view
    - Filter on partner and group by sales team
    - All the payments are grouped in the same sales team
    
    opw-2681041
    
    X-original-commit: 26e035d4394a86b987930a357c3a5dcfbd97f56d

--- addons/sale/wizard/__init__.py
+++ addons/sale/wizard/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import account_payment_register
 from . import mail_compose_message
 from . import payment_acquirer_onboarding_wizard
 from . import sale_make_invoice_advance
