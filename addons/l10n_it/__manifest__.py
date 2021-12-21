53a0aa4d4c5f0fd2323bb13de74ab295d750cd75
smdc-odoo <smdc@odoo.com>
2021-12-21 17:13:46 +0000

    [IMP] l10n_it: Change the Italian Chart of account
    These changes have been made:
    	- Each account name now starts with a upper case letter. This was change to be more coherent with the other localisation
    	- There was too much account that allowed reconciliation. Only some should so we have change that (in particular, receivables and payable accounts)
    	- We have added tags to account to more easily manage the control domains. By doing so, the control domains can be more precise about where the error lies
    	- We have change the type of some account. Before that there was too much 'Current Assets' or 'Liabilities' without distinction for 'Fixed Assets', 'Equity', 'Off Balance'...
    
    It also contains a local migration script to update current installation of the Chart of Account by adding the tags.
    
    task-2468834
    
    closes odoo/odoo#84857
    
    X-original-commit: 534c0384ff64ef141168ee2dd4152116ae0287f8
    Related: odoo/enterprise#24498
    Signed-off-by: Olivier Colson <oco@odoo.com>

--- addons/l10n_it/__manifest__.py
+++ addons/l10n_it/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Italy - Accounting',
-    'version': '0.2',
+    'version': '0.3',
     'depends': [
         'account',
         'base_iban',
@@ -19,6 +19,7 @@ Italian accounting chart and localization.
     'category': 'Accounting/Localizations/Account Charts',
     'website': 'http://www.odoo.com/',
     'data': [
+        'data/account_account_tag.xml',
         'data/account_chart_template.xml',
         'data/account.account.template.csv',
         'data/account_chart_template_accounts.xml',
