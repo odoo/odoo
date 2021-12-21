aa41b953ca8369d5864a4a52d73f82cf552764f1
Didier (did) <did@odoo.com>
2021-12-21 08:54:25 +0100

    Empty FileUploader model

---
+++ addons/mail/static/src/models/file_uploader/file_uploader.js
@@ -0,0 +1,16 @@
+/** @odoo-module **/
+
+import { registerModel } from '@mail/model/model_core';
+import { attr } from '@mail/model/model_field';
+
+registerModel({
+    name: 'FileUploader',
+    identifyingFields: ['id'],
+    lifecycleHooks: {},
+    recordMethods: {
+
+    },
+    fields: {
+
+    },
+});
