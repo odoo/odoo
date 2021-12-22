c0a2d7a2c50eb1bc21e3227462d035da7aa2306e
Didier (did) <did@odoo.com>
2021-12-22 12:52:14 +0100

    [IMP] mail: move activity getters inside activity view
    task-2698446

--- addons/mail/static/src/components/activity/activity.js
+++ addons/mail/static/src/components/activity/activity.js
@@ -2,12 +2,6 @@
 
 import { registerMessagingComponent } from '@mail/utils/messaging_component';
 
-import {
-    auto_str_to_date,
-    getLangDateFormat,
-    getLangDatetimeFormat,
-} from 'web.time';
-
 const { Component } = owl;
 
 export class Activity extends Component {
@@ -21,66 +15,6 @@ export class Activity extends Component {
      */
     get activityView() {
         return this.messaging && this.messaging.models['ActivityView'].get(this.props.activityViewLocalId);
-    }
-
-    /**
-     * @returns {string}
-     */
-    get assignedUserText() {
-        return _.str.sprintf(this.env._t("for %s"), this.activityView.activity.assignee.nameOrDisplayName);
-    }
-
-    /**
-     * @returns {string}
-     */
-    get delayLabel() {
-        const today = moment().startOf('day');
-        const momentDeadlineDate = moment(auto_str_to_date(this.activityView.activity.dateDeadline));
-        // true means no rounding
-        const diff = momentDeadlineDate.diff(today, 'days', true);
-        if (diff === 0) {
-            return this.env._t("Today:");
-        } else if (diff === -1) {
-            return this.env._t("Yesterday:");
-        } else if (diff < 0) {
-            return _.str.sprintf(this.env._t("%d days overdue:"), Math.abs(diff));
-        } else if (diff === 1) {
-            return this.env._t("Tomorrow:");
-        } else {
-            return _.str.sprintf(this.env._t("Due in %d days:"), Math.abs(diff));
-        }
-    }
-
-    /**
-     * @returns {string}
-     */
-    get formattedCreateDatetime() {
-        const momentCreateDate = moment(auto_str_to_date(this.activityView.activity.dateCreate));
-        const datetimeFormat = getLangDatetimeFormat();
-        return momentCreateDate.format(datetimeFormat);
-    }
-
-    /**
-     * @returns {string}
-     */
-    get formattedDeadlineDate() {
-        const momentDeadlineDate = moment(auto_str_to_date(this.activityView.activity.dateDeadline));
-        const datetimeFormat = getLangDateFormat();
-        return momentDeadlineDate.format(datetimeFormat);
-    }
-
-    /**
-     * @returns {string}
-     */
-    get MARK_DONE() {
-        return this.env._t("Mark Done");
-    }
-
-    /**
-     * @returns {string}
-     */
-    get summary() {
-        return _.str.sprintf(this.env._t("“%s”"), this.activityView.activity.summary);
     }
 
 }
