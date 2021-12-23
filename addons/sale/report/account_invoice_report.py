132a163d4d76f17adff2519a425515455020fb1c
Victor Feyens <vfe@odoo.com>
2021-12-23 17:19:19 +0100

    [IMP] sale: report folder code cleanup
    Code indentation
    Attributes ordering
    qweb code cleanup
    ...

--- addons/sale/report/account_invoice_report.py
+++ addons/sale/report/account_invoice_report.py
@@ -7,7 +7,7 @@ from odoo import fields, models
 class AccountInvoiceReport(models.Model):
     _inherit = 'account.invoice.report'
 
-    team_id = fields.Many2one('crm.team', string='Sales Team')
+    team_id = fields.Many2one(comodel_name='crm.team', string="Sales Team")
 
     def _select(self):
-        return super(AccountInvoiceReport, self)._select() + ", move.team_id as team_id"
+        return super()._select() + ", move.team_id as team_id"
