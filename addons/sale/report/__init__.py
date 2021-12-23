2ab3c94049982bc6986bf36c2c45434db8fad3c3
Victor Feyens <vfe@odoo.com>
2021-12-23 16:25:26 +0100

    [MOV] sale: correct file name

--- addons/sale/report/__init__.py
+++ addons/sale/report/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import sale_report
-from . import invoice_report
+from . import account_invoice_report
 from . import report_all_channels_sales
+from . import sale_report
