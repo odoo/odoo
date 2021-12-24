19730047b27f4acc1f2dcca73a58be3d24d83778
Priyanka Kakadiya <pka@odoo.com>
2021-12-24 13:32:09 +0000

    [FIX] mail: fix back button issue to close channel
    Before this commit:
    
    When switching to mobile view and refreshing the channel in discuss than back
    arrow button is not working
    
    After this commit:
    
    Back arrow button will close the channel
    
    Task-2685187
    
    X-original-commit: 7d8851cf9ebcae97a29d438c206b51430a13f226

--- addons/mail/static/src/components/discuss/discuss.js
+++ addons/mail/static/src/components/discuss/discuss.js
@@ -26,8 +26,6 @@ export class Discuss extends Component {
         }
         if (this.discussView.discuss.thread) {
             this.trigger('o-push-state-action-manager');
-        } else if (!this._activeThreadCache) {
-            this.discussView.discuss.openInitThread();
         }
         if (
             this.discussView.discuss.thread &&
@@ -38,7 +36,6 @@ export class Discuss extends Component {
         ) {
             this.trigger('o-show-rainbow-man');
         }
-        this._activeThreadCache = this.discussView.discuss.threadView && this.discussView.discuss.threadView.threadCache;
         this._updateLocalStoreProps();
     }
 
