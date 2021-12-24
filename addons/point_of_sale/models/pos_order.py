boan-odoo/odoo-1

[FIX] point_of_sale: assign SML with correct SN
In case the product is tracked and the "Ship Later" feature is enabled,
the lots defined on the POS order are not the same that the ones on the
SMLs of the associated picking

To reproduce the issue:
(Use demo data)
1. Create a product P:
    - Type: Storable
    - Tracked by lots
    - Available in POS
2. Update P's quantity:
    - 2 x Lot01
    - 2 x Lot02
3. Edit the existing POS:
    - Enable the "Ship Later" feature
4. Start a POS session
5. Add some products:
    - 1 x P (Lot01)
    - 1 x p (Lot02)
6. Process the order with the option "Ship Lated" selected
7. Open the associated delivery order

Error: The reserved lots are incorrect: 2 x Lot01 instead of 1 x Lot01 +
1 x Lot02

When creating such a POS order, if the feature "Ship Later" is used, a
method creates a procurement for each POS order line thanks to
`_launch_stock_rule_from_pos_order_lines`
https://github.com/odoo/odoo/blob/72d6431eb26654b697fc0379f4b6d7e305bb79fc/addons/point_of_sale/models/pos_order.py#L677-L680
Then, these procurements are managed by the standard process, which does
not consider the defined lots. This is the reason why it simply uses the
available quantity in the first lot.

OPW-2704352

addons/point_of_sale/models/pos_order.py
@@ -3,6 +3,7 @@
 import logging
 from datetime import timedelta
 from functools import partial
+from itertools import groupby
 
 import psycopg2
 import pytz
@@ -1084,6 +1085,15 @@ class PosOrderLine(models.Model):
             if pickings_to_confirm:
                 # Trigger the Scheduler for Pickings
                 pickings_to_confirm.action_confirm()
+                tracked_lines = order.lines.filtered(lambda l: l.product_id.tracking != 'none')
+                lines_by_tracked_product = groupby(sorted(tracked_lines, key=lambda l: l.product_id.id), key=lambda l: l.product_id.id)
+                mls_to_unlink = self.env['stock.move.line']
+                for product, lines in lines_by_tracked_product:
+                    lines = self.env['pos.order.line'].concat(*lines)
+                    moves = pickings_to_confirm.move_lines.filtered(lambda m: m.product_id in tracked_lines.product_id)
+                    mls_to_unlink |= moves.move_line_ids
+                    moves._add_mls_related_to_order(lines, are_qties_done=False)
+                mls_to_unlink.unlink()
         return True
 
     def _is_product_storable_fifo_avco(self):

odoo/odoo

X-original-commit: bc59c4eb7ef6bc216474f39ef8e2634b4ab5ff42

addons/point_of_sale/models/pos_order.py
@@ -3,6 +3,7 @@
 import logging
 from datetime import timedelta
 from functools import partial
+from itertools import groupby
 
 import psycopg2
 import pytz
@@ -1084,6 +1085,15 @@ class PosOrderLine(models.Model):
             if pickings_to_confirm:
                 # Trigger the Scheduler for Pickings
                 pickings_to_confirm.action_confirm()
+                tracked_lines = order.lines.filtered(lambda l: l.product_id.tracking != 'none')
+                lines_by_tracked_product = groupby(sorted(tracked_lines, key=lambda l: l.product_id.id), key=lambda l: l.product_id.id)
+                mls_to_unlink = self.env['stock.move.line']
+                for product, lines in lines_by_tracked_product:
+                    lines = self.env['pos.order.line'].concat(*lines)
+                    moves = pickings_to_confirm.move_ids.filtered(lambda m: m.product_id in tracked_lines.product_id)
+                    mls_to_unlink |= moves.move_line_ids
+                    moves._add_mls_related_to_order(lines, are_qties_done=False)
+                mls_to_unlink.unlink()
         return True
 
     def _is_product_storable_fifo_avco(self):