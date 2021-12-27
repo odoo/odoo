744e64a2b29c8491c124498dda0d4093a6de74fc
Martin Trigaux <mat@odoo.com>
2021-12-27 09:43:32 +0000

    [FIX] core: python 3.10 support of collections
    collections.Set was deprecated since 3.6 and removed at 3.10
    https://github.com/python/cpython/blob/3.9/Lib/collections/__init__.py#L62-L65
    
    closes odoo/odoo#85919
    
    X-original-commit: 95358dd00ec8a0a308bedda2f6bce6563eefb6e6
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- odoo/models.py
+++ odoo/models.py
@@ -6666,9 +6666,9 @@ Fields:
         return
 
 
-collections.Set.register(BaseModel)
+collections.abc.Set.register(BaseModel)
 # not exactly true as BaseModel doesn't have index or count
-collections.Sequence.register(BaseModel)
+collections.abc.Sequence.register(BaseModel)
 
 class RecordCache(MutableMapping):
     """ A mapping from field names to values, to read and update the cache of a record. """
