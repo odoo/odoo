62e514fd3d4a87c8e33baeea3b1c0e6582940a8e
Xavier BOL (xbo) <xbo@odoo.com>
2021-12-28 14:31:45 +0000

    [FIX] sale_project,sale_timesheet: get SOLs linked to project
    Before this commit, when we need to get all SOL linked to the project.
    That is the SOL linked to the project and also the SOL linked to all
    tasks of this project.
    
    Technically, a fetch is made to get the SOL of the project, a fetch to
    get all tasks of the project and then a fetch to get the SOL linked to
    all tasks fetched, finally the union is made to avoid having duplicate
    sale order items in the recordset
    (`self.sale_line_id | self.tasks.sale_line_id`).
    
    This way could be a performance issue if the project contained many
    active tasks and so the project update could take more and more time to
    compute the sold items shown in the right side panel of project update
    view.
    
    This commit does a query builder to fetch the SOLs linked to the project
    and its active tasks to avoid doing the prefetchs.
    
    part of task-2710808
    
    Part-of: odoo/odoo#85185

--- addons/sale_timesheet/models/project.py
+++ addons/sale_timesheet/models/project.py
@@ -306,7 +306,7 @@ class Project(models.Model):
         }
 
     def _get_sale_order_lines(self):
-        sale_orders = self.sale_order_id | self.tasks.sale_order_id
+        sale_orders = self._get_sale_orders()
         return self.env['sale.order.line'].search([('order_id', 'in', sale_orders.ids), ('is_service', '=', True), ('is_downpayment', '=', False)], order='id asc')
 
     def _get_sold_items(self):
@@ -341,6 +341,29 @@ class Project(models.Model):
             'color': 'red' if remaining < 0 else 'black',
         }
         return sold_items
+
+    def _fetch_sale_order_item_ids(self, domain_per_model=None, limit=None, offset=None):
+        if not self or not self.filtered('allow_billable'):
+            return []
+        return super()._fetch_sale_order_item_ids(domain_per_model, limit, offset)
+
+    def _get_sale_order_items_query(self, domain_per_model=None):
+        billable_project_domain = [('allow_billable', '=', True)]
+        if domain_per_model is None:
+            domain_per_model = {
+                'project.project': billable_project_domain,
+                'project.task': billable_project_domain,
+            }
+        else:
+            domain_per_model['project.project'] = expression.AND([
+                domain_per_model.get('project.project', []),
+                billable_project_domain,
+            ])
+            domain_per_model['project.task'] = expression.AND([
+                domain_per_model.get('project.task', []),
+                billable_project_domain,
+            ])
+        return super()._get_sale_order_items_query(domain_per_model)
 
     def _get_profitability_items(self):
         if not self.user_has_groups('project.group_project_manager'):
