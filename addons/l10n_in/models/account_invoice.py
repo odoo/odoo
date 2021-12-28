49a3813356ddffb6d4654fb8efecbf9bf8daab5f
Jigar Vaghela <jva@odoo.com>
2021-12-28 17:39:45 +0530

    [IMP] l10n_in*: remove l10n_in_shipping_gstin
    Companies only get a tax credit on the bill to GST number(vat), not on the shipping GST number.
    
    task - 2723111

--- addons/l10n_in/models/account_invoice.py
+++ addons/l10n_in/models/account_invoice.py
@@ -49,16 +49,6 @@ class AccountMove(models.Model):
             return self.env.ref('l10n_in.state_in_ot')
         return partner.state_id
 
-    def _l10n_in_get_shipping_partner(self):
-        """Overwrite in sale"""
-        self.ensure_one()
-        return self.partner_id
-
-    @api.model
-    def _l10n_in_get_shipping_partner_gstin(self, shipping_partner):
-        """Overwrite in sale"""
-        return shipping_partner.vat
-
     def _post(self, soft=True):
         """Use journal type to define document type because not miss state in any entry including POS entry"""
         posted = super()._post(soft)
@@ -76,19 +66,16 @@ class AccountMove(models.Model):
             elif move.journal_id.type == 'purchase':
                 move.l10n_in_state_id = company_unit_partner.state_id
 
-            shipping_partner = move._l10n_in_get_shipping_partner()
-            # In case of shipping address does not have GSTN then also check customer(partner_id) GSTN
-            # This happens when Bill-to Ship-to transaction where shipping(Ship-to) address is unregistered and customer(Bill-to) is registred.
-            move.l10n_in_gstin = move._l10n_in_get_shipping_partner_gstin(shipping_partner) or move.partner_id.vat
+            move.l10n_in_gstin = move.partner_id.vat
             if not move.l10n_in_gstin and move.l10n_in_gst_treatment in ['regular', 'composition', 'special_economic_zone', 'deemed_export']:
                 raise ValidationError(_(
                     "Partner %(partner_name)s (%(partner_id)s) GSTIN is required under GST Treatment %(name)s",
-                    partner_name=shipping_partner.name,
-                    partner_id=shipping_partner.id,
+                    partner_name=move.partner_id.name,
+                    partner_id=move.partner_id.id,
                     name=gst_treatment_name_mapping.get(move.l10n_in_gst_treatment)
                 ))
             if move.journal_id.type == 'sale':
-                move.l10n_in_state_id = self._l10n_in_get_indian_state(shipping_partner)
+                move.l10n_in_state_id = self._l10n_in_get_indian_state(move.partner_id)
                 if not move.l10n_in_state_id:
                     move.l10n_in_state_id = self._l10n_in_get_indian_state(move.partner_id)
                 #still state is not set then assumed that transaction is local like PoS so set state of company unit
