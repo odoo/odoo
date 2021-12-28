e5851b9c2d2e7e527f84f58bc0211329af856843
bat-odoo <bat@odoo.com>
2021-12-28 11:32:58 +0530

    [ADD] l10n_in: introduce TCS report and taxes
    Return filing for the TCS is per category and section described here,
    https://bit.ly/3sHCjBB
    
    So we create a report line and taxes per category and section.
    
    Task-2610237
    PR-#75613

--- addons/l10n_in/__manifest__.py
+++ addons/l10n_in/__manifest__.py
@@ -28,6 +28,8 @@ Sheet, now only Vertical format has been permitted Which is Supported By Odoo.
         'data/account.account.template.csv',
         'data/l10n_in_chart_post_data.xml',
         'data/account_tax_template_data.xml',
+        'data/account_tcs_tax_report_data.xml',
+        'data/account_tcs_tax_template_data.xml',
         'data/account_tds_tax_report_data.xml',
         'data/account_tds_tax_template_data.xml',
         'data/account_fiscal_position_data.xml',
