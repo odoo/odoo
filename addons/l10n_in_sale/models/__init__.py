c20bdf0b4c783f81d2cf52fc9ea1149aaf1b974d
Jigar Vaghela <jva@odoo.com>
2021-12-28 12:09:45 +0000

    [IMP] l10n_in*: remove l10n_in_shipping_gstin
    Companies only get a tax credit on the bill to GST number(vat), not on the shipping GST number.
    
    task - 2723111
    
    closes odoo/odoo#81977
    
    Related: odoo/upgrade#3493
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_in_sale/models/__init__.py
+++ addons/l10n_in_sale/models/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import res_partner
 from . import sale_order
-from . import account_move
