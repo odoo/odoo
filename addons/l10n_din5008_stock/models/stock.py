15461b4225fa755a3647633c512071ee65e2596b
Merlin (megu) <megu@odoo.com>
2022-01-04 11:05:33 +0000

    [FIX] l10n_de_stock,stock: delivery address in the top left
    'Delivery Slip' report with the document layout 'DIN5008' needs to have
    the delivery address in the hole of the envelope
    
    Steps to reproduce:
    1. Install Sales app as well as Drop Shipping and l10n_de module
    2. Go to General Settings -> Document Layout and select
    external_layout_DIN5008
    3. Specify a Dropship route for a product
    4. Create and confirm an order for that product
    5. Open the purchase order with the 'Purchase' tab
    6. Confirm the corresponding purchase order
    7. Open the 'Receipt' tab
    8. Print the Delivery Slip
    9. The document reference is above the address instead of under it
    
    Solution:
    Add the `l10n_de_addresses` computed field and set the addresses through
     `<t t-set="address">` and `<t t-set="information_block"/>`
    
    OPW-2709866
    OPW-2715851
    
    X-original-commit: ec90e9bfee208761f8247b55260895990b7614ed

--- addons/l10n_din5008_stock/models/stock.py
+++ addons/l10n_din5008_stock/models/stock.py
@@ -6,6 +6,7 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     l10n_din5008_template_data = fields.Binary(compute='_compute_l10n_din5008_template_data')
+    l10n_din5008_addresses = fields.Binary(compute='_compute_l10n_din5008_addresses')
 
     def _compute_l10n_din5008_template_data(self):
         for record in self:
@@ -16,3 +17,15 @@ class StockPicking(models.Model):
                 data.append((_("Shipping Date"), format_date(self.env, record.date_done)))
             else:
                 data.append((_("Shipping Date"), format_date(self.env, record.scheduled_date)))
+
+    def _compute_l10n_din5008_addresses(self):
+        for record in self:
+            record.l10n_din5008_addresses = data = []
+            if record.partner_id:
+                if record.picking_type_id.code == 'incoming':
+                    data.append((_('Vendor Address:'), record.partner_id))
+                if record.picking_type_id.code == 'internal':
+                    data.append((_('Warehouse Address:'), record.partner_id))
+                if record.picking_type_id.code == 'outgoing' and record.move_ids_without_package and record.move_ids_without_package[0].partner_id \
+                        and record.move_ids_without_package[0].partner_id.id != record.partner_id.id:
+                    data.append((_('Customer Address:'), record.partner_id))
