815aa5e31eab30d514a7ab418ba028261cea0d7e
Adrien Widart <awt@odoo.com>
2022-01-04 09:51:24 +0100

    [FIX] stock_account: base reversed anglo-saxon AML on original one
    When reversing an invoice that contains some anglo-saxo AML, the new
    anglo-saxo AML should have the same unit price than the original one
    
    To reproduce the issue:
    (Need account_accountant,purchase)
    1. Create a product category PC:
        - Costing Method: AVCO
        - Inventory Valuation: Automated
    2. Create a product P:
        - Type: Storable
        - Product Category: PC
    3. Create a purchase order PO01 with 2 x P for $10
    4. Confirm PO01 and process the receipt
    5. Create and Post an invoice INV01 with 2 x P
        - As listed in the journal items, the anglo-saxo lines have been
    generated with a value of $20
    6. Create a purchase order PO02 with 2 x P for $20
        - The cost of P becomes $15
    7. Confirm PO02 and process the receipt
    8. Add a credit note to INV01:
        - Credit Method: Partial Refund
    9. Set the quantity of P to 1
    10. Post the new invoice INV02
    
    Error: The anglo-saxo lines are listed in the journal items, which is
    correct, but their value is $15 (the new cost of P) while it should be
    $10
    
    When reversing an invoice with the "partial refund" method, the
    anglo-saxo lines are generated via the regular flow (as if it were a
    "standard" invoice). Therefore, when getting the unit price of the line,
    it uses the product's standard price. In such situation, it should use
    the unit price of the original line.
    
    OPW-2646926
    OPW-2628215

--- addons/stock_account/models/account_move.py
+++ addons/stock_account/models/account_move.py
@@ -238,4 +238,8 @@ class AccountMoveLine(models.Model):
         self.ensure_one()
         if not self.product_id:
             return self.price_unit
-        return self.product_id.with_context(force_company=self.company_id.id)._stock_account_get_anglo_saxon_price_unit(uom=self.product_uom_id)
+        original_line = self.move_id.reversed_entry_id.line_ids.filtered(lambda l: l.is_anglo_saxon_line
+            and l.product_id == self.product_id and l.product_uom_id == self.product_uom_id and l.price_unit >= 0)
+        original_line = original_line and original_line[0]
+        return original_line.price_unit if original_line \
+            else self.product_id.with_context(force_company=self.company_id.id)._stock_account_get_anglo_saxon_price_unit(uom=self.product_uom_id)
