83d15bbd94a46d3371a1149d32ac6c7307636c28
roen-odoo <roen@odoo.com>
2022-01-04 14:34:12 +0100

    [FIX] stock: Owner filter crash
    Current behavior:
    Filter "Owner is not set" on Package in inventory app was causing a traceback
    
    Steps to reproduce:
    - Go in inventory app
    - Go in package
    - Apply a filter "Owner is not set"
    - You get a traceback
    
    opw-2714726

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -733,7 +733,7 @@ class QuantPackage(models.Model):
         else:
             packs = self.search([('quant_ids', operator, value)])
         if packs:
-            return [('id', 'parent_of', packs.ids)]
+            return [('id', 'in', packs.ids)]
         else:
             return [('id', '=', False)]
 
