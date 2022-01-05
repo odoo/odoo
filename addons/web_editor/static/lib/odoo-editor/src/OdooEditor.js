b467e4df8cfa1d1424274168c1aa4b5f191263e5
Nicolas Bayet <nby@odoo.com>
2022-01-05 13:04:08 +0000

    [FIX] web_editor: remove image toolbar when removing image
    Task-2728793
    
    closes odoo/odoo#82881
    
    X-original-commit: 0ff6f919e10388f7fd6d8efbfb0c8f9644b2505e
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -1794,6 +1794,12 @@ export class OdooEditor extends EventTarget {
     // TOOLBAR
     // =======
 
+    toolbarHide() {
+        this._updateToolbar(false);
+    }
+    toolbarShow() {
+        this._updateToolbar(true);
+    }
     /**
      * @private
      * @param {boolean} [show]
