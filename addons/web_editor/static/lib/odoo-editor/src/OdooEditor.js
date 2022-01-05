c562c1d6b62a46858248b5a4efb466b7c79e6e11
Nicolas Bayet <nby@odoo.com>
2022-01-05 12:23:10 +0000

    [FIX] web_editor: change fix for scrolling powerbox
    The fix to add an `overflow: hidden` in order to prevent the browser
    from chaning the scroll create a glitch where the scrollbar dissapear
    when it is overflow: hidden.
    A new css property exists to specify exactly this behavior
    (`overscroll-behavior: contain;`).
    
    Task-2728791
    
    closes odoo/odoo#82877
    
    X-original-commit: 4cfa9e786d41a27d1bc850eff4fc89e1c4b32974
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -1625,7 +1625,6 @@ export class OdooEditor extends EventTarget {
     // ===========
 
     _createCommandBar() {
-        this.options.noScrollSelector = this.options.noScrollSelector || 'body';
         this.commandbarTablePicker = new TablePicker({
             document: this.document,
             floating: true,
@@ -1734,16 +1733,6 @@ export class OdooEditor extends EventTarget {
             shouldActivate: () => !!this.options.getPowerboxElement(),
             onActivate: () => {
                 this._beforeCommandbarStepIndex = this._historySteps.length - 1;
-                this.observerUnactive();
-                for (const element of document.querySelectorAll(this.options.noScrollSelector)) {
-                    element.classList.add('oe-noscroll');
-                }
-                for (const element of this.document.querySelectorAll(
-                    this.options.noScrollSelector,
-                )) {
-                    element.classList.add('oe-noscroll');
-                }
-                this.observerActive();
             },
             preValidate: () => {
                 this._historyRevertUntil(this._beforeCommandbarStepIndex);
@@ -1755,16 +1744,6 @@ export class OdooEditor extends EventTarget {
             },
             postValidate: () => {
                 this.historyStep(true);
-            },
-            onStop: () => {
-                this.observerUnactive();
-                for (const element of document.querySelectorAll('.oe-noscroll')) {
-                    element.classList.remove('oe-noscroll');
-                }
-                for (const element of this.document.querySelectorAll('.oe-noscroll')) {
-                    element.classList.remove('oe-noscroll');
-                }
-                this.observerActive();
             },
             commands: [...mainCommands, ...(this.options.commands || [])],
         });
