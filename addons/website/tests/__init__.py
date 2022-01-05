f5331ea9df1972b49791c5e52c5c4c15930d3982
Arthur Detroux (ard) <ard@odoo.com>
2022-01-05 13:45:28 +0000

    [FIX] website: fix editor not starting after creating a website
    Commit [1] introduced an automatic start of edit mode when
    selecting a theme but didn't take into account wether the frontend lang
    of the user was matching that of the website that is supposed to enter
    in edit mode.
    
    Commit [2] introduce a `website_edit` action but also didn't take into
    account that the frontend lang might not be the source's lang.
    
    Steps to reproduce:
    - Create an empty database and install website
    - Add an extra lang on website (e.g FR) and visit the website in that
    lang
    - Go in settings an create a new website, select a theme
    - Redirect is with enable_editor but nothing happens
    
    The editor isn't started if not in the source's lang.
    
    This commit redirect the user to the default_lang of the website
    so that editor can always be started. It also introduces a tour
    to test out the flow.
    
    [1]: https://github.com/odoo/odoo/commit/f474eac4543712ae5b1c7d614f9eff9367445107
    [2]: https://github.com/odoo/odoo/commit/e0cc28dcee977c4aa0c6a7e4ff6c463ab12e55a2
    
    task-2687416
    
    closes odoo/odoo#83508
    
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website/tests/__init__.py
+++ addons/website/tests/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import test_attachment
 from . import test_auth_signup_uninvited
+from . import test_automatic_editor
 from . import test_base_url
 from . import test_converter
 from . import test_crawl
