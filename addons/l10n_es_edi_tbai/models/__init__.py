a31fcbee3bb9909eac1d78a2b25522991bf620bc
Stanislas Gueniffey <stgu@odoo.com>
2022-01-05 11:30:50 +0100

    [ADD] l10n_es_edi_tbai: create module
    Imported from branch 15.0-l10n_es_edi_ticketbai-STGU

---
+++ addons/l10n_es_edi_tbai/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import account_move
+from . import account_edi_format
+from . import l10n_es_edi_tbai_certificate
+from . import res_company
+from . import res_config_settings
+from . import account_tax
