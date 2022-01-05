641e056afb3a0207938c9e86ab45b4ff572c1cc6
Nicolas Bayet <nby@odoo.com>
2022-01-05 13:04:08 +0000

    [FIX] web_editor: remove image toolbar when removing image
    Task-2728793
    
    closes odoo/odoo#82881
    
    X-original-commit: 0ff6f919e10388f7fd6d8efbfb0c8f9644b2505e
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1151,6 +1151,7 @@ const Wysiwyg = Widget.extend({
             }
             $(this.lastMediaClicked).remove();
             this.lastMediaClicked = undefined;
+            this.odooEditor.toolbarHide();
         });
         $toolbar.find('#fa-resize div').click(e => {
             if (!this.lastMediaClicked) {
