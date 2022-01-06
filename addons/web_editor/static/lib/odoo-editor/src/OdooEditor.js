970a93cadcfcfc673e682a5879fd745a9499415b
Nicolas Bayet <nby@odoo.com>
2022-01-06 13:39:28 +0100

    [FIX] web_editor: prevent editor powerbox hint duplication
    Whenever the browser is lagging between multiple enter,
    the command hints were not being updated until the browser
    main thread process the event queue.
    
    Task-2728794
    
    X-original-commit: bfed1edd26312488dc0f45e8982b51ea4d98db76

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -1469,6 +1469,7 @@ export class OdooEditor extends EventTarget {
         const result = this._protect(() => this._applyRawCommand(...args));
         this.sanitize();
         this.historyStep();
+        this._handleCommandHint();
         return result;
     }
     /**
