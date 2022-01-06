77fd5ab55ac138d5b7797ccafd364e2de9eed08c
Michael (mcm) <mcm@odoo.com>
2022-01-06 17:04:36 +0100

    [WIP] web: update many2one after autocomplete rewrite

--- addons/web/static/src/fields/many2one_field.js
+++ addons/web/static/src/fields/many2one_field.js
@@ -10,7 +10,7 @@ import { Domain } from "@web/core/domain";
 import { FormViewDialog } from "@web/views/view_dialogs/form_view_dialog";
 
 const { Component } = owl;
-const { onWillStart, onWillUpdateProps } = owl.hooks;
+const { onWillStart, onWillUpdateProps, useState } = owl.hooks;
 
 export class Many2OneField extends Component {
     setup() {
@@ -21,10 +21,15 @@ export class Many2OneField extends Component {
         this.displayName = "";
         this.extraLines = [];
 
+        this.state = useState({
+            isFloating: !this.props.value,
+        });
+
         onWillStart(async () => {
             [this.displayName, ...this.extraLines] = await this.loadDisplayName(this.props.value);
         });
         onWillUpdateProps(async (nextProps) => {
+            this.state.isFloating = !nextProps.value;
             if (this.props.value !== nextProps.value) {
                 [this.displayName, ...this.extraLines] = await this.loadDisplayName(
                     nextProps.value
@@ -43,11 +48,7 @@ export class Many2OneField extends Component {
         return this.props.options.no_open;
     }
     get hasExternalButton() {
-        return !this.noOpen && !this.isFloating && !!this.props.value;
-    }
-    get isFloating() {
-        return false;
-        // return !!this.inputRef.el && this.displayName !== this.inputRef.el.value;
+        return !this.noOpen && !!this.props.value && !this.state.isFloating;
     }
     get canCreate() {
         const attr =
@@ -64,12 +65,22 @@ export class Many2OneField extends Component {
     get canCreateEdit() {
         return this.canCreate && !this.props.options.no_create_edit;
     }
+    get sources() {
+        return [this.recordSource];
+    }
+    get recordSource() {
+        return {
+            placeholder: this.env._t("Loading..."),
+            options: this.loadRecordSource.bind(this),
+        };
+    }
 
     getDomain() {
         return new Domain(this.props.attrs.domain).toList(
             this.props.record.getFieldContext(this.props.name)
         );
     }
+
     async loadDisplayName(value) {
         if (this.props.options.always_reload && value) {
             const nameGet = await this.orm.call(this.relation, "name_get", [value[0]], {
@@ -79,15 +90,75 @@ export class Many2OneField extends Component {
         }
         return [this.props.formatValue(value)];
     }
-    async fetchAutoCompleteSources(request) {
-        const trimmedRequest = request.trim();
-        const suggestions = [];
-        const context = Object.create(this);
-        for (const source of registry.category("m2oAutoCompleteSources").getAll()) {
-            await source(trimmedRequest, suggestions, context);
+
+    async loadRecordSource(request) {
+        const records = await this.orm.call(this.relation, "name_search", [], {
+            name: request,
+            args: this.getDomain(),
+            operator: "ilike",
+            limit: this.searchLimit + 1,
+            context: this.props.record.getFieldContext(this.props.name),
+        });
+
+        const options = records.map((result) => ({
+            value: result[0],
+            label: result[1],
+        }));
+
+        // Add "Search more..." option if records count is higher than the limit
+        if (this.searchLimit < records.length) {
+            options.push({
+                label: this.env._t("Search More..."),
+                classList: "o_m2o_dropdown_option",
+                action: () => {
+                    console.log("search more");
+                },
+            });
         }
-        return suggestions;
+
+        if (request.length) {
+            // "Quick create" option
+            if (this.canQuickCreate && !records.some((record) => record[1] === request)) {
+                options.push({
+                    label: sprintf(this.env._t(`Create "%s"`), owl.utils.escape(request)),
+                    classList: "o_m2o_dropdown_option",
+                    action: () => {
+                        console.log("create");
+                    },
+                });
+            }
+
+            // "Create and Edit" option
+            if (this.canCreateEdit) {
+                options.push({
+                    label: this.env._t(`Create and Edit...`),
+                    classList: "o_m2o_dropdown_option",
+                    action: () => {
+                        console.log("create and edit");
+                    },
+                });
+            }
+
+            // "No results" option
+            if (!options.length) {
+                options.push({
+                    label: this.env._t("No records"),
+                    classList: "o_m2o_no_result",
+                    unselectable: true,
+                });
+            }
+        } else if (!this.props.value && (this.canQuickCreate || this.canCreateEdit)) {
+            // "Start typing" option
+            options.push({
+                label: this.env._t("Start typing..."),
+                classList: "o_m2o_start_typing",
+                unselectable: true,
+            });
+        }
+
+        return options;
     }
+
     async openAction() {
         const action = await this.orm.call(
             this.props.record.fields[this.props.name].relation,
@@ -126,13 +197,23 @@ export class Many2OneField extends Component {
     onExternalBtnClick() {
         this.openDialog(this.props.value[0]);
     }
-    onChange(value) {
-        if (!value) {
+    onChange({ inputValue }) {
+        if (!inputValue.length) {
             this.props.update(false);
         }
     }
+    onInput({ inputValue }) {
+        this.state.isFloating = !this.props.value || this.props.value[1] !== inputValue;
+    }
+    onSelect(option) {
+        if (option.action) {
+            option.action();
+        } else {
+            this.props.update([option.value, option.label]);
+            this.state.isFloating = false;
+        }
+    }
 }
-
 Object.assign(Many2OneField, {
     template: "web.Many2OneField",
     props: {
@@ -150,77 +231,3 @@ Object.assign(Many2OneField, {
 });
 
 registry.category("fields").add("many2one", Many2OneField);
-
-// FIXME: sources shouldn't be managed outside of many2one
-
-async function searchRecords(request, suggestions, ctx) {
-    const results = await ctx.orm.call(ctx.relation, "name_search", [], {
-        name: request,
-        args: ctx.getDomain(),
-        operator: "ilike",
-        limit: ctx.searchLimit + 1,
-        context: ctx.props.record.getFieldContext(ctx.props.name),
-    });
-
-    suggestions.push(
-        ...results.map((result) => {
-            const displayName = ctx.props.formatValue(result);
-            return {
-                label: owl.utils.escape(displayName),
-                onSelected: ({ setValue }) => {
-                    setValue(displayName);
-                    ctx.props.update(result);
-                },
-            };
-        })
-    );
-
-    // Add "Search more..." option if results count is higher than the limit
-    if (ctx.searchLimit < results.length) {
-        suggestions.push({
-            label: ctx.env._t("Search More..."),
-            classList: "o_m2o_dropdown_option",
-        });
-    }
-}
-
-registry.category("m2oAutoCompleteSources").add("searchRecords", searchRecords);
-
-function additionalOptions(request, suggestions, ctx) {
-    if (request.length) {
-        // "Quick create" option
-        if (
-            ctx.canQuickCreate &&
-            !suggestions.some((s) => s.classList !== "o_m2o_dropdown_option" && s.label === request)
-        ) {
-            suggestions.push({
-                label: sprintf(ctx.env._t(`Create "%s"`), owl.utils.escape(request)),
-                classList: "o_m2o_dropdown_option",
-            });
-        }
-
-        // "Create and Edit" option
-        if (ctx.canCreateEdit) {
-            suggestions.push({
-                label: ctx.env._t(`Create and Edit...`),
-                classList: "o_m2o_dropdown_option",
-            });
-        }
-
-        // "No results" option
-        if (!suggestions.length) {
-            suggestions.push({
-                label: ctx.env._t("No records"),
-                classList: "o_m2o_no_result",
-            });
-        }
-    } else if (!ctx.props.value && (ctx.canQuickCreate || ctx.canCreateEdit)) {
-        // "Start typing" option
-        suggestions.push({
-            label: ctx.env._t("Start typing..."),
-            classList: "o_m2o_start_typing",
-        });
-    }
-}
-
-registry.category("m2oAutoCompleteSources").add("additionalOptions", additionalOptions);
