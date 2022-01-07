5eb84223dcec66ac87c8af28882c4a50c992a588
Mathieu Duckerts-Antoine <dam@odoo.com>
2022-01-07 15:07:21 +0100

    [FIX] web: graph: relabelling of fake data
    In the graph view in line mode, fake data are added when there is few
    data to center the graph (the points in facts). Now, there
    are two problems with the relabelling of the labels created along with the
    fake data:
        - the relabelling (when it works) gives "Total" instead of "" (nothing)
        - the relabelling crashes when the line chart is grouped by a date
          field, a comparison is done on that field and no data is received.
    
    We fix the problem by introducing a custom label similar to NO_DATA
    (a custom label used to manipulate fake data in the pie charts).
    
    opw-2724028

--- addons/web/static/tests/views/graph_tests.js
+++ addons/web/static/tests/views/graph_tests.js
@@ -423,6 +423,39 @@ QUnit.module('Views', {
         graph.destroy();
     });
 
+    QUnit.test('fake data in line chart', async function (assert) {
+        assert.expect(1);
+
+        const unpatchDate = patchDate(2020, 4, 19, 1, 0, 0);
+
+        this.data.foo.records = [];
+
+        var graph = await createView({
+            View: GraphView,
+            model: "foo",
+            data: this.data,
+            context: { search_default_date_filter: 1, },
+            arch: '<graph type="line">' +
+                        '<field name="date"/>' +
+                '</graph>',
+            archs: {
+                'foo,false,search': `
+                    <search>
+                        <filter name="date_filter" domain="[]" date="date" default_period="third_quarter"/>
+                    </search>
+                `,
+            },
+        });
+
+        await cpHelpers.toggleComparisonMenu(graph);
+        await cpHelpers.toggleMenuItem(graph, 'Date: Previous period');
+
+        assert.checkLabels(graph, [[''], ['']]);
+
+        unpatchDate();
+        graph.destroy();
+    });
+
     QUnit.test('no content helper after update', async function (assert) {
         assert.expect(6);
 
