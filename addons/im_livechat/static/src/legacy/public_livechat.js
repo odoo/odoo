4b18b43491b596c1bd8f47a68d649a4ed66c00b9
bit-odoo <bit@odoo.com>
2022-01-10 05:03:10 +0000

    [FIX] im_livechat: few changes for livechat
    Before this commit:
    
    - When the operator edit or delete a message, but the livechat visitor does not
      receive the updated content.
    
    - If the first message of a series of squashed messages is deleted, the avatar
      and the name of the author are lost on the visitor side.
    
    After this commit:
    
    - Livechat visitors will receive the updated content instantly and disable
      the reactions and replay buttons for the LiveChat channel.
    
    - Displayed message with the avatar and the name of the author on visitor side.
    
    Task-2678397
    
    X-original-commit: 992a68a529e628a6f08f3ec623957c3dadb93bdf

--- addons/im_livechat/static/src/legacy/public_livechat.js
+++ addons/im_livechat/static/src/legacy/public_livechat.js
@@ -221,6 +221,15 @@ var LivechatButton = Widget.extend({
                 this._renderMessages();
                 return;
             }
+            case 'mail.message/insert': {
+                const message = this._messages.find(message => message._id === payload.id);
+                if (!message) {
+                    return;
+                }
+                message._body = utils.Markup(payload.body);
+                this._renderMessages()
+                return;
+            }
         }
     },
     /**
@@ -701,7 +710,8 @@ var WebsiteLivechat = AbstractThread.extend(ThreadTypingMixin, {
      * @returns {im_livechat.legacy.im_livechat.model.WebsiteLivechatMessage[]}
      */
     getMessages: function () {
-        return this._messages;
+        // ignore removed messages
+        return this._messages.filter(message => !message.isEmpty());
     },
     /**
      * @returns {Array}
@@ -2997,7 +3007,7 @@ var ThreadWidget = Widget.extend({
         this._currentThreadID = thread.getID();
 
         // copy so that reverse do not alter order in the thread object
-        var messages = _.clone(thread.getMessages({ domain: options.domain || [] }));
+        var messages = _.clone(thread.getMessages());
 
         var modeOptions = options.isCreateMode ? this._disabledOptions :
                                                     this._enabledOptions;
