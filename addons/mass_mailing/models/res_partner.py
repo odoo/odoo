13cbf906980b7e1d6e440ce5c5d8fbe7b49c8cd0
dht-odoo <dht@odoo.com>
2022-01-10 18:41:03 +0530

    [IMP] mass_mailing: improve the flow of 'Send email' action of contacts
    Before this commit, using the 'Send email' action on contacts list view,
    user can send the mail to mass audience. However, this mail is a plaintext
    one. But with the 'Email Marketing' installed, we have option to let user
    design the beautiful mailing (or, user can simply opt the 'Plaintext'
    template while designing the mail).
    
    With this commit we introduce new server action for sending the mails
    from contacts list view, and redirect the user to the simplified
    mass mailing form view where the model and domain are already set based
    on the selected records. This will be only for thos users who have
    mass_mailing rights.
    
    TaskId-2672198

--- addons/mass_mailing/models/res_partner.py
+++ addons/mass_mailing/models/res_partner.py
@@ -7,3 +7,19 @@ from odoo import models
 class Partner(models.Model):
     _inherit = 'res.partner'
     _mailing_enabled = True
+
+    def action_send_mail_to_partner(self):
+        ''' Send emails from mailing.mailing to selected partners if user 
+        has mass_mailing rights '''
+        action = super(Partner, self).action_send_mail_to_partner()
+        if self.env.user.has_group('mass_mailing.group_mass_mailing_user'):
+            action['context'] = {
+                'default_mailing_domain': [('id', 'in', self.env.context['active_ids'])],
+                'default_mailing_model_id': self.env['ir.model']._get(self._name).id,
+                'default_reply_to': '',
+                'default_reply_to_mode': 'new',
+            }
+            action['res_model'] = "mailing.mailing"
+            action['target'] = 'current'
+            action['view_id'] = [self.env.ref('mass_mailing.mailing_mailing_view_form_nomodel').id, 'form']
+        return action
