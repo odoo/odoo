15edf996d4efee56a78a6cbd485b3be719f8a6ae
Nicolas (vin) <vin@odoo.com>
2022-01-11 08:41:44 +0100

    [IMP] l10n_mx: set tags on suspense and outstanding accounts
    Set a default tag on the bank suspense account, outstanding accounts
    and transfer accounts so that they are found and used by the trial
    balance and COA reports.
    
    TAsk id #2612900

--- addons/l10n_mx/models/chart_template.py
+++ addons/l10n_mx/models/chart_template.py
@@ -38,3 +38,29 @@ class AccountChartTemplate(models.Model):
             'show_on_dashboard': True,
         })
         return res
+
+    @api.model
+    def _create_liquidity_journal_suspense_account(self, company, code_digits):
+        if not self == self.env.ref('l10n_mx.mx_coa'):
+            return super()._create_liquidity_journal_suspense_account(company, code_digits)
+        return self.env['account.account'].create({
+            'name': _("Bank Suspense Account"),
+            'code': self.env['account.account']._search_new_account_code(company, code_digits, company.bank_account_code_prefix or ''),
+            'user_type_id': self.env.ref('account.data_account_type_current_assets').id,
+            'tag_ids': [(6, 0, self.env.ref('l10n_mx.account_tag_102_01').ids)],
+            'company_id': company.id,
+        })
+
+    @api.model
+    def _prepare_transfer_account_template(self, prefix=None):
+        res = super()._prepare_transfer_account_template(prefix)
+        if self == self.env.ref('l10n_mx.mx_coa'):
+            res['tag_ids'] = [(6, 0, self.env.ref('l10n_mx.account_tag_102_01').ids)]
+        return res
+
+    def _load(self, sale_tax_rate, purchase_tax_rate, company):
+        res = super()._load(sale_tax_rate, purchase_tax_rate, company)
+        if self == self.env.ref('l10n_mx.mx_coa'):
+            company.account_journal_payment_debit_account_id.tag_ids = [(6, 0, self.env.ref('l10n_mx.account_tag_102_01').ids)]
+            company.account_journal_payment_credit_account_id.tag_ids = [(6, 0, self.env.ref('l10n_mx.account_tag_102_01').ids)]
+        return res
