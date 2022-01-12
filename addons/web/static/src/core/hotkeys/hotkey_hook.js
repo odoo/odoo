786f005e59c07b0a1f81518535bd793dbeef61d3
Bruno Boi <boi@odoo.com>
2022-01-12 18:35:06 +0100

    [IMP][REF] web: capture feature for hotkey service

--- addons/web/static/src/core/hotkeys/hotkey_hook.js
+++ addons/web/static/src/core/hotkeys/hotkey_hook.js
@@ -11,6 +11,11 @@ import { useEffect, useService } from "@web/core/utils/hooks";
  * @param {Object} options additional options
  * @param {boolean} [options.allowRepeat=false]
  *  allow registration to perform multiple times when hotkey is held down
+ * @param {boolean} [options.capture=false]
+ *  registrations in capture mode would get called beforehand and would
+ *  have the opportunity to stop the dispatching of an hotkey by
+ *  returning an object `{captured: true}` in order to cancel current dispatching.
+ *  Note that the capturer may be asynchronous and could delay the dispatching.
  * @param {boolean} [options.global=false]
  *  allow registration to perform no matter the UI active element
  */
