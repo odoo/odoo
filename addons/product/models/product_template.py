36b28bc0223b17408eafd11e7abe04e71968f431
Bruno-brsy <brsy@odoo.com>
2022-01-12 16:14:10 +0000

    [FIX] product: help for field standard_price
    Step to reproduce:
    1) create a product with, the "Product Type" as
    "Storable Product" + A "Product Category" with a "Costing Method"
    set in "FIFO" mode and the Inventory Valuation in "Automated".
    
    2) create a Purchase Order for 1 unit of that product and set the unit
    price to 10.00. Confirm Order -> Receive Products -> Validate
    (the delivery) -> Apply
    
    3) create a second Purchase Order for 1 unit of the same product but
    set the unit price to a different price, like 20.00. Confirm Order ->
    Receive Products -> Validate (the delivery) -> Apply
    
    4) create a Sales Orders for 1 unit -> Confirm -> Smart button
    Delivery -> Validate -> Apply
    
    5) check the cost on the product form of that product.
    
    On the V13 it will be $10, so its the last unit that left the stock
    On V14 and upper, it will be $20, so the next product that will leave
    the stock.
    
    The help popup for the field "standard_price" doesn't reflect the
    correct behavior. It's the standard price for the next unit instead
    of the last one.
    
    closes odoo/odoo#82678
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -79,7 +79,7 @@ class ProductTemplate(models.Model):
         inverse='_set_standard_price', search='_search_standard_price',
         digits='Product Price', groups="base.group_user",
         help="""In Standard Price & AVCO: value of the product (automatically computed in AVCO).
-        In FIFO: value of the last unit that left the stock (automatically computed).
+        In FIFO: value of the next unit that will leave the stock (automatically computed).
         Used to value the product when the purchase cost is not known (e.g. inventory adjustment).
         Used to compute margins on sale orders.""")
 
