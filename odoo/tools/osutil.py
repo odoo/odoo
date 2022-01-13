b6d485418df87688022ab8b57cfa34d761583f2e
Xavier Morel <xmo@odoo.com>
2022-01-13 10:56:16 +0100

    fixup! [REM] core: outdated osutil stuff

--- odoo/tools/osutil.py
+++ odoo/tools/osutil.py
@@ -91,7 +91,7 @@ else:
         try:
             with close_srv(ws.OpenSCManager(None, None, ws.SC_MANAGER_ALL_ACCESS)) as hscm,\
                  close_srv(wsu.SmartOpenService(hscm, nt_service_name, ws.SERVICE_ALL_ACCESS)) as hs:
-                     info = ws.QueryServiceStatusEx(hs)
-                     return info['ProcessId'] == os.getppid()
+                    info = ws.QueryServiceStatusEx(hs)
+                    return info['ProcessId'] == os.getppid()
         except Exception:
             return False
