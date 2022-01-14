e61e2db1bb362943c0f80f477962ed53817db373
Rémy Baranx (bar) <bar@odoo.com>
2022-01-14 08:45:46 +0000

    [FIX] calendar,microsoft_calendar: backport bugfixes from master
    As several customers complained about lot of bugs in microsoft_calendar in 14.0,
    It has been decided to backport bug fixes of the model layer from master to 14.0,
    without the need of an upgrade script (no new field, ...).
    
    In master, we use 2 ids (organizer event id + universal id) instead of only one,
    to handle Odoo <-> Outlook sync correctly when several attendees sync their Outlook
    calendar with their Odoo calendar. For that, we have added a new field.
    To report this bug fix in 14.0, the existing field which stores the organizer event id,
    is now a string storing both ids separated by a ':' as follow: 'organizer_event_id:universal_id'.
    
    2 new compute fields have been added to be able to use these 2 ids more easily.
    
    Part-of: odoo/odoo#82863

--- addons/microsoft_account/models/microsoft_service.py
+++ addons/microsoft_account/models/microsoft_service.py
@@ -17,6 +17,7 @@ TIMEOUT = 20
 MICROSOFT_AUTH_ENDPOINT = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
 MICROSOFT_TOKEN_ENDPOINT = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
 
+RESOURCE_NOT_FOUND_STATUSES = (204, 404)
 
 class MicrosoftService(models.AbstractModel):
     _name = 'microsoft.service'
@@ -141,17 +142,18 @@ class MicrosoftService(models.AbstractModel):
             res.raise_for_status()
             status = res.status_code
 
-            if int(status) in (204, 404):  # Page not found, no response
+            if int(status) in RESOURCE_NOT_FOUND_STATUSES:
                 response = False
             else:
-                response = res.json()
+                # Some answers return empty content
+                response = res.content and res.json() or {}
 
             try:
                 ask_time = datetime.strptime(res.headers.get('date'), "%a, %d %b %Y %H:%M:%S %Z")
             except:
                 pass
         except requests.HTTPError as error:
-            if error.response.status_code in (204, 404):
+            if error.response.status_code in RESOURCE_NOT_FOUND_STATUSES:
                 status = error.response.status_code
                 response = ""
             else:
