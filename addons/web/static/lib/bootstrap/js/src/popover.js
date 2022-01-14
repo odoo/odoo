c48f57ea2538ad51e00ac27d58f8e191781444f3
Romeo Fragomeli <rfr@odoo.com>
2022-01-14 11:03:56 +0000

    [IMP] web,*: upgrade to Bootstrap 5.1.3
    * = base,http_routing,hw_drivers
    
    - Update Bootstrap from 4.3.1 to 5.1.3
    
    - Update PopperJS to version 2 for Bootstrap 5 (JS part)
      Some code was for PopperJS V1, but it's not compatible anymore.
    
    - Remove some BS5 classes utilities backport
    
    - Fix path for BS5
    
    Task ID: 2766483
    
    Part-of: odoo/odoo#95450

---
+++ addons/web/static/lib/bootstrap/js/src/popover.js
@@ -0,0 +1,128 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap (v5.1.3): popover.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import { defineJQueryPlugin } from './util/index'
+import Tooltip from './tooltip'
+
+/**
+ * ------------------------------------------------------------------------
+ * Constants
+ * ------------------------------------------------------------------------
+ */
+
+const NAME = 'popover'
+const DATA_KEY = 'bs.popover'
+const EVENT_KEY = `.${DATA_KEY}`
+const CLASS_PREFIX = 'bs-popover'
+
+const Default = {
+  ...Tooltip.Default,
+  placement: 'right',
+  offset: [0, 8],
+  trigger: 'click',
+  content: '',
+  template: '<div class="popover" role="tooltip">' +
+              '<div class="popover-arrow"></div>' +
+              '<h3 class="popover-header"></h3>' +
+              '<div class="popover-body"></div>' +
+            '</div>'
+}
+
+const DefaultType = {
+  ...Tooltip.DefaultType,
+  content: '(string|element|function)'
+}
+
+const Event = {
+  HIDE: `hide${EVENT_KEY}`,
+  HIDDEN: `hidden${EVENT_KEY}`,
+  SHOW: `show${EVENT_KEY}`,
+  SHOWN: `shown${EVENT_KEY}`,
+  INSERTED: `inserted${EVENT_KEY}`,
+  CLICK: `click${EVENT_KEY}`,
+  FOCUSIN: `focusin${EVENT_KEY}`,
+  FOCUSOUT: `focusout${EVENT_KEY}`,
+  MOUSEENTER: `mouseenter${EVENT_KEY}`,
+  MOUSELEAVE: `mouseleave${EVENT_KEY}`
+}
+
+const SELECTOR_TITLE = '.popover-header'
+const SELECTOR_CONTENT = '.popover-body'
+
+/**
+ * ------------------------------------------------------------------------
+ * Class Definition
+ * ------------------------------------------------------------------------
+ */
+
+class Popover extends Tooltip {
+  // Getters
+
+  static get Default() {
+    return Default
+  }
+
+  static get NAME() {
+    return NAME
+  }
+
+  static get Event() {
+    return Event
+  }
+
+  static get DefaultType() {
+    return DefaultType
+  }
+
+  // Overrides
+
+  isWithContent() {
+    return this.getTitle() || this._getContent()
+  }
+
+  setContent(tip) {
+    this._sanitizeAndSetContent(tip, this.getTitle(), SELECTOR_TITLE)
+    this._sanitizeAndSetContent(tip, this._getContent(), SELECTOR_CONTENT)
+  }
+
+  // Private
+
+  _getContent() {
+    return this._resolvePossibleFunction(this._config.content)
+  }
+
+  _getBasicClassPrefix() {
+    return CLASS_PREFIX
+  }
+
+  // Static
+
+  static jQueryInterface(config) {
+    return this.each(function () {
+      const data = Popover.getOrCreateInstance(this, config)
+
+      if (typeof config === 'string') {
+        if (typeof data[config] === 'undefined') {
+          throw new TypeError(`No method named "${config}"`)
+        }
+
+        data[config]()
+      }
+    })
+  }
+}
+
+/**
+ * ------------------------------------------------------------------------
+ * jQuery
+ * ------------------------------------------------------------------------
+ * add .Popover to jQuery only if jQuery is present
+ */
+
+defineJQueryPlugin(Popover)
+
+export default Popover
