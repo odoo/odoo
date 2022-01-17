6d6e6c13c5789d46ec0cf7d5866c1f633c579f72
Stanislas Gueniffey <stgu@odoo.com>
2022-01-17 10:53:12 +0100

    [ADD] l10n_es_tbai: enable refunds

--- addons/l10n_es_edi_tbai/models/__init__.py
+++ addons/l10n_es_edi_tbai/models/__init__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import account_move
 from . import account_edi_format
+from . import account_invoice_refund
+from . import account_move
+from . import account_tax
+from . import ir_attachment
 from . import l10n_es_edi_tbai_certificate
 from . import res_company
 from . import res_config_settings
-from . import account_tax
-from . import ir_attachment
 from . import utils
