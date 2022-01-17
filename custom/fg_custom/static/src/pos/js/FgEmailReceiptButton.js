d0cdcd817c8242a6bb26f63294479603eb46bd58
cracaza <60120359+cracaza@users.noreply.github.com>
2022-01-17 15:55:51 +0800

    send email receipt (order with customer but no email set)

--- custom/fg_custom/static/src/pos/js/FgEmailReceiptButton.js
+++ custom/fg_custom/static/src/pos/js/FgEmailReceiptButton.js
@@ -16,7 +16,7 @@ odoo.define('fg_custom.FgEmailReceiptButton', function (require) {
         }
         async emailReceipt() {
             var email = this.props.inputEmail || (this.props.order.attributes.client && this.props.order.attributes.client.email) || '';
-            console.log(this.props);
+            
             if(email){
                try {
                     await this._sendReceiptToCustomer(email);
