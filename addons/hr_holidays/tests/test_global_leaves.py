248920f0ab1ea721e99d1a322aa136815df093ca
Dossogne Bertrand <bedo@odoo.com>
2022-01-18 11:01:20 +0000

    [IMP] hr_leave: improve leave ux
    Small changes in the layout of the hr_leave form view,
    mainly for the manager's view point
    
    Task : #2734842 [Time Off] ux changes
    
    closes odoo/odoo#82976
    
    Related: odoo/enterprise#23896
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

---
+++ addons/hr_holidays/tests/test_global_leaves.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from datetime import date
+from odoo.addons.hr_holidays.tests.common import TestHrHolidaysCommon
+from odoo.exceptions import ValidationError
+
+from odoo.tests import tagged
+
+@tagged('global_leaves')
+class TestGlobalLeaves(TestHrHolidaysCommon):
+    """ Test global leaves for a whole company, conflict resolutions """
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.calendar_1 = cls.env['resource.calendar'].create({
+            'name': 'Classic 40h/week',
+            'tz': 'UTC',
+            'hours_per_day': 8.0,
+            'attendance_ids': [
+                (0, 0, {'name': 'Monday Morning', 'dayofweek': '0', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),
+                (0, 0, {'name': 'Monday Afternoon', 'dayofweek': '0', 'hour_from': 13, 'hour_to': 17, 'day_period': 'afternoon'}),
+                (0, 0, {'name': 'Tuesday Morning', 'dayofweek': '1', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),
+                (0, 0, {'name': 'Tuesday Afternoon', 'dayofweek': '1', 'hour_from': 13, 'hour_to': 17, 'day_period': 'afternoon'}),
+                (0, 0, {'name': 'Wednesday Morning', 'dayofweek': '2', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),
+                (0, 0, {'name': 'Wednesday Afternoon', 'dayofweek': '2', 'hour_from': 13, 'hour_to': 17, 'day_period': 'afternoon'}),
+                (0, 0, {'name': 'Thursday Morning', 'dayofweek': '3', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),
+                (0, 0, {'name': 'Thursday Afternoon', 'dayofweek': '3', 'hour_from': 13, 'hour_to': 17, 'day_period': 'afternoon'}),
+                (0, 0, {'name': 'Friday Morning', 'dayofweek': '4', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),
+                (0, 0, {'name': 'Friday Afternoon', 'dayofweek': '4', 'hour_from': 13, 'hour_to': 17, 'day_period': 'afternoon'})
+            ]
+        })
+
+        cls.calendar_2 = cls.env['resource.calendar'].create({
+            'name': 'Classic 20h/week',
+            'tz': 'UTC',
+            'hours_per_day': 4.0,
+            'attendance_ids': [
+                (0, 0, {'name': 'Monday Morning', 'dayofweek': '0', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),
+                (0, 0, {'name': 'Tuesday Morning', 'dayofweek': '1', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),
+                (0, 0, {'name': 'Wednesday Morning', 'dayofweek': '2', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),
+                (0, 0, {'name': 'Thursday Morning', 'dayofweek': '3', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),
+                (0, 0, {'name': 'Friday Morning', 'dayofweek': '4', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),
+            ]
+        })
+
+        cls.global_leave = cls.env['resource.calendar.leaves'].create({
+            'name': 'Global Leave',
+            'date_from': date(2022, 3, 7),
+            'date_to': date(2022, 3, 7),
+        })
+
+        cls.calendar_leave = cls.env['resource.calendar.leaves'].create({
+            'name': 'Global Leave',
+            'date_from': date(2022, 3, 8),
+            'date_to': date(2022, 3, 8),
+            'calendar_id': cls.calendar_1.id,
+        })
+
+    def test_leave_on_global_leave(self):
+        with self.assertRaises(ValidationError):
+            self.env['resource.calendar.leaves'].create({
+                'name': 'Wrong Leave',
+                'date_from': date(2022, 3, 7),
+                'date_to': date(2022, 3, 7),
+                'calendar_id': self.calendar_1.id,
+            })
+
+        with self.assertRaises(ValidationError):
+            self.env['resource.calendar.leaves'].create({
+                'name': 'Wrong Leave',
+                'date_from': date(2022, 3, 7),
+                'date_to': date(2022, 3, 7),
+            })
+
+    def test_leave_on_calendar_leave(self):
+        self.env['resource.calendar.leaves'].create({
+                'name': 'Correct Leave',
+                'date_from': date(2022, 3, 8),
+                'date_to': date(2022, 3, 8),
+                'calendar_id': self.calendar_2.id,
+            })
+
+        with self.assertRaises(ValidationError):
+            self.env['resource.calendar.leaves'].create({
+                'name': 'Wrong Leave',
+                'date_from': date(2022, 3, 8),
+                'date_to': date(2022, 3, 8),
+            })
+
+        with self.assertRaises(ValidationError):
+            self.env['resource.calendar.leaves'].create({
+                'name': 'Wrong Leave',
+                'date_from': date(2022, 3, 8),
+                'date_to': date(2022, 3, 8),
+                'calendar_id': self.calendar_1.id,
+            })
