770490ce971ed6d182fab4f93fda1f48f9da569e
Brieuc-brd <brd@odoo.com>
2022-01-19 16:14:43 +0000

    [IMP] web: revamp graph view
    This commit simplifies the SCSS of the Graph view.
    
    task-2740030
    
    closes odoo/odoo#83132
    
    Related: odoo/enterprise#23633
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>

--- addons/web/static/src/views/graph/graph_renderer.js
+++ addons/web/static/src/views/graph/graph_renderer.js
@@ -84,7 +84,8 @@ export class GraphRenderer extends Component {
             }
             const tr = document.createElement("tr");
             const td = document.createElement("td");
-            tr.classList.add("o_show_more");
+            tr.classList.add("o_show_more", "text-center", "font-weight-bold");
+            td.setAttribute("colspan", "2");
             td.innerText = this.env._t("...");
             tr.appendChild(td);
             tooltip.querySelector("tbody").appendChild(tr);
@@ -550,7 +551,7 @@ export class GraphRenderer extends Component {
         }
         const viewContentTop = this.rootRef.el.getBoundingClientRect().top;
         const legendTooltip = Object.assign(document.createElement("div"), {
-            className: "o_tooltip_legend",
+            className: "o_tooltip_legend popover p-3 o_debounce_disabled",
             innerText: fullText,
         });
         legendTooltip.style.top = `${ev.clientY - viewContentTop}px`;
