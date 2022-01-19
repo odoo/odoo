8ac4c400ad6800f2ae9113989b34a217b75919ef
Xavier Morel <xmo@odoo.com>
2022-01-19 09:48:26 +0000

    [FIX] mail, mass_mailing: fix attachment ownership (cont)
    Followup to #82105: turns out we kind-of forgot that records could be
    updated with new attachment and the exact same issue could occur.
    
    So with the same reasoning as the previous PR, re-attach attachments
    to the current object when updating it.
    
    closes odoo/odoo#83079
    
    X-original-commit: 398070ec1b6ed7d6a8e7c0ab75d45b8a9ad0e0d0
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- addons/mass_mailing/models/mailing.py
+++ addons/mass_mailing/models/mailing.py
@@ -418,10 +418,7 @@ class MassMailing(models.Model):
         ]
         self.env['utm.campaign'].create(campaign_vals)
 
-        # fix attachment ownership
-        for mailing in mailings:
-            if mailing.attachment_ids:
-                mailing.attachment_ids.write({'res_model': self._name, 'res_id': mailing.id})
+        mailings._fix_attachment_ownership()
 
         return mailings
 
@@ -437,6 +434,7 @@ class MassMailing(models.Model):
             raise ValidationError(_("A campaign should be set when A/B test is enabled"))
 
         result = super(MassMailing, self).write(values)
+        self._fix_attachment_ownership()
 
         if any(self.mapped('ab_testing_schedule_datetime')):
             schedule_date = min(m.ab_testing_schedule_datetime for m in self if m.ab_testing_schedule_datetime)
@@ -444,6 +442,11 @@ class MassMailing(models.Model):
             ab_testing_cron._trigger(at=schedule_date)
 
         return result
+
+    def _fix_attachment_ownership(self):
+        for record in self:
+            record.attachment_ids.write({'res_model': record._name, 'res_id': record.id})
+        return self
 
     @api.returns('self', lambda value: value.id)
     def copy(self, default=None):
