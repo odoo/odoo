7c4e591d58dd2b882f0c237d52490b2756c94203
Laurent Smet <las@odoo.com>
2022-01-20 16:38:03 +0000

    [REF] base,account: Refactor PDF generation to allow CH QR-bills
    For the Switzerland localization, we need to append some extra pages during the PDF generation such as QR barcodes or ISR payment slip.
    There were multiple difficulties with the current codes:
    - The ir.attachment was generated before the hook allowing to append the extra pages.
    - There is no existing hook allowing to get the pages for each invoice when printing on multiple records at once.
    
    This commit aims to dispatch the generation of PDFs from the creating of extra attachments:
    - `_render_qweb_pdf_prepare_streams` is called first and creates a stream for each record containing the PDF pages.
    - Then, the ir.attachment are generated if necessary.
    - Finally, all streams are aggregated together to get the final PDF.
    
    Task: 2726507
    Part-of: odoo/odoo#85150

--- addons/l10n_be_edi/models/ir_actions_report.py
+++ addons/l10n_be_edi/models/ir_actions_report.py
@@ -11,43 +11,56 @@ from xml.sax.saxutils import escape, quoteattr
 class IrActionsReport(models.Model):
     _inherit = 'ir.actions.report'
 
-    def _postprocess_pdf_report(self, record, buffer):
-        '''Add the pdf report in the e-fff XML as base64 string.
-        '''
-        result = super()._postprocess_pdf_report(record, buffer)
+    def _l10n_be_edi_add_pdf_into_invoice_xml(self, invoice, stream_data):
+        edi_attachment = invoice.edi_document_ids.filtered(lambda d: d.edi_format_id.code == 'efff_1').attachment_id
+        if not edi_attachment:
+            return
 
-        if record._name == 'account.move':
-            edi_attachment = record.edi_document_ids.filtered(lambda d: d.edi_format_id.code == 'efff_1').attachment_id
-            if edi_attachment:
-                old_xml = base64.b64decode(edi_attachment.with_context(bin_size=False).datas, validate=True)
-                tree = etree.fromstring(old_xml)
-                anchor_elements = tree.xpath("//*[local-name()='AccountingSupplierParty']")
-                additional_document_elements = tree.xpath("//*[local-name()='AdditionalDocumentReference']")
-                if anchor_elements and not additional_document_elements:
-                    pdf = base64.b64encode(buffer.getvalue()).decode()
-                    pdf_name = '%s.pdf' % record._get_efff_name()
-                    to_inject = '''
-                        <cac:AdditionalDocumentReference
-                            xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2"
-                            xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
-                            xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2">
-                            <cbc:ID>%s</cbc:ID>
-                            <cac:Attachment>
-                                <cbc:EmbeddedDocumentBinaryObject mimeCode="application/pdf" filename=%s>
-                                    %s
-                                </cbc:EmbeddedDocumentBinaryObject>
-                            </cac:Attachment>
-                        </cac:AdditionalDocumentReference>
-                    ''' % (escape(pdf_name), quoteattr(pdf_name), pdf)
+        tree = etree.fromstring(edi_attachment.raw)
+        anchor_elements = tree.xpath("//*[local-name()='AccountingSupplierParty']")
+        additional_document_elements = tree.xpath("//*[local-name()='AdditionalDocumentReference']")
+        if anchor_elements and not additional_document_elements:
+            pdf_stream = stream_data['stream']
 
-                    anchor_index = tree.index(anchor_elements[0])
-                    tree.insert(anchor_index, etree.fromstring(to_inject))
-                    new_xml = etree.tostring(tree, pretty_print=True)
-                    edi_attachment.write({
-                        'res_model': 'account.move',
-                        'res_id': record.id,
-                        'datas': base64.b64encode(new_xml),
-                        'mimetype': 'application/xml',
-                    })
+            # Read pdf content.
+            pdf_content_b64 = base64.b64encode(pdf_stream.getvalue()).decode()
+            pdf_name = f'{invoice._get_efff_name()}.pdf'
 
-        return result
+            # Inject the newly generated PDF.
+            to_inject = '''
+                <cac:AdditionalDocumentReference
+                    xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2"
+                    xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
+                    xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2">
+                    <cbc:ID>%s</cbc:ID>
+                    <cac:Attachment>
+                        <cbc:EmbeddedDocumentBinaryObject mimeCode="application/pdf" filename=%s>
+                            %s
+                        </cbc:EmbeddedDocumentBinaryObject>
+                    </cac:Attachment>
+                </cac:AdditionalDocumentReference>
+            ''' % (escape(pdf_name), quoteattr(pdf_name), pdf_content_b64)
+
+            anchor_index = tree.index(anchor_elements[0])
+            tree.insert(anchor_index, etree.fromstring(to_inject))
+            new_xml = etree.tostring(tree, pretty_print=True)
+            edi_attachment.write({
+                'res_model': 'account.move',
+                'res_id': invoice.id,
+                'raw': new_xml,
+                'mimetype': 'application/xml',
+            })
+
+    def _render_qweb_pdf_prepare_streams(self, data, res_ids=None):
+        # EXTENDS base
+        # Add the pdf report in the e-fff XML as base64 string.
+        collected_streams = super()._render_qweb_pdf_prepare_streams(data, res_ids=res_ids)
+
+        if collected_streams \
+                and res_ids \
+                and self.report_name in ('account.report_invoice_with_payments', 'account.report_invoice'):
+            for res_id, stream_data in collected_streams.items():
+                invoice = self.env['account.move'].browse(res_id)
+                self._l10n_be_edi_add_pdf_into_invoice_xml(invoice, stream_data)
+
+        return collected_streams
