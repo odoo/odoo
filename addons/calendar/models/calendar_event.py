98d8bb06dd07f1a842252d4afc67d075e403fe92
Jérémy Hennecart (jeh) <jeh@odoo.com>
2022-01-21 10:29:36 +0100

    [FIX] calendar: use the event_tz in mail templates
    Try to use the event_tz before falling back on the mail_tz (user tz) in the
    context. Add a tool method allowing to override this behavior easily instead
    of concatenating "or". As template is modified anyway translations are already
    broken.
    
    Task-2699765

--- addons/calendar/models/calendar_event.py
+++ addons/calendar/models/calendar_event.py
@@ -715,6 +715,10 @@ class Meeting(models.Model):
         self.ensure_one()
         return ",".join([e for e in self.attendee_ids.mapped("email") if e])
 
+    def _get_mail_tz(self):
+        self.ensure_one()
+        return self.event_tz or self.env.user.tz
+
     def _sync_activities(self, fields):
         # update activities
         for event in self:
