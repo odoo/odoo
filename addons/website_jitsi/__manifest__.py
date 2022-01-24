04ff385290b318edc85a6c372b6d3b0c387a97fd
Yannick Tivisse <yti@odoo.com>
2022-01-24 11:28:26 +0000

    [IMP] website_jitsi: Move domain configuration from website to event
    Part-of: odoo/odoo#82999

--- addons/website_jitsi/__manifest__.py
+++ addons/website_jitsi/__manifest__.py
@@ -15,7 +15,6 @@
     "data": [
         'views/chat_room_templates.xml',
         'views/chat_room_views.xml',
-        'views/res_config_settings.xml',
         'security/ir.model.access.csv',
     ],
     'application': False,
