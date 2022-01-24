74eb19b2498373bf84cb325aaeb240e390092b50
Yannick Tivisse <yti@odoo.com>
2022-01-24 16:09:51 +0100

    [FIX] hr: Avoid erasing departure date when archiving an employee

--- addons/hr/wizard/hr_departure_wizard.py
+++ addons/hr/wizard/hr_departure_wizard.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class HrDepartureWizard(models.TransientModel):
     _name = 'hr.departure.wizard'
     _description = 'Departure Wizard'
 
+    def _get_default_departure_date(self):
+        departure_date = False
+        if self.env.context.get('active_id'):
+            departure_date = self.env['hr.employee'].browse(self.env.context['active_id']).departure_date
+        return departure_date or fields.Date.today()
+
     departure_reason_id = fields.Many2one("hr.departure.reason", default=lambda self: self.env['hr.departure.reason'].search([], limit=1), required=True)
     departure_description = fields.Html(string="Additional Information")
-    departure_date = fields.Date(string="Departure Date", required=True, default=fields.Date.today)
+    departure_date = fields.Date(string="Departure Date", required=True, default=_get_default_departure_date)
     employee_id = fields.Many2one(
         'hr.employee', string='Employee', required=True,
         default=lambda self: self.env.context.get('active_id', None),
