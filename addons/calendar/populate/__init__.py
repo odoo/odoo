de1cba13e3647490ab5d0b3cf8a4aaf9e1229cc0
Florian Charlier <flch@odoo.com>
2022-01-24 14:04:43 +0000

    [IMP] calendar: populate calendar alarms
    Task-2728093
    See odoo/enterprise#23707
    
    closes odoo/odoo#83265
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/calendar/populate/__init__.py
+++ addons/calendar/populate/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import controllers
-from . import models
-from . import populate
+from . import calendar_alarm
+from . import data
