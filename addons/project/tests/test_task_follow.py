27aaa14b31cdc916f57950aedb52534bbb40c310
Shahnawaz Idariya <sid@odoo.com>
2022-01-24 18:27:18 +0530

    [IMP] project: add test for project task follow
    This commit will add new test for project task follow propogation
     - following a project should make the user follow all of its tasks
     - the notification preferences of the user set on the project should be
       propagated to the tasks
    
    task-2723662

--- addons/project/tests/test_task_follow.py
+++ addons/project/tests/test_task_follow.py
@@ -13,3 +13,21 @@ class TestTaskFollow(TestProjectCommon):
         # Tests that the user is follower of the task upon writing new assignees
         self.task_2.user_ids += self.user_projectmanager
         self.assertTrue(self.user_projectmanager.partner_id in self.task_2.message_partner_ids)
+
+    def test_project_task_follow_propogation(self):
+        # Tests that following the project should make user follow its tasks
+        self.project_pigs.message_subscribe(partner_ids=[self.user_projectmanager.partner_id.id])
+        self.assertTrue(self.user_projectmanager.partner_id in self.project_pigs.message_partner_ids)
+        task_1 = self.env['project.task'].with_context(default_project_id=self.project_pigs.id).create({
+            'name': 'Test Task'
+        })
+        self.assertTrue(self.user_projectmanager.partner_id in task_1.message_partner_ids)
+
+    def test_project_task_notification_preferences_propogation(self):
+        # Tests that notification preferences set of the user on project should be propogated to the tasks
+        subtype = self.env.ref('project.mt_task_ready')
+        self.project_pigs.message_subscribe(partner_ids=[self.user_projectmanager.partner_id.id], subtype_ids=[subtype.id])
+        task_2 = self.env['project.task'].with_context(default_project_id=self.project_pigs.id).create({
+            'name': 'Test task 2'
+        })
+        self.assertEqual(task_2.message_follower_ids.subtype_ids, self.project_pigs.message_follower_ids.subtype_ids)
