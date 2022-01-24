4d3548c8ab3959f3ce027c2ab960977b8b56bb17
Yannick Tivisse <yti@odoo.com>
2022-01-24 11:42:03 +0000

    [IMP] website_event_track: Hide advanced option in debug mode
    Part-of: odoo/odoo#82999

--- addons/website_event_track/models/website.py
+++ addons/website_event_track/models/website.py
@@ -2,7 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 
-from PIL import Image
 import base64
 
 from odoo import api, fields, models
@@ -14,8 +13,18 @@ from odoo.tools.translate import _
 class Website(models.Model):
     _inherit = "website"
 
-    app_icon = fields.Image(string='Website App Icon', compute='_compute_app_icon', store=True, readonly=True, help='This field holds the image used as mobile app icon on the website (PNG format).')
-    events_app_name = fields.Char(string='Events App Name', compute='_compute_events_app_name', store=True, readonly=False, help="This fields holds the Event's Progressive Web App name.")
+    app_icon = fields.Image(
+        string='Website App Icon',
+        compute='_compute_app_icon',
+        store=True,
+        readonly=True,
+        help='This field holds the image used as mobile app icon on the website (PNG format).')
+    events_app_name = fields.Char(
+        string='Events App Name',
+        compute='_compute_events_app_name',
+        store=True,
+        readonly=False,
+        help="This fields holds the Event's Progressive Web App name.")
 
     @api.depends('name')
     def _compute_events_app_name(self):
