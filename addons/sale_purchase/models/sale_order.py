b5ae57f68b415e320fad9fee02924a6a941eba72
Florent de Labarre <florent.mirieu@gmail.com>
2022-01-24 11:45:08 +0100

    [FIX] sale_purchase: raise if currency are not the same
    - Create un SO in EUR with a product with service_to_purchase, with supplier A
    - Supplier A work in USD
    --> Confirm order
    Issue fields.datetime.now() doesn't exist.
    
    X-original-commit: 39b9bc63606295a516cd1221d57ce831a11cb9a0

--- addons/sale_purchase/models/sale_order.py
+++ addons/sale_purchase/models/sale_order.py
@@ -243,7 +243,7 @@ class SaleOrderLine(models.Model):
             price_unit = self.env['account.tax'].sudo()._fix_tax_included_price_company(
                 supplierinfo.price, supplier_taxes, taxes, self.company_id)
             if purchase_order.currency_id and supplierinfo.currency_id != purchase_order.currency_id:
-                price_unit = supplierinfo.currency_id._convert(price_unit, purchase_order.currency_id, purchase_order.company_id, fields.datetime.today())
+                price_unit = supplierinfo.currency_id._convert(price_unit, purchase_order.currency_id, purchase_order.company_id, fields.Date.context_today(self))
             product_ctx.update({'seller_id': supplierinfo.id})
         else:
             product_ctx.update({'partner_id': purchase_order.partner_id.id})
