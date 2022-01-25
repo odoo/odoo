b467c2c6536d291ac9b4d426f964fb1d9e309e3f
Julien Van Roy <juvr@odoo.com>
2022-01-25 11:00:20 +0000

    [ADD] account_edi_ubl_cii,l10n_account_edi_ubl_cii_tests: support common UBL and CII edi formats
    This commit aims at unifying the UBL formats for invoices and credit notes.
    
    Starting from the work of LAS, the module account_edi_ubl_cii contains the templates for UBL and CII,
    and provides inheritance for UBL: UBL 2.0 < UBL 2.1 < UBL Bis 3.
    It contains also the formats E-FFF, EHF3 (fully covered by Bis 3), XRechnung (in UBL), Factur-x (the only one in CII).
    All these formats are also improved to pass the ecosio validator and/or the country specific validator
    (for Factur-x: the validator from the FNFE, and Chorus Pro).
    
    Note that the xml files generated contain the pdf of the invoice/credit note encoded in base64.
    An xml file alone imported in Odoo will thus automatically retrieve the pdf.
    
    Before generating the xml files, we now also check a series of known constraints and possibly display
    a warning on top of the move view if some are not enforced (the xml file is generated anyway but it might not be valid).
    
    Tests were required: a new module was needed with dependency to the tested l10n: l10n_account_edi_ubl_cii_tests.
    The prefix "l10n_" prevents runbot from launching the tests everytime.
    
    Backport of: https://github.com/odoo/odoo/pull/88027
    
    closes odoo/odoo#83849
    
    Task: 2628093
    Signed-off-by: Laurent Smet <las@odoo.com>

--- addons/l10n_be_edi/tests/test_ubl.py
+++ addons/l10n_be_edi/tests/test_ubl.py
@@ -9,6 +9,12 @@ class TestUBL(AccountEdiTestCommon):
     def setUpClass(cls, chart_template_ref='l10n_be.l10nbe_chart_template', edi_format_ref='l10n_be_edi.edi_efff_1'):
         super().setUpClass(chart_template_ref=chart_template_ref, edi_format_ref=edi_format_ref)
 
+        if cls.env['ir.module.module'].search(
+            [('name', '=', 'account_edi_ubl_cii'), ('state', '=', 'installed')],
+            limit=1,
+        ):
+            cls.skipTest(cls, "L10n_be_edi Tests skipped because account_edi_ubl_cii is installed.")
+
         cls.partner_a.vat = 'BE0477472701'
 
     def test_invoice_edi_xml_update(self):
