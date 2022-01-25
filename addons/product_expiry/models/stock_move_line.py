221ee0cdc362e1958f9a5326c1966de9dfb47c48
yhu-odoo <yhu@odoo.com>
2022-01-25 09:40:54 +0000

    [IMP] stock, product_expiry: Change how we calculate dates related to
    expiration
    
    Previously, we do
    {expiration,use,removal,alert}_date = receipt_date + {expiration,use,removal,alert}_time
    
    After this commit, expiration date remains unchange, for the other 3
    dates, we calculate them by
    {use,removal,alert}_date = expiration_date - {use,removal,alert}_time
    
    Task-2657043
    
    closes odoo/odoo#86760
    
    Related: odoo/enterprise#25419
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/product_expiry/models/stock_move_line.py
+++ addons/product_expiry/models/stock_move_line.py
@@ -14,6 +14,8 @@ class StockMoveLine(models.Model):
         help='This is the date on which the goods with this Serial Number may'
         ' become dangerous and must not be consumed.')
     is_expired = fields.Boolean(related='lot_id.product_expiry_alert')
+    use_expiration_date = fields.Boolean(
+        string='Use Expiration Date', related='product_id.use_expiration_date')
 
     @api.depends('product_id', 'picking_type_use_create_lots', 'lot_id.expiration_date')
     def _compute_expiration_date(self):
@@ -46,6 +48,8 @@ class StockMoveLine(models.Model):
                 self.expiration_date = False
         return res
 
-    def _assign_production_lot(self, lot):
-        super()._assign_production_lot(lot)
-        self.lot_id._update_date_values(self[0].expiration_date)
+    def _prepare_new_lot_vals(self):
+        vals = super()._prepare_new_lot_vals()
+        if self.expiration_date:
+            vals['expiration_date'] = self.expiration_date
+        return vals
