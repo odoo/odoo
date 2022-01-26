0264816b7750b9d8ca0549090458b1cce24b7ba8
qsm-odoo <qsm@odoo.com>
2022-01-26 15:26:52 +0100

    [WIP] website: replace redirect from frontend to backend by a mini-nav
    Instead of redirecting to the backend with the website iframe when
    trying to reach a frontend URL, let the user reach that URL and add a
    small overlay allowing common actions, which all redirect to the Odoo
    backend.
    
    Of course, work in progress, URL to update, etc.

--- addons/website/static/src/js/content/redirect.js
+++ addons/website/static/src/js/content/redirect.js
@@ -1,5 +1,8 @@
 /** @odoo-module */
+
 import { session } from '@web/session';
+// import { _t } from "@web/core/l10n/translation"; // FIXME don't know why it does not work
+const _t = str => str;
 
 document.addEventListener('DOMContentLoaded', () => {
     if (session.is_website_user) {
@@ -10,9 +13,29 @@ document.addEventListener('DOMContentLoaded', () => {
         const websiteId = document.documentElement.dataset.websiteId;
         const {pathname, search} = window.location;
         const params = new URLSearchParams(search).toString();
-        document.body.innerHTML = '';
 
-        window.location.replace(`/web#action=website.website_editor&path=${encodeURIComponent(params ? `${pathname}?${params}` : pathname)}&website_id=${websiteId}`);
+        const autoredirectToBackendAction = false;
+        if (autoredirectToBackendAction) {
+            document.body.innerHTML = '';
+            window.location.replace(`/web#action=website.website_editor&path=${encodeURIComponent(params ? `${pathname}?${params}` : pathname)}&website_id=${websiteId}`);
+    } else {
+            const frontendToBackendNavEl = document.createElement('div');
+            frontendToBackendNavEl.classList.add('o_frontend_to_backend_nav');
+
+            const backendAppsButtonEl = document.createElement('a');
+            backendAppsButtonEl.href = '/web';
+            backendAppsButtonEl.title = _t("Go to your Odoo Apps");
+            backendAppsButtonEl.classList.add('o_frontend_to_backend_apps_btn', 'fa', 'fa-th');
+            frontendToBackendNavEl.appendChild(backendAppsButtonEl);
+
+            const backendEditButtonEl = document.createElement('a');
+            backendEditButtonEl.href = `/web#action=website.website_editor&path=${encodeURIComponent(params ? `${pathname}?${params}` : pathname)}&website_id=${websiteId}`;
+            backendEditButtonEl.title = _t("Edit your page content");
+            backendEditButtonEl.classList.add('o_frontend_to_backend_edit_btn', 'fa', 'fa-pencil-square-o');
+            frontendToBackendNavEl.appendChild(backendEditButtonEl);
+
+            document.body.appendChild(frontendToBackendNavEl);
+        }
     } else {
         document.addEventListener('click', (ev) => {
             const isEditorEnabled = document.body.classList.contains('editor_enable');
