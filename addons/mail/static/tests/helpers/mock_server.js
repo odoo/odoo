dbeda9e2dcf41bc705f6489fa42dbd32b91ef535
Alexandre Kühn <aku@odoo.com>
2022-01-26 14:41:14 +0100

    [IMP] mail: load mail failures in another RPC
    Task-2746116

--- addons/mail/static/tests/helpers/mock_server.js
+++ addons/mail/static/tests/helpers/mock_server.js
@@ -105,6 +105,9 @@ MockServer.include({
         if (route === '/mail/init_messaging') {
             return this._mockRouteMailInitMessaging();
         }
+        if (route === '/mail/load_message_failures') {
+            return this._mockRouteMailLoadMessageFailures();
+        }
         if (route === '/mail/history/messages') {
             const { min_id, max_id, limit } = args;
             return this._mockRouteMailMessageHistory(min_id, max_id, limit);
@@ -407,6 +410,15 @@ MockServer.include({
      */
     _mockRouteMailInitMessaging() {
         return this._mockResUsers_InitMessaging([this.currentUserId]);
+    },
+    /**
+     * Simulates the `/mail/load_message_failures` route.
+     *
+     * @private
+     * @returns {Object[]}
+     */
+    _mockRouteMailLoadMessageFailures() {
+        return this._mockResPartner_MessageFetchFailed(this.currentPartnerId);
     },
     /**
      * Simulates the `/mail/history/messages` route.
@@ -2088,7 +2100,7 @@ MockServer.include({
             current_partner: this._mockResPartnerMailPartnerFormat(user.partner_id).get(user.partner_id),
             current_user_id: this.currentUserId,
             current_user_settings: this._mockResUsersSettings_FindOrCreateForUser(user.id),
-            mail_failures: this._mockResPartner_MessageFetchFailed(user.partner_id),
+            mail_failures: [],
             menu_id: false, // not useful in QUnit tests
             needaction_inbox_counter: this._mockResPartner_GetNeedactionCount(user.partner_id),
             partner_root: this._mockResPartnerMailPartnerFormat(this.partnerRootId).get(this.partnerRootId),
