03c021c1d36e0771e654fc0486014bd888a3a22f
Thomas Beckers <tbs@odoo.com>
2022-01-27 13:37:05 +0000

    [FIX] Remove the context to disable prediction when importing invoices
    Allow the bill prediction when data like facturx is imported.
    This revert #68311 stating prediction might cause traceback when no account_id is found.
    Now, this error seems to not occur anymore.
    When importing data like facturx and no account_id is predicted, the default account set on the
    vendor journal will be set.
    
    opw-2662158
    
    closes odoo/odoo#83496
    
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account_edi_facturx/models/account_edi_format.py
+++ addons/account_edi_facturx/models/account_edi_format.py
@@ -210,8 +210,7 @@ class AccountEdiFormat(models.Model):
         invoice.move_type = default_move_type
 
         # self could be a single record (editing) or be empty (new).
-        with Form(invoice.with_context(default_move_type=default_move_type,
-                                       account_predictive_bills_disable_prediction=True)) as invoice_form:
+        with Form(invoice.with_context(default_move_type=default_move_type)) as invoice_form:
             self_ctx = self.with_company(invoice.company_id)
 
             # Partner (first step to avoid warning 'Warning! You must first select a partner.').
