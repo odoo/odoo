bb01bf0b233c66c0722a2128c4d2c329ccaabe6f
Brieuc-brd <brd@odoo.com>
2022-01-28 08:07:19 +0000

    [REF] web: update arrow directions & SCSS revamp
    Prior this commit, the popover arrow was in raw SCSS and used only
    the four basic directions (top, right, bottom, left).
    
    After this commit, the popover uses the Bootstrap arrow and this
    arrow can also use the additional directions defined in
    `position_hooks.js`.
    
    It also revamp the SCSS of the component.
    
    task-2748120
    
    closes odoo/odoo#83646
    
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>

--- addons/web/static/src/core/popover/popover.js
+++ addons/web/static/src/core/popover/popover.js
@@ -6,15 +6,41 @@ const { Component } = owl;
 
 export class Popover extends Component {
     setup() {
-        usePosition(this.props.target, "popper", {
-            margin: 16,
+        usePosition(this.props.target, {
             onPositioned: this.onPositioned,
             position: this.props.position,
         });
     }
     onPositioned(el, { direction, variant }) {
         const position = `${direction[0]}${variant[0]}`;
-        el.classList.add(`o-popper-position--${position}`);
+        el.classList.add(`bs-popover-${direction}`, `o-popover--${position}`);
+        const arrowEl = el.firstElementChild;
+        switch (position) {
+            case "tm": // top-middle
+            case "bm": // bottom-middle
+                arrowEl.classList.add("start-0", "end-0", "mx-auto");
+                break;
+            case "lm": // left-middle
+            case "rm": // right-middle
+                arrowEl.classList.add("top-0", "bottom-0", "my-auto");
+                break;
+            case "ts": // top-start
+            case "bs": // bottom-start
+                arrowEl.classList.add("end-auto");
+                break;
+            case "te": // top-end
+            case "be": // bottom-end
+                arrowEl.classList.add("start-auto");
+                break;
+            case "ls": // left-start
+            case "rs": // right-start
+                arrowEl.classList.add("bottom-auto");
+                break;
+            case "le": // left-end
+            case "re": // right-end
+                arrowEl.classList.add("top-auto");
+                break;
+        }
     }
 }
 
