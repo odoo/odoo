0eba294f91a47e99de67f53cd17f59733c998387
Nicolas (vin) <vin@odoo.com>
2022-01-28 09:55:14 +0100

    [FIX] account: allows changing journals on internal transfer
    At the moment, going from let's say bank to cash journal
    while editing an internal transfer will result in a traceback
    due to the fact that seek_for_line cannot properly map the
    move lines after changing the journal.
    This fix will make it so that we drop completely the old
    lines and then recreate them when such an issue arise.

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -793,13 +793,18 @@ class AccountPayment(models.Model):
             line_vals_list = pay._prepare_move_line_default_vals(write_off_line_vals=write_off_line_vals)
 
             line_ids_commands = []
-            if liquidity_lines:
+            if len(liquidity_lines) == 1:
                 line_ids_commands.append((1, liquidity_lines.id, line_vals_list[0]))
             else:
+                for line in liquidity_lines:
+                    line_ids_commands.append((2, line.id, 0))
                 line_ids_commands.append((0, 0, line_vals_list[0]))
-            if counterpart_lines:
+
+            if len(counterpart_lines) == 1:
                 line_ids_commands.append((1, counterpart_lines.id, line_vals_list[1]))
             else:
+                for line in counterpart_lines:
+                    line_ids_commands.append((2, line.id, 0))
                 line_ids_commands.append((0, 0, line_vals_list[1]))
 
             for line in writeoff_lines:
