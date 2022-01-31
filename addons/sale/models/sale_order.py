1ab4e7d8332e1dfd57fce07c06aadfec0b963779
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2022-01-31 18:43:49 +0000

    [FIX] account,sale: fiscal position map tax incl. to incl.
    Create a 21% tax included in price
    Create a 6% tax included in price
    Create a fiscal position that will replace the 21% tax included by the
    6% tax included
    Create a customer and assign this fiscal position
    Create a product :
    Price = 121€
    Tax = 21% tax included
    Create a SO with this customer and this product
    Create an invoice (from scratch) with this customer and this product
    
    SO : the unit price is 100,00€
    Invoice : the unit price is 106,00€
    
    Fix by creating a common method to calculate the price_unit
    
    opw-2699793
    
    closes odoo/odoo#86278
    
    X-original-commit: d470162960d0dba28ebc273b99ebf9c58093ea33
    Signed-off-by: Laurent Smet <las@odoo.com>
    Signed-off-by: Grazioso Andrea (agr) <agr@odoo.com>

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -516,8 +516,15 @@ class SaleOrder(models.Model):
                 pricelist=self.pricelist_id.id,
                 uom=line.product_uom.id
             )
-            price_unit = self.env['account.tax']._fix_tax_included_price_company(
-                line._get_display_price(product), line.product_id.taxes_id, line.tax_id, line.company_id)
+            price_unit = product._get_tax_included_unit_price(
+                line.company_id,
+                line.order_id.currency_id,
+                line.order_id.date_order,
+                'sale',
+                fiscal_position=line.order_id.fiscal_position_id,
+                product_price_unit=line._get_display_price(product),
+                product_currency=line.currency_id
+            )
             if self.pricelist_id.discount_policy == 'without_discount' and price_unit:
                 price_discount_unrounded = self.pricelist_id.get_product_price(product, line.product_uom_qty, self.partner_id, self.date_order, line.product_uom.id)
                 discount = max(0, (price_unit - price_discount_unrounded) * 100 / price_unit)
