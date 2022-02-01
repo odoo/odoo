c2c48d9186f0bd7bb2d8ff992b830094506f7a92
Géry Debongnie <ged@odoo.com>
2022-02-01 09:43:26 +0100

    use onWillDestroy in actioncontainer

--- addons/web/static/src/webclient/actions/action_container.js
+++ addons/web/static/src/webclient/actions/action_container.js
@@ -10,13 +10,14 @@ const { Component, xml, onWillDestroy } = owl;
 export class ActionContainer extends Component {
     setup() {
         this.info = {};
-        this.onActionManagerUpdate = ({ detail: info }) => {
+        const updateInfo = ({ detail: info }) => {
             this.info = info;
             this.render();
         };
-        this.env.bus.addEventListener("ACTION_MANAGER:UPDATE", this.onActionManagerUpdate);
+
+        this.env.bus.addEventListener("ACTION_MANAGER:UPDATE", updateInfo);
         onWillDestroy(() => {
-            this.env.bus.removeEventListener("ACTION_MANAGER:UPDATE", this.onActionManagerUpdate);
+            this.env.bus.removeEventListener("ACTION_MANAGER:UPDATE", updateInfo);
         });
     }
 }
