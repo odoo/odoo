456bbe689459c8b7bac2f8b4bd1ba5f014ba27ae
Nicolas Bayet <nby@odoo.com>
2022-02-01 11:31:50 +0100

    [FIX] web_editor: keep the selection when opening linktool
    task-2750970

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1210,7 +1210,8 @@ const Wysiwyg = Widget.extend({
         if (!this.options.snippets) {
             $toolbar.find('#justify, #table, #media-insert').remove();
         }
-        $toolbar.find('#create-link, #media-insert, #media-replace, #media-description').click(openTools);
+        $toolbar.find('#media-insert, #media-replace, #media-description').click(openTools);
+        $toolbar.find('#create-link').mousedown(openTools);
         $toolbar.find('#image-shape div, #fa-spin').click(e => {
             if (!this.lastMediaClicked) {
                 return;
