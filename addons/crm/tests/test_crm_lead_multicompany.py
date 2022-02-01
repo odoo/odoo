58b9b3e839c7725fdcd66376564ce6554a4b82ac
Hubert Van de Walle <huvw@odoo.com>
2022-02-01 10:42:32 +0000

    [FIX] crm, sales_team: compute company within allowed_company_ids
    Steps to follow
    
      - Select only one company in the company selector but not the default one
        from the current user;
      - Go the the CRM app;
      - Create a lead;
      -> A multi company error appears !
    
    Solution
    
      - The default team is now restricted to the companies in the context
      - When computing a lead's company_id when the team has no company, keep only
        the ones within the allowed_company_ids;
    
    opw-2713757
    
    closes odoo/odoo#83056
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/crm/tests/test_crm_lead_multicompany.py
+++ addons/crm/tests/test_crm_lead_multicompany.py
@@ -102,20 +102,20 @@ class TestCRMLeadMultiCompany(TestCrmCommon):
         lead = LeadUnsyncCids.sudo().create({
             'name': 'My Lead MC',
         })
-        self.assertEqual(lead.company_id, self.company_2)
-        self.assertEqual(lead.team_id, self.team_company2)
-        # self.assertEqual(lead.team_id, self.sales_team_1,
-        #                  'Lead: due to MC rule, took first availability in other company')
+        self.assertFalse(lead.company_id,
+                         'Lead: due to MC rule, avoid setting a company when it would cause crashes')
+        self.assertEqual(lead.team_id, self.sales_team_1,
+                         'Lead: due to MC rule, took first availability in other company')
         self.assertEqual(lead.user_id, self.user_sales_manager_mc)
 
-        # multicompany raises if trying to create manually
-        with self.assertRaises(AccessError):
-            lead = LeadUnsyncCids.create({
-                'name': 'My Lead MC',
-            })
-            # self.assertEqual(lead.company_id.id, False)
-            # self.assertEqual(lead.team_id, self.sales_team_1)
-            # self.assertEqual(lead.user_id, self.user_sales_manager_mc)
+        # manual creation
+        lead = LeadUnsyncCids.create({
+            'name': 'My Lead MC',
+        })
+        self.assertFalse(lead.company_id,
+                         'Lead: due to MC rule, avoid setting a company when it would cause crashes')
+        self.assertEqual(lead.team_id, self.sales_team_1)
+        self.assertEqual(lead.user_id, self.user_sales_manager_mc)
 
     @users('user_sales_manager_mc')
     def test_lead_mc_company_form(self):
