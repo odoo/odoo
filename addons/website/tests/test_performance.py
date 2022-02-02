4cb3293aeb4fecd0f2367704504dc3d20242986f
Rémy Voet (ryv) <ryv@odoo.com>
2022-02-02 14:32:16 +0000

    [FIX] website: remove force prefetch for translate fields
    Issue
    -----
    When website is installed, the rendering of template uses a side effect
    of the ORM cache (cache shared between sudoed env vs non-sudoed env) and
    the fields prefetching feature to work correctly.
    
    The `self.visibility` in (`_handle_visibility`, website/ir_ui_view.py)
    is done in sudo mode, then it will fetch all prefetchable fields and put
    them in the cache (that will be read in non-sudo mode in the render of
    the template).  Another example of issue related to this:
    https://github.com/odoo/odoo/pull/83341.
    
    Because of this, the fields of mixin `website.seo.metadata` were forced
    to be prefetchable (the default for translate is to be not prefetchable
    since https://github.com/odoo/odoo/pull/82896), which causes a useless
    LEFT JOIN on "ir_translation" in most of business flow.
    
    Fix
    ---
    Remove the `prefetch=True` on mixin fields, and add a extra read to fill
    the cache in case of website rendering.  It also allows to read these
    fields at the same time.
    
    Part-of: odoo/odoo#83818

--- addons/website/tests/test_performance.py
+++ addons/website/tests/test_performance.py
@@ -86,25 +86,25 @@ class TestWebsitePerformance(UtilPerf):
 
     def test_10_perf_sql_queries_page(self):
         # standard untracked website.page
-        self.assertEqual(self._get_url_hot_query(self.page.url), 11)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 12)
         self.menu.unlink()
-        self.assertEqual(self._get_url_hot_query(self.page.url), 13)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 14)
 
     def test_15_perf_sql_queries_page(self):
         # standard tracked website.page
         self.page.track = True
-        self.assertEqual(self._get_url_hot_query(self.page.url), 19)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 20)
         self.menu.unlink()
-        self.assertEqual(self._get_url_hot_query(self.page.url), 21)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 22)
 
     def test_20_perf_sql_queries_homepage(self):
         # homepage "/" has its own controller
-        self.assertEqual(self._get_url_hot_query('/'), 18)
+        self.assertEqual(self._get_url_hot_query('/'), 19)
 
     def test_30_perf_sql_queries_page_no_layout(self):
         # website.page with no call to layout templates
         self.page.arch = '<div>I am a blank page</div>'
-        self.assertEqual(self._get_url_hot_query(self.page.url), 9)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 10)
 
     def test_40_perf_sql_queries_page_multi_level_menu(self):
         # menu structure should not impact SQL requests
@@ -122,7 +122,7 @@ class TestWebsitePerformance(UtilPerf):
         menu_bb.parent_id = menu_b
         menu_aa.parent_id = menu_a
 
-        self.assertEqual(self._get_url_hot_query(self.page.url), 11)
+        self.assertEqual(self._get_url_hot_query(self.page.url), 12)
 
     def test_50_perf_sql_web_assets(self):
         # assets route /web/assets/..
