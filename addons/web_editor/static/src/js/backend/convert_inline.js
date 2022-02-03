fae07f2dcd74ed3ce85d37cc7ea0180b40994ff1
Antoine Guenet <age@odoo.com>
2022-02-03 12:49:00 +0000

    [FIX] web_editor: ensure border around comparison snippet
    The conversion of the comparison snippet for mail compatibility involves
    heavy changes in the html. In order to preserve the style, some style
    changes were needed as well. This change to the style of cards allows us
    to restore the border around the snippet.
    
    Part-of: odoo/odoo#84465

--- addons/web_editor/static/src/js/backend/convert_inline.js
+++ addons/web_editor/static/src/js/backend/convert_inline.js
@@ -262,6 +262,7 @@ function cardToTable($editable) {
     const editable = $editable.get(0);
     for (const card of editable.querySelectorAll('.card')) {
         const table = _createTable(card.attributes);
+        table.style.removeProperty('overflow');
         for (const child of [...card.childNodes]) {
             const row = document.createElement('tr');
             const col = document.createElement('td');
