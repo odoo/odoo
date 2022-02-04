c1ab58324f22ec51a63e1428072eea293d6ae99d
Achraf (abz) <abz@odoo.com>
2022-02-04 07:28:16 +0000

    [FIX] point_of_sale: Fix crash in ClientListScreen
    In some cases load_new_partners returns a rejected promise
    with an argument of type string, which causes a crash in
    the catch which considers that it is an rpc error using
    error.message.code and this is an error because error is
    a string in the case of a reject on load_new_partners
    
    opw-2679837
    
    closes odoo/odoo#83988
    
    X-original-commit: b62ce71de221100f6fdf6895556ab7ca42ea1949
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Achraf <abz@odoo.com>

--- addons/point_of_sale/static/src/js/Screens/ClientListScreen/ClientListScreen.js
+++ addons/point_of_sale/static/src/js/Screens/ClientListScreen/ClientListScreen.js
@@ -6,6 +6,7 @@ odoo.define('point_of_sale.ClientListScreen', function(require) {
     const { useListener } = require('web.custom_hooks');
 
     const { Observer, debounce } = owl;
+    const { isConnectionError } = require('point_of_sale.utils');
 
     /**
      * Render this screen using `showTempScreen` to select client.
@@ -163,7 +164,7 @@ odoo.define('point_of_sale.ClientListScreen', function(require) {
                 this.state.detailIsShown = false;
                 this.render();
             } catch (error) {
-                if (error.message.code < 0) {
+                if (isConnectionError(error)) {
                     await this.showPopup('OfflineErrorPopup', {
                         title: this.env._t('Offline'),
                         body: this.env._t('Unable to save changes.'),
