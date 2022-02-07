009e790b42b8bd43ecfba01d1ebd5cb6b2b84610
Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>
2022-02-07 16:48:54 +0100

    [WIP] l10n_pt: add qr code test

--- addons/l10n_pt/tests/test_pt_qr_code.py
+++ addons/l10n_pt/tests/test_pt_qr_code.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from addons.account.tests.account_test_savepoint import AccountTestInvoicingCommon
+from odoo import fields
 from odoo.tests import tagged
 
 
@@ -28,10 +29,12 @@ class PortugalQRCodeTest(AccountTestInvoicingCommon):
         cls.product = cls.env['product.product'].create({'name': 'Product :-)', 'standard_price': 100.0, 'lst_price': 100.0})
 
         cls.partner_a['country_id'] = cls.env.ref('base.be').id
-
+        
         cls.invoice = cls.env['account.move'].create({
             'type': 'out_invoice',
             'partner_id': cls.partner_a.id,
+            'date': fields.Date.from_string('2022-01-01'),
+            'name': 'INV/2022/0001',
             'invoice_line_ids': [
                 (0, 0, {
                     'name': 'Name 0',
@@ -72,14 +75,26 @@ class PortugalQRCodeTest(AccountTestInvoicingCommon):
         })
 
     def test_pt_qr_code_multiple_taxes(self):
-        self.invoice.preview_invoice()
+        self.invoice.preview_invoice()  # Triggers the _compute_qr_code_str method
         self.assertEqual(self.invoice.l10n_pt_qr_code_str,
-                         "A:123456789*B:999999990*C:BE*D:FT*E:N*F:20220204*G:out_invoice /*H:0"
-                         "*I1:PT*I2:100.00*I3:100.00*I4:6.00*I5:100.00*I6:13.00*I7:300.00*I8:69.00"
-                         "*N:88.00*O:600.00*Q:TODO*R:TODO")
+                         'A:123456789*B:999999990*C:BE*D:FT*E:N*F:20220101*G:out_invoice INV/2022/0001*H:0'
+                         '*I1:PT*I2:100.00*I3:100.00*I4:6.00*I5:100.00*I6:13.00*I7:300.00*I8:69.00'
+                         '*N:88.00*O:688.00*Q:TODO*R:TODO')
 
     def test_pt_qr_code_different_currency(self):
         pass
 
     def test_pt_qr_code_credit_note(self):
-        pass
+        move_reversal = self.env['account.move.reversal'].with_context(active_model='account.move', active_ids=self.invoice.ids).create({
+            'date': fields.Date.from_string('2022-01-01'),
+            'reason': 'no reason',
+            'refund_method': 'refund',
+        })
+        reversal = move_reversal.reverse_moves()
+        credit_note = self.env['account.move'].browse(reversal['res_id'])
+        credit_note['name'] = 'RINV/2022/0001'
+        credit_note.preview_invoice()
+        self.assertEqual(credit_note.l10n_pt_qr_code_str,
+                         'A:123456789*B:999999990*C:BE*D:NC*E:N*F:20220101*G:out_refund RINV/2022/0001*H:0'
+                         '*I1:PT*I2:100.00*I3:100.00*I4:6.00*I5:100.00*I6:13.00*I7:300.00*I8:69.00'
+                         '*N:88.00*O:688.00*Q:TODO*R:TODO')
