c6368f87e5a62a1dde8afdfa4efb69d6bb09241d
Sébastien Theys <seb@odoo.com>
2022-02-07 17:59:17 +0000

    [IMP] mail: rename newItem/itemNew to addingItem
    Consistency in naming. Remove CSS as a bonus.
    
    Part of task-2694206, split from main PR to ease its diff
    
    closes odoo/odoo#84127
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/js/tours/mail.js
+++ addons/mail/static/src/js/tours/mail.js
@@ -12,7 +12,7 @@ tour.register('mail_tour', {
     content: Markup(_t("<p>Channels make it easy to organize information across different topics and groups.</p> <p>Try to <b>create your first channel</b> (e.g. sales, marketing, product XYZ, after work party, etc).</p>")),
     position: 'bottom',
 }, {
-    trigger: '.o_DiscussSidebarCategory_itemNewInput',
+    trigger: '.o_DiscussSidebarCategory_addingItemInput',
     content: Markup(_t("<p>Create a channel here.</p>")),
     position: 'bottom',
     auto: true,
