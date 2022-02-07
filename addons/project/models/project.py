4d43fbb4febf1e2e16e01979b16a52313be5fd48
Xavier BOL (xbo) <xbo@odoo.com>
2022-02-07 17:04:35 +0100

    [FIX] project: create subtask in project sharing
    Before this commit, when the current user is in project sharing and see
    the form view of a task with at least a subtask. He cannot create
    another one because of an error in the `_compute_portal_user_names`
    method. Indeed, it is because we want to update the cache to get the
    user_ids of the task related since the portal user cannot see the
    users assigned to the task since he cannot access an internal user. This
    update cache is made by calling the `_read` method but this method
    checks after having fetching the data if the self is equal to the
    recordset fetched. If the `self` contained newIds, then the method will
    raise an error because a Task with newId != Task with id in db.
    
    This commit fixes the compute method to call the _read method only on a
    self with no NewIds to avoid this issue and correctly updates the cache
    to get the name of the users assigned to a task in project sharing for
    the portal user.
    
    Fix found by Florian Damhaut <flda@odoo.com>
    
    Co-authored-by Florian Damhaut <flda@odoo.com>
    
    opw-2745857
    
    Closes #84189

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -1402,8 +1402,9 @@ class Task(models.Model):
             (In other words, this compute is only used in project sharing views to see all assignees for each task)
         """
         if self.ids:
-            # fetch 'user_ids' in superuser mode (and override value in cache)
-            self._read(['user_ids'])
+            # fetch 'user_ids' in superuser mode (and override value in cache
+            # browse is useful to avoid miscache because of the newIds contained in self
+            self.browse(self.ids)._read(['user_ids'])
         for task in self.with_context(prefetch_fields=False):
             task.portal_user_names = ', '.join(task.user_ids.mapped('name'))
 
