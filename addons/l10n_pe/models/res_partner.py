d4ad1abb46aa43de9a550d64caef2d137c958213
Josse Colpaert <jco@odoo.com>
2022-02-08 11:21:12 +0000

    [FIX] l10n_pe: street_name and street had wrong edit/read only class
    Before, if you would put Peru Street 23, it would put Peru in House number.
    This is because the editable field was the street field, so behind the scenes,
    Odoo will split it and according to the standard format without country,
    this means Peru will go into House.
    
    We tried first with a change inversing it, but as we are doing the change,
    the best thing is to remove the split all together.  We added also onchange
    logic that if you put a district it would automatically put the right city.
    
    City or city_id should depend on the country and is correctly inherited by a
    fields_view_get.
    
    Task 2724531
    
    closes odoo/odoo#84163
    
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/l10n_pe/models/res_partner.py
+++ addons/l10n_pe/models/res_partner.py
@@ -1,5 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ResPartner(models.Model):
@@ -8,3 +8,13 @@ class ResPartner(models.Model):
     l10n_pe_district = fields.Many2one(
         'l10n_pe.res.city.district', string='District',
         help='Districts are part of a province or city.')
+
+    @api.onchange('l10n_pe_district')
+    def _onchange_l10n_pe_district(self):
+        if self.l10n_pe_district:
+            self.city_id = self.l10n_pe_district.city_id
+
+    @api.onchange('city_id')
+    def _onchange_l10n_pe_city_id(self):
+        if self.city_id and self.l10n_pe_district.city_id and self.l10n_pe_district.city_id != self.city_id:
+            self.l10n_pe_district = False
