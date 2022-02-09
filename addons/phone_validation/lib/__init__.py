4b2d2aff2b5d92da3b81b3b92ad2cd0b8c7280c2
Bruno-brsy <brsy@odoo.com>
2022-02-09 09:49:55 +0100

    [FIX] phone_validation: new convention number for Ivory Coast
    Step to reproduce:
    
    - Install contacts
    - create a new contact and set "Côte d'Ivoire" as country
    - add a phone number like this one: (+225) 01 02 03 04 05
    - try to send a sms to the number, it's says "(Invalid number)" instead
      of the number, so it's not possible to send the SMS
    
    The Ivory Coast has a new standard for the phone number since the 31
    January 2021, the phone number can have a length of ten digits. And we
    use the library phonenumbers with the version 8.9.10 and 8.8.1 for the
    runbot and the SaaS server, but it's quite old (the 8.9.10 is released
    on Jul 12 2018). The library in version 8.12.29 resolve the issue, but
    using the recommended installation method, we would not have this
    version.
    
    opw-2726582

--- addons/phone_validation/lib/__init__.py
+++ addons/phone_validation/lib/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import lib
-from . import tools
-from . import models
+from . import phonenumbers_patch
