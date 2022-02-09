e656997e6120db97318b53d4374e9b3928179e94
Patrick Hoste <pko@odoo.com>
2022-02-09 14:18:06 +0000

    [FW][FIX] (website_)event_booth_sale: fix e-commerce displayed price
    PURPOSE
    
    Since this change (odoo#75843), the displayed price on the e-commerce
    was the product price and not the sum of the prices of all the
    selected booths. This commit fix the issue.
    
    LINKS
    
    Task-2657480
    closes: odoo/odoo#88863
    
    X-original-commit: 5d9b3827ad0d9ba5937d471cc1e047185713f1be
    Forward-port-of: #84261

---
+++ addons/website_event_booth_sale/tests/test_event_booth_sale.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from datetime import datetime, timedelta
+
+from odoo import Command, fields
+from odoo.tests import HttpCase
+from odoo.tests.common import tagged
+
+
+@tagged('post_install', '-at_install')
+class TestWebsiteEventBoothSale(HttpCase):
+
+    def setUp(self):
+        super().setUp()
+        self.booth_product = self.env['product.product'].create({
+            'name': 'Test Booth Product',
+            'description_sale': 'Mighty Booth Description',
+            'list_price': 20,
+            'standard_price': 60.0,
+            'detailed_type': 'event_booth',
+        })
+        self.event_booth_category = self.env['event.booth.category'].create({
+            'name': 'Standard',
+            'description': '<p>Standard</p>',
+            'product_id': self.booth_product.id,
+            'price': 100.0,
+        })
+        self.event_type = self.env['event.type'].create({
+            'name': 'Booth Type',
+            'event_type_booth_ids': [
+                Command.create({
+                    'name': 'Standard 1',
+                    'booth_category_id': self.event_booth_category.id,
+                }),
+                Command.create({
+                    'name': 'Standard 2',
+                    'booth_category_id': self.event_booth_category.id,
+                }),
+                Command.create({
+                    'name': 'Standard 3',
+                    'booth_category_id': self.event_booth_category.id,
+                }),
+            ],
+        })
+        self.env['event.event'].create({
+            'name': 'Test Event Booths',
+            'event_type_id': self.event_type.id,
+            'date_begin': fields.Datetime.to_string(datetime.today() + timedelta(days=1)),
+            'date_end': fields.Datetime.to_string(datetime.today() + timedelta(days=15)),
+            'website_published': True,
+            'website_menu': True,
+            'booth_menu': True,
+        })
+
+    def test_tour(self):
+        self.start_tour('/event', 'website_event_booth_tour', login='portal')
