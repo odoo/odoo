f27148b4a3fca68a033644bf92810ff3e4d5fd63
Merlin (megu) <megu@odoo.com>
2022-02-10 10:49:56 +0000

    [FIX] point_of_sale: product name on delivery slip from pos order
    Delivery slip created from a POS order contains the product name 3 times
    
    Steps to reproduce:
    1. Install Point of Sale app
    2. On the Point of Sale dashboard, open 'Shop' settings (with the three
    dots on the top right corner)
    3. Check Ship Later in Inventory
    4. Open a POS session, add a product to an order and go to the payment
    5. Select a payment method and a customer, check the option "Ship Later"
     and then validate the order
    6. Quit the POS session and go to Point of Sale > Orders > Orders
    7. Open the created order and access its picking with the smart button
    8. Print the delivery slip
    9. The name of the product is printed 3 times
    
    Solution:
    Override `_get_stock_move_values` in point_of_sale in order to overwrite
    the value of `description_picking` when the order comes from a POS
    
    Problem:
    `product_description_variants` doesn't hold the same value whether it's
    in sales or in point_of_sale. In Sales it describes the variants of the
    product but in point_of_sale, it is the full product name i.e. the
    product name and the variants used.
    
    opw-2736443
    
    closes odoo/odoo#85963
    
    X-original-commit: 1c6710f59e2c9cf679c80a246ebb355a1b21776c
    Signed-off-by: Masereel Pierre <pim@odoo.com>
    Signed-off-by: Guillaume Merlin (megu) <megu@odoo.com>

--- addons/point_of_sale/models/__init__.py
+++ addons/point_of_sale/models/__init__.py
@@ -18,6 +18,7 @@ from . import res_partner
 from . import res_company
 from . import res_config_settings
 from . import stock_picking
+from . import stock_rule
 from . import stock_warehouse
 from . import pos_payment
 from . import pos_payment_method
