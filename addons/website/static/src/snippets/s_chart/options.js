97313afb4e238d0d027fe03635815908a8547a61
Guillaume (gdi) <gdi@odoo.com>
2022-02-10 13:11:19 +0000

    [FIX] website: ensure order of we-matrix's events
    This commit ensures that changes are applied in the same order as the
    events are triggered on the we-matrix. This was not the case because of
    another bug we had to consider and which is not there anymore, this is
    why this fix is done in 15.0 and not before.
    
    This commit is the continuation of [1] which applied the same change for
    we-input.
    
    [1]: https://github.com/odoo/odoo/commit/db62a5fac123fc72e37e94709b1a0c80f32a8a29
    
    task-2761187
    
    closes odoo/odoo#84486
    
    X-original-commit: 703e1ec69c2cbdbdbafeef9ac359db9f0f3b7021
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/static/src/snippets/s_chart/options.js
+++ addons/website/static/src/snippets/s_chart/options.js
@@ -479,18 +479,7 @@ options.registry.InnerChart = options.Class.extend({
      * @param {Event} ev
      */
     _onMatrixInputFocusOut: function (ev) {
-        // Sometimes, an input is focusout for internal reason (like an undo
-        // recording) then focused again manually in the same JS stack
-        // execution. In that case, the blur should not trigger an option
-        // selection as the user did not leave the input. We thus defer the blur
-        // handling to then check that the target is indeed still blurred before
-        // executing the actual option selection.
-        setTimeout(() => {
-            if (ev.currentTarget === document.activeElement) {
-                return;
-            }
-            this._reloadGraph();
-        });
+        this._reloadGraph();
     },
     /**
      * Set the selected cell/header and display the related remove button
