343020c12d487f18c39bff28d74f0fc7f8075a7a
luvi <luvi@odoo.com>
2022-02-11 09:20:09 +0100

    wip

---
+++ addons/web/static/src/fields/journal_dashboard_graph_field.js
@@ -0,0 +1,148 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { standardFieldProps } from "./standard_field_props";
+import { useAssets } from "@web/core/assets";
+
+const { Component, useEffect, useRef } = owl;
+
+export class JournalDashboardGraphField extends Component {
+    setup() {
+        this.chart = null;
+        this.canvasRef = useRef("canvas");
+        this.data = JSON.parse(this.props.value);
+        console.log(this.data);
+
+        useAssets({ jsLibs: ["/web/static/lib/Chart/Chart.js"] });
+
+        useEffect(() => {
+            this.renderChart();
+            return () => {
+                if (this.chart) {
+                    this.chart.destroy();
+                }
+            };
+        });
+    }
+
+    get className() {
+        return this.props.record.data["graph_type"]
+            ? `o_graph_${this.props.record.data["graph_type"]}chart`
+            : "";
+    }
+
+    /**
+     * Instantiates a Chart (Chart.js lib) to render the graph according to
+     * the current config.
+     */
+    renderChart() {
+        if (this.chart) {
+            this.chart.destroy();
+        }
+        let config;
+        if (this.props.record.data["graph_type"] === "line") {
+            config = this._getLineChartConfig();
+        } else if (this.props.record.data["graph_type"] === "bar") {
+            config = this._getBarChartConfig();
+        }
+        this.chart = new Chart(this.canvasRef.el, config);
+        // To perform its animations, ChartJS will perform each animation
+        // step in the next animation frame. The initial rendering itself
+        // is delayed for consistency. We can avoid this by manually
+        // advancing the animation service.
+        Chart.animationService.advance();
+    }
+    _getLineChartConfig() {
+        let labels = this.data[0].values.map(function (pt) {
+            return pt.x;
+        });
+        let borderColor = this.data[0].is_sample_data ? "#dddddd" : "#875a7b";
+        let backgroundColor = this.data[0].is_sample_data ? "#ebebeb" : "#dcd0d9";
+        return {
+            type: "line",
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        data: this.data[0].values,
+                        fill: "start",
+                        label: this.data[0].key,
+                        backgroundColor: backgroundColor,
+                        borderColor: borderColor,
+                        borderWidth: 2,
+                    },
+                ],
+            },
+            options: {
+                legend: { display: false },
+                scales: {
+                    yAxes: [{ display: false }],
+                    xAxes: [{ display: false }],
+                },
+                maintainAspectRatio: false,
+                elements: {
+                    line: {
+                        tension: 0.000001,
+                    },
+                },
+                tooltips: {
+                    intersect: false,
+                    position: "nearest",
+                    caretSize: 0,
+                },
+            },
+        };
+    }
+
+    _getBarChartConfig() {
+        let data = [];
+        let labels = [];
+        let backgroundColor = [];
+
+        this.data[0].values.forEach(function (pt) {
+            data.push(pt.value);
+            labels.push(pt.label);
+            let color =
+                pt.type === "past" ? "#ccbdc8" : pt.type === "future" ? "#a5d8d7" : "#ebebeb";
+            backgroundColor.push(color);
+        });
+        return {
+            type: "bar",
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        data: data,
+                        fill: "start",
+                        label: this.data[0].key,
+                        backgroundColor: backgroundColor,
+                    },
+                ],
+            },
+            options: {
+                legend: { display: false },
+                scales: {
+                    yAxes: [{ display: false }],
+                },
+                maintainAspectRatio: false,
+                tooltips: {
+                    intersect: false,
+                    position: "nearest",
+                    caretSize: 0,
+                },
+                elements: {
+                    line: {
+                        tension: 0.000001,
+                    },
+                },
+            },
+        };
+    }
+}
+
+JournalDashboardGraphField.template = "web.JournalDashboardGraphField";
+JournalDashboardGraphField.props = {
+    ...standardFieldProps,
+};
+
+registry.category("fields").add("dashboard_graph", JournalDashboardGraphField);
--- addons/web/static/tests/fields/journal_dashboard_graph_field_tests.js
+++ addons/web/static/tests/fields/journal_dashboard_graph_field_tests.js
@@ -1,192 +1,57 @@
 /** @odoo-module **/
 
-import { setupViewRegistries } from "../views/helpers";
+import { click, editInput } from "../helpers/utils";
+import { makeView, setupViewRegistries } from "../views/helpers";
 
 let serverData;
 
 QUnit.module("Fields", (hooks) => {
+    var graph_values = [
+        { value: 300, label: "5-11 Dec" },
+        { value: 500, label: "12-18 Dec" },
+        { value: 100, label: "19-25 Dec" },
+    ];
     hooks.beforeEach(() => {
         serverData = {
             models: {
                 partner: {
                     fields: {
-                        date: { string: "A date", type: "date", searchable: true },
-                        datetime: { string: "A datetime", type: "datetime", searchable: true },
-                        display_name: { string: "Displayed name", type: "char", searchable: true },
-                        foo: {
-                            string: "Foo",
-                            type: "char",
-                            default: "My little Foo Value",
-                            searchable: true,
-                            trim: true,
-                        },
-                        bar: { string: "Bar", type: "boolean", default: true, searchable: true },
-                        empty_string: {
-                            string: "Empty string",
-                            type: "char",
-                            default: false,
-                            searchable: true,
-                            trim: true,
-                        },
-                        txt: {
-                            string: "txt",
-                            type: "text",
-                            default: "My little txt Value\nHo-ho-hoooo Merry Christmas",
-                        },
-                        int_field: {
-                            string: "int_field",
-                            type: "integer",
-                            sortable: true,
-                            searchable: true,
-                        },
-                        qux: { string: "Qux", type: "float", digits: [16, 1], searchable: true },
-                        p: {
-                            string: "one2many field",
-                            type: "one2many",
-                            relation: "partner",
-                            searchable: true,
-                        },
-                        trululu: {
-                            string: "Trululu",
-                            type: "many2one",
-                            relation: "partner",
-                            searchable: true,
-                        },
-                        timmy: {
-                            string: "pokemon",
-                            type: "many2many",
-                            relation: "partner_type",
-                            searchable: true,
-                        },
-                        product_id: {
-                            string: "Product",
-                            type: "many2one",
-                            relation: "product",
-                            searchable: true,
-                        },
-                        sequence: { type: "integer", string: "Sequence", searchable: true },
-                        currency_id: {
-                            string: "Currency",
-                            type: "many2one",
-                            relation: "currency",
-                            searchable: true,
-                        },
-                        selection: {
-                            string: "Selection",
+                        graph_data: { string: "Graph Data", type: "text" },
+                        graph_type: {
+                            string: "Graph Type",
                             type: "selection",
-                            searchable: true,
                             selection: [
-                                ["normal", "Normal"],
-                                ["blocked", "Blocked"],
-                                ["done", "Done"],
+                                ["line", "Line"],
+                                ["bar", "Bar"],
                             ],
                         },
-                        document: { string: "Binary", type: "binary" },
-                        hex_color: { string: "hexadecimal color", type: "char" },
                     },
                     records: [
                         {
                             id: 1,
-                            date: "2017-02-03",
-                            datetime: "2017-02-08 10:00:00",
-                            display_name: "first record",
-                            bar: true,
-                            foo: "yop",
-                            int_field: 10,
-                            qux: 0.44444,
-                            p: [],
-                            timmy: [],
-                            trululu: 4,
-                            selection: "blocked",
-                            document: "coucou==\n",
-                            hex_color: "#ff0000",
+                            graph_type: "bar",
+                            graph_data: JSON.stringify([
+                                {
+                                    color: "blue",
+                                    title: "Partner 1",
+                                    values: graph_values,
+                                    key: "A key",
+                                    area: true,
+                                },
+                            ]),
                         },
                         {
                             id: 2,
-                            display_name: "second record",
-                            bar: true,
-                            foo: "blip",
-                            int_field: 0,
-                            qux: 0,
-                            p: [],
-                            timmy: [],
-                            trululu: 1,
-                            sequence: 4,
-                            currency_id: 2,
-                            selection: "normal",
-                        },
-                        {
-                            id: 4,
-                            display_name: "aaa",
-                            foo: "abc",
-                            sequence: 9,
-                            int_field: false,
-                            qux: false,
-                            selection: "done",
-                        },
-                        { id: 3, bar: true, foo: "gnap", int_field: 80, qux: -3.89859 },
-                        { id: 5, bar: false, foo: "blop", int_field: -4, qux: 9.1, currency_id: 1 },
-                    ],
-                    onchanges: {},
-                },
-                product: {
-                    fields: {
-                        name: { string: "Product Name", type: "char", searchable: true },
-                    },
-                    records: [
-                        {
-                            id: 37,
-                            display_name: "xphone",
-                        },
-                        {
-                            id: 41,
-                            display_name: "xpad",
-                        },
-                    ],
-                },
-                partner_type: {
-                    fields: {
-                        name: { string: "Partner Type", type: "char", searchable: true },
-                        color: { string: "Color index", type: "integer", searchable: true },
-                    },
-                    records: [
-                        { id: 12, display_name: "gold", color: 2 },
-                        { id: 14, display_name: "silver", color: 5 },
-                    ],
-                },
-                currency: {
-                    fields: {
-                        digits: { string: "Digits" },
-                        symbol: { string: "Currency Sumbol", type: "char", searchable: true },
-                        position: { string: "Currency Position", type: "char", searchable: true },
-                    },
-                    records: [
-                        {
-                            id: 1,
-                            display_name: "$",
-                            symbol: "$",
-                            position: "before",
-                        },
-                        {
-                            id: 2,
-                            display_name: "€",
-                            symbol: "€",
-                            position: "after",
-                        },
-                    ],
-                },
-                "ir.translation": {
-                    fields: {
-                        lang: { type: "char" },
-                        value: { type: "char" },
-                        res_id: { type: "integer" },
-                    },
-                    records: [
-                        {
-                            id: 99,
-                            res_id: 37,
-                            value: "",
-                            lang: "en_US",
+                            graph_type: "line",
+                            graph_data: JSON.stringify([
+                                {
+                                    color: "red",
+                                    title: "Partner 0",
+                                    values: graph_values,
+                                    key: "A key",
+                                    area: true,
+                                },
+                            ]),
                         },
                     ],
                 },
@@ -242,19 +107,19 @@ QUnit.module("Fields", (hooks) => {
         var done = assert.async();
         assert.expect(6);
 
-        testUtils.mock.patch(JournalDashboardGraph, {
+        /*testUtils.mock.patch(JournalDashboardGraph, {
             on_attach_callback: function () {
                 assert.step("on_attach_callback");
             },
             on_detach_callback: function () {
                 assert.step("on_detach_callback");
             },
-        });
+        });*/
 
-        createView({
-            View: KanbanView,
-            model: "partner",
-            data: this.data,
+        makeView({
+            serverData,
+            type: "kanban",
+            resModel: "partner",
             arch:
                 '<kanban class="o_kanban_test">' +
                 '<field name="graph_type"/>' +
@@ -265,7 +130,8 @@ QUnit.module("Fields", (hooks) => {
                 "</t>" +
                 "</templates></kanban>",
             domain: [["id", "in", [1, 2]]],
-        }).then(function (kanban) {
+        });
+        /*.then(function (kanban) {
             assert.verifySteps(["on_attach_callback", "on_attach_callback"]);
 
             kanban.on_detach_callback();
@@ -275,7 +141,7 @@ QUnit.module("Fields", (hooks) => {
             kanban.destroy();
             testUtils.mock.unpatch(JournalDashboardGraph);
             done();
-        });
+        });*/
     });
 
     QUnit.skip("JournalDashboardGraphField is rendered correctly", async function (assert) {
