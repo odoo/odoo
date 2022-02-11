c11ae302fc0b0ac259d5392e9befa648eeedb697
Brice bib Bartoletti <bib@odoo.com>
2022-02-11 16:44:32 +0000

    [FIX]purchase_stock,stock_account:partner on aml
    The aim of this commit is to set partner on anglosaxon
    aml in order to help our client in their stock
    reconciliation process in case it hasn't been done automatically.
    
    Before this commit:
    anglosaxon line were created without partner
    
    After this commit:
    A partner is set when anglosaxon line are created
    
    closes odoo/odoo#84547
    
    Ticket: 2692308
    Community-pr: https://github.com/odoo/odoo/pull/84468
    X-original-commit: abfe37fcea5b20f77799d9331d4d011530880669
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/stock_account/models/account_move.py
+++ addons/stock_account/models/account_move.py
@@ -134,6 +134,7 @@ class AccountMove(models.Model):
                 lines_vals_list.append({
                     'name': line.name[:64],
                     'move_id': move.id,
+                    'partner_id': move.commercial_partner_id.id,
                     'product_id': line.product_id.id,
                     'product_uom_id': line.product_uom_id.id,
                     'quantity': line.quantity,
@@ -149,6 +150,7 @@ class AccountMove(models.Model):
                 lines_vals_list.append({
                     'name': line.name[:64],
                     'move_id': move.id,
+                    'partner_id': move.commercial_partner_id.id,
                     'product_id': line.product_id.id,
                     'product_uom_id': line.product_uom_id.id,
                     'quantity': line.quantity,
