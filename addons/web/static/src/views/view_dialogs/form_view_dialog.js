907648c27a9c88461d20939e817ea8b961d3dde3
luvi <luvi@odoo.com>
2022-02-11 08:37:59 +0100

    fix FormViewDialog to make tests pass

--- addons/web/static/src/views/view_dialogs/form_view_dialog.js
+++ addons/web/static/src/views/view_dialogs/form_view_dialog.js
@@ -6,33 +6,35 @@ import { sprintf } from "@web/core/utils/strings";
 import { FormRenderer } from "@web/views/form/form_renderer";
 import { FormArchParser } from "@web/views/form/form_view";
 
+const { onWillStart } = owl;
+
 export class FormViewDialog extends Dialog {
     setup() {
         super.setup();
         this.viewService = useService("view");
         this.archInfo = this.props.archInfo;
         this.title = sprintf(this.env._t("Open: %s"), this.props.title);
-    }
 
-    async willStart() {
-        if (!this.archInfo) {
-            const { form } = await this.viewService.loadViews({
-                resModel: this.props.record.resModel,
-                context: this.props.record.context,
-                views: [[this.props.viewId || false, "form"]],
-            });
-            const archInfo = new FormArchParser().parse(form.arch, form.fields);
-            this.archInfo = {
-                ...archInfo,
-                activeFields: archInfo.fields,
-                fields: form.fields,
-            };
-        }
-        // FIXME: here we override the fields of the list/kanban view
-        Object.assign(this.props.record.activeFields, this.archInfo.activeFields);
-        Object.assign(this.props.record.fields, this.archInfo.fields);
-        this.props.record.fieldNames = Object.keys(this.props.record.activeFields);
-        await this.props.record.load();
+        onWillStart(async () => {
+            if (!this.archInfo) {
+                const { form } = await this.viewService.loadViews({
+                    resModel: this.props.record.resModel,
+                    context: this.props.record.context,
+                    views: [[this.props.viewId || false, "form"]],
+                });
+                const archInfo = new FormArchParser().parse(form.arch, form.fields);
+                this.archInfo = {
+                    ...archInfo,
+                    activeFields: archInfo.fields,
+                    fields: form.fields,
+                };
+            }
+            // FIXME: here we override the fields of the list/kanban view
+            Object.assign(this.props.record.activeFields, this.archInfo.activeFields);
+            Object.assign(this.props.record.fields, this.archInfo.fields);
+            this.props.record.fieldNames = Object.keys(this.props.record.activeFields);
+            await this.props.record.load();
+        });
     }
 
     save() {
