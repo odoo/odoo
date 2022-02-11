odoo/odoo

commit ae96f0e272f2edd30d7bccfd11c9623130fcf678

[FIX] stock_landed_costs: fix `MemoryError` on m2m compute
Backport of f329de26.

When there are millions of records in stock_picking table the compute of
alowed_picking_ids may fail with `MemoryError`. This field is only used
on the view view_stock_landed_cost_form thus after this patch it will be
useless. We keep it, but also fix its compute, to not touch the
interface of a stable version.

addons/stock_landed_costs/models/stock_landed_cost.py
@@ -82,6 +82,9 @@ class LandedCost(models.Model):
 
     @api.depends('company_id')
     def _compute_allowed_picking_ids(self):
+        # Backport of f329de26: allowed_picking_ids is useless, view_stock_landed_cost_form no longer uses it,
+        # the field and its compute are kept since this is a stable version. Still, this compute has been made
+        # more resilient to MemoryErrors.
         valued_picking_ids_per_company = defaultdict(list)
         if self.company_id:
             self.env.cr.execute("""SELECT sm.picking_id, sm.company_id
@@ -92,7 +95,10 @@ class LandedCost(models.Model):
             for res in self.env.cr.fetchall():
                 valued_picking_ids_per_company[res[1]].append(res[0])
         for cost in self:
-            cost.allowed_picking_ids = valued_picking_ids_per_company[cost.company_id.id]
+            n = 5000
+            cost.allowed_picking_ids = valued_picking_ids_per_company[cost.company_id.id][:n]
+            for ids_chunk in tools.split_every(n, valued_picking_ids_per_company[cost.company_id.id][n:]):
+                cost.allowed_picking_ids = tuple((4, id_) for id_ in ids_chunk)
 
     @api.model
     def create(self, vals):

addons/stock_landed_costs/views/stock_landed_cost_views.xml
@@ -29,8 +29,8 @@
                         <group>
                             <group>
                                 <field name="date"/>
-                                <field name="allowed_picking_ids" invisible="1"/>
-                                <field name="picking_ids" widget="many2many_tags" options="{'no_create_edit': True}" domain="[('id', 'in', allowed_picking_ids)]"/>
+                                <field name="picking_ids" widget="many2many_tags" options="{'no_create_edit': True}"
+                                       domain="[('company_id', '=', company_id), ('move_lines.stock_valuation_layer_ids', '!=', False)]"/>
                             </group>
                             <group>
                                 <label for="account_journal_id" string="Journal"/>

commit 9ac96ffa89f5e8a41faf24c5b71382c185c6ba4a

[FIX] stock_landed_costs: use list not tuple
ae96f0e introduced an issue because the update of the m2m used a `tuple`
instead of a `list`

How to reproduce:

* Apply only the (Python) code changes done in ae96f0e, keeping the same
view (i.e. ignore the changed done to the view in ae96f0e).
* Instead of `n=5000` in the compute set `n=1` (it cannot be reproduced
with 5000 unless we create enough dummy data).
* On a clean v13 db with Landed costs (Accounting + Inventory + Purchase
+ demo data)
* Confirm a Purchase
* Create a Landed cost
* Try to select a transfer

Traceback:
```
...
    return f(self, *args, **kwargs)
  File "/home/odoo/src/odoo/13.0/odoo/sql_db.py", line 250, in execute
    res = self._obj.execute(query, params)
psycopg2.errors.UndefinedFunction: operator does not exist: integer = integer[]
LINE 1: ...FROM "stock_picking" WHERE (("stock_picking"."id" in (ARRAY[...
                                                             ^
HINT:  No operator matches the given name and argument types. You might need to add explicit type casts.
```

On the reported issues the traceback occur only for DBs with more than
5000 valid pickings, and for views that use the field
`allowed_pickign_ids`.

opw-2762355

addons/stock_landed_costs/models/stock_landed_cost.py
@@ -98,7 +98,7 @@ class LandedCost(models.Model):
             n = 5000
             cost.allowed_picking_ids = valued_picking_ids_per_company[cost.company_id.id][:n]
             for ids_chunk in tools.split_every(n, valued_picking_ids_per_company[cost.company_id.id][n:]):
-                cost.allowed_picking_ids = tuple((4, id_) for id_ in ids_chunk)
+                cost.allowed_picking_ids = [(4, id_) for id_ in ids_chunk]
 
     @api.model
     def create(self, vals):