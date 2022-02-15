c9e3f6d4fc5c2e1108caccff6527a17ec6621df9
Prakash Prajapati <ppr@odoo.com>
2022-02-15 18:36:37 +0530

    [IMP] project_sms: send sms at stage change of project and task
    The purpose of this commit is to include SMS templates. Templates allow people to
    customize the content of sent SMS. When the project/task stage changes, the user will
    be notified and there will be a track in the chatter.
    
    - add this field on the right of the 'email template' one in list view.
    optional and hidden by default.
    - add it below the 'name' quick search in the search view.
    
    task-2683575

---
+++ addons/project_sms/models/project_task_type.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class ProjectTaskType(models.Model):
+    _inherit = "project.task.type"
+
+    sms_template_id = fields.Many2one('sms.template', string="SMS Template",
+        domain=[('model', '=', 'project.task')], help="Template used to render SMS reminder content.")
