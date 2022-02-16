033e51e111054fc423afdcdadb65aee002d15151
Alexandre Kühn <aku@odoo.com>
2022-02-16 12:25:45 +0100

    [IMP] mail: adjust sound effect volume
    Sounds were too loud and inconsistent.

--- addons/mail/static/src/models/sound_effects/sound_effects.js
+++ addons/mail/static/src/models/sound_effects/sound_effects.js
@@ -9,7 +9,7 @@ registerModel({
     identifyingFields: ['messaging'],
     fields: {
         channelJoin: one('SoundEffect', {
-            default: insertAndReplace({ filename: 'channel_01_in' }),
+            default: insertAndReplace({ defaultVolume: 0.3, filename: 'channel_01_in' }),
             isCausal: true,
         }),
         channelLeave: one('SoundEffect', {
@@ -17,19 +17,19 @@ registerModel({
             isCausal: true,
         }),
         deafen: one('SoundEffect', {
-            default: insertAndReplace({ filename: 'deafen_new_01' }),
+            default: insertAndReplace({ defaultVolume: 0.15, filename: 'deafen_new_01' }),
             isCausal: true,
         }),
         incomingCall: one('SoundEffect', {
-            default: insertAndReplace({ filename: 'call_02_in_' }),
+            default: insertAndReplace({ defaultVolume: 0.15, filename: 'call_02_in_' }),
             isCausal: true,
         }),
         memberLeave: one('SoundEffect', {
-            default: insertAndReplace({ filename: 'channel_01_out' }),
+            default: insertAndReplace({ defaultVolume: 0.5, filename: 'channel_01_out' }),
             isCausal: true,
         }),
         mute: one('SoundEffect', {
-            default: insertAndReplace({ filename: 'mute_1' }),
+            default: insertAndReplace({ defaultVolume: 0.2, filename: 'mute_1' }),
             isCausal: true,
         }),
         newMessage: one('SoundEffect', {
@@ -37,23 +37,23 @@ registerModel({
             isCausal: true,
         }),
         pushToTalkOn: one('SoundEffect', {
-            default: insertAndReplace({ filename: 'ptt_push_1' }),
+            default: insertAndReplace({ defaultVolume: 0.05, filename: 'ptt_push_1' }),
             isCausal: true,
         }),
         pushToTalkOff: one('SoundEffect', {
-            default: insertAndReplace({ filename: 'ptt_release_1' }),
+            default: insertAndReplace({ defaultVolume: 0.05, filename: 'ptt_release_1' }),
             isCausal: true,
         }),
         screenSharing: one('SoundEffect', {
-            default: insertAndReplace({ filename: 'share_02' }),
+            default: insertAndReplace({ defaultVolume: 0.5, filename: 'share_02' }),
             isCausal: true,
         }),
         undeafen: one('SoundEffect', {
-            default: insertAndReplace({ filename: 'undeafen_new_01' }),
+            default: insertAndReplace({ defaultVolume: 0.15, filename: 'undeafen_new_01' }),
             isCausal: true,
         }),
         unmute: one('SoundEffect', {
-            default: insertAndReplace({ filename: 'unmute_1' }),
+            default: insertAndReplace({ defaultVolume: 0.2, filename: 'unmute_1' }),
             isCausal: true,
         }),
     },
