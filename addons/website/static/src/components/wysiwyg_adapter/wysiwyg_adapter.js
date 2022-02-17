618d15c0be8cd8b510f91891f0d989ca43323263
Kamen Zhekov <kzh@odoo.com>
2022-02-17 09:56:15 +0000

    [FIX] web_editor, website: fix website editor as restricted editor
    When assigned the role of restricted editor, there are many ways to
    break the web_editor when trying to do actions that should be
    unavailable. There is a traceback for the following scenarios under
    some circumstances (mainly because of pages without editable areas or
    features without the proper access rights):
    
    - Drag and dropping snippet when Restricted Editor.
    - Clicking on product when Restricted Editor in /shop.
    - Clicking on product image on specific product page.
    - Clicking on user name (e.g. Marc Demo).
    - Clicking on menu items or logo.
    - Clicking on a blog's image in /blog.
    - Clicking on a blog's image on specific blog page.
    - Clicking on calendar's image in /calendar.
    
    There is now no longer a traceback which makes the editor crash or
    freeze. This mimics the behavior in other cases where the editor does
    not show a traceback, but there is no message indicating that the action
    is unauthorized.
    
    When accessing a menu that cannot be edited, the "Edit the menu" button
    is not shown to the restricted editor.
    
    task-2747895
    opw-3164176
    
    closes odoo/odoo#118116
    
    X-original-commit: 15c85e4f9f967ee04af3cdeb766d280a2c00731e
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Co-authored-by: Benjamin Vray <bvr@odoo.com>
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/website/static/src/components/wysiwyg_adapter/wysiwyg_adapter.js
+++ addons/website/static/src/components/wysiwyg_adapter/wysiwyg_adapter.js
@@ -343,7 +343,9 @@ export class WysiwygAdapterComponent extends ComponentAdapter {
      * @private
      */
     _addEditorMessages() {
-        const $wrap = this.$editable.find('.oe_structure.oe_empty, [data-oe-type="html"]');
+        const $wrap = this.$editable
+            .find('.oe_structure.oe_empty, [data-oe-type="html"]')
+            .filter(':o_editable');
         this.$editorMessageElement = $wrap.not('[data-editor-message]')
                 .attr('data-editor-message-default', true)
                 .attr('data-editor-message', this.env._t('DRAG BUILDING BLOCKS HERE'));
