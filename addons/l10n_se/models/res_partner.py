48d2da4448f7272cc2a82f2b4a5196083544af47
Julien Alardot (jual) <jual@odoo.com>
2022-02-18 15:09:11 +0100

    [FIX]: l10n_se: Hide Swedish OCR for non-SE
    Swedish OCR options are currently available  everywhere when the
    localisation is implemented.
    
    This hides the options when the current user active company is not
    Swedish
    
    Task: task-2732385
    Signed-off-by: Julien Alardot (jual) <jual@odoo.com>

--- addons/l10n_se/models/res_partner.py
+++ addons/l10n_se/models/res_partner.py
@@ -10,6 +10,7 @@ class ResPartner(models.Model):
 
     l10n_se_check_vendor_ocr = fields.Boolean(string='Check Vendor OCR', help='This Vendor uses OCR Number on their Vendor Bills.')
     l10n_se_default_vendor_payment_ref = fields.Char(string='Default Vendor Payment Ref', help='If set, the vendor uses the same Default Payment Reference or OCR Number on all their Vendor Bills.')
+    user_country_code = fields.Char(string="User current company country code", compute="_compute_user_country_code")
 
     @api.onchange('l10n_se_default_vendor_payment_ref')
     def onchange_l10n_se_default_vendor_payment_ref(self):
@@ -19,3 +20,8 @@ class ResPartner(models.Model):
                 luhn.validate(reference)
             except: 
                 return {'warning': {'title': _('Warning'), 'message': _('Default vendor OCR number isn\'t a valid OCR number.')}}
+
+    @api.depends_context("allowed_company_ids")
+    def _compute_user_country_code(self):
+        cid = self._context.get("allowed_company_ids", [None])[0]
+        self.user_country_code = self.env["res.company"].browse(cid).country_code if cid else "SE"
