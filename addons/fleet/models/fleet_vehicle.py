24354b55ab2bac416534bc84eb7063da4f443010
Thibault Francois <tfr@odoo.com>
2022-02-18 15:29:41 +0000

    [FIX] fleet: fix search method for contract_renewal*
    the search method of contract_renewal_due_soon and
    contract_renewal_overdue return a domain
    id in res_ids where res_ids are id of contract
    not vehicle
    
    So it return random result
    
    Solution
    --------
    Get the vehicle_id from the contract record set
    
    closes odoo/odoo#84987
    
    X-original-commit: 975841953adaa6b7adc8016fb96cc6efc8891613
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/fleet/models/fleet_vehicle.py
+++ addons/fleet/models/fleet_vehicle.py
@@ -216,7 +216,7 @@ class FleetVehicle(models.Model):
             ('expiration_date', '>', today),
             ('expiration_date', '<', limit_date),
             ('state', 'in', ['open', 'expired'])
-        ]).mapped('id')
+        ]).mapped('vehicle_id').ids
         res.append(('id', search_operator, res_ids))
         return res
 
@@ -232,7 +232,7 @@ class FleetVehicle(models.Model):
             ('expiration_date', '!=', False),
             ('expiration_date', '<', today),
             ('state', 'in', ['open', 'expired'])
-        ]).mapped('id')
+        ]).mapped('vehicle_id').ids
         res.append(('id', search_operator, res_ids))
         return res
 
