6426d0fb8cd66667960db45527f17334b2ffee38
Nicolas (vin) <vin@odoo.com>
2022-02-18 15:53:48 +0000

    [FIX] account_edi_facturx: fix upload with inactive currency
    Retroactive fix for an issue in 15.0+
    In 15.0+, importing an invoice with an inactive currency will raise
    a traceback. Before this it was handled by not setting the correct
    currency on the invoice if not active.
    
    With this change, we'll raise an error instead of creating an invoice
    with the wrong currency.
    
    X-original-commit: 14c61c2563efeed1ca14294da32e2eb6d1855c5b

--- addons/account_edi_facturx/models/account_edi_format.py
+++ addons/account_edi_facturx/models/account_edi_format.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, _
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, float_repr, is_html_empty, str2bool
 from odoo.tests.common import Form
-from odoo.exceptions import UserError
+from odoo.exceptions import RedirectWarning, UserError
 
 from datetime import datetime
 import markupsafe
@@ -178,7 +178,20 @@ class AccountEdiFormat(models.Model):
                 # Currency.
                 currency_str = elements[0].attrib.get('currencyID', None)
                 if currency_str:
-                    invoice_form.currency_id = self._retrieve_currency(currency_str)
+                    currency = self._retrieve_currency(currency_str)
+                    if currency and not currency.active:
+                        error_msg = _('The currency (%s) of the document you are uploading is not active in this database.\n'
+                                      'Please activate it before trying again to import.', currency.name)
+                        error_action = {
+                            'view_mode': 'form',
+                            'res_model': 'res.currency',
+                            'type': 'ir.actions.act_window',
+                            'target': 'new',
+                            'res_id': currency.id,
+                            'views': [[False, 'form']]
+                        }
+                        raise RedirectWarning(error_msg, error_action, _('Display the currency'))
+                    invoice_form.currency_id = currency
 
                     # Store xml total amount.
                     amount_total_import = total_amount * refund_sign
