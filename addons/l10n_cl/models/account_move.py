de2292d4a908c972fc69ddbf516df7cc460d5885
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-02-18 10:33:46 +0000

    [FIX] l10n_ar/cl: starting sequence
    When getting l10n_ar/l10n_cl starting_squence check with the proper company (self.company_id) and not self.env.company_id
    
    closes odoo/odoo#85688
    
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_cl/models/account_move.py
+++ addons/l10n_cl/models/account_move.py
@@ -100,7 +100,7 @@ class AccountMove(models.Model):
     def _get_starting_sequence(self):
         """ If use documents then will create a new starting sequence using the document type code prefix and the
         journal document number with a 6 padding number """
-        if self.journal_id.l10n_latam_use_documents and self.env.company.country_id.code == "CL":
+        if self.journal_id.l10n_latam_use_documents and self.company_id.country_id.code == "CL":
             if self.l10n_latam_document_type_id:
                 return self._l10n_cl_get_formatted_sequence()
         return super()._get_starting_sequence()
