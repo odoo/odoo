b3489af4c12dcd70ad3eee8ce728b10f335c90bb
Florian Damhaut <flda@odoo.com>
2022-02-18 12:48:18 +0000

    [FIX] web: basic_model: save a paged nested o2m
    In a form view, have a o2m field (A), with a nested o2m field (B).
    The A field should have multiple pages, with each record having at least one record
    in their B field.
    
    Have a (C) field that will modify a record in the nested o2m when changed.
    
    Change the C field, and save.
    
    Before this commit, the record's nested o2m from the other page had a wrong value
    (a mere 4 command).
    
    After this commit, their value is a command 4 and a command 1, with the right values.
    
    closes odoo/odoo#99986
    
    X-original-commit: e45e3b83f0f3116cc983c6bd8a9212e815b4d4f5
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/legacy/js/views/basic/basic_model.js
+++ addons/web/static/src/legacy/js/views/basic/basic_model.js
@@ -3311,6 +3311,7 @@ var BasicModel = AbstractModel.extend({
     _generateX2ManyCommands: function (record, options) {
         var self = this;
         options = options || {};
+        const changesOnly = options.changesOnly;
         var fields = record.fields;
         if (options.fieldNames) {
             fields = _.pick(fields, options.fieldNames);
@@ -3403,7 +3404,7 @@ var BasicModel = AbstractModel.extend({
                             if (!this.isNew(relRecord.id)) {
                                 // the subrecord already exists in db
                                 commands[fieldName].push(x2ManyCommands.link_to(relRecord.res_id));
-                                if (this.isDirty(relRecord.id)) {
+                                if (changesOnly ? Object.keys(changes).length : this.isDirty(relRecord.id)) {
                                     delete changes.id;
                                     commands[fieldName].push(x2ManyCommands.update(relRecord.res_id, changes));
                                 }
