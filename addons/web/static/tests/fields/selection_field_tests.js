7adc01fa9b69c518f1b3f3b2543bc9afd43a0f7a
luvi <luvi@odoo.com>
2022-02-21 12:29:48 +0100

    serverData cleanup in some tests

--- addons/web/static/tests/fields/selection_field_tests.js
+++ addons/web/static/tests/fields/selection_field_tests.js
@@ -12,24 +12,8 @@ QUnit.module("Fields", (hooks) => {
                 partner: {
                     fields: {
                         display_name: { string: "Displayed name", type: "char" },
-                        foo: { string: "Foo", type: "char", default: "My little Foo Value" },
-                        bar: { string: "Bar", type: "boolean", default: true },
                         int_field: { string: "int_field", type: "integer", sortable: true },
-                        qux: { string: "Qux", type: "float", digits: [16, 1] },
-                        p: {
-                            string: "one2many field",
-                            type: "one2many",
-                            relation: "partner",
-                            relation_field: "trululu",
-                        },
-                        turtles: {
-                            string: "one2many turtle field",
-                            type: "one2many",
-                            relation: "turtle",
-                            relation_field: "turtle_trululu",
-                        },
                         trululu: { string: "Trululu", type: "many2one", relation: "partner" },
-                        timmy: { string: "pokemon", type: "many2many", relation: "partner_type" },
                         product_id: { string: "Product", type: "many2one", relation: "product" },
                         color: {
                             type: "selection",
@@ -40,57 +24,26 @@ QUnit.module("Fields", (hooks) => {
                             default: "red",
                             string: "Color",
                         },
-                        date: { string: "Some Date", type: "date" },
-                        datetime: { string: "Datetime Field", type: "datetime" },
-                        user_id: { string: "User", type: "many2one", relation: "user" },
-                        reference: {
-                            string: "Reference Field",
-                            type: "reference",
-                            selection: [
-                                ["product", "Product"],
-                                ["partner_type", "Partner Type"],
-                                ["partner", "Partner"],
-                            ],
-                        },
-                        model_id: { string: "Model", type: "many2one", relation: "ir.model" },
                     },
                     records: [
                         {
                             id: 1,
                             display_name: "first record",
-                            bar: true,
-                            foo: "yop",
                             int_field: 10,
-                            qux: 0.44,
-                            p: [],
-                            turtles: [2],
-                            timmy: [],
                             trululu: 4,
-                            user_id: 17,
-                            reference: "product,37",
                         },
                         {
                             id: 2,
                             display_name: "second record",
-                            bar: true,
-                            foo: "blip",
                             int_field: 9,
-                            qux: 13,
-                            p: [],
-                            timmy: [],
                             trululu: 1,
                             product_id: 37,
-                            date: "2017-01-25",
-                            datetime: "2016-12-12 10:55:05",
-                            user_id: 17,
                         },
                         {
                             id: 4,
                             display_name: "aaa",
-                            bar: false,
                         },
                     ],
-                    onchanges: {},
                 },
                 product: {
                     fields: {
@@ -106,118 +59,6 @@ QUnit.module("Fields", (hooks) => {
                             display_name: "xpad",
                         },
                     ],
-                },
-                partner_type: {
-                    fields: {
-                        name: { string: "Partner Type", type: "char" },
-                        color: { string: "Color index", type: "integer" },
-                    },
-                    records: [
-                        { id: 12, display_name: "gold", color: 2 },
-                        { id: 14, display_name: "silver", color: 5 },
-                    ],
-                },
-                turtle: {
-                    fields: {
-                        display_name: { string: "Displayed name", type: "char" },
-                        turtle_foo: { string: "Foo", type: "char" },
-                        turtle_bar: { string: "Bar", type: "boolean", default: true },
-                        turtle_int: { string: "int", type: "integer", sortable: true },
-                        turtle_description: { string: "Description", type: "text" },
-                        turtle_trululu: {
-                            string: "Trululu",
-                            type: "many2one",
-                            relation: "partner",
-                        },
-                        turtle_ref: {
-                            string: "Reference",
-                            type: "reference",
-                            selection: [
-                                ["product", "Product"],
-                                ["partner", "Partner"],
-                            ],
-                        },
-                        product_id: {
-                            string: "Product",
-                            type: "many2one",
-                            relation: "product",
-                            required: true,
-                        },
-                        partner_ids: { string: "Partner", type: "many2many", relation: "partner" },
-                    },
-                    records: [
-                        {
-                            id: 1,
-                            display_name: "leonardo",
-                            turtle_bar: true,
-                            turtle_foo: "yop",
-                            partner_ids: [],
-                        },
-                        {
-                            id: 2,
-                            display_name: "donatello",
-                            turtle_bar: true,
-                            turtle_foo: "blip",
-                            turtle_int: 9,
-                            partner_ids: [2, 4],
-                        },
-                        {
-                            id: 3,
-                            display_name: "raphael",
-                            product_id: 37,
-                            turtle_bar: false,
-                            turtle_foo: "kawa",
-                            turtle_int: 21,
-                            partner_ids: [],
-                            turtle_ref: "product,37",
-                        },
-                    ],
-                    onchanges: {},
-                },
-                user: {
-                    fields: {
-                        name: { string: "Name", type: "char" },
-                        partner_ids: {
-                            string: "one2many partners field",
-                            type: "one2many",
-                            relation: "partner",
-                            relation_field: "user_id",
-                        },
-                    },
-                    records: [
-                        {
-                            id: 17,
-                            name: "Aline",
-                            partner_ids: [1, 2],
-                        },
-                        {
-                            id: 19,
-                            name: "Christine",
-                        },
-                    ],
-                },
-                "ir.model": {
-                    fields: {
-                        model: { string: "Model", type: "char" },
-                    },
-                    records: [
-                        {
-                            id: 17,
-                            name: "Partner",
-                            model: "partner",
-                        },
-                        {
-                            id: 20,
-                            name: "Product",
-                            model: "product",
-                        },
-                        {
-                            id: 21,
-                            name: "Partner Type",
-                            model: "partner_type",
-                        },
-                    ],
-                    onchanges: {},
                 },
             },
         };
