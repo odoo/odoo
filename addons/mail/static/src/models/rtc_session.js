860e09cd8b1969dbe32f534b56c1437dfe20d1ba
Alexandre Kühn <aku@odoo.com>
2022-02-21 15:34:41 +0100

    [IMP] mail: turn "RtcSession/rtcDataChannel" into field
    Task-2734101

--- addons/mail/static/src/models/rtc_session.js
+++ addons/mail/static/src/models/rtc_session.js
@@ -412,6 +412,13 @@ registerModel({
             inverse: 'currentRtcSession',
         }),
         /**
+         * Contains the RTCDataChannel of the rtc session.
+         */
+        rtcDataChannel: one('RtcDataChannel', {
+            inverse: 'rtcSession',
+            isCausal: true,
+        }),
+        /**
          * MediaStream of the user's video.
          *
          * Should be divided into userVideoStream and displayStream,
