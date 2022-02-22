c9af9ae9831c32eb8289431f6b379043a036e7f4
Moisés López <moylop260@vauxoo.com>
2022-02-22 03:18:00 +0000

    [REF] base_vat: Speed-up check_vat if there is not vat
    The following line of code:
    
        self.env.ref("base.europe").with_context(lang="fr_CA").country_ids
    
    It is executing the following query:
    
        SELECT res_country_res_country_group_rel.res_country_group_id, res_country_res_country_group_rel.res_country_id FROM res_country_res_country_group_rel, "res_country" LEFT JOIN "ir_translation" AS "res_country__name" ON ("res_country"."id" = "res_country__name"."res_id" AND "res_country__name"."type" = 'model' AND "res_country__name"."name" = 'res.country,name' AND "res_country__name"."lang" = 'fr_CA' AND "res_country__name"."value" != '')
        WHERE 1=1 AND res_country_res_country_group_rel.res_country_group_id IN (1) AND res_country_res_country_group_rel.res_country_id = res_country.id
        ORDER BY COALESCE("res_country__name"."value", "res_country"."name")
        OFFSET 0
    
    With duration: 138.119ms
    
    But it is not needed to spend processing to compute it if it even will not be used if the `vat` is empty
    
    If your website checkout process is creating almost the partners without `vat`
    it could have a better performance
    
    X-original-commit: c4d40410b03c0ac9948e1b30df6ba839f8675d59

--- addons/base_vat/models/res_partner.py
+++ addons/base_vat/models/res_partner.py
@@ -153,6 +153,7 @@ class ResPartner(models.Model):
         # This is for API pushes from external platforms where you have no control over VAT numbers.
         if self.env.context.get('no_vat_validation'):
             return
+<<<<<<< HEAD
 
         for partner in self:
             country = partner.commercial_partner_id.country_id
@@ -178,6 +179,11 @@ class ResPartner(models.Model):
                  for the check, and no conclusion could be made with certainty.
         """
         # Get company
+=======
+        partners_with_vat = self.filtered('vat')
+        if not partners_with_vat:
+            return
+>>>>>>> d14336c12c6... temp
         if self.env.context.get('company_id'):
             company = self.env['res.company'].browse(self.env.context['company_id'])
         else:
@@ -185,6 +191,7 @@ class ResPartner(models.Model):
 
         # Get check function: either simple syntactic check or call to VIES service
         eu_countries = self.env.ref('base.europe').country_ids
+<<<<<<< HEAD
         if company.vat_check_vies and default_country in eu_countries and partner_is_company:
             check_func = self.vies_vat_check
         else:
@@ -213,6 +220,40 @@ class ResPartner(models.Model):
         # triggers two distinct write on res.partner, one for each field, both triggering this constraint.
         # If vat is set before country_id, the constraint must not break.
         return check_result
+=======
+        for partner in partners_with_vat:
+            is_eu_country = partner.commercial_partner_id.country_id in eu_countries
+            if company.vat_check_vies and is_eu_country and partner.is_company:
+                # force full VIES online check
+                check_func = self.vies_vat_check
+            else:
+                # quick and partial off-line checksum validation
+                check_func = self.simple_vat_check
+
+            failed_check = False
+            #check with country code as prefix of the TIN
+            vat_country_code, vat_number = self._split_vat(partner.vat)
+            vat_has_legit_country_code = self.env['res.country'].search([('code', '=', vat_country_code.upper())])
+            if not vat_has_legit_country_code:
+                vat_has_legit_country_code = vat_country_code.lower() in _region_specific_vat_codes
+            if vat_has_legit_country_code:
+                failed_check = not check_func(vat_country_code, vat_number)
+
+            #if fails, check with country code from country
+            partner_country_code = partner.commercial_partner_id.country_id.code
+            if (not vat_has_legit_country_code or failed_check) and partner_country_code:
+                failed_check = not check_func(partner_country_code.lower(), partner.vat)
+
+            # We allow any number if it doesn't start with a country code and the partner has no country.
+            # This is necessary to support an ORM limitation: setting vat and country_id together on a company
+            # triggers two distinct write on res.partner, one for each field, both triggering this constraint.
+            # If vat is set before country_id, the constraint must not break.
+
+            if failed_check:
+                country_code = partner_country_code or vat_country_code
+                msg = partner._construct_constraint_msg(country_code.lower() if country_code else None)
+                raise ValidationError(msg)
+>>>>>>> d14336c12c6... temp
 
     @api.model
     def _build_vat_error_message(self, country_code, wrong_vat, record_label):
