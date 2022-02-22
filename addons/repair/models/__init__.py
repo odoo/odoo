bb94bf70715d9ee165b1dad6f7c7d9fd93ae290c
Tiffany Chang (tic) <tic@odoo.com>
2022-02-22 15:09:16 +0100

    [IMP] repair,stock: allow creation/link of repairs from returns
    This feature supports the use case of:
    - a customer receives a delivery from you.
    - there is a defective product in the delivery
    - they schedule a repair (RO) + return (picking) for the product
    
    We want to ease the creation/management of the repair order associated
    with the return, therefore the following features are added:
    
    - Create links between ROs and return pickings (smart button in return
      of all repairs associated with it + field in RO)
    - Option (requires active setting) to create RO(s) directly from a
      return picking
    
    Implementation notes:
    - Because return picking type doesn't have its own "Type of Operation"
      (it is 'code'='incoming'), we default to only incoming picking types
      that have at least 1 other picking type it is the
      'return_picking_type_id' of.
    - Originally a Wizard for choosing which/how many products should have
      ROs created for them was designed (i.e. to match return wizard + to
      multi-create ROs), but this was determined to be too complicated due
      to SN/lot selection. Instead simple approach of creating a single RO
      each time button is pushed was implemented to match the helpdesk >
      repair button workflow.
    - ROs are purposely allowed for 'draft' and 'cancelled' returns/moves to
      support different types of workflows (e.g. RO is confirmed before
      return is)
    
    Task: 2732517

--- addons/repair/models/__init__.py
+++ addons/repair/models/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import repair
+from . import stock_picking
 from . import stock_traceability
 from . import stock_lot
 from . import account_move
