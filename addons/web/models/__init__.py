808780d48d4014ab2797288d6be52f3bb0bf9bc8
Thibault Delavallée <tde@odoo.com>
2022-02-23 13:54:41 +0100

    [MOV] various: move ir.qweb.fields code into right files
    When working on qweb fields it is difficult to find some custom override as
    they are never in the right file. Finding them is always a bit of random pick.
    
    Task-2607416

--- addons/web/models/__init__.py
+++ addons/web/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import ir_qweb
+from . import ir_qweb_fields
 from . import ir_http
 from . import ir_ui_menu
 from . import models
