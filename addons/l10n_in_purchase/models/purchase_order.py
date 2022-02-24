af2ca9b61d4cc5514f60625ca8c7d5dcfc6f2c71
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2022-02-24 15:22:04 +0100

    [IMP] purchase,sale,l10n_in_[sale|purchase]: remove l10n_in_company_country_code field
    1. Create a PO from Indian vendor [DEMO], confirm it, receive the products.
    2. Go to Accounting app, manually create the vendor bill:
      - select the Vendor [DEMO]
      - in auto-complete field select the one created at 1.
    
    Traceback will raise because the field l10n_in_company_country_code
    was removed from account.move in
    17610e8ca97b2e5315cc063ee3973f81ab910f5e
    
    opw-2745052
    
    X-original-commit: ffbfdde4b5b4cd791e866341a31cdf5e8aeb27e3

--- addons/l10n_in_purchase/models/purchase_order.py
+++ addons/l10n_in_purchase/models/purchase_order.py
@@ -19,11 +19,10 @@ class PurchaseOrder(models.Model):
             ('special_economic_zone', 'Special Economic Zone'),
             ('deemed_export', 'Deemed Export')
         ], string="GST Treatment", states=Purchase.READONLY_STATES, compute="_compute_l10n_in_gst_treatment", store=True)
-    l10n_in_company_country_code = fields.Char(related='company_id.account_fiscal_country_id.code', string="Country code")
 
     @api.onchange('company_id')
     def l10n_in_onchange_company_id(self):
-        if self.l10n_in_company_country_code == 'IN':
+        if self.country_code == 'IN':
             domain = [('company_id', '=', self.company_id.id), ('type', '=', 'purchase')]
             journal = self.env['account.journal'].search(domain, limit=1)
             if journal:
@@ -34,7 +33,7 @@ class PurchaseOrder(models.Model):
         for order in self:
             # set default value as False so CacheMiss error never occurs for this field.
             order.l10n_in_gst_treatment = False
-            if order.l10n_in_company_country_code == 'IN':
+            if order.country_code == 'IN':
                 l10n_in_gst_treatment = order.partner_id.l10n_in_gst_treatment
                 if not l10n_in_gst_treatment and order.partner_id.country_id and order.partner_id.country_id.code != 'IN':
                     l10n_in_gst_treatment = 'overseas'
