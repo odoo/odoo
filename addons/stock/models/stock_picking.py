a3af57275844118b4a99cc495f49f6b8645141ba
Arnold Moyaux <arm@odoo.com>
2022-02-24 10:19:43 +0000

    [FIX] stock: reserved move base on the schedule date
    Usecase to reproduce:
    - 2 product A in stock
    - Deliver 1 the 1/1/2022
    - Deliver 1 the 4/1/2022
    - Deliver 1 the 2/1/2022
    
    Current behavior:
    Check Availability the 3 pickings at once. The 2 first are reserve
    
    Expected behavior:
    The picking for 1/1/2022 and 2/1/2022 are reserve
    
    It happens because the sorted is only done on date deadline
    
    opw-2762223
    
    X-original-commit: 68d579510a7034119c65c3b04be9389a56829c49

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -791,7 +791,7 @@ class Picking(models.Model):
         """
         self.filtered(lambda picking: picking.state == 'draft').action_confirm()
         moves = self.move_ids.filtered(lambda move: move.state not in ('draft', 'cancel', 'done')).sorted(
-            key=lambda move: (-int(move.priority), not bool(move.date_deadline), move.date_deadline, move.id)
+            key=lambda move: (-int(move.priority), not bool(move.date_deadline), move.date_deadline, move.date, move.id)
         )
         if not moves:
             raise UserError(_('Nothing to check the availability for.'))
