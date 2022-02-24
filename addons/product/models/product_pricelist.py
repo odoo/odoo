44c11fafd5919fff0e0e83bde7340ff363662680
Audric Onockx (auon) <auon@odoo.com>
2022-02-24 14:04:57 +0000

    [FIX] product : apply only active price rules
    Steps :
    Install Sale / E-commerce.
    Create product P (price = 100).
    Create price list L.
    Create price rule R (product = P, price = 50).
    Go to product P > Price Rules > archive R.
    Create a quotation. / Website > click 'Go to website' > Shop.
    Select price list L.
    Create a SOL with product P. / Search for product P.
    
    Issue :
    Price is 50.
    
    Cause :
    Archived price rules are included in the 'search' of
    _compute_price_rule_get_items().
    
    Fix :
    Exclude them.
    
    opw-2752184
    
    X-original-commit: 24d757184e7d08811a6cb343ab3c6e0921de9e7c

--- addons/product/models/product_pricelist.py
+++ addons/product/models/product_pricelist.py
@@ -573,6 +573,9 @@ class PricelistItem(models.Model):
                 values.update(dict(categ_id=None))
         return super(PricelistItem, self).write(values)
 
+    def toggle_active(self):
+        raise ValidationError(_("You cannot disable a pricelist rule, please delete it or archive its pricelist instead."))
+
     def _is_applicable_for(self, product, qty_in_product_uom):
         """Check whether the current rule is valid for the given product & qty.
 
