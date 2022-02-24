498e41f734db9a2e5ced3676fb25166446543949
Yannick Tivisse <yti@odoo.com>
2022-02-24 09:24:50 +0000

    [IMP] resource: Move test models in their own module
    Purpose
    =======
    
    When creating resources, SQL request were made to this test table.
    
    Part-of: odoo/odoo#85133

---
+++ addons/test_resource/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Test - Resource',
+    'version': '1.1',
+    'category': 'Hidden',
+    'description': """""",
+    'depends': ['resource'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'assets': {},
+    'license': 'LGPL-3',
+}
