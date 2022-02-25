eabf047055e770e575617c276b619ea8bbf64621
Tiffany Chang (tic) <tic@odoo.com>
2022-02-25 16:18:08 +0000

    [REM] purchase_requisition_stock_dropshipping: rem old call to tender pt1
    The Call to Tender purchase requisition is being removed and implemented
    as something managed completely within a PO. This includes removing the
    'tenders' option for products which means the dropshipping + auto-create
    call to tenders use case no longer exists => this module is no longer
    needed.
    
    Part of task: 2695116
    Upgrade PR: odoo/upgrade#3586
    
    Part-of: odoo/odoo#87656

--- addons/purchase_requisition_stock_dropshipping/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import purchase
-from . import purchase_requisition
--- addons/purchase_requisition_stock_dropshipping/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import test_purchase_requisition_stock_dropshipping
