86384c8c63cec33c91fe3379ff1a5c01d33116fd
Antoine Guenet <age@odoo.com>
2022-02-25 09:52:16 +0000

    [FIX] web_editor: apply format, font-size and color collapsed
    When the selection is collapsed it was impossible to set a format (bold,
    italic, underline, strikethrough), a font-size or a color. This makes it
    possible by inserting and selecting a zero-width space first, then
    placing the caret to its left when we're done.
    
    task-2778416
    
    X-original-commit: b4eb0fe69bce5ca18d8450cd7118740f1d31f780
    Part-of: odoo/odoo#86930

--- addons/web_editor/static/lib/odoo-editor/test/editor-test.js
+++ addons/web_editor/static/lib/odoo-editor/test/editor-test.js
@@ -1,9 +1,11 @@
 import './spec/utils.test.js';
 import './spec/align.test.js';
+import './spec/color.test.js';
 import './spec/editor.test.js';
 import './spec/list.test.js';
 import './spec/link.test.js';
 import './spec/fontSize.test.js';
+import './spec/format.test.js';
 import './spec/insertHTML.test.js';
 import './spec/fontAwesome.test.js';
 import './spec/autostep.test.js';
