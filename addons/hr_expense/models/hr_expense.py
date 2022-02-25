8cb1d853051b0909f8f7de4136810b42e0f7a4b2
Jinjiu Liu <jili@odoo.com>
2022-02-25 11:46:10 +0000

    [FIX] hr_expense: changed taxes computation logic
    Reproduction:
    1. Install Expense, Accounting, then create a new expense
    2. Choose category ”Food & beverages”, enter the amount paid
    3. Choose a tax in Taxes
    4. The amount paid is recomputed to 0
    
    Reason: the expense form view changed in V15 but still uses the same
    computation logic in V14. In V15, parameter product_has_cost=False is a
    new use case where we directly input the total amount, and taxes here
    are already included in the price.
    
    Fix: in _compute_amount function, we need to check if there is
    unit_amount. If yes, we follow the computation flow which is similar to
    V14. If not, we calculate the taxes and other variables using the
    total_amount. But the taxes here must be price_include=True. Otherwise,
    the calculation will be wrong. To remind the customer, a tooltip and
    default text are added to tax_ids field. The domain of tax_ids will only
    include taxes with price_include=True. The translation file and the test
    are updated accordingly
    
    opw-2747288
    
    X-original-commit: 7cdaf331934fed6516c8405145b76a61eece254b

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -72,7 +72,8 @@ class HrExpense(models.Model):
     quantity = fields.Float(required=True, readonly=True, states={'draft': [('readonly', False)], 'reported': [('readonly', False)], 'approved': [('readonly', False)], 'refused': [('readonly', False)]}, digits='Product Unit of Measure', default=1)
     tax_ids = fields.Many2many('account.tax', 'expense_tax', 'expense_id', 'tax_id',
         compute='_compute_from_product_id_company_id', store=True, readonly=False,
-        domain="[('company_id', '=', company_id), ('type_tax_use', '=', 'purchase')]", string='Taxes')
+        domain="[('company_id', '=', company_id), ('type_tax_use', '=', 'purchase'), ('price_include', '=', True)]", string='Taxes',
+        help="The taxes should be \"Included In Price\"")
     amount_residual = fields.Monetary(string='Amount Due', compute='_compute_amount_residual')
     total_amount = fields.Monetary("Total In Currency", compute='_compute_amount', store=True, currency_field='currency_id', tracking=True, readonly=False)
     company_currency_id = fields.Many2one('res.currency', string="Report Company Currency", related='company_id.currency_id', readonly=True)
@@ -151,8 +152,9 @@ class HrExpense(models.Model):
     @api.depends('quantity', 'unit_amount', 'tax_ids', 'currency_id')
     def _compute_amount(self):
         for expense in self:
-            taxes = expense.tax_ids.compute_all(expense.unit_amount, expense.currency_id, expense.quantity, expense.product_id, expense.employee_id.user_id.partner_id)
-            expense.total_amount = taxes.get('total_included')
+            if expense.unit_amount:
+                taxes = expense.tax_ids.compute_all(expense.unit_amount, expense.currency_id, expense.quantity, expense.product_id, expense.employee_id.user_id.partner_id)
+                expense.total_amount = taxes.get('total_included')
 
     @api.depends("sheet_id.account_move_id.line_ids")
     def _compute_amount_residual(self):
