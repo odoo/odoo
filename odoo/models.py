b98c13fdbf9f90171446f95b6c32f80aad4a30ec
Raphael Collet <rco@odoo.com>
2022-02-28 15:27:24 +0100

    [FIX] core: check rules before flush() in unlink()
    Just before deleting records, we mark fields on dependent records as "to
    compute", and their actual computation should happen after the records
    have been deleted.  The problem is that checking security rules may
    trigger the computations that have been prepared.  So we should check
    security rules before marking dependent records.

--- odoo/models.py
+++ odoo/models.py
@@ -3369,6 +3369,7 @@ Fields:
             return True
 
         self.check_access_rights('unlink')
+        self.check_access_rule('unlink')
         self._check_concurrency()
 
         # mark fields that depend on 'self' to recompute them after 'self' has
@@ -3377,8 +3378,6 @@ Fields:
         self.modified(self._fields, before=True)
 
         with self.env.norecompute():
-            self.check_access_rule('unlink')
-
             cr = self._cr
             Data = self.env['ir.model.data'].sudo().with_context({})
             Defaults = self.env['ir.default'].sudo()
