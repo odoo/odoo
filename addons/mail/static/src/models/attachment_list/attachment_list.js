boan-odoo/odoo-1

Make message composition anware of the composer

addons/mail/static/src/models/attachment_list/attachment_list.js
@@ -49,7 +49,7 @@ registerModel({
                 return replace(this.attachmentBoxViewOwner.chatter.thread.allAttachments);
             }
             if (this.composerView && this.composerView.composer) {
-                return replace(this.composerView.composer.messageComposition.attachments);
+                return replace(this.composerView.composer.attachments);
             }
             return clear();
         },

odoo/odoo

addons/mail/static/src/models/attachment_list/attachment_list.js
@@ -49,7 +49,7 @@ registerModel({
                 return replace(this.attachmentBoxViewOwner.chatter.thread.allAttachments);
             }
             if (this.composerViewOwner && this.composerViewOwner.composer) {
-                return replace(this.composerViewOwner.composer.messageComposition.attachments);
+                return replace(this.composerViewOwner.composer.attachments);
             }
             return clear();
         },