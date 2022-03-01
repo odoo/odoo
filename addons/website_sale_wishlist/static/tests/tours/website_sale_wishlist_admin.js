183a966ab5b1e646b04a8b098d02683271580839
Florian Damhaut <flda@odoo.com>
2022-03-01 14:04:41 +0000

    [FIX] website_sale_wishlist: correct variant selection
    Step to reproduce:
    - Create variants for product without grid
    - Go to website
    - Try to add variant of created product to wishlist
    
    Current behaviour:
    - Product ID is not properly fetched
    - Product template is added to wishlist
    
    Behaviour after PR:
    - Product ID is properly fetched
    - Product is added to wishlist
    
    opw-2705883
    
    closes odoo/odoo#96274
    
    X-original-commit: f7ce60511c64002b84d3b4dbd66a2f52becc46fc
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

---
+++ addons/website_sale_wishlist/static/tests/tours/website_sale_wishlist_admin.js
@@ -0,0 +1,86 @@
+odoo.define('website_sale_wishlist_admin.tour', function (require) {
+'use strict';
+
+const wTourUtils = require("website.tour_utils");
+
+wTourUtils.registerEditionTour('shop_wishlist_admin', {
+    url: '/shop?search=Rock',
+    test: true,
+},
+    [
+        {
+            content: "Go to Rock shop page",
+            trigger: 'iframe a:contains("Rock"):first',
+        },
+        {
+            content: "check list view of variants is disabled initially (when on /product page)",
+            trigger: 'iframe body:not(:has(.js_product_change))',
+            extra_trigger: 'iframe #product_details',
+        },
+        wTourUtils.clickOnEdit(),
+        {
+            content: "open customize tab",
+            extra_trigger: '#oe_snippets.o_loaded',
+            trigger: '.o_we_customize_snippet_btn',
+        },
+        {
+            content: "open 'Variants' selector",
+            extra_trigger: '#oe_snippets .o_we_customize_panel',
+            trigger: '[data-name="variants_opt"] we-toggler',
+        },
+        {
+            content: "click on 'List View of Variants'",
+            trigger: 'we-button[data-name="variants_products_list_opt"]',
+        },
+        ...wTourUtils.clickOnSave(),
+        {
+            content: "check page loaded after list of variant customization enabled",
+            trigger: 'iframe .js_product_change',
+        },
+        {
+            content: "Add red product in wishlist",
+            trigger: 'iframe #product_detail .o_add_wishlist_dyn:not(".disabled")',
+        },
+        {
+            content: "Check that wishlist contains 1 items",
+            trigger: 'iframe .my_wish_quantity:contains(1)',
+            run: function () {
+                window.location.href = '/@/shop/wishlist';
+            }
+        },
+        {
+            content: "Check wishlist contains first variant",
+            trigger: 'iframe #o_comparelist_table tr:contains("red")',
+            run: function () {
+                window.location.href = '/@/shop?search=Rock';
+            }
+        },
+        {
+            content: "Go to Rock shop page",
+            trigger: 'iframe a:contains("Rock"):first',
+        },
+        {
+            content: "Switch to black Rock",
+            trigger: 'iframe .js_product span:contains("black")',
+        },
+        {
+            content: "Add black rock to wishlist",
+            trigger: 'iframe #product_detail .o_add_wishlist_dyn:not(".disabled")',
+        },
+        {
+            content: "Check that black product was added",
+            trigger: 'iframe .my_wish_quantity:contains(2)',
+            run: function () {
+                window.location.href = '/@/shop/wishlist';
+            }
+        },
+        {
+            content: "Check wishlist contains both variants",
+            extra_trigger: 'iframe #o_comparelist_table tr:contains("red")',
+            trigger: 'iframe #o_comparelist_table tr:contains("black")',
+            run: function () {}, // This is a check
+        },
+    ]
+);
+
+});
