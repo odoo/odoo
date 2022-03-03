c66da1c5298486d9621cd9e5e989a8a1c3ba9363
Antoine Guenet <age@odoo.com>
2022-03-03 17:02:23 +0000

    [FIX] mass_mailing: target existing paragraph in tour
    After choosing a template, the mass_mailing tour is supposed to tell us
    to click in a paragraph to edit it. It was targeting a snippet that
    doesn't exist anymore so the class name had to be adapted for it to
    work again.
    
    task-2778418
    
    closes odoo/odoo#85934
    
    X-original-commit: 2e2004b7cda8ea4dadb8de06991b7a2c91992661
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Antoine Guenet <age@odoo.com>

--- addons/mass_mailing/static/src/js/tours/mass_mailing_tour.js
+++ addons/mass_mailing/static/src/js/tours/mass_mailing_tour.js
@@ -51,7 +51,7 @@ odoo.define('mass_mailing.mass_mailing_tour', function (require) {
         edition: 'community',
         run: 'click',
     }, {
-        trigger: 'div[name="body_arch"] iframe div.o_mail_block_text',
+        trigger: 'div[name="body_arch"] iframe div.s_text_block',
         content: _t('Click on this paragraph to edit it.'),
         position: 'top',
         edition: 'enterprise',
