f212a967d345e5b439e36483d818502952ecdb86
Thibault Francois <tfr@odoo.com>
2022-03-04 11:01:30 +0000

    [ADD] cloc: count loc in style file and from imported module
    Context
    -------
    
    Web design specific require a lot of work and need maintenance.
    So far the style files were not taken into account despite the
    amount of maintenance they require.
    
    With this commit they are going to be counted,
    knowing they still can be excluded in the manifest.
    
    Imported module allow to deploy frontend assets: stylesheet,
    javascript, xml template and Qweb view that require
    as well some maintenance. They are going to be counted
    
    Implementation
    --------------
    
    - Add method to parse css and scss file
    - Include .scss and .css file in the count
    
    - Add external_id to attachment that store
      frontend asset of imported module
    - Find all attachment with .js, .css, .scss, .xml
      from imported module
    - Find qweb view from imported module
    - cound the content of the attachment and the qweb view
    
    closes odoo/odoo#86816
    
    X-original-commit: d2e0b48271dbd8cd7bbdbc5c2c1649c55c51417b
    Signed-off-by: Christophe Simonis <chs@odoo.com>
    Signed-off-by: Thibault Francois <tfr@odoo.com>

--- addons/base_import_module/models/ir_module.py
+++ addons/base_import_module/models/ir_module.py
@@ -104,7 +104,13 @@ class IrModule(models.Model):
                     if attachment:
                         attachment.write(values)
                     else:
-                        IrAttachment.create(values)
+                        attachment = IrAttachment.create(values)
+                        self.env['ir.model.data'].create({
+                            'name': f"attachment_{url_path}".replace('.', '_'),
+                            'model': 'ir.attachment',
+                            'module': module,
+                            'res_id': attachment.id,
+                        })
 
         IrAsset = self.env['ir.asset']
         assets_vals = []
@@ -139,7 +145,7 @@ class IrModule(models.Model):
         # Create new assets and attach 'ir.model.data' records to them
         created_assets = IrAsset.create(assets_to_create)
         self.env['ir.model.data'].create([{
-            'name': f"{asset['bundle']}.{asset['path']}",
+            'name': f"{asset['bundle']}_{asset['path']}".replace(".", "_"),
             'model': 'ir.asset',
             'module': module,
             'res_id': asset.id,
