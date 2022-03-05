f2244ae92f8bb59b134b75f8b193d8c2edad130c
Aktiv Software <odoo@aktivsoftware.com>
2022-03-05 08:53:37 +0000

    [FIX] web: action service: handle target "main"
    The target "main" feature has been lost during the conversion of
    the ActionManager into the action service. An action with target
    "main" should always clear the breadcrumbs. This commit
    re-introduces the feature.
    
    Fixes #83865
    
    closes odoo/odoo#87593
    
    X-original-commit: 78ec7281ed550eb24629c1b81445b753314d292f
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Co-authored-by: "Aaron Bohy <aab@odoo.com>"

--- addons/web/static/src/webclient/actions/action_service.js
+++ addons/web/static/src/webclient/actions/action_service.js
@@ -23,7 +23,7 @@ const viewRegistry = registry.category("views");
 
 /** @typedef {number|false} ActionId */
 /** @typedef {Object} ActionDescription */
-/** @typedef {"current" | "fullscreen" | "new" | "self" | "inline"} ActionMode */
+/** @typedef {"current" | "fullscreen" | "new" | "main" | "self" | "inline"} ActionMode */
 /** @typedef {string} ActionTag */
 /** @typedef {string} ActionXMLId */
 /** @typedef {Object} Context */
@@ -1094,6 +1094,7 @@ function makeActionManager(env) {
         const actionProm = _loadAction(actionRequest, options.additionalContext);
         let action = await keepLast.add(actionProm);
         action = _preprocessAction(action, options.additionalContext);
+        options.clearBreadcrumbs = action.target === "main" || options.clearBreadcrumbs;
         switch (action.type) {
             case "ir.actions.act_url":
                 return _executeActURLAction(action, options);
