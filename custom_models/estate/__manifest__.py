9d9957e9e4744b8ed16c1f1c87d864df2d582311
DiamondDagger590 <diamonddagger590@gmail.com>
2022-03-06 15:50:01 -0600

    Finish chapter 6

--- custom_models/estate/__manifest__.py
+++ custom_models/estate/__manifest__.py
@@ -4,8 +4,9 @@
         'base',
     ],
     'data': [
-      'data/ir.model.access.csv',
-      'views/estate_property_views.xml',
+        'data/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True,
--- custom_models/estate/models/estate_property.py
+++ custom_models/estate/models/estate_property.py
@@ -1,3 +1,5 @@
+from dateutil.relativedelta import relativedelta
+
 from odoo import fields, models
 
 
@@ -8,10 +10,12 @@ class EstateProperty(models.Model):
     name = fields.Char(string='Estate Name', required=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Data Available')
+    date_availability = fields.Date(string='Data Available',
+                                    default=lambda self: fields.Date.today() + relativedelta(months=+3),
+                                    copy=False)
     expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Bedroom Count')
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer(string='Bedroom Count', default=3)
     living_area = fields.Integer(string='Living Area')
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Has Garage')
@@ -22,3 +26,8 @@ class EstateProperty(models.Model):
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help='Used to select the orientation of the garden.'
     )
+    active = fields.Boolean(string="Active", default=True)
+    state = fields.Selection(string='State', selection=[('new', 'New'), ('offer_received', 'Offer Received'),
+                                                        ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'),
+                                                        ('canceled', 'Canceled')], copy=False, required=True,
+                             default='new')
