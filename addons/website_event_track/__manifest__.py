573714b48fea88fa36cf1b258695dae6dc58364e
Benjamin Vray <bvr@odoo.com>
2022-03-07 12:50:49 +0100

    [IMP] website_event, *: adapt customize show options in event pages *: website_event_exhibitor, website_event_track
    This commit adapts "customize show" options from website event modules
    by creating new options available in edit mode.
    
    task-2710582

--- addons/website_event_track/__manifest__.py
+++ addons/website_event_track/__manifest__.py
@@ -33,6 +33,7 @@
         'views/res_config_settings_view.xml',
         'views/website_visitor_views.xml',
         'views/event_menus.xml',
+        'views/snippets.xml',
     ],
     'demo': [
         'data/event_demo.xml',
