boan-odoo/odoo-1

[IMP] project,hr,sale(_timesheet): generic improvement for project
The purpose of this commit, is to make generic improvements in
project.

In this commit, following changes are made:

- add allocated hours field in project form and list view
- remaining hour widget in project kanban view displays allocated
  hours (-) timesheeted hours on task
- hide progress bar in project list view if planned hours is zero
- hide task stages if user is set
- move company field below tags	field
- project list view: rename 'Activities' to 'Next Activity'

task-2692856
Co-authored-by: Krina Oza <kro@odoo.com>

addons/hr_timesheet/models/project.py
@@ -46,9 +46,8 @@ class Project(models.Model):
     encode_uom_in_days = fields.Boolean(compute='_compute_encode_uom_in_days')
     is_internal_project = fields.Boolean(compute='_compute_is_internal_project', search='_search_is_internal_project')
     remaining_hours = fields.Float(compute='_compute_remaining_hours', string='Remaining Invoiced Time', compute_sudo=True)
-    has_planned_hours_tasks = fields.Boolean(compute='_compute_remaining_hours', compute_sudo=True,
-        help="True if any of the project's task has a set planned hours")
     is_project_overtime = fields.Boolean('Project in Overtime', compute='_compute_remaining_hours', search='_search_is_project_overtime', compute_sudo=True)
+    allocated_hours = fields.Float(string='Allocated Hours')
 
     def _compute_encode_uom_in_days(self):
         self.encode_uom_in_days = self.env.company.timesheet_encode_uom_id == self.env.ref('uom.product_uom_day')
@@ -81,18 +80,24 @@ class Project(models.Model):
             operator_new = 'not inselect'
         return [('id', operator_new, (query, ()))]
 
-    @api.depends('allow_timesheets', 'task_ids.planned_hours', 'task_ids.remaining_hours')
+    @api.model
+    def _fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        result = super()._fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        if view_type in ['tree', 'form'] and self.env.company.timesheet_encode_uom_id == self.env.ref('uom.product_uom_day'):
+            result['arch'] = self.env['account.analytic.line']._apply_time_label(result['arch'], related_model=self._name)
+        return result
+
+    @api.depends('allow_timesheets', 'timesheet_ids')
     def _compute_remaining_hours(self):
-        group_read = self.env['project.task'].read_group(
-            domain=[('planned_hours', '!=', False), ('project_id', 'in', self.filtered('allow_timesheets').ids),
-                     '|', ('stage_id.fold', '=', False), ('stage_id', '=', False)],
-            fields=['planned_hours:sum', 'remaining_hours:sum'], groupby='project_id')
-        group_per_project_id = {group['project_id'][0]: group for group in group_read}
+        timesheets_read_group = self.env['account.analytic.line'].read_group(
+            [('project_id', 'in', self.ids)],
+            ['project_id', 'unit_amount'],
+            ['project_id'],
+            lazy=False)
+        timesheet_time_dict = {res['project_id'][0]: res['unit_amount'] for res in timesheets_read_group}
         for project in self:
-            group = group_per_project_id.get(project.id, {})
-            project.remaining_hours = group.get('remaining_hours', 0)
-            project.has_planned_hours_tasks = bool(group.get('planned_hours', False))
-            project.is_project_overtime = group.get('remaining_hours', 0) < 0
+            project.remaining_hours = project.allocated_hours - timesheet_time_dict.get(project.id, 0)
+            project.is_project_overtime = project.remaining_hours < 0
 
     @api.model
     def _search_is_project_overtime(self, operator, value):

addons/sale_timesheet/models/project.py
@@ -55,6 +55,7 @@ class Project(models.Model):
     warning_employee_rate = fields.Boolean(compute='_compute_warning_employee_rate')
     partner_id = fields.Many2one(
         compute='_compute_partner_id', store=True, readonly=False)
+    allocated_hours = fields.Float(compute='_compute_allocated_hours', store=True, readonly=False, copy=False)
 
     @api.depends('sale_line_id', 'sale_line_employee_ids', 'allow_billable')
     def _compute_pricing_type(self):
@@ -174,6 +175,45 @@ class Project(models.Model):
                 ('remaining_hours', '>', 0)
             ], limit=1)
             project.sale_line_id = sol or project.sale_line_employee_ids.sale_line_id[:1]  # get the first SOL containing in the employee mappings if no sol found in the search
+
+    @api.depends('sale_line_id.product_uom_qty', 'sale_line_id.product_uom')
+    def _compute_allocated_hours(self):
+        sale_orders = self.sale_order_id | self.tasks.sale_order_id
+        sols_read_group = self.env['sale.order.line'].read_group([
+            ('order_id', 'in', sale_orders.ids), ('is_service', '=', True),
+            ('is_downpayment', '=', False),
+            ('product_id.service_tracking', 'in', ['task_in_project', 'project_only'])],
+            ['project_id', 'product_uom_qty', 'product_uom'],
+            ['project_id', 'product_uom'],
+            lazy=False)
+
+        uom_hour = self.env.ref('uom.product_uom_hour')
+        sol_qty_dict = defaultdict(list)
+        uom_ids = set(self.timesheet_encode_uom_id.ids)
+
+        for result in sols_read_group:
+            uom_id = result['product_uom'] and result['product_uom'][0]
+            if uom_id:
+                uom_ids.add(uom_id)
+            if result.get('project_id'):
+                sol_qty_dict[result['project_id'][0]].append((uom_id, result['product_uom_qty']))
+
+        uoms_dict = {uom.id: uom for uom in self.env['uom.uom'].browse(uom_ids)}
+
+        for project in self:
+            if not project.sale_line_id:
+                project.allocated_hours = 0
+                continue
+            # sale order line may be stored in a different unit of measure, so first
+            # we convert all of them to the reference unit
+            # if the sol has no product_uom_id then we take the one of the project
+            allocated_hours = sum([
+                product_uom_qty * uoms_dict.get(product_uom, project.timesheet_encode_uom_id.id).factor_inv
+                for product_uom, product_uom_qty in sol_qty_dict[project.id]
+            ], 0.0)
+            # Now convert to the unit of measure to hours
+            allocated_hours *= uom_hour.factor
+            project.allocated_hours = allocated_hours
 
     def _get_all_sales_orders(self):
         if self.allow_billable:

odoo/odoo

addons/hr_timesheet/models/project.py
@@ -46,9 +46,8 @@ class Project(models.Model):
     encode_uom_in_days = fields.Boolean(compute='_compute_encode_uom_in_days')
     is_internal_project = fields.Boolean(compute='_compute_is_internal_project', search='_search_is_internal_project')
     remaining_hours = fields.Float(compute='_compute_remaining_hours', string='Remaining Invoiced Time', compute_sudo=True)
-    has_planned_hours_tasks = fields.Boolean(compute='_compute_remaining_hours', compute_sudo=True,
-        help="True if any of the project's task has a set planned hours")
     is_project_overtime = fields.Boolean('Project in Overtime', compute='_compute_remaining_hours', search='_search_is_project_overtime', compute_sudo=True)
+    allocated_hours = fields.Float(string='Allocated Hours')
 
     def _compute_encode_uom_in_days(self):
         self.encode_uom_in_days = self.env.company.timesheet_encode_uom_id == self.env.ref('uom.product_uom_day')
@@ -81,18 +80,24 @@ class Project(models.Model):
             operator_new = 'not inselect'
         return [('id', operator_new, (query, ()))]
 
-    @api.depends('allow_timesheets', 'task_ids.planned_hours', 'task_ids.remaining_hours')
+    @api.model
+    def _fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        result = super()._fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        if view_type in ['tree', 'form'] and self.env.company.timesheet_encode_uom_id == self.env.ref('uom.product_uom_day'):
+            result['arch'] = self.env['account.analytic.line']._apply_time_label(result['arch'], related_model=self._name)
+        return result
+
+    @api.depends('allow_timesheets', 'timesheet_ids')
     def _compute_remaining_hours(self):
-        group_read = self.env['project.task']._read_group(
-            domain=[('planned_hours', '!=', False), ('project_id', 'in', self.filtered('allow_timesheets').ids),
-                     '|', ('stage_id.fold', '=', False), ('stage_id', '=', False)],
-            fields=['planned_hours:sum', 'remaining_hours:sum'], groupby='project_id')
-        group_per_project_id = {group['project_id'][0]: group for group in group_read}
+        timesheets_read_group = self.env['account.analytic.line']._read_group(
+            [('project_id', 'in', self.ids)],
+            ['project_id', 'unit_amount'],
+            ['project_id'],
+            lazy=False)
+        timesheet_time_dict = {res['project_id'][0]: res['unit_amount'] for res in timesheets_read_group}
         for project in self:
-            group = group_per_project_id.get(project.id, {})
-            project.remaining_hours = group.get('remaining_hours', 0)
-            project.has_planned_hours_tasks = bool(group.get('planned_hours', False))
-            project.is_project_overtime = group.get('remaining_hours', 0) < 0
+            project.remaining_hours = project.allocated_hours - timesheet_time_dict.get(project.id, 0)
+            project.is_project_overtime = project.remaining_hours < 0
 
     @api.model
     def _search_is_project_overtime(self, operator, value):

addons/sale_timesheet/models/project.py
@@ -54,6 +54,7 @@ class Project(models.Model):
     warning_employee_rate = fields.Boolean(compute='_compute_warning_employee_rate')
     partner_id = fields.Many2one(
         compute='_compute_partner_id', store=True, readonly=False)
+    allocated_hours = fields.Float(compute='_compute_allocated_hours', store=True, readonly=False, copy=False)
 
     @api.depends('sale_line_id', 'sale_line_employee_ids', 'allow_billable')
     def _compute_pricing_type(self):
@@ -173,6 +174,45 @@ class Project(models.Model):
                 ('remaining_hours', '>', 0)
             ], limit=1)
             project.sale_line_id = sol or project.sale_line_employee_ids.sale_line_id[:1]  # get the first SOL containing in the employee mappings if no sol found in the search
+
+    @api.depends('sale_line_id.product_uom_qty', 'sale_line_id.product_uom')
+    def _compute_allocated_hours(self):
+        sol_ids = self._fetch_sale_order_item_ids()
+        sols_read_group = self.env['sale.order.line']._read_group([
+            ('id', 'in', sol_ids), ('is_service', '=', True),
+            ('is_downpayment', '=', False),
+            ('product_id.service_tracking', 'in', ['task_in_project', 'project_only'])],
+            ['project_id', 'product_uom_qty', 'product_uom'],
+            ['project_id', 'product_uom'],
+            lazy=False)
+
+        uom_hour = self.env.ref('uom.product_uom_hour')
+        sol_qty_dict = defaultdict(list)
+        uom_ids = set(self.timesheet_encode_uom_id.ids)
+
+        for result in sols_read_group:
+            uom_id = result['product_uom'] and result['product_uom'][0]
+            if uom_id:
+                uom_ids.add(uom_id)
+            if result.get('project_id'):
+                sol_qty_dict[result['project_id'][0]].append((uom_id, result['product_uom_qty']))
+
+        uoms_dict = {uom.id: uom for uom in self.env['uom.uom'].browse(uom_ids)}
+
+        for project in self:
+            if not project.sale_line_id:
+                project.allocated_hours = 0
+                continue
+            # sale order line may be stored in a different unit of measure, so first
+            # we convert all of them to the reference unit
+            # if the sol has no product_uom_id then we take the one of the project
+            allocated_hours = sum([
+                product_uom_qty * uoms_dict.get(product_uom, project.timesheet_encode_uom_id.id).factor_inv
+                for product_uom, product_uom_qty in sol_qty_dict[project.id]
+            ], 0.0)
+            # Now convert to the unit of measure to hours
+            allocated_hours *= uom_hour.factor
+            project.allocated_hours = allocated_hours
 
     @api.depends('sale_line_employee_ids.sale_line_id', 'allow_billable')
     def _compute_sale_order_count(self):