bfeca637c8a52d9653cfdfe38fa4243b64aac7f4
Michael (mcm) <mcm@odoo.com>
2022-03-07 11:56:31 +0100

    make badge field tests pass

--- addons/web/static/tests/fields/badge_field_tests.js
+++ addons/web/static/tests/fields/badge_field_tests.js
@@ -1,8 +1,10 @@
 /** @odoo-module **/
 
+import { getFixture } from "../helpers/utils";
 import { makeView, setupViewRegistries } from "../views/helpers";
 
 let serverData;
+let target;
 
 QUnit.module("Fields", (hooks) => {
     hooks.beforeEach(() => {
@@ -10,7 +12,7 @@ QUnit.module("Fields", (hooks) => {
             models: {
                 partner: {
                     fields: {
-                        char_field: {
+                        display_name: {
                             string: "Char Field",
                             type: "char",
                             default: "Default char value",
@@ -37,24 +39,24 @@ QUnit.module("Fields", (hooks) => {
                     records: [
                         {
                             id: 1,
-                            char_field: "first record",
+                            display_name: "first record",
                             many2one_field: 4,
                             selection_field: "blocked",
                         },
                         {
                             id: 2,
-                            char_field: "second record",
+                            display_name: "second record",
                             many2one_field: 1,
                             selection_field: "normal",
                         },
                         {
                             id: 3,
-                            char_field: "", // empty value
+                            display_name: "", // empty value
                             selection_field: "done",
                         },
                         {
                             id: 4,
-                            char_field: "fourth record",
+                            display_name: "fourth record",
                             selection_field: "done",
                         },
                     ],
@@ -63,6 +65,7 @@ QUnit.module("Fields", (hooks) => {
         };
 
         setupViewRegistries();
+        target = getFixture();
     });
 
     QUnit.module("BadgeField");
@@ -70,66 +73,66 @@ QUnit.module("Fields", (hooks) => {
     QUnit.test("BadgeField component on a char field in list view", async function (assert) {
         assert.expect(3);
 
-        const list = await makeView({
+        await makeView({
             type: "list",
             serverData,
             resModel: "partner",
-            arch: `<list><field name="char_field" widget="badge"/></list>`,
+            arch: `<list><field name="display_name" widget="badge"/></list>`,
         });
 
-        assert.containsOnce(list.el, '.o_field_badge[name="char_field"]:contains(first record)');
-        assert.containsOnce(list.el, '.o_field_badge[name="char_field"]:contains(second record)');
-        assert.containsOnce(list.el, '.o_field_badge[name="char_field"]:contains(fourth record)');
+        assert.containsOnce(target, '.o_field_badge[name="display_name"]:contains(first record)');
+        assert.containsOnce(target, '.o_field_badge[name="display_name"]:contains(second record)');
+        assert.containsOnce(target, '.o_field_badge[name="display_name"]:contains(fourth record)');
     });
 
     QUnit.test("BadgeField component on a selection field in list view", async function (assert) {
         assert.expect(3);
 
-        const list = await makeView({
+        await makeView({
             type: "list",
             serverData,
             resModel: "partner",
             arch: `<list><field name="selection_field" widget="badge"/></list>`,
         });
 
-        assert.containsOnce(list.el, '.o_field_badge[name="selection_field"]:contains(Blocked)');
-        assert.containsOnce(list.el, '.o_field_badge[name="selection_field"]:contains(Normal)');
-        assert.containsN(list.el, '.o_field_badge[name="selection_field"]:contains(Done)', 2);
+        assert.containsOnce(target, '.o_field_badge[name="selection_field"]:contains(Blocked)');
+        assert.containsOnce(target, '.o_field_badge[name="selection_field"]:contains(Normal)');
+        assert.containsN(target, '.o_field_badge[name="selection_field"]:contains(Done)', 2);
     });
 
-    QUnit.skipWOWL(
-        "BadgeField component on a many2one field in list view",
-        async function (assert) {
-            assert.expect(2);
+    QUnit.test("BadgeField component on a many2one field in list view", async function (assert) {
+        assert.expect(2);
 
-            const list = await makeView({
-                type: "list",
-                serverData,
-                resModel: "partner",
-                arch: `<list><field name="many2one_field" widget="badge"/></list>`,
-            });
+        await makeView({
+            type: "list",
+            serverData,
+            resModel: "partner",
+            arch: `<list><field name="many2one_field" widget="badge"/></list>`,
+        });
 
-            // assert.containsOnce(list.el, '.o_field_badge[name="many2one_field"]:contains(first record)');
-            // assert.containsOnce(list.el, '.o_field_badge[name="many2one_field"]:contains(aaa)');
-        }
-    );
+        assert.containsOnce(target, '.o_field_badge[name="many2one_field"]:contains(first record)');
+        assert.containsOnce(
+            target,
+            '.o_field_badge[name="many2one_field"]:contains(fourth record)'
+        );
+    });
 
-    QUnit.skipWOWL("BadgeField component with decoration-xxx attributes", async function (assert) {
+    QUnit.test("BadgeField component with decoration-xxx attributes", async function (assert) {
         assert.expect(3);
 
-        const list = await makeView({
+        await makeView({
             type: "list",
             serverData,
             resModel: "partner",
             arch: `
                 <list>
                     <field name="selection_field" widget="badge"/>
-                    <field name="char_field" widget="badge" decoration-danger="selection_field == 'done'" decoration-warning="selection_field == 'blocked'"/>
+                    <field name="display_name" widget="badge" decoration-danger="selection_field == 'done'" decoration-warning="selection_field == 'blocked'"/>
                 </list>`,
         });
 
-        assert.containsN(list.el, '.o_field_badge[name="char_field"]', 4);
-        assert.containsN(list.el, '.o_field_badge[name="char_field"].bg-danger-light', 2);
-        assert.containsN(list.el, '.o_field_badge[name="char_field"].bg-warning-light', 1);
+        assert.containsN(target, '.o_field_badge[name="display_name"]', 4);
+        assert.containsOnce(target, '.o_field_badge[name="display_name"] .bg-danger-light');
+        assert.containsOnce(target, '.o_field_badge[name="display_name"] .bg-warning-light');
     });
 });
