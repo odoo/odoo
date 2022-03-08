6aaec7e0de9dd9c61585a3cbe461632aa488add4
Florian Charlier <flch@odoo.com>
2022-03-08 10:24:18 +0100

    [FIX] link_tracker,mass_mailing_sms: fix shorten links
    Purpose
    Fix bugs in link replacement (html and text version)
    
    1/ The replacement of urls in text (not html) was buggy when
    * a string includes multiple urls, and
    * `base_url` is one of them and not the first
    * Also in SMS marketing when adding the sms `id`
    
    When it came to base_url's turn, the replace function for the content would
    replace the `base_url` part of a previously shortened url instead of the
    distinct `base_url` link.
    
    2/ Ampersand character
    A) The ampersand character prevented replacement of an url inside a `Markup`.
    Previous tests passed as the tracker was created but the url was not replaced.
    
    B) The ampersand character was not recognized as part of an url for simple
    strings.
    
    3/ Replacement of already short links
    A faulty logic made it possible to replace "/r/" urls when no "blacklist" was
    passed.
    
    Commit
    * Ensures that only the base_url link would be replaced instead in this case.
    * Adds support for urls with "&"
    * Does prevent shortening short urls
    * Improves link conversion performance (regex + avoid duplication when there
      are several occurrences of the same url)
    * Adds multiple unit tests
    
    Task-2783844
    
    X-original-commit: ef4756fd1b7640fae7cbc57c8e46203e8f12d925

--- odoo/tools/mail.py
+++ odoo/tools/mail.py
@@ -257,7 +257,7 @@ def html_sanitize(src, silent=True, sanitize_tags=True, sanitize_attributes=Fals
 # ----------------------------------------------------------
 
 URL_REGEX = r'(\bhref=[\'"](?!mailto:|tel:|sms:)([^\'"]+)[\'"])'
-TEXT_URL_REGEX = r'https?://[a-zA-Z0-9@:%._\+~#=/-]+(?:\?\S+)?'
+TEXT_URL_REGEX = r'https?://[\w@:%.+&~#=/-]+(?:\?\S+)?'
 # retrieve inner content of the link
 HTML_TAG_URL_REGEX = URL_REGEX + r'([^<>]*>([^<>]+)<\/)?'
 
