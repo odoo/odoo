c9beacb6ed2ac89f75471ace77b842a623daa7ab
Nicolas Bayet <nby@odoo.com>
2022-03-09 14:45:45 +0100

    [FIX] mass_mailing: remove overflow on mass mailing container
    The overflow: auto on the container in mass mailing generated a
    scrollbar whenever the drag and drop of snippets because
    of the drop zones.
    
    Task-2781002

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2414,6 +2414,9 @@ var SnippetsMenu = Widget.extend({
                     const prom = new Promise(resolve => dragAndDropResolve = () => resolve());
                     self._mutex.exec(() => prom);
 
+                    const doc = self.options.wysiwyg.odooEditor.document;
+                    $(doc.body).addClass('oe_dropzone_active');
+
                     self.options.wysiwyg.odooEditor.automaticStepUnactive();
 
                     self.$el.find('.oe_snippet_thumbnail').addClass('o_we_already_dragging');
@@ -2502,6 +2505,7 @@ var SnippetsMenu = Widget.extend({
                 },
                 stop: async function (ev, ui) {
                     const doc = self.options.wysiwyg.odooEditor.document;
+                    $(doc.body).removeClass('oe_dropzone_active');
                     self.options.wysiwyg.odooEditor.automaticStepUnactive();
                     self.options.wysiwyg.odooEditor.automaticStepSkipStack();
                     $toInsert.removeClass('oe_snippet_body');
