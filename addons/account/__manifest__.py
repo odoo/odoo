7aa82c5c5ee003d773f9dacf15bf22bdc49412e8
Nicolas (vin) <vin@odoo.com>
2022-03-09 11:10:06 +0000

    [FIX] account_report: fix update of account report
    see https://github.com/odoo/odoo/pull/77771
    https://github.com/odoo/enterprise/pull/23738
    
    These PR are updating a template and adding a new dependency in
    account_report. This has the side effect of making account report
    raise a traceback when invoicing has not been updated prior to that.
    
    With this change, we will bump the version of account and use it to
    make sure that it has been updated as we would expect prior to updating
    account_reports. If not, we will raise a user error with an explicit
    message asking the user to update invoicing.
    
    closes odoo/odoo#86217
    
    X-original-commit: 0ec9e16cae0cdaa99cfb181b40658e15e34e0b14
    Signed-off-by: Olivier Colson <oco@odoo.com>
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- addons/account/__manifest__.py
+++ addons/account/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name' : 'Invoicing',
-    'version' : '1.1',
+    'version' : '1.2',
     'summary': 'Invoices & Payments',
     'sequence': 10,
     'description': """
