88347badf41ac4b217b3522aef0e71bbf5003d13
Yolann Sabaux <yosa@odoo.com>
2022-03-09 09:01:56 +0000

    [FIX] web: allow user to scroll horizontally
    Steps to reproduce:
    - create a contact with a very long name
    - create a quotation
    - in customer, type the first letter of the very long customer's name
    - try to use the horizontal scroll bar
    
    Issue:
    close window on horizontal scroll
    
    Solution:
    Discriminate an horizontal scroll from a vertical scroll
    A test has been added as well as the triggerScroll utility function
    to allow proper tests when scrolling on elements
    
    opw-2777443
    
    closes odoo/odoo#86735
    
    X-original-commit: e0b26496196bd4ee39fa06e1a3274982d2c7fb12
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Co-authored-by: Iucapad  <luvi@odoo.com>

--- addons/web/static/tests/legacy/fields/relational_fields/field_many2one_tests.js
+++ addons/web/static/tests/legacy/fields/relational_fields/field_many2one_tests.js
@@ -10,7 +10,7 @@ var StandaloneFieldManagerMixin = require('web.StandaloneFieldManagerMixin');
 var testUtils = require('web.test_utils');
 var Widget = require('web.Widget');
 
-const { legacyExtraNextTick } = require("@web/../tests/helpers/utils");
+const { legacyExtraNextTick, triggerScroll } = require("@web/../tests/helpers/utils");
 const { createWebClient, doAction } = require('@web/../tests/webclient/helpers');
 const { browser } = require('@web/core/browser/browser');
 const { patchWithCleanup } = require('@web/../tests/helpers/utils');
@@ -3351,7 +3351,9 @@ QUnit.module('fields', {}, function () {
         });
 
         QUnit.test('many2one dropdown disappears on scroll', async function (assert) {
-            assert.expect(2);
+            assert.expect(4);
+
+            this.data.partner.records[0].display_name = "Veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeery Loooooooooooooooooooooooooooooooooooooooooooong Naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaame";
 
             var form = await createView({
                 View: FormView,
@@ -3369,12 +3371,17 @@ QUnit.module('fields', {}, function () {
             await testUtils.form.clickEdit(form);
 
             var $input = form.$('.o_field_many2one input');
+            var dropdown = document.querySelector(".dropdown-menu.ui-front");
 
             await testUtils.dom.click($input);
             assert.isVisible($input.autocomplete('widget'), "dropdown should be opened");
 
-            form.el.dispatchEvent(new Event('scroll'));
-            assert.isNotVisible($input.autocomplete('widget'), "dropdown should be closed");
+            await triggerScroll(dropdown, { left: 50 }, false);
+            assert.strictEqual(dropdown.scrollLeft, 50, "a scroll happened");
+            assert.isVisible($input.autocomplete('widget'), "dropdown stays open if the scroll is inside the dropdown");
+
+            await triggerScroll(window, { top: 50 });
+            assert.isNotVisible($input.autocomplete('widget'), "dropdown closes if the scroll is outside the dropdown");
 
             form.destroy();
         });
