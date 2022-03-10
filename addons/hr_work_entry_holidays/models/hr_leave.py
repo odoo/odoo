e9813340221b4f8ad3cfd6ed23eee752e61e5772
William Braeckman <wbr@odoo.com>
2022-03-10 09:17:00 +0000

    [FIX] hr_holidays: fix multi company issue in days calculation
    Prior to this commit, the multi company rules were bypassed due implicit
    sudos (compute_sudo and self.env.user).
    The function computing the number of days will now make sure to include
    the right domain.
    
    TaskId-2791030
    
    closes odoo/odoo#86361
    
    X-original-commit: dbcb37ec80bb0fef0115c879c15bdc7073894290
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- addons/hr_work_entry_holidays/models/hr_leave.py
+++ addons/hr_work_entry_holidays/models/hr_leave.py
@@ -203,7 +203,9 @@ class HrLeave(models.Model):
             contracts = employee.sudo()._get_contracts(date_from, date_to, states=['open'])
             contracts |= employee.sudo()._get_incoming_contracts(date_from, date_to)
             calendar = contracts[:1].resource_calendar_id if contracts else None # Note: if len(contracts)>1, the leave creation will crash because of unicity constaint
-            result = employee._get_work_days_data_batch(date_from, date_to, calendar=calendar)[employee.id]
+            # We force the company in the domain as we are more than likely in a compute_sudo
+            domain = [('company_id', 'in', self.env.company.ids + self.env.context.get('allowed_company_ids', []))]
+            result = employee._get_work_days_data_batch(date_from, date_to, calendar=calendar, domain=domain)[employee.id]
             if self.request_unit_half and result['hours'] > 0:
                 result['days'] = 0.5
             return result
