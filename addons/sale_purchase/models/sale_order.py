44284beec167cb0762420a456694ff5835b91356
Adrien Widart <awt@odoo.com>
2022-03-11 09:03:36 +0000

    [FIX] sale_purchase: adapt POL description
    When confirming a sale order, if a purchase order is generated, the
    descriptions of the PO's lines won't be adapted to the vendor
    
    To reproduce the issue:
    1. Create a vendor V
    2. Create a product P:
        - Type: Service
        - In Purchase, add a line L01:
            - Vendor: V
            - Vendor Product Name: Name01
            - Vendor Product Code: C01
        - Purchase Automatically: True
    3. Create and Confirm a SO with 1 x P
    4. Open the generated PO
    
    Error: The description is incorrect (it's the standard name of P instead
    of "[C01] Name01")
    
    OPW-2777702
    
    closes odoo/odoo#86256
    
    Signed-off-by: Tiffany Chang <tic@odoo.com>

--- addons/sale_purchase/models/sale_order.py
+++ addons/sale_purchase/models/sale_order.py
@@ -6,6 +6,7 @@ from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 from odoo.tools import float_compare
+from odoo.tools.misc import get_lang
 
 
 class SaleOrder(models.Model):
@@ -220,22 +221,25 @@ class SaleOrderLine(models.Model):
 
         # compute unit price
         price_unit = 0.0
-        if supplierinfo:
-            price_unit = self.env['account.tax'].sudo()._fix_tax_included_price_company(supplierinfo.price, self.product_id.supplier_taxes_id, taxes, self.company_id)
-            if purchase_order.currency_id and supplierinfo.currency_id != purchase_order.currency_id:
-                price_unit = supplierinfo.currency_id.compute(price_unit, purchase_order.currency_id)
 
         # purchase line description in supplier lang
         product_in_supplier_lang = self.product_id.with_context(
             lang=supplierinfo.name.lang,
-            partner_id=supplierinfo.name.id,
         )
-        name = '[%s] %s' % (self.product_id.default_code, product_in_supplier_lang.display_name)
+        if supplierinfo:
+            price_unit = self.env['account.tax'].sudo()._fix_tax_included_price_company(supplierinfo.price, self.product_id.supplier_taxes_id, taxes, self.company_id)
+            if purchase_order.currency_id and supplierinfo.currency_id != purchase_order.currency_id:
+                price_unit = supplierinfo.currency_id.compute(price_unit, purchase_order.currency_id)
+            product_in_supplier_lang = product_in_supplier_lang.with_context(seller_id=supplierinfo.id)
+        else:
+            product_in_supplier_lang = product_in_supplier_lang.with_context(partner_id=purchase_order.partner_id.id)
+
+        name = product_in_supplier_lang.display_name
         if product_in_supplier_lang.description_purchase:
             name += '\n' + product_in_supplier_lang.description_purchase
 
         return {
-            'name': '[%s] %s' % (self.product_id.default_code, self.name) if self.product_id.default_code else self.name,
+            'name': name,
             'product_qty': purchase_qty_uom,
             'product_id': self.product_id.id,
             'product_uom': self.product_id.uom_po_id.id,
