cc96b45b391a88c1f92275f34db9e7ed080f1c0d
Thibault Delavallée <tde@odoo.com>
2022-03-11 12:14:48 +0000

    [IMP] account, website_sale: improve docstrings / notes
    Purpose is to cleanup some remaining notes in code to better know some cleaning
    is long awaited, planned but still not in WIP. Brol is not a recognized word
    in the whole world. Also add some query counters explanation as counters are
    a bit different depending on accounting, community and enterprise runs.
    
    Part-of: odoo/odoo#86254

--- addons/website_sale/wizard/mail_compose_message.py
+++ addons/website_sale/wizard/mail_compose_message.py
@@ -9,7 +9,7 @@ class MailComposeMessage(models.TransientModel):
 
     def _action_send_mail(self, auto_commit=False):
         context = self._context
-        # TODO TDE: clean that brole one day
+        # Task-2792146: will move to model-based method
         if context.get('website_sale_send_recovery_email') and self.model == 'sale.order' and context.get('active_ids'):
             self.env['sale.order'].search([
                 ('id', 'in', context.get('active_ids')),
