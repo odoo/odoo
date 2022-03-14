925c34bdb5e0d8718e7351f6d65e566caf8f41ec
Goffin Simon <sig@odoo.com>
2022-03-14 11:34:01 +0100

    [FIX] sale: Wrong currency conversion with pricelist
    Steps to reproduce the issue:
    
    - Let's consider a company C in $ and the rate conversion from $ to € is 0.5
    - Let's consider a product P
    - Let's consider a pricelist PL1 in € such as P is set to 100€
    - Let's consider a pricelist PL2 in $ such as P id set to 100$
    - Create a sale order SO with PL1
    - Create a line with P
    
    Bug:
    
    The unit price of P was 200€ instead of 100€ because field currency_id was not set on the line
    when trying to pass it to function _get_tax_included_unit_price
    
    opw:2789324

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -1727,7 +1727,7 @@ class SaleOrderLine(models.Model):
                 'sale',
                 fiscal_position=self.order_id.fiscal_position_id,
                 product_price_unit=self._get_display_price(product),
-                product_currency=self.currency_id
+                product_currency=self.order_id.currency_id
             )
         self.update(vals)
 
@@ -1768,7 +1768,7 @@ class SaleOrderLine(models.Model):
                 'sale',
                 fiscal_position=self.order_id.fiscal_position_id,
                 product_price_unit=self._get_display_price(product),
-                product_currency=self.currency_id
+                product_currency=self.order_id.currency_id
             )
 
     def name_get(self):
