d62e7d9af4d9e298ca67e6e214180960b7cf05f9
Alexandre Kühn <aku@odoo.com>
2022-03-15 13:03:32 +0000

    [IMP] turn "Message/datetime" into field
    Task-2793280
    
    closes odoo/odoo#86448
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/components/message/message.js
+++ addons/mail/static/src/components/message/message.js
@@ -8,7 +8,6 @@ import { isEventHandled, markEventHandled } from '@mail/utils/utils';
 
 import { _lt } from 'web.core';
 import Popover from "web.Popover";
-import { getLangDatetimeFormat } from 'web.time';
 
 const { Component, onWillUnmount, useRef, useState } = owl;
 
@@ -85,15 +84,6 @@ export class Message extends Component {
     //--------------------------------------------------------------------------
     // Public
     //--------------------------------------------------------------------------
-
-    /**
-     * Get the date time of the message at current user locale time.
-     *
-     * @returns {string}
-     */
-    get datetime() {
-        return this.messageView.message.date.format(getLangDatetimeFormat());
-    }
 
     /**
      * Whether the message is "active", ie: hovered or clicked, and should
