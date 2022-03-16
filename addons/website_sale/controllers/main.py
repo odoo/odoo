1d8dc53f8ed2a967b35213bc913cdfbc42ff15bc
Valentin Chevalier <vcr@odoo.com>
2022-03-16 11:00:19 +0000

    [IMP] payment, *: hide tokenization checkbox when paying a subscription
    *: account_payment, sale, website_sale
    
    Users should not be able to discard the tokenization of their payment if
    they pay for a subscription.
    
    This commit introduces a new helper method to better predict when the
    tokenization checkbox should be shown or hidden. This also ensures that
    the behavior will be the same across all modules.
    
    task-2695201
    
    closes odoo/odoo#80046
    
    Related: odoo/enterprise#25335
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -989,12 +989,6 @@ class WebsiteSale(http.Controller):
                 order.amount_total, order.currency_id, order.partner_id.country_id
             ) for acq_sudo in acquirers_sudo.filtered('fees_active')
         }
-        # Prevent public partner from saving payment methods but force it for logged in partners
-        # buying subscription products
-        show_tokenize_input = logged_in \
-            and not request.env['payment.acquirer'].sudo()._is_tokenization_required(
-                sale_order_id=order.id
-            )
         return {
             'website_sale_order': order,
             'errors': [],
@@ -1005,7 +999,9 @@ class WebsiteSale(http.Controller):
             'acquirers': acquirers_sudo,
             'tokens': tokens,
             'fees_by_acquirer': fees_by_acquirer,
-            'show_tokenize_input': show_tokenize_input,
+            'show_tokenize_input': PaymentPortal._compute_show_tokenize_input_mapping(
+                acquirers_sudo, logged_in=logged_in, sale_order_id=order.id
+            ),
             'amount': order.amount_total,
             'currency': order.currency_id,
             'partner_id': order.partner_id.id,
