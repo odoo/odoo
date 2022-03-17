b06481dadfea12816db7a48d9c937e45bf2c955f
Anna <s775518@sbuniv.edu>
2022-03-16 19:43:01 -0500

    Added the "Mode of Therapy" dropdown. I made the default value 'in person'.

--- custom_models/mental_health/models/mental_health_notes.py
+++ custom_models/mental_health/models/mental_health_notes.py
@@ -1,13 +1,21 @@
 from odoo import fields, models
 
-
 class MentalHealthNotes(models.Model):
     _name = "mental_health.notes"
     _description = "A model to be used to write mental health notes"
 
     name = fields.Char(string='Client Name', required=True)
-    description = fields.Text(string='Meeting Notes', copy=False)
+    mode_of_therapy = fields.Selection(string='Mode of Therapy', required=True,
+                                       selection=[('in person', 'In Person'),
+                                                  ('by phone', 'By Phone'),
+                                                  ('video conference online', 'Video Conference Online'),
+                                                  ('email', 'Email'),
+                                                  ('text', 'Text')],
+                                       default='in person',
+                                       help='Select the mode of therapy.')
+
     date = fields.Date(string='Date Recorded', required=True, default=lambda self: fields.Date.today(), copy=False)
     icd10_codes = fields.Selection(string='ICD10 Code', required=False, copy=False,
                                    selection=[('f40.00', 'F40.00 - Agoraphobia unspecified'),
                                               ('f40.01', 'F40.01 - Agoraphobia with panic disorder')])
+    description = fields.Text(string='Meeting Notes', copy=False)
