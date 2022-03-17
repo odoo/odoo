15083ab299bf611490951813f08c2990f64df2dc
Hao-Ming <haomingkpis@gmail.com>
2022-03-17 16:12:57 -0500

    Finished: CONNECT THE CLIENT FIELDS TO THE CONTACT LIST!!!!!

--- custom_models/mental_health/models/mental_health_notes.py
+++ custom_models/mental_health/models/mental_health_notes.py
@@ -5,7 +5,7 @@ class MentalHealthNotes(models.Model):
     _name = "mental_health.notes"
     _description = "A model to be used to write mental health notes"
 
-    name = fields.Char(string='Client Name', required=True)
+    name = fields.Many2one(comodel_name='res.partner')
     description = fields.Text(string='Meeting Notes', copy=False)
     date = fields.Date(string='Date Recorded', required=True, default=lambda self: fields.Date.today(), copy=False)
     icd10_codes = fields.Selection(string='ICD10 Code', required=False, copy=False,
