55976fc5e2177a82a9dc4fc54d595f71399a041b
Anna <s775518@sbuniv.edu>
2022-03-17 12:54:09 -0500

    Added the "Service Provided" dropdown

--- custom_models/mental_health/models/mental_health_notes.py
+++ custom_models/mental_health/models/mental_health_notes.py
@@ -8,6 +8,11 @@ class MentalHealthNotes(models.Model):
     name = fields.Char(string='Client Name', required=True)
     description = fields.Text(string='Meeting Notes', copy=False)
     date = fields.Date(string='Date Recorded', required=True, default=lambda self: fields.Date.today(), copy=False)
+    service_type = fields.Selection(string='Service Provided', required=True,
+                                    selection=[('intake', 'Intake'),
+                                               ('individual therapy', 'Individual Therapy')],
+                                    default='individual therapy',
+                                    help='Select the type of service provided.')
     icd10_codes = fields.Selection(string='ICD10 Code', required=False, copy=False,
                                    selection=[('f40.00', 'F40.00 - Agoraphobia unspecified'),
                                               ('f40.01', 'F40.01 - Agoraphobia with panic disorder')])
