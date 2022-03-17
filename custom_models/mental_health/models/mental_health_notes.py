3841d524c865489de3d9f49f5c8521882ca44ba7
Anna <s775518@sbuniv.edu>
2022-03-16 20:28:09 -0500

    Added the "arrival_status" selection

--- custom_models/mental_health/models/mental_health_notes.py
+++ custom_models/mental_health/models/mental_health_notes.py
@@ -8,6 +8,13 @@ class MentalHealthNotes(models.Model):
     name = fields.Char(string='Client Name', required=True)
     description = fields.Text(string='Meeting Notes', copy=False)
     date = fields.Date(string='Date Recorded', required=True, default=lambda self: fields.Date.today(), copy=False)
+    arrival_status = fields.Selection(string='Arrival Status', required=True,
+                                    selection=[('on time', 'On Time'),
+                                               ('late', 'Late'),
+                                               ('no show', 'No Show'),
+                                               ('cancelled', 'Cancelled')],
+                                    default='on time',
+                                    help='Select the arrival status of the client.')
     icd10_codes = fields.Selection(string='ICD10 Code', required=False, copy=False,
                                    selection=[('f40.00', 'F40.00 - Agoraphobia unspecified'),
                                               ('f40.01', 'F40.01 - Agoraphobia with panic disorder')])
