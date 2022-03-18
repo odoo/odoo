1cd8a156e00ed43079aec8f83e6d4809eb9262c1
William Braeckman <wbr@odoo.com>
2022-03-18 16:01:45 +0100

    [FIX] hr_contract: fix traceback when searching on first_contract_date
    This commit fixes a traceback when filtering on `first_contract_date` on
    `hr.employee.public` (RecursionError).
    
    TaskId-2781421

--- addons/hr_contract/models/hr_employee_public.py
+++ addons/hr_contract/models/hr_employee_public.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class HrEmployeePublic(models.Model):
     _inherit = "hr.employee.public"
 
     first_contract_date = fields.Date(related='employee_id.first_contract_date', groups="base.group_user")
+
+    @api.model
+    def _setup_fields(self):
+        res = super()._setup_fields()
+        self._fields['first_contract_date'].search = '_search_first_contract_date'
+        return res
+
+    def _search_first_contract_date(self, operator, value):
+        return [('id', 'in', [res['id'] for res in self.env['hr.employee'].sudo().search_read([
+                ('first_contract_date', operator, value)
+            ])])]
