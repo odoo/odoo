143f632d96a9e974002ccac2f960404902a26b18
lejeune quentin <qle@odoo.com>
2022-03-18 10:47:54 +0000

    [ADD] pos_stripe: Add the possibility to pay with Stripe in the POS
    Add a stripe payment provider to the POS
    
    To be able to use this payment provider, you must have configured Stripe in Odoo
    
    closes odoo/odoo#92421
    
    Signed-off-by: Quentin Lejeune (qle) <qle@odoo.com>

--- addons/point_of_sale/models/res_config_settings.py
+++ addons/point_of_sale/models/res_config_settings.py
@@ -9,6 +9,7 @@ class ResConfigSettings(models.TransientModel):
     sale_tax_id = fields.Many2one('account.tax', string="Default Sale Tax", related='company_id.account_sale_tax_id', readonly=False)
     module_pos_mercury = fields.Boolean(string="Vantiv Payment Terminal", help="The transactions are processed by Vantiv. Set your Vantiv credentials on the related payment method.")
     module_pos_adyen = fields.Boolean(string="Adyen Payment Terminal", help="The transactions are processed by Adyen. Set your Adyen credentials on the related payment method.")
+    module_pos_stripe = fields.Boolean(string="Stripe Payment Terminal", help="The transactions are processed by Stripe. Set your Stripe credentials on the related payment method.")
     module_pos_six = fields.Boolean(string="Six Payment Terminal", help="The transactions are processed by Six. Set the IP address of the terminal on the related payment method.")
     update_stock_quantities = fields.Selection(related="company_id.point_of_sale_update_stock_quantities", readonly=False)
     account_default_pos_receivable_account_id = fields.Many2one(string='Default Account Receivable (PoS)', related='company_id.account_default_pos_receivable_account_id', readonly=False)
