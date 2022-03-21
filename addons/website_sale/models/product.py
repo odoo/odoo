c0a921c8f85567e67ffb858215586e79b415585c
Nasreddin Boulif (bon) <bon@odoo.com>
2022-03-21 10:17:16 +0000

    [FIX] website_sale: archive pricelist if website have another available
    Steps to reproduce:
    
      - Install eCommerce module
      - Select company A and set only one pricelist with:
        - company = company A
        - website = website of company A
      - Select company B and set multiple pricelist.
      - Switch to company B
      - Archive a pricelist
    
    Issue:
    
      Error: "With this action, "Website of company A" website would not
      have any pricelist available".
    
    Solution:
    
      Use sudo() when searching websites to don't use multi-company
      compliant ir.rule `product_pricelist_comp_rule` (in website_sale) when
      getting pricelist_ids (and therefore allow checking all pricelists for
      all websites in all companies).
    
    opw-2759069
    
    closes odoo/odoo#88453
    
    X-original-commit: 2e1c75998f41a4ede5489a7f1ef242668d31b2ba
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>
    Signed-off-by: Nasreddin Boulif (bon) <bon@odoo.com>
    Co-authored-by: Romain Derie <rde@odoo.com>

--- addons/website_sale/models/product.py
+++ addons/website_sale/models/product.py
@@ -86,7 +86,8 @@ class ProductPricelist(models.Model):
 
     def _check_website_pricelist(self):
         for website in self.env['website'].search([]):
-            if not website.pricelist_ids:
+            # sudo() to be able to read pricelists/website from another company
+            if not website.sudo().pricelist_ids:
                 raise UserError(_("With this action, '%s' website would not have any pricelist available.") % (website.name))
 
     def _is_available_on_website(self, website_id):
