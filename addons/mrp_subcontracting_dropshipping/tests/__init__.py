5e550c314a210a166605921cf9fae449f53d6072
Adrien Widart <awt@odoo.com>
2022-03-21 14:56:23 +0100

    [IMP] mrp_subcontracting_dropshipping: add test with cancelled picking
    Test 52d5ff23c4f16f4cc78c812d389f3475fb3d6a86
    
    X-original-commit: 5ecec072b479dce39c1372b645cb13da4a428747

--- addons/mrp_subcontracting_dropshipping/tests/__init__.py
+++ addons/mrp_subcontracting_dropshipping/tests/__init__.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-
 #  -*- coding: utf-8 -*-
 #  Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_purchase_subcontracting
-
+from . import test_sale_dropshipping
