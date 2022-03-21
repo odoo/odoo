320323503c99ca00c3c2b6e86de1d9d9f8d6d013
tsm-odoo <tsm@odoo.com>
2022-03-21 12:59:54 +0000

    [FIX] mail: fix mock server mail channel write
    Mock server's mail channel write method is incorrect. It assumes
    the id of the channel will always be 20 while it could be anything.
    
    closes odoo/odoo#86860
    
    X-original-commit: 96e732f6bbc7622d50c3a969f5c2bb44c85c2b48
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/tests/helpers/mock_server.js
+++ addons/mail/static/tests/helpers/mock_server.js
@@ -1215,7 +1215,7 @@ MockServer.include({
         this._widget.call('bus_service', 'trigger', 'notification', [{
             type: 'mail.channel/insert',
             payload: {
-                id: 20,
+                id,
                 avatarCacheKey: avatarCacheKey,
             },
         }]);
