c6dd7d6ce46736f1eea712d46f6da0d5fcc8b222
Patrick Hoste <pko@odoo.com>
2022-03-21 12:38:46 +0000

    [FW][FIX] (website_)event_booth_sale: correctly display tax included price
    PURPOSE
    
    Display the correct price when the tax included option is set in
    the website settings.
    
    LINKS
    
    Task-2657480
    closes: odoo/odoo#84261
    
    X-original-commit: 1bdbb1377f0092f8e29441049eaab43ae8eb9339
    Forward-port-of: #84261
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_event_booth_sale/tests/test_event_booth_sale.py
+++ addons/website_event_booth_sale/tests/test_event_booth_sale.py
@@ -13,11 +13,17 @@ class TestWebsiteEventBoothSale(HttpCase):
 
     def setUp(self):
         super().setUp()
+        self.env['ir.config_parameter'].sudo().set_param('account.show_line_subtotals_tax_selection', 'tax_included')
+        self.tax = self.env['account.tax'].sudo().create({
+            'name': 'Tax 10',
+            'amount': 10,
+        })
         self.booth_product = self.env['product.product'].create({
             'name': 'Test Booth Product',
             'description_sale': 'Mighty Booth Description',
             'list_price': 20,
             'standard_price': 60.0,
+            'taxes_id': [(6, 0, [self.tax.id])],
             'detailed_type': 'event_booth',
         })
         self.event_booth_category = self.env['event.booth.category'].create({
