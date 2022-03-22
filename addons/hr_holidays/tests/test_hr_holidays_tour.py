39260d28964dde7c78f0e7598fbf3d954cff6919
Leonardo Pavan Rocha <lpr@odoo.com>
2022-03-22 11:06:27 +0000

    [IMP] hr_holidays: add hr_holidays tour
    Adds a tour for showing the main process of requesting a time-off.
    
    task-2760450
    
    closes odoo/odoo#87108
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

---
+++ addons/hr_holidays/tests/test_hr_holidays_tour.py
@@ -0,0 +1,43 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.tests import HttpCase
+from odoo.tests.common import tagged
+
+from datetime import date
+
+
+@tagged('post_install', '-at_install')
+class TestHrHolidaysTour(HttpCase):
+    def test_hr_holidays_tour(self):
+        admin_user = self.env.ref('base.user_admin')
+        admin_employee = admin_user.employee_id
+        HRLeave = self.env['hr.leave']
+        date_from = date(2022, 1, 17)
+        date_to = date(2022, 1, 18)
+        leaves_on_freeze_date = HRLeave.search([
+            ('date_from', '>=', date_from),
+            ('date_to', "<=", date_to),
+            ('employee_id', '=', admin_employee.id)
+        ])
+        leaves_on_freeze_date.sudo().unlink()
+
+        LeaveType = self.env['hr.leave.type'].with_user(admin_user)
+
+        holidays_type_1 = LeaveType.create({
+            'name': 'NotLimitedHR',
+            'requires_allocation': 'no',
+            'leave_validation_type': 'hr',
+        })
+        # add allocation
+        allocation = self.env['hr.leave.allocation'].create({
+            'name': 'Expired Allocation',
+            'employee_id': admin_employee.id,
+            'holiday_status_id': holidays_type_1.id,
+            'number_of_days': 1,
+            'state': 'confirm',
+            'date_from': '2022-01-01',
+            'date_to': '2022-12-31',
+        })
+        allocation.action_validate()
+
+        self.start_tour('/web', 'hr_holidays_tour', login="admin")
