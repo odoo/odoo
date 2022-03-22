797c6b0e0899eb31129ab7ea5e15bdd046790f14
Samuel Degueldre <sad@odoo.com>
2022-03-22 09:19:47 +0000

    [FIX] web: fix tooling failing to run due to bash-specific syntax
    In #86163 we added a check on the branch name to avoid running the
    tooling on stable branches, however this check used bash-specific
    syntax. While the hashbang in the pre-commit hook specifies that the
    hook should be run using bash, we were using the npm module "husky" to
    manage git hooks, which would ignore this hashbang and always run the
    hook using sh, causing the hook to fail in all cases.
    
    After some consideration, we have decided to stop using husky, as its
    main purpose is to make hook management easier in npm-based projects.
    Since we already need a script to enable the tooling, we can do
    essentially the same thing that husky is doing but with more control
    over the process with no drawbacks.
    
    closes odoo/odoo#86992
    
    X-original-commit: 3104ab16ecf88d4f7b1618db4d62e7f57df34e6c
    Signed-off-by: Simon Genin (ges@odoo) <ges@odoo.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/tooling/reload.sh
+++ addons/web/tooling/reload.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+community=$(cd -- "$(dirname "$0")" &> /dev/null && cd ../../.. && pwd)
 
-script="$0"
-basename="$(dirname "$script")"
-
-"$basename/disable.sh"
-"$basename/enable.sh"
+"$community/addons/web/tooling/disable.sh"
+"$community/addons/web/tooling/enable.sh"
