c44f35f61d3f93f0e4cfe3422ef0629a8b6e534b
Alexandre Kühn <aku@odoo.com>
2022-03-22 15:05:22 +0000

    [IMP] mail: turn 'MessageAuthorPrefixView/message' into field
    Task-2793280
    
    closes odoo/odoo#86980
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/models/message_author_prefix_view.js
+++ addons/mail/static/src/models/message_author_prefix_view.js
@@ -2,11 +2,30 @@
 
 import { registerModel } from '@mail/model/model_core';
 import { one } from '@mail/model/model_field';
+import { clear, replace } from '@mail/model/model_field_command';
 
 registerModel({
     name: 'MessageAuthorPrefixView',
     identifyingFields: [['threadNeedactionPreviewViewOwner', 'threadPreviewViewOwner']],
+    recordMethods: {
+        /**
+         * @private
+         * @returns {FieldCommand}
+         */
+        _computeMessage() {
+            if (this.threadNeedactionPreviewViewOwner) {
+                return replace(this.threadNeedactionPreviewViewOwner.thread.lastNeedactionMessageAsOriginThread);
+            }
+            if (this.threadPreviewViewOwner) {
+                return replace(this.threadPreviewViewOwner.thread.lastMessage);
+            }
+            return clear();
+        },
+    },
     fields: {
+        message: one('Message', {
+            compute: '_computeMessage',
+        }),
         threadNeedactionPreviewViewOwner: one('ThreadNeedactionPreviewView', {
             inverse: 'messageAuthorPrefixView',
             readonly: true,
