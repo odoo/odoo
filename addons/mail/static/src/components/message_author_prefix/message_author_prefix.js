7801ea0de4691f0e70bb5e3ae8774bd6a149df96
Alexandre Kühn <aku@odoo.com>
2022-03-22 16:17:26 +0100

    [IMP] mail: turn 'MessageAuthorPrefixView/thread' into field
    Task-2793280

--- addons/mail/static/src/components/message_author_prefix/message_author_prefix.js
+++ addons/mail/static/src/components/message_author_prefix/message_author_prefix.js
@@ -7,29 +7,16 @@ const { Component } = owl;
 export class MessageAuthorPrefix extends Component {
 
     /**
-     * @returns {messageAuthorPrefixView}
+     * @returns {MessageAuthorPrefixView}
      */
     get messageAuthorPrefixView() {
         return this.messaging && this.messaging.models['MessageAuthorPrefixView'].get(this.props.localId);
     }
 
-    /**
-     * @returns {Thread|undefined}
-     */
-    get thread() {
-        return this.messaging && this.messaging.models['Thread'].get(this.props.threadLocalId);
-    }
-
 }
 
 Object.assign(MessageAuthorPrefix, {
-    props: {
-        localId: String,
-        threadLocalId: {
-            type: String,
-            optional: true,
-        },
-    },
+    props: { localId: String },
     template: 'mail.MessageAuthorPrefix',
 });
 
