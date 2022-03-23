b3538270ceaa511444fd1196a05035e4ff204c69
william <wan@odoo.com>
2022-03-23 13:17:30 +0000

    [FIX] sale: compute analytic defaults based on default rules
    Reproduce:
    * Create default analytic rules
    * Create a quotation/sale order.
    
    Before:
    Default analytic rules were not added on sales orders, like they are on
    invoices and purchase orders.
    
    After:
    Same behavior on all documents.
    
    This behavior was mistakenly removed during a refactoring:
    https://github.com/odoo/odoo/commit/3c67bb20feb42f16799b7491411e213b818c4f83
    
    opw-2800936
    
    X-original-commit: 3ada19384780c223be704bea64dad388d363fb03

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -292,6 +292,7 @@ class SaleOrderLine(models.Model):
     order_partner_id = fields.Many2one(related='order_id.partner_id', store=True, string='Customer')
     analytic_tag_ids = fields.Many2many(
         'account.analytic.tag', string='Analytic Tags',
+        compute='_compute_analytic_tag_ids', store=True, readonly=False,
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
     analytic_line_ids = fields.One2many('account.analytic.line', 'so_line', string="Analytic lines")
     is_expense = fields.Boolean('Is expense', help="Is true if the sales order line comes from an expense or a vendor bills")
@@ -515,6 +516,19 @@ class SaleOrderLine(models.Model):
                     amount_to_invoice = price_subtotal - line.untaxed_amount_invoiced
 
             line.untaxed_amount_to_invoice = amount_to_invoice
+
+    @api.depends('product_id', 'order_id.date_order', 'order_id.partner_id')
+    def _compute_analytic_tag_ids(self):
+        for line in self:
+            if not line.analytic_tag_ids:
+                default_analytic_account = line.env['account.analytic.default'].sudo().account_get(
+                    product_id=line.product_id.id,
+                    partner_id=line.order_id.partner_id.id,
+                    user_id=self.env.uid,
+                    date=line.order_id.date_order,
+                    company_id=line.company_id.id,
+                )
+                line.analytic_tag_ids = default_analytic_account.analytic_tag_ids
 
     def _get_invoice_line_sequence(self, new=0, old=0):
         """
