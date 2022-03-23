odoo/odoo

commit 80d74e7ee0eab83dc5100e0776df09d04b882fec

[IMP] mail, web, *: add support for guest users
* = crm_livechat, hr, hr_holidays, im_livechat, mail_bot, purchase, sms,
    snailmail, survey, test_discuss_full, test_mail, web_editor, website,
    website_livechat

 - Create new model `mail.guest` for guests.
 - Rewrite some RPCs to target routes rather than model methods so that
   guests are able to use them.
 - Patch JS and python models to support guests.
 - Create a stand-alone page and boot the channel in it.

task-2494829

closes odoo/odoo#75496

Related: odoo/enterprise#20417
Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

addons/mail/static/src/models/thread_cache/thread_cache.js
@@ -27,10 +27,7 @@ function factory(dependencies) {
             let fetchedMessages;
             let success;
             try {
-                fetchedMessages = await this._loadMessages({
-                    extraDomain: [['id', '<', Math.min(...messageIds)]],
-                    limit,
-                });
+                fetchedMessages = await this._loadMessages({ limit, maxId: Math.min(...messageIds) });
                 success = true;
             } catch (e) {
                 success = false;
@@ -61,10 +58,7 @@ function factory(dependencies) {
                 return;
             }
             const messageIds = this.fetchedMessages.map(message => message.id);
-            const fetchedMessages = this._loadMessages({
-                extraDomain: [['id', '>', Math.max(...messageIds)]],
-                limit: false,
-            });
+            const fetchedMessages = this._loadMessages({ minId: Math.max(...messageIds) });
             if (!fetchedMessages) {
                 return;
             }
@@ -215,53 +209,23 @@ function factory(dependencies) {
 
         /**
          * @private
-         * @param {Array} domain
-         * @returns {Array}
-         */
-        _extendMessageDomain(domain) {
-            const thread = this.thread;
-            if (thread === this.messaging.inbox) {
-                return domain.concat([['needaction', '=', true]]);
-            } else if (thread === this.messaging.starred) {
-                return domain.concat([
-                    ['starred_partner_ids', 'in', [this.messaging.currentPartner.id]],
-                ]);
-            } else if (thread === this.messaging.history) {
-                return domain.concat([['needaction', '=', false]]);
-            } else {
-                // Avoid to load user_notification as these messages are not
-                // meant to be shown on chatters.
-                return domain.concat([
-                    ['message_type', '!=', 'user_notification'],
-                    ['model', '=', thread.model],
-                    ['res_id', '=', thread.id],
-                ]);
-            }
-        }
-
-        /**
-         * @private
          * @param {Object} [param0={}]
-         * @param {Array[]} [param0.extraDomain]
          * @param {integer} [param0.limit=30]
+         * @param {integer} [param0.maxId]
+         * @param {integer} [param0.minId]
          * @returns {mail.message[]}
          * @throws {Error} when failed to load messages
          */
-        async _loadMessages({ extraDomain, limit = 30 } = {}) {
+        async _loadMessages({ limit = 30, maxId, minId } = {}) {
             this.update({ isLoading: true });
-            let domain = [];
-            domain = this._extendMessageDomain(domain);
-            if (extraDomain) {
-                domain = extraDomain.concat(domain);
-            }
-            const context = this.env.session.user_context;
             let messages;
             try {
-                messages = await this.messaging.models['mail.message'].performRpcMessageFetch(
-                    domain,
+                messages = await this.messaging.models['mail.message'].performRpcMessageFetch(this.thread.fetchMessagesUrl, {
+                    ...this.thread.fetchMessagesParams,
                     limit,
-                    context,
-                );
+                    'max_id': maxId,
+                    'min_id': minId,
+                });
             } catch (e) {
                 if (this.exists()) {
                     this.update({
@@ -280,7 +244,7 @@ function factory(dependencies) {
                 isLoaded: true,
                 isLoading: false,
             });
-            if (!extraDomain && messages.length < limit) {
+            if (!minId && messages.length < limit) {
                 this.update({ isAllHistoryLoaded: true });
             }
             this.messaging.messagingBus.trigger('o-thread-cache-loaded-messages', {
@@ -312,6 +276,9 @@ function factory(dependencies) {
          * @private
          */
         _onChangeMarkAllAsRead() {
+            if (this.messaging.currentGuest) {
+                return;
+            }
             if (
                 !this.isMarkAllAsReadRequested ||
                 !this.isLoaded ||

commit f42d1bb00de24b8c867f8249ab9100732ac22208

[FIX] mail: reintroduce scroll to top on posting message in chatter
The feature was broken in https://github.com/odoo/odoo/commit/80d74e7ee0eab83dc5100e0776df09d04b882fec#diff-14904a101d1c0589d79515df7065aeeaaf0d4cdeaff9a4748ff0331c481efe8eL275
where `loadNewMessages` was mistakenly removed.

But this call is useful to make sure newly posted messages (from other users)
are fetched, as well as having the side-effect to scroll to the newly posted
message of the current user in particular.

addons/mail/static/src/components/message_list/message_list.js
@@ -109,6 +109,7 @@ export class MessageList extends Component {
                     // saved position if it exists or scroll to the end
                     this._adjustScrollFromModel();
                     break;
+                case 'message-posted':
                 case 'message-received':
                 case 'messages-loaded':
                 case 'new-messages-loaded':

addons/mail/static/src/models/composer_view/composer_view.js
@@ -327,12 +327,15 @@ function factory(dependencies) {
                 for (const threadView of message.originThread.threadViews) {
                     // Reset auto scroll to be able to see the newly posted message.
                     threadView.update({ hasAutoScrollOnMessageReceived: true });
+                    threadView.addComponentHint('message-posted', { message });
                 }
                 if (chatterThread) {
                     if (this.exists()) {
                         this.delete();
                     }
                     if (chatterThread.exists()) {
+                        // Load new messages to fetch potential new messages from other users (useful due to lack of auto-sync in chatter).
+                        chatterThread.loadNewMessages();
                         chatterThread.refreshFollowers();
                         chatterThread.fetchAndUpdateSuggestedRecipients();
                     }

addons/mail/static/src/models/thread_cache/thread_cache.js
@@ -59,7 +59,7 @@ function factory(dependencies) {
             }
             const messageIds = this.fetchedMessages.map(message => message.id);
             const fetchedMessages = this._loadMessages({ minId: Math.max(...messageIds) });
-            if (!fetchedMessages) {
+            if (!fetchedMessages || fetchedMessages.length === 0) {
                 return;
             }
             for (const threadView of this.threadViews) {