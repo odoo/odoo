6a21c6419e51ba0da81325ef49615cac4df833c5
Alexandre Kühn <aku@odoo.com>
2022-03-23 15:34:43 +0000

    [IMP] mail: 'ComposerSuggestion' + 'ComposerView/mainSuggestions'
    Task-2793280
    
    closes odoo/odoo#87092
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

---
+++ addons/mail/static/src/models/composer_suggestion.js
@@ -0,0 +1,59 @@
+/** @odoo-module **/
+
+import { registerModel } from '@mail/model/model_core';
+import { one } from '@mail/model/model_field';
+import { clear, replace } from '@mail/model/model_field_command';
+
+/**
+ * Models a suggestion in the composer suggestion.
+ *
+ * For instance, to mention a partner, can type "@" and some keyword,
+ * and display suggested partners to mention.
+ */
+registerModel({
+    name: 'ComposerSuggestion',
+    identifyingFields: ['composerViewOwnerAsMainSuggestion', ['cannedResponse', 'channelCommand', 'partner', 'thread']],
+    recordMethods: {
+        /**
+         * @private
+         * @returns {FieldCommand}
+         */
+        _computeRecord() {
+            if (this.cannedResponse) {
+                return replace(this.cannedResponse);
+            }
+            if (this.channelCommand) {
+                return replace(this.channelCommand);
+            }
+            if (this.partner) {
+                return replace(this.partner);
+            }
+            if (this.thread) {
+                return replace(this.thread);
+            }
+            return clear();
+        },
+    },
+    fields: {
+        cannedResponse: one('CannedResponse', {
+            readonly: true,
+        }),
+        channelCommand: one('ChannelCommand', {
+            readonly: true,
+        }),
+        composerViewOwnerAsMainSuggestion: one('ComposerView', {
+            inverse: 'mainSuggestions',
+            readonly: true,
+            required: true,
+        }),
+        record: one('Record', {
+            compute: '_computeRecord',
+        }),
+        partner: one('Partner', {
+            readonly: true,
+        }),
+        thread: one('Thread', {
+            readonly: true,
+        }),
+    },
+});
