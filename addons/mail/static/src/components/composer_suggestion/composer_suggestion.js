051bca2ecb473d85a965f31dc4e4f97f92b86412
Alexandre Kühn <aku@odoo.com>
2022-03-24 17:27:44 +0000

    [IMP] mail: introduce 'ComposerView/activeSuggestion' field
    Task-2793280
    
    closes odoo/odoo#87220
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/components/composer_suggestion/composer_suggestion.js
+++ addons/mail/static/src/components/composer_suggestion/composer_suggestion.js
@@ -1,7 +1,7 @@
 /** @odoo-module **/
 
 import { useUpdate } from '@mail/component_hooks/use_update';
-import { link } from '@mail/model/model_field_command';
+import { replace } from '@mail/model/model_field_command';
 import { registerMessagingComponent } from '@mail/utils/messaging_component';
 
 const { Component } = owl;
@@ -19,6 +19,13 @@ export class ComposerSuggestion extends Component {
     //--------------------------------------------------------------------------
     // Public
     //--------------------------------------------------------------------------
+
+    /**
+     * @returns {ComposerSuggestion}
+     */
+    get composerSuggestion() {
+        return this.messaging && this.messaging.models['ComposerSuggestion'].get(this.props.localId);
+    }
 
     /**
      * @returns {ComposerView}
@@ -102,7 +109,7 @@ export class ComposerSuggestion extends Component {
      */
     _onClick(ev) {
         ev.preventDefault();
-        this.composerView.update({ activeSuggestedRecord: link(this.record) });
+        this.composerView.update({ activeSuggestion: replace(this.composerSuggestion) });
         this.composerView.insertSuggestion();
         this.composerView.closeSuggestions();
         this.composerView.update({ doFocus: true });
@@ -117,6 +124,7 @@ Object.assign(ComposerSuggestion, {
     props: {
         composerViewLocalId: String,
         isActive: { type: Boolean, optional: true },
+        localId: String,
         modelName: String,
         recordLocalId: String,
     },
