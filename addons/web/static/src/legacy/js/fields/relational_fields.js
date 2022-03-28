4031e65584921e2f78292ad4c9b97f95691d2be6
Hubert Van de Walle (huvw) <huvw@odoo.com>
2022-03-28 10:42:41 +0200

    [FIX] web: radio widget ignores required attribute
    opw-2729810
    
    X-original-commit: 65f4c4f29a9c747f114eb6359d3ebb036efab9d6

--- addons/web/static/src/legacy/js/fields/relational_fields.js
+++ addons/web/static/src/legacy/js/fields/relational_fields.js
@@ -3413,14 +3413,6 @@ var FieldRadio = FieldSelection.extend({
     },
 
     /**
-     * @override
-     * @returns {boolean} always true
-     */
-    isSet: function () {
-        return true;
-    },
-
-    /**
      * Associates the 'for' attribute to the radiogroup, instead of the selected
      * radio button.
      *
