1191abca6aa863cbb4a70c0329fd2271547049fa
Jigar Vaghela <jva@odoo.com>
2022-03-30 18:05:11 +0000

    [FIX] l10n_in_edi: fix token_validity time convert to utc, fix groups name
    we find some bug when backporting to v15 so we fix this in fw-port
    
    value of token_validity is not converted to UTC so fix it,
    And groups in the field are wrong like ```account.group_system``` so set right one ```base.group_system```
    Also, fix IAP buy credits message where set URL out of the Markup
    
    Task 2179664
    
    closes odoo/odoo#88752
    
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_in_edi/models/res_company.py
+++ addons/l10n_in_edi/models/res_company.py
@@ -9,8 +9,8 @@ class ResCompany(models.Model):
 
     l10n_in_edi_username = fields.Char("E-invoice (IN) Username", groups="base.group_system")
     l10n_in_edi_password = fields.Char("E-invoice (IN) Password", groups="base.group_system")
-    l10n_in_edi_token = fields.Char("E-invoice (IN) Token", groups="account.group_system")
-    l10n_in_edi_token_validity = fields.Datetime("E-invoice (IN) Valid Until", groups="account.group_system")
+    l10n_in_edi_token = fields.Char("E-invoice (IN) Token", groups="base.group_system")
+    l10n_in_edi_token_validity = fields.Datetime("E-invoice (IN) Valid Until", groups="base.group_system")
     l10n_in_edi_production_env = fields.Boolean(
         string="E-invoice (IN) Is production OSE environment",
         help="Enable the use of production credentials",
