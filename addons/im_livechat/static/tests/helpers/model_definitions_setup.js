33867b9f5c81799ffe14c45458debbb5ee3b4e71
tsm-odoo <tsm@odoo.com>
2022-03-31 07:11:45 +0000

    [IMP] mail: remove default values from test model definitions
    The get_model_definitions method returns default values for each field. Those default
    values are wrong most of the time (dynamic values, reference to non created records on
    the client side, ...).
    
    task-2792108
    
    closes odoo/odoo#87641
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/im_livechat/static/tests/helpers/model_definitions_setup.js
+++ addons/im_livechat/static/tests/helpers/model_definitions_setup.js
@@ -1,5 +1,8 @@
 /** @odoo-module **/
 
-import { addModelNamesToFetch } from '@mail/../tests/helpers/model_definitions_helpers';
+import { addModelNamesToFetch, insertModelFields } from '@mail/../tests/helpers/model_definitions_helpers';
 
 addModelNamesToFetch(['im_livechat.channel']);
+insertModelFields('res.users.settings', {
+    is_discuss_sidebar_category_livechat_open: { default: true },
+});
