42d0ea64add055aefcc23b17d341e43f6b2ba181
Adrien Widart <awt@odoo.com>
2022-03-31 12:14:25 +0000

    [FIX] product_expiry: propagate lot's expiration date
    On a receipt, when defining the expiration date of a lot, the SML keeps
    <today> as expiration date
    
    To reproduce the issue:
    1. Edit the operation type "Receipts":
        - Show Detailed Operations: True
        - Use Existing Lots/Serial Numbers: True
    2. Create a product P:
        - Type: Storable
        - Tracking: By Lots
        - Expiration Date: Enabled
    3. Create a planned receipt R with 1 x P
    4. Mark R as Todo
    5. Add a detailed operations:
        - 1 x P with new lot L
    6. Edit the lot:
        - Expiration Date: <today + 7days>
    
    Error: When saving the picking (with the lot updated), the expiration
    date is incorrect (set to <today>).
    
    OPW-2785256
    
    closes odoo/odoo#88283
    
    X-original-commit: dd78e43a6286f798c54cf6b3222d3861b9dad34f
    Signed-off-by: Steve Van Essche <svs@odoo.com>
    Signed-off-by: Adrien Widart <awt@odoo.com>

--- addons/product_expiry/models/stock_move_line.py
+++ addons/product_expiry/models/stock_move_line.py
@@ -15,10 +15,12 @@ class StockMoveLine(models.Model):
         ' become dangerous and must not be consumed.')
     is_expired = fields.Boolean(related='lot_id.product_expiry_alert')
 
-    @api.depends('product_id', 'picking_type_use_create_lots')
+    @api.depends('product_id', 'picking_type_use_create_lots', 'lot_id.expiration_date')
     def _compute_expiration_date(self):
         for move_line in self:
-            if move_line.picking_type_use_create_lots:
+            if not move_line.expiration_date and move_line.lot_id.expiration_date:
+                move_line.expiration_date = move_line.lot_id.expiration_date
+            elif move_line.picking_type_use_create_lots:
                 if move_line.product_id.use_expiration_date:
                     if not move_line.expiration_date:
                         move_line.expiration_date = fields.Datetime.today() + datetime.timedelta(days=move_line.product_id.expiration_time)
