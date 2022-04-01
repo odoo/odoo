c69961540e560af5d7d052e711a3a487d57d1ca0
Alexandre Kühn <aku@odoo.com>
2022-04-01 15:39:28 +0200

    [IMP] mail: remove AttachmentViewerComponent/_download
    Task-2579306

--- addons/mail/static/src/components/attachment_viewer/attachment_viewer.js
+++ addons/mail/static/src/components/attachment_viewer/attachment_viewer.js
@@ -93,15 +93,6 @@ export class AttachmentViewer extends Component {
     }
 
     /**
-     * Download the attachment.
-     *
-     * @private
-     */
-    _download() {
-        this.attachmentViewer.attachment.download();
-    }
-
-    /**
      * Determine whether the current image is rendered for the 1st time, and if
      * that's the case, display a spinner until loaded.
      *
@@ -317,7 +308,7 @@ export class AttachmentViewer extends Component {
      */
     _onClickDownload(ev) {
         ev.stopPropagation();
-        this._download();
+        this.attachmentViewer.attachment.download();
     }
 
     /**
