35529553f5c907907c6c73619af4b785b24cb612
Didier (did) <did@odoo.com>
2022-04-01 12:18:26 +0200

    [FIX] mail: add missing checksum convert data
    Follow up of send is_main with the attachments.
    
    task-2783076
    
    X-original-commit: 2eafb395afb0262f2b789e377932ac191db1fdcc

--- addons/mail/static/src/models/attachment/attachment.js
+++ addons/mail/static/src/models/attachment/attachment.js
@@ -15,6 +15,9 @@ registerModel({
          */
         convertData(data) {
             const data2 = {};
+            if ('checksum' in data) {
+                data2.checksum = data.checksum;
+            }
             if ('filename' in data) {
                 data2.filename = data.filename;
             }
