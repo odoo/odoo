ff519046da858b95bab79090a4e492f4bfff8c82
Anna <s775518@sbuniv.edu>
2022-04-02 22:27:12 -0500

    I DID IT!!!
    Added security/mental_health_security.xml to the manifest

--- custom_models/mental_health/__manifest__.py
+++ custom_models/mental_health/__manifest__.py
@@ -4,6 +4,7 @@
         'base',
     ],
     'data': [
+        'security/mental_health_security.xml',
         'security/ir.model.access.csv',
         'views/mental_health_notes_views.xml',
         'views/mental_health_notes_menus.xml',
