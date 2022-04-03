058f90e4bb2c0616f3b223886ddbdec1e91b0de2
Romain Derie <rde@odoo.com>
2022-04-03 23:53:23 +0200

    [FIX] website, *: correct wrong query count after httpocalypse
    *: test_website, website_blog (test perfs)
    
    An error regarding tests was not catch during httpocalypse [1] review.
    The test query counts were lowered in website by 2 everywhere, but it
    shouldn't have as there were no SQL perf improvement from this
    refactoring regarding website.
    The query count actually decreased because the test-only SQL Savepoints
    got divided by 2, probably thanks to a low level test improvement in
    the PR.
    But outside of tests, there were no SQL Query gained.
    This should have been reflected in the `EXTRA_REQUEST` variable
    instead.
    
    Before:
    Savepoint > Rollback > Savepoint > [Actual SQL Queries] > Rollback
    After:
    Savepoint > [Actual SQL Queries] > Rollback
    
    It is important to fix it because:
    1. (Minor) There is a de-sync between the query count in the test and
       the actual query count (in the logs outside of tests), making it
       hard to figure.
    2. (Major) Some controller got then wrongly lowered to 0 query counts
       instead of 2. An incoming PR in master would then make those tests
       expects negative query counts, which doesn't make any sense.
    
    [1]: https://github.com/odoo/odoo/pull/78857
    Note that the exact commit can't be found by bisect as those don't work
    on their own, DB can't be started due to circular import.

--- addons/website_blog/tests/test_performance.py
+++ addons/website_blog/tests/test_performance.py
@@ -13,8 +13,8 @@ class TestBlogPerformance(UtilPerf):
             self.env['website'].search([]).channel_id = False
 
     def test_10_perf_sql_blog_standard_data(self):
-        self.assertEqual(self._get_url_hot_query('/blog'), 26)
-        self.assertEqual(self._get_url_hot_query('/blog', cache=False), 25)
+        self.assertEqual(self._get_url_hot_query('/blog'), 28)
+        self.assertEqual(self._get_url_hot_query('/blog', cache=False), 27)
 
     def test_20_perf_sql_blog_bigger_data_scaling(self):
         BlogPost = self.env['blog.post']
@@ -26,10 +26,10 @@ class TestBlogPerformance(UtilPerf):
         for blog_post in blog_posts:
             blog_post.tag_ids += blog_tags
             blog_tags = blog_tags[:-1]
-        self.assertEqual(self._get_url_hot_query('/blog'), 26)
-        self.assertEqual(self._get_url_hot_query('/blog', cache=False), 25)
-        self.assertEqual(self._get_url_hot_query(blog_post[0].website_url), 30)
-        self.assertEqual(self._get_url_hot_query(blog_post[0].website_url, cache=False), 29)
+        self.assertEqual(self._get_url_hot_query('/blog'), 28)
+        self.assertEqual(self._get_url_hot_query('/blog', cache=False), 27)
+        self.assertEqual(self._get_url_hot_query(blog_post[0].website_url), 32)
+        self.assertEqual(self._get_url_hot_query(blog_post[0].website_url, cache=False), 31)
 
     def test_30_perf_sql_blog_bigger_data_scaling(self):
         BlogPost = self.env['blog.post']
@@ -41,7 +41,7 @@ class TestBlogPerformance(UtilPerf):
         for blog_post in blog_posts:
             blog_post.write({'tag_ids': [[6, 0, random.choices(blog_tags.ids, k=random.randint(0, len(blog_tags)))]]})
 
-        self.assertLessEqual(self._get_url_hot_query('/blog'), 27)
-        self.assertLessEqual(self._get_url_hot_query('/blog', cache=False), 26)
-        self.assertLessEqual(self._get_url_hot_query(blog_post[0].website_url), 32)
-        self.assertLessEqual(self._get_url_hot_query(blog_post[0].website_url, cache=False), 31)
+        self.assertLessEqual(self._get_url_hot_query('/blog'), 29)
+        self.assertLessEqual(self._get_url_hot_query('/blog', cache=False), 28)
+        self.assertLessEqual(self._get_url_hot_query(blog_post[0].website_url), 34)
+        self.assertLessEqual(self._get_url_hot_query(blog_post[0].website_url, cache=False), 33)
