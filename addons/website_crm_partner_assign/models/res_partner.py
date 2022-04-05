fddd8ed890169fef82aab7bc77efabbbd22a5141
Thibault Delavallée <tde@odoo.com>
2022-04-05 07:50:11 +0000

    [FIX] website_crm_partner_assign: split costly search
    On an heavy database searching for company leads or assigned leads can be
    costly. Having an OR does not necessarily triggers index usage and query plan
    uses an index scan.
    
    It is less costly to do two searches: one for the base domain (based on
    commercial entity) and one for the assign domain (based on partner assigned).
    
    Task-2770994
    
    closes odoo/odoo#88538
    
    X-original-commit: 6957d7a57cb3bf25d552d3f8f2760db46ace5e3a
    Signed-off-by: Thibault Francois <tfr@odoo.com>

--- addons/website_crm_partner_assign/models/res_partner.py
+++ addons/website_crm_partner_assign/models/res_partner.py
@@ -73,6 +73,18 @@ class ResPartner(models.Model):
             partner.opportunity_count += assign_counts.get(partner.id, 0)
 
     def action_view_opportunity(self):
+        self.ensure_one()  # especially here as we are doing an id, in, IDS domain
         action = super().action_view_opportunity()
-        action['domain'] = expression.OR([action.get('domain', []), [('partner_assigned_id', '=', self.id)]])
+        action_domain_origin = action.get('domain')
+        action_context_origin = action.get('context') or {}
+        action_domain_assign = [('partner_assigned_id', '=', self.id)]
+        if not action_domain_origin:
+            action['domain'] = action_domain_assign
+            return action
+        # perform searches independently as having OR with those leaves seems to
+        # be counter productive
+        Lead = self.env['crm.lead'].with_context(**action_context_origin)
+        ids_origin = Lead.search(action_domain_origin).ids
+        ids_new = Lead.search(action_domain_assign).ids
+        action['domain'] = [('id', 'in', sorted(list(set(ids_origin) | set(ids_new))))]
         return action
