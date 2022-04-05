3bebd56deccff02f126af3518ec17269e7908c8b
dbkosky <dako@odoo.com>
2022-04-05 12:53:37 +0000

    [FIX] l10n_it_edi: minimise filename duplication
    The problem is that we have a limited space in the filename for
    differentiating one file from the other. Only the five characters at the
    end of the filename are used, this looks like:
    
        <CODICE_FISCALE>_NNNNN.xml (where N is a character a-zA-Z0-9)
    
    The current version utilises the invoice id to get the unique filename.
    This has a couple of problems:
     - It doesn't utilise the (lowercase) a-z characters
     - Draft invoices still have invoice ids
     - In a multicompany environment, the invoice ids are not unique to the
       company (i.e. another company creating an invoice will increment the
       value of the invoice.id on your company)
    These above problems mean that we're not currently utilising the hashing
    space available to us by the characters of the filename.
    
    The solution is to utilise an ir.sequence, that is used to determine
    each filename. This has the benefit of being editable by the user, so if
    they continue to get duplications, they can offset them by adjusting the
    value of the sequence's next number.
    
    A more descriptive error message and its translation term have also been
    added for when a filename duplication occurs.
    
    relates to
    
    closes odoo/odoo#88005
    
    Task-id: 2813957
    Ticket-id: 2788927
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_it_edi/models/account_edi_format.py
+++ addons/l10n_it_edi/models/account_edi_format.py
@@ -30,8 +30,22 @@ class AccountEdiFormat(models.Model):
         '''Returns a name conform to the Fattura pa Specifications:
            See ES documentation 2.2
         '''
-        a = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        n = invoice.id
+        a = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        # Each company should have its own filename sequence. If it does not exist, create it
+        n = self.env['ir.sequence'].with_company(invoice.company_id).next_by_code('l10n_it_edi.fattura_filename')
+        if not n:
+            # The offset is used to avoid conflicts with existing filenames
+            offset = 62 ** 4
+            sequence = self.env['ir.sequence'].sudo().create({
+                'name': 'FatturaPA Filename Sequence',
+                'code': 'l10n_it_edi.fattura_filename',
+                'company_id': invoice.company_id.id,
+                'number_next': offset,
+            })
+            n = sequence._next()
+        # The n is returned as a string, but we require an int
+        n = int(''.join(filter(lambda c: c.isdecimal(), n)))
+
         progressive_number = ""
         while n:
             (n, m) = divmod(n, len(a))
