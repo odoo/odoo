05dda2be2676b1c56ed739c0233241ab58578c4e
Xavier BOL (xbo) <xbo@odoo.com>
2022-04-05 16:12:17 +0000

    [IMP] project,sale_{project{_stock},timesheet}: display form view if one record
    Before this commit, when the user clicks on a row in Sales section
    displayed in the right side panel of project update, the action display
    the list view even if there is only one record to display.
    
    This commit passes the `res_id` with the action to the frontend to
    call the action and directly display the form view when we are sure
    there is only one record to show.
    
    task-2814466
    
    Part-of: odoo/odoo#88219

--- addons/sale_project_stock/models/sale_order_line.py
+++ addons/sale_project_stock/models/sale_order_line.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import models
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-
-    stock_move_count = fields.Integer(compute='_compute_stock_move_count')
-
-    def _compute_stock_move_count(self):
-        stock_move_read_group = self.env['stock.move']._read_group([('sale_line_id', 'in', self.ids)], ['sale_line_id'], ['sale_line_id'])
-        stock_move_count_per_sol = {res['sale_line_id'][0]: res['sale_line_id_count'] for res in stock_move_read_group}
-        for sol in self:
-            sol.stock_move_count = stock_move_count_per_sol.get(sol.id, 0)
 
     def _get_action_per_item(self):
         """ Get action per Sales Order Item to display the stock moves linked
@@ -21,7 +13,12 @@ class SaleOrderLine(models.Model):
         """
         action_per_sol = super()._get_action_per_item()
         stock_move_action = self.env.ref('sale_project_stock.stock_move_per_sale_order_line_action').id
+        stock_move_ids_per_sol = {}
+        if self.user_has_groups('stock.group_stock_user'):
+            stock_move_read_group = self.env['stock.move']._read_group([('sale_line_id', 'in', self.ids)], ['sale_line_id', 'ids:array_agg(id)'], ['sale_line_id'])
+            stock_move_ids_per_sol = {res['sale_line_id'][0]: res['ids'] for res in stock_move_read_group}
         for sol in self:
-            if not sol.is_service and sol.stock_move_count > 0:
-                action_per_sol[sol.id] = stock_move_action
+            stock_move_ids = stock_move_ids_per_sol.get(sol.id, [])
+            if not sol.is_service and stock_move_ids:
+                action_per_sol[sol.id] = stock_move_action, stock_move_ids[0] if len(stock_move_ids) == 1 else False
         return action_per_sol
