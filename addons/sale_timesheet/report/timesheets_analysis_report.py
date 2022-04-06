da1b2c0c3cdc6399b6ef24f071af3ad8f905c7d8
Cedric Prieels (cpr) <cpr@odoo.com>
2022-04-06 07:55:14 +0000

    [IMP] hr_timesheet,timesheet_grid: add new pivot measures and generic improvements
    A new report was created for timesheets, also introducing new measures regarding
    the (non-)billable time (percentage) and the timesheets revenues/costs. Some general
    improvements were furthermore introduced in the timesheet apps.
    
    See task-2783955
    
    closes odoo/odoo#88072
    
    Related: odoo/upgrade#3509
    Related: odoo/enterprise#25922
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

---
+++ addons/sale_timesheet/report/timesheets_analysis_report.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models, api
+
+from odoo.addons.sale_timesheet.models.account import TIMESHEET_INVOICE_TYPES
+
+
+class TimesheetsAnalysisReport(models.Model):
+    _inherit = "timesheets.analysis.report"
+
+    order_id = fields.Many2one("sale.order", string="Sales Order", readonly=True)
+    so_line = fields.Many2one("sale.order.line", string="Sales Order Item", readonly=True)
+    timesheet_invoice_type = fields.Selection(TIMESHEET_INVOICE_TYPES, string="Billable Type", readonly=True)
+    timesheet_invoice_id = fields.Many2one("account.move", string="Invoice", readonly=True, help="Invoice created from the timesheet")
+    timesheet_revenues = fields.Float("Timesheet Revenues", readonly=True, help="Number of hours spent multiplied by the unit price per hour/day.")
+    margin = fields.Float("Margin", readonly=True, help="Timesheets revenues minus the costs")
+    billable_time = fields.Float("Billable Hours", readonly=True, help="Number of hours/days linked to a SOL.")
+    non_billable_time = fields.Float("Non-billable Hours", readonly=True, help="Number of hours/days not linked to a SOL.")
+
+    @property
+    def _table_query(self):
+        return """
+            SELECT A.*,
+                (timesheet_revenues + A.amount) AS margin,
+                (A.unit_amount - billable_time) AS non_billable_time
+            FROM (
+                %s %s %s
+            ) A
+        """ % (self._select(), self._from(), self._where())
+
+    @api.model
+    def _select(self):
+        return super()._select() + """,
+            A.order_id AS order_id,
+            A.so_line AS so_line,
+            A.timesheet_invoice_type AS timesheet_invoice_type,
+            A.timesheet_invoice_id AS timesheet_invoice_id,
+            (-1 * A.unit_amount * A.amount) AS timesheet_revenues,
+            CASE WHEN order_id IS NULL THEN 0 ELSE unit_amount END AS billable_time
+        """
