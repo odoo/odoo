52c0411f2ff833c621f4434e0c520626ddc13563
Victor Feyens <vfe@odoo.com>
2022-04-08 12:10:50 +0000

    [FIX] sale: non required pricelist
    Sales flow require a pricelist to work, but the field was
    set non required by mistake.
    
    Since this is a stable version, we can only reduce potential problems
    through views & create modifications.
    
    The field will be made required again in next version(s).
    
    closes odoo/odoo#88387
    
    X-original-commit: af9f60d6981fb8b010dc60e93561d7af88372f47
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -175,7 +175,7 @@ class SaleOrder(models.Model):
         comodel_name='product.pricelist',
         string="Pricelist",
         compute='_compute_pricelist_id',
-        store=True, readonly=False, precompute=True, check_company=True,  # Unrequired company
+        store=True, readonly=False, precompute=True, check_company=True, required=True,  # Unrequired company
         states=READONLY_FIELD_STATES,
         tracking=1,
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
