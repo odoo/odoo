16f01c31f9779b9f3a80a2b4573f9db1d08e4b3e
Antoine Guenet <age@odoo.com>
2022-04-11 16:40:02 +0200

    [FIX] web_editor: provide proper support for mixed language directions
    This introduces a "direction" option on the editor to set the text
    direction of the editor (passed by Odoo based on the localization), and
    a Powerbox command to switch said direction on a given block. This
    allows users to mix several text directions within the same text, eg.
    when quoting Hebrew in an English text.
    
    task-2814004

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -208,6 +208,7 @@ export class OdooEditor extends EventTarget {
                 },
                 isHintBlacklisted: () => false,
                 filterMutationRecords: (records) => records,
+                direction: 'ltr',
                 _t: string => string,
                 allowCommandVideo: true,
             },
@@ -271,6 +272,7 @@ export class OdooEditor extends EventTarget {
         this._idToNodeMap.set(1, editable);
         this.editable = this.options.toSanitize ? sanitize(editable) : editable;
         this.editable.classList.add("odoo-editor-editable");
+        this.editable.setAttribute('dir', this.options.direction);
 
         // Set contenteditable before clone as FF updates the content at this point.
         this._activateContenteditable();
@@ -1840,6 +1842,15 @@ export class OdooEditor extends EventTarget {
                 },
             },
             {
+                groupName: 'Basic blocks',
+                title: this.options._t('Switch direction'),
+                description: this.options._t('Switch the text\'s direction.'),
+                fontawesome: 'fa-exchange',
+                callback: () => {
+                    this.execCommand('switchDirection');
+                },
+            },
+            {
                 groupName: 'Widgets',
                 title: '3 Stars',
                 description: 'Insert a rating over 3 stars.',
@@ -1969,7 +1980,7 @@ export class OdooEditor extends EventTarget {
             const selectionStartStyle = getComputedStyle(closestStartContainer);
 
             // queryCommandState does not take stylesheets into account
-            for (const format of ['bold', 'italic', 'underline', 'strikeThrough']) {
+            for (const format of ['bold', 'italic', 'underline', 'strikeThrough', 'switchDirection']) {
                 const formatButton = this.toolbar.querySelector(`#${format.toLowerCase()}`);
                 if (formatButton) {
                     formatButton.classList.toggle('active', isSelectionFormat(this.editable, format));
