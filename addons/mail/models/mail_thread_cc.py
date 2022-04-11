6768a733d0d26bcb66dda384c825a4af8ce54055
Thibault Delavallée <tde@odoo.com>
2022-04-11 11:38:31 +0200

    [IMP] various: use 'email_formatted' instead of 'formataddr'
    PURPOSE
    
    Be defensive when dealing with email fields, notably when having multi-emails
    or email field containing an already-formatted email.
    
    SPECIFICATIONS
    
    When possible, use 'email_formatted' field on partner, instead of calling
    'format_addr'. That way management of corner case input (multi emails and
    double encapsulation) is managed by the computed field itself.
    
    When 'format_addr' has to be used, ensure email part is normalized to avoid
    formatting issues.
    
    Also use tools to extract emails instead of 'email_re' regex when possible.
    That way all code extracting and managing emails go through the same stack.
    
    Task-2612945 (Mail: Defensive email formatting)
    
    X-original-commit: odoo/odoo@9ef715fb994564af9e933ba31933a09eca97c24f

--- addons/mail/models/mail_thread_cc.py
+++ addons/mail/models/mail_thread_cc.py
@@ -15,8 +15,10 @@ class MailCCMixin(models.AbstractModel):
         '''return a dict of sanitize_email:raw_email from a string of cc'''
         if not cc_string:
             return {}
-        return {tools.email_normalize(email): tools.formataddr((name, tools.email_normalize(email)))
-            for (name, email) in tools.email_split_tuples(cc_string)}
+        return {
+            tools.email_normalize(email): tools.formataddr((name, tools.email_normalize(email)))
+            for (name, email) in tools.email_split_tuples(cc_string)
+        }
 
     @api.model
     def message_new(self, msg_dict, custom_values=None):
