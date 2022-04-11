4d73c268be1d5188132d5852ba3a7dc5877786e4
Prakash Prajapati <ppr@odoo.com>
2022-04-11 10:28:17 +0530

    [FIX] sale_management: show correct order details to the customer
    Currently, when the quantity of an optional SOline is modified on the
    portal, only the quantity and order/line amounts are updated.
    
    This raises two problematic situations when the pricelist is
    configured to give discount when a certain number of products is
    reached:
    1) If the pricelist is configured to include the discount in the price,
    the price is not refreshed, which means you see a line total which is
    not the result of unit price * qty
    2) If the pricelist is configured to show the discount, you won't see
    it even if there is one (and you won't see the column if there was
    no discount in the SO before your qty modification).
    
    This commit makes sure the order details are refreshed when the quantity
    of an option is modified.  To do so, we simply need to use the previous
    code used to refresh the content when an option was added to the order.
    
    Since we now do a full refresh of the order details whenever the quantity
    is updated, the old code updating part of the order details (qty & amounts)
    is unnecessary and can be safely removed.
    
    While this code & logic is modified, a cleanup & some documentation of
    the code was also made.
    
    task-2180180
    
    Co-authored-by: Victor Feyens <vfe@odoo.com>

--- addons/sale_management/static/src/js/sale_management.js
+++ addons/sale_management/static/src/js/sale_management.js
@@ -6,87 +6,19 @@ var publicWidget = require('web.public.widget');
 publicWidget.registry.SaleUpdateLineButton = publicWidget.Widget.extend({
     selector: '.o_portal_sale_sidebar',
     events: {
-        'click a.js_update_line_json': '_onClick',
-        'click a.js_add_optional_products': '_onClickOptionalProduct',
-        'change .js_quantity': '_onChangeQuantity'
+        'click a.js_update_line_json': '_onClickOptionQuantityButton',
+        'click a.js_add_optional_products': '_onClickAddOptionalProduct',
+        'change .js_quantity': '_onChangeOptionQuantity',
     },
+
     /**
      * @override
      */
     async start() {
         await this._super(...arguments);
         this.orderDetail = this.$el.find('table#sales_order_table').data();
-        this.elems = this._getUpdatableElements();
     },
-    /**
-     * Process the change in line quantity
-     *
-     * @private
-     * @param {Event} ev
-     */
-    _onChangeQuantity(ev) {
-        ev.preventDefault();
-        let self = this,
-            $target = $(ev.currentTarget),
-            quantity = parseInt($target.val());
 
-        this._callUpdateLineRoute(self.orderDetail.orderId, {
-            'line_id': $target.data('lineId'),
-            'input_quantity': quantity >= 0 ? quantity : false,
-            'access_token': self.orderDetail.token
-        }).then((data) => {
-            self._updateOrderLineValues($target.closest('tr'), data);
-            self._updateOrderValues(data);
-        });
-    },
-    /**
-     * Reacts to the click on the -/+ buttons
-     *
-     * @param {Event} ev
-     */
-    _onClick(ev) {
-        ev.preventDefault();
-        let self = this,
-            $target = $(ev.currentTarget);
-        this._callUpdateLineRoute(self.orderDetail.orderId, {
-            'line_id': $target.data('lineId'),
-            'remove': $target.data('remove'),
-            'unlink': $target.data('unlink'),
-            'access_token': self.orderDetail.token
-        }).then((data) => {
-            var $saleTemplate = $(data['sale_template']);
-            if ($saleTemplate.length && data['unlink']) {
-                self.$('#portal_sale_content').html($saleTemplate);
-                self.elems = self._getUpdatableElements();
-            }
-            self._updateOrderLineValues($target.closest('tr'), data);
-            self._updateOrderValues(data);
-        });
-    },
-    /**
-     * trigger when optional product added to order from portal.
-     *
-     * @private
-     * @param {Event} ev
-     */
-    _onClickOptionalProduct(ev) {
-        ev.preventDefault();
-        let self = this,
-            $target = $(ev.currentTarget);
-        // to avoid double click on link with href.
-        $target.css('pointer-events', 'none');
-
-        this._rpc({
-            route: "/my/orders/" + self.orderDetail.orderId + "/add_option/" + $target.data('optionId'),
-            params: {access_token: self.orderDetail.token}
-        }).then((data) => {
-            if (data) {
-                self.$('#portal_sale_content').html($(data['sale_template']));
-                self.elems = self._getUpdatableElements();
-                self._updateOrderValues(data);
-            }
-        });
-    },
     /**
      * Calls the route to get updated values of the line and order
      * when the quantity of a product has changed
@@ -96,87 +28,87 @@ publicWidget.registry.SaleUpdateLineButton = publicWidget.Widget.extend({
      * @param {Object} params
      * @return {Deferred}
      */
-    _callUpdateLineRoute(order_id, params) {
+     _callUpdateLineRoute(order_id, params) {
         return this._rpc({
             route: "/my/orders/" + order_id + "/update_line_dict",
             params: params,
         });
     },
+
     /**
-     * Processes data from the server to update the orderline UI
+     * Refresh the UI of the order details
      *
      * @private
-     * @param {Element} $orderLine: orderline element to update
-     * @param {Object} data: contains order and line updated values
+     * @param {Object} data: contains order html details
      */
-    _updateOrderLineValues($orderLine, data) {
-        let linePriceTotal = data.order_line_price_total,
-            linePriceSubTotal = data.order_line_price_subtotal,
-            $linePriceTotal = $orderLine.find('.oe_order_line_price_total .oe_currency_value'),
-            $linePriceSubTotal = $orderLine.find('.oe_order_line_price_subtotal .oe_currency_value');
-
-        if (!$linePriceTotal.length && !$linePriceSubTotal.length) {
-            $linePriceTotal = $linePriceSubTotal = $orderLine.find('.oe_currency_value').last();
-        }
-
-        $orderLine.find('.js_quantity').val(data.order_line_product_uom_qty);
-        if ($linePriceTotal.length && linePriceTotal !== undefined) {
-            $linePriceTotal.text(linePriceTotal);
-        }
-        if ($linePriceSubTotal.length && linePriceSubTotal !== undefined) {
-            $linePriceSubTotal.text(linePriceSubTotal);
+    _refreshOrderUI(data){
+        const $saleTemplate = $(data['sale_template']);
+        if ($saleTemplate.length) {
+            this.$('#portal_sale_content').html($saleTemplate);
         }
     },
+
     /**
-     * Processes data from the server to update the UI
+     * Process the change in line quantity
      *
      * @private
-     * @param {Object} data: contains order and line updated values
+     * @param {Event} ev
      */
-    _updateOrderValues(data) {
-        let orderAmountTotal = data.order_amount_total,
-            orderAmountUntaxed = data.order_amount_untaxed,
-            orderAmountUndiscounted = data.order_amount_undiscounted,
-            $orderTotalsTable = $(data.order_totals_table);
-        if (orderAmountUntaxed !== undefined) {
-            this.elems.$orderAmountUntaxed.text(orderAmountUntaxed);
-        }
+    async _onChangeOptionQuantity(ev) {
+        ev.preventDefault();
+        let self = this,
+            $target = $(ev.currentTarget),
+            quantity = parseInt($target.val());
 
-        if (orderAmountTotal !== undefined) {
-            this.elems.$orderAmountTotal.text(orderAmountTotal);
-        }
-
-        if (orderAmountUndiscounted !== undefined) {
-            this.elems.$orderAmountUndiscounted.text(orderAmountUndiscounted);
-        }
-        if ($orderTotalsTable.length) {
-            this.elems.$orderTotalsTable.find('table').replaceWith($orderTotalsTable);
-        }
+        const result = await this._callUpdateLineRoute(self.orderDetail.orderId, {
+            'line_id': $target.data('lineId'),
+            'input_quantity': quantity >= 0 ? quantity : false,
+            'access_token': self.orderDetail.token
+        });
+        this._refreshOrderUI(result);
     },
+
     /**
-     * Locate in the DOM the elements to update
-     * Mostly for compatibility, when the module has not been upgraded
-     * In that case, we need to fall back to some other elements
+     * Reacts to the click on the -/+ buttons
      *
      * @private
-     * @return {Object}: Jquery elements to update
+     * @param {Event} ev
      */
-    _getUpdatableElements() {
-        let $orderAmountUntaxed = $('[data-id="total_untaxed"]').find('span, b'),
-            $orderAmountTotal = $('[data-id="total_amount"]').find('span, b'),
-            $orderAmountUndiscounted = $('[data-id="amount_undiscounted"]').find('span, b');
+    async _onClickOptionQuantityButton(ev) {
+        ev.preventDefault();
+        let self = this,
+            $target = $(ev.currentTarget);
 
-        if (!$orderAmountUntaxed.length) {
-            $orderAmountUntaxed = $orderAmountTotal.eq(1);
-            $orderAmountTotal = $orderAmountTotal.eq(0).add($orderAmountTotal.eq(2));
-        }
+        const result = await this._callUpdateLineRoute(self.orderDetail.orderId, {
+            'line_id': $target.data('lineId'),
+            'remove': $target.data('remove'),
+            'unlink': $target.data('unlink'),
+            'access_token': self.orderDetail.token
+        });
+        this._refreshOrderUI(result);
+    },
 
-        return {
-            $orderAmountUntaxed: $orderAmountUntaxed,
-            $orderAmountTotal: $orderAmountTotal,
-            $orderTotalsTable: $('#total'),
-            $orderAmountUndiscounted: $orderAmountUndiscounted,
-        };
-    }
+    /**
+     * Triggered when optional product added to order from portal.
+     *
+     * @private
+     * @param {Event} ev
+     */
+     _onClickAddOptionalProduct(ev) {
+        ev.preventDefault();
+        let self = this,
+            $target = $(ev.currentTarget);
+
+        // to avoid double click on link with href.
+        $target.css('pointer-events', 'none');
+
+        this._rpc({
+            route: "/my/orders/" + self.orderDetail.orderId + "/add_option/" + $target.data('optionId'),
+            params: {access_token: self.orderDetail.token}
+        }).then((data) => {
+            this._refreshOrderUI(data);
+        });
+    },
+
 });
 });
