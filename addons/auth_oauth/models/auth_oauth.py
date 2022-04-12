1c47547c22bd13969afdf697336561395fbbf82e
Matthew Sowden <549624+resba@users.noreply.github.com>
2022-04-12 17:06:53 -0400

    Add help text for non-conforming Oauth2 endpoints.
    Users with a passing or beginner knowledge of Oauth2 will have no idea which endpoints they need to put in the required fields and what extra work they need to do to prepare those endpoints to be accepted.

--- addons/auth_oauth/models/auth_oauth.py
+++ addons/auth_oauth/models/auth_oauth.py
@@ -13,9 +13,9 @@ class AuthOAuthProvider(models.Model):
 
     name = fields.Char(string='Provider name', required=True)  # Name of the OAuth2 entity, Google, etc
     client_id = fields.Char(string='Client ID')  # Our identifier
-    auth_endpoint = fields.Char(string='Authentication URL', required=True)  # OAuth provider URL to authenticate users
+    auth_endpoint = fields.Char(string='Authentication URL', required=True, help='OAuth2 Authentication Endpoint. Must support implicit login flow and token requests.')  # OAuth provider URL to authenticate users
     scope = fields.Char()  # OAUth user data desired to access
-    validation_endpoint = fields.Char(string='Validation URL', required=True)  # OAuth provider URL to validate tokens
+    validation_endpoint = fields.Char(string='Validation URL', required=True, help='User Information URL to validate token. Should have a user_id or id parameter mapped to the preferred_username attribute.')  # OAuth provider URL to validate tokens
     data_endpoint = fields.Char(string='Data URL')
     enabled = fields.Boolean(string='Allowed')
     css_class = fields.Char(string='CSS class', default='fa fa-fw fa-sign-in text-primary')
