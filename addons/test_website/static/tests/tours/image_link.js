dfe885f55d8f616bd65f4029a5b72cd51ff1a2c6
Benoit Socias <bso@odoo.com>
2022-04-12 09:26:06 +0200

    [FIX] web_editor, *: clean previously shown link in link popover
    *: web_tour, test_website
    
    Since [1] when the link popover was introduced, when the URL of a link
    was made empty, an validation error appeared by the previously shown URL
    did still appear inside the popover.
    
    After this commit the previously shown link is replaced by a message
    that indicates that no URL is specified.
    The telephone and envelope icons are now also always removed when the
    URL is change: they used to be only toggled, which made it possible to
    make them appear and disappear by updating an email address or a phone
    number.
    
    Steps to reproduce:
    - Add an image-text snippet to the page
    - Select the image
    - Add a link on the image
    - Specify URL
    - Click on image => link popup shows the entered URL
    - Make the URL field empty
    - Click on image
    => link popup still showed the previously entered URL
    
    [1]: https://github.com/odoo/odoo/commit/8fcf930a6b6b7ffb0965b0a689c7a3117962ce7e
    
    task-2765857

---
+++ addons/test_website/static/tests/tours/image_link.js
@@ -0,0 +1,84 @@
+/** @odoo-module **/
+
+import tour from 'web_tour.tour';
+
+/**
+ * The purpose of this tour is to check the link on image flow.
+ */
+
+tour.register('test_image_link', {
+    url: '/',
+    test: true
+}, [
+    {
+        content: "enter edit mode",
+        trigger: "a[data-action=edit]"
+    }, {
+        content: "drop Text-Image snippet",
+        trigger: "#oe_snippets .oe_snippet[name='Text - Image'] .oe_snippet_thumbnail:not(.o_we_already_dragging)",
+        extra_trigger: "body.editor_enable.editor_has_snippets",
+        moveTrigger: ".oe_drop_zone",
+        run: "drag_and_drop #wrap",
+    }, {
+        content: "select image",
+        trigger: "#wrapwrap .s_text_image img",
+    }, {
+        content: "enable link",
+        trigger: "#oe_snippets we-customizeblock-options:has(we-title:contains('Image')) we-customizeblock-option:has(we-title:contains(Media)) we-button.fa-link",
+    }, {
+        content: "enter site URL",
+        trigger: "#oe_snippets we-customizeblock-options:has(we-title:contains('Image')) we-input:contains(Your URL) input",
+        run: "text odoo.com",
+    }, {
+        content: "select image",
+        trigger: "#wrapwrap .s_text_image img",
+    }, {
+        content: "check popover content has site URL",
+        trigger: ".o_edit_menu_popover a.o_we_url_link[href='http://odoo.com/']:contains(http://odoo.com/)",
+        run: () => {}, // check
+    }, {
+        content: "remove URL",
+        trigger: "#oe_snippets we-customizeblock-options:has(we-title:contains('Image')) we-input:contains(Your URL) input",
+        run: "remove_text",
+    }, {
+        content: "select image",
+        trigger: "#wrapwrap .s_text_image img",
+    }, {
+        content: "check popover content has no URL",
+        trigger: ".o_edit_menu_popover a.o_we_url_link:not([href]):contains(No URL specified)",
+        run: () => {}, // check
+    }, {
+        content: "enter email URL",
+        trigger: "#oe_snippets we-customizeblock-options:has(we-title:contains('Image')) we-input:contains(Your URL) input",
+        run: "text mailto:test@test.com",
+    }, {
+        content: "select image",
+        trigger: "#wrapwrap .s_text_image img",
+    }, {
+        content: "check popover content has mail URL",
+        trigger: ".o_edit_menu_popover:has(.fa-envelope-o) a.o_we_url_link[href='mailto:test@test.com']:contains(mailto:test@test.com)",
+        run: () => {}, // check
+    }, {
+        content: "enter phone URL",
+        trigger: "#oe_snippets we-customizeblock-options:has(we-title:contains('Image')) we-input:contains(Your URL) input",
+        run: "text tel:555-2368",
+    }, {
+        content: "select image",
+        trigger: "#wrapwrap .s_text_image img",
+    }, {
+        content: "check popover content has phone URL",
+        trigger: ".o_edit_menu_popover:has(.fa-phone) a.o_we_url_link[href='tel:555-2368']:contains(tel:555-2368)",
+        run: () => {}, // check
+    }, {
+        content: "remove URL",
+        trigger: "#oe_snippets we-customizeblock-options:has(we-title:contains('Image')) we-input:contains(Your URL) input",
+        run: "remove_text",
+    }, {
+        content: "select image",
+        trigger: "#wrapwrap .s_text_image img",
+    }, {
+        content: "check popover content has no URL",
+        trigger: ".o_edit_menu_popover a.o_we_url_link:not([href]):contains(No URL specified)",
+        run: () => {}, // check
+    },
+]);
