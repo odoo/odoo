19cf89ac885575c8973e6078b03fe307115eb077
Alexandre Kühn <aku@odoo.com>
2022-04-13 18:42:23 +0200

    [IMP] mail: introduce NotificationRequest/handleResponseNotificationPermission
    Task-2579306

--- addons/mail/static/src/models/notification_request_view.js
+++ addons/mail/static/src/models/notification_request_view.js
@@ -6,6 +6,23 @@ import { one } from '@mail/model/model_field';
 registerModel({
     name: 'NotificationRequestView',
     identifyingFields: ['notificationListViewOwner'],
+    recordMethods: {
+        /**
+         * Handle the response of the user when prompted whether push notifications
+         * are granted or denied.
+         *
+         * @param {string} value
+         */
+        handleResponseNotificationPermission(value) {
+            this.messaging.refreshIsNotificationPermissionDefault();
+            if (value !== 'granted') {
+                this.env.services['bus_service'].sendNotification({
+                    message: this.env._t("Odoo will not have the permission to send native notifications on this device."),
+                    title: this.env._t("Permission denied"),
+                });
+            }
+        },
+    },
     fields: {
         notificationListViewOwner: one('NotificationListView', {
             inverse: 'notificationRequestView',
