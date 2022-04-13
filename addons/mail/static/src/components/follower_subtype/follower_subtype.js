48746ecf34239a87b8d8e75057788f25fe47bd3b
Alexandre Kühn <aku@odoo.com>
2022-04-13 16:51:59 +0200

    [IMP] mail: introduce FollowerSubtypeView/onChangeCheckbox
    Task-2579306

--- addons/mail/static/src/components/follower_subtype/follower_subtype.js
+++ addons/mail/static/src/components/follower_subtype/follower_subtype.js
@@ -17,24 +17,6 @@ export class FollowerSubtype extends Component {
         return this.messaging && this.messaging.models['FollowerSubtypeView'].get(this.props.localId);
     }
 
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
-
-    /**
-     * Called when clicking on cancel button.
-     *
-     * @private
-     * @param {Event} ev
-     */
-    _onChangeCheckbox(ev) {
-        if (ev.target.checked) {
-            this.followerSubtypeView.follower.selectSubtype(this.followerSubtypeView.subtype);
-        } else {
-            this.followerSubtypeView.follower.unselectSubtype(this.followerSubtypeView.subtype);
-        }
-    }
-
 }
 
 Object.assign(FollowerSubtype, {
