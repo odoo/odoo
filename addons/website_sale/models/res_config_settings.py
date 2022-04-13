d690f711839ec68069a8f349368b80286f53037f
Bastien Dosimont <doba@odoo.com>
2022-04-13 09:23:51 +0200

    [ADD] website_sale_autocomplete : Autocomplete the address fields in website using Google Places.
    task-id: 2806842
    PR : https://github.com/odoo/odoo/pull/87636
    
    - Implement an 'address.provider' structure, designed to be extendable and support multiple providers (not just google)
    - Add a module website_sale_autocompletion, that could be relocated without too much hassle
    - Add a module autocompletion_google_places thats adds google functionalities on top of the previous module

--- addons/website_sale/models/res_config_settings.py
+++ addons/website_sale/models/res_config_settings.py
@@ -27,6 +27,8 @@ class ResConfigSettings(models.TransientModel):
     module_website_sale_digital = fields.Boolean("Digital Content")
     module_website_sale_wishlist = fields.Boolean("Wishlists")
     module_website_sale_comparison = fields.Boolean("Product Comparison Tool")
+    module_website_sale_autocomplete = fields.Boolean('Address Autocomplete')
+
     module_account = fields.Boolean("Invoicing")
 
     cart_recovery_mail_template = fields.Many2one('mail.template', string='Cart Recovery Email', domain="[('model', '=', 'sale.order')]",
