a2b649bd322d21ee300c04eeaef177942e802fa5
Bastien Dosimont <doba@odoo.com>
2022-04-13 07:23:51 +0000

    [ADD] website_sale_autocomplete : Autocomplete the address fields in website using Google Places.
    task-id: 2806842
    PR : https://github.com/odoo/odoo/pull/87636
    
    - Implement an 'address.provider' structure, designed to be extendable and support multiple providers (not just google)
    - Add a module website_sale_autocompletion, that could be relocated without too much hassle
    - Add a module autocompletion_google_places thats adds google functionalities on top of the previous module
    
    closes odoo/odoo#88782
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ addons/website_sale_autocomplete/models/website.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import models, fields
+
+
+class Website(models.Model):
+    _inherit = 'website'
+
+    google_places_api_key = fields.Char(
+        string='Google Places API Key',
+        groups="base.group_system")
