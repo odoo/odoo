63d77aee69a1d2f56b5298ded90031fc67330ae4
Alexandre Kühn <aku@odoo.com>
2022-04-14 18:06:00 +0200

    [IMP] mail: introduce ActivityView/component
    Task-2579306

--- addons/mail/static/src/components/activity/activity.js
+++ addons/mail/static/src/components/activity/activity.js
@@ -1,11 +1,20 @@
 /** @odoo-module **/
 
+import { useComponentToModel } from '@mail/component_hooks/use_component_to_model';
 import { registerMessagingComponent } from '@mail/utils/messaging_component';
 
 import Popover from "web.Popover";
 import { LegacyComponent } from "@web/legacy/legacy_component";
 
 export class Activity extends LegacyComponent {
+
+    /**
+     * @override
+     */
+     setup() {
+        super.setup();
+        useComponentToModel({ fieldName: 'component', modelName: 'ActivityView' });
+    }
 
     /**
      * @returns {ActivityView}
