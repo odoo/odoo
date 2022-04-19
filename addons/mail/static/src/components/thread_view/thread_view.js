f75b7d19a85500bde8f052248f73756323a50711
Alexandre Kühn <aku@odoo.com>
2022-04-19 13:19:10 +0000

    [IMP] mail: turn props into model fields (step 1)
    This commit moves some business logic from components to models,
    as a step to move further to having essentially all business code in models.
    
    Having code in models is desirable to have very maintainable code, thanks to
    robust and declarative code with an ORM-like architecture.
    
    Task-2826823
    
    closes odoo/odoo#89323
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/components/thread_view/thread_view.js
+++ addons/mail/static/src/components/thread_view/thread_view.js
@@ -20,37 +20,12 @@ export class ThreadView extends Component {
 }
 
 Object.assign(ThreadView, {
-    defaultProps: {
-        hasComposerDiscardButton: false,
-        showComposerAttachmentsExtensions: true,
-        showComposerAttachmentsFilenames: true,
-    },
     props: {
-        hasComposerCurrentPartnerAvatar: {
-            type: Boolean,
-            optional: true,
-        },
-        hasComposerDiscardButton: {
-            type: Boolean,
-            optional: true,
-        },
-        hasComposerSendButton: {
-            type: Boolean,
-            optional: true,
-        },
         hasScrollAdjust: {
             type: Boolean,
             optional: true,
         },
         localId: String,
-        showComposerAttachmentsExtensions: {
-            type: Boolean,
-            optional: true,
-        },
-        showComposerAttachmentsFilenames: {
-            type: Boolean,
-            optional: true,
-        },
     },
     template: 'mail.ThreadView',
 });
---
+++ addons/mail/static/src/models/follow_button_view.js
@@ -0,0 +1,58 @@
+/** @odoo-module **/
+
+import { registerModel } from '@mail/model/model_core';
+import { attr, one } from '@mail/model/model_field';
+import { clear } from '@mail/model/model_field_command';
+
+registerModel({
+    name: 'FollowButtonView',
+    identifyingFields: [['chatterOwner']],
+    recordMethods: {
+        /**
+         * @private
+         * @returns {boolean|FieldCommand}
+         */
+        _computeIsDisabled() {
+            if (!this.chatterOwner) {
+                return clear();
+            }
+            return this.chatterOwner.isDisabled;
+        },
+        /**
+         * @param {MouseEvent} ev 
+         */
+        onClickFollow(ev) {
+            if (!this.exists()) {
+                return;
+            }
+            if (!this.chatterOwner || !this.chatterOwner.thread) {
+                return;
+            }
+            this.chatterOwner.thread.follow();
+        },
+        /**
+         * @param {MouseEvent} ev
+         */
+        onClickUnfollow(ev) {
+            if (!this.exists()) {
+                return;
+            }
+            if (!this.chatterOwner || !this.chatterOwner.thread) {
+                return;
+            }
+            this.chatterOwner.thread.unfollow();
+        },
+    },
+    fields: {
+        chatterOwner: one('Chatter', {
+            inverse: 'followButtonView',
+            readonly: true,
+        }),
+        isDisabled: attr({
+            compute: '_computeIsDisabled',
+        }),
+        isUnfollowButtonHighlighted: attr({
+            default: false,
+        }),
+    },
+});
