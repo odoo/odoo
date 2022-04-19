02c8fd7f9599f3582ce0769f74c41d082bc5c73e
Donnie <shda@odoo.com>
2022-04-19 14:06:46 +0430

    [IMP] website_sale: exclude the excluded variants from attribute search
    Filter product templates search result if there are any possible attribute
    combination to get/create a product variant regarding the searched attributes.
    
    For example if the combination (A, B) is excluded, filtering on (A, B)
    will filter that product out of the result.
    
    task-2558970

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from collections import defaultdict
+from itertools import product as cartesian_product
 import json
 import logging
 from datetime import datetime
@@ -296,6 +298,53 @@ class WebsiteSale(http.Controller):
         product_count, details, fuzzy_search_term = website._search_with_fuzzy("products_only", search,
             limit=None, order=self._get_search_order(post), options=options)
         search_product = details[0].get('results', request.env['product.template']).with_context(bin_size=True)
+
+        if attrib_set:
+            # Attributes value per attribute
+            attribute_values = request.env['product.attribute.value'].browse(attrib_set)
+            values_per_attribute = defaultdict(lambda: request.env['product.attribute.value'])
+            # In case we have only one value per attribute we can check for a combination using those attributes directly
+            multi_value_attribute = False
+            for value in attribute_values:
+                values_per_attribute[value.attribute_id] |= value
+                if len(values_per_attribute[value.attribute_id]) > 1:
+                    multi_value_attribute = True
+
+            def filter_template(template, attribute_values_list):
+                # Transform product.attribute.value to product.template.attribute.value
+                attribute_value_to_ptav = dict()
+                for ptav in template.attribute_line_ids.product_template_value_ids:
+                    attribute_value_to_ptav[ptav.product_attribute_value_id] = ptav.id
+                possible_combinations = False
+                for attribute_values in attribute_values_list:
+                    ptavs = request.env['product.template.attribute.value'].browse(
+                        [attribute_value_to_ptav[val] for val in attribute_values if val in attribute_value_to_ptav]
+                    )
+                    if len(ptavs) < len(attribute_values):
+                        # In this case the template is not compatible with this specific combination
+                        continue
+                    if len(ptavs) == len(template.attribute_line_ids):
+                        if template._is_combination_possible(ptavs):
+                            return True
+                    elif len(ptavs) < len(template.attribute_line_ids):
+                        if len(attribute_values_list) == 1:
+                            if any(template._get_possible_combinations(necessary_values=ptavs)):
+                                return True
+                        if not possible_combinations:
+                            possible_combinations = template._get_possible_combinations()
+                        if any(len(ptavs & combination) == len(ptavs) for combination in possible_combinations):
+                            return True
+                return False
+
+            # If multi_value_attribute is False we know that we have our final combination (or at least a subset of it)
+            if not multi_value_attribute:
+                possible_attrib_values_list = [attribute_values]
+            else:
+                # Cartesian product from dict keys and values
+                possible_attrib_values_list = [request.env['product.attribute.value'].browse([v.id for v in values]) for
+                                               values in cartesian_product(*values_per_attribute.values())]
+
+            search_product = search_product.filtered(lambda tmpl: filter_template(tmpl, possible_attrib_values_list))
 
         filter_by_price_enabled = website.is_view_active('website_sale.filter_products_price')
         if filter_by_price_enabled:
