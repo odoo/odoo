e66bcd64ad4ada276affdb82c1a0513c44e1b24a
Guillaume (gdi) <gdi@odoo.com>
2022-04-19 13:08:33 +0000

    [FIX] web_editor: debounce we-range events
    This commit allows to debounce the events of the RangeUserValueWidget so
    that the user can use this widget with the left and right arrows without
    having a lag effect (especially on the image quality option).
    
    task-2601533
    
    closes odoo/odoo#108850
    
    X-original-commit: e64d04568b8aefc4bbcdb6f7e4d2011461df824d
    Signed-off-by: Bojabza Soukéina (sobo) <sobo@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -2267,6 +2267,8 @@ const RangeUserValueWidget = UnitUserValueWidget.extend({
         }
         this._setInputAttributes(min, max, step);
         this.containerEl.appendChild(this.input);
+
+        this._onInputChange = _.debounce(this._onInputChange, 100);
     },
 
     //--------------------------------------------------------------------------
