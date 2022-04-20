1e47e8a9d8c6ceed69bb27e1ab12defc03e46133
Alexandre Kühn <aku@odoo.com>
2022-04-20 17:30:49 +0200

    [IMP] mail: introduce FollowButtonView/onClickUnfollow
    Task-2826823

--- addons/mail/static/src/models/follow_button_view.js
+++ addons/mail/static/src/models/follow_button_view.js
@@ -6,6 +6,20 @@ import { one } from '@mail/model/model_field';
 registerModel({
     name: 'FollowButtonView',
     identifyingFields: [['chatterOwner']],
+    recordMethods: {
+        /**
+         * @param {MouseEvent} ev
+         */
+        onClickUnfollow(ev) {
+            if (!this.exists()) {
+                return;
+            }
+            if (!this.chatterOwner.thread) {
+                return;
+            }
+            this.chatterOwner.thread.onClickUnfollow(ev);
+        },
+    },
     fields: {
         chatterOwner: one('Chatter', {
             inverse: 'followButtonView',
