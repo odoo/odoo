5a7ed734e58095af3495787c662e7f43beffbd85
Gorash <chm@odoo.com>
2022-04-20 08:33:11 +0000

    [IMP] base: add a cache for company_ids on res.user
    Part-of: odoo/odoo#88276

--- odoo/api.py
+++ odoo/api.py
@@ -630,7 +630,7 @@ class Environment(Mapping):
         company_ids = self.context.get('allowed_company_ids', [])
         if company_ids:
             if not self.su:
-                user_company_ids = self.user.company_ids.ids
+                user_company_ids = self.user._get_company_ids()
                 if any(cid not in user_company_ids for cid in company_ids):
                     raise AccessError(_("Access to unauthorized or invalid companies."))
             return self['res.company'].browse(company_ids[0])
@@ -660,7 +660,7 @@ class Environment(Mapping):
         company_ids = self.context.get('allowed_company_ids', [])
         if company_ids:
             if not self.su:
-                user_company_ids = self.user.company_ids.ids
+                user_company_ids = self.user._get_company_ids()
                 if any(cid not in user_company_ids for cid in company_ids):
                     raise AccessError(_("Access to unauthorized or invalid companies."))
             return self['res.company'].browse(company_ids)
