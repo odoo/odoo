boan-odoo/odoo-1

[REF] base,account: Refactor PDF generation to allow CH QR-bills
For the Switzerland localization, we need to append some extra pages during the PDF generation such as QR barcodes or ISR payment slip.
There were multiple difficulties with the current codes:
- The ir.attachment was generated before the hook allowing to append the extra pages.
- There is no existing hook allowing to get the pages for each invoice when printing on multiple records at once.

This commit aims to dispatch the generation of PDFs from the creating of extra attachments:
- `_render_qweb_pdf_prepare_streams` is called first and creates a stream for each record containing the PDF pages.
- Then, the ir.attachment are generated if necessary.
- Finally, all streams are aggregated together to get the final PDF.

task: 2726507

addons/account_edi/models/ir_actions_report.py
@@ -9,24 +9,36 @@ from odoo.tools.pdf import OdooPdfFileReader, OdooPdfFileWriter
 class IrActionsReport(models.Model):
     _inherit = 'ir.actions.report'
 
-    def _post_pdf(self, save_in_attachment, pdf_content=None, res_ids=None):
-        # OVERRIDE to embed some EDI documents inside the PDF.
-        if self.model == 'account.move' and res_ids and len(res_ids) == 1 and pdf_content:
+    def _render_qweb_pdf_prepare_streams(self, data, res_ids=None):
+        # OVERRIDE
+        collected_streams = super()._render_qweb_pdf_prepare_streams(data, res_ids=res_ids)
+
+        if collected_streams \
+                and len(collected_streams) == 1 \
+                and self.report_name in ('account.report_invoice_with_payments', 'account.report_invoice') \
+                and res_ids:
             invoice = self.env['account.move'].browse(res_ids)
             if invoice.is_sale_document() and invoice.state != 'draft':
                 to_embed = invoice.edi_document_ids
                 # Add the attachments to the pdf file
                 if to_embed:
+                    pdf_stream = collected_streams[invoice.id]['stream']
+
+                    # Read pdf content.
+                    pdf_content = pdf_stream.getvalue()
                     reader_buffer = io.BytesIO(pdf_content)
                     reader = OdooPdfFileReader(reader_buffer, strict=False)
+
+                    # Post-process and embed the additional files.
                     writer = OdooPdfFileWriter()
                     writer.cloneReaderDocumentRoot(reader)
                     for edi_document in to_embed:
                         edi_document.edi_format_id._prepare_invoice_report(writer, edi_document)
-                    buffer = io.BytesIO()
-                    writer.write(buffer)
-                    pdf_content = buffer.getvalue()
-                    reader_buffer.close()
-                    buffer.close()
 
-        return super(IrActionsReport, self)._post_pdf(save_in_attachment, pdf_content=pdf_content, res_ids=res_ids)
+                    # Replace the current content.
+                    pdf_stream.close()
+                    new_pdf_stream = io.BytesIO()
+                    writer.write(new_pdf_stream)
+                    collected_streams[invoice.id]['stream'] = new_pdf_stream
+
+        return collected_streams

odoo/odoo

addons/account_edi/models/ir_actions_report.py
@@ -9,24 +9,36 @@ from odoo.tools.pdf import OdooPdfFileReader, OdooPdfFileWriter
 class IrActionsReport(models.Model):
     _inherit = 'ir.actions.report'
 
-    def _post_pdf(self, save_in_attachment, pdf_content=None, res_ids=None):
-        # OVERRIDE to embed some EDI documents inside the PDF.
-        if self.model == 'account.move' and res_ids and len(res_ids) == 1 and pdf_content:
+    def _render_qweb_pdf_prepare_streams(self, data, res_ids=None):
+        # EXTENDS base
+        collected_streams = super()._render_qweb_pdf_prepare_streams(data, res_ids=res_ids)
+
+        if collected_streams \
+                and res_ids \
+                and len(res_ids) == 1 \
+                and self.report_name in ('account.report_invoice_with_payments', 'account.report_invoice'):
             invoice = self.env['account.move'].browse(res_ids)
             if invoice.is_sale_document() and invoice.state != 'draft':
                 to_embed = invoice.edi_document_ids
                 # Add the attachments to the pdf file
                 if to_embed:
+                    pdf_stream = collected_streams[invoice.id]['stream']
+
+                    # Read pdf content.
+                    pdf_content = pdf_stream.getvalue()
                     reader_buffer = io.BytesIO(pdf_content)
                     reader = OdooPdfFileReader(reader_buffer, strict=False)
+
+                    # Post-process and embed the additional files.
                     writer = OdooPdfFileWriter()
                     writer.cloneReaderDocumentRoot(reader)
                     for edi_document in to_embed:
                         edi_document.edi_format_id._prepare_invoice_report(writer, edi_document)
-                    buffer = io.BytesIO()
-                    writer.write(buffer)
-                    pdf_content = buffer.getvalue()
-                    reader_buffer.close()
-                    buffer.close()
 
-        return super(IrActionsReport, self)._post_pdf(save_in_attachment, pdf_content=pdf_content, res_ids=res_ids)
+                    # Replace the current content.
+                    pdf_stream.close()
+                    new_pdf_stream = io.BytesIO()
+                    writer.write(new_pdf_stream)
+                    collected_streams[invoice.id]['stream'] = new_pdf_stream
+
+        return collected_streams