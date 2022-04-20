ce6e947e3b5ee0eef0d0e87af78e1ffe26ec71db
Arnold Moyaux <arm@odoo.com>
2022-04-20 08:39:36 +0000

    [FIX] mrp_subcontracting: only add transfer in landed costs views
    Currently the linked receipt (in case of subcontracting)
    is visible in the tree view and search view of every MO.
    However we want to limit the feature to landed cost scope
    since it's only useful in that case and it adds noise otherwise.
    
    We need a new module since we don't have any bridge between
    subcontracting and landed costs
    
    closes odoo/odoo#89399
    
    Signed-off-by: Steve Van Essche <svs@odoo.com>

---
+++ addons/mrp_subonctracting_landed_costs/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Landed Costs With Subcontracting order',
+    'version': '1.0',
+    'summary': 'Advanced views to manage landed cost for subcontracting orders',
+    'description': """
+This module allows users to more easily identify subcontracting orders when applying landed costs,
+by also displaying the associated picking reference in the search view.
+    """,
+    'depends': ['stock_landed_costs', 'mrp_subcontracting'],
+    'category': 'Manufacturing/Manufacturing',
+    'data': [
+        'views/stock_landed_cost_views.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
