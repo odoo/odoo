09dcc070a1b8671e7ce72dd8f54ec39504a0cefa
Ivan Yelizariev <iel@odoo.com>
2022-04-21 14:56:58 +0000

    [FIX] core: fix infinite loops with child_of/parent_of
    1.
    
    Infinite loop may happen on using `parent_of`\`child_of` when there is a
    recursion in the tree (e.g. a record is marked as a parent of itself). Fix it by
    excluding seen records from the next iteration.
    
    2.
    
    Another problem with `child_of` is `parent_id` that references to another model.
    For example, the `parent_id` may come from inherited model. It's the case with
    `res.users` and `res.partner` models. It may lead to a random search results.
    Avoid that by raising exception in case of wrong usage of the `child_of`
    operator.
    
    STEPS:
    
    In demo data, there is a partner called "Wood Corner" that is `res.partner(9,)`
    that has 3 sub-contacts. If we give Portal access to two of them, we end up with
    a database, where we have a `res.users(9,)` record that has a partner, which has a
    `parent_id` to "Wood corner". So this way, the user id is the same as the user's
    partner's parent contact id.
    
    After that open a shell and type:
    
    ```
    env['res.partner'].search([["user_ids", "child_of", 9]])
    ```
    
    BEFORE: infinite loop (without change n.1) or random search results (when change
    n.1 is applied)
    AFTER: ValueError exception
    
    ---
    
    opw-2729740
    
    X-original-commit: a7352c644f5f12ca075546bd57912f849f9e20f2

--- odoo/addons/test_new_api/models/test_new_api.py
+++ odoo/addons/test_new_api/models/test_new_api.py
@@ -1777,3 +1777,21 @@ class UnlinkLine(models.Model):
 
     container_id = fields.Many2one('test_new_api.unlink.container')
     container_name = fields.Char('Container Name', related='container_id.name', store=True)
+
+
+class Team(models.Model):
+    _name = 'test_new_api.team'
+    _description = 'Odoo Team'
+
+    name = fields.Char()
+    parent_id = fields.Many2one('test_new_api.team')
+    member_ids = fields.One2many('test_new_api.team.member', 'team_id')
+
+
+class TeamMember(models.Model):
+    _name = 'test_new_api.team.member'
+    _description = 'Odoo Developer'
+
+    name = fields.Char('Name')
+    team_id = fields.Many2one('test_new_api.team')
+    parent_id = fields.Many2one('test_new_api.team', related='team_id.parent_id')
