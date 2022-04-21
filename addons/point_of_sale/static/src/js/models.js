c56b1e618e155c553fb6f766c7b87ee16e5d78dc
Jacky (trj) <trj@odoo.com>
2022-04-21 13:28:11 +0000

    [FIX] point_of_sale,pos_{sale,gift_card}: product taken in total discount
    When showing the discount the to the public, the down payment and gift card products were taken into account in the discount computation shown in the receipt.
    
    This also fix a mistake made in fc546e0481ae0159e67791240833bc1e38a4440f where it was implemented in the wrong place and returning a wrong value in a reduce callback.
    
    closes odoo/odoo#89603
    
    X-original-commit: 597ea7aa3f28ad82e602e1ba5679bb49d2973b78
    Signed-off-by: Masereel Pierre <pim@odoo.com>
    Signed-off-by: Trinh Jacky (trj) <trj@odoo.com>

--- addons/point_of_sale/static/src/js/models.js
+++ addons/point_of_sale/static/src/js/models.js
@@ -2722,17 +2722,20 @@ class Order extends PosModel {
             return sum + orderLine.get_price_without_tax();
         }), 0), this.pos.currency.rounding);
     }
+    _get_ignored_product_ids_total_discount() {
+        return [];
+    }
     get_total_discount() {
-        return round_pr(this.orderlines.reduce((function(sum, orderLine) {
-            if (orderLine.pos.config.down_payment_product_id[0] === orderLine.product.id) {
-                return 0;
-            }
-            sum += (orderLine.get_unit_price() * (orderLine.get_discount()/100) * orderLine.get_quantity());
-            if (orderLine.display_discount_policy() === 'without_discount'){
-                sum += ((orderLine.get_lst_price() - orderLine.get_unit_price()) * orderLine.get_quantity());
+        const ignored_product_ids = this._get_ignored_product_ids_total_discount()
+        return round_pr(this.orderlines.reduce((sum, orderLine) => {
+            if (!ignored_product_ids.includes(orderLine.product.id)) {
+                sum += (orderLine.get_unit_price() * (orderLine.get_discount()/100) * orderLine.get_quantity());
+                if (orderLine.display_discount_policy() === 'without_discount'){
+                    sum += ((orderLine.get_lst_price() - orderLine.get_unit_price()) * orderLine.get_quantity());
+                }
             }
             return sum;
-        }), 0), this.pos.currency.rounding);
+        }, 0), this.pos.currency.rounding);
     }
     get_total_tax() {
         if (this.pos.company.tax_calculation_rounding_method === "round_globally") {
