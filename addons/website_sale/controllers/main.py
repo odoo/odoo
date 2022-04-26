29b761a4bbd75f5f0707780a999a7318497d2099
William Braeckman <wbr@odoo.com>
2022-04-26 12:55:16 +0000

    [FIX] website,website_sale: fix user sign up config
    This commit aims to fix multiple issues with the "Customer Account"
    parameter in the backend config.
    
     - Hide the parameter from general and sales settings if website is
       installed.
     - Properly compute the right value once website is installed, as it was
       not using the website's value.
     - Fix a traceback when clicking on "Default Access Rights".
    
    TaskId-2825096
    
    closes odoo/odoo#89806
    
    X-original-commit: 8b162f507f881619061d61530cc03b4b4654c1fe
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -876,6 +876,7 @@ class WebsiteSale(http.Controller):
             'callback': kw.get('callback'),
             'only_services': order and order.only_services,
             'account_on_checkout': request.website.account_on_checkout,
+            'is_public_user': request.website.is_public_user()
         }
         render_values.update(self._get_country_related_render_values(kw, render_values))
         return request.render("website_sale.address", render_values)
