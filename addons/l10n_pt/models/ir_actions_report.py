312d0a46d01e27f14b43a34eda5457ad9fad5ebd
Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>
2022-04-26 17:15:44 +0200

    [WIP] l10n_pt: new way of spliting main table into multiple tables (one per page)

--- addons/l10n_pt/models/ir_actions_report.py
+++ addons/l10n_pt/models/ir_actions_report.py
@@ -9,73 +9,78 @@ class IrActionsReport(models.Model):
         if account_move.company_id.country_id.code != 'PT':
             return super()._render_qweb_pdf(res_ids=res_ids, data=data, run_script=run_script)
         run_script = """
-            var rows = document.querySelectorAll('tr');
-            var amounts = document.querySelectorAll('span.oe_currency_value');
+            var rows = document.querySelectorAll("tr");
+            var amounts = document.querySelectorAll("span.oe_currency_value");
+            const theader = document.querySelector("thead")
             
             //For each row, add the distance between beginning and the bottom of the row as an attribute
             for (var i = 1; i < rows.length; i++) {
                 var row = rows[i];
                 var bottom = row.getBoundingClientRect().bottom;
-                row.setAttribute('data-bottom', bottom);
-                var first_child = row.querySelector("td");
-                if (first_child != undefined){  
-                    first_child.innerText += " (b: " + bottom + ")";
-                }
+                row.setAttribute("data-bottom", bottom);
             }
             
             const thead_bottom = document.querySelector("thead").getBoundingClientRect().bottom;
-            document.querySelector("thead").style.display = "table-row-group";
             const nb_columns = document.querySelectorAll("th").length;
             const first_page_height = 1000;
             const page_height = 2000;
             var carrying = 0.0;
             var nb_pages = 0;
             
-            //Split table in pages and add the carryover
+            //Split table in pages
+            tables = [{
+                page: 0,
+                rows: [],
+                to_carry: 0.0,
+                last_bottom: 0,
+            }]
             for (var i = 1; i < rows.length; i++) {
                 var row = rows[i];
-                var bottom = row.getAttribute('data-bottom');
-                var amount = "0";
-                if (amounts[i-1] != undefined){ //i-1because we skip the header
-                    amount = amounts[i-1].innerText;
-                }
-                carrying += parseFloat(amount.split(",").join(""));
-                if ( (nb_pages == 0 && bottom > first_page_height + thead_bottom) ||
-                     (nb_pages > 0  && bottom - nb_pages * page_height > page_height + first_page_height) ) { 
+                const bottom = row.getAttribute("data-bottom");
+                if (amounts[i - 1] === undefined) //i-1 because we skip the header
+                    continue
+                tables[tables.length-1].to_carry += parseFloat(amounts[i - 1].innerText.split(",").join(""));
+                tables[tables.length-1].rows.push(i);
+                tables[tables.length-1].last_bottom = bottom;
+                if ((nb_pages == 0 && bottom > first_page_height + thead_bottom) ||
+                    (nb_pages > 0 && bottom - nb_pages * page_height > page_height + first_page_height)) {
                     nb_pages += 1;
-                    td_repeat = ''
-                    for (var j = 0; j < nb_columns-2; j++) {
-                        td_repeat += '<td> </td>';
-                    }
-                    const carrying_text = carrying.toFixed(2) + '&euro;';
-                    const html = '' +
-                                '<tr class="text-right font-weight-bold">' +
-                                    td_repeat +
-                                    '<td> Carrying: </td>' +
-                                    '<td>' + carrying_text + '</td>' +
-                                '</tr>' +
-                            '</tbody>' + 
-                        '</table>' +
-                        '<div style="page-break-before: always; display: block; background-color:red;">--BREAK--</div>' +
-                        '<table>' +
-                            '<thead>' +
-                                '<tr>' +
-                                    '<th>Description2</th>' +
-                                    '<th>Quantity2</th>' +
-                                    '<th>Unit price2</th>' +
-                                    '<th>Taxes2</th>' + 
-                                    '<th>Amount2</th>' +
-                                '</tr>' +
-                            '</thead>' +
-                            '<tbody>' +
-                                '<tr class="text-right font-weight-bold">' +
-                                    td_repeat +
-                                    '<td> Carried: </td>' +
-                                    '<td>' + carrying_text + '</td>' +
-                                '</tr>'
-                    row.insertAdjacentHTML('afterend',  html);
+                    tables.push({
+                        page: nb_pages,
+                        rows: [],
+                        to_carry: tables[tables.length-1].to_carry,
+                        last_bottom: 0,
+                    })
                 }
             }
-            rows[rows.length-1].insertAdjacentHTML('afterend', '</tbody></table>');
+            console.log(JSON.stringify(tables));
+            const mainTable = document.querySelector("table");
+            mainTable.parentNode.removeChild(mainTable);
+
+            
+            // Create the new tables
+            for (var i = 0; i < tables.length; i++){
+                console.log("Creating table " + JSON.stringify(tables[i]));
+                var html = "";
+                if (i != 0) 
+                    html += "<div class='muted text-right'>Carried:" + tables[i-1].to_carry + " </div>";
+                html += "<table class='table table-sm o_main_table' name='invoice_line_table'>";
+                html +=     theader.outerHTML;
+                html += "   <tobdy>";
+                for (var j = 0; j < tables[i].rows.length; j++){
+                    const row = rows[tables[i].rows[j]];
+                    html += row.outerHTML;
+                }
+                html += "   </tbody>";
+                html += "</table>";
+                if (i != tables.length - 1) {
+                    html += "<div class='muted text-right'>Carrying:" + tables[i].to_carry + " </div>";
+                    html += "<div style='page-break-after: always; background-color: red;'>--BREAK--</div>";
+                }
+                const sibling = document.getElementById("total").parentNode;
+                sibling.insertAdjacentHTML("beforebegin", html);
+            }
+
+            //console.log(document.body.innerHTML);
         """
         return super()._render_qweb_pdf(res_ids=res_ids, data=data, run_script=run_script)
