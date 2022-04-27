2482ce77d11cc6db35ceb5b85d8f1fae63210952
Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>
2022-04-27 10:56:50 +0200

    [WIP] l10n_pt: many improvements

--- addons/l10n_pt/models/ir_actions_report.py
+++ addons/l10n_pt/models/ir_actions_report.py
@@ -9,9 +9,14 @@ class IrActionsReport(models.Model):
         if account_move.company_id.country_id.code != 'PT':
             return super()._render_qweb_pdf(res_ids=res_ids, data=data, run_script=run_script)
         run_script = """
+            // Get all necessary elements and data
             var rows = document.querySelectorAll("tr");
             var amounts = document.querySelectorAll("span.oe_currency_value");
+            
             const theader = document.querySelector("thead")
+            const theadBottom = document.querySelector("thead").getBoundingClientRect().bottom;
+            const firstPageHeight = 1000;
+            const pageHeight = 2000;
             
             //For each row, add the distance between beginning and the bottom of the row as an attribute
             for (var i = 1; i < rows.length; i++) {
@@ -20,19 +25,11 @@ class IrActionsReport(models.Model):
                 row.setAttribute("data-bottom", bottom);
             }
             
-            const thead_bottom = document.querySelector("thead").getBoundingClientRect().bottom;
-            const nb_columns = document.querySelectorAll("th").length;
-            const first_page_height = 1000;
-            const page_height = 2000;
-            var carrying = 0.0;
-            var nb_pages = 0;
-            
-            //Split table in pages
+            //Split main table in multiple smaller tables
             tables = [{
                 page: 0,
                 rows: [],
                 to_carry: 0.0,
-                last_bottom: 0,
             }]
             for (var i = 1; i < rows.length; i++) {
                 var row = rows[i];
@@ -41,29 +38,32 @@ class IrActionsReport(models.Model):
                     continue
                 tables[tables.length-1].to_carry += parseFloat(amounts[i - 1].innerText.split(",").join(""));
                 tables[tables.length-1].rows.push(i);
-                tables[tables.length-1].last_bottom = bottom;
-                if ((nb_pages == 0 && bottom > first_page_height + thead_bottom) ||
-                    (nb_pages > 0 && bottom - nb_pages * page_height > page_height + first_page_height)) {
-                    nb_pages += 1;
+                if ((tables.length == 1 && bottom > firstPageHeight + theadBottom) || //First page
+                    (tables.length > 1 && bottom - (tables.length-1) * pageHeight > pageHeight + firstPageHeight)) { //Other pages
                     tables.push({
-                        page: nb_pages,
+                        page: tables.length,
                         rows: [],
                         to_carry: tables[tables.length-1].to_carry,
-                        last_bottom: 0,
                     })
                 }
             }
-            console.log(JSON.stringify(tables));
+            
+            // Delete the old mainTable
             const mainTable = document.querySelector("table");
             mainTable.parentNode.removeChild(mainTable);
-
+            
+            function carryValueElement(amount) {
+                return "<div class='text-bold text-right'>" + 
+                            "Valor acumulado: " + amount.toFixed(2) + "&euro;" + 
+                       "</div>"
+            }
             
             // Create the new tables
+            console.log(JSON.stringify(tables));
             for (var i = 0; i < tables.length; i++){
-                console.log("Creating table " + JSON.stringify(tables[i]));
                 var html = "";
                 if (i != 0) 
-                    html += "<div class='muted text-right'>Carried:" + tables[i-1].to_carry + " </div>";
+                    html += carryValueElement(tables[i-1].to_carry);
                 html += "<table class='table table-sm o_main_table' name='invoice_line_table'>";
                 html +=     theader.outerHTML;
                 html += "   <tobdy>";
@@ -74,11 +74,11 @@ class IrActionsReport(models.Model):
                 html += "   </tbody>";
                 html += "</table>";
                 if (i != tables.length - 1) {
-                    html += "<div class='muted text-right'>Carrying:" + tables[i].to_carry + " </div>";
-                    html += "<div style='page-break-after: always; background-color: red;'>--BREAK--</div>";
+                    html += carryValueElement(tables[i].to_carry);
+                    html += "<div style='page-break-after: always;'/>";
                 }
-                const sibling = document.getElementById("total").parentNode;
-                sibling.insertAdjacentHTML("beforebegin", html);
+                const totalElement = document.getElementById("total").parentNode;
+                totalElement.insertAdjacentHTML("beforebegin", html);
             }
 
             //console.log(document.body.innerHTML);
