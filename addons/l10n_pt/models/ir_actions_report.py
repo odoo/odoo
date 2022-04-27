588a8bfc8fd4c6c4e8ddc68d32a16b96a26fb0ac
Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>
2022-04-27 12:34:25 +0200

    [WIP] l10n_pt: some cleaning

--- addons/l10n_pt/models/ir_actions_report.py
+++ addons/l10n_pt/models/ir_actions_report.py
@@ -9,15 +9,20 @@ class IrActionsReport(models.Model):
         if account_move.company_id.country_id.code != 'PT':
             return super()._render_qweb_pdf(res_ids=res_ids, data=data, run_script=run_script)
         run_script = """
-            // Get all necessary elements and data
-            var rows = document.querySelectorAll("tr");
-            var amounts = document.querySelectorAll("span.oe_currency_value");
+            // Table elements
+            const tableElement = document.querySelector("table");
+            const theadElement = document.querySelector("thead")
+            const theadBottom = theadElement.getBoundingClientRect().bottom;
+            const totalElement = document.getElementById("total").parentNode;
             
-            const theader = document.querySelector("thead")
-            const theadBottom = document.querySelector("thead").getBoundingClientRect().bottom;
+            // Constants
             const firstPageHeight = 1000;
             const pageHeight = 2000;
             
+            // Rows and amounts
+            const rows = document.querySelectorAll("tr");
+            const amounts = document.querySelectorAll("span.oe_currency_value");
+
             //For each row, add the distance between beginning and the bottom of the row as an attribute
             for (var i = 1; i < rows.length; i++) {
                 var row = rows[i];
@@ -30,6 +35,7 @@ class IrActionsReport(models.Model):
                 page: 0,
                 rows: [],
                 to_carry: 0.0,
+                bottoms: [],
             }]
             for (var i = 1; i < rows.length; i++) {
                 var row = rows[i];
@@ -38,19 +44,20 @@ class IrActionsReport(models.Model):
                     continue
                 tables[tables.length-1].to_carry += parseFloat(amounts[i - 1].innerText.split(",").join(""));
                 tables[tables.length-1].rows.push(i);
+                tables[tables.length-1].bottoms.push(bottom);
                 if ((tables.length == 1 && bottom > firstPageHeight + theadBottom) || //First page
                     (tables.length > 1 && bottom - (tables.length-1) * pageHeight > pageHeight + firstPageHeight)) { //Other pages
                     tables.push({
                         page: tables.length,
                         rows: [],
                         to_carry: tables[tables.length-1].to_carry,
+                        bottoms: [],
                     })
                 }
             }
             
             // Delete the old mainTable
-            const mainTable = document.querySelector("table");
-            mainTable.parentNode.removeChild(mainTable);
+            tableElement.parentNode.removeChild(tableElement);
             
             function carryValueElement(amount) {
                 return "<div class='text-bold text-right'>" + 
@@ -65,7 +72,7 @@ class IrActionsReport(models.Model):
                 if (i != 0) 
                     html += carryValueElement(tables[i-1].to_carry);
                 html += "<table class='table table-sm o_main_table' name='invoice_line_table'>";
-                html +=     theader.outerHTML;
+                html +=     theadElement.outerHTML;
                 html += "   <tobdy>";
                 for (var j = 0; j < tables[i].rows.length; j++){
                     const row = rows[tables[i].rows[j]];
@@ -77,7 +84,6 @@ class IrActionsReport(models.Model):
                     html += carryValueElement(tables[i].to_carry);
                     html += "<div style='page-break-after: always;'/>";
                 }
-                const totalElement = document.getElementById("total").parentNode;
                 totalElement.insertAdjacentHTML("beforebegin", html);
             }
 
