9e913e9bfad679d5ccb742de2b204c01b3481a77
Florian Damhaut <flda@odoo.com>
2022-04-27 14:55:24 +0000

    [FIX] project: Copy task dependency on project copy
    Step to reproduce:
    - Create a project with 2 task A and B
    - Set task A blocked by task B
    - Duplicate Project
    
    Current behaviour:
    - Duplicate project is also blocked by original project's task
    - Original project's tasks are blocked by new duplicate project's
     task
    
    Behaviour after PR:
    - Original project's tasks are not changed
    - Duplicate project's tasks are blocked by duplicate project's task
     if they were blocked by the original project.
    - Tasks are still blocked by tasks not linked to the original
     project
    
    opw-2818476
    
    closes odoo/odoo#90234
    
    X-original-commit: 5c184628b2278814d2b28366587c8e4b813d0374
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- addons/project/tests/test_task_dependencies.py
+++ addons/project/tests/test_task_dependencies.py
@@ -159,3 +159,74 @@ class TestTaskDependencies(TestProjectCommon):
             'name': 'My Ducks Project'
         })
         self.assertFalse(self.project_ducks.allow_task_dependencies, "New Projects allow_task_dependencies should default to group_project_task_dependencies")
+
+    def test_duplicate_project_with_task_dependencies(self):
+        self.project_pigs.allow_task_dependencies = True
+        self.task_1.depend_on_ids = self.task_2
+        pigs_copy = self.project_pigs.copy()
+
+        task1_copy = pigs_copy.task_ids.filtered(lambda t: t.name == 'Pigs UserTask')
+        task2_copy = pigs_copy.task_ids.filtered(lambda t: t.name == 'Pigs ManagerTask')
+
+        self.assertEqual(len(task1_copy), 1, "Should only contain 1 copy of UserTask")
+        self.assertEqual(len(task2_copy), 1, "Should only contain 1 copy of ManagerTask")
+
+        self.assertEqual(task1_copy.depend_on_ids.ids, [task2_copy.id],
+                         "Copy should only create a relation between both copy if they are both part of the project")
+
+        task1_copy.depend_on_ids = self.task_1
+
+        pigs_copy_copy = pigs_copy.copy()
+        task1_copy_copy = pigs_copy_copy.task_ids.filtered(lambda t: t.name == 'Pigs UserTask')
+
+        self.assertEqual(task1_copy_copy.depend_on_ids.ids, [self.task_1.id],
+                         "Copy should not alter the relation if the other task is in a different project")
+
+    def test_duplicate_project_with_subtask_dependencies(self):
+        parent_task = self.env['project.task'].with_context({'mail_create_nolog': True}).create({
+            'name': 'Parent Task',
+            'project_id': self.project_goats.id,
+            'child_ids': [
+                Command.create({'name': 'Node 1'}),
+                Command.create({'name': 'SuperNode 2', 'child_ids': [Command.create({'name': 'Node 2'})]}),
+                Command.create({'name': 'Node 3'}),
+            ],
+        })
+
+        node1 = parent_task.child_ids[0]
+        node2 = parent_task.child_ids[1].child_ids
+        node3 = parent_task.child_ids[2]
+
+        node1.dependent_ids = node2
+        node2.dependent_ids = node3
+
+        # Test copying the whole Node tree
+        parent_task_copy = parent_task.copy()
+        parent_copy_node1 = parent_task_copy.child_ids[0]
+        parent_copy_node2 = parent_task_copy.child_ids[1].child_ids
+        parent_copy_node3 = parent_task_copy.child_ids[2]
+
+        # Relation should only be copied between the newly created node
+        self.assertEqual(len(parent_copy_node1.dependent_ids), 1)
+        self.assertEqual(parent_copy_node1.dependent_ids.ids, parent_copy_node2.ids, 'Node1copy - Node2copy relation should be present')
+        self.assertEqual(len(parent_copy_node2.dependent_ids), 1)
+        self.assertEqual(parent_copy_node2.dependent_ids.ids, parent_copy_node3.ids, 'Node2copy - Node3copy relation should be present')
+
+        # Original Node should not have new relation
+        self.assertEqual(len(node1.dependent_ids), 1)
+        self.assertEqual(node1.dependent_ids.ids, node2.ids, 'Only Node1 - Node2 relation should be present')
+        self.assertEqual(len(node2.dependent_ids), 1)
+        self.assertEqual(node2.dependent_ids.ids, node3.ids, 'Only Node2 - Node3 relation should be present')
+
+        # Test copying Node inside the chain
+        single_copy_node2 = node2.copy()
+
+        # Relation should be present between the other original node and the newly copied node
+        self.assertEqual(len(single_copy_node2.depend_on_ids), 1)
+        self.assertEqual(single_copy_node2.depend_on_ids.ids, node1.ids, 'Node1 - Node2copy relation should be present')
+        self.assertEqual(len(single_copy_node2.dependent_ids), 1)
+        self.assertEqual(single_copy_node2.dependent_ids.ids, node3.ids, 'Node2copy - Node3 relation should be present')
+
+        # Original Node should have new relations
+        self.assertEqual(len(node1.dependent_ids), 2)
+        self.assertEqual(len(node3.depend_on_ids), 2)
