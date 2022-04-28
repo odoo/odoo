57d3211c996bc4e58b429fb16930fbacc29c3bea
Nicolas Bayet <nby@odoo.com>
2022-04-28 15:52:01 +0000

    [FIX] web_editor: disable observer only when necessary
    Before this commit, the linktool was disabling the editor observer
    until it was destroyed. This messed up with the undo/redo and the
    isDirty.
    
    This commit only disable the observer when necessary.
    
    task-2836351
    
    Forward-port-of: #90064
    X-original-commit: 57c98f314332bbb4c3225942e1d3748eb131d8b0
    Part-of: odoo/odoo#91074

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -209,6 +209,7 @@ export class OdooEditor extends EventTarget {
                         return closestElement(selection.anchorNode, 'P, DIV');
                     }
                 },
+                preHistoryUndo: () => {},
                 isHintBlacklisted: () => false,
                 filterMutationRecords: (records) => records,
                 direction: 'ltr',
@@ -809,6 +810,7 @@ export class OdooEditor extends EventTarget {
      * "consumed": The position has been undone and is considered consumed.
      */
     historyUndo() {
+        this.options.preHistoryUndo();
         // The last step is considered an uncommited draft so always revert it.
         const lastStep = this._currentStep;
         this.historyRevert(lastStep);
