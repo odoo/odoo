79bf7e66b47ed328f7c44fcb74a6ff56ed490cf8
William Braeckman <wbr@odoo.com>
2022-04-29 09:36:02 +0000

    [FIX] website_sale: Avoid traceback when adding deleted products to cart
    Prior to this commit an user could try to add a product to cart that does not
    exist anymore because it was deleted while the user /shop page wasn't refreshed.
    
    This commit avoids this (rare) use case to happen.
    
    TaskId-2835736
    
    closes odoo/odoo#91351
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/website_sale/tests/test_sale_process.py
+++ addons/website_sale/tests/test_sale_process.py
@@ -59,7 +59,11 @@ class TestWebsiteSaleCheckoutAddress(odoo.tests.TransactionCase):
             'partner_id': partner_id,
             'website_id': self.website.id,
             'order_line': [(0, 0, {
-                'product_id': self.env['product.product'].create({'name': 'Product A', 'list_price': 100}).id,
+                'product_id': self.env['product.product'].create({
+                    'name': 'Product A',
+                    'list_price': 100,
+                    'website_published': True,
+                    'sale_ok': True}).id,
                 'name': 'Product A',
             })]
         })
