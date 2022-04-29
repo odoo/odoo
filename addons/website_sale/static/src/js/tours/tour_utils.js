32c765e3b4e4132a6c23088a21dcf25f432653bd
Alexandre de Pape (alpa) <alpa@odoo.com>
2022-04-29 14:38:42 +0000

    [IMP] website_sale: show comparison price on product page
    add the compare_list_price to /shop, if there is a compare_list_price and a
    price_list price for a product, the fixed_price will be displayed
    
    task-2813257
    
    closes odoo/odoo#90195
    
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- addons/website_sale/static/src/js/tours/tour_utils.js
+++ addons/website_sale/static/src/js/tours/tour_utils.js
@@ -1,6 +1,6 @@
 odoo.define("website_sale.tour_utils", function (require) {
     "use strict";
-    
+
     const core = require("web.core");
     const _t = core._t;
 
@@ -14,7 +14,37 @@ odoo.define("website_sale.tour_utils", function (require) {
         };
     }
 
+    /**
+     * Used to select a pricelist on the /shop view
+     */
+    function selectPriceList(pricelist) {
+        return [
+            {
+                content: "Click on pricelist dropdown",
+                trigger: "div.o_pricelist_dropdown a[data-toggle=dropdown]",
+            },
+            {
+                content: "Click on pricelist",
+                trigger: `span:contains(${pricelist})`,
+            },
+        ]
+    }
+
+    /**
+     * Used to assert if the price attribute of a given product is correct on the /shop view
+     */
+    function assertProductPrice(attribute, value, productName) {
+        return {
+            content: `The ${attribute} of the ${productName} is ${value}`,
+            trigger: `div:contains("${productName}") [data-oe-expression="template_price_vals[\'${attribute}\']"] .oe_currency_value:contains("${value}")`,
+            run: () => {
+            }
+        }
+    }
+
     return {
         goToCart,
+        selectPriceList,
+        assertProductPrice
     };
 });
