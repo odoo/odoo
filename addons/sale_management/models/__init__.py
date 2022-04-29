6c719f53ab3fcf8be1952b4492eb9f19c3acf10d
Victor Feyens <vfe@odoo.com>
2022-04-29 14:04:57 +0200

    [MOV] sale_management: split sale_order.py

--- addons/sale_management/models/__init__.py
+++ addons/sale_management/models/__init__.py
@@ -4,4 +4,6 @@ from . import digest
 from . import res_company
 from . import res_config_settings
 from . import sale_order
+from . import sale_order_line
+from . import sale_order_option
 from . import sale_order_template
