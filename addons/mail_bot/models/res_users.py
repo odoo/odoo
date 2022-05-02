4549de3b61f41c554c9987dc0d0adf46e64b1c6d
tsm-odoo <tsm@odoo.com>
2022-05-02 10:36:36 +0000

    [IMP] mail_bot: don't use session for odoobot initialization
    This PR prepares the ground for the one introducing the new environment in the
    mail module. Indeed, the mail_bot messaging_initializer relies on the session
    in order to known when to initiaze odoobot. With the new env, we won't be able
    to access the session but will instead override init_messaging to automatically
    initialize odoobot. Doing this first reduces the noise in the main PR.
    
    task-2582313
    
    closes odoo/odoo#90291
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail_bot/models/res_users.py
+++ addons/mail_bot/models/res_users.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import models, fields, _
 
 class Users(models.Model):
     _inherit = 'res.users'
@@ -21,3 +21,18 @@ class Users(models.Model):
     @property
     def SELF_READABLE_FIELDS(self):
         return super().SELF_READABLE_FIELDS + ['odoobot_state']
+
+    def _init_messaging(self):
+        if self.odoobot_state in [False, 'not_initialized'] and self.has_group('base.group_user'):
+            self._init_odoobot()
+        return super()._init_messaging()
+
+    def _init_odoobot(self):
+        self.ensure_one()
+        odoobot_id = self.env['ir.model.data']._xmlid_to_res_id("base.partner_root")
+        channel_info = self.env['mail.channel'].channel_get([odoobot_id, self.partner_id.id])
+        channel = self.env['mail.channel'].browse(channel_info['id'])
+        message = _("Hello,<br/>Odoo's chat helps employees collaborate efficiently. I'm here to help you discover its features.<br/><b>Try to send me an emoji</b> <span class=\"o_odoobot_command\">:)</span>")
+        channel.sudo().message_post(body=message, author_id=odoobot_id, message_type="comment", subtype_xmlid="mail.mt_comment")
+        self.sudo().odoobot_state = 'onboarding_emoji'
+        return channel
