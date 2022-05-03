32dc6015bc1623db9501944a38b3ac47df3af23e
Adrien Widart <awt@odoo.com>
2022-05-03 08:41:13 +0000

    [FIX] stock: compute forecasted inventory with inter-wh
    When moving some products between two warehouses thanks to an internal
    transfer, the forecasted inventory becomes incorrect
    
    To reproduce the issue:
    1. In Settings, enable "Multi-Warehouses"
    2. Let WH01 be the existing warehouse. Create a second one WH02
    3. Create a storable product P
    4. Update the quantity of P:
        - 3 in WH01/Stock
    5. Create a planned and internal transfer T:
        - Source: WH01/Stock
        - Destination: WH02/Stock
            - Ignore the warning
        - Operations:
            - 1 x P
    6. Mark T as todo
    7. Open the Forecasted Inventory:
        - Filters:
            - Forecasted Stock
            - Product: P
        - Group By: Warehouse
    
    Error: The report is incorrect, it says there are 3 P in WH01 and 0 in
    WH02 (should be 2 and 1). It becomes worst if T is done (the quantities
    in the past become incorrect)
    
    We should be able to move a product between two warehouses thanks to an
    internal transfer (so the warning of the step 5 should be removed).
    Moreover, the `report.stock.quantity` should consider that use case.
    
    When processing a stock move, the SQL view translates it as an in-move
    or out-move for a specific warehouse. For instance, if the SM comes from
    a warehouse and goes to a location without any warehouse (e.g., customer
    location), the SM is considered as an out-move. But here is the issue:
    in case of an inter-warehouse SM, both source and destination have a
    defined warehouse.
    
    Therefore, we need to:
    - Duplicate the inter-warehouses SM (so we have one in and one out)
    - Fake the values (no destination warehouse for the out move, no source
    warehouse for the in move)
    
    Also, before duplicating all SM, we filter out some useless SM:
    - Draft and cancelled ones
    - SM done more than 3 months ago (because the report only works for [-3
    months; +3 months])
    
    Considering some tests:
    (SM are confirmed. Each test has been repeated 5 times to get an
    average)
    |                                       |          |
    |:-------------------------------------:|:--------:|
    | 5000 SM, 0% inter-wh, without the fix |  ~715 ms |
    | 5000 SM, 0% inter-wh, with the fix    |  ~710 ms |
    |                 Impact                |   0.99x  |
    |                                       |          |
    | 6666 SM, 0% inter-wh, without the fix |  ~911 ms |
    | 5000 SM, 33% inter-wh, with the fix   |  ~999 ms |
    |                 Impact                |   1.10x  |
    |                                       |          |
    | 7500 SM, 0% inter-wh, without the fix | ~1004 ms |
    | 5000 SM, 50% inter-wh, with the fix   | ~1097 ms |
    |                 Impact                |   1.09x  |
    
    The impact is not that significant
    
    OPW-2752017
    task-2822157
    
    X-original-commit: a9dc406b52a3702f6901f686503e8d841f81724e

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -704,15 +704,6 @@ class Picking(models.Model):
 
     @api.onchange('location_id', 'location_dest_id', 'picking_type_id')
     def _onchange_locations(self):
-        from_wh = self.location_id.warehouse_id
-        to_wh = self.location_dest_id.warehouse_id
-        is_immediate = self.immediate_transfer if self._origin else self._context.get('default_immediate_transfer')
-        if self.picking_type_id.code == 'internal' and not is_immediate and from_wh and to_wh and from_wh != to_wh:
-            return {'warning': {
-                'title': _("Warning"),
-                'message': _("You should not use a planned internal transfer to move some products between two warehouses. "
-                             "Instead, use an immediate internal transfer or the resupply route.")
-            }}
         (self.move_lines | self.move_ids_without_package).update({
             "location_id": self.location_id,
             "location_dest_id": self.location_dest_id
