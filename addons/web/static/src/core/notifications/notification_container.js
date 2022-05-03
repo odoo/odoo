9b83c54b11d30a479e2ad21c1e3c0cf9deeea20e
Aaron Bohy <aab@odoo.com>
2022-05-03 10:54:05 +0200

    [FIX] web: notification: no "undefined" class on notification

--- addons/web/static/src/core/notifications/notification_container.js
+++ addons/web/static/src/core/notifications/notification_container.js
@@ -15,7 +15,7 @@ NotificationContainer.template = xml`
     <div class="o_notification_manager">
         <t t-foreach="notifications" t-as="notification" t-key="notification">
             <Transition leaveDuration="0" name="'o_notification_fade'" t-slot-scope="transition">
-                <Notification t-props="notification_value.props" className="notification_value.props.className + ' ' + transition.className"/>
+                <Notification t-props="notification_value.props" className="(notification_value.props.className || '') + ' ' + transition.className"/>
             </Transition>
         </t>
     </div>`;
