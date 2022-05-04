39a0fcba37a4cd7340ca9a0f9719bf755cc8d4a7
aliya <alta@odoo.com>
2022-05-04 15:19:48 +0000

    [IMP] purchase,sale: add a smart button to view source SO/PO
    Task 2833338
    
    Add a smart button to show origin SO/PO on invoices and vendor bills.
    
    closes odoo/odoo#90664
    
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/sale/models/account_move.py
+++ addons/sale/models/account_move.py
@@ -23,6 +23,7 @@ class AccountMove(models.Model):
     campaign_id = fields.Many2one(ondelete='set null')
     medium_id = fields.Many2one(ondelete='set null')
     source_id = fields.Many2one(ondelete='set null')
+    sale_order_count = fields.Integer(compute="_compute_origin_so_count", string='Sale Order Count')
 
     fiscal_position_id = fields.Many2one(
         compute='_compute_fiscal_position_id', store=True)
@@ -53,6 +54,11 @@ class AccountMove(models.Model):
             move.team_id = self.env['crm.team']._get_default_team_id(
                 user_id=move.invoice_user_id.id,
                 domain=[('company_id', '=', move.company_id.id)])
+
+    @api.depends('line_ids.sale_line_ids')
+    def _compute_origin_so_count(self):
+        for move in self:
+            move.sale_order_count = len(move.line_ids.sale_line_ids.order_id)
 
     def _reverse_moves(self, default_values_list=None, cancel=False):
         # OVERRIDE
@@ -125,3 +131,16 @@ class AccountMove(models.Model):
             send_invoice_cron._trigger()
 
         return res
+
+    def action_view_source_sale_orders(self):
+        self.ensure_one()
+        source_orders = self.line_ids.sale_line_ids.order_id
+        result = self.env['ir.actions.act_window']._for_xml_id('sale.action_orders')
+        if len(source_orders) > 1:
+            result['domain'] = [('id', 'in', source_orders.ids)]
+        elif len(source_orders) == 1:
+            result['views'] = [(self.env.ref('sale.view_order_form', False).id, 'form')]
+            result['res_id'] = source_orders.id
+        else:
+            result = {'type': 'ir.actions.act_window_close'}
+        return result
