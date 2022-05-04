239ef03383f470658904d7e10d3ce120c586236b
Adrien Widart <awt@odoo.com>
2022-05-04 13:42:21 +0200

    [FIX] mrp: reserve components when updating MO's qty
    When updating the MO quantity, the reservation of the components doesn't
    respect the reservation method of the operation type.
    
    To reproduce the issue:
    (Use demo data)
    1. Edit the operation type "Manufacturing":
        - Reservation Method: Manually
    2. Create and confirm a MO for 1 x [FURN_8855] Drawer
        - Note: the components are not reserved, as expected
    3. Update the quantity to produce (2 x Drawer)
    
    Error: The components are reserved
    
    The new conditions are directly based on the conditions to reserve a SM
    when confirming it:
    https://github.com/odoo/odoo/blob/e55988fc4d8090f84157a026b7ecc7ea0dc146a7/addons/stock/models/stock_move.py#L1242-L1248
    
    OPW-2822205
    
    X-original-commit: 3f3cc5cc561704dca361abc6b5323880350c3b6d

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -1017,18 +1017,23 @@ class MrpProduction(models.Model):
         self.ensure_one()
         update_info = []
         move_to_unlink = self.env['stock.move']
+        moves_to_assign = self.env['stock.move']
         for move in self.move_raw_ids.filtered(lambda m: m.state not in ('done', 'cancel')):
             old_qty = move.product_uom_qty
             new_qty = old_qty * factor
             if new_qty > 0:
                 move.write({'product_uom_qty': new_qty})
-                move._action_assign()
+                if move._should_bypass_reservation() \
+                        or move.picking_type_id.reservation_method == 'at_confirm' \
+                        or (move.reservation_date and move.reservation_date <= fields.Date.today()):
+                    moves_to_assign |= move
                 update_info.append((move, old_qty, new_qty))
             else:
                 if move.quantity_done > 0:
                     raise UserError(_('Lines need to be deleted, but can not as you still have some quantities to consume in them. '))
                 move._action_cancel()
                 move_to_unlink |= move
+        moves_to_assign._action_assign()
         move_to_unlink.unlink()
         return update_info
 
