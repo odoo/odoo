0a2420f08bc18c5b93a54f387b062b405d3c2453
grzana12 <lukasz@grzana.pl>
2022-05-04 22:13:55 +0200

    [FIX] mass_mailing: check fields for seen list are stored before going into SQL
    Seen list (aka already mailed records) computation is done in SQL to speedup
    computation. It is based on an heuristic to find fields to check (either
    a partner, either an email). However it checks fields are present in model
    but does not check they are stored. This leads to traceback if fields
    are not stored.
    
    Task-2852943

--- addons/mass_mailing/models/mailing.py
+++ addons/mass_mailing/models/mailing.py
@@ -439,7 +439,7 @@ class MassMailing(models.Model):
         """
 
         # Apply same 'get email field' rule from mail_thread.message_get_default_recipients
-        if 'partner_id' in target._fields:
+        if 'partner_id' in target._fields and target._fields['partner_id'].store:
             mail_field = 'email'
             query = """
                 SELECT lower(substring(p.%(mail_field)s, '([^ ,;<@]+@[^> ,;]+)'))
@@ -450,21 +450,21 @@ class MassMailing(models.Model):
             """
         elif issubclass(type(target), self.pool['mail.address.mixin']):
             mail_field = 'email_normalized'
-        elif 'email_from' in target._fields:
+        elif 'email_from' in target._fields and target._fields['email_from'].store:
             mail_field = 'email_from'
-        elif 'partner_email' in target._fields:
+        elif 'partner_email' in target._fields and target._fields['partner_email'].store:
             mail_field = 'partner_email'
-        elif 'email' in target._fields:
+        elif 'email' in target._fields and target._fields['email'].store:
             mail_field = 'email'
         else:
             raise UserError(_("Unsupported mass mailing model %s") % self.mailing_model_id.name)
 
         if self.unique_ab_testing:
-            query +="""
+            query += """
                AND s.campaign_id = %%(mailing_campaign_id)s;
             """
         else:
-            query +="""
+            query += """
                AND s.mass_mailing_id = %%(mailing_id)s
                AND s.model = %%(target_model)s;
             """
