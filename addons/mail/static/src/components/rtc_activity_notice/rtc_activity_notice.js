c5931027935ca9a18913f26c5c0e19446fc8d2aa
Alexandre Kühn <aku@odoo.com>
2022-05-04 17:02:33 +0200

    [IMP] mail: introduce RtcActivityNoticeView
    Task-2831082

--- addons/mail/static/src/components/rtc_activity_notice/rtc_activity_notice.js
+++ addons/mail/static/src/components/rtc_activity_notice/rtc_activity_notice.js
@@ -4,10 +4,14 @@ import { registerMessagingComponent } from '@mail/utils/messaging_component';
 
 const { Component } = owl;
 
-export class RtcActivityNotice extends Component {}
+export class RtcActivityNotice extends Component {
+    get rtcActivityNoticeView() {
+        return this.messaging && this.messaging.models['RtcActivityNotice'].get(this.props.localId);
+    }
+}
 
 Object.assign(RtcActivityNotice, {
-    props: {},
+    props: { localId: String },
     template: 'mail.RtcActivityNotice',
 });
 
