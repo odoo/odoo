e5c2d3d73adbaabf04296c93298278f6b00f28e0
Alexandre Kühn <aku@odoo.com>
2022-05-05 16:28:37 +0200

    [IMP] mail: introduce ChatWindowHeaderView/onClick
    Task-2831082

--- addons/mail/static/src/components/chat_window_header/chat_window_header.js
+++ addons/mail/static/src/components/chat_window_header/chat_window_header.js
@@ -1,7 +1,6 @@
 /** @odoo-module **/
 
 import { registerMessagingComponent } from '@mail/utils/messaging_component';
-import { isEventHandled } from '@mail/utils/utils';
 
 const { Component } = owl;
 
@@ -25,40 +24,12 @@ export class ChatWindowHeader extends Component {
         return this.props.record;
     }
 
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
-
-    /**
-     * @private
-     * @param {MouseEvent} ev
-     */
-    _onClick(ev) {
-        if (
-            isEventHandled(ev, 'ChatWindow.onClickCommand') ||
-            isEventHandled(ev, 'ChatWindowHeader.ClickShiftNext') ||
-            isEventHandled(ev, 'ChatWindowHeader.ClickShiftPrev') ||
-            isEventHandled(ev, 'ChatWindow.onClickHideMemberList') ||
-            isEventHandled(ev, 'ChatWindow.onClickShowMemberList')
-        ) {
-            return;
-        }
-        const chatWindow = this.chatWindow;
-        if (this.props.onClicked) {
-            this.props.onClicked({ chatWindow });
-        }
-    }
-
 }
 
 Object.assign(ChatWindowHeader, {
     props: {
         chatWindow: Object,
         record: Object,
-        onClicked: {
-            type: Function,
-            optional: true,
-        },
     },
     template: 'mail.ChatWindowHeader',
 });
