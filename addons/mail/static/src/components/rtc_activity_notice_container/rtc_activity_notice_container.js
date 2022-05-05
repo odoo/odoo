9a50cf2f736e8708c01cea74cb0b9486fb783e92
tsm-odoo <tsm@odoo.com>
2022-05-05 14:24:00 +0000

    [FIX] mail: ensures getMessagingComponent is called after registration
    Adds an import to wait for the required components to be added to the messaging
    components registry before their parent use getMessagingComponent. Otherwise,
    any slight delay could lead to missing components.
    
    X-original-commit: 31ce79e395e926d77618790d884c12e1c9885284

--- addons/mail/static/src/components/rtc_activity_notice_container/rtc_activity_notice_container.js
+++ addons/mail/static/src/components/rtc_activity_notice_container/rtc_activity_notice_container.js
@@ -1,6 +1,8 @@
 /** @odoo-module **/
 
 import { useModels } from "@mail/component_hooks/use_models/use_models";
+// ensure components are registered beforehand.
+import '@mail/components/rtc_activity_notice/rtc_activity_notice';
 import { getMessagingComponent } from "@mail/utils/messaging_component";
 
 const { Component } = owl;
