79fa4e51dcea2a2be3a2cadc306edd851d5b467c
tsm-odoo <tsm@odoo.com>
2022-05-05 14:24:00 +0000

    [FIX] mail: ensures getMessagingComponent is called after registration
    Adds an import to wait for the required components to be added to the messaging
    components registry before their parent use getMessagingComponent. Otherwise,
    any slight delay could lead to missing components.
    
    closes odoo/odoo#90784
    
    X-original-commit: 31ce79e395e926d77618790d884c12e1c9885284
    Related: odoo/enterprise#27067
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/components/messaging_menu_container/messaging_menu_container.js
+++ addons/mail/static/src/components/messaging_menu_container/messaging_menu_container.js
@@ -1,6 +1,8 @@
 /** @odoo-module **/
 
 import { useModels } from "@mail/component_hooks/use_models/use_models";
+// ensure components are registered beforehand.
+import '@mail/components/messaging_menu/messaging_menu';
 import { getMessagingComponent } from "@mail/utils/messaging_component";
 
 const { Component } = owl;
