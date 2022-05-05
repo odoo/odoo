3c023406089d59b90041807577f4ecfa49b22acd
FrancoisGe <fge@odoo.com>
2022-05-05 16:41:53 +0200

    add test listView

--- addons/web/static/tests/views/list_view_tests.js
+++ addons/web/static/tests/views/list_view_tests.js
@@ -8683,44 +8683,45 @@ QUnit.module("Views", (hooks) => {
         assert.deepEqual(allNames, ["Value 1", "Value 2", "USD", "Value 2", "Value 3"]);
     });
 
-    QUnit.skipWOWL("multi edit in view grouped by field not in view", async function (assert) {
-        // assert.expect(3);
-        // this.data.foo.records = [
-        //     // group 1
-        //     {id: 1, foo: '1', m2o: 1},
-        //     {id: 3, foo: '2', m2o: 1},
-        //     //group 2
-        //     {id: 2, foo: '1', m2o: 2},
-        //     {id: 4, foo: '2', m2o: 2},
-        //     // group 3
-        //     {id: 5, foo: '2', m2o: 3},
-        // ];
-        // const list = await createView({
-        //     View: ListView,
-        //     model: 'foo',
-        //     data: this.data,
-        //     arch: `<tree expand="1" multi_edit="1">
-        //            <field name="foo"/>
-        //        </tree>`,
-        //     groupBy: ['m2o'],
-        // });
-        // // Select items from the first group
-        // await testUtils.dom.click(list.$('.o_data_row .o_list_record_selector input:eq(0)'));
-        // await testUtils.dom.click(list.$('.o_data_row .o_list_record_selector input:eq(1)'));
-        // await testUtils.dom.click(list.$('.o_list_char:eq(0)'));
-        // await testUtils.fields.editInput(list.$('.o_field_widget[name=foo]'), 'test');
-        // assert.containsOnce(document.body, '.modal');
-        // await testUtils.dom.click($('.modal .modal-footer .btn-primary'));
-        // assert.containsNone(document.body, '.modal');
-        // const allNames = [...document.querySelectorAll('.o_data_cell')].map(n => n.textContent);
-        // assert.deepEqual(allNames, [
-        //     'test',
-        //     'test',
-        //     '1',
-        //     '2',
-        //     '2',
-        // ]);
-        // list.destroy();
+    QUnit.test("multi edit in view grouped by field not in view", async function (assert) {
+        serverData.models.foo.records = [
+            // group 1
+            {id: 1, foo: '1', m2o: 1},
+            {id: 3, foo: '2', m2o: 1},
+            //group 2
+            {id: 2, foo: '1', m2o: 2},
+            {id: 4, foo: '2', m2o: 2},
+            // group 3
+            {id: 5, foo: '2', m2o: 3},
+        ];
+
+        await makeView({
+            type: "list",
+            resModel: 'foo',
+            serverData,
+            arch: `<tree expand="1" multi_edit="1">
+                   <field name="foo"/>
+               </tree>`,
+            groupBy: ['m2o'],
+        });
+        // Select items from the first group
+        const rows = target.querySelectorAll(".o_data_row");
+        await click(rows[0], ".o_list_record_selector input");
+        await click(rows[1], ".o_list_record_selector input");
+        await click(target.querySelector(".o_list_char"))
+        await editInput(target, '.o_data_row [name=foo] input', 'test');
+        assert.containsOnce(target, '.modal');
+
+        await click(target, '.modal .modal-footer .btn-primary');
+        assert.containsNone(target, '.modal');
+        const allNames = [...document.querySelectorAll('.o_data_cell')].map(n => n.textContent);
+        assert.deepEqual(allNames, [
+            'test',
+            'test',
+            '1',
+            '2',
+            '2',
+        ]);
     });
 
     QUnit.skipWOWL("multi edit reference field batched in grouped list", async function (assert) {
