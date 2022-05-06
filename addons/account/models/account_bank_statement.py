boan-odoo/odoo-1

[IMP] account: add a line type on account move lines
There is a few use case where we need to filter aml based on some
specificities, such as liquidity/counterpart lines on payment,
cash rounding lines, display lines,...

This change aim to standardize this by adding a line_type field that
would allow to specify the line purpose, making it easy to filter
where needed but also to extend if needed.

Task id #2747764

addons/account/models/account_bank_statement.py
@@ -567,9 +567,9 @@ class AccountBankStatementLine(models.Model):
         other_lines = self.env['account.move.line']
 
         for line in self.move_id.line_ids:
-            if line.account_id == self.journal_id.default_account_id:
+            if line.line_type == 'pay_liquidity_line':
                 liquidity_lines += line
-            elif line.account_id == self.journal_id.suspense_account_id:
+            elif line.line_type == 'pay_counterpart_line':
                 suspense_lines += line
             else:
                 other_lines += line
@@ -618,6 +618,7 @@ class AccountBankStatementLine(models.Model):
             'debit': balance > 0 and balance or 0.0,
             'credit': balance < 0 and -balance or 0.0,
             'amount_currency': amount_currency,
+            'line_type': 'pay_liquidity_line',
         }
 
     @api.model
@@ -732,8 +733,10 @@ class AccountBankStatementLine(models.Model):
         '''
         self.ensure_one()
 
+        line_type = False
         if not counterpart_account_id:
             counterpart_account_id = self.journal_id.suspense_account_id.id
+            line_type = 'pay_counterpart_line'
 
         if not counterpart_account_id:
             raise UserError(_(
@@ -752,6 +755,7 @@ class AccountBankStatementLine(models.Model):
             'name': self.payment_ref,
             'account_id': counterpart_account_id,
             'amount_residual': liquidity_line_vals['debit'] - liquidity_line_vals['credit'],
+            'line_type': line_type,
         }
 
         if self.foreign_currency_id and self.foreign_currency_id != self.company_currency_id:

odoo/odoo

addons/account/models/account_bank_statement.py
@@ -567,9 +567,9 @@ class AccountBankStatementLine(models.Model):
         other_lines = self.env['account.move.line']
 
         for line in self.move_id.line_ids:
-            if line.account_id == self.journal_id.default_account_id:
+            if line.line_type == 'bank_statement_liquidity_line':
                 liquidity_lines += line
-            elif line.account_id == self.journal_id.suspense_account_id:
+            elif line.line_type == 'bank_statement_suspense_line':
                 suspense_lines += line
             else:
                 other_lines += line
@@ -618,6 +618,7 @@ class AccountBankStatementLine(models.Model):
             'debit': balance > 0 and balance or 0.0,
             'credit': balance < 0 and -balance or 0.0,
             'amount_currency': amount_currency,
+            'line_type': 'bank_statement_liquidity_line',
         }
 
     @api.model
@@ -732,8 +733,10 @@ class AccountBankStatementLine(models.Model):
         '''
         self.ensure_one()
 
+        line_type = False
         if not counterpart_account_id:
             counterpart_account_id = self.journal_id.suspense_account_id.id
+            line_type = 'bank_statement_suspense_line'
 
         if not counterpart_account_id:
             raise UserError(_(
@@ -752,6 +755,7 @@ class AccountBankStatementLine(models.Model):
             'name': self.payment_ref,
             'account_id': counterpart_account_id,
             'amount_residual': liquidity_line_vals['debit'] - liquidity_line_vals['credit'],
+            'line_type': line_type,
         }
 
         if self.foreign_currency_id and self.foreign_currency_id != self.company_currency_id: