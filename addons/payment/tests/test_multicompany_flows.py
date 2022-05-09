186e5b4404f760e1e7f14245be3836b0d0256d35
Victor Feyens <vfe@odoo.com>
2022-05-09 16:22:56 +0000

    [IMP] payment: merge common into unique test class using it
    The multi-company common of payment was only used in a single specific
    test class of the same module.  Since we don't aim to test multi-company
    flows in specific payment_ modules, we might as well remove the common
    and move the setup content in the only class using this common.
    
    Task - 2848326
    
    Part-of: odoo/odoo#90716

--- addons/payment/tests/test_multicompany_flows.py
+++ addons/payment/tests/test_multicompany_flows.py
@@ -1,14 +1,41 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo.fields import Command
 from odoo.tests import tagged
 from odoo.tools import mute_logger
 
 from odoo.addons.payment.tests.http_common import PaymentHttpCommon
-from odoo.addons.payment.tests.multicompany_common import PaymentMultiCompanyCommon
 
 
 @tagged('post_install', '-at_install')
-class TestMultiCompanyFlows(PaymentMultiCompanyCommon, PaymentHttpCommon):
+class TestMultiCompanyFlows(PaymentHttpCommon):
+
+    @classmethod
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass(chart_template_ref=chart_template_ref)
+
+        cls.company_a = cls.company_data['company']
+        cls.company_b = cls.company_data_2['company']
+
+        cls.user_company_a = cls.internal_user
+        cls.user_company_b = cls.env['res.users'].create({
+            'name': f"{cls.company_b.name} User (TEST)",
+            'login': 'user_company_b',
+            'password': 'user_company_b',
+            'company_id': cls.company_b.id,
+            'company_ids': [Command.set(cls.company_b.ids)],
+            'groups_id': [Command.link(cls.group_user.id)],
+        })
+        cls.user_multi_company = cls.env['res.users'].create({
+            'name': "Multi Company User (TEST)",
+            'login': 'user_multi_company',
+            'password': 'user_multi_company',
+            'company_id': cls.company_a.id,
+            'company_ids': [Command.set([cls.company_a.id, cls.company_b.id])],
+            'groups_id': [Command.link(cls.group_user.id)],
+        })
+
+        cls.acquirer_company_b = cls._prepare_acquirer(company=cls.company_b)
 
     def test_pay_logged_in_another_company(self):
         """User pays for an amount in another company."""
