1eaa057efdb1c89deffc2528b16f5fc63b418f63
Pratik Raval <prra@odoo.com>
2022-05-09 13:25:18 +0000

    [FIX] mass_mailing(_sms): make 'Retry' button generate a cron trigger
    When we use the "Retry" button in case of a failed SMS mailing, it puts
    the mailing back to the queue, which will be processed when on the next
    cron call of `Mail Marketing: Process queue`  (generally on the next
    day). However, it should immediately generate a cron trigger in this
    case so that SMS(s) can be sent right away.
    
    The same goes for Email Marketing, where it should immediately generate
    a cron trigger so the mailing can be sent immediately.
    
    With this commit, instead of simply putting the mailing back to the
    queue, we utilize the method `action_put_in_queue` which immediately
    generates the cron trigger in both cases.
    
    taskID-2760101
    
    X-original-commit: 572a7effb271ad134bb936751292668ae7b2b590

---
+++ addons/mass_mailing/tests/test_mailing_retry.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.addons.mass_mailing.tests.common import MassMailCommon
+from odoo.addons.base.tests.test_ir_cron import CronMixinCase
+from odoo.tests.common import users
+
+from unittest.mock import patch
+
+class TestMailingRetry(MassMailCommon, CronMixinCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestMailingRetry, cls).setUpClass()
+        cls._create_mailing_list()
+
+    @users('user_marketing')
+    def test_mailing_retry_immediate_trigger(self):
+        mailing = self.env['mailing.mailing'].create({
+            'name': 'TestMailing',
+            'subject': 'Test',
+            'mailing_type': 'mail',
+            'body_html': '<div>Hello</div>',
+            'mailing_model_id': self.env['ir.model']._get('res.partner').id,
+            'contact_list_ids': [(4, self.mailing_list_1.id)],
+        })
+        mailing.action_launch()
+
+        # force email sending to fail to test our retry mechanism
+        def patched_mail_mail_send(mail_records, auto_commit=False, raise_exception=False, smtp_session=None):
+            mail_records.write({'state': 'exception', 'failure_reason': 'forced_failure'})
+
+        with patch('odoo.addons.mail.models.mail_mail.MailMail._send', patched_mail_mail_send):
+            self.env.ref('mass_mailing.ir_cron_mass_mailing_queue').sudo().method_direct_trigger()
+
+        with self.capture_triggers('mass_mailing.ir_cron_mass_mailing_queue') as captured_triggers:
+            mailing.action_retry_failed()
+
+        self.assertEqual(len(captured_triggers.records), 1, "Should have created an additional trigger immediately")
+        captured_trigger = captured_triggers.records[0]
+        self.assertEqual(captured_trigger.cron_id, self.env.ref('mass_mailing.ir_cron_mass_mailing_queue'))
