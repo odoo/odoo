337ac8a75041bc8e94b503d0792e97347b5973e9
root <yenthevg@gmail.com>
2022-05-09 11:59:28 +0200

    [FIX] phone_validation: fallback if no region provided
    Before this commit if you would call phone_parse() but not pass along a
    country code it would crash. This is reproducable by calling the phone_parse
    function and not setting a country on the contact. In this case you would get
    the following traceback:
    
      AttributeError: 'bool' object has no attribute 'upper'.
    
    This happens because the phonenumbers library tries to upper() the region but
    if Odoo doesn't have a country set it passes along False. By doing a fallback
    to 'None' the phonenumbers library will natively handle this and the user in
    the UI does not get a traceback.
    
    Task-2852953
    
    Co-Authored-By: Thibault Delavallee <tde@odoo.com>

---
+++ addons/phone_validation/tests/test_phonenumbers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.addons.phone_validation.tools import phone_validation
+from odoo.tests import tagged
+from odoo.tests.common import BaseCase
+
+
+@tagged('phone_validation')
+class TestPhonenumbers(BaseCase):
+
+    def test_country_code_falsy(self):
+        self.assertEqual(
+            phone_validation.phone_format('0456998877', 'BE', '32', force_format='E164'),
+            '+32456998877'
+        )
+        self.assertEqual(
+            phone_validation.phone_format('0456998877', None, '32', force_format='E164'),
+            '+32456998877'
+        )
