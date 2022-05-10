1e0eab35142e7b0b4333a4f4b34742ee162d340f
Tommy (tong) <tong@odoo.com>
2022-05-10 09:11:37 +0000

    [ADD] payment_asiapay: add an integration to AsiaPay
    task-2845428
    
    closes odoo/odoo#98441
    
    Related: odoo/documentation#2628
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
    Co-authored-by: Antoine Vandevenne (anv) <anv@odoo.com>

---
+++ addons/payment_asiapay/models/account_payment_method.py
@@ -0,0 +1,13 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models
+
+
+class AccountPaymentMethod(models.Model):
+    _inherit = 'account.payment.method'
+
+    @api.model
+    def _get_payment_method_information(self):
+        res = super()._get_payment_method_information()
+        res['asiapay'] = {'mode': 'unique', 'domain': [('type', '=', 'bank')]}
+        return res
---
+++ addons/payment_asiapay/tests/common.py
@@ -0,0 +1,34 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.addons.payment.tests.common import PaymentCommon
+
+
+class AsiaPayCommon(PaymentCommon):
+
+    @classmethod
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass(chart_template_ref=chart_template_ref)
+
+        cls.asiapay = cls._prepare_acquirer('asiapay', update_values={
+            'asiapay_merchant_id': '123456789',
+            'asiapay_currency_id': cls.currency_euro,
+            'asiapay_secure_hash_secret': 'coincoin_motherducker',
+            'asiapay_secure_hash_function': 'sha1',
+        })
+
+        cls.acquirer = cls.asiapay
+
+        cls.redirect_notification_data = {
+            'Ref': cls.reference,
+        }
+        cls.webhook_notification_data = {
+            'src': 'dummy',
+            'prc': 'dummy',
+            'successcode': '0',
+            'Ref': cls.reference,
+            'PayRef': 'dummy',
+            'Cur': cls.currency.name,
+            'Amt': cls.amount,
+            'payerAuth': 'dummy',
+            'secureHash': '3e5bf55d9a23969130a6686db7aa4f0230956d0a',
+        }
