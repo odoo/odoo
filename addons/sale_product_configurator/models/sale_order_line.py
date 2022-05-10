5aa30892c2d5f91561cb4da0207c88bead495e7d
Victor Feyens <vfe@odoo.com>
2022-05-10 15:38:48 +0000

    [IMP] sale_product_*: clean python classes specifications
    * Enforce line length for fields spec
    * clean one little useless import
    * prefer single quotes for strings not shown to the end user
    
    Part-of: odoo/odoo#91222

--- addons/sale_product_configurator/models/sale_order_line.py
+++ addons/sale_product_configurator/models/sale_order_line.py
@@ -5,7 +5,10 @@ from odoo import fields, models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
+    _inherit = 'sale.order.line'
 
-    is_configurable_product = fields.Boolean('Is the product configurable?', related="product_template_id.has_configurable_attributes")
-    product_template_attribute_value_ids = fields.Many2many(related='product_id.product_template_attribute_value_ids', readonly=True)
+    is_configurable_product = fields.Boolean(
+        string="Is the product configurable?",
+        related='product_template_id.has_configurable_attributes')
+    product_template_attribute_value_ids = fields.Many2many(
+        related='product_id.product_template_attribute_value_ids')
