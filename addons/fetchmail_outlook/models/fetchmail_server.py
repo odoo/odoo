26fc99c11c813c8801e73d471dfd260ff3eedabe
std-odoo <std@odoo.com>
2022-05-10 09:39:44 +0200

    [IMP] microsoft_outlook, fetchmail_outlook *: improve usability * fetchmail, google_gmail, fetchmail_gmail
    Purpose
    =======
    Improve the usability of the outlook modules.
    
    Specifications
    ==============
    Remove the checkbox "Use Outlook" and instead use the
    smtp_authentication and the server_type, to be consistent with Gmail.
    
    Hide the password field for Outlook / Gmail mail servers.
    
    Add constraints on the outgoing mail server to force the user to use
    the right configuration (e.g. the from_filter, so the sending does not
    fail).
    
    Add an option in the mail module to install Outlook.
    
    Show a message for the outgoing mail servers to explain each
    authentication methods.
    
    Task-2811567

--- addons/fetchmail_outlook/models/fetchmail_server.py
+++ addons/fetchmail_outlook/models/fetchmail_server.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import _, api, models
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
@@ -13,35 +13,40 @@ class FetchmailServer(models.Model):
 
     _OUTLOOK_SCOPE = 'https://outlook.office.com/IMAP.AccessAsUser.All'
 
-    @api.constrains('use_microsoft_outlook_service', 'server_type', 'password', 'is_ssl')
+    server_type = fields.Selection(selection_add=[('outlook', 'Outlook OAuth Authentication')], ondelete={'outlook': 'set default'})
+
+    def _compute_server_type_info(self):
+        outlook_servers = self.filtered(lambda server: server.server_type == 'outlook')
+        outlook_servers.server_type_info = _(
+            'Connect your personal Outlook account using OAuth. \n'
+            'You will be redirected to the Outlook login page to accept '
+            'the permissions.')
+        super(FetchmailServer, self - outlook_servers)._compute_server_type_info()
+
+    @api.depends('server_type')
+    def _compute_is_microsoft_outlook_configured(self):
+        outlook_servers = self.filtered(lambda server: server.server_type == 'outlook')
+        (self - outlook_servers).is_microsoft_outlook_configured = False
+        super(FetchmailServer, outlook_servers)._compute_is_microsoft_outlook_configured()
+
+    @api.constrains('server_type', 'is_ssl')
     def _check_use_microsoft_outlook_service(self):
         for server in self:
-            if not server.use_microsoft_outlook_service:
-                continue
+            if server.server_type == 'outlook' and not server.is_ssl:
+                raise UserError(_('SSL is required for the server %r.', server.name))
 
-            if server.server_type != 'imap':
-                raise UserError(_('Outlook mail server %r only supports IMAP server type.', server.name))
-
-            if server.password:
-                raise UserError(_(
-                    'Please leave the password field empty for Outlook mail server %r. '
-                    'The OAuth process does not require it', server.name))
-
-            if not server.is_ssl:
-                raise UserError(_('SSL is required .', server.name))
-
-    @api.onchange('use_microsoft_outlook_service')
-    def _onchange_use_microsoft_outlook_service(self):
+    @api.onchange('server_type')
+    def onchange_server_type(self):
         """Set the default configuration for a IMAP Outlook server."""
-        if self.use_microsoft_outlook_service:
+        if self.server_type == 'outlook':
             self.server = 'imap.outlook.com'
-            self.server_type = 'imap'
             self.is_ssl = True
             self.port = 993
         else:
             self.microsoft_outlook_refresh_token = False
             self.microsoft_outlook_access_token = False
             self.microsoft_outlook_access_token_expiration = False
+            super(FetchmailServer, self).onchange_server_type()
 
     def _imap_login(self, connection):
         """Authenticate the IMAP connection.
@@ -49,9 +54,16 @@ class FetchmailServer(models.Model):
         If the mail server is Outlook, we use the OAuth2 authentication protocol.
         """
         self.ensure_one()
-        if self.use_microsoft_outlook_service:
+        if self.server_type == 'outlook':
             auth_string = self._generate_outlook_oauth2_string(self.user)
             connection.authenticate('XOAUTH2', lambda x: auth_string)
             connection.select('INBOX')
         else:
             super()._imap_login(connection)
+
+    def _get_connection_type(self):
+        """Return which connection must be used for this mail server (IMAP or POP).
+        The Outlook mail server used an IMAP connection.
+        """
+        self.ensure_one()
+        return 'imap' if self.server_type == 'outlook' else super()._get_connection_type()
