e4c53e8326fd86e23257e789fdec330d8ad31145
David Beguin <dbe@odoo.com>
2022-05-10 12:28:40 +0200

    [IMP] knowledge: generate private tutorial article per user
    To ease onboarding on the knowledge application, this commit allows to generate
    one tutorial/welcome article in the private section of each existing or new
    user.
    
    That tutorial article can directly be edited by the users to test all the
    functionnality of the Knowledge App.
    
    To avoid generating those articles, a context key can be used:
    - knowledge_skip_onboarding_article
    
    Task-2674460
    
    Co-Authored-By: Damien Abeloos <abd@odoo.com>
    Co-Authored-By: Julien Banken <jbn@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Fabio Barbero <faba@odoo.com>
    Co-Authored-By: Thibault Delavallee <tde@odoo.com>
    Co-Authored-By: Aurelien Warnong <awa@odoo.com>

--- addons/knowledge/__init__.py
+++ addons/knowledge/__init__.py
@@ -4,3 +4,10 @@
 from . import controllers
 from . import models
 from . import wizard
+
+from odoo import api, SUPERUSER_ID
+
+
+def _init_private_article_per_user(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['res.users'].search([('partner_share', '=', False)])._generate_tutorial_articles()
