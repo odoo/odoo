fbaa77b02219c59100e4d6eea3c06e7dcdd29fa9
Daniel Blanco <daniel@blancomartin.cl>
2022-05-11 04:20:04 +0000

    [FIX] l10n_cl: adjust domain for vendor bills to exclude credit notes from in_invoice type
    this fixes the domain of document types for vendor bills, (was failing since it included
    invoices as a document type as option for
    credit notes in the wizard and also fixes the opposite (credit notes in invoices generation)
    
    closes odoo/odoo#96932
    
    X-original-commit: 2028a7ab68444a2621a0e4572b70fcf0d7c15b3d
    Related: odoo/enterprise#29891
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_cl/models/account_move.py
+++ addons/l10n_cl/models/account_move.py
@@ -21,18 +21,18 @@ class AccountMove(models.Model):
                 self.journal_id.l10n_latam_use_documents:
             return super()._get_l10n_latam_documents_domain()
         if self.journal_id.type == 'sale':
-            if self.move_type == 'out_refund':
-                internal_types_domain = ('internal_type', '=', 'credit_note')
-            else:
-                internal_types_domain = ('internal_type', 'in', ['invoice', 'debit_note'])
-            domain = [('country_id.code', '=', 'CL'), internal_types_domain]
+            domain = [('country_id.code', '=', 'CL')]
+            if self.move_type in ['in_invoice', 'out_invoice']:
+                domain += [('internal_type', 'in', ['invoice', 'debit_note', 'invoice_in'])]
+            elif self.move_type in ['in_refund', 'out_refund']:
+                domain += [('internal_type', '=', 'credit_note')]
             if self.company_id.partner_id.l10n_cl_sii_taxpayer_type == '1':
                 domain += [('code', '!=', '71')]  # Companies with VAT Affected doesn't have "Boleta de honorarios Electrónica"
             return domain
         if self.move_type == 'in_refund':
             internal_types_domain = ('internal_type', '=', 'credit_note')
         else:
-            internal_types_domain = ('internal_type', 'in', ['invoice', 'debit_note', 'credit_note', 'invoice_in'])
+            internal_types_domain = ('internal_type', 'in', ['invoice', 'debit_note', 'invoice_in'])
         domain = [
             ('country_id.code', '=', 'CL'),
             internal_types_domain,
@@ -41,8 +41,6 @@ class AccountMove(models.Model):
             domain += [('code', 'not in', ['39', '70', '71', '914', '911'])]
         elif self.partner_id.l10n_cl_sii_taxpayer_type == '1' and self.partner_id_vat == '60805000-0':
             domain += [('code', 'not in', ['39', '70', '71'])]
-            if self.move_type == 'in_invoice':
-                domain += [('internal_type', '!=', 'credit_note')]
         elif self.partner_id.l10n_cl_sii_taxpayer_type == '2':
             domain += [('code', 'in', ['70', '71', '56', '61'])]
         elif self.partner_id.l10n_cl_sii_taxpayer_type == '3':
