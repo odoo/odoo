b23bb7ec8300aa94413b4a48585b2ce8752e069a
Sébastien Geelen (sge) <sge@odoo.com>
2022-05-11 13:51:17 +0000

    [FIX] web_editor,website,website_sale : fix products header edition
    Prevent edition on e-commerce products header bar.
    Since we cannot save any change done in the header object anyway,
    And some edition would trigger error.
    We put the all bar in a read-only mode.
    
    task-2745151
    
    X-original-commit: e2f7b8fad76dc816b2f6864340d3740446117cdb
    Part-of: odoo/odoo#91832

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -200,6 +200,7 @@ export class OdooEditor extends EventTarget {
                 showEmptyElementHint: true,
                 defaultLinkAttributes: {},
                 plugins: [],
+                getReadOnlyAreas: () => [],
                 getContentEditableAreas: () => [],
                 getPowerboxElement: () => {
                     const selection = document.getSelection();
@@ -1627,6 +1628,9 @@ export class OdooEditor extends EventTarget {
             if (!node.isContentEditable) {
                 node.setAttribute('contenteditable', true);
             }
+        }
+        for (const node of this.options.getReadOnlyAreas()) {
+            node.setAttribute('contenteditable', false);
         }
         this.observerActive('_activateContenteditable');
     }
