e15335dac3d69041bd8d3d236c264222cf0edea7
Florian Damhaut <flda@odoo.com>
2022-05-11 11:36:15 +0000

    [FIX] hr_holidays: Timeoff domain in view
    Timeoff is apply in the view to take effect on every view type
    instead of only the calendar.
    
    X-original-commit: 8b82f57728e0d19f2e54bff1131edd7c2e62c45b

--- addons/hr_holidays/static/src/js/time_off_calendar/time_off_calendar_views.js
+++ addons/hr_holidays/static/src/js/time_off_calendar/time_off_calendar_views.js
@@ -2,7 +2,6 @@
 
 import CalendarView from "web.CalendarView";
 import { TimeOffCalendarController } from "./time_off_calendar_controller";
-import { TimeOffCalendarModel } from "./time_off_calendar_model";
 import { TimeOffCalendarRenderer } from "./time_off_calendar_renderer";
 import { TimeOffPopoverRenderer } from "./time_off_popover_renderer";
 import viewRegistry from 'web.view_registry';
@@ -10,7 +9,6 @@ import viewRegistry from 'web.view_registry';
 export const TimeOffCalendarView = CalendarView.extend({
     config: Object.assign({}, CalendarView.prototype.config, {
         Controller: TimeOffCalendarController,
-        Model: TimeOffCalendarModel,
         Renderer: TimeOffCalendarRenderer,
     }),
 });
