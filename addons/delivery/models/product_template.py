ea861bc843034bc0c30c4533ac3a2f0a21190726
InCeyN <nci@odoo.com>
2022-05-12 15:47:24 +0200

    [FIX] delivery: Prevent delivery product to be set as sellable
    Currently, if a product is one configured on a delivery method - it is
    set to be 'is_delivery' = true.
    This has it's limitations if they then configure this same product as
    being sellable or purchassable. Once they have done so and sell/purchase
    a product, the invoice status in the process does not follow the
    standard behaviour.
    
    The point of this commit is to avoid running into this issue and
    preventing the product to be configured as sellable or purchassable if
    'is_delivery' is true.

--- addons/delivery/models/product_template.py
+++ addons/delivery/models/product_template.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
@@ -11,3 +12,11 @@ class ProductTemplate(models.Model):
         string="HS Code",
         help="Standardized code for international shipping and goods declaration. At the moment, only used for the FedEx shipping provider.",
     )
+
+    @api.constrains('sale_ok', 'purchase_ok')
+    def _check_is_delivery(self):
+        carriers = self.env['delivery.carrier'].sudo().search([('product_id', 'in', self.product_variant_ids.ids)])
+        for record in self:
+            if (record.sale_ok or record.purchase_ok) and carriers:
+                raise ValidationError(_('This product is set as delivery product for the following shipping method(s):\n%s',\
+                        '\n'.join(carriers.mapped('display_name'))))
