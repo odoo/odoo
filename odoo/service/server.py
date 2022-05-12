ca1b6d2600ca4a3cb2833523d602a066035fb6ab
Xavier-Do <xdo@odoo.com>
2022-05-12 17:38:15 +0200

    [IMP][POC] server: check signaling in parent process
    When started with -d, registries are pre initialized before the workers
    are spawned.
    This means that the parent process will have some registries in memory
    that will be available to the workers after the fork.
    If the parent process never checks signaling and the sequence changes,
    the copied registry will be reset at the first request.
    This means that a new registry will be create after the fork everytime a
    worker is recycled.
    By checking the registry in the parent worker, we ensure that the forked
    process will have a registry up to date (except if another change is
    made during the fork...).

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -740,6 +740,8 @@ class PreforkServer(CommonServer):
     def worker_spawn(self, klass, workers_registry):
         self.generation += 1
         worker = klass(self)
+        # Empty the cursor pool, we dont want them to be shared among forked workers.
+        odoo.sql_db.close_all()
         pid = os.fork()
         if pid != 0:
             worker.pid = pid
@@ -826,6 +828,10 @@ class PreforkServer(CommonServer):
                               worker.watchdog_timeout)
                 self.worker_kill(pid, signal.SIGKILL)
 
+    def check_registries(self):
+        for registry in Registry.registries.d.values():
+            registry.check_signaling()
+
     def process_spawn(self):
         if config['http_enable']:
             while len(self.workers_http) < self.population:
@@ -910,9 +916,6 @@ class PreforkServer(CommonServer):
             self.stop()
             return rc
 
-        # Empty the cursor pool, we dont want them to be shared among forked workers.
-        odoo.sql_db.close_all()
-
         _logger.debug("Multiprocess starting")
         while 1:
             try:
@@ -920,6 +923,7 @@ class PreforkServer(CommonServer):
                 self.process_signals()
                 self.process_zombie()
                 self.process_timeout()
+                self.check_registries()
                 self.process_spawn()
                 self.sleep()
             except KeyboardInterrupt:
