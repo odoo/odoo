686112317f00164f9bcd3a12a391393df711676b
Katherine Zaoral <kz@adhoc.com.ar>
2022-05-13 14:53:38 -0300

    Revert "[FIX] {mrp_,}account,sale_{stock,mrp}: display lots in invoice"
    This reverts commit 7a030f616f7e04b3994fc718840d265b1dd21068.

--- addons/sale_mrp/tests/__init__.py
+++ addons/sale_mrp/tests/__init__.py
@@ -6,4 +6,3 @@ from . import test_sale_mrp_kit_bom
 from . import test_sale_mrp_lead_time
 from . import test_sale_mrp_procurement
 from . import test_multistep_manufacturing
-from . import test_sale_mrp_report
