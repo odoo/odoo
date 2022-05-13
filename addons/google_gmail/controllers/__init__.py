66c8b9e2be9f3784d62d4e3b56618c2f7f891966
std-odoo <std@odoo.com>
2022-05-13 05:41:34 +0000

    [FIX] google_gmail, fetchmail_gmail: use Odoo endpoint for authentication
    Purpose
    =======
    The flow where we copy / paste the authorization code will be
    depreciated. Because of that, we know use the newest authentication
    system which use redirect URI.
    
    Technical
    =========
    Now, the user is redirected to an Odoo endpoint "google_gmail/confirm"
    and the access token / refresh token are automatically fetched.
    
    Documentation
    https://developers.google.com/identity/protocols/oauth2/native-app
    
    Task-2852560
    
    closes odoo/odoo#94404
    
    X-original-commit: b62d2b45481c1716a1d1ac01b17340a436797cc0
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/google_gmail/controllers/__init__.py
+++ addons/google_gmail/controllers/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import controllers
-from . import models
+from . import main
