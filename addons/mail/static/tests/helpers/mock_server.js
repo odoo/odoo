61e472aa35ac825553144fd9a80fbf62e3e8b5e1
Sébastien Theys <seb@odoo.com>
2022-05-16 18:39:08 +0000

    [IMP] mail, test_discuss_full: only return ids of public partners
    Other data are not actually used. This will improve performance a bit (less
    queries, less data to return) until the need to return this list goes away (it
    requires a bigger refactoring).
    
    task-2847983
    
    closes odoo/odoo#91515
    
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>

--- addons/mail/static/tests/helpers/mock_server.js
+++ addons/mail/static/tests/helpers/mock_server.js
@@ -2415,7 +2415,7 @@ MockServer.include({
             menu_id: false, // not useful in QUnit tests
             needaction_inbox_counter: this._mockResPartner_GetNeedactionCount(user.partner_id),
             partner_root: this._mockResPartnerMailPartnerFormat(this.partnerRootId).get(this.partnerRootId),
-            public_partners: [...this._mockResPartnerMailPartnerFormat(this.publicPartnerId).values()],
+            publicPartners: [['insert', [{ 'id': this.publicPartnerId }]]],
             shortcodes: this._mockSearchRead('mail.shortcode', [[], ['source', 'substitution']], {}),
             starred_counter: this._getRecords('mail.message', [['starred_partner_ids', 'in', user.partner_id]]).length,
         };
