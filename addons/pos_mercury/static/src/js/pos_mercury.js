odoo/odoo

commit b234e97f87c577a7cd85230c4f42277b39e8c842

[IMP] point_of_sale: create one account move by session
Main goal of this task is to create single accounting entry
(AE) when closing a pos.session instead of individual AE for
each order in the session.

This significantly minimizes the number of created journal
entries by point_of_sale which also results to faster closing
of session when there is large numbers (order of thousands)
of orders. It also eases the reconciliation of payments because
on one hand, cash payments are automatically reconciled and on
the other, other payments are combine in a receivable line
based on the payment method.

**Important points to note**

1. Two new models (pos.payment.method and pos.payment) are
introduced replacing the functionalities of account.journal
(and account.bank.statement) and
account.bank.statement.line, which served before as payment
methods and payments.

2. The creation of single AE relies on the receivable_account_id
defined in each pos.payment.method. This receivable_account_id
is supposed to be different from the account module's receivable
account, thus, a new receivable account for pos is introduced
in each localization.

3. A simple example below can illustrate this change.

Given the following orders:

    +---------+----------+----------+-----+-------+-------+
    | order   | payments | product  | qty | price | total |
    +---------+----------+----------+-----+-------+-------+
    | order 1 | cash     | product1 |  10 |    10 |   100 |
    |         |          | product2 |   5 |    20 |   100 |
    +---------+----------+----------+-----+-------+-------+
    | order 2 | bank     | product2 |   7 |    20 |   140 |
    |         |          | product3 |   1 |    30 |    30 |
    +---------+----------+----------+-----+-------+-------+
    | order 3 | bank     | product1 |   1 |    10 |    10 |
    |         |          | product2 |   3 |    20 |    60 |
    |         |          | product3 |   5 |    30 |   150 |

addons/pos_mercury/static/src/js/pos_mercury.js
@@ -13,31 +13,19 @@ var PopupWidget = require('point_of_sale.popups');
 var ScreenWidget = screens.ScreenWidget;
 var PaymentScreenWidget = screens.PaymentScreenWidget;
 
-pos_model.load_fields("account.journal", "pos_mercury_config_id");
+pos_model.load_fields('pos.payment.method', 'pos_mercury_config_id');
 
 pos_model.PosModel = pos_model.PosModel.extend({
-    getOnlinePaymentJournals: function () {
-        var self = this;
-        var online_payment_journals = [];
+    getOnlinePaymentMethods: function () {
+        var online_payment_methods = [];
 
-        $.each(this.journals, function (i, val) {
-            if (val.pos_mercury_config_id) {
-                online_payment_journals.push({label:self.getCashRegisterByJournalID(val.id).journal_id[1], item:val.id});
+        $.each(this.payment_methods, function (i, payment_method) {
+            if (payment_method.pos_mercury_config_id) {
+                online_payment_methods.push({label: payment_method.name, item: payment_method.id});
             }
         });
 
-        return online_payment_journals;
-    },
-    getCashRegisterByJournalID: function (journal_id) {
-        var cashregister_return;
-
-        $.each(this.cashregisters, function (index, cashregister) {
-            if (cashregister.journal_id[0] === journal_id) {
-                cashregister_return = cashregister;
-            }
-        });
-
-        return cashregister_return;
+        return online_payment_methods;
     },
     decodeMagtek: function (magtekInput) {
         // Regular expression to identify and extract data from the track 1 & 2 of the magnetic code
@@ -258,7 +246,7 @@ ScreenWidget.include({
 
     show: function () {
         this._super();
-        if(this.pos.getOnlinePaymentJournals().length !== 0) {
+        if(this.pos.getOnlinePaymentMethods().length !== 0) {
             this.pos.barcode_reader.set_action_callback('credit', _.bind(this.credit_error_action, this));
         }
     }
@@ -348,7 +336,7 @@ PaymentScreenWidget.include({
             return;
         }
 
-        if(this.pos.getOnlinePaymentJournals().length === 0) {
+        if(this.pos.getOnlinePaymentMethods().length === 0) {
             return;
         }
 
@@ -379,7 +367,7 @@ PaymentScreenWidget.include({
             'transaction_code'  : 'Sale',
             'invoice_no'        : self.pos.get_order().uid.replace(/-/g,''),
             'purchase'          : purchase_amount,
-            'journal_id'        : parsed_result.journal_id,
+            'payment_method_id' : parsed_result.payment_method_id,
         };
 
         var def = old_deferred || new $.Deferred();
@@ -426,7 +414,7 @@ PaymentScreenWidget.include({
                 }
 
                 var response = self.pos.decodeMercuryResponse(data);
-                response.journal_id = parsed_result.journal_id;
+                response.payment_method_id = parsed_result.payment_method_id;
 
                 if (response.status === 'Approved') {
                     // AP* indicates a duplicate request, so don't add anything for those
@@ -443,7 +431,7 @@ PaymentScreenWidget.include({
                         if (swipe_pending_line) {
                             order.select_paymentline(swipe_pending_line);
                         } else {
-                            order.add_paymentline(self.pos.getCashRegisterByJournalID(parsed_result.journal_id));
+                            order.add_paymentline(self.payment_methods_by_id[parsed_result.payment_method_id]);
                         }
 
                         order.selected_paymentline.paid = true;
@@ -502,17 +490,17 @@ PaymentScreenWidget.include({
 
     credit_code_action: function (parsed_result) {
         var self = this;
-        var online_payment_journals = this.pos.getOnlinePaymentJournals();
+        var online_payment_methods = this.pos.getOnlinePaymentMethods();
 
-        if (online_payment_journals.length === 1) {
-            parsed_result.journal_id = online_payment_journals[0].item;
+        if (online_payment_methods.length === 1) {
+            parsed_result.payment_method_id = online_payment_methods[0].item;
             self.credit_code_transaction(parsed_result);
         } else { // this is for supporting another payment system like mercury
             this.gui.show_popup('selection',{
                 title:   _t('Pay with: '),
-                list:    online_payment_journals,
+                list:    online_payment_methods,
                 confirm: function (item) {
-                    parsed_result.journal_id = item;
+                    parsed_result.payment_method_id = item;
                     self.credit_code_transaction(parsed_result);
                 },
                 cancel:  self.credit_code_cancel,
@@ -628,20 +616,15 @@ PaymentScreenWidget.include({
     click_paymentmethods: function (id) {
         var i;
         var order = this.pos.get_order();
-        var cashregister = null;
-        for (i = 0; i < this.pos.cashregisters.length; i++) {
-            if (this.pos.cashregisters[i].journal_id[0] === id){
-                cashregister = this.pos.cashregisters[i];
-                break;
-            }
-        }
+        var payment_method = this.pos.payment_methods_by_id[id]
+        // this.pos.get_order().add_paymentline(payment_method);
 
-        if (cashregister.journal.pos_mercury_config_id) {
+        if (payment_method.pos_mercury_config_id) {
             var already_swipe_pending = false;
             var lines = order.get_paymentlines();
 
             for (i = 0; i < lines.length; i++) {
-                if (lines[i].cashregister.journal.pos_mercury_config_id && lines[i].mercury_swipe_pending) {
+                if (lines[i].payment_method.pos_mercury_config_id && lines[i].mercury_swipe_pending) {
                     already_swipe_pending = true;
                 }
             }
@@ -666,7 +649,7 @@ PaymentScreenWidget.include({
 
     show: function () {
         this._super();
-        if (this.pos.getOnlinePaymentJournals().length !== 0) {
+        if (this.pos.getOnlinePaymentMethods().length !== 0) {
             this.pos.barcode_reader.set_action_callback('credit', _.bind(this.credit_code_action, this));
         }
     },

addons/l10n_fr_pos_cert/models/account_bank_statement.py
@@ -9,7 +9,7 @@ class AccountBankStatement(models.Model):
     _inherit = 'account.bank.statement'
 
     def unlink(self):
-        for statement in self.filtered(lambda s: s.company_id._is_accounting_unalterable() and s.journal_id.journal_user):
+        for statement in self.filtered(lambda s: s.company_id._is_accounting_unalterable() and s.journal_id.pos_payment_method_ids):
             raise UserError(_('You cannot modify anything on a bank statement (name: %s) that was created by point of sale operations.') % (statement.name,))
         return super(AccountBankStatement, self).unlink()
 
@@ -18,6 +18,6 @@ class AccountBankStatementLine(models.Model):
     _inherit = 'account.bank.statement.line'
 
     def unlink(self):
-        for line in self.filtered(lambda s: s.company_id._is_accounting_unalterable() and s.journal_id.journal_user):
+        for line in self.filtered(lambda s: s.company_id._is_accounting_unalterable() and s.journal_id.pos_payment_method_ids):
             raise UserError(_('You cannot modify anything on a bank statement line (name: %s) that was created by point of sale operations.') % (line.name,))
         return super(AccountBankStatementLine, self).unlink()

addons/point_of_sale/__manifest__.py
@@ -16,7 +16,6 @@
         'data/digest_data.xml',
         'wizard/pos_box.xml',
         'wizard/pos_details.xml',
-        'wizard/pos_open_statement.xml',
         'wizard/pos_payment.xml',
         'views/pos_templates.xml',
         'views/point_of_sale_template.xml',
@@ -26,6 +25,8 @@
         'views/pos_category_view.xml',
         'views/product_view.xml',
         'views/account_journal_view.xml',
+        'views/pos_payment_method_views.xml',
+        'views/pos_payment_views.xml',
         'views/pos_config_view.xml',
         'views/pos_session_view.xml',
         'views/point_of_sale_sequence.xml',

addons/point_of_sale/models/__init__.py
@@ -17,3 +17,5 @@ from . import res_partner
 from . import res_company
 from . import res_config_settings
 from . import stock_warehouse
+from . import pos_payment
+from . import pos_payment_method

addons/point_of_sale/models/account_journal.py
@@ -7,19 +7,4 @@ from odoo import fields, models, api
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
-    journal_user = fields.Boolean('Use in Point of Sale',
-        help="Check this box if this journal define a payment method that can be used in a point of sale.")
-
-    @api.model
-    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
-        session_id = self.env.context.get('pos_session_id', False)
-        if session_id:
-            session = self.env['pos.session'].browse(session_id)
-            if session:
-                args += [('id', 'in', session.config_id.journal_ids.ids)]
-        return super(AccountJournal, self)._search(args=args, offset=offset, limit=limit, order=order, count=count, access_rights_uid=access_rights_uid)
-
-    @api.onchange('type')
-    def onchange_type(self):
-        if self.type not in ['bank', 'cash']:
-            self.journal_user = False
+    pos_payment_method_ids = fields.One2many('pos.payment.method', 'cash_journal_id', string='Point of Sale Payment Methods')

addons/point_of_sale/models/pos_config.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from uuid import uuid4
 
 from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 
 
 class AccountBankStmtCashWizard(models.Model):
@@ -81,9 +81,8 @@ class PosConfig(models.Model):
     def _default_invoice_journal(self):
         return self.env['account.journal'].search([('type', '=', 'sale'), ('company_id', '=', self.env.company.id)], limit=1)
 
-    def _get_default_journal_ids(self):
-        journals = self.env['account.journal'].search([('journal_user', '=', True), ('type', 'in', ['cash','bank']), ('company_id', '=', self.env.company.id)])
-        return journals or False
+    def _default_payment_methods(self):
+        return self.env['pos.payment.method'].search([('split_transactions', '=', False), ('company_id', '=', self.env.company.id)])
 
     def _default_pricelist(self):
         return self.env['product.pricelist'].search([('currency_id', '=', self.env.company.currency_id.id)], limit=1)
@@ -101,11 +100,6 @@ class PosConfig(models.Model):
     name = fields.Char(string='Point of Sale', index=True, required=True, help="An internal identification of the point of sale.")
     is_installed_account_accountant = fields.Boolean(string="Is the Full Accounting Installed",
         compute="_compute_is_installed_account_accountant")
-    journal_ids = fields.Many2many(
-        'account.journal', 'pos_config_journal_rel',
-        'pos_config_id', 'journal_id', string='Available Payment Methods',
-        domain="[('journal_user', '=', True ), ('type', 'in', ['bank', 'cash']), ('company_id', '=', company_id)]",
-        default=lambda self: self._get_default_journal_ids())
     picking_type_id = fields.Many2one(
         'stock.picking.type',
         string='Operation Type',
@@ -169,8 +163,6 @@ class PosConfig(models.Model):
     pos_session_username = fields.Char(compute='_compute_current_session_user')
     pos_session_state = fields.Char(compute='_compute_current_session_user')
     pos_session_duration = fields.Char(compute='_compute_current_session_user')
-    group_by = fields.Boolean(string='Group Journal Items', default=True,
-        help="Check this if you want to group the Journal Items by Product while closing a Session.")
     pricelist_id = fields.Many2one('product.pricelist', string='Default Pricelist', required=True, default=_default_pricelist,
         help="The pricelist used if no customer is selected or if the customer has no Sale Pricelist configured.")
     available_pricelist_ids = fields.Many2many('product.pricelist', string='Available Pricelists', default=_default_pricelist,
@@ -208,6 +200,7 @@ class PosConfig(models.Model):
         help="This field depicts the maximum difference allowed between the ending balance and the theoretical cash when "
              "closing a session, for non-POS managers. If this maximum is reached, the user will have an error message at "
              "the closing of his session saying that he needs to contact his manager.")
+    payment_method_ids = fields.Many2many('pos.payment.method', string='Payment Methods', default=lambda self: self._default_payment_methods())
 
     def _compute_is_installed_account_accountant(self):
         account_accountant = self.env['ir.module.module'].sudo().search([('name', '=', 'account_accountant'), ('state', '=', 'installed')])
@@ -281,12 +274,12 @@ class PosConfig(models.Model):
         if self.invoice_journal_id and self.invoice_journal_id.company_id.id != self.company_id.id:
             raise ValidationError(_("The invoice journal and the point of sale must belong to the same company."))
 
-    @api.constrains('company_id', 'journal_ids')
+    @api.constrains('company_id', 'payment_method_ids')
     def _check_company_payment(self):
-        if self.env['account.journal'].search_count([('id', 'in', self.journal_ids.ids), ('company_id', '!=', self.company_id.id)]):
+        if self.env['pos.payment.method'].search_count([('id', 'in', self.payment_method_ids.ids), ('company_id', '!=', self.company_id.id)]):
             raise ValidationError(_("The method payments and the point of sale must belong to the same company."))
 
-    @api.constrains('pricelist_id', 'available_pricelist_ids', 'journal_id', 'invoice_journal_id', 'journal_ids')
+    @api.constrains('pricelist_id', 'available_pricelist_ids', 'journal_id', 'invoice_journal_id', 'payment_method_ids')
     def _check_currencies(self):
         if self.pricelist_id not in self.available_pricelist_ids:
             raise ValidationError(_("The default pricelist must be included in the available pricelists."))
@@ -296,7 +289,11 @@ class PosConfig(models.Model):
                                     " the Accounting application."))
         if self.invoice_journal_id.currency_id and self.invoice_journal_id.currency_id != self.currency_id:
             raise ValidationError(_("The invoice journal must be in the same currency as the Sales Journal or the company currency if that is not set."))
-        if any(self.journal_ids.mapped(lambda journal: self.currency_id not in (journal.company_id.currency_id, journal.currency_id))):
+        if any(
+            self.payment_method_ids\
+                .filtered(lambda pm: pm.is_cash_count)\
+                .mapped(lambda pm: self.currency_id not in (self.company_id.currency_id | pm.cash_journal_id.currency_id))
+        ):
             raise ValidationError(_("All payment methods must be in the same currency as the Sales Journal or the company currency if that is not set."))
 
     @api.constrains('company_id', 'available_pricelist_ids')
@@ -412,6 +409,9 @@ class PosConfig(models.Model):
         if config_display:
             super(PosConfig, config_display).write({'customer_facing_display_html': self._compute_default_customer_html()})
 
+        if self.current_session_id:
+            raise UserError(_('Unable to modify this PoS Configuration because there is an open PoS Session based on it.'))
+
         self.sudo()._set_fiscal_position()
         self.sudo()._check_modules_to_install()
         self.sudo()._check_groups_implied()
@@ -526,9 +526,24 @@ class PosConfig(models.Model):
         for company in companies:
             if company.chart_template_id:
                 cash_journal = self.env['account.journal'].search([('company_id', '=', company.id), ('type', '=', 'cash')], limit=1)
-                cash_journal.write({'journal_user':True})
-                existing_pos_config = self.env['pos.config'].search([('company_id', '=', company.id), ('journal_ids', '=', False)])
-                existing_pos_config.write({'journal_ids': [(6, 0, cash_journal.ids)]})
+                pos_receivable_account = company.account_default_pos_receivable_account_id
+                payment_methods = self.env['pos.payment.method']
+                if cash_journal:
+                    payment_methods |= payment_methods.create({
+                        'name': _('Cash'),
+                        'receivable_account_id': pos_receivable_account.id,
+                        'is_cash_count': True,
+                        'cash_journal_id': cash_journal.id,
+                        'company_id': company.id,
+                    })
+                payment_methods |= payment_methods.create({
+                    'name': _('Bank'),
+                    'receivable_account_id': pos_receivable_account.id,
+                    'is_cash_count': False,
+                    'company_id': company.id,
+                })
+                existing_pos_config = self.env['pos.config'].search([('company_id', '=', company.id), ('payment_method_ids', '=', False)])
+                existing_pos_config.write({'payment_method_ids': [(6, 0, payment_methods.ids)]})
 
     @api.model
     def generate_pos_journal(self, companies=False):

addons/point_of_sale/models/pos_payment.py
@@ -1,0 +1,25 @@
+from odoo import api, fields, models
+
+
+class PosPayment(models.Model):
+    """ Used to register payments made in a pos.order.
+
+    See `payment_ids` field of pos.order model.
+    The main characteristics of pos.payment can be read from
+    `payment_method_id`.
+    """
+
+    _name = "pos.payment"
+    _description = "Point of Sale Payments"
+    _order = "id desc"
+
+    name = fields.Char(string='Label', readonly=True)
+    pos_order_id = fields.Many2one('pos.order', string='Order', required=True)
+    amount = fields.Monetary(string='Amount', required=True, currency_field='currency_id', readonly=True, help="Total amount of the payment.")
+    payment_method_id = fields.Many2one('pos.payment.method', string='Payment Method', required=True)
+    payment_date = fields.Datetime(string='Date', required=True, readonly=True, default=lambda self: fields.Datetime.now())
+    currency_id = fields.Many2one('res.currency', string='Currency', related='pos_order_id.currency_id')
+    currency_rate = fields.Float(string='Conversion Rate', related='pos_order_id.currency_rate', help='Conversion rate from company currency to order currency.')
+    partner_id = fields.Many2one('res.partner', string='Customer', related='pos_order_id.partner_id')
+    session_id = fields.Many2one('pos.session', string='Session', related='pos_order_id.session_id')
+    company_id = fields.Many2one('res.company', string='Company', related='pos_order_id.company_id')

commit 6c9d8c9fbed79b16f13a058a5f56f546d2a4fe2f

[FIX] pos_mercury: don't crash when swiping without adding a line first
Small oversight introduced in b234e97f87c. Before this patch swiping
without selecting the mercury/vantiv payment method first resulted in
an error and potential double-charges.

opw-2792847

addons/pos_mercury/static/src/js/pos_mercury.js
@@ -431,7 +431,7 @@ PaymentScreenWidget.include({
                         if (swipe_pending_line) {
                             order.select_paymentline(swipe_pending_line);
                         } else {
-                            order.add_paymentline(self.payment_methods_by_id[parsed_result.payment_method_id]);
+                            order.add_paymentline(self.pos.payment_methods_by_id[parsed_result.payment_method_id]);
                         }
 
                         order.selected_paymentline.paid = true;