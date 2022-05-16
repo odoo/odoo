fe4923ba3330c3dffb5af11a238d43ca1090c9d9
Victor Feyens <vfe@odoo.com>
2022-05-16 10:32:25 +0000

    [MOV] sale_stock: rename xml file according to content
    closes odoo/odoo#91555
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale_stock/__manifest__.py
+++ addons/sale_stock/__manifest__.py
@@ -23,17 +23,21 @@ Preferences
     'data': [
         'security/sale_stock_security.xml',
         'security/ir.model.access.csv',
+
         'views/sale_order_views.xml',
-        'views/stock_views.xml',
+        'views/stock_route_views.xml',
         'views/res_config_settings_views.xml',
         'views/sale_stock_portal_template.xml',
         'views/stock_lot_views.xml',
         'views/res_users_views.xml',
+
         'report/report_stock_forecasted.xml',
         'report/sale_order_report_templates.xml',
         'report/stock_report_deliveryslip.xml',
+
         'data/mail_templates.xml',
         'data/sale_stock_data.xml',
+
         'wizard/stock_rules_report_views.xml',
         'wizard/sale_order_cancel_views.xml',
     ],
