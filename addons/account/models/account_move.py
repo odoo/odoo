5e8889f71e4e8458d662af1c02844bc9a2298fdf
Pierre Masereel <pim@odoo.com>
2022-05-18 11:35:53 +0000

    [REVERT] account: unbalanced entry with price_unit 0
    This fix will try to write a new amount value on the tax when an
    invoice is created from POS, that will raise a user error preventing the
    creation of the order.
    
    This reverts commit bd621fd456811e7f3ce646e3ecade5e9e4a826e8.
    
    OPW-2855694
    
    closes odoo/odoo#91658
    
    Signed-off-by: Masereel Pierre <pim@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -3112,7 +3112,6 @@ class AccountMoveLine(models.Model):
                 if tax.price_include:
                     balance += tax_res['amount']
 
-        tax_included_division_is_zero = any(tax.price_include and tax.amount == 100 and tax.amount_type == 'division' for tax in taxes) and balance == 0
         discount_factor = 1 - (discount / 100.0)
         if balance and discount_factor:
             # discount != 100%
@@ -3127,9 +3126,8 @@ class AccountMoveLine(models.Model):
                 'discount': 0.0,
                 'price_unit': balance / (quantity or 1.0),
             }
-        elif not discount_factor or tax_included_division_is_zero:
-            # balance of line is 0, but discount == 100% or taxes (price included) == 100%,
-            # so we display the normal unit_price
+        elif not discount_factor:
+            # balance of line is 0, but discount  == 100% so we display the normal unit_price
             vals = {}
         else:
             # balance is 0, so unit price is 0 as well
