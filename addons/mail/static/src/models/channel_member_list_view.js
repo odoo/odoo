299138350d236d99c4a00da0ed8fc4e81a1d7a0b
Alexandre Kühn <aku@odoo.com>
2022-05-18 15:22:15 +0000

    [IMP] mail: introduce ChannelMemberListCategoryView
    Task-2831082
    
    closes odoo/odoo#91699
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/models/channel_member_list_view.js
+++ addons/mail/static/src/models/channel_member_list_view.js
@@ -2,7 +2,7 @@
 
 import { registerModel } from '@mail/model/model_core';
 import { one } from '@mail/model/model_field';
-import { clear, replace } from '@mail/model/model_field_command';
+import { clear, insertAndReplace, replace } from '@mail/model/model_field_command';
 
 registerModel({
     name: 'ChannelMemberListView',
@@ -21,6 +21,26 @@ registerModel({
             }
             return clear();
         },
+        /**
+         * @private
+         * @returns {FieldCommand}
+         */
+        _computeOfflineCategoryView() {
+            if (this.channel && this.channel.orderedOfflineMembers.length > 0) {
+                return insertAndReplace();
+            }
+            return clear();
+        },
+        /**
+         * @private
+         * @returns {FieldCommand}
+         */
+        _computeOnlineCategoryView() {
+            if (this.channel && this.channel.orderedOnlineMembers.length > 0) {
+                return insertAndReplace();
+            }
+            return clear();
+        },
     },
     fields: {
         channel: one('Thread', {
@@ -30,6 +50,16 @@ registerModel({
         chatWindowOwner: one('ChatWindow', {
             inverse: 'channelMemberListView',
             readonly: true,
+        }),
+        offlineCategoryView: one('ChannelMemberListCategoryView', {
+            compute: '_computeOfflineCategoryView',
+            inverse: 'channelMemberListViewOwnerAsOffline',
+            isCausal: true,
+        }),
+        onlineCategoryView: one('ChannelMemberListCategoryView', {
+            compute: '_computeOnlineCategoryView',
+            inverse: 'channelMemberListViewOwnerAsOnline',
+            isCausal: true,
         }),
         threadViewOwner: one('ThreadView', {
             inverse: 'channelMemberListView',
