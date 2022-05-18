c8d17c760e27736d0cb809b59e9945f808b1138e
William Braeckman <wbr@odoo.com>
2022-05-18 07:53:31 +0000

    [FIX] hr_holidays: exclude cancelled time off by default in report
    Prior to this commit cancelled time off were included in the leave
    report calendar model and were shown as regular time offs.
    They will now be hidden.
    
    closes odoo/odoo#91633
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_holidays/report/hr_leave_report_calendar.py
+++ addons/hr_holidays/report/hr_leave_report_calendar.py
@@ -66,6 +66,7 @@ class LeaveReportCalendar(models.Model):
                 ON cc.id = co.resource_calendar_id
         WHERE 
             hl.state IN ('confirm', 'validate', 'validate1')
+            AND hl.active IS TRUE
         );
         """)
 
