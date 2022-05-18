723fe143580c67bcf1475cfee48c2da5011ed588
Víctor Martínez <victor.martinez@tecnativa.com>
2022-05-18 10:37:42 +0000

    [FIX] hr_holidays: Allow employee to add attachments on validated time off
    Purpose
    =======
    
    Allow to add attachments in leaves if they don't have officer permissions.
    
    closes odoo/odoo#91776
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -733,7 +733,7 @@ class HolidaysRequest(models.Model):
     def write(self, values):
         is_officer = self.env.user.has_group('hr_holidays.group_hr_holidays_user') or self.env.is_superuser()
 
-        if not is_officer:
+        if not is_officer and values.keys() - {'message_main_attachment_id'}:
             if any(hol.date_from.date() < fields.Date.today() and hol.employee_id.leave_manager_id != self.env.user for hol in self):
                 raise UserError(_('You must have manager rights to modify/validate a time off that already begun'))
 
