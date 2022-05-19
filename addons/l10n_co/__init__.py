536000974bfc88471ed864de73e9113025da64bb
aliya <alta@odoo.com>
2022-05-19 12:27:35 +0200

    [IMP] l10n_co: update CoA, taxes
    This commit:
    - Updates the Chart of Accounts for Colombia
    - Updates taxes
    
    task-2830938

--- addons/l10n_co/__init__.py
+++ addons/l10n_co/__init__.py
@@ -8,4 +8,9 @@
 #               Nhomar Hernandez (Vauxoo)
 #               Humberto Ochoa (Vauxoo)
 
+from odoo import api, SUPERUSER_ID
 from . import models
+
+def load_translations(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('l10n_co.l10n_co_chart_template_generic').process_coa_translations()
