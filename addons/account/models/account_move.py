1cb947733dc33be52dfc97f42699585cb7f04e65
Pouya Malekinejad <poma@odoo.com>
2022-05-20 15:32:23 +0430

    [FIX] account: Tax lines reset when editing tax values or tax groups in V13 and V14
    Before this fix, when there is a 0% tax in an invoice/bill, the taxes recalculate everytime anything changes,
    this will fix, edits on taxes (specially manual edit of total taxes are wiped)
    
    Task: 2859732

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -991,10 +991,23 @@ class AccountMove(models.Model):
         '''
         for invoice in self:
             # Dispatch lines and pre-compute some aggregated values like taxes.
+
+            # determine required tax_lines by finding all non-zero tax amounts
+            lines_with_taxes = invoice.line_ids.filtered(
+                lambda l: l.tax_ids and l.move_id.currency_id.compare_amounts(l.price_subtotal, l.price_total))
+
+            needed_taxes = lines_with_taxes.tax_ids._origin.flatten_taxes_hierarchy().filtered(
+                lambda tax: (
+                        tax.amount_type == 'fixed' and not invoice.company_id.currency_id.is_zero(tax.amount)
+                        or not float_is_zero(tax.amount, precision_digits=4)
+                )
+            )
+            current_taxes = invoice.line_ids.tax_line_id._origin
+
             if (
                 recompute_all_taxes
                 or any(line.recompute_tax_line for line in invoice.line_ids)
-                or invoice.line_ids.tax_ids.flatten_taxes_hierarchy()._origin > invoice.line_ids.tax_line_id._origin
+                or needed_taxes > current_taxes
             ):
                 invoice.line_ids.recompute_tax_line = False
                 invoice._recompute_tax_lines()
