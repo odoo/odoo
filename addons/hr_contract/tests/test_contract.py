19441d2902d9502ab7f035ef43ee9b2b9bf2b1b9
Yannick Tivisse <yti@odoo.com>
2022-05-20 14:04:50 +0000

    [FIX] hr_contract: Reset current contract on stage modification
    Purpose
    =======
    
    Let's say we have 2 contracts A and B:
    
    A --> Running
    B --> Cancelled
    contract_id : A
    
    B is set to Running
    Contract_id : B
    
    B is set to Cancelled
    contract_id : B
    
    In this case, the current contract should be reset to A.
    
    X-original-commit: f736264881cbb3086430ea8cd7dba7c4f5a351f4
    Part-of: odoo/odoo#92040

--- addons/hr_contract/tests/test_contract.py
+++ addons/hr_contract/tests/test_contract.py
@@ -2,10 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
+
 from odoo.exceptions import ValidationError
 from odoo.addons.hr_contract.tests.common import TestContractCommon
+from odoo.tests import tagged
 
-
+@tagged('test_contracts')
 class TestHrContracts(TestContractCommon):
 
     @classmethod
@@ -101,3 +104,15 @@ class TestHrContracts(TestContractCommon):
         # New contract, with gap
         self.create_contract('open', 'normal', date(2016, 1, 1), date(2016, 1, 31))
         self.assertEqual(self.employee.first_contract_date, date(2017, 1, 1))
+
+    def test_current_contract_stage_change(self):
+        today = date.today()
+        contract = self.create_contract('open', 'normal', today + relativedelta(day=1), today + relativedelta(day=31))
+        self.assertEqual(self.employee.contract_id, contract)
+
+        draft_contract = self.create_contract('draft', 'normal', today + relativedelta(months=1, day=1), today + relativedelta(months=1, day=31))
+        draft_contract.state = 'open'
+        self.assertEqual(self.employee.contract_id, draft_contract)
+
+        draft_contract.state = 'draft'
+        self.assertEqual(self.employee.contract_id, contract)
