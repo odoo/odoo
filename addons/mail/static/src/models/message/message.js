dad2bc39e592571edc43f5c2ce77bc4b4b7a157b
Luis González <lgonzalez@vauxoo.com>
2022-05-22 22:00:46 +0000

    [FIX] mail: missing ARIA attributes after OWL
    After the refactor performed on 3fea5b213 to start using OWL, a lot of
    aria attributes were lost.
    
    ARIA attributes are required to ensure good compatibility with asistive
    technologies and keyboard users.
    
    This commit reintroduces such attributes, which include:
    - `aria-label`: used for elements with title but no text
    - `role`: mainly used for elements that behaves as buttons or links
    - `aria-expanded`: used on buttons that toggle menus or content, e.g.
      button to open attachments or followers
    - And another ones like `aria-pressed`, `tabindex`, etc
    
    X-original-commit: ad84b2d740624987188dcc1fc9107ae61b896100

--- addons/mail/static/src/models/message/message.js
+++ addons/mail/static/src/models/message/message.js
@@ -499,6 +499,20 @@ registerModel({
             return false;
         },
         /**
+         * @private
+         * @returns {string}
+         */
+        _computeMessageTypeText() {
+            if (this.message_type === 'notification') {
+                return this.env._t("System notification");
+            }
+            if (!this.is_discussion && !this.is_notification) {
+                return this.env._t("Note");
+            }
+            return this.env._t("Message");
+        },
+
+        /**
          * This value is meant to be based on field body which is
          * returned by the server (and has been sanitized before stored into db).
          * Do not use this value in a 't-raw' if the message has been created
@@ -729,6 +743,9 @@ registerModel({
          */
         isStarred: attr({
             default: false,
+        }),
+        messageTypeText: attr({
+            compute: '_computeMessageTypeText',
         }),
         /**
          * Groups of reactions per content allowing to know the number of
