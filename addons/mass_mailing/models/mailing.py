7f90f4327aa3bee1b9ad4d2ed5ba4891bb641f52
Thibault Delavallée <tde@odoo.com>
2022-05-23 09:26:59 +0200

    [REF] mass_mailing: cleanup subscriptions update (optin / optout from lists)
    PURPOSE
    
    Overall cleaning of subscription and exclusion management code from portal.
    This code comes mainly from v12 and can now benefit from cleaning and update.
    
    SPECIFICATIONS
    
    Purpose of this commit is to cleanup code that manages chosen lists from
    portal and updates opt-in and opt-out accordingly.
    
    Code is now located on mailing list model. When opting-it, new subscriptions
    have to be created for mailing lists so that email is part of the list. When
    opting-out we just have to toggle the opt_out flag on subscription model.
    
    Logged messages are also improved. We log on contact model the updated
    mailing lists for opt-in or opt-out.
    
    Task-2669037 (Mass Mailing: Refactor js/portal for subscription)
    
    Part-of: odoo/odoo#86084

--- addons/mass_mailing/models/mailing.py
+++ addons/mass_mailing/models/mailing.py
@@ -785,25 +785,6 @@ class MassMailing(models.Model):
 
         return values_list
 
-    def update_opt_out(self, email, list_ids, value):
-        if len(list_ids) > 0:
-            model = self.env['mailing.contact'].with_context(active_test=False)
-            records = model.search([('email_normalized', '=', tools.email_normalize(email))])
-            opt_out_records = self.env['mailing.contact.subscription'].search([
-                ('contact_id', 'in', records.ids),
-                ('list_id', 'in', list_ids),
-                ('opt_out', '!=', value)
-            ])
-
-            opt_out_records.write({'opt_out': value})
-            message = Markup(_('The recipient <strong>unsubscribed from %s</strong> mailing list(s)')) \
-                if value else Markup(_('The recipient <strong>subscribed to %s</strong> mailing list(s)'))
-            for record in records:
-                # filter the list_id by record
-                record_lists = opt_out_records.filtered(lambda rec: rec.contact_id.id == record.id)
-                if len(record_lists) > 0:
-                    record.sudo().message_post(body=message % ', '.join(str(list.name) for list in record_lists.mapped('list_id')))
-
     # ------------------------------------------------------
     # A/B Test
     # ------------------------------------------------------
