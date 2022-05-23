3fe33487e1b43c95c65e148500f30769e4fcc762
Ipsita Borisagar <ibo@odoo.com>
2022-05-23 10:38:36 +0000

    [FIX] im_livechat: disable attachment upload via drag and drop
    Before this commit:
    
    Attachments are not properly supported in livechat, so there is no point in
    allowing attachments to be uploaded via drag and drop.
    
    After this commit:
    
    Disable attachment upload via drag and drop in livechat channels.
    
    Task-2824502
    
    closes odoo/odoo#95761
    
    X-original-commit: 1aed05eb1c50ee69b974a140b3080e38c802cb2f
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

---
+++ addons/im_livechat/static/src/models/composer_view.js
@@ -0,0 +1,17 @@
+/** @odoo-module **/
+
+import { patchRecordMethods } from '@mail/model/model_core';
+import { clear } from '@mail/model/model_field_command';
+import '@mail/models/composer_view';
+
+patchRecordMethods('ComposerView', {
+    /**
+     * @override
+     */
+    _computeDropZoneView() {
+        if (this.composer.thread && this.composer.thread.channel_type === 'livechat') {
+            return clear();
+        }
+        return this._super();
+    },
+});
