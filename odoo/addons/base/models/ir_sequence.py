cf3ce45d42d459a3ac75be9e9dd438dc28a1d962
Francisco Javier Luna Vazquez <fluna@vauxoo.com>
2022-05-26 13:06:45 -0500

    [REF] ir_sequence: Use odoo native methods

--- odoo/addons/base/models/ir_sequence.py
+++ odoo/addons/base/models/ir_sequence.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
 import logging
 import pytz
 from psycopg2 import sql, OperationalError, errorcodes
@@ -240,27 +239,45 @@ class IrSequence(models.Model):
 
     def _create_date_range_seq(self, date):
         date_obj = fields.Date.from_string(date)
+        sequence_range = self.env["ir.sequence.date_range"]
         prefix_suffix = "%s %s" % (self.prefix, self.suffix)
-        if '%(range_day)s' in prefix_suffix:
+        if "%(range_day)s" in prefix_suffix:
             date_from = date_obj
             date_to = date_obj
-        elif '%(range_month)s' in prefix_suffix:
-            date_from = date_obj + relativedelta(day=1)
-            date_to = date_obj + relativedelta(day=31)
+        elif "%(range_month)s" in prefix_suffix:
+            date_from = fields.Date.start_of(date_obj, "month")
+            date_to = fields.Date.end_of(date_obj, "month")
         else:
-            date_from = date_obj + relativedelta(day=1, month=1)
-            date_to = date_obj + relativedelta(day=31, month=12)
-        date_range = self.env['ir.sequence.date_range'].search([('sequence_id', '=', self.id), ('date_from', '>=', date), ('date_from', '<=', date_to)], order='date_from desc', limit=1)
+            date_from = fields.Date.start_of(date_obj, "year")
+            date_to = fields.Date.end_of(date_obj, "year")
+        date_range = sequence_range.search(
+            [
+                ("sequence_id", "=", self.id),
+                ("date_from", ">=", date),
+                ("date_from", "<=", date_to),
+            ],
+            order="date_from desc",
+            limit=1,
+        )
         if date_range:
-            date_to = date_range.date_from + relativedelta(days=-1)
-        date_range = self.env['ir.sequence.date_range'].search([('sequence_id', '=', self.id), ('date_to', '>=', date_from), ('date_to', '<=', date)], order='date_to desc', limit=1)
+            date_to = fields.Date.subtract(date_range.date_from, days=1)
+        date_range = sequence_range.search(
+            [
+                ("sequence_id", "=", self.id),
+                ("date_to", ">=", date_from),
+                ("date_to", "<=", date),
+            ],
+            order="date_to desc",
+            limit=1,
+        )
         if date_range:
-            date_from = date_range.date_to + relativedelta(days=1)
-        seq_date_range = self.env['ir.sequence.date_range'].sudo().create({
-            'date_from': date_from,
-            'date_to': date_to,
-            'sequence_id': self.id,
-        })
+            date_to = fields.Date.add(date_range.date_to, days=1)
+        sequence_range_vals = {
+            "date_from": date_from,
+            "date_to": date_to,
+            "sequence_id": self.id,
+        }
+        seq_date_range = sequence_range.sudo().create(sequence_range_vals)
         return seq_date_range
 
     def _next(self, sequence_date=None):
