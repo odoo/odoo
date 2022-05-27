acba1c83144872a47c36289af95bef3242a5bcbf
Kevin Baptiste <kba@odoo.com>
2022-05-27 14:18:35 +0000

    [ADD] l10n_mx_hr: add specific employee fields
    Add fields specific to Mexico.
    
    closes odoo/odoo#92393
    
    Taskid: 2859750
    Related: odoo/enterprise#27892
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr/models/res_company.py
+++ addons/hr/models/res_company.py
@@ -9,3 +9,23 @@ class Company(models.Model):
 
     hr_presence_control_email_amount = fields.Integer(string="# emails to send")
     hr_presence_control_ip_list = fields.Char(string="Valid IP addresses")
+
+    def _install_hr_localizations(self):
+        if any(c.partner_id.country_id.code == 'MX' for c in self):
+            l10n_mx = self.env['ir.module.module'].sudo().search([
+                ('name', '=', 'l10n_mx_hr'),
+                ('state', 'not in', ['installed', 'to install', 'to upgrade']),
+            ])
+            if l10n_mx:
+                l10n_mx.button_immediate_install()
+
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        res._install_hr_localizations()
+        return res
+
+    def write(self, vals):
+        res = super().write(vals)
+        if 'country_id' in vals:
+            self._install_hr_localizations()
+        return res
