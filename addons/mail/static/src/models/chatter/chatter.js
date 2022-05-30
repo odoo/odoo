3f1162a6be9aedaaff24ce2d2547b776044e872c
Radhika Shyara <rash@odoo.com>
2022-05-30 06:20:17 +0000

    [FIX] mail: check if the user can access to the thread
    Before this commit, when the user creates a private task
    and assign another user to that task and then removes
    himself and save the task, the current user should no
    longer access to that task since the task becomes this
    private task of the other user assigned on it.
    So when the current user changes anything in that task,
    it will have a `Access Error`.
    However if the user tries to remove a follower on that
    task he will also have a traceback because no thread is found.
    The reason why no thread is found is because the user has
    no longer access to the record and so he has no longer
    access to the chatter.
    
    This commit checks if a thread is found before continuing
    the process. If no thread is found then the user has no
    access to the chatter instead of throwing a traceback
    because no thread is found.
    
    task-2857229
    
    closes odoo/odoo#91728
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Co-authored-by: Prakash Prajapati <ppr@odoo.com>

--- addons/mail/static/src/models/chatter/chatter.js
+++ addons/mail/static/src/models/chatter/chatter.js
@@ -168,7 +168,7 @@ registerModel({
          * @returns {boolean}
          */
         _computeIsDisabled() {
-            return Boolean(!this.thread || this.thread.isTemporary);
+            return Boolean(!this.thread || this.thread.isTemporary || !this.thread.hasReadAccess);
         },
         /**
          * @private
