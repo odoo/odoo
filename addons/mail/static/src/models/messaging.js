6a3d9c11ff9039e5b47dbbf0e6acf221e6b713c2
Nemael <mapa@odoo.com>
2022-05-30 15:39:21 +0200

    [IMP] mail: introduce Emoji model
    Task-2365687

--- addons/mail/static/src/models/messaging.js
+++ addons/mail/static/src/models/messaging.js
@@ -372,6 +372,11 @@ registerModel({
             isCausal: true,
             readonly: true,
         }),
+        emojiRegistry: one('EmojiRegistry', {
+            default: insertAndReplace(),
+            isCausal: true,
+            readonly: true,
+        }),
         fetchImStatusTimer: one('Timer', {
             inverse: 'messagingOwnerAsFetchImStatusTimer',
             isCausal: true,
