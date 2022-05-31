295bfd4fd71702212a86458fea6ef46f6189b8c0
abd-msyukyu-odoo <abd@odoo.com>
2022-05-31 13:21:29 +0000

    [IMP] knowledge: explicit field tracking with read access
    Consider the following case:
    - Article A
      - Article B
        - Article C
    
    - User X has write access on B, but read access on C.
    
    -> User X moves B so that it is now a sibling of A.
    
    -> Since C is a child of B, it also underwent a change, which is caused by X.
    
    To clarify this situation and the fact that X does not have write access on C, the
    automatic field tracking message is made more explicit in this case (Changes of
    C are a side effect of the changes of B).
    
    Task-2858428
    
    X-original-commit: e6af7c8c6a2eb37038742ce93018a8eb8cfbf6d6
    Part-of: odoo/odoo#93088

--- addons/knowledge/models/knowledge_article.py
+++ addons/knowledge/models/knowledge_article.py
@@ -1413,6 +1413,15 @@ class Article(models.Model):
     # MAILING
     # ------------------------------------------------------------
 
+    def _mail_track(self, tracked_fields, initial):
+        changes, tracking_value_ids = super(Article, self)._mail_track(tracked_fields, initial)
+        if {'parent_id', 'root_article_id'} & changes and not self.user_has_write_access:
+            partner_name = self.env.user.partner_id.display_name
+            message_body = _("Logging changes from %(partner_name)s without write access on article %(article_name)s due to hierarchy tree update",
+                partner_name=partner_name, article_name=self.display_name)
+            self._track_set_log_message("<p>%s</p>" % message_body)
+        return changes, tracking_value_ids
+
     def _send_invite_mail(self, partners):
         # TDE NOTE: try to cleanup and batchize
         self.ensure_one()
