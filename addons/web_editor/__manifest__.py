dddbd8e5d86b931602f94f0b82f8d56ac077af86
Nicolas Bayet <nby@odoo.com>
2022-06-01 15:12:04 +0000

    [FIX] web_editor: prevent linkDialogCommand.js to be loaded in website
    Before this commit, the file `linkDialogCommand.js` was in the bundle
    `web_editor.assets_wysiwyg`. That bundle was loaded with the same
    file in the backend and frontend.
    That file has a dependency with another file that should only be loaded
    in the backend.
    
    After this commit, the file will only be loaded in the backend.
    
    Additionally, remove a related useless line in
    the bundle `web_editor.assets_wysiwyg`.
    
    X-original-commit: 535dc22ca6d70c559edcc96ee5f875a7eb04ffac

--- addons/web_editor/__manifest__.py
+++ addons/web_editor/__manifest__.py
@@ -26,7 +26,10 @@ Odoo Web Editor widget.
             'web_editor/static/src/xml/*.xml',
         ],
         'web_editor.assets_wysiwyg': [
+<<<<<<< HEAD
 
+=======
+>>>>>>> 99b8d7bfe63... temp
             # lib
             'web_editor/static/lib/cropperjs/cropper.css',
             'web_editor/static/lib/cropperjs/cropper.js',
@@ -93,6 +96,7 @@ Odoo Web Editor widget.
             'web_editor/static/lib/odoo-editor/src/utils/utils.js',
         ],
         'web.assets_backend': [
+            'web_editor/static/src/js/wysiwyg/linkDialogCommand.js',
             'web_editor/static/src/scss/web_editor.common.scss',
             'web_editor/static/src/scss/web_editor.backend.scss',
 
