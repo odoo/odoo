33061601bd1ddf4c03f870181c16eb8545ad6c81
Jigar Vaghela <jva@odoo.com>
2022-06-01 04:59:53 +0000

    [IMP] l10n_in_edi: raise error on authentication
    Before this commit
    ==================
    Error not raise In E-invoice(IN) config when authentication is failed
    
    After this commit
    =================
    Error raise in E-invoice(IN) config when authentication is failed
    
    closes odoo/odoo#99148
    
    X-original-commit: c01b26d3391b53468c3bee8ff0117e55f5115943
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_in_edi/models/res_config_settings.py
+++ addons/l10n_in_edi/models/res_config_settings.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models, fields
+from odoo.exceptions import UserError
 
 
 class ResConfigSettings(models.TransientModel):
@@ -17,3 +18,5 @@ class ResConfigSettings(models.TransientModel):
 
     def l10n_in_edi_test(self):
         self.env["account.edi.format"]._l10n_in_edi_authenticate(self.company_id)
+        if not self.company_id.sudo()._l10n_in_edi_token_is_valid():
+            raise UserError("Username/password are incorrect.")
