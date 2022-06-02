ad4e528deee002cd14d3564f9b72347d350f6dc4
Julien Castiaux <juc@odoo.com>
2022-06-02 12:30:50 +0000

    [FIX] test_http: missing import
    Fine-tunning of da8def8
    
    closes odoo/odoo#92775
    
    Signed-off-by: Julien Castiaux <juc@odoo.com>

--- odoo/addons/test_http/tests/__init__.py
+++ odoo/addons/test_http/tests/__init__.py
@@ -4,5 +4,6 @@ from . import test_error
 from . import test_greeting
 from . import test_misc
 from . import test_models
+from . import test_session
 from . import test_static
 from . import test_web_server
