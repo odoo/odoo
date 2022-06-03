5451660d5fd542778aabfd66a6a18ff0444aed5c
abd-msyukyu-odoo <abd@odoo.com>
2022-06-03 15:30:07 +0200

    [fix] web_editor, knowledge: prevent media toolbar on knowledge buttons

--- addons/knowledge/static/src/js/knowledge_clipboard_whitelist.js
+++ addons/knowledge/static/src/js/knowledge_clipboard_whitelist.js
@@ -1,8 +1,14 @@
 /** @odoo-module */
 
 import { CLIPBOARD_WHITELISTS } from '@web_editor/../lib/odoo-editor/src/OdooEditor';
+import { nonEditableMediaAncestorsSelectors } from '@web_editor/js/common/wysiwyg_utils';
 
 /**
  * Allow @see o_knowledge_ classes to be preserved in the editor on paste.
  */
 CLIPBOARD_WHITELISTS.classes.push(/^o_knowledge_/);
+
+nonEditableMediaAncestorsSelectors.push(
+    '.o_knowledge_file',
+    '.o_knowledge_toolbar',
+);
