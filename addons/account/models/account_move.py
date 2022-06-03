fe831de73a9c904cbed2ed0d6cf7b0614b34216e
Pierrot (prro) <prro@odoo.com>
2022-06-03 11:08:35 +0200

    [IMP] account: improve invoice UX
    Improvements to the account move form:
    - "Reconciled Entries" button -> "Reconciled Items", and goes now to the
        Journal Items list view grouped by Matching number
    - Removed the unwanted margin next to the "Journal Items" table (in the
        "Journal Items" tab)
    - the partner can be hidden, in the Journal Items list view
    
    task-2855459

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -5711,7 +5711,7 @@ class AccountMoveLine(models.Model):
         return ids
 
     def open_reconcile_view(self):
-        action = self.env['ir.actions.act_window']._for_xml_id('account.action_account_moves_all_a')
+        action = self.env['ir.actions.act_window']._for_xml_id('account.action_account_moves_all_grouped_matching')
         ids = self._reconciled_lines()
         action['domain'] = [('id', 'in', ids)]
         return action
