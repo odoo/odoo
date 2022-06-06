c3a07ba53ed6caf30a0a03871a56d4482bae5354
MAHAMADASIF ANSARI <maan@odoo.com>
2022-06-06 12:15:38 +0530

    [IMP] hr_skills_slides: prevent duplicate course lines
    Before this commit, when employees viewed content of the course, the
    'hr.resume.line' was created. That means if a course had 8 slides, on
    completion of this course employees would have 8 resume lines for the
    same course by the time they finished the course.
    
    This commit improves the behavior and only creates the new line when
    the whole course is completed by the employees. Apart from that, once
    the line is created and new content is added on the course, the course
    is still considered as completed and no new line will be added when
    employees "re-completes" the course.
    
    taskID-2869305

--- addons/hr_skills_slides/models/slide_channel.py
+++ addons/hr_skills_slides/models/slide_channel.py
@@ -10,12 +10,19 @@ class SlideChannelPartner(models.Model):
 
     def _recompute_completion(self):
         res = super(SlideChannelPartner, self)._recompute_completion()
-        partner_has_completed = {channel_partner.partner_id.id: channel_partner.channel_id for channel_partner in self}
+        partner_has_completed = {channel_partner.partner_id.id: channel_partner.channel_id for channel_partner in self.filtered('completed')}
         employees = self.env['hr.employee'].sudo().search([('user_id.partner_id', 'in', list(partner_has_completed.keys()))])
+        if not employees:
+            return res
+        line_type = self.env.ref('hr_skills_slides.resume_type_training', raise_if_not_found=False)
+        resume_line_vals = []
         for employee in employees:
-            line_type = self.env.ref('hr_skills_slides.resume_type_training', raise_if_not_found=False)
+            # If an employee has completed a course once, and new content is added afterwards, course is still considered
+            # as completed for an employee, so do not add duplicate skill(resume line) for the same course
             channel = partner_has_completed[employee.user_id.partner_id.id]
-            self.env['hr.resume.line'].create({
+            if employee.resume_line_ids.filtered(lambda skill: skill.display_type == 'course' and skill.channel_id == channel):
+                continue
+            resume_line_vals.append({
                 'employee_id': employee.id,
                 'name': channel.name,
                 'date_start': fields.Date.today(),
@@ -25,4 +32,6 @@ class SlideChannelPartner(models.Model):
                 'display_type': 'course',
                 'channel_id': channel.id
             })
+        if resume_line_vals:
+            self.env['hr.resume.line'].create(resume_line_vals)
         return res
