fbcadb5b8aea6c04b0319b951b40f6a741059319
Laurent Desausoi (lade) <lade@odoo.com>
2022-06-07 06:45:36 +0000

    [FIX] sale: allow down payment sections to be properly translated
    When generating an SO, the down payment section is translated into the language
    of the current user instead of the language of the partner.
    
    Step to reproduce the issue:
    1) Install the Sales module
    2) Install another language (e.g.: French) and change the language of a partner
    to this language.
    3) Create a quotation for the partner which you just changed its language (and
    confirm it)
    4) Create an invoice (down payment) of 50% for example (confirm the invoice)
    5) Create another invoice to complete the payment (confirm it too)
    6) Go the latest invoice and print it
    You should see that the "Down Payments" section is kept in English instead of
    the other language (e.g. French).
    
    Solution: The issue is simply that the translation of the concerned term is
    translated using the lang of the user instead of the lang of the partner. To
    fix this, we must update the current context to change the lang to the partner
    language (defaulting to the user language if the partner has no lang).
    The side effect is that in the frontend, the user will see this section in the
    language of the partner instead of his own (which may looks as a bug).
    Unfortunately, it is an inherent functionnality of reports, we cannot do it
    otherwise (we have the same issue with the label of the Down Payment).
    
    opw-2865865
    
    closes odoo/odoo#93198
    
    X-original-commit: 2859f2709f92f8947c1f81f52a580ad3510316eb
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>
    Signed-off-by: Desausoi Laurent (lade) <lade@odoo.com>

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -1297,6 +1297,7 @@ class SaleOrder(models.Model):
 
         :param optional_values: any parameter that should be added to the returned down payment section
         """
+        context = {'lang': self.partner_id.lang}
         down_payments_section_line = {
             'display_type': 'line_section',
             'name': _('Down Payments'),
@@ -1307,6 +1308,7 @@ class SaleOrder(models.Model):
             'price_unit': 0,
             'account_id': False
         }
+        del context
         if optional_values:
             down_payments_section_line.update(optional_values)
         return down_payments_section_line
