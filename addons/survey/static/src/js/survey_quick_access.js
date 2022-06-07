92704847ed3bb12bdc1eb0974383373311f71f1e
Adrien Schoffeniels <adsc@odoo.com>
2022-06-07 16:02:19 +0000

    [IMP] survey: handle not launched live sessions in quick access view
    PURPOSE:
    This commit allows to handle the situation in which the quick access
    link is used but the live session is not launched yet, by showing an
    appropriate error message and by allowing a user with sufficient rights
    to launch the live session directly from the quick access page.
    
    Since a session can be launched multiple times, the error related to
    closed sessions has been removed, so that the user with the rights to do
    it can also "relaunch" a session if it has been closed before.
    
    Task-2816685
    
    Part-of: odoo/odoo#88434

--- addons/survey/static/src/js/survey_quick_access.js
+++ addons/survey/static/src/js/survey_quick_access.js
@@ -8,9 +8,10 @@ publicWidget.registry.SurveyQuickAccessWidget = publicWidget.Widget.extend({
     events: {
         'click button[type="submit"]': '_onSubmit',
         'input #session_code': '_onSessionCodeInput',
+        'click .o_survey_launch_session': '_onLaunchSessionClick',
     },
 
-        //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
     // Widget
     //--------------------------------------------------------------------------
 
@@ -23,9 +24,7 @@ publicWidget.registry.SurveyQuickAccessWidget = publicWidget.Widget.extend({
             // Init event listener
             if (!self.readonly) {
                 $(document).on('keypress', self._onKeyPress.bind(self));
-            }
-
-            self.$('input').focus();
+            }       
         });
     },
 
@@ -36,8 +35,19 @@ publicWidget.registry.SurveyQuickAccessWidget = publicWidget.Widget.extend({
     // Handlers
     // -------------------------------------------------------------------------
 
+    _onLaunchSessionClick: async function () {
+        const sessionResult = await this._rpc({
+            'model': 'survey.survey',
+            'method': 'action_start_session',
+            'args': [[this.$('.o_survey_launch_session').data('surveyId')]],
+        });
+        window.location = sessionResult.url;
+    },
+
     _onSessionCodeInput: function () {
         this.el.querySelectorAll('.o_survey_error > span').forEach((elem) => elem.classList.add('d-none'));
+        this.$('.o_survey_launch_session').addClass('d-none');
+        this.$('button[type="submit"]').removeClass('d-none');
     },
 
     _onKeyPress: function (event) {
@@ -54,10 +64,10 @@ publicWidget.registry.SurveyQuickAccessWidget = publicWidget.Widget.extend({
 
     _submitCode: function () {
         var self = this;
-        this.$('.o_survey_error > span').addClass("d-none");
+        this.$('.o_survey_error > span').addClass('d-none');
         const sessionCodeInputVal = this.$('input#session_code').val().trim();
         if (!sessionCodeInputVal) {
-            self.$('.o_survey_session_error_invalid_code').removeClass("d-none");
+            self.$('.o_survey_session_error_invalid_code').removeClass('d-none');
             return;
         }
         this._rpc({
@@ -66,10 +76,15 @@ publicWidget.registry.SurveyQuickAccessWidget = publicWidget.Widget.extend({
             if (response.survey_url) {
                 window.location = response.survey_url;
             } else {
-                if (response.error && response.error === 'survey_session_closed') {
-                    self.$('.o_survey_session_error_closed').removeClass("d-none");
+                if (response.error && response.error === 'survey_session_not_launched') {
+                    self.$('.o_survey_session_error_not_launched').removeClass('d-none');
+                    if ("survey_id" in response) {
+                        self.$('button[type="submit"]').addClass('d-none');
+                        self.$('.o_survey_launch_session').removeClass('d-none');
+                        self.$('.o_survey_launch_session').data('surveyId', response.survey_id);
+                    }
                 } else {
-                    self.$('.o_survey_session_error_invalid_code').removeClass("d-none");
+                    self.$('.o_survey_session_error_invalid_code').removeClass('d-none');
                 }
             }
         });
