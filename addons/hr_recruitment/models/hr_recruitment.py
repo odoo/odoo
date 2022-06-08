39d16296d8f52ed362b4b9c409e66d57f804e89a
Yannick Tivisse <yti@odoo.com>
2022-06-08 16:32:30 +0200

    [FIX] hr_recruitment: Fix method to be ensure_one
    Purpose
    =======
    
    The current batched version is nonsense

--- addons/hr_recruitment/models/hr_recruitment.py
+++ addons/hr_recruitment/models/hr_recruitment.py
@@ -622,43 +622,39 @@ class Applicant(models.Model):
         return super(Applicant, self)._message_post_after_hook(message, msg_vals)
 
     def create_employee_from_applicant(self):
-        """ Create an hr.employee from the hr.applicants """
+        """ Create an employee from applicant """
+        self.ensure_one()
         self._check_interviewer_access()
 
-        for applicant in self:
-            contact_name = False
-            if applicant.partner_id:
-                address_id = applicant.partner_id.address_get(['contact'])['contact']
-                contact_name = applicant.partner_id.display_name
-            else:
-                if not applicant.partner_name:
-                    raise UserError(_('You must define a Contact Name for this applicant.'))
-                new_partner_id = self.env['res.partner'].create({
-                    'is_company': False,
-                    'type': 'private',
-                    'name': applicant.partner_name,
-                    'email': applicant.email_from,
-                    'phone': applicant.partner_phone,
-                    'mobile': applicant.partner_mobile
-                })
-                applicant.partner_id = new_partner_id
-                address_id = new_partner_id.address_get(['contact'])['contact']
-            if applicant.partner_name or contact_name:
-                employee_data = {
-                    'default_name': applicant.partner_name or contact_name,
-                    'default_job_id': applicant.job_id.id,
-                    'default_job_title': applicant.job_id.name,
-                    'default_address_home_id': address_id,
-                    'default_department_id': applicant.department_id.id or False,
-                    'default_address_id': applicant.company_id and applicant.company_id.partner_id
-                            and applicant.company_id.partner_id.id or False,
-                    'default_work_email': applicant.department_id and applicant.department_id.company_id
-                            and applicant.department_id.company_id.email or False,
-                    'default_work_phone': applicant.department_id.company_id.phone,
-                    'form_view_initial_mode': 'edit',
-                    'default_applicant_id': applicant.ids,
-                    }
-                    
+        contact_name = False
+        if self.partner_id:
+            address_id = self.partner_id.address_get(['contact'])['contact']
+            contact_name = self.partner_id.display_name
+        else:
+            if not self.partner_name:
+                raise UserError(_('You must define a Contact Name for this applicant.'))
+            new_partner_id = self.env['res.partner'].create({
+                'is_company': False,
+                'type': 'private',
+                'name': self.partner_name,
+                'email': self.email_from,
+                'phone': self.partner_phone,
+                'mobile': self.partner_mobile
+            })
+            self.partner_id = new_partner_id
+            address_id = new_partner_id.address_get(['contact'])['contact']
+        employee_data = {
+            'default_name': self.partner_name or contact_name,
+            'default_job_id': self.job_id.id,
+            'default_job_title': self.job_id.name,
+            'default_address_home_id': address_id,
+            'default_department_id': self.department_id.id,
+            'default_address_id': self.company_id.partner_id.id,
+            'default_work_email': self.department_id.company_id.email,
+            'default_work_phone': self.department_id.company_id.phone,
+            'form_view_initial_mode': 'edit',
+            'default_applicant_id': self.ids,
+        }
         dict_act_window = self.env['ir.actions.act_window']._for_xml_id('hr.open_view_employee_list')
         dict_act_window['context'] = employee_data
         return dict_act_window
