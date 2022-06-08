16ee5235e0a5805c65c71bab2c89a4603ed690b8
Yannick Tivisse <yti@odoo.com>
2022-06-08 14:33:39 +0000

    [MOV] hr_skills: Split model files according to coding guidelines
    Part-of: odoo/odoo#93126

---
+++ addons/hr_skills/models/hr_resume_line_type.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class ResumeLineType(models.Model):
+    _name = 'hr.resume.line.type'
+    _description = "Type of a resumé line"
+    _order = "sequence"
+
+    name = fields.Char(required=True)
+    sequence = fields.Integer('Sequence', default=10)
