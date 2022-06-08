65ed78f1151827ae737689555a4b8682e17071cc
Romeo Fragomeli <rfr@odoo.com>
2022-06-08 11:51:22 +0200

    [FIX] web: missing/linting ending square brackets
    Interrestingly enough, those missing ending square bracketsdoesn't have
    consequences on the way the browser interprets these attribute
    selectors... Anyway, for consistency sake, let's close them properly.

--- addons/web/static/tests/legacy/widgets/week_days_tests.js
+++ addons/web/static/tests/legacy/widgets/week_days_tests.js
@@ -89,31 +89,31 @@ odoo.define('web.week_days_tests', function (require) {
             assert.containsNone(form, '.custom-control input:disabled', 7,
                 "all inputs should be enabled in readonly mode");
 
-            await testUtils.dom.click(form.el.querySelector('input[id^="sun"'));
-            assert.ok(form.el.querySelector('input[id^="sun"').checked,
+            await testUtils.dom.click(form.el.querySelector('input[id^="sun"]'));
+            assert.ok(form.el.querySelector('input[id^="sun"]').checked,
                 "sunday checkbox should be checked");
             await testUtils.form.clickSave(form);
 
             await testUtils.form.clickEdit(form);
-            await testUtils.dom.click(form.el.querySelector('input[id^="mon"'));
-            assert.ok(form.el.querySelector('input[id^="mon"').checked,
+            await testUtils.dom.click(form.el.querySelector('input[id^="mon"]'));
+            assert.ok(form.el.querySelector('input[id^="mon"]').checked,
                 "monday checkbox should be checked");
 
-            await testUtils.dom.click(form.el.querySelector('input[id^="tue"'));
-            assert.ok(form.el.querySelector('input[id^="tue"').checked,
+            await testUtils.dom.click(form.el.querySelector('input[id^="tue"]'));
+            assert.ok(form.el.querySelector('input[id^="tue"]').checked,
                 "tuesday checkbox should be checked");
 
             // uncheck Sunday checkbox and check write call
-            await testUtils.dom.click(form.el.querySelector('input[id^="sun"'));
-            assert.notOk(form.el.querySelector('input[id^="sun"').checked,
+            await testUtils.dom.click(form.el.querySelector('input[id^="sun"]'));
+            assert.notOk(form.el.querySelector('input[id^="sun"]').checked,
                 "sunday checkbox should be unchecked");
 
             await testUtils.form.clickSave(form);
-            assert.notOk(form.el.querySelector('input[id^="sun"').checked,
+            assert.notOk(form.el.querySelector('input[id^="sun"]').checked,
                 "sunday checkbox should be unchecked");
-            assert.ok(form.el.querySelector('input[id^="mon"').checked,
+            assert.ok(form.el.querySelector('input[id^="mon"]').checked,
                 "monday checkbox should be checked");
-            assert.ok(form.el.querySelector('input[id^="tue"').checked,
+            assert.ok(form.el.querySelector('input[id^="tue"]').checked,
                 "tuesday checkbox should be checked");
 
             form.destroy();
