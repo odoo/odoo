d05d694d38778140b16928c67db8c639110c9848
Alexandre Kühn <aku@odoo.com>
2022-06-08 17:27:05 +0200

    [IMP] im_livechat, *: introduce LivechatButtonView/buttonText
    *: website_livechat
    
    Task-2878976

--- addons/im_livechat/static/src/models/livechat_button_view.js
+++ addons/im_livechat/static/src/models/livechat_button_view.js
@@ -1,8 +1,23 @@
 /** @odoo-module **/
 
 import { registerModel } from '@mail/model/model_core';
+import { attr } from '@mail/model/model_field';
 
 registerModel({
     name: 'LivechatButtonView',
     identifyingFields: ['messaging'],
+    recordMethods: {
+        /**
+         * @private
+         * @returns {string}
+         */
+        _computeButtonText() {
+            return this.env._t("Chat with one of our collaborators");
+        },
+    },
+    fields: {
+        buttonText: attr({
+            compute: '_computeButtonText',
+        }),
+    },
 });
