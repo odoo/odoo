cdaf13753de2dddde2bbfd0f5dd1db8a48eb3cdb
Sébastien Geelen (sge) <sge@odoo.com>
2022-06-09 13:31:41 +0000

    [FIX] web_editor: ensure no root inline node
    During Editor initialisation, detect if the editable element contain orphan inline nodes.
    If so we transform the base element HTML to put those orphans inside `<p>` containers.
    
    This is used to ensure the HTML generated by Etherpad is compatible with this editor,
    allowing the user to use all the features without loosing the Etherpad look.
    
    task-2877273
    
    closes odoo/odoo#93207
    
    closes odoo/odoo#95347
    
    Related: odoo/enterprise#28965
    Related: odoo/enterprise#29130
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    
    # Conflicts:
    #	addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -2698,6 +2698,42 @@ export class OdooEditor extends EventTarget {
      *
      */
     initElementForEdition(element = this.editable) {
+        // Detect if the editable base element contain orphan inline nodes. If
+        // so we transform the base element HTML to put those orphans inside
+        // `<p>` containers.
+        const orphanInlineChildNodes = [...element.childNodes].find(
+            (n) => !isBlock(n) && (n.nodeType === Node.ELEMENT_NODE || n.textContent.trim() !== "")
+        );
+        if (orphanInlineChildNodes) {
+            const childNodes = [...element.childNodes];
+            const tempEl = document.createElement('temp-container');
+            let currentP = document.createElement('p');
+            currentP.style.marginBottom = '0';
+            do {
+                const node = childNodes.shift();
+                const nodeIsBlock = isBlock(node);
+                const nodeIsBR = node.nodeName === 'BR';
+                // Append to the P unless child is block or an unneeded BR.
+                if (!(nodeIsBlock || (nodeIsBR && currentP.childNodes.length))) {
+                    currentP.append(node);
+                }
+                // Break paragraphs on blocks and BR.
+                if (nodeIsBlock || nodeIsBR || childNodes.length === 0) {
+                    // Ensure we don't add an empty P or a P containing only
+                    // formating spaces that should not be visible.
+                    if (currentP.childNodes.length && currentP.innerHTML.trim() !== '') {
+                        tempEl.append(currentP);
+                    }
+                    currentP = currentP.cloneNode();
+                    // Append block children directly to the template.
+                    if (nodeIsBlock) {
+                        tempEl.append(node);
+                    }
+                }
+            } while (childNodes.length)
+            element.replaceChildren(...tempEl.childNodes);
+        }
+
         // Flag elements with forced contenteditable=false.
         // We need the flag to be able to leave the contentEditable
         // at the end of the edition (see cleanForSave())
