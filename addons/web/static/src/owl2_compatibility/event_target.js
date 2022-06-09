717e2f9a17a02bfa4d0f2a70d95c05bbcc013103
Géry Debongnie <ged@odoo.com>
2022-06-09 09:35:34 +0200

    [FIX] web: make owl 2 compatibility layer more solid
    With this commit, we improve the event target override (made to support
    owl 1 syntax in owl 2) to use a weakmap instead of a map. This should
    help solving issues in some cases where we have some code that do not
    call the 'off' method, but where the target is garbage collected. It
    probably mostly occurs in tests.

--- addons/web/static/src/owl2_compatibility/event_target.js
+++ addons/web/static/src/owl2_compatibility/event_target.js
@@ -10,23 +10,39 @@
     owl.EventBus = class extends EventBus {
         constructor(...args) {
             super(...args);
-            this.targetsCallbacks = new Map();
+            this.targetsCallbacks = new WeakMap();
         }
         on(type, target, callback) {
+            const _callback = wrapCallback(target, callback);
+            if (!target) {
+                return this.addEventListener(type, _callback);
+            }
             if (!this.targetsCallbacks.has(target)) {
                 this.targetsCallbacks.set(target, []);
             }
-            callback = wrapCallback(target, callback);
-            this.targetsCallbacks.get(target).push(callback);
+            this.targetsCallbacks.get(target).push({ type, callback: _callback });
             return this.addEventListener(type, callback);
         }
         off(type, target) {
+            if (!target) {
+                console.warn("unable to remove handlers: invalid target");
+                return;
+            }
             const cbs = this.targetsCallbacks.get(target);
             if (!cbs) {
                 return;
             }
-            for (const callback of cbs) {
-                this.removeEventListener(type, callback);
+            const newCbs = cbs.filter((cb) => {
+                if (cb.type === type) {
+                    this.removeEventListener(cb.type, cb.callback);
+                    return false;
+                }
+                return true;
+            });
+            if (newCbs.length) {
+                this.targetsCallbacks.set(target, newCbs);
+            } else {
+                this.targetsCallbacks.delete(target);
             }
         }
     };
