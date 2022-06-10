ef798dba91050fc6ccef55b1c49618847c524de2
Paolo (pgi) <pgi@odoo.com>
2022-06-10 15:20:56 +0000

    [IMP] l10n_it_edi, l10n_it_edi_sdicoop: External Reverse Charge
    From July 2022, External Reverse Charge (for Import/Export)
    has to be sent through the SdI to the Agenzia delle entrate.
    Exports will be 0% VAT
        (Reverse charged to the buyer)
    Imports will have a -100%/+100% VAT tax
        (We are the buyers, and in charge of VAT)
    
    See specification in the Task description.
    Task: https://www.odoo.com/web#id=2823646&model=project.task
    
    opw-2823646
    
    closes odoo/odoo#95372
    
    X-original-commit: 2eb8b2f5bcef8ddfbc3ad83219368575174918d0
    Related: odoo/enterprise#29138
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>

--- addons/l10n_it_edi/__init__.py
+++ addons/l10n_it_edi/__init__.py
@@ -3,3 +3,23 @@
 
 from . import models
 from . import tools
+
+from odoo import api, SUPERUSER_ID
+
+
+def _l10n_it_edi_update_export_tax(env):
+    chart_template = env.ref('l10n_it.l10n_it_chart_template_generic', raise_if_not_found=False)
+    if chart_template:
+        for company in env['res.company'].search([('chart_template_id', '=', chart_template.id)]):
+            tax = env.ref(f'l10n_it.{company.id}_00eu', raise_if_not_found=False)
+            if tax:
+                tax.write({
+                    'l10n_it_has_exoneration': True,
+                    'l10n_it_kind_exoneration': 'N3.2',
+                    'l10n_it_law_reference': 'Art. 41, DL n. 331/93',
+                })
+
+
+def _l10n_it_edi_post_init(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    _l10n_it_edi_update_export_tax(env)
