b0830ab79b306ccb0e38faee20d4871d4a1deca8
Sébastien Theys <seb@odoo.com>
2022-06-10 11:30:02 +0200

    [FIX] mail: correct unlink in _mockMailThreadMessageUnsubscribe
    X-original-commit: 21fb3eb971b4072ff2eb021491c2b000d5d04946

--- addons/mail/static/tests/helpers/mock_server.js
+++ addons/mail/static/tests/helpers/mock_server.js
@@ -1872,7 +1872,7 @@ MockServer.include({
             ['res_id', 'in', ids],
             ['partner_id', 'in', partner_ids || []],
         ]);
-        this._mockUnlink(model, [followers.map(follower => follower.id)]);
+        this._mockUnlink('mail.followers', [followers.map(follower => follower.id)]);
     },
     /**
      * Simulates `_get_channels_as_member` on `res.partner`.
