4918c1a87b8ae74577c171ad934b1a12a93e4b5a
Yannick Tivisse <yti@odoo.com>
2022-06-10 12:20:19 +0000

    [IMP] hr_skills: Add skills history reporting
    closes odoo/odoo#93312
    
    Taskid: 2868601
    Related: odoo/enterprise#28249
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_skills/models/__init__.py
+++ addons/hr_skills/models/__init__.py
@@ -5,8 +5,9 @@ from . import hr_employee
 from . import hr_employee_public
 from . import hr_resume_line
 from . import hr_resume_line_type
-from . import hr_skills
+from . import hr_skill
 from . import hr_employee_skill
+from . import hr_employee_skill_log
 from . import hr_skill_level
 from . import hr_skill_type
 from . import res_users
