d315ad8a51840a40b5a6d1e1525e71e60bfefbae
Laurent Desausoi (lade) <lade@odoo.com>
2022-06-13 19:18:08 +0200

    [FIX] mail: properly place dropdowns in Kanban Activity views
    For the kanban_activity view (e.g.: in the CRM module), when we have enough
    entries to require the scrolling, dropdowns are not displayed at the correct
    location.
    Furthermore, in kanban views, when dropdowns are opening on the edge of the
    parent element (e.g.: in Documents), the dropdowns are partially hidden and not
    accessible by the client.
    
    Step to reproduce the issue:
    1) Install CRM and Documents module
    2) Go to CRM and create multiple leads with at least one activity
    3) Go to the Activity view and scroll down and click on an item. The dropdown
    is not placed at the correct location -> BUG 1
    4) Go to the Documents modules and check the activity (the small clock icon)
    of a Document on the right most column. The dropdown is hidden by the
    description of the file.
    
    Solution: Firstly, the parent container of the Kanban Activity view did not had
    the `overflow: auto` property activated. Consequently, while the children were
    somehow taking all the spaces of the screen, Popper (the third party that
    handles the dynamic positioning of the dropdowns) was wrongly computing the
    positionning.
    Secondly, dynamically creating the dropdown via Javascript (through the
    JQueryInterface of the Dropdown class in Bootstrap) somehow allows to fix the
    issue in Documents. Consequently, it is important to keep them into the
    Javascript and not move the arguments into the XML.
    
    opw-2827873
    
    X-original-commit: 38fa6a70b70dac0d511be7952c496a5178f9c40e

--- addons/mail/static/src/js/activity.js
+++ addons/mail/static/src/js/activity.js
@@ -610,6 +610,10 @@ var KanbanActivity = BasicActivity.extend({
      */
     _renderDropdown: function () {
         var self = this;
+        this.$el.dropdown({
+            boundary: 'viewport',
+            flip: false,
+        });
         this.$('.o_activity')
             .toggleClass('dropdown-menu-right', config.device.isMobile)
             .html(QWeb.render('mail.KanbanActivityLoading'));
