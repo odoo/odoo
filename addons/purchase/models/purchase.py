2ad9079ef384f9615619bad3658f62342f22ec34
Thomas Beckers <tbs@odoo.com>
2022-06-14 14:12:58 +0000

    [FIX] purchase: don't mix PO's lines in generated invoices
    If we use the auto-complete feature to add PO's lines to an bill or we select multiple PO's then use
    'create bill' button, the generated invoice line will copy the sequence line from the PO's line. This
    can lead to situation where we will have all the first lines of each PO then all the second, etc, ending
    with a mix of all PO's in the bill.
    
    Example:
    Purchase order 1
    - seq 10 line A
    - seq 11 line B
    - seq 12 line C
    
    Purchase order 2
    - seq 10 line A'
    - seq 11 line B'
    - seq 12 line C'
    
    Invoice created from those PO's
    - seq 10 PO1:line A
    - seq 10 PO2:line A'
    - seq 11 PO1:line B
    - seq 11 PO2:line B'
    - seq 12 PO1:line C
    - seq 12 PO2:line C'
    
    After this PR this PR the lines from the same PO's will be contiguous like:
    
    Invoice created from those PO 1 and 2
    - seq 10 PO1:line A
    - seq 11 PO1:line B
    - seq 12 PO1:line C
    - seq 13 PO2:line A'
    - seq 14 PO2:line B'
    - seq 15 PO2:line C'
    
    opw-2749682
    
    closes odoo/odoo#95052
    
    X-original-commit: 49b143e1b7f12ee6cf4d14754904f3e9b64a1011
    Signed-off-by: Steve Van Essche <svs@odoo.com>
    Signed-off-by: Beckers Thomas (tbs) <tbs@odoo.com>

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -509,6 +509,7 @@ class PurchaseOrder(models.Model):
 
         # 1) Prepare invoice vals and clean-up the section lines
         invoice_vals_list = []
+        sequence = 10
         for order in self:
             if order.invoice_status != 'to invoice':
                 continue
@@ -524,9 +525,15 @@ class PurchaseOrder(models.Model):
                     continue
                 if not float_is_zero(line.qty_to_invoice, precision_digits=precision):
                     if pending_section:
-                        invoice_vals['invoice_line_ids'].append((0, 0, pending_section._prepare_account_move_line()))
+                        line_vals = pending_section._prepare_account_move_line()
+                        line_vals.update({'sequence': sequence})
+                        invoice_vals['invoice_line_ids'].append((0, 0, line_vals))
+                        sequence += 1
                         pending_section = None
-                    invoice_vals['invoice_line_ids'].append((0, 0, line._prepare_account_move_line()))
+                    line_vals = line._prepare_account_move_line()
+                    line_vals.update({'sequence': sequence})
+                    invoice_vals['invoice_line_ids'].append((0, 0, line_vals))
+                    sequence += 1
             invoice_vals_list.append(invoice_vals)
 
         if not invoice_vals_list:
