149b18958ef0306db22a94919e9ca8e87acdc4ba
Géry Debongnie <ged@odoo.com>
2022-06-15 09:03:45 +0000

    [REF] barcodes: simplify and improve barcode implementation
    - use keydown events all the time
    - remove dead code
    - simplify implementation
    
    Part-of: odoo/odoo#93387

--- addons/barcodes/static/src/js/barcode_events.js
+++ addons/barcodes/static/src/js/barcode_events.js
@@ -3,20 +3,9 @@ odoo.define('barcodes.BarcodeEvents', function(require) {
 
 var config = require('web.config');
 var core = require('web.core');
-var mixins = require('web.mixins');
 var session = require('web.session');
 
-
-// For IE >= 9, use this, new CustomEvent(), instead of new Event()
-function CustomEvent ( event, params ) {
-    params = params || { bubbles: false, cancelable: false, detail: undefined };
-    var evt = document.createEvent( 'CustomEvent' );
-    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-    return evt;
-   }
-CustomEvent.prototype = window.Event.prototype;
-
-var BarcodeEvents = core.Class.extend(mixins.PropertiesMixin, {
+var BarcodeEvents = core.Class.extend({
     timeout: null,
     key_pressed: {},
     buffered_key_events: [],
@@ -37,38 +26,24 @@ var BarcodeEvents = core.Class.extend(mixins.PropertiesMixin, {
     inputTimeOut: 800,
 
     init: function() {
-        mixins.PropertiesMixin.init.call(this);
-        // Keep a reference of the handler functions to use when adding and removing event listeners
-        this.__keydown_handler = _.bind(this.keydown_handler, this);
-        this.__keyup_handler = _.bind(this.keyup_handler, this);
         this.__handler = _.bind(this.handler, this);
         // Bind event handler once the DOM is loaded
         // TODO: find a way to be active only when there are listeners on the bus
-        $(_.bind(this.start, this, false));
+        $(() => this.start());
 
         // Mobile device detection
         this.isChromeMobile = config.device.isMobileDevice && navigator.userAgent.match(/Chrome/i);
+    },
 
-        // Creates an input who will receive the barcode scanner value.
-        this.$barcodeInput = $('<input/>', {
-            name: 'barcode',
-            type: 'text',
-            css: {
-                'position': 'fixed',
-                'top': '50%',
-                'transform': 'translateY(-50%)',
-                'z-index': '-1',
-                'opacity': '0',
-            },
-        });
-        // Avoid to show autocomplete for a non appearing input
-        this.$barcodeInput.attr('autocomplete', 'off');
-
-        this.__blurBarcodeInput = _.debounce(this._blurBarcodeInput, this.inputTimeOut);
+    getCurrentString() {
+        if (this.$barcodeInput) {
+            return this.$barcodeInput.val();
+        }
+        return this.buffered_key_events.reduce(function(memo, e) { return memo + e.key }, '');
     },
 
     handle_buffered_keys: function() {
-        var str = this.buffered_key_events.reduce(function(memo, e) { return memo + String.fromCharCode(e.which) }, '');
+        let str = this.getCurrentString();
         var match = str.match(this.regexp);
 
         if (match) {
@@ -81,46 +56,9 @@ var BarcodeEvents = core.Class.extend(mixins.PropertiesMixin, {
             // Dispatch a barcode_scanned DOM event to elements that have barcode_events="true" set.
             if (this.buffered_key_events[0].target.getAttribute("barcode_events") === "true")
                 $(this.buffered_key_events[0].target).trigger('barcode_scanned', barcode);
-        } else {
-            this.resend_buffered_keys();
         }
 
         this.buffered_key_events = [];
-    },
-
-    resend_buffered_keys: function() {
-        var old_event, new_event;
-        for(var i = 0; i < this.buffered_key_events.length; i++) {
-            old_event = this.buffered_key_events[i];
-
-            if(old_event.which !== 13) { // ignore returns
-                // We do not create a 'real' keypress event through
-                // eg. KeyboardEvent because there are several issues
-                // with them that make them very different from
-                // genuine keypresses. Chrome per example has had a
-                // bug for the longest time that causes keyCode and
-                // charCode to not be set for events created this way:
-                // https://bugs.webkit.org/show_bug.cgi?id=16735
-                var params = {
-                    'bubbles': old_event.bubbles,
-                    'cancelable': old_event.cancelable,
-                };
-                new_event = $.Event('keypress', params);
-                new_event.viewArg = old_event.viewArg;
-                new_event.ctrl = old_event.ctrl;
-                new_event.alt = old_event.alt;
-                new_event.shift = old_event.shift;
-                new_event.meta = old_event.meta;
-                new_event.char = old_event.char;
-                new_event.key = old_event.key;
-                new_event.charCode = old_event.charCode;
-                new_event.keyCode = old_event.keyCode || old_event.which; // Firefox doesn't set keyCode for keypresses, only keyup/down
-                new_event.which = old_event.which;
-                new_event.dispatched_by_barcode_reader = true;
-
-                $(old_event.target).trigger(new_event);
-            }
-        }
     },
 
     element_is_editable: function(element) {
@@ -138,6 +76,7 @@ var BarcodeEvents = core.Class.extend(mixins.PropertiesMixin, {
             e.key === "Backspace" || e.key === "Delete" ||
             e.key === "Home" || e.key === "End" ||
             e.key === "PageUp" || e.key === "PageDown" ||
+            e.key === "Shift" ||
             e.key === "Unidentified" || /F\d\d?/.test(e.key)) {
             return true;
         } else {
@@ -145,46 +84,27 @@ var BarcodeEvents = core.Class.extend(mixins.PropertiesMixin, {
         }
     },
 
-    // The keydown and keyup handlers are here to disallow key
-    // repeat. When preventDefault() is called on a keydown event
-    // the keypress that normally follows is cancelled.
-    keydown_handler: function(e){
-        if (this.key_pressed[e.which]) {
-            e.preventDefault();
-        } else {
-            this.key_pressed[e.which] = true;
-        }
-    },
-
-    keyup_handler: function(e){
-        this.key_pressed[e.which] = false;
-    },
-
     handler: function(e){
-        // Don't catch events we resent
-        if (e.dispatched_by_barcode_reader)
-            return;
         // Don't catch non-printable keys for which Firefox triggers a keypress
-        if (this.is_special_key(e))
+        if (this.is_special_key(e)) {
             return;
+        }
         // Don't catch keypresses which could have a UX purpose (like shortcuts)
-        if (e.ctrlKey || e.metaKey || e.altKey)
+        if (e.ctrlKey || e.metaKey || e.altKey) {
             return;
-        // Don't catch Return when nothing is buffered. This way users
-        // can still use Return to 'click' on focused buttons or links.
-        if (e.which === 13 && this.buffered_key_events.length === 0)
-            return;
+        }
         // Don't catch events targeting elements that are editable because we
         // have no way of redispatching 'genuine' key events. Resent events
         // don't trigger native event handlers of elements. So this means that
         // our fake events will not appear in eg. an <input> element.
-        if ((this.element_is_editable(e.target) && !$(e.target).data('enableBarcode')) && e.target.getAttribute("barcode_events") !== "true")
+        if ((this.element_is_editable(e.target) && !$(e.target).data('enableBarcode')) && e.target.getAttribute("barcode_events") !== "true") {
             return;
+        }
 
         // Catch and buffer the event
-        this.buffered_key_events.push(e);
-        e.preventDefault();
-        e.stopImmediatePropagation();
+        if (e.key !== "Enter") {
+            this.buffered_key_events.push(e);
+        }
 
         // Handle buffered keys immediately if the keypress marks the end
         // of a barcode or after x milliseconds without a new keypress
@@ -215,37 +135,12 @@ var BarcodeEvents = core.Class.extend(mixins.PropertiesMixin, {
     _listenBarcodeScanner: function (e) {
         if ($(document.activeElement).not('input:text, textarea, [contenteditable], ' +
             '[type="email"], [type="number"], [type="password"], [type="tel"], [type="search"]').length) {
-            $('body').append(this.$barcodeInput);
             this.$barcodeInput.focus();
         }
         if (this.$barcodeInput.is(":focus")) {
-            // Handle buffered keys immediately if the keypress marks the end
-            // of a barcode or after x milliseconds without a new keypress.
-            clearTimeout(this.timeout);
-            // On chrome mobile, e.which only works for some special characters like ENTER or TAB.
-            if (String.fromCharCode(e.which).match(this.suffix)) {
-                this._handleBarcodeValue(e);
-            } else {
-                this.timeout = setTimeout(this._handleBarcodeValue.bind(this, e),
-                    this.max_time_between_keys_in_ms);
-            }
+            this.handler(e);
             // if the barcode input doesn't receive keydown for a while, remove it.
             this.__blurBarcodeInput();
-        }
-    },
-
-    /**
-     * Retrieves the barcode value from the temporary input element.
-     * This checks this value and trigger it on the bus.
-     *
-     * @private
-     * @param  {jQuery.Event} keydown event
-     */
-    _handleBarcodeValue: function (e) {
-        var barcodeValue = this.$barcodeInput.val();
-        if (barcodeValue.match(this.regexp)) {
-            core.bus.trigger('barcode_scanned', barcodeValue, $(e.target).parent()[0]);
-            this._blurBarcodeInput();
         }
     },
 
@@ -262,7 +157,7 @@ var BarcodeEvents = core.Class.extend(mixins.PropertiesMixin, {
         this.$barcodeInput.val('').blur();
     },
 
-    start: function(prevent_key_repeat){
+    start: function(){
         // Chrome Mobile isn't triggering keypress event.
         // This is marked as Legacy in the DOM-Level-3 Standard.
         // See: https://www.w3.org/TR/uievents/#legacy-keyboardevent-event-types
@@ -270,20 +165,35 @@ var BarcodeEvents = core.Class.extend(mixins.PropertiesMixin, {
         // all other cases.
         // In master, we could remove the behavior with keypress and only use keydown.
         if (this.isChromeMobile) {
+            // Creates an input who will receive the barcode scanner value.
+            this.$barcodeInput = $('<input/>', {
+                name: 'barcode',
+                type: 'text',
+                css: {
+                    'position': 'fixed',
+                    'top': '50%',
+                    'transform': 'translateY(-50%)',
+                    'z-index': '-1',
+                    'opacity': '0',
+                },
+            });
+            // Avoid to show autocomplete for a non appearing input
+            this.$barcodeInput.attr('autocomplete', 'off');
+
+            this.__blurBarcodeInput = _.debounce(this._blurBarcodeInput, this.inputTimeOut);
+
+
+            $('body').append(this.$barcodeInput);
+            core.bus.on('barcode_scanned', null, () => this._blurBarcodeInput());
+
             $('body').on("keydown", this._listenBarcodeScanner.bind(this));
         } else {
-            $('body').bind("keypress", this.__handler);
-        }
-        if (prevent_key_repeat === true) {
-            $('body').bind("keydown", this.__keydown_handler);
-            $('body').bind('keyup', this.__keyup_handler);
+            $('body').bind("keydown", this.__handler);
         }
     },
 
     stop: function(){
-        $('body').off("keypress", this.__handler);
-        $('body').off("keydown", this.__keydown_handler);
-        $('body').off('keyup', this.__keyup_handler);
+        $('body').off("keydown", this.__handler);
     },
 });
 
