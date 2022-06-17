5b2c1d243c0dd5b049ceae280dd270fc8a59f955
roen-odoo <roen@odoo.com>
2022-06-17 11:11:40 +0000

    [FIX] account: correctly reconcile refund from PoS
    Current behavior:
    In the PoS if you use 2 differents payment methods (cash and customer
    account), and that the cash amount given by the client require you to
    give some money back, the amount due on the customer account would be
    incorrect on the invoice.
    
    Steps to reproduce:
    - Start a PoS session
    - Add the 750$ desk to the order
    - Go in the payment screen
    - Add customer account with 300$
    - Add cash with 460$
    - Click on invoice
    - The invoice show that the client need to pay 290$ which is not
      correct
    
    Before the fix calling `_prepare_reconciliation_partials()`
    with the sorted lines we had 2 credit lines (750€ and 10€)
    and 1 debit line (460€).
    
    Debit lines     |   Credit lines
    460€            |   750€
                    |   10€
    
    The function would reconcile the 2 first lines and stop after that
    because the only debit line was fully reconciled
    
    After the fix we have this
    
    Debit lines     |   Credit lines
    460€            |   10€
                    |   750€
    
    After reconciling the 2 first lines we have this
    
    Debit lines     |   Credit lines
    450€            |   750€
    
    And after reconciling the 2 last lines we have the correct amount left
    to pay by the customer (300€)
    
    opw-2857064
    
    closes odoo/odoo#93910
    
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -5350,7 +5350,7 @@ class AccountMoveLine(models.Model):
                 raise UserError(_("Entries are not from the same account: %s != %s")
                                 % (account.display_name, line.account_id.display_name))
 
-        sorted_lines = self.sorted(key=lambda line: (line.date_maturity or line.date, line.currency_id))
+        sorted_lines = self.sorted(key=lambda line: (line.date_maturity or line.date, line.currency_id, line.amount_currency))
 
         # ==== Collect all involved lines through the existing reconciliation ====
 
