d6b84a983644bfd381b76c94fae64fd5751955ed
qsm-odoo <qsm@odoo.com>
2022-06-17 17:26:28 +0200

    [FIX] website_mass_mailing, *: fix race condition in newsletter tour
    *: website, test_website
    
    Since [1], a race condition seems to have become more dominant. It is
    actually due to a tour not properly awaiting the editor to be loaded
    before using it.
    
    This commit fixes all occurrences of such 'trigger' selectors, even in
    tours were it could actually be not a problem. That will prevent devs to
    copy paste the problematic trigger.
    
    [1]: https://github.com/odoo/odoo/commit/57793ff912ac5aab8f3d3e014992965d06290bf0
    
    X-original-commit: 38f3a94263b9cf48e9f5a8de553b8602310bcddd

--- addons/website/static/tests/tours/snippets_all_drag_and_drop.js
+++ addons/website/static/tests/tours/snippets_all_drag_and_drop.js
@@ -62,7 +62,7 @@ tour.register("snippets_all_drag_and_drop", {
 }, [
     {
         content: "Ensure snippets are actually passed at the test.",
-        trigger: "#oe_snippets",
+        trigger: "body",
         run: function () {
             // safety check, otherwise the test might "break" one day and
             // receive no steps. The test would then not test anything anymore
