8f27a2fe2c65e4d90b153daac080bcadea20c52d
Krina Oza <kro@odoo.com>
2022-06-17 16:59:59 +0530

    [IMP] hr_timesheet: use name_get to add employee's company
    Currently, if both companies have employees with the same name, it is not known which
    company the selected employee belongs to.
    
    After this commit:
        - The name of the company will only be appended by the name_get when more than
          one company is present in the database.
    
    Example
    - Administrator - My Company
    - Administrator - Tiny ERP

--- addons/hr_timesheet/models/hr_employee.py
+++ addons/hr_timesheet/models/hr_employee.py
@@ -10,3 +10,19 @@ class HrEmployee(models.Model):
     timesheet_cost = fields.Monetary('Cost', currency_field='currency_id',
     	groups="hr.group_hr_user", default=0.0)
     currency_id = fields.Many2one('res.currency', related='company_id.currency_id', readonly=True)
+
+    def name_get(self):
+        res = super().name_get()
+        if len(self.env.context.get('allowed_company_ids', [])) <= 1:
+            return res
+        name_mapping = dict(res)
+        employee_read_group = self.env['hr.employee'].sudo()._read_group(
+            [('user_id', 'in', self.user_id.ids)],
+            ['user_id'],
+            ['user_id'],
+        )
+        employees_count_per_user = {res['user_id'][0]: res['user_id_count'] for res in employee_read_group}
+        for employee in self:
+            if employees_count_per_user.get(employee.user_id.id, 0) > 1:
+                name_mapping[employee.id] = f'{name_mapping[employee.id]} - {employee.company_id.name}'
+        return list(name_mapping.items())
