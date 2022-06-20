64a0c0a78db292538a937ebb898bf2e9853112f5
Pierre-Yves Dufays <pydu@odoo.com>
2022-06-20 16:10:37 +0200

    [IMP] digest, various: improve digest
    We add the following KPIs:
    - In website: #visitors and #Page views
    - In stock: #Delivery and #Receipt
    
    We define a new method allowing to configure the KPIs and add the following
    feature based on it:
    - We add application icon on each kpi of the digest email to help the user to
    identify to which application each kpis belong.
    - We re-order the kpi in the email, placing the most interesting KPIs first.
    - We display a small badge next to KPIs that are computed "cross company".
    - We remove the method _compute_kpis_actions and integrate the configuration
    of the action directly in the new method.
    
    We improve the method _calculate_company_based_kpi to also support the cross
    company computation and rename it _calculate_kpi.
    
    We define a method _raise_if_not_member_of that can be called from the kpi
    computing method to skip kpi the user should not have access to.
    
    Technical note:
    - For the new website KPIs, we use raw sql queries for:
      - computing website visitor count because count distinct is not available in
      the ORM API
      - computing website track count because we cannot group by
      visitor_id.website_id with the ORM API
    - To get the correct KPI's image size on outlook, we have added the following
    attributes to the kpi icon image: valign="middle" width="30" height="30".
    - Module icon displayed in the mail can be overridden using
    OVERRIDDEN_ICON_BY_MODULE (currently used to replace the base module icon).
    - The new template variables: kpi_is_cross_company (for the all company badge)
    and kpi_icon (for displaying the icon) are optional in the template
    addons/digest/data/digest_data.xml as it is used also in massmailing where
    those variables are not defined (ex.: addons/mass_mailing/models/mailing.py:
    MailingMailing._prepare_statistics_email_values).
    - Unfortunately, the module sequence was not enough to give a good kpi order,
    so we have defined a sequence for each kpi through _get_kpi_custom_settings.
    
    various: account, crm, hr_recruitment, im_livechat, point_of_sale, project,
    sale_management, stock, website, website_sale
    
    Task-2705154
    
    Part-of: odoo/odoo#233752
    Related: odoo/enterprise#98455
    Related: odoo/upgrade#8734
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/project/models/digest_digest.py
+++ addons/project/models/digest_digest.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models, _
-from odoo.exceptions import AccessError
+from odoo import fields, models
 
 
 class DigestDigest(models.Model):
@@ -12,16 +11,16 @@ class DigestDigest(models.Model):
     kpi_project_task_opened_value = fields.Integer(compute='_compute_project_task_opened_value', export_string_translation=False)
 
     def _compute_project_task_opened_value(self):
-        if not self.env.user.has_group('project.group_project_user'):
-            raise AccessError(_("Do not have access, skip this data for user's digest email"))
-
-        self._calculate_company_based_kpi(
+        self._raise_if_not_member_of('project.group_project_user')
+        self._calculate_kpi(
             'project.task',
             'kpi_project_task_opened_value',
             additional_domain=[('stage_id.fold', '=', False), ('project_id', '!=', False)],
         )
 
-    def _compute_kpis_actions(self, company, user):
-        res = super()._compute_kpis_actions(company, user)
-        res['kpi_project_task_opened'] = 'project.open_view_project_all?menu_id=%s' % self.env.ref('project.menu_main_pm').id
+    def _get_kpi_custom_settings(self, company, user):
+        res = super()._get_kpi_custom_settings(company, user)
+        menu_id = self.env.ref('project.menu_main_pm').id
+        res['kpi_action']['kpi_project_task_opened'] = f'project.open_view_project_all?menu_id={menu_id}'
+        res['kpi_sequence']['kpi_project_task_opened'] = 7500
         return res
