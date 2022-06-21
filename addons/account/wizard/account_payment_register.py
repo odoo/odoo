da5e3df848895b69ea26a24ec9cfc5f45d85a794
Abdelouahab (abla) <abla@odoo.com>
2022-06-21 16:49:36 +0200

    change coming from future : https://github.com/odoo-dev/odoo/commit/539029ffa36f68a01a2c36016a9a6cb361197d66

--- addons/account/wizard/account_payment_register.py
+++ addons/account/wizard/account_payment_register.py
@@ -219,20 +219,53 @@ class AccountPaymentRegister(models.TransientModel):
             raise UserError(_("You can't open the register payment wizard without at least one receivable/payable line."))
 
         batches = defaultdict(lambda: {'lines': self.env['account.move.line']})
+        banks_per_partner = defaultdict(lambda: {'inbound': set(), 'outbound': set()})
         for line in lines:
             batch_key = self._get_line_batch_key(line)
-            serialized_key = '-'.join(str(v) for v in batch_key.values())
-            vals = batches[serialized_key]
+            vals = batches[frozendict(batch_key)]
             vals['payment_values'] = batch_key
             vals['lines'] += line
+            banks_per_partner[batch_key['partner_id']]['inbound' if line.balance > 0.0 else 'outbound'].add(
+                batch_key['partner_bank_id']
+            )
+
+        partner_unique_inbound = {p for p, b in banks_per_partner.items() if len(b['inbound']) == 1}
+        partner_unique_outbound = {p for p, b in banks_per_partner.items() if len(b['outbound']) == 1}
 
         # Compute 'payment_type'.
-        for vals in batches.values():
+        batch_vals = []
+        seen_keys = set()
+        for i, key in enumerate(list(batches)):
+            if key in seen_keys:
+                continue
+            vals = batches[key]
             lines = vals['lines']
+            merge = (
+                self.group_payment
+                and batch_key['partner_id'] in partner_unique_inbound
+                and batch_key['partner_id'] in partner_unique_outbound
+            )
+            if merge:
+                for other_key in list(batches)[i+1:]:
+                    if other_key in seen_keys:
+                        continue
+                    other_vals = batches[other_key]
+                    if all(
+                        other_vals['payment_values'][k] == v
+                        for k, v in vals['payment_values'].items()
+                        if k not in ('partner_bank_id', 'payment_type')
+                    ):
+                        # add the lines in this batch and mark as seen
+                        lines += other_vals['lines']
+                        seen_keys.add(other_key)
             balance = sum(lines.mapped('balance'))
             vals['payment_values']['payment_type'] = 'inbound' if balance > 0.0 else 'outbound'
-
-        return list(batches.values())
+            if merge:
+                partner_banks = banks_per_partner[batch_key['partner_id']]
+                vals['partner_bank_id'] = partner_banks[vals['payment_values']['payment_type']]
+                vals['lines'] = lines
+            batch_vals.append(vals)
+        return batch_vals
 
     @api.model
     def _get_wizard_values_from_batch(self, batch_result):
