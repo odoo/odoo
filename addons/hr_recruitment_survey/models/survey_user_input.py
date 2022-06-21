a1bc13b39559fa82763a9f0b3b603f6026b8a151
Dossogne Bertrand <bedo@odoo.com>
2022-06-21 14:35:19 +0000

    [IMP] hr_recruitment_survey: add interview template
    This commit adds a new mail template to be sent by default for interviews.
    
    task-2884526
    
    closes odoo/odoo#94178
    
    Related: odoo/upgrade#3731
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_recruitment_survey/models/survey_user_input.py
+++ addons/hr_recruitment_survey/models/survey_user_input.py
@@ -6,12 +6,12 @@ from odoo import fields, models, _
 class SurveyUserInput(models.Model):
     _inherit = "survey.user_input"
 
-    applicant_ids = fields.One2many('hr.applicant', 'response_id', string='Applicant')
+    applicant_id = fields.Many2one('hr.applicant', string='Applicant')
 
     def _mark_done(self):
         odoobot = self.env.ref('base.partner_root')
         for user_input in self:
-            if user_input.applicant_ids:
-                body = _('The applicant "%s" has finished the survey.', user_input.applicant_ids[:1].partner_name)
-                user_input.applicant_ids.message_post(body=body, author_id=odoobot.id)
+            if user_input.applicant_id:
+                body = _('The applicant "%s" has finished the survey.', user_input.applicant_id.partner_name)
+                user_input.applicant_id.message_post(body=body, author_id=odoobot.id)
         return super()._mark_done()
