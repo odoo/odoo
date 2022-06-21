2ba4217e6d1238e1062fe81a66b7a078dcd30e56
Miquel Raïch <miquel.raich@forgeflow.com>
2022-06-21 16:43:14 +0000

    [FIX] sale_stock: propagate SO line sequence to stock moves
    The default order of the picking moves generated from a sale order should be the same order as the sale order lines.
    
    Steps to reproduce:
    - Create a sale order with several sale lines.
    - Change the order of the sale lines.
    - Confirm the sale order.
      => The moves of the picking don't maintain same order as set before.
    
    X-original-commit: 406770225c6eff8b86e9b74c7022628d05bf58e4

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -494,6 +494,7 @@ class SaleOrderLine(models.Model):
             'product_description_variants': self._get_sale_order_line_multiline_description_variants(),
             'company_id': self.order_id.company_id,
             'product_packaging_id': self.product_packaging_id,
+            'sequence': self.sequence,
         })
         return values
 
