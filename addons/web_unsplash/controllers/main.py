e18be607216c8dc93cbb466506b2e3209d744f97
Jeremy Kersten <jke@odoo.com>
2022-06-23 13:48:33 +0200

    [FIX] website, web_unsplash: allow Restricted editor to use Unsplash
    Before this commit, when a restricted editor search for an image, he
    will have an error 404 not found, when he try to access to the unsplash
    key. Now he can search for image on Unsplash.
    
    X-original-commit: bbdd91e778a46c68461574dbea146e178f8c3c3f

--- addons/web_unsplash/controllers/main.py
+++ addons/web_unsplash/controllers/main.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 class Web_Unsplash(http.Controller):
 
     def _get_access_key(self):
-        if request.env.user._has_unsplash_key_rights():
+        if request.env.user._has_unsplash_key_rights(mode='read'):
             return request.env['ir.config_parameter'].sudo().get_param('unsplash.access_key')
         raise werkzeug.exceptions.NotFound()
 
@@ -143,7 +143,7 @@ class Web_Unsplash(http.Controller):
 
     @http.route("/web_unsplash/save_unsplash", type='json', auth="user")
     def save_unsplash(self, **post):
-        if request.env.user._has_unsplash_key_rights():
+        if request.env.user._has_unsplash_key_rights(mode='write'):
             request.env['ir.config_parameter'].sudo().set_param('unsplash.app_id', post.get('appId'))
             request.env['ir.config_parameter'].sudo().set_param('unsplash.access_key', post.get('key'))
             return True
