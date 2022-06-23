2c18dd4126f69938e84b64ef8d2f8553d5589fbc
Mathieu Duckerts-Antoine <dam@odoo.com>
2022-06-23 20:31:01 +0000

    [FIX] web: domain: can follow field relations
    Before this commit, a domain like
    ```
       [["partner_id.name", "ilike", "Agrolait"]]
    ```
    would crash when being evaluated.
    
    Part-of: odoo/odoo#92475

--- addons/web/static/src/core/domain.js
+++ addons/web/static/src/core/domain.js
@@ -187,6 +187,14 @@ function matchCondition(record, condition) {
         return condition;
     }
     const [field, operator, value] = condition;
+
+    if (typeof field === "string") {
+        const names = field.split(".");
+        if (names.length >= 2) {
+            return matchCondition(record[names[0]], [names.slice(1).join("."), operator, value]);
+        }
+    }
+
     const fieldValue = typeof field === "number" ? field : record[field];
     switch (operator) {
         case "=":
