20464c874ba15f6b5e0c2fb05e526b7d1ac9c421
Bruno Boi <boi@odoo.com>
2022-06-23 09:06:45 +0200

    [IMP] web: add hotkey service options
    This commit adds two options to the hotkey service to improve its
    usability in some cases:
    - `area` which is a function that returns an HTMLElement that restricts
      the operating area for an hotkey registration
    - `validate` which is a function that returns a boolean that tells if
      the hotkey registration's callback should be called or not
    
    Some typings adaptations were made in:
    - @web/core/commands/command_palette
    - @web/core/commands/command_service
    - @web/core/utils/ui

--- addons/web/static/src/core/hotkeys/hotkey_hook.js
+++ addons/web/static/src/core/hotkeys/hotkey_hook.js
@@ -9,15 +9,8 @@ const { useEffect } = owl;
  * when the caller component will mount/unmount.
  *
  * @param {string} hotkey
- * @param {()=>void} callback
- * @param {Object} options additional options
- * @param {boolean} [options.allowRepeat=false]
- *  allow registration to perform multiple times when hotkey is held down
- * @param {boolean} [options.bypassEditableProtection=false]
- *  if true the hotkey service will call this registration
- *  even if an editable element is focused
- * @param {boolean} [options.global=false]
- *  allow registration to perform no matter the UI active element
+ * @param {import("./hotkey_service").HotkeyCallback} callback
+ * @param {import("./hotkey_service").HotkeyOptions} [options] additional options
  */
 export function useHotkey(hotkey, callback, options = {}) {
     const hotkeyService = useService("hotkey");
