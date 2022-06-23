0a117b7df6b8343242b2ed22296ccbce97e210fe
FrancoisGe <fge@odoo.com>
2022-06-23 09:11:45 +0200

    [FIX] web: CP: remove "o_promote" is no longer used

--- addons/web/static/src/core/commands/command_service.js
+++ addons/web/static/src/core/commands/command_service.js
@@ -50,12 +50,12 @@ class DefaultFooter extends Component {
 }
 DefaultFooter.template = xml`
 <span>
-    <span class="o_promote font-weight-bolder text-primary">TIP</span> — search for
+    <span class="font-weight-bolder text-primary">TIP</span> — search for
     <t t-foreach="elements" t-as="element" t-key="element.namespace">
         <t t-if="!(element_first || element_last)">, </t>
         <t t-if="element_last and !element_first"> and </t>
         <span class="o_namespace btn-link text-primary o_cursor_pointer" t-on-click="() => this.onClick(element.namespace)">
-            <span t-out="element.namespace" class="o_promote font-weight-bolder text-primary"/><t t-out="element.name"/>
+            <span t-out="element.namespace" class="font-weight-bolder text-primary"/><t t-out="element.name"/>
         </span>
     </t>
 </span>
