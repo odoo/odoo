33edfe92ca41de9b1dcce72a18a73f9add9baaac
std-odoo <std@odoo.com>
2022-06-24 14:03:55 +0200

    [REF] google_gmail, fetchmail_gmail: merge google / fetchmail gmail
    Purpose
    =======
    Most of the time, both module will be installed at the same time, or
    none. For that reason, we want to merge both modules into Google Gmail.
    
    Task-2811567

--- addons/google_gmail/models/__init__.py
+++ addons/google_gmail/models/__init__.py
@@ -2,5 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import google_gmail_mixin
+
+from . import fetchmail_server
 from . import ir_mail_server
 from . import res_config_settings
