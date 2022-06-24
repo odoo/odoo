5c984454852bb5007461dc2f0ea11f7c69046174
Kevin Baptiste <kba@odoo.com>
2022-06-24 13:32:25 +0000

    [REV] hr_recruitment_skills: remove Populate feature
    Partial revert of #93126.
    
    closes odoo/odoo#94554
    
    Taskid: 2894671
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_recruitment_skills/__manifest__.py
+++ addons/hr_recruitment_skills/__manifest__.py
@@ -13,21 +13,11 @@
         'views/hr_applicant_views.xml',
         'views/hr_applicant_skill_views.xml',
         'security/ir.model.access.csv',
-        'wizard/hr_recruitment_populate_wizard_views.xml',
     ],
     'demo': [
     ],
     'installable': True,
     'application': False,
     'auto_install': True,
-    'assets': {
-        'web.assets_backend': [
-            'hr_recruitment_skills/static/src/**/*.js',
-        ],
-        'web.assets_qweb': [
-            'hr_recruitment_skills/static/src/xml/**/*',
-            'hr_recruitment_skills/static/src/**/*.xml',
-        ],
-    },
     'license': 'LGPL-3',
 }
