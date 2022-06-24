9ec04e21440dde9a3d486c59f610221200de3971
Sébastien Geelen (sge) <sge@odoo.com>
2022-06-24 09:17:04 +0200

    [FIX] web_editor: correct link behaviors
    * Broaden the valid URL pool by including some URL without subdomain
     if they have a white listed TLD.
    
    * Change a link 'href' when we change the link label,
     if the new label is a valid URL.
    
    * Ensure the domain extension cannot be only digit.
    
    task-2858621
    task-2901795
    
    X-original-commit: 500bc6dc9958fe8a9ca396f87544e8866efa744b

--- addons/web_editor/static/src/js/editor/odoo-editor/src/utils/sanitize.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/utils/sanitize.js
@@ -13,6 +13,9 @@ import {
     isMediaElement,
     getDeepRange,
     isUnbreakable,
+    closestElement,
+    getUrlsInfosInString,
+    URL_REGEX,
 } from './utils.js';
 
 const NOT_A_NUMBER = /[^\d]/g;
@@ -120,9 +123,10 @@ class Sanitize {
                 node = nodeP;
             }
 
+            const anchor = this.root.ownerDocument.getSelection().anchorNode;
+            const anchorEl = anchor && closestElement(anchor);
             // Remove zero-width spaces added by `fillEmpty` when there is
             // content and the selection is not next to it.
-            const anchor = this.root.ownerDocument.getSelection().anchorNode;
             if (
                 node.nodeType === Node.TEXT_NODE &&
                 node.textContent.includes('\u200B') &&
@@ -187,7 +191,15 @@ class Sanitize {
             ) {
                 node.setAttribute('contenteditable', 'false');
             }
-
+            // update link URL if label is a new valid link
+            if (node.nodeName === 'A' && anchorEl === node) {
+                const linkLabel = node.textContent;
+                const match = linkLabel.match(URL_REGEX);
+                if (match && match[0] === node.textContent) {
+                    const urlInfo = getUrlsInfosInString(linkLabel)[0];
+                    node.setAttribute('href', urlInfo.url);
+                }
+            }
             if (node.firstChild) {
                 this._parse(node.firstChild);
             }
