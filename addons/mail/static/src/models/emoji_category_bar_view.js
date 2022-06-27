68bb383852ae2c3bf4c143c29390c37ece7889b7
Nemael <mapa@odoo.com>
2022-06-27 10:20:45 +0000

    [IMP] mail: improve EmojiPicker (step 1)
    Emoji picker now has a bar to filter
    emojis per category.
    
    Task-2898475
    
    closes odoo/odoo#94646
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

---
+++ addons/mail/static/src/models/emoji_category_bar_view.js
@@ -0,0 +1,66 @@
+/** @odoo-module **/
+
+import { registerModel } from '@mail/model/model_core';
+import { many, one } from '@mail/model/model_field';
+import { insertAndReplace, replace } from '@mail/model/model_field_command';
+
+registerModel({
+    name: 'EmojiCategoryBarView',
+    identifyingFields: ['emojiPickerViewOwner'],
+    recordMethods: {
+        /**
+         * @private
+         * @returns {FieldCommand}
+         */
+        _computeActiveCategoryView() {
+            if (!this.activeByUserCategoryView) {
+                return replace(this.defaultActiveCategoryView);
+            }
+            return replace(this.activeByUserCategoryView);
+        },
+        /**
+         * @private
+         * @returns {FieldCommand}
+         */
+        _computeDefaultActiveCategoryView() {
+            return insertAndReplace({
+                emojiCategory: insertAndReplace({ categoryName: "all" }),
+                emojiCategoryBarViewOwner: replace(this),
+            });
+        },
+        /**
+         * @private
+         * @returns {FieldCommand}
+         */
+        _computeEmojiCategoryViews() {
+            return insertAndReplace(
+                this.messaging.emojiRegistry.allCategories.map(emojiCategory => {
+                    return { emojiCategory: replace(emojiCategory) };
+                })
+            );
+        },
+    },
+    fields: {
+        activeByUserCategoryView: one('EmojiCategoryView', {
+            inverse: 'emojiCategoryBarViewOwnerAsActiveByUser',
+        }),
+        activeCategoryView: one('EmojiCategoryView', {
+            compute: '_computeActiveCategoryView',
+            inverse: 'emojiCategoryBarViewOwnerAsActive',
+            required: true,
+        }),
+        defaultActiveCategoryView: one('EmojiCategoryView', {
+            compute: '_computeDefaultActiveCategoryView',
+        }),
+        emojiCategoryViews: many('EmojiCategoryView', {
+            compute: '_computeEmojiCategoryViews',
+            inverse: 'emojiCategoryBarViewOwner',
+            isCausal: true,
+        }),
+        emojiPickerViewOwner: one('EmojiPickerView', {
+            inverse: 'emojiCategoryBarView',
+            readonly: true,
+            required: true,
+        }),
+    },
+});
--- addons/mail/static/src/models/emoji_picker_view.js
+++ addons/mail/static/src/models/emoji_picker_view.js
@@ -1,59 +1,25 @@
 /** @odoo-module **/
 
 import { registerModel } from '@mail/model/model_core';
-import { many, one } from '@mail/model/model_field';
-import { insertAndReplace, replace } from '@mail/model/model_field_command';
+import { one } from '@mail/model/model_field';
+import { insertAndReplace } from '@mail/model/model_field_command';
 
 registerModel({
     name: 'EmojiPickerView',
     identifyingFields: ['popoverViewOwner'],
-    recordMethods: {
-        /**
-         * @param {MouseEvent} ev
-         */
-        onClickEmoji(ev) {
-            if (this.popoverViewOwner.messageActionListOwnerAsReaction) {
-                this.popoverViewOwner.messageActionListOwnerAsReaction.onClickReaction(ev);
-            } else if (this.popoverViewOwner.composerViewOwnerAsEmoji) {
-                this.popoverViewOwner.composerViewOwnerAsEmoji.onClickEmoji(ev);
-            }
-        },
-        
-        /**
-         * @private
-         * @returns {FieldCommand}
-         */
-        _computeEmojiViews() {
-            return insertAndReplace(
-                this.messaging.emojiRegistry.allEmojis.map(emoji => {
-                    return { emoji: replace(emoji) };
-                })
-            );
-        },
-        
-        /**
-         * @private
-         * @returns {FieldCommand}
-         */
-        _computeEmojiCategoryViews() {
-            return insertAndReplace(
-                this.messaging.emojiRegistry.allCategories.map(emojiCategory => {
-                    return { emojiCategory: replace(emojiCategory) };
-                })
-            );
-        },
-    },
     fields: {
-        emojiCategoryViews: many('EmojiCategoryView', {
-            compute: '_computeEmojiCategoryViews',
-            inverse: 'emojiPickerView',
+        emojiCategoryBarView: one('EmojiCategoryBarView', {
+            default: insertAndReplace(),
+            inverse: 'emojiPickerViewOwner',
             readonly: true,
+            required: true,
             isCausal: true,
         }),
-        emojiViews: many('EmojiView', {
-            compute: '_computeEmojiViews',
-            inverse: 'emojiPickerView',
+        emojiGridView: one('EmojiGridView', {
+            default: insertAndReplace(),
+            inverse: 'emojiPickerViewOwner',
             readonly: true,
+            required: true,
             isCausal: true,
         }),
         popoverViewOwner: one('PopoverView', {
