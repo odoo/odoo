421086d6be46834c79da9468c2de47d01028361b
Julien Castiaux <juc@odoo.com>
2022-06-28 13:04:48 +0000

    [FIX] web: session cookie lost between requests
    Each cookie binds to a domain name, multiple cookies can be set for the
    same name if they are for different domain name. In this case, two
    `session_id` cookies were set: (1) the first set right on the opener at
    `opener.cookies[...] = ...`, (2) the second set upon inside of
    `http.Request._save_session` because the session was rotated upon login.
    The problem is that the former cookie (the one set on the opener, the
    one *not* rotated) was used instead of the second cookie (the one
    holding the registered user) in the subsequent queries. There is a long
    comment explaining the same problem inside of
    `odoo.tests.common.HttpCase.authenticate`, we used the same solution as
    they did inside of `authenticate`: we diched the previous opener.
    
    closes odoo/odoo#94773
    
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

--- addons/portal/tests/test_login.py
+++ addons/portal/tests/test_login.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo.tests.common import tagged
 from odoo.addons.web.tests.test_login import TestWebLoginCommon
 
 
+@tagged('-at_install', 'post_install')
 class TestWebLoginPortal(TestWebLoginCommon):
     def test_web_login_external(self):
         res_post = self.login('portal_user', 'portal_user')
