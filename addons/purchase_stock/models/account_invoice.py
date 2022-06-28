50fca7e3f7a14d396710cec8f80c5a7f61e4696d
Arnold Moyaux <arm@odoo.com>
2022-06-28 17:34:56 +0200

    m

--- addons/purchase_stock/models/account_invoice.py
+++ addons/purchase_stock/models/account_invoice.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
-from odoo.tools.float_utils import float_compare
+from odoo import models
 
 
 class AccountMove(models.Model):
