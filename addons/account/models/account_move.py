a18f09d34da7c9a9b01c5c08a35a0295cb74da0e
william <wan@odoo.com>
2022-06-29 10:45:00 +0000

    [FIX] account: reduce locking on moves
    By trying to avoid concurrency issues while assigning the sequence [1],
    we are locking too many operations even when not in the process of
    assigning a new number.
    * when computing `highest_name`, we don't need to lock as this is mostly
      an information on the form that can possibly change anyway while the
      form view is open (without any lock anymore)
    * when computing the accounting date, we don't need to lock, the only
      information we want is the format of the sequence (yearly, monthly, no
      reset), which is not likely to change on a same journal.
    * when changing the `journal_id`, it is only a helper for encoding, if
      there is a concurrency error it will be shown during the post anyways.
      Also same argument as for `highest_name`
    
    [1] https://github.com/odoo/odoo/commit/ba6ee0b75d66d6f22a592b1b6a7c5158461bec41
    
    X-original-commit: 4ca53afa075fe1a003e92cdad85a096ad5e51887

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -417,7 +417,7 @@ class AccountMove(models.Model):
         """
         lock_dates = self._get_violated_lock_dates(invoice_date, has_tax)
         today = fields.Date.today()
-        highest_name = self.highest_name or self._get_last_sequence(relaxed=True)
+        highest_name = self.highest_name or self._get_last_sequence(relaxed=True, lock=False)
         number_reset = self._deduce_sequence_number_reset(highest_name)
         if lock_dates:
             invoice_date = lock_dates[-1][0] + timedelta(days=1)
@@ -479,7 +479,7 @@ class AccountMove(models.Model):
             if new_currency != self.currency_id:
                 self.currency_id = new_currency
                 self._onchange_currency()
-        if self.state == 'draft' and self._get_last_sequence() and self.name and self.name != '/':
+        if self.state == 'draft' and self._get_last_sequence(lock=False) and self.name and self.name != '/':
             self.name = '/'
 
     @api.onchange('partner_id')
@@ -1215,7 +1215,7 @@ class AccountMove(models.Model):
             )
         )
         self = self.sorted(lambda m: (m.date, m.ref or '', m.id))
-        highest_name = self[0]._get_last_sequence() if self else False
+        highest_name = self[0]._get_last_sequence(lock=False) if self else False
 
         # Group the moves by journal and month
         for move in self:
@@ -1275,7 +1275,7 @@ class AccountMove(models.Model):
     @api.depends('journal_id', 'date')
     def _compute_highest_name(self):
         for record in self:
-            record.highest_name = record._get_last_sequence()
+            record.highest_name = record._get_last_sequence(lock=False)
 
     @api.onchange('name', 'highest_name')
     def _onchange_name_warning(self):
