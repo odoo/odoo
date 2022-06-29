7704bb799020924acb09dbd434e58077a7d4dbb1
Loan (lse) <lse@odoo.com>
2022-06-29 14:08:02 +0000

    [REV] pos_restaurant: stop closing paymentscreen while paying
    Revert of
    https://github.com/odoo/odoo/pull/92794
    
    If you install pos_restaurant and open a PoS sale session, issue will start to appear after a certain time (about 1 minute):
    
    For V15:
    Javascript console logs will log errors:
    `TypeError: Cannot read properties of undefined (reading 'id') Chrome.js:97`
    
    For V15.2:
    The products button will become unresponsive (so the PoS is unusable).
    With error in the JS console as well:
    `TypeError: Cannot read properties of undefined (reading '0') Chrome.js`
    
    OPW-2889985
    
    closes odoo/odoo#94896
    
    X-original-commit: 863b389e67e45be73add7fd99810c66e1781ca73
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/pos_restaurant/static/src/js/Chrome.js
+++ addons/pos_restaurant/static/src/js/Chrome.js
@@ -76,8 +76,7 @@ odoo.define('pos_restaurant.chrome', function (require) {
                 this.showScreen('FloorScreen', { floor: table ? table.floor : null });
             }
             _shouldResetIdleTimer() {
-                const stayPaymentScreen = this.mainScreen.name === 'PaymentScreen' && this.env.pos.get_order().paymentlines.length > 0;
-                return super._shouldResetIdleTimer() && !stayPaymentScreen && this.mainScreen.name !== 'FloorScreen';
+                return super._shouldResetIdleTimer() && this.env.pos.config.iface_floorplan && this.mainScreen.name !== 'FloorScreen';
             }
             __showScreen() {
                 super.__showScreen(...arguments);
