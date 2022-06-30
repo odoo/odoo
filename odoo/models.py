5ef6d507a9347141c5b1faa1b49c4c33c0ece705
std-odoo <std@odoo.com>
2022-06-30 15:56:41 +0200

    [IMP] base: add new "Properties" fields
    Purpose
    =======
    
    Add a new field "Properties" to be able to light customization of workflows
    based on a parent model. Those properties acts in some ways like Odoo fields
    without requiring specific columns e.g. add new properties on tasks of a
    specific project.
    
    Usage
    =====
    
    Define properties on a parent model (e.g. project) with
    
    ```
        attributes_definition = fields.PropertiesDefinition('Message Properties')
    ```
    
    It defines properties available on children: types, default, value, model for
    relational properties,...
    
    Use it on children records (e.g. task) with
    
    ```
        attributes = fields.Properties(
            string='Properties',
            definition='parent_id.attributes_definition',
        )
    ```
    
    Technical
    =========
    
    Parent | Properties definition
    ------------------------------
    The properties definition is stored on the parent, on a JSON field.
    This definition contains the type of the properties, the default value,
    the model of the many2one,...
    ```
    [
        {
            'name': 'name',
            'string': 'Name',
            'type': 'char',
            'default': 'Default Name',
        }, {
            'name': 'partner_id',
            'string': 'Partner',
            'type': 'many2one',
            'comodel': 'res.partner',
        },
    ]
    ```
    
    Child | Properties values
    -------------------------
    The value is stored on the child, using a Properties field.
    ```
    {
        'name': 'Mitchel',
        'partner_id': 1337,
    }
    ```
    
    When we read this field, we will automatically read the definition on
    the parent, and merge both JSON into one, so the web client has the
    value of each property, and their definition.
    
    ```
    [
        {
            'name': 'name',
            'string': 'Name',
            'type': 'char',
            'default': 'Default Name',
            'value': 'Mitchel',
        }, {
            'name': 'partner_id',
            'string': 'Partner',
            'type': 'many2one',
            'comodel': 'res.partner',
            'value': 1337,
        },
    ]
    ```
    
    Integrity
    ---------
    If we remove a property on the parent, we won't update the child value.
    
    Instead, when we read the child properties, we will filter them based
    on the parent. So the removed properties will be removed the next time
    we write on the field.
    
    In the same logic, the many2one existence is checked when we read the
    field. There's no foreign key between the integer stored in the JSON
    in the SQL row corresponding to the record in database.
    
    Write
    -----
    We can write on the Properties field with a list of field definition
    + value.
    
    Some types are not JSONifiable (like the date, datetime), they are
    stored as string in database and parsed when we read the value.
    
    In order to update the parent definition by writing on the child,
    you need to add the dict key `definition_changed` or
    `definition_deleted`. This is because we need to be able to know
    if the definition has been changed without doing extra SQL queries.
    
    Access rights
    -------------
    A user can add a many2one / many2many property to a model only if he
    has the access rights to it.
    
    Many2one / Many2many
    --------------------
    
    The model choice of a many2one / many2many properties was subject to
    changes.
    
    First implementation stored models in both parent and children to easily
    spot changes and avoid complex queries when fetching records, trying to
    synchronize them, ...
    
    As this leads to storing a lot of duplicated content we choose to instead
    reset the value on the child if the model has been change. We generate a
    new name for the property. So it behaves like if we removed the property
    and created a new one.
    
    To be able to restore the old value (e.g. if by mistake we changed the
    model, and go back to the old model), we store the initial states.
    
    Task-2852259

--- odoo/models.py
+++ odoo/models.py
@@ -2049,19 +2049,26 @@ class BaseModel(metaclass=MetaModel):
             if not avoid(field)
         }
 
-        if not missing_defaults:
-            return values
+        if missing_defaults:
+            # override defaults with the provided values, never allow the other way around
+            defaults = self.default_get(list(missing_defaults))
+            for name, value in defaults.items():
+                if self._fields[name].type == 'many2many' and value and isinstance(value[0], int):
+                    # convert a list of ids into a list of commands
+                    defaults[name] = [Command.set(value)]
+                elif self._fields[name].type == 'one2many' and value and isinstance(value[0], dict):
+                    # convert a list of dicts into a list of commands
+                    defaults[name] = [Command.create(x) for x in value]
+            defaults.update(values)
 
-        # override defaults with the provided values, never allow the other way around
-        defaults = self.default_get(list(missing_defaults))
-        for name, value in defaults.items():
-            if self._fields[name].type == 'many2many' and value and isinstance(value[0], int):
-                # convert a list of ids into a list of commands
-                defaults[name] = [Command.set(value)]
-            elif self._fields[name].type == 'one2many' and value and isinstance(value[0], dict):
-                # convert a list of dicts into a list of commands
-                defaults[name] = [Command.create(x) for x in value]
-        defaults.update(values)
+        else:
+            defaults = values
+
+        # delegate the default properties to the properties field
+        for field in self._fields.values():
+            if field.type == 'properties':
+                defaults[field.name] = field._add_default_values(self.env, defaults)
+
         return defaults
 
     @classmethod
@@ -4346,6 +4353,11 @@ class BaseModel(metaclass=MetaModel):
                         else:
                             row.append(SQL_DEFAULT)
                 else:
+                    other_fields.add(field)
+
+                if field.type == 'properties':
+                    # force calling fields.create for properties field because
+                    # we might want to update the parent definition
                     other_fields.add(field)
 
             if not columns:
