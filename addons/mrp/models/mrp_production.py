6ff7be4c7bf8106edae483c4c6ed97ad3f9ff89a
Touati Djamel (otd) <otd@odoo.com>
2022-06-30 15:24:58 +0000

    [FIX] mrp: allow using an unbuilt serial number in manufacturing order
    Steps to reproduce the bug:
    - Create a storable product “P1”
        - Tracking: by serial number
        - BOM:
            - Component: C1
    
    - Create a storable product “P2”
        - BOM:
            - Component: P1
    
    - Create a MO to produce one unit of P1:
        - serial number: SN1
        - Confirm and mark as done
    
    - Unbuild the manufactured product
    - Manufacture the same product using the same serial number again
    
    - Create a new MO to produce one unit of “P2”:
        - Component P1 → select SN1
        - Try to confirm and validate the MO
    
    Problem:
    Get User Error: The serial number “SN1” used for component “P1”
    has already been consumed
    
    We do a search in the `stock.move.line` to find if the SN has already
    been used in a previous MO, but there is no specific condition to get
    only those used in an MO so the unbuild order is in the same condition
    and therefore the SN is considered as it has already been used
    opw-2883450
    
    closes odoo/odoo#95935
    
    X-original-commit: 5ac7f4d6fa5551ba7c6da49a52edf0a7df2d0745
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Djamel Touati (otd) <otd@odoo.com>

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -2108,6 +2108,7 @@ class MrpProduction(models.Model):
                     ('qty_done', '=', 1),
                     ('state', '=', 'done'),
                     ('location_dest_id.usage', '=', 'production'),
+                    ('production_id', '!=', False),
                 ])
                 if duplicates:
                     # Maybe some move lines have been compensated by unbuild
