852a0473a9a4aa25463ca15dbc0ea53425209fb5
Saran440 <saranl@ecosoft.co.th>
2022-06-30 10:30:56 +0700

    [ADD] permission bank_account_id in employee

--- addons/hr_expense/wizard/account_payment_register.py
+++ addons/hr_expense/wizard/account_payment_register.py
@@ -14,7 +14,7 @@ class AccountPaymentRegister(models.TransientModel):
     def _get_line_batch_key(self, line):
         # OVERRIDE to set the bank account defined on the employee
         res = super()._get_line_batch_key(line)
-        expense_sheet = self.env['hr.expense.sheet'].search([('payment_mode', '=', 'own_account'), ('account_move_id', 'in', line.move_id.ids)])
+        expense_sheet = self.env['hr.expense.sheet'].search([('payment_mode', '=', 'own_account'), ('account_move_id', 'in', line.move_id.ids)]).sudo()
         if expense_sheet and not line.move_id.partner_bank_id:
             res['partner_bank_id'] = expense_sheet.employee_id.bank_account_id.id or line.partner_id.bank_ids and line.partner_id.bank_ids.ids[0]
         return res
