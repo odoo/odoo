b8423ba218e0736593c3329d7438ca09112a735f
David (dafr) <dafr@odoo.com>
2022-06-30 11:19:14 +0000

    [FIX] stock: Add index of product_id field of stock.move.line model
    Adding a @depend decorator on product_id in the stock_move_line model can trigger a search on stock.move.line.product_id
    When the number of StockMoveLine reach a million, a simple `SELECT id FROM stock_move_line WHERE product_id = XXX` can take 200ms.
    When a transfer contains a few hundred StockMoveLine (ex: when dealing with serial numbers), the process of validating it will take a few minutes.
    Indexing product_id will change the process time back to a few seconds.
    
    OPW-2893131
    
    closes odoo/odoo#95467
    
    X-original-commit: ed4839d4d87e217dcce42ab01753c34b96d6f250
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: DavidFesquet <dafr@odoo.com>

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -25,7 +25,7 @@ class StockMoveLine(models.Model):
         check_company=True,
         help="Change to a better name", index=True)
     company_id = fields.Many2one('res.company', string='Company', readonly=True, required=True, index=True)
-    product_id = fields.Many2one('product.product', 'Product', ondelete="cascade", check_company=True, domain="[('type', '!=', 'service'), '|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    product_id = fields.Many2one('product.product', 'Product', ondelete="cascade", check_company=True, domain="[('type', '!=', 'service'), '|', ('company_id', '=', False), ('company_id', '=', company_id)]", index=True)
     product_uom_id = fields.Many2one('uom.uom', 'Unit of Measure', required=True, domain="[('category_id', '=', product_uom_category_id)]")
     product_uom_category_id = fields.Many2one(related='product_id.uom_id.category_id')
     product_qty = fields.Float(
