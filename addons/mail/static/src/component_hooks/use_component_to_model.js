f5d6cc47e23831028ef8d1feb47607fda7c79b9b
tsm-odoo <tsm@odoo.com>
2022-06-30 11:15:03 +0000

    [FIX] mail: fix error caused by useComponentToModel
    Some components use the `useComponentToModel` hook to set the component
    field on their related record. When a record is deleted then created
    again, its localId can remain the same. This means the Component won't
    trigger the `setup` method neither the `willUpdateprops`. Therefore,
    the second record's component field would be undefined. In order to
    solve this issue, let's check that the record's field is properly set
    when the component is rendered.
    
    X-original-commit: 4e1a685d61bc261a331fb563f82aa3ad54ad8e2d

--- addons/mail/static/src/component_hooks/use_component_to_model.js
+++ addons/mail/static/src/component_hooks/use_component_to_model.js
@@ -2,7 +2,11 @@
 
 import { clear } from '@mail/model/model_field_command';
 
+<<<<<<< HEAD
 const { onWillUpdateProps, useComponent } = owl;
+=======
+const { onWillDestroy, onWillRender, onWillUpdateProps, useComponent } = owl;
+>>>>>>> 9a2420a6e69... temp
 
 /**
  * This hook provides support for saving the reference of the component directly
@@ -21,6 +25,28 @@ export function useComponentToModel({ fieldName }) {
         if (currentRecord.exists() && currentRecord !== nextRecord) {
             currentRecord.update({ [fieldName]: clear() });
         }
+<<<<<<< HEAD
         nextRecord.update({ [fieldName]: component });
+=======
+        if (nextRecord) {
+            nextRecord.update({ [fieldName]: component });
+        }
+    });
+    onWillRender(() => {
+        const record = modelManager.models[modelName].get(component.props.localId);
+        if (record && !record[fieldName]) {
+            // When the record is deleted then created again, its
+            // localId can be the same. In this scenario, the Component
+            // would not call setup neither willUpdateprops. Therefore,
+            // we need to set the component for this new record.
+            record.update({ [fieldName]: component });
+        }
+    });
+    onWillDestroy(() => {
+        const record = modelManager.models[modelName].get(component.props.localId);
+        if (record) {
+            record.update({ [fieldName]: clear() });
+        }
+>>>>>>> 9a2420a6e69... temp
     });
 }
