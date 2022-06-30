749411671c7daca7276a7a49e8264a46f14d10f2
Alexandre Kühn <aku@odoo.com>
2022-06-30 18:12:31 +0200

    [FIX] im_livechat: remove compute of fields with legacy widgets
    These fields should not be declarative with compute, because
    these widgets need to manager existence by hand (i.e. manually
    `new` or `.destroy()`).
    
    Task-2900003

--- addons/im_livechat/static/src/models/public_livechat.js
+++ addons/im_livechat/static/src/models/public_livechat.js
@@ -4,31 +4,28 @@ import PublicLivechat from '@im_livechat/legacy/models/public_livechat';
 
 import { registerModel } from '@mail/model/model_core';
 import { attr, one } from '@mail/model/model_field';
-import { clear } from '@mail/model/model_field_command';
 
 registerModel({
     name: 'PublicLivechat',
     identifyingFields: ['livechatButtonOwner'],
-    recordMethods: {
-        /**
-         * @private
-         * @returns {FieldCommand|im_livechat/legacy/models/public_livechat}
-         */
-        _computeLegacyPublicLivechat() {
-            if (!this.data) {
-                return clear();
-            }
-            return new PublicLivechat({
-                parent: this.livechatButtonOwner.widget,
-                data: this.data,
+    lifecycleHooks: {
+        _created() {
+            this.update({
+                legacyPublicLivechat: new PublicLivechat({
+                    parent: this.livechatButtonOwner.widget,
+                    data: this.data,
+                }),
             });
         },
+        _willDelete() {
+            this.legacyPublicLivechat.destroy();
+        },
+    },
+    recordMethods: {
     },
     fields: {
         data: attr(),
-        legacyPublicLivechat: attr({
-            compute: '_computeLegacyPublicLivechat',
-        }),
+        legacyPublicLivechat: attr(),
         livechatButtonOwner: one('LivechatButtonView', {
             inverse: 'publicLivechat',
             readonly: true,
