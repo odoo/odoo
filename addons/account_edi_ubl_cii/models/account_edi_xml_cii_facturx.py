d05effb6b3d5a6f77e7506305e0aa492f11d9b75
Ivan Yelizariev <iel@odoo.com>
2022-07-01 11:23:07 +0000

    [FIX] account_edi{,_facturx}: properly retrieve product/description
    Odoo attaches factur-x doc to every invoice pdf. That data can be used to upload
    invoice to another Odoo instance.
    
    On uploading such an invoice, Odoo tries to find product in its DB. But it
    doesn't work if original product has Sales Description which is by default
    copied to line's Description (field `name`).
    
    Fix it by searching by first line in the name value of factur-x. Product name
    doesn't contain \n symbol in most cases anyway.
    
    This commit doesn't fix factur-x doc generation because of stable version
    policy. In next Odoo release we should use separate factur-x attributes for
    product name and invoice line description.
    
    opw-2878530
    
    closes odoo/odoo#99006
    
    X-original-commit: c5e08b8dcb9041235721afb99a5d05a0f51789e4
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Ivan Elizaryev (iel) <iel@odoo.com>

--- addons/account_edi_ubl_cii/models/account_edi_xml_cii_facturx.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_cii_facturx.py
@@ -307,13 +307,14 @@ class AccountEdiXmlCII(models.AbstractModel):
 
         # Product.
         name = _find_value('.//ram:SpecifiedTradeProduct/ram:Name', tree)
-        if name:
-            invoice_line.name = name
         invoice_line.product_id = self.env['account.edi.format']._retrieve_product(
             default_code=_find_value('.//ram:SpecifiedTradeProduct/ram:SellerAssignedID', tree),
             name=_find_value('.//ram:SpecifiedTradeProduct/ram:Name', tree),
             barcode=_find_value('.//ram:SpecifiedTradeProduct/ram:GlobalID', tree)
         )
+        # force original line description instead of the one copied from product's Sales Description
+        if name:
+            invoice_line.name = name
 
         xpath_dict = {
             'basis_qty': [
