4f13635ec104cea84d4b099142401cf83baf91e1
David Tran <david.tran@tvtmarine.com>
2022-07-03 04:00:29 +0000

    [FIX] payment: cron miss online payments of long verification
    Sometime Paypal took 3 or 4 days for some payment verification due to
    weekend. This raises the retry limit days for 4 days instead of 2 to
    solve the issue
    
    X-original-commit: f92abecaa5ceaf1015daef4d0ef4c555f0b03ea4

--- addons/payment/models/payment_transaction.py
+++ addons/payment/models/payment_transaction.py
@@ -866,8 +866,9 @@ class PaymentTransaction(models.Model):
         if not txs_to_post_process:
             # Let the client post-process transactions so that they remain available in the portal
             client_handling_limit_date = datetime.now() - relativedelta.relativedelta(minutes=10)
-            # Don't try forever to post-process a transaction that doesn't go through
-            retry_limit_date = datetime.now() - relativedelta.relativedelta(days=2)
+            # Don't try forever to post-process a transaction that doesn't go through. Set the limit
+            # to 4 days because some providers (PayPal) need that much for the payment verification.
+            retry_limit_date = datetime.now() - relativedelta.relativedelta(days=4)
             # Retrieve all transactions matching the criteria for post-processing
             txs_to_post_process = self.search([
                 ('state', '=', 'done'),
