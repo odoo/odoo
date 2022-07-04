8387ccbe48b204db49628b78e8c5d274773b7173
mafo-odoo <mafo@odoo.com>
2022-07-04 11:15:29 +0000

    [FIX] project_timesheet_holidays: create/remove global time off timesheets
    Before this commit, when the employee is generated with the salary
    configurator, the employee is archived since the employee will not
    directly start in the company. Even if the employee is archived and the
    timesheets are generated for this employee based on the global time offs
    found in the future. The problem is the timesheets cannot be created
    for archived employees and so a user error is raised.
    This problem could also happen if the user creates an archived employee.
    
    This commit fixes the issue by preventing the timesheets generation when
    the employee created is archived. When this employee will be updated and
    active field will be set to True, the timesheets generation will be
    applied. In the inverse use case, when the user will archive an employee
    and timesheets generated for the future global time offs will be removed.
    
    opw-2887727
    opw-2870739
    
    closes odoo/odoo#95438
    
    X-original-commit: cb51a3e311b98dc6a0b90a75024e772a27fd7958
    Signed-off-by: Xavier <xbo@odoo.com>

--- addons/project_timesheet_holidays/models/hr_employee.py
+++ addons/project_timesheet_holidays/models/hr_employee.py
@@ -9,27 +9,44 @@ class Employee(models.Model):
 
     @api.model
     def create(self, vals):
-        result = super(Employee, self).create(vals)
+        employees = super(Employee, self).create(vals)
 
         # We need to create timesheet entries for the global time off that are already created
         # and are planned for after this employee creation date
 
-        # First we look for the global time off that are already planned after today
-        today = fields.Datetime.today()
-        global_leaves = result.resource_calendar_id.global_leave_ids.filtered(lambda l: l.date_from >= today)
-        work_hours_data = global_leaves._work_time_per_day()
+        self._create_future_public_holidays_timesheets(employees)
+        return employees
 
-        vals_list = []
-        for global_time_off in global_leaves:
-            for index, (day_date, work_hours_count) in enumerate(work_hours_data[global_time_off.id]):
-                vals_list.append(
-                    global_time_off._timesheet_prepare_line_values(
-                        index,
-                        result,
-                        work_hours_data[global_time_off.id],
-                        day_date,
-                        work_hours_count
-                    )
-                )
-        self.env['account.analytic.line'].sudo().create(vals_list)
+    def write(self, vals):
+        result = super(Employee, self).write(vals)
+        if 'active' in vals:
+            if vals.get('active'):
+                # Create furtur holiday timesheets
+                self._create_future_public_holidays_timesheets(self)
+            else:
+                # Delete furtur holiday timesheets
+                future_timesheets = self.env['account.analytic.line'].search([('global_leave_id', '!=', False), ('date', '>=', fields.date.today()), ('employee_id', 'in', self.ids)])
+                future_timesheets.write({'global_leave_id': False})
+                future_timesheets.unlink()
         return result
+
+    def _create_future_public_holidays_timesheets(self, employees):
+        lines_vals = []
+        for employee in employees:
+            if not employee.active:
+                continue
+            # First we look for the global time off that are already planned after today
+            global_leaves = employee.resource_calendar_id.global_leave_ids.filtered(lambda l: l.date_from >= fields.Datetime.today())
+            work_hours_data = global_leaves._work_time_per_day()
+            for global_time_off in global_leaves:
+                for index, (day_date, work_hours_count) in enumerate(work_hours_data[global_time_off.id]):
+                    lines_vals.append(
+                        global_time_off._timesheet_prepare_line_values(
+                            index,
+                            employee,
+                            work_hours_data[global_time_off.id],
+                            day_date,
+                            work_hours_count
+                        )
+                    )
+        return self.env['account.analytic.line'].sudo().create(lines_vals)
