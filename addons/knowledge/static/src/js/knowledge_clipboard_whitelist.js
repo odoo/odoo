9ba76621dc66fcf672ce514a47cc933bd9462f38
Thibault Delavallée <tde@odoo.com>
2022-07-05 08:04:54 +0000

    [REM][MOV] knowledge: move to enterprise
    Due to business decisions knowledge is moved to enterprise repository. As
    some other moves are planned, notably coupon or the complete responsible UI
    and support that are coming to community Odoo feels like knowledge belongs
    to enterprise applications.
    
    Planned move from enterprise to community are
    
      * mass mailing themes;
      * mobile UI;
      * complete coupon and loyalty cards support;
      * spreadsheet library;
    
    For any questions please refer to decision makers.
    
    Task-2900765
    
    closes odoo/odoo#95297
    
    Related: odoo/enterprise#29107
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/knowledge/static/src/js/knowledge_clipboard_whitelist.js
+++
@@ -1,8 +0,0 @@
-/** @odoo-module */
-
-import { CLIPBOARD_WHITELISTS } from '@web_editor/../lib/odoo-editor/src/OdooEditor';
-
-/**
- * Allow @see o_knowledge_ classes to be preserved in the editor on paste.
- */
-CLIPBOARD_WHITELISTS.classes.push(/^o_knowledge_/);
