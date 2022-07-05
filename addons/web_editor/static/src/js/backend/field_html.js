odoo/odoo

commit 9e02cec488ee745361c5d63c302ee65dcb7283a9

[FIX] mass_mailing: clean oe_edited_link
Task-2729195

closes odoo/odoo#82452

Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

addons/web_editor/static/src/js/backend/field_html.js
@@ -92,15 +92,14 @@ var FieldHtml = basic_fields.DebouncedField.extend(TranslatableFieldMixin, {
      *
      * @override
      */
-    commitChanges: function () {
+    commitChanges: async function () {
         if (this.mode == "readonly" || !this.isRendered) {
             return this._super();
         }
         var _super = this._super.bind(this);
-        this.wysiwyg.odooEditor.clean();
+        await this.wysiwyg.cleanForSave();
         this._setValue(this._getValue());
-        return this.wysiwyg.saveModifiedImages(this.$content).then(async () => {
-            await this.wysiwyg.preSavePromise;
+        return this.wysiwyg.saveModifiedImages(this.$content).then(() => {
             this._isDirty = this.wysiwyg.isDirty();
             _super();
         });

addons/mass_mailing/static/src/js/mass_mailing_widget.js
@@ -60,9 +60,7 @@ var MassMailingFieldHtml = FieldHtml.extend({
         }
 
         var $editable = this.wysiwyg.getEditable();
-        if (this.wysiwyg.snippetsMenu) {
-            await this.wysiwyg.snippetsMenu.cleanForSave();
-        }
+        await this.wysiwyg.cleanForSave();
         return this.wysiwyg.saveModifiedImages(this.$content).then(function () {
             self._isDirty = self.wysiwyg.isDirty();
             self._doAction();

addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -812,11 +812,7 @@ const Wysiwyg = Widget.extend({
         this.trigger_up('ready_to_save', {defs: defs});
         await Promise.all(defs);
 
-        this._cleanForSave();
-
-        if (this.snippetsMenu) {
-            await this.snippetsMenu.cleanForSave();
-        }
+        await this.cleanForSave();
 
         const editables = this.options.getContentEditableAreas();
         await this.saveModifiedImages(editables.length ? $(editables) : this.$editable);
@@ -942,6 +938,14 @@ const Wysiwyg = Widget.extend({
     },
     isSelectionInEditable: function () {
         return this.odooEditor.isSelectionInEditable();
+    },
+    cleanForSave: async function () {
+        this.odooEditor.clean();
+        this.$editable.find('.oe_edited_link').removeClass('oe_edited_link');
+
+        if (this.snippetsMenu) {
+            await this.snippetsMenu.cleanForSave();
+        }
     },
     /**
      * Start or resume the Odoo field changes muation observers.
@@ -1786,10 +1790,6 @@ const Wysiwyg = Widget.extend({
                 noContextKeys: 'lang',
             });
         }
-    },
-    _cleanForSave: function () {
-        this.odooEditor.clean();
-        this.$editable.find('.oe_edited_link').removeClass('oe_edited_link');
     },
     _getCommands: function () {
         const options = this._editorOptions();

commit b11340f258fe125880aba4525e86163d334684a2

[FIX] web_editor: only partially clean for urgentSave
The urgentSave fix at f9d27d41 was broken by 9e02cec48.

Do not wait for the resolution of the cleanForSave promise to update
the internal value in case this happens during an urgentSave as the
beforeunload event does not play well with asynchronicity. It is
better to have a partially cleared value than to lose changes. When
this function is called outside of an urgentSave context, the full
cleaning is still awaited below and `_super` will reupdate the value.

Task-2861050

addons/web_editor/static/src/js/backend/field_html.js
@@ -97,12 +97,20 @@ var FieldHtml = basic_fields.DebouncedField.extend(TranslatableFieldMixin, {
             return this._super();
         }
         var _super = this._super.bind(this);
-        await this.wysiwyg.cleanForSave();
+        // Do not wait for the resolution of the cleanForSave promise to update
+        // the internal value in case this happens during an urgentSave as the
+        // beforeunload event does not play well with asynchronicity. It is
+        // better to have a partially cleared value than to lose changes. When
+        // this function is called outside of an urgentSave context, the full
+        // cleaning is still awaited below and `_super` will reupdate the value.
+        const fullClean = this.wysiwyg.cleanForSave();
         this._setValue(this._getValue());
-        return this.wysiwyg.saveModifiedImages(this.$content).then(() => {
-            this._isDirty = this.wysiwyg.isDirty();
-            _super();
-        });
+        this._isDirty = this.wysiwyg.isDirty();
+        await fullClean;
+        await this.wysiwyg.saveModifiedImages(this.$content);
+        // Update the value to the fully cleaned version.
+        this._setValue(this._getValue());
+        _super();
     },
     /**
      * @override