06d253cb98ebd8f77ad12a15955415d8556a7125
Thibault Delavallée <tde@odoo.com>
2022-07-05 08:04:54 +0000

    [REM][MOV] knowledge: move to enterprise
    Due to business decisions knowledge is moved to enterprise repository. As
    some other moves are planned, notably coupon or the complete responsible UI
    and support that are coming to community Odoo feels like knowledge belongs
    to enterprise applications.
    
    Planned move from enterprise to community are
    
      * mass mailing themes;
      * mobile UI;
      * complete coupon and loyalty cards support;
      * spreadsheet library;
    
    For any questions please refer to decision makers.
    
    Task-2900765
    
    X-original-commit: 9ba76621dc66fcf672ce514a47cc933bd9462f38

--- addons/knowledge/static/src/webclient/commands/command_category.js
+++
@@ -1,9 +0,0 @@
-/** @odoo-module **/
-
-import { registry } from "@web/core/registry";
-
-const commandCategoryRegistry = registry.category("command_categories");
-// displays the articles on input "?"
-commandCategoryRegistry.add("knowledge_articles", { namespace: "?" }, { sequence: 10 });
-// displays the advanced search command on input "?"
-commandCategoryRegistry.add("knowledge_extra", { namespace: "?" }, { sequence: 20 });
