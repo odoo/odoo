518ed987d026e9f5c6966955b7515eda9c180814
Thibault Delavallée <tde@odoo.com>
2022-07-05 08:04:54 +0000

    [REM][MOV] knowledge: move to enterprise
    Due to business decisions knowledge is moved to enterprise repository. As
    some other moves are planned, notably coupon or the complete responsible UI
    and support that are coming to community Odoo feels like knowledge belongs
    to enterprise applications.
    
    Planned move from enterprise to community are
    
      * mass mailing themes;
      * mobile UI;
      * complete coupon and loyalty cards support;
      * spreadsheet library;
    
    For any questions please refer to decision makers.
    
    Task-2900765
    
    X-original-commit: 9ba76621dc66fcf672ce514a47cc933bd9462f38
    Part-of: odoo/odoo#95556

--- addons/knowledge/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import knowledge_article_favorite
-from . import knowledge_article_member
-from . import knowledge_article
-from . import res_partner
-from . import res_users
