03ff355b700389083a08fa732f03939ac31eefbf
Sébastien Theys <seb@odoo.com>
2022-07-05 14:14:57 +0000

    [IMP] mail: move suggestionViews to ComposerSuggestionListView
    To correctly follow DOM tree in models.
    
    Part of task-2741982
    
    Part-of: odoo/odoo#95341

--- addons/mail/static/src/models/composer_suggestion_list_view.js
+++ addons/mail/static/src/models/composer_suggestion_list_view.js
@@ -1,16 +1,49 @@
 /** @odoo-module **/
 
 import { registerModel } from '@mail/model/model_core';
-import { one } from '@mail/model/model_field';
+import { many, one } from '@mail/model/model_field';
+import { insertAndReplace, replace } from '@mail/model/model_field_command';
 
 registerModel({
     name: 'ComposerSuggestionListView',
     identifyingFields: ['composerViewOwner'],
+    recordMethods: {
+        /**
+         * @returns {FieldCommand}
+         */
+        _computeExtraSuggestionViews() {
+            return insertAndReplace(this.composerViewOwner.extraSuggestions.map(suggestable => {
+                return {
+                    suggestable: replace(suggestable),
+                };
+            }));
+        },
+        /**
+         * @returns {FieldCommand}
+         */
+        _computeMainSuggestionViews() {
+            return insertAndReplace(this.composerViewOwner.mainSuggestions.map(suggestable => {
+                return {
+                    suggestable: replace(suggestable),
+                };
+            }));
+        },
+    },
     fields: {
         composerViewOwner: one('ComposerView', {
             inverse: 'composerSuggestionListView',
             readonly: true,
             required: true,
+        }),
+        extraSuggestionViews: many('ComposerSuggestionView', {
+            compute: '_computeExtraSuggestionViews',
+            inverse: 'composerSuggestionListViewOwnerAsExtraSuggestion',
+            isCausal: true,
+        }),
+        mainSuggestionViews: many('ComposerSuggestionView', {
+            compute: '_computeMainSuggestionViews',
+            inverse: 'composerSuggestionListViewOwnerAsMainSuggestion',
+            isCausal: true,
         }),
     },
 });
