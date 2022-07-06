9ea13513342b14e6b8d4e10e162b618a6ab2cce0
Romeo Fragomeli <rfr@odoo.com>
2022-07-06 08:10:07 +0000

    [REF] *: BS5: Migrate forms and inputs
    - BS5 uses native inputs instead of pseudo-element for some
      input like checkbox, radio, ...
      in BS5 input checkbox don't have "virtual visual" checkbox anymore
      (::before), so we remove the relative's rules
    
    - removed `.custom-control` class
    
    - `form-switch` use a new layout system in BS5 we adapt the code to
      match the Bootstrap approach (inline SVG).
    
    - In tests, we don't check the exact value of the background as it
      change in community/enterprise, and it's difficult to check the value
      of an SVG.
    
    - Overflow in progressbar is now hidden.
      -> we had to restore it.
    
    - In BS5 margins in forms/inputs has been changed
      -> we had to restore it (e.g. 'mb-3')
    
    - .form-group, .form-row, .form-inline
    > Dropped form-specific layout classes for our grid system.
    > Use our grid and utilities instead of .form-group, .form-row,
    > or .form-inline.
    
    - .input-group-append and .input-group-prepend
    > Dropped .input-group-append and .input-group-prepend.
    > You can now just add buttons and .input-group-text as direct
    > children of the input groups.
    
    Ref:
    [1] https://getbootstrap.com/docs/5.1/migration/#forms
    
    Task ID: 2766483
    
    Part-of: odoo/odoo#95450

--- addons/web/static/src/legacy/js/_deprecated/basic_fields.js
+++ addons/web/static/src/legacy/js/_deprecated/basic_fields.js
@@ -43,7 +43,7 @@ var FieldBoolean = AbstractField.extend({
         // the one rendered by the widget itself. Even though the event might
         // have been fired on the non-widget version of this field, we can still
         // test the presence of its custom class.
-        if (activated && options && options.event && $(options.event.target).closest('.custom-control.custom-checkbox').length) {
+        if (activated && options && options.event && $(options.event.target).closest('.custom-checkbox').length) {
             this._setValue(!this.value);  // Toggle the checkbox
         }
         return activated;
