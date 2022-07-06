bab95ef554bcffb16fa7c4560d559a68fd7ae16f
Baffour Kusi Frimpong <bakf@odoo.com>
2022-07-06 15:16:05 +0300

    [FIX] crm_lead: crm.lead created from smart button steps to reproduce issue - go to contacts and create a contact for an individual - after creating the contact click on opportunity, to create an crm.lead for that contact - after creating the crm.lead move it to the won stage
    Expected Behavior
    - there should not be two *LOST* buttons on the lead won stage
    - the lead created under a contact should be automatically converted to
    an opportunity
    
    Solution
    to fix the issue of two LOST buttons on lead won stage, the following
    code snippet was used in the crm_lead_views.xml file
    `attrs="{'invisible': ['|', ('type', '=', 'lead'),'&amp;',
    ('active', '=', False),('probability', '&lt;', 100)]}`
    
    to fix the issue with the crm.lead creation, the type field default was
    changed to opportunity
    
    opw-2886623

--- addons/crm/models/res_partner.py
+++ addons/crm/models/res_partner.py
@@ -57,7 +57,7 @@ class Partner(models.Model):
         This function returns an action that displays the opportunities from partner.
         '''
         action = self.env['ir.actions.act_window']._for_xml_id('crm.crm_lead_opportunities')
-        action['context'] = {'active_test': False}
+        action['context'] = {'active_test': False, 'default_type':'opportunity'}
         if self.is_company:
             action['domain'] = [('partner_id.commercial_partner_id.id', '=', self.id)]
         else:
