0faa975abcee86247f77edb956c508fd492f08df
stefanorigano (SRI) <sri@odoo.com>
2022-07-07 11:52:08 +0200

    [IMP] website_sale: provide a visual feedback changing filters
    Prior to this commit no visual feedback was provided when, clicking a
    filter or category, the page loads and redirect.
    
    This commit reduces the products grid's opacity after clicking, proving
    the user that his click triggered an action.
    
    task-2907955

--- addons/website_sale/static/src/js/website_sale_category_link.js
+++ addons/website_sale/static/src/js/website_sale_category_link.js
@@ -3,12 +3,13 @@
 import * as publicWidget from 'web.public.widget'
 
 publicWidget.registry.ProductCategoriesLinks = publicWidget.Widget.extend({
-    selector: '.products_categories',
+    selector: '.o_wsale_products_page',
     events: {
         'click [data-link-href]': '_openLink',
     },
 
     _openLink: function (ev) {
+        this.el.querySelector('.o_wsale_products_grid_table_wrapper').classList.add('opacity-50');
         window.location.href = ev.currentTarget.getAttribute('data-link-href');
     },
 });
