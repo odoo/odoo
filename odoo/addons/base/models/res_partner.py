c8b26fb666b15d10aadbbda149aab21bda33ba6d
arg-odoo <arg@odoo.com>
2022-07-07 10:48:38 +0000

    [FIX] base: avoid `KeyError` in `res_partner._display_address`
    In some case, users use custom `address_format` with keys that do not
    exists. This lead to errors in odoo and upgrade process. Avoid
    `KeyError` by using defaultdict with `str` value.
    
    upg-361146
    upg-361241
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/14.0/odoo/service/server.py", line 1201, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "/home/odoo/src/odoo/14.0/odoo/modules/registry.py", line 89, in new
        odoo.modules.load_modules(registry._db, force_demo, status, update_module)
      File "/home/odoo/src/odoo/14.0/odoo/modules/loading.py", line 457, in load_modules
        force, status, report, loaded_modules, update_module, models_to_check)
      File "/home/odoo/src/odoo/14.0/odoo/modules/loading.py", line 349, in load_marked_modules
        perform_checks=perform_checks, models_to_check=models_to_check
      File "/home/odoo/src/odoo/14.0/odoo/modules/loading.py", line 227, in load_module_graph
        migrations.migrate_module(package, 'post')
      File "/home/odoo/src/odoo/14.0/odoo/modules/migration.py", line 180, in migrate_module
        migrate(self.cr, installed_version)
      File "/tmp/tmp1bew9m7l/migrations/website/14.0.1.0/post-adapt-footer-data.py", line 69, in migrate
        address = html_escape(partner._display_address(without_company=True))
      File "/home/odoo/src/odoo/14.0/odoo/addons/base/models/res_partner.py", line 950, in _display_address
        return address_format % args
    KeyError: 'town_name'
    ```
    
    closes odoo/odoo#96622
    
    X-original-commit: 17c49b411065bea3d43ea5c1a4bf9aad646f023b
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -10,6 +10,7 @@ import threading
 import re
 
 import requests
+from collections import defaultdict
 from lxml import etree
 from random import randint
 from werkzeug import urls
@@ -941,13 +942,13 @@ class Partner(models.Model):
         # get the information that will be injected into the display format
         # get the address format
         address_format = self._get_address_format()
-        args = {
+        args = defaultdict(str, {
             'state_code': self.state_id.code or '',
             'state_name': self.state_id.name or '',
             'country_code': self.country_id.code or '',
             'country_name': self._get_country_name(),
             'company_name': self.commercial_company_name or '',
-        }
+        })
         for field in self._formatting_address_fields():
             args[field] = getattr(self, field) or ''
         if without_company:
