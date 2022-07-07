odoo/odoo

commit f4bf83382d479cd7060d09ccfe7bd528ec9e29dd

[FIX] account : compute invoice_partner_bank_id when computing bank_partner_id
To reproduce
============

- add a bank account to *MyCompany*
- create contact who has a bank account
- create customer invoice for this contact
- create credit note from this invoice

the Bank Account of the credit note is set to *MyCompany's* bank account, where it should be
the customer's account

Purpose
=======

when creating the credit note we don't set the field `invoice_partner_bank_id`

Specification
=============

to solve the issue we compute the field `invoice_partner_bank_id` when computing the field `bank_partner_id`

opw-2862601

addons/account/models/account_move.py
@@ -216,6 +216,7 @@ class AccountMove(models.Model):
         domain=[('exclude_from_invoice_tab', '=', False)],
         states={'draft': [('readonly', False)]})
     invoice_partner_bank_id = fields.Many2one('res.partner.bank', string='Bank Account',
+        compute="_compute_invoice_partner_bank_id", store=True, readonly=False,
         help='Bank Account Number to which the invoice will be paid. A Company bank account if this is a Customer Invoice or Vendor Credit Note, otherwise a Partner bank account number.',
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
     invoice_incoterm_id = fields.Many2one('account.incoterms', string='Incoterm',
@@ -362,7 +363,6 @@ class AccountMove(models.Model):
                 line.account_id = new_term_account
 
         self._compute_bank_partner_id()
-        self.invoice_partner_bank_id = self.bank_partner_id.bank_ids and self.bank_partner_id.bank_ids[0]
 
         # Find the new fiscal position.
         delivery_partner_id = self._get_invoice_delivery_partner_id()
@@ -1071,7 +1071,13 @@ class AccountMove(models.Model):
         for move in self:
             move.commercial_partner_id = move.partner_id.commercial_partner_id
 
-    @api.depends('commercial_partner_id')
+    @api.depends('bank_partner_id')
+    def _compute_invoice_partner_bank_id(self):
+        for move in self:
+            filtered_bank_partner_id = move.bank_partner_id.filtered(lambda bank: bank.company_id.id in (False, move.company_id.id))
+            move.invoice_partner_bank_id = filtered_bank_partner_id.bank_ids[:1]
+
+    @api.depends('commercial_partner_id', 'type')
     def _compute_bank_partner_id(self):
         for move in self:
             if move.is_outbound():
@@ -2587,7 +2593,7 @@ class AccountMove(models.Model):
                         'debit' : line_vals['credit'],
                         'credit' : line_vals['debit']
                     })
-            move.write({'invoice_line_ids' : [(5, 0, 0)], 'invoice_partner_bank_id': False})
+            move.write({'invoice_line_ids' : [(5, 0, 0)]})
             move.write({'invoice_line_ids' : new_invoice_line_ids})
 
     def _get_report_base_filename(self):

commit 638043c1c996a69fa0dfbca832c670cd14b845d2

[FIX] account: bank statement with erroneous partner_bank_id Bug introduced in commit : https://github.com/odoo/odoo/pull/92660/commits/f4bf83382d479cd7060d09ccfe7bd528ec9e29dd would trigger partner_bank_id computation on bst entries and erroneously set the company bank as partner bank.
opw-2900828

addons/account/models/account_move.py
@@ -1370,10 +1370,10 @@ class AccountMove(models.Model):
     @api.depends('commercial_partner_id', 'move_type')
     def _compute_bank_partner_id(self):
         for move in self:
-            if move.is_outbound():
-                move.bank_partner_id = move.commercial_partner_id
-            else:
+            if move.is_inbound():
                 move.bank_partner_id = move.company_id.partner_id
+            else:
+                move.bank_partner_id = move.commercial_partner_id
 
     @api.model
     def _get_invoice_in_payment_state(self):