c9609573fbbda5ed0864b1407a50a9d65bb06be7
Mathieu Duckerts-Antoine <dam@odoo.com>
2022-07-08 15:31:45 +0200

    [FIX] website_sale: unskip test_01_shop_mail_tour

--- addons/website_sale/tests/test_website_sale_mail.py
+++ addons/website_sale/tests/test_website_sale_mail.py
@@ -13,8 +13,6 @@ class TestWebsiteSaleMail(HttpCase):
 
     def test_01_shop_mail_tour(self):
         """The goal of this test is to make sure sending SO by email works."""
-        import unittest; raise unittest.SkipTest("skipWOWL")
-
         self.env['product.product'].create({
             'name': 'Acoustic Bloc Screens',
             'list_price': 2950.0,
