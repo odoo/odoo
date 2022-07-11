46871906d5daab5312aae03d8293eb85e74239a5
Géry Debongnie <ged@odoo.com>
2022-07-11 12:49:53 +0000

    [REF] barcodes: simplify 'O-CMD.' implementation
    With this commit, we simplify the way barcodes prefixed by `O-CMD.` are
    handled, by simply executing a ui action whenever it happens. The code
    is much simpler, instead of having to add a widget=barcode_handler in a
    specific view.
    
    Note that it changes the behaviour: now, barcodes are available
    everywhere, instead of just a selection of form views. This seems more
    natural, since we consider that the barcode scanner is basically some
    kind of input device.
    
    Part-of: odoo/odoo#95892

--- addons/barcodes/static/src/barcode_handlers.js
+++ addons/barcodes/static/src/barcode_handlers.js
@@ -2,29 +2,88 @@
 
 import { registry } from "@web/core/registry";
 import { getVisibleElements } from "@web/core/utils/ui";
+import { MacroEngine } from "@web/core/macro";
 
-export const barcodeAutoClick = {
+function clickOnButton(selector) {
+    const button = document.body.querySelector(selector);
+    if (button) {
+        button.click();
+    }
+}
+function updatePager(position) {
+    const pager = document.body.querySelector("nav.o_pager");
+    if (!pager || pager.innerText.includes("-")) {
+        // we don't change pages if we are in a multi record view
+        return;
+    }
+    let next;
+    if (position === "first") {
+        next = 1;
+    } else {
+        next = parseInt(pager.querySelector(".o_pager_limit").textContent, 10);
+    }
+    let current = parseInt(pager.innerText.split('/')[0], 10);
+    if (current === next) {
+        return;
+    }
+    const engine = new MacroEngine();
+    engine.activate({
+        name: "updating pager",
+        timeout: 1000,
+        interval: 0,
+        steps: [
+            {
+                trigger: "span.o_pager_value",
+                action: "click"
+            },
+            {
+                trigger: "input.o_pager_value",
+                action: "text",
+                value: next
+            }
+        ]
+    });
+}
+
+const COMMANDS = {
+    "O-CMD.EDIT": () => clickOnButton(".o_form_button_edit"),
+    "O-CMD.DISCARD": () => clickOnButton(".o_form_button_cancel"),
+    "O-CMD.SAVE": () => clickOnButton(".o_form_button_save"),
+    "O-CMD.PREV": () => clickOnButton(".o_pager_previous"),
+    "O-CMD.NEXT": () => clickOnButton(".o_pager_next"),
+    "O-CMD.PAGER-FIRST": () => updatePager("first"),
+    "O-CMD.PAGER-LAST": () => updatePager("last"),
+};
+
+export const barcodeGenericHandlers = {
     dependencies: ["ui", "barcode"],
     start(env, { ui, barcode }) {
 
         barcode.bus.addEventListener("barcode_scanned", (ev) => {
             const barcode = ev.detail.barcode;
-            if (!barcode.startsWith("O-BTN.")) {
-                return;
+            if (barcode.startsWith("O-BTN.")) {
+                let targets = [];
+                try {
+                    // the scanned barcode could be anything, and could crash the queryselectorall
+                    // function
+                    targets = getVisibleElements(ui.activeElement, `[barcode_trigger=${barcode.slice(6)}]`);
+                } catch (_e) {
+                    console.warn(`Barcode '${barcode}' is not valid`);
+                }
+                for (let elem of targets) {
+                    elem.click();
+                }
             }
-            let targets = [];
-            try {
-                // the scanned barcode could be anything, and could crash the queryselectorall
-                // function
-                targets = getVisibleElements(ui.activeElement, `[barcode_trigger=${barcode.slice(6)}]`);
-            } catch (_e) {
-                console.warn(`Barcode '${barcode}' is not valid`)
-            }
-            for (let elem of targets) {
-                elem.click();
+            if (barcode.startsWith("O-CMD.")) {
+                const fn = COMMANDS[barcode];
+                if (fn) {
+                    fn();
+                } else {
+                    console.warn(`Barcode '${barcode}' is not a valid command`);
+                }
             }
         });
     }
 };
 
-registry.category("services").add("barcode_autoclick", barcodeAutoClick);
+registry.category("services").add("barcode_handlers", barcodeGenericHandlers);
