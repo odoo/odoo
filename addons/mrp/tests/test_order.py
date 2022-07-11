af80f8c28071c802349bd064170d3ab4c33a870c
Denis Ledoux <dle@odoo.com>
2022-07-11 11:27:06 +0000

    [IMP] mrp: fix import order in test_order.py
    CI/Style
    ```
    odoo/addons/mrp/tests/test_order.py:8
    from odoo import Command, fields
    C0412(ungrouped-imports) coll 0: Imports from package odoo are not grouped
    ```
    
    closes odoo/odoo#95733
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- addons/mrp/tests/test_order.py
+++ addons/mrp/tests/test_order.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.tests import Form
 from datetime import datetime, timedelta
 from freezegun import freeze_time
 
 from odoo import Command, fields
 from odoo.exceptions import UserError
-from odoo.addons.mrp.tests.common import TestMrpCommon
+from odoo.tests import Form
 from odoo.tools.misc import format_date
+
+from odoo.addons.mrp.tests.common import TestMrpCommon
 
 class TestMrpOrder(TestMrpCommon):
 
