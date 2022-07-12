1ec2c9ec22c70006c722700765daebf20a6d25c5
Adrien Schoffeniels <adsc@odoo.com>
2022-07-12 17:30:15 +0200

    [IMP] web_unsplash: allow to specify image orientation
    Purpose:
    ========
    Knowledge allows to search images from unsplash when selecting a cover
    for an article. One would like to show only images with a landscape
    orientation when selecting a cover, since images with other orientations
    do not fit properly.
    
    Also made "UnsplashError" component exportable so that it can be used
    in Knowledge,'s Cover Selector.
    
    Specs:
    ======
    Modified unsplash_service and image_selector so that one can specify
    the orientation of the images to fetch from unsplash.
    Added the orientation as cache key so that images in cache are not shown
    when using the same query word but with another orientation (eg. in
    knowledge, search an image for the body of the article then searching a
    cover using the same query term).
    
    Task-2852916
    
    X-original-commit: e65d5d617bd13cf0381b372205a07a4a3bef2963

--- addons/web_unsplash/static/src/components/media_dialog/image_selector.js
+++ addons/web_unsplash/static/src/components/media_dialog/image_selector.js
@@ -30,7 +30,7 @@ class UnsplashCredentials extends Component {
 }
 UnsplashCredentials.template = 'web_unsplash.UnsplashCredentials';
 
-class UnsplashError extends Component {}
+export class UnsplashError extends Component {}
 UnsplashError.template = 'web_unsplash.UnsplashError';
 UnsplashError.components = {
     UnsplashCredentials,
@@ -120,7 +120,7 @@ patch(ImageSelector.prototype, 'image_selector_unsplash', {
             return { records: [], isMaxed: false };
         }
         try {
-            const { isMaxed, images } = await this.unsplash.getImages(this.state.needle, offset, this.NUMBER_OF_ATTACHMENTS_TO_DISPLAY);
+            const { isMaxed, images } = await this.unsplash.getImages(this.state.needle, offset, this.NUMBER_OF_ATTACHMENTS_TO_DISPLAY, this.props.orientation);
             this.state.isFetchingUnsplash = false;
             this.state.unsplashError = false;
             const records = images.map(record => {
