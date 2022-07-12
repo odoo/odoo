9c8bdac2290b8334952dd46323826fdaa528c7a3
Hugo Adan <hugo@vauxoo.com>
2022-07-12 14:29:38 +0000

    [REF] purchase_stock: Split _get_quantity_in_progress method in order to make it more inheritable
    closes odoo/odoo#96555
    
    X-original-commit: cb9314e902fc899c5ab42b6fd4d8c1ed811c24e7
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/purchase_stock/models/product.py
+++ addons/purchase_stock/models/product.py
@@ -33,6 +33,24 @@ class ProductProduct(models.Model):
             warehouse_ids = []
 
         qty_by_product_location, qty_by_product_wh = super()._get_quantity_in_progress(location_ids, warehouse_ids)
+        domain = self._get_lines_domain(location_ids, warehouse_ids)
+        groups = self.env['purchase.order.line'].read_group(domain,
+            ['product_id', 'product_qty', 'order_id', 'product_uom', 'orderpoint_id'],
+            ['order_id', 'product_id', 'product_uom', 'orderpoint_id'], lazy=False)
+        for group in groups:
+            if group.get('orderpoint_id'):
+                location = self.env['stock.warehouse.orderpoint'].browse(group['orderpoint_id'][:1]).location_id
+            else:
+                order = self.env['purchase.order'].browse(group['order_id'][0])
+                location = order.picking_type_id.default_location_dest_id
+            product = self.env['product.product'].browse(group['product_id'][0])
+            uom = self.env['uom.uom'].browse(group['product_uom'][0])
+            product_qty = uom._compute_quantity(group['product_qty'], product.uom_id, round=False)
+            qty_by_product_location[(product.id, location.id)] += product_qty
+            qty_by_product_wh[(product.id, location.warehouse_id.id)] += product_qty
+        return qty_by_product_location, qty_by_product_wh
+
+    def _get_lines_domain(self, location_ids=False, warehouse_ids=False):
         domain = []
         rfq_domain = [
             ('state', 'in', ('draft', 'sent', 'to approve')),
@@ -55,21 +73,7 @@ class ProductProduct(models.Model):
                         ('orderpoint_id.warehouse_id', 'in', warehouse_ids)
             ]])
             domain = expression.OR([domain, wh_domain])
-        groups = self.env['purchase.order.line'].read_group(domain,
-            ['product_id', 'product_qty', 'order_id', 'product_uom', 'orderpoint_id'],
-            ['order_id', 'product_id', 'product_uom', 'orderpoint_id'], lazy=False)
-        for group in groups:
-            if group.get('orderpoint_id'):
-                location = self.env['stock.warehouse.orderpoint'].browse(group['orderpoint_id'][:1]).location_id
-            else:
-                order = self.env['purchase.order'].browse(group['order_id'][0])
-                location = order.picking_type_id.default_location_dest_id
-            product = self.env['product.product'].browse(group['product_id'][0])
-            uom = self.env['uom.uom'].browse(group['product_uom'][0])
-            product_qty = uom._compute_quantity(group['product_qty'], product.uom_id, round=False)
-            qty_by_product_location[(product.id, location.id)] += product_qty
-            qty_by_product_wh[(product.id, location.warehouse_id.id)] += product_qty
-        return qty_by_product_location, qty_by_product_wh
+        return domain
 
 
 class SupplierInfo(models.Model):
