b4772177e69bb2798c0730500a7d5043d59e5204
Daniel Blanco <daniel@blancomartin.cl>
2022-07-13 17:36:00 +0000

    [FIX] l10n_cl: add l10n_cl_activity_description field to community
    since this value is used as a required field in website sale
    [FIX] l10n_cl: add activity description field to company
    
    closes odoo/odoo#95964
    
    Related: odoo/enterprise#29462
    Related: odoo/upgrade#3764
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_cl/models/res_company.py
+++ addons/l10n_cl/models/res_company.py
@@ -6,6 +6,9 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = "res.company"
 
+    l10n_cl_activity_description = fields.Char(
+        string='Company Activity Description', related='partner_id.l10n_cl_activity_description', readonly=False)
+
     def _localization_use_documents(self):
         """ Chilean localization use documents """
         self.ensure_one()
