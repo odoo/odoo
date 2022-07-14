34d2da67458d5d1b548ec5572d720bc7e8bd9bf8
Romain Derie <rde@odoo.com>
2022-07-14 15:58:46 +0000

    [FIX] website: make enable_editor=1 links work from backend
    Since frontend to backend commit [1], links having `enable_editor=1` to
    enable edit mode were not working anymore in certain cases.
    
    Case 1 [OK]*:
    From frontend, click on `enable_editor=1` link without `/@/` in it.
    
    Case 2 [OK]:
    From frontend, click on `enable_editor=1` link with `/@/` in it.
    
    Case 3 [KO]*:
    From backend, click on `enable_editor=1` link without `/@/` in it.
    
    Case 4 [KO]:
    From backend, click on `enable_editor=1` link with `/@/` in it.
    
    This commit fixes case 4 and adds a test for all cases, while leaving
    case 3 commented as it will be fixed later with a larger fix related to
    URL change listener.
    
    * Note that we will probably change the behavior for case 1 and 3: if
      the version with /@/ + enable_editor=1 enters edit mode properly in
      both frontend and backend contexts (case 2 and 4), it's probably not
      worth having code to support case 1 and 3.
    
    The issue for case 4 was that it was trying to load the whole Odoo app
    inside of website preview iframe instead of making the parent window
    load that URL.
    
    [1]: https://github.com/odoo/odoo/commit/31cc10b91dc7762e23b4bde9b945be0c4ce3fe3b
    
    closes odoo/odoo#96054
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/tests/__init__.py
+++ addons/website/tests/__init__.py
@@ -4,6 +4,7 @@ from . import test_attachment
 from . import test_auth_signup_uninvited
 from . import test_automatic_editor
 from . import test_base_url
+from . import test_client_action
 from . import test_configurator
 from . import test_controllers
 from . import test_converter
