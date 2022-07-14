9b30d75a1bb5a2c2e221a4cdcd245528a1a174ae
Aurélien (avd) <avd@odoo.com>
2022-07-14 15:51:21 +0200

    [IMP] purchase: speed up dashboard rfq sent computation.
    Add a new subtype_id for 'RFQ Sent' state. This removes
    the need to join on mail.tracking.value when computing
    'all_sent_rfqs'. Since mail.tracking.value is usually
    a big table, removing this join leads to a substantial
    speedup when loading the purchase.order dashboard on big
    databases.

--- addons/purchase/tests/__init__.py
+++ addons/purchase/tests/__init__.py
@@ -7,3 +7,4 @@ from . import test_purchase_invoice
 from . import test_access_rights
 from . import test_accrued_purchase_orders
 from . import test_purchase_tax_totals
+from . import test_purchase_dashboard
