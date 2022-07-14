2011b1982645301ef945cafe722f2bd1da6d8e6f
Stanislas Gueniffey <stgu@odoo.com>
2022-07-14 11:17:08 +0200

    [IMP] l10n_ec: update and upgrade
    Data improvements:
    - Added missing tax groups
    - Added VAT 8%, used for tourism operations on special hollidays
    - Fixes base percentage on Withhold VAT taxes
    - Corrected tax name from Iva to IVA
    - Improves SRI paymment options, with sequence widget and inactavtes
    deprecated payment options.
    - Fixes short code of document types
    - Corrects document type name for insurance
    - Adds withhold document type, to be implemented in separated module
    
    Other improvements:
    - Automatic setup of several journals when creating new companies
    - Adds restrictions to ensure proper setup of ecuadorian journals
    - Improves ecuadorian journal form
    
    More improvements:
    - Removes unused fiscal positions
    - Removes unnecessary journals
    - Desactivates unused payment methods
    - Adds migration scripts
    - Add SRI Root menu
    - More fixes and improvements
    
    Co-authored-by: Andres Calle <andres.calle@trescloud.com>

--- addons/l10n_ec/__manifest__.py
+++ addons/l10n_ec/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     "name": "Ecuadorian Accounting",
-    "version": "3.3",
+    "version": "3.4",
     "description": """
 Functional
 ----------
@@ -32,10 +32,10 @@ Master Data:
 * Ecuador banks
 * Partners: Consumidor Final, SRI, IESS, and also basic VAT validation
     """,
-    "author": "OPA CONSULTING & TRESCLOUD",
+    "author": "TRESCLOUD & OPA CONSULTING",
     "category": "Accounting/Localizations/Account Charts",
-    "maintainer": "OPA CONSULTING",
-    "website": "https://opa-consulting.com",
+    "maintainer": "TRESCLOUD",
+    "website": "https://www.trescloud.com",
     "license": "LGPL-3",
     "depends": [
         "base",
@@ -46,6 +46,8 @@ Master Data:
         "l10n_latam_base",
     ],
     "data": [
+        # Security
+        "security/ir.model.access.csv",
         # Chart of Accounts
         "data/account_chart_template_data.xml",
         "data/account_group_template_data.xml",
@@ -63,14 +65,15 @@ Master Data:
         "data/l10n_latam_identification_type_data.xml",
         "data/res_partner_data.xml",
         # Other data
-        "data/account_chart_template_configure_data.xml",
         "data/l10n_latam.document.type.csv",
+        "data/account_chart_template_configure_data.xml",
         "data/l10n_ec.sri.payment.csv",
+        # Views
+        "views/root_sri_menu.xml",
         "views/account_tax_view.xml",
         "views/l10n_latam_document_type_view.xml",
         "views/l10n_ec_sri_payment.xml",
         "views/account_journal_view.xml",
-        "security/ir.model.access.csv",
     ],
     "demo": [
         "demo/demo_company.xml",
--- addons/l10n_ec/models/account_journal.py
+++ addons/l10n_ec/models/account_journal.py
@@ -1,18 +1,21 @@
-from odoo import fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
 
 
 class AccountJournal(models.Model):
 
     _inherit = "account.journal"
 
-    l10n_ec_entity = fields.Char(string="Emission Entity", size=3, default="001")
-    l10n_ec_emission = fields.Char(string="Emission Point", size=3, default="001")
+    l10n_ec_entity = fields.Char(string="Emission Entity", size=3, copy=False)
+    l10n_ec_emission = fields.Char(string="Emission Point", size=3, copy=False)
     l10n_ec_emission_address_id = fields.Many2one(
         comodel_name="res.partner",
         string="Emission address",
         domain="['|', ('id', '=', company_partner_id), '&', ('id', 'child_of', company_partner_id), ('type', '!=', 'contact')]",
     )
 
+    #TODO: On next release deprecate field as is it has no use
     l10n_ec_emission_type = fields.Selection(
         string="Emission type",
         selection=[
@@ -22,3 +25,10 @@ class AccountJournal(models.Model):
         ],
         default="electronic",
     )
+
+    @api.constrains('l10n_ec_entity', 'l10n_ec_emission')
+    def l10n_ec_check_moves_entity_emmision(self):
+        for journal in self:
+            if self.env['account.move'].search([('journal_id', '=', journal.id), ('posted_before', '=', True)], limit=1):
+                raise ValidationError(_(
+                    'You can not modify the field "Emission Entity or Emission Point" if there are validated invoices in this journal!'))
