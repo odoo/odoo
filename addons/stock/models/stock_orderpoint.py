be1cd3ee739fa087ff3b1f479b0b9ef00a6977d6
PNO <pno@odoo.com>
2022-07-15 09:25:29 +0200

    [FIX] mrp: Wrong replenishment order notification
    On the commit 69dcdd4, the order='create_date desc'
     was removed and the way of getting the MO for the notification changed. Now, the context is used, it should be written_after instead of written_date.
    In addition, the now declaration should be at the beginning of the method action_replenish.
    (The MO created is correct, only the notification is wrong)
    
    Should be forwarded
    OPW-2877457

--- addons/stock/models/stock_orderpoint.py
+++ addons/stock/models/stock_orderpoint.py
@@ -206,6 +206,7 @@ class StockWarehouseOrderpoint(models.Model):
         return action
 
     def action_replenish(self):
+        now = datetime.now()
         try:
             self._procure_orderpoint_confirm(company_id=self.env.company)
         except UserError as e:
@@ -219,7 +220,6 @@ class StockWarehouseOrderpoint(models.Model):
                 'views': [(self.env.ref('product.product_normal_form_view').id, 'form')],
                 'context': {'form_view_initial_mode': 'edit'}
             }, _('Edit Product'))
-        now = datetime.now()
         notification = False
         if len(self) == 1:
             notification = self.with_context(written_after=now)._get_replenishment_order_notification()
@@ -446,7 +446,7 @@ class StockWarehouseOrderpoint(models.Model):
     def _get_replenishment_order_notification(self):
         self.ensure_one()
         domain = [('orderpoint_id', 'in', self.ids)]
-        if self.env.context.get('written_date'):
+        if self.env.context.get('written_after'):
             domain = expression.AND([domain, [('write_date', '>', self.env.context.get('written_after'))]])
         move = self.env['stock.move'].search(domain, limit=1)
         if move.picking_id:
