843f24c33dacb3146b4d05b377cf7394516ee823
hiroh-odoo <hiro@odoo.com>
2022-07-19 12:27:45 +0530

    [IMP] sale_(project,timesheet): improve generic UX
    Purpose of this commit is to improve the generic UX of the  project and
    timesheet app.
    
    So, in this commit improve the following:
    
     - set allocated_hous 0 of task when task is created from SO and SOL product
       invoice policy in either based on manual or based on milestone
     - in project.update description improve the following points,
         - hide the 'activities' heading if there is no activity to display.
         - hide the 'profitability' sentence if there in no AAL in project's AA.
     - rename the 'service on timesheet' product into 'service on timesheets'.
    
    task-2924215

--- addons/sale_timesheet/models/project_update.py
+++ addons/sale_timesheet/models/project_update.py
@@ -14,12 +14,13 @@ class ProjectUpdate(models.Model):
         template_values = super(ProjectUpdate, self)._get_template_values(project)
         services = self._get_services_values(project)
         profitability = self._get_profitability_values(project)
+        show_profitability = bool(profitability and profitability.get('analytic_account_id') and (profitability.get('costs') or profitability.get('revenues')))
         show_sold = template_values['project'].allow_billable and len(services.get('data', [])) > 0
         return {
             **template_values,
             'show_sold': show_sold,
-            'show_profitability': bool(profitability),
-            'show_activities': template_values['show_activities'] or show_sold or bool(profitability),
+            'show_profitability': show_profitability,
+            'show_activities': template_values['show_activities'] or show_profitability or show_sold,
             'services': services,
             'profitability': profitability,
             'format_value': lambda value, is_hour: str(round(value, 2)) if not is_hour else format_duration(value),
