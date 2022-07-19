d08cf855efe1ffea1ef9b972a78e2a19de354edb
William Braeckman <wbr@odoo.com>
2022-07-19 15:03:33 +0000

    [FIX] website_sale: prevent adding archived products to cart
    Prevents adding archived products to the cart.
    
    TaskId-2925397
    
    X-original-commit: 49125340122a131e68999818d54fb8de34eabd4b

--- addons/website_sale/tests/test_website_sale_cart.py
+++ addons/website_sale/tests/test_website_sale_cart.py
@@ -47,3 +47,15 @@ class WebsiteSaleCart(TransactionCase):
         with self.assertRaises(UserError):
             with MockRequest(product.with_user(self.public_user).env, website=self.website.with_user(self.public_user)):
                 self.WebsiteSaleController.cart_update_json(product_id=product.id, add_qty=1)
+
+    def test_add_cart_archived_product(self):
+        # Try to add an archived product
+        product = self.env['product.product'].create({
+            'name': 'Test Product',
+            'sale_ok': True,
+        })
+        product.active = False
+
+        with self.assertRaises(UserError):
+            with MockRequest(product.with_user(self.public_user).env, website=self.website.with_user(self.public_user)):
+                self.WebsiteSaleController.cart_update_json(product_id=product.id, add_qty=1)
