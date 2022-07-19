odoo/odoo

commit 4a64f49b2f18ede5c22ac644db182a4118936e8d

[IMP] product: Remove price/pricelist_id field from template/variant
Purpose
=======

The fields are not used, don't work correctly and there is a specific
report to generate the product prices according to the pricelist and
the ordered quantities

addons/website_sale/data/product_snippet_template_data.xml
@@ -283,7 +283,7 @@
         </template>
 
         <template id="price_dynamic_filter_template_product_product" name="Dynamic Product Filter Price">
-            <span t-esc="data['price']" class="font-weight-bold"
+            <span t-esc="record._get_contextual_price()" class="font-weight-bold"
                   t-options="{'widget': 'monetary', 'display_currency': website.currency_id}"/>
             <del t-if="data.get('has_discounted_price')" class="text-danger ml-1 h6" style="white-space: nowrap;"
                  t-esc="data['list_price']" t-options="{'widget': 'monetary', 'display_currency': website.currency_id}"/>

addons/website_sale/models/product_product.py
@@ -19,13 +19,13 @@ class Product(models.Model):
     base_unit_price = fields.Monetary("Price Per Unit", currency_field="currency_id", compute="_compute_base_unit_price")
     base_unit_name = fields.Char(compute='_compute_base_unit_name', help='Displays the custom unit for the products if defined or the selected unit of measure otherwise.')
 
-    @api.depends('price', 'lst_price', 'base_unit_count')
+    @api.depends('lst_price', 'base_unit_count')
     def _compute_base_unit_price(self):
         for product in self:
             if not product.id:
                 product.base_unit_price = 0
             else:
-                product.base_unit_price = product.base_unit_count and (product.price or product.lst_price) / product.base_unit_count
+                product.base_unit_price = product.base_unit_count and product.lst_price / product.base_unit_count
 
     @api.depends('uom_name', 'base_unit_id')
     def _compute_base_unit_name(self):

addons/event_booth_sale/models/event_booth_category.py
@@ -45,7 +45,7 @@ class EventBoothCategory(models.Model):
         for category in self:
             product = category.product_id
             list_price = product.list_price + product.price_extra
-            discount = (list_price - product.price) / list_price if list_price else 0.0
+            discount = (list_price - product._get_contextual_price()) / list_price if list_price else 0.0
             category.price_reduce = (1.0 - discount) * category.price
 
     def _init_column(self, column_name):

addons/event_sale/models/event_ticket.py
@@ -44,7 +44,7 @@ class EventTemplateTicket(models.Model):
                 ticket.description = False
 
     # TODO clean this feature in master
-    # Feature broken by design, depending on the hacky `price` field on products
+    # Feature broken by design, depending on the hacky `_get_contextual_price` field on products
     # context_dependent, core part of the pricelist mess
     # This field usage should be restricted to the UX, and any use in effective
     # price computation should be replaced by clear calls to the pricelist API
@@ -56,7 +56,9 @@ class EventTemplateTicket(models.Model):
             # TODO drop price field usage
             # and ideally remove this whole price_reduce logic
             # seems strange to not apply pricelist logic but still use pricelist discount...
-            discount = (product.lst_price - product.price) / product.lst_price if product.lst_price else 0.0
+            discount = (
+                product.lst_price - product._get_contextual_price()
+            ) / product.lst_price if product.lst_price else 0.0
             ticket.price_reduce = (1.0 - discount) * ticket.price
 
     def _init_column(self, column_name):

addons/point_of_sale/static/tests/tours/point_of_sale.js
@@ -13,35 +13,23 @@ odoo.define('point_of_sale.tour.pricelist', function (require) {
         }
     }
 
-    function _build_pricelist_context (pricelist, quantity, date) {
-        return {
-            pricelist: pricelist.id,
-            quantity: quantity,
-        };
-    }
-
-    function compare_backend_frontend (product, pricelist_name, quantity) {
+    function assertProductPrice(product, pricelist_name, quantity, expected_price) {
         return function () {
             var pricelist = _.findWhere(posmodel.pricelists, {name: pricelist_name});
             var frontend_price = product.get_price(pricelist, quantity);
-            // ORM applies digits= on non-stored computed field when
-            // reading. It does not however truncate like it does when
-            // storing the field.
             frontend_price = round_di(frontend_price, posmodel.dp['Product Price']);
 
-            var context = _build_pricelist_context(pricelist, quantity);
-            return rpc.query({model: 'product.product', method: 'read', args: [[product.id], ['price']], context: context})
-                .then(function (backend_result) {
-                    var debug_info = _.extend(context, {
-                        product: product.id,
-                        product_display_name: product.display_name,
-                        pricelist_name: pricelist.name,
-                    });
-                    var backend_price = backend_result[0].price;
-                    assert(frontend_price === backend_price,
-                           JSON.stringify(debug_info) + ' DOESN\'T MATCH -> ' + backend_price + ' (backend) != ' + frontend_price + ' (frontend)');
-                    return Promise.resolve();
-                });
+            var diff = Math.abs( expected_price - frontend_price );
+
+            assert(diff < 0.001,
+                JSON.stringify({
+                    product: product.id,
+                    product_display_name: product.display_name,
+                    pricelist_name: pricelist_name,
+                    quantity: quantity
+                }) + ' DOESN\'T MATCH -> ' + expected_price + ' != ' + frontend_price);
+
+            return Promise.resolve();
         };
     }
 
@@ -60,27 +48,27 @@ odoo.define('point_of_sale.tour.pricelist', function (require) {
             var product_letter_tray = posmodel.db.search_product_in_category(0, 'Letter Tray')[0];
             var product_whiteboard = posmodel.db.search_product_in_category(0, 'Whiteboard')[0];
 
-            compare_backend_frontend(product_letter_tray, 'Public Pricelist', 0, undefined)()
-                .then(compare_backend_frontend(product_letter_tray, 'Public Pricelist', 1, undefined))
-                .then(compare_backend_frontend(product_letter_tray, 'Fixed', 1, undefined))
-                .then(compare_backend_frontend(product_wall_shelf, 'Fixed', 1, undefined))
-                .then(compare_backend_frontend(product_small_shelf, 'Fixed', 1, undefined))
-                .then(compare_backend_frontend(product_wall_shelf, 'Percentage', 1, undefined))
-                .then(compare_backend_frontend(product_small_shelf, 'Percentage', 1, undefined))
-                .then(compare_backend_frontend(product_magnetic_board, 'Percentage', 1, undefined))
-                .then(compare_backend_frontend(product_wall_shelf, 'Formula', 1, undefined))
-                .then(compare_backend_frontend(product_small_shelf, 'Formula', 1, undefined))
-                .then(compare_backend_frontend(product_magnetic_board, 'Formula', 1, undefined))
-                .then(compare_backend_frontend(product_monitor_stand, 'Formula', 1, undefined))
-                .then(compare_backend_frontend(product_desk_pad, 'Formula', 1, undefined))
-                .then(compare_backend_frontend(product_wall_shelf, 'min_quantity ordering', 1, undefined))
-                .then(compare_backend_frontend(product_wall_shelf, 'min_quantity ordering', 2, undefined))
-                .then(compare_backend_frontend(product_letter_tray, 'Category vs no category', 1, undefined))
-                .then(compare_backend_frontend(product_letter_tray, 'Category', 1, undefined))
-                .then(compare_backend_frontend(product_wall_shelf, 'Product template', 1, undefined))
-                .then(compare_backend_frontend(product_wall_shelf, 'Dates', 1, undefined))
-                .then(compare_backend_frontend(product_small_shelf, 'Pricelist base rounding', 1, undefined))
-                .then(compare_backend_frontend(product_whiteboard, 'Public Pricelist', 1, undefined))
+            assertProductPrice(product_letter_tray, 'Public Pricelist', 0, 4.8)()
+                .then(assertProductPrice(product_letter_tray, 'Public Pricelist', 1, 4.8))
+                .then(assertProductPrice(product_letter_tray, 'Fixed', 1, 1))
+                .then(assertProductPrice(product_wall_shelf, 'Fixed', 1, 2))
+                .then(assertProductPrice(product_small_shelf, 'Fixed', 1, 13.95))
+                .then(assertProductPrice(product_wall_shelf, 'Percentage', 1, 0))
+                .then(assertProductPrice(product_small_shelf, 'Percentage', 1, 0.03))
+                .then(assertProductPrice(product_magnetic_board, 'Percentage', 1, 1.98))
+                .then(assertProductPrice(product_wall_shelf, 'Formula', 1, 6.86))
+                .then(assertProductPrice(product_small_shelf, 'Formula', 1, 2.99))
+                .then(assertProductPrice(product_magnetic_board, 'Formula', 1, 11.98))
+                .then(assertProductPrice(product_monitor_stand, 'Formula', 1, 8.19))
+                .then(assertProductPrice(product_desk_pad, 'Formula', 1, 6.98))
+                .then(assertProductPrice(product_wall_shelf, 'min_quantity ordering', 1, 2))
+                .then(assertProductPrice(product_wall_shelf, 'min_quantity ordering', 2, 1))
+                .then(assertProductPrice(product_letter_tray, 'Category vs no category', 1, 2))
+                .then(assertProductPrice(product_letter_tray, 'Category', 1, 2))
+                .then(assertProductPrice(product_wall_shelf, 'Product template', 1, 1))
+                .then(assertProductPrice(product_wall_shelf, 'Dates', 1, 2))
+                .then(assertProductPrice(product_small_shelf, 'Pricelist base rounding', 1, 13.95))
+                .then(assertProductPrice(product_whiteboard, 'Public Pricelist', 1, 3.2))
                 .then(function () {
                     $('.pos').addClass('done-testing');
                 });

addons/point_of_sale/tests/test_js.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import odoo.tests
+from odoo.tests import tagged, HttpCase
 
 
-@odoo.tests.tagged("post_install", "-at_install")
-class WebSuite(odoo.tests.HttpCase):
+@tagged("post_install", "-at_install")
+class WebSuite(HttpCase):
     def setUp(self):
         super().setUp()
         env = self.env(user=self.env.ref('base.user_admin'))

addons/product/models/product.py
@@ -75,10 +75,6 @@ class ProductProduct(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _order = 'priority desc, default_code, name, id'
 
-    # price: total price, context dependent (partner, pricelist, quantity)
-    price = fields.Float(
-        'Price', compute='_compute_product_price',
-        digits='Product Price', inverse='_set_product_price')
     # price_extra: catalog extra value only, sum of variant extra attributes
     price_extra = fields.Float(
         'Variant Price Extra', compute='_compute_product_price_extra',
@@ -233,41 +229,6 @@ class ProductProduct(models.Model):
 
     def _compute_is_product_variant(self):
         self.is_product_variant = True
-
-    @api.depends_context('pricelist', 'quantity', 'uom', 'date', 'no_variant_attributes_price_extra')
-    def _compute_product_price(self):
-        prices = {}
-        pricelist_id_or_name = self._context.get('pricelist')
-        if pricelist_id_or_name:
-            pricelist = None
-
-            # Support context pricelists specified as list, display_name or ID for compatibility
-            if isinstance(pricelist_id_or_name, list):
-                pricelist_id_or_name = pricelist_id_or_name[0]
-            if isinstance(pricelist_id_or_name, str):
-                pricelist_name_search = self.env['product.pricelist'].name_search(pricelist_id_or_name, operator='=', limit=1)
-                if pricelist_name_search:
-                    pricelist = self.env['product.pricelist'].browse([pricelist_name_search[0][0]])
-            elif isinstance(pricelist_id_or_name, int):
-                pricelist = self.env['product.pricelist'].browse(pricelist_id_or_name)
-
-            if pricelist:
-                quantity = self.env.context.get('quantity', 1.0)
-                uom = self.env['uom.uom'].browse(self.env.context.get('uom'))
-                date = self.env.context.get('date')
-                prices = pricelist._get_products_price(self, quantity, uom=uom, date=date)
-
-        for product in self:
-            product.price = prices.get(product.id, 0.0)
-
-    def _set_product_price(self):
-        for product in self:
-            if self._context.get('uom'):
-                value = self.env['uom.uom'].browse(self._context['uom'])._compute_price(product.price, product.uom_id)
-            else:
-                value = product.price
-            value -= product.price_extra
-            product.write({'list_price': value})
 
     @api.onchange('lst_price')
     def _set_product_lst_price(self):
@@ -749,6 +710,18 @@ class ProductProduct(models.Model):
                                                           and product.product_tmpl_id.product_variant_ids)).mapped('product_tmpl_id')
         (tmpl_to_deactivate + tmpl_to_activate).toggle_active()
         return result
+
+    def _get_contextual_price(self):
+        self.ensure_one()
+        # YTI TODO: During website_sale cleaning, we should get rid of those crappy context thing
+        if not self._context.get('pricelist'):
+            return 0.0
+        pricelist = self.env['product.pricelist'].browse(self._context.get('pricelist'))
+
+        quantity = self.env.context.get('quantity', 1.0)
+        uom = self.env['uom.uom'].browse(self.env.context.get('uom'))
+        date = self.env.context.get('date')
+        return pricelist._get_product_price(self, quantity, uom=uom, date=date)
 
 
 class ProductPackaging(models.Model):

addons/product/models/product_template.py
@@ -69,11 +69,6 @@ class ProductTemplate(models.Model):
     cost_currency_id = fields.Many2one(
         'res.currency', 'Cost Currency', compute='_compute_cost_currency_id')
 
-    # price fields
-    # price: total template price, context dependent (pricelist, quantity)
-    price = fields.Float(
-        'Price', compute='_compute_template_price', inverse='_set_template_price',
-        digits='Product Price')
     # list_price: catalog price, user defined
     list_price = fields.Float(
         'Sales Price', default=1.0,
@@ -99,10 +94,6 @@ class ProductTemplate(models.Model):
 
     sale_ok = fields.Boolean('Can be Sold', default=True)
     purchase_ok = fields.Boolean('Can be Purchased', default=True)
-    # TODO VFE remove this search magic & the price field?
-    pricelist_id = fields.Many2one(
-        'product.pricelist', 'Pricelist', store=False,
-        help='Technical field. Used for searching on pricelists, not stored in database.')
     uom_id = fields.Many2one(
         'uom.uom', 'Unit of Measure',
         default=_get_default_uom_id, required=True,
@@ -188,47 +179,6 @@ class ProductTemplate(models.Model):
     @api.depends_context('company')
     def _compute_cost_currency_id(self):
         self.cost_currency_id = self.env.company.currency_id.id
-
-    @api.depends_context('pricelist', 'partner', 'quantity', 'uom', 'date', 'company')
-    def _compute_template_price(self):
-        prices = self._compute_template_price_no_inverse()
-        for template in self:
-            template.price = prices.get(template.id, 0.0)
-
-    def _compute_template_price_no_inverse(self):
-        """The _compute_template_price writes the 'list_price' field with an inverse method
-        This method allows computing the price without writing the 'list_price'
-        """
-        prices = {}
-        pricelist_id_or_name = self._context.get('pricelist')
-        if pricelist_id_or_name:
-            pricelist = None
-
-            # Support context pricelists specified as list, display_name or ID for compatibility
-            if isinstance(pricelist_id_or_name, list):
-                pricelist_id_or_name = pricelist_id_or_name[0]
-            if isinstance(pricelist_id_or_name, str):
-                pricelist_data = self.env['product.pricelist'].name_search(pricelist_id_or_name, operator='=', limit=1)
-                if pricelist_data:
-                    pricelist = self.env['product.pricelist'].browse(pricelist_data[0][0])
-            elif isinstance(pricelist_id_or_name, int):
-                pricelist = self.env['product.pricelist'].browse(pricelist_id_or_name)
-
-            if pricelist:
-                quantity = self.env.context.get('quantity', 1.0)
-                uom = self.env['uom.uom'].browse(self.env.context.get('uom'))
-                date = self.env.context.get('date')
-                prices = pricelist._get_products_price(self, quantity, uom=uom, date=date)
-
-        return prices
-
-    def _set_template_price(self):
-        if self._context.get('uom'):
-            for template in self:
-                value = self.env['uom.uom'].browse(self._context['uom'])._compute_price(template.price, template.uom_id)
-                template.write({'list_price': value})
-        else:
-            self.write({'list_price': self.price})
 
     @api.depends_context('company')
     @api.depends('product_variant_ids', 'product_variant_ids.standard_price')
@@ -1332,3 +1282,15 @@ class ProductTemplate(models.Model):
             'label': _('Import Template for Products'),
             'template': '/product/static/xls/product_template.xls'
         }]
+
+    def _get_contextual_price(self):
+        self.ensure_one()
+        # YTI TODO: During website_sale cleaning, we should get rid of those crappy context thing
+        if not self._context.get('pricelist'):
+            return 0.0
+        pricelist = self.env['product.pricelist'].browse(self._context.get('pricelist'))
+
+        quantity = self.env.context.get('quantity', 1.0)
+        uom = self.env['uom.uom'].browse(self.env.context.get('uom'))
+        date = self.env.context.get('date')
+        return pricelist._get_product_price(self, quantity, uom=uom, date=date)

commit 275f2c82c7a743643cffe7361e52e89ecb797f41

[FIX] website_sale: no tax included in snipets products
Steps to reproduce:
- Take a product sold on the e-commerce and add taxes
- In the Website settings chose the product prices to be
 shown tax-included
- Add the snippet "Products"

Problem:
The price shown on the snippet is tax-excluded while the
price shown on the product page is tax-included. The
price on the snippet should also be tax-included.

Explanation
Bug introduced by commit 4a64f49b2f18ede5c22ac644db182a4118936e8d
using the _get_contextual_price ignored the option where the taxes
needed to be computed. To solve the issue we create a new function
that calls _get_contextual_price but also add the taxes if
necessary.

opw-2894461

closes odoo/odoo#96285

X-original-commit: a029aea6e332eca8abbc0591f8885afaaa223e75
Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

addons/website_sale/data/product_snippet_template_data.xml
@@ -287,7 +287,7 @@
         </template>
 
         <template id="price_dynamic_filter_template_product_product" name="Dynamic Product Filter Price">
-            <t t-set="record_price" t-value="record._get_contextual_price()"/>
+            <t t-set="record_price" t-value="record._get_contextual_price_tax_selection()"/>
             <t t-if="not website.prevent_zero_price_sale or record_price">
                 <span t-esc="record_price" class="fw-bold"
                       t-options="{'widget': 'monetary', 'display_currency': website.currency_id}"/>

addons/website_sale/models/product_product.py
@@ -94,3 +94,11 @@ class Product(models.Model):
     def _is_add_to_cart_allowed(self):
         self.ensure_one()
         return self.user_has_groups('base.group_system') or (self.sale_ok and self.website_published)
+
+    def _get_contextual_price_tax_selection(self):
+        self.ensure_one()
+        price = self._get_contextual_price()
+        line_tax_type = self.env['ir.config_parameter'].sudo().get_param('account.show_line_subtotals_tax_selection')
+        if line_tax_type == "tax_included" and self.taxes_id:
+            price = self.taxes_id.compute_all(price, product=self, partner=self.env['res.partner'])['total_included']
+        return price