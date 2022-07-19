7727c09a0c32416c7c4febec91002e8339b65ba5
Yolann Sabaux <yosa@odoo.com>
2022-07-19 07:05:08 +0000

    [FIX] project: filter stages based on the user_id
    Steps to reproduce:
    - have a project with a stage having the user_id set to Mitchell Admin (in this scenario you would have to add the field in the view)
    - create a task in this stage
    - log in with Marc Demo
    - Try to open the project
    
    Issue:
    There will be an access error
    
    Cause:
    The domain allows to fecth all tasks from a project; even those from a prohibited stage
    
    Solution:
    - As in d4252825f52a3172420dcda0ea394e42da9f8853, we'll restrict the domain and "hide task stages if user is set".
    - Prevent the user to create/modify a record to it with with a `user_id` and `project_ids`
    
    opw-2917631
    
    closes odoo/odoo#96284
    
    Signed-off-by: Xavier <xbo@odoo.com>

--- addons/project/tests/__init__.py
+++ addons/project/tests/__init__.py
@@ -10,6 +10,7 @@ from . import test_project_sharing
 from . import test_project_sharing_portal_access
 from . import test_project_sharing_ui
 from . import test_project_subtasks
+from . import test_project_task_type
 from . import test_project_ui
 from . import test_project_update_access_rights
 from . import test_project_update_flow
