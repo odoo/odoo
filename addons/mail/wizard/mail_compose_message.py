da34326d33c8cb4f4b983431e3302533167964da
Pierre-Yves Dufays <pydu@odoo.com>
2022-07-19 14:00:28 +0000

    [FIX] test_mass_mailing, {test_}mail: always apply blacklist for mass mailing
    Always apply the blacklist in mass_mail composition mode regardless of the
    recipient model implementing mail.thread.blacklist or not.
    
    This solves the problem of mail sent to black listed address for model not
    inheriting from mail.thread.blacklist.
    
    Technical notes:
    - it has been done in mail.compose.message _get_blacklist_record_ids ignoring
    the mixin mail.thread.blacklist to avoid model change in stable.
    - some tests have one added query because the blacklist is now queried for each
    batch mail sends even if the model of the recipient doesn't implement
    mail.thread.blacklist.
    
    Task-2834862
    
    closes odoo/odoo#118497
    
    X-original-commit: 263e86114c60650f421354e165364afcd4122461
    Signed-off-by: Dufays Pierre-Yves (pydu) <pydu@odoo.com>
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/wizard/mail_compose_message.py
+++ addons/mail/wizard/mail_compose_message.py
@@ -1089,7 +1089,7 @@ class MailComposer(models.TransientModel):
         :return: updated mail_values_dict
         """
         recipients_info = self._get_recipients_data(mail_values_dict)
-        blacklist_ids = self._get_blacklist_record_ids(mail_values_dict)
+        blacklist_ids = self._get_blacklist_record_ids(mail_values_dict, recipients_info)
         optout_emails = self._get_optout_emails(mail_values_dict)
         done_emails = self._get_done_emails(mail_values_dict)
         # in case of an invoice e.g.
@@ -1183,20 +1183,27 @@ class MailComposer(models.TransientModel):
     # EMAIL MANAGEMENT
     # ---------------------------------------------------------------------
 
-    def _get_blacklist_record_ids(self, mail_values_dict):
+    def _get_blacklist_record_ids(self, mail_values_dict, recipients_info=None):
         blacklisted_rec_ids = set()
         if not self.use_exclusion_list:
             return blacklisted_rec_ids
-        if self.composition_mode == 'mass_mail' and issubclass(type(self.env[self.model]), self.pool['mail.thread.blacklist']):
+        if self.composition_mode == 'mass_mail':
             self.env['mail.blacklist'].flush_model(['email', 'active'])
             self._cr.execute("SELECT email FROM mail_blacklist WHERE active=true")
             blacklist = {x[0] for x in self._cr.fetchall()}
-            if blacklist:
+            if not blacklist:
+                return blacklisted_rec_ids
+            if issubclass(type(self.env[self.model]), self.pool['mail.thread.blacklist']):
                 targets = self.env[self.model].browse(mail_values_dict.keys())
                 targets.fetch(['email_normalized'])
                 # First extract email from recipient before comparing with blacklist
                 blacklisted_rec_ids.update(target.id for target in targets
                                            if target.email_normalized in blacklist)
+            elif recipients_info:
+                # Note that we exclude the record if at least one recipient is blacklisted (-> even if not all)
+                # But as commented above: Mass mailing should always have a single recipient per record.
+                blacklisted_rec_ids.update(res_id for res_id, recipient_info in recipients_info.items()
+                                           if blacklist & set(recipient_info['mail_to_normalized']))
         return blacklisted_rec_ids
 
     def _get_done_emails(self, mail_values_dict):
