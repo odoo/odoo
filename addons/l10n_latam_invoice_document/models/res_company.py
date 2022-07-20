54bfef097066bae39329083707a4871c2b231b7c
Nasreddin Boulif (bon) <bon@odoo.com>
2022-07-20 14:58:23 +0200

    [FIX] l10n_latam_invoice_document: Update journal on change country
    Steps to reproduce:
    
      - Install a localisation module (ex.: l10n_ar)
      - Unlink or archive all entries
      - Go to main company (Your company) and switch company to US (
        should be AR)
      - Ensure there is no draft entries for the Sale Journal
      - Create an invoice
    
    Issue:
    
      Fields `Document Type` and `Document Number` are required when they
      should not.
    
    Cause:
    
      When changing a country for a company, we do not update journals
      related to this company to see if they can still use documents
      (l10n_latam_use_documents).
      Therefore, when creating an invoice, the sale journal still have
      `l10n_latam_use_documents` set to True (in case of l10n_ar install),
      even if now country is US.
    
    Solution:
    
      If country is changed, trigger onchange_company on journals to
      recompute `l10n_latam_use_documents` value.
      Note: It might not be possible to change `l10n_latam_use_documents`
      value if journal have validated entries; in this case, an error is
      triggered.
    
    opw-2899680

--- addons/l10n_latam_invoice_document/models/res_company.py
+++ addons/l10n_latam_invoice_document/models/res_company.py
@@ -10,3 +10,14 @@ class ResCompany(models.Model):
         """ This method is to be inherited by localizations and return True if localization use documents """
         self.ensure_one()
         return False
+
+    def write(self, vals):
+        companies = False
+        if 'country_id' in vals:
+            companies = self.filtered(lambda company: company.country_id.id != vals['country_id'])
+        result = super(ResCompany, self).write(vals)
+        if companies:
+            journal_ids = self.env['account.journal'].search([('company_id', 'in', companies.ids)])
+            for journal in journal_ids:
+                journal._onchange_company()
+        return result
