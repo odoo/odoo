a1169f065f70d79b15ebd5a61ce5556c35e6efc6
Nicolas Pierre <npi@odoo.com>
2022-07-25 14:36:40 +0200

    [IMP] stock: adapt counted quantity widget to owl

--- addons/stock/static/src/js/counted_quantity_widget.js
+++ addons/stock/static/src/js/counted_quantity_widget.js
@@ -1,36 +1,20 @@
 /** @odoo-module alias=stock.counted_quantity_widget **/
 
-import BasicFields from 'web.basic_fields';
-import fieldRegistry from 'web.field_registry';
+import { FloatField } from "@web/views/fields/float/float_field";
+import { registry } from "@web/core/registry";
+import { _lt } from "@web/core/l10n/translation";
 
-const CountedQuantityWidgetField = BasicFields.FieldFloat.extend({
-    supportedFieldTypes: ['float'],
+export class CountedQuantityWidgetField extends FloatField{
 
-     _renderReadonly: function () {
-        if (this.recordData.inventory_quantity_set) {
-            this.el.textContent = this._formatValue(this.recordData.inventory_quantity);
-        } else {
-            this.el.textContent = "";
+    get formattedValue() {
+        if (this.props.record.data.inventory_quantity_set) {
+            return super.formattedValue;
         }
-    },
-
-    _onChange: function () {
-        if (!this.recordData.inventory_quantity_set) {
-            this.recordData.inventory_quantity_set = true;
-        }
-        this._super.apply(this);
-    },
-
-    _isSameValue: function(value) {
-        // We want to trigger the update of the view when inserting 0
-        if (value == 0) {
-            return false;
-        }
-        return this._super(...arguments);
+        return "";
     }
 
-});
+}
 
-fieldRegistry.add('counted_quantity_widget', CountedQuantityWidgetField);
+CountedQuantityWidgetField.displayName = _lt("Counted Quantity");
 
-export default CountedQuantityWidgetField;
+registry.category("fields").add('counted_quantity_widget', CountedQuantityWidgetField);
