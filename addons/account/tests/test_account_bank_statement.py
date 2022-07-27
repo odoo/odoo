776441099b9700d6002e01ab0dad66bc73b2d7ff
Florian(flg) <flg@odoo.com>
2022-07-27 10:42:33 +0200

    [FIX] account: Remove useless check on bank statement button post.
    As we allow editing balances in processing state, we do not want
    to check balances before the validation of the bank statement
    (only in case of a bank journal).
    Commit about editing balances: 03f83eb87da00938b9d2df78651c1e06cf8a6d58
    This fix split the _check_balance_end_real_same_as_computed into
    2 functions. One to check "bank" bank statement and one to check
    "cash" bank statement. It allows to keep the same behavior for
    a "cash" bank statement when we post the bank statement, and
    having the right behavior for a "bank" bank statement.
    
    opw-2835728
    
    X-original-commit: b1b66f7190a02380b884e5540b04521f9625cf3f

--- addons/account/tests/test_account_bank_statement.py
+++ addons/account/tests/test_account_bank_statement.py
@@ -294,6 +294,44 @@ class TestAccountBankStatement(TestAccountBankStatementCommon):
             'is_reconciled': True,
         }])
 
+    def test_bank_statement_with_difference(self):
+        """ Test that a bank statement with difference could be posted but not validated. """
+        bank_statement = self.env['account.bank.statement'].create({
+            'name': 'test_statement',
+            'date': '2019-01-01',
+            'journal_id': self.bank_journal_1.id,
+            'balance_start': 10.0,
+            'balance_end_real': 100.0,
+            'balance_end': 10.0,
+            'line_ids': [(0, 0, {
+                'date': fields.Date.to_date('2019-01-01'),
+                'payment_ref': 'transaction_abc',
+                'amount': 10,
+                'partner_id': self.partner_a.id,
+            })]
+        })
+        bank_statement.button_post()
+        # Check if the bank statement is well posted (No check on the balance as the balance could be edited after).
+        self.assertEqual(bank_statement.state, 'posted')
+
+        # Check that the bank statement couldn't be validated with balance_end != balance_end_real
+        with self.assertRaises(UserError):
+            bank_statement.button_validate()
+
+        # Check if we can validate the bank statement if balance_end == balance_end_real
+        bank_statement.balance_end_real = bank_statement.balance_end
+        for line in bank_statement.line_ids:
+            line.reconcile([{
+                'name': line.payment_ref,
+                'partner_id': line.partner_id.id,
+                'currency_id': self.currency_1.id,
+                'account_id': bank_statement.journal_id.suspense_account_id.id,
+                'debit': 10.0,
+                'credit': 0.0,
+                'amount_currency': 10.0,
+            }])
+        bank_statement.button_validate()
+        self.assertEqual(bank_statement.state, 'confirm')
 
 @tagged('post_install', '-at_install')
 class TestAccountBankStatementLine(TestAccountBankStatementCommon):
