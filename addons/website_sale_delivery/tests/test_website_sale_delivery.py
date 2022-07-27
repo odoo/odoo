ae21a49410485f2a7865d1d7d8521f28e5104e0a
roen-odoo <roen@odoo.com>
2022-07-27 11:26:33 +0000

    [FIX] website_sale_delivery: keep delivery method when using giftcards
    Current behavior:
    If you have atleast 2 delivery methods available on the website,
    and try to pay with a giftcard the selected delivery method will
    always switch to the partner preferred delivery method.
    
    Steps to reproduce:
    - Activate giftcards for website
    - Set a preferred shipping method for partner A
    - Make sure the preferred shipping method is website published
    - Publish atleast one other shipping method for the website
    - Go on the website shop, and add product to the cart
    - Process checkout and select any shipping method that is not
      the preferred shipping method of partner A
    - Pay with giftcard
    - The selected delivery method switch back to the preferred method
      of partner A
    
    opw-2883592
    
    closes odoo/odoo#98619
    
    X-original-commit: b356091d579bf4a29c9c8ad84915139984cf16e9
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

---
+++ addons/website_sale_delivery/tests/test_website_sale_delivery.py
@@ -0,0 +1,85 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo.tests.common import HttpCase
+from odoo.tests import tagged
+
+@tagged('post_install', '-at_install')
+class TestWebsiteSaleDelivery(HttpCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.gift_card = self.env['product.product'].create({
+            'name': 'TEST - Gift Card',
+            'list_price': 50,
+            'type': 'service',
+            'is_published': True,
+            'sale_ok': True,
+            'taxes_id': False,
+        })
+
+        # Disable any other program
+        self.env['loyalty.program'].search([]).write({'active': False})
+
+        self.gift_card_program = self.env['loyalty.program'].create({
+            'name': 'Gift Cards',
+            'program_type': 'gift_card',
+            'applies_on': 'future',
+            'trigger': 'auto',
+            'rule_ids': [(0, 0, {
+                'reward_point_amount': 1,
+                'reward_point_mode': 'money',
+                'reward_point_split': True,
+                'product_ids': self.gift_card,
+            })],
+            'reward_ids': [(0, 0, {
+                'reward_type': 'discount',
+                'discount_mode': 'per_point',
+                'discount': 1,
+                'discount_applicability': 'order',
+                'required_points': 1,
+                'description': 'PAY WITH GIFT CARD',
+            })],
+        })
+
+        # Create a gift card to be used
+        self.env['loyalty.card'].create({
+            'program_id': self.gift_card_program.id,
+            'points': 50000,
+            'code': '123456',
+        })
+
+        self.product_delivery_normal1 = self.env['product.product'].create({
+            'name': 'Normal Delivery Charges',
+            'invoice_policy': 'order',
+            'type': 'service',
+        })
+
+        self.product_delivery_normal2 = self.env['product.product'].create({
+            'name': 'Normal Delivery Charges',
+            'invoice_policy': 'order',
+            'type': 'service',
+        })
+
+        self.normal_delivery = self.env['delivery.carrier'].create({
+            'name': 'delivery1',
+            'fixed_price': 5,
+            'delivery_type': 'fixed',
+            'website_published': True,
+            'product_id': self.product_delivery_normal1.id,
+        })
+
+        self.normal_delivery2 = self.env['delivery.carrier'].create({
+            'name': 'delivery2',
+            'fixed_price': 10,
+            'delivery_type': 'fixed',
+            'website_published': True,
+            'product_id': self.product_delivery_normal2.id,
+        })
+
+    def test_shop_sale_gift_card_keep_delivery(self):
+
+        #get admin user and set his preferred shipping method to normal delivery
+        admin_user = self.env.ref('base.user_admin')
+        admin_user.partner_id.write({'property_delivery_carrier_id': self.normal_delivery.id})
+
+        self.start_tour("/", 'shop_sale_giftcard_delivery', login='admin')
