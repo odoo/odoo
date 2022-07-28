f0454a63c1c8a8f42d0d31712c84e4309653af2f
Nshimiyimana Séna <sesn@odoo.com>
2022-07-28 15:51:16 +0200

    [FIX] sale: set correct invoice status
    Steps to reproduce:
    - Create a quotation
    - Add a product that is invoiced on "ordered quantities" and set the
      quantity to a negative number.
    - Leave 'delivered' to 0
    - Confirm the quotation
    - Create the Invoice and validate it.
    - Go to the SO list view (Orders > Orders)
    
    You should see that the Invoice Status of the order you just created is
    'Upselling Opportunity', which is not correct.
    
    Why this is happening:
    
    Here's an explanation on the 'Upselling Opportunity' state (found in
    the code):
    " upselling: this is possible only for a product invoiced on ordered
    quantities for which *we delivered more than expected.* "
    
    In our case, the invoice line's state is set to 'upselling'
    because we have a negative ordered quantity and a zero delivered
    quantity (i.e. we delivered more than expected)
    
    opw-2895218

--- addons/sale/tests/test_sale_order.py
+++ addons/sale/tests/test_sale_order.py
@@ -159,6 +159,20 @@ class TestSaleOrder(TestCommonSaleNoChart):
         self.assertEqual(invoice3.amount_total, 8 * self.product_map['serv_order'].list_price, 'Sale: second invoice total amount is wrong')
         self.assertTrue(self.sale_order.invoice_status == 'invoiced', 'Sale: SO status after invoicing everything (including the upsel) should be "invoiced"')
 
+    def test_invoice_state_when_ordered_quantity_is_negative(self):
+        """When you invoice a SO line with a product that is invoiced on ordered quantities and has negative ordered quantity,
+        this test ensures that the  invoicing status of the SO line is 'invoiced' (and not 'upselling')."""
+        sale_order = self.env['sale.order'].create({
+            'partner_id': self.partner_customer_usd.id,
+            'order_line': [(0, 0, {
+                'product_id': self.product_order.id,
+                'product_uom_qty': -1,
+            })]
+        })
+        sale_order.action_confirm()
+        sale_order._create_invoices(final=True)
+        self.assertTrue(sale_order.invoice_status == 'invoiced', 'Sale: The invoicing status of the SO should be "invoiced"')
+
     def test_sale_sequence(self):
         self.env['ir.sequence'].search([
             ('code', '=', 'sale.order'),
