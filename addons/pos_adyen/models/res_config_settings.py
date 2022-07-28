593ce4f7d35ddd75188863339c2608ad706a40d7
Joseph Caburnay <jcb@odoo.com>
2022-07-28 11:22:42 +0200

    [FIX] point_of_sale,*pos*: remove 'pos' field attribute
    With this change, we are now assuming each field that starts with
    'pos_' to be a 'pos.config' field that has value that will be saved
    to the 'pos_config_id' record. We made that modification because by keeping
    some related fields not written at the same time can trigger contraints at the
    wrong moments that would lead to validation errors when not supposed to be.

--- addons/pos_adyen/models/res_config_settings.py
+++ addons/pos_adyen/models/res_config_settings.py
@@ -8,7 +8,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     # pos.config fields
-    pos_adyen_ask_customer_for_tip = fields.Boolean(compute='_compute_pos_adyen_ask_customer_for_tip', store=True, readonly=False, pos='adyen_ask_customer_for_tip')
+    pos_adyen_ask_customer_for_tip = fields.Boolean(compute='_compute_pos_adyen_ask_customer_for_tip', store=True, readonly=False)
 
     @api.depends('pos_iface_tipproduct', 'pos_config_id')
     def _compute_pos_adyen_ask_customer_for_tip(self):
