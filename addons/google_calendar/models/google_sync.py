50ba5608a429ef415bb8b5a1f5165817d9fd9858
pedrambiria <pebr@odoo.com>
2022-07-28 15:59:59 +0200

    [FIX] google_calendar: not sending emails to existing event attendees Before this commit: syncing with google led to sending emails to attendees of existing future events on Odoo.
    Steps to reproduce the first issue:
    
    - Install 'google_calendar' module
    - Integrate with Google Calendar in setting
    - Add an event to the Odoo calendar for future date
    - Add one external attendee to the event
    - Sync with Google
    
    Invitation emails would be sent to the attendees of the events.
    Solution
    
    It's possible to not send emails to the attendees in api calls. So
    the solution is to not send emails to the attendees for the syncing
    time.
    opw-2819046

--- addons/google_calendar/models/google_sync.py
+++ addons/google_calendar/models/google_sync.py
@@ -199,6 +199,8 @@ class GoogleSync(models.AbstractModel):
             return
         with google_calendar_token(self.env.user.sudo()) as token:
             if token:
+                send_updates = self._context.get('send_updates', True)
+                google_service.google_service = google_service.google_service.with_context(send_updates=send_updates)
                 google_id = google_service.insert(values, token=token, timeout=timeout)
                 self.write({
                     'google_id': google_id,
