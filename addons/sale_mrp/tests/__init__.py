ca4cddcba73a5eb12e1310d8ea22295838ba7c2f
Adrien Widart <awt@odoo.com>
2022-08-01 10:57:19 +0000

    [REF] sale_mrp: extract anglo-saxon tests
    Part-of: odoo/odoo#96882

--- addons/sale_mrp/tests/__init__.py
+++ addons/sale_mrp/tests/__init__.py
@@ -7,3 +7,4 @@ from . import test_sale_mrp_lead_time
 from . import test_sale_mrp_procurement
 from . import test_multistep_manufacturing
 from . import test_sale_mrp_report
+from . import test_sale_mrp_anglo_saxon_valuation
