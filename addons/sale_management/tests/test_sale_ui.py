fb786082d84bba8c2ca5ef3c9cf2f890c9dd22e3
Victor Feyens <vfe@odoo.com>
2022-08-01 13:52:28 +0200

    [REF] sale_management: test infrastructure
    * New common with a unique dumb empty SO template
    * clean tests to rely on the new sale commo data

--- addons/sale_management/tests/test_sale_ui.py
+++ addons/sale_management/tests/test_sale_ui.py
@@ -1,9 +1,11 @@
-import odoo.tests
+# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo.tests import HttpCase, tagged
 
-@odoo.tests.tagged('post_install', '-at_install')
-class TestUi(odoo.tests.HttpCase):
+
+@tagged('post_install', '-at_install')
+class TestUi(HttpCase):
 
     def test_01_sale_tour(self):
         self.start_tour("/web", 'sale_tour', login="admin", step_delay=100)
