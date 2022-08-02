04b61d09fbf24b66244f30ecbbdf6a5d64830a76
dbkosky <dako@odoo.com>
2022-08-02 12:16:12 +0000

    [FIX] account,sale: is_downpayment method on account_move
    Currently there is no simple way of finding if an invoice is a
    downpayment.
    
    Downpayments can be created from a sale order. An invoice is generated
    representing the downpayment. When the sale order is used to generate
    the complete invoice, the downpayment is deducted.
    
    There is no fast way to distinguish a downpayment invoice. The only way
    of knowing is that the lines of the invoice will have sale_line_ids for
    which 'is_downpayment' (a boolean field on the sale order line) is true.
    (also, by default, the product 'Downpayment' is used for the invoice
    line representing the downpayment)
    
    This commit adds a helper method '_is_downpayment' to the account.move
    model, which will return False if sale is not installed, and True if
    sale is installed, and is_downpayment (the boolean field on
    sale.order.line) is true of all the sale lines associated with all the
    lines of the invoice.
    
    Another method '_get_downpayment_lines' is added to the
    account.move.line model which, for all sale lines associated with the
    move line upon which it is called, returns the invoice lines of the sale
    lines for which their move_id._is_downpayment() is true. This
    distinguishes between the downpayment lines in the final invoice (which
    aren't retrieved by this method), and the downpayment lines in the
    downpayment invoice (which are retrieved by this method).
    
    Part-of: odoo/odoo#98534

--- addons/sale/models/account_move.py
+++ addons/sale/models/account_move.py
@@ -125,3 +125,8 @@ class AccountMove(models.Model):
         else:
             result = {'type': 'ir.actions.act_window_close'}
         return result
+
+    def _is_downpayment(self):
+        # OVERRIDE
+        self.ensure_one()
+        return self.line_ids.sale_line_ids and all(sale_line.is_downpayment for sale_line in self.line_ids.sale_line_ids) or False
