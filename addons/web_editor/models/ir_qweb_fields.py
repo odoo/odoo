b66e49ada7485eb83a74c9f0c1b0372a37a7a79e
Romain Derie <rde@odoo.com>
2022-08-02 16:24:51 +0000

    [IMP] web_editor, website: warn restricted editor about forbidden change
    See previous commit(s) for more details about the new sanitize feature.
    
    This commit implement a solution in the website builder to warn
    restricted users when they can't edit a field due to the sanitizer
    restriction.
    
    Long story short: a HTML field can be flag as `sanitize_overridable`
    which will allow users with the `base.group_sanitize_override` group to
    not go through the sanitize process.
    It means that such users can write some content which is not sanitize
    friendly. A restricted user trying to add content in such fields would
    then break the original content as the sanitizer would remove part of
    the DOM.
    In such cases, the field in the website builder is not detected as an
    editable part and clicking on it will warn the user about it.
    
    closes odoo/odoo#97398
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/web_editor/models/ir_qweb_fields.py
+++ addons/web_editor/models/ir_qweb_fields.py
@@ -29,6 +29,7 @@ from werkzeug import urls
 import odoo.modules
 
 from odoo import _, api, models, fields
+from odoo.exceptions import UserError
 from odoo.tools import ustr, posix_to_ldml, pycompat
 from odoo.tools import html_escape as escape
 from odoo.tools.misc import get_lang, babel_locale_parse
@@ -380,8 +381,21 @@ class HTML(models.AbstractModel):
         attrs = super().attributes(record, field_name, options, values)
         if options.get('inherit_branding'):
             field = record._fields[field_name]
-            if field.sanitize and not (field.sanitize_overridable and record.user_has_groups('base.group_sanitize_override')):
-                attrs['data-oe-sanitize'] = 1 if field.sanitize_form else 'allow_form'
+            if field.sanitize:
+                if field.sanitize_overridable and not record.user_has_groups('base.group_sanitize_override'):
+                    try:
+                        field.convert_to_column(record[field_name], record)
+                    except UserError:
+                        # The field contains element(s) that would be removed if
+                        # sanitized. It means that someone who was part of a
+                        # group allowing to bypass the sanitation saved that
+                        # field previously. Mark the field as not editable.
+                        attrs['data-oe-sanitize-prevent-edition'] = 1
+                if not (field.sanitize_overridable and record.user_has_groups('base.group_sanitize_override')):
+                    # Don't mark the field as 'sanitize' if the sanitize is
+                    # defined as overridable and the user has the right to do so
+                    attrs['data-oe-sanitize'] = 1 if field.sanitize_form else 'allow_form'
+
         return attrs
 
     @api.model
