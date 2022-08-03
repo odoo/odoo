76a2d6d79e0590555e92e7d2c9aded9aaa8fa30d
John Laterre (jol) <jol@odoo.com>
2022-08-03 13:06:59 +0000

    [FIX] account,base,web: fix "Send & Print" on invoices
    The purpose of the task is twofold:
    
    1. Remove empty lines in the company address.
    
    Until now, the address format was fixed, which could
    lead to empty lines if one or more field(s) were missing.
    We are now removing empty fields to avoid that.
    
    2. Make sure the external report layout is configured
    before generating the PDF.
    
    This will ensure that the company data will appear
    in the file. If no layout is defined,
    it would not be shown.
    
    task-2834517
    
    closes odoo/odoo#100801
    
    X-original-commit: 5321f21da8dd3456c41f2ea61706e3c953c74a2d
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -3229,8 +3229,10 @@ class AccountMove(models.Model):
             default_email_layout_xmlid="mail.mail_notification_paynow",
             model_description=self.with_context(lang=lang).type_name,
             force_email=True,
+            active_ids=self.ids,
         )
-        return {
+
+        report_action = {
             'name': _('Send Invoice'),
             'type': 'ir.actions.act_window',
             'view_type': 'form',
@@ -3241,6 +3243,11 @@ class AccountMove(models.Model):
             'target': 'new',
             'context': ctx,
         }
+
+        if self.env.is_admin() and not self.env.company.external_report_layout_id and not self.env.context.get('discard_logo_check'):
+            return self.env['ir.actions.report']._action_configure_external_report_layout(report_action)
+
+        return report_action
 
     def _get_new_hash(self, secure_seq_number):
         """ Returns the hash to write on journal entries when they get posted"""
