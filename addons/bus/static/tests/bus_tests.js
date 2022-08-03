odoo/odoo

commit 39747e82095a5162da33c7f458e54da10a46d74b

[IMP] bus, *: introduce multi tab service
*: calendar, survey.

In order to ease the PR introducing websockets in Odoo, introduce
the multi service. Indeed, the cross tab bus won't be necessary
anymore but there will still be a need to elect a main tab: some actions
should only be triggered once. To achieve this, the code electing the main
tab has been split with the one handling the longpolling. The crosstab
bus now relies on the multi tab service to known whether or not the
current tab is the main tab.

task-2053917

closes odoo/odoo#96174

Related: odoo/enterprise#29573
Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

addons/bus/static/src/crosstab_bus.js
@@ -9,34 +9,24 @@ import { Longpolling } from '@bus/longpolling_bus';
  * CrossTab
  *
  * This is an extension of the longpolling bus with browser cross-tab synchronization.
- * It uses a Master/Slaves with Leader Election architecture:
- * - a single tab handles longpolling.
+ * It uses the multiTab service.
+ * - only the main tab handles longpolling.
  * - tabs are synchronized by means of the local storage.
  *
  * localStorage used keys are:
  * - {LOCAL_STORAGE_PREFIX}.{sanitizedOrigin}.channels : shared public channel list to listen during the poll
  * - {LOCAL_STORAGE_PREFIX}.{sanitizedOrigin}.options : shared options
  * - {LOCAL_STORAGE_PREFIX}.{sanitizedOrigin}.notification : the received notifications from the last poll
- * - {LOCAL_STORAGE_PREFIX}.{sanitizedOrigin}.tab_list : list of opened tab ids
- * - {LOCAL_STORAGE_PREFIX}.{sanitizedOrigin}.tab_master : generated id of the master tab
  *
  * trigger:
  * - notification : when a notification is receive from the long polling
- * - become_master : when this tab became the master
- * - no_longer_master : when this tab is not longer the master (the user swith tab)
  */
 export class CrossTab extends Longpolling {
     constructor(env, services) {
         super(env, services);
-         // constants
-        this.TAB_HEARTBEAT_PERIOD = 10000; // 10 seconds
-        this.MASTER_TAB_HEARTBEAT_PERIOD = 1500; // 1.5 seconds
-        this.HEARTBEAT_OUT_OF_DATE_PERIOD = 5000; // 5 seconds
-        this.HEARTBEAT_KILL_OLD_PERIOD = 15000; // 15 seconds
         this.LOCAL_STORAGE_PREFIX = 'bus';
 
         // properties
-        this._isMasterTab = false;
         this._isRegistered = false;
 
         var now = new Date().getTime();
@@ -49,7 +39,11 @@ export class CrossTab extends Longpolling {
             this._callLocalStorage('removeItem', 'last');
         }
         this._lastNotificationID = this._callLocalStorage('getItem', 'last', 0);
+        this._registerWindowUnload();
         browser.addEventListener('storage', this._onStorage.bind(this));
+
+        env.bus.addEventListener('no_longer_main_tab', () => this.stopPolling());
+        env.bus.addEventListener('become_main_tab', () => this.startPolling());
     }
 
     //--------------------------------------------------------------------------
@@ -86,48 +80,23 @@ export class CrossTab extends Longpolling {
     }
 
     /**
-     * Tells whether this bus is related to the master tab.
-     *
-     * @returns {boolean}
-     */
-    isMasterTab() {
-        return this._isMasterTab;
-    }
-
-    /**
      * Use the local storage to share the long polling from the master tab.
      *
      * @override
      */
     startPolling() {
-        if (this._isActive === null) {
-            this._heartbeat = this._heartbeat.bind(this);
-        }
         if (!this._isRegistered) {
             this._isRegistered = true;
 
-            var peers = this._callLocalStorage('getItem', 'peers', {});
-            peers[this._id] = new Date().getTime();
-            this._callLocalStorage('setItem', 'peers', peers);
-
-            this._registerWindowUnload();
-
-            if (!this._callLocalStorage('getItem', 'master')) {
-                this._startElection();
-            }
-
-            this._heartbeat();
-
-            if (this._isMasterTab) {
+            if (this.env.services['multiTab'].isOnMainTab()) {
                 this._callLocalStorage('setItem', 'options', this._options);
             } else {
                 this._options = this._callLocalStorage('getItem', 'options', this._options);
             }
             this._updateChannels();
-            return;  // startPolling will be called again on tab registration
         }
 
-        if (this._isMasterTab) {
+        if (this.env.services['multiTab'].isOnMainTab()) {
             super.startPolling();
         }
     }
@@ -181,100 +150,10 @@ export class CrossTab extends Longpolling {
     }
 
     /**
-     * Check all the time (according to the constants) if the tab is the master tab and
-     * check if it is active. Use the local storage for this checks.
-     *
-     * @private
-     * @see _startElection method
-     */
-    _heartbeat() {
-        var now = new Date().getTime();
-        var heartbeatValue = parseInt(this._callLocalStorage('getItem', 'heartbeat', 0));
-        var peers = this._callLocalStorage('getItem', 'peers', {});
-
-        if ((heartbeatValue + this.HEARTBEAT_OUT_OF_DATE_PERIOD) < now) {
-            // Heartbeat is out of date. Electing new master
-            this._startElection();
-            heartbeatValue = parseInt(this._callLocalStorage('getItem', 'heartbeat', 0));
-        }
-
-        if (this._isMasterTab) {
-            //walk through all peers and kill old
-            var cleanedPeers = {};
-            for (var peerName in peers) {
-                if (peers[peerName] + this.HEARTBEAT_KILL_OLD_PERIOD > now) {
-                    cleanedPeers[peerName] = peers[peerName];
-                }
-            }
-
-            if (heartbeatValue !== this.lastHeartbeat) {
-                // someone else is also master...
-                // it should not happen, except in some race condition situation.
-                this._isMasterTab = false;
-                this.lastHeartbeat = 0;
-                peers[this._id] = now;
-                this._callLocalStorage('setItem', 'peers', peers);
-                this.stopPolling();
-                this.trigger('no_longer_master');
-            } else {
-                this.lastHeartbeat = now;
-                this._callLocalStorage('setItem', 'heartbeat', now);
-                this._callLocalStorage('setItem', 'peers', cleanedPeers);
-            }
-        } else {
-            //update own heartbeat
-            peers[this._id] = now;
-            this._callLocalStorage('setItem', 'peers', peers);
-        }
-
-        var hbPeriod = this._isMasterTab ? this.MASTER_TAB_HEARTBEAT_PERIOD : this.TAB_HEARTBEAT_PERIOD;
-        this._heartbeatTimeout = browser.setTimeout(this._heartbeat.bind(this), hbPeriod);
-    }
-
-    /**
      * @private
      */
     _registerWindowUnload() {
         browser.addEventListener('unload', this._onUnload.bind(this));
-    }
-
-    /**
-     * Check with the local storage if the current tab is the master tab.
-     * If this tab became the master, trigger 'become_master' event
-     *
-     * @private
-     */
-    _startElection() {
-        if (this._isMasterTab) {
-            return;
-        }
-        //check who's next
-        var now = new Date().getTime();
-        var peers = this._callLocalStorage('getItem', 'peers', {});
-        var heartbeatKillOld = now - this.HEARTBEAT_KILL_OLD_PERIOD;
-        var newMaster;
-        for (var peerName in peers) {
-            //check for dead peers
-            if (peers[peerName] < heartbeatKillOld) {
-                continue;
-            }
-            newMaster = peerName;
-            break;
-        }
-
-        if (newMaster === this._id) {
-            //we're next in queue. Electing as master
-            this.lastHeartbeat = now;
-            this._callLocalStorage('setItem', 'heartbeat', this.lastHeartbeat);
-            this._callLocalStorage('setItem', 'master', true);
-            this._isMasterTab = true;
-            this.startPolling();
-            this.trigger('become_master');
-
-            //removing master peer from queue
-            delete peers[newMaster];
-            this._callLocalStorage('setItem', 'peers', peers);
-        }
     }
 
     /**
@@ -284,7 +163,7 @@ export class CrossTab extends Longpolling {
      * @return {boolean} true if the aggregated channels has changed.
      */
     _updateChannels() {
-        const currentPeerIds = new Set(Object.keys(this._callLocalStorage('getItem', 'peers') || {}));
+        const currentPeerIds = new Set(this.env.services['multiTab'].getAllTabIds());
         const peerChannels = this._callLocalStorage('getItem', 'channels')  || {};
         const peerChannelsBefore = JSON.stringify(peerChannels);
         peerChannels[this._id] = Array.from(this._currentTabChannels);
@@ -329,7 +208,7 @@ export class CrossTab extends Longpolling {
      */
     _onPoll(notifications) {
         var notifs = super._onPoll(notifications);
-        if (this._isMasterTab && notifs.length) {
+        if (this.env.services['multiTab'].isOnMainTab() && notifs.length) {
             this._callLocalStorage('setItem', 'last', this._lastNotificationID);
             this._callLocalStorage('setItem', 'last_ts', new Date().getTime());
             this._callLocalStorage('setItem', 'notification', notifs);
@@ -351,18 +230,13 @@ export class CrossTab extends Longpolling {
         var value = JSON.parse(e.newValue);
         var key = e.key;
 
-        if (this._isRegistered && key === this._generateKey('master') && !value) {
-            //master was unloaded
-            this._startElection();
-        }
-
         // last notification id changed
         if (key === this._generateKey('last')) {
             this._lastNotificationID = value || 0;
         }
         // notifications changed
         else if (key === this._generateKey('notification')) {
-            if (!this._isMasterTab) {
+            if (!this.env.services['multiTab'].isOnMainTab()) {
                 this.trigger("notification", value);
             }
         }
@@ -384,16 +258,7 @@ export class CrossTab extends Longpolling {
      * @private
      */
     _onUnload() {
-        // unload peer
-        var peers = this._callLocalStorage('getItem', 'peers') || {};
-        delete peers[this._id];
-        this._callLocalStorage('setItem', 'peers', peers);
         this._currentTabChannels.clear();
         this._updateChannels();
-
-        // unload master
-        if (this._isMasterTab) {
-            this._callLocalStorage('removeItem', 'master');
-        }
     }
 }

addons/bus/static/tests/bus_tests.js
@@ -3,6 +3,7 @@ odoo.define('web.bus_tests', function (require) {
 
 var { busService } = require('@bus/services/bus_service');
 const { presenceService } = require('@bus/services/presence_service');
+const { multiTabService } = require('@bus/services/multi_tab_service');
 
 var { CrossTab } = require('@bus/crosstab_bus');
 var testUtils = require('web.test_utils');
@@ -14,17 +15,18 @@ const { makeTestEnv } = require('@web/../tests/helpers/mock_env');
 
 QUnit.module('Bus', {
     beforeEach: function () {
-        const customBusService = {
-            ...busService,
+        const customMultiTabService = {
+            ...multiTabService,
             start() {
-                const originalBusService = busService.start(...arguments);
-                originalBusService.TAB_HEARTBEAT_PERIOD = 10;
-                originalBusService.MASTER_TAB_HEARTBEAT_PERIOD = 1;
-                return originalBusService;
+                const originalMultiTabService = multiTabService.start(...arguments);
+                originalMultiTabService.TAB_HEARTBEAT_PERIOD = 10;
+                originalMultiTabService.MAIN_TAB_HEARTBEAT_PERIOD = 1;
+                return originalMultiTabService;
             },
         };
-        registry.category('services').add('bus_service', customBusService);
+        registry.category('services').add('bus_service', busService);
         registry.category('services').add('presence', presenceService);
+        registry.category('services').add('multiTab', customMultiTabService);
     },
 }, function () {
     QUnit.test('notifications received from the longpolling channel', async function (assert) {
@@ -194,7 +196,7 @@ QUnit.module('Bus', {
         ]);
     });
 
-    QUnit.test('cross tab bus elect new master on master unload', async function (assert) {
+    QUnit.test('multi tab service elects new master on master unload', async function (assert) {
         assert.expect(8);
 
         // master
@@ -240,7 +242,7 @@ QUnit.module('Bus', {
         await testUtils.nextTick();
 
         // simulate unloading master
-        masterEnv.services['bus_service']._onUnload();
+        masterEnv.services['multiTab']._onUnload();
 
         pollPromiseSlave.resolve([{
             id: 2,

addons/bus/static/src/services/multi_tab_service.js
@@ -1,0 +1,213 @@
+/** @odoo-module **/
+
+import { registry } from '@web/core/registry';
+import { browser } from '@web/core/browser/browser';
+import session from 'web.session';
+
+/**
+ * This class uses a Master/Slaves with Leader Election architecture in
+ * order to keep track of the main tab. Tabs are synchronized thanks to the
+ * localStorage.
+ *
+ * localStorage used keys are:
+ * - {LOCAL_STORAGE_PREFIX}.{sanitizedOrigin}.lastPresenceByTab:
+ *   mapping of tab ids to their last recorded presence.
+ * - {LOCAL_STORAGE_PREFIX}.{sanitizedOrigin}.main : id of the current
+ *   main tab.
+ * - {LOCAL_STORAGE_PREFIX}.{sanitizedOrigin}.heartbeat : last main tab
+ *   heartbeat time.
+ *
+ * trigger on env.bus:
+ * - become_main_tab : when this tab became the main.
+ * - no_longer_main_tab : when this tab is no longer the main.
+ */
+export class MultiTab {
+    constructor(env) {
+        this.env = env;
+
+        // CONSTANTS
+        this.TAB_HEARTBEAT_PERIOD = 10000; // 10 seconds
+        this.MAIN_TAB_HEARTBEAT_PERIOD = 1500; // 1.5 seconds
+        this.HEARTBEAT_OUT_OF_DATE_PERIOD = 5000; // 5 seconds
+        this.HEARTBEAT_KILL_OLD_PERIOD = 15000; // 15 seconds
+        this.LOCAL_STORAGE_PREFIX = 'multiTabService';
+
+        // PROPERTIES
+        this._isOnMainTab = false;
+        this._sanitizedOrigin = session.origin.replace(/:\/{0,2}/g, '_');
+
+        const now = new Date().getTime();
+        this._id = _.uniqueId(this.LOCAL_STORAGE_PREFIX) + ':' + now;
+        browser.addEventListener('unload', this._onUnload.bind(this));
+        browser.addEventListener('storage', this._onStorage.bind(this));
+        // REGISTER THIS TAB
+        const lastPresenceByTab = this._callLocalStorage('getItem', 'lastPresenceByTab', {});
+        lastPresenceByTab[this._id] = now;
+        this._callLocalStorage('setItem', 'lastPresenceByTab', lastPresenceByTab);
+
+        if (!this._callLocalStorage('getItem', 'main')) {
+            this._startElection();
+        }
+        this._heartbeat();
+    }
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    isOnMainTab() {
+        return this._isOnMainTab;
+    }
+
+    getAllTabIds() {
+        return Object.keys(this._callLocalStorage('getItem', 'lastPresenceByTab', {}));
+    }
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Call browser localStorage.
+     *
+     * @private
+     * @param {string} method (getItem, setItem, removeItem)
+     * @param {string} key
+     * @param {any} param
+     * @returns Result of the called method, parsed.
+     */
+    _callLocalStorage(method, key, param) {
+        if (method === 'setItem') {
+            param = JSON.stringify(param);
+        }
+        const result = browser.localStorage[method](this._generateKey(key), param);
+        if (method === 'getItem') {
+            return result ? JSON.parse(result) : param;
+        }
+    }
+
+    /**
+     * Generates localStorage keys prefixed by LOCAL_STORAGE_PREFIX and
+     * the sanitized origin, to prevent keys from conflicting when
+     * several multi tab services co-exist.
+     *
+     * @private
+     * @param {string} key
+     * @returns Key prefixed with the origin.
+     */
+    _generateKey(key) {
+        return this.LOCAL_STORAGE_PREFIX + '.' + this._sanitizedOrigin + '.' + key;
+    }
+
+    /**
+     * Check all the time (according to the constants) if the tab is the main tab and
+     * check if it is active. Use the local storage for this checks.
+     *
+     * @private
+     * @see _startElection method
+     */
+    _heartbeat() {
+        const now = new Date().getTime();
+        let heartbeatValue = this._callLocalStorage('getItem', 'heartbeat', 0);
+        const lastPresenceByTab = this._callLocalStorage('getItem', 'lastPresenceByTab', {});
+        if (heartbeatValue + this.HEARTBEAT_OUT_OF_DATE_PERIOD < now) {
+            // Heartbeat is out of date. Electing new main.
+            this._startElection();
+            heartbeatValue = this._callLocalStorage('getItem', 'heartbeat', 0);
+        }
+        if (this._isOnMainTab) {
+            // Walk through all tabs and kill old ones.
+            const cleanedTabs = {};
+            for (const [tabId, lastPresence] of Object.entries(lastPresenceByTab)) {
+                if (lastPresence + this.HEARTBEAT_KILL_OLD_PERIOD > now) {
+                    cleanedTabs[tabId] = lastPresence;
+                }
+            }
+            if (heartbeatValue !== this.lastHeartbeat) {
+                // Someone else is also main...
+                // It should not happen, except in some race condition situation.
+                this._isOnMainTab = false;
+                this.lastHeartbeat = 0;
+                lastPresenceByTab[this._id] = now;
+                this._callLocalStorage('setItem', 'lastPresenceByTab', lastPresenceByTab);
+                this.env.bus.trigger('no_longer_main_tab');
+            } else {
+                this.lastHeartbeat = now;
+                this._callLocalStorage('setItem', 'heartbeat', now);
+                this._callLocalStorage('setItem', 'lastPresenceByTab', cleanedTabs);
+            }
+        } else {
+            // Update own heartbeat.
+            lastPresenceByTab[this._id] = now;
+            this._callLocalStorage('setItem', 'lastPresenceByTab', lastPresenceByTab);
+        }
+        const hbPeriod = this._isOnMainTab ? this.MAIN_TAB_HEARTBEAT_PERIOD : this.TAB_HEARTBEAT_PERIOD;
+        this._heartbeatTimeout = browser.setTimeout(this._heartbeat.bind(this), hbPeriod);
+    }
+
+    /**
+     * Check with the local storage if the current tab is the main tab.
+     * If this tab became the main, trigger 'become_main_tab' event.
+     *
+     * @private
+     */
+    _startElection() {
+        if (this._isOnMainTab) {
+            return;
+        }
+        // Check who's next.
+        const now = new Date().getTime();
+        const lastPresenceByTab = this._callLocalStorage('getItem', 'lastPresenceByTab', {});
+        const heartbeatKillOld = now - this.HEARTBEAT_KILL_OLD_PERIOD;
+        let newMain;
+        for (const [tab, lastPresence] of Object.entries(lastPresenceByTab)) {
+            // Check for dead tabs.
+            if (lastPresence < heartbeatKillOld) {
+                continue;
+            }
+            newMain = tab;
+            break;
+        }
+        if (newMain === this._id) {
+            // We're next in queue. Electing as main.
+            this.lastHeartbeat = now;
+            this._callLocalStorage('setItem', 'heartbeat', this.lastHeartbeat);
+            this._callLocalStorage('setItem', 'main', true);
+            this._isOnMainTab = true;
+            this.env.bus.trigger('become_main_tab');
+            // Removing main peer from queue.
+            delete lastPresenceByTab[newMain];
+            this._callLocalStorage('setItem', 'lastPresenceByTab', lastPresenceByTab);
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    _onStorage({ key, newValue }) {
+        if (key === this._generateKey('main') && !newValue) {
+            // Main was unloaded.
+            this._startElection();
+        }
+    }
+
+    _onUnload() {
+        const lastPresenceByTab = this._callLocalStorage('getItem', 'lastPresenceByTab', {});
+        delete lastPresenceByTab[this._id];
+        this._callLocalStorage('setItem', 'lastPresenceByTab', lastPresenceByTab);
+
+        // Unload main.
+        if (this._isOnMainTab) {
+            this._callLocalStorage('removeItem', 'main');
+        }
+    }
+}
+
+export const multiTabService = {
+    start(env) {
+        return new MultiTab(env);
+    },
+};
+
+registry.category('services').add('multiTab', multiTabService);

addons/bus/static/src/services/bus_service.js
@@ -28,7 +28,7 @@ export class BusService extends CrossTab {
      */
     sendNotification(options, callback) {
         if (window.Notification && Notification.permission === "granted") {
-            if (this.isMasterTab()) {
+            if (this.env.services['multiTab'].isOnMainTab()) {
                 try {
                     this._sendNativeNotification(options.title, options.message, callback);
                 } catch (error) {
@@ -45,7 +45,7 @@ export class BusService extends CrossTab {
             }
         } else {
             this.env.services['notification'].add(options.message, options);
-            if (this.isMasterTab()) {
+            if (this.env.services['multiTab'].isOnMainTab()) {
                 this._beep();
             }
         }
@@ -121,7 +121,7 @@ export class BusService extends CrossTab {
 }
 
 export const busService = {
-    dependencies: ['notification', 'presence', 'rpc'],
+    dependencies: ['notification', 'presence', 'rpc', 'multiTab'],
     start(env, services) {
         return new BusService(env, services);
     },

addons/bus/static/src/services/legacy/make_multi_tab_to_legacy_env.js
@@ -1,0 +1,14 @@
+/** @odoo-module **/
+
+import { registry } from '@web/core/registry';
+
+export function makeMultiTabToLegacyEnv(legacyEnv) {
+    return {
+        dependencies: ['multiTab'],
+        start(_, { multiTab }) {
+            legacyEnv.services['multiTab'] = multiTab;
+        },
+    };
+}
+
+registry.category('wowlToLegacyServiceMappers').add('multi_tab_to_legacy_env', makeMultiTabToLegacyEnv);

addons/bus/static/tests/assets_watchdog_tests.js
@@ -2,6 +2,7 @@
 
 import { busService } from "@bus/services/bus_service";
 import { presenceService } from "@bus/services/presence_service";
+import { multiTabService } from "@bus/services/multi_tab_service";
 
 import { createWebClient } from "@web/../tests/webclient/helpers";
 import { assetsWatchdogService } from "@bus/services/assets_watchdog_service";
@@ -17,6 +18,7 @@ QUnit.module("Bus Assets WatchDog", (hooks) => {
         serviceRegistry.add("assetsWatchdog", assetsWatchdogService);
         serviceRegistry.add("bus_service", busService);
         serviceRegistry.add("presence", presenceService);
+        serviceRegistry.add("multiTab", multiTabService);
         patchWithCleanup(browser, {
             setTimeout(fn) {
                 return this._super(fn, 0);

commit b6f1c2e2197b4218c5f3f223f1e10249768bfa7c

[FIX] bus: poll different channels in different tabs
Commit 10c90e5 doesn't seem to work properly.
Steps to reproduce:
- Open a spreadsheet A in one tab
- Open a spreadsheet B in another tab
=> the master tab should poll both spreadsheet channels but it
does not.

The condition here sheems wrong
https://github.com/odoo/odoo/blob/bf2ce0aa0c7d45c90d7ff104d5af5f99b29d59ae/addons/bus/static/src/js/crosstab_bus.js#L299
`peerChannelsBefore` is not really "before the channel was added".
It's "before outdated channels are cleared".
It already contains the new channel added to local storage by the slave tab!
Hence it wrongly returns `false` which means the poll request is not
restarted.

Additional fix required since 39747e8
-------------------------------------

Commit 39747e8 broke a little more the feature.
It is assumed the ids in `channels` are matching the ids in `lastPresenceByTab`
(previously named `peers`). It was no longer true since `lastPresenceByTab`
is now managed by multi_tab service (with it's own id).
With those non-matching ids, channels from other tabs were always considered
outdated and therefore cleaned up.

Small comment on the change in the mock server
----------------------------------------------

Cross tab bus tests were not working properly.
The error message string "XmlHttpRequestError abort" was stringified
(stringifying a string), leading to "\"XmlHttpRequestError abort\"". Since the
implementation depends on the exact error message, tests didn't propely
reflect reality.
This is technically not needed anymore since bus tests are no longer using
the legacy mock server but it can't hurt to fix it. (the new mock server
doesn't have this issue)

closes odoo/odoo#97036

X-original-commit: f25f36a
Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
Co-authored-by: Goaman <nby@odoo.com>
Co-authored-by: tsm-odoo <tsm@odoo.com>

addons/bus/static/src/crosstab_bus.js
@@ -132,10 +132,9 @@ export class CrossTab extends Longpolling {
         }
 
         const peerChannelsAfter = JSON.stringify(peerChannels);
-        if (peerChannelsBefore === peerChannelsAfter) {
-            return false;
+        if (peerChannelsBefore !== peerChannelsAfter) {
+            this.env.services['multi_tab'].setSharedValue('channels', peerChannels);
         }
-        this.env.services['multi_tab'].setSharedValue('channels', peerChannels);
 
         const allChannels = new Set();
         for (const channels of Object.values(peerChannels)) {
@@ -148,8 +147,13 @@ export class CrossTab extends Longpolling {
         for (const channel of this._currentTabChannels) {
             allChannels.add(channel);
         }
-        this._channels = Array.from(allChannels);
-        return true;
+        const allChannelsSorted = Array.from(allChannels).sort();
+        if (JSON.stringify(allChannelsSorted) === JSON.stringify(this._channels.sort())) {
+            return false;
+        } else {
+            this._channels = allChannelsSorted;
+            return true;
+        }
     }
 
     //--------------------------------------------------------------------------

addons/bus/static/src/longpolling_bus.js
@@ -9,7 +9,7 @@ const { EventBus } = owl;
  * - notification : when a notification is receive from the long polling
  */
 export class Longpolling extends EventBus {
-    constructor(env) {
+    constructor(env, { multi_tab }) {
         super();
         this.env = env;
 
@@ -20,7 +20,7 @@ export class Longpolling extends EventBus {
 
         // PROPERTIES
         this._isActive = null;
-        this._id = _.uniqueId('bus');
+        this._id = multi_tab.currentTabId;
         this._lastNotificationID = 0;
         this._pollRetryTimeout = null;
 

addons/bus/static/src/multi_tab_service.js
@@ -172,6 +172,9 @@ export const multiTabService = {
 
         return {
             bus,
+            get currentTabId() {
+                return tabId;
+            },
             /**
              * Determine whether or not this tab is the main one.
              *

addons/bus/static/tests/bus_tests.js
@@ -317,6 +317,32 @@ QUnit.module('Bus', {
             "Tab 2: addChannel beta",
         ]);
     });
+
+    QUnit.test('two tabs adding a different channel', async function (assert) {
+        const firstTabEnv = await makeTestEnv({
+            mockRPC: function (route, args) {
+                if (route === '/longpolling/poll') {
+                    assert.step(args.channels.join());
+                    return testUtils.makeTestPromise();
+                }
+            }
+        });
+        const secondTabEnv = await makeTestEnv({
+            mockRPC: function (route, args) {
+                if (route === '/longpolling/poll') {
+                    throw new Error("slave tab should not use the polling route");
+                }
+            }
+        });
+
+        firstTabEnv.services['bus_service'].addChannel("alpha");
+        await nextTick();
+        assert.verifySteps(["alpha"]);
+
+        secondTabEnv.services['bus_service'].addChannel("beta");
+        await nextTick();
+        assert.verifySteps(["alpha,beta"]);
+    });
 });
 
 });

addons/web/static/tests/legacy/helpers/mock_server.js
@@ -177,7 +177,7 @@ var MockServer = Class.extend({
         }, function (result) {
             var message = result && result.message;
             var event = result && result.event;
-            var errorString = JSON.stringify(message || false);
+            var errorString = typeof message !== "string" ? JSON.stringify(message || false) : message;
             if (debug) {
                 console.warn(
                     '%c[rpc] response (error) %s%s, during test %s',