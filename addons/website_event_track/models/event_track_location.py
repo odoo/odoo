31c625b76f0f12cb0da5e3014de5f61ab572d5a4
Pratik Raval <prra@odoo.com>
2022-08-08 09:25:43 +0000

    [IMP] website_event_track: add location ordering in event agenda
    This commit adds a `sequence` field in `event.track.location`, allowing event
    managers to organize their event location as they want in the "Agenda" page.
    To allow re-arrangement of the locations a bit more visible and easy, this
    commit adds handle widget on track locations tree view, and removes the debug
    group from the menu "Events > Configuration > Track Locations".
    
    To match the order on agenda page and on the back-end, the same ordering
    is applied at model level as well.
    
    taskID-2942634
    
    closes odoo/odoo#97763
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_event_track/models/event_track_location.py
+++ addons/website_event_track/models/event_track_location.py
@@ -7,5 +7,7 @@ from odoo import fields, models
 class TrackLocation(models.Model):
     _name = "event.track.location"
     _description = 'Event Track Location'
+    _order = 'sequence, id'
 
     name = fields.Char('Location', required=True)
+    sequence = fields.Integer(default=10, help='Define the order in which the location will appear on "Agenda" page')
