e0c7ae63abfc6e3e16726b08e0d41f820fa8779e
Eteil Djoumatchoua (etdj) <etdj@odoo.com>
2022-08-08 08:54:45 +0000

    [FIX] gift_card,pos_gift_card: Card with no expiration date can't be used
    Steps to reproduce the issue:
    - Create a gift card without expiration date
    - Create an order an use the gift card code
    
    Issue:
    Empty expiration dates have a boolean type(don't really know why), so when the validity of the card is checked,
    we try to compare a boolean to the current date
    
    Solution:
    On check, return true if the expiration date is empty both in sales and point of sale.
    
    opw-2947267
    
    Signed-off by: Eteil Djoumatchoua <etdj@odoo.com>
    
    closes odoo/odoo#97678
    
    Signed-off-by: Grazioso Andrea (agr) <agr@odoo.com>

--- addons/gift_card/models/gift_card.py
+++ addons/gift_card/models/gift_card.py
@@ -50,7 +50,7 @@ class GiftCard(models.Model):
 
     def can_be_used(self):
         # expired state are computed once a day, so can be not synchro
-        return self.state == 'valid' and self.balance > 0 and self.expired_date >= fields.Date.today()
+        return self.state == 'valid' and self.balance > 0 and (not self.expired_date or self.expired_date >= fields.Date.today())
 
     @api.depends("initial_amount")
     def _compute_balance(self):
