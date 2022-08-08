dddbc86e3f96410ee783305ea4266976af90e43f
Eteil Djoumatchoua (etdj) <etdj@odoo.com>
2022-08-08 08:54:45 +0000

    [FIX] gift_card,pos_gift_card: Card with no expiration date can't be used
    Steps to reproduce the issue:
    - Create a gift card without expiration date
    - Create an order an use the gift card code
    
    Issue:
    Empty expiration dates have a boolean type(don't really know why), so when the validity of the card is checked,
    we try to compare a boolean to the current date
    
    Solution:
    On check, return true if the expiration date is empty both in sales and point of sale.
    
    opw-2947267
    
    Signed-off by: Eteil Djoumatchoua <etdj@odoo.com>
    
    X-original-commit: e0c7ae63abfc6e3e16726b08e0d41f820fa8779e

---
+++ addons/pos_gift_card/models/gift_card.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class GiftCard(models.Model):
+    _inherit = "gift.card"
+
+    buy_pos_order_line_id = fields.Many2one(
+        "pos.order.line",
+        copy=False,
+        readonly=True,
+        help="Pos Order line where this gift card has been bought.",
+    )
+    redeem_pos_order_line_ids = fields.One2many(
+        "pos.order.line", "gift_card_id", string="Pos Redeems"
+    )
+
+    @api.depends("redeem_pos_order_line_ids")
+    def _compute_balance(self):
+        super()._compute_balance()
+        for record in self:
+            confirmed_line = record.redeem_pos_order_line_ids.sudo().filtered(
+                lambda l: l.order_id.state in ('paid', 'done', 'invoiced')
+            )
+            balance = record.balance
+            if confirmed_line:
+                balance -= sum(
+                    confirmed_line.mapped(
+                        lambda line: line.currency_id._convert(
+                            line.price_unit,
+                            record.currency_id,
+                            record.env.company,
+                            line.create_date,
+                        )
+                        * -1
+                    )
+                )
+            record.balance = balance
+
+    def can_be_used_in_pos(self):
+        # expired state are computed once a day, so can be not synchro
+        return self.state == 'valid' and self.balance > 0 and (not self.expired_date or self.expired_date >= fields.Date.today())
