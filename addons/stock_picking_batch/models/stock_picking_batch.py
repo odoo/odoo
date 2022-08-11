9e1975f3ea4cf07eb73a4702a13baa01ec45caa3
Ahmed Khalaf (ahkh) <ahkh@odoo.com>
2022-08-11 09:39:28 +0200

    [IMP] stock_picking_batch: remove ensure_one in action_cancel
    This commit removes a needless `ensure_one` in batch `action_cancel`
    
    X-original-commit: cf6013fff67441f67800df56349877c820873110

--- addons/stock_picking_batch/models/stock_picking_batch.py
+++ addons/stock_picking_batch/models/stock_picking_batch.py
@@ -209,7 +209,6 @@ class StockPickingBatch(models.Model):
         return True
 
     def action_cancel(self):
-        self.ensure_one()
         self.state = 'cancel'
         self.picking_ids = False
         return True
