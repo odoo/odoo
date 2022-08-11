ddaa7dbfd0d11fc4b38f7a2cfd4f2725a488b561
Benjamin Vray <bvr@odoo.com>
2022-08-11 09:14:11 +0000

    [IMP] website, *: review mobile view layout
    *: web_editor, website_blog
    
    This commit improves the mobile preview of websites. A mobile phone
    image has been added around the iframe during the mobile preview. This
    image was already used for the themes mobile preview, this is why the
    css code has been unified to be used in these 2 mobile previews.
    Parallel to this commit, we have modified the websites displayed in the
    iframe of the preview of the themes so that the scrollbar of these is
    visible (before it was hidden behind the image of the phone) but with a
    smaller width in mobile mode.
    
    This commit also improves the button to switch to mobile preview by
    coloring it green when mobile preview is active.
    
    task-2890050
    
    closes odoo/odoo#98061
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/static/src/systray_items/mobile_preview.js
+++ addons/website/static/src/systray_items/mobile_preview.js
@@ -3,11 +3,12 @@
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 
-const { Component } = owl;
+const { Component, useState } = owl;
 
 class MobilePreviewSystray extends Component {
     setup() {
         this.websiteService = useService('website');
+        this.state = useState(this.websiteService.context);
     }
 }
 MobilePreviewSystray.template = "website.MobilePreviewSystray";
