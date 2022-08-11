b7aca87cdc6f3bc619097dfea7f9e84532983e35
Miquel Raïch <miquel.raich@forgeflow.com>
2022-08-11 11:08:10 +0200

    [FIX] mrp, purchase, sale: correct filter on button to pivot view

--- addons/purchase/models/product.py
+++ addons/purchase/models/product.py
@@ -44,7 +44,7 @@ class ProductTemplate(models.Model):
         action['context'] = {
             'graph_measure': 'qty_ordered',
             'search_default_orders': 1,
-            'time_ranges': {'field': 'date_approve', 'range': 'last_365_days'}
+            'search_default_filter_date_order_365days': 1,
         }
         return action
 
@@ -62,7 +62,6 @@ class ProductProduct(models.Model):
             ('product_id', 'in', self.ids),
             ('date_order', '>', date_from)
         ]
-        PurchaseOrderLines = self.env['purchase.order.line'].search(domain)
         order_lines = self.env['purchase.order.line'].read_group(domain, ['product_id', 'product_uom_qty'], ['product_id'])
         purchased_data = dict([(data['product_id'][0], data['product_uom_qty']) for data in order_lines])
         for product in self:
@@ -75,7 +74,7 @@ class ProductProduct(models.Model):
         action = self.env.ref('purchase.action_purchase_order_report_all').read()[0]
         action['domain'] = ['&', ('state', 'in', ['purchase', 'done']), ('product_id', 'in', self.ids)]
         action['context'] = {
-            'search_default_last_year_purchase': 1,
+            'search_default_filter_date_order_365days': 1,
             'search_default_status': 1, 'search_default_order_month': 1,
             'graph_measure': 'qty_ordered'
         }
