09bcdf17041bcbec7b03d28e09a641e0eac17ad1
Kartik Chavda <kcv@odoo.com>
2022-08-13 06:06:55 +0000

    [IMP] {hr,sale}_timesheet: compute total time wo checking security rules
    Before this commit total time on timesheet for projects and
    invoices computed according to user's access to timesheet and
    security rules on account.analytic.line are quite complex.
    
    So, in this commit compute those fields with sudo to ease the
    perfomance of project and sale apps.
    
    task-2890184
    
    closes odoo/odoo#99372
    
    Related: odoo/enterprise#30950
    Related: odoo/upgrade#3843
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_timesheet/models/account_move.py
+++ addons/sale_timesheet/models/account_move.py
@@ -11,9 +11,11 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     timesheet_ids = fields.One2many('account.analytic.line', 'timesheet_invoice_id', string='Timesheets', readonly=True, copy=False)
-    timesheet_count = fields.Integer("Number of timesheets", compute='_compute_timesheet_count')
+    timesheet_count = fields.Integer("Number of timesheets", compute='_compute_timesheet_count', compute_sudo=True)
     timesheet_encode_uom_id = fields.Many2one('uom.uom', related='company_id.timesheet_encode_uom_id')
-    timesheet_total_duration = fields.Integer("Timesheet Total Duration", compute='_compute_timesheet_total_duration', help="Total recorded duration, expressed in the encoding UoM, and rounded to the unit")
+    timesheet_total_duration = fields.Integer("Timesheet Total Duration",
+        compute='_compute_timesheet_total_duration', compute_sudo=True,
+        help="Total recorded duration, expressed in the encoding UoM, and rounded to the unit")
 
     @api.depends('timesheet_ids', 'company_id.timesheet_encode_uom_id')
     def _compute_timesheet_total_duration(self):
