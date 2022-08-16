0c9e9e3c64746b5bc0f02b497dc6e5554e005f66
Yannick Tivisse <yti@odoo.com>
2022-08-16 11:21:21 +0000

    [IMP] sale_expense: Add stat button from expense sheet to linked SO
    Purpose
    =======
    
    Make it easier to navigate between an expense report and the sales orders it impacted.
    
    closes odoo/odoo#98169
    
    Taskid: 2948615
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/sale_expense/models/__init__.py
+++ addons/sale_expense/models/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import hr_expense
+from . import hr_expense_sheet
 from . import hr_expense_split
 from . import sale_order
 from . import product_template
