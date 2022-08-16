3255f21ea857ba5ae5fa622929787b23690f6762
Yannick Tivisse <yti@odoo.com>
2022-08-16 11:21:21 +0000

    [IMP] sale_expense: Add stat button from expense sheet to linked SO
    Purpose
    =======
    
    Make it easier to navigate between an expense report and the sales orders it impacted.
    
    closes odoo/odoo#98169
    
    Taskid: 2948615
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/sale_expense/__manifest__.py
+++ addons/sale_expense/__manifest__.py
@@ -18,6 +18,7 @@ This module allow to reinvoice employee expense, by setting the SO directly on t
         'views/product_view.xml',
         'views/hr_expense_views.xml',
         'views/sale_order_views.xml',
+        'views/hr_expense_sheet_views.xml',
     ],
     'demo': ['data/sale_expense_demo.xml'],
     'installable': True,
