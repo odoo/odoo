250c398959a05888a83b45fe5d79e55072217df8
Yolann Sabaux <yosa@odoo.com>
2022-08-16 09:55:49 +0000

    [FIX] purchase: update analytic account on change of the line
    Steps to reproduce:
    - create two products
    - create two analytic accounts
    - create two new analytic rules by assigning an account to a product
    - Create a new PO and select on of the product
    - On the same line, change the product to the other one
    
    Issue:
    - The analytic account won't be updated
    
    Cause:
    Bypass of the `account_analytic_id` whenever there is already on defined
    
    Solution:
    For the `Sale` fix, I added the `line.state == 'draft'`.
    The reason? Because the compute is triggered after confirmation of the order due to:
    https://github.com/odoo/odoo/blob/14.0/addons/sale/models/sale.py#L931-L935
    Therefore, the compute won't also be triggered after confirmation of the SO.
    
    opw-2948950
    
    closes odoo/odoo#99858
    
    X-original-commit: 61604e18128c1cfd0e6d4e510852f780491d6e4e
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -881,7 +881,7 @@ class SaleOrderLine(models.Model):
     @api.depends('product_id', 'order_id.date_order', 'order_id.partner_id')
     def _compute_analytic_tag_ids(self):
         for line in self:
-            if not line.display_type and not line.analytic_tag_ids:
+            if not line.display_type and line.state == 'draft':
                 default_analytic_account = line.env['account.analytic.default'].sudo().account_get(
                     product_id=line.product_id.id,
                     partner_id=line.order_id.partner_id.id,
