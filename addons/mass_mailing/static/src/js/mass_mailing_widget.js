2bf085ad6094268f0dc31e18276fd50d92508bea
Nicolas Bayet <nby@odoo.com>
2022-08-17 14:56:18 +0000

    [FIX] web_editor: properly remove image loading attribute
    The fix that removed the loading attribute of images was targeting
    the wrong method `_createWysiwygInstance`.
    In 15.0, the method does have a typo (`_createWysiwygIntance`).
    
    X-original-commit: 7a352330094ee2e267f29d9f479b4e31e90dbec6

--- addons/mass_mailing/static/src/js/mass_mailing_widget.js
+++ addons/mass_mailing/static/src/js/mass_mailing_widget.js
@@ -124,6 +124,7 @@ var MassMailingFieldHtml = FieldHtml.extend({
         // We don't want to drop snippets directly within the wysiwyg.
         this.$content.removeClass('o_editable');
         initializeDesignTabCss(this.wysiwyg.getEditable());
+        this.wysiwyg.getEditable().find('img').attr('loading', '');
     },
     /**
      * Returns true if the editable area is empty.
@@ -159,11 +160,6 @@ var MassMailingFieldHtml = FieldHtml.extend({
             this.value = this.recordData[this.nodeOptions['inline-field']];
         }
         return this._super.apply(this, arguments);
-    },
-    _createWysiwygInstance: async function () {
-        const res = await this._super(...arguments);
-        this.wysiwyg.getEditable().find('img').attr('loading', '');
-        return res;
     },
 
     /**
