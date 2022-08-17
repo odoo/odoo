20c7562513546f95b8d4acdeed9f604119b6a36b
mafo-odoo <mafo@odoo.com>
2022-08-17 13:22:40 +0000

    [FIX] payment_stripe: 3DS failure puts transaction in error
    Problem:
    When using a card requiring 3D secure authentication
    The payment is confirmed as received by Odoo whereas the
    authentication for 3DS is not completed yet. If we change
    the payment flow to 'redirect to acquirer website', then
    It's working as expected, Stripe only redirects to Odoo
    when the authentication with 3DS is done.
    
    Expected behavior:
    The payment should be displayed as received only when the
    authentication with 3DS Secure is completed!
    
    Solution:
    To fix the issue we add an rpc route that can put a
    transaction in error and we use the payment_intent and
    payment_intent_secret like if they were access_tokens
    opw-2909300
    
    X-original-commit: a739d443b29163bcf6f54c2fa277d59723dbfefd

--- addons/payment_stripe/controllers/main.py
+++ addons/payment_stripe/controllers/main.py
@@ -19,6 +19,7 @@ _logger = logging.getLogger(__name__)
 
 
 class StripeController(http.Controller):
+<<<<<<< HEAD
     _checkout_return_url = '/payment/stripe/checkout_return'
     _validation_return_url = '/payment/stripe/validation_return'
     _webhook_url = '/payment/stripe/webhook'
@@ -175,3 +176,111 @@ class StripeController(http.Controller):
             return False
 
         return True
+||||||| parent of 9d516ff6d694... temp
+    _success_url = '/payment/stripe/success'
+    _cancel_url = '/payment/stripe/cancel'
+
+    @http.route(['/payment/stripe/success', '/payment/stripe/cancel'], type='http', auth='public')
+    def stripe_success(self, **kwargs):
+        request.env['payment.transaction'].sudo().form_feedback(kwargs, 'stripe')
+        return werkzeug.utils.redirect('/payment/process')
+
+    @http.route(['/payment/stripe/s2s/create_json_3ds'], type='json', auth='public', csrf=False)
+    def stripe_s2s_create_json_3ds(self, verify_validity=False, **kwargs):
+        if not kwargs.get('partner_id'):
+            kwargs = dict(kwargs, partner_id=request.env.user.partner_id.id)
+        token = request.env['payment.acquirer'].browse(int(kwargs.get('acquirer_id'))).with_context(stripe_manual_payment=True).s2s_process(kwargs)
+
+        if not token:
+            res = {
+                'result': False,
+            }
+            return res
+
+        res = {
+            'result': True,
+            'id': token.id,
+            'short_name': token.short_name,
+            '3d_secure': False,
+            'verified': False,
+        }
+
+        if verify_validity != False:
+            token.validate()
+            res['verified'] = token.verified
+
+        return res
+
+    @http.route('/payment/stripe/s2s/create_setup_intent', type='json', auth='public', csrf=False)
+    def stripe_s2s_create_setup_intent(self, acquirer_id, **kwargs):
+        acquirer = request.env['payment.acquirer'].browse(int(acquirer_id))
+        res = acquirer.with_context(stripe_manual_payment=True)._create_setup_intent(kwargs)
+        return res.get('client_secret')
+
+    @http.route('/payment/stripe/s2s/process_payment_intent', type='json', auth='public', csrf=False)
+    def stripe_s2s_process_payment_intent(self, **post):
+        return request.env['payment.transaction'].sudo().form_feedback(post, 'stripe')
+
+    @http.route('/payment/stripe/webhook', type='json', auth='public', csrf=False)
+    def stripe_webhook(self, **kwargs):
+        data = json.loads(request.httprequest.data)
+        request.env['payment.acquirer'].sudo()._handle_stripe_webhook(data)
+        return 'OK'
+=======
+    _success_url = '/payment/stripe/success'
+    _cancel_url = '/payment/stripe/cancel'
+
+    @http.route(['/payment/stripe/success', '/payment/stripe/cancel'], type='http', auth='public')
+    def stripe_success(self, **kwargs):
+        request.env['payment.transaction'].sudo().form_feedback(kwargs, 'stripe')
+        return werkzeug.utils.redirect('/payment/process')
+
+    @http.route(['/payment/stripe/s2s/create_json_3ds'], type='json', auth='public', csrf=False)
+    def stripe_s2s_create_json_3ds(self, verify_validity=False, **kwargs):
+        if not kwargs.get('partner_id'):
+            kwargs = dict(kwargs, partner_id=request.env.user.partner_id.id)
+        token = request.env['payment.acquirer'].browse(int(kwargs.get('acquirer_id'))).with_context(stripe_manual_payment=True).s2s_process(kwargs)
+
+        if not token:
+            res = {
+                'result': False,
+            }
+            return res
+
+        res = {
+            'result': True,
+            'id': token.id,
+            'short_name': token.short_name,
+            '3d_secure': False,
+            'verified': False,
+        }
+
+        if verify_validity != False:
+            token.validate()
+            res['verified'] = token.verified
+
+        return res
+
+    @http.route('/payment/stripe/s2s/create_setup_intent', type='json', auth='public', csrf=False)
+    def stripe_s2s_create_setup_intent(self, acquirer_id, **kwargs):
+        acquirer = request.env['payment.acquirer'].browse(int(acquirer_id))
+        res = acquirer.with_context(stripe_manual_payment=True)._create_setup_intent(kwargs)
+        return res.get('client_secret')
+
+    @http.route('/payment/stripe/s2s/process_payment_intent', type='json', auth='public', csrf=False)
+    def stripe_s2s_process_payment_intent(self, **post):
+        return request.env['payment.transaction'].sudo().form_feedback(post, 'stripe')
+
+    @http.route('/payment/stripe/s2s/process_payment_error', type='json', auth='public', csrf=False)
+    def stripe_s2s_process_payment_error(self, **post):
+        transaction_sudo = request.env['payment.transaction'].sudo().search([('reference', '=', post['reference']),
+                                                                            ('provider', '=', 'stripe'),
+                                                                            ('stripe_payment_intent_secret', '=', post['stripe_payment_intent_secret'])])
+        transaction_sudo.write({'state': 'error', 'state_message': post['error']})
+
+    @http.route('/payment/stripe/webhook', type='json', auth='public', csrf=False)
+    def stripe_webhook(self, **kwargs):
+        data = json.loads(request.httprequest.data)
+        request.env['payment.acquirer'].sudo()._handle_stripe_webhook(data)
+        return 'OK'
+>>>>>>> 9d516ff6d694... temp
