5b054031a35160fa9253c223b068fe94daa75ddc
Raphael Collet <rco@odoo.com>
2022-08-18 16:22:50 +0200

    [FIX] *: avoid non-searchable field dependencies

--- addons/hr/models/hr_employee.py
+++ addons/hr/models/hr_employee.py
@@ -37,7 +37,7 @@ class HrEmployeePrivate(models.Model):
     active = fields.Boolean('Active', related='resource_id.active', default=True, store=True, readonly=False)
     company_id = fields.Many2one('res.company', required=True)
     company_country_id = fields.Many2one('res.country', 'Company Country', related='company_id.country_id', readonly=True)
-    company_country_code = fields.Char(related='company_country_id.code', readonly=True)
+    company_country_code = fields.Char(related='company_country_id.code', depends=['company_country_id'], readonly=True)
     # private partner
     address_home_id = fields.Many2one(
         'res.partner', 'Address', help='Enter here the private address of the employee, not the one linked to your company.',
