5eb2c956bce3114b2307b77c00e3a40993af347b
mafo-odoo <mafo@odoo.com>
2022-08-18 10:54:45 +0000

    [FIX] stock_account, purchase_stock: no stock account in manual valuation
    Issues:
    1) When the inventory valuation is Manual the stock accounts field of the
    product category should be empty and they should be set when the
    inventory valuation is Automated.
    2) For the moment at installation all the categories have the Manual default
    value and the comapy account default values for the stock accounts (not
    respecting condition 1)
    
    Solutions:
    For issue 1 we add checks on the write and create functions of the product
    category model so that after creation/modification the instance respect the
    condition
    For issue 2 we add at the end of the post_hook script some code to set an
    empty stock account property for every product category.
    
    opw-2746384
    
    X-original-commit: 9dc7835cf62302ce05db822dcc1db9e5b577338f

--- addons/stock_account/tests/test_stockvaluationlayer.py
+++ addons/stock_account/tests/test_stockvaluationlayer.py
@@ -891,6 +891,11 @@ class TestStockValuationChangeValuation(TestStockValuationCommon):
             'property_stock_valuation_account_id': cls.stock_valuation_account.id,
             'property_stock_journal': cls.stock_journal.id,
         })
+        cls.env.company.write({
+            'property_stock_account_input_categ_id': cls.stock_input_account.id,
+            'property_stock_account_output_categ_id': cls.stock_output_account.id,
+            'property_stock_valuation_account_id': cls.stock_valuation_account.id,
+        })
 
     def test_standard_manual_to_auto_1(self):
         self.product1.product_tmpl_id.categ_id.property_cost_method = 'standard'
