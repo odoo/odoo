76b6afc6f27db3282e22a691b2b12c3d129a1795
std-odoo <std@odoo.com>
2022-08-19 08:03:55 +0000

    [FIX] im_livechat: fix the session duration
    Bug
    ===
    The session duration added in the KPI have the wrong
    value because of a typo in the SQL query.
    
    Task-2827996
    
    closes odoo/odoo#116007
    
    X-original-commit: f286ba3d6311f8620d8baa8022bbf092b5001b54
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>
    Signed-off-by: Stéphane Debauche (std) <std@odoo.com>

--- addons/im_livechat/tests/__init__.py
+++ addons/im_livechat/tests/__init__.py
@@ -2,4 +2,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_get_mail_channel
+from . import test_im_livechat_report
 from . import test_im_livechat_support_page
