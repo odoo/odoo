8d4662eecb52486dcc5d67042cd7c5caa97eac80
pedrambiria <pebr@odoo.com>
2022-08-19 08:55:52 +0000

    [FIX] point_of_sale: remove "willstart" for a performance issue
    Before this commit: if the number of contacts is enormous, and the
    "Limited Partners Loading" with "Load all remaining partners in the
    background" is enabled, clicking on the product information button would
    take lots of time to reload. Also, it would cause the same issue for the
    "ClosePoSPopup".
    
    The problem is that when "willstart" takes time more than the background
    process it would call it again, and for every call.
    
    The solution is to prepare the "willstart" function's data and pass it to the
    constructor.
    
    opw-2956104
    
    X-original-commit: 0201d93e4ec2441638171f36d67c2afaac183807

--- addons/point_of_sale/static/src/js/Popups/ProductInfoPopup.js
+++ addons/point_of_sale/static/src/js/Popups/ProductInfoPopup.js
@@ -3,8 +3,6 @@ odoo.define('point_of_sale.ProductInfoPopup', function(require) {
 
     const AbstractAwaitablePopup = require('point_of_sale.AbstractAwaitablePopup');
     const Registries = require('point_of_sale.Registries');
-    const { identifyError } = require('point_of_sale.utils');
-    const { ConnectionLostError } = require('@web/core/network/rpc_service')
 
     /**
      * This popup needs to be self-dependent because it needs to be called from different place. In order to avoid code
@@ -17,47 +15,7 @@ odoo.define('point_of_sale.ProductInfoPopup', function(require) {
     class ProductInfoPopup extends AbstractAwaitablePopup {
         setup() {
             super.setup();
-            owl.onWillStart(this.onWillStart);
-        }
-        async onWillStart() {
-            const order = this.env.pos.get_order();
-            try {
-                // check back-end method `get_product_info_pos` to see what it returns
-                // We do this so it's easier to override the value returned and use it in the component template later
-                this.productInfo = await this.rpc({
-                    model: 'product.product',
-                    method: 'get_product_info_pos',
-                    args: [[this.props.product.id],
-                        this.props.product.get_price(order.pricelist, this.props.quantity),
-                        this.props.quantity,
-                        this.env.pos.config.id],
-                    kwargs: {context: this.env.session.user_context},
-                });
-
-                const priceWithoutTax = this.productInfo['all_prices']['price_without_tax'];
-                const margin = priceWithoutTax - this.props.product.standard_price;
-                const orderPriceWithoutTax = order.get_total_without_tax();
-                const orderCost = order.get_total_cost();
-                const orderMargin = orderPriceWithoutTax - orderCost;
-
-                this.costCurrency = this.env.pos.format_currency(this.props.product.standard_price);
-                this.marginCurrency = this.env.pos.format_currency(margin);
-                this.marginPercent = priceWithoutTax ? Math.round(margin/priceWithoutTax * 10000) / 100 : 0;
-                this.orderPriceWithoutTaxCurrency = this.env.pos.format_currency(orderPriceWithoutTax);
-                this.orderCostCurrency = this.env.pos.format_currency(orderCost);
-                this.orderMarginCurrency = this.env.pos.format_currency(orderMargin);
-                this.orderMarginPercent = orderPriceWithoutTax ? Math.round(orderMargin/orderPriceWithoutTax * 10000) / 100 : 0;
-            } catch (error) {
-                this.cancel();
-                if (identifyError(error) instanceof ConnectionLostError) {
-                    this.showPopup('ErrorPopup', {
-                        title: this.env._t('Network Error'),
-                        body: this.env._t('Cannot access product information screen if offline.'),
-                    });
-                } else {
-                    throw error;
-                }
-            }
+            Object.assign(this, this.props.info);
         }
         searchProduct(productName) {
             this.env.posbus.trigger('search-product-from-info-popup', productName);
