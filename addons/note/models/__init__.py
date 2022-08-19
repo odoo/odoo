11f288db00d83924d1932bab7bc7c0a62148fa61
Thibault Delavallée <tde@odoo.com>
2022-08-19 08:46:43 +0200

    [MOV] calendar: move activity (type) code and views
    Purpose is to have one file / model, easier when searching for inherited parts
    of the code and see at a glance various usage of those models.
    
    Spotted during Task-2207626 (Rating: Delay rating notification to ease feedback)

--- addons/note/models/__init__.py
+++ addons/note/models/__init__.py
@@ -2,5 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import mail_activity
+from . import mail_activity_type
 from . import note
 from . import res_users
