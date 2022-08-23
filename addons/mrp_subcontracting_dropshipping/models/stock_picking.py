41e4276770add28a2b4f3d4b7fe1633f13f0a506
Adrien Widart <awt@odoo.com>
2022-08-23 11:53:55 +0000

    [FIX] mrp_subcontracting_dropshipping: dropship from PO
    Confirming a PO with a customer as delivery address does not work
    
    To reproduce the issue:
    1. In Settings, enable "Multi-Locations"
    2. Create two products P_finished, P_compo
        - P_finished:
            - Add a vendor V
    3. Create a bill of materials:
        - Product: P_finished
        - Type: Subcontract
        - Subcontractor: V
        - Components: 1 x P_compo
    4. Create a PO:
        - Vendor: V
        - Products: 1 x P_finished
        - Deliver To: Dropship
        - Drop Ship Address: a customer
    5. Confirm the PO
    
    Error: a validation error is displayed: "[...] a mandatory field is not
    set. [...] Model: Production Order (mrp.production), Field: Operation
    Type (picking_type_id)"
    
    When confirming the PO, we extract some values of the subcontracted SM
    to create a MO:
    https://github.com/odoo/odoo/blob/829369d3ca0530f1aad0599fbb1598810a928cc3/addons/mrp_subcontracting/models/stock_picking.py#L105-L117
    However, because the SM is not created from the SO, it does not have any
    `sale_line_id`. And, because its a dropshipped one, neither the SM nor
    its picking type has a defined warehouse. As a result, `_get_warehouse`
    does not return anything and we can't define the `picking_type_id` of
    the MO. This is the reason why the error will be triggered on its
    creation.
    
    OPW-2922546
    
    X-original-commit: 28a20daab6a7097ecd26bf66eeb80b2f5ad294a8

--- addons/mrp_subcontracting_dropshipping/models/stock_picking.py
+++ addons/mrp_subcontracting_dropshipping/models/stock_picking.py
@@ -16,3 +16,15 @@ class StockPicking(models.Model):
         res = super()._action_done()
         self.move_ids.move_dest_ids._action_assign()
         return res
+
+    def _prepare_subcontract_mo_vals(self, subcontract_move, bom):
+        res = super()._prepare_subcontract_mo_vals(subcontract_move, bom)
+        if not res.get('picking_type_id') and subcontract_move.location_dest_id.usage == 'customer':
+            # If the if-condition is respected, it means that `subcontract_move` is not
+            # related to a specific warehouse. This can happen if, for instance, the user
+            # confirms a PO with a subcontracted product that should be delivered to a
+            # customer (dropshipping). In that case, we can use a default warehouse to
+            # get the picking type
+            default_warehouse = self.env['stock.warehouse'].search([('company_id', '=', subcontract_move.company_id.id)], limit=1)
+            res['picking_type_id'] = default_warehouse.subcontracting_type_id.id,
+        return res
