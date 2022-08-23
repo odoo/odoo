5c4fcbeb3091a4d853b388a2ef3f8a6c8e506661
Simon Genin (ges) <ges@odoo.com>
2022-08-23 09:15:22 +0000

    [FIX] sms: fix sms button in phone field
    The sms field extension had been badly converted.
    The button didn't do anything cause the action wasn't properly
    configured.
    
    closes odoo/odoo#98646
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/sms/static/src/components/sms_button/sms_button.js
+++ addons/sms/static/src/components/sms_button/sms_button.js
@@ -7,16 +7,27 @@ const { Component } = owl;
 export class SendSMSButton extends Component {
     setup() {
         this.action = useService("action");
+        this.user = useService("user");
         this.title = this.env._t("Send SMS Text Message");
     }
     onClick() {
         this.action.doAction({
             type: "ir.actions.act_window",
-            name: this.title,
-            res_id: this.props.record.resId,
-            res_model: "sms.composer",
             target: "new",
+            name: this.title,
+            res_model: "sms.composer",
             views: [[false, "form"]],
+            context: {
+                ...this.user.context,
+                default_res_model: this.props.record.resModel,
+                default_res_id: this.props.record.resId,
+                default_number_field_name: this.props.name,
+                default_composition_mode: 'comment',
+            }
+        }, {
+            onClose: () => {
+                this.props.record.model.load()
+            },
         });
     }
 };
