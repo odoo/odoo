935262edf06c10e83d7349cb56e73d99542b18e5
Yaroslav <yaso@odoo.com>
2022-08-24 10:21:23 +0200

    [FIX] hw_drivers: Printers disconnecting issue fix
    This commit aims to solve the reported issue where printers are switching states from "connected" to "disconnected" and stay unreachable for 2 minutes. Now theavailable printers will be searched for multiple times before disconnecting them
    task 2891909
    
    Fixes 2687380
    Fixes 2856101

--- addons/hw_drivers/iot_handlers/interfaces/PrinterInterface.py
+++ addons/hw_drivers/iot_handlers/interfaces/PrinterInterface.py
@@ -3,6 +3,7 @@
 
 from cups import Connection as cups_connection
 from re import sub
+from time import sleep
 from threading import Lock
 
 from odoo.addons.hw_drivers.interface import Interface
@@ -16,26 +17,34 @@ class PrinterInterface(Interface):
     connection_type = 'printer'
 
     def get_devices(self):
-        printer_devices = {}
-        with cups_lock:
-            printers = conn.getPrinters()
-            devices = conn.getDevices()
-            for printer_name, printer in printers.items():
-                path = printer.get('device-uri', False)
-                if printer_name != self.get_identifier(path):
-                    printer.update({'supported': True}) # these printers are automatically supported
-                    device_class = 'network'
-                    if 'usb' in printer.get('device-uri'):
-                        device_class = 'direct'
-                    printer.update({'device-class': device_class})
-                    printer.update({'device-make-and-model': printer}) # give name setted in Cups
-                    printer.update({'device-id': ''})
-                    devices.update({printer_name: printer})
-        for path, device in devices.items():
-            identifier = self.get_identifier(path)
-            device.update({'identifier': identifier})
-            device.update({'url': path})
-            printer_devices.update({identifier: device})
+        max_retry = 3
+        # Before deleting printer from the list retry 3 times to see if it's really removed
+        for _ in range(max_retry):
+            with cups_lock:
+                printer_devices = {}
+                printers = conn.getPrinters()
+                devices = conn.getDevices()
+                for printer_name, printer in printers.items():
+                    path = printer.get('device-uri', False)
+                    if printer_name != self.get_identifier(path):
+                        printer.update({'supported': True}) # these printers are automatically supported
+                        device_class = 'network'
+                        if 'usb' in printer.get('device-uri'):
+                            device_class = 'direct'
+                        printer.update({'device-class': device_class})
+                        printer.update({'device-make-and-model': printer}) # give name setted in Cups
+                        printer.update({'device-id': ''})
+                        devices.update({printer_name: printer})
+            for path, device in devices.items():
+                identifier = self.get_identifier(path)
+                device.update({'identifier': identifier})
+                device.update({'url': path})
+                printer_devices.update({identifier: device})
+            removed = self._detected_devices - printer_devices.keys()
+            if removed:
+                sleep(10)
+            else:
+                break
         return printer_devices
 
     def get_identifier(self, path):
