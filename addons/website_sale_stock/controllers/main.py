2e4a225fb4504d7cca4cb6bda3dfe7fe3866e4f6
Alexandre de Pape (alpa) <alpa@odoo.com>
2022-08-24 16:50:28 +0000

    [IMP] websale_stock: back in stock notification
    This commit adds a message "Be notified when in stock" on a product when it is out
    of stock. When clicking the message, a form appears where the user can enter
    his email. If he is a logged user, the email associated to the partner of the
    user is pre-filled in the form. Otherwise the user enters his email.
    
    task-id 2806828
    
    closes odoo/odoo#96633
    
    Related: odoo/upgrade#3718
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- addons/website_sale_stock/controllers/main.py
+++ addons/website_sale_stock/controllers/main.py
@@ -2,10 +2,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.addons.website_sale.controllers import main as website_sale_controller
-
-from odoo import http
+from odoo.tools import email_re
+from odoo import http, _
 from odoo.http import request
 from odoo.exceptions import ValidationError
+from werkzeug.exceptions import BadRequest
 
 
 class PaymentPortal(website_sale_controller.PaymentPortal):
@@ -26,3 +27,30 @@ class PaymentPortal(website_sale_controller.PaymentPortal):
         if values:
             raise ValidationError(' '.join(values))
         return super().shop_payment_transaction(*args, **kwargs)
+
+
+class WebsiteSale(website_sale_controller.WebsiteSale):
+    @http.route(['/shop/add/stock_notification'], type="json", auth="public", website=True)
+    def add_stock_email_notification(self, email, product_id):
+        if not email_re.match(email):
+            raise BadRequest(_("Invalid Email"))
+
+        product = request.env['product.product'].browse(int(product_id))
+        partners = request.env['res.partner'].sudo()._mail_find_partner_from_emails([email], force_create=True)
+        partner = partners[0]
+
+        if not product._has_stock_notification(partner):
+            product.sudo().stock_notification_partner_ids += partner
+
+        if request.website.is_public_user():
+            request.session['product_with_stock_notification_enabled'] = request.session.get(
+                'product_with_stock_notification_enabled',
+                set()
+            ) | {product_id}
+            request.session['stock_notification_email'] = email
+
+    def _prepare_product_values(self, product, category='', search='', **kwargs):
+        values = super()._prepare_product_values(product, category, search, **kwargs)
+        # We need the user mail to prefill the back of stock notification, so we put it in the value that will be sent
+        values['user_email'] = request.env.user.email or request.session.get('stock_notification_email', '')
+        return values
