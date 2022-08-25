4f22171f9cf53cd3f67eff6bce1e6ce47cb750ff
Benoit Socias <bso@odoo.com>
2022-08-25 14:14:33 +0000

    [FIX] website, *: define specific dynamic snippet name
    *: web_editor, website_blog, website_sale
    
    Since [1] when the Dynamic Snippet was first introduced, it also
    introduced a concept of "inherited" snippets. Specific snippets would
    all `t-call` the same template for their rendering.
    A mechanism was introduced to deduce the `data-snippet` from the caller
    template, but it stored the obtained value in the `t-called` template
    itself. Because of this if several "specific snippets" that used that
    template had to be rendered, they would all have the `data-snippet`
    value of the first one that got compiled.
    
    We could compile the snippet template into something having a
    dynamically obtained `data-snippet` value, but then that would be
    equivalent to just using a `t-attf-data-snippet`.
    All specific snippets already do set a `snippet_name` in the context
    because it needs to be added in the classes.
    
    This commit therefore adds a `t-att-data-snippet` attribute on the
    base template, and populates with that same value in `onBuilt` for
    stable versions.
    
    During forward ports across stable versions, each new caller must be
    patched as well - and all patches must be removed in master.
    
    [1]: https://github.com/odoo/odoo/pull/53175
    
    task-2922635
    
    X-original-commit: d34925e3c2dcb089893bb1ec9e2690ff1632d01b

--- addons/web_editor/models/ir_qweb.py
+++ addons/web_editor/models/ir_qweb.py
@@ -64,7 +64,7 @@ class QWeb(models.AbstractModel):
                 sub_call = el.get('t-call')
                 if sub_call:
                     el.set('t-options', f"{{'snippet-key': '{snippet_key}', 'snippet-sub-call-key': '{sub_call}'}}")
-                # If it already has a data-snippet it is a saved snippet.
+                # If it already has a data-snippet it is a saved or an inherited snippet.
                 # Do not override it.
                 elif 'data-snippet' not in el.attrib:
                     el.attrib['data-snippet'] = snippet_key.split('.', 1)[-1]
