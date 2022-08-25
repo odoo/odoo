e57647208fcde6ea582c921d67d0153da89a90ca
AaronHForgeFlow <ahenriquez@eficent.com>
2022-08-25 17:45:32 +0200

    [IMP] stock_account: prepare methods for _stock_account_prepare_anglo_saxon_out_lines_vals

--- addons/stock_account/models/account_move.py
+++ addons/stock_account/models/account_move.py
@@ -125,44 +125,15 @@ class AccountMove(models.Model):
                 if not debit_interim_account or not credit_expense_account:
                     continue
 
-                # Compute accounting fields.
-                sign = -1 if move.move_type == 'out_refund' else 1
-                price_unit = line._stock_account_get_anglo_saxon_price_unit()
-                balance = sign * line.quantity * price_unit
+                interim_account_line_vals = self._prepare_interim_account_line_vals(
+                    line, move, debit_interim_account
+                )
+                lines_vals_list.append(interim_account_line_vals)
+                expense_account_line_vals = self._prepare_expense_account_line_vals(
+                    line, move, credit_expense_account
+                )
+                lines_vals_list.append(expense_account_line_vals)
 
-                # Add interim account line.
-                lines_vals_list.append({
-                    'name': line.name[:64],
-                    'move_id': move.id,
-                    'partner_id': move.commercial_partner_id.id,
-                    'product_id': line.product_id.id,
-                    'product_uom_id': line.product_uom_id.id,
-                    'quantity': line.quantity,
-                    'price_unit': price_unit,
-                    'debit': balance < 0.0 and -balance or 0.0,
-                    'credit': balance > 0.0 and balance or 0.0,
-                    'account_id': debit_interim_account.id,
-                    'exclude_from_invoice_tab': True,
-                    'is_anglo_saxon_line': True,
-                })
-
-                # Add expense account line.
-                lines_vals_list.append({
-                    'name': line.name[:64],
-                    'move_id': move.id,
-                    'partner_id': move.commercial_partner_id.id,
-                    'product_id': line.product_id.id,
-                    'product_uom_id': line.product_uom_id.id,
-                    'quantity': line.quantity,
-                    'price_unit': -price_unit,
-                    'debit': balance > 0.0 and balance or 0.0,
-                    'credit': balance < 0.0 and -balance or 0.0,
-                    'account_id': credit_expense_account.id,
-                    'analytic_account_id': line.analytic_account_id.id,
-                    'analytic_tag_ids': [(6, 0, line.analytic_tag_ids.ids)],
-                    'exclude_from_invoice_tab': True,
-                    'is_anglo_saxon_line': True,
-                })
         return lines_vals_list
 
     def _stock_account_get_last_step_stock_moves(self):
@@ -213,6 +184,48 @@ class AccountMove(models.Model):
 
     def _get_invoiced_lot_values(self):
         return []
+
+    def _prepare_interim_account_line_vals(self, line, move, debit_interim_account):
+        # Compute accounting fields.
+        sign = -1 if move.move_type == "out_refund" else 1
+        price_unit = line._stock_account_get_anglo_saxon_price_unit()
+        balance = sign * line.quantity * price_unit
+        return {
+            "name": line.name[:64],
+            "move_id": move.id,
+            "partner_id": move.commercial_partner_id.id,
+            "product_id": line.product_id.id,
+            "product_uom_id": line.product_uom_id.id,
+            "quantity": line.quantity,
+            "price_unit": price_unit,
+            "debit": balance < 0.0 and -balance or 0.0,
+            "credit": balance > 0.0 and balance or 0.0,
+            "account_id": debit_interim_account.id,
+            "exclude_from_invoice_tab": True,
+            "is_anglo_saxon_line": True,
+        }
+
+    def _prepare_expense_account_line_vals(self, line, move, credit_expense_account):
+        # Compute accounting fields.
+        sign = -1 if move.move_type == "out_refund" else 1
+        price_unit = line._stock_account_get_anglo_saxon_price_unit()
+        balance = sign * line.quantity * price_unit
+        return {
+            "name": line.name[:64],
+            "move_id": move.id,
+            "partner_id": move.commercial_partner_id.id,
+            "product_id": line.product_id.id,
+            "product_uom_id": line.product_uom_id.id,
+            "quantity": line.quantity,
+            "price_unit": -price_unit,
+            "debit": balance > 0.0 and balance or 0.0,
+            "credit": balance < 0.0 and -balance or 0.0,
+            "account_id": credit_expense_account.id,
+            "analytic_account_id": line.analytic_account_id.id,
+            "analytic_tag_ids": [(6, 0, line.analytic_tag_ids.ids)],
+            "exclude_from_invoice_tab": True,
+            "is_anglo_saxon_line": True,
+        }
 
 
 class AccountMoveLine(models.Model):
