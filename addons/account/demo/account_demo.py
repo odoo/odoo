odoo/odoo

commit 4b23d11184be9bac5ef9790cc7f38bc4d637b47a

[FIX] account: improve demo data
Improve the demo data for bank statement in account by formatting the
payment_ref amounts using the company currency, and fixing the due
amount in one payment_ref to match the statement line amount.

Task id #2855485

closes odoo/odoo#97289

Signed-off-by: Laurent Smet <las@odoo.com>

addons/account/demo/account_demo.py
@@ -6,7 +6,7 @@ from dateutil.relativedelta import relativedelta
 
 from odoo import api, fields, models, Command
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools.misc import file_open
+from odoo.tools.misc import file_open, formatLang
 
 _logger = logging.getLogger(__name__)
 
@@ -132,7 +132,7 @@ class AccountChartTemplate(models.Model):
                         'partner_id': ref('base.res_partner_12').id
                     }),
                     Command.create({
-                        'payment_ref': time.strftime('First 2000 $ of invoice %Y/00001'),
+                        'payment_ref': time.strftime(f'First {formatLang(self.env, 2000, currency_obj=self.env.company.currency_id)} of invoice %Y/00001'),
                         'amount': 2000,
                         'date': time.strftime('%Y-01-01'),
                         'partner_id': ref('base.res_partner_12').id
@@ -149,10 +149,9 @@ class AccountChartTemplate(models.Model):
                         'partner_id': ref('base.res_partner_2').id
                     }),
                     Command.create({
-                        'payment_ref': 'R:9772938  10/07 AX 9415116318 T:5 BRT: 100,00€ C/ croip',
+                        'payment_ref': f'R:9772938  10/07 AX 9415116318 T:5 BRT: {formatLang(self.env, 96.67, currency_obj=self.env.company.currency_id)} C/ croip',
                         'amount': 96.67,
                         'date': time.strftime('%Y-01-01'),
-                        'partner_id': ref('base.res_partner_2').id
                     }),
                 ]
             },

commit 2dadae5f2d9c6490f0ced23258adf0291e3f10a4

[FIX] account: Fix demo data for matching rules
Fix a bug introduced by:
https://github.com/odoo/odoo/commit/4b23d11184be9bac5ef9790cc7f38bc4d637b47a

Since this commit, the matching rules's regex "BRT: ([\d,]+)" that was matching "BRT: 100,00" is no longer matching "BRT: $ 96.67".

addons/account/demo/account_demo.py
@@ -149,7 +149,7 @@ class AccountChartTemplate(models.Model):
                         'partner_id': ref('base.res_partner_2').id
                     }),
                     Command.create({
-                        'payment_ref': f'R:9772938  10/07 AX 9415116318 T:5 BRT: {formatLang(self.env, 96.67, currency_obj=self.env.company.currency_id)} C/ croip',
+                        'payment_ref': f'R:9772938  10/07 AX 9415116318 T:5 BRT: {formatLang(self.env, 100.0, digits=2)} C/ croip',
                         'amount': 96.67,
                         'date': time.strftime('%Y-01-01'),
                     }),
@@ -166,7 +166,6 @@ class AccountChartTemplate(models.Model):
                 'rule_type': 'writeoff_suggestion',
                 'match_label': 'contains',
                 'match_label_param': 'BRT',
-                'decimal_separator': ',',
                 'line_ids': [
                     Command.create({
                         'label': 'Due amount',
@@ -176,7 +175,7 @@ class AccountChartTemplate(models.Model):
                             self.env.company,
                         ).id,
                         'amount_type': 'regex',
-                        'amount_string': r'BRT: ([\d,]+)',
+                        'amount_string': r'BRT: ([\d,.]+)',
                     }),
                     Command.create({
                         'label': 'Bank Fees',