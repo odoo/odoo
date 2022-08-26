26918eff92d43d3082b02af21b1d856adca89a80
William Braeckman <wbr@odoo.com>
2022-08-26 14:08:45 +0000

    [IMP] website_sale_loyalty: make it possible to remove an auto reward
    Prior to this commit there was no way of removing an automatically
    claimed reward from a website order, since it would always get claimed
    right after the update to the cart.
    
    TaskId-2951413
    
    closes odoo/odoo#99023
    
    Related: odoo/enterprise#30794
    Related: odoo/upgrade#3820
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- addons/website_sale_loyalty/controllers/main.py
+++ addons/website_sale_loyalty/controllers/main.py
@@ -87,3 +87,13 @@ class WebsiteSale(main.WebsiteSale):
             order._apply_program_reward(reward, coupon)
         except UserError as e:
             request.session['error_promo_code'] = str(e)
+
+    @http.route()
+    def cart_update_json(self, *args, set_qty=None, **kwargs):
+        # When a reward line is deleted we remove it from the auto claimable rewards
+        if set_qty == 0:
+            request.update_context(website_sale_loyalty_delete=True)
+            # We need to update the website since `get_sale_order` is called on the website
+            # and does not follow the request's context
+            request.website = request.website.with_context(website_sale_loyalty_delete=True)
+        return super().cart_update_json(*args, set_qty=set_qty, **kwargs)
