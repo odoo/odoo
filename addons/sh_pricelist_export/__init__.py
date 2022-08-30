a3858ce95aae34376ca720197dec650830aa6847
JUAN PABLO YAÑEZ CHAPITAL <compraschapital@hotmail.com>
2022-08-29 20:57:46 -0500

    Delete addons/sh_pricelist_export directory

--- addons/sh_pricelist_export/__init__.py
+++
@@ -1,3 +0,0 @@
-# Part of Softhealer Technologies.
-
-from . import models
--- addons/sh_pricelist_export/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Part of Softhealer Technologies.
-
-from . import sh_customer_pricelist
-from . import sh_customer_details_report
