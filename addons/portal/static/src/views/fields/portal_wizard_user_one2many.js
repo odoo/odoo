2937df566ed945ae10046272ade1675ec2defe78
Noe Antoine <nan@odoo.com>
2022-08-31 11:48:18 +0000

    [FIX] portal: allocate Renderer correctly on new x2many field
    The new structure of X2manyField stores ListRenderer in its components.
    PortalUserX2ManyField is not using that structure properly. Therefore,
    PortalWizardUserListRenderer it is not found nor used correctly.
    This commit correctly places that List Renderer in the components.
    
    Task-2968411
    
    closes odoo/odoo#99320
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/portal/static/src/views/fields/portal_wizard_user_one2many.js
+++ addons/portal/static/src/views/fields/portal_wizard_user_one2many.js
@@ -4,11 +4,10 @@ import { PortalWizardUserListRenderer } from "../list/portal_wizard_user_list_re
 import { X2ManyField } from "@web/views/fields/x2many/x2many_field";
 import { registry } from "@web/core/registry";
 
-export class PortalUserX2ManyField extends X2ManyField {
-    setup() {
-        super.setup();
-        this.Renderer = PortalWizardUserListRenderer;
-    }
-}
+export class PortalUserX2ManyField extends X2ManyField {}
+PortalUserX2ManyField.components = {
+    ...X2ManyField.components,
+    ListRenderer: PortalWizardUserListRenderer,
+};
 
 registry.category("fields").add("portal_wizard_user_one2many", PortalUserX2ManyField);
