6c547ff3586a4448d1c60527c9fb72cd5e30aa1e
Stanislas Gueniffey <stgu@odoo.com>
2022-09-01 13:46:26 +0200

    [FIX] l10n_ec: demo data document types
    The document type definition was loaded too late
    This resulted in missing references in AccountChartTemplate's method
    _get_demo_data_move
    
    Changing the order fixes the missing references
    
    X-original-commit: c04be2b85674042c6d255d1b1ace5f0128f0a943

--- addons/l10n_ec/__manifest__.py
+++ addons/l10n_ec/__manifest__.py
@@ -64,11 +64,13 @@ Master Data:
         "data/res_partner_data.xml",
         # Other data
         "data/l10n_latam.document.type.csv",
+        "data/account_chart_template_configure_data.xml",
         "data/l10n_ec.sri.payment.csv",
         "views/account_tax_view.xml",
         "views/l10n_latam_document_type_view.xml",
         "views/l10n_ec_sri_payment.xml",
         "views/account_journal_view.xml",
+        # Security
         "security/ir.model.access.csv",
         # Try loading CoA
         "data/account_chart_template_configure_data.xml",
