d3d092c597dadcf97142b9348ad951874f3247be
Victor Feyens <vfe@odoo.com>
2022-09-01 11:13:06 +0000

    [REF] payment_custom, *: distinguish custom payment modes
    This commit adds a `custom_mode` field to payment acquirers to allow
    distinguishing the different payment modes made possible through the
    `payment_custom` module: Wire Transfer, Cash on Delivery, On-site
    picking. This avoids making direct references to the modules' data when
    trying to figure out which acquirer is which, hence allowing duplicating
    the acquirers in multi-company databases.
    
    task-2853489
    
    closes odoo/odoo#99400
    
    Related: odoo/upgrade#3847
    Related: odoo/enterprise#30966
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/website_sale_picking/models/payment_acquirer.py
+++ addons/website_sale_picking/models/payment_acquirer.py
@@ -1,32 +1,45 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PaymentAcquirer(models.Model):
     _inherit = 'payment.acquirer'
 
-    is_onsite_acquirer = fields.Boolean('Is Onsite Acquirer', default=False)
+    custom_mode = fields.Selection(
+        selection_add=[('onsite', "On Site")]
+    )
 
+    @api.model
     def _get_compatible_acquirers(self, *args, sale_order_id=None, website_id=None, **kwargs):
-        compatible_acquirers = super()._get_compatible_acquirers(*args, sale_order_id=sale_order_id, website_id=website_id, **kwargs)
+        """ Override of payment to exclude onsite acquirers if the delivery doesn't match.
+
+        :param int sale_order_id: The sale order to be paid, if any, as a `sale.order` id
+        :param int website_id: The provided website, as a `website` id
+        :return: The compatible acquirers
+        :rtype: recordset of `payment.acquirer`
+        """
+        compatible_acquirers = super()._get_compatible_acquirers(
+            *args, sale_order_id=sale_order_id, website_id=website_id, **kwargs)
         # Show on site picking only if delivery carriers onsite exists
-        onsite_carriers = self.env['delivery.carrier'].search(
-            ['&',
-                ('website_published', '=', True),
-                ('delivery_type', '=', 'onsite'),
-             '|',
+        onsite_carriers = self.env['delivery.carrier'].search([
+            ('website_published', '=', True),
+            ('delivery_type', '=', 'onsite'),
+            '|',
                 ('website_id', '=?', website_id),
                 ('website_id', '=', False)
-             ])
-        if not onsite_carriers:
-            compatible_acquirers -= self.env.ref('website_sale_picking.payment_acquirer_onsite')
-
-        # FIXME MULTI COMPANY !!!
-        # Show onsite payment only if it is a physical product
+        ])
         order = self.env['sale.order'].browse(sale_order_id).exists()
-        if not any(product.type in ['consu', 'product'] for product in order.order_line.product_id):
-            compatible_acquirers -= self.env.ref('website_sale_picking.payment_acquirer_onsite')
+
+        # Show onsite acquirers only if onsite carriers exists
+        # and the order contains physical products
+        if not onsite_carriers or not any(
+            product.type in ('consu', 'product')
+            for product in order.order_line.product_id
+        ):
+            compatible_acquirers.filtered(
+                lambda acq: acq.provider != 'custom' or acq.custom_mode != 'onsite'
+            )
 
         return compatible_acquirers
