66b6c02256039a66d17b42658bdfef1b5fdd1436
Kartik Chavda <kcv@odoo.com>
2022-09-01 12:08:26 +0000

    [IMP] {hr,sale}_timesheet, _*: make separate method for access
    _* = project_timesheet_holidays
    
    Before this commit checking validation for creating and writing
    on timesheet done in create and write method which makes
    customization difficult for user.
    
    So, in this commit check validation for creating and writing on
    timesheet done in separate method _check_can_create and
    _check_can_write respectively for timesheet.
    
    task-2928942
    
    closes odoo/odoo#100943
    
    Related: odoo/enterprise#31689
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project_timesheet_holidays/models/account_analytic.py
+++ addons/project_timesheet_holidays/models/account_analytic.py
@@ -36,16 +36,12 @@ class AccountAnalyticLine(models.Model):
             action = self._get_redirect_action()
             raise RedirectWarning(warning_msg, action, _('View Time Off'))
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        if not self.env.su:
-            task_ids = [vals['task_id'] for vals in vals_list if vals.get('task_id')]
-            has_timeoff_task = self.env['project.task'].search_count([('id', 'in', task_ids), ('is_timeoff_task', '=', True)], limit=1) > 0
-            if has_timeoff_task:
-                raise UserError(_('You cannot create timesheets for a task that is linked to a time off type. Please use the Time Off application to request new time off instead.'))
-        return super().create(vals_list)
-
-    def write(self, vals):
-        if not self.env.su and self.filtered('holiday_id'):
+    def _check_can_write(self, values):
+        if not self.env.su and self.holiday_id:
             raise UserError(_('You cannot modify timesheets that are linked to time off requests. Please use the Time Off application to modify your time off requests instead.'))
-        return super().write(vals)
+        return super()._check_can_write(values)
+
+    def _check_can_create(self):
+        if not self.env.su and any(task.is_timeoff_task for task in self.task_id):
+            raise UserError(_('You cannot create timesheets for a task that is linked to a time off type. Please use the Time Off application to request new time off instead.'))
+        return  super()._check_can_create()
