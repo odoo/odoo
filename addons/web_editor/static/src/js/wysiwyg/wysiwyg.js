2c41515409a23fbf07854e2952f183a1f2fc9681
Sébastien Geelen (sge) <sge@odoo.com>
2022-09-02 12:06:59 +0000

    [FIX] web_editor: traceback selection null in FF
    In Firefox only :
    In some case (editor in page but not visible) the
    `document.getSelection` sometimes return `null` instead
    of an empty selection object. We add some check to ensure
    we avoid unnecessary traceback.
    
    task-2971126
    
    closes odoo/odoo#99485
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1680,7 +1680,7 @@ const Wysiwyg = Widget.extend({
         this.toolbar.$el.find('#mediaParagraphDropdownButton').attr('id', 'paragraphDropdownButton');
         // Hide the create-link button if the selection spans several blocks.
         const selection = this.odooEditor.document.getSelection();
-        const range = selection.rangeCount && selection.getRangeAt(0);
+        const range = selection && selection.rangeCount && selection.getRangeAt(0);
         const $rangeContainer = range && $(range.commonAncestorContainer);
         const spansBlocks = range && !!$rangeContainer.contents().filter((i, node) => isBlock(node)).length;
         this.toolbar.$el.find('#create-link').toggleClass('d-none', !range || spansBlocks);
