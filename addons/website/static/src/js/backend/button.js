cbd833ed144bfa47f5503fb3d7ce0b26840492db
Arthur Detroux (ard) <ard@odoo.com>
2022-09-02 11:56:31 +0200

    [IMP] website: adapt website stat buttons to OWL
    This commit adapts the website_published button and the settings field
    "website cookie bar" to OWL.
    
    Note: The website_redirect_button was adapted by [1] but the legacy
    redirect button is still needed by a single partner view.
    
    [1]: https://github.com/odoo/odoo/commit/9434969d5dc06aadcdbbfc600514a0bdb5635ea2

--- addons/website/static/src/js/backend/button.js
+++ addons/website/static/src/js/backend/button.js
@@ -7,46 +7,7 @@ var field_registry = require('web.field_registry');
 
 var _t = core._t;
 
-var WebsitePublishButton = AbstractField.extend({
-    className: 'o_stat_info',
-    supportedFieldTypes: ['boolean'],
-
-    //--------------------------------------------------------------------------
-    // Public
-    //--------------------------------------------------------------------------
-
-    /**
-     * A boolean field is always set since false is a valid value.
-     *
-     * @override
-     */
-    isSet: function () {
-        return true;
-    },
-
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    /**
-     * This widget is supposed to be used inside a stat button and, as such, is
-     * rendered the same way in edit and readonly mode.
-     *
-     * @override
-     * @private
-     */
-    _render: function () {
-        this.$el.empty();
-        var text = this.value ? _t("Published") : _t("Unpublished");
-        var hover = this.value ? _t("Unpublish") : _t("Publish");
-        var valColor = this.value ? 'text-success' : 'text-danger';
-        var hoverColor = this.value ? 'text-danger' : 'text-success';
-        var $val = $('<span>').addClass('o_stat_text o_not_hover ' + valColor).text(text);
-        var $hover = $('<span>').addClass('o_stat_text o_hover ' + hoverColor).text(hover);
-        this.$el.append($val).append($hover);
-    },
-});
-
+// TODO: Remove me when Partner view is adapted to OWL
 var WidgetWebsiteButtonIcon = AbstractField.extend({
     template: 'WidgetWebsiteButtonIcon',
     events: {
@@ -111,6 +72,5 @@ var WidgetWebsiteButtonIcon = AbstractField.extend({
 });
 
 field_registry
-    .add('website_redirect_button', WidgetWebsiteButtonIcon)
-    .add('website_publish_button', WebsitePublishButton);
+    .add('website_redirect_button', WidgetWebsiteButtonIcon);
 });
