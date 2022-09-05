3b9c520f666f6ec5e35a9ebd0683446fe9e3fe1a
Julien Castiaux <juc@odoo.com>
2022-09-05 15:31:19 +0200

    [FIX] website_links: unused imports in tests
    Fine tuning of e8a1b4c0cdf

--- addons/website_links/tests/test_controller.py
+++ addons/website_links/tests/test_controller.py
@@ -1,7 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import secrets
-from unittest.mock import patch
 from odoo.tests import tagged
 from odoo.tests.common import HttpCase
 
