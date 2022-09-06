4b850b1c2d596eab87f8991c2644ed7cf0b70795
Laurent Smet <las@odoo.com>
2022-09-06 13:39:40 +0200

    [FIX] account: Fix ensure_one method called on 'self' instead of 'st_line'
    X-original-commit: f95000096c848aabb1990237390a659ed739f491

--- addons/account/models/account_bank_statement.py
+++ addons/account/models/account_bank_statement.py
@@ -1016,7 +1016,7 @@ class AccountBankStatementLine(models.Model):
             company_currency = st_line.journal_id.company_id.currency_id
             journal_currency = st_line.journal_id.currency_id if st_line.journal_id.currency_id != company_currency else False
 
-            line_vals_list = self._prepare_move_line_default_vals()
+            line_vals_list = st_line._prepare_move_line_default_vals()
             line_ids_commands = [(1, liquidity_lines.id, line_vals_list[0])]
 
             if suspense_lines:
