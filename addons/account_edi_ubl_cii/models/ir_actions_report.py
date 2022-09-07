862998cdfba31abc6644fba6a830d5930c39cef8
Julien Van Roy <juvr@odoo.com>
2022-09-07 09:39:49 +0000

    [IMP] account_edi_ubl_cii: always generate Factur-X silently
    Always generate a Factur-X xml and embed it when a PDF is created. But no edi.document
    is created, no warnings will be displayed on the invoice, and the PDF is not PDF/A-3.
    The only purpose here is the Odoo interoperability.
    
    Rename the 'Factur-X' option to 'Factur-X PDF/A-3' and make it only visible for FR and DE companies.
    
    This 'Factur-X PDF/A-3' option will trigger the regular account.edi flow: create an edi.document
    and show warnings on the move if errors are detected. When a PDF is printed, embed the Factur-X xml
    and make the PDF a PDF/A-3. Thus, it can be used on Chorus Pro.
    
    task-2957808
    
    closes odoo/odoo#102311
    
    X-original-commit: 25d4635d544a0a48ac937b35c996f2cbdb8073ec
    Related: odoo/enterprise#32344
    Related: odoo/documentation#2789
    Signed-off-by: Laurent Smet <las@odoo.com>
    Signed-off-by: Julien Van Roy <juvr@odoo.com>

--- addons/account_edi_ubl_cii/models/ir_actions_report.py
+++ addons/account_edi_ubl_cii/models/ir_actions_report.py
@@ -3,10 +3,12 @@
 
 from odoo import models
 from odoo.tools import cleanup_xml_node
+from odoo.tools.pdf import OdooPdfFileReader, OdooPdfFileWriter
 
 from lxml import etree
 import base64
 from xml.sax.saxutils import escape, quoteattr
+import io
 
 
 class IrActionsReport(models.Model):
@@ -61,5 +63,40 @@ class IrActionsReport(models.Model):
             for res_id, stream_data in collected_streams.items():
                 invoice = self.env['account.move'].browse(res_id)
                 self._add_pdf_into_invoice_xml(invoice, stream_data)
+
+            # If Factur-X isn't already generated, generate and embed it inside the PDF
+            if len(res_ids) == 1:
+                invoice = self.env['account.move'].browse(res_ids)
+                edi_doc_codes = invoice.edi_document_ids.edi_format_id.mapped('code')
+                # If Factur-X hasn't been generated, generate and embed it anyway
+                if invoice.is_sale_document() \
+                        and invoice.state == 'posted' \
+                        and 'facturx_1_0_05' not in edi_doc_codes \
+                        and self.env.ref('account_edi_ubl_cii.edi_facturx_1_0_05', raise_if_not_found=False):
+                    # Add the attachments to the pdf file
+                    pdf_stream = collected_streams[invoice.id]['stream']
+
+                    # Read pdf content.
+                    pdf_content = pdf_stream.getvalue()
+                    reader_buffer = io.BytesIO(pdf_content)
+                    reader = OdooPdfFileReader(reader_buffer, strict=False)
+
+                    # Post-process and embed the additional files.
+                    writer = OdooPdfFileWriter()
+                    writer.cloneReaderDocumentRoot(reader)
+
+                    # Generate and embed Factur-X
+                    xml_content, _errors = self.env['account.edi.xml.cii']._export_invoice(invoice)
+                    writer.addAttachment(
+                        name=self.env['account.edi.xml.cii']._export_invoice_filename(invoice),
+                        data=xml_content,
+                        subtype='text/xml',
+                    )
+
+                    # Replace the current content.
+                    pdf_stream.close()
+                    new_pdf_stream = io.BytesIO()
+                    writer.write(new_pdf_stream)
+                    collected_streams[invoice.id]['stream'] = new_pdf_stream
 
         return collected_streams
