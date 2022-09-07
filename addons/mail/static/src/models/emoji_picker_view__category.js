f48479e7ee9891598f6f1a322c1f8ce1da8c9c3a
Alexandre Kühn <aku@odoo.com>
2022-09-07 15:35:29 +0000

    [FIX] mail: remove manual .update() in compute of emoji grid rows
    Task-2976694
    
    closes odoo/odoo#99771
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/models/emoji_picker_view__category.js
+++ addons/mail/static/src/models/emoji_picker_view__category.js
@@ -2,6 +2,7 @@
 
 import { registerModel } from '@mail/model/model_core';
 import { attr, one } from '@mail/model/model_field';
+import { clear } from '@mail/model/model_field_command';
 
 /**
  * Emoji category info of a specific emoji picker view
@@ -27,8 +28,34 @@ registerModel({
         emojiGridRowView: one('EmojiGridRowView', {
             inverse: 'viewCategory',
         }),
+        emojiPickerViewOwnerAsLastCategory: one('EmojiPickerView', {
+            compute() {
+                if (this.emojiPickerViewOwner.categories[this.emojiPickerViewOwner.categories.length - 1] === this) {
+                    return this.emojiPickerViewOwner;
+                }
+                return clear();
+            },
+        }),
         endSectionIndex: attr({
+            compute() {
+                if (!this.nextViewCategory || !this.nextViewCategory.emojiGridRowView) {
+                    return clear();
+                }
+                return this.nextViewCategory.emojiGridRowView.index - 1;
+            },
             default: 0,
+        }),
+        nextViewCategory: one('EmojiPickerView.Category', {
+            compute() {
+                const index = this.emojiPickerViewOwner.categories.findIndex(category => category === this);
+                if (index === -1) {
+                    return clear();
+                }
+                if (index === this.emojiPickerViewOwner.categories.length - 1) {
+                    return clear();
+                }
+                return this.emojiPickerViewOwner.categories[index + 1];
+            },
         }),
     },
 });
