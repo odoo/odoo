67aaa7166079d6fac7c655428455c4c3e294b116
Lucas Lefèvre <lul@odoo.com>
2022-09-07 10:09:21 +0200

    [ADD] spreadsheet_dashboard_account: add finance dashboards
    Co-authored-by: Nathan Grognet <ngr@odoo.com>

---
+++ addons/spreadsheet_dashboard_account/static/tests/tours/accounting_dashboard_tours.js
@@ -0,0 +1,7 @@
+/** @odoo-module */
+
+import { registerDashboardTour } from "@spreadsheet_dashboard/../tests/utils/tour";
+
+registerDashboardTour("spreadsheet_dashboard_accounting", "Accounting");
+registerDashboardTour("spreadsheet_dashboard_invoicing", "Invoicing");
+registerDashboardTour("spreadsheet_dashboard_benchmark", "Benchmark");
---
+++ addons/spreadsheet_dashboard_account/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_accounting_dashboard_tour
