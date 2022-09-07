44cfa0358737325edbabb9edb36cca503a024e0f
William Braeckman <wbr@odoo.com>
2022-09-07 14:17:16 +0000

    [IMP] mrp: migrate mrp_documents_kanban to wowl2
    This views has multiple custom features such as file upload, attachment
    viewer and download.
    A lot of files have been rendered obsolete and have been deleted,
    notably `file_upload_mixin` which has been replaced by the
    file_upload service and hooks.
    
    closes odoo/odoo#99769
    
    Related: odoo/enterprise#31145
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ addons/web/static/src/core/file_upload/file_upload_progress_record.js
@@ -0,0 +1,35 @@
+/** @odoo-module **/
+
+import { sprintf } from "../utils/strings";
+import { FileUploadProgressBar } from "./file_upload_progress_bar";
+
+const { Component } = owl;
+
+export class FileUploadProgressRecord extends Component {
+    getProgressTexts() {
+        const fileUpload = this.props.fileUpload;
+        const percent = Math.round(fileUpload.progress * 100);
+        if (percent === 100) {
+            return {
+                left: this.env._t("Processing..."),
+                right: "",
+            };
+        } else {
+            const mbLoaded = Math.round(fileUpload.loaded / 1000000);
+            const mbTotal = Math.round(fileUpload.total / 1000000);
+            return {
+                left: sprintf(this.env._t("Uploading... (%s%)"), percent),
+                right: sprintf(this.env._t("(%s/%sMB)"), mbLoaded, mbTotal),
+            };
+        }
+    }
+}
+FileUploadProgressRecord.components = {
+    FileUploadProgressBar,
+};
+
+export class FileUploadProgressKanbanRecord extends FileUploadProgressRecord {}
+FileUploadProgressKanbanRecord.template = "web.FileUploadProgressKanbanRecord";
+
+export class FileUploadProgressDataRow extends FileUploadProgressRecord {}
+FileUploadProgressDataRow.template = "web.FileUploadProgressDataRow";
