cb50eed639698c25482f1f633853d7d456a6d669
Tiffany Chang (tic) <tic@odoo.com>
2022-09-07 14:53:43 +0200

    [IMP] purchase_requsition: convert custom js to OWL
    For original specs, see [1].
    
    Some performance changes: the best price/date/unit price fields are now
    loaded by JS instead of passed via action context (allows for easier/
    consistent reuse of view + now we can refresh without undoing any state
    changes due to buttons being pushed).
    
    [1] https://github.com/odoo/odoo/commit/f533e40f0e3f1cd34f1047b70dfdf95837d84501

--- addons/purchase_requisition/__manifest__.py
+++ addons/purchase_requisition/__manifest__.py
@@ -27,12 +27,11 @@ are agreements you have with vendors to benefit from a predetermined pricing.
     ],
     'assets': {
         'web.assets_backend': [
-            'purchase_requisition/static/src/js/purchase_order_alternatives_widget.js',
-            'purchase_requisition/static/src/js/purchase_order_compare_list_renderer.js',
-            'purchase_requisition/static/src/js/purchase_order_line_compare_list_controller.js',
-            'purchase_requisition/static/src/js/purchase_order_line_compare_list_renderer.js',
-            'purchase_requisition/static/src/js/purchase_order_line_compare_list_view.js',
-            'purchase_requisition/static/src/xml/purchase_order_line_compare.xml',
+            'purchase_requisition/static/src/*/**.js',
+            'purchase_requisition/static/src/views/*/**.js',
+        ],
+        'web.assets_qweb': [
+            'purchase_requisition/static/src/*/**.xml',
         ],
     },
     'license': 'LGPL-3',
