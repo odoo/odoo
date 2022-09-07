dab4b67b12a5bdde329dbf314b44126ee810ba58
Philippe Wauthy <phwa@odoo.com>
2022-09-07 11:20:14 +0200

    [FIX] hr_holidays: delete calendar event when time off is deleted
    Since https://github.com/odoo/odoo/pull/76722, it is possible to cancel an approved time off.
    However, the calendar event created when the leave is validated is not archived when the leave is cancelled.
    
    This commit archives the calendar event when the leave is cancelled.
    
    task-2965754

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -1308,6 +1308,7 @@ class HolidaysRequest(models.Model):
 
         leave_sudo = self.sudo()
         leave_sudo.with_context(from_cancel_wizard=True).active = False
+        leave_sudo.meeting_id.active = False
         leave_sudo._remove_resource_leave()
 
     def action_documents(self):
