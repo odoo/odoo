c623ea66863a98085e9d7c82aa9384dd4d1fa182
Lucas Lefèvre <lul@odoo.com>
2022-09-09 06:59:01 +0000

    [ADD] spreadsheet_dashboard_sale: add sales dashboard
    Part-of: odoo/odoo#97606
    Co-authored-by: Nathan Grognet <ngr@odoo.com>

---
+++ addons/spreadsheet_dashboard_sale/static/tests/tours/sale_dashboard_tours.js
@@ -0,0 +1,6 @@
+/** @odoo-module */
+
+import { registerDashboardTour } from "@spreadsheet_dashboard/../tests/utils/tour";
+
+registerDashboardTour("spreadsheet_dashboard_sales", "Sales");
+registerDashboardTour("spreadsheet_dashboard_product", "Product");
