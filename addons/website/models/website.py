59cf789a671e4fad8d315eb50b5f7eddbc9d44b9
Romain Derie <rde@odoo.com>
2022-09-09 18:04:11 +0200

    [FIX] website: remove confusing comment
    The comment was introduced alongside its related line with [1].
    The line was removed with [2] but its comment was not, leading to
    confusion now.
    Instead of removing it in master, might as well remove the confusion in
    lower version..
    
    [1]: https://github.com/odoo/odoo/commit/3542a27f9c4c44472184bbedc348ad646d46f20a
    [2]: https://github.com/odoo/odoo/commit/0effbe3ca628460b9a0c257ea0bd6060c3eeb253
    
    X-original-commit: 7258c82a46713f404df5cf1bdfa0e29975011fcf

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -1140,7 +1140,6 @@ class Website(models.Model):
             :rtype: list({name: str, url: str})
         """
         router = http.root.get_db_router(request.db)
-        # Force enumeration to be performed as public user
         url_set = set()
 
         sitemap_endpoint_done = set()
