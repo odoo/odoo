e8a8a661e0b16515ba44044e55de804f45f0f254
Sébastien Geelen (sge) <sge@odoo.com>
2022-09-09 14:03:39 +0200

    [FIX] web_editor,website: top menu edition
    Ensure all the `#top_menu` links are unremovable.
    This prevent the editor to merge two `<a>` elements together
    during delete commands.
    
    task-2967314

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -201,6 +201,7 @@ export class OdooEditor extends EventTarget {
                 showEmptyElementHint: true,
                 defaultLinkAttributes: {},
                 plugins: [],
+                getUnremovableElements: () => [],
                 getReadOnlyAreas: () => [],
                 getContentEditableAreas: () => [],
                 getPowerboxElement: () => {
@@ -1658,6 +1659,9 @@ export class OdooEditor extends EventTarget {
         }
         for (const node of this.options.getReadOnlyAreas()) {
             node.setAttribute('contenteditable', false);
+        }
+        for (const element of this.options.getUnremovableElements()) {
+            element.classList.add("oe_unremovable");
         }
         this.observerActive('_activateContenteditable');
     }
