ffab0570de130a72800b3ec5bb14baa7682fa482
Dossogne Bertrand <bedo@odoo.com>
2022-09-10 09:26:50 +0200

    [IMP] website_hr_recruitment: fix job apply page colors
    On the application page, too much elements are set
    in the primary color, creating confusion as of where is
    located the main page action.
    
    Also changes the phone field in the job application page
    to be stored as a mobile phone instead of a home phone.
    
    task-2980889

--- addons/website_hr_recruitment/static/src/js/website_hr_recruitment_editor.js
+++ addons/website_hr_recruitment/static/src/js/website_hr_recruitment_editor.js
@@ -23,7 +23,7 @@ FormEditorRegistry.add('apply_job', {
         type: 'char',
         required: true,
         fillWith: 'phone',
-        name: 'partner_phone',
+        name: 'partner_mobile',
         string: 'Phone Number',
     }, {
         type: 'char',
--- addons/website_hr_recruitment/tests/test_website_hr_recruitment.py
+++ addons/website_hr_recruitment/tests/test_website_hr_recruitment.py
@@ -27,13 +27,13 @@ class TestWebsiteHrRecruitmentForm(odoo.tests.HttpCase):
         guru_applicant = capt.records[1]
         self.assertEqual(guru_applicant.partner_name, 'John Smith')
         self.assertEqual(guru_applicant.email_from, 'john@smith.com')
-        self.assertEqual(guru_applicant.partner_phone, '118.218')
+        self.assertEqual(guru_applicant.partner_mobile, '118.218')
         self.assertEqual(html2plaintext(guru_applicant.description), '### [GURU] HR RECRUITMENT TEST DATA ###')
         self.assertEqual(guru_applicant.job_id, job_guru)
 
         internship_applicant = capt.records[0]
         self.assertEqual(internship_applicant.partner_name, 'Jack Doe')
         self.assertEqual(internship_applicant.email_from, 'jack@doe.com')
-        self.assertEqual(internship_applicant.partner_phone, '118.712')
+        self.assertEqual(internship_applicant.partner_mobile, '118.712')
         self.assertEqual(html2plaintext(internship_applicant.description), '### HR [INTERN] RECRUITMENT TEST DATA ###')
         self.assertEqual(internship_applicant.job_id, job_intern)
