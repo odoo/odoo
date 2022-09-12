2346881014f002897cf92106eef966c7f8f0b1cc
William Braeckman <wbr@odoo.com>
2022-09-12 08:30:07 +0000

    [FIX] website_sale_picking: fix access error as public user
    When selecting on site picking and/or onsite payment the public user was
    presented with an access error while it shouldn't.
    
    closes odoo/odoo#100001
    
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- addons/website_sale_picking/controllers/main.py
+++ addons/website_sale_picking/controllers/main.py
@@ -15,7 +15,7 @@ class PaymentPortalOnsite(PaymentPortal):
         Also sets the sale order's warehouse id to the carrier's if it exists
         """
         super()._validate_transaction_for_order(transaction, sale_order_id)
-        sale_order = request.env['sale.order'].browse(sale_order_id).exists()
+        sale_order = request.env['sale.order'].browse(sale_order_id).exists().sudo()
 
         # This should never be triggered unless the user intentionally forges a request.
         if sale_order.carrier_id.delivery_type != 'onsite' and (
