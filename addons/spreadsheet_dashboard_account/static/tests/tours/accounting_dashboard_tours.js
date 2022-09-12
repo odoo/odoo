a79fb8dacbc238c83bc4451d26058fbb7ca78035
Lucas Lefèvre <lul@odoo.com>
2022-09-12 09:39:02 +0200

    [MOV] spreadsheet_dashboard_account: move dashboard to account_accountant

--- addons/spreadsheet_dashboard_account/static/tests/tours/accounting_dashboard_tours.js
+++ addons/spreadsheet_dashboard_account/static/tests/tours/accounting_dashboard_tours.js
@@ -2,6 +2,4 @@
 
 import { registerDashboardTour } from "@spreadsheet_dashboard/../tests/utils/tour";
 
-registerDashboardTour("spreadsheet_dashboard_accounting", "Accounting");
 registerDashboardTour("spreadsheet_dashboard_invoicing", "Invoicing");
-registerDashboardTour("spreadsheet_dashboard_benchmark", "Benchmark");
