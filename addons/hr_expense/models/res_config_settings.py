1f8e265e43626b7c40e00d7b3280ab9d078aa52f
Damien Bouvy <dbo@odoo.com>
2022-09-12 08:31:59 +0000

    [IMP] hr_expense: allow default for the company journal
    Revision 26a5653f48016d20f4bf050051a0844a760e4470 introduced
    the possibility to set up a default journal for accounting
    entries created by "employee-paid" expenses
    
    This commit adds the same possibility for the "company-paid"
    expenses (because why allow it for one and not the other?).
    
    closes odoo/odoo#99993
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_expense/models/res_config_settings.py
+++ addons/hr_expense/models/res_config_settings.py
@@ -17,6 +17,7 @@ class ResConfigSettings(models.TransientModel):
     module_hr_payroll_expense = fields.Boolean(string='Reimburse Expenses in Payslip')
     module_hr_expense_extract = fields.Boolean(string='Send bills to OCR to generate expenses')
     expense_journal_id = fields.Many2one('account.journal', related='company_id.expense_journal_id', readonly=False)
+    company_expense_journal_id = fields.Many2one('account.journal', related='company_id.company_expense_journal_id', readonly=False)
 
     @api.model
     def get_values(self):
