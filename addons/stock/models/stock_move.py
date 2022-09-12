841ebfead1f9944f50d0a651cc39b7d41d32df2d
PNO <pno@odoo.com>
2022-09-12 13:03:58 +0200

    [FIX] stock: Unwanted purchase orders
    Currently, in a 2 step incoming shipments scenario, if we create a purchase order and duplicate the transfer that goes from Input to Stock, it will get the procure_method make_to_order.
    This is a problem because an unwanted purchase order will be generated. 
    As a solution, on the procure_method field of the stock.move, the copy is set to False. This is already the case in v15.
    
    OPW-2952181

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -120,7 +120,7 @@ class StockMove(models.Model):
     procure_method = fields.Selection([
         ('make_to_stock', 'Default: Take From Stock'),
         ('make_to_order', 'Advanced: Apply Procurement Rules')], string='Supply Method',
-        default='make_to_stock', required=True,
+        default='make_to_stock', required=True, copy=False,
         help="By default, the system will take from the stock in the source location and passively wait for availability. "
              "The other possibility allows you to directly create a procurement on the source location (and thus ignore "
              "its current stock) to gather products. If we want to chain moves and have this one to wait for the previous, "
