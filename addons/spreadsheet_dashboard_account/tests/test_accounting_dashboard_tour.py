6707ca897c6ca779396402d7516939dacf74f262
Lucas Lefèvre <lul@odoo.com>
2022-09-12 07:39:02 +0000

    [MOV] spreadsheet_dashboard_account: move dashboard to account_accountant
    closes odoo/odoo#99985
    
    Related: odoo/enterprise#31221
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- addons/spreadsheet_dashboard_account/tests/test_accounting_dashboard_tour.py
+++ addons/spreadsheet_dashboard_account/tests/test_accounting_dashboard_tour.py
@@ -5,11 +5,5 @@ from odoo.tests.common import HttpCase
 @tagged('post_install', '-at_install')
 class TestAccountingDashboard(HttpCase):
 
-    def test_accounting_dashboard(self):
-        self.start_tour('/web', 'spreadsheet_dashboard_accounting', login='admin')
-
     def test_invoicing_dashboard(self):
         self.start_tour('/web', 'spreadsheet_dashboard_invoicing', login='admin')
-
-    def test_benchmark_dashboard(self):
-        self.start_tour('/web', 'spreadsheet_dashboard_benchmark', login='admin')
