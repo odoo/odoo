bf52a18cc4140fbc74ec787a5cb21560a2ad1069
Damien Bouvy <dbo@odoo.com>
2022-09-12 08:31:59 +0000

    [IMP] hr_expense: allow default for the company journal
    Revision 45c0a5952b82f0914429b7e21b1d96423a1dcd22 introduced
    the possibility to set up a default journal for accounting
    entries created by "employee-paid" expenses
    
    This commit adds the same possibility for the "company-paid"
    expenses (because why allow it for one and not the other?).
    
    closes odoo/odoo#99993
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_expense/models/res_company.py
+++ addons/hr_expense/models/res_company.py
@@ -2,8 +2,21 @@
 
 from odoo import fields, models
 
+
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    expense_journal_id = fields.Many2one('account.journal', string='Default Expense Journal', check_company=True, domain="[('type', '=', 'purchase'), ('company_id', '=', company_id)]",
-        help="The Default journal for the company used when the expense is done.")
+    expense_journal_id = fields.Many2one(
+        "account.journal",
+        string="Default Expense Journal",
+        check_company=True,
+        domain="[('type', '=', 'purchase'), ('company_id', '=', company_id)]",
+        help="The company's default journal used when an employee expense is created.",
+    )
+    company_expense_journal_id = fields.Many2one(
+        "account.journal",
+        string="Default Company Expense Journal",
+        check_company=True,
+        domain="[('type', 'in', ['cash', 'bank']), ('company_id', '=', company_id)]",
+        help="The company's default journal used when a company expense is created.",
+    )
