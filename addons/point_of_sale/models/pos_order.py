ed49bc766cf6956691ca87be50f352247828453f
Pierre Masereel <pim@odoo.com>
2022-09-13 14:29:56 +0000

    [FIX] point_of_sale: apply payment terms on invoices from POS
    When an invoice is generated from the POS there are no payment terms and
    due dates put on the invoices. It was not really an issue before the pay
    later feature, but now it is needed.
    
    closes odoo/odoo#100100
    
    Signed-off-by: Trinh Jacky (trj) <trj@odoo.com>

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -605,6 +605,7 @@ class PosOrder(models.Model):
             'invoice_date': self.date_order.astimezone(timezone).date(),
             'fiscal_position_id': self.fiscal_position_id.id,
             'invoice_line_ids': self._prepare_invoice_lines(),
+            'invoice_payment_term_id': self.partner_id.property_payment_term_id.id or False,
             'invoice_cash_rounding_id': self.config_id.rounding_method.id
             if self.config_id.cash_rounding and (not self.config_id.only_round_cash_method or any(p.payment_method_id.is_cash_count for p in self.payment_ids))
             else False
