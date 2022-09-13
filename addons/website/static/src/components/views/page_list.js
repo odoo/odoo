c7bb98f4968367951695f14cab94bbf6ac37a425
xO-Tx <mou@odoo.com>
2022-09-13 10:33:46 +0200

    [IMP] website: add publish / unpublish buttons
    The goal of this commit is to add a 'Publish' / 'Unpublish' button to
    the additional actions of the 'website content' list views.
    
    'Archive' / 'Unarchive' options are also removed for the 'website.page'
    records.
    
    task-2889981

--- addons/website/static/src/components/views/page_list.js
+++ addons/website/static/src/components/views/page_list.js
@@ -3,14 +3,60 @@
 import {PageControllerMixin, PageRendererMixin} from "./page_views_mixin";
 import {registry} from '@web/core/registry';
 import {listView} from '@web/views/list/list_view';
+import {ConfirmationDialog} from "@web/core/confirmation_dialog/confirmation_dialog";
+import {useService} from "@web/core/utils/hooks";
+import {sprintf} from "@web/core/utils/strings";
 
 
 export class PageListController extends PageControllerMixin(listView.Controller) {
     /**
      * @override
      */
+    setup() {
+        super.setup();
+        this.orm = useService('orm');
+    }
+
+    /**
+     * @override
+     */
     onClickCreate() {
         return this.createWebsiteContent();
+    }
+
+    /**
+     * Adds a "Publish/Unpublish" button to the 'action' menu of the list view.
+     *
+     * @override
+     */
+    getActionMenuItems() {
+        const actionMenuItems = super.getActionMenuItems();
+        // 'Archive' / 'Unarchive' options are disabled only on 'website.page' list view.
+        if (this.props.resModel === 'website.page') {
+            actionMenuItems.other = actionMenuItems.other
+                .filter(item => !['archive', 'unarchive'].includes(item.key));
+        }
+        actionMenuItems.other.splice(-1, 0, {
+            description: this.env._t("Publish"),
+            callback: async () => {
+                this.dialogService.add(ConfirmationDialog, {
+                    title: this.env._t("Publish Website Content"),
+                    body: sprintf(this.env._t("%s record(s) selected, are you sure you want to publish them all?"), this.model.root.selection.length),
+                    confirm: () => this.togglePublished(true),
+                });
+            }
+        },
+        {
+            description: this.env._t("Unpublish"),
+            callback: async () => this.togglePublished(false),
+        });
+        return actionMenuItems;
+    }
+
+    async togglePublished(publish) {
+        const resIds = this.model.root.selection.map(record => record.resId);
+        await this.orm.write(this.props.resModel, resIds, {is_published: publish});
+        this.actionService.switchView('list');
     }
 }
 PageListController.template = `website.PageListView`;
