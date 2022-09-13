odoo/odoo

commit db379b6838b8883ae8fef2659912854dd08cf31c

[FIX] project: redirect `/my/task/<task_id>`
Before this commit, the commit [1] renames the routes to keep the url
and add the id when the user selects a task id.
However, the redirection is missing for `/my/task/<task_id>` due to
an issue in the routes selected for the redirection.

This commit redirects the `/my/task/<task_id>` to `/my/tasks/<task_id>`
to keep the old route accessible.

[1] b517fdc19d3a3530695895757679bdafb415e6e4

closes odoo/odoo#99696

Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

addons/project/controllers/portal.py
@@ -105,10 +105,12 @@ class ProjectCustomerPortal(CustomerPortal):
         """ Redirect the outdated routes to the new routes. """
         return request.redirect(request.httprequest.full_path.replace('/my/project/', '/my/projects/'))
 
-    @http.route(['/my/task', '/my/task/page/<int:page>'], type='http', auth='public')
+    @http.route(['/my/task',
+                 '/my/task/page/<int:page>'
+                 '/my/task/<int:task_id>'], type='http', auth='public')
     def portal_my_task_routes_outdated(self, **kwargs):
         """ Redirect the outdated routes to the new routes. """
-        return request.redirect(request.httprequest.path.replace('/my/task', '/my/tasks'))
+        return request.redirect(request.httprequest.full_path.replace('/my/task', '/my/tasks'))
 
     @http.route(['/my/projects/<int:project_id>', '/my/projects/<int:project_id>/page/<int:page>'], type='http', auth="public", website=True)
     def portal_my_project(self, project_id=None, access_token=None, page=1, date_begin=None, date_end=None, sortby=None, search=None, search_in='content', groupby=None, task_id=None, **kw):

commit 2f246a3b8f48519c68d05dae05e2d0f242f4b8df

[FIX] project: fix typo in /my/task/<task_id> controller
Missing comma, introduced by db379b6838b8

addons/project/controllers/portal.py
@@ -106,7 +106,7 @@ class ProjectCustomerPortal(CustomerPortal):
         return request.redirect(request.httprequest.full_path.replace('/my/project/', '/my/projects/'))
 
     @http.route(['/my/task',
-                 '/my/task/page/<int:page>'
+                 '/my/task/page/<int:page>',
                  '/my/task/<int:task_id>'], type='http', auth='public')
     def portal_my_task_routes_outdated(self, **kwargs):
         """ Redirect the outdated routes to the new routes. """