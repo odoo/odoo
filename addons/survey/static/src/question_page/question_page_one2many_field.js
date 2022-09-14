9624ee56e4fb11a7c153867659aae5cefb9d09f6
Florian Charlier <flch@odoo.com>
2022-09-14 11:33:01 +0200

    [IMP] survey, web: enable chaining question creation
    ## Survey
    Due to the behavior of X2Many fields, the questions created when editing
    a survey were not usable immediately after creation to be conditional
    triggers for new questions. The whole survey had to be saved first, which
    wasn't a smooth experience for the user.
    
    This commit will save the survey and question at several times:
    
    ### For each new question added
    
    Similarly to what was implemented for chatbot scripts, enables the
    immediate computation of allowed question/answer triggers, permitting their
    use when chaining question creation.
    
    This also enables to check the answers "validation" fields immediately, and
    activating scoring and add scores to question/answers without requiring the
    extra steps of saving and re-entering edit mode for the survey.
    
    ### When rows are deleted
    
    This will trigger the immediate removal of the triggers icons on
    questions that were triggered by an answer to the deleted question.
    
    ### NOT when questions are moved
    
    As there is no convenient way to ensure questions are properly ordered with
    respect to their triggers (order dependency) without popping error messages
    at every step when reordering questions, we chose to mark the misplaced lines,
    missing trigger records or triggers that will be ignored (with the new
    `trigger` widget).
    
    This could have been done entirely with 'server' logic, but that would have
    required additional saves and cleaning fields even more often, even during a
    temporary reordering step, which isn't convenient.
    
    ### Misc
    Finally, note the extra fields added on the question_and_page_ids field of the
    survey form view, required to trigger these fields' update consistently
    (required after reordering or removing records which sometimes doesn't occur
    without these).
    
    Tests ensuring the behavior of the X2Many (saving parent on open/edit and show
    notifications on error) and the trigger widget are included.
    
    ## web
    To support the functionality described above, we need to allow the caller of
    'save' to catch errors to prevent default handling (here to use a notification
    instead of an additional Dialog).
    
    Task-2623482
    Part of odoo#78705

--- addons/survey/static/src/question_page/question_page_one2many_field.js
+++ addons/survey/static/src/question_page/question_page_one2many_field.js
@@ -2,6 +2,8 @@
 
 import { QuestionPageListRenderer } from "./question_page_list_renderer";
 import { registry } from "@web/core/registry";
+import { useOpenX2ManyRecord, useX2ManyCrud } from "@web/views/fields/relational_utils";
+import { useService } from "@web/core/utils/hooks";
 import { X2ManyField } from "@web/views/fields/x2many/x2many_field";
 
 const { useSubEnv } = owl;
@@ -12,6 +14,88 @@ class QuestionPageOneToManyField extends X2ManyField {
         useSubEnv({
             openRecord: (record) => this.openRecord(record),
         });
+        this.notificationService = useService("notification");
+
+        // Systematically and automatically save SurveyForm at each question edit/creation
+        // enables checking validation parameters consistency and using questions as triggers
+        // immediately during question creation.
+        // Preparing everything in order to override `this._openRecord` below.
+        const { saveRecord: superSaveRecord, updateRecord: superUpdateRecord } = useX2ManyCrud(
+            () => this.list,
+            this.isMany2Many
+        );
+
+        const self = this;
+        const saveRecord = async (record) => {
+            const saveResponse = await superSaveRecord(record);
+            try {
+                await self.props.record.save({stayInEdition: true, throwOnError: true});
+            } catch (error) {
+                return self.handleSurveySaveError(error, record);
+            }
+            return saveResponse;
+        };
+
+        const updateRecord = async (record) => {
+            const updateResponse = await superUpdateRecord(record);
+            try {
+                await self.props.record.save({stayInEdition: true, throwOnError: true});
+            } catch (error) {
+                return self.handleSurveySaveError(error);
+            }
+            return updateResponse;
+        };
+
+        const openRecord = useOpenX2ManyRecord({
+            resModel: this.list.resModel,
+            activeField: this.activeField,
+            activeActions: this.activeActions,
+            getList: () => this.list,
+            saveRecord,
+            updateRecord,
+        });
+        this._openRecord = async (params) => {
+            if (!await self.props.record.save({ stayInEdition: true })) {
+                // do not open question form as it won't be savable either.
+                return;
+            }
+            if (params.record) {
+                // Force synchronization of fields that depend on sequence
+                // (allowed_triggering_question_ids, is_placed_before_trigger)
+                // as records may have been re-ordered before opening this one.
+                await params.record.load();
+            }
+            openRecord(params);
+        };
+    }
+
+    /**
+     * For convenience, we'll prevent closing the question form dialog and
+     * stay in edit mode to make sure only valid records are saved. Therefore,
+     * two things should be cared for in case of error occurring when saving
+     * the question:
+     *   * Remove erroneous question row added to the embedded list
+     *   * Replace default error modal with a notification
+     *
+     *   @param {Error} error Error thrown when saving survey/question.
+     *   @param {Record?} recordToDelete (optional) In case the error is
+     *   thrown when saving a new question, it should be deleted from the
+     *   list.
+     */
+    async handleSurveySaveError(error, recordToDelete) {
+        error.event.preventDefault();
+        if (recordToDelete) {
+            const listRecord = this.list.records.find(r => r.__bm_handle__ === recordToDelete.__bm_handle__);
+            await this.list.delete(listRecord.id);
+        }
+        this.notificationService.add(
+            error.message.data.message, {
+                title: this.env._t("Validation Error"),
+                type: "danger"
+            }
+        );
+        // Prevent closing the question form view
+        return Promise.reject("Impossible to save survey, see error notification.");
     }
 }
 QuestionPageOneToManyField.components = {
