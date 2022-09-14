2dc5bf555f1cc96e3faa04563af2e0051489ed29
Valentin Chevalier <vcr@odoo.com>
2022-09-14 15:56:15 +0000

    [IMP] account_payment, sale: log messages with superuser
    When payment providers receive a notification from a webhooks and log
    something in the chatter of transactions, sale orders or invoices, the
    message is shown as from 'Public User'.
    
    Now, all messages will be logged as 'OdooBot'.
    
    Part-of: odoo/odoo#100186

--- addons/sale/models/payment_transaction.py
+++ addons/sale/models/payment_transaction.py
@@ -98,6 +98,7 @@ class PaymentTransaction(models.Model):
         :return: None
         """
         super()._log_message_on_linked_documents(message)
+        self = self.with_user(SUPERUSER_ID)  # Log messages as 'OdooBot'
         for order in self.sale_order_ids:
             order.message_post(body=message)
 
