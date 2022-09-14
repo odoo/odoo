30ea9ba8bb85e5b5611d621899c9c1e0c648616d
Valentin Chevalier <vcr@odoo.com>
2022-09-14 12:08:30 +0200

    [REF] payment(_stripe): cleanup express checkout implementation
    Fine-tuning of c624ed56038db524d781df4e034f0fe501e777d9.
    
    task-3004118

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -150,7 +150,7 @@ class Website(main.Website):
         }
 
 class WebsiteSale(http.Controller):
-    _express_checkout_route = '/shop/express'
+    _express_checkout_route = '/shop/express_checkout'
 
     WRITABLE_PARTNER_FIELDS = [
         'name',
@@ -1436,7 +1436,7 @@ class WebsiteSale(http.Controller):
             'partner_id': order.partner_id.id if logged_in else -1,
             'payment_access_token': order._portal_ensure_token(),
             'transaction_route': f'/shop/payment/transaction/{order.id}',
-            'express_route': self._express_checkout_route,
+            'express_checkout_route': self._express_checkout_route,
             'landing_route': '/shop/payment/validate',
         }
 
@@ -1520,7 +1520,7 @@ class WebsiteSale(http.Controller):
         }
 
     @http.route('/shop/payment/validate', type='http', auth="public", website=True, sitemap=False)
-    def shop_payment_validate(self, transaction_id=None, sale_order_id=None, **post):
+    def shop_payment_validate(self, sale_order_id=None, **post):
         """ Method that should be called by the server when receiving an update
         for a transaction. State at this point :
 
@@ -1538,13 +1538,7 @@ class WebsiteSale(http.Controller):
             order = request.env['sale.order'].sudo().browse(sale_order_id)
             assert order.id == request.session.get('sale_last_order_id')
 
-        if transaction_id:
-            tx = request.env['payment.transaction'].sudo().browse(int(transaction_id))
-            assert tx in order.transaction_ids
-        elif order:
-            tx = order.get_portal_last_transaction()
-        else:
-            tx = None
+        tx = order.get_portal_last_transaction()
 
         if not order or (order.amount_total and not tx):
             return request.redirect('/shop')
@@ -1756,8 +1750,6 @@ class PaymentPortal(payment_portal.PaymentPortal):
         if last_tx:
             PaymentPostProcessing.remove_transactions(last_tx)
         request.session['__website_sale_last_tx_id'] = tx_sudo.id
-        if kwargs.pop('add_id_to_landing_route', False):
-            tx_sudo.landing_route += f'?transaction_id={tx_sudo.id}'
 
         self._validate_transaction_for_order(tx_sudo, order_id)
 
