6a84da0637196dae72505f5204d00f77d1285649
Yannick Tivisse <yti@odoo.com>
2022-09-14 10:22:12 +0000

    [ADD] base_install_request: Allow internal users requesting an app install
    closes odoo/odoo#99438
    
    Taskid: 2969678
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ addons/base_install_request/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import models
+from . import wizard
+
+from odoo import api, tools, SUPERUSER_ID
+
+
+def _auto_install_apps(cr, registry):
+    if not tools.config.get('default_productivity_apps', True):
+        return
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['ir.module.module'].sudo().search([
+        ('name', 'in', [
+            # Community
+            'board', 'hr', 'mass_mailing', 'project', 'survey',
+            # Enterprise
+            'appointment', 'knowledge', 'planning', 'sign',
+        ]),
+        ('state', '=', 'uninstalled')
+    ]).button_install()
