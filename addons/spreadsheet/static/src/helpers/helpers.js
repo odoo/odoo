ad2701d21625364a839c2221804462f538d8b2f5
Pierre Rousseau <pro@odoo.com>
2022-09-14 07:19:18 +0000

    [MOV] spreadsheet: move template code to enterprise
    Spreadsheet template feature is a feature enterprise but some part of
    the code is located in community following a split in the modules.
    The code is moved where it belongs.
    
    closes odoo/odoo#100120
    
    Related: odoo/enterprise#31292
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/spreadsheet/static/src/helpers/helpers.js
+++ addons/spreadsheet/static/src/helpers/helpers.js
@@ -1,14 +1,9 @@
 /** @odoo-module */
 
-import spreadsheet from "../o_spreadsheet/o_spreadsheet_extended";
-import { DataSources } from "../data_sources/data_sources";
 import { serializeDate } from "@web/core/l10n/dates";
-import { migrate } from "@spreadsheet/o_spreadsheet/migration";
 import { loadJS } from "@web/core/assets";
 
 const { DateTime } = luxon;
-
-const Model = spreadsheet.Model;
 
 /**
  * Get the intersection of two arrays
@@ -40,78 +35,6 @@ export function getMaxObjectId(o) {
     const nums = keys.map((id) => parseInt(id, 10));
     const max = Math.max(...nums);
     return max;
-}
-
-/**
- * see https://stackoverflow.com/a/30106551
- * @param {string} string
- * @returns {string}
- */
-function utf8ToBase64(str) {
-    // first we use encodeURIComponent to get percent-encoded UTF-8,
-    // then we convert the percent encodings into raw bytes which
-    // can be fed into btoa.
-    return btoa(
-        encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function toSolidBytes(match, p1) {
-            return String.fromCharCode("0x" + p1);
-        })
-    );
-}
-
-/**
- * see https://stackoverflow.com/a/30106551
- * @param {string} string
- * @returns {string}
- */
-function base64ToUtf8(str) {
-    // Going backwards: from bytestream, to percent-encoding, to original string.
-    return decodeURIComponent(
-        atob(str)
-            .split("")
-            .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
-            .join("")
-    );
-}
-
-/**
- * Encode a json to a base64 string
- * @param {object} json
- */
-export function jsonToBase64(json) {
-    return utf8ToBase64(JSON.stringify(json));
-}
-
-/**
- * Decode a base64 encoded json
- * @param {string} string
- */
-export function base64ToJson(string) {
-    return JSON.parse(base64ToUtf8(string));
-}
-
-/**
- * Takes a template id as input, will convert the formulas
- * from relative to absolute in a way that they can be used to create a sheet.
- *
- * @param {Function} rpc
- * @param {number} templateId
- * @returns {Promise<Object>} spreadsheetData
- */
-export async function getDataFromTemplate(env, orm, templateId) {
-    let [{ data }] = await orm.read("spreadsheet.template", [templateId], ["data"]);
-    data = base64ToJson(data);
-
-    const model = new Model(migrate(data), {
-        dataSources: new DataSources(orm),
-    });
-    await model.config.dataSources.waitForAllLoaded();
-    const proms = [];
-    for (const pivotId of model.getters.getPivotIds()) {
-        proms.push(model.getters.getPivotDataSource(pivotId).prepareForTemplateGeneration());
-    }
-    await Promise.all(proms);
-    model.dispatch("CONVERT_PIVOT_FROM_TEMPLATE");
-    return model.exportData();
 }
 
 /** converts and orderBy Object to a string equivalent that can be processed by orm.call */
