598492ef4de25ceb9cf603b11263d05b3796865e
Xavier Morel <xmo@odoo.com>
2022-09-14 14:12:54 +0000

    [FIX] auth_password_policy_signup: port to modern js and fix
    b3b85cae9b1951b82053d7c6b55e559cbc48307d unwittingly broke
    auth_password_policy_signup when it ported everything to owl.
    
    - move the CSS file back to assets_common
    - reintroduce the old password gauge, moved over to signup (as it's
      not used anymore by auth_password_policy), and converted to modern
      JS style
    - convert signup_policy to more modern JS style (for consistency)
    
    Manifest file doesn't need to be updated because it globs.
    
    closes odoo/odoo#100169
    
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>

--- addons/auth_password_policy/__manifest__.py
+++ addons/auth_password_policy/__manifest__.py
@@ -10,11 +10,11 @@
     ],
     'assets': {
         'web.assets_backend': [
-            'auth_password_policy/static/src/css/password_field.css',
             'auth_password_policy/static/src/password_meter.js',
             'auth_password_policy/static/src/password_field.js',
         ],
         'web.assets_common': [
+            'auth_password_policy/static/src/css/password_field.css',
             'auth_password_policy/static/src/password_policy.js',
         ],
     },
