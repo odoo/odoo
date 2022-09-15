fa3e8d1b743e7ba2de6c91155e0aae4508626de7
Nasreddin Boulif (bon) <bon@odoo.com>
2022-09-15 14:47:25 +0000

    [FIX] sale_loyalty: allow cancel order with coupons of pos
    Steps to reproduce:
    
      - Install POS Loyalty
      - Create a loyalty program with a coupon
      - Create an order with the coupon
      - Try to cancel the order
    
    Issue:
    
      Access error raised because have no access to POS lines model.
    
    Cause:
    
      During the cancel process, we are trying to get `use_count` of the
      coupon, that trigger compute func `_compute_use_count` that is also in
      pos_loyalty. This last one will try to find related POS lines while
      user have no access to POS.
    
    Solution:
    
      Do sudo on coupons before filtering.
    
    opw-2959627
    
    X-original-commit: 503945600c15ab49700c7cf00cd61dedc554d4a4
    Part-of: odoo/odoo#99736

--- addons/sale_loyalty/tests/test_loyalty.py
+++ addons/sale_loyalty/tests/test_loyalty.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.tests import TransactionCase, tagged
+from odoo.tests import TransactionCase, tagged, new_test_user
 
 @tagged('post_install', '-at_install')
 class TestLoyalty(TransactionCase):
@@ -10,6 +10,17 @@ class TestLoyalty(TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.env['loyalty.program'].search([]).write({'active': False})
+
+        cls.partner_a = cls.env['res.partner'].create({'name': 'Jean Jacques'})
+
+        cls.product_a = cls.env['product.product'].create({
+            'name': 'Product C',
+            'list_price': 100,
+            'sale_ok': True,
+            'taxes_id': [(6, 0, [])],
+        })
+
+        cls.user_salemanager = new_test_user(cls.env, login='user_salemanager', groups='sales_team.group_sale_manager')
 
     def test_nominative_programs(self):
         loyalty_program, ewallet_program = self.env['loyalty.program'].create([
@@ -36,16 +47,9 @@ class TestLoyalty(TransactionCase):
                 'reward_ids': [(0, 0, {})],
             }
         ])
-        product = self.env['product.product'].create({
-            'name': 'Product C',
-            'list_price': 100,
-            'sale_ok': True,
-            'taxes_id': [(6, 0, [])],
-        })
 
-        jean_jacques = self.env['res.partner'].create({'name': 'Jean Jacques'})
         order = self.env['sale.order'].create({
-            'partner_id': jean_jacques.id,
+            'partner_id': self.partner_a.id,
         })
         order._update_programs_and_rewards()
         claimable_rewards = order._get_claimable_rewards()
@@ -54,18 +58,18 @@ class TestLoyalty(TransactionCase):
         _, ewallet_coupon = self.env['loyalty.card'].create([
             {
                 'program_id': loyalty_program.id,
-                'partner_id': jean_jacques.id,
+                'partner_id': self.partner_a.id,
                 'points': 10,
             },
             {
                 'program_id': ewallet_program.id,
-                'partner_id': jean_jacques.id,
+                'partner_id': self.partner_a.id,
                 'points': 0,
             },
         ])
         order.write({
             'order_line': [(0, 0, {
-                'product_id': product.id,
+                'product_id': self.product_a.id,
                 'product_uom_qty': 1,
             })]
         })
@@ -76,3 +80,40 @@ class TestLoyalty(TransactionCase):
         order._update_programs_and_rewards()
         claimable_rewards = order._get_claimable_rewards()
         self.assertEqual(len(claimable_rewards), 2, "Now that the ewallet has some points they should both be applicable.")
+
+    def test_cancel_order_with_coupons(self):
+        """This test ensure that creating an order with coupons will not
+        raise an access error on POS line modele when canceling the order."""
+
+        self.env['loyalty.program'].create({
+            'name': '10% Discount',
+            'program_type': 'coupons',
+            'applies_on': 'current',
+            'trigger': 'auto',
+            'rule_ids': [(0, 0, {})],
+            'reward_ids': [(0, 0, {
+                'reward_type': 'discount',
+                'discount': 10,
+                'discount_mode': 'percent',
+                'discount_applicability': 'order',
+            })]
+        })
+
+        order = self.env['sale.order'].with_user(self.user_salemanager).create({
+            'partner_id': self.partner_a.id,
+            'order_line': [
+                (0, 0, {
+                    'product_id': self.product_a.id,
+                })
+            ]
+        })
+
+        order._update_programs_and_rewards()
+        self.assertTrue(order.coupon_point_ids)
+
+        # Canceling the order should not raise an access error:
+        # During the cancel process, we are trying to get `use_count` of the coupon,
+        # and we call the `_compute_use_count` that is also in pos_loyalty.
+        # This last one will try to find related POS lines while user have not access to POS.
+        order._action_cancel()
+        self.assertFalse(order.coupon_point_ids)
