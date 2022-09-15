79b9c0e03e29510c61769cd4f1be7390ab54de5a
Lucas Lefèvre <lul@odoo.com>
2022-09-15 11:34:41 +0000

    [REF] spreadsheet: convert record selector to wowl components
    The X2ManyTagSelector component still used a legacy many2many tags
    widget under the hood and an adapter layer on top.
    With this commit, it uses new components
    
    Part-of: odoo/odoo#100246

--- addons/spreadsheet/static/src/global_filters/components/tag_selector_widget.js
+++ addons/spreadsheet/static/src/global_filters/components/tag_selector_widget.js
@@ -1,174 +1,108 @@
 /** @odoo-module **/
-"use strict";
 
-import { ComponentAdapter } from "web.OwlCompatibility";
-import { FieldMany2ManyTags } from "web.relational_fields";
-import StandaloneFieldManagerMixin from "web.StandaloneFieldManagerMixin";
-import Widget from "web.Widget";
+import { Domain } from "@web/core/domain";
+import { useService } from "@web/core/utils/hooks";
+import { TagsList } from "@web/views/fields/many2many_tags/tags_list";
+import { Many2XAutocomplete } from "@web/views/fields/relational_utils";
 
-const { Component, xml } = owl;
-
-/**
- * This widget is used in the global filters to select a value for a
- * relation filter
- * It uses a FieldMany2ManyTags widget.
- */
-const TagSelectorWidget = Widget.extend(StandaloneFieldManagerMixin, {
-    /**
-     * @constructor
-     *
-     * @param {string} relatedModel Name of the related model
-     * @param {Array<number>} selectedValues Values already selected
-     * @param {string | undefined} placeholder
-     */
-    init: function (parent, relatedModel, selectedValues, placeholder) {
-        this._super.apply(this, arguments);
-        StandaloneFieldManagerMixin.init.call(this);
-        this.relatedModel = relatedModel;
-        this.selectedValues = selectedValues;
-        this.placeholder = placeholder;
-        this.widget = undefined;
-    },
-    updateWidgetState: async function (state) {
-        this.selectedValues = state.selectedValues;
-        this.relatedModel = state.relatedModel;
-        await this._makeM2MWidget();
-        const $content = $("<div/>");
-        this.$el.empty().append($content);
-        this.widget.appendTo($content);
-    },
-    /**
-     * @override
-     */
-    willStart: async function () {
-        await this._super.apply(this, arguments);
-        await this._makeM2MWidget();
-    },
-    /**
-     * @override
-     */
-    start: function () {
-        const $content = $("<div/>");
-        this.$el.append($content);
-        this.widget.appendTo($content);
-        return this._super.apply(this, arguments);
-    },
-    //----------------------------------------------------------------------
-    // Private
-    //----------------------------------------------------------------------
-
-    /**
-     * @override
-     */
-    _confirmChange: async function () {
-        const result = await StandaloneFieldManagerMixin._confirmChange.apply(this, arguments);
-        this.trigger_up("value_changed", {
-            value: this.widget.value.data.map((record) => record.data),
-        });
-        return result;
-    },
-    /**
-     * Create a record for the related model and a FieldMany2ManyTags linked
-     * to this record
-     */
-    _makeM2MWidget: async function () {
-        const options = {};
-        options[this.relatedModel] = {
-            options: {
-                no_create_edit: true,
-                no_create: true,
-            },
-        };
-        const recordID = await this.model.makeRecord(
-            this.relatedModel,
-            [
-                {
-                    fields: [
-                        {
-                            name: "id",
-                            type: "integer",
-                        },
-                        {
-                            name: "display_name",
-                            type: "char",
-                        },
-                    ],
-                    name: this.relatedModel,
-                    relation: this.relatedModel,
-                    type: "many2many",
-                    value: this.selectedValues,
-                },
-            ],
-            options
-        );
-        this.widget = new FieldMany2ManyTags(this, this.relatedModel, this.model.get(recordID), {
-            mode: "edit",
-            attrs: { placeholder: this.placeholder },
-        });
-        this._registerWidget(recordID, this.relatedModel, this.widget);
-    },
-});
-
-class TagSelectorWidgetAdapter extends ComponentAdapter {
-    setup() {
-        super.setup();
-        this.env = Component.env;
-    }
-
-    _trigger_up(ev) {
-        if (ev.name === "value_changed") {
-            const { value } = ev.data;
-            return this.props.onValueChanged(value);
-        }
-        super._trigger_up(ev);
-    }
-
-    /**
-     * @override
-     */
-    get widgetArgs() {
-        return [this.props.relatedModel, this.props.selectedValues, this.props.placeholder];
-    }
-
-    /**
-     * @override
-     */
-    async updateWidget(nextProps) {
-        if (
-            nextProps.relatedModel !== this.props.relatedModel ||
-            JSON.stringify(nextProps.selectedValues) !== JSON.stringify(this.props.selectedValues)
-        ) {
-            const payload = {
-                relatedModel: nextProps.relatedModel,
-                selectedValues: nextProps.selectedValues,
-            };
-            return this.widget.updateWidgetState(payload);
-        }
-    }
-
-    /**
-     * @override
-     */
-    renderWidget() {}
-}
+const { Component, xml, onWillStart, onWillUpdateProps } = owl;
 
 export class X2ManyTagSelector extends Component {
     setup() {
-        this.TagSelectorWidget = TagSelectorWidget;
+        /** @type {Record<number, string>} */
+        this.displayNames = {};
+        /** @type {import("@web/core/orm_service").ORM}*/
+        this.orm = useService("orm");
+        onWillStart(() =>
+            this.fetchMissingDisplayNames(this.props.relatedModel, this.props.selectedValues)
+        );
+        onWillUpdateProps((nextProps) =>
+            this.fetchMissingDisplayNames(nextProps.relatedModel, nextProps.selectedValues)
+        );
+    }
+
+    get tags() {
+        return this.props.selectedValues.map((id) => ({
+            text: this.displayNames[id],
+            onDelete: () => this.removeRecord(id),
+            displayBadge: true,
+        }));
+    }
+
+    searchDomain() {
+        return Domain.not([["id", "in", this.props.selectedValues]]).toList();
+    }
+
+    /**
+     * @param {number} recordId
+     */
+    removeRecord(recordId) {
+        delete this.displayNames[recordId];
+        this.notifyChange(this.props.selectedValues.filter((id) => id !== recordId));
+    }
+
+    /**
+     * @param {{ id: number; name?: string}[]} records
+     */
+    update(records) {
+        for (const record of records.filter((record) => record.name)) {
+            this.displayNames[record.id] = record.name;
+        }
+        this.notifyChange(this.props.selectedValues.concat(records.map(({ id }) => id)));
+    }
+
+    /**
+     * @param {number[]} selectedIds
+     */
+    notifyChange(selectedIds) {
+        this.props.onValueChanged(
+            selectedIds.map((id) => ({ id, display_name: this.displayNames[id] }))
+        );
+    }
+
+    /**
+     * @param {string} resModel
+     * @param {number[]} recordIds
+     */
+    async fetchMissingDisplayNames(resModel, recordIds) {
+        const missingNameIds = recordIds.filter((id) => !(id in this.displayNames));
+        if (missingNameIds.length === 0) {
+            return;
+        }
+        const results = await this.orm.read(resModel, missingNameIds, ["display_name"]);
+        for (const { id, display_name } of results) {
+            this.displayNames[id] = display_name;
+        }
     }
 }
-X2ManyTagSelector.components = { TagSelectorWidgetAdapter };
+X2ManyTagSelector.components = { TagsList, Many2XAutocomplete };
 X2ManyTagSelector.template = xml/*xml*/ `
-    <TagSelectorWidgetAdapter
-        Component="TagSelectorWidget"
-        relatedModel="props.relatedModel"
-        selectedValues="props.selectedValues"
-        onValueChanged="props.onValueChanged"
-        placeholder="props.placeholder"
-    />`;
+    <div class="o_field_widget o_field_many2many_tags">
+        <div class="o_field_tags d-inline-flex flex-wrap mw-100 o_tags_input o_input">
+            <TagsList tags="tags"/>
+            <div class="o_field_many2many_selection d-inline-flex w-100">
+                <Many2XAutocomplete
+                    placeholder="props.placeholder"
+                    resModel="props.relatedModel"
+                    fieldString="props.placeholder"
+                    activeActions="{}"
+                    update.bind="update"
+                    getDomain.bind="searchDomain"
+                    isToMany="true"
+                />
+            </div>
+        </div>
+    </div>`;
 X2ManyTagSelector.props = {
+    /**
+     * Callback called when a record is selected or removed.
+     * (selectedRecords: Array<{ id: number; display_name: string }>) => void
+     **/
     onValueChanged: Function,
     relatedModel: String,
+    /**
+     * Array of selected record ids
+     */
     selectedValues: {
         optional: true,
         type: Array,
