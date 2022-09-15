4d27b02fd63f9520332f8c8fabce7a402bf55b70
Demesmaeker <edm@odoo.com>
2022-09-15 13:46:50 +0000

    [FIX] sale_product_matrix: error in matrix tour
    Following the steps that changed every quantity in a matrix, the next
    step was triggered after at least one sale order line's quantity was
    changed. Due to the number of lines created, one of the so lines was
    sometimes not updated before the next step, which produces an error in
    the final sale_count.
    
    In order to check the total and ensure every line was updated, we set
    the partner sooner in the tour, which in turn sets the pricelist used to
    get the price of each line. Thus allowing a check on the subtotal.
    
    Backport of commit-af1e67aa2c6c676479ee4ea73106bcabd20c621e
    
    X-original-commit: 4164bd7494639accdb91c6d6198eef4ee1c13074

--- addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
+++ addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
@@ -26,10 +26,35 @@ tour.register('sale_matrix_tour', {
     trigger: '.o_list_button_add',
     extra_trigger: '.o_sale_order'
 }, {
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
     trigger: 'a:contains("Add a product")'
+||||||| parent of f9d3d0f7089e... temp
+    trigger: "a:contains('Add a product')"
+=======
+    trigger: '.o_required_modifier[name=partner_id] input',
+    run: 'text Agrolait',
+}, {
+    trigger: '.ui-menu-item > a:contains("Agrolait")',
+    auto: true,
+}, {
+    trigger: 'a:contains("Add a product")',
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 }, {
     trigger: 'div[name="product_template_id"] input',
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
     run: "text Matrix",
+||||||| parent of f9d3d0f7089e... temp
+    run: function () {
+        var $input = $('div[name="product_template_id"] input');
+        $input.click();
+        $input.val('Matrix');
+        var keyDownEvent = jQuery.Event("keydown");
+        keyDownEvent.which = 42;
+        $input.trigger(keyDownEvent);
+    }
+=======
+    run: 'text Matrix',
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 }, {
     trigger: 'ul.ui-autocomplete a:contains("Matrix")',
 }, {
@@ -40,6 +65,15 @@ tour.register('sale_matrix_tour', {
     }
 }, {
     trigger: 'span:contains("Confirm")',
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
+||||||| parent of f9d3d0f7089e... temp
+    run: 'click'
+=======
+}, {
+    trigger: '.o_sale_order',
+    // wait for qty to be 1 => check the total to be sure all qties are set to 1
+    extra_trigger: '.oe_subtotal_footer_separator:contains("248.40")',
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 }, {
     trigger: 'span:contains("Matrix (PAV11, PAV22, PAV31)\n\nPA4: PAV41")',
     extra_trigger: '.o_form_editable',
@@ -64,13 +98,30 @@ tour.register('sale_matrix_tour', {
         $('.o_matrix_input').val(3);
     }
 }, {
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
     trigger: 'span:contains("Confirm")',  // apply the matrix
+||||||| parent of f9d3d0f7089e... temp
+    trigger: 'span:contains("Confirm")',
+    run: 'click' // apply the matrix
+=======
+    trigger: 'span:contains("Confirm")',  // apply the matrix
+}, {
+    trigger: '.o_sale_order',
+    // wait for qty to be changed => check the total to be sure all qties are set to 3
+    extra_trigger: '.oe_subtotal_footer_separator:contains("745.20")',
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 }, {
     trigger: 'span:contains("Matrix (PAV11, PAV22, PAV31)\n\nPA4: PAV41")',
     extra_trigger: '.o_form_editable',
-    run: 'click'
 }, {
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
     trigger: '.o_edit_product_configuration',  // edit the matrix
+||||||| parent of f9d3d0f7089e... temp
+    trigger: '.o_edit_product_configuration',
+    run: 'click' // edit the matrix
+=======
+    trigger: '.o_edit_product_configuration',
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 }, {
     trigger: '.o_product_variant_matrix',
     run: function () {
@@ -78,26 +129,61 @@ tour.register('sale_matrix_tour', {
         $('.o_matrix_input').val(1);
     }
 }, {
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
     trigger: 'span:contains("Confirm")',  // apply the matrix
+||||||| parent of f9d3d0f7089e... temp
+    trigger: 'span:contains("Confirm")',
+    run: 'click' // apply the matrix
+=======
+    trigger: 'span:contains("Confirm")',
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 }, {
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
     trigger: '.o_form_editable .o_field_many2one[name="partner_id"] input',
     // wait for qty to be 1
     extra_trigger: '.o_sale_order .o_field_cell.o_data_cell.o_list_number:contains("1.00")',
     run: 'text Agrolait'
+||||||| parent of f9d3d0f7089e... temp
+    trigger: ".o_form_editable .o_field_many2one[name='partner_id'] input",
+    // wait for qty to be 1
+    extra_trigger: '.o_sale_order .o_field_cell.o_data_cell.o_list_number:contains("1.00")',
+    run: 'text Agrolait'
+=======
+    trigger: '.o_sale_order',
+    // wait for qty to be 1 => check the total to be sure all qties are reset to 1
+    extra_trigger: '.oe_subtotal_footer_separator:contains("248.40")',
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 }, {
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
     trigger: '.ui-menu-item > a',
     auto: true,
     in_modal: false,
 }, {
     trigger: '.o_form_button_save:contains("Save")',  // SAVE Sales Order.
+||||||| parent of f9d3d0f7089e... temp
+    trigger: ".ui-menu-item > a",
+    auto: true,
+    in_modal: false,
+}, {
+    trigger: '.o_form_button_save:contains("Save")',
+    run: 'click' // SAVE Sales Order.
+=======
+    trigger: '.o_form_button_save:contains("Save")',  // SAVE Sales Order.
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 },
 // Open the matrix through the pencil button next to the product in line edit mode.
 {
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
     trigger: '.o_form_button_edit:contains("Edit")',  // Edit Sales Order.
+||||||| parent of f9d3d0f7089e... temp
+    trigger: '.o_form_button_edit:contains("Edit")',
+    run: 'click' // Edit Sales Order.
+=======
+    trigger: '.o_form_button_edit:contains("Edit")',   // Edit Sales Order.
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 }, {
     trigger: 'span:contains("Matrix (PAV11, PAV22, PAV31)\n\nPA4: PAV41")',
     extra_trigger: '.o_form_editable',
-    run: 'click'
 }, {
     trigger: '.o_edit_product_configuration',  // edit the matrix
 }, {
@@ -107,11 +193,20 @@ tour.register('sale_matrix_tour', {
         $('.o_matrix_input').slice(8, 16).val(4);
     } // set the qty to 4 for half of the matrix products.
 }, {
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
+    trigger: 'span:contains("Confirm")',  // apply the matrix
+||||||| parent of f9d3d0f7089e... temp
+    trigger: 'span:contains("Confirm")',
+    run: 'click' // apply the matrix
+=======
     trigger: 'span:contains("Confirm")',  // apply the matrix
 }, {
+    trigger: '.o_sale_order',
+    // wait for qty to be changed => check the total to be sure all qties are set to either 1 or 4
+    extra_trigger: '.oe_subtotal_footer_separator:contains("621.00")',
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
+}, {
     trigger: '.o_form_button_save:contains("Save")',
-    extra_trigger: '.o_field_cell.o_data_cell.o_list_number:contains("4.00")',
-    run: 'click' // SAVE Sales Order, after matrix has been applied (extra_trigger).
 }, {
     trigger: '.o_form_button_edit:contains("Edit")',  // Edit Sales Order.
 },
@@ -121,7 +216,20 @@ tour.register('sale_matrix_tour', {
     extra_trigger: '.o_form_editable',
 }, {
     trigger: 'div[name="product_template_id"] input',
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
     run: 'text Matrix',
+||||||| parent of f9d3d0f7089e... temp
+    run: function () {
+        var $input = $('div[name="product_template_id"] input');
+        $input.click();
+        $input.val('Matrix');
+        var keyDownEvent = jQuery.Event("keydown");
+        keyDownEvent.which = 42;
+        $input.trigger(keyDownEvent);
+    }
+=======
+    run: 'text Matrix'
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 }, {
     trigger: 'ul.ui-autocomplete a:contains("Matrix")',
 }, {
@@ -131,10 +239,19 @@ tour.register('sale_matrix_tour', {
         $("input[value='4']").slice(0, 4).val(8.2);
     }
 }, {
+<<<<<<< HEAD:addons/test_sale_product_configurators/static/tests/tours/product_matrix_tour.js
     trigger: 'span:contains("Confirm")',  // apply the matrix
+||||||| parent of f9d3d0f7089e... temp
+    trigger: 'span:contains("Confirm")',
+    run: 'click' // apply the matrix
+=======
+    trigger: 'span:contains("Confirm")',
+}, {
+    trigger: '.o_sale_order',
+    // wait for qty to be changed => check the total to be sure all qties are set
+    extra_trigger: '.oe_subtotal_footer_separator:contains("640.32")',
+>>>>>>> f9d3d0f7089e... temp:addons/sale_product_matrix/static/tests/tours/sale_product_matrix_tour.js
 }, {
     trigger: '.o_form_button_save:contains("Save")',
-    extra_trigger: '.o_field_cell.o_data_cell.o_list_number:contains("8.20")',
-    run: 'click' // SAVE Sales Order, after matrix has been applied (extra_trigger).
 },
 ]);
