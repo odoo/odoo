9be3f0f4ffa1db943fceca9f426e89be0d0020f0
tsm-odoo <tsm@odoo.com>
2022-09-15 16:30:30 +0000

    [FIX] bus: make subscribe method only accept strings
    Before [1], only string channels were allowed for polling. This
    ensured no one could send a server-side channel from the frontend,
    this PR restores this behavior.
    
    [1]: odoo/odoo@a5623d2
    
    closes odoo/odoo#100309
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/bus/tests/__init__.py
+++ addons/bus/tests/__init__.py
@@ -1,5 +1,6 @@
 from . import common
 from . import test_assetsbundle
 from . import test_health
+from . import test_ir_websocket
 from . import test_websocket_caryall
 from . import test_websocket_rate_limiting
