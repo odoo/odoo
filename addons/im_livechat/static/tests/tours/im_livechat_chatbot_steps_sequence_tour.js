d6fc0cad340a91bc15290ee8503201d0a9b13863
Aurélien Warnon <awa@odoo.com>
2022-09-15 14:28:18 +0200

    [IMP] im_livechat: port the chatbot form to OWL
    This commit ports the custom implementation of the view form for the chatbot
    scripts to the new OWL framework.
    
    The goal is the same as the base customization, which is mainly to correctly
    assign a sequence to every step and save the form in-between every new steps.
    
    This is necessary to allow the end-user to easily configure its script, since
    steps can depend on each other.
    
    More details in https://github.com/odoo/odoo/pull/84000
    
    In addition, we slightly adapt the CRM bridge to enable no_open for the sales
    team field.
    This avoids issues when the user tries to open modals on top of modals.
    
    Task-2981969
    
    X-original-commit: 68d5da02642d22551603e4c6b5583755b2f4fc2d

--- addons/im_livechat/static/tests/tours/im_livechat_chatbot_steps_sequence_tour.js
+++ addons/im_livechat/static/tests/tours/im_livechat_chatbot_steps_sequence_tour.js
@@ -11,7 +11,7 @@ const commonSteps = [tour.stepUtils.showAppsMenuItem(), {
 }, {
     trigger: '.o_list_button_add',
 }, {
-    trigger: 'input[name="title"]',
+    trigger: 'input[id="title"]',
     run: 'text Test Chatbot Sequence'
 }, {
     trigger: 'div[name="script_step_ids"] .o_field_x2many_list_row_add a'
@@ -88,41 +88,22 @@ tour.register('im_livechat_chatbot_steps_sequence_with_move_tour', {
     trigger: 'tr:contains("Step 5") .o_row_handle',
     run: () => {
         // move 'step 5' between 'step 1' and 'step 2'
-        // tried to use to built-in 'drag_and_drop' action but it doesn't work here
-        // somehow it only registers the move if 2 separate 'mousemove' events are triggered
-        const $element = $('tr:contains("Step 5") .o_row_handle');
-        const elementCenter = $element.offset();
-        elementCenter.left += $element.outerWidth() / 2;
-        elementCenter.top += $element.outerHeight() / 2;
-        const $to = $('tr:eq(1)');
-        const toCenter = $to.offset();
-        toCenter.left += $to.outerWidth() / 2;
-        toCenter.top += $to.outerHeight() / 2;
+        const from = document.querySelector('div[name="script_step_ids"] tr:nth-child(5) .o_row_handle');
+        const fromPosition = from.getBoundingClientRect();
+        fromPosition.x += from.offsetWidth / 2;
+        fromPosition.y += from.offsetHeight / 2;
 
-        $element.trigger($.Event("mouseenter"));
-        $element.trigger($.Event("mousedown", {
+        const to = document.querySelector('div[name="script_step_ids"] tr:nth-child(2) .o_row_handle');
+        from.dispatchEvent(new Event("mouseenter", { bubbles: true }));
+        from.dispatchEvent(new MouseEvent("mousedown", {
+            bubbles: true,
             which: 1,
-            pageX: elementCenter.left,
-            pageY: elementCenter.top
-        }));
-
-        $element.trigger($.Event("mousemove", {
-            which: 1,
-            pageX: toCenter.left - 1,
-            pageY: toCenter.top - 1
-        }));
-
-        $element.trigger($.Event("mousemove", {
-            which: 1,
-            pageX: toCenter.left,
-            pageY: toCenter.top
-        }));
-
-        $element.trigger($.Event("mouseup", {
-            which: 1,
-            pageX: toCenter.left,
-            pageY: toCenter.top
-        }));
+            button: 0,
+            clientX: fromPosition.x,
+            clientY: fromPosition.y}));
+        from.dispatchEvent(new Event("mousemove", { bubbles: true }));
+        to.dispatchEvent(new Event("mouseenter", { bubbles: true }));
+        from.dispatchEvent(new Event("mouseup", { bubbles: true }));
     }
 }, {
     trigger: 'div[name="script_step_ids"] .o_field_x2many_list_row_add a'
