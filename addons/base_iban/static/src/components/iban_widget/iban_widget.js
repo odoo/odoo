d6b1bb3d263a57cdcc115e79b6d1471de8da7e6e
Brice bib Bartoletti <bib@odoo.com>
2022-09-16 08:16:56 +0000

    [IMP] base_iban: convert to owl
    1) conversion to owl
    
    2) very small improvement:
     - make the validation icon appears when starting edition
    
    closes odoo/odoo#100435
    
    Task-id: 2857743
    Signed-off-by: William André (wan) <wan@odoo.com>

---
+++ addons/base_iban/static/src/components/iban_widget/iban_widget.js
@@ -0,0 +1,30 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { CharField } from "@web/views/fields/char/char_field";
+import { useDebounced } from "@web/core/utils/timing";
+import { useService } from "@web/core/utils/hooks";
+
+const { useState } = owl;
+export const DELAY = 400;
+
+export class IbanWidget extends CharField {
+    setup() {
+        super.setup();
+        this.state = useState({ isValidIBAN: null });
+        this.orm = useService("orm");
+        this.validateIbanDebounced = useDebounced(async (ev) => {
+            const iban = ev.target.value;
+            if (!iban) {
+                this.state.isValidIBAN = null;
+            } else if (!/[A-Za-z]{2}.{3,}/.test(iban)) {
+                this.state.isValidIBAN = false;
+            } else {
+                this.state.isValidIBAN = await this.orm.call("res.partner.bank", "check_iban", [[], iban]);
+            }
+        }, DELAY);
+    }
+}
+IbanWidget.template = "base_iban.iban";
+
+registry.category("fields").add("iban", IbanWidget);
