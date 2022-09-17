fb2b86a65311a94de35f6ab8d263b5d511cd6ad8
Christoph Giesel <christoph.giesel@jobrad.org>
2022-09-17 17:14:08 +0200

    Catch error when savepoint was already released to avoid errors in migration scripts

--- odoo/sql_db.py
+++ odoo/sql_db.py
@@ -18,6 +18,7 @@ import warnings
 
 from decorator import decorator
 import psycopg2
+import psycopg2.errors
 import psycopg2.extras
 import psycopg2.extensions
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT, ISOLATION_LEVEL_READ_COMMITTED, ISOLATION_LEVEL_REPEATABLE_READ
@@ -131,7 +132,10 @@ class BaseCursor:
             self.execute('ROLLBACK TO SAVEPOINT "%s"' % name)
             raise
         else:
-            self.execute('RELEASE SAVEPOINT "%s"' % name)
+            try:
+                self.execute('RELEASE SAVEPOINT "%s"' % name)
+            except psycopg2.errors.InvalidSavepointSpecification:
+                _logger.error('Savepoint %s already released' % name)
 
     def __enter__(self):
         """ Using the cursor as a contextmanager automatically commits and
