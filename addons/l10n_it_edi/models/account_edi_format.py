e002921abfeec518a56acd83d7c20a4ce909cfc7
Josse Colpaert <jco@odoo.com>
2022-09-19 07:39:17 +0000

    [FIX] l10n_it_edi: detect fatturapa also if extension in upper case
    Before: it won't work with extension in uppercase: .XML or .XML.P7M
    
    We put ?i: in the regex so that part would be matched case insensitively
    
    opw-2703669
    
    closes odoo/odoo#100465
    
    Signed-off-by: Quentin De Paoli <qdp@odoo.com>

--- addons/l10n_it_edi/models/account_edi_format.py
+++ addons/l10n_it_edi/models/account_edi_format.py
@@ -312,7 +312,7 @@ class AccountEdiFormat(models.Model):
     # -------------------------------------------------------------------------
 
     def _check_filename_is_fattura_pa(self, filename):
-        return re.search("([A-Z]{2}[A-Za-z0-9]{2,28}_[A-Za-z0-9]{0,5}.(xml.p7m|xml))", filename)
+        return re.search("[A-Z]{2}[A-Za-z0-9]{2,28}_[A-Za-z0-9]{0,5}.((?i:xml.p7m|xml))", filename)
 
     def _is_fattura_pa(self, filename, tree):
         return self.code == 'fattura_pa' and self._check_filename_is_fattura_pa(filename)
