936079571a31a2dd12f7d679ce34ad4e866ef44a
Lucas Lefèvre <lul@odoo.com>
2022-09-19 09:38:15 +0200

    [ADD] spreadsheet_dashboard_stock_account: add stock dashboard

---
+++ addons/spreadsheet_dashboard_stock_account/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Spreadsheet dashboard for stock",
+    'category': 'Hidden',
+    'summary': 'Spreadsheet',
+    'description': 'Spreadsheet',
+    'depends': ['spreadsheet_dashboard', 'stock_account'],
+    'data': [
+        "data/dashboards.xml",
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': ['stock_account'],
+    'license': 'LGPL-3',
+    'assets': {}
+}
