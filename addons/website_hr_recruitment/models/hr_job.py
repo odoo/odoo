9b8662835a4a5647ffeed13fa23994e1dc8a67c2
Kevin Baptiste <kba@odoo.com>
2022-09-19 09:54:05 +0200

    [IMP] website_hr_recruitment: use t-field in website template
    Use a `t-field` on the job apply template to make it editable through
    the editor.

--- addons/website_hr_recruitment/models/hr_job.py
+++ addons/website_hr_recruitment/models/hr_job.py
@@ -10,17 +10,11 @@ class Job(models.Model):
     job_details = fields.Html('Process Details',
         help="Complementary information that will appear on the job submission page",
         default="""
-            <div class="d-flex flex-column align-items-baseline">
-                <span class="text-muted small">Time to Answer</span>
-                <h6>2 open days</h6>
-            </div>
-            <div class="d-flex flex-column align-items-baseline">
-                <span class="text-muted small">Process</span>
-                <h6>1 Phone Call</h6>
-                <h6>1 Onsite Interview</h6>
-            </div>
-            <div class="d-flex flex-column align-items-baseline">
-                <span class="text-muted small">Days to get an Offer</span>
-                <h6>4 Days after Interview</h6>
-            </div>
+            <span class="text-muted small">Time to Answer</span>
+            <h6>2 open days</h6>
+            <span class="text-muted small">Process</span>
+            <h6>1 Phone Call</h6>
+            <h6>1 Onsite Interview</h6>
+            <span class="text-muted small">Days to get an Offer</span>
+            <h6>4 Days after Interview</h6>
         """)
