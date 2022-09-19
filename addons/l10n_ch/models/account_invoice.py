395adc411e0612d6ae21be25901a48fe3c7828b0
momegahed <mome@odoo.com>
2022-09-19 10:21:46 +0200

    [FIX] l10n_ch: QR-Invoice don't work for LI customers
    Issue :
    - Liechtenstein adapted the same QR-Invoice as Switzerland.
    However, Odoo only allows the issuance of QR-Invoices to
    swiss customers
    
    https://www.llb.li/en/private/paying-and-saving/payment-services/qr-bill#:~:text=Standing%20orders%20based%20on%20orange%20payment%20slips%20can%20no%20longer%20be%20processed%20after%2030%20September%202022.%20Therefore%2C%20these%20standing%20orders%20need%20to%20be%20newly%20set%20up%20on%20the%20basis%20of%20QR%20bills.
    
    Fix:
    - Allow LI users to be issued QR-Invoices
    
    OPW-2977644

--- addons/l10n_ch/models/account_invoice.py
+++ addons/l10n_ch/models/account_invoice.py
@@ -244,7 +244,7 @@ class AccountMove(models.Model):
     @api.depends('move_type', 'partner_bank_id', 'payment_reference')
     def _compute_l10n_ch_isr_needs_fixing(self):
         for inv in self:
-            if inv.move_type == 'in_invoice' and inv.company_id.country_id.code == "CH":
+            if inv.move_type == 'in_invoice' and inv.company_id.country_id.code in ('CH', 'LI'):
                 partner_bank = inv.partner_bank_id
                 if partner_bank:
                     needs_isr_ref = partner_bank._is_qr_iban() or partner_bank._is_isr_issuer()
