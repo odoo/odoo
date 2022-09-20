c42aeeaa69417ac8a62feae57d806730bb19e345
tsm-odoo <tsm@odoo.com>
2022-09-20 14:07:44 +0200

    [FIX] bus: fix im_status during tests
    Before this PR, the `_mockIrWebsocket__updatePresence` route of the
    bus mock server could lead to errors when no im status ids were
    provided. This commit fixes this issue by ensuring im status ids
    were passed before calling `_mockIrWebsocket__getImStatus` method.
    
    Moreover, old longpolling routes were still present on the bus mock
    server. This commit removes these routes.

--- addons/bus/static/tests/helpers/mock_server.js
+++ addons/bus/static/tests/helpers/mock_server.js
@@ -4,9 +4,7 @@ import { TEST_USER_IDS } from "@bus/../tests/helpers/test_constants";
 import { patchWebsocketWorkerWithCleanup } from '@bus/../tests/helpers/mock_websocket';
 
 import { patch } from "@web/core/utils/patch";
-import { ConnectionLostError } from "@web/core/network/rpc_service";
 import { MockServer } from "@web/../tests/helpers/mock_server";
-import { makeDeferred } from "@web/../tests/helpers/utils";
 
 patch(MockServer.prototype, 'bus', {
     init() {
@@ -39,26 +37,6 @@ patch(MockServer.prototype, 'bus', {
             const { inactivity_period, im_status_ids_by_model } = data;
             this._mockIrWebsocket__updatePresence(inactivity_period, im_status_ids_by_model);
         }
-    },
-
-    /**
-     * @override
-     */
-    async _performRPC(route, args) {
-        if (route === '/longpolling/poll') {
-            const longpollingPromise = makeDeferred();
-            if (this.hasLostConnection) {
-                longpollingPromise.reject(new ConnectionLostError());
-                this.hasLostConnection = false;
-            } else if (this.notificationsToBeResolved.length) {
-                longpollingPromise.resolve(this.notificationsToBeResolved);
-                this.notificationsToBeResolved = [];
-            } else {
-                this.pendingLongpollingPromise = longpollingPromise;
-            }
-            return longpollingPromise;
-        }
-        return this._super(route, args);
     },
     /**
      * Simulates `_sendone` on `bus.bus`.
