c82fdaa5387498c54c0dbe8572e26c263ce69b23
FrancoisGe <fge@odoo.com>
2022-09-20 09:34:42 +0000

    [REF] web: convert signature field/widget to owl
    closes odoo/odoo#100865
    
    X-original-commit: f13e4c486b9d0bf5828f83425b21d11388e45bdb
    Related: odoo/enterprise#31653
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/legacy/js/widgets/name_and_signature.js
+++ addons/web/static/src/legacy/js/widgets/name_and_signature.js
@@ -377,7 +377,7 @@ var NameAndSignature = Widget.extend({
      * @returns {string} image = mimetype + image data
      */
     _getSVGText: function (font, text, width, height) {
-        var $svg = $(core.qweb.render('web.sign_svg_text', {
+        var $svg = $(core.qweb.render('web.legacy.sign_svg_text', {
             width: width,
             height: height,
             font: font,
