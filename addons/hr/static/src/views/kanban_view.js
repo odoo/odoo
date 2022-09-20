ee976b110f9002f9cfd22c1c0a3e9b99d233a7af
William Braeckman <wbr@odoo.com>
2022-09-20 12:19:32 +0000

    [IMP] hr: remove mixins
    Prefer using hooks instead.
    Makes use of the new open_chat_hook from mail when possible.
    
    closes odoo/odoo#100764
    
    X-original-commit: d058734cf55f2c3f46f2f23e0bbbfc54f2f0da15
    Related: odoo/enterprise#31605
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- addons/hr/static/src/views/kanban_view.js
+++ addons/hr/static/src/views/kanban_view.js
@@ -1,17 +1,24 @@
 /** @odoo-module */
 
 import { registry } from '@web/core/registry';
-import { patch } from '@web/core/utils/patch';
 
 import { kanbanView } from '@web/views/kanban/kanban_view';
 import { KanbanModel } from '@web/views/kanban/kanban_model';
 
-import { EmployeeChatMixin } from '../mixins/chat_mixin';
+export class EmployeeKanbanRecord extends KanbanModel.Record {
+    async openChat(employeeId) {
+        const messaging = await this.model.messaging.get();
+        messaging.openChat({ employeeId });
+    }
+}
 
-export class EmployeeKanbanRecord extends KanbanModel.Record {}
-patch(EmployeeKanbanRecord.prototype, 'employee_kanban_record_mixin', EmployeeChatMixin);
-
-export class EmployeeKanbanModel extends KanbanModel {}
+export class EmployeeKanbanModel extends KanbanModel {
+    setup(params, { messaging }) {
+        super.setup(...arguments);
+        this.messagingService = messaging;
+    }
+}
+EmployeeKanbanModel.services = [...KanbanModel.services, "messaging"];
 EmployeeKanbanModel.Record = EmployeeKanbanRecord;
 
 registry.category('views').add('hr_employee_kanban', {
--- addons/hr/static/src/views/list_view.js
+++ addons/hr/static/src/views/list_view.js
@@ -1,18 +1,16 @@
 /** @odoo-module */
 
 import { registry } from '@web/core/registry';
-import { useService } from "@web/core/utils/hooks";
-import { patch } from '@web/core/utils/patch';
 
 import { listView } from '@web/views/list/list_view';
 import { ListController } from '@web/views/list/list_controller';
 
-import { ArchiveEmployeeMixin } from '../mixins/archive_employee_mixin';
+import { useArchiveEmployee } from '@hr/views/archive_employee_hook';
 
 export class EmployeeListController extends ListController {
     setup() {
         super.setup();
-        this.actionService = useService('action');
+        this.archiveEmployee = useArchiveEmployee();
     }
 
     getActionMenuItems() {
@@ -26,20 +24,11 @@ export class EmployeeListController extends ListController {
 
         const archiveAction = menuItems.other.find((item) => item.key === "archive");
         if (archiveAction) {
-            archiveAction.callback = () => {
-                const archiveAction = this._openArchiveEmployee(this.model.root.resId);
-                this.actionService.doAction(archiveAction, {
-                    onClose: async () => {
-                        await this.model.load();
-                        this.model.notify();
-                    }
-                });
-            };
+            archiveAction.callback = this.archiveEmployee.bind(this, selectedRecords[0].resId);
         }
         return menuItems;
     }
 }
-patch(EmployeeListController.prototype, 'employee_list_controller_archive_mixin', ArchiveEmployeeMixin);
 
 registry.category('views').add('hr_employee_list', {
     ...listView,
