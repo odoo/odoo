b248266a583c7b25c581950651b163d6967d8d20
Alexandre Kühn <aku@odoo.com>
2022-09-20 15:20:08 +0200

    [FIX] mail: guard ChatWindowHeaderView/onClick
    This handler was not properly guarded, and made
    `.TestMailChannelExpand` fail on runbot.

--- addons/mail/static/src/models/chat_window_header_view.js
+++ addons/mail/static/src/models/chat_window_header_view.js
@@ -11,6 +11,9 @@ registerModel({
          * @param {MouseEvent} ev
          */
         onClick(ev) {
+            if (!this.exists()) {
+                return;
+            }
             if (
                 isEventHandled(ev, 'ChatWindow.onClickCommand') ||
                 isEventHandled(ev, 'ChatWindow.onClickHideMemberList') ||
