71b62dd1485d1350bb52d086e79121b36bb65869
Victor Feyens <vfe@odoo.com>
2022-09-20 16:20:07 +0000

    [IMP] sale_product_matrix: add related field to reduce rpc calls
    The matrix configurators makes a rpc call to know if the edited line is 
    configurable through matrix or standard configurator.
    This rpc can be avoided (and the logic simplified while converting the 
    widget to owl) with a related field on the lines.
    
    closes odoo/odoo#100613
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale_product_matrix/models/__init__.py
+++ addons/sale_product_matrix/models/__init__.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from . import sale_order
+
 from . import product_template
+from . import sale_order
+from . import sale_order_line
