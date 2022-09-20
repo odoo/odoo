2811c74c0d2266d8ba1583e229c088848bd473f5
Didier (did) <did@odoo.com>
2022-09-20 11:22:07 +0000

    [IMP] mail: convert notification alert
    The notification_alert component can now be registered as a view_widgets directly
    
    closes odoo/odoo#100898
    
    X-original-commit: 4f91758d1aefb6e459ccf02220bf4cf27f3a2ff2
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/components/notification_alert/notification_alert.js
+++ addons/mail/static/src/components/notification_alert/notification_alert.js
@@ -5,10 +5,7 @@ import { registerMessagingComponent } from '@mail/utils/messaging_component';
 const { Component } = owl;
 
 export class NotificationAlert extends Component {}
-
 Object.assign(NotificationAlert, {
-    props: {},
     template: 'mail.NotificationAlert',
 });
-
 registerMessagingComponent(NotificationAlert);
