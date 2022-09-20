be9d2dfbbce3c0c5e1c06e7f666e15c2446c8224
William Braeckman <wbr@odoo.com>
2022-09-20 14:19:32 +0200

    [IMP] hr: remove mixins
    Prefer using hooks instead.
    Makes use of the new open_chat_hook from mail when possible.

--- addons/hr/static/src/views/form_view.js
+++ addons/hr/static/src/views/form_view.js
@@ -1,20 +1,18 @@
 /** @odoo-module */
 
 import { registry } from '@web/core/registry';
-import { useService } from "@web/core/utils/hooks";
-import { patch } from '@web/core/utils/patch';
 
 import { formView } from '@web/views/form/form_view';
 import { FormController } from '@web/views/form/form_controller';
 import { FormRenderer } from '@web/views/form/form_renderer';
 
-import { ArchiveEmployeeMixin } from '../mixins/archive_employee_mixin';
-import { EmployeeChatMixin } from '../mixins/chat_mixin';
+import { useArchiveEmployee } from '@hr/views/archive_employee_hook';
+import { useOpenChat } from "@mail/views/open_chat_hook";
 
 export class EmployeeFormController extends FormController {
     setup() {
         super.setup();
-        this.actionService = useService('action');
+        this.archiveEmployee = useArchiveEmployee();
     }
 
     getActionMenuItems() {
@@ -25,23 +23,18 @@ export class EmployeeFormController extends FormController {
 
         const archiveAction = menuItems.other.find((item) => item.key === "archive");
         if (archiveAction) {
-            archiveAction.callback = () => {
-                const archiveAction = this._openArchiveEmployee(this.model.root.resId);
-                this.actionService.doAction(archiveAction, {
-                    onClose: async () => {
-                        await this.model.load();
-                        this.model.notify();
-                    }
-                });
-            };
+            archiveAction.callback = this.archiveEmployee.bind(this, this.model.root.resId);
         }
         return menuItems;
     }
 }
-patch(EmployeeFormController.prototype, 'employee_form_controller_archive_mixin', ArchiveEmployeeMixin);
 
-export class EmployeeFormRenderer extends FormRenderer {}
-patch(EmployeeFormRenderer.prototype, 'employee_form_renderer_mixin', EmployeeChatMixin);
+export class EmployeeFormRenderer extends FormRenderer {
+    setup() {
+        super.setup();
+        this.openChat = useOpenChat(this.props.record.resModel);
+    }
+}
 
 registry.category('views').add('hr_employee_form', {
     ...formView,
