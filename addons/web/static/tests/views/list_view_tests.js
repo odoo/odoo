bde2985959ef8914da4b8aca96efc5772262d0c0
Achraf <abz@odoo.com>
2022-09-22 14:22:43 +0000

    [FIX] web: Allow users to enable list view with group_by_no_leaf
    - Go to accounting/Report/Invoices Analysis
    - Open studio
    - enable list view
    - empty view
    
    This code bellow compute count_field as `__count` because group_by_no_leaf
    is in the context.
    
    But in the basic_model we don't consider this case.
    
    https://github.com/odoo/odoo/blob/5797fd80a63309269f15bcbe4948d4429a53eec2/odoo/models.py#L2798-L2802
    
    opw-2980923
    
    closes odoo/odoo#100872
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/list_view_tests.js
+++ addons/web/static/tests/views/list_view_tests.js
@@ -15147,4 +15147,28 @@ QUnit.module("Views", (hooks) => {
         await pagerPrevious(target);
         assert.deepEqual([...getPagerValue(target), getPagerLimit(target)], [1, 2]);
     });
+
+    QUnit.test("list with group_by_no_leaf and group by", async function (assert) {
+        assert.expect(4);
+
+        await makeView({
+            type: "list",
+            resModel: "foo",
+            serverData,
+            arch: '<tree expand="1"><field name="foo"/></tree>',
+            groupBy: ["currency_id"],
+            context: { group_by_no_leaf: true },
+        });
+
+        const groups = target.querySelectorAll(".o_group_name");
+        const groupsRecords = [...target.querySelectorAll(".o_data_row .o_data_cell")];
+        assert.strictEqual(groups.length, 2, "There should be 2 groups");
+        assert.strictEqual(groups[0].textContent, "USD (3) ", "Second group should have 3 records");
+        assert.strictEqual(groups[1].textContent, "EUR (1) ", "First group should have 1 record");
+        assert.deepEqual(
+            groupsRecords.map((groupEl) => groupEl.textContent),
+            ["blip", "gnap", "blip", "yop"],
+            "Groups should contains correct records"
+        );
+    });
 });
