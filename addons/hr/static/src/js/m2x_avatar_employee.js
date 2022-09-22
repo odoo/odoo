adb5508afd2749dbbfeb85803d707df43358b011
William Braeckman <wbr@odoo.com>
2022-09-22 14:51:43 +0000

    [FIX] hr,mail: reintroduce legacy x2m_avatar widgets
    Some views still use them.
    
    closes odoo/odoo#100884
    
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- addons/hr/static/src/js/m2x_avatar_employee.js
+++ addons/hr/static/src/js/m2x_avatar_employee.js
@@ -4,7 +4,9 @@ import fieldRegistry from 'web.field_registry';
 
 import {
     Many2OneAvatarUser,
+    KanbanMany2OneAvatarUser,
     KanbanMany2ManyAvatarUser,
+    ListMany2ManyAvatarUser,
     Many2ManyAvatarUser,
 } from '@mail/js/m2x_avatar_user';
 
@@ -42,8 +44,10 @@ const M2XAvatarEmployeeMixin = {
 };
 
 export const Many2OneAvatarEmployee = Many2OneAvatarUser.extend(M2XAvatarEmployeeMixin);
+export const KanbanMany2OneAvatarEmployee = KanbanMany2OneAvatarUser.extend(M2XAvatarEmployeeMixin);
 
 fieldRegistry.add('many2one_avatar_employee', Many2OneAvatarEmployee);
+fieldRegistry.add('kanban.many2one_avatar_employee', KanbanMany2OneAvatarEmployee);
 
 const M2MAvatarEmployeeMixin = Object.assign(M2XAvatarEmployeeMixin, {
     //----------------------------------------------------------------------
@@ -59,8 +63,11 @@ export const Many2ManyAvatarEmployee = Many2ManyAvatarUser.extend(M2MAvatarEmplo
 
 export const KanbanMany2ManyAvatarEmployee = KanbanMany2ManyAvatarUser.extend(M2MAvatarEmployeeMixin, {});
 
+export const ListMany2ManyAvatarEmployee = ListMany2ManyAvatarUser.extend(M2MAvatarEmployeeMixin, {});
+
 fieldRegistry.add('many2many_avatar_employee', Many2ManyAvatarEmployee);
 fieldRegistry.add('kanban.many2many_avatar_employee', KanbanMany2ManyAvatarEmployee);
+fieldRegistry.add('list.many2many_avatar_employee', ListMany2ManyAvatarEmployee);
 
 export default {
     Many2OneAvatarEmployee,
