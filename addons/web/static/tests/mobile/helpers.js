e3a2a5d6ee18539e4a42fd14dee2097d6894a0a8
Bruno Boi <boi@odoo.com>
2022-09-22 08:44:43 +0000

    [MOV] web: transfer ActionSwiper to community
    This commit adapts codebase following the move and integration of the
    touch device detection and customization (through the o_touch_device
    class) in community.
    
    X-original-commit: 5ae86ad473dd69559820b1f599ce389484cdebbc
    Part-of: odoo/odoo#101185
    Co-authored-by: Jorge Pinna Puissant <jpp@odoo.com>

---
+++ addons/web/static/tests/mobile/helpers.js
@@ -0,0 +1,58 @@
+/** @odoo-module **/
+
+import { findElement, triggerEvent } from "../helpers/utils";
+
+async function swipe(target, selector, direction) {
+    const touchTarget = findElement(target, selector);
+    if (direction === "left") {
+        // The scrollable element is set at its right limit
+        touchTarget.scrollLeft = touchTarget.scrollWidth - touchTarget.offsetWidth;
+    } else {
+        // The scrollable element is set at its left limit
+        touchTarget.scrollLeft = 0;
+    }
+
+    await triggerEvent(target, selector, "touchstart", {
+        touches: [
+            {
+                identifier: 0,
+                clientX: 0,
+                clientY: 0,
+                target: touchTarget,
+            },
+        ],
+    });
+    await triggerEvent(target, selector, "touchmove", {
+        touches: [
+            {
+                identifier: 0,
+                clientX: (direction === "left" ? -1 : 1) * touchTarget.clientWidth,
+                clientY: 0,
+                target: touchTarget,
+            },
+        ],
+    });
+    await triggerEvent(target, selector, "touchend", {});
+}
+
+/**
+ * Will simulate a swipe right on the target element with the given selector.
+ *
+ * @param {HTMLElement} target
+ * @param {DOMSelector} [selector]
+ * @returns {Promise}
+ */
+export async function swipeRight(target, selector) {
+    return swipe(target, selector, "right");
+}
+
+/**
+ * Will simulate a swipe left on the target element with the given selector.
+ *
+ * @param {HTMLElement} target
+ * @param {DOMSelector} [selector]
+ * @returns {Promise}
+ */
+export async function swipeLeft(target, selector) {
+    return swipe(target, selector, "left");
+}
