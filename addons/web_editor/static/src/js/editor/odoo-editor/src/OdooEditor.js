bd6ecad1627faa43d2707dddec6b653ee9988458
Nicolas Bayet <nby@odoo.com>
2022-09-22 11:14:41 +0200

    TO IMPROVE OR DELETE [IMP] knowledge: do not alter code pasted from editor

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -3907,7 +3907,22 @@ export class OdooEditor extends EventTarget {
         const clipboardHtml = ev.clipboardData.getData('text/html');
         if (odooEditorHtml) {
             const fragment = parseHTML(odooEditorHtml);
-            DOMPurify.sanitize(fragment, { IN_PLACE: true });
+
+            // DOMPurify.sanitize remove an attribute that contains a ">" for
+            // security reasons. Make an exception for `data-behavior-props`.
+            // Encoding it hides the character ">".
+            for (const el of fragment.querySelectorAll('[data-behavior-props]')) {
+                el.setAttribute('data-behavior-props', encodeURIComponent(el.getAttribute('data-behavior-props')));
+            }
+
+            DOMPurify.sanitize(fragment, {
+                IN_PLACE: true,
+            });
+
+            for (const el of fragment.querySelectorAll('[data-behavior-props]')) {
+                el.setAttribute('data-behavior-props', decodeURIComponent(el.getAttribute('data-behavior-props')));
+            }
+
             if (fragment.hasChildNodes()) {
                 this.execCommand('insert', fragment);
             }
