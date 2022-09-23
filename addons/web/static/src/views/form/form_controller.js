9f6c2b8e969635f35786d58ee89ca767ff57144d
William Braeckman <wbr@odoo.com>
2022-09-23 11:23:14 +0000

    [IMP] web: expose before and after execute action on controllers
    Make it possible to override both before and after execute actions on
    all view controllers using `useViewButtons`.
    This avoids having to change a value in the env to override the function
    being called when clicking on a button.
    
    closes odoo/odoo#100981
    
    Related: odoo/enterprise#31715
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- addons/web/static/src/views/form/form_controller.js
+++ addons/web/static/src/views/form/form_controller.js
@@ -172,20 +172,11 @@ export class FormController extends Component {
             this.archInfo.arch = this.archInfo.xmlDoc.outerHTML;
         }
 
-        const beforeExecuteAction = (clickParams) => {
-            if (clickParams.special !== "cancel") {
-                return this.model.root.save({ stayInEdition: true }).then((saved) => {
-                    if (saved && this.props.onSave) {
-                        this.props.onSave(this.model.root);
-                    }
-                    return saved;
-                });
-            } else if (this.props.onDiscard) {
-                this.props.onDiscard(this.model.root);
-            }
-        };
         const rootRef = useRef("root");
-        useViewButtons(this.model, rootRef, { beforeExecuteAction });
+        useViewButtons(this.model, rootRef, {
+            beforeExecuteAction: this.beforeExecuteActionButton.bind(this),
+            afterExecuteAction: this.afterExecuteActionButton.bind(this),
+        });
         useSetupView({
             rootRef,
             beforeLeave: () => {
@@ -353,6 +344,21 @@ export class FormController extends Component {
     enableButtons() {
         this.state.isDisabled = false;
     }
+
+    async beforeExecuteActionButton(clickParams) {
+        if (clickParams.special !== "cancel") {
+            return this.model.root.save({ stayInEdition: true }).then((saved) => {
+                if (saved && this.props.onSave) {
+                    this.props.onSave(this.model.root);
+                }
+                return saved;
+            });
+        } else if (this.props.onDiscard) {
+            this.props.onDiscard(this.model.root);
+        }
+    }
+
+    async afterExecuteActionButton(clickParams) {}
 
     async edit() {
         await this.model.root.switchMode("edit");
