65b3e551aee0ea451a7f48c6778ce78931bff3e8
Antoine Guenet <age@odoo.com>
2022-09-23 08:32:52 +0000

    [IMP] mail, mass_mailing, im_livechat: port emoji fields to OWL
    This ports the `text_emojis` and `char_emojis` fields to OWL.
    
    Part-of: odoo/odoo#100956

--- addons/mail/static/src/views/fields/emojis_char_field/emojis_char_field.js
+++ addons/mail/static/src/views/fields/emojis_char_field/emojis_char_field.js
@@ -1,16 +1,28 @@
 /** @odoo-module **/
 
+import { CharField } from "@web/views/fields/char/char_field";
+import { patch } from "@web/core/utils/patch";
 import MailEmojisMixin from '@mail/js/emojis_mixin';
-import EmojisFieldCommon from '@mail/views/fields/emojis_field_common';
+import { EmojisDropdown } from '@mail/js/emojis_dropdown';
+import { EmojisFieldCommon } from '@mail/views/fields/emojis_field_common';
+import { registry } from "@web/core/registry";
 
-import basicFields from 'web.basic_fields';
-import registry from 'web.field_registry';
+const { useRef } = owl;
 
 /**
  * Extension of the FieldChar that will add emojis support
  */
-var EmojisCharField = basicFields.FieldChar.extend(MailEmojisMixin, EmojisFieldCommon);
+export class EmojisCharField extends CharField {
+    setup() {
+        super.setup();
+        this.targetEditElement = useRef('input');
+        this._setupOverride();
+    }
+};
 
-registry.add('char_emojis', EmojisCharField);
-
-export default EmojisCharField;
+patch(EmojisCharField.prototype, 'emojis_char_field_mail_mixin', MailEmojisMixin);
+patch(EmojisCharField.prototype, 'emojis_char_field_field_mixin', EmojisFieldCommon);
+EmojisCharField.template = 'mail.EmojisCharField';
+EmojisCharField.components = { ...CharField.components, EmojisDropdown };
+EmojisCharField.additionalClasses = [...(CharField.additionalClasses || []), 'o_field_text'];
+registry.category("fields").add("char_emojis", EmojisCharField);
