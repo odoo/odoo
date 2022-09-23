b78385c02d21ff8c52bcaffd94bfab2f1bec8bfc
Antoine Guenet <age@odoo.com>
2022-09-23 08:59:34 +0000

    [IMP] sms, mass_mailing_sms : port sms widget field to OWL
    This ports the `sms_widget` field to OWL.
    
    closes odoo/odoo#101350
    
    X-original-commit: 89109de6d3752388f0b2f976c8a32588f760ec48
    Related: odoo/enterprise#31882
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/sms/static/tests/sms_widget_test.js
+++ addons/sms/static/tests/sms_widget_test.js
@@ -3,8 +3,10 @@
 import FormView from 'web.FormView';
 import ListView from 'web.ListView';
 import testUtils from 'web.test_utils';
+import { makeView, setupViewRegistries } from "@web/../tests/views/helpers";
+import { getFixture } from "@web/../tests/helpers/utils";
 
-var createView = testUtils.createView;
+const createView = testUtils.createView;
 
 QUnit.module('fields', {
     beforeEach: function () {
@@ -36,6 +38,8 @@ QUnit.module('fields', {
                 }]
             },
         };
+        setupViewRegistries();
+        this.target = getFixture();
     }
 }, function () {
 
@@ -43,84 +47,79 @@ QUnit.module('fields', {
 
     QUnit.test('Sms widgets are correctly rendered', async function (assert) {
         assert.expect(9);
-        var form = await createView({
-            View: FormView,
-            model: 'partner',
-            data: this.data,
-            arch: '<form><sheet><field name="message" widget="sms_widget"/></sheet></form>',
+        await makeView({
+            type: "form",
+            resModel: "partner",
+            serverData: { models: this.data },
+            arch: /* xml */ `<form><sheet><field name="message" widget="sms_widget"/></sheet></form>`,
         });
 
-        assert.containsOnce(form, '.o_sms_count', "Should have a sms counter");
-        assert.strictEqual(form.$('.o_sms_count').text(), '0 characters, fits in 0 SMS (GSM7) ',
+        assert.containsOnce(this.target, '.o_sms_count', "Should have a sms counter");
+        assert.strictEqual(this.target.querySelector('.o_sms_count').textContent, '0 characters, fits in 0 SMS (GSM7) ',
             'Should be "0 characters, fits in 0 SMS (GSM7) " by default');
         // GSM-7
-        await testUtils.fields.editAndTrigger(form.$('.o_input'), "Hello from Odoo", 'input');
-        assert.strictEqual(form.$('.o_sms_count').text(), '15 characters, fits in 1 SMS (GSM7) ',
+        await testUtils.fields.editAndTrigger(this.target.querySelector('.o_input'), "Hello from Odoo", 'input');
+        assert.strictEqual(this.target.querySelector('.o_sms_count').textContent, '15 characters, fits in 1 SMS (GSM7) ',
             'Should be "15 characters, fits in 1 SMS (GSM7) " for "Hello from Odoo"');
         // GSM-7 with \n => this one count as 2 characters
-        form.$('.o_input').val("Hello from Odoo\n").trigger('input');
-        assert.strictEqual(form.$('.o_sms_count').text(), '17 characters, fits in 1 SMS (GSM7) ',
+        await testUtils.fields.editAndTrigger(this.target.querySelector('.o_input'), "Hello from Odoo\n", 'input');
+        assert.strictEqual(this.target.querySelector('.o_sms_count').textContent, '17 characters, fits in 1 SMS (GSM7) ',
             'Should be "17 characters, fits in 1 SMS (GSM7) " for "Hello from Odoo\\n"');
         // Unicode => ê
-        form.$('.o_input').val("Hêllo from Odoo").trigger('input');
-        assert.strictEqual(form.$('.o_sms_count').text(), '15 characters, fits in 1 SMS (UNICODE) ',
+        await testUtils.fields.editAndTrigger(this.target.querySelector('.o_input'), "Hêllo from Odoo", 'input');
+        assert.strictEqual(this.target.querySelector('.o_sms_count').textContent, '15 characters, fits in 1 SMS (UNICODE) ',
             'Should be "15 characters, fits in 1 SMS (UNICODE) " for "Hêllo from Odoo"');
         // GSM-7 with 160c
         var text = Array(161).join('a');
-        await testUtils.fields.editAndTrigger(form.$('.o_input'), text, 'input');
-        assert.strictEqual(form.$('.o_sms_count').text(), '160 characters, fits in 1 SMS (GSM7) ',
+        await testUtils.fields.editAndTrigger(this.target.querySelector('.o_input'), text, 'input');
+        assert.strictEqual(this.target.querySelector('.o_sms_count').textContent, '160 characters, fits in 1 SMS (GSM7) ',
             'Should be "160 characters, fits in 1 SMS (GSM7) " for 160 x "a"');
         // GSM-7 with 161c
         text = Array(162).join('a');
-        await testUtils.fields.editAndTrigger(form.$('.o_input'), text, 'input');
-        assert.strictEqual(form.$('.o_sms_count').text(), '161 characters, fits in 2 SMS (GSM7) ',
+        await testUtils.fields.editAndTrigger(this.target.querySelector('.o_input'), text, 'input');
+        assert.strictEqual(this.target.querySelector('.o_sms_count').textContent, '161 characters, fits in 2 SMS (GSM7) ',
             'Should be "161 characters, fits in 2 SMS (GSM7) " for 161 x "a"');
         // Unicode with 70c
         text = Array(71).join('ê');
-        await testUtils.fields.editAndTrigger(form.$('.o_input'), text, 'input');
-        assert.strictEqual(form.$('.o_sms_count').text(), '70 characters, fits in 1 SMS (UNICODE) ',
+        await testUtils.fields.editAndTrigger(this.target.querySelector('.o_input'), text, 'input');
+        assert.strictEqual(this.target.querySelector('.o_sms_count').textContent, '70 characters, fits in 1 SMS (UNICODE) ',
             'Should be "70 characters, fits in 1 SMS (UNICODE) " for 70 x "ê"');
         // Unicode with 71c
         text = Array(72).join('ê');
-        await testUtils.fields.editAndTrigger(form.$('.o_input'), text, 'input');
-        assert.strictEqual(form.$('.o_sms_count').text(), '71 characters, fits in 2 SMS (UNICODE) ',
+        await testUtils.fields.editAndTrigger(this.target.querySelector('.o_input'), text, 'input');
+        assert.strictEqual(this.target.querySelector('.o_sms_count').textContent, '71 characters, fits in 2 SMS (UNICODE) ',
             'Should be "71 characters, fits in 2 SMS (UNICODE) " for 71 x "ê"');
 
-        form.destroy();
     });
 
     QUnit.test('Sms widgets with non-empty initial value', async function (assert) {
         assert.expect(1);
-        var form = await createView({
-            View: FormView,
-            model: 'visitor',
-            data: this.data,
-            arch: `<form><sheet><field name="mobile" widget="sms_widget"/></sheet></form>`,
-            res_id: 1,
-            res_ids: [1],
+        await makeView({
+            type: "form",
+            resModel: "visitor",
+            resId: 1,
+            serverData: { models: this.data },
+            arch: /* xml */ `<form><sheet><field name="mobile" widget="sms_widget" readonly="true"/></sheet></form>`,
         });
 
-        assert.strictEqual(form.$('.o_field_text').text(), '+32494444444',
+        assert.strictEqual(this.target.querySelector('.o_field_text span').textContent, '+32494444444',
             'Should have the initial value');
 
-        form.destroy();
     });
 
     QUnit.test('Sms widgets with empty initial value', async function (assert) {
         assert.expect(1);
-        var form = await createView({
-            View: FormView,
-            model: 'partner',
-            data: this.data,
-            arch: `<form><sheet><field name="message" widget="sms_widget"/></sheet></form>`,
-            res_id: 1,
-            res_ids: [1],
+        await makeView({
+            type: "form",
+            resModel: "partner",
+            resId: 1,
+            serverData: { models: this.data },
+            arch: /* xml */ `<form><sheet><field name="message" widget="sms_widget" readonly="true"/></sheet></form>`,
         });
 
-        assert.strictEqual(form.$('.o_field_text').text(), '',
+        assert.strictEqual(this.target.querySelector('.o_field_text span').textContent, '',
             'Should have the empty initial value');
 
-        form.destroy();
     });
 
     QUnit.module('PhoneWidget');
