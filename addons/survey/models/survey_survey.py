8c8b2c3fec9e81ed0a70690c107978fbab1bd51c
Thomas Josse (thjo) <thjo@odoo.com>
2022-09-27 12:23:39 +0200

    [IMP] survey: mail modal simplification
    Purpose
    =======
    
    Simplifying the mail modal used to share surveys
    
    Specifications
    ==============
    
    * We simplified the modal, you can hide all the parameters for the template creation
    * The dynamic values are displayed as computed in the modal
    
    task-2901947

--- addons/survey/models/survey_survey.py
+++ addons/survey/models/survey_survey.py
@@ -905,6 +905,7 @@ class Survey(models.Model):
             default_use_template=bool(template),
             default_template_id=template and template.id or False,
             default_email_layout_xmlid='mail.mail_notification_light',
+            default_send_email=(self.access_mode != 'public'),
         )
         return {
             'type': 'ir.actions.act_window',
