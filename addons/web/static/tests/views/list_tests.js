f5e4dd6b2877471c0d7d4743bc6e4753933c84a8
Hubert Van de Walle (huvw) <huvw@odoo.com>
2022-09-28 10:58:29 +0200

    [FIX] web: list view: don't rerender a row before the previous render is done
    Setup:
    
      - Add a checkbox to the sublist view of quotations with studio and save
      - In the chrome devtools, select a custom network throttling profile
        with 1s of latency
    
    Steps to reproduce:
    
      First example:
    
      - Go to a quotation with at least two lines
      - Enter edit mode
      - Change the quantity of the first line
      - Double click on the price field of the second line
      -> A traceback appears: `Cannot set properties of null (setting 'props')`
    
      Second example:
      - Go to a quotation with at least two lines
      - Enter edit mode
      - Change the quantity of the first line
      - Click on the price field of the second line
      - Click on Save
      -> The same traceback is there
    
    Cause of the issue:
    
      The checkbox field is implemented in owl and thus the
      owl_compatibility layer is used.
      Mounted is not called on the checkbox if a new rendering is being done
      cf: https://github.com/odoo/odoo/pull/75950
      Currently, it is possible to set a row mode before the previous call
      and rendering is done.
    
    Solution:
    
      Use a mutex for each record to prevent simultaneous render
    
    opw-2937444

--- addons/web/static/tests/views/list_tests.js
+++ addons/web/static/tests/views/list_tests.js
@@ -11811,6 +11811,54 @@ QUnit.module('Views', {
         list.destroy();
     });
 
+    QUnit.test('quickly setting row mode with owl_compatibility', async function (assert) {
+        assert.expect(3);
+
+        this.data.bar.fields.bool = { string: 'bool', type: 'boolean' };
+        this.data.foo.records[0].o2m = [1, 2, 3];
+
+        const form = await createView({
+            View: FormView,
+            model: 'foo',
+            data: this.data,
+            res_id: 1,
+            viewOptions: { mode: 'edit' },
+            arch: `<form>
+                <sheet>
+                    <notebook>
+                        <page>
+                            <field name="o2m">
+                                <tree editable="bottom">
+                                    <field name="bool"/>
+                                    <field name="display_name"/>
+                                </tree>
+                            </field>
+                        </page>
+                    </notebook>
+                </sheet>
+            </form>`,
+        });
+
+        await testUtils.dom.click(form.$('tbody tr:eq(0) td:eq(1)'));
+        await testUtils.fields.editInput(form.$('input[name=display_name]'), 'another value');
+
+        // Double-click on a field of the second row
+        testUtils.dom.click(form.$('tbody tr:eq(1) td:eq(1)'));
+        testUtils.dom.click(form.$('tbody tr:eq(1) td:eq(1)'));
+        await testUtils.nextTick();
+        await testUtils.nextTick();
+
+        assert.strictEqual(
+            form.el.querySelector('tr:nth-child(1) > td.o_data_cell.o_field_cell.o_list_char').textContent,
+            'another value');
+
+        const rows = form.$('.o_data_row .o_list_char');
+        assert.containsNone(rows[0], 'input'); // this means that the first row is saved
+        assert.containsOnce(rows[1], 'input'); // this means that the second row is in edit mode
+
+        form.destroy();
+    });
+
 });
 
 });
