e95aceddd2f2bdc4abc297a05ab0501a50fd7867
clesgow <quwo@odoo.com>
2022-09-28 13:52:18 +0200

    [FIX] stock_picking_batch: Add_to_batch wizard
    When selecting the "Add to batch" wizard, if a user selected "Add to an
    existing batch transfer", the only batches appearing there would be only
    draft batches, while it could be added to in_progress batches as well.
    It also displayed waves transfers with the batches, so it now only shows
    batches.
    
    Also restricted the creation of a batch in the related field since there
    is an explicit option to create a new batch transfer.
    
    Somewhat the same for the "Add to wave" wizard as it would display
    cancelled waves as well.
    
    Part of task-2985735

--- addons/stock_picking_batch/wizard/stock_picking_to_batch.py
+++ addons/stock_picking_batch/wizard/stock_picking_to_batch.py
@@ -9,7 +9,7 @@ class StockPickingToBatch(models.TransientModel):
     _name = 'stock.picking.to.batch'
     _description = 'Batch Transfer Lines'
 
-    batch_id = fields.Many2one('stock.picking.batch', string='Batch Transfer', domain="[('state', '=', 'draft')]")
+    batch_id = fields.Many2one('stock.picking.batch', string='Batch Transfer', domain="[('is_wave', '=', False), ('state', 'in', ('draft', 'in_progress'))]")
     mode = fields.Selection([('existing', 'an existing batch transfer'), ('new', 'a new batch transfer')], default='new')
     user_id = fields.Many2one('res.users', string='Responsible')
     is_create_draft = fields.Boolean(string="Draft", help='When checked, create the batch in draft status')
