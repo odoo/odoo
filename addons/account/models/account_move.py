edd9f11cabadf61869f83db0bcc638c9652dfea1
Nicolas (vin) <vin@odoo.com>
2022-09-28 13:09:00 +0200

    [FIX] account: do not check sequence name in an onchange
    If the sequence and the date do not match upon saving, it is an
    issue.
    But we should not block the user during the process by raising a
    warning when the sequence is changed before the date, if it's ok
    to change the date then the sequence.
    
    The onchange check on the sequence is thus removed, and we will rely
    on the error raised upon saving when the sequence and the date doesn't
    match to make sure the user does not break the sequencing.

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -1571,59 +1571,6 @@ class AccountMove(models.Model):
                     self.partner_id = False
                 return {'warning': warning}
 
-    @api.onchange('name', 'highest_name')
-    def _onchange_name_warning(self):
-        if self.name and self.name != '/' and self.name <= (self.highest_name or '') and not self.quick_edit_mode:
-            self.show_name_warning = True
-        else:
-            self.show_name_warning = False
-
-        origin_name = self._origin.name
-        if not origin_name or origin_name == '/':
-            origin_name = self.highest_name
-        if (
-            self.name and self.name != '/'
-            and origin_name and origin_name != '/'
-            and self.date == self._origin.date
-            and self.journal_id == self._origin.journal_id
-        ):
-            new_format, new_format_values = self._get_sequence_format_param(self.name)
-            origin_format, origin_format_values = self._get_sequence_format_param(origin_name)
-
-            if (
-                new_format != origin_format
-                or dict(new_format_values, seq=0) != dict(origin_format_values, seq=0)
-            ):
-                changed = _(
-                    "It was previously '%(previous)s' and it is now '%(current)s'.",
-                    previous=origin_name,
-                    current=self.name,
-                )
-                reset = self._deduce_sequence_number_reset(self.name)
-                if reset == 'month':
-                    detected = _(
-                        "The sequence will restart at 1 at the start of every month.\n"
-                        "The year detected here is '%(year)s' and the month is '%(month)s'.\n"
-                        "The incrementing number in this case is '%(formatted_seq)s'."
-                    )
-                elif reset == 'year':
-                    detected = _(
-                        "The sequence will restart at 1 at the start of every year.\n"
-                        "The year detected here is '%(year)s'.\n"
-                        "The incrementing number in this case is '%(formatted_seq)s'."
-                    )
-                else:
-                    detected = _(
-                        "The sequence will never restart.\n"
-                        "The incrementing number in this case is '%(formatted_seq)s'."
-                    )
-                new_format_values['formatted_seq'] = "{seq:0{seq_length}d}".format(**new_format_values)
-                detected = detected % new_format_values
-                return {'warning': {
-                    'title': _("The sequence format has changed."),
-                    'message': "%s\n\n%s" % (changed, detected)
-                }}
-
     @api.onchange('journal_id')
     def _onchange_journal_id(self):
         if not self.quick_edit_mode and self._get_last_sequence(lock=False):
@@ -2389,7 +2336,7 @@ class AccountMove(models.Model):
     # -------------------------------------------------------------------------
 
     def _must_check_constrains_date_sequence(self):
-        return not self.posted_before and not self.quick_edit_mode
+        return not self.posted_before or not self.quick_edit_mode
 
     def _get_last_sequence_domain(self, relaxed=False):
         # EXTENDS account sequence.mixin
