d5bfc839df77ed6474969282c1cf734ce071a9d2
John Laterre (jol) <jol@odoo.com>
2022-09-28 14:46:34 +0000

    [FIX] account: automatically create balancing lines on entries
    Currently, in a Journal Entry, applying a tax on any journal item doesn't compute the corresponding tax line.
    
    We end up in a situation where the move appears balanced but doesn't take into account the taxes applied. Moreover, the move cannot be posted.
    
    The solution involves several steps:
    - Changing a tax on a line now triggers a save.
    - Changing the debit or credit on a line with taxes will also trigger a save.
    - The company's suspense account is used for the balancing line.
    
    During the save, a check is added to see if the move is unbalanced.
    In that case, a balancing line will automatically be created to ensure the move can be saved.
    
    X-original-commit: a3e9ca9586cba9d6161d4c05a7bfff9beeeddea7
    Part-of: odoo/odoo#102487

--- addons/account/tests/test_account_move_entry.py
+++ addons/account/tests/test_account_move_entry.py
@@ -393,24 +393,6 @@ class TestAccountMove(AccountTestInvoicingCommon):
         with self.assertRaises(UserError), self.cr.savepoint():
             draft_moves.unlink()
 
-    def test_misc_always_balanced_move(self):
-        ''' Ensure there is no way to make '''
-        # You can't remove a journal item making the journal entry unbalanced.
-        with self.assertRaises(UserError), self.cr.savepoint():
-            self.test_move.line_ids[0].unlink()
-
-        # Same check using write instead of unlink.
-        with self.assertRaises(UserError), self.cr.savepoint():
-            balance = self.test_move.line_ids[0].balance + 5
-            self.test_move.line_ids[0].write({
-                'debit': balance if balance > 0.0 else 0.0,
-                'credit': -balance if balance < 0.0 else 0.0,
-            })
-
-        # You can remove journal items if the related journal entry is still balanced.
-        self.test_move.line_ids.filtered(lambda l: not l.tax_repartition_line_id).balance = 0
-        self.test_move.line_ids[0].unlink()
-
     def test_add_followers_on_post(self):
         # Add some existing partners, some from another company
         company = self.env['res.company'].create({'name': 'Oopo'})
