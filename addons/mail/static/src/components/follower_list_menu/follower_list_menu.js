e9e2de2c43c3b939a21b4a8f396413f82d3d4548
Alexandre Kühn <aku@odoo.com>
2022-09-28 14:12:52 +0000

    [IMP] mail: move code to models (FollowerListMenu)
    Task-3000317
    
    closes odoo/odoo#101604
    
    X-original-commit: 89e60f414b88def011dc5b8b63444b12edb5d824
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>

--- addons/mail/static/src/components/follower_list_menu/follower_list_menu.js
+++ addons/mail/static/src/components/follower_list_menu/follower_list_menu.js
@@ -1,8 +1,9 @@
 /** @odoo-module **/
 
+import { useRefToModel } from '@mail/component_hooks/use_ref_to_model';
 import { registerMessagingComponent } from '@mail/utils/messaging_component';
 
-const { Component, onMounted, onWillUnmount, useRef } = owl;
+const { Component } = owl;
 
 export class FollowerListMenu extends Component {
 
@@ -11,18 +12,7 @@ export class FollowerListMenu extends Component {
      */
     setup() {
         super.setup();
-        this._dropdownRef = useRef('dropdown');
-        this._onClickCaptureGlobal = this._onClickCaptureGlobal.bind(this);
-        onMounted(() => this._mounted());
-        onWillUnmount(() => this._willUnmount());
-    }
-
-    _mounted() {
-        document.addEventListener('click', this._onClickCaptureGlobal, true);
-    }
-
-    _willUnmount() {
-        document.removeEventListener('click', this._onClickCaptureGlobal, true);
+        useRefToModel({ fieldName: 'dropdownRef', refName: 'dropdown' });
     }
 
     //--------------------------------------------------------------------------
@@ -34,36 +24,6 @@ export class FollowerListMenu extends Component {
      */
     get followerListMenuView() {
         return this.props.record;
-    }
-
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
-
-    /**
-     * @private
-     * @param {MouseEvent} ev
-     */
-    _onClickAddFollowers(ev) {
-        ev.preventDefault();
-        this.followerListMenuView.hide();
-        this.followerListMenuView.chatterOwner.promptAddPartnerFollower();
-    }
-
-    /**
-     * Close the dropdown when clicking outside of it.
-     *
-     * @private
-     * @param {MouseEvent} ev
-     */
-    _onClickCaptureGlobal(ev) {
-        if (!this.followerListMenuView.exists()) {
-            return;
-        }
-        // since dropdown is conditionally shown based on state, dropdownRef can be null
-        if (this._dropdownRef.el && !this._dropdownRef.el.contains(ev.target)) {
-            this.followerListMenuView.hide();
-        }
     }
 
 }
