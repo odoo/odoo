636a8276e6797c1d8fe43da61d2fd26b558bfa45
Dossogne Bertrand <bedo@odoo.com>
2022-09-29 11:37:37 +0000

    [FIX] hr_holidays: fix multiple-employee leave
    Before this commit, creating a leave for multiple employees
    from the manager's page allowed to create leaves regardless
    of the concerned employee's allocations.
    After this commit, an error message will be displayed to inform
    which employee cannot take that leave.
    
    Also removes unused imports.
    
    task-2995120
    
    closes odoo/odoo#103639
    
    X-original-commit: b1f6cc470271a74eaf43abc003e1977b828264b1
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_work_entry_holidays/tests/test_performance.py
+++ addons/hr_work_entry_holidays/tests/test_performance.py
@@ -56,7 +56,7 @@ class TestWorkEntryHolidaysPerformance(TestWorkEntryHolidaysBase):
     def test_performance_leave_confirm(self):
         leave = self.create_leave(datetime(2018, 1, 1, 7, 0), datetime(2018, 1, 1, 18, 0))
         leave.action_draft()
-        with self.assertQueryCount(__system__=24, admin=25):
+        with self.assertQueryCount(__system__=25, admin=26):
             leave.action_confirm()
         leave.state = 'refuse'
 
