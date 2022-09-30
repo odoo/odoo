a569305acaa1c812ede739c267e2e9530945f65f
Simon Genin (ges) <ges@odoo.com>
2022-09-30 13:12:38 +0000

    [FIX] web,*: fix kanban useSortable screen width limit
    Reproduce:
    Go on a kanban view, with enough columns so it goes off the screens.
    Scroll on the right, try to drag anything beyond the original (before
    scrolling) limit of the screen. The cards are stuck floating at that
    limit. Note it didn't prevent the drop to work properly where the mouse
    was.
    
    Issue:
    The useSortable hook gets an owl ref, which is used as a limit to where
    things can be dragged and dropped. Originaly and logically, it was the
    current component ref, the renderer. But for some unknown reason, this
    space was not getting wider than the initial screen size. It may be a
    problem with the flexbox layout.
    
    Tried solutions:
    - A first solution was to add the overflow-x-scroll css property to the
    renderer. It works, but now the horizontal scroll bar is no longer
    always visible. You have to scroll all the way down to see it. UX wise
    this is not acceptable.
    
    - We could remove the clamp mecanism inside useSortable to let the drag
    and drop motion go anywhere. This works, but decrease the quality of the
    interaction. It looks cheap and not polished.
    
    Final Solution
    Finally, we decided to pass a ref to the o_content div from the layout
    component down to the kanban renderer. If the useSortable is given this
    ref, it works as intended. While it may seem too much, it is not
    unreasonable to propose a reference to the most parent element of the
    view contents to the renderer.
    
    Part-of: odoo/odoo#101763

--- addons/web/static/src/views/kanban/kanban_renderer.js
+++ addons/web/static/src/views/kanban/kanban_renderer.js
@@ -45,7 +45,7 @@ export class KanbanRenderer extends Component {
             useSortable({
                 enable: () => this.canResequenceRecords,
                 // Params
-                ref: rootRef,
+                ref: this.props.layoutContentRef || rootRef,
                 elements: ".o_record_draggable",
                 ignore: ".dropdown",
                 groups: () => this.props.list.isGrouped && ".o_kanban_group",
@@ -606,6 +606,7 @@ KanbanRenderer.props = [
     "readonly",
     "forceGlobalClick?",
     "noContentHelp?",
+    "layoutContentRef?",
 ];
 KanbanRenderer.components = {
     Dropdown,
