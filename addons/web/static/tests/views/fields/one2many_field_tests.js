6d63d1035b28f2004774a99a7be84b92a1cb1883
clesgow <quwo@odoo.com>
2022-09-30 12:07:45 +0000

    [FIX] web: relational_utils save & new
    As of now, if a user clicks on the "Save & New" button while the form is
    not valid, it will disable all buttons without any options to make them
    enabled again. The only solution then is to close the form and re-open
    it.
    
    Steps to reproduce:
    Inventory -> Configuration -> Route -> Select any route -> Add a line to
    the rules.
    As soon as you click on the "Save & New" button and the form isn't
    valid, you can't use any buttons anymore.
    
    Part of task-2985735
    
    closes odoo/odoo#101748
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/fields/one2many_field_tests.js
+++ addons/web/static/tests/views/fields/one2many_field_tests.js
@@ -12543,4 +12543,36 @@ QUnit.module("Fields", (hooks) => {
         assert.containsNone(target, ".o_list_record_remove");
         assert.hasClass(target.querySelector(".o_data_row:first-child"), "o_selected_row");
     });
+
+    QUnit.test('Add a line, click on "Save & New" with an invalid form', async function (assert) {
+        await makeView({
+            type: "form",
+            resModel: "partner",
+            serverData,
+            arch: `
+                <form>
+                    <field name="p">
+                        <tree>
+                            <field name="display_name"/>
+                        </tree>
+                        <form>
+                            <field name="display_name" required="1"/>
+                        </form>
+                    </field>
+                </form>`,
+        });
+
+        assert.containsNone(target, ".o_data_row");
+        // Add a new record
+        await addRow(target);
+        assert.containsOnce(target, ".o_dialog .o_form_view");
+
+        // Click on "Save & New" with an invalid form 
+        await click(target, ".o_dialog .o_form_button_save_new");
+        assert.containsOnce(target, ".o_dialog .o_form_view");
+
+        // Check that no buttons are disabled
+        assert.hasAttrValue(target.querySelector(".o_dialog .o_form_button_save_new"), "disabled", undefined);
+        assert.hasAttrValue(target.querySelector(".o_dialog .o_form_button_cancel"), "disabled", undefined);
+    });
 });
