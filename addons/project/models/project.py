09cfc46f048c42317026310686d0a13b45648e45
Raouf <abgh@odoo.com>
2022-09-30 14:40:00 +0000

    [FIX] project: add missing stage when import tasks
    Before this commit:
    Let's consider project P1 having no stages.
    Let's consider project P2 having Stage 1, Stage 2
    When importing tasks via csv file:
    - Task 1 for project P1 and Stage 1
    - Task 2 for project P1 and Stage 2
    
    Stage 1 and Stage 2 are still assigned only to project P1.
    
    After this commit:
    Stage 1 and Stage 2 are now assigned to P1 and P2.
    
    Task-2996393
    
    X-original-commit: a51058d17ad70764f795e272d6578b33842dba1c

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -1683,7 +1683,16 @@ class Task(models.Model):
                 else:
                     for field_name in self._get_recurrence_fields() + ['recurring_task']:
                         vals.pop(field_name, None)
-        return super()._load_records_create(vals_list)
+        tasks = super()._load_records_create(vals_list)
+        stage_ids_per_project = defaultdict(list)
+        for task in tasks:
+            if task.stage_id and task.stage_id not in task.project_id.type_ids and task.stage_id.id not in stage_ids_per_project[task.project_id]:
+                stage_ids_per_project[task.project_id].append(task.stage_id.id)
+
+        for project, stage_ids in stage_ids_per_project.items():
+            project.write({'type_ids': [Command.link(stage_id) for stage_id in stage_ids]})
+
+        return tasks
 
     @api.model_create_multi
     def create(self, vals_list):
