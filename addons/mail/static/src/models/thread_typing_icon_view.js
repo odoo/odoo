4d14d9ec02a3d503eeb70c52c77719d6e4c390da
Louis Wicket (wil) <wil@odoo.com>
2022-10-03 09:35:33 +0200

    [IMP] mail, *: move code from components to models (ThreadTypingIcon)
    *: im_livechat
    
    Task-3004018.

---
+++ addons/mail/static/src/models/thread_typing_icon_view.js
@@ -0,0 +1,37 @@
+/** @odoo-module **/
+
+import { registerModel } from '@mail/model/model_core';
+import { attr, one } from '@mail/model/model_field';
+import { clear } from '@mail/model/model_field_command';
+
+registerModel({
+    name: 'ThreadTypingIconView',
+    identifyingMode: 'xor',
+    fields: {
+        size: attr({
+            default: 'small',
+            compute() {
+                if (this.threadTextualTypingStatusViewOwner) {
+                    return 'medium';
+                }
+                return clear();
+            },
+        }),
+        threadIconViewOwner: one('ThreadIconView', {
+            identifying: true,
+            inverse: 'threadTypingIconView',
+        }),
+        threadTextualTypingStatusViewOwner: one('ThreadTextualTypingStatusView', {
+            identifying: true,
+            inverse: 'threadTypingIconView',
+        }),
+        title: attr({
+            compute() {
+                if (this.threadIconViewOwner) {
+                    return this.threadIconViewOwner.thread.typingStatusText;
+                }
+                return clear();
+            },
+        }),
+    },
+});
