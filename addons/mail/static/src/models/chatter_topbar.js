babf7634e0d431471e5b708b937407c8b731b77b
stefanorigano (SRI) <sri@odoo.com>
2022-10-03 10:49:11 +0000

    [IMP] mail: `topChatter`, review design
    task-3005130
    
    Part-of: odoo/odoo#101979

--- addons/mail/static/src/models/chatter_topbar.js
+++ addons/mail/static/src/models/chatter_topbar.js
@@ -2,8 +2,7 @@
 
 import { registerModel } from '@mail/model/model_core';
 import { attr, one } from '@mail/model/model_field';
-
-import { sprintf } from '@web/core/utils/strings';
+import { clear } from '@mail/model/model_field_command';
 
 registerModel({
     name: 'ChatterTopbar',
@@ -14,23 +13,15 @@ registerModel({
         attachmentButtonText: attr({
             compute() {
                 if (!this.chatter || !this.chatter.thread) {
-                    return;
+                    return clear();
                 }
                 const attachments = this.chatter.thread.allAttachments;
-                switch (attachments.length) {
-                    case 0:
-                        return this.env._t("Attach files");
-                    case 1:
-                        return this.env._t("1 file");
-                    case 2:
-                        return this.env._t("2 files");
-                    default:
-                        return sprintf(
-                            this.env._t("%(attachmentCount)s files"),
-                            { attachmentCount: attachments.length }
-                        );
+                if (attachments.length === 0) {
+                    return clear();
                 }
+                return attachments.length;
             },
+            default: "",
         }),
         chatter: one('Chatter', {
             identifying: true,
