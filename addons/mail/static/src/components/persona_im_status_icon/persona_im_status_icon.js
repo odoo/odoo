c90e18c3dffd2bcd3f66a13a4617d76b1a09f24d
Louis Wicket (wil) <wil@odoo.com>
2022-10-03 12:53:26 +0000

    [IMP] mail: move code from components to models (PersonaImStatusIcon)
    + Turn props on component into fields on PersonImStatusIconView model
    + Move handler from component methods to model record methods
    
    Task-3004188.
    
    closes odoo/odoo#101943
    
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>

--- addons/mail/static/src/components/persona_im_status_icon/persona_im_status_icon.js
+++ addons/mail/static/src/components/persona_im_status_icon/persona_im_status_icon.js
@@ -1,15 +1,10 @@
 /** @odoo-module **/
 
 import { registerMessagingComponent } from '@mail/utils/messaging_component';
-import { markEventHandled } from '@mail/utils/utils';
 
 const { Component } = owl;
 
 export class PersonaImStatusIcon extends Component {
-
-    //--------------------------------------------------------------------------
-    // Public
-    //--------------------------------------------------------------------------
 
     /**
      * @returns {PersonaImStatusIconView}
@@ -18,38 +13,10 @@ export class PersonaImStatusIcon extends Component {
         return this.props.record;
     }
 
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
-
-    /**
-     * @private
-     * @param {MouseEvent} ev
-     */
-    _onClick(ev) {
-        markEventHandled(ev, 'PersonaImStatusIcon.Click');
-        if (!this.props.hasOpenChat || !this.personaImStatusIconView.persona.partner) {
-            return;
-        }
-        this.personaImStatusIconView.persona.partner.openChat();
-    }
-
 }
 
 Object.assign(PersonaImStatusIcon, {
-    defaultProps: {
-        hasBackground: true,
-        hasOpenChat: false,
-    },
-    props: {
-        hasBackground: { type: Boolean, optional: true },
-        /**
-         * Determines whether a click on `this` should open a chat with
-         * `this.partner`.
-         */
-        hasOpenChat: { type: Boolean, optional: true },
-        record: Object,
-    },
+    props: { record: Object },
     template: 'mail.PersonaImStatusIcon',
 });
 
