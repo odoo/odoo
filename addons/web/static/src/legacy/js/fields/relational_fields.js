47c54d68755d5d5d75607e16fd861e98015751fe
Renaud Thiry <reth@odoo.com>
2022-10-03 07:29:50 +0000

    [FIX] web_editor: onAttachmentChange only composer
    Due to a previous fix, attachments uploaded through media dialog would appear in the attachments of mail marketting.
    That fix prevents attachments from 'dangling' and being garbage collected later.
    
    That fix is now limited to the mail composer in this commit as attachments are only garbage collected for that model, for now.
    
    related commit: c112361bf9e2f5e7b087c5e5b9a31879856b1da4
    
    task 3003939
    
    X-original-commit: 07eea72ddf3a350f39900f14aab3738e32dbbd7f

--- addons/web/static/src/legacy/js/fields/relational_fields.js
+++ addons/web/static/src/legacy/js/fields/relational_fields.js
@@ -2348,8 +2348,6 @@ var FieldMany2ManyBinaryMultiFiles = AbstractField.extend({
     fieldsToFetch: {
         name: {type: 'char'},
         mimetype: {type: 'char'},
-        res_id: {type: 'number'},
-        access_token: {type: 'char'},
     },
     events: {
         'click .o_attach': '_onAttach',
