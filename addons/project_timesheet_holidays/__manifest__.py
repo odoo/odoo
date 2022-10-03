1f3a392ffa3cae88afcd323e20808e68367c2a5d
Kartik Chavda <kcv@odoo.com>
2022-10-03 05:33:23 +0000

    [IMP] project_timesheet_holidys: make timesheet page readonly for time off tasks
    This commit make timesheet page of tasks related to time off types readonly
    so user can not create,update or delete timesheets create by leaves
    
    task-2888607
    
    closes odoo/odoo#96359
    
    Related: odoo/enterprise#29678
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- addons/project_timesheet_holidays/__manifest__.py
+++ addons/project_timesheet_holidays/__manifest__.py
@@ -17,6 +17,7 @@ on leaves. Project and task can be configured company-wide.
     'data': [
         'views/res_config_settings_views.xml',
         'views/hr_holidays_views.xml',
+        'views/project_task_views.xml',
         'security/ir.model.access.csv',
 
     ],
