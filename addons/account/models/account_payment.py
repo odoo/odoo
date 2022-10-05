5fb4879b0130e89cb7ccb402573e0242dc259f31
Thomas Lefebvre (thle) <thle@odoo.com>
2022-10-05 11:44:15 +0000

    [FIX] account: overwrite journal accounts of a payment
    Steps to reproduce:
    	- create two journals, A and B, which have the default accounts defined in the payment method manual in "outstanding receipts accounts".
        - create a payment with a journal A;
        - duplicate the draft of the payment (because it is not possible to change a journal if it was posted before);
        - select the journal B and save (or confirm)
        - The journal is changed, but the default journal B accounts are not applied.
    
    Issue:
       Despite the payment is in draft and has not been posted before, move_line accounts do not change with the journal selected for the move.
    
    Cause:
       When a payment is duplicate, records are save in the database.
       Modify the journal en then save will trigger the write method of the payment model (and not the create).
       During this method, we begin to find the move lines which correspond to liquidity, counterpart and writeoff lines.
       Unfortunately, the line corresponding to the old journal will be detected as a writeoff and not as a liquidity line (because the journal has changed).
       This fault will cause a balance error in the rest of the procedure.
    
    Solution:
    	Take into account the case where writeoffs are detected when there is no liquidity line (and no counterpart line).
    	In this case, force the no writeoff line.
    	Generate the correct ORM commands taking in consideration if the line exist (1: update) or not (0: create).
    
    opw-2998031
    
    closes odoo/odoo#103003
    
    X-original-commit: ab1fa20de84ed8ab92edea9ee5f9cd9b5605e443
    Signed-off-by: Laurent Smet <las@odoo.com>
    Signed-off-by: Lefebvre Thomas (thle) <thle@odoo.com>

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
+from odoo import models, fields, api, Command, _
 from odoo.exceptions import UserError, ValidationError
 
 
@@ -822,7 +822,7 @@ class AccountPayment(models.Model):
 
         if not any(field_name in changed_fields for field_name in (
             'date', 'amount', 'payment_type', 'partner_type', 'payment_reference', 'is_internal_transfer',
-            'currency_id', 'partner_id', 'destination_account_id', 'partner_bank_id',
+            'currency_id', 'partner_id', 'destination_account_id', 'partner_bank_id', 'journal_id'
         )):
             return
 
@@ -832,7 +832,7 @@ class AccountPayment(models.Model):
             # Make sure to preserve the write-off amount.
             # This allows to create a new payment with custom 'line_ids'.
 
-            if writeoff_lines:
+            if liquidity_lines and counterpart_lines and writeoff_lines:
                 counterpart_amount = sum(counterpart_lines.mapped('amount_currency'))
                 writeoff_amount = sum(writeoff_lines.mapped('amount_currency'))
 
@@ -856,8 +856,8 @@ class AccountPayment(models.Model):
             line_vals_list = pay._prepare_move_line_default_vals(write_off_line_vals=write_off_line_vals)
 
             line_ids_commands = [
-                (1, liquidity_lines.id, line_vals_list[0]),
-                (1, counterpart_lines.id, line_vals_list[1]),
+                Command.update(liquidity_lines.id, line_vals_list[0]) if liquidity_lines else Command.create(line_vals_list[0]),
+                Command.update(counterpart_lines.id, line_vals_list[1]) if counterpart_lines else Command.create(line_vals_list[1])
             ]
 
             for line in writeoff_lines:
