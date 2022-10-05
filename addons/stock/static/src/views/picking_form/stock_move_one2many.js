9f9d68d163b49292918222ad1d1d3c283ca646a2
Arnold Moyaux <arm@odoo.com>
2022-10-05 10:34:40 +0200

    [IMP] stock: quantity done on stock.move is always editable
    Quantity done will be always editable. Different use case:
    1. If there is not location/package/tracing/owner -> same behavior than
      before
    2. If the details are needed:
    - Immediate transfer. In case of increase the quants are reserved and the
    `qty_done` on `stock.move.line` is increased depending the reservation.
    If the quantity couldn't be fully reserve then default `stock.move.line`
    are create (depending the putaway rules or multiple ones if the products
    are tracked). In case of decrease, the quantity reserved are unreserve
    but the `stock.move.line` are not unlink until the validation.
    - Planned transfer. The quantity done will fill the reservation. If the
    picking is not assign, it will reserve the quantity needed. In case of
    decrease, it will only unreserve the extra quantity (compare to the
    initial demand). To unreserve the quantity bellow the intial demand, the
    `stock.move.line` could be unlink or use the `unreserve` button.
    
    force_qty in `_action_assign` is needed because we don't reserve only
    base on the initial demand (`product_uom_qty`) but sometimes on the
    quantity_done.
    
    In order to never block the user. The action_show_details is always
    available. If the id is not available, it will save and open the record
    (we take the record based on the new existing line with same product
    and highest id).
    
    task-id: 2960983

---
+++ addons/stock/static/src/views/picking_form/stock_move_one2many.js
@@ -0,0 +1,33 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { ListRenderer } from "@web/views/list/list_renderer";
+import { X2ManyField } from "@web/views/fields/x2many/x2many_field";
+import { ViewButton } from "@web/views/view_button/view_button";
+
+class MoveViewButton extends ViewButton {
+    async onClick(ev) {
+        if (this.props.clickParams.name != "action_show_details") {
+            super.onClick(ev);
+        } else {
+            await this.props.record.saveAndOpenDetails();
+        }
+    }
+
+    get disabled() {
+        if (this.props.clickParams.name == "action_show_details") {
+            return false;
+        }
+        return super.disabled;
+    }
+}
+
+MoveViewButton.props = [...ViewButton.props];
+export class MovesListRenderer extends ListRenderer {}
+
+MovesListRenderer.components = { ...ListRenderer.components, ViewButton: MoveViewButton };
+
+export class StockMoveX2ManyField extends X2ManyField {}
+StockMoveX2ManyField.components = { ...X2ManyField.components, ListRenderer: MovesListRenderer };
+
+registry.category("fields").add("stock_move_one2many", StockMoveX2ManyField);
