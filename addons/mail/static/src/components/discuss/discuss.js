99521052b1b3650de9d529c1cfb26fcc9e46fc64
Didier (did) <did@odoo.com>
2022-10-06 10:38:10 +0000

    [IMP] mail: convert LegacyComponent discuss to Component
    part of task-2783065
    
    closes odoo/odoo#102388
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/components/discuss/discuss.js
+++ addons/mail/static/src/components/discuss/discuss.js
@@ -1,9 +1,10 @@
 /** @odoo-module **/
 
 import { registerMessagingComponent } from '@mail/utils/messaging_component';
-import { LegacyComponent } from '@web/legacy/legacy_component';
 
-export class Discuss extends LegacyComponent {
+const { Component } = owl;
+
+export class Discuss extends Component {
 
     //--------------------------------------------------------------------------
     // Public
