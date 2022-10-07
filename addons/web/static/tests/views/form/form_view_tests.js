3e92c60034cedcd999572bb7e93c8b05ca07be70
Mathieu Duckerts-Antoine <dam@odoo.com>
2022-10-07 14:55:27 +0000

    [REF] web: remove PieChart
    Now that the module web_dashboard has been removed https://github.com/odoo/enterprise/pull/31641,
    the component PieChart is unused. We remove it.
    
    closes odoo/odoo#102665
    
    Related: odoo/enterprise#32520
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/form/form_view_tests.js
+++ addons/web/static/tests/views/form/form_view_tests.js
@@ -9290,7 +9290,10 @@ QUnit.module("Views", (hooks) => {
                 },
             });
 
-            assert.hasClass(target.querySelector('button[data-value="4"]'), "o_arrow_button_current");
+            assert.hasClass(
+                target.querySelector('button[data-value="4"]'),
+                "o_arrow_button_current"
+            );
             assert.hasClass(target.querySelector('button[data-value="4"]'), "disabled");
 
             failFlag = true;
@@ -11102,32 +11105,6 @@ QUnit.module("Views", (hooks) => {
             );
         }
     );
-
-    QUnit.test("reload a form view with a pie chart does not crash", async function (assert) {
-        await makeView({
-            type: "form",
-            resModel: "partner",
-            serverData,
-            arch: `
-                <form>
-                    <widget name="pie_chart" title="qux by product" attrs="{'measure': 'qux', 'groupby': 'product_id'}"/>
-                </form>`,
-            resIds: [1, 2],
-            resId: 1,
-        });
-
-        assert.containsOnce(
-            target,
-            ".o_widget_pie_chart .o_graph_canvas_container .chartjs-render-monitor"
-        );
-
-        await click(target.querySelector(".o_pager_next"));
-
-        assert.containsOnce(
-            target,
-            ".o_widget_pie_chart .o_graph_canvas_container .chartjs-render-monitor"
-        );
-    });
 
     QUnit.test("Auto save: save when page changed", async function (assert) {
         assert.expect(10);
