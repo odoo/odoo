1651eaceb94a43f733118dced0d616271742d3a2
stefanorigano (SRI) <sri@odoo.com>
2022-10-07 11:29:34 +0200

    [IMP] web, base: adapt for dark-mode
    This commit adapts several components in order to correctly handle
    color-scheme variations.
    
    It also allows user_menu to handle switch entries 
    
    task-2710677

--- addons/web/static/src/views/graph/colors.js
+++ addons/web/static/src/views/graph/colors.js
@@ -1,6 +1,8 @@
 /** @odoo-module **/
 
-export const COLORS = [
+import { getCookie } from "web.utils.cookies";
+
+const COLORS_BRIGHT = [
     "#1f77b4",
     "#ff7f0e",
     "#aec7e8",
@@ -11,17 +13,42 @@ export const COLORS = [
     "#ff9896",
     "#9467bd",
     "#c5b0d5",
-    "#8c564b",
+    "#875a7b", // ~ Enterprise
     "#c49c94",
     "#e377c2",
-    "#f7b6d2",
+    "#dcd0d9", // Dashboards Primary
     "#7f7f7f",
     "#c7c7c7",
     "#bcbd22",
     "#dbdb8d",
     "#17becf",
-    "#9edae5",
+    "#a5d8d7", // Dashboards Secondary
 ];
+
+const COLORS_DARK = [
+    "#00ffff",
+    "#ff6347",
+    "#00ced1",
+    "#ffd700",
+    "#29ef29",
+    "#c5fabb",
+    "#fe4b4c",
+    "#ffb6c1",
+    "#ba87e9",
+    "#eadbf6",
+    "#c568af", // ~ Enterprise
+    "#ecc1b8",
+    "#fda9e3",
+    "#BB86FC", // Dashboards Primary
+    "#808080",
+    "#f2e8e8",
+    "#fcfe2d",
+    "#f8f8bc",
+    "#17becf",
+    "#10efed", // Dashboards Secondary
+];
+
+export const COLORS = getCookie("color_scheme") === "dark" ? COLORS_DARK : COLORS_BRIGHT;
 
 /**
  * @param {number} index
@@ -33,7 +60,8 @@ export function getColor(index) {
 
 export const DEFAULT_BG = "#d3d3d3";
 
-export const BORDER_WHITE = "rgba(255,255,255,0.6)";
+export const BORDER_WHITE =
+    getCookie("color_scheme") === "dark" ? "rgba(0, 0, 0, 0.6)" : "rgba(255,255,255,0.6)";
 
 const RGB_REGEX = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
 
