6a60a62372e0fe4170055e5b4b794310c02b346e
Romain Derie <rde@odoo.com>
2022-10-07 16:19:22 +0000

    [REM] website, website_blog: remove the search for key dependencies
    This is basically reverting commit [1] which was done a few years ago
    as improvement to [2], but it was a bit overkill IMHO.
    
    Indeed, there is a sort of hack when creating a page with special
    extension like "my-page.js" that will actually create a special page
    with special arch. The goal is for such pages to be `t-call`ed later.
    That's commit [2].
    
    On top of that, when deleting such a special page, we introduced a
    mechanism to show the views and pages that would possibly `t-call` the
    that page which is requested to be deleted.
    That's commit [1].
    It basically is mimicking what was already done when you change the URL
    of a normal page, we tell the user where that url is actually possibly
    used.
    
    But since the recent improvement in Odoo 16 done at [3], the page
    manager is now a backend list view. It means that multi delete is now a
    thing.
    Thus, the page dependencies behaviors (key and url) need to be
    refactored to handle multiple given URL/Key and not just one.
    We choose to remove the key dependencies part (only used for those
    special pages) instead of adapting it:
    - That 'hack' is probably almost never used
    - It's some code to maintain, eg now we need to refactor it
    - We would need some extra code to make it only triggered for website
      pages and not all records, unlike the url dependencies screen which
      concerns all records
    - That's an advanced feature (special pages), if you are using it, you
      probably knows how to handle a website and you don't need us to remind
      you where this special page's view is used.
    
    [1]: https://github.com/odoo/odoo/commit/a91a3a563338e746d6d23cd57551af30e322e367
    [2]: https://github.com/odoo/odoo/commit/727d461e1d2bcec4571665b90b6d1630f671a0a3
    [3]: https://github.com/odoo/odoo/commit/31cc10b91dc7762e23b4bde9b945be0c4ce3fe3b
    
    X-original-commit: 77f03abc56b147171eeba66bc55df5ee92c8af1a
    Part-of: odoo/odoo#103136

--- addons/website_blog/models/website.py
+++ addons/website_blog/models/website.py
@@ -40,31 +40,6 @@ class Website(models.Model):
 
         return dep
 
-    @api.model
-    def page_search_key_dependencies(self, page_id=False):
-        dep = super(Website, self).page_search_key_dependencies(page_id=page_id)
-
-        page = self.env['website.page'].browse(int(page_id))
-        key = page.key
-
-        dom = [
-            ('content', 'ilike', key)
-        ]
-        posts = self.env['blog.post'].search(dom)
-        if posts:
-            page_key = _('Blog Post')
-            if len(posts) > 1:
-                page_key = _('Blog Posts')
-            dep[page_key] = []
-        for p in posts:
-            dep[page_key].append({
-                'content': Markup(_('Blog Post <b>%s</b> seems to be calling this file !', p.name)),
-                'item': p.name,
-                'link': p.website_url,
-            })
-
-        return dep
-
     def get_suggested_controllers(self):
         suggested_controllers = super(Website, self).get_suggested_controllers()
         suggested_controllers.append((_('Blog'), url_for('/blog'), 'website_blog'))
