6d91f6b4f485a51be99bde640114496e0052eb5e
Julien Van Roy <juvr@odoo.com>
2022-10-07 14:36:05 +0000

    [FIX] account: products domain income/expense accounts
    For the product, the default income and expense accounts should
    only contain the income/expense accounts.
    Such that the dropdown menu in the product view only lists the possible
    income or expense accounts. Adapt the domain on the product and product
    template fields.
    
    closes odoo/odoo#102661
    
    Signed-off-by: Laurent Smet <las@odoo.com>

--- addons/account/models/product.py
+++ addons/account/models/product.py
@@ -2,18 +2,18 @@
 
 from odoo import api, fields, models, _
 
-ACCOUNT_DOMAIN = "['&', '&', '&', ('deprecated', '=', False), ('internal_type','=','other'), ('company_id', '=', current_company_id), ('is_off_balance', '=', False)]"
+ACCOUNT_DOMAIN = "[('deprecated', '=', False), ('internal_type','=','other'), ('company_id', '=', current_company_id), ('is_off_balance', '=', False), ('internal_group', '=', '%s')]"
 
 class ProductCategory(models.Model):
     _inherit = "product.category"
 
     property_account_income_categ_id = fields.Many2one('account.account', company_dependent=True,
         string="Income Account",
-        domain=ACCOUNT_DOMAIN,
+        domain=ACCOUNT_DOMAIN % "income",
         help="This account will be used when validating a customer invoice.")
     property_account_expense_categ_id = fields.Many2one('account.account', company_dependent=True,
         string="Expense Account",
-        domain=ACCOUNT_DOMAIN,
+        domain=ACCOUNT_DOMAIN % "expense",
         help="The expense is accounted for when a vendor bill is validated, except in anglo-saxon accounting with perpetual inventory valuation in which case the expense (Cost of Goods Sold account) is recognized at the customer invoice validation.")
 
 #----------------------------------------------------------
@@ -28,11 +28,11 @@ class ProductTemplate(models.Model):
         domain=[('type_tax_use', '=', 'purchase')], default=lambda self: self.env.company.account_purchase_tax_id)
     property_account_income_id = fields.Many2one('account.account', company_dependent=True,
         string="Income Account",
-        domain=ACCOUNT_DOMAIN,
+        domain=ACCOUNT_DOMAIN % "income",
         help="Keep this field empty to use the default value from the product category.")
     property_account_expense_id = fields.Many2one('account.account', company_dependent=True,
         string="Expense Account",
-        domain=ACCOUNT_DOMAIN,
+        domain=ACCOUNT_DOMAIN % "expense",
         help="Keep this field empty to use the default value from the product category. If anglo-saxon accounting with automated valuation method is configured, the expense account on the product category will be used.")
 
     def _get_product_accounts(self):
