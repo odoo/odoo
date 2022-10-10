6ac17b93437868cbefbe13448a6fcbb29953f221
Romain Derie <rde@odoo.com>
2022-10-10 15:12:45 +0000

    [IMP] website, website_blog: check all html fields for url depencencies
    Previous commit adapted the URL dependencies screen following the
    website frontend > backend merge done at [1] and [2].
    It allowed to pass other records than website.page and also added the
    multi record capability.
    
    This commit is going a step further, by searching for the URL in all
    HTML fields and not only views + pages + menu + blog.
    
    It also let the XML take care of the wording instead of the python.
    
    closes odoo/odoo#101844
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website_blog/models/website.py
+++ addons/website_blog/models/website.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from markupsafe import Markup
-
-from odoo import api, models, _
+from odoo import models, _
 from odoo.addons.http_routing.models.ir_http import url_for
 from odoo.addons.website.models.website import SEARCH_TYPE_MODELS
-from odoo.osv.expression import AND, OR
 
 SEARCH_TYPE_MODELS['blogs'] |= 'blog.blog', 'blog.post'
 SEARCH_TYPE_MODELS['blogs_only'] |= 'blog.blog',
@@ -15,26 +12,6 @@ SEARCH_TYPE_MODELS['blog_posts_only'] |= 'blog.post',
 
 class Website(models.Model):
     _inherit = "website"
-
-    @api.model
-    def search_url_dependencies(self, res_ids, res_model):
-        dep = super().search_url_dependencies(res_ids, res_model)
-
-        domain = []
-        for record in self.env[res_model].browse([int(res_id) for res_id in res_ids]):
-            url = getattr(record, 'website_url', False) or record.url
-            domain.append(AND([[('content', 'ilike', url)]]))
-
-        posts = self.env['blog.post'].search(OR(domain))
-        if posts:
-            page_key = _('Blog Post') if len(posts) == 1 else _('Blog Posts')
-            dep[page_key] = [{
-                'content': Markup(_('Blog Post <b>%s</b> seems to have a link to this page !', post.name)),
-                'item': post.name,
-                'link': post.website_url,
-            } for post in posts]
-
-        return dep
 
     def get_suggested_controllers(self):
         suggested_controllers = super(Website, self).get_suggested_controllers()
