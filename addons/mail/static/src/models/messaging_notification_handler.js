0fe4b9efc5384beb555d57c38a2811d1eb987877
Rahul Prajapati <rapr@odoo.com>
2022-10-10 10:31:56 +0000

    [FIX] mail: channel unpin/delete issues
    Before this commit:
    
    Channel was not getting unpinned after being deleted
    
    After this commit:
    
    Channel gets unpinned for all the channel members after being deleted
    
    Task-2814440
    
    X-original-commit: 089b386ee2eb04b650b2bd8a23a263e33336cdfb

--- addons/mail/static/src/models/messaging_notification_handler.js
+++ addons/mail/static/src/models/messaging_notification_handler.js
@@ -106,6 +106,8 @@ Model({
                             return this._handleNotificationPartnerTransientMessage(message.payload);
                         case "mail.channel/leave":
                             return this._handleNotificationChannelLeave(message.payload);
+                        case "mail.channel/delete":
+                            return this._handleNotificationChannelDelete(message.payload);
                         case "res.users/connection":
                             return this._handleNotificationPartnerUserConnection(message.payload);
                         case "mail.activity/updated": {
@@ -172,6 +174,21 @@ Model({
                 this.messaging.messagingBus.trigger("o-attachment-deleted", { attachment });
                 attachment.delete();
             }
+        },
+        /**
+         * @private
+         * @param {Object} payload
+         * @param {integer} payload.id
+         */
+        async _handleNotificationChannelDelete({ id }) {
+            const channel = this.messaging.models["Thread"].findFromIdentifyingData({
+                id,
+                model: "mail.channel",
+            });
+            if (!channel) {
+                return;
+            }
+            channel.delete();
         },
         /**
          * @private
