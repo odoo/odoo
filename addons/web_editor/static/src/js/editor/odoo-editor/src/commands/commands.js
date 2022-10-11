5889dd362e6eb4339fd338e445e5a23f7671f71d
vishal padhiyar <visp@odoo.com>
2022-10-11 12:43:45 +0530

    [FIX] web_editor: impossible to add signature at some times
    Before this commit:
    
    When we try to add signature from powerbox then sometimes it does not show
    the signature.
    
    After this commit:
    
    Now when we try to add signature from powerbox then it will show the signature.
    
    Task-2939080

--- addons/web_editor/static/src/js/editor/odoo-editor/src/commands/commands.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/commands/commands.js
@@ -23,6 +23,7 @@ import {
     isSelectionFormat,
     isShrunkBlock,
     isVisible,
+    isVisibleEmpty,
     isVisibleStr,
     leftLeafFirstPath,
     preserveCursor,
@@ -196,7 +197,11 @@ export const editorCommands = {
         if (startNode.nodeType === Node.ELEMENT_NODE) {
             if (selection.anchorOffset === 0) {
                 const textNode = editor.document.createTextNode('');
-                startNode.prepend(textNode);
+                if (isVisibleEmpty(startNode)) {
+                    startNode.parentNode.insertBefore(textNode, startNode);
+                } else {
+                    startNode.prepend(textNode);
+                }
                 startNode = textNode;
             } else {
                 startNode = startNode.childNodes[selection.anchorOffset - 1];
