ea74abf8c12ac5f8942bfe068fb9edd9f3f832bf
Julien Van Roy <juvr@odoo.com>
2022-10-11 13:54:05 +0200

    [IMP] account_edi_ubl_cii: add support for A-NZ Peppol BIS3
    Add edi_format "A-NZ BIS Billing 3.0" used in Australia and New-Zealand.
    This format is derived from Peppol BIS Billing 3.0.
    
    task-2994014

--- addons/account_edi_ubl_cii/models/__init__.py
+++ addons/account_edi_ubl_cii/models/__init__.py
@@ -10,4 +10,5 @@ from . import account_edi_xml_ubl_bis3
 from . import account_edi_xml_ubl_xrechnung
 from . import account_edi_xml_ubl_nlcius
 from . import account_edi_xml_ubl_efff
+from . import account_edi_xml_ubl_a_nz
 from . import ir_actions_report
