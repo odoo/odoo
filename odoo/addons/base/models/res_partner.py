a646b8fac9867327166a4a6998dd2116fbee522b
niyasraphy <niyasraphyk@gmail.com>
2022-10-16 14:32:32 +0300

    [FIX] Button label, name field required, and better labels

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -1047,6 +1047,6 @@ class ResPartnerIndustry(models.Model):
     _name = "res.partner.industry"
     _order = "name"
 
-    name = fields.Char('Name', translate=True)
+    name = fields.Char('Name', translate=True, required=True)
     full_name = fields.Char('Full Name', translate=True)
     active = fields.Boolean('Active', default=True)
