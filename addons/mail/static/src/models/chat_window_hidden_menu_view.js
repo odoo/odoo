4b8874749adc78cfe2d3bde80f7fc14b3a81a389
Alexandre Kühn <aku@odoo.com>
2022-10-17 16:49:21 +0200

    [IMP] mail: Move code from components to models (ChatWindowHiddenMenu)
    Task-3014736

---
+++ addons/mail/static/src/models/chat_window_hidden_menu_view.js
@@ -0,0 +1,63 @@
+/** @odoo-module **/
+
+import { registerModel } from '@mail/model/model_core';
+import { attr, one } from '@mail/model/model_field';
+
+registerModel({
+    name: 'ChatWindowHiddenMenuView',
+    lifecycleHooks: {
+        _created() {
+            document.addEventListener('click', this._onClickCaptureGlobal, true);
+        },
+        _willDelete() {
+            document.removeEventListener('click', this._onClickCaptureGlobal, true);
+        },
+    },
+    recordMethods: {
+        applyListHeight() {
+            const device = this.messaging.device;
+            const height = device.globalWindowInnerHeight / 2;
+            this.listRef.el.style['max-height'] = `${height}px`;
+        },
+        applyOffset() {
+            const textDirection = this.messaging.locale.textDirection;
+            const offsetFrom = textDirection === 'rtl' ? 'left' : 'right';
+            const oppositeFrom = offsetFrom === 'right' ? 'left' : 'right';
+            const offset = this.owner.visual.hiddenMenuOffset;
+            this.component.root.el.style[offsetFrom] = `${offset}px`;
+            this.component.root.el.style[oppositeFrom] = 'auto';
+        },
+        onComponentUpdate() {
+            this.applyListHeight();
+            this.applyOffset();
+        },
+        /**
+         * Closes the menu when clicking outside.
+         * Must be done as capture to avoid stop propagation.
+         *
+         * @private
+         * @param {MouseEvent} ev
+         */
+        _onClickCaptureGlobal(ev) {
+            if (!this.component || !this.component.root.el || this.component.root.el.contains(ev.target)) {
+                return;
+            }
+            this.owner.closeHiddenMenu();
+        }
+    },
+    fields: {
+        component: attr(),
+        isOpen: attr({
+            default: false,
+        }),
+        /**
+         * Reference of the dropup list. Useful to auto-set max height based on
+         * browser screen height.
+         */
+        listRef: attr(),
+        owner: one('ChatWindowManager', {
+            identifying: true,
+            inverse: 'hiddenMenuView',
+        }),
+    },
+});
