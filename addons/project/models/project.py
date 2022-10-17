6a65da4759313c6877f8074780bcd5110569d04c
Xavier BOL (xbo) <xbo@odoo.com>
2022-10-17 14:01:43 +0000

    [FIX] project: manage true and false domain for project sharing
    Before this commit, when the domain used in `read_group` and `search` to
    check if the portal user can access to the fields used in the domain
    does not take into account the `TRUE_LEAF` (that is, `(1, '=', 1)`) and
    `FALSE_LEAF` (that is, `(0, '=', 1)`). In other words, when the
    `TRUE_LEAF` or `FALSE_LEAF` is in the domain, an access error is raised
    saying the field name `1` or `0` cannot be accessible by the portal user.
    
    This commit allows to add `TRUE_LEAF` and `FALSE_LEAF` in the domain to
    avoid having an access error because of that.
    
    close #103214
    
    closes odoo/odoo#103465
    
    X-original-commit: 953033e1ab20a01918dae7ca495654cddef14af3
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>
    Signed-off-by: Xavier <xbo@odoo.com>

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -1884,13 +1884,13 @@ class Task(models.Model):
             # only take field name when having ':' e.g 'date_deadline:week' => 'date_deadline'
             fields_list += [f.split(':')[0] for f in fields_groupby]
         if domain:
-            fields_list += [term[0].split('.')[0] for term in domain if isinstance(term, (tuple, list))]
+            fields_list += [term[0].split('.')[0] for term in domain if isinstance(term, (tuple, list)) and term not in [expression.TRUE_LEAF, expression.FALSE_LEAF]]
         self._ensure_fields_are_accessible(fields_list)
         return super(Task, self).read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
 
     @api.model
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
-        fields_list = {term[0] for term in args if isinstance(term, (tuple, list))}
+        fields_list = {term[0] for term in args if isinstance(term, (tuple, list)) and term not in [expression.TRUE_LEAF, expression.FALSE_LEAF]}
         self._ensure_fields_are_accessible(fields_list)
         return super(Task, self)._search(args, offset=offset, limit=limit, order=order, count=count, access_rights_uid=access_rights_uid)
 
@@ -1902,7 +1902,7 @@ class Task(models.Model):
         return super(Task, self).mapped(func)
 
     def filtered_domain(self, domain):
-        fields_list = [term[0] for term in domain if isinstance(term, (tuple, list))]
+        fields_list = [term[0] for term in domain if isinstance(term, (tuple, list)) and term not in [expression.TRUE_LEAF, expression.FALSE_LEAF]]
         self._ensure_fields_are_accessible(fields_list)
         return super(Task, self).filtered_domain(domain)
 
