376602317d798c6f0cd7f1e6ce5fa8c257f40bbe
Alexandre Kühn <aku@odoo.com>
2022-10-17 18:34:44 +0200

    introduce ComposerSuggestedRecipient/onChangeCheckbox

--- addons/mail/static/src/components/composer_suggested_recipient/composer_suggested_recipient.js
+++ addons/mail/static/src/components/composer_suggested_recipient/composer_suggested_recipient.js
@@ -4,9 +4,6 @@ import { useRefToModel } from '@mail/component_hooks/use_ref_to_model';
 import { useUpdateToModel } from '@mail/component_hooks/use_update_to_model';
 import { registerMessagingComponent } from '@mail/utils/messaging_component';
 
-import { useService } from "@web/core/utils/hooks";
-import { FormViewDialog } from '@web/views/view_dialogs/form_view_dialog';
-
 const { Component } = owl;
 
 export class ComposerSuggestedRecipient extends Component {
@@ -18,7 +15,6 @@ export class ComposerSuggestedRecipient extends Component {
         super.setup();
         useRefToModel({ fieldName: 'checkboxRef', refName: 'checkbox' });
         useUpdateToModel({ methodName: 'onComponentUpdate' });
-        this.dialogService = useService("dialog");
     }
 
     //--------------------------------------------------------------------------
@@ -30,42 +26,6 @@ export class ComposerSuggestedRecipient extends Component {
      */
     get composerSuggestedRecipientView() {
         return this.props.record;
-    }
-
-    //--------------------------------------------------------------------------
-    // Handler
-    //--------------------------------------------------------------------------
-
-    /**
-     * @private
-     */
-    _onChangeCheckbox() {
-        if (!this.composerSuggestedRecipientView.exists()) {
-            return;
-        }
-        const isChecked = this.composerSuggestedRecipientView.checkboxRef.el.checked;
-        this.composerSuggestedRecipientView.suggestedRecipientInfo.update({ isSelected: isChecked });
-        if (!this.composerSuggestedRecipientView.suggestedRecipientInfo.partner) {
-            // Recipients must always be partners. On selecting a suggested
-            // recipient that does not have a partner, the partner creation form
-            // should be opened.
-            if (isChecked) {
-                this.dialogService.add(FormViewDialog, {
-                    context: {
-                        active_id: this.composerSuggestedRecipientView.suggestedRecipientInfo.thread.id,
-                        active_model: 'mail.compose.message',
-                        default_email: this.composerSuggestedRecipientView.suggestedRecipientInfo.email,
-                        default_name: this.composerSuggestedRecipientView.suggestedRecipientInfo.name,
-                        default_lang: this.composerSuggestedRecipientView.suggestedRecipientInfo.lang,
-                        force_email: true,
-                        ref: 'compound_context',
-                    },
-                    onRecordSaved: () => this.composerSuggestedRecipientView.onDialogSaved(),
-                    resModel: "res.partner",
-                    title: this.composerSuggestedRecipientView.suggestedRecipientInfo.dialogText,
-                });
-            }
-        }
     }
 
 }
--- addons/mail/static/src/models/composer_suggested_recipient_view.js
+++ addons/mail/static/src/models/composer_suggested_recipient_view.js
@@ -3,9 +3,39 @@
 import { registerModel } from '@mail/model/model_core';
 import { attr, one } from '@mail/model/model_field';
 
+import { FormViewDialog } from '@web/views/view_dialogs/form_view_dialog';
+
 registerModel({
     name: 'ComposerSuggestedRecipientView',
     recordMethods: {
+        onChangeCheckbox() {
+            if (!this.exists()) {
+                return;
+            }
+            const isChecked = this.checkboxRef.el.checked;
+            this.suggestedRecipientInfo.update({ isSelected: isChecked });
+            if (!this.suggestedRecipientInfo.partner) {
+                // Recipients must always be partners. On selecting a suggested
+                // recipient that does not have a partner, the partner creation form
+                // should be opened.
+                if (isChecked) {
+                    this.env.services.dialog.add(FormViewDialog, {
+                        context: {
+                            active_id: this.suggestedRecipientInfo.thread.id,
+                            active_model: 'mail.compose.message',
+                            default_email: this.suggestedRecipientInfo.email,
+                            default_name: this.suggestedRecipientInfo.name,
+                            default_lang: this.suggestedRecipientInfo.lang,
+                            force_email: true,
+                            ref: 'compound_context',
+                        },
+                        onRecordSaved: () => this.onDialogSaved(),
+                        resModel: "res.partner",
+                        title: this.suggestedRecipientInfo.dialogText,
+                    });
+                }
+            }
+        },
         onComponentUpdate() {
             if (this.checkboxRef.el && this.suggestedRecipientInfo) {
                 this.checkboxRef.el.checked = this.suggestedRecipientInfo.isSelected;
