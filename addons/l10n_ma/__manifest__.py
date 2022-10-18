a08117f6fd17f444a2e34a004fe2d9cf9b7fd1f3
ArnaudVerlaine <verlainearnaud200@icloud.com>
2022-10-18 15:55:16 +0200

    [I18N] l10n_ma: adds french translation

--- addons/l10n_ma/__manifest__.py
+++ addons/l10n_ma/__manifest__.py
@@ -3,12 +3,15 @@
 
 {
     'name': 'Morocco - Accounting',
-    'author': 'kazacube',
+    'author': 'Odoo SA',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
 This is the base module to manage the accounting chart for Morocco.
 """,
-    'depends': ['base', 'account'],
+    'depends': [
+        'account',
+        'l10n_multilang',
+    ],
     'data': [
         'data/account_chart_template_data.xml',
         'data/account.account.template.csv',
@@ -18,10 +21,10 @@ This is the base module to manage the accounting chart for Morocco.
         'data/account_tax_data.xml',
         'data/account.group.template.csv',
         'data/account_chart_template_configure_data.xml',
-        "data/menuitem_data.xml",
     ],
     'demo': [
         'demo/demo_company.xml',
     ],
+    'post_init_hook': 'load_translations',
     'license': 'LGPL-3',
 }
