b0d745eb316c8e9b4d13e6a6561d6988bbd6b381
kir-odoo <kir@odoo.com>
2022-10-18 19:03:49 +0530

    [FIX] delivery: raise traceback while getting shipping rate
    Steps to produce
    ================
       - Make SO which has 2 products with the same weight.
       - Both product's quantity is the same but one of the product quantity is negative.
       - Then click on `Add Shipping` and we try to get the shipping rate then it raises traceback.
    
    After this commit
    =================
    In this commit, I ignore the rate of shipping for negative quantity in SO line.
    
    TaskId - 3028023

--- addons/delivery/models/sale_order.py
+++ addons/delivery/models/sale_order.py
@@ -153,7 +153,7 @@ class SaleOrder(models.Model):
     def _get_estimated_weight(self):
         self.ensure_one()
         weight = 0.0
-        for order_line in self.order_line.filtered(lambda l: l.product_id.type in ['product', 'consu'] and not l.is_delivery and not l.display_type):
+        for order_line in self.order_line.filtered(lambda l: l.product_id.type in ['product', 'consu'] and not l.is_delivery and not l.display_type and l.product_uom_qty > 0):
             weight += order_line.product_qty * order_line.product_id.weight
         return weight
 
