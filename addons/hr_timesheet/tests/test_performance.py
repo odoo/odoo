b23c54d670ce5724062abc40aea57cc744f8947d
Audric Onockx (auon) <auon@odoo.com>
2022-10-18 18:49:10 +0200

    [REF] hr_timesheet: make _timesheet_preprocess work in batch
    With this commit, we allow _timesheet_preprocess to accept a list of
    dict of values instead of dict of values.
    This represents an important gain in performance.
    
    With 6 projects, 17 tasks/project, 10 timsheet vals/task = 1020 'vals':
    Queries : reduced from 110 to 7.
    Time : divided by ≃2.5.
    
    task-3030942

---
+++ addons/hr_timesheet/tests/test_performance.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.addons.hr_timesheet.tests.test_timesheet import TestCommonTimesheet
+
+class TestPerformanceTimesheet(TestCommonTimesheet):
+
+    def test_timesheet_preprocess(self):
+        projects = self.env['project.project'].create([{'name': 'Project %s' % i} for i in range(6)])
+        tasks = self.env['project.task'].create([{
+            'name': 'Task %s (%s)' % (i, project.name),
+            'project_id': project.id,
+        } for i in range(17) for project in projects])
+        self.env.invalidate_all()
+        projects.clear_caches()
+        tasks.clear_caches()
+        with self.assertQueryCount(4):
+            self.env['account.analytic.line']._timesheet_preprocess([
+                {'task_id': task.id} for task in tasks for _i in range(10)
+            ])
