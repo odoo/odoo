1d44b3b225ab6716b5da468eb1c215a95f35ca9b
ArnaudVerlaine <arve@odoo.com>
2022-10-19 13:44:56 +0200

    [FIX] l10n_ma: rework of the CoA and french translation

--- addons/l10n_ma/__manifest__.py
+++ addons/l10n_ma/__manifest__.py
@@ -1,29 +1,31 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Copyright (c) 2010 kazacube (http://kazacube.com).
-
 {
-    'name': 'Maroc - Accounting',
-    'author': 'kazacube',
+    'name': 'Morocco - Accounting',
+    'author': 'Odoo SA',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
-This is the base module to manage the accounting chart for Maroc.
-=================================================================
-
-Ce Module charge le modèle du plan de comptes standard Marocain et permet de
-générer les états comptables aux normes marocaines (Bilan, CPC (comptes de
-produits et charges), balance générale à 6 colonnes, Grand livre cumulatif...).
-L'intégration comptable a été validé avec l'aide du Cabinet d'expertise comptable
-Seddik au cours du troisième trimestre 2010.""",
-    'website': 'http://www.kazacube.com',
-    'depends': ['base', 'account'],
+This is the base module to manage the accounting chart for Morocco.
+""",
+    'depends': [
+        'account',
+        'l10n_multilang',
+    ],
     'data': [
-        'data/l10n_ma_chart_data.xml',
+        'data/account_chart_template_data.xml',
+        'data/account.account.template.csv',
+        'data/account_cgnc_morocco.xml',
         'data/account_tax_group_data.xml',
         'data/account_tax_report_data.xml',
         'data/account_tax_data.xml',
-        'data/account_chart_template_data.xml',
+        'data/fiscal_templates_data.xml',
+        'data/account.group.template.csv',
+        'data/account_chart_template_configure_data.xml',
     ],
+    'demo': [
+        'demo/demo_company.xml',
+    ],
+    'post_init_hook': 'load_translations',
     'license': 'LGPL-3',
 }
