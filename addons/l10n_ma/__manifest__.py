09eca2809279f35e85edb001175fe3b6945f4c6f
Dylan Kiss <dyki@odoo.com>
2022-10-19 11:44:56 +0000

    [IMP] l10n_ma: update localization
    Complete revamp of the Moroccan localization.
    
    - Chart of Accounts according to the official CGNC (Code Général de la Normalisation Comptable)
    - English source terms with official French translations
    - Sensible default accounts for all properties
    - All taxes needed for the tax report
    - Fiscal positions
    - Official Moroccan tax report
    - Demo data
    
    References:
    - http://www.befec.ma/documentation/comptabilite/Plan_comptable/CGNC/cgnc.pdf
    - https://www.tax.gov.ma/wps/wcm/connect/2aabc75b-959e-4753-a152-3441104acaf7/adc_080b_22i.pdf?MOD=AJPERES
    
    task-3040333
    
    closes odoo/odoo#109498
    
    Related: odoo/enterprise#35604
    Signed-off-by: William André (wan) <wan@odoo.com>
    Co-authored-by: Arnaud Verlaine <arve@odoo.com>

--- addons/l10n_ma/__manifest__.py
+++ addons/l10n_ma/__manifest__.py
@@ -1,20 +1,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'Morocco - Accounting',
     'icon': '/account/static/description/l10n.png',
     'countries': ['ma'],
-    'author': 'kazacube (http://www.kazacube.com)',
+    'author': 'Odoo SA',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
 This is the base module to manage the accounting chart for Morocco.
-====================================================================
 
-Ce Module charge le modèle du plan de comptes standard Marocain et permet de
-générer les états comptables aux normes marocaines (Bilan, CPC (comptes de
-produits et charges), balance générale à 6 colonnes, Grand livre cumulatif...).
-L'intégration comptable a été validé avec l'aide du Cabinet d'expertise comptable
-Seddik au cours du troisième trimestre 2010.""",
-    'website': 'https://www.odoo.com/documentation/master/applications/finance/fiscal_localizations.html',
+This module has been built with the help of Caudigef.
+""",
     'depends': [
         'base',
         'account',
