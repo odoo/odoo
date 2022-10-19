fc890dc6e91a6731bf31a40ee77dad0f8697f5b4
Valentin Chevalier <vcr@odoo.com>
2022-10-19 13:47:36 +0000

    [IMP] payment_stripe: adjust the code for Stripe Connect
    task-2982347
    
    Part-of: odoo/odoo#102483

--- addons/payment_stripe/static/src/js/payment_form.js
+++ addons/payment_stripe/static/src/js/payment_form.js
@@ -1,49 +1,37 @@
+/** @odoo-module */
 /* global Stripe */
-odoo.define('payment_stripe.payment_form', require => {
-    'use strict';
 
-    const checkoutForm = require('payment.checkout_form');
-    const manageForm = require('payment.manage_form');
+import checkoutForm from 'payment.checkout_form';
+import manageForm from 'payment.manage_form';
+import { StripeOptions } from '@payment_stripe/js/stripe_options';
 
-    const stripeMixin = {
+const stripeMixin = {
 
-        /**
-         * Redirect the customer to Stripe hosted payment page.
-         *
-         * @override method from payment.payment_form_mixin
-         * @private
-         * @param {string} code - The code of the payment option
-         * @param {number} paymentOptionId - The id of the payment option handling the transaction
-         * @param {object} processingValues - The processing values of the transaction
-         * @return {undefined}
-         */
-        _processRedirectPayment: function (code, paymentOptionId, processingValues) {
-            if (code !== 'stripe') {
-                return this._super(...arguments);
-            }
+    /**
+     * Redirect the customer to Stripe hosted payment page.
+     *
+     * @override method from payment.payment_form_mixin
+     * @private
+     * @param {string} code - The code of the payment option
+     * @param {number} paymentOptionId - The id of the payment option handling the transaction
+     * @param {object} processingValues - The processing values of the transaction
+     * @return {undefined}
+     */
+    _processRedirectPayment: function (code, paymentOptionId, processingValues) {
+        if (code !== 'stripe') {
+            return this._super(...arguments);
+        }
 
-            const stripeJS = Stripe(processingValues['publishable_key'],
-                this._prepareStripeOptions(processingValues));
-            stripeJS.redirectToCheckout({
-                sessionId: processingValues['session_id']
-            });
-        },
+        const stripeJS = Stripe(
+            processingValues['publishable_key'],
+            // Instantiate the StripeOptions class to allow patching the method and add options.
+            new StripeOptions()._prepareStripeOptions(processingValues),
+        );
+        stripeJS.redirectToCheckout({
+            sessionId: processingValues['session_id']
+        });
+    },
+};
 
-        /**
-         * Prepare the options to init the Stripe JS Object
-         *
-         * Function overriden in internal module
-         *
-         * @param {object} processingValues
-         * @return {object}
-         */
-        _prepareStripeOptions: function (processingValues) {
-            return {};
-        },
-    };
-
-    checkoutForm.include(stripeMixin);
-    manageForm.include(stripeMixin);
-
-    return stripeMixin;
-});
+checkoutForm.include(stripeMixin);
+manageForm.include(stripeMixin);
