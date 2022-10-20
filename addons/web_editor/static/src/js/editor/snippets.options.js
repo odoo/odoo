bc9177f9ae834700d7a41b054312c0d2b7150727
qsm-odoo <qsm@odoo.com>
2022-10-20 15:13:29 +0200

    [FIX] web_editor: prevent crash when colorpickers receive split values
    Steps to reproduce:
    - Edit
    - Add a snippet with columns
    - Save
    - HTML editor
    - Set up a border on one column of the snippet with different colors for
      the top/right/bottom/left sides
    - Edit
    - Click on the column => crash
    
    This is only an example, but if a colorpicker widget is linked to a
    SnippetOption instance's `selectStyle` method designed to handle the
    "border-color" property of an element, the value received can be split
    if the item uses different colors for its top/right/bottom/left borders.
    For instance, you could receive "red blue" if the item has red top and
    bottom borders and blue left and right borders. In that case, the
    colorpicker widget code would try to add the class "bg-red blue" on its
    preview item which would crash because of the space inside).
    
    After this commit, we simply do not show any color for this case.
    
    opw-2803311

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -1389,7 +1389,23 @@ const ColorpickerUserValueWidget = SelectUserValueWidget.extend({
             } else if (weUtils.isColorCombinationName(this._value)) {
                 this.colorPreviewEl.classList.add('o_cc', `o_cc${this._value}`);
             } else {
-                this.colorPreviewEl.classList.add(`bg-${this._value}`);
+                // Checking if the className actually exists seems overkill but
+                // it is actually needed to prevent a crash. As an example, if a
+                // colorpicker widget is linked to a SnippetOption instance's
+                // `selectStyle` method designed to handle the "border-color"
+                // property of an element, the value received can be split if
+                // the item uses different colors for its top/right/bottom/left
+                // borders. For instance, you could receive "red blue" if the
+                // item as red top and bottom borders and blue left and right
+                // borders, in which case you would reach this `else` and try to
+                // add the class "bg-red blue" which would crash because of the
+                // space inside). In that case, we simply do not show any color.
+                // We could choose to handle this split-value case specifically
+                // but it was decided that this is enough for the moment.
+                const className = `bg-${this._value}`;
+                if (classes.includes(className)) {
+                    this.colorPreviewEl.classList.add(className);
+                }
             }
         }
     },
