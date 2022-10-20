b8d64a90d6d095a0ab0f99c268c5daddb1a5cc05
Adrien Minne <adrm@odoo.com>
2022-10-20 16:23:44 +0200

    [IMP] srpeadsheet: make xlsx import verbose in debug mode
    Make the xlsx import display warning for things that couldn't be imported,
    but only if Odoo is in debug mode.
    
    Odoo task 3035674

--- addons/spreadsheet/static/src/o_spreadsheet/migration.js
+++ addons/spreadsheet/static/src/o_spreadsheet/migration.js
@@ -18,7 +18,7 @@ const MAP = {
 const dmyRegex = /^([0|1|2|3][1-9])\/(0[1-9]|1[0-2])\/(\d{4})$/i;
 
 export function migrate(data) {
-    let _data = load(data);
+    let _data = load(data, !!odoo.debug);
     const version = _data.odooVersion || 0;
     if (version < 1) {
         _data = migrate0to1(_data);
