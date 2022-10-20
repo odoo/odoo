abcf390f90dbdd39fd61abc53f8516e7278e0931
Jérémy Hennecart (jeh) <jeh@odoo.com>
2022-10-20 17:30:36 +0200

    [IMP] event(_sale): improve wording & usability
    - Improve wording link to event ticket.
    - Add a constraint checking that sale order lines with a product type
    event have an event and a ticket linked to it.
    - Add domain on event and event ticket field in the embedded tree view
    for sale order line to match the form view above. This is done to avoid
    creating sale order line with event in the past.
    - Auto select the event ticket in the configurator if it's the only one for
    the event selected.
    - When no event or ticket is available for the product, display a message with
    a link that redirects the user in a new tab to configure his event.
    - Remove the skip button in the registration editor form as it was just another
    way to continue with the default value already present which can be done with
    the "Create/Update" button.
    
    task-2485710
    
    closes odoo/odoo#105885
    
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- addons/event_sale/wizard/event_configurator.py
+++ addons/event_sale/wizard/event_configurator.py
@@ -11,7 +11,9 @@ class EventConfigurator(models.TransientModel):
 
     product_id = fields.Many2one('product.product', string="Product", readonly=True)
     event_id = fields.Many2one('event.event', string="Event")
-    event_ticket_id = fields.Many2one('event.event.ticket', string="Event Ticket")
+    event_ticket_id = fields.Many2one('event.event.ticket', string="Ticket Type",
+        compute="_compute_event_ticket_id", readonly=False, store=True)
+    has_available_tickets = fields.Boolean("Has Available Tickets", compute="_compute_has_available_tickets")
 
     @api.constrains('event_id', 'event_ticket_id')
     def check_event_id(self):
@@ -22,3 +24,23 @@ class EventConfigurator(models.TransientModel):
                     _('Invalid ticket choice "%(ticket_name)s" for event "%(event_name)s".'))
         if error_messages:
             raise ValidationError('\n'.join(error_messages))
+
+    @api.depends('product_id')
+    def _compute_has_available_tickets(self):
+        product_ticket_data = self.env['event.event.ticket']._read_group([
+            ('product_id', 'in', self.product_id.ids),
+            ('event_id.date_end', '>=', fields.Date.today())],
+            ['product_id'],
+            ['__count'])
+        mapped_data = {product: ticket_count for product, ticket_count in product_ticket_data}
+        for configurator in self:
+            configurator.has_available_tickets = bool(mapped_data.get(configurator.product_id, 0))
+
+    @api.depends('event_id')
+    def _compute_event_ticket_id(self):
+        """ Pre-select the ticket of the event selected if it is the only one """
+        for configurator in self:
+            event_ticket_ids = self.env['event.event.ticket'].search([
+                ('event_id', '=', configurator.event_id.id),
+                ('product_id', '=', configurator.product_id.id)], limit=2)
+            configurator.event_ticket_id = event_ticket_ids if len(event_ticket_ids) == 1 else False
