bba8748fca84387b499a84230f48ffc78dddeb51
Anita (anko) <anko@odoo.com>
2022-10-21 10:53:07 +0200

    [IMP] payment(_paypal): UX and payment flow improvements
    UX was lacking comparing to other payment providers,
    important fields were not always shown or were checkboxes
    when they should be automatically true.
    
    In order to make payment flow easier and more intitive, unnecessary fields
    were removed, email is automatically filled. Now when user cancels transaction
    on paypal before paying, it automatically cancels transaction on Odoo. Additionaly, quick
    onboarding is only available if user already has paypal account and Stripe
    no longer installs ond configures paypal if Stripe's onboarding get canceled.
    
    task-2854184

--- addons/payment_paypal/__manifest__.py
+++ addons/payment_paypal/__manifest__.py
@@ -13,7 +13,6 @@
         'views/payment_transaction_views.xml',
 
         'data/payment_provider_data.xml',
-        'data/payment_paypal_email_data.xml',
     ],
     'post_init_hook': 'post_init_hook',
     'uninstall_hook': 'uninstall_hook',
