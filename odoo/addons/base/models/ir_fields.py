8459dd5b6da68d513e38303e49ae76b6b57d7ce9
Rémy Voet (ryv) <ryv@odoo.com>
2022-10-24 07:41:25 +0200

    [MOV] core,base: move logic of `O2MIdMapper`
    O2MIdMapper Class inherit of 'base' Model to change the behavior of
    `create`.
    Because it is in base and the `_import_current_module` is set in
    models.py, move it at the end of create in models.py.

--- odoo/addons/base/models/ir_fields.py
+++ odoo/addons/base/models/ir_fields.py
@@ -640,37 +640,3 @@ class IrFieldsConverter(models.AbstractModel):
                 commands.append(Command.create(writable))
 
         return commands, warnings
-
-class O2MIdMapper(models.AbstractModel):
-    """
-    Updates the base class to support setting xids directly in create by
-    providing an "id" key (otherwise stripped by create) during an import
-    (which should strip 'id' from the input data anyway)
-    """
-    _inherit = 'base'
-
-    # sadly _load_records_create is only called for the toplevel record so we
-    # can't hook into that
-    @api.model_create_multi
-    @api.returns('self', lambda value: value.id)
-    def create(self, vals_list):
-        recs = super().create(vals_list)
-
-        import_module = self.env.context.get('_import_current_module')
-        if not import_module: # not an import -> bail
-            return recs
-        noupdate = self.env.context.get('noupdate', False)
-
-        xids = (v.get('id') for v in vals_list)
-        self.env['ir.model.data']._update_xmlids([
-            {
-                'xml_id': xid if '.' in xid else ('%s.%s' % (import_module, xid)),
-                'record': rec,
-                # note: this is not used when updating o2ms above...
-                'noupdate': noupdate,
-            }
-            for rec, xid in zip(recs, xids)
-            if xid and isinstance(xid, str)
-        ])
-
-        return recs
