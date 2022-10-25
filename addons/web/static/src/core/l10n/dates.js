2e0bc215120b739122097f5f69ccf4589c50697c
Bruno Boi <boi@odoo.com>
2022-10-25 09:05:29 +0200

    [FIX] web: moment.js to luxon's conversions
    The web client exposes an utility function that converts the tokens
    of a luxon's DateTime format into a moment.js format.
    While this function is not false (still it is uncomplete), the strategy
    of using it to convert a luxon's DateTime object into a moment.js moment
    is not good.
    
    The daterange field make use of this bad strategy.
    
    Because of the differences between luxon and moment.js locale handling.
    Luxon relies on the native JavaScript Intl API.
    Moment has its own implementation of locale handling.
    
    So having to pass through formatters/parsers may yield to unexpected
    results. For instance, when a locale does not have the same way (in
    moment.js and Intl) to express i.e. months and we try to convert a
    luxon's DateTime into a moment by formatting the first and then
    parsing into the second, the second will be invalid. This is the same
    the other way around.
    
    This was in fact encountered with some locales, i.e.
    - syrian arabic (ar-SY)
    - albanian shqip (sq-AL)
    
    This commit brings an alternative strategy by providing other utility
    functions to directly convert a moment into a luxon's DateTime
    and vice versa.
    
    This is a better strategy for various reasons:
    - it abstracts the way the conversions are done
    - no need to play with timezone's offsets
    - probably all libs that has moment as a dependency accept a moment
      object in their options, that may not be the case for formats
    
    - The daterange field now use the new strategy
    - The datepicker component now use the new utility functions as it had
      implemented locally something similar.

--- addons/web/static/src/core/l10n/dates.js
+++ addons/web/static/src/core/l10n/dates.js
@@ -164,6 +164,44 @@ export const luxonToMomentFormat = memoize(function luxonToMomentFormat(format) 
     });
 });
 
+/**
+ * Converts a luxon's DateTime object into a moment.js object.
+ * NB: the passed object's values will be utilized as is, regardless of its corresponding timezone.
+ * So passing a luxon's DateTime having 8 as hours value will result in a moment.js object having
+ * also 8 as hours value. But the moment.js object will be in the browser's timezone.
+ *
+ * @param {DateTime} dt a luxon's DateTime object
+ * @returns {moment} a moment.js object in the browser's timezone
+ */
+export function luxonToMoment(dt) {
+    const o = dt.toObject();
+    // Note: the month is 0-based in moment.js, but 1-based in luxon.js
+    return moment({ ...o, month: o.month - 1 });
+}
+
+/**
+ * Converts a moment.js object into a luxon's DateTime object.
+ * NB: the passed object's values will be utilized as is, regardless of its corresponding timezone.
+ * So passing a moment.js object having 8 as hours value will result in a luxon's DateTime object
+ * having also 8 as hours value. But the luxon's DateTime object will be in the user's timezone.
+ *
+ * @param {moment} dt a moment.js object
+ * @returns {DateTime} a luxon's DateTime object in the user's timezone
+ */
+export function momentToLuxon(dt) {
+    const o = dt.toObject();
+    // Note: the month is 0-based in moment.js, but 1-based in luxon.js
+    return DateTime.fromObject({
+        year: o.years,
+        month: o.months + 1,
+        day: o.date,
+        hour: o.hours,
+        minute: o.minutes,
+        second: o.seconds,
+        millisecond: o.milliseconds,
+    });
+}
+
 // -----------------------------------------------------------------------------
 // Formatting
 // -----------------------------------------------------------------------------
