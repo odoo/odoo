4c2a572005664aaaf91585a658c88249bca4a4cd
Florian Charlier <flch@odoo.com>
2022-10-26 18:19:33 +0200

    [FIX] sale: fix image link for onb. sample QO
    The image for the sample product was updated in 76141b0d which made the
    'Sample quotation' onboarding step hang for databases without any
    `product.product` record.
    
    A simple unit test for `_get_sample_sales_order` is included.
    
    Technically, it wasn't possible to add it to the existing onboarding test class
    because these tests are run post_install, and would fail on ENT builds because
    of pos_loyalty preventing archiving `product.product`s.
    
    --Additional note for 15.3 fw-port:
    As logic moved from pos_loyalty to loyalty, which is installed before sale on
    runbots, the problem described above resurfaces. We chose this cross module but
    simple and limited-impact fix to avoid it.
    
    Task-3045287
    
    X-original-commit: 5181612d67c5411fe7f2999eb6b4c03eab614ee2

--- addons/sale/tests/__init__.py
+++ addons/sale/tests/__init__.py
@@ -7,6 +7,7 @@ from . import test_onchange
 from . import test_payment_flow
 from . import test_reinvoice
 from . import test_sale_flow
+from . import test_sale_onboarding
 from . import test_sale_order
 from . import test_sale_pricelist
 from . import test_sale_product_attribute_value_config
