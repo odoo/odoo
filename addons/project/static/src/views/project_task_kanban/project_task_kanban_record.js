6ff75c612abbbe40e7a5eec9ecceece747ebb84a
Hubert Van de Walle (huvw) <huvw@odoo.com>
2022-10-26 15:10:01 +0000

    [FIX] project: applychanges for personal_stage_type_ids
    Steps to reproduce
    ==================
    
    - Go to Project > My tasks
    - Drag a card to another column
    -> An error occurs
    
    Cause of the issue
    ==================
    
    `personal_stage_type_ids` was not changed to `personal_stage_type_id`
    
    opw-3036820
    
    closes odoo/odoo#104442
    
    X-original-commit: 3afc32af953556227dc07f8c65b5301d10fa92fe
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Hubert Van De Walle <huvw@odoo.com>

--- addons/project/static/src/views/project_task_kanban/project_task_kanban_record.js
+++ addons/project/static/src/views/project_task_kanban/project_task_kanban_record.js
@@ -5,7 +5,7 @@ import { Record } from '@web/views/relational_model';
 export class ProjectTaskRecord extends Record {
     async _applyChanges(changes) {
         const value = changes.personal_stage_type_ids;
-        if (value && Array.isArray(value) && value.length === 1) {
+        if (value && Array.isArray(value)) {
             delete changes.personal_stage_type_ids;
             changes.personal_stage_type_id = value;
         }
