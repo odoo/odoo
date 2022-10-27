74ae921033adda2c2e226c394f70504b75e70fc8
Alexandre Kühn <aku@odoo.com>
2022-10-27 13:29:03 +0200

    [IMP] mail: rename ChannelMember (component)
    Task-3046298

--- addons/mail/static/tests/qunit_suite_tests/components/channel_member_list_tests.js
+++ addons/mail/static/tests/qunit_suite_tests/components/channel_member_list_tests.js
@@ -87,18 +87,18 @@ QUnit.test('should have correct members in member list', async function (assert)
     await click('.o_ThreadViewTopbar_showMemberListButton');
     assert.containsN(
         document.body,
-        '.o_ChannelMember',
+        '.o_ChannelMemberView',
         2,
         "should have 2 members in member list",
     );
     assert.containsOnce(
         document.body,
-        `.o_ChannelMember[data-partner-id="${pyEnv.currentPartnerId}"]`,
+        `.o_ChannelMemberView[data-partner-id="${pyEnv.currentPartnerId}"]`,
         "should have current partner in member list (current partner is a member)",
     );
     assert.containsOnce(
         document.body,
-        `.o_ChannelMember[data-partner-id="${resPartnerId1}"]`,
+        `.o_ChannelMemberView[data-partner-id="${resPartnerId1}"]`,
         "should have 'Demo' in member list ('Demo' is a member)",
     );
 });
@@ -186,7 +186,7 @@ QUnit.test('Load more button should load more members', async function (assert) 
     await click('.o_ChannelMemberList_loadMoreButton');
     assert.containsN(
         document.body,
-        '.o_ChannelMember',
+        '.o_ChannelMemberView',
         102,
         "should load all the members because 100 members were fetched initially, and load more fetched the remaining 2 members",
     );
@@ -215,7 +215,7 @@ QUnit.test('chat with member should be opened after clicking on channel member',
     await openDiscuss();
 
     await click('.o_ThreadViewTopbar_showMemberListButton');
-    await click(`.o_ChannelMember[data-partner-id="${resPartnerId1}"]`);
+    await click(`.o_ChannelMemberView[data-partner-id="${resPartnerId1}"]`);
     assert.containsOnce(
         document.body,
         `.o_ThreadView[data-correspondent-id="${resPartnerId1}"]`,
