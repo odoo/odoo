fe11c789a963e01120b31416d657ca506ebc5cb9
william <wan@odoo.com>
2022-10-27 14:51:12 +0200

    [FIX] hr_expense: expenses should be tax included
    During a refactoring [1], the behavior of having the taxes included in
    the price for all expenses was broken/removed.
    All the fields related to taxes are computed should be computed with the
    correct context key when computing entries related to expenses. This is
    preferred to adding the context key only when creating because it will
    then keep the behavior even when editing the document.
    
    task-3043252
    
    [1]: https://github.com/odoo/odoo/commit/d8d47f9ff8554f4b39487fd2f13c153c7d6f958d

--- addons/hr_expense/models/account_move_line.py
+++ addons/hr_expense/models/account_move_line.py
@@ -34,15 +34,28 @@ class AccountMoveLine(models.Model):
                 line.tax_key = frozendict(**line.tax_key, expense_id=line.expense_id.id)
 
     def _compute_all_tax(self):
-        super()._compute_all_tax()
-        for line in self:
-            if line.expense_id:
-                for key in list(line.compute_all_tax.keys()):
-                    new_key = frozendict(**key, expense_id=line.expense_id.id)
-                    line.compute_all_tax[new_key] = line.compute_all_tax.pop(key)
+        expense_lines = self.filtered('expense_id')
+        super(AccountMoveLine, expense_lines.with_context(force_price_include=True))._compute_all_tax()
+        super(AccountMoveLine, self - expense_lines)._compute_all_tax()
+        for line in expense_lines:
+            for key in list(line.compute_all_tax.keys()):
+                new_key = frozendict(**key, expense_id=line.expense_id.id)
+                line.compute_all_tax[new_key] = line.compute_all_tax.pop(key)
+
+    def _compute_totals(self):
+        expenses = self.filtered('expense_id')
+        super(AccountMoveLine, expenses.with_context(force_price_include=True))._compute_totals()
+        super(AccountMoveLine, self - expenses)._compute_totals()
 
     def _compute_term_key(self):
         super()._compute_term_key()
         for line in self:
             if line.expense_id:
                 line.term_key = line.term_key and frozendict(**line.term_key, expense_id=line.expense_id.id)
+
+    def _convert_to_tax_base_line_dict(self):
+        result = super()._convert_to_tax_base_line_dict()
+        if self.move_id.expense_sheet_id:
+            result.setdefault('extra_context', {})
+            result['extra_context']['force_price_include'] = True
+        return result
