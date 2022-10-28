811d23d1b1bacbd7c1fd669c11532c8ad9748f33
Trinh Ngoc Hung <ngochung207@gmail.com>
2022-10-28 16:42:36 +0700

    [FIX] mrp: duplicate MO error lost move_byproduct_ids
    Steps to reproduce:
    	Step 1: make a MO
    	Step 2: duplicate the MO
    Expected result: tab move_byproduct_ids is cloned
    Actual result: tab move_byproduct_ids is empty

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -775,11 +775,17 @@ class MrpProduction(models.Model):
     def create(self, values):
         # Remove from `move_finished_ids` the by-product moves and then move `move_byproduct_ids`
         # into `move_finished_ids` to avoid duplicate and inconsistency.
+        values_copy = values.copy()
+        vl_byprodct = []
         if values.get('move_finished_ids', False):
-            values['move_finished_ids'] = list(filter(lambda move: move[2].get('byproduct_id', False) is False, values['move_finished_ids']))
+            values['move_finished_ids'] = list(filter(lambda move: move[2].get('byproduct_id', False) is False, values_copy['move_finished_ids']))
+            vl_byprodct = list(filter(lambda move: bool(move[2].get('byproduct_id', False)), values_copy['move_finished_ids']))
         if values.get('move_byproduct_ids', False):
             values['move_finished_ids'] = values.get('move_finished_ids', []) + values['move_byproduct_ids']
             del values['move_byproduct_ids']
+        else:
+            if bool(vl_byprodct):
+                values['move_finished_ids'] = values.get('move_finished_ids', []) + vl_byprodct
         if not values.get('name', False) or values['name'] == _('New'):
             picking_type_id = values.get('picking_type_id') or self._get_default_picking_type()
             picking_type_id = self.env['stock.picking.type'].browse(picking_type_id)
