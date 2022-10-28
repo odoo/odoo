c2896ca8e0589c5b30e37456f127cf2d7b0a725b
Joseph Caburnay <jcb@odoo.com>
2022-10-28 10:44:01 +0000

    [FIX] web: allow custom grouping with a stored m2m field
    Custom grouping by m2m field is introduced here: https://github.com/odoo/odoo/pull/74985.
    However, after refactoring in https://github.com/odoo/odoo/pull/73311,
    users are unable to add custom groups based on stored m2m fields.
    This is because the stored m2m fields are not included in the list of
    "Add Custom Group" options.
    
    In this commit, we are now again including the stored m2m in the options
    for "Add Custom Group".
    
    closes odoo/odoo#104873
    
    Task-id: 3046183
    X-original-commit: 5087d9011be7f46a4a3e05b1e951f2556f254b4c
    Signed-off-by: Mathieu Duckerts-Antoine <dam@odoo.com>
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/web/static/src/search/group_by_menu/group_by_menu.js
+++ addons/web/static/src/search/group_by_menu/group_by_menu.js
@@ -48,8 +48,12 @@ export class GroupByMenu extends Component {
      * @returns {boolean}
      */
     validateField(fieldName, field) {
-        const { sortable, type } = field;
-        return fieldName !== "id" && sortable && GROUPABLE_TYPES.includes(type);
+        const { sortable, store, type } = field;
+        return (
+            (type === "many2many" ? store : sortable) &&
+            fieldName !== "id" &&
+            GROUPABLE_TYPES.includes(type)
+        );
     }
 
     /**
