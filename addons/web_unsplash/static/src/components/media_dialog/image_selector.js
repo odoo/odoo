3ec0efc3e80039183d048a2615ac8b70a4c2ef85
Younn Olivier <yol@odoo.com>
2022-10-31 09:20:31 +0100

    [FIX] web_editor, web_unsplash: fix debounced search from media dialog
    Following this flow on the Media Dialog:
    - Type "hell"
    - Wait a few ms so that the RPC to search images starts
    - Type "o"
    - The RPC of before finishes, the o is removed and no new search is done
    
    Before this commit, the search input value of the SearchMedia component
    was coming from a "needle" props. This needle prop was a state on the
    parent component (FileSelector), set through a debounced handler.
    
    This is not a good design:
    - Type "hell": after 1000ms, the debounced handler will be executed with
    the value "hell"
    - While the handler is executing: type "o"
    - When the handler finishes, it has set the needle state value to "hell"
    on the parent. This will rerender the SearchMedia with "hell" as a
    needle prop, and the "hello" input will be replaced with "hell".
    
    Instead of doing that, the SearchMedia component should have its own
    input state, which models the input element.
    On that state, we use an effect to call the debounced "search" callback.
    This way, the SearchMedia is less dependent on its parent: it only
    rerenders when its input element changes.
    
    Additionally, the fetch results are only used if they are the ones from
    the last call to `search`.
    
    Dedicated `KeepLast` instances are used for attachments, media library
    and unsplash.
    
    task-3060679
    
    Co-authored-by: Arthur Detroux <ard@odoo.com>
    Co-authored-by: Benoit Socias <bso@odoo.com>

--- addons/web_unsplash/static/src/components/media_dialog/image_selector.js
+++ addons/web_unsplash/static/src/components/media_dialog/image_selector.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 import { patch } from 'web.utils';
+import { KeepLast } from "@web/core/utils/concurrency";
 import { MediaDialog, TABS } from '@web_editor/components/media_dialog/media_dialog';
 import { ImageSelector } from '@web_editor/components/media_dialog/image_selector';
 import { useService } from '@web/core/utils/hooks';
@@ -40,6 +41,7 @@ patch(ImageSelector.prototype, 'image_selector_unsplash', {
     setup() {
         this._super();
         this.unsplash = useService('unsplash');
+        this.keepLastUnsplash = new KeepLast();
 
         this.state.unsplashRecords = [];
         this.state.isFetchingUnsplash = false;
@@ -146,9 +148,11 @@ patch(ImageSelector.prototype, 'image_selector_unsplash', {
 
     async loadMore(...args) {
         await this._super(...args);
-        const { records, isMaxed } = await this.fetchUnsplashRecords(this.state.unsplashRecords.length);
-        this.state.unsplashRecords.push(...records);
-        this.state.isMaxed = isMaxed;
+        return this.keepLastUnsplash.add(this.fetchUnsplashRecords(this.state.unsplashRecords.length)).then(({ records, isMaxed }) => {
+            // This is never reached if another search or loadMore occurred.
+            this.state.unsplashRecords.push(...records);
+            this.state.isMaxed = isMaxed;
+        });
     },
 
     async search(...args) {
@@ -162,9 +166,11 @@ patch(ImageSelector.prototype, 'image_selector_unsplash', {
             this.state.unsplashRecords = [];
             this.state.isMaxed = false;
         }
-        const { records, isMaxed } = await this.fetchUnsplashRecords(0);
-        this.state.unsplashRecords = records;
-        this.state.isMaxed = isMaxed;
+        return this.keepLastUnsplash.add(this.fetchUnsplashRecords(0)).then(({ records, isMaxed }) => {
+            // This is never reached if a new search occurred.
+            this.state.unsplashRecords = records;
+            this.state.isMaxed = isMaxed;
+        });
     },
 
     async onClickRecord(media) {
