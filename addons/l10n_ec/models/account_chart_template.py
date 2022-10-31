6a2283f2b6dcda092bc7d3634d5a0a805ca232ae
Stanislas Gueniffey <stgu@odoo.com>
2022-10-31 15:05:14 +0100

    [FIX] AccountChartTemplate: add condition & TODO!?

--- addons/l10n_ec/models/account_chart_template.py
+++ addons/l10n_ec/models/account_chart_template.py
@@ -9,11 +9,14 @@ class AccountChartTemplate(models.Model):
 
     def _prepare_all_journals(self, acc_template_ref, company, journals_dict=None):
         res = super(AccountChartTemplate, self)._prepare_all_journals(acc_template_ref, company, journals_dict=journals_dict)
-        for r in res:
-            if r.get('code') == 'INV':
-                r.update({'name': '001-001 ' + r.get('name'),
-                          'l10n_ec_entity': '001',
-                          'l10n_ec_emission': '001',
-                          'l10n_ec_emission_address_id': company.partner_id.id,
-                          })
+        for journal in res:
+            # TODO figure out why the "account.chart.template" from l10n_ec_edi is being called
+            # TODO move this code to l10n_ec_edi's "account.chart.template" ?
+            if journal.get('code') == 'INV' and company.country_code == 'EC':
+                journal.update({
+                    'name': '001-001 ' + journal.get('name'),
+                    'l10n_ec_entity': '001',
+                    'l10n_ec_emission': '001',
+                    'l10n_ec_emission_address_id': company.partner_id.id,
+                })
         return res
