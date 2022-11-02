0c7b889e1c91985504921659dc6ea715e1b7b779
Valentin Chevalier <vcr@odoo.com>
2022-11-02 14:40:59 +0100

    [IMP] sale: add the possibility to mass cancel SOs
    For a lot of clients, the number of quotations is big, and canceling
    them one by one is time-consuming.
    
    task-2991744

--- addons/sale/wizard/__init__.py
+++ addons/sale/wizard/__init__.py
@@ -8,3 +8,4 @@ from . import payment_provider_onboarding_wizard
 from . import res_config_settings
 from . import sale_make_invoice_advance
 from . import sale_order_cancel
+from . import mass_cancel_orders
