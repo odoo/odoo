7b6501794c9e3e06e4056f75b42afe421045eff9
hupo-odoo <hupo@odoo.com>
2022-11-02 11:12:53 +0100

    [IMP] l10n_dz : Better CoA, Taxes, Tax report and account groups
    Fulfilled the CoA which was incomplete. Now, every account with 3 or more digits are included. Accounts with only 2 digits are considered as tax groups.
    Added the tax report mostly based on the series G°50 official document (only VAT part).
    New taxes have been added to correspond to the tax report.
    New fiscal position have been added.
    A demo company is now available.
    Official docs must be in French, reason why I added a French translation.
    All references can be found in task details
    
    task-3047876
    PR-105320

--- addons/l10n_dz/__manifest__.py
+++ addons/l10n_dz/__manifest__.py
@@ -11,14 +11,21 @@ This is the module to manage the accounting chart for Algeria in Odoo.
 This module applies to companies based in Algeria.
 """,
     'author': 'Osis',
-    'depends': ['account'],
+    'depends': ['account', 'base_vat', 'l10n_multilang'],
     'data': [
         'data/account_chart_template_data.xml',
         'data/account.account.template.csv',
+        'data/account.group.template.csv',
         'data/account_chart_template_post_data.xml',
-        'data/account_tax_data.xml',
-        'data/account_fiscal_position_template_data.xml',
         'data/account_chart_template_configuration_data.xml',
+        'data/tax_report.xml',
+        'data/account_tax_group_data.xml',
+        'data/account_tax_template_data.xml',
+        'data/account_fiscal_position_template_data.xml',
     ],
+    'demo': [
+        'demo/demo_company.xml',
+    ],
+    'post_init_hook': 'load_translations',
     'license': 'LGPL-3',
 }
