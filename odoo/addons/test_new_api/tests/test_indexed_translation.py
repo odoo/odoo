d5e211b1c19c351fa3fc0269fa480428823114e1
Chong Wang (cwg) <cwg@odoo.com>
2022-11-03 17:35:39 +0000

    [FIX] core: fix search with accent
    For a trigram indexed translated field field_x,
    domain leaf ('field_x', '=', value) and ('field_x', 'ilike', pattern)
    will miss records if the pattern contains accent characters
    
    This commit set ensure_ascii=False for json.dumps to avoid escaping special
    characters
    
    opw-3048182
    
    closes odoo/odoo#105240
    
    X-original-commit: 26b5c8c2baeaee9fcd5384aafa5f9b39543d6903
    Signed-off-by: Raphael Collet <rco@odoo.com>
    Signed-off-by: Wang Chong (cwg) <cwg@odoo.com>

--- odoo/addons/test_new_api/tests/test_indexed_translation.py
+++ odoo/addons/test_new_api/tests/test_indexed_translation.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import odoo.tests
+from odoo.addons.base.tests.test_translate import SPECIAL_CHARACTERS
 
 
 @odoo.tests.tagged('post_install', '-at_install')
@@ -10,31 +11,56 @@ class TestIndexedTranslation(odoo.tests.TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.env['res.lang']._activate_lang('fr_FR')
-        cls.record_en = cls.env['test_new_api.indexed_translation'].with_context(lang='en_US').create({})
-        cls.record_fr = cls.record_en.with_context(lang='fr_FR')
-        cls.record_en.name = '<div class="my_class">hello</div>\n<div/>'
-        cls.record_fr.name = '<div class="my_class">%bonjour\\</div>\a<div/>'
 
     def test_search_ilike(self):
-        self.assertEqual(self.record_en.name, '<div class="my_class">hello</div>\n<div/>')
-        self.assertEqual(self.record_fr.name, '<div class="my_class">%bonjour\\</div>\a<div/>')
+        record_en = self.env['test_new_api.indexed_translation'].with_context(lang='en_US').create({})
+        record_fr = record_en.with_context(lang='fr_FR')
+        record_en.name = '<div class="my_class">hello</div>\n<div/>'
+        record_fr.name = '<div class="my_class">%bonjour\\</div>\a<div/>'
+
+        self.assertEqual(record_en.name, '<div class="my_class">hello</div>\n<div/>')
+        self.assertEqual(record_fr.name, '<div class="my_class">%bonjour\\</div>\a<div/>')
 
         # matching double quotes
-        self.assertEqual(self.record_en.search([('name', 'ilike', 'class="my_class')]), self.record_en)
+        self.assertEqual(record_en.search([('name', 'ilike', 'class="my_class')]), record_en)
 
         # escaped and unescaped PG wildcards
-        self.assertEqual(self.record_en.search([('name', 'ilike', 'class%class')]), self.record_en)
-        self.assertEqual(self.record_en.search([('name', 'ilike', r'class="m_\_class')]), self.record_en)
-        self.assertEqual(self.record_en.search([('name', 'ilike', 'bonjour')]), self.record_en.browse())
-        self.assertEqual(self.record_en.search([('name', 'ilike', '</div>\n<div/>')]), self.record_en)
-        self.assertEqual(self.record_fr.search([('name', 'ilike', '</div>\a<div/>')]), self.record_fr)
-        self.assertEqual(self.record_fr.search([('name', 'ilike', r'\%bonjour\\')]), self.record_fr)
+        self.assertEqual(record_en.search([('name', 'ilike', 'class%class')]), record_en)
+        self.assertEqual(record_en.search([('name', 'ilike', r'class="m_\_class')]), record_en)
+        self.assertEqual(record_en.search([('name', 'ilike', 'bonjour')]), record_en.browse())
+        self.assertEqual(record_en.search([('name', 'ilike', '</div>\n<div/>')]), record_en)
+        self.assertEqual(record_fr.search([('name', 'ilike', '</div>\a<div/>')]), record_fr)
+        self.assertEqual(record_fr.search([('name', 'ilike', r'\%bonjour\\')]), record_fr)
 
         self.assertEqual(
-            self.record_en.search([('name', '=', '<div class="my_class">hello</div>\n<div/>')]),
-            self.record_en,
+            record_en.search([('name', '=', '<div class="my_class">hello</div>\n<div/>')]),
+            record_en,
         )
         self.assertEqual(
-            self.record_fr.search([('name', '=', '<div class="my_class">%bonjour\\</div>\a<div/>')]),
-            self.record_fr,
+            record_fr.search([('name', '=', '<div class="my_class">%bonjour\\</div>\a<div/>')]),
+            record_fr,
         )
+
+    def test_search_special_characters(self):
+        name_en = f'{SPECIAL_CHARACTERS}_en'
+        name_fr = f'{SPECIAL_CHARACTERS}_fr'
+        record_en = self.env['test_new_api.indexed_translation'].with_context(lang='en_US').create({})
+        record_fr = record_en.with_context(lang='fr_FR')
+        record_en.name = name_en
+        record_fr.name = name_fr
+
+        self.assertEqual(record_en.search([('name', 'ilike', name_en)]), record_en)
+        self.assertEqual(record_en.search([('name', '=', name_en)]), record_en)
+        self.assertEqual(record_en.search([('name', 'in', [name_en])]), record_en)
+
+        self.assertEqual(record_fr.search([('name', 'ilike', name_fr)]), record_en)
+        self.assertEqual(record_fr.search([('name', '=', name_fr)]), record_en)
+        self.assertEqual(record_fr.search([('name', 'in', [name_fr])]), record_en)
+
+        self.assertFalse(record_fr.search([('name', 'ilike', name_en)]))
+        self.assertFalse(record_fr.search([('name', '=', name_en)]))
+        self.assertFalse(record_fr.search([('name', 'in', [name_en])]))
+
+        self.assertFalse(record_en.search([('name', 'ilike', name_fr)]))
+        self.assertFalse(record_en.search([('name', '=', name_fr)]))
+        self.assertFalse(record_en.search([('name', 'in', [name_fr])]))
