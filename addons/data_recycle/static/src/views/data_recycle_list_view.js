e7a772adbb1b45ad8a8b9cfb44ede9bae249773f
FrancoisGe <fge@odoo.com>
2022-11-04 10:28:34 +0100

    data_recycle

--- addons/data_recycle/static/src/views/data_recycle_list_view.js
+++ addons/data_recycle/static/src/views/data_recycle_list_view.js
@@ -3,21 +3,15 @@
 import { DataCleaningCommonListController } from "@data_recycle/views/data_cleaning_common_list";
 import { registry } from '@web/core/registry';
 import { listView } from '@web/views/list/list_view';
-import { session } from "@web/session";
 
 export class DataRecycleListController extends DataCleaningCommonListController {
     /**
      * Validate all the records selected
      */
     async onValidateClick() {
-        let record_ids;
-        if (this.isDomainSelected) {
-            const domain = this.props.domain;
-            record_ids = await this._domainToResIds(domain, session.active_ids_limit);
-        } else {
-            record_ids = await this.getSelectedResIds();
-        }
-
+        // _domainToResIds is legacy ? It should crash.
+        // getSelectedResIds check isDomainSelected
+        const record_ids = await this.getSelectedResIds();
         await this.orm.call('data_recycle.record', 'action_validate', [record_ids]);
         await this.model.load();
         this.model.notify();
