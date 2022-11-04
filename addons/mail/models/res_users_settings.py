eebc3e048345da3c482c17246d4cd4db4650b019
niyasraphy <niyasraphyk@gmail.com>
2022-11-04 04:26:53 +0000

    [IMP] mail : better breadcrumb for models
    closes odoo/odoo#104955
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/models/res_users_settings.py
+++ addons/mail/models/res_users_settings.py
@@ -7,6 +7,7 @@ from odoo import api, fields, models
 class ResUsersSettings(models.Model):
     _name = 'res.users.settings'
     _description = 'User Settings'
+    _rec_name = 'user_id'
 
     user_id = fields.Many2one('res.users', string="User", required=True, readonly=True, ondelete='cascade')
     is_discuss_sidebar_category_channel_open = fields.Boolean(string="Is discuss sidebar category channel open?", default=True)
