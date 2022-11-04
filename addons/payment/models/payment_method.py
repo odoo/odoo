acb192eafb37600a75fe8e8daafa2b746c69e183
Anita (anko) <anko@odoo.com>
2022-11-04 15:47:35 +0000

    [IMP] payment: require name and image for payment method
    Payment methods without image or without name does not make any sense.
    Previously created payment methods without a name will be
    given default name and ones without an image will be deleted.
    Payment icon can no longer be created or edited from payment
    provider form view.
    
    task-2853424
    
    closes odoo/odoo#105909
    
    Related: odoo/upgrade#4043
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- addons/payment/models/payment_method.py
+++ addons/payment/models/payment_method.py
@@ -8,12 +8,15 @@ class PaymentMethod(models.Model):
     _description = 'Payment Method'
     _order = 'sequence, name'
 
-    name = fields.Char(string="Name")
+    name = fields.Char(string="Name", required=True)
     provider_ids = fields.Many2many(
         string="Providers", comodel_name='payment.provider',
         help="The list of providers supporting this payment method")
     image = fields.Image(
-        string="Image", max_width=64, max_height=64,
+        string="Image",
+        max_width=64,
+        max_height=64,
+        required="True",
         help="This field holds the image used for this payment method, limited to 64x64 px")
     image_payment_form = fields.Image(
         string="Image displayed on the payment form", related='image', store=True, max_width=45,
