48ac799288e76a0256b9ce0712a70ed5abc0f62e
Denis Ledoux <dle@odoo.com>
2022-11-04 12:53:00 +0000

    [FIX] web: "Set Defaults" feature from the debug menu
    Since odoo/odoo#99660
    the `depends` field attribute is no longer passed
    from the server to the web client.
    
    Because of this, the "Set Defaults" feature
    of the debug menu is completely unusable,
    as the list of fields on which to set a default is empty because of it.
    
    The use of this `depends` field attribute has been introduced in
    revision odoo/odoo@ce9bbf6c10962e64138c04ca34b7485084e4cdaa
    In the commit description, we can read that setting a default
    on computed fields having depends has been disabled
    because the defaults set were being overwritten by the call to onchange.
    Note that only the fact the field has depends is required,
    the actual list of field names is not used, just the fact the list is
    not empty.
    
    However, since revision
    odoo/odoo@488e334fc8fd6601e304635fa577c98b9bfd18e6,
    available since Odoo 14.0,
    this is no longer the case, the defaults are no longer being overwritten
    by the call to `onchange`, since the `default_get` has been integrated
    into the `onchange`.
    
    A video attached to the pull request of this revision
    shows the behavior difference.
    
    Hence, setting a default on a computed fields having depends
    (with an inverse method, hence not readonly)
    can be allowed again in this "Set defaults" debug feature,
    therefore avoiding to have to send back the `depends` field attributes
    to the web client.
    
    opw-3031290
    opw-3038711
    opw-3055292
    
    closes odoo/odoo#105009
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

---
+++ addons/contacts/static/tests/tours/debug_menu_set_defaults.js
@@ -0,0 +1,70 @@
+odoo.define('debug_menu_set_defaults.tour', function (require) {
+    "use strict";
+
+    var tour = require('web_tour.tour');
+
+    tour.register('debug_menu_set_defaults', {
+        test: true,
+        url: '/web?debug=1',
+    },
+        [
+            ...tour.stepUtils.goToAppSteps('contacts.menu_contacts', "Open the contacts menu"),
+            {
+                content: "Create a new contact",
+                trigger: '.o-kanban-button-new',
+            },
+            {
+                content: "Check that Company is checked by default, and not Individual",
+                trigger: '.o_field_widget[name="company_type"] input[data-value="company"]:checked',
+                run: function () {},
+            },
+            {
+                content: "Select the individual radio button",
+                trigger: '.o_field_widget[name="company_type"] input[data-value="person"]',
+            },
+            {
+                content: "Open the debug menu",
+                trigger: '.o_debug_manager button',
+            },
+            {
+                content: "Click the Set Defaults menu",
+                trigger: '.o_debug_manager .dropdown-item:contains(Set Defaults)',
+            },
+            {
+                content: "Choose Company Type = Individual",
+                trigger: '#formview_default_fields',
+                run: function () {
+                    const element_field = document.querySelector('select#formview_default_fields');
+                    element_field.value = 'company_type';
+                    element_field.dispatchEvent(new Event("change"));
+                },
+            },
+            {
+                content: "Save the new default",
+                trigger: 'footer button:contains(Save default)',
+            },
+            {
+                content: "Discard the contact creation",
+                trigger: 'button.o_form_button_cancel',
+            },
+            {
+                trigger: '.o-kanban-button-new',
+            },
+            {
+                content: "Check that Individual is checked instead of Company",
+                trigger: '.o_field_widget[name="company_type"] input[data-value="person"]:checked',
+                run: function () {},
+            },
+            {
+                content: "Discard the contact creation",
+                trigger: 'button.o_form_button_cancel',
+            },
+            {
+                content: "Wait for discard",
+                trigger: '.o_control_panel .o-kanban-button-new',
+                run() {},
+            },
+        ]
+    );
+
+});
