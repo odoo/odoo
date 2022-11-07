3c63396e07d9cca603639ada004cff87a92cb4c8
Yannick Tivisse <yti@odoo.com>
2022-11-07 09:00:03 +0000

    [IMP] hr: Add multi company rule on contract types
    Part-of: odoo/odoo#105119

--- addons/hr/models/hr_contract_type.py
+++ addons/hr/models/hr_contract_type.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ContractType(models.Model):
@@ -10,5 +10,13 @@ class ContractType(models.Model):
     _order = 'sequence'
 
     name = fields.Char(required=True, translate=True)
+    code = fields.Char(compute='_compute_code', store=True, readonly=False)
     sequence = fields.Integer()
     country_id = fields.Many2one('res.country')
+
+    @api.depends('name')
+    def _compute_code(self):
+        for contract_type in self:
+            if contract_type.code:
+                continue
+            contract_type.code = contract_type.name
