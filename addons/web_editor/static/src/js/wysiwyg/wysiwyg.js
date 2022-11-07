9cae999d413a71f3591fcbbadeefea1509778c76
Rodopho Cammarosano de Lima (rcdl) <rcdl@odoo.com>
2022-11-07 18:31:00 +0000

    [FIX] web_editor: allow text node at editable root
    An option to disable the automatic wrapping of inline nodes into p at
    editable root is needed by Studio's report editor.
    
    task #2995601
    
    closes odoo/odoo#105585
    
    Related: odoo/enterprise#33886
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -150,6 +150,7 @@ const Wysiwyg = Widget.extend({
             getUnremovableElements: this.options.getUnremovableElements,
             defaultLinkAttributes: this.options.userGeneratedContent ? {rel: 'ugc' } : {},
             allowCommandVideo: this.options.allowCommandVideo,
+            allowInlineAtRoot: this.options.allowInlineAtRoot,
             getYoutubeVideoElement: getYoutubeVideoElement,
             getContextFromParentRect: options.getContextFromParentRect,
             getPowerboxElement: () => {
