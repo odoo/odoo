45cc8fda5190c779f7914b288811d58580884cbd
Malay Khamar <cmk.malay.khamar@gmail.com>
2022-11-08 00:32:25 +0530

    [FIX] (*_)sale(_*): sale report inheritance.
    Provide a hook for clean specification of additional fields, without
    relying on risky default dict method parameters.
    
    Use the existing `_group_by_sale` to cleanly add additional
    "group by" information.
    
    Partial backport of 740a507de690b71cd0387b38177dbd1b21ec5738
    
    X-original-commit: 9c7bdb951e6db10e13e5c1e22e7098fac57b7fbb
    Co-authored-by: Victor Feyens <vfe@odoo.com>

--- addons/pos_sale_margin/report/sale_report.py
+++ addons/pos_sale_margin/report/sale_report.py
@@ -8,5 +8,7 @@ class SaleReport(models.Model):
     _inherit = "sale.report"
 
     def _select_pos(self, fields=None):
+        if not fields:
+            fields = {}
         fields['margin'] = ', SUM(l.price_subtotal - l.total_cost / CASE COALESCE(pos.currency_rate, 0) WHEN 0 THEN 1.0 ELSE pos.currency_rate END) AS margin'
         return super()._select_pos(fields)
