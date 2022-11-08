2c3f870f78e1f2c27a7fffb8c9b575a2de83ce13
Andres Calle <andres.calle@trescloud.com>
2022-11-07 21:01:56 -0500

    [IMP] Use company account_fiscal_country_id
    This way the chart of accounts load methods are called even if the
    country was not set in the company

--- addons/l10n_ec/models/account_chart_template.py
+++ addons/l10n_ec/models/account_chart_template.py
@@ -10,7 +10,7 @@ class AccountChartTemplate(models.Model):
     def _prepare_all_journals(self, acc_template_ref, company, journals_dict=None):
         res = super(AccountChartTemplate, self)._prepare_all_journals(acc_template_ref, company, journals_dict=journals_dict)
         for journal in res:
-            if journal.get('code') == 'INV' and company.country_code == 'EC':
+            if journal.get('code') == 'INV' and company.account_fiscal_country_id.code == 'EC':
                 journal.update({
                     'name': '001-001 ' + journal.get('name'),
                     'l10n_ec_entity': '001',
