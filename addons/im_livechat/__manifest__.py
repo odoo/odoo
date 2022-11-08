c050642da300f73fbf5cda94d2228189900bcd6f
Sébastien Theys <seb@odoo.com>
2022-11-08 12:43:43 +0100

    [MOV] im_livechat: archive old code

--- addons/im_livechat/__manifest__.py
+++ addons/im_livechat/__manifest__.py
@@ -46,9 +46,6 @@ Help your customers with this chat, and analyse their feedback.
     'installable': True,
     'application': True,
     'assets': {
-        'mail.assets_discuss_public': [
-            'im_livechat/static/src/components/*/*',
-        ],
         'web._assets_primary_variables': [
             'im_livechat/static/src/primary_variables.scss',
         ],
@@ -59,19 +56,13 @@ Help your customers with this chat, and analyse their feedback.
             'im_livechat/static/src/legacy/public_livechat_chatbot.js',
             'im_livechat/static/src/legacy/public_livechat.scss',
             'im_livechat/static/src/legacy/public_livechat_chatbot.scss',
-            'mail/static/src/utils/*.js',
-            'mail/static/src/js/utils.js',
-            'mail/static/src/component_hooks/*.js',
-            'mail/static/src/services/messaging_service.js',
         ],
         'web.assets_backend': [
             'im_livechat/static/src/js/colors_reset_button/*',
             'im_livechat/static/src/js/im_livechat_chatbot_steps_one2many.js',
             'im_livechat/static/src/js/im_livechat_chatbot_script_answers_m2m.js',
-            'im_livechat/static/src/components/*/*.js',
             'im_livechat/static/src/scss/im_livechat_history.scss',
             'im_livechat/static/src/scss/im_livechat_form.scss',
-            'im_livechat/static/src/components/*/*.xml',
         ],
         'web.tests_assets': [
             'im_livechat/static/tests/helpers/**/*.js',
@@ -81,9 +72,6 @@ Help your customers with this chat, and analyse their feedback.
         ],
         'web.assets_tests': [
             'im_livechat/static/tests/tours/**/*',
-        ],
-        'mail.assets_messaging': [
-            'im_livechat/static/src/models/*.js',
         ],
         'im_livechat.assets_public_livechat': [
             ('include', 'mail.assets_core_messaging'),
@@ -189,7 +177,6 @@ Help your customers with this chat, and analyse their feedback.
             'bus/static/src/services/legacy/make_bus_service_to_legacy_env.js',
             'bus/static/src/workers/websocket_worker.js',
             'bus/static/src/workers/websocket_worker_utils.js',
-            'mail/static/src/js/utils.js',
             'im_livechat/static/src/legacy/public_livechat_chatbot.js',
 
             ('include', 'web._assets_helpers'),
@@ -203,11 +190,7 @@ Help your customers with this chat, and analyse their feedback.
 
             'web/static/src/core/utils/transitions.scss',
 
-            'mail/static/src/utils/*.js',
-            'mail/static/src/js/emojis.js',
-            'mail/static/src/component_hooks/*.js',
             ('include', 'im_livechat.assets_public_livechat'),
-            'mail/static/src/services/messaging_service.js',
             # Framework JS
             'bus/static/src/*.js',
             'bus/static/src/services/presence_service.js',
