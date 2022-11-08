f07afac69ee1f17c85fd7b27761f661a9f7f8b79
Sébastien Theys <seb@odoo.com>
2022-11-08 13:25:32 +0100

    [MOV] calendar: archive old code

--- addons/calendar/__manifest__.py
+++ addons/calendar/__manifest__.py
@@ -39,15 +39,11 @@ If you need to manage your meetings, you should install the CRM module.
     'installable': True,
     'application': True,
     'assets': {
-        'mail.assets_messaging': [
-            'calendar/static/src/models/*.js',
-        ],
         'web.assets_backend': [
             'calendar/static/src/scss/calendar.scss',
             'calendar/static/src/js/base_calendar.js',
             'calendar/static/src/js/services/calendar_notification_service.js',
             'calendar/static/src/views/**/*',
-            'calendar/static/src/components/*/*.xml',
         ],
         'web.qunit_suite_tests': [
             'calendar/static/tests/**/*',
