5534e0917fa219daab8d735611eb26e731826a41
Fernanda Hernández <fernanda@vauxoo.com>
2022-11-08 21:54:44 +0000

    [FIX] pos_restaurant: use odoo group instead itertools
    closes odoo/odoo#106342
    
    X-original-commit: 92d07db5078ddb9f58a907a32cc7b9322a2792b1
    Signed-off-by: Masereel Pierre <pim@odoo.com>

--- addons/pos_restaurant/models/pos_order.py
+++ addons/pos_restaurant/models/pos_order.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from itertools import groupby
+from odoo.tools import groupby
 from re import search
 from functools import partial
 
