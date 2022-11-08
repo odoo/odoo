df6f9595637534a35c454f30abc502b65e50eeb1
Dossogne Bertrand <bedo@odoo.com>
2022-11-08 13:00:12 +0000

    [IMP] hr_contract: ease contract navigation
    Before this commit, the contract smartbutton on the
    employee page redirected to the contract history page.
    After this commit, the user will be redirected to the running
    contract or any contract if there is some, or towards a new
    contract form otherwise. To access the contract history view,
    a new smartbutton has been added on the contract form.
    
    Also added some hr_responsible_id to contract in demo datas
    to avoid having empty required fields for employee contracts.
    
    task-3056891
    
    closes odoo/odoo#105416
    
    Related: odoo/enterprise#33799
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_contract/models/hr_contract.py
+++ addons/hr_contract/models/hr_contract.py
@@ -41,12 +41,13 @@ class Contract(models.Model):
         ('close', 'Expired'),
         ('cancel', 'Cancelled')
     ], string='Status', group_expand='_expand_states', copy=False,
-       tracking=True, help='Status of the contract', default='draft')
+        tracking=True, help='Status of the contract', default='draft')
     company_id = fields.Many2one('res.company', compute='_compute_employee_contract', store=True, readonly=False,
         default=lambda self: self.env.company, required=True)
     company_country_id = fields.Many2one('res.country', string="Company country", related='company_id.country_id', readonly=True)
     country_code = fields.Char(related='company_country_id.code', depends=['company_country_id'], readonly=True)
     contract_type_id = fields.Many2one('hr.contract.type', "Contract Type")
+    contracts_count = fields.Integer(related='employee_id.contracts_count')
 
     """
         kanban_state:
@@ -227,7 +228,7 @@ class Contract(models.Model):
             self._assign_open_contract()
         today = fields.Date.today()
         for contract in self:
-            if contract == contract.employee_id.contract_id \
+            if contract == contract.sudo().employee_id.contract_id \
                 and old_state[contract.id] == 'open' \
                 and new_state[contract.id] != 'open':
                 running_contract = self.env['hr.contract'].search([
@@ -281,4 +282,10 @@ class Contract(models.Model):
             'views': [(self.env.ref('hr_contract.hr_contract_view_form').id, 'form')],
             'res_id': self.id,
         })
+        return action
+
+    def action_open_contract_history(self):
+        self.ensure_one()
+        action = self.env["ir.actions.actions"]._for_xml_id('hr_contract.hr_contract_history_view_form_action')
+        action['res_id'] = self.employee_id.id
         return action
