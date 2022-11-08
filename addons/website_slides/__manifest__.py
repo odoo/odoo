673f3c9a7d84057286b294314efbafb7695eebe2
Sébastien Theys <seb@odoo.com>
2022-11-08 13:37:05 +0100

    [MOV] website_slides: archive old code

--- addons/website_slides/__manifest__.py
+++ addons/website_slides/__manifest__.py
@@ -68,16 +68,12 @@ Featuring
     'installable': True,
     'application': True,
     'assets': {
-        'mail.assets_messaging': [
-            'website_slides/static/src/models/*.js',
-        ],
         'web.assets_backend': [
             'website_slides/static/src/slide_category_one2many_field.js',
             'website_slides/static/src/slide_category_list_renderer.js',
             'website_slides/static/src/scss/slide_views.scss',
             'website_slides/static/src/js/slide_category_one2many.js',
             'website_slides/static/src/js/components/**/*.js',
-            'website_slides/static/src/components/**/*.xml',
         ],
         'web.assets_frontend': [
             'website_slides/static/src/scss/website_slides.scss',
