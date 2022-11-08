373e9ea58a9358dca2be3c8adfb515cb98f7398c
AaronHForgeFlow <ahenriquez@eficent.com>
2022-11-08 11:24:18 +0100

    [FIX] sale_stock: Do not launch procurement when reducing quantity in the SO

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -551,7 +551,7 @@ class SaleOrderLine(models.Model):
             if line.state != 'sale' or not line.product_id.type in ('consu','product'):
                 continue
             qty = line._get_qty_procurement(previous_product_uom_qty)
-            if float_compare(qty, line.product_uom_qty, precision_digits=precision) == 0:
+            if float_compare(qty, line.product_uom_qty, precision_digits=precision) <= 0:
                 continue
 
             group_id = line._get_procurement_group()
