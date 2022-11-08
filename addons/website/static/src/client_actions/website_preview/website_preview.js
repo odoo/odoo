4817f45fc33cbe8899abfb8da2d037d766c0fed9
Sébastien Theys <seb@odoo.com>
2022-11-08 15:36:52 +0100

    [FIX] website: TEMP comment chat window code for website in backend

--- addons/website/static/src/client_actions/website_preview/website_preview.js
+++ addons/website/static/src/client_actions/website_preview/website_preview.js
@@ -97,15 +97,15 @@ export class WebsitePreview extends Component {
             // OdooFrameContentLoaded event to unblock the iframe, as it is
             // triggered faster than the load event.
             this.iframe.el.addEventListener('OdooFrameContentLoaded', () => this.websiteService.unblockPreview('load-iframe'), { once: true });
-            this.env.services.messaging.modelManager.messagingCreatedPromise.then(() => {
-                this.env.services.messaging.modelManager.messaging.update({ isWebsitePreviewOpen: true });
-            });
+            // this.env.services.messaging.modelManager.messagingCreatedPromise.then(() => {
+            //     this.env.services.messaging.modelManager.messaging.update({ isWebsitePreviewOpen: true });
+            // });
         });
 
         onWillUnmount(() => {
-            this.env.services.messaging.modelManager.messagingCreatedPromise.then(() => {
-                this.env.services.messaging.modelManager.messaging.update({ isWebsitePreviewOpen: false });
-            });
+            // this.env.services.messaging.modelManager.messagingCreatedPromise.then(() => {
+            //     this.env.services.messaging.modelManager.messaging.update({ isWebsitePreviewOpen: false });
+            // });
             const { pathname, search, hash } = this.iframe.el.contentWindow.location;
             this.websiteService.lastUrl = `${pathname}${search}${hash}`;
             this.websiteService.currentWebsiteId = null;
