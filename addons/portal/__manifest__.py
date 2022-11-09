505028cd8d9d0f37e8db8ecec787ffa64893eec3
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-09 09:52:32 +0100

    [IMP] portal: Add rule for partner managers in portal share.

--- addons/portal/__manifest__.py
+++ addons/portal/__manifest__.py
@@ -17,6 +17,7 @@ of this module is to allow the display of a customer portal without having
 a dependency towards website editing and customization capabilities.""",
     'depends': ['web', 'web_editor', 'http_routing', 'mail', 'auth_signup'],
     'data': [
+        'security/portal_security.xml',
         'security/ir.model.access.csv',
         'data/portal_data.xml',
         'views/assets.xml',
