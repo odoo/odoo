c495bf7433b13507729e672c638e561ec2b5ad93
Jacky (trj) <trj@odoo.com>
2022-11-09 08:58:39 +0000

    [FIX] point_of_sale,pos_sale,l10n_fr_pos_cert: add error popups back
    Due to bc8d7590c04a0f1391021c8d1354f1eabca9742c, the error popups were mistakenly removed.
    
    closes odoo/odoo#105403
    
    Signed-off-by: Heinz Robin (rhe) <rhe@odoo.com>

--- addons/point_of_sale/static/src/js/Popups/ProductInfoPopup.js
+++ addons/point_of_sale/static/src/js/Popups/ProductInfoPopup.js
@@ -3,36 +3,17 @@ odoo.define('point_of_sale.ProductInfoPopup', function(require) {
 
     const AbstractAwaitablePopup = require('point_of_sale.AbstractAwaitablePopup');
     const Registries = require('point_of_sale.Registries');
-    const { ConnectionLostError } = require('@web/core/network/rpc_service')
 
     /**
-     * This popup needs to be self-dependent because it needs to be called from different place. In order to avoid code
      * Props:
      *  {
-     *      product: a product object
-     *      quantity: number
+     *      info: {object of data}
      *  }
      */
     class ProductInfoPopup extends AbstractAwaitablePopup {
         constructor() {
             super(...arguments);
             Object.assign(this, this.props.info)
-        }
-        /*
-         * Since this popup need to be self dependent, in case of an error, the popup need to be closed on its own.
-         */
-        mounted() {
-            if (this.error) {
-                this.cancel();
-                if (this.error.message instanceof ConnectionLostError) {
-                    this.showPopup('ErrorPopup', {
-                        title: this.env._t('Network Error'),
-                        body: this.env._t('Cannot access product information screen if offline.'),
-                    });
-                } else {
-                    throw this.error;
-                }
-            }
         }
         searchProduct(productName) {
             this.env.posbus.trigger('search-product-from-info-popup', productName);
