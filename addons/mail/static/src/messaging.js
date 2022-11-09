861735b2d7ef8599a354c096170142f66821016a
Sébastien Theys <seb@odoo.com>
2022-11-09 14:35:53 +0100

    [FIX] mail: fix "compute" of chatPartnerId
    To use proper data and take current partner into account.

--- addons/mail/static/src/messaging.js
+++ addons/mail/static/src/messaging.js
@@ -158,12 +158,13 @@ export class Messaging {
                 const avatarCacheKey = data.serverData.channel.avatarCacheKey;
                 for (const elem of data.serverData.channel.channelMembers[0][1]) {
                     this.createPartner(elem.persona.partner.id, elem.persona.partner.name);
-                }
-                for (const partner of data.serverData.seen_partners_info) {
-                    if (partner.partner_id !== this.user.partnerId) {
-                        thread.chatPartnerId = partner.partner_id;
-                        thread.name = this.partners[partner.partner_id].name;
-                        break;
+                    if (
+                        elem.persona.partner.id !== this.user.partnerId ||
+                        (data.serverData.channel.channelMembers[0][1].length === 1 &&
+                            elem.persona.partner.id === this.user.partnerId)
+                    ) {
+                        thread.chatPartnerId = elem.persona.partner.id;
+                        thread.name = this.partners[elem.persona.partner.id].name;
                     }
                 }
                 thread.imgUrl = `/web/image/res.partner/${thread.chatPartnerId}/avatar_128?unique=${avatarCacheKey}`;
