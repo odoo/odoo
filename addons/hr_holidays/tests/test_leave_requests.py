28ccbbab07a0c25570ca9b940428b8e541d262d7
Tom De Caluwé <tdc@odoo.com>
2022-11-09 07:23:23 +0000

    [FIX] hr_holidays: ignore unconfirmed leaves in current leave status
    As of this commit, only confirmed leaves will be considered when computing the
    current leave or absence of an employee. Additionally, a bug in the is_absent
    search implementation was fixed: before this commit, the same results were
    returned for absent and non-absent searches.
    
    opw-2877328
    
    closes odoo/odoo#123999
    
    X-original-commit: 21910ec0167b35e3fef977f102debd34a6249933
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_holidays/tests/test_leave_requests.py
+++ addons/hr_holidays/tests/test_leave_requests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import time
 from dateutil.relativedelta import relativedelta
 from freezegun import freeze_time
@@ -1057,3 +1057,48 @@ class TestLeaveRequests(TestHrHolidaysCommon):
 
         with self.assertRaises(ValidationError):
             self.env['hr.leave'].with_user(self.user_employee_id).create(trigger_error_leave)
+
+    def test_current_leave_status(self):
+        types = ('no_validation', 'manager', 'hr', 'both')
+        employee = self.employee_emp
+
+        def run_validation_flow(leave_validation_type):
+            LeaveType = self.env['hr.leave.type'].with_user(self.user_hrmanager_id)
+            leave_type = LeaveType.with_context(tracking_disable=True).create({
+                'name': leave_validation_type.capitalize(),
+                'leave_validation_type': leave_validation_type,
+                'requires_allocation': 'no',
+            })
+            current_leave = self.env['hr.leave'].with_user(self.user_employee_id).create({
+                'name': 'Holiday Request',
+                'holiday_type': 'employee',
+                'employee_id': employee.id,
+                'holiday_status_id': leave_type.id,
+                'date_from': datetime.today() - timedelta(days=1),
+                'date_to': datetime.today() + timedelta(days=1),
+            })
+
+            if leave_validation_type in ('manager', 'both'):
+                self.assertFalse(employee.is_absent)
+                self.assertFalse(employee.current_leave_id)
+                self.assertEqual(employee.filtered_domain([('is_absent', '=', False)]), employee)
+                self.assertFalse(employee.filtered_domain([('is_absent', '=', True)]))
+                current_leave.with_user(self.user_hruser_id).action_approve()
+
+            if leave_validation_type in ('hr', 'both'):
+                self.assertFalse(employee.is_absent)
+                self.assertFalse(employee.current_leave_id)
+                self.assertEqual(employee.filtered_domain([('is_absent', '=', False)]), employee)
+                self.assertFalse(employee.filtered_domain([('is_absent', '=', True)]))
+                current_leave.with_user(self.user_hrmanager_id).action_validate()
+
+            self.assertTrue(employee.is_absent)
+            self.assertEqual(employee.current_leave_id, current_leave.holiday_status_id)
+            self.assertFalse(employee.filtered_domain([('is_absent', '=', False)]))
+            self.assertEqual(employee.filtered_domain([('is_absent', '=', True)]), employee)
+
+            raise RuntimeError()
+
+        for leave_validation_type in types:
+            with self.assertRaises(RuntimeError), self.env.cr.savepoint():
+                run_validation_flow(leave_validation_type)
