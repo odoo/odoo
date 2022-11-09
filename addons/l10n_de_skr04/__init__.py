25801a6ab026caf9de02becba6b823cc46683ea0
maximilien(malb) <malb@odoo.com>
2022-11-09 13:43:29 +0100

    [FIX] l10n_de_skr04: translation
    Ideally, we  develop in English any localization (then translate to the domestic language, in this case German)
    All the module has been translated in english and then a .po file has been create to translate back in German
    
    task-id: 3059115

--- addons/l10n_de_skr04/__init__.py
+++ addons/l10n_de_skr04/__init__.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, SUPERUSER_ID
+
+def load_translations(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('l10n_de_skr04.l10n_chart_de_skr04').process_coa_translations()
