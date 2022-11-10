a95a0dca20a26f95d7d815ea32d17233f0897d1c
Sébastien Theys <seb@odoo.com>
2022-11-10 17:51:57 +0100

    [IMP] mail: restore activity_exception

--- addons/mail/static/src/views/fields/activity_exception/activity_exception.js
+++ addons/mail/static/src/views/fields/activity_exception/activity_exception.js
@@ -3,26 +3,24 @@
 import { registry } from "@web/core/registry";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 
-import { Component } from '@odoo/owl';
+import { Component } from "@odoo/owl";
 
 class ActivityException extends Component {
-
     get textClass() {
         if (this.props.value) {
-            return 'text-' + this.props.value + ' fa ' + this.props.record.data.activity_exception_icon;
+            return `text-${this.props.value} fa ${this.props.record.data.activity_exception_icon}`;
         }
         return undefined;
     }
-
 }
 
 Object.assign(ActivityException, {
-    props: standardFieldProps,
-    template: 'mail.ActivityException',
     fieldDependencies: {
-        activity_exception_icon: { type: 'char' },
+        activity_exception_icon: { type: "char" },
     },
     noLabel: true,
+    props: standardFieldProps,
+    template: "mail.ActivityException",
 });
 
-registry.category('fields').add('activity_exception', ActivityException);
+registry.category("fields").add("activity_exception", ActivityException);
