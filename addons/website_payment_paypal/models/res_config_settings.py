8e63e0379abf4ed4505e7a21b1f768d5ae8a4e6e
niyasraphy <niyasraphyk@gmail.com>
2022-11-11 09:38:35 +0300

    [FIX] website_payment_paypal: trace back accessing settings after deleting paypal
    traceback is raised in settings page and on clicking activate stripe button after deleting the paypal payment provider from the database

--- addons/website_payment_paypal/models/res_config_settings.py
+++ addons/website_payment_paypal/models/res_config_settings.py
@@ -12,12 +12,13 @@ class ResConfigSettings(models.TransientModel):
     @api.model
     def get_values(self):
         res = super().get_values()
-        paypal = self.env.ref('payment.payment_provider_paypal').sudo()
-        res['paypal_email_account'] = paypal.paypal_email_account
+        paypal = self.env.ref('payment.payment_provider_paypal', raise_if_not_found=False)
+        if paypal:
+            res['paypal_email_account'] = paypal.sudo().paypal_email_account
         return res
 
     def set_values(self):
         super().set_values()
-        paypal = self.env.ref('payment.payment_provider_paypal').sudo()
-        if paypal.paypal_email_account != self.paypal_email_account:
-            paypal.paypal_email_account = self.paypal_email_account
+        paypal = self.env.ref('payment.payment_provider_paypal', raise_if_not_found=False)
+        if paypal and paypal.sudo().paypal_email_account != self.paypal_email_account:
+            paypal.sudo().paypal_email_account = self.paypal_email_account
