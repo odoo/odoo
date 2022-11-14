f3986bfac1403692ab2bb8e5200fe56d1eeb19b5
aath-odoo <aath@odoo.com>
2022-11-14 12:52:28 +0000

    [FIX] web_editor: command prompt not displaying on focus
    Before this commit:
    
    command prompt is not displayed when html field has placeholder.
    
    After this commit:
    
    placeholder is replaced with command prompt when focused.
    
    Task-2881079
    
    closes odoo/odoo#106305
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -2895,7 +2895,8 @@ export class OdooEditor extends EventTarget {
         }
 
         // placeholder hint
-        if (this.editable.textContent === '' && this.options.placeholder) {
+        const sel = this.document.getSelection();
+        if (this.editable.textContent.trim() === '' && this.options.placeholder && !this.editable.contains(sel.focusNode) ) {
             this._makeHint(this.editable.firstChild, this.options.placeholder, true);
         }
     }
