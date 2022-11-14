dad331289db9840592af28fd94e5dbd7aa1e6cfd
Loan (lse) <lse@odoo.com>
2022-11-14 18:17:59 +0100

    [FIX] pos_adyen: JS error in loop if deleted Adyen payment line
    Before this commit:
     If we remove a payment line using an Adyen payment method,
     `pending_adyen_line()` return `undefined`.
     With the `_poll_for_response` still being executed,
     it will pop some JS traceback each call with:
     ```js
     TypeError: Cannot read properties of undefined (reading 'terminalServiceId')
     ```
    
    After this commit:
     No JS traceback loop
    
    OPW-3032391

--- addons/pos_adyen/static/src/js/payment_adyen.js
+++ addons/pos_adyen/static/src/js/payment_adyen.js
@@ -212,7 +212,7 @@ var PaymentAdyen = PaymentInterface.extend({
         }).then(function (status) {
             var notification = status.latest_response;
             var order = self.pos.get_order();
-            var line = self.pending_adyen_line();
+            var line = self.pending_adyen_line() || resolve(false);
 
             if (notification && notification.SaleToPOIResponse.MessageHeader.ServiceID == line.terminalServiceId) {
                 var response = notification.SaleToPOIResponse.PaymentResponse.Response;
