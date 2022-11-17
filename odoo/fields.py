96630a9cc918b63f96b0f60cebeb668c33ff7541
Chong Wang (cwg) <cwg@odoo.com>
2022-11-17 15:28:01 +0100

    [FIX] core: drop mismatched and illegal model terms
    The model terms translations assume the number of terms in each language of a
    model_term translated field should be the same.
    However, sometimes the assumption cannot be promised for sake of bad
    translations
    
    This commit
    1. drops illegal model term translations while translating
    2. drops mismatched terms at run-time in case the database has been contaminated
    
    X-original-commit: f9ab5ca3e99b2883ada18a8f6deb12d45608b43f

--- odoo/fields.py
+++ odoo/fields.py
@@ -1732,8 +1732,12 @@ class _String(Field):
 
         for lang, to_lang_value in to_lang_values.items():
             to_lang_terms = self.get_trans_terms(to_lang_value)
-            for from_lang_term, to_lang_term in zip(from_lang_terms, to_lang_terms):
-                dictionary[from_lang_term].update({lang: to_lang_term})
+            if len(from_lang_terms) != len(to_lang_terms):
+                for from_lang_term in from_lang_terms:
+                    dictionary[from_lang_term][lang] = from_lang_term
+            else:
+                for from_lang_term, to_lang_term in zip(from_lang_terms, to_lang_terms):
+                    dictionary[from_lang_term][lang] = to_lang_term
         return dictionary
 
     def _get_stored_translations(self, record):
