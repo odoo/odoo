93bbb67a9541b935b0193eb65c5dd7b99757d7f9
Jinjiu Liu <jili@odoo.com>
2022-11-17 10:48:02 +0100

    [FIX] sale_stock: formatting and moving the test in 15.2
    This is a patch for a previous fix. From 15.2, the file test_report is
    removed, thus the added tests should be in test_sale_stock_report.py.
    The code spacing in account_move.py is also optimized in all versions.

--- addons/sale_stock/models/account_move.py
+++ addons/sale_stock/models/account_move.py
@@ -62,9 +62,9 @@ class AccountMove(models.Model):
 
             # is it a stock return considering the document type (should it be it thought of as positively or negatively?)
             is_stock_return = (
-                    self.move_type == 'out_invoice' and (sml.location_id.usage, sml.location_dest_id.usage) == ('customer', 'internal')
-                    or
-                    self.move_type == 'out_refund' and (sml.location_id.usage, sml.location_dest_id.usage) == ('internal', 'customer')
+                self.move_type == 'out_invoice' and (sml.location_id.usage, sml.location_dest_id.usage) == ('customer', 'internal')
+                or
+                self.move_type == 'out_refund' and (sml.location_id.usage, sml.location_dest_id.usage) == ('internal', 'customer')
             )
             if is_stock_return:
                 returned_qty = min(qties_per_lot[sml.lot_id], qty_done)
