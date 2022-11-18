e631db812848442ce4c48a26d7ca8fd93c56ae3e
pedrambiria <pebr@odoo.com>
2022-11-18 13:48:27 +0000

    [FIX] pos_coupon: send only the id (int) of program_id to UI
    Before this commit: when you use the pos restaurant, it will send the
    program_id with its id and name([id(int), name(str)]). But it should only
    send its id.
    
    Steps to reproduce the issue:
     1. Open a new session in a pos_restaurant
     2. Create a new order and add some products
     3. Click on Orders
     4. Click on New Orde and add some products
     5. Go to the Floor screen
     6. Click the table you created the orders
     7. Click on Orders
     8. Click on any of two orders
     9. Click on the Floors button
    
    You will get this Error in the log:
    ```
    INSERT INTO "pos_order_line" ("id", "coupon_id", "create_date", "create_uid", "discount", "full_product_name", "is_program_reward", "mp_dirty", "mp_skip", "name", "note", "order_id", "price_subtotal", "price_subtotal_incl", "price_unit", "product_id", "program_id", "qty", "write_date", "write_uid") VALUES (nextval('pos_order_line_id_seq'), NULL, '2022-11-10 19:56:49.031959', 2, 0.0, '10.0% discount on total amount', true, false, false, 'Bar/0014', '', 2,  -0.22,  -0.22,  -0.22, 205, ARRAY[5,'Code for 10% on orders (Automatic)'], '1.00', '2022-11-10 19:56:49.031959', 2) RETURNING id
    ERROR: invalid input syntax for type integer: "Code for 10% on orders (Automatic)"
    LINE 1: ...014', '', 2,  -0.22,  -0.22,  -0.22, 205, ARRAY[5,'Code for ...
    ```
    
    opw-3064038
    
    X-original-commit: cc17b784f384deea882a05a34e99122c60357913

--- addons/pos_coupon/static/src/js/coupon.js
+++ addons/pos_coupon/static/src/js/coupon.js
@@ -238,8 +238,8 @@ odoo.define('pos_coupon.pos', function (require) {
             this._initializePrograms();
         }
         init_from_JSON(json) {
-            this.bookedCouponCodes = this.bookedCouponCodes ? this.order.bookedCouponCodes : {};
-            this.activePromoProgramIds = this.activePromoProgramIds ? this.order.activePromoProgramIds : [];
+            this.bookedCouponCodes = json.bookedCouponCodes ? json.bookedCouponCodes : {};
+            this.activePromoProgramIds = json.activePromoProgramIds ? json.activePromoProgramIds : [];
             super.init_from_JSON(...arguments);
         }
         export_as_JSON() {
@@ -1122,9 +1122,10 @@ odoo.define('pos_coupon.pos', function (require) {
                 this.program_id = json.program_id;
                 this.coupon_id = json.coupon_id;
                 if (this.coupon_id && this.coupon_id[1]) {
-                    this.order.bookedCouponCodes[this.coupon_id[1]] = new CouponCode(this.coupon_id[1], this.coupon_id[0], this.program_id[0]);
-                } else if (json.program_id && json.program_id[0]) {
-                    this.order.activePromoProgramIds.push(json.program_id[0]);
+                    this.order.bookedCouponCodes[this.coupon_id[1]] = new CouponCode(this.coupon_id[1], this.coupon_id[0], this.program_id);
+                    this.coupon_id = json.coupon_id[0];
+                } else if (json.program_id && this.order.activePromoProgramIds.length === 0) {
+                    this.order.activePromoProgramIds.push(json.program_id);
                 }
             }
             super.init_from_JSON(...arguments);
