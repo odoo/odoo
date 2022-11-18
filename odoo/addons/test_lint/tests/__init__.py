f1f6a6f6d23fcdf4af12a9addb94e13065108da1
Simon Genin (ges) <ges@odoo.com>
2022-11-18 13:04:36 +0000

    [IMP] test_lint: remove escheck test
    As of 16.1, the ecmascript version accepted in odoo is 2022.
    This basically makes escheck almost irrelevant.
    Escheck being only able to check up to the version 2021, it can be
    removed safely.
    
    closes odoo/odoo#106052
    
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- odoo/addons/test_lint/tests/__init__.py
+++ odoo/addons/test_lint/tests/__init__.py
@@ -2,7 +2,6 @@ from . import test_dunderinit
 from . import test_checkers
 from . import test_pylint
 from . import test_pofile
-from . import test_ecmascript
 from . import test_eslint
 from . import test_jstranslate
 from . import test_l10n
