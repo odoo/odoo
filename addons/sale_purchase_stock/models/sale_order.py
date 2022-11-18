706f9286f4bbca97ef9159b2ce009de829275e86
clesgow <quwo@odoo.com>
2022-11-18 15:19:11 +0000

    [IMP] *purchase*: Allow multiple created_purchase_line on a single move
    Problem case :
    - Create a product with MTO and Buy route activated.
    - Create a Sale Order containing this product.
    - Confirm the Sale Order, this will generate a Purchase Order for this
    product.
    - Go to Alternatives -> Create Alternative -> Select another vendor &
    select "Copy products".
    
    The alternative PO won't be linked to the Sale Order, as this link is
    done through the relation `created_purchase_line_id` <-> `move_dest_ids`
    respectively on the `stock.move` and the `purchase.order.line`.
    The only way to enable multiple Purchase Order to be linked to a single
    move is to extend the number of `created_purchase_line_id` linked to a
    move, hence changing it to a Many2Many relation.
    
    Then, when an alternative PO is created, we link the original PO lines
    `move_dest_ids` to the newly created PO lines, which correctly links the
    SO and the alternative PO.
    
    closes odoo/odoo#106411
    
    Related: odoo/upgrade#4116
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/sale_purchase_stock/models/sale_order.py
+++ addons/sale_purchase_stock/models/sale_order.py
@@ -7,9 +7,9 @@ from odoo import api, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.depends('procurement_group_id.stock_move_ids.created_purchase_line_id.order_id', 'procurement_group_id.stock_move_ids.move_orig_ids.purchase_line_id.order_id')
+    @api.depends('procurement_group_id.stock_move_ids.created_purchase_line_ids.order_id', 'procurement_group_id.stock_move_ids.move_orig_ids.purchase_line_id.order_id')
     def _compute_purchase_order_count(self):
         super(SaleOrder, self)._compute_purchase_order_count()
 
     def _get_purchase_orders(self):
-        return super(SaleOrder, self)._get_purchase_orders() | self.procurement_group_id.stock_move_ids.created_purchase_line_id.order_id | self.procurement_group_id.stock_move_ids.move_orig_ids.purchase_line_id.order_id
+        return super(SaleOrder, self)._get_purchase_orders() | self.procurement_group_id.stock_move_ids.created_purchase_line_ids.order_id | self.procurement_group_id.stock_move_ids.move_orig_ids.purchase_line_id.order_id
