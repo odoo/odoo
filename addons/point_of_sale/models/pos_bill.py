d45258d56358dbfda4516dedf98d5aee66e63c93
Joanna <joanna@kaist.ac.kr>
2022-11-19 12:43:51 +0900

    [IMP] pos_bill: fix typo in model definition

--- addons/point_of_sale/models/pos_bill.py
+++ addons/point_of_sale/models/pos_bill.py
@@ -7,10 +7,14 @@ class Bill(models.Model):
     _description = "Coins/Bills"
 
     name = fields.Char("Name")
-    value = fields.Float("Coin/Bill Value", required=True, digits=0)
+    value = fields.Float("Coin/Bill Value", required=True)
     pos_config_ids = fields.Many2many("pos.config", string="Point of Sales")
 
     @api.model
     def name_create(self, name):
         result = super().create({"name": name, "value": float(name)})
         return result.name_get()[0]
+
+    @api.onchange('value')
+    def onchange_value(self):
+        self.name = self.value
