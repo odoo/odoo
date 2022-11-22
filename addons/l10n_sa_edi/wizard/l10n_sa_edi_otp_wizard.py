d5f237b34b0f564d16ac0c46f86aa7d2aedca700
Mehdi Bendali Hacine <mebe@odoo.com>
2022-11-22 06:08:58 +0000

    [ADD] l10n_sa_edi: Implement Saudi ZATCA invoicing standards
    The ZATCA edi has an onboarding process which happens per journal.
    A private key is generated for the company and with the data from
    the company and journal a CSR is made to get a certificate to sign the
    invoices.  In that process that contains multiple steps (see account_journal.py),
    we also have to send the compliance files which are some example (simplified or not)
    invoices/debit and credit notes.  We have a separate folder with those files
    and also use them in the tests.  For the onboarding, they still need to be signed however.
    
    For the sending of the customer invoices themselves, UBL 2.1 is used, but
    with ZATCA style adaptations.  That is why we inherit from that class
    to be able to add those specific adaptations.  This UBL needs to be
    signed XadeS and in this case we need to do the hash with the sign information
    present but with empty tags.
    
    For ZATCA invoices, as is the case with Ticketbai, the previous hash is needed
    for the next invoice and is stored per journal.  (so we have a chain of hashes)
    
    Tests written by Simon (smdc)
    
    closes odoo/odoo#126353
    
    X-original-commit: caf6b0f2e09e7175221894cbb6d22bd6306c4e53
    Signed-off-by: Josse Colpaert <jco@odoo.com>

---
+++ addons/l10n_sa_edi/wizard/l10n_sa_edi_otp_wizard.py
@@ -0,0 +1,28 @@
+from odoo import fields, models, _, api
+from odoo.exceptions import UserError
+
+
+class RequestZATCAOtp(models.TransientModel):
+    _name = 'l10n_sa_edi.otp.wizard'
+    _description = 'Request ZATCA OTP'
+
+    l10n_sa_renewal = fields.Boolean("PCSID Renewal",
+                                     help="Used to decide whether we should call the PCSID renewal API or the CCSID API",
+                                     default=False)
+    l10n_sa_otp = fields.Char("OTP", copy=False, help="OTP required to get a CCSID. Can only be acquired through "
+                                                      "the Fatoora portal.")
+    journal_id = fields.Many2one('account.journal', default=lambda self: self.env.context.get('active_id'), required=True)
+
+    @api.model
+    def default_get(self, fields):
+        res = super().default_get(fields)
+        if self.env.company.l10n_sa_api_mode == 'sandbox':
+            res['l10n_sa_otp'] = '123456' if self.l10n_sa_renewal else '123345'
+        return res
+
+    def validate(self):
+        if not self.l10n_sa_otp:
+            raise UserError(_("You need to provide an OTP to be able to request a CCSID"))
+        if self.l10n_sa_renewal:
+            return self.journal_id._l10n_sa_get_production_CSID(self.l10n_sa_otp)
+        self.journal_id._l10n_sa_api_onboard_journal(self.l10n_sa_otp)
