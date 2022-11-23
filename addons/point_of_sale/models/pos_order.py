55f1a6ecd7afc5bc4426cd330fb6986f2ad892fe
Brice bib Bartoletti <bib@odoo.com>
2022-11-23 17:10:05 +0000

    [FIX] account,pos,stock_account:add/update indexes
    1) forward-port of this PR: https://github.com/odoo/odoo/pull/97000
       commit: c9209664ce3a4b10142bba4da6afb912afc983c2
    
    2) use btree_not_null instead of simple btree for space optimisation
    
    closes odoo/odoo#106484
    
    X-original-commit: b743e736f764fd7d6e232d2c8eaf6b2e314c5f31
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -265,7 +265,7 @@ class PosOrder(models.Model):
         [('draft', 'New'), ('cancel', 'Cancelled'), ('paid', 'Paid'), ('done', 'Posted'), ('invoiced', 'Invoiced')],
         'Status', readonly=True, copy=False, default='draft')
 
-    account_move = fields.Many2one('account.move', string='Invoice', readonly=True, copy=False, index=True)
+    account_move = fields.Many2one('account.move', string='Invoice', readonly=True, copy=False, index="btree_not_null")
     picking_ids = fields.One2many('stock.picking', 'pos_order_id')
     picking_count = fields.Integer(compute='_compute_picking_count')
     failed_pickings = fields.Boolean(compute='_compute_picking_count')
