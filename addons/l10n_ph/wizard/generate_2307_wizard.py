cbb943b7cfa529edcdc14c95710b6bcddfd28276
Tommy (tong) <tong@odoo.com>
2022-11-23 12:12:22 +0800

    [FIX] l10n_ph: cannot open vendor bill form view in generate 2307 wizard
    Steps to reproduce:
    - Create a vendor bill and add an invoice line with taxes from l10n_ph
    - Open generate 2307 wizard view
    - Open the vendor bill form view from the wizard view
    
    Current behavior:
    Exception prompt.
    
    Expected behavior:
    Should be able to see the vendor bill inside the wizard view in readonly
    mode.
    
    Explanation:
    With this modification there are two changes:
    
    1) Wizard is using FormRenderer while account.move form is using
    AccountMoveFormRenderer. Therefore it doesn't have a reference of the
    default form view `AccountMoveFormNotebook`. To fix the issue, I copy the
    view from account.move to make it work.
    
    2) When no product is set on the invoice line, generating 2307 report will display
    exception due to cannot find the product name. Now it should show empty when no
    product is set on the invoice line.
    
    X-original-commit: bd11387

--- addons/l10n_ph/wizard/generate_2307_wizard.py
+++ addons/l10n_ph/wizard/generate_2307_wizard.py
@@ -57,7 +57,7 @@ class Generate2307Wizard(models.TransientModel):
             }
             for invoice_line in move.invoice_line_ids.filtered(lambda l: l.display_type not in ('line_note', 'line_section')):
                 for tax in invoice_line.tax_ids.filtered(lambda x: x.l10n_ph_atc):
-                    values['product_name'] = re.sub(r'[\(\)]', '', invoice_line.product_id.name)
+                    values['product_name'] = re.sub(r'[\(\)]', '', invoice_line.product_id.name) if invoice_line.product_id else ''
                     values['atc'] = tax.l10n_ph_atc
                     values['price_subtotal'] = invoice_line.price_subtotal
                     values['amount'] = tax.amount
