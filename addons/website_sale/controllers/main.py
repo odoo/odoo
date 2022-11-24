073439b806b369d06698de5a48e9ed488d686da3
Donatas <donatas@vialaurea.lt>
2022-11-24 17:18:35 +0200

    [IMP] website_sale: adds search_product to shop values

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -386,6 +386,7 @@ class WebsiteSale(http.Controller):
             'pricelist': pricelist,
             'add_qty': add_qty,
             'products': products,
+            'search_product': search_product,
             'search_count': product_count,  # common for all searchbox
             'bins': TableCompute().process(products, ppg, ppr),
             'ppg': ppg,
