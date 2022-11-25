400ed64726294e4a8c9864cf6c8a5fa99175824e
luvi <luvi@odoo.com>
2022-11-25 12:35:25 +0000

    [IMP] web_editor: use of the useSpellcheck hook
    This commit makes use of the spellcheck hook to only enable the spellcheck attribute
    on the textarea element when needed (on focused element). Since form view are now
    always displaying a textarea, instead of relying on a readonly state, the textarea
    used by the HtmlField always displayed red underline on typos.
    
    This means the UI can become difficult to read if there are multiple red lines in
    the text.  Now, it is only present on focused textarea thanks to the useSpellcheck
    hook.
    
    This change can be noticeable in many apps, including notes from records and the
    Knowledge editor.
    
    A test has been added to verify that the hook enables/disables the attribute on the
    contenteditable element used by the html editor..
    
    task #2861428
    
    closes odoo/odoo#106866
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web_editor/static/src/js/backend/html_field.js
+++ addons/web_editor/static/src/js/backend/html_field.js
@@ -14,6 +14,7 @@ import ajax from 'web.ajax';
 import {
     useBus,
     useService,
+    useSpellCheck,
 } from "@web/core/utils/hooks";
 import {
     getAdjacentPreviousSiblings,
@@ -90,6 +91,8 @@ export class HtmlField extends Component {
 
         useBus(this.env.bus, "RELATIONAL_MODEL:WILL_SAVE_URGENTLY", () => this.commitChanges({ urgent: true }));
         useBus(this.env.bus, "RELATIONAL_MODEL:NEED_LOCAL_CHANGES", ({detail}) => detail.proms.push(this.commitChanges()));
+
+        useSpellCheck();
 
         this._onUpdateIframeId = 'onLoad_' + _.uniqueId('FieldHtml');
 
