7dbf1e9d091132baeb04092d22be17e2131018e7
niyasraphy <niyasraphyk@gmail.com>
2022-11-26 06:59:50 +0000

    [FIX] sale_expense_margin: remove duplicated if condition
    closes odoo/odoo#106748
    
    X-original-commit: 63c85bf237eaa2262fa9abf7e63595b8465d5586
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/sale_expense_margin/models/account_move.py
+++ addons/sale_expense_margin/models/account_move.py
@@ -10,6 +10,5 @@ class AccountMoveLine(models.Model):
     def _sale_prepare_sale_line_values(self, order, price):
         res = super()._sale_prepare_sale_line_values(order, price)
         if self.expense_id:
-            if self.expense_id:
-                res['expense_id'] = self.expense_id.id
+            res['expense_id'] = self.expense_id.id
         return res
