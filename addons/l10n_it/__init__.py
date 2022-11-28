1adb7b214d31b4537eb6553275896123993da340
maximilien(malb) <malb@odoo.com>
2022-11-28 09:50:34 +0000

    [FIX] l10n_it: translation
    Ideally, we develop in English any localization (then translate to the domestic language, in this case Italian) All the module has been translated in english and then a .po file has been create to translate back in Italian
    
    closes odoo/odoo#106693
    
    Task-id: 3082347
    Related: odoo/enterprise#34465
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- addons/l10n_it/__init__.py
+++ addons/l10n_it/__init__.py
@@ -2,3 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import models
+from odoo import api, SUPERUSER_ID
+
+def load_translations(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('l10n_it.l10n_it_chart_template_generic').process_coa_translations()
