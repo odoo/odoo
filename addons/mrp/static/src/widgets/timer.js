35496501ac718b176ea5dfedafa676b849402f04
JF Aubert <ajf@odoo.com>
2022-11-28 08:21:50 +0000

    [FIX] mrp(workorder_hr): fix workorder times
    Fix workorder times problems:
    - sum in workorders list view
      (displays numeric value rather than mins:secs)
    - timers sometimes starts at -00:01
    - almost all tablet view actions reset the timer (click burger, ...)
    - clicking pause stops the timer at the next second
    - seconds are lost in employee timers
    
    Task: 2985735
    X-original-commit: 7dceef12771890f9fb9fa554e70414c622053ced
    Part-of: odoo/odoo#106908

--- addons/mrp/static/src/widgets/timer.js
+++ addons/mrp/static/src/widgets/timer.js
@@ -1,14 +1,31 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 import { parseFloatTime } from "@web/views/fields/parsers";
 import { useInputField } from "@web/views/fields/input_field_hook";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 
 const { Component, useState, onWillUpdateProps, onWillStart, onWillDestroy } = owl;
 
+function formatMinutes(value) {
+    if (value === false) {
+        return "";
+    }
+    const isNegative = value < 0;
+    if (isNegative) {
+        value = Math.abs(value);
+    }
+    let min = Math.floor(value);
+    let sec = Math.floor((value % 1) * 60);
+    sec = `${sec}`.padStart(2, "0");
+    min = `${min}`.padStart(2, "0");
+    return `${isNegative ? "-" : ""}${min}:${sec}`;
+}
+
 export class MrpTimer extends Component {
     setup() {
+        this.orm = useService('orm');
         this.state = useState({
             // duration is expected to be given in minutes
             duration:
@@ -25,9 +42,16 @@ export class MrpTimer extends Component {
                 ? this.props.ongoing
                 : this.props.record.data.is_user_working;
 
-        onWillStart(() => this._runTimer());
+        onWillStart(async () => {
+            if(this.props.ongoing === undefined && this.props.record.data.state == "progress") {
+                const additionalDuration = await this.orm.call('mrp.workorder', 'get_working_duration', [this.props.record.resId]);
+                this.state.duration += additionalDuration;
+            }
+            if (this.ongoing) {
+                this._runTimer();
+            }
+        });
         onWillUpdateProps((nextProps) => {
-            this.state.duration = nextProps.value;
             const newOngoing =
                 "ongoing" in nextProps
                     ? nextProps.ongoing
@@ -35,6 +59,7 @@ export class MrpTimer extends Component {
             const rerun = !this.ongoing && newOngoing;
             this.ongoing = newOngoing;
             if (rerun) {
+                this.state.duration = nextProps.value;
                 this._runTimer();
             }
         });
@@ -42,34 +67,16 @@ export class MrpTimer extends Component {
     }
 
     get durationFormatted() {
-        return this._formatMinutes();
+        return formatMinutes(this.state.duration);
     }
 
     _runTimer() {
-        if (this.ongoing) {
-            this.timer = setTimeout(() => {
+        this.timer = setTimeout(() => {
+            if (this.ongoing) {
                 this.state.duration += 1 / 60;
                 this._runTimer();
-            }, 1000);
-        }
-    }
-
-    _formatMinutes() {
-        let value = this.state.duration;
-        if (value === false) {
-            return "";
-        }
-        const isNegative = value < 0;
-        if (isNegative) {
-            value = Math.abs(value);
-        }
-        let min = Math.floor(value);
-        // Although looking quite overkill, the following line ensures that we do
-        // not have float issues while still considering that 59s is 00:00.
-        let sec = Math.floor(Math.round((value % 1) * 60));
-        sec = `${sec}`.padStart(2, "0");
-        min = `${min}`.padStart(2, "0");
-        return `${isNegative ? "-" : ""}${min}:${sec}`;
+            }
+        }, 1000);
     }
 }
 
@@ -83,3 +90,4 @@ MrpTimer.props = {
 MrpTimer.template = "mrp.MrpTimer";
 
 registry.category("fields").add("mrp_timer", MrpTimer);
+registry.category("formatters").add("mrp_timer", formatMinutes);
