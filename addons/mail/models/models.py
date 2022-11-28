4f6868e78bc40462cfd85b90cc9e6e6e8c24b8cf
Thibault Delavallée <tde@odoo.com>
2022-11-28 16:34:41 +0100

    [REM] mail: remove dead model method
    Not called anymore, maybe since views refactoring.
    
    Task-2710804 (Mail: Clean MailThread Posting API)

--- addons/mail/models/models.py
+++ addons/mail/models/models.py
@@ -199,22 +199,6 @@ class BaseModel(models.AbstractModel):
         return False
 
     # ------------------------------------------------------------
-    # ACTIVITY
-    # ------------------------------------------------------------
-
-    @api.model
-    def _get_default_activity_view(self):
-        """ Generates an empty activity view.
-
-        :returns: a activity view as an lxml document
-        :rtype: etree._Element
-        """
-        field = E.field(name=self._rec_name_fallback())
-        activity_box = E.div(field, {'t-name': "activity-box"})
-        templates = E.templates(activity_box)
-        return E.activity(templates, string=self._description)
-
-    # ------------------------------------------------------------
     # GATEWAY: NOTIFICATION
     # ------------------------------------------------------------
 
