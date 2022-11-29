d06528728db2b99245b9c766425a66ed0820b6d1
roen-odoo <roen@odoo.com>
2022-11-29 17:18:39 +0000

    [FIX] point_of_sale: avoid overpayment if no cash method configured
    Current behavior:
    If the PoS has no cash method configured, and you try to make a
    payment with a greater amount than the due amount, you will get an
    error message. Because you cannot give money back to the customer as you
    have no cash method configured.
    
    Steps to reproduce:
    - Remove Cash payment method from the PoS
    - Open a PoS session
    - Create an order with some products
    - Pay the order with a greater amount than the due amount
    - Validate the order, you will get an error message
    
    Solution:
    If the Pos has no cash method configured, and the user try to make a
    payment with a greater amount than the due amount, we will automatically
    set the amount to pay to the due amount and show a message to the user.
    
    opw-3061612
    
    closes odoo/odoo#107398
    
    X-original-commit: 6280e5ac921b21d10100cd78a18072120d823aca
    Signed-off-by: Trinh Jacky (trj) <trj@odoo.com>
    Signed-off-by: Engels Robin (roen) <roen@odoo.com>

--- addons/point_of_sale/static/tests/tours/PaymentScreen.tour.js
+++ addons/point_of_sale/static/tests/tours/PaymentScreen.tour.js
@@ -67,4 +67,18 @@ odoo.define('point_of_sale.tour.PaymentScreen', function (require) {
     PaymentScreen.check.validateButtonIsHighlighted(true);
 
     Tour.register('PaymentScreenTour', { test: true, url: '/pos/ui' }, getSteps());
+
+    startSteps();
+
+    ProductScreen.do.clickHomeCategory();
+    ProductScreen.exec.addOrderline('Letter Tray', '1', '10');
+    ProductScreen.do.clickPayButton();
+
+    PaymentScreen.do.clickPaymentMethod('Bank');
+    PaymentScreen.do.pressNumpad('1 0 0 0');
+
+    PaymentScreen.check.remainingIs('0.0');
+    PaymentScreen.check.changeIs('0.0');
+
+    Tour.register('PaymentScreenTour2', { test: true, url: '/pos/ui' }, getSteps());
 });
