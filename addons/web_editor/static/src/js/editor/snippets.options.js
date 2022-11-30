737a3cfa71a3efeaf8e449f2c3093afd275d8335
Nicolas Bayet <nby@odoo.com>
2022-11-30 15:43:50 +0000

    [FIX] web_editor: replaceMedia on contenteeditable
    In website, clicking on the snippet option action "Relpace" did
    not work for an icon inside s_social_media as s_social_media is
    contenteditable false.
    
    The code was simulating a double click on the icon to open the media
    dialog but in the double click handler, a check is made to prevent
    the media dialog to be shown if the contenteditable is false. This
    is important for the knowledge app to prevent changing a button inside
    a knowledge view.
    
    task-3086881
    
    closes odoo/odoo#107487
    
    X-original-commit: 3339c50fbebca8a6d419df4ab14ef03952ea6635
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -5248,9 +5248,7 @@ registry.ReplaceMedia = SnippetOptionWidget.extend({
      * @see this.selectClass for parameters
      */
     async replaceMedia() {
-        // TODO for now, this simulates a double click on the media,
-        // to be refactored when the new editor is merged
-        this.$target.dblclick();
+        this.options.wysiwyg.openMediaDialog({ node: this.$target[0] });
     },
     /**
      * Makes the image a clickable link by wrapping it in an <a>.
