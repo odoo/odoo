ba8321429b913578b14efb218f22189e3aabfea4
joda-odoo <joda@odoo.com>
2022-11-30 11:12:56 +0100

    [FIX] hr_expense: singleton error when registering batch payment
    STEPS TO REPRODUCE THE ISSUE:
    * Take any two employees - In my example I took Abigail and Marc Demo
    * Go to Employee, Under the Private Information enter a unique bank account number for both these employees
    * Go to Expenses, create the new expense reports for both employees and post
    * Go to Expenses Reports > Report to pay > Select both these expense report > Click register payment
    * The singleton error occurs
    
    opw-3061525

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -1201,7 +1201,7 @@ class HrExpenseSheet(models.Model):
             'context': {
                 'active_model': 'account.move',
                 'active_ids': self.account_move_id.ids,
-                'default_partner_bank_id': self.employee_id.sudo().bank_account_id.id,
+                'default_partner_bank_id': self.employee_id.sudo().bank_account_id[:1].id,
             },
             'target': 'new',
             'type': 'ir.actions.act_window',
