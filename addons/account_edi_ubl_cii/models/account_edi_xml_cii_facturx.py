a8c669996fb90b8b62023f0659e31c539ec04819
Julien Van Roy <juvr@odoo.com>
2022-11-30 11:37:47 +0100

    [FIX] account_edi_ubl_cii: import customer when uploading an invoice
    Previously, it was only possible to upload a bill, so the partner_id was read
    from the vendor in the imported file. Now, it is also possible to upload a
    file to create an invoice, so we need to be able to decode the customer in
    the imported file.
    
    opw-3072989
    
    X-original-commit: 7eb401e39372bacff7410d7c57a8eb83382c0d58

--- addons/account_edi_ubl_cii/models/account_edi_xml_cii_facturx.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_cii_facturx.py
@@ -215,14 +215,14 @@ class AccountEdiXmlCII(models.AbstractModel):
             logs.append(_("The invoice has been converted into a credit note and the quantities have been reverted."))
 
         # ==== partner_id ====
-        partner_type = invoice_form.journal_id.type == journal.type and 'SellerTradeParty' or 'BuyerTradeParty'
+        partner_type = invoice_form.journal_id.type == 'purchase' and 'SellerTradeParty' or 'BuyerTradeParty'
         invoice_form.partner_id = self.env['account.edi.format']._retrieve_partner(
             name=_find_value(f"//ram:{partner_type}/ram:Name"),
             mail=_find_value(f"//ram:{partner_type}//ram:URIID[@schemeID='SMTP']"),
             vat=_find_value(f"//ram:{partner_type}/ram:SpecifiedTaxRegistration/ram:ID"),
         )
         if not invoice_form.partner_id:
-            logs.append(_("Could not retrieve the vendor."))
+            logs.append(_("Could not retrieve the %s.", _("customer") if invoice_form.move_type in ('out_invoice', 'out_refund') else _("vendor")))
 
         # ==== currency_id ====
 
