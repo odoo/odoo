d1e8921c0bb5854fc550302c743dee6dbf0e6126
sofiagvaladze <sgv@odoo.com>
2022-12-01 08:40:46 +0000

    [IMP] hr_holidays: hinder allocation date_to to precede date_from
    Purpose: In order to reduce the possibility to make errors, make the end
    date of the validity period always come after the start date.
    
    task - 3069666
    
    closes odoo/odoo#106966
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>
    X-original-commit: a7b8f91231016e5c188d491c0284f26d89f6fe4c

--- addons/hr_holidays/tests/test_leave_requests.py
+++ addons/hr_holidays/tests/test_leave_requests.py
@@ -7,11 +7,9 @@ from freezegun import freeze_time
 from pytz import timezone
 
 from odoo import fields, Command
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
 from odoo.tools import date_utils, mute_logger
 from odoo.tests import Form, tagged
-
-from odoo.exceptions import UserError
 
 from odoo.addons.hr_holidays.tests.common import TestHrHolidaysCommon
 
@@ -233,6 +231,18 @@ class TestLeaveRequests(TestHrHolidaysCommon):
         allocation_form.date_to = date(2019, 5, 6)
         allocation_form.name = 'New Allocation Request'
         allocation_form.save()
+
+    def test_allocation_constrain_dates_check(self):
+        with self.assertRaises(UserError):
+            self.env['hr.leave.allocation'].create({
+                'name': 'Test allocation',
+                'holiday_status_id': self.holidays_type_2.id,
+                'number_of_days': 1,
+                'employee_id': self.employee_emp_id,
+                'state': 'confirm',
+                'date_from': time.strftime('%Y-%m-10'),
+                'date_to': time.strftime('%Y-%m-01'),
+            })
 
     @mute_logger('odoo.models.unlink', 'odoo.addons.mail.models.mail_mail')
     def test_employee_is_absent(self):
@@ -904,7 +914,6 @@ class TestLeaveRequests(TestHrHolidaysCommon):
                 self.holidays_type_2.get_allocation_data(self.employee_emp, target_date=date(2021, 12, 1))[self.employee_emp][0][1],
                 ml=10, lt=5, rl=5, vrl=5, vlt=5,
             )
-
 
     def test_cancel_leave(self):
         with freeze_time('2020-09-15'):
