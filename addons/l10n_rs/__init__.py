b3c8ad2d9b128558fe41e5744a32fc0ca0015aca
maximilien(malb) <malb@odoo.com>
2022-12-01 13:52:47 +0000

    [IMP] l10n_rs: translation and small modification
    Ideally, we develop in English any localization (then translate to the domestic language, in this case Serbian) All the module has been translated in english and then a .po file has been create to translate back in Serbian
    
    Added more info on the description of the module and activate storno by default.
    
    closes odoo/odoo#107101
    
    Task-id: 3086250
    Related: odoo/enterprise#34608
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_rs/__init__.py
+++ addons/l10n_rs/__init__.py
@@ -2,3 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import models
+from odoo import api, SUPERUSER_ID
+
+def load_translations(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('l10n_rs.l10n_rs_chart_template').process_coa_translations()
--- addons/l10n_rs/__manifest__.py
+++ addons/l10n_rs/__manifest__.py
@@ -7,9 +7,11 @@
     "category": "Accounting/Localizations/Account Charts",
     "description": """
         This is the base module of the Serbian localization. It manages chart of accounts and taxes.
+        This module is based on the official document "Pravilnik o kontnom okviru i sadržini računa u kontnom okviru za privredna društva, zadruge i preduzetnike ("Sl. glasnik RS", br. 89/2020)"
+        Source: https://www.paragraf.rs/propisi/pravilnik-o-kontnom-okviru-sadrzini-racuna-za-privredna-drustva-zadruge.html
     """,
     "author": "Modoolar, Odoo S.A.",
-    "depends": ["account", "base_vat"],
+    "depends": ["account", "base_vat", 'l10n_multilang'],
     "data": [
         "data/account_chart_template_data.xml",
         "data/account.account.template.csv",
