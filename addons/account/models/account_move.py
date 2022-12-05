4efc4071d5e79b1fb75f38ac53fddecdc23371f1
Claire Bretton (clbr) <clbr@odoo.com>
2022-12-05 14:14:10 +0000

    [IMP] account: Archive tax tags upon report changes
    When unlinking or writing a tax report line, the related tags used to be deleted.
    This means when a new version of a report was deployed, the history was lost and the user didn't get notified.
    This was especially annoying for customers that got migrated automatically.
    We now archive tags instead of deleting them if they are still used in account.move.line and display a banner
    in report that let the user know there might be a problem and invite him to correct tax tags.
    
    closes odoo/odoo#108489
    
    Task: 3084028
    X-original-commit: ace1c4686db262fa8f7b3ee6755588e0f4cd6c1c
    Related: odoo/enterprise#35166
    Signed-off-by: Josse Colpaert <jco@odoo.com>
    Signed-off-by: Claire Bretton <clbr@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -3720,7 +3720,7 @@ class AccountMoveLine(models.Model):
         string="Originator Tax Distribution Line", ondelete='restrict', readonly=True,
         check_company=True,
         help="Tax distribution line that caused the creation of this move line, if any")
-    tax_tag_ids = fields.Many2many(string="Tags", comodel_name='account.account.tag', ondelete='restrict',
+    tax_tag_ids = fields.Many2many(string="Tags", comodel_name='account.account.tag', ondelete='restrict', context={'active_test': False},
         help="Tags assigned to this line by the tax creating it, if any. It determines its impact on financial reports.", tracking=True)
     tax_audit = fields.Char(string="Tax Audit String", compute="_compute_tax_audit", store=True,
         help="Computed field, listing the tax grids impacted by this line, and the amount it applies to each of them.")
