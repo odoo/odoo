e20370a816222629958897f921037c3ed2158dac
Thibault Delavallée <tde@odoo.com>
2022-12-05 15:05:02 +0000

    [IMP] test_mail: improve tests about template usage and post helpers
    Purpose is to cleanup and add some details in some tests, notably about
    default values when using message_post wrappers with either a view or
    a template.
    
    Tests with void recordsets are also added to check their current behavior
    and try to keep it consistent when cleaning code in a near future.
    
    Add a missing mute logger.
    
    Task-2710804 (Mail: Clean MailThread Posting API)
    
    Part-of: odoo/odoo#108320

--- addons/mail/tests/common.py
+++ addons/mail/tests/common.py
@@ -1180,13 +1180,10 @@ class MailCommon(common.TransactionCase, MailCase):
             }
         })
 
-    def _generate_attachments_data(self, count, res_model=None, res_id=None, attach_values=None):
+    @staticmethod
+    def _generate_attachments_data(count, res_model, res_id, attach_values=None):
         # attachment visibility depends on what they are attached to
         attach_values = attach_values or {}
-        if res_model is None:
-            res_model = self.template._name
-        if res_id is None:
-            res_id = self.template.id
         return [{
             'datas': base64.b64encode(b'AttContent_%02d' % x),
             'name': 'AttFileName_%02d.txt' % x,
