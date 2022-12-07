50ee5d7a091ea0406ce029565c17b379b69beb66
niyasraphy <niyasraphyk@gmail.com>
2022-12-07 09:13:52 +0000

    [FIX] l10n_pe: documentation link
    current link will return 404 error, updating the website link to correct documentation link
    
    closes odoo/odoo#107399
    
    X-original-commit: 33f94926038bb5cf9a9f7feadd2d6596e331b4e1
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/l10n_pe/__manifest__.py
+++ addons/l10n_pe/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': "PCGE Simplified",
     'category': 'Accounting/Localizations/Account Charts',
     'author': 'Vauxoo, Odoo S.A.',
-    'website': 'https://www.odoo.com/documentation/master/accounting/fiscal_localizations/localizations/peru.html',
+    'website': 'https://www.odoo.com/documentation/master/applications/finance/accounting/fiscal_localizations/localizations/peru.html',
     'license': 'LGPL-3',
     'depends': [
         'base_vat',
