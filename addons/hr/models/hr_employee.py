ca1f116b68939928764b12551a81a96db95ea04e
Dossogne Bertrand <bedo@odoo.com>
2022-12-07 09:21:48 +0100

    [IMP] hr{|_hourly_cost}: move employee currency field
    This commit moves the currency_id field on the
    employee model from hr_hourly_cost to hr in order
    for it to be used in payroll localisations.
    
    task-3029348

--- addons/hr/models/hr_employee.py
+++ addons/hr/models/hr_employee.py
@@ -121,6 +121,7 @@ class HrEmployeePrivate(models.Model):
     id_card = fields.Binary(string="ID Card Copy", groups="hr.group_hr_user")
     driving_license = fields.Binary(string="Driving License", groups="hr.group_hr_user")
     private_car_plate = fields.Char(groups="hr.group_hr_user", help="If you have more than one car, just separate the plates by a space.")
+    currency_id = fields.Many2one('res.currency', related='company_id.currency_id', readonly=True)
 
     _sql_constraints = [
         ('barcode_uniq', 'unique (barcode)', "The Badge ID must be unique, this one is already assigned to another employee."),
