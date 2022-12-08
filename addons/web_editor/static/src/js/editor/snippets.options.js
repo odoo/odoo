74ee842569397288e8d95249bb7768b974bfdaa0
Benjamin Vray <bvr@odoo.com>
2022-12-08 08:55:44 +0000

    [FIX] web_editor, website: mitigate line between shapes issues
    This commit mitigates an issue with shape elements sometimes having tiny
    spacing on their top or bottom edge.
    
    This issue is mainly on Chrome when the user has changed the page zoom.
    When the page is zoomed the elements can have a width value with a
    decimal part. Because of it, the size of the SVG background images are
    not correctly rounded compared to their container and therefore leaves a
    visible space.
    
    Steps to reproduce the bug:
    
    - On Chrome, add several shapes on a website page.
    - Change the zoom of the page and change the window width.
    - At some points, the gap will appear.
    
    The least bad solution we found to fix this is to ensure that the width
    of the shape elements is always as close to an integer as possible.
    
    Reminder: several commits had attempted to fix this before ([1], [2],
    [3], [4]). But that wasn't enough to stop it happening. Some shapes
    still need to be fixed although that will likely be done in future
    versions.
    
    [1]: https://github.com/odoo/odoo/commit/c0f62593a4da37e8404dddc0a5762879a5ebbf7f
    [2]: https://github.com/odoo/odoo/commit/ac82407b259eab929486bf0a333a48bc507efa60
    [3]: https://github.com/odoo/odoo/commit/42b3ad10e0b32b7fc72f801e2c67d6baf938c566
    [4]: https://github.com/odoo/odoo/commit/e386f318f5cff50901dba3db8a2587911ba02a6e
    
    task-2824607
    opw-3069213
    opw-3057533
    
    X-original-commit: 623db525e18931a4dd5794da78be840afc34991a
    Part-of: odoo/odoo#108219

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -6755,7 +6755,7 @@ registry.BackgroundShape = SnippetOptionWidget.extend({
 
         const shapeContainer = target.querySelector(':scope > .o_we_shape');
         if (shapeContainer) {
-            shapeContainer.remove();
+            this._removeShapeEl(shapeContainer);
         }
         if (newContainer) {
             const preShapeLayerElement = this._getLastPreShapeLayerElement();
@@ -6852,6 +6852,13 @@ registry.BackgroundShape = SnippetOptionWidget.extend({
             this.prevShapeContainer = shapeContainer.cloneNode(true);
             this.prevShape = target.dataset.oeShapeData;
         }
+    },
+    /**
+     * @private
+     * @param {HTMLElement} shapeEl
+     */
+    _removeShapeEl(shapeEl) {
+        shapeEl.remove();
     },
     /**
      * Overwrites shape properties with the specified data.
