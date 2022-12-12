771d631a0d01b36e8ca726b51857c6d22fa42058
luvi <luvi@odoo.com>
2022-12-12 23:10:11 +0100

    [REF] mail, *: convert Activity view to Owl
    *: project, web
    
    This commit converts the Activity view to the current framework shared by
    other views. Instead of extending Kanban view/elements, the Activity view
    has been implemented by its own.
    
    The project customization of the view has also been converted, as legacy
    files have been removed.
    
    Some tests have been adapted since some classnames might differ from the old
    implementation, resulting to failing tests. But the overall testing cases
    have been conserved.
    
    Co-authored-by: Florent Dardenne <dafl@odoo.com>

---
+++ addons/mail/static/src/views/activity/activity_record.js
@@ -0,0 +1,49 @@
+/** @odoo-module */
+
+import { ActivityCompiler } from "./activity_compiler";
+import { Component } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+import { Field } from "@web/views/fields/field";
+import {
+    getImageSrcFromRecordInfo,
+    isHtmlEmpty,
+} from "@web/views/kanban/kanban_record";
+import { useViewCompiler } from "@web/views/view_compiler";
+
+export class ActivityRecord extends Component {
+    static components = {
+        Field,
+    };
+    static props = {
+        archInfo: { type: Object },
+        openRecord: { type: Function },
+        record: { type: Object },
+    };
+    static template = "mail.ActivityRecord";
+
+    setup() {
+        this.user = useService("user");
+        this.widget = {
+            deletable: false,
+            editable: false,
+            isHtmlEmpty,
+        };
+        const { arch, templateDocs } = this.props.archInfo;
+        const compileParams = {
+            recordExpr: `record`,
+        };
+        this.recordTemplate = useViewCompiler(ActivityCompiler, arch, templateDocs, compileParams)[
+            "activity-box"
+        ];
+    }
+
+    getRenderingContext() {
+        const { record } = this.props;
+        return {
+            record: record.formattedRecord,
+            activity_image: (...args) => getImageSrcFromRecordInfo(record, ...args),
+            user_context: this.user.context,
+            widget: this.widget,
+        };
+    }
+}
