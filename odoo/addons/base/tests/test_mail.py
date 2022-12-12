eccac1ce7b7483331b7674e8ff55a73dc571f9e1
Thibault Delavallée <tde@odoo.com>
2022-12-12 13:09:10 +0000

    [FIX] website(_sale): stop creating message manually
    In website(_sale) messages are created from website forms. However those
    are technical models, you should always use the MailThread API notably to
    ensure values coherency. In our case using message_log seems to be what
    original committers wanted to do (even creating a message as a comment
    which has no effect as the notification process is not called that way).
    
    Task-2710804 (Mail: Clean MailThread Posting API)
    
    Part-of: odoo/odoo#99482

--- odoo/addons/base/tests/test_mail.py
+++ odoo/addons/base/tests/test_mail.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
 from unittest.mock import patch
+
 import email.policy
 import email.message
 import re
