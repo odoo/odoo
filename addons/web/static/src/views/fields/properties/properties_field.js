bc538f6944461a642bac0f757ec96d7f8cc14c9a
Ipsita Borisagar <ibo@odoo.com>
2022-12-12 11:15:43 +0000

    [FIX] base, web: allow reading property fields when no access to parent record
    This commit fixes a use case where a user would not be able to access the
    property field values of a record because he did not have access to that
    record's parent (from the property field definition configuration).
    
    In a concrete example:
    - In the Knowledge App
    - Marc Demo has access to the "Article B".
    - The parent article of "Article B" is "Article A".
    - Marc Demo does not have access to "Article A".
    - Article B has some property fields values configured.
    - If Marc Demo tries to open "Article B" -> crash
    
    This is caused by the framework having to read the property definition from the
    parent record, which we don't have access to.
    A "sudo" was added to allow that use case (which is supported).
    
    In addition, this commit also adds further checks on the web client side to
    gracefully handle a special use case where the end-user could not write on the
    parent record because of record rules.
    (It only checked *access rules*).
    
    To avoid making an extra RPC every time we load the record (to check if we can
    write on the parent), we only make that check when the user tries to modify
    (add/edit/remove) a property and show a proper error message.
    (This also applies when creating new property field tags).
    
    Task-3062064
    
    closes odoo/odoo#118835
    
    X-original-commit: 0293674b8ac28c5b70fa16adbe5951ab59424a8c
    Related: odoo/enterprise#39906
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- addons/web/static/src/views/fields/properties/properties_field.js
+++ addons/web/static/src/views/fields/properties/properties_field.js
@@ -221,9 +221,15 @@ export class PropertiesField extends Component {
      * @param {event} event
      * @param {string} propertyName
      */
-    onPropertyEdit(event, propertyName) {
+    async onPropertyEdit(event, propertyName) {
         event.stopPropagation();
         event.preventDefault();
+        if (!await this.checkDefinitionWriteAccess()) {
+            this.notification.add(_lt("You need to be able to edit parent first to configure property fields"), {
+                type: "warning",
+            });
+            return;
+        }
         if (event.target.classList.contains("disabled")) {
             // remove the glitch if we click on the edit button
             // while the popover is already opened
@@ -284,7 +290,13 @@ export class PropertiesField extends Component {
         this.dialogService.add(ConfirmationDialog, dialogProps);
     }
 
-    onPropertyCreate() {
+    async onPropertyCreate() {
+        if (!await this.checkDefinitionWriteAccess()) {
+            this.notification.add(_lt("You need to be able to edit parent first to configure property fields"), {
+                type: "warning",
+            });
+            return;
+        }
         const propertiesDefinitions = this.propertiesList || [];
 
         if (
@@ -310,6 +322,28 @@ export class PropertiesField extends Component {
         });
         this.openLastPropertyDefinition = true;
         this.props.record.update({ [this.props.name]: propertiesDefinitions });
+    }
+
+    /**
+     * Verify that we can write on properties,
+     * if we don't have access for parent
+     */
+    async checkDefinitionWriteAccess() {
+        const definitionRecordId = this.props.record.data[this.definitionRecordField][0];
+        const definitionRecordModel = this.props.record.fields[this.definitionRecordField].relation;
+        try {
+            await this.orm.call(
+                definitionRecordModel,
+                "check_access_rule",
+                [definitionRecordId],
+                {
+                    operation: "write",
+                }
+            );
+        } catch (_e) { // eslint-disable-line no-unused-vars
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -473,6 +507,7 @@ export class PropertiesField extends Component {
             {
                 readonly: this.props.readonly || !this.state.canChangeDefinition,
                 canChangeDefinition: this.state.canChangeDefinition,
+                checkDefinitionWriteAccess: () => this.checkDefinitionWriteAccess(),
                 propertyDefinition: this.propertiesList.find(
                     (property) => property.name === currentName(propertyName)
                 ),
