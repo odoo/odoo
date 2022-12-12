20dae3d34fd9e78bb4bb77fc030a1e25a63ee444
Kevin Baptiste <kba@odoo.com>
2022-12-12 10:20:27 +0100

    [FIX] hr_holidays: fix refresh dashboard on leave creation
    Due to a recent change, the time off dashboard was not properly updated
    when a leave was created/deleted/canceled.
    
    task-3097259

--- addons/hr_holidays/static/src/views/calendar/calendar_controller.js
+++ addons/hr_holidays/static/src/views/calendar/calendar_controller.js
@@ -1,5 +1,6 @@
 /** @odoo-module */
 
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { CalendarController } from '@web/views/calendar/calendar_controller';
 import { FormViewDialog } from '@web/views/view_dialogs/form_view_dialog';
 
@@ -68,16 +69,29 @@ export class TimeOffCalendarController extends CalendarController {
 
     deleteRecord(record) {
         if (!record.can_cancel) {
-            return super.deleteRecord(record);
+            this.displayDialog(ConfirmationDialog, {
+                title: this.env._t("Confirmation"),
+                body: this.env._t("Are you sure you want to delete this record ?"),
+                confirm: async () => {
+                    await this.model.unlinkRecord(record.id);
+                    this.env.timeOffBus.trigger('update_dashboard');
+                },
+                cancel: () => {},
+            });
+        } else {
+            this.leaveCancelWizard(record.id, () => {
+                this.model.load();
+                this.env.timeOffBus.trigger('update_dashboard');
+            });
         }
-
-        this.leaveCancelWizard(record.id, () => {
-            this.model.load();
-            this.env.timeOffBus.trigger('update_dashboard');
-        });
     }
 
     async editRecord(record, context = {}, shouldFetchFormViewId = true) {
+        const onDialogClosed = () => {
+            this.model.load();
+            this.env.timeOffBus.trigger('update_dashboard');
+        };
+
         return new Promise((resolve) => {
             this.displayDialog(
                 TimeOffFormViewDialog, {
@@ -86,12 +100,9 @@ export class TimeOffCalendarController extends CalendarController {
                     context,
                     title: record.title,
                     viewId: this.model.formViewId,
-                    onRecordSaved: () => this.model.load(),
+                    onRecordSaved: onDialogClosed,
                     onRecordDeleted: (record) => this.deleteRecord(record),
-                    onLeaveCancelled: () => {
-                        this.model.load();
-                        this.env.timeOffBus.trigger('update_dashboard');
-                    }
+                    onLeaveCancelled: onDialogClosed,
                 },
                 { onClose: () => resolve() }
             );
