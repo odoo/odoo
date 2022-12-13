930368c28cc9c01c140ba1edcf3a4eff2bb33ae7
Rémy Voet (ryv) <ryv@odoo.com>
2022-12-13 15:58:58 +0000

    [FIX] core: unexpected MissingError when prefetching record
    This is a rare case where method _read() raises a MissingError instead
    of just ignoring it.
    
    The issue is triggered by several conditions on a model M:
     - at least one ir.rule on M with a domain using a column field on M;
     - one deleted record Y which is in the prefetch set of a record X;
     - one reads a non-column field on record X.
    
    Fix method _read() to manage that case.  It adds an extra call to
    exists() in that case, but adds no overhead in the general case.
    
    closes odoo/odoo#107965
    
    X-original-commit: 65a012c2bba0ed81d9b525a7779c726043b17707
    Signed-off-by: Rémy Voet <ryv@odoo.com>
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/fields.py
+++ odoo/fields.py
@@ -1087,7 +1087,7 @@ class Field(MetaField('DummyField', (object,), {})):
                     recs._fetch_field(self)
                 except AccessError:
                     record._fetch_field(self)
-                if not env.cache.contains(record, self) and not record.exists():
+                if not env.cache.contains(record, self):
                     raise MissingError("\n".join([
                         _("Record does not exist or has been deleted."),
                         _("(Record: %s, User: %s)") % (record, env.uid),
