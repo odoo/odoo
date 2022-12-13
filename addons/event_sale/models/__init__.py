0164162fafba5f949cdf437515bd561198966581
Victor Feyens <vfe@odoo.com>
2022-12-13 16:24:29 +0000

    [MOV] event_sale: split sale_order.py
    Part-of: odoo/odoo#110833

--- addons/event_sale/models/__init__.py
+++ addons/event_sale/models/__init__.py
@@ -4,6 +4,7 @@ from . import account_move
 from . import event_event
 from . import event_registration
 from . import event_ticket
-from . import sale_order
 from . import product
+from . import sale_order
+from . import sale_order_line
 from . import sale_order_template_line
