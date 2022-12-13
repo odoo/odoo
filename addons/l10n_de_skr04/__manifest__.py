6b74b1c83c7f339f78b3bba64f7341f984744ccc
gawa-odoo <gawa@odoo.com>
2022-12-13 13:39:31 +0000

    [FIX] l10n_de{,_skr03,_skr04}: change tax report lines name and order
    The regulation for the German tax report has changed.
    We adapt it and its export to the regulation (that changed in Jan 2023)
    The reference for this tax report comes from https://www.bundesfinanzministerium.de/Content/DE/Downloads/BMF_Schreiben/Steuerarten/Umsatzsteuer/2022-12-21-muster-der-vordrucke-im-umsatzsteuer-voranmeldungs-und-vorauszahlungsverfahren-fuer-das-kalenderjahr-2023.pdf?__blob=publicationFile&v=1
    and from elster.de
    
    We can see some lines are wrong.
    Some categories are also not well-ordered.
    Some indentation should be done better.
    Some taxes had to change, as the report lines they were pointing to should not exist,
    and the tax row had to be added
    Some taxes should point to a tax report line when they were not.
    
    opw-2767535
    opw-2974560
    task-3082233
    
    closes odoo/odoo#108601
    
    Related: odoo/enterprise#35210
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- addons/l10n_de_skr04/__manifest__.py
+++ addons/l10n_de_skr04/__manifest__.py
@@ -32,7 +32,7 @@
 
 {
     'name': 'Germany SKR04 - Accounting',
-    'version': '3.0',
+    'version': '3.1',
     'author': 'openbig.org',
     'website': 'http://www.openbig.org',
     'category': 'Accounting/Localizations/Account Charts',
