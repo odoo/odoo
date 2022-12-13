1f5c6f78852a8ee8012b81b081f6a217ce7fcdff
Rémy Voet (ryv) <ryv@odoo.com>
2022-12-13 15:58:58 +0000

    [FIX] core: unexpected MissingError when prefetching record
    This is a rare case where method _read() raises a MissingError instead
    of just ignoring it.
    
    The issue is triggered by several conditions on a model M:
     - at least one ir.rule on M with a domain using a column field on M;
     - one deleted record Y which is in the prefetch set of a record X;
     - one reads a non-column field on record X.
    
    Fix method _read() to manage that case.  It adds an extra call to
    exists() in that case, but adds no overhead in the general case.
    
    closes odoo/odoo#108050
    
    X-original-commit: 1876dc87e5c88c711c9b3882c39be704ffc46532
    Signed-off-by: Raphael Collet <rco@odoo.com>
    Signed-off-by: Rémy Voet <ryv@odoo.com>

--- odoo/addons/test_new_api/tests/test_new_fields.py
+++ odoo/addons/test_new_api/tests/test_new_fields.py
@@ -13,7 +13,7 @@ import psycopg2
 
 from odoo import models, fields, Command
 from odoo.addons.base.tests.common import TransactionCaseWithUserDemo
-from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.exceptions import AccessError, MissingError, UserError, ValidationError
 from odoo.tests import common
 from odoo.tools import mute_logger, float_repr
 from odoo.tools.date_utils import add, subtract, start_of, end_of
@@ -1623,6 +1623,54 @@ class TestFields(TransactionCaseWithUserDemo):
         self.assertEqual(cat2.parent, cat1)
         with self.assertRaises(AccessError):
             cat1.name
+
+    def test_32_prefetch_missing_error(self):
+        """ Test that prefetching non-column fields works in the presence of deleted records. """
+        Discussion = self.env['test_new_api.discussion']
+
+        # add an ir.rule that forces reading field 'name'
+        self.env['ir.rule'].create({
+            'model_id': self.env['ir.model']._get(Discussion._name).id,
+            'groups': [self.env.ref('base.group_user').id],
+            'domain_force': "[('name', '!=', 'Super Secret discution')]",
+        })
+
+        records = Discussion.with_user(self.user_demo).create([
+            {'name': 'EXISTING'},
+            {'name': 'MISSING'},
+        ])
+
+        # unpack to keep the prefetch on each recordset
+        existing, deleted = records
+        self.assertEqual(existing._prefetch_ids, records._ids)
+
+        # this invalidates the caches but the prefetching remains the same
+        deleted.unlink()
+
+        # this should not trigger a MissingError
+        existing.categories
+
+        # invalidate 'categories' for the assertQueryCount
+        records.invalidate_model(['categories'])
+        with self.assertQueryCount(4):
+            # <categories>.__get__(existing)
+            #  -> records._fetch_field(['categories'])
+            #      -> records._read(['categories'])
+            #          -> records.check_access_rule('read')
+            #              -> records._filter_access_rules_python('read')
+            #                  -> records.filtered_domain(...)
+            #                      -> <name>.__get__(existing)
+            #                          -> records._fetch_field(['name'])
+            #                              -> records._read(['name', ...])
+            #                                  -> ONE QUERY to read ['name', ...] of records
+            #                                  -> ONE QUERY for deleted.exists() / code: forbidden = missing.exists()
+            #          -> ONE QUERY for records.exists() / code: self = self.exists()
+            #          -> ONE QUERY to read the many2many of existing
+            existing.categories
+
+        # this one must trigger a MissingError
+        with self.assertRaises(MissingError):
+            deleted.categories
 
     def test_40_real_vs_new(self):
         """ test field access on new records vs real records. """
