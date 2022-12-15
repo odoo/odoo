15489d6be29dcac093ce95b3de95c657edf93d6e
Xavier-Do <xdo@odoo.com>
2022-12-15 11:25:25 +0000

    [IMP] tests: make python version accessible without importing sys
    closes odoo/odoo#108202
    
    X-original-commit: 27575ed59476238c76d40143d81f6975f1934f7c
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- odoo/addons/test_lint/tests/test_pylint.py
+++ odoo/addons/test_lint/tests/test_pylint.py
@@ -9,7 +9,6 @@ except ImportError:
 import subprocess
 import os
 from os.path import join
-import sys
 
 from odoo.tests.common import TransactionCase
 from odoo import tools
@@ -53,7 +52,7 @@ class TestPyLint(TransactionCase):
         if pylint is None:
             self._skip_test('please install pylint')
         required_pylint_version = tools.parse_version('1.6.4')
-        if sys.version_info >= (3, 6):
+        if self._python_version >= (3, 6):
             required_pylint_version = tools.parse_version('1.7.0')
         if tools.parse_version(getattr(pylint, '__version__', '0.0.1')) < required_pylint_version:
             self._skip_test('please upgrade pylint to >= %s' % required_pylint_version)
