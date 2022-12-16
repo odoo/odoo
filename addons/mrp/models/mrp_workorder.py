46e1ae4d9918de1aaf6b4a7565dba422b03fcb36
William Henrotin <whe@odoo.com>
2022-12-16 14:31:05 +0000

    [FIX] mrp: do not recompute duration expected
    Since 2515482d5a70, the duration expected of a workorder was always set
    to the real duration at the validation of the workorder. This commit
    ensure the duration expected is left unchanged even if the
    date_planned_finished is updated.
    
    closes odoo/odoo#108189
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/mrp/models/mrp_workorder.py
+++ addons/mrp/models/mrp_workorder.py
@@ -654,7 +654,7 @@ class MrpWorkorder(models.Model):
                 vals['date_start'] = end_date
             if not workorder.date_planned_start or end_date < workorder.date_planned_start:
                 vals['date_planned_start'] = end_date
-            workorder.write(vals)
+            workorder.with_context(bypass_duration_calculation=True).write(vals)
         return True
 
     def end_previous(self, doall=False):
