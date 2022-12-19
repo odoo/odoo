f4bd4e6ed4e1f7368f2cee7505ae701f7e15e572
Michael (mcm) <mcm@odoo.com>
2022-12-19 13:27:47 +0100

    [IMP] web: edit view in current action
    Before this commit, editing views from debug menu opened a dialog
    and the user needed to refresh the page to see the changes.
    Now, the edit view is opened in a new action so when the user goes back
    in the breadcrumb, the view is updated.
    
    task id: 3081332

--- addons/web/static/src/core/debug/debug_utils.js
+++ addons/web/static/src/core/debug/debug_utils.js
@@ -8,6 +8,6 @@ export function editModelDebug(env, title, model, id) {
         type: "ir.actions.act_window",
         views: [[false, "form"]],
         view_mode: "form",
-        target: "new",
+        target: "current",
     });
 }
