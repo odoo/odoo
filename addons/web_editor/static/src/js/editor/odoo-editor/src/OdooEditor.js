e0afc654e47ba1397bd8f42ec2d32579b1f21d34
Rodopho Cammarosano de Lima (rcdl) <rcdl@odoo.com>
2022-12-21 17:47:26 +0000

    [FIX] web_editor: fix issues caused by textContent
    Issue #1: reading textContent
    
    When reading the textContent property of an element, its does not take into
    account line breaks introduced by <br> and block elements, whilst innerHtml
    does.
    
    This commit solves the issue of updating a link's href to a non-sense url
    upon label change. Before this commit, "www.abc.com <br> www.xyz.com" was
    being incorrectely parsed as "www.abc.comwww.xyz.com".
    
    Issue #2: setting textContent
    
    When setting the textContent property of an element, all of its child nodes are
    removed and replaced by a single text node. While this is fine when the
    element contains a single text node, it might not be the desired result when
    the element contains other children.
    
    This commit solves the issue obtained when pasting html such as
    "link.com<br>link.be" inside an empty inline element such as STRONG or A,
    in which cleanForSave was changing its content (removing the line-break) due
    to misuse of textContent, when the intended effected was simply removing
    ZeroWidthSpace characters. A safer alternative for removing ZeroWidthSpace is
    thus provided.
    
    task-3081254
    
    closes odoo/odoo#112448
    
    X-original-commit: 0d1150f73bf6248df6e20b86021b2b95029cc364
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -13,7 +13,6 @@ import './commands/align.js';
 import { sanitize } from './utils/sanitize.js';
 import { serializeNode, unserializeNode, serializeSelection } from './utils/serialize.js';
 import {
-    isMacOS,
     closestBlock,
     commonParentGet,
     containsUnremovable,
@@ -69,7 +68,10 @@ import {
     pxToFloat,
     parseHTML,
     splitTextNode,
-    isVoidElement
+    isMacOS,
+    isVoidElement,
+    cleanZWS,
+    isZWS,
 } from './utils/utils.js';
 import { editorCommands } from './commands/commands.js';
 import { Powerbox } from './powerbox/Powerbox.js';
@@ -3725,14 +3727,14 @@ export class OdooEditor extends EventTarget {
         // we only remove the attribute to ensure we don't break some style.
         // Otherwise we remove the entire inline element.
         for (const emptyElement of element.querySelectorAll('[data-oe-zws-empty-inline]')) {
-            if (emptyElement.textContent.length === 1 && emptyElement.textContent.includes('\u200B')) {
+            if (isZWS(emptyElement)) {
                 if (emptyElement.classList.length > 0) {
                     emptyElement.removeAttribute('data-oe-zws-empty-inline');
                 } else {
                     emptyElement.remove();
                 }
             } else {
-                emptyElement.textContent = emptyElement.textContent.replace('\u200B', '');
+                cleanZWS(emptyElement);
                 emptyElement.removeAttribute('data-oe-zws-empty-inline');
             }
         }
@@ -3759,7 +3761,7 @@ export class OdooEditor extends EventTarget {
         // Remove Zero Width Spaces on Font awesome elements
         const faSelector = 'i.fa,span.fa,i.fab,span.fab,i.fad,span.fad,i.far,span.far';
         for (const el of element.querySelectorAll(faSelector)) {
-            el.textContent = el.textContent.replace('\u200B', '');
+            cleanZWS(el);
         }
 
         // Clean custom selections
