07f758a18caeb88e5532fbf84edb84b33c8cb0e3
xO-Tx <mou@odoo.com>
2022-12-21 10:00:09 +0000

    [FIX] website: fix homepage translation overwriting
    To reproduce the issue:
    
    - Go to website > Add language > Add blocks to your homepage and
    translate it.
    - Make translations on the "Contact us" page (or a new custom page).
    - Install a new app > The translation of the homepage is lost (only on
    homepage).
    
    Explanation:
    
    After [1], The `_load_module_terms()` method was updated on website
    module to copy translations from base to specific views when module
    translation is loaded. And since [2], the translated field's column is
    either NULL or a JSON dict mapping language codes to text (the field's
    value in the corresponding language).
    
    To explain what happens exactly when new app is installed, let's suppose
    we want to load module translation for the config {'en_US', 'fr_BE'} in
    the following situations:
    
    S1 (e.g. "Contact us" page):
    -=-=-=-=-=-=--=-=-=-=-=-=-=-
    
    ```
    generic_arch_db = {
    'en_US': '<div>Generic (EN)</div>',
    'fr_BE': '<div>Generic (FR)</div>'
    }
    
    - specific_arch_db = {
    'en_US': '<div>Specific (EN)</div>',
    'fr_BE': '<div>Specific (FR)</div>'
    }
    ```
    
    `_load_module_terms()` will copy translations for 'fr_BE' from generic
    to specific `arch_db` (using generic translation dictionary).
    
    result:
    
    ```
    new_specific_arch_db = {
    'en_US': '<div>Specific (EN)</div>',
    'fr_BE': '<div>Updated Specific (FR)</div>'
    }
    ```
    
    S2 (new custom page):
    -=-=-=-=-=-=--=-=-=-=
    
    ```
    generic_arch_db = NULL
    
    specific_arch_db = {
    'en_US': '<div>Specific (EN)</div>',
    'fr_BE': '<div>Specific (FR)</div>'
    }
    ```
    
    result:
    
    Nothing to do here (only specific version), the specific translation
    will not be updated.
    
    S3 (e.g. website homepage):
    -=-=-=-=-=-=--=-=-=-=-=-=-=
    
    The generic view has no translated `arch_db` (only the 'en_US' version):
    
    ```
    generic_arch_db = {'en_US': '<div>Generic (EN)</div>'}
    
    specific_arch_db = {
    'en_US': '<div>Specific (EN)</div>',
    'fr_BE': '<div>Specific (FR)</div>'
    }
    ```
    
    `_load_module_terms()` will copy generic translations only for languages
    available on "generic_arch_db" and the 'fr_BE' version will be lost.
    
    result:
    
    `new_specific_arch_db = {'en_US': '<div>Specific (EN)</div>'}`
    
    The goal of this commit is to prevent this behaviour by keeping the
    specific translated `arch_db` even when the generic value has no content
    for the translation language.
    
    Remark: This behaviour occurred while loading module translations,
    as a consequence, the specific translations are also lost when the
    website module is updated.
    
    [1]: https://github.com/odoo/odoo/commit/6bea18143069de9622a221557f3919d86f6dcf2c
    [2]: https://github.com/odoo/odoo/commit/992e256d370c55d4eeaf884a7d2eadfc444a3972
    
    opw-3083480
    
    closes odoo/odoo#108477
    
    X-original-commit: 91a9c870eede96040ac49d2375fe0e18342342e1
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website/models/ir_module_module.py
+++ addons/website/models/ir_module_module.py
@@ -501,10 +501,11 @@ class IrModuleModule(models.Model):
             if not generic_arch_db:
                 continue
             langs_update = (langs & generic_arch_db.keys()) - {'en_US'}
+            specific_langs = (langs & specific_arch_db.keys()) - {'en_US'}
             generic_arch_db_en = generic_arch_db.pop('en_US')
             specific_arch_db_en = specific_arch_db.pop('en_US')
             generic_arch_db = {k: generic_arch_db[k] for k in langs_update}
-            specific_arch_db = {k: specific_arch_db.get(k, specific_arch_db_en) for k in langs_update}
+            specific_arch_db = {k: specific_arch_db.get(k, specific_arch_db_en) for k in specific_langs}
             generic_translation_dictionary = field.get_translation_dictionary(generic_arch_db_en, generic_arch_db)
             specific_translation_dictionary = field.get_translation_dictionary(specific_arch_db_en, specific_arch_db)
             # update specific_translation_dictionary
