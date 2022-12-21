c66842fff4f3a1cbcede124fddcde7b289bced22
Pierre-Yves Dufays <pydu@odoo.com>
2022-12-21 13:21:37 +0000

    [IMP] mail: avoid displaying twice the email in the composer recipients
    When the contact name was not yet set, the email was displayed twice in the
    composer: first as the contact name and then as the email in parentheses. Now
    when the contact is the same as the email, nothing is displayed in parentheses.
    
    Technical note: the regexp in the mail util parseEmail method has been slightly
    modified because it was returning in some cases the name surrounded by quotes
    (ex.: "newlead3@ex.com" <newlead3@ex.com> --> name: "newlead3@ex.com"). This is
    no longer the case with this change. That change was needed for comparing the
    name and the email to avoid to display the same thing twice (when the email and
    the name are the same). Note that the method is only used once, where we have
    done the change.
    
    Task-3024050
    
    Part-of: odoo/odoo#105111

--- addons/mail/static/src/js/utils.js
+++ addons/mail/static/src/js/utils.js
@@ -173,7 +173,7 @@ function inline(node, transform_children) {
 // Parses text to find email: Tagada <address@mail.fr> -> [Tagada, address@mail.fr] or False
 function parseEmail(text) {
     if (text) {
-        var result = text.match(/(.*)<(.*@.*)>/);
+        var result = text.match(/"?(.*?)"? <(.*@.*)>/);
         if (result) {
             return [_.str.trim(result[1]), _.str.trim(result[2])];
         }
