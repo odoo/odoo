66b741ec701949f6e6badd714e0ad823f4ee0d8c
Yannick Tivisse <yti@odoo.com>
2022-12-21 16:32:10 +0100

    [FIX] hr_work_entry_contract: Improve work entries generation perf

--- addons/hr_work_entry_holidays/tests/test_performance.py
+++ addons/hr_work_entry_holidays/tests/test_performance.py
@@ -6,7 +6,7 @@ from odoo.addons.hr_work_entry_holidays.tests.common import TestWorkEntryHoliday
 from odoo.tests.common import users, warmup, tagged
 
 
-@tagged('work_entry_perf')
+@tagged('work_entry_holidays_perf')
 class TestWorkEntryHolidaysPerformance(TestWorkEntryHolidaysBase):
 
     @classmethod
@@ -32,7 +32,7 @@ class TestWorkEntryHolidaysPerformance(TestWorkEntryHolidaysBase):
         self.richard_emp.generate_work_entries(date(2018, 1, 1), date(2018, 1, 2))
         leave = self.create_leave(datetime(2018, 1, 1, 7, 0), datetime(2018, 1, 1, 18, 0))
 
-        with self.assertQueryCount(__system__=91, admin=93):
+        with self.assertQueryCount(__system__=91, admin=95):
             leave.action_validate()
         leave.action_refuse()
 
@@ -112,7 +112,7 @@ class TestWorkEntryHolidaysPerformancesBigData(TestWorkEntryHolidaysBase):
     def test_work_entries_generation_perf(self):
         # Test Case 7: Try to generate work entries for
         # a hundred employees over a month
-        with self.assertQueryCount(__system__=500, admin=2807):
+        with self.assertQueryCount(__system__=406, admin=406):
             work_entries = self.contracts._generate_work_entries(date(2020, 7, 1), date(2020, 8, 31))
 
         # Original work entries to generate when we don't adapt date_generated_from and
