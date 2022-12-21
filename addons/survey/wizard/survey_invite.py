80b8d2ca1d2d0892fc75e8a6e44245effcf42799
Pierre-Yves Dufays <pydu@odoo.com>
2022-12-21 12:10:18 +0100

    [FIX] survey: fix detect duplicate partner invitation
    When sending the same survey to the same partner, the system detects it and
    allows the user to choose between sending a new invitation or resending the
    old one so that the user can continue the survey that might already be ongoing.
    The detection was not working anymore and this fixes the problem.
    
    Technical note: _compute_existing_partner_ids was intersecting record set
    partner_ids (the one currently added) and a record set of partner for which an
    invitation has already been sent. The intersection didn't work because
    partner_ids relation was not completely set and instead of id, had "origin" id.
    Comparing id using a filter solves the problem (using the trick that .ids,
    return also the origin ids).
    
    Task-3087702
    
    X-original-commit: 78912f10be80d529885a71ee0f6ce07b19c4424f

--- addons/survey/wizard/survey_invite.py
+++ addons/survey/wizard/survey_invite.py
@@ -70,8 +70,7 @@ class SurveyInvite(models.TransientModel):
 
     @api.depends('partner_ids', 'survey_id')
     def _compute_existing_partner_ids(self):
-        existing_answers = self.survey_id.user_input_ids
-        self.existing_partner_ids = existing_answers.mapped('partner_id') & self.partner_ids
+        self.existing_partner_ids = list(set(self.survey_id.user_input_ids.partner_id.ids) & set(self.partner_ids.ids))
 
     @api.depends('emails', 'survey_id')
     def _compute_existing_emails(self):
