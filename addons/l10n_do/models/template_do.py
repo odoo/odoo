a279c889bddf8739c5ce511a3f04b3a327fd8883
det <det@odoo.com>
2022-12-23 14:03:15 +0000

    [IMP] l10n_do: review CoA, Taxes, Groups, Tags
    1/ remove non necessary stuffs
    
    - rm unused journals
    - rm unused partner title
    - reformat account.group
    
    2/ review / update CoA, taxes, groups, tags
    
    - account id convention
    - missing tags, groups
    
    3/ review tax report (IT-1)
    
    -  get closer to the official one [DGII](https://dgii.gov.do/publicacionesOficiales/bibliotecaVirtual/contribuyentes/itbis/Documents/Instructivo%20LLENADO%20IT-1%202020.pdf)
    
    4/ add traductions
    
    - module should be in english
    
    closes odoo/odoo#109310
    
    Related: odoo/enterprise#35520
    Signed-off-by: Quentin De Paoli <qdp@odoo.com>

--- addons/l10n_do/models/template_do.py
+++ addons/l10n_do/models/template_do.py
@@ -11,13 +11,13 @@ class AccountChartTemplate(models.AbstractModel):
         return {
             'code_digits': '8',
             'use_anglo_saxon': True,
-            'property_account_receivable_id': 'do_niif_11030201',
-            'property_account_payable_id': 'do_niif_21010200',
-            'property_account_income_categ_id': 'do_niif_41010100',
-            'property_account_expense_categ_id': 'do_niif_51010100',
-            'property_stock_account_input_categ_id': 'do_niif_21021200',
-            'property_stock_account_output_categ_id': 'do_niif_11050600',
-            'property_stock_valuation_account_id': 'do_niif_11050100',
+            'property_account_receivable_id': 'l10n_do_11030201',
+            'property_account_payable_id': 'l10n_do_21010200',
+            'property_account_income_categ_id': 'l10n_do_41010100',
+            'property_account_expense_categ_id': 'l10n_do_51010100',
+            'property_stock_account_input_categ_id': 'l10n_do_21021200',
+            'property_stock_account_output_categ_id': 'l10n_do_11050600',
+            'property_stock_valuation_account_id': 'l10n_do_11050100',
         }
 
     @template('do', 'res.company')
@@ -28,43 +28,12 @@ class AccountChartTemplate(models.AbstractModel):
                 'bank_account_code_prefix': '110102',
                 'cash_account_code_prefix': '110101',
                 'transfer_account_code_prefix': '11010100',
-                'account_default_pos_receivable_account_id': 'do_niif_11030210',
-                'income_currency_exchange_account_id': 'do_niif_42040100',
-                'expense_currency_exchange_account_id': 'do_niif_52070800',
-                'account_journal_early_pay_discount_loss_account_id': 'do_niif_99900003',
-                'account_journal_early_pay_discount_gain_account_id': 'do_niif_99900004',
-            },
-        }
-
-    @template('do', 'account.journal')
-    def _get_do_account_journal(self):
-        return {
-            "caja_chica": {
-                'name': _('Caja Chica'),
-                'type': 'cash',
-                'sequence': 10,
-            },
-            "cheques_clientes": {
-                'name': _('Cheques Clientes'),
-                'type': 'cash',
-                'sequence': 10,
-            },
-            "gasto": {
-                'type': 'purchase',
-                'name': _('Gastos No Deducibles'),
-                'code': 'GASTO',
-                'show_on_dashboard': True,
-            },
-            "cxp": {
-                'type': 'purchase',
-                'name': _('Migración CxP'),
-                'code': 'CXP',
-                'show_on_dashboard': True,
-            },
-            "cxc": {
-                'type': 'sale',
-                'name': _('Migración CxC'),
-                'code': 'CXC',
-                'show_on_dashboard': True,
+                'account_default_pos_receivable_account_id': 'l10n_do_11030210',
+                'income_currency_exchange_account_id': 'l10n_do_42040100',
+                'expense_currency_exchange_account_id': 'l10n_do_61070800',
+                'account_journal_early_pay_discount_loss_account_id': 'l10n_do_61081000',
+                'account_journal_early_pay_discount_gain_account_id': 'l10n_do_42040400',
+                'default_cash_difference_income_account_id': 'l10n_do_42040400',
+                'default_cash_difference_expense_account_id': 'l10n_do_61081000',
             },
         }
