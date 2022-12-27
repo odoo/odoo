2420c640f18e39bfe912d1bdd6325f0acd46a73c
Arnold Moyaux <arm@odoo.com>
2022-12-27 13:39:06 +0000

    [FIX] purchase, purchase_stock: add price difference account
    Version 16.0 removed the price different account following this pull request #99411
    
    This decision has been made because it was only used anymore by standard cost method and real time valuation.
    We thought that standard was not a valid accounting method and we didn't want to maintain code for it.
    
    But:
    - Standard could be valid if you manualy complete the accounting entries
    by yourself (e.g. employees/machines cost in mrp).
    - It's also valid if you record the difference between standard price and
    vendo price (price diff)
    
    If you want to have an estimation of your cogs (benefits and loss) during an
    accounting period. People just do a manual correction at the end but they have
    a real time reporting on the situation.
    
    It's a too big regression to be acceptable. We apologize and reintroduce it for
    16.0 and future version.
    
    This PR reintroduce the fields in purchase and the fix module in 16.0 is
    not needed anymore.
    
    closes odoo/odoo#108736
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/purchase_stock/models/product.py
+++ addons/purchase_stock/models/product.py
@@ -5,9 +5,23 @@ from odoo import api, fields, models
 from odoo.osv import expression
 
 
+class ProductCategory(models.Model):
+    _inherit = "product.category"
+
+    property_account_creditor_price_difference_categ = fields.Many2one(
+        'account.account', string="Price Difference Account",
+        company_dependent=True,
+        help="This account will be used to value price difference between purchase price and accounting cost.")
+
+
 class ProductTemplate(models.Model):
     _name = 'product.template'
     _inherit = 'product.template'
+
+    property_account_creditor_price_difference = fields.Many2one(
+        'account.account', string="Price Difference Account", company_dependent=True,
+        help="This account is used in automated inventory valuation to "\
+             "record the price difference between a purchase order and its related vendor bill when validating this vendor bill.")
 
     @api.model
     def _get_buy_route(self):
