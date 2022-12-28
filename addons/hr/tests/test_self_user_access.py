14d97ec28a9850c5ff9f8f63667f0ae17b773d7a
sofiagvaladze <sgv@odoo.com>
2022-12-28 14:44:11 +0000

    [IMP] base,auth_password_policy: add form view for changing password
    Purpose: The form view is more intuitive then list view in case
    user wants to change the password only for one user.
    
    task - 3105178
    
    closes odoo/odoo#109869
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr/tests/test_self_user_access.py
+++ addons/hr/tests/test_self_user_access.py
@@ -87,7 +87,7 @@ class TestSelfAccessProfile(TestHrCommon):
         })
         view = self.env.ref('hr.res_users_view_form_profile')
         available_actions = james.get_views([(view.id, 'form')], {'toolbar': True})['views']['form']['toolbar']['action']
-        change_password_action = self.env.ref("base.change_password_wizard_action")
+        change_password_action = self.env.ref("base.change_password_action_server")
 
         self.assertFalse(any(x['id'] == change_password_action.id for x in available_actions))
 
