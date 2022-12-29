a3d631d0ce05e00a6943562cf4139f5b766d51e0
Sébastien Theys <seb@odoo.com>
2022-12-29 11:26:26 +0100

    [REF] test_mail: lint files

--- addons/test_mail/static/tests/helpers/model_definitions_setup.js
+++ addons/test_mail/static/tests/helpers/model_definitions_setup.js
@@ -1,5 +1,10 @@
 /** @odoo-module **/
 
-import { addModelNamesToFetch } from '@bus/../tests/helpers/model_definitions_helpers';
+import { addModelNamesToFetch } from "@bus/../tests/helpers/model_definitions_helpers";
 
-addModelNamesToFetch(['mail.test.track.all', 'mail.test.activity', 'mail.test.multi.company', 'mail.test.multi.company.read']);
+addModelNamesToFetch([
+    "mail.test.track.all",
+    "mail.test.activity",
+    "mail.test.multi.company",
+    "mail.test.multi.company.read",
+]);
