8bee46f117b1e6822429e8688f70cb27cc237bb4
maximilien(malb) <malb@odoo.com>
2022-12-29 14:55:06 +0000

    [IMP] l10n_fr: Translation
    The French localisation was written in french which was wrong, all the localisation have to be written in english and then translated back to the native language thanks to PO files.
    
    Thanks to this PR, all the french localisation has been translated.
    
    closes odoo/odoo#108816
    
    Task-id: 3112366
    Related: odoo/enterprise#35296
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_fr/__init__.py
+++ addons/l10n_fr/__init__.py
@@ -6,6 +6,10 @@
 from . import models
 from odoo import api, SUPERUSER_ID
 
+def load_translations(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('l10n_fr.l10n_fr_pcg_chart_template').process_coa_translations()
+
 def _l10n_fr_post_init_hook(cr, registry):
     _preserve_tag_on_taxes(cr, registry)
     _setup_inalterability(cr, registry)
