b23349714d4dbc16b9b1ca5e6251cc40842e0bf1
Julien Castiaux <juc@odoo.com>
2023-01-03 19:19:32 +0100

    [FIX] base: order imports in ir_actions_report

--- odoo/addons/base/models/ir_actions_report.py
+++ odoo/addons/base/models/ir_actions_report.py
@@ -1,31 +1,33 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from markupsafe import Markup
-
-from odoo import api, fields, models, tools, SUPERUSER_ID, _
-from odoo.exceptions import UserError, AccessError
-from odoo.tools.safe_eval import safe_eval, time
-from odoo.tools.misc import find_in_path
-from odoo.tools import check_barcode_encoding, config, is_html_empty, parse_version
-from odoo.http import request
-from odoo.osv.expression import NEGATIVE_TERM_OPERATORS, FALSE_DOMAIN
-
 import io
-import logging
-import os
-import lxml.html
-import tempfile
-import subprocess
-import re
 import json
+import logging
+import lxml.html
+import os
+import re
+import subprocess
+import tempfile
 
-from lxml import etree
-from contextlib import closing
-from reportlab.graphics.barcode import createBarcodeDrawing
-from PyPDF2 import PdfFileWriter, PdfFileReader, utils
 from collections import OrderedDict
 from collections.abc import Iterable
+from contextlib import closing
+
+from lxml import etree
+from markupsafe import Markup
 from PIL import Image, ImageFile
+from PyPDF2 import PdfFileWriter, PdfFileReader, utils
+from reportlab.graphics.barcode import createBarcodeDrawing
+
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import UserError, AccessError
+from odoo.http import request
+from odoo.osv.expression import NEGATIVE_TERM_OPERATORS, FALSE_DOMAIN
+from odoo.tools import check_barcode_encoding, config, is_html_empty, parse_version
+from odoo.tools.misc import find_in_path
+from odoo.tools.safe_eval import safe_eval, time
+
+
 # Allow truncated images
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
