34f0bf38641b237713668ebda60ed4559c3af0b6
Joseph Caburnay <jcb@odoo.com>
2023-01-03 17:59:50 +0100

    [IMP] web: auto-select content on focus for autocomplete (e.g. m2o) fields
    On first focus in an autocomplete field, the text content of the input element
    is automatically highlighted.
    
    TASK-ID: 3117420

--- addons/analytic/static/src/components/autocomplete/autocomplete.js
+++ addons/analytic/static/src/components/autocomplete/autocomplete.js
@@ -4,11 +4,3 @@ import { AutoComplete } from "@web/core/autocomplete/autocomplete";
 
 export class AnalyticAutoComplete extends AutoComplete {}
 AnalyticAutoComplete.template = "analytic.AutoComplete";
-AnalyticAutoComplete.props = {
-    ...AutoComplete.props,
-    onFocus: { type: Function, optional: true },
-}
-AnalyticAutoComplete.defaultProps = {
-    ...AutoComplete.defaultProps,
-    onFocus: () => {},
-}
