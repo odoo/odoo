fa3bac08b4a3bd4f96e5ed625f33ebaa92b7e3fd
François Honoré <francois.honore@acsone.eu>
2023-01-04 13:09:54 +0100

    [FIX] Notify (message_post) should be done one by one
    X-original-commit: 7f62a1bc7f9410c3d853391d7fdf436536f0c165

--- addons/account/models/chart_template.py
+++ addons/account/models/chart_template.py
@@ -128,14 +128,15 @@ def update_taxes_from_templates(cr, chart_template_xmlid):
         for account_tax in taxes_to_check:
             message_body += f"<li>{html_escape(account_tax.name)}</li>"
         message_body += "</ul>"
-        partner_managers_ids.message_post(
-            subject=_('Your taxes have been updated !'),
-            author_id=odoobot.id,
-            body=message_body,
-            message_type='notification',
-            subtype_xmlid='mail.mt_comment',
-            partner_ids=[partner.id for partner in partner_managers_ids],
-        )
+        for partner_manager in partner_managers_ids:
+            partner_manager.message_post(
+                subject=_('Your taxes have been updated !'),
+                author_id=odoobot.id,
+                body=message_body,
+                message_type='notification',
+                subtype_xmlid='mail.mt_comment',
+                partner_ids=partner_manager.ids,
+            )
 
     env = api.Environment(cr, SUPERUSER_ID, {})
     chart_template_id = env['ir.model.data']._xmlid_to_res_id(chart_template_xmlid)
