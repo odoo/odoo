473869222768a037fc328296b47f98d5f18ebd0d
Lucas Lefèvre <lul@odoo.com>
2023-01-04 10:34:46 +0100

    [FIX] spreadsheet: improve performance of clickable cells
    Rendering the clickable cells is not a negligeable operation.
    And it happens at every rendering (every scroll operation).
    
    Especially, detecting which functions the cell contains.
    The slow operation is parsing formula.
    
    On the "CRM Leads" dashboard, depending on the visible cells:
    Before: 10+ms (almost the full 16ms allowed to reach 60fps)
    After: ~2-3ms

--- addons/spreadsheet/static/src/helpers/odoo_functions_helpers.js
+++ addons/spreadsheet/static/src/helpers/odoo_functions_helpers.js
@@ -16,18 +16,24 @@ const { parse } = spreadsheet;
  * from the given formula
  *
  * @param {string} formula
- * @param {Function} matcher a predicate that matches a function name
+ * @param {string[]} functionNames e.g. ["ODOO.LIST", "ODOO.LIST.HEADER"]
  * @private
  * @returns {Array<OdooFunctionDescription>}
  */
-export function getOdooFunctions(formula, matcher) {
+export function getOdooFunctions(formula, functionNames) {
+    const formulaUpperCased = formula.toUpperCase();
+    // Parsing is an expensive operation, so we first check if the
+    // formula contains one of the function names
+    if (!functionNames.some((fn) => formulaUpperCased.includes(fn.toUpperCase()))) {
+        return [];
+    }
     let ast;
     try {
         ast = parse(formula);
     } catch (_) {
         return [];
     }
-    return _getOdooFunctionsFromAST(ast, matcher);
+    return _getOdooFunctionsFromAST(ast, functionNames);
 }
 
 /**
@@ -35,27 +41,27 @@ export function getOdooFunctions(formula, matcher) {
  * from the given AST
  *
  * @param {Object} ast (see o-spreadsheet)
- * @param {Function} matcher a predicate that matches a function name
+ * @param {string[]} functionNames e.g. ["ODOO.LIST", "ODOO.LIST.HEADER"]
  *
  * @private
  * @returns {Array<OdooFunctionDescription>}
  */
-function _getOdooFunctionsFromAST(ast, matcher) {
+function _getOdooFunctionsFromAST(ast, functionNames) {
     switch (ast.type) {
         case "UNARY_OPERATION":
-            return _getOdooFunctionsFromAST(ast.operand, matcher);
+            return _getOdooFunctionsFromAST(ast.operand, functionNames);
         case "BIN_OPERATION": {
-            return _getOdooFunctionsFromAST(ast.left, matcher).concat(
-                _getOdooFunctionsFromAST(ast.right, matcher)
+            return _getOdooFunctionsFromAST(ast.left, functionNames).concat(
+                _getOdooFunctionsFromAST(ast.right, functionNames)
             );
         }
         case "FUNCALL": {
             const functionName = ast.value.toUpperCase();
 
-            if (matcher(functionName)) {
+            if (functionNames.includes(functionName)) {
                 return [{ functionName, args: ast.args, isMatched: true }];
             } else {
-                return ast.args.map((arg) => _getOdooFunctionsFromAST(arg, matcher)).flat();
+                return ast.args.map((arg) => _getOdooFunctionsFromAST(arg, functionNames)).flat();
             }
         }
         default:
