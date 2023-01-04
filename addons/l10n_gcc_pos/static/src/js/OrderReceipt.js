90b300f6ee779fb348db18d880ac6eb1c62a0082
rhe-odoo <rhe@odoo.com>
2023-01-04 13:25:14 +0000

    [FIX] l10n_gcc_pos: fix country undefined
    When the country is not defined on the company, there was an error when using the gcc l10n.
    To fix this, we check if the country is set before going further.
    
    closes odoo/odoo#111325
    
    X-original-commit: 0ac8554751fbe22d58108eda0ac24edc933a41d8
    Signed-off-by: Trinh Jacky (trj) <trj@odoo.com>

--- addons/l10n_gcc_pos/static/src/js/OrderReceipt.js
+++ addons/l10n_gcc_pos/static/src/js/OrderReceipt.js
@@ -8,9 +8,7 @@ patch(OrderReceipt.prototype, "l10n_gcc_pos.OrderReceipt", {
         const receipt_render_env = this._super(...arguments);
         const receipt = receipt_render_env.receipt;
         const country = receipt_render_env.order.pos.company.country;
-        receipt.is_gcc_country = ["SA", "AE", "BH", "OM", "QA", "KW"].includes(
-            country && country.code
-        );
+        receipt.is_gcc_country =  country ? ["SA", "AE", "BH", "OM", "QA", "KW"].includes(country && country.code) : false;
         return receipt_render_env;
     },
 });
