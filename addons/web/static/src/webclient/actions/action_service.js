6aa0f9199affcf993f01687054d971595201b68c
Bruno Boi <boi@odoo.com>
2023-01-04 16:02:17 +0100

    [FIX] web: back to same action w/ pending changes
    Before this commit, when a user was on a form view with pending changes
    and would click on the browser back button. If the previous action was
    the same action, the user would be redirected to the form view but a
    traceback would be raised because the form view was not able to handle
    the pending changes "in time".
    
    Here "in time" means before the form view is rendered. The form view
    when rendered will try to update its display name through its controller
    config, but as the new controller will be in fact the same as the
    previous one, its config is currently being changed while the previous
    form view is still using it. This leads to a traceback.
    
    This commit fixes this issue by making sure that the changes are
    properly commited before changing the config for the new controller.
    
    The same kind of issue has been fixed in the action service for the
    restore function, but there are no sensible ways to test this particular
    use case.
    
    X-original-commit: e8a95c37e4e9ee4e4a65a3bfb216d3fa55544933

--- addons/web/static/src/webclient/actions/action_service.js
+++ addons/web/static/src/webclient/actions/action_service.js
@@ -1391,6 +1391,11 @@ function makeActionManager(env) {
         }
         // END LEGACY CODE COMPATIBILITY
 
+        const canProceed = await clearUncommittedChanges(env);
+        if (!canProceed) {
+            return;
+        }
+
         Object.assign(
             newController,
             _getViewInfo(view, controller.action, controller.views, props)
@@ -1408,10 +1413,7 @@ function makeActionManager(env) {
             );
             index = index > -1 ? index : controllerStack.length;
         }
-        const canProceed = await clearUncommittedChanges(env);
-        if (canProceed) {
-            return _updateUI(newController, { index });
-        }
+        return _updateUI(newController, { index });
     }
 
     /**
@@ -1433,6 +1435,10 @@ function makeActionManager(env) {
             const msg = jsId ? "Invalid controller to restore" : "No controller to restore";
             throw new ControllerNotFoundError(msg);
         }
+        const canProceed = await clearUncommittedChanges(env);
+        if (!canProceed) {
+            return;
+        }
         const controller = controllerStack[index];
         if (controller.action.type === "ir.actions.act_window") {
             const { action, exportedState, view, views } = controller;
@@ -1443,10 +1449,7 @@ function makeActionManager(env) {
             }
             Object.assign(controller, _getViewInfo(view, action, views, props));
         }
-        const canProceed = await clearUncommittedChanges(env);
-        if (canProceed) {
-            return _updateUI(controller, { index });
-        }
+        return _updateUI(controller, { index });
     }
 
     /**
