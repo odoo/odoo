505e82859b6f60008e250993d73e894dada25fe1
Soukéina Bojabza <sobo@odoo.com>
2023-01-04 10:51:28 +0100

    [FIX] website: increase the breakpoint of the 'Show on mobile' option
    When using the 'Show on mobile' option, we can see that there is a
    mismatch between the screen breakpoint at which the elements are
    displayed like in mobile view (=> under 992px or `lg`) and the one that
    is impacted by the 'Hide/Show' option (=> under 768px or `md`). This is
    a problem because between these two breakpoints, the display is like in
    mobile view but is not considered as such and so, hiding an element in
    the mobile view (for example, if it does not look good in it) has no
    effect until the screen reaches 768px.
    
    This commit increases the screen breakpoint at which the 'Show on
    mobile' option is applied, that is, up to 992px instead of 768px, in
    order to be consistent with the display.
    
    task-3110770
    
    X-original-commit: 8688df9c0601064b5e4b6eea93e003225af77eb4

--- addons/website/static/src/js/editor/snippets.options.js
+++ addons/website/static/src/js/editor/snippets.options.js
@@ -2437,7 +2437,12 @@ options.registry.MobileVisibility = options.Class.extend({
      * @see this.selectClass for parameters
      */
     showOnMobile(previewMode, widgetValue, params) {
-        const classes = `d-none d-md-${this.$target.css('display')}`;
+        // For compatibility with former implementation: remove the previously
+        // added `d-md-*` class if any, as it should now be `d-lg-*`.
+        if (widgetValue) {
+            this.$target[0].classList.remove(`d-md-${this.$target.css('display')}`);
+        }
+        const classes = `d-none d-lg-${this.$target.css('display')}`;
         this.$target.toggleClass(classes, !widgetValue);
     },
 
@@ -2452,7 +2457,7 @@ options.registry.MobileVisibility = options.Class.extend({
         if (methodName === 'showOnMobile') {
             const classList = [...this.$target[0].classList];
             return classList.includes('d-none') &&
-                classList.some(className => className.startsWith('d-md-')) ? '' : 'true';
+                classList.some(className => className.match(/^(d-md-|d-lg-)/g)) ? '' : 'true';
         }
         return await this._super(...arguments);
     },
