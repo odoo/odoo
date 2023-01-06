9bee704a2be363362d3a30deac20ae677f4e18c9
Sébastien Geelen (sge) <sge@odoo.com>
2023-01-06 13:27:43 +0000

    [REF] web,web_editor,mail : dynamicplaceholder hook
    Refactor the Dynamic Placeholder hook to have better
    separation of concern from the Field that need it.
    
    task-3081325
    
    Part-of: odoo/odoo#109326

--- addons/web/static/src/views/fields/char/char_field.js
+++ addons/web/static/src/views/fields/char/char_field.js
@@ -7,9 +7,9 @@ import { formatChar } from "../formatters";
 import { useInputField } from "../input_field_hook";
 import { standardFieldProps } from "../standard_field_props";
 import { TranslationButton } from "../translation_button";
-import { useDynamicPlaceholder } from "../dynamicplaceholder_hook";
+import { useDynamicPlaceholder } from "../dynamic_placeholder_hook";
 
-import { Component, onMounted, onWillUnmount, useRef } from "@odoo/owl";
+import { Component, useExternalListener, useRef, useEffect } from "@odoo/owl";
 
 export class CharField extends Component {
     static template = "web.CharField";
@@ -22,55 +22,20 @@ export class CharField extends Component {
         isPassword: { type: Boolean, optional: true },
         placeholder: { type: String, optional: true },
         dynamicPlaceholder: { type: Boolean, optional: true },
+        dynamicPlaceholderModelReferenceField: { type: String, optional: true },
     };
     static defaultProps = { dynamicPlaceholder: false };
 
     setup() {
-        if (this.props.dynamicPlaceholder) {
-            this.dynamicPlaceholder = useDynamicPlaceholder();
-        }
-
         this.input = useRef("input");
+        if (this.props.dynamicPlaceholder) {
+            const dynamicPlaceholder = useDynamicPlaceholder(this.input);
+            useExternalListener(document, "keydown", dynamicPlaceholder.onKeydown);
+            useEffect(() =>
+                dynamicPlaceholder.updateModel(this.props.dynamicPlaceholderModelReferenceField)
+            );
+        }
         useInputField({ getValue: () => this.props.value || "", parse: (v) => this.parse(v) });
-        onMounted(this.onMounted);
-        onWillUnmount(this.onWillUnmount);
-    }
-    async onKeydownListener(ev) {
-        if (ev.key === this.dynamicPlaceholder.TRIGGER_KEY && ev.target === this.input.el) {
-            const baseModel = this.props.record.data.mailing_model_real;
-            if (baseModel) {
-                await this.dynamicPlaceholder.open(this.input.el, baseModel, {
-                    validateCallback: this.onDynamicPlaceholderValidate.bind(this),
-                    closeCallback: this.onDynamicPlaceholderClose.bind(this),
-                });
-            }
-        }
-    }
-    onMounted() {
-        if (this.props.dynamicPlaceholder) {
-            this.keydownListenerCallback = this.onKeydownListener.bind(this);
-            document.addEventListener("keydown", this.keydownListenerCallback);
-        }
-    }
-    onWillUnmount() {
-        if (this.props.dynamicPlaceholder) {
-            document.removeEventListener("keydown", this.keydownListenerCallback);
-        }
-    }
-    onDynamicPlaceholderValidate(chain, defaultValue) {
-        if (chain) {
-            const triggerKeyReplaceRegex = new RegExp(`${this.dynamicPlaceholder.TRIGGER_KEY}$`);
-            let dynamicPlaceholder = "{{object." + chain.join(".");
-            dynamicPlaceholder +=
-                defaultValue && defaultValue !== "" ? ` or '''${defaultValue}'''}}` : "}}";
-            this.props.record.update({
-                [this.props.name]:
-                    this.input.el.value.replace(triggerKeyReplaceRegex, "") + dynamicPlaceholder,
-            });
-        }
-    }
-    onDynamicPlaceholderClose() {
-        this.input.el.focus();
     }
 
     get shouldTrim() {
@@ -100,7 +65,8 @@ export const charField = {
     supportedTypes: ["char"],
     extractProps: ({ attrs, options }) => ({
         isPassword: archParseBoolean(attrs.password),
-        dynamicPlaceholder: options.dynamic_placeholder,
+        dynamicPlaceholder: options?.dynamic_placeholder || false,
+        dynamicPlaceholderModelReferenceField: options?.dynamic_placeholder_model_reference_field || "",
         autocomplete: attrs.autocomplete,
         placeholder: attrs.placeholder,
     }),
