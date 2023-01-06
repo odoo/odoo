0f6d4f72bb425f1c16b34d2543df36bce3580d3c
Shawcker <anpp@odoo.com>
2023-01-06 09:13:40 +0100

    [FIX] account: restrict display of 'reconciled entries' button on moves
    When resetting a payment/move to draft, the related entries are unreconciled. The 'reconciled entries' button raises an error since there isn't any.
    
    This is due to the field `has_reconciled_entries` being wrongly computed since the `_reconciled_lines()` method doesn't actually filter on lines that are reconciled.
    
    Fixing this removes the button when there is no reconciled lines, and avoids the errors raised.
    
    task id=3120327

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -2495,7 +2495,7 @@ class AccountMoveLine(models.Model):
 
     def _reconciled_lines(self):
         ids = []
-        for aml in self.filtered('account_id.reconcile'):
+        for aml in self.filtered('reconciled'):
             ids.extend([r.debit_move_id.id for r in aml.matched_debit_ids] if aml.credit > 0 else [r.credit_move_id.id for r in aml.matched_credit_ids])
             ids.append(aml.id)
         return ids
