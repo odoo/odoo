cc47cfce7d1190e95d9f892b0ebf0e8afd5c8f63
Maximilien (malb) <malb@odoo.com>
2023-01-09 15:35:12 +0000

    [IMP] l10n_ve: Translation
    All the localisation should be written in english and then translated to the native language of the country. In this case, the Venezuelian localisation was written is Spanish, this PR translate the module in english and add the corresponding PO file in spanish.
    
    closes odoo/odoo#109494
    
    Task-id: 3126154
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_ve/__init__.py
+++ addons/l10n_ve/__init__.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, SUPERUSER_ID
+
+def load_translations(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('l10n_ve.ve_chart_template_amd').process_coa_translations()
