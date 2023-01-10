f826e7308368af230d92f1c61cb732be48141612
roen-odoo <roen@odoo.com>
2023-01-10 17:22:23 +0000

    [FIX] pos_sale_margin: correctly compute margin when no cost is set
    Current behavior:
    If there was no cost set on a product, the margin couldn't be calculated
    for the PoS sale report.
    
    Steps to reproduce:
    - Install PoS and Sales
    - Create a product with no cost
    - Open a session and make an order with this item
    - Close the session and go to the PoS sale report
    - Add the margin measure to the report, and show the stats by product
    - The new product does not have any margin.
    
    opw-3120208
    
    closes odoo/odoo#109618
    
    Signed-off-by: Trinh Jacky (trj) <trj@odoo.com>

--- addons/pos_sale_margin/report/sale_report.py
+++ addons/pos_sale_margin/report/sale_report.py
@@ -10,5 +10,5 @@ class SaleReport(models.Model):
     def _select_pos(self, fields=None):
         if not fields:
             fields = {}
-        fields['margin'] = ', SUM(l.price_subtotal - l.total_cost / CASE COALESCE(pos.currency_rate, 0) WHEN 0 THEN 1.0 ELSE pos.currency_rate END) AS margin'
+        fields['margin'] = ', SUM(l.price_subtotal - COALESCE(l.total_cost,0) / CASE COALESCE(pos.currency_rate, 0) WHEN 0 THEN 1.0 ELSE pos.currency_rate END) AS margin'
         return super()._select_pos(fields)
