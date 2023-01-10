4f7d03829e88ac3c7500fb1eb15dfbfbe26cc431
smdc-odoo <smdc@odoo.com>
2023-01-10 13:07:13 +0000

    [IMP] account,account_debit_note: Change credit / debit note buttons
    Debit Notes are a type of financial documents that is not very often used. Even though it's an official document type, in many countries their usage is not
    
    Odoo does not come with a preinstalled Debit Note management, but it can be added by installing the module account_debit_note. The module as mostly been developped following localisation requirements, and while it's a corner case, it appears all the time in one of our most critical screen. Let's make it a bit more discrete.
    
    task-3125955
    
    closes odoo/odoo#109539
    
    Signed-off-by: Laurent Smet <las@odoo.com>

--- addons/account_debit_note/wizard/account_debit_note.py
+++ addons/account_debit_note/wizard/account_debit_note.py
@@ -33,6 +33,10 @@ class AccountDebitNote(models.TransientModel):
         move_ids = self.env['account.move'].browse(self.env.context['active_ids']) if self.env.context.get('active_model') == 'account.move' else self.env['account.move']
         if any(move.state != "posted" for move in move_ids):
             raise UserError(_('You can only debit posted moves.'))
+        elif any(move.debit_origin_id for move in move_ids):
+            raise UserError(_("You can't make a debit note for an invoice that is already linked to a debit note."))
+        elif any(move.move_type not in ['out_invoice', 'in_invoice', 'out_refund', 'in_refund'] for move in move_ids):
+            raise UserError(_("You can make a debit note only for a Customer Invoice, a Customer Credit Note, a Vendor Bill or a Vendor Credit Note."))
         res['move_ids'] = [(6, 0, move_ids.ids)]
         return res
 
