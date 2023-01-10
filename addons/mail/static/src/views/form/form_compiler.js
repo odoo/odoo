40b27e2d83424019cae57d9f4d4379c80301a8f4
Michael (mcm) <mcm@odoo.com>
2023-01-10 09:34:05 +0000

    [FIX] mail: prevent displaying attachment preview in dialog
    Before this commit, attachment preview could be displayed
    in x2m dialogs. Now, the compiler removes the attachment preview
    from the arch so it is never displayed in renderer.
    
    closes odoo/odoo#114967
    
    X-original-commit: 4938d27da71da1e30899bad80c9cad3cb96aa417
    Signed-off-by: Géry Debongnie <ged@odoo.com>
    Signed-off-by: Michaël Mattiello <mcm@odoo.com>

--- addons/mail/static/src/views/form/form_compiler.js
+++ addons/mail/static/src/views/form/form_compiler.js
@@ -150,11 +150,7 @@ registry.category("form_compilers").add("chatter_compiler", {
 
 registry.category("form_compilers").add("attachment_preview_compiler", {
     selector: "div.o_attachment_preview",
-    fn: (node) =>
-        compileAttachmentPreview(node, {
-            threadId: "__comp__.props.record.resId or undefined",
-            threadModel: "__comp__.props.record.resModel",
-        }),
+    fn: () => createElement("t"),
 });
 
 patch(FormCompiler.prototype, "mail", {
