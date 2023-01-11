e99df03b3fa758555a1aa11b4bce03ee0fc55d6a
yhu-odoo <yhu@odoo.com>
2023-01-11 11:15:54 +0100

    [IMP] mrp: manual consumption improvement
    Previously we set a component on MO to be manual consumption by comparing
    the To Consume and Consumed, if they are not the same, we consider it a
    manual consumption. Now if any input activity in the Consumed cell, we
    will consider it a manual consumption.
    
    Note that in the code, we make the css class change happended in the
    list renderer instead of the field widget. We do that because we want to
    change the background color of the whole cell not just the text of the
    field.
    
    Task-3116125

---
+++ addons/mrp/static/src/widgets/mrp_consumed.js
@@ -0,0 +1,37 @@
+/** @odoo-module */
+
+import { registry } from "@web/core/registry";
+import { FloatField, floatField } from '@web/views/fields/float/float_field';
+
+const { useRef, useEffect } = owl;
+
+export class MrpConsumed extends FloatField {
+    setup() {
+        super.setup();
+        const inputRef = useRef("numpadDecimal");
+
+        useEffect(
+            (inputEl) => {
+                if (inputEl) {
+                    inputEl.addEventListener("input", this.onInput.bind(this));
+                    return () => {
+                        inputEl.removeEventListener("input", this.onInput.bind(this));
+                    };
+                }
+            },
+            () => [inputRef.el]
+        );
+    }
+
+    onInput(ev) {
+        this.props.setDirty(true);
+        return this.props.record.update({ manual_consumption: true });
+    }
+}
+
+export const mrpConsumed = {
+    ...floatField,
+    component: MrpConsumed,
+};
+
+registry.category('fields').add('mrp_consumed', mrpConsumed);
---
+++ addons/mrp/static/src/widgets/mrp_production_components_x2many.js
@@ -0,0 +1,28 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { X2ManyField, x2ManyField } from "@web/views/fields/x2many/x2many_field";
+import { ListRenderer } from "@web/views/list/list_renderer";
+
+export class MrpProductionComponentsListRenderer extends ListRenderer {
+    getCellClass(column, record) {
+        let classNames = super.getCellClass(...arguments);
+        if (column.name == "quantity_done" && !record.data.manual_consumption) {
+            classNames += ' o_non_manual_consumption';
+        }
+        return classNames;
+    }
+}
+
+export class MrpProductionComponentsX2ManyField extends X2ManyField {}
+MrpProductionComponentsX2ManyField.components = {
+    ...X2ManyField.components,
+    ListRenderer: MrpProductionComponentsListRenderer
+};
+
+export const mrpProductionComponentsX2ManyField = {
+    ...x2ManyField,
+    component: MrpProductionComponentsX2ManyField,
+};
+
+registry.category("fields").add("mrp_production_components_x2many", mrpProductionComponentsX2ManyField);
