dd2741503e9c6bc009109f1229feef7a823ce036
Abdelouahab (abla) <abla@odoo.com>
2023-01-12 15:33:11 +0000

    [FIX] point_of_sale: show full address
    To Reproduce
    ============
    on POS go to customer, on the customers list we only display a short address.
    which gives bad experience to the user.
    
    Solution
    ========
    The address information displayed was reduced in early 15 because it was often
    too long and caused rendering issues, on both desktop and mobile.
    On 16 we have more space, so we bring back the full address.
    
    opw-3123832
    
    X-original-commit: 1d546031ac74241eb9d7eb090a5291aa70371d3f

--- addons/point_of_sale/static/src/js/Screens/PartnerListScreen/PartnerLine.js
+++ addons/point_of_sale/static/src/js/Screens/PartnerListScreen/PartnerLine.js
@@ -7,10 +7,6 @@ class PartnerLine extends PosComponent {
     get highlight() {
         return this._isPartnerSelected ? "highlight" : "";
     }
-    get shortAddress() {
-        const { partner } = this.props;
-        return [partner.zip, partner.city, partner.state_id[1]].filter((field) => field).join(", ");
-    }
     get _isPartnerSelected() {
         return this.props.partner === this.props.selectedPartner;
     }
