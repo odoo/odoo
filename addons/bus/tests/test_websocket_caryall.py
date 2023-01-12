95a914813d737bddbee24a4f6d339b52c348b175
tsm-odoo <tsm@odoo.com>
2023-01-12 09:48:54 +0000

    [FIX] bus: fix websocket timeout burst
    Before this commit, the keep alive timeout could occur for many websockets at
    the same time resulting in a burst of transaction (cursor is open when connecting,
    disconnecting a websocket).
    
    This issue was even worse because the cursor was opened even when it was
    not needed: we don't need to open a cursor if there is no callback
    registered for the lifecycle event (OPEN/CLOSE).
    
    This commit fixes those issues by:
    - adding a random delay to the keep alive timeout of every websocket.
    - not triggering lifecycle events if no callbacks are registered.
    
    closes odoo/odoo#110127
    
    X-original-commit: b4fb1359021b6c882ece3037c82aaaf65cfc2e1e
    Signed-off-by: Julien Castiaux <juc@odoo.com>

--- addons/bus/tests/test_websocket_caryall.py
+++ addons/bus/tests/test_websocket_caryall.py
@@ -88,9 +88,9 @@ class TestWebsocketCaryall(WebsocketCase):
     def test_timeout_manager_keep_alive_timeout(self):
         with freeze_time('2022-08-19') as frozen_time:
             timeout_manager = TimeoutManager()
-            frozen_time.tick(delta=timedelta(seconds=TimeoutManager.KEEP_ALIVE_TIMEOUT / 2))
+            frozen_time.tick(delta=timedelta(seconds=timeout_manager._keep_alive_timeout / 2))
             self.assertFalse(timeout_manager.has_timed_out())
-            frozen_time.tick(delta=timedelta(seconds=TimeoutManager.KEEP_ALIVE_TIMEOUT / 2))
+            frozen_time.tick(delta=timedelta(seconds=timeout_manager._keep_alive_timeout / 2 + 1))
             self.assertTrue(timeout_manager.has_timed_out())
             self.assertEqual(timeout_manager.timeout_reason, TimeoutReason.KEEP_ALIVE)
 
@@ -284,3 +284,9 @@ class TestWebsocketCaryall(WebsocketCase):
                 'data': {'channels': ['my_channel'], 'last': client_last_notification_id}
             }))
             subscribe_done_event.wait()
+
+    def test_no_cursor_when_no_callback_for_lifecycle_event(self):
+        with patch.object(Websocket, '_event_callbacks', defaultdict(set)):
+            with patch('odoo.addons.bus.websocket.acquire_cursor') as mock:
+                self.websocket_connect()
+                self.assertFalse(mock.called)
