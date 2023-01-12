61bf9ae3bfe7820e4f724e985050c29da027e836
Andrius Laukavičius <anlaukavic@gmail.com>
2023-01-12 08:26:54 +0000

    [FIX] hr_expense: improve computed field performance
    If you have large amount of product.template records (e.g. over 40k),
    `hr_expense` module can't be installed as you would get MemoryError. It
    would consume all memory while computing `can_be_expensed` field.
    
    For that, using `_auto_init` to make it less of a memory hog.
    
    closes odoo/odoo#131954
    
    X-original-commit: 1138a24760ad57cb9e9895926012d269b4e2bfb0
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>

--- addons/hr_expense/models/product_template.py
+++ addons/hr_expense/models/product_template.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+from odoo.tools.sql import column_exists, create_column
 
 
 class ProductTemplate(models.Model):
@@ -16,6 +17,18 @@ class ProductTemplate(models.Model):
 
     can_be_expensed = fields.Boolean(string="Can be Expensed", compute='_compute_can_be_expensed',
         store=True, readonly=False, help="Specify whether the product can be selected in an expense.")
+
+    def _auto_init(self):
+        if not column_exists(self.env.cr, "product_template", "can_be_expensed"):
+            create_column(self.env.cr, "product_template", "can_be_expensed", "boolean")
+            self.env.cr.execute(
+                """
+                UPDATE product_template
+                SET can_be_expensed = false
+                WHERE type NOT IN ('consu', 'service')
+                """
+            )
+        return super()._auto_init()
 
     @api.depends('type')
     def _compute_can_be_expensed(self):
