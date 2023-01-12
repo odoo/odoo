fb0359e750a73519eb3699ea4ff726f37cc69892
Thibault Delavallée <tde@odoo.com>
2023-01-12 17:44:12 +0000

    [IMP] account: update account.invoice.send
    Due to new composer, it is a bit less necessary to create them on the fly
    randomly. Some quick code cleaning is performed to better understand code
    and variables. Notably 'default_get' override was a bit messy and should
    not return unwanted values.
    
    Task-3093257 (Mail: The Composer Update)
    Task-3149286 (Account: Cleanup account.invoice.send wizard)
    
    Part-of: odoo/odoo#107356

--- addons/account/wizard/account_invoice_send.py
+++ addons/account/wizard/account_invoice_send.py
@@ -31,23 +31,23 @@ class AccountInvoiceSend(models.TransientModel):
         readonly=True)
 
     @api.model
-    def default_get(self, fields):
-        res = super(AccountInvoiceSend, self).default_get(fields)
-        res_ids = self._context.get('active_ids')
+    def default_get(self, fields_list):
+        result = super().default_get(fields_list)
+        res_ids = self.env.context.get('active_ids')
 
         invoices = self.env['account.move'].browse(res_ids).filtered(lambda move: move.is_invoice(include_receipts=True))
         if not invoices:
             raise UserError(_("You can only send invoices."))
 
-        composer = self.env['mail.compose.message'].create({
-            'composition_mode': 'comment' if len(res_ids) == 1 else 'mass_mail',
-            'subtype_id': self.env['ir.model.data']._xmlid_to_res_id('mail.mt_comment'),
-        })
-        res.update({
-            'invoice_ids': res_ids,
-            'composer_id': composer.id,
-        })
-        return res
+        if 'invoice_ids' in fields_list and not result.get('invoice_ids'):
+            result['invoice_ids'] = res_ids
+        if 'composer_id' in fields_list and not result.get('composer_id'):
+            composer = self.env['mail.compose.message'].create({
+                'composition_mode': 'comment' if len(res_ids) == 1 else 'mass_mail',
+                'subtype_id': self.env['ir.model.data']._xmlid_to_res_id('mail.mt_comment'),
+            })
+            result['composer_id'] = composer.id
+        return result
 
     @api.onchange('invoice_ids')
     def _compute_composition_mode(self):
