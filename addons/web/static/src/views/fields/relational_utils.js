df10af68dff53761b5aa74ee08d0d226556c6e11
Lucas Perais <lpe@odoo.com>
2023-01-12 11:37:18 +0100

    [FIX] web: one2many: field in list and not in form
    Have a one2many that defines a list arch.
    The list should not be editable, precisely, when adding a record, it should go through
    a form view.
    
    Define a standard form view in DB, not in the main arch, that doesn't have
    at least one of the field that the list arch has.
    
    Create a record in the one2many list, change a field in order to trigger an onchange.
    
    Before this commit, there was a crash, because the onchange asked for the field present in the list
    and the ones present in the form, but there was a description (the result of fields_get) only
    for the fields present in the form.
    
    After this commit, this is solved.

--- addons/web/static/src/views/fields/relational_utils.js
+++ addons/web/static/src/views/fields/relational_utils.js
@@ -537,7 +537,10 @@ async function getFormViewInfo({ list, activeField, viewService, userService, en
             views: [[false, "form"]],
         });
         const archInfo = new FormArchParser().parse(views.form.arch, relatedModels, comodel);
-        formViewInfo = { ...archInfo, fields }; // should be good to memorize this on activeField
+        // Fields that need to be defined are the ones in the form view, this is natural,
+        // plus the ones that the list record has, that is, present in either the list arch or the kanban arch
+        // of the one2many field
+        formViewInfo = { ...archInfo, fields: { ...list.fields, ...fields } }; // should be good to memorize this on activeField
     }
 
     await loadSubViews(
