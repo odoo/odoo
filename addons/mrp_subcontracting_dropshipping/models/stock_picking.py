f880512d88c0e45048514c35f153813e3d0b7078
Adrien Widart (awt) <awt@odoo.com>
2023-01-12 15:51:34 +0000

    [FIX] mrp_subcontracting_dropshipping,*: dropship valued product
    *:purchase_stock,stock_account
    
    To reproduce the issue:
    1. In Settings, enable 'Multi-Step Routes'
    2. Create a product category PC:
       - Method: FIFO
       - Valuation: Auto
    3. Create two products P_compo, P_finished
       - Both:
         - Type: Storable
         - Category: PC
         - With a positive cost
       - P_finished:
         - Vendors: a new vendor V
    4. Create and confirm a SO with one order line:
       - Product: P_finished
       - Route: Dropship
    5. Confirm the PO and process the dropship
    6. Accounting > Accounting > Miscellaneous > Journal Items
    
    Error: There are a lot of lines and most are incorrect
    
    When marking the dropship SM as done, we check how we should
    value it:
    https://github.com/odoo/odoo/blob/18450973eaf3ca048cdcd2046a8cba5f5c0f98b0/addons/stock_account/models/stock_move.py#L244-L252
    Here is one issue: `_is_dropshipped` returns `True`, which is
    correct because the source is the subcontractor location and the
    destination a customer:
    https://github.com/odoo/odoo/blob/8276a9b51b7c5fc0ae6dd557ae88a9c31f1d1962/addons/mrp_subcontracting_dropshipping/models/stock_move.py#L14-L20
    However, `_is_out` will return `True` too. To get this value, we
    check if the source and destination locations have to be valued:
    https://github.com/odoo/odoo/blob/18450973eaf3ca048cdcd2046a8cba5f5c0f98b0/addons/stock_account/models/stock_move.py#L99-L100
    Again, since the source location is the subcontractor one (which is
    an internal one) and the destination location is the customer one,
    we will consider that we have to value this SM. As a result, we will
    apply `_create_out_svl` and `_create_dropshipped_svl` on the same SM
    (this explains some incorrect AMLs are generated).
    
    Then, there is another issue. In `_create_dropshipped_svl`, we
    create an in-SVL and an out-SVL for P_finished. However, when
    processing the MO, an in-SVL will also be generated for P_finished.
    The issue comes from `_create_dropshipped_svl`: it creates an in-SVL
    but the source location is not outside from the company, so it
    should not create the incoming layer.
    
    Finally, when creating the SVL in `_create_dropshipped_svl`, we use
    the unit price of the SM to define the value of the SVL. This is an
    issue: suppose the unit price of the POL is $10 and the costs of
    P_compo is $1, the unit price of the SM is then $10 (and so does the
    value of the SVL) but the actual value of the delivered P_finished
    is $11. Howver, when calling `_action_done` on a SM, we first
    process it and only then we will process the MO:
    https://github.com/odoo/odoo/blob/829369d3ca0530f1aad0599fbb1598810a928cc3/addons/mrp_subcontracting/models/stock_picking.py#L33-L36
    So, in `_create_dropshipped_svl`, we can't know the actual cost of
    P_finished. This is the reason why we have to create a compensation
    layer once the MO is processed (i.e. in the override from
    `mrp_subcontracting_dropshipping`)
    
    Similar issues will happen if we return the
    dropshipped/subcontracted product.
    
    OPW-3078499
    
    closes odoo/odoo#109791
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/mrp_subcontracting_dropshipping/models/stock_picking.py
+++ addons/mrp_subcontracting_dropshipping/models/stock_picking.py
@@ -2,10 +2,50 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models
+from odoo.tools import float_compare
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
+
+    def _action_done(self):
+        """ If needed, create a compensation layer, so we add the MO cost
+        to the dropship one
+        """
+        res = super()._action_done()
+        for move in self.move_lines:
+            if not (move.is_subcontract and move._is_dropshipped() and move.state == 'done'):
+                continue
+
+            dropship_svls = move.stock_valuation_layer_ids
+            if not dropship_svls:
+                continue
+
+            subcontract_svls = move.move_orig_ids.stock_valuation_layer_ids
+            subcontract_value = sum(subcontract_svls.mapped('value'))
+            dropship_value = abs(sum(dropship_svls.mapped('value')))
+            diff = subcontract_value - dropship_value
+            if float_compare(diff, 0, precision_rounding=move.company_id.currency_id.rounding) <= 0:
+                continue
+
+            svl_vals = move._prepare_common_svl_vals()
+            svl_vals.update({
+                'remaining_value': 0,
+                'remaining_qty': 0,
+                'value': -diff,
+                'quantity': 0,
+                'unit_cost': 0,
+                'stock_valuation_layer_id': dropship_svls[0].id,
+                'stock_move_id': False,
+            })
+            svl = self.env['stock.valuation.layer'].create(svl_vals)
+
+            move = move.with_company(move.company_id)
+            if move.product_id.valuation != 'real_time':
+                continue
+            move._account_entry_move(svl.quantity, svl.description, svl.id, svl.value)
+
+        return res
 
     def _get_warehouse(self, subcontract_move):
         if subcontract_move.sale_line_id:
