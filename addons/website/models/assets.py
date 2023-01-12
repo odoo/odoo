88ba92a60ce3b6f9910380e401ea4d321ddac67f
qsm-odoo <qsm@odoo.com>
2023-01-12 14:25:27 +0000

    [FIX] website: allow to reinstall website after deleted user-websites
    Before this commit, this flow was broken:
    
    - Install website
    - Create a new website of your own (not using the one created
      automatically from XML data)
    - Choose another color palette for that website
    - Uninstall the website app
    - Reinstall the website app
    - Try to choose another color palette for any website
    => It does not work
    
    Indeed, after the uninstallation, the DB is left in an invalid state:
    the SCSS customizations attachments of the website that was created by
    the user are not removed, they just have their website_id field emptied.
    Some code made at [1] was already there to remove those attachments. The
    problem is that it only worked for websites which were created by XML
    data (at website installation), not by the user. Indeed, the `unlink`
    method is not called during uninstallation to remove records that were
    created by the user, thus the `unlink` override was not called either.
    See [2] for some details.
    
    This fixes the issues by moving this attachment cleaning code in a
    dedicated method, called in `unlink` but also in the `uninstall_hook` of
    the website app.
    
    [1]: https://github.com/odoo/odoo/commit/2f361bec36dff09181b96d140d62c477cdf013a1
    [2]: https://github.com/odoo/odoo/pull/97852#pullrequestreview-1067851656
    
    opw-3127531
    
    X-original-commit: e83f83fb8180683793a4030f91c246deaec5308b

--- addons/website/models/assets.py
+++ addons/website/models/assets.py
@@ -143,6 +143,15 @@ class Assets(models.AbstractModel):
             self = self.sudo()
         website = self.env['website'].get_current_website()
         res = super(Assets, self)._get_custom_attachment(custom_url, op=op)
+        # FIXME (?) In website, those attachments should always have been
+        # created with a website_id. The "not website_id" part in the following
+        # condition might therefore be useless (especially since the attachments
+        # do not seem ordered). It was developed in the spirit of served
+        # attachments which follow this rule of "serve what belongs to the
+        # current website or all the websites" but it probably does not make
+        # sense here. It however allowed to discover a bug where attachments
+        # were left without website_id. This will be kept untouched in stable
+        # but will be reviewed and made more robust in master.
         return res.with_context(website_id=website.id).filtered(lambda x: not x.website_id or x.website_id == website)
 
     def _get_custom_asset(self, custom_url):
