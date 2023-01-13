3663b365e52820ef69340c2bd930f7b4dac0bf35
Laurent Smet <las@odoo.com>
2023-01-13 08:13:40 +0000

    [FIX] account: Fix missing where clause on constraint
    Introduced by: https://github.com/odoo/odoo/commit/f9500ca4c898cd86681cc2e48ec2c66cf5670917
    
    closes odoo/odoo#109845
    
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/account/models/account_account.py
+++ addons/account/models/account_account.py
@@ -305,6 +305,7 @@ class AccountAccount(models.Model):
               FROM account_journal journal
               JOIN account_account account ON journal.default_account_id = account.id
              WHERE account.account_type IN ('asset_receivable', 'liability_payable')
+               AND account.id IN %s
              LIMIT 1;
         ''', [tuple(self.ids)])
 
