8a8caf70f9b90b0b9154ac9a9475c9878cd3eea3
abd-msyukyu-odoo <abd@odoo.com>
2023-01-13 13:42:30 +0000

    [IMP] crm: owl improvements
    Applies minor fixups to improve owl usage in CRM, following the JS team advice:
    https://github.com/odoo/odoo/pull/104975
    
    * remove unused export
    * use orm service from model
    * remove redundancy for component call
    
    Task-3056665
    
    Part-of: odoo/odoo#109882

--- addons/crm/static/src/views/check_rainbowman_message.js
+++ addons/crm/static/src/views/check_rainbowman_message.js
@@ -1,9 +1,5 @@
 /** @odoo-module **/
 
-import { useService } from "@web/core/utils/hooks";
-
-const { useComponent } = owl;
-
 export async function checkRainbowmanMessage(orm, effect, recordId) {
     const message = await orm.call("crm.lead", "get_rainbowman_message", [[recordId]]);
     if (message) {
@@ -12,11 +8,4 @@ export async function checkRainbowmanMessage(orm, effect, recordId) {
             type: "rainbow_man",
         });
     }
-}
-
-export function useCheckRainbowman() {
-    const component = useComponent();
-    const orm = useService("orm");
-    const effect = useService("effect");
-    return checkRainbowmanMessage.bind(component, orm, effect);
 }
