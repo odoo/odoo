1555e79be0b52c30df43e8de50f5db56fbbda630
Rémi Rahir <rar@odoo.com>
2023-01-13 10:48:28 +0000

    [FIX] spreadsheet: Catch access errors on spreadsheet
    Currently, a user opening a spreadsheet containing pivots/lists/graphs
    that they don't have the rights to access (be it because of the parent
    model or the domain applied) will end up in an infinite loop.
    
    While starting the datasources, the fetch step will throw, potentially
    spamming the user with access errors.
    
    Manual forward port of https://github.com/odoo/enterprise/pull/35146
    
    Task 3107650
    
    closes odoo/odoo#109991
    
    X-original-commit: 51c2a61e6eda66a973fdd7b41d3cdd6f0ba7fe5b
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/spreadsheet/static/src/data_sources/data_source.js
+++ addons/spreadsheet/static/src/data_sources/data_source.js
@@ -1,6 +1,7 @@
 /** @odoo-module */
 
 import { LoadingDataError } from "@spreadsheet/o_spreadsheet/errors";
+import { RPCError } from "@web/core/network/rpc_service";
 import { KeepLast } from "@web/core/utils/concurrency";
 
 /**
@@ -30,6 +31,8 @@ export class LoadableDataSource {
          */
         this._loadPromise = undefined;
         this._isFullyLoaded = false;
+        this._isValid = true;
+        this._loadErrorMessage = "";
     }
 
     /**
@@ -45,11 +48,19 @@ export class LoadableDataSource {
         }
         if (!this._loadPromise) {
             this._isFullyLoaded = false;
-            this._loadPromise = this._concurrency.add(this._load()).then(() => {
-                this._lastUpdate = Date.now();
-                this._isFullyLoaded = true;
-                this._notify();
-            });
+            this._isValid = true;
+            this._loadErrorMessage = "";
+            this._loadPromise = this._concurrency
+                .add(this._load())
+                .catch((e) => {
+                    this._isValid = false;
+                    this._loadErrorMessage = e instanceof RPCError ? e.data.message : e.message;
+                })
+                .finally(() => {
+                    this._lastUpdate = Date.now();
+                    this._isFullyLoaded = true;
+                    this._notify();
+                });
         }
         return this._loadPromise;
     }
@@ -72,6 +83,9 @@ export class LoadableDataSource {
         if (!this._isFullyLoaded) {
             this.load();
             throw new LoadingDataError();
+        }
+        if (!this._isValid) {
+            throw new Error(this._loadErrorMessage);
         }
     }
 
