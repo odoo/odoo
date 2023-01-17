b6098a6b88993de214344532519b1d025804a0bb
Benoit Socias <bso@odoo.com>
2023-01-17 10:15:56 +0100

    [FIX] web_editor, *: activate all snippets after move with drag and drop
    *: website
    
    Before this commit, dynamic snippets were not activated after being
    moved by using the drag and drop, because they are made invisible until
    their content is fetched.
    
    This commit makes sure any pending `onMove` is completed during a drop
    before proceeding so that `_activateSnippet` does not ignore snippets
    that could be in the process of being refreshed.
    
    Since asynchronous code is added to the `_onDragAndDropStop` event
    handler, a `snippet_edition_suspend` event is used to prevent
    concurrent operations until the whole handling is completed.
    
    Steps to reproduce:
    - Edit a website page.
    - Drop a Banner snippet.
    - Drop a Products snippet.
    - Drag the Products snippet above the Banner.
    => On drop, the Products snippet is not selected.
    
    task-3129065

--- addons/website/static/src/js/editor/snippets.options.js
+++ addons/website/static/src/js/editor/snippets.options.js
@@ -2309,7 +2309,7 @@ options.registry.Parallax = options.Class.extend({
      * @override
      */
     onMove() {
-        this._refreshPublicWidgets();
+        return this._refreshPublicWidgets();
     },
     /**
      * @override
