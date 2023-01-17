79d53e08d8a216d9728bc35a6e4ead5da4f10cef
Walid HANNICHE (waha) <waha@odoo.com>
2023-01-17 16:56:30 +0100

    [FIX] stock_account: SVL unit price precision
    Steps to reproduce:
    - Increase decimal accuracy of product price (eg: to 4)
    - Create a product with fifo automated valuation and price of 280.8475
    - Confirm a sale order of that product and Confirm stock picking
    - Create a return on the stock picking
    
    Bug:
    rounding error on the stock valuation
    
    Fix:
    do not round unit price on currency rounding
    
    opw-2988071

--- addons/stock_account/models/stock_move.py
+++ addons/stock_account/models/stock_move.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
-from odoo.tools import float_is_zero, OrderedSet
+from odoo.tools import float_is_zero, float_round, OrderedSet
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -48,7 +48,7 @@ class StockMove(models.Model):
             layers = self.origin_returned_move_id.sudo().stock_valuation_layer_ids
             layers |= layers.stock_valuation_layer_ids
             quantity = sum(layers.mapped("quantity"))
-            return layers.currency_id.round(sum(layers.mapped("value")) / quantity) if not float_is_zero(quantity, precision_rounding=layers.uom_id.rounding) else 0
+            return float_round(sum(layers.mapped("value")) / quantity, precision_digits=precision) if not float_is_zero(quantity, precision_rounding=layers.uom_id.rounding) else 0
         return price_unit if not float_is_zero(price_unit, precision) or self._should_force_price_unit() else self.product_id.standard_price
 
     @api.model
