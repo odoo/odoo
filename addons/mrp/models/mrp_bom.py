2085759b3bdfbea0ef9a9529ee6284ba4cfc71ea
niyasraphy <niyasraphyk@gmail.com>
2023-01-18 20:55:24 +0300

    [FIX] mrp: mrp.bom model product_qty field precision
    before this commit, the precision given for the field product_qty in mrp.bom model is Unit of Measure, which is not existing by default in Odoo decimal precisions.
    
    after this commit, precision will be updated to default Product Unit of Measure precision

--- addons/mrp/models/mrp_bom.py
+++ addons/mrp/models/mrp_bom.py
@@ -42,7 +42,7 @@ class MrpBom(models.Model):
     byproduct_ids = fields.One2many('mrp.bom.byproduct', 'bom_id', 'By-products', copy=True)
     product_qty = fields.Float(
         'Quantity', default=1.0,
-        digits='Unit of Measure', required=True)
+        digits='Product Unit of Measure', required=True)
     product_uom_id = fields.Many2one(
         'uom.uom', 'Unit of Measure',
         default=_get_default_product_uom_id, required=True,
