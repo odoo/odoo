7a6ad5ea92912a49f8cf3ba6e84be504c9ac0613
Nishant Jain (niai) <niai@odoo.com>
2023-01-18 15:07:30 +0530

    [MOV] l10n_in: move demo data to l10n_in_reports_gstr
    This demo data was made for the purpose of GST return report but now we have
    new functionality of GST return E-filing(l10n_in_reports_gstr) so moves demo
    data in that module so we don't need the demo data in this module.

--- addons/l10n_in/__manifest__.py
+++ addons/l10n_in/__manifest__.py
@@ -50,7 +50,6 @@ Sheet, now only Vertical format has been permitted Which is Supported By Odoo.
         'demo/demo_company.xml',
         'demo/res_partner_demo.xml',
         'demo/product_demo.xml',
-        'demo/account_payment_demo.xml',
         'demo/account_invoice_demo.xml',
     ],
     'license': 'LGPL-3',
