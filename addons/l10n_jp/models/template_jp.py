c50c943a6ebc714357c555f654e3b4d089599b0f
Tommy (tong) <tong@odoo.com>
2023-01-19 10:38:03 +0000

    [IMP] l10n_jp: update chart of account
    - Japan chart of account template updates
    - Translation updates
    - Use better code names for tax report
    
    task-3127047
    
    closes odoo/odoo#115175
    
    X-original-commit: 7a914a0e1d519767b3caeb8e38eeb2c930fce4fc
    Signed-off-by: Quentin De Paoli <qdp@odoo.com>

--- addons/l10n_jp/models/template_jp.py
+++ addons/l10n_jp/models/template_jp.py
@@ -9,13 +9,16 @@ class AccountChartTemplate(models.AbstractModel):
     @template('jp')
     def _get_jp_template_data(self):
         return {
-            'code_digits': '7',
-            'property_account_receivable_id': 'A11211',
-            'property_account_payable_id': 'A21211',
-            'property_account_expense_id': 'A21219',
-            'property_account_income_id': 'B41001',
-            'property_account_expense_categ_id': 'A21219',
-            'property_account_income_categ_id': 'B41001',
+            'code_digits': '6',
+            'property_account_receivable_id': 'l10n_jp_126000',
+            'property_account_payable_id': 'l10n_jp_220000',
+            'property_account_expense_id': 'l10n_jp_510000',
+            'property_account_income_id': 'l10n_jp_410000',
+            'property_account_expense_categ_id': 'l10n_jp_510000',
+            'property_account_income_categ_id': 'l10n_jp_410000',
+            'property_stock_valuation_account_id': 'l10n_jp_121100',
+            'property_stock_account_input_categ_id': 'l10n_jp_121200',
+            'property_stock_account_output_categ_id': 'l10n_jp_121300',
         }
 
     @template('jp', 'res.company')
@@ -23,11 +26,19 @@ class AccountChartTemplate(models.AbstractModel):
         return {
             self.env.company.id: {
                 'account_fiscal_country_id': 'base.jp',
-                'bank_account_code_prefix': 'A11102',
-                'cash_account_code_prefix': 'A11105',
-                'transfer_account_code_prefix': 'A11109',
-                'account_default_pos_receivable_account_id': 'A11213',
-                'income_currency_exchange_account_id': 'B61501',
-                'expense_currency_exchange_account_id': 'B62501',
+                'bank_account_code_prefix': '1202',
+                'cash_account_code_prefix': '1201',
+                'transfer_account_code_prefix': '1236',
+                'transfer_account_id': 'l10n_jp_123600',
+                'account_default_pos_receivable_account_id': 'l10n_jp_126200',
+                'income_currency_exchange_account_id': 'l10n_jp_425700',
+                'expense_currency_exchange_account_id': 'l10n_jp_513500',
+                'account_journal_suspense_account_id': 'l10n_jp_123900',
+                'account_journal_payment_credit_account_id': 'l10n_jp_123700',
+                'account_journal_payment_debit_account_id': 'l10n_jp_123800',
+                'default_cash_difference_expense_account_id': 'l10n_jp_510100',
+                'default_cash_difference_income_account_id': 'l10n_jp_999002',
+                'account_journal_early_pay_discount_loss_account_id': 'l10n_jp_510200',
+                'account_journal_early_pay_discount_gain_account_id': 'l10n_jp_425000',
             },
         }
