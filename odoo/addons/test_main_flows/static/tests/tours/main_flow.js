4d4f5e1f097533c1366773d3b341619839e531e3
Laurent Smet <las@odoo.com>
2023-01-20 13:15:44 +0100

    [FIX] account_accountant: Fix dashboard default bank rec action
    Open the kanban view by default when clicking on the journal name.
    
    Task: 3141127
    X-original-commit: 05553202e0a4a59600214fcbf965cfb3c29afbc9

--- odoo/addons/test_main_flows/static/tests/tours/main_flow.js
+++ odoo/addons/test_main_flows/static/tests/tours/main_flow.js
@@ -893,6 +893,11 @@ tour.stepUtils.mobileModifier(tour.stepUtils.autoExpandMoreButtons('.o_control_p
 }, {
     mobile: false,
     edition: "enterprise",
+    trigger: "button.o_switch_view.o_list",
+    content: _t("Move back to the list view"),
+}, {
+    mobile: false,
+    edition: "enterprise",
     trigger: "button.o_list_button_add",
     content: _t("Create a new bank transaction"),
 }, {
