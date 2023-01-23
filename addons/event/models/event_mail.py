ef503e62480c774e8a0d193666cc4cce5ab90282
miad-odoo <miad@odoo.com>
2023-01-23 12:12:33 +0000

    [FIX] event: make scheduler respect mail template
    Before this commit, the mail scheduler in event bypassed the email_from field
    in the selected template, implementing instead its own logic.
    
    With this commit, the scheduler respects the `email_from` field from the mail
    template, and only implements its own logic if that field is not set.
    
    Task-3092425
    
    closes odoo/odoo#110681
    
    X-original-commit: e01342a
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/event/models/event_mail.py
+++ addons/event/models/event_mail.py
@@ -276,11 +276,12 @@ class EventMailRegistration(models.Model):
                 author = company.partner_id
             elif self.env.user.email:
                 author = self.env.user
-            
+
             email_values = {
-                'email_from': author.email_formatted,
                 'author_id': author.id,
             }
+            if not reg_mail.scheduler_id.template_ref.email_from:
+                email_values['email_from'] = author.email_formatted
             reg_mail.scheduler_id.template_ref.send_mail(reg_mail.registration_id.id, email_values=email_values)
         todo.write({'mail_sent': True})
 
