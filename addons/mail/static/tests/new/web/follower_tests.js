cdc2c99de1d76652355a244059dd3fe22e7fae62
Didier (did) <did@odoo.com>
2023-01-23 11:53:12 +0100

    [IMP] mail: move already working test

--- addons/mail/static/tests/new/web/follower_tests.js
+++ addons/mail/static/tests/new/web/follower_tests.js
@@ -3,7 +3,7 @@
 import { makeDeferred } from "@mail/utils/deferred";
 import { click, start, startServer } from "@mail/../tests/helpers/test_utils";
 
-import { getFixture, patchWithCleanup } from "@web/../tests/helpers/utils";
+import { editInput, getFixture, patchWithCleanup } from "@web/../tests/helpers/utils";
 
 let target;
 QUnit.module("follower", {
@@ -172,4 +172,40 @@ QUnit.test("edit follower and close subtype dialog", async function (assert) {
 
     await click(".o-mail-follower-subtype-dialog-close");
     assert.containsNone(target, ".o-mail-follower-subtype-dialog");
+});
+
+QUnit.test("remove a follower in a dirty form view", async function (assert) {
+    const pyEnv = await startServer();
+    const [threadId, partnerId] = pyEnv["res.partner"].create([{}, {}]);
+    pyEnv["mail.followers"].create({
+        is_active: true,
+        partner_id: partnerId,
+        res_id: threadId,
+        res_model: "res.partner",
+    });
+    const { openFormView } = await start({
+        async mockRPC(route, args) {
+            if (args.method === "read") {
+                assert.step(`read ${args.args[0][0]}`);
+            }
+        },
+    });
+    await openFormView("res.partner", threadId);
+    assert.strictEqual(
+        target.querySelector(".o-mail-chatter-topbar-followers-count").innerText,
+        "1"
+    );
+    assert.verifySteps([`read ${threadId}`]);
+
+    await editInput(target, ".o_field_char[name=name] input", "some value");
+    await click(".o-mail-chatter-topbar-follower-list-button");
+    assert.containsOnce(target, ".o-mail-chatter-topbar-follower-list-follower");
+
+    await click(".o-mail-chatter-topbar-follower-list-follower-remove-button");
+    assert.strictEqual(
+        target.querySelector(".o-mail-chatter-topbar-followers-count").innerText,
+        "0"
+    );
+    assert.strictEqual(target.querySelector(".o_field_char[name=name] input").value, "some value");
+    assert.verifySteps([`read ${threadId}`]);
 });
