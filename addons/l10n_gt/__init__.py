796cd4335ec169b24be8de941e1aabef18444da7
Maximilien (malb) <malb@odoo.com>
2023-01-23 08:49:30 +0000

    [FIX] l10n_gt: translation
    All the localisation should be written in english and then translated to the native language of the country. In this case, the Guatemala localisation was written is Spanish, this PR translate the module in english and add the corresponding PO file in spanish.
    
    closes odoo/odoo#110661
    
    Task-id: 3145295
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_gt/__init__.py
+++ addons/l10n_gt/__init__.py
@@ -4,3 +4,8 @@
 # Copyright (c) 2009-2010 Soluciones Tecnologócias Prisma S.A. All Rights Reserved.
 # José Rodrigo Fernández Menegazzo, Soluciones Tecnologócias Prisma S.A.
 # (http://www.solucionesprisma.com)
+from odoo import api, SUPERUSER_ID
+
+def load_translations(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('l10n_gt.cuentas_plantilla').process_coa_translations()
