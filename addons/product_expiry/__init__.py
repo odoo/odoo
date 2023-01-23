23d6e5ef19653f43861d9f3f43e64afc04deff9e
svs-odoo <svs@odoo.com>
2023-01-23 15:01:37 +0000

    [IMP] product_expiry, stock: toggle tracking
    In the Inventory's Configuration, the option to enable expiration dates
    depends of the activation of lot and serial numbers.
    But if the user disables the tracking and the expirations dates were
    enabled, they still are.
    Also, if the module `product_expiry` is installed directly, the tracking
    numbers aren't enabled alongside.
    
    To fix that, this commit modifies the `res.config.settings` onchange
    `_onchange_group_stock_production_lot` to disable automatically
    expiration dates when tracking is disabled, and this commit also adds a
    hook in the `product_expiry` init to enable tracking numbers at the
    installation.
    
    Part-of: odoo/odoo#109511

--- addons/product_expiry/__init__.py
+++ addons/product_expiry/__init__.py
@@ -2,3 +2,13 @@
 
 from . import models
 from . import wizard
+
+from odoo import api, SUPERUSER_ID, Command
+
+def _enable_tracking_numbers(env):
+    """ This hook ensures the tracking numbers are enabled when the module is installed since the
+    user can install `product_expiry` manually without enable `group_production_lot`.
+    """
+    group_production_lot = env.ref('stock.group_production_lot')
+    groups = env.ref('base.group_user') + env.ref('base.group_portal')
+    groups.write({'implied_ids': [Command.link(group_production_lot.id)]})
