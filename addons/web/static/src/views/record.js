58005c7be30d0c4995cce66cc5a606a63fa9c48b
Lucas Perais <lpe@odoo.com>
2023-01-25 10:34:18 +0100

    [IMP] web: Record: improving Record and RelationalModel for usability
    The Record component becomes more and more used, but its API and the flows
    it can support are both a work in progress.
    
    Its specs become a little clearer, we probably want two things:
    - be able to display some values via Fields widget and interact with them, in
    particular relational fields.
    - fetch, display and interact with a proper record in the db.
    - disconnect it from the use of model in Views (via the hook useModel), because Record's
    feature are simpler.
    
    This commit aims at clarifying at least 2 aspects:
    - how to pass the values to the record, that is, without reading anything from the db
    => a `values` props contains server formatted data, it supports changing them via the props
    
    - when to reload the data from the db, when not to.
    => when props did not change, don't reload anything.
    
    We expect further improvements on this component, as the feature is still very basic at this point.
    
    See 475f8aa for earlier improvement

--- addons/web/static/src/views/record.js
+++ addons/web/static/src/views/record.js
@@ -1,7 +1,6 @@
 /** @odoo-module **/
 
-import { useService } from "@web/core/utils/hooks";
-import { useModel } from "@web/views/model";
+import { useBus, useService } from "@web/core/utils/hooks";
 import { RelationalModel } from "@web/views/relational_model";
 import { Component, xml, onWillStart, onWillUpdateProps } from "@odoo/owl";
 
@@ -9,21 +8,44 @@ const defaultActiveField = { attrs: {}, options: {}, domain: "[]", string: "" };
 
 class _Record extends Component {
     setup() {
-        this.model = useModel(RelationalModel, {
-            resId: this.props.info.resId,
-            resModel: this.props.info.resModel,
+        const resModel = this.props.info.resModel;
+        const modelParams = {
+            resModel,
             fields: this.props.fields,
             viewMode: "form",
             rootType: "record",
             activeFields: this.getActiveFields(),
-            mode: this.props.info.mode === "edit" ? "edit" : undefined,
-            initialValues: this.props.info.initialValues,
-        });
-        onWillUpdateProps(async (nextProps) => {
-            await this.model.load({
-                resId: nextProps.info.resId,
-                mode: nextProps.info.mode,
-            });
+        };
+        const modelServices = Object.fromEntries(
+            RelationalModel.services.map((servName) => {
+                return [servName, useService(servName)];
+            })
+        );
+        if (resModel) {
+            modelServices.orm = useService("orm");
+        }
+
+        this.model = new RelationalModel(this.env, modelParams, modelServices);
+        useBus(this.model, "update", () => this.render(true));
+
+        let loadKey;
+        const load = (props) => {
+            const loadParams = {
+                resId: props.info.resId,
+                mode: props.info.mode,
+                values: props.values,
+            };
+            const nextLoadKey = JSON.stringify(loadParams);
+            if (loadKey === nextLoadKey) {
+                return;
+            }
+            loadKey = nextLoadKey;
+            return this.model.load(loadParams);
+        };
+        onWillStart(() => load(this.props));
+        onWillUpdateProps((nextProps) => {
+            // don't wait because the keeplast may make willUpdateProps hang forever
+            load(nextProps);
         });
 
         if (this.props.info.onRecordChanged) {
@@ -54,7 +76,7 @@ class _Record extends Component {
     }
 }
 _Record.template = xml`<t t-slot="default" record="model.root"/>`;
-_Record.props = ["slots", "info", "fields"];
+_Record.props = ["slots", "info", "fields", "values?"];
 
 export class Record extends Component {
     setup() {
@@ -73,16 +95,16 @@ export class Record extends Component {
         }
     }
 }
-Record.template = xml`<_Record fields="fields" slots="props.slots" info="props" />`;
+Record.template = xml`<_Record fields="fields" slots="props.slots" values="props.values" info="props" />`;
 Record.components = { _Record };
 Record.props = [
     "slots",
-    "resModel",
+    "resModel?",
     "fieldNames?",
     "activeFields?",
     "fields?",
     "resId?",
     "mode?",
-    "initialValues?",
+    "values?",
     "onRecordChanged?",
 ];
