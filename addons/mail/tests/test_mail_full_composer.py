cf7edd4427dce3adfdcc6ff8ddcbad86982ded79
Sébastien Theys <seb@odoo.com>
2023-01-25 12:08:09 +0100

    [REF] mail: remove unused import

--- addons/mail/tests/test_mail_full_composer.py
+++ addons/mail/tests/test_mail_full_composer.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from unittest import skip
-
 from odoo.tests.common import tagged, HttpCase
 from odoo import Command
 
