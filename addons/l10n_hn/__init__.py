260c00fe61e691fecf7662191fee9a25680c7f18
Maximilien (malb) <malb@odoo.com>
2023-01-25 11:24:06 +0100

    [IMP] l10n_hn: translation
    Before this PR, all this localisation was written in spanish, but all the localisation have to be written in english and then translated back in the native language. This PR correct that.
    
    Task-id: 3149182&

--- addons/l10n_hn/__init__.py
+++ addons/l10n_hn/__init__.py
@@ -3,3 +3,8 @@
 
 # Copyright (c) 2009-2010 Salvatore J. Trimarchi <salvatore@trimarchi.co.cc>
 # (http://salvatoreweb.co.cc)
+from odoo import api, SUPERUSER_ID
+
+def load_translations(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('l10n_hn.cuentas_plantilla').process_coa_translations()
