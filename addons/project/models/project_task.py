adf9ca64088be51220b37ca78d550775caa1aa8c
Vincent Larcin <vila@odoo.com>
2023-01-25 13:18:03 +0000

    [IMP] project: add sudo in project.task write dict comprehension
    All write operations on the `project.task` model are slowed down significantly
    by a dict comprehension mapping tasks to their user_ids because of the access
    rules checks. By adding a sudo to that dict comprehension, this commit makes it
    bypass those checks and speeds it up.
    
    Task-3164004
    
    closes odoo/odoo#115037
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/models/project_task.py
+++ addons/project/models/project_task.py
@@ -1027,7 +1027,7 @@ class Task(models.Model):
             self = self.sudo()
 
         # Track user_ids to send assignment notifications
-        old_user_ids = {t: t.user_ids for t in self}
+        old_user_ids = {t: t.user_ids for t in self.sudo()}
 
         if "personal_stage_type_id" in vals and not vals['personal_stage_type_id']:
             del vals['personal_stage_type_id']
