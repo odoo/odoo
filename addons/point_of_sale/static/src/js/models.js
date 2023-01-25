6906c32626cb222246a895ba2ba5d15b3263935d
Walid HANNICHE (waha) <waha@odoo.com>
2023-01-25 10:57:28 +0000

    [FIX] stock: unit price rounding
    Steps to reproduce the issue:
    . Change the decimal accuracy of the product price to 0
    . Create a storable product and set the Vendor Tax
    . Create a purchase order with that item.
    . Set the unit price to have 0 decimal places. E.g: 20
    . Change the demand quantities to 10
    . Confirm the purchase order -> Traceback
    
    Bug:
    wrong key word arg used for _float_compare for this PR [1]
    
    Also this PR, remove the rounding on `total_void` in `_compute_all()` to
    ensure the price unit have the maximum precision. Usually, the price
    unit is rounded following the 'Product Price' accuracy rather than the
    currency one.
    
    opw-3136160
    [1]:https://github.com/odoo/odoo/pull/105080
    
    closes odoo/odoo#112553
    
    X-original-commit: 6bc08d296b0850a6c7517dc77fc28231a6a584b3
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/point_of_sale/static/src/js/models.js
+++ addons/point_of_sale/static/src/js/models.js
@@ -2233,7 +2233,6 @@ export class Orderline extends PosModel {
         return {
             priceWithTax: all_taxes.total_included,
             priceWithoutTax: all_taxes.total_excluded,
-            priceSumTaxVoid: all_taxes.total_void,
             priceWithTaxBeforeDiscount: all_taxes_before_discount.total_included,
             tax: taxtotal,
             taxDetails: taxdetail,
