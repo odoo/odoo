058b43ae33d06b10e94f50ba0b90a47d90b60c5b
Nicolas Bayet <nby@odoo.com>
2023-01-25 13:05:29 +0100

    [FIX] web_editor: make only one step when creating a link
    Before this commit, creating a link through the link tool made two
    steps instead of one.
    
    task-3147819
    
    X-original-commit: 918136e7792a9c02e951caf4436d253741a9a2f7

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1148,6 +1148,7 @@ const Wysiwyg = Widget.extend({
             }
         } else {
             const historyStepIndex = this.odooEditor.historySize() - 1;
+            this.odooEditor.historyPauseSteps();
             let { link } = Link.getOrCreateLink({
                 containerNode: this.odooEditor.editable,
                 startNode: options.link,
@@ -1172,6 +1173,7 @@ const Wysiwyg = Widget.extend({
                     data.rel = 'ugc';
                 }
                 linkWidget.applyLinkToDom(data);
+                this.odooEditor.historyUnpauseSteps();
                 this.odooEditor.historyStep();
                 link = linkWidget.$link[0];
                 this.odooEditor.setContenteditableLink(linkWidget.$link[0]);
@@ -1186,6 +1188,7 @@ const Wysiwyg = Widget.extend({
                 Promise.resolve().then(() => link.focus());
             });
             linkDialog.on('closed', this, function () {
+                this.odooEditor.historyUnpauseSteps();
                 // If the linkDialog content has been saved
                 // the previous selection in not relevant anymore.
                 if (linkDialog.destroyAction !== 'save') {
