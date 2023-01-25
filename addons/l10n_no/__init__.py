f89d70d3d2e17b130f035a46d5421806e50d6ba2
Maximilien (malb) <malb@odoo.com>
2023-01-25 14:48:19 +0000

    [IMP] l10n_no: translation
    Before this PR, all this localisation was written in Norwegian, but all the localisation have to be written in english and then translated back in the native language. This PR correct that.
    
    closes odoo/odoo#110986
    
    Task-id: 3149392
    Related: odoo/enterprise#36280
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_no/__init__.py
+++ addons/l10n_no/__init__.py
@@ -2,7 +2,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import models
+from odoo import api, SUPERUSER_ID
 
+def load_translations(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('l10n_no.no_chart_template').process_coa_translations()
 
 def _preserve_tag_on_taxes(cr, registry):
     from odoo.addons.account.models.chart_template import preserve_existing_tags_on_taxes
