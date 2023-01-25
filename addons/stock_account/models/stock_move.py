6cad68257789860202ca30368584ea0fa02762fa
nda-odoo <nda@odoo.com>
2023-01-25 21:02:17 +0100

    [FIX] stock_account: anglo-saxon accounting & storno
    First issue: post a credit note
    
    Steps to reproduce:
    - enable anglo-saxon accounting and storno
    - create a storable product with sale price = 50 and cost = 30
    - set costing method FIFO and automated inventory valuation on product category
    - create a credit note using this product
    - post credit note
    
    Error:
    
    There was a problem with the following move(s):
    - Move with id 18
    	The debit and credit should be negative and only one can be set
    
    Second issue: return a product
    
    In anglo-saxon accounting, returning a product means creating a reverse move.
    Reverse move in storno should have a negative debit/credit.
    
    opw-3110934

--- addons/stock_account/models/stock_move.py
+++ addons/stock_account/models/stock_move.py
@@ -445,6 +445,11 @@ class StockMove(models.Model):
 
     def _generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id, description):
         # This method returns a dictionary to provide an easy extension hook to modify the valuation lines (see purchase for an example)
+        def get_credit_debit_from_value(amount):
+            if self.env.context.get('is_returned') and self.env.company.account_storno:
+                return amount if amount < 0 else 0
+            return amount if amount > 0 else 0
+
         self.ensure_one()
         debit_line_vals = {
             'name': description,
@@ -453,8 +458,8 @@ class StockMove(models.Model):
             'product_uom_id': self.product_id.uom_id.id,
             'ref': description,
             'partner_id': partner_id,
-            'debit': debit_value if debit_value > 0 else 0,
-            'credit': -debit_value if debit_value < 0 else 0,
+            'debit': get_credit_debit_from_value(debit_value),
+            'credit': get_credit_debit_from_value(-debit_value),
             'account_id': debit_account_id,
         }
 
@@ -465,8 +470,8 @@ class StockMove(models.Model):
             'product_uom_id': self.product_id.uom_id.id,
             'ref': description,
             'partner_id': partner_id,
-            'credit': credit_value if credit_value > 0 else 0,
-            'debit': -credit_value if credit_value < 0 else 0,
+            'credit': get_credit_debit_from_value(credit_value),
+            'debit': get_credit_debit_from_value(-credit_value),
             'account_id': credit_account_id,
         }
 
@@ -488,8 +493,8 @@ class StockMove(models.Model):
                 'product_uom_id': self.product_id.uom_id.id,
                 'ref': description,
                 'partner_id': partner_id,
-                'credit': diff_amount > 0 and diff_amount or 0,
-                'debit': diff_amount < 0 and -diff_amount or 0,
+                'credit': get_credit_debit_from_value(diff_amount),
+                'debit': get_credit_debit_from_value(-diff_amount),
                 'account_id': price_diff_account.id,
             }
         return rslt
@@ -550,7 +555,7 @@ class StockMove(models.Model):
         # warehouse of the same company, the transit location belongs to this company, so we don't need to create accounting entries
         if self._is_in():
             if self._is_returned(valued_type='in'):
-                am_vals.append(self.with_company(company_to)._prepare_account_move_vals(acc_dest, acc_valuation, journal_id, qty, description, svl_id, cost))
+                am_vals.append(self.with_company(company_to).with_context(is_returned=True)._prepare_account_move_vals(acc_dest, acc_valuation, journal_id, qty, description, svl_id, cost))
             else:
                 am_vals.append(self.with_company(company_to)._prepare_account_move_vals(acc_src, acc_valuation, journal_id, qty, description, svl_id, cost))
 
@@ -558,7 +563,7 @@ class StockMove(models.Model):
         if self._is_out():
             cost = -1 * cost
             if self._is_returned(valued_type='out'):
-                am_vals.append(self.with_company(company_from)._prepare_account_move_vals(acc_valuation, acc_src, journal_id, qty, description, svl_id, cost))
+                am_vals.append(self.with_company(company_from).with_context(is_returned=True)._prepare_account_move_vals(acc_valuation, acc_src, journal_id, qty, description, svl_id, cost))
             else:
                 am_vals.append(self.with_company(company_from)._prepare_account_move_vals(acc_valuation, acc_dest, journal_id, qty, description, svl_id, cost))
 
@@ -572,7 +577,7 @@ class StockMove(models.Model):
                     am_vals.append(self.with_company(self.company_id)._prepare_account_move_vals(acc_valuation, acc_dest, journal_id, qty, description, svl_id, cost))
             elif self._is_dropshipped_returned():
                 if cost > 0:
-                    am_vals.append(self.with_company(self.company_id)._prepare_account_move_vals(acc_valuation, acc_src, journal_id, qty, description, svl_id, cost))
+                    am_vals.append(self.with_company(self.company_id).with_context(is_returned=True)._prepare_account_move_vals(acc_valuation, acc_src, journal_id, qty, description, svl_id, cost))
                 else:
                     cost = -1 * cost
                     am_vals.append(self.with_company(self.company_id)._prepare_account_move_vals(acc_dest, acc_valuation, journal_id, qty, description, svl_id, cost))
