e68e0b71ab6dbf054ef00545bcb212c6ba39f0cf
Antoine Guenet <age@odoo.com>
2023-01-26 10:04:23 +0100

    [FIX] web_editor: implement unlink without relying on execCommand
    Before this commit when a button containing a text then an icon was
    unlinked in Chrome, all further buttons were also unlinked (see steps
    below).
    
    It turns out this bug is due to the browser's `execCommand`
    implementation so this commit reimplements the feature without relying
    on it.
    
    Steps to reproduce (in website module):
    
    * drop Text+Image snippet twice
    * inside first button type /image and insert a pictogram after the text
    * click on the text of the first button => the link popover appears
    * click on the unlink button of the popover
    => both buttons become flat texts in Chrome
    => nothing happens visually in Firefox, but the <a> actually gets
    turned into a <span>
    
    task-2778912
    
    X-original-commit: ca3335b5d184c378fde1d6b608962cee93d221ec
    Co-authored-by: Antoine Guenet <age@odoo.com>
    Co-authored-by: Benoît Socias <bso@odoo.com>

--- addons/web_editor/static/lib/odoo-editor/src/commands/commands.js
+++ addons/web_editor/static/lib/odoo-editor/src/commands/commands.js
@@ -41,6 +41,8 @@ import {
     getDeepestPosition,
     fillEmpty,
     isEmptyBlock,
+    unwrapContents,
+    getCursorDirection,
 } from '../utils/utils.js';
 
 const TEXT_CLASSES_REGEX = /\btext-[^\s]*\b/g;
@@ -463,25 +465,37 @@ export const editorCommands = {
     },
     unlink: editor => {
         const sel = editor.document.getSelection();
-        // we need to remove the contentEditable isolation of links
-        // before we apply the unlink, otherwise the command is not performed
-        // because the content editable root is the link
-        const closestEl = closestElement(sel.focusNode, 'a');
-        if(closestEl) {
-            closestEl.removeAttribute('class'); // To prevent firefox from adding unnecessary <span>.
+        const isCollapsed = sel.isCollapsed;
+        // If the selection is collapsed, unlink the whole link:
+        // `<a>a[]b</a>` => `a[]b`.
+        getDeepRange(editor.editable, { sel, splitText: true, select: true });
+        if (!isCollapsed) {
+            // If not, unlink only the part(s) of the link(s) that are selected:
+            // `<a>a[b</a>c<a>d</a>e<a>f]g</a>` => `<a>a</a>[bcdef]<a>g</a>`.
+            let { anchorNode, focusNode, anchorOffset, focusOffset } = sel;
+            const direction = getCursorDirection(anchorNode, anchorOffset, focusNode, focusOffset);
+            // Split the links around the selection.
+            const [startLink, endLink] = [closestElement(anchorNode, 'a'), closestElement(focusNode, 'a')];
+            if (startLink) {
+                anchorNode = splitAroundUntil(anchorNode, startLink);
+                anchorOffset = direction === DIRECTIONS.RIGHT ? 0 : nodeSize(anchorNode);
+                setSelection(anchorNode, anchorOffset, focusNode, focusOffset, true);
+            }
+            // Only split the end link if it was not already done above.
+            if (endLink && endLink.isConnected) {
+                focusNode = splitAroundUntil(focusNode, endLink);
+                focusOffset = direction === DIRECTIONS.RIGHT ? nodeSize(focusNode) : 0;
+                setSelection(anchorNode, anchorOffset, focusNode, focusOffset, true);
+            }
         }
-        if (closestEl && closestEl.getAttribute('contenteditable') === 'true') {
-            editor._activateContenteditable();
-        }
-        if (sel.isCollapsed) {
+        const targetedNodes = isCollapsed ? [sel.anchorNode] : getSelectedNodes(editor.editable);
+        const links = new Set(targetedNodes.map(node => closestElement(node, 'a')).filter(a => a));
+        if (links.size) {
             const cr = preserveCursor(editor.document);
-            const node = closestElement(sel.focusNode, 'a');
-            setSelection(node, 0, node, node.childNodes.length, false);
-            editor.document.execCommand('unlink');
+            for (const link of links) {
+                unwrapContents(link);
+            }
             cr();
-        } else {
-            editor.document.execCommand('unlink');
-            setSelection(sel.anchorNode, sel.anchorOffset, sel.focusNode, sel.focusOffset);
         }
     },
 
