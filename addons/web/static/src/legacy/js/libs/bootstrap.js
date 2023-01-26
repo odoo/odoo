1199c78ce0c408b0f02fde9eaf37285dfcf77229
Benjamin Vray <bvr@odoo.com>
2023-01-26 08:27:36 +0000

    [FIX] web, website, web_editor: fix popup scrollbar
    Steps to reproduce the bug:
    
    - Drag and drop several snippets onto a web page in edit mode to make
    the vertical scrollbar appear.
    - Drag and drop a popup onto the page.
    - Drag and drop snippets into the popup so that the height of the popup
    exceeds the height of the viewport.
    - Try to scroll the popup downwards by clicking and dragging on the
    scrollbar (not using the mouse wheel).
    - Bug: the page is scrolled instead of the popup.
    
    Note: This bug occurs in Chrome (not in Firefox).
    
    This issue occurs because two scrollbars are present at the same
    location (one for the page and one for the popup) and they overlap each
    other. Normally, Bootstrap removes the scrollbar from the body when a
    modal is opened, and this behavior was adapted for the #wrapwrap element
    with this commit [1]. However, when transitioning to Bootstrap 5, the
    code that overrides Bootstrap was removed instead of being adapted (this
    was done in this commit [2]).
    
    In later commits [3] and [4], we continued to remove code that updated
    the scrollbar based on the opening of a modal because this code was
    causing errors due to the fact that it was incomplete without the part
    removed by commit [2].
    
    This commit restores the original behavior (before the deletions made by
    the aforementioned commits) by restoring the missing code and properly
    adapting it to Bootstrap 5.
    
    [1]: https://github.com/odoo/odoo/commit/9cf8b97fe40444b44ebb6e6fb992bc658a087d32
    [2]: https://github.com/odoo/odoo/commit/0b94da214b7017e8580e671cbaa68ade6de2fbc7
    [3]: https://github.com/odoo/odoo/commit/cb7cf77ed080e86f800af61c9a3dc4ad36ad6cfb
    [4]: https://github.com/odoo/odoo/commit/51939d09f84579f61f1ff77aacd754beba036dc2
    
    task-3102275
    
    closes odoo/odoo#112091
    
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/web/static/src/legacy/js/libs/bootstrap.js
+++ addons/web/static/src/legacy/js/libs/bootstrap.js
@@ -148,3 +148,27 @@ ScrollSpy.prototype._activate = function (target) {
     }
     bootstrapSpyActivateFunction.apply(this, arguments);
 };
+
+/* Bootstrap modal scrollbar compensation on non-body */
+const bsAdjustDialogFunction = Modal.prototype._adjustDialog;
+Modal.prototype._adjustDialog = function () {
+    const document = this._element.ownerDocument;
+    document.body.classList.remove('modal-open');
+    const $scrollable = $().getScrollingElement(document);
+    if (document.body.contains($scrollable[0])) {
+        $scrollable.compensateScrollbar(true);
+    }
+    document.body.classList.add('modal-open');
+    return bsAdjustDialogFunction.apply(this, arguments);
+};
+
+const bsResetAdjustmentsFunction = Modal.prototype._resetAdjustments;
+Modal.prototype._resetAdjustments = function () {
+    const document = this._element.ownerDocument;
+    document.body.classList.remove('modal-open');
+    const $scrollable = $().getScrollingElement(document);
+    if (document.body.contains($scrollable[0])) {
+        $scrollable.compensateScrollbar(false);
+    }
+    return bsResetAdjustmentsFunction.apply(this, arguments);
+};
