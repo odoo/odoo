0702ef87c1d8517f03a050bdfc7126ec63e7d709
bat-odoo <bat@odoo.com>
2023-01-27 12:08:50 +0530

    [ADD] l10n_co_pos_report: extend PoS Sales Details Report for Colombia

---
+++ addons/l10n_co_pos_report/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    "name": "Colombian - Point of Sale Details",
+    "version": "1.0",
+    "description": """
+Colombian - Point of Sale
+=========================
+
+    Extend POS Sales Details Report for Colombia
+""",
+    "category": "Localization",
+    "auto_install": True,
+    "depends": [
+        "l10n_co_pos"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/pos_details_view.xml",
+        "views/pos_config_views.xml",
+        "views/templates.xml",
+    ],
+}
