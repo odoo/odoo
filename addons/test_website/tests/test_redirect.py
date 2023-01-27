e6e1814996557e16de0af8f2c0fbced102a8dad0
Christophe Monniez <moc@odoo.com>
2023-01-27 16:35:22 +0100

    [FIX] test_website: remove base_url from assertions
    Since Werkzeug 2.1.0, the Response.autocorrect_location_header is
    disabled by default.
    
    As it's RFC compliant and supported by browsers, the base_url is simply
    removed from the assertions.
    
    backport of 3c08b79198d31da29406a33908951fed7b626c17

--- addons/test_website/tests/test_redirect.py
+++ addons/test_website/tests/test_redirect.py
@@ -7,6 +7,7 @@ from odoo.tools import mute_logger
 from odoo.addons.http_routing.models.ir_http import slug
 
 from unittest.mock import patch
+from urllib.parse import urlparse
 
 
 @tagged('-at_install', 'post_install')
@@ -83,7 +84,7 @@ class TestRedirect(HttpCase):
             # published
             resp = self.url_open("/test_website/200/name-%s" % rec_published.id, allow_redirects=False)
             self.assertEqual(resp.status_code, 308)
-            self.assertEqual(resp.headers.get('Location'), self.base_url + "/test_website/308/name-%s" % rec_published.id)
+            self.assertEqual(urlparse(resp.headers.get('Location', '')).path, f"/test_website/308/name-{rec_published.id}")
 
             resp = self.url_open("/test_website/308/name-%s" % rec_published.id, allow_redirects=False)
             self.assertEqual(resp.status_code, 200)
@@ -94,7 +95,7 @@ class TestRedirect(HttpCase):
 
             resp = self.url_open("/test_website/308/xx-%s" % rec_published.id, allow_redirects=False)
             self.assertEqual(resp.status_code, 301)
-            self.assertEqual(resp.headers.get('Location'), self.base_url + "/test_website/308/name-%s" % rec_published.id)
+            self.assertEqual(urlparse(resp.headers.get('Location'), '').path, f"/test_website/308/name-{rec_published.id}")
 
             resp = self.url_open("/test_website/200/xx-%s" % rec_published.id, allow_redirects=True)
             self.assertEqual(resp.status_code, 200)
