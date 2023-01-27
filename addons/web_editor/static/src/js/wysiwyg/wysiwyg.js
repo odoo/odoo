7b1b3ab4d7dde7c1264ffd633d5874f3812e4ad8
xO-Tx <mou@odoo.com>
2023-01-27 10:37:28 +0000

    [FIX] web_editor: prevent unwanted actions on save
    Starting from [1], a `willDestroyEditors` attribute was added to prevent
    race conditions caused by an update on options visibility during the
    "save" (after checking the code, it seems that the issue here was caused
    by the `hide.bs.modal.SnippetPopup` event which triggers the
    `_onSnippetOptionVisibilityUpdate()` handler on `snippetMenu`, leading
    to a traceback if editors are destroyed).
    So with this fix, the visibility update will be ignored during the save
    process.
    
    [A] Note that `willDestroyEditors` is set to `true` on `cleanForSave()`
    and never set to `false` after saving the content or destroying the
    editors.
    
    Another fix was added in [2] to prevent the click (before the preview
    is blocked) on the page while saving, and uses the `willDestroyEditors`
    attribute to ignore the click.
    
    Now since `cleanForSave()` is called on `MassMailingHtmlField` >
    `commitChanges()` and because of [A], the click will be always ignored
    on this field's content.
    
    The goal of this commit is to fix this behavior using a global
    `savingContent` indicator which will be used to handle both cases
    (a click on page & options visibility update) during the save.
    It will be only set to `true`  during the content save.
    
    Note: the forward-port of [1] actually took too long to reach master.
    Actually [2] already reached master, having no effect as relying on [1].
    This commit here will actually be merged first (preventing [2] to create
    the mass mailing bug when [1] reaches master). [1]'s forward-ported
    version will actually be the commit following this one.
    
    [1]: https://github.com/odoo/odoo/commit/47cdb15082f774da0aa07e289361445ddc3a48f1
    [2]: https://github.com/odoo/odoo/commit/6bf3823c2996af4ec0534ff69471887ac43a8ddf
    
    X-original-commit: b2b8590d19e07659c6ad7c3e3afba0a848b414a5
    Part-of: odoo/odoo#107387

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -886,16 +886,27 @@ const Wysiwyg = Widget.extend({
      * @returns {Promise}
      */
     saveContent: async function (reload = true) {
+        this.savingContent = true;
         await this.cleanForSave();
 
         const editables = this.options.getContentEditableAreas();
         await this.savePendingImages(editables.length ? $(editables) : this.$editable);
         await this._saveViewBlocks();
+        this.savingContent = false;
 
         window.removeEventListener('beforeunload', this._onBeforeUnload);
         if (reload) {
             window.location.reload();
         }
+    },
+    /**
+     * Checks if the Wysiwyg is currently saving content. It can be used to
+     * prevent some unwanted actions during save.
+     *
+     * @returns {Boolean}
+     */
+    isSaving() {
+        return !!this.savingContent;
     },
     /**
      * Asks the user if he really wants to discard its changes (if there are
