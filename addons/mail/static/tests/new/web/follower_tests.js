c8895f62f0c23a4776adf04402eb88383011c009
Alexandre Kühn <aku@odoo.com>
2023-01-27 16:17:03 +0100

    [FIX] mail:tests: unskip 'click on edit follower'

--- addons/mail/static/tests/new/web/follower_tests.js
+++ addons/mail/static/tests/new/web/follower_tests.js
@@ -101,7 +101,7 @@ QUnit.test("click on partner follower details", async function (assert) {
     assert.verifySteps(["do_action"], "redirect to partner profile");
 });
 
-QUnit.skipRefactoring("click on edit follower", async function (assert) {
+QUnit.test("click on edit follower", async function (assert) {
     const pyEnv = await startServer();
     const [threadId, partnerId] = pyEnv["res.partner"].create([{}, {}]);
     pyEnv["mail.followers"].create({
@@ -110,23 +110,13 @@ QUnit.skipRefactoring("click on edit follower", async function (assert) {
         res_id: threadId,
         res_model: "res.partner",
     });
-    const { messaging, openView } = await start({
+    const { openView } = await start({
         async mockRPC(route, args) {
             if (route.includes("/mail/read_subscription_data")) {
                 assert.step("fetch_subtypes");
             }
         },
     });
-    await openView({
-        res_id: threadId,
-        res_model: "res.partner",
-        views: [[false, "form"]],
-    });
-    const thread = messaging.models["Thread"].insert({
-        id: threadId,
-        model: "res.partner",
-    });
-    await thread.fetchData(["followers"]);
     await openView({
         res_id: threadId,
         res_model: "res.partner",
