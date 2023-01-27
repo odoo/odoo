847b763b2929ff7d5b9e75cd849eb5d638c23744
xO-Tx <mou@odoo.com>
2023-01-27 11:37:28 +0100

    [FIX] web_editor: prevent unwanted actions on save
    Starting from [1], a `willDestroyEditors` attribute was added to prevent
    race conditions caused by an update on options visibility during the
    "save" (after checking the code, it seems that the issue here was caused
    by the `hide.bs.modal.SnippetPopup` event which triggers the
    `_onSnippetOptionVisibilityUpdate()` handler on `snippetMenu`, leading
    to a traceback if editors are destroyed).
    So with this fix, the visibility update will be ignored during the save
    process.
    
    [A] Note that `willDestroyEditors` is set to `true` on `cleanForSave()`
    and never set to `false` after saving the content or destroying the
    editors.
    
    Another fix was added in [2] to prevent the click (before the preview
    is blocked) on the page while saving, and uses the `willDestroyEditors`
    attribute to ignore the click.
    
    Now since `cleanForSave()` is called on `MassMailingHtmlField` >
    `commitChanges()` and because of [A], the click will be always ignored
    on this field's content.
    
    The goal of this commit is to fix this behavior using a global
    `savingContent` indicator which will be used to handle both cases
    (a click on page & options visibility update) during the save.
    It will be only set to `true`  during the content save.
    
    [1]: https://github.com/odoo/odoo/commit/47cdb15082f774da0aa07e289361445ddc3a48f1
    [2]: https://github.com/odoo/odoo/commit/6bf3823c2996af4ec0534ff69471887ac43a8ddf
    
    X-original-commit: b2b8590d19e07659c6ad7c3e3afba0a848b414a5

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -313,7 +313,6 @@ var SnippetEditor = Widget.extend({
         if (this.isDestroyed()) {
             return;
         }
-        this.willDestroyEditors = true;
         await this.toggleTargetVisibility(!this.$target.hasClass('o_snippet_invisible')
             && !this.$target.hasClass('o_snippet_mobile_invisible')
             && !this.$target.hasClass('o_snippet_desktop_invisible'));
@@ -1530,7 +1529,7 @@ var SnippetEditor = Widget.extend({
      * @param {OdooEvent} ev
      */
     _onSnippetOptionVisibilityUpdate: function (ev) {
-        if (this.willDestroyEditors) {
+        if (this.options.wysiwyg.isSaving()) {
             // Do not update the option visibilities if we are destroying them.
             return;
         }
@@ -2047,7 +2046,6 @@ var SnippetsMenu = Widget.extend({
         // when hidden, destroying the widget hides it.)
         await this._mutex.getUnlockedDef();
 
-        this.willDestroyEditors = true;
         // Then destroy all snippet editors, making them call their own
         // "clean for save" methods (and options ones).
         await this._destroyEditors();
@@ -3447,7 +3445,7 @@ var SnippetsMenu = Widget.extend({
      */
     _onClick(ev) {
         // Clicking in the page should be ignored on save
-        if (this.willDestroyEditors) {
+        if (this.options.wysiwyg.isSaving()) {
             return;
         }
 
@@ -3983,7 +3981,7 @@ var SnippetsMenu = Widget.extend({
      * @param {OdooEvent} ev
      */
     _onSnippetOptionVisibilityUpdate: async function (ev) {
-        if (this.willDestroyEditors) {
+        if (this.options.wysiwyg.isSaving()) {
             // Do not update the option visibilities if we are destroying them.
             return;
         }
