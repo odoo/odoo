abfcb88e8fd699ad77cb6da886c4760bfecea956
Nicolas Bayet <nby@odoo.com>
2023-01-31 12:49:42 +0100

    [FIX] web_editor: remove focus listener on resetEditor
    When resetting the editor, we need to remove the focus listener if one
    was previously added. Otherwise, the editor will try to connect too
    much for each resetEditor call.
    
    task-3164032
    
    X-original-commit: ed73015ee8dc53692026d3bc040ece4226e2c117

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2538,6 +2538,7 @@ const Wysiwyg = Widget.extend({
         }
     },
     resetEditor: function (value, options) {
+        this.$editable[0].removeEventListener('focus', this._joinPeerToPeer);
         if (options) {
             this.options = this._getEditorOptions(options);
         }
