b1e5adaff7dffc0e5fd12c79c71a5466ec64d17f
Géry Debongnie <ged@odoo.com>
2023-02-01 09:46:33 +0000

    [IMP] web_editor: only load ice server configuration once
    Before this commit, the web editor loaded the configuration for ice
    servers each time it was created.  It is useless to do it more than
    once, so this commit simply keep the initial value around.
    
    Part-of: odoo/odoo#111571

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -63,6 +63,9 @@ const PTP_CLIENT_DISCONNECTED_STATES = [
     'disconnected',
 ];
 
+// this is a local cache for ice server descriptions
+let ICE_SERVERS = null;
+
 const Wysiwyg = Widget.extend({
     defaultOptions: {
         lang: 'odoo',
@@ -594,7 +597,10 @@ const Wysiwyg = Widget.extend({
         }, CHECK_OFFLINE_TIME);
 
         this._peerToPeerLoading = new Promise(async (resolve) => {
-            let iceServers = await this._rpc({route: '/web_editor/get_ice_servers'});
+            if (!ICE_SERVERS) {
+                ICE_SERVERS = await this._rpc({route: '/web_editor/get_ice_servers'});
+            }
+            let iceServers = structuredClone(ICE_SERVERS);
             if (!iceServers.length) {
                 iceServers = [
                     {
