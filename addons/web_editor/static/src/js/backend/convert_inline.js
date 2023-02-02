5041043419eca33244fc578e535567a80e975103
Antoine Guenet <age@odoo.com>
2023-02-02 13:37:36 +0000

    [FIX] web_editor: properly convert nested lists
    Nested lists should not have a bullet point in front of them, but the
    current conversion of HTML for emails made them reappear.
    
    opw-3136324
    task-3166100
    
    closes odoo/odoo#111822
    
    X-original-commit: 11f44de490d7e9c59b76aaade1d31b3041c9fb3b
    Signed-off-by: Antoine Guenet (age) <age@odoo.com>
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/backend/convert_inline.js
+++ addons/web_editor/static/src/js/backend/convert_inline.js
@@ -1235,7 +1235,7 @@ function _getMatchedCSSRules(node, cssRules) {
         }
     };
 
-    if (processedStyle.display === 'block') {
+    if (processedStyle.display === 'block' && !(node.classList && node.classList.contains('oe-nested'))) {
         delete processedStyle.display;
     }
     if (!processedStyle['box-sizing']) {
