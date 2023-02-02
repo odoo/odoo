4fa3f44952254ec36889184e4a8b4664217ec37a
Valentin Vallaeys (vava) <vava@odoo.com>
2023-02-02 14:05:14 +0000

    [CLN] delivery: alphabetical order and guidelines for imports
    Part-of: odoo/odoo#111748

--- addons/delivery/models/sale_order.py
+++ addons/delivery/models/sale_order.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
--- addons/delivery/wizard/choose_delivery_carrier.py
+++ addons/delivery/wizard/choose_delivery_carrier.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
