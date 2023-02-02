e83ca38b10df9e94f355050483a6c21fdd3836cc
Louis (loco) <loco@odoo.com>
2023-02-02 12:26:52 +0000

    [FIX] web_editor, website: correctly remove the image gallery snippet
    Steps to reproduce the bug:
    - Add an Image Gallery (IG) snippet on the page.
    - Click on "Remove all" to remove all the images of the IG snippet.
    - Add 2 new images in the IG.
    - Click on the first image of the IG to load its data.
    - Click on the trash button to remove the snippet.
    - Bug => The snippet is not removed (an image is removed instead).
    
    When a snippet is removed, the `removeSnippet` function is called. The
    problem is that the `call_for_each_child_snippet` will never resolve.
    Two mechanisms are of interest to understand why: the first one is the
    `updateCurrentSnippetEditorOverlay` function. Its goal is to destroy a
    snippet each time its target is not in the DOM anymore. The second
    mechanism is specific to the IG snippet: when an image of this snippet
    is destroyed, the `slideshow` function goes through the remaining
    images to update parameters. To do it, the function uses the
    `_replaceContent` function that empties the content of the carousel and
    then fills it with new data.
    
    When a snippet is removed, a `SnippetEditor` is created for each
    element of it. In the case of the IG, a `SnippetEditor` is created for
    each image of the the snippet. Because the first image already has a
    `SnippetEditor` (because it has been clicked), the callback of
    `call_for_each_child_snippet` is called to remove this image from the
    IG snippet. The second mechanism explained before will then be called.
    Meanwhile, a `SnippetEditor` will be created for the second image.
    However, because the `_replaceContent` function emptied the content of
    the carousel, the `updateCurrentSnippetEditorOverlay` function will
    destroy the `SnippetEditor` of the second image as its target is not
    considered present in the DOM anymore. Unfortunately, the
    `call_for_each_child_snippet` still needed this `SnippetEditor` and
    will never entirely resolve.
    
    To solve this problem, the `removeSnippet` function is executed inside
    a mutex. Because the mutex is also used by
    `updateCurrentSnippetEditorOverlay`, we are sure that this function
    will not destroy the snippetEditor while the `removeSnippet` is still
    running.
    
    task-3147271
    
    X-original-commit: 135cf541d80f22130142f20869788fefb9690f9f

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -1152,7 +1152,7 @@ var SnippetEditor = Widget.extend({
     _onRemoveClick: function (ev) {
         ev.preventDefault();
         ev.stopPropagation();
-        this.removeSnippet();
+        this.trigger_up('snippet_edition_request', {exec: this.removeSnippet.bind(this)});
     },
     /**
      * @private
