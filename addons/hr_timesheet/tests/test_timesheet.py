b4a900f136abe8630a2ad20a2b37cc49c5b310ea
Maruan Aguerdouh (magm) <magm@odoo.com>
2023-02-03 13:26:13 +0000

    [FIX] hr_timesheet: fix timesheet computation between hours and days
    Steps to reproduce:
    
    - Install Project, Timesheets.
    - Create a new porject with allocated hours set to 40 hours (5 days).
    - Taking into account that our company have a 8-hour/day work schedule
    (40 hours/week).
    - We go then to the timesheet app and we add a line for the project
    created with 8 hours as hours spent.
    - Now we got to the timesheet configuration and we change the uom to
    days.
    - We go back to the timesheet app and we see the result, we can also
    try to add just 1 day.
    
    Issue:
    
    The days are not properly computed, only when we are in hours are being
    computed properly. But when we change the uom to days, the computation
    that we see in the list will still be wrong.
    
    Solution:
    
    We really take into account which uom are we working with so we
    compute the time properly depending on the uom.
    
    opw-3138053
    
    closes odoo/odoo#115039
    
    X-original-commit: a31a1b6aff4db0415dc9c44bae33901f4833aba1
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/hr_timesheet/tests/test_timesheet.py
+++ addons/hr_timesheet/tests/test_timesheet.py
@@ -549,3 +549,27 @@ class TestTimesheet(TestCommonTimesheet):
             ('hr_timesheet.view_task_project_user_graph_inherited', '//field[@name="planned_hours"]', [None, 'Initially Planned Days']),
             ('hr_timesheet.timesheets_analysis_report_pivot_employee', '//field[@name="unit_amount"]', [None, 'Days Spent']),
         ])
+
+    def test_uom_change_timesheet(self):
+        """
+        We check that we don't over transform the timesheet unit amount when changing
+        the company encoding timesheet uom, we keep it in the project as hours.
+        So it will be transformed only once when encoding the timesheet.
+        """
+        Timesheet = self.env['account.analytic.line']
+        project = self.env['project.project'].create({
+            'name': 'Project',
+            'allow_timesheets': True,
+            'partner_id': self.partner.id,
+        })
+        project.allocated_hours = 40.0
+
+        Timesheet.create({
+            'name': 'FirstTimeSheet',
+            'project_id': project.id,
+            'unit_amount': 8,
+            'employee_id': self.empl_employee2.id
+        })
+        self.env.company.timesheet_encode_uom_id = self.env.ref('uom.product_uom_day')
+        self.assertEqual(project.total_timesheet_time, 8, "Total timesheet time should be 8 hours")
+        self.assertEqual(project.timesheet_encode_uom_id.name, 'Days', "Timesheet encode uom should be 'Days'")
