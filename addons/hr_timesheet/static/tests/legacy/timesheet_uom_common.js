49297bc7bba75541ca139e6c84e0d3b311765145
Aaron Bohy <aab@odoo.com>
2023-02-03 17:19:50 +0000

    [REM] *: remove legacy basic views + some fields/widgets
    This commit removes the legacy implementation of the form, kanban
    and list views. It also removes the legacy view widget registry,
    and all legacy widgets it contained. The legacy field registry
    couldn't be removed yet as some fields are still used (e.g. in
    client actions: FieldMany2One, FieldMany2ManyTags...), and
    sometimes accessed from that registry (e.g. uom service). More
    clean up will come later. Note that all tests using legacy views
    have thus been removed, even though the tested feature might still
    remain (e.g. FieldMany2One tests have been removed, but that field
    is still there). However, those features are deprecated and
    unlikely to evolve. They should be removed in the next saas, or the
    one after.
    
    Finally, this commit also removes the legacy view dialogs.
    
    Task 3168640
    
    Part-of: odoo/odoo#111809

--- addons/hr_timesheet/static/tests/legacy/timesheet_uom_common.js
+++ addons/hr_timesheet/static/tests/legacy/timesheet_uom_common.js
@@ -3,7 +3,6 @@ odoo.define("hr_timesheet.timesheet_uom_tests_env", function (require) {
 
 const session = require('web.session');
 const { createView } = require("web.test_utils");
-const ListView = require('web.ListView');
 const { timesheetUomService } = require('hr_timesheet.timesheet_uom');
 const { makeTestEnv } = require("@web/../tests/helpers/mock_env");
 const { registry } = require("@web/core/registry");
@@ -178,13 +177,8 @@ function SetupTimesheetUOMWidgetsTestEnvironment () {
         await this.patchSessionAndStartServices(sessionToApply);
         return await createView(Object.assign(
             {
-                View: ListView,
                 data: this.data,
                 model: 'account.analytic.line',
-                arch: `
-                    <tree>
-                        <field name="unit_amount" widget="timesheet_uom"/>
-                    </tree>`,
             },
             options || { },
         ));
