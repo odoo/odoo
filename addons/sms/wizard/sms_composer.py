a696a83e984487f2524570d74a38045ae1df09a9
Eteil Djoumatchoua (etdj) <etdj@odoo.com>
2023-02-06 10:31:59 +0100

    [FIX] sms: Don't get error when sending sms on sale order
    Steps:
    - Go to Sales
    -  Open a sale order
    - Click on Action
    - Send SMS
    - Fill phone number and message
    - Send SMS
    
    Issue:
    Traceback
    
    Cause:
    Sale order doesn't have a phone or mobile field so the sms.composer tries to write on it use "False"
    
    Back-port of https://github.com/odoo/odoo/commit/70a9995c9e02f36dc15d2723151ef0a205ebc12b
    Already approved by @Thiryr
    
    opw-3162761

--- addons/sms/wizard/sms_composer.py
+++ addons/sms/wizard/sms_composer.py
@@ -230,7 +230,9 @@ class SendSMS(models.TransientModel):
         # on the numbers in the database.
         records = records if records is not None else self._get_records()
         records.ensure_one()
-        if self.recipient_single_number_itf and self.recipient_single_number_itf != self.recipient_single_number:
+        if not self.number_field_name:
+            self.numbers = self.recipient_single_number_itf or self.recipient_single_number
+        elif self.recipient_single_number_itf and self.recipient_single_number_itf != self.recipient_single_number:
             records.write({self.number_field_name: self.recipient_single_number_itf})
         return self._action_send_sms_comment(records=records)
 
