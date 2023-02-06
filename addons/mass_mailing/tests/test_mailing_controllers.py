592faede4b9c5a9e2c7dc545dbf3589234646844
Nasreddin Boulif (bon) <bon@odoo.com>
2023-02-07 00:30:46 +0100

    [FIX] mass_mailing: replace dynamic placeholder in 'View Online' page
    Steps to reproduce:
    
    - Install `mass_mailing` module
    - Create a new mailing
    - Set a subject and a mailing list
    - Select for the mail body the template "Thank you for joining us!'
    - Add a dynamic placeholder (e.g. ${object.email})
    - Click on "Send"
    - Open the email received and click on "View Online"
    
    Issue:
    
      The dynamic placeholder is not replaced.
    
    Cause:
    
      The controller does not re-render the body based on the res_id.
    
    Solution:
    
      Use `mailing` variable (that inherit from `mail.render.mixin`) to
      re-render the body based on the res_id if available (same as when
      sending the mail).
      https://github.com/odoo/odoo/blob/d3a6a20788c78ddaaff0919ea627e7b10c7ab81c/addons/mail/wizard/mail_compose_message.py#L476
    
    opw-3133975
    
    X-original-commit: 997864c8f949ffc5ba4133a82ea385125b113d44

--- addons/mass_mailing/tests/test_mailing_controllers.py
+++ addons/mass_mailing/tests/test_mailing_controllers.py
@@ -20,3 +20,23 @@ class TestMassMailingControllers(MassMailCommon, HttpCase):
 
         response = self.url_open(url)
         self.assertEqual(response.status_code, 400)
+
+    def test_mailing_view(self):
+        mailing = self.env['mailing.mailing'].create({
+            'name': 'TestMailing',
+            'subject': 'Test',
+            'mailing_type': 'mail',
+            'body_html': '<p>Hello <t t-out="object.name" contenteditable="false" data-oe-t-inline="true"></t></p>',
+            'mailing_model_id': self.env['ir.model']._get('res.partner').id,
+        })
+
+        partner_id = self.user_admin.partner_id
+        self.authenticate('admin', 'admin')
+
+        url = werkzeug.urls.url_join(mailing.get_base_url(), '/mailing/%s/view?res_id=%s' % (mailing.id, partner_id.id))
+        response = self.url_open(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn('<t t-out', response.text)
+        self.assertNotIn('</t>', response.text)
+        self.assertIn("<p>Hello %s</p>" % partner_id.name, response.text)
