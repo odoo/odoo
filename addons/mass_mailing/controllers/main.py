0634f17e49a0a54d2dbc52488cf42707d4fbaaa3
Benoit Socias <bso@odoo.com>
2023-02-06 13:25:13 +0000

    [FIX] *: replace werkzeug's Response by odoo's Response
    *: base, http_routing, mass_mailing, web, web_editor, website_slides
    
    In some situations `werkzeug.wrappers.Response` are used instead of
    `odoo.http.Reponse` that extends it.
    This is a problem because since [1] the calls to `set_cookie` expect it
    to accept the `cookie_type` parameter, which is not the case in the base
    werkzeug implementation.
    
    This commit replaces the `werkzeug.wrappers.Response` by
    `odoo.http.Response`.
    
    [1]: https://github.com/odoo/odoo/commit/2cbda6c98ee947cea1d06c09880eee8c758304a8
    
    closes odoo/odoo#112827
    
    X-original-commit: 28da08292b7028575e628c5ad846fc05d30498f2
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- addons/mass_mailing/controllers/main.py
+++ addons/mass_mailing/controllers/main.py
@@ -5,7 +5,7 @@ import base64
 import werkzeug
 
 from odoo import _, exceptions, http, tools
-from odoo.http import request
+from odoo.http import request, Response
 from odoo.tools import consteq
 
 from markupsafe import Markup, escape
@@ -123,7 +123,7 @@ class MassMailController(http.Controller):
             raise BadRequest()
 
         request.env['mailing.trace'].sudo().set_opened(domain=[('mail_mail_id_int', 'in', [mail_id])])
-        response = werkzeug.wrappers.Response()
+        response = Response()
         response.mimetype = 'image/gif'
         response.data = base64.b64decode(b'R0lGODlhAQABAIAAANvf7wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==')
 
