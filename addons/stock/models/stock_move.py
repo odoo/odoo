c02e9426361b2a7ae65b510aff571e5b455ef79c
Touati Djamel (otd) <otd@odoo.com>
2023-02-08 14:30:09 +0000

    [FIX] stock, purchase_stock: apply putaway strategy even if no package
    Steps to reproduce the bug:
    - Enable “Storage location” option
    - Create a storable product “P1”
    - Create a putaway rule:
        - When in: WH/Stock
        - Store to: WH/Stock/shelf1
        - product: P1
    - Go to operation types -> “Receipts orders”
        - Enable “Show Detailed operations” and
        “Pre-fill detailed operations”
    - Create a purchase order:
         - Add 2unit of “P1”
         - Confirm the PO
    - Go to the receipt:
         - the destination location is correctly set “WH/stock/shelf1”
         - set the qty done to 1
         - validate the delivery and create a back order
    
    Problem:
    The destination location is “WH/Stock” instead of “WH/stock/shelf1”
    because the ```_apply_putaway_strategy``` function is not called on
    the ```stock.move.line``` when there is no package
    
    opw-3162934
    
    closes odoo/odoo#112387
    
    X-original-commit: b4c83ab2b202702e096c775ae73f4646ba93eae4
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Djamel Touati (otd) <otd@odoo.com>

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -1685,9 +1685,8 @@ Please change the quantity done or the rounding precision of your unit of measur
         self.env['stock.move.line'].create(move_line_vals_list)
         StockMove.browse(partially_available_moves_ids).write({'state': 'partially_available'})
         StockMove.browse(assigned_moves_ids).write({'state': 'assigned'})
-        if self.env.context.get('bypass_entire_pack'):
-            return
-        self.mapped('picking_id')._check_entire_pack()
+        if not self.env.context.get('bypass_entire_pack'):
+            self.mapped('picking_id')._check_entire_pack()
         StockMove.browse(moves_to_redirect).move_line_ids._apply_putaway_strategy()
 
     def _action_cancel(self):
