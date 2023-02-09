66f2d4960546ec7e173df97b46bb466dd4735774
Giang Pham <phamgiang2510@gmail.com>
2023-02-09 10:11:46 +0700

    [IMP] membership: Restructure code for extension

--- addons/membership/models/account_move.py
+++ addons/membership/models/account_move.py
@@ -45,36 +45,15 @@ class AccountMoveLine(models.Model):
     def write(self, vals):
         # OVERRIDE
         res = super(AccountMoveLine, self).write(vals)
-
-        to_process = self.filtered(lambda line: line.move_id.move_type == 'out_invoice' and line.product_id.membership)
+        to_process = self._filter_to_create_membership_lines()
 
         # Nothing to process, break.
         if not to_process:
             return res
 
-        existing_memberships = self.env['membership.membership_line'].search([
-            ('account_invoice_line', 'in', to_process.ids)])
-        to_process = to_process - existing_memberships.mapped('account_invoice_line')
-
-        # All memberships already exist, break.
-        if not to_process:
-            return res
-
         memberships_vals = []
         for line in to_process:
-            date_from = line.product_id.membership_date_from
-            date_to = line.product_id.membership_date_to
-            if (date_from and date_from < (line.move_id.invoice_date or date.min) < (date_to or date.min)):
-                date_from = line.move_id.invoice_date
-            memberships_vals.append({
-                'partner': line.move_id.partner_id.id,
-                'membership_id': line.product_id.id,
-                'member_price': line.price_unit,
-                'date': fields.Date.today(),
-                'date_from': date_from,
-                'date_to': date_to,
-                'account_invoice_line': line.id,
-            })
+            memberships_vals.append(line._prepare_membership_line_vals())
         self.env['membership.membership_line'].create(memberships_vals)
         return res
 
@@ -82,34 +61,40 @@ class AccountMoveLine(models.Model):
     def create(self, vals_list):
         # OVERRIDE
         lines = super(AccountMoveLine, self).create(vals_list)
-        to_process = lines.filtered(lambda line: line.move_id.move_type == 'out_invoice' and line.product_id.membership)
+        to_process = lines._filter_to_create_membership_lines()
 
         # Nothing to process, break.
         if not to_process:
             return lines
 
+        memberships_vals = []
+        for line in to_process:
+            memberships_vals.append(line._prepare_membership_line_vals())
+        self.env['membership.membership_line'].create(memberships_vals)
+        return lines
+
+    def _filter_to_create_membership_lines(self):
+        to_process = self.filtered(lambda line: line.move_id.move_type == 'out_invoice' and line.product_id.membership)
+        if not to_process:
+            return to_process
         existing_memberships = self.env['membership.membership_line'].search([
             ('account_invoice_line', 'in', to_process.ids)])
         to_process = to_process - existing_memberships.mapped('account_invoice_line')
+        return to_process
 
-        # All memberships already exist, break.
-        if not to_process:
-            return lines
-
-        memberships_vals = []
-        for line in to_process:
-            date_from = line.product_id.membership_date_from
-            date_to = line.product_id.membership_date_to
-            if (date_from and date_from < (line.move_id.invoice_date or date.min) < (date_to or date.min)):
-                date_from = line.move_id.invoice_date
-            memberships_vals.append({
-                'partner': line.move_id.partner_id.id,
-                'membership_id': line.product_id.id,
-                'member_price': line.price_unit,
-                'date': fields.Date.today(),
-                'date_from': date_from,
-                'date_to': date_to,
-                'account_invoice_line': line.id,
-            })
-        self.env['membership.membership_line'].create(memberships_vals)
-        return lines
+    def _prepare_membership_line_vals(self):
+        self.ensure_one()
+        date_from = self.product_id.membership_date_from
+        date_to = self.product_id.membership_date_to
+        if (date_from and date_from < (self.move_id.invoice_date or date.min) < (date_to or date.min)):
+            date_from = self.move_id.invoice_date
+        memberships_vals = {
+            'partner': self.move_id.partner_id.id,
+            'membership_id': self.product_id.id,
+            'member_price': self.price_unit,
+            'date': fields.Date.today(),
+            'date_from': date_from,
+            'date_to': date_to,
+            'account_invoice_line': self.id,
+        }
+        return memberships_vals
