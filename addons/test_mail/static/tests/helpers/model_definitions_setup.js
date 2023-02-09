2cfeff00918fef25ebc0601850c08798f744e4b0
Pierre-Yves Dufays <pydu@odoo.com>
2023-02-09 08:30:02 +0000

    [FIX] {test_}mail: fix send message button activation (_mail_post_access)
    The attribute _mail_post_access='read' on a model allows user with read only
    access on the model to post a message. It was not taken into account on the
    client side, making the send message button disabled for user with readonly
    access on such model. This commit fixes this issue and now correctly takes
    into account that class parameter.
    
    Task-3178885 (Mail: fix 'readonly' post support)
    
    X-original-commit: odoo/odoo@d2b8612cf7d9c118a12a1f0407bbc893f07e4b56

--- addons/test_mail/static/tests/helpers/model_definitions_setup.js
+++ addons/test_mail/static/tests/helpers/model_definitions_setup.js
@@ -2,4 +2,4 @@
 
 import { addModelNamesToFetch } from '@bus/../tests/helpers/model_definitions_helpers';
 
-addModelNamesToFetch(['mail.test.track.all', 'mail.test.activity']);
+addModelNamesToFetch(['mail.test.track.all', 'mail.test.activity', 'mail.test.multi.company', 'mail.test.multi.company.read']);
