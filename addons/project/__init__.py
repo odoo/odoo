828e6c37950fc44983e6d26d912ff2e65a5ca680
Xavier BOL (xbo) <xbo@odoo.com>
2023-02-10 20:42:01 +0000

    [FIX] project: improve performance for burndown chart report
    Before this commit when the user wants to see the burndown chart of a
    old large project then the report could take more than 20 sec to be
    loaded.
    
    This commit adds 2 indexes one on `mail_tracking_value` table
    (`mail.tracking.value` model) and the other one on `mail_message` table
    (`mail.message` model) to reduce the load of that report to less than
    10 sec.
    
    task-3177072
    
    X-original-commit: c6b355d471889634ae26a020e14c81e9a315b3de
    Co-authored-by: Yannick Tivisse <yti@odoo.com>

--- addons/project/__init__.py
+++ addons/project/__init__.py
@@ -7,6 +7,8 @@ from . import report
 from . import wizard
 from . import populate
 
+from odoo.tools.sql import create_index
+
 
 def _check_exists_collaborators_for_project_sharing(env):
     """ Check if it exists at least a collaborator in a shared project
@@ -21,3 +23,13 @@ def _check_exists_collaborators_for_project_sharing(env):
 
 def _project_post_init(env):
     _check_exists_collaborators_for_project_sharing(env)
+
+    # Index to improve the performance of burndown chart.
+    project_task_stage_field_id = env['ir.model.fields']._get_ids('project.task').get('stage_id')
+    create_index(
+        env.cr,
+        'mail_tracking_value_mail_message_id_old_value_integer_task_stage',
+        env['mail.tracking.value']._table,
+        ['mail_message_id', 'old_value_integer'],
+        where=f'field={project_task_stage_field_id}'
+    )
