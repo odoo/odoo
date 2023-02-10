c81135dc4b6dfe54b87acab53f5b182cc3be1700
Pierre Verkest <pierreverkest84@gmail.com>
2023-02-10 11:51:24 +0100

    [FIX] resource: make linter happy

--- addons/resource/__init__.py
+++ addons/resource/__init__.py
@@ -2,4 +2,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import models
-from .tests import models
+from .tests import tests_models
---
+++ addons/resource/tests/tests_models/__init__.py
@@ -0,0 +1,1 @@
+from . import resource_test
