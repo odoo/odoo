c83dc9cb969132335c559b74d67516141b8b0066
Benjamin Vray <bvr@odoo.com>
2023-02-10 08:32:55 +0000

    [FIX] website: add test for popup and scrollbar
    Add test to verify scrollbar behavior for page and popups with or
    without backdrop. The expected behavior is that the page scrollbar
    should not be present when a modal with a backdrop is open, and that the
    page scrollbar should be displayed if the popup does not have a
    backdrop, such as a cookie bar. However, if a modal without a backdrop
    itself has a scrollbar, the page scrollbar should not be displayed to
    prevent the two scrollbars from overlapping, which prevents scrolling of
    the popup on Chrome (when clicking on the scrollbar instead of using the
    mouse wheel).
    
    In addition to that, this commit includes a test to confirm the proper
    functionality of snippet animations when a cookie bar is displayed on
    the page. Moreover, it also verifies that the snippet animations perform
    correctly within a popup.
    
    During the forward port of this commit (Master branch), the test failed
    due to missing braces around 'throttleForAnimation' in the '000.js' file
    of 's_popup'. These were directly added in this commit.
    
    task-2983901
    
    X-original-commit: 4dd462f20ccb29923e5a053e0ed4c08545f82ae7

--- addons/website/static/src/snippets/s_popup/000.js
+++ addons/website/static/src/snippets/s_popup/000.js
@@ -4,7 +4,7 @@ import config from "web.config";
 import publicWidget from "web.public.widget";
 import {getCookie, setCookie} from "web.utils.cookies";
 import dom from "web.dom";
-import throttleForAnimation from "@web/core/utils/timing";
+import {throttleForAnimation} from "@web/core/utils/timing";
 
 // TODO In master, export this class too or merge it with PopupWidget
 const SharedPopupWidget = publicWidget.Widget.extend({
