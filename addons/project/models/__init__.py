506e7b5ff55a8a28fbd6789e110233c9dc70bf7e
Xavier BOL (xbo) <xbo@odoo.com>
2023-02-10 21:42:01 +0100

    [FIX] project: improve performance for burndown chart report
    Before this commit when the user wants to see the burndown chart of a
    old large project then the report could take more than 20 sec to be
    loaded.
    
    This commit adds 2 indexes one on `mail_tracking_value` table
    (`mail.tracking.value` model) and the other one on `mail_message` table
    (`mail.message` model) to reduce the load of that report to less than
    10 sec.
    
    task-3177072
    
    Co-authored-by: Yannick Tivisse <yti@odoo.com>

--- addons/project/models/__init__.py
+++ addons/project/models/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import analytic_account
+from . import mail_message
 from . import project_milestone
 from . import project_project_stage
 from . import project_task_recurrence
