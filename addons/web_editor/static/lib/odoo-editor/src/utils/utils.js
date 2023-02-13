d112c7538f3970c7af3f3ca8a3b77052efe58350
Antoine Guenet <age@odoo.com>
2023-02-13 08:25:52 +0000

    [FIX] web_editor: fix triple click on block followed by empty block
    Triple click correction failed whenever the triple-click-selected block
    was followed by an empty block. The reason is that `getDeepestPosition`
    prevents the selection from being placed within a line break (`<br>`)
    and therefore returns the block itself (`<p>[]<br></p>` will be
    expressed as `(p, 0)`), so the heuristic to determine if triple click
    correction in needed in `getDeepRange` would fail as it assumed the
    `end` node was a leaf where it wasn't. This fixes the issue by applying
    the heurstic to the first leaf of the `end` node instead, and making the
    correction for there as well.
    
    task-3180922
    
    closes odoo/odoo#112530
    
    X-original-commit: c630f0c60899db1d874de9fd0477e38ed2ca504a
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/lib/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/lib/odoo-editor/src/utils/utils.js
@@ -683,17 +683,18 @@ export function getDeepRange(editable, { range, sel, splitText, select, correctT
             }
         }
     }
-    // A selection spanning multiple nodes and ending at position 0 of a
-    // node, like the one resulting from a triple click, are corrected so
-    // that it ends at the last position of the previous node instead.
-    const beforeEnd = end.previousSibling;
+    // A selection spanning multiple nodes and ending at position 0 of a node,
+    // like the one resulting from a triple click, is corrected so that it ends
+    // at the last position of the previous node instead.
+    const endLeaf = firstLeaf(end);
+    const beforeEnd = endLeaf.previousSibling;
     if (
         correctTripleClick &&
         !endOffset &&
         (start !== end || startOffset !== endOffset) &&
         (!beforeEnd || (beforeEnd.nodeType === Node.TEXT_NODE && !isVisibleStr(beforeEnd)))
     ) {
-        const previous = previousLeaf(end, editable, true);
+        const previous = previousLeaf(endLeaf, editable, true);
         if (previous && closestElement(previous).isContentEditable) {
             [end, endOffset] = [previous, nodeSize(previous)];
         }
