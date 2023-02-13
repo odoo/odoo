80a5f99daa2619ab0aa2c69e13e934c3e24e1c7f
Sébastien Theys <seb@odoo.com>
2023-02-13 14:32:01 +0100

    [FIX] calendar: fix activity service override

--- addons/calendar/static/src/new/activity/activity_service.js
+++ addons/calendar/static/src/new/activity/activity_service.js
@@ -4,11 +4,12 @@ import { ActivityService } from "@mail/new/web/activity/activity_service";
 import { patch } from "@web/core/utils/patch";
 
 patch(ActivityService.prototype, "calendar/activity_service", {
-    update(activity, data) {
-        this._super(activity, data);
+    insert(data) {
+        const activity = this._super(data);
         const { calendar_event_id: calendarEventId } = data;
         if (calendarEventId) {
             activity["calendar_event_id"] = calendarEventId;
         }
+        return activity;
     },
 });
