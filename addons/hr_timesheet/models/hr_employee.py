aeb2388f14d516ef14794e8a05bde169ceedfabb
sofiagvaladze <sgv@odoo.com>
2023-02-14 16:19:22 +0000

    [IMP] *: manage smartbuttons on employee form view
    *: hr_attendance,hr_skills_slides,hr_timesheet,hr_work_entry
    
    Don't show smartbuttons that lead to a view without records
    
    task - 3134772
    
    closes odoo/odoo#113105
    
    Related: odoo/enterprise#37274
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_timesheet/models/hr_employee.py
+++ addons/hr_timesheet/models/hr_employee.py
@@ -2,12 +2,26 @@
 
 from ast import literal_eval
 
-from odoo import models, _
+from odoo import models, fields, _
 from odoo.exceptions import UserError
 
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
+
+    has_timesheet = fields.Boolean(compute='_compute_has_timesheet')
+
+    def _compute_has_timesheet(self):
+        self.env.cr.execute("""
+        SELECT id, EXISTS(SELECT 1 FROM account_analytic_line WHERE project_id IS NOT NULL AND employee_id = e.id limit 1)
+          FROM hr_employee e
+         WHERE id in %s
+        """, (tuple(self.ids), ))
+
+        result = {eid[0]: eid[1] for eid in self.env.cr.fetchall()}
+
+        for employee in self:
+            employee.has_timesheet = result.get(employee.id, False)
 
     def action_unlink_wizard(self):
         wizard = self.env['hr.employee.delete.wizard'].create({
