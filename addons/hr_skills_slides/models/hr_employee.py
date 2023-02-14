7c64e6b82b3c8a0a5a2109045801ebbafea3b494
sofiagvaladze <sgv@odoo.com>
2023-02-14 17:19:22 +0100

    [IMP] *: manage smartbuttons on employee form view
    *: hr_attendance,hr_skills_slides,hr_timesheet,hr_work_entry
    
    Don't show smartbuttons that lead to a view without records
    
    task - 3134772

--- addons/hr_skills_slides/models/hr_employee.py
+++ addons/hr_skills_slides/models/hr_employee.py
@@ -8,18 +8,23 @@ class Employee(models.Model):
     _inherit = 'hr.employee'
 
     subscribed_courses = fields.Many2many('slide.channel', related='user_partner_id.slide_channel_ids')
+    has_subscribed_courses = fields.Boolean(compute='_compute_courses_completion_text')
     courses_completion_text = fields.Char(compute="_compute_courses_completion_text")
 
     @api.depends_context('lang')
+    @api.depends('subscribed_courses', 'user_partner_id.slide_channel_completed_ids')
     def _compute_courses_completion_text(self):
         for employee in self:
             if not employee.user_partner_id:
                 employee.courses_completion_text = False
+                employee.has_subscribed_courses = False
                 continue
             total_completed_courses = len(employee.user_partner_id.slide_channel_completed_ids)
+            total = len(employee.subscribed_courses)
             employee.courses_completion_text = _("%(completed)s / %(total)s",
                 completed=total_completed_courses,
-                total=len(employee.subscribed_courses))
+                total=total)
+            employee.has_subscribed_courses = total > 0
 
     def action_open_courses(self):
         self.ensure_one()
