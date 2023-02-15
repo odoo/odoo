4056d21de1c5d79b6fbf5cbc646ec7a0bb09e41e
nda-odoo <nda@odoo.com>
2023-02-15 15:12:04 -0400

    [IMP] core: add query time in the logging of SQL queries
    backport of 46040d11cf1ecf880f76d9d8a445d72c8ba77b4e

--- odoo/sql_db.py
+++ odoo/sql_db.py
@@ -285,15 +285,16 @@ class Cursor(BaseCursor):
             _logger.warning(msg)
             self._close(True)
 
+    def _format(self, query, params=None):
+        encoding = psycopg2.extensions.encodings[self.connection.encoding]
+        return self._obj.mogrify(query, params).decode(encoding, 'replace')
+
     @check
     def execute(self, query, params=None, log_exceptions=None):
         if params and not isinstance(params, (tuple, list, dict)):
             # psycopg2's TypeError is not clear if you mess up the params
             raise ValueError("SQL query parameters should be a tuple, list or dict; got %r" % (params,))
 
-        if self.sql_log:
-            encoding = psycopg2.extensions.encodings[self.connection.encoding]
-            _logger.debug("query: %s", self._obj.mogrify(query, params).decode(encoding, 'replace'))
         now = time.time()
         try:
             params = params or None
@@ -302,10 +303,14 @@ class Cursor(BaseCursor):
             if self._default_log_exceptions if log_exceptions is None else log_exceptions:
                 _logger.error("bad query: %s\nERROR: %s", tools.ustr(self._obj.query or query), e)
             raise
+        finally:
+            delay = time.time() - now
+            if self.sql_log:
+                _logger.debug("[%.3f ms] query: %s", 1000 * delay, self._format(query, params))
 
         # simple query count is always computed
         self.sql_log_count += 1
-        delay = (time.time() - now)
+
         if hasattr(threading.current_thread(), 'query_count'):
             threading.current_thread().query_count += 1
             threading.current_thread().query_time += delay
