092ca862e56cd7362774a13c6979fb0de2b97bd8
Yolann Sabaux <yosa@odoo.com>
2023-02-15 17:16:19 +0000

    [FIX] account_check_printing: extend limit for check numbering
    Steps to reproduce:
    - Create a new journal Bank
    - In Sequence, find the 'New Bank Check" and edit it so the sequence can be 10 number digits long
    - Create a Vendor Payments with the the New Banck and Check as a method
    - Click on Print a check
    - Set any number > 2147483647 and validate
    
    Issue:
    Traceback
    
    Cause:
    The query SQL make a check to verifiy that the number is correct ('025'::integer == '25'::integer but '025'!='25)
    But using INTEGER limits the number up to 2147483647 (https://www.postgresql.org/docs/current/datatype-numeric.html)
    
    Solution:
    Use BIGINT whose limit is 9223372036854775807
    
    opw-3140973
    
    closes odoo/odoo#113477
    
    X-original-commit: 49dd9dffd7b96fd90860123f8b3f0e623979b246
    Signed-off-by: Yolann Sabaux (yosa) <yosa@odoo.com>
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- addons/account_check_printing/models/account_payment.py
+++ addons/account_check_printing/models/account_payment.py
@@ -60,7 +60,7 @@ class AccountPayment(models.Model):
               JOIN account_journal journal ON journal.id = move.journal_id,
                    account_payment other_payment
               JOIN account_move other_move ON other_move.id = other_payment.move_id
-             WHERE payment.check_number::INTEGER = other_payment.check_number::INTEGER
+             WHERE payment.check_number::BIGINT = other_payment.check_number::BIGINT
                AND move.journal_id = other_move.journal_id
                AND payment.id != other_payment.id
                AND payment.id IN %(ids)s
