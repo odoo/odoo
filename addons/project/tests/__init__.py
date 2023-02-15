c45d23f5501c3ec78f0d89010639bb077a06f712
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2023-02-15 17:55:40 +0100

    [IMP] project: add task quick create shortcuts
    In this commit,
    -We Ease the quick creation of tasks by providing shortcuts allowing the user
    to set different fields (planned_hours, tags, priority, and assign to users)
    without opening the form view.
    
    task-3145203

--- addons/project/tests/__init__.py
+++ addons/project/tests/__init__.py
@@ -25,3 +25,4 @@ from . import test_task_dependencies
 from . import test_task_follow
 from . import test_task_tracking
 from . import test_project_report
+from . import test_project_task_quick_create
