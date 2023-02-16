c444688da7ffa1e9c69d2587175b34bc7aa0a7b5
Hansun (hale) <hale@odoo.com>
2023-02-16 17:45:12 +0100

    [FIX] account: enable menu addition through studio
    1. Install [Studio], [Accounting] on Apps
    2. Enter [Accounting]
    - Click the Studio icon second to left on the top right corner
    - [Edit Menu]
    - [NEW MENU]
    - Fill in e.g.`Test`, select [Existing Model], Model: Payment Methods
    - Click the icon on the right, check it is `account.payment.method`
    - [SAVE & CLOSE]
    - [CONFIRM]
    - [CLOSE]
    3. Click on `Test` menu created next to [Configuration]
    - [NEW]
    - Type in all info, click to Save
    - Error returns
    
    Impacted versions: 15-master
    
    opw-3148453

--- addons/account/models/account_payment_method.py
+++ addons/account/models/account_payment_method.py
@@ -24,6 +24,8 @@ class AccountPaymentMethod(models.Model):
         methods_info = self._get_payment_method_information()
         for method in payment_methods:
             information = methods_info.get(method.code)
+            if not information:
+                raise UserError(_("Cannot add payment methods without valid information"))
 
             if information.get('mode') == 'multi':
                 method_domain = method._get_payment_method_domain()
