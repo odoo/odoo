6228e999c7445083ef93c06f774056542c8e56cd
Victor Feyens <vfe@odoo.com>
2023-02-16 14:58:39 +0000

    [FIX] sale: salesman computation
    Task-3105314
    
    X-original-commit: 812942a58501e2d64eac74e3a03d8cddc7b8beb5

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -401,9 +401,15 @@ class SaleOrder(models.Model):
     @api.depends('partner_id')
     def _compute_user_id(self):
         for order in self:
-            if order.partner_id and not order.user_id:
-                order.user_id = order.partner_id.user_id or order.partner_id.commercial_partner_id.user_id or \
-                    (self.user_has_groups('sales_team.group_sale_salesman') and self.env.user)
+            if order.partner_id and not (order._origin.id and order.user_id):
+                # Recompute the salesman on partner change
+                #   * if partner is set (is required anyway, so it will be set sooner or later)
+                #   * if the order is not saved or has no salesman already
+                order.user_id = (
+                    order.partner_id.user_id
+                    or order.partner_id.commercial_partner_id.user_id
+                    or (self.user_has_groups('sales_team.group_sale_salesman') and self.env.user)
+                )
 
     @api.depends('partner_id', 'user_id')
     def _compute_team_id(self):
