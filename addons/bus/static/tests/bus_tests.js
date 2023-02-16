daf49055de17e4c9328c665bf61fd8e0602ffce8
tsm-odoo <tsm@odoo.com>
2023-02-16 07:37:28 +0100

    [FIX] bus: shared-worker failing to start on incognito
     Before this PR, the bus service's `SharedWorker` sometimes failed
     to initialized with data url. This error only occurred on some devices,
     on chrome and in incognito mode.
    
     While the specifications specify that `SharedWorker` constructor should
     handle data url properly [1], there is no way to tell whether or not the
     user browser supports it.
    
     This commit solves this issue by introducing a fallback on simple `Worker`s
     since they seem to handle data url correctly in this case [2].
    
    [1] https://html.spec.whatwg.org/multipage/workers.html#dom-worker
    [2] https://chromium.googlesource.com/chromium/src.git/+/9801daec162285a1f2c6695682ace5d508ea9b7f
    
    opw-3146671

--- addons/bus/static/tests/bus_tests.js
+++ addons/bus/static/tests/bus_tests.js
@@ -541,6 +541,38 @@ QUnit.module('Bus', {
             "reconnect",
         ]);
     });
+
+    QUnit.test(
+        "Fallback on simple worker when shared worker failed to initialize",
+        async function (assert) {
+            const originalSharedWorker = browser.SharedWorker;
+            const originalWorker = browser.Worker;
+            patchWithCleanup(browser, {
+                SharedWorker: function (url, options) {
+                    assert.step("shared-worker creation");
+                    const sw = new originalSharedWorker(url, options);
+                    // Simulate error during shared worker creation.
+                    setTimeout(() => sw.dispatchEvent(new Event("error")));
+                    return sw;
+                },
+                Worker: function (url, options) {
+                    assert.step("worker creation");
+                    return new originalWorker(url, options);
+                },
+            }, { pure: true });
+            patchWithCleanup(window.console, {
+                warn(message) {
+                    assert.step(message);
+                },
+            })
+            await makeTestEnv();
+            assert.verifySteps([
+                "shared-worker creation",
+                "Error while loading \"bus_service\" SharedWorker, fallback on Worker.",
+                "worker creation",
+            ]);
+        }
+    );
 });
 });
 
