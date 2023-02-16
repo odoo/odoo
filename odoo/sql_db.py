01aa7f900217b884f1e6f168608ba349a4c160f7
bve-odoo <bve@odoo.com>
2023-02-16 18:28:31 +0000

    [FIX] core: recycle more connections in multi-tenant servers
    During revision 415525cecceb9af054b57fd85e3791cc779f3d6c
    the code comparing the connections without password has been
    dropped, for an unknown reason.
    
    Because of the above, and the fact psycopg2 alter the password
    with 'xxx' for security reasons, the connections recycling was
    no longer working as expected, and was recycling less connections.
    
    closes odoo/odoo#37005
    
    Signed-off-by: Rémy Voet <ryv@odoo.com>

--- odoo/sql_db.py
+++ odoo/sql_db.py
@@ -565,6 +565,15 @@ class PsycoConnection(psycopg2.extensions.connection):
     def lobject(*args, **kwargs):
         pass
 
+    if hasattr(psycopg2.extensions, 'ConnectionInfo'):
+        @property
+        def info(self):
+            class PsycoConnectionInfo(psycopg2.extensions.ConnectionInfo):
+                @property
+                def password(self):
+                    pass
+            return PsycoConnectionInfo(self)
+
 
 class ConnectionPool(object):
     """ The pool of connections to database(s)
@@ -607,7 +616,7 @@ class ConnectionPool(object):
                 _logger.info('%r: Free leaked connection to %r', self, cnx.dsn)
 
         for i, (cnx, used) in enumerate(self._connections):
-            if not used and cnx._original_dsn == connection_info:
+            if not used and self._dsn_equals(cnx.dsn, connection_info):
                 try:
                     cnx.reset()
                 except psycopg2.OperationalError:
@@ -642,7 +651,6 @@ class ConnectionPool(object):
         except psycopg2.Error:
             _logger.info('Connection to the database failed')
             raise
-        result._original_dsn = connection_info
         self._connections.append((result, True))
         self._debug('Create new connection backend PID %d', result.get_backend_pid())
         return result
@@ -668,28 +676,51 @@ class ConnectionPool(object):
         count = 0
         last = None
         for i, (cnx, used) in tools.reverse_enumerate(self._connections):
-            if dsn is None or cnx._original_dsn == dsn:
+            if dsn is None or self._dsn_equals(cnx.dsn, dsn):
                 cnx.close()
                 last = self._connections.pop(i)[0]
                 count += 1
         _logger.info('%r: Closed %d connections %s', self, count,
                     (dsn and last and 'to %r' % last.dsn) or '')
 
+    def _dsn_equals(self, dsn1, dsn2):
+        alias_keys = {'dbname': 'database'}
+        ignore_keys = ['password']
+        dsn1, dsn2 = ({
+            alias_keys.get(key, key): str(value)
+            for key, value in (isinstance(dsn, str) and self._dsn_to_dict(dsn) or dsn).items()
+            if key not in ignore_keys
+        } for dsn in (dsn1, dsn2))
+        return dsn1 == dsn2
+
+    def _dsn_to_dict(self, dsn):
+        return dict(value.split('=', 1) for value in dsn.strip().split())
+
 
 class Connection(object):
     """ A lightweight instance of a connection to postgres
     """
     def __init__(self, pool, dbname, dsn):
-        self.dbname = dbname
-        self.dsn = dsn
+        self.__dbname = dbname
+        self.__dsn = dsn
         self.__pool = pool
+
+    @property
+    def dsn(self):
+        dsn = dict(self.__dsn)
+        dsn.pop('password', None)
+        return dsn
+
+    @property
+    def dbname(self):
+        return self.__dbname
 
     def cursor(self, **kwargs):
         if 'serialized' in kwargs:
             warnings.warn("Since 16.0, 'serialized' parameter is deprecated", DeprecationWarning, 2)
         cursor_type = kwargs.pop('serialized', True) and 'serialized ' or ''
         _logger.debug('create %scursor to %r', cursor_type, self.dsn)
-        return Cursor(self.__pool, self.dbname, self.dsn)
+        return Cursor(self.__pool, self.__dbname, self.__dsn)
 
     def serialized_cursor(self, **kwargs):
         warnings.warn("Since 16.0, 'serialized_cursor' is deprecated, use `cursor` instead", DeprecationWarning, 2)
