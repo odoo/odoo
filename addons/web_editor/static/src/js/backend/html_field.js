d7af13ec3af2073816aa88b67e03a5060e69e9a2
FrancoisGe <fge@odoo.com>
2023-02-17 09:37:19 +0100

    [FIX] web_editor: Html Field in Record
    Using the web_editor html field in the Record component causes crashes.
    
    Why does this happen?
    The fields used in the Record component do not always have modifiers.
    
    Solution:
    Use the isReadonly function of the record to know if the field is
    readonly or not.

--- addons/web_editor/static/src/js/backend/html_field.js
+++ addons/web_editor/static/src/js/backend/html_field.js
@@ -138,7 +138,7 @@ export class HtmlField extends Component {
                         // Ensure all external links are opened in a new tab.
                         retargetLinks(this.readonlyElementRef.el);
 
-                        const hasReadonlyModifiers = Boolean(this.props.record.activeFields[this.props.fieldName].modifiers.readonly);
+                        const hasReadonlyModifiers = Boolean(this.props.record.isReadonly(this.props.fieldName));
                         if (!hasReadonlyModifiers) {
                             const $el = $(this.readonlyElementRef.el);
                             $el.off('.checklistBinding');
