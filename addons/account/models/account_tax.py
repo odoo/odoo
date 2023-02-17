5bf96df0d17ed52139aa631bc88a8503b8c8af4a
Claire Bretton (clbr) <clbr@odoo.com>
2023-02-17 13:32:00 +0100

    [FIX] account: relax repartition lines validation when tax has children
    If a tax is an aggregation of its sub-taxes it makes sense to have no
    repartition line. This PR relaxes the validation in that case.

--- addons/account/models/account_tax.py
+++ addons/account/models/account_tax.py
@@ -181,6 +181,12 @@ class AccountTax(models.Model):
     @api.constrains('invoice_repartition_line_ids', 'refund_repartition_line_ids')
     def _validate_repartition_lines(self):
         for record in self:
+            # if the tax is an aggregation of its sub-taxes (group) it can have no repartition lines
+            if record.amount_type == 'group' and \
+                    not record.invoice_repartition_line_ids and \
+                    not record.refund_repartition_line_ids:
+                continue
+
             invoice_repartition_line_ids = record.invoice_repartition_line_ids.sorted()
             refund_repartition_line_ids = record.refund_repartition_line_ids.sorted()
             record._check_repartition_lines(invoice_repartition_line_ids)
