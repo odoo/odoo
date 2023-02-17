9e1077f6aa09edc2ce25748d99c291e5d338d76d
Aaron Bohy <aab@odoo.com>
2023-02-17 08:12:02 +0000

    [FIX] web: grouped kanban: only open at most 10 groups
    For performance reasons, the grouped kanban view automatically
    folds groups when they are to many, s.t. at most 10 groups are
    open by default (because for each one of them, we must perform
    a web_search_read rpc to fetch their records) [1].
    
    However, during the convertion to owl, we broke half of this logic.
    The qunit test introduced by [1] simulates a read_group that only
    sets the __fold key to true in folded groups, but doesn't define it
    in other groups. This test passed. But the logic in the model
    didn't correctly handle the case where the __fold key was given for
    all groups, even when its value was set to false. This happens for
    instance in the project task kanban view.
    
    This commit fixes the issue.
    
    [1] https://github.com/odoo/odoo/commit/e9c5229c41acd2de36d7d57dba9372ff66c5d9c2
    
    Task 3191948
    
    closes odoo/odoo#113061
    
    X-original-commit: dd71662cc994fb8716cae63944b4df9d4201dcdc
    Signed-off-by: Georis François (fge) <fge@odoo.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/views/relational_model.js
+++ addons/web/static/src/views/relational_model.js
@@ -2453,7 +2453,8 @@ export class DynamicGroupList extends DynamicList {
                     }
                     case "__fold": {
                         // optional
-                        groupParams.isFolded = value;
+                        groupParams.isFolded =
+                            openGroups >= this.constructor.DEFAULT_LOAD_LIMIT || value;
                         if (!value) {
                             openGroups++;
                         }
