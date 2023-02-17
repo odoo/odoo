5423f397bca2297c2eb957be5ece296dadd9f2d9
Stefan-Calin Crainiciuc (stcc) <stcc@odoo.com>
2023-02-17 17:21:18 +0000

    [FIX] mrp: save qty_producing in tablet view
    Related enterprise PR: https://github.com/odoo/enterprise/pull/37398
    
    Steps to reproduce:
    
    - Install mrp_workorder
    - Edit product Table > Inventory tab > Tracking: No Tracking
    - Manufacturing Orders > Create order for Table > Quantity = 3
    - Confirm > Work orders tab > Open Tablet View
    - Change the number of units to produce in the top from 3 to 2.
    - Pause > Continue
    
    Issue:
    The quantity to produce is reset to the original amount.
    
    Solution:
    Do not reset the qty_producing when it is already set.
    
    Behaviour introduced in https://github.com/odoo/odoo/pull/70153 and later removed in https://github.com/odoo/odoo/pull/83389
    
    opw-3032052
    
    X-original-commit: 25c34d1bb4340db07f302aee4a447cc7ee555dbd

--- addons/mrp/models/mrp_workorder.py
+++ addons/mrp/models/mrp_workorder.py
@@ -591,6 +591,8 @@ class MrpWorkorder(models.Model):
 
         if self.product_tracking == 'serial':
             self.qty_producing = 1.0
+        elif self.qty_producing == 0:
+            self.qty_producing = self.qty_remaining
 
         self.env['mrp.workcenter.productivity'].create(
             self._prepare_timeline_vals(self.duration, datetime.now())
--- addons/mrp/tests/test_order.py
+++ addons/mrp/tests/test_order.py
@@ -367,6 +367,43 @@ class TestMrpOrder(TestMrpCommon):
         production = mo_form.save()
         self.assertEqual(production.workorder_ids.duration_expected, 90)
 
+    def test_qty_producing(self):
+        """Qty producing should be the qty remain to produce, instead of 0"""
+        bom = self.env['mrp.bom'].create({
+            'product_id': self.product_6.id,
+            'product_tmpl_id': self.product_6.product_tmpl_id.id,
+            'product_qty': 1,
+            'product_uom_id': self.product_6.uom_id.id,
+            'type': 'normal',
+            'bom_line_ids': [
+                (0, 0, {'product_id': self.product_2.id, 'product_qty': 2.00}),
+            ],
+        })
+        production_form = Form(self.env['mrp.production'])
+        production_form.product_id = self.product_6
+        production_form.bom_id = bom
+        production_form.product_qty = 5
+        production_form.product_uom_id = self.product_6.uom_id
+        production = production_form.save()
+        production_form = Form(production)
+        with production_form.workorder_ids.new() as wo:
+            wo.name = 'OP1'
+            wo.workcenter_id = self.workcenter_1
+            wo.duration_expected = 40
+        production = production_form.save()
+        production.action_confirm()
+        production.button_plan()
+
+        wo = production.workorder_ids[0]
+        wo.button_start()
+        self.assertEqual(wo.qty_producing, 5, "Wrong quantity is suggested to produce.")
+
+        # Simulate changing the qty_producing in the frontend
+        wo.qty_producing = 4
+        wo.button_pending()
+        wo.button_start()
+        self.assertEqual(wo.qty_producing, 4, "Changing the qty_producing in the frontend is not persisted")
+
     def test_update_quantity_5(self):
         bom = self.env['mrp.bom'].create({
             'product_id': self.product_6.id,
