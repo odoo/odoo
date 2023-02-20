b2541e6218e014a3ed1f2b58915ad897eb2a3f58
Valentin Vallaeys (vava) <vava@odoo.com>
2023-02-20 14:34:03 +0100

    [REF] (stock_)delivery: split delivery from stock
    Extract the Inventory logic from the `delivery` module into a new
    `stock_delivery` module. This will allow to integrate the basic delivery
    features into the website_sale app in a one-app free database.
    
    task-3074497

--- addons/delivery/__manifest__.py
+++ addons/delivery/__manifest__.py
@@ -4,33 +4,22 @@
 {
     'name': 'Delivery Costs',
     'version': '1.0',
-    'category': 'Inventory/Delivery',
+    'category': 'Sales/Delivery',
     'description': """
-Allows you to add delivery methods in sale orders and picking.
-==============================================================
-
-You can define your own carrier for prices. When creating
-invoices from picking, the system is able to add and compute the shipping line.
+Allows you to add delivery methods in sale orders.
+==================================================
+You can define your own carrier for prices.
+The system is able to add and compute the shipping line.
 """,
-    'depends': ['sale_stock', 'sale_management'],
+    'depends': ['sale'],
     'data': [
         'security/ir.model.access.csv',
         'security/delivery_carrier_security.xml',
-        'views/product_template_view.xml',
         'views/delivery_view.xml',
         'views/partner_view.xml',
-        'views/delivery_portal_template.xml',
         'data/delivery_data.xml',
-        'views/report_shipping.xml',
-        'views/report_deliveryslip.xml',
-        'views/report_package_barcode.xml',
         'views/res_config_settings_views.xml',
-        'wizard/choose_delivery_package_views.xml',
         'wizard/choose_delivery_carrier_views.xml',
-        'views/stock_package_type_views.xml',
-        'views/stock_picking_type_views.xml',
-        'views/stock_rule_views.xml',
-        'views/stock_move_line_views.xml',
         'report/ir_actions_report_templates.xml',
     ],
     'demo': ['data/delivery_demo.xml'],
