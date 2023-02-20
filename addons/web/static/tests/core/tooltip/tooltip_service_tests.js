1d6c349fdd0975f0adaff0d15bd072760aa4d113
Aaron Bohy <aab@odoo.com>
2023-02-20 11:24:57 +0100

    [FIX] web,mail: tooltip service: correctly apply default delay
    The delay of tooltips opened with the tooltip service can be given
    in xml, with attribute data-tooltip-delay. If not specified, there
    is a fallback in the service to 400ms. Before this commit, the
    fallback wasn't correctly applied, because we parsed the xml delay
    into an integer, which was NaN when the delay wasn't specified,
    and as NaN isn't undefined, the fallback wasn't applied. As a
    consequence, we opened the tooltip in a setTimeout with NaN as
    delay, and it thus opened directly.
    
    X-original-commit: 553b2775a4b31c75d1d246816b749e4521e25726

--- addons/web/static/tests/core/tooltip/tooltip_service_tests.js
+++ addons/web/static/tests/core/tooltip/tooltip_service_tests.js
@@ -343,14 +343,27 @@ QUnit.module("Tooltip service", (hooks) => {
         assert.containsNone(target, ".o_popover_container .o_popover");
     });
 
-    QUnit.test("tooltip with a delay", async (assert) => {
+    QUnit.test("tooltip with no delay (default delay)", async (assert) => {
+        assert.expect(1);
         class MyComponent extends Component {}
-        MyComponent.template = xml`<button data-tooltip="'helpful tooltip'" data-tooltip-delay="2000">Action</button>`;
+        MyComponent.template = xml`<button class="myBtn" data-tooltip="'helpful tooltip'">Action</button>`;
+        const mockSetTimeout = (fn, delay) => {
+            assert.strictEqual(delay, 400);
+        };
+        await makeParent(MyComponent, { mockSetTimeout });
+        target.querySelector("button.myBtn").dispatchEvent(new Event("mouseenter"));
+        await nextTick();
+    });
+
+    QUnit.test("tooltip with a delay", async (assert) => {
+        assert.expect(1);
+        class MyComponent extends Component {}
+        MyComponent.template = xml`<button class="myBtn" data-tooltip="'helpful tooltip'" data-tooltip-delay="2000">Action</button>`;
         const mockSetTimeout = (fn, delay) => {
             assert.strictEqual(delay, 2000);
         };
         await makeParent(MyComponent, { mockSetTimeout });
-        target.querySelector("button").dispatchEvent(new Event("mouseenter"));
+        target.querySelector("button.myBtn").dispatchEvent(new Event("mouseenter"));
         await nextTick();
     });
 
