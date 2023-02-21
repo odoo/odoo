efbe5d837e20b24bfac743b36936a0b183a51ca0
Victor Feyens <vfe@odoo.com>
2023-02-21 11:17:56 +0000

    [MOV] product: split product_attribute.py
    Part-of: odoo/odoo#113612

--- addons/product/models/__init__.py
+++ addons/product/models/__init__.py
@@ -10,12 +10,17 @@ from . import product_product
 
 from . import decimal_precision
 from . import product_attribute
+from . import product_attribute_custom_value
+from . import product_attribute_value
 from . import product_category
 from . import product_packaging
 from . import product_pricelist
 from . import product_pricelist_item
 from . import product_supplierinfo
 from . import product_tag
+from . import product_template_attribute_line
+from . import product_template_attribute_exclusion
+from . import product_template_attribute_value
 from . import res_company
 from . import res_config_settings
 from . import res_country_group
---
+++ addons/product/models/product_template_attribute_exclusion.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class ProductTemplateAttributeExclusion(models.Model):
+    _name = "product.template.attribute.exclusion"
+    _description = 'Product Template Attribute Exclusion'
+    _order = 'product_tmpl_id, id'
+
+    product_template_attribute_value_id = fields.Many2one(
+        'product.template.attribute.value', string="Attribute Value", ondelete='cascade', index=True)
+    product_tmpl_id = fields.Many2one(
+        'product.template', string='Product Template', ondelete='cascade', required=True, index=True)
+    value_ids = fields.Many2many(
+        'product.template.attribute.value', relation="product_attr_exclusion_value_ids_rel",
+        string='Attribute Values', domain="[('product_tmpl_id', '=', product_tmpl_id), ('ptav_active', '=', True)]")
