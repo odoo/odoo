c8ea9f9f2e3c04922af0d5494c0b5ad648b95c82
Claire Bretton (clbr) <clbr@odoo.com>
2023-02-21 17:03:32 +0000

    [FIX] account, l10n_ch: enable new taxes after module update
    Swiss taxes retrieved by module update (and the update of taxes it triggers)
    need to be active, even if the template data was set to inactive.
    
    closes odoo/odoo#113001
    
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- addons/l10n_ch/migrations/11.1/end-migrate_update_taxes.py
+++ addons/l10n_ch/migrations/11.1/end-migrate_update_taxes.py
@@ -1,6 +1,10 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, SUPERUSER_ID
 from odoo.addons.account.models.chart_template import update_taxes_from_templates
 
 
 def migrate(cr, version):
-    update_taxes_from_templates(cr, 'l10n_ch.l10nch_chart_template')
+    new_template_to_tax = update_taxes_from_templates(cr, 'l10n_ch.l10nch_chart_template')
+    _, new_tax_ids = zip(*new_template_to_tax)
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['account.tax'].browse(new_tax_ids).active = True
