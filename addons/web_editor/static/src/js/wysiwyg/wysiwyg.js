0e079a38e844847438846910d1b74d459be824a4
vishal padhiyar <visp@odoo.com>
2023-02-21 10:46:34 +0530

    [FIX] web_editor: image cropping gives traceback
    Before this commit:
    
    The traceback appears after cropping an image and then trying to crop it again.
    
    After this commit:
    
    Cropping an image and then cropping it again won't give us tracebacks anymore.
    
    Task-3134764

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2438,7 +2438,7 @@ const Wysiwyg = Widget.extend({
             this.$editable.find('.o_editable_date_field_linked').removeClass('o_editable_date_field_linked');
         }
         const closestDialog = e.target.closest('.o_dialog, .o_web_editor_dialog');
-        if (e.target.closest('.oe-toolbar') || (closestDialog && closestDialog.querySelector('.o_select_media_dialog, .o_link_dialog'))) {
+        if (e.target.closest('.oe-toolbar') || e.target.closest('.o_we_crop_buttons') || (closestDialog && closestDialog.querySelector('.o_select_media_dialog, .o_link_dialog'))) {
             this._shouldDelayBlur = true;
         } else {
             if (this._pendingBlur && !e.target.closest('.o_wysiwyg_wrapper')) {
