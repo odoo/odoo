f94a6dccc8b078e337b167a9da056238792fa2ff
Nicolas Bayet <nby@odoo.com>
2023-02-21 11:49:03 +0100

    [FIX] web_editor: test equality of stripped values in updateValue
    Since
    https://github.com/odoo/odoo/commit/1b8d663cdccced35fd294ef3481aa7ce294880f0,
    in `updateValue`, the `value` and `lastValue` could have different
    `data-last-history-steps` and still be the same value.
    
    This commit fixes the method to compare the values stripped of their
    history ids.
    
    opw-3134569

--- addons/web_editor/static/src/js/backend/html_field.js
+++ addons/web_editor/static/src/js/backend/html_field.js
@@ -278,7 +278,11 @@ export class HtmlField extends Component {
     async updateValue() {
         const value = this.getEditingValue();
         const lastValue = (this.props.value || "").toString();
-        if (value !== null && !(!lastValue && stripHistoryIds(value) === "<p><br></p>") && value !== lastValue) {
+        if (
+            value !== null &&
+            !(!lastValue && stripHistoryIds(value) === "<p><br></p>") &&
+            stripHistoryIds(value) !== stripHistoryIds(lastValue)
+        ) {
             this.props.setDirty(false);
             this.currentEditingValue = value;
             await this.props.update(value);
