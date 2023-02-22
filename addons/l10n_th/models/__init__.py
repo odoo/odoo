c51f06fb85a9e9e08fbd01b3a679e2a4ba5e63e7
nni-odoo <nni@odoo.com>
2023-02-22 06:21:01 +0000

    [IMP] l10n_th: Tax Invoice
    When an invoice is issued in Thailand, the Revenue department will make a note that taxes need to be paid. This is important for both corporate tax and VAT purposes. A tax invoice is important to be generated and should contain the title "Tax Invoice".
    On top of that, the tax invoice should also contain the information on supplier's tax id number (headquarters or branch number). In this case, we set the original Odoo invoice to be labelled Tax Invoice and create another invoice called commercial invoice.
    
    2879718
    
    X-original-commit: 1cdaa362c9f0377a7ec4929c2f581ccd84ae3b33

--- addons/l10n_th/models/__init__.py
+++ addons/l10n_th/models/__init__.py
@@ -1,2 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import template_th
+from . import res_partner
+from . import account_move
+from . import ir_actions_report
