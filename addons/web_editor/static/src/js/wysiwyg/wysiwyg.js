6760975e827249754a84caa668354cef4e747aeb
David Monjoie <dmo@odoo.com>
2023-02-22 10:40:19 +0000

    [FIX] web_editor: better history divergence regexp
    Using a lazy `?` quantifier lead to poor matching performances and
    was not needed technically.
    
    Using `*` instead of `+` lead to a lot of false positives when trying
    to write records with an empty value for `data-last-history-steps`.
    
    It is currently unclear how to reach such a case but in the meantime
    it is better to allow people to write the record as such rather than
    being completely locked with no possibility of saving.
    
    closes odoo/odoo#113540
    
    X-original-commit: 0a370758972e6a0c399471f9982f3898da92ef68
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Co-authored-by: Olivier Dony <odo@openerp.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2522,7 +2522,7 @@ const Wysiwyg = Widget.extend({
         }
     },
     _getInitialHistoryId: function (value) {
-        const matchId = value.match(/data-last-history-steps="([0-9,]*?)"/);
+        const matchId = value.match(/data-last-history-steps="([0-9,]+)"/);
         return matchId && matchId[1];
     },
     /**
