f1477ab8f39d3aec96f5c0dcf84eb1298dd8c189
roen-odoo <roen@odoo.com>
2023-02-22 16:49:19 +0100

    [FIX] point_of_sale: correctly round total due in payment screen
    Current behavior:
    When you make a purchase that should be rounded down, for example 1.97
    rounded down to 1.95. On the payment screen if you select a payment
    method and pay with a greater amount than the base amount. The total due
    would be rounded incorrectly to 2.00 instead of 1.95.
    
    Steps to reproduce:
    - Create a product with a price of 1.97
    - Set a rouding down method of 0.05
    - Open a pos session
    - Add the product to the order
    - Select a payment method, for example cash and enter an amount greater
      than the base amount, for example 5.00
    - The total due will be 2.00 instead of 1.95
    
    opw-3175726

--- addons/point_of_sale/static/src/js/models.js
+++ addons/point_of_sale/static/src/js/models.js
@@ -3088,7 +3088,7 @@ class Order extends PosModel {
                 var remaining = this.get_total_with_tax() - this.get_total_paid();
                 var sign = this.get_total_with_tax() > 0 ? 1.0 : -1.0;
                 if(this.get_total_with_tax() < 0 && remaining > 0 || this.get_total_with_tax() > 0 && remaining < 0) {
-                    rounding_method = rounding_method.endsWith("UP") ? "DOWN" : rounding_method;
+                    rounding_method = rounding_method.endsWith("UP") ? "DOWN" : "UP";
                 }
 
                 remaining *= sign;
