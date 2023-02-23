e13447fd4d01f654f70a6b87d9dd897e95985f13
diagnoza <owi@odoo.com>
2023-02-23 17:08:22 +0000

    [FIX] l10n_de_skr04: fixup
    closes odoo/odoo#113663
    
    X-original-commit: 7f52c41ad882d1bcfcde5ba99c05f9a727fe5d02
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- addons/l10n_de_skr04/migrations/3.1/post-migrate_update_taxes.py
+++ addons/l10n_de_skr04/migrations/3.1/post-migrate_update_taxes.py
@@ -3,4 +3,4 @@ from odoo.addons.account.models.chart_template import update_taxes_from_template
 
 
 def migrate(cr, version):
-    update_taxes_from_templates(cr, 'l10n_de_skr04.l10n_de_chart_template')
+    update_taxes_from_templates(cr, 'l10n_de_skr04.l10n_chart_de_skr04')
