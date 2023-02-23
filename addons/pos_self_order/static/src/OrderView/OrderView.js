2d89215b8b39573c8ac99dcfa34c558b7ed0c13e
vlst <vlst@odoo.com>
2023-02-23 11:52:54 +0100

    m

--- addons/pos_self_order/static/src/OrderView/OrderView.js
+++ addons/pos_self_order/static/src/OrderView/OrderView.js
@@ -4,7 +4,6 @@ const { Component } = owl;
 import { NavBar } from "../NavBar/NavBar.js";
 import { formatMonetary } from "@web/views/fields/formatters";
 import { useSelfOrder } from "@pos_self_order/SelfOrderService";
-// import { session } from "@web/session.js";
 export class OrderView extends Component {
     setup() {
         this.formatMonetary = formatMonetary;
