1ad1155b51fc22a0ef0c4d9c4daa306aa978d972
diagnoza <owi@odoo.com>
2023-02-23 17:08:22 +0000

    [FIX] l10n_de_skr04: fixup
    X-original-commit: 7f52c41ad882d1bcfcde5ba99c05f9a727fe5d02

--- addons/l10n_no/migrations/2.1/post-migrate_update_taxes.py
+++ addons/l10n_no/migrations/2.1/post-migrate_update_taxes.py
@@ -3,4 +3,10 @@ from odoo.addons.account.models.chart_template import update_taxes_from_template
 
 
 def migrate(cr, version):
+<<<<<<< HEAD:addons/l10n_no/migrations/2.1/post-migrate_update_taxes.py
     update_taxes_from_templates(cr, 'l10n_no.no_chart_template')
+||||||| parent of 39472103027 (temp):addons/l10n_de_skr04/migrations/3.1/post-migrate_update_taxes.py
+    update_taxes_from_templates(cr, 'l10n_de_skr04.l10n_de_chart_template')
+=======
+    update_taxes_from_templates(cr, 'l10n_de_skr04.l10n_chart_de_skr04')
+>>>>>>> 39472103027 (temp):addons/l10n_de_skr04/migrations/3.1/post-migrate_update_taxes.py
