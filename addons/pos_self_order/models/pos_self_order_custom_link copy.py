7f67907fda29c40bd5d01a643c60d81b489037f6
vlst <vlst@odoo.com>
2023-02-24 16:46:28 +0100

    - self ordering station --> self ordering kiosk

---
+++ addons/pos_self_order/models/pos_self_order_custom_link copy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+# TODO: make it so you can start typing the name of the link w\o having to click on the 'NEW' button first
+class PosSelfOrderCustomLink(models.Model):
+    _name = 'pos_self_order.custom_link'
+    _description = 'Custom links that the restaurant can configure to be displayed on the self order screen'
+    url =  fields.Char(string='URL', required=True)
+    name = fields.Char(string='Label', required=True)
+    pos_config_id = fields.Many2many('pos.config', string='Pos Config')
+
+    
