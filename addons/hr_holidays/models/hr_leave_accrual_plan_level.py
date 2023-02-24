75519cde15149b865f9ba5a44f2e599bfe72d775
Jurgen (jugj) <jugj@odoo.com>
2023-02-24 13:51:01 +0000

    [FIX] hr_holidays : Fix accrual tooltip
    task - 3203843
    
    closes odoo/odoo#113655
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_holidays/models/hr_leave_accrual_plan_level.py
+++ addons/hr_holidays/models/hr_leave_accrual_plan_level.py
@@ -31,7 +31,7 @@ class AccrualPlanLevel(models.Model):
     accrual_plan_id = fields.Many2one('hr.leave.accrual.plan', "Accrual Plan", required=True)
     start_count = fields.Integer(
         "Start after",
-        help="The accrual starts after a defined period from the employee start date. This field defines the number of days, months or years after which accrual is used.", default="1")
+        help="The accrual starts after a defined period from the allocation start date. This field defines the number of days, months or years after which accrual is used.", default="1")
     start_type = fields.Selection(
         [('day', 'day(s)'),
          ('month', 'month(s)'),
