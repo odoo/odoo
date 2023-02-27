6ea5cbdae743ed2657863d28f4a03f76a3231355
Romain Estievenart <res@odoo.com>
2023-02-27 13:10:03 +0000

    [IMP] web,mail: many2one avatar quick assign
    Purpose:
    
    Allow quick assignation of records in the kanban views (and other places
    by extension).
    
    Currently, if you want to work in a board-like env (e.g. tasks in
    kanban, crm in pipeline, that kind of thing), having the possibility to
    quickly assign/unassign records without having to open them fullscreen
    can be really useful => it should be possible to "quick edit" these
    fields in kanban views easily, without switching to form views.
    
    If you want to keep the field readonly don't forget to add the readonly
    attribute.
    
    Task-id: 3121207
    Part-of: odoo/odoo#114400

--- addons/web/static/src/views/fields/many2one_avatar/many2one_avatar_field.js
+++ addons/web/static/src/views/fields/many2one_avatar/many2one_avatar_field.js
@@ -1,9 +1,11 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
+import { isMobileOS } from "@web/core/browser/feature_detection";
+import { usePopover } from "@web/core/popover/popover_hook";
 import { many2OneField, Many2OneField } from "../many2one/many2one_field";
 
-import { Component } from "@odoo/owl";
+import { Component, onMounted } from "@odoo/owl";
 
 export class Many2OneAvatarField extends Component {
     static template = "web.Many2OneAvatarField";
@@ -31,4 +33,87 @@ export const many2OneAvatarField = {
     component: Many2OneAvatarField,
 };
 
+export class Many2OneFieldPopover extends Many2OneField {
+    static props = {
+        ...Many2OneField.props,
+        close: { type: Function },
+    };
+    setup() {
+        super.setup();
+        onMounted(() => this.focusInput());
+    }
+
+    async updateRecord(value) {
+        const updatedValue = await super.updateRecord(...arguments);
+        await this.props.record.save();
+        return updatedValue;
+    }
+}
+
+export class KanbanMany2OneAvatarField extends Many2OneAvatarField {
+    static template = "web.KanbanMany2OneAvatarField";
+    setup() {
+        super.setup();
+        this.popover = usePopover();
+    }
+
+    closePopover() {
+        this.closePopoverFn();
+        this.closePopoverFn = null;
+    }
+    get popoverProps() {
+        return {
+            ...this.props,
+            readonly: false,
+        };
+    }
+    get isFieldReadonly() {
+        return this.props.record.isReadonly(this.props.name);
+    }
+    get many2OneProps() {
+        return {
+            ...super.many2OneProps,
+            readonly: this.isFieldReadonly,
+        };
+    }
+    openPopover(ev) {
+        if (this.isFieldReadonly) {
+            return;
+        }
+        if (this.closePopoverFn) {
+            this.closePopover();
+        }
+        this.closePopoverFn = this.popover.add(
+            ev.currentTarget,
+            Many2OneFieldPopover,
+            {
+                ...this.popoverProps,
+                canCreate: false,
+                canCreateEdit: false,
+                canQuickCreate: false,
+            },
+            {
+                position: "bottom",
+            }
+        );
+    }
+
+    get canDisplayDelete() {
+        return !this.isFieldReadonly && this.props.record.data[this.props.name] && !isMobileOS();
+    }
+    async remove(ev) {
+        if (this.isFieldReadonly) {
+            return;
+        }
+        await this.props.record.update({ [this.props.name]: false });
+        await this.props.record.save();
+    }
+}
+
+export const kanbanMany2OneAvatarField = {
+    ...many2OneField,
+    component: KanbanMany2OneAvatarField,
+    additionalClasses: ["o_field_many2one_avatar_kanban"],
+};
 registry.category("fields").add("many2one_avatar", many2OneAvatarField);
+registry.category("fields").add("kanban.many2one_avatar", kanbanMany2OneAvatarField);
