3675169effe37532210c6616feb1bed428127e90
Romain Estievenart <res@odoo.com>
2023-02-27 14:10:03 +0100

    [IMP] web,mail: many2one avatar quick assign
    Purpose:
    
    Allow quick assignation of records in the kanban views (and other places
    by extension).
    
    Currently, if you want to work in a board-like env (e.g. tasks in
    kanban, crm in pipeline, that kind of thing), having the possibility to
    quickly assign/unassign records without having to open them fullscreen
    can be really useful => it should be possible to "quick edit" these
    fields in kanban views easily, without switching to form views.
    
    If you want to keep the field readonly don't forget to add the readonly
    attribute.
    
    task-ID: 3121207

--- addons/mail/static/src/views/fields/many2one_avatar_user_field/many2one_avatar_user_field.js
+++ addons/mail/static/src/views/fields/many2one_avatar_user_field/many2one_avatar_user_field.js
@@ -4,29 +4,35 @@ import { registry } from "@web/core/registry";
 import {
     Many2OneAvatarField,
     many2OneAvatarField,
+    KanbanMany2OneAvatarField,
+    kanbanMany2OneAvatarField,
 } from "@web/views/fields/many2one_avatar/many2one_avatar_field";
 import { useOpenChat } from "@mail/views/open_chat_hook";
 import { useAssignUserCommand } from "@mail/views/fields/assign_user_command_hook";
+import { patch } from "@web/core/utils/patch";
 
-export class Many2OneAvatarUserField extends Many2OneAvatarField {
-    static props = {
-        ...Many2OneAvatarField.props,
-        withCommand: { type: Boolean, optional: true },
-    };
-
+const userChatter = {
     setup() {
-        super.setup();
+        this._super(...arguments);
         this.openChat = useOpenChat(this.relation);
         if (this.props.withCommand) {
             useAssignUserCommand();
         }
-    }
+    },
 
     onClickAvatar() {
         this.openChat(this.props.record.data[this.props.name][0]);
-    }
+    },
+};
+
+export class Many2OneAvatarUserField extends Many2OneAvatarField {
+    static template = "mail.Many2OneAvatarUserField";
+    static props = {
+        ...Many2OneAvatarField.props,
+        withCommand: { type: Boolean, optional: true },
+    };
 }
-Many2OneAvatarUserField.template = "mail.Many2OneAvatarUserField";
+patch(Many2OneAvatarUserField.prototype, "mail", userChatter);
 
 export const many2OneAvatarUserField = {
     ...many2OneAvatarField,
@@ -40,18 +46,39 @@ export const many2OneAvatarUserField = {
 
 registry.category("fields").add("many2one_avatar_user", many2OneAvatarUserField);
 
-export class KanbanMany2OneAvatarUserField extends Many2OneAvatarUserField {}
-KanbanMany2OneAvatarUserField.template = "mail.KanbanMany2OneAvatarUserField";
-KanbanMany2OneAvatarUserField.props = {
-    ...Many2OneAvatarUserField.props,
-    displayAvatarName: { type: Boolean, optional: true },
-};
+export class KanbanMany2OneAvatarUserField extends KanbanMany2OneAvatarField {
+    static template = "mail.KanbanMany2OneAvatarUserField";
+    static props = {
+        ...KanbanMany2OneAvatarField.props,
+        displayAvatarName: { type: Boolean, optional: true },
+    };
+    /**
+     * All props are normally passed to the Many2OneField however since
+     * we add a new one, we need to filter it out.
+     */
+    get m2oFieldProps() {
+        const props = {
+            ...this.props,
+        };
+        delete props.displayAvatarName;
+        return props;
+    }
+
+    get popoverProps() {
+        return {
+            ...this.m2oFieldProps,
+            readonly: false,
+        };
+    }
+}
+patch(KanbanMany2OneAvatarUserField.prototype, "mail", userChatter);
 
 export const kanbanMany2OneAvatarUserField = {
-    ...many2OneAvatarUserField,
+    ...kanbanMany2OneAvatarField,
     component: KanbanMany2OneAvatarUserField,
+    additionalClasses: [...kanbanMany2OneAvatarField.additionalClasses, "o_field_many2one_avatar"],
     extractProps: (fieldInfo) => ({
-        ...many2OneAvatarUserField.extractProps(fieldInfo),
+        ...kanbanMany2OneAvatarField.extractProps(fieldInfo),
         displayAvatarName: fieldInfo.options.display_avatar_name || false,
     }),
 };
