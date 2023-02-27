541e0d7f7996b41d0f8ab2ecbf7dfcb84c754aa7
sofiagvaladze <sgv@odoo.com>
2023-02-27 09:50:12 +0100

    [IMP] hr_holidays: allow quick approval on dashboard
    Purpose: HR can view employee leave records from the calendar but can
    not approve, validate or refuse it from there.
    In case they want to make action, they have to go through
    menu < time off < approvals < time off < find the employee
    < find the requested time off < open it < approve.
    
    In this commit the action buttons are added on the dashboard.
    
    task - 3175495

--- addons/hr_holidays/static/src/views/view_dialog/form_view_dialog.js
+++ addons/hr_holidays/static/src/views/view_dialog/form_view_dialog.js
@@ -3,6 +3,7 @@
 import { FormViewDialog } from "@web/views/view_dialogs/form_view_dialog";
 
 import { registry } from '@web/core/registry';
+import { useService } from "@web/core/utils/hooks";
 
 import { formView } from '@web/views/form/form_view';
 import { FormController } from '@web/views/form/form_controller';
@@ -13,23 +14,43 @@ export class TimeOffDialogFormController extends FormController {
     setup() {
         super.setup();
         this.leaveCancelWizard = useLeaveCancelWizard();
+        this.orm = useService("orm");
+    }
+
+    get record() {
+        return this.model.root.data;
+    }
+
+    async onClick(action) {
+        const args = (action === 'action_approve') ? [this.record.id, false] : [this.record.id];
+        await this.orm.call("hr.leave", action, args);
+        this.props.onLeaveUpdated();
+    }
+
+    get canApprove() {
+        return !this.model.root.isNew && (this.record.can_approve && ['confirm', 'refuse',].includes(this.record.state));
+    }
+
+    get canValidate() {
+        return !this.model.root.isNew && (this.record.can_approve && this.record.state === 'validate1');
+    }
+
+    get canRefuse() {
+        return !this.model.root.isNew && (this.record.can_approve && this.record.state && ['confirm', 'validate1', 'validate'].includes(this.record.state));
     }
 
     deleteRecord() {
-        const record = this.model.root.data
-
-        this.props.onRecordDeleted(record)
+        this.props.onRecordDeleted(this.record)
         this.props.onCancelLeave();
-        if (record.can_cancel) {
-            this.leaveCancelWizard(record.id, () => {
+        if (this.record.can_cancel) {
+            this.leaveCancelWizard(this.record.id, () => {
                 this.props.onLeaveCancelled();
             });
         }
     }
 
     get canDelete() {
-        const record = this.model.root.data;
-        return !this.model.root.isNew && (record.can_cancel || record.state && !['validate', 'refuse'].includes(record.state));
+        return !this.model.root.isNew && (this.record.can_cancel || this.record.state && !['validate', 'refuse'].includes(this.record.state));
     }
 }
 
@@ -38,6 +59,7 @@ TimeOffDialogFormController.props = {
     onCancelLeave: Function,
     onRecordDeleted: Function,
     onLeaveCancelled: Function,
+    onLeaveUpdated: Function,
 }
 
 registry.category('views').add('timeoff_dialog_form', {
@@ -54,6 +76,10 @@ export class TimeOffFormViewDialog extends FormViewDialog {
             type: "timeoff_dialog_form",
             buttonTemplate: 'hr_holidays.FormViewDialog.buttons',
             onCancelLeave: () => {
+                this.props.close();
+            },
+            onLeaveUpdated: () => {
+                this.props.onRecordSaved();
                 this.props.close();
             },
             onRecordDeleted: (record) => {
