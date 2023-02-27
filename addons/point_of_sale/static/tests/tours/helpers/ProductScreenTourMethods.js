a07d671bfb64976814ba3475f1181e047e91a3f2
roen-odoo <roen@odoo.com>
2023-02-27 13:20:14 +0100

    [FIX] point_of_sale: stop showing changes of fiscal position as discount
    Current behavior:
    When a fiscal position was applied in the PoS, it was considered as a
    discount and was shown in the receipt and on the product screen.
    
    Steps to reproduce:
    - Create a product with a price of 10€ with 15% tax included in the
      price
    - Create a fiscal position that map the 15% tax to 0% tax
    - Open a PoS session
    - Add the product to the order
    - Apply the fiscal position
    - The price of the product is now 8.70€ but the order show it as a
      discount of 1.30€ (e.g 10€ -> 8.70€).
    
    opw-3154499

--- addons/point_of_sale/static/tests/tours/helpers/ProductScreenTourMethods.js
+++ addons/point_of_sale/static/tests/tours/helpers/ProductScreenTourMethods.js
@@ -123,6 +123,18 @@ odoo.define('point_of_sale.tour.ProductScreenTourMethods', function (require) {
                 },
             ];
         }
+        changeFiscalPosition(name) {
+            return [
+                {
+                    content: 'click fiscal position button',
+                    trigger: '.o_fiscal_position_button',
+                },
+                {
+                    content: 'fiscal position screen is shown',
+                    trigger: `.selection-item:contains("${name}")`,
+                },
+            ];
+        }
     }
 
     class Check {
@@ -199,6 +211,14 @@ odoo.define('point_of_sale.tour.ProductScreenTourMethods', function (require) {
                     content: `'${mode}' is active`,
                     trigger: `.numpad button.selected-mode:contains('${mode}')`,
                     run: function () {},
+                },
+            ];
+        }
+        noDiscountApplied(originalPrice) {
+            return [
+                {
+                    content: 'no discount is applied',
+                    trigger: `.info:not(:contains(${originalPrice}))`,
                 },
             ];
         }
