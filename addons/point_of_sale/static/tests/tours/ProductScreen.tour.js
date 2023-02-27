742ab9fc0afbd28e835f7fc02d51d32c06459cdd
roen-odoo <roen@odoo.com>
2023-02-27 12:20:14 +0000

    [FIX] point_of_sale: stop showing changes of fiscal position as discount
    Current behavior:
    When a fiscal position was applied in the PoS, it was considered as a
    discount and was shown in the receipt and on the product screen.
    
    Steps to reproduce:
    - Create a product with a price of 10€ with 15% tax included in the
      price
    - Create a fiscal position that map the 15% tax to 0% tax
    - Open a PoS session
    - Add the product to the order
    - Apply the fiscal position
    - The price of the product is now 8.70€ but the order show it as a
      discount of 1.30€ (e.g 10€ -> 8.70€).
    
    opw-3154499
    
    X-original-commit: b93f6a93b2881ab2331b42934ed60ffc0e1a6903

--- addons/point_of_sale/static/tests/tours/ProductScreen.tour.js
+++ addons/point_of_sale/static/tests/tours/ProductScreen.tour.js
@@ -133,6 +133,18 @@ Tour.register("ProductScreenTour", { test: true, url: "/pos/ui" }, getSteps());
 
 startSteps();
 
+ProductScreen.do.clickHomeCategory();
+ProductScreen.do.clickDisplayedProduct('Test Product');
+ProductScreen.check.totalAmountIs('100.00');
+ProductScreen.do.changeFiscalPosition('No Tax');
+ProductScreen.check.noDiscountApplied("100.00");
+ProductScreen.check.totalAmountIs('86.96');
+
+Tour.register('FiscalPositionNoTax', { test: true, url: '/pos/ui' }, getSteps());
+
+
+startSteps();
+
 ProductScreen.do.enterOpeningAmount('90');
 ProductScreen.do.confirmOpeningPopup();
 ProductScreen.check.checkSecondCashClosingDetailsLineAmount('10.00', '-');
