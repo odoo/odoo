c9b7c43c69eaf87e7900801b926d3868adc36d8c
Joseph Caburnay <jcb@odoo.com>
2023-02-28 10:35:53 +0000

    [FIX] web: auto save record when changing value of 'toggle' fields
    Before 16.0 and the "always edit" form views, some fields allowed to
    be edited in readonly (e.g. the statusbar in crm lead or project
    task). Users could thus change the stage of a record in readonly and
    the change was saved directly. If the field was tracked, the change
    was even logged in the chatter directly. Since the form view is always
    in edition now, we loose that behavior and the user must click on the
    save cloud icon to manually save and see the tracking messages. To
    mitigate this, those fields that were editable in readonly could save
    the record directly when edited, like buttons do.
    
    Changing the values of the following fields should automatically save
    the record.
    
    - BooleanToggleField
    - PriorityField
    - StateSelectionField
    - StatusBarField
    
    To accomplish this, we now unconditionally save the record when the above
    mentioned fields are changed.
    
    closes odoo/odoo#115173
    
    Task-id: 3175672
    X-original-commit: bbbe1d42a65d4f1c8001bbe14ce6b8a209081bc6
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/views/fields/boolean_toggle/boolean_toggle_field.js
+++ addons/web/static/src/views/fields/boolean_toggle/boolean_toggle_field.js
@@ -13,14 +13,7 @@ export class BooleanToggleField extends BooleanField {
 
     async onChange(newValue) {
         await this.props.record.update({ [this.props.name]: newValue });
-        const rootRecord =
-            this.props.record.model.root instanceof this.props.record.constructor &&
-            this.props.record.model.root;
-        const isInEdition = rootRecord ? rootRecord.isInEdition : this.props.record.isInEdition;
-        // We save only if we're on view mode readonly and no readonly field modifier
-        if (!isInEdition) {
-            return this.props.record.save();
-        }
+        return this.props.record.save();
     }
 }
 
