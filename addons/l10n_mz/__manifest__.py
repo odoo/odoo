49e2a5b995796608e8c446cf2367f827a8849ecb
moerradi <moer@odoo.com>
2023-03-02 17:53:55 +0000

    [ADD] l10n_mz: add COA, taxes and tax report
    Add chart of accounts, taxes and tax report for Mozambican localization
    
    closes odoo/odoo#115710
    
    Task-id: 3162293
    X-original-commit: a794b29d84eefc91c037112b44293698c7db4d49
    Related: odoo/enterprise#38348
    Signed-off-by: Quentin De Paoli <qdp@odoo.com>
    Signed-off-by: Erradi Mohammed (moer) <moer@odoo.com>

---
+++ addons/l10n_mz/__manifest__.py
@@ -0,0 +1,20 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Mozambique - Accounting',
+    'description': """
+        Mozambican Accounting localization
+    """,
+    'version': '1.0',
+    'category': 'Accounting/Localizations/Account Charts',
+    'depends': [
+        'base',
+        'account',
+    ],
+    'data': [
+        'data/tax_report.xml',
+    ],
+    'demo': [
+        'demo/demo_company.xml',
+    ],
+    'license': 'LGPL-3',
+}
