0171f4a1e4a0cb9b0171e329e937385e957dd3bb
yhu-odoo <yhu@odoo.com>
2023-03-02 14:39:23 +0000

    [FIX] {,purchase_}mrp: add missing days when calculate DTPMO
    When calculate Days to Prepare Manufacturing Order, we didn't take into
    account Days to Purchase and Security Lead times for Purchaseing of the
    company. In this commmit, if the (sub-)bom has company_id set, these two
    days will added to the calculation.
    
    Task-3078049
    
    X-original-commit: bf59aedad649dec350f091bb074d3ef1e51d5fcb
    Part-of: odoo/odoo#121813

--- addons/mrp/models/mrp_bom.py
+++ addons/mrp/models/mrp_bom.py
@@ -87,8 +87,8 @@ class MrpBom(models.Model):
         help="Average lead time in days to manufacture this product. In the case of multi-level BOM, the manufacturing lead times of the components will be added. In case the product is subcontracted, this can be used to determine the date at which components should be sent to the subcontractor.")
     days_to_prepare_mo = fields.Float(
         string="Days to prepare Manufacturing Order", default=0.0,
-        help="Create and confirm Manufacturing Orders these many days in advance, to have enough time to replenish components or manufacture semi-finished products.\n"
-             "Note that this does not affect the MO scheduled date, which still respects the just-in-time mechanism.")
+        help="Create and confirm Manufacturing Orders this many days in advance, to have enough time to replenish components or manufacture semi-finished products.\n"
+             "Note that security lead times will also be considered when appropriate.")
 
     _sql_constraints = [
         ('qty_positive', 'check (product_qty > 0)', 'The quantity to produce must be positive!'),
