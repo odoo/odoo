051dc0b63fca58fbb0136a2fd9526a00b5e607a0
william-andre <wan@odoo.com>
2023-03-02 13:00:12 +0100

    [REF] l10n_*: clean code
    A lot of the separate files were kept up to now because it made the
    process easier while applying the script to rebase.
    The files can now be merged.
    
    Some code is also cleaned by using CSV instead of a python dict.

--- addons/l10n_ae/models/__init__.py
+++ addons/l10n_ae/models/__init__.py
@@ -1,4 +1,3 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import template_ae
 from . import account_move_line
-from . import account_chart_template
--- addons/l10n_ae/models/template_ae.py
+++ addons/l10n_ae/models/template_ae.py
@@ -1,5 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models
+from odoo import models, Command
 from odoo.addons.account.models.chart_template import template
 
 
@@ -29,5 +29,39 @@ class AccountChartTemplate(models.AbstractModel):
                 'expense_currency_exchange_account_id': 'uae_account_400053',
                 'account_journal_early_pay_discount_loss_account_id': 'uae_account_400071',
                 'account_journal_early_pay_discount_gain_account_id': 'uae_account_500014',
+            },
+        }
+
+    @template('ae', 'account.journal')
+    def _get_ae_account_journal(self):
+        """ If UAE chart, we add 2 new journals TA and IFRS"""
+        return {
+            "tax_adjustment":{
+                "name": "Tax Adjustments",
+                "code": "TA",
+                "type": "general",
+                "show_on_dashboard": True,
+                "sequence": 1,
+            },
+            "ifrs16": {
+                "name": "IFRS 16",
+                "code": "IFRS",
+                "type": "general",
+                "show_on_dashboard": True,
+                "sequence": 10,
+            }
+        }
+
+    @template('ae', 'account.account')
+    def _get_ae_account_account(self):
+        return {
+            "uae_account_100101": {
+                'allowed_journal_ids': [Command.link('ifrs16')],
+            },
+            "uae_account_100102": {
+                'allowed_journal_ids': [Command.link('ifrs16')],
+            },
+            "uae_account_400070": {
+                'allowed_journal_ids': [Command.link('ifrs16')],
             },
         }
