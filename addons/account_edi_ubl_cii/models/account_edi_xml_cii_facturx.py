79f46cdba23715203ed218a36664d30927e45e7a
Julien Van Roy <juvr@odoo.com>
2023-03-03 09:31:58 +0000

    [FIX] account_edi_ubl_cii: change mapping for Buyer/Order Reference
    The BuyerReference (BT-10) should be easily editable by the user, so it is read
    from the `commercial_partner_id.ref`.
    
    The OrderReference (BT-13) should also be editable by the user, it is read from
    the `move.ref`.
    
    The definition for both tags in the peppol doc defines these tags as:
    "An identifier assigned by the Buyer used for internal routing
    purposes".
    
    The new tag SalesOrderId (BT-14) is added, and is the sale order's name
    linked to the invoice.
    
    opw-3175906
    
    closes odoo/odoo#114291
    
    Signed-off-by: Laurent Smet <las@odoo.com>

--- addons/account_edi_ubl_cii/models/account_edi_xml_cii_facturx.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_cii_facturx.py
@@ -155,9 +155,9 @@ class AccountEdiXmlCII(models.AbstractModel):
                 else invoice.commercial_partner_id,
             # Chorus Pro fields
             'buyer_reference': invoice.buyer_reference if 'buyer_reference' in invoice._fields
-                and invoice.buyer_reference else invoice.ref,
+                and invoice.buyer_reference else invoice.commercial_partner_id.ref,
             'purchase_order_reference': invoice.purchase_order_reference if 'purchase_order_reference' in invoice._fields
-                and invoice.purchase_order_reference else invoice.payment_reference or invoice.name,
+                and invoice.purchase_order_reference else invoice.ref or invoice.name,
             'contract_reference': invoice.contract_reference if 'contract_reference' in invoice._fields
                 and invoice.contract_reference else '',
         }
