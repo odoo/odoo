abd9c7defbb3622c4ebdb01129ae69809eb36c60
niyasraphy <niyasraphyk@gmail.com>
2023-03-03 11:35:12 +0000

    [IMP] core: remove non existing modules from test
    document_ftp, website_instantclick, pad, pad_project, note_pad, pos_cache is not currently existing in the addons, removing the non existing modules from the BLACKLIST dictionary.
    
    closes odoo/odoo#107213
    
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- odoo/tests/test_module_operations.py
+++ odoo/tests/test_module_operations.py
@@ -17,8 +17,8 @@ import odoo.tests.loader
 _logger = logging.getLogger('odoo.tests.test_module_operations')
 
 BLACKLIST = {
-    'auth_ldap', 'document_ftp', 'website_instantclick', 'pad',
-    'pad_project', 'note_pad', 'pos_cache', 'pos_blackbox_be',
+    'auth_ldap',
+    'pos_blackbox_be',
 }
 IGNORE = ('hw_', 'theme_', 'l10n_', 'test_')
 
