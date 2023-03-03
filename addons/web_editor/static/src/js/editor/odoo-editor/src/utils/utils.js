dacc8fd8fa4c8ca1486b4dbb6dbc74359f816725
dhba-odoo <dhba@odoo.com>
2023-03-03 07:26:43 +0000

    [FIX] web_editor: shift+enter on styled elements.
    Before this commit:
    
    After selecting the text of shift+enter which is styled and pressing any key
    the new hideous child gets born.
    
    After this commit:
    
    Now, The new input will replace the selected text.
    
    Task-3209969
    
    X-original-commit: 56a57f7c26b4c0598840b82fab909d21113aa572

--- addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
@@ -1896,12 +1896,7 @@ export function fillEmpty(el) {
         blockEl.appendChild(br);
         fillers.br = br;
     }
-    if (
-        !el.textContent.length &&
-        !isBlock(el) &&
-        el.nodeName !== 'BR' &&
-        !el.hasAttribute("data-oe-zws-empty-inline")
-    ) {
+    if (!isVisible(el) && !el.hasAttribute("data-oe-zws-empty-inline")) {
         // As soon as there is actual content in the node, the zero-width space
         // is removed by the sanitize function.
         const zws = document.createTextNode('\u200B');
