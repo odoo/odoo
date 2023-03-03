1eba8e676099acedb0ee1b6438f9d6813e2f79b0
yhu-odoo <yhu@odoo.com>
2023-03-03 12:53:29 +0100

    [IMP] mrp_subcontracting{, _dropshipping}: split subcontracting account move
    Considering a subcontracting use case where:
     component cost = 50
     service cost = 100
    
    we post:
    |account,              |debit,         |credit
    -----------------------------------------------
    Receipt Component from vendor
    |Stock Valuation       |50             |
    |Stock Input           |               |50
    -----------------------------------------------
    Invoice Component from vendor
    |Stock Input           |50             |
    |Payable               |               |50
    -----------------------------------------------
    Resupply Component to subcontractor
    |Stock Valuation       |               |50
    |Stock Output          |50             |
    ----------------------------------------------
    Receive Final from subcontractor
    |Stock Valuation       |150            |
    |Stock Input           |               |150
    ----------------------------------------------
    Invoice Final from subcontractor
    |Stock Input           |100            |
    |Payable               |               |100
    -----------------------------------------------
    Deliver Final to client
    |Stock Valuation       |               |150
    |Stock Output          |150            |
    -----------------------------------------------
    Invoice Client
    |Stock Output          |               |150
    |Receivable            |150            |
    
    Stock Input/Output account are not balanced.
    In this commit, we split the credit value when received final from
    subcontractor to make them balanced.
    
    |account,              |debit,         |credit
    -----------------------------------------------
    Receipt Component from vendor
    |Stock Valuation       |50             |
    |Stock Input           |               |50
    -----------------------------------------------
    Invoice Component from vendor
    |Stock Input           |50             |
    |Payable               |               |50
    -----------------------------------------------
    Resupply Component to subcontractor
    |Stock Valuation       |               |50
    |Stock Output          |50             |
    ----------------------------------------------
    Receive Final from subcontractor
    |Stock Valuation       |150            |
    |Stock Input           |               |100
    |Stock Output          |               |50
    ----------------------------------------------
    Invoice Final from subcontractor
    |Stock Input           |100            |
    |Payable               |               |100
    -----------------------------------------------
    Deliver Final to client
    |Stock Valuation       |               |150
    |Stock Output          |150            |
    -----------------------------------------------
    Invoice Client
    |Stock Output          |               |150
    |Receivable            |150            |
    ---

--- addons/mrp_subcontracting_account/models/stock_move.py
+++ addons/mrp_subcontracting_account/models/stock_move.py
@@ -10,3 +10,38 @@ class StockMove(models.Model):
     def _should_force_price_unit(self):
         self.ensure_one()
         return self.is_subcontract or super()._should_force_price_unit()
+
+    def _generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id, svl_id, description):
+        rslt = super()._generate_valuation_lines_data(partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id, svl_id, description)
+
+        subcontract_production = self.production_id.filtered(lambda p: p.subcontractor_id)
+        if not subcontract_production:
+            return rslt
+        # split the credit line to two, one for component cost, one for subcontracting service cost
+        currency = self.company_id.currency_id
+        subcontract_service_cost = currency.round(subcontract_production.extra_cost * qty)
+        if not currency.is_zero(subcontract_service_cost):
+            del rslt['credit_line_vals']
+            component_cost = -credit_value + subcontract_service_cost
+            component_cost_account = self.product_id.product_tmpl_id.get_product_accounts()['stock_output']
+            rslt['subcontract_credit_line_vals'] = {
+                'name': description,
+                'product_id': self.product_id.id,
+                'quantity': qty,
+                'product_uom_id': self.product_id.uom_id.id,
+                'ref': description,
+                'partner_id': partner_id,
+                'balance': -subcontract_service_cost,
+                'account_id': credit_account_id,
+            }
+            rslt['component_credit_line_vals'] = {
+                'name': description,
+                'product_id': self.product_id.id,
+                'quantity': qty,
+                'product_uom_id': self.product_id.uom_id.id,
+                'ref': description,
+                'partner_id': partner_id,
+                'balance': component_cost,
+                'account_id': component_cost_account.id,
+            }
+        return rslt
