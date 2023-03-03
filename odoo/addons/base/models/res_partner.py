f242cad9a27fb0e58e1b9c726573b316534f6be8
pedrambiria <pebr@odoo.com>
2023-03-03 16:26:19 +0000

    [FIX] base: add `commercial_company_name` to the depends
    Before this commit: if you add a custom one2many field to the
    'res.partner', like x_related_commercial_partner_ids, that is related to the
    `commercial_partner_id` in the`res.partner` model, it won't update the
    display name of a contact in case of changing its parent_id name.
    
    Here are the steps to reproduce the problem:
     1. Create a new custom field with these values:
       a. Field Type = one2many
       b. Model = Contact
       c. Related Model = res.partner
       d. Relation Field = commercial_partner_id
     2. Create a new Contact that is the "Company" (e.g. "My Company")
     3. Create a new Contact that is the "Individual" (e.g. "My Name"), and
        put the "My Company" as its parent_id.
     4. Now the display_name is "My Company, My Name" which is correct
     5. Change the company name to "My new Company"
      -> display_name won't change, and is "My Company, My Name"
    
    The solution is to add the 'commercial_company_name' to the
    `display_name` depends.
    
    opw-3202894
    
    closes odoo/odoo#120669
    
    X-original-commit: 6bd2bdf02b95ecf9d64941c83d51683c84528787
    Signed-off-by: Rémy Voet <ryv@odoo.com>

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -278,7 +278,7 @@ class Partner(models.Model):
             return "base/static/img/money.png"
         return super()._avatar_get_placeholder_path()
 
-    @api.depends('is_company', 'name', 'parent_id.display_name', 'type', 'company_name')
+    @api.depends('is_company', 'name', 'parent_id.display_name', 'type', 'company_name', 'commercial_company_name')
     def _compute_display_name(self):
         diff = dict(show_address=None, show_address_only=None, show_email=None, html_format=None, show_vat=None)
         names = dict(self.with_context(**diff).name_get())
