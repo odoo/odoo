ca2cde80c29e5ab43cd78aa147255c5663dc7969
mb-myodoo <mb@myodoo.pl>
2023-03-06 13:54:11 +0100

    [IMP] V16 2.1 l10n_pl updating the chart of accounts removed unused taxes from fiscal position removed unuser taxes form account_tax_data updating taxes with accounting tags under JPK_VAT removed unused taxes group Tax Report line names have been updated

--- addons/l10n_pl/__manifest__.py
+++ addons/l10n_pl/__manifest__.py
@@ -1,23 +1,17 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Copyright (C) 2009 - now Grzegorz Grzelak grzegorz.grzelak@openglobe.pl
-
 {
     'name' : 'Poland - Accounting',
-    'version' : '2.0',
-    'author': 'Odoo S.A., Grzegorz Grzelak (OpenGLOBE)',
-    'website': 'http://www.openglobe.pl',
+    'version' : '2.1',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
 This is the module to manage the accounting chart and taxes for Poland in Odoo.
 ==================================================================================
 
-To jest moduł do tworzenia wzorcowego planu kont, podatków, obszarów podatkowych i
-rejestrów podatkowych. Moduł ustawia też konta do kupna i sprzedaży towarów
-zakładając, że wszystkie towary są w obrocie hurtowym.
+Module adds basic chart of account for Polish accounting, taxes and tags for JPK.
 
-Niniejszy moduł jest przeznaczony dla odoo 8.0.
-Wewnętrzny numer wersji OpenGLOBE 1.02
+Niniejszy moduł jest przeznaczony dla odoo 16.0.
+
     """,
     'depends' : [
         'account',
@@ -25,16 +19,17 @@ Wewnętrzny numer wersji OpenGLOBE 1.02
         'base_vat',
     ],
     'data': [
-              'data/l10n_pl_chart_data.xml',
-              'data/account.account.template.csv',
-              'data/account.group.template.csv',
-              'data/res.country.state.csv',
-              'data/l10n_pl_chart_post_data.xml',
-              'data/account_tax_group_data.xml',
-              'data/account_tax_report_data.xml',
-              'data/account_tax_data.xml',
-              'data/account_fiscal_position_data.xml',
-              'data/account_chart_template_data.xml'
+            'data/accounting_tags.xml',
+            'data/l10n_pl_chart_data.xml',
+            'data/account.account.template.csv',
+            'data/account.group.template.csv',
+            'data/res.country.state.csv',
+            'data/l10n_pl_chart_post_data.xml',
+            'data/account_tax_group_data.xml',
+            'data/account_tax_report_data.xml',
+            'data/account_tax_data.xml',
+            'data/account_fiscal_position_data.xml',
+            'data/account_chart_template_data.xml'
     ],
     'demo': [
         'demo/demo_company.xml',
