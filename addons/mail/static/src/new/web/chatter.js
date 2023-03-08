09b614592abfe2d88a1d4ae2bc0dc7921ab92c9f
Didier (did) <did@odoo.com>
2023-03-08 15:58:48 +0100

    [FIX] mail: avoid attachment counter flicker

--- addons/mail/static/src/new/web/chatter.js
+++ addons/mail/static/src/new/web/chatter.js
@@ -258,8 +258,6 @@ export class Chatter extends Component {
     }
 
     onFollowerChanged() {
-        // TODO condition to reload parent view (message_follower_ids / hasParentReloadOnFollowersUpdate)
-        this.reloadParentView();
         this.load(this.props.threadId, ["followers", "suggestedRecipients"]);
     }
 
