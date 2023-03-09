099b8d9d815f3d691dac0ffaf905a9a7938e80a5
Zelong Lin <zel@odoo.com>
2023-03-09 10:17:42 +0100

    [IMP] mail: boot.js clean up

--- addons/mail/static/src/new/public/boot.js
+++ addons/mail/static/src/new/public/boot.js
@@ -4,15 +4,13 @@ import { data } from "mail.discuss_public_template";
 import { MainComponentsContainer } from "@web/core/main_components_container";
 import { registry } from "@web/core/registry";
 import { makeEnv, startServices } from "@web/env";
-// import { session } from "@web/session";
 import { templates } from "@web/core/assets";
 
 import { mapLegacyEnvToWowlEnv } from "@web/legacy/utils";
-// import * as legacySession from "web.session";
 
 import * as legacyEnv from "web.env";
 import { Component, mount, whenReady } from "@odoo/owl";
-import { DiscussPublic } from "./discuss_public";
+import { DiscussPublic } from "@mail/new/public/discuss_public";
 
 Component.env = legacyEnv;
 
@@ -25,16 +23,6 @@ Component.env = legacyEnv;
         props: { data },
     });
 
-    // await legacySession.is_bound;
-    // Object.assign(odoo, {
-    //     info: {
-    //         db: session.db,
-    //         server_version: session.server_version,
-    //         server_version_info: session.server_version_info,
-    //         isEnterprise: session.server_version_info.slice(-1)[0] === "e",
-    //     },
-    //     isReady: false,
-    // });
     const env = makeEnv();
     await startServices(env);
     env.services["mail.store"].inPublicPage = true;
