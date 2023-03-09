84fb8138b1d19942d54f7814e2d36a4797a9a437
Mylyna Hy <myhy@odoo.com>
2023-03-09 23:20:57 +0000

    [FIX] website_sale_stock: translate restock email
    Problem:
    The email created by _send_availability_email is not translated correctly.
    The body is always sent in English
    and the subject will always use the current language of the database.
    
    Example:
    Partner A's language = Spanish
    Current database language = "French"
    
    The body of the email sent to Partner A will be in English and
    the subject of the email sent will be in French and
    the field attributes of the products will be in English.
    
    Solution:
    Pass in the partner's language into the self's context and the product's context.
    This fix will translate the entire email uniformly according to the context's language.
    
    opw-3207979
    
    closes odoo/odoo#117375
    
    X-original-commit: 8777f86adc3ad382b6948461781fb72b1b068944
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/website_sale_stock/models/product_product.py
+++ addons/website_sale_stock/models/product_product.py
@@ -35,20 +35,25 @@ class ProductProduct(models.Model):
             if product._is_sold_out():
                 continue
             for partner in product.stock_notification_partner_ids:
-                body_html = self.env['ir.qweb']._render('website_sale_stock.availability_email_body', {"product": product})
-                msg = self.env["mail.message"].sudo().new(dict(body=body_html, record_name=product.name))
-                full_mail = self.env["mail.render.mixin"]._render_encapsulate(
+                self_ctxt = self.with_context(lang=partner.lang)
+                product_ctxt = product.with_context(lang=partner.lang)
+                body_html = self_ctxt.env['ir.qweb']._render(
+                    'website_sale_stock.availability_email_body', {'product': product_ctxt})
+                msg = self_ctxt.env['mail.message'].sudo().new(dict(body=body_html, record_name=product_ctxt.name))
+                full_mail = self_ctxt.env['mail.render.mixin']._render_encapsulate(
                     "mail.mail_notification_light",
                     body_html,
                     add_context=dict(message=msg, model_description=_("Product")),
                 )
+                context = {'lang': partner.lang}  # Use partner lang to translate mail subject below
                 mail_values = {
-                    "subject": _("The product '%(product_name)s' is now available") % {'product_name': product.name},
+                    "subject": _("The product '%(product_name)s' is now available", product_name=product_ctxt.name),
                     "email_from": (product.company_id.partner_id or self.env.user).email_formatted,
                     "email_to": partner.email_formatted,
                     "body_html": full_mail,
                 }
+                del context
 
-                mail = self.env["mail.mail"].sudo().create(mail_values)
+                mail = self_ctxt.env['mail.mail'].sudo().create(mail_values)
                 mail.send(raise_exception=False)
                 product.stock_notification_partner_ids -= partner
