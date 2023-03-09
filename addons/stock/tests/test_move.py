6f818d99c325697747f3a6a73a209853741aec9f
svs-odoo <svs@odoo.com>
2023-03-09 13:36:50 +0000

    [FIX] stock: immediate transfer w/ tracked product
    How to reproduce:
    - Create a tracked product;
    - In the receipt picking type form view, for the lot and serial numbers,
      uncheck "Create New" and "Use Existing ones";
    - Create a planned receipt for some of this tracked product;
    - Confirm and validate the receipt:
      It opens the "Immediate Transfer" wizard;
    - Apply the immediate transfer:
      -> It asks if you want to create a backorder.
    
    Since the picking type doesn't use new or existing LN/SN, it accepts to
    be confirmed even if some move lines for tracked product have no
    tracking numbers.
    The issue was, when a `stock.move` sets its done quantity to its
    reserved quantity, if its product is tracked, it doesn't change its done
    quantity if it has no tracking number, regardless the picking type's
    configuration.
    
    OPW-3186155
    
    closes odoo/odoo#115211
    
    X-original-commit: 62078564595d014a17ea7788b173380fef1b6436
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/stock/tests/test_move.py
+++ addons/stock/tests/test_move.py
@@ -4578,6 +4578,32 @@ class StockMove(TransactionCase):
         self.assertEqual(receipt2.state, 'done')
         self.assertEqual(receipt3.state, 'done')
 
+    def test_immediate_validate_9_tracked_move_with_0_qty_done(self):
+        """When trying to validate a picking as an immediate transfer, the done
+        quantity of tracked move should be automatically fulfilled if the
+        picking type doesn't use new or existing LN/SN."""
+        picking_type_receipt = self.env.ref('stock.picking_type_in')
+        picking_type_receipt.use_create_lots = False
+        picking_type_receipt.use_existing_lots = False
+
+        picking_form = Form(self.env['stock.picking'])
+        picking_form.picking_type_id = picking_type_receipt
+        with picking_form.move_ids_without_package.new() as move:
+            move.product_id = self.product_serial
+            move.product_uom_qty = 4
+        with picking_form.move_ids_without_package.new() as move:
+            move.product_id = self.product_lot
+            move.product_uom_qty = 20
+        receipt = picking_form.save()
+        receipt.action_confirm()
+
+        immediate_wizard = receipt.button_validate()
+        immediate_wizard_form = Form(
+            self.env[immediate_wizard['res_model']].with_context(immediate_wizard['context'])
+        ).save()
+        immediate_wizard_form.process()
+        self.assertEqual(receipt.state, 'done')
+
     def test_set_quantity_done_1(self):
         move1 = self.env['stock.move'].create({
             'name': 'test_set_quantity_done_1',
