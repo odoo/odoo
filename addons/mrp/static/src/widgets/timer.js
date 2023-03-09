9af602bb19dcab9d092192fa2502ffc53aa24ee4
Jorge Pinna Puissant <jpp@odoo.com>
2023-03-09 09:47:46 +0000

    [REF] mrp: MrpTimer and MrpTimerField
    backport-of: https://github.com/odoo/odoo/commit/745d67cfa864d85ae5a19d5f890d8b5e1f414181
    
    Since [1], [2], [3] and [4], the field API was simplified and specified.
    Some props were removed (update, type, setDirty and value) and some were
    now mandatory (record and name).  This specification make it difficult
    to use the fields as a "normal" component (without a record). This was
    the case with MrpTimer, that was used as a field, but also as a
    component. To solve this issue, in this commit, we divide it in two
    different components, MrpTimer and MrpTimerField.
    
    [1]: https://github.com/odoo/odoo/commit/aed1ba484d0c48a59e166ef01e69967bd618a562
    [2]: https://github.com/odoo/odoo/commit/8cde3e84bb70a2bd097921c08c0059bf65bee602
    [3]: https://github.com/odoo/odoo/commit/688986f888f2fe2371d58b74ded81315ba6bb353
    [4]: https://github.com/odoo/odoo/commit/91303252f413325859a6f1651d056593a8cf6382
    
    closes odoo/odoo#114761
    
    closes odoo/odoo#116170
    
    Related: odoo/enterprise#37960
    Related: odoo/enterprise#38546
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp/static/src/widgets/timer.js
+++ addons/mrp/static/src/widgets/timer.js
@@ -8,7 +8,7 @@ import { standardFieldProps } from "@web/views/fields/standard_field_props";
 
 const { Component, useState, onWillUpdateProps, onWillStart, onWillDestroy } = owl;
 
-export function formatMinutes(value) {
+function formatMinutes(value) {
     if (value === false) {
         return "";
     }
@@ -24,59 +24,39 @@ export function formatMinutes(value) {
 }
 
 export class MrpTimer extends Component {
+    static template = "mrp.MrpTimer";
+    static props = {
+        value: { type: Number },
+        ongoing: { type: Boolean, optional: true },
+    };
+    static defaultProps = { ongoing: false };
+
     setup() {
-        this.orm = useService('orm');
         this.state = useState({
             // duration is expected to be given in minutes
-            duration:
-                this.props.record.data[this.props.name] !== undefined ? this.props.record.data[this.props.name] : this.props.record.data.duration,
+            duration: this.props.value,
         });
         this.lastDateTime = Date.now();
-        useInputField({
-            getValue: () => this.durationFormatted,
-            refName: "numpadDecimal",
-            parse: (v) => parseFloatTime(v),
-        });
-
-        this.ongoing =
-            this.props.ongoing !== undefined
-                ? this.props.ongoing
-                : this.props.record.data.is_user_working;
-
-        onWillStart(async () => {
-            if(this.props.ongoing === undefined && !this.props.record.model.useSampleModel && this.props.record.data.state == "progress") {
-                const additionalDuration = await this.orm.call('mrp.workorder', 'get_working_duration', [this.props.record.resId]);
-                this.state.duration += additionalDuration;
-            }
+        this.ongoing = this.props.ongoing;
+        onWillStart(() => {
             if (this.ongoing) {
                 this._runTimer();
                 this._runSleepTimer();
             }
         });
         onWillUpdateProps((nextProps) => {
-            const newOngoing =
-                "ongoing" in nextProps
-                    ? nextProps.ongoing
-                    : "record" in nextProps && nextProps.record.data.is_user_working;
-            const rerun = !this.ongoing && newOngoing;
-            this.ongoing = newOngoing;
+            const rerun = !this.ongoing && nextProps.ongoing;
+            this.ongoing = nextProps.ongoing;
             if (rerun) {
-                this.state.duration = nextProps.record.data[nextProps.name];
+                this.state.duration = nextProps.value;
                 this._runTimer();
-                this._runSleepTimer()
+                this._runSleepTimer();
             }
         });
         onWillDestroy(() => clearTimeout(this.timer));
     }
 
     get durationFormatted() {
-        if (
-            this.props.record.data[this.props.name] != this.state.duration &&
-            this.props.record &&
-            this.props.record.isDirty
-        ) {
-            this.state.duration = this.props.record.data[this.props.name];
-        }
         return formatMinutes(this.state.duration);
     }
 
@@ -92,7 +72,7 @@ export class MrpTimer extends Component {
     //updates the time when the computer wakes from sleep mode
     _runSleepTimer() {
         this.timer = setTimeout(async () => {
-            let diff = Date.now() - this.lastDateTime - 10000;
+            const diff = Date.now() - this.lastDateTime - 10000;
             if (diff > 1000) {
                 this.state.duration += diff / (1000 * 60);
             }
@@ -102,17 +82,58 @@ export class MrpTimer extends Component {
     }
 }
 
-MrpTimer.props = {
-    ...standardFieldProps,
-    duration: { type: Number, optional: true },
-    ongoing: { type: Boolean, optional: true },
-};
-MrpTimer.template = "mrp.MrpTimer";
+class MrpTimerField extends Component {
+    static template = "mrp.MrpTimerField";
+    static components = { MrpTimer };
+    static props = standardFieldProps;
 
-export const mrpTimer = {
-    component: MrpTimer,
+    setup() {
+        this.orm = useService("orm");
+        useInputField({
+            getValue: () => this.durationFormatted,
+            refName: "numpadDecimal",
+            parse: (v) => parseFloatTime(v),
+        });
+
+        // duration is expected to be given in minutes
+        this.duration = this.props.record.data[this.props.name];
+        this.ongoing = this.props.record.data.is_user_working;
+
+        onWillStart(async () => {
+            if (
+                !this.props.record.model.useSampleModel &&
+                this.props.record.data.state == "progress"
+            ) {
+                const additionalDuration = await this.orm.call(
+                    "mrp.workorder",
+                    "get_working_duration",
+                    [this.props.record.resId]
+                );
+                this.duration += additionalDuration;
+            }
+        });
+        onWillUpdateProps((nextProps) => {
+            const rerun = !this.ongoing && nextProps.record.data.is_user_working;
+            this.ongoing = nextProps.record.data.is_user_working;
+            if (rerun) {
+                this.duration = nextProps.record.data[nextProps.name];
+            }
+        });
+        onWillDestroy(() => clearTimeout(this.timer));
+    }
+
+    get durationFormatted() {
+        if (this.props.record.data[this.props.name] != this.duration && this.props.record.isDirty) {
+            this.duration = this.props.record.data[this.props.name];
+        }
+        return formatMinutes(this.duration);
+    }
+}
+
+export const mrpTimerField = {
+    component: MrpTimerField,
     supportedTypes: ["float"],
 };
 
-registry.category("fields").add("mrp_timer", mrpTimer);
+registry.category("fields").add("mrp_timer", mrpTimerField);
 registry.category("formatters").add("mrp_timer", formatMinutes);
