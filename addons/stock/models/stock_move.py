05ab26da3064875b2d3d594e83ad79e41d8b79a2
svs-odoo <svs@odoo.com>
2023-03-09 14:36:50 +0100

    [FIX] stock: immediate transfer w/ tracked product
    How to reproduce:
    - Create a tracked product;
    - In the receipt picking type form view, for the lot and serial numbers,
      uncheck "Create New" and "Use Existing ones";
    - Create a planned receipt for some of this tracked product;
    - Confirm and validate the receipt:
      It opens the "Immediate Transfer" wizard;
    - Apply the immediate transfer:
      -> It asks if you want to create a backorder.
    
    Since the picking type doesn't use new or existing LN/SN, it accepts to
    be confirmed even if some move lines for tracked product have no
    tracking numbers.
    The issue was, when a `stock.move` sets its done quantity to its
    reserved quantity, if its product is tracked, it doesn't change its done
    quantity if it has no tracking number, regardless the picking type's
    configuration.
    
    OPW-3186155

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -1929,7 +1929,9 @@ class StockMove(models.Model):
             if move.state not in ('partially_available', 'assigned'):
                 continue
             for move_line in move.move_line_ids:
-                if move.has_tracking != 'none' and not (move_line.lot_id or move_line.lot_name):
+                if move.has_tracking != 'none' and (
+                    (move.picking_type_id.use_existing_lots and not move_line.lot_id) or
+                    (move.picking_type_id.use_create_lots and not move_line.lot_name)):
                     continue
                 move_line.qty_done = move_line.product_uom_qty
 
