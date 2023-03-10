ad360ca24f6674fc8c89cd83c886b7fb21e23433
Loan (LSE) <lse@odoo.com>
2023-03-10 17:41:15 +0000

    [FIX] hw_drivers: HTTPS certificate info on IoT homepage
    HTTPS certificate IoT issues can be complicated to troubleshoot
    as the information are not visible/given.
    This PR aim to share this information on the IoT box homepage.
    As there is a lot of possible causes for a given problem,
    a code is used that will be explained/detailed in Odoo's
    IoT documentation:
    https://github.com/odoo/documentation/pull/3818
    
    OPW-3227004
    
    closes odoo/odoo#116650
    
    X-original-commit: 8bc6b2d0033676507f95b74b3ae383ab17164203
    Signed-off-by: Loan (LSE) <lse@odoo.com>
    Signed-off-by: Sens Loan (lse) <lse@odoo.com>

--- addons/hw_drivers/main.py
+++ addons/hw_drivers/main.py
@@ -80,7 +80,10 @@ class Manager(Thread):
         helpers.start_nginx_server()
         if platform.system() == 'Linux':
             helpers.check_git_branch()
-        helpers.check_certificate()
+        is_certificate_ok, certificate_details = helpers.get_certificate_status()
+        if not is_certificate_ok:
+            _logger.warning("An error happened when trying to get the HTTPS certificate: %s",
+                            certificate_details)
 
         # We first add the IoT Box to the connected DB because IoT handlers cannot be downloaded if
         # the identifier of the Box is not found in the DB. So add the Box to the DB.
