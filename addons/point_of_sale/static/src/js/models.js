6c9504cc7bb2b4311d1d90a6ce2de88d7aa12f1e
roen-odoo <roen@odoo.com>
2023-03-13 15:58:47 +0100

    [FIX] point_of_sale: keep refunded line when going back to floor plan
    Current behavior:
    In PoS restaurant, if you refund an order and go back to the floor plan,
    before validating the refund, the link between the original order and
    the refund is lost.
    
    Steps to reproduce:
    - Open PoS restaurant
    - Create an order
    - Refund the order but do not validate it
    - Go back to the floor plan, and go back on the order
    - Validate the refund
    - Close the session
    - Go on the orders, you will see that the refund has no link to the
      original order
    
    opw-3202383

--- addons/point_of_sale/static/src/js/models.js
+++ addons/point_of_sale/static/src/js/models.js
@@ -2464,7 +2464,8 @@ exports.Orderline = Backbone.Model.extend({
             price_extra: this.get_price_extra(),
             customer_note: this.get_customer_note(),
             refunded_orderline_id: this.refunded_orderline_id,
-            price_manually_set: this.price_manually_set
+            price_manually_set: this.price_manually_set,
+            refunded_orderline_id: this.refunded_orderline_id,
         };
     },
     //used to create a json of the ticket, to be sent to the printer
