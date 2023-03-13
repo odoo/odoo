2278b3a413ad9c4c09156147f86dc86690c0a76c
William Henrotin <whe@odoo.com>
2023-03-13 15:47:34 +0000

    [IMP] mrp: (de)activate workorder demo data
    Some demo data bill of material have workorder operation. Trying to use
    them without the workorder setting activated will give an error on some
    fields not instantiated.
    
    This commit activate/deactivate the operations depending on the
    workorder setting.
    
    opw - 3229621
    
    closes odoo/odoo#119254
    
    X-original-commit: aaba4eb4deb3f9e10fd611924c5a24680d633537
    Related: odoo/enterprise#40092
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/mrp/models/res_config_settings.py
+++ addons/mrp/models/res_config_settings.py
@@ -24,7 +24,16 @@ class ResConfigSettings(models.TransientModel):
     group_mrp_workorder_dependencies = fields.Boolean("Work Order Dependencies", implied_group="mrp.group_mrp_workorder_dependencies")
 
     def set_values(self):
+        routing_before = self.env.user.has_group('mrp.group_mrp_routings')
         super().set_values()
+        if routing_before and not self.group_mrp_routings:
+            self.env['mrp.routing.workcenter'].search([]).active = False
+        elif not routing_before and self.group_mrp_routings:
+            operations = self.env['mrp.routing.workcenter'].search_read([('active', '=', False)], ['id', 'write_date'])
+            last_updated = max((op['write_date'] for op in operations), default=0)
+            if last_updated:
+                op_to_update = self.env['mrp.routing.workcenter'].browse([op['id'] for op in operations if op['write_date'] == last_updated])
+                op_to_update.active = True
         if not self.group_mrp_workorder_dependencies:
             # Disabling this option should not interfere with currently planned productions
             self.env['mrp.bom'].sudo().search([('allow_operation_dependencies', '=', True)]).allow_operation_dependencies = False
