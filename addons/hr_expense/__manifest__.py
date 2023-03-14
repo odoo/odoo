f9f8935335fbb8f90f8d82b0b874abc6d6fddced
det <det@odoo.com>
2023-03-14 09:59:56 +0100

    [FIX] hr_expense: missing dependency
    Before this commit, it was not possible to install hr_expense alone.
    This commit enable the installation of standalone hr_expense by adding
    missing dependency.

--- addons/hr_expense/__manifest__.py
+++ addons/hr_expense/__manifest__.py
@@ -26,7 +26,7 @@ The whole flow is implemented as:
 This module also uses analytic accounting and is compatible with the invoice on timesheet module so that you are able to automatically re-invoice your customers' expenses if your work by project.
     """,
     'website': 'https://www.odoo.com/app/expenses',
-    'depends': ['account', 'web_tour'],
+    'depends': ['account', 'web_tour', 'hr'],
     'data': [
         'security/hr_expense_security.xml',
         'security/ir.model.access.csv',
