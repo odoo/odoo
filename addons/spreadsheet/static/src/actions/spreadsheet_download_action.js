705924c45a3f05b92c6fe12923c984e7870b0f2e
Adrien Minne (adrm) <adrm@odoo.com>
2023-03-14 11:17:12 +0000

    [FIX] spreadsheet: download excel with filtered values
    Open a spreadsheet, add a data filter, filter some values,
    File > Download
    => the filtered values are not exported.
    
    Previously, we gave the exported data to the action_download_spreadsheet
    action, and we created a new model based on the data. This was a problem
    for data that was only exported for the xlsx in UI plugins, because this
    wasn't in the exported data.
    
    Fixed by giving the xlsx data to the action_download_spreadsheet action instead
    of the data.
    
    Odoo task 3231170
    
    X-original-commit: 33a9b6db1e686afc52f0232352e907c1aee76744

--- addons/spreadsheet/static/src/actions/spreadsheet_download_action.js
+++ addons/spreadsheet/static/src/actions/spreadsheet_download_action.js
@@ -5,15 +5,17 @@ import { registry } from "@web/core/registry";
 import { createSpreadsheetModel, waitForDataLoaded } from "@spreadsheet/helpers/model";
 
 async function downloadSpreadsheet(env, action) {
-    const { orm, name, data, stateUpdateMessages } = action.params;
-    const model = await createSpreadsheetModel({ orm, data, revisions: stateUpdateMessages });
-    await waitForDataLoaded(model);
-    const { files } = model.exportXLSX();
+    let { orm, name, data, stateUpdateMessages, xlsxData } = action.params;
+    if (!xlsxData) {
+        const model = await createSpreadsheetModel({ orm, data, revisions: stateUpdateMessages });
+        await waitForDataLoaded(model);
+        xlsxData = model.exportXLSX();
+    }
     await download({
         url: "/spreadsheet/xlsx",
         data: {
             zip_name: `${name}.xlsx`,
-            files: JSON.stringify(files),
+            files: JSON.stringify(xlsxData.files),
         },
     });
 }
