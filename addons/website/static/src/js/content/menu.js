d67a1ccc2f362323189f067d75acf6b20e8f5d78
Guillaume (gdi) <gdi@odoo.com>
2023-03-14 10:26:42 +0100

    [FIX] website: support RTL for the opening direction of the sub-menus
    Since [this commit], some JS code tries to open the sub-menus in the
    right direction so that the page overflow is limited. Unfortunately,
    languages that read from right to left (RTL) were not supported by this
    code, which left some sub-menus of the navbar overflowing when there was
    room to open them without overflowing (on the other side).
    Steps to reproduce the bug:
    - Have a sub-menu in the last but one position of the navbar with long
    strings of characters.
    - Align navbar items to the right.
    - Have an RTL language installed on your website.
    
    => When you open the sub-menu in RTL, it opens on the wrong side and
    causes an overflow of the page.
    
    [this commit]: https://github.com/odoo/odoo/commit/392c91cda3133b921e9aca9a7b1c511231027438
    
    task-2904507
    
    X-original-commit: https://github.com/odoo/odoo/commit/e4171a6eb2a54c77f27909f287ad899b69520df7

--- addons/website/static/src/js/content/menu.js
+++ addons/website/static/src/js/content/menu.js
@@ -500,6 +500,7 @@ publicWidget.registry.menuDirection = publicWidget.Widget.extend({
      */
     start: function () {
         this.defaultAlignment = this.$el.is('.ms-auto, .ms-auto ~ *') ? 'end' : 'start';
+        this.isRtl = !!this.el.closest('#wrapwrap.o_rtl');
         return this._super.apply(this, arguments);
     },
 
@@ -517,7 +518,7 @@ publicWidget.registry.menuDirection = publicWidget.Widget.extend({
      * @returns {boolean}
      */
     _checkOpening: function (alignment, liOffset, liWidth, menuWidth, pageWidth) {
-        if (alignment === 'start') {
+        if (alignment === 'start' && !this.isRtl || alignment === 'end' && this.isRtl) {
             // Check if ok to open the dropdown to the right (no window overflow)
             return (liOffset + menuWidth <= pageWidth);
         } else {
@@ -545,7 +546,8 @@ publicWidget.registry.menuDirection = publicWidget.Widget.extend({
 
         var alignment = this.defaultAlignment;
         if ($dropdown.nextAll(':visible').length === 0) {
-            // The dropdown is the last menu item, open to the left
+            // The dropdown is the last menu item, open to the left side
+            // (right side with rtl languages).
             alignment = 'end';
         }
 
