ec25101671f526d98803a1750263284ff4083872
svs-odoo <svs@odoo.com>
2023-03-14 17:18:45 +0100

    [IMP] product_expiry,stock: generate LN
    Before, when the user pastes a list of tracking numbers into the move
    line's `lot_name` field (through the move's detailed view), a lot is
    created for each line, creating a move line for each new lot, or
    assigning each lot to an existing line with no lot.
    
    With this commit, it will also take `qty_done` and `expiration_date` (if
    `product_expiry` is installed) from the pasted list if it's relevant.
    
    Each data should be separate either by a semicolon, either by a tab, and
    the first data should always be the lot name.
    Valid examples for lots and quantities:
    
    lot-01;22
    lot-02	12
    
    Valid examples for lots, expiration date and quantities:
    
    lot-03;8;01-02-2005
    lot-04	2005/02/01	12-20
    
    The only valid separation characters for the date are the space (" "),
    the dash ("-") and the slash ("/"). Examples of valid dates:
    20-02-24        20-Feb-2024
    20 02 24        20 Feb 2024
    20/02/24        20/Feb/2024
    
    If any of the extra values can't be converted into either a quantity or
    a date, the whole line will be used as the tracking number.
    
    Also, if dates are pasted but the product doesn't use any expiration
    date, the date's part will be ignored.
    
    task-3171866

--- addons/product_expiry/models/stock_move.py
+++ addons/product_expiry/models/stock_move.py
@@ -2,8 +2,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import datetime
+import dateutil.parser as dparser
+from re import findall as re_findall
 
 from odoo import fields, models
+from odoo.tools import get_lang
 
 
 class StockMove(models.Model):
@@ -12,15 +15,62 @@ class StockMove(models.Model):
     use_expiration_date = fields.Boolean(
         string='Use Expiration Date', related='product_id.use_expiration_date')
 
-    def _generate_serial_move_line_commands(self, lot_names, origin_move_line=None):
+    def _generate_serial_move_line_commands(self, field_data, origin_move_line=None):
         """Override to add a default `expiration_date` into the move lines values."""
-        move_lines_commands = super()._generate_serial_move_line_commands(lot_names, origin_move_line=origin_move_line)
+        move_lines_commands = super()._generate_serial_move_line_commands(field_data, origin_move_line)
         if self.product_id.use_expiration_date:
             date = fields.Datetime.today() + datetime.timedelta(days=self.product_id.expiration_time)
             for move_line_command in move_lines_commands:
                 move_line_vals = move_line_command[2]
-                move_line_vals['expiration_date'] = date
+                if 'expiration_date' not in move_line_vals:
+                    move_line_vals['expiration_date'] = date
         return move_lines_commands
+
+    def _convert_string_into_field_data(self, string, options):
+        res = super()._convert_string_into_field_data(string, options)
+        if not res:
+            try:
+                datetime = dparser.parse(string, **options)
+                if not self.use_expiration_date:
+                    # The datetime was correctly parsed but this move's product doesn't use expiration date.
+                    return "ignore"
+                return {'expiration_date': datetime}
+            except ValueError:
+                pass
+        return res
+
+    def _get_formating_options(self, strings):
+        options = super()._get_formating_options(strings)
+        separators = "-/ "
+        date_regex = f'[^{separators}]+'
+        for string in strings:
+            # Searches for a date.
+            date_data = re_findall(date_regex, string)
+            if len(date_data) < 2:  # Not enough data.
+                continue
+            value_1, value_2 = date_data[:2]
+            if re_findall('[a-zA-Z]', value_1):
+                # Assumes the first value is the mounth (written in letters). Don't add any option
+                # as mounth as the first date's value is the default behavior for `dateutil.parse`.
+                break
+            # Try to guess if the first data is the day or the year.
+            if int(value_1) > 31:
+                options['yearfirst'] = True
+                break
+            elif int(value_1) > 12 and (re_findall('[a-zA-Z]', value_2) or int(value_2) <= 12):
+                options['dayfirst'] = True
+                break
+            else:  # Too ambiguous, gets the option from the user's lang's date setting.
+                user_lang_format = get_lang(self.env).date_format
+                if re_findall('^%[mbB]', user_lang_format):  # First parameter is for month.
+                    return options
+                elif re_findall('^%[djaA]', user_lang_format):  # First parameter is for day.
+                    options['dayfirst'] = True
+                    break
+                elif re_findall('^%[yY]', user_lang_format):  # First parameter is for year.
+                    options['yearfirst'] = True
+                    break
+        return options
 
     def _update_reserved_quantity(self, need, available_quantity, location_id, lot_id=None, package_id=None, owner_id=None, strict=True):
         if self.product_id.use_expiration_date:
