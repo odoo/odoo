58183d47e730a1f64c140108a34c58116878d584
Xavier Bol (xbo) <xbo@odoo.com>
2023-03-14 13:18:42 +0000

    [IMP] project: do not display private on project if task is subtask
    Before this commit, since the display_project_id field has been removed and
    the project_id field could be unset on subtasks (default value as the
    `display_project_id` field), the widget `project_private_task` consider
    a task as private if the task has no project_id set.
    
    This commit changes the widget to check if `is_private` field is true
    to display the `Private` on `project_id` field value.
    
    task-3230063
    
    X-original-commit: b0a8a9aedb3b5cfb2acb6ff3b10a77c279bda8fd
    Part-of: odoo/odoo#115781

--- addons/project/static/src/components/project_private_task_many2one_field/project_private_task_many2one_field.js
+++ addons/project/static/src/components/project_private_task_many2one_field/project_private_task_many2one_field.js
@@ -3,12 +3,24 @@
 import { registry } from '@web/core/registry';
 import { Many2OneField, many2OneField } from '@web/views/fields/many2one/many2one_field';
 
-export class ProjectPrivateTaskMany2OneField extends Many2OneField { }
+export class ProjectPrivateTaskMany2OneField extends Many2OneField {
+    get Many2XAutocompleteProps() {
+        const props = super.Many2XAutocompleteProps;
+        if (this.props.record.data.is_private) {
+            props.placeholder = this.env._t("Private");
+        }
+        return props;
+    }
+}
 ProjectPrivateTaskMany2OneField.template = 'project.ProjectPrivateTaskMany2OneField';
 
 export const projectPrivateTaskMany2OneField = {
     ...many2OneField,
     component: ProjectPrivateTaskMany2OneField,
+    fieldDependencies: [
+        ...(many2OneField.fieldDependencies || []),
+        { name: "is_private", type: "boolean" },
+    ],
 };
 
 registry.category("fields").add("project_private_task", projectPrivateTaskMany2OneField);
