7ba2f7a46ff28c854958d9cfff8f22232a3109fe
nomadgeek <5652065+nomad-geek@users.noreply.github.com>
2023-03-15 11:36:29 -0400

    Add Mail from Contacts Addon

---
+++ addons/mail_from_contacts/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Mail From Contacts",
+    'summary': """
+        Mail Form from Customer Contacts.
+        """,
+    'description': """
+        Add button to access the form to compose emails from customer contacts.
+        Only tested in CE.
+    """,
+    'version': '16.0.22.10.26',
+    'author': "FlashOdoo",
+    'website': "http://www.flashodoo.com",
+    'category': 'Customizations',
+    'license': 'LGPL-3',
+    'support': 'foxodoo@gmail.com',
+    'depends': ['crm'],
+    'data': [
+        'views/customer_form.xml',
+    ],
+    'images': [
+        'static/description/banner.png'
+    ],
+   
+}
