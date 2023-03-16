4d0f4ba49207fe54621813a5d65a58c0b98ef148
Jorge Pinna Puissant <jpp@odoo.com>
2023-03-16 13:57:21 +0000

    [FIX] web: remove translation alert banner
    The translation alert banner is an old remaining edit/save feature that
    is not relevant anymore with the new "always edit" and the "save when
    leave" features.
    
    Task-id: 3232795
    X-original-commit: 912fe8768a9e9776d89f8d8e2d824fb065ce52d8

--- addons/web/static/src/views/form/form_controller.js
+++ addons/web/static/src/views/form/form_controller.js
@@ -19,7 +19,7 @@ import { useViewButtons } from "@web/views/view_button/view_button_hook";
 import { useSetupView } from "@web/views/view_hook";
 import { FormStatusIndicator } from "./form_status_indicator/form_status_indicator";
 
-import { Component, onRendered, onWillStart, toRaw, useEffect, useRef, useState } from "@odoo/owl";
+import { Component, onRendered, onWillStart, useEffect, useRef, useState } from "@odoo/owl";
 
 const viewRegistry = registry.category("views");
 
@@ -184,8 +184,6 @@ export class FormController extends Component {
         });
 
         const state = this.props.state || {};
-        const { fieldsToTranslate } = state;
-        this.fieldsToTranslate = useState(fieldsToTranslate || {});
         const activeNotebookPages = { ...state.activeNotebookPages };
         this.onNotebookPageChange = (notebookId, page) => {
             if (page) {
@@ -202,7 +200,6 @@ export class FormController extends Component {
                 return {
                     activeNotebookPages: !this.model.root.isNew ? activeNotebookPages : {},
                     resId: this.model.root.resId,
-                    fieldsToTranslate: toRaw(this.fieldsToTranslate),
                 };
             },
         });
@@ -444,15 +441,6 @@ export class FormController extends Component {
         const record = this.model.root;
         let saved = false;
 
-        // Before we save, we gather dirty translate fields data. It needs to be done before the
-        // save as nothing will be dirty after. It is why there is a compute part and a show part.
-        if (record.dirtyTranslatableFields.length) {
-            const { resId } = record;
-            this.fieldsToTranslate[resId] = new Set([
-                ...toRaw(this.fieldsToTranslate[resId] || []),
-                ...record.dirtyTranslatableFields,
-            ]);
-        }
         if (this.props.saveRecord) {
             saved = await this.props.saveRecord(record, params);
         } else {
@@ -463,13 +451,6 @@ export class FormController extends Component {
             this.props.onSave(record, params);
         }
 
-        // After we saved, we show the previously computed data in the alert (if there is any).
-        // It needs to be done after the save because if we were in record creation, the resId
-        // changed from false to a number. So it first needs to update the computed data to the new id.
-        if (this.fieldsToTranslate.false) {
-            this.fieldsToTranslate[record.resId] = this.fieldsToTranslate.false;
-            delete this.fieldsToTranslate.false;
-        }
         return saved;
     }
 
@@ -485,20 +466,6 @@ export class FormController extends Component {
         if (this.model.root.isVirtual || this.env.inDialog) {
             this.env.config.historyBack();
         }
-    }
-
-    get translateAlert() {
-        const { resId } = this.model.root;
-        if (!this.fieldsToTranslate[resId]) {
-            return null;
-        }
-
-        return {
-            fields: this.fieldsToTranslate[resId],
-            close: () => {
-                delete this.fieldsToTranslate[resId];
-            },
-        };
     }
 
     get className() {
