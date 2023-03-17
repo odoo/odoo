171bc1ef3ab2e11400681099d38eb101476a88c6
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2023-03-17 14:07:00 +0100

    [FIX] project: review UX and UI in Project app
    This commit reviews the UX and UI in Project app and improves the usability of the
    new features recently added (subtasks list in kanban card, state field replacing
    kanban_state field, etc).
    
    The following changes are made in this commit:
    
    - swap of user_id icon in the kanban box and the removal of the allow_unassign field and setting
    - change the ordering of the fields in the subtask list
    - make small layout changes in the project.task and project.project kanban cards
    - remove the "lock-icon private" sub-title of private tasks, replace it with a
      little lock icon on the bottom right icons of the kanban card.
    - remove the break tag in the project.task kanban card that was unecessary
      given the new display and margin style settings
    - lock icon is bigger
    - kanban icons are better aligned
    - state is as big as avatar
    - remove the 'remaining hours on SO' field
    - deadline field will be optional and hidden by default in project.task list view
    - remove the rating field
    - add stage field as optional in project.task list views
    - priority and state fields will no longer be optional
    - stage_id will be copied when duplicating a task, except when the task is
      generated through the recurrence
    - remove the tooltip of the tag_ids field
    - When duplicating a task having sub-tasks, '(copy)' is no longer
      added to the name of the sub-tasks of this task.
    - project.task kanban view:
      * (+ x tasks) mention next to the name is removed
      * The caret is replaced with 'fa-check-square-o x/y'
        which will represent the number of sub-tasks closed
        compared to the total number of sub-tasks
      * Only open subtasks are displayed
      * When changing the state of a sub-task to a closing one,
        the sub-task is muted and removed from the list on the view reload
      * The name of the parent task on the kanban card of sub-tasks is
        displayed except when viewing the sub-tasks of a particular task
        through the sub-tasks stat button
      * project.project kanban view: the fa-check-square-o icon of
        milestones is replaced with fa-flag-o
      * project.task kanban card: the fa-play and fa-pause icons
        are moved on the right of the remaining hours widge.
      * Allow users to edit the stage_id in batch from the list view of tasks
        if all of the selected tasks are part of the same project.
      * the state will have the same size as the avatar
      * change the opacity of the tasks that are closed
      * state is at the right of subtask list
      * the striked should be replaced by the opacity on the kanban card
    
    Enterprise PR: odoo/enterprise#38132
    
    Task-3229873
    
    Co-Authored-by: Panagiotis Kyriakou <paky@odoo.com>
    Co-Authored-by: Bastien (bvdn) <bvdn@odoo.com>

--- addons/project/static/tests/project_subtask_kanban_list_tests.js
+++ addons/project/static/tests/project_subtask_kanban_list_tests.js
@@ -18,28 +18,34 @@ QUnit.module('Subtask Kanban List tests', {
     beforeEach: async function () {
         const pyEnv = await startServer();
         const projectId = pyEnv['project.project'].create([
-            { name: "Project One", allow_subtasks: true },
+            { name: "Project One" },
         ]);
         const userId = pyEnv['res.users'].create([
             { name: "User One", login: 'one', password: 'one' },
         ]);
         pyEnv['project.task'].create([
-            { name: 'task one', project_id: projectId, subtask_count: 2, child_ids: [2, 3], state: '01_in_progress', user_ids: [userId] },
-            { name: 'task two', subtask_count: 0, child_ids: [], state: '03_approved' },
-            { name: 'task three', subtask_count: 0, child_ids: [], state: '02_changes_requested' },
-            { name: 'task four', project_id: projectId, subtask_count: 0, child_ids: [], state: '01_in_progress' },
+            { name: 'task one', project_id: projectId, closed_subtask_count: 2, subtask_count: 3, child_ids: [2, 3, 4], state: '01_in_progress', user_ids: [userId] },
+            { name: 'task two', parent_id: 1, closed_subtask_count: 0, subtask_count: 0, child_ids: [], state: '03_approved' },
+            { name: 'task three', parent_id: 1, closed_subtask_count: 0, subtask_count: 0, child_ids: [], state: '02_changes_requested' },
+            { name: 'task four', parent_id: 1, closed_subtask_count: 0, subtask_count: 0, child_ids: [], state: '1_done' },
+            { name: 'task five', closed_subtask_count: 0, subtask_count: 1, child_ids: [6], state: '03_approved' },
+            { name: 'task six', parent_id: 5, closed_subtask_count: 0, subtask_count: 0, child_ids: [], state: '1_canceled' },
         ]);
         this.views = {
             "project.task,false,kanban":
                 `<kanban js_class="project_task_kanban">
                     <field name="subtask_count"/>
+                    <field name="closed_subtask_count"/>
                     <templates>
                         <t t-name="kanban-box">
                             <div>
                                 <field name="name" widget="name_with_subtask_count"/>
                                 <field name="user_ids" invisible="1"/>
                                 <field name="state" invisible="1"/>
-                                <a t-if="record.subtask_count.raw_value" class="subtask_list_button" title="See Subtasks" style="width: 29px;"/>
+                                <a t-if="record.closed_subtask_count.raw_value">
+                                    <span title="See Subtasks" class="subtask_list_button fa fa-check-square-o me-1"/>
+                                    <t t-out="record.closed_subtask_count.value"/>/<t t-out="record.subtask_count.value"/>
+                                </a>
                             </div>
                             <div class="kanban_bottom_subtasks_section"/>
                         </t>
@@ -60,13 +66,13 @@ QUnit.module('Subtask Kanban List tests', {
             views: [[false, "kanban"]],
         });
 
-        assert.containsOnce(target, '.subtask_list_button', "Only kanban boxes of parent tasks should have the drawdown button, in this case this is 1");
+        assert.containsOnce(target, '.subtask_list_button', "Only kanban boxes of parent tasks having open subtasks should have the drawdown button, in this case this is 1");
         assert.containsNone(target, '.subtask_list', "If the drawdown button is not clicked, the subtasks list should be hidden");
 
         await click(target, '.subtask_list_button');
 
         assert.containsOnce(target, '.subtask_list', "Clicking on the button should make the subtask list render, in this case we are expectig 1 list");
-        assert.containsN(target, '.subtask_list_row', 2, "The list rendered should show all the subtasks of the task, in this case 2");
+        assert.containsN(target, '.subtask_list_row', 2, "The list rendered should show the open subtasks of the task, in this case 2");
         assert.containsN(target, '.subtask_state_widget_col', 2, "Each of the list's rows should have 1 state widget, thus we are looking for 2 in total");
         assert.containsN(target, '.subtask_user_widget_col', 2, "Each of the list's rows should have 1 user widgets, thus we are looking for 2 in total");
         assert.containsN(target, '.subtask_name_col', 2, "Each of the list's rows should display the subtask's name, thus we are looking for 2 in total");
