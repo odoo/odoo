dc5976370252f8e63b790a469ccaa0914d4df84c
hupo-odoo <hupo@odoo.com>
2023-03-17 13:18:58 +0100

    [FIX] account: fiduciary mode sequence checks
    When the fiduciary mode is activated, we want to bypass the check of constrains on the date sequence.
    Before this commit, fiduciary mode enabled to modify the name of a customer invoice but if the name resembled somewhat a normal sequence number,  there still might be cases where Odoo will try to match a "Journal ID" and a "Year" in a manual sequence number that is provided.
    
    For example, writing 23-31-070 as the new invoice name, Odoo mistakingly interpreted the following:
    - 23 is the name of the journal
    - 31 is the year
    - 070 is the sequence number
    
    X-original-commit: f1b695d9ec8a551c93197b3aaad1b5504af8fdd6

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -2434,6 +2434,10 @@ class AccountMove(models.Model):
     # SEQUENCE MIXIN
     # -------------------------------------------------------------------------
 
+    def _must_check_constrains_date_sequence(self):
+        # OVERRIDES sequence.mixin
+        return not self.quick_edit_mode
+
     def _get_last_sequence_domain(self, relaxed=False):
         #pylint: disable=sql-injection
         # EXTENDS account sequence.mixin
