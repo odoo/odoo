4977b46d11815e50133810d289356c2ae7a4c1f2
luvi <luvi@odoo.com>
2023-03-17 08:39:24 +0000

    [FIX] web: make draggable hook tolerant with mouse movements
    Before this commit, when a user moved its mouse from only a few pixels,
    a kanban record could not be clicked on it, or an item in a sortable list
    couldn't handle the click, since the dragging state was immediatly applied
    to the element.
    
    Now, there is a 10 pixels tolerance, meaning that the dragging sequence is
    only applied once the user has moved the cursor from at least 10px horizontally,
    or vertically from the original point where the mouse button was pressed and
    remains down. This reduces frustation when the user didn't intended to move the
    kanban card with the mouse moving by mistake.
    
    A test has been added to verify the behavior when dragging an element in a sortable
    list, from at least 10 pixels.
    
    Finally, a test had to be modified, since the triggered mousemove didn't had any
    clientX value to enable the dragging sequence.
    
    task-3233698
    
    closes odoo/odoo#115905
    
    X-original-commit: f9faea77e3fbef65d588f64243ea4dab06c88e83
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/web/static/src/core/utils/draggable_hook_builder.js
+++ addons/web/static/src/core/utils/draggable_hook_builder.js
@@ -52,6 +52,7 @@ import { debounce, setRecurringAnimationFrame } from "@web/core/utils/timing";
  * @property {Position} [mouse={ x: 0, y: 0 }]
  * @property {Position} [offset={ x: 0, y: 0 }]
  * @property {EdgeScrollingOptions} [edgeScrolling]
+ * @property {Number} [pixelsTolerance=10]
  */
 
 /**
@@ -226,6 +227,19 @@ export function makeDraggableHook(hookParams = {}) {
             };
 
             /**
+             * Returns whether the user has moved from at least the number of pixels
+             * that are tolerated from the initial mouse position.
+             * @param {MouseEvent} ev
+             */
+            const canDrag = (ev) => {
+                return (
+                    ctx.origin &&
+                    Math.hypot(ev.clientX - ctx.origin.x, ev.clientY - ctx.origin.y) >=
+                        ctx.pixelsTolerance
+                );
+            };
+
+            /**
              * Main entry function to start a drag sequence.
              */
             const dragStart = () => {
@@ -304,6 +318,7 @@ export function makeDraggableHook(hookParams = {}) {
                 ctx.currentContainer = null;
                 ctx.currentElement = null;
                 ctx.currentElementRect = null;
+                ctx.origin = null;
                 ctx.scrollParent = null;
 
                 state.dragging = false;
@@ -423,6 +438,10 @@ export function makeDraggableHook(hookParams = {}) {
 
                 ctx.currentContainer = ctx.ref.el;
                 ctx.currentElement = ev.target.closest(ctx.elementSelector);
+                ctx.origin = {
+                    x: ev.clientX,
+                    y: ev.clientY,
+                };
 
                 Object.assign(ctx.offset, ctx.mouse);
 
@@ -434,14 +453,18 @@ export function makeDraggableHook(hookParams = {}) {
              * @param {MouseEvent} ev
              */
             const onMousemove = (ev) => {
-                updateMousePosition(ev);
-
                 if (!ctx.enabled || !ctx.currentElement) {
                     return;
                 }
                 if (!state.dragging) {
-                    dragStart();
+                    // Prevent the drag and drop to start if the user has only moved its mouse from a few pixels
+                    if (canDrag(ev)) {
+                        dragStart();
+                    }
                 }
+
+                updateMousePosition(ev);
+
                 if (state.dragging) {
                     const [eRect, cRect] = updateRects();
 
@@ -533,6 +556,7 @@ export function makeDraggableHook(hookParams = {}) {
                 mouse: { x: 0, y: 0 },
                 offset: { x: 0, y: 0 },
                 edgeScrolling: { enabled: true },
+                pixelsTolerance: 10,
             };
 
             // Effect depending on the params to update them.
