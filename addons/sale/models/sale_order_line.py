12355a80042c70d4c4c23f6e22145358ccc747f8
Yolann Sabaux <yosa@odoo.com>
2023-03-20 10:59:18 +0100

    [FIX] sale: refund of bill re-invoiced
    Steps to reproduce:
    - create an expense product, re-invoice: "at cost"
    - create a sale order with the product and set an analytic account
    - create a purchase order with the expense product and the same analytic account
    - create the bill
    -> a new line on the SO is created with the same "Quantity Delivered" as the "Quantity Invoiced" for the purchase order
    - for the bill, create a Credit Note of x unit
    -> the purchase order has the Quantity Invoiced diminished of x
    
    Issue:
    - the Sale Order has not taken into account the new quantity after refund
    - Since the line is an "analytic" one, the quantity delivered cannot be changed even if we wreate a credit note for the invoice
    
    Solution:
    During the processus of creation of new analytic lines, instead of creating new so lines we adapt the analytic line

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -344,7 +344,7 @@ class SaleOrderLine(models.Model):
         """
         # compute for analytic lines
         lines_by_analytic = self.filtered(lambda sol: sol.qty_delivered_method == 'analytic')
-        mapping = lines_by_analytic._get_delivered_quantity_by_analytic([('amount', '<=', 0.0)])
+        mapping = lines_by_analytic._get_delivered_quantity_by_analytic([])
         for so_line in lines_by_analytic:
             so_line.qty_delivered = mapping.get(so_line.id or so_line._origin.id, 0.0)
         # compute for manual lines
@@ -365,29 +365,20 @@ class SaleOrderLine(models.Model):
 
         # group analytic lines by product uom and so line
         domain = expression.AND([[('so_line', 'in', self.ids)], additional_domain])
-        data = self.env['account.analytic.line'].read_group(
-            domain,
-            ['so_line', 'unit_amount', 'product_uom_id'], ['product_uom_id', 'so_line'], lazy=False
-        )
-
-        # convert uom and sum all unit_amount of analytic lines to get the delivered qty of SO lines
-        # browse so lines and product uoms here to make them share the same prefetch
-        lines = self.browse([item['so_line'][0] for item in data])
-        lines_map = {line.id: line for line in lines}
-        product_uom_ids = [item['product_uom_id'][0] for item in data if item['product_uom_id']]
-        product_uom_map = {uom.id: uom for uom in self.env['uom.uom'].browse(product_uom_ids)}
-        for item in data:
-            if not item['product_uom_id']:
+        analytic_lines = self.env['account.analytic.line'].search(domain)
+        for line in analytic_lines:
+            if not line.product_uom_id:
                 continue
-            so_line_id = item['so_line'][0]
-            so_line = lines_map[so_line_id]
-            result.setdefault(so_line_id, 0.0)
-            uom = product_uom_map.get(item['product_uom_id'][0])
-            if so_line.product_uom.category_id == uom.category_id:
-                qty = uom._compute_quantity(item['unit_amount'], so_line.product_uom, rounding_method='HALF-UP')
+            result.setdefault(line.so_line.id, 0.0)
+            if line.so_line.product_uom.category_id == line.product_uom_id.category_id:
+                qty = line.product_uom_id._compute_quantity(line.unit_amount, line.so_line.product_uom, rounding_method='HALF-UP')
             else:
-                qty = item['unit_amount']
-            result[so_line_id] += qty
+                qty = line.unit_amount
+
+            # if greater than 0 -> refund
+            sign = line.amount and -line.amount / abs(line.amount) or 1
+
+            result[line.so_line.id] += sign * qty
 
         return result
 
