8ccfd838df14eed28649c6463b9ddfc1c87c8d0b
Arnis Putniņš <arnis@allegro.lv>
2023-03-21 09:06:08 +0200

    Update __init__.py

--- addons/l10n_lv/__init__.py
+++ addons/l10n_lv/__init__.py
@@ -5,6 +5,4 @@ def load_translations(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     env.ref(
         'l10n_lv.account_chart_template_latvia').process_coa_translations()
-        
-        
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    
