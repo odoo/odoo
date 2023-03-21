3b790b27dee4d75bdec651e6ab30e36033eacc06
Julien Van Roy <juvr@odoo.com>
2023-03-21 08:22:17 +0000

    [IMP] account_edi_ubl_cii: add Singapore Bis 3 format
    Add a new edi_format "SG BIS Billing 3.0" available for SG companies.
    This format is based on BIS Billing 3.0.
    
    task-3180983
    
    X-original-commit: 1cf5b48786bed8dd698f10ffa33791b0d837e53f

--- addons/account_edi_ubl_cii/models/__init__.py
+++ addons/account_edi_ubl_cii/models/__init__.py
@@ -9,7 +9,14 @@ from . import account_edi_xml_ubl_xrechnung
 from . import account_edi_xml_ubl_nlcius
 from . import account_edi_xml_ubl_efff
 from . import account_edi_xml_ubl_a_nz
+<<<<<<< HEAD
 from . import account_edi_xml_ubl_sg
 from . import account_move_send
 from . import account_move
 from . import res_partner
+||||||| parent of ef14907654a (temp)
+from . import ir_actions_report
+=======
+from . import account_edi_xml_ubl_sg
+from . import ir_actions_report
+>>>>>>> ef14907654a (temp)
