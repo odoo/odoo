3e9b0e2aa7f9c008eeb4850cd457e9d83a88e041
Claire Bretton (clbr) <clbr@odoo.com>
2023-03-21 14:36:14 +0100

    [FIX] account: update of taxes when chart_template installed via fiscal position
    When a chart_template was installed through the fiscal position system
    on a company, related taxes were not updated correctly.
    Also adds a better error message when there is an incompatibility
    between existing taxes' country and chart_template's country/fiscal country.
    
    Task: 3201982
    X-original-commit: a04a10d2cbfd8d1395f827026d79059a63037941

--- addons/account/tests/test_chart_template.py
+++ addons/account/tests/test_chart_template.py
@@ -2,6 +2,7 @@ import logging
 
 from odoo import Command
 from odoo.addons.account.models.chart_template import update_taxes_from_templates
+from odoo.exceptions import ValidationError
 from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
 
@@ -29,9 +30,9 @@ class TestChartTemplate(TransactionCase):
             cls.skipTest(cls, "Accounting Tests skipped because the generic chart of accounts was not found")
 
         cls.fiscal_position_template = cls._create_fiscal_position_template('account.test_fiscal_position_template',
-                                                                            'BE fiscal position test', us_country_id)
-        cls.tax_template_1 = cls._create_tax_template('account.test_tax_template_1', 'Tax name 1', 1, 'tag_name_1')
-        cls.tax_template_2 = cls._create_tax_template('account.test_tax_template_2', 'Tax name 2', 2, 'tag_name_2')
+                                                                            'US fiscal position test', us_country_id)
+        cls.tax_template_1 = cls._create_tax_template('account.test_tax_template_1', 'Tax name 1', 1, tag_name='tag_name_1')
+        cls.tax_template_2 = cls._create_tax_template('account.test_tax_template_2', 'Tax name 2', 2, tag_name='tag_name_2')
         cls.fiscal_position_tax_template_1 = cls._create_fiscal_position_tax_template(
             cls.fiscal_position_template, 'account.test_fp_tax_template_1', cls.tax_template_1, cls.tax_template_2
         )
@@ -50,7 +51,7 @@ class TestChartTemplate(TransactionCase):
         return cls._create_tax_template(template_name, name, amount, tag_name=None)
 
     @classmethod
-    def _create_tax_template(cls, tax_template_xmlid, name, amount, tag_name=None):
+    def _create_tax_template(cls, tax_template_xmlid, name, amount, tag_name=None, chart_template_id=None):
         if tag_name:
             tag = cls.env['account.account.tag'].create({
                 'name': tag_name,
@@ -63,7 +64,7 @@ class TestChartTemplate(TransactionCase):
                 'name': name,
                 'amount': amount,
                 'type_tax_use': 'none',
-                'chart_template_id': cls.chart_template.id,
+                'chart_template_id': chart_template_id if chart_template_id else cls.chart_template.id,
                 'invoice_repartition_line_ids': [
                     Command.create({
                         'factor_percent': 100,
@@ -116,7 +117,7 @@ class TestChartTemplate(TransactionCase):
         """ Tests that adding a new tax template and a fiscal position tax template
         creates this new tax and fiscal position line when updating
         """
-        tax_template_3 = self._create_tax_template('account.test_tax_3_template', 'Tax name 3', 3, 'tag_name_3')
+        tax_template_3 = self._create_tax_template('account.test_tax_3_template', 'Tax name 3', 3, tag_name='tag_name_3')
         tax_template_4 = self._create_tax_template('account.test_tax_4_template', 'Tax name 4', 4)
         self._create_fiscal_position_tax_template(self.fiscal_position_template, 'account.test_fiscal_position_tax_template', tax_template_3, tax_template_4)
         update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
@@ -267,3 +268,53 @@ class TestChartTemplate(TransactionCase):
             ('partner_ids', 'in', normal_user.partner_id.ids),
             ('body', 'like', f"%{self.tax_template_1.name}%"),  # we look for taxes' name that have been sent in the message's body
         ]), 0)
+
+    def test_update_taxes_foreign_taxes(self):
+        """ When taxes are instantiated through the fiscal position system (in multivat),
+        its taxes should also be updated.
+        """
+        country_test = self.env['res.country'].create({
+            'name': 'Country Test',
+            'code': 'ZZ',
+        })
+        chart_template_xmlid_test = 'l10n_test.test_chart_template_xmlid'
+        chart_template_test = self.env['account.chart.template']._load_records([{
+            'xml_id': chart_template_xmlid_test,
+            'values': {
+                'name': 'Test Chart Template',
+                'currency_id': self.env.ref('base.EUR').id,
+                'bank_account_code_prefix': 1000,
+                'cash_account_code_prefix': 2000,
+                'transfer_account_code_prefix': 3000,
+                'country_id': country_test.id,
+            }
+        }])
+        self._create_tax_template('account.test_tax_test_template', 'Tax name 1 TEST', 10, chart_template_id=chart_template_test.id)
+        self.env['account.tax.template']._try_instantiating_foreign_taxes(country_test, self.company)
+        self._create_tax_template('account.test_tax_test_template2', 'Tax name 2 TEST', 15, chart_template_id=chart_template_test.id)
+        update_taxes_from_templates(self.env.cr, chart_template_xmlid_test)
+
+        tax_test_model_data = self.env['ir.model.data'].search([
+            ('name', '=', f'{self.company.id}_test_tax_test_template2'),
+            ('model', '=', 'account.tax'),
+        ])
+        self.assertEqual(len(tax_test_model_data), 1, "Taxes should have been created even if the chart_template is installed through fiscal position system.")
+
+    def test_update_taxes_chart_template_country_check(self):
+        """ We can't update taxes that don't match the chart_template's country. """
+        self.company.chart_template_id.country_id = self.env.ref('base.lu')
+        # We provoke one recreation and one update
+        self.tax_template_1.amount += 1
+        self.tax_template_2.invoice_repartition_line_ids.tag_ids.name = 'tag_name_2_modified'
+        with self.assertRaises(ValidationError):
+            update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
+
+    def test_update_taxes_fiscal_country_check(self):
+        """ If there is no country set on chart_template (which is the case for the generic one), the taxes can only be updated if
+        their country matches the fiscal country. """
+        country_lu = self.env.ref('base.lu')
+        self.company.account_fiscal_country_id = country_lu
+        self.tax_template_1.amount += 1
+        self.tax_template_2.invoice_repartition_line_ids.tag_ids.name = 'tag_name_2_modified'
+        with self.assertRaises(ValidationError):
+            update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
