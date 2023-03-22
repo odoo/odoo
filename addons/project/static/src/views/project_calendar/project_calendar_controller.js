3020f866ad5424ba1f1f7dd9d03f404e255fa99b
Kamlesh Pathekar <kpt@odoo.com>
2023-03-22 05:42:36 +0000

    [IMP] project, web: modify calendar filter in project calendar view
    In this commit, we renamed the calendar filter in task from
    'Undefined' to 'Private' to be consistent with the terminology
    used in the other views task.
    
    task-3214107
    
    closes odoo/odoo#115621
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/static/src/views/project_calendar/project_calendar_controller.js
+++ addons/project/static/src/views/project_calendar/project_calendar_controller.js
@@ -1,8 +1,13 @@
 /** @odoo-module **/
 
 import { CalendarController } from "@web/views/calendar/calendar_controller";
+import { ProjectCalendarFilterPanel } from "./project_calendar_filter_panel/project_calendar_filter_panel";
 
 export class ProjectCalendarController extends CalendarController {
+    static components = {
+        ...ProjectCalendarController.components,
+        FilterPanel: ProjectCalendarFilterPanel,
+    };
     setup() {
         super.setup(...arguments);
         this.displayName += this.env._t(" - Tasks by Deadline");
