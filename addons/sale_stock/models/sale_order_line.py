f5f54899a912b895b3f9b1e474dac1f1b403ef90
Victor Feyens <vfe@odoo.com>
2023-03-23 13:49:37 +0000

    [CLN] sale_stock: remove old strange logic
    Commit b30afcbdb98fa6b444035a3bb43707f734bd07b5 introduced
    a strange logic where the invoice status was magically considered
    invoiced on locked orders if there was no pending stock logic and
    the full delivered qty was invoiced.
    
    Following our removal of the locked status, replaced by another boolean field,
    we consider it's wrong to rely on the locked status for functional logic,
    therefore removing this old logic.
    
    If it's really a problem (and comes back in the future), another clean solution
    will be thought and implemented.
    
    task-3163931
    
    Part-of: odoo/odoo#115871

--- addons/sale_stock/models/sale_order_line.py
+++ addons/sale_stock/models/sale_order_line.py
@@ -187,33 +187,6 @@ class SaleOrderLine(models.Model):
             lines._action_launch_stock_rule(previous_product_uom_qty)
         return res
 
-    @api.depends('order_id.locked')  # Why the fuck does this depend on whether the SO is locked or not ???
-    def _compute_invoice_status(self):
-        def check_moves_state(moves):
-            # All moves states are either 'done' or 'cancel', and there is at least one 'done'
-            at_least_one_done = False
-            for move in moves:
-                if move.state not in ['done', 'cancel']:
-                    return False
-                at_least_one_done = at_least_one_done or move.state == 'done'
-            return at_least_one_done
-        super()._compute_invoice_status()
-        for line in self:
-            # We handle the following specific situation: a physical product is partially delivered,
-            # but we would like to set its invoice status to 'Fully Invoiced'. The use case is for
-            # products sold by weight, where the delivered quantity rarely matches exactly the
-            # quantity ordered.
-            if (
-                line.state == 'sale'
-                and line.order_id.locked  # Why only for locked orders ???
-                and line.invoice_status == 'no'
-                and line.product_id.type in ['consu', 'product']
-                and line.product_id.invoice_policy == 'delivery'
-                and line.move_ids
-                and check_moves_state(line.move_ids)
-            ):
-                line.invoice_status = 'invoiced'
-
     @api.depends('move_ids')
     def _compute_product_updatable(self):
         super()._compute_product_updatable()
