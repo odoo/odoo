e66857e733a70a5dabd07aeb256852db27e8fd6e
Guillaume (gdi) <gdi@odoo.com>
2023-03-23 14:02:58 +0100

    [FIX] website: ignore the scheme for page indexing
    When a user sets up a domain name on Odoo, we consider that he has a
    configuration that makes only one site visible. To do this, the standard
    solution is to have the following redirections:
    - http://example.com => https://example.com
    - https://www.example.com => https://example.com
    - http://www.example.com => https://example.com
    
    It happens that users enter something other than https://example.com in
    the setting to define the domain names of their websites. If this is the
    case, [this other commit] would cause an error:
    - The page indexing bot went to https://example.com but since this was
    not what was set in the settings, a no index was added so that the page
    was not referenced.
    - As soon as the indexing bot went to http://example.com,
    https://www.example.com or http://www.example.com, it was redirected to
    https://example.com.
    
    As a result, the client ended up with a non-indexed website.
    The purpose of [this other commit] was just to prevent double indexation
    of websites (the https://example.odoo.com and the https://example.com).
    
    After this commit, the pages will be indexed even if the scheme is not
    the same as the one specified in the settings. The same goes for the
    www. which is also ignored.
    
    Note that this can have an undesirable effect if the client has a bad
    configuration and has several sites exposed (https://example.com and
    https://www.example.com for example). If this is the case, he will end
    up with a site that is indexed twice.
    
    [this other commit]: https://github.com/odoo/odoo/commit/3739d74afe824554b37b1b52ed32ada33692c01a
    
    task-3110888

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -18,7 +18,7 @@ from werkzeug.exceptions import NotFound
 from odoo import api, fields, models, tools, http, release, registry
 from odoo.addons.http_routing.models.ir_http import slugify, _guess_mimetype, url_for
 from odoo.addons.website.models.ir_http import sitemap_qs2dom
-from odoo.addons.website.tools import similarity_score, text_from_html
+from odoo.addons.website.tools import similarity_score, text_from_html, get_base_domain
 from odoo.addons.portal.controllers.portal import pager
 from odoo.addons.iap.tools import iap_tools
 from odoo.exceptions import UserError, AccessError
@@ -295,6 +295,20 @@ class Website(models.Model):
         self._force()
         configurator_action_todo = self.env.ref('website.website_configurator_todo')
         return configurator_action_todo.action_launch()
+
+    def _is_indexable_url(self, url):
+        """
+        Returns True if the given url has to be indexed by search engines.
+        It is considered that the website must be indexed if the domain name
+        matches the URL. We check if they are equal while ignoring the www. and
+        http(s). This is to index the site even if the user put the www. in the
+        settings while he has a configuration that redirects the www. to the
+        naked domain for example (same thing for http and https).
+
+        :param url: the url to check
+        :return: True if the url has to be indexed, False otherwise
+        """
+        return get_base_domain(url, True) == get_base_domain(self.domain, True)
 
     # ----------------------------------------------------------
     # Configurator
@@ -991,7 +1005,7 @@ class Website(models.Model):
             is host:port in the version of `url_parse` we use."""
             # Here we add http:// to the domain if it's not set because
             # `url_parse` expects it to be set to correctly return the `netloc`.
-            website_domain = urls.url_parse(website.domain or '').netloc
+            website_domain = get_base_domain(website.domain)
             if ignore_port:
                 website_domain = _remove_port(website_domain)
                 domain_name = _remove_port(domain_name)
