80afbed633a56c180693bcee54db30b72ac925c1
Yashkumar Vaishnav <yava@odoo.com>
2023-03-24 18:58:24 +0530

    [FIX] analytic: fix multi company issue in default plan
    Steps to reproduce the issue:
    -First of all, you can create a multi-company environment and generate different
     analytic plans for each company, assigning parent and child relationships
     between them.
    -Once this is done when trying to create a project with only one of the companies
    and a timesheet, it results in an Access Error.
    
    Cause:
    -After getting the first step try to create a project using the previously
    created analytic plan, while now you do not have access to that company its
    generating access error
    
    Fix:
    -To fix the issue, _check_company_auto = True is set on the class, and assign
    check_company=True as an attribute to the parent_id field.
    
    task-3247213
    
    closes odoo/odoo#145724
    
    X-original-commit: b33dbe416aa4345327ddf9f6445fd792bbcc9bf8
    Related: odoo/enterprise#52501
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/analytic/models/analytic_plan.py
+++ addons/analytic/models/analytic_plan.py
@@ -11,6 +11,7 @@ class AccountAnalyticPlan(models.Model):
     _parent_store = True
     _rec_name = 'complete_name'
     _order = 'complete_name asc'
+    _check_company_auto = True
 
     def _default_color(self):
         return randint(1, 11)
@@ -22,6 +23,7 @@ class AccountAnalyticPlan(models.Model):
         string="Parent",
         ondelete='cascade',
         domain="[('id', '!=', id), ('company_id', 'in', [False, company_id])]",
+        check_company=True,
     )
     parent_path = fields.Char(
         index='btree',
