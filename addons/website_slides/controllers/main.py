97bddde5f8d48d6ed6d4cf31c309a33933961d75
Andro Gvivradze <angv@odoo.com>
2023-03-24 13:42:16 +0000

    [IMP] website_slides: improve quiz ux/ui
    This commit improves ux/ui by adding the slide
    description on top of a quiz and offering links to additional
    resources if the user gets questions wrong.
    
    task-3246366
    
    closes odoo/odoo#117528
    
    X-original-commit: a551f956f05bf7b6782e04a95351bf2d0a0a6610
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_slides/controllers/main.py
+++ addons/website_slides/controllers/main.py
@@ -137,17 +137,26 @@ class WebsiteSlides(WebsiteProfile):
 
     def _get_slide_quiz_data(self, slide):
         is_designer = request.env.user.has_group('website.group_website_designer')
+        slides_resources = slide.slide_resource_ids if slide.channel_id.is_member else []
         values = {
+            'slide_description': slide.description,
             'slide_questions': [{
+                'answer_ids': [{
+                    'comment': answer.comment if is_designer else None,
+                    'id': answer.id,
+                    'is_correct': answer.is_correct if slide.user_has_completed or is_designer else None,
+                    'text_value': answer.text_value,
+                } for answer in question.sudo().answer_ids],
                 'id': question.id,
                 'question': question.question,
-                'answer_ids': [{
-                    'id': answer.id,
-                    'text_value': answer.text_value,
-                    'is_correct': answer.is_correct if slide.user_has_completed or is_designer else None,
-                    'comment': answer.comment if is_designer else None
-                } for answer in question.sudo().answer_ids],
-            } for question in slide.question_ids]
+            } for question in slide.question_ids],
+            'slide_resource_ids': [{
+                'display_name' : resource.display_name,
+                'download_url': resource._get_download_url(),
+                'id': resource.id,
+                'link': resource.link,
+                'resource_type': resource.resource_type,
+            } for resource in slides_resources]
         }
         if 'slide_answer_quiz' in request.session:
             slide_answer_quiz = json.loads(request.session['slide_answer_quiz'])
