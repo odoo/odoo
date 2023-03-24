bc25a68a814a1f010cad9e49c6471a5abcc1ea06
Pierrot (prro) <prro@odoo.com>
2023-03-24 15:49:51 +0000

    [IMP] l10n_nl: add triangular tax
    The Dutch localization was not using any mean by default to know if a
    transaction was triangular or not. Similar to the Belgian localization,
    the Dutch one can now use the new 0% "ABC-levering binnen EU" tax.
    The tax report can will now also display that additionnal information
    (without modifying the existing ones).
    
    task-3281024
    
    closes odoo/odoo#124535
    
    X-original-commit: b63f21b9bd445e352ff9505ea62eb2193bf2ab1e
    Related: odoo/enterprise#42292
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- addons/l10n_nl/__manifest__.py
+++ addons/l10n_nl/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Netherlands - Accounting',
-    'version': '3.0',
+    'version': '3.1',
     'category': 'Accounting/Localizations/Account Charts',
     'author': 'Onestein',
     'website': 'http://www.onestein.eu',
