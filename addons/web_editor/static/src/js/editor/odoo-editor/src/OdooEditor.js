e6ffe98a45f194bc4950b63d34b771478a8c4689
David Beguin <dbe@odoo.com>
2023-03-27 16:20:46 +0200

    add hook to allow pausing the editor event listeners

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -1801,6 +1801,15 @@ export class OdooEditor extends EventTarget {
     //--------------------------------------------------------------------------
     // Private
     //--------------------------------------------------------------------------
+    _toggleDomListeners(setActive) {
+        for (const [element, eventName, boundCallback] of this._domListeners) {
+            if (setActive) {
+                element.addEventListener(eventName, boundCallback);
+            } else {
+                element.removeEventListener(eventName, boundCallback);
+            }
+        }
+    }
 
     _removeDomListener() {
         for (const [element, eventName, boundCallback] of this._domListeners) {
