96d92ccb227d22332f7b1d21dd1406ab0ae58bae
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2023-03-27 07:59:49 +0000

    [FIX] l10n_ph: traceback on partner vat empty
    Create a customer with Country Philippines and no vat
    Try to save
    Traceback
    
    Fixup of f0e9a3ce7fec9bb86b8703a6d49c0bc70c80d5c3
    
    X-original-commit: 350eef2d153b11936a787fb4e066b628beb0f56e

--- addons/l10n_ph/models/res_partner.py
+++ addons/l10n_ph/models/res_partner.py
@@ -18,5 +18,25 @@ class ResPartner(models.Model):
     def _commercial_fields(self):
         return super()._commercial_fields() + ['branch_code']
 
+<<<<<<< HEAD
     def check_vat_ph(self, vat):
         return len(vat) == 17 and CHECK_VAT_PH_RE.match(vat)
+||||||| parent of 59624029c1a (temp)
+    @api.depends('vat', 'country_id')
+    def _compute_branch_code(self):
+        for partner in self:
+            branch_code = '000'
+            if partner.country_id.code == 'PH':
+                match = partner.__check_vat_ph_re.match(partner.vat)
+                branch_code = match and match.group(1) and match.group(1)[1:] or branch_code
+            partner.branch_code = branch_code
+=======
+    @api.depends('vat', 'country_id')
+    def _compute_branch_code(self):
+        for partner in self:
+            branch_code = '000'
+            if partner.country_id.code == 'PH' and partner.vat:
+                match = partner.__check_vat_ph_re.match(partner.vat)
+                branch_code = match and match.group(1) and match.group(1)[1:] or branch_code
+            partner.branch_code = branch_code
+>>>>>>> 59624029c1a (temp)
