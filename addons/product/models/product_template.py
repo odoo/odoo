4d9da8b9eb86e6f0dc439d56f561113f397d4b90
Tejas Modi <temo@odoo.com>
2023-03-28 13:05:26 +0000

    [IMP] product: rename labels in product list view
    In this commit, I have make responsible optional hidden and rename some labels
    in product list view.
    
    TaskID - 3150983
    
    Part-of: odoo/odoo#116799

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -179,9 +179,10 @@ class ProductTemplate(models.Model):
         for template in self:
             template.currency_id = template.company_id.sudo().currency_id.id or main_company.currency_id.id
 
+    @api.depends('company_id')
     @api.depends_context('company')
     def _compute_cost_currency_id(self):
-        self.cost_currency_id = self.env.company.currency_id.id
+        self.cost_currency_id = self.company_id.currency_id or self.env.company.currency_id.id
 
     def _compute_template_field_from_variant_field(self, fname, default=False):
         """Sets the value of the given field based on the template variant values
