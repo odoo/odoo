5e740575ae771892111ef7638b84c3f7cfc3b999
Andro Gvivradze <angv@odoo.com>
2023-03-29 10:37:59 +0200

    [IMP] event[_booth]_sale: add ticket and booth in sale order template
    This commit allows us to add the events on sales order templates, and then
    configure them in the sales order quotations. Users will get an error when
    trying to confirm a quotation if events are not configured, and they will
    be able to configure said events by clicking on them.
    
    Task-2988003

--- addons/event_sale/models/sale_order_template_line.py
+++
@@ -1,8 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import fields, models
-
-class SaleOrderTemplateLine(models.Model):
-    _inherit = "sale.order.template.line"
-
-    product_id = fields.Many2one(domain="[('sale_ok', '=', True), ('detailed_type', '!=', 'event'), ('company_id', 'in', [company_id, False])]")
