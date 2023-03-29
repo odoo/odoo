bebf15b7c40b48eb40c27a404922e32b95c00349
Julien Castiaux <juc@odoo.com>
2023-03-29 15:36:37 +0200

    [IMP] test_new_api: relational fields queries
    This commit hads a few tests that report the previous (before merging
    subqueries WHERE clauses) situation.
    
    task-id-3234671
    Co-authored-by: Raphaël Collet <rco@odoo.com>

--- odoo/addons/test_new_api/tests/test_new_fields.py
+++ odoo/addons/test_new_api/tests/test_new_fields.py
@@ -3791,6 +3791,257 @@ def update(model, *fnames):
     )
 
 
+class TestRelationalQueries(common.TransactionCase):
+    """ Test the queries made by search() with relational fields. """
+    maxDiff = None
+
+    def test_00_many2one_multiple_or_predicates(self):
+        with self.assertQueries(["""
+            SELECT "test_new_api_multi"."id"
+            FROM "test_new_api_multi"
+            WHERE (("test_new_api_multi"."partner" IN (
+                SELECT "res_partner"."id"
+                FROM "res_partner"
+                WHERE ("res_partner"."name"::text LIKE %s)
+            )) OR ("test_new_api_multi"."partner" IN (
+                SELECT "res_partner"."id"
+                FROM "res_partner"
+                WHERE ("res_partner"."phone"::text LIKE %s)
+            )))
+            ORDER BY "test_new_api_multi"."id"
+        """]):
+            self.env['test_new_api.multi'].search([
+                '|',
+                    ('partner.name', 'like', 'jack'),
+                    ('partner.phone', 'like', '01234'),
+            ])
+
+    def test_01_many2one_multiple_and_predicates(self):
+        with self.assertQueries(["""
+            SELECT "test_new_api_multi"."id"
+            FROM "test_new_api_multi"
+            WHERE (("test_new_api_multi"."partner" IN (
+                SELECT "res_partner"."id"
+                FROM "res_partner"
+                WHERE ("res_partner"."name"::text LIKE %s)
+            )) AND ("test_new_api_multi"."partner" IN (
+                SELECT "res_partner"."id"
+                FROM "res_partner"
+                WHERE ("res_partner"."phone"::text LIKE %s)
+            )))
+            ORDER BY "test_new_api_multi"."id"
+        """]):
+            self.env['test_new_api.multi'].search([
+                ('partner.name', 'like', 'jack'),
+                ('partner.phone', 'like', '01234'),
+            ])
+
+    def test_02_many2one_multiple_mixed_predicates(self):
+        with self.assertQueries(["""
+            SELECT "test_new_api_multi"."id"
+            FROM "test_new_api_multi"
+            WHERE (("test_new_api_multi"."partner" IN (
+                SELECT "res_partner"."id"
+                FROM "res_partner"
+                WHERE ("res_partner"."email"::text LIKE %s)
+            )) AND ((
+                "test_new_api_multi"."partner" IN (
+                    SELECT "res_partner"."id"
+                    FROM "res_partner"
+                    WHERE ("res_partner"."name"::text LIKE %s)
+                )) OR ("test_new_api_multi"."partner" IN (
+                    SELECT "res_partner"."id"
+                    FROM "res_partner"
+                    WHERE ("res_partner"."phone"::text LIKE %s)
+                )
+            )))
+            ORDER BY "test_new_api_multi"."id"
+        """]):
+            self.env['test_new_api.multi'].search([
+                ('partner.email', 'like', '@sgc.us'),
+                '|',
+                    ('partner.name', 'like', 'jack'),
+                    ('partner.phone', 'like', '01234'),
+            ])
+
+    def test_10_one2many_multiple_or_predicates(self):
+        with self.assertQueries(["""
+            SELECT "test_new_api_multi"."id"
+            FROM "test_new_api_multi"
+            WHERE (("test_new_api_multi"."id" IN (
+                SELECT "test_new_api_multi_line"."multi"
+                FROM "test_new_api_multi_line"
+                WHERE ("test_new_api_multi_line"."name"::text LIKE %s)
+                      AND "test_new_api_multi_line"."multi" IS NOT NULL
+            )) OR ("test_new_api_multi"."id" IN (
+                SELECT "test_new_api_multi_line"."multi"
+                FROM "test_new_api_multi_line"
+                WHERE ("test_new_api_multi_line"."name"::text LIKE %s)
+                      AND "test_new_api_multi_line"."multi" IS NOT NULL
+            )))
+            ORDER BY "test_new_api_multi"."id"
+        """]):
+            self.env['test_new_api.multi'].search([
+                '|',
+                    ('lines.name', 'like', 'x'),
+                    ('lines.name', 'like', 'y'),
+            ])
+
+    def test_11_one2many_multiple_and_predicates(self):
+        with self.assertQueries(["""
+            SELECT "test_new_api_multi"."id"
+            FROM "test_new_api_multi"
+            WHERE (("test_new_api_multi"."id" IN (
+                SELECT "test_new_api_multi_line"."multi"
+                FROM "test_new_api_multi_line"
+                WHERE ("test_new_api_multi_line"."name"::text LIKE %s)
+                      AND "test_new_api_multi_line"."multi" IS NOT NULL
+            )) AND ("test_new_api_multi"."id" IN (
+                SELECT "test_new_api_multi_line"."multi"
+                FROM "test_new_api_multi_line"
+                WHERE ("test_new_api_multi_line"."name"::text LIKE %s)
+                      AND "test_new_api_multi_line"."multi" IS NOT NULL
+            )))
+            ORDER BY "test_new_api_multi"."id"
+        """]):
+            self.env['test_new_api.multi'].search([
+                ('lines.name', 'like', 'x'),
+                ('lines.name', 'like', 'y'),
+            ])
+
+    def test_12_one2many_multiple_mixed_predicates(self):
+        with self.assertQueries(["""
+            SELECT "test_new_api_multi"."id"
+            FROM "test_new_api_multi"
+            WHERE (("test_new_api_multi"."id" IN (
+                SELECT "test_new_api_multi_line"."multi"
+                FROM "test_new_api_multi_line"
+                WHERE ("test_new_api_multi_line"."name"::text LIKE %s)
+                      AND "test_new_api_multi_line"."multi" IS NOT NULL
+            )) AND ((
+                "test_new_api_multi"."id" IN (
+                    SELECT "test_new_api_multi_line"."multi"
+                    FROM "test_new_api_multi_line"
+                    WHERE ("test_new_api_multi_line"."name"::text LIKE %s)
+                          AND "test_new_api_multi_line"."multi" IS NOT NULL
+                )) OR ("test_new_api_multi"."id" IN (
+                    SELECT "test_new_api_multi_line"."multi"
+                    FROM "test_new_api_multi_line"
+                    WHERE ("test_new_api_multi_line"."name"::text LIKE %s)
+                          AND "test_new_api_multi_line"."multi" IS NOT NULL
+                ))
+            ))
+            ORDER BY "test_new_api_multi"."id"
+        """]):
+            self.env['test_new_api.multi'].search([
+                ('lines.name', 'like', 'x'),
+                '|',
+                    ('lines.name', 'like', 'y'),
+                    ('lines.name', 'like', 'z'),
+            ])
+
+    def test_20_many2many_multiple_or_predicates(self):
+        with self.assertQueries(["""
+            SELECT "test_new_api_multi"."id"
+            FROM "test_new_api_multi"
+            WHERE (EXISTS (
+                SELECT 1
+                FROM "test_new_api_multi_test_new_api_multi_tag_rel" AS "test_new_api_multi__tags"
+                WHERE "test_new_api_multi__tags"."test_new_api_multi_id" = "test_new_api_multi"."id"
+                AND "test_new_api_multi__tags"."test_new_api_multi_tag_id" IN (
+                    SELECT "test_new_api_multi_tag"."id"
+                    FROM "test_new_api_multi_tag"
+                    WHERE ("test_new_api_multi_tag"."name"::text LIKE %s)
+            )) OR EXISTS (
+                SELECT 1
+                FROM "test_new_api_multi_test_new_api_multi_tag_rel" AS "test_new_api_multi__tags"
+                WHERE "test_new_api_multi__tags"."test_new_api_multi_id" = "test_new_api_multi"."id"
+                AND "test_new_api_multi__tags"."test_new_api_multi_tag_id" IN (
+                    SELECT "test_new_api_multi_tag"."id"
+                    FROM "test_new_api_multi_tag"
+                    WHERE ("test_new_api_multi_tag"."name"::text LIKE %s)
+            )))
+            ORDER BY "test_new_api_multi"."id"
+        """]):
+            self.env['test_new_api.multi'].search([
+                '|',
+                    ('tags.name', 'like', 'x'),
+                    ('tags.name', 'like', 'y'),
+            ])
+
+    def test_21_many2many_multiple_or_predicates(self):
+        with self.assertQueries(["""
+            SELECT "test_new_api_multi"."id"
+            FROM "test_new_api_multi"
+            WHERE (EXISTS (
+                SELECT 1
+                FROM "test_new_api_multi_test_new_api_multi_tag_rel" AS "test_new_api_multi__tags"
+                WHERE "test_new_api_multi__tags"."test_new_api_multi_id" = "test_new_api_multi"."id"
+                AND "test_new_api_multi__tags"."test_new_api_multi_tag_id" IN (
+                    SELECT "test_new_api_multi_tag"."id"
+                    FROM "test_new_api_multi_tag"
+                    WHERE ("test_new_api_multi_tag"."name"::text LIKE %s)
+                )
+            ) AND EXISTS (
+                SELECT 1
+                FROM "test_new_api_multi_test_new_api_multi_tag_rel" AS "test_new_api_multi__tags"
+                WHERE "test_new_api_multi__tags"."test_new_api_multi_id" = "test_new_api_multi"."id"
+                AND "test_new_api_multi__tags"."test_new_api_multi_tag_id" IN (
+                    SELECT "test_new_api_multi_tag"."id"
+                    FROM "test_new_api_multi_tag"
+                    WHERE ("test_new_api_multi_tag"."name"::text LIKE %s)
+                )
+            ))
+            ORDER BY "test_new_api_multi"."id"
+        """]):
+            self.env['test_new_api.multi'].search([
+                ('tags.name', 'like', 'x'),
+                ('tags.name', 'like', 'y'),
+            ])
+
+    def test_22_many2many_multiple_mixed_predicates(self):
+        with self.assertQueries(["""
+            SELECT "test_new_api_multi"."id"
+            FROM "test_new_api_multi"
+            WHERE (EXISTS (
+                SELECT 1
+                FROM "test_new_api_multi_test_new_api_multi_tag_rel" AS "test_new_api_multi__tags"
+                WHERE "test_new_api_multi__tags"."test_new_api_multi_id" = "test_new_api_multi"."id"
+                AND "test_new_api_multi__tags"."test_new_api_multi_tag_id" IN (
+                    SELECT "test_new_api_multi_tag"."id"
+                    FROM "test_new_api_multi_tag"
+                    WHERE ("test_new_api_multi_tag"."name"::text LIKE %s)
+                )
+            ) AND (
+                EXISTS (
+                    SELECT 1
+                    FROM "test_new_api_multi_test_new_api_multi_tag_rel" AS "test_new_api_multi__tags"
+                    WHERE "test_new_api_multi__tags"."test_new_api_multi_id" = "test_new_api_multi"."id"
+                    AND "test_new_api_multi__tags"."test_new_api_multi_tag_id" IN (
+                        SELECT "test_new_api_multi_tag"."id"
+                        FROM "test_new_api_multi_tag"
+                        WHERE ("test_new_api_multi_tag"."name"::text LIKE %s)
+                    )
+                ) OR EXISTS (
+                    SELECT 1
+                    FROM "test_new_api_multi_test_new_api_multi_tag_rel" AS "test_new_api_multi__tags"
+                    WHERE "test_new_api_multi__tags"."test_new_api_multi_id" = "test_new_api_multi"."id"
+                    AND "test_new_api_multi__tags"."test_new_api_multi_tag_id" IN (
+                        SELECT "test_new_api_multi_tag"."id"
+                        FROM "test_new_api_multi_tag"
+                        WHERE ("test_new_api_multi_tag"."name"::text LIKE %s)
+                    )
+                )
+            ))
+            ORDER BY "test_new_api_multi"."id"
+        """]):
+            self.env['test_new_api.multi'].search([
+                ('tags.name', 'like', 'x'),
+                '|',
+                    ('tags.name', 'like', 'y'),
+                    ('tags.name', 'like', 'z'),
+            ])
+
 class TestComputeQueries(common.TransactionCase):
     """ Test the queries made by create() with computed fields. """
 
