be9ea6f7d8985a90c58bda01465890ac13ccaa5b
Renaud Thiry <reth@odoo.com>
2023-03-30 14:08:51 +0200

    [IMP] event_*: add unicode-based model indicators for template references
    Reference field in their 'readonly' state don't clearly mention the model
    they are based on. For communication templates we want to be able to tell
    at a glance from the list view what kind of templates we're using.
    
    task-3256524
    
    Part-of: odoo/odoo#152424
    Related: odoo/enterprise#60625
    Related: odoo/upgrade#5914
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- addons/event/__manifest__.py
+++ addons/event/__manifest__.py
@@ -55,6 +55,7 @@ Key Features
             'event/static/src/scss/event.scss',
             'event/static/src/icon_selection_field/icon_selection_field.js',
             'event/static/src/icon_selection_field/icon_selection_field.xml',
+            'event/static/src/template_reference_field/*',
             'event/static/src/js/tours/**/*',
         ],
         'web.assets_frontend': [
