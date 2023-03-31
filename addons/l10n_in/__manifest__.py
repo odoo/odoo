aa40aca8655c6dded8e219cbf5495f911510cfc3
Jigar Vaghela <jva@odoo.com>
2023-03-31 12:28:09 +0000

    [IMP] l10n_in: Audit Trail
    In Indian government required audit trail report for private limited companies
    So from this commit user can't delete journal entries after posting once.
    
    Create an audit trail report from mail.message to show a full log of journal entries
    
    task - 3239932
    
    X-original-commit: 1c07fa7ef4c879cf85667e658273ae40904e3569

--- addons/l10n_in/__manifest__.py
+++ addons/l10n_in/__manifest__.py
@@ -43,7 +43,8 @@ Sheet, now only Vertical format has been permitted Which is Supported By Odoo.
         'views/res_partner_views.xml',
         'views/account_tax_views.xml',
         'views/uom_uom_views.xml',
-        'data/account_chart_template_data.xml'
+        'data/account_chart_template_data.xml',
+        'report/audit_trail_report_views.xml',
     ],
     'demo': [
         'demo/demo_company.xml',
