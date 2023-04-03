d1eacd230ac3bb4a45181ea7f130c4343240958c
Bruno Boi <boi@odoo.com>
2023-04-03 08:47:54 +0200

    [FIX] web,*: restrict kanban examples availability
    *: crm,project,utm
    
    **Before this commit**
    With the following steps, it is possible to use kanban column quick
    creation with unexpected fields, i.e. creating projects instead of
    stages inside a project:
    - Project > New > Groupby 'Project'
    - Click on the "see examples" link in the column in creation
    - Apply any column examples
    - Instead of stages inside the project, new projects are created
    
    **After this commit**
    The kanban_examples registry elements should now clearly
    state which are the allowed groupby fields.
    The 'See examples' link will not be displayed if the groupby field
    is not allowed.
    
    **Usage**
    See the modified files in project and utm modules in this commit.
    
    Taskid: 3246042
    X-original-commit: a1f78062416c3f7deab7036e6adc0d70970f3403

--- addons/web/static/src/views/kanban/kanban_renderer.js
+++ addons/web/static/src/views/kanban/kanban_renderer.js
@@ -24,6 +24,16 @@ import { Component, useState, useRef, onPatched, onWillPatch, onWillDestroy } fr
 const DRAGGABLE_GROUP_TYPES = ["many2one"];
 const MOVABLE_RECORD_TYPES = ["char", "boolean", "integer", "selection", "many2one"];
 
+function validateColumnQuickCreateExamples(data) {
+    const { allowedGroupBys = [], examples = [] } = data;
+    if (!allowedGroupBys.length) {
+        throw new Error("The example data must contain an array of allowed groupbys");
+    }
+    if (!examples.length) {
+        throw new Error("The example data must contain an array of examples");
+    }
+}
+
 export class KanbanRenderer extends Component {
     static template = "web.KanbanRenderer";
     static components = {
@@ -58,6 +68,9 @@ export class KanbanRenderer extends Component {
         this.exampleData = registry
             .category("kanban_examples")
             .get(this.props.archInfo.examples, null);
+        if (this.exampleData) {
+            validateColumnQuickCreateExamples(this.exampleData);
+        }
         this.ghostColumns = this.generateGhostColumns();
 
         // Sortable
