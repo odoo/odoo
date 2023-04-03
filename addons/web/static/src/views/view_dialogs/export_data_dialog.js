6e821ef1a12f497f49d7f3d303b8638472fe5dc3
Filip Brcic <brcha@yandex.com>
2023-04-03 17:44:29 +0200

    [FIX] web: correctly update expandable fields in export data dialog
    Before this fix, expandable fields caused errors after compatible data
    was toggled due to expandable fields not being updated. This fix
    resolves that issue.

--- addons/web/static/src/views/view_dialogs/export_data_dialog.js
+++ addons/web/static/src/views/view_dialogs/export_data_dialog.js
@@ -159,6 +159,7 @@ export class ExportDataDialog extends Component {
     async fetchFields() {
         this.state.search = [];
         this.knownFields = {};
+        this.expandedFields = {};
         await this.loadFields();
         await this.setDefaultExportList();
         if (this.searchRef.el) {
