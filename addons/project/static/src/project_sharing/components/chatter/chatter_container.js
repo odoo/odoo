a7152758501869a62cef93611a85a58b74d8c46d
Thibault Delavallée <tde@odoo.com>
2023-04-03 13:45:59 +0200

    [REF] portal(_rating): make portal message_format standalone
    RATIONALE
    
    Purpose of this change to rewrite the formatting done on messages displayed
    on simple frontend i.e. portal chatter widget and project chatter.
    
    We stop calling 'message_format' which computes a lot of unnecessary data
    and sends too much information to frontend. We choose to instead manually
    handcraft the returned data, already tailored for frontend widget.
    
    SPECIFICATIONS
    
    Remove call to '_message_format' in 'portal_message_format'. Instead have
    a list of properties (fields or computation based on fields e.g. rating publisher
    information) that can be overridden in sub-addons. Use those to generate
    the data used by frontend chatter widget.
    
    Remove extra formatting or data computation done in JS files. Do it directly
    in 'portal_message_format' in order to have clean information sent.
    
    This change targets mainly portal and portal_rating. Making those modules
    Independent from backend message formatting allows to save queries and
    also to avoid sending useless information to the frontend.
    
    Task-3322905

--- addons/project/static/src/project_sharing/components/chatter/chatter_container.js
+++ addons/project/static/src/project_sharing/components/chatter/chatter_container.js
@@ -1,8 +1,6 @@
 /** @odoo-module */
 
-import { formatDateTime, parseDateTime } from "@web/core/l10n/dates";
 import { useService } from "@web/core/utils/hooks";
-import { sprintf } from '@web/core/utils/strings';
 import { ChatterComposer } from "./chatter_composer";
 import { ChatterMessageCounter } from "./chatter_message_counter";
 import { ChatterMessages } from "./chatter_messages";
@@ -124,16 +122,6 @@ export class ChatterContainer extends Component {
     preprocessMessages(messages) {
         return messages.map(m => ({
             ...m,
-            author_avatar_url: sprintf('/web/image/mail.message/%s/author_avatar/50x50', m.id),
-            published_date_str: sprintf(
-                this.env._t('Published on %s'),
-                formatDateTime(
-                    parseDateTime(
-                        m.date,
-                        { format: 'MM-dd-yyy HH:mm:ss' },
-                    ),
-                )
-            ),
             body: markup(m.body),
         }));
     }
