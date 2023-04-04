82787d2b987ae918f462565098b3d674451ee473
Walid HANNICHE (waha) <waha@odoo.com>
2023-04-04 16:12:23 +0000

    [FIX] purchase_stock: units rounding
    Steps to reproduce:
    1-create a weight unit 'jm' bigger than reference
        ratio 2.47541 rounding 0.001
    2-create a stored product uom:'jm' / purchase_uom: 'kg'
    3-set valuation method to automated (fifo)
    4-create a replenishement for 200 (jm)
    5-confirm and recieve the products
    6-valuation for that stock move is null
    
    Bug:
    computations in the stock module are done with rounding method (half-up)
    when computing the unit price the recieved quantity is computed with up
    rounding which leads to a mismatch
    
    Fix:
    applied the same rounding method on all the Steps
    
    opw-3213997
    
    closes odoo/odoo#118251
    
    X-original-commit: c305942958c73414def47ebd299227999841c6ed
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Walid Hanniche (waha) <waha@odoo.com>

--- addons/purchase_stock/models/stock_move.py
+++ addons/purchase_stock/models/stock_move.py
@@ -41,7 +41,7 @@ class StockMove(models.Model):
         order = line.order_id
         received_qty = line.qty_received
         if self.state == 'done':
-            received_qty -= self.product_uom._compute_quantity(self.quantity_done, line.product_uom)
+            received_qty -= self.product_uom._compute_quantity(self.quantity_done, line.product_uom, rounding_method='HALF-UP')
         if float_compare(line.qty_invoiced, received_qty, precision_rounding=line.product_uom.rounding) > 0:
             move_layer = line.move_ids.stock_valuation_layer_ids
             invoiced_layer = line.invoice_lines.stock_valuation_layer_ids
