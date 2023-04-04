e64a458fccba2a788b57fbe81f320e7350d35d36
Yashkumar Vaishnav <yava@odoo.com>
2023-04-04 04:39:48 +0000

    [FIX] project: fix customer rating issue in project
    Before this commit, even if customer rating is disabled from setting user can
    get subscription of task rating.
    
    This commit ensures that only when the customer rating field is enabled, the user can
    get a subscription to task rating.
    
    task-323170
    
    closes odoo/odoo#117577
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/models/res_config_settings.py
+++ addons/project/models/res_config_settings.py
@@ -65,6 +65,10 @@ class ResConfigSettings(models.TransientModel):
         if project_stage_change_mail_type.hidden == self['group_project_stages']:
             project_stage_change_mail_type.hidden = not self['group_project_stages']
         # Hide task rating tempalate when customer rating is disbled
+        task_rating_subtype_id = self.env.ref('project.mt_project_task_rating')
+        task_rating_subtype_id.hidden = not self['group_project_rating']
+        self.env.ref('project.mt_task_rating').hidden = not self['group_project_rating']
+        task_rating_subtype_id.default = self['group_project_rating']
         rating_project_request_email_template = self.env.ref('project.rating_project_request_email_template')
         if rating_project_request_email_template.active != self['group_project_rating']:
             rating_project_request_email_template.active = self['group_project_rating']
