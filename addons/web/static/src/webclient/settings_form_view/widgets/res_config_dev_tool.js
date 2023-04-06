db32e03c261ac86678f42f446171b37e9e322095
Antoine Dupuis (andu) <andu@odoo.com>
2023-04-06 13:01:01 +0000

    [IMP] tools, account: remove XSD crons; download XSD button
    At the moment, XSD files are automatically downloaded at database
    initialization, which is quite unnecessary.
    
    The idea is to change Odoo's use of XSDs from being systematically
    downloaded and used for validation to simply being available if desired
    (e.g. for development or for customers who want them).
    
    To achieve this, this commit does the following:
    
    - remove the XSD download crons;
    - provide a 'download XSDs' button in the Settings (next to the debug
      mode button) which is available in debug mode;
    - skip XSD validation if any required XSD file is not present; and
    - deprecate the 'force_reload' option.
    
    Entreprise PR: odoo/enterprise#38350
    Task id: 3010716
    
    closes odoo/odoo#118385
    
    X-original-commit: 9211a58743b87a79377122c73456c51dd1b67576
    Related: odoo/enterprise#39681
    Signed-off-by: Josse Colpaert <jco@odoo.com>
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/web/static/src/webclient/settings_form_view/widgets/res_config_dev_tool.js
+++ addons/web/static/src/webclient/settings_form_view/widgets/res_config_dev_tool.js
@@ -13,7 +13,7 @@ import { standardWidgetProps } from "@web/views/widgets/standard_widget_props";
  * Can be used to enable/disable the debug modes.
  * Can be used to load the demo data.
  */
-class ResConfigDevTool extends Component {
+export class ResConfigDevTool extends Component {
     setup() {
         this.isDebug = Boolean(odoo.debug);
         this.isAssets = odoo.debug.includes("assets");
