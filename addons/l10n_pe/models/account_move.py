40899740242f06eecfc413e76d125c9f4b8e7064
Luis Torres <luis_t@vauxoo.com>
2023-04-06 01:15:04 +0000

    [IMP] l10n_pe: Welcome PLE reports
    Complement module to allow generate the PLE reports.
    
    - Missing purchase tax groups and taxes added to complete the vendor
      bills flow
    - Demo data completed to has data to allow generate the PLE reports
    - Document types for vendor bills completed in the data
    - Method to improve the domain in document types for customer invoices
      added
    - Extend method to autocomplete the number on vendor bills with the
      standard expected by the document type.
    
    X-original-commit: 66faf037ebdb79c29a9066f1becd2f92059cab8a

--- addons/l10n_pe/models/account_move.py
+++ addons/l10n_pe/models/account_move.py
@@ -1,6 +1,35 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models, fields
+from odoo import api, models, fields
 from odoo.tools.sql import column_exists, create_column
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _get_l10n_latam_documents_domain(self):
+        self.ensure_one()
+        result = super()._get_l10n_latam_documents_domain()
+        if self.company_id.country_id.code != "PE" or not self.journal_id.l10n_latam_use_documents:
+            return result
+        if self.journal_id.type == "sale":
+            result.append(("code", "in", ("01", "03", "07", "08", "20", "40")))
+        return result
+
+    @api.onchange('l10n_latam_document_type_id', 'l10n_latam_document_number')
+    def _inverse_l10n_latam_document_number(self):
+        """Inherit to complete the l10n_latam_document_number with the expected 8 characters after that a '-'
+        Example: Change FFF-32 by FFF-00000032, to avoid incorrect values on the reports"""
+        super()._inverse_l10n_latam_document_number()
+        to_review = self.filtered(
+            lambda x: x.journal_id.type == "purchase"
+            and x.l10n_latam_document_type_id.code in ("01", "03", "07", "08")
+            and x.l10n_latam_document_number
+            and "-" in x.l10n_latam_document_number
+            and x.l10n_latam_document_type_id.country_id.code == "PE"
+        )
+        for rec in to_review:
+            number = rec.l10n_latam_document_number.split("-")
+            rec.l10n_latam_document_number = "%s-%s" % (number[0], number[1].zfill(8))
 
 
 class AccountMoveLine(models.Model):
