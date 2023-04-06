c8174c7914e567489de74b574b076467440bdb2d
Antoine Dupuis (andu) <andu@odoo.com>
2023-04-06 13:01:01 +0000

    [IMP] tools, account: remove XSD crons; download XSD button
    At the moment, XSD files are automatically downloaded at database
    initialization, which is quite unnecessary.
    
    The idea is to change Odoo's use of XSDs from being systematically
    downloaded and used for validation to simply being available if desired
    (e.g. for development or for customers who want them).
    
    To achieve this, this commit does the following:
    
    - remove the XSD download crons;
    - provide a 'download XSDs' button in the Settings (next to the debug
      mode button) which is available in debug mode;
    - skip XSD validation if any required XSD file is not present; and
    - deprecate the 'force_reload' option.
    
    Entreprise PR: odoo/enterprise#38350
    Task id: 3010716
    
    closes odoo/odoo#118677
    
    X-original-commit: db32e03c261ac86678f42f446171b37e9e322095
    Related: odoo/enterprise#39832
    Signed-off-by: Josse Colpaert <jco@odoo.com>
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -38,3 +38,4 @@ from . import test_account_payment_register
 from . import test_tour
 from . import test_early_payment_discount
 from . import test_ir_actions_report
+from . import test_download_xsds
