ccc6f1f6cf6eca72be9cc70b31ac690d59118a91
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2023-04-06 08:34:59 +0000

    [FIX] sale: sale order line quantity not rounded
    Create a Field Service Task
    Assing a customer, in Timesheet tab add a line with Hours Spent 00:20
    Mark as Done
    Sale Order will be created with a sale order line
    Total of the line and Total of the order will not match
    
    This occurs because the sale order line has been created with an
    unrounded quantity of 0.333333
    
    opw-3184468
    
    closes odoo/odoo#118601
    
    X-original-commit: ca72697042687e205fb288a97ec4717a97c89664
    Related: odoo/enterprise#39785
    Signed-off-by: Grazioso Andrea (agr) <agr@odoo.com>

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -948,16 +948,16 @@ class SaleOrderLine(models.Model):
 
     #=== CRUD METHODS ===#
     def _add_precomputed_values(self, vals_list):
-        """ In the specific case where the discount is provided in the create values
+        """ In case an editable precomputed field is provided in the create values
         without being rounded, we have to 'manually' round it otherwise it won't be,
-        because editable precomputed field values are kept 'as is'.
+        because those field values are kept 'as is'.
 
         This is a temporary fix until the problem is fixed in the ORM.
         """
-        precision = self.env['decimal.precision'].precision_get('Discount')
         for vals in vals_list:
-            if vals.get('discount'):
-                vals['discount'] = float_round(vals['discount'], precision_digits=precision)
+            for fname in ('discount', 'product_uom_qty'):
+                if fname in vals:
+                    vals[fname] = self._fields[fname].convert_to_cache(vals[fname], self)
         return super()._add_precomputed_values(vals_list)
 
     @api.model_create_multi
