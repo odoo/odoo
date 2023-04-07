bacd02f91a4f728f43e251917969b571a641a46d
Bastien Fafchamps (bafa) <bafa@odoo.com>
2023-04-07 13:25:39 +0000

    [IMP] web: Make AceField use the CodeEditor component
    Before this commit:
    The AceField component used the ace library to provide the user a code
    editing field
    
    After this commit:
    The AceField component is now using the CodeEditor component and the usage
    of the ace editor lib is now abstracted behind the component.
    
    closes odoo/odoo#117782
    
    Related: odoo/enterprise#39365
    Signed-off-by: Géry Debongnie <ged@odoo.com>

--- addons/web/static/src/views/fields/ace/ace_field.js
+++ addons/web/static/src/views/fields/ace/ace_field.js
@@ -1,14 +1,14 @@
 /** @odoo-module **/
-/* global ace */
 
-import { loadJS } from "@web/core/assets";
 import { _lt } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { useBus, useService } from "@web/core/utils/hooks";
 import { formatText } from "../formatters";
 import { standardFieldProps } from "../standard_field_props";
 
-import { Component, onWillStart, onWillUpdateProps, useEffect, useRef } from "@odoo/owl";
+import { CodeEditor } from "@web/core/code_editor/code_editor";
+import { Component, onWillUpdateProps, useState } from "@odoo/owl";
+import { useDebounced } from "@web/core/utils/timing";
 
 export class AceField extends Component {
     static template = "web.AceField";
@@ -19,33 +19,20 @@ export class AceField extends Component {
     static defaultProps = {
         mode: "qweb",
     };
+    static components = { CodeEditor };
 
     setup() {
-        this.aceEditor = null;
-        this.editorRef = useRef("editor");
         this.cookies = useService("cookie");
+        this.debouncedCommit = useDebounced(this.commitChanges, 10);
 
-        onWillStart(async () => {
-            await loadJS("/web/static/lib/ace/ace.js");
-            const jsLibs = [
-                "/web/static/lib/ace/mode-python.js",
-                "/web/static/lib/ace/mode-xml.js",
-                "/web/static/lib/ace/mode-qweb.js",
-            ];
-            const proms = jsLibs.map((url) => loadJS(url));
-            return Promise.all(proms);
+        this.state = useState({
+            onChange: (value) => this.handleChange(value),
+            class: "ace-view-editor",
+            theme: this.theme,
         });
 
-        onWillUpdateProps(this.updateAce);
-
-        useEffect(
-            () => {
-                this.setupAce();
-                this.updateAce(this.props);
-                return () => this.destroyAce();
-            },
-            () => [this.editorRef.el]
-        );
+        this.updateCodeEditor(this.props);
+        onWillUpdateProps(this.updateCodeEditor);
 
         const { model } = this.props.record;
         useBus(model.bus, "WILL_SAVE_URGENTLY", () => this.commitChanges());
@@ -54,65 +41,24 @@ export class AceField extends Component {
         );
     }
 
-    get aceSession() {
-        return this.aceEditor.getSession();
+    get theme() {
+        return this.cookies.current.color_scheme === "dark" ? "monokai" : "";
     }
 
-    setupAce() {
-        this.aceEditor = ace.edit(this.editorRef.el);
-        this.aceEditor.setOptions({
-            maxLines: Infinity,
-            showPrintMargin: false,
-            theme: this.cookies.current.color_scheme === "dark" ? "ace/theme/monokai" : "",
-        });
-        this.aceEditor.$blockScrolling = true;
-
-        this.aceSession.setOptions({
-            useWorker: false,
-            tabSize: 2,
-            useSoftTabs: true,
-        });
-
-        this.aceEditor.on("blur", this.commitChanges.bind(this));
+    handleChange(newValue) {
+        this.state.value = newValue;
+        this.debouncedCommit();
     }
 
-    updateAce({ mode, readonly, record }) {
-        if (!this.aceEditor) {
-            return;
-        }
-
-        this.aceSession.setOptions({
-            mode: `ace/mode/${mode === "xml" ? "qweb" : mode}`,
-        });
-
-        this.aceEditor.setOptions({
-            readOnly: readonly,
-            highlightActiveLine: !readonly,
-            highlightGutterLine: !readonly,
-        });
-
-        this.aceEditor.renderer.setOptions({
-            displayIndentGuides: !readonly,
-            showGutter: !readonly,
-        });
-
-        this.aceEditor.renderer.$cursorLayer.element.style.display = readonly ? "none" : "block";
-
-        const formattedValue = formatText(record.data[this.props.name]);
-        if (this.aceSession.getValue() !== formattedValue) {
-            this.aceSession.setValue(formattedValue);
-        }
-    }
-
-    destroyAce() {
-        if (this.aceEditor) {
-            this.aceEditor.destroy();
-        }
+    updateCodeEditor({ record, mode, readonly }) {
+        this.state.value = formatText(record.data[this.props.name]);
+        this.state.mode = mode === "xml" ? "qweb" : mode;
+        this.state.readonly = readonly;
     }
 
     commitChanges() {
         if (!this.props.readonly) {
-            const value = this.aceSession.getValue();
+            const value = this.state.value;
             if (this.props.record.data[this.props.name] !== value) {
                 return this.props.record.update({ [this.props.name]: value });
             }
@@ -130,3 +76,4 @@ export const aceField = {
 };
 
 registry.category("fields").add("ace", aceField);
+registry.category("fields").add("code", aceField);
