fd16f58b4449b7af08bbdaf6f12257466c4a4881
sofiagvaladze <sgv@odoo.com>
2023-04-07 10:07:58 +0200

    [FIX] hr_recruitment: correctly count the number of applications
    Previously, the "Other applications" smartbutton was incorrectly
    matching with the applicants with '' on the fields. We don't want
    applicants to match if both of them has '' on the matching fields.
    Moreover, record rules were not correctly applied for application_count
    calculation, as current record will not match with similar record
    which has no company defined on it.
    
    This commit fixes the above mentioned issues.
    
    task - 3266694
    
    X-original-commit: e8263e56423432e9465f70d26fce60c021eabaa2

--- addons/hr_recruitment/tests/test_recruitment.py
+++ addons/hr_recruitment/tests/test_recruitment.py
@@ -31,7 +31,7 @@ class TestRecruitment(TransactionCase):
         """ Test that we find same applicants based on simmilar mail,
             phone or mobile phone.
         """
-        A, B, C, D, E, F = self.env['hr.applicant'].create([
+        A, B, C, D, E, F, _ = self.env['hr.applicant'].create([
             {
                 'name': 'Application A',
                 'email_from': 'abc@odoo.com',
@@ -57,15 +57,54 @@ class TestRecruitment(TransactionCase):
             },
             {
                 'name': 'Application E',
+                'partner_phone': '',
             },
             {
                 'name': 'Application F',
                 'partner_phone': '11-12-13', # In case phone is configured in a wrong field
-            }
+            },
+            {
+                'name': 'Application G',
+                'partner_phone': '',
+            },
         ])
+
         self.assertEqual(A.application_count, 2) # C, D
         self.assertEqual(B.application_count, 2) # D, F
         self.assertEqual(C.application_count, 2) # A, D
         self.assertEqual(D.application_count, 3) # A, B, C
-        self.assertEqual(E.application_count, 0)
+        self.assertEqual(E.application_count, 0) # Should not match with G
         self.assertEqual(F.application_count, 1) # B
+
+    def test_application_count_multi_company(self):
+        company_A = self.env['res.company'].create({'name': 'A company'})
+        company_B = self.env['res.company'].create({'name': 'B company'})
+        appX, _, _ = self.env['hr.applicant'].create([
+            {
+                'name': 'Application X',
+                'email_from': 'abc@odoo.com',
+                'partner_phone': '123',
+                'partner_mobile': '14-15-16',
+                'company_id': company_A.id
+            },
+            {
+                'name': 'Application Y',
+                'email_from': 'abc@odoo.com',
+                'partner_phone': '123',
+                'partner_mobile': '14-15-16',
+                'company_id': company_B.id
+            },
+            {
+                'name': 'Application Z',
+                'email_from': 'abc@odoo.com',
+                'partner_phone': '123',
+                'partner_mobile': '14-15-16',
+                'company_id': False
+            },
+        ])
+
+        appX.with_context(allowed_company_ids=[company_A.id])._compute_application_count()
+        self.assertEqual(appX.application_count, 1, "Should not have counted the applicant from another company.") # Z
+
+        appX.with_context(allowed_company_ids=[company_A.id, company_B.id])._compute_application_count()
+        self.assertEqual(appX.application_count, 2, "Should have counted the applicant from another company as well.") # Y, Z
