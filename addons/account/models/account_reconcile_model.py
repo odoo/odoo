9503da2dc7ac4943c2e83f961994afc66a661c0d
Claire Bretton (clbr) <clbr@odoo.com>
2023-04-11 20:59:15 +0200

    [FIX] account: allow automatic reconciliation from reconciliation models
    Adds a button on reconciliation models to trigger synchronous automatic
    reconciliation. It will handle as many transactions as possible within allocated time.
    
    Task: 3259126
    Related: odoo/enterprise#39143

--- addons/account/models/account_reconcile_model.py
+++ addons/account/models/account_reconcile_model.py
@@ -1023,3 +1023,11 @@ class AccountReconcileModel(models.Model):
             return {'allow_write_off', 'allow_auto_reconcile'}
 
         return {'rejected'}
+
+    def run_auto_reconciliation(self):
+        """ Tries to auto-reconcile as many statements as possible within time limit
+        arbitrary set to 3 minutes (the rest will be reconciled asynchronously with the regular cron).
+        """
+        cron_limit_time = tools.config['limit_time_real_cron']  # default is -1
+        limit_time = cron_limit_time if 0 < cron_limit_time < 180 else 180
+        self.env['account.bank.statement.line']._cron_try_auto_reconcile_statement_lines(limit_time=limit_time)
