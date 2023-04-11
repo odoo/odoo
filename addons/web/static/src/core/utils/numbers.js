f61c3870dc8921173d8c056eb010dd79fd5b8a5d
Mathieu Duckerts-Antoine <dam@odoo.com>
2023-04-11 07:05:59 +0000

    [REF] web: move computeVariation in pivot_model
    Since the dashboard view has been removed, there was only one call to
    computeVariation of utils/numbers. We move the definition of that
    function where it is used (pivot_model.js).
    
    Part-of: odoo/odoo#118057

--- addons/web/static/src/core/utils/numbers.js
+++ addons/web/static/src/core/utils/numbers.js
@@ -1,27 +1,6 @@
 /** @odoo-module **/
 
 /**
- * @param {number} value
- * @param {number} comparisonValue
- * @returns {number}
- */
-export function computeVariation(value, comparisonValue) {
-    if (isNaN(value) || isNaN(comparisonValue)) {
-        return NaN;
-    }
-    if (comparisonValue === 0) {
-        if (value === 0) {
-            return 0;
-        } else if (value > 0) {
-            return 1;
-        } else {
-            return -1;
-        }
-    }
-    return (value - comparisonValue) / Math.abs(comparisonValue);
-}
-
-/**
  * Returns value clamped to the inclusive range of min and max.
  *
  * @param {number} num
