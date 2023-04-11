6837f91b38bf16a59bd2963c52b3a790598e7810
Lucas Lefèvre <lul@odoo.com>
2023-04-11 12:05:33 +0000

    [FIX] documents_spreadsheet: list dates with correct format
    Commit odoo/enterprise@4bbf70f was fixing a timezone issue. Datetimes coming
    from lists inserted in a spreadsheet were always displayed in UTC.
    
    However, this commit also changed the formatting behavior.
    Before the fix: date/datetimes were dispalyed with the server format
    After: they are displayed with the local format (FR if the user lang
    is French)
    
    This can lead to issues because spreadsheet only understands a
    handfull of date(time) format.
    For example, it doesn't understand dd/mm/yyyy format but only
    the other way around (mm/dd/yyyy)
    
    This commit restores the previous format (server format), while
    preserving the timezone conversion.
    
    opw-3251586
    
    Manual forward-port of odoo/enterprise#39438
    
    closes odoo/odoo#118285
    
    X-original-commit: bf84195bce4fbea55d1a4e6a6cd9f17d9be86b73
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet/static/src/list/list_data_source.js
+++ addons/spreadsheet/static/src/list/list_data_source.js
@@ -172,12 +172,18 @@ export default class ListDataSource extends OdooViewsDataSource {
 
     _formatDateTime(dateValue) {
         const date = deserializeDateTime(dateValue);
-        return formatDateTime(date);
+        return formatDateTime(date, {
+            format: "yyyy-MM-dd HH:mm:ss",
+            numberingSystem: "latn",
+        });
     }
 
     _formatDate(dateValue) {
         const date = deserializeDate(dateValue);
-        return formatDate(date);
+        return formatDate(date, {
+            format: "yyyy-MM-dd",
+            numberingSystem: "latn",
+        });
     }
 
     /**
