0f23f4ea4f7fff5b14dca83ed7eee8a81b96fe26
Juan Jose Scarafia <jjs@adhoc.com.ar>
2023-04-12 17:37:31 -0300

    [IMP] first functional POC

--- addons/l10n_ar_withholding/wizards/account_payment_register.py
+++ addons/l10n_ar_withholding/wizards/account_payment_register.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError
 
 
 class AccountPaymentRegisterWithholding(models.TransientModel):
@@ -18,6 +17,7 @@ class AccountPaymentRegisterWithholding(models.TransientModel):
     @api.depends('tax_id', 'payment_register_id.line_ids', 'payment_register_id.amount')
     def _compute_base_amount(self):
         # TODO improve this method and send some funcionality to account.tax, this approach is for demo purpose
+        # TODO also consider multicurrency use case
         for line in self:
             factor = line.payment_register_id.amount / line.payment_register_id.source_amount
             if not line.tax_id:
@@ -58,33 +58,40 @@ class AccountPaymentRegister(models.TransientModel):
         for rec in self:
             rec.net_amount = rec.amount - sum(rec.withholding_ids.mapped('amount'))
 
-    # TODO implement here instead on _create_payments
-    # def _create_payment_vals_from_wizard(self):
-    #     payment_vals = super()._create_payment_vals_from_wizard()
+    # def _create_payment_vals_from_wizard(self, batch_result):
+    #     payment_vals = super()._create_payment_vals_from_wizard(batch_result)
+    #     import pdb; pdb.set_trace()
     #     if self.withholding_ids:
-    #         payment_vals['withholding_vals'] = [{
-    #             'name': line.name,
-    #             'amount': line.amount,
-    #             'account_id': 5,
-    #             'tax_base_amount': line.base_amount,
-    #             'tax_id': line.tax_id.id,
-    #         } for line in self.withholding_ids]
+    #         # payment_vals['withholding_vals'] = [{
+    #         #     'name': line.name,
+    #         #     'amount': line.amount,
+    #         #     'account_id': 5,
+    #         #     'tax_base_amount': line.base_amount,
+    #         #     'tax_id': line.tax_id.id,
+    #         # } for line in self.withholding_ids]
     #     return payment_vals
 
+    # TODO implement on _create_payment_vals_from_wizard instead of here
     def _create_payments(self):
         payments = super()._create_payments()
         if self.withholding_ids:
             for payment in payments.with_context(check_move_validity=False):
-                liquidity_lines, counterpart_lines, writeoff_lines = payment._seek_for_lines()
-                line_ids = []
-                for line in self.withholding_ids:
-                    line_ids += [(0, 0, payment._prepare_withholding_line_vals(line.tax_id, line.amount, line.base_amount, line.name))]
-                liquidity_lines.debit = self.net_amount
-
                 # TODO remove this hack when creating withholdings properly
+                liquidity_lines, counterpart_lines, writeoff_lines = payment._seek_for_lines()
                 counterpart_lines.parent_state = 'draft'
+                # TODO ver el caso de mas de una liquidity_lines
+                rate = liquidity_lines.amount_currency / liquidity_lines.balance if liquidity_lines.balance else 1
+                liquidity_lines.balance = self.net_amount / rate * (-1 if liquidity_lines.tax_tag_invert else 1)
+                liquidity_lines.amount_currency = self.net_amount * (-1 if liquidity_lines.tax_tag_invert else 1)
                 counterpart_lines.tax_ids = self.withholding_ids.mapped('tax_id')
+                for line in self.withholding_ids:
+                    tax_line = payment.line_ids.filtered(lambda x: x.tax_line_id == line.tax_id)
+                    rate = tax_line.amount_currency / tax_line.balance if tax_line.balance else 1
+                    tax_line.write({
+                        'amount_currency': line.amount,
+                        'tax_base_amount': line.base_amount / rate * (-1 if tax_line.tax_tag_invert else 1),
+                        'balance': line.amount / rate,
+                    })
                 counterpart_lines.parent_state = 'posted'
 
-                payment.move_id.write({'line_ids': line_ids})
         return payments
