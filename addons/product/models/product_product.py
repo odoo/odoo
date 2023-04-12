3007e396ecda11da4d230becbd0604e4144e2e5d
Preksha Chouhan <prec@odoo.com>
2023-04-12 09:00:11 +0000

    [FIX] product: prevent getting value of domain from other method
    While creating a new product in product category with barcode value in inventory
    module, `_check_barcode_uniqueness` method is called. In which variable 'domain'
    is passed, and getting wrong value.
    
    Steps to Produce:-
    1) Go to Inventory then configuration
    2) Click on 'Product Categories' under products
    3) Click/Create Product Category
    4) Click stat button 'product'
    5) Create a product
    6) Then add barcode value
    7) Click on Save
    8) Trace-back will be generated
    
    Reason:-
    
    While creating new product from product category when user add the barcode value
    in product form, the '_check_barcode_uniqueness' method is called. In this
    method the value of 'domain' is getting updated by reference from variable
    'domain' from method '_search' present in the same model.
    This results in a traceback with the message
    
    'Invalid field product.packaging.categ_id'.
    
    Applying these changes will resolve this issue.
    
    sentry - 4073963761
    
    closes odoo/odoo#118339
    
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- addons/product/models/product_product.py
+++ addons/product/models/product_product.py
@@ -397,6 +397,7 @@ class ProductProduct(models.Model):
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
         # TDE FIXME: strange
         if self._context.get('search_default_categ_id'):
+            args = args.copy()
             args.append((('categ_id', 'child_of', self._context['search_default_categ_id'])))
         return super(ProductProduct, self)._search(args, offset=offset, limit=limit, order=order, count=count, access_rights_uid=access_rights_uid)
 
