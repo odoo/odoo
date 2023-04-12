4f770806c7ea2dfef647a62384e4cba95f6676ef
Victor Piryns (pivi) <pivi@odoo.com>
2023-04-12 11:23:44 +0000

    [FIX] project: dup milestones on tasks when dup project
    Current behaviour:
    When duplicating a project, new milestones are copied for the new
    project, but none of them are assigned to the copied tasks like in
    the original project.
    
    Expected behaviour:
    The new tasks in the new project should have the corresponding copy
    of the milestone that were assigned in the original project.
    
    Steps to reproduce:
    - Install Project
    - Duplicate "Office Design" (it has milestones)
    - Observe that the tasks in the new project don't have milestones
      assigned to them, like in the original project.
    
    Reason for the problem:
    When we copy the tasks, they have the milestones of the original
    project correctly assigned to them, but since the project of the
    milestone is different from the project of the task (former
    references the original project, while the latter references the
    copied project), so in `_compute_milestone_id`, the milestone of the
    task is set to False.
    
    Fix:
    Remove `copy=True` from `milestone_ids` on the project, and copy the
    milestone by hand. This allows us to use an overwrite of `copy()`
    for `project.milestone`, and we create a mapping between the old
    milestones and the new ones in the context, similar to how we did
    with `task_mapping`. With this we can assign the newly created
    milestones on the copied tasks correctly (while preserving the
    mapping like in the original project).
    
    Affected versions:
    - 16.0
    - saas-16.1
    - saas-16.2
    - master
    
    opw-3254868
    
    closes odoo/odoo#118454
    
    X-original-commit: aad60a2c8a887f3ea226af99be9b162673c33ec8
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -424,7 +424,7 @@ class Project(models.Model):
         ('done', 'Done'),
     ], default='to_define', compute='_compute_last_update_status', store=True, readonly=False, required=True)
     last_update_color = fields.Integer(compute='_compute_last_update_color')
-    milestone_ids = fields.One2many('project.milestone', 'project_id', copy=True)
+    milestone_ids = fields.One2many('project.milestone', 'project_id')
     milestone_count = fields.Integer(compute='_compute_milestone_count', groups='project.group_project_milestone')
     milestone_count_reached = fields.Integer(compute='_compute_milestone_reached_count', groups='project.group_project_milestone')
     is_milestone_exceeded = fields.Boolean(compute="_compute_is_milestone_exceeded", search='_search_is_milestone_exceeded')
@@ -626,6 +626,10 @@ class Project(models.Model):
         project = super(Project, self_with_mail_context).copy(default)
         for follower in self.message_follower_ids:
             project.message_subscribe(partner_ids=follower.partner_id.ids, subtype_ids=follower.subtype_ids.ids)
+        if self.allow_milestones:
+            if 'milestone_mapping' not in self.env.context:
+                self = self.with_context(milestone_mapping=dict())
+            project.milestone_ids = [milestone.copy().id for milestone in self.milestone_ids]
         if 'tasks' not in default:
             self.map_tasks(project.id)
 
@@ -1764,6 +1768,9 @@ class Task(models.Model):
             self.write({'dependent_ids': [Command.unlink(t.id) for t in self.dependent_ids if t.id in new_tasks]})
             task_copy.write({'depend_on_ids': [Command.link(task_mapping.get(t.id, t.id)) for t in self.depend_on_ids]})
             task_copy.write({'dependent_ids': [Command.link(task_mapping.get(t.id, t.id)) for t in self.dependent_ids]})
+        if self.allow_milestones:
+            milestone_mapping = self.env.context.get('milestone_mapping', {})
+            task_copy.milestone_id = milestone_mapping.get(task_copy.milestone_id.id, task_copy.milestone_id.id)
         return task_copy
 
     @api.model
