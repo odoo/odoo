a66bb1b8e0736ee538780cd186139f62fda49aab
dasz <dasz@odoo.com>
2023-04-13 09:23:26 +0000

    [IMP] hr,hr_presence,hr_attendance,hr_holidays: move presence computation to employee_base
    Employee presence is not a sensitive data and should be available for all db users
    this commit moves the computations from attendance, holidays and hr_presence to the
    employee_base model, which is how it was originaly made in the hr module. This way
    the presence marker does not depend on the user's hr rights
    
    closes odoo/odoo#118437
    
    Task: 3267790
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_attendance/models/__init__.py
+++ addons/hr_attendance/models/__init__.py
@@ -3,6 +3,7 @@
 from . import res_config_settings
 from . import hr_attendance
 from . import hr_attendance_overtime
+from . import hr_employee_base
 from . import hr_employee
 from . import hr_employee_public
 from . import ir_ui_menu
--- addons/hr_presence/models/__init__.py
+++ addons/hr_presence/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import hr_employee_base
 from . import hr_employee
 from . import ir_websocket
 from . import res_company
