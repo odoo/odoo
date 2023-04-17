dd645bf67ebc3dafda65d334fc1d2092476ab64b
Denis Ledoux <dle@odoo.com>
2023-04-17 12:44:58 +0200

    [FIX] snailmail: accurate error message when no access to the attachment
    This revision follows
    - odoo/odoo@5e81850ea64b533f0576c919a427820554096284
    - odoo/enterprise@e484b4da0c8b70225e91cc66b64ac1b3a0bcc685

--- addons/snailmail/models/snailmail_letter.py
+++ addons/snailmail/models/snailmail_letter.py
@@ -119,7 +119,14 @@ class SnailmailLetter(models.Model):
 
         self.env['mail.notification'].sudo().create(notification_vals)
 
+        letters.attachment_id.check('read')
         return letters
+
+    def write(self, vals):
+        res = super().write(vals)
+        if 'attachment_id' in vals:
+            self.attachment_id.check('read')
+        return res
 
     def _fetch_attachment(self):
         """
