f5562ea32c6b5d737ec5a87091615003761910ca
Camille Spiritus <casp@odoo.com>
2023-04-18 11:32:56 +0000

    [IMP] account/l10n_ch: usable demo data for QR bill
    Until 15.0, to show the behaviour of the QR billing in Switzerland, you had to manually configure a bank account for CH company and create a QR Bill compliant customer before creating an invoice, which was time consuming in the contaxt of a demo.
    
    Updated the demo company's bank account and added an adequate customer to ease the process.
    
    task-3264826
    
    closes odoo/odoo#120343
    
    X-original-commit: 69aab9f4db89f8bf3ba8f29a96fbf9eabd4bac4a
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- addons/l10n_ch/__manifest__.py
+++ addons/l10n_ch/__manifest__.py
@@ -47,6 +47,7 @@ Here is how it works:
     'demo': [
         'demo/account_cash_rounding.xml',
         'demo/demo_company.xml',
+        'demo/res_partner_demo.xml',
     ],
     'post_init_hook': 'post_init',
     'assets': {
