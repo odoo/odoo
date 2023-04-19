c3ae415512fe4669f6f9f89ec5cf9a4aaf7ed062
Alexandre Kühn <aku@odoo.com>
2023-04-19 10:27:07 +0000

    [FIX] mail: save record on schedule activity
    Before this commit, when adding a tag on record and then
    scheduling an activity, the tag was removed from the record.
    
    This happens because reloading the chatter does not save changes.
    
    Although there is a `keepChanges: true` option, this looks
    ineffective. This commit fixes the issue by letting chatter reload
    always save any changes.
    
    closes odoo/odoo#119275
    
    X-original-commit: 97465dfb0ec53080bf05c3614dfa7429629729a6
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/web/chatter.js
+++ addons/mail/static/src/web/chatter.js
@@ -295,6 +295,7 @@ export class Chatter extends Component {
     }
 
     async reloadParentView() {
+        await this.props.saveRecord?.();
         if (this.props.webRecord) {
             await this.props.webRecord.model.root.load(
                 { resId: this.props.threadId },
