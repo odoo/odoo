5594d8f191cdf040bf074a2afe08079b7a8da759
Xavier-Do <xdo@odoo.com>
2023-04-19 08:23:35 +0000

    [IMP] base, *: speedup assets unique computation
    One of the most costly part of a page loading when the ormcache is cold
    is computing the assets node, the unique identifier of an attachment to
    validate whether the existing attachment is still valid with the current
    version of the static files.
    
    This operation needs to glob assets path in the filesystem,
    get the modification date, check attachments, ...
    
    Right now this task is not really optimized and can take some time
    because of an excessive number of glob on the filesystem, unnecessary
    exists to define absolute path, double computation of file list and
    modified times when getting js and css bundle separately, ...
    
    A list of modifications mainly discussed in the pr message are made
    with this commit to speedup things.
    
    - split css and js unique
    - prepare api for an in memory glob
    - change api to propagate absolute path and meta information through
    `ir.asset._get_paths`-> _get_asset_paths -> `_get_asset_content` ->
    `AssetsBundle`
    
    closes odoo/odoo#121159
    
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- odoo/tools/__init__.py
+++ odoo/tools/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-SUPPORTED_DEBUGGER = {'pdb', 'ipdb', 'wdb', 'pudb'}
 from . import _monkeypatches
 from . import appdirs
 from . import cloc
+from . import constants
 from . import pdf
 from . import pycompat
 from . import win32
