odoo/odoo

commit 6f11060d6afd9edce904dd34a2d415a2a2461108

[FIX] sale_management: copy sequence from quotation template
When selecting a quotation template for a quotation all the template lines are
copied into the quotation. However, the sequence field is omitted when adding
the lines. As a result all quotation lines have, by default, the same sequence
number.

This should not be a problem, since the lines are still shown in order in the
frontend quotation view. Once the lines are shown, the user can freely reorder
the lines, while the frontend takes care of reordering the sequence numbers.

However, there is an interaction with an existing problem in One2Many and
Many2Many fields. When these fields are paginated a sequence update will only
update the sequence numbers on the first page. This means that, before this
commit, when we move around a line on the first page, all the lines on the
second page will be inserted at the second position on the first page (because
of the way _onResequenceRecords is implemented).

This commit does include the sequence number in the copied quotation lines,
resolving the problem described above.

Note: It is still possible to insert new lines in the first page, extending
the sequence numbers at the end of the page. Since the sequence numbers on the
second page still remain the same, there are now lines on the first and second
page having the same sequence numbers, resulting in a similar bug.

opw-2730746

closes odoo/odoo#88806

Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

addons/sale_management/models/sale_order.py
@@ -58,6 +58,7 @@ class SaleOrder(models.Model):
 
     def _compute_line_data_for_template_change(self, line):
         return {
+            'sequence': line.sequence,
             'display_type': line.display_type,
             'name': line.name,
             'state': 'draft',

commit b047783c003082badbc049181c30e299e306cee6

[FIX] sale_management: quotation template+sequence=lessbug
Scenario:

- set a quotation template on a quotation that adds lines ordered with
  a sequence (eg. 3 lines with sequences 10, 11, 12)
- add lines in this quotation
- save

Issue:

The added lines have sequence 10 and the seen order changes.

This is caused by 6f11060d6afd9edce904dd34a2d415a2a2461108 that tries to
mitigate a more rare issue when there is more than one page of lines.

Solution:

Reverting the previous fix, and setting the sequence of the first line
to -99:

- when resequencing the first page, the sequence will be -99 to -60 =>
  records from the second page will not get in the first page

- new lines will be added at the end

Issues still present:

- when resequencing the second page, item from 3 pages get into it (it
  was already the case before 6f11060d6afd9edce904dd34a2d415a2a2461108)

- when adding a new line (without resequencing), it is added at the end
  and not at the beginning of the next page

But those are just behavior in any list view (as is the original issue
of 6f11060d6afd9edce904dd34a2d415a2a2461108, but since it was partially
fixed for a year, this patch try to give an in-between solution instead
of just reverting it).

opw-2833913

addons/sale_management/models/sale_order.py
@@ -58,7 +58,6 @@ class SaleOrder(models.Model):
 
     def _compute_line_data_for_template_change(self, line):
         return {
-            'sequence': line.sequence,
             'display_type': line.display_type,
             'name': line.name,
             'state': 'draft',
@@ -128,6 +127,11 @@ class SaleOrder(models.Model):
                 })
 
             order_lines.append((0, 0, data))
+
+        # set first line to sequence -99, so a resequence on first page doesn't cause following page
+        # lines (that all have sequence 10 by default) to get mixed in the first page
+        if len(order_lines) >= 2:
+            order_lines[1][2]['sequence'] = -99
 
         self.order_line = order_lines
         self.order_line._compute_tax_id()