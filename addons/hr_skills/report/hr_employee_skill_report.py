335c6c9e9c8d2343846a561e77f4b1db7f6ce9a1
Diamond Nguyen <diamondnguyenkc@gmail.com>
2023-04-20 16:35:56 +0700

    [IMP] hr_skills: Expand to inherit

--- addons/hr_skills/report/hr_employee_skill_report.py
+++ addons/hr_skills/report/hr_employee_skill_report.py
@@ -19,12 +19,10 @@ class HrEmployeeSkillReport(models.BaseModel):
     skill_level = fields.Char(readonly=True)
     level_progress = fields.Float(readonly=True, group_operator='avg')
 
-    def init(self):
-        tools.drop_view_if_exists(self.env.cr, self._table)
-
-        self.env.cr.execute("""
-        CREATE OR REPLACE VIEW %s AS (
-            SELECT
+    def _select_skill(self, fields=None):
+        if not fields:
+            fields = {}
+        select_ = """
                 row_number() OVER () AS id,
                 e.id AS employee_id,
                 e.company_id AS company_id,
@@ -32,9 +30,37 @@ class HrEmployeeSkillReport(models.BaseModel):
                 s.skill_id AS skill_id,
                 s.skill_type_id AS skill_type_id,
                 sl.level_progress / 100.0 AS level_progress,
-                sl.name AS skill_level
-            FROM hr_employee e
-            LEFT OUTER JOIN hr_employee_skill s ON e.id = s.employee_id
-            LEFT OUTER JOIN hr_skill_level sl ON sl.id = s.skill_level_id
-        )
-        """ % (self._table, ))
+                sl.name AS skill_level"""
+        for field in fields.values():
+            select_ += field
+        return select_
+
+    def _from_skill(self, from_clause=''):
+        from_ = """
+            hr_employee e
+                LEFT OUTER JOIN hr_employee_skill s ON e.id = s.employee_id
+                LEFT OUTER JOIN hr_skill_level sl ON sl.id = s.skill_level_id
+                %s
+        """ % from_clause
+        return from_
+
+    def _select_additional_fields(self, fields):
+        """Hook to return additional fields SQL specification for select part of the table query.
+        :param dict fields: additional fields info provided by _query overrides (old API), prefer overriding
+            _select_additional_fields instead.
+        :returns: mapping field -> SQL computation of the field
+        :rtype: dict
+        """
+        return fields
+
+    def _query(self, with_clause='', fields=None, from_clause=''):
+        if not fields:
+            fields = {}
+        skill_report_fields = self._select_additional_fields(fields)
+        with_ = ("WITH %s" % with_clause) if with_clause else ""
+        return '%s (SELECT %s FROM %s)' % \
+               (with_, self._select_skill(skill_report_fields), self._from_skill(from_clause))
+
+    def init(self):
+        tools.drop_view_if_exists(self.env.cr, self._table)
+        self.env.cr.execute("""CREATE or REPLACE VIEW %s AS (%s)""" % (self._table, self._query()))
