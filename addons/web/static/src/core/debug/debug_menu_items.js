d1f3ae8e7ed951a563d76be05094c0dfe9e1ce18
Lou (loha) <loha@odoo.com>
2023-04-20 15:27:53 +0200

    "[ADD] debug function to view assets size"

--- addons/web/static/src/core/debug/debug_menu_items.js
+++ addons/web/static/src/core/debug/debug_menu_items.js
@@ -46,6 +46,87 @@ export function regenerateAssets({ env }) {
     };
 }
 
+import { Component, xml } from "@odoo/owl";
+import { Dialog } from "@web/core/dialog/dialog";
+
+class ViewAssetsDialog extends Component {}
+ViewAssetsDialog.template = xml`
+<Dialog title="this.constructor.title">
+    <p>
+    Found files :
+    </p>
+    <pre t-esc="props.output"/>
+</Dialog>`;
+ViewAssetsDialog.components = { Dialog };
+ViewAssetsDialog.props = {
+    output: { type: String },
+    close: { type: Function },
+};
+ViewAssetsDialog.title = "Assets size";
+
+async function get_used_bundles(){
+    const url = "./"
+    const regex = /(?<=\=\"\/web\/assets\/)(.*)(?=\" data-asset-bundle)/gm
+
+    const page = await fetch(url)
+        .then( r => r.text() )
+        .then( t => {return(t)} )
+
+    const matches = page.match(regex)
+    let bundles = []
+    matches.forEach((match) => {
+        let parts = match.split('/')
+        bundles.push({path : match, name : parts[parts.length - 1]})
+    });
+    return bundles
+}
+
+function get_stats(bundle){
+    const regex = /\/\* | \*\//
+    let parts = bundle.split(regex)
+    parts.splice(0,1)
+    let parts_info = []
+    for (let i = 0; i < parts.length; i+=2){
+        let data = parts[i + 1]
+        parts_info.push({
+            'file': parts[i],
+            'data': data,
+            'length': data.length,
+        })
+    }
+    return parts_info.sort(function(a, b){return a.length - b.length}).reverse()
+}
+
+export function viewAssetsSize({ env }) {
+    return {
+        type: "item",
+        description: env._t("View Assets Size"),
+        callback: async() => {
+            const bundles= await get_used_bundles()
+            let output=""
+            for (let bundle in bundles){
+                console.log(bundles[bundle])
+                const url = "./web/assets/"+bundles[bundle].path
+                const bundle_content = await fetch(url)
+                    .then( r => r.text() )
+                    .then( t => {return(t)} )
+                console.log(bundle_content)
+                const stats = get_stats(bundle_content)
+
+                output+="Assets in " + bundles[bundle].name + " :\n"
+                for (let stat in stats ){
+                    output += stats[stat].file + " : " + stats[stat].length + " o\n"
+                }
+                output+='\n'
+            }
+
+            env.services.dialog.add(ViewAssetsDialog, { output } );
+        },
+        sequence: 440,
+    };
+}
+
+
 function becomeSuperuser({ env }) {
     const becomeSuperuserURL = browser.location.origin + "/web/become";
     return {
@@ -56,7 +137,7 @@ function becomeSuperuser({ env }) {
         callback: () => {
             browser.open(becomeSuperuserURL, "_self");
         },
-        sequence: 440,
+        sequence: 450,
     };
 }
 
@@ -69,7 +150,7 @@ function leaveDebugMode({ env }) {
             route.search.debug = "";
             browser.location.href = browser.location.origin + routeToUrl(route);
         },
-        sequence: 450,
+        sequence: 460,
     };
 }
 
@@ -80,4 +161,5 @@ registry
     .add("regenerateAssets", regenerateAssets)
     .add("becomeSuperuser", becomeSuperuser)
     .add("leaveDebugMode", leaveDebugMode)
-    .add("activateTestsAssetsDebugging", activateTestsAssetsDebugging);
+    .add("activateTestsAssetsDebugging", activateTestsAssetsDebugging)
+    .add("viewAssetSize", viewAssetsSize);
