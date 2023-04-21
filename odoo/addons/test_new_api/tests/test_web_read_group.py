odoo/odoo

commit 234db70d861a7bc9c6620f9ef6a24dbe98eabb98

[IMP] *: Use the new API of `_read_group` for backend use
Part-of: odoo/odoo#110737

addons/web/models/models.py
@@ -98,15 +98,13 @@ class Base(models.AbstractModel):
         if not groups:
             length = 0
         elif limit and len(groups) == limit:
-            # We need to fetch all groups to know the total number
-            # this cannot be done all at once to avoid MemoryError
-            length = limit
-            chunk_size = 100000
-            while True:
-                more = len(self.read_group(domain, ['display_name'], groupby, offset=length, limit=chunk_size, lazy=True))
-                length += more
-                if more < chunk_size:
-                    break
+            length = limit + len(self._read_group(
+                domain,
+                groupby=groupby if not lazy else groupby[0],
+                offset=limit,
+                order=orderby,
+            ))
+
         else:
             length = len(groups) + offset
         return {

odoo/models.py
@@ -1799,21 +1799,21 @@ class BaseModel(metaclass=MetaModel):
                 list to match all records.
         :param list groupby: list of groupby descriptions by which the records will be grouped.
                 A groupby description is either a field (then it will be grouped by that field)
-                or a string 'field:granularity'. Right now, the only supported granularities
-                are 'day', 'week', 'month', 'quarter' or 'year', and they only make sense for
+                or a string `'field:granularity'`. Right now, the only supported granularities
+                are `'day'`, `'week'`, `'month'`, `'quarter'` or `'year'`, and they only make sense for
                 date/datetime fields.
         :param list aggregates: list of aggregates specification.
-                Each element is 'field:agg' (aggregate field with aggregation function 'agg').
+                Each element is `'field:agg'` (aggregate field with aggregation function `'agg'`).
                 The possible aggregation functions are the ones provided by
                 `PostgreSQL <https://www.postgresql.org/docs/current/static/functions-aggregate.html>`_,
-                'count_distinct' with the expected meaning and 'recordset' to act like 'array_agg'
+                `'count_distinct'` with the expected meaning and `'recordset'` to act like `'array_agg'`
                 converted into a recordset.
         :param list having: A domain where the valid "fields" are the aggregates.
         :param int offset: optional number of groups to skip
         :param int limit: optional max number of groups to return
         :param str order: optional ``order by`` specification, for
                 overriding the natural sort ordering of the groups,
-                see also :py:meth:`~osv.osv.osv.search`.
+                see also :meth:`~.search`.
         :return: list of tuple containing in the order the groups values and aggregates values (flatten):
                 `[(groupby_1_value, ... , aggregate_1_value_aggregate, ...), ...]`.
                 If group is related field, the value of it will be a recordset (with a correct prefetch set).

commit 91e7b912214521fec8beb195343ed6bd63649a0f

[FIX] web: fix bad groupby as `str` instead of list
A mistake introduced in  234db70d861a7bc9c6620f9ef6a24dbe98eabb98, the
`groupby` of  `_read_group` should be list/tuple of  `str`, not a `str`.

addons/web/models/models.py
@@ -185,7 +185,7 @@ class Base(models.AbstractModel):
         elif limit and len(groups) == limit:
             length = limit + len(self._read_group(
                 domain,
-                groupby=groupby if not lazy else groupby[0],
+                groupby=groupby if not lazy else [groupby[0]],
                 offset=limit,
                 order=orderby,
             ))

odoo/addons/test_new_api/tests/__init__.py
@@ -15,3 +15,4 @@ from . import test_domain
 from . import test_schema
 from . import test_company_checks
 from . import test_unity_read
+from . import test_web_read_group

odoo/addons/test_new_api/tests/test_web_read_group.py
@@ -1,0 +1,41 @@
+from odoo.tests.common import TransactionCase
+
+
+class TestWebReadGroup(TransactionCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env['test_new_api.multi.tag'].create([
+            {'name': 'one'},
+            {'name': 'two'},
+            {'name': 'two'},
+            {'name': 'there'},
+            {'name': 'there'},
+            {'name': 'there'},
+        ])
+
+    def test_web_read_group_limit_not_reached(self):
+        result = self.env['test_new_api.multi.tag'].web_read_group(
+            [], ['__count'], ['name'], limit=80,
+        )
+        self.assertEqual(result, {
+            'groups': [
+                {'name': 'one', 'name_count': 1, '__domain': [('name', '=', 'one')]},
+                {'name': 'there', 'name_count': 3, '__domain': [('name', '=', 'there')]},
+                {'name': 'two', 'name_count': 2, '__domain': [('name', '=', 'two')]},
+            ],
+            'length': 3,
+        })
+
+    def test_web_read_group_limit_reached(self):
+        result = self.env['test_new_api.multi.tag'].web_read_group(
+            [], ['__count'], ['name'], limit=2,
+        )
+        self.assertEqual(result, {
+            'groups': [
+                {'name': 'one', 'name_count': 1, '__domain': [('name', '=', 'one')]},
+                {'name': 'there', 'name_count': 3, '__domain': [('name', '=', 'there')]},
+            ],
+            'length': 3,
+        })