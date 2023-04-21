c976b215b2bcb8da929ac7af482df13a44db4938
hote <hote@odoo.com>
2023-04-21 14:58:11 +0200

    [FIX] sale: currency conversions and rates
    Before this commit the reports in sales and associated app suffered from a confusion between
    multiplicative and divisive rates for currency conversion. After this commit this will no longer be
    the case.
    
    There are several models with their own currencies that are involved in computing the amount of a
    sale order:
    - the order's company currency `order.company_id.currency_id`
    - the order's pricelist currency `order.pricelist_id.currency_id`
    - the product's currency for each product in the sale order `product.currency_id`
    
    In the case of a report we need also to take into account the current user's company currency:
    `self.env.company.currency_id`.
    
    The `sale_order.currency_rate` is the **multiplicative <u>rate</u>** to convert from the company
    currency into the SO currency.
    
    Any product added to a sale order follow the next conversion. Let `conversion_rate` be the conversion
    rate between the product's currency and the order's company currency, then:
    ```python
    product_amount_converted = product.list_price / conversion_rate
    
    order.amount_total +=  product_amount_converted * order.currency_rate
    ```
    
    The rates returned by `_get_query_currency_table()`, on the other hand, are computed as the
    **multiplicative <u>ratio</u>** to convert from each company currency into the current user's
    company currency.
    
    Any order that is to be analysed on a *report* follow the next conversion. Let `current_company_currency_rate`
    be the conversion rate between the order's company currency and the current user's company, then:
    ```python
    order_amount_in_cmp_currency = order.amount_total / order.currency_rate
    
    report.order_id.price_total = order_amount_in_cmp_currency * current_company_currency_rate
    ```
    
    What follow is a numerical example. Suppose you have the following multi-company and
    multi-currency environment.
    
    |order.id|order.company  |line.subtotal|order.currency|order.currency_rate|
    | ------ | -----------   | ----------- | ------------ | ----------------- |
    |1      |BE Company (EUR)|1000         |INR           |89.46              |
    |2      |BE Company (EUR)|2000         |USD           |1.09               |
    |3      |BE Company (EUR)|3000         |EUR           |1.0                |
    |4      |US Company (USD)|4000         |USD           |1.0                |
    
    If we try to convert all of this for reporting in the US company, the currency table returned by
    `_get_query_currency_table()` will give this (company currencies vs USD):
    
    |currency_table.company|currency_table.rate|
    | -------------------- | ------------------ |
    |BE Company (EUR)      |1.09                |
    |US Company (USD)      |1.0                 |
    
    Finally, in order to convert each amount into USD, applying the formulae listed above should give
    the following result:
    
    |order.id|order.company|display_price_usd           |
    | ------ | ----------- | -------------------------- |
    |1       |BE Company   |1000 / 89.46 * 1.09 = 12.18 |
    |2       |BE Company   |2000 / 1.09 * 1.09 = 2000.00|
    |3       |BE Company   |3000 / 1.0 * 1.09 = 3270.00 |
    |4       |US Company   |4000 / 1.0 * 1.0 = 4000.00  |
    |Total (USD): 9282.18|
    
    Task - 3277006
    
    X-original-commit: 66eb732cfdae199ab655b7ca40b67384dd693aa7

--- addons/sale/report/sale_report.py
+++ addons/sale/report/sale_report.py
@@ -80,22 +80,22 @@ class SaleReport(models.Model):
             CASE WHEN l.product_id IS NOT NULL THEN SUM(l.qty_to_invoice / u.factor * u2.factor) ELSE 0 END AS qty_to_invoice,
             CASE WHEN l.product_id IS NOT NULL THEN SUM(l.price_total
                 / {self._case_value_or_one('s.currency_rate')}
-                / {self._case_value_or_one('currency_table.rate')}
+                * {self._case_value_or_one('currency_table.rate')}
                 ) ELSE 0
             END AS price_total,
             CASE WHEN l.product_id IS NOT NULL THEN SUM(l.price_subtotal
                 / {self._case_value_or_one('s.currency_rate')}
-                / {self._case_value_or_one('currency_table.rate')}
+                * {self._case_value_or_one('currency_table.rate')}
                 ) ELSE 0
             END AS price_subtotal,
             CASE WHEN l.product_id IS NOT NULL THEN SUM(l.untaxed_amount_to_invoice
                 / {self._case_value_or_one('s.currency_rate')}
-                / {self._case_value_or_one('currency_table.rate')}
+                * {self._case_value_or_one('currency_table.rate')}
                 ) ELSE 0
             END AS untaxed_amount_to_invoice,
             CASE WHEN l.product_id IS NOT NULL THEN SUM(l.untaxed_amount_invoiced
                 / {self._case_value_or_one('s.currency_rate')}
-                / {self._case_value_or_one('currency_table.rate')}
+                * {self._case_value_or_one('currency_table.rate')}
                 ) ELSE 0
             END AS untaxed_amount_invoiced,
             COUNT(*) AS nbr,
@@ -122,7 +122,7 @@ class SaleReport(models.Model):
             l.discount AS discount,
             CASE WHEN l.product_id IS NOT NULL THEN SUM(l.price_unit * l.product_uom_qty * l.discount / 100.0
                 / {self._case_value_or_one('s.currency_rate')}
-                / {self._case_value_or_one('currency_table.rate')}
+                * {self._case_value_or_one('currency_table.rate')}
                 ) ELSE 0
             END AS discount_amount,
             s.id AS order_id"""
