3fdc5afa003f70c8969f8e3b38a0035c98d7db52
Rohit Kumar <roku@odoo.com>
2023-04-24 12:32:03 +0530

    [IMP] project: task stage improvement in the project app
    This commit removes the tooltip of the `fold` field because
    the second part is no longer true and the first part does not
    provide any added value.
    It also makes `project_ids` field required in the form view of
    Task Stage model (`project.task.type`) to force the user to link
    his new stage to a project. Otherwise, the stage won't ever be
    visible anywhere in the interface. Indeed, stages not holding
    tasks are hidden in cross-projects views.
    
    task-3251621

--- addons/project/models/project_task_type.py
+++ addons/project/models/project_task_type.py
@@ -27,8 +27,7 @@ class ProjectTaskType(models.Model):
         string='Email Template',
         domain=[('model', '=', 'project.task')],
         help="If set, an email will be automatically sent to the customer when the task reaches this stage.")
-    fold = fields.Boolean(string='Folded in Kanban',
-        help='If enabled, this stage will be displayed as folded in the Kanban view of your tasks. Tasks in a folded stage are considered as closed.')
+    fold = fields.Boolean(string='Folded in Kanban')
     rating_template_id = fields.Many2one(
         'mail.template',
         string='Rating Email Template',
