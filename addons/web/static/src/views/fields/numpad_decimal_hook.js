e76643568105f086f3ab156b805dc4c3b42467f5
Hubert Van de Walle (huvw) <huvw@odoo.com>
2023-04-24 14:33:28 +0000

    [FIX] web: use the correct inputmode for fields
    Steps to reproduce
    ==================
    
    - Use an IOS device
    - Open a view with a float field
    - Try to edit it
    
    The virtual keyboard displayed is missing a dot, so you can only input
    integers
    
    Cause of the issue
    ==================
    
    For fields that should accept a float value, the inputmode attribute
    should be decimal.
    Also, specifically on IOS, even with the inputmode set to decimal, it
    isn't possible to enter a negative value.
    
    > decimal:
    > Fractional numeric input keyboard containing the digits and decimal separator
    > Devices may or may not show a minus key (-)
    
    Source: https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/inputmode
    
    Solution
    ========
    
    For IOS, remove the inputmode attribute so that the default keyboard
    shows. For other devices, use the decimal/numeric value depending on
    whether a float should be accepted.
    
    opw-3269275
    
    closes odoo/odoo#120129
    
    X-original-commit: fb37550715034e7a1fbe751889cd8ea2bd6e4128
    Signed-off-by: Hubert Van De Walle <huvw@odoo.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/views/fields/numpad_decimal_hook.js
+++ addons/web/static/src/views/fields/numpad_decimal_hook.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 import { localization } from "@web/core/l10n/localization";
+import { isIOS } from "@web/core/browser/feature_detection";
 
 import { useRef, useEffect } from "@odoo/owl";
 
@@ -31,9 +32,14 @@ function onFocus(ev) {
  *
  * NOTE: Special consideration for the input type = "number". In this
  * case, whatever the user types, we let the browser's default behavior.
+ *
+ * NOTE: On IOS devices, the inputmode attribute prevents the user from
+ * entering a negative number (the minus sign is not on the virtual keyboard),
+ * so we need to remove it.
  */
 export function useNumpadDecimal() {
     const ref = useRef("numpadDecimal");
+    const isIOSDevice = isIOS();
     useEffect(() => {
         let inputs = [];
         const el = ref.el;
@@ -41,6 +47,9 @@ export function useNumpadDecimal() {
             inputs = el.nodeName === "INPUT" ? [el] : el.querySelectorAll("input");
             inputs.forEach((input) => input.addEventListener("keydown", onKeydown));
             inputs.forEach((input) => input.addEventListener("focus", onFocus));
+            if (isIOSDevice) {
+                inputs.forEach((input) => input.removeAttribute("inputmode"));
+            }
         }
         return () => {
             inputs.forEach((input) => input.removeEventListener("keydown", onKeydown));
