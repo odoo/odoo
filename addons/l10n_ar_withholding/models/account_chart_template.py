592d0dae408ed1f24aa4bb3857483be1820aaba9
Juan Jose Scarafia <jjs@adhoc.com.ar>
2023-04-25 18:07:16 -0300

    [IMP] add chart data

--- addons/l10n_ar_withholding/models/account_chart_template.py
+++ addons/l10n_ar_withholding/models/account_chart_template.py
@@ -5,12 +5,25 @@ from odoo.addons.account.models.chart_template import template
 class AccountChartTemplate(models.AbstractModel):
     _inherit = 'account.chart.template'
 
+    # ri chart
     @template('ar_ri', 'account.tax.group')
-    def _get_ar_withholding_account_tax_group(self):
+    def _get_ar_ri_withholding_account_tax_group(self):
         return self._parse_csv('ar_ri', 'account.tax.group', module='l10n_ar_withholding')
 
     @template('ar_ri', 'account.tax')
-    def _get_ar_withholding_account_tax(self):
+    def _get_ar_ri_withholding_account_tax(self):
         additionnal = self._parse_csv('ar_ri', 'account.tax', module='l10n_ar_withholding')
         self._deref_account_tags('ar_ri', additionnal)
+        self._deref_account_tags('ar_ex', additionnal)
+        return additionnal
+
+    # ex chart
+    @template('ar_ex', 'account.tax.group')
+    def _get_ar_ex_withholding_account_tax_group(self):
+        return self._parse_csv('ar_ex', 'account.tax.group', module='l10n_ar_withholding')
+
+    @template('ar_ex', 'account.tax')
+    def _get_ar_ex_withholding_account_tax(self):
+        additionnal = self._parse_csv('ar_ex', 'account.tax', module='l10n_ar_withholding')
+        self._deref_account_tags('ar_ex', additionnal)
         return additionnal
