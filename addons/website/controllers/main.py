f7fee054a29895d4a5144cf6d57d83f58b4ebb58
Romain Derie <rde@odoo.com>
2023-04-25 16:49:51 +0000

    [FIX] website: don't show website info page in sitemap if disabled
    There is a historical code to return an error/404 when someone disable
    the website info view, see [1].
    There is also a feature introduced to add an option to disable the
    content of this controller, see [2].
    Commit [2] should probably have gotten rid of the view check in [1]
    which was probably the way people used to stop this page leaking their
    DB information.
    
    From there, regardless if you deleted or disabled the view, the page
    would still be referenced in the sitemap.xml, which would lead to a
    traceback page (case 1) or a blank page (case 2).
    
    This commit is simply conditionning the presence of this page in the
    sitemap so it's removed from it if you disable/remove the website info
    view.
    It will help SEO wise to not have error page in it.
    
    Note that we probably don't want to keep indexing this page, as it is
    a technical link which doesn't bring any value to our clients to be
    included in their sitemap.
    It might have been useful for Odoo to perform some google search / stats
    or something like that.
    But since this is a stable fix, the behavior is kept as is as much as
    possible.
    
    Also note that it has always been in sitemap, even if marked
    excplicitely since [3].
    
    [1]: https://github.com/odoo/odoo/commit/8aca457e34bdec6257b9bfa917aebe4de053e2aa#diff-d41b2dc5ff6fd6a303373f86e1af97d055db315ccc431749b4ffac1488dea119R146-R149
    [2]: https://github.com/odoo/odoo/commit/f025d3e17cbb3b1fd05152ca46af091b6e11ee20
    [3]: https://github.com/odoo/odoo/commit/e19227d3ba9c9296bfc0c221ac70a863a571b9a6#diff-d41b2dc5ff6fd6a303373f86e1af97d055db315ccc431749b4ffac1488dea119L200
    
    opw-3255831
    
    closes odoo/odoo#120734
    
    X-original-commit: 58e7ff404e1b92df9f5e5958e5982373c69d7ca4
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -237,10 +237,24 @@ class Website(Home):
 
         return request.make_response(content, [('Content-Type', mimetype)])
 
-    @http.route('/website/info', type='http', auth="public", website=True, sitemap=True)
+    def sitemap_website_info(env, rule, qs):
+        website = env['website'].get_current_website()
+        if not (
+            website.viewref('website.website_info', False).active
+            and website.viewref('website.show_website_info', False).active
+        ):
+            # avoid 404 or blank page in sitemap
+            return False
+
+        if not qs or qs.lower() in '/website/info':
+            yield {'loc': '/website/info'}
+
+    @http.route('/website/info', type='http', auth="public", website=True, sitemap=sitemap_website_info)
     def website_info(self, **kwargs):
         if not request.website.viewref('website.website_info', False).active:
             # Deleted or archived view (through manual operation in backend).
+            # Don't check `show_website_info` view: still need to access if
+            # disabled to be able to enable it through the customize show.
             raise request.not_found()
 
         Module = request.env['ir.module.module'].sudo()
