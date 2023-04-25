c52670c9e91c369410422afacfbd9080d432e8bd
tsm-odoo <tsm@odoo.com>
2023-04-25 10:21:32 +0000

    [FIX] bus: fix non-deterministic bus tests
    Since [1], some bus tests were failing in a non-deterministic
    way. This PR fixes the issue.
    
    [1]: https://github.com/odoo/odoo/pull/119525
    Fixes runbot errors: 20755, 20756, 19933.
    
    closes odoo/odoo#119671
    
    X-original-commit: de02a6230d3e21e42359111c95a51485c6700773
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Stockbauer Matthieu (tsm) <tsm@odoo.com>

--- addons/bus/static/tests/bus_tests.js
+++ addons/bus/static/tests/bus_tests.js
@@ -101,7 +101,7 @@ QUnit.module('Bus', {
 
         const pyEnv = await startServer();
         const env = await makeTestEnv({ activateMockServer: true });
-        env.services["bus_service"].start();
+        await env.services["bus_service"].start();
         await nextTick();
         env.services['bus_service'].addEventListener('notification', ({ detail: notifications }) => {
             assert.step('notification - ' + notifications.map(notif => notif.payload).toString());
@@ -470,14 +470,20 @@ QUnit.module('Bus', {
 
     QUnit.test("Disconnect on offline, re-connect on online", async function (assert) {
         patchWebsocketWorkerWithCleanup();
+        let websocketConnectedDeferred = makeDeferred();
         const env = await makeTestEnv();
-        env.services["bus_service"].addEventListener("connect", () => assert.step("connect"));
+        env.services["bus_service"].addEventListener("connect", () => {
+            assert.step("connect");
+            websocketConnectedDeferred.resolve();
+            websocketConnectedDeferred = makeDeferred();
+        });
         env.services["bus_service"].addEventListener("disconnect", () => assert.step("disconnect"));
         await env.services["bus_service"].start();
+        await websocketConnectedDeferred;
         window.dispatchEvent(new Event("offline"));
         await nextTick();
         window.dispatchEvent(new Event("online"));
-        await nextTick();
+        await websocketConnectedDeferred;
         assert.verifySteps(["connect", "disconnect", "connect"]);
     });
 
