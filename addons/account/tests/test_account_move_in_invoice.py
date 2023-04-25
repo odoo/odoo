4352dea503a6140eeeb6f617d623530517c9fad1
Nshimiyimana Séna <sesn@odoo.com>
2023-04-25 09:47:01 +0000

    [FIX] account: allow sending invoice to partner other than the customer
    ## Bug
    
    When sending an invoice to a recipient who is not the customer, they are
    unable to view the invoice in the customer portal and are prompted to
    log in.
    
    ## Steps to reproduce
    
    1. Create and validate an invoice
    2. Click on the Send & Print button, then add a recipient who is not the
       customer associated with the invoice.
    3. Proceed to send the invoice.
    4. Access the email that was sent to the added recipient (who is not the
       customer)
    5. Using an incognito or private browsing window, open the link
       `View Invoice`
    
    => you should see that you are asked to log in, instead of being
    directed to the customer portal.
    
    opw-3114579
    
    X-original-commit: 95c585f67bd37e1202c57b3e9eed09b0a964472c

--- addons/account/tests/test_account_move_in_invoice.py
+++ addons/account/tests/test_account_move_in_invoice.py
@@ -2016,3 +2016,48 @@ class TestAccountMoveInInvoiceOnchanges(AccountTestInvoicingCommon):
 
         # Assert the tax line is recreated for the tax
         self.assertIn(tax, self.invoice.line_ids.tax_line_id)
+
+    def test_invoice_sent_to_additional_partner(self):
+        """
+        Make sure that when an invoice is a partner to a partner who is not
+        the invoiced customer, they receive a link containing an access token,
+        allowing them to view the invoice without needing to log in.
+        """
+
+        # Create a simple invoice for the partner
+        invoice = self.init_invoice(
+            'out_invoice', partner=self.partner_a, invoice_date='2023-04-17', amounts=[100])
+
+        # Set the invoice to the 'posted' state
+        invoice.action_post()
+
+        # Create a partner not related to the invoice
+        additional_partner = self.env['res.partner'].create({
+            'name': "Additional Partner",
+            'email': "additional@example.com",
+        })
+
+        # Send the invoice
+        action = invoice.with_context(discard_logo_check=True).action_invoice_sent()
+        action_context = action['context']
+
+        # Create the email using the wizard and add the additional partner as a recipient
+        invoice_send_wizard = self.env['account.invoice.send'].with_context(
+            action_context,
+            active_ids=[invoice.id]
+        ).create({'is_print': False})
+        invoice_send_wizard.partner_ids |= additional_partner
+
+        invoice_send_wizard.template_id.auto_delete = False
+
+        invoice_send_wizard.send_and_print_action()
+
+        # Find the email sent to the additional partner
+        additional_partner_mail = self.env['mail.mail'].search([
+            ('res_id', '=', invoice.id),
+            ('recipient_ids', '=', additional_partner.id)
+        ])
+        self.assertTrue(additional_partner_mail)
+
+        self.assertIn('access_token=', additional_partner_mail.body_html,
+                      "The additional partner should be sent the link including the token")
