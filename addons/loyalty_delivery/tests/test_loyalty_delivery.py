8689e832e909e847a72121b6dadee86bd1a3ab56
Horacio Tellez (hote) <hote@odoo.com>
2023-04-26 11:35:45 +0200

    [FIX] loyalty_delivery: shipping costs accounts for loyalty programs
    For delivery costs the rewards ewallet and gift cards were being
    treated as products and not money, this implies that free delivery
    was not being triggered even if the order amount exceed the
    necessary amount for free shipping.
    After this commit free shipping calculation will ignore rewards
    when computing the amount.
    
    See also odoo/odoo#110952
    
    opw - 3223038

--- addons/loyalty_delivery/tests/test_loyalty_delivery.py
+++ addons/loyalty_delivery/tests/test_loyalty_delivery.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.tests import common, Form
 from odoo.exceptions import ValidationError
+from odoo.fields import Command
+from odoo.tests import common, Form
+
 
 @common.tagged('post_install', '-at_install')
 class TestLoyaltyDeliveryCost(common.TransactionCase):
@@ -17,6 +19,70 @@ class TestLoyaltyDeliveryCost(common.TransactionCase):
         })
         cls.product_4 = cls.env['product.product'].create({'name': 'A product to deliver'})
         cls.product_uom_unit = cls.env.ref('uom.product_uom_unit')
+
+    def test_free_delivery_cost_with_ewallet(self):
+        """
+        Automatic free shipping of a delivery carrier should not be affected by the
+        use of an ewallet when paying.
+        Paying for an order of value 200 with an ewallet should still trigger the
+        free shipping of the selected carrier if the free shipping is for amounts
+        over 100.
+        """
+
+        # Create a delivery product and service.
+        product_delivery_free = self.env['product.product'].create({
+            'name': 'Test Delivery Product',
+            'type': 'service',
+            'list_price': 10.0,
+            'categ_id': self.env.ref('delivery.product_category_deliveries').id,
+        })
+        free_delivery = self.env['delivery.carrier'].create({
+            'name': 'Test Free Delivery',
+            'product_id': product_delivery_free.id,
+            'free_over': True,
+            'amount': 100,
+        })
+
+        # Create an eWallet Program and its corresponding rewards and coupons.
+        program_ewallet = self.env['loyalty.program'].create({
+            'name': 'eWallet',
+            'program_type': 'ewallet',
+            'reward_ids': [Command.create({
+                'reward_type': 'discount',
+                'discount_mode': 'per_point',
+                'discount': 1,
+                'discount_applicability': 'order',
+                'required_points': 1,
+            })],
+        })
+        self.env['loyalty.generate.wizard'].with_context(active_id=program_ewallet.id).create({
+            'coupon_qty': 1,
+            'points_granted': 1000,
+        }).generate_coupons()
+        reward_ewallet = program_ewallet.reward_ids[0]
+        ewallet = program_ewallet.coupon_ids[0]
+
+        # Create an order and pay with the ewallet.
+        order = self.SaleOrder.create({
+            'partner_id': self.partner_1.id,
+            'pricelist_id': self.pricelist.id,
+            'order_line': [Command.create({
+                'product_id': self.product_4.id,
+                'price_unit': 200.00,
+            })]
+        })
+        order._apply_program_reward(reward_ewallet, ewallet)
+        order.action_confirm()
+
+        # Ask for delivery, delivery cost should not take into account the ewallet when computing
+        # the amount of the sale order.
+        delivery_wizard = self.env['choose.delivery.carrier'].with_context({
+            'default_order_id': order.id,
+            'default_carrier_id': free_delivery.id,
+        })
+        delivery_wizard.button_confirm()
+
+        self.assertEqual(order.amount_untaxed, 200.0, "Delivery cost is not added.")
 
     def test_delivery_cost_gift_card(self):
         """
