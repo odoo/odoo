e0b51848ec18536d669dbd2118b5026518840b7e
Rodolpho Lima <rcdl@odoo.com>
2023-04-26 11:41:59 +0200

    [FIX] web_editor: auto transforming email URL
    Typing a URL + SPACE transforms the URL into a link.
    
    Before this commit, email URLs were incorrectly prepended with
    'https://'.
    
    Because it is not desirable to automatically transform email URLs, this
    commit disables link transformation for email URLs, leaving it only
    active for http URLs.
    
    task-3284649

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -78,6 +78,7 @@ import {
     leftPos,
     isNotAllowedContent,
     isEditorTab,
+    EMAIL_REGEX,
 } from './utils/utils.js';
 import { editorCommands } from './commands/commands.js';
 import { Powerbox } from './powerbox/Powerbox.js';
@@ -3328,7 +3329,7 @@ export class OdooEditor extends EventTarget {
                     // Remove added space
                     textNodeSplitted.pop();
                     const potentialUrl = textNodeSplitted.pop();
-                    const lastWordMatch = potentialUrl.match(URL_REGEX_WITH_INFOS);
+                    const lastWordMatch = potentialUrl.match(URL_REGEX_WITH_INFOS) && !potentialUrl.match(EMAIL_REGEX);
 
                     if (lastWordMatch) {
                         const matches = getUrlsInfosInString(textSliced);
