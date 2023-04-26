e6de0a50c3da536f90c8c181599816f87b83996e
Thomas (thbe) <thbe@odoo.com>
2023-04-26 10:42:31 +0200

    [ADD] auth_signup: Password reset request spam prevention
    No system is implemented to lower the amount of password reset a user can make
    
    This system uses the ir.logging table to store the date of the password
    reset requests the user made. When a user asks for a password reset, the table
    is checked to see if the user has already requested three password resets in
    the last hour and if so, the user is not granted the permission to ask for a
    reset. If not, the user is granted the permission to request a password reset
    and if the mail is sent and a new entry is added to the table.
    
    This adds a way to control how much a user can reset his password to make
    spamming password reset not possible.
    
    task-3191567
    
    Changes brought with this commit :
    
    Added a test for the password reset spam limitation process to be checked.

---
+++ addons/auth_signup/tests/test_spam_password_reset.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from unittest.mock import patch
+
+from odoo.exceptions import UserError
+from odoo.tests import HttpCase
+
+class TestSpamPasswordReset(HttpCase):
+
+    def setUp(self):
+        super(TestSpamPasswordReset, self).setUp()
+        res_config = self.env['res.config.settings']
+        self.default_values = res_config.default_get(list(res_config.fields_get()))
+
+    def _activate_free_signup(self):
+        self.default_values.update({'auth_signup_uninvited': 'b2c'})
+
+    def _get_free_signup_url(self):
+        return '/web/signup'
+
+    def test_password_reset_spam(self):
+        """
+        Check if a new user can spam the password reset
+        """
+
+        # Create a new user (automatically sends a password reset)
+        new_user = self.env['res.users'].create({
+            'login': 'test',
+            'name': 'toto',
+            'email': 'toto@example.com',
+        })
+
+        # Check if the user can ask for two more password resets after the one sent at its creation
+        new_user.action_reset_password()
+        new_user.action_reset_password()
+        # Check if the user is limited at the fourth password reset
+        with self.assertRaises(UserError):
+            new_user.action_reset_password()
