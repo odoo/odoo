272aa935d7a3a09e678e19aea1a322cafc7be8a5
Mountain Tran <tranngocson8296@gmail.com>
2023-04-26 06:47:28 +0000

    [FIX] point_of_sale: correctly translate some UI elements
    Before some translations didn't work in the point_of_sale because in owl
    you can't use t-set to manage translations. In cashMovePopup, _t was
    used at a time when loading translations is not yet guaranteed.
    
    Now we have changed _t to _lt to ensure that the translation is done
    correctly. For the t-set, we have added the translations in static
    properties of the concerned components.
    
    X-original-commit: b2b9f760844304858aa64c51e7bda87190b11386

--- addons/point_of_sale/static/src/js/Screens/ProductScreen/ProductScreen.js
+++ addons/point_of_sale/static/src/js/Screens/ProductScreen/ProductScreen.js
@@ -5,6 +5,7 @@ import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { useBarcodeReader } from "@point_of_sale/app/barcode_reader_hook";
 import { parseFloat } from "@web/views/fields/parsers";
+import { _lt } from "@web/core/l10n/translation";
 
 import { NumberPopup } from "@point_of_sale/js/Popups/NumberPopup";
 import { ErrorPopup } from "@point_of_sale/js/Popups/ErrorPopup";
@@ -31,6 +32,7 @@ export class ProductScreen extends ControlButtonsMixin(Component) {
         OrderWidget,
         ProductsWidget,
     };
+    static numpadActionName = _lt("Payment");
 
     setup() {
         super.setup();
