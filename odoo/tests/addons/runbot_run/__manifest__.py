93e1150ce4d06f6109f1a80afc06f189a0cb9418
Xavier-Do <xdo@odoo.com>
2023-04-26 13:32:53 +0200

    [IMP] runbot_run: add a runbot addon

---
+++ odoo/tests/addons/runbot_run/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "Runbot run",
+    "description": "Provide basic safeguard to avoid blocking other tester on -all databases. Enable it using --load runbot_run after adding odoo/tests/addons in your addons path",
+    'license': 'LGPL-3',
+    'installable': False,
+    'application': False,
+}
