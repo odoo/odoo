ef24190266513094cafc54dcbae0963d38315019
Claire Bretton (clbr) <clbr@odoo.com>
2023-04-26 08:37:23 +0000

    [FIX] account: don't install demo_data in chart_template tests
    Chart template tests install demo_data that are not required and
    can cause an access right error in some builds:
    https://runbot.odoo.com/runbot/build/27709490
    
    X-original-commit: db85f087ea4fd8743ad133b70588a58b81c27102

--- addons/account/tests/test_chart_template.py
+++ addons/account/tests/test_chart_template.py
@@ -121,8 +121,24 @@ class TestChartTemplate(TransactionCase):
             'company_id': cls.company_1.id,
         })
 
+<<<<<<< HEAD
         with patch.object(AccountChartTemplate, '_get_chart_template_data', side_effect=test_get_data, autospec=True):
             cls.env['account.chart.template'].try_loading('test', company=cls.company_1, install_demo=False)
+||||||| parent of e5ccaffa4d8 (temp)
+        cls.chart_template.try_loading(company=cls.company)
+        cls.chart_template_xmlid = cls.chart_template.get_external_id()[cls.chart_template.id]
+        cls.fiscal_position = cls.env['account.fiscal.position'].search([
+            ('company_id', '=', cls.company.id),
+            ('name', '=', cls.fiscal_position_template.name),
+        ])
+=======
+        cls.chart_template.try_loading(company=cls.company, install_demo=False)
+        cls.chart_template_xmlid = cls.chart_template.get_external_id()[cls.chart_template.id]
+        cls.fiscal_position = cls.env['account.fiscal.position'].search([
+            ('company_id', '=', cls.company.id),
+            ('name', '=', cls.fiscal_position_template.name),
+        ])
+>>>>>>> e5ccaffa4d8 (temp)
 
     def test_update_taxes_from_templates(self):
         """
@@ -202,6 +218,7 @@ class TestChartTemplate(TransactionCase):
             data['account.tax']['test_tax_1_template']['amount'] = 40
             return data
 
+<<<<<<< HEAD
         tax_1_existing = self.env['account.tax'].search([('company_id', '=', self.company_1.id), ('name', '=', "Tax 1")])
         with patch.object(AccountChartTemplate, '_get_chart_template_data', side_effect=local_get_data, autospec=True):
             self.env['account.chart.template'].try_loading('test', company=self.company_1, install_demo=False)
@@ -209,3 +226,240 @@ class TestChartTemplate(TransactionCase):
         tax_1_new = self.env['account.tax'].search([('company_id', '=', self.company_1.id), ('name', '=', "Tax 1")])
         self.assertEqual(tax_1_old, tax_1_existing, "Old tax still exists but with a different name.")
         self.assertEqual(len(tax_1_new), 1, "New tax have been created with the original name.")
+||||||| parent of e5ccaffa4d8 (temp)
+        taxes_from_template_1 = self.env['account.tax'].search([
+            ('company_id', '=', self.company.id),
+            ('name', 'like', f"%{self.tax_template_1.name}"),
+        ])
+        self.assertRecordValues(taxes_from_template_1, [
+            {'name': f"[old] {self.tax_template_1.name}", 'amount': old_amount},
+            {'name': f"{self.tax_template_1.name}", 'amount': self.tax_template_1.amount},
+        ])
+
+    def test_update_taxes_multi_company(self):
+        """ In a multi-company environment all companies should be correctly updated."""
+        company_2 = self.env['res.company'].create({
+            'name': 'TestCompany2',
+            'country_id': self.env.ref('base.us').id,
+            'account_fiscal_country_id': self.env.ref('base.us').id,
+        })
+        self.chart_template.try_loading(company=company_2)
+
+        # triggers recreation of taxes related to template 1
+        self.tax_template_1.amount += 1
+        update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
+
+        taxes_from_template_1 = self.env['account.tax'].search([
+            ('name', 'like', f"%{self.tax_template_1.name}"),
+            ('company_id', 'in', [self.company.id, company_2.id]),
+        ])
+        # we should have 4 records: 2 companies * (1 original tax + 1 recreated tax)
+        self.assertEqual(len(taxes_from_template_1), 4)
+
+    def test_message_to_accountants(self):
+        """ When we duplicate a tax because it was too different from the existing one we send
+        a message to accountant advisors. This message should only be sent to advisors
+        and not to regular users.
+        """
+        # create 1 normal user, 2 accountants managers
+        accountant_manager_group = self.env.ref('account.group_account_manager')
+        advisor_users = self.env['res.users'].create([{
+            'name': 'AccountAdvisorTest1',
+            'login': 'aat1',
+            'password': 'aat1aat1',
+            'groups_id': [(4, accountant_manager_group.id)],
+        }, {
+            'name': 'AccountAdvisorTest2',
+            'login': 'aat2',
+            'password': 'aat2aat2',
+            'groups_id': [(4, accountant_manager_group.id)],
+        }])
+        normal_user = self.env['res.users'].create([{
+            'name': 'AccountUserTest1',
+            'login': 'aut1',
+            'password': 'aut1aut1',
+            'groups_id': [(4, self.env.ref('account.group_account_user').id)],
+        }])
+        # create situation where we need to recreate the tax during update to get notification(s) sent
+        self.tax_template_1.amount += 1
+        update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
+
+        # accountants received the message
+        self.assertEqual(self.env['mail.message'].search_count([
+            ('partner_ids', 'in', advisor_users.partner_id.ids),
+            ('body', 'like', f"%{self.tax_template_1.name}%"),  # we look for taxes' name that have been sent in the message's body
+        ]), 1)
+        # normal user didn't
+        self.assertEqual(self.env['mail.message'].search_count([
+            ('partner_ids', 'in', normal_user.partner_id.ids),
+            ('body', 'like', f"%{self.tax_template_1.name}%"),  # we look for taxes' name that have been sent in the message's body
+        ]), 0)
+
+    def test_update_taxes_foreign_taxes(self):
+        """ When taxes are instantiated through the fiscal position system (in multivat),
+        its taxes should also be updated.
+        """
+        country_test = self.env['res.country'].create({
+            'name': 'Country Test',
+            'code': 'ZZ',
+        })
+        chart_template_xmlid_test = 'l10n_test.test_chart_template_xmlid'
+        chart_template_test = self.env['account.chart.template']._load_records([{
+            'xml_id': chart_template_xmlid_test,
+            'values': {
+                'name': 'Test Chart Template',
+                'currency_id': self.env.ref('base.EUR').id,
+                'bank_account_code_prefix': 1000,
+                'cash_account_code_prefix': 2000,
+                'transfer_account_code_prefix': 3000,
+                'country_id': country_test.id,
+            }
+        }])
+        self._create_tax_template('account.test_tax_test_template', 'Tax name 1 TEST', 10, chart_template_id=chart_template_test.id)
+        self.env['account.tax.template']._try_instantiating_foreign_taxes(country_test, self.company)
+        self._create_tax_template('account.test_tax_test_template2', 'Tax name 2 TEST', 15, chart_template_id=chart_template_test.id)
+        update_taxes_from_templates(self.env.cr, chart_template_xmlid_test)
+
+        tax_test_model_data = self.env['ir.model.data'].search([
+            ('name', '=', f'{self.company.id}_test_tax_test_template2'),
+            ('model', '=', 'account.tax'),
+        ])
+        self.assertEqual(len(tax_test_model_data), 1, "Taxes should have been created even if the chart_template is installed through fiscal position system.")
+
+    def test_update_taxes_chart_template_country_check(self):
+        """ We can't update taxes that don't match the chart_template's country. """
+        self.company.chart_template_id.country_id = self.env.ref('base.lu')
+        # We provoke one recreation and one update
+        self.tax_template_1.amount += 1
+        self.tax_template_2.invoice_repartition_line_ids.tag_ids.name = 'tag_name_2_modified'
+        with self.assertRaises(ValidationError):
+            update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
+
+    def test_update_taxes_fiscal_country_check(self):
+        """ If there is no country set on chart_template (which is the case for the generic one), the taxes can only be updated if
+        their country matches the fiscal country. """
+        country_lu = self.env.ref('base.lu')
+        self.company.account_fiscal_country_id = country_lu
+        self.tax_template_1.amount += 1
+        self.tax_template_2.invoice_repartition_line_ids.tag_ids.name = 'tag_name_2_modified'
+        with self.assertRaises(ValidationError):
+            update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
+=======
+        taxes_from_template_1 = self.env['account.tax'].search([
+            ('company_id', '=', self.company.id),
+            ('name', 'like', f"%{self.tax_template_1.name}"),
+        ])
+        self.assertRecordValues(taxes_from_template_1, [
+            {'name': f"[old] {self.tax_template_1.name}", 'amount': old_amount},
+            {'name': f"{self.tax_template_1.name}", 'amount': self.tax_template_1.amount},
+        ])
+
+    def test_update_taxes_multi_company(self):
+        """ In a multi-company environment all companies should be correctly updated."""
+        company_2 = self.env['res.company'].create({
+            'name': 'TestCompany2',
+            'country_id': self.env.ref('base.us').id,
+            'account_fiscal_country_id': self.env.ref('base.us').id,
+        })
+        self.chart_template.try_loading(company=company_2, install_demo=False)
+
+        # triggers recreation of taxes related to template 1
+        self.tax_template_1.amount += 1
+        update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
+
+        taxes_from_template_1 = self.env['account.tax'].search([
+            ('name', 'like', f"%{self.tax_template_1.name}"),
+            ('company_id', 'in', [self.company.id, company_2.id]),
+        ])
+        # we should have 4 records: 2 companies * (1 original tax + 1 recreated tax)
+        self.assertEqual(len(taxes_from_template_1), 4)
+
+    def test_message_to_accountants(self):
+        """ When we duplicate a tax because it was too different from the existing one we send
+        a message to accountant advisors. This message should only be sent to advisors
+        and not to regular users.
+        """
+        # create 1 normal user, 2 accountants managers
+        accountant_manager_group = self.env.ref('account.group_account_manager')
+        advisor_users = self.env['res.users'].create([{
+            'name': 'AccountAdvisorTest1',
+            'login': 'aat1',
+            'password': 'aat1aat1',
+            'groups_id': [(4, accountant_manager_group.id)],
+        }, {
+            'name': 'AccountAdvisorTest2',
+            'login': 'aat2',
+            'password': 'aat2aat2',
+            'groups_id': [(4, accountant_manager_group.id)],
+        }])
+        normal_user = self.env['res.users'].create([{
+            'name': 'AccountUserTest1',
+            'login': 'aut1',
+            'password': 'aut1aut1',
+            'groups_id': [(4, self.env.ref('account.group_account_user').id)],
+        }])
+        # create situation where we need to recreate the tax during update to get notification(s) sent
+        self.tax_template_1.amount += 1
+        update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
+
+        # accountants received the message
+        self.assertEqual(self.env['mail.message'].search_count([
+            ('partner_ids', 'in', advisor_users.partner_id.ids),
+            ('body', 'like', f"%{self.tax_template_1.name}%"),  # we look for taxes' name that have been sent in the message's body
+        ]), 1)
+        # normal user didn't
+        self.assertEqual(self.env['mail.message'].search_count([
+            ('partner_ids', 'in', normal_user.partner_id.ids),
+            ('body', 'like', f"%{self.tax_template_1.name}%"),  # we look for taxes' name that have been sent in the message's body
+        ]), 0)
+
+    def test_update_taxes_foreign_taxes(self):
+        """ When taxes are instantiated through the fiscal position system (in multivat),
+        its taxes should also be updated.
+        """
+        country_test = self.env['res.country'].create({
+            'name': 'Country Test',
+            'code': 'ZZ',
+        })
+        chart_template_xmlid_test = 'l10n_test.test_chart_template_xmlid'
+        chart_template_test = self.env['account.chart.template']._load_records([{
+            'xml_id': chart_template_xmlid_test,
+            'values': {
+                'name': 'Test Chart Template',
+                'currency_id': self.env.ref('base.EUR').id,
+                'bank_account_code_prefix': 1000,
+                'cash_account_code_prefix': 2000,
+                'transfer_account_code_prefix': 3000,
+                'country_id': country_test.id,
+            }
+        }])
+        self._create_tax_template('account.test_tax_test_template', 'Tax name 1 TEST', 10, chart_template_id=chart_template_test.id)
+        self.env['account.tax.template']._try_instantiating_foreign_taxes(country_test, self.company)
+        self._create_tax_template('account.test_tax_test_template2', 'Tax name 2 TEST', 15, chart_template_id=chart_template_test.id)
+        update_taxes_from_templates(self.env.cr, chart_template_xmlid_test)
+
+        tax_test_model_data = self.env['ir.model.data'].search([
+            ('name', '=', f'{self.company.id}_test_tax_test_template2'),
+            ('model', '=', 'account.tax'),
+        ])
+        self.assertEqual(len(tax_test_model_data), 1, "Taxes should have been created even if the chart_template is installed through fiscal position system.")
+
+    def test_update_taxes_chart_template_country_check(self):
+        """ We can't update taxes that don't match the chart_template's country. """
+        self.company.chart_template_id.country_id = self.env.ref('base.lu')
+        # We provoke one recreation and one update
+        self.tax_template_1.amount += 1
+        self.tax_template_2.invoice_repartition_line_ids.tag_ids.name = 'tag_name_2_modified'
+        with self.assertRaises(ValidationError):
+            update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
+
+    def test_update_taxes_fiscal_country_check(self):
+        """ If there is no country set on chart_template (which is the case for the generic one), the taxes can only be updated if
+        their country matches the fiscal country. """
+        country_lu = self.env.ref('base.lu')
+        self.company.account_fiscal_country_id = country_lu
+        self.tax_template_1.amount += 1
+        self.tax_template_2.invoice_repartition_line_ids.tag_ids.name = 'tag_name_2_modified'
+        with self.assertRaises(ValidationError):
+            update_taxes_from_templates(self.env.cr, self.chart_template_xmlid)
+>>>>>>> e5ccaffa4d8 (temp)
