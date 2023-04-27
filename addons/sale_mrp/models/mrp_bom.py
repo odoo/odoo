74b28b647226c15d71b5c2cfa407c1898b6a6cdb
Adrien Widart (awt) <awt@odoo.com>
2023-04-27 09:32:08 +0000

    [FIX] mrp_account: avoid deleting a BoM in use
    Deleting a kit BoM will lead to an error if the kit is in the middle
    of a sale process
    
    To reproduce the issue:
    1. In Settings, enable UoM
    2. Create a product category PC:
       - AVCO
       - Auto
    3. Create three storable products P_kit, P_01, P_02
       - UoM of P_01: `m`
       - UoM of P_02: `kg`
    4. Create a BoM:
       - Product: P_kit
       - Type: kit
       - Components:
         - 1 x P_01
         - 1 x P_02
    5. Create and confirm a SO with 1 x P_kit
    6. Force the delivery
    7. Delete the BoM
    8. Create and confirm the SO's invoice
    
    Error: a traceback appears "ValueError: Expected singleton: uom.uom..."
    
    Because of the configuration, when posting the invoice, we compute
    the COGS. During that process, we need the average price of P_kit
    https://github.com/odoo/odoo/blob/640907ec1852c4e477957c865549a87d3ae840dd/addons/sale_stock/models/account_move.py#L133
    We give to that method the components' SMs. In
    `_compute_average_price`, since we don't find a BoM, we will bypass
    the mrp override
    https://github.com/odoo/odoo/blob/419e42132d9526e9d98c2126158d4f88716d17c4/addons/mrp_account/models/product.py#L48-L50
    As a result, it leads to
    https://github.com/odoo/odoo/blob/640907ec1852c4e477957c865549a87d3ae840dd/addons/stock_account/models/product.py#L680-L684
    Where we will have the candidates of all components (i.e., several
    different products). But that method is not designed for such a
    situation. This is the reason why, when calling `_consume_all`, it
    will fail:
    https://github.com/odoo/odoo/blob/858d24a611c4c8af0dcae82a6eb4077e391398ed/addons/stock_account/models/stock_valuation_layer.py#L85
    We try to get the rounding of the product, but there are actually
    two of them (the two components) -> singleton error
    
    OPW-3218141
    
    closes odoo/odoo#120304
    
    X-original-commit: 980c8380cbd1cad514d96ea682074e3787f10c1a
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Adrien Widart <awt@odoo.com>

---
+++ addons/sale_mrp/models/mrp_bom.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import _, models
+from odoo.exceptions import UserError
+
+
+class MrpBom(models.Model):
+    _inherit = 'mrp.bom'
+
+    def toggle_active(self):
+        self.filtered(lambda bom: bom.active)._ensure_bom_is_free()
+        return super().toggle_active()
+
+    def unlink(self):
+        self._ensure_bom_is_free()
+        return super().unlink()
+
+    def _ensure_bom_is_free(self):
+        product_ids = []
+        for bom in self:
+            if bom.type != 'phantom':
+                continue
+            product_ids += bom.product_id.ids or bom.product_tmpl_id.product_variant_ids.ids
+        if not product_ids:
+            return
+        lines = self.env['sale.order.line'].sudo().search([
+            ('state', 'in', ('sale', 'done')),
+            ('invoice_status', 'in', ('no', 'to invoice')),
+            ('product_id', 'in', product_ids),
+            ('move_ids.state', '!=', 'cancel'),
+        ])
+        if lines:
+            product_names = ', '.join(lines.product_id.mapped('display_name'))
+            raise UserError(_('As long as there are some sale order lines that must be delivered/invoiced and are '
+                              'related to these bills of materials, you can not remove them.\n'
+                              'The error concerns these products: %s', product_names))
