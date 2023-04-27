8df009fd1004484b0ccc73b222f74db364fd3cb9
Alexandre Kühn <aku@odoo.com>
2023-04-27 13:44:49 +0000

    [FIX] web: make disabled date visible in dark theme
    Before this commit, calendar view in dark theme did not visually
    show the disabled date, such as weekends.
    
    In white theme it's fine because bg-view is white, so gray-200 on
    white is visible. In dark theme, however, bg-view is identical to
    gray-200, so it's not visible.
    
    This commit fixes the issue specifically in dark theme by showing
    disabled dates with gray-300.
    
    closes odoo/odoo#122744
    
    X-original-commit: 968b4a284acab3f67f192f1029abbbd1edb2b442
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/web/__manifest__.py
+++ addons/web/__manifest__.py
@@ -253,6 +253,8 @@ This module provides the core of the Odoo Web Client.
             'web/static/src/legacy/xml/base.xml',
             'web/static/src/legacy/xml/fields.xml',
             'web/static/src/legacy/xml/week_days.xml',
+            # Don't include dark mode files in light mode
+            ('remove', 'web/static/src/**/*.dark.scss'),
         ],
         'web.assets_frontend_minimal': [
             'web/static/src/legacy/js/promise_extension.js',
@@ -484,6 +486,7 @@ This module provides the core of the Odoo Web Client.
         ],
         "web.dark_mode_assets_backend": [
             ('include', 'web.assets_backend'),
+            'web/static/src/**/*.dark.scss',
         ],
         "web.dark_mode_variables": [
             ('before', 'base/static/src/scss/onboarding.variables.scss', 'base/static/src/scss/onboarding.variables.dark.scss'),
