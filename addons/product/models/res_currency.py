4c0146d939e40e9433794802388cf06147e716f2
Demesmaeker <edm@odoo.com>
2023-04-27 11:59:00 +0000

    [FIX] product: only activate pricelists when enabling pricelists
    Following 83c52575d0ce3d7deb1737c622c6ce366db8c31b we activated or created pricelists as soon as a
    currency was activated/created/deleted, for every company that might already have another
    pricelist setup done, which was quite confusing.
    
    With this fix, we ensure this will only happen:
    - at company creation
    - when enabling the pricelist feature
    - when enabling the multi-currency setting (only the first time, unless the pricelists were
    disabled manually afterward)
    
    closes odoo/odoo#119959
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/product/models/res_currency.py
+++ addons/product/models/res_currency.py
@@ -10,6 +10,7 @@ class ResCurrency(models.Model):
     def _activate_group_multi_currency(self):
         # for Sale/ POS - Multi currency flows require pricelists
         super()._activate_group_multi_currency()
-        group_user = self.env.ref('base.group_user').sudo()
-        group_user._apply_group(self.env.ref('product.group_product_pricelist'))
-        self.env['res.company']._activate_or_create_pricelists()
+        if not self.user_has_groups('product.group_product_pricelist'):
+            group_user = self.env.ref('base.group_user').sudo()
+            group_user._apply_group(self.env.ref('product.group_product_pricelist'))
+            self.env['res.company']._activate_or_create_pricelists()
