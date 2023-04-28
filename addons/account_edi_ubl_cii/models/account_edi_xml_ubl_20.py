32f3929b7a58d1e7e28c70ea88a03be03d592184
Julien Van Roy <juvr@odoo.com>
2023-04-28 11:15:05 +0000

    [FIX] account_edi_ubl_cii: handle paid invoice at import
    The edi attachment (for every format in `account_edi_ubl_cii`) contains
    a tag indicating whether the invoice is fully/partially paid.
    
    Before this fix, when this tag was filled, a down payment section was
    created on the invoice. For a fully paid invoice, the resulting amount
    was then 0, so the invoice was marked as "Paid".
    
    This was wrong. The correct amounts need to be kept on the invoices.
    This fix no longer creates a down payment, instead a message is logged
    in the chatter.
    
    task-3264843
    opw-3248200
    
    X-original-commit: 47905d21ab65f985291d78cb7ecb78f2f7cdcf67

--- addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py
@@ -561,10 +561,10 @@ class AccountEdiXmlUBL20(models.AbstractModel):
 
         logs += self._import_fill_invoice_allowance_charge(tree, invoice, journal, qty_factor)
 
-        # ==== Down Payment (prepaid amount) ====
+        # ==== Prepaid amount ====
 
         prepaid_node = tree.find('./{*}LegalMonetaryTotal/{*}PrepaidAmount')
-        self._import_fill_invoice_down_payment(invoice, prepaid_node, qty_factor)
+        logs += self._import_log_prepaid_amount(invoice, prepaid_node, qty_factor)
 
         # ==== invoice_line_ids: InvoiceLine/CreditNoteLine ====
 
