4b60c4c12cc6539ac9fdb8b0c067333459bc1043
MerlinGuillaume <megu@odoo.com>
2023-05-02 14:25:25 +0000

    [FIX] mail: disable the sidebar when opening a pdf in the chatter
    The pdf viewer sometimes displays the sidebar when we open the pdf
    
    Solution:
    Add `pagemode=none` to the url when opening a pdf in attachments
    
    opw-3193516
    
    X-original-commit: da9b89ad2697df7492e3f41c35f46c0b29afaef5

--- addons/mail/static/src/models/attachment/attachment.js
+++ addons/mail/static/src/models/attachment/attachment.js
@@ -126,10 +126,10 @@ function factory(dependencies) {
             if (this.isPdf) {
                 const pdf_lib = `/web/static/lib/pdfjs/web/viewer.html?file=`
                 if (!this.accessToken && this.originThread && this.originThread.model === 'mail.channel') {
-                    return `${pdf_lib}/mail/channel/${this.originThread.id}/attachment/${this.id}`;
+                    return `${pdf_lib}/mail/channel/${this.originThread.id}/attachment/${this.id}#pagemode=none`;
                 }
                 const accessToken = this.accessToken ? `?access_token%3D${this.accessToken}` : '';
-                return `${pdf_lib}/web/content/${this.id}${accessToken}`;
+                return `${pdf_lib}/web/content/${this.id}${accessToken}#pagemode=none`;
             }
             if (this.isUrlYoutube) {
                 const urlArr = this.url.split('/');
