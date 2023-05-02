70b52ab9b569e7ee6281a5c4009933a3eacdbe92
Mahamadasif Ansari <maan@odoo.com>
2023-05-02 11:16:07 +0530

    [FIX] payment: prevent deletion of payment acquirer if it has external reference
    "ValueError: External ID not found in the system: payment.payment_acquirer_
    stripe" is generated because the user deleted the Stripe payment acquirer
    record and its corresponding model tried to access the record of it.
    
    Steps to produce the error:
    1. Install e-commerce
    2. Install install 'Stripe Payment Acquirer ' module
    2. delete the Stripe payment from payment acquirer
    3. Go to the e-commerce dashoboard
    4. Click Set Payment
    5. select Credit card (via Stripe)
    6. enter any secret and Publishable key
    7. click apply
    
    This commit solves the above issue by preventing the deletion
    of the payment acquirer if it has a external reference.
    
    sentry-4041178833

--- addons/payment/models/payment_acquirer.py
+++ addons/payment/models/payment_acquirer.py
@@ -10,11 +10,13 @@ import psycopg2
 
 from odoo import api, exceptions, fields, models, _, SUPERUSER_ID
 from odoo.tools import consteq, float_round, image_process, ustr
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
 from odoo.tools.misc import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.tools.misc import formatLang
 from odoo.http import request
 from odoo.osv import expression
+
+from odoo.addons.base.models.ir_model import MODULE_UNINSTALL_FLAG
 
 _logger = logging.getLogger(__name__)
 
@@ -335,6 +337,19 @@ class PaymentAcquirer(models.Model):
         result = super(PaymentAcquirer, self).write(vals)
         self._check_required_if_provider()
         return result
+
+    def unlink(self):
+        """ Prevent the deletion of the payment acquirer if it has an xmlid. """
+        external_ids = self.get_external_id()
+        for acquirer in self:
+            external_id = external_ids[acquirer.id]
+            if external_id \
+               and not external_id.startswith('__export__') \
+               and not self._context.get(MODULE_UNINSTALL_FLAG):
+                raise UserError(
+                    _("You cannot delete the payment acquirer %s; archive it instead.", acquirer.name)
+                )
+        return super().unlink()
 
     def get_acquirer_extra_fees(self, amount, currency_id, country_id):
         extra_fees = {
