286121c8881c985a4bd337327d12278aae2a377e
Victor Feyens <vfe@odoo.com>
2023-05-02 10:37:26 +0200

    [CLN] sale_loyalty: xml & files cleanup
    * Files ordering
    * Indentation
    * Simplified xpath when possible
    * Menus in specific file
    * Clear file names

--- addons/sale_loyalty/__manifest__.py
+++ addons/sale_loyalty/__manifest__.py
@@ -10,14 +10,18 @@
     'auto_install': True,
     'data': [
         'security/ir.model.access.csv',
+
         'data/sale_loyalty_data.xml',
+
         'wizard/sale_loyalty_coupon_wizard_views.xml',
         'wizard/sale_loyalty_reward_wizard_views.xml',
+
         'views/loyalty_card_views.xml',
         'views/loyalty_program_views.xml',
         'views/sale_order_views.xml',
-        'views/templates.xml',
+        'views/sale_portal_templates.xml',
         'views/res_partner_views.xml',
+        'views/sale_loyalty_menus.xml',
     ],
     'license': 'LGPL-3',
 }
