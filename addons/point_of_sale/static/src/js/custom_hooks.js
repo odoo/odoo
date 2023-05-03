6ce895a226b710c856fc7e79baa117d272989913
pedrambiria <pebr@odoo.com>
2023-05-03 19:49:15 +0000

    [FIX] point_of_sale: enhance cash input handling
    This commit addresses several issues with inputting cash values in the
    closing and opening pop-up, particularly when the computer's locale
    doesn't match the Odoo instance's locale and when using the Safari
    browser. Previously, pressing the wrong decimal point would remove the
    inputted value, and the input handling was not consistent in Safari.
    
    Changes:
    - Added restrictions similar to the number popup
    - Ensured compatibility with localization settings, allowing the
    configured decimal point to work correctly
    - Improved input handling consistency across browsers, including Safari
    
    opw-3288550
    
    X-original-commit: 2475e52d76ddf290befc795f136a8431b00cceeb

--- addons/point_of_sale/static/src/js/custom_hooks.js
+++ addons/point_of_sale/static/src/js/custom_hooks.js
@@ -1,7 +1,8 @@
 odoo.define('point_of_sale.custom_hooks', function (require) {
     'use strict';
 
-    const { onMounted, onPatched, onWillUnmount, useComponent } = owl;
+    const { onMounted, onPatched, onWillUnmount, useComponent, useRef } = owl;
+    const { escapeRegExp } = require('@web/core/utils/strings');
 
     /**
      * Introduce error handlers in the component.
@@ -114,5 +115,38 @@ odoo.define('point_of_sale.custom_hooks', function (require) {
         });
     }
 
-    return { useErrorHandlers, useAutoFocusToLast, useBarcodeReader };
+    function useValidateCashInput(inputRef, startingValue) {
+        const cashInput = useRef(inputRef);
+        const current = useComponent();
+        const decimalPoint = current.env._t.database.parameters.decimal_point;
+        // Replace the thousands separator and decimal point with regex-escaped versions
+        const escapedThousandsSep = escapeRegExp(current.env._t.database.parameters.thousands_sep);
+        const escapedDecimalPoint = escapeRegExp(decimalPoint);
+        const floatRegex = new RegExp(`^-?(?:\\d+(${escapedThousandsSep}\\d+)*)?(?:${escapedDecimalPoint}\\d*)?$`);
+        function isValidFloat(inputValue) {
+            return ![decimalPoint, '-'].includes(inputValue) && floatRegex.test(inputValue);
+        }
+        function handleCashInputChange(event) {
+            let inputValue = (event.target.value || "").trim();
+
+            // Check if the current input value is a valid float
+            if (!isValidFloat(inputValue)) {
+                event.target.classList.add('invalid-cash-input');
+            } else {
+                event.target.classList.remove('invalid-cash-input');
+            }
+        }
+        
+
+        onMounted(() => {
+            cashInput.el.value = (startingValue || 0).toString().replace('.', decimalPoint);
+            cashInput.el.addEventListener("input", handleCashInputChange);
+        });
+
+        onWillUnmount(() => {
+            cashInput.el.removeEventListener("input", handleCashInputChange);
+        })
+    }
+
+    return { useErrorHandlers, useAutoFocusToLast, useBarcodeReader, useValidateCashInput };
 });
