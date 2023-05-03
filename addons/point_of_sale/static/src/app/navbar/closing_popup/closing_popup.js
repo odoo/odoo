1356fea3797215c636154eb344cf61b29e8cf4ce
pedrambiria <pebr@odoo.com>
2023-05-03 19:49:15 +0000

    [FIX] point_of_sale: enhance cash input handling
    This commit addresses several issues with inputting cash values in the
    closing and opening pop-up, particularly when the computer's locale
    doesn't match the Odoo instance's locale and when using the Safari
    browser. Previously, pressing the wrong decimal point would remove the
    inputted value, and the input handling was not consistent in Safari.
    
    Changes:
    - Added restrictions similar to the number popup
    - Ensured compatibility with localization settings, allowing the
    configured decimal point to work correctly
    - Improved input handling consistency across browsers, including Safari
    
    opw-3288550
    
    closes odoo/odoo#128308
    
    X-original-commit: c017a9f947843dbef99546e12057452be2fd5e76
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/navbar/closing_popup/closing_popup.js
+++ addons/point_of_sale/static/src/app/navbar/closing_popup/closing_popup.js
@@ -13,6 +13,8 @@ import { identifyError } from "@point_of_sale/app/errors/error_handlers";
 import { _t } from "@web/core/l10n/translation";
 import { usePos } from "@point_of_sale/app/store/pos_hook";
 import { sprintf } from "@web/core/utils/strings";
+import { parseFloat } from "@web/views/fields/parsers";
+import { useValidateCashInput } from "@point_of_sale/app/utils/hooks";
 
 export class ClosePosPopup extends AbstractAwaitablePopup {
     static components = { SaleDetailsButton };
@@ -35,6 +37,14 @@ export class ClosePosPopup extends AbstractAwaitablePopup {
             displayMoneyDetailsPopup: false,
         });
         Object.assign(this.state, this.props.info.state);
+        useValidateCashInput("closingCashInput");
+        if (this.otherPaymentMethods && this.otherPaymentMethods.length > 0) {
+            this.otherPaymentMethods.forEach(pm => {
+                if (this._getShowDiff(pm)) {
+                    useValidateCashInput("closingCashInput_" + pm.id, this.state.payments[pm.id].counted);
+                }
+            })
+        }
     }
     //@override
     async confirm() {
@@ -100,7 +110,8 @@ export class ClosePosPopup extends AbstractAwaitablePopup {
             this.pos.pos_session.id,
         ]);
     }
-    handleInputChange(paymentId) {
+    handleInputChange(paymentId, event) {
+        if (event.target.classList.contains('invalid-cash-input')) return;
         let expectedAmount;
         if (paymentId === this.defaultCashDetails.id) {
             this.manualInputCashCount = true;
@@ -110,6 +121,7 @@ export class ClosePosPopup extends AbstractAwaitablePopup {
         } else {
             expectedAmount = this.otherPaymentMethods.find((pm) => paymentId === pm.id).amount;
         }
+        this.state.payments[paymentId].counted = parseFloat(event.target.value);
         this.state.payments[paymentId].difference = this.env.utils.roundCurrency(
             this.state.payments[paymentId].counted - expectedAmount
         );
