951089cb54a53a3808a004ce65dfb5e6b4dfe97c
Gauthier Wala (gawa) <gawa@odoo.com>
2023-05-03 10:42:20 +0200

    [ADD] l10n_* : add all localizations modules for taxes for SYSCOHADA countries
    All these countries use the chart of accounts that is defined in l10n_syscohada.
    
    This commit then add the tax report for each localization, and taxes to be able to fill it.
    
    task-2841655
    
    X-original-commit: d0510dff87a3020b0fcae8bd355a538870c55d53

---
+++ addons/l10n_cm/models/template_cm.py
@@ -0,0 +1,30 @@
+from odoo import models
+from odoo.addons.account.models.chart_template import template
+
+
+class AccountChartTemplate(models.AbstractModel):
+    _inherit = 'account.chart.template'
+
+    @template('cm')
+    def _get_cm_template_data(self):
+        return {
+            'name': 'Syscohada Chart of Accounts for Cameroon',
+            'parent': 'syscohada',
+            'code_digits': '6',
+        }
+
+    @template('cm', 'res.company')
+    def _get_cm_res_company(self):
+        company_values = super()._get_syscohada_res_company()
+        company_values[self.env.company.id].update(
+            {
+                'account_fiscal_country_id': 'base.cm',
+                'account_sale_tax_id': 'tva_sale_19_25',
+                'account_purchase_tax_id': 'tva_purchase_good_19_25',
+            }
+        )
+        return company_values
+
+    @template('cm', 'account.account')
+    def _get_cm_account_account(self):
+        return self._parse_csv('cm', 'account.account', module='l10n_syscohada')
