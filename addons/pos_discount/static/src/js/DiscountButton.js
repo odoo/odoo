97c7ebcd203de64da43a74e298d6bfab08694c5c
roen-odoo <roen@odoo.com>
2023-05-03 14:30:39 +0000

    [FIX] pos_discount: stop applying discount to tips
    Current behavior:
    When adding a tips to an order, then applying a global discount. The
    discount is applied to the tips when it should not.
    
    Steps to reproduce:
    - Open POS
    - Add a product to the order
    - Add a tips to the order
    - Apply a global discount
    - The discount is calculated on the tips and the products
    
    opw-3271886
    
    closes odoo/odoo#120926
    
    X-original-commit: 7a1aaf12658adc8c2dcc70a03b5009d5e2c45e19
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Engels Robin (roen) <roen@odoo.com>

--- addons/pos_discount/static/src/js/DiscountButton.js
+++ addons/pos_discount/static/src/js/DiscountButton.js
@@ -55,7 +55,7 @@ export class DiscountButton extends Component {
                 .filter((id) => id !== "")
                 .map((id) => Number(id));
 
-            const baseToDiscount = order.calculate_base_amount(tax_ids_array, lines);
+            const baseToDiscount = order.calculate_base_amount(tax_ids_array, lines.filter((line) => !this.env.pos.config.tip_product_id || line.product.id !== this.env.pos.config.tip_product_id[0]));
 
             // We add the price as manually set to avoid recomputation when changing customer.
             const discount = (-pc / 100.0) * baseToDiscount;
