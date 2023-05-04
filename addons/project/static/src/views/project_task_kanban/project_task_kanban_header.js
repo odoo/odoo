1fd0e88423dde51f0eeb59562b3b0cb13582ef8f
Bastien (bvdn) <bvdn@odoo.com>
2023-05-04 12:16:58 +0000

    [FIX] project: sort kanban header/renderer functions
    the deleteGroup method in the header opens the confirmation dialog then calls the kanbanrenderer deleteGroup method so no need to override it
    the editGroup canEditGroup and canDeleteGroup methods must be placed and overrided in the kanbanHeader
    as well as the onWillStart the sets the this.isProjectManager, which will be used in the canEditGroup
    the canCreateGroup and isProjectTasksContext should stay in the renderer
    
    Task-3276463
    
    closes odoo/odoo#119096
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/static/src/views/project_task_kanban/project_task_kanban_header.js
+++ addons/project/static/src/views/project_task_kanban/project_task_kanban_header.js
@@ -1,9 +1,25 @@
 /** @odoo-module */
-
+import { useService } from '@web/core/utils/hooks';
 import { KanbanHeader } from "@web/views/kanban/kanban_header";
 import { FormViewDialog } from "@web/views/view_dialogs/form_view_dialog";
+import { onWillStart } from "@odoo/owl";
 
 export class ProjectTaskKanbanHeader extends KanbanHeader {
+    setup() {
+        super.setup();
+        this.action = useService('action');
+        this.userService = useService('user');
+
+        this.isProjectManager = false;
+        onWillStart(this.onWillStart);
+    }
+
+    async onWillStart() {
+        if (!this.props.list.isGroupedByPersonalStages) { // no need to check it if the group by is personal stages
+            this.isProjectManager = await this.userService.hasGroup('project.group_project_manager');
+        }
+    }
+
     editGroup() {
         const { resModel, value } = this.group;
         const groupBy = this.props.list.groupBy;
@@ -24,5 +40,27 @@ export class ProjectTaskKanbanHeader extends KanbanHeader {
                 this.props.list.model.notify();
             },
         });
+    }
+
+    async deleteGroup() {
+        if (this.group.groupByField.name === 'stage_id') {
+            const action = await this.group.model.orm.call(
+                this.group.resModel,
+                'unlink_wizard',
+                [this.group.resId],
+                { context: this.group.context },
+            );
+            this.action.doAction(action);
+            return;
+        }
+        super.deleteGroup();
+    }
+
+    canEditGroup(group) {
+        return super.canEditGroup(group) && (!this.props.list.isGroupedByStage || this.isProjectManager) || this.props.list.isGroupedByPersonalStages;
+    }
+
+    canDeleteGroup(group) {
+        return super.canDeleteGroup(group) && (!this.props.list.isGroupedByStage || this.isProjectManager) || this.props.list.isGroupedByPersonalStages;
     }
 }
