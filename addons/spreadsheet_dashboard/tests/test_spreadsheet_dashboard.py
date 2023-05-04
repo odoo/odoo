9d7425cf9f9ac32fea18078eabc76c5db446af6a
Om Rabara <omra@odoo.com>
2023-05-04 06:16:46 +0000

    [FIX] spreadsheet_dashboard: check valid json data file while uploading
    JSONDecodeError: Expecting value: line 1 column 1 (char 0)
    This error occurs when we try to upload an invalid json data file.
    
    sentry- 3957419658
    
    closes odoo/odoo#120019
    
    X-original-commit: https://github.com/odoo/odoo/pull/117105/commits/1a60a19a1c001c12cf933dd8c82332167b609038
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Achraf <abz@odoo.com>

--- addons/spreadsheet_dashboard/tests/test_spreadsheet_dashboard.py
+++ addons/spreadsheet_dashboard/tests/test_spreadsheet_dashboard.py
@@ -1,5 +1,7 @@
-from odoo.tests.common import TransactionCase
-from odoo.exceptions import UserError
+import base64
+import json
+from odoo.tests.common import TransactionCase, Form
+from odoo.exceptions import UserError, ValidationError
 
 
 class TestSpreadsheetDashboard(TransactionCase):
@@ -31,3 +33,14 @@ class TestSpreadsheetDashboard(TransactionCase):
         })
         with self.assertRaises(UserError, msg="You cannot delete a_group as it is used in another module"):
             group.unlink()
+
+    def test_onchange_json_data(self):
+        group = self.env["spreadsheet.dashboard.group"].create(
+            {"name": "a group"}
+        )
+        spreadsheet_form = Form(self.env['spreadsheet.dashboard'])
+        spreadsheet_form.name = 'Test spreadsheet'
+        spreadsheet_form.dashboard_group_id = group
+        spreadsheet_form.data = base64.b64encode(json.dumps({'key': 'value'}).encode('utf-8'))
+        with self.assertRaises(ValidationError, msg='Invalid JSON Data'):
+            spreadsheet_form.data = base64.b64encode('invalid json'.encode('utf-8'))
