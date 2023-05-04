95214ab6c133553371926804e788766edab2b8ca
yhu-odoo <yhu@odoo.com>
2023-05-04 13:37:04 +0000

    [FIX] mrp: duplicate lot when auto-generate serial
    Previous commit 1b840d066999bde1f05563567f439967770701ca tried to fix
    the issue action_generate_serial may try to generate a existing lot.
    But it made the lot number completely product specific. This change is
    too big for stable, we revert the change in this commit.
    
    Also to fix the issue, when action_generate_serial tries to generate a
    lot, we always try the result from ir.sequence frist, only when the lot
    already exists, we then try create one based on the product's lastest
    lot number.
    
    Task-3187003
    
    closes odoo/odoo#120667
    
    X-original-commit: 697399be117fc5e8807136628c6635dc8eca1498
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -1187,10 +1187,21 @@ class MrpProduction(models.Model):
 
     def action_generate_serial(self):
         self.ensure_one()
+        if self.product_id.tracking == 'lot':
+            name = self.env['ir.sequence'].next_by_code('stock.lot.serial')
+            exist_lot = self.env['stock.lot'].search([
+                ('product_id', '=', self.product_id.id),
+                ('company_id', '=', self.company_id.id),
+                ('name', '=', name),
+            ], limit=1)
+            if exist_lot:
+                name = self.env['stock.lot']._get_next_serial(self.company_id, self.product_id)
+        else:
+            name = self.env['stock.lot']._get_next_serial(self.company_id, self.product_id) or self.env['ir.sequence'].next_by_code('stock.lot.serial')
         self.lot_producing_id = self.env['stock.lot'].create({
             'product_id': self.product_id.id,
             'company_id': self.company_id.id,
-            'name': self.env['stock.lot']._get_next_serial(self.company_id, self.product_id) or self.env['ir.sequence'].next_by_code('stock.lot.serial'),
+            'name': name,
         })
         if self.move_finished_ids.filtered(lambda m: m.product_id == self.product_id).move_line_ids:
             self.move_finished_ids.filtered(lambda m: m.product_id == self.product_id).move_line_ids.lot_id = self.lot_producing_id
