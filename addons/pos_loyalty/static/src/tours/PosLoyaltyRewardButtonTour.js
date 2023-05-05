99e9a5180a1634687895c9d4ee3811cc4bbc077e
pedrambiria <pebr@odoo.com>
2023-05-05 09:21:40 +0000

    [FIX] pos_loyalty: correctly compute discount with existing discount
    Before this commit, if you have a discount promotion on the order,
    adding a free product reward would change the discount incorrectly.
    
    The problem is that in the `_getDiscountableOnSpecific` function the
    non-discount rewards weren't excluded from `discountLinesPerReward`.
    
    opw-3276337
    
    X-original-commit: 00893381c6090d03945e714e59f6364b16931e05

--- addons/pos_loyalty/static/src/tours/PosLoyaltyRewardButtonTour.js
+++ addons/pos_loyalty/static/src/tours/PosLoyaltyRewardButtonTour.js
@@ -157,3 +157,17 @@ PosLoyalty.check.hasRewardLine('$ 10 per order on specific products', '-10.00', 
 PosLoyalty.check.orderTotalIs('60.00');
 
 Tour.register('PosLoyaltySpecificDiscountTour', { test: true, url: '/pos/web' }, getSteps());
+
+startSteps();
+
+ProductScreen.do.confirmOpeningPopup();
+ProductScreen.do.clickHomeCategory();
+
+ProductScreen.do.clickDisplayedProduct('Test Product A');
+ProductScreen.do.clickDisplayedProduct('Test Product C');
+PosLoyalty.check.orderTotalIs('130.00');
+PosLoyalty.check.isRewardButtonHighlighted(true);
+PosLoyalty.do.clickRewardButton();
+PosLoyalty.check.orderTotalIs('130.00');
+
+Tour.register('PosLoyaltySpecificDiscountWithFreeProductTour', { test: true, url: '/pos/web' }, getSteps());
