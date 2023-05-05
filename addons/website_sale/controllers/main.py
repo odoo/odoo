c6405e3f3a25ac97e251c470e5757adb93a50c09
Valeriya(vchu) <vchu@odoo.com>
2023-05-05 13:50:42 +0000

    [IMP] website_payment: filter payment providers in portal
    Before from the portal the user could see all the payment providers
    even if the payment provider was not available from the website
    where the order was made. Now the payment providers are filtered if
    the order has website_id.
    task-3284582
    
    closes odoo/odoo#120679
    
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -1795,6 +1795,19 @@ class PaymentPortal(payment_portal.PaymentPortal):
 
 
 class CustomerPortal(sale_portal.CustomerPortal):
+
+    def _get_payment_values(self, order_sudo, website_id=None, **kwargs):
+       """ Override of `sale` to inject the `website_id` into the kwargs.
+
+       :param sale.order order_sudo: The sales order being paid.
+       :param int website_id: The website on which the order was made, if any, as a `website` id.
+       :param dict kwargs: Locally unused keywords arguments.
+       :return: The payment-specific values.
+       :rtype: dict
+       """
+       website_id = website_id or order_sudo.website_id.id
+       return super()._get_payment_values(order_sudo, website_id=website_id, **kwargs)
+
     def _sale_reorder_get_line_context(self):
         return {}
 
