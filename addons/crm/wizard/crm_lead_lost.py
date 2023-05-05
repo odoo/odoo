c005ac5fc643ee4e00098da476dba21559bdcf00
Renaud Thiry <reth@odoo.com>
2023-05-05 10:32:58 +0200

    [IMP] crm: remove set_lost_with_reason action
    This cancels the changes added to f91782279aaa9597762a8764271e5b45b26b5166
    for minor version 16.3 as the server action is completely removed
    instead of being fixed in this.
    
    The action is not removed in the upgrade as it is still technically
    working. Though it should be considered deprecated.
    
    ------------------------------------
    
    We remove the action that was used to conditionally call a window action
    
    The action was not called consistently and added confusing indirection.
    
    To maintain the current behaviour of only suggesting adding a loss
    reason for opportunities.
    We make the reason fields toggleable in the wizard, and hide them by
    default for leads.
    
    This leaves the user with the option to add a reason anyway, and serves
    as a confirmation wizard if they don't want to.
    
    -------------------
    
    task-3272955

--- addons/crm/wizard/crm_lead_lost.py
+++ addons/crm/wizard/crm_lead_lost.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from markupsafe import Markup
-from odoo import fields, models, _
+from odoo import api, fields, models, _
 from odoo.tools.mail import is_html_empty
 
 
@@ -10,21 +10,22 @@ class CrmLeadLost(models.TransientModel):
     _name = 'crm.lead.lost'
     _description = 'Get Lost Reason'
 
-    lead_ids = fields.Many2many('crm.lead')
+    set_reason = fields.Boolean(string='Set a Loss Reason', compute='_compute_set_reason',
+                                readonly=False, store=True)
+    lead_ids = fields.Many2many('crm.lead', string='Leads')
     lost_reason_id = fields.Many2one('crm.lost.reason', 'Lost Reason')
     lost_feedback = fields.Html(
         'Closing Note', sanitize=True
     )
 
+    @api.depends('lead_ids')
+    def _compute_set_reason(self):
+        for wizard in self:
+            wizard.set_reason = bool(wizard.lead_ids.filtered(lambda lead: lead.type == 'opportunity'))
+
     def action_lost_reason_apply(self):
         """Mark lead as lost and apply the loss reason"""
         self.ensure_one()
-        # get lead from context for retro-compatibility
-        if not self.lead_ids and self.env.context.get('active_model', '') == 'crm.lead':
-            ctx = self.env.context
-            ctx_lead_ids = ctx.get('active_ids') or [ctx['active_id']] if ctx.get('active_id') else []
-            self.lead_ids = self.lead_ids.browse(ctx_lead_ids)
-
         if not is_html_empty(self.lost_feedback):
             self.lead_ids._track_set_log_message(
                 Markup('<div style="margin-bottom: 4px;"><p>%s:</p>%s<br /></div>') % (
