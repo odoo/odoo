1bbcd95a7d96ce7ea227d6abb2264a3035ef5ea7
David Monnom (moda) <moda@odoo.com>
2023-05-09 06:08:46 +0000

    [IMP] point_of_sale,pos_restaurant: add bus_service
    Before a longpolling every 5 seconds was set up to sync the number of
    orders per table in the pos_restaurant, which was not very
    userfriendly / efficient.
    
    Now, the longpolling has been replaced by a bus (websocket). As soon as
    an order is sent to the server by another PoS, the server dispatches the
    information to all other PoS using the corresponding floor plan.
    
    The bus_service was initialized in the pos_store of the point_of_sale,
    but is now used only in the pos_restaurant via override.
    This was done for obvious future use.
    
    closes odoo/odoo#121875
    
    Signed-off-by: Heinz Robin (rhe) <rhe@odoo.com>

---
+++ addons/point_of_sale/static/src/app/pos_bus.js
@@ -0,0 +1,35 @@
+/** @odoo-module */
+
+import { registry } from "@web/core/registry";
+
+export class PosBus {
+    static serviceDependencies = ["pos", "orm", "bus_service"];
+
+    constructor(...args) {
+        this.setup(...args);
+    }
+
+    setup(env, { pos, orm, bus_service }) {
+        this.pos = pos;
+        this.orm = orm;
+
+        // initialize bus_service and listen on pos_config-`id` channel
+        bus_service.addChannel(`pos_config-${pos.globalState.config.id}`);
+        bus_service.addEventListener("notification", ({ detail }) => {
+            for (const message of detail) {
+                this.dispatch(message);
+            }
+        });
+    }
+
+    dispatch(message) {}
+}
+
+export const posBusService = {
+    dependencies: PosBus.serviceDependencies,
+    async start(env, deps) {
+        return new PosBus(env, deps);
+    },
+};
+
+registry.category("services").add("pos_bus", posBusService);
