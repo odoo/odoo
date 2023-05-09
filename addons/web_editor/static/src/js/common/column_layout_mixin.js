958b533507bd4cf8b51a718991a48060b4ac7767
Robin Lejeune (role) <role@odoo.com>
2023-05-09 17:22:46 +0200

    [IMP] web_editor, website, *: allow columns on mobile snippets
    *: mass_mailing
    
    This commit introduces options for the flex column layout and the
    horizontal resizing to be used on mobile and be independent from
    the desktop layout.
    
    This makes it possible to:
    - Have different number of columns on mobile and on desktop.
    - Use different column widths and offsets for mobile and desktop.
    - Reorder columns independently.
    
    The behavior expected from the columns is:
    - In the editor panel, Cols indicate the number of columns per row on
    both mobile and desktop. The option is conditional of your environment:
    you edit the number for the screen size of your current resolution.
    - When columns have different sizes (either because it is the default
    behavior of the snippet, or because the user resized some), the counter
    should read "Custom". Same when the user changed some offsets.
    - If manual modifications were made on width and offset, they are reset
    on the current display if the number of elements is updated. This was
    decided because an old, specific layout with custom offsets / width
    doesn't work well with a different number of elements. (This is also the
    default behavior before this PR with desktop-only modifications.)
    
    The expected behavior when reordering is:
    - On mobile, it should only affect the mobile layout.
    - On desktop, it should affect both layouts.
    
    In order to have a coherent feature on both mobile and desktop as well
    as correct some non-ideal but non-blocking behaviors, the commit also
    changes the following behaviors:
    - When setting a columns count lower than the current number of items in
    the `.row` container, extra items are now wrapped on the next flex-rows
    (still within the container) instead of being deleted.
    - When going from e.g. 3 to 5 columns (and as many items), the change
    happens all at once instead of each item being visibly added one by one
    on the next row, then brought back to the first row.
    - The columns count automatically updates when resizing an item, instead
    of updating only after you click somewhere else on the snippet.
    
    This commit also adds a test to validate the new behavior.
    
    task-3097045

---
+++ addons/web_editor/static/src/js/common/column_layout_mixin.js
@@ -0,0 +1,99 @@
+/** @odoo-module **/
+
+export const ColumnLayoutMixin = {
+    /**
+     * Calculates the number of columns for the mobile or desktop version.
+     * If all elements don't have the same size, returns "custom".
+     *
+     * @private
+     * @param {HTMLCollection} columnEls - elements in the .row container
+     * @param {boolean} isMobile
+     * @returns {integer|string} number of columns or "custom"
+     */
+    _getNbColumns(columnEls, isMobile) {
+        if (!columnEls) {
+            return 0;
+        }
+        if (this._areColsCustomized(columnEls, isMobile)) {
+            return "custom";
+        }
+
+        const resolutionModifier = isMobile ? "" : "lg-";
+        const colRegex = new RegExp(`(?:^|\\s+)col-${resolutionModifier}(\\d{1,2})(?!\\S)`);
+        const colSize = parseInt(columnEls[0].className.match(colRegex)?.[1] || 12);
+        const offsetSize = this._getFirstItem(columnEls, isMobile).classList
+            .contains(`offset-${resolutionModifier}1`) ? 1 : 0;
+
+        return Math.floor((12 - offsetSize) / colSize);
+    },
+    /**
+     * Retrieves the mobile order class as a match array if there is one.
+     *
+     * @private
+     * @param {HTMLElement} el
+     * @returns {Array|null} class match ["o_we_mobile_order_XX", "XX"]
+     */
+    _getItemMobileOrder(el) {
+        return el.className.match(/\bo_we_mobile_order_([0-9]+)\b/);
+    },
+    /**
+     * Gets the first item, whether it has a mobile order class or not.
+     *
+     * @private
+     * @param {HTMLCollection} columnEls - elements in the .row container
+     * @param {boolean} isMobile
+     * @returns {HTMLElement} first HTMLElement in order
+     */
+    _getFirstItem(columnEls, isMobile) {
+        return isMobile && [...columnEls].find(el => el.classList.contains("o_we_mobile_order_0"))
+            || columnEls[0];
+    },
+    /**
+     * Adds the classes for mobile order.
+     *
+     * @private
+     * @param {HTMLCollection} columnEls - elements in the .row container
+     */
+    _addMobileOrders(columnEls) {
+        for (let i = 0; i < columnEls.length; i++) {
+            const mobileOrderClass = `o_we_mobile_order_${i}`;
+            columnEls[i].classList.add(mobileOrderClass);
+        }
+    },
+    /**
+     * Checks whether some columns were resized or were added offsets manually.
+     *
+     * @private
+     * @param {HTMLElement} columnEls
+     * @param {boolean} isMobile
+     * @returns {boolean}
+     */
+    _areColsCustomized(columnEls, isMobile) {
+        const resolutionModifier = isMobile ? "" : "lg-";
+        const colRegex = new RegExp(`(?:^|\\s+)col-${resolutionModifier}(\\d{1,2})(?!\\S)`);
+        const colSize = parseInt(columnEls[0].className.match(colRegex)?.[1] || 12);
+
+        // Cases where we know the columns sizes and/or offsets are NOT custom:
+        // - if all columns have an equal size AND
+        //     - if there are no offsets OR
+        //     - if, with 5 columns, there is exactly one offset-1 and it's on
+        //       the 1st item
+        // Any other case is custom.
+        const allColsSizesEqual = [...columnEls].every((columnEl) =>
+            parseInt(columnEl.className.match(colRegex)?.[1] || 12) === colSize);
+        if (!allColsSizesEqual) {
+            return true;
+        }
+        const offsetRegex = new RegExp(`(?:^|\\s+)offset-${resolutionModifier}[1-9][0-1]?(?!\\S)`);
+        const nbOffsets = [...columnEls]
+            .filter((columnEl) => columnEl.className.match(offsetRegex)).length;
+        if (nbOffsets === 0) {
+            return false;
+        }
+        if (nbOffsets === 1 && colSize === 2 && this._getFirstItem(columnEls, isMobile).className
+                .match(`offset-${resolutionModifier}1`)) {
+            return false;
+        }
+        return true;
+    },
+};
