e912b8fa819004003682dfea525f6ea91eb7cc6b
pedrambiria <pebr@odoo.com>
2023-05-10 10:39:08 +0000

    [FIX] pos_restaurant: stay on current table after refund
    Previously, when conducting a refund on a table, the system would
    switch the active table to the table associated with the refunded
    order. This was not the desired behavior, as it resulted in the details
    of the order disappearing, requiring the user to navigate back to
    the refund process.
    
    Steps to reproduce the issue:
    
     - install PoS restaurant
     - pick a table, do an order and pay it
     - pick another table, refund
     - choose the previous order and refund
    -> the order details disappear directly. You have to go back to refund.
    
    opw-3301603
    
    X-original-commit: 22b37fa9958eb07f9a46cc2e3770febc33bff77b

--- addons/pos_restaurant/tests/test_frontend.py
+++ addons/pos_restaurant/tests/test_frontend.py
@@ -211,3 +211,7 @@ class TestFrontend(odoo.tests.HttpCase):
     def test_06_split_bill_screen(self):
         self.pos_config.with_user(self.env.ref('base.user_demo')).open_ui()
         self.start_tour("/pos/ui?config_id=%d" % self.pos_config.id, 'SplitBillScreenTour2', login="demo")
+
+    def test_07_refund_stay_current_table(self):
+        self.pos_config.with_user(self.env.ref('base.user_demo')).open_ui()
+        self.start_tour("/pos/ui?config_id=%d" % self.pos_config.id, 'RefundStayCurrentTableTour', login="demo")
