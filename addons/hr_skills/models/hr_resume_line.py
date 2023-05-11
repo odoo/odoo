16d9f295784140236d01e488cbdb5d21f7a721d1
Dossogne Bertrand <bedo@odoo.com>
2023-05-11 12:56:20 +0000

    [IMP] hr_skills: make resume lines translatable
    Before this commit, the name and description of resume lines
    were not translatable.
    
    task-3323624
    
    closes odoo/odoo#121166
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_skills/models/hr_resume_line.py
+++ addons/hr_skills/models/hr_resume_line.py
@@ -10,10 +10,10 @@ class ResumeLine(models.Model):
     _order = "line_type_id, date_end desc, date_start desc"
 
     employee_id = fields.Many2one('hr.employee', required=True, ondelete='cascade')
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, translate=True)
     date_start = fields.Date(required=True)
     date_end = fields.Date()
-    description = fields.Text(string="Description")
+    description = fields.Text(string="Description", translate=True)
     line_type_id = fields.Many2one('hr.resume.line.type', string="Type")
 
     # Used to apply specific template on a line
