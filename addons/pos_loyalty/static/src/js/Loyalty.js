7316409ecf55e4e911c97cd25585d65895624848
roen-odoo <roen@odoo.com>
2023-05-11 15:59:49 +0000

    [FIX] pos_loyalty: specific discountable take discount into account
    Current behavior:
    When computing the discountable values of an order, the discount of the
    program where not taken into account if you used specific discountable
    products.
    
    Steps to reproduce:
    - Create products A and B with a price of 20$
    - Create a loyalty program
    - Rule:
      - Minimum qty : 2
      - Apply on specific products : Product A and Product B
      - Reward : 5 points per order
    - Reward:
      - 10$ per order
      - In exchange of 2 points
      - Apply on specific products : Product A and Product B
    - Open PoS and add Product A and Product B to the order
    - A reward should be added automatically with a value of -10$
    - Click on the reward button, another reward should be added but it has
      the wrong value. It should be -10$ but it is not.
    
    opw-3232565
    
    X-original-commit: beff6b0d3b9cd7327cc1049496aeff7746955080

--- addons/pos_loyalty/static/src/js/Loyalty.js
+++ addons/pos_loyalty/static/src/js/Loyalty.js
@@ -1285,7 +1285,7 @@ patch(Order.prototype, "pos_loyalty.Order", {
             } else if (line.reward_id) {
                 const lineReward = this.pos.reward_by_id[line.reward_id];
                 if (lineReward.id === reward.id) {
-                    continue;
+                    linesToDiscount.push(line);
                 }
                 if (!discountLinesPerReward[line.reward_identifier_code]) {
                     discountLinesPerReward[line.reward_identifier_code] = [];
