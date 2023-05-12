1393735831d9d33737006fc5b20480b0c12f2d35
stevTresCloud <steven.luna@trescloud.com>
2023-05-12 11:25:37 -0500

    [WIP] l10n_ec: Migrations for withhold, sales and purchase taxes to fix tax report 103
    - Migration for sales profit tax witholds that should not affect the tax report 103, it must be empty.
    - Migration for missings account tags in vat and purchase taxes that affect the tax report 103.
      Also add the account tag for the account move lines that must will have the missing account tag to fix the tax report 103.

--- addons/l10n_ec/__manifest__.py
+++ addons/l10n_ec/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     "name": "Ecuadorian Accounting",
-    "version": "3.4",
+    "version": "3.1",
     "description": """
 Functional
 ----------
