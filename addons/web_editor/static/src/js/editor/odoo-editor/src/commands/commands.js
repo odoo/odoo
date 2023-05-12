f98436ad79bbc86e02f57a3405864bf0716ee2e5
abd-msyukyu-odoo <abd@odoo.com>
2023-05-12 09:57:25 +0000

    [FIX] web_editor: prevent edition of non editable content in selections
    Some commands of the editor are able to modify non editable nodes in the editor,
    notably those that use the `formatSelection` util. This commit intent is to
    prevent such commands to have an effect on `contenteditable="false"` nodes.
    Here is the list of impacted methods:
    - setTag
    - switchDirection
    - unlink
    - indentList
    - toggleList
    - applyColor
    - every function using `formatSelection`:
      - bold
      - italic
      - underline
      - strikeThrough
      - setFontSize
    
    In knowledge, `Behaviors` are OWL components that are used in the editor, and
    they all have `contenteditable="false"` parts that are managed by OWL. Those
    functions could modify those managed nodes and make OWL crash.
    
    task-3280704
    
    Part-of: odoo/odoo#121249

--- addons/web_editor/static/src/js/editor/odoo-editor/src/commands/commands.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/commands/commands.js
@@ -323,7 +323,8 @@ export const editorCommands = {
         const range = getDeepRange(editor.editable, { correctTripleClick: true });
         const selectedBlocks = [...new Set(getTraversedNodes(editor.editable, range).map(closestBlock))];
         const deepestSelectedBlocks = selectedBlocks.filter(block => (
-            !descendants(block).some(descendant => selectedBlocks.includes(descendant))
+            !descendants(block).some(descendant => selectedBlocks.includes(descendant)) &&
+            block.isContentEditable
         ));
         for (const block of deepestSelectedBlocks) {
             if (
@@ -370,7 +371,7 @@ export const editorCommands = {
         getDeepRange(editor.editable, { splitText: true, select: true, correctTripleClick: true });
         const selection = editor.document.getSelection();
         const selectedTextNodes = [selection.anchorNode, ...getSelectedNodes(editor.editable), selection.focusNode]
-            .filter(n => n.nodeType === Node.TEXT_NODE && n.nodeValue.trim().length);
+            .filter(n => n.nodeType === Node.TEXT_NODE && closestElement(n).isContentEditable && n.nodeValue.trim().length);
 
         const changedElements = [];
         const defaultDirection = editor.options.direction;
@@ -455,7 +456,7 @@ export const editorCommands = {
             }
         }
         const targetedNodes = isCollapsed ? [sel.anchorNode] : getSelectedNodes(editor.editable);
-        const links = new Set(targetedNodes.map(node => closestElement(node, 'a')).filter(a => a));
+        const links = new Set(targetedNodes.map(node => closestElement(node, 'a')).filter(a => a && a.isContentEditable));
         if (links.size) {
             const cr = preserveCursor(editor.document);
             for (const link of links) {
@@ -476,7 +477,8 @@ export const editorCommands = {
                 cli &&
                 cli.tagName == 'LI' &&
                 !li.has(cli) &&
-                !cli.classList.contains('oe-nested')
+                !cli.classList.contains('oe-nested') &&
+                cli.isContentEditable
             ) {
                 li.add(cli);
             }
@@ -496,11 +498,11 @@ export const editorCommands = {
         const blocks = new Set();
 
         for (const node of getTraversedNodes(editor.editable)) {
-            if (node.nodeType === Node.TEXT_NODE && !isVisibleStr(node)) {
+            if (node.nodeType === Node.TEXT_NODE && !isVisibleStr(node) && closestElement(node).isContentEditable) {
                 node.remove();
             } else {
                 let block = closestBlock(node);
-                if (!['OL', 'UL'].includes(block.tagName)) {
+                if (!['OL', 'UL'].includes(block.tagName) && block.isContentEditable) {
                     block = block.closest('li') || block;
                     const ublock = block.closest('ol, ul');
                     ublock && getListMode(ublock) == mode ? li.add(block) : blocks.add(block);
@@ -528,7 +530,9 @@ export const editorCommands = {
      * @param {Element} [element]
      */
     applyColor: (editor, color, mode, element) => {
-        const selectedTds = editor.editable.querySelectorAll('td.o_selected_td');
+        const selectedTds = [...editor.editable.querySelectorAll('td.o_selected_td')].filter(
+            node => closestElement(node).isContentEditable
+        );
         let coloredTds = [];
         if (selectedTds.length) {
             for (const td of selectedTds) {
@@ -549,7 +553,7 @@ export const editorCommands = {
         if (!range) return;
         const restoreCursor = preserveCursor(editor.document);
         // Get the <font> nodes to color
-        const selectionNodes = getSelectedNodes(editor.editable);
+        const selectionNodes = getSelectedNodes(editor.editable).filter(node => closestElement(node).isContentEditable);
         if (isEmptyBlock(range.endContainer)) {
             selectionNodes.push(range.endContainer, ...descendants(range.endContainer));
         }
