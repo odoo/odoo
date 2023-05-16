1d5eb9b0f29b57f12e5007bd6b5fb40637f8c35a
dhba-odoo <dhba@odoo.com>
2023-05-16 09:36:10 +0000

    [FIX] web_editor: shift paste of vs code content looses indentation
    Before this commit:
    
    when performing a shift paste of copied VS Code content, the indentation was
    lost.
    
    After this commit:
    
    Now, when you perform a shift paste of copied VS Code content, the indentation
    is preserved
    
    Task-2884491
    
    closes odoo/odoo#136855
    
    X-original-commit: 1641bb50aa30ac64340eb261b26d4706944299ba
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/test/spec/copyPaste.test.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/test/spec/copyPaste.test.js
@@ -275,7 +275,7 @@ describe('Paste', () => {
                     stepFunction: async editor => {
                         await pasteText(editor, 'x    y');
                     },
-                    contentAfter: '<p>abx    y[]cd</p>',
+                    contentAfter: '<p>abx&nbsp; &nbsp; y[]cd</p>',
                 });
             });
             it('should paste a text in a span', async () => {
@@ -335,7 +335,7 @@ describe('Paste', () => {
                     stepFunction: async editor => {
                         await pasteText(editor, 'x    y');
                     },
-                    contentAfter: '<p>ax    y[]d</p>',
+                    contentAfter: '<p>ax&nbsp; &nbsp; y[]d</p>',
                 });
             });
             it('should paste a text in a span', async () => {
