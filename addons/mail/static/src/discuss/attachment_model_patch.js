892eacd4c76b4aa5614e6207f4806b578bbc50b1
Sébastien Theys <seb@odoo.com>
2023-05-16 17:37:20 +0200

    [REF] mail: move attachment deletable (channel) to discuss folder
    Part of task-3265211

--- addons/mail/static/src/discuss/attachment_model_patch.js
+++ addons/mail/static/src/discuss/attachment_model_patch.js
@@ -4,6 +4,12 @@ import { Attachment } from "@mail/attachments/attachment_model";
 import { patch } from "@web/core/utils/patch";
 
 patch(Attachment.prototype, "discuss", {
+    get isDeletable() {
+        if (this.message && this.originThread?.model === "discuss.channel") {
+            return this.message.editable;
+        }
+        return this._super();
+    },
     get urlRoute() {
         if (!this.accessToken && this.originThread?.model === "discuss.channel") {
             return this.isImage
