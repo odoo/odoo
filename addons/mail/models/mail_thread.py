2dd78d2e899302a81bd1194dfa4d1033a38acb44
Krzysztof Magusiak <chris.magusiak@gmail.com>
2023-05-16 14:53:06 +0000

    [FIX] mail: _search_message_partner_ids performance
    Manual backport of
    https://github.com/odoo/odoo/commit/d1aac322c2201d7b96d43a82f7c130ff2c140776
    
    Use the operator inselect instead of in with a read() before it.
    This ensures a subquery is used instead of two separate queries.
    
    When you have a user that is following thousands of tasks and the
    security filter depends on this search, all queries are very slow. Using
    a subquery prevents from reading these ids and sending back a very long
    query to the database.
    
    closes odoo/odoo#121561
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -119,17 +119,17 @@ class MailThread(models.AbstractModel):
 
     @api.model
     def _search_message_partner_ids(self, operator, operand):
-        """Search function for message_follower_ids
-
-        Do not use with operator 'not in'. Use instead message_is_followers
-        """
-        # TOFIX make it work with not in
-        assert operator != "not in", "Do not search message_follower_ids with 'not in'"
-        followers = self.env['mail.followers'].sudo().search([
+        """Search function for message_follower_ids"""
+        neg = ''
+        if operator in expression.NEGATIVE_TERM_OPERATORS:
+            neg = 'not '
+            operator = expression.TERM_OPERATORS_NEGATION[operator]
+        followers = self.env['mail.followers'].sudo()._search([
             ('res_model', '=', self._name),
-            ('partner_id', operator, operand)])
-        # using read() below is much faster than followers.mapped('res_id')
-        return [('id', 'in', [res['res_id'] for res in followers.read(['res_id'])])]
+            ('partner_id', operator, operand),
+        ])
+        # use inselect to avoid reading thousands of potentially followed objects
+        return [('id', neg + 'inselect', followers.subselect('res_id'))]
 
     @api.depends('message_follower_ids')
     def _compute_message_is_follower(self):
