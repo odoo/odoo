0baa14141649391d7a12e960568e792ca3979f51
dane@odoo.com <dane@odoo.com>
2023-05-17 11:19:47 +0200

    [FIX] mass_mailing: fix photo uploading
    Images weren't shown when using predefined templates, the problem was
    that returned  object wasn't assigned to anything, because sometimes
    `filename` wasn't found in `imagesInfo` object,
    which caused wierd behaviour.
    bug happened: f4f0f783183507df8227b37fe1234c256325df6d
    
    task-3328675

--- addons/mass_mailing/static/src/js/mass_mailing_html_field.js
+++ addons/mass_mailing/static/src/js/mass_mailing_html_field.js
@@ -273,8 +273,8 @@ export class MassMailingHtmlField extends HtmlField {
                 const imagesInfo = Object.assign({
                     all: {}
                 }, $theme.data("imagesInfo") || {});
-                for (const info of Object.values(imagesInfo)) {
-                    Object.assign({
+                for (const [key, info] of Object.entries(imagesInfo)) {
+                    imagesInfo[key] = Object.assign({
                         module: "mass_mailing",
                         format: "jpg"
                         },
