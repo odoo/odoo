6a4bac7e5dac96077a1b02c6bc1dbb5331c804ab
Rodolpho Lima <rcdl@odoo.com>
2023-05-17 15:51:07 +0000

    [FIX] web_editor: toolbar scroll over navbar
    Before this commit the floating toolbar would scroll over the headers
    above the html field, such as the form view header and the web client
    navbar.
    
    This commit ensures the toolbar is hidden when it overflows the scroll
    container.
    
    task-3263463
    
    Part-of: odoo/odoo#132714

--- addons/web_editor/static/tests/field_html_tests.js
+++ addons/web_editor/static/tests/field_html_tests.js
@@ -253,6 +253,7 @@ QUnit.module('web_editor', {}, function () {
             });
 
             await testUtils.form.clickEdit(form);
+            await new Promise(resolve => setTimeout(resolve, 50));
             var $field = form.$('.oe_form_field[name="body"]');
 
             // select the text
@@ -274,7 +275,6 @@ QUnit.module('web_editor', {}, function () {
                 return openingProm;
             }
 
-            await new Promise((resolve)=>setTimeout(resolve, 50));
             await openColorpicker('#toolbar .note-back-color-preview');
             assert.ok($('.note-back-color-preview .dropdown-menu').hasClass('show'),
                 "should display the color picker");
