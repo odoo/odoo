673e7b73c46d2c37b1edc462107db2067c2676ea
Alexandre Kühn <aku@odoo.com>
2023-05-17 10:25:24 +0000

    [FIX] mail: several small discuss code cleaning
    Follow-up of https://github.com/odoo/odoo/pull/117357
    post-merge comments.
    
    Part-of: odoo/odoo#121683

--- addons/mail/static/src/core/thread_service.js
+++ addons/mail/static/src/core/thread_service.js
@@ -1088,6 +1088,18 @@ export class ThreadService {
         }
         return DEFAULT_AVATAR;
     }
+
+    /**
+     * @param {number} threadId
+     * @param {string} data base64 representation of the binary
+     * @returns 
+     */
+    async notifyThreadAvatarToServer(threadId, data) {
+        return this.rpc("/discuss/channel/update_avatar", {
+            channel_id: threadId,
+            data,
+        });
+    }
 }
 
 export const threadService = {
