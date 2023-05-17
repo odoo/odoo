36c7fc5449f1fd924af34cc1e1f37e5f475b915a
Sébastien Theys <seb@odoo.com>
2023-05-17 12:46:29 +0000

    [REF] mail: clean typing service
    Make the whole service reactive to avoid state forwarding issues.
    
    Part-of: odoo/odoo#121418

--- addons/mail/static/src/discuss/typing/typing_service.js
+++ addons/mail/static/src/discuss/typing/typing_service.js
@@ -7,20 +7,14 @@ import { useService } from "@web/core/utils/hooks";
 
 export const OTHER_LONG_TYPING = 60000;
 
-/**
- * @typedef TypingState
- * @property {Map<number, Set<number>>} memberIdsByChannelId
- * @property {Map<number, number>} timerByMemberId
- */
 export class Typing {
     busService;
     /** @type {import("@mail/core/channel_member_service").ChannelMemberService} */
     channelMemberService;
-    /** @type {TypingState} */
-    state = reactive({
-        memberIdsByChannelId: new Map(),
-        timerByMemberId: new Map(),
-    });
+    /** @type {Map<number, Set<number>>} */
+    memberIdsByChannelId = new Map();
+    /** @type {Map<number, number>} */
+    timerByMemberId = new Map();
     /** @type {import("@mail/core/store_service").Store} */
     storeService;
 
@@ -30,7 +24,6 @@ export class Typing {
         "mail.store": storeService,
     }) {
         Object.assign(this, { busService, channelMemberService, storeService });
-        this.setup();
     }
 
     setup() {
@@ -49,82 +42,66 @@ export class Typing {
      * @param {import("@mail/core/channel_member_model").ChannelMember} member
      */
     addTypingMember(member) {
-        if (!this.state.memberIdsByChannelId.has(member.thread.id)) {
-            this.state.memberIdsByChannelId.set(member.thread.id, new Set());
+        if (!this.memberIdsByChannelId.has(member.thread.id)) {
+            this.memberIdsByChannelId.set(member.thread.id, new Set());
         }
-        const memberIds = this.state.memberIdsByChannelId.get(member.thread.id);
+        const memberIds = this.memberIdsByChannelId.get(member.thread.id);
         memberIds.add(member.id);
-        browser.clearTimeout(this.state.timerByMemberId.get(member.id));
-        this.state.timerByMemberId.set(
+        browser.clearTimeout(this.timerByMemberId.get(member.id));
+        this.timerByMemberId.set(
             member.id,
             browser.setTimeout(() => this.removeTypingMember(member), OTHER_LONG_TYPING)
         );
     }
 
     /**
-     * @param {TypingState} state
      * @param {import("@mail/core/thread_model").Thread} channel
      * @returns {import("@mail/core/channel_member_model").ChannelMember[]}
      */
-    getTypingMembers(state, channel) {
-        return [...(state.memberIdsByChannelId.get(channel.id) ?? new Set())]
+    getTypingMembers(channel) {
+        return [...(this.memberIdsByChannelId.get(channel.id) ?? new Set())]
             .map((id) => this.channelMemberService.insert({ id }))
             .filter((member) => member.persona !== this.storeService.self);
     }
 
     /**
-     * @param {TypingState} state
      * @param {import("@mail/core/thread_model").Thread} channel
      * @returns {boolean}
      */
-    hasTypingMembers(state, channel) {
-        return this.getTypingMembers(state, channel).length > 0;
+    hasTypingMembers(channel) {
+        return this.getTypingMembers(channel).length > 0;
     }
 
     /**
      * @param {import("@mail/core/channel_member_model").ChannelMember} member
      */
     removeTypingMember(member) {
-        const memberIds = this.state.memberIdsByChannelId.get(member.thread.id);
+        const memberIds = this.memberIdsByChannelId.get(member.thread.id);
         if (memberIds) {
             memberIds.delete(member.id);
             if (memberIds.size === 0) {
-                this.state.memberIdsByChannelId.delete(member.thread.id);
+                this.memberIdsByChannelId.delete(member.thread.id);
             }
         }
-        browser.clearTimeout(this.state.timerByMemberId.get(member.id));
-        this.state.timerByMemberId.delete(member.id);
+        browser.clearTimeout(this.timerByMemberId.get(member.id));
+        this.timerByMemberId.delete(member.id);
     }
 }
 
 export const discussTypingService = {
     dependencies: ["bus_service", "discuss.channel.member", "mail.store"],
     start(env, services) {
-        return new Typing(services);
+        const typing = reactive(new Typing(services));
+        typing.setup();
+        return typing;
     },
 };
 
 registry.category("services").add("discuss.typing", discussTypingService);
 
-// wrapper to ensure service getters receive the reactive state of the component
+/**
+ * @returns {Typing}
+ */
 export function useTypingService() {
-    /** @type {Typing} */
-    const typingService = useService("discuss.typing");
-    const state = useState(typingService.state);
-    return {
-        /**
-         * @param {import("@mail/core/thread_model").Thread} channel
-         * @returns {import("@mail/core/channel_member_model").ChannelMember[]}
-         */
-        getTypingMembers(channel) {
-            return typingService.getTypingMembers(state, channel);
-        },
-        /**
-         * @param {import("@mail/core/thread_model").Thread} channel
-         * @returns {boolean}
-         */
-        hasTypingMembers(channel) {
-            return typingService.hasTypingMembers(state, channel);
-        },
-    };
+    return useState(useService("discuss.typing"));
 }
