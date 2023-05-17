82eb9bbb8801dfd259e4c695d25167d0fa21fa04
abd-msyukyu-odoo <abd@odoo.com>
2023-05-17 10:55:22 +0000

    [FIX] web: rework draggable_hook scrolling
    In Knowledge, the scrollParent for the `Y` axis of an embedded ungrouped Kanban
    view is the article body, which contains the draggable `container` (which is
    itself contained in the scrollParent for the `X` axis).
    
    This commit introduce 2 fixes related to the `draggable_hook_builder`:
    - In `draggable_hook_builder`, `updateRects` was modifying values of the
      `containerRect` from values of the `scrollParent`. Then, the `containerRect`
      was used in `handleEdgeScrolling` to compute a scroll value to apply on the
      `scrollParent`, and when updating the dragged element position, to compute the
      boundaries for the dragged element. This commit stores values for
      `scrollParent` in `scrollParentRect` alongside the modified `containerRect`.
      `scrollParentRect` will be used to compute the scroll value based only on the
      dimensions of the scrollParent, and the modified `containerRect` will be used
      to compute the boundaries.
    - The `scrollParent` in the `X` axis can be different from the `scrollParent` in
      the `Y` axis. This commit stores both of them individually so that one is not
      neglected when higher in the DOM than the other.
    
    Task-3291771
    
    closes odoo/odoo#122211
    
    X-original-commit: 1f784652f9431c8716329f284133df215656c4b9
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>
    Signed-off-by: Abeloos Damien (abd) <abd@odoo.com>

--- addons/web/static/tests/core/utils/sortable_tests.js
+++ addons/web/static/tests/core/utils/sortable_tests.js
@@ -1,6 +1,13 @@
 /** @odoo-module **/
 
-import { drag, dragAndDrop, getFixture, mount, nextTick } from "@web/../tests/helpers/utils";
+import {
+    drag,
+    dragAndDrop,
+    getFixture,
+    mockAnimationFrame,
+    mount,
+    nextTick,
+} from "@web/../tests/helpers/utils";
 import { useSortable } from "@web/core/utils/sortable";
 
 import { Component, reactive, useRef, useState, xml } from "@odoo/owl";
@@ -191,6 +198,99 @@ QUnit.module("Draggable", ({ beforeEach }) => {
         assert.containsN(target, ".list", 3);
         assert.containsN(target, ".item", 9);
         assert.verifySteps(["start", "groupenter", "drop", "end"]);
+    });
+
+    QUnit.test("Sorting in groups with distinct per-axis scrolling", async (assert) => {
+        const { advanceFrame } = mockAnimationFrame();
+        class List extends Component {
+            setup() {
+                useSortable({
+                    ref: useRef("root"),
+                    elements: ".item",
+                    groups: ".list",
+                    connectGroups: true,
+                    edgeScrolling: { speed: 16, threshold: 25 },
+                });
+            }
+        }
+
+        List.template = xml`
+            <div class="scroll_parent_y" style="max-width: 150px; max-height: 200px; overflow-y: scroll; overflow-x: hidden;">
+                <div class="spacer_before" style="min-height: 50px;"></div>
+                <div class="spacer_horizontal" style="min-height: 50px;"></div>
+                <div t-ref="root" class="root d-flex align-items-end" style="overflow-x: scroll;">
+                    <div class="d-flex">
+                        <div style="padding-left: 20px;"
+                            t-foreach="[1, 2, 3]" t-as="c" t-key="c" t-attf-class="list m-0 list{{ c }}">
+                            <div style="min-width: 50px; min-height: 50px; padding-top: 20px;"
+                                t-foreach="[1, 2, 3]" t-as="l" t-key="l" t-esc="'item' + l + '' + c" t-attf-class="item item{{ l + '' + c }}"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="spacer_after" style="min-height: 150px;"></div>
+            </div>
+        `;
+        await mount(List, target);
+
+        assert.containsN(target, ".list", 3);
+        assert.containsN(target, ".item", 9);
+
+        const scrollParentX = target.querySelector(".root");
+        const scrollParentY = target.querySelector(".scroll_parent_y");
+        const assertScrolling = (top, left) => {
+            assert.strictEqual(scrollParentY.scrollTop, top);
+            assert.strictEqual(scrollParentX.scrollLeft, left);
+        };
+        const cancelDrag = async (cancel) => {
+            await cancel();
+            await nextTick();
+            scrollParentY.scrollTop = 0;
+            scrollParentX.scrollLeft = 0;
+            await nextTick();
+            assert.containsNone(target, ".o_dragged");
+        };
+        assert.containsNone(target, ".o_dragged");
+
+        // Negative horizontal scrolling.
+        target.querySelector(".spacer_horizontal").scrollIntoView();
+        scrollParentX.scrollLeft = 16;
+        await nextTick();
+        assertScrolling(50, 16);
+        let dragHelpers = await drag(".item12");
+        await dragHelpers.moveTo(".item11", "left");
+        await advanceFrame();
+        assertScrolling(50, 0);
+        await cancelDrag(dragHelpers.cancel);
+
+        // Positive horizontal scrolling.
+        target.querySelector(".spacer_horizontal").scrollIntoView();
+        await nextTick();
+        assertScrolling(50, 0);
+        dragHelpers = await drag(".item11");
+        await dragHelpers.moveTo(".item12", "right");
+        await advanceFrame();
+        assertScrolling(50, 16);
+        await cancelDrag(dragHelpers.cancel);
+
+        // Negative vertical scrolling.
+        target.querySelector(".root").scrollIntoView();
+        await nextTick();
+        assertScrolling(100, 0);
+        dragHelpers = await drag(".item11");
+        await dragHelpers.moveTo(".item11", "top");
+        await advanceFrame();
+        assertScrolling(84, 0);
+        await cancelDrag(dragHelpers.cancel);
+
+        // Positive vertical scrolling.
+        target.querySelector(".spacer_before").scrollIntoView();
+        await nextTick();
+        assertScrolling(0, 0);
+        dragHelpers = await drag(".item21");
+        await dragHelpers.moveTo(".item21", "bottom");
+        await advanceFrame();
+        assertScrolling(16, 0);
+        await cancelDrag(dragHelpers.cancel);
     });
 
     QUnit.test("Dynamically disable sortable feature", async (assert) => {
