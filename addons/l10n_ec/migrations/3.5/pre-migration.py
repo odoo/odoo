69b0cb131a5f2806f5630fc90e7c7f99d7908dca
stevTresCloud <steven.luna@trescloud.com>
2023-05-18 11:48:33 -0500

    [FIX] l10n_ec: English method and commentary name

--- addons/l10n_ec/migrations/3.5/pre-migration.py
+++ addons/l10n_ec/migrations/3.5/pre-migration.py
@@ -2,10 +2,10 @@
 
 from odoo import api, SUPERUSER_ID
 
-def update_no_updateable_option_in_l10n_ec_ifrs_record(env):
-    # Change the no updateable option to False, in the l10n_ec_ifrs record
+def update_no_updatable_option_in_l10n_ec_ifrs_record(env):
+    # Change the no updatable option to False, in the l10n_ec_ifrs record
     env['ir.model.data'].search([('module', '=', 'l10n_ec'), ('name', '=', 'l10n_ec_ifrs')]).write({'noupdate': False})
 
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    update_no_updateable_option_in_l10n_ec_ifrs_record(env)
+    update_no_updatable_option_in_l10n_ec_ifrs_record(env)
