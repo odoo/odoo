495d2b2c4b0b004bb6f5b3898db9360a207adb05
Marten (msc) <msc@odoo.com>
2023-05-19 09:54:07 -0400

    [FIX] phone_validation: skip test w/o phonenumbers installed
    `phonenumbers` is not included in the odoo `requirements.txt`.
    Without the library, a standard test fails.
    
    Following a discussion with rd-runbot/odoosh,
    the test is skipped w/o the library

--- addons/phone_validation/tests/test_phonenumbers.py
+++ addons/phone_validation/tests/test_phonenumbers.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+try:
+    import phonenumbers
+except ImportError:
+    phonenumbers = None
+
 from odoo.addons.phone_validation.tools import phone_validation
 from odoo.exceptions import UserError
 from odoo.tests import tagged
@@ -11,6 +16,9 @@ from odoo.tests.common import BaseCase
 class TestPhonenumbers(BaseCase):
 
     def test_country_code_falsy(self):
+        if not phonenumbers:
+            self.skipTest('Cannot test without phonenumbers module installed.')
+
         self.assertEqual(
             phone_validation.phone_format('0456998877', 'BE', '32', force_format='E164'),
             '+32456998877'
