36f079a26b5096d82b95ffeb6f62716ad7bcce14
William Henrotin <whe@odoo.com>
2023-05-19 14:34:22 +0000

    [FIX] stock: open stock quant form view to create move Lines
    If no stock quant are founded in the list when adding a new move line in
    the detailed operation. The `new` button opened a form view on
    stock.move.line model instead of stock quant.
    
    Task: 3256447
    Part-of: odoo/odoo#122445

--- addons/stock/static/src/fields/stock_move_line_x2_many_field.js
+++ addons/stock/static/src/fields/stock_move_line_x2_many_field.js
@@ -3,21 +3,29 @@
 import { registry } from "@web/core/registry";
 import { X2ManyField, x2ManyField } from "@web/views/fields/x2many/x2many_field";
 import { sprintf } from "@web/core/utils/strings";
-import { useSelectCreate } from "@web/views/fields/relational_utils";
+import { useSelectCreate, useOpenMany2XRecord} from "@web/views/fields/relational_utils";
 export class SMLX2ManyField extends X2ManyField {
     setup() {
         super.setup();
+
         const selectCreate = useSelectCreate({
             resModel: "stock.quant",
             activeActions: this.activeActions,
             onSelected: (resIds) => this.selectRecord(resIds),
-            onCreateEdit: ({ context }) => this._openRecord({ context }),
+            onCreateEdit: () => this.createOpenRecord(),
         });
 
         this.selectCreate = (params) => {
-            const p = Object.assign({}, params);
-            return selectCreate(p);
+            return selectCreate(params);
         };
+        this.openRecord = useOpenMany2XRecord({
+            resModel: "stock.quant",
+            activeActions: this.activeActions,
+            onRecordSaved: (resId) => this.selectRecord([resId.data.id]),
+            onRecordDiscarted: (resId) => this.selectRecord(resId),
+            fieldString: this.props.string,
+            is2Many: true,
+        });
     }
 
     async onAdd({ context, editable } = {}) {
@@ -40,6 +48,22 @@ export class SMLX2ManyField extends X2ManyField {
             context: { default_quant_id: res_ids[0] },
         };
         this.addInLine(params);
+    }
+
+    createOpenRecord() {
+        const activeElement = document.activeElement;
+        this.openRecord({
+            context: {
+                ...this.props.context,
+                form_view_ref: "stock.view_stock_quant_form",
+            },
+            immediate: true,
+            onClose: () => {
+                if (activeElement) {
+                    activeElement.focus();
+                }
+            },
+        });
     }
 }
 
