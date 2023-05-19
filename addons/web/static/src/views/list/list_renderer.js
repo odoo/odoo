5337fea906ab44be88dfeff5f6794455d843b448
paso-odoo <paso@odoo.com>
2023-05-19 10:43:45 +0000

    [FIX] web: prevent selection while record in edit mode from editable list view
    If applied, this commit will solve the issue of the list view selection when
    the record is not actually created but it can be selected and can able to
    perform the action.
    
    Steps to produce:
    - Open Accounting > Configurations > Chart of Accounts.
    - Click on the 'New' button.
    - Select the new line which has been added.
    - Perform the 'Duplicate' action for that record.
    
    This commit will fix the generic issue by preventing the record selection while
    the record is in 'Edit' mode.
    
    sentry - 4191036697
    
    closes odoo/odoo#128583
    
    X-original-commit: 201dfafb45c1285fbe4dcf0adb0d136097f676c9
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>

--- addons/web/static/src/views/list/list_renderer.js
+++ addons/web/static/src/views/list/list_renderer.js
@@ -1701,8 +1701,15 @@ export class ListRenderer extends Component {
         group.toggle();
     }
 
+    get canSelectRecord() {
+        return !this.props.list.editedRecord && !this.props.list.model.useSampleModel;
+    }
+
     toggleSelection() {
         const list = this.props.list;
+        if (!this.canSelectRecord) {
+            return;
+        }
         if (list.selection.length === list.records.length) {
             list.records.forEach((record) => {
                 record.toggleSelection(false);
@@ -1716,6 +1723,9 @@ export class ListRenderer extends Component {
     }
 
     toggleRecordSelection(record) {
+        if (!this.canSelectRecord) {
+            return;
+        }
         if (this.shiftKeyMode && this.lastCheckedRecord) {
             this.toggleRecordShiftSelection(record);
         } else {
