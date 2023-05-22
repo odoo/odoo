33bcb674d72e841db1f931526a424e0d81b410e1
Samuel Degueldre <sad@odoo.com>
2023-05-22 10:09:18 +0200

    [REF] pos_sale: remove unused mobile order management screen
    The mobile order management screen has never worked. A previous commit
    partially fixed it and caused it to now be rendered, but it's always
    rendered in a way that makes it no different from the desktop order
    management screen. The code has been dead for so long that allowing the
    code to go through the dead path crashes and after fixing the crashes
    the layout is broken. As such, this commit removes it entirely, and if
    the flow needs improvement on mobile it will be done as part of a
    separate task instead.

--- addons/pos_sale/static/src/js/SetSaleOrderButton.js
+++ addons/pos_sale/static/src/js/SetSaleOrderButton.js
@@ -2,31 +2,16 @@
 
 import { ProductScreen } from "@point_of_sale/js/Screens/ProductScreen/ProductScreen";
 import { usePos } from "@point_of_sale/app/pos_hook";
-import { Component, useState } from "@odoo/owl";
-import { useService } from "@web/core/utils/hooks";
+import { Component } from "@odoo/owl";
+
 export class SetSaleOrderButton extends Component {
     static template = "SetSaleOrderButton";
-
     setup() {
-        super.setup();
         this.pos = usePos();
-        this.ui = useState(useService("ui"));
-        this.orm = useService("orm");
     }
     async click() {
-        // FIXME POSREF why are we calling browse for a ping?
-        // Why don't we let the order management screen deal with the offline error?
-        await this.orm.call("sale.order", "browse", [[]]);
-        const screen = this.ui.isSmall
-            ? "MobileSaleOrderManagementScreen"
-            : "SaleOrderManagementScreen";
-        this.pos.showScreen(screen);
+        this.pos.showScreen("SaleOrderManagementScreen");
     }
 }
 
-ProductScreen.addControlButton({
-    component: SetSaleOrderButton,
-    condition: function () {
-        return true;
-    },
-});
+ProductScreen.addControlButton({ component: SetSaleOrderButton });
