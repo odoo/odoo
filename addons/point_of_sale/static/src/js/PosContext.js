bf4d2730d2eb0d14ab15adb82b0b00c6df8dab51
Samuel Degueldre <sad@odoo.com>
2023-05-22 15:23:27 +0200

    [REF] point_of_sale, pos_sale: remove orderManagement context
    Previously, the pos used singletons of reactive object to create shared
    state between components that could be imported. This is an ugly
    solution and this kind of "module level" object is difficult to patch or
    mock during tests. It's also a non-standard way to manage shared state.
    
    This commit simply adds this piece of shared state to the pos store,
    which is meant to hold the shared state of the pos.

--- addons/point_of_sale/static/src/js/PosContext.js
+++
@@ -1,5 +0,0 @@
-/** @odoo-module */
-import { reactive } from "@odoo/owl";
-// Create global context objects
-// FIXME POSREF singletons exported by modules are not testable, this should probably be a service
-export const orderManagement = reactive({ searchString: "", selectedOrder: null });
