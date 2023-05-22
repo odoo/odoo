abcda5e5a131ca466daabb16dfe860c986394789
Habib (ayh) <ayh@odoo.com>
2023-05-22 14:59:49 +0200

    [IMP] analytic: add search more option Allow users to find analytic accounts in the analytic widget if there are more than 7 results for their search criteria, by providing a Search More option.
    Additional Enhancements include:
    - remove the 'Add a Line' button. replace with an empty tag (no analytic account, remainder of distribution)
    - remove the custom AutoComplete. this was used to set the active group on autoComplete focus, and to prevent propogation of click events.
    - no longer keep track of the active group (editing plan). Instead pass the group (plan id) to the relevant functions.
    - rename variables, for example request > searchTerm
    - keep the header of the popup in place (no scroll when there are many tags)
    - test the widget in list view (with the search more feature)
    - add a new analytic account list view, as users should not multi-edit in the search more dialog
    
    Current behavior before PR:
    Users were forced to type the entire analytic account name if they had many analytic accounts with similar names.
    
    forward port of 36391b9aea82091bf7b689658e07149860b01a61
    Desired behavior after PR is merged:
    A "Search More" option is added to the Autocomplete options when there are more than 6 analytic accounts found. The Search More option provides a SelectCreate Dialog where users are able to find the required analytic account easily. Users are able to select multiple accounts by checking the items in the list.
    
    Task-3180055
    
    Forward port of bef8e00d78f439936534cef8b2fd8313f03f3b1c

--- addons/analytic/static/src/components/analytic_distribution/analytic_distribution.js
+++ addons/analytic/static/src/components/analytic_distribution/analytic_distribution.js
@@ -1,20 +1,22 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { useService } from "@web/core/utils/hooks";
+import { useService, useOwnedDialogs } from "@web/core/utils/hooks";
 import { evaluateExpr } from "@web/core/py_js/py";
 import { getNextTabableElement, getPreviousTabableElement } from "@web/core/utils/ui";
 import { usePosition } from "@web/core/position_hook";
 import { getActiveHotkey } from "@web/core/hotkeys/hotkey_service";
 import { shallowEqual } from "@web/core/utils/arrays";
+import { sprintf } from "@web/core/utils/strings";
 import { _lt } from "@web/core/l10n/translation";
-import { AnalyticAutoComplete } from "../autocomplete/autocomplete";
+import { AutoComplete } from "@web/core/autocomplete/autocomplete";
 
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { TagsList } from "@web/views/fields/many2many_tags/tags_list";
 import { useOpenMany2XRecord } from "@web/views/fields/relational_utils";
 import { parseFloat as oParseFloat } from "@web/views/fields/parsers";
 import { formatPercentage } from "@web/views/fields/formatters";
+import { SelectCreateDialog } from "@web/views/view_dialogs/select_create_dialog";
 
 const { Component, useState, useRef, useExternalListener, onWillUpdateProps, onWillStart, onPatched } = owl;
 
@@ -23,7 +25,6 @@ const PLAN_APPLICABILITY = {
     optional: _lt("Optional"),
 }
 const PLAN_STATUS = {
-    editing: _lt("Editing"),
     invalid: _lt("Invalid"),
     ok: _lt("OK"),
 }
@@ -45,7 +46,6 @@ export class AnalyticDistribution extends Component {
 
         this.nextId = 1;
         this.focusSelector = false;
-        this.activeGroup = false;
 
         this.currentValue = this.props.record.data[this.props.name];
 
@@ -74,6 +74,10 @@ export class AnalyticDistribution extends Component {
         this.allPlans = [];
         this.lastAccount = this.props.account_field && this.props.record.data[this.props.account_field] || false;
         this.lastProduct = this.props.product_field && this.props.record.data[this.props.product_field] || false;
+
+        this.selectCreateIsOpen = false;
+        this.addDialog = useOwnedDialogs();
+        this.onSearchMore = this._onSearchMore.bind(this);
     }
 
     // Lifecycle
@@ -190,18 +194,18 @@ export class AnalyticDistribution extends Component {
     }
 
     // Autocomplete
-    get sourcesAnalyticAccount() {
-        return [this.optionsSourceAnalytic];
+    sourcesAnalyticAccount(groupId) {
+        return [this.optionsSourceAnalytic(groupId)];
     }
 
-    get optionsSourceAnalytic() {
+    optionsSourceAnalytic(groupId) {
         return {
             placeholder: this.env._t("Loading..."),
-            options: this.loadOptionsSourceAnalytic.bind(this),
+            options:(searchTerm) => this.loadOptionsSourceAnalytic(groupId, searchTerm),
         };
     }
 
-    async loadOptionsSourceAnalytic(request) {
+    analyticAccountDomain(groupId=null) {
         let domain = [['id', 'not in', this.existingAnalyticAccountIDs]];
         if (this.props.record.data.company_id){
             domain.push(
@@ -211,11 +215,28 @@ export class AnalyticDistribution extends Component {
             );
         }
 
-        if (this.activeGroup) {
-            domain.push(['root_plan_id', '=', this.activeGroup]);
+        if (groupId) {
+            domain.push(['root_plan_id', '=', groupId]);
         }
+        return domain;
+    }
 
-        const records = await this.fetchAnalyticAccounts([...domain, '|', ["name", "ilike", request], '|', ['code', 'ilike', request], ['partner_id', 'ilike', request]], 7);
+    searchAnalyticDomain(searchTerm) {
+        return [
+            '|',
+            ["name", "ilike", searchTerm],
+            '|',
+            ['code', 'ilike', searchTerm],
+            ['partner_id', 'ilike', searchTerm],
+        ];
+    }
+
+    async loadOptionsSourceAnalytic(groupId, searchTerm) {
+        const searchLimit = 6;
+
+        const records = await this.fetchAnalyticAccounts([
+            ...this.analyticAccountDomain(groupId),
+            ...this.searchAnalyticDomain(searchTerm)], searchLimit + 1);
 
         let options = records.map((result) => ({
             value: result.id,
@@ -223,6 +244,14 @@ export class AnalyticDistribution extends Component {
             group_id: result.root_plan_id[0],
             color: result.color,
         }));
+
+        if (searchLimit < records.length) {
+            options.push({
+                label: this.env._t("Search More..."),
+                action: (editedTag) => this.onSearchMore(searchTerm, editedTag),
+                classList: "o_m2o_dropdown_option o_m2o_dropdown_option_search_more",
+            });
+        }
 
         if (!options.length) {
             options.push({
@@ -235,56 +264,98 @@ export class AnalyticDistribution extends Component {
         return options;
     }
 
-    groupAutocompleteFocus(group_id) {
-        this.activeGroup = group_id;
+    async _onSearchMore(searchTerm, editedTag) {
+        let dynamicFilters = [];
+        if (searchTerm.length) {
+            dynamicFilters = [
+                {
+                    description: sprintf(this.env._t("Quick search: %s"), searchTerm),
+                    domain: this.searchAnalyticDomain(searchTerm),
+                },
+            ];
+        }
+        this.selectCreateIsOpen = true;
+        this.addDialog(SelectCreateDialog, {
+            title: this.env._t("Search: Analytic Account"),
+            noCreate: true,
+            multiSelect: true,
+            resModel: 'account.analytic.account',
+            context: {
+                tree_view_ref: "analytic.view_account_analytic_account_list_select",
+            },
+            domain: this.analyticAccountDomain(editedTag.group_id),
+            dynamicFilters: dynamicFilters,
+            onSelected: async (resIds) => {
+                const analytic_accounts = await this.fetchAnalyticAccounts([["id", "in", resIds]]);
+                // modify the edited tag
+                editedTag.analytic_account_id = analytic_accounts[0].id;
+                editedTag.analytic_account_name = analytic_accounts[0].display_name;
+                this.setFocusSelector(`.tag_${editedTag.id} .o_analytic_percentage`);
+                if (analytic_accounts.length > 1) {
+                    const planId = editedTag.group_id;
+                    // remove the autofill line
+                    this.list[planId].distribution = this.list[planId].distribution.filter((t) => !!t.analytic_account_id);
+                    for (const account of analytic_accounts.slice(1)) {
+                        // add new tags
+                        const tag = this.newTag(planId);
+                        tag.analytic_account_id = account.id;
+                        tag.analytic_account_name = account.display_name;
+                        this.list[planId].distribution.push(tag);
+                    }
+                }
+                this.autoFill();
+            },
+            onCreateEdit: () => {},
+        }, {
+            onClose: () => {
+                if (!editedTag.analytic_account_id) {
+                    this.setFocusSelector(`.tag_${editedTag.id} .o_analytic_account_name`);
+                    this.focusToSelector();
+                }
+                this.selectCreateIsOpen = false;
+            },
+        });
     }
 
-    autoCompleteInput(dist_tag, {inputValue}) {
-        if (inputValue === "") {
-            dist_tag.analytic_account_id = "";
+    autoCompleteInputChanged(distTag, inputValue) {
+        if (inputValue === "" && distTag.analytic_account_id) {
+            this.deleteTag(distTag.id, distTag.group_id);
         }
     }
 
     // Editing Distributions
     async onSelect(option, params, tag) {
+        if (option.action) {
+            return option.action(tag);
+        }
         const selected_option = Object.getPrototypeOf(option);
         tag.analytic_account_id = parseInt(selected_option.value);
         tag.analytic_account_name = selected_option.label;
         tag.color = selected_option.color;
         this.setFocusSelector(`.tag_${tag.id} .o_analytic_percentage`);
+        this.autoFill();
     }
 
     async percentageChanged(dist_tag, ev) {
         dist_tag.percentage = this.parse(ev.target.value);
-
-        if (this.remainderByGroup(dist_tag.group_id)) {
-            this.setFocusSelector(`#plan_${dist_tag.group_id} .incomplete .o_analytic_account_name`);
+        if (dist_tag.percentage == 0) {
+            this.deleteTag(dist_tag.id, dist_tag.group_id);
         }
         this.autoFill();
     }
 
-    deleteTag(id) {
-        for (const group_id in this.list) {
-            this.list[group_id].distribution = this.list[group_id].distribution.filter((dist_tag) => dist_tag.id != id);
-            if (this.list[group_id].applicability === 'mandatory' && this.remainderByGroup(group_id)) this.addLineToGroup(group_id);
-        }
+    deleteTag(id, fromGroup) {
+        // find the next tag to focus to before deleting the tag
+        const allTags = this.listFlat;
+        const currentTagIndex = allTags.findIndex((t) => t.id === id);
+        const nextTag = allTags[(currentTagIndex + 1) % allTags.length];
+        // remove the tag from the groups distribution
+        this.list[fromGroup].distribution = this.list[fromGroup].distribution.filter((dist_tag) => dist_tag.id != id);
         if (!this.isDropdownOpen){
             this.save();
-        }
-    }
-
-    addLineToGroup(id) {
-        const tag_cnt_diff = this.list[id].distribution.length - this.listReadyByGroup(id).length;
-        if (!tag_cnt_diff) {
-            this.list[id].distribution.push(this.newTag(id));
         } else {
-            //all tags are not ready - update the value of tag without analytic account or add
-            const tagToUpdate = this.list[id].distribution.filter((t) => !this.tagIsReady(t) && !t.analytic_account_id)[0];
-            if (!!tagToUpdate) {
-                tagToUpdate.percentage = this.remainderByGroup(id);
-            } else {
-                this.list[id].distribution.push(this.newTag(id));
-            }
+            this.setFocusSelector(`.tag_${nextTag.id} .o_analytic_account_name`);
+            this.autoFill();
         }
     }
 
@@ -296,7 +367,7 @@ export class AnalyticDistribution extends Component {
             colorIndex: dist_tag.color,
             group_id: dist_tag.group_id,
             onClick: (ev) => this.tagClicked(ev, dist_tag.id),
-            onDelete: this.editingRecord ? () => this.deleteTag(dist_tag.id) : undefined
+            onDelete: this.editingRecord ? () => this.deleteTag(dist_tag.id, dist_tag.group_id) : undefined
         }));
     }
 
@@ -310,8 +381,9 @@ export class AnalyticDistribution extends Component {
 
     get firstIncompletePlanId() {
         for (const group_id in this.list) {
-            const group_status = this.groupStatus(group_id);
-            if (["editing", "invalid"].includes(group_status)) return group_id;
+            if (this.groupStatus(group_id) == "invalid") {
+                return group_id;
+            }
         }
         return 0;
     }
@@ -341,8 +413,8 @@ export class AnalyticDistribution extends Component {
     }
 
     get allowSave() {
-        for (const group_id in this.list) {
-            if (['editing', 'invalid'].includes(this.groupStatus(group_id))) return false;
+        if (this.firstIncompletePlanId > 0) {
+            return false;
         }
         return this.props.allow_save;
     }
@@ -364,11 +436,7 @@ export class AnalyticDistribution extends Component {
 
     groupStatus(id) {
         const group = this.list[id];
-        const ready_tags = this.listReadyByGroup(id);
         if (group.applicability === 'mandatory') {
-            if (group.distribution.length > ready_tags.length) {
-                return 'editing'
-            }
             const sum = this.sumByGroup(id);
             if (sum < 99.99 || sum >= 100.01) {
                 return 'invalid';
@@ -405,10 +473,25 @@ export class AnalyticDistribution extends Component {
         }
     }
 
+    /**
+     * This method is typically called when opening the popup and after any change to the distribution.
+     * The remainder, will be placed in the first tag with 0%.
+     * It adds an empty tag allowing users to continue the distribution (replaced 'Add a Line').
+     * Where an empty tag exists, the percentage is updated.
+     */
     autoFill() {
-        for (const group of this.allPlans.filter((p) => p.all_account_count > 0)){
-            if (this.remainderByGroup(group.id)) {
-                this.addLineToGroup(group.id);
+        for (const group of this.allPlans.filter((p) => p.all_account_count > 0)) {
+            // update the first unmodified tag containing 0%
+            const tagToUpdate = this.list[group.id].distribution.find((t) => t.percentage == 0);
+            if (tagToUpdate) {
+                tagToUpdate.percentage = this.remainderByGroup(group.id);
+            }
+            // a tag with no analytic account must always be added / updated
+            const emptyTag = this.list[group.id].distribution.find((t) => !t.analytic_account_id);
+            if (emptyTag) {
+                emptyTag.percentage = this.remainderByGroup(group.id);
+            } else {
+                this.list[group.id].distribution.push(this.newTag(group.id));
             }
         }
     }
@@ -421,8 +504,7 @@ export class AnalyticDistribution extends Component {
 
     async save() {
         const currentDistribution = this.listForJson;
-        const dataToSave = currentDistribution;
-        await this.props.record.update({ [this.props.name]: dataToSave });
+        await this.props.record.update({ [this.props.name]: currentDistribution });
     }
 
     onSaveNew() {
@@ -561,8 +643,11 @@ export class AnalyticDistribution extends Component {
     }
 
     onWindowClick(ev) {
-        if (this.isDropdownOpen && this.dropdownRef.el && !this.dropdownRef.el.contains(ev.target) && !this.widgetRef.el.contains(ev.target)) {
-            this.closeAnalyticEditor();
+        if (this.isDropdownOpen
+            && this.dropdownRef.el && !this.dropdownRef.el.contains(ev.target)
+            && !this.widgetRef.el.contains(ev.target)
+            && !this.selectCreateIsOpen) {
+            this.forceCloseEditor();
         }
     }
 
@@ -581,7 +666,7 @@ export class AnalyticDistribution extends Component {
 }
 AnalyticDistribution.template = "analytic.AnalyticDistribution";
 AnalyticDistribution.components = {
-    AnalyticAutoComplete,
+    AutoComplete,
     TagsList,
 }
 
