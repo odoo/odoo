08eb87a9823bef003318d28ea37f5919e5a2fe6e
Sébastien Theys <seb@odoo.com>
2023-05-22 16:25:51 +0000

    [FIX] mail: fix uncessary render in message (potential initie loop)
    Overwriting array with new array triggers reactive. This leads to
    uncessary renders, and potential infitine loop if both read and insert
    methods are used inside rendering cycle.
    
    Part-of: odoo/odoo#121418

--- addons/mail/static/src/utils/arrays.js
+++ addons/mail/static/src/utils/arrays.js
@@ -1,5 +1,7 @@
 /* @odoo-module */
 
+import { toRaw } from "@odoo/owl";
+
 export function removeFromArray(array, elem) {
     const index = array.indexOf(elem);
     if (index >= 0) {
@@ -14,14 +16,27 @@ export function removeFromArrayWithPredicate(array, predicate) {
     }
 }
 
-export function replaceArrayWithCompare(array1, array2, compareFn) {
+/**
+ * Replaces the content of array1 with the content of array2. Order of elements
+ * is not guaranteed: new elements are inserted last.
+ *
+ * Smart process to avoid triggering reactives when there is no change between
+ * the 2 arrays.
+ */
+export function replaceArrayWithCompare(array1, array2) {
+    array1 = toRaw(array1);
+    array2 = toRaw(array2);
+    const elementsToRemove = new Set();
     for (const el1 of array1) {
-        if (!array2.some((el2) => compareFn(el1, el2))) {
-            removeFromArrayWithPredicate(array1, (el) => compareFn(el, el1));
+        if (!array2.includes(el1)) {
+            elementsToRemove.add(el1);
         }
     }
+    for (const el of elementsToRemove) {
+        removeFromArray(array1, el);
+    }
     for (const el2 of array2) {
-        if (!array1.some((el1) => compareFn(el1, el2))) {
+        if (!array1.includes(el2)) {
             array1.push(el2);
         }
     }
