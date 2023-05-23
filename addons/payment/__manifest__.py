968016f1353c7adea5e0e4c119a95bf9cffea0f0
Antoine Vandevenne (anv) <anv@odoo.com>
2023-05-23 16:46:13 +0200

    [REM] payment: remove unused payment method icons and SCSS rules
    task-2882677
    
    Part-of: odoo/odoo#120446

--- addons/payment/__manifest__.py
+++ addons/payment/__manifest__.py
@@ -30,7 +30,6 @@
     ],
     'assets': {
         'web.assets_frontend': [
-            'payment/static/src/scss/portal_payment.scss',
             'payment/static/src/scss/payment_templates.scss',
             'payment/static/src/scss/payment_form.scss',
             'payment/static/lib/jquery.payment/jquery.payment.js',
