e27ce3ff448b05bbdf0785968a145f8aa952d1b5
Zelong Lin <zel@odoo.com>
2023-05-23 12:21:03 +0000

    [FIX] mail: done activity not updated
    task-3331827
    
    closes odoo/odoo#121907
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

---
+++ addons/mail/static/tests/tours/mail_chatter_activity_test_tour.js
@@ -0,0 +1,36 @@
+/* @odoo-module */
+
+import { registry } from "@web/core/registry";
+
+
+registry.category("web_tour.tours").add("mail_activity_schedule_from_chatter", {
+    test: true,
+    steps: [
+        {
+            trigger: "button:contains('Activities')",
+        },
+        {
+            trigger: "div[name='summary'] input",
+            run: "text Play Mario Party",
+        },
+        {
+            trigger: "button:contains('Schedule')",
+        },
+        {
+            trigger: ".o-mail-Activity:contains('Play Mario Party')",
+        },
+        {
+            trigger: "button:contains('Activities')",
+        },
+        {
+            trigger: "div[name='summary'] input",
+            run: "text Play Mario Kart",
+        },
+        {
+            trigger: "button:contains('Mark as Done')",
+        },
+        {
+            trigger: ".o-mail-Message:contains('Play Mario Kart')",
+        },
+    ],
+});
