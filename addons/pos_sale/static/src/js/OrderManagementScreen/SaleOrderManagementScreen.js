6f20131bb97fe174b19da45e8ceb982995261eff
David Monnom (moda) <moda@odoo.com>
2023-05-23 12:45:13 +0000

    [FIX] pos_sale: fix wrong function argument
    Following the suppression of the use of useListener in this PR
    https://github.com/odoo/odoo/pull/112219. A function adaptation has
    been forgotten.
    
    A correction was made so that now the argument in the declaration of
    the said function is not decomposed.
    
    closes odoo/odoo#122104
    
    Signed-off-by: Guilliams Adrien (adgu) <adgu@odoo.com>

--- addons/pos_sale/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
+++ addons/pos_sale/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
@@ -85,7 +85,7 @@ export class SaleOrderManagementScreen extends ControlButtonsMixin(IndependentTo
     onPrevPage() {
         this.saleOrderFetcher.prevPage();
     }
-    onSearch({ detail: domain }) {
+    onSearch(domain) {
         this.saleOrderFetcher.setSearchDomain(domain);
         this.saleOrderFetcher.setPage(1);
         this.saleOrderFetcher.fetch();
