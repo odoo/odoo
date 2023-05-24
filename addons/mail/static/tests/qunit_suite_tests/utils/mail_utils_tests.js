1ce08410272fad9919986937a55824a95b272427
tsm-odoo <tsm@odoo.com>
2023-05-24 06:18:12 +0000

    [FIX] mail: invalid url regex
    Since [1], comma can be included in url by the discuss regex. This
    should not be the case if the comma is at the end of the url/line
    since it could be part of a sentance.
    
    This PR applies the same logic to the dot and the semicolon character
    since they could also be used as punctuation.
    
    [1] https://github.com/odoo/odoo/pull/122093
    
    X-original-commit: 0dc68dcc94ab9bb2e47943967fa2bc31780d20fa

--- addons/mail/static/tests/qunit_suite_tests/utils/mail_utils_tests.js
+++ addons/mail/static/tests/qunit_suite_tests/utils/mail_utils_tests.js
@@ -140,3 +140,313 @@ QUnit.module("mail", {}, function () {
         );
     });
 });
+<<<<<<< HEAD
+||||||| parent of 551f53e147b (temp)
+
+QUnit.test('addLink: utility function and special entities', function (assert) {
+    assert.expect(8);
+
+    var testInputs = {
+        // textContent not unescaped
+        '<p>https://example.com/?&amp;currency_id</p>':
+        '<p><a target="_blank" rel="noreferrer noopener" href="https://example.com/?&amp;currency_id">https://example.com/?&amp;currency_id</a></p>',
+        // entities not unescaped
+        '&amp; &amp;amp; &gt; &lt;': '&amp; &amp;amp; &gt; &lt;',
+        // > and " not linkified since they are not in URL regex
+        '<p>https://example.com/&gt;</p>':
+        '<p><a target="_blank" rel="noreferrer noopener" href="https://example.com/">https://example.com/</a>&gt;</p>',
+        '<p>https://example.com/"hello"&gt;</p>':
+        '<p><a target="_blank" rel="noreferrer noopener" href="https://example.com/">https://example.com/</a>"hello"&gt;</p>',
+        // & and ' linkified since they are in URL regex
+        '<p>https://example.com/&amp;hello</p>':
+        '<p><a target="_blank" rel="noreferrer noopener" href="https://example.com/&amp;hello">https://example.com/&amp;hello</a></p>',
+        '<p>https://example.com/\'yeah\'</p>':
+        '<p><a target="_blank" rel="noreferrer noopener" href="https://example.com/\'yeah\'">https://example.com/\'yeah\'</a></p>',
+        // normal character should not be escaped
+        ':\'(': ':\'(',
+        // special character in smileys should be escaped
+        '&lt;3': '&lt;3',
+    };
+
+    _.each(testInputs, function (result, content) {
+        var output = utils.parseAndTransform(content, utils.addLink);
+        assert.strictEqual(output, result);
+    });
+});
+
+QUnit.test('addLink: linkify inside text node (1 occurrence)', function (assert) {
+    assert.expect(5);
+
+    const content = '<p>some text https://somelink.com</p>';
+    const linkified = utils.parseAndTransform(content, utils.addLink);
+    assert.ok(
+        linkified.startsWith('<p>some text <a'),
+        "linkified text should start with non-linkified start part, followed by an '<a>' tag"
+    );
+    assert.ok(
+        linkified.endsWith('</a></p>'),
+        "linkified text should end with closing '<a>' tag"
+    );
+
+    // linkify may add some attributes. Since we do not care of their exact
+    // stringified representation, we continue deeper assertion with query
+    // selectors.
+    const fragment = document.createDocumentFragment();
+    const div = document.createElement('div');
+    fragment.appendChild(div);
+    div.innerHTML = linkified;
+    assert.strictEqual(
+        div.textContent,
+        'some text https://somelink.com',
+        "linkified text should have same text content as non-linkified version"
+    );
+    assert.strictEqual(
+        div.querySelectorAll(':scope a').length,
+        1,
+        "linkified text should have an <a> tag"
+    );
+    assert.strictEqual(
+        div.querySelector(':scope a').textContent,
+        'https://somelink.com',
+        "text content of link should be equivalent of its non-linkified version"
+    );
+});
+
+QUnit.test('addLink: linkify inside text node (2 occurrences)', function (assert) {
+    assert.expect(4);
+
+    // linkify may add some attributes. Since we do not care of their exact
+    // stringified representation, we continue deeper assertion with query
+    // selectors.
+    const content = '<p>some text https://somelink.com and again https://somelink2.com ...</p>';
+    const linkified = utils.parseAndTransform(content, utils.addLink);
+    const fragment = document.createDocumentFragment();
+    const div = document.createElement('div');
+    fragment.appendChild(div);
+    div.innerHTML = linkified;
+    assert.strictEqual(
+        div.textContent,
+        'some text https://somelink.com and again https://somelink2.com ...',
+        "linkified text should have same text content as non-linkified version"
+    );
+    assert.strictEqual(
+        div.querySelectorAll(':scope a').length,
+        2,
+        "linkified text should have 2 <a> tags"
+    );
+    assert.strictEqual(
+        div.querySelectorAll(':scope a')[0].textContent,
+        'https://somelink.com',
+        "text content of 1st link should be equivalent to its non-linkified version"
+    );
+    assert.strictEqual(
+        div.querySelectorAll(':scope a')[1].textContent,
+        'https://somelink2.com',
+        "text content of 2nd link should be equivalent to its non-linkified version"
+    );
+});
+
+QUnit.test("url", async (assert) => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["mail.channel"].create({ name: "General" });
+    const { click, insertText, openDiscuss } = await start({
+        discuss: {
+            context: { active_id: channelId },
+        },
+    });
+    await openDiscuss();
+    // see: https://www.ietf.org/rfc/rfc1738.txt
+    const messageBody = "https://odoo.com?test=~^|`{}[]#";
+    await insertText(".o_ComposerTextInput_textarea", messageBody);
+    await click("button:contains(Send)");
+    assert.containsOnce($, `.o_Message a:contains(${messageBody})`);
+})
+
+});
+=======
+
+QUnit.test('addLink: utility function and special entities', function (assert) {
+    assert.expect(8);
+
+    var testInputs = {
+        // textContent not unescaped
+        '<p>https://example.com/?&amp;currency_id</p>':
+        '<p><a target="_blank" rel="noreferrer noopener" href="https://example.com/?&amp;currency_id">https://example.com/?&amp;currency_id</a></p>',
+        // entities not unescaped
+        '&amp; &amp;amp; &gt; &lt;': '&amp; &amp;amp; &gt; &lt;',
+        // > and " not linkified since they are not in URL regex
+        '<p>https://example.com/&gt;</p>':
+        '<p><a target="_blank" rel="noreferrer noopener" href="https://example.com/">https://example.com/</a>&gt;</p>',
+        '<p>https://example.com/"hello"&gt;</p>':
+        '<p><a target="_blank" rel="noreferrer noopener" href="https://example.com/">https://example.com/</a>"hello"&gt;</p>',
+        // & and ' linkified since they are in URL regex
+        '<p>https://example.com/&amp;hello</p>':
+        '<p><a target="_blank" rel="noreferrer noopener" href="https://example.com/&amp;hello">https://example.com/&amp;hello</a></p>',
+        '<p>https://example.com/\'yeah\'</p>':
+        '<p><a target="_blank" rel="noreferrer noopener" href="https://example.com/\'yeah\'">https://example.com/\'yeah\'</a></p>',
+        // normal character should not be escaped
+        ':\'(': ':\'(',
+        // special character in smileys should be escaped
+        '&lt;3': '&lt;3',
+    };
+
+    _.each(testInputs, function (result, content) {
+        var output = utils.parseAndTransform(content, utils.addLink);
+        assert.strictEqual(output, result);
+    });
+});
+
+QUnit.test('addLink: linkify inside text node (1 occurrence)', function (assert) {
+    assert.expect(5);
+
+    const content = '<p>some text https://somelink.com</p>';
+    const linkified = utils.parseAndTransform(content, utils.addLink);
+    assert.ok(
+        linkified.startsWith('<p>some text <a'),
+        "linkified text should start with non-linkified start part, followed by an '<a>' tag"
+    );
+    assert.ok(
+        linkified.endsWith('</a></p>'),
+        "linkified text should end with closing '<a>' tag"
+    );
+
+    // linkify may add some attributes. Since we do not care of their exact
+    // stringified representation, we continue deeper assertion with query
+    // selectors.
+    const fragment = document.createDocumentFragment();
+    const div = document.createElement('div');
+    fragment.appendChild(div);
+    div.innerHTML = linkified;
+    assert.strictEqual(
+        div.textContent,
+        'some text https://somelink.com',
+        "linkified text should have same text content as non-linkified version"
+    );
+    assert.strictEqual(
+        div.querySelectorAll(':scope a').length,
+        1,
+        "linkified text should have an <a> tag"
+    );
+    assert.strictEqual(
+        div.querySelector(':scope a').textContent,
+        'https://somelink.com',
+        "text content of link should be equivalent of its non-linkified version"
+    );
+});
+
+QUnit.test('addLink: linkify inside text node (2 occurrences)', function (assert) {
+    assert.expect(4);
+
+    // linkify may add some attributes. Since we do not care of their exact
+    // stringified representation, we continue deeper assertion with query
+    // selectors.
+    const content = '<p>some text https://somelink.com and again https://somelink2.com ...</p>';
+    const linkified = utils.parseAndTransform(content, utils.addLink);
+    const fragment = document.createDocumentFragment();
+    const div = document.createElement('div');
+    fragment.appendChild(div);
+    div.innerHTML = linkified;
+    assert.strictEqual(
+        div.textContent,
+        'some text https://somelink.com and again https://somelink2.com ...',
+        "linkified text should have same text content as non-linkified version"
+    );
+    assert.strictEqual(
+        div.querySelectorAll(':scope a').length,
+        2,
+        "linkified text should have 2 <a> tags"
+    );
+    assert.strictEqual(
+        div.querySelectorAll(':scope a')[0].textContent,
+        'https://somelink.com',
+        "text content of 1st link should be equivalent to its non-linkified version"
+    );
+    assert.strictEqual(
+        div.querySelectorAll(':scope a')[1].textContent,
+        'https://somelink2.com',
+        "text content of 2nd link should be equivalent to its non-linkified version"
+    );
+});
+
+QUnit.test("url", async (assert) => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["mail.channel"].create({ name: "General" });
+    const { click, insertText, openDiscuss } = await start({
+        discuss: {
+            context: { active_id: channelId },
+        },
+    });
+    await openDiscuss();
+    // see: https://www.ietf.org/rfc/rfc1738.txt
+    const messageBody = "https://odoo.com?test=~^|`{}[]#";
+    await insertText(".o_ComposerTextInput_textarea", messageBody);
+    await click("button:contains(Send)");
+    assert.containsOnce($, `.o_Message a:contains(${messageBody})`);
+});
+
+QUnit.test("url with comma at the end", async (assert) => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["mail.channel"].create({ name: "General" });
+    const { click, insertText, openDiscuss } = await start({
+        discuss: {
+            context: { active_id: channelId },
+        },
+    });
+    await openDiscuss();
+    const messageBody = "Go to https://odoo.com, it's great!";
+    await insertText(".o_ComposerTextInput_textarea", messageBody);
+    await click("button:contains(Send)");
+    assert.containsOnce($, `.o_Message a:contains(https://odoo.com)`);
+    assert.containsOnce($, `.o_Message:contains(${messageBody})`);
+});
+
+QUnit.test("url with dot at the end", async (assert) => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["mail.channel"].create({ name: "General" });
+    const { click, insertText, openDiscuss } = await start({
+        discuss: {
+            context: { active_id: channelId },
+        },
+    });
+    await openDiscuss();
+    const messageBody = "Go to https://odoo.com. It's great!";
+    await insertText(".o_ComposerTextInput_textarea", messageBody);
+    await click("button:contains(Send)");
+    assert.containsOnce($, `.o_Message a:contains(https://odoo.com)`);
+    assert.containsOnce($, `.o_Message:contains(${messageBody})`);
+});
+
+QUnit.test("url with semicolon at the end", async (assert) => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["mail.channel"].create({ name: "General" });
+    const { click, insertText, openDiscuss } = await start({
+        discuss: {
+            context: { active_id: channelId },
+        },
+    });
+    await openDiscuss();
+    const messageBody = "Go to https://odoo.com; it's great!";
+    await insertText(".o_ComposerTextInput_textarea", messageBody);
+    await click("button:contains(Send)");
+    assert.containsOnce($, `.o_Message a:contains(https://odoo.com)`);
+    assert.containsOnce($, `.o_Message:contains(${messageBody})`);
+});
+
+QUnit.test("url with ellipsis at the end", async (assert) => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["mail.channel"].create({ name: "General" });
+    const { click, insertText, openDiscuss } = await start({
+        discuss: {
+            context: { active_id: channelId },
+        },
+    });
+    await openDiscuss();
+    const messageBody = "Go to https://odoo.com... it's great!";
+    await insertText(".o_ComposerTextInput_textarea", messageBody);
+    await click("button:contains(Send)");
+    assert.containsOnce($, `.o_Message a:contains(https://odoo.com)`);
+    assert.containsOnce($, `.o_Message:contains(${messageBody})`);
+});
+
+});
+>>>>>>> 551f53e147b (temp)
