fc02839d574ff7ca53781de51d562225663f848e
Prakash Prajapati <ppr@odoo.com>
2023-05-24 14:13:50 +0000

    [IMP] hr_recruitment: add sequence field in Refuse reasons
    In this commit, We have added the sequence field so that the user can
    easily reorder the Refusal Reason.
    
    task-3336247
    
    closes odoo/odoo#122429
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr_recruitment/models/hr_applicant_refuse_reason.py
+++ addons/hr_recruitment/models/hr_applicant_refuse_reason.py
@@ -7,7 +7,9 @@ from odoo import fields, models
 class ApplicantRefuseReason(models.Model):
     _name = "hr.applicant.refuse.reason"
     _description = 'Refuse Reason of Applicant'
+    _order = 'sequence'
 
+    sequence = fields.Integer(copy=False, default=10)
     name = fields.Char('Description', required=True, translate=True)
     template_id = fields.Many2one('mail.template', string='Email Template', domain="[('model', '=', 'hr.applicant')]")
     active = fields.Boolean('Active', default=True)
