dcec4ad28dac9ba499366bcea774b35839d02109
Kartik Chavda <kcv@odoo.com>
2023-05-24 06:57:48 +0000

    [FIX] project: fix no content helper in project sharing
    Steps:
    - Install project app.
    - Share project editable to user which has no
    task.
    - Open that project in project sharing.
    
    Issue:
    - No content hepler is not properly showing.
    
    Cause:
    - Helper in project sharing action is not markup
    even and that action is type object so we have to
    markup no content manually.
    
    Fix:
    - Markup project sharing helper.
    
    task-3293310
    
    closes odoo/odoo#144516
    
    X-original-commit: 34aa8c46a50ebb0923565594c6c74d15c83756b8
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/static/src/project_sharing/project_sharing.js
+++ addons/project/static/src/project_sharing/project_sharing.js
@@ -5,7 +5,7 @@ import { ActionContainer } from '@web/webclient/actions/action_container';
 import { MainComponentsContainer } from "@web/core/main_components_container";
 import { useOwnDebugContext } from "@web/core/debug/debug_context";
 import { session } from '@web/session';
-import { Component, useEffect, useExternalListener, useState } from "@odoo/owl";
+import { Component, markup, useEffect, useExternalListener, useState } from "@odoo/owl";
 
 export class ProjectSharingWebClient extends Component {
     setup() {
@@ -32,6 +32,9 @@ export class ProjectSharingWebClient extends Component {
 
     async _showView() {
         const { action_name, project_id, open_task_action } = session;
+        if (action_name.help) {
+            action_name.help = markup(action_name.help);
+        }
         await this.actionService.doAction(
             action_name,
             {
