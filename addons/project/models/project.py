2d5945ae0f9e98113ebe7db5b1aad6744bd7b895
Audric Onockx (auon) <auon@odoo.com>
2023-05-24 15:47:50 +0000

    [FIX] project: make _get_subtasks_recursively actually recursive
    Previous commit (https://github.com/odoo/odoo/pull/116570) renamed
    this method _get_subtasks_recursively.
    But instead of calling itself in the return statment, the
    method _get_all_subtasks was called resulting in a non-recursive
    function and to potential undesired behavior as _get_all_subtasks calls
    _get_subtask_ids_per_task_id that calls _get_subtasks_recursively in
    some cases.
    
    With this commit, we just call _get_subtasks_recursively on the children
    to make it actually recursive, like it was in the first place.
    
    closes odoo/odoo#122908
    
    X-original-commit: 7dab2946d8a0e43c22931191260f88c802f2e3fa
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -2474,7 +2474,7 @@ class Task(models.Model):
         children = self.child_ids
         if not children:
             return self.env['project.task']
-        return children + children._get_all_subtasks()
+        return children + children._get_subtasks_recursively()
 
     def action_open_parent_task(self):
         return {
