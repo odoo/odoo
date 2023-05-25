0ee21c664687b8722bd78b988c534430352e3a49
Loan (LSE) <lse@odoo.com>
2023-05-25 16:15:03 +0000

    [FIX] point_of_sale: prevent validating pending electronic payments orders
    Before this commit:
    
     1. Open a POS session configured with any terminal payment method
     2. Click on Payment, pick the terminal payment method
     3. Enter an amount
     4. Click on "Send" to send the amount to the terminal
     5. Click on "Validate" before the terminal returns anything
    =>
    The ticket is validated at $0.
    The actual amount is credited in the terminal but the cash closing does
    not contain this actual amount.
    
    After this commit:
    An error message appear if trying to validate such an order.
    The validate button is also grayed out (but can still be clicked).
    The pot file have also been updated
    
    Note: the issue will also happen in version 14.0, but as I am a bit
    afraid of the side effects that it might have (angry customers used to
    the previous way), I prefer to push it first to the customer version
    only. If I don't have echo of angry customers on this, I will do a
    backward port and validate the forward ports.
    
    opw-3292442
    
    X-original-commit: 5841c417107bba03ef12c995be1743572886c338

--- addons/point_of_sale/static/src/js/Screens/PaymentScreen/PaymentScreen.js
+++ addons/point_of_sale/static/src/js/Screens/PaymentScreen/PaymentScreen.js
@@ -189,7 +189,289 @@ class PaymentScreen extends PosComponent {
                         "The amount of your payment lines must be rounded to validate the transaction."
                     ),
                 });
+<<<<<<< HEAD
                 return;
+||||||| parent of 4b7cd336351 (temp)
+                return false;
+            }
+
+            const splitPayments = this.paymentLines.filter(payment => payment.payment_method.split_transactions)
+            if (splitPayments.length && !this.currentOrder.get_partner()) {
+                const paymentMethod = splitPayments[0].payment_method
+                const { confirmed } = await this.showPopup('ConfirmPopup', {
+                    title: this.env._t('Customer Required'),
+                    body: _.str.sprintf(this.env._t('Customer is required for %s payment method.'), paymentMethod.name),
+                });
+                if (confirmed) {
+                    this.selectPartner();
+                }
+                return false;
+            }
+
+            if ((this.currentOrder.is_to_invoice() || this.currentOrder.is_to_ship()) && !this.currentOrder.get_partner()) {
+                const { confirmed } = await this.showPopup('ConfirmPopup', {
+                    title: this.env._t('Please select the Customer'),
+                    body: this.env._t(
+                        'You need to select the customer before you can invoice or ship an order.'
+                    ),
+                });
+                if (confirmed) {
+                    this.selectPartner();
+                }
+                return false;
+            }
+
+            let partner = this.currentOrder.get_partner()
+            if (this.currentOrder.is_to_ship() && !(partner.name && partner.street && partner.city && partner.country_id)) {
+                this.showPopup('ErrorPopup', {
+                    title: this.env._t('Incorrect address for shipping'),
+                    body: this.env._t('The selected customer needs an address.'),
+                });
+                return false;
+            }
+
+            if (this.currentOrder.get_total_with_tax() != 0 && this.currentOrder.get_paymentlines().length === 0) {
+                this.showNotification(this.env._t('Select a payment method to validate the order.'));
+                return false;
+            }
+
+            if (!this.currentOrder.is_paid() || this.invoicing) {
+                return false;
+            }
+
+            if (this.currentOrder.has_not_valid_rounding()) {
+                var line = this.currentOrder.has_not_valid_rounding();
+                this.showPopup('ErrorPopup', {
+                    title: this.env._t('Incorrect rounding'),
+                    body: this.env._t(
+                        'You have to round your payments lines.' + line.amount + ' is not rounded.'
+                    ),
+                });
+                return false;
+            }
+
+            // The exact amount must be paid if there is no cash payment method defined.
+            if (
+                Math.abs(
+                    this.currentOrder.get_total_with_tax() - this.currentOrder.get_total_paid()  + this.currentOrder.get_rounding_applied()
+                ) > 0.00001
+            ) {
+                var cash = false;
+                for (var i = 0; i < this.env.pos.payment_methods.length; i++) {
+                    cash = cash || this.env.pos.payment_methods[i].is_cash_count;
+                }
+                if (!cash) {
+                    this.showPopup('ErrorPopup', {
+                        title: this.env._t('Cannot return change without a cash payment method'),
+                        body: this.env._t(
+                            'There is no cash payment method available in this point of sale to handle the change.\n\n Please pay the exact amount or add a cash payment method in the point of sale configuration'
+                        ),
+                    });
+                    return false;
+                }
+            }
+
+            // if the change is too large, it's probably an input error, make the user confirm.
+            if (
+                !isForceValidate &&
+                this.currentOrder.get_total_with_tax() > 0 &&
+                this.currentOrder.get_total_with_tax() * 1000 < this.currentOrder.get_total_paid()
+            ) {
+                this.showPopup('ConfirmPopup', {
+                    title: this.env._t('Please Confirm Large Amount'),
+                    body:
+                        this.env._t('Are you sure that the customer wants to  pay') +
+                        ' ' +
+                        this.env.pos.format_currency(this.currentOrder.get_total_paid()) +
+                        ' ' +
+                        this.env._t('for an order of') +
+                        ' ' +
+                        this.env.pos.format_currency(this.currentOrder.get_total_with_tax()) +
+                        ' ' +
+                        this.env._t('? Clicking "Confirm" will validate the payment.'),
+                }).then(({ confirmed }) => {
+                    if (confirmed) this.validateOrder(true);
+                });
+                return false;
+            }
+
+            if (!this.currentOrder._isValidEmptyOrder()) return false;
+
+            return true;
+        }
+        async _postPushOrderResolve(order, order_server_ids) {
+            return true;
+        }
+        async _sendPaymentRequest({ detail: line }) {
+            // Other payment lines can not be reversed anymore
+            this.paymentLines.forEach(function (line) {
+                line.can_be_reversed = false;
+            });
+
+            const payment_terminal = line.payment_method.payment_terminal;
+            line.set_payment_status('waiting');
+
+            const isPaymentSuccessful = await payment_terminal.send_payment_request(line.cid);
+            if (isPaymentSuccessful) {
+                line.set_payment_status('done');
+                line.can_be_reversed = payment_terminal.supports_reversals;
+                // Automatically validate the order when after an electronic payment,
+                // the current order is fully paid and due is zero.
+                if (
+                    this.currentOrder.is_paid() &&
+                    utils.float_is_zero(this.currentOrder.get_due(), this.env.pos.currency.decimal_places)
+                ) {
+                    this.trigger('validate-order');
+                }
+            } else {
+                line.set_payment_status('retry');
+=======
+                return false;
+            }
+
+            if (this.currentOrder.electronic_payment_in_progress()) {
+                this.showPopup('ErrorPopup', {
+                    title: this.env._t('Pending Electronic Payments'),
+                    body: this.env._t(
+                        'There is at least one pending electronic payment.\n' +
+                        'Please finish the payment with the terminal or ' +
+                        'cancel it then remove the payment line.'
+                    ),
+                });
+                return false;
+            }
+
+            const splitPayments = this.paymentLines.filter(payment => payment.payment_method.split_transactions)
+            if (splitPayments.length && !this.currentOrder.get_partner()) {
+                const paymentMethod = splitPayments[0].payment_method
+                const { confirmed } = await this.showPopup('ConfirmPopup', {
+                    title: this.env._t('Customer Required'),
+                    body: _.str.sprintf(this.env._t('Customer is required for %s payment method.'), paymentMethod.name),
+                });
+                if (confirmed) {
+                    this.selectPartner();
+                }
+                return false;
+            }
+
+            if ((this.currentOrder.is_to_invoice() || this.currentOrder.is_to_ship()) && !this.currentOrder.get_partner()) {
+                const { confirmed } = await this.showPopup('ConfirmPopup', {
+                    title: this.env._t('Please select the Customer'),
+                    body: this.env._t(
+                        'You need to select the customer before you can invoice or ship an order.'
+                    ),
+                });
+                if (confirmed) {
+                    this.selectPartner();
+                }
+                return false;
+            }
+
+            let partner = this.currentOrder.get_partner()
+            if (this.currentOrder.is_to_ship() && !(partner.name && partner.street && partner.city && partner.country_id)) {
+                this.showPopup('ErrorPopup', {
+                    title: this.env._t('Incorrect address for shipping'),
+                    body: this.env._t('The selected customer needs an address.'),
+                });
+                return false;
+            }
+
+            if (this.currentOrder.get_total_with_tax() != 0 && this.currentOrder.get_paymentlines().length === 0) {
+                this.showNotification(this.env._t('Select a payment method to validate the order.'));
+                return false;
+            }
+
+            if (!this.currentOrder.is_paid() || this.invoicing) {
+                return false;
+            }
+
+            if (this.currentOrder.has_not_valid_rounding()) {
+                var line = this.currentOrder.has_not_valid_rounding();
+                this.showPopup('ErrorPopup', {
+                    title: this.env._t('Incorrect rounding'),
+                    body: this.env._t(
+                        'You have to round your payments lines.' + line.amount + ' is not rounded.'
+                    ),
+                });
+                return false;
+            }
+
+            // The exact amount must be paid if there is no cash payment method defined.
+            if (
+                Math.abs(
+                    this.currentOrder.get_total_with_tax() - this.currentOrder.get_total_paid()  + this.currentOrder.get_rounding_applied()
+                ) > 0.00001
+            ) {
+                var cash = false;
+                for (var i = 0; i < this.env.pos.payment_methods.length; i++) {
+                    cash = cash || this.env.pos.payment_methods[i].is_cash_count;
+                }
+                if (!cash) {
+                    this.showPopup('ErrorPopup', {
+                        title: this.env._t('Cannot return change without a cash payment method'),
+                        body: this.env._t(
+                            'There is no cash payment method available in this point of sale to handle the change.\n\n Please pay the exact amount or add a cash payment method in the point of sale configuration'
+                        ),
+                    });
+                    return false;
+                }
+            }
+
+            // if the change is too large, it's probably an input error, make the user confirm.
+            if (
+                !isForceValidate &&
+                this.currentOrder.get_total_with_tax() > 0 &&
+                this.currentOrder.get_total_with_tax() * 1000 < this.currentOrder.get_total_paid()
+            ) {
+                this.showPopup('ConfirmPopup', {
+                    title: this.env._t('Please Confirm Large Amount'),
+                    body:
+                        this.env._t('Are you sure that the customer wants to  pay') +
+                        ' ' +
+                        this.env.pos.format_currency(this.currentOrder.get_total_paid()) +
+                        ' ' +
+                        this.env._t('for an order of') +
+                        ' ' +
+                        this.env.pos.format_currency(this.currentOrder.get_total_with_tax()) +
+                        ' ' +
+                        this.env._t('? Clicking "Confirm" will validate the payment.'),
+                }).then(({ confirmed }) => {
+                    if (confirmed) this.validateOrder(true);
+                });
+                return false;
+            }
+
+            if (!this.currentOrder._isValidEmptyOrder()) return false;
+
+            return true;
+        }
+        async _postPushOrderResolve(order, order_server_ids) {
+            return true;
+        }
+        async _sendPaymentRequest({ detail: line }) {
+            // Other payment lines can not be reversed anymore
+            this.paymentLines.forEach(function (line) {
+                line.can_be_reversed = false;
+            });
+
+            const payment_terminal = line.payment_method.payment_terminal;
+            line.set_payment_status('waiting');
+
+            const isPaymentSuccessful = await payment_terminal.send_payment_request(line.cid);
+            if (isPaymentSuccessful) {
+                line.set_payment_status('done');
+                line.can_be_reversed = payment_terminal.supports_reversals;
+                // Automatically validate the order when after an electronic payment,
+                // the current order is fully paid and due is zero.
+                if (
+                    this.currentOrder.is_paid() &&
+                    utils.float_is_zero(this.currentOrder.get_due(), this.env.pos.currency.decimal_places)
+                ) {
+                    this.trigger('validate-order');
+                }
+            } else {
+                line.set_payment_status('retry');
+>>>>>>> 4b7cd336351 (temp)
             }
         }
         if (await this._isOrderValid(isForceValidate)) {
