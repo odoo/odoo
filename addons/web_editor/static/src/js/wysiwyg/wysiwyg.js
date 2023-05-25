8f33bd771a358a8699f49869a023363bf9f59721
Rodolpho Lima <rcdl@odoo.com>
2023-05-25 16:28:53 +0000

    [FIX] web_editor: toolbar position after mouseup
    On mouseup the toolbar size might change (eg. link button removed),
    leading to an ill-positioned toolbar.
    
    This commit makes sure the toolbar position is updated after going
    through changes after a mouseup.
    
    task-3347670
    
    closes odoo/odoo#123599
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1954,6 +1954,8 @@ const Wysiwyg = Widget.extend({
                 buttonGroup.classList.add('d-none');
             }
         }
+        // Toolbar might have changed size, update its position.
+        this.odooEditor.updateToolbarPosition();
         // Update color of already opened colorpickers.
         setTimeout(() => {
             for (let eventName in this.colorpickers) {
