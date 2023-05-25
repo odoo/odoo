aa05fc3cc8307b76f7c8fbc4f61d60adaf4dd39e
Thomas Lefebvre (thle) <thle@odoo.com>
2023-05-25 11:37:31 +0000

    [FIX] website_sale: prevent multiple click on confirm button
    Issue:
    When the product sold has a zero price,
    it is possible to click the "Confirm Order"
    button several times.
    This will send several confirmation emails.
    
    Cause:
    Because the price is zero,
    we do not have a widget that handles
    the `disabled` attribute of the button.
    This is handled by a widget
    when we have the `o_payment_checkout` form,
    but it is not the case when
    selling a zero price product.
    
    Solution:
    Change the attribute of the submit
    button when submitting the form to prevent
    multiple click.
    
    opw-3246935
    
    X-original-commit: 9c82e4594dd72e67fd4a2737ec90ec6cb10ed058

--- addons/website_sale/static/src/js/website_sale.js
+++ addons/website_sale/static/src/js/website_sale.js
@@ -235,6 +235,7 @@ publicWidget.registry.WebsiteSale = publicWidget.Widget.extend(VariantMixin, car
         'mouseup .o_wsale_filmstip_wrapper': '_onMouseUp',
         'mousemove .o_wsale_filmstip_wrapper': '_onMouseMove',
         'click .o_wsale_filmstip_wrapper' : '_onClickHandler',
+        'submit': '_onClickConfirmOrder',
     }),
 
     /**
@@ -952,6 +953,16 @@ publicWidget.registry.WebsiteSale = publicWidget.Widget.extend(VariantMixin, car
      */
     _onClickReviewsLink: function () {
         $('#o_product_page_reviews_content').collapse('show');
+    },
+    /**
+     * Prevent multiclicks on confirm button when the form is submitted
+     *
+     * @private
+     */
+    _onClickConfirmOrder: function () {
+        const submitFormButton = $('form[name="o_wsale_confirm_order"]').find('button[type="submit"]');
+        submitFormButton.attr('disabled', true);
+        setTimeout(() => submitFormButton.attr('disabled', false), 5000);
     },
 
     // -------------------------------------
