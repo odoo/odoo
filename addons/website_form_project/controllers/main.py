b746a694dba0014188914425622bc12291ce725b
damr <damr@odoo.com>
2023-05-26 12:45:51 +0200

    [IMP] project: improve activities and web editor for tasks
    This commit's purpose is :
    - to ensure task created from the web editor are not assigned to the
    odoo bot, for user might mistake those tasks as assigned task
    - to improve the default view open by the systray for task activities
    - to ensure that when a task is created with an email_cc field set, the
    partners linked to this email will be set as follower of the task, in
    order for them to be able to follow the task evolution
    
    task-3251657
    
    closes odoo/odoo#123226
    
    Related: odoo/enterprise#43063
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/website_form_project/controllers/main.py
+++ addons/website_form_project/controllers/main.py
@@ -11,5 +11,7 @@ class WebsiteForm(form.WebsiteForm):
             visitor_partner = visitor_sudo.partner_id
             if visitor_partner:
                 values['partner_id'] = visitor_partner.id
+            # When a task is created from the web editor, if the key 'user_ids' is not present, the user_ids is filled with the odoo bot. We set it to False to ensure it is not.
+            values.setdefault('user_ids', False)
 
         return super().insert_record(request, model, values, custom, meta=meta)
