89b0ce0c74458342fea5e1b093e9383f82e2440f
Antoine Guenet <age@odoo.com>
2023-05-26 11:32:41 +0200

    [FIX] web_editor: handle selection at edges of links
    This replaces the link isolation mechanism with a more robust one that
    doesn't require setting the `contenteditable` attribute on the editable
    to `false` (which is rife with issues). In so doing, we also improve the
    handling of selection at the edges of links, making a clear distinction
    between the selection being inside the link or outside of it.
    Note: we only do this for collapsed selections, as it's not clear what
    should happen when the selection is not collapsed.
    
    task-3103566
    
    Part-of: odoo/odoo#140304

--- addons/web_editor/static/src/js/editor/odoo-editor/test/spec/link.test.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/test/spec/link.test.js
@@ -664,9 +664,14 @@ describe('Link', () => {
                     console.log(a.closest('.odoo-editor-editable').outerHTML);
                     await deleteBackward(editor);
                     console.log(a.closest('.odoo-editor-editable').outerHTML);
-                    window.chai.expect(a.parentElement.isContentEditable).to.be.equal(false);
                 },
-                contentAfterEdit: '<p>a<a href="#/" contenteditable="true" data-oe-zws-empty-inline="">[]\u200B</a>c</p>',
+                contentAfterEdit: '<p>a<a href="#/" data-oe-zws-empty-inline="">' +
+                        '<span data-o-link-zws="start" contenteditable="false">\u200B</span>' + // start zws
+                        '[]\u200B' + // content: empty inline zws
+                        '<span data-o-link-zws="end">\u200B</span>' + // end zws
+                    '</a>' +
+                    '<span data-o-link-zws="after" contenteditable="false">\u200B</span>' + // after zws
+                    'c</p>',
                 contentAfter: '<p>a[]c</p>',
             });
         });
@@ -674,16 +679,11 @@ describe('Link', () => {
             await testEditor(BasicEditor, {
                 contentBefore: '<p>a<a href="#/">b[]</a>c</p>',
                 stepFunction: async editor => {
-                    const a = await clickOnLink(editor);
-                    window.chai.expect(a.parentElement.isContentEditable).to.be.equal(false);
+                    await clickOnLink(editor);
                     await deleteBackward(editor);
-                    window.chai.expect(a.parentElement.isContentEditable).to.be.equal(false);
                     await insertText(editor, '1');
-                    window.chai.expect(a.parentElement.isContentEditable).to.be.equal(false);
                     await insertText(editor, '2');
-                    window.chai.expect(a.parentElement.isContentEditable).to.be.equal(false);
                     await insertText(editor, '3');
-                    window.chai.expect(a.parentElement.isContentEditable).to.be.equal(false);
                 },
                 contentAfter: '<p>a<a href="#/">123[]</a>c</p>',
             });
@@ -692,14 +692,10 @@ describe('Link', () => {
             await testEditor(BasicEditor, {
                 contentBefore: '<p><a href="#/">abc[]abc</a></p>',
                 stepFunction: async editor => {
-                    const a = await clickOnLink(editor);
-                    window.chai.expect(a.parentElement.isContentEditable).to.be.equal(false);
+                    await clickOnLink(editor);
                     await deleteBackward(editor);
-                    window.chai.expect(a.parentElement.isContentEditable).to.be.equal(false);
                     await deleteBackward(editor);
-                    window.chai.expect(a.parentElement.isContentEditable).to.be.equal(false);
                     await deleteBackward(editor);
-                    window.chai.expect(a.parentElement.isContentEditable).to.be.equal(false);
                     await deleteBackward(editor);
                 },
                 contentAfter: '<p><a href="#/">[]abc</a></p>',
