32cad55649c5416c24e3bf69b0c178148c8c64c8
Kartik Chavda <kcv@odoo.com>
2023-05-26 12:30:04 +0000

    [FIX] sale_timesheet: fix activity 'upsell' when threshold is not exceed
    Description of the issue/feature this PR addresses:
    In sale, when threshold value of product is not exceed than upsell activity is
    created while creating invoice. upsell activity is only create when threshold
    value exceed otherwise not.
    
    Current behavior before PR:
    When creating an invoice, an 'upsell' activity is created on the sales order and
    assigned to the salesperson even if the threshold has not been exceeded.
    
    Desired behavior after PR is merged:
    Do not create a 'upsell' activity unless threshold is exceeded.
    
    Fix:
    super class is invoke before checking the upsell and create activity. so
    now, we prevent super method from creating upsell activity via passing
    `mail_activity_automation_skip` context.
    
    task- 3330815
    
    closes odoo/odoo#126015
    
    X-original-commit: 9232093da48cbffe2c3a9d85106292082fdbdcce
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_timesheet/models/sale_order.py
+++ addons/sale_timesheet/models/sale_order.py
@@ -55,9 +55,8 @@ class SaleOrder(models.Model):
             sale_order.timesheet_total_duration = round(total_time)
 
     def _compute_field_value(self, field):
-        super()._compute_field_value(field)
         if field.name != 'invoice_status' or self.env.context.get('mail_activity_automation_skip'):
-            return
+            return super()._compute_field_value(field)
 
         # Get SOs which their state is not equal to upselling or invoied and if at least a SOL has warning prepaid service upsell set to True and the warning has not already been displayed
         upsellable_orders = self.filtered(lambda so:
@@ -65,12 +64,14 @@ class SaleOrder(models.Model):
             and so.invoice_status not in ('upselling', 'invoiced')
             and (so.user_id or so.partner_id.user_id)  # salesperson needed to assign upsell activity
         )
+        super(SaleOrder, upsellable_orders.with_context(mail_activity_automation_skip=True))._compute_field_value(field)
         for order in upsellable_orders:
             upsellable_lines = order._get_prepaid_service_lines_to_upsell()
             if upsellable_lines:
                 order._create_upsell_activity()
                 # We want to display only one time the warning for each SOL
                 upsellable_lines.write({'has_displayed_warning_upsell': True})
+        super(SaleOrder, self - upsellable_orders)._compute_field_value(field)
 
     def _get_prepaid_service_lines_to_upsell(self):
         """ Retrieve all sols which need to display an upsell activity warning in the SO
