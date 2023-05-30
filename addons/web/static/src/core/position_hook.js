14ed5f7337a4863c20c1184eb79100af23666ace
Bruno Boi <boi@odoo.com>
2023-05-30 15:06:22 +0200

    [FIX] web: account for popper margins from style
    **Before this commit**
    Since [1] poppers margins computed from the style are
    not taken into account.
    
    **Additional note**
    The commit [1] has been backported to 16.0 branch
    right in front of this commit.
    
    **After this commit**
    This commit fixes that by taking those margins into account.
    
    [1] b9551a50
    
    X-original-commit: d1664a1a12ce57f2d7fca33174cfe183b6f8db5c

--- addons/web/static/src/core/position_hook.js
+++ addons/web/static/src/core/position_hook.js
@@ -9,13 +9,15 @@ import { localization } from "@web/core/l10n/localization";
  */
 
 /**
- * @typedef {{
- *  popper?: string;
- *  container?: HTMLElement | (() => HTMLElement);
- *  margin?: number;
- *  position?: Direction | Position;
- *  onPositioned?: PositionEventHandler;
- * }} Options
+ * @typedef Options
+ * @property {string} [popper="popper"] useRef reference to the popper element
+ * @property {HTMLElement} [container] container element
+ * @property {number} [margin=0]
+ *  margin in pixels between the popper and the reference.
+ * @property {Direction | Position} [position="bottom"]
+ *  position of the popper relative to the reference
+ * @property {PositionEventHandler} [onPositioned]
+ *  callback called everytime the popper has just been positioned
  *
  * @typedef {keyof DirectionsData} DirectionsDataKey
  * @typedef {{
@@ -77,8 +79,9 @@ const DEFAULTS = {
  * to the requested position.
  * The positioning data used to determine each possible position is based on
  * the reference, popper, and container sizes.
- * Particularly, a popper must not overflow the container in any direction,
- * it should actually stay at `margin` distance from the border to look good.
+ * Particularly, a popper must not overflow the container in any direction.
+ * The popper will stay at `margin` distance from its reference. One could also
+ * use the CSS margins of the popper element to achieve the same result.
  *
  * @param {HTMLElement} reference
  * @param {HTMLElement} popper
@@ -98,6 +101,16 @@ function getBestPosition(reference, popper, { container, margin, position }) {
         container = container();
     }
 
+    // Account for popper actual margins
+    const popperStyle = getComputedStyle(popper);
+    const { marginTop, marginLeft, marginRight, marginBottom } = popperStyle;
+    const popMargins = {
+        top: parseFloat(marginTop),
+        left: parseFloat(marginLeft),
+        right: parseFloat(marginRight),
+        bottom: parseFloat(marginBottom),
+    };
+
     // Boxes
     const popBox = popper.getBoundingClientRect();
     const refBox = reference.getBoundingClientRect();
@@ -108,10 +121,10 @@ function getBestPosition(reference, popper, { container, margin, position }) {
     // Compute positioning data
     /** @type {DirectionsData} */
     const directionsData = {
-        t: refBox.top - popBox.height - margin,
-        b: refBox.bottom + margin,
-        r: refBox.right + margin,
-        l: refBox.left - popBox.width - margin,
+        t: refBox.top - popMargins.bottom - popBox.height - margin,
+        b: refBox.bottom + popMargins.top + margin,
+        r: refBox.right + popMargins.left + margin,
+        l: refBox.left - popMargins.right - popBox.width - margin,
     };
     /** @type {VariantsData} */
     const variantsData = {
@@ -133,8 +146,8 @@ function getBestPosition(reference, popper, { container, margin, position }) {
 
         if (containerRestricted) {
             const [directionSize, variantSize] = vertical
-                ? [popBox.height + margin, popBox.width]
-                : [popBox.width, popBox.height + margin];
+                ? [popBox.height, popBox.width]
+                : [popBox.width, popBox.height];
             let [directionMin, directionMax] = vertical
                 ? [contBox.top, contBox.bottom]
                 : [contBox.left, contBox.right];
@@ -205,9 +218,6 @@ function getBestPosition(reference, popper, { container, margin, position }) {
  * If the requested position does not fit the container, other positions will be
  * tried in different direction and variant flip orders (depending on the requested position).
  * If no position is found that fits the container, the requested position stays used.
- *
- * When the final position is applied, a corresponding CSS class is also added to the popper.
- * This could be used to further styling.
  *
  * @param {HTMLElement} reference
  * @param {HTMLElement} popper
