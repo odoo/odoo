15c1b74f6274a104441a178c842ae4787bb739d7
Thibault Delavallée <tde@odoo.com>
2023-05-31 16:44:30 +0200

    [IMP] mass_mailing: add unsubscribe headers in mailing emails
    PURPOSE
    
    Globally improve usability and features given by mailing portal about exclusion
    list and opt-out management.
    
    SPECIFICATIONS
    
    Add unsubscribe headers to email sent by mass mailings. As emails are already
    parsed to change the generic 'unsubscribe_from_list' link to an email-specific
    link we can add the List-Unsubscribe header at the same time. Add header for
    post 'List-Unsubscribe=One-Click' to avoid one-click unsubscribe when readers
    crawl email links.
    
    Task-2150462 (Mass Mailing: Improve subscription management)
    
    Part-of: odoo/odoo#86084

--- addons/mass_mailing/models/mail_mail.py
+++ addons/mass_mailing/models/mail_mail.py
@@ -63,28 +63,43 @@ class MailMail(models.Model):
         return body
 
     def _prepare_outgoing_list(self, recipients_follower_status=None):
-        """ Update mailing specific links to add tracking based on res_id """
+        """ Update mailing specific links to replace generic unsubscribe and
+        view links by email-specific links. Also add headers to allow
+        unsubscribe from email managers. """
         email_list = super()._prepare_outgoing_list(recipients_follower_status)
         if not self.res_id or not self.mailing_id:
             return email_list
 
         base_url = self.mailing_id.get_base_url()
         for email_values in email_list:
-            if tools.is_html_empty(email_values['body']) or not email_values['email_to']:
+            if not email_values['email_to']:
                 continue
+
             emails = tools.email_split(email_values['email_to'][0])
             email_to = emails[0] if emails else False
+            unsubscribe_url = self.mailing_id._get_unsubscribe_url(email_to, self.res_id)
+            view_url = self.mailing_id._get_view_url(email_to, self.res_id)
 
-            if f'{base_url}/unsubscribe_from_list' in email_values['body']:
-                email_values['body'] = email_values['body'].replace(
-                    f'{base_url}/unsubscribe_from_list',
-                    self.mailing_id._get_unsubscribe_url(email_to, self.res_id),
-                )
-            if f'{base_url}/view' in email_values['body']:
-                email_values['body'] = email_values['body'].replace(
-                    f'{base_url}/view',
-                    self.mailing_id._get_view_url(email_to, self.res_id),
-                )
+            # replace links in body
+            if not tools.is_html_empty(email_values['body']):
+                if f'{base_url}/unsubscribe_from_list' in email_values['body']:
+                    email_values['body'] = email_values['body'].replace(
+                        f'{base_url}/unsubscribe_from_list',
+                        unsubscribe_url,
+                    )
+                if f'{base_url}/view' in email_values['body']:
+                    email_values['body'] = email_values['body'].replace(
+                        f'{base_url}/view',
+                        view_url,
+                    )
+
+            # add headers
+            email_values['headers'].update({
+                'List-Unsubscribe': f'<{unsubscribe_url}>',
+                'List-Unsubscribe-Post': 'List-Unsubscribe=One-Click',
+                'Precedence': 'list',
+                'X-Auto-Response-Suppress': 'OOF',  # avoid out-of-office replies from MS Exchange
+            })
         return email_list
 
     def _postprocess_sent_message(self, success_pids, failure_reason=False, failure_type=None):
