0d5637d147b5af6b69898be078dbaa863c735667
clesgow <quwo@odoo.com>
2023-05-31 11:15:49 +0200

    [FIX] purchase_requisition: copy origins for alternatives
    Backport of ea939f6 for earlier versions.
    When creating an alternative RFQ, set the source document of the
    original RFQ as source document of the newly created alternative.
    
    opw-3293564
    
    X-original-commit: 9fd468f77018dcccaa712abab9252be632725260

--- addons/purchase_requisition/wizard/purchase_requisition_create_alternative.py
+++ addons/purchase_requisition/wizard/purchase_requisition_create_alternative.py
@@ -76,6 +76,7 @@ class PurchaseRequisitionCreateAlternative(models.TransientModel):
             'partner_id': self.partner_id.id,
             'user_id': self.origin_po_id.user_id.id,
             'dest_address_id': self.origin_po_id.dest_address_id.id,
+            'origin': self.origin_po_id.origin,
         }
         if self.copy_products and self.origin_po_id:
             vals['order_line'] = [Command.create(self._get_alternative_line_value(line)) for line in self.origin_po_id.order_line]
