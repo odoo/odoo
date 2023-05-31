8414d6698d7ce6f6cca5f1a44d86094b9bc1b875
tsm-odoo <tsm@odoo.com>
2023-05-31 08:21:56 +0200

    [REF] website_livechat: prettify tour files
    This commit prepares the ground for the refactoring of the
    public livechat by prettifying the files that will be modified
    in order to ease the diff.
    
    part of task-2212347

--- addons/website_livechat/static/tests/tours/website_livechat_rating.js
+++ addons/website_livechat/static/tests/tours/website_livechat_rating.js
@@ -3,34 +3,66 @@
 import commonSteps from "website_livechat.tour_common";
 import { registry } from "@web/core/registry";
 
-registry.category("web_tour.tours").add('website_livechat_complete_flow_tour', {
+registry.category("web_tour.tours").add("website_livechat_complete_flow_tour", {
     test: true,
-    url: '/',
-    steps: [].concat(commonSteps.startStep, commonSteps.endDiscussionStep, commonSteps.okRatingStep, commonSteps.feedbackStep, commonSteps.transcriptStep, commonSteps.closeStep)});
+    url: "/",
+    steps: [].concat(
+        commonSteps.startStep,
+        commonSteps.endDiscussionStep,
+        commonSteps.okRatingStep,
+        commonSteps.feedbackStep,
+        commonSteps.transcriptStep,
+        commonSteps.closeStep
+    ),
+});
 
-registry.category("web_tour.tours").add('website_livechat_happy_rating_tour', {
+registry.category("web_tour.tours").add("website_livechat_happy_rating_tour", {
     test: true,
-    url: '/',
-    steps: [].concat(commonSteps.startStep, commonSteps.endDiscussionStep, commonSteps.goodRatingStep)});
+    url: "/",
+    steps: [].concat(
+        commonSteps.startStep,
+        commonSteps.endDiscussionStep,
+        commonSteps.goodRatingStep
+    ),
+});
 
-registry.category("web_tour.tours").add('website_livechat_ok_rating_tour', {
+registry.category("web_tour.tours").add("website_livechat_ok_rating_tour", {
     test: true,
-    url: '/',
-    steps: [].concat(commonSteps.startStep, commonSteps.endDiscussionStep, commonSteps.okRatingStep, commonSteps.feedbackStep)});
+    url: "/",
+    steps: [].concat(
+        commonSteps.startStep,
+        commonSteps.endDiscussionStep,
+        commonSteps.okRatingStep,
+        commonSteps.feedbackStep
+    ),
+});
 
-registry.category("web_tour.tours").add('website_livechat_sad_rating_tour', {
+registry.category("web_tour.tours").add("website_livechat_sad_rating_tour", {
     test: true,
-    url: '/',
-    steps: [].concat(commonSteps.startStep, commonSteps.endDiscussionStep, commonSteps.sadRatingStep, commonSteps.feedbackStep)});
+    url: "/",
+    steps: [].concat(
+        commonSteps.startStep,
+        commonSteps.endDiscussionStep,
+        commonSteps.sadRatingStep,
+        commonSteps.feedbackStep
+    ),
+});
 
-registry.category("web_tour.tours").add('website_livechat_no_rating_tour', {
+registry.category("web_tour.tours").add("website_livechat_no_rating_tour", {
     test: true,
-    url: '/',
-    steps: [].concat(commonSteps.startStep, commonSteps.endDiscussionStep, commonSteps.transcriptStep, commonSteps.closeStep)});
+    url: "/",
+    steps: [].concat(
+        commonSteps.startStep,
+        commonSteps.endDiscussionStep,
+        commonSteps.transcriptStep,
+        commonSteps.closeStep
+    ),
+});
 
-registry.category("web_tour.tours").add('website_livechat_no_rating_no_close_tour', {
+registry.category("web_tour.tours").add("website_livechat_no_rating_no_close_tour", {
     test: true,
-    url: '/',
-    steps: [].concat(commonSteps.startStep)});
+    url: "/",
+    steps: [].concat(commonSteps.startStep),
+});
 
 export default {};
