32c30a034327a927fcdbcb57f6498accd2ed4cd6
tsm-odoo <tsm@odoo.com>
2023-05-31 06:21:56 +0000

    [REF] website_livechat: prettify tour files
    This commit prepares the ground for the refactoring of the
    public livechat by prettifying the files that will be modified
    in order to ease the diff.
    
    part of task-2212347
    
    closes odoo/odoo#123022
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/website_livechat/static/tests/tours/website_livechat_common.js
+++ addons/website_livechat/static/tests/tours/website_livechat_common.js
@@ -10,7 +10,7 @@ import "@im_livechat/legacy/public_models/livechat_button_view";
 import { Patch } from "@im_livechat/legacy/model";
 
 Patch({
-    name: 'LivechatButtonView',
+    name: "LivechatButtonView",
     recordMethods: {
         /**
          * Alter this method for test purposes.
@@ -31,13 +31,13 @@ Patch({
         async sendMessage(message) {
             await this._super(message);
             if (message.isFeedback) {
-                $('div.o_thread_window_header').addClass('feedback_sent');
+                $("div.o_thread_window_header").addClass("feedback_sent");
             } else {
-                this.messaging.rpc({ route: '/bus/test_mode_activated' }).then(in_test_mode => {
+                this.messaging.rpc({ route: "/bus/test_mode_activated" }).then((in_test_mode) => {
                     if (!in_test_mode) {
                         return;
                     }
-                    this.env.services['bus_service'].forceUpdateChannels();
+                    this.env.services["bus_service"].forceUpdateChannels();
                 });
             }
         },
@@ -45,117 +45,152 @@ Patch({
 });
 
 /*******************************
-*         Common Steps
-*******************************/
+ *         Common Steps
+ *******************************/
 
-var startStep = [{
-    content: "click on livechat widget",
-    trigger: "div.o_livechat_button"
-}, {
-    content: "Say hello!",
-    trigger: "input.o_composer_text_field",
-    run: "text Hello Sir!"
-}, {
-    content: "Send the message",
-    trigger: "input.o_composer_text_field",
-    run: function() {
-        $('input.o_composer_text_field').trigger($.Event('keydown', {which: $.ui.keyCode.ENTER}));
-    }
-}, {
-    content: "Verify your message has been typed",
-    trigger: "div.o_thread_message_content>p:contains('Hello Sir!')"
-}, {
-    content: "Verify there is no duplicates",
-    trigger: "body",
-    run: function () {
-        if ($("div.o_thread_message_content p:contains('Hello Sir!')").length === 1) {
-            $('body').addClass('no_duplicated_message');
-        }
-    }
-}, {
-    content: "Is your message correctly sent ?",
-    trigger: 'body.no_duplicated_message'
-}];
+var startStep = [
+    {
+        content: "click on livechat widget",
+        trigger: "div.o_livechat_button",
+    },
+    {
+        content: "Say hello!",
+        trigger: "input.o_composer_text_field",
+        run: "text Hello Sir!",
+    },
+    {
+        content: "Send the message",
+        trigger: "input.o_composer_text_field",
+        run: function () {
+            $("input.o_composer_text_field").trigger(
+                $.Event("keydown", { which: $.ui.keyCode.ENTER })
+            );
+        },
+    },
+    {
+        content: "Verify your message has been typed",
+        trigger: "div.o_thread_message_content>p:contains('Hello Sir!')",
+    },
+    {
+        content: "Verify there is no duplicates",
+        trigger: "body",
+        run: function () {
+            if ($("div.o_thread_message_content p:contains('Hello Sir!')").length === 1) {
+                $("body").addClass("no_duplicated_message");
+            }
+        },
+    },
+    {
+        content: "Is your message correctly sent ?",
+        trigger: "body.no_duplicated_message",
+    },
+];
 
-var endDiscussionStep = [{
-    content: "Close the chatter",
-    trigger: "a.o_thread_window_close",
-    run: function() {
-        $('a.o_thread_window_close').click();
-    }
-}];
+var endDiscussionStep = [
+    {
+        content: "Close the chatter",
+        trigger: "a.o_thread_window_close",
+        run: function () {
+            $("a.o_thread_window_close").click();
+        },
+    },
+];
 
-var feedbackStep = [{
-    content: "Type a feedback",
-    trigger: "div.o_livechat_rating_reason > textarea",
-    run: "text ;-) This was really helpful. Thanks ;-)!"
-}, {
-    content: "Send the feedback",
-    trigger: "input[type='button'].o_rating_submit_button",
-}, {
-    content: "Check if feedback has been sent",
-    trigger: "div.o_thread_window_header.feedback_sent",
-}, {
-    content: "Thanks for your feedback",
-    trigger: "div.o_livechat_rating_box:has(div:contains('Thank you for your feedback'))",
-}];
+var feedbackStep = [
+    {
+        content: "Type a feedback",
+        trigger: "div.o_livechat_rating_reason > textarea",
+        run: "text ;-) This was really helpful. Thanks ;-)!",
+    },
+    {
+        content: "Send the feedback",
+        trigger: "input[type='button'].o_rating_submit_button",
+    },
+    {
+        content: "Check if feedback has been sent",
+        trigger: "div.o_thread_window_header.feedback_sent",
+    },
+    {
+        content: "Thanks for your feedback",
+        trigger: "div.o_livechat_rating_box:has(div:contains('Thank you for your feedback'))",
+    },
+];
 
-var transcriptStep = [{
-    content: "Type your email",
-    trigger: "input[id='o_email']",
-    run: "text deboul@onner.com"
-}, {
-    content: "Send the conversation to your email address",
-    trigger: "button.o_email_chat_button",
-}, {
-    content: "Type your email",
-    trigger: "div.o_livechat_email:has(strong:contains('Conversation Sent'))",
-}];
+var transcriptStep = [
+    {
+        content: "Type your email",
+        trigger: "input[id='o_email']",
+        run: "text deboul@onner.com",
+    },
+    {
+        content: "Send the conversation to your email address",
+        trigger: "button.o_email_chat_button",
+    },
+    {
+        content: "Type your email",
+        trigger: "div.o_livechat_email:has(strong:contains('Conversation Sent'))",
+    },
+];
 
-var closeStep = [{
-    content: "Close the conversation with the x button",
-    trigger: "a.o_thread_window_close",
-},  {
-    content: "Check that the chat window is closed",
-    trigger: 'body',
-    run: function () {
-        if ($('div.o_livechat_button').length === 1 && !$('div.o_livechat_button').is(':visible')) {
-            $('body').addClass('tour_success');
-        }
-    }
-}, {
-    content: "Is the Test succeded ?",
-    trigger: 'body.tour_success'
-}];
+var closeStep = [
+    {
+        content: "Close the conversation with the x button",
+        trigger: "a.o_thread_window_close",
+    },
+    {
+        content: "Check that the chat window is closed",
+        trigger: "body",
+        run: function () {
+            if (
+                $("div.o_livechat_button").length === 1 &&
+                !$("div.o_livechat_button").is(":visible")
+            ) {
+                $("body").addClass("tour_success");
+            }
+        },
+    },
+    {
+        content: "Is the Test succeded ?",
+        trigger: "body.tour_success",
+    },
+];
 
-var goodRatingStep = [{
-    content: "Send Good Rating",
-    trigger: "div.o_livechat_rating_choices > img[data-value=5]",
-}, {
-    content: "Check if feedback has been sent",
-    trigger: "div.o_thread_window_header.feedback_sent",
-}, {
-    content: "Thanks for your feedback",
-    trigger: "div.o_livechat_rating_box:has(div:contains('Thank you for your feedback'))"
-}];
+var goodRatingStep = [
+    {
+        content: "Send Good Rating",
+        trigger: "div.o_livechat_rating_choices > img[data-value=5]",
+    },
+    {
+        content: "Check if feedback has been sent",
+        trigger: "div.o_thread_window_header.feedback_sent",
+    },
+    {
+        content: "Thanks for your feedback",
+        trigger: "div.o_livechat_rating_box:has(div:contains('Thank you for your feedback'))",
+    },
+];
 
-var okRatingStep = [{
-    content: "Send ok Rating",
-    trigger: "div.o_livechat_rating_choices > img[data-value=3]",
-}];
+var okRatingStep = [
+    {
+        content: "Send ok Rating",
+        trigger: "div.o_livechat_rating_choices > img[data-value=3]",
+    },
+];
 
-var sadRatingStep = [{
-    content: "Send bad Rating",
-    trigger: "div.o_livechat_rating_choices > img[data-value=1]",
-}];
+var sadRatingStep = [
+    {
+        content: "Send bad Rating",
+        trigger: "div.o_livechat_rating_choices > img[data-value=1]",
+    },
+];
 
 export default {
-    'startStep': startStep,
-    'endDiscussionStep': endDiscussionStep,
-    'transcriptStep': transcriptStep,
-    'feedbackStep': feedbackStep,
-    'closeStep': closeStep,
-    'goodRatingStep': goodRatingStep,
-    'okRatingStep': okRatingStep,
-    'sadRatingStep': sadRatingStep,
+    startStep: startStep,
+    endDiscussionStep: endDiscussionStep,
+    transcriptStep: transcriptStep,
+    feedbackStep: feedbackStep,
+    closeStep: closeStep,
+    goodRatingStep: goodRatingStep,
+    okRatingStep: okRatingStep,
+    sadRatingStep: sadRatingStep,
 };
--- addons/website_livechat/static/tests/tours/website_livechat_request.js
+++ addons/website_livechat/static/tests/tours/website_livechat_request.js
@@ -3,42 +3,61 @@
 import commonSteps from "website_livechat.tour_common";
 import { registry } from "@web/core/registry";
 
+var stepWithChatRequestStep = [
+    {
+        content: "Answer the chat request!",
+        trigger: "input.o_composer_text_field",
+        run: "text Hi ! What a coincidence! I need your help indeed.",
+    },
+    {
+        content: "Send the message",
+        trigger: "input.o_composer_text_field",
+        run: function () {
+            $("input.o_composer_text_field").trigger(
+                $.Event("keydown", { which: $.ui.keyCode.ENTER })
+            );
+        },
+    },
+    {
+        content: "Verify your message has been typed",
+        trigger:
+            "div.o_thread_message_content>p:contains('Hi ! What a coincidence! I need your help indeed.')",
+    },
+    {
+        content: "Verify there is no duplicates",
+        trigger: "body",
+        run: function () {
+            if (
+                $(
+                    "div.o_thread_message_content p:contains('Hi ! What a coincidence! I need your help indeed.')"
+                ).length === 1
+            ) {
+                $("body").addClass("no_duplicated_message");
+            }
+        },
+    },
+    {
+        content: "Is your message correctly sent ?",
+        trigger: "body.no_duplicated_message",
+    },
+];
 
-var stepWithChatRequestStep = [{
-    content: "Answer the chat request!",
-    trigger: "input.o_composer_text_field",
-    run: "text Hi ! What a coincidence! I need your help indeed."
-}, {
-    content: "Send the message",
-    trigger: "input.o_composer_text_field",
-    run: function() {
-        $('input.o_composer_text_field').trigger($.Event('keydown', {which: $.ui.keyCode.ENTER}));
-    }
-}, {
-    content: "Verify your message has been typed",
-    trigger: "div.o_thread_message_content>p:contains('Hi ! What a coincidence! I need your help indeed.')"
-}, {
-    content: "Verify there is no duplicates",
-    trigger: "body",
-    run: function () {
-        if ($("div.o_thread_message_content p:contains('Hi ! What a coincidence! I need your help indeed.')").length === 1) {
-            $('body').addClass('no_duplicated_message');
-        }
-    }
-}, {
-    content: "Is your message correctly sent ?",
-    trigger: 'body.no_duplicated_message'
-}];
-
-
-registry.category("web_tour.tours").add('website_livechat_chat_request_part_1_no_close_tour', {
+registry.category("web_tour.tours").add("website_livechat_chat_request_part_1_no_close_tour", {
     test: true,
-    url: '/',
-    steps: [].concat(stepWithChatRequestStep)});
+    url: "/",
+    steps: [].concat(stepWithChatRequestStep),
+});
 
-registry.category("web_tour.tours").add('website_livechat_chat_request_part_2_end_session_tour', {
+registry.category("web_tour.tours").add("website_livechat_chat_request_part_2_end_session_tour", {
     test: true,
-    url: '/',
-    steps: [].concat(commonSteps.endDiscussionStep, commonSteps.okRatingStep, commonSteps.feedbackStep, commonSteps.transcriptStep, commonSteps.closeStep)});
+    url: "/",
+    steps: [].concat(
+        commonSteps.endDiscussionStep,
+        commonSteps.okRatingStep,
+        commonSteps.feedbackStep,
+        commonSteps.transcriptStep,
+        commonSteps.closeStep
+    ),
+});
 
 export default {};
