eabc74b6fdd89622d041865ae6b3bfbabeb13db2
miad-odoo <miad@odoo.com>
2023-06-01 08:40:02 +0000

    [IMP] mail: add new field StatusBarDurationField
    This commit implements a new widget in mail, `StatusBarDurationField`. Purpose:
    seeing how long a record has spent in each stage in a form view.
    
    It is implemented for records with a `many2one` status field and gets the data
    from the `duration_tracking` field of the record. `duration_tracking` is computed
    by adding the `mail.tracking.duration.mixin` to a model.
    
    Time is shown using a luxon Duration object. "narrow" specifies that time is
    expressed with one character and luxon takes care of translations.
    
    Task-3032773
    
    Part-of: odoo/odoo#108554

---
+++ addons/mail/static/src/views/fields/statusbar_duration/statusbar_duration_field.js
@@ -0,0 +1,60 @@
+/** @odoo-module **/
+
+import { _lt } from "@web/core/l10n/translation";
+import { registry } from "@web/core/registry";
+import { formatDuration } from "@web/views/fields/formatters";
+import {
+    preloadStatusBar,
+    statusBarField,
+    StatusBarField,
+} from "@web/views/fields/statusbar/statusbar_field";
+
+export class StatusBarDurationField extends StatusBarField {
+    static template = "mail.StatusBarDurationField";
+
+    computeItems(grouped = true) {
+        const items = super.computeItems(grouped);
+        if (!grouped) {
+            return items;
+        }
+        if (this.props.record.data.duration_tracking) {
+            for (const property in items) {
+                this.setStatusbarDuration(items[property]);
+            }
+        }
+        return items;
+    }
+
+    setStatusbarDuration(stages) {
+        const durationTracking = this.props.record.data.duration_tracking;
+        if (!Object.keys(durationTracking).length) {
+            return;
+        }
+        for (const stage of stages) {
+            if (stage.id in durationTracking && durationTracking[stage.id] > 0) {
+                stage.shortTimeInStage = formatDuration(durationTracking[stage.id], false);
+                stage.fullTimeInStage = formatDuration(durationTracking[stage.id], true);
+            } else {
+                stage.shortTimeInStage = false;
+            }
+        }
+    }
+}
+
+export const statusBarDurationField = {
+    ...statusBarField,
+    component: StatusBarDurationField,
+    displayName: _lt("Status with time"),
+    supportedTypes: ["many2one"],
+    fieldDependencies: [{ name: "duration_tracking", type: "JSON" }],
+};
+
+registry.category("fields").add("statusbar_duration", statusBarDurationField);
+
+registry.category("preloadedData").add("statusbar_duration", {
+    loadOnTypes: ["many2one"],
+    extraMemoizationKey: (record, fieldName) => {
+        return record.data[fieldName];
+    },
+    preload: preloadStatusBar,
+});
