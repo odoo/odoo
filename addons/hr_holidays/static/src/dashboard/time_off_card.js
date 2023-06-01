d6db65f1fdb371b566fa2dca3f2a95498650e647
dasz <dasz@odoo.com>
2023-06-01 14:44:54 +0200

    [FIX] hr_holidays_attendance,hr_holidays: fix overtime hour deduction workflow
    Creating an allocation for extra hour deduction is usually done by the employee himself,
    but the allocation is created in 'to confirm' state, which the employee can't see if he
    is not an HR officer, so it appears as if the extra hours are gone, but the allocation
    is not made. This commit changes it so that the allocation is immediately in 'to confirm'
    state or even in 'validate' if it doesn't require validation.
    - Also fixes a small bug for when the allocation is created and deleted: the extra hours are
      now correctly given back.
    - Added float time widget for the display of hours on time off.
    - Also fixes bug of wrong employee_ids selection when going through extra hours: if you are
      a manager and deduct hours for an employee, employee_ids will take the value of your user
      instead of the employee.
    
    task 3345986

--- addons/hr_holidays/static/src/dashboard/time_off_card.js
+++ addons/hr_holidays/static/src/dashboard/time_off_card.js
@@ -1,19 +1,23 @@
 /* @odoo-module */
 
+import { formatFloatTime } from "@web/views/fields/formatters";
 import { usePopover } from "@web/core/popover/popover_hook";
 
 const { Component } = owl;
 
 export class TimeOffCardPopover extends Component {}
+TimeOffCardPopover.components = { Popover };
 
 TimeOffCardPopover.template = 'hr_holidays.TimeOffCardPopover';
-TimeOffCardPopover.props = ['allocated', 'approved', 'planned', 'left', 'close?'];
+TimeOffCardPopover.props = ['allocated', 'approved', 'planned', 'left'];
 
 export class TimeOffCard extends Component {
     setup() {
         this.popover = usePopover(TimeOffCardPopover, { position: "right", popoverClass: "bg-view" });
     }
-
+    get durationFormatted() {
+        return formatFloatTime(this.duration);
+    }
     onClickInfo(ev) {
         const { data } = this.props;
         this.popover.open(ev.target, {
@@ -25,6 +29,7 @@ export class TimeOffCard extends Component {
     }
 }
 
+TimeOffCard.components = { TimeOffCardPopover };
 TimeOffCard.template = 'hr_holidays.TimeOffCard';
 TimeOffCard.props = ['name', 'id', 'data', 'requires_allocation'];
 
