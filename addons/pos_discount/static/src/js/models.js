0c441737308bf7f47193c64d3e29c895154c3df8
Javier Vega <javier@vauxoo.com>
2023-06-01 00:32:00 +0000

    [IMP] pos_discount: using helper method to retreive applicable lines global discount
    We added a new method to identify if the current pos.order.line can be discounted
    in a global discount, fromt he pos_discount, module.
    
    This in order to be inherited from third ones.
    
    closes odoo/odoo#127171
    
    X-original-commit: 1cf31cfc3d6489f78cb55ece9c611706b7d73189
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

---
+++ addons/pos_discount/static/src/js/models.js
@@ -0,0 +1,16 @@
+/* @odoo-modules */
+
+import { Orderline } from "@point_of_sale/js/models";
+import { patch } from "@web/core/utils/patch";
+
+patch(Orderline.prototype, "pos_discount.Orderline", {
+    /**
+     * Checks if the current line applies for a global discount from `pos_discount.DiscountButton`.
+     * @returns Boolean
+     */
+    isGlobalDiscountApplicable() {
+        return !(
+            this.pos.config.tip_product_id && this.product.id === this.pos.config.tip_product_id[0]
+        );
+    },
+});
