0dde7cc440885788e4f45c12e6e8c6a260f8b0a8
niyasraphy <niyasraphyk@gmail.com>
2023-06-02 06:17:57 +0000

    [IMP] website_profile: add karma_profile_min field to view
    before this commit, currently the field karma_profile_min
    field in website model is not added in any views and
    the default value set for the field is 150, as it is
    not added in any views, the end users have no option to
    change the value of this field.
    
    this field value is used in _check_user_profile_access
    function and thus the non logged in users or logged in
    users with karma less than 150(default value) is not
    able to open any public profiles.
    
    after this commit, end users is not able to change
    the default value set for the field and also default
    value is changed to 0.
    
    closes odoo/odoo#123411
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_profile/__manifest__.py
+++ addons/website_profile/__manifest__.py
@@ -15,6 +15,7 @@
         'data/mail_template_data.xml',
         'views/gamification_badge_views.xml',
         'views/website_profile.xml',
+        'views/website_views.xml',
         'security/ir.model.access.csv',
     ],
     'assets': {
