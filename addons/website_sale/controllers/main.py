3bee61b41dfd3fff5aeb2936cb84eee9a7b4f94e
Victor Feyens <vfe@odoo.com>
2023-06-02 12:47:53 +0200

    [FIX] website_sale: correctly handle no_variant attributes on reorder
    New "Re-order from portal" feature for e-commerce orders was not
    considering specified no_variant & custom information on the order at all.
    
    This commit makes sure the information is correctly forwarded to the cart,
    ensuring the reordered products are the same as the ones previously ordered.
    
    opw-3380312
    
    X-original-commit: 9e774cf88398b7ffc701c4adf44451e39963db08
    Co-authored-by: Florent de Labarre <florent.mirieu@gmail.com>

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -1820,19 +1820,32 @@ class CustomerPortal(sale_portal.CustomerPortal):
                 continue
             if line._is_delivery():
                 continue
+            combination = line.product_id.product_template_attribute_value_ids | line.product_no_variant_attribute_value_ids
             res = {
                 'product_template_id': line.product_id.product_tmpl_id.id,
                 'product_id': line.product_id.id,
+                'combination': combination.ids,
+                'no_variant_attribute_values': [
+                    { # Same input format as provided by product configurator
+                        'value': ptav.id,
+                    } for ptav in line.product_no_variant_attribute_value_ids
+                ],
+                'product_custom_attribute_values': [
+                    { # Same input format as provided by product configurator
+                        'custom_product_template_attribute_value_id': pcav.custom_product_template_attribute_value_id.id,
+                        'custom_value': pcav.custom_value,
+                    } for pcav in line.product_custom_attribute_value_ids
+                ],
                 'type': line.product_id.type,
-                'name': line.product_id.name,
-                'description_sale': line.product_id.description_sale,
+                'name': line.name_short,
+                'description_sale': line.product_id.description_sale or '' + line._get_sale_order_line_multiline_description_variants(),
                 'qty': line.product_uom_qty,
                 'add_to_cart_allowed': line.with_user(request.env.user).sudo()._is_reorder_allowed(),
                 'has_image': bool(line.product_id.image_128),
             }
             if res['add_to_cart_allowed']:
                 res['combinationInfo'] = line.product_id.product_tmpl_id.with_context(**self._sale_reorder_get_line_context())\
-                    ._get_combination_info([], res['product_id'], res['qty'], pricelist)
+                    ._get_combination_info(combination, res['product_id'], res['qty'], pricelist)
             else:
                 res['combinationInfo'] = {}
             result['products'].append(res)
