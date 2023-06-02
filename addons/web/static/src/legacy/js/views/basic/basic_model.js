08f23f903b0c267c49067643def0dc294f81632d
FrancoisGe <fge@odoo.com>
2023-06-02 16:28:53 +0200

    [FIX] web: trigger save and urgentSave at the same time
    Before this commit, in a form view, if you click on the save button and
    reload the page at the same time, two rpc write/create are made to the
    server.
    
    The problem:
    Two creates => there are two records created instead of 1
    Two write   => if we have an x2m field with a new record, this record will
                   be duplicated.
    
    How to reproduce:
    - Go to a form view in edit mode
    - Edit a field
    - Click on Save then reload the page
    
    Before this commit:
        Two rpc writes have been done.
    
    After this commit:
        One rpc write has been done.
    
    Taskid: 3281855
    X-original-commit: 3599c37

--- addons/web/static/src/legacy/js/views/basic/basic_model.js
+++ addons/web/static/src/legacy/js/views/basic/basic_model.js
@@ -1171,6 +1171,12 @@ var BasicModel = AbstractModel.extend({
 
                 // in the case of a write, only perform the RPC if there are changes to save
                 if (method === 'create' || changedFields.length) {
+                    // Prevents multiple create/write when a save and urgentSave on occur at the same time.
+                    // We only want to do a create/write.
+                    if (record.saveInProgress) {
+                        return;
+                    }
+                    record.saveInProgress = true;
                     var args = method === 'write' ? [[record.data.id], changes] : [changes];
                     self._rpc({
                             model: record.model,
@@ -1205,6 +1211,7 @@ var BasicModel = AbstractModel.extend({
 
                             self.unfreezeOrder(record.id);
 
+                            record.saveInProgress = false;
                             // Update the data directly or reload them
                             if (shouldReload) {
                                 self._fetchRecord(record).then(function () {
@@ -1220,7 +1227,10 @@ var BasicModel = AbstractModel.extend({
                                 }
                                 resolve(changedFields);
                             }
-                        }).guardedCatch(reject);
+                        }).guardedCatch((...args) => {
+                            record.saveInProgress = false;
+                            reject(...args)
+                        });
                 } else {
                     resolve(changedFields);
                 }
