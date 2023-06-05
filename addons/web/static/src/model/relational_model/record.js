8b54b08c26f81fa6b685637ed2a4120c3aadeecc
std-odoo <std@odoo.com>
2023-06-05 06:02:01 +0000

    [FIX] web: properties, fix the label requirement check
    Bug
    ===
    The labels of the properties and their names are always required,
    the check was wrongly done and still used the historical name "id"
    instead of "name".
    
    If a property value is missing, the color of the label is marked in red.
    But the datepicker and dropdown are also in read which we don't want
    (only the label).
    
    Task-3188915
    
    closes odoo/odoo#133479
    
    X-original-commit: 5c0720743d299eec305a025d20201103f9f45dc5
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/web/static/src/model/relational_model/record.js
+++ addons/web/static/src/model/relational_model/record.js
@@ -331,6 +331,22 @@ export class Record extends DataPoint {
                     }
                     break;
                 }
+                case "properties": {
+                    const value = this.data[fieldName];
+                    if (value) {
+                        const ok = value.every(
+                            (propertyDefinition) =>
+                                propertyDefinition.name &&
+                                propertyDefinition.name.length &&
+                                propertyDefinition.string &&
+                                propertyDefinition.string.length
+                        );
+                        if (!ok) {
+                            unsetRequiredFields.push(fieldName);
+                        }
+                    }
+                    break;
+                }
                 default:
                     if (!this.data[fieldName] && this._isRequired(fieldName)) {
                         unsetRequiredFields.push(fieldName);
