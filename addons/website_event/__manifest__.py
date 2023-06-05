29a1b46afbdc46c6efab67ad8713a8eddf504cd5
dane@odoo.com <dane@odoo.com>
2023-06-05 20:48:52 +0000

    [IMP] website_event: filters on multi website
    When having a multiple website filters appear on both website.
    event_tag_category determines what the filter should be. The problem is if I assign one event_tag_category
    one website it also appears as filter on other website.
    task-3254058
    
    closes odoo/odoo#118136
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_event/__manifest__.py
+++ addons/website_event/__manifest__.py
@@ -31,6 +31,7 @@
         'views/event_question_views.xml',
         'views/event_registration_answer_views.xml',
         'views/event_tag_category_views.xml',
+        'views/event_tag_views.xml',
         'views/event_type_views.xml',
         'views/website_event_menu_views.xml',
         'views/website_visitor_views.xml',
