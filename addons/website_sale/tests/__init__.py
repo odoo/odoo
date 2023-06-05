dffb0c7016807cb27f90c4ee0c66c7572ca2d304
Victor Feyens <vfe@odoo.com>
2023-06-05 14:44:32 +0000

    [REV] website_sale: stop handling exclusions in /shop
    Commit 757b9b8a33a6d1e4478ef58e898badd39d8b1d2d made sure that excluded
    combination did not appear in /shop search results, but it significantly
    slowed down searches, even in databases without exclusions.
    
    Since the excluded combinations cannot be added to the cart (and the
    original feedback did not come from an effective ticket), we believe
    the gain is not worth the cost.
    
    This commit reverts that change.
    
    Task-3326948
    
    closes odoo/odoo#125722
    
    X-original-commit: 3082ac99dc195a89e1299be73ef149f40231f86e
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/website_sale/tests/__init__.py
+++ addons/website_sale/tests/__init__.py
@@ -22,7 +22,6 @@ from . import test_website_sequence
 from . import test_website_sale_show_compare_list_price
 from . import test_website_sale_visitor
 from . import test_website_sale_product
-from . import test_website_sale_shop_variant_exclusion
 from . import test_website_editor
 from . import test_website_sale_reorder_from_portal
 from . import test_website_sale_snippets
