515d725164dc8cd02a8114cb0efbb4a641fb92a9
Hubert Van de Walle (huvw) <huvw@odoo.com>
2023-06-06 12:12:24 +0000

    [FIX] web: indeterministic test fail
    Apply the same solution used in other tests with "test:hashchange":
    
    ```js
    await testUtils.nextTick();
    await legacyExtraNextTick();
    ```
    
    closes odoo/odoo#124041
    
    X-original-commit: 8bd9809bc4700637ffb26bbcfbd00c62b0dbd220
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Hubert Van De Walle <huvw@odoo.com>

--- addons/web/static/tests/webclient/actions/window_action_tests.js
+++ addons/web/static/tests/webclient/actions/window_action_tests.js
@@ -571,6 +571,7 @@ QUnit.module("ActionManager", (hooks) => {
             action: 3,
             view_type: "form",
         });
+        await testUtils.nextTick();
         await legacyExtraNextTick();
 
         // Go back to the list view
@@ -579,7 +580,7 @@ QUnit.module("ActionManager", (hooks) => {
             action: 3,
             view_type: "list",
         });
-        await legacyExtraNextTick();
+        await testUtils.nextTick();
         await legacyExtraNextTick();
         assert.containsOnce(target, ".o_list_view", "should still display the list view");
 
