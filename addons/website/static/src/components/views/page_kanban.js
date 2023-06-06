9237ca777fb9c5e9bda03c2d0dbe0deed8e1b2e5
Romeo Fragomeli <rfr@odoo.com>
2023-06-06 09:23:43 +0000

    [FIX] web,website: website filter doesn't appear anymore
    Since commit [1], the custom filter for website was disabled.
    This commit restores the filter and adapts it for "Milk" redesign.
    
    Steps to reproduce:
    * Open the Website app
    * Select the menu "Site" -> "Pages"
    * Open the "Dropdown" in the SearchBar
      => Bug there is no filter for website
    
    [1]: odoo/odoo@caef16ee4e5351142ef04f02798999ccb38a56bc
    
    closes odoo/odoo#123886
    
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- addons/website/static/src/components/views/page_kanban.js
+++ addons/website/static/src/components/views/page_kanban.js
@@ -3,6 +3,7 @@
 import {PageControllerMixin, PageRendererMixin} from "./page_views_mixin";
 import {registry} from '@web/core/registry';
 import {kanbanView} from "@web/views/kanban/kanban_view";
+import {SearchDropdownItem} from "@web/search/search_dropdown_item/search_dropdown_item";
 
 
 export class PageKanbanController extends PageControllerMixin(kanbanView.Controller) {
@@ -14,6 +15,10 @@ export class PageKanbanController extends PageControllerMixin(kanbanView.Control
     }
 }
 PageKanbanController.template = 'website.PageKanbanView';
+PageKanbanController.components = {
+    ...kanbanView.Controller.components,
+    SearchDropdownItem,
+};
 
 export class PageKanbanRenderer extends PageRendererMixin(kanbanView.Renderer) {}
 PageKanbanRenderer.props = [
