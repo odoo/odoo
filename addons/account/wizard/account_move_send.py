cf7ae6b9d562622709e2730c1ea8c43816c169bf
Thibault Delavallée <tde@odoo.com>
2023-06-06 09:07:57 +0200

    [FIX] account, mail: defensively evaluate 'partner_to' on mail templates
    MailTemplate model has a 'partner_to' field is dynamically rendered to contain
    partners being recipients. After rendering it should hold a comma-separated
    list of partner IDs.
    
    However as we are unsure it was correctly written better be defensive. We now
    check that we can effectively transform items to IDs using 'isdigit()'.
    
    Task-2612945 (Mail: Defensive email formatting)
    
    X-original-commit: odoo/odoo@f96834b402214a8f51900d26c5845703b81c7501
    Part-of: odoo/odoo#134374

--- addons/account/wizard/account_move_send.py
+++ addons/account/wizard/account_move_send.py
@@ -121,7 +121,7 @@ class AccountMoveSend(models.Model):
                 partners |= partners.find_or_create(mail_data)
         if mail_template.partner_to:
             partner_to = self._get_mail_default_field_value_from_template(mail_template, lang, move, 'partner_to')
-            partner_ids = [int(pid) for pid in partner_to.split(',') if pid]
+            partner_ids = mail_template._parse_partner_to(partner_to)
             partners |= self.env['res.partner'].sudo().browse(partner_ids).exists()
         return partners
 
