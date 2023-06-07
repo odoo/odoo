0b891e7c5295cddf885a9dc39233b319bf654639
Thibault Delavallée <tde@odoo.com>
2023-06-07 18:36:27 +0200

    [REF] mail: remove 'field_{desc/type}' from tracking value
    PURPOSE
    
    Simplify 'mail.tracking.value' model and code. Remove unnecessary fields and
    computation. Make code easier to handle and more batch-enabled.
    
    SPECIFICATIONS
    
    Remove 'field_desc' and 'field_type' from 'mail.tracking.value' model. Those
    can be retrieved when necessary, as it is mainly used for frontend display
    in Chatter.
    
    Task-3345979 (Mail: Simplify tracking model)
    
    Part-of: odoo/odoo#124182

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -4404,14 +4404,14 @@ class MailThread(models.AbstractModel):
             tracking_message = return_line + message.subtype_id.description + return_line
 
         for value in message.sudo().tracking_value_ids.filtered(lambda tracking: not tracking.field_groups):
-            if value.field_type == 'boolean':
+            if value.field.ttype == 'boolean':
                 old_value = str(bool(value.old_value_integer))
                 new_value = str(bool(value.new_value_integer))
             else:
                 old_value = value.old_value_char if value.old_value_char else str(value.old_value_integer)
                 new_value = value.new_value_char if value.new_value_char else str(value.new_value_integer)
 
-            tracking_message += value.field_desc + ': ' + old_value
+            tracking_message += value.field.field_description + ': ' + old_value
             if old_value != new_value:
                 tracking_message += ' → ' + new_value
             tracking_message += return_line
