028089b0f09d47ff0bc5c9f95018c62e77a2bf16
Romain Derie <rde@odoo.com>
2023-06-07 18:58:14 +0200

    [FIX] web_editor, website: correctly invalidate cache on translation
    Before this commit, when writing translation on a record from the
    website builder, the cache would not be invalidated enough as it was
    only invalidated the given record on which you wrote the translation.
    
    This is not enough, as other model records cached value might depend of
    this record.
    
    It's the case for `ir.ui.view` which depends of the `website.menu`
    values.
    Invalidating only the `website.menu` is thus not enough as it would
    leave outdated and wrong values in the `ir.ui.view` arch cache which is
    ultimately used to render a website page.
    
    Step to reproduce:
    - Create a mega menu, select any template, `Odoo Menu` for the example
    - Install another language on the website
    - Go to the translated version of your website and enter translate mode
    - Change "Camera" in the mega menu to something else
    - Save
    
    The change won't be replicated, looking like it did nothing.
    From there, removing or adding `edit_translations=1` in the URL will
    use different cache version of the page's views and you will see the
    outdated value on one and the correct on the other one.
    
    Note that for regular menus (not mega menu ones) and other fields, it
    most of the time goes through the `save` method of `ir.ui.view` which
    ends up calling `save_embedded_field` which does a `write` and from
    there the models are usually doing the necessary cache invalidation.
    For instance, the views will clear the cache on `write`, same as for the
    `website.menu`.
    
    opw-3305117

--- addons/web_editor/models/models.py
+++ addons/web_editor/models/models.py
@@ -25,4 +25,15 @@ class BaseModel(models.AbstractModel):
             for translation in translations.values():
                 for key, value in translation.items():
                     translation[key] = field.translate.term_converter(value)
-        return self._update_field_translations(fname, translations, lambda old_term: sha256(old_term.encode()).hexdigest())
+        res = self._update_field_translations(fname, translations, lambda old_term: sha256(old_term.encode()).hexdigest())
+        # The `_update_field_translations` is in charge of updating the cache
+        # of the given field of the given record.
+        # But this is not enough as the record might actually be used in another
+        # model cached value, for instance updating the `website.menu` cache of
+        # `mega_menu_content` won't be enough as it's part of the `ir.ui.view`
+        # Qweb cache of compiled views, which stores the compiled value of the
+        # `website.submenu` template which is looping on `website.menu`.
+        # It would means that even if the cache of the `website.menu` was
+        # correctly invalidated, records depending of it won't be.
+        self.clear_caches()
+        return res
