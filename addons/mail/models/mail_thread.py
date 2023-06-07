201ffa7ed9a7ef923b44e49df70d2bd12b18f6c7
Thibault Delavallée <tde@odoo.com>
2023-06-07 18:19:50 +0200

    [REF] mail: cleanup 'mail.tracking.value' model code
    PURPOSE
    
    Simplify 'mail.tracking.value' model and code. Remove unnecessary fields and
    computation. Make code easier to handle and more batch-enabled.
    
    SPECIFICATIONS
    
    SPEC 1: prepare all tracking values at same code place
    
    Delegate all computation of tracking values into the 'create_tracking_values'
    method. Curently part of it (currency field) is done in the caller. Better
    split code per feature.
    
    Method is also made private, as it is not required to expose it.
    
    SPEC 2: cleanup tracking value formatting methods and calls
    
    Code used to display tracking value can be simplified: remove unnecessary
    wrappers, make code easier to read, avoid composition of field name but
    use a mapping instead (easier to grep 'old_value_char' when it is effectively
    used).
    
    Ensure code always calls '_tracking_value_format' to ease future improvements
    and have all formatting code being batch-enabled.
    
    SPEC 3: simplify Chatter formatted value structure
    
    'fieldType' is currently added in old and new values. As it is a field
    property it can be moved higher in the formatting result to be included
    only once. Also add 'fieldName', the column name, to the formatted results
    as it will soon help various tool methods. Moreover it makes sense to have
    the source of the tracking as the real column name, in addition to its
    string and type.
    
    Task-3345979 (Mail: Simplify tracking model)

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -3333,15 +3333,22 @@ class MailThread(models.AbstractModel):
             )
         record_name = msg_vals.get('record_name') if 'record_name' in msg_vals else message.record_name
 
-        # tracking
+        # tracking: in case of missing value, perform search (skip only if sure we don't have any)
+        check_tracking = msg_vals.get('tracking_value_ids', True) if msg_vals else bool(self)
         tracking = []
-        if msg_vals.get('tracking_value_ids', True) if msg_vals else bool(self): # could be tracking
-            for tracking_value in self.env['mail.tracking.value'].sudo().search([('mail_message_id', '=', message.id)]):
-                groups = tracking_value.field_groups
-                if not groups or self.env.is_superuser() or self.user_has_groups(groups):
-                    tracking.append((tracking_value.field_desc,
-                                     tracking_value._get_old_display_value()[0],
-                                     tracking_value._get_new_display_value()[0]))
+        if check_tracking:
+            tracking_values = self.env['mail.tracking.value'].sudo().search(
+                [('mail_message_id', '=', message.id)]
+            ).filtered(
+                lambda track: not track.field_groups or self.env.is_superuser() or self.user_has_groups(track.field_groups)
+            )
+            tracking = [
+                (
+                    fmt_vals['changedField'],
+                    fmt_vals['oldValue']['value'],
+                    fmt_vals['newValue']['value'],
+                ) for fmt_vals in tracking_values._tracking_value_format()
+            ]
 
         subtype_id = msg_vals.get('subtype_id') if msg_vals and 'subtype_id' in msg_vals else message.subtype_id.id
         is_discussion = subtype_id == self.env['ir.model.data']._xmlid_to_res_id('mail.mt_comment')
