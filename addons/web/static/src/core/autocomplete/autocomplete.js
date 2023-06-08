ebd46c3272ce8ed5139251556600c58a79b5d61a
Michael (mcm) <mcm@odoo.com>
2023-06-08 09:24:15 +0000

    [FIX] web: prevent autocomplete crash on selection
    Before this commit, it could happen that the autocomplete does not
    find the option to select. Now, we give the option to select so
    we're sure that the option exists when we select it.
    
    runbot errors: 5759, 102534, 111418
    
    closes odoo/odoo#194935
    
    X-original-commit: 3f597d9d178db10047609c6501bdbdd213fa6a6a
    Signed-off-by: Julien Carion (juca) <juca@odoo.com>
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>

--- addons/web/static/src/core/autocomplete/autocomplete.js
+++ addons/web/static/src/core/autocomplete/autocomplete.js
@@ -152,6 +152,11 @@ export class AutoComplete extends Component {
         return false;
     }
 
+    get activeOption() {
+        const [sourceIndex, optionIndex] = this.state.activeSourceOption;
+        return this.sources[sourceIndex].options[optionIndex];
+    }
+
     open(useInput = false) {
         this.state.open = true;
         return this.loadSources(useInput);
@@ -232,8 +237,8 @@ export class AutoComplete extends Component {
             this.state.activeSourceOption[1] === optionIndex
         );
     }
-    selectOption(indices, params = {}) {
-        const option = this.sources[indices[0]].options[indices[1]];
+
+    selectOption(option, params = {}) {
         this.inEdition = false;
         if (option.unselectable) {
             this.inputRef.el.value = "";
@@ -376,7 +381,7 @@ export class AutoComplete extends Component {
                 if (!this.isOpened || !this.state.activeSourceOption) {
                     return;
                 }
-                this.selectOption(this.state.activeSourceOption);
+                this.selectOption(this.activeOption);
                 break;
             case "escape":
                 if (!this.isOpened) {
@@ -393,7 +398,7 @@ export class AutoComplete extends Component {
                     this.state.activeSourceOption &&
                     (this.state.navigationRev > 0 || this.inputRef.el.value.length > 0)
                 ) {
-                    this.selectOption(this.state.activeSourceOption);
+                    this.selectOption(this.activeOption);
                 }
                 this.close();
                 return;
@@ -423,8 +428,8 @@ export class AutoComplete extends Component {
     onOptionMouseLeave() {
         this.state.activeSourceOption = null;
     }
-    onOptionClick(indices) {
-        this.selectOption(indices);
+    onOptionClick(option) {
+        this.selectOption(option);
         this.inputRef.el.focus();
     }
 
