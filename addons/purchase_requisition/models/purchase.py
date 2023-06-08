ad59eb0d78f97832375baac33b7ad5b86cb35501
Paweł Fertyk <pafe@odoo.com>
2023-06-08 10:24:44 +0200

    [IMP] purchase_requisition: improve "Alternative Warning" popup
    Changes applied also to "Alternatives" tab in RfQ.
    
    Task: 3290154

--- addons/purchase_requisition/models/purchase.py
+++ addons/purchase_requisition/models/purchase.py
@@ -113,7 +113,7 @@ class PurchaseOrder(models.Model):
             if alternative_po_ids:
                 view = self.env.ref('purchase_requisition.purchase_requisition_alternative_warning_form')
                 return {
-                    'name': _('Alternative Warning'),
+                    'name': _('Remaining Open Alternatives'),
                     'type': 'ir.actions.act_window',
                     'view_mode': 'form',
                     'res_model': 'purchase.requisition.alternative.warning',
