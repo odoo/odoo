037fabd81efc28c12359700ee8e21e0454e0535e
Brice bib Bartoletti <bib@odoo.com>
2023-06-08 16:27:57 +0000

    [IMP] l10n_{be/dk}: compute CVR when VAT is DK
    The aim of this commit is to:
    - ease the company configuration in DK
    - make the feature work when the country_code prefix isn't present
    
    Context:
    The DK company registry is the CVR. The CVR is quite the same than the
    VAT number.
    Re-applied logic used in `l10n_be` to ease the configuration.
    
    Before the commit:
    The company registry has to be inputed.
    
    After the commit:
    The company registry don't need to inputed while a DK VAT is setup for a
    DK company.
    
    closes odoo/odoo#125058
    
    Task-id: 3334595
    Related: odoo/enterprise#42561
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_be/models/res_partner.py
+++ addons/l10n_be/models/res_partner.py
@@ -12,9 +12,12 @@ class ResPartner(models.Model):
     @api.depends('vat', 'country_id')
     def _compute_company_registry(self):
         # OVERRIDE
-        # If a belgian company has a VAT number then it's company registry is it's VAT Number (without country code).
+        # If a belgian company has a VAT number then its company registry is its VAT Number (without country code).
         super()._compute_company_registry()
         for partner in self.filtered(lambda p: p.country_id.code == 'BE' and p.vat):
             vat_country, vat_number = self._split_vat(partner.vat)
+            if vat_country.isnumeric():
+                vat_country = 'be'
+                vat_number = partner.vat
             if vat_country == 'be' and self.simple_vat_check(vat_country, vat_number):
                 partner.company_registry = vat_number
