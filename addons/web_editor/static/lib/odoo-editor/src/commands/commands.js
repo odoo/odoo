aa44c083c1c74c4ccd31d31238465b7a60328f01
Odoo <maad@odoo.com>
2023-06-09 10:18:26 +0530

    [FIX] website: strange padding when background color is reset
    Before this commit:
    
    On applying background or on remove background color, strange padding
    is added on the text.
    
    After this commit:
    
    Now, padding is not added when background is colored is applied or background
    color is removed.
    
    task-3237693
    
    closes odoo/odoo#124394
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/lib/odoo-editor/src/commands/commands.js
+++ addons/web_editor/static/lib/odoo-editor/src/commands/commands.js
@@ -45,8 +45,8 @@ import {
     getCursorDirection,
 } from '../utils/utils.js';
 
-const TEXT_CLASSES_REGEX = /\btext-[^\s]*\b/g;
-const BG_CLASSES_REGEX = /\bbg-[^\s]*\b/g;
+const TEXT_CLASSES_REGEX = /\btext-[^\s]*\b/;
+const BG_CLASSES_REGEX = /\bbg-[^\s]*\b/;
 
 function insert(editor, data, isText = true) {
     if (!data) {
@@ -633,7 +633,7 @@ export const editorCommands = {
         const fontsSet = new Set(fonts);
         for (const font of fontsSet) {
             colorElement(font, color, mode);
-            if (!hasColor(font, mode) && !font.hasAttribute('style')) {
+            if ((!hasColor(font, 'color') && !hasColor(font,'backgroundColor')) && (!font.hasAttribute('style') || !color)) {
                 for (const child of [...font.childNodes]) {
                     font.parentNode.insertBefore(child, font);
                 }
