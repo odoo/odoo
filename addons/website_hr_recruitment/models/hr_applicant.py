f4cb4d17d92df94a50f97be89a867f044085aa42
Thomas Lefebvre (thle) <thle@odoo.com>
2023-06-09 06:39:46 +0000

    [FIX] website_hr_recruitment: prevent applying for unpublished job
    Issue:
    ------
    When we have the link to the form to apply for a job,
    even if the job is not published, we can still apply.
    
    Solution:
    ---------
    Check that the job is published before the creation of the record
    in the `website_form_input_filter`.
    
    opw-3331717
    
    X-original-commit: 72072263ae01a8bea01c7fead000159ae9d060d2

--- addons/website_hr_recruitment/models/hr_applicant.py
+++ addons/website_hr_recruitment/models/hr_applicant.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models, _
+from odoo.exceptions import UserError
 
 
 class Applicant(models.Model):
@@ -14,6 +15,9 @@ class Applicant(models.Model):
             name = '%s - %s' % (values['partner_name'], applicant_job) if applicant_job else _("%s's Application", values['partner_name'])
             values.setdefault('name', name)
         if values.get('job_id'):
+            job = self.env['hr.job'].browse(values.get('job_id'))
+            if not job.sudo().website_published:
+                raise UserError(_("You cannot apply for this job."))
             stage = self.env['hr.recruitment.stage'].sudo().search([
                 ('fold', '=', False),
                 '|', ('job_ids', '=', False), ('job_ids', '=', values['job_id']),
