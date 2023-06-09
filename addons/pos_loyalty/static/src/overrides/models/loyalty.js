30e5443053960f887acd32c6908aa647cbd913ba
roen-odoo <roen@odoo.com>
2023-06-09 12:58:31 +0000

    [FIX] pos_loyalty:reward should not affect point rewarded
    Current behavior:
    When creating a loyalty program, with a rules that award points for each
    dollar spent, the value of the point was inconsistent because it was
    taking the rewards into account.
    
    Steps to reproduce:
    - Create a loyalty program with a rule that award 0.1 point for each
      dollar spent, and a reward that gives 1$ per points.
    - Create a product with a price of 265$.
    - Open the PoS and create a new order for 1000$.
    - Select a customer and pay the order.(The customer now have 100 points)
    - Create a new order with the product created earlier, and apply the
      reward.
    - The value of the reward shoudl be 126.5$ (100 points + 26.5 points
      from the current order) but it is not.
    
    opw-3275585
    
    X-original-commit: 70256e5cf8359a09687acc3e2d4f8792ef4f9801

--- addons/pos_loyalty/static/src/overrides/models/loyalty.js
+++ addons/pos_loyalty/static/src/overrides/models/loyalty.js
@@ -803,7 +803,9 @@ patch(Order.prototype, "pos_loyalty.Order", {
                             qtyPerProduct[line.reward_product_id || line.get_product().id] =
                                 lineQty;
                         }
-                        orderedProductPaid += line.get_price_with_tax();
+                        if (!line.is_reward_line) {
+                            orderedProductPaid += line.get_price_with_tax();
+                        }
                     }
                 }
                 if (totalProductQty < rule.minimum_qty) {
