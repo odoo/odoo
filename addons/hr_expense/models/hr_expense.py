0a7d8452c937b24aa556eb8e206bd0c838d6ad5e
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2023-06-12 10:46:52 +0000

    [FIX] hr_expense: amount due display currency
    1. Create Expense in a different currency than the company's base currency
    2. Create a report
    3. Post journal entry
    
    Issue: Amount due field on the report shows the wrong amount (expressed in
       foreign currency).
    
    opw-3344200
    
    closes odoo/odoo#127648
    
    X-original-commit: 22f3145ec8bbdda039fcdce43986b03a870255be
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Andrea Grazioso (agr) <agr@odoo.com>

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -958,7 +958,7 @@ class HrExpenseSheet(models.Model):
     amount_residual = fields.Monetary(
         string="Amount Due", store=True,
         currency_field='currency_id',
-        related='account_move_id.amount_residual')
+        compute='_compute_amount_residual')
     company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True, states={'draft': [('readonly', False)]}, default=lambda self: self.env.company)
     currency_id = fields.Many2one('res.currency', string='Currency', readonly=True, states={'draft': [('readonly', False)]}, default=lambda self: self.env.company.currency_id)
     attachment_number = fields.Integer(compute='_compute_attachment_number', string='Number of Attachments')
@@ -984,6 +984,13 @@ class HrExpenseSheet(models.Model):
             sheet.total_amount = sum(sheet.expense_line_ids.mapped('total_amount_company'))
             sheet.total_amount_taxes = sum(sheet.expense_line_ids.mapped('amount_tax_company'))
             sheet.untaxed_amount = sheet.total_amount - sheet.total_amount_taxes
+
+    @api.depends("account_move_id.line_ids")
+    def _compute_amount_residual(self):
+        for sheet in self:
+            payment_term_lines = sheet.account_move_id.sudo().line_ids \
+                .filtered(lambda line: line.expense_id in sheet.expense_line_ids and line.account_internal_type in ('receivable', 'payable'))
+            sheet.amount_residual = -sum(payment_term_lines.mapped('amount_residual'))
 
     @api.depends('account_move_id.payment_state')
     def _compute_payment_state(self):
