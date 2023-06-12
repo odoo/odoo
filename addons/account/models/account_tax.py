0ab1a7d4388b04a1fe0cd03c575ae3e07c79f154
qdp-odoo <qdp@odoo.com>
2023-06-12 15:33:28 +0000

    [IMP] account: prevent the deletion of tag when set on tax/account
    Because if a tag is still linked to a tax or an account, then it's probably also still referenced in a report line, and it makes no sense allowing to delete such a tag.
    
    X-original-commit: 3e72f87662c5061e9459c032f98b80233d7ddd6b

--- addons/account/models/account_tax.py
+++ addons/account/models/account_tax.py
@@ -1328,7 +1328,7 @@ class AccountTaxRepartitionLine(models.Model):
         domain="[('deprecated', '=', False), ('company_id', '=', company_id), ('account_type', 'not in', ('asset_receivable', 'liability_payable'))]",
         check_company=True,
         help="Account on which to post the tax amount")
-    tag_ids = fields.Many2many(string="Tax Grids", comodel_name='account.account.tag', domain=[('applicability', '=', 'taxes')], copy=True)
+    tag_ids = fields.Many2many(string="Tax Grids", comodel_name='account.account.tag', domain=[('applicability', '=', 'taxes')], copy=True, ondelete='restrict')
     tax_id = fields.Many2one(comodel_name='account.tax', ondelete='cascade', check_company=True)
     company_id = fields.Many2one(string="Company", comodel_name='res.company', related="tax_id.company_id", store=True, help="The company this distribution line belongs to.")
     sequence = fields.Integer(string="Sequence", default=1,
