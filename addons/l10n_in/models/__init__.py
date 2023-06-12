6ee9842764e0b1244927cb8ece1f6407ccec20d5
hupo-odoo <hupo@odoo.com>
2023-06-12 13:41:59 +0000

    [IMP] l10n_{mx,ua,in,dz,ma}: activate default setting
    For Mexican localization, activate by default the setting display_invoice_amount_total_words for the Mexican localization. As this localization uses its own amount to words function, the related commit enables an override of the generic function to use it correctly with Mexican localization.
    
    For the other mentioned localizations (ua,in,dz,ma), the setting was already activated but overriding the create method was not a proper way of doing.
    
    task - 3367243
    
    closes odoo/odoo#124801
    
    X-original-commit: b318504aee84fd7476952b4809222579e33c7af4
    Related: odoo/enterprise#42452
    Signed-off-by: Laurent Smet <las@odoo.com>
    Signed-off-by: hupo-odoo <hupo@odoo.com>

--- addons/l10n_in/models/__init__.py
+++ addons/l10n_in/models/__init__.py
@@ -2,7 +2,6 @@
 from . import template_in
 from . import account
 from . import account_invoice
-from . import company
 from . import product_template
 from . import port_code
 from . import res_config_settings
