ee462353d61c15813390eb899f48a4d5ff3b48e2
luvi <luvi@odoo.com>
2023-06-12 18:34:33 +0200

    [FIX] mail: display strip animation on filtering activities
    This commit fixes the display of the ColumnProgress component, which was
    no longer displaying an animation when a filter is applied. This was due
    to changes made in commit (1), introducing an "activeBar" key on the
    value given to the ColumnProgress component.
    
    A test has been modified to assert that the classes corresponding to the
    animation are added as expected when a filter is selected.
    
    1) 58ca40b03215ef4c6c575267494dc8bccc30a033

--- addons/mail/static/src/views/activity/activity_renderer.js
+++ addons/mail/static/src/views/activity/activity_renderer.js
@@ -87,6 +87,7 @@ export class ActivityRenderer extends Component {
 
         const progressBar = {
             bars: [],
+            activeBar: isColumnFiltered ? this.activeFilter.progressValue.active : null,
         };
         for (const [value, count] of Object.entries(types)) {
             progressBar.bars.push({
