3a315e0c6e8421141f8cc56b087021d1d9a3dfd4
David (dafr) <dafr@odoo.com>
2023-06-13 13:05:48 +0000

    [FIX] mrp: Prevent multiples 'move_finished_ids.move_line_ids' on production
    - Create product FNS tracked by lots
    - Create Bom for FNS with component CMP
    - Set CMP qty to 100
    - For FNS Create 2 quants with 10 units each and a different lot
    - Create an internal transfer for 20 units of FNS from Stock to Production
    - Create a MO for 20 units of FNS > Confirm
    - Without touching the 'Create a new serial/lot number' button, create & select a new lot.
    - Mark as Done -> Apply
    - Go to Traceability: You can see 2 moves (quantity is correct)
    
    When you update the lot_producing_id in the production without using the
    button 'Create a new serial/lot number', the
    move_finished_ids.move_line_ids will not be correctly updated, they will
    keep there original lot_id and product_uom_qty will be unchanged. Then,
    in _post_inventory when you validate the production, the method
    `_set_quantity_done` will work with the `product_uom_qty` from incorrect
    lots, lots which will be replaced just after.
    
    Now, when the lot_producing_id is updated, we automatically update the
    move_finished_ids.move_line_ids.lot_id with it.
    
    OPW-3327347
    
    X-original-commit: 7fc63d4ff873122eed3fd59c0faf656d89c85555

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -845,6 +845,8 @@ class MrpProduction(models.Model):
                     finished_move_lines.write({'lot_id': vals.get('lot_producing_id')})
                 if 'qty_producing' in vals:
                     finished_move_lines.write({'qty_done': vals.get('qty_producing')})
+            elif production.state not in ['draft', 'done', 'cancel'] and 'lot_producing_id' in vals:
+                production.move_finished_ids.filtered(lambda m: m.product_id == production.product_id).move_line_ids.write({'lot_id': production.lot_producing_id.id})
             if self._has_workorders() and not production.workorder_ids.operation_id and vals.get('date_start') and not vals.get('date_finished'):
                 new_date_start = fields.Datetime.to_datetime(vals.get('date_start'))
                 if not production.date_finished or new_date_start >= production.date_finished:
