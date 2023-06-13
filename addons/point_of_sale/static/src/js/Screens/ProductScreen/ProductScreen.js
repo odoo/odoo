fb3068d566326351aad57d2996b7392992a884b6
Walid <waha@odoo.com>
2023-06-13 12:59:38 +0200

    [FIX] point_of_sale: apply quantity bracode nomenclature
    Steps to reproduce:
    1) Have Barcode Installed and have Barcode Nomenclature set
    2) edit 1st rule(Price Barcodes 2 Decimals) to type Quantity
    3) Create a product with barcode 2380201000008
    4) Open POS and scan barcode 2380201010007
    
    Bug:
    error popup product not found because the callback for this rule
    is not set
    
    Fix:
    apply same callback as weight (set product quantity)
    
    opw-3213595
    
    X-original-commit: 36459d26f1adb92f92d52ce05329e8ad3e95dd91

--- addons/point_of_sale/static/src/js/Screens/ProductScreen/ProductScreen.js
+++ addons/point_of_sale/static/src/js/Screens/ProductScreen/ProductScreen.js
@@ -22,6 +22,7 @@ class ProductScreen extends ControlButtonsMixin(PosComponent) {
         useListener("click-pay", this._onClickPay);
         useBarcodeReader({
             product: this._barcodeProductAction,
+            quantity: this._barcodeProductAction,
             weight: this._barcodeProductAction,
             price: this._barcodeProductAction,
             client: this._barcodePartnerAction,
@@ -281,7 +282,7 @@ class ProductScreen extends ControlButtonsMixin(PosComponent) {
                     price_manually_set: true,
                 },
             });
-        } else if (code.type === "weight") {
+        } else if (code.type === "weight" || code.type === 'quantity') {
             Object.assign(options, {
                 quantity: code.value,
                 merge: false,
