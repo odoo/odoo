e01d1ba9bbc778e5760ce612c7f317df09c1eb51
Hugo Carlier (Huca) <huca@odoo.com>
2023-06-14 18:08:59 +0200

    [WIP] project: refactor personal stage (function D.a)
    One single edition form for stage and personal stage (instead of 2
    with js custo to select the good one)

--- addons/project/static/src/views/project_task_kanban/project_task_kanban_header.js
+++ addons/project/static/src/views/project_task_kanban/project_task_kanban_header.js
@@ -20,28 +20,6 @@ export class ProjectTaskKanbanHeader extends KanbanHeader {
         }
     }
 
-    editGroup() {
-        const { resModel, value } = this.group;
-        const groupBy = this.props.list.groupBy;
-        if (groupBy.length !== 1 || groupBy[0] !== 'personal_stage_type_ids') {
-            super.editGroup();
-            return;
-        }
-        const context = Object.assign({}, this.group.context, {
-            form_view_ref: 'project.personal_task_type_edit',
-        });
-        this.dialog.add(FormViewDialog, {
-            context,
-            resId: value,
-            resModel: resModel,
-            title: this.env._t('Edit Personal Stage'),
-            onRecordSaved: async () => {
-                await this.props.list.load();
-                this.props.list.model.notify();
-            },
-        });
-    }
-
     async deleteGroup() {
         if (this.group.groupByField.name === 'stage_id') {
             const action = await this.group.model.orm.call(
