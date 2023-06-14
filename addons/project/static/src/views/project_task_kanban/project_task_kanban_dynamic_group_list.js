2187b88cff252f56a5ac44751a17988718f16141
Hugo Carlier (Huca) <huca@odoo.com>
2023-06-14 17:50:30 +0200

    [WIP] project: refactor personal stage (function D.c)
    No more edition of personal stages in other views than my task

--- addons/project/static/src/views/project_task_kanban/project_task_kanban_dynamic_group_list.js
+++ addons/project/static/src/views/project_task_kanban/project_task_kanban_dynamic_group_list.js
@@ -5,34 +5,11 @@ import { Domain } from '@web/core/domain';
 import { session } from '@web/session';
 
 export class ProjectTaskKanbanDynamicGroupList extends KanbanDynamicGroupList {
-    get context() {
-        const context = super.context;
-        if (context.createPersonalStageGroup) {
-            context.default_user_id = context.uid;
-            delete context.createPersonalStageGroup;
-            delete context.default_project_id;
-        }
-        return context;
-    }
-
     get isGroupedByStage() {
         return !!this.groupByField && this.groupByField.name === 'stage_id';
     }
 
     get isGroupedByPersonalStages() {
         return !!this.groupByField && this.groupByField.name === 'personal_stage_type_id';
-    }
-
-    async createGroup() {
-        if (this.isGroupedByPersonalStages) {
-            this.defaultContext = Object.assign({}, this.defaultContext || {}, {
-                createPersonalStageGroup: true,
-            });
-        }
-        const result = await super.createGroup(...arguments);
-        if (this.isGroupedByPersonalStages) {
-            delete this.defaultContext.createPersonalStageGroup;
-        }
-        return result;
     }
 }
