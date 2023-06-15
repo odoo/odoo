d855669020937a96342fecb684d7acdb586a77bb
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-06-14 20:18:31 -0600

    removed user_menu_items with branding

--- addons/web/static/src/webclient/user_menu/user_menu_items.js
+++ addons/web/static/src/webclient/user_menu/user_menu_items.js
@@ -118,9 +118,7 @@ function logOutItem(env) {
 registry
     .category("user_menuitems")
     .add("documentation", documentationItem)
-    .add("support", supportItem)
     .add("shortcuts", shortCutsItem)
     .add("separator", separator)
     .add("profile", preferencesItem)
-    .add("odoo_account", odooAccountItem)
     .add("log_out", logOutItem);
