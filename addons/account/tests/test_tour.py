5386c586ae3c657a844fcdb97ff2c9b81f7c9cbc
Laurent Smet <las@odoo.com>
2023-06-15 14:47:03 +0200

    [IMP] account: Add 'Request Cancel' step in invoice workflow
    Allow localizations having an EDI to prevent resetting to draft an invoice already
    sent to a government. By law, in some countries, if you want to cancel an invoice,
    you need to ask an authorization from the government.
    
    task: 3069324

--- addons/account/tests/test_tour.py
+++ addons/account/tests/test_tour.py
@@ -12,7 +12,7 @@ class TestUi(odoo.tests.HttpCase):
         super().setUpClass()
 
         all_moves = cls.env['account.move'].search([('move_type', '!=', 'entry')])
-        all_moves.button_draft()
+        all_moves.filtered(lambda x: x.state in ('posted', 'cancel')).button_draft()
         all_moves.with_context(force_delete=True).unlink()
 
         # In case of latam impacting multiple countries, disable the required fields manually.
