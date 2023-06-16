15d5c7dd7749cc486b3eb1051968333b359c0651
Romain Derie <rde@odoo.com>
2023-06-16 15:35:07 +0200

    [IMP] web_editor, website: keep custom snippet translations
    This commit adds translation capability to custom snippets.
    Before this commit, the custom snippet was not translate friendly:
    1. Neither when saving a block as custom snippet
    2. Neither when dropping a custom snippet into a page.
    
    This was a known limitation for years. But now it's time to make it
    work.
    
    Step to reproduce (part 1):
    - Enable french
    - Drag & drop Title snippet in a page
    - Translate the Title
    - Save the block as custom snippet
    -> Go in the backend view of this custom snippet, in debug, there is no
       translation that followed the title.
    
    Step to reproduce (part 2):
    - Following previous steps, now add the translation manually on the
      custom snippet view
    - Back in a page in edit mode, drag & drop this custom snippet in the
      page
    - Switch to french
    -> The title is not translated, the drag & drop copy code but no
       translations
    
    Those are the "page/view" to "page/view" translation transfer, but it
    could also be any html field (like a product description) to view
    (custom snippet) or view (custom snippet) to any html field.
    
    task-3375518
    opw-3242100

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -3588,7 +3588,10 @@ var SnippetsMenu = Widget.extend({
      */
     _registerDefaultTexts: function ($in) {
         if ($in === undefined) {
-            $in = this.$snippets.find('.oe_snippet_body');
+            // By default, we don't want the `o_default_snippet_text` class on
+            // custom snippets. Those are most likely already ready, we don't
+            // really need the auto-selection by the editor.
+            $in = this.$snippets.find('.oe_snippet_body:not(.s_custom_snippet)');
         }
 
         $in.find('*').addBack()
