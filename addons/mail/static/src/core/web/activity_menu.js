daee4741614cbeaa083e0e5ed8b53506684a913b
Pierre-Yves Dufays <pydu@odoo.com>
2023-06-16 14:32:05 +0200

    [IMP] mail, various: simplify the systray
    The system tray has been simplified:
    - only one clickable area per activity group
    - by default, a click on an activity group open the list view, except for
    journal entries, project, task, documents where the activity view is opened.
    - the clock icon providing access to the activity view has been removed. As it
    is the only action, we also remove "actions" returned by systray_get_activities
    in res_users.
    - the KPIs are no longer clickable
    
    The tests have been updated (for example, because we open the list view instead
    of the kanban view) and one removed "activity menu widget: activity view icon"
    as we only have one clickable area now and no more icons.
    
    Behavior not changed intentionaly when opening a view from the activity menu:
    - the view is opened with a hidden filter (just a domain) that displays the
    records with at least one activity assigned to the user
    - the crm.lead and calendar.event view is opened with custom filters
    
    Task-3300854

--- addons/mail/static/src/core/web/activity_menu.js
+++ addons/mail/static/src/core/web/activity_menu.js
@@ -48,47 +48,32 @@ export class ActivityMenu extends Component {
         ];
     }
 
-    onClickAction(action, group) {
-        document.body.click(); // hack to close dropdown
-        if (action.action_xmlid) {
-            this.env.services.action.doAction(action.action_xmlid);
-        } else {
-            let domain = [["activity_ids.user_id", "=", this.userId]];
-            if (group.domain) {
-                domain = Domain.and([domain, group.domain]).toList();
-            }
-            this.action.doAction(
-                {
-                    domain,
-                    name: group.name,
-                    res_model: group.model,
-                    type: "ir.actions.act_window",
-                    views: this.availableViews(group),
-                },
-                { clearBreadcrumbs: true, viewType: "activity" }
-            );
-        }
+    /**
+     * Allow to define the view type used for opening an activity group.
+     *
+     * Meant to be overridden by other modules to define custom view type per model.
+     *
+     * @param {string} model The model for which the view type needs to be determined.
+     * @returns {string} view type for the given model.
+     */
+    getActivityGroupViewType(model) {
+        return "list";
     }
 
-    openActivityGroup(group, filter = "all") {
+    openActivityGroup(group) {
         document.body.click(); // hack to close dropdown
         const context = {
             // Necessary because activity_ids of mail.activity.mixin has auto_join
             // So, duplicates are faking the count and "Load more" doesn't show up
             force_search_count: 1,
         };
-        if (filter === "all") {
-            context.search_default_activities_overdue = 1;
-            context.search_default_activities_today = 1;
-        } else {
-            context["search_default_activities_" + filter] = 1;
-        }
         let domain = [["activity_ids.user_id", "=", this.userId]];
         if (group.domain) {
             domain = Domain.and([domain, group.domain]).toList();
         }
         const views = this.availableViews(group);
 
+        const viewType = this.getActivityGroupViewType(group.model);
         this.action.doAction(
             {
                 context,
@@ -99,7 +84,7 @@ export class ActivityMenu extends Component {
                 type: "ir.actions.act_window",
                 views,
             },
-            { clearBreadcrumbs: true }
+            { clearBreadcrumbs: true, viewType: viewType }
         );
     }
 }
--- addons/test_mail/tests/test_mail_multicompany.py
+++ addons/test_mail/tests/test_mail_multicompany.py
@@ -399,7 +399,6 @@ class TestMultiCompanySetup(TestMailMCCommon):
         self.assertEqual(
             test_activity,
             {
-                "actions": [{"icon": "fa-clock-o", "name": "Summary"}],
                 "icon": "/base/static/description/icon.png",
                 "id": self.env["ir.model"]._get_id("mail.test.multi.company.with.activity"),
                 "model": "mail.test.multi.company.with.activity",
@@ -407,6 +406,7 @@ class TestMultiCompanySetup(TestMailMCCommon):
                 "overdue_count": 0,
                 "planned_count": 0,
                 "today_count": 2,
+                "done_count": 0,
                 "total_count": 2,
                 "type": "activity",
             }
@@ -419,13 +419,13 @@ class TestMultiCompanySetup(TestMailMCCommon):
         self.assertEqual(
             test_activity,
             {
-                "actions": [{"icon": "fa-clock-o", "name": "Summary"}],
                 "icon": "/base/static/description/icon.png",
                 "id": self.env["ir.model"]._get_id("mail.test.multi.company.with.activity"),
                 "model": "mail.test.multi.company.with.activity",
                 "name": "Test Multi Company Mail With Activity",
                 "overdue_count": 0,
                 "planned_count": 0,
+                "done_count": 0,
                 "today_count": 1,
                 "total_count": 1,
                 "type": "activity",
