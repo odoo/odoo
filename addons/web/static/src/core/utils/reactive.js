3fed2e14a488eec8fce581e273fb2eb2871b82ce
Samuel Degueldre <sad@odoo.com>
2023-06-19 10:56:30 +0200

    [IMP] web: add tests and comments for reactivity utils
    The reactivity utils were initially an ad-hoc solution to some problems
    that came up when using the reactivity in places that are not
    components. As it turned out that these needs are actually quite common,
    the utils that were originally duplicated in many modules were moved to
    the core utils in web, but there were no tests for these utils, and the
    Reactive class was not documented.
    
    This commit adds some tests for the reactivity utils and a docstring
    motivating the existence of the Reactive class.

--- addons/web/static/src/core/utils/reactive.js
+++ addons/web/static/src/core/utils/reactive.js
@@ -2,6 +2,25 @@
 
 import { reactive } from "@odoo/owl";
 
+/**
+ * This class should be used as a base when creating a class that is intended to
+ * be used within the reactivity system, it avoids a specific class of bug where
+ * callbacks that capture `this` declared in the constructor would escape the
+ * reactivity system and prevent the observers from being notified:
+ *
+ * const bus = new EventBus();
+ * class MyClass {
+ *   constructor() {
+ *     this.counter = 0;
+ *     bus.addEventListener("change", () => this.counter++);
+ *     //                                   ^ Will never be reactive, this mutation will be missed
+ *   }
+ * }
+ * const myObj = reactive(new MyClass(bus), () => console.log(myObj.counter));
+ * myObj.counter++; // logs 0;
+ * bus.trigger("change"); // logs nothing!
+ * myObj.counter++; // logs 2. counter == 1 was missed.
+ */
 export class Reactive {
     constructor() {
         return reactive(this);
