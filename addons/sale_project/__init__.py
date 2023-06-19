19ce9dc71a59b9029387fa2ca671b8daa5d84848
Xavier Bol (xbo) <xbo@odoo.com>
2023-06-19 10:08:54 +0000

    [FIX] {sale_}project: make project billable if one of its tasks has partner
    Before this commit, when the user installs `sale_project` and some
    of his projects has no partner set but a partner is set on some
    tasks of those projects then the user will no longer see the partner
    field in those tasks because the project is not billable.
    
    This commit checks if one of the tasks has a partner set to make the
    project linked billable if it is not yet the case.
    
    closes odoo/odoo#126880
    
    X-original-commit: b0cb255577a7a0c49c2c764dbad715d3842b9508
    Related: odoo/enterprise#43384
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_project/__init__.py
+++ addons/sale_project/__init__.py
@@ -6,5 +6,13 @@ from . import controllers
 from . import report
 
 def _set_allow_billable_in_project(env):
-    projects = env['project.project'].search([('partner_id', '!=', False), ('allow_billable', '=', False)])
+    Project = env['project.project']
+    Task = env['project.task']
+    projects = Project.search(Project._get_projects_to_make_billable_domain())
+    non_billable_projects, = Task._read_group(
+        Task._get_projects_to_make_billable_domain([('project_id', 'not in', projects.ids)]),
+        [],
+        ['project_id:recordset'],
+    )[0]
+    projects += non_billable_projects
     projects.allow_billable = True
