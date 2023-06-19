ca4ba84d98acb3b0f7e8a4a6a73e0da07eb7c1ca
Julien Van Roy <juvr@odoo.com>
2023-06-19 13:25:20 +0000

    [FIX] mail: UTF-8 text/xml attachment and omitted charset
    When parsing an email containing an xml attachment, the `email` python
    module will decode the base64 attachment using the charset or ascii if
    the charset is missing.
    
    In some cases, the payload is in UTF-8 but the charset is omitted. This
    results in replacement characters for the non ASCII characters.
    
    The solution is to force the charset to UTF-8, since it is a superset of
    ASCII that should not be a problem.
    
    NB1: Omitting the charset for text/xml is not recommended. See the RFC
    (section 6.4): https://www.ietf.org/rfc/rfc2376.txt
    
    opw-3144519
    
    closes odoo/odoo#126474
    
    X-original-commit: e3a5b46c7b4f5030e165a6d943ec2275aea5d583
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>
    Signed-off-by: Julien Van Roy (juvr) <juvr@odoo.com>

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -1420,6 +1420,10 @@ class MailThread(models.AbstractModel):
                     continue  # skip container
 
                 filename = part.get_filename()  # I may not properly handle all charsets
+                if part.get_content_type() == 'text/xml' and not part.get_param('charset'):
+                    # for text/xml with omitted charset, the charset is assumed to be ASCII by the `email` module
+                    # although the payload might be in UTF8
+                    part.set_charset('utf-8')
                 encoding = part.get_content_charset()  # None if attachment
 
                 content = part.get_content()
