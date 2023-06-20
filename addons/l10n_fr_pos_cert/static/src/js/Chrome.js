2030c64ccf5007c76f65dd87fae5c29888709f1e
pedrambiria <pebr@odoo.com>
2023-06-20 12:25:40 +0000

    [FIX] l10n_fr_pos_cert: keep behind close popup
    Before this commit, if a session be open more than one day, it's won't
    be possible to close the session with a cash difference. The problem
    is that when the `ConfirmPopup` be added to the popup the
    `ClosePosPopup` will be destroyed, and by confirming the "Payments
    Difference" popup you will get this error:
    ```
    Error: Component is destroyed
        at ORM.call (.../web/assets/debug/point_of_sale.pos_assets_backend.js:17590:35) (/web/static/src/core/utils/hooks.js:174)
        at ClosePosPopup.closeSession (.../web/assets/debug/point_of_sale.assets.js:9443:49) (/point_of_sale/static/src/js/Popups/ClosePosPopup.js:124)
        at ClosePosPopup.confirm (.../web/assets/debug/point_of_sale.assets.js:9363:22) (/point_of_sale/static/src/js/Popups/ClosePosPopup.js:44)
    ```
    
    The solution is to use `keepBehind: true` to prevent closing the close
    popup. I tested in other versions and it wasn't an issue in
    previous versions.
    
    opw-3368772
    
    closes odoo/odoo#125779
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/l10n_fr_pos_cert/static/src/js/Chrome.js
+++ addons/l10n_fr_pos_cert/static/src/js/Chrome.js
@@ -13,7 +13,7 @@ patch(Chrome.prototype, "l10n_fr_pos_cert.Chrome", {
             limitDate.setDate(limitDate.getDate() + 1);
             if (limitDate.getTime() < now) {
                 const info = await this.env.pos.getClosePosInfo();
-                this.popup.add(ClosePosPopup, { info });
+                this.popup.add(ClosePosPopup, { info: info, keepBehind: true });
             }
         }
     },
