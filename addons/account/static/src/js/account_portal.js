2e055506cc68fd3e03ee1192856ad8dc71386c0f
Arthur Detroux (ard) <ard@odoo.com>
2023-06-20 16:27:21 +0200

    [FIX] account, sale: fix dependency on PortalHomeCounters
    Prior to this commit, the patches done by `account_portal.js` and
    `sale_portal.js` were done without any import.
    
    This means that the widget it modifies (PortalHomeCounters) might not
    already be loaded in the registry. This could lead to some issue later.
    
    This commit uses an "import" statement on the module that defines
    PortalHomeCounters to ensure that it is present in the registry.
    
    task-3249625
    
    X-original-commit: 7937dbec9699e7d0f68450c469c9b742446303eb

--- addons/account/static/src/js/account_portal.js
+++ addons/account/static/src/js/account_portal.js
@@ -1,6 +1,7 @@
 /** @odoo-module */
 
 import publicWidget from 'web.public.widget';
+import "@portal/js/portal"; // force dependencies
 
 publicWidget.registry.PortalHomeCounters.include({
     /**
