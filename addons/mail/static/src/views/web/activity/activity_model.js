45ac9930999642b5f1e58d4ae05062fd405023b0
Pierre-Yves Dufays <pydu@odoo.com>
2023-06-20 08:40:46 +0200

    [IMP] mail: add pager on activity view
    Add a pager on the activity views.
    
    Task-3300854

--- addons/mail/static/src/views/web/activity/activity_model.js
+++ addons/mail/static/src/views/web/activity/activity_model.js
@@ -11,14 +11,62 @@ class ActivityDynamicRecordList extends DynamicRecordList {
 export class ActivityModel extends RelationalModel {
     static DynamicRecordList = ActivityDynamicRecordList;
 
+    /**
+     * Extracts pagination parameters 'limit' and 'offset' from params and return them along with the rest of the params
+     * properties.
+     *
+     * If 'limit' and/or 'offset' are not found in the object, default values are set (80 for limit, 0 for offset).
+     *
+     * @param params search parameters
+     * @returns {{paramsWithoutPagination: Object, offset: number, limit: number}}
+     * @private
+     */
+    static _extractPaginationParams(params) {
+        const paramsWithoutPagination = {...params};
+        let limit = 80; // default limit
+        if ('limit' in paramsWithoutPagination) {
+            limit = paramsWithoutPagination.limit;
+            delete paramsWithoutPagination.limit;
+        }
+        let offset = 0;
+        if ('offset' in paramsWithoutPagination) {
+            offset = paramsWithoutPagination.offset;
+            delete paramsWithoutPagination.offset;
+        }
+        return {paramsWithoutPagination:paramsWithoutPagination, offset:offset, limit:limit};
+    }
+
+    /**
+     * Loads model instances linked to the loaded activities (activityData).
+     *
+     * For example, for event activities, loads the event.event instance related to the activities.
+     *
+     * @param params search parameters
+     * @returns {Promise<void>}
+     */
+    async loadRelatedModel(params){
+        const { paramsWithoutPagination, offset, limit } = ActivityModel._extractPaginationParams(params);
+        if ( (limit) && (this.activityData.activity_res_ids.length > limit) ) {
+            const selectedIds = this.activityData.activity_res_ids.slice(offset, offset + limit);
+            if (!paramsWithoutPagination.domain) {
+                paramsWithoutPagination.domain = [];
+            }
+            paramsWithoutPagination.domain.push(["id", "in", selectedIds]);
+        }
+        await super.load(paramsWithoutPagination);
+        this.offset = offset;
+        this.limit = limit;
+    }
+
     async load(params = {}) {
         this.originalDomain = params.domain ? [...params.domain] : [];
         params.domain?.push(["activity_ids", "!=", false]);
         if (params && "groupBy" in params) {
             params.groupBy = [];
         }
-        this.activityData = await this.fetchActivityData(params);
-        await super.load(params);
+        const { paramsWithoutPagination, offset, limit } = ActivityModel._extractPaginationParams(params);
+        this.activityData = await this.fetchActivityData(paramsWithoutPagination);
+        await this.loadRelatedModel({ ...paramsWithoutPagination, offset: offset, limit: limit });
     }
 
     fetchActivityData(params) {
