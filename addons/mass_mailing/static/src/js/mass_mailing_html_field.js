51bd1c51480ebb967604221a66a3c160f5da7664
Antoine Guenet <age@odoo.com>
2023-06-20 11:47:59 +0200

    [FIX] mass_mailing: update iframe size on pick template
    When picking a mailing template, the size of the contents of the iframe
    changes but we failed to signal it so the iframe could resize as well.
    
    X-original-commit: 387c4fb815a802e6138671a22864dac97a490365

--- addons/mass_mailing/static/src/js/mass_mailing_html_field.js
+++ addons/mass_mailing/static/src/js/mass_mailing_html_field.js
@@ -385,6 +385,7 @@ export class MassMailingHtmlField extends HtmlField {
                 }
                 // mark selection done for tour testing
                 $editable.addClass('theme_selection_done');
+                this.onIframeUpdated();
             }, 0);
         });
 
