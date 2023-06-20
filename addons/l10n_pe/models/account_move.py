398e4d43ba6e70c6220747aec4e376392c06d521
Luis Torres <luis_t@vauxoo.com>
2023-06-20 16:58:01 +0000

    [IMP] l10n_pe_edi: Improve sales invoice document type selection
    The domain to document type on the invoice  has been updated to filter
    based on the partner's identification type.
    
    If the partner identification type is RUC, the default document type is
    set to 'Factura', but allows assign Boleta if its necessary; for all other
    partner identification types, the domain document type only allows 'Boleta'.
    
    Task latam:
    https://latam-localizations.odoo.com/web#id=916&menu_id=88&cids=1&action=188&model=project.task&view_type=form

--- addons/l10n_pe/models/account_move.py
+++ addons/l10n_pe/models/account_move.py
@@ -3,6 +3,20 @@ from odoo import models, fields
 from odoo.tools.sql import column_exists, create_column
 
 
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _get_l10n_latam_documents_domain(self):
+        self.ensure_one()
+        result = super()._get_l10n_latam_documents_domain()
+        if self.country_code != "PE" or not self.journal_id.l10n_latam_use_documents or self.journal_id.type != "sale":
+            return result
+        result.append(("code", "in", ("01", "03", "07", "08", "20", "40")))
+        if self.partner_id.l10n_latam_identification_type_id.l10n_pe_vat_code != '6':
+            result.append(('id', 'in', (self.env.ref('l10n_pe_edi.document_type08b') | self.env.ref('l10n_pe_edi.document_type02') | self.env.ref('l10n_pe_edi.document_type07b')).ids))
+        return result
+
+
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
