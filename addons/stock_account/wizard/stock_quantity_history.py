c9ce5970d5d2abc155082f7f228cba04a80d51fb
kir-odoo <kir@odoo.com>
2023-06-20 19:03:50 +0530

    [IMP] stock_account: add new menu inventory aging
    - Add a new menu called Inventory aging.
    
    - The purpose of inventory aging is that users can track the quantity and time
      spent on inventory.
    
    - When accessing the `Inventory Aging` menu, users will be directed to the same
      model found in the valuation menu.
      However,the `Inventory Ageing` menu presents an open default pivot view
      with predefined filters that facilitate the visualization of inventory aging based on quantity.
    
    Task: 3207613
    X-original-commit: 03b6d593eac4a3d9c977687619b4da7c7386a3fd

--- addons/stock_account/wizard/stock_quantity_history.py
+++ addons/stock_account/wizard/stock_quantity_history.py
@@ -11,8 +11,13 @@ class StockQuantityHistory(models.TransientModel):
         active_model = self.env.context.get('active_model')
         if active_model == 'stock.valuation.layer':
             action = self.env["ir.actions.actions"]._for_xml_id("stock_account.stock_valuation_layer_action")
+            action['views'] = [(self.env.ref('stock_account.stock_valuation_layer_valuation_at_date_tree_inherited').id, 'tree'),
+                               (self.env.ref('stock_account.stock_valuation_layer_form').id, 'form'),
+                               (self.env.ref('stock_account.stock_valuation_layer_pivot').id, 'pivot'),
+                               (self.env.ref('stock_account.stock_valuation_layer_graph').id, 'graph')]
             action['domain'] = [('create_date', '<=', self.inventory_datetime), ('product_id.type', '=', 'product')]
             action['display_name'] = format_datetime(self.env, self.inventory_datetime)
+            action['context'] = "{}"
             return action
 
         return super(StockQuantityHistory, self).open_at_date()
