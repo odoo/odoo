145bc6ecae6a0b153d721a0e3911675ea8b33681
Sébastien Theys <seb@odoo.com>
2023-06-20 14:59:18 +0000

    [FIX] mail: make file upload consistent between guest and portal
    opw-3370926
    
    closes odoo/odoo#82912
    
    Signed-off-by: Vranckx Florian (flvr) <flvr@odoo.com>

--- addons/mail/static/src/models/attachment/attachment.js
+++ addons/mail/static/src/models/attachment/attachment.js
@@ -28,6 +28,9 @@ function factory(dependencies) {
          */
         static convertData(data) {
             const data2 = {};
+            if ('access_token' in data) {
+                data2.accessToken = data.access_token;
+            }
             if ('checksum' in data) {
                 data2.checksum = data.checksum;
             }
