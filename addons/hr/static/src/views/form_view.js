a2840d45c02b87bbc3643090b8d3bdcb65be0e89
Kevin Baptiste <kba@odoo.com>
2023-06-21 09:26:59 +0000

    [REM] hr,hr_holidays: remove deprecated code
    Remove deprecated code from odoo/odoo#96545
    
    closes odoo/odoo#125894
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr/static/src/views/form_view.js
+++ addons/hr/static/src/views/form_view.js
@@ -4,10 +4,8 @@ import { registry } from "@web/core/registry";
 
 import { formView } from "@web/views/form/form_view";
 import { FormController } from "@web/views/form/form_controller";
-import { FormRenderer } from "@web/views/form/form_renderer";
 
 import { useArchiveEmployee } from "@hr/views/archive_employee_hook";
-import { useOpenChat } from "@mail/core/web/open_chat_hook";
 
 export class EmployeeFormController extends FormController {
     setup() {
@@ -22,16 +20,7 @@ export class EmployeeFormController extends FormController {
     }
 }
 
-// TODO KBA: to remove in master
-export class EmployeeFormRenderer extends FormRenderer {
-    setup() {
-        super.setup();
-        this.openChat = useOpenChat(this.props.record.resModel);
-    }
-}
-
 registry.category("views").add("hr_employee_form", {
     ...formView,
     Controller: EmployeeFormController,
-    Renderer: EmployeeFormRenderer,
 });
