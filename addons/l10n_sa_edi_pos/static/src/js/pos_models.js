98af22d9fdae103e7e18f483206ef379bd13e1c0
rhe-odoo <rhe@odoo.com>
2023-06-21 15:34:13 +0200

    [FIX] l10n_sa_edi_pos: fix order constructor
    Before this, it was old code that was used for this file.
    Now, we made it compatible with the refactoring.
    
    runbot-id: 22517

--- addons/l10n_sa_edi_pos/static/src/js/pos_models.js
+++ addons/l10n_sa_edi_pos/static/src/js/pos_models.js
@@ -1,15 +1,16 @@
 odoo.define("l10n_sa_edi_pos.models", function (require) {
     "use strict";
 
-    const models = require("point_of_sale.models");
-    const _super_order = models.Order.prototype;
+    var { Order } = require('point_of_sale.models');
+    const Registries = require('point_of_sale.Registries');
 
-    models.Order = models.Order.extend({
-        initialize: function () {
-            _super_order.initialize.apply(this, arguments);
+    const L10nSaOrder = (Order) => class L10nSaOrder extends Order {
+        constructor() {
+            super(...arguments);
             if (this.pos.company.country && this.pos.company.country.code === 'SA') {
                 this.set_to_invoice(true);
             }
-        },
-    });
+        }
+    }
+    Registries.Model.extend(Order, L10nSaOrder);
 });
