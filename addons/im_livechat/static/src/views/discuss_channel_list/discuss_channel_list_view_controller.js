97fc93d7a098d7ec95058c15d67dbdad125cbe41
tsm-odoo <tsm@odoo.com>
2023-06-21 10:03:54 +0000

    [IMP] im_livechat: improve rating views
    This commit improves the tree/kanban rating view for livechat
    channels:
    - Open the channel in discuss if the member is a member of it.
    Open the form view otherwise.
    
    part of task-3332872
    
    closes odoo/odoo#125902
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/src/views/discuss_channel_list/discuss_channel_list_view_controller.js
+++ addons/im_livechat/static/src/views/discuss_channel_list/discuss_channel_list_view_controller.js
@@ -1,30 +1,20 @@
 /* @odoo-module */
 
-import { createLocalId } from "@mail/utils/common/misc";
 import { _t } from "@web/core/l10n/translation";
-import { useService } from "@web/core/utils/hooks";
 import { ListController } from "@web/views/list/list_controller";
 
 export class DiscussChannelListController extends ListController {
     setup() {
         super.setup(...arguments);
-        this.threadService = useService("mail.thread");
-        this.store = useService("mail.store");
     }
 
     async openRecord(record) {
         if (!record.data.is_member) {
             return super.openRecord(record);
         }
-        const channel =
-            this.store.threads[createLocalId(record.resModel, record.resId)] ??
-            (await this.threadService.fetchChannel(record.resId));
-        if (!channel.is_pinned) {
-            this.threadService.pin(channel);
-        }
-        this.threadService.setDiscussThread(channel);
         this.actionService.doAction("mail.action_discuss", {
             name: _t("Discuss"),
+            additionalContext: { active_id: record.resId },
         });
     }
 }
