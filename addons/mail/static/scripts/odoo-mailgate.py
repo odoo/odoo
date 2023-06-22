13668765170d6740ccb69c2d8378a11dac9b6f39
bve-odoo <bve@odoo.com>
2023-06-22 15:27:16 +0200

    [IMP] mail: mailgate script sending mail using an EML:
    Using the technical format of an mail (called EML),
    this script can be used to test an incoming mail in a Odoo db.
    
    In order to use it, you at least need:
    - an EML,
    - a running db (and his URL).
    
    Here is how to get the EML for the most common providers:
    https://www.odoo.com/documentation/14.0/applications/general/email_communication/faq.html
    
    As a complement for local testing you can use the outgoing mail catcher:
    - mailcatcher (https://mailcatcher.me/)
    or
    - mailhog (https://github.com/mailhog/MailHog)

--- addons/mail/static/scripts/odoo-mailgate.py
+++ addons/mail/static/scripts/odoo-mailgate.py
@@ -1,46 +1,41 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-#
-# odoo-mailgate
-#
-# This program will read an email from stdin and forward it to odoo. Configure
-# a pipe alias in your mail server to use it, postfix uses a syntax that looks
-# like:
-#
-# email@address: "|/home/odoo/src/odoo-mail.py"
-#
-# while exim uses a syntax that looks like:
-#
-# *: |/home/odoo/src/odoo-mail.py
-#
-# Note python2 was chosen on purpose for backward compatibility with old mail
-# servers.
-#
-import optparse
-import sys
-import traceback
-import xmlrpclib
 
-def main():
-    op = optparse.OptionParser(usage='usage: %prog [options]', version='%prog v1.2')
-    op.add_option("-d", "--database", dest="database", help="Odoo database name (default: %default)", default='odoo')
-    op.add_option("-u", "--userid", dest="userid", help="Odoo user id to connect with (default: %default)", default=1, type=int)
-    op.add_option("-p", "--password", dest="password", help="Odoo user password (default: %default)", default='admin')
-    op.add_option("--host", dest="host", help="Odoo host (default: %default)", default='localhost')
-    op.add_option("--port", dest="port", help="Odoo port (default: %default)", default=8069, type=int)
-    (o, args) = op.parse_args()
+# This tool is sending mail to an Odoo instance (local or not). 
 
-    try:
-        msg = sys.stdin.read()
-        models = xmlrpclib.ServerProxy('http://%s:%s/xmlrpc/2/object' % (o.host, o.port), allow_none=True)
-        models.execute_kw(o.database, o.userid, o.password, 'mail.thread', 'message_process', [False, xmlrpclib.Binary(msg)], {})
-    except xmlrpclib.Fault as e:
-        # reformat xmlrpc faults to print a readable traceback
-        err = "xmlrpclib.Fault: %s\n%s" % (e.faultCode, e.faultString)
-        sys.exit(err)
-    except Exception as e:
-        traceback.print_exc(None, sys.stderr)
-        sys.exit(2)
+# Usage: 
+#     send-mail.py <eml_file> 
+# 					<-d <database>>
+# 					[-u <url> default:"http://localhost:8069"] 
+# 					[-U <user> default:"admin"] 
+# 					[-p <password> default:"admin"]
+# ]
 
-if __name__ == '__main__':
-    main()
+# Commit odoo#ab70afb is removing the previously used route '/mail/receive'
+# Now we use the xmlrpc function to log to the db and the odoo's method message_process of mail.thread,
+# the first step on received emails.
+
+# Working version: 8+
+
+# import base64
+import xmlrpc.client
+
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+
+parser = ArgumentParser(description='Send an email to Odoo databases',
+                        formatter_class=ArgumentDefaultsHelpFormatter)
+parser.add_argument('eml_file', help='EML File to send')
+parser.add_argument('-u', '--url', dest='url', default='http://localhost:8069', help='URL of the Odoo server')
+parser.add_argument('-d', '--db', dest='db', required=True, help='Odoo database to send the email to')
+parser.add_argument('-U', '--user', dest='username', default='admin', help='Login to connect with')
+parser.add_argument('-p', '--password', dest='password', default='admin', help='Password to connect with')
+args = parser.parse_args()
+
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(args.url))
+uid = common.authenticate(args.db, args.username, args.password, {})
+
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(args.url))
+with open(args.eml_file, 'rb') as f:
+    message = f.read()
+    #message = base64.b64encode(f.read()).decode()
+models.execute_kw(args.db, uid, args.password, 'mail.thread', 'message_process', [False, message],)
