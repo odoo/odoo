8dfa8180f03de358c3abb27e6b2d18beab13271e
Harsh Modi <hamo@odoo.com>
2023-06-23 17:53:25 +0530

    [ADD] l10n_in_ewaybill_stock: ewaybill for stock
    Add new module with functionality of `l10n_in_edi_ewaybill`
    on `stock`
    
    This module enables user to create ewaybill from stock picking
    without creating an account move
    
    task-3376530
    
    X-original-commit: 0fdb86622e72d80c96cb611fb7e4a59e9a2d57ec
    Co-authored-by: Jigar Vaghela (jva) <jva@odoo.com>

--- addons/l10n_in_stock/models/stock_picking.py
+++ addons/l10n_in_stock/models/stock_picking.py
@@ -10,3 +10,24 @@ class StockPicking(models.Model):
     def _should_generate_commercial_invoice(self):
         super(StockPicking, self)._should_generate_commercial_invoice()
         return True
+
+    def _get_l10n_in_dropship_dest_partner(self):
+        """
+        To be overriden by `l10n_in_purchase_stock` will be ideal to use it for `l10n_in_ewaybill_stock`
+        returns destination partner from purchase_id
+        """
+        pass
+
+    def _l10n_in_get_invoice_partner(self):
+        """
+        To be overriden by `l10n_in_sale_stock` will be ideal to use it for `l10n_in_ewaybill_stock`
+        returns invoice partner from sale_id
+        """
+        pass
+
+    def _l10n_in_get_fiscal_position(self):
+        """
+        To be inherited by `l10n_in_*_stock` will be ideal to use it for `l10n_in_ewaybill_stock`
+        returns fiscal position from order
+        """
+        pass
