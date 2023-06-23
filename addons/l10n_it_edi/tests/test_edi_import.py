8d4b0e1a186d7c0a0dce5b0388f0417ee3303a76
Nshimiyimana Séna <sesn@odoo.com>
2023-06-23 14:01:51 +0200

    [FIX] l10n_it_edi: global discount should not be taxed
    Bug
    ---
    When importing a fatturapa XML that has a global discount, the discount
    should be applied on the taxed amount.
    
    Cause
    -----
    Currently, the system automatically adds a tax on the discount line.

--- addons/l10n_it_edi/tests/test_edi_import.py
+++ addons/l10n_it_edi/tests/test_edi_import.py
@@ -137,13 +137,25 @@ class TestItEdiImport(TestItEdi):
                 <xpath expr="//FatturaElettronicaBody/DatiGenerali/DatiGeneraliDocumento" position="inside">
                     <ScontoMaggiorazione>
                         <Tipo>SC</Tipo>
-                        <Importo>10</Importo>
+                        <Importo>2</Importo>
                     </ScontoMaggiorazione>
                 </xpath>
             ''')
         invoices = self.edi_format._create_invoice_from_xml_tree(self.invoice_filename2, content)
 
-        discount_line = invoices.invoice_line_ids.filtered(lambda line: line.name == 'SCONTO')
-        self.assertRecordValues(discount_line, [{
-            'price_unit': -10,
+        self.assertRecordValues(invoices, [{
+            'amount_untaxed': 3.0,
+            'amount_tax': 1.1,
         }])
+        self.assertRecordValues(invoices.invoice_line_ids, [
+            {
+                'quantity': 5.0,
+                'name': 'DESCRIZIONE DELLA FORNITURA',
+                'price_unit': 1.0,
+            },
+            {
+                'quantity': 1.0,
+                'name': 'SCONTO',
+                'price_unit': -2,
+            }
+        ])
