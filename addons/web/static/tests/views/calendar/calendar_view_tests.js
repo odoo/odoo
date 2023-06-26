6c4702dc9b08af7a85ed8600cba22a81e27ec6d9
Leonardo Pavan Rocha <lpr@odoo.com>
2023-06-26 16:16:39 +0000

    [FIX] calendar: show time and duration on popover
    In 15.0 calendar events would show the time and duration of the event on the
    event popover, however after the migration of the calendar view to OWL, only
    events shown in a monthly scale would have the time being shown on the popover.
    This commit changes this behavior so that all scales except for the year one
    show the time and duration.
    
    task-3373470
    
    closes odoo/odoo#126463
    
    Signed-off-by: Leonardo Pavan Rocha <lpr@odoo.com>

--- addons/web/static/tests/views/calendar/calendar_view_tests.js
+++ addons/web/static/tests/views/calendar/calendar_view_tests.js
@@ -2464,10 +2464,18 @@ QUnit.module("Views", ({ beforeEach }) => {
 
         // switch to week mode
         await changeScale(target, "week");
-        assert.containsNone(
+        assert.containsOnce(
             findEvent(target, 2),
             ".fc-content .fc-time",
-            "should not show time in week mode as week mode already have time on y-axis"
+            "should show time in week mode"
+        );
+
+        // switch to day mode
+        await changeScale(target, "day");
+        assert.containsOnce(
+            findEvent(target, 2),
+            ".fc-content .fc-time",
+            "should show time in day mode"
         );
     });
 
@@ -3534,7 +3542,7 @@ QUnit.module("Views", ({ beforeEach }) => {
         );
         await click(target, ".o-calendar-quick-create--create-btn");
         assert.strictEqual(
-            findEvent(target, 8).textContent,
+            findEvent(target, 8).querySelector(".o_event_title").textContent,
             "new event in quick create",
             "should display the new record after quick create dialog"
         );
