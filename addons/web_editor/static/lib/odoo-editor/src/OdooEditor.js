8103eeffbd6aca2e1215b53aa503e77ad1121793
Adnan Saiyed <adsa@odoo.com>
2023-06-26 12:33:21 +0530

    [FIX] web_editor: space before word lost
    Current behaviour before commit:
    
    In website, space before the word is lost when deleting the word.
    
    Desired behaviour after commit:
    
    Now space is preserved when deleting the word.
    
    task-3233352

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -60,6 +60,7 @@ import {
     prepareUpdate,
     splitTextNode,
     boundariesOut,
+    rightLeafOnlyNotBlockPath,
 } from './utils/utils.js';
 import { editorCommands } from './commands/commands.js';
 import { Powerbox } from './powerbox/Powerbox.js';
@@ -1569,7 +1570,11 @@ export class OdooEditor extends EventTarget {
             fillEmpty(closestBlock(start));
         }
         fillEmpty(closestBlock(range.endContainer));
-        const joinWith = range.endContainer;
+        let joinWith = range.endContainer;
+        const rightLeaf = rightLeafOnlyNotBlockPath(joinWith).next().value;
+        if (rightLeaf && rightLeaf.nodeValue === ' ') {
+            joinWith = rightLeaf;
+        }
         // Rejoin blocks that extractContents may have split in two.
         while (
             doJoin &&
