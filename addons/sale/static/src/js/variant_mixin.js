a52a0c046697ef2654773127c0bd1dbeafc71f90
stcc-odoo <stcc@odoo.com>
2023-06-26 11:11:17 +0000

    [FIX] sale: fix pill variant styling
    Enterprise PR: odoo/enterprise#42551
    
    Steps to reproduce:
    
    1. Install website_sale
    2. Create product P
    	- Go to Attributes & Variants tab
    	- Add a line
    	- Create and edit attribute "PILL"
    		- Display Type = Pills
    		- Add some attribute values
    		- Save & Close
    	- Add the attribute values in the list view
    	- Save the product
    3. Go to website product page
    
    Issue:
    
    The pills have a visible radio button inside, requiring users to click
    on the radio button to change the variant. In V15, there was no radio
    button, and users could click anywhere on the variant container to
    change the variant.
    
    The change in behaviour happened because the jquery code for handling
    button clicks has changed. In V15 jquery, clicking a button would
    trigger a change event, which would then be captured by the owl event
    selector `change [data-attribute_exclusions]`, and the
    `onChangeVariant` method was called.
    
    In version 16, jQuery no longer triggers a change event when clicking
    a button.
    
    Solution:
    
    Hide the radio input visually, then convert the variant text to a
    label. When clicking on the label, the corresponding radio input
    is checked.
    
    This means that the whole button is not clickable anymore, only the
    text.
    
    opw-3185147
    
    X-original-commit: 38ca6bc355b9aea47bf0bf2e7ac67dfd1410da07

--- addons/sale/static/src/js/variant_mixin.js
+++ addons/sale/static/src/js/variant_mixin.js
@@ -10,6 +10,7 @@ var _t = core._t;
 var VariantMixin = {
     events: {
         'change .css_attribute_color input': '_onChangeColorAttribute',
+        'change .o_variant_pills input' :'_onChangePillsAttribute',
         'change .main_product:not(.in_cart) input.js_quantity': 'onChangeAddQuantity',
         'change [data-attribute_exclusions]': 'onChangeVariant'
     },
@@ -702,6 +703,14 @@ var VariantMixin = {
     _onChangeColorAttribute: function (ev) {
         var $parent = $(ev.target).closest('.js_product');
         $parent.find('.css_attribute_color')
+            .removeClass("active")
+            .filter(':has(input:checked)')
+            .addClass("active");
+    },
+
+    _onChangePillsAttribute: function (ev) {
+        var $parent = $(ev.target).closest('.js_product');
+        $parent.find('.o_variant_pills')
             .removeClass("active")
             .filter(':has(input:checked)')
             .addClass("active");
