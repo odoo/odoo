553031c020bb149e1b4208c82f4ead50f7ecf29e
Pierre-Yves Dufays <pydu@odoo.com>
2023-06-27 12:12:14 +0200

    [IMP] various: improve custom activity views
    The following summary view of the record in the activity views have been
    improved:
    - project_task: task state added
    - project_project: project manager added
    - event_event: responsible and the dates (date_begin_located and
    date_end_located) added
    - account_move: total amount, customer and state added
    - sale_order: total amount and the state added
    - purchase_order: total amount and the state added
    - crm_lead: customer and stage added
    - hr_applicant (hr_recruitment): recruiter added
    - survey_survey: responsible added
    - maintenance.request: added equipment and responsible
    - stock.picking: added scheduled_date
    - repair.order: responsible, schedule_date and product_id added
    - mrp.production: added responsible
    - hr_leave (hr_holidays): status is added and default deadline modified see
    below
    
    Ensures that "Schedule activity" is in one line by adding a colspan.
    
    hr_leave activity: When a time off approval activity is created as a
    consequence of the creation of a hr.leave, the deadline of the activity is set
    to the date_from of the hr.leave minus activity type delay_count (default 15)
    except if it leads to a date anterior to today. In that case it is set to
    today. That way, in the activity view, the time off approval activities are
    more or less sorted by related hr.leave date_from and the cell date is an
    indication of when the time off is planned.
    
    Technical note: on most activity view, the activity record was not occupying
    all the horizontal space. To solve that problem the css has been modified and
    the max-width (200) that was imposed on the sub div has been removed. And as
    it was impossible to impose a max-width for a flex div (which is the common
    case for the activity record), the max width is imposed on each text that might
    be too long using the class o_text_block. That class has been modified to
    impose a max-width. That's why that class has been added in most view.
    Alternativly, we could have modified the activity compiler to add that class
    when the attribute full was set (not done because not sure of the consequence).
    
    Task-3300854
    
    Part-of: odoo/odoo#138135

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -9,6 +9,7 @@ import pytz
 from collections import namedtuple, defaultdict
 
 from datetime import datetime, timedelta, time
+from dateutil.relativedelta import relativedelta
 from pytz import timezone, UTC
 
 from odoo.addons.base.models.ir_model import MODULE_UNINSTALL_FLAG
@@ -1460,10 +1461,19 @@ class HolidaysRequest(models.Model):
             elif holiday.state == 'confirm':
                 if holiday.holiday_status_id.leave_validation_type != 'no_validation':
                     user_ids = holiday.sudo()._get_responsible_for_approval().ids or self.env.user.ids
+                    today = fields.Date.today()
+                    activity_type = self.env.ref('hr_holidays.mail_act_leave_approval')
                     for user_id in user_ids:
+                        date_deadline = (
+                            (holiday.date_from -
+                             relativedelta(**{activity_type.delay_unit: activity_type.delay_count or 0})).date()
+                            if holiday.date_from else today)
+                        if date_deadline < today:
+                            date_deadline = today
                         activity_vals.append({
-                            'activity_type_id': self.env.ref('hr_holidays.mail_act_leave_approval').id,
+                            'activity_type_id': activity_type.id,
                             'automated': True,
+                            'date_deadline': date_deadline,
                             'note': note,
                             'user_id': user_id,
                             'res_id': holiday.id,
