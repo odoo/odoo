59828459ad203a8e4e4ddff8d765d6825e761db2
Antoine (anso) <anso@odoo.com>
2023-06-27 15:20:24 +0000

    [IMP] mass_mailing: restore sheet
    This commit restores the sheet layout in mass_mailing for consistency
    reasons.
    
    The chatter was extracted from the tab it was in and moved back on
    the right side.
    
    This commit also fixes an alignment problem when an alert is displayed
    and the alert was vertically bigger that the buttons of the status bar
    
    task-3355071
    
    closes odoo/odoo#130616
    
    Forward-port-of: https://github.com/odoo/odoo/pull/126646
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg_iframe.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg_iframe.js
@@ -70,7 +70,6 @@ patch(Wysiwyg.prototype, {
             this.$root = this.$editable;
         }
         this.$iframe = $('<iframe class="wysiwyg_iframe o_iframe">').css({
-            'min-height': '55vh',
             width: '100%'
         });
         var avoidDoubleLoad = 0; // this bug only appears on some configurations.
