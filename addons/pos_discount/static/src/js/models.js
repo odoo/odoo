c30c1dac33da05a4064d5353215098b2a5b3c87d
Joseph Caburnay <jcb@odoo.com>
2023-06-27 08:08:24 +0000

    [FIX] pos_discount: replace use of is_program_reward by reward_id
    In https://github.com/odoo/odoo/pull/126217, we forward-ported an introduction
    of a helper method to identify if an orderline is discountable
    (`isGlobalDiscountApplicable`). However, starting 16.0, the field
    `is_program_reward` and `gift_card_id` (used by the method) are no longer
    available. Their use in the method can be replaced by `reward_id` which is done
    in this commit.
    
    closes odoo/odoo#126505
    
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/pos_discount/static/src/js/models.js
+++ addons/pos_discount/static/src/js/models.js
@@ -10,8 +10,8 @@ odoo.define('pos_discount.models', function (require) {
      * @returns Boolean
      */
     isGlobalDiscountApplicable() {
-      const is_tips_product = () => this.pos.config.tip_product_id && this.product.id === this.pos.config.tip_product_id[0];
-      return !this.is_program_reward && !this.gift_card_id && !is_tips_product();
+      const isTipsProduct = this.pos.config.tip_product_id && this.product.id === this.pos.config.tip_product_id[0];
+      return !this.reward_id && !isTipsProduct;
     }
   }
   Registries.Model.extend(Orderline, PosDiscountOrderline);
