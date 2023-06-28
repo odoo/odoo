5b19d079ff6d4f8ceb5ba0ca5a119f4261a1b173
Claire Bretton (clbr) <clbr@odoo.com>
2023-06-28 09:38:42 +0000

    [FIX] account,l10n_es: revert commit f2c7b7b72f8877a642bbd4d66218b5630cc9ac6c
    We chose a different solution to handle default chart_template.
    This is more robust and explicit since we define a sequence on
    chart_template, instead of relying on import order.
    New solution commit:
    https://github.com/odoo/odoo/commit/41794859a85bacbf16604ffd37c7f1b0a358e196
    Old solution commit:
    https://github.com/odoo/odoo/commit/f2c7b7b72f8877a642bbd4d66218b5630cc9ac6c
    
    opw-3329987
    
    closes odoo/odoo#126649
    
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/l10n_es/models/__init__.py
+++ addons/l10n_es/models/__init__.py
@@ -1,5 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from . import template_es_pymes
-from . import template_es_full
 from . import template_es_assec
 from . import template_es_common
+from . import template_es_full
+from . import template_es_pymes
