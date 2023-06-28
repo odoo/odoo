53eea04038a8d86dd250cd241254605c83eeb342
Aman Patel <ampa@odoo.com>
2023-06-28 05:12:42 +0000

    [FIX] hr_timesheet: internal project visible only in configuration menu
    Steps:
    - Install timesheet.
    - Go to configuration->projects.
    - Internal project is visible.
    - Check the projects menu.
    - Internal project not visible.
    - Turn on project stages from the configuration.
    
    Issue:
    - After turning on project stages, the internal project gets visible in
    projects menu as well.
    
    Cause:
    - We have not set 'is_internal_project' to false for group stages.
    
    Fix:
    - Set 'is_internal_project' to false.
    
    task-3392129
    
    closes odoo/odoo#143816
    
    X-original-commit: ac2aa3fc810f2edf60ae2e276abfbb7432e5b633
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/hr_timesheet/__init__.py
+++ addons/hr_timesheet/__init__.py
@@ -33,9 +33,14 @@ def create_internal_project(env):
     _check_exists_collaborators_for_project_sharing(env)
 
 def _uninstall_hook(env):
-    act_window = env.ref('project.open_view_project_all', raise_if_not_found=False)
-    if act_window and act_window.domain and 'is_internal_project' in act_window.domain:
-        act_window.domain = []
+
+    def update_action_window(xmlid):
+        act_window = env.ref(xmlid, raise_if_not_found=False)
+        if act_window and act_window.domain and 'is_internal_project' in act_window.domain:
+            act_window.domain = []
+
+    update_action_window('project.open_view_project_all')
+    update_action_window('project.open_view_project_all_group_stage')
 
     # archive the internal projects
     project_ids = env['res.company'].search([('internal_project_id', '!=', False)]).mapped('internal_project_id')
