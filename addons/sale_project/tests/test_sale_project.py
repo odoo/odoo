8f601517a71512d5e46d8ca81a909b771f7494b4
Victor Piryns (pivi) <pivi@odoo.com>
2023-06-28 16:09:39 +0200

    [FIX] sale_project: create task when confirm SO from template
    Current behaviour:
    When creating a new quotation from an FSM task, if we use a template
    that adds a line that is a service that would have added a task on
    SO confirmation, no new task is generated.
    
    Expected behaviour:
    A new task should be created, just like when we add manually the
    line with service without using a quotation template.
    
    Steps to reproduce:
    - Install FSM
    - Activate "Extra Quotations" from Settings
    - Create a new service that generates a task on SO confirmations
    - Create a new quotation template, add a line that uses the service
      we created.
    - In a FSM task, click on "New Quotation"
    - Add the template to the quotation and confirm it
    - Observe there is no Task attached to the SO. No new task was created.
    
    Reason for the problem:
    When we create a new quotation view the action "New Quotation", we
    add in the context a key `default_task_id` with the current task
    that generated the quotation. When we add a template, an onchange is
    triggered which creates the lines for the SO, and since we have a
    `default_task_id` in the context, the related task of the new line
    is set to the task we came from. This prevent subsequently the
    creation of a task if the service was supposed to generate a new
    task on SO confirmation.
    
    Fix:
    Add an override when generating the data used to create the lines
    when the onchange is triggered, explicitly setting the `task_id` on
    the line to `False` if the key is in the context and the service
    would've generated a task.
    
    Affected versions:
    - 15.0
    - saas-15.2
    - 16.0
    - saas-16.1
    - saas-16.2
    - saas-16.3
    - master
    
    opw-3360403

--- addons/sale_project/tests/test_sale_project.py
+++ addons/sale_project/tests/test_sale_project.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo import Command
 from odoo.tests.common import TransactionCase, users
 
 
@@ -213,3 +214,44 @@ class TestSaleProject(TransactionCase):
         self.project_global.sale_line_id = sale_order_line
         sale_order.action_cancel()
         self.assertFalse(self.project_global.sale_line_id, "The project should not be linked to the SOL anymore")
+
+    def test_create_task_from_template_line(self):
+        """
+        When we add an SOL from a template that is a service that has a service_policy that will generate a task,
+        even if default_task_id is present in the context, a new task should be created when confirming the SO.
+        """
+        default_task = self.env['project.task'].with_context(tracking_disable=True).create({
+            'name': 'Task',
+            'project_id': self.project_global.id
+        })
+        sale_order = self.env['sale.order'].with_context(tracking_disable=True, default_task_id=default_task.id).create({
+            'partner_id': self.partner.id,
+        })
+        quotation_template = self.env['sale.order.template'].create({
+            'name': 'Test quotation',
+        })
+        quotation_template.write({
+            'sale_order_template_line_ids': [
+                Command.set(
+                    self.env['sale.order.template.line'].create([{
+                     'name': self.product_order_service2.display_name,
+                     'sale_order_template_id': quotation_template.id,
+                     'product_id': self.product_order_service2.id,
+                     'product_uom_id': self.product_order_service2.uom_id.id,
+                    }, {
+                     'name': self.product_order_service3.display_name,
+                     'sale_order_template_id': quotation_template.id,
+                     'product_id': self.product_order_service3.id,
+                     'product_uom_id': self.product_order_service3.uom_id.id,
+                    }]).ids
+                )
+            ]
+        })
+        sale_order.with_context(default_task_id=default_task.id).write({
+            'sale_order_template_id': quotation_template.id,
+        })
+        sale_order.with_context(default_task_id=default_task.id).onchange_sale_order_template_id()
+        self.assertFalse(sale_order.order_line.mapped('task_id'), "SOL should have no related tasks, because they are from services that generates a task")
+        sale_order.action_confirm()
+        self.assertEqual(sale_order.tasks_count, 2, "SO should have 2 related tasks")
+        self.assertNotIn(default_task, sale_order.tasks_ids, "SO should link to the default task from the context")
