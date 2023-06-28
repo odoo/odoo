43c1d60ab586fefdbcfb41771ab899d9402b4f70
Pratik Awasthi <praw@odoo.com>
2023-06-28 06:36:55 +0000

    [FIX] project: fix project tour description
    Steps:
    
    - Install project
    - run project tour
    
    Issue:
    
    - no description while adding the column
    
    Cause:
    
    - the description was not present in the project tour for column
    
    Fix:
    
    - added description for adding a column in the tour
    
    task-3388985
    
    closes odoo/odoo#132997
    
    X-original-commit: a3f12bcebf66e57212ce0e6d4b46f986ae6c9c33
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/static/src/js/tours/project.js
+++ addons/project/static/src/js/tours/project.js
@@ -46,6 +46,7 @@ registry.category("web_tour.tours").add('project_tour', {
     run: "text Test",
 }, {
     trigger: ".o_kanban_project_tasks .o_column_quick_create .o_kanban_add",
+    content: Markup(_t('Let\'s create your first <b>stage</b>.')),
     position: 'right',
 }, {
     trigger: ".o_kanban_project_tasks .o_column_quick_create .input-group .o_input",
@@ -55,6 +56,7 @@ registry.category("web_tour.tours").add('project_tour', {
     run: "text Test",
 }, {
     trigger: ".o_kanban_project_tasks .o_column_quick_create .o_kanban_add",
+    content: Markup(_t('Let\'s create your second <b>stage</b>.')),
     position: 'right',
 }, {
     trigger: '.o-kanban-button-new',
