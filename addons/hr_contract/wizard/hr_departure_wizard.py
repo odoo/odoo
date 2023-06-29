8305ec0834b138eeb40519aa25442dbd9f13255d
kusi-odoo <kusi@odoo.com>
2023-06-29 09:37:35 +0000

    [FIX] hr, hr_contract: set contract end date when we archive the employee
    1) If the contract is running when the employee is archived, we move the contract in the
       expired stage and we set the date of the archive wizard to the
       contract's end date.
    
    2) If the contract has expired when the employee is archived, do not change the date.
    
    task-3373275
    
    closes odoo/odoo#130662
    
    X-original-commit: e84106cdc5ae8015ee0c15a6c842d91dc2da1343
    Signed-off-by: Sofie Gvaladze (sgv) <sgv@odoo.com>
    Signed-off-by: Kuldeep Singh (kusi) <kusi@odoo.com>

--- addons/hr_contract/wizard/hr_departure_wizard.py
+++ addons/hr_contract/wizard/hr_departure_wizard.py
@@ -8,6 +8,12 @@ from odoo.exceptions import UserError
 class HrDepartureWizard(models.TransientModel):
     _inherit = 'hr.departure.wizard'
 
+    def _get_employee_departure_date(self):
+        expired_contract = self.env['hr.contract'].search([('employee_id', '=', self.env.context['active_id']), ('state', '=', 'close')], limit=1, order='date_end desc')
+        if expired_contract:
+            return expired_contract.date_end
+        return super()._get_employee_departure_date()
+
     set_date_end = fields.Boolean(string="Set Contract End Date", default=lambda self: self.env.user.user_has_groups('hr_contract.group_hr_contract_manager'),
         help="Set the end date on the current contract.")
 
@@ -21,5 +27,7 @@ class HrDepartureWizard(models.TransientModel):
         super(HrDepartureWizard, self).action_register_departure()
         if self.set_date_end:
             self.sudo().employee_id.contract_ids.filtered(lambda c: c.state == 'draft').write({'state': 'cancel'})
-            if current_contract:
+            if current_contract and current_contract.state in ['open', 'draft']:
                 self.sudo().employee_id.contract_id.write({'date_end': self.departure_date})
+            if current_contract.state == 'open':
+                current_contract.state = 'close'
