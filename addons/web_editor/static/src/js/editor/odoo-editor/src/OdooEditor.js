b6f103bd3e2345cbd44254714489a989ad5a72ed
abd-msyukyu-odoo <abd@odoo.com>
2023-06-29 17:52:49 +0000

    [IMP] web_editor: render templates insertion collaborative-friendly
    In Knowledge, there is a custom `KnowledgeHtmlField` which adds a menu
    placeholder if the `HtmlField` value is empty. It evaluates if it is empty after
    each `historyStep`.
    
    This commit introduces new collaborative step events so that the
    `KnowledgeHtmlField` is able to properly hide/show that menu even after a
    collaborator made the value empty.
    
    task-3378266
    
    Part-of: odoo/odoo#127935

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -1105,6 +1105,7 @@ export class OdooEditor extends EventTarget {
         this._handleCommandHint();
         this.multiselectionRefresh();
         this.observerActive();
+        this.dispatchEvent(new Event('historyResetFromSteps'));
     }
     historyGetMissingSteps({fromStepId, toStepId}) {
         const fromIndex = this._historySteps.findIndex(x => x.id === fromStepId);
@@ -1606,6 +1607,7 @@ export class OdooEditor extends EventTarget {
         this.historyResetLatestComputedSelection();
         this._handleCommandHint();
         this.multiselectionRefresh();
+        this.dispatchEvent(new Event('onExternalHistorySteps'));
     }
 
     // Multi selection
