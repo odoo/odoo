a68cf4807f362f2ea0dc0de3916bf3b4b658ddf5
Didier (did) <did@odoo.com>
2023-06-29 10:28:23 +0200

    [FIX] im_livechat: archived chatbot not active on the frontend
    task-3374783

--- addons/im_livechat/controllers/main.py
+++ addons/im_livechat/controllers/main.py
@@ -72,7 +72,7 @@ class LivechatController(http.Controller):
                 'auto_popup_timer': matching_rule.auto_popup_timer,
                 'regex_url': matching_rule.regex_url,
             }
-            if matching_rule.chatbot_script_id and (not matching_rule.chatbot_only_if_no_operator or
+            if matching_rule.chatbot_script_id.active and (not matching_rule.chatbot_only_if_no_operator or
                (not operator_available and matching_rule.chatbot_only_if_no_operator)) and matching_rule.chatbot_script_id.script_step_ids:
                 chatbot_script = matching_rule.chatbot_script_id
                 rule.update({'chatbot': chatbot_script._format_for_frontend()})
