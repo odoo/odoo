b9e98fd9a4470a9fbcf9a007d03e5b113169f30b
Sven Fuehr <svfu@odoo.com>
2023-06-30 16:20:12 +0200

    [FIX] account: fixes payment to self with early payment discount
    This commit fixes a bug (corner case) concerning payments
    to yourself where an early payment discount is applied.
    
    Details / steps to reproduce the bug:
        1. Create an invoice
        2. Select your own (currently selected) company as customer
        3. Select some payment term with early payment discount (e.g. '2/7 Net 30')
        4. Confirm the invoice.
        5. Click "Register Payment" and keep the default values (i.e. "Mark as fully paid" should be selected)
        6. Click "Create Payment"
        7. A User Error like the following should appear (with different journal entry names):
           "Journal Entry Draft Entry PBNK1/2023/00001 (INV/2023/00005) is not valid. In order to proceed, the journal items must include one and only one receivable/payable account (with an exception of internal transfers)."
    
    Why the bug happens:
    The early payment discount adds some extra lines to the journal entry associated with the payment.
    Currently those extra lines are classified as "counterpart lines" since the customer is our own company.
    But there must be exactly 1 "counterpart line".
    
    How the bug is fixed:
    The condition that classifies the extra lines as "counterpart lines" was only intended for internal transfers.
    Each company has a related "transfer account" that is used as an intermediary account for internal transfers.
    Thus the old condition (c.f. commit diff) when checking whether a line is a "counterpart line"
    can be replaced by checking whether the account associated with the line is the "transfer account" of our company.
    
    task-3388294
    
    X-original-commit: b1536ecb6bc94a2bd5ff3bdf1b73289ca87a744d

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -184,7 +184,7 @@ class AccountPayment(models.Model):
         for line in self.move_id.line_ids:
             if line.account_id in self._get_valid_liquidity_accounts():
                 liquidity_lines += line
-            elif line.account_id.account_type in ('asset_receivable', 'liability_payable') or line.partner_id == line.company_id.partner_id:
+            elif line.account_id.account_type in ('asset_receivable', 'liability_payable') or line.account_id == self.company_id.transfer_account_id:
                 counterpart_lines += line
             else:
                 writeoff_lines += line
