86b7d2c55ec34282a7f2e6dc84f25453cfd7e082
Wolfgang Taferner <wolfgang.taferner@wt-io-it.at>
2023-07-01 16:44:21 +0200

    [FIX] account: import of an init class was not working
    a88027af5f5eb5d0efb4d2c42d5a179aa9bd2dc1 introduced a failed import
    for the LegacyHTTPAdapter. Move the class into an own file and
    import initialize.
    
    Closes https://github.com/odoo/odoo/issues/126998

--- addons/account/tools/__init__.py
+++ addons/account/tools/__init__.py
@@ -1,6 +1,5 @@
 from itertools import zip_longest
-import requests
-from urllib3.util.ssl_ import create_urllib3_context
+from .legacy_http_adapter import *
 
 
 def calc_check_digits(number: str) -> str:
@@ -23,18 +22,3 @@ def format_rf_reference(number: str) -> str:
         check_digits,
         " ".join("".join(x) for x in zip_longest(*[iter(str(number))]*4, fillvalue=""))
     )
-
-
-class LegacyHTTPAdapter(requests.adapters.HTTPAdapter):
-    """ An adapter to allow unsafe legacy renegotiation necessary to connect to
-    gravely outdated ETA production servers.
-    """
-
-    def init_poolmanager(self, *args, **kwargs):
-        # This is not defined before Python 3.12
-        # cfr. https://github.com/python/cpython/pull/93927
-        # Origin: https://github.com/openssl/openssl/commit/ef51b4b9
-        OP_LEGACY_SERVER_CONNECT = 0x04
-        context = create_urllib3_context(options=OP_LEGACY_SERVER_CONNECT)
-        kwargs["ssl_context"] = context
-        return super().init_poolmanager(*args, **kwargs)

odoo/odoo

commit a88027af5f5eb5d0efb4d2c42d5a179aa9bd2dc1

[IMP] account, l10n_eg_edi_eta: moved L10nEgHTTPAdapter to account
For task-3347812, we needed to create the same Adapter used in l10n_eg_edi_eta. Moving the adapter to account.tools allows us to reuse the same class in different modules.

closes odoo/odoo#125161

Related: odoo/enterprise#40936
Signed-off-by: Quentin De Paoli <qdp@odoo.com>

addons/account/tools/__init__.py
@@ -1,4 +1,6 @@
 from itertools import zip_longest
+import requests
+from urllib3.util.ssl_ import create_urllib3_context
 
 
 def calc_check_digits(number: str) -> str:
@@ -21,3 +23,18 @@ def format_rf_reference(number: str) -> str:
         check_digits,
         " ".join("".join(x) for x in zip_longest(*[iter(str(number))]*4, fillvalue=""))
     )
+
+
+class LegacyHTTPAdapter(requests.adapters.HTTPAdapter):
+    """ An adapter to allow unsafe legacy renegotiation necessary to connect to
+    gravely outdated ETA production servers.
+    """
+
+    def init_poolmanager(self, *args, **kwargs):
+        # This is not defined before Python 3.12
+        # cfr. https://github.com/python/cpython/pull/93927
+        # Origin: https://github.com/openssl/openssl/commit/ef51b4b9
+        OP_LEGACY_SERVER_CONNECT = 0x04
+        context = create_urllib3_context(options=OP_LEGACY_SERVER_CONNECT)
+        kwargs["ssl_context"] = context
+        return super().init_poolmanager(*args, **kwargs)

addons/l10n_eg_edi_eta/models/account_edi_format.py
@@ -7,7 +7,7 @@ import logging
 import requests
 from werkzeug.urls import url_quote
 from base64 import b64encode
-from urllib3.util.ssl_ import create_urllib3_context
+from odoo.addons.account.tools import LegacyHTTPAdapter
 
 from odoo import api, models, _
 from odoo.tools.float_utils import json_float_round
@@ -23,21 +23,6 @@ ETA_DOMAINS = {
     'token.preproduction': 'https://id.preprod.eta.gov.eg',
     'token.production': 'https://id.eta.gov.eg',
 }
-
-
-class L10nEgHTTPAdapter(requests.adapters.HTTPAdapter):
-    """ An adapter to allow unsafe legacy renegotiation necessary to connect to
-    gravely outdated ETA production servers.
-    """
-
-    def init_poolmanager(self, *args, **kwargs):
-        # This is not defined before Python 3.12
-        # cfr. https://github.com/python/cpython/pull/93927
-        # Origin: https://github.com/openssl/openssl/commit/ef51b4b9
-        OP_LEGACY_SERVER_CONNECT = 0x04
-        context = create_urllib3_context(options=OP_LEGACY_SERVER_CONNECT)
-        kwargs["ssl_context"] = context
-        return super().init_poolmanager(*args, **kwargs)
 
 
 class AccountEdiFormat(models.Model):
@@ -61,7 +46,7 @@ class AccountEdiFormat(models.Model):
         request_url = api_domain + request_url
         try:
             session = requests.session()
-            session.mount("https://", L10nEgHTTPAdapter())
+            session.mount("https://", LegacyHTTPAdapter())
             request_response = session.request(method, request_url, data=request_data.get('body'), headers=request_data.get('header'), timeout=(5, 10))
         except (ValueError, requests.exceptions.ConnectionError, requests.exceptions.MissingSchema, requests.exceptions.Timeout, requests.exceptions.HTTPError) as ex:
             return {

commit 86b7d2c55ec34282a7f2e6dc84f25453cfd7e082

[FIX] account: import of an init class was not working
a88027af5f5eb5d0efb4d2c42d5a179aa9bd2dc1 introduced a failed import
for the LegacyHTTPAdapter. Move the class into an own file and
import initialize.

Closes https://github.com/odoo/odoo/issues/126998

addons/account/tools/__init__.py
@@ -1,6 +1,5 @@
 from itertools import zip_longest
-import requests
-from urllib3.util.ssl_ import create_urllib3_context
+from .legacy_http_adapter import *
 
 
 def calc_check_digits(number: str) -> str:
@@ -23,18 +22,3 @@ def format_rf_reference(number: str) -> str:
         check_digits,
         " ".join("".join(x) for x in zip_longest(*[iter(str(number))]*4, fillvalue=""))
     )
-
-
-class LegacyHTTPAdapter(requests.adapters.HTTPAdapter):
-    """ An adapter to allow unsafe legacy renegotiation necessary to connect to
-    gravely outdated ETA production servers.
-    """
-
-    def init_poolmanager(self, *args, **kwargs):
-        # This is not defined before Python 3.12
-        # cfr. https://github.com/python/cpython/pull/93927
-        # Origin: https://github.com/openssl/openssl/commit/ef51b4b9
-        OP_LEGACY_SERVER_CONNECT = 0x04
-        context = create_urllib3_context(options=OP_LEGACY_SERVER_CONNECT)
-        kwargs["ssl_context"] = context
-        return super().init_poolmanager(*args, **kwargs)

addons/account/tools/legacy_http_adapter.py
@@ -1,0 +1,16 @@
+import requests
+from urllib3.util.ssl_ import create_urllib3_context
+
+class LegacyHTTPAdapter(requests.adapters.HTTPAdapter):
+    """ An adapter to allow unsafe legacy renegotiation necessary to connect to
+    gravely outdated ETA production servers.
+    """
+
+    def init_poolmanager(self, *args, **kwargs):
+        # This is not defined before Python 3.12
+        # cfr. https://github.com/python/cpython/pull/93927
+        # Origin: https://github.com/openssl/openssl/commit/ef51b4b9
+        OP_LEGACY_SERVER_CONNECT = 0x04
+        context = create_urllib3_context(options=OP_LEGACY_SERVER_CONNECT)
+        kwargs["ssl_context"] = context
+        return super().init_poolmanager(*args, **kwargs)