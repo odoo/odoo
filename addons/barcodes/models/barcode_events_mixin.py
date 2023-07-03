3fd156bd6cc624cbee73190bb31554c6387df58a
arpr-odoo <arpr@odoo.com>
2023-07-03 06:18:38 +0000

    [IMP] generic: make missing translatable terms translatable
    In this commit, we have addressed a crucial issue related to
    translation. We discovered that certain key
    terms in our application were not properly marked for
    translation, resulting in a lack of support for different
    languages. To rectify this, we have implemented the necessary
    changes to make these terms translatable. By doing so, we have
    successfully resolved the language compatibility problem and
    improved the overall internationalization of our application.
    This commit ensures that all terms, previously overlooked or
    untranslatable, are now fully accessible for translation,
    enhancing the localization experience for our users across the globe.
    
    task:3358438
    
    closes odoo/odoo#212518
    
    X-original-commit: cf8870085085cc04285d6516b45efcbcfe6a1fd3
    Related: odoo/enterprise#86761
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/barcodes/models/barcode_events_mixin.py
+++ addons/barcodes/models/barcode_events_mixin.py
@@ -23,4 +23,4 @@ class BarcodesBarcode_Events_Mixin(models.AbstractModel):
             return self.on_barcode_scanned(barcode)
 
     def on_barcode_scanned(self, barcode):
-        raise NotImplementedError("In order to use barcodes.barcode_events_mixin, method on_barcode_scanned must be implemented")
+        raise NotImplementedError(self.env._("In order to use barcodes.barcode_events_mixin, method on_barcode_scanned must be implemented"))
