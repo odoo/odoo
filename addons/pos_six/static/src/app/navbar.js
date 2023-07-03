bd1331f81e09ae61537b931f47ecb46e141b6ba0
vlst <vlst@odoo.com>
2023-07-03 09:09:39 +0000

    [REF] pos*: structure static files
    pos*: l10n_ae_pos,l10n_co_pos,l10n_gcc_pos,l10n_in_pos,
    l10n_sa_pos,pos_adyen,pos_discount,pos_epson_printer,
    pos_hr,pos_hr_restaurant,pos_loyalty,pos_mercury,
    pos_restaurant,pos_restaurant_adyen,pos_restaurant_stripe,pos_sale,
    pos_sale_loyalty,pos_sale_product_configurator,pos_six,pos_stripe,
    
    The POS has a large number of add-on modules. As such, it is
    important that they all respect strict guidelines when it comes
    to file structures.
    
    Before, each module had more or less it's own rules when it came to
    this, apart from the fact that code was mostly organised into
    `js` and `xml` folders.
    
    In this PR, we move to a much more structured approach,
    in which each module follows the following pattern:
    
    src/
    |-- app/  <-- the module's own new logic
    |-- overrides/
    |   |-- components
    |	|   |-- overridden_component_a
    |	|	|   |-- overridden_component_a.js
    |	|	|   |-- overridden_component_a.xml
    |	|	|   |-- overridden_component_a.css
    |   |-- models
    |	|   |-- overridden_model.js
    
    Apart from the structural changes, this PR introduces changes
    to file names, such that they respect the default pattern of
    snake_case naming.
    
    In this commit, we move the files. In the following one we
    make the needed changes in the code, such as adapting imports.
    
    In order to efficiently accomplish this task, i used the following [script](https://github.com/vlst-odoo/tools/blob/main/rename.py)
    
    Task: 3394192
    Part-of: odoo/odoo#127056

--- addons/pos_six/static/src/app/navbar.js
+++
@@ -1,9 +0,0 @@
-/** @odoo-module */
-
-import { Navbar } from "@point_of_sale/app/navbar/navbar";
-import { BalanceButton } from "@pos_six/js/BalanceButton";
-import { patch } from "@web/core/utils/patch";
-
-patch(Navbar, "pos_six.Navbar", {
-    components: { ...Navbar.components, BalanceButton },
-});
