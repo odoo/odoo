01717dc71bae261dad8ad6515ff2b1e2be861295
Alvaro Fuentes <afu@odoo.com>
2023-07-04 11:37:45 +0000

    [FIX] test_main_flows: skip broken tests in 11.0
    closes odoo/odoo#127276
    
    Related: odoo/enterprise#43420
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- odoo/addons/test_main_flows/tests/test_flow.py
+++ odoo/addons/test_main_flows/tests/test_flow.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import unittest
+
 import odoo.tests
 
 
@@ -7,5 +9,6 @@ import odoo.tests
 @odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
     
+    @unittest.skip('broken, unmaintained for 11.0')
     def test_01_main_flow_tour(self):
         self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('main_flow_tour')", "odoo.__DEBUG__.services['web_tour.tour'].tours.main_flow_tour.ready", login="admin", timeout=180)
