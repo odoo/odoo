e2d6832e0ce591ba2da744b0143fa7a3fc937de3
Romain Derie <rde@odoo.com>
2023-07-04 13:06:42 +0000

    [IMP] website: use website domain in sitemap instead of current url
    This commit is:
    1. Introducing a cache per domain for sitemaps. There were already a
       different sitemap per website but it was not enough, as it was cached
       for 12 hours, the domain which would be used to compute the sitemap
       would be the one used to construct the URLs. But that domain could be
       the non-canonical one.
       It's not the end of the world as those non canonical pages have a
       canonical meta tag anyway to correctly "explain" to the crawler
       where to go, but always using the correct domain is better.
    2. Adding link to the canonical sitemap on the robots.txt page if we are
       not on the canonical robots.txt.
       Note that this robots.txt was already forbidding all pages with
       `Disallow: /`.
       This is a good practice, see this google doc [1]:
       `The URL doesn't have to be on the same host as the robots.txt file.`
    3. Using the website domain (if set) to generate the opengraph links.
       This one is probably just nitpicking and to prevent bad copy paste
       as it doesn't really change anything.
    
    Step to reproduce:
    - Install website
    - Set `http://localhost:8069/` as website domain
    - Go to `http://127.0.0.1:8069/sitemap.xml` (it's cached for 12 hours
      once you generated it by accessing it the first time)
    - Go to `http://localhost:8069/sitemap.xml`, it will be displaying pages
      with `127.0.0.1` URL instead of the canonical localhost.
    
    Note that while it seems like useless code, since there is no real bug
    or real SEO issue, it's still a low effort to fix a real problem from
    our client perspective: they think it's wrong and open ticket.
    On top of not being happy, they make our support/bugfix waste time since
    they don't know exactly what to do / answer and will tell client to
    check with their registrar etc.. Worst they (client or support) sometime
    hardcode the domain in the sitemap xml view..
    
    Also note that sitemap index, when the sitemap is split in multiple
    files because it's too long, can only contain (sub-sitemap) URLs from
    the same domain as itself. See [2].
    ```
    The referenced sitemaps must be hosted on the same site as your sitemap
    index file.
    ```
    
    Finally, note that as opposed to sitemap index as explained just above,
    sitemap URLs can be from another domain.
    So we could have forced the website domain if set regardless of the
    domain it is generated from, but:
    1. It would be more risky, having different domains when you have a
       sitemap index (as the index would contains sub-sitemap URL on the
       same domain but those sub-sitemap would contains URL on the canonical
       domain)
    2. Having the sitemap generating URLs from the current visited domain
       should remove all source of confusion anyway.
    
    [1]: https://developers.google.com/search/docs/crawling-indexing/robots/robots_txt?hl=en#sitemap
    [2]: https://developers.google.com/search/docs/crawling-indexing/sitemaps/large-sitemaps?hl=en
    
    task-3391021
    opw-3366013
    opw-2311003
    opw-2358120
    opw-3105191
    opw-2949165
    opw-3341083
    opw-3103527
    opw-3075342
    opw-2962433
    opw-2718606
    opw-2674387
    opw-2647926
    opw-3163677
    opw-3114853
    
    closes odoo/odoo#164111
    
    X-original-commit: ab505557ba4d690e653d5952caff0e2cf979618e
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website/models/mixins.py
+++ addons/website/models/mixins.py
@@ -56,7 +56,7 @@ class SeoMetadata(models.AbstractModel):
             'og:type': 'website',
             'og:title': title,
             'og:site_name': request.website.name,
-            'og:url': url_join(request.httprequest.url_root, url_for(request.httprequest.path)),
+            'og:url': url_join(request.website.domain or request.httprequest.url_root, url_for(request.httprequest.path)),
             'og:image': request.website.image_url(request.website, img_field),
         }
         # Default meta for Twitter
@@ -82,7 +82,7 @@ class SeoMetadata(models.AbstractModel):
             override `_default_website_meta` method instead of this method. This
             method only replaces user custom values in defaults.
         """
-        root_url = request.httprequest.url_root.strip('/')
+        root_url = request.website.domain or request.httprequest.url_root.strip('/')
         default_meta = self._default_website_meta()
         opengraph_meta, twitter_meta = default_meta['default_opengraph'], default_meta['default_twitter']
         if self.website_meta_title:
