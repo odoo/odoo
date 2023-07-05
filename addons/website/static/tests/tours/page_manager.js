f96e3244acac8ad0af296ef430953b06f6db29de
Benoit Socias <bso@odoo.com>
2023-07-05 14:29:56 +0200

    [FIX] website: wait for OK button to be available in page manager tour
    Since [1] the page manager tour test fails from time to time because it
    manages to click on the Delete Page popup's OK button before it is
    enabled.
    
    This commit makes sure it only clicks on the button once it is enabled.
    
    [1]: https://github.com/odoo/odoo/commit/a65d8811119f4d1a044be3e140fd0c27ad1cf823
    
    runbot-20823
    runbot-23032

--- addons/website/static/tests/tours/page_manager.js
+++ addons/website/static/tests/tours/page_manager.js
@@ -71,7 +71,7 @@ const deleteSelectedPage = [
     },
     {
         content: "Click on OK",
-        trigger: '.modal-content footer button.btn-primary',
+        trigger: '.modal-content footer button.btn-primary:not([disabled])',
     }
 ];
 const homePage = 'tr:contains("Home")';
