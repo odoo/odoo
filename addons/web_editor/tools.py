ca9c8c4fa960ed23c2b0ac01462dbf9d506742a1
Julien Banken <jbn@odoo.com>
2023-07-05 14:22:56 +0200

    [FIX] web_editor: video selector component adjustments for Knowledge
    This commit will apply the following changes:
    1. Introduce a new `VideoIframe` component for the `VideoSelector`
       component. With that change, one can easily mock the `VideoIframe`
       component in a tour and replace the iframe of the video with
       alternative content. It will then be possible to test the whole video
       integration flow with a tour without relying on Third-Party services.
    2. Enhance the Python video parser to include the video id and the video
       parameters of the url being parsed. These new information will be
       used for the new video behavior of Knowledge.
    3. Call the callback function notifying that the video url has changed
       when the textarea is emptied (see: `selectMedia`).
    4. Authorize non-internal users to call the /web_editor/video_url/data
       route. This will ensure that portal users will be able to integrate
       videos in their Knowledge articles.
    5. Apply some minor UI and UX adjustments:
       - The label next to the toggle buttons will now be properly aligned.
       - The textarea to input the video url will be automatically focused.
    
    task-3297215

--- addons/web_editor/tools.py
+++ addons/web_editor/tools.py
@@ -52,7 +52,7 @@ def get_video_source_data(video_url):
 
 
 def get_video_url_data(video_url, autoplay=False, loop=False, hide_controls=False, hide_fullscreen=False, hide_yt_logo=False, hide_dm_logo=False, hide_dm_share=False):
-    """ Computes the platform name and embed_url from given URL
+    """ Computes the platform name, the embed_url, the video id and the video params of the given URL
         (or error message in case of invalid URL).
     """
     source = get_video_source_data(video_url)
@@ -115,7 +115,12 @@ def get_video_url_data(video_url, autoplay=False, loop=False, hide_controls=Fals
     if params:
         embed_url = f'{embed_url}?{url_encode(params)}'
 
-    return {'platform': platform, 'embed_url': embed_url}
+    return {
+        'platform': platform,
+        'embed_url': embed_url,
+        'video_id': video_id,
+        'params': params
+    }
 
 
 def get_video_embed_code(video_url):
