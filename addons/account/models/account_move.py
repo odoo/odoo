fdde02b2adbfee86e6280dd2e29b854f2a9ed267
Alpesh Valaki <alpeshvalaki@gmail.com>
2023-07-05 17:46:58 +0530

    [ADD] account: acced mssing float_round import

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -2,7 +2,7 @@
 
 from odoo import api, fields, models, _
 from odoo.exceptions import RedirectWarning, UserError, ValidationError, AccessError
-from odoo.tools import float_compare, date_utils, email_split, email_re, float_is_zero
+from odoo.tools import float_compare, date_utils, email_split, email_re, float_is_zero, float_round
 from odoo.tools.misc import formatLang, format_date, get_lang
 from odoo.osv import expression
 
