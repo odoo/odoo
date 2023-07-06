fa3714e0353273d7fb8cb79e80d4412dba53df96
Sébastien Theys <seb@odoo.com>
2023-07-06 17:24:58 +0200

    [FIX] mail: standardize way to get guest from context
    Backport of `_get_guest_from_context`.
    
    Prevent crash when unexpected (not recordset) values are in the context.
    
    Ensure the mere existence of a value (example integer) does not lead to
    executing flows where an actual guest is expected.
    
    task-2819597
    
    X-original-commit: 9a47101964a56fe5a7a44da37209e8f67b508395

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -1976,8 +1976,9 @@ class MailThread(models.AbstractModel):
         self = self._fallback_lang() # add lang to context immediately since it will be useful in various flows latter.
 
         # Find the message's author
-        if self.env.user._is_public() and 'guest' in self.env.context:
-            author_guest_id = self.env.context['guest'].id
+        guest = self.env['mail.guest']._get_guest_from_context()
+        if self.env.user._is_public() and guest:
+            author_guest_id = guest.id
             author_id, email_from = False, False
         else:
             author_guest_id = False
