f64c40d677885f615f51d8171c4ff58b33b9d840
Alexandre Kühn <aku@odoo.com>
2023-07-06 09:03:58 +0000

    [FIX] im_livechat: open profile on clicking on avatar in livechat
    Before this commit, when clicking on avatar of author of an
    authenticated visitor in a livechat, it was opening the chat with
    this user.
    
    The intended behaviour is to show the profile, as it's quite useful
    for livechat. Opening a DM chat is useless, because the livechat
    already represents a chat with the person
    
    closes odoo/odoo#127615
    
    X-original-commit: 4eedc234fe19ab681c6a35c2265e9763b2a80760
    Signed-off-by: Didier Debondt (did) <did@odoo.com>
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

---
+++ addons/im_livechat/static/tests/message_patch_tests.js
@@ -0,0 +1,28 @@
+/** @odoo-module */
+
+import { start, startServer } from "@mail/../tests/helpers/test_utils";
+import { Command } from "@mail/../tests/helpers/command";
+
+QUnit.module("message (patch)");
+
+QUnit.test("redirect to author (open profile) in livechat", async (assert) => {
+    const pyEnv = await startServer();
+    const partnerId = pyEnv["res.partner"].create({ name: "Demo" });
+    pyEnv["res.users"].create({ partner_id: partnerId });
+    const channelId = pyEnv["discuss.channel"].create({
+        channel_member_ids: [
+            Command.create({ partner_id: pyEnv.currentPartnerId }),
+            Command.create({ partner_id: partnerId }),
+        ],
+        channel_type: "livechat",
+    });
+    pyEnv["mail.message"].create({
+        author_id: partnerId,
+        body: "not empty",
+        model: "discuss.channel",
+        res_id: channelId,
+    });
+    const { openDiscuss } = await start();
+    await openDiscuss(channelId);
+    assert.containsOnce($, ".o-mail-Message-avatarContainer[title='Open profile']");
+});
