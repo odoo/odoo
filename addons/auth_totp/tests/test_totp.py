e917c8d9410111f10e5560dcd02daacec6d76551
Julien Castiaux <juc@odoo.com>
2023-07-07 11:11:37 +0200

    [FIX] auth_totp: session race condition

--- addons/auth_totp/tests/test_totp.py
+++ addons/auth_totp/tests/test_totp.py
@@ -1,16 +1,20 @@
+import base64
 import json
+import logging
+import os
 import time
+import threading
 from xmlrpc.client import Fault
 
 from passlib.totp import TOTP
 
 from odoo import http
-from odoo.exceptions import AccessDenied
-from odoo.service import common as auth, model
 from odoo.tests import tagged, HttpCase, get_db_name
 from odoo.tools import mute_logger
 
 from ..controllers.home import Home
+
+_logger = logging.getLogger(__name__)
 
 @tagged('post_install', '-at_install')
 class TestTOTP(HttpCase):
@@ -114,3 +118,96 @@ class TestTOTP(HttpCase):
         response = self.url_open("/web/session/authenticate", data=json.dumps(payload), headers=headers)
         data = response.json()
         self.assertEqual(data['result']['uid'], None)
+
+
+@tagged('-at_install', 'post_install')
+class TestTOTPConcurrent(HttpCase):
+    def test_totp_concurrent_session_expired(self):
+        ct_json = {'Content-Type': 'application/json'}
+        jsonrpc = {
+            'jsonrpc': '2.0',
+            'method': 'call',
+            'id': 0,
+        }
+        session = self.authenticate('demo', 'demo')
+        session['identity-check-last'] = time.time()  # needed by check_identity
+        http.root.session_store.save(session)
+
+
+        # Setup
+        #
+        # Both requests get the same session then /web/session/check
+        # waits for /web/dataset/call_kw to finish
+        barrier = threading.Barrier(2)
+        event = threading.Event()
+
+        _post_init = http.Request._post_init
+        def sync_post_init(self):
+            _post_init(self)
+            _logger.info("%s %s", self.httprequest.path, "waits at barrier")
+            barrier.wait(timeout=1)
+            _logger.info("%s %s", self.httprequest.path, "resumes from barrier")
+            if self.httprequest.path == '/web/session/check':
+                _logger.info("%s %s", self.httprequest.path, "waits at event")
+                event.wait(timeout=1)
+                _logger.info("%s %s", self.httprequest.path, "resume from event")
+        self.patch(http.Request, '_post_init', sync_post_init)
+
+        _totp_try_setting = self.env.registry['res.users']._totp_try_setting
+        def signal_totp_try_setting(*args, **kwargs):
+            res = _totp_try_setting(*args, **kwargs)
+            _logger.info("%s %s", http.request.httprequest.path, "sets event")
+            event.set()
+            return res
+        self.patch(self.env.registry['res.users'], '_totp_try_setting', signal_totp_try_setting)
+
+
+        # Test case
+        #
+        # Start both requests in their own thread so that the two run
+        # in parallel
+        check_session_res = None
+        def check_session():
+            nonlocal check_session_res
+            check_session_res = self.url_open('/web/session/check', data=json.dumps(jsonrpc), headers=ct_json)
+
+        totp_enable_res = None
+        def totp_enable():
+            nonlocal totp_enable_res
+            secret = base64.b32encode(os.urandom(20)).decode()
+            code = TOTP(secret).generate().token
+            auto_totp_wizard = self.env['auth_totp.wizard'].create([{
+                'user_id': session.uid,
+                'secret': secret,
+                'code': code,
+            }])
+            totp_enable_res = self.url_open(
+                '/web/dataset/call_kw',
+                data=json.dumps(jsonrpc | {'params': {
+                    'model': 'auth_totp.wizard',
+                    'method': 'enable',
+                    'args': [auto_totp_wizard.id],
+                    'kwargs': {},
+                }}),
+                headers=ct_json,
+            )
+            check_session_thread.join()
+
+        totp_enable_thread = threading.Thread(target=totp_enable)
+        check_session_thread = threading.Thread(target=check_session)
+        totp_enable_thread.start()
+        check_session_thread.start()
+        totp_enable_thread.join()
+        check_session_thread.join()
+
+        # Assertion
+        #
+        # Both request should succeed, the /web/session/check one
+        # shouldn't fail because of an expired session
+        check_session_res.raise_for_status()
+        if error := check_session_res.json().get('error'):
+            self.fail('\n' + error['data']['debug'])
+
+        totp_enable_res.raise_for_status()
+        if error := totp_enable_res.json().get('error'):
+            self.fail('\n' + error['data']['debug'])
