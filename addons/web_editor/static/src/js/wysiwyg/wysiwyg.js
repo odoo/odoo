915c2b1804fdf53534810ad6a353a7ca6ad04166
abd-msyukyu-odoo <abd@odoo.com>
2023-07-10 11:19:11 +0000

    [FIX] web_editor: make urgent commitChanges quasi synchronous
    In the context of an urgent htmlField `commitChanges`, there is no time to wait
    the response of `rpc` calls, therefore this commit proposes to use the same
    strategy as `_toInline` for `savePendingImages`  and not await the result of
    the promises before a first `updateValue` if the commit is `urgent`.
    This will allow to save most of the changes during a `beforeunload` scenario.
    
    ENTERPRISE PR: https://github.com/odoo/enterprise/pull/42644
    
    task-3372343
    
    X-original-commit: 9385db0cb6224583826ded326fa10c7b4db746b4

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -976,7 +976,6 @@ const Wysiwyg = Widget.extend({
             });
             const modifiedProms = [...editableEl.querySelectorAll('.o_modified_image_to_save')].map(async el => {
                 const isBackground = !el.matches('img');
-                el.classList.remove('o_modified_image_to_save');
                 // Modifying an image always creates a copy of the original, even if
                 // it was modified previously, as the other modified image may be used
                 // elsewhere if the snippet was duplicated or was saved as a custom one.
@@ -990,6 +989,7 @@ const Wysiwyg = Widget.extend({
                         name: (el.dataset.fileName ? el.dataset.fileName : null),
                     },
                 });
+                el.classList.remove('o_modified_image_to_save');
                 if (isBackground) {
                     const parts = weUtils.backgroundImageCssToParts($(el).css('background-image'));
                     parts.url = `url('${newAttachmentSrc}')`;
