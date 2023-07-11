2bfd3a7f8af8aa8b9f1ed83bb7b0368765aa798d
Géry Debongnie <ged@odoo.com>
2023-07-11 15:27:40 +0200

    [IMP] web: allow lang to be overriden by url query params
    With this commit, it is possible to load the web client in a language
    different than the user by using the `lang=` query string in the url:
    For example, `/web?lang=en_US`
    
    This is useful when we need to quickly check something, without changing
    the current user settings. Note that it will only work if the url lang
    has been installed on the database (if lang does not exist, it will fall
    back to en_us).
    
    TaskId: #3420252

--- addons/web/controllers/home.py
+++ addons/web/controllers/home.py
@@ -62,12 +62,16 @@ class Home(http.Controller):
             return request.redirect('/web/login?error=access')
 
     @http.route('/web/webclient/load_menus/<string:unique>', type='http', auth='user', methods=['GET'])
-    def web_load_menus(self, unique):
+    def web_load_menus(self, unique, lang=None):
         """
         Loads the menus for the webclient
         :param unique: this parameters is not used, but mandatory: it is used by the HTTP stack to make a unique request
+        :param lang: language in which the menus should be loaded (only works if language is installed)
         :return: the menus (including the images in Base64)
         """
+        if lang:
+            request.update_context(lang=lang)
+
         menus = request.env["ir.ui.menu"].load_web_menus(request.session.debug)
         body = json.dumps(menus, default=ustr)
         response = request.make_response(body, [
