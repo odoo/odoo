5a321de1a229325ec7091c42bb36f1460d488264
Valentin Vallaeys (vava) <vava@odoo.com>
2023-07-11 12:20:38 +0200

    [FIX] sale_stock: add dependency to warehouse_id for qty_at_date
    Part-of: odoo/odoo#117379

--- addons/sale_stock/models/sale_order_line.py
+++ addons/sale_stock/models/sale_order_line.py
@@ -43,7 +43,8 @@ class SaleOrderLine(models.Model):
 
     @api.depends(
         'product_id', 'customer_lead', 'product_uom_qty', 'product_uom', 'order_id.commitment_date',
-        'move_ids', 'move_ids.forecast_expected_date', 'move_ids.forecast_availability')
+        'move_ids', 'move_ids.forecast_expected_date', 'move_ids.forecast_availability',
+        'warehouse_id')
     def _compute_qty_at_date(self):
         """ Compute the quantity forecasted of product at delivery date. There are
         two cases:
