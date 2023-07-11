88b4938b7ce7ff2cf76f2381303da8bb62960e58
Jorge Pinna Puissant <jpp@odoo.com>
2023-07-11 13:58:11 +0000

    [FIX] web: one2many, await onchange when resequence
    - On an one2many;
    - drag and drop two records;
    - a resequence is called, with an onchange;
    - click save;
    
    Before this commit, if the network was a little too slow, the return of
    the onchange wasn't taken it into account for the save.
    
    Now, we await the onchange of the resequence before executing another
    action.
    
    Task 3179751
    
    Part-of: odoo/odoo#114024

--- addons/web/static/src/model/relational_model/static_list.js
+++ addons/web/static/src/model/relational_model/static_list.js
@@ -868,7 +868,7 @@ export class StaticList extends DataPoint {
         await Promise.all(proms);
 
         await this._sort();
-        this._onUpdate();
+        await this._onUpdate();
     }
 
     async _sort(currentIds = this.currentIds, orderBy = this.orderBy) {
