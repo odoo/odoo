b10e32e882b8d7e184740862a37504b607c1c77f
Rodolpho Lima <rcdl@odoo.com>
2023-07-12 10:57:10 +0200

    [IMP] web_editor: switch toolbar offset to the left
    Before this commit, the floating toolbar left border was positioned
    slightly offset to the right of the selection rectangle's border. As a
    result, when only a few characters were selected, the toolbar arrow
    would point to a character ahead (to the right) of the selected
    characters.
    
    This commit flips the offset to the left of the selection's left border
    instead, making sure the arrow points to the selection's right border
    even for a narrow selection range.
    
    task-3422372
    
    Part-of: odoo/odoo#136687

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -3305,7 +3305,7 @@ export class OdooEditor extends EventTarget {
             rangeRects.length > 1 && rangeRects.at(-1).bottom - rangeRects[0].bottom > BASELINE_MARGIN;
 
         // Get left position.
-        let left = correctedSelectionRect.left + OFFSET;
+        let left = correctedSelectionRect.left - OFFSET;
         // Ensure the toolbar doesn't overflow the editor on the left.
         left = Math.max(OFFSET, left);
         // Ensure the toolbar doesn't overflow the editor on the right.
