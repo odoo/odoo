1e1296274310a1f8834d0944154b33b42e08a53f
Didier (did) <did@odoo.com>
2023-07-12 13:15:17 +0000

    [FIX] mail: suggested recipients are checked by default in chatter
    Before this PR, suggested recipients without partner where not
    checked and needed to be manually created. This is prone to mistakes
    when sending an email to the suggested recipient.
    
    This PR checks all the suggested recipients by default and create
    the missing partners when a message is posted, and when full composer
    is open. That way, there's far less risk to not send the email to
    suggested recipient, and instead the user should manually uncheck
    to not send the email.
    
    closes odoo/odoo#128647
    
    X-original-commit: 258b2420676fb1a0d7e834ab79f6bf1ec0f73254
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/composer.js
+++ addons/mail/static/src/core/common/composer.js
@@ -99,9 +99,11 @@ export class Composer extends Component {
             );
         }
         this.messageService = useState(useService("mail.message"));
+        this.personaService = useState(useService("mail.persona"));
         /** @type {import("@mail/core/common/thread_service").ThreadService} */
         this.threadService = useService("mail.thread");
         this.ui = useState(useService("ui"));
+        this.rpc = useService("rpc");
         this.ref = useRef("textarea");
         this.fakeTextarea = useRef("fakeTextarea");
         this.state = useState({
@@ -429,6 +431,26 @@ export class Composer extends Component {
     }
 
     async onClickFullComposer(ev) {
+        if (this.props.type !== "note") {
+            // auto-create partners of checked suggested partners
+            const emailsWithoutPartners = this.props.composer.thread.suggestedRecipients
+                .filter((recipient) => recipient.checked && !recipient.persona)
+                .map((recipient) => recipient.email);
+            if (emailsWithoutPartners.length !== 0) {
+                const partners = await this.rpc("/mail/partner/from_email", {
+                    emails: emailsWithoutPartners,
+                });
+                for (const index in partners) {
+                    const partnerData = partners[index];
+                    const persona = this.personaService.insert({ ...partnerData, type: "partner" });
+                    const email = emailsWithoutPartners[index];
+                    const recipient = this.props.composer.thread.suggestedRecipients.find(
+                        (recipient) => recipient.email === email
+                    );
+                    Object.assign(recipient, { persona });
+                }
+            }
+        }
         const attachmentIds = this.props.composer.attachments.map((attachment) => attachment.id);
         const context = {
             default_attachment_ids: attachmentIds,
