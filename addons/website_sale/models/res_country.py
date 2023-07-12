8102c7fa61a5eada2caa534a97b437aa962f8490
Walid <waha@odoo.com>
2023-07-12 10:36:06 +0000

    [FIX] website_sale_delivery: correct adress states
    Steps to reproduce:
    - set shipping methods country to mexico (empty states)
    - fill an order on website on check out select seprate shipping/billing
    - on billing set country as US
    - on shipping the only country avialable is mexico
    - avialable states are the ones from the US not mexico
    
    Bug:
    the states are set from the country of the customer even though it isn't
    an option on any carrier.
    bug was introduced by this commit[1]
    
    Fix:
    revert the mentioned commit(1) since a new commit[2] was introduced that
    correctly handles that use case too
    
    [1]:https://github.com/odoo/odoo/commit/b729858b42b30ed34428054690c7d4d861092510
    [2]:https://github.com/odoo/odoo/commit/a668d4414064b7d8f403835856df25a4fe383ab4
    
    opw-3230361
    
    closes odoo/odoo#128349
    
    X-original-commit: c59bc765f186858bffb642987cd88fcb80fe262a
    Signed-off-by: Walid Hanniche (waha) <waha@odoo.com>
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/website_sale/models/res_country.py
+++ addons/website_sale/models/res_country.py
@@ -34,7 +34,5 @@ class ResCountry(models.Model):
                     states = res
                     break
                 states |= carrier.state_ids
-            if not states:
-                states = states.search([('country_id', '=', self.id)])
             res = res & states
         return res
