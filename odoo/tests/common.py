eeaf102b179f7cd36c637b5d4d68ce09484c4c2b
Xavier Morel <xmo@odoo.com>
2023-07-12 09:53:42 +0000

    [FIX] link_tracker: stop performing external HTTP calls
    Only tests tagged as external should actually call external
    sites. `TestMailRenderMixin` not only does that but it calls URLs
    which literally don't exist either.
    
    Patch `requests.Session.request` (via a utility added to odoo tests
    common) to disable that in the class.
    
    X-original-commit: 99878b1a975d6f45d52b6fe3dd480702a5d4ece5

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -37,7 +37,7 @@ except ImportError:
 from contextlib import contextmanager, ExitStack
 from datetime import datetime
 from itertools import zip_longest as izip_longest
-from unittest.mock import patch, _patch
+from unittest.mock import patch, _patch, Mock
 from xmlrpc import client as xmlrpclib
 
 import requests
@@ -673,6 +673,12 @@ class BaseCase(case.TestCase, metaclass=MetaCase):
             profile_session=self.profile_session,
             **kwargs)
 
+    def patch_requests(self):
+        # requests.get -> requests.api.request -> Session().request
+        # TBD: enable by default & set side_effect=NotImplementedError to force an error
+        p = patch('requests.Session.request', Mock(spec_set=[]))
+        self.addCleanup(p.stop)
+        return p.start()
 
 savepoint_seq = itertools.count()
 
