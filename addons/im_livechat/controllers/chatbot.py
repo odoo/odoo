535772eba9afa05b3a6edec4b204928007588f17
Louis Wicket (wil) <wil@odoo.com>
2023-07-14 13:20:20 +0000

    [FIX] im_livechat: use frontend language for welcome bot
    Translate Welcome Bot's messages according to the language of the
    frontend.
    
    X-original-commit: 0dec3d2a093f65c05ee3d2516dc8c895b1c5b673

--- addons/im_livechat/controllers/chatbot.py
+++ addons/im_livechat/controllers/chatbot.py
@@ -3,14 +3,14 @@
 
 from odoo import http
 from odoo.http import request
-from odoo.tools import is_html_empty, plaintext2html
+from odoo.tools import get_lang, is_html_empty, plaintext2html
 
 
 class LivechatChatbotScriptController(http.Controller):
     @http.route('/chatbot/restart', type="json", auth="public", cors="*")
     def chatbot_restart(self, channel_uuid, chatbot_script_id):
-
-        discuss_channel = request.env['discuss.channel'].sudo().search([('uuid', '=', channel_uuid)], limit=1)
+        chatbot_language = self._get_chatbot_language()
+        discuss_channel = request.env['discuss.channel'].sudo().with_context(lang=chatbot_language).search([('uuid', '=', channel_uuid)], limit=1)
         chatbot = request.env['chatbot.script'].browse(chatbot_script_id)
         if not discuss_channel or not chatbot.exists():
             return None
@@ -20,7 +20,8 @@ class LivechatChatbotScriptController(http.Controller):
     @http.route('/chatbot/post_welcome_steps', type="json", auth="public", cors="*")
     def chatbot_post_welcome_steps(self, channel_uuid, chatbot_script_id):
         discuss_channel = request.env['discuss.channel'].sudo().search([('uuid', '=', channel_uuid)], limit=1)
-        chatbot = request.env['chatbot.script'].sudo().browse(chatbot_script_id)
+        chatbot_language = self._get_chatbot_language()
+        chatbot = request.env['chatbot.script'].sudo().with_context(lang=chatbot_language).browse(chatbot_script_id)
         if not discuss_channel or not chatbot.exists():
             return None
 
@@ -43,7 +44,8 @@ class LivechatChatbotScriptController(http.Controller):
 
     @http.route('/chatbot/step/trigger', type="json", auth="public", cors="*")
     def chatbot_trigger_step(self, channel_uuid, chatbot_script_id=None):
-        discuss_channel = request.env['discuss.channel'].sudo().search([('uuid', '=', channel_uuid)], limit=1)
+        chatbot_language = self._get_chatbot_language()
+        discuss_channel = request.env['discuss.channel'].sudo().with_context(lang=chatbot_language).search([('uuid', '=', channel_uuid)], limit=1)
         if not discuss_channel:
             return None
 
@@ -58,7 +60,7 @@ class LivechatChatbotScriptController(http.Controller):
                 user_answer = user_messages.sorted(lambda message: message.id)[-1]
             next_step = discuss_channel.chatbot_current_step_id._process_answer(discuss_channel, user_answer.body)
         elif chatbot_script_id:  # when restarting, we don't have a "current step" -> set "next" as first step of the script
-            chatbot = request.env['chatbot.script'].sudo().browse(chatbot_script_id)
+            chatbot = request.env['chatbot.script'].sudo().with_context(lang=chatbot_language).browse(chatbot_script_id)
             if chatbot.exists():
                 next_step = chatbot.script_step_ids[:1]
 
@@ -102,3 +104,6 @@ class LivechatChatbotScriptController(http.Controller):
                 result['posted_message'] = result['posted_message'].message_format()[0]
 
         return result
+
+    def _get_chatbot_language(self):
+        return request.httprequest.cookies.get('frontend_lang', request.env.user.lang or get_lang(request.env).code)
