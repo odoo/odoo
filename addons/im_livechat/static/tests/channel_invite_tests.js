6652924c501547fa7e5c396fef20ecf22cc946a1
tsm-odoo <tsm@odoo.com>
2023-07-17 11:00:56 +0000

    [IMP] im_livechat, mail: add user from member list
    This PR focuses on partner invitations for livechat channels:
    - Add "invite a user" button to the member list
    - Sort partners by availibility (partner is a livechat operator
    and connected) then most invited users (by the current partner)
    - Display partner lang of suggested partners
    
    part of task-3332872
    
    closes odoo/odoo#128834
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/im_livechat/static/tests/channel_invite_tests.js
+++ addons/im_livechat/static/tests/channel_invite_tests.js
@@ -32,3 +32,79 @@ QUnit.test("Can invite a partner to a livechat channel", async (assert) => {
     await click("button[title='Show Member List']");
     assert.containsOnce($, ".o-discuss-ChannelMember:contains(James)");
 });
+
+QUnit.test("Available operators come first", async (assert) => {
+    const pyEnv = await startServer();
+    pyEnv["res.partner"].create({
+        name: "Harry",
+        im_status: "offline",
+        user_ids: [pyEnv["res.users"].create({ name: "Harry" })],
+    });
+    const ronId = pyEnv["res.partner"].create({
+        name: "Ron",
+        im_status: "online",
+        user_ids: [pyEnv["res.users"].create({ name: "Available operator" })],
+    });
+    pyEnv["im_livechat.channel"].create({
+        available_operator_ids: [Command.create({ partner_id: ronId })],
+    });
+    const channelId = pyEnv["discuss.channel"].create({
+        anonymous_name: "Visitor #1",
+        channel_member_ids: [
+            Command.create({ partner_id: pyEnv.currentPartnerId }),
+            Command.create({ partner_id: pyEnv.publicPartnerId }),
+        ],
+        channel_type: "livechat",
+    });
+
+    const { openDiscuss } = await start();
+    await openDiscuss(channelId);
+    await click("button[title='Add Users']");
+    const partnerSuggestions = document.querySelectorAll(".o-discuss-ChannelInvitation-selectable");
+    assert.ok(partnerSuggestions[0].textContent.includes("Ron"));
+    assert.ok(partnerSuggestions[1].textContent.includes("Harry"));
+});
+
+QUnit.test("Partners invited most frequently by the current user come first", async (assert) => {
+    const pyEnv = await startServer();
+    pyEnv["res.partner"].create({
+        name: "John",
+        im_status: "offline",
+        user_ids: [pyEnv["res.users"].create({ name: "John" })],
+    });
+    pyEnv["res.partner"].create({
+        name: "Albert",
+        im_status: "offline",
+        user_ids: [pyEnv["res.users"].create({ name: "Albert" })],
+    });
+    pyEnv["discuss.channel"].create({
+        anonymous_name: "Visitor #1",
+        channel_type: "livechat",
+        channel_member_ids: [
+            Command.create({ partner_id: pyEnv.currentPartnerId }),
+            Command.create({ partner_id: pyEnv.publicPartnerId }),
+        ],
+        livechat_operator_id: pyEnv.currentPartnerId,
+    });
+    pyEnv["discuss.channel"].create({
+        anonymous_name: "Visitor #2",
+        channel_type: "livechat",
+        channel_member_ids: [
+            Command.create({ partner_id: pyEnv.currentPartnerId }),
+            Command.create({ partner_id: pyEnv.publicPartnerId }),
+        ],
+        livechat_operator_id: pyEnv.currentPartnerId,
+    });
+
+    const { openDiscuss } = await start();
+    await openDiscuss();
+    await click(".o-mail-DiscussCategoryItem:contains(Visitor #1)");
+    await click("button[title='Add Users']");
+    await click(".o-discuss-ChannelInvitation-selectable:contains(John) input");
+    await click("button:contains(Invite)");
+    await click(".o-mail-DiscussCategoryItem:contains(Visitor #2)");
+    await click("button[title='Add Users']");
+    const partnerSuggestions = document.querySelectorAll(".o-discuss-ChannelInvitation-selectable");
+    assert.ok(partnerSuggestions[0].textContent.includes("John"));
+    assert.ok(partnerSuggestions[1].textContent.includes("Albert"));
+});
