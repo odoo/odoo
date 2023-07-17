928e73f4ae516c7eecc45a6fea2f178daf48bec3
stcc-odoo <stcc@odoo.com>
2023-07-17 12:30:38 +0000

    [FIX] web: show warning instead of failing silently
    Steps to reproduce:
    
    - Install Accounting
    - Accounting > Accounting > Journal Items
    - Select all items using the checkbox
    - Change account to another value
    - Click "OK" on the confirmation dialog
    
    Issue:
    
    The system fails to change the account field on the records. This issue occurs because the JavaScript code catches an RPCError when attempting
    to write the values to the database but ignores it. Although this
    behavior is intended, it leaves the user without any feedback
    throughout the process.
    
    Solution:
    
    Add a danger notification with the error message when the write
    operation fails.
    
    opw-3354646
    
    closes odoo/odoo#135993
    
    X-original-commit: 266d0bcbbb607970aab51cecff31c95dc48b377b
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Guillaume Merlin (megu) <megu@odoo.com>

--- addons/web/static/src/views/relational_model.js
+++ addons/web/static/src/views/relational_model.js
@@ -11,6 +11,7 @@ import {
     serializeDate,
     serializeDateTime,
 } from "@web/core/l10n/dates";
+import { RPCError } from "@web/core/network/rpc_service";
 import { ORM, x2ManyCommands } from "@web/core/orm_service";
 import { evaluateExpr } from "@web/core/py_js/py";
 import { registry } from "@web/core/registry";
@@ -1753,8 +1754,14 @@ class DynamicList extends DataPoint {
                     await this.model.orm.write(this.resModel, resIds, changes, { context });
                     await Promise.all(validSelection.map((record) => record.load()));
                     record.switchMode("readonly");
-                } catch {
+                } catch (e) {
                     record.discard();
+                    const errorMessage = e instanceof RPCError ? e.data.message : e.message;
+                    const errorTitle = e instanceof RPCError ? e.message : this.model.env._t("Error");
+                    this.model.notificationService.add(errorMessage, {
+                        title: errorTitle,
+                        type: "danger",
+                    });
                 }
             } else {
                 await record._save();
