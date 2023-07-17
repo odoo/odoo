06b6394c2635dea0ea09b784a22a15ffd3e30c34
Daniel Kosky (dako) <dako@odoo.com>
2023-07-17 11:35:19 +0000

    [FIX] l10n_ke_edi_tremol:credit note currency rate
    The problem:
    - Multi-currency is allowed, although the fiscal device expects amounts
    only in KSh.
    - The amounts are converted from the invoice amounts at the moment of
    sending to the device.
    - When sending a credit note to the device, the amount of the credit
    note cannot exceed the amount of the original invoice (otherwise the
    device will throw an error).
    - In odoo, the journal items of the credit note are created using the
    current currency rate (not that of the original invoice), and thus the
    amount can exceed the original amount (depending on changes in the
    currency rate).
    
    This commit addresses this issue by using the currency rate from the
    original invoice (reversed_entry_id) in the case of a credit note.
    
    opw-3422555
    
    closes odoo/odoo#132346
    
    X-original-commit: f28ec1f419fb6e35f6f8a86604796f826283ff20
    Signed-off-by: Josse Colpaert <jco@odoo.com>
    Signed-off-by: Daniel Kosky (dako) <dako@odoo.com>

--- addons/l10n_ke_edi_tremol/models/account_move.py
+++ addons/l10n_ke_edi_tremol/models/account_move.py
@@ -141,8 +141,11 @@ class AccountMove(models.Model):
         # The device expects all monetary values in Kenyan Shillings
         if self.currency_id == self.company_id.currency_id:
             currency_rate = 1
+        # In the case of a refund, use the currency rate of the original invoice
+        elif self.move_type == 'out_refund' and self.reversed_entry_id:
+            currency_rate = abs(self.reversed_entry_id.amount_total_signed / self.reversed_entry_id.amount_total)
         else:
-            currency_rate = abs(lines[0].balance / lines[0].price_subtotal)
+            currency_rate = abs(self.amount_total_signed / self.amount_total)
 
         discount_dict = {line.id: line.discount for line in lines if line.price_total > 0}
         for line in lines:
