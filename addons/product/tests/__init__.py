c19bd7aa4e4db6c8dbcf00ac99c7dff56e6705fc
Victor Feyens <vfe@odoo.com>
2023-07-18 15:24:23 +0000

    [FIX] product: correct currency for default pricelist
    If an inactive currency is given as company currency write values,
    it is unarchived, but before the company update.
    
    Therefore, if the unarchiving of the currency enables the multi-currency,
    a default pricelist will be created for the company, but with the wrong
    currency since it still wasn't updated.
    
    This commit postpones the automatic creation of pricelists after the update
    of the company, making sure the currency of the pricelist is the expected one.
    
    opw-3423706
    
    closes odoo/odoo#128958
    
    X-original-commit: f7c90e4e1c5b6a3335e7c9b9e209a786d4238434
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/product/tests/__init__.py
+++ addons/product/tests/__init__.py
@@ -5,6 +5,7 @@ from . import test_barcode
 from . import test_common
 from . import test_name
 from . import test_pricelist
+from . import test_pricelist_auto_creation
 from . import test_product_attribute_value_config
 from . import test_product_pricelist
 from . import test_seller
