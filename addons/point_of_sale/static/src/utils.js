e15ed80f22a2c8eabdb348a8e99e996fa6979c97
Aaron Bohy <aab@odoo.com>
2023-07-18 13:19:52 +0200

    [REF] point_of_sale,pos_*: use batched from web

--- addons/point_of_sale/static/src/utils.js
+++ addons/point_of_sale/static/src/utils.js
@@ -1,30 +1,5 @@
 /** @odoo-module */
 
-/**
- * Creates a batched version of a callback so that all calls to it in the same
- * microtick will only call the original callback once.
- *
- * @param callback the callback to batch
- * @returns a batched version of the original callback
- */
-export function batched(callback) {
-    let called = false;
-    return async (...args) => {
-        // This await blocks all calls to the callback here, then releases them sequentially
-        // in the next microtick. This line decides the granularity of the batch.
-        await Promise.resolve();
-        if (!called) {
-            called = true;
-            // so that only the first call to the batched function calls the original callback.
-            // Schedule this before calling the callback so that calls to the batched function
-            // within the callback will proceed only after resetting called to false, and have
-            // a chance to execute the callback again
-            Promise.resolve().then(() => (called = false));
-            callback(...args);
-        }
-    };
-}
-
 /*
  * comes from o_spreadsheet.js
  * https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
