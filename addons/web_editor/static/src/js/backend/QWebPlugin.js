f3eb3e3def8dcd713a6e35967d9f89bcd9dbf1d8
Lucas Perais <lpe@odoo.com>
2023-07-18 11:04:49 +0000

    [FIX] web_editor: QWebPlugin branching shows condition
    Before this commit, when displaying the available conditional branches
    on a node, the QWebPlugin's select element only had the name of the attribute.
    
    This commit adds to this name, the actual condition present inside the attribute.
    
    Part-of: odoo/odoo#128228

--- addons/web_editor/static/src/js/backend/QWebPlugin.js
+++ addons/web_editor/static/src/js/backend/QWebPlugin.js
@@ -201,9 +201,9 @@ export class QWebPlugin {
         for (const element of groupElements) {
             const optionElement = document.createElement('option');
             if (element.hasAttribute('t-if')) {
-                optionElement.innerText = 'if';
+                optionElement.innerText = `if: "${element.getAttribute("t-if")}"`;
             } else if (element.hasAttribute('t-elif')) {
-                optionElement.innerText = 'elif';
+                optionElement.innerText = `elif: "${element.getAttribute("t-elif")}"`;
             } else if (element.hasAttribute('t-else')) {
                 optionElement.innerText = 'else';
             }
