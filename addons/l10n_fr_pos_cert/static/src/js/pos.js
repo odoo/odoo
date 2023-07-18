0a4e5e10793cd9836aa7519b1f4473758c78e197
roen-odoo <roen@odoo.com>
2023-07-18 12:55:45 +0000

    [FIX] l10n_fr_pos_cert: allow you to remove reward line in FR PoS
    Current behavior:
    When a reward line had 0 quantity, it was considered as active and the
    points were deducted from the customer's balance.
    
    Steps to reproduce:
    - Install l10n_fr_pos_cert
    - Change the Current company to FR company
    - Create a loyalty card program
    - Open the POS and add a product to the order to get points and trigger
      the reward.
    - Remove the reward line, it should go to 0 quantity instead of being
      removed.
    - The point are still removed from the customer's balance.
    
    opw-3372998
    
    X-original-commit: b30ceb0a0a6b64ad7b4cbb58655eadb318f9f00b

--- addons/l10n_fr_pos_cert/static/src/js/pos.js
+++ addons/l10n_fr_pos_cert/static/src/js/pos.js
@@ -23,7 +23,13 @@ patch(PosGlobalState.prototype, "l10n_fr_pos_cert.PosGlobalState", {
         return _.contains(french_countries, this.company.country.code);
     },
     disallowLineQuantityChange() {
-        const result = this._super(...arguments);
+        let result = this._super(...arguments);
+        let selectedOrderLine = this.selectedOrder.get_selected_orderline();
+        //Note: is_reward_line is a field in the pos_loyalty module
+        if (selectedOrderLine.is_reward_line) {
+            //Always allow quantity change for reward lines
+            return false || result;
+        }
         return this.is_french_country() || result;
     },
 });
