d840a8bb39290d4a034aee5502c00d2baa9edd5f
momegahed <mome@odoo.com>
2023-07-19 16:43:36 +0200

    [FIX] l10n_mx: vat number is missing in default layouts
    Steps to reproduce:
    1. Install l10n_mx
    2. issue an invoice or look into the default document layouts
    3. in the light and background layouts, the vat number (RFC) is missing
    
    Issue:
    the vat number is mandatory in the document layouts right below the
    address. this is not the case now [1]
    
    [1] https://www.sat.gob.mx/articulo/99662/articulo-29-a#:~:text=I.%C2%A0%C2%A0%C2%A0%C2%A0%C2%A0%C2%A0%C2%A0%C2%A0%C2%A0%20The,that%20issues%20it
    
    Fix:
    inherit the templates and add `vat` to the contact widget fields
    
    opw-3425847

--- addons/l10n_mx/__manifest__.py
+++ addons/l10n_mx/__manifest__.py
@@ -47,6 +47,7 @@ With this module you will have:
         "views/res_bank_view.xml",
         "views/res_config_settings_views.xml",
         "views/account_views.xml",
+        "views/report_templates.xml",
     ],
     'demo': [
         'demo/demo_company.xml',
