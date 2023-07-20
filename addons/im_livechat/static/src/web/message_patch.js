43699aea88adbdeca43150e9659fc5571de333e0
Alexandre Kühn <aku@odoo.com>
2023-07-20 14:31:32 +0000

    [FIX] mail: show message action list next to attachment
    Before this commit, when a message had no text, the action list was
    placed above the message content, including attachments.
    
    This made it hard to find the action list for these messages, as
    all action lists of any messages are expected to always be next to
    the 1st line of the message content.
    
    This commit fixes the issue by placing action list next to text
    content if there are any (same as before), but if there are no text,
    then the actions are displayed next to attachments.
    
    The template requiring defining a shared container for text content
    and attachments, so that action list comes after. Also when the
    message has some text and some attachments, the action list is still
    expected to come inline of text content, which is why this commit
    conditionally places the action list accordingly.
    
    Part of Task-3340653
    
    Forward-port-of: #128940
    Part-of: odoo/odoo#129368

--- addons/im_livechat/static/src/web/message_patch.js
+++ addons/im_livechat/static/src/web/message_patch.js
@@ -6,7 +6,7 @@ import "@mail/discuss/core/web/message_patch"; // dependency ordering
 import { patch } from "@web/core/utils/patch";
 
 patch(Message.prototype, "im_livechat/web", {
-    get hasOpenChatFeature() {
+    hasOpenChatFeature() {
         return this.message.originThread?.channel?.channel_type === "livechat"
             ? false
             : this._super();
--- addons/mail/static/src/discuss/core/web/message_patch.js
+++ addons/mail/static/src/discuss/core/web/message_patch.js
@@ -8,21 +8,21 @@ import { _t } from "@web/core/l10n/translation";
 import { patch } from "@web/core/utils/patch";
 
 patch(Message.prototype, "discuss/core/web", {
-    get authorText() {
-        return this.hasOpenChatFeature ? _t("Open chat") : this._super();
+    getAuthorText() {
+        return this.hasOpenChatFeature() ? _t("Open chat") : this._super();
     },
-    get hasAuthorClickable() {
+    hasAuthorClickable() {
         return (
             this._super() &&
             this.message.author.type !== "guest" &&
             this.message.originThread?.channel?.channel_type !== "chat"
         );
     },
-    get hasOpenChatFeature() {
-        return this.hasAuthorClickable;
+    hasOpenChatFeature() {
+        return this.hasAuthorClickable();
     },
     onClickAuthor(ev) {
-        if (this.hasOpenChatFeature) {
+        if (this.hasOpenChatFeature()) {
             markEventHandled(ev, "Message.ClickAuthor");
             this.threadService.openChat({ partnerId: this.message.author.id });
             return;
