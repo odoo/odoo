d78e9371e06a757b26ad65003e9029396346c836
Thomas Lefebvre (thle) <thle@odoo.com>
2023-07-20 12:30:34 +0000

    [FIX] calendar: add attendee_id for deletion
    Steps to reproduce:
    -------------------
    - create an event in the calendar;
    - click on "Everybody's calendars" filter;
    - delete the event.
    
    Issue:
    ------
    The event name is crossed out (declined)
    instead of being deleted,
    even if we are the organizer.
    
    Cause:
    ------
    The value of `event.attendee_id` inside the
    `_onDeleteRecord` is `undefined`.
    The condition will therefore never be `true`.
    
    Solution:
    ---------
    Add the organizer's id to the event
    so that he can delete it in the case
    of "Everybody's calendars" filter.
    
    Note:
    -----
    Correct decline event from calendar view.
    
    opw-3230254
    
    X-original-commit: cfac0c6eb7744f87075f537523330515dfc396c9

--- addons/calendar/static/src/views/attendee_calendar/attendee_calendar_controller.js
+++ addons/calendar/static/src/views/attendee_calendar/attendee_calendar_controller.js
@@ -80,7 +80,7 @@ export class AttendeeCalendarController extends CalendarController {
             this.orm.call(
                 "calendar.attendee",
                 "do_decline",
-                [record.attendeeId],
+                [record.calendarAttendeeId],
             ).then(this.model.load.bind(this.model));
         }
     }
