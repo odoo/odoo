0baaa4b65489727354e566435fd33f9b5a2ede9e
Audric Onockx (auon) <auon@odoo.com>
2023-07-20 18:01:36 +0200

    [IMP] project,hr_timesheet: improve project update
    Add a new column, after tasks that shows the following information :
    
    Tasks: Total done tasks /  Total tasks (ratio in percentage)
    Timesheet: Hours spent/ allocated hours (ratio in percentage) 
    \> visibility: only show if the timesheet setting is activated.
    
    Right side panel  :
    
    Add the Done icon before Tasks 
    Add hours spent/allocated hours information under the Timesheet
    Extra time: Add a new stat button that shows
    the overall extra time used for the project. 
    visible if hours spent > allocated hours.
    By clicking on this it will redirect to the same view
    as we have for the timesheet stat button.
    
    task-3432102
    
    closes odoo/odoo#129177
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/hr_timesheet/models/project_project.py
+++ addons/hr_timesheet/models/project_project.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from collections import defaultdict
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api, _, _lt
 from odoo.exceptions import ValidationError, RedirectWarning
 
 class Project(models.Model):
@@ -211,3 +211,75 @@ class Project(models.Model):
         action = self.env['ir.actions.act_window']._for_xml_id('hr_timesheet.act_hr_timesheet_line_by_project')
         action['display_name'] = _("%(name)s's Timesheets", name=self.name)
         return action
+
+    # ----------------------------
+    #  Project Updates
+    # ----------------------------
+
+    def _get_stat_buttons(self):
+        buttons = super(Project, self)._get_stat_buttons()
+        if not self.allow_timesheets or not self.env.user.has_group("hr_timesheet.group_hr_timesheet_user"):
+            return buttons
+
+        encode_uom = self.env.company.timesheet_encode_uom_id
+        uom_ratio = self.env.ref('uom.product_uom_hour').factor / encode_uom.factor
+
+        allocated = self.allocated_hours / uom_ratio
+        effective = self.total_timesheet_time / uom_ratio
+        color = ""
+        if allocated:
+            number = f"{round(effective)} / {round(allocated)} {encode_uom.name}"
+            success_rate = round(100 * effective / allocated)
+            if success_rate > 100:
+                number = _lt(
+                    "%(effective)s / %(allocated)s %(uom_name)s",
+                    effective=round(effective),
+                    allocated=round(allocated),
+                    uom_name=encode_uom.name,
+                )
+                color = "text-danger"
+            else:
+                number = _lt(
+                    "%(effective)s / %(allocated)s %(uom_name)s (%(success_rate)s%%)",
+                    effective=round(effective),
+                    allocated=round(allocated),
+                    uom_name=encode_uom.name,
+                    success_rate=success_rate,
+                )
+                if success_rate >= 80:
+                    color = "text-warning"
+                else:
+                    color = "text-success"
+        else:
+            number = _lt(
+                    "%(effective)s %(uom_name)s",
+                    effective=round(effective),
+                    uom_name=encode_uom.name,
+                )
+
+        buttons.append({
+            "icon": f"clock-o {color}",
+            "text": _lt("Timesheets"),
+            "number": number,
+            "action_type": "object",
+            "action": "action_project_timesheets",
+            "show": True,
+            "sequence": 2,
+        })
+        if allocated and success_rate > 100:
+            buttons.append({
+                "icon": f"warning {color}",
+                "text": _lt("Extra Time"),
+                "number": _lt(
+                    "%(exceeding_hours)s %(uom_name)s (+%(exceeding_rate)s%%)",
+                    exceeding_hours=round(effective - allocated),
+                    uom_name=encode_uom.name,
+                    exceeding_rate=round(100 * (effective - allocated) / allocated),
+                ),
+                "action_type": "object",
+                "action": "action_project_timesheets",
+                "show": True,
+                "sequence": 3,
+            })
+
+        return buttons
