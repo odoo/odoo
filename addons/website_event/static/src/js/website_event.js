02720ba28c5187afb07e8832b6fa313583204493
adsh-odoo <adsh@odoo.com>
2023-07-25 18:10:15 +0530

    [IMP] website_event: disable register button and remove the deadcode
    -> Prevent registration button activation without selected tickets.
    -> Remove the deadcode which was there to append the warning when we click on
       the register button without selecting the ticket.
    
    Task-3345854
    
    closes odoo/odoo#131715
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_event/static/src/js/website_event.js
+++ addons/website_event/static/src/js/website_event.js
@@ -1,7 +1,6 @@
 /** @odoo-module **/
 
 import ajax from "@web/legacy/js/core/ajax";
-import { _t } from "@web/core/l10n/translation";
 import publicWidget from "@web/legacy/js/public/public_widget";
 
 // Catch registration form event, because of JS for attendee details
@@ -12,15 +11,25 @@ var EventRegistrationForm = publicWidget.Widget.extend({
      */
     start: function () {
         var self = this;
+        const post = this._getPost();
+        const noTicketsOrdered = Object.values(post).map((value) => parseInt(value)).every(value => value === 0);
         var res = this._super.apply(this.arguments).then(function () {
             $('#registration_form .a-submit')
                 .off('click')
                 .click(function (ev) {
                     self.on_click(ev);
                 })
-                .prop('disabled', false);
+                .prop('disabled', noTicketsOrdered);
         });
         return res;
+    },
+
+    _getPost: function () {
+        var post = {};
+        $('#registration_form select').each(function () {
+            post[$(this).attr('name')] = $(this).val();
+        });
+        return post;
     },
 
     //--------------------------------------------------------------------------
@@ -36,37 +45,23 @@ var EventRegistrationForm = publicWidget.Widget.extend({
         ev.stopPropagation();
         var $form = $(ev.currentTarget).closest('form');
         var $button = $(ev.currentTarget).closest('[type="submit"]');
-        var post = {};
-        $('#registration_form table').siblings('.alert').remove();
-        $('#registration_form select').each(function () {
-            post[$(this).attr('name')] = $(this).val();
-        });
-
-        // "post" looks like: { nb_register-9: "2", nb_register-10: "0" }
-        const noTicketsOrdered = Object.values(post).map((value) => parseInt(value)).every(value => value === 0);
-        if (noTicketsOrdered) {
-            $('<div class="alert alert-info"/>')
-                .text(_t('Please select at least one ticket.'))
-                .insertAfter('#registration_form table');
-            return new Promise(function () {});
-        } else {
-            $button.attr('disabled', true);
-            return ajax.jsonRpc($form.attr('action'), 'call', post).then(function (modal) {
-                var $modal = $(modal);
-                $modal.find('.modal-body > div').removeClass('container'); // retrocompatibility - REMOVE ME in master / saas-19
-                $modal.appendTo(document.body);
-                const modalBS = new Modal($modal[0], {backdrop: 'static', keyboard: false});
-                modalBS.show();
-                $modal.appendTo('body').modal('show');
-                $modal.on('click', '.js_goto_event', function () {
-                    $modal.modal('hide');
-                    $button.prop('disabled', false);
-                });
-                $modal.on('click', '.btn-close', function () {
-                    $button.prop('disabled', false);
-                });
+        const post = this._getPost();
+        $button.attr('disabled', true);
+        return ajax.jsonRpc($form.attr('action'), 'call', post).then(function (modal) {
+            var $modal = $(modal);
+            $modal.find('.modal-body > div').removeClass('container'); // retrocompatibility - REMOVE ME in master / saas-19
+            $modal.appendTo(document.body);
+            const modalBS = new Modal($modal[0], {backdrop: 'static', keyboard: false});
+            modalBS.show();
+            $modal.appendTo('body').modal('show');
+            $modal.on('click', '.js_goto_event', function () {
+                $modal.modal('hide');
+                $button.prop('disabled', false);
             });
-        }
+            $modal.on('click', '.btn-close', function () {
+                $button.prop('disabled', false);
+            });
+        });
     },
 });
 
