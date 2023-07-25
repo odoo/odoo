b6bea9032057eabd263bf3b80bcdb99153ae1abb
Aaron Bohy <aab@odoo.com>
2023-07-25 14:20:49 +0200

    [REF] web,*: remove legacy client action support
    This commit removes a lot of legacy code that we kept for the few
    legacy client actions that were still to be converted to owl. Now
    that the last one has been converted, we can finally get rid of a
    lot of legacy code (ActionAdapter, ActionMixin, ControlPanel,
    ActionMenus, DropdownMenu(Item), Pager, DomainSelector...).
    
    Part of task~3439226

--- addons/point_of_sale/__manifest__.py
+++ addons/point_of_sale/__manifest__.py
@@ -156,7 +156,6 @@
             'web/static/src/legacy/js/**/*',
             ('remove', 'web/static/src/legacy/js/libs/**/*'),
             ('remove', 'web/static/src/legacy/js/public/**/*'),
-            'web/static/src/legacy/action_adapters.js',
             'web/static/src/legacy/backend_utils.js',
             'web/static/src/legacy/legacy_load_views.js',
             'web/static/src/legacy/legacy_service_provider.js',
