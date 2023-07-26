4f7548a5c0cd1d81a29eb14dac3f50dcbf3cbb02
rhe-odoo <rhe@odoo.com>
2023-07-26 11:26:02 +0000

    [FIX] pos_restaurant: auto printing bill button
    The usage of the bill button should just work as a normal receipt.
    If the user decided to skip the preview when printing,
    it should skip the preview for this screen.
    
    This commit checks if the user ticked the option in the config and skip the receipt screen,
    then skip if needed the preview when pressing the bill button.
    
    X-original-commit: 99844c6ac3ab54217209975674eb1e505dfe3dc6

--- addons/pos_restaurant/static/src/js/Screens/BillScreen.js
+++ addons/pos_restaurant/static/src/js/Screens/BillScreen.js
@@ -3,11 +3,50 @@
 import { ReceiptScreen } from "@point_of_sale/js/Screens/ReceiptScreen/ReceiptScreen";
 import { registry } from "@web/core/registry";
 
+<<<<<<< HEAD
 export class BillScreen extends ReceiptScreen {
     static template = "pos_restaurant.BillScreen";
     confirm() {
         this.props.resolve({ confirmed: true, payload: null });
         this.pos.closeTempScreen();
+||||||| parent of 8dbfd93303f (temp)
+const BillScreen = (ReceiptScreen) => {
+    class BillScreen extends ReceiptScreen {
+        confirm() {
+            this.props.resolve({ confirmed: true, payload: null });
+            this.trigger("close-temp-screen");
+        }
+        whenClosing() {
+            this.confirm();
+        }
+        /**
+         * @override
+         */
+        async printReceipt() {
+            await super.printReceipt();
+            this.currentOrder._printed = false;
+        }
+=======
+const BillScreen = (ReceiptScreen) => {
+    class BillScreen extends ReceiptScreen {
+        confirm() {
+            this.props.resolve({ confirmed: true, payload: null });
+            this.trigger("close-temp-screen");
+        }
+        whenClosing() {
+            this.confirm();
+        }
+        /**
+         * @override
+         */
+        async printReceipt() {
+            await super.printReceipt();
+            this.currentOrder._printed = false;
+            if (this.env.pos.config.iface_print_skip_screen) {
+                this.confirm();
+            }
+        }
+>>>>>>> 8dbfd93303f (temp)
     }
     whenClosing() {
         this.confirm();
