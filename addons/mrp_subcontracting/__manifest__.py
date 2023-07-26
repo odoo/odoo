bd6bf7841135878ce85c99434c1931216c75900d
Romain Estievenart <res@odoo.com>
2023-07-26 10:54:55 +0000

    [IMP] web *: remove unused jquery.scrollTo library
    *: project, mrp_subcontracting
    
    This commit is a code cleanup, we remove an unused lib from the codebase
    
    The last usage of jquery.scrollTo was dropped in saas-16.4 after the
    refactoring of livechat, see here [1].
    
    Note that we have our own ´scrollTo´ utility.
    
    References :
    https://github.com/flesler/jquery.scrollTo
    
    [1] https://github.com/odoo/odoo/commit/7117d37b0ddc77f082277aef55691cfb099059d2#diff-8925fb7187d27cf7abc3fbb149a22cdcfa83eaa435a667b5beb042c4fd0fe3fdL220
    
    closes odoo/odoo#132035
    
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>

--- addons/mrp_subcontracting/__manifest__.py
+++ addons/mrp_subcontracting/__manifest__.py
@@ -74,7 +74,6 @@
 
             'web/static/src/env.js',
 
-            'web/static/lib/jquery.scrollTo/jquery.scrollTo.js',
             'web/static/lib/jquery.ba-bbq/jquery.ba-bbq.js',
 
             'web/static/src/legacy/scss/fields.scss',
