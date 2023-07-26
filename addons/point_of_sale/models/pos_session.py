7bb26ab3564344b1c6273cd94d16303e3d738f19
Pedram (PEBR) <pebr@odoo.com>
2023-07-26 14:19:48 +0000

    [FIX] point_of_sale: limited packaging loading based on products
    Before this commit, the PoS unnecessarily loaded all product packaging,
    even if their corresponding products were not loaded in the PoS due to
    the limited product loading option. Adding to this inefficiency, the
    `add_packagings` function would only include those packagings whose
    associated products were available in the PoS, making the loading of
    all packagings pointless. The optimal approach should focus on loading
    only those packaging whose products are already loaded into the PoS.
    
    Also, if you scan a product packaging that was not loaded in the PoS,
    it won't fetch it from the server.
    
    opw-3436019
    
    closes odoo/odoo#129794
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/point_of_sale/models/pos_session.py
+++ addons/point_of_sale/models/pos_session.py
@@ -1965,9 +1965,15 @@ class PosSession(models.Model):
         return products
 
     def _loader_params_product_packaging(self):
+        domain = [('barcode', 'not in', ['', False])]
+        loaded_data = self._context.get('loaded_data')
+        if loaded_data and self.config_id.limited_products_loading:
+            loaded_product_ids = [x['id'] for x in loaded_data['product.product']]
+            domain = AND([domain, [('product_id', 'in', loaded_product_ids)]])
+
         return {
             'search_params': {
-                'domain': [('barcode', 'not in', ['', False])],
+                'domain': domain,
                 'fields': ['name', 'barcode', 'product_id', 'qty'],
             },
         }
@@ -2034,6 +2040,20 @@ class PosSession(models.Model):
         params['search_params'] = {**params['search_params'], **custom_search_params}
         partners = self.env['res.partner'].search_read(**params['search_params'])
         return partners
+
+    def find_product_by_barcode(self, barcode):
+        product = self.env['product.product'].search([['barcode', '=', barcode], ['sale_ok', '=', True]])
+        if product:
+            return {'product_id': [product.id]}
+
+        packaging_params = self._loader_params_product_packaging()
+        packaging_params['search_params']['domain'] = [['barcode', '=', barcode]]
+        packaging = self.env['product.packaging'].search_read(**packaging_params['search_params'])
+        if packaging:
+            product_id = packaging[0]['product_id']
+            if product_id:
+                return {'product_id': [product_id[0]], 'packaging': packaging}
+        return {}
 
 
 class ProcurementGroup(models.Model):
