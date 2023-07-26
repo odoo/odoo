fbd8c96d0ea097a23fc1e283721f8219fe7ebcd0
Adrien Widart (awt) <awt@odoo.com>
2023-07-26 11:49:05 +0000

    [FIX] sale_stock,stock_account,point_of_sale: post anglo saxon inv
    There will be some access errors if an accountman tries to post an
    invoice
    
    To reproduce the issue:
    1. Create a product category FIFO-AUTO
    2. Receive a product of that category at $10
    3. Sale, deliver and create the invoice for that product
    4. Create a user with basic access to account app only
    5. Switch into that user
    6. Confirm the invoice
    
    Error: An error access is raised, he can not post the invoice
    
    In the invoice posting process, we try to read some other models
    (pos order, SVL, and so on) but the user has not any access to them
    
    OPW-3302458
    
    closes odoo/odoo#129758
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/point_of_sale/models/account_move.py
+++ addons/point_of_sale/models/account_move.py
@@ -59,7 +59,7 @@ class AccountMoveLine(models.Model):
         if not self.product_id:
             return self.price_unit
         price_unit = super(AccountMoveLine, self)._stock_account_get_anglo_saxon_price_unit()
-        order = self.move_id.pos_order_ids
-        if order:
-            price_unit = order._get_pos_anglo_saxon_price_unit(self.product_id, self.move_id.partner_id.id, self.quantity)
+        sudo_order = self.move_id.sudo().pos_order_ids
+        if sudo_order:
+            price_unit = sudo_order._get_pos_anglo_saxon_price_unit(self.product_id, self.move_id.partner_id.id, self.quantity)
         return price_unit
