fdbe3c659c3615a3335884511992d5e0a8d03176
Thomas Beckers <tbs@odoo.com>
2023-07-27 07:57:09 +0000

    [FIX] account_edi_proxy_client: neutralize proxy user
    Since commit 9546ba0, the edi proxy user is no more neutralized.
    Now proxy users related to l10n_it_edi are set to 'demo' mode and other users
    are set to 'test' mode.
    
    opw-3439385
    
    closes odoo/odoo#129935
    
    X-original-commit: 4b8759d340e7ad2ae38572f374726087db2437cf
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Thomas Beckers (tbs) <tbs@odoo.com>

--- addons/account_edi_proxy_client/data/neutralize.sql
+++ addons/account_edi_proxy_client/data/neutralize.sql
@@ -1,5 +1,6 @@
 -- disable edi connections in general, and the Italian one (l10n_it_edi_sdicoop) in particular
-INSERT INTO ir_config_parameter (key, value)
-VALUES ('account_edi_proxy_client.demo', true)
-    ON CONFLICT (key) DO
-       UPDATE SET value = true;
+UPDATE account_edi_proxy_client_user
+SET edi_mode = CASE
+                    WHEN proxy_type = 'l10n_it_edi' THEN 'demo'
+                    ELSE 'test'
+               END;
