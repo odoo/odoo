e96094e5c22298966d65ace066c100b0f2aef115
Touati Djamel (otd) <otd@odoo.com>
2023-07-27 17:45:58 +0200

    [FIX] delivery: set carrier in package type creation

--- addons/delivery/models/stock_picking.py
+++ addons/delivery/models/stock_picking.py
@@ -205,15 +205,15 @@ class StockPicking(models.Model):
         view_id = self.env.ref('delivery.choose_delivery_package_view_form').id
         context = dict(
             self.env.context,
-            current_package_carrier_type=self.carrier_id.delivery_type,
+            default_package_carrier_type=self.carrier_id.delivery_type,
             default_picking_id=self.id
         )
         # As we pass the `delivery_type` ('fixed' or 'base_on_rule' by default) in a key who
         # correspond to the `package_carrier_type` ('none' to default), we make a conversion.
         # No need conversion for other carriers as the `delivery_type` and
         #`package_carrier_type` will be the same in these cases.
-        if context['current_package_carrier_type'] in ['fixed', 'base_on_rule']:
-            context['current_package_carrier_type'] = 'none'
+        if context['default_package_carrier_type'] in ['fixed', 'base_on_rule']:
+            context['default_package_carrier_type'] = 'none'
         return {
             'name': _('Package Details'),
             'type': 'ir.actions.act_window',
