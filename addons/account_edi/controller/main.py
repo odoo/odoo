620b92845f36d7b249cb4df1543d49fe3c90f2e3
hupo-odoo <hupo@odoo.com>
2023-07-28 14:53:43 +0200

    [FIX] account{,_edi,_ubl_cii}: mass export edi documents
    When selecting several invoices from the list view, it is possible to trigger an action to export all edi documents in a zip file. This commit fixes 2 different issues:
    
    a) We want to be able to export edi documents that have not been sent. Therefore, we no longer filter for 'sent' and 'cancelled' edi documents.
    
    b) We want to also export edi documents that have ubl format. These documents are, from 16.2, in another field on account.move and no longer part of edi_document_ids. This is the reason why we had to move the logic from account_edi to account to make it overridable to other modules. This new way of overriding the function will also enable other formats to be included in the export function.
    
    task-3441449 (issue 1)
    task-3439427 (issue 2)
    
    X-original-commit: f8654b3501aca6e5d77ced5f73cb351c61684cd2

--- addons/account_edi/controller/main.py
+++ addons/account_edi/controller/main.py
@@ -1,18 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import http
-from odoo.http import request, content_disposition
+from odoo.addons.account.controllers.download_edi_docs import AccountEdiDocumentDownloadController
 
-
-class EdiDocumentDownloadController(http.Controller):
+class EdiDocumentDownloadController(AccountEdiDocumentDownloadController):
     @http.route('/account_edi/download_edi_documents', type='http', auth='user')
     def download_edi_documents(self, **args):
-        ids = list(map(int, request.httprequest.args.getlist('ids')))
-        content = request.env['account.move'].browse(ids)._create_zipped()
-        headers = [
-            ('Content-Type', 'zip'),
-            ('X-Content-Type-Options', 'nosniff'),
-            ('Content-Length', len(content)),
-            ('Content-Disposition', content_disposition('edi_documents.zip')),
-        ]
-        return request.make_response(content, headers)
+        return super().export_edi_documents()
