4636e47675ac5b6e71149f96f2f130e6bc237e43
desdelinux <luigys@vauxoo.com>
2023-07-30 10:44:25 -0500

    [ADD] Welcome l10n_pe_website_sale
    New module that adds peruvian requirements to the website ecommerce
    
    * Add fields to the checkout form
    	*Identification type
    	*City
    	*District
    * Add logic to validate the new fields
    * Take into account the identification type when validating the VAT in the checkout form
    * Change label name for the vat field, to show "Number" instead of "VAT number".
    * Add demo data to display a peruvian website and show products with taxes
    * Add module translations
    * Hide default city
    * Refactored code to create the function
    _changeOption which combines the functionality
    of _changeState and _changeCity.
    
    closes odoo/odoo#148796
    
    X-original-commit: 62ead4a0ca668d9262bf6b994aff912d44e93ca4
    Signed-off-by: Josse Colpaert <jco@odoo.com>
    Co-authored-by: Andrea Geraldo <myrna.andrea@vauxoo.com>

---
+++ addons/l10n_pe_website_sale/__manifest__.py
@@ -0,0 +1,26 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    "name": "Peruvian eCommerce",
+    "countries": ["pe"],
+    "version": "0.1",
+    "summary": "Be able to see Identification Type in ecommerce checkout form.",
+    "category": "Accounting/Localizations/Website",
+    "author": "Vauxoo, Odoo",
+    "license": "LGPL-3",
+    "depends": [
+        "website_sale",
+        "l10n_pe",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_model_fields.xml",
+        "views/templates.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "l10n_pe_website_sale/static/src/js/website_sale.js",
+        ],
+    },
+    "installable": True,
+    "auto_install": True,
+}
---
+++ addons/l10n_pe_website_sale/static/src/js/website_sale.js
@@ -0,0 +1,90 @@
+/** @odoo-module **/
+import {WebsiteSale} from "@website_sale/js/website_sale";
+
+WebsiteSale.include({
+    events: Object.assign({}, WebsiteSale.prototype.events, {
+        "change select[name='state_id']": "_onChangeState",
+        "change select[name='city_id']": "_onChangeCity",
+    }),
+    start: function () {
+        this.elementCities = document.querySelector("select[name='city_id']");
+        this.elementDistricts = document.querySelector("select[name='l10n_pe_district']");
+        this.cityBlock = document.querySelector(".div_city");
+        this.autoFormat = document.querySelector(".checkout_autoformat");
+        this.elementState = document.querySelector("select[name='state_id']");
+        this.elemenCountry = document.querySelector("select[name='country_id']");
+        this.isPeruvianCompany = this.elemenCountry?.dataset.company_country_code === 'PE';
+        return this._super.apply(this, arguments);
+    },
+    _changeOption: function (selectCheck, rpcRoute, place, selectElement) {
+        if (!selectCheck) {
+            return;
+        }
+        return this.rpc(rpcRoute, {
+        }).then((data) => {
+            if (this.isPeruvianCompany) {
+                if (data[place]?.length) {
+                    selectElement.innerHTML = "";
+                    data[place].forEach((item) => {
+                        let opt = document.createElement("option");
+                        opt.textContent = item[1];
+                        opt.value = item[0];
+                        opt.setAttribute("data-code", item[2]);
+                        selectElement.appendChild(opt);
+                    });
+                    selectElement.parentElement.style.display = "block";
+                } else {
+                    selectElement.value = "";
+                    selectElement.parentElement.style.display = "none";
+                }
+            }
+        });
+    },
+    _onChangeState: function () {
+        if (this.isPeruvianCompany) {
+            if (this.elementState.value === "" && this.elemenCountry.value !== '') {
+                this.elementState.options[1].selected = true;
+            }
+            const state = this.elementState.value;
+            const rpcRoute = `/shop/state_infos/${state}`;
+            return this.autoFormat.length
+                ? this._changeOption(state, rpcRoute, "cities", this.elementCities).then(() => this._onChangeCity())
+                : undefined;
+        }
+    },
+    _onChangeCity: function () {
+        if (this.isPeruvianCompany) {
+            const city = this.elementCities.value;
+            const rpcRoute = `/shop/city_infos/${city}`;
+            return this.autoFormat.length
+                ? this._changeOption(city, rpcRoute, "districts", this.elementDistricts)
+                : undefined;
+        }
+    },
+    _onChangeCountry: function (ev) {
+        return this._super.apply(this, arguments).then(() => {
+            if (this.isPeruvianCompany) {
+                let selectedCountry = ev.currentTarget.options[ev.currentTarget.selectedIndex].getAttribute("code");
+                let cityInput = document.querySelector(".form-control[name='city']");
+                if (selectedCountry == "PE") {
+                    if (cityInput.value) {
+                        cityInput.value = "";
+                    }
+                    this.cityBlock.classList.add("d-none");
+                    return this._onChangeState().then(() => {
+                        this._onChangeCity();
+                    });
+                } else {
+                    this.cityBlock.querySelectorAll("input").forEach((input) => {
+                        input.value = "";
+                    });
+                    this.cityBlock.classList.remove("d-none");
+                    this.elementCities.value = "";
+                    this.elementCities.parentElement.style.display = "none";
+                    this.elementDistricts.value = "";
+                    this.elementDistricts.parentElement.style.display = "none";
+                }
+            }
+        });
+    },
+});
