0923d6bb7600cc97501770f8d16cd00ace222f0e
Walid <waha@odoo.com>
2023-07-31 09:01:33 +0000

    [FIX] stock: return packages
    Steps to reproduce:
    - Deliver an SN-tracked product with the Destination Package set (put in pack button also sets this).
    - Return that product without setting the Source Package (you can also click "put in pack" which will put the product in yet another pack).
    - This already results in two lines with the same SN in the same location, one with +1.00 and one with -1.00 quantity.
    - Deliver that product again and put in pack.
    - Now there's three lines with the same SN in the same location, two with +1.00 and one with -1.00 quantity.
    
    Bug:
    source Package isn't set bydefault when confirming the move_line
    we update the stock quantity for that lot_id and Package set to False
    the existing quantity has a Package set so it is filtered out and a new
    negative quantity is created
    
    Fix:
    set the source Package on the return
    
    opw-3179388
    
    closes odoo/odoo#129505
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -922,19 +922,22 @@ class Picking(models.Model):
                     package_level_ids = picking.package_level_ids.filtered(lambda pl: pl.package_id == pack)
                     move_lines_to_pack = picking.move_line_ids.filtered(lambda ml: ml.package_id == pack and not ml.result_package_id and ml.state not in ('done', 'cancel'))
                     if not package_level_ids:
-                        self.env['stock.package_level'].create({
+                        package_location = self._get_entire_pack_location_dest(move_lines_to_pack) or picking.location_dest_id.id
+                        self.env['stock.package_level'].with_context(
+                            bypass_reservation_update=package_location
+                        ).create({
                             'picking_id': picking.id,
                             'package_id': pack.id,
                             'location_id': pack.location_id.id,
-                            'location_dest_id': self._get_entire_pack_location_dest(move_lines_to_pack) or picking.location_dest_id.id,
+                            'location_dest_id': package_location,
                             'move_line_ids': [(6, 0, move_lines_to_pack.ids)],
                             'company_id': picking.company_id.id,
                         })
                         # Propagate the result package in the next move for disposable packages only.
                         if pack.package_use == 'disposable':
-                            move_lines_to_pack.write({
-                                'result_package_id': pack.id,
-                            })
+                            move_lines_to_pack.with_context(
+                                bypass_reservation_update=package_location
+                            ).write({'result_package_id': pack.id})
                     else:
                         move_lines_in_package_level = move_lines_to_pack.filtered(lambda ml: ml.move_id.package_level_id)
                         move_lines_without_package_level = move_lines_to_pack - move_lines_in_package_level
