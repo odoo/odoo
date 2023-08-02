78601d54e93693f92054728b184cfcc3878d8f2f
vishal padhiyar <visp@odoo.com>
2023-08-02 11:59:34 +0530

    [FIX] web_editor: tableUiMenu border stuck after deleting table
    Before this commit:
    
    When tableUiMenu is visible and we try to delete the table then tableUiMenu
    stuck after table deletion.
    
    After this commit:
    
    Now after table deletion tableUiMenu also hide.
    
    task-3429097
    
    X-original-commit: 951dc35e17dfa9fbbb8906afbec82216c63e7d11

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -2244,6 +2244,7 @@ export class OdooEditor extends EventTarget {
         } else if (fullySelectedTables.length) {
             fullySelectedTables.forEach(table => table.remove());
         }
+        this._toggleTableUi();
         return false;
     }
 
