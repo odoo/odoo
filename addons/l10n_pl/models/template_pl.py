3b5acf00f3e197afc89dce17e7aa41c45a5ef806
Thomas Becquevort (thbe) <thbe@odoo.com>
2023-08-02 15:01:15 +0200

    [IMP] l10n_pl: Update of the polish accounts, groups and positions
    Currently, the polish chart of account is not up to date and is therefore
    underused by user since it is not relevant.
    
    This PR aims to improve the polish chart of accounts, account groups and
    fiscal position in order to make it usable by users.
    
    This new chart of account and other elements linked to it that are modified
    by the PR are based on the documents available on the gofin.pl website. The
    chart of account is based on the one available at this link :
    https://plankont.gofin.pl/ which was consulted on the 2nd of August 2023.
    
    The polish chart of account should now be relevant and useable.
    
    task-3332731
    
    closes odoo/odoo#147716
    
    X-original-commit: 00435d2a3f73d8bb505f41683d1e19a74b09f59c
    Related: odoo/enterprise#53479
    Signed-off-by: Quentin De Paoli <qdp@odoo.com>
    Signed-off-by: Thomas Becquevort (thbe) <thbe@odoo.com>

--- addons/l10n_pl/models/template_pl.py
+++ addons/l10n_pl/models/template_pl.py
@@ -9,10 +9,10 @@ class AccountChartTemplate(models.AbstractModel):
     @template('pl')
     def _get_pl_template_data(self):
         return {
-            'property_account_receivable_id': 'chart20000000',
-            'property_account_payable_id': 'chart21000000',
-            'property_account_expense_categ_id': 'chart70110000',
-            'property_account_income_categ_id': 'chart73010000',
+            'property_account_receivable_id': 'chart20000100',
+            'property_account_payable_id': 'chart21000100',
+            'property_account_expense_categ_id': 'chart70010100',
+            'property_account_income_categ_id': 'chart73000100',
             'code_digits': '8',
             'use_storno_accounting': True,
         }
@@ -25,12 +25,12 @@ class AccountChartTemplate(models.AbstractModel):
                 'bank_account_code_prefix': '11.000.00',
                 'cash_account_code_prefix': '12.000.00',
                 'transfer_account_code_prefix': '11.090.00',
-                'account_default_pos_receivable_account_id': 'chart20000100',
-                'income_currency_exchange_account_id': 'chart75060000',
-                'expense_currency_exchange_account_id': 'chart75140000',
-                'account_journal_early_pay_discount_loss_account_id': 'chart75190000',
-                'account_journal_early_pay_discount_gain_account_id': 'chart75070000',
-                'default_cash_difference_income_account_id': 'chart77000000',
-                'default_cash_difference_expense_account_id': 'chart77100000',
+                'account_default_pos_receivable_account_id': 'chart20000200',
+                'income_currency_exchange_account_id': 'chart75000600',
+                'expense_currency_exchange_account_id': 'chart75010400',
+                'account_journal_early_pay_discount_loss_account_id': 'chart75010900',
+                'account_journal_early_pay_discount_gain_account_id': 'chart75000900',
+                'default_cash_difference_income_account_id': 'chart75000700',
+                'default_cash_difference_expense_account_id': 'chart75010500',
             },
         }
