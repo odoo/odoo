198e226f5bd66dba6598d1ef85e5fa5fb82b38b2
Lucas Perais <lpe@odoo.com>
2023-08-02 15:40:37 +0200

    [IMP] website: Expose models on the website
    This commit implements a way to expose any model publicly on the website. Both manual and
    existing models can create such pages called 'Model Pages'.
    A manual model is a model that has been created by the user at runtime, via the technical menu,
    via studio, or with an xml declaration.
    
    The main interface to easily create such page is in Studio, in the tab Website Integration. But a
    partner or developer could easily import those without studio installed. That's the reason most of
    the code to handle the display of such pages is present in the website module directly.
    
    The listing can handle two display modes (Grid or List). Once the user changes the display mode,
    the latest value is set in the session to be remembered for the next visit of the page.
    A default_layout can be set and is customizable in the website editor or from the backend of Website.
    This value is linked to the page to display, so each listing page can use a different layout by default.
    
    The route on which the models are exposed is:
    "/model/<string:page_name_slugified>",
    
    With the derived routes:
    "/model/<string:page_name_slugified>/page/<int:page_number>",
    "/model/<string:page_name_slugified>/<string:record_slug>"
    
    task-id-3231144
    
    Part-of: odoo/odoo#130544
    Co-authored-by: Florent Dardenne (dafl) <dafl@odoo.com>
    Co-authored-by: Luca Vitali (luvi) <luvi@odoo.com>

--- addons/website/models/ir_ui_view.py
+++ addons/website/models/ir_ui_view.py
@@ -23,6 +23,7 @@ class View(models.Model):
 
     website_id = fields.Many2one('website', ondelete='cascade', string="Website")
     page_ids = fields.One2many('website.page', 'view_id')
+    controller_page_ids = fields.One2many('website.controller.page', 'view_id')
     first_page_id = fields.Many2one('website.page', string='Website Page', help='First page linked to this view', compute='_compute_first_page_id')
     track = fields.Boolean(string='Track', default=False, help="Allow to specify for one page of the website to be trackable or not")
     visibility = fields.Selection([('', 'All'), ('connected', 'Signed In'), ('restricted_group', 'Restricted Group'), ('password', 'With Password')], default='')
