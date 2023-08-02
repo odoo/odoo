7f83f510e73c25db591a751a33470b8bf2c17e7f
Thibault Delavallée <tde@odoo.com>
2023-08-02 12:43:55 +0000

    [REF] various, calendar_sms: remove '_sms_get_default_partners'
    RATIONALE
    
    Simplify field management for mail / phone / sms flows. Make it working out
    of the box, easier to use and tweak.
    
    SPECIFICATIONS
    
    '_sms_get_default_partners' exists in SMS to find default customers to
    notify when sending automated SMS. It can be replaced by '_mail_get_partners'
    helper available on BaseModel in mail.
    
    A specific case exists in calendar where it was overridden to avoid notifying
    attendees that declined the event, notably for alarms. This feature has been
    moved directly in the reminder sending method, to keep behavior coherent.
    Test is updated accordingly, and is now really using the SMS mocks to simulate
    the sending.
    
    Task-3422449 (Mail, Phone: Move and improve field helpers)
    
    Part-of: odoo/odoo#130468

--- addons/sms/models/res_partner.py
+++ addons/sms/models/res_partner.py
@@ -7,9 +7,3 @@ from odoo import models
 class ResPartner(models.Model):
     _name = 'res.partner'
     _inherit = ['mail.thread.phone', 'res.partner']
-
-    def _sms_get_default_partners(self):
-        """ Override of mail.thread method.
-            SMS recipients on partners are the partners themselves.
-        """
-        return self
