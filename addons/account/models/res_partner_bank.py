93b6d3f0bad435a413b1b5b44445ddc4a6cd7852
Achraf (abz) <abz@odoo.com>
2023-08-02 12:21:57 +0000

    [FIX] account: Adapting barcode API to the new one
    This commit change /report/barcode/?type= to /report/barcode/?barcode_type
    
    See ee324e8#diff-8f0bbb50491623995fba965c3d64585c6cbfdbbe519ec95473e472826d253826R1977-R1979
    similar fix done previously in `l10n_sa`: https://github.com/odoo/odoo/pull/127222
    
    sentry-3983735307
    
    closes odoo/odoo#130526
    
    Signed-off-by: Andrea Grazioso (agr) <agr@odoo.com>

--- addons/account/models/res_partner_bank.py
+++ addons/account/models/res_partner_bank.py
@@ -106,10 +106,7 @@ class ResPartnerBank(models.Model):
         :param structured_communication: Structured communication to add to the payment when generating one with the QR-code
         """
         params = self._get_qr_code_generation_params(qr_method, amount, currency, debtor_partner, free_communication, structured_communication)
-        if params:
-            params['type'] = params.pop('barcode_type')
-            return '/report/barcode/?' + werkzeug.urls.url_encode(params)
-        return None
+        return '/report/barcode/?' + werkzeug.urls.url_encode(params) if params else None
 
     def _get_qr_code_base64(self, qr_method, amount, currency, debtor_partner, free_communication, structured_communication):
         """ Hook for extension, to support the different QR generation methods.
