b46544d3cbd4116f69a03064c61798cef8435c5b
abd-msyukyu-odoo <abd@odoo.com>
2023-08-03 15:15:21 +0000

    [IMP] web_editor: put rollback code assignation on its own line
    If the `_toRollback` property of the editor is assigned on its own line, it is
    easier to identify which assignation caused/prevented a rollback when
    debugging.
    
    closes odoo/odoo#130830
    
    X-original-commit: 263dcbaf9aa0c48bd61bf85b091c746d7850cfe1
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -892,9 +892,9 @@ export class OdooEditor extends EventTarget {
                 }
                 case 'childList': {
                     record.addedNodes.forEach(added => {
-                        this._toRollback =
-                            this._toRollback ||
-                            (containsUnremovable(added) && UNREMOVABLE_ROLLBACK_CODE);
+                        if (!this._toRollback && containsUnremovable(added)) {
+                            this._toRollback = UNREMOVABLE_ROLLBACK_CODE;
+                        }
                         const mutation = {
                             'type': 'add',
                         };
@@ -1381,8 +1381,9 @@ export class OdooEditor extends EventTarget {
         }
     }
     unbreakableStepUnactive() {
-        this._toRollback =
-            this._toRollback === UNBREAKABLE_ROLLBACK_CODE ? false : this._toRollback;
+        if (this._toRollback === UNBREAKABLE_ROLLBACK_CODE) {
+            this._toRollback = false;
+        }
         this._checkStepUnbreakable = false;
     }
     historyPauseSteps() {
