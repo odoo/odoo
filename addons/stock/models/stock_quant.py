9f748e72ade7dc118f3472411d8dad29670a77da
Michele <dicroce.m@stesi.eu>
2023-08-04 15:04:31 +0000

    [IMP] stock: gather_domain in dedicated function
    With this commit you wil have the function _get_gather_domain so you can
    override and manage the gather_domain by your own logic
    
    closes odoo/odoo#131467
    
    X-original-commit: 92b6fd647c77ae88dfb685654ba9e9f14b7a1d03
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -700,8 +700,7 @@ class StockQuant(models.Model):
             return domain, 'in_date ASC, id'
         raise UserError(_('Removal strategy %s not implemented.') % (removal_strategy,))
 
-    def _gather(self, product_id, location_id, lot_id=None, package_id=None, owner_id=None, strict=False, qty=0):
-        removal_strategy = self._get_removal_strategy(product_id, location_id)
+    def _get_gather_domain(self, product_id, location_id, lot_id=None, package_id=None, owner_id=None, strict=False):
         domain = [('product_id', '=', product_id.id)]
         if not strict:
             if lot_id:
@@ -718,7 +717,11 @@ class StockQuant(models.Model):
             domain = expression.AND([[('location_id', '=', location_id.id)], domain])
         if self.env.context.get('with_expiration'):
             domain = expression.AND([[('expiration_date', '>=', self.env.context['with_expiration'])], domain])
+        return domain
 
+    def _gather(self, product_id, location_id, lot_id=None, package_id=None, owner_id=None, strict=False, qty=0):
+        removal_strategy = self._get_removal_strategy(product_id, location_id)
+        domain = self._get_gather_domain(product_id, location_id, lot_id, package_id, owner_id, strict)
         domain, order = self._get_removal_strategy_domain_order(domain, removal_strategy, qty)
         return self.search(domain, order=order).sorted(lambda q: not q.lot_id)
 
