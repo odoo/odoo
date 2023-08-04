242baaa0ad3f7d4ce9e2022810565fa15c69d559
Julien Alardot (jual) <jual@odoo.com>
2023-08-04 14:21:14 +0000

    [FIX] hr_expense: Fix expense unit_amount computation
    This fixes a bug when you change currency on an already saved expense
    due to the fact that the unit_amount is never recomputed but used to
    generate the move line balance
    
    Step to reproduce:
    - Duplicate an existing expense paid by an employee
    - Change currency + save
    - create -> post expense report
    - The total amount and remaining amount do not match
    
    closes odoo/odoo#131709
    
    X-original-commit: e0bceb37ee7656c37ab99a4a4bb3b71d85199e1b
    Signed-off-by: Josse Colpaert <jco@odoo.com>
    Signed-off-by: Julien Alardot (jual) <jual@odoo.com>

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -313,12 +313,13 @@ class HrExpense(models.Model):
         for expense in self:
             expense.product_uom_id = expense.product_id.uom_id
 
-    @api.depends('product_id', 'attachment_number')
+    @api.depends('product_id', 'attachment_number', 'currency_rate')
     def _compute_unit_amount(self):
         for expense in self:
-            # Only change unit_amount if the product has no cost defined on it
-            if expense.product_id and not expense.attachment_number or (expense.attachment_number and not expense.unit_amount):
+            if expense.product_id and expense.product_has_cost and not expense.attachment_number or (expense.attachment_number and not expense.unit_amount):
                 expense.unit_amount = expense.product_id._price_compute('standard_price', currency=expense.currency_id)[expense.product_id.id]
+            else:  # Even if we don't add a product, the unit_amount is still used for the move.line balance computation
+                expense.unit_amount = expense.company_currency_id.round(expense.total_amount_company / (expense.quantity or 1))
 
     @api.depends('product_id', 'company_id')
     def _compute_tax_ids(self):
