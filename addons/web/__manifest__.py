435834915e7a26deef6bc4feafb9311cf8edfd71
Aaron Bohy <aab@odoo.com>
2023-08-04 09:35:17 +0000

    [REF] web, website: move initAutoMoreMenu to website
    There's a single usecase left for this helper, which is in website.
    This commit directly moves the function definition in the file
    using it.
    
    Part of task~3439226
    
    closes odoo/odoo#130843
    
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- addons/web/__manifest__.py
+++ addons/web/__manifest__.py
@@ -231,7 +231,6 @@ This module provides the core of the Odoo Web Client.
             'web/static/src/boot.js',
             'web/static/src/session.js',
             'web/static/src/legacy/js/core/cookie_utils.js',
-            'web/static/src/legacy/js/core/menu.js',
             'web/static/src/legacy/js/public/lazyloader.js',
         ],
         'web.assets_frontend': [
@@ -332,7 +331,6 @@ This module provides the core of the Odoo Web Client.
             'web/static/src/legacy/js/core/popover.js',
             'web/static/src/legacy/js/core/dom.js',
             'web/static/src/legacy/js/core/local_storage.js',
-            'web/static/src/legacy/js/core/menu.js',
             'web/static/src/legacy/js/core/mixins.js',
             'web/static/src/legacy/js/core/qweb.js',
             'web/static/src/legacy/js/core/ram_storage.js',
@@ -385,7 +383,6 @@ This module provides the core of the Odoo Web Client.
             ('remove', 'web/static/src/boot.js'),
             ('remove', 'web/static/src/session.js'),
             ('remove', 'web/static/src/legacy/js/core/cookie_utils.js'),
-            ('remove', 'web/static/src/legacy/js/core/menu.js'),
             ('remove', 'web/static/src/legacy/js/public/lazyloader.js'),
         ],
         'web.assets_backend_prod_only': [
