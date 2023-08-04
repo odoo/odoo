8af29970bd4f7907fac96e5935bd743d2eec89bc
divy-odoo <divy@odoo.com>
2023-08-04 12:21:55 +0530

    [FIX] website, web_editor: prevent entering edit mode for drag/drop block
    Steps to reproduce :
    1. Enter the edit mode of the homepage
    2. Click on the page
    -> Bug: Drag building block is editable
    -> Bug: the "Type / to enter a command message" appear and stays
    
    With this PR [1], we are using the `o_editable` and `o_not_editable`
    classes as markers for contentEditable values in HTML. Before this, we
    only considered the `o_not_editable` class as a marker.
    
    We have an `o_editable` class with contrast of `contenteditable=false`
    attribute in the drag/drop block. So it's getting set to `true` by the
    observer during sanitizing the field that makes the block editable.
    
    To prevent it, added the flag attribute `data-oe-not-contenteditable`
    which will keep the value of contenteditable = 'false'  by sanitize the
    function.
    
    [1]: odoo@17ee75a
    
    task-3443430

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2881,7 +2881,9 @@ export class Wysiwyg extends Component {
         if (node?.querySelectorAll) {
             for (const element of node.querySelectorAll('.o_editable, .o_not_editable')) {
                 const editable = element.classList.contains('o_editable');
-                if (element.isContentEditable !== editable) {
+                if(element.hasAttribute('data-oe-not-contenteditable') && !element.firstChild) {
+                    element.contentEditable = false;
+                } else if (element.isContentEditable !== editable) {
                     element.contentEditable = editable;
                 }
             }
