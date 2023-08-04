0a57e8a387ec9e000eefa8d19ec769375b067edf
Claire Bretton (clbr) <clbr@odoo.com>
2023-08-04 10:06:47 +0000

    [IMP] account_edi,l10n_{eg,es,it}_edi{_eta,_sii}: Disable account_edi auto_install
    We change account_edi to be auto_install=False to avoid installing it when
    it is not nedeed and create noise data that will always be empty.
    It was previously auto_installed when Invoicing was installed.
    
    Note that auto_install=['parent_module'] will automatically install the module
    when 'parent_module' is installed (in this case the localization) AND install
    missing dependencies (in particular here 'account_edi').
    
    task-3454076
    
    closes odoo/odoo#130997
    
    X-original-commit: 2d2faafb744abdb09f199ab85f11d317ee1d065e
    Related: odoo/enterprise#45337
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/l10n_es_edi_sii/__manifest__.py
+++ addons/l10n_es_edi_sii/__manifest__.py
@@ -44,6 +44,7 @@
     'external_dependencies': {
         'python': ['pyOpenSSL'],
     },
+    'auto_install': ['l10n_es'],
     'post_init_hook': '_l10n_es_edi_post_init',
     'license': 'LGPL-3',
 }
