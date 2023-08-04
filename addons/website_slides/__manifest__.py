7a5513b729d77b538ce926bb062e35190aed72a9
Aaron Bohy <aab@odoo.com>
2023-08-04 07:50:16 +0000

    [REF] web,web_editor: move SmoothScrollOnDrag to editor
    This commit moves the smooth scroll on drag helper from web to
    web_editor, which allows us to move it from frontend and common
    assets to web_editor.assets_legacy_wysiwyg which is only loaded
    in the website editor, the only place where it is used.
    
    Note that it uses legacy Class and mixins, so it still needs to be
    converted to the modern codebase, alongside editor snippets.
    
    Part of task~3439226
    
    closes odoo/odoo#130813
    
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- addons/website_slides/__manifest__.py
+++ addons/website_slides/__manifest__.py
@@ -236,7 +236,6 @@ Featuring
             'web/static/src/legacy/js/common_env.js',
             'web/static/src/legacy/js/widgets/name_and_signature.js',
             'web/static/src/legacy/xml/name_and_signature.xml',
-            'web/static/src/legacy/js/core/smooth_scroll_on_drag.js',
             'web/static/src/core/**/*.js',
             'web/static/src/env.js',
             'web/static/src/legacy/js/services/session.js',
