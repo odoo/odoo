0afaf3c3eba00b1a70b8f780d5e37dcb4a284e28
Brieuc-brd <brd@odoo.com>
2023-08-04 14:52:23 +0200

    [IMP] website: set up new default images
    This commit updates the demo images according to the new default
    theme redesign.
    
    task-3097005
    
    Part-of: odoo/odoo#120302

--- addons/website/static/tests/tours/snippet_image_quality.js
+++ addons/website/static/tests/tours/snippet_image_quality.js
@@ -29,7 +29,7 @@ wTourUtils.registerWebsitePreviewTour('website_image_quality', {
         content: "Check image size",
         // Reached size cannot be hardcoded because it changes with
         // different versions of Chrome.
-        trigger: 'we-customizeblock-options:has(we-title:contains("Image")) .o_we_image_weight:contains(" kb"):not(:contains("16.1 kb"))',
+        trigger: 'we-customizeblock-options:has(we-title:contains("Image")) .o_we_image_weight:contains(" kb"):not(:contains("41.5 kb"))',
         run() {
             // Make sure the reached size is smaller than the original one.
             if (parseFloat(this.$anchor[0].innerText) >= 47.5) {
@@ -44,12 +44,12 @@ wTourUtils.registerWebsitePreviewTour('website_image_quality', {
     },
     {
         content: "Wait for image update: back to original image",
-        trigger: 'iframe .s_text_image img[src$="QUntVvdqEdljipEUu7ez5lb6IqyGKzQmRxVnZX/2Q=="]',
+        trigger: 'iframe .s_text_image img[src$="0sOnkdNPFFV0lRVRLK+B7PJ5F4If2IY8ngQsDP//Z"]',
         run: () => {}, // It is a check.
     },
     {
         content: "Check image size",
-        trigger: 'we-customizeblock-options:has(we-title:contains("Image")) .o_we_image_weight:contains("47.5 kb")',
+        trigger: 'we-customizeblock-options:has(we-title:contains("Image")) .o_we_image_weight:contains("41.5 kb")',
         run: () => {}, // It is a check.
     },
 ]);
--- addons/website/static/tests/tours/snippet_images_wall.js
+++ addons/website/static/tests/tours/snippet_images_wall.js
@@ -3,9 +3,9 @@
 import wTourUtils from "@website/js/tours/tour_utils";
 
 const selectSignImageStep = {
-    content: "Click on sign image",
+    content: "Click on image 14",
     extra_trigger: ".o_we_customize_panel:not(:has(.snippet-option-GalleryElement))",
-    trigger: "iframe .s_image_gallery img[data-original-src*='sign']",
+    trigger: "iframe .s_image_gallery img[data-original-src*='library_image_14']",
 };
 // Without reselecting the image, the tour manages to click on the
 // move button before the active image is updated.
@@ -32,7 +32,7 @@ selectSignImageStep,
     trigger: ".snippet-option-GalleryElement we-button[data-position='prev']",
 }, {
     content: "Check if sign is in second column",
-    trigger: "iframe .s_image_gallery .o_masonry_col:nth-child(2):has(img[data-index='1'][data-original-src*='sign'])",
+    trigger: "iframe .s_image_gallery .o_masonry_col:nth-child(2):has(img[data-index='1'][data-original-src*='library_image_14'])",
     isCheck: true,
 },
 ...reselectSignImageSteps,
@@ -41,7 +41,7 @@ selectSignImageStep,
     trigger: ".snippet-option-GalleryElement we-button[data-position='first']",
 }, {
     content: "Check if sign is in first column",
-    trigger: "iframe .s_image_gallery .o_masonry_col:nth-child(1):has(img[data-index='0'][data-original-src*='sign'])",
+    trigger: "iframe .s_image_gallery .o_masonry_col:nth-child(1):has(img[data-index='0'][data-original-src*='library_image_14'])",
     isCheck: true,
 },
 ...reselectSignImageSteps,
@@ -50,7 +50,7 @@ selectSignImageStep,
     trigger: ".snippet-option-GalleryElement we-button[data-position='prev']",
 }, {
     content: "Check if sign is in third column",
-    trigger: "iframe .s_image_gallery .o_masonry_col:nth-child(3):has(img[data-index='5'][data-original-src*='sign'])",
+    trigger: "iframe .s_image_gallery .o_masonry_col:nth-child(3):has(img[data-index='5'][data-original-src*='library_image_14'])",
     isCheck: true,
 },
 ...reselectSignImageSteps,
@@ -59,7 +59,7 @@ selectSignImageStep,
     trigger: ".snippet-option-GalleryElement we-button[data-position='next']",
 }, {
     content: "Check if sign is in first column",
-    trigger: "iframe .s_image_gallery .o_masonry_col:nth-child(1):has(img[data-index='0'][data-original-src*='sign'])",
+    trigger: "iframe .s_image_gallery .o_masonry_col:nth-child(1):has(img[data-index='0'][data-original-src*='library_image_14'])",
     isCheck: true,
 },
 ...reselectSignImageSteps,
@@ -68,6 +68,6 @@ selectSignImageStep,
     trigger: ".snippet-option-GalleryElement we-button[data-position='last']",
 }, {
     content: "Check layout",
-    trigger: "iframe .s_image_gallery .o_masonry_col:nth-child(3):has(img[data-index='5'][data-original-src*='sign'])",
+    trigger: "iframe .s_image_gallery .o_masonry_col:nth-child(3):has(img[data-index='5'][data-original-src*='library_image_14'])",
     isCheck: true,
 }]);
