15d270975623975d157b13ad3f789aa7892827de
Laurent Smet <las@odoo.com>
2023-08-07 15:33:45 +0200

    [FIX] account_edi_ubl_cii: Fix call to '_prepare_edi_tax_details'
    This module no longer depends of account_edi.
    Then, `_prepare_edi_tax_details` can't not be used.
    We need to use `_prepare_invoice_aggregated_taxes` instead.
    
    X-original-commit: 89debfc12e2712a03896080a707cee6942b11581

--- addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py
@@ -370,9 +370,11 @@ class AccountEdiXmlUBL20(models.AbstractModel):
         self._validate_taxes(invoice)
 
         # Compute the tax details for the whole invoice and each invoice line separately.
-        taxes_vals = invoice._prepare_edi_tax_details(grouping_key_generator=grouping_key_generator,
-                                                      filter_to_apply=self._apply_invoice_tax_filter,
-                                                      filter_invl_to_apply=self._apply_invoice_line_filter)
+        taxes_vals = invoice._prepare_invoice_aggregated_taxes(
+            grouping_key_generator=grouping_key_generator,
+            filter_tax_values_to_apply=self._apply_invoice_tax_filter,
+            filter_invl_to_apply=self._apply_invoice_line_filter,
+        )
 
         # Fixed Taxes: filter them on the document level, and adapt the totals
         # Fixed taxes are not supposed to be taxes in real live. However, this is the way in Odoo to manage recupel
