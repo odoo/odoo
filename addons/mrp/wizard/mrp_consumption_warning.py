d9fd1f2aac56cb3ae4dd15782d3ef1d082134b10
Tiffany Chang (tic) <tic@odoo.com>
2023-08-08 12:13:24 +0200

    [FIX] mrp: correctly set consumption warning values
    Previous fix odoo/odoo#121602 did not correctly handle the case when not
    all of the qty to manufacture is manufactured (the qtys to change to
    were miscalculated in this case).
    
    Additionally, it missed fixing a few more use cases when setting the
    qtys to match the wizard's lines/qtys:
    - if the UoM of a MO's component line is changed => the correct qty was
      not correctly converted into the move.product_uom's qty (now it is)
    - if a component's move is deleted before the MO is confirmed => the
      move (i.e. the missing component) was not correctly added back into
      the MO (now it is)
    - if there are 2 MO component moves with the same product => both were
      set to the same "correct qty" value (now we only set the first move to
      that qty, others are set to 0 since we have no way of knowing how to
      distribute the qtys otherwise)
    
    Also, since an UserError needed to be added in case of a missing comp
    move for a tracked product, existing error logic has been updated to
    list all applicable products and the message has been improved to be
    more helpful.
    
    Note that the fix for saas-16.4 and earlier is slightly different from
    this fix due to the change in how stock move original demand qtys no
    longer change like they used to (see: odoo/odoo#130342) and because a
    backorder bug was also exposed by this change.
    
    closes odoo/odoo#135796
    
    Task: 3456604
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>

--- addons/mrp/wizard/mrp_consumption_warning.py
+++ addons/mrp/wizard/mrp_consumption_warning.py
@@ -3,7 +3,7 @@
 
 from odoo import _, fields, models, api
 from odoo.exceptions import UserError
-from odoo.tools import float_compare, float_round
+from odoo.tools import float_compare, float_is_zero
 
 
 class MrpConsumptionWarning(models.TransientModel):
@@ -36,23 +36,46 @@ class MrpConsumptionWarning(models.TransientModel):
         return self.mrp_production_ids.with_context(ctx, skip_consumption=True).button_mark_done()
 
     def action_set_qty(self):
-        self.mrp_production_ids.action_assign()
+        missing_move_vals = []
+        problem_tracked_products = self.env['product.product']
         for production in self.mrp_production_ids:
-            for move in production.move_raw_ids:
-                rounding = move.product_uom.rounding
-                for line in self.mrp_consumption_warning_line_ids:
+            for line in self.mrp_consumption_warning_line_ids:
+                if line.mrp_production_id != production:
+                    continue
+                for move in production.move_raw_ids:
                     if line.product_id != move.product_id:
                         continue
-                    if float_compare(line.product_expected_qty_uom, move.product_uom_qty, precision_rounding=rounding) != 0:
-                        move.product_uom_qty = line.product_uom_id._compute_quantity(line.product_expected_qty_uom, move.product_uom)
-                if float_compare(move.quantity_done, move.should_consume_qty, precision_rounding=rounding) == 0:
-                    continue
-                new_qty = float_round((production.qty_producing - production.qty_produced) * move.unit_factor, precision_rounding=move.product_uom.rounding)
-                if move.has_tracking in ('lot', 'serial'):
-                    if not (production.use_auto_consume_components_lots and
-                            float_compare(move.reserved_availability, new_qty, precision_rounding=move.product_uom.rounding) >= 0):
-                        raise UserError(_('You need to supply Lot/Serial Number'))
-                move.quantity_done = new_qty
+                    qty_expected = line.product_uom_id._compute_quantity(line.product_expected_qty_uom, move.product_uom)
+                    qty_compare_result = float_compare(qty_expected, move.quantity_done, precision_rounding=move.product_uom.rounding)
+                    if qty_compare_result != 0:
+                        if (move.has_tracking in ('lot', 'serial')
+                            and not production.use_auto_consume_components_lots
+                            and qty_compare_result > 0):
+                            problem_tracked_products |= line.product_id
+                            break
+                        move.quantity_done = qty_expected
+                    # in case multiple lines with same product => set others to 0 since we have no way to know how to distribute the qty done
+                    line.product_expected_qty_uom = 0
+                # move was deleted before confirming MO or force deleted somehow
+                if not float_is_zero(line.product_expected_qty_uom, precision_rounding=line.product_uom_id.rounding):
+                    if line.product_id.tracking in ('lot', 'serial') and not line.mrp_production_id.use_auto_consume_components_lots:
+                        problem_tracked_products |= line.product_id
+                        continue
+                    missing_move_vals.append({
+                        'product_id': line.product_id.id,
+                        'product_uom': line.product_uom_id.id,
+                        'product_uom_qty': line.product_expected_qty_uom,
+                        'quantity_done': line.product_expected_qty_uom,
+                        'raw_material_production_id': line.mrp_production_id.id,
+                        'additional': True,
+                    })
+        if problem_tracked_products:
+            raise UserError(
+                _("Values cannot be set and validated because a Lot/Serial Number needs to be specified for a tracked product that is having its consumed amount increased:\n- ") +
+                "\n- ".join(problem_tracked_products.mapped('name'))
+            )
+        if missing_move_vals:
+            self.env['stock.move'].create(missing_move_vals)
         return self.action_confirm()
 
     def action_cancel(self):
