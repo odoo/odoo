11b656f7f48e1d645767a93711e350b409448769
shsa-odoo <shsa@odoo.com>
2023-08-08 11:32:01 +0530

    [FIX] web_editor: remove toolbar on selection collape in table
    Before this commit:
    
    when selected cell is clicked, its selection collapses yet toolbar is visible.
    
    After this commit:
    
    Now toolbar is removed when selection collapses.
    
    task-3458048
    
    closes odoo/odoo#136488
    
    X-original-commit: 89116f6f356df0b33ba61f8b2e725ae7530bdceb
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -4370,6 +4370,7 @@ export class OdooEditor extends EventTarget {
             this.toolbar.style.pointerEvents = 'none';
             if (this.deselectTable() && hasValidSelection(this.editable)) {
                 this.document.getSelection().collapseToStart();
+                this._updateToolbar(false);
             }
         }
         // Handle table resizing.
