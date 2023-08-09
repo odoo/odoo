ff38ed72bec502ad42952391cf2394d2b68a23ca
Gaetan Vanden Bergh (gavb) <gavb@odoo.com>
2023-08-09 14:50:01 +0200

    [FIX] *: assertAlmostEqual parameters identification
    purchase_stock, point_of_sale, sale_mrp
    
    What are the steps to reproduce your issue?
    When Odoo.sh runs unit tests on a development branch,
    it tests the code in the MRP module for rounding errors.
    
    What is the current behavior that you observe?
    I had a customization that introduced a rounding error in the standard MRP module.
    When the unit tests ran on the development branch, I got a traceback.
    
    What would be your expected behavior in this case?
    The rounding error should be reported as a failed test with a message. No traceback.
    
    opw-3454998

--- addons/point_of_sale/tests/test_pos_products_with_tax.py
+++ addons/point_of_sale/tests/test_pos_products_with_tax.py
@@ -92,7 +92,7 @@ class TestPoSProductsWithTax(TestPoSCommon):
         # check values before closing the session
         self.assertEqual(3, self.pos_session.order_count)
         orders_total = sum(order.amount_total for order in self.pos_session.order_ids)
-        self.assertAlmostEqual(orders_total, self.pos_session.total_payments_amount, 'Total order amount should be equal to the total payment amount.')
+        self.assertAlmostEqual(orders_total, self.pos_session.total_payments_amount, msg='Total order amount should be equal to the total payment amount.')
 
         # close the session
         self.pos_session.action_pos_session_validate()
