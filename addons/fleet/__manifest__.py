947d037370ca151d7cfcde624da871e13d1ea873
Jurgen (jugj) <jugj@odoo.com>
2023-08-09 13:24:48 +0200

    [IMP] fleet: Add possibility to mass mail drivers
    task-3459884
    
    closes odoo/odoo#131433
    
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/fleet/__manifest__.py
+++ addons/fleet/__manifest__.py
@@ -41,6 +41,7 @@ Main Features
         'data/fleet_data.xml',
         'data/mail_message_subtype_data.xml',
         'data/mail_activity_type_data.xml',
+        'wizard/fleet_vehicle_send_mail_views.xml'
     ],
 
     'demo': ['data/fleet_demo.xml'],
