3222b078bbb917cd2b5e0afff58109b98b0af63c
Jinjiu Liu <jili@odoo.com>
2023-08-10 14:01:02 +0200

    [FIX] web_editor: cursor one step late in collaboration
    Reproduction:
    1. Install note
    2. Open an existing note and open it again in another tab
    3. When you click in one tab, the cursor position in the other one is
    always one step behind
    
    Fix: separate the logic between collaboration and appliedCustomSelection
    and revert the calling of getCurrentCollaborativeSelection to the place
    after _computeHistorySelection
    
    Task-3221119
    
    X-original-commit: 854ef7df4db577d010bed9a3561c57b3700447d8

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -3961,9 +3961,6 @@ export class OdooEditor extends EventTarget {
             //    wasn't changed.
             this._setLinkZws();
 
-            if (this.options.onCollaborativeSelectionChange) {
-                this.options.onCollaborativeSelectionChange(this.getCurrentCollaborativeSelection());
-            }
         }
         const isSelectionInEditable = this.isSelectionInEditable(selection);
         if (!appliedCustomSelection) {
@@ -3995,6 +3992,13 @@ export class OdooEditor extends EventTarget {
 
         if (this._currentMouseState === 'mouseup') {
             this._fixFontAwesomeSelection();
+        }
+        if (
+            selection.rangeCount &&
+            selection.getRangeAt(0) &&
+            this.options.onCollaborativeSelectionChange
+        ) {
+            this.options.onCollaborativeSelectionChange(this.getCurrentCollaborativeSelection());
         }
     }
 
