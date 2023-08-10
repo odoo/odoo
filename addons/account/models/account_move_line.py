1a714722459a18814cab6cb149d925412a7dd9df
Laurent Smet <las@odoo.com>
2023-08-10 09:04:17 +0000

    [FIX] account: Fix reconciliation with very tiny rate with one foreign currency
    Suppose a very tiny rate 1:10000.
    l1 is expressed in company currency and has an amount of -10.
    l2 is expressed in foreign currency and has 1000000 in company currency for 100 in foreign currency.
    
    The reconciliation is made in foreign currency but there is nothing left to reconcile after converting
    -10 to the foreign currency because -10 / 10000 = 0.0001 ~= 0.0
    So no partial is created.
    
    After this fix, the reconciliation fallbacks to a reconciliation in company currency if there is nothing
    left to reconcile after applying the rate.
    
    closes odoo/odoo#131519
    
    Issue: 128234
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -1715,6 +1715,16 @@ class AccountMoveLine(models.Model):
             credit_rate = get_accounting_rate(credit_vals)
             recon_debit_amount = recon_currency.round(remaining_debit_amount * debit_rate)
             recon_credit_amount = -remaining_credit_amount_curr
+
+            # If there is nothing left after applying the rate to reconcile in foreign currency,
+            # try to fallback on the company currency instead.
+            if recon_currency.is_zero(recon_debit_amount) or recon_currency.is_zero(recon_credit_amount):
+                recon_currency = company_currency
+                debit_rate = 1
+                credit_rate = None
+                recon_debit_amount = remaining_debit_amount
+                recon_credit_amount = -remaining_credit_amount
+
         elif debit_vals['currency'] != company_currency \
                 and is_rec_pay_account \
                 and not has_debit_zero_residual_currency \
@@ -1727,6 +1737,16 @@ class AccountMoveLine(models.Model):
             credit_rate = get_odoo_rate(credit_vals)
             recon_debit_amount = remaining_debit_amount_curr
             recon_credit_amount = recon_currency.round(-remaining_credit_amount * credit_rate)
+
+            # If there is nothing left after applying the rate to reconcile in foreign currency,
+            # try to fallback on the company currency instead.
+            if recon_currency.is_zero(recon_debit_amount) or recon_currency.is_zero(recon_credit_amount):
+                recon_currency = company_currency
+                debit_rate = None
+                credit_rate = 1
+                recon_debit_amount = remaining_debit_amount
+                recon_credit_amount = -remaining_credit_amount
+
         elif debit_vals['currency'] == credit_vals['currency'] \
                 and debit_vals['currency'] != company_currency \
                 and not has_debit_zero_residual_currency \
