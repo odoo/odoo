2dea94ebd2e6f1de08ff5ca94aa53283e8e33631
dane@odoo.com <dane@odoo.com>
2023-08-10 08:51:42 +0200

    [IMP] event: Generate QR
    This PR allows organizers to use QR codes without needing a barcode.
    
    task-3411827

--- addons/event/controllers/main.py
+++ addons/event/controllers/main.py
@@ -135,3 +135,23 @@ class EventController(Controller):
             ('id', 'in', registration_ids),
             ('event_id', '=', event_id)
         ])
+
+    @route(['/event/init_barcode_interface'], type='json', auth="user")
+    def init_barcode_interface(self, event_id):
+        event = request.env['event.event'].browse(event_id).exists() if event_id else False
+        if event:
+            return {
+                'name': event.name,
+                'country': event.address_id.country_id.name,
+                'city': event.address_id.city,
+                'company_name': event.company_id.name,
+                'company_id': event.company_id.id
+            }
+        else:
+            return {
+                'name': _('Registration Desk'),
+                'country': False,
+                'city': False,
+                'company_name': request.env.company.name,
+                'company_id': request.env.company.id
+            }
