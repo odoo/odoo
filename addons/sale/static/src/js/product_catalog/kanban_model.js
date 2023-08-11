90f1aa792f6fb9d3c43f1e8378726ab6e84a9e41
FrancoisGe <fge@odoo.com>
2023-08-11 12:28:33 +0000

    [FIX] sale: remove product in product catalog
    Since the new Relational Model (PR 114024), it was no longer possible to
    remove a product from the Product Catalog view. This commit adapts the
    ProductCatalogModel to the new model.
    
    closes odoo/odoo#131734
    
    Signed-off-by: Samuel Degueldre (sad) <sad@odoo.com>

--- addons/sale/static/src/js/product_catalog/kanban_model.js
+++ addons/sale/static/src/js/product_catalog/kanban_model.js
@@ -1,17 +1,30 @@
 /** @odoo-module */
 
+import { Record } from "@web/model/relational_model/record";
 import { RelationalModel } from "@web/model/relational_model/relational_model";
 
-export class ProductCatalogKanbanModel extends RelationalModel {
-    async load() {
-        await super.load(...arguments);
+class ProductCatalogRecord extends Record {
+    setup(config, data, options = {}) {
+        this.productCatalogData = data.productCatalogData;
+        data = { ...data };
+        delete data.productCatalogData;
+        super.setup(config, data, options);
+    }
+}
 
+export class ProductCatalogKanbanModel extends RelationalModel {
+    static Record = ProductCatalogRecord;
+
+    async _loadData(params) {
+        const result = await super._loadData(...arguments);
         const saleOrderLinesInfo = await this.rpc("/sales/catalog/sale_order_lines_info", {
-            order_id: this.config.context.order_id,
-            product_ids: this.root.records.map((rec) => rec.resId),
+            order_id: params.context.order_id,
+            product_ids: result.records.map((rec) => rec.id),
         });
-        for (const record of this.root.records) {
-            record.productCatalogData = saleOrderLinesInfo[record.resId];
+        for (const record of result.records) {
+            record.productCatalogData = saleOrderLinesInfo[record.id];
+            record.productCatalogData.quantity = record.productCatalogData.quantity || 0;
         }
+        return result;
     }
 }
