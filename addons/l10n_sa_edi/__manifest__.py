422dd44ea3896eb7ae21835a71597b61c49d7de8
Claire Bretton (clbr) <clbr@odoo.com>
2023-08-11 12:43:42 +0200

    [FIX] l10n_sa_edi: fix dependencies
    l10n_sa_edi uses account_edi methods but has no dependency
    on it. It still works since for the moment account_edi is
    auto_installed with account but it won't be the case starting
    from saas-16.4.
    
    Related: https://github.com/odoo/odoo/pull/130836

--- addons/l10n_sa_edi/__manifest__.py
+++ addons/l10n_sa_edi/__manifest__.py
@@ -6,6 +6,7 @@
     'icon': '/l10n_sa/static/description/icon.png',
     'version': '0.1',
     'depends': [
+        'account_edi',
         'account_edi_ubl_cii',
         'account_debit_note',
         'l10n_sa',
