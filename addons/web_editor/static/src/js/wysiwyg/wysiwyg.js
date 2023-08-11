ce1760c242f2ecdee8a0440740e3eb860f4e7a4a
David Monjoie <dmo@odoo.com>
2023-08-11 14:42:14 +0200

    [FIX] web_editor: fix traceback when reconnecting
    X-original-commit: 581ec9209a2d5cc0f13de9ccb6f10e6255ba4959

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2602,7 +2602,7 @@ const Wysiwyg = Widget.extend({
             this.preSavePromiseResolve();
             resetPreSavePromise();
         } catch (e) {
-            this.preSavePromiseReject(e);
+            this.preSavePromiseReject && this.preSavePromiseReject(e);
             resetPreSavePromise();
         }
     },
