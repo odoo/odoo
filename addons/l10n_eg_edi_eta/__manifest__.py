odoo/odoo

commit 2d2faafb744abdb09f199ab85f11d317ee1d065e

[IMP] account_edi,l10n_{eg,es,it}_edi{_eta,_sii}: Disable account_edi auto_install
We change account_edi to be auto_install=False to avoid installing it when
it is not nedeed and create noise data that will always be empty.
It was previously auto_installed when Invoicing was installed.

Note that auto_install=['parent_module'] will automatically install the module
when 'parent_module' is installed (in this case the localization) AND install
missing dependencies (in particular here 'account_edi').

task-3454076

closes odoo/odoo#130836

Related: odoo/enterprise#45244
Signed-off-by: Laurent Smet (las) <las@odoo.com>

addons/l10n_eg_edi_eta/__manifest__.py
@@ -14,6 +14,7 @@
     'version': '0.2',
     'license': 'LGPL-3',
     'depends': ['account_edi', 'l10n_eg'],
+    'auto_install': ['l10n_eg'],
     'data': [
         'data/account_edi_data.xml',
         'data/l10n_eg_edi.activity.type.csv',

addons/account_edi/__manifest__.py
@@ -21,6 +21,5 @@ governements, etc.)
         'data/cron.xml'
     ],
     'installable': True,
-    'auto_install': True,
     'license': 'LGPL-3',
 }

addons/l10n_es_edi_sii/__manifest__.py
@@ -44,6 +44,7 @@
     'external_dependencies': {
         'python': ['pyOpenSSL'],
     },
+    'auto_install': ['l10n_es'],
     'post_init_hook': '_l10n_es_edi_post_init',
     'license': 'LGPL-3',
 }

addons/l10n_it_edi/__manifest__.py
@@ -12,7 +12,7 @@
         'account_edi',
         'account_edi_proxy_client',
     ],
-    'auto_install': ['l10n_it', 'account_edi'],
+    'auto_install': ['l10n_it'],
     'description': """
 E-invoice implementation
     """,

commit 26c0684ce2681aeb528dd769d1ff20d07ed291ff

[FIX] l10n_eg_edi_eta: Remove auto-install
The auto-install has wrongly been introduced by:
https://github.com/odoo/odoo/commit/2d2faafb744abdb09f199ab85f11d317ee1d065e

l10n_es_edi_sii has already been fixed by:
https://github.com/odoo-dev/odoo/commit/02f72d6a2ee73e844537e27594d02c9533954d26

addons/l10n_eg_edi_eta/__manifest__.py
@@ -14,7 +14,6 @@
     'version': '0.2',
     'license': 'LGPL-3',
     'depends': ['account_edi', 'l10n_eg'],
-    'auto_install': ['l10n_eg'],
     'data': [
         'data/account_edi_data.xml',
         'data/l10n_eg_edi.activity.type.csv',