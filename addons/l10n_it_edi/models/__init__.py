f56508ca24477159249aa91aa66c71a52b0d8987
MerlinGuillaume <megu@odoo.com>
2023-08-16 07:30:33 +0200

    [FIX] l10n_it_edi: create 0% EU tax with correct exoneration values
    When creating a new company with the italian fiscal localization, the
    tax "0% EU" is not instanciated properly
    
    Steps to reproduce:
    1. Install Accounting and module l10n_it_edi
    2. Create a new company and select it
    3. Go to Settings > Accounting > Fiscal Localization, set it to `Italy -
       Generic Chart of Accounts` and save
    4. Go to Accounting > Configuration > Taxes and open tax "0% EU"
    5. The fields for the italian exoneration are not properly set, they
       should have the same values as the same tax for company "IT Company"
       of the demo data
    
    Solution:
    When we set the company chart template, update tax "0% EU" with the
    correct values
    
    opw-3414593
    
    closes odoo/odoo#136423
    
    X-original-commit: f6b386c3b840ea7c5ce575f3ba80dfdf9ec3a722
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Guillaume Merlin (megu) <megu@odoo.com>

--- addons/l10n_it_edi/models/__init__.py
+++ addons/l10n_it_edi/models/__init__.py
@@ -5,6 +5,7 @@ from . import res_partner
 from . import res_company
 from . import account_invoice
 from . import account_edi_format
+from . import account_chart_template
 from . import ir_mail_server
 from . import ddt
 from . import mail_template
