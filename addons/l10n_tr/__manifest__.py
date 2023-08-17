ca77e01c4902ed9000961cf6e24daae11aded4f2
Daniel Kosky (dako) <dako@odoo.com>
2023-08-17 12:17:58 +0000

    [IMP] l10n_tr: KDV 20%
    Turkey has adapted its standard VAT from 18% up to 20%
    This commit sets the 18% VAT taxes and its tax group to inactive and
    introduces the respective 20% taxes and a 20% tax group.
    
    Task-id: None
    X-original-commit: c37da088f1b2ac8ca1c5443126477dc0dbb21261
    Co-authored-by: Ramazan Subaşı <subasiramazan@gmail.com>

--- addons/l10n_tr/__manifest__.py
+++ addons/l10n_tr/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Turkey - Accounting',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
 Türkiye için Tek düzen hesap planı şablonu Odoo Modülü.
