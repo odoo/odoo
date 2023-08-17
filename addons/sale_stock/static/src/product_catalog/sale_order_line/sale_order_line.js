7fa6afcf78e56af84c668c3996cf6d7e186ee036
Bastien (bvdn) <bvdn@odoo.com>
2023-08-17 14:56:58 +0200

    [IMP] sale_stock: adapt product_catalog for stock module
    When the stock module is installed some conditions changes in the sale orders
    Here's the list and the implementation made to help this
    
    Patched the kanban_record so that the asked_qty of a product cannot go below the delivered_qty
    
    the delivered_qty is added in the productCatalogData of the record through the _get_catalog_info() hook in the following PR:
    https://github.com/odoo/odoo/pull/132341/commits
    
    Patched the sale_order_line xml to disabled removeProduct and decreaseProduct buttons when the quantity is equal to the
    delivered quantity
    
    if the user tries to remove an already delivered product it'll set to the delivered quantity instead of 0
    if the user tries to set a quantity that is below the delivered quantity it'll set it to the delivered quantity
    
    Added action action_product_forecast_report to the menuitem of the products
    
    Task-3343547

--- addons/sale_stock/static/src/product_catalog/sale_order_line/sale_order_line.js
+++ addons/sale_stock/static/src/product_catalog/sale_order_line/sale_order_line.js
@@ -2,10 +2,23 @@
 
 import { ProductCatalogSOL } from "@sale/js/product_catalog/sale_order_line/sale_order_line";
 import { patch } from "@web/core/utils/patch";
+import { _t } from "@web/core/l10n/translation";
 
 patch(ProductCatalogSOL, {
     props: {
         ...ProductCatalogSOL.props,
         deliveredQty: Number,
+    },
+});
+
+patch(ProductCatalogSOL.prototype, {
+    get disableRemove() {
+        return this.props.quantity === this.props.deliveredQty;
+    },
+    get disabledButtonTooltip() {
+        if (this.disableRemove) {
+            return _t("The ordered quantity cannot be decreased below the amount already delivered. Instead, create a return in your inventory.");
+        }
+        return super.disabledButtonTooltip;
     },
 });
