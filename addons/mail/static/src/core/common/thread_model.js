3f4ab2fbb6dfdd38bc3657b5ebc5d46e5b8ef376
Loan (LSE) <lse@odoo.com>
2023-08-18 12:06:46 +0000

    [FIX] mail: "Invalid Date" errors on iOS Safari
    Before this commit:
    JS errors on iOS with Safari's browser due to the date syntax not being
    supported:
    ```js
    new Date("2023-08-18 14:02:53")
    -> Invalid Date
    ```
    This does prevent Odoo's backend to start up on the browser and the app
    on iOS devices.
    
    opw-3465337
    
    X-original-commit: a39391d0df644fce86f435ba3f39e7634d8e5172

--- addons/mail/static/src/core/common/thread_model.js
+++ addons/mail/static/src/core/common/thread_model.js
@@ -3,6 +3,7 @@
 import { ScrollPosition } from "@mail/core/common/scroll_position_model";
 import { createLocalId } from "@mail/utils/common/misc";
 
+import { deserializeDateTime } from "@web/core/l10n/dates";
 import { _t } from "@web/core/l10n/translation";
 import { Deferred } from "@web/core/utils/concurrency";
 
@@ -430,7 +431,7 @@ export class Thread {
         if (!this.last_interest_dt) {
             return undefined;
         }
-        return luxon.DateTime.fromISO(new Date(this.last_interest_dt).toISOString());
+        return deserializeDateTime(this.last_interest_dt);
     }
 
     /**
