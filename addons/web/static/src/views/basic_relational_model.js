cf10655d6665eca6060613f134d38a1933161b98
niyasraphy <niyasraphyk@gmail.com>
2023-08-20 21:39:56 +0300

    [FIX] web: missing required attribute for html field
    To reproduce
    ============
    
    * referrals -> configuration -> rewards
    * try creating a new reward keeping description empty
    * here description is required html field
    * traceback on clicking save
    
    Problem
    =======
    in case of html field, `this.data[fieldName]` is an object `Markup{''}`,
    so `!this.data[fieldName]` is always `false` as objects are considered
    as true value in JS.
    
    Solution
    ========
    in case of html, check the length of the Markup object
    
    opw-3469539
    
    Co-authored-by: Niyas Raphy <https://github.com/niyasraphy>

--- addons/web/static/src/views/basic_relational_model.js
+++ addons/web/static/src/views/basic_relational_model.js
@@ -243,6 +243,11 @@ export class Record extends DataPoint {
                 case "integer":
                 case "monetary":
                     continue;
+                case "html":
+                    if (this.isRequired(fieldName) && this.data[fieldName].length === 0) {
+                        this._setInvalidField(fieldName);
+                    }
+                    break;
                 case "properties":
                     if (!this.checkPropertiesValidity(fieldName)) {
                         this._setInvalidField(fieldName);
