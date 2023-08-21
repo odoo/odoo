777eb767558462f3982146a2b7c4ad1897fa617e
Pierre Paridans <app@odoo.com>
2023-08-21 13:55:56 +0000

    [REF] web,hr_holidays,website_slides: remove legacy Popover component
    Replace usage of legacy Popover component.
    
    Also, small css cleanup in hr_holidays.
    
    task-3439226
    
    closes odoo/odoo#132604
    
    Related: odoo/enterprise#46062
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>

--- addons/hr_holidays/static/src/dashboard/time_off_card.js
+++ addons/hr_holidays/static/src/dashboard/time_off_card.js
@@ -1,18 +1,30 @@
 /* @odoo-module */
 
-import Popover from "@web/legacy/js/core/popover";
+import { usePopover } from "@web/core/popover/popover_hook";
 
 const { Component } = owl;
 
 export class TimeOffCardPopover extends Component {}
-TimeOffCardPopover.components = { Popover };
 
 TimeOffCardPopover.template = 'hr_holidays.TimeOffCardPopover';
-TimeOffCardPopover.props = ['allocated', 'approved', 'planned', 'left'];
+TimeOffCardPopover.props = ['allocated', 'approved', 'planned', 'left', 'close?'];
 
-export class TimeOffCard extends Component {}
+export class TimeOffCard extends Component {
+    setup() {
+        this.popover = usePopover(TimeOffCardPopover, { position: "right", popoverClass: "bg-view" });
+    }
 
-TimeOffCard.components = { TimeOffCardPopover };
+    onClickInfo(ev) {
+        const { data } = this.props;
+        this.popover.open(ev.target, {
+            allocated: data.max_leaves,
+            approved: data.leaves_approved,
+            planned: data.leaves_requested,
+            left: data.virtual_remaining_leaves,
+        });
+    }
+}
+
 TimeOffCard.template = 'hr_holidays.TimeOffCard';
 TimeOffCard.props = ['name', 'id', 'data', 'requires_allocation'];
 
