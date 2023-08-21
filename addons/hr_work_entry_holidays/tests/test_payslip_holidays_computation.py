ddc7007c9cbd1473a1afa33e736d00d2b56562f0
pash-odoo <pash@odoo.com>
2023-08-21 16:41:15 +0530

    [REF] hr_work_entry_holidays,hr_holidays_contract: create bridge module between hr_holidays and hr_contract
    Before this commit, there is no module between hr_holidays and hr_contracts if
    both module are installed so time off duration computation is not correct if we
    dont use work entry.
    In this commit, moved leave and contract related code from
    hr_work_entry_holidays to a new module hr_holidays_contract which work as bridge
    module between hr_holiday and hr_contracts.
    
    task-3462279

--- addons/hr_work_entry_holidays/tests/test_payslip_holidays_computation.py
+++ addons/hr_work_entry_holidays/tests/test_payslip_holidays_computation.py
@@ -8,27 +8,8 @@ from odoo.addons.hr_work_entry_holidays.tests.common import TestWorkEntryHoliday
 
 class TestPayslipHolidaysComputation(TestWorkEntryHolidaysBase):
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        cls.leave_type = cls.env['hr.leave.type'].create({
-            'name': 'Legal Leaves',
-            'time_type': 'leave',
-            'requires_allocation': 'no',
-            'work_entry_type_id': cls.work_entry_type_leave.id
-        })
-
     def test_work_data(self):
-        start = datetime(2015, 11, 8, 8, 0)
-        end = datetime(2015, 11, 10, 22, 0)
-        leave = self.env['hr.leave'].create({
-            'name': 'Doctor Appointment',
-            'employee_id': self.jules_emp.id,
-            'holiday_status_id': self.leave_type.id,
-            'request_date_from': start,
-            'request_date_to': end,
-        })
+        leave = self.create_leave(datetime(2015, 11, 8, 8, 0), datetime(2015, 11, 10, 22, 0), name="Doctor Appointment", employee_id=self.jules_emp.id)
         leave.action_approve()
 
         work_entries = self.jules_emp.contract_ids.generate_work_entries(date(2015, 11, 10), date(2015, 11, 21))
