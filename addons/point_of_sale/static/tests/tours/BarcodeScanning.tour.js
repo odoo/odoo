6d21663782693c025573d31895efcb1beb23181c
Nils Hamerlinck <nils@hamerlinck.fr>
2023-08-22 07:51:23 +0000

    [IMP] point_of_sale: avoid barcode collision between demo and tests data
    closes odoo/odoo#133840
    
    X-original-commit: 2af924713e3d9096e90ac2d9c9a34354a9905c6d
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/point_of_sale/static/tests/tours/BarcodeScanning.tour.js
+++ addons/point_of_sale/static/tests/tours/BarcodeScanning.tour.js
@@ -19,15 +19,15 @@ ProductScreen.do.scan_barcode("0123456789");
 ProductScreen.check.selectedOrderlineHas('Monitor Stand', 2);
 
 // Test "Prices product" EAN-13 `23.....{NNNDD}` barcode pattern
-ProductScreen.do.scan_ean13_barcode("2301000000006");
+ProductScreen.do.scan_ean13_barcode("2305000000004");
 ProductScreen.check.selectedOrderlineHas('Magnetic Board', 1, "0.00");
-ProductScreen.do.scan_ean13_barcode("2301000123453");
+ProductScreen.do.scan_ean13_barcode("2305000123451");
 ProductScreen.check.selectedOrderlineHas('Magnetic Board', 1, "123.45");
 
 // Test "Weighted product" EAN-13 `21.....{NNDDD}` barcode pattern
-ProductScreen.do.scan_ean13_barcode("2100002000000");
+ProductScreen.do.scan_ean13_barcode("2100005000000");
 ProductScreen.check.selectedOrderlineHas('Wall Shelf Unit', 0, "0.00");
-ProductScreen.do.scan_ean13_barcode("2100002080000");
+ProductScreen.do.scan_ean13_barcode("2100005080002");
 ProductScreen.check.selectedOrderlineHas('Wall Shelf Unit', 8);
 
 registry.category("web_tour.tours").add("BarcodeScanningTour", { test: true, url: '/pos/ui', steps: getSteps() });
