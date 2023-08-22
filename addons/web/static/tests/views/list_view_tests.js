3b13da8d8c3d4998819a1cf405ed3aa7513893f7
Meow <luciferevgeniy@bk.ru>
2023-08-22 21:16:49 +0600

    [FIX] Add test to check monetary aggregation error

--- addons/web/static/tests/views/list_view_tests.js
+++ addons/web/static/tests/views/list_view_tests.js
@@ -3993,9 +3993,9 @@ QUnit.module("Views", (hooks) => {
                 serverData,
                 arch: `
                 <tree>
-                    <field name="company_currency_id" invisible="1"/>
-                    <field name="currency_id" invisible="1"/>
-                    <field name="amount"/>
+                    <field name="company_currency_id" column_invisible="1"/>
+                    <field name="currency_id" column_invisible="1"/>
+                    <field name="amount" sum="Sum"/>
                     <field name="amount_currency"/>
                 </tree>`,
             });
@@ -4014,6 +4014,12 @@ QUnit.module("Views", (hooks) => {
                 target.querySelectorAll("tfoot td")[1].textContent,
                 "—",
                 "aggregates monetary should never work if different currencies are used"
+            );
+            assert.strictEqual(
+                target.querySelectorAll("tfoot td")[2].textContent,
+                "",
+                "monetary aggregation should only be attempted with an active aggregation function" +
+                " when using different currencies"
             );
         }
     );
