69d1776513a5eff06fca84bc070c4678b84829ed
Alexandre Kühn <aku@odoo.com>
2023-08-22 14:40:15 +0000

    [FIX] mail: can use full composer in reply-to from mailbox
    Before this commit, when replying to a message from mailbox
    in the Discuss app (e.g. Inbox or History) and then clicking
    on the "Full Composer" button, there was the following crash:
    
    ```
    Invalid res_ids ['history'] (type <class 'str'>)
    ```
    
    This happens because the full composer button was attempting to open
    the full composer on the mailbox at hand, e.g. 'history', instead of
    the document related to the message, i.e. the origin thread of the
    message.
    
    The code of this button assumes that it's only visible when viewing
    the message in the origin thread. In this case, the shown thread
    (`props.composer.thread`) matches the origin thread (`thread`) of
    message. This is always the case except in mailboxes, so using
    `props.composer.thread` is erroneous in mailboxes.
    
    This commit fixes the issue by correctly opening the full composer of
    the document related to the message, by using `thread` instead of
    `props.composer.thread`, which is the origin thread of the message.
    
    Also make full composer button rounded (`mx-1` => `m-1`)
    
    opw-3453680
    
    closes odoo/odoo#132867
    
    X-original-commit: 845cd839981a97316cdfa058d431a632fb1e43ba
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/composer.js
+++ addons/mail/static/src/core/common/composer.js
@@ -433,7 +433,7 @@ export class Composer extends Component {
     async onClickFullComposer(ev) {
         if (this.props.type !== "note") {
             // auto-create partners of checked suggested partners
-            const emailsWithoutPartners = this.props.composer.thread.suggestedRecipients
+            const emailsWithoutPartners = this.thread.suggestedRecipients
                 .filter((recipient) => recipient.checked && !recipient.persona)
                 .map((recipient) => recipient.email);
             if (emailsWithoutPartners.length !== 0) {
@@ -444,7 +444,7 @@ export class Composer extends Component {
                     const partnerData = partners[index];
                     const persona = this.personaService.insert({ ...partnerData, type: "partner" });
                     const email = emailsWithoutPartners[index];
-                    const recipient = this.props.composer.thread.suggestedRecipients.find(
+                    const recipient = this.thread.suggestedRecipients.find(
                         (recipient) => recipient.email === email
                     );
                     Object.assign(recipient, { persona });
@@ -455,16 +455,16 @@ export class Composer extends Component {
         const context = {
             default_attachment_ids: attachmentIds,
             default_body: escapeAndCompactTextContent(this.props.composer.textInputContent),
-            default_model: this.props.composer.thread.model,
+            default_model: this.thread.model,
             default_partner_ids:
                 this.props.type === "note"
                     ? []
-                    : this.props.composer.thread.suggestedRecipients
+                    : this.thread.suggestedRecipients
                           .filter((recipient) => recipient.checked)
                           .map((recipient) => recipient.persona.id),
-            default_res_ids: [this.props.composer.thread.id],
+            default_res_ids: [this.thread.id],
             default_subtype_xmlid: this.props.type === "note" ? "mail.mt_note" : "mail.mt_comment",
-            mail_post_autofollow: this.props.composer.thread.hasWriteAccess,
+            mail_post_autofollow: this.thread.hasWriteAccess,
         };
         const action = {
             name: this.props.type === "note" ? _t("Log note") : _t("Compose Email"),
@@ -476,10 +476,18 @@ export class Composer extends Component {
             context: context,
         };
         const options = {
-            onClose: () => {
+            onClose: (...args) => {
+                // args === [] : click on 'X'
+                // args === { special: true } : click on 'discard'
+                const isDiscard = args.length === 0 || args[0]?.special;
+                // otherwise message is posted (args === [undefined])
+                if (!isDiscard && this.props.composer.thread.type === "mailbox") {
+                    this.notifySendFromMailbox();
+                }
                 this.clear();
-                if (this.props.composer.thread) {
-                    this.threadService.fetchNewMessages(this.props.composer.thread);
+                this.props.messageToReplyTo?.cancel();
+                if (this.thread) {
+                    this.threadService.fetchNewMessages(this.thread);
                 }
             },
         };
@@ -489,6 +497,13 @@ export class Composer extends Component {
     clear() {
         this.attachmentUploader?.clear();
         this.threadService.clearComposer(this.props.composer);
+    }
+
+    notifySendFromMailbox() {
+        this.env.services.notification.add(
+            sprintf(_t('Message posted on "%s"'), this.thread.displayName),
+            { type: "info" }
+        );
     }
 
     onClickAddEmoji(ev) {
@@ -555,12 +570,9 @@ export class Composer extends Component {
      * @param {postData} postData Message meta data info
      */
     async _sendMessage(value, postData) {
-        const message = await this.threadService.post(this.thread, value, postData);
+        await this.threadService.post(this.thread, value, postData);
         if (this.props.composer.thread.type === "mailbox") {
-            this.env.services.notification.add(
-                sprintf(_t('Message posted on "%s"'), message.originThread.displayName),
-                { type: "info" }
-            );
+            this.notifySendFromMailbox();
         }
         this.suggestion?.clearRawMentions();
         this.suggestion?.clearCannedReponses();
