5cc4a98290daf59a7de8ddd027d1803709afe05e
Thibault Delavallée <tde@odoo.com>
2023-08-23 16:12:08 +0200

    [FIX] test_mail: prepare and fixup tests for alias domains
    Some tests are updated to lessen diff in future tests, especially about
    aliases. Some tests are fixed as they are somehow incorrect (notably
    in gateway testing) but currently passing as mail gateway is quite
    permissive.
    
    Add some tests preparing MC / alias domains configuration notably about
    company / alias synchronization, which is currently only based on config
    parameters.
    
    Also update some tests by using fstrings which are generally more readable.
    
    Finally move some tests to their right file / main testing class to keep
    them ordered by main topic.
    
    Prepares Task-36879 (Mail: Support MultiCompany Aliases)
    
    X-original-commit: odoo/odoo@06e980d43445a51f17c09b7377bc6ea1540e3fee

--- addons/test_mail/tests/test_mail_multicompany.py
+++ addons/test_mail/tests/test_mail_multicompany.py
@@ -12,8 +12,7 @@ from odoo.addons.mail.models.mail_message import Message
 from odoo.addons.mail.tests.common import MailCommon
 from odoo.addons.test_mail.tests.common import TestRecipients
 from odoo.exceptions import AccessError
-from odoo.tests import tagged
-from odoo.tests.common import users, HttpCase
+from odoo.tests import tagged, users, HttpCase
 from odoo.tools import formataddr, mute_logger
 
 
@@ -67,135 +66,6 @@ class TestMultiCompanySetup(MailCommon, TestRecipients):
         # patch registry to simulate a ready environment
         self.patch(self.env.registry, 'ready', True)
         self.flush_tracking()
-
-    @users('erp_manager')
-    @mute_logger('odoo.addons.mail.models.mail_mail', 'odoo.models.unlink')
-    def test_mail_composer_wtpl_populate_new_recipient(self):
-        """ Test auto-populate of auto created partner with related record
-        values when sending a mail with a template.
-        """
-        self.user_erp_manager.write({
-            'groups_id': [(4, self.env.ref('base.group_partner_manager').id)],
-        })
-        companies = self.company_admin + self.company_2
-        test_records = self.env['mail.test.ticket.mc'].create([
-            {
-                'email_from': f'newpartner{idx}@example.com',
-                'company_id': companies[idx].id,
-                'customer_id': False,
-                'mobile_number': f'+3319900{idx:02d}{idx:02d}',
-                'name': f'TestRecord{idx}',
-                'phone_number': False,
-                'user_id': False,
-            } for idx in range(2)
-        ])
-        manual_recipients = test_records.mapped('email_from')
-        template = self.env['mail.template'].create({
-            'email_to': '{{ object.email_from }}',
-            'model_id': self.env['ir.model']._get_id(test_records._name),
-            'partner_to': False,
-        })
-
-        for composition_mode, batch_mode in product(
-            ('comment', 'mass_mail'),
-            (True, False)
-        ):
-            with self.subTest(composition_mode=composition_mode, batch_mode=batch_mode):
-                test_records = test_records if batch_mode else test_records[0]
-
-                self.assertFalse(
-                    self.env['res.partner'].search(
-                        [('email_normalized', 'in', manual_recipients)]
-                    )
-                )
-                ctx = {
-                    'default_composition_mode': composition_mode,
-                    'default_model': test_records._name,
-                    'default_res_ids': test_records.ids,
-                }
-                composer = self.env['mail.compose.message'].with_context(ctx).create({
-                    'template_id': template.id,
-                })
-
-                with self.mock_mail_gateway():
-                    composer._action_send_mail()
-
-                new_partners = self.env['res.partner'].search([('email_normalized', 'in', manual_recipients)],
-                                                              order='email')
-                try:
-                    self.assertEqual(
-                        len(new_partners), len(test_records)
-                    )
-                    self.assertEqual(
-                        new_partners.mapped('company_id'),
-                        test_records.mapped('company_id')
-                    )
-                    self.assertEqual(
-                        new_partners.mapped('mobile'),
-                        test_records.mapped('mobile_number')
-                    )
-                finally:
-                    new_partners.unlink()
-
-    @users('employee')
-    def test_notify_reply_to_computation(self):
-        test_record = self.env['mail.test.gateway'].browse(self.test_record.ids)
-        res = test_record._notify_get_reply_to()
-        self.assertEqual(
-            res[test_record.id],
-            formataddr((
-                "%s %s" % (self.user_employee.company_id.name, test_record.name),
-                "%s@%s" % (self.alias_catchall, self.alias_domain)))
-        )
-
-    @users('employee_c2')
-    def test_notify_reply_to_computation_mc(self):
-        """ Test reply-to computation in multi company mode. Add notably tests
-        depending on user and records company_id / company_ids. """
-
-        # Test1: no company_id field
-        test_record = self.env['mail.test.gateway'].browse(self.test_record.ids)
-        res = test_record._notify_get_reply_to()
-        self.assertEqual(
-            res[test_record.id],
-            formataddr((
-                "%s %s" % (self.user_employee_c2.company_id.name, test_record.name),
-                "%s@%s" % (self.alias_catchall, self.alias_domain)))
-        )
-
-        # Test2: MC environment get default value from env
-        self.user_employee_c2.write({'company_ids': [(4, self.user_employee.company_id.id)]})
-        test_records = self.env['mail.test.multi.company'].create([
-            {'name': 'Test',
-             'company_id': self.user_employee.company_id.id},
-            {'name': 'Test',
-             'company_id': self.user_employee_c2.company_id.id},
-        ])
-        res = test_records._notify_get_reply_to()
-        for test_record in test_records:
-            self.assertEqual(
-                res[test_record.id],
-                formataddr((
-                    "%s %s" % (self.user_employee_c2.company_id.name, test_record.name),
-                    "%s@%s" % (self.alias_catchall, self.alias_domain)))
-            )
-
-        # Test3: get company from record (company_id field)
-        self.user_employee_c2.write({'company_ids': [(4, self.company_3.id)]})
-        test_records = self.env['mail.test.multi.company'].create([
-            {'name': 'Test1',
-            'company_id': self.company_3.id},
-            {'name': 'Test2',
-            'company_id': self.company_3.id},
-        ])
-        res = test_records._notify_get_reply_to()
-        for test_record in test_records:
-            self.assertEqual(
-                res[test_record.id],
-                formataddr((
-                    "%s %s" % (self.company_3.name, test_record.name),
-                    "%s@%s" % (self.alias_catchall, self.alias_domain)))
-            )
 
     @users('employee_c2')
     @mute_logger('odoo.addons.base.models.ir_rule')
