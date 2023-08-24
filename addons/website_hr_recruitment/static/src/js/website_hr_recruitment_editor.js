ba748f23269d7ac2af1c9d4c5736c4c9ca754cd4
Romain Derie <rde@odoo.com>
2023-08-24 11:31:02 +0200

    [FIX] website, *: add missing _lt and missing .pot entries
    There were 2 errors fixed by this commit:
    1. The form registry fields added in [1] and [2] in Odoo 13 had a
       `string` attribute translated with `_t()` while now it should be
       translated with `_lt()`. It was surely working in Odoo 13 but not
       anymore. It's not worth investigating to be sure.
    2. The IMP done at [3] added a lot of fields but all of those forgot the
       `_t()`/`_lt()` translation.
    
    Note that:
    1. A fix to replace `_t` by `_lt` was done at [4] but it was badly done
       as it was not targetting the correct version... and only fixed one of
       many occurences.
    
    [1]: https://github.com/odoo/odoo/pull/32565
    [2]: https://github.com/odoo/enterprise/pull/4063
    [3]: https://github.com/odoo/odoo/commit/617eba941785aa680edb826e14b0009637d60dee
    [4]: https://github.com/odoo/odoo/commit/2be83cae7bcda369ed5bc7806aef845c6c76e687
    
    opw-3471873

--- addons/website_hr_recruitment/static/src/js/website_hr_recruitment_editor.js
+++ addons/website_hr_recruitment/static/src/js/website_hr_recruitment_editor.js
@@ -4,7 +4,7 @@ odoo.define('website_hr_recruitment.form', function (require) {
 var core = require('web.core');
 var FormEditorRegistry = require('website.form_editor_registry');
 
-var _t = core._t;
+const _lt = core._lt;
 
 FormEditorRegistry.add('apply_job', {
     formFields: [{
@@ -12,27 +12,27 @@ FormEditorRegistry.add('apply_job', {
         modelRequired: true,
         name: 'partner_name',
         fillWith: 'name',
-        string: 'Your Name',
+        string: _lt('Your Name'),
     }, {
         type: 'email',
         required: true,
         fillWith: 'email',
         name: 'email_from',
-        string: 'Your Email',
+        string: _lt('Your Email'),
     }, {
         type: 'char',
         required: true,
         fillWith: 'phone',
         name: 'partner_mobile',
-        string: 'Phone Number',
+        string: _lt('Phone Number'),
     }, {
         type: 'char',
         name: 'linkedin_profile',
-        string: 'LinkedIn Profile',
+        string: _lt('LinkedIn Profile'),
     }, {
         type: 'text',
         name: 'description',
-        string: 'Short Introduction',
+        string: _lt('Short Introduction'),
     }, {
         type: 'binary',
         custom: true,
@@ -42,12 +42,12 @@ FormEditorRegistry.add('apply_job', {
         name: 'job_id',
         type: 'many2one',
         relation: 'hr.job',
-        string: _t('Applied Job'),
+        string: _lt('Applied Job'),
     }, {
         name: 'department_id',
         type: 'many2one',
         relation: 'hr.department',
-        string: _t('Department'),
+        string: _lt('Department'),
     }],
     successPage: '/job-thank-you',
 });
