bd203b555fb4fb73750ac54a03aeeec5a9637f76
Baskhuu Lodoikhuu <baskhuujacara@gmail.com>
2023-08-24 13:13:58 +0000

    [IMP] l10n_mn: Changes needed for updated reports
    Necessary changes to:
    - CoA
    - Taxes
    - Fiscal Positions
    - Account Tags
    for supporting up-to-date Mongolian reports.
    
    closes odoo/odoo#134198
    
    X-original-commit: d9d73d2ffda8be99e5977e6922e6a668b2954ae4
    Related: odoo/enterprise#46832
    Signed-off-by: Josse Colpaert <jco@odoo.com>
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>
    Co-authored-by: Antoine Dupuis <andu@odoo.com>

--- addons/l10n_mn/__manifest__.py
+++ addons/l10n_mn/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Mongolia - Accounting',
     'website': 'https://www.odoo.com/documentation/saas-16.3/applications/finance/fiscal_localizations.html',
-    'version': '1.0',
+    'version': '1.1',
     'icon': '/account/static/description/l10n.png',
     'countries': ['mn'],
     'category': 'Accounting/Localizations/Account Charts',
