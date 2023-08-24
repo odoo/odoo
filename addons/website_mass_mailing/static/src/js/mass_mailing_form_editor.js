1f9621993b4f07d7a00cfba0eefd86fb85ad1e01
Romain Derie <rde@odoo.com>
2023-08-24 11:31:02 +0200

    [FIX] website, *: add missing _lt and missing .pot entries
    There were 2 errors fixed by this commit:
    1. The form registry fields added in [1] and [2] in Odoo 13 had a
       `string` attribute translated with `_t()` while now it should be
       translated with `_lt()`. It was surely working in Odoo 13 but not
       anymore. It's not worth investigating to be sure.
    2. The IMP done at [3] added a lot of fields but all of those forgot the
       `_t()`/`_lt()` translation.
    
    Note that:
    1. A fix to replace `_t` by `_lt` was done at [4] but it was badly done
       as it was not targetting the correct version... and only fixed one of
       many occurences.
    
    [1]: https://github.com/odoo/odoo/pull/32565
    [2]: https://github.com/odoo/enterprise/pull/4063
    [3]: https://github.com/odoo/odoo/commit/617eba941785aa680edb826e14b0009637d60dee
    [4]: https://github.com/odoo/odoo/commit/2be83cae7bcda369ed5bc7806aef845c6c76e687
    
    opw-3471873
    
    closes odoo/odoo#133692
    
    Related: odoo/enterprise#46590
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website_mass_mailing/static/src/js/mass_mailing_form_editor.js
+++ addons/website_mass_mailing/static/src/js/mass_mailing_form_editor.js
@@ -3,24 +3,24 @@
 import core from "web.core";
 import FormEditorRegistry from "website.form_editor_registry";
 
-const _t = core._t;
+const _lt = core._lt;
 
 FormEditorRegistry.add('create_mailing_contact', {
     formFields: [{
         name: 'name',
         required: true,
-        string: _t('Your Name'),
+        string: _lt('Your Name'),
         type: 'char',
     }, {
         name: 'email',
         required: true,
-        string: _t('Your Email'),
+        string: _lt('Your Email'),
         type: 'email',
     }, {
         name: 'list_ids',
         relation: 'mailing.list',
         required: true,
-        string: _t('Subscribe to'),
+        string: _lt('Subscribe to'),
         type: 'many2many',
     }],
 });
