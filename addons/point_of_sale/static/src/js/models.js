6ce8382dd58479afd831812d5fab60922616d11e
roen-odoo <roen@odoo.com>
2023-08-24 15:13:50 +0000

    [FIX] point_of_sale: total due converted in pos currency
    Current behavior:
    If a PoS had a different currency than the company currency, the total
    due was not converted in the PoS currency. So when you paid the total
    due it was not the correct amount.
    
    Fix:
    The total due is now converted in the PoS currency when the PoS is
    loading the data. We also had to change the refreshTotalDueOfPartner to
    retrieve the total due in the PoS currency.
    
    Steps to reproduce:
    - Create a PoS that uses a different currency than the company currency
      (change the journal, pricelist and payment method)
    - Make some payments with this PoS using the customer account
    - Check the total due of the customer account in the PoS, it's not the
      correct value.
    - Try to pay the total due, it's not the correct value either.
    
    opw-3469682
    
    closes odoo/odoo#135530
    
    X-original-commit: a2bf3535ca790e407bfed256c3e884538f344d8a
    Related: odoo/enterprise#47419
    Signed-off-by: Robin Heinz (rhe) <rhe@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/point_of_sale/static/src/js/models.js
+++ addons/point_of_sale/static/src/js/models.js
@@ -1520,16 +1520,6 @@ export class PosGlobalState extends PosModel {
         });
         this._loadProductProduct(product);
     }
-    async refreshTotalDueOfPartner(partner) {
-        const partnerWithUpdatedTotalDue = await this.env.services.rpc({
-            model: "res.partner",
-            method: "search_read",
-            fields: ["total_due"],
-            domain: [["id", "=", partner.id]],
-        });
-        this.db.update_partners(partnerWithUpdatedTotalDue);
-        return partnerWithUpdatedTotalDue;
-    }
     doNotAllowRefundAndSales() {
         return false;
     }
