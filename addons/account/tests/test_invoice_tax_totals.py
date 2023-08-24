8d3c1b01d43e5eaf137d5019a888e7b2b7976fe5
Laurent Smet <las@odoo.com>
2023-08-24 14:12:40 +0200

    [FIX] account: Fix 'division' taxes for l10n_br_avatax
    Since l10n_br_avatax, we face issues regarding the taxes computation with division price-included taxes.
    The case is the following:
    5 taxes of 5, 3, 0.65, 9, 15 having the division type, price_included but not include_base_amount.
    Applied on a base of 48.0, we expect each one to be computed on 48.0 with a base=48.0.
    
    This case is not supported by the current taxes engine finding the total_excluded amount before doing any
    computation. However, the division tax amount can only be computed from the total_included amount.
    
    Here, the total of the 5 divisions taxes is 32.65.
    So on 32.65, we expect a total_excluded amount of 48.0 * 0.6735 = 32.33.
    The computed tax amount for the division tax of 15 is (48.0 - 48.0 * 0.85) = 7.2
    Starting from 32.33, you are no longer able to compute the correct tax amount of 7.2.
    Then, the solution is to compute them when finding the total_excluded and cache the amounts in order
    to avoid further computation later.
    
    opw: 3443703

--- addons/account/tests/test_invoice_tax_totals.py
+++ addons/account/tests/test_invoice_tax_totals.py
@@ -57,15 +57,31 @@ class TestTaxTotals(AccountTestInvoicingCommon):
         group_keys_to_ignore = {'group_key', 'formatted_tax_group_amount', 'formatted_tax_group_base_amount'}
         subtotals_keys_to_ignore = {'formatted_amount'}
 
+        document.invalidate_recordset(fnames=['tax_totals'])
         to_compare = document.tax_totals
 
         for key in main_keys_to_ignore:
             del to_compare[key]
 
-        for key in group_keys_to_ignore:
-            for groups in to_compare['groups_by_subtotal'].values():
-                for group in groups:
+        for group_key, groups in to_compare['groups_by_subtotal'].items():
+            expected_groups = expected_values['groups_by_subtotal'].get(group_key)
+            for i, group in enumerate(groups):
+                for key in group_keys_to_ignore:
                     del group[key]
+
+                # Fix monetary field to avoid 40.8 != 40.8000000004
+                expected_group = i < len(expected_groups) and expected_groups[i]
+                if expected_group:
+                    for monetary_field in ('tax_group_amount', 'tax_group_base_amount'):
+                        if (
+                            expected_group
+                            and monetary_field in expected_group
+                            and document.currency_id.compare_amounts(
+                                expected_group[monetary_field],
+                                group[monetary_field],
+                            ) == 0
+                        ):
+                            expected_group[monetary_field] = group[monetary_field]
 
         for key in subtotals_keys_to_ignore:
             for subtotal in to_compare['subtotals']:
@@ -829,3 +845,101 @@ class TestTaxTotals(AccountTestInvoicingCommon):
             })
             move.invoice_cash_rounding_id = cash_rounding
             self.assertEqual(move.tax_totals['amount_total'], 120)
+
+    def test_division_taxes_for_l10n_br(self):
+        tax_groups = self.env['account.tax.group'].create([
+            {
+                'name': str(i),
+                'sequence': 1,
+            }
+            for i in range(1, 6)
+        ])
+        taxes = self.env['account.tax'].create([
+            {
+                'name': f"division_{amount}",
+                'amount_type': 'division',
+                'amount': amount,
+                'tax_group_id': tax_groups[0].id,
+                'price_include': True,
+                'include_base_amount': False,
+            }
+            for amount in (5, 3, 0.65, 9, 15)
+        ])
+
+        invoice = self._create_document_for_tax_totals_test([(48.0, taxes)])
+
+        # Same tax group.
+        with self.subTest("Same tax group"):
+            self.assertTaxTotals(invoice, {
+                'amount_total': 48.0,
+                'amount_untaxed': 32.33,
+                'display_tax_base': True,
+                'groups_by_subtotal': {
+                    "Untaxed Amount": [
+                        {
+                            'tax_group_name': taxes.tax_group_id.name,
+                            'tax_group_amount': 15.67,
+                            'tax_group_base_amount': 48.0,
+                            'tax_group_id': taxes.tax_group_id.id,
+                        },
+                    ],
+                },
+                'subtotals': [
+                    {
+                        'name': "Untaxed Amount",
+                        'amount': 32.33,
+                    }
+                ],
+                'subtotals_order': ["Untaxed Amount"],
+            })
+
+        # Multiple tax groups.
+        for tax, tax_group in zip(taxes, tax_groups):
+            tax.tax_group_id = tax_group
+        with self.subTest("Multiple tax groups"):
+            self.assertTaxTotals(invoice, {
+                'amount_total': 48.0,
+                'amount_untaxed': 32.33,
+                'display_tax_base': True,
+                'groups_by_subtotal': {
+                    "Untaxed Amount": [
+                        {
+                            'tax_group_name': tax_groups[0].name,
+                            'tax_group_amount': 2.4,
+                            'tax_group_base_amount': 48.0,
+                            'tax_group_id': tax_groups[0].id,
+                        },
+                        {
+                            'tax_group_name': tax_groups[1].name,
+                            'tax_group_amount': 1.44,
+                            'tax_group_base_amount': 48.0,
+                            'tax_group_id': tax_groups[1].id,
+                        },
+                        {
+                            'tax_group_name': tax_groups[2].name,
+                            'tax_group_amount': 0.31,
+                            'tax_group_base_amount': 48.0,
+                            'tax_group_id': tax_groups[2].id,
+                        },
+                        {
+                            'tax_group_name': tax_groups[3].name,
+                            'tax_group_amount': 4.32,
+                            'tax_group_base_amount': 48.0,
+                            'tax_group_id': tax_groups[3].id,
+                        },
+                        {
+                            'tax_group_name': tax_groups[4].name,
+                            'tax_group_amount': 7.2,
+                            'tax_group_base_amount': 48.0,
+                            'tax_group_id': tax_groups[4].id,
+                        },
+                    ],
+                },
+                'subtotals': [
+                    {
+                        'name': "Untaxed Amount",
+                        'amount': 32.33,
+                    },
+                ],
+                'subtotals_order': ["Untaxed Amount"],
+            })
