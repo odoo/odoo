8daceeb7e3ef4f5a6cdc6b8f66f43cb850424b43
Nishant Jain <niai@odoo.com>
2023-08-24 15:45:10 +0200

    [IMP] web: add DynamicModelFieldSelectorChar widget
    In this commit we create a DynamicModelFieldSelectorChar widget. Purpose is to
    use it in WhatsApp to select fields when trying to make a valid path to find
    information on a given model. Example
    
      <field name="xxx" widget="DynamicModelFieldSelectorChar"
             options="{'only_searchable': true}"/>
    
    It is based on char widget, using only_searchable options.
    
    Task-2377154 (Add WhatsApp Support)
    
    Co-authored-by: Akshat Trivedi <aktr@odoo.com>
    Co-authored-by: Aurélien Warnon <awa@odoo.com>
    Co-authored-by: Gitashri Mantha <gman@odoo.com>
    Co-authored-by: Jigar Vaghela <jva@odoo.com>
    Co-authored-by: Kashyap Patel <kasp@odoo.com>
    Co-authored-by: Khushi Vakil <khva@odoo.com>
    Co-authored-by: Nishant Jain <niai@odoo.com>
    Co-authored-by: Prakash Prajapati <ppr@odoo.com>
    Co-authored-by: Priyanshi patel <prpa@odoo.com>
    Co-authored-by: Rahul Prajapati <rapr@odoo.com>
    Co-authored-by: Renaud Thiry <reth@odoo.com>
    Co-authored-by: Shreya Patel <shpa@odoo.com>
    Co-authored-by: Stéphane Debauche <std@odoo.com>
    Co-authored-by: Zeel Patel <zepa@odoo.com>

---
+++ addons/web/static/src/views/fields/dynamic_widget/dynamic_model_field_selector_char.js
@@ -0,0 +1,74 @@
+/** @odoo-module */
+
+import { CharField, charField } from "@web/views/fields/char/char_field";
+import { registry } from "@web/core/registry";
+import { archParseBoolean } from "@web/views/utils";
+
+import { DynamicModelFieldSelector }  from "./dynamic_model_field_selector";
+
+export class DynamicModelFieldSelectorChar extends CharField {
+    static components = {
+        ...CharField.components,
+        DynamicModelFieldSelector
+    };
+
+    static props = {
+        ...CharField.props,
+        resModel : { type: String, optional: true },
+        onlySearchable: { type: Boolean, optional: true },
+        followRelations: { type: Boolean, optional: true },
+    }
+
+    /**
+     * Update record
+     *
+     * @param {string} value
+     * @private
+     */
+    async _onRecordUpdate(value) {
+        await this.props.record.update({[this.props.name]: value});
+    }
+
+    //---- Getters ----
+    get getSelectorProps() {
+        return {
+            path: this.props.record.data[this.props.name],
+            resModel: this.getResModel(),
+            readonly: this.props.readonly,
+            record: this.props.record,
+            recordProps: this.props,
+            update: this._onRecordUpdate.bind(this),
+            isDebugMode: !!this.env.debug,
+            filter: this.filter.bind(this),
+            followRelations: this.props.followRelations,
+        };
+    }
+
+    filter(fieldDef) {
+        return !this.props.onlySearchable || fieldDef.searchable;
+    }
+
+    getResModel(props = this.props) {
+        const resModel = props.record.data[props.resModel];
+        if (!resModel) {
+            return props.record.resModel;
+        }
+        return resModel;
+    }
+}
+
+DynamicModelFieldSelectorChar.template = "web.DynamicModelFieldSelectorChar";
+
+export const dynamicModelFieldSelectorChar = {
+    ...charField,
+    component: DynamicModelFieldSelectorChar,
+    extractProps({ options }, dynamicInfo) {
+        return {
+            followRelations: options.follow_relations ?? true,
+            onlySearchable: archParseBoolean(options.only_searchable),
+            resModel: options.model,
+        };
+    },
+};
+
+registry.category("fields").add("DynamicModelFieldSelectorChar", dynamicModelFieldSelectorChar);
