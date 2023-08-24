942d0e96e269829b145d6c8be650f9eacd33789d
tong-odoo <tong@odoo.com>
2023-08-24 05:47:58 +0000

    [FIX] hr_work_entry_contract: work entry timezone issue for attendance
    Steps to reproduce:
    
    - Install Attendances app and Employee app
    - Setup an employee with a contract which work entry source is from
    attendance
    - Change the working schedule to different timezone, for example
    Asia/Hong_Kong
    - By default the working schedule should be start to work from 0800 to 1700
    - Now create an attendance in the first day of the contract, i.e.
    1st May 2023, check in time 0700, check out time 2000
    - The total work hours should be 13
    - However, in the work entry, the genearted work entry is 0800-2000, only
    12 work hours.
    
    Current behaviour:
    The generated work entry mismatches the value of the attendance
    
    Expected behaviour:
    The work entry should match the attendance
    
    Explanation:
    This issue is casued by the timezone issue. When the system generate
    the work entry, it will calculate the date_from datetime and date_to datetime.
    Then it will compare between the attendance and the date_from datetime to choose
    the larger datetime to put inside the work entry as start time.
    
    However, the date_from datetime didn't consider the timezone of the working schedule.
    In the above example, Asia/Hong Kong time is UTC+8 time. Therefore if we convert
    the attendance time back to UTC time. It is check in time 30 April 2023, 2300 to
    check out time 1 May 2023, 1200. In the mean time, if we compare between the date_from
    datetime (1 May 2023, 0000) and the attendance check in time (30 April 2023, 2300).
    The system will take the date_from time and therefore the generated work entry time
    is incorrect.
    
    task-3468012
    
    closes odoo/odoo#134533
    
    X-original-commit: 2baf4e3ebcd9ce3beb47ee2881aa3e8bd8a74cbf
    Related: odoo/enterprise#47007
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Co-authored-by: Yannick Tivisse <yti@odoo.com>

--- addons/hr_work_entry_contract/models/hr_employee.py
+++ addons/hr_work_entry_contract/models/hr_employee.py
@@ -1,13 +1,11 @@
 # -*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from collections import defaultdict
 from odoo import fields, models
 
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
-    _description = 'Employee'
 
     def generate_work_entries(self, date_start, date_stop, force=False):
         date_start = fields.Date.to_date(date_start)
@@ -18,12 +16,4 @@ class HrEmployee(models.Model):
         else:
             current_contracts = self._get_all_contracts(date_start, date_stop, states=['open', 'close'])
 
-        new_work_entries = False
-        contracts_by_company = defaultdict(lambda: self.env['hr.contract'])
-        for contract in current_contracts:
-            contracts_by_company[contract.company_id] |= contract
-
-        for company, contracts in contracts_by_company.items():
-            new_work_entries = bool(contracts.with_company(company).sudo()._generate_work_entries(
-                date_start, date_stop, force)) or new_work_entries
-        return new_work_entries
+        return current_contracts.generate_work_entries(date_start, date_stop, force=force)
