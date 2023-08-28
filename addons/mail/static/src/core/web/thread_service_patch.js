e258defd737cfe6982711728b98c0b5fff4074d2
Nasreddin Boulif (bon) <bon@odoo.com>
2023-08-28 17:17:44 +0200

    [FIX] mail: set name with default name instead of email
    Steps to reproduce:
    
      - Install `website_crm` module (for test purpose)
      - Go to `Contact Us` page on website
      - Edit the page
      - Change the submit button to create an opportunity instead, then save
      - Fill the form and save
      - Go back to CRM app and open the opportunity just created
      - In the chatter, try to send a message
      - Click on the checkbox of the suggested recipient (to enable it)
    
    Issue:
    
      - Email is displayed twice in the suggested recipients before click
        (instead of name + email or just email if same as name)
      - Email is set as default name in the opened dialog
    
    Cause:
    
      - Displaying email even if same as name
      - Not setting the fields with default values (received from
        `/mail/thread/data` rpc call)
    
    Solution:
    
      - Don't display email if same as name
      - For the suggested recipient label (next checkbox) : set name with
        default name value if name parsed from mail is same value as email
      - For the default values in the opened dialog: set default values with
        defaults received from `/mail/thread/data` rpc call
    
    opw-3470295
    
    X-original-commit: 02ef0425a1f4ae30d846f6c57489cdebc3b4bbad

--- addons/mail/static/src/core/web/thread_service_patch.js
+++ addons/mail/static/src/core/web/thread_service_patch.js
@@ -165,8 +165,11 @@ patch(ThreadService.prototype, "mail/core/web", {
     async insertSuggestedRecipients(thread, dataList) {
         const recipients = [];
         for (const data of dataList) {
-            const [partner_id, emailInfo, lang, reason] = data;
-            const [name, email] = emailInfo && parseEmail(emailInfo);
+            const [partner_id, emailInfo, lang, reason, customerInfo] = data;
+            let [name, email] = emailInfo ? parseEmail(emailInfo) : [];
+            if ((!name || name === email) && customerInfo?.name) {
+                name = customerInfo.name;
+            }
             recipients.push({
                 id: nextId++,
                 name,
