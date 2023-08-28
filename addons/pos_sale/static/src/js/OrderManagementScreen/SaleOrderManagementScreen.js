ba176d8b36493e899a3001b61544aa2e6df16d3f
roen-odoo <roen@odoo.com>
2023-08-28 17:57:41 +0200

    [FIX] pos_sale: Correctly set manually_set_price on settle order
    Current behavior:
    When settling an order, the manually_set_price field is set to true
    when it shoudln't. This cause issues in the french localization, as it
    was always showing the current price as the old price.
    
    Steps to reproduce:
    - Install the l10n_fr_pos_cert module, and pos_sale
    - Create a new order, add a product and confirm the order
    - Open the PoS
    - Settle the order you created
    - The current price is shown as the old price
    
    opw-3440797

--- addons/pos_sale/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
+++ addons/pos_sale/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
@@ -197,7 +197,7 @@ odoo.define('pos_sale.SaleOrderManagementScreen', function (require) {
                         description: line.name,
                         price: line.price_unit,
                         tax_ids: orderFiscalPos ? undefined : line.tax_id,
-                        price_manually_set: true,
+                        price_manually_set: false,
                         sale_order_origin_id: clickedOrder,
                         sale_order_line_id: line,
                         customer_note: line.customer_note,
