ba0d428a85e8392d8f892872abe56bb7d6c568d3
roen-odoo <roen@odoo.com>
2023-08-29 14:26:12 +0200

    [FIX] sale_timesheet: Fallback on user employee when creating timesheet
    Current behavior:
      If you set a specific sale_order_item for an employee on a project, and
      create a timesheet line from the "My Timesheets" menu,
      the sale_order_item will not be the one you selected for this employee.
      This happens because when you create a timesheet line from the "My
      Timesheets" menu, the employee is not set on the timesheet line.
      To fix this we fallback on the user's employee when creating a timesheet
      line from the "My Timesheets" menu.
    
    Steps to reproduce:
      - Make sure you'r logged in as Mitchel Admin
      - Create a service product that create project and tasks
      - Create a sale order with this product
      - Go to the project and set a sale order item for Mitchel Admin in the
        invoicing tab
      - Go to the "My Timesheets" menu and click "Add Line"
      - Select the project you created
      - The sale order item will not be autofilled with the one set on the
        invoicing tab of the project.
    
    opw-3463849
    
    closes odoo/odoo#133478
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_timesheet/models/account.py
+++ addons/sale_timesheet/models/account.py
@@ -161,7 +161,7 @@ class AccountAnalyticLine(models.Model):
 
     def _get_employee_mapping_entry(self):
         self.ensure_one()
-        return self.env['project.sale.line.employee.map'].search([('project_id', '=', self.project_id.id), ('employee_id', '=', self.employee_id.id)])
+        return self.env['project.sale.line.employee.map'].search([('project_id', '=', self.project_id.id), ('employee_id', '=', self.employee_id.id or self.env.user.employee_id.id)])
 
     def _employee_timesheet_cost(self):
         if self.project_id.pricing_type == 'employee_rate':
