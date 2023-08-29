8a13ca14d3fd251bbf85b1d8949b9d3c6ea76119
Denis Ledoux <dle@odoo.com>
2023-08-29 15:14:44 +0000

    [FIX] sale: apply record rules on order cancellation subject and body
    When rendering the sale order cancellation message,
    the rendering should apply the current user record rules,
    for instance the count of the partner sale orders should
    match the count of what the salesman can see in his ui.
    Otherwise he doesn't understand why he has a different count
    in the UI and in the cancellation message.
    
    In case you want the behavior of seeing all records
    and not just the current salesman records only,
    then you apply within the template itself the
    `sudo()`.
    
    Applying the `sudo` where you actually need it in the template,
    and not computing the full subject/body as sudo,
    offers more granularity.
    
    closes odoo/odoo#135224
    
    X-original-commit: 5d49a08f60b1f299784e2e983e87cdec178642c1
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- addons/sale/tests/__init__.py
+++ addons/sale/tests/__init__.py
@@ -12,6 +12,7 @@ from . import test_reinvoice
 from . import test_sale_flow
 from . import test_sale_onboarding
 from . import test_sale_order
+from . import test_sale_order_cancel
 from . import test_sale_prices
 from . import test_sale_product_attribute_value_config
 from . import test_sale_refund
