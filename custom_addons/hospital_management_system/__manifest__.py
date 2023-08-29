7a3e2650ad03ec3846854e9baf1a47ede1c2b9de
Sakib0017 <sakibkamal0017.com>
2023-08-29 12:29:49 +0600

    i create new folder hospital management system

---
+++ custom_addons/hospital_management_system/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Hospital Management System",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Author Name",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        'security/ir.model.access.csv'
+        'views/hospital.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        
+    ],
+}
