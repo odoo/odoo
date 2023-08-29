a21bd678d47fd4f6075052ecb028ae6cc7730d9e
Tiffany Chang (tic) <tic@odoo.com>
2023-08-29 12:46:42 +0200

    [FIX] mrp,purchase,repair,stock: cleanup of modifier refactoring
    Minor cleanup after odoo/odoo#104741 thanks to edge-cases and the fun
    complexity of logistics code. Fixes include:
    
    - removing some readonly=False that were added on computed/stored fields
      that already have an inverse func or were already not readonly (i.e.
      redundant => cleanup)
    - fixed visibility of qty to prod in subcontractor portal view of a MO
      (i.e. yay they know how much they're supposed to make again)
    - remove unused imports
    - adding back in readonly functionality of default dest of repair
      operation type (and removing the now useless field override that used
      to add in the readonly functionality)
    - adding back in the `_set_product_qty` inverse function since it was
      probably removed by mistake (and is still important to have)
    
    unrelated to viewpocolypse change, also fixed:
    - mobile view of PO was for some reason allowing products to be changed
      in already confirmed/done POs, which could lead to some inconsistent
      data => made this consistent with existing desktop view behavior
    
    closes odoo/odoo#133502
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/purchase_stock/models/purchase.py
+++ addons/purchase_stock/models/purchase.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from markupsafe import Markup
-from dateutil.relativedelta import relativedelta
 
 from odoo import api, Command, fields, models, SUPERUSER_ID, _
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 from odoo.exceptions import UserError
-
-from odoo.addons.purchase.models.purchase import PurchaseOrder as Purchase
 
 
 class PurchaseOrder(models.Model):
--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -48,7 +48,7 @@ class StockMove(models.Model):
         domain="[('type', 'in', ['product', 'consu'])]", index=True, required=True)
     description_picking = fields.Text('Description of Picking')
     product_qty = fields.Float(
-        'Real Quantity', compute='_compute_product_qty',
+        'Real Quantity', compute='_compute_product_qty', inverse='_set_product_qty',
         digits=0, store=True, compute_sudo=True,
         help='Quantity in the default UoM of the product')
     product_uom_qty = fields.Float(
