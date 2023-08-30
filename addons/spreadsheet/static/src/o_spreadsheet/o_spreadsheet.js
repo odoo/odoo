46c916eefcb79607e383b11d0dc095e35c5fce53
Rémi Rahir (rar) <rar@odoo.com>
2023-08-30 10:41:28 +0200

    [FIX] spreadsheet: update o_spreadsheet to latest version
    ### Contains the following commits:
    
    https://github.com/odoo/o-spreadsheet/commit/053a8434 [REL] 16.1.19
    https://github.com/odoo/o-spreadsheet/commit/a3b2e7cb [REV] components: prevent mouse event defaults on drag-and-drop
    
    closes odoo/odoo#133572
    
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet/static/src/o_spreadsheet/o_spreadsheet.js
+++ addons/spreadsheet/static/src/o_spreadsheet/o_spreadsheet.js
@@ -7478,34 +7478,25 @@
     });
 
     function startDnd(onMouseMove, onMouseUp, onMouseDown = () => { }) {
-        const _onMouseDown = (ev) => {
-            ev.preventDefault();
-            onMouseDown(ev);
-        };
-        const _onMouseMove = (ev) => {
-            ev.preventDefault();
-            onMouseMove(ev);
-        };
         const _onMouseUp = (ev) => {
-            ev.preventDefault();
             onMouseUp(ev);
-            window.removeEventListener("mousedown", _onMouseDown);
+            window.removeEventListener("mousedown", onMouseDown);
             window.removeEventListener("mouseup", _onMouseUp);
             window.removeEventListener("dragstart", _onDragStart);
-            window.removeEventListener("mousemove", _onMouseMove);
-            window.removeEventListener("wheel", _onMouseMove);
+            window.removeEventListener("mousemove", onMouseMove);
+            window.removeEventListener("wheel", onMouseMove);
         };
         function _onDragStart(ev) {
             ev.preventDefault();
         }
-        window.addEventListener("mousedown", _onMouseDown);
+        window.addEventListener("mousedown", onMouseDown);
         window.addEventListener("mouseup", _onMouseUp);
         window.addEventListener("dragstart", _onDragStart);
-        window.addEventListener("mousemove", _onMouseMove);
+        window.addEventListener("mousemove", onMouseMove);
         // mouse wheel on window is by default a passive event.
         // preventDefault() is not allowed in passive event handler.
         // https://chromestatus.com/feature/6662647093133312
-        window.addEventListener("wheel", _onMouseMove, { passive: false });
+        window.addEventListener("wheel", onMouseMove, { passive: false });
     }
     /**
      * Function to be used during a mousedown event, this function allows to
@@ -23855,7 +23846,8 @@ day_count_convention (number, default=${DEFAULT_DAY_COUNT_CONVENTION} ) ${_lt("A
             }
             let prevCol = col;
             let prevRow = row;
-            const onMouseMove = (col, row) => {
+            const onMouseMove = (col, row, ev) => {
+                ev.preventDefault();
                 if ((col !== prevCol && col != -1) || (row !== prevRow && row != -1)) {
                     prevCol = col === -1 ? prevCol : col;
                     prevRow = row === -1 ? prevRow : row;
@@ -44312,9 +44304,9 @@ day_count_convention (number, default=${DEFAULT_DAY_COUNT_CONVENTION} ) ${_lt("A
     Object.defineProperty(exports, '__esModule', { value: true });
 
 
-    __info__.version = '16.1.18';
-    __info__.date = '2023-08-09T11:57:26.607Z';
-    __info__.hash = '2e594e3';
+    __info__.version = '16.1.19';
+    __info__.date = '2023-08-30T08:41:20.800Z';
+    __info__.hash = '053a843';
 
 
 })(this.o_spreadsheet = this.o_spreadsheet || {}, owl);
