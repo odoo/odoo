aa3df0237412e9a16cb5580964336ebd506d6eb4
Benjamin Vray <bvr@odoo.com>
2023-08-30 10:34:04 +0200

    [FIX] website: fix number comparison for form field visibility
    Steps to reproduce the bug.
    
    - In website edit mode, add a form snippet to a page.
    - Add a "number" field within this form.
    - Modify the label of this field in the options and name it "Field 1".
    - Add a "text" field to the form.
    - In the options of the "Text" field, select "Visible only if" for the
    "visibility" option.
    - Then, choose "Field 1" and "Is greater than" in the two dropdowns that
    follow.
    - Enter "10" in the input below.
    - Save the page.
    - Enter "2" in the "Field 1" input.
    - Bug: The text field is displayed, even though it should only be
    visible if "Field 1" is greater than 10.
    
    This bug exists since the "conditionally show form fields" feature was
    added to Website (in this commit [1]). The bug happened because we were
    comparing strings instead of numbers.
    
    [1]: https://github.com/odoo/odoo/commit/2dcbfecf3c10687a2cd3af36335d5be70c904fce
    
    opw-3449900
    
    X-original-commit: e4d64c9f4470874ca853bd871064bb8374c8b5cc

--- addons/website/static/src/snippets/s_website_form/000.js
+++ addons/website/static/src/snippets/s_website_form/000.js
@@ -703,13 +703,13 @@
                 case '!set':
                     return !value;
                 case 'greater':
-                    return value > comparable;
+                    return parseFloat(value) > parseFloat(comparable);
                 case 'less':
-                    return value < comparable;
+                    return parseFloat(value) < parseFloat(comparable);
                 case 'greater or equal':
-                    return value >= comparable;
+                    return parseFloat(value) >= parseFloat(comparable);
                 case 'less or equal':
-                    return value <= comparable;
+                    return parseFloat(value) <= parseFloat(comparable);
                 case 'fileSet':
                     return value.name !== '';
                 case '!fileSet':
