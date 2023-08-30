7ea455bb8837fade9a05f5673df9dd43254fa124
Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>
2023-08-30 10:39:19 +0200

    [IMP] account: allow switching from credit note to invoice
    Currently, the user has access to a "Switch into credit note/refund" but
    not the reverse button to go from a credit note/refund to an
    invoice/bill. This is now the case.
    
    This is a partial backport of odoo#103427
    
    task-id 3102251
    
    closes odoo/odoo#133497
    
    X-original-commit: 6f9593a
    Related: odoo/enterprise#46492
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>
    Signed-off-by: Louis Baudoux (lba) <lba@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -3586,12 +3586,16 @@ class AccountMove(models.Model):
         return action
 
     def action_switch_invoice_into_refund_credit_note(self):
-        if any(move.move_type not in ('in_invoice', 'out_invoice') for move in self):
-            raise ValidationError(_("This action isn't available for this document."))
-
         for move in self:
+            if move.posted_before:
+                raise ValidationError(_("You cannot switch the type of a posted document."))
+            if move.move_type == 'entry':
+                raise ValidationError(_("This action isn't available for this document."))
+            in_out, old_move_type = move.move_type.split('_')
+            new_move_type = f"{in_out}_{'invoice' if old_move_type == 'refund' else 'refund'}"
+            move.name = False
             move.write({
-                'move_type': move.move_type.replace('invoice', 'refund'),
+                'move_type': new_move_type,
                 'partner_bank_id': False,
                 'currency_id': move.currency_id.id,
             })
