dce1f96941e8a5337a0c83824145950571825311
Thibault Delavallée <tde@odoo.com>
2023-08-31 16:58:11 +0200

    [FIX] tools, base, mail: add a fallback when parsing wrongly-formatted emails
    With input 'name email@domain.com' (missing chevrons allowing to clearly spot
    the email part) 'getaddresses' returns ('', 'name email@domain.com) i.e. the
    whole input is considered as being the email.
    
    To improve the heuristic we can add a fallback by recalling 'getadresses'
    on the input with spaces replaced by commas when it found only an email and
    no name. The new email will be split into sub pairs allowing to find the real
    email and various name parts, allowing to make a new name / email pair.
    
    Emails should not contain spaces thus this is coherent with email formation.
    This fallback actually comes from a specific code done in '_parse_partner_name'
    of Partner model. Supporting it directly at tools level make the behavior
    coherent for all models.
    
    Task-2612945 (Mail: Defensive email formatting)

--- odoo/addons/base/tests/test_mail.py
+++ odoo/addons/base/tests/test_mail.py
@@ -411,7 +411,7 @@ class TestEmailTools(BaseCase):
         ]
         expected_list = [
             'deboulonneur@example.com',
-            'Déboulonneur deboulonneur@example.com',
+            'deboulonneur@example.com',
             'deboulonneur@example.comdéboulonneur',
             False,
             '@example.com',  # funny
@@ -421,7 +421,7 @@ class TestEmailTools(BaseCase):
         ]
         expected_fmt_utf8_list = [
             f'"{format_name}" <deboulonneur@example.com>',
-            f'"{format_name}" <Déboulonneur deboulonneur@example.com>',
+            f'"{format_name}" <deboulonneur@example.com>',
             f'"{format_name}" <deboulonneur@example.comdéboulonneur>',
             f'"{format_name}" <@>',
             f'"{format_name}" <@example.com>',
@@ -431,7 +431,7 @@ class TestEmailTools(BaseCase):
         ]
         expected_fmt_ascii_list = [
             f'{format_name_ascii} <deboulonneur@example.com>',
-            f'{format_name_ascii} <Déboulonneur deboulonneur@example.com>',
+            f'{format_name_ascii} <deboulonneur@example.com>',
             f'{format_name_ascii} <deboulonneur@example.xn--comdboulonneur-ekb>',
             f'{format_name_ascii} <@>',
             f'{format_name_ascii} <@example.com>',
@@ -487,7 +487,7 @@ class TestEmailTools(BaseCase):
             ['"Super Déboulonneur" <deboulonneur@example.com>'],
             # wrong formatting
             ['"Déboulonneur" <deboulonneur@example.com>'],
-            ['Déboulonneur deboulonneur@example.com'],  # returned as it, a bit strange but hey
+            ['"Déboulonneur" <deboulonneur@example.com>'],  # extra part correctly considered as a name
             ['deboulonneur@example.comDéboulonneur'],  # concatenated, not sure why
             # multi
             ['deboulonneur@example.com'],
