4911dd2a6be3b9dda8b453d1a10d0edf0270fde9
Thibault Delavallée <tde@odoo.com>
2023-08-31 16:58:11 +0200

    [FIX] tools, base, mail: add a fallback when parsing wrongly-formatted emails
    With input 'name email@domain.com' (missing chevrons allowing to clearly spot
    the email part) 'getaddresses' returns ('', 'name email@domain.com) i.e. the
    whole input is considered as being the email.
    
    To improve the heuristic we can add a fallback by recalling 'getadresses'
    on the input with spaces replaced by commas when it found only an email and
    no name. The new email will be split into sub pairs allowing to find the real
    email and various name parts, allowing to make a new name / email pair.
    
    Emails should not contain spaces thus this is coherent with email formation.
    This fallback actually comes from a specific code done in '_parse_partner_name'
    of Partner model. Supporting it directly at tools level make the behavior
    coherent for all models.
    
    Task-2612945 (Mail: Defensive email formatting)
    
    X-original-commit: odoo/odoo@b2f3f0f3e13cef993541bcd093e61ffbb6a8c92e
    Part-of: odoo/odoo#134188

--- odoo/tools/mail.py
+++ odoo/tools/mail.py
@@ -527,14 +527,35 @@ def email_split_tuples(text):
     """ Return a list of (name, email) address tuples found in ``text`` . Note
     that text should be an email header or a stringified email list as it may
     give broader results than expected on actual text. """
+    def _parse_based_on_spaces(pair):
+        """ With input 'name email@domain.com' (missing quotes for a formatting)
+        getaddresses returns ('', 'name email@domain.com). This when having no
+        name and an email a fallback to enhance parsing is to redo a getaddresses
+        by replacing spaces by commas. The new email will be split into sub pairs
+        allowing to find the email and name parts, allowing to make a new name /
+        email pair. Emails should not contain spaces thus this is coherent with
+        email formation. """
+        name, email = pair
+        if not name and email and ' ' in email:
+            inside_pairs = getaddresses([email.replace(' ', ',')])
+            name_parts, found_email = [], False
+            for pair in inside_pairs:
+                if pair[1] and '@' not in pair[1]:
+                    name_parts.append(pair[1])
+                if pair[1] and '@' in pair[1]:
+                    found_email = pair[1]
+            name, email = (' '.join(name_parts), found_email) if found_email else (name, email)
+        return (name, email)
+
     if not text:
         return []
-    return [(addr[0], addr[1]) for addr in getaddresses([text])
-                # getaddresses() returns '' when email parsing fails, and
-                # sometimes returns emails without at least '@'. The '@'
-                # is strictly required in RFC2822's `addr-spec`.
-                if addr[1]
-                if '@' in addr[1]]
+    return list(map(_parse_based_on_spaces, [
+        (addr[0], addr[1]) for addr in getaddresses([text])
+        # getaddresses() returns '' when email parsing fails, and
+        # sometimes returns emails without at least '@'. The '@'
+        # is strictly required in RFC2822's `addr-spec`.
+        if addr[1] and '@' in addr[1]
+    ]))
 
 def email_split(text):
     """ Return a list of the email addresses found in ``text`` """
