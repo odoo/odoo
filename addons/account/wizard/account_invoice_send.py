d8e9e568cc6971b8ad1ee046799c90050ea9e777
Nasreddin Boulif (bon) <bon@odoo.com>
2023-08-31 13:02:31 +0200

    [FIX] account: send invoice mail in partner language
    Steps to reproduce:
    
      - Install `account` module
      - Activate another language (e.g. French)
      - Create a partner with the new activated language
      - Create an invoice for this partner and sent it by mail
    
    Issue:
    
      Some words in the external layout of the email are not translated;
      button and title
    
    Cause:
    
      The language of the partner is not taken into account (except for the
      model name in the button) when creating the external layout of the
      mail.
    
    Solution:
    
      Call send_mail with the partner language in the context.
    
    opw-3476315

--- addons/account/wizard/account_invoice_send.py
+++ addons/account/wizard/account_invoice_send.py
@@ -156,11 +156,13 @@ class AccountInvoiceSend(models.TransientModel):
             default_lang = get_lang(self.env)
             for lang in sorted((set(langs)) or [default_lang]):
                 active_ids_lang = active_records.filtered(lambda r: r.partner_id.lang == lang).ids
-                self_lang = self.with_context(active_ids=active_ids_lang, lang=lang)
+                self_lang = self.with_context(active_ids=active_ids_lang, lang=get_lang(self.env, lang).code)
                 self_lang.onchange_template_id()
                 self_lang._send_email()
         else:
-            self._send_email()
+            active_record = self.env[self.model].browse(self.res_id)
+            lang = get_lang(self.env, active_record.partner_id.lang).code
+            self.with_context(lang=lang)._send_email()
         if self.is_print:
             return self._print_document()
         return {'type': 'ir.actions.act_window_close'}
