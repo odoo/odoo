35f02ffc2594565239e3ccb6b62e177517c06f73
oco-odoo <oco@odoo.com>
2023-08-31 11:53:48 +0200

    [IMP] account: reports: introduce current_groupby field
    This field is needed by the enterprise counterpart of this commit. It is used to store custom-defined groupby on the report lines without overriding the standard value that's given in data files.
    
    Task 3413437

--- addons/account/models/account_report.py
+++ addons/account/models/account_report.py
@@ -291,6 +291,7 @@ class AccountReportLine(models.Model):
     parent_id = fields.Many2one(string="Parent Line", comodel_name='account.report.line', ondelete='set null')
     children_ids = fields.One2many(string="Child Lines", comodel_name='account.report.line', inverse_name='parent_id')
     groupby = fields.Char(string="Group By", help="Comma-separated list of fields from account.move.line (Journal Item). When set, this line will generate sublines grouped by those keys.")
+    user_groupby = fields.Char(string="User Group By", help="Comma-separated list of fields from account.move.line (Journal Item). When set, this line will generate sublines grouped by those keys.")
     sequence = fields.Integer(string="Sequence")
     code = fields.Char(string="Code", help="Unique identifier for this line.")
     foldable = fields.Boolean(string="Foldable", help="By default, we always unfold the lines that can be. If this is checked, the line won't be unfolded by default, and a folding button will be displayed.")
@@ -323,13 +324,13 @@ class AccountReportLine(models.Model):
     @api.constrains('parent_id')
     def _validate_groupby_no_child(self):
         for report_line in self:
-            if report_line.parent_id.groupby:
+            if report_line.parent_id.groupby or report_line.parent_id.user_groupby:
                 raise ValidationError(_("A line cannot have both children and a groupby value (line '%s').", report_line.parent_id.name))
 
     @api.constrains('expression_ids', 'groupby')
     def _validate_formula(self):
         for expression in self.expression_ids:
-            if expression.engine == 'aggregation' and expression.report_line_id.groupby:
+            if expression.engine == 'aggregation' and (expression.report_line_id.groupby or expression.report_line_id.user_groupby):
                 raise ValidationError(_(
                     "Groupby feature isn't supported by aggregation engine. Please remove the groupby value on '%s'",
                     expression.report_line_id.display_name,
@@ -339,6 +340,13 @@ class AccountReportLine(models.Model):
     def _check_parent_line(self):
         for line in self.filtered(lambda x: x.parent_id == x):
             raise ValidationError(_('Line "%s" defines itself as its parent.', line.name))
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'groupby' in vals:
+                vals['user_groupby'] = vals['groupby']
+        return super().create(vals_list)
 
     def _copy_hierarchy(self, copied_report, parent=None, code_mapping=None):
         ''' Copy the whole hierarchy from this line by copying each line children recursively and adapting the
