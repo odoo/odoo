2e93c1aa1986705dc46cd88bdd5ae5ca57a639e7
Thomas Lefebvre (thle) <thle@odoo.com>
2023-08-31 11:14:22 +0200

    [FIX] project: avoid modify `display_in_project` during duplication
    Version:
    --------
    - saas-16.4 only
    
    Issue:
    ------
    When a project is duplicated, all tasks and sub-tasks
    are visible in the new project's kanban view.
    
    Cause:
    ------
    When writing `project.write({'tasks': [Command.set(new_tasks.ids)]})`,
    we trigger an inverse which writes the `project_id` field to the tasks.
    This will set the value of `display_in_project` to `True`.
    
    Solution:
    ---------
    Rewrite the `display_in_project` field if necessary.
    
    opw-3479714

--- addons/project/models/project_project.py
+++ addons/project/models/project_project.py
@@ -408,11 +408,18 @@ class Project(models.Model):
             # preserve task name and stage, normally altered during copy
             defaults = self._map_tasks_default_valeus(task, project)
             new_tasks |= task.copy(defaults)
+        all_subtasks = new_tasks._get_all_subtasks()
+        subtasks_not_displayed = all_subtasks.filtered(
+            lambda task: not task.display_in_project
+        )
         project.write({'tasks': [Command.set(new_tasks.ids)]})
-        new_tasks._get_all_subtasks().filtered(
+        all_subtasks.filtered(
             lambda child: child.project_id == self
         ).write({
             'project_id': project.id
+        })
+        subtasks_not_displayed.write({
+            'display_in_project': False
         })
         return True
 
