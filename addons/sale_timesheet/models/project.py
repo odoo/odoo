b59aa4ef489e15c308ca01929ed0ce4a9ca5ae67
Yann Papouin <ypa@decgroupe.com>
2023-09-01 14:14:17 +0000

    [FIX] sale_timesheet: Invalid `read_group` usage
    `read_group` signature is not respected: the offset integer param is filled with a list
    
    X-original-commit: 2c11691545fa16b2c2f1ccc5b23cfe42a882fd24

--- addons/sale_timesheet/models/project.py
+++ addons/sale_timesheet/models/project.py
@@ -152,7 +152,7 @@ class Project(models.Model):
     @api.depends('pricing_type', 'allow_timesheets', 'allow_billable', 'sale_line_employee_ids', 'sale_line_employee_ids.employee_id')
     def _compute_warning_employee_rate(self):
         projects = self.filtered(lambda p: p.allow_billable and p.allow_timesheets and p.pricing_type == 'employee_rate')
-        employees = self.env['account.analytic.line']._read_group([('task_id', 'in', projects.task_ids.ids)], ['employee_id', 'project_id'], ['employee_id', 'project_id'], ['employee_id', 'project_id'], lazy=False)
+        employees = self.env['account.analytic.line']._read_group([('task_id', 'in', projects.task_ids.ids)], ['employee_id', 'project_id'], ['employee_id', 'project_id'], lazy=False)
         dict_project_employee = defaultdict(list)
         for line in employees:
             dict_project_employee[line['project_id'][0]] += [line['employee_id'][0]] if line['employee_id'] else []
