7143658bf25980837ffe88fed2faf24778ad052e
Antoine Guenet <age@odoo.com>
2023-09-01 16:38:16 +0200

    [FIX] web_editor: do not normalize on preserveCursor
    When preserving the selection, we typically want our selection to remain
    as close as possible to what it was before. But `preserveCursor` called
    `setSelection` with its default `normalize` argument, which would change
    the selection in sometimes unexpected ways.

--- addons/web_editor/static/lib/odoo-editor/test/spec/format.test.js
+++ addons/web_editor/static/lib/odoo-editor/test/spec/format.test.js
@@ -518,7 +518,7 @@ describe('Format', () => {
                 contentAfter: `<p style="text-decoration: line-through;">a[b]c</p>`,
             });
         });
-        it('should insert new character inside strikethrough at first position', async () => {
+        it('should insert before strikethrough', async () => {
             await testEditor(BasicEditor, {
                 contentBefore: `<p>d[a${s('bc]<br><br>')}</p>`,
                 stepFunction: async editor => {
@@ -531,7 +531,7 @@ describe('Format', () => {
                 stepFunction: async editor => {
                     insertText(editor, 'A');
                 },
-                contentAfter: `<p>${s(`A[]<br><br>`)}</p>`,
+                contentAfter: `<p>A[]${s(`<br><br>`)}</p>`,
             });
         });
     });
