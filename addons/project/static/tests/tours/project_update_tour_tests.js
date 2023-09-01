1e7948d09039d1437bfc6c7d2739bb0622a4af37
Antoine Guenet <age@odoo.com>
2023-09-01 16:46:29 +0200

    [IMP] web_editor, project: remove attributeless spans/fonts on sanitize
    `<span>` and `<font>` elements without attributes are useless and can be
    removed. Removing them will make the editor output more readable and
    will prevent endless nesting of attributeless `<span>` and `<font>`
    elements when successively styling, unstyling and deleting text.
    
    X-original-commit: 0f4914a1576108bb0843694a62d679bc78a8574a

--- addons/project/static/tests/tours/project_update_tour_tests.js
+++ addons/project/static/tests/tours/project_update_tour_tests.js
@@ -148,10 +148,10 @@ registry.category("web_tour.tours").add('project_update_tour', {
     trigger: ".o_field_widget[name='description'] div[name='milestone'] ul li:contains('(12/12/2099 => 12/12/2100)')",
     run: function () {},
 }, {
-    trigger: ".o_field_widget[name='description'] div[name='milestone'] ul li span:contains('(due 12/12/2022)')",
+    trigger: ".o_field_widget[name='description'] div[name='milestone'] ul li:contains('(due 12/12/2022)')",
     run: function () {},
 }, {
-    trigger: ".o_field_widget[name='description'] div[name='milestone'] ul li span:contains('(due 12/12/2100)')",
+    trigger: ".o_field_widget[name='description'] div[name='milestone'] ul li:contains('(due 12/12/2100)')",
     run: function () {},
 }, {
     trigger: '.o_back_button',
