a47054bab42b13236a02ec186f95405157e75da8
Mathieu Walravens <wama@odoo.com>
2023-09-01 18:01:28 +0200

    [FIX] stock: clean-up sequences at uninstall
    When stock is uninstalled, the sequences used in the warehouses for the
    picking types are kept. However, since [1], picking types cannot be
    duplicated anymore. When the module is re-installed, the picking types
    are created with the same name, which is not allowed by [1].
    
    [1]: https://github.com/odoo/odoo/commit/9034602a028fb22fbd139cf41a2f5c71d9d8d032
    
    opw-3484352
    
    X-original-commit: 7b0f38fea8bc9a541972d29f289e6175ee15919b

--- addons/stock/__manifest__.py
+++ addons/stock/__manifest__.py
@@ -95,6 +95,7 @@
     'application': True,
     'pre_init_hook': 'pre_init_hook',
     'post_init_hook': '_assign_default_mail_template_picking_id',
+    'uninstall_hook': 'uninstall_hook',
     'assets': {
         'web.report_assets_common': [
             'stock/static/src/scss/report_stock_reception.scss',
