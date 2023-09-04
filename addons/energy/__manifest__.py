0c7356dde175b1554918f3be731fc3d6bbc9e138
Luan Jubica <luanjubica@gmail.com>
2023-09-04 10:05:56 +0200

    init

---
+++ addons/energy/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Energy Trading",  # The name that will appear in the App list
+    "version": "16.0",  # Version
+    "application": True,  # This line says the module is an App, and not a module
+    "depends": ["base"],  # dependencies
+    "installable": True,
+    'license': 'LGPL-3',
+    'category': 'Productivity'
+}
---
+++ odoo/addons/energy/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Energy Trading",  # The name that will appear in the App list
+    "version": "16.0",  # Version
+    "application": True,  # This line says the module is an App, and not a module
+    "depends": ["base"],  # dependencies
+    "data": [
+
+    ],
+    "installable": True,
+    'license': 'LGPL-3',
+    'category': 'Productivity',
+
+}
