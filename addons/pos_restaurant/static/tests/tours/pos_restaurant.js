86d703361437ffd6ad02352ed38a9a364636bc95
Adrien Guilliams (adgu) <adgu@odoo.com>
2023-09-05 07:56:32 +0000

    [FIX] pos_restaurant: put disappeared lines back in tours
    A recent PR removed some lines in the pos_restaurant tour ( #124157 ).
    This commit put them back to avoid runbot error.
    This is a backport of this PR: #129155
    
    X-original-commit: e16eb15d548bf446f9e19232868106aa3926e72f

--- addons/pos_restaurant/static/tests/tours/pos_restaurant.js
+++ addons/pos_restaurant/static/tests/tours/pos_restaurant.js
@@ -84,6 +84,148 @@ registry.category("web_tour.tours").add("pos_restaurant_sync", {
     },
 });
 
+<<<<<<< HEAD
+||||||| parent of c37ce38a852 (temp)
+            startSteps();
+            
+            ProductScreen.do.confirmOpeningPopup();
+            
+            // Create first order
+            FloorScreen.do.clickTable("5");
+            ProductScreen.check.orderBtnIsPresent();
+            ProductScreen.do.clickDisplayedProduct("Coca-Cola");
+            ProductScreen.check.selectedOrderlineHas("Coca-Cola");
+            ProductScreen.do.doubleClickOrderline("Coca-Cola");
+            ProductScreen.do.clickDisplayedProduct("Water");
+            ProductScreen.check.selectedOrderlineHas("Water");
+            ProductScreen.check.orderlineIsToOrder("Water");
+            ProductScreen.check.orderlineIsToSkip("Coca-Cola");
+            ProductScreen.do.clickOrderButton();
+            ProductScreen.check.orderlinesHaveNoChange();
+            ProductScreen.check.isPrintingError();
+            ProductScreen.check.totalAmountIs("4.40");
+            
+            // Create 2nd order (paid)
+            Chrome.do.clickMenuButton();
+            Chrome.do.clickTicketButton();
+            TicketScreen.do.clickNewTicket();
+            ProductScreen.do.clickDisplayedProduct("Coca-Cola");
+            ProductScreen.check.selectedOrderlineHas("Coca-Cola");
+            ProductScreen.do.clickDisplayedProduct("Minute Maid");
+            ProductScreen.check.selectedOrderlineHas("Minute Maid");
+            ProductScreen.check.totalAmountIs("4.40");
+            ProductScreen.do.clickPayButton();
+            PaymentScreen.do.clickPaymentMethod("Cash");
+            PaymentScreen.do.clickValidate();
+            ReceiptScreen.do.clickNextOrder();
+            
+            // After clicking next order, floor screen is shown.
+            // It should have 1 as number of draft synced order.
+            FloorScreen.check.orderCountSyncedInTableIs("5", "1");
+            FloorScreen.do.clickTable("5");
+            ProductScreen.check.totalAmountIs("4.40");
+            
+            // Create another draft order and go back to floor
+            Chrome.do.clickMenuButton();
+            Chrome.do.clickTicketButton();
+            TicketScreen.do.clickNewTicket();
+            ProductScreen.do.clickDisplayedProduct("Coca-Cola");
+            ProductScreen.check.selectedOrderlineHas("Coca-Cola");
+            ProductScreen.do.clickDisplayedProduct("Minute Maid");
+            ProductScreen.check.selectedOrderlineHas("Minute Maid");
+            Chrome.do.backToFloor();
+            
+            // At floor screen, there should be 2 synced draft orders
+            FloorScreen.check.orderCountSyncedInTableIs("5", "2");
+            
+            // Delete the first order then go back to floor
+            FloorScreen.do.clickTable("5");
+            ProductScreen.check.isShown();
+            Chrome.do.clickMenuButton();
+            Chrome.do.clickTicketButton();
+            TicketScreen.do.deleteOrder("-0001");
+            Chrome.do.confirmPopup();
+            TicketScreen.do.selectOrder("-0003");
+            Chrome.do.backToFloor();
+            
+            // There should be 1 synced draft order.
+            FloorScreen.check.orderCountSyncedInTableIs("5", "2");
+            return getSteps(); 
+        } 
+    });
+    
+=======
+            startSteps();
+            
+            ProductScreen.do.confirmOpeningPopup();
+            
+            // Create first order
+            FloorScreen.do.clickTable("5");
+            ProductScreen.check.orderBtnIsPresent();
+            ProductScreen.do.clickDisplayedProduct("Coca-Cola");
+            ProductScreen.check.selectedOrderlineHas("Coca-Cola");
+            ProductScreen.do.doubleClickOrderline("Coca-Cola");
+            ProductScreen.do.clickDisplayedProduct("Water");
+            ProductScreen.check.selectedOrderlineHas("Water");
+            ProductScreen.check.orderlineIsToOrder("Water");
+            ProductScreen.check.orderlineIsToSkip("Coca-Cola");
+            ProductScreen.do.clickOrderButton();
+            ProductScreen.check.orderlinesHaveNoChange();
+            ProductScreen.check.isPrintingError();
+            ProductScreen.check.totalAmountIs("4.40");
+            
+            // Create 2nd order (paid)
+            Chrome.do.clickMenuButton();
+            Chrome.do.clickTicketButton();
+            TicketScreen.do.clickNewTicket();
+            ProductScreen.do.clickDisplayedProduct("Coca-Cola");
+            ProductScreen.check.selectedOrderlineHas("Coca-Cola");
+            ProductScreen.do.clickDisplayedProduct("Minute Maid");
+            ProductScreen.check.selectedOrderlineHas("Minute Maid");
+            ProductScreen.check.totalAmountIs("4.40");
+            ProductScreen.do.clickPayButton();
+            PaymentScreen.do.clickPaymentMethod("Cash");
+            PaymentScreen.do.clickValidate();
+            ReceiptScreen.do.clickNextOrder();
+            
+            // After clicking next order, floor screen is shown.
+            // It should have 1 as number of draft synced order.
+            FloorScreen.check.orderCountSyncedInTableIs("5", "1");
+            FloorScreen.do.clickTable("5");
+            ProductScreen.check.totalAmountIs("4.40");
+            
+            // Create another draft order and go back to floor
+            Chrome.do.clickMenuButton();
+            Chrome.do.clickTicketButton();
+            TicketScreen.do.clickNewTicket();
+            ProductScreen.do.clickDisplayedProduct("Coca-Cola");
+            ProductScreen.check.selectedOrderlineHas("Coca-Cola");
+            ProductScreen.do.clickDisplayedProduct("Minute Maid");
+            ProductScreen.check.selectedOrderlineHas("Minute Maid");
+            Chrome.do.backToFloor();
+            
+            // At floor screen, there should be 2 synced draft orders
+            FloorScreen.check.orderCountSyncedInTableIs("5", "2");
+            
+            // Delete the first order then go back to floor
+            FloorScreen.do.clickTable("5");
+            ProductScreen.check.isShown();
+            Chrome.do.clickMenuButton();
+            Chrome.do.clickTicketButton();
+            TicketScreen.do.deleteOrder("-0001");
+            Chrome.do.confirmPopup();
+            Chrome.check.isSyncStatusPending();
+            Chrome.check.isSyncStatusConnected();
+            TicketScreen.do.selectOrder("-0003");
+            Chrome.do.backToFloor();
+            
+            // There should be 1 synced draft order.
+            FloorScreen.check.orderCountSyncedInTableIs("5", "2");
+            return getSteps(); 
+        } 
+    });
+    
+>>>>>>> c37ce38a852 (temp)
 /* pos_restaurant_sync_second_login
  *
  * This tour should be run after the first tour is done.
