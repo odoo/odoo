f8507f8d2d5eb66a547d0cf03687e36ba734a7b5
prye-odoo <prye@odoo.com>
2023-09-05 18:43:52 +0530

    [FIX] point_of_sale: handle parse-error if reinstall point_of_sale module
    When users try to reinstall the "point_of_sale", a traceback will
    be generated.
    
    Error:  while parsing /home/odoo/src/odoo/saas-16.4/addons/point_of_sale/
    data/point_of_sale_data.xml:4, somewhere inside
    <function model="stock.warehouse" name="_create_missing_pos_picking_types"/>
    
    This is because duplicate sequence records exist and raise the ParseError.
    
    code reference:
    https://github.com/odoo/odoo/blob/saas-16.3/addons/stock/models/stock_picking.py#L284
    https://github.com/odoo/odoo/blob/saas-16.3/addons/point_of_sale/data/point_of_sale_data.xml#L4C49-L4C82
    
    sentry-4452246611

--- addons/point_of_sale/__init__.py
+++ addons/point_of_sale/__init__.py
@@ -5,3 +5,11 @@ from . import models
 from . import controllers
 from . import report
 from . import wizard
+
+
+def uninstall_hook(env):
+    warehouses = env['stock.warehouse'].sudo().search([('pos_type_id', '!=', False)])
+    for warehouse in warehouses:
+        sequence_data = warehouse._get_sequence_values()
+        if sequence_data and sequence_data.get('pos_type_id'):
+            env['ir.sequence'].sudo().search([('prefix', '=', sequence_data['pos_type_id']['prefix'])]).unlink()
