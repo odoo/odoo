b78193e14d3ecb2d27b16c049ca9098d8c308e23
Romeo Fragomeli <rfr@odoo.com>
2023-09-05 09:25:13 +0000

    [REV] web: blockui when executing a target=self act_url action
    This reverts commit odoo/odoo@91b80fbb, as it adds some unwanted visual
    effect. In other words, the "block UI" is never unblocked when some
    actions are triggered. This is specific for "Download" actions as the
    download is correctly executed, but the page is never `unload`.
    e.g.:
    ```python
    action = {
        'type': 'ir.actions.act_url',
        'url': '/web_enterprise/partner/%d/vcard' % record.id,
        'target': 'self',
    }
    ```
    
    Note:
    We can't use "'target': 'new'" as it creates a bug in Mobile Apps.
    When The Mobile Apps create a new "Tab/Page", they do it in a new
    sandboxed browsing environment, so the user isn't logged in and the
    resource isn't accessible anymore.
    
    Task ID: 3435131
    
    closes odoo/odoo#134430
    
    X-original-commit: 08db3deb3336f0c1ff9fe2703ebeb8ea5b5aeb3d
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/webclient/actions/url_action_tests.js
+++ addons/web/static/tests/webclient/actions/url_action_tests.js
@@ -1,13 +1,11 @@
 /** @odoo-module **/
 
-import { registry } from "@web/core/registry";
 import { makeTestEnv } from "../../helpers/mock_env";
 import { setupWebClientRegistries, doAction, getActionManagerServerData } from "./../helpers";
 import { patchWithCleanup } from "@web/../tests/helpers/utils";
 import { browser } from "@web/core/browser/browser";
 
 let serverData;
-const serviceRegistry = registry.category("services");
 
 QUnit.module("ActionManager", (hooks) => {
     hooks.beforeEach(() => {
@@ -32,28 +30,6 @@ QUnit.module("ActionManager", (hooks) => {
         assert.verifySteps(["/my/test/url"]);
     });
 
-    QUnit.test("an 'ir.actions.act_url' with target 'self' blocks the ui", async (assert) => {
-        serviceRegistry.add("ui", {
-            start() {
-                return {
-                    block: () => assert.step("block"),
-                    // we can't simulate a page unload in the tests, so in this scenario the
-                    // ui will be unblocked directly (and we thus need to define the unblock
-                    // function)
-                    unblock: () => {},
-                };
-            },
-        });
-        setupWebClientRegistries();
-        const env = await makeTestEnv({ serverData });
-        await doAction(env, {
-            type: "ir.actions.act_url",
-            target: "self",
-            url: "/my/test/url",
-        });
-        assert.verifySteps(["block"]);
-    });
-
     QUnit.test("execute an 'ir.actions.act_url' action with onClose option", async (assert) => {
         setupWebClientRegistries();
         patchWithCleanup(browser, {
@@ -66,7 +42,7 @@ QUnit.module("ActionManager", (hooks) => {
         await doAction(env, { type: "ir.actions.act_url" }, options);
         assert.verifySteps(["browser open", "onClose"]);
     });
-    
+
     QUnit.test("execute an 'ir.actions.act_url' action with url javascript:", async (assert) => {
         assert.expect(2);
         patchWithCleanup(browser.location, {
