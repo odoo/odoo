6db8079d7bfc34d3e6ef048f172897065b493221
Yosua Nicolaus <yoni@odoo.com>
2023-09-05 11:58:10 +0200

    [REM] account: remove useless fixme & extra_trigger
    remove unrelevant FIXME from 1 year ago as it was not relevant anymore
    
    Task-id: 2632645
    Part-of: odoo/odoo#132382

--- addons/l10n_ar/static/src/js/tours/account.js
+++ addons/l10n_ar/static/src/js/tours/account.js
@@ -10,9 +10,8 @@ patch(registry.category("web_tour.tours").get("account_tour"), {
         const filteredSteps = originalSteps.filter((step) => step.trigger != "input[name=name]");
         const stepIndex = filteredSteps.findIndex((step) => step.trigger == 'div[name=partner_id] input');
         filteredSteps.splice(stepIndex + 2, 0, {
-        // FIXME WOWL: this selector needs to work in both legacy and non-legacy views
             trigger: "div[name=l10n_ar_afip_responsibility_type_id] input",
-            extra_trigger: "[name=move_type] [raw-value=out_invoice], [name=move_type][raw-value=out_invoice]",
+            extra_trigger: "[name=move_type] [raw-value=out_invoice]",
             position: "bottom",
             content: "Set the AFIP Responsability",
             run: "text IVA",
