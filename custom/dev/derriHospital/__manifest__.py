cc55ce7c7027f777dd1aede73944cde948da645c
derrimugisha <derrimugisha@gmail.com>
2023-09-06 11:10:42 +0300

    worked on tracking fields,added a chatter section

--- custom/dev/derriHospital/__manifest__.py
+++ custom/dev/derriHospital/__manifest__.py
@@ -1,5 +1,6 @@
 {
     "name": "Derick Hospital Management System",
     "author": "Derick Mugisha",
+    "depends": ["mail"],
     "data": ["security/ir.model.access.csv", "views/menu.xml", "views/patient.xml",]
 }
--- custom/dev/derriHospital/models/patient.py
+++ custom/dev/derriHospital/models/patient.py
@@ -4,9 +4,10 @@ from odoo import api, fields, models
 class DerriHospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "patient recors"
-    name = fields.Char(string="Name", required=True)
-    age = fields.Integer(string="Age")
-    is_child = fields.Boolean(string="is child ?")
-    notes = fields.Text(string="Notes")
+    _inherit = "mail.thread"
+    name = fields.Char(string="Name", required=True, tracking=True)
+    age = fields.Integer(string="Age", tracking=True)
+    is_child = fields.Boolean(string="is child ?", tracking=True)
+    notes = fields.Text(string="Notes", tracking=True)
     gender = fields.Selection(
-        [("male", "Male"), ("female", "Female"), ("others", "Others")], string="Gender")
+        [("male", "Male"), ("female", "Female"), ("others", "Others")], string="Gender", tracking=True)
