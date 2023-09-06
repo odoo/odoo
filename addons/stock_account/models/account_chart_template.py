84606394a462f27bf5e8795fa206006834dc4d2d
Roy Le <roy.le@viindoo.com>
2023-09-06 01:58:45 +0000

    [FIX] stock_account: don't skip journal_dict arg when extending
    Journals passed in journal_dict weren't actually created.
    Credits to Roy Le @ Viindoo, original PR: odoo/odoo#134446
    
    X-original-commit: 7e536998d84744a464e91fa5e056fc06f7796d6f
    Co-authored-by: Roy Le <roy.le@viindoo.com>

--- addons/stock_account/models/account_chart_template.py
+++ addons/stock_account/models/account_chart_template.py
@@ -8,6 +8,7 @@ from odoo.addons.account.models.chart_template import template
 class AccountChartTemplate(models.AbstractModel):
     _inherit = "account.chart.template"
 
+<<<<<<< 17.0
     def _post_load_data(self, template_code, company, template_data):
         super()._post_load_data(template_code, company, template_data)
         company = company or self.env.company
@@ -24,6 +25,17 @@ class AccountChartTemplate(models.AbstractModel):
             value = template_data.get(fname)
             if value:
                 self.env['ir.property']._set_default(fname, 'product.category', self.ref(value).id, company=company)
+||||||| 315279c7aaefb6e0194fdd15e39ef3f19adf451c
+    @api.model
+    def generate_journals(self, acc_template_ref, company, journals_dict=None):
+        journal_to_add = [{'name': _('Inventory Valuation'), 'type': 'general', 'code': 'STJ', 'favorite': False, 'sequence': 8}]
+        return super(AccountChartTemplate, self).generate_journals(acc_template_ref=acc_template_ref, company=company, journals_dict=journal_to_add)
+=======
+    @api.model
+    def generate_journals(self, acc_template_ref, company, journals_dict=None):
+        journal_to_add = (journals_dict or []) + [{'name': _('Inventory Valuation'), 'type': 'general', 'code': 'STJ', 'favorite': False, 'sequence': 8}]
+        return super(AccountChartTemplate, self).generate_journals(acc_template_ref=acc_template_ref, company=company, journals_dict=journal_to_add)
+>>>>>>> 74b590d86fc051ed697cdb312a7c19aaca72183a
 
     @template(model='account.journal')
     def _get_stock_account_journal(self, template_code):
