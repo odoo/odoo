e6dcc84b5683cf38d36c3d6c60fdb8d6d49c81be
Martin Quinteros <maq@adhoc.com.ar>
2023-09-07 10:59:33 -0300

    [FIX] As long as the WTH module is an AR-specific module (l10n_ar_withholding) we need to move the "Base Tax Account" from the "Taxes" section to the "Argentinean Localization" section in the res.config. -> So let's move it.       Let's rename the field "Base Tax Account" >> to >> "Tax Base Account"       There is one tax name still in ESP and needs to be in ENG. "Ret IVA A" needs to be in ENG "VAT WTH A".

--- addons/l10n_ar_withholding/models/res_company.py
+++ addons/l10n_ar_withholding/models/res_company.py
@@ -9,5 +9,5 @@ class ResCompany(models.Model):
     l10n_ar_tax_base_account_id = fields.Many2one(
         comodel_name='account.account',
         domain=[('deprecated', '=', False)],
-        string="Base Tax Account",
+        string="Tax Base Account",
         help="Account that will be set on lines created to represent the tax base amounts.")
