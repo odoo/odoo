67d3ac8c478bd0787d6651423b79c640c1bf2fbc
Yolann Sabaux <yosa@odoo.com>
2023-09-07 09:14:11 +0000

    [FIX] account, base: prevent merging account.account
    Steps to reproduce:
    
    - Enable the merge on account.account in data cleaning APP (in debug)
    - Create an invoice with a receivable account = A
    - Define a lock  date after the invoice date
    - Go to chart of accounts
    - Select your receivable = A et receivable = B
    - Action = Merge accounts where B is the MASTER
    
    Issue:
    Upon revisiting the customer invoice: Notice that the journal items have been updated.
    
    Solution:
    We simply prevent ~~the use of a nuclear weapon~~ the merge of `account.account` as there other possibilities less dangerous such as multi-edit + archiving
    
    We also prevent the merge of `res.partner` if this one is used in hashed entries
    
    oe:https://github.com/odoo/enterprise/pull/47053
    
    opw-3389157
    
    closes odoo/odoo#136344
    
    X-original-commit: a40659bcda1a4d635f2caacffb740b001bed9e30
    Related: odoo/enterprise#47795
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Yolann Sabaux (yosa) <yosa@odoo.com>

--- addons/account/models/partner.py
+++ addons/account/models/partner.py
@@ -667,3 +667,11 @@ class ResPartner(models.Model):
         :return: an array of ir.model.fields for which the user should provide values.
         """
         return []
+
+    def _merge_method(self, destination, source):
+        """
+        Prevent merging partners that are linked to already hashed journal items.
+        """
+        if self.env['account.move.line'].search([('move_id.inalterable_hash', '!=', False), ('partner_id', 'in', source.ids)], limit=1):
+            raise UserError(_('Partners that are used in hashed entries cannot be merged.'))
+        return super()._merge_method(destination, source)
