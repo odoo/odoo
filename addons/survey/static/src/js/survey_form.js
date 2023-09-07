odoo/odoo

commit 910897fc97d87b08f01627094ec8c159f5267628

[REF] web, *: remove tempusdominus date(time) picker lib
As tempusdominus relies on jQuery, we have written our own date(time)
picker in owl. This commit converts the remaining uses of the
tempusdominus date(time) picker and replaces them by our own.

closes odoo/odoo#133349

Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

addons/survey/static/src/js/survey_form.js
@@ -1,9 +1,6 @@
 /** @odoo-module **/
 
-import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import publicWidget from "@web/legacy/js/public/public_widget";
-import { formatDateTime, parseDate, parseDateTime} from "@web/legacy/js/core/dates";
-import time from "@web/legacy/js/core/time";
 import { _t } from "@web/core/l10n/translation";
 import dom from "@web/legacy/js/core/dom";
 import {getCookie, setCookie, deleteCookie} from "@web/legacy/js/core/cookie_utils";
@@ -11,9 +8,17 @@ import { utils as uiUtils } from "@web/core/ui/ui_service";
 
 import SurveyPreloadImageMixin from "@survey/js/survey_preload_image_mixin";
 import { SurveyImageZoomer } from "@survey/js/survey_image_zoomer";
+import {
+    deserializeDate,
+    deserializeDateTime,
+    parseDateTime,
+    parseDate,
+    serializeDateTime,
+    serializeDate,
+} from "@web/core/l10n/dates";
+const { DateTime } = luxon;
 
 var isMac = navigator.platform.toUpperCase().includes('MAC');
-const { DateTime, Settings } = luxon;
 
 publicWidget.registry.SurveyFormWidget = publicWidget.Widget.extend(SurveyPreloadImageMixin, {
     selector: '.o_survey_form',
@@ -56,9 +61,6 @@ publicWidget.registry.SurveyFormWidget = publicWidget.Widget.extend(SurveyPreloa
                 self._initTimer();
                 self._initBreadcrumb();
             }
-            self.$('div.o_survey_form_date').each(function () {
-                self._initDateTimePicker($(this));
-            });
             self._initChoiceItems();
             self._initTextArea();
             self._focusOnFirstInput();
@@ -472,9 +474,8 @@ publicWidget.registry.SurveyFormWidget = publicWidget.Widget.extend(SurveyPreloa
                 options.isFinish = true;
             }
 
-            this.$('div.o_survey_form_date').each(function () {
-                self._initDateTimePicker($(this));
-            });
+            // Start datetime pickers
+            self.trigger_up("widgets_start_request");
             if (this.options.isStartScreen || (options && options.initTimer)) {
                 this._initTimer();
                 this.options.isStartScreen = false;
@@ -602,16 +603,20 @@ publicWidget.registry.SurveyFormWidget = publicWidget.Widget.extend(SurveyPreloa
                     if (questionRequired && !data[questionId]) {
                         errors[questionId] = constrErrorMsg;
                     } else if (data[questionId]) {
-                        var datetimepickerFormat = $input.data('questionType') === 'datetime' ? time.getLangDatetimeFormat() : time.getLangDateFormat();
-                        var momentDate = moment($input.val(), datetimepickerFormat);
-                        if (!momentDate.isValid()) {
+                        const [parse, deserialize] =
+                            $input.data("questionType") === "date"
+                                ? [parseDate, deserializeDate]
+                                : [parseDateTime, deserializeDateTime];
+                        const date = parse($input.val());
+                        if (!date || !date.isValid) {
                             errors[questionId] = validationDateMsg;
                         } else {
-                            var $dateDiv = $questionWrapper.find('.o_survey_form_date');
-                            var maxDate = $dateDiv.data('maxdate');
-                            var minDate = $dateDiv.data('mindate');
-                            if ((maxDate && momentDate.isAfter(moment(maxDate)))
-                                    || (minDate && momentDate.isBefore(moment(minDate)))) {
+                            const maxDate = deserialize($input.data('max-date'));
+                            const minDate = deserialize($input.data('min-date'));
+                            if (
+                                (maxDate.isValid && date > maxDate) ||
+                                (minDate.isValid && date < minDate)
+                            ) {
                                 errors[questionId] = validationErrorMsg;
                             }
                         }
@@ -691,11 +696,15 @@ publicWidget.registry.SurveyFormWidget = publicWidget.Widget.extend(SurveyPreloa
                     params[this.name] = this.value;
                     break;
                 case 'date':
-                    params = self._prepareSubmitDates(params, this.name, this.value, false);
+                case 'datetime':{
+                    const [parse, serialize] =
+                        $(this).data("questionType") === "date"
+                            ? [parseDate, serializeDate]
+                            : [parseDateTime, serializeDateTime];
+                    const date = parse(this.value);
+                    params[this.name] = date ? serialize(date) : "";
                     break;
-                case 'datetime':
-                    params = self._prepareSubmitDates(params, this.name, this.value, true);
-                    break;
+                }
                 case 'simple_choice_radio':
                 case 'multiple_choice':
                     params = self._prepareSubmitChoices(params, $(this), $(this).data('name'));
@@ -706,19 +715,6 @@ publicWidget.registry.SurveyFormWidget = publicWidget.Widget.extend(SurveyPreloa
             }
         });
     },
-
-    /**
-    *   Prepare date answer before submitting form.
-    *   Convert date value from client current timezone to UTC Date to correspond to the server format.
-    *   return params = { 'dateQuestionId' : '2019-05-23', 'datetimeQuestionId' : '2019-05-23 14:05:12' }
-    */
-    _prepareSubmitDates: function (params, questionId, value, isDateTime) {
-        var momentDate = isDateTime ? parseDateTime(value, null, {timezone: true}) : parseDate(value);
-        var formattedDate = momentDate ? momentDate.toJSON() : '';
-        params[questionId] = formattedDate;
-        return params;
-    },
-
     /**
     *   Prepare choice answer before submitting form.
     *   If the answer is not the 'comment selection' (=Other), calls the _prepareSubmitAnswer method to add the answer to the params
@@ -917,77 +913,6 @@ publicWidget.registry.SurveyFormWidget = publicWidget.Widget.extend(SurveyPreloa
                 });
             });
         }
-    },
-
-    /**
-    * Initialize datetimepicker in correct format and with constraints
-    */
-    _initDateTimePicker: function ($dateGroup) {
-        var disabledDates = [];
-        var questionType = $dateGroup.find('input').data('questionType');
-        var minDateData = $dateGroup.data('mindate');
-        var maxDateData = $dateGroup.data('maxdate');
-
-        var datetimepickerFormat = questionType === 'datetime' ? time.getLangDatetimeFormat() : time.getLangDateFormat();
-
-        var minDate = minDateData
-            ? this._formatDateTime(minDateData, datetimepickerFormat)
-            : moment({ y: 1000 });
-
-        var maxDate = maxDateData
-            ? this._formatDateTime(maxDateData, datetimepickerFormat)
-            : moment().add(200, "y");
-
-        if (questionType === 'date') {
-            // Include min and max date in selectable values
-            maxDate = moment(maxDate).add(1, "d");
-            minDate = moment(minDate).subtract(1, "d");
-            disabledDates = [minDate, maxDate];
-        }
-
-        $dateGroup.datetimepicker({
-            format: datetimepickerFormat,
-            minDate: minDate,
-            maxDate: maxDate,
-            disabledDates: disabledDates,
-            useCurrent: false,
-            viewDate: moment(new Date()).hours(minDate.hours()).minutes(minDate.minutes()).seconds(minDate.seconds()).milliseconds(minDate.milliseconds()),
-            calendarWeeks: true,
-            icons: {
-                time: 'fa fa-clock-o',
-                date: 'fa fa-calendar',
-                next: 'oi oi-chevron-right',
-                previous: 'oi oi-chevron-left',
-                up: 'oi oi-chevron-up',
-                down: 'oi oi-chevron-down',
-            },
-            locale: Settings.defaultLocale,
-            allowInputToggle: true,
-        });
-        $dateGroup.on('error.datetimepicker', (err) => {
-            if (err.date) {
-                if (err.date < minDate) {
-                    this.call("dialog", "add", AlertDialog, {
-                        body:
-                            _t("The date you selected is lower than the minimum date: ") +
-                            minDate.format(datetimepickerFormat),
-                    });
-                }
-
-                if (err.date > maxDate) {
-                    this.call("dialog", "add", AlertDialog, {
-                        body:
-                            _t("The date you selected is greater than the maximum date: ") +
-                            maxDate.format(datetimepickerFormat),
-                    });
-                }
-            }
-            return false;
-        });
-    },
-
-    _formatDateTime: function (datetimeValue, format) {
-        return moment(formatDateTime(moment(datetimeValue), null, {timezone: true}), format);
     },
 
     _initResultWidget: function () {

addons/purchase/controllers/portal.py
@@ -167,7 +167,7 @@ class CustomerPortal(portal.CustomerPortal):
             return request.render("purchase.portal_my_purchase_order_update_date", values)
         return request.render("purchase.portal_my_purchase_order", values)
 
-    @http.route(['/my/purchase/<int:order_id>/update'], type='http', methods=['POST'], auth="public", website=True)
+    @http.route(['/my/purchase/<int:order_id>/update'], type='json', auth="public", website=True)
     def portal_my_purchase_order_update_dates(self, order_id=None, access_token=None, **kw):
         """User update scheduled date on purchase order line.
         """

addons/purchase/static/src/js/purchase_datetimepicker.js
@@ -1,6 +1,26 @@
-$(function () {
-    $('input.o-purchase-datetimepicker').datetimepicker();
-    $('input.o-purchase-datetimepicker').on("hide.datetimepicker", function () {
-        $(this).parents('form').submit();
-    });
-})
+/** @odoo-module */
+import PublicWidget from "@web/legacy/js/public/public_widget";
+
+export const PurchaseDatePicker = PublicWidget.Widget.extend({
+    selector: ".o-purchase-datetimepicker",
+    start() {
+        this.call("datetime_picker", "create", {
+            target: this.el,
+            onChange: (newDate) => {
+                const { accessToken, orderId, lineId } = this.el.dataset;
+                this._rpc({
+                    route: `/my/purchase/${orderId}/update?access_token=${accessToken}`,
+                    params: {
+                        [lineId]: newDate.toISODate(),
+                    },
+                });
+            },
+            pickerProps: {
+                type: "date",
+                value: luxon.DateTime.fromISO(this.el.dataset.value),
+            },
+        }).enable();
+    },
+});
+
+PublicWidget.registry.PurchaseDatePicker = PurchaseDatePicker;

addons/purchase/views/portal_templates.xml
@@ -262,7 +262,6 @@
                             <th class="text-start">Products</th>
                             <th class="text-end">Quantity</th>
                             <th t-if="update_dates" class="text-end">Scheduled Date</th>
-                            <th t-if="update_dates" class="text-end"><strong>Update Here</strong></th>
                             <th t-if="not update_dates and order.state in ['purchase', 'done']" t-attf-class="text-end {{ 'd-none d-sm-table-cell' if report_type == 'html' else '' }}">Unit Price</th>
                             <th t-if="not update_dates and order.state in ['purchase', 'done']" t-attf-class="text-end {{ 'd-none d-md-table-cell' if report_type == 'html' else '' }}">
                                 <span>Taxes</span>
@@ -296,19 +295,20 @@
                                             <span t-field="line.product_uom" groups="uom.group_uom"/>
                                         </div>
                                     </td>
-                                    <td t-if="update_dates" class="text-end"><span t-esc="line.date_planned.date()"/></td>
                                     <td t-if="update_dates" class="text-end">
-                                      <form t-attf-action="/my/purchase/#{order.id}/update?access_token=#{order.access_token}" method="post">
-                                        <input type="hidden" name="csrf_token" t-att-value="request.csrf_token()"/>
                                         <div class="container">
                                           <div class="mb-3">
                                             <div class="input-group date">
-                                                <input type="text" class="form-control datetimepicker-input o-purchase-datetimepicker" t-attf-id="datetimepicker_#{line.id}" t-att-name="line.id"
-                                                  data-toggle="datetimepicker" data-date-format="YYYY-MM-DD" t-attf-data-target="#datetimepicker_#{line.id}"/>
+                                                <input type="text"
+                                                    class="form-control datetimepicker-input o-purchase-datetimepicker text-end"
+                                                    t-att-data-access-token="order.access_token"
+                                                    t-att-data-order-id="order.id"
+                                                    t-att-data-line-id="line.id"
+                                                    t-att-data-value="line.date_planned.isoformat()"
+                                                />
                                             </div>
                                           </div>
                                         </div>
-                                      </form>
                                     </td>
                                     <td t-if="not update_dates and order.state in ['purchase', 'done']" t-attf-class="text-end {{ 'd-none d-sm-table-cell' if report_type == 'html' else '' }}">
                                         <div

addons/survey/static/src/scss/survey_templates_form.scss
@@ -114,12 +114,6 @@ _::-webkit-full-page-media, _:future, :root .o_survey_wrap {
         }
     }
 
-    .o_survey_form_date [data-toggle="datetimepicker"]  {
-        right: 0;
-        bottom: 5px;
-        top: auto;
-    }
-
     .o_survey_choice_btn {
         transition: background-color 0.3s ease;
         flex: 1 0 300px;

addons/survey/views/survey_templates.xml
@@ -371,28 +371,28 @@
     </template>
 
     <template id="question_date" name="Question: date box">
-        <div class="input-group o_survey_form_date" t-attf-id="datetimepicker_#{question.id}" data-target-input="nearest"
-                t-att-data-mindate="question.validation_min_date"
-                t-att-data-maxdate="question.validation_max_date">
+        <div class="input-group o_survey_form_date">
             <input type="text" class="form-control datetimepicker-input o_survey_question_date bg-transparent text-dark rounded-0 p-0"
-                   t-attf-data-target="#datetimepicker_#{question.id}"
                    t-att-name="question.id" t-att-placeholder="question.question_placeholder"
                    t-att-value="format_date(answer_lines[0].value_date) if answer_lines else None"
-                   t-att-data-question-type="question.question_type"/>
-            <div t-if="not survey_form_readonly" class="position-absolute input-group-text text-primary border-0 bg-transparent p-0" t-attf-data-target="#datetimepicker_#{question.id}" data-toggle="datetimepicker"><i class="fa fa-calendar"></i></div>
+                   t-att-data-question-type="question.question_type"
+                   data-widget="datetime-picker" data-widget-type="date"
+                   t-att-data-min-date="question.validation_min_date"
+                   t-att-data-max-date="question.validation_max_date"/>
+            <div t-if="not survey_form_readonly" class="position-absolute input-group-text text-primary border-0 bg-transparent p-0 end-0 pe-none"><i class="fa fa-calendar"></i></div>
         </div>
     </template>
 
     <template id="question_datetime" name="Question: datetime box">
-        <div class="input-group o_survey_form_date" t-attf-id="datetimepicker_#{question.id}" data-target-input="nearest"
-                t-att-data-mindate="question.validation_min_datetime"
-                t-att-data-maxdate="question.validation_max_datetime">
+        <div class="input-group o_survey_form_date">
             <input type="text" class="form-control datetimepicker-input o_survey_question_datetime bg-transparent text-dark rounded-0 p-0"
-                   t-attf-data-target="#datetimepicker_#{question.id}"
                    t-att-name="question.id" t-att-placeholder="question.question_placeholder"
                    t-att-value="format_datetime(answer_lines[0].value_datetime) if answer_lines else None"
-                   t-att-data-question-type="question.question_type"/>
-            <div t-if="not survey_form_readonly" class="position-absolute input-group-text text-primary border-0 bg-transparent p-0" t-attf-data-target="#datetimepicker_#{question.id}" data-toggle="datetimepicker"><i class="fa fa-calendar"></i></div>
+                   t-att-data-question-type="question.question_type"
+                   data-widget="datetime-picker" data-widget-type="datetime"
+                   t-att-data-min-date="question.validation_min_datetime"
+                   t-att-data-max-date="question.validation_max_datetime"/>
+            <div t-if="not survey_form_readonly" class="position-absolute input-group-text text-primary border-0 bg-transparent p-0 end-0 pe-none"><i class="fa fa-calendar"></i></div>
         </div>
     </template>
 

addons/web/__manifest__.py
@@ -182,8 +182,6 @@ This module provides the core of the Odoo Web Client.
 
             ('include', 'web._assets_bootstrap_frontend'),
 
-            'web/static/src/legacy/scss/tempusdominus_overridden.scss',
-            'web/static/lib/tempusdominus/tempusdominus.scss',
             'web/static/lib/jquery.ui/jquery-ui.css',
             'web/static/src/libs/fontawesome/css/font-awesome.css',
             'web/static/lib/odoo_ui_icons/*',
@@ -234,7 +232,6 @@ This module provides the core of the Odoo Web Client.
             'web/static/lib/bootstrap/js/dist/scrollspy.js',
             'web/static/lib/bootstrap/js/dist/tab.js',
             'web/static/lib/bootstrap/js/dist/toast.js',
-            'web/static/lib/tempusdominus/tempusdominus.js',
             'web/static/lib/select2/select2.js',
             'web/static/lib/clipboard/clipboard.js',
             'web/static/lib/jSignature/jSignatureCustom.js',
@@ -268,6 +265,7 @@ This module provides the core of the Odoo Web Client.
             ('remove', 'web/static/src/core/file_viewer/file_viewer.dark.scss'),
             ('remove', 'web/static/src/core/emoji_picker/emoji_data.js'),
             'web/static/src/public/error_notifications.js',
+            'web/static/src/public/datetime_picker_widget.js',
             'web/static/src/libs/pdfjs.js',
 
             'web/static/src/legacy/utils.js',
@@ -512,7 +510,6 @@ This module provides the core of the Odoo Web Client.
             ('remove', 'web/static/tests/webclient/**/helpers.js'),
 
             # Legacy
-            'web/static/lib/tempusdominus/tempusdominus.js',
             'web/static/tests/legacy/**/*.js',
             ('remove', 'web/static/tests/legacy/helpers/**/*.js'),
             ('remove', 'web/static/tests/legacy/legacy_setup.js'),

addons/web/static/lib/tempusdominus/LICENSE
@@ -1,21 +1,0 @@
-MIT License
-
-Copyright (c) 2016 Tempus Dominus
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.

commit f24b512077c45d01d7875515945f5c954a059824

[FIX] survey: fix duplicate timer
This commit fix a bug introduced by 910897fc97d87b08f01627094ec8c159f5267628
which displayed two timers when starting a survey.

Task-3497526

addons/survey/static/src/js/survey_form.js
@@ -475,7 +475,7 @@ publicWidget.registry.SurveyFormWidget = publicWidget.Widget.extend(SurveyPreloa
             }
 
             // Start datetime pickers
-            self.trigger_up("widgets_start_request");
+            self.trigger_up("widgets_start_request", { $target: this.$el.find('.o_survey_form_date') });
             if (this.options.isStartScreen || (options && options.initTimer)) {
                 this._initTimer();
                 this.options.isStartScreen = false;