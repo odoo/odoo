fb6401beeb320d2258f81cb861a5194730c8b5f2
Antoine Dupuis (andu) <andu@odoo.com>
2023-09-08 11:11:03 +0200

    [FIX] l10n_mn: 2 bugfixes:
    1. Fix in account tags migration
    
    Although the account templates defined in account.account.template.csv
    each have just one account tag, which is the cashflow tag for the new
    cash flow report, the same might not be the case for extra account
    templates defined by a user's custom modules.
    
    When upgrading the accounts to work with the new cash flow report,
    we want to be careful to only write the new cashflow tags on accounts,
    and nothing else.
    
    2. Mark the old cashflow statement tags as deprecated.

--- addons/l10n_mn/migrations/1.1/post-migrate.py
+++ addons/l10n_mn/migrations/1.1/post-migrate.py
@@ -9,13 +9,15 @@ def migrate(cr, version):
 
     # Set the new cashflow statement tags based on the templates
     account_templates = env['account.account.template'].search([('chart_template_id', '=', env.ref('l10n_mn.mn_chart_1').id)])
+    all_account_tags_xmlids = env['account.account.tag'].search([('applicability', '=', 'accounts')]).get_external_id()
+    mn_cashflow_tags = env['account.account.tag'].browse([id for id, xmlid in all_account_tags_xmlids.items() if xmlid.startswith('l10n_mn.account_cashflow_tag_')])
     for company in env['res.company'].search([('chart_template_id', '=', env.ref('l10n_mn.mn_chart_1').id)]):
         for account in env['account.account'].search([('company_id', '=', company.id)]):
             matching_template = account_templates.filtered(lambda account_template: account.code.startswith(account_template.code))
             if matching_template:
-                cashflow_tag = matching_template.tag_ids
-                if cashflow_tag:
-                    account.write({'tag_ids': [Command.link(cashflow_tag.id)]})
+                cashflow_tags = matching_template.tag_ids & mn_cashflow_tags
+                if cashflow_tags:
+                    account.write({'tag_ids': [Command.link(tag_id) for tag_id in cashflow_tags.ids]})
 
     # Update all taxes with the new tax repartition lines on templates.
     update_taxes_from_templates(cr, 'l10n_mn.mn_chart_1')
