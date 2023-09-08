a869ee87f28c517c67d782b132b65aaf86309d1e
vlst <vlst@odoo.com>
2023-09-08 16:18:43 +0200

    [REF] pos: use reactivity in ClosingPopup
    The "ClosingPopup" relies on an error-prone pattern of calling
    a "handle" function on each input change.
    
    In this commit we refactor it's logic such that the calculation
    is done reactively. 
    
    This leads to much simpler code.
    
    In addition, we remove multiple pieces of dead code.
    
    closes odoo/odoo#131171
    
    Task: 3499250
    Related: odoo/enterprise#47323
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/point_of_sale/models/pos_session.py
+++ addons/point_of_sale/models/pos_session.py
@@ -545,7 +545,6 @@ class PosSession(models.Model):
         self.ensure_one()
         orders = self.order_ids.filtered(lambda o: o.state == 'paid' or o.state == 'invoiced')
         payments = orders.payment_ids.filtered(lambda p: p.payment_method_id.type != "pay_later")
-        pay_later_payments = orders.payment_ids - payments
         cash_payment_method_ids = self.payment_method_ids.filtered(lambda pm: pm.type == 'cash')
         default_cash_payment_method_id = cash_payment_method_ids[0] if cash_payment_method_ids else None
         total_default_cash_payment_amount = sum(payments.filtered(lambda p: p.payment_method_id == default_cash_payment_method_id).mapped('amount')) if default_cash_payment_method_id else 0
@@ -571,8 +570,6 @@ class PosSession(models.Model):
                 'quantity': len(orders),
                 'amount': sum(orders.mapped('amount_total'))
             },
-            'payments_amount': sum(payments.mapped('amount')),
-            'pay_later_amount': sum(pay_later_payments.mapped('amount')),
             'opening_notes': self.opening_notes,
             'default_cash_details': {
                 'name': default_cash_payment_method_id.name,
