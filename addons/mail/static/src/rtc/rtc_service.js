3e4ed2a43ad23116c095af67cb7e5736a9854820
tsm-odoo <tsm@odoo.com>
2023-09-11 07:56:46 +0200

    [FIX] mail: fix push to talk key detection
    Before this commit, the push to talk key combination was wrongly
    detected. Indeed, it was dependent on which key was pressed first.
    
    Steps to reproduce:
    - Register "ctrl + alt" as the push to talk key
    - Press control then alt, push to talk does not work
    - Press alt then control, push to talk works
    - If you unpress the alt key, the push to talk will stop as expected
    - If you unpress the control key, the push to talk will keep going
    
    This commit fixes the issue.
    
    closes odoo/odoo#134940
    
    X-original-commit: 5b1eeca
    Signed-off-by: Thanh Son Dodeur (tso) <tso@odoo.com>
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/mail/static/src/rtc/rtc_service.js
+++ addons/mail/static/src/rtc/rtc_service.js
@@ -218,7 +218,7 @@ export class Rtc {
             if (
                 !this.state.channel ||
                 !this.userSettingsService.usePushToTalk ||
-                !this.userSettingsService.isPushToTalkKey(ev, { ignoreModifiers: true }) ||
+                !this.userSettingsService.isPushToTalkKey(ev) ||
                 !this.state.selfSession.isTalking
             ) {
                 return;
