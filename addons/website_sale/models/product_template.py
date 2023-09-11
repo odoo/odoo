649509fd6f1657a5ffd987fb6feaef5427e576e4
Nicolas Martinelli <nim@odoo.com>
2023-09-11 14:32:33 +0200

    [FIX] website_sale: add index to website_sequence
    On a DB with 2M+ products, the computation of the
    `_default_website_sequence` [1] takes several seconds. In case of a
    product creation in batch, it can negatively affect the whole
    server performance.
    
    Adding an index solves the problem.
    
    [1] https://github.com/odoo/odoo/blob/1b9c7fecc5b4031c5d7b9bbe4f26e1e510063d12/addons/website_sale/models/product_template.py#L368
    
    closes odoo/odoo#135117
    
    X-original-commit: 44c707814f1d415a5b13086942188629b0bb7d28
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/website_sale/models/product_template.py
+++ addons/website_sale/models/product_template.py
@@ -38,7 +38,7 @@ class ProductTemplate(models.Model):
     website_size_y = fields.Integer('Size Y', default=1)
     website_ribbon_id = fields.Many2one('product.ribbon', string='Ribbon')
     website_sequence = fields.Integer('Website Sequence', help="Determine the display order in the Website E-commerce",
-                                      default=lambda self: self._default_website_sequence(), copy=False)
+                                      default=lambda self: self._default_website_sequence(), copy=False, index=True)
     public_categ_ids = fields.Many2many(
         'product.public.category', relation='product_public_category_product_template_rel',
         string='Website Product Category',
