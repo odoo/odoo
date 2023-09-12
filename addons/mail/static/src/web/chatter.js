8e1a273af69d32891a751e6d16124e3b366af4aa
Alexandre Kühn <aku@odoo.com>
2023-09-12 15:29:41 +0200

    [FIX] mail: do not autoscroll attachment box when initially open
    Before this commit, when a form view choose to open attachment box
    initially and chatter is at bottom, opening the form view was
    scrolling down to the attachment box.
    
    The auto-scroll to opened attachment box is desirable when the
    user explicitly interact with the attachment button, to show
    the attachments. However, it should not be triggered when opening
    the form.
    
    This commit fixes the issue by limiting auto-scroll to attachment
    box only when explicitly chosen by the user.

--- addons/mail/static/src/web/chatter.js
+++ addons/mail/static/src/web/chatter.js
@@ -98,6 +98,7 @@ export class Chatter extends Component {
             composerType: false,
             isAttachmentBoxOpened: this.props.isAttachmentBoxVisibleInitially,
             jumpThreadPresent: 0,
+            scrollToAttachments: 0,
             showActivities: true,
             showAttachmentLoading: false,
             /** @type {import("@mail/core/thread_model").Thread} */
@@ -172,12 +173,12 @@ export class Chatter extends Component {
             () => [this.attachments]
         );
         useEffect(
-            (opened) => {
-                if (opened) {
+            () => {
+                if (this.state.scrollToAttachments > 0) {
                     this.attachmentBox.el.scrollIntoView({ block: "center" });
                 }
             },
-            () => [this.state.isAttachmentBoxOpened]
+            () => [this.state.scrollToAttachments]
         );
         useEffect(
             () => {
@@ -437,6 +438,9 @@ export class Chatter extends Component {
             return;
         }
         this.state.isAttachmentBoxOpened = !this.state.isAttachmentBoxOpened;
+        if (this.state.isAttachmentBoxOpened) {
+            this.state.scrollToAttachments++;
+        }
     }
 
     async onClickAttachFile(ev) {
