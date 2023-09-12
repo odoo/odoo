19b18eac7058731f92c98239bc4930a6c5e4b9d9
maad-odoo <maad@odoo.com>
2023-09-12 05:36:25 +0000

    [FIX] mail: activities creation date according to user timezone
    Before this commit:
    When any activity was created, the displayed created date was in UTC time
    and not adjusted to the user's timezone. The time was not converted
    according to the user's timezone.
    
    After this commit:
    The activity's created time now displayed is converted from the UTC time
    stored during the activity's creation.
    
    task-3483695
    
    closes odoo/odoo#137684
    
    X-original-commit: d016456c72d7927c10120a556adce9e73227523a
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/web/activity.js
+++ addons/mail/static/src/core/web/activity.js
@@ -9,6 +9,7 @@ import { AvatarCardPopover } from "@mail/discuss/web/avatar_card/avatar_card_pop
 import { Component, onMounted, onWillUnmount, useState } from "@odoo/owl";
 
 import { browser } from "@web/core/browser/browser";
+import { deserializeDateTime } from "@web/core/l10n/dates";
 import { _t } from "@web/core/l10n/translation";
 import { usePopover } from "@web/core/popover/popover_hook";
 import { useService } from "@web/core/utils/hooks";
@@ -48,6 +49,12 @@ export class Activity extends Component {
             return _t("“%s”", this.props.data.summary);
         }
         return this.props.data.display_name;
+    }
+
+    get displayCreateDate() {
+        return deserializeDateTime(this.props.data.create_date).toLocaleString(
+            luxon.DateTime.DATETIME_SHORT_WITH_SECONDS
+        );
     }
 
     updateDelayAtNight() {
