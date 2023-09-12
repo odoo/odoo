88f3a521193320c9cda9c7f0ceb69a64ba16db55
roen-odoo <roen@odoo.com>
2023-09-12 14:21:12 +0000

    [FIX] pos_sale: Correctly split lines when product are not groupable
    Current behavior:
    When importing a sale order with a product that is not groupable, the
    line is not split and the whole quantity is added to one line.
    
    Steps to reproduce:
    - Create a new uom category that is not groupable
    - Add a new uom to the category
    - Create a new product with the uom
    - Create a new sale order with the product
    - Settle the order in the PoS
    - All quantity is on one line
    
    This is also an issue if the product is tracked by serial number. The
    popup to fill the serial number will only have one field, even if the
    quantity is greater than 1.
    
    opw-3493378
    
    X-original-commit: 8e1af710ba15a6cba1d176491942a5fa436a0604

--- addons/pos_sale/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
+++ addons/pos_sale/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
@@ -198,21 +198,19 @@ class SaleOrderManagementScreen extends ControlButtonsMixin(IndependentToOrderSc
                         continue;
                     }
 
-                    const new_line = Orderline.create(
-                        {},
-                        {
-                            pos: this.env.pos,
-                            order: this.env.pos.get_order(),
-                            product: this.env.pos.db.get_product_by_id(line.product_id[0]),
-                            description: line.name,
-                            price: line.price_unit,
-                            tax_ids: orderFiscalPos ? undefined : line.tax_id,
-                            price_manually_set: false,
-                            sale_order_origin_id: clickedOrder,
-                            sale_order_line_id: line,
-                            customer_note: line.customer_note,
-                        }
-                    );
+                    const line_values = {
+                        pos: this.env.pos,
+                        order: this.env.pos.get_order(),
+                        product: this.env.pos.db.get_product_by_id(line.product_id[0]),
+                        description: line.name,
+                        price: line.price_unit,
+                        tax_ids: orderFiscalPos ? undefined : line.tax_id,
+                        price_manually_set: false,
+                        sale_order_origin_id: clickedOrder,
+                        sale_order_line_id: line,
+                        customer_note: line.customer_note,
+                    };
+                    let new_line = Orderline.create({}, line_values);
 
                     if (
                         new_line.get_product().tracking !== "none" &&
@@ -245,7 +243,19 @@ class SaleOrderManagementScreen extends ControlButtonsMixin(IndependentToOrderSc
                     new_line.setQuantityFromSOL(line);
                     new_line.set_unit_price(line.price_unit);
                     new_line.set_discount(line.discount);
-                    this.env.pos.get_order().add_orderline(new_line);
+                    const product = this.env.pos.db.get_product_by_id(line.product_id[0]);
+                    const product_unit = product.get_unit();
+                    if (product_unit && !product.get_unit().is_pos_groupable) {
+                        //loop for value of quantity
+                        for (let j = 0; j < new_line.quantity; j++) {
+                            let splitted_line = Orderline.create({}, line_values);
+                            splitted_line.quantity = 1;
+                            this.env.pos.get_order().add_orderline(splitted_line);
+                        }
+                    }
+                    else {
+                        this.env.pos.get_order().add_orderline(new_line);
+                    }
                 }
             } else {
                 // apply a downpayment
