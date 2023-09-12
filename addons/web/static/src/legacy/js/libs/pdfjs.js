3b5ba53f7718792b0ad1705de400a275ee042d25
David <david.vidal@tecnativa.com>
2023-09-12 11:07:50 +0200

    [FIX] web: recover bookmark functionality
    This reverts commit c07f693550a81ab17acf9bdf40bd26e183d1c3e7.
    
    Some users were used to this functionality in previous versions for
    opening the documents in separate tabs or windows via right click so
    they can handle that info in split screens or multiple windows or even
    share direct document links among co-workers.
    
    TT45024

--- addons/web/static/src/legacy/js/libs/pdfjs.js
+++ addons/web/static/src/legacy/js/libs/pdfjs.js
@@ -6,9 +6,8 @@ import config from 'web.config';
  * Until we have our own implementation of the /web/static/lib/pdfjs/web/viewer.{html,js,css}
  * (currently based on Firefox), this method allows us to hide the buttons that we do not want:
  * * "Open File"
- * * "View Bookmark"
- * * "Print" (Hidden on mobile device like Android, iOS, ...)
- * * "Download" (Hidden on mobile device like Android, iOS, ...)
+ * * "Print" (Hidden on mobile)
+ * * "Download"
  *
  * @link https://mozilla.github.io/pdf.js/getting_started/
  *
@@ -17,8 +16,7 @@ import config from 'web.config';
 export function hidePDFJSButtons(rootElement) {
     const cssStyle = document.createElement("style");
     cssStyle.rel = "stylesheet";
-    cssStyle.innerHTML = `button#secondaryOpenFile.secondaryToolbarButton, button#openFile.toolbarButton,
-a#secondaryViewBookmark.secondaryToolbarButton, a#viewBookmark.toolbarButton {
+    cssStyle.innerHTML = `button#secondaryOpenFile.secondaryToolbarButton, button#openFile.toolbarButton {
 display: none !important;
 }`;
     if (config.device.isMobileDevice) {
