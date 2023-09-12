15b939111b2ec47846f4127603fd1d2f1e473156
stefanorigano (SRI) <sri@odoo.com>
2023-09-12 10:16:24 +0200

    [IMP] website, *: improve the default theme
    *: web, web_editor, portal, theme_default, website_sale
    
    The goal of this commit is to improve the overall style of the default
    website theme mostly by overriding Bootstrap variables with a bit of
    customization for more advanced changes.
    
    task-3097005
    
    Part-of: odoo/odoo#120302
    Co-authored-by: Brieuc-brd <brd@odoo.com>
    Co-authored-by: Antoine (anso) <anso@odoo.com>

--- addons/website/static/src/js/tours/homepage.js
+++ addons/website/static/src/js/tours/homepage.js
@@ -4,40 +4,51 @@ import wTourUtils from "@website/js/tours/tour_utils";
 
 const snippets = [
     {
-        id: 's_cover',
-        name: 'Cover',
-    },
-    {
-        id: 's_text_image',
-        name: 'Text - Image',
+        id: 's_banner',
+        name: 'Banner',
     },
     {
         id: 's_three_columns',
         name: 'Columns',
     },
     {
-        id: 's_picture',
-        name: 'Picture',
+        id: 's_text_image',
+        name: 'Image - Text',
     },
     {
-        id: 's_quotes_carousel',
-        name: 'Quotes',
+        id: 's_masonry_block',
+        name: 'Masonry',
+    },
+    {
+        id: 's_title',
+        name: 'Title',
+    },
+    {
+        id: 's_showcase',
+        name: 'Showcase',
     },
     {
         id: 's_call_to_action',
         name: 'Call to Action',
     },
+    {
+        id: 's_quotes_carousel',
+        name: 'Quotes',
+    },
 ];
 
 wTourUtils.registerThemeHomepageTour('homepage', () => [
-    wTourUtils.dragNDrop(snippets[0]),
+    wTourUtils.dragNDrop(snippets[0], 'top'),
     wTourUtils.clickOnText(snippets[0], 'h1'),
     wTourUtils.goBackToBlocks(),
     wTourUtils.dragNDrop(snippets[1]),
     wTourUtils.dragNDrop(snippets[2]),
+    wTourUtils.clickOnSnippet(snippets[2], 'top'),
+    wTourUtils.changeBackgroundColor(),
+    wTourUtils.goBackToBlocks(),
     wTourUtils.dragNDrop(snippets[3]),
     wTourUtils.dragNDrop(snippets[4], 'top'),
     wTourUtils.dragNDrop(snippets[5]),
-    wTourUtils.clickOnSnippet(snippets[5], 'top'),
-    wTourUtils.changeBackgroundColor(),
+    wTourUtils.dragNDrop(snippets[6]),
+    wTourUtils.dragNDrop(snippets[7]),
 ]);
