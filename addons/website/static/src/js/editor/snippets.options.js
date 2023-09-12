dfd703add7d17b6e43a1dcf1c97f976f29d6547f
Benjamin Vray <bvr@odoo.com>
2023-09-12 13:55:46 +0200

    [FIX] website: fix header overlay with sidebar header
    This commit fixes two bugs with the sidebar header.
    
    Steps to reproduce the 1st bug:
    
    - Go to "/shop" and edit the page.
    - Click on the header.
    - Open the header selector.
    - Choose the "sidebar header".
    - Bug: infinite loader (or traceback from V16).
    
    Steps to reproduce the 2nd bug:
    
    - Go to "/contactus" and edit the page.
    - Click on the header.
    - Change the "Header Position" option to "over the content".
    - Save the page.
    - Go to the homepage and edit the page.
    - Click on the header.
    - Open the header selector.
    - Choose the "sidebar header".
    - Save the page.
    - Go to /contactus.
    - Bug: the "sidebar" header is broken on the "/contactus" page.
    
    The first bug was caused by triggering the deactivation of the "Overlay"
    header from a location other than a website.page (in this case, the
    "/shop" page in the steps to reproduce). In this place, the "Overlay"
    header option isn't available.
    
    However, while trying to fix this, we noticed the second bug => When we
    activate the "sidebar" header (which is a general option for all pages),
    we were deactivating the "Overlay" header only on the current page (this
    option is specific to the page). It was done since this commit [1].
    This doesn't make sense because the "Overlay" header should be
    deactivated on all pages, not just the current one.
    
    To address this in the simplest way, we modified the CSS so that the
    "Overlay" header doesn't have an impact when the sidebar header is
    activated. Without this change, we would have needed to add an RPC to
    remove the "Overlay" header on all pages, which wouldn't have been worth
    it.
    
    [1]: https://github.com/odoo/odoo/commit/618fd49642310c7b97ef3b9e6c01f8f691c7b12f
    
    task-3454161
    
    X-original-commit: 53a3509a10f4ed2f4edf5374bb19ebda57e7530e

--- addons/website/static/src/js/editor/snippets.options.js
+++ addons/website/static/src/js/editor/snippets.options.js
@@ -769,21 +769,6 @@ options.Class.include({
         const disableDataKeys = allDataKeys.filter(value => !enableDataKeys.includes(value));
         const resetViewArch = !!params.resetViewArch;
 
-        if (params.name === 'header_sidebar_opt') {
-            // When the user selects sidebar as header, make sure that the
-            // header position is regular.
-            // TODO we should avoid having that `if` in the generic option
-            // class (maybe simply use data-trigger but the header template
-            // option as no associated data-js to hack). To adapt in master.
-            await new Promise(resolve => {
-                this.trigger_up('action_demand', {
-                    actionName: 'toggle_page_option',
-                    params: [{name: 'header_overlay', value: false}],
-                    onSuccess: () => resolve(),
-                });
-            });
-        }
-
         return this._rpc({
             route: '/website/theme_customize_data',
             params: {
