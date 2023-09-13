47a4d64171982a9752353bb3f934f296a39a443e
tsm-odoo <tsm@odoo.com>
2023-09-13 16:47:20 +0200

    [REV] im_livechat, mail: remove cors from discuss route overrides
    This PR removes the livechat's discuss route overrides
    that added `cors="*"` since they are not safe (different databases on our
    cloud are considered as SameSite).
    
    In the meantime, embed livechats won't work as expected since cors is
    prevented and will be fixed in a following PR.
    
    closes odoo/odoo#135260
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/im_livechat/controllers/__init__.py
+++ addons/im_livechat/controllers/__init__.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import channel
 from . import chatbot
-from . import link_preview
 from . import main
-from . import message_reaction
-from . import rtc
-from . import thread
 from . import webclient
