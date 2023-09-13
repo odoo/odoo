e643d1431443e5a76d71b8a6ee3fb5b1e0f1b696
Julien Carion (juca) <juca@odoo.com>
2023-09-13 13:26:33 +0200

    [IMP] web: easier translation for many2one
    This commit gathers the different parts of the content from the many2one
    confirmation dialog into a single _t in order to facilitate the foreign
    translations of the content
    
    closes odoo/odoo#135468
    
    X-original-commit: e97ec328e8c6ec2b083c854b19cefe5c19db3991
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Julien Carion (juca) <juca@odoo.com>

--- addons/web/static/src/views/fields/many2one/many2one_field.js
+++ addons/web/static/src/views/fields/many2one/many2one_field.js
@@ -8,12 +8,12 @@ import { _lt } from "@web/core/l10n/translation";
 import { evaluateExpr } from "@web/core/py_js/py";
 import { registry } from "@web/core/registry";
 import { useChildRef, useOwnedDialogs, useService } from "@web/core/utils/hooks";
-import { sprintf } from "@web/core/utils/strings";
+import { escape, sprintf } from "@web/core/utils/strings";
 import { Many2XAutocomplete, useOpenMany2XRecord } from "@web/views/fields/relational_utils";
 import * as BarcodeScanner from "@web/webclient/barcode/barcode_scanner";
 import { standardFieldProps } from "../standard_field_props";
 
-import { Component, onWillUpdateProps, useState } from "@odoo/owl";
+import { Component, onWillUpdateProps, useState, markup } from "@odoo/owl";
 
 class CreateConfirmationDialog extends Component {
     static template = "web.Many2OneField.CreateConfirmationDialog";
@@ -21,6 +21,16 @@ class CreateConfirmationDialog extends Component {
 
     get title() {
         return sprintf(this.env._t("New: %s"), this.props.name);
+    }
+
+    get dialogContent() {
+        return markup(
+            sprintf(
+                this.env._t("Create <strong>%s</strong> as a new %s?"),
+                escape(this.props.value),
+                escape(this.props.name)
+            )
+        );
     }
 
     async onCreate() {
