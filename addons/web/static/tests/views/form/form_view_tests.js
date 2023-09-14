0dc8f934aa3459af0edc004653da30db0af2c046
Aaron Bohy <aab@odoo.com>
2023-09-14 09:51:06 +0200

    [FIX] web: fix randomly failing form tests
    We must wait for an additional tick to be sure that the error
    dialog is displayed, because the "unhandledrejection" event is
    triggered asynchronously.
    
    Runbot issue-24690
    Runbot issue-24691
    Runbot issue-24744
    Runbot issue-24733
    Runbot issue-24742
    
    closes odoo/odoo#135461
    
    X-original-commit: e341f592e2a8ebc3c2e479566425df89a29bf2c7
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- addons/web/static/tests/views/form/form_view_tests.js
+++ addons/web/static/tests/views/form/form_view_tests.js
@@ -6013,6 +6013,7 @@ QUnit.module("Views", (hooks) => {
         );
 
         await editInput(target, ".o_field_widget[name=int_field] input", 64);
+        await nextTick();
 
         assert.containsOnce(document.body, ".modal");
         assert.strictEqual(
@@ -12604,6 +12605,7 @@ QUnit.module("Views", (hooks) => {
         });
 
         await click(target, "button[name=do_it]");
+        await nextTick();
         assert.containsNone(target, ".o_form_error_dialog");
         assert.containsOnce(target, ".modal");
         assert.strictEqual(
