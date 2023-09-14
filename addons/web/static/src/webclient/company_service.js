73d15490373aea5efc1e3e3040f75b831e51eeaf
Dylan Kiss (dyki) <dyki@odoo.com>
2023-09-14 12:30:38 +0200

    [FIX] web: show branch hierarchy in switcher when no access to parent
    Currently, when a user has access to a branch of a company, but not to
    the company itself, the branch will not show up in the company switcher.
    As a result, the user is not able to switch back to that branch when
    logged in to another company.
    
    To fix this, we now show the whole hierarchy of the branches you have
    access to, disabling the companies/branches you don't have access to.
    
    task-3503204

--- addons/web/static/src/webclient/company_service.js
+++ addons/web/static/src/webclient/company_service.js
@@ -40,6 +40,11 @@ export const companyService = {
         }
 
         const availableCompanies = session.user_companies.allowed_companies;
+        const unavailableAncestorCompanies = session.user_companies.disallowed_ancestor_companies;
+        const availableCompaniesWithAncestors = {
+            ...availableCompanies,
+            ...unavailableAncestorCompanies,
+        };
         const allowedCompanyIds = computeAllowedCompanyIds(cids);
         const nextAvailableCompanies = allowedCompanyIds.slice();  // not using a Set because order is important
         nextAvailableCompanies.add = (companyId) => {
@@ -73,6 +78,7 @@ export const companyService = {
 
         return {
             availableCompanies,
+            unavailableAncestorCompanies,
             nextAvailableCompanies,
             get allowedCompanyIds() {
                 return allowedCompanyIds.slice();
@@ -81,7 +87,7 @@ export const companyService = {
                 return availableCompanies[allowedCompanyIds[0]];
             },
             getCompany(companyId) {
-                return availableCompanies[companyId];
+                return availableCompaniesWithAncestors[companyId];
             },
             setCompanies(mode, companyId) {
                 if (mode === "toggle") {
