b7491b5b94a1c411fce55565c2bdbddb99b32de5
Audric Onockx (auon) <auon@odoo.com>
2023-09-14 17:18:15 +0200

    [FIX] sale(_timesheet): take super's domain into account for aal.so_line
    The model `account.analytic.line` has a field `so_line`.
    In module `sale`, this field has a domain, which is defined in
    `_default_sale_line_domain()`.
    
    In module `sale_timesheet`, we override the method to add another
    condition to super's.
    
    The issue is that we also add the arg `dommain=...`, in the field
    definition, which totally overrides both methods.
    
    Moreover, it is impossible to just move that domain in the method,
    because it has to depend on `self.commercial_partner_id`,
    but the method is static.
    
    The solution is to:
    - Keep `_default_sale_line_domain()` in both modules,
    but not call it directly from the field.
    - Create a new compute field (`allowed_so_line_ids`),
    to determine all allowed so_lines,
    following the domain that is returned by those methods.
    The compute method will call the domain method on instances, which
    allows us to get their `commercial_partner_id`
    - Replace the domain arg of `so_line` by `[('id', 'in', allowed_so_line_ids)]`.
    
    task-3507195
    
    closes odoo/odoo#135528
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale/models/analytic.py
+++ addons/sale/models/analytic.py
@@ -7,13 +7,20 @@ from odoo import fields, models
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
+    allowed_so_line_ids = fields.Many2many('sale.order.line', compute='_compute_allowed_so_line_ids')
+    so_line = fields.Many2one('sale.order.line', string='Sales Order Item', domain="[('id', 'in', allowed_so_line_ids)]")
+
     def _default_sale_line_domain(self):
         """ This is only used for delivered quantity of SO line based on analytic line, and timesheet
             (see sale_timesheet). This can be override to allow further customization.
         """
+        self.ensure_one()
         return [('qty_delivered_method', '=', 'analytic')]
 
-    so_line = fields.Many2one('sale.order.line', string='Sales Order Item', domain=lambda self: self._default_sale_line_domain())
+    def _compute_allowed_so_line_ids(self):
+        for timesheet in self:
+            domain = timesheet._default_sale_line_domain()
+            timesheet.allowed_so_line_ids = self.env['sale.order.line'].search(domain)
 
 
 class AccountAnalyticApplicability(models.Model):
