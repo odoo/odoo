becef98e5dfd38e9f8158d422857436e94252e17
moerradi <moer@odoo.com>
2023-09-15 00:25:06 +0100

    [IMP] account: Exclude Off Balance Accounts from tax repartition lines
    This commit excludes off-balance accounts from appearing in tax
    repartition lines.
    Previously, off-balance accounts were included in the selection, which
    was causing confusion and unnecessary clutter in the interface.
    
    The need for this change was raised due to the observation
    that off-balance accounts are never actually used in tax repartition
    scenarios.
    Including them only complicates the account selection process without
    adding any functional value
    
    closes odoo/odoo#135808
    
    X-original-commit: 05ea6bf3a4bb94a78ca9114a8fa583a77f4eddf2
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- addons/account/models/account_tax.py
+++ addons/account/models/account_tax.py
@@ -1279,7 +1279,7 @@ class AccountTaxRepartitionLine(models.Model):
     repartition_type = fields.Selection(string="Based On", selection=[('base', 'Base'), ('tax', 'of tax')], required=True, default='tax', help="Base on which the factor will be applied.")
     account_id = fields.Many2one(string="Account",
         comodel_name='account.account',
-        domain="[('deprecated', '=', False), ('company_id', '=', company_id), ('account_type', 'not in', ('asset_receivable', 'liability_payable'))]",
+        domain="[('deprecated', '=', False), ('company_id', '=', company_id), ('account_type', 'not in', ('asset_receivable', 'liability_payable', 'off_balance'))]",
         check_company=True,
         help="Account on which to post the tax amount")
     tag_ids = fields.Many2many(string="Tax Grids", comodel_name='account.account.tag', domain=[('applicability', '=', 'taxes')], copy=True, ondelete='restrict')
