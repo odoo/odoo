835302c481817ee895c9b1f90554ec00080a8c94
Theo VINCENT (thvi) <thvi@odoo.com>
2023-09-14 15:08:55 +0200

    [FIX] pos_adyen, pos_restaurant_adyen: permissions
     - Adds some permissions to protect sensitive pos_adyen data and methods.
     - Reduces the risks of abuses of the pos_adyen features.
    
    Those changes are not optimal. The whole system should be changed.
    
     - Fixes errors when requesting adjustment/capture to Adyen (in pos_restaurant_adyen).
    
    closes odoo/odoo#135499
    
    Task-id: 3500598
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/pos_adyen/controllers/main.py
+++ addons/pos_adyen/controllers/main.py
@@ -2,14 +2,16 @@
 import logging
 import pprint
 import json
+from urllib.parse import parse_qs
 from odoo import fields, http
 from odoo.http import request
+from odoo.tools import consteq
 
 _logger = logging.getLogger(__name__)
 
 
 class PosAdyenController(http.Controller):
-    @http.route('/pos_adyen/notification', type='json', methods=['POST'], auth='none', csrf=False)
+    @http.route('/pos_adyen/notification', type='json', methods=['POST'], auth='public', csrf=False, save_session=False)
     def notification(self):
         data = json.loads(request.httprequest.data)
 
@@ -27,6 +29,24 @@ class PosAdyenController(http.Controller):
             if data['SaleToPOIResponse'].get('DiagnosisResponse'):
                 payment_method.adyen_latest_diagnosis = data['SaleToPOIResponse']['MessageHeader']['ServiceID']
             else:
+                try:
+                    adyen_additional_response = data['SaleToPOIResponse']['PaymentResponse']['Response']['AdditionalResponse']
+                    parsed_adyen_additional_response = parse_qs(adyen_additional_response)
+                    pos_hmac_metadata = parsed_adyen_additional_response.get('metadata.pos_hmac')
+                    pos_hmac = pos_hmac_metadata[0] if pos_hmac_metadata and len(pos_hmac_metadata) == 1 else None
+
+                    msg_header = data['SaleToPOIResponse']['MessageHeader']
+                    if not pos_hmac or not consteq(pos_hmac, payment_method._get_hmac(msg_header['SaleID'], msg_header['ServiceID'], msg_header['POIID'], data['SaleToPOIResponse']['PaymentResponse']['SaleData']['SaleTransactionID']['TransactionID'])):
+                        _logger.warning('Received an invalid Adyen event notification (invalid hmac): \n%s', pprint.pformat(data))
+                        return
+
+                    # The HMAC is removed to prevent anyone from using it in place of Adyen.
+                    pos_hmac_metadata_raw = 'metadata.pos_hmac='+pos_hmac
+                    safe_additional_response = adyen_additional_response.replace('&'+pos_hmac_metadata_raw, '').replace(pos_hmac_metadata_raw, '')
+                    data['SaleToPOIResponse']['PaymentResponse']['Response']['AdditionalResponse'] = safe_additional_response
+                except (KeyError, AttributeError):
+                    _logger.warning('Received an invalid Adyen event notification: \n%s', pprint.pformat(data))
+                    return
                 payment_method.adyen_latest_response = json.dumps(data)
             _logger.info('notification writed from adyen\n%s', data)
         else:
