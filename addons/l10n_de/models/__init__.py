e5a04a40cdb2baa580f29317a9dd0f4821537d5f
Antoine Dupuis (andu) <andu@odoo.com>
2023-09-18 16:32:12 +0000

    [MOV] l10n_de: Move code for setting default tags on accounts to l10n_de
    Previously, the code that sets default tags on bank accounts was in
    l10n_de_reports, so was not yet loaded when the demo company is created
    at module init of l10n_de.
    
    As a result, the demo company was created without the necessary tags
    on the 1001 Cash and 1201 Bank accounts, which meant that the Balance
    Sheet would not be impacted by these accounts on the demo company.
    
    This commit fixes this.
    
    taskid:none
    
    closes odoo/odoo#145483
    
    X-original-commit: 68a6db4f96cfb74e8d82127e7089b809c81b1a22
    Related: odoo/enterprise#52358
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/l10n_de/models/__init__.py
+++ addons/l10n_de/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import account_journal
 from . import account_move
 from . import datev
 from . import chart_template
