62b88a7769c8c36d950bab0ef5051a20f8caef1d
Aditya Dubey <addu@odoo.com>
2023-09-18 13:19:24 +0000

    [FIX] hr_holidays: fixed user access right in kanban view
    Steps:
    - Open Time Off module
    - Navigate to user's kanban view
    - Without any HR app access, mouseover a leave request
    - Approve/Refuse buttons appear even when user lacks necessary permissions
    
    Issue:
    - Users without any HR app access rights are still able to see "Approve" and
    "Refuse" buttons from the kanban view.
    
    Cause:
    - The kanban view does not properly check for HR app permissions before
    displaying the "Approve" and "Refuse" buttons.
    
    Fix:
    - Updated the kanban view logic to hide the "Approve" and "Refuse" buttons for
    users who don't have the necessary HR app access.
    
    Task-3507942
    
    X-original-commit: 4a30e170d4b0bea0b1178dc67c41bdfe83bbe884

--- addons/hr_holidays/static/src/views/calendar/calendar_controller.js
+++ addons/hr_holidays/static/src/views/calendar/calendar_controller.js
@@ -93,7 +93,7 @@ export class TimeOffCalendarController extends CalendarController {
                     resModel: this.model.resModel,
                     resId: record.id || false,
                     context,
-                    title: record.title,
+                    title: _t("Time Off Request"),
                     viewId: this.model.formViewId,
                     onRecordSaved: onDialogClosed,
                     onRecordDeleted: (record) => this.deleteRecord(record),
