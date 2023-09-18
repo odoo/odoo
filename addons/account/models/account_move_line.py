ecf07eda1412f8d63d6f1f7a98bc2d6684f26158
Nshimiyimana Séna <sesn@odoo.com>
2023-09-18 15:28:43 +0200

    [FIX] account: prevent unnecessary recompute of invoice line account_id
    Summary
    ------
    Currently, when removing a product from an invoice line, the account for
    that invoice line is automatically reset to a default value.
    
    Steps to reproduce
    ------------------
    * install `account_accountant`
    * create an invoice and add a product
    * set a different account on that invoice line
    * remove the product from the line
    
    You should see that the account is automatically changed.
    
    opw-3092556
    
    closes odoo/odoo#139361
    
    X-original-commit: 39b44dbfef0d533722ce2c96d375ada113d32b00
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>
    Signed-off-by: Séna Serge Nshimiyimana (sesn) <sesn@odoo.com>

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -1135,6 +1135,7 @@ class AccountMoveLine(models.Model):
     @api.onchange('product_id')
     def _inverse_product_id(self):
         self._conditional_add_to_compute('account_id', lambda line: (
+            (self.product_id or not self.account_id) and
             line.display_type == 'product' and line.move_id.is_invoice(True)
         ))
 
