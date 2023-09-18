99de2cb064169ae4eabc2846def50f5ce9903996
Bastien (bvdn) <bvdn@odoo.com>
2023-09-18 15:55:04 +0200

    [FIX] project: write tests for project tags filter
    Created custom filters using tag_ids for the tour to click on
    using the add custom filter in the filter dropdown was buggy when used in the tour
    the o_filter_condition div didnt update when changing the custom filter
    
    Task-3511261
    
    closes odoo/odoo#138683
    
    X-original-commit: 6a4c421f368cf64760f008a316592b4fdc59f10f
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

---
+++ addons/project/static/tests/tours/project_tags_filter_tour_tests.js
@@ -0,0 +1,55 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { stepUtils } from "@web_tour/js/tour_step_utils";
+
+function changeFilter(filterName) {
+    return [
+        {
+            trigger: '.o_favorite_menu button:has(i.fa-star)',
+            content: 'click on the favorite menu',
+        },
+        {
+            trigger: `.o_favorite_menu .dropdown-item span:contains("${filterName}")`,
+        },
+        {
+            trigger: '.o_group_by_menu button:has(i.oi-group)',
+            content: 'click on the groupby menu',
+            run: function (actions) {
+                this.$anchor[0].dispatchEvent(new Event('mouseenter'));
+            },
+        },
+        {
+            trigger: '.o_group_by_menu span:contains("Stage")',
+            content: 'click on the stage gb',
+        },
+    ];
+}
+
+registry.category("web_tour.tours").add('project_tags_filter_tour',
+    {
+        test: true,
+        url: '/web',
+        steps: [stepUtils.showAppsMenuItem(),
+{
+    trigger: '.o_app[data-menu-xmlid="project.menu_main_pm"]',
+}, ...changeFilter("Corkscrew tail tag filter"),
+{
+    trigger: '.o_kanban_group:has(.o_kanban_header:has(span:contains("pig"))) .o_kanban_record:has(span:contains("Pigs"))',
+    extra_trigger: '.o_kanban_group:has(.o_kanban_header:has(span:contains("goat"))):not(:has(.o_kanban_record))',
+    content: 'check that the corkscrew tail filter has taken effect',
+    run: () => {},
+}, ...changeFilter("horned tag filter"),
+{
+    trigger: '.o_kanban_group:has(.o_kanban_header:has(span:contains("goat"))) .o_kanban_record:has(span:contains("Goats"))',
+    extra_trigger: '.o_kanban_group:has(.o_kanban_header:has(span:contains("pig"))):not(:has(.o_kanban_record))',
+    content: 'check that the horned filter has taken effect',
+    run: () => {},
+}, ...changeFilter("4 Legged tag filter"),
+{
+    trigger: '.o_kanban_group:has(.o_kanban_header:has(span:contains("goat"))) .o_kanban_record:has(span:contains("Goats"))',
+    extra_trigger: '.o_kanban_group:has(.o_kanban_header:has(span:contains("pig"))) .o_kanban_record:has(span:contains("Pigs"))',
+    content: 'check that the 4 legged filter has taken effect',
+    run: () => {},
+},
+]});
