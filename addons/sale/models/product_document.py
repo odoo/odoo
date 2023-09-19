f81a57b5129190fbbf5430fccb2df113c23c87a2
Victor Feyens <vfe@odoo.com>
2023-09-19 13:24:00 +0200

    [FIX] sale: new field help
    Part-of: odoo/odoo#135894

--- addons/sale/models/product_document.py
+++ addons/sale/models/product_document.py
@@ -12,11 +12,11 @@ class ProductDocument(models.Model):
             ('sale_order', "Confirmed order"),
         ],
         string="Visible at",
-        help="Allows you to share the document with your customers within a sale."
-            "Leave it empty if you don't want to share this document with sales customer."
-            "Quotation: the document will be sent to and accessible by customers at any time."
-                "e.g. this option can be useful to share Product description files."
-            "Confirmed order: the document will be sent to and accessible by customers"
+        help="Allows you to share the document with your customers within a sale.\n"
+            "Leave it empty if you don't want to share this document with sales customer.\n"
+            "Quotation: the document will be sent to and accessible by customers at any time.\n"
+                "e.g. this option can be useful to share Product description files.\n"
+            "Confirmed order: the document will be sent to and accessible by customers.\n"
                 "e.g. this option can be useful to share User Manual or digital content bought"
                 " on ecommerce. ",
     )
