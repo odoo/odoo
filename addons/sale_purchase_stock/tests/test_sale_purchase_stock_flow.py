99b182f53404641fbe335b293066c5c66aa35eed
Adrien Widart (awt) <awt@odoo.com>
2023-09-19 08:08:06 +0000

    [FIX] sale_stock: base delivered qty on done qty
    In some cases, the delivered quantity of a SOL is incorrect
    
    To reproduce the issue:
    1. In Settings, enable "Multi Routes"
    2. In Routes, unarchive MTO
    3. Create a product P:
       - Storable
       - With a vendor
       - Routes: MTO + Buy
    4. Create and confirm a SO for 10 x P
    5. On the delivery, set the done quantity to 12
    6. Validate the delivery
    7. Unlock it and edit the done qty: 10
    8. Open the SO
    
    Error: The delivered quantity is still 12
    
    Step 5, because the done quantity is greater than the demand, we
    create and process a SM for 2 x P (the done quantity of this SM will
    be zero as the initial one keeps a done quantity equal to 12).
    Moreover, Since the procure methods are different (MTO vs MTS), the
    SMs are not merged.
    
    Step 7, we decrease the done quantity of the initial SM, it impacts
    its demand (here actually it does nothing as the demand is already
    equal to 10), but we don't change the demand of the newly-created SM.
    Here is the issue: to compute the delivered qty, we use the demand
    of the done SMs: 10 + 2.
    
    OPW-3504138
    
    X-original-commit: aed12941b32a462c1b49e93a81b88349a354e089

--- addons/sale_purchase_stock/tests/test_sale_purchase_stock_flow.py
+++ addons/sale_purchase_stock/tests/test_sale_purchase_stock_flow.py
@@ -6,38 +6,80 @@ from odoo.tests.common import TransactionCase, Form
 
 class TestSalePurchaseStockFlow(TransactionCase):
 
+    @classmethod
+    def setUpClass(cls):
+        super(TestSalePurchaseStockFlow, cls).setUpClass()
+        cls.mto_route = cls.env.ref('stock.route_warehouse0_mto')
+        cls.buy_route = cls.env.ref('purchase_stock.route_warehouse0_buy')
+        cls.mto_route.active = True
+
+        cls.customer_location = cls.env.ref('stock.stock_location_customers')
+
+        cls.vendor = cls.env['res.partner'].create({'name': 'Super Vendor'})
+        cls.customer = cls.env['res.partner'].create({'name': 'Super Customer'})
+
+        cls.mto_product = cls.env['product.product'].create({
+            'name': 'SuperProduct',
+            'type': 'product',
+            'route_ids': [(6, 0, (cls.mto_route + cls.buy_route).ids)],
+            'seller_ids': [(0, 0, {
+                'partner_id': cls.vendor.id,
+            })],
+        })
+
     def test_cancel_so_with_draft_po(self):
         """
         Sell a MTO+Buy product -> a PO is generated
         Cancel the SO -> an activity should be added to the PO
         """
-        mto_route = self.env.ref('stock.route_warehouse0_mto')
-        buy_route = self.env.ref('purchase_stock.route_warehouse0_buy')
-        mto_route.active = True
-
-        vendor = self.env['res.partner'].create({
-            'name': 'Super Vendor'
-        })
-
-        product = self.env['product.product'].create({
-            'name': 'SuperProduct',
-            'type': 'product',
-            'route_ids': [(6, 0, (mto_route + buy_route).ids)],
-            'seller_ids': [(0, 0, {
-                'partner_id': vendor.id,
-            })],
-        })
-
         so_form = Form(self.env['sale.order'])
         so_form.partner_id = self.env.user.partner_id
         with so_form.order_line.new() as line:
-            line.product_id = product
+            line.product_id = self.mto_product
         so = so_form.save()
         so.action_confirm()
 
-        po = self.env['purchase.order'].search([('partner_id', '=', vendor.id)])
+        po = self.env['purchase.order'].search([('partner_id', '=', self.vendor.id)])
 
         so._action_cancel()
 
         self.assertTrue(po.activity_ids)
         self.assertIn(so.name, po.activity_ids.note)
+
+    def test_qty_delivered_with_mto_and_done_quantity_change(self):
+        """
+        MTO product P
+        Sell 10 x P. On the delivery, set the done quantity to 12, validate and
+        then set the done quantity to 10: the delivered qty of the SOL should
+        be 10
+        """
+        so = self.env['sale.order'].create({
+            'partner_id': self.customer.id,
+            'order_line': [(0, 0, {
+                'name': self.mto_product.name,
+                'product_id': self.mto_product.id,
+                'product_uom_qty': 10,
+                'product_uom': self.mto_product.uom_id.id,
+                'price_unit': 1,
+            })],
+        })
+        so.action_confirm()
+
+        delivery = so.picking_ids.filtered(lambda p: p.location_dest_id == self.customer_location)
+        sm = delivery.move_ids
+        sm.move_line_ids = [(5, 0, 0), (0, 0, {
+            'location_id': sm.location_id.id,
+            'location_dest_id': sm.location_dest_id.id,
+            'product_id': sm.product_id.id,
+            'qty_done': 12,
+            'company_id': sm.company_id.id,
+            'product_uom_id': sm.product_uom.id,
+            'picking_id': delivery.id,
+        })]
+        delivery.button_validate()
+
+        self.assertEqual(delivery.state, 'done')
+        self.assertEqual(so.order_line.qty_delivered, 12)
+
+        sm.move_line_ids.qty_done = 10
+        self.assertEqual(so.order_line.qty_delivered, 10)
