0c6a3b6a13feade242b723111d8838b583be56e5
niyasraphy <niyasraphyk@gmail.com>
2023-09-19 20:50:31 +0300

    [FIX] web: traceback on opening full accounting user group

--- addons/web/models/models.py
+++ addons/web/models/models.py
@@ -135,7 +135,7 @@ class Base(models.AbstractModel):
                 co_records = self[field_name]
 
                 if 'order' in field_spec and field_spec['order']:
-                    ids_to_read = sorted(co_records.ids, key=lambda k: field_spec['order'])
+                    ids_to_read = sorted(co_records.ids, key=lambda k: field_spec.get(k, 0))
                     co_records = co_records.browse(ids_to_read)
                     order_key = {
                         co_record.id: index
