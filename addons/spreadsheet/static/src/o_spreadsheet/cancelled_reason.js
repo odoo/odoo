c39c329d6b9d7562cff98240c2c60d55e8f86f52
Adrien Minne (adrm) <adrm@odoo.com>
2023-09-19 11:22:28 +0200

    [IMP] spreadsheet: allow to reorder global filters
    This commit allows to reorder global filters via drag and drop. The
    fact that global filters have a concept of order means that the
    structure of the global filters in the core plugin need to change from
    an Object<string, GlobalFilter> to an Array<GlobalFilter>.
    
    closes odoo/odoo#135869
    
    Task: 3502684
    Related: odoo/enterprise#47583
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/spreadsheet/static/src/o_spreadsheet/cancelled_reason.js
+++ addons/spreadsheet/static/src/o_spreadsheet/cancelled_reason.js
@@ -3,6 +3,7 @@
 export default {
     Success: "Success", // should be imported from o-spreadsheet instead of redefined here
     FilterNotFound: "FilterNotFound",
+    InvalidFilterMove: "InvalidFilterMove",
     DuplicatedFilterLabel: "DuplicatedFilterLabel",
     PivotCacheNotLoaded: "PivotCacheNotLoaded",
     InvalidValueTypeCombination: "InvalidValueTypeCombination",
