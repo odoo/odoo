9dc44320e9b3f156abaf603639f22bff8125f1c1
Lucas Lefèvre (lul) <lul@odoo.com>
2023-09-19 07:09:18 +0000

    [FIX] spreadsheet: cast currency rate date
    This commit fixes two issues with the ´date´ argument for
    the ODOO.CURRENCY.RATE function.
    
    First, the helper to cast the date is `toJsDate`, not `toJSDate`. Using
    the wrong function name obviously crashes.
    
    Second, the date was actually given to the server as a full ISO datetime
    string, including timezone (UTC).
    
    For function `ODOO.CURRENCY.RATE("EUR","USD", "12-31-2020 00:00:00")`, we
    actually sent to the server "2020-12-30T23:00:00.000Z" (Brussels local time)
    Notice that it's the previous day!
    
    The field of `res.currency.date` is a date field, so it doesn't make sense
    to send a datetime.
    
    Now, only the date is sent ("2020-12-31")
    
    opw-3498115
    
    closes odoo/odoo#135987
    
    X-original-commit: 4797c511dacf1a46042b90e04b9ced8a25a71238
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet/static/src/currency/formulas.js
+++ addons/spreadsheet/static/src/currency/formulas.js
@@ -2,23 +2,23 @@
 
 import { _t } from "@web/core/l10n/translation";
 import spreadsheet from "../o_spreadsheet/o_spreadsheet_extended";
-const { arg, toString, toJSDate } = spreadsheet.helpers;
+const { arg, toString, toJsDate } = spreadsheet.helpers;
 const { functionRegistry } = spreadsheet.registries;
 
-
-functionRegistry
-    .add("ODOO.CURRENCY.RATE", {
-        description: _t("This function takes in two currency codes as arguments, and returns the exchange rate from the first currency to the second as float."),
-        compute: function (currencyFrom, currencyTo, date) {
-            const from = toString(currencyFrom);
-            const to = toString(currencyTo);
-            const _date = date ? toJSDate(date) : undefined;
-            return this.getters.getCurrencyRate(from, to, _date);
-        },
-        args: [
-            arg("currency_from (string)", _t("First currency code.")),
-            arg("currency_to (string)", _t("Second currency code.")),
-            arg("date (date, optional)", _t("Date of the rate.")),
-        ],
+functionRegistry.add("ODOO.CURRENCY.RATE", {
+    description: _t(
+        "This function takes in two currency codes as arguments, and returns the exchange rate from the first currency to the second as float."
+    ),
+    compute: function (currencyFrom, currencyTo, date) {
+        const from = toString(currencyFrom);
+        const to = toString(currencyTo);
+        const _date = date ? toJsDate(date) : undefined;
+        return this.getters.getCurrencyRate(from, to, _date);
+    },
+    args: [
+        arg("currency_from (string)", _t("First currency code.")),
+        arg("currency_to (string)", _t("Second currency code.")),
+        arg("date (date, optional)", _t("Date of the rate.")),
+    ],
     returns: ["NUMBER"],
 });
