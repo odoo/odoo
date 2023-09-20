10b855ea8702baade556edf6aa3318916e4505f7
Sébastien Geelen (sge) <sge@odoo.com>
2023-09-20 10:58:56 +0200

    [ADD] web_editor: add field html history
    Create a new model mixins that allow other model to
    activate the history feature on html fields.
    
    This history automatically track the changes
    on the related html field and allow the user
    to revert to a previous version at any time.
    
    This also introduce a new component that can be used to :
    * List the recent history
    * See a previous version of the document
    * Compare a previous version to the current one
    * Revert the document to a previous version of it
    
    task-3039787
    
    closes odoo/odoo#112957
    
    Related: odoo/enterprise#37211
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

---
+++ addons/web_editor/static/src/components/history_dialog/history_dialog.js
@@ -0,0 +1,97 @@
+/** @odoo-module **/
+
+import { Dialog } from '@web/core/dialog/dialog';
+import { formatDateTime } from '@web/core/l10n/dates';
+import { useService } from '@web/core/utils/hooks';
+import { memoize } from '@web/core/utils/functions';
+import { Component, onMounted, useState, markup } from '@odoo/owl';
+import { _t } from '@web/core/l10n/translation';
+
+const { DateTime } = luxon;
+
+class HistoryDialog extends Component {
+    static template = 'web_editor.HistoryDialog';
+    static components = { Dialog };
+    static props = {
+        recordId: Number,
+        recordModel: String,
+        close: Function,
+        restoreRequested: Function,
+        historyMetadata: Array,
+        versionedFieldName: String
+    };
+
+    state = useState({
+        revisionsData: [],
+        revisionContent: null,
+        revisionComparison: null,
+        revisionId: null
+    });
+
+    setup() {
+        this.size = 'xl';
+        this.title = _t('History');
+        this.orm = useService('orm');
+
+        onMounted(() => this.init());
+    }
+
+    async init() {
+        this.state.revisionsData = this.props.historyMetadata;
+        await this.updateCurrentRevision(this.props.historyMetadata[0]['revision_id']);
+    }
+
+    async updateCurrentRevision(revisionId) {
+        if (this.state.revisionId === revisionId) {
+            return;
+        }
+        this.env.services.ui.block();
+        this.state.revisionId = revisionId;
+        this.state.revisionContent = await this.getRevisionContent(revisionId);
+        this.state.revisionComparison = await this.getRevisionComparison(
+            revisionId
+        );
+        this.env.services.ui.unblock();
+    }
+
+    getRevisionComparison = memoize(
+        async function getRevisionComparison(revisionId) {
+            const comparison = await this.orm.call(
+                this.props.recordModel,
+                'html_field_history_get_comparison_at_revision',
+                [this.props.recordId, this.props.versionedFieldName, revisionId]
+            );
+            return markup(comparison);
+        }.bind(this)
+    );
+
+    getRevisionContent = memoize(
+        async function getRevisionContent(revisionId) {
+            const content = await this.orm.call(
+                this.props.recordModel,
+                'html_field_history_get_content_at_revision',
+                [this.props.recordId, this.props.versionedFieldName, revisionId]
+            );
+            return markup(content);
+        }.bind(this)
+    );
+
+    async _onRestoreRevisionClick() {
+        this.env.services.ui.block();
+        const restoredContent = await this.getRevisionContent(
+            this.state.revisionId
+        );
+        this.props.restoreRequested(restoredContent);
+        this.env.services.ui.unblock();
+        this.props.close();
+    }
+
+    /**
+     * Getters
+     **/
+    getRevisionDate(revision) {
+        return formatDateTime(DateTime.fromISO(revision['create_date']));
+    }
+}
+
+export default HistoryDialog;
