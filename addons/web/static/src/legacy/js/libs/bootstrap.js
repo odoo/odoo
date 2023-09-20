cb744dbd5537027abbb214834601e66e09edbf3e
Benjamin Vray <bvr@odoo.com>
2023-09-20 10:41:50 +0200

    [FIX] website, web: fix dropdown hidden in mobile menu
    Steps to reproduce the bug:
    - Modify the website's menu to include only two items: a link to a page
    and a dropdown menu.
    - Switch to mobile preview.
    - Open the "Hamburger" menu.
    - Click on the dropdown toggle.
    - Bug: The dropdown menu is hidden and requires scrolling the navbar to
    see it.
    
    Since commit [1], dynamic positioning of dropdowns within a navbar has
    been enabled (before that, Bootstrap disabled it) but this change
    introduced this bug.
    
    To fix this, we disable dynamic positioning of the dropdowns when they
    are inside a "Hamburger" menu.
    
    [1]: https://github.com/odoo/odoo/commit/8689241f86e2d4ddb4e4510951f92b80e115b914
    
    opw-3493512

--- addons/web/static/src/legacy/js/libs/bootstrap.js
+++ addons/web/static/src/legacy/js/libs/bootstrap.js
@@ -153,10 +153,11 @@ ScrollSpy.prototype._activate = function (target) {
  * Bootstrap disables dynamic dropdown positioning when it is in a navbar. Here
  * we make this patch to activate this dynamic navbar's dropdown positioning
  * which is useful to avoid that the elements of the website sub-menus overflow
- * the page.
+ * the page. Note that when the menu is "hamburger" type, we leave dynamic
+ * positioning disabled.
  */
 Dropdown.prototype._detectNavbar = function () {
-    return false;
+    return this._element.closest(".navbar-collapse.show");
 };
 
 /* Bootstrap modal scrollbar compensation on non-body */
