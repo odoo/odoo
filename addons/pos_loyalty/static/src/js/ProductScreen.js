bbe5c29c7bd83890580e0dc9fc85a7e80d1c66c6
Akim Juillerat <akim.juillerat@camptocamp.com>
2023-09-21 18:29:41 +0200

    [FIX] pos_loyalty: Check gift card  prefix from nomenclature
    The barcode nomenclature allows to define custom prefix for gift cards.
    e.g. the default nomenclature in demo data allows both 043 or 044 as
    prefix for rule of type coupon.
    
    Therefore the hardcoded string with 044 doesn't allow to sell a gift
    card whose barcode does not start with this.
    
    Instead of hardcoding the value, fetch it from the configuration and
    the nomenclature that is defined in the settings.
    
    Steps to reproduce:
    1. Activate Gift cards in the POS config settings
    2. Set as Scan existing cards
    3. Make sure default nomenclature is selected in the point of sale with 043 as prefix for coupons
    4. Start a pos session
    5. Select gift card product
    6. Scan or enter a barcode beginning with 043
    
    OPW-3499787

--- addons/pos_loyalty/static/src/js/ProductScreen.js
+++ addons/pos_loyalty/static/src/js/ProductScreen.js
@@ -56,7 +56,21 @@ export const PosLoyaltyProductScreen = (ProductScreen) =>
                     return false;
                 }
                 const trimmedCode = code.trim();
-                if (trimmedCode && trimmedCode.startsWith('044')) {
+                let nomenclatureRules = this.env.barcode_reader.barcode_parser.nomenclature.rules;
+                if (this.env.barcode_reader.fallbackBarcodeParser) {
+                    nomenclatureRules.push(...this.env.barcode_reader.fallbackBarcodeParser.nomenclature.rules);
+                }
+                const couponNomenclatureRules = _.filter(nomenclatureRules, function(rule) {
+                    return rule.type == "coupon";
+                });
+                let nomenclatureCodePatterns = [];
+                _.each(_.pluck(couponNomenclatureRules, "pattern"), function(pattern){
+                    nomenclatureCodePatterns.push(...pattern.split("|"));
+                });
+                const trimmedCodeValid = _.find(nomenclatureCodePatterns, function(pattern) {
+                    return trimmedCode.startsWith(pattern);
+                });
+                if (trimmedCode && trimmedCodeValid) {
                     // check if the code exist in the database
                     // if so, use its balance, otherwise, use the unit price of the gift card product
                     const fetchedGiftCard = await this.rpc({
