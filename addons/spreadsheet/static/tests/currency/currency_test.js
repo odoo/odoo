odoo/odoo

commit f693d9b5ffb8a6789260f890e595e542ce154922

[FIX] spreadsheet: cast currency rate date
This commit fixes two issues with the ´date´ argument for
the ODOO.CURRENCY.RATE function.

First, the helper to cast the date is `toJsDate`, not `toJSDate`. Using
the wrong function name obviously crashes.

Second, the date was actually given to the server as a full ISO datetime
string, including timezone (UTC).

For function `ODOO.CURRENCY.RATE("EUR","USD", "11-30-2020 00:00:00")`, we
actually sent to the server "2020-12-30T23:00:00.000Z" (Brussels local time)
Notice that it's the previous day!

The field of `res.currency.date` is a date field, so it doesn't make sense
to send a datetime.

Now, only the date is sent ("2020-12-31")

opw-3498115

closes odoo/odoo#135847

Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

addons/spreadsheet/static/src/currency/currency_data_source.js
@@ -2,6 +2,7 @@
 
 import { _t } from "@web/core/l10n/translation";
 import { ServerData } from "../data_sources/server_data";
+import { toServerDateString } from "../helpers/helpers";
 
 /**
  * @typedef Currency
@@ -29,7 +30,7 @@ export class CurrencyDataSource {
         const data = this.serverData.batch.get("res.currency.rate", "get_rates_for_spreadsheet", {
             from,
             to,
-            date,
+            date: toServerDateString(date),
         });
         const rate = data !== undefined ? data.rate : undefined;
         if (rate === false) {

addons/spreadsheet/static/tests/currency/currency_test.js
@@ -2,7 +2,10 @@
 
 import { setCellContent } from "@spreadsheet/../tests/utils/commands";
 import { getCell, getCellValue } from "@spreadsheet/../tests/utils/getters";
-import { createModelWithDataSource, waitForDataSourcesLoaded } from "@spreadsheet/../tests/utils/model";
+import {
+    createModelWithDataSource,
+    waitForDataSourcesLoaded,
+} from "@spreadsheet/../tests/utils/model";
 
 QUnit.module("spreadsheet > Currency");
 
@@ -19,6 +22,27 @@ QUnit.test("Basic exchange formula", async (assert) => {
     assert.strictEqual(getCellValue(model, "A1"), "Loading...");
     await waitForDataSourcesLoaded(model);
     assert.strictEqual(getCellValue(model, "A1"), 0.9);
+});
+
+QUnit.test("rate formula at a given date(time)", async (assert) => {
+    const model = await createModelWithDataSource({
+        mockRPC: async function (route, args) {
+            if (args.method === "get_rates_for_spreadsheet") {
+                const [A1, A2] = args.args[0];
+                assert.equal(A1.date, "2020-12-31");
+                assert.equal(A2.date, "2020-11-30");
+                assert.step("rate fetched");
+                return [
+                    { ...A1, rate: 0.9 },
+                    { ...A2, rate: 0.9 },
+                ];
+            }
+        },
+    });
+    setCellContent(model, "A1", `=ODOO.CURRENCY.RATE("EUR","USD", "12-31-2020")`);
+    setCellContent(model, "A2", `=ODOO.CURRENCY.RATE("EUR","USD", "11-30-2020 00:00:00")`);
+    await waitForDataSourcesLoaded(model);
+    assert.verifySteps(["rate fetched"]);
 });
 
 QUnit.test("Currency rate throw with unknown currency", async (assert) => {

addons/spreadsheet/static/src/currency/formulas.js
@@ -2,20 +2,20 @@
 
 import { _t } from "@web/core/l10n/translation";
 import spreadsheet from "../o_spreadsheet/o_spreadsheet_extended";
-const { args, toString, toJSDate } = spreadsheet.helpers;
+const { args, toString, toJsDate } = spreadsheet.helpers;
 const { functionRegistry } = spreadsheet.registries;
 
-
-functionRegistry
-    .add("ODOO.CURRENCY.RATE", {
-        description: _t("This function takes in two currency codes as arguments, and returns the exchange rate from the first currency to the second as float."),
-        compute: function (currencyFrom, currencyTo, date) {
-            const from = toString(currencyFrom);
-            const to = toString(currencyTo);
-            const _date = date ? toJSDate(date) : undefined;
-            return this.getters.getCurrencyRate(from, to, _date);
-        },
-        args: args(`
+functionRegistry.add("ODOO.CURRENCY.RATE", {
+    description: _t(
+        "This function takes in two currency codes as arguments, and returns the exchange rate from the first currency to the second as float."
+    ),
+    compute: function (currencyFrom, currencyTo, date) {
+        const from = toString(currencyFrom);
+        const to = toString(currencyTo);
+        const _date = date ? toJsDate(date) : undefined;
+        return this.getters.getCurrencyRate(from, to, _date);
+    },
+    args: args(`
             currency_from (string) ${_t("First currency code.")}
             currency_to (string) ${_t("Second currency code.")}
             date (date, optional) ${_t("Date of the rate.")}

commit df8faeffaa4cc5afeca54b593b4bcd886c76dc64

[FIX] spreadsheet: get currency rate without date
Commit f693d9b5 broke ODOO.CURRENCY.RATE when no date is provided.
The text 'Invalid date' is sent to the server, which obvious can't
parse it to a date.

opw-3498115

addons/spreadsheet/static/src/currency/currency_data_source.js
@@ -30,7 +30,7 @@ export class CurrencyDataSource {
         const data = this.serverData.batch.get("res.currency.rate", "get_rates_for_spreadsheet", {
             from,
             to,
-            date: toServerDateString(date),
+            date: date ? toServerDateString(date) : undefined,
         });
         const rate = data !== undefined ? data.rate : undefined;
         if (rate === false) {

addons/spreadsheet/static/tests/currency/currency_test.js
@@ -14,6 +14,10 @@ QUnit.test("Basic exchange formula", async (assert) => {
         mockRPC: async function (route, args) {
             if (args.method === "get_rates_for_spreadsheet") {
                 const info = args.args[0][0];
+                assert.equal(info.from, "EUR");
+                assert.equal(info.to, "USD");
+                assert.equal(info.date, undefined);
+                assert.step("rate fetched");
                 return [{ ...info, rate: 0.9 }];
             }
         },
@@ -22,6 +26,7 @@ QUnit.test("Basic exchange formula", async (assert) => {
     assert.strictEqual(getCellValue(model, "A1"), "Loading...");
     await waitForDataSourcesLoaded(model);
     assert.strictEqual(getCellValue(model, "A1"), 0.9);
+    assert.verifySteps(["rate fetched"]);
 });
 
 QUnit.test("rate formula at a given date(time)", async (assert) => {
@@ -43,6 +48,23 @@ QUnit.test("rate formula at a given date(time)", async (assert) => {
     setCellContent(model, "A2", `=ODOO.CURRENCY.RATE("EUR","USD", "11-30-2020 00:00:00")`);
     await waitForDataSourcesLoaded(model);
     assert.verifySteps(["rate fetched"]);
+});
+
+QUnit.test("invalid date", async (assert) => {
+    const model = await createModelWithDataSource({
+        mockRPC: async function (route, args) {
+            if (args.method === "get_rates_for_spreadsheet") {
+                throw new Error("Should not be called");
+            }
+        },
+    });
+    setCellContent(model, "A1", `=ODOO.CURRENCY.RATE("EUR","USD", "hello")`);
+    await waitForDataSourcesLoaded(model);
+    assert.strictEqual(getCellValue(model, "A1"), "#ERROR");
+    assert.strictEqual(
+        getCell(model, "A1").evaluated.error.message,
+        "The function ODOO.CURRENCY.RATE expects a number value, but 'hello' is a string, and cannot be coerced to a number."
+    );
 });
 
 QUnit.test("Currency rate throw with unknown currency", async (assert) => {