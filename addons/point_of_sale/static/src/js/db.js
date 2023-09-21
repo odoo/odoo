de2c694125f32c32eaf0b466d4481823d2c252fd
Pedram (PEBR) <pebr@odoo.com>
2023-09-21 14:23:19 +0000

    [FIX] point_of_sale: Prevent adding archived products by barcode
    When limited product loading is enabled, archived products can still be
    added to orders by scanning their barcode, because
    `get_pos_ui_product_product_by_params` loads archived products to
    prevent errors when refunding orders.
    
    This allows adding archived products via barcode even though they are not
    visible in the POS interface.
    
    To prevent this, we exclude archived products from the product_by_barcode
    when adding products to it. This ensures archived products cannot be
    added by barcode when limited product loading is enabled.
    
    opw-3502136
    
    X-original-commit: 95c3807d541afe0581b636eb8394cd8f79bb770e

--- addons/point_of_sale/static/src/js/db.js
+++ addons/point_of_sale/static/src/js/db.js
@@ -223,7 +223,7 @@ export const PosDB = core.Class.extend({
                 }
             }
             this.product_by_id[product.id] = product;
-            if (product.barcode) {
+            if (product.barcode && product.active) {
                 this.product_by_barcode[product.barcode] = product;
             }
         }
