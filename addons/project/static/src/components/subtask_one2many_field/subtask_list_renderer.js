c9154f2deeb98ff7bda6970561fcc5c9759957c4
Prakash Prajapati <ppr@odoo.com>
2023-09-25 16:40:44 +0530

    [FIX] project: default focus set on sub-task title
    Issue:
          When user add sub task then first field is focused.
    Fixed:
        In this commit, we have made the title the default focus so that the user does not
        have to click manually and time will also be saved.
    
    Steps:
         - Install project app
         - Active Task Dependencies
         - Create task and add subtask
    
    task-3471102

--- addons/project/static/src/components/subtask_one2many_field/subtask_list_renderer.js
+++ addons/project/static/src/components/subtask_one2many_field/subtask_list_renderer.js
@@ -4,10 +4,23 @@ import { useService } from "@web/core/utils/hooks";
 import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { ListRenderer } from '@web/views/list/list_renderer';
 
+const { useEffect } = owl;
+
 export class SubtaskListRenderer extends ListRenderer {
     setup() {
         super.setup();
         this.dialog = useService("dialog");
+        useEffect(
+            () => this.focusName(this.props.list.editedRecord),
+            () => [this.props.list.editedRecord]
+        );
+    }
+
+    focusName(editedRecord) {
+        if (editedRecord?.isNew && !editedRecord.isDirty) {
+            const col = this.state.columns.find((c) => c.name === "name");
+            this.focusCell(col);
+        }
     }
 
     async onDeleteRecord(record) {
