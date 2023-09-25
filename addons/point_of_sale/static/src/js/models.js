b17d09d084363655db17d9ea7b11db7e7d08390f
Loukas Wets (lowe) <lowe@odoo.com>
2023-09-25 11:38:45 +0200

    [FIX] point_of_sale: missing product cross order
    This is a backport of 17faef92aa7f6d9e5f39b8d281ffdd0208f5d05e commit to
    16.2 in accordance with the cross order task-3090565.
    
    X-original-commit: b3178b25d5303cafe327ae94934f726ba32f5ef6

--- addons/point_of_sale/static/src/js/models.js
+++ addons/point_of_sale/static/src/js/models.js
@@ -723,6 +723,7 @@ export class PosGlobalState extends PosModel {
                 message = messageFp;
             }
         }
+        await this._getMissingProducts(ordersJson);
         const allOrders = [...this.get_order_list()];
         this._replaceOrders(allOrders, ordersJson);
         this.sortOrders();
@@ -760,6 +761,17 @@ export class PosGlobalState extends PosModel {
             "get_pos_ui_product_pricelists_by_ids",
             [[odoo.pos_session_id], pricelistsToGet]
         );
+    }
+    async _getMissingProducts(ordersJson) {
+        const productIds = [];
+        for (const order of ordersJson) {
+            for (const orderline of order.lines) {
+                if (!this.db.get_product_by_id(orderline[2].product_id)) {
+                    productIds.push(orderline[2].product_id);
+                }
+            }
+        }
+        await this._addProducts(productIds, false);
     }
     _addPosPricelists(pricelistsJson) {
         if (!this.config.use_pricelist) {
