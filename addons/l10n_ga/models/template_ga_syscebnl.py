ac45fa6462210553c01ca92fe3a3e41d87da5bdd
Gauthier Wala (gawa) <gawa@odoo.com>
2023-09-26 11:54:00 +0200

    [IMP] l10n_syscohada,l10n_*: add syscebnl coa for associations
    Added the accounts and account groups for SYSCEBNL (for Associations).
    We need to create new taxes, to adapt to these accounts.
    As they are completely different, we can't make a common base for SYSCOHADA vs SYSCEBNL.
    
    closes odoo/odoo#174625
    
    X-original-commit: 4ba6ff6ad1fe03332082394a77ff5618d62d570c
    Related: odoo/enterprise#67428
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>

---
+++ addons/l10n_ga/models/template_ga_syscebnl.py
@@ -0,0 +1,30 @@
+from odoo import models, _
+from odoo.addons.account.models.chart_template import template
+
+
+class AccountChartTemplate(models.AbstractModel):
+    _inherit = 'account.chart.template'
+
+    @template('ga_syscebnl')
+    def _get_ga_syscebnl_template_data(self):
+        return {
+            'name': _('SYSCEBNL for Associations'),
+            'parent': 'syscebnl',
+            'code_digits': '6',
+        }
+
+    @template('ga_syscebnl', 'res.company')
+    def _get_ga_syscebnl_res_company(self):
+        company_values = super()._get_syscebnl_res_company()
+        company_values[self.env.company.id].update(
+            {
+                'account_fiscal_country_id': 'base.ga',
+                'account_sale_tax_id': 'syscebnl_tva_sale_19',
+                'account_purchase_tax_id': 'syscebnl_tva_purchase_19',
+            }
+        )
+        return company_values
+
+    @template('ga_syscebnl', 'account.account')
+    def _get_ga_syscebnl_account_account(self):
+        return self._parse_csv('ga_syscebnl', 'account.account', module='l10n_syscohada')
