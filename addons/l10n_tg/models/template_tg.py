0c0b1407c98d36689911e1fd69a4f80bca125b80
Gauthier Wala (gawa) <gawa@odoo.com>
2023-09-26 11:54:00 +0200

    [IMP] l10n_syscohada,l10n_*: add syscebnl coa for associations
    Added the accounts and account groups for SYSCEBNL (for Associations).
    We need to create new taxes, to adapt to these accounts.
    As they are completely different, we can't make a common base for SYSCOHADA vs SYSCEBNL.
    
    closes odoo/odoo#174498
    
    X-original-commit: 4ba6ff6ad1fe03332082394a77ff5618d62d570c
    Related: odoo/enterprise#67333
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>

--- addons/l10n_tg/models/template_tg.py
+++ addons/l10n_tg/models/template_tg.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import models, _
 from odoo.addons.account.models.chart_template import template
 
 
@@ -8,7 +8,7 @@ class AccountChartTemplate(models.AbstractModel):
     @template('tg')
     def _get_tg_template_data(self):
         return {
-            'name': 'Syscohada Chart of Accounts for Togo',
+            'name': _('SYSCOHADA for Companies'),
             'parent': 'syscohada',
             'code_digits': '6',
         }
