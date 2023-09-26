b6ef0650dd1afcb946542da77e55406acb126b63
Nicolas Bayet <nby@odoo.com>
2023-09-26 14:05:09 +0200

    [FIX] web_editor: reconnect peers after when reconnecting internet
    Prior to this commit, when the user experienced an internet
    disconnection, after few seconds the PeerToPeer connection state would
    change to "closed" and the PeerToPeer connection would be removed
    permanently. Consequently, when the user reestablished their internet
    connection, the peers would remain disconnected to each other.
    
    With this commit, peers are now re-connecting to each other.
    
    task-3488166

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2473,19 +2473,11 @@ const Wysiwyg = Widget.extend({
         this._isOnline = true;
         if (!this.ptp) return;
 
-        // Ask for potential missing steps from all peers.
-        return Promise.all(this._getPtpClients().map(client => {
-            return this.requestClient(
-                client.id,
-                'get_missing_steps', {
-                    fromStepId: peek(this.odooEditor.historyGetBranchIds()).id,
-                },
-                { transport: 'rtc' }
-            ).then(missingSteps => {
-                if (missingSteps === REQUEST_ERROR) return;
-                this._processMissingSteps(missingSteps);
-            });
-        }));
+        // If it was disconnected to some peers, send the join signal again.
+        this.ptp.notifyAllClients('ptp_join');
+        // Send last step to all peers. If the peers cannot add the step, they
+        // will ask for missing steps.
+        this.ptp.notifyAllClients('oe_history_step', peek(this.odooEditor.historyGetSteps()), { transport: 'rtc' });
     },
     /**
      * Process missing steps received from a peer.
@@ -2619,9 +2611,10 @@ const Wysiwyg = Widget.extend({
                             }
                             this._historySyncFinished = true;
                         } else {
-                            const currentStep = this.odooEditor._historySteps[this.odooEditor._historySteps.length - 1];
+                            // Make both send their last step to each other to
+                            // ensure they are in sync.
+                            this.ptp.notifyAllClients('oe_history_step', peek(this.odooEditor.historyGetSteps()), { transport: 'rtc' });
                             this._setCollaborativeSelection(fromClientId);
-                            this.ptp.notifyClient(fromClientId, 'oe_history_step', currentStep, { transport: 'rtc' });
                         }
 
                         this.requestClient(fromClientId, 'get_client_name', undefined, { transport: 'rtc' }).then((clientName) => {
