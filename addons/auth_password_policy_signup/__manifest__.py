d9349a6bc0bfbfed6630c60ec7d81d52d03f9856
FrancoisGe <fge@odoo.com>
2023-09-26 17:18:14 +0200

    [REF] auth_*: convert PasswordMeter to Public Component
    Addons: auth_password_policy_portal, auth_password_policy_signup
    
    The purpose of this commit is to convert PasswordMeter into a Public Component
    so that the legacy Widget is no longer used.
    
    Part of task 3439226
    
    closes odoo/odoo#136159
    
    Signed-off-by: Samuel Degueldre (sad) <sad@odoo.com>

--- addons/auth_password_policy_signup/__manifest__.py
+++ addons/auth_password_policy_signup/__manifest__.py
@@ -8,7 +8,9 @@
     ],
     'assets': {
         'web.assets_frontend': [
-            'auth_password_policy_signup/static/**/*',
+            'auth_password_policy_signup/static/src/public/**/*',
+            'auth_password_policy/static/src/password_meter.js',
+            'auth_password_policy/static/src/password_policy.js',
         ],
     },
     'license': 'LGPL-3',
