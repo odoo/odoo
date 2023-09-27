dae34c1c990b31162ab010dc100df04a6c7c4f26
Roy Le <roy.le@viindoo.com>
2023-09-27 16:51:58 +0700

    [IMP] stock_account: remove unsed params on generate_properties method

--- addons/stock_account/models/account_chart_template.py
+++ addons/stock_account/models/account_chart_template.py
@@ -1,37 +1,37 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, _
-from odoo.addons.account.models.chart_template import template
+from odoo import api, models, _
+
+import logging
+_logger = logging.getLogger(__name__)
 
 
-class AccountChartTemplate(models.AbstractModel):
+class AccountChartTemplate(models.Model):
     _inherit = "account.chart.template"
 
-    def _post_load_data(self, template_code, company, template_data):
-        super()._post_load_data(template_code, company, template_data)
-        company = company or self.env.company
+    @api.model
+    def generate_journals(self, acc_template_ref, company, journals_dict=None):
+        journal_to_add = [{'name': _('Inventory Valuation'), 'type': 'general', 'code': 'STJ', 'favorite': False, 'sequence': 8}]
+        return super(AccountChartTemplate, self).generate_journals(acc_template_ref=acc_template_ref, company=company, journals_dict=journal_to_add)
+
+    def generate_properties(self, acc_template_ref, company):
+        res = super(AccountChartTemplate, self).generate_properties(acc_template_ref=acc_template_ref, company=company)
+        PropertyObj = self.env['ir.property']  # Property Stock Journal
+        value = self.env['account.journal'].search([('company_id', '=', company.id), ('code', '=', 'STJ'), ('type', '=', 'general')], limit=1)
+        if value:
+            PropertyObj._set_default("property_stock_journal", "product.category", value, company)
+
+        todo_list = [  # Property Stock Accounts
+            'property_stock_account_input_categ_id',
+            'property_stock_account_output_categ_id',
+            'property_stock_valuation_account_id',
+        ]
         categ_values = {category.id: False for category in self.env['product.category'].search([])}
-        for fname in self.env['product.category']._get_stock_account_property_field_names():
-            self.env['ir.property'].with_company(company.id)._set_multi(fname, 'product.category', categ_values, True)
-            value = template_data.get(fname)
-            if value:
-                self.env['ir.property']._set_default(fname, 'product.category', self.ref(value).id, company=company)
+        for field in todo_list:
+            account = self[field]
+            value = acc_template_ref[account].id if account else False
+            PropertyObj._set_default(field, "product.category", value, company)
+            PropertyObj._set_multi(field, "product.category", categ_values, True)
 
-    @template(model='account.journal')
-    def _get_stock_account_journal(self, template_code):
-        return {
-            'inventory_valuation': {
-                'name': _('Inventory Valuation'),
-                'code': 'STJ',
-                'type': 'general',
-                'sequence': 8,
-                'show_on_dashboard': False,
-            },
-        }
-
-    @template()
-    def _get_stock_template_data(self, template_code):
-        return {
-            'property_stock_journal': 'inventory_valuation',
-        }
+        return res
