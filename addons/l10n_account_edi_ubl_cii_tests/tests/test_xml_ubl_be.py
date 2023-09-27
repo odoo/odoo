e1037d5dfe3a5c3f2f52834e9c005c90b35ebe17
Julien Van Roy <juvr@odoo.com>
2023-09-27 11:13:23 +0200

    [FIX] account_edi_ubl_cii: fix peppol fields computes & update BE EAS
    Replace every mentions of 9925 to 0208 for Belgian companies (public
    administration also have this 10 digits number). The EAS 0208
    corresponds to the company_registry.
    
    Introduce a mechanism to validate the `peppol_endpoint` and refactor the
    computes for the `peppol_endpoint` and `peppol_eas` fields.
    
    See: https://openpeppol.atlassian.net/wiki/spaces/Belgium/overview
    
    task-3297311

--- addons/l10n_account_edi_ubl_cii_tests/tests/test_xml_ubl_be.py
+++ addons/l10n_account_edi_ubl_cii_tests/tests/test_xml_ubl_be.py
@@ -233,10 +233,11 @@ class TestUBLBE(TestUBLCommon):
         `peppol_endpoint`).
         """
         # Setup a public admin in Luxembourg
-        self.partner_2.update({
+        self.partner_2.write({
             'vat': None,
-            'peppol_endpoint': '00005000041',
             'peppol_eas': '9938',
+            'peppol_endpoint': '00005000041',
+            'country_id': self.env.ref('base.lu').id,
         })
 
         invoice = self._generate_move(
