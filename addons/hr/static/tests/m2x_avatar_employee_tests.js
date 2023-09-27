7e8d2ce26afcced520e0ae58e31084b8f6af1783
Leonardo Pavan Rocha <lpr@odoo.com>
2023-09-27 13:42:23 +0200

    [IMP] hr*: change employee field widgets to show private/public employee
    In HR modules we have both hr.employee and hr.employee.public models and the
    issue is that if a certain user doesn't have access to read the hr.employee
    record, it will throw errors/avatar pictures won't appear. For this reason,
    https://github.com/odoo/odoo/pull/116828 introduced a change in many2many
    employee fields so that they always show hr.employee.public records. However,
    we can improve this behavior by checking if the user has access to the record.
    If they have access, the private record is shown (making it easier to navigate,
    edit employee information) and if they don't have access, we show the public
    record. In order to allow forcing a relation, it was decided to convert all
    employee fields to accept a relation parameter in the options. If this option
    is set, we use that as the relation.
    
    This commit also changes the HR views using these widgets, removing the
    relation option from fields that would benefit from this dynamic check for the
    most suitable relation.
    
    task-3524305
    
    closes odoo/odoo#136786
    
    Related: odoo/enterprise#48008
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr/static/tests/m2x_avatar_employee_tests.js
+++ addons/hr/static/tests/m2x_avatar_employee_tests.js
@@ -6,6 +6,10 @@ import { start } from "@mail/../tests/helpers/test_utils";
 
 import { dom } from "@web/../tests/legacy/helpers/test_utils";
 import { contains } from "@web/../tests/utils";
+import { registry } from "@web/core/registry";
+import { makeFakeUserService } from "@web/../tests/helpers/mock_services";
+
+const serviceRegistry = registry.category("services");
 
 QUnit.module("M2XAvatarEmployee");
 
@@ -125,6 +129,94 @@ QUnit.test("many2one_avatar_employee widget in kanban view", async function (ass
     );
 });
 
+QUnit.test("many2one_avatar_employee with hr group widget in kanban view", async function (assert) {
+    const pyEnv = await startServer();
+    const partnerId = pyEnv["res.partner"].create({});
+    const userId = pyEnv["res.users"].create({ partner_id: partnerId });
+    const employeeId = pyEnv["hr.employee.public"].create({
+        user_id: userId,
+        user_partner_id: partnerId,
+    });
+    pyEnv["m2x.avatar.employee"].create({
+        employee_id: employeeId,
+        employee_ids: [employeeId],
+    });
+
+    serviceRegistry.add(
+        "user",
+        makeFakeUserService(() => true),
+        { force: true }
+    );
+
+    const views = {
+        "m2x.avatar.employee,false,kanban": `<kanban>
+                <templates>
+                    <t t-name="kanban-box">
+                        <div>
+                            <field name="employee_id" widget="many2one_avatar_employee"/>
+                        </div>
+                    </t>
+                </templates>
+            </kanban>`,
+    };
+
+    const { openView } = await start({ serverData: { views } });
+    await openView({
+        res_model: "m2x.avatar.employee",
+        views: [[false, "kanban"]],
+    });
+    assert.strictEqual(document.querySelector(".o_kanban_record").innerText.trim(), "");
+    await contains(".o_m2o_avatar");
+    assert.strictEqual(
+        document.querySelector(".o_m2o_avatar > img").getAttribute("data-src"),
+        `/web/image/hr.employee/${employeeId}/avatar_128`
+    );
+});
+
+QUnit.test("many2one_avatar_employee with relation set in options", async function (assert) {
+    const pyEnv = await startServer();
+    const partnerId = pyEnv["res.partner"].create({});
+    const userId = pyEnv["res.users"].create({ partner_id: partnerId });
+    const employeeId = pyEnv["hr.employee.public"].create({
+        user_id: userId,
+        user_partner_id: partnerId,
+    });
+    pyEnv["m2x.avatar.employee"].create({
+        employee_id: employeeId,
+        employee_ids: [employeeId],
+    });
+
+    serviceRegistry.add(
+        "user",
+        makeFakeUserService(() => true),
+        { force: true }
+    );
+
+    const views = {
+        "m2x.avatar.employee,false,kanban": `<kanban>
+                <templates>
+                    <t t-name="kanban-box">
+                        <div>
+                            <field name="employee_id" widget="many2one_avatar_employee" options="{'relation': 'hr.employee.public'}"/>
+                        </div>
+                    </t>
+                </templates>
+            </kanban>`,
+    };
+
+    const { openView } = await start({ serverData: { views } });
+    await openView({
+        res_model: "m2x.avatar.employee",
+        views: [[false, "kanban"]],
+    });
+    assert.strictEqual(document.querySelector(".o_kanban_record").innerText.trim(), "");
+    await contains(".o_m2o_avatar");
+    assert.strictEqual(
+        document.querySelector(".o_m2o_avatar > img").getAttribute("data-src"),
+        `/web/image/hr.employee.public/${employeeId}/avatar_128`
+    );
+});
+
 QUnit.test(
     "many2one_avatar_employee: click on an employee not associated with a user",
     async function (assert) {
@@ -217,6 +309,70 @@ QUnit.test("many2many_avatar_employee widget in form view", async function (asse
         document.querySelectorAll(".o_field_many2many_avatar_employee .o_tag .o_m2m_avatar")[1]
     );
     // TODO: avatar card employee
+    assert.verifySteps([`web_read m2x.avatar.employee ${avatarId_1}`]);
+});
+
+QUnit.test("many2many_avatar_employee with hr group widget in form view", async function (assert) {
+    const pyEnv = await startServer();
+    const [partnerId_1, partnerId_2] = pyEnv["res.partner"].create([{}, {}]);
+    const [userId_1, userId_2] = pyEnv["res.users"].create([
+        { partner_id: partnerId_1 },
+        { partner_id: partnerId_2 },
+    ]);
+    const [employeeId_1, employeeId_2] = pyEnv["hr.employee.public"].create([
+        { user_id: userId_1, user_partner_id: partnerId_1 },
+        { user_id: userId_2, user_partner_id: partnerId_2 },
+    ]);
+    const avatarId_1 = pyEnv["m2x.avatar.employee"].create({
+        employee_ids: [employeeId_1, employeeId_2],
+    });
+    const views = {
+        "m2x.avatar.employee,false,form":
+            '<form><field name="employee_ids" widget="many2many_avatar_employee"/></form>',
+    };
+
+    // if the user doesn't have access to hr.group_hr_user, the employee field should show the public employee
+    serviceRegistry.add(
+        "user",
+        makeFakeUserService(() => true),
+        { force: true }
+    );
+
+    const { openView } = await start({
+        mockRPC(route, args) {
+            if (args.method === "web_read") {
+                assert.step(`web_read ${args.model} ${args.args[0]}`);
+            }
+            if (args.method === "read") {
+                assert.step(`read ${args.model} ${args.args[0]}`);
+            }
+        },
+        serverData: { views },
+    });
+    await openView({
+        res_model: "m2x.avatar.employee",
+        res_id: avatarId_1,
+        views: [[false, "form"]],
+    });
+    assert.containsN(
+        document.body,
+        ".o_field_many2many_avatar_employee .o_tag",
+        2,
+        "should have 2 records"
+    );
+    assert.strictEqual(
+        document
+            .querySelector(".o_field_many2many_avatar_employee .o_tag img")
+            .getAttribute("data-src"),
+        `/web/image/hr.employee/${employeeId_1}/avatar_128`
+    );
+
+    await dom.click(
+        document.querySelector(".o_field_many2many_avatar_employee .o_tag .o_m2m_avatar")
+    );
+    await dom.click(
+        document.querySelectorAll(".o_field_many2many_avatar_employee .o_tag .o_m2m_avatar")[1]
+    );
     assert.verifySteps([`web_read m2x.avatar.employee ${avatarId_1}`]);
 });
 
