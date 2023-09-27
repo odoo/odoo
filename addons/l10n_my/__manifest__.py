c0369a88ff9af021ae8efc571d28b080b558b7be
Nicolas Viseur (vin) <vin@odoo.com>
2023-09-27 14:10:02 +0800

    [IMP] l10n_my: add tax report
    Adds the SST-02 tax report to the Malaysian
    localisation
    
    Task id # 2868179

--- addons/l10n_my/__manifest__.py
+++ addons/l10n_my/__manifest__.py
@@ -5,7 +5,7 @@
     'icon': '/account/static/description/l10n.png',
     'countries': ['my'],
     'author': 'Odoo PS',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
 This is the base module to manage the accounting chart for Malaysia in Odoo.
@@ -13,6 +13,9 @@ This is the base module to manage the accounting chart for Malaysia in Odoo.
     """,
     'depends': [
         'account',
+    ],
+    'data': [
+        'data/account_tax_report_data.xml',
     ],
     'demo': [
         'demo/demo_company.xml',
