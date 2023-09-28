9cc81bc3650d48ec1e77fc488ca6fee61535a363
Pierre-Yves Dufays <pydu@odoo.com>
2023-09-28 16:36:58 +0200

    [IMP] hr_contract: improve default planned due date
    Before the first contract date of the first selected element was chosen to
    determine the planned due date if all first contract date were different
    otherwise the minimum was chosen. So if the selection included 2 different date
    among 3, the minimum was chosen but the first if the 3 were different.
    With this change, the minimum is always used to determine the default planned
    due date.
    
    Task-3390865

--- addons/hr_contract/wizard/mail_activity_schedule.py
+++ addons/hr_contract/wizard/mail_activity_schedule.py
@@ -8,25 +8,18 @@ from dateutil.relativedelta import relativedelta
 class MailActivitySchedule(models.TransientModel):
     _inherit = 'mail.activity.schedule'
 
-    @api.model
-    def default_get(self, field_list):
-        result = super().default_get(field_list)
-        model = self._get_default_res_model()
-        res_ids = self._get_default_res_ids()
-        if not ('date_plan_deadline' in field_list and model == 'hr.employee' and res_ids):
-            return result
-        selected_employees = self.env['hr.employee'].browse(self._get_converted_res_ids(res_ids))
-        start_dates = selected_employees.filtered('first_contract_date').mapped('first_contract_date')
-        if start_dates:
-            today = fields.Date.today()
-            planned_due_date = start_dates[0] if len(set(start_dates)) == len(selected_employees) else min(start_dates)
+    @api.depends('res_model_id', 'res_ids')
+    def _compute_date_plan_deadline(self):
+        for wizard in self:
+            if wizard.res_model != 'hr.employee':
+                continue
+            selected_employees = wizard._get_applied_on_records()
+            start_dates = selected_employees.filtered('first_contract_date').mapped('first_contract_date')
+            if start_dates:
+                today = fields.Date.today()
+                planned_due_date = min(start_dates)
 
-            if planned_due_date < today:
-                result['date_plan_deadline'] = today + relativedelta(days=+30)
-            else:
-                if (planned_due_date-today).days < 30:
-                    result['date_plan_deadline'] = today + relativedelta(days=+30)
+                if planned_due_date < today or (planned_due_date - today).days < 30:
+                    wizard.date_plan_deadline = today + relativedelta(days=+30)
                 else:
-                    result['date_plan_deadline'] = planned_due_date
-
-        return result
+                    wizard.date_plan_deadline = planned_due_date
