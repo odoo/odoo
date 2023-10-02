7875ff51cd3c7061f82360e8d9580d948bc09a07
Joren Van Onder <jov@odoo.com>
2023-10-02 16:41:05 -0700

    [IMP] l10n_br: update manifest description
    Update the description of the Brazilian localization to reflect the
    most recent changes that happened here. Also fix some wrong formatting
    that made some parts render as quoted text.
    
    closes odoo/odoo#135959
    
    Signed-off-by: Josse Colpaert <jco@odoo.com>
    Co-authored-by: Rubi Navarro (ren) <ren@odoo.com>

--- addons/l10n_br/__manifest__.py
+++ addons/l10n_br/__manifest__.py
@@ -11,36 +11,43 @@ Base module for the Brazilian localization
 
 This module consists of:
 
- - Generic Brazilian chart of accounts
- - Brazilian taxes such as:
+- Generic Brazilian chart of accounts
+- Brazilian taxes such as:
 
-        - IPI
-        - ICMS
-        - PIS
-        - COFINS
-        - ISS
-        - IR
-        - IRPJ
-        - CSLL
+  - IPI
+  - ICMS
+  - PIS
+  - COFINS
+  - ISS
+  - IR
+  - CSLL
+
+- Document Types as NFC-e, NFS-e, etc.
+- Identification Documents as CNPJ and CPF
 
 The field tax_discount has also been added in the account.tax.template and
 account.tax objects to allow the proper computation of some Brazilian VATs
 such as ICMS. The chart of account creation wizard has been extended to
 propagate those new data properly.
 
-It's important to note however that this module lack many implementations to
-use Odoo properly in Brazil. Those implementations (such as the electronic
-fiscal Invoicing which is already operational) are brought by more than 15
-additional modules of the Brazilian Launchpad localization project
-https://launchpad.net/openerp.pt-br-localiz and their dependencies in the
-extra addons branch. Those modules aim at not breaking with the remarkable
-Odoo modularity, this is why they are numerous but small. One of the
-reasons for maintaining those modules apart is that Brazilian Localization
-leaders need commit rights agility to complete the localization as companies
-fund the remaining legal requirements (such as soon fiscal ledgers,
-accounting SPED, fiscal SPED and PAF ECF that are still missing as September
-2011). Those modules are also strictly licensed under AGPL V3 and today don't
-come with any additional paid permission for online use of 'private modules'.
+In addition to this module, the Brazilian Localizations is also
+extended and complemented with several additional modules.
+
+Brazil - Accounting Reports (l10n_br_reports)
+---------------------------------------------
+Adds a simple tax report that helps check the tax amount per tax group
+in a given period of time. Also adds the P&L and BS adapted for the
+Brazilian market.
+
+Avatax Brazil (l10n_br_avatax)
+------------------------------
+Add Brazilian tax calculation via Avatax and all necessary fields needed to
+configure Odoo in order to properly use Avatax and send the needed fiscal
+information to retrieve the correct taxes.
+
+Avatax for SOs in Brazil (l10n_br_avatax_sale)
+----------------------------------------------
+Same as the l10n_br_avatax module with the extension to the sales order module.
 """,
     'author': 'Akretion, Odoo Brasil',
     'depends': [
