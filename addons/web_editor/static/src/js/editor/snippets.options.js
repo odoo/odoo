2bb4c364b038ff81aed3541d895aa44ef795152c
Louis (loco) <loco@odoo.com>
2023-10-02 10:03:16 +0200

    [FIX] web_editor, *: store the correct mimetype of an image with a shape
    *: tools
    
    Steps to reproduce the bug:
    - Add an image on the website.
    - Replace it by a "jpeg". Note that the mimetype of the image is
    "image/webp" at the upload since [1].
    - Add a shape on the image.
    - Save and Edit.
    
    -> If you check on the available "Format", the mimetype of the
    "original" is "webp" but it should be "jpeg".
    
    Before this commit, there were two types of mimetype data attribute:
    - `mimetype`: the current mimetype of the image.
    - `originalMimetype`: the mimetype of the image without a shape.
    Before [1], it was also the mimetype of the original image. However,
    since [1], the user has the possibility to change the mimetype of the
    image so the "originalMimetype" attribute does not always refer to the
    mimetype of the original image anymore.
    
    To resolve the problem, another data attribute has to be introduced.
    Here is a summary of the mimetype related attribute:
    - `mimetype`: the current mimetype of an image.
    - `originalMimetype`: the mimetype of the image before a shape has
    been applied. It is needed when removing a shape to recover the correct
    mimetype.
    - `mimetypeBeforeConversion`: the mimetype of the original image. It is
    needed in order to be able to change the format of an image and come
    back to the original one.
    
    In the case of an uploaded "jpeg" image on which a shape has been
    applied, `mimetypeBeforeConversion` is "image/jpeg", `originalMimetype`
    is "image/webp" (since [1]) and `mimetype` is "image/svg+xml".
    
    The `loadImageInfo()` has been adapted to also handle the case of an
    image that has already been loaded but that does not have the
    `mimetypeBeforeConversion` attribute (for example all the images that
    were uploaded on the website before this commit). In this case, the
    mimetype attribute is kept and not set to the original one as the user
    could have changed it.
    
    [1]: https://github.com/odoo/odoo/commit/0449fe85cb0e1d639a4e1aeba26e90906f79254d
    
    task-3449866

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -5876,9 +5876,9 @@ const ImageHandlerOption = SnippetOptionWidget.extend({
         };
         widths[img.naturalWidth] = [sprintf(_t("%spx"), img.naturalWidth), 'image/webp'];
         widths[optimizedWidth] = [sprintf(_t("%spx (Suggested)"), optimizedWidth), 'image/webp'];
-        const imgMimetype = this._getImageMimetype(img);
-        widths[maxWidth] = [sprintf(_t("%spx (Original)"), maxWidth), imgMimetype];
-        if (imgMimetype !== 'image/webp') {
+        const mimetypeBeforeConversion = img.dataset.mimetypeBeforeConversion;
+        widths[maxWidth] = [sprintf(_t("%spx (Original)"), maxWidth), mimetypeBeforeConversion];
+        if (mimetypeBeforeConversion !== "image/webp") {
             // Avoid a key collision by subtracting 0.1 - putting the webp
             // above the original format one of the same size.
             widths[maxWidth - 0.1] = [sprintf(_t("%spx"), maxWidth), 'image/webp'];
