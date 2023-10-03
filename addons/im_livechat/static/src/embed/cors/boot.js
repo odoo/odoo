e01568b73d708eebed2d318da095a0c086058ffb
tsm-odoo <tsm@odoo.com>
2023-10-03 13:25:06 +0200

    [IMP] im_livechat, mail: allow CORS livechat actions
    This PR makes several actions available for the embed livechat
    such as:
    - calls
    - message edition
    - message reactions
    - link previews
    
    task-3523930

---
+++ addons/im_livechat/static/src/embed/cors/boot.js
@@ -0,0 +1,45 @@
+/* @odoo-module */
+
+import { livechatRoutingMap } from "@im_livechat/embed/cors/livechat_routing_map";
+
+import { browser } from "@web/core/browser/browser";
+import { jsonrpc } from "@web/core/network/rpc_service";
+import { registry } from "@web/core/registry";
+import { session } from "@web/session";
+
+(async function boot() {
+    const { fetch } = browser;
+    browser.fetch = function (url, ...args) {
+        if (!url.match(/^(?:https?:)?\/\//)) {
+            url = session.origin + url;
+        }
+        return fetch(url, ...args);
+    };
+    // Override the rpc service to forward requests to CORS-allowed routes. The
+    // "guest_token" will be appended to the request parameters for
+    // authentication.
+    registry.category("services").add(
+        "rpc",
+        {
+            async: true,
+            start(env) {
+                return function rpc(route, params = {}, settings) {
+                    if (route in livechatRoutingMap.content) {
+                        route = livechatRoutingMap.get(route, route);
+                        if (env.services["im_livechat.livechat"]?.guestToken) {
+                            params = {
+                                ...params,
+                                guest_token: env.services["im_livechat.livechat"].guestToken,
+                            };
+                        }
+                    }
+                    if (!route.match(/^(?:https?:)?\/\//)) {
+                        route = session.origin + route;
+                    }
+                    return jsonrpc(route, params, { bus: env.bus, ...settings });
+                };
+            },
+        },
+        { force: true }
+    );
+})();
