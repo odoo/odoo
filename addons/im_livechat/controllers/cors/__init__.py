bd4e418fc11b8d4f4ba93161b974d7fb7e418458
tsm-odoo <tsm@odoo.com>
2023-10-03 13:25:06 +0200

    [IMP] im_livechat, mail: allow CORS livechat actions
    This PR makes several actions available for the embed livechat
    such as:
    - calls
    - message edition
    - message reactions
    - link previews
    
    task-3523930
    
    closes odoo/odoo#136618
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/im_livechat/controllers/cors/__init__.py
+++ addons/im_livechat/controllers/cors/__init__.py
@@ -1,7 +1,9 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import chatbot
+from . import channel
+from . import link_preview
 from . import main
+from . import message_reaction
+from . import rtc
+from . import thread
 from . import webclient
-from . import cors
