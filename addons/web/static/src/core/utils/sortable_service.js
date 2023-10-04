a6f070e2e9910301c3a6aca1100e4cecbcd8f570
Romeo Fragomeli <rfr@odoo.com>
2023-10-04 09:05:49 +0200

    [IMP] web,web_editor,website_slide: sortable service for frontend usage
    This commit removes the module sortable from jQueryUI and adds instead a
    sortable service to use the sortable code already used by the backend.
    
    The service can be called using `this.call("sortable", ...)` and the
    sortable listeners are added when the `enable` function is called.
    This function (`enable()`) returns an object that contains the `cleanup`
    function (`cleanup()`).
    
    ```javascript
    // start / setup
    const sortable = this.call(
        "sortable",
        "create",
        {
            ref: { el: document.querySelector(".list") },
            elements: ".items",
            handle: ".drag-icon",
        },
    );
    
    // on attach callback / on mounted
    const sortableInstance = sortable.enable();
    
    // on destroy / on willUnmount
    sortableInstance.cleanup();
    ```
    
    Also, it adds some new property to the useSortable:
    * clone: the placeholder is a clone of the drag element
    * placeholderClasses: classes added to the placeholder element
    * applyChangeOnDrop: on drop the change is applied to the DOM
    * followingElementClasses: classes added to the element following mouse
    
    task-3439226

---
+++ addons/web/static/src/core/utils/sortable_service.js
@@ -0,0 +1,98 @@
+/** @odoo-module **/
+
+import { registry } from "../registry";
+import { useSortable } from "@web/core/utils/sortable";
+import { throttleForAnimation } from "@web/core/utils/timing";
+import { reactive } from "@odoo/owl";
+
+/**
+ * @typedef SortableServiceHookParams
+ * @extends SortableParams
+ * @property {{el: HTMLElement} | ReturnType<typeof import("@odoo/owl").useRef>} [ref] container of sortable
+ * @property {string | Symbol} [sortableId] identifier when multiple sortable on the same container
+ */
+
+const DEFAULT_SORTABLE_ID = Symbol.for("defaultSortable");
+export const sortableService = {
+    start() {
+        /**
+         * Map to avoid to setup/enable twice or more time the same element
+         * @type {Map<Element, Object>}
+         */
+        const boundElements = new Map();
+        return {
+            /**
+             * @param {SortableServiceHookParams} hookParams
+             */
+            create: (hookParams) => {
+                const element = hookParams.ref.el;
+                const sortableId = hookParams.sortableId ?? DEFAULT_SORTABLE_ID;
+                if (boundElements.has(element)) {
+                    const boundElement = boundElements.get(element);
+                    if (sortableId in boundElement) {
+                        return {
+                            enable() {
+                                return {
+                                    cleanup: boundElement[sortableId],
+                                };
+                            },
+                        };
+                    }
+                }
+                /**
+                 * @type {Map<Function, function():Array>}
+                 */
+                const setupFunctions = new Map();
+                /**
+                 * @type {Array<Function>}
+                 */
+                const cleanupFunctions = [];
+
+                const cleanup = () => {
+                    const boundElement = boundElements.get(element);
+                    if (sortableId in boundElement) {
+                        delete boundElement[sortableId];
+                        if (boundElement.length === 0) {
+                            boundElements.delete(element);
+                        }
+                    }
+                    cleanupFunctions.forEach((fn) => fn());
+                };
+
+                // Setup hookParam
+                const setupHooks = {
+                    wrapState: reactive,
+                    throttle: throttleForAnimation,
+                    addListener: (el, type, listener) => {
+                        el.addEventListener(type, listener);
+                        cleanupFunctions.push(() => el.removeEventListener(type, listener));
+                    },
+                    setup: (setupFn, dependenciesFn) => setupFunctions.set(setupFn, dependenciesFn),
+                    teardown: (fn) => cleanupFunctions.push(fn),
+                };
+
+                useSortable({ setupHooks, ...hookParams });
+
+                const boundElement = boundElements.get(element);
+                if (boundElement) {
+                    boundElement[sortableId] = cleanup;
+                } else {
+                    boundElements.set(element, { [sortableId]: cleanup });
+                }
+
+                return {
+                    enable() {
+                        setupFunctions.forEach((dependenciesFn, setupFn) =>
+                            setupFn(...dependenciesFn())
+                        );
+                        return {
+                            cleanup,
+                        };
+                    },
+                };
+            },
+        };
+    },
+};
+
+registry.category("services").add("sortable", sortableService);
