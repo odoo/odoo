odoo/odoo

commit 8f96e08985ebac3309745f1036a5e83d287e6382

[FIX] purchase_requisition: fix multicurrency alternative
Steps to reproduce:

- Activate 2 currencies (assume EUR and USD, conversion rate: 1 EUR = 0.9 USD)
- Create new product P
	- Purchase tab, add two vendors
	- V_USD, currency = USD, price = 100
	- V_EUR, currency = EUR, price = 101
- Create PO, vendor = V_USD, Currency = USD
- Add P, Unit price should be 100
- Alternative tab > Create alternative > Vendor = V_EUR
- Compare product lines

Issue:

The line with price = 100 USD is highlighted as being the cheapest option, but if we apply conversion rules,
101 EUR = 90.9 USD < 100 USD.

Solution:

Convert prices in company currency before comparing.

opw-3378253

addons/purchase_requisition/models/purchase.py
@@ -215,22 +215,45 @@ class PurchaseOrder(models.Model):
         product_to_best_price_line = defaultdict(lambda: self.env['purchase.order.line'])
         product_to_best_date_line = defaultdict(lambda: self.env['purchase.order.line'])
         product_to_best_price_unit = defaultdict(lambda: self.env['purchase.order.line'])
-        order_lines = self.order_line | self.alternative_po_ids.order_line
-        for line in order_lines:
+        po_alternatives = self | self.alternative_po_ids
+
+        multiple_currencies = False
+        if len(po_alternatives.currency_id) > 1:
+            multiple_currencies = True
+
+        for line in po_alternatives.order_line:
             if not line.product_qty or not line.price_subtotal or line.state in ['cancel', 'purchase', 'done']:
                 continue
-            if not product_to_best_price_line[line.product_id] or product_to_best_price_line[line.product_id][0].price_subtotal > line.price_subtotal:
+
+            # if no best price line => no best price unit line either
+            if not product_to_best_price_line[line.product_id]:
                 product_to_best_price_line[line.product_id] = line
-            elif product_to_best_price_line[line.product_id][0].price_subtotal == line.price_subtotal:
-                product_to_best_price_line[line.product_id] |= line
+                product_to_best_price_unit[line.product_id] = line
+            else:
+                price_subtotal = line.price_subtotal
+                price_unit = line.price_unit
+                current_price_subtotal = product_to_best_price_line[line.product_id][0].price_subtotal
+                current_price_unit = product_to_best_price_unit[line.product_id][0].price_unit
+                if multiple_currencies:
+                    price_subtotal *= line.order_id.currency_rate
+                    price_unit *= line.order_id.currency_rate
+                    current_price_subtotal *= product_to_best_price_line[line.product_id][0].order_id.currency_rate
+                    current_price_unit *= product_to_best_price_unit[line.product_id][0].order_id.currency_rate
+
+                if current_price_subtotal > price_subtotal:
+                    product_to_best_price_line[line.product_id] = line
+                elif current_price_subtotal == price_subtotal:
+                    product_to_best_price_line[line.product_id] |= line
+                if current_price_unit > price_unit:
+                    product_to_best_price_unit[line.product_id] = line
+                elif current_price_unit == price_unit:
+                    product_to_best_price_unit[line.product_id] |= line
+
             if not product_to_best_date_line[line.product_id] or product_to_best_date_line[line.product_id][0].date_planned > line.date_planned:
                 product_to_best_date_line[line.product_id] = line
             elif product_to_best_date_line[line.product_id][0].date_planned == line.date_planned:
                 product_to_best_date_line[line.product_id] |= line
-            if not product_to_best_price_unit[line.product_id] or product_to_best_price_unit[line.product_id][0].price_unit > line.price_unit:
-                product_to_best_price_unit[line.product_id] = line
-            elif product_to_best_price_unit[line.product_id][0].price_unit == line.price_unit:
-                product_to_best_price_unit[line.product_id] |= line
+
         best_price_ids = set()
         best_date_ids = set()
         best_price_unit_ids = set()

addons/purchase_requisition/tests/test_purchase_requisition.py
@@ -348,3 +348,58 @@ class TestPurchaseRequisition(TestPurchaseRequisitionCommon):
 
         po_2 = po_1.alternative_po_ids - po_1
         self.assertFalse(po_2.requisition_id, "The requisition_id should not be set in the alternative purchase order")
+
+    def test_12_alternative_po_line_different_currency(self):
+        """ Check alternative PO with different currency is compared correctly"""
+
+        currency_eur = self.env.ref("base.EUR")
+        currency_usd = self.env.ref("base.USD")
+
+        # 1 USD = 0.5 EUR
+        self.env['res.currency.rate'].create([{
+            'currency_id': self.env.ref('base.USD').id,
+            'rate': 1,
+        }, {
+            'currency_id': self.env.ref('base.EUR').id,
+            'rate': 0.5,
+        }])
+
+        po_form = Form(self.env['purchase.order'])
+        po_form.partner_id = self.res_partner_1
+        po_form.currency_id = currency_usd
+        with po_form.order_line.new() as line:
+            line.product_id = self.product_09
+            line.product_qty = 1
+            line.price_unit = 10
+        po_orig = po_form.save()
+
+        # Creates an alternative PO
+        action = po_orig.action_create_alternative()
+        alt_po_wizard_form = Form(self.env['purchase.requisition.create.alternative'].with_context(**action['context']))
+        alt_po_wizard_form.partner_id = self.res_partner_1
+        alt_po_wizard_form.copy_products = True
+        alt_po_wizard = alt_po_wizard_form.save()
+        alt_po_wizard.action_create_alternative()
+
+        po_alt = po_orig.alternative_po_ids - po_orig
+        po_alt.currency_id = currency_eur
+        po_alt.order_line.price_unit = 12
+        # po_alt has cheaper price_unit/price_subtotal after conversion USD -> EUR
+        # 12 USD = 12 * 0.5 = 6 EUR < 10 EUR
+
+        best_price_ids, best_date_ids, best_price_unit_ids = po_orig.get_tender_best_lines()
+        self.assertEqual(len(best_price_ids), 1)
+        # Equal dates
+        self.assertEqual(len(best_date_ids), 2)
+        self.assertEqual(len(best_price_unit_ids), 1)
+        # alt_po is cheaper than orig_po
+        self.assertEqual(best_price_ids[0], po_alt.order_line.id)
+        self.assertEqual(best_price_unit_ids[0], po_alt.order_line.id)
+
+        po_alt.order_line.price_unit = 20
+        # po_alt has same price_unit/price_subtotal after conversion USD -> EUR
+        # 20 USD = 20 * 0.5 = 10 EUR
+        best_price_ids, best_date_ids, best_price_unit_ids = po_orig.get_tender_best_lines()
+        self.assertEqual(len(best_price_ids), 2)
+        self.assertEqual(len(best_date_ids), 2)
+        self.assertEqual(len(best_price_unit_ids), 2)

commit ee5846212763efbbde28e8c8fadfabfa57b423ef

[FIX] purchase_requisition: convert purchase price to current currency
Bug introduced in:
https://github.com/odoo/odoo/pull/128327/commits/8f96e08985ebac3309745f1036a5e83d287e6382

Steps to reproduce the bug:
- Activate 2 currencies (assume EUR and USD, conversion
   rate: 0.65 EUR = 1 USD)
- Create new product “P1”:
    - Purchase tab:
        - add two vendors:
- Vendor_USD, currency = USD, price = 100
- Vendor_EUR, currency = EUR, price = 80
 - Create PO:
    - vendor = vendor_EUR
    - Currency = Eur
    - Add P1, Unit price should be 80

- Alternative tab > Create alternative > Vendor = Vendor_EUR
- Compare product lines Issue: The line with price = 100 USD is
 highlighted as being the cheapest option, but if we apply conversion
 rules, 80 EUR = 123,07 USD > 100 USD.

Solution:
Before selecting the cheapest line, we convert the prices of the
purchase order lines that are in a currency other than the current
currency of the company.

opw-3378253

addons/purchase_requisition/models/purchase.py
@@ -235,10 +235,10 @@ class PurchaseOrder(models.Model):
                 current_price_subtotal = product_to_best_price_line[line.product_id][0].price_subtotal
                 current_price_unit = product_to_best_price_unit[line.product_id][0].price_unit
                 if multiple_currencies:
-                    price_subtotal *= line.order_id.currency_rate
-                    price_unit *= line.order_id.currency_rate
-                    current_price_subtotal *= product_to_best_price_line[line.product_id][0].order_id.currency_rate
-                    current_price_unit *= product_to_best_price_unit[line.product_id][0].order_id.currency_rate
+                    price_subtotal /= line.order_id.currency_rate
+                    price_unit /= line.order_id.currency_rate
+                    current_price_subtotal /= product_to_best_price_line[line.product_id][0].order_id.currency_rate
+                    current_price_unit /= product_to_best_price_unit[line.product_id][0].order_id.currency_rate
 
                 if current_price_subtotal > price_subtotal:
                     product_to_best_price_line[line.product_id] = line

addons/purchase_requisition/tests/test_purchase_requisition.py
@@ -7,7 +7,10 @@ from odoo.tests import Form
 
 from datetime import timedelta
 
+from odoo.tests.common import tagged
 
+
+@tagged('post_install', '-at_install')
 class TestPurchaseRequisition(TestPurchaseRequisitionCommon):
 
     def test_00_purchase_requisition_users(self):
@@ -376,30 +379,51 @@ class TestPurchaseRequisition(TestPurchaseRequisitionCommon):
             'currency_id': self.env.ref('base.EUR').id,
             'rate': 0.5,
         }])
+        vendor_usd = self.env["res.partner"].create({
+            "name": "Supplier A",
+        })
+        vendor_eur = self.env["res.partner"].create({
+            "name": "Supplier B",
+        })
 
+        product = self.env['product.product'].create({
+            'name': 'Product',
+            'seller_ids': [(0, 0, {
+                'partner_id': vendor_usd.id,
+                'price': 100,
+                'currency_id': currency_usd.id,
+            }), (0, 0, {
+                'partner_id': vendor_eur.id,
+                'price': 80,
+                'currency_id': currency_eur.id,
+            })]
+        })
         po_form = Form(self.env['purchase.order'])
-        po_form.partner_id = self.res_partner_1
-        po_form.currency_id = currency_usd
+        po_form.partner_id = vendor_eur
+        po_form.currency_id = currency_eur
         with po_form.order_line.new() as line:
-            line.product_id = self.product_09
+            line.product_id = product
             line.product_qty = 1
-            line.price_unit = 10
         po_orig = po_form.save()
+        self.assertEqual(po_orig.order_line.price_unit, 80)
+        self.assertEqual(po_orig.currency_id, currency_eur)
 
         # Creates an alternative PO
         action = po_orig.action_create_alternative()
         alt_po_wizard_form = Form(self.env['purchase.requisition.create.alternative'].with_context(**action['context']))
-        alt_po_wizard_form.partner_id = self.res_partner_1
+        alt_po_wizard_form.partner_id = vendor_usd
         alt_po_wizard_form.copy_products = True
         alt_po_wizard = alt_po_wizard_form.save()
         alt_po_wizard.action_create_alternative()
 
         po_alt = po_orig.alternative_po_ids - po_orig
-        po_alt.currency_id = currency_eur
-        po_alt.order_line.price_unit = 12
-        # po_alt has cheaper price_unit/price_subtotal after conversion USD -> EUR
-        # 12 USD = 12 * 0.5 = 6 EUR < 10 EUR
+        # Ensure that the currency in the alternative purchase order is set to USD
+        # because, in some case, the company's default currency is EUR.
+        self.assertEqual(po_alt.currency_id, currency_usd)
+        self.assertEqual(po_alt.order_line.price_unit, 100)
 
+        # po_alt has cheaper price_unit/price_subtotal after conversion USD -> EUR
+        # 80 / 0.5 = 160 USD > 100 EUR
         best_price_ids, best_date_ids, best_price_unit_ids = po_orig.get_tender_best_lines()
         self.assertEqual(len(best_price_ids), 1)
         # Equal dates
@@ -408,11 +432,3 @@ class TestPurchaseRequisition(TestPurchaseRequisitionCommon):
         # alt_po is cheaper than orig_po
         self.assertEqual(best_price_ids[0], po_alt.order_line.id)
         self.assertEqual(best_price_unit_ids[0], po_alt.order_line.id)
-
-        po_alt.order_line.price_unit = 20
-        # po_alt has same price_unit/price_subtotal after conversion USD -> EUR
-        # 20 USD = 20 * 0.5 = 10 EUR
-        best_price_ids, best_date_ids, best_price_unit_ids = po_orig.get_tender_best_lines()
-        self.assertEqual(len(best_price_ids), 2)
-        self.assertEqual(len(best_date_ids), 2)
-        self.assertEqual(len(best_price_unit_ids), 2)