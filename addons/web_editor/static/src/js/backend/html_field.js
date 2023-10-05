52895ddf053b4ffb6cba6bbddf691dd762e52cb8
Sébastien Geelen (sge) <sge@odoo.com>
2023-10-05 15:37:02 +0200

    [FIX] web_editor: commit changes in onWillUnmount
    In some cases the changes done in the editor were lost when using other element of the the UI.
    For example : changing the tab containing the editor component to another one.
    
    To avoid loosing the changes made by the user in those cases, we force an urgent commit change
    in the `onWillUnmount` hook.
    
    task-3530998
    
    closes odoo/odoo#137716
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/backend/html_field.js
+++ addons/web_editor/static/src/js/backend/html_field.js
@@ -156,6 +156,13 @@ export class HtmlField extends Component {
             })();
         });
         onWillUnmount(() => {
+            if (!this.props.readonly && this._isDirty()) {
+                // If we still have uncommited changes, commit them with the
+                // urgent flag to avoid losing them. Urgent flag is used to be
+                // able to save the changes before the component is destroyed
+                // by the owl component manager.
+                this.commitChanges({ urgent: true });
+            }
             if (this._qwebPlugin) {
                 this._qwebPlugin.destroy();
             }
@@ -373,18 +380,16 @@ export class HtmlField extends Component {
     async commitChanges({ urgent } = {}) {
         if (this._isDirty() || urgent) {
             let saveModifiedImagesPromise, toInlinePromise;
-            if (this.wysiwyg) {
+            if (this.wysiwyg && this.wysiwyg.odooEditor) {
                 this.wysiwyg.odooEditor.observerUnactive('commitChanges');
                 saveModifiedImagesPromise = this.wysiwyg.saveModifiedImages();
                 if (this.props.isInlineStyle) {
                     // Avoid listening to changes made during the _toInline process.
                     toInlinePromise = this._toInline();
                 }
-            }
-            if (urgent) {
-                await this.updateValue();
-            }
-            if (this.wysiwyg) {
+                if (urgent) {
+                    await this.updateValue();
+                }
                 await saveModifiedImagesPromise;
                 if (this.props.isInlineStyle) {
                     await toInlinePromise;
