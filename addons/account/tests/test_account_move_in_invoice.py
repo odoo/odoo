odoo/odoo

commit 95c585f67bd37e1202c57b3e9eed09b0a964472c

[FIX] account: allow sending invoice to partner other than the customer
## Bug

When sending an invoice to a recipient who is not the customer, they are
unable to view the invoice in the customer portal and are prompted to
log in.

## Steps to reproduce

1. Create and validate an invoice
2. Click on the Send & Print button, then add a recipient who is not the
   customer associated with the invoice.
3. Proceed to send the invoice.
4. Access the email that was sent to the added recipient (who is not the
   customer)
5. Using an incognito or private browsing window, open the link
   `View Invoice`

=> you should see that you are asked to log in, instead of being
directed to the customer portal.

opw-3114579

closes odoo/odoo#118766

Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

addons/account/models/account_move.py
@@ -3217,8 +3217,11 @@ class AccountMove(models.Model):
         self.ensure_one()
         if self.move_type != 'entry':
             for group_name, _group_method, group_data in groups:
-                if group_name == 'portal_customer':
+                if group_name in ('portal_customer', 'customer'):
                     group_data['has_button_access'] = True
+
+                    # 'notification_is_customer' is used to determine whether the group should be sent the access_token
+                    group_data['notification_is_customer'] = True
 
         return groups
 

addons/account/tests/test_account_move_in_invoice.py
@@ -2025,3 +2025,47 @@ class TestAccountMoveInInvoiceOnchanges(AccountTestInvoicingCommon):
                                     'debit': 500,
                                     'tax_line_id': self.company_data['default_tax_purchase'].id,
                                 }])
+
+    def test_invoice_sent_to_additional_partner(self):
+        """
+        Make sure that when an invoice is a partner to a partner who is not
+        the invoiced customer, they receive a link containing an access token,
+        allowing them to view the invoice without needing to log in.
+        """
+
+        # Create a simple invoice for the partner
+        invoice = self.init_invoice(
+            'out_invoice', partner=self.partner_a, invoice_date='2023-04-17', amounts=[100])
+
+        # Set the invoice to the 'posted' state
+        invoice.action_post()
+
+        # Create a partner not related to the invoice
+        additional_partner = self.env["res.partner"].create({
+            "name": "Additional Partner",
+            "email": "additional@example.com",
+        })
+
+        # Send the invoice
+        action = invoice.action_invoice_sent()
+        action_context = action["context"]
+
+        # Create the email using the wizard and add the additional partner as a recipient
+        invoice_send_wizard = self.env["account.invoice.send"].with_context(
+            action_context,
+            active_ids=[invoice.id]
+        ).create({'is_print': False})
+        invoice_send_wizard.partner_ids |= additional_partner
+
+        invoice_send_wizard.template_id.auto_delete = False
+
+        invoice_send_wizard.send_and_print_action()
+
+        # Find the email sent to the additional partner
+        additional_partner_mail = self.env["mail.mail"].search([
+            ("res_id", "=", invoice.id),
+            ('recipient_ids', '=', additional_partner.id)
+        ])
+
+        self.assertIn('access_token=', additional_partner_mail.body_html,
+                      "The additional partner should be sent the link including the token")

commit be694ba2c2390ded7560be2dda366a3ec027bca7

[FIX] account: restrict invoice sending to customer and added recipients
Summary
-------
Currently, when you send an invoice, every recipient receives the link
including the access token. Only the invoice's customer and manually
added recipients should receive the token.

Steps to reproduce
------------------
* Create and validate an invoice
* Add a follower `F `to the invoice
* Click on the Send & Print button, then add a recipient `R` who is not
  the customer associated with the invoice
* Proceed to send the invoice.
* Access the emails that were sent
* Using an incognito or private browsing window, open the `View Invoice`
  link for each of the 3 recipients.

You should see that all the links give access to the invoice.
What should happen is that only partner `R` and the invoice's customer
should haveaccess to the invoice.
`F` should be asked to login.

Cause
-----
Issue was introduced by 95c585f67bd37e1202c57b3e9eed09b0a964472c

Fix
---
Add a new notification group for partners that were manually added as
recipients, and give that group access to the invoice.

opw-3385302

addons/account/models/account_move.py
@@ -3246,17 +3246,28 @@ class AccountMove(models.Model):
         in account. Customer and portal group have probably no right to see
         the document so they don't have the access button. """
         groups = super(AccountMove, self)._notify_get_groups(msg_vals=msg_vals)
+        recipient_group = None
 
         self.ensure_one()
         if self.move_type != 'entry':
+            access_link = None
             for group_name, _group_method, group_data in groups:
-                if group_name in ('portal_customer', 'customer'):
+                if group_name == 'portal_customer':
                     group_data['has_button_access'] = True
+                    access_link = group_data['button_access']['url']
 
-                    # 'notification_is_customer' is used to determine whether the group should be sent the access_token
-                    group_data['notification_is_customer'] = True
+            button_access = {'url': access_link} if access_link else {}
+            recipient_group = (
+                'additional_intended_recipient',
+                lambda pdata: pdata['id'] in msg_vals['partner_ids'] and pdata['id'] != self.partner_id.id,
+                {
+                    'has_button_access': True,
+                    'button_access': button_access,
+                    'notification_is_customer': True,
+                }
+            )
 
-        return groups
+        return [recipient_group] + groups if recipient_group else groups
 
     def _is_downpayment(self):
         ''' Return true if the invoice is a downpayment.

addons/account/tests/test_account_move_in_invoice.py
@@ -2028,44 +2028,55 @@ class TestAccountMoveInInvoiceOnchanges(AccountTestInvoicingCommon):
 
     def test_invoice_sent_to_additional_partner(self):
         """
-        Make sure that when an invoice is a partner to a partner who is not
+        Make sure that when an invoice is deliberately sent to a partner who is not
         the invoiced customer, they receive a link containing an access token,
         allowing them to view the invoice without needing to log in.
         """
 
         # Create a simple invoice for the partner
-        invoice = self.init_invoice(
-            'out_invoice', partner=self.partner_a, invoice_date='2023-04-17', amounts=[100])
+        invoice = self.init_invoice('out_invoice', partner=self.partner_a, invoice_date='2023-04-17', amounts=[100])
 
         # Set the invoice to the 'posted' state
         invoice.action_post()
 
+        # add a follower to the invoice
+        self.partner_b.email = 'partner_b@example.com'
+        invoice.message_subscribe(self.partner_b.ids)
+
         # Create a partner not related to the invoice
-        additional_partner = self.env["res.partner"].create({
-            "name": "Additional Partner",
-            "email": "additional@example.com",
+        additional_partner = self.env['res.partner'].create({
+            'name': "Additional Partner",
+            'email': "additional@example.com",
         })
 
         # Send the invoice
-        action = invoice.action_invoice_sent()
-        action_context = action["context"]
+        action_context = invoice.action_invoice_sent().get('context', {})
 
         # Create the email using the wizard and add the additional partner as a recipient
-        invoice_send_wizard = self.env["account.invoice.send"].with_context(
+        invoice_send_wizard = self.env['account.invoice.send'].with_context(
             action_context,
             active_ids=[invoice.id]
         ).create({'is_print': False})
         invoice_send_wizard.partner_ids |= additional_partner
 
+        # prevent mail.mail record from being deleted after being sent.
         invoice_send_wizard.template_id.auto_delete = False
 
         invoice_send_wizard.send_and_print_action()
 
         # Find the email sent to the additional partner
-        additional_partner_mail = self.env["mail.mail"].search([
-            ("res_id", "=", invoice.id),
+        additional_partner_mail = self.env['mail.mail'].search([
+            ('res_id', '=', invoice.id),
             ('recipient_ids', '=', additional_partner.id)
         ])
 
         self.assertIn('access_token=', additional_partner_mail.body_html,
                       "The additional partner should be sent the link including the token")
+
+        # Find the email sent to the followers
+        additional_partner_mail = self.env['mail.mail'].search([
+            ('res_id', '=', invoice.id),
+            ('recipient_ids', '=', self.partner_b.id)
+        ])
+        self.assertNotIn('access_token=', additional_partner_mail.body_html,
+                      "The followers should not bet sent the access token by default")