cc8a4ee5738322f7e99e65734eba1352a36fb05e
Mélanie <mepe@odoo.com>
2023-10-05 16:49:59 +0200

    [IMP] * : redesign employee kanban card (master version)
    * = hr, hr_holidays, hr_homeworking
    
    Changes the hover on plane icon for leave. In public employee the hover is : "on leave" and in private employee the hover is the name of type's leave
    
    task : 3527398
    
    closes odoo/odoo#137741
    
    Signed-off-by: Sofie Gvaladze (sgv) <sgv@odoo.com>
    Co-authored-by: Xavier Bol <xbo@odoo.com>

--- addons/hr_holidays/static/src/components/hr_presence_status/hr_presence_status.js
+++ addons/hr_holidays/static/src/components/hr_presence_status/hr_presence_status.js
@@ -3,8 +3,9 @@
 import { patch } from "@web/core/utils/patch";
 
 import { HrPresenceStatus } from "@hr/components/hr_presence_status/hr_presence_status";
+import { HrPresenceStatusPrivate, hrPresenceStatusPrivate } from "@hr/components/hr_presence_status_private/hr_presence_status_private";
 
-patch(HrPresenceStatus.prototype, {
+const patchHrPresenceStatus = () => ({
     get icon() {
         if (this.value.startsWith("presence_holiday")) {
             return "fa-plane";
@@ -18,4 +19,24 @@ patch(HrPresenceStatus.prototype, {
         }
         return super.color;
     },
+});
+
+// Applies common patch on both components
+patch(HrPresenceStatus.prototype, patchHrPresenceStatus());
+patch(HrPresenceStatusPrivate.prototype, patchHrPresenceStatus());
+
+// Applies patch to hr_presence_status_private to display the time off type instead of default label
+patch(HrPresenceStatusPrivate.prototype, {
+    get label() {
+        return this.props.record.data.current_leave_id
+            ? this.props.record.data.current_leave_id[1]
+            : super.label;
+    }
+});
+
+Object.assign(hrPresenceStatusPrivate, {
+    fieldDependencies: [
+        ...(hrPresenceStatusPrivate.fieldDependencies || []),
+        { name: "current_leave_id", type:"many2one"}
+    ],
 });
