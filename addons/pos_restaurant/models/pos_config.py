aac2f4ca2cd3155daaeadecc901936416f3c21f9
Martin Trigaux <mat@odoo.com>
2023-10-05 14:43:00 +0000

    [FIX] point_of_sale: translate journal accounts
    closes odoo/odoo#138306
    
    X-original-commit: 3b84ccb330679a4ff35802ce8afbaa8106444645
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- addons/pos_restaurant/models/pos_config.py
+++ addons/pos_restaurant/models/pos_config.py
@@ -107,7 +107,7 @@ class PosConfig(models.Model):
                 ], limit=1)
                 if not cash_journal:
                     cash_journal = self.env['account.journal'].create({
-                        'name': 'Cash %s' % journal_counter,
+                        'name': _('Cash %s', journal_counter),
                         'code': 'RCSH%s' % journal_counter,
                         'type': 'cash',
                         'company_id': company.id
