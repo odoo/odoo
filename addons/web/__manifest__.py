6cd2cccbdef12414925a63009abb958993f8d6e2
Jorge Pinna Puissant <jpp@odoo.com>
2023-10-05 12:41:28 +0000

    [FIX] web: clickbot set all filters
    Since the new milk theme, the clickbot for click everywhere, didn't set
    the filters on the views.
    
    Now, the clickbot set all the filters on the views. This commit also add
    a test suit, to avoid future downgrades. This commit transform the
    clickbot to a module ES6.
    
    task-id 3535596
    
    X-original-commit: e2f531ec47e4f946cc37afee6b65531a637b5dfe
    Part-of: odoo/odoo#139032

--- addons/web/__manifest__.py
+++ addons/web/__manifest__.py
@@ -452,6 +452,7 @@ This module provides the core of the Odoo Web Client.
             'web/static/tests/mock_server_tests.js',
             'web/static/tests/setup.js',
             'web/static/tests/utils.js',
+            'web/static/src/webclient/clickbot/clickbot.js',
         ],
         'web.qunit_suite_tests': [
             'web/static/tests/env_tests.js',
@@ -473,6 +474,9 @@ This module provides the core of the Odoo Web Client.
         ],
         'web.qunit_mobile_suite_tests': [
             'web/static/tests/mobile/**/*.js',
+        ],
+        'web.assets_clickbot': [
+            'web/static/src/webclient/clickbot/clickbot.js',
         ],
     },
     'bootstrap': True,  # load translations for login screen,
--- addons/web/static/src/webclient/clickbot/clickbot_loader.js
+++ addons/web/static/src/webclient/clickbot/clickbot_loader.js
@@ -1,11 +1,11 @@
 /** @odoo-module **/
 
 import { _t } from "@web/core/l10n/translation";
-import { loadJS } from "@web/core/assets";
+import { loadBundle } from "@web/core/assets";
 import { registry } from "@web/core/registry";
 
 export async function startClickEverywhere(xmlId, appsMenusOnly) {
-    await loadJS("web/static/src/webclient/clickbot/clickbot.js");
+    await loadBundle("web.assets_clickbot");
     window.clickEverywhere(xmlId, appsMenusOnly);
 }
 
