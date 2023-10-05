7c71bf9b090a194f3077539321d3f0ea52dc7f56
Didier (did) <did@odoo.com>
2023-10-05 15:20:51 +0200

    [FIX] mail: do not display archived partner in recipient list
    Before this PR, archived partner where displayed in the recipient list, but they
    will never get notified.
    This PR exclude archived partner from the recipient list.
    
    task-3535433

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -4027,6 +4027,7 @@ class MailThread(models.AbstractModel):
             domain = expression.AND([domain, [
                 ('subtype_ids', '=', subtype_id),
                 ('partner_id', '!=', self.env.user.partner_id.id),
+                ("partner_id.active", "=", True),
             ]])
         if after:
             domain = expression.AND([domain, [('id', '>', after)]])
@@ -4207,6 +4208,7 @@ class MailThread(models.AbstractModel):
                 ("res_model", "=", self._name),
                 ('partner_id', '!=', self.env.user.partner_id.id),
                 ('subtype_ids', '=', subtype_id),
+                ("partner_id.active", "=", True)
             ])
             res['recipients'] = self.message_get_followers(filter_recipients=True)
         if 'suggestedRecipients' in request_list:
