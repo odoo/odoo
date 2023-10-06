3e18f9419e7d77c4c7d149eff869aec55b6eee49
sameerz09 <sameerz09@hotmail.com>
2023-10-06 17:11:28 +0300

    Creating a pull request to configure the ODOO 16 API

---
+++ addons/accounts_balances/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# from . import models
+
+from . import account_account
+
---
+++ addons/accounts_balances/wizard/__init__.py
@@ -0,0 +1,3 @@
+# from . import report_emp_wizrd
+##from . import test_wizard
+# from . import employee_report_wizard
