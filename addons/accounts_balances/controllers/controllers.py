1355ed873ee37e91b97efedeff54d6535b0ecc3e
sameerz09 <sameerz09@hotmail.com>
2023-10-06 14:27:00 +0300

    Revert "modified the API"
    This reverts commit af2e81c5bd130e96b2aa8eacc81911f8e919748c.

--- addons/accounts_balances/controllers/controllers.py
+++ addons/accounts_balances/controllers/controllers.py
@@ -1,5 +1,6 @@
 from odoo import http
 from odoo.http import request
+
 from datetime import datetime
 
 
@@ -36,7 +37,7 @@ class AccountBalanceController(http.Controller):
                         'account_name': account_name,
                         'balance': balance,
                         'account_root_id': account_root_id,
-                        'movement_date': account_date.strftime('%Y-%m-%d'),
+                        'account_date': account_date.strftime('%Y-%m-%d'),
                     }
                 else:
                     account_data[account_id]['balance'] += balance
@@ -54,3 +55,18 @@ class AccountBalanceController(http.Controller):
             'message': 'Success',
         }
         return data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
