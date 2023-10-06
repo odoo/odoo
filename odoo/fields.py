75208024a1cb86794a5c1ad7bd96858796f8ab42
Raphael Collet <rco@odoo.com>
2023-10-06 16:29:55 +0200

    [IMP] core: introduce _order_to_sql() to replace _generate_order_by()
    The new method should be used to generate an SQL object that represents
    how to order by a field in an SQL query.  We introduced the auxiliary
    method _order_field_to_sql() so that one can specify some SQL for
    ordering by a given field with a simple method override.
    
    Part-of: odoo/odoo#138019

--- odoo/fields.py
+++ odoo/fields.py
@@ -4766,7 +4766,7 @@ class Many2many(_RelationalMulti):
         comodel._flush_search(domain, order=comodel._order)
         query = comodel._where_calc(domain)
         comodel._apply_ir_rules(query, 'read')
-        query.order = comodel._generate_order_by(comodel._order, query).replace(' ORDER BY ', '')
+        query.order = comodel._order_to_sql(comodel._order, query)
 
         # join with many2many relation table
         sql_id1 = SQL.identifier(self.relation, self.column1)
