ddc9da828ad338678bfa6e3b44461f87683b828f
Sébastien Theys <seb@odoo.com>
2023-10-09 15:04:59 +0200

    [FIX] mail: tests: fix "open the chatWindow of a channel from [...]"
    Back-port test with `contains` from `master` and specify text present
    on command before clicking it.
    
    runbot-24981
    
    closes odoo/odoo#137946
    
    X-original-commit: ee99b2352f7d135e35a85553779e1fca2b351a85
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/tests/discuss/core/web/command_palette_tests.js
+++ addons/mail/static/tests/discuss/core/web/command_palette_tests.js
@@ -1,11 +1,13 @@
 /* @odoo-module */
 
-import { afterNextRender, click, start, startServer } from "@mail/../tests/helpers/test_utils";
+import { startServer } from "@bus/../tests/helpers/mock_python_environment";
+
+import { start } from "@mail/../tests/helpers/test_utils";
 
 import { commandService } from "@web/core/commands/command_service";
 import { registry } from "@web/core/registry";
-import { editSearchBar } from "@web/../tests/core/commands/command_service_tests";
-import { nextTick, triggerHotkey } from "@web/../tests/helpers/utils";
+import { triggerHotkey } from "@web/../tests/helpers/utils";
+import { click, contains, insertText } from "@web/../tests/utils";
 
 const serviceRegistry = registry.category("services");
 const commandSetupRegistry = registry.category("command_setup");
@@ -17,31 +19,25 @@ QUnit.module("command palette", {
     },
 });
 
-QUnit.test("open the chatWindow of a user from the command palette", async (assert) => {
+QUnit.test("open the chatWindow of a user from the command palette", async () => {
     const { advanceTime } = await start({ hasTimeControl: true });
     triggerHotkey("control+k");
-    await nextTick();
-    // Switch to partners
-    await editSearchBar("@");
-    await afterNextRender(() => advanceTime(commandSetupRegistry.get("@").debounceDelay));
-    await click(".o_command.focused");
-    assert.containsOnce($, ".o-mail-ChatWindow");
+    await insertText(".o_command_palette_search input", "@");
+    advanceTime(commandSetupRegistry.get("@").debounceDelay);
+    await contains(".o_command", { count: 1 });
+    await click(".o_command.focused", { text: "Mitchell Admin" });
+    await contains(".o-mail-ChatWindow", { text: "Mitchell Admin" });
 });
 
-QUnit.test("open the chatWindow of a channel from the command palette", async (assert) => {
+QUnit.test("open the chatWindow of a channel from the command palette", async () => {
     const pyEnv = await startServer();
     pyEnv["discuss.channel"].create({ name: "general" });
     pyEnv["discuss.channel"].create({ name: "project" });
     const { advanceTime } = await start({ hasTimeControl: true });
     triggerHotkey("control+k");
-    await nextTick();
-    // Switch to channels
-    await editSearchBar("#");
-    await afterNextRender(() => advanceTime(commandSetupRegistry.get("#").debounceDelay));
-    assert.containsOnce($, ".o_command:contains(general)");
-    assert.containsOnce($, ".o_command:contains(project)");
-
-    await click(".o_command.focused");
-    assert.containsOnce($, ".o-mail-ChatWindow");
-    assert.containsOnce($, ".o-mail-ChatWindow-name:contains(general)");
+    await insertText(".o_command_palette_search input", "#");
+    advanceTime(commandSetupRegistry.get("#").debounceDelay);
+    await contains(".o_command", { count: 2 });
+    await click(".o_command.focused", { text: "general" });
+    await contains(".o-mail-ChatWindow", { text: "general" });
 });
