d32122e08c2a37aaa58ea3e7da5c3d19f3d33b5e
Aaron Bohy <aab@odoo.com>
2023-10-09 12:42:33 +0200

    [REF] web,sale,portal: adapt SignatureForm to owl
    This commit converts the SignatureForm widget into a Component.
    This allows to remove the last usage of the legacy NameAndSignature
    widget, which we thus remove.
    
    Part of task~3439226

--- addons/sale/static/tests/tours/sale_signature.js
+++ addons/sale/static/tests/tours/sale_signature.js
@@ -22,11 +22,11 @@ registry.category("web_tour.tours").add('sale_signature', {
     },
     {
         content: "click select style",
-        trigger: '.o_web_sign_auto_select_style a',
+        trigger: '.o_web_sign_auto_select_style button',
     },
     {
         content: "click style 4",
-        trigger: '.o_web_sign_auto_font_selection a:eq(3)',
+        trigger: '.o_web_sign_auto_select_style .dropdown-item:eq(3)',
     },
     {
         content: "click submit",
