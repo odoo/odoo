6cf279501f701d9a3d5a0ea1a76e578c96c2bc27
sameerz09 <sameerz09@hotmail.com>
2023-10-09 08:11:12 +0300

    Change the implementation from using the http.request method to utilizing an API method for data retrieval

--- addons/movments_accounts_balances/controllers/controllers.py
+++ addons/movments_accounts_balances/controllers/controllers.py
@@ -1,76 +1,27 @@
-from odoo import http
-from odoo.http import request
+from odoo import api, fields, models
 
-from datetime import datetime
+class AccountBalance(models.Model):
+    _inherit = 'account.account'
 
+    @api.model
+    def getbalance(self, target_date_start, target_date_end):
+        # Initialize a list to store the results as dictionaries
+        results = []
 
-class AccountBalanceController(http.Controller):
+        # Retrieve all accounts
+        accounts = self.env['account.account'].search([])
 
-    @http.route('/get_balance_sum', type='json', auth='user', csrf=False, cors='*')
-    def balance_sum(self, db, login, password, start_date=None, end_date=None):
-        request.session.logout()
-        request.session.authenticate(db, login, password)
+        for account in accounts:
+            # Calculate the balance for the account within the date range
+            self.env.cr.execute("""
+                SELECT a.id AS account_id, a.name AS account_name, a.root_id AS root_id,
+                       l.date AS date, COALESCE(SUM(debit - credit), 0) AS balance
+                FROM account_move_line AS l
+                LEFT JOIN account_account AS a ON l.account_id = a.id
+                WHERE l.account_id = %s AND l.date >= %s AND l.date <= %s
+                GROUP BY a.id, a.name, a.root_id, l.date
+            """, (account.id, target_date_start, target_date_end))
 
-        # Convert the input date strings to datetime objects
-        start_date = datetime.strptime(start_date, '%Y-%m-%d')
-        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+            results += self.env.cr.dictfetchall()
 
-        # Fetch move lines from the 'account.move.line' model
-        move_lines = request.env['account.move.line'].search([])
-
-        account_data = {}
-
-        for move_line in move_lines:
-            account_id = move_line.account_id.id
-            balance = move_line.balance
-            account_root_id = move_line.account_root_id.id
-            account_date = datetime.combine(move_line.date, datetime.min.time())  # Convert to datetime object
-
-            # Check if the account_date is within the specified date range
-            if start_date <= account_date <= end_date:
-                # Fetch account name from the 'account.account' model
-                account_name = request.env['account.account'].browse(account_id).name
-
-                if account_id not in account_data:
-                    account_data[account_id] = {
-                        'account_id': account_id,
-                        'account_name': account_name,
-                        'balance': balance,
-                        'account_root_id': account_root_id,
-                        'movement_date': account_date.strftime('%Y-%m-%d'),
-                    }
-                else:
-                    account_data[account_id]['balance'] += balance
-
-        # Convert the dictionary of account balances to a list
-        account_data_list = list(account_data.values())
-
-        # Add the total balance to the response
-        total_balance = sum(entry['balance'] for entry in account_data_list)
-
-        data = {
-            'status': 200,
-            'response': account_data_list,
-            'total_balance': total_balance,
-            'message': 'Success',
-        }
-        return data
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return results
