f51c56687e04d95c1d3d0a2d4f7d64ea8753b176
jugj-odoo <jugj@odoo.com>
2023-10-10 16:03:09 +0200

    [IMP] hr_attendance : Bumping counters in tests
    Due to the Systray item add for attendance,
    an additional rpc call will be performed for employees,
    meaning that different counters have to be bumped.
    
    closes odoo/odoo#135439
    
    Related: odoo/upgrade#5234
    Related: odoo/enterprise#47384
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/mail/static/tests/helpers/webclient_setup.js
+++ addons/mail/static/tests/helpers/webclient_setup.js
@@ -14,6 +14,7 @@ const ROUTES_TO_IGNORE = [
     "/web/webclient/load_menus",
     "/web/dataset/call_kw/res.users/load_views",
     "/web/dataset/call_kw/res.users/systray_get_activities",
+    "/hr_attendance/attendance_user_data",
 ];
 const WEBCLIENT_PARAMETER_NAMES = new Set(["mockRPC", "serverData", "target", "webClientClass"]);
 const SERVICES_PARAMETER_NAMES = new Set([
