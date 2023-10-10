13b3f8af4b55d32912b698ae1672be9e144cb775
Pierre Pulinckx (pipu) <pipu@odoo.com>
2023-10-10 11:12:23 +0200

    [REF] website, mass_mailing: remove getBundle usages
    In this commit, we remove three usages of the getBundle function from
    @web/core/assets.js. The final goal is to remove it totally to simplify
    the understanding of assets's API. To replace the use of getBundle in
    mobile preview dialog, an xml template has been created on the server
    side and then called by http requests. During the request, we retrieve
    the list of assets (server side getbundle) to inject these into
    the xml template.
    
    taskId : 3266441
    
    closes odoo/odoo#138055
    
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>

--- addons/website/static/src/js/content/website_root_instance.js
+++ addons/website/static/src/js/content/website_root_instance.js
@@ -2,14 +2,13 @@
 
 import { createPublicRoot } from "@web/legacy/js/public/public_root";
 import { WebsiteRoot } from "./website_root";
-import { getBundle, loadBundle } from "@web/core/assets";
+import { loadBundle } from "@web/core/assets";
 
 export default createPublicRoot(WebsiteRoot).then(async (rootInstance) => {
     // This data attribute is set by the WebsitePreview client action for a
     // restricted editor user.
     if (window.frameElement && window.frameElement.dataset.loadWysiwyg === 'true') {
-        const assets = await getBundle("website.assets_all_wysiwyg");
-        await loadBundle(assets);
+        await loadBundle("website.assets_all_wysiwyg");
         window.dispatchEvent(new CustomEvent('PUBLIC-ROOT-READY', {detail: {rootInstance}}));
     }
     return rootInstance;
