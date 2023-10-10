2fd9c8e138d5e5218098e0462f549d389bf43893
Adrien Widart (awt) <awt@odoo.com>
2023-10-10 15:29:04 +0200

    [FIX] stock: display backorder wizard
    To reproduce the issue:
    1. Create a planned receipt with two lines:
       - 10 x P
       - 10 x P
    2. Confirm and set the done quantities:
       - 12 x P
       - 8 x P
    3. Validate
    
    Error: The receipt is directly processed and a backorder is created.
    In case a backorder could be created, we should first display the
    wizard that asks for a user's confirmation
    
    When checking for the backorder wizard, we compare, for each
    **product**, the sum of the demands with the sum of the done
    quantities. In the above use case, we will have `10 + 10 == 12 + 8`,
    so we don't display the wizard.
    
    However, while processing the SMs, this is different. For the second
    SM, we will set its demand to 8, create a third SM with a demand
    equal to 2 and create a new picking (the backorder) for this new SM.
    This behaviour is legit, the backorder creation makes sense. This is
    why this commit fixes the wizard display conditions.
    
    OPW-3373255

--- addons/stock/tests/test_stock_flow.py
+++ addons/stock/tests/test_stock_flow.py
@@ -2277,3 +2277,36 @@ class TestStockFlow(TestStockCommon):
         receipt.button_validate()
 
         self.assertEqual(receipt.move_lines.move_line_ids.location_dest_id, sub_loc)
+
+    def test_several_sm_with_same_product_and_backorders(self):
+        picking = self.env['stock.picking'].create({
+            'picking_type_id': self.picking_type_in,
+            'location_id': self.supplier_location,
+            'location_dest_id': self.stock_location,
+        })
+        move01, move02 = self.env['stock.move'].create([{
+            'name': self.productA.name,
+            'product_id': self.productA.id,
+            'product_uom_qty': 10,
+            'product_uom': self.productA.uom_id.id,
+            'description_picking': desc,
+            'picking_id': picking.id,
+            'location_id': self.supplier_location,
+            'location_dest_id': self.stock_location,
+        } for desc in ['Lorem', 'Ipsum']])
+
+        picking.action_confirm()
+
+        move01.quantity_done = 12
+        move02.quantity_done = 8
+
+        res = picking.button_validate()
+        self.assertIsInstance(res, dict)
+        self.assertEqual(res.get('res_model'), 'stock.backorder.confirmation')
+
+        wizard = Form(self.env[res['res_model']].with_context(res['context'])).save()
+        wizard.process()
+
+        backorder = picking.backorder_ids
+        self.assertEqual(backorder.move_lines.product_uom_qty, 2)
+        self.assertEqual(backorder.move_lines.description_picking, 'Ipsum')
