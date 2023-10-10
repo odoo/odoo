8748ee65be6f35c825743cac06dea3194aac6677
sameerz09 <sameerz09@hotmail.com>
2023-10-10 18:29:43 +0300

    enhance the API to include not only the date but also incorporate a dropdown menu for selecting the account

--- addons/movments_accounts_balances/controllers/controllers.py
+++ addons/movments_accounts_balances/controllers/controllers.py
@@ -4,15 +4,15 @@ class AccountBalance(models.Model):
     _inherit = 'account.account'
 
     @api.model
-    def getbalance(self, target_date_start, target_date_end):
+    def getbalance(self, target_date_start, target_date_end, selected_account_id):
         # Initialize a list to store the results as dictionaries
         results = []
 
-        # Retrieve all accounts
-        accounts = self.env['account.account'].search([])
+        # Retrieve the selected account based on the provided account ID
+        selected_account = self.env['account.account'].browse(selected_account_id)
 
-        for account in accounts:
-            # Calculate the balance for the account within the date range
+        if selected_account:
+            # Calculate the balance for the selected account within the date range
             self.env.cr.execute("""
                 SELECT a.id AS account_id, a.name AS account_name, a.root_id AS root_id,
                        l.date AS date, COALESCE(SUM(debit - credit), 0) AS balance
@@ -20,7 +20,7 @@ class AccountBalance(models.Model):
                 LEFT JOIN account_account AS a ON l.account_id = a.id
                 WHERE l.account_id = %s AND l.date >= %s AND l.date <= %s
                 GROUP BY a.id, a.name, a.root_id, l.date
-            """, (account.id, target_date_start, target_date_end))
+            """, (selected_account.id, target_date_start, target_date_end))
 
             results += self.env.cr.dictfetchall()
 
