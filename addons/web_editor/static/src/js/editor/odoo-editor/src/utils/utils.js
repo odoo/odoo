3a3353b1a14f9d685badeb10d4ce53f156debbb7
shsa-odoo <shsa@odoo.com>
2023-10-10 16:46:06 +0530

    [FIX] web_editor: font styles shows active while selecting cells
    Before this commit:
    
    -Font styles shows active while selecting cells.
    
    After this commit:
    
    -Font styles should not be active by default.
    
    task-3547652
    
    X-original-commit: d026f4d7160ec00b6cb27b4cf5b34afc9dbfda13

--- addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
@@ -1362,9 +1362,9 @@ export function hasClass(node, props) {
  */
 export function isSelectionFormat(editable, format) {
     const selectedNodes = getTraversedNodes(editable)
-        .filter(n => n.nodeType === Node.TEXT_NODE && n.nodeValue.trim().length);
+        .filter(n => n.nodeType === Node.TEXT_NODE);
     const isFormatted = formatsSpecs[format].isFormatted;
-    return selectedNodes && selectedNodes.every(n => isFormatted(n, editable));
+    return selectedNodes.length && selectedNodes.every(n => isFormatted(n, editable));
 }
 
 export function isUnbreakable(node) {
