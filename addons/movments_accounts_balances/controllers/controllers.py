744dcd385f55819ea72cebaa6c8b243f77e82174
sameerz09 <sameerz09@hotmail.com>
2023-10-11 15:49:54 +0300

    Apply query 2 when start_date is not provided

--- addons/movments_accounts_balances/controllers/controllers.py
+++ addons/movments_accounts_balances/controllers/controllers.py
@@ -1,56 +1,55 @@
-import re
-from odoo import models, fields, api
+from odoo import models, api
 from odoo.exceptions import ValidationError
-
 
 class AccountBalance(models.Model):
     _inherit = 'account.account'
 
     @api.model
-    def getbalance(self, target_date_start, target_date_end, selected_account_id):
-        # Initialize a list to store the results as dictionaries
+    def getbalance(self, start_date, end_date, selected_account_id):
         results = []
 
         try:
-            # Check if target_date_start is provided
-            if not target_date_start:
-                raise ValidationError("Start date is mandatory.")
-
-            # Check if target_date_end is provided
-            if not target_date_end:
-                raise ValidationError("End date is mandatory.")
-
-            # Convert the selected account ID to a string
-            selected_account_id = str(selected_account_id)
+            # Convert the selected account ID to an integer
+            selected_account_id = int(selected_account_id)
 
             # Retrieve the selected account based on the provided account ID
             selected_account = self.env['account.account'].browse(selected_account_id)
 
             if selected_account:
-                # Calculate the balance for the selected account within the date range
-                self.env.cr.execute("""
-                    SELECT a.id AS account_id, a.name AS account_name, a.root_id AS root_id,
-                           l.date AS date, COALESCE(SUM(debit - credit), 0) AS balance
-                    FROM account_move_line AS l
-                    LEFT JOIN account_account AS a ON l.account_id = a.id
-                    WHERE l.account_id = %s AND l.date >= %s AND l.date <= %s
-                    GROUP BY a.id, a.name, a.root_id, l.date
-                """, (selected_account_id, target_date_start, target_date_end))
+                if not start_date:  # Check if start_date is not provided
+                    # Apply query 2 when start_date is not provided
+                    query = """
+                        SELECT a.id AS account_id, a.name AS account_name, a.root_id AS root_id,
+                               l.date AS date, COALESCE(SUM(debit - credit), 0) AS balance
+                        FROM account_move_line AS l
+                        LEFT JOIN account_account AS a ON l.account_id = a.id
+                        WHERE l.account_id = %s AND l.date <= %s
+                        GROUP BY a.id, a.name, a.root_id, l.date
+                    """
+                    self.env.cr.execute(query, (selected_account_id, end_date))
+                else:
+                    # Apply query 1 when start_date is provided
+                    query = """
+                        SELECT a.id AS account_id, a.name AS account_name, a.root_id AS root_id,
+                               l.date AS date, COALESCE(SUM(debit - credit), 0) AS balance
+                        FROM account_move_line AS l
+                        LEFT JOIN account_account AS a ON l.account_id = a.id
+                        WHERE l.account_id = %s AND l.date >= %s AND l.date <= %s
+                        GROUP BY a.id, a.name, a.root_id, l.date
+                    """
+                    self.env.cr.execute(query, (selected_account_id, start_date, end_date))
 
-                results += self.env.cr.dictfetchall()
+                results = self.env.cr.dictfetchall()
         except ValidationError as e:
+            # Handle validation error
+            main_error_message = e.args[0]
+            print("Validation Error:", main_error_message)
+        except Exception as e:
+            # Handle other exceptions
             error_message = str(e)
-
-            # Extract the main error message using regular expressions
-            match = re.search(r"'([^']*)'", error_message)
-            if match:
-                main_error_message = match.group(1)
-                print("Error:", main_error_message)
-            else:
-                print("Error format not recognized.")
+            print("Error:", error_message)
 
         return results
-
     @api.model
     def get_all_accounts(self):
         accounts = self.env['account.account'].search([])
@@ -58,3 +57,4 @@ class AccountBalance(models.Model):
         account_ids = [str(account.id) for account in accounts]
 
         return account_ids
+
