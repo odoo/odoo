7d1f4f6c58d0a965bad627bf06a58508a6124648
Saurabh Choraria <sauc@odoo.com>
2023-10-11 12:05:10 +0530

    [FIX] base: raise ValueError when user enters wrong path
    When the user tries to add properties field in domain of a model
    without property name the error occurs.
    
    To reproduce the issue:
    - Install knowledge module.
    - Go to Settings > Technical > User Defined Filters.
    - Enter a name, add 'Knowledge Article' as model and then add domain
    [('article_properties', '!=', False)].
    - Click on save button and then on refresh button below code editor of domain.
    - The traceback will be generated
    
    Error: IndexError: list index out of range
    
    The issue is occurring because we are getting single element
    in path over here -
    https://github.com/odoo/odoo/blob/f4e60765db57fe3612f267e8fa44d10ccf5ff107/odoo/osv/expression.py#L649
    but we are trying to access path[1] due to which IndexError is occurring
    over here -
    https://github.com/odoo/odoo/blob/f4e60765db57fe3612f267e8fa44d10ccf5ff107/odoo/osv/expression.py#L683
    and here -
    https://github.com/odoo/odoo/blob/f4e60765db57fe3612f267e8fa44d10ccf5ff107/odoo/osv/expression.py#L687
    
    The condition is also wrong over here -
    https://github.com/odoo/odoo/blob/f4e60765db57fe3612f267e8fa44d10ccf5ff107/odoo/osv/expression.py#L683-L684
    in which we are checking length of path is not equals to 2 and then also
    we are trying to access path[1], due to which ValueError becomes a dead code.
    
    To fix this issue 'and' is replaced with 'or' in that condition.
    
    sentry-4499339271

--- odoo/addons/test_new_api/tests/test_properties.py
+++ odoo/addons/test_new_api/tests/test_properties.py
@@ -1849,3 +1849,8 @@ class PropertiesSearchCase(PropertiesCase):
             for order in orders:
                 with self.assertRaises(UserError), self.assertQueryCount(0):
                     self.env['test_new_api.message'].search(domain=[], order=order)
+
+    @mute_logger('odoo.fields')
+    def test_properties_field_search(self):
+        with self.assertRaises(ValueError):
+            self.env['test_new_api.message'].search([('attributes', '=', '"Test"')])
