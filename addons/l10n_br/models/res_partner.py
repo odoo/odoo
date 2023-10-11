31312ca14f5b4509a7723e00abb56331d0b8a3ee
Nshimiyimana Séna <sesn@odoo.com>
2023-10-11 06:11:08 +0000

    [FIX] l10n_br, base_vat: move Brazilian vat check to base_vat
    Summary
    -------
    Currently, adding a CPF tax ID on a Brazilian contact always fails.
    
    Steps to reproduce
    ------------------
    * install `contacts` and `base_vat`
    * create a new contact with country Brazil
    * enter a valid CPF (ex: 11144477735)
    
    You should be met with an error
    
    Cause
    -----
    The CPF check is only implemented in `l10n_br`. Without this module, the
    default vat check is applied, but it fails for CPF numbers.
    
    Fix
    ---
    Move the CPF/CNPJ check from `l10n_br` to `base_vat`
    
    opw-3421476
    
    closes odoo/odoo#139222
    
    X-original-commit: 5dc5832f438006a9a4b9abeb1f093d7ed520c771
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>
    Signed-off-by: Séna Serge Nshimiyimana (sesn) <sesn@odoo.com>

--- addons/l10n_br/models/res_partner.py
+++ addons/l10n_br/models/res_partner.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-import stdnum.br.cpf
-
-from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
@@ -12,9 +9,3 @@ class ResPartner(models.Model):
     l10n_br_ie_code = fields.Char(string="IE", help="State Tax Identification Number. Should contain 9-14 digits.")
     l10n_br_im_code = fields.Char(string="IM", help="Municipal Tax Identification Number")
     l10n_br_isuf_code = fields.Char(string="SUFRAMA code", help="SUFRAMA registration number.")
-
-    @api.constrains('vat', 'l10n_latam_identification_type_id')
-    def check_cpf(self):
-        for partner in self.filtered(lambda partner: partner.l10n_latam_identification_type_id == self.env.ref('l10n_br.cpf')):
-            if partner.vat and not stdnum.br.cpf.is_valid(partner.vat) and not stdnum.br.cnpj.is_valid(partner.vat):
-                raise ValidationError(_('CPF/CNPJ number %s for %s is not valid.', partner.vat, partner.display_name))
