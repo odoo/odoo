8c699a5eea18b9ff14acfc8dc838d90851c65723
sameerz09 <sameerz09@hotmail.com>
2023-10-11 04:03:17 +0300

    enhance the API to include not only the date but also incorporate a dropdown menu for selecting the account

--- addons/movments_accounts_balances/controllers/controllers.py
+++ addons/movments_accounts_balances/controllers/controllers.py
@@ -1,4 +1,7 @@
+import re
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 
 class AccountBalance(models.Model):
     _inherit = 'account.account'
@@ -8,24 +11,43 @@ class AccountBalance(models.Model):
         # Initialize a list to store the results as dictionaries
         results = []
 
-        # Convert the selected account ID to a string
-        selected_account_id = str(selected_account_id)
+        try:
+            # Check if target_date_start is provided
+            if not target_date_start:
+                raise ValidationError("Start date is mandatory.")
 
-        # Retrieve the selected account based on the provided account ID
-        selected_account = self.env['account.account'].browse(selected_account_id)
+            # Check if target_date_end is provided
+            if not target_date_end:
+                raise ValidationError("End date is mandatory.")
 
-        if selected_account:
-            # Calculate the balance for the selected account within the date range
-            self.env.cr.execute("""
-                SELECT a.id AS account_id, a.name AS account_name, a.root_id AS root_id,
-                       l.date AS date, COALESCE(SUM(debit - credit), 0) AS balance
-                FROM account_move_line AS l
-                LEFT JOIN account_account AS a ON l.account_id = a.id
-                WHERE l.account_id = %s AND l.date >= %s AND l.date <= %s
-                GROUP BY a.id, a.name, a.root_id, l.date
-            """, (selected_account_id, target_date_start, target_date_end))
+            # Convert the selected account ID to a string
+            selected_account_id = str(selected_account_id)
 
-            results += self.env.cr.dictfetchall()
+            # Retrieve the selected account based on the provided account ID
+            selected_account = self.env['account.account'].browse(selected_account_id)
+
+            if selected_account:
+                # Calculate the balance for the selected account within the date range
+                self.env.cr.execute("""
+                    SELECT a.id AS account_id, a.name AS account_name, a.root_id AS root_id,
+                           l.date AS date, COALESCE(SUM(debit - credit), 0) AS balance
+                    FROM account_move_line AS l
+                    LEFT JOIN account_account AS a ON l.account_id = a.id
+                    WHERE l.account_id = %s AND l.date >= %s AND l.date <= %s
+                    GROUP BY a.id, a.name, a.root_id, l.date
+                """, (selected_account_id, target_date_start, target_date_end))
+
+                results += self.env.cr.dictfetchall()
+        except ValidationError as e:
+            error_message = str(e)
+
+            # Extract the main error message using regular expressions
+            match = re.search(r"'([^']*)'", error_message)
+            if match:
+                main_error_message = match.group(1)
+                print("Error:", main_error_message)
+            else:
+                print("Error format not recognized.")
 
         return results
 
