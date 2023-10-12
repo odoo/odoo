7f72c33190004d16ab1a9382e6c60c5ec193514a
Mathieu Walravens <wama@odoo.com>
2023-10-12 11:05:04 +0200

    [FIX] product: prevent archived product variants from overshadowing active ones
    This commit addresses an issue where archived product variants were
    taking precedence over active variants with the same combination,
    rendering the combination unusable in the sales app.
    
    Steps to reproduce:
    1. Install Sale & enable product variants
    2. Create a product with two attributes, each having two values
    3. Make a sale for each variant
    4. Remove one of the attributes and save
    5. Add back the same attribute with only one of the values
    6. Make a new quote with the product; Option is not available.
    
    After this commit:
    Archived variants will no longer exclude a combination if an active
    variant with the same combination exists.
    
    opw-3538366
    
    X-original-commit: 7456e4814d1845d6fb592984cd2dd90e45e2bc06
    Co-authored-by: Victor Feyens <vfe@odoo.com>

--- addons/sale/tests/test_sale_product_attribute_value_config.py
+++ addons/sale/tests/test_sale_product_attribute_value_config.py
@@ -2,8 +2,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields
-from odoo.addons.product.tests.test_product_attribute_value_config import TestProductAttributeValueCommon
+from odoo.fields import Command
 from odoo.tests import tagged
+
+from odoo.addons.product.tests.test_product_attribute_value_config import TestProductAttributeValueCommon
+from odoo.addons.product.tests.common import ProductAttributesCommon
+from odoo.addons.sale.tests.common import SaleCommon
 
 
 class TestSaleProductAttributeValueCommon(TestProductAttributeValueCommon):
@@ -409,3 +413,101 @@ class TestSaleProductAttributeValueConfig(TestSaleProductAttributeValueCommon):
         })
         self.computer_keyboard_attribute_lines.product_template_value_ids[0].price_extra = 5
         self.computer_keyboard_attribute_lines.product_template_value_ids[1].price_extra = -5
+
+
+@tagged('post_install', '-at_install')
+class TestSaleProductVariants(ProductAttributesCommon, SaleCommon):
+
+    # TODO move to sale_product_configurator tests in 16.3+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.product_template_2lines_2attributes = cls.env['product.template'].create({
+            'name': '2 lines 2 attributes',
+            'uom_id': cls.uom_unit.id,
+            'uom_po_id': cls.uom_unit.id,
+            'categ_id': cls.product_category.id,
+            'attribute_line_ids': [
+                Command.create({
+                    'attribute_id': cls.color_attribute.id,
+                    'value_ids': [Command.set([
+                        cls.color_attribute_red.id,
+                        cls.color_attribute_blue.id,
+                    ])],
+                }),
+                Command.create({
+                    'attribute_id': cls.size_attribute.id,
+                    'value_ids': [Command.set([
+                        cls.size_attribute_s.id,
+                        cls.size_attribute_m.id,
+                    ])]
+                })
+            ]
+        })
+
+        # Sell all variants
+        cls.empty_order.order_line = [
+            Command.create({
+                'product_id': product.id,
+            })
+            for product in cls.product_template_2lines_2attributes.product_variant_ids
+        ]
+
+    def test_attribute_removal(self):
+        def _get_ptavs():
+            return self.product_template_2lines_2attributes.with_context(
+                active_test=False
+            ).attribute_line_ids.product_template_value_ids
+
+        def _get_archived_variants():
+            return self.product_template_2lines_2attributes.with_context(
+                active_test=False
+            ).product_variant_ids.filtered(lambda p: not p.active)
+
+        def _get_active_variants():
+            return self.product_template_2lines_2attributes.product_variant_ids
+
+        self.assertEqual(len(_get_ptavs()), 4)
+        self.product_template_2lines_2attributes.attribute_line_ids = [
+            Command.unlink(self.product_template_2lines_2attributes.attribute_line_ids.filtered(
+                lambda ptal: ptal.attribute_id.id == self.size_attribute.id
+            ).id)
+        ]
+        self.assertEqual(len(_get_ptavs()), 4)
+
+        # Use products s.t. they are archived and not deleted
+        self.empty_order.order_line = [
+            Command.create({
+                'product_id': product.id,
+            })
+            for product in self.product_template_2lines_2attributes.product_variant_ids
+        ]
+
+        self.assertEqual(len(_get_archived_variants()), 4)
+        self.assertEqual(len(_get_active_variants()), 2)
+
+        self.product_template_2lines_2attributes.attribute_line_ids = [
+            Command.create({
+                'attribute_id': self.size_attribute.id,
+                'value_ids': [Command.set([
+                    self.size_attribute_s.id,
+                ])]
+            })
+        ]
+        self.assertEqual(len(_get_ptavs()), 4)
+        self.assertEqual(len(_get_active_variants()), 2)
+        self.assertEqual(len(_get_archived_variants()), 4)
+
+        # When adding a single attribute line, the attribute will be added to all existing variants
+        # Instead of unarchiving existing archived variants with the same combination
+        # Leading to a state where the database holds two variants with the same combination
+        # We don't want this combination to be excluded from the product configurator as it is valid
+        # as long as there is one active variant with this configuration.
+        exclusions_data = self.product_template_2lines_2attributes._get_attribute_exclusions()
+        self.assertTrue(
+            all(
+                tuple(product.product_template_attribute_value_ids.ids) not in exclusions_data['archived_combinations']
+                for product in _get_active_variants()
+            )
+        )
