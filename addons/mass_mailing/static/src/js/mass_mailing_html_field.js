cdfc7b083a9fede1327db1f67d1b08a7d5bc2d36
Rodolpho Lima <rcdl@odoo.com>
2023-10-12 15:01:53 +0200

    [FIX] mass_mailing: duplicate call to saveModifiedImages
    The 'saveModifiedImages' function is already called by the parent class'
    (HtmlField) 'commitChanges', in which the replacement of the images' src
    attribute and the removal of the 'o_modified_image_to_save' class are
    done with the editor's observer deactivated.
    
    Before this commit, calling this function before the call to
    super.commitChanges resulted in converting the images within a history
    step that was later reverted by the 'historyRevertCurrentStep()' call at
    the end of the '_pendingCommitChanges' function.
    
    Morover, the '$content' property is undefined.

--- addons/mass_mailing/static/src/js/mass_mailing_html_field.js
+++ addons/mass_mailing/static/src/js/mass_mailing_html_field.js
@@ -103,7 +103,6 @@ export class MassMailingHtmlField extends HtmlField {
             const $editable = this.wysiwyg.getEditable();
             this.wysiwyg.odooEditor.historyPauseSteps();
             await this.wysiwyg.cleanForSave();
-            await this.wysiwyg.saveModifiedImages(this.$content);
 
             await super.commitChanges();
 
