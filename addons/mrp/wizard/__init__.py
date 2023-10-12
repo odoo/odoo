c4ba58bc8b89feefc7dd200d7f2445cc71fe3bc4
Martin Maes <marm@odoo.com>
2023-10-12 14:15:04 +0200

    [FIX] stock,mrp,purchase_stock: replenish fix
    In 16.4, this commit https://github.com/odoo/odoo/commit/c3b7a87462cd41654d0e2bd3beb2f0e065ddfb75
    created a notification when replenishing a product.
    However, it achieved it by modifying a stock.order_point which was
    not the ideal solution as we don't want the order_point to change.
    
    In this commit, we will revert to the previous behaviour,
    but we'll keep the notification by delegating it to the wizard itself.
    
    The way the record created were retreived (to display the notification)
    was thanks to the orderpoint.
    As we do not have access to orderpoints now, we are just retreive
    the first record (of a certain type) created just after the start
    of the function.
    
    This method has a big problem : concurrencies.
    If anyone creates a record on ``manufacturing.order``,
    ``purchase.order.line`` or ``stock.move`` between the start of
    our timer and the creation of our record, a wrong record will be
    selected.
    
    task-3527727

--- addons/mrp/wizard/__init__.py
+++ addons/mrp/wizard/__init__.py
@@ -5,6 +5,7 @@ from . import change_production_qty
 from . import stock_warn_insufficient_qty
 from . import mrp_production_backorder
 from . import mrp_consumption_warning
+from . import product_replenish
 from . import stock_assign_serial_numbers
 from . import mrp_production_split
 from . import stock_label_type
