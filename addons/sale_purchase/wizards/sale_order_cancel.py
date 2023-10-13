8f48859f5d3522586b974251c1b4f1816e74fb5c
Roy Le <roy.le@viindoo.com>
2023-10-13 09:03:58 +0700

    [FIX] sale_purchase: Sales user (no purchasing permissions) cannot
    cancel their SO
    
    Steps:
    Sales user (no purchasing permissions) create SO and confirm it, then
    cancel it
    
    Issue:
    Sales user cannot cancel their SO without permissions in the purchasing
    
    Analyze:
    Because the method `_compute_display_purchase_orders_alert` has a read
    the SO's purchase_order_count, it causes an error
    
    Fix:
    Add a group on the field to not display it in the view instead. Anyway
    if the user doesn't have the right to correct it, he will probably just
    ignore the warning and another mechanism should exist to warn the
    purchase representative.
    
    closes odoo/odoo#142547
    
    X-original-commit: 77a614d63c64a075db009d8c3acaf19db0fc7cd0
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/sale_purchase/wizards/sale_order_cancel.py
+++ addons/sale_purchase/wizards/sale_order_cancel.py
@@ -6,7 +6,8 @@ class SaleOrderCancel(models.TransientModel):
 
     display_purchase_orders_alert = fields.Boolean(
         string="Purchase Order Alert",
-        compute='_compute_display_purchase_orders_alert'
+        compute='_compute_display_purchase_orders_alert',
+        groups='purchase.group_purchase_user'
     )
 
     @api.depends('order_id')
