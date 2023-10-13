d8c0373f336ef3bc4dcea891ff70113b864ea8cc
Michael (mcm) <mcm@odoo.com>
2023-10-13 15:39:49 +0200

    [IMP] web: link popover together
    Before this commit, cliking on a popover that was open inside another
    popover closed both of them.
    After this commit, the popovers are not closed. them will close only if
    we click out of them and the popovers they opened.
    
    closes odoo/odoo#140258
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/web/static/tests/core/popover/popover_hook_tests.js
+++ addons/web/static/tests/core/popover/popover_hook_tests.js
@@ -4,7 +4,7 @@ import { usePopover } from "@web/core/popover/popover_hook";
 import { popoverService } from "@web/core/popover/popover_service";
 import { registry } from "@web/core/registry";
 import { clearRegistryWithCleanup, makeTestEnv } from "../../helpers/mock_env";
-import { destroy, getFixture, mount, nextTick } from "../../helpers/utils";
+import { click, destroy, getFixture, mount, nextTick } from "../../helpers/utils";
 
 import { Component, xml } from "@odoo/owl";
 import { hotkeyService } from "@web/core/hotkeys/hotkey_service";
@@ -84,4 +84,55 @@ QUnit.test("close popover when component is unmounted", async (assert) => {
     assert.containsNone(target, ".o_popover");
     assert.containsNone(target, ".o_popover #comp1");
     assert.containsNone(target, ".o_popover #comp2");
+});
+
+QUnit.test("popover opened from another", async (assert) => {
+    class Comp extends Component {
+        static id = 0;
+        static template = xml`
+            <div class="p-4">
+                <button class="pop-open" t-on-click="(ev) => this.popover.open(ev.target, {})">open popover</button>
+            </div>
+        `;
+        setup() {
+            this.popover = usePopover(Comp, {
+                popoverClass: `popover-${++Comp.id}`,
+            });
+        }
+    }
+
+    await mount(Comp, target, { env });
+
+    await click(target, ".pop-open");
+    assert.containsOnce(target, ".popover-1", "open first popover");
+
+    await click(target, ".popover-1 .pop-open");
+    assert.containsN(target, ".o_popover", 2, "open second popover from the first one");
+    assert.containsOnce(target, ".popover-1");
+    assert.containsOnce(target, ".popover-2");
+
+    await click(target, ".popover-2 .pop-open");
+    assert.containsN(target, ".o_popover", 3, "open third popover from the second one");
+    assert.containsOnce(target, ".popover-1");
+    assert.containsOnce(target, ".popover-2");
+    assert.containsOnce(target, ".popover-3");
+
+    await click(target, ".popover-3");
+    assert.containsN(target, ".o_popover", 3, "clicking inside third popover closes nothing");
+    assert.containsOnce(target, ".popover-1");
+    assert.containsOnce(target, ".popover-2");
+    assert.containsOnce(target, ".popover-3");
+
+    await click(target, ".popover-2");
+    assert.containsN(
+        target,
+        ".o_popover",
+        2,
+        "clicking inside second popover closes third popover"
+    );
+    assert.containsOnce(target, ".popover-1");
+    assert.containsOnce(target, ".popover-2");
+
+    await click(target, "#close");
+    assert.containsNone(target, ".o_popover", "clicking out of any popover closes them all");
 });
