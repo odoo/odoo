0bad2328305187c29ce1fff99a1f6042dacc84a2
“moda” <moda@odoo.com>
2023-10-16 15:42:50 +0200

    [IMP] pos_online_payment_self_order,pos_self_order: kiosk online payment
    Online payment method added to kiosk. This works in the same way as in
    the mobile SelfOrder, except that the payment page does not open
    automatically in the kiosk. A QR code appears and the user is asked to
    scan it to make the payment on their phone.
    
    Once the QR code has been scanned, the page on the kiosk updates to show
    "payment in progress", and once the payment has been validated, the
    validation page appears on the kiosk. These different statuses are sent
    via websocket.
    
    closes odoo/odoo#138825
    
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/pos_online_payment_self_order/static/src/self_order_service.js
+++ addons/pos_online_payment_self_order/static/src/self_order_service.js
@@ -4,31 +4,43 @@ import { SelfOrder } from "@pos_self_order/app/self_order_service";
 import { session } from "@web/session";
 
 patch(SelfOrder.prototype, {
-    openOnlinePaymentPage({
-        id: order_id,
-        access_token: order_access_token,
-        pos_config_id: order_pos_config_id,
-    }) {
+    async setup(...args) {
+        await super.setup(...args);
+        this.onlinePaymentStatus = null;
+    },
+    finalizeOrder() {
+        const order = this.currentOrder;
+
+        this.updateOrdersFromServer([order], [order.access_token]);
+        this.router.navigate("confirmation", {
+            orderAccessToken: order.access_token,
+            screenMode: "order",
+        });
+    },
+    getOnlinePaymentUrl(
+        { id: order_id, access_token: order_access_token, pos_config_id: order_pos_config_id },
+        exitRoute = true
+    ) {
         const baseUrl = session.base_url;
         const order = this.currentOrder;
         const tableIdentifier = this.table?.identifier;
-        let exitRouteUrl = `${baseUrl}/pos-self/${order_pos_config_id}`;
+        let exitRouteUrl = baseUrl;
 
-        if (this.config.self_ordering_pay_after === "each") {
-            exitRouteUrl += `/confirmation/${order.access_token}/order`;
+        if (exitRoute) {
+            exitRouteUrl += `/pos-self/${order_pos_config_id}`;
+
+            if (this.config.self_ordering_pay_after === "each") {
+                exitRouteUrl += `/confirmation/${order.access_token}/order`;
+            }
+
+            exitRouteUrl += `?access_token=${this.access_token}`;
+
+            if (tableIdentifier) {
+                exitRouteUrl += `&table_identifier=${tableIdentifier}`;
+            }
         }
 
-        exitRouteUrl += `?access_token=${this.access_token}`;
-
-        if (tableIdentifier) {
-            exitRouteUrl += `&table_identifier=${tableIdentifier}`;
-        }
-
-        const exitRoute = encodeURIComponent(exitRouteUrl);
-        window.open(
-            baseUrl +
-                `/pos/pay/${order_id}?access_token=${order_access_token}&exit_route=${exitRoute}`,
-            "_self"
-        );
+        const exit = encodeURIComponent(exitRouteUrl);
+        return `${baseUrl}/pos/pay/${order_id}?access_token=${order_access_token}&exit_route=${exit}`;
     },
 });
