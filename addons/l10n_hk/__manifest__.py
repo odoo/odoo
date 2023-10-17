6bba47919f749042bc8f3c88e79f09e0c4d642e7
Nicolas Viseur (vin) <vin@odoo.com>
2023-10-17 15:14:48 +0800

    [REF] l10n_hk: Demo Data Cleanup
    Up until now, we did not have any common demo data package for localizations.
    This is an issue for localizations having both accounting and hr packages,
    because these do not depend on each others.
    It means that to have complete demo data individually, they need to define
    demo company in both cases.
    
    This leads to duplication in demo companies for a single country when both
    packages are installed, which is not ideal for testing as it also means that
    demo data is split between multiple companies.
    
    This commit will rename things a bit to bring a common demo data package
    for the Hong Kong localization.
    
    l10n_hk as it is known today becomes l10n_hk_account (as it was all along
    an invoicing module).
    l10n_hk is instead a new module containing the demo company data.
    
    task id # 3555251

--- addons/l10n_hk/__manifest__.py
+++ addons/l10n_hk/__manifest__.py
@@ -1,19 +1,15 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Hong Kong - Accounting',
-    'website': 'https://www.odoo.com/documentation/master/applications/finance/fiscal_localizations/hong_kong.html',
+    'name': 'Hong Kong - Demo Data',
+    'version': '1.0',
     'icon': '/account/static/description/l10n.png',
     'countries': ['hk'],
-    'version': '1.0',
-    'category': 'Accounting/Localizations/Account Charts',
-    'description': ' This is the base module to manage chart of accounting and localization for Hong Kong ',
-    'depends': [
-        'account_qr_code_emv',
-    ],
-    'data': [
-        'data/account_chart_template_data.xml',
-        'views/res_bank_views.xml',
-    ],
+    'category': 'Localization/Demo Data',
+    'description': """
+This is the base module to create demonstration data for the Hong Kong localization in Odoo.
+============================================================================================
+    """,
+    'depends': ['base'],
     'demo': [
         'demo/demo_company.xml',
     ],
