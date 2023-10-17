6dd5de72d2297776791f925f04a14174e20177c5
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2023-10-17 15:11:58 +0000

    [FIX] l10n_gcc_invoice: payment terms not shown in english/arabic
    Have a SA company
    In Accounting settings activate invoice terms
    Add a payment terms string in english, add the arabic translation
    Create an invoice
    Click preview
    
    Issue: Payment terms will be shown in english twice
    
    This occurs because, even if `narration` field is translatable the
    translation is not automatically copied when the field is copied from
    the company `invoice_terms`.
    
    opw-3530811
    
    X-original-commit: 862e95a6c118a053c67808e34be347ca1a5f7a2f

--- addons/l10n_gcc_invoice/models/account_move.py
+++ addons/l10n_gcc_invoice/models/account_move.py
@@ -32,6 +32,38 @@ class AccountMove(models.Model):
 
         return num2words(number, lang=lang).title()
 
+    def _load_narration_translation(self):
+        # Workaround to have the english/arabic version of the payment terms
+        # in the report
+        if not self:
+            return
+        moves_to_fix = self.env['account.move']
+        for move in self.filtered(lambda m: m.narration and m.is_sale_document(include_receipts=True) and m.company_id.country_id in self.env.ref('base.gulf_cooperation_council').country_ids):
+            lang = move.partner_id.lang or self.env.user.lang
+            if move.company_id.terms_type == 'html' or move.narration != move.company_id.with_context(lang=lang).invoice_terms:
+                continue
+            moves_to_fix |= move
+        if not moves_to_fix:
+            return
+        self.env['res.company'].flush_model(['invoice_terms'])
+        self.env.cr.execute('SELECT "id","invoice_terms" FROM "res_company" WHERE id = any(%s)', [moves_to_fix.company_id.ids])
+        translation_by_company_id = {company_id: narration for company_id, narration in self.env.cr.fetchall()}
+        self.env.cache.update_raw(moves_to_fix, self._fields['narration'], [
+            translation_by_company_id[move.company_id.id]
+            for move in moves_to_fix
+        ], dirty=True)
+        moves_to_fix.modified(['narration'])
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        moves = super().create(vals_list)
+        moves._load_narration_translation()
+        return moves
+
+    def _compute_narration(self):
+        super()._compute_narration()
+        # Only update translations of real records
+        self.filtered('id')._load_narration_translation()
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
