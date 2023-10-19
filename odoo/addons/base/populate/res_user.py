73a5e322f6e250569ee1942d678f8a4f87616bd7
Sébastien Theys <seb@odoo.com>
2023-10-19 19:04:15 +0200

    [FIX] base, mail: correct populate of base models
    Significantly improves the usability and the result of the populate for
    base models and channel/member/message.
    
    - make populate of base models re-entrant
    - adapt size to values that make sense:
      - small shows something relevant, and can be quickly ran several times
      - medium is fast enough to be usuable but big enough to highlight
        performance issues
      - large is... still not to be attempted
    - avoid conflicting conditions that either make no sense or can lead to
      crashes when populating the data or running the database
    - better spread of data in channel/member/messages
    
    closes odoo/odoo#139244
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- odoo/addons/base/populate/res_user.py
+++ odoo/addons/base/populate/res_user.py
@@ -20,6 +20,8 @@ class Users(models.Model):
     _populate_dependencies = ["res.partner"]
 
     def _populate_factories(self):
+        last_id = self.env["res.users"].search([], order="id desc", limit=1).id
+
         def generate_partner_id(iterator, *args):
             partner_factories = self.env['res.partner']._populate_factories()
             partner_generator = populate.chain_factories(partner_factories, self._name)
@@ -30,13 +32,19 @@ class Users(models.Model):
         def get_company_ids(values, **kwargs):
             return [(6, 0, [values['company_id']])]
 
+        def compute_login(values=None, counter=0, **kwargs):
+            return f'user_login_{last_id + counter + 1}'
+
+        def compute_name(values=None, counter=0, **kwargs):
+            return f'user_{last_id + counter + 1}'
+
         return [
             ('active', populate.cartesian([True, False], [0.9, 0.1])),
             ('partner_id', generate_partner_id),
             ('company_id', populate.randomize(self.env.registry.populated_models['res.company'])),
             ('company_ids', populate.compute(get_company_ids)),
-            ('login', populate.constant('user_login_{counter}')),
-            ('name', populate.constant('user_{counter}')),
+            ('login', populate.compute(compute_login)),
+            ('name', populate.compute(compute_name)),
         ]
 
     def _populate(self, size):
