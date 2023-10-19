2ebfabe64deda3f94479b656ecd87699526a2ea5
Didier (did) <did@odoo.com>
2023-10-19 13:24:27 +0000

    [FIX] mail: improve messaging menu performance
    To reproduce:
    
    * Populate mail.message as medium or more
    * Open odoo and click the messaging menu
    * The messaging menu take an unreasonable amount of time to open.
    
    This PR cache the threads getter to avoid unnecessary computations
    
    task-3551625
    
    X-original-commit: c564f07c95ce85670cf55797e2da3d5798924630

--- addons/mail/static/src/core/web/messaging_menu.js
+++ addons/mail/static/src/core/web/messaging_menu.js
@@ -6,7 +6,7 @@ import { NotificationItem } from "@mail/core/web/notification_item";
 import { onExternalClick } from "@mail/utils/common/hooks";
 import { createLocalId } from "@mail/utils/common/misc";
 
-import { Component, useState } from "@odoo/owl";
+import { Component, onWillRender, useState } from "@odoo/owl";
 
 import { hasTouch } from "@web/core/browser/feature_detection";
 import { Dropdown } from "@web/core/dropdown/dropdown";
@@ -39,6 +39,7 @@ export class MessagingMenu extends Component {
         onExternalClick("selector", () => {
             Object.assign(this.state, { addingChat: false, addingChannel: false });
         });
+        onWillRender(() => this.threads = this.getThreads());
     }
 
     beforeOpen() {
@@ -95,13 +96,12 @@ export class MessagingMenu extends Component {
             displayName: sprintf(_t("%s has a request"), this.store.odoobot.name),
             iconSrc: this.threadService.avatarUrl(this.store.odoobot),
             partner: this.store.odoobot,
-            isLast: this.threads.length === 0 && this.store.notificationGroups.length === 0,
             isShown:
                 this.store.discuss.activeTab === "all" && this.notification.permission === "prompt",
         };
     }
 
-    get threads() {
+    getThreads() {
         /** @type {import("@mail/core/common/thread_model").Thread[]} */
         let threads = Object.values(this.store.threads).filter(
             (thread) =>
