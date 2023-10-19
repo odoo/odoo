11210caa938cafb2c03332fc60a4ca9d839a5f7e
Serge Bayet (seba) <seba@odoo.com>
2023-10-19 09:21:35 +0200

    [FIX] website_project, base: update the module description
    This commit revises the description of the `website_project` module to
    accurately reflect its transition from the enterprise version to the
    community version.
    
    task-3282988
    
    closes odoo/odoo#139151
    
    Related: odoo/upgrade#5301
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website_project/__manifest__.py
+++ addons/website_project/__manifest__.py
@@ -7,11 +7,11 @@
     'summary': 'Add a task suggestion form to your website',
     'version': '1.0',
     'description': """
-Generate tasks in Project app from a form published on your website. This module requires the use of the *Form Builder* module (available in Odoo Enterprise) in order to build the form.
+Generate tasks in Project app from a form published on your website. This module requires the use of the *Form Builder* module in order to build the form.
     """,
     'depends': ['website', 'project'],
     'data': [
-        'data/website_form_project_data.xml',
+        'data/website_project_data.xml',
         'views/project_portal_project_task_template.xml',
         'views/project_portal_project_project_template.xml',
         ],
@@ -19,7 +19,7 @@ Generate tasks in Project app from a form published on your website. This module
     'auto_install': True,
     'assets': {
         'website.assets_wysiwyg': [
-            'website_form_project/static/src/js/website_form_project_editor.js',
+            'website_project/static/src/js/website_project_editor.js',
         ],
         'project.webclient': [
             # In website, there is a patch of the LinkDialog (see
