ac2746837809ce8065b373d3eed41e3ab2dfff51
sameerz09 <sameerz09@hotmail.com>
2023-10-19 20:07:49 +0300

    Fixing Problems with Balance and General Ledger Reports

--- addons/movments_accounts_balances/controllers/controllers.py
+++ addons/movments_accounts_balances/controllers/controllers.py
@@ -13,48 +13,52 @@ class AccountBalance(models.Model):
         return account_data
 
     @api.model
-    def getbalance(self, start_date, end_date, selected_account_id):
-        results = []
+    def getbalance(self, account_id, start_date, end_date):
+        # Define search criteria to filter account move lines
+        domain = [
+            ('account_id', '=', account_id),
+        ]
 
-        try:
-            # Convert the selected account ID to an integer
-            selected_account_id = int(selected_account_id)
+        if start_date and end_date:
+            domain.extend([
+                ('date', '>=', start_date),
+                ('date', '<=', end_date)
+            ])
+        else:
+            domain.append(('date', '<=', end_date))
 
-            # Retrieve the selected account based on the provided account ID
-            selected_account = self.env['account.account'].browse(selected_account_id)
+        # Retrieve account move lines based on the criteria
+        move_lines = self.env['account.move.line'].search(domain)
 
-            if selected_account:
-                query_params = [selected_account_id, end_date]
+        # Prepare a list to store ledger data
+        ledger_data = []
 
-                query = """
-                            SELECT a.id AS account_id, a.name AS account_name, a.root_id AS root_id,
-                                   l.date AS date, COALESCE(SUM(debit - credit), 0) AS balance
-                            FROM account_move_line AS l
-                            LEFT JOIN account_account AS a ON l.account_id = a.id
-                            WHERE l.account_id = %s AND l.date <= %s
-                            """
+        # Iterate through the retrieved move lines and assemble ledger data
+        for line in move_lines:
+            ledger_data.append({
+                'date': line.date,
+                'debit': line.debit,
+                'credit': line.credit,
+                'balance': line.balance,
+                'account_root_id': line.account_root_id.id
 
-                # Check if start_date is not provided or is an empty string
-                if start_date and start_date != "":
-                    query += " AND l.date >= %s"
-                    query_params.append(start_date)
+            })
 
-                query += " GROUP BY a.id, a.name, a.root_id, l.date"
-                query += " ORDER BY l.date DESC"  # Order results by date in descending order
+        # Retrieve account information for the specified account_id
+        account_info = self.env['account.account'].browse(account_id)
 
-                self.env.cr.execute(query, query_params)
-                results = self.env.cr.dictfetchall()
+        # Create a dictionary to combine account information and ledger data
+        general_ledger = {
+            'account_info': {
+                'id': account_info.id,
+                'name': account_info.name,
+                # You can add more account details here as needed
+            },
+            'ledger_data': ledger_data,
+        }
 
-        except ValueError as e:
-            # Handle conversion error
-            error_message = "Error converting selected_account_id to an integer: " + str(e)
-            print("Error:", error_message)
-        except Exception as e:
-            # Handle other exceptions
-            error_message = str(e)
-            print("Error:", error_message)
-
-        return results
+        # Return the general ledger data
+        return general_ledger
 
     @api.model
     def general_ledger_report(self, account_id, start_date, end_date):
@@ -68,10 +72,7 @@ class AccountBalance(models.Model):
                 ('date', '>=', start_date),
                 ('date', '<=', end_date)
             ])
-        # else:
-        # domain.append(('date', '<=', end_date))
 
-        # Retrieve account move lines based on the criteria
         move_lines = self.env['account.move.line'].search(domain)
 
         # Prepare a list to store ledger data
