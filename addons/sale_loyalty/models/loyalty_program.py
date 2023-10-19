3bd5eafcaa0cd1950673c868bc49465884dc571a
“moda” <moda@odoo.com>
2023-10-19 12:46:05 +0000

    [FIX] point_of_sale: access right point_of_sale pos_user
    Previously, when the user pos_user only had access rights to point of
    sale in "user", when he tried to invoice an error was raised because
    he didn't have access to account.move and account.move.line.
    
    We have added rules to correct this problem for future upgrades.
    
    X-original-commit: df14810ad472795a621e91bde377d65d0afff837

--- addons/sale_loyalty/models/loyalty_program.py
+++ addons/sale_loyalty/models/loyalty_program.py
@@ -12,8 +12,9 @@ class LoyaltyProgram(models.Model):
 
     def _compute_order_count(self):
         # An order should count only once PER program but may appear in multiple programs
-        read_group_res = self.env['sale.order.line']._read_group(
-            [('reward_id', 'in', self.reward_ids.ids)], ['order_id'], ['reward_id:array_agg'])
+        read_group_res = self.env['sale.order.line'].sudo()._read_group(
+            [('reward_id', 'in', self.reward_ids.ids)], ['reward_id:array_agg'], ['order_id'])
+
         for program in self:
             program_reward_ids = program.reward_ids.ids
             program.order_count = sum(
