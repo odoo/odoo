70535d3e409886a460b32528e297dc2b9676ca28
Sébastien Theys <seb@odoo.com>
2023-10-19 19:04:15 +0200

    [FIX] base, mail: correct populate of base models
    Significantly improves the usability and the result of the populate for
    base models and channel/member/message.
    
    - make populate of base models re-entrant
    - adapt size to values that make sense:
      - small shows something relevant, and can be quickly ran several times
      - medium is fast enough to be usuable but big enough to highlight
        performance issues
      - large is... still not to be attempted
    - avoid conflicting conditions that either make no sense or can lead to
      crashes when populating the data or running the database
    - better spread of data in channel/member/messages
    
    X-original-commit: 73a5e322f6e250569ee1942d678f8a4f87616bd7

--- addons/mail/populate/discuss/mail_message.py
+++ addons/mail/populate/discuss/mail_message.py
@@ -1,26 +1,28 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import random
+
 from odoo import models
 from odoo.addons.mail.populate.mail_message import Message as MailMessage
-from odoo.tools import populate
 
 
 class Message(models.Model):
     _inherit = "mail.message"
     _populate_dependencies = ["discuss.channel", "discuss.channel.member"] + MailMessage._populate_dependencies
 
-    def _populate_factories(self):
-        channel_ids = self.env.registry.populated_models["discuss.channel"]
-
-        def get_author_id(values, counter, random):
-            channel = self.env[values["model"]].browse(values["res_id"])
-            channel_member_ids = [channel["id"] for channel in channel.channel_member_ids.read(["id"])]
-            return random.choice(channel_member_ids)
-
-        return [
-            ("body", populate.constant("message_body_{counter}")),
-            ("message_type", populate.constant("comment")),
-            ("model", populate.constant("discuss.channel")),
-            ("res_id", populate.randomize(channel_ids)),
-            ("author_id", populate.compute(get_author_id)),
-        ]
+    def _populate(self, size):
+        res = super()._populate(size)
+        channels = self.env["discuss.channel"].browse(self.env.registry.populated_models["discuss.channel"])
+        messages = []
+        for channel in channels.filtered(lambda channel: channel.channel_member_ids):
+            for counter in range(random.randrange({"small": 100, "medium": 1000, "large": 10000}[size])):
+                messages.append(
+                    {
+                        "body": f"message_body_{counter}",
+                        "message_type": "comment",
+                        "model": "discuss.channel",
+                        "res_id": channel.id,
+                        "author_id": random.choice(channel.channel_member_ids.partner_id).id,
+                    }
+                )
+        return res + self.env["mail.message"].create(messages)
