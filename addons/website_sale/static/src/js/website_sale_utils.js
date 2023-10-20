b51e3e60e44ef7bd91d23fee650ce11b67108643
vava-odoo <vava@odoo.com>
2023-10-20 13:29:34 +0200

    [IMP] website_sale: display warning in notifications
    When adding items to the cart, the server returns warnings that are
    never displayed to the customer. This commit makes use of the new
    notification system of website_sale to display it.
    
    task-3502911
    
    X-original-commit: bfe857db1dd5408af57f46aa51eae8be82a4c6f9
    Part-of: odoo/odoo#142037

--- addons/website_sale/static/src/js/website_sale_utils.js
+++ addons/website_sale/static/src/js/website_sale_utils.js
@@ -35,8 +35,8 @@ export const cartHandlerMixin = {
         });
         if (data.cart_quantity && (data.cart_quantity !== parseInt($(".my_cart_quantity").text()))) {
             updateCartNavBar(data);
-            showCartNotification(this.call.bind(this), data.notification_info);
         };
+        showCartNotification(this.call.bind(this), data.notification_info);
         return data;
     },
 };
@@ -119,7 +119,14 @@ function showCartNotification(callService, props, options = {}) {
     // Show the notification about the cart
     if (props.lines) {
         callService("cartNotificationService", "add", "Item(s) added to your cart", {
-            ...props,
+            lines: props.lines,
+            currency_id: props.currency_id,
+            ...options,
+        });
+    }
+    if (props.warning) {
+        callService("cartNotificationService", "add", "Warning", {
+            warning: props.warning,
             ...options,
         });
     }
