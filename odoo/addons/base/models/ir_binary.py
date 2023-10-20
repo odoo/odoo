24a3111f874ec82f1047493626483e69910c1948
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2023-10-20 11:35:50 +0000

    [FIX] base: skip etag when attachment checksum is missing
    png images not shown on ir.attachment kanban
    
    Attachment that use db_datas have no checksum by default, which is used
    to compute the stream's http ETag. Skip updating the ETag when it is
    missing and determine freshness using the Last-Modified header instead.
    
    closes odoo/odoo#139498
    
    X-original-commit: 2d43bbae72fddf17e7d9045e90dbc070b2f57a19
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- odoo/addons/base/models/ir_binary.py
+++ odoo/addons/base/models/ir_binary.py
@@ -220,13 +220,14 @@ class IrBinary(models.AbstractModel):
 
         if (width, height) == (0, 0):
             width, height = image_guess_size_from_field_name(field_name)
-        stream.etag += f'-{width}x{height}-crop={crop}-quality={quality}'
 
+        if isinstance(stream.etag, str):
+            stream.etag += f'-{width}x{height}-crop={crop}-quality={quality}'
         if isinstance(stream.last_modified, (int, float)):
             stream.last_modified = datetime.utcfromtimestamp(stream.last_modified)
         modified = werkzeug.http.is_resource_modified(
             request.httprequest.environ,
-            etag=stream.etag,
+            etag=stream.etag if isinstance(stream.etag, str) else None,
             last_modified=stream.last_modified
         )
 
