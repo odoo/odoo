85372ffa9426e2ef52844a733d84f36bf4a5ca72
Aaron Bohy <aab@odoo.com>
2023-10-20 12:04:09 +0200

    [REF] web,*: remove last legacy stuff from several bundles
    This commit removes the last low level legacy stuff (e.g. Widget,
    mixins...) from the backend bundle, and from the webclient bundles
    of mrp_subcontracting and project. They are no longer used in the
    backend. There're still necessary for the frontend and for the
    web_editor though, so we had to manually add them in the lazy
    loaded bundle of the editor. Hopefully, the last widgets and
    dialogs will be converted soon, and we'll finally get rid of all
    those legacy files.
    
    Part-of: odoo/odoo#139154

--- addons/mrp_subcontracting/__manifest__.py
+++ addons/mrp_subcontracting/__manifest__.py
@@ -54,7 +54,6 @@
             'web/static/src/scss/mimetypes.scss',
             'web/static/src/scss/ui.scss',
             'web/static/src/legacy/scss/ui.scss',
-            'web/static/src/legacy/scss/modal.scss',
             'web/static/src/views/fields/translation_dialog.scss',
             'web/static/src/scss/fontawesome_overridden.scss',
 
