d05a7a6e1dc119a6e684e093a557f0c5cf6b27cc
David Monjoie <dmo@odoo.com>
2023-10-20 13:32:50 +0200

    [FIX] web_editor: remove lingering debug code

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -2795,7 +2795,6 @@ export class OdooEditor extends EventTarget {
      */
     _onSelectionChange() {
         const selection = this.document.getSelection();
-        console.log(selection);
         // When CTRL+A in the editor, sometimes the browser use the editable
         // element as an anchor & focus node. This is an issue for the commands
         // and the toolbar so we need to fix the selection to be based on the
