e4d3d8e4818bd77afbac5c17e4a7a85cfff48882
Nicolas Viseur (vin) <vin@odoo.com>
2023-10-20 09:28:54 +0800

    [REF] l10n_sk: Demo Data Cleanup
    Up until now, we did not have any common demo data package for localizations.
    This is an issue for localizations having both accounting and hr packages,
    because these do not depend on each others.
    It means that to have complete demo data individually, they need to define
    demo company in both cases.
    
    This leads to duplication in demo companies for a single country when both
    packages are installed, which is not ideal for testing as it also means that
    demo data is split between multiple companies.
    
    l10n_sk as it is known today becomes l10n_sk_account (as it was all along
    an invoicing module).
    l10n_sk is instead a new module containing the common demo data.
    
    task id # 3555251

--- addons/l10n_sk/__manifest__.py
+++ addons/l10n_sk/__manifest__.py
@@ -1,39 +1,15 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Slovak - Accounting',
+    'name': 'Slovakia - Demo Data',
+    'version': '1.0',
     'icon': '/account/static/description/l10n.png',
     'countries': ['sk'],
-    'version': '1.0',
-    'author': '26HOUSE (http://www.26house.com)',
-    'website': 'https://www.odoo.com/documentation/master/applications/finance/fiscal_localizations.html',
-    'category': 'Accounting/Localizations/Account Charts',
+    'category': 'Localization/Demo Data',
     'description': """
-Slovakia accounting chart and localization: Chart of Accounts 2020, basic VAT rates +
-fiscal positions.
-
-Tento modul definuje:
-• Slovenskú účtovú osnovu za rok 2020
-
-• Základné sadzby pre DPH z predaja a nákupu
-
-• Základné fiškálne pozície pre slovenskú legislatívu
-
-
-Pre viac informácií kontaktujte info@26house.com alebo navštívte https://www.26house.com.
-
+This is the base module to create demonstration data for the Slovak localization in Odoo.
+=========================================================================================
     """,
-    'depends': [
-        'base_iban',
-        'base_vat',
-        'account',
-    ],
-    'data': [
-        'views/res_partner_views.xml',
-        'views/res_company_views.xml',
-        'views/report_invoice.xml',
-        'views/account_move_views.xml',
-        'views/report_template.xml',
-    ],
+    'depends': ['base'],
     'demo': [
         'demo/demo_company.xml',
     ],
