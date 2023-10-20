6ee6b784fedddbbb43f7e49fcc7f3dcbcb31964c
Sébastien Theys <seb@odoo.com>
2023-10-20 16:42:53 +0200

    [FIX] account_edi, web, mail: add attachment delete in public tour
    This allows better coverage of the feature and in particular its access
    check.
    
    Follow-up of https://github.com/odoo/odoo/pull/118701

--- addons/account_edi/models/ir_attachment.py
+++ addons/account_edi/models/ir_attachment.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
-from odoo import api, models, fields, _
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models, _
 from odoo.exceptions import UserError
 
 
@@ -8,7 +9,8 @@ class IrAttachment(models.Model):
 
     @api.ondelete(at_uninstall=False)
     def _unlink_except_government_document(self):
-        linked_edi_documents = self.env['account.edi.document'].search([('attachment_id', 'in', self.ids)])
+        # sudo: account.edi.document - constraint that must be applied regardless of ACL
+        linked_edi_documents = self.env['account.edi.document'].sudo().search([('attachment_id', 'in', self.ids)])
         linked_edi_formats_ws = linked_edi_documents.edi_format_id.filtered(lambda edi_format: edi_format._needs_web_services())
         if linked_edi_formats_ws:
             raise UserError(_("You can't unlink an attachment being an EDI document sent to the government."))
