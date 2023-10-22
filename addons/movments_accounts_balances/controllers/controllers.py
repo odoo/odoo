8f367438958f5c7b50f7d856da8ca2b7ef7ed8de
Sugam Pandey <sugam@docyt.com>
2023-10-22 15:46:36 -0700

    Keep the methods lean

--- addons/movments_accounts_balances/controllers/controllers.py
+++ addons/movments_accounts_balances/controllers/controllers.py
@@ -13,56 +13,28 @@ class AccountBalance(models.Model):
         return account_data
 
     @api.model
-    def getbalance(self, account_id, start_date, end_date):
+    def get_balance(self, account_id, start_date, end_date):
         # Define search criteria to filter account move lines
         domain = [
             ('account_id', '=', account_id),
+            ('date', '<=', end_date)
         ]
 
-        if start_date and end_date:
-            domain.extend([
-                ('date', '>=', start_date),
-                ('date', '<=', end_date)
-            ])
-        else:
-            domain.append(('date', '<=', end_date))
-
         # Retrieve account move lines based on the criteria
-        move_lines = self.env['account.move.line'].search(domain)
-
-        # Prepare a list to store ledger data
-        ledger_data = []
-
-        # Iterate through the retrieved move lines and assemble ledger data
-        for line in move_lines:
-            ledger_data.append({
-                'date': line.date,
-                'debit': line.debit,
-                'credit': line.credit,
-                'balance': line.balance,
-                'account_root_id': line.account_root_id.id,
-                'name': line.name,
-
-            })
-
-        # Retrieve account information for the specified account_id
-        account_info = self.env['account.account'].browse(account_id)
-
-        # Create a dictionary to combine account information and ledger data
-        general_ledger = {
-            'account_info': {
-                'id': account_info.id,
-                'name': account_info.name,
-                # You can add more account details here as needed
-            },
-            'ledger_data': ledger_data,
-        }
-
-        # Return the general ledger data
-        return general_ledger
+        move_lines = self.env['account.move.line'].search(domain, order='date DESC', limit=1)
+    
+        balance_info = {}
+        if move_lines:
+            move_line = move_lines[0]
+            balance_info = {
+                'date': move_line.date,
+                'balance': move_line.balance
+            }
+        
+        return balance_info
 
     @api.model
-    def general_ledger_report(self, account_id, start_date, end_date):
+    def get_general_ledger(self, account_id, start_date, end_date):
         # Define search criteria to filter account move lines
         domain = [
             ('account_id', '=', account_id),
@@ -87,40 +59,25 @@ class AccountBalance(models.Model):
                 # Get the first analytic move ID if available
                 analytic_move_id = analytic_account_info[0].id
                 analytic_amount = analytic_account_info[0].amount
-                analytic_description = analytic_account_info[0].name
+                analytic_name = analytic_account_info[0].name
             else:
-                analytic_move_id = 'Analytic Inactive.'
-                analytic_amount = 'Analytic Inactive.'
-                analytic_description = 'Analytic Inactive.'
+                analytic_move_id = None
+                analytic_amount = None
+                analytic_name = None
 
             ledger_data.append({
                 'id': line.id,
                 'date': line.date,
                 'debit': line.debit,
                 'credit': line.credit,
-                'balance': line.balance,
-                'account_root_id': line.account_root_id.id,
                 'analytic_move_id': analytic_move_id,
                 'analytic_amount': analytic_amount,
-                'analytic_description': analytic_description,
+                'analytic_name': analytic_name
 
             })
 
-        # Retrieve account information for the specified account_id
-        account_info = self.env['account.account'].browse(account_id)
-
-        # Create a dictionary to combine account information and ledger data
-        general_ledger = {
-            'account_info': {
-                'id': account_info.id,
-                'name': account_info.name,
-                # You can add more account details here as needed
-            },
-            'ledger_data': ledger_data,
-        }
-
         # Return the general ledger data
-        return general_ledger
+        return ledger_data
 
 
 
