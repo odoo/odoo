6f3bd59d8ac8b19859a7c548bb1b1fc32b19be9b
Sugam Pandey <sugam@docyt.com>
2023-10-22 16:20:46 -0700

    Keep the methods lean

--- addons/movments_accounts_balances/controllers/controllers.py
+++ addons/movments_accounts_balances/controllers/controllers.py
@@ -35,36 +35,24 @@ class AccountBalance(models.Model):
 
     @api.model
     def get_general_ledger(self, account_id, start_date, end_date):
-        # Define search criteria to filter account move lines
-        domain = [
-            ('account_id', '=', account_id),
-        ]
-
+        domain = [('account_id', '=', account_id)]
+    
         if start_date and end_date:
             domain.extend([
                 ('date', '>=', start_date),
                 ('date', '<=', end_date)
             ])
-
+        
         move_lines = self.env['account.move.line'].search(domain)
-
-        # Prepare a list to store ledger data
         ledger_data = []
-
-        # Iterate through the retrieved move lines and assemble ledger data
+    
         for line in move_lines:
-            move_line_id = line.id
-            analytic_account_info = self.env['account.analytic.line'].search([('move_line_id', '=', move_line_id)])
-            if analytic_account_info:
-                # Get the first analytic move ID if available
-                analytic_move_id = analytic_account_info[0].id
-                analytic_amount = analytic_account_info[0].amount
-                analytic_name = analytic_account_info[0].name
-            else:
-                analytic_move_id = None
-                analytic_amount = None
-                analytic_name = None
-
+            analytic_info = self.env['account.analytic.line'].search([('move_line_id', '=', line.id)], limit=1)
+        
+            analytic_move_id = analytic_info.id if analytic_info else None
+            analytic_amount = analytic_info.amount if analytic_info else None
+            analytic_name = analytic_info.name if analytic_info else None
+        
             ledger_data.append({
                 'id': line.id,
                 'date': line.date,
@@ -73,11 +61,5 @@ class AccountBalance(models.Model):
                 'analytic_move_id': analytic_move_id,
                 'analytic_amount': analytic_amount,
                 'analytic_name': analytic_name
-
             })
-
-        # Return the general ledger data
         return ledger_data
-
-
-
