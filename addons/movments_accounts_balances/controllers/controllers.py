9a4b2cc2af354f6b0d7e7211ac16e0f727f7dae9
sameerz09 <sameerz09@hotmail.com>
2023-10-23 07:36:02 +0300

    Optimized the code.

--- addons/movments_accounts_balances/controllers/controllers.py
+++ addons/movments_accounts_balances/controllers/controllers.py
@@ -1,5 +1,6 @@
-from odoo import models, api
+from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+
 
 class AccountBalance(models.Model):
     _inherit = 'account.account'
@@ -13,60 +14,29 @@ class AccountBalance(models.Model):
         return account_data
 
     @api.model
-    def getbalance(self, account_id, start_date, end_date):
+    def get_balance(self, account_id, end_date):
         # Define search criteria to filter account move lines
-        domain = [
-            ('account_id', '=', account_id),
-        ]
+        domain = [('account_id', '=', account_id)]
 
-        if start_date and end_date:
-            domain.extend([
-                ('date', '>=', start_date),
-                ('date', '<=', end_date)
-            ])
-        else:
+        if end_date:
             domain.append(('date', '<=', end_date))
 
-        # Retrieve account move lines based on the criteria
-        move_lines = self.env['account.move.line'].search(domain)
+        # Retrieve the most recent account move line based on the criteria
+        move_line = self.env['account.move.line'].search(domain, order='date DESC', limit=1)
 
-        # Prepare a list to store ledger data
-        ledger_data = []
-
-        # Iterate through the retrieved move lines and assemble ledger data
-        for line in move_lines:
-            ledger_data.append({
-                'date': line.date,
-                'debit': line.debit,
-                'credit': line.credit,
-                'balance': line.balance,
-                'account_root_id': line.account_root_id.id,
-                'name': line.name,
-
-            })
-
-        # Retrieve account information for the specified account_id
-        account_info = self.env['account.account'].browse(account_id)
-
-        # Create a dictionary to combine account information and ledger data
-        general_ledger = {
-            'account_info': {
-                'id': account_info.id,
-                'name': account_info.name,
-                # You can add more account details here as needed
-            },
-            'ledger_data': ledger_data,
-        }
+        # Prepare ledger data or return an empty list if no move lines are found
+        balance_info = [{
+            'name': move_line.name,
+            'date': move_line.date,
+            'balance': move_line.balance,
+        }] if move_line else []
 
         # Return the general ledger data
-        return general_ledger
+        return {'balance_info': balance_info}
 
     @api.model
     def general_ledger_report(self, account_id, start_date, end_date):
-        # Define search criteria to filter account move lines
-        domain = [
-            ('account_id', '=', account_id),
-        ]
+        domain = [('account_id', '=', account_id)]
 
         if start_date and end_date:
             domain.extend([
@@ -76,51 +46,27 @@ class AccountBalance(models.Model):
 
         move_lines = self.env['account.move.line'].search(domain)
 
-        # Prepare a list to store ledger data
         ledger_data = []
 
-        # Iterate through the retrieved move lines and assemble ledger data
         for line in move_lines:
-            move_line_id = line.id
-            analytic_account_info = self.env['account.analytic.line'].search([('move_line_id', '=', move_line_id)])
-            if analytic_account_info:
-                # Get the first analytic move ID if available
-                analytic_move_id = analytic_account_info[0].id
-                analytic_amount = analytic_account_info[0].amount
-                analytic_description = analytic_account_info[0].name
-            else:
-                analytic_move_id = 'Analytic Inactive.'
-                analytic_amount = 'Analytic Inactive.'
-                analytic_description = 'Analytic Inactive.'
+            analytic_info = self.env['account.analytic.line'].search([('move_line_id', '=', line.id)], limit=1)
 
             ledger_data.append({
-                'id': line.id,
                 'date': line.date,
                 'debit': line.debit,
                 'credit': line.credit,
-                'balance': line.balance,
                 'account_root_id': line.account_root_id.id,
-                'analytic_move_id': analytic_move_id,
-                'analytic_amount': analytic_amount,
-                'analytic_description': analytic_description,
-
+                'analytic_move_id': analytic_info.id if analytic_info else '',
+                'analytic_amount': analytic_info.amount if analytic_info else '',
+                'analytic_description': analytic_info.name if analytic_info else ''
             })
 
-        # Retrieve account information for the specified account_id
-        account_info = self.env['account.account'].browse(account_id)
-
-        # Create a dictionary to combine account information and ledger data
-        general_ledger = {
+        return {
             'account_info': {
-                'id': account_info.id,
-                'name': account_info.name,
-                # You can add more account details here as needed
+                'id': account_id,
+                'name': self.env['account.account'].browse(account_id).name,
+                # Add more account details as needed
             },
-            'ledger_data': ledger_data,
+            'ledger_data': ledger_data
         }
-
-        # Return the general ledger data
-        return general_ledger
-
-
 
