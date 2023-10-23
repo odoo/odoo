e2bc56a6570da2cb141660d5bc85aa3ba2dd69a4
roen-odoo <roen@odoo.com>
2023-10-23 12:04:18 +0000

    [FIX] point_of_sale,l10n_be_pos_sale: invoice settled order
    Current behavior:
    ?In Belgium? when using a EU intracom tax, as we need to report those
    special sales to the government we need to create an invoice as we need
    to report the customer name, VAT number and the country where the goods
    are going to end up
    
    Steps to reproduce:
    - Create an order with a product and a tax (0% EU M)
    - Settle the order in the PoS and pay it (no invoice)
    - Go to the accounting and check the EC Sales List report
    - There is an error shown in the report
    
    opw-3514737
    
    X-original-commit: bea9ab596950967873766434787e705ccfe04839

--- addons/point_of_sale/static/tests/tours/helpers/PaymentScreenTourMethods.js
+++ addons/point_of_sale/static/tests/tours/helpers/PaymentScreenTourMethods.js
@@ -218,6 +218,15 @@ class Check {
             },
         ];
     }
+    isInvoiceButtonChecked() {
+        return [
+            {
+                content: 'check invoice button is checked',
+                trigger: '.js_invoice.highlight',
+                run: () => {},
+            }
+        ]
+    }
 }
 
 class Execute {
