d46f2007d0e2e500fefe33469855197d1febf4dc
roen-odoo <roen@odoo.com>
2023-10-23 12:04:18 +0000

    [FIX] point_of_sale,l10n_be_pos_sale: invoice settled order
    Current behavior:
    ?In Belgium? when using a EU intracom tax, as we need to report those
    special sales to the government we need to create an invoice as we need
    to report the customer name, VAT number and the country where the goods
    are going to end up
    
    Steps to reproduce:
    - Create an order with a product and a tax (0% EU M)
    - Settle the order in the PoS and pay it (no invoice)
    - Go to the accounting and check the EC Sales List report
    - There is an error shown in the report
    
    opw-3514737
    
    closes odoo/odoo#149201
    
    X-original-commit: bea9ab596950967873766434787e705ccfe04839
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

---
+++ addons/l10n_be_pos_sale/static/src/js/models.js
@@ -0,0 +1,14 @@
+/** @odoo-module **/
+
+import { patch } from "@web/core/utils/patch";
+import { Order } from "@point_of_sale/js/models";
+
+patch(Order.prototype, "l10n_be_pos_sale.Order", {
+    async pay() {
+        const has_origin_order = this.get_orderlines().some(line => line.sale_order_origin_id);
+        if (this.pos.company.country && this.pos.company.country.code === "BE" && has_origin_order) {
+            this.to_invoice = true;
+        }
+        return this._super(...arguments);
+    }
+});
