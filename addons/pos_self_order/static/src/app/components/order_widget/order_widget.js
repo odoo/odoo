11e36a6cc1ab51e18b746ce1fa8779105a303543
“moda” <moda@odoo.com>
2023-10-24 10:17:08 +0200

    [IMP] point_of_sale,pos_*: automaticaly paid order when amount is 0
    *: pos_online_payment_self_order,pos_restaurant,pos_self_order
    
    Previously, when self order was set to "pay after each" and a user
    ordered an order with a total of $0.00, the order was not automatically
    set to paid status, as is the case with point_of_sale.
    
    Now, when the self order is set to "pay after each" and an order with a
    total of $0.00 is placed, it will be automatically paid.
    
    Minor fixes:
    - A props has been removed from `ReceiptHeader` because it was causing
    an error in debug mode.
    - In point_of_sale, when an order was placed with a total of $0.00, an
    error was raised during validation because no payment method was
    selected. Now, before checking this payment method, we first check
    whether it exists.

--- addons/pos_self_order/static/src/app/components/order_widget/order_widget.js
+++ addons/pos_self_order/static/src/app/components/order_widget/order_widget.js
@@ -58,6 +58,10 @@ export class OrderWidget extends Component {
             disabled = !kioskPayment && !isMobilePayment;
         }
 
+        if (this.selfOrder.currentOrder.amount_total === 0) {
+            label = _t("Order");
+        }
+
         return { label, disabled };
     }
 
