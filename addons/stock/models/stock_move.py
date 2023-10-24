655c408d319a14cc9b972d4f49816c91fb502b22
Adrien Widart (awt) <awt@odoo.com>
2023-10-24 11:43:10 +0000

    [FIX] {purchase_,}stock, mrp: merge SM with `self` only
    Same case as [1] but suppose the over-consumption is on the second
    raw SM. Then, in the loop:
    https://github.com/odoo/odoo/blob/165f8881e2850b4e2087bce77692965843609363/addons/stock/models/stock_move.py#L1705-L1713
    On the first iteration, everything is ok, we add the SM to
    `moves_ids_todo`. Second iteration, there is an extra move, it will
    lead to the merge of both initial SM (see [1] for more details).
    Therefore, `moves_ids_todo` now contains a deleted SM: it will lead
    to the same error ("Record has been deleted') when calling
    `_check_company`.
    
    Commit [1] is then too naive and can be reverted. We should rather
    avoid "extra merge": when confirming a stock move SM01, we should avoid
    the merge of SM02 and SM03 as side effect.
    
    [1] b870ffa65410435237a172a491ed1454957ce28a
    
    OPW-3432293
    
    closes odoo/odoo#140821
    
    X-original-commit: 1376cbf97185c837e35a4766db4cb8d439f29715
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Adrien Widart (awt) <awt@odoo.com>

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -1069,7 +1069,7 @@ Please change the quantity done or the rounding precision of your unit of measur
             for __, g in groupby(candidate_moves, key=itemgetter(*distinct_fields)):
                 moves = self.env['stock.move'].concat(*g)
                 # Merge all positive moves together
-                if len(moves) > 1:
+                if len(moves) > 1 and any(m in self for m in moves):
                     # link all move lines to record 0 (the one we will keep).
                     moves.mapped('move_line_ids').write({'move_id': moves[0].id})
                     # merge move data
@@ -1077,8 +1077,9 @@ Please change the quantity done or the rounding precision of your unit of measur
                     # update merged moves dicts
                     moves_to_unlink |= moves[1:]
                     merged_moves |= moves[0]
-                # Add the now single positive move to its limited key record
-                moves_by_neg_key[neg_key(moves[0])] |= moves[0]
+                    moves = moves[0]
+                for m in moves:
+                    moves_by_neg_key[neg_key(m)] |= m
 
         for neg_move in neg_qty_moves:
             # Check all the candidates that matches the same limited key, and adjust their quantities to absorb negative moves
