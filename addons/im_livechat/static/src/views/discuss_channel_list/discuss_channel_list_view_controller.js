39ed4ec73ed64ebd661ac8605a71e958f578121c
Killian Frappart <kfr@odoo.com>
2023-10-24 13:21:15 +0200

    [FIX] mail, im_livechat: Render thread inside ChatWindow for small UI The mobile layout of Discuss does not support most of the thread actions which produce a terrible user experience.
    Also, for consistency purpose, ChatWindow is the preferred display on
    mobile in Discuss and Livechat should behave similarly.
    
    Task-3497801

--- addons/im_livechat/static/src/views/discuss_channel_list/discuss_channel_list_view_controller.js
+++ addons/im_livechat/static/src/views/discuss_channel_list/discuss_channel_list_view_controller.js
@@ -1,17 +1,34 @@
 /* @odoo-module */
 
-import { _t } from "@web/core/l10n/translation";
+import { useState } from "@odoo/owl";
+
 import { ListController } from "@web/views/list/list_controller";
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
 
 export class DiscussChannelListController extends ListController {
     setup() {
         super.setup(...arguments);
+        this.threadService = useService("mail.thread");
+        this.store = useState(useService("mail.store"));
+        this.ui = useState(useService("ui"));
     }
 
     async openRecord(record) {
-        this.actionService.doAction("mail.action_discuss", {
-            name: _t("Discuss"),
-            additionalContext: { active_id: record.resId },
-        });
+        if (!this.ui.isSmall) {
+            this.actionService.doAction("mail.action_discuss", {
+                name: _t("Discuss"),
+                additionalContext: { active_id: record.resId },
+            });
+        } else {
+            let thread = this.store.Thread.get({
+                model: "discuss.channel",
+                id: record.resId,
+            });
+            if (!thread) {
+                thread = await this.threadService.fetchChannel(record.resId);
+            }
+            this.threadService.open(thread);
+        }
     }
 }
