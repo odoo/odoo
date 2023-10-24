0ded9af389d341531f0750e6d3faff152b0ec19c
Antoine Guenet <age@odoo.com>
2023-10-24 11:23:25 +0200

    [FIX] web_editor: ensure observer is always disabled for link zws
    To prevent history steps that include the link zws, we need to ensure
    that the observer is always disabled when the link zws is inserted or
    removed.
    
    Due to these changes, we need to initialize `_currentStep` before
    setting the original link zws, so that `observerApply` can access it in
    the process of pausing the mutations observer.
    
    X-original-commit: 2f1786c5d234fcc2f418cafd285d8cf4ecdc5020
    Part-of: odoo/odoo#141599

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -341,6 +341,11 @@ export class OdooEditor extends EventTarget {
         // Set contenteditable before clone as FF updates the content at this point.
         this._activateContenteditable();
 
+        this._currentStep = {
+            selection: {},
+            mutations: [],
+        };
+
         this._setLinkZws();
 
         this._collabClientId = this.options.collaborationClientId;
@@ -1284,7 +1289,7 @@ export class OdooEditor extends EventTarget {
         if (!this._historyStepsActive) {
             return;
         }
-        this._setLinkZws();
+        this._resetLinkZws();
         this.sanitize();
         // check that not two unBreakables modified
         if (this._toRollback) {
@@ -1307,6 +1312,7 @@ export class OdooEditor extends EventTarget {
         if (this.options.onHistoryStep) {
             this.options.onHistoryStep(currentStep);
         }
+        this._setLinkZws();
         this._currentStep = {
             selection: {},
             mutations: [],
@@ -2025,15 +2031,14 @@ export class OdooEditor extends EventTarget {
                         link.firstElementChild.nodeName === 'IMG'
                     )
                 ) {
-                    link.prepend(this._createLinkZws('start'));
+                    this._insertLinkZws('start', link);
                     // Only add the ZWS at the end if the link is in selection.
                     if (link === linkInSelection) {
-                        link.append(this._createLinkZws('end'));
+                        this._insertLinkZws('end', link);
                         link.classList.add('o_link_in_selection');
                         didAddZwsInLinkInSelection = true;
                     }
-                    const zwsAfter = this._createLinkZws('after');
-                    link.after(zwsAfter);
+                    const zwsAfter = this._insertLinkZws('after', link);
                     if (!zwsAfter.parentElement || !zwsAfter.parentElement.isContentEditable) {
                         zwsAfter.remove();
                     }
@@ -2526,8 +2531,10 @@ export class OdooEditor extends EventTarget {
         }
     }
     _resetLinkZws(element = this.editable) {
+        this.observerUnactive('_resetLinkZws');
         element.querySelectorAll('[data-o-link-zws]').forEach(zws => zws.remove());
         element.querySelectorAll('.o_link_in_selection').forEach(link => link.classList.remove('o_link_in_selection'));
+        this.observerActive('_resetLinkZws');
     }
     _activateContenteditable() {
         this.observerUnactive('_activateContenteditable');
@@ -3494,13 +3501,22 @@ export class OdooEditor extends EventTarget {
         }
         this.observer.takeRecords();
     }
-    _createLinkZws(side) {
+    _insertLinkZws(side, link) {
+        this.observerUnactive('_insertLinkZws');
         const span = document.createElement('span');
         span.setAttribute('data-o-link-zws', side);
         if (side !== 'end') {
             span.setAttribute('contenteditable', 'false');
         }
         span.textContent = '\u200B';
+        if (side === 'start') {
+            link.prepend(span);
+        } else if (side === 'end') {
+            link.append(span);
+        } else if (side === 'after') {
+            link.after(span);
+        }
+        this.observerActive('_insertLinkZws');
         return span;
     }
 
@@ -4050,8 +4066,7 @@ export class OdooEditor extends EventTarget {
                 if (isAtEndOfLink) {
                     let afterZws = link.nextElementSibling;
                     if (!afterZws) {
-                        afterZws = this._createLinkZws('after');
-                        link.after(afterZws);
+                        afterZws = this._insertLinkZws('after', link);
                     }
                     setSelection(
                         afterZws.nextSibling || afterZws.parentElement,
