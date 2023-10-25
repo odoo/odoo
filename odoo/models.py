193f889744c48f551a348f6472e3e83ba12e4a15
VAN BOSSUYT Nicolas <nivb@odoo.com>
2023-10-25 13:30:43 +0200

    [IMP] base: introduce _search_display_name.
    This new function return a domain instead of ids which enable us new optimisation when applying an ilike on one2many's.
    
    For exemple given the following domain [('age', '>', '16'), ('bars', 'ilike', "cool%")] given that bars is a one2many before  this search had to be done in two query a first one doing a name_search on the one2many and a second one checking if there is any match for bar and if age is over 16

--- odoo/models.py
+++ odoo/models.py
@@ -1689,6 +1689,24 @@ class BaseModel(metaclass=MetaModel):
             for record in self:
                 record.display_name = f"{record._name},{record.id}"
 
+    def _search_display_name(self, name, operator='ilike'):
+        """
+        Returns a domain that matches records whose display name matches the
+        given ``name`` pattern when compared with the given ``operator``.
+
+        This method is used to implement the search on the ``display_name``
+        field, and can be overridden to change the search criteria.
+        """
+        if not self._rec_name and not self._rec_names_search:
+            _logger.error("Cannot execute name_search, no _rec_name or _rec_names_search defined on %s", self._name)
+
+        aggregator = expression.OR
+        if operator in expression.NEGATIVE_TERM_OPERATORS:
+            aggregator = expression.AND
+
+        search_fnames = self._rec_names_search or ([self._rec_name] if self._rec_name else [])
+        return aggregator([[(field_name, operator, name)] for field_name in search_fnames])
+
     def name_get(self):
         """Returns a textual representation for the records in ``self``, with
         one item output per input record, in the same order.
@@ -1776,14 +1794,7 @@ class BaseModel(metaclass=MetaModel):
 
         No default is applied for parameters ``limit`` and ``order``.
         """
-        domain = list(domain or ())
-        search_fnames = self._rec_names_search or ([self._rec_name] if self._rec_name else [])
-        if not search_fnames:
-            _logger.warning("Cannot execute name_search, no _rec_name or _rec_names_search defined on %s", self._name)
-        # optimize out the default criterion of ``like ''`` that matches everything
-        elif not (name == '' and operator in ('like', 'ilike')):
-            aggregator = expression.AND if operator in expression.NEGATIVE_TERM_OPERATORS else expression.OR
-            domain += aggregator([[(field_name, operator, name)] for field_name in search_fnames])
+        domain = list(domain or ()) + self._search_display_name(name, operator)
         return self._search(domain, limit=limit, order=order)
 
     @api.model
