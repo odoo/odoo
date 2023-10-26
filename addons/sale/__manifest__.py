65167ce343be0627cbca8268c0fc516b5b086eb6
Pierre-Yves Dufays <pydu@odoo.com>
2023-10-26 10:02:07 +0200

    [IMP] sale: add activity plan menu
    We add the activity plan to allows sale managers to configure it.
    
    Task-3558929
    
    X-original-commit: 2fe11d4131a2b56b6b5285446f32c0c768c3a9cf

--- addons/sale/__manifest__.py
+++ addons/sale/__manifest__.py
@@ -47,6 +47,7 @@ This module contains all the common features of Sales Management and eCommerce.
         'views/account_views.xml',
         'views/crm_team_views.xml',
         'views/mail_activity_views.xml',
+        'views/mail_activity_plan_views.xml',
         'views/payment_views.xml',
         'views/product_document_views.xml',
         'views/product_packaging_views.xml',
