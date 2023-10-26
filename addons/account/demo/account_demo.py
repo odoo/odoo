78c745c84d0af411a281f3b06f7b1c904b7e3109
Josse Colpaert <jco@odoo.com>
2023-10-26 12:54:13 +0200

    [IMP] account: do not override demo product taxes already set
    This way we can more easily define products in demo or data
    that need specific taxes.
    
    We needed to filter out the taxes not belonging to this company
    as the many2many does not filter them out.
    
    Part-of: odoo/odoo#139514

--- addons/account/demo/account_demo.py
+++ addons/account/demo/account_demo.py
@@ -61,9 +61,13 @@ class AccountChartTemplate(models.AbstractModel):
     def _get_demo_data_products(self, company=False):
         prod_templates = self.env['product.product'].search(self.env['product.product']._check_company_domain(company))
         if self.env.company.account_sale_tax_id:
-            prod_templates.write({'taxes_id': [Command.link(self.env.company.account_sale_tax_id.id)]})
+            prod_templates_sale = prod_templates.filtered(
+                lambda p: not p.taxes_id.filtered_domain(p.taxes_id._check_company_domain(company)))
+            prod_templates_sale.write({'taxes_id': [Command.link(self.env.company.account_sale_tax_id.id)]})
         if self.env.company.account_purchase_tax_id:
-            prod_templates.write({'supplier_taxes_id': [Command.link(self.env.company.account_purchase_tax_id.id)]})
+            prod_templates_purchase = prod_templates.filtered(
+                lambda p: not p.supplier_taxes_id.filtered_domain(p.taxes_id._check_company_domain(company)))
+            prod_templates_purchase.write({'supplier_taxes_id': [Command.link(self.env.company.account_purchase_tax_id.id)]})
 
     @api.model
     def _get_demo_data_move(self, company=False):
