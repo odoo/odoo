22cf4b50bf702f2fd609ae95c9e7451f725beea3
Maximilien (malb) <malb@odoo.com>
2023-10-26 14:31:51 +0200

    [IMP] l10n_mx: clean up continue
    This commit will hide some fields to other companies by adding fiscal country
    codes to some model used in the mexican localization.
    
    Task: 3551098
    Part-of: odoo/odoo#140303

--- addons/l10n_mx/models/res_bank.py
+++ addons/l10n_mx/models/res_bank.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Bank(models.Model):
@@ -11,6 +11,12 @@ class Bank(models.Model):
         "ABM Code",
         help="Three-digit number assigned by the ABM to identify banking "
         "institutions (ABM is an acronym for Asociación de Bancos de México)")
+    fiscal_country_codes = fields.Char(compute="_compute_fiscal_country_codes")
+
+    @api.depends_context('allowed_company_ids')
+    def _compute_fiscal_country_codes(self):
+        for record in self:
+            record.fiscal_country_codes = ",".join(self.env.companies.mapped('account_fiscal_country_id.code'))
 
 
 class ResPartnerBank(models.Model):
@@ -19,3 +25,9 @@ class ResPartnerBank(models.Model):
     l10n_mx_edi_clabe = fields.Char(
         "CLABE", help="Standardized banking cipher for Mexico. More info "
         "wikipedia.org/wiki/CLABE")
+    fiscal_country_codes = fields.Char(compute="_compute_fiscal_country_codes")
+
+    @api.depends_context('allowed_company_ids')
+    def _compute_fiscal_country_codes(self):
+        for record in self:
+            record.fiscal_country_codes = ",".join(self.env.companies.mapped('account_fiscal_country_id.code'))
