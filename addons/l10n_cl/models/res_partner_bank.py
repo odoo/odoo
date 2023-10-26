811194702f16231c0cb7c89261ff1007bc092b11
Maximilien (malb) <malb@odoo.com>
2023-10-26 15:07:00 +0200

    [IMP] l10n_cl: clean up continue
    This commit will hide some fields to other companies by adding fiscal country
    codes to some model used in the chilian localization.
    
    closes odoo/odoo#140303
    
    Task: 3551098
    Related: odoo/enterprise#49843
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- addons/l10n_cl/models/res_partner_bank.py
+++ addons/l10n_cl/models/res_partner_bank.py
@@ -1,5 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResBank(models.Model):
@@ -7,3 +7,9 @@ class ResBank(models.Model):
     _inherit = 'res.bank'
 
     l10n_cl_sbif_code = fields.Char('Cod. SBIF', size=10)
+    fiscal_country_codes = fields.Char(compute="_compute_fiscal_country_codes")
+
+    @api.depends_context('allowed_company_ids')
+    def _compute_fiscal_country_codes(self):
+        for record in self:
+            record.fiscal_country_codes = ",".join(self.env.companies.mapped('account_fiscal_country_id.code'))
