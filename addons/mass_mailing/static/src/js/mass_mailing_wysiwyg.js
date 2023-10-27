fab70452bdad8ebb1b6b9660abb9bd757ad34cd6
Rodolpho Lima <rcdl@odoo.com>
2023-10-27 16:03:27 +0000

    [FIX] mass_mailing: switch to basic theme record
    Steps:
    - Create a draft mailing based on a theme that has the snippets menu
      (the "Event Promo" theme, for instance).
    - Create another draft mailing, this time based on the basic theme
      ("Plain Text").
    - Switch from the former to the latter using the form view pager.
    
    The editor for the basic theme record is not supposed to have a sidebar
    (snippets menu), as it uses the floating toolbar instead. But when
    switching between records like described above, the basic theme gets
    replaced by the theme of the preceding record, and the snippets menu is
    added to the UI.
    
    This happens because [1] patched the `setValue` function to handle the
    case where the element for dropping snippets (.o_mail_wrapper_td) is
    missing, in particular if it gets removed via the codeview. Such fix
    most likely had in mind the scenario where the codeview is toggled on
    and off, but not the call to `setValue` that also happens when switching
    between records via the form view pager. As a result, the html content
    of a record based on the basic theme, in which such "dropzone" is
    absent, is added as a child of the previous record's .o_mail_wrapper_td
    element. This is certainly not the desired behavior, as it ends up
    overwriting the basic theme by the one from the previous record (the
    theme is defined by a class in the .o_layout div, which is a parent of
    the .o_mail_wrapper_td element).
    
    This commit, besides fixing the described issue, takes the opportunity
    to add a comment with the presumed reasoning behind commit [1] and
    extends the test tour in order to prevent regressions.
    
    [1]: https://github.com/odoo/odoo/commit/9be2cb538f937645be4650af1031c8a1445bb48b
    
    task-3573951
    
    closes odoo/odoo#151065
    
    X-original-commit: ed4727d922107745482dbbeb068d4bd6eba788dd
    Signed-off-by: Nicolas Bayet (nby) <nby@odoo.com>
    Signed-off-by: Geelen Sébastien (sge) <sge@odoo.com>
    Signed-off-by: Antoine Guenet (age) <age@odoo.com>

--- addons/mass_mailing/static/src/js/mass_mailing_wysiwyg.js
+++ addons/mass_mailing/static/src/js/mass_mailing_wysiwyg.js
@@ -82,18 +82,6 @@ export class MassMailingWysiwyg extends Wysiwyg {
         }
     }
 
-    /**
-     * @override
-     */
-    setValue(currentValue) {
-        const initialDropZone = this.$editable[0].querySelector('.o_mail_wrapper_td');
-        const parsedHtml = new DOMParser().parseFromString(currentValue, "text/html");
-        if (initialDropZone && !parsedHtml.querySelector('.o_mail_wrapper_td')) {
-            initialDropZone.replaceChildren(...parsedHtml.body.childNodes);
-        } else {
-            super.setValue(...arguments);
-        }
-    }
     //--------------------------------------------------------------------------
     // Private
     //--------------------------------------------------------------------------
