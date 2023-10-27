edefee03110c63b152148b7599aed250dc9978df
Victor Piryns (pivi) <pivi@odoo.com>
2023-10-27 10:47:13 +0200

    [PERF] hr_expense: add missing index
    Since 81fe11fabb60de56241ea912a65f41a2d9f8538f, the field
    `expense_sheet_id` was converted from a One2many to a Many2one,
    making it a stored field.
    Since then opening the form view of an expense report can be quite
    slow (around +-1.6s) in a database with a lot of `account.move`, as
    the whole table needs to be scanned to match the `id` of the
    `expense_sheet_id`.
    We add a partial not null index on the field (since not a lot of
    `account.move` are linked to an expense) and it speeds up the read
    to 60ms.
    
    Reference:
    task-3572440
    
    closes odoo/odoo#140057
    
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/hr_expense/models/account_move.py
+++ addons/hr_expense/models/account_move.py
@@ -11,7 +11,7 @@ from odoo.tools.misc import frozendict
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    expense_sheet_id = fields.Many2one('hr.expense.sheet', ondelete='set null', copy=False)
+    expense_sheet_id = fields.Many2one('hr.expense.sheet', ondelete='set null', copy=False, index='btree_not_null')
 
     def action_open_expense_report(self):
         self.ensure_one()
