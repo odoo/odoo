641055b0448828c4e04794c7adbdb9a6bc3217eb
FrancoisGe <fge@odoo.com>
2023-10-27 15:09:32 +0200

    [FIX] web: FormStatusIndicator flickering
    Purpose:
    --------
    Before this commit, if you edit a char field with an onchange in
    an x2many, the FormStatusIndicator buttons will flicker.
    
    How to reproduce:
    -----------------
    - Go to a form view with an x2many field in list mode
    - Type a char in the char field of a record
    - The FormStatusIndicator buttons are visible
    - Click outside the char field
    - An onchange is triggered
    
    Before this commit:
        The FormStatusIndicator buttons will disappear until the onchange returns.
    
    After this commit:
        The FormStatusIndicator buttons remain visible.
    
    closes odoo/odoo#140351
    
    X-original-commit: 64037d03d0076db5f02469f8bfe6306c987a094a
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/web/static/src/views/fields/input_field_hook.js
+++ addons/web/static/src/views/fields/input_field_hook.js
@@ -53,7 +53,7 @@ export function useInputField(params) {
      * On blur, we consider the field no longer dirty, even if it were to be invalid.
      * However, if the field is invalid, the new value will not be committed to the model.
      */
-    function onChange(ev) {
+    async function onChange(ev) {
         if (isDirty) {
             isDirty = false;
             let isInvalid = false;
@@ -69,15 +69,12 @@ export function useInputField(params) {
 
             if (!isInvalid) {
                 pendingUpdate = true;
-                Promise.resolve(
-                    component.props.record.update({ [component.props.name]: val })
-                ).then(() => {
-                    pendingUpdate = false;
-                });
-                lastSetValue = ev.target.value;
-            }
+                lastSetValue = inputRef.el.value;
 
-            component.props.record.model.bus.trigger("FIELD_IS_DIRTY", isDirty);
+                await component.props.record.update({ [component.props.name]: val });
+                pendingUpdate = false;
+                component.props.record.model.bus.trigger("FIELD_IS_DIRTY", isDirty);
+            }
         }
     }
     function onKeydown(ev) {
