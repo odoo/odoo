odoo/odoo

commit ad2a1d019b43896bd9d22af64476e2e5f0a1285a

[IMP] payment(_demo, _stripe), website_sale_picking: PM improvements
1) Add default filter to list view of payment methods.
2) Raise UserError when the user activates a PM linked to
   only disabled providers.
3) Activate default PMs with the activation of the provider.
4) Disable PMs linked to only disabled providers.
5) Add multi editing to list view of PMs.

task-3557315

closes odoo/odoo#139641

Related: odoo/enterprise#49551
Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

addons/payment/models/payment_provider.py
@@ -331,15 +331,26 @@ class PaymentProvider(models.Model):
         return providers
 
     def write(self, values):
-        # Handle provider disabling.
+        # Handle provider state changes.
+        providers_by_state_change = {}
         if 'state' in values:
-            state_changed_providers = self.filtered(
-                lambda p: p.state not in ('disabled', values['state'])
-            )  # Don't handle providers being enabled or whose state is not updated.
-            state_changed_providers._handle_state_change()
+            if values['state'] == 'disabled':
+                providers_by_state_change['disabled'] = self.filtered(
+                    lambda p: p.state != 'disabled'
+                )
+            else:  # 'enabled' or 'test'
+                providers_by_state_change['activated'] = self.filtered(
+                    lambda p: p.state == 'disabled'
+                )
 
         result = super().write(values)
         self._check_required_if_provider()
+
+        if 'disabled' in providers_by_state_change:
+            providers_by_state_change['disabled']._archive_linked_tokens()
+            providers_by_state_change['disabled']._deactivate_unsupported_payment_methods()
+        elif 'activated' in providers_by_state_change:
+            providers_by_state_change['activated']._activate_default_pms()
 
         return result
 
@@ -370,12 +381,32 @@ class PaymentProvider(models.Model):
                 _("The following fields must be filled: %s", ", ".join(field_names))
             )
 
-    def _handle_state_change(self):
+    def _archive_linked_tokens(self):
         """ Archive all the payment tokens linked to the providers.
 
         :return: None
         """
         self.env['payment.token'].search([('provider_id', 'in', self.ids)]).write({'active': False})
+
+    def _deactivate_unsupported_payment_methods(self):
+        """ Deactivate payment methods linked to only disabled providers.
+
+        :return: None
+        """
+        unsupported_pms = self.payment_method_ids.filtered(
+            lambda pm: all(p.state == 'disabled' for p in pm.provider_ids)
+        )
+        (unsupported_pms + unsupported_pms.brand_ids).active = False
+
+    def _activate_default_pms(self):
+        """ Activate the default payment methods of the provider.
+
+        :return: None
+        """
+        for provider in self:
+            pm_codes = provider._get_default_payment_method_codes()
+            pms = provider.with_context(active_test=False).payment_method_ids
+            (pms + pms.brand_ids).filtered(lambda pm: pm.code in pm_codes).active = True
 
     @api.ondelete(at_uninstall=False)
     def _unlink_except_master_data(self):
@@ -656,3 +687,14 @@ class PaymentProvider(models.Model):
         """
         self.ensure_one()
         return self.code
+
+    def _get_default_payment_method_codes(self):
+        """ Return the default payment methods for this provider.
+
+        Note: self.ensure_one()
+
+        :return: The default payment method codes.
+        :rtype: list
+        """
+        self.ensure_one()
+        return []

addons/payment_demo/models/payment_provider.py
@@ -3,6 +3,8 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
+from odoo.addons.payment_demo import const
+
 
 class PaymentProvider(models.Model):
     _inherit = 'payment.provider'
@@ -36,3 +38,10 @@ class PaymentProvider(models.Model):
     def _check_provider_state(self):
         if self.filtered(lambda p: p.code == 'demo' and p.state not in ('test', 'disabled')):
             raise UserError(_("Demo providers should never be enabled."))
+
+    def _get_default_payment_method_codes(self):
+        """ Override of `payment` to return the default payment method codes. """
+        default_codes = super()._get_default_payment_method_codes()
+        if self.code != 'demo':
+            return default_codes
+        return const.DEFAULT_PAYMENT_METHOD_CODES

addons/payment_stripe/models/payment_provider.py
@@ -503,3 +503,10 @@ class PaymentProvider(models.Model):
             'payment_methods_mapping': const.PAYMENT_METHODS_MAPPING,
         }
         return json.dumps(inline_form_values)
+
+    def _get_default_payment_method_codes(self):
+        """ Override of `payment` to return the default payment method codes. """
+        default_codes = super()._get_default_payment_method_codes()
+        if self.code != 'stripe':
+            return default_codes
+        return const.DEFAULT_PAYMENT_METHODS_CODES

addons/website_sale_picking/models/payment_provider.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
-
+from odoo.addons.website_sale_picking import const
 
 class PaymentProvider(models.Model):
     _inherit = 'payment.provider'
@@ -42,3 +42,10 @@ class PaymentProvider(models.Model):
             )
 
         return compatible_providers
+
+    def _get_default_payment_method_codes(self):
+        """ Override of `payment` to return the default payment method codes. """
+        default_codes = super()._get_default_payment_method_codes()
+        if self.custom_mode != 'onsite':
+            return default_codes
+        return const.DEFAULT_PAYMENT_METHOD_CODES

addons/payment/models/payment_method.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import _, api, fields, models, Command
+from odoo import Command, _, api, fields, models
+from odoo.exceptions import UserError
 from odoo.osv import expression
 
 
@@ -164,6 +165,20 @@ class PaymentMethod(models.Model):
                     ('payment_method_id', 'in', (self + self.brand_ids).ids),
                 ])  # Fix `active_test` in the context forwarded by the view.
                 linked_tokens.active = False
+
+        # Prevent enabling a payment method if it is not linked to an enabled provider.
+        if values.get('active'):
+            for pm in self:
+                primary_pm = pm if pm.is_primary else pm.primary_payment_method_id
+                if (
+                    not primary_pm.active  # Don't bother for already enabled payment methods.
+                    and all(p.state == 'disabled' for p in primary_pm.provider_ids)
+                ):
+                    raise UserError(_(
+                        "This payment method needs a partner in crime; you should enable a payment"
+                        " provider supporting this method first."
+                    ))
+
         return super().write(values)
 
     # === BUSINESS METHODS === #

addons/payment/tests/common.py
@@ -61,11 +61,6 @@ class PaymentCommon(BaseCommon):
             'country_id': cls.country_belgium.id,
         })
 
-        # Activate pm
-        cls.env.ref('payment.payment_method_unknown').write({
-            'active': True,
-            'support_tokenization': True,
-        })
         # Create a dummy provider to allow basic tests without any specific provider implementation
         arch = """
         <form action="dummy" method="post">
@@ -90,6 +85,11 @@ class PaymentCommon(BaseCommon):
             'available_currency_ids': [Command.set(
                 (cls.currency_euro + cls.currency_usd + cls.env.company.currency_id).ids
             )],
+        })
+        # Activate pm
+        cls.env.ref('payment.payment_method_unknown').write({
+            'active': True,
+            'support_tokenization': True,
         })
 
         cls.provider = cls.dummy_provider

addons/payment/views/payment_method_views.xml
@@ -89,7 +89,7 @@
         <field name="name">payment.method.tree</field>
         <field name="model">payment.method</field>
         <field name="arch" type="xml">
-            <tree decoration-muted="not active">
+            <tree multi_edit="True" decoration-muted="not active">
                 <field name="sequence" widget="handle"/>
                 <field name="name"/>
                 <field name="active" widget="boolean_toggle"/>
@@ -130,6 +130,10 @@
         <field name="arch" type="xml">
             <search>
                 <field name="name" string="Name"/>
+                <filter name="available_pms"
+                        string="Available methods"
+                        domain="[('provider_ids.state', '!=', 'disabled')]"
+                />
             </search>
         </field>
     </record>
@@ -138,12 +142,8 @@
         <field name="name">Payment Methods</field>
         <field name="res_model">payment.method</field>
         <field name="view_mode">tree,kanban,form</field>
-        <field name="domain">[
-            ('is_primary', '=', True),
-            ('provider_ids.state', '!=', 'disabled')
-        ]
-        </field>
-        <field name="context">{'active_test': False}</field>
+        <field name="domain">[('is_primary', '=', True)]</field>
+        <field name="context">{'active_test': False, 'search_default_available_pms': 1}</field>
         <field name="help" type="html">
             <p class="o_view_nocontent_smiling_face">
                 No payment methods found for your payment providers.

addons/payment_demo/const.py
@@ -1,0 +1,6 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# The codes of the payment methods to activate when Demo is activated.
+DEFAULT_PAYMENT_METHOD_CODES = [
+    'demo',
+]

commit 517bf2023e39e6b729c1bebb692c9cdeb4ac0f82

[FIX] payment: archive tokens on state changed
Commit ad2a1d01 broke the auto-archive of tokens when the provider's
state moves from 'test' <-> 'enabled'.

closes odoo/odoo#140052

Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

addons/payment/models/payment_provider.py
@@ -332,25 +332,23 @@ class PaymentProvider(models.Model):
 
     def write(self, values):
         # Handle provider state changes.
-        providers_by_state_change = {}
+        deactivated_providers = self.env['payment.provider']
+        activated_providers = self.env['payment.provider']
         if 'state' in values:
+            state_changed_providers = self.filtered(
+                lambda p: p.state not in ('disabled', values['state'])
+            )  # Don't handle providers being enabled or whose state is not updated.
+            state_changed_providers._archive_linked_tokens()
             if values['state'] == 'disabled':
-                providers_by_state_change['disabled'] = self.filtered(
-                    lambda p: p.state != 'disabled'
-                )
+                deactivated_providers = state_changed_providers
             else:  # 'enabled' or 'test'
-                providers_by_state_change['activated'] = self.filtered(
-                    lambda p: p.state == 'disabled'
-                )
+                activated_providers = self.filtered(lambda p: p.state == 'disabled')
 
         result = super().write(values)
         self._check_required_if_provider()
 
-        if 'disabled' in providers_by_state_change:
-            providers_by_state_change['disabled']._archive_linked_tokens()
-            providers_by_state_change['disabled']._deactivate_unsupported_payment_methods()
-        elif 'activated' in providers_by_state_change:
-            providers_by_state_change['activated']._activate_default_pms()
+        deactivated_providers._deactivate_unsupported_payment_methods()
+        activated_providers._activate_default_pms()
 
         return result
 