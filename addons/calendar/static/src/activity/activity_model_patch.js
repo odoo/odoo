c27032dea12d76734d673ca62b4b45d0225e93c0
Alexandre Kühn <aku@odoo.com>
2023-10-27 10:57:33 +0200

    [FIX] mail,*: markup chatbot livechat message
    calendar, im_livechat
    
    Before this commit, messages in chatbot conversation were not
    properly markup. This comes from changes from [1] that added
    trusted insert. Original code in `Record.insert()` makes a model
    insertion of the data using the `html` flag. Patches must not
    override `static insert()` as only the super call is affected by
    the provided `html` flag. For patches to take account of it, they
    must instead patch `_insert`, with `_` prefix, which is internally
    called by original `Record.insert()`.
    
    In addition:
    - `static insert` allows array of data while `static _insert` works
      with data on single record. Patches were designed with data on
      single record, so some code were not working properly
    - Signature of `static insert` has been overloaded with new option
      `html: true`, so patches must propagate it. They were only
      propagating `data` and omitting the 2nd paramater, which results
      in omitting provided `html` thus falling back to `html: false`,
      resulting to non-escaping message body
    
    This commit fixes all model patches to override `static _insert`
    instead of `static insert`.
    
    [1]: https://github.com/odoo/odoo/pull/139501
    
    closes odoo/odoo#140202
    
    X-original-commit: b3c231b1b26588bcb0052980d6f68b134c5bb63d
    Related: odoo/enterprise#49784
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/calendar/static/src/activity/activity_model_patch.js
+++ addons/calendar/static/src/activity/activity_model_patch.js
@@ -1,15 +1,13 @@
 /** @odoo-module */
 
 import { Activity } from "@mail/core/web/activity_model";
+import { assignIn } from "@mail/utils/common/misc";
 import { patch } from "@web/core/utils/patch";
 
 patch(Activity, {
-    insert(data) {
-        const activity = super.insert(...arguments);
-        const { calendar_event_id: calendarEventId } = data;
-        if (calendarEventId) {
-            activity["calendar_event_id"] = calendarEventId;
-        }
+    _insert(data) {
+        const activity = super._insert(...arguments);
+        assignIn(activity, data, ["calendar_event_id"]);
         return activity;
     },
 });
--- addons/im_livechat/static/src/embed/common/message_model_patch.js
+++ addons/im_livechat/static/src/embed/common/message_model_patch.js
@@ -7,9 +7,9 @@ import { Message } from "@mail/core/common/message_model";
 import { patch } from "@web/core/utils/patch";
 
 patch(Message, {
-    insert(data) {
+    _insert(data) {
         const chatbotStep = this.store.Message.get(data)?.chatbotStep;
-        const message = super.insert(data);
+        const message = super._insert(...arguments);
         if (data.chatbotStep) {
             message.chatbotStep = new ChatbotStep({ ...chatbotStep, ...data.chatbotStep });
         }
