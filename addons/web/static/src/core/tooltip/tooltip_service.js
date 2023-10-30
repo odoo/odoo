5cbfa9751d6186ac6515a4341676706a807642ec
MerlinGuillaume <megu@odoo.com>
2023-10-30 10:30:19 +0100

    [FIX] web: open tooltip with mouse event on touch device
    On device which are primarily recognized as "touch", when using a
    mouse-like pointer, the tooltip can't be opened both on "touch" and on
    "hover" (withe the mouse).
    
    This commit fixes it by removing the escaping that prevented the mouse
    events' listeners from being registered on touch-enabled devices.
    
    Steps to reproduce:
    1. Using Chrome, open the developer tools and click on "Toggle device
       toolbar"
    2. Install Contacts
    3. Go to Contacts and open the form view of any contact
    4. Now click again on "Toggle device toolbar" to disable it but do not
       reload the page: touch device capabilities will be loaded but you
       will be able to use a mouse
    5. Click on the question mark next to "Tax ID", nothing happens
    
    opw-3493571
    
    closes odoo/odoo#140263
    
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- addons/web/static/src/core/tooltip/tooltip_service.js
+++ addons/web/static/src/core/tooltip/tooltip_service.js
@@ -199,8 +199,6 @@ export const tooltipService = {
                         }
                     }
                 });
-
-                return;
             }
 
             // Listen (using event delegation) to "mouseenter" events to open the tooltip if any
