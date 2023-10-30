afa66447afca930e7885af48d75d4e819e84e500
Joseph Caburnay <jcb@odoo.com>
2023-10-30 16:06:17 +0100

    [FIX] point_of_sale: should not load default pricelist
    Since 40f0482b680a7872d338fa8fd10eec4663637b46, pricelist is no longer required.
    Because of that, pos no longer needs to implicitly load a default pricelist when
    "Flexible Pricelists" (use_pricelist) flag is active. Therefore, when
    deactivating the option, we clear the value of pos.config.pricelist_id.
    
    We also make sure that during loading of data when opening a session,
    default_pricelist is not set if use_pricelist is false.
    
    Also note that we are not clearing available_pricelist_ids because of some
    limitation in the synchronization between res.config.settings and pos.config. It
    should okay to keep the links because they won't be loaded when use_pricelist is
    false. Also, it's nice to see the field filled when reactivating the flag.

--- addons/point_of_sale/models/res_config_settings.py
+++ addons/point_of_sale/models/res_config_settings.py
@@ -254,8 +254,7 @@ class ResConfigSettings(models.TransientModel):
             currency_id = res_config.pos_journal_id.currency_id.id if res_config.pos_journal_id.currency_id else res_config.pos_config_id.company_id.currency_id.id
             pricelists_in_current_currency = self.env['product.pricelist'].search([('company_id', 'in', (False, res_config.pos_config_id.company_id.id)), ('currency_id', '=', currency_id)])
             if not res_config.pos_use_pricelist:
-                res_config.pos_available_pricelist_ids = pricelists_in_current_currency[:1]
-                res_config.pos_pricelist_id = pricelists_in_current_currency[:1]
+                res_config.pos_pricelist_id = False
             else:
                 if any([p.currency_id.id != currency_id for p in res_config.pos_available_pricelist_ids]):
                     res_config.pos_available_pricelist_ids = pricelists_in_current_currency
