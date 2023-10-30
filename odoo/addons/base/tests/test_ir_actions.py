7488375483251cd856f536fd5fbf824f2d84acd9
Christophe Simonis <chs@odoo.com>
2023-10-30 09:41:56 +0100

    [IMP] core: reflect the model inherits in the database
    This will help the upgrade process. Currently, there is an harcoded[^1]
    inherit tree stored in the upgrade source. By storing this information
    in the databases, we:
     - avoid this hardcoded list.
     - support more than the standard modules.
    
    task-3504720
    
    [^1]: hardcoded, but autogenerated.
    
    closes odoo/odoo#140560
    
    X-original-commit: 35e22b8fe906d26618ebbc02583f980f7fa20e42
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/addons/base/tests/test_ir_actions.py
+++ odoo/addons/base/tests/test_ir_actions.py
@@ -735,7 +735,7 @@ class TestCustomFields(TestCommonCustomFields):
 
         # create a non-computed field, and assert how many queries it takes
         model_id = self.env['ir.model']._get_id('res.partner')
-        query_count = 38
+        query_count = 44
         with self.assertQueryCount(query_count):
             self.env.registry.clear_cache()
             self.env['ir.model.fields'].create({
