dde9df05b980ae1aecdea664535f1b44772e18f4
Levi Siuzdak (sile) <sile@odoo.com>
2023-10-31 13:16:49 +0000

    [FIX] hr_timesheet: show all tasks assigned to user
    Versions:
    ---------
    - 15.0+
    
    Steps to reproduce:
    -------------------
    1. install project & hr_timesheet;
    2. create a follower-only project with timesheets enabled;
    3. assign a task to a non-follower with user-level access rights;
    4. log in as user;
    5. go to timesheets app;
    6. add a line.
    
    Issue:
    ------
    The assigned task doesn't show in the search results.
    
    Cause:
    ------
    The task's `allow_timesheets` field is set to be `related` to its
    project's `allow_timesheets` field. This field is inaccessible for
    private projects the user doesn't follow, so it doesn't show up when
    searching tasks assigned to user where `allow_timesheets=True`.
    
    Solution:
    ---------
    Add compute and search methods to have `sudo` access without having to
    make to field storable.
    
    opw-3450671
    
    X-original-commit: 06e67687cf944ea14d38d3bf0272aece15d44faf

--- addons/hr_timesheet/models/hr_timesheet.py
+++ addons/hr_timesheet/models/hr_timesheet.py
@@ -50,7 +50,7 @@ class AccountAnalyticLine(models.Model):
     task_id = fields.Many2one(
         'project.task', 'Task', index='btree_not_null',
         compute='_compute_task_id', store=True, readonly=False,
-        domain="[('project_id.allow_timesheets', '=', True), ('project_id', '=?', project_id)]")
+        domain="[('allow_timesheets', '=', True), ('project_id', '=?', project_id)]")
     parent_task_id = fields.Many2one('project.task', related='task_id.parent_id', store=True)
     project_id = fields.Many2one(
         'project.project', 'Project', domain=_domain_project_id, index=True,
