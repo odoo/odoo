e8167ada700dafbcbb2744e26bc3011f721af987
roen-odoo <roen@odoo.com>
2023-10-31 10:12:02 +0000

    [FIX] pos_sale: runbot error
    X-original-commit: 35017363d59ef2c78bb754431522b2386b224ae4

--- addons/pos_sale/static/tests/tours/PosSaleTour.js
+++ addons/pos_sale/static/tests/tours/PosSaleTour.js
@@ -1,6 +1,5 @@
 /** @odoo-module */
 
-import { Chrome } from "@point_of_sale/../tests/tours/helpers/ChromeTourMethods";
 import { PaymentScreen } from "@point_of_sale/../tests/tours/helpers/PaymentScreenTourMethods";
 import { ReceiptScreen } from "@point_of_sale/../tests/tours/helpers/ReceiptScreenTourMethods";
 import { ProductScreen } from "@pos_sale/../tests/helpers/ProductScreenTourMethods";
@@ -22,8 +21,6 @@ ProductScreen.do.clickPayButton();
 PaymentScreen.do.clickPaymentMethod('Bank');
 PaymentScreen.do.clickValidate();
 ReceiptScreen.check.isShown();
-Chrome.do.clickMenuButton();
-Chrome.do.clickTicketButton();
 
 registry.category("web_tour.tours").add('PosSettleOrder', { test: true, url: '/pos/ui', steps: getSteps() });
 
