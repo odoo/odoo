670c21ed5f40a699916c14216bb42a8fe7ab5fe0
Lucas Lefèvre (lul) <lul@odoo.com>
2023-10-31 11:43:36 +0100

    [FIX] spreadsheet(_dashboard): add data validation
    Currently, spreadsheet json files are checked in a post-install test.
    It as several drawbacks:
    - it's not validating the data when a user upload a new version from the UI,
      just like when a user edits a view and the arch isn't valid.
    - more importantly, it doesn't properly check the module dependencies since
      the test runs after all modules are installed. We already made the mistake
      in the past to have a dashboard module with wrong dependencies.
    
    Task: 3577092
    Part-of: odoo/odoo#140515

--- addons/spreadsheet/utils/validate_data.py
+++ addons/spreadsheet/utils/validate_data.py
@@ -1,10 +1,7 @@
 from collections import defaultdict
 from itertools import chain
-import base64
 import json
 import re
-from odoo.tests import TransactionCase
-from odoo.exceptions import ValidationError
 
 markdown_link_regex = r"^\[([^\[]+)\]\((.+)\)$"
 
@@ -16,10 +13,10 @@ odoo_view_link_prefix = "odoo://view/"
 def odoo_charts(data):
     """return all odoo chart definition in the spreadsheet"""
     figures = []
-    for sheet in data["sheets"]:
+    for sheet in data.get("sheets", []):
         figures += [
             dict(figure["data"], id=figure["id"])
-            for figure in sheet["figures"]
+            for figure in sheet.get("figures", [])
             if figure["tag"] == "chart" and figure["data"]["type"].startswith("odoo_")
         ]
     return figures
@@ -29,8 +26,8 @@ def links_urls(data):
     """return all markdown links in cells"""
     urls = []
     link_prefix = "odoo://view/"
-    for sheet in data["sheets"]:
-        for cell in sheet["cells"].values():
+    for sheet in data.get("sheets", []):
+        for cell in sheet.get("cells", {}).values():
             content = cell.get("content", "")
             match = re.match(markdown_link_regex, content)
             if match and match.group(2).startswith(link_prefix):
@@ -189,37 +186,3 @@ def menus_xml_ids_in_spreadsheet(data):
         for url in links_urls(data)
         if url.startswith(xml_id_url_prefix)
     }
-
-
-class ValidateSpreadsheetData(TransactionCase):
-    def validate_spreadsheet_data(self, stringified_data, spreadsheet_name):
-        data = json.loads(stringified_data)
-        for model, fields in fields_in_spreadsheet(data).items():
-            if model not in self.env:
-                raise AssertionError(
-                    f"model '{model}' used in '{spreadsheet_name}' does not exist"
-                )
-            for field_chain in fields:
-                field_model = model
-                for fname in field_chain.split(
-                    "."
-                ):  # field chain 'product_id.channel_ids'
-                    if fname not in self.env[field_model]._fields:
-                        raise AssertionError(
-                            f"field '{fname}' used in spreadsheet '{spreadsheet_name}' does not exist on model '{field_model}'"
-                        )
-                    field = self.env[field_model]._fields[fname]
-                    if field.relational:
-                        field_model = field.comodel_name
-
-        for xml_id in menus_xml_ids_in_spreadsheet(data):
-            record = self.env.ref(xml_id, raise_if_not_found=False)
-            if not record:
-                raise AssertionError(
-                    f"xml id '{xml_id}' used in spreadsheet '{spreadsheet_name}' does not exist"
-                )
-            # check that the menu has an action. Root menus always have an action.
-            if not record.action and record.parent_id.id:
-                raise AssertionError(
-                    f"menu with xml id '{xml_id}' used in spreadsheet '{spreadsheet_name}' does not have an action"
-                )
