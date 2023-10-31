dec902180f8f8ae34fc7eb1850e4b6c0c28e4812
Levi Siuzdak (sile) <sile@odoo.com>
2023-10-31 13:16:49 +0000

    [FIX] hr_timesheet: show all tasks assigned to user
    Versions:
    ---------
    - 15.0+
    
    Steps to reproduce:
    -------------------
    1. install project & hr_timesheet;
    2. create a follower-only project with timesheets enabled;
    3. assign a task to a non-follower with user-level access rights;
    4. log in as user;
    5. go to timesheets app;
    6. add a line.
    
    Issue:
    ------
    The assigned task doesn't show in the search results.
    
    Cause:
    ------
    The task's `allow_timesheets` field is set to be `related` to its
    project's `allow_timesheets` field. This field is inaccessible for
    private projects the user doesn't follow, so it doesn't show up when
    searching tasks assigned to user where `allow_timesheets=True`.
    
    Solution:
    ---------
    Add compute and search methods to have `sudo` access without having to
    make to field storable.
    
    opw-3450671
    
    closes odoo/odoo#141760
    
    X-original-commit: 06e67687cf944ea14d38d3bf0272aece15d44faf
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project_timesheet_holidays/models/account_analytic.py
+++ addons/project_timesheet_holidays/models/account_analytic.py
@@ -10,7 +10,7 @@ class AccountAnalyticLine(models.Model):
 
     holiday_id = fields.Many2one("hr.leave", string='Time Off Request', copy=False)
     global_leave_id = fields.Many2one("resource.calendar.leaves", string="Global Time Off", ondelete='cascade')
-    task_id = fields.Many2one(domain="[('project_id.allow_timesheets', '=', True), ('project_id', '=?', project_id), ('is_timeoff_task', '=', False)]")
+    task_id = fields.Many2one(domain="[('allow_timesheets', '=', True), ('project_id', '=?', project_id), ('is_timeoff_task', '=', False)]")
 
     def _get_redirect_action(self):
         leave_form_view_id = self.env.ref('hr_holidays.hr_leave_view_form').id
