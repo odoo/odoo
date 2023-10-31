7458484b07f146d28624b477f7c612724fd7c840
tsm-odoo <tsm@odoo.com>
2023-10-31 09:46:13 +0100

    [FIX] im_livechat: crash on new odoo instance
    The live chat relies on `session.origin` to send its requests to
    the server when its embed in an external page. `web.base.url` is
    only set when a user authenticates and is wrong when no user
    connected yet. Since `session.origin` uses the value of `web.base.url`,
    a crash occurs when opening the website with live chat enabled on a
    fresh instance of odoo. This commit removes the use of `session.origin`
    in the frontend code since it is useless anyway. The `url` function
    is used to format URLs since it either happens `session.origin` if
    available or `location.origin` otherwise.
    
    Steps to reproduce:
    - Create a new odoo database with website_livechat installed
    - Go to the log in page
    - A crash occurs
    
    closes odoo/odoo#140156
    
    X-original-commit: 62e266e
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/im_livechat/static/src/embed/external/emoji_loader_patch.js
+++ addons/im_livechat/static/src/embed/external/emoji_loader_patch.js
@@ -5,12 +5,12 @@ import { loader } from "@web/core/emoji_picker/emoji_picker";
 import { loadBundle } from "@web/core/assets";
 import { memoize } from "@web/core/utils/functions";
 import { patch } from "@web/core/utils/patch";
-import { session } from "@web/session";
+import { url } from "@web/core/utils/urls";
 
 patch(loader, {
     loadEmoji: memoize(() =>
         loadBundle({
-            jsLibs: [`${session.origin}/im_livechat/emoji_bundle`],
+            jsLibs: [url("/im_livechat/emoji_bundle")],
         })
     ),
 });
