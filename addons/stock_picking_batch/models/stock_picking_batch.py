bef4d07bbae2518d957b464386ea1a8707028212
Walid <waha@odoo.com>
2023-10-31 10:25:21 +0100

    [FIX] stock_picking_batch, delivery: put delivered batch in pack
    Steps to reproduce:
    - Install DHL delivery and enable batch transfers
    - Create 2 SO with adding shipping
    - Select the 2 delivery orders and add them to the same batch
    - Put the batch in a pack
    - Only movelines of the first delivery are put in the pack
    
    Bug:
    when delivery is Installed a Delivery Package Type has to be chosen
    this will in turn put the active picking mls in pack
    the case of batch picking was disregarded
    https://github.com/odoo/odoo/pull/55096/files#r469780148
    
    Fix:
    implementing a method to get the movelines to pack will allow to simply
    make an override in stock_picking_batch instaed of creating a new module
    this has also the added benefit of unifiying the behavior accross the
    different methods
    
    opw-3357423
    
    closes odoo/odoo#140394
    
    X-original-commit: 815edefb4d36ca953a262a8abae7222b402b5abf
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Walid Hanniche (waha) <waha@odoo.com>

--- addons/stock_picking_batch/models/stock_picking_batch.py
+++ addons/stock_picking_batch/models/stock_picking_batch.py
@@ -267,16 +267,7 @@ class StockPickingBatch(models.Model):
         """
         self.ensure_one()
         if self.state not in ('done', 'cancel'):
-            picking_move_lines = self.move_line_ids
-
-            move_line_ids = picking_move_lines.filtered(lambda ml:
-                float_compare(ml.qty_done, 0.0, precision_rounding=ml.product_uom_id.rounding) > 0
-                and not ml.result_package_id
-            )
-            if not move_line_ids:
-                move_line_ids = picking_move_lines.filtered(lambda ml: float_compare(ml.reserved_uom_qty, 0.0,
-                                     precision_rounding=ml.product_uom_id.rounding) > 0 and float_compare(ml.qty_done, 0.0,
-                                     precision_rounding=ml.product_uom_id.rounding) == 0)
+            move_line_ids = self.picking_ids[0]._package_move_lines()
             if move_line_ids:
                 res = move_line_ids.picking_id[0]._pre_put_in_pack_hook(move_line_ids)
                 if not res:
