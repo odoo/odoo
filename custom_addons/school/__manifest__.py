cb690d275026c55ee07cbf4df656272a3b733223
benjamin <benkisenge03@gmail.com>
2023-11-02 08:34:31 +0200

    Rename the module name

---
+++ custom_addons/school/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "School Management System",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Benjamin Kisenge",
+    'category': 'Education',
+    'description': """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
