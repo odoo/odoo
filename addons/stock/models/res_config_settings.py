dc35c95eb68d09b25972f1080f4238d67b6180e9
Djamel Touati <otd@odoo.com>
2023-11-02 10:52:47 +0100

    [FIX] stock: fix production_lot settings
    For existing users who had previously disabled the "Lots & Serial
    Numbers" settings, by applying this fix: https://github.com/odoo/odoo/pull/138907/commits/376ee1289eba840f1d8b2b6149d933bdedd47518
    
    They will get the user error each time they try to access the settings.
    Therefore, we prefer to prevent this and trigger the error only when
    necessary.
    
    opw-3452946
    
    closes odoo/odoo#140707
    
    X-original-commit: d8dc66a901bcf00e73f1c8048a71daf5e0bd321f
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Djamel Touati (otd) <otd@odoo.com>

--- addons/stock/models/res_config_settings.py
+++ addons/stock/models/res_config_settings.py
@@ -59,8 +59,6 @@ class ResConfigSettings(models.TransientModel):
     def _onchange_group_stock_production_lot(self):
         if not self.group_stock_production_lot:
             self.group_lot_on_delivery_slip = False
-            if self.env['product.product'].search_count([('tracking', '!=', 'none')]):
-                raise UserError(_("You have product(s) in stock that have lot/serial number tracking enabled. \nSwitch off tracking on all the products before switching off this setting."))
 
     @api.onchange('group_stock_adv_location')
     def onchange_adv_location(self):
@@ -114,5 +112,8 @@ class ResConfigSettings(models.TransientModel):
                 ('show_operations', '=', False)
             ])
             picking_types.sudo().write({'show_operations': True})
+        if not self.group_stock_production_lot and previous_group.get('group_stock_production_lot'):
+            if self.env['product.product'].search_count([('tracking', '!=', 'none')]):
+                raise UserError(_("You have product(s) in stock that have lot/serial number tracking enabled. \nSwitch off tracking on all the products before switching off this setting."))
 
         return res
