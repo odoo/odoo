0fbc8fffd6550aeba6878f63ee26e9565c27a277
Lina (liew) <liew@odoo.com>
2023-11-02 10:41:04 +0000

    [IMP] partner_autocomplete: improve vat number matching
    The first search request for a VAT number will always fail,
    because we validate the VAT based on previous requests of the VAT
    before we return the company information to avoid returning the wrong company
    
    If no validated VAT is found and vies vat found the company,
    we return the information provided by vies vat
    task-3548046
    
    closes odoo/odoo#148972
    
    X-original-commit: a591e9c061374bfaf84d41a94246cff83ab1899c
    Signed-off-by: Louis Baudoux (lba) <lba@odoo.com>
    Signed-off-by: Lina Eweis (liew) <liew@odoo.com>

--- addons/partner_autocomplete/models/res_partner.py
+++ addons/partner_autocomplete/models/res_partner.py
@@ -7,6 +7,8 @@ import logging
 import re
 import requests
 
+from stdnum.eu.vat import check_vies
+
 from odoo import api, fields, models, tools, _
 
 _logger = logging.getLogger(__name__)
@@ -136,6 +138,28 @@ class ResPartner(models.Model):
         if vies_vat_data:
             return [self._format_data_company(vies_vat_data)]
         else:
+            vies_result = None
+            try:
+                vies_result = check_vies(vat)
+            except Exception:
+                _logger.exception("Failed VIES VAT check.")
+            if vies_result:
+                name = vies_result['name']
+                if vies_result['valid'] and name != '---':
+                    address = list(filter(bool, vies_result['address'].split('\n')))
+                    street = address[0]
+                    zip_city = address[-1].split(' ', 1) if len(address) > 1 else [None, None]
+                    street2 = address[1] if len(address) > 2 else None
+                    return [self._iap_replace_location_codes({
+                        'name': name,
+                        'vat': vat,
+                        'street': street,
+                        'street2': street2,
+                        'city': zip_city[1],
+                        'zip': zip_city[0],
+                        'country_code': vies_result['countryCode'],
+                        'skip_enrich': True,
+                    })]
             return []
 
     @api.model
