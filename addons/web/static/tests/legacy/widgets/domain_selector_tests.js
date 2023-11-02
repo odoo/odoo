52cf8fe3d007eca0aa137997e3088df258217939
Hubert Van de Walle (huvw) <huvw@odoo.com>
2023-11-02 16:44:11 +0100

    [FIX] web: domain selector crashes with no children
    Steps to reproduce
    ==================
    
    - Go to invoices
    - Open studio > report > Invoices
    - Click on a field, for example "Due Date"
    - Click on the pencil next to "Visible if" on the sidebar
    - Click on "Add filter"
    - Click on the cross to remove the filter
    
    -> `RangeError: invalid array length`
    
    Cause of the issue
    ==================
    
    `_.times(length)` has been replaced by `new Array(length)`.
    In this case, the length is -1
    
    opw-3560346
    
    closes odoo/odoo#140755
    
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>

---
+++ addons/web/static/tests/legacy/widgets/domain_selector_tests.js
@@ -0,0 +1,83 @@
+/** @odoo-module **/
+
+import { click, getFixture } from "@web/../tests/helpers/utils";
+import DomainSelector from "web.DomainSelector";
+import testUtilsMock from "web.test_utils_mock";
+
+let serverData;
+
+QUnit.module("Widgets", {}, function () {
+    QUnit.module("DomainSelector", {
+        beforeEach() {
+            serverData = {
+                partner: {
+                    fields: {
+                        foo: { string: "Foo", type: "char", searchable: true },
+                        bar: { string: "Bar", type: "boolean", searchable: true },
+                        nice_datetime: { string: "Datetime", type: "datetime", searchable: true },
+                        product_id: {
+                            string: "Product",
+                            type: "many2one",
+                            relation: "product",
+                            searchable: true,
+                        },
+                    },
+                    records: [
+                        {
+                            id: 1,
+                            foo: "yop",
+                            bar: true,
+                            product_id: 37,
+                        },
+                        {
+                            id: 2,
+                            foo: "blip",
+                            bar: true,
+                            product_id: false,
+                        },
+                        {
+                            id: 4,
+                            foo: "abc",
+                            bar: false,
+                            product_id: 41,
+                        },
+                    ],
+                    onchanges: {},
+                },
+                product: {
+                    fields: {
+                        name: { string: "Product Name", type: "char", searchable: true },
+                    },
+                    records: [
+                        {
+                            id: 37,
+                            display_name: "xphone",
+                        },
+                        {
+                            id: 41,
+                            display_name: "xpad",
+                        },
+                    ],
+                },
+            };
+        },
+    });
+
+    QUnit.test("deleting domain node", async (assert) => {
+        const target = getFixture();
+        const domainSelector = new DomainSelector(null, "partner", [], {
+            readonly: false,
+            debugMode: true,
+        });
+        await testUtilsMock.addMockEnvironment(domainSelector, {
+            debug: QUnit.config.debug,
+            data: serverData,
+        });
+        await domainSelector.appendTo(target);
+        await click(target, ".o_domain_add_first_node_button");
+        assert.strictEqual(target.querySelector(".o_domain_debug_input").value, '[["id","=",1]]');
+        await click(target, ".o_domain_delete_node_button");
+        assert.strictEqual(target.querySelector(".o_domain_debug_input").value, "[]");
+        domainSelector.destroy();
+    });
+});
