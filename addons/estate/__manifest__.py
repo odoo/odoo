c84f295676248f48b28e9c08c6597689abf57d5a
benjamin <benkisenge03@gmail.com>
2023-11-02 08:34:04 +0200

    Create the manifest for the estate module

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Benjamin Kisenge",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [  ],
+    # data files containing optionally loaded demonstration data
+    'demo': [  ],
+}
