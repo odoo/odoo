98031a16f2fc5c008e752d8548994f718495346a
David (dafr) <dafr@odoo.com>
2023-11-02 09:53:58 +0000

    [FIX] purchase_stock: compute amount_currency from balance for valuation lines
    Previously, 'amount_currency' was calculated using data from the purchase line. However, numerous variables could interfere with the accurate computation of 'purchase_price_unit' (e.g., taxes, taxes without account, standard costing method, kit product, and potentially more).
    
    This commit revises the approach by utilizing the pre-calculated value and converting it to the desired currency. This maintains coherence between the balance and 'amount_currency' without necessitating the recreation of a function like '_get_price_unit()' in 'purchase_stock.'
    
    ## Reproduce Errors ##
    
    ### Tax Included in price without Account:
    - Create a currency, CTest, with a conversion factor of 10 (10 CTest = 1 USD).
    - Define a new tax:
      ° No accounts in repartition lines
      ° Rate: 15%
      ° Included in the price
    - Create a Purchase Order:
      ° 1 unit | 100 CTest | Tax applied
    - Receive the product.
    -> SVL value is 10 USD, because tax without account increments the stock value. Refer to 'test_valuation_multicurrency_with_tax'.
    -> Check SVL journal entry: credit / debit is 10, but amount currency is 86.96 CTest instead of 100 CTest.
    
    ### Reproduce With Kit:
    - Create a currency, CTest, with a conversion factor of 10 (10 CTest = 1 USD).
    - Create a Kit (storable, fifo), with 5 unit of CPM (storable fifo) in it's BoM
    - Create a Purchase Order:
      ° Kit: 1 unit | 100 CTest
    - Receive product
    -> Check SVL journal entry: credit / debit is 10, but amount currency is 500 CTest instead of 100 CTest.
    
    OPW-3453703
    
    closes odoo/odoo#143013
    
    X-original-commit: 5ea188fb65507d4bd06ab3361241d121ac2a468b
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: David Fesquet (dafr) <dafr@odoo.com>

--- addons/purchase_stock/models/stock_move.py
+++ addons/purchase_stock/models/stock_move.py
@@ -90,29 +90,23 @@ class StockMove(models.Model):
 
         rslt = super(StockMove, self)._generate_valuation_lines_data(partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id, svl_id, description)
         purchase_currency = self.purchase_line_id.currency_id
-        if not self.purchase_line_id or purchase_currency == self.company_id.currency_id:
+        company_currency = self.company_id.currency_id
+        if not self.purchase_line_id or purchase_currency == company_currency:
             return rslt
         svl = self.env['stock.valuation.layer'].browse(svl_id)
         if not svl.account_move_line_id:
-            if(self.purchase_line_id.product_id.cost_method == 'standard'):
-                purchase_price_unit = self.purchase_line_id.product_id.cost_currency_id._convert(
-                    self.purchase_line_id.product_id.standard_price,
-                    purchase_currency,
-                    self.company_id,
-                    self.date,
-                    round=False,
-                )
-            else:
-                # Do not use price_unit since we want the price tax excluded. And by the way, qty
-                # is in the UOM of the product, not the UOM of the PO line.
-                purchase_price_unit = (
-                    self.purchase_line_id.price_subtotal / self.purchase_line_id.product_uom_qty
-                    if self.purchase_line_id.product_uom_qty
-                    else self.purchase_line_id.price_unit
-                )
-            currency_move_valuation = purchase_currency.round(purchase_price_unit * abs(qty))
-            rslt['credit_line_vals']['amount_currency'] = rslt['credit_line_vals']['balance'] < 0 and -currency_move_valuation or currency_move_valuation
-            rslt['debit_line_vals']['amount_currency'] = rslt['debit_line_vals']['balance'] < 0 and -currency_move_valuation or currency_move_valuation
+            rslt['credit_line_vals']['amount_currency'] = company_currency._convert(
+                rslt['credit_line_vals']['balance'],
+                purchase_currency,
+                self.company_id,
+                self.date
+            )
+            rslt['debit_line_vals']['amount_currency'] = company_currency._convert(
+                rslt['debit_line_vals']['balance'],
+                purchase_currency,
+                self.company_id,
+                self.date
+            )
             rslt['debit_line_vals']['currency_id'] = purchase_currency.id
             rslt['credit_line_vals']['currency_id'] = purchase_currency.id
         else:
