8914b879aa9ae172af631370476501680ef22f6c
tsm-odoo <tsm@odoo.com>
2023-11-02 13:00:34 +0100

    [IMP] mail: add ptt extension for chromium-based browsers
    Currently, discuss push-to-talk only works when the browser has focus
    because key events are not fired otherwise.
    
    This commit adds an extension for chromium-based browsers that allows
    to register a system-wide push-to-talk shortcut that work adapt the
    rtc service in order to listen to the extension events.

---
+++ addons/mail/push-to-talk-extension/background.js
@@ -0,0 +1,86 @@
+/* global chrome */
+
+import { throttle } from "./utils.js";
+
+// ServiceWorker can be terminated at any time, storage session is
+// used to ensure local state is not lost.
+chrome.storage.session.set({ isTalkingByTabId: {} });
+
+chrome.tabs.onRemoved.addListener(async (tabId) => {
+    const { isTalkingByTabId } = await chrome.storage.session.get();
+    delete isTalkingByTabId[tabId];
+    await chrome.storage.session.set({ isTalkingByTabId });
+    await updateAppIcon();
+});
+
+chrome.action.onClicked.addListener(function () {
+    chrome.tabs.create({ url: "chrome://extensions/shortcuts" });
+});
+
+async function updateAppIcon() {
+    const { isTalkingByTabId } = await chrome.storage.session.get();
+    const isTalking = Object.values(isTalkingByTabId).some(Boolean);
+    chrome.action.setIcon({
+        path: `/assets/icons/icon_${isTalking ? "active" : "inactive"}.png`,
+    });
+}
+
+chrome.runtime.onMessage.addListener(async function (request, sender) {
+    const { from, type, value } = request;
+    if (from !== "discuss") {
+        return;
+    }
+    switch (type) {
+        case "subscribe":
+            {
+                const { isTalkingByTabId } = await chrome.storage.session.get();
+                isTalkingByTabId[sender.tab.id] = false;
+                await chrome.storage.session.set({ isTalkingByTabId });
+            }
+            break;
+        case "unsubscribe":
+            {
+                const { isTalkingByTabId } = await chrome.storage.session.get();
+                delete isTalkingByTabId[sender.tab.id];
+                await chrome.storage.session.set({ isTalkingByTabId });
+                await updateAppIcon();
+            }
+            break;
+        case "is-talking":
+            {
+                const { isTalkingByTabId } = await chrome.storage.session.get();
+                isTalkingByTabId[sender.tab.id] = value;
+                await chrome.storage.session.set({ isTalkingByTabId });
+                await updateAppIcon();
+            }
+            break;
+        case "ask-is-enabled":
+            chrome.tabs.sendMessage(sender.tab.id, {
+                from: "discuss-push-to-talk",
+                type: "answer-is-enabled",
+            });
+            break;
+    }
+});
+
+async function onCommand(command) {
+    const { isTalkingByTabId } = await chrome.storage.session.get();
+    for (const tabId of Object.keys(isTalkingByTabId)) {
+        switch (command) {
+            case "toggle-voice":
+                chrome.tabs.sendMessage(Number(tabId), {
+                    from: "discuss-push-to-talk",
+                    type: "toggle-voice",
+                });
+                break;
+            case "ptt-pressed":
+                chrome.tabs.sendMessage(Number(tabId), {
+                    from: "discuss-push-to-talk",
+                    type: "push-to-talk-pressed",
+                });
+                break;
+        }
+    }
+}
+
+chrome.commands.onCommand.addListener(throttle(onCommand, 150));
