1c255dd626cfde12647606b8e20864adc9cb8fb5
Arnold Moyaux <arm@odoo.com>
2023-11-02 13:16:55 +0100

    [FIX] stock: refresh on state change
    Temporary fix to revert once it will be done correctly on
    client side. The idea is to remove the variable after a record
    editation in order to trigger the recompute of column width
    
    closes odoo/odoo#141045
    
    X-original-commit: 2e254f07c2c77ed51523ce930947eea2c261bcf3
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/stock/static/src/views/picking_form/stock_move_one2many.js
+++ addons/stock/static/src/views/picking_form/stock_move_one2many.js
@@ -3,10 +3,21 @@
 import { registry } from "@web/core/registry";
 import { ListRenderer } from "@web/views/list/list_renderer";
 import { X2ManyField, x2ManyField } from "@web/views/fields/x2many/x2many_field";
+import { useEffect } from "@odoo/owl";
 
 export class MovesListRenderer extends ListRenderer {
     static recordRowTemplate = "stock.MovesListRenderer.RecordRow";
     static props = [...ListRenderer.props, "stockMoveOpen?"];
+
+    setup() {
+        super.setup();
+        useEffect(
+            () => {
+                this.keepColumnWidths = false;
+            },
+            () => [this.state.columns]
+        );
+    }
 
     processAllColumn(allColumns, list) {
         let cols = super.processAllColumn(...arguments);
