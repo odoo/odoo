5a7a589609ff35c3defb1d73e8aa973964e0c4be
Sébastien Theys <seb@odoo.com>
2023-11-02 21:48:07 +0100

    [FIX] mail: show translations for emails
    Due to shadowRoot, emails were not properly translated.
    
    closes odoo/odoo#140854
    
    X-original-commit: 06603e9dc7db71b5a3f6e58f38533c17dc012d07
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/tests/translation/translation_test.js
+++ addons/mail/static/tests/translation/translation_test.js
@@ -46,3 +46,42 @@ QUnit.test("Toggle display of original/translated version of chatter message", a
     // The translation button should not trigger more than one external request for a single message.
     assert.verifySteps(["Request"]);
 });
+
+QUnit.test("translation of email message", async () => {
+    const pyEnv = await startServer();
+    const partnerId = pyEnv["res.partner"].create({});
+    pyEnv["mail.message"].create({
+        model: "res.partner",
+        body: "Al mal tiempo, buena cara.",
+        message_type: "email",
+        author_id: partnerId,
+        res_id: partnerId,
+    });
+    const { openFormView } = await start({
+        mockRPC(route) {
+            if (route === "/mail/message/translate") {
+                return { body: "To bad weather, good face.", lang_name: "Spanish", error: null };
+            }
+        },
+    });
+    await openFormView("res.partner", partnerId);
+    await contains("span", {
+        text: "Al mal tiempo, buena cara.",
+        parent: [".o-mail-Message-body > div", { shadowRoot: true }],
+    });
+    await click("button[title='Expand']");
+    await click("span[title='Translate']");
+    await contains("span", {
+        text: "To bad weather, good face.",
+        parent: [".o-mail-Message-body > div", { shadowRoot: true }],
+    });
+    await contains(".o-mail-Message-body", {
+        text: "(Translated from: Spanish)",
+    });
+    await click("button[title='Expand']");
+    await click("span[title='Revert']");
+    await contains("span", {
+        text: "Al mal tiempo, buena cara.",
+        parent: [".o-mail-Message-body > div", { shadowRoot: true }],
+    });
+});
