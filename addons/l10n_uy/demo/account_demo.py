80c6ea5725e869ec526a08ff3be49881156d8cb7
Maximiliano Mezzavilla <mem@adhoc.com.ar>
2023-11-02 14:01:02 -0300

    [ADD] l10n_uy: Uruguayan accounting demo
    Task: 33903

--- addons/l10n_uy/demo/account_demo.py
+++ addons/l10n_uy/demo/account_demo.py
@@ -3,18 +3,16 @@ from odoo import api, models
 
 
 class AccountChartTemplate(models.AbstractModel):
-
     _inherit = "account.chart.template"
 
     @api.model
-    def _get_demo_data_move(self, company=False):
-        """ Set the l10n_latam_document_number on demo invoices """
-        move_data = super()._get_demo_data_move(company)
-        if company.account_fiscal_country_id.code == 'UY':
-            number = 1
-            for move in move_data.values():
-                if move['move_type'] in ('in_invoice', 'in_refund'):
-                    move['l10n_latam_document_number'] = 'A' + f'{number:07d}'
-                    number += 1
+    def _get_demo_data(self, company=False):
+        # Do not load generic demo data on these companies
+        uy_demo_companies = (
+            self.env.ref('l10n_uy.demo_company_uy', raise_if_not_found=False),
+        )
+        if self.env.company in uy_demo_companies:
+            return []
 
-        return move_data
+        for model, data in super()._get_demo_data(company=company):
+            yield model, data
