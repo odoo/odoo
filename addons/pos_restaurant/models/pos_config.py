3f21874268585ea9558917e09f16b1387e7f13d5
Joseph Caburnay <jcb@odoo.com>
2023-11-03 15:28:13 +0100

    [FIX] pos_restaurant: default floor on restaurant config when no demo
    - We remove default floor on when demo data is active.
    - We do the automatic creation of floor and table on a restaurant config during
      the write method of the pos.config. It shouldn't be done in the
      res.config.settings at onchange because of risk of retriggering the creation
      multiple times.
    
    Part-of: odoo/odoo#141330

--- addons/pos_restaurant/models/pos_config.py
+++ addons/pos_restaurant/models/pos_config.py
@@ -90,6 +90,9 @@ class PosConfig(models.Model):
         if ('module_pos_restaurant' in vals and not vals['module_pos_restaurant']) or ('iface_tipproduct' in vals and not vals['iface_tipproduct']):
             vals['set_tip_after_payment'] = False
 
+        if ('module_pos_restaurant' in vals and vals['module_pos_restaurant']):
+            self._setup_default_floor(self)
+
         return super().write(vals)
 
     @api.model
