174d010604bef11cf52c91fb441c3e2498c26b75
Mayurrajsinh Rathod <mrsr@odoo.com>
2023-11-03 07:01:53 +0000

    [FIX] website_slides: prevent constraint error when changing the type
    Before this commit
    ==================
    Modifying the type of content from video to webpage or webpage to video
    violates the constraint.
    
    Technical
    =========
    When we change the 'slide_type' of the slide.slide model from video to any
    other type, or from webpage to any other type, the value url or html_content
    is not removed and remains same. However, when switching between video and
    webpage, the 'exclusion_html_content_and_url' constraint is violated because one
    of them should be null.
    
    After this commit
    =================
    When the type of content is changed, the existing URL or HTML content is
    removed as they are no longer needed.
    
    Task-3474466
    
    X-original-commit: 47464ffe24c3b7180f2ecc8908c2f2d209cf9c4c

--- addons/website_slides/tests/test_slide_slide.py
+++ addons/website_slides/tests/test_slide_slide.py
@@ -22,6 +22,23 @@ class TestSlideInternals(slides_common.SlidesCase):
                 'vote': 2,
             })
 
+    def test_change_content_type(self):
+        """ To prevent constraint violation when changing type from video to article and vice-versa """
+        slide = self.env['slide.slide'].create({
+            'name': 'dummy',
+            'channel_id': self.channel.id,
+            'slide_category': 'video',
+            'is_published': True,
+            'url': 'https://youtu.be/W0JQcpGLSFw',
+        })
+
+        slide.write({'slide_category': 'article', 'html_content': '<p>Hello</p>'})
+        self.assertTrue(slide.html_content)
+        self.assertFalse(slide.url)
+
+        slide.slide_category = 'document'
+        self.assertFalse(slide.html_content)
+
 class TestVideoFromURL(slides_common.SlidesCase):
     def test_video_youtube(self):
         youtube_urls = {
