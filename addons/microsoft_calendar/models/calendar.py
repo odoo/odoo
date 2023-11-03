344519cf861f7c01ecd6f56b7c9c78a3a5b3c60d
Gabriel de Paula Felix (gdpf) <gdpf@odoo.com>
2023-11-03 07:51:37 -0300

    [FIX] microsoft_calendar: validation error creating events from microsoft
    Before this commit, for some synced users, a weird behavior was happening when an event was created in Microsoft: the event info was being sent to Odoo with a different organizer than 'self.env.user' even though it was the same user creating the event.
    
    After this commit, a condition checking if the event was created in Microsoft is added for skipping the checking since only events created in Odoo can have different organizers during their creation.
    
    Issue from: 3450045
    
    X-original-commit: 176ca9789b0c2e032773f3ba2c72ab7e2ed24d91

--- addons/microsoft_calendar/models/calendar.py
+++ addons/microsoft_calendar/models/calendar.py
@@ -95,7 +95,9 @@ class Meeting(models.Model):
 
     def _check_organizer_validation(self, sender_user, partner_included):
         """ Check if the proposed event organizer can be set accordingly. """
-        if sender_user and sender_user != self.env.user:
+        # Edge case: events created or updated from Microsoft should not check organizer validation.
+        change_from_microsoft = self.env.context.get('dont_notify', False)
+        if sender_user and sender_user != self.env.user and not change_from_microsoft:
             current_sync_status = self._check_microsoft_sync_status()
             sender_sync_status = self.with_user(sender_user)._check_microsoft_sync_status()
             if not sender_sync_status and current_sync_status:
