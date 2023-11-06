01647232c2568d6cf6cbde5e3b744e44aa2466a5
Roy Le <roy.le@viindoo.com>
2023-11-06 14:30:39 +0700

    [FIX] account: A traceback occurs because `self.ids` is empty in the context of the creation of a new record

--- addons/account/models/partner.py
+++ addons/account/models/partner.py
@@ -297,6 +297,10 @@ class ResPartner(models.Model):
 
     @api.depends_context('company')
     def _credit_debit_get(self):
+        if not self.ids:
+            self.debit = False
+            self.credit = False
+            return
         tables, where_clause, where_params = self.env['account.move.line'].with_context(state='posted', company_id=self.env.company.id)._query_get()
         where_params = [tuple(self.ids)] + where_params
         if where_clause:
