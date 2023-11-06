d1cbe38bb9b1a4b5ae350f7f957ffe67a513c836
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2023-11-06 12:22:02 +0000

    [FIX] hr_holidays: fix _get_responsible_for_approval
    leave_manager_id is used to create activity when new allocation request is created.
    In case when employee doesnt have leave_manager_id set,
    current user (employee creating allocation request) is used for activity.
    As a result manager doesn't receive email about allocation request by employee.
    Fix is to use employee's parent_id instead.
    
    X-original-commit: cef006c41eedc02e2ba43efa30dd0a258b7d803b

--- addons/hr_holidays/models/hr_leave_allocation.py
+++ addons/hr_holidays/models/hr_leave_allocation.py
@@ -699,7 +699,21 @@ class HolidaysAllocation(models.Model):
         self.ensure_one()
         responsible = self.env.user
 
+<<<<<<< HEAD
         if self.validation_type == 'officer' or self.validation_type == 'set':
+||||||| parent of 73e070df41b1 (temp)
+        if self.validation_type == 'manager' or (self.validation_type == 'both' and self.state == 'confirm'):
+            if self.employee_id.leave_manager_id:
+                responsible = self.employee_id.leave_manager_id
+        elif self.validation_type == 'hr' or (self.validation_type == 'both' and self.state == 'validate1'):
+=======
+        if self.validation_type == 'manager' or (self.validation_type == 'both' and self.state == 'confirm'):
+            if self.employee_id.leave_manager_id:
+                responsible = self.employee_id.leave_manager_id
+            elif self.employee_id.parent_id.user_id:
+                responsible = self.employee_id.parent_id.user_id
+        elif self.validation_type == 'hr' or (self.validation_type == 'both' and self.state == 'validate1'):
+>>>>>>> 73e070df41b1 (temp)
             if self.holiday_status_id.responsible_id:
                 responsible = self.holiday_status_id.responsible_id
 
