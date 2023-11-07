ad64f2c0d257930f6a0f87f4db1790e7146abf21
Eteil Djoumatchoua(etdj) etdj@odoo.com <etdj@odoo.com>
2023-11-07 11:21:16 +0100

    [FIX] portal: display avatar in portal chatter even if the user is not logged
    Steps:
    - Create a sale order
    - Send it to the customer
    - Go to the link in the mail
    
    Issue:
    The avatar of the sender is not displayed in the portal chatter but should be.
    
    Reason:
    Since 16.0 some rules have been added on ``sale.order`` Model, so you need to log in to get the read right
    
    Solution:
    After discussing with reth, we can create a new route in ``portal`` controller just for avatar to be showed
    by bypassing record access rules but ensure token used is valid.
    
    opw-3383983

--- addons/portal/static/src/js/portal_chatter.js
+++ addons/portal/static/src/js/portal_chatter.js
@@ -101,8 +101,14 @@ var PortalChatter = publicWidget.Widget.extend({
      * @returns {Array}
      */
     preprocessMessages(messages) {
+        const token = this.options['token'];
         _.each(messages, function (m) {
-            m['author_avatar_url'] = _.str.sprintf('/web/image/%s/%s/author_avatar/50x50', 'mail.message', m.id);
+            if (token) {
+                m['author_avatar_url'] = _.str.sprintf('/mail/avatar/mail.message/%s/author_avatar/50x50?access_token=%s', m.id, token);
+            } else {
+                m['author_avatar_url'] = _.str.sprintf('/mail/avatar/mail.message/%s/author_avatar/50x50', m.id);
+
+            }
             m['published_date_str'] = _.str.sprintf(_t('Published on %s'), moment(time.str_to_datetime(m.date)).format('MMMM Do YYYY, h:mm:ss a'));
             m['body'] = Markup(m.body);
         });
