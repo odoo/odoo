c862d3f19e506e25224c489cde5f16a659787492
roen-odoo <roen@odoo.com>
2023-11-07 16:50:53 +0100

    [FIX] pos_mrp: fix COGS with kit made of kit
    Current behavior:
    When you sell a kit made of kit, the COGS are not registered correctly
    in the invoice.
    
    Steps to reproduce:
    - Create Kit A with product A
    - Create Kit B with product B
    - Create Kit Final with kit A and kit B
    - Make sure the Category of the products is in FIFO automated
    - Open PoS, and sell Kit Final and invoice it.
    - Go in the backend, and check the invoice lines of the order.
    - The COGS of the products are not registered correctly.
    
    Note:
    This was happening because the price unit of the kit was not calculated
    correctly. Because when the product was a kit made of kit, the bom was
    not exploded to consider all the components of the kit.
    
    opw-3548604
    
    closes odoo/odoo#142246
    
    X-original-commit: b8b3945ee0e4a82e18f85d8bbeb1a32654088308
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/pos_mrp/models/pos_order.py
+++ addons/pos_mrp/models/pos_order.py
@@ -12,3 +12,13 @@ class PosOrderLine(models.Model):
             return super()._get_stock_moves_to_consider(stock_moves, product)
         ml_product_to_consider = (product.bom_ids and bom.bom_line_ids.mapped('product_id').mapped('id')) or [product.id]
         return stock_moves.filtered(lambda ml: ml.product_id.id in ml_product_to_consider and ml.bom_line_id)
+
+class PosOrder(models.Model):
+    _inherit = "pos.order"
+
+    def _get_pos_anglo_saxon_price_unit(self, product, partner_id, quantity):
+        bom = product.env['mrp.bom']._bom_find(product, company_id=self.mapped('picking_ids.move_line_ids').company_id.id, bom_type='phantom')[product]
+        if not bom:
+            return super()._get_pos_anglo_saxon_price_unit(product, partner_id, quantity)
+        dummy, components = bom.explode(product, quantity)
+        return sum(super(PosOrder, self)._get_pos_anglo_saxon_price_unit(comp[1]['product'], partner_id, comp[1]['qty']) for comp in components)
