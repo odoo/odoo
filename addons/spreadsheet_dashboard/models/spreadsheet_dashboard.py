6da92f2d56c4fe4d4667c78b36a9d6bf6f27e003
Lucas Lefèvre (lul) <lul@odoo.com>
2023-11-08 13:14:02 +0000

    [FIX] spreadsheet_dashboard: copy dashboard
    This commit is the communitiy counter-part to allow copying a dashboard.
    
    Yet another 'copy' overrride to add the same postfix!
    
    closes odoo/odoo#144084
    
    Task: 3588237
    X-original-commit: b39f63b1509e6dc2149b1a09da5a877d45e36be3
    Related: odoo/enterprise#51712
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet_dashboard/models/spreadsheet_dashboard.py
+++ addons/spreadsheet_dashboard/models/spreadsheet_dashboard.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import _, fields, models
 
 class SpreadsheetDashboard(models.Model):
     _name = 'spreadsheet.dashboard'
@@ -10,3 +10,11 @@ class SpreadsheetDashboard(models.Model):
     dashboard_group_id = fields.Many2one('spreadsheet.dashboard.group', required=True)
     sequence = fields.Integer()
     group_ids = fields.Many2many('res.groups', default=lambda self: self.env.ref('base.group_user'))
+
+    def copy(self, default=None):
+        self.ensure_one()
+        if default is None:
+            default = {}
+        if 'name' not in default:
+            default['name'] = _("%s (copy)") % self.name
+        return super().copy(default=default)
