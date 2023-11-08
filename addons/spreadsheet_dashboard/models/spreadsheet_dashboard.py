d11790c5a47d1ce4fc0e53cf7f630c2945dacb14
Lucas Lefèvre (lul) <lul@odoo.com>
2023-11-08 13:14:02 +0000

    [FIX] spreadsheet_dashboard: inverse raw string
    This commit is the communitiy counter-part to allow copying a dashboard.
    With this commit, we can now write on 'raw' field. It allows to give a value
    for the field when calling 'copy'.
    
    Yet another 'copy' overrride to add the same postfix!
    
    Task: 3588237
    X-original-commit: 8067e7d44c5e2c4801e23afbe0a503d382318400

--- addons/spreadsheet_dashboard/models/spreadsheet_dashboard.py
+++ addons/spreadsheet_dashboard/models/spreadsheet_dashboard.py
@@ -14,7 +14,7 @@ class SpreadsheetDashboard(models.Model):
     name = fields.Char(required=True, translate=True)
     dashboard_group_id = fields.Many2one('spreadsheet.dashboard.group', required=True)
     data = fields.Binary(required=True, default=lambda self: empty_spreadsheet_data_base64())
-    raw = fields.Binary(compute='_compute_raw')
+    raw = fields.Binary(compute='_compute_raw', inverse='_inverse_raw')
     thumbnail = fields.Binary()
     sequence = fields.Integer()
     group_ids = fields.Many2many('res.groups', default=lambda self: self.env.ref('base.group_user'))
@@ -24,6 +24,10 @@ class SpreadsheetDashboard(models.Model):
         for dashboard in self:
             dashboard.raw = base64.decodebytes(dashboard.data)
 
+    def _inverse_raw(self):
+        for dashboard in self:
+            dashboard.data = base64.encodebytes(dashboard.raw)
+
     @api.onchange('data')
     def _onchange_data_(self):
         if self.data:
@@ -32,3 +36,11 @@ class SpreadsheetDashboard(models.Model):
                 json.loads(data_str)
             except:
                 raise ValidationError(_('Invalid JSON Data'))
+
+    def copy(self, default=None):
+        self.ensure_one()
+        if default is None:
+            default = {}
+        if 'name' not in default:
+            default['name'] = _("%s (copy)") % self.name
+        return super().copy(default=default)
