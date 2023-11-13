1163497cc55d7bf339610fc24ca83ccc00162e2a
FrancoisGe <fge@odoo.com>
2023-11-13 10:29:45 +0100

    [FIX] web: image/sign field doesn't correctly update its unique
    Before this commit, if the value of an image/sign field is modified by an
    onchange and the record is saved manually, the old image is displayed.
    
    Why:
    ImageField's rawCacheKey, which is used to know if the image has been updated,
    is not updated. So we will reuse the old image.
    
    Solution:
    rawCacheKey becomes a getter that always returns the current value of __last_updated.
    
    How to reproduce:
    - Go to a form view with a char field and an image field
    - Edit the char field
    - An onchange is performed and modifies the value of the image field
    - The new image is displayed
    - Click on the save button
    
    Before this commit:
        The old image is displayed
    
    After this commit:
        The new image is displayed
    
    closes odoo/odoo#142694
    
    X-original-commit: daab70796f57fc9ea419099d6cf671fb86456c23
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/web/static/tests/views/fields/signature_field_tests.js
+++ addons/web/static/tests/views/fields/signature_field_tests.js
@@ -1,10 +1,13 @@
 /** @odoo-module **/
 import {
     click,
+    clickSave,
+    editInput,
     getFixture,
     makeDeferred,
     nextTick,
     patchWithCleanup,
+    triggerEvent,
     triggerEvents,
 } from "@web/../tests/helpers/utils";
 import { makeView, setupViewRegistries } from "@web/../tests/views/helpers";
@@ -12,6 +15,11 @@ import { NameAndSignature } from "@web/core/signature/name_and_signature";
 
 let serverData;
 let target;
+
+function getUnique(target) {
+    const src = target.dataset.src;
+    return new URL(src).searchParams.get("unique");
+}
 
 QUnit.module("Fields", (hooks) => {
     hooks.beforeEach(() => {
@@ -230,5 +238,165 @@ QUnit.module("Fields", (hooks) => {
         const sign3 = target.querySelector("[name='sign3'] .o_signature");
         assert.strictEqual(sign3.style.width, "120px");
         assert.strictEqual(sign3.style.height, "40px");
+    });
+
+    QUnit.test(
+        "clicking save manually after changing signature should change the unique of the image src",
+        async function (assert) {
+            serverData.models.partner.fields.foo = { type: "char" };
+            serverData.models.partner.onchanges = { foo: () => {} };
+
+            const rec = serverData.models.partner.records.find((rec) => rec.id === 1);
+            rec.sign = "3 kb";
+            rec.write_date = "2022-08-05 08:37:00"; // 1659688620000
+
+            // 1659692220000, 1659695820000
+            const lastUpdates = ["2022-08-05 09:37:00", "2022-08-05 10:37:00"];
+            let index = 0;
+
+            await makeView({
+                type: "form",
+                resModel: "partner",
+                resId: 1,
+                serverData,
+                arch: /* xml */ `
+                    <form>
+                        <field name="foo" />
+                        <field name="sign" widget="signature" />
+                    </form>`,
+                mockRPC(route, { method, args }) {
+                    if (route === "/web/sign/get_fonts/") {
+                        return {};
+                    }
+                    if (method === "web_save") {
+                        assert.step("web_save");
+                        args[1].write_date = lastUpdates[index];
+                        args[1].sign = "4 kb";
+                        index++;
+                    }
+                },
+            });
+            assert.strictEqual(
+                getUnique(target.querySelector(".o_field_signature img")),
+                "1659688620000"
+            );
+
+            await click(target, ".o_field_signature img", { skipVisibilityCheck: true });
+            assert.containsOnce(target, ".modal canvas");
+
+            let canvas = target.querySelector(".modal canvas");
+            canvas.setAttribute("width", "2px");
+            canvas.setAttribute("height", "2px");
+            let ctx = canvas.getContext("2d");
+            ctx.beginPath();
+            ctx.strokeStyle = "blue";
+            ctx.moveTo(0, 0);
+            ctx.lineTo(0, 2);
+            ctx.stroke();
+            await triggerEvent(target, ".o_web_sign_signature", "change");
+            await click(target, ".modal-footer .btn-primary");
+
+            const MYB64 = `iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAAXNSR0IArs4c6QAAABRJREFUGFdjZGD438DAwNjACGMAACQlBAMW7JulAAAAAElFTkSuQmCC`;
+            assert.strictEqual(
+                target.querySelector("div[name=sign] img").dataset.src,
+                `data:image/png;base64,${MYB64}`
+            );
+
+            await editInput(target, ".o_field_widget[name='foo'] input", "grrr");
+            assert.strictEqual(
+                target.querySelector("div[name=sign] img").dataset.src,
+                `data:image/png;base64,${MYB64}`
+            );
+
+            await clickSave(target);
+            assert.verifySteps(["web_save"]);
+            assert.strictEqual(
+                getUnique(target.querySelector(".o_field_signature img")),
+                "1659692220000"
+            );
+
+            await click(target, ".o_field_signature img", { skipVisibilityCheck: true });
+            assert.containsOnce(target, ".modal canvas");
+
+            canvas = target.querySelector(".modal canvas");
+            canvas.setAttribute("width", "2px");
+            canvas.setAttribute("height", "2px");
+            ctx = canvas.getContext("2d");
+            ctx.beginPath();
+            ctx.strokeStyle = "blue";
+            ctx.moveTo(0, 0);
+            ctx.lineTo(2, 0);
+            ctx.stroke();
+            await triggerEvent(target, ".o_web_sign_signature", "change");
+            await click(target, ".modal-footer .btn-primary");
+
+            const MYB64_2 = `iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAAXNSR0IArs4c6QAAABVJREFUGFdjZGD438DAwMDACCJAAAAWHgGCN0++VgAAAABJRU5ErkJggg==`;
+            assert.notOk(MYB64 === MYB64_2);
+            assert.strictEqual(
+                target.querySelector("div[name=sign] img").dataset.src,
+                `data:image/png;base64,${MYB64_2}`
+            );
+            await clickSave(target);
+            assert.verifySteps(["web_save"]);
+            assert.strictEqual(
+                getUnique(target.querySelector(".o_field_signature img")),
+                "1659695820000"
+            );
+        }
+    );
+
+    QUnit.test("save record with signature field modified by onchange", async function (assert) {
+        const MYB64 = `iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAAXNSR0IArs4c6QAAABRJREFUGFdjZGD438DAwNjACGMAACQlBAMW7JulAAAAAElFTkSuQmCC`;
+
+        serverData.models.partner.fields.foo = { type: "char" };
+        serverData.models.partner.onchanges = {
+            foo: (data) => {
+                data.sign = MYB64;
+            },
+        };
+
+        const rec = serverData.models.partner.records.find((rec) => rec.id === 1);
+        rec.sign = "3 kb";
+        rec.write_date = "2022-08-05 08:37:00"; // 1659688620000
+
+        // 1659692220000, 1659695820000
+        const lastUpdates = ["2022-08-05 09:37:00", "2022-08-05 10:37:00"];
+        let index = 0;
+
+        await makeView({
+            type: "form",
+            resModel: "partner",
+            resId: 1,
+            serverData,
+            arch: /* xml */ `
+                    <form>
+                        <field name="foo" />
+                        <field name="sign" widget="signature" />
+                    </form>`,
+            mockRPC(route, { method, args }) {
+                if (method === "web_save") {
+                    assert.step("web_save");
+                    args[1].write_date = lastUpdates[index];
+                    args[1].sign = "4 kb";
+                    index++;
+                }
+            },
+        });
+        assert.strictEqual(
+            getUnique(target.querySelector(".o_field_signature img")),
+            "1659688620000"
+        );
+        await editInput(target, "[name='foo'] input", "grrr");
+        assert.strictEqual(
+            target.querySelector("div[name=sign] img").dataset.src,
+            `data:image/png;base64,${MYB64}`
+        );
+
+        await clickSave(target);
+        assert.strictEqual(
+            getUnique(target.querySelector(".o_field_signature img")),
+            "1659692220000"
+        );
+        assert.verifySteps(["web_save"]);
     });
 });
