f503af2c197d01c6dec3b7d9ea8a4b90ad648d76
Thomas Lefebvre (thle) <thle@odoo.com>
2023-11-14 08:27:32 +0000

    [FIX] calendar: trigger activity according to timezone
    Steps to reproduce:
    -------------------
    - be in UTC -5 (America/Lima for example)
        on the system (and so browser) and the user profile;
    - create an event from 4pm to 5pm the next day;
    - be at 2pm today;
    
    Issue:
    ------
    We have a notification in activities for this
    event today, which is one day too soon.
    
    Cause:
    ------
    To find the events that need to be notified to the user, we use a domain which, in this case,
    can be expressed as follows:
    ```
    (
    the `start` of the event must be after "now" (the event has not yet passed)
    OR
    the event `stop` must be after now (the event is in progress)
    )
    AND
    the `start` of the event must be less than the end of the day (the event must be today)
    ```
    
    The end of the day must be determined, but taking into account the user's timezone.
    As the domain has to work with UTC values (because the `start` and `stop` values of the event are in UTC in DB),
    in order to determine the end of the current day, we first need to determine which day the user is in UTC.
    
    Logic before this commit (using the example):
    ```py
    start_dt = datetime.datetime.utcnow() # 19:00 (because 14h in America/Lima -5)
    start_date = timezone(tz).localize(start_dt).astimezone(UTC).date() # 00:00 next day
    end_dt = datetime.datetime.combine(start_date, datetime.time.max) # 23:59 next day
    end_dt = timezone(tz).localize(end_dt).astimezone(UTC) # 4:59 second next day
    ```
    The result (for an America/Lima timezone) is that if the event has its `start`
    in UTC before 4:59 two days later, the event will be notified.
    
    Solution:
    ---------
    Correct the values used to construct the domain.
    
    Logic after this commit (using the example):
    ```py
    start_dt_utc = now_utc.replace(tzinfo=UTC) # 19:00 (because 14h in America/Lima -5)
    start_dt = start_dt_utc.astimezone(user_tz) # 14:00 (because 14h in America/Lima -5)
    stop_dt = datetime.datetime.combine(start_dt.date(), datetime.time.max).replace(tzinfo=user_tz).astimezone(UTC) # 4:59 next day
    ```
    Because 23:59 today using user timezone and then converted to UTC gives 4:59 next day.
    In fact, the day of a user in the America/Lima (UTC -5) timezone ends in UTC at 4:59 the next day.
    
    If this same user creates an event that starts at midnight (and therefore tomorrow),
    the activity must not be displayed before midnight and therefore 5:00 the next day in UTC.
    
    opw-3523558
    
    closes odoo/odoo#144376
    
    X-original-commit: 75374ad17d10c646aaa2fbb5ffe6ba260a2cfd0e
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>
    Signed-off-by: Thomas Lefebvre (thle) <thle@odoo.com>

--- addons/calendar/models/res_users.py
+++ addons/calendar/models/res_users.py
@@ -11,26 +11,56 @@ class Users(models.Model):
     _inherit = 'res.users'
 
     def _systray_get_calendar_event_domain(self):
+        # Determine the domain for which the users should be notified. This method sends notification to
+        # events occurring between now and the end of the day. Note that "now" needs to be computed in the
+        # user TZ and converted into UTC to compare with the records values and "the end of the day" needs
+        # also conversion. Otherwise TZ diverting a lot from UTC would send notification for events occurring
+        # tomorrow.
+        # The user is notified if the start is occurring between now and the end of the day
+        # if the event is not finished.
+        #   |           |
+        #   |===========|===> DAY A (`start_dt`): now in the user TZ
+        #   |           |
+        #   |           | <--- `start_dt_utc`: now is on the right if the user lives
+        #   |           |               in West Longitude (America for example)
+        #   |           |
+        #   |  -------  | <--- `start`: the start of the event (in UTC)
+        #   | | event | |
+        #   |  -------  | <--- `stop`: the stop of the event (in UTC)
+        #   |           |
+        #   |           |
+        #   |           | <--- `stop_dt_utc` = `stop_dt` if user lives in an area of East longitude (positive shift compared to UTC, Belgium for example)
+        #   |           |
+        #   |           |
+        #   |-----------| <--- `stop_dt` = end of the day for DAY A from user point of view (23:59 in this TZ)
+        #   |===========|===> DAY B
+        #   |           |
+        #   |           | <--- `stop_dt_utc` = `stop_dt` if user lives in an area of West longitude (positive shift compared to UTC, America for example)
+        #   |           |
+        now_utc = datetime.datetime.utcnow()
+        start_dt_utc = start_dt = now_utc.replace(tzinfo=UTC)
+        stop_dt_utc = datetime.datetime.combine(now_utc.date(), datetime.time.max).replace(tzinfo=UTC)
+
+        tz = self.env.user.tz
+        if tz:
+            user_tz = timezone(tz)
+            start_dt = start_dt_utc.astimezone(user_tz)
+            stop_dt = datetime.datetime.combine(start_dt.date(), datetime.time.max).replace(tzinfo=user_tz)
+            stop_dt_utc = stop_dt.astimezone(UTC)
+
+        start_date = start_dt.date()
+
         current_user_non_declined_attendee_ids = self.env['calendar.attendee']._search([
             ('partner_id', '=', self.env.user.partner_id.id),
             ('state', '!=', 'declined'),
         ])
-        tz = self.env.user.tz
-        start_dt = datetime.datetime.utcnow()
-        if tz:
-            start_date = timezone(tz).localize(start_dt).astimezone(UTC).date()
-        else:
-            start_date = datetime.date.today()
-        end_dt = datetime.datetime.combine(start_date, datetime.time.max)
-        if tz:
-            end_dt = timezone(tz).localize(end_dt).astimezone(UTC)
 
         return ['&', '|',
                 '&',
                     '|',
-                        ['start', '>=', fields.Datetime.to_string(start_dt)],
-                        ['stop', '>=', fields.Datetime.to_string(start_dt)],
-                    ['start', '<=', fields.Datetime.to_string(end_dt)],
+                        ['start', '>=', fields.Datetime.to_string(start_dt_utc)],
+                        ['stop', '>=', fields.Datetime.to_string(start_dt_utc)],
+                    ['start', '<=', fields.Datetime.to_string(stop_dt_utc)],
                 '&',
                     ['allday', '=', True],
                     ['start_date', '=', fields.Date.to_string(start_date)],
