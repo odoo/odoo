ea249806ea3556b23237ae0e4954a167457b0177
Louis (loti) <loti@odoo.com>
2023-11-14 16:19:26 +0100

    [FIX] website_sale_delivery: do not show unavailable carriers
    Carriers with no/invalid/incompatible rules shouldn't be displayed in the checkout process.
    
    Cherry-picked from https://github.com/odoo/odoo/commit/5052b9e4a15155c694cf005fdf330997770c6cac and added tests.
    
    task-3284612
    opw-3413820

--- addons/website_sale_delivery/tests/test_controller.py
+++ addons/website_sale_delivery/tests/test_controller.py
@@ -4,13 +4,16 @@
 from unittest.mock import patch
 
 from odoo.exceptions import UserError, ValidationError
-from odoo.addons.payment.tests.common import PaymentCommon
-from odoo.addons.website_sale_delivery.controllers.main import WebsiteSaleDelivery
-from odoo.addons.website.tools import MockRequest
+from odoo.fields import Command
 from odoo.tests import tagged
 
+from odoo.addons.payment.tests.common import PaymentCommon
+from odoo.addons.sale.tests.common import SaleCommon
+from odoo.addons.website_sale_delivery.controllers.main import WebsiteSaleDelivery
+from odoo.addons.website.tools import MockRequest
+
 @tagged('post_install', '-at_install')
-class TestWebsiteSaleDeliveryController(PaymentCommon):
+class TestWebsiteSaleDeliveryController(PaymentCommon, SaleCommon):
     def setUp(self):
         super().setUp()
         self.website = self.env.ref('website.default_website')
@@ -56,6 +59,55 @@ class TestWebsiteSaleDeliveryController(PaymentCommon):
 
         country_info = self.Controller.country_infos(country=US, mode="shipping")
         self.assertEqual(len(country_info['states']), 0)
+
+    def test_available_methods(self):
+        self.env['delivery.carrier'].search([]).action_archive()
+        self.product_delivery_poste = self.env['product.product'].create({
+            'name': 'The Poste',
+            'type': 'service',
+            'categ_id': self.env.ref('delivery.product_category_deliveries').id,
+            'sale_ok': False,
+            'purchase_ok': False,
+            'list_price': 20.0,
+        })
+        self.env['delivery.carrier'].create([
+            {
+                'name': 'Over 300',
+                'delivery_type': 'base_on_rule',
+                'product_id': self.product_delivery_poste.id,
+                'website_published': True,
+                'price_rule_ids': [
+                    Command.create({
+                        'operator': '>=',
+                        'max_value': 300,
+                        'variable': 'price',
+                    }),
+                ],
+            }, {
+                'name': 'Under 300',
+                'delivery_type': 'base_on_rule',
+                'product_id': self.product_delivery_poste.id,
+                'website_published': True,
+                'price_rule_ids': [
+                    Command.create({
+                        'operator': '<',
+                        'max_value': 300,
+                        'variable': 'price',
+                    }),
+                ],
+            }, {
+                'name': 'No rules',
+                'delivery_type': 'base_on_rule',
+                'product_id': self.product_delivery_poste.id,
+                'website_published': True,
+            }, {
+                'name': 'Fixed',
+                'product_id': self.product_delivery_poste.id,
+                'website_published': True,
+            },
+        ])
+
+        self.assertEqual(self.empty_order._get_delivery_methods().mapped('name'), ['Under 300', 'Fixed'])
 
     def test_validate_payment_with_no_available_delivery_method(self):
         """
