0f0f75bf9b4978105c2af79d11917e81f94baaff
Michele <dicroce.m@stesi.eu>
2023-11-14 09:12:21 +0100

    [FIX] stock: get only package with available qty in least package removal strategy
    closes odoo/odoo#143070
    
    X-original-commit: 217b500a9455cb84dbf6e7e2a45ad2e421bbb68b
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -617,7 +617,7 @@ class StockQuant(models.Model):
         # Fetch the available packages and contents
         query = self._where_calc(domain)
         query_str, params = query.select('package_id', 'SUM(quantity - reserved_quantity) AS available_qty')
-        query_str += ' GROUP BY package_id ORDER BY available_qty DESC'
+        query_str += ' GROUP BY package_id HAVING SUM(quantity - reserved_quantity) > 0 ORDER BY available_qty DESC'
         self._cr.execute(query_str, params)
         qty_by_package = self._cr.fetchall()
 
