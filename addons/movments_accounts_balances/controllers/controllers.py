bd77cea871220a25d61ed1373d80e2c5108657fc
sameerz09 <sameerz09@hotmail.com>
2023-11-15 17:55:01 +0200

    Improvements to the create_bill method

--- addons/movments_accounts_balances/controllers/controllers.py
+++ addons/movments_accounts_balances/controllers/controllers.py
@@ -14,6 +14,22 @@ class AccountBalance(models.Model):
         return account_data
 
     @api.model
+    def get_analytic_accounts(self):
+        accounts = self.env['account.analytic.account'].search([])
+
+        account_data = [{'id': account.id, 'name': account.name} for account in accounts]
+
+        return account_data
+
+    @api.model
+    def get_all_partners(self):
+        partners = self.env['res.partner'].search([])
+
+        partner_data = [{'id': partner.id, 'name': partner.name} for partner in partners]
+
+        return partner_data
+
+    @api.model
     def get_balance(self, account_id, end_date):
         # Define search criteria to filter account move lines
         domain = [('account_id', '=', account_id),
@@ -45,10 +61,12 @@ class AccountBalance(models.Model):
 
         for line in move_lines:
             analytic_info = self.env['account.analytic.line'].search([('move_line_id', '=', line.id)], limit=1)
-            analytic_account_id = analytic_info.account_id if analytic_info else "",
-            analytic_account_name = analytic_account_id.name if analytic_info else "",
+            analytic_partner_id = analytic_info.partner_id if analytic_info else False
+            partner_name = analytic_partner_id.name if analytic_info else False
+            analytic_account_id = analytic_info.account_id if analytic_info else ""
+            analytic_account_name = analytic_account_id.name if analytic_info else ""
             analytic_account_amount = analytic_info.amount if analytic_info else "",
-            partner_id = line.partner_id.id if line.partner_id else "",
+            partner_id = line.partner_id.id if line.partner_id else ""
             partner_type = None
             if line.partner_id:
                 partner = line.partner_id
@@ -70,7 +88,7 @@ class AccountBalance(models.Model):
                 'analytic_move_id': analytic_info.id,
                 'analytic_account_amount': analytic_account_amount,
                 'analytic_account_name': analytic_account_name,
-                'partner_id': partner_id,
+                'partner_id': line.partner_id.name,
                 'partner_type': partner_type,
 
             })
@@ -96,7 +114,14 @@ class AccountBalance(models.Model):
         for line in move_lines:
             account_info = self.env['account.move.line'].search([('move_id', '=', line.id)], limit=1)
             selected_account_id = account_info.account_id.name if account_info else False
-            
+
+            # partner_name = account_info.name if account_info else False
+            # analytic_account_id = analytic_info.account_id if analytic_info else ""
+            # analytic_account_name = analytic_account_id.name if analytic_info else ""
+            # analytic_account_amount = analytic_info.amount if analytic_info else "",
+            # partner_id = line.partner_id.id if line.partner_id else ""
+            # partner_type = None
+
             # Iterate through the retrieved bills and assemble bill data
             for bill in bills:
                 bill_data.append({
@@ -118,66 +143,45 @@ class AccountBalance(models.Model):
             return response
 
     @api.model
-    def create_bills(self, bill_data):
+    # def create_bill(self, partner_id, invoice_date, due_date, reference, line_data, global_narration):
+    def create_bill(self, bill_vals, partner_id, invoice_date, invoice_date_due, ref, narration):
         """
-        Create bills based on the provided data and associated account.move.line records with analytic lines.
+        Create a bill based on the provided data.
 
-        :param bill_data: A list of dictionaries containing bill information.
-        :return: A response indicating the success or failure of bill creation.
+        :param partner_id: Partner ID for the bill.
+        :param invoice_date: Invoice date for the bill.
+        :param due_date: Due date for the bill.
+        :param reference: Reference for the bill.
+        :param line_data: List of dictionaries containing line data for the bill.
+        :param global_narration: Global narration for the entire bill.
+        :return: The created bill record.
         """
-        # Initialize a list to store the created bill records
-        created_bills = []
+        # Prepare the invoice lines
+        invoice_lines = []
+        for line in bill_vals:
+            invoice_line_vals = {
+                'name': line.get('description', ''),
+                'price_unit': line.get('amount', 0.0),
+                'account_id': line.get('account_id'),
+                'analytic_account_id': line.get('analytic_account_id', False),
+            }
+            invoice_lines.append((0, 0, invoice_line_vals))
 
-        for data in bill_data:
-            # Create a new bill record
-            bill = self.env['account.move'].create({
-                'name': data.get('bill_number'),
-                'date': data.get('bill_date'),
-                'invoice_date': data.get('bill_date'),
-                'invoice_date_due': data.get('bill_date_due'),
-                'ref': data.get('reference'),
-                'narration': data.get('narration'),
-                'partner_id': data.get('supplier_id'),
-                'amount_total': data.get('total_amount'),
-                'move_type': data.get('move_type', 'in_invoice'),  # You may adjust the move_type as needed
-                # Add more fields here as needed
-            })
+        # Create a new bill record
+        bill = self.env['account.move'].create({
 
-            # Create an account.move.line for the bill
-            currency = self.env.user.company_id.currency_id
-            move_line = self.env['account.move.line'].create({
-                'move_id': bill.id,
-                'account_id': data.get('account_id'),  # Replace with the actual account ID
-                'name': data.get('account_entry_description', 'Account Entry Description'),
-                'price_unit': data.get('price_unit'),
-                'quantity': 1,
-                'currency_id': currency.id,
-                # Add more fields as needed
-            })
+            'move_type': 'in_invoice',
+            'partner_id': partner_id,
+            'invoice_date': invoice_date,
+            'invoice_date_due': invoice_date_due,
+            'ref': ref,
+            'narration': narration,
+            'invoice_line_ids': invoice_lines,
+        })
 
-            # Create an account.analytic.line if selected_analytic_id is provided
-            selected_analytic_id = data.get('selected_analytic_id')
-            if selected_analytic_id:
-                analytic_line = self.env['account.analytic.line'].create({
-                    'name': 'Analytic Line Description',  # Customize the description
-                    'account_id': selected_analytic_id,
-                    'move_line_id': move_line.id,
-                    'date': bill.date,
-                    'amount': data.get('price_unit'),
-                })
+        return bill
 
-            # Append the created bill record to the list
-            created_bills.append(bill)
 
-        # You can add error handling or validation logic here
-
-        # Create a response indicating the success of bill creation
-        response = {
-            'message': 'Bills and associated account.move.lines created successfully',
-            'created_bills': [(bill.id, bill.name, bill.amount_total, bill.date) for bill in created_bills],
-        }
-
-        return response
 
     @api.model
     def unpost_entry(self, entry):
@@ -194,6 +198,8 @@ class AccountBalance(models.Model):
             return f"Journal entry (ID: {entry.id}) unposted successfully."
         except Exception as e:
             return f"Failed to unpost the journal entry (ID: {entry.id}): {str(e)}"
+
+
 
     @api.model
     def delete_bills(self, start_date, end_date):
@@ -259,4 +265,21 @@ class AccountBalance(models.Model):
             return f"Bill (ID: {bill.id}) un-reconciled and deleted successfully."
         else:
             return "Bill not found."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
