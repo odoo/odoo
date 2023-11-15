c8c547f00a210124ba093e39700e7e1546526c2b
Sven Fuehr <svfu@odoo.com>
2023-11-15 14:57:11 +0100

    [ADD] l10n_ec_website_sale: adapt eCommerce to allow EDI invoice
    Currently the eCommerce workflow in Ecuador lacks some information
    needed to issue EDI invoices.
    
    The main adaptions needed are:
      * Identification type and number in the checkout process (in the billing address)
      * SRI Payment Method in the eCommerce Payment Method
    
    SRI Payment Method
      * The SRI Payment Method value is required for the EDI
      * In the payment method form view the SRI Payment Method field is hidden
        if the list of fiscal countries of the selected companies does not include Ecuador.
      * Payment methods which do not have an associated SRI Payment Method
        cannot be used in the eCommerce checkout of an Ecuadorian company.
      * A (default) SRI Payment Method has been addeded to some payment methods to simplify the setup.
      * When creating an invoice from a sales order we try to compute the SRI Payment Method as follows:
        We consider all the SRI Payment Methods of the payment methods of the payment transactions associated with the sales order.
        If there is exactly 1 such SRI Payment Method we use this on the invoice.
        This should ensures that we can always set the SRI Payment Method on invoices created through the eCommerce flow.
    
    task-3585823
    
    X-original-commit: a513523ed65ee0a16b410a3ad765ac73bc8dc69f
    Part-of: odoo/odoo#152614

---
+++ addons/l10n_ec_website_sale/models/sale_order.py
@@ -0,0 +1,25 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def _create_invoices(self, grouped=False, final=False, date=None):
+        """ Create invoice(s) for the given Sales Order(s).
+
+        :param bool grouped: if True, invoices are grouped by SO id.
+            If False, invoices are grouped by keys returned by :meth:`_get_invoice_grouping_keys`
+        :param bool final: if True, refunds will be generated if necessary
+        :param date: unused parameter
+        :returns: created invoices
+        :rtype: `account.move` recordset
+        :raises: UserError if one of the orders has no invoiceable lines.
+        """
+        moves = super()._create_invoices(grouped, final, date)
+        for move in moves:
+            if move.transaction_ids:
+                sri_payment_methods = move.transaction_ids.mapped('payment_method_id.l10n_ec_sri_payment_id')
+                if len(sri_payment_methods) == 1:
+                    move.l10n_ec_sri_payment_id = sri_payment_methods
+        return moves
