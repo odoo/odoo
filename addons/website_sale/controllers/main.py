f7cfc788639c05e2bac25d4a782a378d12d9e736
nda <nda@odoo.com>
2023-11-15 21:11:00 +0000

    [FIX] website_sale: forbid payment if an error is displayed
    steps to reproduce:
    - as admin, go to /shop and add a storable product to cart
    - disable all delivery methods
    - use a coupon to set the price to 0
    
    before this commit:
    - the payment button is clickable even if there is a big red
    warning saying "Sorry, we are unable to ship your order"
    
    after this commit:
    - the payment button is hidden if an error is displayed and
    the route /shop/payment/validate is blocked if there is an
    error displayed
    
    opw-3582207-nda
    
    X-original-commit: 48e40a6c1f449a4093fa2a172bf9b6f3c731ad48

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -1467,7 +1467,7 @@ class WebsiteSale(http.Controller):
         }
         return {
             'website_sale_order': order,
-            'errors': [],
+            'errors': self._get_shop_payment_errors(order),
             'partner': order.partner_invoice_id,
             'order': order,
             'payment_action_id': request.env.ref('payment.action_payment_provider').id,
@@ -1485,6 +1485,15 @@ class WebsiteSale(http.Controller):
             'transaction_route': f'/shop/payment/transaction/{order.id}',
             'landing_route': '/shop/payment/validate',
         }
+
+    def _get_shop_payment_errors(self, order):
+        """ Check that there is no error that should block the payment.
+
+        :param sale.order order: The sales order to pay
+        :return: A list of errors (error_title, error_message)
+        :rtype: list[tuple]
+        """
+        return []
 
     @http.route('/shop/payment', type='http', auth='public', website=True, sitemap=False)
     def shop_payment(self, **post):
@@ -1544,6 +1553,12 @@ class WebsiteSale(http.Controller):
         else:
             order = request.env['sale.order'].sudo().browse(sale_order_id)
             assert order.id == request.session.get('sale_last_order_id')
+
+        errors = self._get_shop_payment_errors(order)
+        if errors:
+            first_error = errors[0]  # only display first error
+            error_msg = f"{first_error[0]}\n{first_error[1]}"
+            raise ValidationError(error_msg)
 
         tx = order.get_portal_last_transaction() if order else order.env['payment.transaction']
 
