6eb9e66df9b2189e05b7f0bd6e797708d83a42e3
Djamel Touati <otd@odoo.com>
2023-11-16 17:13:11 +0100

    [FIX] mrp: clear move_raw_id when the product variant is changed
    Steps to reproduce the bug:
    - Create a storable product with two attribute “P1”:
        - Color: Red and Blue
    
    - Create a BoM:
        - Product template: P1
        - BoM lines:
            - C1: 1 unit -> apply on variant P1 red
            - C2: 1 unit -> apply on variant P1 blue
    
    - Create a MO:
        - 1 unit of P1 red
    
    - Only the component C1 is added, which is correct.
    - Change the product to P1 blue.
    
    Problem:
    The C1 component is not removed because to clear the move_raw_id,
    we only check if the BoM is changed. However, in this case, it doesn't
    change.
    
    opw-3591800

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -703,7 +703,8 @@ class MrpProduction(models.Model):
             list_move_raw = [Command.link(move.id) for move in production.move_raw_ids.filtered(lambda m: not m.bom_line_id)]
             if not production.bom_id and not production._origin.product_id:
                 production.move_raw_ids = list_move_raw
-            if any(move.bom_line_id.bom_id != production.bom_id for move in production.move_raw_ids if move.bom_line_id):
+            if any(move.bom_line_id.bom_id != production.bom_id or move.bom_line_id._skip_bom_line(production.product_id)\
+                for move in production.move_raw_ids if move.bom_line_id):
                 production.move_raw_ids = [Command.clear()]
             if production.bom_id and production.product_id and production.product_qty > 0:
                 # keep manual entries
