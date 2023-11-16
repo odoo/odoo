42149dec48dcb89129fc92e7b17a051469a20a7f
Xavier-Do <xdo@odoo.com>
2023-11-16 14:12:46 +0100

    [FIX] base: avoid no autoinstall propagation
    A custom script is modifying the output of
    load_information_from_description_file to disable the
    auto-install of modules during local testing. It was naively adapted for
    v16.0 by replacing the corresponding methods. Since a lru cache was added
    (nice optimization in most cases) this is an issue because running lint
    test afterward will get the cached value with an incorrect autoinstall
    value. It makes sens to avoid reading the file on the filesystem each
    time, but making a deepcopy looks like an acceptable safeguard to avoid
    hard to debug behaviors.
    
    X-original-commit: ad10ff4410ccf7c38f6154480484495bb57c53ff

--- odoo/modules/module.py
+++ odoo/modules/module.py
@@ -348,7 +348,6 @@ def load_manifest(module, mod_path=None):
 
     return manifest
 
-@functools.lru_cache(maxsize=None)
 def get_manifest(module, mod_path=None):
     """
     Get the module manifest.
@@ -361,6 +360,10 @@ def get_manifest(module, mod_path=None):
         when the manifest was not found.
     :rtype: dict
     """
+    return copy.deepcopy(_get_manifest_cached(module, mod_path))
+
+@functools.lru_cache(maxsize=None)
+def _get_manifest_cached(module, mod_path=None):
     return load_manifest(module, mod_path)
 
 def load_information_from_description_file(module, mod_path=None):
