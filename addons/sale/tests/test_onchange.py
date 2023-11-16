9c1dace9e745b6e95745c8171613fd329a4fc074
svs-odoo <svs@odoo.com>
2023-11-16 15:42:58 +0100

    [FIX] sale: product company constrains
    Before this commit, the `product.template` constrains
    `_check_sale_product_company` didn't work if the recordset contains
    products from different companies, causing a traceback.
    
    How to reproduce:
    - Create multiple products at once in different companies. For example,
      in the shell of a newly created DB:
            - company_b, company_a = self.env["res.company"].search([])
            - products = self.env["product.product"].create([
                    {'name': "prod1", 'company_id': company_a.id},
                    {'name': "prod2", 'company_id': company_b.id}])
    -> Traceback.
    
    To fix this issue, this commit groups products by their company in
    `_check_sale_product_company`.
    
    closes odoo/odoo#142341
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/sale/tests/test_onchange.py
+++ addons/sale/tests/test_onchange.py
@@ -68,3 +68,20 @@ class TestSaleOnchanges(TransactionCase):
         })
 
         self.assertFalse(sale_order_line.product_id.id)
+
+    def test_create_products_in_different_companies(self):
+        """ Ensures the product's constrain on `company_id` doesn't block the creation of multiple
+        products in different companies (see `product.template` `_check_sale_product_company`.)
+        """
+        company_a = self.env['res.company'].create({'name': 'Company A'})
+        company_b = self.env['res.company'].create({'name': 'Company B'})
+        products = self.env['product.template'].create([
+            {'name': "Product Test 1", 'company_id': company_a.id},
+            {'name': "Product Test 2", 'company_id': company_b.id},
+            {'name': "Product Test 3", 'company_id': False},
+        ])
+        self.assertRecordValues(products, [
+            {'company_id': company_a.id},
+            {'company_id': company_b.id},
+            {'company_id': False},
+        ])
