0db3d1eec1e77794b6504b23563f8f23607ac808
Robin Lejeune (role) <role@odoo.com>
2023-11-16 11:14:50 +0100

    [FIX] website: fix dropping form snippet
    Since [1], dropping a form snippet generates a `TypeError: Cannot read
    properties of undefined (reading 'disconnect')`.
    Note that it only happens if this is the 1st snippet dropped, and if it
    is dropped as a section, not as inner content of another snippet.
    
    This is due to the SnippetEditor being destroyed before being started,
    as `destroy()` is synchronous but `start()` is async. A field from
    `s_website_form` (specifically the field `.s_website_form_dnone`) is
    removed through `_addHiddenField()` when dropping a new form. The result
    is that it gets destroyed immediately and tries to disconnect an
    observer that doesn't exist yet, but the async start() ends a little
    later - and creates + connects the observer that will never be
    disconnected.
    
    To solve the issue, this commit makes sure (1) in `destroy()` that the
    observer was initiated and (2) in `start()` that if the SnippetEditor
    was destroyed, it should not create an observer.
    
    [1]: https://github.com/odoo/odoo/commit/2e30f2c
    
    task-3598997
    
    closes odoo/odoo#142641
    
    X-original-commit: c7b4035e26f91b08d94899e905362c805445f3c5
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/static/src/js/editor/snippets.editor.js
+++ addons/website/static/src/js/editor/snippets.editor.js
@@ -692,6 +692,12 @@ weSnippetEditor.SnippetEditor.include({
      */
     async start() {
         await this._super(...arguments);
+        // When a SnippetEditor is destroyed before the start() has ended
+        // (which can happen when a widget is immediately removed, as start()
+        // is async but destroy() is not), it could create a useless observer.
+        if (this.isDestroyed()) {
+            return;
+        }
         this._adaptOnOptionResize = throttleForAnimation(this._adaptOnOptionResize.bind(this));
         this.editorResizeObserver = new window.ResizeObserver(entries => {
             // In addition to window resizing, every editor's option that
@@ -711,7 +717,7 @@ weSnippetEditor.SnippetEditor.include({
      * @override
      */
     destroy() {
-        this.editorResizeObserver.disconnect();
+        this.editorResizeObserver?.disconnect();
         return this._super(...arguments);
     },
     /**
