40a46f712fb78d37c8a0a7249a688ef83a26aa97
Jinjiu Liu <jili@odoo.com>
2023-11-16 10:06:39 +0100

    [FIX] web_editor: mobile odoo enter after enter clears content
    Reproduction:
    
    1. On mobile app, open note, write something, type "enter"
    2.  the line is removed
    
    Fix: add a case of inputText for mobile input to make sure the history
    step is correctly set up
    
    Related commit: https://github.com/odoo/odoo/commit/53b4afe747ab6ec2cfc889867e86fb8bc0ac3408
    
    task-3573657
    
    X-original-commit: f6c71c651acd3772789862309b767028c6cc83dc

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -3579,7 +3579,7 @@ export class OdooEditor extends EventTarget {
             } else {
                 this.historyStep();
             }
-        } else if (ev.inputType === 'insertCompositionText') {
+        } else if (['insertText', 'insertCompositionText'].includes(ev.inputType)) {
             this._fromCompositionText = true;
         }
         if (shouldOpenPowerbox) {
