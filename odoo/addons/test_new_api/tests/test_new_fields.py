da20eabd8b09cf978257cb440d991276cd40bca5
Raphael Collet <rco@odoo.com>
2023-11-17 12:15:50 +0000

    [FIX] core: modified() on recursive fields before unlink()
    The following situation happened with module industry_fsm, when trying
    to delete a cancelled sales order corresponding to a task.  When doing
    so, the server crashes with error "Could not find all values of X to
    flush them", which means that a dirty field (pending update) has lost
    its value from cache.
    
    The issue is related to recursive computed fields.  Before deleting a
    record, method unlink() invokes modified(), which determines all the
    fields that depend on the record to be deleted, and marks them to
    recompute.  Those fields should be recomputed after the record is
    deleted, and not before.  We found out that the recursive call to
    modified() made for recursive fields can force the recomputation of the
    recursive field itself before the record is deleted, which causes
    unlink() to crash.
    
    The fix consists in marking the fields for recomputation at the very end
    of method modified(), after all the fields to recompute have been
    determined.  This ensures that the processing of recursive fields always
    uses the current value of the field instead of its recomputed value.
    
    X-original-commit: 1f5293bc63ae7b06fd0df8d509dc7fafaa016a22
    Part-of: odoo/odoo#143659

--- odoo/addons/test_new_api/tests/test_new_fields.py
+++ odoo/addons/test_new_api/tests/test_new_fields.py
@@ -505,6 +505,25 @@ class TestFields(TransactionCaseWithUserDemo):
         baz = foo.create({'name': 'baz', 'parent_id': bar.id})
         self.assertEqual(foo.display_name, 'foo(bar(baz()))')
 
+    def test_12_recursive_unlink(self):
+        order = self.env['test_new_api.recursive.order'].create({'value': 42})
+        line = self.env['test_new_api.recursive.line'].create({'order_id': order.id})
+        task = self.env['test_new_api.recursive.task'].create({'value': 42})
+        self.assertEqual(task.line_id, line)
+        self.assertEqual(line.task_ids, task)
+        self.assertTrue(line.task_number)
+
+        # Before deleting order, the following are marked to recompute:
+        #  - task.line_id (recursive, depends on task.line_id.order_id.value)
+        #  - line.task_number (implicitely depends on line.task_ids.line_id)
+        #
+        # If task.line_id is ever recomputed in order to mark line.task_number,
+        # its recomputed value will be lost in the cache invalidation, and
+        # there will be nothing left to write in the database afterwards!  This
+        # makes the call to unlink() crash in that case.
+        #
+        order.unlink()
+
     def test_12_cascade(self):
         """ test computed field depending on computed field """
         message = self.env.ref('test_new_api.message_0_0')
