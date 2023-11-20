8533c7d86e6ebb3524ea3cc7e9b59e48592154c4
Dylan Kiss (dyki) <dyki@odoo.com>
2023-11-20 16:41:37 +0000

    [FIX] l10n_es{_edi_facturae}{_adm_centers}: add administrative centers
    Currently it's not possible to send facturae invoices through FACe,
    since we did not include administrative centers in the XML.
    
    This commit fixes that by:
    
    - Introducing a new partner type for Administrative Centers, with the
      necessary fields
    - Adding all Administrative Centers linked to a partner on the facturae
      electronic invoice
    
    Since often the three required Administrative Centers (Fiscal, Receiver
    and Payer) are the same, we allow the user to specify multiple roles
    on an Administrative Center.
    
    Demo data was also added for the Administrative Centers and updated to
    pass the facturae validator.
    
    For stable versions, we use a patch module to not break things.
    This should be moved to the main module in master.
    
    task-3599447
    
    closes odoo/odoo#146675
    
    X-original-commit: 7c4e846f023f92f48ee465008769848075757a29
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Dylan Kiss (dyki) <dyki@odoo.com>

--- addons/l10n_es_edi_facturae/wizard/account_move_reversal.py
+++ addons/l10n_es_edi_facturae/wizard/account_move_reversal.py
@@ -5,30 +5,10 @@ class AccountMoveReversal(models.TransientModel):
     _inherit = 'account.move.reversal'
 
     l10n_es_edi_facturae_reason_code = fields.Selection(
-        selection=[
-            ('01', 'Invoice number'),
-            ('02', 'Invoice series'),
-            ('03', 'Date of issue'),
-            ('04', 'Name and surname/company name - Issuer'),
-            ('05', 'Name and surname/company name - Recipient'),
-            ('06', 'Tax identification Issuer/Oblige'),
-            ('07', 'Tax identification Receiver'),
-            ('08', 'Issuer/Oblige Address'),
-            ('09', 'Receiving Address'),
-            ('10', 'Transaction Details'),
-            ('11', 'Tax rate to be applied'),
-            ('12', 'Tax rate to be applied'),
-            ('13', 'Date/Period to apply'),
-            ('14', 'Invoice type'),
-            ('15', 'Statutory letters'),
-            ('16', 'Taxable amount'),
-            ('80', 'Calculation of output quotas'),
-            ('81', 'Calculation of withholding taxes'),
-            ('82', 'Taxable amount modified by return of containers/packaging'),
-            ('83', 'Taxable income modified by discounts and allowances'),
-            ('84', 'Taxable income modified by final, judicial or administrative ruling'),
-            ('85', 'Taxable income modified by unpaid tax assessments. Order of declaration of bankruptcy'),
-        ], string='Spanish Facturae EDI Reason Code', default='10')
+        selection=lambda self: self.env['account.move']._fields['l10n_es_edi_facturae_reason_code']._description_selection(self.env),
+        string='Spanish Facturae EDI Reason Code',
+        default='10'
+    )
 
     def reverse_moves(self, is_modify=False):
         # Extends account_account
