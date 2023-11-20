92b9d36cfbafbfe194446f5551b0e6c7dae73e00
Djamel Touati <otd@odoo.com>
2023-11-20 10:34:26 +0100

    [FIX] stock: avoid error when validating a return
    Steps to reproduce the bug:
    - Create a tracked product “P1”
    - Go to inventory > overview:
    - Create a receipts:
        - select “P1”
        - qty: 4 units
        - Set 4 serial numbers and validate
    
    - Create a return
    - Try to validate
    
    Problem:
    A user error is triggered: "Record does not exist or has been deleted."
    
    When clicking the validate button, we attempt to update the qty_done
    in each `move_lines` with the `quantity_reserved` through a for loop
    iteration. However, when setting the first one, we set the
    "product_uom_qty=1" in the associated move. Since the reserved
    availability is greater, the move is unreserved, and the other three
    `move_lines` are deleted:
    
    https://github.com/odoo/odoo/blob/a9a43362df6a6ec031ea14e09ae28115ece62e04/addons/stock/models/stock_move.py#L675-L679
    
    https://github.com/odoo/odoo/blob/56e7bcf0cb88acf3d60420569ab3eea9bdb19bdb/addons/stock/models/stock_move.py#L758
    
    opw-3553042
    
    closes odoo/odoo#142857
    
    X-original-commit: d070336fcf65d1043535ac4ccc40b608d5906512
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Djamel Touati (otd) <otd@odoo.com>

--- addons/stock/tests/test_picking.py
+++ addons/stock/tests/test_picking.py
@@ -1,4 +1,4 @@
-from odoo.tests import common
+from odoo.tests import common, Form
 from odoo.exceptions import UserError
 
 class TestPicking(common.TransactionCase):
@@ -22,3 +22,55 @@ class TestPicking(common.TransactionCase):
             })
         self.assertEqual(
             er.exception.args[0], f'Sequences {operation_type_1.sequence_id.name} already exist.')
+
+    def test_return_picking_validation_with_tracked_product(self):
+        """
+        Test That return picking can be validated when the product is tracked by serial number
+        - Create an incoming immediate transfer with a tracked picking, validate it
+        - Create a return and validate it
+        """
+        in_picking_type = self.env.ref('stock.picking_type_in')
+        supplier_location = self.env.ref('stock.stock_location_suppliers')
+        stock_location = self.env.ref('stock.stock_location_stock')
+        product = self.env['product.product'].create({
+            'name': 'Test Product',
+            'type': 'product',
+            'tracking': 'serial',
+        })
+        sn_1, sn_2 = [self.env['stock.lot'].create({
+            'name': str(i),
+            'product_id': product.id,
+        }) for i in range(2)]
+        # create an immediate transfer
+        picking_in = self.env['stock.picking'].create({
+            'picking_type_id': in_picking_type.id,
+            'location_id': supplier_location.id,
+            'location_dest_id': stock_location.id,
+            'immediate_transfer': True,
+        })
+        move = self.env['stock.move'].create({
+                'picking_id': picking_in.id,
+                'name': product.name,
+                'product_id': product.id,
+                'quantity_done': 2,
+                'product_uom': product.uom_id.id,
+                'location_id': supplier_location.id,
+                'location_dest_id': stock_location.id,
+        })
+        self.assertEqual(picking_in.state, 'assigned')
+        move.move_line_ids[0].write({'lot_id': sn_1.id, 'qty_done': 1})
+        move.move_line_ids[1].write({'lot_id': sn_2.id, 'qty_done': 1})
+        picking_in.button_validate()
+        # create a return picking
+        stock_return_picking_form = Form(self.env['stock.return.picking']
+            .with_context(active_ids=picking_in.ids, active_id=picking_in.ids[0],
+            active_model='stock.picking'))
+        stock_return_picking = stock_return_picking_form.save()
+        stock_return_picking.product_return_moves.quantity = 2.0
+        stock_return_picking_action = stock_return_picking.create_returns()
+        return_picking = self.env['stock.picking'].browse(stock_return_picking_action['res_id'])
+        self.assertEqual(return_picking.state, 'assigned')
+        wiz = return_picking.button_validate()
+        wiz = Form(self.env['stock.immediate.transfer'].with_context(wiz['context'])).save().process()
+        self.assertEqual(return_picking.move_ids.quantity_done, 2)
+        self.assertEqual(return_picking.state, 'done')
