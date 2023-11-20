6162000171de31486cc07bea5c6a5537129c45dd
Aaron Bohy <aab@odoo.com>
2023-11-20 16:16:56 +0100

    [FIX] web: fix form test failing randomly
    The test 'buttons with attr "special" in dialog close the dialog'
    sometimes fail on runbot because it can't click on 'Create and edit...'
    inside the many2one dropdown. Before this commit, the test edited
    the many2one as follows:
     1) edit input to write a new value (with editInput)
     2) click on the input to open the dropdown
     3) click on 'Create and edit...' in the input
    
    But calling editInput opens the dropdown (even though the opening
    is a bit debounced, which is why it only failed sometimes). So it
    might happens, in rare cases, that the dropdown is already opened
    when we click in the input (step 2), which closes it and makes
    step 3 fail.
    
    This commit changes the test to do something similar as what we do
    in many2one_tests.js: we patch setTimeout to execute the callback
    directly, and thus remove the opening delay. We call editInput
    which triggers the opening of the dropdown, and we click inside
    the dropdown.
    
    Runbot issue 24739
    
    closes odoo/odoo#142776
    
    X-original-commit: c6a1468ae6696b91d8780d2a82c14e9ee7f4574b
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/form/form_view_tests.js
+++ addons/web/static/tests/views/form/form_view_tests.js
@@ -6,7 +6,6 @@ import {
     addRow,
     click,
     clickDiscard,
-    clickOpenM2ODropdown,
     clickOpenedDropdownItem,
     clickSave,
     dragAndDrop,
@@ -4449,6 +4448,10 @@ QUnit.module("Views", (hooks) => {
     });
 
     QUnit.test('buttons with attr "special" in dialog close the dialog', async function (assert) {
+        patchWithCleanup(browser, {
+            setTimeout: (fn) => fn(),
+        });
+
         serverData.views = {
             "product,false,form": `
                 <form>
@@ -4488,7 +4491,6 @@ QUnit.module("Views", (hooks) => {
         });
 
         await editInput(target, "input[id=product_id]", "ABC");
-        await clickOpenM2ODropdown(target, "product_id");
         await clickOpenedDropdownItem(target, "product_id", "Create and edit...");
         assert.containsOnce(target, ".o_dialog", "dialog is present to create the product");
 
@@ -4511,7 +4513,6 @@ QUnit.module("Views", (hooks) => {
         assert.verifySteps(["write RPC"], "write RPC has been made");
 
         await editInput(target, "input[id=product_id]", "XYZ");
-        await clickOpenM2ODropdown(target, "product_id");
         await clickOpenedDropdownItem(target, "product_id", "Create and edit...");
         await click(target.querySelector("button[special=cancel]"));
         assert.containsNone(target, ".o_dialog", "dialog has been closed");
