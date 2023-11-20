f52fa43802f2db9a28388b3f024b41bcff11d450
roen-odoo <roen@odoo.com>
2023-11-20 15:05:54 +0000

    [FIX] pos_loyalty: Consider reward divided by tax as 1 reward
    Current behavior:
    When a reward is applied on an order containing different product with
    different taxes, the rewarded is divided in multiple lines (one per tax)
    This cause issue when calling, the `_updateRewardLines` method.
    Because it will consider each line as a full reward, and therefore will
    apply the reward multiple times even though the reward is only applied
    once.
    
    Steps to reproduce:
    - Create a reward with a discount of 5$ in exchange of 100 points
    - The reward should give 1 point per 1$ spent
    - Create a product with a price of 100$ and a tax of 10%
    - Create a product with a price of 100$ and no tax
    - Open the POS and add the 2 products to the order
    - Select a customer, and click the reward button
    - The reward will be applied 2 times (4 reward lines are created)
    
    opw-3583174
    
    closes odoo/odoo#144785
    
    X-original-commit: 782cd627cbd20bd5b3d2b833d949af94d1655f65
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/pos_loyalty/static/src/js/Loyalty.js
+++ addons/pos_loyalty/static/src/js/Loyalty.js
@@ -671,6 +671,7 @@ const PosLoyaltyOrder = (Order) =>
                     args: {
                         product: line.reward_product_id,
                     },
+                    reward_identifier_code: line.reward_identifier_code,
                 };
                 if (
                     claimedReward.reward.program_id.program_type === "gift_card" ||
@@ -679,7 +680,7 @@ const PosLoyaltyOrder = (Order) =>
                     paymentRewards.push(claimedReward);
                 } else if (claimedReward.reward.reward_type === "product") {
                     productRewards.push(claimedReward);
-                } else {
+                } else if (!otherRewards.some(reward => reward.reward_identifier_code === claimedReward.reward_identifier_code)){
                     otherRewards.push(claimedReward);
                 }
                 this.orderlines.remove(line);
