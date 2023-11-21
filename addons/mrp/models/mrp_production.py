207d88d5eb5e8ce1d8dba65073ddb7f9c7d2cd67
Tiffany Chang (tic) <tic@odoo.com>
2023-11-21 14:52:40 +0100

    [FIX] mrp: avoid lot_id req for manual consumption
    During previous fix: odoo/odoo#141797, the `not move.picked` check was
    moved to earlier in the if statement to avoid unnecessarily looping
    through the move lines, but the ( was forgotten to be moved with it.
    Since we don't want to force all manual consumption marked moves to have
    a lot_id, let's move the (.
    
    X-original-commit: 55d01456fa7cc3034a07734103e404b0a4072a18
    Part-of: odoo/odoo#143302

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -2638,7 +2638,7 @@ class MrpProduction(models.Model):
                 continue
             rounding = move.product_uom.rounding
             if move.manual_consumption:
-                if move.has_tracking in ('serial', 'lot') and not move.picked or (any(not line.lot_id for line in move.move_line_ids if line.quantity and line.picked)):
+                if move.has_tracking in ('serial', 'lot') and (not move.picked or any(not line.lot_id for line in move.move_line_ids if line.quantity and line.picked)):
                     missing_lot_id_products += "\n  - %s" % move.product_id.display_name
         if missing_lot_id_products:
             error_msg = _("You need to supply Lot/Serial Number for products and 'picked' them:") + missing_lot_id_products
