6adca7e0e7818678db80ba34b584986ecf556a77
Victor Piryns (pivi) <pivi@odoo.com>
2023-11-21 15:20:43 +0000

    [FIX] hr_expense: set main attachment on `hr.expense.sheet`
    Issue:
    Main attachments on expense sheets was never correctly set, leading
    to missing attachment previous in the hr.expense.sheet form view,
    and unnecessary RPC requests to `register_main_attachment`, because
    the attachment was never set.
    
    Steps to reproduce:
    - Install Expenses
    - Create 2 different expenses with different attachments
    - Create a report from those 2 expenses, save it
    - Notice there is nothing in the preview for the attachments, but if
      you zoom in or zoom out the page, then you can see it. Also
      swiping on the list of attachments isn't persistent, once you
      refresh the page, the main attachment we selected lastly is lost.
    
    Cause:
    There are 2 main issues:
    - `hr.expense.sheet` has `message_main_attachment_id`, but it's
      never set.
    - When writing an attachment via `register_main_attachment`, we
      pass only the id of the attachment. Then in the backend, the
      related record is based on the model of the attachment, but the
      attachment is linked to `hr.expense`, not `hr.expense.sheet`.
      Therefor we re-write the same attachment on the record we read the
      attachment from. And then the frontend will continue to call
      `register_main_attachment` everytime it tries to find the
      attachments linked to the sheet, but it's never done.
    
    Fix:
    - Add a compute to initially set the value of the
      `message_main_attachment_id` based on the spec "we take the first
      line that has an attachment and we set it on the sheet"
    - The `hr.expense.sheet` now maintains a copies of the attachments
      linked to it's expenses, for proper ACL and functionality of
      `register_main_attachment`.
    
    Affected versions:
    16.0 up to master = saas-17.1
    
    Reference:
    task-3572440
    
    closes odoo/odoo#150387
    
    X-original-commit: 70b3d4ab7190a3c9c4565d46a791b5f821b3dc10
    Signed-off-by: Cedric Snauwaert <csn@odoo.com>
    Signed-off-by: Piryns Victor (pivi) <pivi@odoo.com>

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -88,6 +88,7 @@ class HrExpense(models.Model):
         ("company_account", "Company")
     ], default='own_account', tracking=True, states={'done': [('readonly', True)], 'approved': [('readonly', True)], 'reported': [('readonly', True)]}, string="Paid By")
     attachment_number = fields.Integer('Number of Attachments', compute='_compute_attachment_number')
+    attachment_ids = fields.One2many('ir.attachment', 'res_id', domain="[('res_model', '=', 'hr.expense')]", string="Attachments")
     state = fields.Selection(
         selection=[
             ('draft', 'To Report'),
@@ -428,8 +429,11 @@ class HrExpense(models.Model):
                 raise UserError(_('You cannot delete a posted or approved expense.'))
 
     def write(self, vals):
+        expense_to_previous_sheet = {}
         if 'sheet_id' in vals:
             self.env['hr.expense.sheet'].browse(vals['sheet_id']).check_access_rule('write')
+            for expense in self:
+                expense_to_previous_sheet[expense] = expense.sheet_id
         if 'tax_ids' in vals or 'analytic_distribution' in vals or 'account_id' in vals:
             if any(not expense.is_editable for expense in self):
                 raise UserError(_('You are not authorized to edit this expense report.'))
@@ -447,7 +451,34 @@ class HrExpense(models.Model):
                     self.sheet_id.write({'employee_id': vals['employee_id']})
                 elif len(employees) > 1:
                     self.sheet_id = False
+        if 'sheet_id' in vals:
+            # The sheet_id has been modified, either by an explicit write on sheet_id of the expense,
+            # or by processing a command on the sheet's expense_line_ids.
+            # We need to delete the attachments on the previous sheet coming from the expenses that were modified,
+            # and copy the attachments of the expenses to the new sheet,
+            # if it's a no-op (writing same sheet_id as the current sheet_id of the expense),
+            # nothing should be done (no unlink then copy of the same attachments)
+            attachments_to_unlink = self.env['ir.attachment']
+            for expense in self:
+                previous_sheet = expense_to_previous_sheet[expense]
+                checksums = set((expense.attachment_ids - previous_sheet.expense_line_ids.attachment_ids).mapped('checksum'))
+                attachments_to_unlink += previous_sheet.attachment_ids.filtered(lambda att: att.checksum in checksums)
+                if vals['sheet_id'] and expense.sheet_id != previous_sheet:
+                    for attachment in expense.attachment_ids.with_context(sync_attachment=False):
+                        attachment.copy({
+                            'res_model': 'hr.expense.sheet',
+                            'res_id': vals['sheet_id'],
+                        })
+            attachments_to_unlink.with_context(sync_attachment=False).unlink()
         return res
+
+    def unlink(self):
+        attachments_to_unlink = self.env['ir.attachment']
+        for sheet in self.sheet_id:
+            checksums = set((sheet.expense_line_ids.attachment_ids & self.attachment_ids).mapped('checksum'))
+            attachments_to_unlink += sheet.attachment_ids.filtered(lambda att: att.checksum in checksums)
+        attachments_to_unlink.with_context(sync_attachment=False).unlink()
+        return super().unlink()
 
     @api.model
     def get_empty_list_help(self, help_message):
@@ -925,6 +956,8 @@ class HrExpenseSheet(models.Model):
         readonly=False,
         help="The payment method used when the expense is paid by the company.",
     )
+    attachment_ids = fields.One2many('ir.attachment', 'res_id', domain="[('res_model', '=', 'hr.expense.sheet')]", string='Attachments of expenses')
+    message_main_attachment_id = fields.Many2one(compute='_compute_main_attachment', store=True)
     accounting_date = fields.Date(
         string='Accounting Date',
         compute='_compute_accounting_date',
@@ -991,6 +1024,12 @@ class HrExpenseSheet(models.Model):
                 sheet.state = sheet.approval_state
             else:
                 sheet.state = 'draft'
+
+    @api.depends('expense_line_ids.attachment_ids')
+    def _compute_main_attachment(self):
+        for sheet in self:
+            if not sheet.message_main_attachment_id or sheet.message_main_attachment_id not in sheet.attachment_ids:
+                sheet.message_main_attachment_id = sheet.attachment_ids[:1]
 
     @api.depends('company_id.currency_id')
     def _compute_currency_id(self):
@@ -1131,17 +1170,6 @@ class HrExpenseSheet(models.Model):
     # --------------------------------------------
     # Mail Thread
     # --------------------------------------------
-
-    def _get_mail_thread_data_attachments(self):
-        """
-        In order to see in the sheet attachment preview the corresponding
-        expenses' attachments, the latter attachments are added to the fetched data for the sheet record.
-        """
-        self.ensure_one()
-        res = super()._get_mail_thread_data_attachments()
-        expense_ids = self.expense_line_ids
-        expense_attachments = self.env['ir.attachment'].search([('res_id', 'in', expense_ids.ids), ('res_model', '=', 'hr.expense')], order='id desc')
-        return res | expense_attachments
 
     def _track_subtype(self, init_values):
         self.ensure_one()
