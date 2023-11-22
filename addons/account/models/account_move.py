fab87a147b447fbff66a1f2c732e6448ac579164
Aurelien van Delft (avd) <avd@odoo.com>
2023-11-22 13:22:48 +0100

    [FIX] account: speedup _compute_tax_country_id
    Speedup computation of tax_country_id by first filtering
    on record with fiscal_position_id.foreign_vat. Then group records
    without foreign_vat by company_id and call __setitem__ on each group.
    
    In the best case (1 company and no record with foreign_vat) this gives
    a speedup of 7.37s -> 450ms for 1000 records.
    This also speeds up the importing of account_bank_statements.
    
    opw-3576526

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models, Command, _
 from odoo.addons.base.models.decimal_precision import DecimalPrecision
 from odoo.exceptions import RedirectWarning, UserError, ValidationError, AccessError
 from odoo.tools import float_compare, float_is_zero, date_utils, email_split, email_re, html_escape, is_html_empty
-from odoo.tools.misc import formatLang, format_date, get_lang
+from odoo.tools.misc import formatLang, format_date, get_lang, groupby
 from odoo.osv import expression
 
 from datetime import date, timedelta
@@ -2042,11 +2042,11 @@ class AccountMove(models.Model):
 
     @api.depends('company_id.account_fiscal_country_id', 'fiscal_position_id.country_id', 'fiscal_position_id.foreign_vat')
     def _compute_tax_country_id(self):
-        for record in self:
-            if record.fiscal_position_id.foreign_vat:
-                record.tax_country_id = record.fiscal_position_id.country_id
-            else:
-                record.tax_country_id = record.company_id.account_fiscal_country_id
+        foreign_vat_records = self.filtered(lambda r: r.fiscal_position_id.foreign_vat)
+        for fiscal_position_id, record_group in groupby(foreign_vat_records, key=lambda r: r.fiscal_position_id):
+            self.env['account.move'].concat(*record_group).tax_country_id = fiscal_position_id.country_id
+        for company_id, record_group in groupby((self-foreign_vat_records), key=lambda r: r.company_id):
+            self.env['account.move'].concat(*record_group).tax_country_id = company_id.account_fiscal_country_id
 
     @api.depends('tax_country_id.code')
     def _compute_tax_country_code(self):
