afe3532b830acef185183b07665982be05c81790
Josse Colpaert <jco@odoo.com>
2023-11-22 15:05:17 +0100

    [FIX] l10n_es_edi_sii: ImporteTotal and intra-community + tests
    When a line with a factor_percent of -100 is applied in the tax,
    it should be subtracted from the ImporteTotal.
    
    This way, we might think that the total of the invoice should do,
    but we need the amount before application of the withholdings.
    And in the case of DUA it should be the sum of base and tax.
    
    Doing this, we realized that we do not have any tests for
    vendor bills and their refunds for the intra-community case, so
    we added one for vendor bill and one for vendor refund.  (there
    is no -100 line for sale)
    
    In the meantime, we added docstrings on the existing tests
    and realized that the sale of intra-community services, it should
    be no sujeto por reglas de localizacion instead of sujeto.
    (because as well with the fiscal position, the delivery address counts)
    
    We also saw that for intra-community, the clave regimen depended
    on the tags on the amls but the refund repartition lines  were not mapped on the
    tax report line with Intra-community but in a separate refunds section
    (mod303), so we fixed by checking all tags on the tax from all the
    repartition lines.
    
    task 3603788
    
    closes odoo/odoo#146465
    
    X-original-commit: a897880a1da9d388a5f8144c19866f69d6ec9a95
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_es_edi_sii/tests/test_edi_xml.py
+++ addons/l10n_es_edi_sii/tests/test_edi_xml.py
@@ -25,6 +25,7 @@ class TestEdiXmls(TestEsEdiCommon):
         })
 
     def test_010_out_invoice_s_iva10b_s_iva21s(self):
+        """ Invoice with goods and services as they need to be reported in different sections for customer invoices. """
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -97,6 +98,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_020_out_invoice_s_iva10b_s_iva0_ns(self):
+        """ The ns tax is a special case with l10n_es_type ignore and should not appear in what we send"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -148,6 +150,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_030_out_invoice_s_iva10b_s_req014_s_iva21s_s_req52(self):
+        """Recargo de Equivalencia with 2 different taxes and 2 different IVAs as it is reported in the same tag as the IVA"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -230,6 +233,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_040_out_refund_s_iva10b_s_iva10b_s_iva21s(self):
+        """For a customer refund, the amounts need to be reported as negative and also have goods and services separate"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -305,6 +309,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_050_out_invoice_s_iva0_sp_i_s_iva0_ic(self):
+        """An intra-community sale needs to be reported as exempt and intra-community services as no sujeto por reglas de localizacion (no_sujeto_loc)"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -336,7 +341,7 @@ class TestEdiXmls(TestEsEdiCommon):
                         'DesgloseTipoOperacion': {
                             'PrestacionServicios': {
                                 'NoSujeta': {
-                                    'ImportePorArticulos7_14_Otros': 100.0
+                                    'ImporteTAIReglasLocalizacion': 100.0
                                 },
                             },
                             'Entrega': {
@@ -362,6 +367,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_060_out_refund_s_iva0_sp_i_s_iva0_ic(self):
+        """ Intra-community refund of service and good"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -395,7 +401,7 @@ class TestEdiXmls(TestEsEdiCommon):
                         'DesgloseTipoOperacion': {
                             'PrestacionServicios': {
                                 'NoSujeta': {
-                                    'ImportePorArticulos7_14_Otros': -100.0
+                                    'ImporteTAIReglasLocalizacion': -100.0
                                 },
                             },
                             'Entrega': {
@@ -421,6 +427,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_070_out_invoice_s_iva_e_s_iva0_e(self):
+        """ Export of service (no sujeto por reglas de localization) and export of goods (exempt)"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -478,6 +485,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_080_out_refund_s_iva0_sp_i_s_iva0_ic(self):
+        """Customer refund of an intracom good and service"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -511,7 +519,7 @@ class TestEdiXmls(TestEsEdiCommon):
                         'DesgloseTipoOperacion': {
                             'PrestacionServicios': {
                                 'NoSujeta': {
-                                    'ImportePorArticulos7_14_Otros': -100.0,
+                                    'ImporteTAIReglasLocalizacion': -100.0,
                                 },
                             },
                             'Entrega': {
@@ -537,6 +545,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_085_out_refund_s_iva0_sp_i_s_iva0_ic_multi_currency(self):
+        """ Same as test_080 but in multi-currency"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -571,7 +580,7 @@ class TestEdiXmls(TestEsEdiCommon):
                         'DesgloseTipoOperacion': {
                             'PrestacionServicios': {
                                 'NoSujeta': {
-                                    'ImportePorArticulos7_14_Otros': -100.0,
+                                    'ImporteTAIReglasLocalizacion': -100.0,
                                 },
                             },
                             'Entrega': {
@@ -597,6 +606,9 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_090_in_invoice_p_iva10_bc_p_irpf19_p_iva21_sc_p_irpf19(self):
+        """ Vendor bill 10% IVA 19% retention, 21% IVA 19% retention
+        The retention just needs to be ignored basically, but in the ImporteTotal,
+        we need the amount before retention (withholding). """
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -649,6 +661,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_100_in_refund_p_iva10_bc(self):
+        """Vendor bill refund of VAT 10% goods"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -692,6 +705,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_110_in_invoice_p_iva10_bc_p_req014_p_iva21_sc_p_req52(self):
+        """Vendor bill with recargo de equivalencia that needs to be reported within the VAT tax"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -756,6 +770,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_120_in_invoice_p_iva21_sp_ex(self):
+        """ Extra-community vendor bill with reverse charge (-100 line which changes importetotal)"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -783,7 +798,7 @@ class TestEdiXmls(TestEsEdiCommon):
                     'Contraparte': {'NombreRazon': 'partner_b', 'NIF': 'F35999705'},
                     'DescripcionOperacion': 'manual',
                     'ClaveRegimenEspecialOTrascendencia': '01',
-                    'ImporteTotal': 121.0,
+                    'ImporteTotal': 100.0,
                     'FechaRegContable': '02-01-2019',
                     'DesgloseFactura': {
                         'InversionSujetoPasivo': {
@@ -800,7 +815,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_130_in_invoice_p_iva0_ns_p_iva10_bc(self):
-        # TODO make it work
+        """Vendor bill with a line of no sujeto services and a line of 10% goods.  Here, there is no separation between goods and services"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -847,7 +862,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_140_out_invoice_s_iva10b_s_irpf1(self):
-        # TODO: debug
+        """Customer invoice with a 10% VAT and a retention.  The retention should not be deducted from the importetotal."""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -902,6 +917,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_150_in_invoice_p_iva10_bc_p_irpf1(self):
+        """Same as test_140 but for vendor bills"""
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -953,6 +969,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_160_in_refund_p_iva10_bc_p_irpf1(self):
+        """Same as 150 but for supplier refunds.  The amounts need to be negative. """
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -1005,6 +1022,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_165_in_refund_p_iva10_bc_p_irpf1_multi_currency(self):
+        """Same as test_160, but with another currency.  With double the amounts, the result is the same. """
         with freeze_time(self.frozen_today), \
              patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                    new=mocked_l10n_es_edi_call_web_service_sign):
@@ -1058,6 +1076,7 @@ class TestEdiXmls(TestEsEdiCommon):
             })
 
     def test_170_in_invoice_dua(self):
+        """DUA invoice.  The TipoFactura needs to change as well as the importetotal needs to include the base. """
         with freeze_time(self.frozen_today), patch(
                 'odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
                 new=mocked_l10n_es_edi_call_web_service_sign
@@ -1101,3 +1120,110 @@ class TestEdiXmls(TestEsEdiCommon):
                     'ImporteTotal': 605.0,
                     'CuotaDeducible': 105.0
                 }})
+
+    def test_180_in_invoice_iva21_sp_in_iva21_ic_bc(self):
+        """ For intra-community purchase of services and goods, the -100 needs to be taken into account in the importe total.
+        The clave should also change to 09. """
+        with freeze_time(self.frozen_today), \
+             patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
+                   new=mocked_l10n_es_edi_call_web_service_sign):
+            invoice = self.create_invoice(
+                move_type='in_invoice',
+                ref='sup0001',
+                partner_id=self.partner_a.id,
+                l10n_es_registration_date='2019-01-02',
+                invoice_line_ids=[
+                    {
+                        'price_unit': 100.0,
+                        'tax_ids': [(6, 0, self._get_tax_by_xml_id('p_iva21_sp_in').ids)],
+                    },
+                    {
+                        'price_unit': 200.0,
+                        'tax_ids': [(6, 0, self._get_tax_by_xml_id('p_iva21_ic_bc').ids)],
+                    },
+                ],
+            )
+            invoice.action_post()
+
+            generated_files = self._process_documents_web_services(invoice, {'es_sii'})
+            self.assertTrue(generated_files)
+
+            json_file = json.loads(generated_files[0].decode())[0]
+            self.assertEqual(json_file, {
+                'IDFactura': {
+                    'FechaExpedicionFacturaEmisor': '01-01-2019',
+                    'NumSerieFacturaEmisor': 'sup0001',
+                    'IDEmisorFactura': {'IDOtro': {'IDType': '02', 'ID': 'BE0477472701'}}
+                },
+                'FacturaRecibida': {
+                    'TipoFactura': 'F1',
+                    'Contraparte': {'IDOtro': {'IDType': '02', 'ID': 'BE0477472701'}, 'NombreRazon': 'partner_a'},
+                    'DescripcionOperacion': 'manual',
+                    'ClaveRegimenEspecialOTrascendencia': '09',
+                    'ImporteTotal': 300.0,
+                    'FechaRegContable': '02-01-2019',
+                    'DesgloseFactura': {
+                        'DesgloseIVA': {
+                            'DetalleIVA': [
+                                {'BaseImponible': 300.0, 'CuotaSoportada': 63.0, 'TipoImpositivo': 21.0},
+                            ]
+                        }
+                    },
+                    'CuotaDeducible': 63.0
+                },
+                'PeriodoLiquidacion': {'Periodo': '01', 'Ejercicio': '2019'}
+            })
+
+    def test_190_in_refund_iva21_sp_in_iva21_ic_bc(self):
+        """ For intra-community purchase return services and goods, the -100 needs to be taken into account in the importe total.
+        For a refund, the type should change to R4"""
+        with freeze_time(self.frozen_today), \
+             patch('odoo.addons.l10n_es_edi_sii.models.account_edi_format.AccountEdiFormat._l10n_es_edi_call_web_service_sign',
+                   new=mocked_l10n_es_edi_call_web_service_sign):
+            invoice = self.create_invoice(
+                move_type='in_refund',
+                ref='sup0001',
+                partner_id=self.partner_a.id,
+                l10n_es_registration_date='2019-01-02',
+                invoice_line_ids=[
+                    {
+                        'price_unit': 100.0,
+                        'tax_ids': [(6, 0, self._get_tax_by_xml_id('p_iva21_sp_in').ids)],
+                    },
+                    {
+                        'price_unit': 200.0,
+                        'tax_ids': [(6, 0, self._get_tax_by_xml_id('p_iva21_ic_bc').ids)],
+                    },
+                ],
+            )
+            invoice.action_post()
+
+            generated_files = self._process_documents_web_services(invoice, {'es_sii'})
+            self.assertTrue(generated_files)
+
+            json_file = json.loads(generated_files[0].decode())[0]
+            self.assertEqual(json_file, {
+                'IDFactura': {
+                    'FechaExpedicionFacturaEmisor': '01-01-2019',
+                    'NumSerieFacturaEmisor': 'sup0001',
+                    'IDEmisorFactura': {'IDOtro': {'IDType': '02', 'ID': 'BE0477472701'}}
+                },
+                'FacturaRecibida': {
+                    'TipoFactura': 'R4',
+                    'TipoRectificativa': 'I',
+                    'Contraparte': {'IDOtro': {'IDType': '02', 'ID': 'BE0477472701'}, 'NombreRazon': 'partner_a'},
+                    'DescripcionOperacion': 'manual',
+                    'ClaveRegimenEspecialOTrascendencia': '09',
+                    'ImporteTotal': -300.0,
+                    'FechaRegContable': '02-01-2019',
+                    'DesgloseFactura': {
+                        'DesgloseIVA': {
+                            'DetalleIVA': [
+                                {'BaseImponible': -300.0, 'CuotaSoportada': -63.0, 'TipoImpositivo': 21.0},
+                            ]
+                        }
+                    },
+                    'CuotaDeducible': -63.0
+                },
+                'PeriodoLiquidacion': {'Periodo': '01', 'Ejercicio': '2019'}
+            })
