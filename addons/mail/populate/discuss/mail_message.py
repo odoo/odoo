c2c1ea97ec8a70c0c6bdbd7c737f3d117b4bfa3f
Sébastien Theys <seb@odoo.com>
2023-11-22 10:27:18 +0100

    [IMP] mail: improve existing populate, add mail.notification
    - add populate for mail.notification
    - add batch processing and logging for a more pleasant waiting
    - adapt existing numbers to find a better balance between usefulness of
      data and waiting time
    
    closes odoo/odoo#143181
    
    X-original-commit: 544e094384152be2aa7c60f782154b7cc81ae407
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/populate/discuss/mail_message.py
+++ addons/mail/populate/discuss/mail_message.py
@@ -1,8 +1,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import logging
+
 from odoo import models
-from odoo.addons.mail.populate.mail_message import Message as MailMessage
 from odoo.tools import populate
+
+_logger = logging.getLogger(__name__)
 
 
 class Message(models.Model):
@@ -14,11 +17,20 @@ class Message(models.Model):
 
     def _populate(self, size):
         res = super()._populate(size)
+        admin = self.env.ref("base.user_admin").partner_id
         random = populate.Random("mail.message in discuss")
         channels = self.env["discuss.channel"].browse(self.env.registry.populated_models["discuss.channel"])
         messages = []
+        big_done = 0
         for channel in channels.filtered(lambda channel: channel.channel_member_ids):
-            for counter in range(random.randrange({"small": 100, "medium": 1000, "large": 10000}[size])):
+            big = {"small": 80, "medium": 150, "large": 300}[size]
+            small_big_ratio = {"small": 10, "medium": 150, "large": 1000}[size]
+            max_messages = big if random.randint(1, small_big_ratio) == 1 else 60
+            admin_is_member = admin in channel.channel_member_ids.partner_id
+            number_messages = 500 if big_done < 2 and admin_is_member else random.randrange(max_messages)
+            if number_messages >= 500 and admin_is_member:
+                big_done += 1
+            for counter in range(number_messages):
                 messages.append(
                     {
                         "author_id": random.choice(channel.channel_member_ids.partner_id).id,
@@ -28,4 +40,10 @@ class Message(models.Model):
                         "res_id": channel.id,
                     }
                 )
-        return res + self.env["mail.message"].create(messages)
+        batches = [messages[i : i + 1000] for i in range(0, len(messages), 1000)]
+        count = 0
+        for batch in batches:
+            count += len(batch)
+            _logger.info("Batch of mail.message for discuss.channel: %s/%s", count, len(messages))
+            res += self.env["mail.message"].create(batch)
+        return res
--- addons/mail/populate/mail_message.py
+++ addons/mail/populate/mail_message.py
@@ -1,7 +1,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import logging
+
 from odoo import models
 from odoo.tools import populate
+
+_logger = logging.getLogger(__name__)
 
 
 class Message(models.Model):
@@ -12,16 +16,24 @@ class Message(models.Model):
         return super()._populate(size) + self._populate_threads(size, "res.partner")
 
     def _populate_threads(self, size, model_name):
-        comment_subtype = self.env.ref('mail.mt_comment')
-        note_subtype = self.env.ref('mail.mt_note')
+        comment_subtype = self.env.ref("mail.mt_comment")
+        note_subtype = self.env.ref("mail.mt_note")
         random = populate.Random("mail.message")
         partners = self.env["res.partner"].browse(self.env.registry.populated_models["res.partner"])
         threads = self.env[model_name].browse(self.env.registry.populated_models[model_name])
         admin = self.env.ref("base.user_admin").partner_id
         messages = []
-        for thread in threads:
-            for counter in range(random.randrange({"small": 10, "medium": 100, "large": 1000}[size])):
-                author = random.choice(admin + partners)
+        big_done = 0
+        for thread in random.sample(threads, k={"small": 30, "medium": 200, "large": 1000}[size]):
+            max_messages = {"small": 60, "medium": 200, "large": 1000}[size]
+            number_messages = 200 if big_done < 2 else random.randrange(max_messages)
+            if number_messages >= 200:
+                big_done += 1
+            with_admin = False
+            for counter in range(number_messages):
+                author = admin if not with_admin and random.randint(1, 5) == 1 else random.choice(admin + partners)
+                if author == admin:
+                    with_admin = True
                 message_type = random.choice(["email", "comment"]) if author.partner_share else "comment"
                 subtype = comment_subtype if author.partner_share else random.choice(comment_subtype + note_subtype)
                 messages.append(
@@ -34,4 +46,11 @@ class Message(models.Model):
                         "subtype_id": subtype.id,
                     }
                 )
-        return self.env["mail.message"].create(messages)
+        res = self.env["mail.message"]
+        batches = [messages[i : i + 1000] for i in range(0, len(messages), 1000)]
+        count = 0
+        for batch in batches:
+            count += len(batch)
+            _logger.info("Batch of mail.message for %s: %s/%s", model_name, count, len(messages))
+            res += self.env["mail.message"].create(batch)
+        return res
