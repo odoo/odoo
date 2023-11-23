c0d787d3658fc8c37b6faabde860a81bd16a51dd
Adrien Guilliams (adgu) <adgu@odoo.com>
2023-11-23 08:16:35 +0100

    [IMP] point_of_sale: display free on receipt for free orderlines
    For orderlines with a price of 0, the receipt will now display the
    price as 'Free' instead of '0.00'.
    
    closes odoo/odoo#144664
    
    X-original-commit: 16396aad67118ba0b3bfea7e0ab66661f67e42ad
    Related: odoo/enterprise#51986
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/store/models.js
+++ addons/point_of_sale/static/src/app/store/models.js
@@ -1077,7 +1077,10 @@ export class Orderline extends PosModel {
     getDisplayData() {
         return {
             productName: this.get_full_product_name(),
-            price: this.env.utils.formatCurrency(this.get_display_price()),
+            price:
+                this.get_discount_str() === "100"
+                    ? "free"
+                    : this.env.utils.formatCurrency(this.get_display_price()),
             qty: this.get_quantity_str(),
             unit: this.get_unit().name,
             unitPrice: this.env.utils.formatCurrency(this.get_unit_display_price()),
