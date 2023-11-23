13c595b342fbf09e188c5fab99724f4265a94317
Romain Derie <rde@odoo.com>
2023-11-23 16:17:23 +0100

    [FIX] website: make forms work again when inside html fields
    Recent commit [1] in Odoo 15 (which is a backport of commit [2] which
    was merged in Odoo 17) introduced a security layer on forms but only
    for forms which are inside `ir.ui.view`. The forms inside HTML fields
    are thus not working anymore, because those don't receive the required
    signature.
    
    For the record:
    - `ir.ui.view` = `website.page` pages, some part of the controller pages
    - HTML fields = the most part of the editable areas in controller pages
    
    [1]: https://github.com/odoo/odoo/commit/17c6f6f30bf13bd3c303b28d9a314bd76dd8f4dc
    [2]: https://github.com/odoo/odoo/commit/7d25e7bf9243367c87b1b2005587ae728730b49c
    
    opw-3586333
    
    closes odoo/odoo#143139
    
    closes odoo/odoo#143715
    
    Signed-off-by: Jérémy Kersten <jke@odoo.com>
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>
    Co-authored-by: Vranckx Florian (flvr) <flvr@odoo.com>
    Co-authored-by: Romain Derie <rde@odoo.com>

--- addons/website/models/ir_ui_view.py
+++ addons/website/models/ir_ui_view.py
@@ -7,6 +7,7 @@ import werkzeug
 
 from odoo import api, fields, models
 from odoo import tools
+from odoo.addons.website.tools import add_form_signature
 from odoo.exceptions import AccessError
 from odoo.osv import expression
 from odoo.http import request
@@ -469,6 +470,11 @@ class View(models.Model):
         return super()._get_allowed_root_attrs() + [
             'data-bg-video-src', 'data-shape', 'data-scroll-background-ratio',
         ]
+
+    def _get_combined_arch(self):
+        root = super()._get_combined_arch()
+        add_form_signature(root, self.sudo().env)
+        return root
 
     # --------------------------------------------------------------------------
     # Snippet saving
