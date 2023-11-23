2c48029ba12614fb65ab3d681000c3f26e8ff784
jbjourget-b <137054756+jbjourget-b@users.noreply.github.com>
2023-11-23 17:30:54 +0000

    [FIX] base: Update wrong cache invalidation
    Avoid invalidating cache when not needed.
    This improves a lot performances.
    
    Closes #143403
    
    Fixes a bug which causes that: self.flush() and self.clear_caches() were called on every property write/update and delete.
    Known at least in V14/15/16/17.
    
    closes odoo/odoo#145558
    
    X-original-commit: b444330050a4d98e7ade42a30a6a27522b5cf4c8
    Related: odoo/enterprise#52411
    Signed-off-by: Raphael Collet <rco@odoo.com>
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>

--- odoo/addons/base/models/ir_property.py
+++ odoo/addons/base/models/ir_property.py
@@ -145,13 +145,7 @@ class Property(models.Model):
         return r
 
     def unlink(self):
-        default_deleted = False
-        if self._ids:
-            self.env.cr.execute(
-                'SELECT EXISTS (SELECT 1 FROM ir_property WHERE id in %s)',
-                [self._ids]
-            )
-            default_deleted = self.env.cr.rowcount == 1
+        default_deleted = any(not p.res_id for p in self)
         r = super().unlink()
         if default_deleted:
             self.env.registry.clear_cache()
@@ -310,6 +304,7 @@ class Property(models.Model):
             return dict.fromkeys(ids, False)
 
         # retrieve values
+        self.flush_model()
         cr = self.env.cr
         result = {}
         refs = {"%s,%s" % (model, id) for id in ids}
