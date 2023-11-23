bde1046154492121e4e6cf34dba3c88e596bbb73
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2023-11-23 17:09:20 +0100

    [FIX] hr_{,holidays}_attendance, resource: overtime is wrong for multi companies
    Steps to Reproduce:
    
    Create 2 companies > Create employee in 1st company > Enable both companies > Create public holidays company-wise> In company 1 create a public holiday for (13/11/2023) > In company 2 create public holidays (13/11/2023) and (29/11/2023).
    
    Then switch to the one company (where the public holiday was created only for (13/11/2023))> Go to attendance > Configuration > Enable the count extra hours feature > Create attendance date (29/11/2023) for ex. 9 hours > Go to employee form > You may see the extra hour of "1 hour" > Because there is no public holiday on 29/11/2023.
    
    Then enable both companies > Create attendance date (29/11/2023) for ex. 9 hours > Go to employee form > You may see the extra hours of "9 hours" > Because there is a public holiday in another company on 29/11/2023.
    
    Bug Source:
    commit 1407052 fixed the fact that public holidays are not taken into account when computing the overtime, but it doesn't cover the multi companies case.
    
    Solution:
    Verify that the public holiday company and the employee company are the same before applying the compute.
    An optimization was also added to avoid searching for public holidays in db by filtering with the
    employee company.
    
    opw-3597287
    
    X-original-commit: 7ea6aa345c64c05cb23935e11d40bc59497355a8

--- addons/resource/models/resource.py
+++ addons/resource/models/resource.py
@@ -516,7 +516,7 @@ class ResourceCalendar(models.Model):
         tz_dates = {}
         for leave in self.env['resource.calendar.leaves'].search(domain):
             for resource in resources_list:
-                if leave.resource_id.id not in [False, resource.id]:
+                if leave.resource_id.id not in [False, resource.id] or (not leave.resource_id and resource and resource.company_id != leave.company_id):
                     continue
                 tz = tz if tz else timezone((resource or self).tz)
                 if (tz, start_dt) in tz_dates:
