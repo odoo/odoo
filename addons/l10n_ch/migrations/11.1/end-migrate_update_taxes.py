6ba55a9f2636eb966f8a6a40bf11e9d11e1f1ca6
Claire Bretton (clbr) <clbr@odoo.com>
2023-11-24 09:17:48 +0100

    [FIX] l10n_ch: activate 2024 taxes, deactivate outdated taxes
    REMINDER: old taxes = {7.7%, 3.7%, 2.5%}; new taxes = {8.1%, 3.8%, 2.6%}
    
    1. We created 2024 taxes in advance and made them inactive by default
    to avoid confusion for our users, while making them available for those
    who needed them. Now that we are about to enter 2024 its time to active
    new taxes by default and deactivate outdated ones.
    
    We are keeping outdated taxes a bit more for clients that are not closing
    their accounting at the end of the year and might need them for upcoming
    months.
    
    2. Remove the activation of new taxes that was made in the module migration
    since now those taxes are already active.
    
    task-id: 3184792

--- addons/l10n_ch/migrations/11.1/end-migrate_update_taxes.py
+++ addons/l10n_ch/migrations/11.1/end-migrate_update_taxes.py
@@ -4,8 +4,4 @@ from odoo.addons.account.models.chart_template import update_taxes_from_template
 
 
 def migrate(cr, version):
-    new_template_to_tax = update_taxes_from_templates(cr, 'l10n_ch.l10nch_chart_template')
-    if new_template_to_tax:
-        _, new_tax_ids = zip(*new_template_to_tax)
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        env['account.tax'].browse(new_tax_ids).active = True
+    update_taxes_from_templates(cr, 'l10n_ch.l10nch_chart_template')
