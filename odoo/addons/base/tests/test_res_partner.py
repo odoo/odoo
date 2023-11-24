e116bed0356733da8e3016905d5d3639341f1e7a
AH-Yussef <alhy@odoo.com>
2023-11-24 09:27:34 +0000

    [FIX] base: translate partner display_name
    Steps to reproduce:
    - Install Contacts app
    - Go to General Settings and add another language
    - Add a new contact to a parent.
            - Don't add a specific contact name
            - Assign a contact type (delivery address, invoice address, other address)
    - Go to the list or kanban view of the contancts and change the language
    - The type of the contact listed next to the parent name is not translated from English.
    
    Investigation:
    - When the contact name is not set, the `display_name` displayed in both kanban and list views is set by concatinating the parent name with the contact type.
    - The line https://github.com/odoo/odoo/blob/03b7e17faef4075dbbb805bca4e7f40f7fbcc988/odoo/addons/base/models/res_partner.py#L345 in the function `_compute_display_name`, `with_context({})` in particular basically enforce to compute the name in english language regardless of the active language. That actually makes sense as the `display_name` field has `store=True` https://github.com/odoo/odoo/blob/03b7e17faef4075dbbb805bca4e7f40f7fbcc988/odoo/addons/base/models/res_partner.py#L199
    
    Solution:
    - add a computed field that is not stored that gets recomputed on changing the language.
    
    opw-3569171
    
    closes odoo/odoo#147650
    
    X-original-commit: 60b3ae846de59b1f6e71df68e99e29758ba734fe
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>
    Signed-off-by: Ali Hassan Youssef (alhy) <alhy@odoo.com>

--- odoo/addons/base/tests/test_res_partner.py
+++ odoo/addons/base/tests/test_res_partner.py
@@ -394,6 +394,26 @@ class TestPartner(TransactionCaseWithUserDemo):
         self.assertEqual([g['title_count'] for g in groups_data], [2, 4], 'Incorrect number of results')
         self.assertEqual([g['color'] for g in groups_data], [-1, 10], 'Incorrect aggregation of int column')
 
+    def test_display_name_translation(self):
+        self.env['res.lang']._activate_lang('fr_FR')
+        self.env.ref('base.module_base')._update_translations(['fr_FR'])
+
+        res_partner = self.env['res.partner']
+
+        parent_contact = res_partner.create({
+            'name': 'Parent',
+            'type': 'contact',
+        })
+
+        child_contact = res_partner.create({
+            'type': 'other',
+            'parent_id': parent_contact.id,
+        })
+
+        self.assertEqual(child_contact.with_context(lang='en_US').display_name, 'Parent, Other Address')
+
+        self.assertEqual(child_contact.with_context(lang='fr_FR').display_name, 'Parent, Autre adresse')
+
 
 @tagged('res_partner')
 class TestPartnerAddressCompany(TransactionCase):
