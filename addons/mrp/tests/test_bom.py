odoo/odoo

commit f13b4d1ae8c12a1668222e6994dea26a27c6f5d4

[FIX] mrp: BoM overview kit fix
When overviewing a BoM with a "kit" type and with no route,
the availability was always unavailable, wich is wrong if all the parts
are in stock or can be replenished.

Task-3184663

closes odoo/odoo#119311

Related: odoo/enterprise#40141
Signed-off-by: William Henrotin (whe) <whe@odoo.com>

addons/mrp/report/mrp_report_bom_structure.py
@@ -237,7 +237,7 @@ class ReportBomStructure(models.AbstractModel):
             bom_report_line['byproducts_total'] = sum(byproduct['quantity'] for byproduct in byproducts)
             bom_report_line['bom_cost'] *= bom_report_line['cost_share']
 
-        availabilities = self._get_availabilities(product, current_quantity, product_info, bom_key, quantities_info, level, ignore_stock, components)
+        availabilities = self._get_availabilities(product, current_quantity, product_info, bom_key, quantities_info, level, ignore_stock, components, bom_report_line)
         bom_report_line.update(availabilities)
 
         if level == 0:
@@ -513,7 +513,7 @@ class ReportBomStructure(models.AbstractModel):
         return {}
 
     @api.model
-    def _get_availabilities(self, product, quantity, product_info, bom_key, quantities_info, level, ignore_stock=False, components=False):
+    def _get_availabilities(self, product, quantity, product_info, bom_key, quantities_info, level, ignore_stock=False, components=False, report_line=False):
         # Get availabilities according to stock (today & forecasted).
         stock_state, stock_delay = ('unavailable', False)
         if not ignore_stock:
@@ -527,6 +527,10 @@ class ReportBomStructure(models.AbstractModel):
             resupply_state, resupply_delay = ('available', 0)
         elif route_info:
             resupply_state, resupply_delay = self._get_resupply_availability(route_info, components)
+
+        if resupply_state == "unavailable" and route_info == {} and components and report_line:
+            val = self._get_last_availability(report_line)
+            return val
 
         base = {
             'resupply_avail_delay': resupply_delay,
@@ -607,7 +611,6 @@ class ReportBomStructure(models.AbstractModel):
     def _has_attachments(self, data):
         return data['attachment_ids'] or any(self._has_attachments(component) for component in data.get('components', []))
 
-    @api.model
     def _merge_components(self, component_1, component_2):
         qty = component_2['quantity']
         component_1["quantity"] = component_1["quantity"] + qty
@@ -618,3 +621,19 @@ class ReportBomStructure(models.AbstractModel):
             return
         for index in range(len(component_1.get("components"))):
             self._merge_components(component_1["components"][index], component_2["components"][index])
+
+    def _get_last_availability(self, report_line):
+        delay = 0
+        component_max_delay = False
+        for component in report_line["components"]:
+            if component["availability_delay"] is False:
+                component_max_delay = component
+                break
+            elif component["availability_delay"] >= delay:
+                component_max_delay = component
+                delay = component["availability_delay"]
+        return {'resupply_avail_delay': component_max_delay['resupply_avail_delay'],
+                'stock_avail_state': component_max_delay['stock_avail_state'],
+                'availability_display': component_max_delay['availability_display'],
+                'availability_state': component_max_delay['availability_state'],
+                'availability_delay': component_max_delay['availability_delay']}

addons/mrp/static/src/components/bom_overview_control_panel/mrp_bom_overview_control_panel.xml
@@ -19,13 +19,13 @@
             <div class="d-flex flex-column gap-1">
                 <form class="d-flex w-100 gap-2">
                         <label class="visually-hidden" for="bom_quantity">Quantity</label>
-                        <div class="input-group">
+                        <div class="input-group col-xl-4">
                             <div class="input-group-text">Quantity</div>
                             <input id="bom_quantity" type="number" step="any" t-on-change="ev => this.updateQuantity(ev)" t-on-keypress="ev => this.onKeyPress(ev)" t-att-value="props.bomQuantity" min="1" class="o_input form-control text-end"/>
                             <div t-if="props.showOptions.uom" t-esc="props.uomName" class="input-group-text">Quantity</div>
                         </div>
                     <t t-if="props.showVariants" >
-                        <label class="col-xl-2 col-lg-3 col-sm-4">Variant:</label>
+                        <div class="input-group-text">Variant:</div>
                         <div class="col-xl-10 col-lg-9 col-sm-8">
                             <Many2XAutocomplete
                                 value="props.data.name"

commit a3c5953205bb80d52e72a0e7b77f467e37848f04

[FIX] mrp: availability if bom type is not kit
Current behaviour:
When checking the overview of a normal BoM,
the product is available if component is in stock

Expected behaviour:
The product should not be available unless BoM type is kit

Steps to reproduce:
1. Go to Manufacturing
2. Go to Products > Bills of Materials
3. Create a new BoM
4. Product: p1 (and create product p1)
5. Component: p2 (and create product p2)
6. BoM type: Manufacture this product (normal)
7. Save the BoM
8. Click on Overview
9. p1 and p2 are not available (Normal behavior)
10. Go to the p2 product page
11. Click on Update Quantity > new quantity > Apply all
12. Go back to the p1 BoM
13. Click on Overview
14. p1 and p2 are available
15. p1 should only be available if BoM type is kit

Cause of the issue:
Caused by https://github.com/odoo/odoo/commit/f13b4d1ae8c12a1668222e6994dea26a27c6f5d4

opw-3601298

addons/mrp/report/mrp_report_bom_structure.py
@@ -607,7 +607,7 @@ class ReportBomStructure(models.AbstractModel):
         elif route_info:
             resupply_state, resupply_delay = self._get_resupply_availability(route_info, components)
 
-        if resupply_state == "unavailable" and route_info == {} and components and report_line:
+        if resupply_state == "unavailable" and route_info == {} and components and report_line and report_line['phantom_bom']:
             val = self._get_last_availability(report_line)
             return val
 

addons/mrp/tests/test_bom.py
@@ -1488,3 +1488,51 @@ class TestBoM(TestMrpCommon):
         # Check that component 1 is set again and component 2 is removed
         self.assertEqual(mo_form.move_raw_ids._records[0]['product_id'], self.product_1.id)
         self.assertEqual(len(mo_form.move_raw_ids._records), 1)
+
+    def test_availability_bom_type_kit(self):
+        """ Product should only be available if bom type is kit """
+        uom_unit = self.env.ref('uom.product_uom_unit')
+        location = self.env.ref('stock.stock_location_stock')
+        product_one = self.env['product.product'].create({
+            'name': 'Product',
+            'type': 'product',
+            'uom_id': uom_unit.id,
+        })
+        product_two = self.env['product.product'].create({
+            'name': 'Component',
+            'type': 'product',
+            'uom_id': uom_unit.id,
+        })
+        self.env['stock.quant']._update_available_quantity(product_two, location, 4.0)
+
+        bom_normal = self.env['mrp.bom'].create({
+            'product_tmpl_id': product_one.product_tmpl_id.id,
+            'product_uom_id': product_one.product_tmpl_id.uom_id.id,
+            'product_qty': 1.0,
+            'type': 'normal',
+            'bom_line_ids': [
+                Command.create({
+                    'product_id': product_two.id,
+                    'product_qty': 1,
+                }),
+            ]
+        })
+        report_values = self.env['report.mrp.report_bom_structure']._get_report_data(bom_id=bom_normal.id)
+        line_values = report_values['lines']
+        self.assertEqual(line_values['availability_state'], 'unavailable')
+
+        bom_kit = self.env['mrp.bom'].create({
+            'product_tmpl_id': product_one.product_tmpl_id.id,
+            'product_uom_id': product_one.product_tmpl_id.uom_id.id,
+            'product_qty': 1.0,
+            'type': 'phantom',
+            'bom_line_ids': [
+                Command.create({
+                    'product_id': product_two.id,
+                    'product_qty': 1,
+                }),
+            ]
+        })
+        report_values = self.env['report.mrp.report_bom_structure']._get_report_data(bom_id=bom_kit.id)
+        line_values = report_values['lines']
+        self.assertEqual(line_values['availability_state'], 'available')