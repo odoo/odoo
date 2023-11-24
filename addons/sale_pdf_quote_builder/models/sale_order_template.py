94a4b330efa4448b2ef1ba5b54257c495c37bf27
sami odoo <sami@odoo.com>
2023-11-24 16:37:00 +0530

    [FIX] sale_pdf_quote_builder: restrict user to upload corrupt pdf
    When the header and footer pages of Quotation template contains corrupted pdf
    then while previewing the quotation if the user clicks on view details or clicks
    on action quote pdf then the user will face error.
    
    Steps to produce:
    - Sales > Configuration > Sale Orders > Quotation Templates.
    - Create a new Quotation Template > Fill the necessary details.
    - In PDF Quote Builder > Header Pages > Add a pdf which is corrupted, or does
      not have EOF.
    - Create a new quotation, add the newly created quotation template.
    - Save the details and preview the quotation.
    - In the preview of quotaion click on `View Details` button.
    - or after saving the details open the tree view of quotations
      select the quotation in which you kept newly created quotation
      template and then click on print then `PDF Quote`.
    
    Error: `PdfReadError: EOF marker not found`
    
    This commit does not allow user to upload corrupted pdf as header and footer
    pages of Quotation templates.
    
    sentry-4621647394

--- addons/sale_pdf_quote_builder/models/sale_order_template.py
+++ addons/sale_pdf_quote_builder/models/sale_order_template.py
@@ -1,7 +1,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import fields, models
-
+import io
+from odoo import api, fields, models, _
+import base64
+from odoo.exceptions import ValidationError
+from PyPDF2 import PdfFileReader
+try:
+    from PyPDF2.errors import PdfReadError
+except ImportError:
+    from PyPDF2.utils import PdfReadError
 
 class SaleOrderTemplate(models.Model):
     _inherit = 'sale.order.template'
@@ -12,3 +18,13 @@ class SaleOrderTemplate(models.Model):
     sale_footer = fields.Binary(
         string="Footer pages", default=lambda self: self.env.company.sale_footer)
     sale_footer_name = fields.Char(default=lambda self: self.env.company.sale_footer_name)
+
+    @api.constrains('sale_header', 'sale_footer')
+    def _check_valid_header_and_footer(self):
+        try:
+            if self.sale_header:
+                PdfFileReader(io.BytesIO(base64.b64decode(self.sale_header)), strict=False, overwriteWarnings=False)
+            if self.sale_footer:
+                PdfFileReader(io.BytesIO(base64.b64decode(self.sale_footer)), strict=False, overwriteWarnings=False)
+        except PdfReadError:
+            raise ValidationError(_("The uploaded file is not a valid PDF. Please upload a valid PDF file."))
