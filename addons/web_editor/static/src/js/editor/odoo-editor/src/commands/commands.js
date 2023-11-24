84291284554b7da1089e1095c1fe0a6baf1258ec
Deependra Solanki <deso@odoo.com>
2023-11-24 15:23:24 +0530

    [FIX] web_editor: fix selection on table creation
    Before this commit:
    
    When a table is created the selection is set to the first `TD` of the table.
    
    After this commit:
    
    When a table is created the selection is set to the `P` tag inside of first `TD`
    of the table.
    
    task-3610536
    
    closes odoo/odoo#144887
    
    X-original-commit: 07d00347822f2d4b9fd0d315a52892030fd41693
    Signed-off-by: Nicolas Bayet (nby) <nby@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/src/commands/commands.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/commands/commands.js
@@ -657,7 +657,7 @@ export const editorCommands = {
             setSelection(...newPosition, ...newPosition, false);
         }
         const [table] = editorCommands.insert(editor, parseHTML(tableHtml));
-        setCursorStart(table.querySelector('td'));
+        setCursorStart(table.querySelector('p'));
     },
     addColumn: (editor, beforeOrAfter, referenceCell) => {
         if (!referenceCell) {
