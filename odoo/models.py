a3da70910105f3d447f582885e7e8942c06ffed9
Roy Le <roy.le@viindoo.com>
2023-11-27 18:42:22 +0700

    [FIX] odoo/models.py: ignore fields that already have values when delegating default properties to property fields

--- odoo/models.py
+++ odoo/models.py
@@ -1676,7 +1676,7 @@ class BaseModel(metaclass=MetaModel):
 
         # delegate the default properties to the properties field
         for field in self._fields.values():
-            if field.type == 'properties':
+            if field.type == 'properties' and field.name not in defaults:
                 defaults[field.name] = field._add_default_values(self.env, defaults)
 
         return defaults
