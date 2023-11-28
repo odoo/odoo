fe5b8436c34608c2fbe6391235a6722aaccb90ed
abd-msyukyu-odoo <abd@odoo.com>
2023-11-28 19:22:09 +0100

    [FIX] web, web_hierarchy: ensure proper scroll speed/behavior
    In [1], an automatic scroll feature was introduced in order to always scroll to
    the bottom of the hierarchy view after each patch.
    
    The `isAnchor` option is removed, because in `Knowledge` it has the side effect
    of completely scrolling the article down to the last card of the Hierarchy
    view, hiding the rest of the view. Also, that option was not useful in the
    standard configuration since there is only one scrollable parent.
    
    Another issue is that the `scroll-behavior: smooth` css property also affects
    the scroll speed of the `draggable_hook_builder`, which means that drag scroll
    speed is extremely slow and does not follow the speed computed in that file.
    This style is therefore removed, and `scrollTo` is refactored:
      - Make use of the native `Element.scrollTo` method, which has a `behavior:
        "smooth"` option.
      - Move and rename methods to compute the closest scrollable from the
        `draggable_hook_builder.js` to `scrolling.js`.
        - They check if a scrollable has a client[Dimension] greater than 0 for
          eligibility.
        - They check the proper `overflow-[axis]` from the computed style (`auto`
          and `scroll` are eligible).
        - A test of `scroller_service_tests` was updated because even if an element
          has `max-height: 0`, its children still can have a height and make their
          scrollable ancestor have a `scrollHeight` greater than its own height.
      - If the `scrollable` option is given to the function and the provided
        element is not actually scrollable, compute a `scrollable` ancestor instead
        of doing nothing.
        - This is more flexible in a situation where a supposedly "scrollable" is
          computed with a selector and ends up not "scrollable" in some custom
          configuration (i.e. embedded view in Knowledge).
    
    The `scrollable` option is removed, since `scrollTo` is already able to compute
    the correct closest scrollable element, no need to hardcode a selector for that.
    
    Finally, the draggable speed and threshold for the Hierarchy view are set to
    the same values as a Kanban view for user experience consistency.
    
    [1]: https://github.com/odoo/odoo/commit/37fa2c0e52c1ab87bf10f90fb6dbb446e3e6e840
    
    task-3562116
    
    Part-of: odoo/odoo#145463

--- addons/web/static/src/core/utils/scrolling.js
+++ addons/web/static/src/core/utils/scrolling.js
@@ -1,29 +1,58 @@
 /** @odoo-module */
 
 /**
+ * Get the closest horizontally scrollable for a given element.
+ *
+ * @param {HTMLElement} el
+ * @returns {HTMLElement | null}
+ */
+export function closestScrollableX(el) {
+    if (!el) {
+        return null;
+    }
+    if (el.scrollWidth > el.clientWidth && el.clientWidth > 0) {
+        const overflow = getComputedStyle(el).getPropertyValue("overflow-x");
+        if (/\bauto\b|\bscroll\b/.test(overflow)) {
+            return el;
+        }
+    }
+    return closestScrollableX(el.parentElement);
+}
+
+/**
+ * Get the closest vertically scrollable for a given element.
+ *
+ * @param {HTMLElement} el
+ * @returns {HTMLElement | null}
+ */
+export function closestScrollableY(el) {
+    if (!el) {
+        return null;
+    }
+    if (el.scrollHeight > el.clientHeight && el.clientHeight > 0) {
+        const overflow = getComputedStyle(el).getPropertyValue("overflow-y");
+        if (/\bauto\b|\bscroll\b/.test(overflow)) {
+            return el;
+        }
+    }
+    return closestScrollableY(el.parentElement);
+}
+
+/**
  * Ensures that `element` will be visible in its `scrollable`.
  *
  * @param {HTMLElement} element
  * @param {Object} options
  * @param {HTMLElement} [options.scrollable] a scrollable area
  * @param {Boolean} [options.isAnchor] states if the scroll is to an anchor
+ * @param {String} [options.behavior] "smooth", "instant", "auto" <=> undefined
+ *        @url https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo#behavior
  */
-export function scrollTo(element, options = { scrollable: null, isAnchor: false }) {
-    function _getScrollParent(node) {
-        if (node == null) {
-            return null;
-        }
-
-        if (node.scrollHeight > node.clientHeight && node.clientHeight > 0) {
-            return node;
-        } else {
-            return _getScrollParent(node.parentNode);
-        }
-    }
-
-    const scrollable = options.scrollable
-        ? options.scrollable
-        : _getScrollParent(element.parentNode);
+export function scrollTo(
+    element,
+    options = { behavior: "auto", scrollable: null, isAnchor: false }
+) {
+    const scrollable = closestScrollableY(options.scrollable || element.parentElement);
     if (scrollable) {
         const scrollBottom = scrollable.getBoundingClientRect().bottom;
         const scrollTop = scrollable.getBoundingClientRect().top;
@@ -31,16 +60,29 @@ export function scrollTo(element, options = { scrollable: null, isAnchor: false 
         const elementTop = element.getBoundingClientRect().top;
         if (elementBottom > scrollBottom && !options.isAnchor) {
             // The scroll place the element at the bottom border of the scrollable
-            scrollable.scrollTop +=
-                elementTop - scrollBottom + Math.ceil(element.getBoundingClientRect().height);
+            scrollable.scrollTo({
+                top:
+                    scrollable.scrollTop +
+                    elementTop -
+                    scrollBottom +
+                    Math.ceil(element.getBoundingClientRect().height),
+                behavior: options.behavior,
+            });
         } else if (elementTop < scrollTop || options.isAnchor) {
             // The scroll place the element at the top of the scrollable
-            scrollable.scrollTop -= scrollTop - elementTop;
+            scrollable.scrollTo({
+                top: scrollable.scrollTop - scrollTop + elementTop,
+                behavior: options.behavior,
+            });
             if (options.isAnchor) {
                 // If the scrollable is within a scrollable, another scroll should be done
-                const parentScrollable = _getScrollParent(scrollable.parentNode);
+                const parentScrollable = closestScrollableY(scrollable.parentElement);
                 if (parentScrollable) {
-                    scrollTo(scrollable, { isAnchor: true, scrollable: parentScrollable });
+                    scrollTo(scrollable, {
+                        behavior: options.behavior,
+                        isAnchor: true,
+                        scrollable: parentScrollable,
+                    });
                 }
             }
         }
