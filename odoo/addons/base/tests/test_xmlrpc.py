876b235c8cc8afb8fb50837e85b3e5c5fb542057
Yannick Tivisse <yti@odoo.com>
2023-11-28 13:14:26 +0100

    [FIX] base: Make tests demo data independent
    Part-of: odoo/odoo#143999

--- odoo/addons/base/tests/test_xmlrpc.py
+++ odoo/addons/base/tests/test_xmlrpc.py
@@ -12,7 +12,7 @@ from odoo.service import common as auth, model
 from odoo.tools import DotDict
 
 
-@common.tagged('post_install', '-at_install')
+@common.tagged('post_install', '-at_install', 'test_xmlrpc')
 class TestXMLRPC(common.HttpCase):
 
     def setUp(self):
@@ -65,6 +65,7 @@ class TestXMLRPC(common.HttpCase):
 
     def test_xmlrpc_frozendict_marshalling(self):
         """ Test that the marshalling of a frozendict object works properly over XMLRPC """
+        self.env.ref('base.user_admin').tz = "Europe/Brussels"
         ctx = self.xmlrpc_object.execute(
             common.get_db_name(), self.admin_uid, 'admin',
             'res.users', 'context_get',
