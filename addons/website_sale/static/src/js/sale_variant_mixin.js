08fb6435f4badf4ba926974735c903bbb513dc46
Victor Feyens <vfe@odoo.com>
2023-11-28 17:42:54 +0100

    [MOV] website_sale: merge configurator logic
    Since the refactoring of the backend configurator to OWL,
    the variant mixin logic is only used by the frontend configurator.
    The two files holding the logic (one of the two was previously in the sale
    module) can be safely merged into one single file holding the frontend
    configurator logic.
    
    closes odoo/odoo#144429
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/website_sale/static/src/js/sale_variant_mixin.js
+++ addons/website_sale/static/src/js/sale_variant_mixin.js
@@ -525,6 +525,45 @@ var VariantMixin = {
      * @param {Array} combination
      */
     _onChangeCombination: function (ev, $parent, combination) {
+        const $pricePerUom = $parent.find(".o_base_unit_price:first .oe_currency_value");
+        if ($pricePerUom) {
+            if (combination.is_combination_possible !== false && combination.base_unit_price != 0) {
+                $pricePerUom.parents(".o_base_unit_price_wrapper").removeClass("d-none");
+                $pricePerUom.text(this._priceToStr(combination.base_unit_price));
+                $parent.find(".oe_custom_base_unit:first").text(combination.base_unit_name);
+            } else {
+                $pricePerUom.parents(".o_base_unit_price_wrapper").addClass("d-none");
+            }
+        }
+
+        // Triggers a new JS event with the correct payload, which is then handled
+        // by the google analytics tracking code.
+        // Indeed, every time another variant is selected, a new view_item event
+        // needs to be tracked by google analytics.
+        if ('product_tracking_info' in combination) {
+            const $product = $('#product_detail');
+            $product.data('product-tracking-info', combination['product_tracking_info']);
+            $product.trigger('view_item_event', combination['product_tracking_info']);
+        }
+        const addToCart = $parent.find('#add_to_cart_wrap');
+        const contactUsButton = $parent.find('#contact_us_wrapper');
+        const productPrice = $parent.find('.product_price');
+        const quantity = $parent.find('.css_quantity');
+        const product_unavailable = $parent.find('#product_unavailable');
+        if (combination.prevent_zero_price_sale) {
+            productPrice.removeClass('d-inline-block').addClass('d-none');
+            quantity.removeClass('d-inline-flex').addClass('d-none');
+            addToCart.removeClass('d-inline-flex').addClass('d-none');
+            contactUsButton.removeClass('d-none').addClass('d-flex');
+            product_unavailable.removeClass('d-none').addClass('d-flex')
+        } else {
+            productPrice.removeClass('d-none').addClass('d-inline-block');
+            quantity.removeClass('d-none').addClass('d-inline-flex');
+            addToCart.removeClass('d-none').addClass('d-inline-flex');
+            contactUsButton.removeClass('d-flex').addClass('d-none');
+            product_unavailable.removeClass('d-flex').addClass('d-none')
+        }
+
         var self = this;
         var $price = $parent.find(".oe_price:first .oe_currency_value");
         var $default_price = $parent.find(".oe_default_price:first .oe_currency_value");
@@ -657,6 +696,11 @@ var VariantMixin = {
      * @param {boolean} isCombinationPossible
      */
     _toggleDisable: function ($parent, isCombinationPossible) {
+        if ($parent.hasClass('in_cart')) {
+            const secondaryButton = $parent.parents('.modal-content').find('.modal-footer .btn-secondary');
+            secondaryButton.prop('disabled', !isCombinationPossible);
+            secondaryButton.toggleClass('disabled', !isCombinationPossible);
+        }
         $parent.toggleClass('css_not_available', !isCombinationPossible);
         if ($parent.hasClass('in_cart')) {
             const primaryButton = $parent.parents('.modal-content').find('.modal-footer .btn-primary');
