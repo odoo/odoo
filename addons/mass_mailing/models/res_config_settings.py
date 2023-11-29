7716e905b3f4ea4b8cf866473353ae9ee3ed96a9
Pierre-Yves Dufays <pydu@odoo.com>
2023-11-29 10:51:03 +0100

    [IMP] {website_}mass_mailing: split contact name into first and last name
    Add an option that allows to fill the contact name by providing a first and a
    last name instead of a single name. This option is not set by default.
    
    If the option is enabled, the name will automatically be filled when filling
    the first and the last name (as first name followed by last name).
    The first and the last name fields will also be present on the front-end when
    inserting the newsletter subscription form. Unfortunately, when changing the
    option, it won't be updated automatically (it must be done manually).
    
    A small inconvenient is that when updating first or last name, it will update
    the name so that the other part of the name is lost. To mitigate that problem,
    we have enabled the tracking on "name" so that any change on that field is
    logged in the chatter.
    
    If the option is not enabled, only names can be filled. First and last name
    will remain empty.
    
    Enabling the option won't convert names to first and last names. We have
    decided to not implement that conversion because it is not reliable as the
    order of the first and last name can vary, some country have also middle
    name, ... So to keep things simple, that operation must be done manually.
    
    When not enabled, we make the first and last name fields not searchable so that
    they don't appears in custom filters or dynamic placeholder.
    
    Note: the column name of the tree view has the option optional="show". When
    enabling the split name feature, the column first and last name are added to
    the view with the option optional="hide" to allow the user to configure the
    view with first and last name columns if needed (by default, only the name
    column will be shown).
    
    Task-3597124

--- addons/mass_mailing/models/res_config_settings.py
+++ addons/mass_mailing/models/res_config_settings.py
@@ -26,14 +26,30 @@ class ResConfigSettings(models.TransientModel):
         string='24H Stat Mailing Reports',
         config_parameter='mass_mailing.mass_mailing_reports',
         help='Check how well your mailing is doing a day after it has been sent.')
+    mass_mailing_split_contact_name = fields.Boolean(
+        string='Split First and Last Name',
+        help='Separate Mailing Contact Names into two fields')
 
     @api.onchange('mass_mailing_outgoing_mail_server')
     def _onchange_mass_mailing_outgoing_mail_server(self):
         if not self.mass_mailing_outgoing_mail_server:
             self.mass_mailing_mail_server_id = False
 
+    @api.model
+    def get_values(self):
+        res = super().get_values()
+        res.update(
+            mass_mailing_split_contact_name=self.env['mailing.contact']._is_name_split_activated(),
+        )
+        return res
+
     def set_values(self):
         super().set_values()
         ab_test_cron = self.env.ref('mass_mailing.ir_cron_mass_mailing_ab_testing').sudo()
         if ab_test_cron and ab_test_cron.active != self.group_mass_mailing_campaign:
             ab_test_cron.active = self.group_mass_mailing_campaign
+        if self.env['mailing.contact']._is_name_split_activated() != self.mass_mailing_split_contact_name:
+            self.env.ref(
+                "mass_mailing.mailing_contact_view_tree_split_name").active = self.mass_mailing_split_contact_name
+            self.env.ref(
+                "mass_mailing.mailing_contact_view_form_split_name").active = self.mass_mailing_split_contact_name
