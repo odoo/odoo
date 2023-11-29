826ed5b7c54b7a667ca533893e86ba8451204a9f
Walid <waha@odoo.com>
2023-11-29 18:09:44 +0100

    [FIX] pos_mrp: subkits cost
    Steps to reproduce:
    - Create the following subkits boms
    F-> SK1 -> A
            -> B
        SK2 -> C
            -> D
    - Set all these products to be available in pos and have AVCO automated
    valuation with A,B,C and D having a cost of 5$
    - Sell F in pos creaing an invoice
    - In accounting check the expense account (cost of goods) wrong cost
    
    Bug:
    ´comp[1]['product']´ referrs to the kit product of the last children boms
    (SK1 and SK2 in this case) which would then each be counted twice
    instead use ´comp[0].product_id´ (the components of the child boms)
    
    also fixed total_cost computation for pos lines which would filter out
    all the subkits moves
    
    opw-3538285
    
    closes odoo/odoo#150937
    
    X-original-commit: d6a513355ee0ae4b02e38baacdde760cc35fb873
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Walid Hanniche (waha) <waha@odoo.com>

--- addons/pos_mrp/models/pos_order.py
+++ addons/pos_mrp/models/pos_order.py
@@ -10,7 +10,8 @@ class PosOrderLine(models.Model):
         bom = product.env['mrp.bom']._bom_find(product, company_id=stock_moves.company_id.id, bom_type='phantom')[product]
         if not bom:
             return super()._get_stock_moves_to_consider(stock_moves, product)
-        ml_product_to_consider = (product.bom_ids and bom.bom_line_ids.mapped('product_id').mapped('id')) or [product.id]
+        _dummy, components = bom.explode(product, self.qty)
+        ml_product_to_consider = (product.bom_ids and [comp[0].product_id.id for comp in components]) or [product.id]
         return stock_moves.filtered(lambda ml: ml.product_id.id in ml_product_to_consider and ml.bom_line_id)
 
 class PosOrder(models.Model):
@@ -21,4 +22,4 @@ class PosOrder(models.Model):
         if not bom:
             return super()._get_pos_anglo_saxon_price_unit(product, partner_id, quantity)
         dummy, components = bom.explode(product, quantity)
-        return sum(super(PosOrder, self)._get_pos_anglo_saxon_price_unit(comp[1]['product'], partner_id, comp[1]['qty']) for comp in components)
+        return sum(super(PosOrder, self)._get_pos_anglo_saxon_price_unit(comp[0].product_id, partner_id, comp[1]['qty']) for comp in components)
