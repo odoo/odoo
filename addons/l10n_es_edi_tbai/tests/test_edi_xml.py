5e406124a91cea9972de5fb998c99ab8f3386224
Josse Colpaert <jco@odoo.com>
2023-11-29 12:50:03 +0100

    [IMP] l10n_es_edi_tbai: lroe vendor bill for Batuz
    From the first of January, Ticketbai becomes required in
    the region of Batuz (Bizkaia).
    
    And we were missing a piece where also the vendor bills
    need to be sent, similar to SII (but different)
    
    For that, they use an LROE structure, which we also used
    for sending customer invoices, but with the regular Ticketbai
    XML in there (encoded in base64).
    
    For vendor bills, it is however not the regular Ticketbai
    XML and the chaining and signature are also not needed, but
    the enveloping structure is built in the same way.
    
    So, we activate Ticketbai by default on vendor bill journals,
    but it will only be sending when the agency is Batuz.
    
    Thanks to Moduon and Landoo for the extra push.
    
    opw-3628750

--- addons/l10n_es_edi_tbai/tests/test_edi_xml.py
+++ addons/l10n_es_edi_tbai/tests/test_edi_xml.py
@@ -33,14 +33,60 @@ class TestEdiTbaiXmls(TestEsEdiTbaiCommon):
                 'tax_ids': [(6, 0, cls._get_tax_by_xml_id('s_iva21b').ids)],
             })],
         })
-
         cls.edi_format = cls.env.ref('l10n_es_edi_tbai.edi_es_tbai')
 
     def test_xml_tree_post(self):
+        """Test of Customer Invoice XML"""
         with freeze_time(self.frozen_today):
             xml_doc = self.edi_format._get_l10n_es_tbai_invoice_xml(self.out_invoice, cancel=False)[self.out_invoice]['xml_file']
             xml_doc.remove(xml_doc.find("Signature", namespaces=NS_MAP))
             xml_expected = etree.fromstring(super().L10N_ES_TBAI_SAMPLE_XML_POST)
+            self.assertXmlTreeEqual(xml_doc, xml_expected)
+
+    def test_xml_tree_in_post(self):
+        """Test XML of vendor bill for LROE Batuz"""
+        with freeze_time(self.frozen_today):
+            self.in_invoice = self.env['account.move'].create({
+                'name': 'INV/01',
+                'move_type': 'in_invoice',
+                'invoice_date': datetime.now(),
+                'partner_id': self.partner_a.id,
+                'invoice_line_ids': [(0, 0, {
+                    'product_id': self.product_a.id,
+                    'price_unit': 1000.0,
+                    'quantity': 5,
+                    'discount': 20.0,
+                    'tax_ids': [(6, 0, self._get_tax_by_xml_id('p_iva21_bc').ids)],
+                })],
+            })
+            xml_doc = etree.fromstring(self.edi_format._l10n_es_tbai_get_invoice_content_edi(self.in_invoice))
+            xml_expected = etree.fromstring(super().L10N_ES_TBAI_SAMPLE_XML_POST_IN)
+            self.assertXmlTreeEqual(xml_doc, xml_expected)
+
+    def test_xml_tree_in_ic_post(self):
+        """Test XML of vendor bill for LROE Batuz intra-community"""
+        with freeze_time(self.frozen_today):
+            self.in_invoice = self.env['account.move'].create({
+                'name': 'INV/01',
+                'move_type': 'in_invoice',
+                'invoice_date': datetime.now(),
+                'partner_id': self.partner_b.id,
+                'invoice_line_ids': [(0, 0, {
+                    'product_id': self.product_a.id,
+                    'price_unit': 1000.0,
+                    'quantity': 5,
+                    'discount': 20.0,
+                    'tax_ids': [(6, 0, self._get_tax_by_xml_id('p_iva21_ic_bc').ids)],
+                }), (0, 0, {
+                    'product_id': self.product_b.id,
+                    'price_unit': 2000.0,
+                    'quantity': 5,
+                    'discount': 20.0,
+                    'tax_ids': [(6, 0, self._get_tax_by_xml_id('p_iva21_sp_in').ids)],
+                })],
+            })
+            xml_doc = etree.fromstring(self.edi_format._l10n_es_tbai_get_invoice_content_edi(self.in_invoice))
+            xml_expected = etree.fromstring(super().L10N_ES_TBAI_SAMPLE_XML_POST_IN_IC)
             self.assertXmlTreeEqual(xml_doc, xml_expected)
 
     def test_xml_tree_cancel(self):
