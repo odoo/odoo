cdec4e41acb4e4234139497f9eb16abfa1c90c80
guva-odoo <guva@odoo.com>
2023-11-30 16:08:36 +0100

    [FIX] account: tax_id_quick_encoding_suggestion
    The right tax is not always proposed when using
    accounting firm mode and quick edit suggestion.
    
    Steps:
    
    - Have purchase a tax T with repartition lines that split tax in
      two, 50% to a specific account (other than the vendor bill's
      default account), 50% with no account set. Note that bug is
      happening with sale taxes as well
    - Create a new partner P
    - Create and confirm a bill 1 for partner X with one invoice line with tax T
      and default vendor bill's account (A)
    - Activate the quick encoding for invoice/bills (accounting firms mode)
    - Open a new bill form, select partner P and set quick_edit_suggestion_amount
      field (right below the partner field)
    -> The tax T is not set in the invoice line
    
    This is because bill 1 has two aml with account A, the product line and
    one of the two tax lines, note that the bug might not occure depending
    of the way sql orders the result.
    
    Note that this commit also fixes this flow:
    
    - Same tax T
    - Same partner P
    - Bill 1 already existing
    - Create another bill for P, with account A and no tax
    - Go on with the same flow
    -> No tax set
    
    Now we have 3 aml related to P, one with tax T and 2 with no tax,
    therefore no tax is set in any case.
    
    opw-3568433
    
    closes odoo/odoo#144417
    
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -2642,6 +2642,7 @@ class AccountMove(models.Model):
                WHERE
                    account.company_id = %s
                    AND account.deprecated = FALSE
+                   AND aml.display_type = 'product'
                       {where_internal_group}
                GROUP BY account.id, account.code, aml.id
                ) AS foo
@@ -2649,7 +2650,7 @@ class AccountMove(models.Model):
             ORDER BY COUNT(foo.id) DESC, foo.code
             LIMIT 1
         """, [partner_id, company_id])
-        return self._cr.fetchone()
+        return self._cr.fetchone() or (None, None, None)
 
     def _get_quick_edit_suggestions(self):
         """
