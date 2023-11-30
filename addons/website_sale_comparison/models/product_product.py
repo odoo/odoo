10d3e0683ec710ac1a30b8acb82e876679355366
Victor Feyens <vfe@odoo.com>
2023-11-30 17:14:54 +0100

    [MOV] website_sale_comparison: split files by classes (2)

--- addons/website_sale_comparison/models/product_product.py
+++ addons/website_sale_comparison/models/product_product.py
@@ -1,55 +1,8 @@
-# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from collections import OrderedDict
 
-from odoo import fields, models
-
-
-class ProductAttributeCategory(models.Model):
-    _name = "product.attribute.category"
-    _description = "Product Attribute Category"
-    _order = 'sequence, id'
-
-    name = fields.Char("Category Name", required=True, translate=True)
-    sequence = fields.Integer("Sequence", default=10, index=True)
-
-    attribute_ids = fields.One2many('product.attribute', 'category_id', string="Related Attributes", domain="[('category_id', '=', False)]")
-
-
-class ProductAttribute(models.Model):
-    _inherit = 'product.attribute'
-    _order = 'category_id, sequence, id'
-
-    category_id = fields.Many2one(
-        comodel_name='product.attribute.category',
-        string="eCommerce Category",
-        index=True,
-        help="Set a category to regroup similar attributes under the same section in the Comparison"
-             " page of eCommerce.",
-    )
-
-
-class ProductTemplateAttributeLine(models.Model):
-    _inherit = 'product.template.attribute.line'
-
-    def _prepare_categories_for_display(self):
-        """On the product page group together the attribute lines that concern
-        attributes that are in the same category.
-
-        The returned categories are ordered following their default order.
-
-        :return: OrderedDict [{
-            product.attribute.category: [product.template.attribute.line]
-        }]
-        """
-        attributes = self.attribute_id
-        categories = OrderedDict([(cat, self.env['product.template.attribute.line']) for cat in attributes.category_id.sorted()])
-        if any(not pa.category_id for pa in attributes):
-            # category_id is not required and the mapped does not return empty
-            categories[self.env['product.attribute.category']] = self.env['product.template.attribute.line']
-        for ptal in self:
-            categories[ptal.attribute_id.category_id] |= ptal
-        return categories
+from odoo import models
 
 
 class ProductProduct(models.Model):
