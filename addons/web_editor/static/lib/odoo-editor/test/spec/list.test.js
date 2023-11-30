142548c58f0393111174daadba205a817814df19
Deependra Solanki <deso@odoo.com>
2023-11-30 15:55:28 +0530

    [FIX] web_editor: do not toggle list with class nav-item
    When inserting a list to a mega menu, the closest `LI` is a nav-item within the
    navbar. We aim to prevent the toggling of this list. This commit ensures that
    lists with class nav-item in the navbar are not toggled.
    
    task-3609500
    
    X-original-commit: 2217af170b5893adca1f2b9220a7fe112d5fd44a

--- addons/web_editor/static/lib/odoo-editor/test/spec/list.test.js
+++ addons/web_editor/static/lib/odoo-editor/test/spec/list.test.js
@@ -95,6 +95,20 @@ describe('List', () => {
                             `),
                         });
                     });
+                    it('should create a new unordered list if current node is inside a nav-item list', async () => {
+                        await testEditor(BasicEditor, {
+                            contentBefore: '<ul><li class="nav-item">a[]b</li></ul>',
+                            stepFunction: toggleUnorderedList,
+                            contentAfter: '<ul><li class="nav-item"><ul><li>a[]b</li></ul></li></ul>',
+                        });
+                    });
+                    it('should create a new unordered list if closestBlock is inside a nav-item list', async () => {
+                        await testEditor(BasicEditor, {
+                            contentBefore: '<ul><li class="nav-item"><div><p>a[]b</p></div></li></ul>',
+                            stepFunction: toggleUnorderedList,
+                            contentAfter: '<ul><li class="nav-item"><div><ul><li>a[]b</li></ul></div></li></ul>',
+                        });
+                    });
                 });
                 describe('Remove', () => {
                     it('should turn an empty list into a paragraph', async () => {
@@ -302,6 +316,20 @@ describe('List', () => {
                                     </tbody>
                                 </table>
                             `),
+                        });
+                    });
+                    it('should create a new ordered list if current node is inside a nav-item list', async () => {
+                        await testEditor(BasicEditor, {
+                            contentBefore: '<ul><li class="nav-item">a[]b</li></ul>',
+                            stepFunction: toggleOrderedList,
+                            contentAfter: '<ul><li class="nav-item"><ol><li>a[]b</li></ol></li></ul>',
+                        });
+                    });
+                    it('should create a new ordered list if closestBlock is inside a nav-item list', async () => {
+                        await testEditor(BasicEditor, {
+                            contentBefore: '<ul><li class="nav-item"><div><h1>a[]b</h1></div></li></ul>',
+                            stepFunction: toggleOrderedList,
+                            contentAfter: '<ul><li class="nav-item"><div><ol><li><h1>a[]b</h1></li></ol></div></li></ul>',
                         });
                     });
                 });
@@ -573,6 +601,22 @@ describe('List', () => {
                                     </tbody>
                                 </table>
                             `),
+                        });
+                    });
+                    it('should create a new checked list if current node is inside a nav-item list', async () => {
+                        await testEditor(BasicEditor, {
+                            removeCheckIds: true,
+                            contentBefore: '<ul><li class="nav-item">a[]b</li></ul>',
+                            stepFunction: toggleCheckList,
+                            contentAfter: '<ul><li class="nav-item"><ul class="o_checklist"><li>a[]b</li></ul></li></ul>',
+                        });
+                    });
+                    it('should create a new checked list if closestBlock is inside a nav-item list', async () => {
+                        await testEditor(BasicEditor, {
+                            removeCheckIds: true,
+                            contentBefore: '<ul><li class="nav-item"><div><p>a[]b</p></div></li></ul>',
+                            stepFunction: toggleCheckList,
+                            contentAfter: '<ul><li class="nav-item"><div><ul class="o_checklist"><li>a[]b</li></ul></div></li></ul>',
                         });
                     });
                 });
@@ -7319,6 +7363,18 @@ describe('List', () => {
                 </ol>`),
                 });
             });
+            it('should not indent a nav-item list', async () => {
+                await testEditor(BasicEditor, {
+                    contentBefore: '<ul><li class="nav-item">a[]</li></ul>',
+                    stepFunction: indentList,
+                    contentAfter: '<ul><li class="nav-item">a[]</li></ul>',
+                });
+                await testEditor(BasicEditor, {
+                    contentBefore: '<ul><li class="nav-item"><div><p>a[]</p></div></li></ul>',
+                    stepFunction: indentList,
+                    contentAfter: '<ul><li class="nav-item"><div><p>a[]</p></div></li></ul>',
+                });
+            });
         });
         describe('with selection', () => {
             it('should indent the first element of a list', async () => {
@@ -7841,6 +7897,136 @@ describe('List', () => {
                             </li>
                             <li>[]c</li>
                         </ul>`),
+                });
+            });
+            it('should outdent a list inside a nav-item list', async () => {
+                await testEditor(BasicEditor, {
+                    contentBefore: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <ul>
+                                    <li>a[]b</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    `),
+                    stepFunction: outdentList,
+                    contentAfter: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <p>a[]b</p>
+                            </li>
+                        </ul>
+                    `),
+                });
+                await testEditor(BasicEditor, {
+                    contentBefore: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <ol>
+                                    <li>a[]b</li>
+                                </ol>
+                            </li>
+                        </ul>
+                    `),
+                    stepFunction: outdentList,
+                    contentAfter: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <p>a[]b</p>
+                            </li>
+                        </ul>
+                    `),
+                });
+                await testEditor(BasicEditor, {
+                    contentBefore: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <ul class="o_checklist">
+                                    <li>a[]b</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    `),
+                    stepFunction: outdentList,
+                    contentAfter: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <p>a[]b</p>
+                            </li>
+                        </ul>
+                    `),
+                });
+                await testEditor(BasicEditor, {
+                    contentBefore: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <div>
+                                    <ul>
+                                        <li>a[]b</li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    `),
+                    stepFunction: outdentList,
+                    contentAfter: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <div>
+                                    <p>a[]b</p>
+                                </div>
+                            </li>
+                        </ul>
+                    `),
+                });
+                await testEditor(BasicEditor, {
+                    contentBefore: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <div>
+                                    <ol>
+                                        <li>
+                                            <h1>a[]b</h1>
+                                        </li>
+                                    </ol>
+                                </div>
+                            </li>
+                        </ul>
+                    `),
+                    stepFunction: outdentList,
+                    contentAfter: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <div>
+                                    <h1>a[]b</h1>
+                                </div>
+                            </li>
+                        </ul>
+                    `),
+                });
+                await testEditor(BasicEditor, {
+                    contentBefore: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <div>
+                                    <ul class="o_checklist">
+                                        <li>a[]b</li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    `),
+                    stepFunction: outdentList,
+                    contentAfter: unformat(`
+                        <ul>
+                            <li class="nav-item">
+                                <div>
+                                    <p>a[]b</p>
+                                </div>
+                            </li>
+                        </ul>
+                    `),
                 });
             });
         });
