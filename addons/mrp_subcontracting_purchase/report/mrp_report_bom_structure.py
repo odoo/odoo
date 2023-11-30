0767d1fb5596114b29cee43984bc1c0eace559a9
Paweł Fertyk <pafe@odoo.com>
2023-11-30 17:51:00 +0100

    [FIX] mrp: fix negative values in subcontracting BoM Overview
    A subcontracting BoM sometimes shows negative values in Overview, if
    components are available in subcontractor's location.
    
    closes odoo/odoo#157542
    
    Task: 3607854
    X-original-commit: 3e169c0c699a47fb3567a792077cc3fdcb3af7fb
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/mrp_subcontracting_purchase/report/mrp_report_bom_structure.py
+++ addons/mrp_subcontracting_purchase/report/mrp_report_bom_structure.py
@@ -14,10 +14,11 @@ class ReportBomStructure(models.AbstractModel):
     @api.model
     def _get_resupply_availability(self, route_info, components):
         resupply_state, resupply_delay = super()._get_resupply_availability(route_info, components)
-        if route_info.get('route_type') == 'subcontract':
+        if route_info.get('route_type') == 'subcontract' and resupply_delay:
             # always add `Purchase security lead days` and `Days to Purchase`
             extra_delay = route_info['bom'].company_id.po_lead + route_info['bom'].company_id.days_to_purchase
             route_info['lead_time'] += extra_delay
-            subcontract_delay = (resupply_delay if resupply_delay else 0) + extra_delay
+            route_info['manufacture_delay'] += extra_delay
+            subcontract_delay = resupply_delay + extra_delay
             return ('estimated', subcontract_delay)
         return (resupply_state, resupply_delay)
