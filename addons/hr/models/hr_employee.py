8443a6bf1fe420e7a6f75471875658cddf8e392c
kdeb@odoo.com <kdeb@odoo.com>
2023-11-30 23:11:43 +0000

    [FIX] hr: change user on employee Access Error
    Issue
    
        Admin unable to change related user on employee profile due to
        restricted access to bank account.
    
    Steps to reproduce
    
        1- In the Employee app, go to the HR settings tab of an employee.
        2- Remove the related user from this profile.
        3- Set that removed user as the related user on a different employee
            profile and save (expect no error).
        4- Attempt to revert back to the original user.
        5- Encounter an access error.
    
    resolution
    
        - Added sudo() in the browse operation to prevent access errors.
    
    opw-3578412
    
    closes odoo/odoo#148211
    
    X-original-commit: f1d6919642f43750db2f7937fb02b0f1a41ecb96
    Signed-off-by: Sofie Gvaladze (sgv) <sgv@odoo.com>
    Signed-off-by: Kawtar Drissi El Bouzaidi (kdeb) <kdeb@odoo.com>

--- addons/hr/models/hr_employee.py
+++ addons/hr/models/hr_employee.py
@@ -411,11 +411,11 @@ class HrEmployeePrivate(models.Model):
         if 'work_contact_id' in vals:
             account_ids = vals.get('bank_account_id') or self.bank_account_id.ids
             if account_ids:
-                bank_accounts = self.env['res.partner.bank'].browse(account_ids)
+                bank_accounts = self.env['res.partner.bank'].sudo().browse(account_ids)
                 for bank_account in bank_accounts:
                     if vals['work_contact_id'] != bank_account.partner_id.id:
                         if bank_account.allow_out_payment:
-                            bank_account.sudo().allow_out_payment = False
+                            bank_account.allow_out_payment = False
                         if vals['work_contact_id']:
                             bank_account.partner_id = vals['work_contact_id']
             self.message_unsubscribe(self.work_contact_id.ids)
