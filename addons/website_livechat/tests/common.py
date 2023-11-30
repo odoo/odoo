d5219b31742fecc31a4641a8976bf319f47dd7b4
Yannick Tivisse <yti@odoo.com>
2023-11-30 14:52:52 +0100

    [FIX] mail: Make tests demo data independent

--- addons/website_livechat/tests/common.py
+++ addons/website_livechat/tests/common.py
@@ -7,8 +7,8 @@ from odoo.addons.base.tests.common import TransactionCaseWithUserDemo
 
 class TestLivechatCommon(TransactionCaseWithUserDemo):
     def setUp(self):
-        super(TestLivechatCommon, self).setUp()
-        self.env.ref("base.main_partner").email = "info@mycompany.com"
+        super().setUp()
+        self.env.company.email = "test@test.example.com"
         self.base_datetime = fields.Datetime.from_string("2019-11-11 21:30:00")
 
         self.group_user = self.env.ref('base.group_user')
@@ -40,7 +40,7 @@ class TestLivechatCommon(TransactionCaseWithUserDemo):
             'lang_id': self.env.ref('base.lang_en').id,
             'country_id': self.env.ref('base.de').id,
             'website_id': self.env.ref('website.default_website').id,
-            'partner_id': self.partner_demo.id,
+            'partner_id': self.user_demo.partner_id.id,
         }] + [visitor_vals]*self.max_sessions_per_operator)
         self.visitor_demo, self.visitor = self.visitors[0], self.visitors[1]
 
