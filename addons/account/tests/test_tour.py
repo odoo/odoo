652ab52e74c9cae7e01f367012d05839d5ffe72d
Yannick Tivisse <yti@odoo.com>
2023-11-30 14:45:30 +0100

    [FIX] account: Make tests demo data independent

--- addons/account/tests/test_tour.py
+++ addons/account/tests/test_tour.py
@@ -1,11 +1,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import Command
 import odoo.tests
+
+from odoo import Command
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
 @odoo.tests.tagged('post_install_l10n', 'post_install', '-at_install')
-class TestUi(odoo.tests.HttpCase):
+class TestUi(AccountTestInvoicingCommon, odoo.tests.HttpCase):
 
     def test_01_account_tour(self):
         # Reset country and fiscal country, so that fields added by localizations are
@@ -21,8 +23,4 @@ class TestUi(odoo.tests.HttpCase):
         account_with_taxes.write({
             'tax_ids': [Command.clear()],
         })
-        # This tour doesn't work with demo data on runbot
-        all_moves = self.env['account.move'].search([('move_type', '!=', 'entry')])
-        all_moves.button_draft()
-        all_moves.with_context(force_delete=True).unlink()
         self.start_tour("/web", 'account_tour', login="admin")
