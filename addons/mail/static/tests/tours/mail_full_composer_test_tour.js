3b261b6856e935f8fe9d4e23cfddc09df8ff7fdf
Sébastien Theys <seb@odoo.com>
2023-11-30 13:31:00 +0100

    [FIX] mail: fix test failing at "Write something in composer"
    The issue is not easy to reproduce, so the fix is not a guaranteed one.
    
    Estimation: the click on the "send message" buttons happens too early,
    and it is either doing nothing, or being immediately reset by another
    action that happens at page load.
    
    Adding a little bit of extra delay (in particular waiting for messages
    to be loaded) might resolve the issue.
    
    Also back-port code allowing to use contains in tours.
    
    runbot-27847
    
    closes odoo/odoo#144216
    
    X-original-commit: 95f5b9003a7073c63fecf8e66f4a259e038fae77
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/tests/tours/mail_full_composer_test_tour.js
+++ addons/mail/static/tests/tours/mail_full_composer_test_tour.js
@@ -2,6 +2,7 @@
 
 import { registry } from "@web/core/registry";
 import { createFile, inputFiles } from "web.test_utils_file";
+import { contains } from "@web/../tests/utils";
 
 /**
  * This tour depends on data created by python test in charge of launching it.
@@ -12,6 +13,13 @@ import { createFile, inputFiles } from "web.test_utils_file";
 registry.category("web_tour.tours").add("mail/static/tests/tours/mail_full_composer_test_tour.js", {
     test: true,
     steps: () => [
+        {
+            content: "Wait for the chatter to be fully loaded",
+            trigger: ".o-mail-Chatter",
+            async run() {
+                await contains(".o-mail-Message", { count: 1 });
+            },
+        },
         {
             content: "Click on Send Message",
             trigger: "button:contains(Send message)",
