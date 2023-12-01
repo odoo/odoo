bf474aabd91ef60e08e2075643b585345ea240be
Guillaume-gdi <gdi@odoo.com>
2023-12-01 13:39:05 +0100

    [FIX] website: make email_to of contactus form dynamic again
    The email_to field of the contactus form have to be dynamic except if
    the user manually set it via the form options. Unfortunately, the
    website signature task broke this feature. This commit fixes it and adds
    a test to prevent this regression in the future.
    
    Steps to reproduce the bug:
    - Go to the contactus page
    - Edit the page
    - Add a field on the form
    - Save the page
    - Change the company email address via the settings
    
    => The contactus form still send the email to the old email address.
    
    Related to task-3382195
    
    X-original-commit: f9154bb6db9987d4e8780063abfbcd98d7ad4a1e
    Part-of: odoo/odoo#148132

--- addons/website/tools.py
+++ addons/website/tools.py
@@ -203,10 +203,17 @@ def add_form_signature(html_fragment, env_sudo):
         # the value of email_to can still be None in case of default value
         if 'email_to' not in form_values:
             continue
-        elif not form_values['email_to'].attrib.get('value'):
-            form_values['email_to'].attrib['value'] = env_sudo.company.email or ''
+
+        email_to_value = form_values['email_to'].attrib.get('value')
+        if (not email_to_value
+            or (email_to_value == 'info@yourcompany.example.com'
+                and html_fragment.xpath('//span[@data-for="contactus_form"]'))):
+            # This means that the mail will be sent to the value of the dataFor
+            # which is the company email.
+            email_to_value = env_sudo.company.email or ''
+
         has_cc = {'email_cc', 'email_bcc'} & form_values.keys()
-        value = form_values['email_to'].attrib['value'] + (':email_cc' if has_cc else '')
+        value = email_to_value + (':email_cc' if has_cc else '')
         hash_value = hmac(env_sudo, 'website_form_signature', value)
         if has_cc:
             hash_value += ':email_cc'
