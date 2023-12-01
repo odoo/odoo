d377d89e39f72f4bf32557c857b91575077bb4f5
Thomas Beckers <tbs@odoo.com>
2023-12-01 11:12:19 +0100

    [FIX] account: avoid error when moving item to account of parent company
    On a company with branches, you can move an amount from a branch company
    account to a parent company account using "Move to Account" action. This
    will create a new entry with both accounts. If this entry is created on
    the parent company, it will trigger an error since parent company does not
    have access to branch company accounts.
    
    Steps to reproduce
    - Have a company with a branch company
    - Create an account belonging to the branch company
    - Create an entry with this account
    - Select parent company and go to journal item list
    - Select one item with the account created above and use the
    action "Move to Account"
    - Try to move to an account of parent company => Company access error
    
    Now we create the entry on the lowest child company.
    
    opw-3554088
    
    closes odoo/odoo#145089
    
    X-original-commit: ad43eb54104671a606d1d8b142df2154e86678bf
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Thomas Beckers (tbs) <tbs@odoo.com>

--- addons/account/wizard/account_automatic_entry_wizard.py
+++ addons/account/wizard/account_automatic_entry_wizard.py
@@ -207,10 +207,16 @@ class AutomaticEntryWizard(models.TransientModel):
                     'amount_currency': (account_balance > 0 and -1 or 1) * abs(account_amount_currency),
                 })
 
+        # Get the lowest child company based on accounts used to avoid access error
+        accounts = self.env['account.account'].browse([line['account_id'] for line in line_vals])
+        companies = accounts.company_id | self.env.company
+        lowest_child_company = max(companies, key=lambda company: len(company.parent_ids))
+
         return [{
             'currency_id': self.journal_id.currency_id.id or self.journal_id.company_id.currency_id.id,
             'move_type': 'entry',
             'journal_id': self.journal_id.id,
+            'company_id': lowest_child_company.id,
             'date': fields.Date.to_string(self.date),
             'ref': self.destination_account_id.display_name and _("Transfer entry to %s", self.destination_account_id.display_name or ''),
             'line_ids': [(0, 0, line) for line in line_vals],
