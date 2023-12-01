bbd53171514f9e80f8621003ad5ddcf2738ecf81
snd <snd@odoo.com>
2023-12-01 13:29:25 +0000

    [FIX] stock: unreserve the correct quantity
    case:
    - create a UoM 'Hundreds' related to UoM 'Units', bigger by factor x100
    - create a delivery picking
    - create a move with the UoM Hundreds, indicate 10 Hundreds
    - validate, 1000 should be reserved
    - unreserve, only 10 units are unreserved, 990 are still reserved
    
    This is because the stock move line takes the UoM of the stock move by default on the `quantity` field
    and `unreserve` uses that field instead of `quantity_product_uom`
    
    This bug led to the discovery of other cases where `quantity_product_uom` should be used instead of `quantity`
    
    X-original-commit: a4b21bf5c858a51313e9cc4c03c610db56f2de19

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -1559,7 +1559,7 @@ Please change the quantity done or the rounding precision of your unit of measur
                 quantity += ml.product_uom_id._compute_quantity(ml.quantity, ml.product_id.uom_id)
             grouped_move_lines_out[k] = quantity
         for k, g in groupby(move_lines_out_reserved, key=_keys_out_groupby):
-            grouped_move_lines_out[k] = sum(self.env['stock.move.line'].concat(*list(g)).mapped('quantity'))
+            grouped_move_lines_out[k] = sum(self.env['stock.move.line'].concat(*list(g)).mapped('quantity_product_uom'))
 
         return grouped_move_lines_out
 
@@ -1677,11 +1677,11 @@ Please change the quantity done or the rounding precision of your unit of measur
                     available_move_lines = move._get_available_move_lines(assigned_moves_ids, partially_available_moves_ids)
                     if not available_move_lines:
                         continue
-                    for move_line in move.move_line_ids.filtered(lambda m: m.quantity):
+                    for move_line in move.move_line_ids.filtered(lambda m: m.quantity_product_uom):
                         if available_move_lines.get((move_line.location_id, move_line.lot_id, move_line.result_package_id, move_line.owner_id)):
-                            available_move_lines[(move_line.location_id, move_line.lot_id, move_line.result_package_id, move_line.owner_id)] -= move_line.quantity
+                            available_move_lines[(move_line.location_id, move_line.lot_id, move_line.result_package_id, move_line.owner_id)] -= move_line.quantity_product_uom
                     for (location_id, lot_id, package_id, owner_id), quantity in available_move_lines.items():
-                        need = move.product_qty - sum(move.move_line_ids.mapped('quantity'))
+                        need = move.product_qty - sum(move.move_line_ids.mapped('quantity_product_uom'))
                         # `quantity` is what is brought by chained done move lines. We double check
                         # here this quantity is available on the quants themselves. If not, this
                         # could be the result of an inventory adjustment that removed totally of
