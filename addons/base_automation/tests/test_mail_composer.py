11181c28e526926fa65735b223226035cf576c7a
Sébastien Theys <seb@odoo.com>
2023-12-04 15:07:19 +0100

    [FIX] mail: fix test failing at "Write something in composer"
    The issue is not easy to reproduce, so the fix is not a guaranteed one.
    
    Estimation: the click on the "send message" buttons happens too early,
    and it is either doing nothing, or being immediately reset by another
    action that happens at page load.
    
    Adding a little bit of extra delay (in particular waiting for messages
    to be loaded) might resolve the issue.
    
    Also back-port code allowing to use contains in tours.
    
    runbot-27847
    
    closes odoo/odoo#144921
    
    X-original-commit: 5455bfcfdccd6e1003ebc760ef9f972a012de5e6
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/base_automation/tests/test_mail_composer.py
+++ addons/base_automation/tests/test_mail_composer.py
@@ -34,10 +34,11 @@ class TestMailFullComposer(MailCommon, HttpCase):
             'model_id': self.env.ref('mail.model_mail_compose_message').id,
         })
         automation.write({'action_server_ids': [(4, server_action.id)]})
+        partner = self.env["res.partner"].create({"name": "Jane", "email": "jane@example.com"})
         user = self.env["res.users"].create({"name": "Not A Demo User", "login": "nadu"})
         with self.mock_mail_app():
             self.start_tour(
-                f"/web#id={self.user_employee.partner_id.id}&model=res.partner",
+                f"/web#id={partner.id}&model=res.partner",
                 "mail/static/tests/tours/mail_composer_test_tour.js",
                 login=self.user_employee.login
             )
