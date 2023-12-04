685b419fc5a34515b99f0df4668a6eb12563be98
luvi <luvi@odoo.com>
2023-12-04 22:51:25 +0100

    [FIX] web: prevent annoying focus on touch devices
    This commit fixes an annoying issue when using Odoo on a tablet or a PC when using
    the touch screen primarly. The virtual keyboard popped way too much when navigating
    in between views and screens, since the focus is often set on inputs (mostly the
    search bar with useAutofocus).
    
    Steps to reproduce:
    - on a Windows laptop or tablet with a touch screen, open any view
    - the virtual keyboard appears
    - you must touch out of the keyboard to use Odoo
    
    To fix the useAutofocus hook, hasTouch is now being used instead of relying on the size
    of the screen.
    
    Autofocus test with isSmall has been adapted to specify that touch isn't modified, and
    that the autofocus is still present. And another test has been added asserting the
    behavior on a touch device.
    
    In the form renderer, the autofocus set manually on the first element is now prevented as
    well, reducing the popping effect of the keyboard when opening a view. A test has been
    added as well.
    
    task-3627697

--- addons/web/static/src/core/utils/hooks.js
+++ addons/web/static/src/core/utils/hooks.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 import { SERVICES_METADATA } from "@web/env";
+import { hasTouch } from "@web/core/browser/feature_detection";
 
 import { status, useComponent, useEffect, useRef, onWillUnmount } from "@odoo/owl";
 
@@ -34,14 +35,14 @@ import { status, useComponent, useEffect, useRef, onWillUnmount } from "@odoo/ow
  * @param {Object} [params]
  * @param {string} [params.refName] override the ref name "autofocus"
  * @param {boolean} [params.selectAll] if true, will select the entire text value.
- * @param {boolean} [params.mobile] if true, will autofocus on mobile devices.
+ * @param {boolean} [params.mobile] if true, will force autofocus on touch devices.
  * @returns {Ref} the element reference
  */
 export function useAutofocus({ refName, selectAll, mobile } = {}) {
     const comp = useComponent();
     const ref = useRef(refName || "autofocus");
-    // Prevent autofocus in mobile
-    if (!mobile && comp.env.isSmall) {
+    // Prevent autofocus on touch devices to avoid the virtual keyboard from popping up unexpectedly
+    if (!mobile && hasTouch()) {
         return ref;
     }
     // LEGACY
