960fb2faca38e11eeb820dcb5f4cbc5c430a1a4c
Pedram (PEBR) <pebr@odoo.com>
2023-12-04 09:38:00 +0000

    [FIX] l10n_fr_pos_cert: avoid crash on backspace with empty order
    Prior to this commit, pressing the backspace key to remove an orderline
    when no orderline was present resulted in a traceback error.
    
    opw-3622679
    
    X-original-commit: 80eb60388d9aeab377ecc570284d6aeb991c5aea

--- addons/l10n_fr_pos_cert/static/src/js/pos.js
+++ addons/l10n_fr_pos_cert/static/src/js/pos.js
@@ -22,7 +22,7 @@ patch(PosGlobalState.prototype, "l10n_fr_pos_cert.PosGlobalState", {
         let result = this._super(...arguments);
         let selectedOrderLine = this.selectedOrder.get_selected_orderline();
         //Note: is_reward_line is a field in the pos_loyalty module
-        if (selectedOrderLine.is_reward_line) {
+        if (selectedOrderLine && selectedOrderLine.is_reward_line) {
             //Always allow quantity change for reward lines
             return false || result;
         }
