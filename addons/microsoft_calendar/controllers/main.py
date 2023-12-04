b073546bd18a860a4afccaf141884e96c1eaea28
Gabriel de Paula Felix (gdpf) <gdpf@odoo.com>
2023-12-04 11:40:13 -0300

    [FIX] microsoft_calendar: recurrence outliers blocking sync restart and account reset
    Before this commit, when restarting the synchronization, recurrence outliers were blocking the synchronization restarting because they were being considered as complete recurrences, although they must be treated as single events. Additionaly, when resetting the Outlook account, choosing to delete events in Odoo should not be allowed since recurrence outliers must be treated as single events.
    
    After this commit, this issue is now solved by adding the 'dont_notify' param in the context to skip the validation of single events when restarting the sync. Additionaly, when resetting the Outlook account, the forbiddance of deleting events in Odoo is not a blocking point anymore.
    
    closes odoo/odoo#144917
    
    Task-id: 3617978
    X-original-commit: c0e1bbe2351068592a25db3757d18e11e43b464c
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>
    Signed-off-by: Gabriel de Paula Felix (gdpf) <gdpf@odoo.com>

--- addons/microsoft_calendar/controllers/main.py
+++ addons/microsoft_calendar/controllers/main.py
@@ -52,6 +52,7 @@ class MicrosoftCalendarController(http.Controller):
                 })
 
             # If App authorized, and user access accepted, We launch the synchronization
+            sync_context.update({'dont_notify': True})
             need_refresh = request.env.user.sudo().with_context(sync_context)._sync_microsoft_calendar()
 
             # If synchronization has been stopped
