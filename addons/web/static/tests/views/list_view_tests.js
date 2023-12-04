8fbdbb59eb3f37fad1fb75f4ff363f7fa17e6d27
Aaron Bohy <aab@odoo.com>
2023-12-04 13:15:07 +0100

    [FIX] web: allow to resize columns in grouped list views
    Before this commit, columns in list views could be manually resized
    only if there were records in the table. For the ungrouped case it
    makes sense, as the table is empty anyway. However, for the grouped
    case (either with empty groups, or with all groups being folded),
    the list contains no records, but resizing could make sense as
    there could be aggregate values displayed on group headers.
    
    Technically speaking, there's no reason to restrict the resize
    feature if the table is empty. Functionally wise, a user could get
    confused if the feature is sometimes available, and sometimes not.
    For that reason, this commit simply removes the isEmpty condition,
    meaning that resizing columns is now always available, even if the
    table is empty.
    
    Task 3621490
    
    closes odoo/odoo#144767
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/web/static/tests/views/list_view_tests.js
+++ addons/web/static/tests/views/list_view_tests.js
@@ -4505,11 +4505,7 @@ QUnit.module("Views", (hooks) => {
                     </tree>`,
             });
 
-            assert.containsNone(
-                target,
-                ".o_resize",
-                "There shouldn't be any resize handle if no data"
-            );
+            assert.containsN(target, ".o_resize", 8);
             assertions.forEach((a) => {
                 assert.strictEqual(
                     target.querySelector(`th[data-name="${a.field}"]`).offsetWidth,
@@ -4696,11 +4692,7 @@ QUnit.module("Views", (hooks) => {
 
             assert.isVisible(target.querySelector(".o_field_one2many"));
 
-            assert.containsNone(
-                target,
-                ".o_field_one2many .o_resize",
-                "There shouldn't be any resize handle if no data"
-            );
+            assert.containsN(target, ".o_field_one2many .o_resize", 8);
             assertions.forEach((a) => {
                 assert.strictEqual(
                     target.querySelector(`.o_field_one2many th[data-name="${a.field}"]`).style
@@ -5136,11 +5128,7 @@ QUnit.module("Views", (hooks) => {
                 groupBy: ["int_field"],
             });
 
-            assert.containsNone(
-                target,
-                ".o_resize",
-                "There shouldn't be any resize handle if no data"
-            );
+            assert.containsN(target, ".o_resize", 8);
             assertions.forEach((a) => {
                 assert.strictEqual(
                     a.expected,
