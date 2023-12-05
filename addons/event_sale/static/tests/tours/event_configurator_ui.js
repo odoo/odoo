9850d973fd666466bc32b1cfa6e89a154ce14393
Yannick Tivisse <yti@odoo.com>
2023-12-05 14:15:56 +0100

    [FIX] event: Make tests demo data independent
    Part-of: odoo/odoo#146686

--- addons/event_sale/static/tests/tours/event_configurator_ui.js
+++ addons/event_sale/static/tests/tours/event_configurator_ui.js
@@ -23,7 +23,7 @@ registry.category("web_tour.tours").add('event_configurator_tour', {
         actions.text('Event Registration');
     }
 }, {
-    trigger: 'ul.ui-autocomplete a:contains("Event")',
+    trigger: 'ul.ui-autocomplete a:contains("Event Registration")',
     run: 'click'
 }, {
     trigger: 'div[name="event_id"] input',
--- addons/event_sale/tests/test_event_sale_ui.py
+++ addons/event_sale/tests/test_event_sale_ui.py
@@ -10,6 +10,20 @@ from odoo.tests import HttpCase, tagged
 class TestUi(HttpCase):
 
     def test_event_configurator(self):
+        # self.env["account.chart.template"].try_loading('generic_coa', self.env.company)
+
+        self.env['account.tax.group'].create(
+            {'name': 'Test Account Tax Group', 'company_id': self.env.company.id}
+        )
+
+        self.tax_10 = self.env['account.tax'].sudo().create({
+            'name': 'Tax 10',
+            'amount': 10,
+        })
+
+        self.product_event = self.env.ref('event_sale.product_product_event')
+        self.product_event.taxes_id = [(6, 0, self.tax_10.ids)]
+
         event = self.env['event.event'].create({
             'name': 'Design Fair Los Angeles',
             'date_begin': Datetime.now() + timedelta(days=1),
@@ -19,10 +33,10 @@ class TestUi(HttpCase):
         self.env['event.event.ticket'].create([{
             'name': 'Standard',
             'event_id': event.id,
-            'product_id': self.env.ref('event_sale.product_product_event').id,
+            'product_id': self.product_event.id,
         }, {
             'name': 'VIP',
             'event_id': event.id,
-            'product_id': self.env.ref('event_sale.product_product_event').id,
+            'product_id': self.product_event.id,
         }])
         self.start_tour("/web", 'event_configurator_tour', login="admin")
