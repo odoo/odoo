c492b321c91cf80d41bda62baf2f48c6f067c8ea
Yannick Tivisse <yti@odoo.com>
2023-12-05 14:22:38 +0100

    [FIX] website: Make tests demo data independent

--- addons/website_blog/tests/test_performance.py
+++ addons/website_blog/tests/test_performance.py
@@ -12,9 +12,35 @@ class TestBlogPerformance(UtilPerf):
         if 'channel_id' in self.env['website']:
             self.env['website'].search([]).channel_id = False
 
+        self.env['blog.blog'].search([]).active = False
+        blogs = self.env['blog.blog'].create([{
+            "name": 'aaa Blog Test',
+            "subtitle": 'Blog Test Subtitle',
+            "cover_properties": """{"background-image": "url('/website_blog/static/src/img/blog_1.jpeg')", "resize_class": "o_record_has_cover o_half_screen_height", "opacity": "0.4"}""",
+        }, {
+            "name": 'bbb Blog Test',
+            "subtitle": 'Blog Test Subtitle',
+            "cover_properties": """{"background-image": "url('/website_blog/static/src/img/blog_1.jpeg')", "resize_class": "o_record_has_cover o_half_screen_height", "opacity": "0.4"}""",
+        }])
+
+        blog_tags = self.env['blog.tag'].create([{
+            'name': 'Tag 1',
+        }, {
+            'name': 'Tag 2',
+        }])
+        self.env['blog.post'].create([{
+            "name": "Post Test",
+            "subtitle": "Subtitle Test",
+            "blog_id": blog.id,
+            "author_id": self.env.user.id,
+            "tag_ids": [(4, tag.id) for tag in blog_tags],
+            "is_published": True,
+            "cover_properties": """{"background-image": "url('/website_blog/static/src/img/cover_1.jpg')", "resize_class": "o_record_has_cover o_half_screen_height", "opacity": "0"}""",
+        } for blog in blogs])
+
     def test_10_perf_sql_blog_standard_data(self):
         self.assertEqual(self._get_url_hot_query('/blog'), 11)
-        self.assertEqual(self._get_url_hot_query('/blog', cache=False), 24)
+        self.assertEqual(self._get_url_hot_query('/blog', cache=False), 21)
 
     def test_20_perf_sql_blog_bigger_data_scaling(self):
         BlogPost = self.env['blog.post']
