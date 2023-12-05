a0764f6ab0e93e0ce4adc8df2c6fa1d284e89f61
Yannick Tivisse <yti@odoo.com>
2023-12-05 14:17:36 +0100

    [FIX] mail: Make tests demo data independent

--- addons/mail/tests/test_user_modify_own_profile.py
+++ addons/mail/tests/test_user_modify_own_profile.py
@@ -11,6 +11,11 @@ class TestUserModifyOwnProfile(HttpCaseWithUserDemo):
     def test_user_modify_own_profile(self):
         """" A user should be able to modify their own profile.
         Even if that user does not have access rights to write on the res.users model. """
-
+        if 'hr.employee' in self.env and not self.user_demo.employee_id:
+            self.env['hr.employee'].create({
+                'name': 'Marc Demo',
+                'user_id': self.user_demo.id,
+            })
+        self.user_demo.tz = "Europe/Brussels"
         self.start_tour("/web", "mail/static/tests/tours/user_modify_own_profile_tour.js", login="demo")
-        self.assertEqual(self.env.ref('base.user_demo').email, "updatedemail@example.com")
+        self.assertEqual(self.user_demo.email, "updatedemail@example.com")
