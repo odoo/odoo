b2b7f590ed53efaaeb78385030fec975935133e1
Yannick Tivisse <yti@odoo.com>
2023-12-05 14:14:49 +0100

    [FIX] crm: Make tests demo data independent

--- addons/crm/tests/test_crm_lead.py
+++ addons/crm/tests/test_crm_lead.py
@@ -814,6 +814,7 @@ class TestCRMLead(TestCrmCommon):
 class TestLeadFormTools(FormatAddressCase):
 
     def test_address_view(self):
+        self.env.company.country_id = self.env.ref('base.us')
         self.assertAddressView('crm.lead')
 
 
