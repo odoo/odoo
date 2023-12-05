2978195604d436b90c5d9b88ed4384f745e5a7de
Benoit Socias <bso@odoo.com>
2023-12-05 14:28:11 +0000

    [FIX] website: edit HTML in default website language
    Since the introduction of delay translation in [1] only the edited
    master/default language version of the page was being stored in the
    database's JSONB `arch_db`.
    
    Because of this if the default language of a website was not `en_US`,
    after edit the `en_US` version of the page remained the empty page.
    But since [2] the HTML editor edits (reads and writes) the page in
    `en_US` only, it was therefore editing the empty page instead of the
    actual "master" one. This led to destroying the actual content that
    existed in other languages when saving such a modified page from the
    HTML editor.
    
    To avoid this [3] temporarily disabled the delay translation feature.
    
    This commit makes the HTML editor modify the version of the page in the
    default language of the website by using a dedicated `save_xml` route.
    
    Steps to reproduce:
    - Add Dutch as the default language of a website.
    - Create a new Dutch page.
    - Drop an accordion snippet.
    - Save.
    - Edit the page using the HTML editor. => Empty page is being edited.
    - Add a blank line.
    - Save.
    
    => Content was lost.
    
    Note that this commit does not reactive the delay translation feature
    because further verifications are needed as described in the commit
    message of [3].
    
    [1]: https://github.com/odoo/odoo/commit/2d08f97c0778469b409fca23f2be5f5a98ce3df8
    [2]: https://github.com/odoo/odoo/commit/fdb9f8273be62d0a6d8051f7ca0a66cdf22ae5e7
    [3]: https://github.com/odoo/odoo/commit/0e0a74f8c5fc9f45311e629a76608c6c986d635d
    
    opw-3493355
    task-3621753
    
    X-original-commit: d3c6fbb695343a26d34a8dbddfe9d8a7fa11f2c5

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -733,6 +733,10 @@ class Website(Home):
                 result.append(group)
         return result
 
+    @http.route('/website/save_xml', type='jsonrpc', auth='user', website=True)
+    def save_xml(self, view_id, arch):
+        request.env['ir.ui.view'].browse(view_id).with_context(lang=request.website.default_lang_id.code).arch = arch
+
     @http.route("/website/get_switchable_related_views", type="jsonrpc", auth="user", website=True, readonly=True)
     def get_switchable_related_views(self, key):
         views = request.env["ir.ui.view"].get_related_views(key, bundles=False).filtered(lambda v: v.customize_show)
