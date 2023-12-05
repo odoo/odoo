26a89e713666ed89eae17b0d1e038a2ae3e0353e
Mathieu Duckerts-Antoine <dam@odoo.com>
2023-12-05 13:59:41 +0100

    [FIX] web: py_js: do not parse []a
    Before this commit, py_js would parse successfully "[]a" and would return
    the ast corresponding to "[]". This would lead the domain selector (for
    instance) to recognize "[]a" as a valid domain.
    
    Task ID: 3570274
    
    closes odoo/odoo#144998
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/core/py_js/py_parser.js
+++ addons/web/static/src/core/py_js/py_parser.js
@@ -369,7 +369,11 @@ function _parse(tokens, bp = 0) {
  */
 export function parse(tokens) {
     if (tokens.length) {
-        return _parse(tokens, 0);
+        const ast = _parse(tokens, 0);
+        if (tokens.length) {
+            throw new ParserError("Token(s) unused");
+        }
+        return ast;
     }
     throw new ParserError("Missing token");
 }
