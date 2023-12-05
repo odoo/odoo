4662515dd971cb345856c82e9d7c27c8b5556e83
Yosua Nicolaus <yoni@odoo.com>
2023-12-05 11:51:41 +0100

    [FIX] account: unaccessible parent's account on product form
    If the current company is a branch and we're in the product form, we
    will not be able to access our parent's account in the following fields:
    
    - `property_account_income_categ_id`
    - `property_account_expense_categ_id`
    - `property_account_income_id`
    - `property_account_expense_id`
    
    This commit aims to fix that behavior by updating the domain to consider
    branch companies too
    
    closes odoo/odoo#140773
    
    Task-id: 3549961
    Related: odoo/enterprise#49456
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/models/product.py
+++ addons/account/models/product.py
@@ -5,7 +5,7 @@ from odoo.exceptions import ValidationError
 from odoo.osv import expression
 from odoo.tools import format_amount
 
-ACCOUNT_DOMAIN = "['&', '&', ('deprecated', '=', False), ('account_type', 'not in', ('asset_receivable','liability_payable','asset_cash','liability_credit_card','off_balance')), ('company_id', '=', current_company_id)]"
+ACCOUNT_DOMAIN = "['&', ('deprecated', '=', False), ('account_type', 'not in', ('asset_receivable','liability_payable','asset_cash','liability_credit_card','off_balance'))]"
 
 class ProductCategory(models.Model):
     _inherit = "product.category"
