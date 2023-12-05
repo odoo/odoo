5e8588ddf63d94b6d71cecc9648e9dd173b7ef84
Yannick Tivisse <yti@odoo.com>
2023-12-05 14:14:05 +0100

    [FIX] calendar: Make tests demo data independent

--- addons/calendar/tests/test_calendar.py
+++ addons/calendar/tests/test_calendar.py
@@ -5,6 +5,7 @@ import datetime
 from datetime import date, datetime, timedelta
 
 from odoo import fields, Command
+from odoo.addons.base.tests.common import HttpCaseWithUserDemo
 from odoo.tests import Form, HttpCase, tagged
 from odoo.addons.base.tests.common import SavepointCaseWithUserDemo
 import pytz
@@ -393,7 +394,7 @@ class TestCalendar(SavepointCaseWithUserDemo):
 
 
 @tagged('post_install', '-at_install')
-class TestCalendarTours(HttpCase):
+class TestCalendarTours(HttpCaseWithUserDemo):
     def test_calendar_month_view_start_hour_displayed(self):
         """ Test that the time is displayed in the month view. """
         self.start_tour("/web", 'calendar_appointments_hour_tour', login="demo")
@@ -426,7 +427,7 @@ class TestCalendarTours(HttpCase):
             Check that we can decline events.
         """
         user_admin = self.env.ref('base.user_admin')
-        user_demo = self.env.ref('base.user_demo')
+        user_demo = self.user_demo
         start = datetime.combine(date.today(), datetime.min.time()).replace(hour=9)
         stop = datetime.combine(date.today(), datetime.min.time()).replace(hour=12)
         event = self.env['calendar.event'].with_user(user_admin).create({
@@ -451,7 +452,7 @@ class TestCalendarTours(HttpCase):
             Check that we can decline events with the "Everybody's calendars" filter.
         """
         user_admin = self.env.ref('base.user_admin')
-        user_demo = self.env.ref('base.user_demo')
+        user_demo = self.user_demo
         start = datetime.combine(date.today(), datetime.min.time()).replace(hour=9)
         stop = datetime.combine(date.today(), datetime.min.time()).replace(hour=12)
         event = self.env['calendar.event'].with_user(user_admin).create({
