5a12a57e6b928064a326111da9936688a3f689fd
Yannick Tivisse <yti@odoo.com>
2023-12-05 14:14:05 +0100

    [FIX] calendar: Make tests demo data independent
    Part-of: odoo/odoo#146314

--- addons/google_calendar/tests/test_sync_odoo2google.py
+++ addons/google_calendar/tests/test_sync_odoo2google.py
@@ -19,6 +19,7 @@ class TestSyncOdoo2Google(TestSyncGoogle):
 
     def setUp(self):
         super().setUp()
+        self.env.user.partner_id.tz = "Europe/Brussels"
         self.google_service = GoogleCalendarService(self.env['google.service'])
         # Make sure this test will work for the next 30 years
         self.env['ir.config_parameter'].set_param('google_calendar.sync.range_days', 10000)
@@ -123,7 +124,7 @@ class TestSyncOdoo2Google(TestSyncGoogle):
                 'res_id': partner.id,
             })
 
-        with self.assertQueryCount(__system__=32):
+        with self.assertQueryCount(__system__=37):
             event.unlink()
 
     def test_event_without_user(self):
