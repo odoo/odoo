9d15b70b2febf6d03bcf56b421923f14f8e3653f
Pedram (PEBR) <pebr@odoo.com>
2023-12-05 13:21:02 +0000

    [FIX] pos_sale: display Internal Reference for loaded orderlines
    Previously, after the changes made in https://github.com/odoo/odoo/commit/5a0113a7d74ff8c429501e922c3cd1fae2535695, PoS stopped
    showing the internal ref for products in imported sale orders.
    
    opw-3620141
    
    X-original-commit: b3a1d0e280ce4a67b07e74e6f981cc3f31c45007

--- addons/pos_sale/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
+++ addons/pos_sale/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
@@ -214,7 +214,7 @@ export class SaleOrderManagementScreen extends ControlButtonsMixin(IndependentTo
                         pos: globalState,
                         order: globalState.get_order(),
                         product: globalState.db.get_product_by_id(line.product_id[0]),
-                        description: line.name,
+                        description: line.product_id[1],
                         price: line.price_unit,
                         tax_ids: orderFiscalPos ? undefined : line.tax_id,
                         price_manually_set: false,
