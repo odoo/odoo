db0a1beea7d6b2f2d2c38f0719878b1f5c8f6ba0
Yannick Tivisse <yti@odoo.com>
2023-12-05 14:21:24 +0100

    [FIX] sale: Make tests demo data independent

--- addons/sale/tests/test_sale_product_attribute_value_config.py
+++ addons/sale/tests/test_sale_product_attribute_value_config.py
@@ -2,16 +2,20 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 from odoo.addons.product.tests.test_product_attribute_value_config import TestProductAttributeValueCommon
 from odoo.tests import tagged
 
 
-class TestSaleProductAttributeValueCommon(TestProductAttributeValueCommon):
+@tagged("post_install", "-at_install")
+class TestSaleProductAttributeValueCommon(AccountTestInvoicingCommon, TestProductAttributeValueCommon):
 
     @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass(chart_template_ref=chart_template_ref)
+        cls.computer.company_id = cls.env.company
+        cls.computer = cls.computer.with_env(cls.env)
         cls.env['product.pricelist'].sudo().search([]).action_archive()
         cls.env['product.pricelist'].create({'name': 'Base Pricelist'})
 
