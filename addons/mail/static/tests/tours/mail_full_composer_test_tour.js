25f10ba4fdb35dc375a3bd2e7d39c79086429222
Sébastien Theys <seb@odoo.com>
2023-12-06 11:14:12 +0100

    [FIX] mail, base_automation: fix indeterministic composer tour
    Back-port changes done in recent versions.
    
    Also run can be async but it's not actually awaited, so an extra_trigger
    is necessary for proper waiting.
    
    runbot-45508
    
    closes odoo/odoo#145120
    
    X-original-commit: 8843ebff97550614ba890f45725339d1103dca35
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/tests/tours/mail_full_composer_test_tour.js
+++ addons/mail/static/tests/tours/mail_full_composer_test_tour.js
@@ -20,12 +20,14 @@ tour.register(
             content: "Wait for the chatter to be fully loaded",
             trigger: ".o_Chatter",
             async run() {
-                await contains(".o_Message", { count: 2 });
+                await contains(".o_MessageView", { count: 1 });
+                document.body.setAttribute("data-found-message", 1);
             },
         },
         {
             content: "Click on Send Message",
             trigger: ".o_ChatterTopbar_buttonSendMessage",
+            extra_trigger: "body[data-found-message=1]",
         },
         {
             content: "Write something in composer",
@@ -61,9 +63,9 @@ tour.register(
             trigger: '[name="subject"] input',
             run() {
                 const subjectValue = document.querySelector('[name="subject"] input').value;
-                if (subjectValue !== "Test User") {
+                if (subjectValue !== "Jane") {
                     console.error(
-                        `Full composer should have "Test User" in subject input (actual: ${subjectValue})`
+                        `Full composer should have "Jane" in subject input (actual: ${subjectValue})`
                     );
                 }
             },
