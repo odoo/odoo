6ab1f9e96dfc7b3e97a035d0fc2d2698f56ddd1e
Loan (LSE) <lse@odoo.com>
2023-12-06 16:57:29 +0100

    [IMP] hw_drivers: remove iot daily.cron
    This is no longer necessary with the introduction of:
    https://github.com/odoo/odoo/pull/144652

--- addons/hw_drivers/controllers/driver.py
+++ addons/hw_drivers/controllers/driver.py
@@ -47,7 +47,7 @@ class DriverController(http.Controller):
     def check_certificate(self):
         """
         This route is called when we want to check if certificate is up-to-date
-        Used in cron.daily
+        Previously used in iot-box cron.daily, now deprecated
         """
         helpers.get_certificate_status()
 
