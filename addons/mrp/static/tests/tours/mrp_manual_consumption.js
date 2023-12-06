a10fc0e040ea8cfe2d8d06a4237df363351d1997
Djamel Touati <otd@odoo.com>
2023-12-06 08:10:37 +0100

    [FIX] mrp: validate a MO with manual consumption
    Steps to reproduce the bug:
    - Create a storable produce P1 with BoM:
        - select any component, qty: 1 unit
    
    - Create a MO to produce one unit of P1
    - Confirm the MO
    - Update the qty of the component to 2
    - Validate the MO
    
    Problem:
    The quantity of the component is overwritten and reset to 1
    Because, when the new qty of the component is set, only the
    “manual_consumption” field is updated to “True” thanks to their compute:
    
    https://github.com/odoo/odoo/blob/17.0/addons/mrp/models/stock_move.py#L138-L145
    
    But the field ‘picked’, is not updated
    So when we check if we have to set a quantity, the condition is false
    and the quantity is reset to the original value:
    
    https://github.com/odoo/odoo/blob/26193fdb707a631d498cbc37d68c08497e32a31d/addons/mrp/models/mrp_production.py#L1201-L1202
    
    opw-3616628

--- addons/mrp/static/tests/tours/mrp_manual_consumption.js
+++ addons/mrp/static/tests/tours/mrp_manual_consumption.js
@@ -70,3 +70,37 @@ registry.category("web_tour.tours").add('test_mrp_manual_consumption', {
         },
         ...stepUtils.saveForm(),
 ]});
+
+registry.category("web_tour.tours").add('test_mrp_manual_consumption_02', {
+    test: true,
+    steps: () => [
+        {
+            trigger: 'div[name=move_raw_ids] td[name="quantity"]:last:contains("0.00")',
+            run: () => {},
+        },
+        {
+            trigger: 'div[name=move_raw_ids] td[name="quantity"]:last',
+            run: 'click',
+        },
+        {
+            trigger: 'div[name="quantity"] input',
+            run: 'text 16.0'
+        },
+        {
+            content: "Click Pager",
+            trigger: ".o_pager_value:first()",
+        },
+        {
+            trigger: "input[id='qty_producing_0']",
+            run: 'text 8.0',
+        },
+        {
+            content: "Click Pager",
+            trigger: ".o_pager_value:first()",
+        },
+        {
+            trigger: 'div[name=move_raw_ids] td[name="quantity"]:last:contains("16.00")',
+            run: () => {},
+        },
+        ...stepUtils.saveForm(),
+]});
