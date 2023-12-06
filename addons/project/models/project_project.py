5930e725a460f4d44d38ee912c1bde617c34848e
Hugo Carlier (Huca) <huca@odoo.com>
2023-12-06 11:09:09 +0100

    [FIX] project: remove onchange on date/date_end for projects
    Steps
    =====
    - Install module project
    - Open the form view of a project
    - Set a planned date
    - Click anywhere on the page to remove the focus from the date picker
    
    Issue
    =====
    The chosen date disappeared without any explanation to the user
    
    Cause
    =====
    odoo#123268 introduced some changes related
    to the planned dates of projects.
    1. the write method is modified to avoid the write of only a start/end
    date (both date should be set or none of them)
    2. a required is added on the start date field in the form view of
    project
    3. an onchange is added to avoid to have one of both field set and not
    the other
    
    This last change is problematic as it overrides the required behavior of
    the date fields.
    
    On top of that, https://github.com/odoo/odoo/pull/143509 changed the
    behavior of the datepicker widget that only display the full range by
    default if both fields are required (which is not the case here if they
    are empty).
    
    The combination of those changes have the consequence that the planned
    date field is unusable in the form view of projects.
    
    Fix
    ===
    The onchange being only useful in form view, it is deprecated as the
    required attribute is sufficient for this usecase and provide the user
    warnings if he forget a field. On top of that a required attribute is
    also set on the end date field.
    
    task-3628069
    
    closes odoo/odoo#147928
    
    X-original-commit: e9127a9fdffad3281548eb2774871d0d58114a92
    Related: odoo/enterprise#53589
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/models/project_project.py
+++ addons/project/models/project_project.py
@@ -400,13 +400,6 @@ class Project(models.Model):
             else:
                 project.access_instruction_message = ''
 
-    @api.onchange('date_start', 'date')
-    def _onchange_planned_date(self):
-        if not self.date and self.date_start:
-            self.date_start = False
-        elif not self.date_start and self.date:
-            self.date = False
-
     @api.model
     def _map_tasks_default_values(self, task, project):
         """ get the default value for the copied task on project duplication """
