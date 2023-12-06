a41d12fa41ee501c2d084a9df9c2b7eb4936b837
Louis (loco) <loco@odoo.com>
2023-12-06 10:40:23 +0100

    [FIX] website: add longer timeout for the website form editor test
    When running the `website_form_editor_tour` test, it may sometimes fail
    due to a `Script timeout exceeded` error as the tour is taking longer
    than 60 seconds. The goal of this commit is to, as already done in
    [this commit] for the version 17, extend the timeout allowed for it to
    run.
    
    [this commit]: https://github.com/odoo/odoo/commit/701a775ec4c16a1adfab62e15a2d56b3933e0057
    
    runbot-24988

--- addons/website/tests/test_website_form_editor.py
+++ addons/website/tests/test_website_form_editor.py
@@ -18,7 +18,7 @@ class TestWebsiteFormEditor(HttpCaseWithUserPortal):
         })
 
     def test_00_tour(self):
-        self.start_tour("/", 'website_form_editor_tour', login="admin")
+        self.start_tour("/", 'website_form_editor_tour', login="admin", timeout=150)
         self.start_tour("/", 'website_form_editor_tour_submit')
         self.start_tour("/", 'website_form_editor_tour_results', login="admin")
 
