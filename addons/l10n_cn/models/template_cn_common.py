f727a71ada150d18caa4a320bcc6a55e1147ea1a
Anthony Chan <anch@odoo.com>
2023-12-07 14:17:07 +0800

    [IMP] l10n_cn: Add COA for large business
    Adds a new COA for large businesses in China to better support their needs out of the box. Also implements new taxes for the same large businesses.
    
    3202831
    
    closes odoo/odoo#148258
    
    X-original-commit: 66a94b27ed6bc82b60708948a9e579a468af5153
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_cn/models/template_cn_common.py
+++ addons/l10n_cn/models/template_cn_common.py
@@ -1,19 +1,26 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models
+from odoo import models, _
 from odoo.addons.account.models.chart_template import template
 
 
 class AccountChartTemplate(models.AbstractModel):
     _inherit = 'account.chart.template'
 
-    @template('cn')
-    def _get_cn_template_data(self):
+    @template('cn_common')
+    def _get_cn_common_template_data(self):
         return {
-            'parent': 'cn_common',
+            'name': _('Common'),
+            'visible': 0,
+            'code_digits': 6,
+            'use_storno_accounting': True,
+            'property_account_receivable_id': 'l10n_cn_common_112200',
+            'property_account_payable_id': 'l10n_cn_common_220200',
+            'property_account_expense_categ_id': 'l10n_cn_common_640100',
+            'property_account_income_categ_id': 'l10n_cn_common_600100',
         }
 
-    @template('cn', 'res.company')
-    def _get_cn_res_company(self):
+    @template('cn_common', 'res.company')
+    def _get_cn_common_res_company(self):
         return {
             self.env.company.id: {
                 'account_fiscal_country_id': 'base.cn',
@@ -23,16 +30,5 @@ class AccountChartTemplate(models.AbstractModel):
                 'account_default_pos_receivable_account_id': 'l10n_cn_common_112400',
                 'income_currency_exchange_account_id': 'l10n_cn_common_605100',
                 'expense_currency_exchange_account_id': 'l10n_cn_common_671100',
-                'account_sale_tax_id': 'l10n_cn_sales_included_13',
-                'account_purchase_tax_id': 'l10n_cn_purchase_excluded_13',
-            },
-        }
-
-    @template('cn', 'account.journal')
-    def _get_cn_account_journal(self):
-        return {
-            'cash': {
-                'name': 'Cash on Hand',
-                'default_account_id': 'l10n_cn_common_100100'
             },
         }
