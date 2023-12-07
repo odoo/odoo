e5d3782a7e4bfa947d1ab75f5f71bbd3ecf32ec5
Pedram (PEBR) <pebr@odoo.com>
2023-12-07 14:25:44 +0100

    [FIX] pos_sale: keep sale order references in pos restaurant
    Before this commit, if the user tried to settle a sale order
    in PoS restaurant, then went back to floor and then returned
    to the table, the sale order reference will disappear.
    
    The problem is that the `sale_order_origin_id` is not loaded.
    
    opw-3550351
    
    closes odoo/odoo#149128
    
    X-original-commit: 13db10c1c73418ce4eabc52ef2415a1f27f1e24e
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/pos_sale/models/pos_order.py
+++ addons/pos_sale/models/pos_order.py
@@ -124,6 +124,27 @@ class PosOrder(models.Model):
             'domain': [('id', 'in', linked_orders.ids)],
         }
 
+    def _get_fields_for_order_line(self):
+        fields = super(PosOrder, self)._get_fields_for_order_line()
+        fields.extend([
+            'sale_order_origin_id',
+            'down_payment_details',
+            'sale_order_line_id',
+        ])
+        return fields
+
+    def _prepare_order_line(self, order_line):
+        order_line = super()._prepare_order_line(order_line)
+        if order_line.get('sale_order_origin_id'):
+            order_line['sale_order_origin_id'] = {
+                'id': order_line['sale_order_origin_id'][0],
+                'name': order_line['sale_order_origin_id'][1],
+            }
+        if order_line.get('sale_order_line_id'):
+            order_line['sale_order_line_id'] = {
+                'id': order_line['sale_order_line_id'][0],
+            }
+        return order_line
 
 class PosOrderLine(models.Model):
     _inherit = 'pos.order.line'
