37b022e5cbb3521551c51b5eecb04a225e2afe8f
Hubert Van De Walle <huvw@odoo.com>
2023-12-07 15:27:58 +0100

    [FIX] web: no_quick_create option ignored
    Steps to reproduce
    ==================
    
    1. Install event
    2. Go to Events
    3. Configuration > Event Templates
    4. Click on any event
    5. Click on Communication
    6. Add a line, change type to SMS
    7. Type anything in Template
    8. Click on Create "..."
    9. The operation cannot be completed
    
    Cause of the issue
    ==================
    
    For many2one and many2many editable fields, the server sets the
    can_create and can_write attributes on the field nodes. [1][2]
    
    In some cases [3], those attributes are missing even though the fields
    are actually editable. They are also absent for reference fields.
    
    The `canQuickCreate` props was set to the following value:
    `canCreate && !noQuickCreate`. When canCreate was absent, this was
    evaluated to undefined, thus ignoring the noQuickCreate option.
    
    Solution
    ========
    
    When the permission attributes are missing, we have to assume that the
    user has the appropriate permissions.
    
    We also have to cast the extracted props to booleans as if you have an
    props with a value of undefined, it will fall back to the default value.
    
    In the test `no_quick_create option on a many2one when can_create is absent`,
    we set the field as readonly and then cancel the readonly on the view.
    This reproduces one of the bug of [3] which causes the permission
    attributes to be missing.
    
    -----
    
    [1]: https://github.com/odoo/odoo/blob/fa6da3d63a3a4cce4e8ca788a49772925d318764/odoo/addons/base/models/ir_ui_view.py#L1296-L1297
    [2]: https://github.com/odoo/odoo/blob/fa6da3d63a3a4cce4e8ca788a49772925d318764/odoo/addons/base/models/ir_ui_view.py#L1080-L1086
    [3]: https://github.com/odoo/odoo/pull/138032
    
    opw-3609799
    
    closes odoo/odoo#147364
    
    X-original-commit: 696a34443fde16c33e05ab3bc81e55aaaaffad47
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Simon Goffin (sig) <sig@odoo.com>

--- addons/web/static/tests/mock_server_tests.js
+++ addons/web/static/tests/mock_server_tests.js
@@ -1301,4 +1301,53 @@ QUnit.module("MockServer", (hooks) => {
         const { views } = mockServer.mockGetViews("bar", { views: [[10001, "list"]], options: {} });
         assert.deepEqual(views.list.arch, expectedList);
     });
+
+    QUnit.test(
+        "access rights attributes are present on an editable many2one field",
+        async function (assert) {
+            data.views = {
+                "bar,10001,form": `<form>
+                    <field name="partner_id"/>
+                </form>`,
+                "bar,10001,search": `<search></search>`,
+            };
+
+            const expectedForm = `<form>
+                    <field name="partner_id" can_create="true" can_write="true"/>
+                </form>`;
+            const mockServer = new MockServer(data);
+            const { views } = mockServer.mockGetViews("bar", {
+                views: [[10001, "form"]],
+                options: {},
+            });
+            assert.deepEqual(views.form.arch, expectedForm);
+        }
+    );
+
+    QUnit.test(
+        "access rights attributes are missing on an editable many2one field",
+        async function (assert) {
+            // The access rights attributes should be present,
+            // but are actually missing when a field definition is readonly and readonly=0 is on the view.
+            // @see the commit description for more details.
+
+            data.models.bar.fields.partner_id.readonly = true;
+            data.views = {
+                "bar,10001,form": `<form>
+                    <field name="partner_id" readonly="0"/>
+                </form>`,
+                "bar,10001,search": `<search></search>`,
+            };
+
+            const expectedForm = `<form>
+                    <field name="partner_id"/>
+                </form>`;
+            const mockServer = new MockServer(data);
+            const { views } = mockServer.mockGetViews("bar", {
+                views: [[10001, "form"]],
+                options: {},
+            });
+            assert.deepEqual(views.form.arch, expectedForm);
+        }
+    );
 });
