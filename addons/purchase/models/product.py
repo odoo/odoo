202bf8d8b7f76643a9350c6abdcf32517fd95ce1
AH-Yussef <alhy@odoo.com>
2023-12-07 14:48:18 +0000

    [FIX] purchase: set product control policy based on purchase bill policy
    Steps to reproduce:
    - Install Purchase and Inventory apps
    - Go to General Settings > Purchase > Invoicing > Bill Control
    - Choose "Ordered quantities" and Save
    - Go to Purchase app > Products > Products
    - Create a New product
    - Set the product type to "Storable product"
    - Go to "Purchase" tab > Control Policy
    - Notice how the control policy is "On received quantities" although we set it up to be "On Ordered quantities"
    
    Discussion:
    - I added a compute method to manage the default **purchase method** value based on the res.config.setting **Bill Control**.
    - I kept the behavior of setting the `purchase_method` to **receive** when the product type is **service** based on [task-2897867](https://www.odoo.com/web#id=2897867&cids=1&menu_id=4720&action=4043&model=project.task&view_type=form) which is associated with the commit **479a38466751e7bc31b26e5635b9735b37dde81f** and [PR](https://github.com/odoo/odoo/pull/96548)
    
    opw-3552786
    
    X-original-commit: b0ea838364f63d25ca79211d8be5cbf332b6bf70

--- addons/purchase/models/product.py
+++ addons/purchase/models/product.py
@@ -15,7 +15,7 @@ class ProductTemplate(models.Model):
     purchase_method = fields.Selection([
         ('purchase', 'On ordered quantities'),
         ('receive', 'On received quantities'),
-    ], string="Control Policy", compute='_compute_purchase_method', store=True, readonly=False,
+    ], string="Control Policy", compute='_compute_purchase_method', default='receive', store=True, readonly=False,
         help="On ordered quantities: Control bills based on ordered quantities.\n"
             "On received quantities: Control bills based on received quantities.")
     purchase_line_warn = fields.Selection(WARNING_MESSAGE, 'Purchase Order Line Warning', help=WARNING_HELP, required=True, default="no-message")
@@ -23,8 +23,12 @@ class ProductTemplate(models.Model):
 
     @api.depends('detailed_type')
     def _compute_purchase_method(self):
+        default_purchase_method = self.env['product.template'].default_get(['purchase_method']).get('purchase_method')
         for product in self:
-            product.purchase_method = 'purchase' if product.detailed_type == 'service' else 'receive'
+            if product.detailed_type == 'service':
+                product.purchase_method = 'purchase'
+            else:
+                product.purchase_method = default_purchase_method
 
     def _compute_purchased_product_qty(self):
         for template in self:
