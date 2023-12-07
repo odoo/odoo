d86cf6eff278f38bc43f985f1c60b1dec9a33a47
Hugo Carlier (Huca) <huca@odoo.com>
2023-12-07 16:15:09 +0100

    [FIX] hr, hr_skills: add TagsList component to avatar card in hr instead of planning
    Steps
    =====
    - Install module hr_skills (but not planning)
    - Open the avatar card of an employee with skills linked to it
    
    Issue
    =====
    A traceback appears indicating: "OwlError: Cannot find the definition of
    component "TagsList", missing static components key in parent".
    
    Cause
    =====
    The AvatarCardResourcePopover component was patched in planning to add
    the component TagsList to it. This component was also used by the patch
    applied on AvatarCardResourcePopover in hr_skills. However, when
    hr_skills is installed but not planning, the component TagsList is
    used (to display the skills of the employee) but no append to the
    AvatarCardResourcePopover (or the AvatarCardEmployeePopover that
    inherits from it).
    
    Fix
    ===
    The component TagsList is added in hr instead of planning as this module
    is required by both planning and hr_skills.

--- addons/hr/static/src/components/avatar_card_resource/avatar_card_resource_popover_patch.js
+++ addons/hr/static/src/components/avatar_card_resource/avatar_card_resource_popover_patch.js
@@ -3,6 +3,7 @@
 import { patch } from "@web/core/utils/patch";
 import { AvatarCardResourcePopover } from "@resource_mail/components/avatar_card_resource/avatar_card_resource_popover";
 import { useService } from "@web/core/utils/hooks";
+import { TagsList } from "@web/core/tags_list/tags_list";
 
 const patchAvatarCardResourcePopover = {
     setup() {
@@ -31,3 +32,11 @@ const patchAvatarCardResourcePopover = {
 };
 
 patch(AvatarCardResourcePopover.prototype, patchAvatarCardResourcePopover);
+// Adding TagsList component allows display tag lists on the resource/employee avatar card
+// This is used by multiple modules depending on hr (planning for roles and hr_skills for skills)
+patch(AvatarCardResourcePopover, {
+    components: {
+        ...AvatarCardResourcePopover.components,
+        TagsList,
+    },
+});
