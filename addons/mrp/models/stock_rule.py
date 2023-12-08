e85f628eeaffe8bd2c1132644ae94e1789c90472
Akim Juillerat <akim.juillerat@camptocamp.com>
2023-12-08 19:34:03 +0100

    fixup! [FIX] mrp: Remove unrelated variant description from kit component procurement

--- addons/mrp/models/stock_rule.py
+++ addons/mrp/models/stock_rule.py
@@ -218,7 +218,7 @@ class ProcurementGroup(models.Model):
                     # Remove from product_description_variants in procurement values
                     # any description that does not apply to the actual component
                     if values.get("product_description_variants"):
-                        # FIXME: Consider lang
+                        lang = self._get_product_description_variants_lang(values)
                         kit_product_description_variants_list = values.get("product_description_variants").lstrip("\n").split("\n")
                         valid_product_ptavs = bom_line.product_id.product_tmpl_id.valid_product_template_attribute_line_ids.product_template_value_ids
                         product_description_variant_list = [
@@ -226,8 +226,8 @@ class ProcurementGroup(models.Model):
                             for kpdvl in kit_product_description_variants_list
                             if kpdvl.split(":")[0] in [
                                 vpptavs.split(":")[0]
-                                for vpptavs in valid_product_ptavs.mapped("display_name")
-                                ]
+                                for vpptavs in valid_product_ptavs.with_context(lang=lang).mapped("display_name")
+                            ]
                         ]
                         values["product_description_variants"] = "\n\n%s" % "\n".join(product_description_variant_list)
                     component_qty, procurement_uom = bom_line_uom._adjust_uom_quantities(bom_line_data['qty'], quant_uom)
@@ -238,6 +238,9 @@ class ProcurementGroup(models.Model):
             else:
                 procurements_without_kit.append(procurement)
         return super(ProcurementGroup, self).run(procurements_without_kit, raise_user_error=raise_user_error)
+
+    def _get_product_description_variants_lang(self, values):
+        return self.env["res.partner"].browse(values.get("partner_id")).lang or "en_US"
 
     def _get_moves_to_assign_domain(self, company_id):
         domain = super(ProcurementGroup, self)._get_moves_to_assign_domain(company_id)
