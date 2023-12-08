f12adbaf73699374eda07aedc01eadcf872f586d
Valeriya(vchu) <vchu@odoo.com>
2023-12-08 15:00:39 +0100

    [IMP] website_sale: simplify product ribbons
    To ease a user experience a product.ribbon model and its
    configuration were simplified. HTML customisation of ribbons was
    removed and replaced with a position selection, color pickers and
    a name as a text field type.
    
    task-3392068
    
    closes odoo/odoo#145562
    
    Related: odoo/upgrade#5843
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- addons/website_sale/models/product_ribbon.py
+++ addons/website_sale/models/product_ribbon.py
@@ -1,34 +1,21 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, tools
+from odoo import fields, models
 
 
 class ProductRibbon(models.Model):
     _name = 'product.ribbon'
     _description = "Product ribbon"
 
-    html = fields.Html(string="Ribbon html", required=True, translate=True, sanitize=False)
-    bg_color = fields.Char(string="Ribbon background color", required=False)
-    text_color = fields.Char(string="Ribbon text color")
-    html_class = fields.Char(string="Ribbon class", required=True, default='')
+    name = fields.Char(string="Ribbon Name", required=True, translate=True, size=20)
+    bg_color = fields.Char(string="Background Color", required=True, default='#000000')
+    text_color = fields.Char(string="Text Color", required=True, default='#FFFFFF')
+    position = fields.Selection(
+        string='Position',
+        selection=[('left', "Left"), ('right', "Right")],
+        required=True,
+        default='left',
+    )
 
-    #=== COMPUTE METHODS ===#
-
-    @api.depends('html')
-    def _compute_display_name(self):
-        for ribbon in self:
-            ribbon.display_name = f'{tools.html2plaintext(ribbon.html)} (#{ribbon.id})'
-
-    #=== CRUD METHODS ===#
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            if 'bg_color' in vals and not '!important' in vals['bg_color']:
-                vals['bg_color'] += ' !important'
-        return super().create(vals_list)
-
-    def write(self, data):
-        if 'bg_color' in data and not '!important' in data['bg_color']:
-            data['bg_color'] += ' !important'
-        return super().write(data)
+    def _get_position_class(self):
+        return 'o_ribbon_left' if self.position == 'left' else 'o_ribbon_right'
