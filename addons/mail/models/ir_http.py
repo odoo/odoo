67abfdca9ed32c7c348541dcb8e47baf01993808
damr <damr@odoo.com>
2023-12-08 15:26:00 +0000

    [FIX] mail: add context to the init_messaging function
    This commit is a follow up of its enterprise part.
    We add the user_context to the the params of the function to have access
    to the allowed_companies of the current user later on in the process.
    
    affected version: 15.0- master
    task - 361682
    https://www.odoo.com/web#id=3631682&menu_id=4720&cids=1&action=333&active_id=4105&model=project.task&view_type=form
    
    X-original-commit: aa1051b5eb021ec9cdda258647cfc9446581fc67

--- addons/mail/models/ir_http.py
+++ addons/mail/models/ir_http.py
@@ -13,7 +13,14 @@ class IrHttp(models.AbstractModel):
         """Override to add the current user data (partner or guest) if applicable."""
         result = super().session_info()
         store = Store()
-        self.env["res.users"]._init_store_data(store)
+        ResUsers = self.env["res.users"]
+        if cids := request.httprequest.cookies.get("cids", False):
+            allowed_company_ids = []
+            for company_id in [int(cid) for cid in cids.split("-")]:
+                if company_id in self.env.user.company_ids.ids:
+                    allowed_company_ids.append(company_id)
+            ResUsers = self.with_context(allowed_company_ids=allowed_company_ids).env["res.users"]
+        ResUsers._init_store_data(store)
         result["storeData"] = store.get_result()
         guest = self.env['mail.guest']._get_guest_from_context()
         if not request.session.uid and guest:
