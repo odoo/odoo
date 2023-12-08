513b77a6411459968617f7a9a64997dd640b0aec
abd-msyukyu-odoo <abd@odoo.com>
2023-12-08 14:49:17 +0100

    [FIX] survey: prevent soft lock with conditional questions
    If multiple questions are conditionally displayed if the user selects an answer
    in another multi-choice question, that answer was added multiple times in the
    list used to filter mandatory questions that have to be answered when displayed.
    This is an issue because the answer was only removed once if the user changes
    his choice resulting in a state where a mandatory question that is hidden stays
    mandatory even though it is not displayed to the user.
    
    How to reproduce:
    - create a new survey with:
          - Question1:
            - multi-choice with one answer
            - 2 answers (A, B)
          - Question2:
            - single line text box
            - mandatory answer
            - conditional display depending on answer B of Question1
          - Question3:
            - single line text box
            - conditional display depending on answer B of Question1
    - start the survey
    - click on answer B then click on answer A
    
    Current behavior:
    - user is not able to submit the survey with answer A selected
    
    Expected behavior:
    - user should be able to submit the survey
    
    task-3630079
    
    Co-authored-by: Salvo Rapisarda <sdrapisarda@gmail.com>
    Co-authored-by: Damien Abeloos <abd@odoo.com>

--- addons/survey/static/tests/tours/survey_chained_conditional_questions.js
+++ addons/survey/static/tests/tours/survey_chained_conditional_questions.js
@@ -11,24 +11,45 @@ tour.register('test_survey_chained_conditional_questions', {
         content: 'Click on Start',
         trigger: 'button.btn:contains("Start")',
     }, {
+        content: 'Answer Q1 with Answer 2',
+        trigger: 'div.js_question-wrapper:contains("Q1") label:contains("Answer 2")',
+    }, {
+        content: 'Check that Q4 and Q5 are visible',
+        trigger: 'div.js_question-wrapper:contains(Q4)',
+        extra_trigger: 'div.js_question-wrapper:contains(Q5)',
+        run: () => {
+            const selector = 'div.js_question-wrapper.d-none';
+            if (document.querySelectorAll(selector).length !== 2) {
+                throw new Error('Q2 and Q3 should be hidden.');
+            }
+        },
+    }, {
         content: 'Answer Q1 with Answer 1',
         trigger: 'div.js_question-wrapper:contains("Q1") label:contains("Answer 1")',
     }, {
         content: 'Answer Q2 with Answer 1',
         trigger: 'div.js_question-wrapper:contains("Q2") label:contains("Answer 1")',
+        run: function (actions) {
+            const selector = 'div.js_question-wrapper.d-none';
+            if (document.querySelectorAll(selector).length !== 3) {
+                throw new Error('Q3, Q4 and Q5 should be hidden.');
+            }
+            // Select Answer 1, in order to trigger the display of Q3.
+            actions.click(this.$anchor);
+        }
     }, {
         content: 'Answer Q3 with Answer 1',
         trigger: 'div.js_question-wrapper:contains("Q3") label:contains("Answer 1")',
     }, {
-        content: 'Answer Q1 with Answer 2',  // This should hide all remaining questions.
-        trigger: 'div.js_question-wrapper:contains("Q1") label:contains("Answer 2")',
+        content: 'Answer Q1 with Answer 3',  // This should hide all remaining questions.
+        trigger: 'div.js_question-wrapper:contains("Q1") label:contains("Answer 3")',
     }, {
         content: 'Check that only question 1 is now visible',
         trigger: 'div.js_question-wrapper:contains("Q1")',
         run: () => {
             const selector = 'div.js_question-wrapper.d-none';
-            if (document.querySelectorAll(selector).length !== 2) {
-                throw new Error('Q2 and Q3 should have been hidden.');
+            if (document.querySelectorAll(selector).length !== 4) {
+                throw new Error('Q2, Q3, Q4 and Q5 should have been hidden.');
             }
         }
     }, {
