84a9c712983a78ba310ddab05cb4bc00701bdfa5
Theo VINCENT (thvi) <thvi@odoo.com>
2023-12-08 16:19:45 +0000

    [FIX] pos_online_payment: full flow test
     - Replaces a randomly failing test by a simpler test.
    As reported by XDO (https://github.com/odoo/odoo/pull/123237#discussion_r1410620206), executing web requests asynchronously during the execution of a tour which does an RPC is randomly failing.
    The fix consists in not testing the full online payment flow with the cashier frontend interface, but instead simulate an order saved on the server and simulate real customer web requests for paying online for that order.
    
     - Simplifies the way payment portal URLs are build.
    
    closes odoo/odoo#145595
    
    Task-id: 3632217
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    X-original-commit: 147910449555f6a28e3b77867ececa967f72ac80

--- addons/pos_online_payment/static/tests/tours/helpers/OnlinePaymentPopupTourMethods.js
+++ addons/pos_online_payment/static/tests/tours/helpers/OnlinePaymentPopupTourMethods.js
@@ -1,7 +1,5 @@
 /** @odoo-module */
 
-import * as Dialog from "@point_of_sale/../tests/tours/helpers/DialogTourMethods";
-
 export function amountIs(amount) {
     return {
         content: `displayed amount is ${amount}`,
@@ -9,44 +7,4 @@ export function amountIs(amount) {
         isCheck: true,
         is_modal: true,
     };
-}
-
-/**
- * Used to replace the POS bus web socket communication that doesn't seem to work when executing a test tour.
- * The server is regularly checked to see if the fake online payment has been done.
- *
- * @param {integer} checksAmount
- * @param {integer} delayBetweenChecks
- * @returns
- */
-export function waitForOnlinePayment(checksAmount = 10, delayBetweenChecks = 3000) {
-    return [
-        {
-            ...Dialog.is(),
-            run: () => {
-                let checkIndex = 0;
-                const checkFunc = async () => {
-                    const currentOrder = odoo.__WOWL_DEBUG__.root.env.services.pos.get_order();
-                    let opData;
-                    if (currentOrder) {
-                        opData = await currentOrder.update_online_payments_data_with_server(
-                            odoo.__WOWL_DEBUG__.root.env.services.orm,
-                            false
-                        );
-                    }
-                    const isMaxChecksReached = checkIndex >= checksAmount - 1;
-                    const isOrderPaid = opData && opData.is_paid;
-                    if (!isOrderPaid && !isMaxChecksReached) {
-                        checkIndex++;
-                        setTimeout(checkFunc, delayBetweenChecks);
-                    }
-                };
-                setTimeout(checkFunc, delayBetweenChecks);
-            },
-        },
-        {
-            ...Dialog.isNot(),
-            timeout: checksAmount * delayBetweenChecks + 3000,
-        },
-    ];
 }
