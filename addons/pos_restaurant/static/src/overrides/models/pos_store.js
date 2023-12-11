odoo/odoo

commit bea540ad850f6f208eca49f434399d8e8cef1ace

[REF] pos*: use dialog service
pos*: l10n_es_pos, l10n_fr_pos_cert, point_of_sale, pos_adyen, pos_discount, pos_hr,
pos_loyalty, pos_mercury, pos_online_payment, pos_paytm, pos_restaurant, pos_sale,
pos_six, pos_stripe

In this commit we replace the `popup_service` from `pos`
with the `dialog_service` from `web`. There is no need to
have a separate service for the same functionality.
This leads to less code duplication and a more consistent
user experience.

closes odoo/odoo#136845

Task: 3524758
Related: odoo/enterprise#48180
Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

addons/pos_restaurant/static/src/overrides/models/pos_store.js
@@ -49,8 +49,7 @@ patch(PosStore.prototype, {
         }
     },
     async actionAfterIdle() {
-        const isPopupClosed = this.popup.closePopupsButError();
-        if (isPopupClosed) {
+        if (document.querySelector(".modal-open")) {
             this.closeTempScreen();
             const table = this.table;
             const order = this.get_order();

addons/point_of_sale/static/src/app/store/pos_store.js
@@ -7,12 +7,11 @@ import { PosDB } from "@point_of_sale/app/store/db";
 import { markRaw, reactive } from "@odoo/owl";
 import { roundPrecision as round_pr, floatIsZero } from "@web/core/utils/numbers";
 import { registry } from "@web/core/registry";
-import { ConfirmPopup } from "@point_of_sale/app/utils/confirm_popup/confirm_popup";
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { deduceUrl } from "@point_of_sale/utils";
 import { Reactive } from "@web/core/utils/reactive";
 import { HWPrinter } from "@point_of_sale/app/printer/hw_printer";
 import { memoize } from "@web/core/utils/functions";
-import { ErrorPopup } from "@point_of_sale/app/errors/popups/error_popup";
 import { ConnectionLostError } from "@web/core/network/rpc_service";
 import { _t } from "@web/core/l10n/translation";
 import { CashOpeningPopup } from "@point_of_sale/app/store/cash_opening_popup/cash_opening_popup";
@@ -21,7 +20,8 @@ import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product
 import { renderToString } from "@web/core/utils/render";
 import { batched } from "@web/core/utils/timing";
 import { TicketScreen } from "@point_of_sale/app/screens/ticket_screen/ticket_screen";
-import { EditListPopup } from "./select_lot_popup/select_lot_popup";
+import { EditListPopup } from "@point_of_sale/app/store/select_lot_popup/select_lot_popup";
+import { makeAwaitable, ask } from "@point_of_sale/app/store/make_awaitable_dialog";
 
 /* Returns an array containing all elements of the given
  * array corresponding to the rule function {agg} and without duplicates
@@ -76,26 +76,26 @@ export class PosStore extends Reactive {
     tempScreen = null;
 
     static serviceDependencies = [
-        "popup",
         "orm",
         "number_buffer",
         "barcode_reader",
         "hardware_proxy",
         "ui",
+        "dialog",
     ];
     constructor() {
         super();
         this.ready = this.setup(...arguments).then(() => this);
     }
     // use setup instead of constructor because setup can be patched.
-    async setup(env, { popup, orm, number_buffer, hardware_proxy, barcode_reader, ui }) {
+    async setup(env, { popup, orm, number_buffer, hardware_proxy, barcode_reader, ui, dialog }) {
         this.env = env;
         this.orm = orm;
         this.popup = popup;
         this.numberBuffer = number_buffer;
         this.barcodeReader = barcode_reader;
         this.ui = ui;
-
+        this.dialog = dialog;
         this.db = new PosDB(); // a local database used to search trough products and categories & store pending orders
         this.unwatched = markRaw({});
         this.pushOrderMutex = new Mutex();
@@ -1666,7 +1666,7 @@ export class PosStore extends Reactive {
                 if (changes.cancelled.length > 0 || changes.new.length > 0) {
                     const isPrintSuccessful = await order.printChanges(cancelled);
                     if (!isPrintSuccessful) {
-                        this.popup.add(ErrorPopup, {
+                        this.dialog.add(AlertDialog, {
                             title: _t("Printing failed"),
                             body: _t("Failed in printing the changes in the order"),
                         });
@@ -1758,7 +1758,7 @@ export class PosStore extends Reactive {
                           "not close the session before the issue " +
                           "has been resolved."
                   );
-            const { confirmed } = await this.popup.add(ConfirmPopup, {
+            const confirmed = await ask(this.dialog, {
                 title: _t("Offline Orders"),
                 body: reason,
             });
@@ -1775,7 +1775,7 @@ export class PosStore extends Reactive {
         }
         const currentPartner = currentOrder.get_partner();
         if (currentPartner && currentOrder.getHasRefundLines()) {
-            this.popup.add(ErrorPopup, {
+            this.dialog.add(AlertDialog, {
                 title: _t("Can't change customer"),
                 body: _t(
                     "This order already has refund lines for %s. We can't change the customer associated to it. Create a new order for the new customer.",
@@ -1813,20 +1813,20 @@ export class PosStore extends Reactive {
     }
 
     async getEditedPackLotLines(isAllowOnlyOneLot, packLotLinesToEdit, productName) {
-        const { confirmed, payload } = await this.env.services.popup.add(EditListPopup, {
+        const payload = await makeAwaitable(this.dialog, EditListPopup, {
             title: _t("Lot/Serial Number(s) Required"),
             name: productName,
             isSingleItem: isAllowOnlyOneLot,
             array: packLotLinesToEdit,
         });
-        if (!confirmed) {
+        if (!payload) {
             return;
         }
         // Segregate the old and new packlot lines
         const modifiedPackLotLines = Object.fromEntries(
-            payload.newArray.filter((item) => item.id).map((item) => [item.id, item.text])
+            payload.filter((item) => item.id).map((item) => [item.id, item.text])
         );
-        const newPackLotLines = payload.newArray
+        const newPackLotLines = payload
             .filter((item) => !item.id)
             .map((item) => ({ lot_name: item.text }));
 
@@ -1849,7 +1849,17 @@ export class PosStore extends Reactive {
     }
     openCashControl() {
         if (this.shouldShowCashControl()) {
-            this.popup.add(CashOpeningPopup, { keepBehind: true });
+            this.dialog.add(
+                CashOpeningPopup,
+                {},
+                {
+                    onClose: () => {
+                        if (this.pos_session.state !== "opened") {
+                            this.closePos();
+                        }
+                    },
+                }
+            );
         }
     }
     shouldShowCashControl() {

addons/pos_loyalty/static/src/overrides/models/pos_store.js
@@ -4,10 +4,11 @@ import { patch } from "@web/core/utils/patch";
 import { PosStore } from "@point_of_sale/app/store/pos_store";
 import { _t } from "@web/core/l10n/translation";
 import { SelectionPopup } from "@point_of_sale/app/utils/input_popups/selection_popup";
-import { ErrorPopup } from "@point_of_sale/app/errors/popups/error_popup";
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { TextInputPopup } from "@point_of_sale/app/utils/input_popups/text_input_popup";
 import { Domain, InvalidDomainError } from "@web/core/domain";
 import { PosLoyaltyCard } from "@pos_loyalty/overrides/models/loyalty";
+import { makeAwaitable } from "@point_of_sale/app/store/make_awaitable_dialog";
 
 const COUPON_CACHE_MAX_SIZE = 4096; // Maximum coupon cache size, prevents long run memory issues and (to some extent) invalid data
 
@@ -18,7 +19,7 @@ patch(PosStore.prototype, {
         const linkedPrograms = linkedProgramIds.map((id) => this.program_by_id[id]);
         let selectedProgram = null;
         if (linkedPrograms.length > 1) {
-            const { confirmed, payload: program } = await this.popup.add(SelectionPopup, {
+            selectedProgram = await makeAwaitable(this.dialog, SelectionPopup, {
                 title: _t("Select program"),
                 list: linkedPrograms.map((program) => ({
                     id: program.id,
@@ -26,10 +27,7 @@ patch(PosStore.prototype, {
                     label: program.name,
                 })),
             });
-            if (confirmed) {
-                selectedProgram = program;
-            } else {
-                // Do nothing here if the selection is cancelled.
+            if (!selectedProgram) {
                 return;
             }
         } else if (linkedPrograms.length === 1) {
@@ -86,12 +84,12 @@ patch(PosStore.prototype, {
 
         // If gift card program setting is 'scan_use', ask for the code.
         if (this.config.gift_card_settings == "scan_use") {
-            const { confirmed, payload: code } = await this.env.services.popup.add(TextInputPopup, {
+            const code = await makeAwaitable(this.dialog, TextInputPopup, {
                 title: _t("Generate a Gift Card"),
                 startingValue: "",
                 placeholder: _t("Enter the gift card code"),
             });
-            if (!confirmed) {
+            if (!code) {
                 return false;
             }
             const trimmedCode = code.trim();
@@ -109,7 +107,7 @@ patch(PosStore.prototype, {
                 // There should be maximum one gift card for a given code.
                 const giftCard = fetchedGiftCard[0];
                 if (giftCard && giftCard.source_pos_order_id) {
-                    this.popup.add(ErrorPopup, {
+                    this.dialog.add(AlertDialog, {
                         title: _t("This gift card has already been sold"),
                         body: _t("You cannot sell a gift card that has already been sold."),
                     });
@@ -161,8 +159,10 @@ patch(PosStore.prototype, {
         const result = [];
         for (const couponProgram of allCouponPrograms) {
             const program = this.program_by_id[couponProgram.program_id];
-            if (program.pricelist_ids.length > 0
-                && (!order.pricelist || !program.pricelist_ids.includes(order.pricelist.id))) {
+            if (
+                program.pricelist_ids.length > 0 &&
+                (!order.pricelist || !program.pricelist_ids.includes(order.pricelist.id))
+            ) {
                 continue;
             }
 
@@ -207,7 +207,7 @@ patch(PosStore.prototype, {
             reward.all_discount_product_ids = new Set(reward.all_discount_product_ids);
         }
 
-        this.fieldTypes = loadedData['field_types'];
+        this.fieldTypes = loadedData["field_types"];
         await super._processData(loadedData);
         this.productId2ProgramIds = loadedData["product_id_to_program_ids"];
         this.programs = loadedData["loyalty.program"] || []; //TODO: rename to `loyaltyPrograms` etc
@@ -235,10 +235,10 @@ patch(PosStore.prototype, {
 
         try {
             products
-                .map(product => {
+                .map((product) => {
                     const modifiedProduct = { ...product };
-                    Object.keys(modifiedProduct).forEach(key => {
-                        if (this.fieldTypes['product.product'][key] === 'many2one') {
+                    Object.keys(modifiedProduct).forEach((key) => {
+                        if (this.fieldTypes["product.product"][key] === "many2one") {
                             modifiedProduct[key] = modifiedProduct[key][1];
                         }
                     });
@@ -252,7 +252,7 @@ patch(PosStore.prototype, {
             }
             const index = this.rewards.indexOf(reward);
             if (index != -1) {
-                this.env.services.popup.add(ErrorPopup, {
+                this.env.services.dialog.add(AlertDialog, {
                     title: _t("A reward could not be loaded"),
                     body: _t(
                         'The reward "%s" contain an error in its domain, your domain must be compatible with the PoS client',

commit 966b6bb9df22bfd5929ae307e8a919e716201021

[FIX] pos_restaurant: fix actionAfterIdle
The pos restaurant has a feature that takes the pos back to the
FloorScreen automatically after a certain period of inactivity.

Commit bea540a broke this functionality. In this commit we restore it with
one slight caveat. Before, the pos would close all opened dialogs, except for
the errors. Given that this closing mechanism is not available with the
`dialog_service` and that the way it was working before was quite fragile
anyways, we've decided to only allow the pos to perform this action if
no dialogs are opened.

closes odoo/odoo#141293

Related: odoo/enterprise#51561
Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

addons/pos_restaurant/static/src/overrides/models/pos_store.js
@@ -49,7 +49,7 @@ patch(PosStore.prototype, {
         }
     },
     async actionAfterIdle() {
-        if (document.querySelector(".modal-open")) {
+        if (!document.querySelector(".modal-open")) {
             const table = this.table;
             const order = this.get_order();
             if (order && order.get_screen_data().name === "ReceiptScreen") {