0042259794ab9c7edc561fa13eb2e0aa7e6c595c
roen-odoo <roen@odoo.com>
2023-12-11 14:25:59 +0000

    [FIX] pos_loyalty: prevent using expired eWallets
    Current behavior:
    It was possible to use an expired eWallet to pay for an order. It was
    also possible to put money on an expired eWallet.
    
    Steps to reproduce:
    - Create an eWallet with an expiration date in the past
    - Open PoS and add products to the order
    - Click on the eWallet button
    - You can still use the eWallet to pay for the order
    
    opw-3568270
    
    X-original-commit: a95caa65fc7f930764e3699bdd392fe8c55d2070

--- addons/pos_loyalty/static/tests/tours/EWalletProgramTours.js
+++ addons/pos_loyalty/static/tests/tours/EWalletProgramTours.js
@@ -104,3 +104,21 @@ registry.category("web_tour.tours").add("EWalletProgramTour2", {
 });
 
 //#endregion
+
+registry
+    .category("web_tour.tours")
+    .add('ExpiredEWalletProgramTour', {
+        test: true,
+        url: '/pos/ui',
+        steps: () => [
+            Dialog.confirm("Open session"),
+            ProductScreen.clickHomeCategory(),
+            ProductScreen.clickPartnerButton(),
+            ProductScreen.clickCustomer('AAAA'),
+            ProductScreen.addOrderline('Whiteboard Pen', '2', '6', '12.00'),
+            PosLoyalty.eWalletButtonState({ highlighted: false }),
+            PosLoyalty.clickEWalletButton(),
+            Dialog.is({ title: "No valid eWallet found" }),
+            Dialog.confirm(),
+        ].flat(),
+});
