b5299f4b92f0e3f1fc46c2cc9f119c1ca67ef03b
Xavier-Do <xdo@odoo.com>
2023-12-11 11:50:44 +0100

    [FIX] web: avoid useless cr in serve_db
    If request.db has a value, we went through serve_db and we already have
    an open connection on this database in teh request.environment.
    
    This commit removes the useless connection.
    
    Part-of: odoo/odoo#112000

--- addons/web/controllers/binary.py
+++ addons/web/controllers/binary.py
@@ -247,41 +247,41 @@ class Binary(http.Controller):
             response = http.Stream.from_path(file_path('web/static/img/logo.png')).get_response()
         else:
             try:
-                # create an empty registry
-                registry = odoo.modules.registry.Registry(dbname)
-                with registry.cursor() as cr:
-                    company = int(kw['company']) if kw and kw.get('company') else False
-                    if company:
-                        cr.execute("""SELECT logo_web, write_date
-                                        FROM res_company
-                                       WHERE id = %s
-                                   """, (company,))
-                    else:
-                        cr.execute("""SELECT c.logo_web, c.write_date
-                                        FROM res_users u
-                                   LEFT JOIN res_company c
-                                          ON c.id = u.company_id
-                                       WHERE u.id = %s
-                                   """, (uid,))
-                    row = cr.fetchone()
-                    if row and row[0]:
-                        image_base64 = base64.b64decode(row[0])
-                        image_data = io.BytesIO(image_base64)
-                        mimetype = guess_mimetype(image_base64, default='image/png')
-                        imgext = '.' + mimetype.split('/')[1]
-                        if imgext == '.svg+xml':
-                            imgext = '.svg'
-                        response = send_file(
-                            image_data,
-                            request.httprequest.environ,
-                            download_name=imgname + imgext,
-                            mimetype=mimetype,
-                            last_modified=row[1],
-                            response_class=Response,
-                        )
-                    else:
-                        response = http.Stream.from_path(file_path('web/static/img/nologo.png')).get_response()
+                company = int(kw['company']) if kw and kw.get('company') else False
+                if company:
+                    request.env.cr.execute("""
+                        SELECT logo_web, write_date
+                          FROM res_company
+                         WHERE id = %s
+                    """, (company,))
+                else:
+                    request.env.cr.execute("""
+                        SELECT c.logo_web, c.write_date
+                          FROM res_users u
+                     LEFT JOIN res_company c
+                            ON c.id = u.company_id
+                         WHERE u.id = %s
+                    """, (uid,))
+                row = request.env.cr.fetchone()
+                if row and row[0]:
+                    image_base64 = base64.b64decode(row[0])
+                    image_data = io.BytesIO(image_base64)
+                    mimetype = guess_mimetype(image_base64, default='image/png')
+                    imgext = '.' + mimetype.split('/')[1]
+                    if imgext == '.svg+xml':
+                        imgext = '.svg'
+                    response = send_file(
+                        image_data,
+                        request.httprequest.environ,
+                        download_name=imgname + imgext,
+                        mimetype=mimetype,
+                        last_modified=row[1],
+                        response_class=Response,
+                    )
+                else:
+                    response = http.Stream.from_path(file_path('web/static/img/nologo.png')).get_response()
             except Exception:
+                _logger.warning("While retrieving the company logo, using the Odoo logo instead", exc_info=True)
                 response = http.Stream.from_path(file_path(f'web/static/img/{imgname}{imgext}')).get_response()
 
         return response
