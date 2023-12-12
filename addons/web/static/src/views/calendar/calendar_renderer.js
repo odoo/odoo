521ded6b2e1447ee17f0b3e4f447182ad8b707b1
Leonardo Pavan Rocha <lpr@odoo.com>
2023-12-12 11:04:56 +0100

    [IMP] web: prevent action swiper from being triggered by fullcalendar
    Currently in calendar views in mobile it is possible to create events selecting
    the time of the event by dragging. However, in calendar we also have the action
    swiper responsible for moving between weeks when swiping close to the borders.
    The issue is that when trying to create events by dragging close to the border
    will cause the action swiper to trigger a swipe, therefore switching to the
    next week. This commit fixes this by adding a swipeInvalid prop to the action
    swiper that will be checked during the touch move event handler. If the
    swipeInvalid is executed at any move and returns true, the swipe is cancelled.
    
    task-3635841

--- addons/web/static/src/views/calendar/calendar_renderer.js
+++ addons/web/static/src/views/calendar/calendar_renderer.js
@@ -32,6 +32,7 @@ export class CalendarRenderer extends Component {
             animationOnMove: false,
             animationType: "forwards",
             swipeDistanceRatio: 6,
+            swipeInvalid: () => Boolean(document.querySelector(".o_event.fc-mirror")),
         };
     }
 }
