d9d5db829e860b1bf0b1a512505c22ef4496c2cf
Pedram (PEBR) <pebr@odoo.com>
2023-12-12 14:38:23 +0000

    [FIX] point_of_sale: prevent opening Product popup without options
    This commit addresses the issue where the Product Configurator Popup
    was displayed unnecessarily when there was only one value for each
    attribute of a product. The implementation now includes a check to
    ensure that the popup is shown only when there are more that one
    choices available. This enhancement improves the user experience by
    avoiding the display of a redundant popup without viable options.
    
    owp-3621666
    
    closes odoo/odoo#151848
    
    X-original-commit: 1f7eab2938ef72c73a48a12a76ea89d5a09b9989
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/js/Screens/ProductScreen/ProductScreen.js
+++ addons/point_of_sale/static/src/js/Screens/ProductScreen/ProductScreen.js
@@ -68,15 +68,7 @@ class ProductScreen extends ControlButtonsMixin(PosComponent) {
         let draftPackLotLines, weight, description, packLotLinesToEdit;
 
         if (_.some(product.attribute_line_ids, (id) => id in this.env.pos.attributes_by_ptal_id)) {
-            const attributes = _.map(
-                product.attribute_line_ids,
-                (id) => this.env.pos.attributes_by_ptal_id[id]
-            ).filter((attr) => attr !== undefined);
-            const { confirmed, payload } = await this.showPopup("ProductConfiguratorPopup", {
-                product: product,
-                attributes: attributes,
-            });
-
+            let { confirmed, payload } = await this._openProductConfiguratorPopup(product);
             if (confirmed) {
                 description = payload.selected_attributes.join(", ");
                 price_extra += payload.price_extra;
@@ -161,6 +153,36 @@ class ProductScreen extends ControlButtonsMixin(PosComponent) {
             weight = this.env.pos.db.product_packaging_by_barcode[code.code].qty;
         }
         return { draftPackLotLines, quantity: weight, description, price_extra };
+    }
+    async _openProductConfiguratorPopup(product) {
+        const attributes = _.map(
+            product.attribute_line_ids,
+            (id) => this.env.pos.attributes_by_ptal_id[id]
+        ).filter((attr) => attr !== undefined);
+
+        // avoid opening the popup when each attribute has only one available option.
+        if (_.some(attributes, (attribute) => attribute.values.length > 1 || _.some(attribute.values, (value) => value.is_custom))) {
+            return await this.showPopup('ProductConfiguratorPopup', {
+                product: product,
+                attributes: attributes,
+            });
+        };
+
+        let selected_attributes = [];
+        let price_extra = 0.0;
+
+        attributes.forEach((attribute) => {
+            selected_attributes.push(attribute.values[0].name);
+            price_extra += attribute.values[0].price_extra;
+        });
+
+        return {
+            confirmed: true,
+            payload: {
+                selected_attributes,
+                price_extra,
+            }
+        };
     }
     async _addProduct(product, options) {
         this.currentOrder.add_product(product, options);
