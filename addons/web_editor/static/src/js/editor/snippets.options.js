b73db5816e4afc84f553cdb8a35053a079a2ed0d
Romain Derie <rde@odoo.com>
2023-12-12 18:02:35 +0100

    [FIX] web_editor, website: remove clone button on mega menu block
    Step to reproduce:
    - Create a mega menu
    - Enter edit mode and select it
    - You can duplicate the top level block (but not remove it)
    - If you duplicate it, you end up with a second top level block that you
      can't delete ever, even by deleting inner elements one by one.
    
    Technical details:
    1. The remove button of the mega menu is already hidden thanks to commit
       [1] which used the `forceNoDeleteButton` editor option introduced
       with commit [2].
    2. The table of content snippet also need to hide both the delete and
       clone button. It was done in an "non-ideal" way with commit [3].
    3. The delete button removal for table of content snippet was actually
       improved to use the `forceNoDeleteButton` option of commit [2].
    4. It's also commit [1] which prevent the deletion of the top level
       block when deleting inner elements one by one: when the last one is
       deleted, it regenerates the whole block.
    
    This commit thus simply makes it so `forceNoDeleteButton` also hides the
    clone button.
    A first solution was made by introducing a new `forceNoCloneButton`
    option, but there is good chances that you always want to either hide
    both or show both, so merging those options for now is the simplest
    solution.
    
    It also takes the opportunity to remove the (now) useless code in the
    table of content snippet.
    
    [1]: https://github.com/odoo/odoo/commit/97810a9c40396bb27cb5779937734849d185cf1f
    [2]: https://github.com/odoo/odoo/commit/7ef484377a493ebe558242480d0da6b542d6c247
    [3]: https://github.com/odoo/odoo/commit/9fb2dad97cfbd412bee3cb5d1358a9835e721f60#diff-ea32a091d6b1a47aeea680fa39bbc9111260cbdaf07e9f388a9d04741806ea8fR128-R129
    
    opw-3604033
    opw-3627319
    
    X-original-commit: 44b006778df913b847c58b3e405d2a177fcb1a7c

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -3075,7 +3075,8 @@ const SnippetOptionWidget = Widget.extend({
      */
     isTopFirstOption: false,
     /**
-     * Forces the target to not be possible to remove.
+     * Forces the target to not be possible to remove. It will also hide the
+     * clone button.
      *
      * @type {boolean}
      */
