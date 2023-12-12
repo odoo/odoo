825a8afa8718b296dd471b19e582198c013115ee
Kashyap Patel <kasp@odoo.com>
2023-12-12 11:07:47 +0530

    [ADD] l10n_in: HSN code length selection at setting level
    In this commit,
    - The taxpayers having Aggregate Annual Turnover (AATO) above Rs. 5 Crore shall
      use at least a 6-digit HSN code and other taxpayers shall use at least
      a 4-digit HSN code.
    - However, there was no functionality to select the length of the HSN code.
    - So added settings to select the length of the HSN code at the setting level.
      Users can now define the desired length for HSN codes.
    
    task_id: 3527378

--- addons/l10n_in/models/product_template.py
+++ addons/l10n_in/models/product_template.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
@@ -9,3 +10,13 @@ class ProductTemplate(models.Model):
 
     l10n_in_hsn_code = fields.Char(string="HSN/SAC Code", help="Harmonized System Nomenclature/Services Accounting Code")
     l10n_in_hsn_description = fields.Char(string="HSN/SAC Description", help="HSN/SAC description is required if HSN/SAC code is not provided.")
+
+    @api.constrains('l10n_in_hsn_code')
+    def _check_hsn_code_validation(self):
+        for record in self:
+            company = record.company_id or self.env.company
+            minimum_hsn_len = company.l10n_in_hsn_code_digit
+            check_hsn = record.l10n_in_hsn_code and minimum_hsn_len
+            if check_hsn and len(record.l10n_in_hsn_code) < int(minimum_hsn_len):
+                error_message = _("As per your HSN/SAC code validation, minimum %s digits HSN/SAC code is required.", minimum_hsn_len)
+                raise ValidationError(error_message)
