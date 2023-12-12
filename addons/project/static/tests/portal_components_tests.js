odoo/odoo

commit 82b4395b2f12e1c86d2bd5c4acd47c321be071d8

[FIX] web: prevent upload of large files
(NB: this is a backport of commit 2a72028)

This commit ensures that files uploaded using the FileInput, upload
service or AttachDocumentWidget will not exceed the maximum file size of
the context in size before sending them to the server. It also moves
some utils from the formatters file to the numbers file so that one
can use them without requiring the view assets.

task-3390746

addons/web/static/src/core/file_input/file_input.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 import { useService } from "@web/core/utils/hooks";
+import { checkFileSize } from "@web/core/utils/files";
 
 import { Component, onMounted, useRef } from "@odoo/owl";
 
@@ -50,6 +51,19 @@ export class FileInput extends Component {
     }
 
     async uploadFiles(params) {
+        if (params.ufile.length) {
+            const fileSize = params.ufile[0].size;
+            if (!checkFileSize(fileSize, this.env.services.notification)) {
+                // FIXME
+                // Note that the notification service is not added as a
+                // dependency of this component (through useService hook),
+                // in order to avoid introducing a breaking change in a stable version.
+                // If the notification service is not available, the
+                // checkFileSize function will not display any notification
+                // but will still return the correct value.
+                return null;
+            }
+        }
         const fileData = await this.http.post(this.props.route, params, "text");
         const parsedFileData = JSON.parse(fileData);
         if (parsedFileData.error) {
@@ -71,7 +85,9 @@ export class FileInput extends Component {
      */
     async onFileInputChange() {
         const parsedFileData = await this.uploadFiles(this.httpParams);
-        this.props.onUpload(parsedFileData);
+        if (parsedFileData) {
+            this.props.onUpload(parsedFileData);
+        }
     }
 
     /**

addons/web/static/src/core/utils/files.js
@@ -1,0 +1,33 @@
+/** @odoo-module **/
+
+import { humanNumber } from "@web/core/utils/numbers";
+import { session } from "@web/session";
+import { _t } from "@web/core/l10n/translation";
+import { sprintf } from "./strings";
+
+const DEFAULT_MAX_FILE_SIZE = 128 * 1024 * 1024;
+
+/**
+ * @param {number} fileSize
+ * @param {Services["notification"]} [notificationService]
+ * @returns {boolean}
+ */
+export function checkFileSize(fileSize, notificationService) {
+    const maxUploadSize = session.max_file_upload_size || DEFAULT_MAX_FILE_SIZE;
+    if (fileSize > maxUploadSize) {
+        if (notificationService) {
+            notificationService.add(
+                sprintf(
+                    _t("The selected file (%sB) is over the maximum allowed file size (%sB)."),
+                    humanNumber(fileSize),
+                    humanNumber(maxUploadSize)
+                ),
+                {
+                    type: "danger",
+                }
+            );
+        }
+        return false;
+    }
+    return true;
+}

addons/web/static/src/core/utils/numbers.js
@@ -1,5 +1,9 @@
 /** @odoo-module **/
 
+import { localization as l10n } from "@web/core/l10n/localization";
+import { _t } from "@web/core/l10n/translation";
+import { intersperse } from "@web/core/utils/strings";
+
 /**
  * @param {Number} value
  * @param {Number} comparisonValue
@@ -31,4 +35,76 @@ export function computeVariation(value, comparisonValue) {
  */
 export function clamp(num, min, max) {
     return Math.max(Math.min(num, max), min);
+}
+
+/**
+ * Inserts "thousands" separators in the provided number.
+ *
+ * @param {string} string representing integer number
+ * @param {string} [thousandsSep=","] the separator to insert
+ * @param {number[]} [grouping=[]]
+ *   array of relative offsets at which to insert `thousandsSep`.
+ *   See `strings.intersperse` method.
+ * @returns {string}
+ */
+export function insertThousandsSep(number, thousandsSep = ",", grouping = []) {
+    const negative = number[0] === "-";
+    number = negative ? number.slice(1) : number;
+    return (negative ? "-" : "") + intersperse(number, grouping, thousandsSep);
+}
+
+/**
+ * Format a number to a human readable format. For example, 3000 could become 3k.
+ * Or massive number can use the scientific exponential notation.
+ *
+ * @param {number} number to format
+ * @param {Object} [options] Options to format
+ * @param {number} [options.decimals=0] number of decimals to use
+ *    if minDigits > 1 is used and effective on the number then decimals
+ *    will be shrunk to zero, to avoid displaying irrelevant figures ( 0.01 compared to 1000 )
+ * @param {number} [options.minDigits=1]
+ *    the minimum number of digits to preserve when switching to another
+ *    level of thousands (e.g. with a value of '2', 4321 will still be
+ *    represented as 4321 otherwise it will be down to one digit (4k))
+ * @returns {string}
+ */
+export function humanNumber(number, options = { decimals: 0, minDigits: 1 }) {
+    const decimals = options.decimals || 0;
+    const minDigits = options.minDigits || 1;
+    const d2 = Math.pow(10, decimals);
+    const numberMagnitude = +number.toExponential().split("e+")[1];
+    number = Math.round(number * d2) / d2;
+    // the case numberMagnitude >= 21 corresponds to a number
+    // better expressed in the scientific format.
+    if (numberMagnitude >= 21) {
+        // we do not use number.toExponential(decimals) because we want to
+        // avoid the possible useless O decimals: 1e.+24 preferred to 1.0e+24
+        number = Math.round(number * Math.pow(10, decimals - numberMagnitude)) / d2;
+        return `${number}e+${numberMagnitude}`;
+    }
+    // note: we need to call toString here to make sure we manipulate the resulting
+    // string, not an object with a toString method.
+    const unitSymbols = _t("kMGTPE").toString();
+    const sign = Math.sign(number);
+    number = Math.abs(number);
+    let symbol = "";
+    for (let i = unitSymbols.length; i > 0; i--) {
+        const s = Math.pow(10, i * 3);
+        if (s <= number / Math.pow(10, minDigits - 1)) {
+            number = Math.round((number * d2) / s) / d2;
+            symbol = unitSymbols[i - 1];
+            break;
+        }
+    }
+    const { decimalPoint, grouping, thousandsSep } = l10n;
+
+    // determine if we should keep the decimals (we don't want to display 1,020.02k for 1020020)
+    const decimalsToKeep = number >= 1000 ? 0 : decimals;
+    number = sign * number;
+    const [integerPart, decimalPart] = number.toFixed(decimalsToKeep).split(".");
+    const int = insertThousandsSep(integerPart, thousandsSep, grouping);
+    if (!decimalPart) {
+        return int + symbol;
+    }
+    return int + decimalPoint + decimalPart + symbol;
 }

addons/web/static/src/views/fields/file_handler.js
@@ -1,14 +1,10 @@
 /** @odoo-module **/
 
 import { useService } from "@web/core/utils/hooks";
-import { sprintf } from "@web/core/utils/strings";
+import { checkFileSize } from "@web/core/utils/files";
 import { getDataURLFromFile } from "@web/core/utils/urls";
-import { session } from "@web/session";
-import { formatFloat } from "./formatters";
 
 import { Component, useRef, useState } from "@odoo/owl";
-
-const DEFAULT_MAX_FILE_SIZE = 128 * 1024 * 1024;
 
 export class FileUploader extends Component {
     setup() {
@@ -19,9 +15,6 @@ export class FileUploader extends Component {
         });
     }
 
-    get maxUploadSize() {
-        return session.max_file_upload_size || DEFAULT_MAX_FILE_SIZE;
-    }
     /**
      * @param {Event} ev
      */
@@ -30,17 +23,8 @@ export class FileUploader extends Component {
             return;
         }
         for (const file of ev.target.files) {
-            if (file.size > this.maxUploadSize) {
-                this.notification.add(
-                    sprintf(
-                        this.env._t("The selected file exceed the maximum file size of %s."),
-                        formatFloat(this.maxUploadSize, { humanReadable: true })
-                    ),
-                    {
-                        title: this.env._t("File upload"),
-                        type: "danger",
-                    }
-                );
+            if (!checkFileSize(file.size, this.notification)) {
+                return null;
             }
             this.state.isUploading = true;
             const data = await getDataURLFromFile(file);

addons/web/static/src/views/fields/formatters.js
@@ -4,89 +4,16 @@ import { formatDate, formatDateTime } from "@web/core/l10n/dates";
 import { localization as l10n } from "@web/core/l10n/localization";
 import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
-import { escape, intersperse, nbsp, sprintf } from "@web/core/utils/strings";
+import { escape, nbsp, sprintf } from "@web/core/utils/strings";
 import { isBinarySize } from "@web/core/utils/binary";
 import { session } from "@web/session";
+import { humanNumber, insertThousandsSep } from "@web/core/utils/numbers";
 
 import { markup } from "@odoo/owl";
 
 // -----------------------------------------------------------------------------
 // Helpers
 // -----------------------------------------------------------------------------
-
-/**
- * Inserts "thousands" separators in the provided number.
- *
- * @private
- * @param {string} string representing integer number
- * @param {string} [thousandsSep=","] the separator to insert
- * @param {number[]} [grouping=[]]
- *   array of relative offsets at which to insert `thousandsSep`.
- *   See `strings.intersperse` method.
- * @returns {string}
- */
-function insertThousandsSep(number, thousandsSep = ",", grouping = []) {
-    const negative = number[0] === "-";
-    number = negative ? number.slice(1) : number;
-    return (negative ? "-" : "") + intersperse(number, grouping, thousandsSep);
-}
-
-/**
- * Format a number to a human readable format. For example, 3000 could become 3k.
- * Or massive number can use the scientific exponential notation.
- *
- * @private
- * @param {number} number to format
- * @param {Object} [options] Options to format
- * @param {number} [options.decimals=0] number of decimals to use
- *    if minDigits > 1 is used and effective on the number then decimals
- *    will be shrunk to zero, to avoid displaying irrelevant figures ( 0.01 compared to 1000 )
- * @param {number} [options.minDigits=1]
- *    the minimum number of digits to preserve when switching to another
- *    level of thousands (e.g. with a value of '2', 4321 will still be
- *    represented as 4321 otherwise it will be down to one digit (4k))
- * @returns {string}
- */
-function humanNumber(number, options = { decimals: 0, minDigits: 1 }) {
-    const decimals = options.decimals || 0;
-    const minDigits = options.minDigits || 1;
-    const d2 = Math.pow(10, decimals);
-    const numberMagnitude = +number.toExponential().split("e+")[1];
-    number = Math.round(number * d2) / d2;
-    // the case numberMagnitude >= 21 corresponds to a number
-    // better expressed in the scientific format.
-    if (numberMagnitude >= 21) {
-        // we do not use number.toExponential(decimals) because we want to
-        // avoid the possible useless O decimals: 1e.+24 preferred to 1.0e+24
-        number = Math.round(number * Math.pow(10, decimals - numberMagnitude)) / d2;
-        return `${number}e+${numberMagnitude}`;
-    }
-    // note: we need to call toString here to make sure we manipulate the resulting
-    // string, not an object with a toString method.
-    const unitSymbols = _t("kMGTPE").toString();
-    const sign = Math.sign(number);
-    number = Math.abs(number);
-    let symbol = "";
-    for (let i = unitSymbols.length; i > 0; i--) {
-        const s = Math.pow(10, i * 3);
-        if (s <= number / Math.pow(10, minDigits - 1)) {
-            number = Math.round((number * d2) / s) / d2;
-            symbol = unitSymbols[i - 1];
-            break;
-        }
-    }
-    const { decimalPoint, grouping, thousandsSep } = l10n;
-
-    // determine if we should keep the decimals (we don't want to display 1,020.02k for 1020020)
-    const decimalsToKeep = number >= 1000 ? 0 : decimals;
-    number = sign * number;
-    const [integerPart, decimalPart] = number.toFixed(decimalsToKeep).split(".");
-    const int = insertThousandsSep(integerPart, thousandsSep, grouping);
-    if (!decimalPart) {
-        return int + symbol;
-    }
-    return int + decimalPoint + decimalPart + symbol;
-}
 
 function humanSize(value) {
     if (!value) {

addons/web/static/src/views/widgets/attach_document/attach_document.js
@@ -3,6 +3,7 @@
 import { FileInput } from "@web/core/file_input/file_input";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
+import { checkFileSize } from "@web/core/utils/files";
 import { standardWidgetProps } from "@web/views/widgets/standard_widget_props";
 
 import { Component } from "@odoo/owl";
@@ -20,11 +21,17 @@ export class AttachDocumentWidget extends Component {
     }
 
     async onInputChange() {
+        const ufile = [...this.fileInput.files];
+        for (const file of ufile) {
+            if (!checkFileSize(file.size, this.notification)) {
+                return null;
+            }
+        }
         const fileData = await this.http.post(
             "/web/binary/upload_attachment",
             {
                 csrf_token: odoo.csrf_token,
-                ufile: [...this.fileInput.files],
+                ufile: ufile,
                 model: this.props.record.resModel,
                 id: this.props.record.resId,
             },

addons/web/static/tests/core/file_input_tests.js
@@ -1,9 +1,17 @@
 /** @odoo-module **/
 
 import { makeTestEnv } from "@web/../tests/helpers/mock_env";
-import { getFixture, mount, patchWithCleanup, triggerEvent } from "@web/../tests/helpers/utils";
+import {
+    editInput,
+    getFixture,
+    mount,
+    patchWithCleanup,
+    triggerEvent,
+} from "@web/../tests/helpers/utils";
 import { FileInput } from "@web/core/file_input/file_input";
 import { registry } from "@web/core/registry";
+import { session } from "@web/session";
+import { makeFakeLocalizationService } from "@web/../tests/helpers/mock_services";
 
 const serviceRegistry = registry.category("services");
 
@@ -13,7 +21,12 @@ let target;
 // Helpers
 // -----------------------------------------------------------------------------
 
-async function createFileInput({ mockPost, props }) {
+async function createFileInput({ mockPost, mockAdd, props }) {
+    serviceRegistry.add("notification", {
+        start: () => ({
+            add: mockAdd || (() => {}),
+        }),
+    });
     serviceRegistry.add("http", {
         start: () => ({
             post: mockPost || (() => {}),
@@ -115,5 +128,36 @@ QUnit.module("Components", ({ beforeEach }) => {
         });
 
         assert.isNotVisible(target.querySelector(".o_file_input"));
+    });
+
+    QUnit.test("uploading a file that is too heavy will send a notification", async (assert) => {
+        serviceRegistry.add("localization", makeFakeLocalizationService());
+        patchWithCleanup(session, { max_file_upload_size: 2 });
+        await createFileInput({
+            props: {
+                onUpload(files) {
+                    // This code should be unreachable in this case
+                    assert.step(files[0].name);
+                },
+            },
+            mockPost: (route, params) => {
+                return JSON.stringify([{ name: params.ufile[0].name }]);
+            },
+            mockAdd: (message) => {
+                assert.step("notification");
+                // Message is a bit weird because values (2 and 4 bytes) are simplified to 2 decimals in regards to megabytes
+                assert.strictEqual(
+                    message,
+                    "The selected file (4B) is over the maximum allowed file size (2B)."
+                );
+            },
+        });
+
+        const file = new File(["test"], "fake_file.txt", { type: "text/plain" });
+        await editInput(target, ".o_file_input input", file);
+        assert.verifySteps(
+            ["notification"],
+            "Only the notification will be triggered and the file won't be uploaded."
+        );
     });
 });

addons/web_editor/static/src/components/upload_progress_toast/upload_service.js
@@ -3,6 +3,8 @@
 import { registry } from '@web/core/registry';
 import { UploadProgressToast } from './upload_progress_toast';
 import { getDataURLFromFile } from 'web.utils';
+import { checkFileSize } from "@web/core/utils/files";
+import { humanNumber } from "@web/core/utils/numbers";
 
 import { reactive } from "@odoo/owl";
 
@@ -69,14 +71,20 @@ export const uploadService = {
                 const sortedFiles = Array.from(files).sort((a, b) => a.size - b.size);
                 for (const file of sortedFiles) {
                     let fileSize = file.size;
+                    if (!checkFileSize(fileSize, env.services.notification)) {
+                        // FIXME
+                        // Note that the notification service is not added as a
+                        // dependency of this service, in order to avoid introducing
+                        // a breaking change in a stable version.
+                        // If the notification service is not available, the
+                        // checkFileSize function will not display any notification
+                        // but will still return the correct value.
+                        return null;
+                    }
                     if (!fileSize) {
                         fileSize = null;
-                    } else if (fileSize < 1024) {
-                        fileSize = fileSize.toFixed(2) + " bytes";
-                    } else if (fileSize < 1048576) {
-                        fileSize = (fileSize / 1024).toFixed(2) + " KB";
                     } else {
-                        fileSize = (fileSize / 1048576).toFixed(2) + " MB";
+                        fileSize = humanNumber(fileSize) + "B";
                     }
 
                     const id = ++fileId;

commit d00ef39647adb145cc53529158612f4302bb8a0b

[FIX] web, project: avoid crash on file upload in portal
This commit fixes a crash that would happen on file upload in the project
sharing portal chatter. This crash was caused by the file size check in
the uploadFiles method of the FileInput component due to the fact that
the PortalFileInput calls this method once per file instead of once for
all files. The fix therefore ensures that the file size check will be
correctly applied in the case of a single file parameter instead of a
files array.

Steps to reproduce:
- Access the portal (with the /my route)
- Go to Projects
- Open the AGR - S00064 - Sales Order project
- Open any task in the kanban view
- Send an attachment in the chatter
- Before the fix, a traceback occurs on file upload

Crash introduced by https://github.com/odoo/odoo/pull/138203/commits/82b4395b2f12e1c86d2bd5c4acd47c321be071d8

opw-3628996

addons/project/__manifest__.py
@@ -83,6 +83,7 @@
             'project/static/src/js/portal_rating.js',
         ],
         'web.qunit_suite_tests': [
+            'project/static/src/project_sharing/components/portal_file_input/portal_file_input.js',
             'project/static/tests/**/*.js',
         ],
         'web.assets_tests': [

addons/web/static/src/core/file_input/file_input.js
@@ -51,8 +51,8 @@ export class FileInput extends Component {
     }
 
     async uploadFiles(params) {
-        if (params.ufile.length) {
-            const fileSize = params.ufile[0].size;
+        if ((params.ufile && params.ufile.length) || params.file) {
+            const fileSize = (params.ufile && params.ufile[0].size) || params.file.size;
             if (!checkFileSize(fileSize, this.env.services.notification)) {
                 // FIXME
                 // Note that the notification service is not added as a

addons/project/static/tests/portal_components_tests.js
@@ -1,0 +1,76 @@
+/** @odoo-module **/
+
+import { makeTestEnv } from "@web/../tests/helpers/mock_env";
+import {
+    editInput,
+    getFixture,
+    mount,
+    patchWithCleanup,
+} from "@web/../tests/helpers/utils";
+import { PortalFileInput } from "@project/project_sharing/components/portal_file_input/portal_file_input";
+import { registry } from "@web/core/registry";
+import { session } from "@web/session";
+import { makeFakeLocalizationService } from "@web/../tests/helpers/mock_services";
+
+const serviceRegistry = registry.category("services");
+
+let target;
+
+// -----------------------------------------------------------------------------
+// Helpers
+// -----------------------------------------------------------------------------
+
+async function createFileInput({ mockPost, mockAdd, props }) {
+    serviceRegistry.add("notification", {
+        start: () => ({
+            add: mockAdd || (() => {}),
+        }),
+    });
+    serviceRegistry.add("http", {
+        start: () => ({
+            post: mockPost || (() => {}),
+        }),
+    });
+    const env = await makeTestEnv();
+    await mount(PortalFileInput, target, { env, props });
+}
+
+// -----------------------------------------------------------------------------
+// Tests
+// -----------------------------------------------------------------------------
+
+QUnit.module("Project", ({ beforeEach }) => {
+    beforeEach(() => {
+        patchWithCleanup(odoo, { csrf_token: "dummy" });
+
+        target = getFixture();
+    });
+
+    QUnit.module("PortalComponents");
+
+    QUnit.test("uploading a file that is too heavy in portal will send a notification", async (assert) => {
+        serviceRegistry.add("localization", makeFakeLocalizationService());
+        patchWithCleanup(session, { max_file_upload_size: 2 });
+        await createFileInput({
+            props: {
+                onUpload(files) {
+                    assert.deepEqual(files, [null]);
+                },
+            },
+            mockAdd: (message) => {
+                assert.step("notification");
+                assert.strictEqual(
+                    message,
+                    "The selected file (4B) is over the maximum allowed file size (2B)."
+                );
+            },
+        });
+
+        const file = new File(["test"], "fake_file.txt", { type: "text/plain" });
+        await editInput(target, ".o_file_input input", file);
+        assert.verifySteps(
+            ["notification"],
+            "Only the notification will be triggered and the file won't be uploaded."
+        );
+    });
+});