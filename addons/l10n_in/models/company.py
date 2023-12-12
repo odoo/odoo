c88b356cfae7002d97e60711de9d4cb8f6761d76
Kashyap Patel <kasp@odoo.com>
2023-12-12 11:07:47 +0530

    [ADD] l10n_in: HSN code length selection at setting level
    In this commit,
    - The taxpayers having Aggregate Annual Turnover (AATO) above Rs. 5 Crore shall
      use at least a 6-digit HSN code and other taxpayers shall use at least
      a 4-digit HSN code.
    - However, there was no functionality to select the length of the HSN code.
    - So added settings to select the length of the HSN code at the setting level.
      Users can now define the desired length for HSN codes.
    
    task_id: 3527378
    
    closes odoo/odoo#145912
    
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_in/models/company.py
+++ addons/l10n_in/models/company.py
@@ -4,3 +4,11 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     l10n_in_upi_id = fields.Char(string="UPI Id")
+    l10n_in_hsn_code_digit = fields.Selection(
+        selection=[
+            ("4", "4 Digits"),
+            ("6", "6 Digits"),
+            ("8", "8 Digits"),
+        ],
+        string="HSN Code Digit",
+    )
