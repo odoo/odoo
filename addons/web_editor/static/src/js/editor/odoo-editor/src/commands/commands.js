08bee8b5955add13bf5dc9b6a24fe4b767c4428d
Deependra Solanki <deso@odoo.com>
2023-12-12 11:06:23 +0530

    [FIX] web_editor: properly restore cursor in setTag
    Commit [1] introduces an approach to preserve the cursor in `setTag` when new
    node is inserted. It used `setStart` at `firstLeaf` of `startContainer` and used
    offset of the `startContainer` similarly for `endContainer` which is incorrect
    and would throw traceback regarding no child at that offset.
    This commit fix it by getting the correct startContainer and endContainer when
    new node is created.
    
    [1]: d04e32c
    
    task-3245819
    
    X-original-commit: 51e4ac9e09ccbfcbb23a7a61ed2047f85b74fb39
    Part-of: odoo/odoo#152253

--- addons/web_editor/static/src/js/editor/odoo-editor/src/commands/commands.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/commands/commands.js
@@ -46,8 +46,6 @@ import {
     isVisibleTextNode,
     getCursorDirection,
     resetOuids,
-    firstLeaf,
-    lastLeaf,
     FONT_SIZE_CLASSES,
     TEXT_STYLE_CLASSES,
 } from '../utils/utils.js';
@@ -330,7 +328,9 @@ export const editorCommands = {
             !descendants(block).some(descendant => selectedBlocks.includes(descendant)) &&
             block.isContentEditable
         ));
-        const [startContainer, startOffset, endContainer, endOffset] = [firstLeaf(range.startContainer), range.startOffset, lastLeaf(range.endContainer), range.endOffset];
+        let { startContainer, startOffset, endContainer, endOffset } = range;
+        const startContainerChild = startContainer.firstChild;
+        const endContainerChild = endContainer.lastChild;
         for (const block of deepestSelectedBlocks) {
             if (
                 ['P', 'PRE', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'LI', 'BLOCKQUOTE'].includes(
@@ -369,10 +369,17 @@ export const editorCommands = {
                 children.forEach(child => newBlock.appendChild(child));
             }
         }
+        const isContextBlock = container => ['TD', 'DIV', 'LI'].includes(container.nodeName);
+        if (!startContainer.isConnected || isContextBlock(startContainer)) {
+            startContainer = startContainerChild.parentNode;
+        }
+        if (!endContainer.isConnected || isContextBlock(endContainer)) {
+            endContainer = endContainerChild.parentNode;
+        }
         const newRange = new Range();
-        newRange.setStart(startContainer,startOffset);
-        newRange.setEnd(endContainer,endOffset);
-        getDeepRange(editor.editable, { range: newRange, select: true, });
+        newRange.setStart(startContainer, startOffset);
+        newRange.setEnd(endContainer, endOffset);
+        getDeepRange(editor.editable, { range: newRange, select: true });
         editor.historyStep();
     },
 
