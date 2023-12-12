e61645386a2542c2949537c5e273aeb471d88cf6
Kashyap Patel <kasp@odoo.com>
2023-12-12 11:07:47 +0530

    [ADD] l10n_in: HSN code length selection at setting level
    In this commit,
    - The taxpayers having Aggregate Annual Turnover (AATO) above Rs. 5 Crore shall
      use at least a 6-digit HSN code and other taxpayers shall use at least
      a 4-digit HSN code.
    - However, there was no functionality to select the length of the HSN code.
    - So added settings to select the length of the HSN code at the setting level.
      Users can now define the desired length for HSN codes.
    
    task_id: 3527378
    
    closes odoo/odoo#145912
    
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_in/models/res_config_settings.py
+++ addons/l10n_in/models/res_config_settings.py
@@ -10,3 +10,4 @@ class ResConfigSettings(models.TransientModel):
     group_l10n_in_reseller = fields.Boolean(implied_group='l10n_in.group_l10n_in_reseller', string="Manage Reseller(E-Commerce)")
     module_l10n_in_edi = fields.Boolean('Indian Electronic Invoicing')
     module_l10n_in_edi_ewaybill = fields.Boolean('Indian Electronic Waybill')
+    l10n_in_hsn_code_digit = fields.Selection(related='company_id.l10n_in_hsn_code_digit', readonly=False)
