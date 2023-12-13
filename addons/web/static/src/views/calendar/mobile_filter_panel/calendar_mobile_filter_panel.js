95fcb25396e313ae4ca5075c027b22fee977d17b
Pierre Pulinckx (pipu) <pipu@odoo.com>
2023-12-13 13:55:05 +0100

    [FIX] web: calendar > filter panel > sort items ASC
    Before this commit:
    Filter items in the sidebar/panel are sorted DESC. Items without value
    are put at the end of the sorted list (eg : Open Shifts)
    
    After this commit:
    Filter items are sorted ASC. Label that begins with numeric are also
    sorted correctly. Items without value are stull put at the end of the
    sorted list.
    
    task~3524031
    
    closes odoo/odoo#146127
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/web/static/src/views/calendar/mobile_filter_panel/calendar_mobile_filter_panel.js
+++ addons/web/static/src/views/calendar/mobile_filter_panel/calendar_mobile_filter_panel.js
@@ -18,10 +18,15 @@ export class CalendarMobileFilterPanel extends Component {
             if (a.type === b.type) {
                 const va = a.value ? -1 : 0;
                 const vb = b.value ? -1 : 0;
+                //Condition to put unvaluable item (eg: Open Shifts) at the end of the sorted list.
                 if (a.type === "dynamic" && va !== vb) {
                     return va - vb;
                 }
-                return b.label.localeCompare(a.label);
+                return a.label.localeCompare(b.label, undefined, {
+                    numeric: true,
+                    sensitivity: "base",
+                    ignorePunctuation: true,
+                });
             } else {
                 return this.getFilterTypePriority(a.type) - this.getFilterTypePriority(b.type);
             }
