ac1da232ff1a9b0de23cecde600dfe709670355a
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2023-12-13 13:15:14 +0100

    [REV] account, point_of_sale: Send & Print traceback in MX
    This reverts commit 096ced730b6107111908478d81c5d547a5ca0c20.
    
    opw-3634504
    
    closes odoo/odoo#146142
    
    Related: odoo/enterprise#52688
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -4407,16 +4407,13 @@ class AccountMove(models.Model):
         composer.action_send_and_print(from_cron=from_cron, allow_fallback_pdf=allow_fallback_pdf)
 
     def generate_pdf_and_send_invoice(self):
-        if len(self) < 2 and self.invoice_pdf_report_id:
-            # if the Send & Print succeeded
+        """ Calls `_generate_pdf_and_send_invoice` and returns the (pdf, filename)"""
+        template = self.env.ref(self._get_mail_template())
+        self.with_context(skip_invoice_sync=True)._generate_pdf_and_send_invoice(template)
+        if len(self) < 2:
             return self.invoice_pdf_report_id.raw, self.invoice_pdf_report_id.name
-        elif len(self) < 2 and self.message_main_attachment_id:
-            # if the Send & Print failed with fallback=True -> proforma PDF
-            return self.message_main_attachment_id.raw, self.message_main_attachment_id.name
-        # all other cases
-        pdf_content = self.env['ir.actions.report']._render('account.account_invoices', self.ids)[0]
-        pdf_name = self._get_invoice_report_filename() if len(self) == 1 else "Invoices.pdf"
-        return pdf_content, pdf_name
+        else:
+            return self.env['ir.actions.report']._render('account.account_invoices', self.ids)[0], "Invoices.pdf"
 
     def _get_invoice_pdf_report_filename(self):
         """ Get the filename of the generated PDF invoice report. """
