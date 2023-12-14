a1eff4d9b541d5746e9754fdedbbb3ea741c8c4d
Ruben Gomes (rugo) <rugo@odoo.com>
2023-12-14 15:18:01 +0100

    [FIX] l10n_de: tax report input positive
    In the German tax report, they like to have all their taxes express in
    positive number. This PR changes the symbol of deductible taxes amount
    from negative to positive.
    
    We also reduce the number of taxes that are active and simplify some
    formula names in the tax report.
    
    task-3100357
    
    closes odoo/odoo#146317
    
    Related: odoo/enterprise#52789
    Related: odoo/upgrade#5444
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- addons/l10n_de/__manifest__.py
+++ addons/l10n_de/__manifest__.py
@@ -6,7 +6,7 @@
     'icon': '/account/static/description/l10n.png',
     'countries': ['de'],
     'author': 'openbig.org (http://www.openbig.org)',
-    'version': '2.0',
+    'version': '2.1',
     'website': 'https://www.odoo.com/documentation/master/applications/finance/fiscal_localizations/germany.html',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
