3000ac24ed68006b39a6ca49a0e131ebec3addd9
sesn-odoo <sesn@odoo.com>
2023-12-14 10:03:29 +0100

    [FIX] base,l10n_gcc_invoice: apply the 'Total amount in letters' setting
    Summary
    -------
    Currently, the 'Total amount of invoice in letters' setting doesn't do
    anything on GCC invoices.
    
    Steps to reproduce
    ------------------
    * install `l10n_sa`
    * enable the Arabic language
    * in the settings, enable 'Total amount of invoice in letters'
    * create and print an invoice
    
    You should see that the amount in words in not displayed.
    
    Note:
    Currently, currency labels are not translatable. Since this isn't
    something that can be changed in stable version, it was decided to not
    include them in the Arabic amount in words.
    
    opw-3501112
    opw-3485691
    
    closes odoo/odoo#146379
    
    X-original-commit: 1fb28f229c701ae1261dc502ef345ffda162f994
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>
    Signed-off-by: Séna Serge Nshimiyimana (sesn) <sesn@odoo.com>

--- addons/l10n_gcc_invoice/models/account_move.py
+++ addons/l10n_gcc_invoice/models/account_move.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import logging
+
 from odoo import fields, models, api
 
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from num2words import num2words
+except ImportError:
+    _logger.warning("The num2words python library is not installed, amount-to-text features won't be fully available.")
+    num2words = None
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -14,6 +24,13 @@ class AccountMove(models.Model):
         if self.company_id.country_id in self.env.ref('base.gulf_cooperation_council').country_ids:
             return 'l10n_gcc_invoice.arabic_english_invoice'
         return super()._get_name_invoice_report()
+
+    def _num2words(self, number, lang):
+        if num2words is None:
+            _logger.warning("The library 'num2words' is missing, cannot render textual amounts.")
+            return ""
+
+        return num2words(number, lang=lang).title()
 
 
 class AccountMoveLine(models.Model):
