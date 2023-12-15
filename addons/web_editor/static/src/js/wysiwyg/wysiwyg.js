64a2c67342b21c94f24e9b2d47c259752270ae2e
Benoit Socias <bso@odoo.com>
2023-12-15 14:07:03 +0100

    [FIX] web_editor: restore gradient custom buttons
    Since [1] when the `Colorpicker` template cache was moved from
    `ColorPalette` to `getColorPickerTemplateService`, the custom buttons
    fill color cannot be set as a gradient anymore.
    This happens because the needed `getTemplate` props is injected for the
    font and background colors, for the `we-colorpicker` but not for the
    link tools color palettes.
    
    This commit restores the gradient color selection for custom buttons by
    linking the `getColorPickerTemplateService` to the link tools color
    palettes.
    
    Steps to reproduce:
    - Edit Home page.
    - Click on "Contact Us" link in header.
    - Select link style "Custom" in link tools.
    - Open "Fill Color" palette.
    
    => Gradient tab was not shown in palette.
    
    [1]: https://github.com/odoo/odoo/commit/1d2e54088b0f0e28464ab6aa884fb2e1110e8e04
    
    task-3641914
    
    closes odoo/odoo#147397
    
    X-original-commit: 087861bcbfe444765fdb0b81e0398c12d8db34e8
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1375,7 +1375,8 @@ export class Wysiwyg extends Component {
                         onDestroy: () => {
                             removeHintClasses();
                             this.linkToolsInfos.onDestroy();
-                        }
+                        },
+                        getColorpickerTemplate: this.getColorpickerTemplate.bind(this),
                     };
                 }
                 // update the shouldFocusUrl prop to focus on url when double click and click edit link
