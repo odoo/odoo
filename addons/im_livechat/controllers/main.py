4f4fa8b5ccb93f8a8f4e81ce8a6475ba97362cd8
rkka-odoo <rkka@odoo.com>
2023-12-18 18:17:23 +0530

    [FIX] im_livechat: allowed user to close chat when user delete channel
    When user tries to close chat window  when current channels/members
     are deleted.
    
    Steps to reproduce error:
    - Activate 'im_livechat' and 'website' module
    - Open the 'website' module and clicked Contact us menu
    - Open chat-bot and start conversation
    - Duplicate the tab and Go to the Settings>Technical>Channels/Members in second
    tab
    - Delete the current channels/members from second tab
    - Go to the first tab and close chat windowq
    - Error will be generated
    
    Error: ValueError : Expected singleton: discuss.channel.member()
    
    Here at [1] member got empty value when current channels/members  are deleted
    from  'discuss.channel.member' model by user.
    
    This commit resolved the above issue by calling  '_rtc_leave_call()' method
    when a member is available.
    
    [1]-https://github.com/odoo/odoo/blob/952ebac37dc65b3fd62ae211cee1fd45fa63cd47/addons/im_livechat/controllers/main.py#L284-L286
    
    sentry-4719812967

--- addons/im_livechat/controllers/main.py
+++ addons/im_livechat/controllers/main.py
@@ -283,5 +283,6 @@ class LivechatController(http.Controller):
         domain = [("channel_id", "=", channel_sudo.id), ("is_self", "=", True)]
         member = request.env["discuss.channel.member"].search(domain)
         # sudo: discuss.channel.rtc.session - member of current user can leave call
-        member.sudo()._rtc_leave_call()
+        if member:
+            member.sudo()._rtc_leave_call()
         channel_sudo._close_livechat_session()
