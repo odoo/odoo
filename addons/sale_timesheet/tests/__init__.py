9b470144368a2879b7cefeb8506265dc55b6a2b5
kpt-odoo <kpt@odoo.com>
2023-12-18 16:12:20 +0530

    [REM] sale_timesheet: remove project create sale order wizard
    In this commit we have removed unused "project.create.sale.order" wizard
    this wizard was deprecated in this task 2637495, but it's still in the code
    and used in some tests.
    
    task-3632161
    
    closes odoo/odoo#147533
    
    Related: odoo/enterprise#53390
    Related: odoo/upgrade#5470
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_timesheet/tests/__init__.py
+++ addons/sale_timesheet/tests/__init__.py
@@ -8,7 +8,6 @@ from . import test_project
 from . import test_project_billing
 from . import test_project_profitability
 from . import test_reinvoice
-from . import test_project_billing_multicompany
 from . import test_upsell_warning
 from . import test_edit_so_line_timesheet
 from . import test_so_line_determined_in_timesheet
