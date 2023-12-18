9872e1f432e304b82480595eabd250657e89ff4f
OmarAbosamaha <abom@odoo.com>
2023-12-18 23:40:26 +0200

    [FIX] l10n_ar: add missing translations
    Issue: spanish_ar translation is missing for some menu items and template names in the accounting app
    
    Steps to reproduce:
    1- install l10n_ar
    2- switch language to Spanish(AR)
    3- Open accounting app
    4- check menu items
    
    Solution:
    Issue was that the translation file was called es.po and for spanish (AR) language it should fallback to es_419
    As per the translation code found here in in 'translate.py' in odoo tools
    
    https://github.com/odoo/odoo/blob/20a3998c3073af8a5d95f8303283107363caed85/odoo/tools/translate.py#L1565-L1574
    
    opw-3601556
    
    X-original-commit: 72e123e8db0d7c4e50489742b5b584138baacdc3

--- addons/l10n_ar/models/template_ar_ex.py
+++ addons/l10n_ar/models/template_ar_ex.py
@@ -1,5 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models
+from odoo import models, _
 from odoo.addons.account.models.chart_template import template
 
 
@@ -9,7 +9,7 @@ class AccountChartTemplate(models.AbstractModel):
     @template('ar_ex')
     def _get_ar_ex_template_data(self):
         return {
-            'name': 'Argentine Generic Chart of Accounts for Exempt Individuals',
+            'name': _('Argentine Generic Chart of Accounts for Exempt Individuals'),
             'parent': 'ar_base',
             'code_digits': '12',
         }
