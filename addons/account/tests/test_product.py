8de719de1083e024b78dfe7ceb26ed5203713c6e
Jairo Llopis <jairo@moduon.team>
2023-12-19 09:43:46 +0000

    [FIX] account: allow users to see products after configuring taxes
    The `account` module adds the `tax_string` field to product forms:
    https://github.com/odoo/odoo/blob/ceb8b785fba1055deeff8d9075ccc707f2317278/addons/account/views/product_view.xml#L76
    
    That field calls `Model(account.tax).compute_all()`:
    https://github.com/odoo/odoo/blob/ceb8b785fba1055deeff8d9075ccc707f2317278/addons/account/models/product.py#L71
    
    That method needs access to `account.account.tag` records:
    https://github.com/odoo/odoo/blob/ceb8b785fba1055deeff8d9075ccc707f2317278/addons/account/models/account_tax.py#L563
    https://github.com/odoo/odoo/blob/ceb8b785fba1055deeff8d9075ccc707f2317278/addons/account/models/account_tax.py#L632
    https://github.com/odoo/odoo/blob/ceb8b785fba1055deeff8d9075ccc707f2317278/addons/account/models/account_tax.py#L648
    https://github.com/odoo/odoo/blob/ceb8b785fba1055deeff8d9075ccc707f2317278/addons/account/models/account_tax.py#L671
    
    Internal users were able to read `account.tax` and `account.tax.repartition.line` records, but they couldn't read `account.account.tag` records.
    
    So, this lead to the absurd situation where a user with permissions to read products (like stock, PoS or event users) couldn't be able to see the product form anymore whenever that product happened to have a tax with a repartition line with a tag.
    
    This is a regression from Odoo 14, introduced in https://github.com/odoo/odoo/pull/74138 and https://github.com/odoo/odoo/pull/73602.
    
    I'm granting all internal users read permission over `account.account.tag`, just like the one they have for `account.tax` and `account.tax.repartition.line`. BTW that's the fix suggested by Odoo helpdesk.
    
    Funny enough, while exercising the test, I also noticed that a similar situation happens with MRP. When installed, other users that would normally be able to browse products, would no longer have access. The fix is incouded too; otherwise the test wouldn't pass.
    
    @moduon MT-4390
    OPW-3636032
    
    closes odoo/odoo#149298
    
    X-original-commit: 846d7a2e875a08095001dc7a3d90df7d9c18e5d8
    Signed-off-by: Cedric Snauwaert <csn@odoo.com>

---
+++ addons/account/tests/test_product.py
@@ -0,0 +1,38 @@
+from .common import AccountTestInvoicingCommon
+from odoo.tests.common import Form, tagged, new_test_user
+from odoo import Command
+
+
+@tagged("post_install", "-at_install")
+class AccountProductCase(AccountTestInvoicingCommon):
+    @classmethod
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass(chart_template_ref)
+        cls.internal_user = new_test_user(
+            cls.env, login="internal_user", groups="base.group_user"
+        )
+
+    def test_internal_user_can_read_product_with_tax_and_tags(self):
+        """Internal users need read access to products, no matter their taxes."""
+        # Add a tag to product_a's default tax
+        self.company_data["company"].country_id = self.env.ref("base.us")
+        tax_line_tag = self.env["account.account.tag"].create(
+            {
+                "name": "Tax tag",
+                "applicability": "taxes",
+                "country_id": self.company_data["company"].country_id.id,
+            }
+        )
+        repartition_lines = (
+            self.product_a.taxes_id.invoice_repartition_line_ids
+            | self.product_a.taxes_id.refund_repartition_line_ids
+        ).filtered_domain([("repartition_type", "=", "tax")])
+        repartition_lines.write({"tag_ids": [Command.link(tax_line_tag.id)]})
+        repartition_lines.invalidate_cache()
+        # Check that internal user can read product_a
+        with Form(
+            self.product_a.with_user(self.internal_user).with_context(lang="en_US")
+        ) as form_a:
+            # The tax string itself is not very important here; we just check
+            # it has a value and we can read it, so there were no access errors
+            self.assertTrue(form_a.tax_string)
