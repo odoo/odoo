6b472a485fe20ea90c029234f3f05691aeec6b10
Thomas Lefebvre (thle) <thle@odoo.com>
2023-12-19 10:38:29 +0100

    [FIX] website_form_project: give access token to portal attachment
    Issue:
    ------
    When we create a task from website form with an attachment,
    the portal user no longer has access to his attachment
    (even if it is a follower of the project).
    
    This is problematic because he may think that it has not been sent correctly.
    
    Cause:
    ------
    The document has no `access_token`.
    
    Solution:
    ---------
    Give an `acces_token` to the document linked to the ticket.
    In this way, the document will appear in the portal view's chat.
    
    opw-3601205

--- addons/website_form_project/controllers/main.py
+++ addons/website_form_project/controllers/main.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.addons.website.controllers import form
-
+from odoo.http import request
 
 class WebsiteForm(form.WebsiteForm):
     def insert_record(self, request, model, values, custom, meta=None):
@@ -13,3 +13,17 @@ class WebsiteForm(form.WebsiteForm):
                 values['partner_id'] = visitor_partner.id
 
         return super().insert_record(request, model, values, custom, meta=meta)
+
+    def insert_attachment(self, model, id_record, files):
+        super().insert_attachment(model, id_record, files)
+        # If the task form is submit with attachments,
+        # Give access token to these attachments and make the message
+        # accessible to the portal user.
+        model_name = model.model
+        if model_name == "project.task":
+            task = model.env[model_name].browse(id_record)
+            attachments = request.env['ir.attachment'].sudo().search([('res_model', '=', model_name), ('res_id', '=', task.id), ('access_token', '=', False)])
+            attachments.generate_access_token()
+            message_ids = task.message_ids.filtered(lambda m: m.attachment_ids == attachments)
+            message_ids.is_internal = False
+            message_ids.subtype_id.internal = False
