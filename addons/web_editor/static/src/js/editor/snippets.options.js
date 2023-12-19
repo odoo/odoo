7a5ad30d2831efbe6d3e497b267a37769cbc9254
Robin Lejeune (role) <role@odoo.com>
2023-12-19 13:50:08 +0100

    [FIX] web_editor: unobserve background grid z-index
    Minor fix: the background grid is not observed in the first place,
    setting a z-index on it should not be observed either.
    Noticed when fixing `automaticStep(Un)active()` at [1].
    
    [1]: https://github.com/odoo/odoo/commit/c2d952689c97add74a372c90aa18787094d5d93a
    
    Related to task-3576046
    
    X-original-commit: 901ed6d8c6c6bdb8aff8e9366d63bba59051019a

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -4409,8 +4409,8 @@ registry.sizing = SnippetOptionWidget.extend({
             if (rowEl.classList.contains('o_grid_mode')) {
                 self.options.wysiwyg.odooEditor.observerUnactive('displayBackgroundGrid');
                 backgroundGridEl = gridUtils._addBackgroundGrid(rowEl, 0);
-                self.options.wysiwyg.odooEditor.observerActive('displayBackgroundGrid');
                 gridUtils._setElementToMaxZindex(backgroundGridEl, rowEl);
+                self.options.wysiwyg.odooEditor.observerActive('displayBackgroundGrid');
             }
 
             // For loop to handle the cases where it is ne, nw, se or sw. Since
