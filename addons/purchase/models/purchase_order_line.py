b687b7b5d968ee1a0e6aace8374b201589b60d9c
Nasreddin Boulif (bon) <bon@odoo.com>
2023-12-20 16:00:14 +0000

    [FIX] purchase: sent message with date_planned localised in PO timezone
    Steps to reproduce:
    
      - Activate debug mode
      - Install `Purchase` module
      - Set user timezone to `Europe/Brussels`
      - Create a purchase order and add an order line
      - Set `Receipt Date` to any date in the future with time 01:00:00
      - Confirm the purchase order
      - Click on "Confirm Receipt Date" button
      - Check message in the chatter
    
    Issue:
    
      The receipt date is the day before the one set in the purchase order.
      (Same issue when sending reminder by mail)
    
    Cause:
    
      `date_planned` is stored in UTC in the database and used as it is in
      the message.
    
    Solution:
    
      Convert `date_planned` to the order timezone before sending the
      message.
    
    opw-3503928
    
    X-original-commit: 7407cf441ae271aa6a4e67c351a5d5ca8de30b08

--- addons/purchase/models/purchase_order_line.py
+++ addons/purchase/models/purchase_order_line.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from datetime import datetime, time
 from dateutil.relativedelta import relativedelta
-from pytz import timezone, UTC
+from pytz import UTC
 
 from odoo import api, fields, models, _
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, get_lang
@@ -623,7 +623,7 @@ class PurchaseOrderLine(models.Model):
         """Return a datetime which is the noon of the input date(time) according
         to order user's time zone, convert to UTC time.
         """
-        return timezone(self.order_id.user_id.tz or self.company_id.partner_id.tz or 'UTC').localize(datetime.combine(date, time(12))).astimezone(UTC).replace(tzinfo=None)
+        return self.order_id.get_order_timezone().localize(datetime.combine(date, time(12))).astimezone(UTC).replace(tzinfo=None)
 
     def _update_date_planned(self, updated_date):
         self.date_planned = updated_date
