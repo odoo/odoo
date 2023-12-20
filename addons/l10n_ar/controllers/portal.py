c6a3e9ac4cc4dfd125da0cf03cc5af25280b16a1
Katherine Zaoral <kz@adhoc.com.ar>
2023-12-20 18:55:56 -0300

    [ADD] l10n_ar: fixup apply changes as ecuador

--- addons/l10n_ar/controllers/portal.py
+++ addons/l10n_ar/controllers/portal.py
@@ -6,29 +6,52 @@ from odoo.exceptions import ValidationError
 
 class CustomerPortalAr(CustomerPortal):
 
-    OPTIONAL_BILLING_FIELDS = CustomerPortal.OPTIONAL_BILLING_FIELDS + [
-        "l10n_latam_identification_type_id", "l10n_ar_afip_responsibility_type_id"]
+    OPTIONAL_BILLING_FIELDS = [*CustomerPortal.OPTIONAL_BILLING_FIELDS, "l10n_latam_identification_type_id", "l10n_ar_afip_responsibility_type_id"]
 
-    def is_argentinian_company(self, redirect=None, **post):
+    def is_argentinian_company(self):
         return request.env.company.country_code == 'AR'
 
     @route()
     def account(self, redirect=None, **post):
         """Extend in order to add information about the identification types and AFIP responsibility show in portal"""
-        if not self.is_argentinian_company():
-            return super().account(redirect=redirect, **post)
-
-        if post and request.httprequest.method == 'POST':
-            post.update({'l10n_latam_identification_type_id': int(post.pop('l10n_latam_identification_type_id') or False) or False,
-                         'l10n_ar_afip_responsibility_type_id': int(post.pop('l10n_ar_afip_responsibility_type_id') or False) or False})
-
         response = super().account(redirect=redirect, **post)
-        response.qcontext.update({
-            'identification_types': request.env['l10n_latam.identification.type'].sudo().search([('l10n_ar_afip_code', '!=', False)]),
-            'responsibility_types': request.env['l10n_ar.afip.responsibility.type'].sudo().search([]),
-            'identification': post.get('l10n_latam_identification_type_id'),
-            'responsibility': post.get('l10n_ar_afip_responsibility_type_id')})
+
+        if self.is_argentinian_company():
+            if post and request.httprequest.method == 'POST':
+                l10n_latam_identification_type_id = int(post.pop('l10n_latam_identification_type_id') or False) or False
+                l10n_ar_afip_responsibility_type_id = int(post.pop('l10n_ar_afip_responsibility_type_id') or False) or False
+
+                partner = request.env.user.partner_id
+                post.update({
+                    'l10n_latam_identification_type_id': l10n_latam_identification_type_id,
+                    'l10n_ar_afip_responsibility_type_id': l10n_ar_afip_responsibility_type_id,
+                })
+                partner.sudo().write({
+                    'l10n_latam_identification_type_id': l10n_latam_identification_type_id,
+                    'l10n_ar_afip_responsibility_type_id': l10n_ar_afip_responsibility_type_id,
+                })
+
+            response.qcontext.update({
+                'identification': post.get('l10n_latam_identification_type_id'),
+                'identification_types': request.env['l10n_latam.identification.type'].search(
+                    [('l10n_ar_afip_code', '!=', False)]),
+
+                'responsibility': post.get('l10n_ar_afip_responsibility_type_id'),
+                'responsibility_types': request.env['l10n_ar.afip.responsibility.type'].sudo().search([]),
+            })
         return response
+
+    def details_form_validate(self, data, partner_creation=False):
+        # EXTEND 'portal'
+        error, error_message = super(CustomerPortalAr, self).details_form_validate(data)
+
+        if self.is_argentinian_company():
+            required_ar_fields = ("l10n_latam_identification_type_id", "vat", "l10n_ar_afip_responsibility_type_id")
+            for field_name in required_ar_fields:
+                if not data.get(field_name):
+                    error[field_name] = 'missing'
+
+        return error, error_message
 
     def _vat_validation(self, data, error, error_message, partner_creation=False):
         """ If Argentinian Company Do the vat validation taking into account the identification_type """
