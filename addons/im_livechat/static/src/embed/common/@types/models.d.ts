2d452094b2de6e315ddf7777967b383c340783c1
tsm-odoo <tsm@odoo.com>
2023-12-20 12:31:39 +0100

    [FIX] im_livechat: avoid useless message fetch when chat is created
    When a live chat is created on the server, the `/mail/thread/messages`
    route is called. This is not required since we know for a fact the
    thread was just created. This PR prevent this useless request.
    
    task-3646201
    
    X-original-commit: ba5585fea7b3efed283cf9d8cc80cccf311dd3a3

--- addons/im_livechat/static/src/embed/common/@types/models.d.ts
+++ addons/im_livechat/static/src/embed/common/@types/models.d.ts
@@ -2,5 +2,8 @@ declare module "models" {
     export interface Thread {
         chatbotTypingMessage: Message,
         livechatWelcomeMessage: Message,
+        chatbot_script_id: number | null,
+        requested_by_operator: boolean,
+        isNewlyCreated: boolean,
     }
 }
