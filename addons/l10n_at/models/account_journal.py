0aece054d9cdf89d054dcf6fcfb29864e9479aa9
Antoine Dupuis (andu) <andu@odoo.com>
2023-12-21 13:01:06 +0000

    [FIX] l10n_at: Add appropriate account tags to accounts.
    In order to work with the Balance Sheet, several accounts needed to be
    set up with correct account tags.
    
    Taskid: 3060790
    X-original-commit: fb0bec3c7beaac0958f87fa3bcf7a6005367fcea

--- addons/l10n_at/models/account_journal.py
+++ addons/l10n_at/models/account_journal.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 
 from odoo import api, models, Command
@@ -20,3 +21,26 @@ class AccountJournal(models.Model):
             ]
 
         return account_vals
+||||||| parent of ded6374b8067 (temp)
+=======
+from odoo import api, models, Command
+
+
+class AccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+    @api.model
+    def _prepare_liquidity_account_vals(self, company, code, vals):
+        ''' Set Balance Sheet and SAF-T tags on new bank and cash accounts.'''
+        # OVERRIDE
+        account_vals = super()._prepare_liquidity_account_vals(company, code, vals)
+
+        if company.account_fiscal_country_id.code == 'AT':
+            account_vals.setdefault('tag_ids', [])
+            account_vals['tag_ids'] += [
+                Command.link(self.env.ref('l10n_at.account_tag_l10n_at_ABIV').id),
+                Command.link(self.env.ref('l10n_at.account_tag_external_code_2300').id),
+            ]
+
+        return account_vals
+>>>>>>> ded6374b8067 (temp)
