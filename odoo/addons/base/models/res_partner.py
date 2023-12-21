97dae2efd7c4bafb1a0b8ed8adac1490d4e26f88
Jérémy Hennecart (jeh) <jeh@odoo.com>
2023-12-21 16:09:55 +0100

    [IMP] various*: improve usability & UI
    *: base,calendar,crm,mass_mailing{_sms},point_of_sale,purchase,resource
    
    This commit adds some minor improvements to usability & UI, notably:
    - Resize the truck/dollar images used as a placeholders for delivery & invoice
      addresses for partners. This is avoids the image being cut in the kanban view
    - Add some margin to a few icons
    - Rework a few tooltips, placeholders & icons
    - Use the daterange widget in the resource.calendar.leaves form
    - Use unicode icons for better support inside mass_mailing
    - Remove the tooltip of address type as it creates a big block of text, we
      instead favor the muted-text that explains the currently selected type
      directly inside the form view
    
    task-3515819
    
    closes odoo/odoo#148850
    
    Related: odoo/enterprise#54048
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>
    Co-authored-by: Aurélien Warnon <awa@odoo.com>

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -229,11 +229,7 @@ class Partner(models.Model):
          ('delivery', 'Delivery Address'),
          ('other', 'Other Address'),
         ], string='Address Type',
-        default='contact',
-        help="- Contact: Use this to organize the contact details of employees of a given company (e.g. CEO, CFO, ...).\n"
-             "- Invoice Address: Preferred address for all invoices. Selected by default when you invoice an order that belongs to this company.\n"
-             "- Delivery Address: Preferred address for all deliveries. Selected by default when you deliver an order that belongs to this company.\n"
-             "- Other: Other address for the company (e.g. subsidiary, ...)")
+        default='contact')
     # address fields
     street = fields.Char()
     street2 = fields.Char()
