f0ac42ac58a2c8a69ef81c6e011f190821cc0eaf
Katherine Zaoral <kz@adhoc.com.ar>
2023-12-21 10:15:41 -0300

    Revert "[FIX] l10n_ar: try to avoid runbot warning - PY007: qcontext"
    This reverts commit 79d95cf61802f3eb888ac769bef91c75245f17c0.

--- addons/l10n_ar/controllers/portal.py
+++ addons/l10n_ar/controllers/portal.py
@@ -29,11 +29,14 @@ class CustomerPortalAr(CustomerPortal):
                     'l10n_ar_afip_responsibility_type_id': l10n_ar_afip_responsibility_type_id,
                 })
 
-            response.qcontext['identification'] = post.get('l10n_latam_identification_type_id')
-            response.qcontext['identification_types'] = request.env['l10n_latam.identification.type'].search(
-                    [('l10n_ar_afip_code', '!=', False)])
-            response.qcontext['responsibility'] = post.get('l10n_ar_afip_responsibility_type_id')
-            response.qcontext['responsibility_types'] = request.env['l10n_ar.afip.responsibility.type'].sudo().search([])
+            response.qcontext.update({
+                'identification': post.get('l10n_latam_identification_type_id'),
+                'identification_types': request.env['l10n_latam.identification.type'].search(
+                    [('l10n_ar_afip_code', '!=', False)]),
+
+                'responsibility': post.get('l10n_ar_afip_responsibility_type_id'),
+                'responsibility_types': request.env['l10n_ar.afip.responsibility.type'].sudo().search([]),
+            })
         return response
 
     def details_form_validate(self, data, partner_creation=False):
