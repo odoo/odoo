e84b7cbde7da92ebff39e7aebeafc6b171d6a35b
Pedram (PEBR) <pebr@odoo.com>
2023-12-21 14:58:41 +0000

    [FIX] point_of_sale: keeping failed to sync orders as an attachment
    Before this commit:
     If an error happened when trying to process a PoS order nothing
     is saved on the backend to inform the user regarding the error.
     Note: Odoo logs would contain the information, but it is out of reach
     for certain clients (on odoo online for instance).
    
     In theory, we can't lose any information as, if the sync process raise
     an exception, the order is still on the PoS browser cache that will
     then try to be resync when another order sync happen.
     But, in practice, the support received some cases of "missing PoS
     orders". Which can happen as we fully rely on the client browser
     cache that can be cleared or use another computer/browser/session.
    
    After this commit:
     If an order can not be processed in the backend:
      - the PoS order data is saved in the PoS session attachments
      - a scheduled activity is created in the PoS session
    
     As an un-synced keep being tried to be sync (and will likely fail
     each time), we compare it with the already attached one to avoid having
     the content repeated multiple times.
     If the order was modified in between, a new attachment with the same
     name is created.
    
     Note: draft orders that will fail to validate are NOT stored
    
     The attachment and activity are automatically removed when the order
     of same reference is validated
    
    We also add logs when a PoS order start/finish to synchronise
    
    opw-3650239
    
    closes odoo/odoo#155401
    
    X-original-commit: 8d9cea6ed83e5d284562173720db102178223121
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-import logging
+from collections import defaultdict
 from datetime import datetime
-from markupsafe import Markup
 from functools import partial, lru_cache
 from itertools import groupby
-from collections import defaultdict
-from markupsafe import escape
+from markupsafe import escape, Markup
+from random import randrange
 
+import logging
 import psycopg2
 import pytz
 import re
@@ -148,6 +148,10 @@ class PosOrder(models.Model):
         pos_order = pos_order.with_company(pos_order.company_id)
         self = self.with_company(pos_order.company_id)
         self._process_payment_lines(order, pos_order, pos_session, draft)
+
+        if pos_session._is_capture_system_activated():
+            pos_session._remove_capture_content(order)
+
         return pos_order._process_saved_order(draft)
 
     def _process_saved_order(self, draft):
@@ -167,10 +171,6 @@ class PosOrder(models.Model):
             self._generate_pos_order_invoice()
 
         return self.id
-
-    def _clean_payment_lines(self):
-        self.ensure_one()
-        self.payment_ids.unlink()
 
     def _process_payment_lines(self, pos_order, order, pos_session, draft):
         """Create account.bank.statement.lines from the dictionary given to the parent function.
@@ -921,8 +921,12 @@ class PosOrder(models.Model):
         :type draft: bool.
         :Returns: list -- list of db-ids for the created and updated orders.
         """
+        order_names = [order['data']['name'] for order in orders]
+        sync_token = randrange(100000000)  # Use to differentiate 2 parallels calls to this function in the logs
+        _logger.info("Start PoS synchronisation #%d for PoS orders references: %s (draft: %s)", sync_token, order_names, draft)
         order_ids = []
         for order in orders:
+            order_name = order['data']['name']
             existing_draft_order = None
 
             if 'server_id' in order['data'] and order['data']['server_id']:
@@ -934,16 +938,23 @@ class PosOrder(models.Model):
                     continue
 
             if not existing_draft_order:
-                existing_draft_order = self.env['pos.order'].search(['&', ('pos_reference', '=', order['data']['name']), ('state', '=', 'draft')], limit=1)
+                existing_draft_order = self.env['pos.order'].search(['&', ('pos_reference', '=', order_name), ('state', '=', 'draft')], limit=1)
 
-            if existing_draft_order:
-                order_ids.append(self._process_order(order, draft, existing_draft_order))
-            else:
-                existing_orders = self.env['pos.order'].search([('pos_reference', '=', order['data']['name'])])
-                if all(not self._is_the_same_order(order['data'], existing_order) for existing_order in existing_orders):
-                    order_ids.append(self._process_order(order, draft, False))
-
-        return self.env['pos.order'].search_read(domain=[('id', 'in', order_ids)], fields=['id', 'pos_reference', 'account_move'], load=False)
+            try:
+                if existing_draft_order:
+                    order_ids.append(self._process_order(order, draft, existing_draft_order))
+                else:
+                    existing_orders = self.env['pos.order'].search([('pos_reference', '=', order_name)])
+                    if all(not self._is_the_same_order(order['data'], existing_order) for existing_order in existing_orders):
+                        order_ids.append(self._process_order(order, draft, False))
+            except Exception as e:
+                _logger.exception("An error occurred when processing the PoS order %s", order_name)
+                pos_session = self.env['pos.session'].browse(order['data']['pos_session_id'])
+                pos_session._handle_order_process_fail(order, e, draft)
+                raise
+        res = self.env['pos.order'].search_read(domain=[('id', 'in', order_ids)], fields=['id', 'pos_reference', 'account_move'], load=False)
+        _logger.info("Finish PoS synchronisation #%d with result: %s", sync_token, res)
+        return res
 
     def _is_the_same_order(self, data, existing_order):
         received_payments = [(p[2]['amount'], p[2]['payment_method_id']) for p in data['statement_ids']]
