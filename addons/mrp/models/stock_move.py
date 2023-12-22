a8b97727c68ff29b615623c1efa82a0bedaf7dff
JF Aubert <ajf@odoo.com>
2023-12-22 11:17:34 +0100

    [FIX] mrp: allow to consume less than expected
    In Shop Floor, consuming less tracked by lots components than expected
    gives an 'Invalid Operation: lot/serial number needs to be specified
    for a tracked product' error.
    Discarding the dialog to fix the quantities makes the production order
    disappear from the Shop Floor.
    This because of the MO Readiness filter: as the move becomes partially
    available, the reservation state goes to confirmed rather than assigned.
    
    closes odoo/odoo#162481
    
    X-original-commit: b742b79aaa6c6040c55d578079142760d2b1989a
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Jean-Francois Aubert (ajf) <ajf@odoo.com>

--- addons/mrp/models/stock_move.py
+++ addons/mrp/models/stock_move.py
@@ -619,6 +619,8 @@ class StockMove(models.Model):
         res = super()._get_relevant_state_among_moves()
         if res == 'partially_available'\
                 and self.raw_material_production_id\
-                and all(move.should_consume_qty and float_compare(move.quantity, move.should_consume_qty, precision_rounding=move.product_uom.rounding) >= 0 for move in self):
+                and all(move.should_consume_qty and float_compare(move.quantity, move.should_consume_qty, precision_rounding=move.product_uom.rounding) >= 0
+                        or (float_compare(move.quantity, move.product_uom_qty, precision_rounding=move.product_uom.rounding) >= 0 or (move.manual_consumption and move.picked))
+                        for move in self):
             res = 'assigned'
         return res
