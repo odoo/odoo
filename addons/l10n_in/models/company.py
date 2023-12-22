62c78606a04bb0244bc34f03026271608efadb3a
Kashyap Patel <kasp@odoo.com>
2023-12-22 14:15:38 +0530

    [REM] l10n_in: remove audit trail feature
    Before this commit
    ==================
    As the `account_audit_trail` module was separated from the `l10n_in` module,
    but there were duplicate views and models of the audit trail feature in the
    `l10n_in` module.
    
    After this commit
    =================
    Removed duplicate views and models of the audit trail feature from the
    `l10n_in` module.
    
    task_id: 3326258
    
    closes odoo/odoo#147258
    
    Related: odoo/upgrade#5462
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/l10n_in/models/company.py
+++ addons/l10n_in/models/company.py
@@ -1,15 +1,6 @@
 from odoo import fields, models, _
-from odoo.exceptions import UserError
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
     l10n_in_upi_id = fields.Char(string="UPI Id")
-    l10n_in_active_audit_trail = fields.Boolean(string="Audit Trail", default=False, help="Once you activate audit trail it can't be deactivated.")
-
-    def write(self, vals):
-        if 'l10n_in_active_audit_trail' in vals:
-            for company in self:
-                if company.l10n_in_active_audit_trail and not vals['l10n_in_active_audit_trail']:
-                    raise UserError(_("Deactivation of Audit Trail will not be allowed as per Government Mandate."))
-        return super(ResCompany, self).write(vals)
