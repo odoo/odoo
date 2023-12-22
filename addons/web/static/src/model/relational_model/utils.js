675a171f3f37df99af7a5322e30ea828eba4d4ee
FrancoisGe <fge@odoo.com>
2023-12-22 10:18:46 +0100

    [FIX] web: x2m in m2m_tags and x2m in list can be reordered
    Before this commit, in a non-editable x2many in list mode, if there
    is a field  "A" with many2many_tags which is displayed as an x2many
    in list mode in the form dialog, it cannot be reordered with handle field.
    
    Problem:
    When we extend a record from an x2many to a form dialog, we use the
    activeFields defined in the form view and patch them with those in the
    list view. Currently, we always use the default order from the list view
    even if it doesn't exist. So we always ignore the one defined in the form view.
    
    Solution:
    If there is no default order in the list view, we use the one in the form view.
    
    How to reproduce:
    - Go to a form view with an x2many field in list mode which contains
      an "A" field with the many2many_tags widget.
    - Click on a record in the x2many
    - A form dialog opens with field "A" in the form of an x2many in list mode,
      containing a handle field.
    - drag and drop a line to reorder the records
    
    Before this commit:
      It is not possible to reorder records
    
    After this commit:
      It is possible to reorder records
    
    Task ID: 3641933
    
    closes odoo/odoo#147385
    
    X-original-commit: b4676581707872d8e31afd2e778f82af6fa923d6
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/web/static/src/model/relational_model/utils.js
+++ addons/web/static/src/model/relational_model/utils.js
@@ -177,7 +177,7 @@ export function patchActiveFields(activeField, patch) {
     if ("limit" in patch) {
         activeField.limit = patch.limit;
     }
-    if ("defaultOrderBy" in patch) {
+    if (patch.defaultOrderBy) {
         activeField.defaultOrderBy = patch.defaultOrderBy;
     }
 }
