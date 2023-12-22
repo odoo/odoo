460ecb7225cc59bc369624371c98d246414ef1b4
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2023-12-22 10:44:11 +0000

    [FIX] account: branch user cannot add products
    Create a branch under the main company
    Select an internal user and keep only this branch on the fields
    "Allowed companies" and "Default Company"
    Access with the user
    Create an invoice
    Add a product
    
    Issue: user will be blocked by error
    """
    Access Error
    
    Due to security restrictions, you are not allowed to access 'Companies' (res.company) records.
    
    Records: YourCompany (id=1)
    User: Marc Demo (id=6)
    
    This restriction is due to the following rules:
    - company rule employee
    
    Note: this might be a multi-company issue.
    
    Contact your administrator to request access if necessary.
    """
    
    This occurs because the accounting info is taken from the parent company,
    but the branch user has no access to the parent company records
    
    opw-3627439
    
    X-original-commit: 735f75073b039048859b44f9b11d58ffdfe3c28b

--- addons/account/models/account_tax.py
+++ addons/account/models/account_tax.py
@@ -531,7 +531,7 @@ class AccountTax(models.Model):
                 name += ' (%s)' % tax_scope.get(record.tax_scope)
             if len(self.env.companies) > 1 and self.env.context.get('params', {}).get('model') == 'product.template':
                 name += ' (%s)' % record.company_id.display_name
-            if record.country_id != record.company_id.account_fiscal_country_id:
+            if record.country_id != record.company_id._accessible_branches()[:1].account_fiscal_country_id:
                 name += ' (%s)' % record.country_code
             record.display_name = name
 
