d765e19ecf6706b30c3284abd8d0cb607d482c0b
Nasreddin Boulif (bon) <bon@odoo.com>
2023-12-26 18:27:16 +0100

    [FIX] mail: set default values for suggested_recipients
    Steps to reproduce:
    
      - Install `CRM` module (for test purposes)
      - Create a lead and set the name, email, phone, company name and
        contact name
      - Save the lead
      - Click on `Send message` button in the chatter
      - Uncheck/Check the recipient checkbox
      - Save the partner
      - Discard the changes on the lead and open the partner created
    
    Issue:
    
      - In the wizard, the `phone` value is not set with default value and
        the name is set with the company name instead of the contact name
      - On the partner created, the company and phone are not set
    
    Cause:
    
      The default values (used in the partner creation wizard) retrieved
      from "/mail/thread/data" route are not set on the recipient object.
    
    opw-3512045

--- addons/mail/static/src/web/thread_service_patch.js
+++ addons/mail/static/src/web/thread_service_patch.js
@@ -149,10 +149,10 @@ patch(ThreadService.prototype, "mail/web", {
     async insertSuggestedRecipients(thread, dataList) {
         const recipients = [];
         for (const data of dataList) {
-            const [partner_id, emailInfo, lang, reason, customerInfo] = data;
+            const [partner_id, emailInfo, lang, reason, defaultCreateValues] = data;
             let [name, email] = emailInfo ? parseEmail(emailInfo) : [];
-            if ((!name || name === email) && customerInfo?.name) {
-                name = customerInfo.name;
+            if ((!name || name === email) && defaultCreateValues?.name) {
+                name = defaultCreateValues.name;
             }
             recipients.push({
                 id: nextId++,
@@ -167,6 +167,7 @@ patch(ThreadService.prototype, "mail/web", {
                       })
                     : false,
                 checked: true,
+                defaultCreateValues,
             });
         }
         thread.suggestedRecipients = recipients;
