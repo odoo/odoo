c1f2fa122e39c14f999272f17d0cdfffb45baeb9
Milwell <milwell@milwell.com>
2023-12-27 12:49:50 +0800

    [FIX] check payment prevent delete when posted once or state is posted

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -1045,16 +1045,14 @@ class AccountPayment(models.Model):
     
     @api.ondelete(at_uninstall=False)
     def _unlink_if_posted(self):
-        posted_payments = self.filtered(lambda a:a.state == 'posted')
+        posted_payments = self.filtered(lambda a: a.state == 'posted')
         if posted_payments:
             raise ValidationError("Cannot delete a posted payment.  Set to draft then cancel instead")
         moves = self.mapped('move_id')
-        moves_posted_before = moves.filtered(lambda a:a.posted_before)
+        moves_posted_before = moves.filtered(lambda a: a.posted_before)
         if moves_posted_before:
             raise ValidationError("Cannot delete a payment that was posted once.  You can cancel")
     
-
-
 # For optimization purpose, creating the reverse relation of m2o in _inherits saves
 # a lot of SQL queries
 class AccountMove(models.Model):
