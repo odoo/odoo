284687f9deea4acf68eff48942030f395102b2f7
moerradi <moer@odoo.com>
2023-12-28 11:04:11 +0000

    [FIX] account: Payment terms labels not displaying correctly
    This commit addresses an issue where, upon application to an invoice,
    the Receivables, while accurate in terms of Amounts and Due Dates,
    have incorrect labels.
    The root cause of this problem was identified in the compute_name method
     of the account.move.line model. The modifications in this commit
    rectify the compute_name method to ensure that payment terms labels are
    computed correctly.
    
    task-3636484
    
    closes odoo/odoo#153364
    
    X-original-commit: 59c0350b55b11321ba49e1ae4124eb5828d5f665
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -295,6 +295,7 @@ class AccountMove(models.Model):
         comodel_name='account.payment.term',
         string='Payment Terms',
         compute='_compute_invoice_payment_term_id', store=True, readonly=False, precompute=True,
+        inverse='_inverse_invoice_payment_term_id',
         check_company=True,
     )
     needed_terms = fields.Binary(compute='_compute_needed_terms', exportable=False)
@@ -1706,9 +1707,16 @@ class AccountMove(models.Model):
             or m.journal_id.currency_id and m.currency_id != m.journal_id.currency_id
         ))
 
+    @api.onchange('payment_reference')
     def _inverse_payment_reference(self):
         self.line_ids._conditional_add_to_compute('name', lambda line: (
             line.display_type == 'payment_term'
+        ))
+
+    @api.onchange('invoice_payment_term_id')
+    def _inverse_invoice_payment_term_id(self):
+        self.line_ids._conditional_add_to_compute('name', lambda l: (
+            l.display_type == 'payment_term'
         ))
 
     def _inverse_name(self):
