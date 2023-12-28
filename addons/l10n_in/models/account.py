ce03735df34a4110ad174328137f4e09515d47ee
Jitendra Prajapat <jipr@odoo.com>
2023-12-28 10:53:54 +0530

    [IMP] l10n_in: hsn compliance value to be stored at Invoice Level and POS line
    Before this commit
    ==================
    HSN Codes are mapped to Product and same Carried to the various reporting level.
    
    After this commit
    =================
    HSN Codes are stored at invoice level and displayed in invoice lines and POS line
    
    Related Enterprise PR: https://github.com/odoo/enterprise/pull/53437
    Related Upgrade PR: https://github.com/odoo/upgrade/pull/5474
    
    task- 3410236

--- addons/l10n_in/models/account.py
+++ addons/l10n_in/models/account.py
@@ -9,6 +9,16 @@ from odoo import tools
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
+    l10n_in_hsn_code = fields.Char(string="HSN/SAC Code", compute="_compute_l10n_in_hsn_code", store=True, readonly=False, copy=False)
+
+    @api.depends('product_id')
+    def _compute_l10n_in_hsn_code(self):
+        indian_lines = self.filtered(lambda line: line.company_id.account_fiscal_country_id.code == 'IN')
+        (self - indian_lines).l10n_in_hsn_code = False
+        for line in indian_lines:
+            if line.product_id:
+                line.l10n_in_hsn_code = line.product_id.l10n_in_hsn_code
+
     def init(self):
         tools.create_index(self._cr, 'account_move_line_move_product_index', self._table, ['move_id', 'product_id'])
 
