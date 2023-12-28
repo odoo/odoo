a61410d29059b5df4998b3eddd123e5686ceb1e6
Sébastien Theys <seb@odoo.com>
2023-12-28 15:08:56 +0100

    [REF] mail: add prop for `onActivityChanged`
    This makes the `load` method only used internally by chatter, making it
    easier to refactor in future commits.
    
    Also make the prop required on Activity as it is always given.
    
    Part-of: odoo/odoo#149593

--- addons/mail/static/src/core/web/activity.js
+++ addons/mail/static/src/core/web/activity.js
@@ -18,14 +18,13 @@ import { FileUploader } from "@web/views/fields/file_handler";
 /**
  * @typedef {Object} Props
  * @property {import("models").Activity} data
- * @property {function} [onUpdate]
+ * @property {function} onActivityChanged
  * @property {function} reloadParentView
  * @extends {Component<Props, Env>}
  */
 export class Activity extends Component {
     static components = { ActivityMailTemplate, FileUploader };
-    static props = ["data", "onUpdate?", "reloadParentView"];
-    static defaultProps = { onUpdate: () => {} };
+    static props = ["data", "onActivityChanged", "reloadParentView"];
     static template = "mail.Activity";
 
     /** @type {function} */
@@ -81,7 +80,7 @@ export class Activity extends Component {
         this.popover.open(ev.currentTarget, {
             activity: this.props.data,
             hasHeader: true,
-            reload: this.props.onUpdate,
+            reload: this.props.onActivityChanged,
         });
     }
 
@@ -89,7 +88,7 @@ export class Activity extends Component {
         const thread = this.thread;
         const { id: attachmentId } = await this.attachmentUploader.uploadData(data);
         await this.activityService.markAsDone(this.props.data, [attachmentId]);
-        this.props.onUpdate(thread);
+        this.props.onActivityChanged(thread);
         await this.threadService.fetchNewMessages(thread);
     }
 
@@ -106,14 +105,14 @@ export class Activity extends Component {
         const thread = this.thread;
         const id = this.props.data.id;
         await this.env.services["mail.activity"].edit(id);
-        this.props.onUpdate(thread);
+        this.props.onActivityChanged(thread);
     }
 
     async unlink() {
         const thread = this.thread;
         this.activityService.delete(this.props.data);
         await this.env.services.orm.unlink("mail.activity", [this.props.data.id]);
-        this.props.onUpdate(thread);
+        this.props.onActivityChanged(thread);
     }
 
     get thread() {
