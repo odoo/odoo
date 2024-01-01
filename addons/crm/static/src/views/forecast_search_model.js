eacc86783835263e5784f60b181d189fc03a7cca
OmarAbosamaha <abom@odoo.com>
2024-01-01 16:29:22 +0200

    [FIX] crm: upgrade from moment to luxon
    Issue:
    When trying to view the forecast report in arabic, Odoo gives an error.
    This is because when using moment to calculate the forecast start date, it is calculated in arabic.
    Therefore, sending it to the filter query raises an error as it has an invalid date format.
    
    Steps to reproduce:
    1- Install CRM app
    2- Install Arabic language and switch to arabic
    3- Open CRM App
    4- from reports menu choose forecast
    
    Solution:
    Replacing moment with a call to luxon library solves the issue as in v17.0 following this commit: https://github.com/odoo/odoo/pull/109882/commits/09c968a8cd15cc815aed1d7db37e5939c8410086
    
    opw-3658305

--- addons/crm/static/src/views/forecast_search_model.js
+++ addons/crm/static/src/views/forecast_search_model.js
@@ -3,15 +3,12 @@
 import { Domain } from "@web/core/domain";
 import { makeContext } from "@web/core/context";
 import { SearchModel } from "@web/search/search_model";
+import { serializeDate, serializeDateTime } from "@web/core/l10n/dates";
 
 /**
  * This is the conversion of ForecastModelExtension. See there for more
  * explanations of what is done here.
  */
-const DATE_FORMAT = {
-    datetime: "YYYY-MM-DD HH:mm:ss",
-    date: "YYYY-MM-DD",
-};
 
 export class ForecastSearchModel extends SearchModel {
     /**
@@ -65,9 +62,7 @@ export class ForecastSearchModel extends SearchModel {
      */
     _getForecastStart(forecastField) {
         if (!this.forecastStart) {
-            /** @todo stop using moment */
             const { type } = this.searchViewFields[forecastField];
-            let startMoment;
             const groupBy = this.groupBy;
             const firstForecastGroupBy = groupBy.find((gb) => gb.includes(forecastField));
             let granularity = "month";
@@ -76,12 +71,8 @@ export class ForecastSearchModel extends SearchModel {
             } else if (groupBy.length) {
                 granularity = "day";
             }
-            startMoment = moment().startOf(granularity);
-            if (type === "datetime") {
-                startMoment = moment.utc(startMoment);
-            }
-            const format = DATE_FORMAT[type];
-            this.forecastStart = startMoment.format(format);
+            const startDateTime = luxon.DateTime.now().startOf(granularity);
+            this.forecastStart = type === "datetime" ? serializeDateTime(startDateTime) : serializeDate(startDateTime);
         }
         return this.forecastStart;
     }
