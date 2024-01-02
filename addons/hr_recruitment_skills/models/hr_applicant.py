4d53e5f4d7e0cf61a79838a0e71306d982242154
Yash Vaishnav <yava@odoo.com>
2024-01-02 15:22:01 +0530

    [IMP] hr_recruitment(_skills): created the matching and missing skills fields
    In this commit,
    - Now, the form-view of the job position has the Expected Skills field in which
    we can define the skills for the job position
    - Search Matching Applicants action will search across all the applicants and
    give the matched applicant based on the expected skills
    - Add to Job button is added to the list view of the Search Matching Applicants
    while clicking upon the add to job will change the job position of the applicant
    to the current
    - Missing Skills, Matching Score, and Matching Skills are added to the list view
    of the applicant
    -Applicants in the pipeline do not receive an acknowledgment email anymore
    
    task-3636002
    
    closes odoo/odoo#147559
    
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/hr_recruitment_skills/models/hr_applicant.py
+++ addons/hr_recruitment_skills/models/hr_applicant.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from ast import literal_eval
 
 from odoo import fields, models, api
 
@@ -10,6 +11,9 @@ class HrApplicant(models.Model):
     applicant_skill_ids = fields.One2many('hr.applicant.skill', 'applicant_id', string="Skills")
     skill_ids = fields.Many2many('hr.skill', compute='_compute_skill_ids', store=True)
     is_interviewer = fields.Boolean(compute='_compute_is_interviewer')
+    matching_skill_ids = fields.Many2many(comodel_name='hr.skill', string="Matching Skills", compute="_compute_matching_skill_ids")
+    missing_skill_ids = fields.Many2many(comodel_name='hr.skill', string="Missing Skills", compute="_compute_matching_skill_ids")
+    matching_score = fields.Float(string="Matching Score(%)", compute="_compute_matching_skill_ids")
 
     @api.depends_context('uid')
     @api.depends('interviewer_ids', 'job_id.interviewer_ids')
@@ -22,6 +26,20 @@ class HrApplicant(models.Model):
     def _compute_skill_ids(self):
         for applicant in self:
             applicant.skill_ids = applicant.applicant_skill_ids.skill_id
+
+    @api.depends_context('active_id')
+    @api.depends('skill_ids')
+    def _compute_matching_skill_ids(self):
+        job_id = self.env.context.get('active_id')
+        if not job_id:
+            self.matching_skill_ids = False
+            self.matching_score = 0
+        else:
+            for applicant in self:
+                job_skills = self.env['hr.job'].browse(job_id).skill_ids
+                applicant.matching_skill_ids = job_skills & applicant.skill_ids
+                applicant.missing_skill_ids = job_skills - applicant.skill_ids
+                applicant.matching_score = (len(applicant.matching_skill_ids) / len(job_skills)) * 100 if job_skills else 0
 
     def _get_employee_create_vals(self):
         vals = super()._get_employee_create_vals()
@@ -45,3 +63,12 @@ class HrApplicant(models.Model):
             } for skill in skills_to_create])
         self.env['hr.employee.skill'].create(vals_list)
         return super()._update_employee_from_applicant()
+
+    def action_add_to_job(self):
+        self.with_context(just_moved=True).write({
+            'job_id': self.env['hr.job'].browse(self.env.context.get('active_id')).id,
+            'stage_id': self.env.ref('hr_recruitment.stage_job0'),
+        })
+        action = self.env['ir.actions.actions']._for_xml_id('hr_recruitment.action_hr_job_applications')
+        action['context'] = literal_eval(action['context'].replace('active_id', str(self.job_id.id)))
+        return action
