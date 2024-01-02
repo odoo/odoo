8ea9fc23832faca0f3698667eb76ff4d64ce62ca
Mattis Megevand (mmeg) <mmeg@odoo.com>
2024-01-02 14:01:59 +0100

    [FIX] purchase: always display inactive taxes on purchase order
    Steps to reproduce:
    
    - Create a PO with a tax
    - Confirm PO and bill it
    - Deactivate tax
    
    Issue:
    Tax is shown on bill but not on PO
    
    Solution:
    Make sure that we display tax even if inactive.
    
    opw-3631087
    
    X-original-commit: 4e424750eccff168c3cbb1a3f8411b35905717f3

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -968,7 +968,7 @@ class PurchaseOrderLine(models.Model):
         string='Expected Arrival', index=True,
         compute="_compute_price_unit_and_date_planned_and_name", readonly=False, store=True,
         help="Delivery date expected from vendor. This date respectively defaults to vendor pricelist lead time then today's date.")
-    taxes_id = fields.Many2many('account.tax', string='Taxes', domain=['|', ('active', '=', False), ('active', '=', True)])
+    taxes_id = fields.Many2many('account.tax', string='Taxes', domain=['|', ('active', '=', False), ('active', '=', True)], context={'active_test': False})
     product_uom = fields.Many2one('uom.uom', string='Unit of Measure', domain="[('category_id', '=', product_uom_category_id)]")
     product_uom_category_id = fields.Many2one(related='product_id.uom_id.category_id')
     product_id = fields.Many2one('product.product', string='Product', domain=[('purchase_ok', '=', True)], change_default=True, index='btree_not_null')
