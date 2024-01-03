3273f028b8db65e48ebc9849cc92d681ca471675
Djamel Touati <otd@odoo.com>
2024-01-03 22:42:39 +0100

    [FIX] sale_stock: avoid traceback when returning a product
    Steps to reproduce the bug:
    - Create a storable product “P1”:
        - Tracking: SN
        - Update the qty: 1 unit, “SN1”
    
    - Create a SO with and validate it
    - Go to the delivery and validate it
    - Create a return and validate
    - Create a return of return
    
    Problem:
    error message: RecursionError: maximum recursion depth exceeded while
    calling a Python object
    
    The `_get_origin_moves` and `_get_destination_moves` functions add the
    list of origin or destination moves, but the functions themselves are
    called with that list, creating an infinite loop.
    
    opw-3659818
    
    closes odoo/odoo#148031
    
    Related: odoo/enterprise#53695
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -2143,6 +2143,7 @@ Please change the quantity done or the rounding precision of your unit of measur
         moves_to_reserve._action_assign()
 
     def _rollup_move_dests(self, seen=False):
+        self.ensure_one()
         if not seen:
             seen = OrderedSet()
         if self.id in seen:
@@ -2153,6 +2154,7 @@ Please change the quantity done or the rounding precision of your unit of measur
         return seen
 
     def _rollup_move_origs(self, seen=False):
+        self.ensure_one()
         if not seen:
             seen = OrderedSet()
         if self.id in seen:
