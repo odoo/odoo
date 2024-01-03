d32fbcfb67f9fa08849cf6b29d5a17a743b0b0d0
Alexandre Kühn <aku@odoo.com>
2024-01-03 17:42:03 +0100

    [FIX] mail: 'Logged _in_ as' on welcome page
    Label of guest name on public discuss welcome page
    was gramatically incorrect. It should be "Logged in as"
    rather than "Logged as".
    
    Also took the opportunity to properly translate the label.

--- addons/mail/static/src/components/welcome_view/welcome_view.js
+++ addons/mail/static/src/components/welcome_view/welcome_view.js
@@ -4,6 +4,9 @@ import { registerMessagingComponent } from '@mail/utils/messaging_component';
 import { useRefToModel } from '@mail/component_hooks/use_ref_to_model/use_ref_to_model';
 import { useUpdateToModel } from '@mail/component_hooks/use_update_to_model/use_update_to_model';
 
+import { _t } from "@web/core/l10n/translation";
+import { sprintf } from "@web/core/utils/strings";
+
 export class WelcomeView extends owl.Component {
 
     /**
@@ -22,6 +25,9 @@ export class WelcomeView extends owl.Component {
         return this.messaging && this.messaging.models['mail.welcome_view'].get(this.props.localId);
     }
 
+    get loggedInAsText() {
+        return sprintf(_t("Logged in as %s"), this.messaging.currentUser.nameOrDisplayName);
+    }
 }
 
 Object.assign(WelcomeView, {
