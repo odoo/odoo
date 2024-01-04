7fd25cf659bc8ae8a4e27207adf466759b866773
FrancoisGe <fge@odoo.com>
2024-01-04 09:51:53 +0000

    [FIX] web: originalError could be null in formSaveErrorHandler
    In formSaveErrorHandler, we think that originalError is always defined.
    But the originalError could be null or undefined. in this case,
    that will cause a crash, so we need to check if originalError is defined.
    
    The crash only occurs when the error is not an UncaughtPromiseError
    (because the legacyRejectPromiseHandler catches them all). So, for example,
    you need have a ScriptError to reproduce the crash.
    
    X-original-commit: 5633a5712d352d4871d8e12ff4b041c8f85c0fa7

--- addons/web/static/src/views/form/form_error_dialog/form_error_dialog.js
+++ addons/web/static/src/views/form/form_error_dialog/form_error_dialog.js
@@ -17,3 +17,63 @@ export class FormErrorDialog extends Component {
 }
 FormErrorDialog.template = "web.FormErrorDialog";
 FormErrorDialog.components = { Dialog };
+<<<<<<< HEAD
+||||||| parent of f869443be7d2 (temp)
+
+function formSaveErrorHandler(env, error, originalError) {
+    if (originalError.__raisedOnFormSave) {
+        const event = originalError.event;
+        error.unhandledRejectionEvent.preventDefault();
+        if (event.isDefaultPrevented()) {
+            // in theory, here, event was already handled
+            return true;
+        }
+        event.preventDefault();
+
+        env.services.dialog.add(
+            FormErrorDialog,
+            {
+                message: originalError.message.message,
+                data: originalError.message.data,
+                onDiscard: originalError.onDiscard,
+                onStayHere: originalError.onStayHere,
+            },
+            {
+                onClose: originalError.onStayHere,
+            }
+        );
+
+        return true;
+    }
+}
+errorHandlerRegistry.add("formSaveErrorHandler", formSaveErrorHandler);
+=======
+
+function formSaveErrorHandler(env, error, originalError) {
+    if (originalError && originalError.__raisedOnFormSave) {
+        const event = originalError.event;
+        error.unhandledRejectionEvent.preventDefault();
+        if (event.isDefaultPrevented()) {
+            // in theory, here, event was already handled
+            return true;
+        }
+        event.preventDefault();
+
+        env.services.dialog.add(
+            FormErrorDialog,
+            {
+                message: originalError.message.message,
+                data: originalError.message.data,
+                onDiscard: originalError.onDiscard,
+                onStayHere: originalError.onStayHere,
+            },
+            {
+                onClose: originalError.onStayHere,
+            }
+        );
+
+        return true;
+    }
+}
+errorHandlerRegistry.add("formSaveErrorHandler", formSaveErrorHandler);
+>>>>>>> f869443be7d2 (temp)
