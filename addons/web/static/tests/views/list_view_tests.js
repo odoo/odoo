20be068d3efc0b27f9ef99a43c1d143817dec137
FrancoisGe <fge@odoo.com>
2024-01-04 11:49:30 +0100

    [FIX] web: header button with display="always" and invisible
    Since the commit odoo/odoo@0d99f4b9d9de404fea44ecf5480ec3b07c2b0fd6, the header buttons with display="always"
    can't be invisible. The invisible attribute is ignored for this type of
    button in list view and kanban view.
    
    This commit fixes this issue by adding t-if that will evaluate the invisible attribute.
    
    X-original-commit: e0109a99f87f5d9eabe024526491bd90750462e3

--- addons/web/static/tests/views/list_view_tests.js
+++ addons/web/static/tests/views/list_view_tests.js
@@ -1030,10 +1030,15 @@ QUnit.module("Views", (hooks) => {
                 <tree>
                     <header>
                         <button name="display" type="object" class="display" string="display" display="always"/>
+                        <button name="display" type="object" class="display_invisible" string="invisible 1" display="always" invisible="1"/>
+                        <button name="display" type="object" class="display_invisible" string="invisible context" display="always" invisible="context.get('a')"/>
                         <button name="default-selection" type="object" class="default-selection" string="default-selection"/>
                     </header>
                     <field name="foo" />
                 </tree>`,
+                context: {
+                    a: true,
+                },
             });
             let cpButtons = getVisibleButtons(target);
             assert.deepEqual(
