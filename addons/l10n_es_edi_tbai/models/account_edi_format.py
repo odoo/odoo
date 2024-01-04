46a8cda75f110c9fa3bdccc5322c1a2aa9f3a466
Sven Fuehr <svfu@odoo.com>
2024-01-04 08:43:18 +0100

    [FIX] l10n_es_edi_tbai: fix unit price, discount in multicurrency export
    Currently there are the following bugs when XML-exporting invoices
    that are not in company currency and use discounts:
      * The discount is computed and exported in document currency.
        But it should be exported in company currency.
      * The unit price value (excl. discount) is computed wrongly
        since it uses the wrong discount value.
    
    This commit corrects the discount and unit price computation / export.
    
    closes odoo/odoo#148566
    
    X-original-commit: 1cc846e8a53fd7c4298b7324d9c4f272592f0497
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>

--- addons/l10n_es_edi_tbai/models/account_edi_format.py
+++ addons/l10n_es_edi_tbai/models/account_edi_format.py
@@ -330,10 +330,11 @@ class AccountEdiFormat(models.Model):
         invoice_lines = []
         for line in invoice.invoice_line_ids.filtered(lambda line: line.display_type not in ('line_section', 'line_note')):
             if line.discount == 100.0:
-                gross_price_subtotal = line.price_unit * line.quantity
+                inverse_currency_rate = abs(line.move_id.amount_total_signed / line.move_id.amount_total) if line.move_id.amount_total else 1
+                balance_before_discount = - line.price_unit * line.quantity * inverse_currency_rate
             else:
-                gross_price_subtotal = line.price_subtotal / (1 - line.discount / 100.0)
-            discount = (gross_price_subtotal - line.price_subtotal) * refund_sign
+                balance_before_discount = line.balance / (1 - line.discount / 100)
+            discount = (balance_before_discount - line.balance)
 
             if not any([t.l10n_es_type == 'sujeto_isp' for t in line.tax_ids]):
                 total = line.price_total * abs(line.balance / line.amount_currency if line.amount_currency != 0 else 1) * -refund_sign
