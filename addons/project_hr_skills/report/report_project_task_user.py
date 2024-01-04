be5f779cecab20779e20a1588ed08ba233f82c26
Vincent Larcin <vila@odoo.com>
2024-01-04 14:21:20 +0100

    [FIX] project_hr_skills: use correct model in related fields
    This commit fixes two related fields using the wrong model.

--- addons/project_hr_skills/report/report_project_task_user.py
+++ addons/project_hr_skills/report/report_project_task_user.py
@@ -5,4 +5,4 @@ from odoo import fields, models
 class ReportProjectTaskUser(models.Model):
     _inherit = 'report.project.task.user'
 
-    user_skill_ids = fields.One2many('hr.skill', related='user_ids.employee_skill_ids', string='Skills')
+    user_skill_ids = fields.One2many('hr.employee.skill', related='user_ids.employee_skill_ids', string='Skills')
