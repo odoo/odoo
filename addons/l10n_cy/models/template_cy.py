71d4c0809b85096ad7a091b6cd37309ca3c4b0b9
AllePilli <peal@odoo.com>
2024-01-05 16:37:09 +0000

    [ADD] l10n_cy: Basic package
    This commit will add the basic package for the Cyprus localization.
    - Default Settings
    - CAO
    - Taxes
    - Fiscal positions
    - Tax Report
    
    closes odoo/odoo#150175
    
    Task: 3640438
    Related: odoo/enterprise#54620
    Signed-off-by: Quentin De Paoli <qdp@odoo.com>
    (cherry picked from commit 2c82126b23a29955acf0bf8c5f05014a71cba081)
    
    X-original-commit: 1db47a80dfd51f1b5c1463bf7891b7e0bc84a727

--- addons/l10n_cy/models/template_cy.py
+++ addons/l10n_cy/models/template_cy.py
@@ -1,6 +1,7 @@
 from odoo import models
 from odoo.addons.account.models.chart_template import template
 
+
 class AccountChartTemplate(models.AbstractModel):
     _inherit = 'account.chart.template'
 
