8bae9832c0ef0e6bf847ca95e44d25200b1f6c6c
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2024-01-05 14:17:39 +0100

    [IMP] project: Tracking Revisions in Tasks
    Before this commit:
    When many people work on the same task, important details can get lost or misplaced, causing confusion.
    
    After this commit:
    The generic history wizard was applied on project.task to review changes and allow users to go back to previous versions.
    
    task-3617834
    
    Part-of: odoo/odoo#145459

--- addons/project/static/src/views/project_task_form/project_task_form_controller.js
+++ addons/project/static/src/views/project_task_form/project_task_form_controller.js
@@ -2,8 +2,26 @@
 
 import { FormController } from '@web/views/form/form_controller';
 import { DeleteSubtasksConfirmationDialog } from "@project/components/delete_subtasks_confirmation_dialog/delete_subtasks_confirmation_dialog";
+import { _t } from "@web/core/l10n/translation";
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import HistoryDialog from '@web_editor/components/history_dialog/history_dialog';
 
 export class ProjectTaskFormController extends FormController {
+    /**
+     * @override
+     */
+    getStaticActionMenuItems() {
+        return {
+            ...super.getStaticActionMenuItems(),
+            openHistoryDialog: {
+                sequence: 50,
+                icon: "fa fa-history",
+                description: _t("Restore History"),
+                callback: () => this.openHistoryDialog(),
+            },
+        };
+    }
+
     deleteRecord() {
         if (!this.model.root.data.subtask_count) {
             return super.deleteRecord();
@@ -16,5 +34,38 @@ export class ProjectTaskFormController extends FormController {
                 }
             },
         });
+    }
+
+    async openHistoryDialog() {
+        const record = this.model.root;
+        const versionedFieldName = 'description';
+        const historyMetadata = record.data["html_field_history_metadata"]?.[versionedFieldName];
+        if (!historyMetadata) {
+            return;
+        }
+
+        this.dialogService.add(
+            HistoryDialog,
+            {
+                recordId: this.props.resId,
+                recordModel: this.props.resModel,
+                versionedFieldName,
+                historyMetadata,
+                restoreRequested: (html) => {
+                    this.dialogService.add(ConfirmationDialog, {
+                        title: _t("Are you sure you want to restore this version ?"),
+                        body: _t("Restoring will replace the current content with the selected version. Any unsaved changes will be lost."),
+                        confirm: () => {
+                            const restoredData = {};
+                            restoredData[versionedFieldName] = html;
+                            record.update(restoredData);
+                        },
+                        confirmLabel: _t("Restore"),
+                        cancel: () => {},
+                    });
+                },
+                close: () => {},
+            },
+        );
     }
 }
