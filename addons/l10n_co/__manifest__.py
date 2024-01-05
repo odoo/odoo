9a714b52410650260456e4058b0d516ee3251522
Sven Fuehr <svfu@odoo.com>
2024-01-05 15:33:02 +0100

    [FIX] l10n_co: necessary changes for EDI update to anexo 1.9
    The spec for electronic invoices in Colombia was updated and is now
    known as Anexo 1.9. This was done in the related enterprise PR (module l10n_co_edi).
    This commit introduces some changes in the base module that are needed
    for the Anexo 1.9 update.
    
    task-3639271
    
    closes odoo/odoo#163503
    
    X-original-commit: f7846ce90fe4522a74c183772af4353ded19db9e
    Related: odoo/enterprise#61531
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_co/__manifest__.py
+++ addons/l10n_co/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Colombia - Accounting',
     'icon': '/account/static/description/l10n.png',
     'countries': ['co'],
-    'version': '0.8',
+    'version': '0.9',
     'category': 'Accounting/Localizations/Account Charts',
     'description': 'Colombian Accounting and Tax Preconfiguration',
     'author': 'David Arnold (XOE Solutions)',
