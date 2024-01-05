5fa5216755cf50d1bc695131786a70858a91ca27
Arnold Moyaux <arm@odoo.com>
2024-01-05 11:49:57 +0100

    [FIX] mrp: sfp and picking before manufacturing always cancel
    It happens due to the new split functionality:
    - Create a MO for 2 units in 3 steps manufacturing
    - Split it in 2 MO of 1 unit
    - Cancel 1 MO.
    
    The SFP and PBM are both cancel (with 2 units each).
    It's always the case because they belong to the same procurement group
    and the system don't rely on the propagate cancel of rules.
    
    Adpat it to have a workaround

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -1513,8 +1513,6 @@ class MrpProduction(models.Model):
         finish_moves = self.move_finished_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
         raw_moves = self.move_raw_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
         (finish_moves | raw_moves)._action_cancel()
-        picking_ids = self.picking_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
-        picking_ids.action_cancel()
 
         for production, documents in documents_by_production.items():
             filtered_documents = {}
