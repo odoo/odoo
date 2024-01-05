cadc6ec2348b883d6c91792ce17a219c376c0c91
Vivek Pathak <vivp@odoo.com>
2024-01-05 04:42:17 +0000

    [FIX] l10n_in: resolves demo data install error
    This commit fixes the UserError raised when the demo data installs.
    The issue was due to an incorrect `company.country_id` and
    `country.state_id`.
    
    Task-3339099
    
    X-original-commit: 361c1dfeb1fdef0a05d38f854effc27f75985820

--- addons/l10n_in/demo/account_demo.py
+++ addons/l10n_in/demo/account_demo.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 import logging
@@ -610,3 +611,21 @@ class AccountChartTemplate(models.AbstractModel):
                         _logger.exception('Error while posting demo data')
         else:
             return super()._post_load_demo_data(company)
+||||||| parent of 4c37d058da40 (temp)
+=======
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, models
+class AccountChartTemplate(models.AbstractModel):
+    _inherit = "account.chart.template"
+
+    @api.model
+    def _get_demo_data(self):
+        company = self.env.company
+        if company.account_fiscal_country_id.code == "IN":
+            if company.state_id.country_id.code != "IN":
+                company.state_id = self.env.ref("base.state_in_gj")
+            if company.country_id.code != "IN":
+                company.country_id = self.env.ref("base.in")
+        return super()._get_demo_data()
+>>>>>>> 4c37d058da40 (temp)
