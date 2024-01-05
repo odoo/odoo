e145d3f79516975e0c306a9a5c338d8ee9ff6d3b
Aaron Bohy <aab@odoo.com>
2024-01-05 09:16:46 +0100

    [FIX] web: view compilers: use compiler in cache key
    Before this commit, the key for the cache of compiled templates
    (in the useViewCompiler hook) only relied on the arch, i.e. we had
    a cache hit if we had to compile an arch that had already been
    compiled before.
    
    However, the Compiler to use might change. When this happens, the
    template must be re-compiled, obviously, as the ouput may differ.
    
    For instance, have a kanban arch with a js_class (pointing to a
    custom view with a custom compiler), and have that arch used in an
    x2many (where js_class is ignored, i.e. where it is rendered with
    the basic kanban renderer). This happens in project, with the task
    kanban view, which is also used for the child_ids field, in the
    task form view.
    
    This could lead to traceback, as the compiled template might refer
    to attributes of a the KanbanRecord that only exist on the custom
    KanbanRecord, not on the basic one.
    
    Part-of: odoo/odoo#148351

--- addons/web/static/src/views/view_compiler.js
+++ addons/web/static/src/views/view_compiler.js
@@ -453,7 +453,7 @@ export function useViewCompiler(ViewCompiler, templates, params) {
     const compiledTemplates = {};
     let compiler;
     for (const tname in templates) {
-        const key = templates[tname].outerHTML;
+        const key = `${ViewCompiler.name}/${templates[tname].outerHTML}`;
         if (!templateCache[key]) {
             compiler = compiler || new ViewCompiler(templates);
             templateCache[key] = xml`${compiler.compile(tname, params).outerHTML}`;
