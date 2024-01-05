59225a982995e67982974c61f91e8d370730eb0f
Romain Derie <rde@odoo.com>
2024-01-05 16:54:34 +0000

    [FIX] website: properly filter website.page records on list/kanban view
    Since commit [1], which adapted the website pages list view to OWL, the
    records listed on screen are filtered according to the active website
    filter. However, the full list of records is still used behind the
    scenes for all potential actions.
    
    Steps to reproduce:
    1. Go to the pages list view.
    2. Select a specific website (if it's not already the case).
       => The total of records in the upper right corner does not match the
          number of pages on that specific website.
    3. Click on the "Select all" checkbox.
       => All the pages are selected, including those that do not appear on
       screen.
    
    This is because the records were just visually hidden with a `t-if`.
    
    [1]: https://github.com/odoo/odoo/commit/940f4ee875332dafa1f379970a7683be6b3ee606
    [2]: https://github.com/odoo/odoo/commit/db670f64f4c2190f1655f9077ea62885049a3c84
    
    Courtesy of @robinlej and @detrouxdev
    
    Related to task-3676124
    opw-3554064
    opw-3658648
    
    closes odoo/odoo#150555
    
    X-original-commit: 41e2c4859bb46190f1ebcdba9df950d8b2a282ca
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website/static/src/components/views/page_list.js
+++ addons/website/static/src/components/views/page_list.js
@@ -2,6 +2,7 @@
 
 import { _t } from "@web/core/l10n/translation";
 import {PageControllerMixin, PageRendererMixin} from "./page_views_mixin";
+import {PageSearchModel} from "./page_search_model";
 import {registry} from '@web/core/registry';
 import {listView} from '@web/views/list/list_view';
 import {ConfirmationDialog} from "@web/core/confirmation_dialog/confirmation_dialog";
@@ -96,6 +97,7 @@ export class PageListController extends PageControllerMixin(listView.Controller)
     }
 }
 
+// TODO master: remove `PageRendererMixin` extend and props override
 export class PageListRenderer extends PageRendererMixin(listView.Renderer) {
     static props = [...listView.Renderer.props, "activeWebsite"];
     static recordRowTemplate = "website.PageListRenderer.RecordRow";
@@ -105,6 +107,7 @@ export const PageListView = {
     ...listView,
     Renderer: PageListRenderer,
     Controller: PageListController,
+    SearchModel: PageSearchModel,
 };
 
 registry.category("views").add("website_pages_list", PageListView);
