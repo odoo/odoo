89c67b90125e51e7c14afd0fa4bf779ffb1f694a
Romain Derie <rde@odoo.com>
2024-01-05 17:54:34 +0100

    [FIX] website: properly filter website.page records on list/kanban view
    Note: This fw-port commit cherry-picked and squashed commit [2] directly
          as it was fixing this original commit before it had the chance to
          be forward ported.
    
    Since commit [1], which adapted the website pages list view to OWL, the
    records listed on screen are filtered according to the active website
    filter. However, the full list of records is still used behind the
    scenes for all potential actions.
    
    Steps to reproduce:
    1. Go to the pages list view.
    2. Select a specific website (if it's not already the case).
       => The total of records in the upper right corner does not match the
          number of pages on that specific website.
    3. Click on the "Select all" checkbox.
       => All the pages are selected, including those that do not appear on
       screen.
    
    This is because the records were just visually hidden with a `t-if`.
    
    [1]: https://github.com/odoo/odoo/commit/940f4ee875332dafa1f379970a7683be6b3ee606
    [2]: https://github.com/odoo/odoo/commit/db670f64f4c2190f1655f9077ea62885049a3c84
    
    Courtesy of @robinlej and @detrouxdev
    
    Related to task-3676124
    opw-3554064
    opw-3658648
    
    closes odoo/odoo#149517
    
    X-original-commit: 8d78a916dc8c0eca82e8a6a2c6f5541938709930
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website/static/src/components/views/page_kanban.js
+++ addons/website/static/src/components/views/page_kanban.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 import {PageControllerMixin, PageRendererMixin} from "./page_views_mixin";
+import {PageSearchModel} from "./page_search_model";
 import {registry} from '@web/core/registry';
 import {kanbanView} from "@web/views/kanban/kanban_view";
 import {SearchDropdownItem} from "@web/search/search_dropdown_item/search_dropdown_item";
@@ -20,6 +21,7 @@ PageKanbanController.components = {
     SearchDropdownItem,
 };
 
+// TODO master: remove `PageRendererMixin` extend, props override and template
 export class PageKanbanRenderer extends PageRendererMixin(kanbanView.Renderer) {}
 PageKanbanRenderer.props = [
     ...kanbanView.Renderer.props,
@@ -31,6 +33,7 @@ export const PageKanbanView = {
     ...kanbanView,
     Renderer: PageKanbanRenderer,
     Controller: PageKanbanController,
+    SearchModel: PageSearchModel,
 };
 
 registry.category("views").add("website_pages_kanban", PageKanbanView);
