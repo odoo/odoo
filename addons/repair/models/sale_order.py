0fbab7b03bc482de9378df0016b78b38ab804b33
Atul Patel <patelatul891@gmail.com>
2024-01-06 13:44:02 +0530

    [MOD] Formatted code

--- addons/repair/models/sale_order.py
+++ addons/repair/models/sale_order.py
@@ -69,7 +69,7 @@ class SaleOrderLine(models.Model):
     def write(self, vals_list):
         old_product_uom_qty = {line.id: line.product_uom_qty for line in self}
         res = super().write(vals_list)
-        for line in self.filtered(lambda x:x.state in ('sale', 'done') and not x.display_type):
+        for line in self.filtered(lambda x: x.state in ('sale', 'done') and not x.display_type):
             if float_compare(old_product_uom_qty[line.id], 0, precision_rounding=line.product_uom.rounding) <= 0 and float_compare(line.product_uom_qty, 0, precision_rounding=line.product_uom.rounding) > 0:
                 self._create_repair_order()
             if float_compare(old_product_uom_qty[line.id], 0, precision_rounding=line.product_uom.rounding) > 0 and float_compare(line.product_uom_qty, 0, precision_rounding=line.product_uom.rounding) <= 0:
