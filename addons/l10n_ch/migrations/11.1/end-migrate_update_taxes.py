d56bcd568515d6280474ae66186c33953759d5a7
Claire Bretton (clbr) <clbr@odoo.com>
2024-01-09 16:43:50 +0100

    [FIX] l10n_ch: fix data migration
    In a previous commit we fixed Swiss localization's taxes data.
    We changed two children taxes that had wrong tax_type_use since it depends
    on their parents.
    This could raise the python constraint `_check_children_scope()` which is preventing
    some databases to correctly update their taxes.
    
    See https://github.com/odoo/odoo/commit/7b07df873535446f97abc1de9176b9332de5cb07

--- addons/l10n_ch/migrations/11.1/end-migrate_update_taxes.py
+++ addons/l10n_ch/migrations/11.1/end-migrate_update_taxes.py
@@ -4,6 +4,18 @@ from odoo.addons.account.models.chart_template import update_taxes_from_template
 
 
 def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    # We had corrupted data, handle the correction so the tax update can proceed.
+    # See https://github.com/odoo/odoo/commit/7b07df873535446f97abc1de9176b9332de5cb07
+    taxes_to_check = ('vat_purchase_81_return', 'vat_77_purchase_return')
+    tax_ids = env['ir.model.data'].search([('name', 'in', taxes_to_check)]).mapped('res_id')
+    for tax in env['account.tax'].browse(tax_ids):
+        for child in tax.children_tax_ids:
+            if child.type_tax_use not in ('none', tax.type_tax_use):
+                # set the child to it's parent's value
+                child.type_tax_use = tax.type_tax_use
+
+    # Update taxes
     new_template_to_tax = update_taxes_from_templates(cr, 'l10n_ch.l10nch_chart_template')
     if new_template_to_tax:
         _, new_tax_ids = zip(*new_template_to_tax)
