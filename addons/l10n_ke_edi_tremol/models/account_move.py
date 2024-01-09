d069e03d71fe4cdef1a9c0f1c43e3c06310b72c2
Daniel Kosky (dako) <dako@odoo.com>
2024-01-09 11:08:41 +0100

    [FIX] l10n_ke_edi_tremol: fix reference to prepare_edi_tax_details
    A recent commit (03466c2) introduced a change that allowed for users to
    place multiple taxes on a line (provided only one of them was a VAT
    tax) when sending invoices to the device. This made use of the
    _prepare_edi_tax_details method, which is only available from the
    account_edi module.
    
    This fix adapts the function to make use of the
    _pepare_invoice_aggregated_taxes method (available from) the account
    move model instead. The references to the keys in the returned tax
    details dictionary have also be corrected (since the structure of the
    dictionary between from version 16 onwards).
    
    closes odoo/odoo#148635
    
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_ke_edi_tremol/models/account_move.py
+++ addons/l10n_ke_edi_tremol/models/account_move.py
@@ -165,14 +165,14 @@ class AccountMove(models.Model):
 
         vat_class = {16.0: 'A', 8.0: 'B'}
         msgs = []
-        tax_details = self._prepare_edi_tax_details()
+        tax_details = self._prepare_invoice_aggregated_taxes()
         for line in self.invoice_line_ids.filtered(lambda l: l.display_type == 'product' and l.quantity and l.price_total > 0 and not discount_dict.get(l.id) >= 100):
             # Here we use the original discount of the line, since it the distributed discount has not been applied in the price_total
             price_total = 0
             percentage = 0
-            for tax in tax_details['invoice_line_tax_details'][line]['tax_details']:
+            for tax in tax_details['tax_details_per_record'][line]['tax_details']:
                 if tax['tax'].amount in (16, 8, 0): # This should only occur once
-                    tax_details = tax_details['invoice_line_tax_details'][line]['tax_details'][tax]
+                    tax_details = tax_details['tax_details_per_record'][line]['tax_details'][tax]
                     price_total = abs(tax_details['base_amount_currency']) + abs(tax_details['tax_amount_currency'])
                     percentage = tax['tax'].amount
 
