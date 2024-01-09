c057cc356ebef057a234f02c61eca708b258f87c
adsh-odoo <adsh@odoo.com>
2024-01-09 12:06:03 +0530

    [IMP] spreadsheet_dashboard: change category
    Purpose
    ===========
    Change the category of the the Dashboard.
    
    Specification
    =================
    Move Dashboard to Productivity (last).
    
    Technical
    ==========
    In the module category record, we utilize the "base" prefix to ensure that the
    sequence specified within this record is honored. Omitting the "base" prefix
    would result in the sequence not being respected. This convention is
    consistently applied throughout the entire Odoo code base.
    
    Upgrade Pr-https://github.com/odoo/upgrade/pull/5838
    
    Task-3612501
    
    closes odoo/odoo#151051
    
    Related: odoo/upgrade#5838
    Related: odoo/enterprise#55109
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- addons/spreadsheet_dashboard/__manifest__.py
+++ addons/spreadsheet_dashboard/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Spreadsheet dashboard",
     "version": "1.0",
-    "category": "Hidden",
+    "category": "Productivity/Dashboard",
     "summary": "Spreadsheet",
     "description": "Spreadsheet",
     "depends": ["spreadsheet"],
