96b41355fd82de29f97dae61cac32397e24ef0d0
Alexandre Kühn <aku@odoo.com>
2024-01-10 16:50:37 +0100

    [FIX] mail: no crash on edit message in mobile
    Before this commit, editing a message in message in a conversation
    and then pressing on the paper plane icon was throwing the following
    error:
    
    ```
    TypeError: null is not an object (evaluating 'thread.model')
    @_sendMessage
    ```
    
    This happens because this button was in "send" message mode rather
    than in edit mode. Since we cannot post a message while editing a
    message, the crash happens because composer is not related to thread
    but the message.
    
    This button is not visible in desktop mode, and user should use
    suggested keyboard shortcuts or click on links to either save edit or
    discard it. In mobile, these interactions are not intuitive.
    
    This commit fixes the issue by keeping the paper-plane button while
    editing in mobile but it acts as "save". The labels to save/discard
    was not appropriate in mobile, so it has been turned to a simple
    button to discard editing.
    
    opw-3670482
    
    closes odoo/odoo#148918
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/tests/message/message_tests.js
+++ addons/mail/static/tests/message/message_tests.js
@@ -9,6 +9,7 @@ import { deserializeDateTime } from "@web/core/l10n/dates";
 import { getOrigin } from "@web/core/utils/urls";
 import { makeDeferred, patchWithCleanup, triggerHotkey } from "@web/../tests/helpers/utils";
 import { click, contains, insertText } from "@web/../tests/utils";
+import { SIZES, patchUiSize } from "../helpers/patch_ui_size";
 
 const { DateTime } = luxon;
 
@@ -32,6 +33,38 @@ QUnit.test("Start edition on click edit", async () => {
     await click(".o-mail-Message [title='Expand']");
     await click(".o-mail-Message [title='Edit']");
     await contains(".o-mail-Message-editable .o-mail-Composer-input", { value: "Hello world" });
+});
+
+QUnit.test("Edit message (mobile)", async () => {
+    patchUiSize({ size: SIZES.SM });
+    const pyEnv = await startServer();
+    const channelId = pyEnv["discuss.channel"].create({
+        name: "general",
+        channel_type: "channel",
+    });
+    pyEnv["mail.message"].create({
+        author_id: pyEnv.currentPartnerId,
+        body: "Hello world",
+        model: "discuss.channel",
+        res_id: channelId,
+        message_type: "comment",
+    });
+    const { openDiscuss } = await start();
+    await openDiscuss();
+    await click("button", { text: "Channel" });
+    await click("button", { text: "general" });
+    await contains(".o-mail-Message");
+    await click(".o-mail-Message [title='Expand']");
+    await click(".o-mail-Message [title='Edit']");
+    await contains(".o-mail-Message-editable .o-mail-Composer-input", { value: "Hello world" });
+    await click("button", { text: "Discard editing" });
+    await contains(".o-mail-Message-editable .o-mail-Composer", { count: 0 });
+    await contains(".o-mail-Message-content", { text: "Hello world" });
+    await click(".o-mail-Message [title='Expand']");
+    await click(".o-mail-Message [title='Edit']");
+    await insertText(".o-mail-Message .o-mail-Composer-input", "edited message", { replace: true });
+    await click(".o-mail-Message .fa-paper-plane-o");
+    await contains(".o-mail-Message-content", { text: "edited message" });
 });
 
 QUnit.test("Can edit message comment in chatter", async () => {
