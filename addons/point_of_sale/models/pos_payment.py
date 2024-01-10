8f64e5c6e6d7749a0e0b46e509e475165ddbc375
Aurelien van Delft (avd) <avd@odoo.com>
2024-01-10 10:35:32 +0000

    [FIX] point_of_sale: add missing indexes for create_from_ui
    Add two missing indexes on pos.order and one on pos.payment. These
    speed up queries executed when calling create_from_ui from the
    pos interface.
    
    opw-3619270
    
    closes odoo/odoo#151434
    
    X-original-commit: 6f1b5bd530dbb211bb0dbc41b523233dfb57d002
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Aurélien van Delft (avd) <avd@odoo.com>

--- addons/point_of_sale/models/pos_payment.py
+++ addons/point_of_sale/models/pos_payment.py
@@ -16,7 +16,7 @@ class PosPayment(models.Model):
     _order = "id desc"
 
     name = fields.Char(string='Label', readonly=True)
-    pos_order_id = fields.Many2one('pos.order', string='Order', required=True)
+    pos_order_id = fields.Many2one('pos.order', string='Order', required=True, index=True)
     amount = fields.Monetary(string='Amount', required=True, currency_field='currency_id', readonly=True, help="Total amount of the payment.")
     payment_method_id = fields.Many2one('pos.payment.method', string='Payment Method', required=True)
     payment_date = fields.Datetime(string='Date', required=True, readonly=True, default=lambda self: fields.Datetime.now())
