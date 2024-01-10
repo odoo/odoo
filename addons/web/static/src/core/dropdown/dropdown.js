3e834f3fb1bef39b386513ab730f5e5e3276adbc
Michael (mcm) <mcm@odoo.com>
2024-01-10 09:46:28 +0100

    [FIX] web: fix searchbar menu position
    Before this commit, the searchbar menu was misplaced after opening the advanced search dialog.
    After this commit, the searchbar menu is always at the correct position.
    
    Task-id: 3673493
    X-original-commit: 19d40f48fb98b28b7fd49b79581ac0ac5fcecc87

--- addons/web/static/src/core/dropdown/dropdown.js
+++ addons/web/static/src/core/dropdown/dropdown.js
@@ -167,6 +167,8 @@ export class Dropdown extends Component {
             (isOpen) => {
                 if (isOpen) {
                     this.props.onOpened();
+                } else {
+                    this.position.unlock();
                 }
             },
             () => [this.state.open]
