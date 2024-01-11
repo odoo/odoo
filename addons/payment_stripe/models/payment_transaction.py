32df4ef1bdad4d2522778dee450d1ffcce641eac
mathysPaul <matp@odoo.com>
2024-01-11 15:39:05 +0100

    [FIX] payment_stripe: Store Stripe mandates
    Addresses the issue where mandates returned by Stripe were not
    being saved in the database, rendering them unused. The fix
    involves retrieving mandate information from the charge object
    in Stripe, as the mandate data is included in the payment method
    details of the charge object, rather than in the payment methods
    object.
    
    task-3594745
    
    closes odoo/odoo#153342
    
    X-original-commit: 1db395eb8a12058702b5e7dbd7213c65ae2ac52b
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- addons/payment_stripe/models/payment_transaction.py
+++ addons/payment_stripe/models/payment_transaction.py
@@ -442,9 +442,14 @@ class PaymentTransaction(models.Model):
             )
             return
 
+        mandate = None
         # Extract the Stripe objects from the notification data.
         if self.operation == 'online_direct':
             customer_id = notification_data['payment_intent']['customer']
+            charges_data = notification_data['payment_intent']['charges']
+            payment_method_details = charges_data['data'][0].get('payment_method_details')
+            if payment_method_details:
+                mandate = payment_method_details[payment_method_details['type']].get("mandate")
         else:  # 'validation'
             customer_id = notification_data['setup_intent']['customer']
         # Another payment method (e.g., SEPA) might have been generated.
@@ -463,7 +468,7 @@ class PaymentTransaction(models.Model):
             'partner_id': self.partner_id.id,
             'provider_ref': customer_id,
             'stripe_payment_method': payment_method['id'],
-            'stripe_mandate': payment_method[payment_method['type']].get('mandate'),
+            'stripe_mandate': mandate,
         })
         self.write({
             'token_id': token,
