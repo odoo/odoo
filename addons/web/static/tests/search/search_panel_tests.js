098c237e2eabe40df890f768625cc7044dd20146
Julien Carion (juca) <juca@odoo.com>
2024-01-11 10:45:31 +0100

    [FIX] web: fix duplicate t-key error in search panel
    This commit fixes a duplicate t-key error which would happen when
    there is a selection in multiple categories or filters and then the
    panel is collapsed (or the selection is applied in mobile). This crash
    is due to the t-key being set to the id of the category/filter which
    never exists.

--- addons/web/static/tests/search/search_panel_tests.js
+++ addons/web/static/tests/search/search_panel_tests.js
@@ -3537,6 +3537,39 @@ QUnit.module("Search", (hooks) => {
         );
     });
 
+    QUnit.test("search panel collapse with multiple filter categories selected", async (assert) => {
+        serverData.views["partner,false,search"] = /* xml */ `
+            <search>
+                <searchpanel>
+                    <field name="company_id" enable_counters="1"/>
+                    <field name="category_id" select="multi" enable_counters="1"/>
+                    <field name="state" select="multi" enable_counters="1"/>
+                </searchpanel>
+            </search>`;
+        const { TestComponent } = makeTestComponent();
+        await makeWithSearch({
+            serverData,
+            Component: TestComponent,
+            resModel: "partner",
+            searchViewId: false,
+        });
+
+        assert.containsOnce(target, ".o_search_panel");
+        assert.containsN(target, ".o_search_panel_section", 3);
+
+        await click(target.querySelectorAll(".o_search_panel_category_value header")[1]);
+        await click(target.querySelectorAll(".o_search_panel_filter_value input")[1]);
+        await click(target.querySelectorAll(".o_search_panel_filter_value input")[2]);
+
+        await click(target.querySelector(".o_search_panel button"));
+        assert.containsNone(target, ".o_search_panel");
+        assert.containsOnce(target, ".o_search_panel_sidebar");
+        assert.strictEqual(
+            target.querySelector(".o_search_panel_sidebar").textContent,
+            "asusteksilverABC"
+        );
+    });
+
     QUnit.test("search panel should be resizable", async function (assert) {
         const { TestComponent } = makeTestComponent();
         await makeWithSearch({
