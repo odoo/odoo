5f164248200bdf938491a8ffe1c4195bb1e073c2
Louis (loco) <loco@odoo.com>
2024-01-11 15:08:01 +0100

    [FIX] web_editor, *: correctly display the hierarchy between elements
    *website
    
    Steps to reproduce:
    - Drop a "Text-Image" snippet on the website.
    - Click on a column and hide it on desktop.
    - Click on the parent snippet and hide it on desktop.
    
    -> The column is not a child of the parent in the invisible entry.
    
    Since [1], invisible snippets are shown hierarchically in order to
    better visualize the "parent-children" relation that could exist between
    them. To do so, [1] based its logic on the `.o_snippet_invisible` to
    find the invisible elements. However, since [2], a distinction has been
    made between elements that are hidden only on desktop, only on mobile or
    on both depending on some conditions. The goal of this commit is to take
    the new `o_snippet_mobile_invisible` and `o_snippet_desktop_invisible`
    classes into account at the creation of the invisible entry.
    
    [1]: https://github.com/odoo/odoo/commit/922d3e63b597b1ef7fbbb5ebb4641a3ad8bdf4ed
    [2]: https://github.com/odoo/odoo/commit/3103e0553011b5c1f4078972d7a88fa3fd4068b2
    
    task-3679823
    
    closes odoo/odoo#150477
    
    X-original-commit: c8f247ee5fa178f012b2852cba3d0e9322a2c3a6
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2560,7 +2560,7 @@ var SnippetsMenu = Widget.extend({
             // their descendant snippets.
             const rootInvisibleSnippetEls = [...$invisibleSnippets].filter(invisibleSnippetEl => {
                 const ancestorInvisibleEl = invisibleSnippetEl
-                                                 .parentElement.closest(".o_snippet_invisible");
+                                                 .parentElement.closest(invisibleSelector);
                 if (!ancestorInvisibleEl) {
                     return true;
                 }
