f30b6521cf54947843e2e2b6e9a9092e874d99be
Florian(flg) <flg@odoo.com>
2024-01-11 10:26:40 +0100

    [FIX] l10n_dk: Add Flex ERP as author
    As Flex ERP help us a lot to implement this localization, we
    add them as author of the module.
    
    issue id: #148085

--- addons/l10n_dk/__manifest__.py
+++ addons/l10n_dk/__manifest__.py
@@ -4,7 +4,7 @@
     'icon': '/account/static/description/l10n.png',
     'countries': ['dk'],
     'version': '1.1',
-    'author': 'Odoo House ApS, VK DATA ApS',
+    'author': 'Odoo House ApS, VK DATA ApS, FlexERP ApS',
     'website': 'https://www.odoo.com/documentation/17.0/applications/finance/fiscal_localizations.html',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
