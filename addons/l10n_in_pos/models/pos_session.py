686e0b7673cdaac3344f34caaa46d91785902ce7
Loukas Wets (lowe) <lowe@odoo.com>
2024-01-11 11:11:04 +0100

    [FIX] l10n_in_pos: demo data post installation
    To reproduce:
    - Install POS with demo data
    - Install Indian localization(l10n_in)
    - Open POS terminal
    - Click on the load demo data
    - Traceback thrown
    
    Fix:
    The issue occurs when the demo data executes the closing of the sessions.
    The l10n_in _post method checks for a company state that is not yet set
    at that point. It is important to note that this issue only arises
    when loading demo data on a DB that doesn't contain demo data. The solution
    is to extract the part of the method that executes the demo data linked to
    the main config and to override it to not perform that part in the l10n_in_pos.

--- addons/l10n_in_pos/models/pos_session.py
+++ addons/l10n_in_pos/models/pos_session.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import models, api
 
 
 class PosSession(models.Model):
@@ -11,3 +11,10 @@ class PosSession(models.Model):
         result = super()._loader_params_product_product()
         result['search_params']['fields'].append('l10n_in_hsn_code')
         return result
+
+    @api.model
+    def _load_onboarding_main_config_data(self, shop_config):
+        if shop_config.company_id.country_code == 'IN' and not shop_config.company_id.state_id:
+            return
+
+        super()._load_onboarding_main_config_data(shop_config)
