29949b77e31338901fe421949d002880e206b956
Mehdi Outagant (MOU) <mou@odoo.com>
2024-01-12 15:26:09 +0100

    [IMP] website: simplify the text highlight code
    The goal of this commit is to improve the current text highlight code
    by adding the following updates:
    
    - The editor's code (`_handleTextOptions()`) will only set text
    highlight classes and activate the snippet. Highlight DOM adaptations
    (multi-line detection, setting SVGs...) are done by the public widget.
    
    - The `o_text_highlight_item_dirty` class is no longer used to adapt
    non-detected highlight items (the highlight mutation observer was
    tweaked to detect and adapt the new highlight items added to the DOM
    after editors commands. E.g. after the `oEnter()`).
    
    task-3285817

--- addons/website/static/src/js/content/snippets.animation.js
+++ addons/website/static/src/js/content/snippets.animation.js
@@ -19,8 +19,8 @@ import { hasTouch } from "@web/core/browser/feature_detection";
 import { SIZES, utils as uiUtils } from "@web/core/ui/ui_service";
 import {
     applyTextHighlight,
+    removeTextHighlight,
     switchTextHighlight,
-    getCurrentTextHighlight,
 } from "@website/js/text_processing";
 import { touching } from "@web/core/utils/ui";
 
@@ -1886,7 +1886,7 @@ registry.TextHighlight = publicWidget.Widget.extend({
         // information to build the effects, So we need to make the adaptation
         // here to show the SVGs.
         for (const textEl of this.el.querySelectorAll(".o_text_highlight")) {
-            applyTextHighlight(textEl, getCurrentTextHighlight(textEl));
+            applyTextHighlight(textEl);
         }
         // We need to adapt the text highlights on resize, mainly to take in
         // consideration the rendered line breaks in text nodes...
@@ -1894,7 +1894,7 @@ registry.TextHighlight = publicWidget.Widget.extend({
             this.options.wysiwyg?.odooEditor.observerUnactive("textHighlightResize");
             for (const textEl of this.el.querySelectorAll(".o_text_highlight")) {
                 // Remove old effect, normalize content, redraw SVGs...
-                switchTextHighlight(textEl, getCurrentTextHighlight(textEl));
+                switchTextHighlight(textEl);
             }
             this.options.wysiwyg?.odooEditor.observerActive("textHighlightResize");
         });
@@ -1913,6 +1913,12 @@ registry.TextHighlight = publicWidget.Widget.extend({
         if (!this.editableMode) {
             this.resizeObserver.disconnect();
             this.observerLocked.clear();
+        } else {
+            // We only save the highlight information on the main text wrapper,
+            // the full structure will be restored on page load.
+            for (const textHighlightEl of this.el.querySelectorAll(".o_text_highlight")) {
+                removeTextHighlight(textHighlightEl);
+            }
         }
     },
 
@@ -1967,7 +1973,7 @@ registry.TextHighlight = publicWidget.Widget.extend({
                         this.observerLocked.delete(unit);
                     });
                     // Adapt the highlight, lock new items and observe them.
-                    switchTextHighlight(topTextEl, getCurrentTextHighlight(topTextEl));
+                    switchTextHighlight(topTextEl);
                     this._lockHighlightObserver(topTextEl);
                     this._observeHighlightResize(topTextEl);
                 });
