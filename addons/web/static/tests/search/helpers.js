b4d8c7defad173ccea0db5a423fe0b1ee1944c87
Mathieu Duckerts-Antoine <dam@odoo.com>
2024-01-12 12:29:30 +0100

    [FIX] web: adapt editFavoriteName helper
    With the last realease of owl, .trim now implies .lazy. This means that
    if t-model.trim is used, the value will be updated in the state only when
    the change event is triggered. Here we adapt the helper editFavoriteName
    which was supposed to update the state value. Similarly, we do trigger
    the change event in the (legacy) test "save search filter in modal".
    
    X-original-commit: 66b9fd77836d9bd4af17304a4ee37a3c427fd3e1

--- addons/web/static/tests/search/helpers.js
+++ addons/web/static/tests/search/helpers.js
@@ -9,6 +9,7 @@ import {
     mouseEnter,
     nextTick,
     triggerEvent,
+    triggerEvents,
 } from "@web/../tests/helpers/utils";
 import { hotkeyService } from "@web/core/hotkeys/hotkey_service";
 import { notificationService } from "@web/core/notifications/notification_service";
@@ -292,7 +293,7 @@ export async function editFavoriteName(el, name) {
         `.o_favorite_menu .o_add_favorite .dropdown-menu input[type="text"]`
     );
     input.value = name;
-    await triggerEvent(input, null, "input");
+    await triggerEvents(input, null, ["input", "change"]);
 }
 
 export async function saveFavorite(el) {
