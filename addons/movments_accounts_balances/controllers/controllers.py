d3ff63e8ef55e6148ef8eed0c06e7a97c478b323
sameerz09 <sameerz09@hotmail.com>
2024-01-15 00:27:09 +0200

    Adding some fixs create_bill method.

--- addons/movments_accounts_balances/controllers/controllers.py
+++ addons/movments_accounts_balances/controllers/controllers.py
@@ -100,47 +100,52 @@ class AccountBalance(models.Model):
 
     ##create/get/delete_bills
     @api.model
-    def create_bill(self, invoice_vals, partner_id, invoice_date, invoice_date_due, reference, narration):
+    def create_bill(self, bill_vals, partner_id, bill_date, bill_date_due, reference, narration):
         """
-        Create an AR invoice and corresponding analytic lines based on the provided data.
+        Create a bill and corresponding analytic lines based on the provided data.
         """
         # Prepare the invoice lines
-        invoice_lines = []
-        for line in invoice_vals:
-            invoice_line_vals = {
+        bill_lines = []
+        for line in bill_vals:
+            bill_line_vals = {
                 'name': line.get('description', ''),
                 'quantity': line.get('quantity', 1.0),
                 'price_unit': line.get('price_unit', 0.0),
                 'account_id': line.get('account_id'),
-                'product_id': line.get('product_id', False),
+                # Assuming that the analytic account ID is also provided in line
+                'analytic_account_id': line.get('analytic_account_id'),
             }
-            invoice_lines.append((0, 0, invoice_line_vals))
+            bill_lines.append((0, 0, bill_line_vals))
 
         # Create a new AR invoice record
-        invoice = self.env['account.move'].create({
+        bill = self.env['account.move'].create({
             'move_type': 'in_invoice',
             'partner_id': partner_id,
-            'invoice_date': invoice_date,
-            'invoice_date_due': invoice_date_due,
-            'ref': self.name,
+            'invoice_date': bill_date,
+            'invoice_date_due': bill_date_due,
+            'ref': reference,
             'narration': narration,
-            'invoice_line_ids': invoice_lines,
+            'invoice_line_ids': bill_lines,
         })
-        invoice.action_post()
+        bill.action_post()
 
         # Create analytic lines for each invoice line if analytic_account_id is provided
-        for line, line_vals in zip(invoice.invoice_line_ids, invoice_vals):
-            analytic_account_id = line_vals.get('analytic_account_id')
-            if analytic_account_id:
+        for line in bill.invoice_line_ids:
+            if line.analytic_account_id:
                 self.env['account.analytic.line'].create({
-                    'account_id': analytic_account_id,
+                    'account_id': line.analytic_account_id.id,
                     'name': line.name,
                     'amount': line.price_subtotal,  # or any other relevant amount
-                    # 'move_id': line.id,
-                    # Add other necessary fields
+                    'move_id': line.id,
                 })
 
-        return f"ID: {invoice.id}, Name: {invoice.name}, Amount: {invoice.amount_total}, Date: {invoice.invoice_date}, Due Date: {invoice.invoice_date_due}"
+        return {
+            "ID": bill.id,
+            "Name": bill.name,
+            "Amount": bill.amount_total,
+            "Date": bill.invoice_date,
+            "Due Date": bill.invoice_date_due
+        }
 
     @api.model
     def get_bill(self, bill_id):
