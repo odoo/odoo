42834afe8ed0bf4ad52542961719249ea84d4d33
nda <nda@odoo.com>
2024-01-16 12:46:23 +0000

    [FIX] base_import_module: set module dependencies
    Dependencies and other manifest attributes are not set for data modules.
    
    Note: it's easier to reproduce issue in 17 since we have data module available
    on runbot.
    
    steps to reproduce (in 17.0):
    - install an industry (ex: bar_and_lounge)
    - uninstall a dependency of that module (ex: mrp)
    
    before this commit:
    - bar_and_lounge is not uninstalled if you uninstall mrp
    
    after this commit:
    - data model dependencies are handled the same way as 'regular' modules
    
    opw-3660052
    
    closes odoo/odoo#151470
    
    X-original-commit: c4c796d35313dcc65df91282a8c270bf4c627456
    Signed-off-by: Pierre Masereel (pim) <pim@odoo.com>

--- addons/base_import_module/models/ir_module.py
+++ addons/base_import_module/models/ir_module.py
@@ -98,7 +98,7 @@ class IrModule(models.Model):
             mode = 'update' if not force else 'init'
         else:
             assert terp.get('installable', True), "Module not installable"
-            self.create(dict(name=module, state='installed', imported=True, **values))
+            mod = self.create(dict(name=module, state='installed', imported=True, **values))
             mode = 'init'
 
         kind_of_files = ['data', 'init_xml', 'update_xml']
@@ -193,6 +193,8 @@ class IrModule(models.Model):
             'module': module,
             'res_id': asset.id,
         } for asset in created_assets])
+
+        mod._update_from_terp(terp)
 
         return True
 
