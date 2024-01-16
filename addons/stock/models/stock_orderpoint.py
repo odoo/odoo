9ec69d55ed31106b71f704853874ff5580b9c33e
Andrzej(pian) <pian@odoo.com>
2024-01-16 11:26:01 +0100

    [FIX] stock: use visibility days in reordering
     If reordering rules' visibility is set bigger than DAYS_FROM_TODAY_TO_ORDER
     (plus lead time). Then the order should be included in the calculation of
     quantity to order.
    
                ┌─ Today                     ┌── Scheduled Delivery
                │ (2024-01-01)               │    (2024-02-01)
                │                            │    aka commitment_date
                │                            │
                ▼                            ▼          time
              ──────────────────────────────────────────►
                ◄────────────────────────────►
                    DAYS_FROM_TODAY_TO_ORDER
                                              ◄────►
                                              lead_time
    
    Before this commit visibility_days were taken into the account only if
    there forecasted quantity was lower than product minimal quantity. This
    commits ensures that the visibility_days will always be included into
    the calculation.
    
    [Reproduce]
    - install stock,purchase,sale_management
    - Create a product P (storable)
    - Add vendor V under the purchase tab
    - Create Reordering with route buy, vendor V, min 0, max 0
    - Create a Sale Order for 1 unit of P, under the 'Other Info' tab, set the Delivery date to 1 month in the future, Confirm.
    	=> If you go back to the reordering rule, you have Qty To Order at 0 (ok)
    - Set Visibility Days at 40
    	=> Qty To Order is still at 0, even though it should now see the sale order we made before (bug)
    - Set the Min qty at 1
    	=> Qty To Order is now at 2, it found the sale order we made, and computed the correct quantity. (ok)
    - Set the Min Qty back to 0
    	=> Qty To Order is back at 0 (bug)
    opw-3638398
    
    closes odoo/odoo#152135
    
    X-original-commit: 8a5541d16caa793c7549d78c082f7879d3aba233
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Andrzej Pietrusiak (pian) <pian@odoo.com>

--- addons/stock/models/stock_orderpoint.py
+++ addons/stock/models/stock_orderpoint.py
@@ -269,10 +269,11 @@ class StockWarehouseOrderpoint(models.Model):
                 continue
             qty_to_order = 0.0
             rounding = orderpoint.product_uom.rounding
-            if float_compare(orderpoint.qty_forecast, orderpoint.product_min_qty, precision_rounding=rounding) < 0:
-                # We want to know how much we should order to also satisfy the needs that gonna appear in the next (visibility) days
-                product_context = orderpoint._get_product_context(visibility_days=orderpoint.visibility_days)
-                qty_forecast_with_visibility = orderpoint.product_id.with_context(product_context).read(['virtual_available'])[0]['virtual_available'] + orderpoint._quantity_in_progress()[orderpoint.id]
+            # We want to know how much we should order to also satisfy the needs that gonna appear in the next (visibility) days
+            product_context = orderpoint._get_product_context(visibility_days=orderpoint.visibility_days)
+            qty_forecast_with_visibility = orderpoint.product_id.with_context(product_context).read(['virtual_available'])[0]['virtual_available'] + orderpoint._quantity_in_progress()[orderpoint.id]
+
+            if float_compare(qty_forecast_with_visibility, orderpoint.product_min_qty, precision_rounding=rounding) < 0:
                 qty_to_order = max(orderpoint.product_min_qty, orderpoint.product_max_qty) - qty_forecast_with_visibility
                 remainder = orderpoint.qty_multiple > 0.0 and qty_to_order % orderpoint.qty_multiple or 0.0
                 if (float_compare(remainder, 0.0, precision_rounding=rounding) > 0
