ed65bf0f62e5ef61a2d4fe59128bbb4c19732a5f
nda <nda@odoo.com>
2024-01-16 08:46:23 -0400

    [FIX] base_import_module: set module dependencies
    Dependencies and other manifest attributes are not set for data modules.
    
    Note: it's easier to reproduce issue in 17 since we have data module available
    on runbot.
    
    steps to reproduce (in 17.0):
    - install an industry (ex: bar_and_lounge)
    - uninstall a dependency of that module (ex: mrp)
    
    before this commit:
    - bar_and_lounge is not uninstalled if you uninstall mrp
    
    after this commit:
    - data model dependencies are handled the same way as 'regular' modules
    
    opw-3660052
    
    closes odoo/odoo#149574
    
    Signed-off-by: Pierre Masereel (pim) <pim@odoo.com>

--- odoo/addons/base/models/ir_module.py
+++ odoo/addons/base/models/ir_module.py
@@ -772,9 +772,7 @@ class Module(models.Model):
                 mod = self.create(dict(name=mod_name, state=state, **values))
                 res[1] += 1
 
-            mod._update_dependencies(terp.get('depends', []), terp.get('auto_install'))
-            mod._update_exclusions(terp.get('excludes', []))
-            mod._update_category(terp.get('category', 'Uncategorized'))
+            mod._update_from_terp(terp)
 
         return res
 
@@ -881,6 +879,11 @@ class Module(models.Model):
     @api.model
     def get_apps_server(self):
         return tools.config.get('apps_server', 'https://apps.odoo.com/apps')
+
+    def _update_from_terp(self, terp):
+        self._update_dependencies(terp.get('depends', []), terp.get('auto_install'))
+        self._update_exclusions(terp.get('excludes', []))
+        self._update_category(terp.get('category', 'Uncategorized'))
 
     def _update_dependencies(self, depends=None, auto_install_requirements=()):
         existing = set(dep.name for dep in self.dependencies_id)
