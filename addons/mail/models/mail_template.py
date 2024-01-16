372904678c61e1cd3b53a1ed6fdc23175ac57071
Antoine (ande) <ande@odoo.com>
2024-01-16 13:39:53 +0100

    [FIX] mail: templates preview wrong recipients
    Steps to reproduce:
    1. Activate dev mode
    2. Go to Email Templates
    3. Create a new template
    4. Applies to Task
    5. Email Configuration > To (Partners)
    6. {{[p.id for p in object.message_partner_ids]}}
    7. Click on Preview
    8. Select a record with followers
    9. Look at Recipients
    10. => No or not all followers/partners are included
    
    Cause of the issue:
    Caused by https://github.com/odoo/odoo/commit/cf7ae6b9d562622709e2730c1ea8c43816c169bf
    In the case of partner_to being '[2,3,4]'
    Split would output '[2', '3', '4]'
    So after the isdigit it would be '3'
    It should have been 2, 3, 4
    
    opw-3677069
    
    closes odoo/odoo#156345
    
    X-original-commit: d2e473f5b2b1b0fa1eae0f2a1e3698d663af07d8
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/models/mail_template.py
+++ addons/mail/models/mail_template.py
@@ -4,6 +4,7 @@
 import base64
 import itertools
 import logging
+from ast import literal_eval
 
 from odoo import _, api, fields, models, tools, Command
 from odoo.exceptions import ValidationError, UserError
@@ -553,9 +554,15 @@ class MailTemplate(models.Model):
 
     @classmethod
     def _parse_partner_to(cls, partner_to):
+        try:
+            partner_to = literal_eval(partner_to or '[]')
+        except (ValueError, SyntaxError):
+            partner_to = partner_to.split(',')
+        if not isinstance(partner_to, (list, tuple)):
+            partner_to = [partner_to]
         return [
-            int(pid.strip()) for pid in partner_to.split(',')
-            if (pid and pid.strip().isdigit())
+            int(pid.strip()) if isinstance(pid, str) else int(pid) for pid in partner_to
+            if (isinstance(pid, str) and pid.strip().isdigit()) or (pid and not isinstance(pid, str))
         ]
 
     # ------------------------------------------------------------
