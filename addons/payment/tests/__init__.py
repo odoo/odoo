4c82f9fd79ae242ed0ec9fc732248dacd9cbdcaa
Antoine Vandevenne (anv) <anv@odoo.com>
2024-01-17 17:55:07 +0100

    [IMP] payment: improve test coverage
    Some logic of the `payment.provider` and `provider.method` models was
    not properly covered by unit tests.
    
    X-original-commit: 5cea9dddcc51b729ba9e2fd2ca029419d58735fe

--- addons/payment/tests/__init__.py
+++ addons/payment/tests/__init__.py
@@ -4,6 +4,7 @@ from . import common
 from . import http_common
 from . import test_flows
 from . import test_multicompany_flows
+from . import test_payment_method
 from . import test_payment_provider
 from . import test_payment_token
 from . import test_payment_transaction
--- addons/payment/tests/common.py
+++ addons/payment/tests/common.py
@@ -94,8 +94,9 @@ class PaymentCommon(BaseCommon):
 
         cls.provider = cls.dummy_provider
         cls.payment_methods = cls.provider.payment_method_ids
-        cls.payment_method_id = cls.provider.payment_method_ids[:1].id
-        cls.payment_method_code = cls.provider.payment_method_ids[:1].code
+        cls.payment_method = cls.payment_methods[:1]
+        cls.payment_method_id = cls.payment_method.id
+        cls.payment_method_code = cls.payment_method.code
         cls.amount = 1111.11
         cls.company = cls.env.company
         cls.company_id = cls.company.id
