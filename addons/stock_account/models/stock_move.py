6134eca404ff0503c0dadc86e0bc0ebf0a45ba77
Arthur Jochems (ajoc) <ajoc@odoo.com>
2024-01-17 01:32:09 +0100

    [FIX] stock(_account): don't count unpicked moves for analytics
    As a result of the Quantitypocalypse (7dda6bb), move lines are being
    created for MO components before they're actually consumed. This in turn
    automatically creates analytics lines, debiting accounts for these
    unconsumed parts. This commit globally prevents any move line that is
    not yet picked (i.e. consumed/reserved) from being included in an
    analytic entry.
    
    Additionally, an edge case where setting a move's qty to 0 did not
    trigger adjustments of dependent models was fixed.
    
    Part-of: odoo#146114
    
    Task-3621105
    
    closes odoo/odoo#146114
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/stock_account/models/stock_move.py
+++ addons/stock_account/models/stock_move.py
@@ -20,6 +20,10 @@ class StockMove(models.Model):
     stock_valuation_layer_ids = fields.One2many('stock.valuation.layer', 'stock_move_id')
     analytic_account_line_ids = fields.Many2many('account.analytic.line', copy=False)
 
+    def _inverse_picked(self):
+        super()._inverse_picked()
+        self._account_analytic_entry_move()
+
     def _filter_anglo_saxon_moves(self, product):
         return self.filtered(lambda m: m.product_id.id == product.id)
 
@@ -420,11 +424,12 @@ class StockMove(models.Model):
 
         amount, unit_amount = 0, 0
         if self.state != 'done':
-            unit_amount = self.product_uom._compute_quantity(
-                self.quantity, self.product_id.uom_id)
-            # Falsy in FIFO but since it's an estimation we don't require exact correct cost. Otherwise
-            # we would have to recompute all the analytic estimation at each out.
-            amount = - unit_amount * self.product_id.standard_price
+            if self.picked:
+                unit_amount = self.product_uom._compute_quantity(
+                    self.quantity, self.product_id.uom_id)
+                # Falsy in FIFO but since it's an estimation we don't require exact correct cost. Otherwise
+                # we would have to recompute all the analytic estimation at each out.
+                amount = - unit_amount * self.product_id.standard_price
         elif self.product_id.valuation == 'real_time' and not self._ignore_automatic_valuation():
             accounts_data = self.product_id.product_tmpl_id.get_product_accounts()
             account_valuation = accounts_data.get('stock_valuation', False)
