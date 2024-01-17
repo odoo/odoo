e93bac64077d5c956182252c29d2ae838ba83ff6
nni-odoo <nni@odoo.com>
2024-01-17 14:30:26 +0800

    [IMP] payment_xendit: add support for tokenization
    Currently, in the APAC region, there are no payment providers
    implemented that support tokenization. This commit adds tokenization
    support for the `Card` payment method through Xendit. As tokenization
    can not be implemented via form redirect, `Card` payments will
    specifically be using a direct flow. The changes made here will only
    affect `Card` payments, while other payment methods will continue using
    redirect implementation.
    
    task-3704600

--- addons/payment_xendit/models/payment_transaction.py
+++ addons/payment_xendit/models/payment_transaction.py
@@ -27,13 +27,13 @@ class PaymentTransaction(models.Model):
         :rtype: dict
         """
         res = super()._get_specific_rendering_values(processing_values)
-        if self.provider_code != 'xendit':
+        if self.provider_code != 'xendit' and self.payment_method_code != 'card':
             return res
 
         # Initiate the payment and retrieve the invoice data.
         payload = self._xendit_prepare_invoice_request_payload()
         _logger.info("Sending invoice request for link creation:\n%s", pprint.pformat(payload))
-        invoice_data = self.provider_id._xendit_make_request(payload)
+        invoice_data = self.provider_id._xendit_make_request('v2/invoices', payload=payload)
         _logger.info("Received invoice request response:\n%s", pprint.pformat(invoice_data))
 
         # Extract the payment link URL and embed it in the redirect form.
@@ -84,6 +84,40 @@ class PaymentTransaction(models.Model):
             payload['customer']['addresses'] = [address_details]
 
         return payload
+
+    def _send_payment_request(self):
+        """ Override of `payment` to send a payment request to Xendit.
+
+        Note: self.ensure_one()
+
+        :return: None
+        :raise UserError: If the transaction is not linked to a token.
+        """
+        super()._send_payment_request()
+        if self.provider_code != 'xendit':
+            return
+
+        if not self.token_id:
+            raise ValidationError("Xendit: " + _("The transaction is not linked to a token."))
+
+        self._xendit_create_charge(self.token_id.provider_ref)
+
+    def _xendit_create_charge(self, token_ref):
+        """ Create a charge on Xendit using the `credit_card_charges` endpoint.
+
+        :param str token_ref: The reference of the Xendit token to use to make the payment.
+        :return: None
+        """
+        payload = {
+            'token_id': token_ref,
+            'external_id': self.reference,
+            'amount': self.amount,
+            'currency': self.currency_id.name,
+        }
+        charge_notification_data = self.provider_id._xendit_make_request(
+            'credit_card_charges', payload=payload
+        )
+        self._handle_notification_data('xendit', charge_notification_data)
 
     def _get_tx_from_notification_data(self, provider_code, notification_data):
         """ Override of `payment` to find the transaction based on the notification data.
@@ -140,11 +174,43 @@ class PaymentTransaction(models.Model):
         if payment_status in const.PAYMENT_STATUS_MAPPING['pending']:
             self._set_pending()
         elif payment_status in const.PAYMENT_STATUS_MAPPING['done']:
+            if self.tokenize:
+                self._xendit_tokenize_from_notification_data(notification_data)
             self._set_done()
         elif payment_status in const.PAYMENT_STATUS_MAPPING['cancel']:
             self._set_canceled()
         elif payment_status in const.PAYMENT_STATUS_MAPPING['error']:
+            failure_reason = notification_data.get('failure_reason')
             self._set_error(_(
-                "An error occurred during the processing of your payment (status %s). Please try "
-                "again."
+                "An error occurred during the processing of your payment (%s). Please try again.",
+                failure_reason,
             ))
+
+    def _xendit_tokenize_from_notification_data(self, notification_data):
+        """ Create a new token based on the notification data.
+
+        :param dict notification_data: Xendit's response to a charge API request.
+        :return: None
+        """
+        card_info = notification_data['masked_card_number'][-4:]  # Xendit pads details with X's.
+        token_id = notification_data['credit_card_token_id']
+        token = self.env['payment.token'].create({
+            "provider_id": self.provider_id.id,
+            "payment_method_id": self.payment_method_id.id,
+            "payment_details": card_info,
+            "partner_id": self.partner_id.id,
+            "provider_ref": token_id,
+        })
+        self.write({
+            'token_id': token.id,
+            'tokenize': False,
+        })
+        _logger.info(
+            "created token with id %(token_id)s for partner with id %(partner_id)s from "
+            "transaction with reference %(ref)s",
+            {
+                'token_id': token.id,
+                'partner_id': self.partner_id.id,
+                'ref': self.reference,
+            },
+        )
