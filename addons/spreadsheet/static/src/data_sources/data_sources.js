233fa199534152a8c9b9a29800aa6af36f59470c
Lucas Lefèvre (lul) <lul@odoo.com>
2024-01-17 13:55:25 +0100

    [FIX] spreadsheet: improve perf by avoiding useless evaluation
    In a spreadsheet with multiple data sources (2 pivots), each data source
    initially loads and triggers a new evaluation upon loading.
    This results in two evaluations, even if both data sources resolve in less
    than 10ms apart. In such cases, the first re-evaluation becomes redundant,
    as a new one is immediately triggered.
    
    The issue is worse when more than 6 RPCs are required, as most browsers limit
    network calls to 6 in parallel. Consequently, the 7th RPC will unnecessarily
    wait after the evaluation triggered by the first RPC to resolve.
    
    For spreadsheets with many many data sources, the accumulation of these
    pointless evaluations significantly impacts performance.
    In a real-life scenario with 18 data sources from our production database,
    the spreadsheet took approximately ~33s to fully load and become reactive.
    With this commit, the loading time is reduced to ~7s (only one evaluation
    instead of 18) (tested in 17.0).
    
    Note that this testing was conducted locally, with minimal latency, and with
    a limited amount of data.
    
    One consequence of this commit is that cells won't load incrementally as
    each data source loads. Instead, all cells will display "Loading..." until
    all data sources are loaded. Given the substantial speed improvement, we
    consider this trade-off worthwhile.
    
    This fix only impacts loadable datasources (pivot, lists, graphs),
    it could also include data sources using individual RPCs (currency,
    accounting). Maybe for master.
    
    closes odoo/odoo#149992
    
    X-original-commit: 076f10a26e96f2b5f3a5903d60e0bcee88f7f1f6
    Signed-off-by: Vincent Schippefilt (vsc) <vsc@odoo.com>
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet/static/src/data_sources/data_sources.js
+++ addons/spreadsheet/static/src/data_sources/data_sources.js
@@ -22,6 +22,7 @@ export class DataSources extends EventBus {
         this._metadataRepository.addEventListener("labels-fetched", () => this.notify());
         /** @type {Object.<string, any>} */
         this._dataSources = {};
+        this.pendingPromises = new Set();
     }
 
     /**
@@ -38,6 +39,7 @@ export class DataSources extends EventBus {
                 orm: this._orm,
                 metadataRepository: this._metadataRepository,
                 notify: () => this.notify(),
+                notifyWhenPromiseResolves: this.notifyWhenPromiseResolves.bind(this),
             },
             params
         );
@@ -86,11 +88,37 @@ export class DataSources extends EventBus {
         return id in this._dataSources;
     }
 
+    async notifyWhenPromiseResolves(promise) {
+        this.pendingPromises.add(promise);
+        await promise
+            .then(() => {
+                this.pendingPromises.delete(promise);
+                this.notify();
+            })
+            .catch(() => {
+                this.pendingPromises.delete(promise);
+                this.notify();
+            });
+    }
+
     /**
      * Notify that a data source has been updated. Could be useful to
      * request a re-evaluation.
      */
     notify() {
+        if (this.pendingPromises.size) {
+            if (!this.nextTriggerTimeOutId) {
+                // evaluates at least every 10 seconds, even if there are pending promises
+                // to avoid blocking everything if there is a really long request
+                this.nextTriggerTimeOutId = setTimeout(() => {
+                    this.nextTriggerTimeOutId = undefined;
+                    if (this.pendingPromises.size) {
+                        this.trigger("data-source-updated");
+                    }
+                }, 10000);
+            }
+            return;
+        }
         this.trigger("data-source-updated");
     }
 
