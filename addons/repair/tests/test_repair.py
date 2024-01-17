349cf63603c63c8bb8cffa8d93690071f951c88e
Djamel Touati <otd@odoo.com>
2024-01-17 12:51:08 +0100

    [FIX] repair: validate a repair with a product on package
    Steps to reproduce the bug:
    - Create a product “P1” tracked by SN
    - Update the quantity with “SN1” and a package “Pack 1”
    - Create a repair order to repair the product P1:
        - Select the “SN1”
    
    - Try to confirm the repair
    
    Problem:
    A wizard with the following message is triggered:
    Insufficient Quantity to repair “The product is not available in
    sufficient quantity in WH/Stock
    
    Because when the function “action_validate” is called, we check if
    there is quant with the same SN but with strict=True so, the package
    should be false to find the quant:
    
    https://github.com/odoo/odoo/blob/61c9921596662a2cbc15a154a91dd2f52c9854fd/addons/mrp_repair/models/mrp_repair.py#L210-L211
    
    https://github.com/odoo/odoo/blob/b3180c841101510081ee8ef9c52d205497efdd4f/addons/stock/models/stock_quant.py#L102
    
    Opw-3648874

--- addons/repair/tests/test_repair.py
+++ addons/repair/tests/test_repair.py
@@ -346,3 +346,46 @@ class TestRepair(AccountTestInvoicingCommon):
         self.assertEqual(mail_message.author_id, repair_order.partner_id, 'Repair: author should be same as customer')
         self.assertEqual(mail_message.author_id, mail_message.partner_ids, 'Repair: author should be in composer recipients thanks to "partner_to" field set on template')
         self.assertEqual(mail_message.partner_ids, mail_message.sudo().mail_ids.recipient_ids, 'Repair: author should receive mail due to presence in composer recipients')
+
+    def test_repair_with_product_in_package(self):
+        """
+        Test That a repair order can be validated when the repaired product is tracked and in a package
+        """
+        self.product_a.tracking = 'serial'
+        self.product_a.type = 'product'
+        # Create two serial numbers
+        sn_1 = self.env['stock.production.lot'].create({'name': 'sn_1', 'product_id': self.product_a.id})
+        sn_2 = self.env['stock.production.lot'].create({'name': 'sn_2', 'product_id': self.product_a.id})
+
+        # Create two packages
+        package_1 = self.env['stock.quant.package'].create({'name': 'Package-test-1'})
+        package_2 = self.env['stock.quant.package'].create({'name': 'Package-test-2'})
+
+        # update the quantity of the product in the stock
+        self.env['stock.quant']._update_available_quantity(self.product_a, self.stock_warehouse.lot_stock_id, 1, lot_id=sn_1, package_id=package_1)
+        self.env['stock.quant']._update_available_quantity(self.product_a, self.stock_warehouse.lot_stock_id, 1, lot_id=sn_2, package_id=package_2)
+        self.assertEqual(self.product_a.qty_available, 2)
+        # create a repair order
+        repair_order = self.env['repair.order'].create({
+            'product_id': self.product_a.id,
+            'product_uom': self.product_a.uom_id.id,
+            'guarantee_limit': '2019-01-01',
+            'location_id': self.stock_warehouse.lot_stock_id.id,
+            'lot_id': sn_1.id,
+            'operations': [
+                (0, 0, {
+                    'name': 'foo',
+                    'product_id': self.product_b.id,
+                    'product_uom': self.product_b.uom_id.id,
+                    'product_uom_qty': 1,
+                    'price_unit': 50.0,
+                    'location_id': self.stock_warehouse.lot_stock_id.id,
+                    'location_dest_id': self.product_b.property_stock_production.id,
+                })
+            ],
+        })
+        # Validate and complete the repair order
+        repair_order.action_validate()
+        repair_order.action_repair_start()
+        repair_order.action_repair_end()
+        self.assertEqual(repair_order.state, 'done')
