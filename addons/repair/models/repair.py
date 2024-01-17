38b6ebd18081a9026b912735e4326c0fa901331b
Djamel Touati <otd@odoo.com>
2024-01-17 11:51:08 +0000

    [FIX] repair: validate a repair with a product on package
    Steps to reproduce the bug:
    - Create a product “P1” tracked by SN
    - Update the quantity with “SN1” and a package “Pack 1”
    - Create a repair order to repair the product P1:
        - Select the “SN1”
    
    - Try to confirm the repair
    
    Problem:
    A wizard with the following message is triggered:
    Insufficient Quantity to repair “The product is not available in
    sufficient quantity in WH/Stock
    
    Because when the function “action_validate” is called, we check if
    there is quant with the same SN but with strict=True so, the package
    should be false to find the quant:
    
    https://github.com/odoo/odoo/blob/61c9921596662a2cbc15a154a91dd2f52c9854fd/addons/mrp_repair/models/mrp_repair.py#L210-L211
    
    https://github.com/odoo/odoo/blob/b3180c841101510081ee8ef9c52d205497efdd4f/addons/stock/models/stock_quant.py#L102
    
    Opw-3648874
    
    closes odoo/odoo#150335
    
    X-original-commit: 8a443f335d993557c0a10aee615eae16090db850
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/repair/models/repair.py
+++ addons/repair/models/repair.py
@@ -280,8 +280,18 @@ class Repair(models.Model):
         if self.product_id.type == 'consu':
             return self.action_repair_confirm()
         precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
-        available_qty_owner = self.env['stock.quant']._get_available_quantity(self.product_id, self.location_id, self.lot_id, owner_id=self.partner_id, strict=True)
-        available_qty_noown = self.env['stock.quant']._get_available_quantity(self.product_id, self.location_id, self.lot_id, strict=True)
+        available_qty_owner = sum(self.env['stock.quant'].search([
+            ('product_id', '=', self.product_id.id),
+            ('location_id', '=', self.location_id.id),
+            ('lot_id', '=', self.lot_id.id),
+            ('owner_id', '=', self.partner_id.id),
+        ]).mapped('quantity'))
+        available_qty_noown = sum(self.env['stock.quant'].search([
+            ('product_id', '=', self.product_id.id),
+            ('location_id', '=', self.location_id.id),
+            ('lot_id', '=', self.lot_id.id),
+            ('owner_id', '=', False),
+        ]).mapped('quantity'))
         repair_qty = self.product_uom._compute_quantity(self.product_qty, self.product_id.uom_id)
         for available_qty in [available_qty_owner, available_qty_noown]:
             if float_compare(available_qty, repair_qty, precision_digits=precision) >= 0:
