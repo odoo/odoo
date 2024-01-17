9f5623b1f8e1289f057aae7f5747c10632afb294
Ruben Gomes (rugo) <rugo@odoo.com>
2024-01-17 15:03:09 +0100

    [IMP] odoo.tools: add tests to formatLang()
    Tests the method formatLang() to be able to do safe(r) refactoring in future versions.
    
    closes odoo/odoo#149775
    
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- odoo/addons/base/tests/test_misc.py
+++ odoo/addons/base/tests/test_misc.py
@@ -507,3 +507,68 @@ class TestDictTools(BaseCase):
             d.update({'baz': 'xyz'})
         with self.assertRaises(TypeError):
             dict.update(d, {'baz': 'xyz'})
+
+class TestFormatLang(TransactionCase):
+    def test_value_and_digits(self):
+        self.assertEqual(misc.formatLang(self.env, 100.23, digits=1), '100.2')
+        self.assertEqual(misc.formatLang(self.env, 100.23, digits=3), '100.230')
+
+        self.assertEqual(misc.formatLang(self.env, ''), '', 'If value is an empty string, it should return an empty string (not 0)')
+
+        self.assertEqual(misc.formatLang(self.env, 100), '100.00', 'If digits is None (default value), it should default to 2')
+
+        # Default rounding is 'HALF_EVEN'
+        self.assertEqual(misc.formatLang(self.env, 100.205), '100.20')
+        self.assertEqual(misc.formatLang(self.env, 100.215), '100.22')
+
+    def test_grouping(self):
+        self.env["res.lang"].create({
+            "name": "formatLang Lang",
+            "code": "fLT",
+            "grouping": "[3,2,-1]",
+            "decimal_point": "!",
+            "thousands_sep": "?",
+        })
+
+        self.env['res.lang']._activate_lang('fLT')
+
+        self.assertEqual(misc.formatLang(self.env['res.lang'].with_context(lang='fLT').env, 1000000000, grouping=True), '10000?00?000!00')
+        self.assertEqual(misc.formatLang(self.env['res.lang'].with_context(lang='fLT').env, 1000000000, grouping=False), '1000000000.00')
+
+    def test_decimal_precision(self):
+        decimal_precision = self.env['decimal.precision'].create({
+            'name': 'formatLang Decimal Precision',
+            'digits': 3,  # We want .001 decimals to make sure the decimal precision parameter 'dp' is chosen.
+        })
+
+        self.assertEqual(misc.formatLang(self.env, 100, dp=decimal_precision.name), '100.000')
+
+    def test_currency_object(self):
+        currency_object = self.env['res.currency'].create({
+            'name': 'formatLang Currency',
+            'symbol': 'fL',
+            'rounding': 0.1,  # We want .1 decimals to make sure 'currency_obj' is chosen.
+            'position': 'after',
+        })
+
+        self.assertEqual(misc.formatLang(self.env, 100, currency_obj=currency_object), '100.0%sfL' % u'\N{NO-BREAK SPACE}')
+
+        currency_object.write({'position': 'before'})
+
+        self.assertEqual(misc.formatLang(self.env, 100, currency_obj=currency_object), 'fL%s100.0' % u'\N{NO-BREAK SPACE}')
+
+    def test_decimal_precision_and_currency_object(self):
+        decimal_precision = self.env['decimal.precision'].create({
+            'name': 'formatLang Decimal Precision',
+            'digits': 3,
+        })
+
+        currency_object = self.env['res.currency'].create({
+            'name': 'formatLang Currency',
+            'symbol': 'fL',
+            'rounding': 0.1,
+            'position': 'after',
+        })
+
+        # If we have a 'dp' and 'currency_obj', we use the decimal precision of 'dp' and the format of 'currency_obj'.
+        self.assertEqual(misc.formatLang(self.env, 100, dp=decimal_precision.name, currency_obj=currency_object), '100.000%sfL' % u'\N{NO-BREAK SPACE}')
