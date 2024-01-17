2ade29ad5a240d8a7da2fd61f65f6080cfe2e74a
Julien Castiaux <juc@odoo.com>
2024-01-17 12:49:12 +0000

    [FIX] mail: support hebrew charset iso-8859-8-i
    One of our customers is receiving emails with headers and attachments
    encoded using the "iso-8859-8-i" charset instead of "iso-8859-8" which
    is natively supported by Python. Both encoding are using the same
    character set[^1] and only differ in the way the text is rendered on
    screen[^2][^3] which is not revelant for Python.
    
    Add an alias for iso-8859-8-i so that the emails that this customer
    receive stop failing in Odoo. Note that there is a PR opened on
    CPython for exactly that, see [bpo-18624].
    
    opw-3653210
    [bpo-18624]: https://bugs.python.org/issue18624
    [^1]: https://encoding.spec.whatwg.org/#legacy-single-byte-encodings
    [^2]: <data:text/html;charset=iso-8859-8,hello%20%E0%E1%E2%E3>
    [^3]: <data:text/html;charset=iso-8859-8-i,hello%20%E0%E1%E2%E3>
    
    closes odoo/odoo#150467
    
    X-original-commit: 5a025467a605ca4fa963b79bae254c823518d54c
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- odoo/__init__.py
+++ odoo/__init__.py
@@ -85,6 +85,18 @@ try:
 except ImportError:
     pass # no fix required
 
+
+#----------------------------------------------------------
+# alias hebrew iso-8859-8-i and iso-8859-8-e on iso-8859-8
+# https://bugs.python.org/issue18624
+#----------------------------------------------------------
+import codecs
+import re
+
+iso8859_8 = codecs.lookup('iso8859_8')
+iso8859_8ie_re = re.compile(r'iso[-_]?8859[-_]8[-_]?[ei]', re.IGNORECASE)
+codecs.register(lambda charset: iso8859_8 if iso8859_8ie_re.match(charset) else None)
+
 #----------------------------------------------------------
 # Shortcuts
 #----------------------------------------------------------
