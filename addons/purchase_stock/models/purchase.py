9ec1a634f7485c1b65d6145753345df98259ee44
Djamel Touati <otd@odoo.com>
2024-01-18 18:40:53 +0100

    [FIX] purchase_stock: allow updating the purchase qty
    Steps to reproduce the bug:
    - Create a storable product P1:
        - Create a BoM:
            - type: subcontracting
            - Add vendor
            - Add any component
    - Create a purchase order with 3 units of P1
    - Confirm the purchase order
    - Go to the picking
    - receive 2 units of P1 and validate it
    - create a backorder
    - Cancel the backorder
    - Try to update the purchased qty in the PO line
    
    Problem:
    A user error is triggered:
    “Nothing to check the availability for.”
    
    When updating the quantity in the purchase order line, the
    "_create_or_update_picking" function is called to search for a linked
    picking and update its quantity. However, in our case, since no picking
    can be updated, a new picking is created:
    https://github.com/odoo/odoo/blob/783048616362702f/addons/purchase_stock/models/purchase.py#L271-L272
    
    Subsequently, the moves are confirmed:
    https://github.com/odoo/odoo/blob/e38ed7c780d35fb9d5d799ac783048616362702f/addons/purchase_stock/models/purchase.py#L277
    
    However, as the product is subcontracted, the "action_assign" function
    is called: https://github.com/odoo/odoo/blob/4055293873132d56fe3787c77e499808b136f300/addons/mrp_subcontracting/models/stock_move.py#L105-L106
    
    Since there is no move in the confirmed state, a user error is
    triggered: https://github.com/odoo/odoo/blob/d45996c9155b9576c74aa8c6c6f718f8ab4d0af6/addons/stock/models/stock_picking.py#L450-L453
    
    Solution:
    When updating the quantity in the purchase order, if no picking requires
    an update, it is better to avoid creating a new picking.
    
    opw-3616381

--- addons/purchase_stock/models/purchase.py
+++ addons/purchase_stock/models/purchase.py
@@ -448,7 +448,7 @@ class PurchaseOrderLine(models.Model):
                 # If the user increased quantity of existing line or created a new line
                 pickings = line.order_id.picking_ids.filtered(lambda x: x.state not in ('done', 'cancel') and x.location_dest_id.usage in ('internal', 'transit', 'customer'))
                 picking = pickings and pickings[0] or False
-                if not picking:
+                if not picking and line.product_qty > line.qty_received:
                     res = line.order_id._prepare_picking()
                     picking = self.env['stock.picking'].create(res)
 
