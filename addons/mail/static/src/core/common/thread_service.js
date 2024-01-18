f63643bebfc21393128360064b16717018900bdc
Alexandre Kühn <aku@odoo.com>
2024-01-18 12:50:43 +0000

    [FIX] mail: new message notif on channel only with needaction
    Before this commit, any new message in a channel of type "channel"
    was automatically opening a chat window and showing new message
    counter in tab title when out-of-focus.
    
    This behaviour is only intended for important messages. In chat
    (group chat, DM chat, livechat), all new messages are considered
    as important so this is good. However, for channels, these are
    intended for communication with many users, and we only want to
    notify on messages that are explicitly flagged as "needaction".
    As a reminder, message are needaction through `@mention` or
    reply-to for example.
    
    This commit fixes the issue by limiting notifying out-of-focus of
    new messages in channel "channel" to only needaction. Also the
    auto-opening of chat window as a consequence from this new message
    is also limited to needaction messages.
    
    closes odoo/odoo#150070
    
    X-original-commit: c22fa196364b712b66c2faaacf2f85c6d9a83cf4
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/thread_service.js
+++ addons/mail/static/src/core/common/thread_service.js
@@ -776,13 +776,9 @@ export class ThreadService {
      * @param {Message} message
      */
     notifyMessageToUser(thread, message) {
-        if (
-            thread.type === "channel" &&
-            message.recipients?.includes(this.store.self) &&
-            message.notIn(thread.needactionMessages)
-        ) {
-            thread.needactionMessages.add(message);
-            thread.message_needaction_counter++;
+        let notify = thread.type !== "channel";
+        if (thread.type === "channel" && message.recipients?.includes(this.store.self)) {
+            notify = true;
         }
         if (
             thread.correspondent?.eq(this.store.odoobot) ||
@@ -793,8 +789,10 @@ export class ThreadService {
         ) {
             return;
         }
-        this.store.ChatWindow.insert({ thread });
-        this.outOfFocusService.notify(message, thread);
+        if (notify) {
+            this.store.ChatWindow.insert({ thread });
+            this.outOfFocusService.notify(message, thread);
+        }
     }
 
     /**
