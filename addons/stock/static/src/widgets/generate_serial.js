98f1992f9ce5cb9eb590bd50b42ce07e24c3b919
snd <snd@odoo.com>
2024-01-18 17:08:34 +0100

    [IMP] stock: add generate button for lots
    This PR makes it possible to generate lot names/numbers in batch from operations in stock picking.
    It uses the same `count` field as generate serials to ask for a specific quantity per lot.
    A new field `totalReceived` is used to indicate how much is received in total. This is necessary
    in cases of partial reception.
    The value indicated in `count` will be assigned to as many lots as possible without exceeding
    `totalReceived`, with the leftover assigned to a last lot.
    The formula: `number_of_lots_generated = (totalReceived // quantity_per_lot) + 1` (if there's leftover).
    The rest works the same as the generate serials flow.
    
    task 3506686
    
    closes odoo/odoo#150707
    
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/stock/static/src/widgets/generate_serial.js
+++ addons/stock/static/src/widgets/generate_serial.js
@@ -14,34 +14,52 @@ export class GenerateDialog extends Component {
     static template = "stock.generate_serial_dialog";
     static components = { Dialog };
     static props = {
-        type: { type: String },
+        mode: { type: String },
         move: { type: Object },
         close: { type: Function },
     };
     setup() {
         this.size = 'md';
-        if (this.props.type === 'serial') {
-            this.title = _t("Generate Serials numbers");
+        if (this.props.mode === 'generate') {
+            this.title = this.props.move.data.has_tracking === 'lot'
+            ? _t("Generate Lot numbers")
+            : _t("Generate Serial numbers");
         } else {
-            this.title = _t("Import Lots");
+            this.title = this.props.move.data.has_tracking === 'lot' ? _t("Import Lots") : _t("Import Serials");
         }
 
         this.nextSerial = useRef('nextSerial');
         this.nextSerialCount = useRef('nextSerialCount');
+        this.totalReceived = useRef('totalReceived');
         this.keepLines = useRef('keepLines');
         this.lots = useRef('lots');
         this.orm = useService("orm");
         onMounted(() => {
-            if (this.props.type === 'serial') {
+            if (this.props.mode === 'generate') {
                 this.nextSerialCount.el.value = this.props.move.data.product_uom_qty || 2;
+                if (this.props.move.data.has_tracking === 'lot') {
+                    this.totalReceived.el.value = this.props.move.data.quantity;
+                }
             }
         });
     }
     async _onGenerate() {
-        const count = parseInteger(this.nextSerialCount.el?.value || '0');
-        const move_line_vals = await this.orm.call("stock.move", "action_generate_lot_line_vals", [
-            {...this.props.move.context, default_product_id: this.props.move.data.product_id[0]},
-            this.props.type,
+        let count;
+        let qtyToProcess;
+        if (this.props.move.data.has_tracking === 'lot'){
+            count = parseFloat(this.nextSerialCount.el?.value || '0');
+            qtyToProcess = parseFloat(this.totalReceived.el.value || this.props.move.data.product_qty);
+        } else {
+            count = parseInteger(this.nextSerialCount.el?.value || '0');
+            qtyToProcess = this.props.move.data.product_qty;
+        }
+        const move_line_vals = await this.orm.call("stock.move", "action_generate_lot_line_vals", [{
+                ...this.props.move.context,
+                default_product_id: this.props.move.data.product_id[0],
+                default_tracking: this.props.move.data.has_tracking,
+                default_quantity: qtyToProcess,
+            },
+            this.props.mode,
             this.nextSerial.el?.value,
             count,
             this.lots.el?.value,
@@ -78,7 +96,7 @@ export class GenerateDialog extends Component {
 }
 
 class GenerateSerials extends Component {
-    static template = xml`<button class="btn btn-link" t-on-click="openDialog">Generate Serials</button>`;
+    static template = xml`<button class="btn btn-link" t-on-click="openDialog">Generate Serials/Lots</button>`;
     static props = {...standardWidgetProps};
 
     setup(){
@@ -88,15 +106,14 @@ class GenerateSerials extends Component {
     openDialog(ev){
         this.dialog.add(GenerateDialog, {
             move: this.props.record,
-            type: 'serial',
+            mode: 'generate',
         });
     }
 }
 
 class ImportLots extends Component {
-    static template = xml`<button class="btn btn-link" t-on-click="openDialog">Import Lots</button>`;
+    static template = xml`<button class="btn btn-link" t-on-click="openDialog">Import Serials/Lots</button>`;
     static props = {...standardWidgetProps};
-
     setup(){
         this.dialog = useService("dialog");
     }
@@ -104,7 +121,7 @@ class ImportLots extends Component {
     openDialog(ev){
         this.dialog.add(GenerateDialog, {
             move: this.props.record,
-            type: 'import',
+            mode: 'import',
         });
     }
 }
