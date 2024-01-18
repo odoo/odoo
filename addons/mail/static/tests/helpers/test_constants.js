3fc7f3b1c8f40436c3843579a75e7559984cc966
Alexandre Kühn <aku@odoo.com>
2024-01-18 14:32:05 +0000

    [FIX] mail: open chat window from home menu reliably
    Before this commit, opening chat window from messaging menu
    might not work.
    
    Step to reproduce:
    - open Discuss app
    - Go to home menu
    - Reload the page
    - Click on Messaging Menu
    - Click on chat item to open chat
    => Chat window is not open
    
    This happens because when in the home menu from Discuss app
    in background page reload, the URL contains the Discuss app
    menu_id, but the action is "menu" rather the action id. This
    difference is crucial to distinct discuss app being actively
    open or it's in the background from home menu. The latter
    should NOT consider Discuss app being open.
    
    This is not a problem when opening/closing Discuss app, because
    the mounting/unmounting of the Discuss app component is good
    enough to detect that. However, with page reload, the way to
    detect Discuss app being open from URL was only relying on `menu_id`
    instead of `action` value.
    
    This commit fixes the issue by checking `active_id` of discuss app
    rather than `menu_id`, as `active_id` is 100% reliable whereas
    `menu_id` is not. The problem with home menu is one example among
    many other cases (e.g. page reload in channel settings form view).
    
    closes odoo/odoo#150578
    
    X-original-commit: d37dadddeed862a8984269a79aa42cb16070f815
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

---
+++ addons/mail/static/tests/helpers/test_constants.js
@@ -0,0 +1,3 @@
+/* @odoo-module */
+
+export const DISCUSS_ACTION_ID = 104;
--- addons/mail/static/tests/helpers/test_utils.js
+++ addons/mail/static/tests/helpers/test_utils.js
@@ -5,6 +5,7 @@ import { timings } from "@bus/misc";
 
 import { loadLamejs } from "@mail/discuss/voice_message/common/voice_message_service";
 import { patchBrowserNotification } from "@mail/../tests/helpers/patch_notifications";
+import { DISCUSS_ACTION_ID } from "@mail/../tests/helpers/test_constants";
 import { getAdvanceTime } from "@mail/../tests/helpers/time_control";
 import { getWebClientReady } from "@mail/../tests/helpers/webclient_setup";
 
@@ -34,8 +35,7 @@ function getOpenDiscuss(webClient, { context = {}, params = {}, ...props } = {})
     return async function openDiscuss(pActiveId) {
         const actionOpenDiscuss = {
             context: { ...context, active_id: pActiveId },
-            // hardcoded actionId, required for discuss_container props validation.
-            id: 104,
+            id: DISCUSS_ACTION_ID,
             params,
             tag: "mail.action_discuss",
             type: "ir.actions.client",
