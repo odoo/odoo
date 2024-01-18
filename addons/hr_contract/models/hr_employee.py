8faed465fc78254f8619ffe2c9e70236f7dc5611
Levi Siuzdak (sile) <sile@odoo.com>
2024-01-18 11:41:28 +0100

    [FIX] hr{,_contract,_holidays}: default to company calendar
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Go to Employees app as admin;
    2. clear the "Working Hours" field & save;
    3. go to Time Off app.
    
    Issue
    -----
    Odoo Server Error.
    
    Cause
    -----
    Commit 8f87e102a95412aa7dd1b0ce07365d9d3bbdba6a added the
    `_get_consumed_leaves` method to `hr.employee`, which calls on
    `resource.calendar` methods with `ensure_one()` active. The
    `resource_calendar_id` is not a required field for employees, so an
    error occurs when these methods are called on an empty record.
    
    Solution
    --------
    Default to `company_id.resource_calendar_id` where a calendar is
    expected.
    
    Also fixes a potential issue in `hr_contract` when getting attendances
    between a time interval that includes multiple contracts using different
    calendars.
    
    opw-3665412
    
    closes odoo/odoo#149908
    
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/hr_contract/models/hr_employee.py
+++ addons/hr_contract/models/hr_employee.py
@@ -145,13 +145,21 @@ class Employee(models.Model):
         valid_contracts = self.sudo()._get_contracts(date_from, date_to, states=['open', 'close'])
         if not valid_contracts:
             return super()._get_calendar_attendances(date_from, date_to)
-        employee_timezone = timezone(self.tz) if self.tz else None
-        return valid_contracts.resource_calendar_id\
-            .with_context(employee_timezone=employee_timezone)\
-            .get_work_duration_data(
-                date_from,
-                date_to,
-                domain=[('company_id', 'in', [False, self.company_id.id])])
+        employee_tz = timezone(self.tz) if self.tz else None
+        duration_data = {'days': 0, 'hours': 0}
+        for contract in valid_contracts:
+            contract_start = datetime.combine(contract.date_start, time.min, employee_tz)
+            contract_end = datetime.combine(contract.date_end or date.max, time.max, employee_tz)
+            calendar = contract.resource_calendar_id or contract.company_id.resource_calendar_id
+            contract_duration_data = calendar\
+                .with_context(employee_timezone=employee_tz)\
+                .get_work_duration_data(
+                    max(date_from, contract_start),
+                    min(date_to, contract_end),
+                    domain=[('company_id', 'in', [False, contract.company_id.id])])
+            duration_data['days'] += contract_duration_data['days']
+            duration_data['hours'] += contract_duration_data['hours']
+        return duration_data
 
     def write(self, vals):
         res = super().write(vals)
