7f8237e37db2a13ada1c6288e8e3e719822290a9
Simon Genin (ges) <ges@odoo.com>
2024-01-18 13:24:20 +0100

    [FIX] web: kanban column quick create validates on enter
    When creating a new column in kanban view, the user should be able to
    either hit enter or click the add button. This launched the validation
    process to add the column.
    
    There has been a fix in owl that would force a t-model.trim to be lazy.
    This was causing problems in this case as the "enter" pressed by the
    user was detected through the hotkey system. This was causing the
    change event not to fire, resulting in the behavior of the feature being
    broken.
    
    We fix this by using the t-on-keydown directive instead of the hotkey.
    We also remove the t-model directive as it is no longer of any use.
    The trim is done in the validate function.
    
    task id 3691208
    
    X-original-commit: e3ad26632b00d0b0dded25282e4dc910983ef9dc

--- addons/web/static/src/views/kanban/kanban_column_quick_create.js
+++ addons/web/static/src/views/kanban/kanban_column_quick_create.js
@@ -11,10 +11,11 @@ export class KanbanColumnQuickCreate extends Component {
         this.dialog = useService("dialog");
         this.root = useRef("root");
         this.state = useState({
-            columnTitle: "",
+            hasInputFocused: false,
         });
 
         useAutofocus();
+        this.inputRef = useRef("autofocus");
 
         // Close on outside click
         useExternalListener(window, "mousedown", (/** @type {MouseEvent} */ ev) => {
@@ -37,11 +38,6 @@ export class KanbanColumnQuickCreate extends Component {
         );
 
         // Key Navigation
-        const inputRef = useRef("autofocus");
-        useHotkey("enter", () => this.validate(), {
-            area: () => inputRef.el,
-            bypassEditableProtection: true,
-        });
         useHotkey("escape", () => this.fold());
     }
 
@@ -64,9 +60,10 @@ export class KanbanColumnQuickCreate extends Component {
     }
 
     validate() {
-        if (this.state.columnTitle.length) {
-            this.props.onValidate(this.state.columnTitle);
-            this.state.columnTitle = "";
+        const title = this.inputRef.el.value.trim();
+        if (title.length) {
+            this.props.onValidate(title);
+            this.inputRef.el.value = "";
         }
     }
 
@@ -81,6 +78,12 @@ export class KanbanColumnQuickCreate extends Component {
                 }
             },
         });
+    }
+
+    onInputKeydown(ev) {
+        if (ev.key === "Enter") {
+            this.validate();
+        }
     }
 }
 KanbanColumnQuickCreate.props = {
