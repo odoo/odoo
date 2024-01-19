297dbfd4d96ea8eda1b7d61f5d698feab90dea6b
tsm-odoo <tsm@odoo.com>
2024-01-19 08:27:33 +0100

    [FIX] mail: fix non deterministic retry loading messages test
    Before this PR, the "Retry loading more messages on failed load more
    messages" test was sometimes failing. This test uses the `afterEvent`
    helper to wait for the load message event to occur. This is too
    technical and not reliable: this test just ensures that clicking on
    the retry button after failing to load more messages work as expected.
    This PR simplify this test and make use of the `contains` helper which
    is more reliable to do so. First, 30 messages are loaded, then the
    retry button appears and finally, clicking on the retry button should
    load 30 more messages for a total of 60.
    
    fixes runbot-53201
    
    closes odoo/odoo#150312
    
    X-original-commit: 4380131b5251240bd87aab114a6454d75dcbad67
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/tests/discuss_app/discuss_tests.js
+++ addons/mail/static/tests/discuss_app/discuss_tests.js
@@ -1658,6 +1658,7 @@ QUnit.test(
         await contains(".o-mail-Message", { count: 30 });
         messageFetchShouldFail = true;
         await click("button", { text: "Load More" });
+        await contains("button", { text: "Click here to retry" });
         messageFetchShouldFail = false;
         await click("button", { text: "Click here to retry" });
         await contains(".o-mail-Message", { count: 60 });
