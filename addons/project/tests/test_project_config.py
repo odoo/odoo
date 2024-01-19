c799256f9c1c6321d3ab2bbb2ab83551b38d73b3
dise <dise@odoo.com>
2024-01-19 10:53:40 +0100

    [IMP] project: add test to check that project stages enables views
    This test will check that the Gantt, Calendar and Activities views are
    enabled when the 'Project Stages' feature is on.
    
    task-3231698
    
    Part-of: odoo/odoo#143745

--- addons/project/tests/test_project_config.py
+++ addons/project/tests/test_project_config.py
@@ -57,3 +57,12 @@ class TestProjectConfig(TestProjectCommon):
                 project_cows[project_flag],
                 f"Newly created project failed to adopt activation of "
                 f"{config_flag}/{project_flag} feature")
+
+    def test_project_stages_feature_enable_views(self):
+        """Check that the Gantt, Calendar and Activities views are
+        enabled when the 'Project Stage' feature is enabled.
+        """
+        self.Settings.create({"group_project_stages": True}).execute() # enabling feature
+        menu_ids = set([self.env.ref('project.menu_projects').id, self.env.ref('project.menu_projects_config').id])
+        menu_loaded = set(self.env['ir.ui.menu']._load_menus_blacklist())
+        self.assertTrue(menu_ids.issubset(menu_loaded), "The menu project and menu projects config should be loaded")
