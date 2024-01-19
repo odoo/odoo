965630c4308c503c3c5b282ec4e4e77a862bbfc6
Christophe Monniez <moc@odoo.com>
2024-01-19 14:55:10 +0100

    [FIX] barcodes_gs1_nomenclature: deduplicate tests
    When inheriting from a test class having tests methods, the tests are
    played twice. In this particular case, the inheritance was not necessary
    at all.
    
    closes odoo/odoo#150287
    
    X-original-commit: 839fa009f3f032cb45c56acfe237933bf0fb69c9
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

--- addons/barcodes_gs1_nomenclature/tests/test_barcodes_gs1_nomenclature.py
+++ addons/barcodes_gs1_nomenclature/tests/test_barcodes_gs1_nomenclature.py
@@ -1,8 +1,8 @@
-from odoo.addons.barcodes.tests.test_barcode_nomenclature import TestBarcodeNomenclature
 from odoo.exceptions import ValidationError
+from odoo.tests.common import TransactionCase
 
 
-class TestBarcodeGS1Nomenclature(TestBarcodeNomenclature):
+class TestBarcodeGS1Nomenclature(TransactionCase):
     def test_gs1_date_to_date(self):
         barcode_nomenclature = self.env['barcode.nomenclature']
         # 20/10/2015 -> 151020
