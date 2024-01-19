4b71d8451fabe9b8c83f9391cdd5354d54a05608
Joseph Caburnay <jcb@odoo.com>
2024-01-19 17:34:27 +0100

    [FIX] point_of_sale: runbot error pricelist
    After
    https://github.com/odoo/odoo/commit/54a6f1688fb8d15b662848b03cecbe8236292d07,
    the `TestConfigureShops.test_should_not_affect_other_pos_config` and
    `TestConfigureShops.test_is_header_or_footer_to_false` started to fail randomly
    in runbot. This is because when res.config.settings form initializes, it may
    start with `pos_config_id` that contains pricelists that are not compatible to
    the currency of the pos_config that will be selected in the form.
    
    This commit makes sure that when the `pos_config_id` is changed to something
    that doesn't use pricelist, the `pos_available_pricelist_ids` field is updated
    accordingly.
    
    Runbot Errors: 31294, 31293
    
    closes odoo/odoo#150423
    
    X-original-commit: f271b0e373b29b7d708c4fab454318776e1bee91
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/point_of_sale/models/res_config_settings.py
+++ addons/point_of_sale/models/res_config_settings.py
@@ -273,6 +273,7 @@ class ResConfigSettings(models.TransientModel):
             pricelists_in_current_currency = self.env['product.pricelist'].search([('company_id', 'in', (False, res_config.pos_config_id.company_id.id)), ('currency_id', '=', currency_id)])
             if not res_config.pos_use_pricelist:
                 res_config.pos_pricelist_id = False
+                res_config.pos_available_pricelist_ids = res_config.pos_config_id.available_pricelist_ids
             else:
                 if any([p.currency_id.id != currency_id for p in res_config.pos_available_pricelist_ids]):
                     res_config.pos_available_pricelist_ids = pricelists_in_current_currency
