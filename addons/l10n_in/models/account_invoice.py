58f56756aee8e8e3e6c325b70bd92603c715ca1b
Xavier ALT <xal@odoo.com>
2024-01-19 17:21:38 +0100

    [FIX] l10n_in: make invoice template primary
    Avoid specific changes of l10n_in invoice report to affect other
    countries.
    
    OPW-2504287

--- addons/l10n_in/models/account_invoice.py
+++ addons/l10n_in/models/account_invoice.py
@@ -88,6 +88,14 @@ class AccountMove(models.Model):
         """Overwrite in sale"""
         return shipping_partner.vat
 
+    def _get_name_invoice_report(self):
+        if self.country_code == 'IN':
+            # TODO: remove the view mode check in master, only for stable releases
+            in_invoice_view = self.env.ref('l10n_in.l10n_in_report_invoice_document_inherit', raise_if_not_found=False)
+            if (in_invoice_view and in_invoice_view.mode == "primary"):
+                return 'l10n_in.l10n_in_report_invoice_document_inherit'
+        return super()._get_name_invoice_report()
+
     def _post(self, soft=True):
         """Use journal type to define document type because not miss state in any entry including POS entry"""
         posted = super()._post(soft)
