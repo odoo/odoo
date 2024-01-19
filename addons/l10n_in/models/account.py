cbca6c812f70182c142a518268ac70a999afcc87
Laurent Smet <las@odoo.com>
2024-01-19 12:56:46 +0000

    [IMP] l10n_in: add hsn summary to invoice report
    task_id: 3196107
    
    closes odoo/odoo#155662
    
    X-original-commit: 523ff524e2ec96502909c4b8d14e9d09c1907c58
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Co-authored-by: Harsh Modi <hamo@odoo.com>

--- addons/l10n_in/models/account.py
+++ addons/l10n_in/models/account.py
@@ -2,8 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
 from odoo import tools
+from odoo.tools import frozendict
 
 
 class AccountMoveLine(models.Model):
@@ -56,3 +56,94 @@ class AccountTax(models.Model):
             tax_values['_l10n_in_tax_type'] = l10n_in_tax_type
 
         return tax_values
+
+    # -------------------------------------------------------------------------
+    # HELPERS IN BOTH PYTHON/JAVASCRIPT (hsn_summary.js / account_tax.py)
+
+    # HSN SUMMARY
+    # -------------------------------------------------------------------------
+
+    @api.model
+    def _l10n_in_get_hsn_summary_table(self, base_lines, display_uom):
+        results_map = {}
+        l10n_in_tax_types = set()
+        for base_line in base_lines:
+            l10n_in_hsn_code = base_line['l10n_in_hsn_code']
+            if not l10n_in_hsn_code:
+                continue
+
+            price_unit = base_line['price_unit']
+            quantity = base_line['quantity']
+            uom = base_line['uom'] or {}
+            tax_values_list = base_line['tax_values_list']
+
+            # Compute the taxes.
+            evaluation_context = self.env['account.tax']._eval_taxes_computation_prepare_context(
+                price_unit,
+                quantity,
+                rounding_method='round_per_line',
+                precision_rounding=0.01,
+            )
+            taxes_computation = self.env['account.tax']._eval_taxes_computation(
+                self.env['account.tax']._prepare_taxes_computation(tax_values_list),
+                evaluation_context,
+            )
+
+            # Rate.
+            rate = sum(
+                tax_values['amount']
+                for tax_values in taxes_computation['tax_values_list']
+                if tax_values['_l10n_in_tax_type'] in ('igst', 'cgst', 'sgst')
+            )
+
+            key = frozendict({
+                'l10n_in_hsn_code': l10n_in_hsn_code,
+                'rate': rate,
+                'uom_name': uom.get('name'),
+            })
+
+            if key in results_map:
+                values = results_map[key]
+                values['quantity'] += quantity
+                values['amount_untaxed'] += taxes_computation['total_excluded']
+            else:
+                results_map[key] = {
+                    **key,
+                    'quantity': quantity,
+                    'amount_untaxed': taxes_computation['total_excluded'],
+                    'tax_amounts': {
+                        'igst': 0.0,
+                        'cgst': 0.0,
+                        'sgst': 0.0,
+                        'cess': 0.0,
+                    },
+                }
+
+            for tax_values in taxes_computation['tax_values_list']:
+                l10n_in_tax_type = tax_values['_l10n_in_tax_type']
+                if l10n_in_tax_type:
+                    results_map[key]['tax_amounts'][l10n_in_tax_type] += tax_values['tax_amount_factorized']
+                    l10n_in_tax_types.add(l10n_in_tax_type)
+
+        items = [
+            {
+                'l10n_in_hsn_code': value['l10n_in_hsn_code'],
+                'uom_name': value['uom_name'],
+                'rate': value['rate'],
+                'quantity': value['quantity'],
+                'amount_untaxed': value['amount_untaxed'],
+                'tax_amount_igst': value['tax_amounts']['igst'],
+                'tax_amount_cgst': value['tax_amounts']['cgst'],
+                'tax_amount_sgst': value['tax_amounts']['sgst'],
+                'tax_amount_cess': value['tax_amounts']['cess'],
+            }
+            for value in results_map.values()
+        ]
+        return {
+            'has_igst': 'igst' in l10n_in_tax_types,
+            'has_gst': bool({'cgst', 'sgst'} & l10n_in_tax_types),
+            'has_cess': 'cess' in l10n_in_tax_types,
+            'nb_columns': 5 + len(l10n_in_tax_types),
+            'display_uom': display_uom,
+            'items': items,
+        }
