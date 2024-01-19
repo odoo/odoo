364fe5fec1f27a885b5397391ddb89d3e889beb3
Sven Fuehr <svfu@odoo.com>
2024-01-19 07:53:44 +0100

    [FIX] account: fix balance / amount_currency sync during write
    Currently it can happen that slightly changing the amount in (document)
    currency (field `amount_currency`) i.e. on a tax line can lead to an
    unbalanced move. (See example and "Reproduce" below.)
    
    The issue is the syncing (function `_sync_invoice`) between
    - amount in currency (document currency; field `amount_currency`) and
    - balance (company currency; field `balance`).
    
    There we update the amount in currency in case the balance is changed
    and the amount in currency is not changed.
    But this is problematic in case we want (in the same write) set
    a different amount in currency but explicitly set the same balance
    (and not just not write anything to the `balance` field).
    Since we write the same balance it is not detected as a
    change and we update the balance based on the amount in currency.
    But this should not happen; the balance should be "protected"
    from being updated (since it is the value we explicitly want).
    
    This can i.e. cause the balance of the receivable line to receive the wrong value
    I.e. consider the following case; with currency rate = 2 (also see "Reproduce" below)
    
    Journal Items:
    - product  1: amount in currency = -1000.00 €, balance = -500.00
    - product  2: amount in currency =  -200.00 €, balance = -100.00
    - tax line 1: amount in currency =  -180.03 €, balance =  -90.02
    - tax line 2: amount in currency =   -30.00 €, balance =  -15.00
    - Receivable: amount in currency = -1410.03 €, balance =  705.02
    
    And we modify tax line 2: amount in currency = -29.99 €.
    Then first we set the correct values because we basically compute the
    receivable line by summing the other lines:
    
    Journal Items:
    - product  1: amount in currency = -1000.00 €, balance = -500.00
    - product  2: amount in currency =  -200.00 €, balance = -100.00
    - tax line 1: amount in currency =  -180.03 €, balance =  -90.02
    - tax line 2: amount in currency =   -29.99 €, balance =  -15.00
    - Receivable: amount in currency = -1410.02 €, balance =  705.02
    
    So we have set the balance to the same as before but changed the
    amount in currency. So then we recompute the balance on the receivable
    line from the amount in currency due to the syncing (`_sync_invoice`)
    
    Journal Items:
    - product  1: amount in currency = -1000.00 €, balance = -500.00
    - product  2: amount in currency =  -200.00 €, balance = -100.00
    - tax line 1: amount in currency =  -180.03 €, balance =  -90.02
    - tax line 2: amount in currency =   -29.99 €, balance =  -15.00
    - Receivable: amount in currency = -1410.02 €, balance =  705.01
    
    But now the move is not balanced (the sum of the balances should be 0)
      -500.00 + -100.00 + -90.02 + -15.00 = -705.02 (and not -705.01)
    
    After this commit we "protect" the balance and amount in currency from
    being updated by `_sync_invoice` in case the surrounding `write`
    sets the balance (`balance`) or amount in currency (`amount_currency`)
    respectively.
    
    Reproduce: (c.f. example above)
      1. Select company "My Company (San Francisco)"
      2. Ensure EUR currency is activated and has value 2 unit per USD
         (at the time of the invoice that will be created further below)
      3. Copy the 15% tax
      4. Create a new invoice in EUR with 2 lines
         (1) qty = 1, unit price = 1000, taxes = 15%
         (2) qty = 1, unit price = 200, taxes = 15%, 15% (Copy)
      5. There should be 2 tax lines in the "Journal Items" tab
         (1) amount in currency = -180.00 €, credit = 90.00 USD
         (2) amount in currency = -30.00 €, credit = 90.00 USD
      6. Modify tax line (1); set amount in currency to -180.03 €
         and save.
         The credit will be updated to 90.02 USD.
      7. Modify tax line (2); set amount in currency to -29.99 €
         and try to save
      8. An "Invalid Operation" error is raised:
         The move (Draft Invoice ) is not balanced.
         The total of debits equals $ 705.01 and the total of credits equals $ 705.02.

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -1417,9 +1417,11 @@ class AccountMoveLine(models.Model):
         before = existing()
         yield
         after = existing()
+        protected = container.get('protected', {})
         for line in after:
             if (
                 line.display_type == 'product'
+                and 'amount_currency' not in protected.get(line, {})
                 and (not changed('amount_currency') or line not in before)
             ):
                 amount_currency = line.move_id.direction_sign * line.currency_id.round(line.price_subtotal)
@@ -1432,6 +1434,7 @@ class AccountMoveLine(models.Model):
         for line in after:
             if (
                 (changed('amount_currency') or changed('currency_rate') or changed('move_type'))
+                and 'balance' not in protected.get(line, {})
                 and (not changed('balance') or (line not in before and not line.balance))
             ):
                 balance = line.company_id.currency_id.round(line.amount_currency / line.currency_rate)
@@ -1452,6 +1455,7 @@ class AccountMoveLine(models.Model):
              self._sync_invoice(container):
             lines = super().create([self._sanitize_vals(vals) for vals in vals_list])
             container['records'] = lines
+            container['protected'] = {line: set(vals.keys()) for line, vals in zip(lines, vals_list)}
 
         for line in lines:
             if line.move_id.state == 'posted':
@@ -1507,7 +1511,7 @@ class AccountMoveLine(models.Model):
         move_container = {'records': self.move_id}
         with self.move_id._check_balanced(move_container),\
              self.move_id._sync_dynamic_lines(move_container),\
-             self._sync_invoice({'records': self}):
+             self._sync_invoice({'records': self, 'protected': {line: set(vals.keys()) for line in self}}):
             self = line_to_write
             if not self:
                 return True
