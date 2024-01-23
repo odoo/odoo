99968c81964e49437eef319d855927ece42f031a
Kartik Chavda (kcv) <kcv@odoo.com>
2024-01-23 11:10:39 +0530

    [FIX] sale_pdf_quote_builder: make header/footer name writable in settings
    Steps:
    - Install sale apps.
    - Upload a header file from settings with xyz.pdf for
    example.
    
    Issue:
    - Header/Footer file is not updated according to uploaded
    file name.
    
    Cause:
    - Header/Footer file name in setting is related and
    readonly is should not be readonly in order to update
    header file name.
    
    Fix:
    - Make settings header/footer file not readonly to
    set proper updated file names.
    
    task-3620555
    
    closes odoo/odoo#162524
    
    X-original-commit: b412d1174769e8ab21d1dd8080f559c6446bcce7
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale_pdf_quote_builder/wizards/res_config_settings.py
+++ addons/sale_pdf_quote_builder/wizards/res_config_settings.py
@@ -7,6 +7,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     sale_header = fields.Binary(related='company_id.sale_header', readonly=False)
-    sale_header_name = fields.Char(related='company_id.sale_header_name')
+    sale_header_name = fields.Char(related='company_id.sale_header_name', readonly=False)
     sale_footer = fields.Binary(related='company_id.sale_footer', readonly=False)
-    sale_footer_name = fields.Char(related='company_id.sale_footer_name')
+    sale_footer_name = fields.Char(related='company_id.sale_footer_name', readonly=False)
