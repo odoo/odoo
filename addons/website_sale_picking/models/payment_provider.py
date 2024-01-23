87aea72acf3f7677ec5ebb87d8eb4603d293db6d
Victor Feyens <vfe@odoo.com>
2024-01-23 12:58:40 +0100

    [FIX] website_sale_picking: onsite provider shown when carrier unavailable
    Loaded carriers (in sudo mode) did not consider the company for which
    the payment was being started.
    
    This commit makes sure that carriers from other companies do not impact
    the display of the onsite provider.
    
    Part-of: odoo/odoo#150670

--- addons/website_sale_picking/models/payment_provider.py
+++ addons/website_sale_picking/models/payment_provider.py
@@ -1,7 +1,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+
 from odoo.addons.website_sale_picking import const
+
 
 class PaymentProvider(models.Model):
     _inherit = 'payment.provider'
@@ -11,23 +13,30 @@ class PaymentProvider(models.Model):
     )
 
     @api.model
-    def _get_compatible_providers(self, *args, sale_order_id=None, website_id=None, **kwargs):
+    def _get_compatible_providers(self, company_id, *args, sale_order_id=None, website_id=None, **kwargs):
         """ Override of payment to exclude onsite providers if the delivery doesn't match.
 
+        :param int company_id: The company to which providers must belong, as a `res.company` id
         :param int sale_order_id: The sale order to be paid, if any, as a `sale.order` id
         :param int website_id: The provided website, as a `website` id
         :return: The compatible providers
         :rtype: recordset of `payment.provider`
         """
         compatible_providers = super()._get_compatible_providers(
-            *args, sale_order_id=sale_order_id, website_id=website_id, **kwargs)
+            company_id,
+            *args,
+            sale_order_id=sale_order_id,
+            website_id=website_id,
+            **kwargs,
+        )
         # Show on site picking only if delivery carriers onsite exists
         onsite_carriers = self.env['delivery.carrier'].search([
             ('website_published', '=', True),
             ('delivery_type', '=', 'onsite'),
+            ('company_id', 'in', [False, company_id]),
             '|',
                 ('website_id', '=?', website_id),
-                ('website_id', '=', False)
+                ('website_id', '=', False),
         ])
         order = self.env['sale.order'].browse(sale_order_id).exists()
 
