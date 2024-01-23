52ca915b95ad415ca42ee59d931c710d9c2f4f4f
Martin Quinteros <maq@adhoc.com.ar>
2024-01-23 12:17:43 -0300

    [FIX] Add l10n_latam_document_type_id on prerare_default_values method for debit notes wizards 	If we make a wrong credit note and we want to correct it we must generate a debit note related to it. 	Currently odoo only allows to generate debit notes from an invoice.
          Description of the issue/feature this PR addresses:
    
    	Steps to reproduce the error.
    	  1 Install the Argentine localization
    	  2 Create an invoice and post it
    	  3 From the invoice using the wizard create a credit note and post it.
    	  4 From the credit note open the wizard to create a debit note.
              5 On submit a the wizard we obtain a exeption "You can not use a credit_note document type with a invoice"
    	This happens because the debit note wizard use copy method without change the l10n_latam_document_type_id value.
    Current behavior before PR:
       When creating a debit note from a credit note get an error.
    
    Desired behavior after PR is merged:
     We can create a debit memo from a credit note.
    
    Adhoc tiket 69428 - 69854

--- addons/l10n_latam_invoice_document/wizards/account_debit_note.py
+++ addons/l10n_latam_invoice_document/wizards/account_debit_note.py
@@ -14,3 +14,11 @@ class AccountDebitNote(models.TransientModel):
             new_move._compute_l10n_latam_document_type()
             new_move._onchange_l10n_latam_document_type_id()
         return res
+
+    def _prepare_default_values(self, move):
+        default_values = super()._prepare_default_values(move)
+        document_types = self.env['l10n_latam.document.type'].search([('internal_type', '=', 'debit_note'),
+                                                                         ('country_id', '=', move.company_id.account_fiscal_country_id.id)])
+        default_values['l10n_latam_document_type_id'] = document_types and document_types[0].id
+
+        return default_values
