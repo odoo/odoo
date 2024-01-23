4673b029bbf3866197884d286f0bc9c0a536fcf6
Nasreddin Boulif (bon) <bon@odoo.com>
2024-01-23 13:03:28 +0000

    [FIX] mail: give additional values for partner creation
    Steps to reproduce:
    
      - Install `crm` module (for test purpose)
      - Create a new lead
      - Set an email address, phone number, company name and contact name
      - Save the lead
      - In the chatter, send a mail (with the default recipient checked)
    
    Issue:
    
      - The partner has only the email address set (also set as name).
      - The `contact name` on the lead is updated with the partner name
        (who is the email address).
    
    Cause:
    
      When sending a mail with the default recipient checked, the partner
      is created based only on the email address (therefore, name is same as
      email), and when assigning the new partner on the lead, the
      `contact name` is updated with the partner name (who is the email
      address).
    
    Solution:
    
      Alter the route `/mail/partner/from_email` and `/mail/message/post`
      so it can take or manage additional values for the creation of the
      partner.
    
    opw-3512045
    
    X-original-commit: 9115213f7b51ff9ff373d6ae792f57f54beb12b7

--- addons/mail/static/src/core/common/thread_service.js
+++ addons/mail/static/src/core/common/thread_service.js
@@ -963,14 +963,18 @@ export class ThreadService {
             ? this.messageService.getMentionsFromText(rawMentions, body)
             : undefined;
         const partner_ids = validMentions?.partners.map((partner) => partner.id);
-        let recipientEmails = [];
+        const recipientEmails = [];
+        const recipientAdditionalValues = {};
         if (!isNote) {
             const recipientIds = thread.suggestedRecipients
                 .filter((recipient) => recipient.persona && recipient.checked)
                 .map((recipient) => recipient.persona.id);
-            recipientEmails = thread.suggestedRecipients
+            thread.suggestedRecipients
                 .filter((recipient) => recipient.checked && !recipient.persona)
-                .map((recipient) => recipient.email);
+                .forEach((recipient) => {
+                    recipientEmails.push(recipient.email);
+                    recipientAdditionalValues[recipient.email] = recipient.defaultCreateValues;
+                });
             partner_ids?.push(...recipientIds);
         }
         return {
@@ -986,6 +990,7 @@ export class ThreadService {
                 partner_ids,
                 subtype_xmlid: subtype,
                 partner_emails: recipientEmails,
+                partner_additional_values: recipientAdditionalValues,
             },
             thread_id: thread.id,
             thread_model: thread.model,
