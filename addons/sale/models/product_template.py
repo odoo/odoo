f849f960fb7fe81a095ee98101863aae9455adf0
sbel-odoo <sbel@odoo.com>
2024-01-23 10:47:40 +0000

    [FIX] sale: check for conflicting product types
    Currently, it is possible to create a product which can be rentable and recurring. This fix intends to replicate the behavior of previous versions where it was impossible to save a product with both boxes enabled.
    
    Steps to reproduce:
    -------------------
    * Go to **Rental** App
    * Select Products
    * Create a new product
    * Enable Reccuring and Can be Rented
    * Save
    
    Why the fix:
    ------------
    https://github.com/odoo/enterprise/commit/2579b5896a4c5e949d30a0a51f18437adc2b5db2 removed the dependence of sale_temporal for sale_subcription. After that, there was no point in keeping the sale_temporal dependence in sale_renting. Thus, the dependence for sale_temporal has also been removed here: https://github.com/odoo/enterprise/commit/6c86a278dbc752009c0985702cb2927c26f5f636. The module sale_temporal was responsible for checking incompatible products types.
    
    Currently, both sale_renting and sale_subscription have the module sale in common. This commit is part of a 2-part fix and targets Odoo Community. It transfers the code responsible for the checking of incompatible product types in the module sale.
    
    Code transferred: https://github.com/odoo/enterprise/blob/4cf9e6f013f75c082c5b5b42b9d2591554b75503/sale_temporal/models/product_template.py#L15-L33
    
    opw-3678715
    
    X-original-commit: b708a909ac6567800de9a2fd9b1bf311afba586c
    
    tempo

--- addons/sale/models/product_template.py
+++ addons/sale/models/product_template.py
@@ -118,3 +118,27 @@ class ProductTemplate(models.Model):
                     'template': '/product/static/xls/product_template.xls'
                 }]
         return res
+
+    @api.model
+    def _get_incompatible_types(self):
+        return []
+
+    @api.constrains(lambda self: self._get_incompatible_types())
+    def _check_incompatible_types(self):
+        incompatible_types = self._get_incompatible_types()
+        if len(incompatible_types) < 2:
+            return
+        fields = self.env['ir.model.fields'].sudo().search_read(
+            [('model', '=', 'product.template'), ('name', 'in', incompatible_types)],
+            ['name', 'field_description'])
+        field_descriptions = {v['name']: v['field_description'] for v in fields}
+        field_list = incompatible_types + ['name']
+        values = self.read(field_list)
+        for val in values:
+            incompatible_fields = [f for f in incompatible_types if val[f]]
+            if len(incompatible_fields) > 1:
+                raise ValidationError(_(
+                    "The product (%s) has incompatible values: %s",
+                    val['name'],
+                    ','.join(field_descriptions[v] for v in incompatible_fields),
+                ))
