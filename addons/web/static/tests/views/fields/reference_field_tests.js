b3e5fc36e38d5038e79d916d853af336f5f1581f
Paul Stroobant <stpa@odoo.com>
2024-01-24 11:42:02 +0100

    [FIX] web: reload many2xautocomplete component on props update
    Steps to reproduce the issue:
    
    1. Open Menu Items
    2. Open the form to a menu
    3. Select an action type (e.g. ir.actions.report)
    4. Select another action type (e.g. ir.actions.act_url)
    5. Click on the field next to it
    6. Click on "Search More..."
    7. The results displayed match the first action type selected (report), not the second (act_url)
    
    Explanation:
    
    `res_model` is initiated in two functions: `useOpenMany2XRecord()` and `useSelectCreate()` in the `setup()` of the `Many2XAutocomplete` class. The value is never updated for as long as this instance of the field exists.
    
    Suggested fix:
    
    Everytime the key changes, a new instance of the field will replace the current one, calling its own `setup()` with the updated values.
    
    opw-3628017
    
    closes odoo/odoo#151019
    
    X-original-commit: 98ce791dd8f41ea4b22010d92080ed2f94fcdfaf
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Stroobant Paul (stpa) <stpa@odoo.com>

--- addons/web/static/tests/views/fields/reference_field_tests.js
+++ addons/web/static/tests/views/fields/reference_field_tests.js
@@ -14,6 +14,7 @@ import {
     nextTick,
 } from "@web/../tests/helpers/utils";
 import { makeView, setupViewRegistries } from "@web/../tests/views/helpers";
+import { Many2XAutocomplete } from "@web/views/fields/relational_utils";
 
 let target;
 let serverData;
@@ -522,6 +523,42 @@ QUnit.module("Fields", (hooks) => {
             target.querySelector(".o_field_widget[name=reference] input").value,
             "gold",
             "should contain a link with the new value"
+        );
+    });
+
+    QUnit.test("Many2One 'Search More...' updates on resModel change", async function (assert) {
+
+        // Patch the Many2XAutocomplete default search limit options
+        patchWithCleanup(Many2XAutocomplete.defaultProps, {
+            searchLimit: -1,
+        });
+
+        serverData.views = {
+            "product,false,list": '<tree><field name="display_name"/></tree>',
+            "product,false,search": '<search/>',
+        };
+
+        await makeView({
+            type: "form",
+            resModel: "partner",
+            serverData,
+            arch: '<form><field name="reference"/></form>',
+        });
+
+        // Selecting a relation
+        await editSelect(target.querySelector("div.o_field_reference"), "select.o_input", "partner_type");
+
+        // Selecting another relation
+        await editSelect(target.querySelector("div.o_field_reference"), "select.o_input", "product");
+
+        // Opening the Search More... option
+        await click(target.querySelector("div.o_field_reference"), "input.o_input");
+        await click(target.querySelector("div.o_field_reference"), ".o_m2o_dropdown_option_search_more");
+
+        assert.strictEqual(
+            target.querySelector("div.modal td.o_data_cell").innerText,
+            "xphone",
+            "The search more should lead to the values of product."
         );
     });
 
