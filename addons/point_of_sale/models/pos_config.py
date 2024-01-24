07cd4053547c0001addec86799420695d5699546
Aurelien van Delft (avd) <avd@odoo.com>
2024-01-24 13:06:09 +0100

    [FIX] point_of_sale: fix issues with x2many in settings
    The change introduced in 72f7c0d7d16a1b avoids recomputes
    when there is nothing that needs to be written on a pos.config.
    
    However, starting v17 the way x2many are managed changed and
    while the command to assign values on x2many from settings was
    [(6, 0, ids)], in v17 it's [[4,id],...,[4,id]]. The set command
    was replaced by the link command. This changes the returned value
    of the convert_to_cache method and made it impossible to change
    pos_config.x2many values in v17 because of the changes in 72f7c0d7d16a1b.
    
    To fix that, move the _keep_new_vals method from res_config_settings
    to pos_config and use _preprocess_x2many_vals_from_settings_view
    to change link commands to unlink + link commands.
    
    Steps to reproduce:
       - Install point_of_sale
       - Go to Settings > Point of Sale
       - Remove a payment method
       - Save
       - The payment method is still set on the pos.config.
    
    opw-3692150
    
    closes odoo/odoo#150979
    
    X-original-commit: 66607625111043e59c82f7a8a9f17dd2d50f1374
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Aurélien van Delft (avd) <avd@odoo.com>

--- addons/point_of_sale/models/pos_config.py
+++ addons/point_of_sale/models/pos_config.py
@@ -415,6 +415,7 @@ class PosConfig(models.Model):
         bypass_payment_method_ids_forbidden_change = self.env.context.get('bypass_payment_method_ids_forbidden_change', False)
 
         self._preprocess_x2many_vals_from_settings_view(vals)
+        vals = self._keep_new_vals(vals)
         opened_session = self.mapped('session_ids').filtered(lambda s: s.state != 'closed')
         if opened_session:
             forbidden_fields = []
@@ -503,6 +504,23 @@ class PosConfig(models.Model):
                 unlink_commands = [Command.unlink(_id) for _id in linked_ids]
 
                 vals[x2many_field] = unlink_commands + vals[x2many_field]
+
+    def _keep_new_vals(self, vals):
+        """ Keep values in vals that are different than
+        self's values.
+        """
+        from_settings_view = self.env.context.get('from_settings_view')
+        if not from_settings_view:
+            return vals
+        new_vals = {}
+        for field, val in vals.items():
+            config_field = self._fields.get(field)
+            if config_field:
+                cache_value = config_field.convert_to_cache(val, self)
+                record_value = config_field.convert_to_record(cache_value, self)
+                if record_value != self[field]:
+                    new_vals[field] = val
+        return new_vals
 
     def _get_forbidden_change_fields(self):
         forbidden_keys = ['module_pos_hr', 'module_pos_restaurant', 'available_pricelist_ids',
