odoo/odoo

commit 72f7c0d7d16a1b791bba61fd885261227f73d263

[FIX] point_of_sale: skip recomputes on settings change
In v16 the configuration of a pos_config got moved from Point of Sale
to Settings, with new settings fields related to pos_config fields.
The create method got overriden in point_of_sale
to write values on the current pos_config
after the creation of the new settings.

The side-effect of doing so is that each creation of a res_config_setting,
i.e. after changing any given setting (pos-related or not), triggers a
write on ~50 fields of the current pos_config. Among those fields there are
modifying_relations fields that trigger recomputes on pos.order. Because
pos.order can grow quite big in some databases (> 200.000 pos.orders), it
makes it almost impossible to change any setting in these database. They
reach a TimeoutError on SaaS/SH.

To fix this, this commit filter pos_fields_vals to only write new
values on pos_config. If no value got changed since the last settings
creation, no write is triggered, and no recompute happens.

Because we are in the context of res.config.settings create, and all
the pos_config settings fields are related fields, everything should be
in cache and no (or very few) query should be executed when doing this
filtering.

opw-3547106
opw-3508239

closes odoo/odoo#139634

Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

addons/point_of_sale/models/res_config_settings.py
@@ -104,6 +104,20 @@ class ResConfigSettings(models.TransientModel):
     pos_warehouse_id = fields.Many2one(related='pos_config_id.warehouse_id', readonly=False, string="Warehouse (PoS)")
     point_of_sale_use_ticket_qr_code = fields.Boolean(related='company_id.point_of_sale_use_ticket_qr_code', readonly=False)
 
+    @api.model
+    def _keep_new_vals(self, pos_config, pos_fields_vals):
+        """ Keep vals in pos_fields_vals that are different than
+        pos_config's values.
+        """
+        new_vals = {}
+        for field, val in pos_fields_vals.items():
+            if pos_config._fields.get(field):
+                cache_value = pos_config._fields.get(field).convert_to_cache(val, pos_config)
+                record_value = pos_config._fields.get(field).convert_to_record(cache_value, pos_config)
+                if record_value != pos_config[field]:
+                    new_vals[field] = val
+        return new_vals
+
     @api.model_create_multi
     def create(self, vals_list):
         # STEP: Remove the 'pos' fields from each vals.
@@ -148,6 +162,7 @@ class ResConfigSettings(models.TransientModel):
         # STEP: Finally, we write the value of 'pos' fields to 'pos_config_id'.
         for pos_config_id, pos_fields_vals in pos_config_id_to_fields_vals_map.items():
             pos_config = self.env['pos.config'].browse(pos_config_id)
+            pos_fields_vals = self._keep_new_vals(pos_config, pos_fields_vals)
             pos_config.write(pos_fields_vals)
 
         return result

commit 66607625111043e59c82f7a8a9f17dd2d50f1374

[FIX] point_of_sale: fix issues with x2many in settings
The change introduced in 72f7c0d7d16a1b avoids recomputes
when there is nothing that needs to be written on a pos.config.

However, starting v17 the way x2many are managed changed and
while the command to assign values on x2many from settings was
[(6, 0, ids)], in v17 it's [[4,id],...,[4,id]]. The set command
was replaced by the link command. This changes the returned value
of the convert_to_cache method and made it impossible to change
pos_config.x2many values in v17 because of the changes in 72f7c0d7d16a1b.

To fix that, move the _keep_new_vals method from res_config_settings
to pos_config and use _preprocess_x2many_vals_from_settings_view
to change link commands to unlink + link commands.

Steps to reproduce:
   - Install point_of_sale
   - Go to Settings > Point of Sale
   - Remove a payment method
   - Save
   - The payment method is still set on the pos.config.

opw-3692150

closes odoo/odoo#150900

Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

addons/point_of_sale/models/pos_config.py
@@ -436,6 +436,7 @@ class PosConfig(models.Model):
                 ))
 
         self._preprocess_x2many_vals_from_settings_view(vals)
+        vals = self._keep_new_vals(vals)
         result = super(PosConfig, self).write(vals)
 
         self.sudo()._set_fiscal_position()
@@ -479,6 +480,23 @@ class PosConfig(models.Model):
                 unlink_commands = [Command.unlink(_id) for _id in linked_ids]
 
                 vals[x2many_field] = unlink_commands + vals[x2many_field]
+
+    def _keep_new_vals(self, vals):
+        """ Keep values in vals that are different than
+        self's values.
+        """
+        from_settings_view = self.env.context.get('from_settings_view')
+        if not from_settings_view:
+            return vals
+        new_vals = {}
+        for field, val in vals.items():
+            config_field = self._fields.get(field)
+            if config_field:
+                cache_value = config_field.convert_to_cache(val, self)
+                record_value = config_field.convert_to_record(cache_value, self)
+                if record_value != self[field]:
+                    new_vals[field] = val
+        return new_vals
 
     def _get_forbidden_change_fields(self):
         forbidden_keys = ['module_pos_hr', 'module_pos_restaurant', 'available_pricelist_ids',

addons/point_of_sale/models/res_config_settings.py
@@ -106,20 +106,6 @@ class ResConfigSettings(models.TransientModel):
     pos_trusted_config_ids = fields.Many2many(related='pos_config_id.trusted_config_ids', readonly=False)
     point_of_sale_ticket_unique_code = fields.Boolean(related='company_id.point_of_sale_ticket_unique_code', readonly=False)
 
-    @api.model
-    def _keep_new_vals(self, pos_config, pos_fields_vals):
-        """ Keep vals in pos_fields_vals that are different than
-        pos_config's values.
-        """
-        new_vals = {}
-        for field, val in pos_fields_vals.items():
-            if pos_config._fields.get(field):
-                cache_value = pos_config._fields.get(field).convert_to_cache(val, pos_config)
-                record_value = pos_config._fields.get(field).convert_to_record(cache_value, pos_config)
-                if record_value != pos_config[field]:
-                    new_vals[field] = val
-        return new_vals
-
     @api.model_create_multi
     def create(self, vals_list):
         # STEP: Remove the 'pos' fields from each vals.
@@ -164,7 +150,6 @@ class ResConfigSettings(models.TransientModel):
         # STEP: Finally, we write the value of 'pos' fields to 'pos_config_id'.
         for pos_config_id, pos_fields_vals in pos_config_id_to_fields_vals_map.items():
             pos_config = self.env['pos.config'].browse(pos_config_id)
-            pos_fields_vals = self._keep_new_vals(pos_config, pos_fields_vals)
             pos_config.with_context(from_settings_view=True).write(pos_fields_vals)
 
         return result