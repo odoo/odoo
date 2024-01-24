e75e6de7556b7e7ded7429be2d69a51b9b087da6
Paul Morelle <pmo@odoo.com>
2024-01-24 16:12:51 +0100

    [FIX] portal: fix submitting /my/account when can't edit VAT
    Since 1be6866b15ebf76e197d9135a7b737640cdf14f6, when VAT can't be
    edited, the country_id select field is disabled.
    Disabled fields aren't submitted in the POST query, which causes an
    error `Some required fields are empty`, with the Country field being
    highlighted even if a value is selected.
    
    With this commit, when the user cannot edit their country, the value is
    forced into the POST data so that it is found.
    
    X-original-commit: d6d6bee087fe2d3dc17974054353430c2662aecf

--- addons/portal/tests/test_tours.py
+++ addons/portal/tests/test_tours.py
@@ -9,3 +9,9 @@ from odoo.tests import tagged
 class TestUi(HttpCaseWithUserPortal):
     def test_01_portal_load_tour(self):
         self.start_tour("/", 'portal_load_homepage', login="portal")
+
+    def test_02_portal_load_tour_cant_edit_vat(self):
+        willis = self.env.ref('base.demo_user0')
+        willis.parent_id = self.env.ref('base.partner_demo').id
+        self.start_tour("/", 'portal_load_homepage', login="portal")
+        self.assertEqual(willis.phone, "+1 555 666 7788")
