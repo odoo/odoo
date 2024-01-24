fda3a28c02f19a73a2107900b06959db453463d8
Aurelien MARIE (auma) <auma@odoo.com>
2024-01-24 14:25:17 +0100

    [PERF] mrp_subcontracting, purchase_stock: Speedup cancel of PO
    Current behavior:
    Cancelling a Purchase Order with more than 50 lines takes
    too much time to be processed. In the case of the client
    they had PO with more than 200 lines which makes it
    impossible for them to cancel them.
    
    Step to reproduce:
    - Install mrp and mrp_subcontracting
    - Create PO with more than 50 order lines or more
    - Confirm the PO
    - Try to cancel it
    - Take a long time or timeout
    
    Benchmark (made in 16):
    | No. of PO lines | Before  | After  |
    |-----------------|:-------:|:------:|
    | 9               |   650ms |  600ms |
    | 91              |  1min26 |     9s |
    | 273             |  4min50 |    34s |
    | 405             |  7min13 |    55s |
    
    Fix:
    Batch more actions and records to reduce the number of
    queries generated by the ORM.
    
    opw-3690875
    
    closes odoo/odoo#151526
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/mrp_subcontracting/models/stock_move.py
+++ addons/mrp_subcontracting/models/stock_move.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 from odoo import fields, models, _
 from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_compare, float_is_zero
+from odoo.tools.misc import OrderedSet
 
 
 class StockMove(models.Model):
@@ -117,12 +118,18 @@ class StockMove(models.Model):
         return super(StockMove, self - move_untouchable)._set_quantities_to_reservation()
 
     def _action_cancel(self):
+        productions_to_cancel_ids = OrderedSet()
         for move in self:
             if move.is_subcontract:
                 active_production = move.move_orig_ids.production_id.filtered(lambda p: p.state not in ('done', 'cancel'))
                 moves = self.env.context.get('moves_todo')
                 if not moves or active_production not in moves.move_orig_ids.production_id:
-                    active_production.with_context(skip_activity=True).action_cancel()
+                    productions_to_cancel_ids.update(active_production.ids)
+
+        if productions_to_cancel_ids:
+            productions_to_cancel = self.env['mrp.production'].browse(productions_to_cancel_ids)
+            productions_to_cancel.with_context(skip_activity=True).action_cancel()
+
         return super()._action_cancel()
 
     def _action_confirm(self, merge=True, merge_into=False):
