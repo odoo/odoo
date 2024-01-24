efb6d937816caeab647fc452ce0004cf38c5934c
Romain Derie <rde@odoo.com>
2024-01-24 16:13:04 +0000

    [FIX] website, *: make custom search model work when no website_id
    *: test_website
    
    Commit [1] modified the custom page search model so it is adding the
    current website in the search domain.
    
    But it was not considering the case of models not having a website_id
    field.
    The only case was the appoitment.type model.
    
    Steps to reproduce:
    - Install website_appointment (enterprise)
    - Click on Website > Site > Appointments
    - Crash
    
    [1]: https://github.com/odoo/odoo/commit/db670f64f4c2190f1655f9077ea62885049a3c84
    
    closes odoo/odoo#151973
    
    X-original-commit: 50e27b3c8454d59a0be6ac64f0b50f2a8f6ea11b
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/test_website/__manifest__.py
+++ addons/test_website/__manifest__.py
@@ -22,6 +22,7 @@ models which only purpose is to run tests.""",
     ],
     'data': [
         'views/templates.xml',
+        'views/test_model_multi_website_views.xml',
         'views/test_model_views.xml',
         'data/test_website_data.xml',
         'security/ir.model.access.csv',
