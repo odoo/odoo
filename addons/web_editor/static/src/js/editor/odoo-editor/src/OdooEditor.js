7bbf09ac603b0224402e02596afbdb1b0b8d7967
Adnan Saiyed <adsa@odoo.com>
2024-01-25 12:06:40 +0530

    [FIX] web_editor: colorpicker label detects wrong color
    In dark mode, colorpicker label detects wrong background
    color of selected element when no style is applied. This
    happens because getComputedStyle fails to get the actual
    background color of element in dark mode.
    
    task-3273315
    
    X-original-commit: 517e246bdae368f418e16f738b715ac902460c18

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -2422,7 +2422,7 @@ export class OdooEditor extends EventTarget {
                         ancestor = ancestor.parentElement;
                     }
                     if (!hiliteColor) {
-                        hiliteColor = computedStyle.backgroundColor;
+                        hiliteColor = this.document.queryCommandValue('backColor');
                     }
                 }
             }
