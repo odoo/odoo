9239ca020662e51c9af1a275041e7f639aa799f6
Yannick Tivisse <yti@odoo.com>
2024-01-25 13:45:14 +0100

    [FIX] project_timesheet_holidays: Fix company right issues
    Before this commit, when the company of the employees updated is not
    in the allowed_company_ids of the current user (could happen in the
    execution of a cron), an error is triggered saying we are not allowed
    to create a timesheet for an employee inactive. The reason is because the
    employee is not fetched in the check made in the timesheet creation because
    of the company of that employee is not in the companies contained in the
    environment.
    
    This commit makes sure the companies in the environment are the ones
    set on the employees created/updated, to be sure those employees are fetched
    in the check made in timesheet creation.

--- addons/hr_timesheet/tests/test_timesheet.py
+++ addons/hr_timesheet/tests/test_timesheet.py
@@ -456,9 +456,10 @@ class TestTimesheet(TestCommonTimesheet):
     def test_create_timesheet_with_multi_company(self):
         """ Always set the current company in the timesheet, not the employee company """
         company_4 = self.env['res.company'].create({'name': 'Company 4'})
-        empl_employee = self.env['hr.employee'].with_company(company_4).create({
-            'name': 'Employee 3',
-        })
+        empl_employee, archived_employee = self.env['hr.employee'].with_company(company_4).create([
+            {'name': 'Employee 3'},
+            {'name': 'Employee 4', 'active': False},
+        ])
 
         Timesheet = self.env['account.analytic.line'].with_context(allowed_company_ids=[company_4.id, self.env.company.id])
 
@@ -469,8 +470,16 @@ class TestTimesheet(TestCommonTimesheet):
             'unit_amount': 4,
             'employee_id': empl_employee.id,
         })
-
         self.assertEqual(timesheet.company_id.id, self.env.company.id)
+
+        with self.assertRaises(UserError, msg="The employee must be active to encode a timesheet"):
+            Timesheet.create({
+                'project_id': self.project_customer.id,
+                'task_id': self.task1.id,
+                'name': 'my first timesheet',
+                'unit_amount': 4,
+                'employee_id': archived_employee.id,
+            })
 
     def test_subtask_log_timesheet(self):
         """ Test parent task takes into account the timesheets of its sub-tasks.
