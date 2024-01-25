aa0c6f2204b247b660566410d16e8b4845950faf
Walid <waha@odoo.com>
2024-01-25 10:46:38 +0100

    [FIX] stock: update SO line (reserved package)
    Steps to Reproduce:
    - Activate packaging
    - Create a storable product update on hand qty 1 package: "PACK6"
    - Create a sale order with the product qty 1
    - Confirm the order, picking will have a package already assigned
    - Edit the sale order and set the product quantity to 0
    
    Bug:
    a new move will be created with negtaive quantity and merged with original
    (correct behaviour)
    but when the picking is confirmed an extra move will be created due to the
    picking still having a package level set
    
    Fix:
    unlink the package level when its last linked SML is unlinked
    
    opw-3597188
    
    closes odoo/odoo#150939
    
    X-original-commit: 4983923973bacbff84d67cdd442d4b6985f33577
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Walid Hanniche (waha) <waha@odoo.com>

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -488,7 +488,11 @@ class StockMoveLine(models.Model):
                 quants = quants_by_product[ml.product_id.id]
                 quants._update_reserved_quantity(ml.product_id, ml.location_id, -ml.quantity_product_uom, lot_id=ml.lot_id, package_id=ml.package_id, owner_id=ml.owner_id, strict=True)
         moves = self.mapped('move_id')
+        package_levels = self.package_level_id
         res = super().unlink()
+        package_levels = package_levels.filtered(lambda pl: not (pl.move_line_ids or pl.move_ids))
+        if package_levels:
+            package_levels.unlink()
         if moves:
             # Add with_prefetch() to set the _prefecht_ids = _ids
             # because _prefecht_ids generator look lazily on the cache of move_id
