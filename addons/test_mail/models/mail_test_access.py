4239e64ddef05b09c9ed8d23bac882581f2911ac
Thibault Delavallée <tde@odoo.com>
2024-01-26 11:28:36 +0100

    [IMP] test_mail: make access tests independent from discuss models
    Purpose is to make tests independent from channel model which has its own
    access rules. We now use a test model with simple rules, allowing to
    simulate models with
    
      * public access;
      * portal access;
      * internal access;
      * admin access;
    
    Tests are rewritten to be more concise and precise and use this new model.
    
    This is done in stable to keep coherency in tests codebase. It also eases
    writing bugfixes as tests won't have to be modified during forward port
    process. Finally it allows to backport improvements or fixes in later
    versions if it applies to previous versions.
    
    Task-4320561
    
    X-original-commit: odoo/odoo@6370ca168c001cf4c44aa67f4c0bc48e9b90e5f1
    Part-of: odoo/odoo#189226
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/test_mail/models/mail_test_access.py
+++ addons/test_mail/models/mail_test_access.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import exceptions, fields, models
 
 
 class MailTestAccess(models.Model):
@@ -6,7 +6,7 @@ class MailTestAccess(models.Model):
     or partner which have their own set of ACLs. Public, portal and internal
     have access to this model depending on 'access' field, allowing to check
     ir.rule usage. """
-    _description = 'Access Test'
+    _description = 'Mail Access Test'
     _name = 'mail.test.access'
     _inherit = ['mail.thread.blacklist']
     _mail_post_access = 'write'  # default value but ease mock
@@ -31,6 +31,35 @@ class MailTestAccess(models.Model):
 
     def _mail_get_partner_fields(self):
         return ['customer_id']
+
+
+class MailTestAccessCusto(models.Model):
+    """ Test access on mail models without depending on real models like channel
+    or partner which have their own set of ACLs. """
+    _description = 'Mail Access Test with Custo'
+    _name = 'mail.test.access.custo'
+    _inherit = ['mail.thread.blacklist']
+    _mail_post_access = 'write'  # default value but ease mock
+    _order = 'id DESC'
+    _primary_email = 'email_from'
+
+    name = fields.Char()
+    email_from = fields.Char()
+    phone = fields.Char()
+    customer_id = fields.Many2one('res.partner', 'Customer')
+    is_locked = fields.Boolean()
+
+    def _mail_get_partner_fields(self):
+        return ['customer_id']
+
+    def _get_mail_message_access(self, res_ids, operation, model_name=None):
+        # customize message creation
+        if operation == "create":
+            if any(record.is_locked for record in self.browse(res_ids)):
+                raise exceptions.AccessError('Cannot post on locked records')
+            else:
+                return "read"
+        return super()._get_mail_message_access(res_ids, operation, model_name=model_name)
 
 
 class MailTestAccessPublic(models.Model):
