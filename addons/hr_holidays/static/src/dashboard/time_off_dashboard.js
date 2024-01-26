54689ddfcad080818cb585db8f02ad0235603911
Dossogne Bertrand <bedo@odoo.com>
2024-01-26 11:09:32 +0100

    [FIX] hr_holidays: display valid allocations on dashboard
    Before this commit, the displayed allocation
    on the dashboard were all the ones linked to allocations
    regardless of their validity period. This resulted with
    an overloaded dashboard with leave types that are not supposed
    to be displayed.
    
    closes odoo/odoo#151306
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_holidays/static/src/dashboard/time_off_dashboard.js
+++ addons/hr_holidays/static/src/dashboard/time_off_dashboard.js
@@ -25,7 +25,7 @@ export class TimeOffDashboard extends Component {
     }
 
     async loadDashboardData(date = false) {
-        const context = {};
+        const context = { from_dashboard: true };
         if (this.props && this.props.employeeId !== null) {
             context["employee_id"] = this.props.employeeId;
         }
