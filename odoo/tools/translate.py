1c3cdffe799f3d75d2f66deb79bfce0468910f5d
Romain Derie <rde@odoo.com>
2024-01-26 16:33:13 +0100

    [IMP] website_*, translate: remove data-editor-message from translation
    * website_blog, website_sale, website_slides
    
    Commit [1] added data-editor-message as translatable element, which we
    don't want to. Arguably, it was ok in stable so those elements would be
    translatable without any view update, but even in that case it was
    probably not the way to go (or we would end up adding many stuff in
    there).
    
    We can't just add "module specific" attribute in there, or it would
    become an infinite list of attribute and a mess.
    The correct way is just to do as we do for every attribute which need to
    be translated: set it inside a `t-set`.
    
    [1]: https://github.com/odoo/odoo/commit/87177e6e7aa4eca0ec096f0a43c51f87c1a9719d
    
    opw-3671051
    
    closes odoo/odoo#152316
    
    Related: odoo/enterprise#55644
    Signed-off-by: Guillaume Dieleman (gdi) <gdi@odoo.com>

--- odoo/tools/translate.py
+++ odoo/tools/translate.py
@@ -154,7 +154,7 @@ TRANSLATED_ELEMENTS = {
 TRANSLATED_ATTRS = dict.fromkeys({
     'string', 'add-label', 'help', 'sum', 'avg', 'confirm', 'placeholder', 'alt', 'title', 'aria-label',
     'aria-keyshortcuts', 'aria-placeholder', 'aria-roledescription', 'aria-valuetext',
-    'value_label', 'data-tooltip', 'data-editor-message',
+    'value_label', 'data-tooltip',
 }, lambda e: True)
 
 def translate_attrib_value(node):
