eda2487a8ac5b4d7f039e6af96d7a2e36e2553bd
Chong Wang (cwg) <cwg@odoo.com>
2024-01-28 12:22:15 +0100

    [FIX] website: fix request with inactive language
    after #142814
    when query with inactive/invalid context languages for website, an error will be
    raised. For example, the best lang in `Accept Language` is inactive
    The error is caused because of the website http customization which may fetch
    translated field before the Http._pre_dispatch with the inactive language
    This commit forces the context langauge to en_US in website http customization
    
    Part-of: odoo/odoo#151502

--- addons/test_website/tests/test_session.py
+++ addons/test_website/tests/test_session.py
@@ -1,5 +1,4 @@
 import odoo.tests
-from odoo.tools import mute_logger
 
 
 @odoo.tests.common.tagged('post_install', '-at_install')
@@ -7,3 +6,11 @@ class TestWebsiteSession(odoo.tests.HttpCase):
 
     def test_01_run_test(self):
         self.start_tour('/', 'test_json_auth')
+
+    def test_02_inactive_session_lang(self):
+        session = self.authenticate(None, None)
+        self.env.ref('base.lang_fr').active = False
+        session.context['lang'] = 'fr_FR'
+        odoo.http.root.session_store.save(session)
+        res = self.url_open('/test_website_sitemap')  # any auth='public' route would do
+        res.raise_for_status()
