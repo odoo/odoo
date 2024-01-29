e45b99ed2f4d2d4b88051eddefb6c793e4b6e629
Jeremy Kersten <jke@odoo.com>
2024-01-29 10:11:16 +0100

    [FIX] website_[membership,partner,customer]: redirect to real slug
    + website_crm_partner_assign that overrides website_partner
    
    Implement partner slug validation for accurate URL redirection
    
    Prior to this commit, partner slugs weren't verified, leading to Google
    indexing outdated partner URLs even after renaming. With this change,
    when the virtual slug (fake slug sinc no read access on res.partner)
    differs from the actual one, redirection to the correct URL is enforced.
    
    closes odoo/odoo#151671
    
    X-original-commit: ab87283a9ba3112157f7066a58f354fc3d8181f4
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>
    Signed-off-by: Jérémy Kersten <jke@odoo.com>

--- addons/website_crm_partner_assign/controllers/main.py
+++ addons/website_crm_partner_assign/controllers/main.py
@@ -320,6 +320,7 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
     # Do not use semantic controller due to sudo()
     @http.route(['/partners/<partner_id>'], type='http', auth="public", website=True)
     def partners_detail(self, partner_id, **post):
+        current_slug = partner_id
         _, partner_id = unslug(partner_id)
         current_grade, current_country = None, None
         grade_id = post.get('grade_id')
@@ -332,6 +333,8 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
             partner = request.env['res.partner'].sudo().browse(partner_id)
             is_website_publisher = request.env['res.users'].has_group('website.group_website_publisher')
             if partner.exists() and (partner.website_published or is_website_publisher):
+                if slug(partner) != current_slug:
+                    return request.redirect('/partners/%s' % slug(partner))
                 values = {
                     'main_object': partner,
                     'partner': partner,
@@ -339,4 +342,4 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
                     'current_country': current_country
                 }
                 return request.render("website_crm_partner_assign.partner", values)
-        return self.partners(**post)
+        raise request.not_found()
