b8dadb3748ed52d1462bef5180ffc342f7b55ba5
Xavier Bol (xbo) <xbo@odoo.com>
2024-01-29 15:10:06 +0100

    [FIX] project: evaluate column_invisible for project sharing list view
    Before this commit, the custom code to evaluate the `column_invisible` with
    the first record in the list to determine if the column should be completely
    hidden or not, no longer work since recent changes made in the generic
    list renderer.
    
    This commit fixes the issue by evaluating the `column_invisible` which is not
    either "0", "1", "True" nor "False" and altered the value of `column_invisible`
    by the value returned by the evaluation to avoid evaluating again.
    
    task-3615844
    
    Part-of: odoo/odoo#147707

--- addons/project/static/src/project_sharing/views/list/list_renderer.js
+++ addons/project/static/src/project_sharing/views/list/list_renderer.js
@@ -1,33 +1,29 @@
-/** @odoo-module */
-
 import { evaluateBooleanExpr } from "@web/core/py_js/py";
 import { ListRenderer } from "@web/views/list/list_renderer";
-import { onWillRender } from "@odoo/owl";
 
 export class ProjectSharingListRenderer extends ListRenderer {
-    setup() {
-        super.setup(...arguments);
-        onWillRender(() => {
-            this.setColumns(this.allColumns);
-        });
-    }
-
-    setColumns(columns) {
+    processAllColumn() {
+        const columns = super.processAllColumn(...arguments);
         if (this.props.list.records.length) {
             const allColumns = [];
             const firstRecord = this.props.list.records[0];
             for (const column of columns) {
-                if (evaluateBooleanExpr(column.column_invisible, firstRecord.evalContextWithVirtualIds)) {
-                    continue;
+                if (
+                    column.column_invisible &&
+                    !["1", "0", "True", "False"].includes(column.column_invisible)
+                ) {
+                    column.column_invisible = evaluateBooleanExpr(
+                        column.column_invisible,
+                        firstRecord.evalContextWithVirtualIds
+                    );
+                    if (column.column_invisible) {
+                        continue;
+                    }
                 }
                 allColumns.push(column);
             }
-            this.allColumns = allColumns;
-        } else {
-            this.allColumns = columns;
+            return allColumns;
         }
-        this.columns = this.allColumns.filter(
-            (col) => !col.optional || this.optionalActiveFields[col.name]
-        );
+        return columns;
     }
 }
