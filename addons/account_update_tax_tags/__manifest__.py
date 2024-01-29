97577002d4a4eb4ddce6b2613b2e4fc9d176f21c
Claire Bretton (clbr) <clbr@odoo.com>
2024-01-29 16:55:00 +0100

    [IMP] account_rewrite_tax_tags: rewrite existing journal entries's tax tags wizard
    After updating taxes, we didn't update already existing journal entries
    tags to map correctly into new reports.
    We let the user change his data which can be very time consuming.
    To tackle that we introduce a new wizard accessible in accounting settings
    in which user can set a date from which we will update tags on his journal entries
    for him, so that they are synchronized with current taxes configuration.
    
    Note: initial script is part of support tools' scripts, written by Brice (bib), slightly
    modified for this wizard's purpose.
    
    task-3330327
    
    closes odoo/odoo#174339
    
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

---
+++ addons/account_update_tax_tags/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Account - Allow updating tax grids',
+    'category': 'Accounting/Accounting',
+    'summary': 'Allow updating tax grids on existing entries',
+    'version': '1.0',
+    'description': """
+    This module allows updating tax grids on existing accounting entries.
+    In debug mode a button to update your entries' tax grids will be available
+    in Accounting settings.
+    This is typically useful after some legal changes were done on the tax report,
+    requiring a new tax configuration.
+    """,
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_config_settings_views.xml',
+        'wizard/account_update_tax_tags_wizard.xml',
+    ],
+    'license': 'LGPL-3',
+}
