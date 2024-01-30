5259d47261031c743683063ae3142d781f4bbddd
tsm-odoo <tsm@odoo.com>
2024-01-30 10:14:37 +0000

    [FIX] im_livechat: only return latest chat from init messaging
    Since [1], the `is_for_livechat` context is used to restrict the
    channels returned by the `init_messaging` request in the live chat
    context.
    
    This does not work in CORS since the CORS route does not pass the
    context parameter to the `mail_init_messaging` method.
    
    This PR fixes the issue.
    
    [1]: https://github.com/odoo/odoo/pull/146098
    
    X-original-commit: 5d324ad25e4f058e468ff831402fbd4343d9cbe5

--- addons/im_livechat/controllers/cors/webclient.py
+++ addons/im_livechat/controllers/cors/webclient.py
@@ -6,14 +6,28 @@ from odoo.addons.im_livechat.tools.misc import force_guest_env
 
 
 class WebClient(WebclientController):
+<<<<<<< HEAD
     """Override to add CORS support."""
 
     @route("/im_livechat/cors/action", methods=["POST"], type="json", auth="public", cors="*")
     def livechat_action(self, guest_token, **kwargs):
+||||||| parent of f3db9f113786 (temp)
+    @route("/im_livechat/cors/init_messaging", methods=["POST"], type="json", auth="public", cors="*")
+    def livechat_init_messaging(self, guest_token):
+=======
+    @route("/im_livechat/cors/init_messaging", methods=["POST"], type="json", auth="public", cors="*")
+    def livechat_init_messaging(self, guest_token, context=None):
+>>>>>>> f3db9f113786 (temp)
         force_guest_env(guest_token)
+<<<<<<< HEAD
         return self.mail_action(**kwargs)
 
     @route("/im_livechat/cors/data", methods=["POST"], type="json", auth="public", cors="*", readonly=True)
     def livechat_data(self, guest_token, **kwargs):
         force_guest_env(guest_token)
         return self.mail_data(**kwargs)
+||||||| parent of f3db9f113786 (temp)
+        return self.mail_init_messaging()
+=======
+        return self.mail_init_messaging(context=context)
+>>>>>>> f3db9f113786 (temp)
