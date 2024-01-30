df5975054f60fb0cb2e160c7a23b76673f493ef0
Lucas Lefèvre (lul) <lul@odoo.com>
2024-01-30 11:40:46 +0100

    [PERF] spreadsheet_dashboard: avoid currency RPC
    Most (if not all) dashboards have monetary amounts. They are formatted
    with the main company currency format.
    Before this commit, a RPC was made to fetch the company currency.
    With this commit, the dashboard is loaded with the currency.
    
    It saves one network request and a full spreadsheet evaluation (which would
    have occured after the request is done)
    
    closes odoo/odoo#153849
    
    Task: 3709466
    X-original-commit: 49af4d6d9ad5dfe7c93c6975513b4db3bcb00dbe
    Related: odoo/enterprise#56506
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet_dashboard/models/spreadsheet_dashboard.py
+++ addons/spreadsheet_dashboard/models/spreadsheet_dashboard.py
@@ -20,9 +20,11 @@ class SpreadsheetDashboard(models.Model):
         snapshot = json.loads(self.spreadsheet_data)
         user_locale = self.env['res.lang']._get_user_spreadsheet_locale()
         snapshot.setdefault('settings', {})['locale'] = user_locale
+        default_currency = self.env['res.currency'].get_company_currency_for_spreadsheet()
         return {
             'snapshot': snapshot,
             'revisions': [],
+            'default_currency': default_currency,
         }
 
     def copy(self, default=None):
