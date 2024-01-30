a70c4c9d9fe4dd235761ded6af90da4adff4081b
Sébastien Theys <seb@odoo.com>
2024-01-30 13:35:58 +0100

    [REF] mail, im_livechat: move canned reponse code to mail
    Most of the code for this feature is already in mail, but there were
    some parts that still made it usable only when livechat is installed.
    
    Part-of: odoo/odoo#151760

--- addons/im_livechat/static/src/core/web/suggestion_service_patch.js
+++ addons/im_livechat/static/src/core/web/suggestion_service_patch.js
@@ -1,66 +1,13 @@
 /* @odoo-module */
 
 import { SuggestionService } from "@mail/core/common/suggestion_service";
-import { cleanTerm } from "@mail/utils/common/format";
 
 import { patch } from "@web/core/utils/patch";
 
 patch(SuggestionService.prototype, {
+    /** @override */
     getSupportedDelimiters(thread) {
-        return thread?.model !== "discuss.channel" || thread.type === "livechat"
-            ? [...super.getSupportedDelimiters(...arguments), [":"]]
-            : super.getSupportedDelimiters(...arguments);
-    },
-    /**
-     * Returns suggestions that match the given search term from specified type.
-     *
-     * @param {Object} [param0={}]
-     * @param {String} [param0.delimiter] can be one one of the following: ["@", ":", "#", "/"]
-     * @param {String} [param0.term]
-     * @param {Object} [options={}]
-     * @param {Integer} [options.thread] prioritize and/or restrict
-     *  result in the context of given thread
-     * @returns {[mainSuggestion[], extraSuggestion[]]}
-     */
-    searchSuggestions({ delimiter, term }, { thread } = {}, sort = false) {
-        if (delimiter === ":") {
-            return this.searchCannedResponseSuggestions(cleanTerm(term), sort);
-        }
-        return super.searchSuggestions(...arguments);
-    },
-
-    searchCannedResponseSuggestions(cleanedSearchTerm, sort) {
-        const cannedResponses = Object.values(this.store.CannedResponse.records).filter(
-            (cannedResponse) => {
-                return cleanTerm(cannedResponse.source).includes(cleanedSearchTerm);
-            }
-        );
-        const sortFunc = (c1, c2) => {
-            const cleanedName1 = cleanTerm(c1.source);
-            const cleanedName2 = cleanTerm(c2.source);
-            if (
-                cleanedName1.startsWith(cleanedSearchTerm) &&
-                !cleanedName2.startsWith(cleanedSearchTerm)
-            ) {
-                return -1;
-            }
-            if (
-                !cleanedName1.startsWith(cleanedSearchTerm) &&
-                cleanedName2.startsWith(cleanedSearchTerm)
-            ) {
-                return 1;
-            }
-            if (cleanedName1 < cleanedName2) {
-                return -1;
-            }
-            if (cleanedName1 > cleanedName2) {
-                return 1;
-            }
-            return c1.id - c2.id;
-        };
-        return {
-            type: "CannedResponse",
-            mainSuggestions: sort ? cannedResponses.sort(sortFunc) : cannedResponses,
-        };
+        const res = super.getSupportedDelimiters(...arguments);
+        return thread.type === "livechat" ? [...res, [":"]] : res;
     },
 });
