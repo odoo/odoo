defd5ef63b83f92f26be11d2f62b25f0f4c671a3
Pedram (pebr) <pebr@odoo.com>
2024-01-31 10:57:44 +0000

    [FIX] pos_loyalty: ensure free product with tags is added to order
    This commit fixes an issue where free products associated with a tag
    were not being added to the order. The logic for adding free products
    based on tags has been corrected to ensure proper functionality.
    
    opw-3702040
    
    closes odoo/odoo#154284
    
    X-original-commit: 31e61c71485ad2e89ec6d092b93068f06b85c257
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/pos_loyalty/static/src/tours/PosLoyaltyTour.js
+++ addons/pos_loyalty/static/src/tours/PosLoyaltyTour.js
@@ -313,3 +313,25 @@ registry
         ProductScreen.check.totalAmountIs('200.00');
         return getSteps();
     }});
+
+registry
+    .category("web_tour.tours")
+    .add('PosLoyaltyTour10', { test: true, url: '/pos/web', steps: () => {
+        ProductScreen.do.clickHomeCategory();
+        ProductScreen.do.confirmOpeningPopup();
+
+        ProductScreen.do.clickPartnerButton();
+        ProductScreen.do.clickCustomer('AAA Partner');
+        PosLoyalty.check.customerIs('AAA Partner');
+        ProductScreen.do.clickDisplayedProduct('Product Test');
+        ProductScreen.check.totalAmountIs('1.00');
+        ProductScreen.check.selectedOrderlineHas("Product Test", "1.00");
+        PosLoyalty.check.isRewardButtonHighlighted(true);
+        PosLoyalty.do.clickRewardButton();
+        SelectionPopup.do.clickItem('Free Product with Tag');
+        SelectionPopup.do.clickItem('Free Product B');
+        PosLoyalty.check.hasRewardLine('Free Product B', '-1.00');
+        ProductScreen.check.totalAmountIs('1.00');
+        PosLoyalty.check.isRewardButtonHighlighted(false);
+        return getSteps();
+    }});
