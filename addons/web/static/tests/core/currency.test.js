3c19cf288a6fb74f2ab72de959f105d9ab0d66a1
Julien Carion (juca) <juca@odoo.com>
2024-01-31 10:22:11 +0100

    [IMP] web: convert core/currency tests to hoot
    closes odoo/odoo#151975
    
    Task-id: 3705027
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>

---
+++ addons/web/static/tests/core/currency.test.js
@@ -0,0 +1,50 @@
+import { expect, test, beforeEach } from "@odoo/hoot";
+import { makeMockEnv, patchWithCleanup } from "@web/../tests/web_test_helpers";
+
+import { currencies, formatCurrency } from "@web/core/currency";
+import { session } from "@web/session";
+
+beforeEach(async () => {
+    await makeMockEnv(); // To start the localization service
+});
+
+test`headless`("formatCurrency", async () => {
+    patchWithCleanup(currencies, {
+        10: {
+            digits: [69, 2],
+            position: "after",
+            symbol: "€",
+        },
+        11: {
+            digits: [69, 2],
+            position: "before",
+            symbol: "$",
+        },
+        12: {
+            digits: [69, 2],
+            position: "after",
+            symbol: "&",
+        },
+    });
+
+    expect(formatCurrency(200)).toBe("200.00");
+    expect(formatCurrency(1234567.654, 10)).toBe("1,234,567.65\u00a0€");
+    expect(formatCurrency(1234567.654, 11)).toBe("$\u00a01,234,567.65");
+    expect(formatCurrency(1234567.654, 44)).toBe("1,234,567.65");
+    expect(formatCurrency(1234567.654, 10, { noSymbol: true })).toBe("1,234,567.65");
+    expect(formatCurrency(8.0, 10, { humanReadable: true })).toBe("8.00\u00a0€");
+    expect(formatCurrency(1234567.654, 10, { humanReadable: true })).toBe("1.23M\u00a0€");
+    expect(formatCurrency(1990000.001, 10, { humanReadable: true })).toBe("1.99M\u00a0€");
+    expect(formatCurrency(1234567.654, 44, { digits: [69, 1] })).toBe("1,234,567.7");
+    expect(formatCurrency(1234567.654, 11, { digits: [69, 1] })).toBe("$\u00a01,234,567.7", {
+        message: "options digits should take over currency digits when both are defined",
+    });
+});
+
+test`headless`("formatCurrency without currency", async () => {
+    patchWithCleanup(session, {
+        currencies: {},
+    });
+    expect(formatCurrency(1234567.654, 10, { humanReadable: true })).toBe("1.23M");
+    expect(formatCurrency(1234567.654, 10)).toBe("1,234,567.65");
+});
