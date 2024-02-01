e6d1b8b16b4fc9dc4e285cb453f58c03e78978b1
Demesmaeker <edm@odoo.com>
2024-02-01 10:39:09 +0100

    [FIX] sale: consider draft quotation could be expired
    Following commit 94d4205, _has_to_be_signed and has_to_be_paid now
    consider draft quotation, as such, we now have to also ensure a
    quotation isn't expired before allowing it to be signed and/or paid.
    
    opw-357130
    
    closes odoo/odoo#152216
    
    Signed-off-by: Morgane Demesmaeker <edm@odoo.com>

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -597,7 +597,8 @@ class SaleOrder(models.Model):
     def _compute_is_expired(self):
         today = fields.Date.today()
         for order in self:
-            order.is_expired = order.state == 'sent' and order.validity_date and order.validity_date < today
+            is_date_expired = order.validity_date and order.validity_date < today
+            order.is_expired = order.state in ('draft', 'sent') and is_date_expired
 
     @api.depends('company_id', 'fiscal_position_id')
     def _compute_tax_country_id(self):
