f23ff8f8614f483465f7fbd3d6b464ad69c29252
Yannick Tivisse <yti@odoo.com>
2024-02-01 14:32:36 +0100

    [MOV] l10n_fr: Rename into l10n_fr_account
    Purpose
    =======
    
    First step to move accounting part to a dedicated module l10n_fr_account.
    
    Advantage:
    - We don't have define different company demo data in account and payroll.
    - It will fix dependency issues for modules requiring fr data and no accounting (example: l10n_fr_hr_holidays)
    - End users only have to install l10n_fr (done automatically now) and everything is auto-installed afterwards.
    
    Taskid: 3714759, 3646244
    Part-of: odoo/odoo#152279

--- addons/l10n_fr_account/__manifest__.py
+++ addons/l10n_fr_account/__manifest__.py
@@ -23,7 +23,7 @@ This localisation module doesn't properly handle the scenario when a France-main
 company sells services to a company based in the DOMs. We could manage it in the
 fiscal positions, but it would require to differentiate between 'product' VAT taxes
 and 'service' VAT taxes. We consider that it is too 'heavy' to have this by default
-in l10n_fr; companies that sell services to DOM-based companies should update the
+in l10n_fr_account; companies that sell services to DOM-based companies should update the
 configuration of their taxes and fiscal positions manually.
 
 **Credits:** Sistheo, Zeekom, CrysaLEAD, Akretion and Camptocamp.
