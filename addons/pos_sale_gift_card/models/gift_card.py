928ab8ec418694b611ec9a63081cd5aafd263b33
Antoine (ande) <ande@odoo.com>
2024-02-01 16:23:29 +0000

    [FIX] pos_sale_gift_card: initial amount issue
    Current behaviour:
    When trying to settle an order that contains
    a gift card through PoS, we get a validation error:
    The initial amount must be positive
    
    Steps to reproduce:
    1. Install website_sale_gift_card
    2. Create a gift card of $50 (you get a code)
    3. Go to website > Shop
    4. Add to cart the Customizable Desk
    5. Go to cart > Process Checkout
    6. Pay with a gift card (use the code)
    7. Pay now (you get an order)
    8. Go to PoS > select session > Settings
    9. Check Gift Card > Save
    10. Open new session
    11. Quotation/Order > order (your order)
    12. Settle the order > Payment
    13. Select Bank > Validate
    14. Error: The initial amount must be positive.
    
    Cause of the issue:
    Caused by https://github.com/odoo/odoo/commit/a8b95c628713571edaf32820e76eb990e94d1171
    To avoid the gift card being used two times,
    gift_card_id in pos order line was set as None after use.
    This caused, after usage of a gift card,
    create_from_ui in pos order to create a new one with negative amount.
    (which caused the validation error)
    See https://github.com/odoo/odoo/blob/809f0bbe1b05f15da15dac1bfaaa583fcffdbda4/addons/pos_gift_card/models/pos_order.py#L25
    
    Fix:
    Fixed the tests, removed the set to None,
    Added a new way to avoid using a gift card multiple times
    
    opw-3631691
    
    X-original-commit: 7edc880bf6dd850c9fda3feec5e69990663e167a
    Co-authored-by: Robin Engels <roen@odoo.com>

--- addons/pos_sale_gift_card/models/gift_card.py
+++ addons/pos_sale_gift_card/models/gift_card.py
@@ -10,3 +10,9 @@ class GiftCard(models.Model):
     def can_be_used_in_pos(self, sale_order_origin_id=False):
         can_be_used = super().can_be_used()
         return can_be_used or (sale_order_origin_id.id == self.sale_order_id.id)
+
+    def _get_confirmed_redeem_pos_order_lines(self):
+        confirmed = super()._get_confirmed_redeem_pos_order_lines()
+        return confirmed.filtered(
+                lambda l: not l.sale_order_line_id
+            )
