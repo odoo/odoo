cd62e3fcaab3e71e606607308cace739bc480c4f
sesn-odoo <sesn@odoo.com>
2024-02-01 07:20:49 +0100

    [FIX] account, sale: prevent reversal of moves across companies/branches
    Steps to reproduce:
    
    - Create a child company.
    - In the child company, create a new sales journal.
    - Create and confirm an invoice using this new journal.
    - Attempt to create a credit note from that invoice.
    
    In this scenario, you would encounter an error.
    
    Cause:
    
    The `AccountMoveReversal` wizard is currently setting its company to the
    root company of the moves, which in this case is the parent company.
    However, its journal is set to the one created in the child company.
    This mismatch causes an error due to company inconsistency.
    
    Fix:
    
    The `company_id` of `AccountMoveReversal` will now be assigned to the
    company of the moves, rather than the root company. To ensure this works
    correctly, we also added a check to guarantee that all moves being
    reversed are from the same company.
    
    Note:
    
    This fix also resolves an issue where a traceback occurred if two
    invoices were created (one in the child company and another in the
    parent company) and an attempt was made to reverse both simultaneously.
    
    opw-3640719
    
    closes odoo/odoo#147891
    
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- addons/account/wizard/account_move_reversal.py
+++ addons/account/wizard/account_move_reversal.py
@@ -69,10 +69,13 @@ class AccountMoveReversal(models.TransientModel):
         res = super(AccountMoveReversal, self).default_get(fields)
         move_ids = self.env['account.move'].browse(self.env.context['active_ids']) if self.env.context.get('active_model') == 'account.move' else self.env['account.move']
 
+        if len(move_ids.company_id) > 1:
+            raise UserError(_("All selected moves for reversal must belong to the same company."))
+
         if any(move.state != "posted" for move in move_ids):
             raise UserError(_('You can only reverse posted moves.'))
         if 'company_id' in fields:
-            res['company_id'] = move_ids.company_id.root_id.id or self.env.company.id
+            res['company_id'] = move_ids.company_id.id or self.env.company.id
         if 'move_ids' in fields:
             res['move_ids'] = [(6, 0, move_ids.ids)]
         return res
