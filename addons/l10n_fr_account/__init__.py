f19300c0bc938f169d5735670e7c3bf1cad45575
Yannick Tivisse <yti@odoo.com>
2024-02-01 14:54:22 +0100

    [ADD] l10n_fr: Move non accounting data to l10n_fr
    Second step to move accounting part to a dedicated module l10n_fr_account.
    
    Advantage:
    - We don't have define different company demo data in account and payroll.
    - It will fix dependency issues for modules requiring fr data and no accounting (example: l10n_fr_hr_holidays)
    - End users only have to install l10n_fr (done automatically now) and everything is auto-installed afterwards.
    
    Taskid: 3714759, 3646244
    Part-of: odoo/odoo#152279

--- addons/l10n_fr_account/__init__.py
+++ addons/l10n_fr_account/__init__.py
@@ -5,13 +5,14 @@
 from . import models
 from . import wizard
 
+from odoo.addons.account.models.chart_template import preserve_existing_tags_on_taxes
+
 
 def _l10n_fr_post_init_hook(env):
     _preserve_tag_on_taxes(env)
     _setup_inalterability(env)
 
 def _preserve_tag_on_taxes(env):
-    from odoo.addons.account.models.chart_template import preserve_existing_tags_on_taxes
     preserve_existing_tags_on_taxes(env, 'l10n_fr')
 
 def _setup_inalterability(env):
