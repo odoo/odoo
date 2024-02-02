be547b0e1e9efe8f933ec2428874de9ee0a9c9c2
Lucas Lefèvre (lul) <lul@odoo.com>
2024-02-02 15:08:24 +0100

    [IMP] spreadsheet: update o_spreadsheet to latest version
    https://github.com/odoo/o-spreadsheet/commit/816e346fc [REL] 17.2.0-alpha.3
    https://github.com/odoo/o-spreadsheet/commit/da3fd36ca [FIX] link: use updated secondary color
    https://github.com/odoo/o-spreadsheet/commit/8f233b349 [REF] model: rename drawGrid method
    https://github.com/odoo/o-spreadsheet/commit/cf1af1799 [MOV] composer: rename store test file
    https://github.com/odoo/o-spreadsheet/commit/34c244576 [REF] composer: rewrite plugin with stores
    https://github.com/odoo/o-spreadsheet/commit/f851c3cdb [REF] notification: rewrite with stores
    https://github.com/odoo/o-spreadsheet/commit/78c973722 [REF] Popover: rewrite plugin with store
    https://github.com/odoo/o-spreadsheet/commit/7e5469736 [MOV] find & replace: rename test file
    https://github.com/odoo/o-spreadsheet/commit/471fd6c7b [REF] find & replace: rewrite with a store
    https://github.com/odoo/o-spreadsheet/commit/74d6c4c44 [MOV] SelectionInput: rename test file
    https://github.com/odoo/o-spreadsheet/commit/25d50701d [REF] SelectionInput: rewrite plugin with store
    https://github.com/odoo/o-spreadsheet/commit/69f4397b6 [IMP] highlight: add highlight on CF/DV hover Task: 3571796
    https://github.com/odoo/o-spreadsheet/commit/8d2ae4d21 [IMP] stores: add a way for store to draw on the canvas Task: 3571796
    https://github.com/odoo/o-spreadsheet/commit/f0486f503 [IMP] highlight: draw highlight over cell borders Task: 3571796
    https://github.com/odoo/o-spreadsheet/commit/0ea6a277f [REF] conditional_format: split list panel and cf preview Task: 3571796
    https://github.com/odoo/o-spreadsheet/commit/1f29a5831 [ADD] store engine: a state management system for components
    https://github.com/odoo/o-spreadsheet/commit/4f478b584 [IMP] formulas: allow lookup formulas to perform wildcard search Task: 3703048
    https://github.com/odoo/o-spreadsheet/commit/81577bfa4 [IMP] build: add odoo-module ignore annotation Task: 3439583
    https://github.com/odoo/o-spreadsheet/commit/1402030d4 [REF] clipboard: Rename handlers files
    https://github.com/odoo/o-spreadsheet/commit/d7b90d4ef [REF] model: preserve default currency format
    https://github.com/odoo/o-spreadsheet/commit/3c3d312f1 [REF] clipboard: refactor copy-paste handling Task: 3637334
    https://github.com/odoo/o-spreadsheet/commit/76ce136d0 [FIX] export: transform unbound references Task: 3618912
    https://github.com/odoo/o-spreadsheet/commit/fb47aa22f [FIX] export: transform unbound references Task: 3618912
    https://github.com/odoo/o-spreadsheet/commit/05e1e1d4d [FIX] export: remove line returns in formula Task: 3618912
    https://github.com/odoo/o-spreadsheet/commit/97c88097c [FIX] Grid: Support Mac shortcuts in `Grid` Task: 3707416
    
    closes odoo/odoo#152484
    
    Related: odoo/enterprise#55755
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/spreadsheet/static/src/hooks.js
+++ addons/spreadsheet/static/src/hooks.js
@@ -1,8 +1,16 @@
 /** @odoo-module */
 
-import { useEffect, useExternalListener, useState } from "@odoo/owl";
+import { _t } from "@web/core/l10n/translation";
+import { useService } from "@web/core/utils/hooks";
+
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+
+import { stores } from "@odoo/o-spreadsheet";
+import { useSubEnv, useEffect, useExternalListener, useState } from "@odoo/owl";
+
 import { loadBundle } from "@web/core/assets";
 
+const { useStoreProvider, NotificationStore } = stores;
 /**
  * Hook that will capture the 'Ctrl+p' press that corresponds to the user intent to print a spreadsheet.
  * It will prepare the spreadsheet for printing by:
@@ -36,13 +44,16 @@ export function useSpreadsheetPrint(model) {
         },
         { capture: true }
     );
-    useExternalListener(window, "afterprint", afterPrint)
+    useExternalListener(window, "afterprint", afterPrint);
 
-    useEffect(() => {
-        if (printState.active) {
-            window.print();
-        }
-    }, () => [printState.active]);
+    useEffect(
+        () => {
+            if (printState.active) {
+                window.print();
+            }
+        },
+        () => [printState.active]
+    );
 
     /**
      * Returns the DOM position & dimensions such that the whole spreadsheet content is visible.
@@ -52,11 +63,8 @@ export function useSpreadsheetPrint(model) {
         const sheetId = model().getters.getActiveSheetId();
         const { bottom, right } = model().getters.getSheetZone(sheetId);
         const { end: width } = model().getters.getColDimensions(sheetId, right);
-        const { end: height } = model().getters.getRowDimensions(
-            sheetId,
-            bottom
-        );
-        return { x:0, y:0, width, height };
+        const { end: height } = model().getters.getRowDimensions(sheetId, bottom);
+        return { x: 0, y: 0, width, height };
     }
 
     /**
@@ -65,7 +73,7 @@ export function useSpreadsheetPrint(model) {
      */
     async function preparePrint() {
         if (!model()) {
-             return;
+            return;
         }
         await loadBundle("spreadsheet.assets_print");
         const { width, height } = model().getters.getSheetViewDimension();
@@ -97,8 +105,7 @@ export function useSpreadsheetPrint(model) {
         }
         if (frozenPrintState) {
             model().dispatch("RESIZE_SHEETVIEW", frozenPrintState.viewRect);
-            const { scrollX: offsetX, scrollY: offsetY } =
-                frozenPrintState.offset;
+            const { scrollX: offsetX, scrollY: offsetY } = frozenPrintState.offset;
             model().dispatch("SET_VIEWPORT_OFFSET", { offsetX, offsetY });
             model().updateMode(frozenPrintState.mode);
             frozenPrintState = undefined;
@@ -107,4 +114,65 @@ export function useSpreadsheetPrint(model) {
     }
 
     return preparePrint;
+}
+
+export function useSpreadsheetNotificationStore() {
+    /**
+     * Open a dialog to ask a confirmation to the user.
+     *
+     * @param {string} body body content to display
+     * @param {Function} confirm Callback if the user press 'Confirm'
+     */
+    function askConfirmation(body, confirm) {
+        dialog.add(ConfirmationDialog, {
+            title: _t("Odoo Spreadsheet"),
+            body,
+            confirm,
+            cancel: () => {}, // Must be defined to display the Cancel button
+            confirmLabel: _t("Confirm"),
+        });
+    }
+
+    /**
+     * Adds a notification to display to the user
+     * @param {{text: string, type: string, sticky: boolean }} notification
+     */
+    function notifyUser(notification) {
+        notifications.add(notification.text, {
+            type: notification.type,
+            sticky: notification.sticky,
+        });
+    }
+
+    /**
+     * Open a dialog to display an error message to the user.
+     *
+     * @param {string} body Content to display
+     * @param {function} callBack Callback function to be executed when the dialog is closed
+     */
+    function raiseError(body, callBack) {
+        dialog.add(
+            ConfirmationDialog,
+            {
+                title: _t("Odoo Spreadsheet"),
+                body,
+            },
+            {
+                onClose: callBack,
+            }
+        );
+    }
+    const dialog = useService("dialog");
+    const notifications = useService("notification");
+
+    const stores = useStoreProvider();
+    const notificationStore = {
+        notifyUser: notifyUser,
+        raiseError: raiseError,
+        askConfirmation: askConfirmation,
+    };
+    stores.inject(NotificationStore, notificationStore);
+    useSubEnv({
+        ...notificationStore,
+    });
 }
