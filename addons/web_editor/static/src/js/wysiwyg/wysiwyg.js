90b63046ba589ad581f9f7aa134a8fb744da1853
Antoine Guenet <age@odoo.com>
2024-02-02 10:36:52 +0000

    [FIX] web editor : selection change on hover colorpicker colors
    This commit fixes the problem of selection change on colorpicker fast
    hover.
    
    Before this commit, hovering too fast on the colors of the colorpicker
    unselected text that would be on the edge of the selection and
    uncolored. This is fixed by deep ranging the selection at the moment we
    limit it to fonts. This improves the accuracy of the history as, in case
    the font tag has been removed, it saves the text node as the current
    selection. And since hovering updates rely on it, it prevents losing the
    selection when unhovering fast (which is the cause of the bug).
    
    We noticed two behavior when dealing with this bug. When hoving on a
    color cell and exiting out of the colorpicker altogether, no bug
    appears. But when switching between two cells, multiple selection issue
    happen. After analysis of the performance graph, we believe that this is
    due to the fact in the first case, onSelectionChange events are fired
    and the handler is called, whereas in the other case it doesn't happen.
    That handler particularly reacalulates the latest selection. And this
    doesn't happen in our pathological case. After testing this theory, we
    view that the problem is solved.
    
    task-3295858
    
    closes odoo/odoo#152834
    
    X-original-commit: 749133f3170f795c9deabc6ad6f7684baa76db59
    Signed-off-by: Geelen Sébastien (sge) <sge@odoo.com>
    Co-authored-by: Hamza Maimoune <hmai@odoo.com>
    Co-authored-by: Antoine Guenet <age@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1715,6 +1715,10 @@ const Wysiwyg = Widget.extend({
                     });
                     colorpicker.on('color_leave', null, ev => {
                         this.odooEditor.historyRevertCurrentStep();
+                        // Compute the selection to ensure it's preserved
+                        // between selectionchange events in case this gets
+                        // triggered multiple times quickly.
+                        this.odooEditor._computeHistorySelection();
                     });
                     const $childElement = $dropdown.children('.dropdown-toggle');
                     const dropdownToggle = new Dropdown($childElement);
@@ -1765,7 +1769,7 @@ const Wysiwyg = Widget.extend({
             const range = new Range();
             range.setStart(first, 0);
             range.setEnd(...endPos(last));
-            sel.addRange(range);
+            sel.addRange(getDeepRange(this.odooEditor.editable, { range }));
         }
 
         const hexColor = this._colorToHex(color);
