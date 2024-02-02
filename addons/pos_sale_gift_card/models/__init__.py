odoo/odoo

commit a8b95c628713571edaf32820e76eb990e94d1171

[FIX] pos_sale_gift_card: fix gift card usage in POS from sale order
Steps to reproduce:
- Install eCommerce, PoS, sale and gift card
- Create a gift card for 500€
- Open the eCommerce and make an order for 750€
- Pay the order using the gift card, and wire transfer
- Open the PoS and settle the order
- Try to pay for the order
- You will get an error saying that the gift card is not usable

This fix follows this one (https://github.com/odoo/odoo/pull/134249)
and fix the use case where the sale order is not confirmed.
It also fixe the fact that the gift card balance would be negative,
because it was counted as used in the Sale Order and also in the PoS
order.

opw-3443452

closes odoo/odoo#140275

Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>

addons/pos_sale_gift_card/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import pos_order_line
 from . import sale_order_line
 from . import gift_card

addons/pos_sale_gift_card/models/pos_order_line.py
@@ -1,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, api
+
+class PosOrderLine(models.Model):
+    _inherit = "pos.order.line"
+
+    @api.model
+    def create(self, values):
+        if 'sale_order_line_id' in values:
+            gift_card = self.env['sale.order.line'].browse(values['sale_order_line_id']).gift_card_id
+            #If the giftcard was already in a sale order before loading the order in the PoS, we need to remove the giftcard_id because it would be counted as used 2 times
+            if gift_card:
+                values['gift_card_id'] = None
+        return super(PosOrderLine, self).create(values)

addons/pos_sale_gift_card/tests/test_pos_sale_gift_card.py
@@ -40,3 +40,37 @@ class PosSaleGiftCardTest(TestPointOfSaleHttpCommon):
         self.main_pos_config.open_session_cb()
         self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'PosSettleOrderGiftCardSale', login="accountman")
         self.assertEqual(order.pos_order_line_ids[0].order_id.amount_paid, 250, "The amount paid should be 250 because the gift card is 500 and the product is 750")
+        self.assertEqual(gift_card.balance, 0, "The gift card balance should be 0 because it has been used")
+
+    def test_gift_card_from_unconfirmed_sale_order_in_pos(self):
+        gift_card = self.env['gift.card'].create({
+            'initial_amount': 500,
+        })
+
+        product_A = self.env['product.product'].create({
+            'name': 'Product A',
+            'lst_price': 750,
+            'type': 'product',
+            'taxes_id': False,
+        })
+
+        self.partner1 = self.env['res.partner'].create({
+            'name': 'Test',
+        })
+
+        order = self.env['sale.order'].create({
+            'partner_id': self.partner1.id,
+            'order_line': [
+                Command.create({
+                    'product_id': product_A.id,
+                    'product_uom_qty': 1,
+                    'price_unit': 750,
+                }),
+            ],
+        })
+
+        order._pay_with_gift_card(gift_card)
+        self.main_pos_config.open_session_cb()
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'PosSettleOrderGiftCardSale', login="accountman")
+        self.assertEqual(order.pos_order_line_ids[0].order_id.amount_paid, 250, "The amount paid should be 250 because the gift card is 500 and the product is 750")
+        self.assertEqual(gift_card.balance, 0, "The gift card balance should be 0 because it has been used")

addons/pos_sale_gift_card/models/sale_order_line.py
@@ -12,5 +12,7 @@ class SaleOrderLine(models.Model):
             gift_card = self.env['sale.order.line'].browse(sale_line['id']).gift_card_id
             if gift_card:
                 sale_line['gift_card_id'] = gift_card.id
+                #If the order hasn't been confirmed the qty_to_invoice is 0, and the qty in the PoS will also be 0 so we set it to 1
+                sale_line['qty_to_invoice'] = 1
 
         return result

commit bc4f03169994c073ea7a8e92721c8ef64bcf9d85

[FIX] pos_sale_gift_card: initial amount issue
Current behaviour:
When trying to settle an order that contains
a gift card through PoS, we get a validation error:
The initial amount must be positive

Steps to reproduce:
1. Install website_sale_gift_card
2. Create a gift card of $50 (you get a code)
3. Go to website > Shop
4. Add to cart the Customizable Desk
5. Go to cart > Process Checkout
6. Pay with a gift card (use the code)
7. Pay now (you get an order)
8. Go to PoS > select session > Settings
9. Check Gift Card > Save
10. Open new session
11. Quotation/Order > order (your order)
12. Settle the order > Payment
13. Select Bank > Validate
14. Error: The initial amount must be positive.

Cause of the issue:
Caused by https://github.com/odoo/odoo/commit/a8b95c628713571edaf32820e76eb990e94d1171
To avoid the gift card being used two times,
gift_card_id in pos order line was set as None after use.
This caused, after usage of a gift card,
create_from_ui in pos order to create a new one with negative amount.
(which caused the validation error)
See https://github.com/odoo/odoo/blob/809f0bbe1b05f15da15dac1bfaaa583fcffdbda4/addons/pos_gift_card/models/pos_order.py#L25

Fix:
Fixed the tests, removed the set to None,
Added a new way to avoid using a gift card multiple times

opw-3631691
Co-authored-by: Robin Engels <roen@odoo.com>

addons/pos_sale_gift_card/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import pos_order_line
 from . import sale_order_line
 from . import gift_card

addons/pos_sale_gift_card/models/gift_card.py
@@ -10,3 +10,9 @@ class GiftCard(models.Model):
     def can_be_used_in_pos(self, sale_order_origin_id=False):
         can_be_used = super().can_be_used()
         return can_be_used or (sale_order_origin_id.id == self.sale_order_id.id)
+
+    def _get_confirmed_redeem_pos_order_lines(self):
+        confirmed = super()._get_confirmed_redeem_pos_order_lines()
+        return confirmed.filtered(
+                lambda l: not l.sale_order_line_id
+            )

addons/pos_sale_gift_card/models/pos_order_line.py
@@ -1,16 +1,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import models, api
-
-class PosOrderLine(models.Model):
-    _inherit = "pos.order.line"
-
-    @api.model
-    def create(self, values):
-        if 'sale_order_line_id' in values:
-            gift_card = self.env['sale.order.line'].browse(values['sale_order_line_id']).gift_card_id
-            #If the giftcard was already in a sale order before loading the order in the PoS, we need to remove the giftcard_id because it would be counted as used 2 times
-            if gift_card:
-                values['gift_card_id'] = None
-        return super(PosOrderLine, self).create(values)

addons/pos_sale_gift_card/tests/test_pos_sale_gift_card.py
@@ -8,6 +8,10 @@ from odoo.tests.common import tagged
 @tagged('-at_install', 'post_install')
 class PosSaleGiftCardTest(TestPointOfSaleHttpCommon):
 
+    def setUp(self):
+        super(PosSaleGiftCardTest, self).setUp()
+        self.main_pos_config.gift_card_product_id = self.env.ref('gift_card.pay_with_gift_card_product')
+
     def test_gift_card_from_sale_order_in_pos(self):
         gift_card = self.env['gift.card'].create({
             'initial_amount': 500,