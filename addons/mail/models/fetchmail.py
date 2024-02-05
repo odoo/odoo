7ad2a9817698ff5e128b9a73f4fd6e582437ac9c
duongnguyen-viindoo <daiduongnguyen2709@gmail.com>
2024-02-05 10:55:27 +0700

    [IMP] mail: raise error when manually fetch mail
    -Problem: Currently when fetch mail either using cron or manually action
    in the form view,
    the system only log in the logger so it really hard for normal user to
    understand . Only log info is not enough for user to know which kind of
    error they get when fetching mail (error can be connection problem,
    wrong configuration,...). And it will make the
    IT guy may go into log and search for it to know which error they get.
    -Solution: raise the error when user hit the button fetch mail manually
    in order to have a clear view of errors
    
    Task-3727028
    
    closes odoo/odoo#152637
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/models/fetchmail.py
+++ addons/mail/models/fetchmail.py
@@ -11,7 +11,7 @@ from socket import gaierror, timeout
 from ssl import SSLError
 
 from odoo import api, fields, models, tools, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 
 _logger = logging.getLogger(__name__)
@@ -183,9 +183,9 @@ odoo_mailgate: "|/path/to/odoo-mailgate.py --host=localhost -u %(uid)d -p PASSWO
     @api.model
     def _fetch_mails(self):
         """ Method called by cron to fetch mails from servers """
-        return self.search([('state', '=', 'done'), ('server_type', '!=', 'local')]).fetch_mail()
+        return self.search([('state', '=', 'done'), ('server_type', '!=', 'local')]).fetch_mail(raise_exception=False)
 
-    def fetch_mail(self):
+    def fetch_mail(self, raise_exception=True):
         """ WARNING: meant for cron usage only - will commit() after each email! """
         additionnal_context = {
             'fetchmail_cron_running': True
@@ -216,8 +216,11 @@ odoo_mailgate: "|/path/to/odoo-mailgate.py --host=localhost -u %(uid)d -p PASSWO
                         self._cr.commit()
                         count += 1
                     _logger.info("Fetched %d email(s) on %s server %s; %d succeeded, %d failed.", count, server.server_type, server.name, (count - failed), failed)
-                except Exception:
-                    _logger.info("General failure when trying to fetch mail from %s server %s.", server.server_type, server.name, exc_info=True)
+                except Exception as e:
+                    if raise_exception:
+                        raise ValidationError(_("Couldn't get your emails. Check out the error message below for more info:\n%s", e)) from e
+                    else:
+                        _logger.info("General failure when trying to fetch mail from %s server %s.", server.server_type, server.name, exc_info=True)
                 finally:
                     if imap_server:
                         try:
@@ -250,8 +253,11 @@ odoo_mailgate: "|/path/to/odoo-mailgate.py --host=localhost -u %(uid)d -p PASSWO
                         if num_messages < MAX_POP_MESSAGES or failed_in_loop == num:
                             break
                         pop_server.quit()
-                except Exception:
-                    _logger.info("General failure when trying to fetch mail from %s server %s.", server.server_type, server.name, exc_info=True)
+                except Exception as e:
+                    if raise_exception:
+                        raise ValidationError(_("Couldn't get your emails. Check out the error message below for more info:\n%s", e)) from e
+                    else:
+                        _logger.info("General failure when trying to fetch mail from %s server %s.", server.server_type, server.name, exc_info=True)
                 finally:
                     if pop_server:
                         try:
