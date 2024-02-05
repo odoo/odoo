73ab324aae29dfdeecadf2f2af353767a7d50755
Pedram (pebr) <pebr@odoo.com>
2024-02-05 16:59:34 +0100

    [FIX] point_of_sale: prevent printing empty page
    Before this commit, printing a receipt would result in an additional
    empty page being printed. This not only wasted paper but also caused
    issues. The problem was related to the notification element on the
    print page. By removing this element, the issue has been resolved.
    
    opw-3706233
    
    closes odoo/odoo#152973
    
    X-original-commit: 7c025b2779ccf4bbc0afac414e2be77d79f53d91
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/printer/printer_service.js
+++ addons/point_of_sale/static/src/app/printer/printer_service.js
@@ -22,19 +22,7 @@ export class PrinterService extends Reactive {
         this.device = newDevice;
     }
     printWeb(el) {
-        this.renderer.whenMounted({
-            el,
-            callback: () => {
-                var orig = document.querySelector(".pos-receipt");
-                if (orig) {
-                    orig.style.display = "none";
-                }
-                window.print();
-                if (orig) {
-                    orig.style.display = "";
-                }
-            },
-        });
+        this.renderer.whenMounted({ el, callback: window.print });
         return true;
     }
     async printHtml(el, { webPrintFallback = false } = {}) {
