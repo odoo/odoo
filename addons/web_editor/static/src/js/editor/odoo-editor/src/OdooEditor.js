b31a202cac23a1b10b8133e811c122f996836b37
visp-odoo <visp@odoo.com>
2024-02-06 14:45:47 +0530

    [FIX] web_editor: link button disappearing in mass_mailing Before this commit:
    When selecting multiple blocks, the link button sometimes fails to disappear,
    and sometimes, after selection clicking on a line, the link button does not
    reappear because the code resides within the _updateEditorUi function, and this
    function is not invoked upon a selection change. Therefore, after modifying the
    selection, the user must wait for the _updateEditorUi function to be triggered,
    which happens upon a click or keydown event.
    
    After this commit:
    
    Now the link button will disappear on multiple block selection and it will be
    reappear on single block. as we moved code to updateToolbar which is triggered
    on selection change.
    
    task-3514347

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -3015,6 +3015,7 @@ export class OdooEditor extends EventTarget {
             styleSection.querySelector('button span').textContent = activeLabel;
         }
 
+        const isInMedia = this.toolbar.classList.contains('oe-media');
         const linkNode = getInSelection(this.document, 'a');
         const linkButton = this.toolbar.querySelector('#create-link');
         linkButton && linkButton.classList.toggle('active', !!linkNode);
@@ -3025,6 +3026,11 @@ export class OdooEditor extends EventTarget {
         if (this.autohideToolbar && !this.isMobile && !this.toolbar.contains(sel.anchorNode)) {
             this._positionToolbar();
         }
+        // Hide create-link button if selection spans several blocks, always
+        // hide on media elements.
+        const range = getDeepRange(this.editable, { sel, correctTripleClick: true });
+        const spansBlocks = [...range.commonAncestorContainer.childNodes].some(isBlock);
+        linkButton && linkButton.classList.toggle('d-none', spansBlocks || isInMedia);
     }
     updateToolbarPosition() {
         if (
