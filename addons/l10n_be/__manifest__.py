d4cf310b1cd81c0ed0418052d9f1ffe4fbcbcea2
Yannick Tivisse <yti@odoo.com>
2024-02-08 13:50:09 +0100

    [ADD] l10n_be_account: Move non accounting code to l10n_be
    Purpose
    =======
    
    Second step to move accounting part to a dedicated module l10n_be_account.
    
    Advantage:
    - We don't have define different company demo data in account and payroll.
    - It will fix dependency issues for modules requiring country specific data and no accounting (example: l10n_fr_hr_holidays)
    - End users only have to install l10n_xx(done automatically now) and everything is auto-installed afterwards.
    
    TaskID: 3714759

---
+++ addons/l10n_be/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Belgium - Localizations',
+    'version': '2.0',
+    'icon': '/account/static/description/l10n.png',
+    'countries': ['be'],
+    'category': 'Accounting/Localizations/Account Charts',
+    'description': """
+    """,
+    'depends': [
+        'base',
+    ],
+    'demo': [
+        'data/l10n_be_demo.xml',
+    ],
+    'license': 'LGPL-3',
+}
