b2e00ef217cc4ac57a44016c69b0c55a23024cf4
Gauthier Wala (gawa) <gawa@odoo.com>
2024-02-08 17:00:21 +0100

    [FIX] account, base, l10n_*: fiscal_country_codes are not triggered
    Install `l10n_ar`
    Switch to an AR company
    Create a new currency
    => the field `l10n_ar_afip_code` is not visible
    
    Go on an existing one
    => it is visible
    
    The compute is not triggered before the save.
    We should add a default to have the fiscal_code.
    We can even remove the compute (and keep the store=False),
    to avoid duplicating the code.
    
    Linked to runbot error 45392, 45417
    
    closes odoo/odoo#154260
    
    X-original-commit: 3b24e5ef7a6e4226abcff55ac9d74317849e0ce0
    Related: odoo/enterprise#56734
    Signed-off-by: John Laterre (jol) <jol@odoo.com>
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

--- addons/l10n_cl/models/res_partner_bank.py
+++ addons/l10n_cl/models/res_partner_bank.py
@@ -1,15 +1,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResBank(models.Model):
     _name = 'res.bank'
     _inherit = 'res.bank'
 
-    l10n_cl_sbif_code = fields.Char('Cod. SBIF', size=10)
-    fiscal_country_codes = fields.Char(compute="_compute_fiscal_country_codes")
+    def _get_fiscal_country_codes(self):
+        return ','.join(self.env.companies.mapped('account_fiscal_country_id.code'))
 
-    @api.depends_context('allowed_company_ids')
-    def _compute_fiscal_country_codes(self):
-        for record in self:
-            record.fiscal_country_codes = ",".join(self.env.companies.mapped('account_fiscal_country_id.code'))
+    l10n_cl_sbif_code = fields.Char('Cod. SBIF', size=10)
+    fiscal_country_codes = fields.Char(store=False, default=_get_fiscal_country_codes)
