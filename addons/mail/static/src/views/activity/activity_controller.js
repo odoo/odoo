21252afa068b53d2078b63dfed6a7cc81b04f487
Khushi <khpa@odoo.com>
2024-02-08 11:07:56 +0000

    [FIX] mail: activity view model domain
    Steps to reproduce:
    
    - Go to activity view.
    - Schedule an activity for one record. (Say 8 records are available)
    - Schedule an activity for the second time. (Again 8 records are available)
    - Try to schedule an activity for the 3rd time. (Only 2 records are available)
    
    Issue:
    Since 7682286, the existing props of activity model are being passed as params
    while scheduling an activity. Currently, `["activity_ids", "!=", false]` is
    being pushed to the domain of activity model in order to display only those
    records on which activities have been set. As a result, after you schedule
    activities more than once, `["activity_ids", "!=", false]` domain gets applied
    and the list of records available for scheduling activity is restricted from
    the third time onwards.
    
    Fix:
    This commit passes the 'searchParams' of activity model as params to the
    'load' method while scheduling activity, instead of existing props, to ensure
    that the existing params are applied as well as all records are accessible
    for scheduling an activity, in `searchCreateDialog`
    (i.e.["activity_ids", "!=", false] condition is not added as domain).
    
    Task : 3721750
    
    X-original-commit: 394803e32feb1d7efdf05211774f4edf3fe6055d

--- addons/mail/static/src/views/activity/activity_controller.js
+++ addons/mail/static/src/views/activity/activity_controller.js
@@ -8,7 +8,7 @@ import { Layout } from "@web/search/layout";
 import { standardViewProps } from "@web/views/standard_view_props";
 import { SelectCreateDialog } from "@web/views/view_dialogs/select_create_dialog";
 import { sprintf } from "@web/core/utils/strings";
-import { useModel } from "@web/views/model";
+import { useModel, getSearchParams } from "@web/views/model";
 import { useService } from "@web/core/utils/hooks";
 import { SearchBar } from "@web/search/search_bar/search_bar";
 import { CogMenu } from "@web/search/cog_menu/cog_menu";
@@ -53,7 +53,7 @@ export class ActivityController extends Component {
             context: this.props.context,
             onSelected: async (resIds) => {
                 await this.activity.schedule(this.props.resModel, resIds[0]);
-                this.model.load(this.props);
+                this.model.load(getSearchParams(this.props));
             },
         });
     }
@@ -75,7 +75,7 @@ export class ActivityController extends Component {
                 },
             },
             {
-                onClose: () => this.model.load(this.props),
+                onClose: () => this.model.load(getSearchParams(this.props)),
             }
         );
     }
@@ -108,7 +108,7 @@ export class ActivityController extends Component {
             archInfo: this.props.archInfo,
             groupedActivities: this.model.activityData.grouped_activities,
             scheduleActivity: this.scheduleActivity.bind(this),
-            onReloadData: () => this.model.load(this.props),
+            onReloadData: () => this.model.load(getSearchParams(this.props)),
             onEmptyCell: this.openActivityFormView.bind(this),
             onSendMailTemplate: this.sendMailTemplate.bind(this),
             openRecord: this.openRecord.bind(this),
