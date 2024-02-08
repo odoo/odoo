8f2a84ca145ffdf834bcb506708aec310ad900dc
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2024-02-08 11:20:46 +0100

    [FIX] l10n_gcc_invoice: failing test with just l10n_gcc_invoice
    Test test_invoice_narration_translation is failing when only
    l10n_gcc_invoice is installed because the system param
    `account.use_invoice_terms` is not set

--- addons/l10n_gcc_invoice/tests/test_gcc_invoice.py
+++ addons/l10n_gcc_invoice/tests/test_gcc_invoice.py
@@ -11,13 +11,18 @@ class TestGccInvoice(AccountTestInvoicingCommon):
 
     def test_invoice_narration_translation(self):
         ''' The narration field should be copied translations included'''
+
+        # Activate second lang and parameter needed to display invoice terms
         self.env['res.lang']._activate_lang('ar_001')
+        self.env['ir.config_parameter'].sudo().set_param('account.use_invoice_terms', True)
+
         gcc_countries = self.env.ref('base.gulf_cooperation_council').country_ids
         self.env.company.write({
             'country_id': gcc_countries[0].id,
             'invoice_terms': 'English Terms',
             'terms_type': 'plain',
         })
+        # Add translation to invoice terms
         self.env.company.update_field_translations('invoice_terms', {'en_US': {'English Terms': 'English Terms'}, 'ar_001': {'English Terms': 'Arabic Terms'}})
         invoice = self.init_invoice('out_invoice', products=self.product_a)
 
