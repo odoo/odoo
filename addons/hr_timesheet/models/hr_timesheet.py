7ff097fb41f475bbc2234617c592439910254d0c
Maxime de Neuville <mane@odoo.com>
2024-02-08 13:52:02 +0100

    [IMP] hr_timesheet: add custom filter and group by based on task milestone
    Adding a "milestone_id" field allows the user to create a custom filter
    or a custom group by based on the milestone_id linked to the task of the
    timesheet entry.
    
    task-3729951
    
    closes odoo/odoo#153239
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/hr_timesheet/models/hr_timesheet.py
+++ addons/hr_timesheet/models/hr_timesheet.py
@@ -67,6 +67,7 @@ class AccountAnalyticLine(models.Model):
     encoding_uom_id = fields.Many2one('uom.uom', compute='_compute_encoding_uom_id')
     partner_id = fields.Many2one(compute='_compute_partner_id', store=True, readonly=False)
     readonly_timesheet = fields.Boolean(string="Readonly Timesheet", compute="_compute_readonly_timesheet", compute_sudo=True)
+    milestone_id = fields.Many2one('project.milestone', related='task_id.milestone_id')
 
     @api.depends('project_id', 'task_id')
     def _compute_display_name(self):
