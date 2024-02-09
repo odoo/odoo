36ecc6856dfacef6c605e4c7daa25038ad547535
Yannick Tivisse <yti@odoo.com>
2024-02-09 14:00:45 +0100

    [IMP] l10n_us: Remove dependency to account
    Purpose
    =======
    
    Nothing depending on account itself + future accounting related
    stuff should go into a dedicated l10n_us_account module.
    
    closes odoo/odoo#153492
    
    Related: odoo/enterprise#56275
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/l10n_us/__manifest__.py
+++ addons/l10n_us/__manifest__.py
@@ -2,18 +2,15 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'United States - Accounting',
+    'name': 'United States - Localizations',
     'website': 'https://www.odoo.com/documentation/master/applications/finance/fiscal_localizations.html',
     'icon': '/account/static/description/l10n.png',
     'countries': ['us'],
     'version': '1.1',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
-United States - Chart of accounts.
-==================================
     """,
-    'depends': ['account'],
-    'auto_install': ['account'],
+    'depends': ['base'],
     'data': [
         'data/res_company_data.xml',
         'views/res_partner_bank_views.xml'
