290ef313601fee4f01680fa8d9655fbcf37567bf
Lucas Perais <lpe@odoo.com>
2024-02-09 16:21:53 +0100

    [IMP] web: tests: _framework: add webclient helpers

---
+++ addons/web/static/tests/_framework/webclient_test_helpers.js
@@ -0,0 +1,24 @@
+import { after } from "@odoo/hoot";
+import { Component, xml } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+
+class TestClientAction extends Component {
+    static template = xml`
+        <div class="test_client_action">
+            ClientAction_<t t-esc="props.action.params?.description"/>
+        </div>`;
+    static props = ["*"];
+}
+export function useTestClientAction() {
+    const tag = "__test__client__action__";
+    registry.category("actions").add(tag, TestClientAction);
+    after(() => {
+        registry.category("actions").remove(tag);
+    });
+    return {
+        tag,
+        target: "main",
+        type: "ir.actions.client",
+        params: { description: "Id 1" },
+    };
+}
