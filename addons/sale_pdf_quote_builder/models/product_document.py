3da684784c4cd71af4ccbabb264e1e8af3394559
Lina (liew) <liew@odoo.com>
2024-02-09 15:20:06 +0100

    [IMP] sale: product document private visibility
    Add a visibility value hidden (private) instead of empty and use it as default
    Rename visibility options to: on quote, inside quote pdf, on confirmed order, hidden
    Replace "Show on product page" by "Publish on website"
    Remove "Publish on website" option for product variants
    
    task-3682322
    
    closes odoo/odoo#153465
    
    Related: odoo/upgrade#5741
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale_pdf_quote_builder/models/product_document.py
+++ addons/sale_pdf_quote_builder/models/product_document.py
@@ -8,16 +8,17 @@ class ProductDocument(models.Model):
     _inherit = 'product.document'
 
     attached_on = fields.Selection(
-        selection_add=[('inside', "Inside quote")],
+        selection_add=[('inside', "Inside quote pdf")],
         help="Allows you to share the document with your customers within a sale.\n"
              "Leave it empty if you don't want to share this document with sales customer.\n"
-             "Quotation: the document will be sent to and accessible by customers at any time.\n"
+             "On quote: the document will be sent to and accessible by customers at any time.\n"
              "e.g. this option can be useful to share Product description files.\n"
-             "Confirmed order: the document will be sent to and accessible by customers.\n"
+             "On order confirmation: the document will be sent to and accessible by customers.\n"
              "e.g. this option can be useful to share User Manual or digital content bought"
              " on ecommerce. \n"
-             "Inside quote: The document will be included in the pdf of the quotation between the "
+             "Inside quote pdf: The document will be included in the pdf of the quotation between the "
              "header pages and the quote table. ",
+        ondelete={'inside': 'set default'},
     )
 
     def write(self, vals):
