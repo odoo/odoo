99df80da4f76b6c1e44dee3b939887f5cfe47360
Hubert Van De Walle <huvw@odoo.com>
2024-02-12 09:15:03 +0000

    [FIX] web: missing measures in graph view
    Steps to reproduce
    ==================
    
    - Install account_accountant
    - Go to settings
    - Enable Budget Management
    - Go to Accounting > Reporting > Management > Budgets Analysis
    - Switch to the graph view
    - Change the measure to "Planned amount" and then back to
      "Practical amount"
    
    => The practical_amount measure is undefined,
       The theoritical_amount measure is missing.
    
    Cause of the issue
    ==================
    
    The view is defined as follows:
    
    ```xml
    <graph string="Budget Lines" sample="1">
        <field name="crossovered_budget_id" type="row"/>
        <field name="planned_amount"  type="measure" string="Planned amount"/>
        <field name="theoritical_amount"  type="measure" string="Theoretical amount"/>
        <field name="practical_amount" type="measure" string="Practical amount"/>
    </graph>
    ```
    
    The theoritical_amount and practical_amount are non stored fields and
    thus are skipped inside `computeReportMeasures` unless they are passed
    in `activeMeasures | additionalMeasures`. [0]
    
    When parsing the graph view, the last field of type measure is
    passed to the graph model and is the one that will be used initially. [1]
    
    This is why the practical_amount is initially defined.
    
    Solution
    ========
    
    We simply need to keep track of fields of type measure.
    This was the case in 14.0 but got lost in the conversion.
    
    ---
    
    [0]: https://github.com/odoo/odoo/blob/e7a9ebec3176c37485643fcda2381e489a1df86f/addons/web/static/src/views/helpers/utils.js#L49-L60
    [1]: https://github.com/odoo/odoo/blob/0fb64bef16914937cf4a1d1618fb58ade6d16f14/addons/web/static/src/views/graph/graph_arch_parser.js#L63
    
    opw-3713613
    
    closes odoo/odoo#153935
    
    X-original-commit: 76178cd61ba10ff29b24183edb550c54b319a230
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>
    Signed-off-by: Hubert Van De Walle <huvw@odoo.com>

--- addons/web/static/tests/views/graph_view_tests.js
+++ addons/web/static/tests/views/graph_view_tests.js
@@ -2322,7 +2322,7 @@ QUnit.module("Views", (hooks) => {
     QUnit.test("process default view description", async function (assert) {
         assert.expect(1);
         const propsFromArch = new GraphArchParser().parse();
-        assert.deepEqual(propsFromArch, { fields: {}, fieldAttrs: {}, groupBy: [] });
+        assert.deepEqual(propsFromArch, { fields: {}, fieldAttrs: {}, groupBy: [], measures: [] });
     });
 
     QUnit.test("process simple arch (no field tag)", async function (assert) {
@@ -2336,6 +2336,7 @@ QUnit.module("Views", (hooks) => {
             fields,
             fieldAttrs: {},
             groupBy: [],
+            measures: [],
             mode: "line",
             order: "ASC",
         });
@@ -2347,6 +2348,7 @@ QUnit.module("Views", (hooks) => {
             fields,
             fieldAttrs: {},
             groupBy: [],
+            measures: [],
             stacked: false,
             title: "Title",
         });
@@ -2374,8 +2376,30 @@ QUnit.module("Views", (hooks) => {
                 fighters: { string: "FooFighters" },
             },
             measure: "revenue",
+            measures: ["revenue"],
             groupBy: ["date:day", "foo"],
             mode: "pie",
+        });
+    });
+
+    QUnit.test("process arch with non stored field tags of type measure", async function (assert) {
+        assert.expect(1);
+        const fields = serverData.models.foo.fields;
+        fields.revenue.store = false;
+        const arch = `
+            <graph>
+                <field name="product_id"/>
+                <field name="revenue" type="measure"/>
+                <field name="foo" type="measure"/>
+            </graph>
+        `;
+        const propsFromArch = new GraphArchParser().parse(arch, fields);
+        assert.deepEqual(propsFromArch, {
+            fields,
+            fieldAttrs: {},
+            measure: "foo",
+            measures: ["revenue", "foo"],
+            groupBy: ["product_id"],
         });
     });
 
@@ -2966,6 +2990,28 @@ QUnit.module("Views", (hooks) => {
         checkLegend(assert, graph, "Product");
         assert.strictEqual(getYAxeLabel(graph), "Product");
     });
+
+    QUnit.test(
+        "non store fields defined on the arch are present in the measures",
+        async function (assert) {
+            serverData.models.foo.fields.revenue.store = false;
+            await makeView({
+                serverData,
+                type: "graph",
+                resModel: "foo",
+                arch: `<graph>
+                <field name="product_id"/>
+                <field name="revenue" type="measure"/>
+                <field name="foo" type="measure"/>
+            </graph>`,
+            });
+            await toggleMenu(target, "Measures");
+            assert.deepEqual(
+                Array.from(target.querySelectorAll(".o_menu_item")).map((e) => e.innerText.trim()),
+                ["Foo", "Revenue", "Count"]
+            );
+        }
+    );
 
     QUnit.test('graph view "graph_measure" field in context', async function (assert) {
         assert.expect(6);
