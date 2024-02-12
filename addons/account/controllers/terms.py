529eb0846be610e20558d8ec40703242c1159ce2
“Darius <dariusr@tractioncrm.com>
2024-02-12 13:24:01 -0500

    added a comment for initial commit

--- addons/account/controllers/terms.py
+++ addons/account/controllers/terms.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+#comment as first change
 
 from odoo import http, _
 from odoo.http import request
