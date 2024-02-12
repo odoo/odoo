e71c4dedce75de2414056be0e47e1a0d585023c7
yosa-odoo <yosa@odoo.com>
2024-02-12 11:20:14 +0100

    [FIX] partner_autocomplete: display autocomplete createEdit
    Steps to reproduce:
    [account, iap credit]
    - create a new invoice
    - start to write "test" for the partner
    
    Issue:
    The partner autocomplete is not displayed
    
    Cause:
    in #150106 we add a condition for the quickCreate bypassing the possibility of having createEdit set to true
    
    opw-3698400
    
    closes odoo/odoo#155781
    
    X-original-commit: 5f342b816c16fbbaefdb61d8bfac8376132dbfeb
    Signed-off-by: Louis Baudoux (lba) <lba@odoo.com>
    Signed-off-by: Yolann Sabaux (yosa) <yosa@odoo.com>

--- addons/partner_autocomplete/static/src/js/partner_autocomplete_many2one.js
+++ addons/partner_autocomplete/static/src/js/partner_autocomplete_many2one.js
@@ -19,7 +19,7 @@ export class PartnerMany2XAutocomplete extends Many2XAutocomplete {
 
     get sources() {
         const sources = super.sources;
-        if (!this.props.quickCreate)
+        if (!this.props.canCreate)
         {
             return sources;
         }
@@ -67,7 +67,19 @@ export class PartnerMany2XAutocomplete extends Many2XAutocomplete {
 
 }
 
-export class PartnerAutoCompleteMany2one extends Many2OneField {}
+PartnerMany2XAutocomplete.props = {
+    ...Many2XAutocomplete.props,
+    canCreate: { type: Boolean, optional: true },
+}
+
+export class PartnerAutoCompleteMany2one extends Many2OneField {
+    get Many2XAutocompleteProps() {
+        return {
+            ...super.Many2XAutocompleteProps,
+            canCreate: this.props.canCreate,
+        };
+    }
+}
 
 PartnerAutoCompleteMany2one.components = {
     ...Many2OneField.components,
