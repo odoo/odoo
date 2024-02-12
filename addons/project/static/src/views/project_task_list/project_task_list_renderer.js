069a6b7d752525bc6e0518b34c65083ee4092fe5
Aaron Bohy <aab@odoo.com>
2024-02-12 11:45:18 +0100

    [FIX] project,web: improve perf of project task list view
    Before this commit, to render the table, the project task list
    view computed the list of selected records once for each cell,
    and then iterated over that selection to check whether selected
    tasks were all associated with the same project (to set the
    stage_id field readonly if not). However, computing the selection
    requires to iterate over all records, so the rendering was O(n^2).
    As a consequence, the rendering of (not so) large tables was very
    slow (~1s for 80 records).
    
    With this commit, we compute only once for the whole table whether
    the selection contains records from different projects.

--- addons/project/static/src/views/project_task_list/project_task_list_renderer.js
+++ addons/project/static/src/views/project_task_list/project_task_list_renderer.js
@@ -5,14 +5,32 @@ import { getRawValue } from "@web/views/kanban/kanban_record";
 import { _t } from "@web/core/l10n/translation";
 
 export class ProjectTaskListRenderer extends ListRenderer {
+    /**
+     * This method prevents from computing the selection once for each cell when
+     * rendering the list. Indeed, `selection` is a getter which browses all
+     * records, so computing it for each cell slows down the rendering a lot on
+     * large tables. Moreover, it also prevents from iterating over the selection
+     * to compare tasks' projects.
+     *
+     * It returns true iff the selected tasks are all in the same project.
+     */
+    areSelectedTasksInSameProject() {
+        if (this._areSelectedTasksInSameProject === undefined) {
+            const selection = this.props.list.selection;
+            const projectId = selection.length && getRawValue(selection[0], "project_id");
+            this._areSelectedTasksInSameProject = selection.every(
+                (task) => getRawValue(task, "project_id") === projectId
+            );
+            Promise.resolve().then(() => {
+                delete this._areSelectedTasksInSameProject;
+            });
+        }
+        return this._areSelectedTasksInSameProject;
+    }
     isCellReadonly(column, record) {
         let readonly = false;
-        const selection = this.props.list.selection;
-        if (column.name === "stage_id" && selection.length) {
-            const projectId = getRawValue(selection[0], "project_id");
-            readonly = selection.some(
-                (task) => getRawValue(task, "project_id") !== projectId
-            );
+        if (column.name === "stage_id") {
+            readonly = !this.areSelectedTasksInSameProject();
         }
         return readonly || super.isCellReadonly(column, record);
     }
