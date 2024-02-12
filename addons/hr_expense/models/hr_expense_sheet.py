a7431c7c9afaaebd89491d3e4c641a60277ab5d2
late-odoo <late@odoo.com>
2024-02-12 16:33:18 +0530

    [FIX] hr_expense: prevent error when generating post journal entries
    Currently an error occurs when the user is generating Post Journal Entries of an
    employee whose work email is not specified.
    
    Steps to Reproduce:
    - Install `hr_expense` module.
    - Open Expenses > Expense Reports.
    - Open a report of employee whose work email is not specified.
    - Make sure the status of the selected recordset is `Approved`
    - Now click on the `Post Journal Entries` button.
    - The error is generated.
    
    Error: ValueError: External ID not found in the
    system: hr.open_view_employee_tree
    
    This error arises at [1] when it attempts to access the xml id with
    the view`hr.open_view_employee_tree`but due to recent changes in forward port of
    master this view has been removed with [2] as it wasn't used anywhere; but in
    [3] `hr.open_view_employee_tree` was used and due to this now the error is
    generated.
    
    This commit fixes the above issue by using view `hr.open_view_employee_list_my`
    instead of the removed view  `hr.open_view_employee_tree`.
    
    Link: [1]-https://github.com/odoo/odoo/blob/4126dcc08ef22f2b644559ba24bd4ce093684f1c/addons/hr_expense/models/hr_expense_sheet.py#L638
    
    [2]-https://github.com/odoo/odoo/commit/abd909498e4fd519ff8431ea9d7d09da4fc7c59a
    
    [3]-https://github.com/odoo/odoo/commit/2c0b01e79e81e6a28ca5f610ad14d4cb8fd5d756
    
    sentry-4968357276
    
    closes odoo/odoo#154165
    
    X-original-commit: 65adc3d92a1ce1d1c8bdab1a7631531c75e78e8a
    Signed-off-by: Thomas Lefebvre (thle) <thle@odoo.com>

--- addons/hr_expense/models/hr_expense_sheet.py
+++ addons/hr_expense/models/hr_expense_sheet.py
@@ -635,7 +635,7 @@ class HrExpenseSheet(models.Model):
 
         missing_email_employees = self.filtered(lambda sheet: not sheet.employee_id.work_email).employee_id
         if missing_email_employees:
-            action = self.env['ir.actions.actions']._for_xml_id('hr.open_view_employee_tree')
+            action = self.env['ir.actions.actions']._for_xml_id('hr.open_view_employee_list_my')
             action['domain'] = [('id', 'in', missing_email_employees.ids)]
             raise RedirectWarning(_("The work email of some employees is missing. Please add it on the employee form"), action, _("Show missing work email employees"))
 
