7bec0b1303b24fb075970bfd46b481676c5027d2
Aaron Bohy <aab@odoo.com>
2024-02-12 11:45:18 +0100

    [FIX] project,web: improve perf of project task list view
    Before this commit, to render the table, the project task list
    view computed the list of selected records once for each cell,
    and then iterated over that selection to check whether selected
    tasks were all associated with the same project (to set the
    stage_id field readonly if not). However, computing the selection
    requires to iterate over all records, so the rendering was O(n^2).
    As a consequence, the rendering of (not so) large tables was very
    slow (~1s for 80 records).
    
    With this commit, we compute only once for the whole table whether
    the selection contains records from different projects.
    
    X-original-commit: 22f897275db40649f4ca4958fe29a2641ed1742a

--- addons/web/static/src/model/relational_model/dynamic_list.js
+++ addons/web/static/src/model/relational_model/dynamic_list.js
@@ -40,6 +40,10 @@ export class DynamicList extends DataPoint {
         return this.config.domain;
     }
 
+    /**
+     * Be careful that this getter is costly, as it iterates over the whole list
+     * of records. This property should not be accessed in a loop.
+     */
     get editedRecord() {
         return this.records.find((record) => record.isInEdition);
     }
@@ -52,6 +56,10 @@ export class DynamicList extends DataPoint {
         return this.config.offset;
     }
 
+    /**
+     * Be careful that this getter is costly, as it iterates over the whole list
+     * of records. This property should not be accessed in a loop.
+     */
     get selection() {
         return this.records.filter((record) => record.selected);
     }
