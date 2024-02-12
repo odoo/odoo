662e93396213ded1bc3015c8ffa0489c393b2411
Maximilien (malb) <malb@odoo.com>
2024-02-12 09:51:39 +0100

    [FIX] account: analytic widget UX
    On the analytic widget, when putting an Analytic Account, a floppy disk appear
    on top of the wizard.
    This button is used to create a new analytic distribution template. It is
    confusing for users that thinks that the purpose of the button is to save the
    analytic distribution.
    
    This PR will replace the button to be a link called "New model".
    
    Also, this pr will fill some field (partner_id, account_prefix and product_id)
    if there are populated.
    
    Task: 3736786
    X-original-commit: c44367a2f67f305052d28a3b9e313ad63ff46907

--- addons/analytic/static/src/components/analytic_distribution/analytic_distribution.js
+++ addons/analytic/static/src/components/analytic_distribution/analytic_distribution.js
@@ -507,9 +507,17 @@ export class AnalyticDistribution extends Component {
     }
 
     onSaveNew() {
-        this.openTemplate({ resId: false, context: {
-            'default_analytic_distribution': this.listForJson,
-        }});
+        const { record, product_field, account_field } = this.props;
+        this.openTemplate({
+            resId: false,
+            context: {
+                default_analytic_distribution: this.listForJson,
+                default_partner_id: record.data['partner_id'] ? record.data['partner_id'][0] : undefined,
+                default_product_id: product_field ? record.data[product_field][0] : undefined,
+                default_account_prefix: account_field ? record.data[account_field][1].substr(0, 3) : undefined,
+            },
+        });
+
         this.closeAnalyticEditor();
     }
 
