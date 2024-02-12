04c9b0d8e365600a05982966049fec800ab9a2d8
Julien Van Roy <juvr@odoo.com>
2024-02-12 10:33:28 +0000

    [FIX] account_edi_ubl_cii: import with fixed taxes and discount
    Fix import of invoices with fixed taxes, discount and quantity > 1.
    
    When exporting the UBL xml, we compute the fixed tax amount of the line
    and create a charge on the line with this amount (e.g. see the test).
    
    When importing this xml, we read the charges with code 'AEO' on each
    lines and use the `amount` and `name` to search the fixed taxes on the
    database. This issue is that we do not take the quantity of the line
    into account: if we have a line with quantity = 2, and the fixed tax
    amount = 1, the xml indicates a charge of 2. When we import the line,
    the amount of the fixed tax to be searched should be 2 / quantity = 1.
    
    In addition, add an test for the export and import of a line with
    quantity > 1, a discount, a fixed tax and a regular tax.
    
    opw-3717306
    
    X-original-commit: a1bcdc28973f712e608c6ab8067f58a2ba69ea95

--- addons/account_edi_ubl_cii/models/account_edi_common.py
+++ addons/account_edi_ubl_cii/models/account_edi_common.py
@@ -607,7 +607,7 @@ class AccountEdiCommon(models.AbstractModel):
                     # Handle Fixed Taxes: when exporting from Odoo, we use the allowance_charge node
                     fixed_taxes_list.append({
                         'tax_name': reason.text,
-                        'tax_amount': float(amount.text),
+                        'tax_amount': float(amount.text) / billed_qty,
                     })
                 else:
                     allow_charge_amount += float(amount.text) * discount_factor
@@ -637,7 +637,7 @@ class AccountEdiCommon(models.AbstractModel):
 
         # discount
         discount = 0
-        amount_fixed_taxes = sum(d['tax_amount'] for d in fixed_taxes_list)
+        amount_fixed_taxes = sum(d['tax_amount'] * billed_qty for d in fixed_taxes_list)
         if billed_qty * price_unit != 0 and price_subtotal is not None:
             discount = 100 * (1 - (price_subtotal - amount_fixed_taxes) / (billed_qty * price_unit))
 
