96dfa7f39b84a2fea2076c7be72b3b3161790cd1
Hubert Van De Walle <huvw@odoo.com>
2024-02-12 09:15:03 +0000

    [FIX] web: missing measures in graph view
    Steps to reproduce
    ==================
    
    - Install account_accountant
    - Go to settings
    - Enable Budget Management
    - Go to Accounting > Reporting > Management > Budgets Analysis
    - Switch to the graph view
    - Change the measure to "Planned amount" and then back to
      "Practical amount"
    
    => The practical_amount measure is undefined,
       The theoritical_amount measure is missing.
    
    Cause of the issue
    ==================
    
    The view is defined as follows:
    
    ```xml
    <graph string="Budget Lines" sample="1">
        <field name="crossovered_budget_id" type="row"/>
        <field name="planned_amount"  type="measure" string="Planned amount"/>
        <field name="theoritical_amount"  type="measure" string="Theoretical amount"/>
        <field name="practical_amount" type="measure" string="Practical amount"/>
    </graph>
    ```
    
    The theoritical_amount and practical_amount are non stored fields and
    thus are skipped inside `computeReportMeasures` unless they are passed
    in `activeMeasures | additionalMeasures`. [0]
    
    When parsing the graph view, the last field of type measure is
    passed to the graph model and is the one that will be used initially. [1]
    
    This is why the practical_amount is initially defined.
    
    Solution
    ========
    
    We simply need to keep track of fields of type measure.
    This was the case in 14.0 but got lost in the conversion.
    
    ---
    
    [0]: https://github.com/odoo/odoo/blob/e7a9ebec3176c37485643fcda2381e489a1df86f/addons/web/static/src/views/helpers/utils.js#L49-L60
    [1]: https://github.com/odoo/odoo/blob/0fb64bef16914937cf4a1d1618fb58ade6d16f14/addons/web/static/src/views/graph/graph_arch_parser.js#L63
    
    opw-3713613
    
    closes odoo/odoo#153947
    
    X-original-commit: 76178cd61ba10ff29b24183edb550c54b319a230
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>
    Signed-off-by: Hubert Van De Walle <huvw@odoo.com>

--- addons/web/static/src/views/graph/graph_model.js
+++ addons/web/static/src/views/graph/graph_model.js
@@ -179,6 +179,7 @@ export class GraphModel extends Model {
         this._normalize(metaData);
 
         metaData.measures = computeReportMeasures(metaData.fields, metaData.fieldAttrs, [
+            ...(metaData.viewMeasures || []),
             metaData.measure,
         ]);
 
