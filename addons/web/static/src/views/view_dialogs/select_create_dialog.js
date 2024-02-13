ed004abed49794bba282127cb012dd752d03238a
Khushi <khpa@odoo.com>
2024-02-13 13:09:47 +0000

    [FIX] documents: handle multiple clicks on record in selectCreateDialog
    Steps to reproduce:
    - Go to Documents activity view.
    - Click on Schedule activity.
    - Perform multiple clicks on any record.
    
    Issue:
    The 'Schedule Activity' wizard opened as many times as the record was clicked.
    As a result, even after successfully scheduling an activity on that record,
    the user still faced multiple open wizards remaining and had to manually close
    each one of them.
    
    Fix:
    This commit introduces a method `executeOnceAndClose` which makes use of a
    flag 'busy' to ensure that the `onSelected` function is called only once and
    hence exactly one `Schedule Activity` wizard is opened, despite clicking a record
    more than once.
    
    closes odoo/odoo#154738
    
    Task: 3721404
    X-original-commit: 12889ecbc5f76ee70ea6caeac47faeeeb31772af
    Signed-off-by: Khushi Patel (khpa) <khpa@odoo.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/views/view_dialogs/select_create_dialog.js
+++ addons/web/static/src/views/view_dialogs/select_create_dialog.js
@@ -47,6 +47,7 @@ export class SelectCreateDialog extends Component {
         this.dialogService = useService("dialog");
         this.state = useState({ resIds: [] });
         const noContentHelp = this.props.noContentHelp || getDefaultNoContentHelp();
+        this.busy = false; // flag used to ensure we only call once the onSelected/onUnselect props
         this.baseViewProps = {
             display: { searchPanel: false },
             editable: false, // readonly
@@ -80,17 +81,28 @@ export class SelectCreateDialog extends Component {
         return props;
     }
 
+    async executeOnceAndClose(callback) {
+        if (!this.busy) {
+            this.busy = true;
+            try {
+                await callback();
+            } catch (e) {
+                this.busy = false;
+                throw e;
+            }
+            this.props.close();
+        }
+    }
+
     async select(resIds) {
         if (this.props.onSelected) {
-            await this.props.onSelected(resIds);
-            this.props.close();
+            this.executeOnceAndClose(() => this.props.onSelected(resIds));
         }
     }
 
     async unselect() {
         if (this.props.onUnselect) {
-            await this.props.onUnselect();
-            this.props.close();
+            this.executeOnceAndClose(() => this.props.onUnselect());
         }
     }
 
