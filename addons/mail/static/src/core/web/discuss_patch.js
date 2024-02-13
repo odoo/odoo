bcc7964ff7a32aeb27d5b2c0fc45671b2c0bfd33
Killian Frappart <kfr@odoo.com>
2024-02-13 10:58:37 +0100

    [IMP] mail: add breadcrumbs to Discuss
    This commit adds breadcrumbs to Discuss page, so that users can easily
    browse backwards.
    
    Task-3734245
    
    closes odoo/odoo#153802
    
    Related: odoo/enterprise#60896
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/core/web/discuss_patch.js
+++ addons/mail/static/src/core/web/discuss_patch.js
@@ -1,5 +1,21 @@
+import { onRendered } from "@odoo/owl";
+
 import { Discuss } from "@mail/core/common/discuss";
 import { DiscussSidebar } from "@mail/core/web/discuss_sidebar";
 import { MessagingMenu } from "@mail/core/web/messaging_menu";
 
-Object.assign(Discuss.components, { DiscussSidebar, MessagingMenu });
+import { ControlPanel } from "@web/search/control_panel/control_panel";
+import { patch } from "@web/core/utils/patch";
+
+Object.assign(Discuss.components, { ControlPanel, DiscussSidebar, MessagingMenu });
+
+patch(Discuss.prototype, {
+    setup() {
+        super.setup();
+        onRendered(() => {
+            if (this.thread?.displayName) {
+                this.env.config?.setDisplayName(this.thread.displayName);
+            }
+        });
+    },
+});
