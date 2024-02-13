bbfc86c815aa6d4d1907afc94cfecadf059efa74
Harsh Modi <hamo@odoo.com>
2024-02-13 12:54:46 +0530

    [REF] l10n_in: seperate account.py to account_tax.py, account_move_line.py
    Since existing model AccountMoveLine and AccountTax where
    lying under the same file which was bit hard to find, now
    the AccountTax(account_tax.py) and AccountMoveLine(account_move_line.py)
    have seperate file and removes the bad code from `account.move.line`

--- addons/l10n_in/models/__init__.py
+++ addons/l10n_in/models/__init__.py
@@ -1,7 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import template_in
-from . import account
 from . import account_invoice
+from . import account_move_line
+from . import account_tax
 from . import company
 from . import product_template
 from . import port_code
