1bbbdacb358b18aaf4c7bd9229e374123af45578
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-02-13 11:15:19 +0100

    [FIX] web_editor: undo only table resize website editor
    Issue:
    =====
    Undo of resize operation on table do 2 undos in a row.
    
    Steps to reproduce the issue:
    =============================
    - Go to website editor
    - Add a text block
    - write `/table` to insert a table
    - Resize the table
    - Press `ctr+z` to undo
    - The table is removed
    
    Origin of the issue:
    ====================
    The resize operation wasn't saved a step and is considered as draft
    then it will be discarded and then the undo applied on the last saved
    step that's why it appears as 2 undo in a row.
    
    Solution:
    =========
    Call `historyStep` when we stop resizing.
    
    task-3743697
    
    X-original-commit: 836a68e29c8fa881854b19c7489086537127d4a3

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -4397,6 +4397,7 @@ export class OdooEditor extends EventTarget {
                 ev.preventDefault();
                 this._isResizingTable = false;
                 this._toggleTableResizeCursor(false);
+                this.historyStep();
                 this.document.removeEventListener('mousemove', resizeTable);
                 this.document.removeEventListener('mouseup', stopResizing);
                 this.document.removeEventListener('mouseleave', stopResizing);
