1dc4ff2161f0d0b788854b389ede5738ce940448
Pierre Rousseau <pro@odoo.com>
2024-02-13 07:17:50 +0100

    [REF] spreadsheet: split pivot core in two
    This commit splits the pivot core in two parts:
    - pivot_core_plugin.js: the plugin that handles the pivot commands
    - pivot_core_global_filters.js: the plugin that handles the global
    filters related to pivot
    
    This is done to make the pivot core ready for the arrival of the
    spreadsheet pivot table.
    
    Task: 3748392

--- addons/spreadsheet/static/src/@types/pivot.d.ts
+++ addons/spreadsheet/static/src/@types/pivot.d.ts
@@ -29,11 +29,38 @@ declare module "@spreadsheet" {
       sortedColumn: SortedColumn | null;
     }
 
-    export interface OdooPivotDefinition extends CommonPivotDefinition{
+    export interface OdooPivotDefinition extends CommonPivotDefinition {
         model: string;
         domain: Array;
         context: Object;
     }
+
+    export interface PivotHandler {
+      getTable(payload: ExtendedInsertPivotPayload): SpreadsheetPivotTable;
+    }
+
+    export interface OdooLocalPivot {
+      type: "ODOO";
+      definition: OdooPivotDefinition;
+    }
+
+    export interface SpreadsheetLocalPivot {
+      type: "SPREADSHEET";
+      definition: SpreadsheetPivotDefinition;
+    }
+
+    export type LocalPivot = OdooLocalPivot | SpreadsheetLocalPivot;
+
+    export interface SpreadsheetPivotDefinition extends CommonPivotDefinition {
+        range: string;
+    }
+
+    export interface GFLocalPivot {
+      id: string;
+      fieldMatching: Record<string, any>;
+    }
+
+    export type PivotDefinition = OdooPivotDefinition | SpreadsheetPivotDefinition;
 
     export interface Field {
         name: string;
