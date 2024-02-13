1e1d30127292569e9916b8652333d575366d9aea
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-02-13 08:11:57 +0100

    [FIX] web_editor: hide dropdown on hiding toolbar
    Steps to reproduce the issue:
    =============================
    - Go to email marketing
    - Create a new one with plain text
    - Add some text
    - select some text and open color dropdown (or any other dropdown)
    - Click out and select some other text
    - The color dropdown is opened by default.
    
    Origin of the issue:
    ====================
    When hiding the toolbar, the dropdown becomes hidden but not closed.
    
    Solution:
    =========
    We need to close the dropdown when hiding the toolbar
    
    task-3636212

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -2130,6 +2130,11 @@ export class OdooEditor extends EventTarget {
                     this.toolbar.style.visibility = show ? 'visible' : 'hidden';
                 }
                 if (show === false) {
+                    // close all dropdowns
+                    for (const dropdown of this.toolbar.querySelectorAll("ul.dropdown-menu.show")) {
+                        dropdown.classList.remove('show');
+                        dropdown.classList.add('hide');
+                    }
                     return;
                 }
             }
