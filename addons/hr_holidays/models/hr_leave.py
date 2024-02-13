1940d920cdad45c61717f9e7a53332231c80ee2f
Mélanie <mepe@odoo.com>
2024-02-13 17:29:55 +0100

    [FIX] hr_holidays_gantt : fix default date on gantt view
    STEP TO REPRODUCE :
    ===================
    
            1- Change Anita Oliver's contract's calendar's timezone
            2- Go to time off
            3- Select Approvals/Time off
            4- select gantt view
            5- Click on a 22 february
    You will see these default dates : 21 february and 22 february instead of 22 february and 22 february
    
    Task: 3689849
    X-original-commit: 343fc32fd12590805724ec2bdf017b0c32c57fc1

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -93,7 +93,7 @@ class HolidaysRequest(models.Model):
             lt = self.env['hr.leave.type'].browse(defaults.get('holiday_status_id'))
             defaults['state'] = 'confirm' if lt and lt.leave_validation_type != 'no_validation' else 'draft'
 
-        if 'date_from' and 'date_to' in fields_list:
+        if 'date_from' and 'date_to' in fields_list and not ('request_date_from' and 'request_date_to' in defaults):
             now = fields.Datetime.now()
             if 'date_from' not in defaults:
                 defaults['date_from'] = now.replace(hour=0, minute=0, second=0)
