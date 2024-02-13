ce4f47bafa425ed7977a0ba6eaac8721932eed33
Gaetan Vanden Bergh (gavb) <gavb@odoo.com>
2024-02-13 10:42:58 +0100

    [FIX] mail: no author message fallback avatar
    Prerequisite:
    - User A (admin)
    - User B (random internal)
    
    Steps:
    - Login as B
    - Find a chatter and send a log note or a message
    - Logout
    - Login with A
    - Delete user B and the contact related
    - Open the same chatter than above
    
    Actual result:
    - Multiple traceback due to "no author"
    - No avatar
    
    Expected result:
    - No traceback
    - Message avatar is the default one / the placeholder
    - Message author name is the email_from field
    
    opw-3743727
    
    odoo/odoo@b1a96927
    
    closes odoo/odoo#153793
    
    Signed-off-by: Didier Debondt (did) <did@odoo.com>

--- addons/mail/static/src/core/common/message_in_reply.js
+++ addons/mail/static/src/core/common/message_in_reply.js
@@ -2,6 +2,7 @@
 
 import { Component, useState } from "@odoo/owl";
 
+import { DEFAULT_AVATAR } from "@mail/core/common/persona_service";
 import { useService } from "@web/core/utils/hooks";
 import { url } from "@web/core/utils/urls";
 
@@ -22,6 +23,11 @@ export class MessageInReply extends Component {
         ) {
             return url("/mail/static/src/img/email_icon.png");
         }
-        return this.props.message.parentMessage.author.avatarUrl;
+
+        if (this.props.message.parentMessage.author) {
+            return this.props.message.parentMessage.author.avatarUrl;
+        }
+
+        return DEFAULT_AVATAR
     }
 }
