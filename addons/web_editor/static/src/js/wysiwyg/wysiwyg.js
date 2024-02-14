b8c0b0fd39a898e2bcaa6e4b71cfed8684c8945d
Benoit Socias <bso@odoo.com>
2024-02-14 13:32:04 +0100

    [FIX] web_editor: not show dialog when displaying error in popup
    If an error happens when a website page is being saved, the error
    message is displayed as a popup on the edited block.
    Unfortunately, an error dialog is also displayed.
    
    This commit prevents that dialog from being displayed when the error is
    already shown to the user.
    
    task-3599890
    
    X-original-commit: b8d48c19854029a868d04d5693f809ec9012294c

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2341,6 +2341,9 @@ const Wysiwyg = Widget.extend({
                             placement: 'auto',
                         })
                         .popover('show');
+                    // Error has been handled by showing it near the element.
+                    // Do not display a traceback dialog about it.
+                    response.event.preventDefault();
                 });
             });
         });
