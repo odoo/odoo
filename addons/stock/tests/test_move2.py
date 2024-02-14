62a55a1e01d690beddc68a505e0493f88e9b004c
JF Aubert <ajf@odoo.com>
2024-02-14 11:30:39 +0100

    [PERF] stock: refactor _get_rule & _search_rule
    This refactor consists of:
    - create a new _search_rules function that searches for all applicable
      rules from an aggregated dataset
    - move _search_rule, _get_rule & _get_rule_domain to the stock.rule model
    - make them filter the 'self' rules when called on a recordset
      prepared by _search_rules
    
    This a an impact on:
    - 'procurement.group' run()
    - 'stock.move' _push_apply() & _adjust_procure_method()
    
    The gain of confirming a purchase order (10-1000 lines) is 5-15%
    The gain of confirming a sale order (10-1000 lines) is 5-30%
    (procurement's gain 40-90%)

--- addons/stock/tests/test_move2.py
+++ addons/stock/tests/test_move2.py
@@ -2870,7 +2870,7 @@ class TestRoutes(TestStockCommon):
         })
 
         # We alter one rule and we set it to 'mts_else_mto'
-        rule = self.env['procurement.group']._get_rule(product_A, final_location, {'warehouse_id': warehouse})
+        rule = self.env['stock.rule']._get_rule(product_A, final_location, {'warehouse_id': warehouse})
         rule.procure_method = 'mts_else_mto'
 
         self.env['stock.quant']._update_available_quantity(product_A, warehouse.lot_stock_id, 5.0)
@@ -2923,7 +2923,7 @@ class TestRoutes(TestStockCommon):
         })
 
         # We alter one rule and we set it to 'mts_else_mto'
-        rule = self.env['procurement.group']._get_rule(product_A, final_location, {'warehouse_id': warehouse})
+        rule = self.env['stock.rule']._get_rule(product_A, final_location, {'warehouse_id': warehouse})
         rule.procure_method = 'mts_else_mto'
 
         self.env['stock.quant']._update_available_quantity(product_A, warehouse.lot_stock_id, 5.0)
@@ -2986,7 +2986,7 @@ class TestRoutes(TestStockCommon):
         })
 
         # We alter one rule and we set it to 'mts_else_mto'
-        rule = self.env['procurement.group']._get_rule(product_A, final_location, {'warehouse_id': warehouse})
+        rule = self.env['stock.rule']._get_rule(product_A, final_location, {'warehouse_id': warehouse})
         rule.procure_method = 'mts_else_mto'
 
         self.env['stock.quant']._update_available_quantity(product_A, warehouse.lot_stock_id, 4.0)
