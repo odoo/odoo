527f6fa3e42ea424107b6e03c0a6a2c30947d218
Arnaud Sibille <arsi@odoo.com>
2024-02-15 11:31:39 +0100

    [FIX] l10n_es: add more logic to l10n_es_is_simplified compute
    If you don't have enough information about your customer, the invoice
    should be made simplified.
    
    opw-3679325

--- addons/l10n_es/models/account_move.py
+++ addons/l10n_es/models/account_move.py
@@ -8,9 +8,19 @@ class AccountMove(models.Model):
     l10n_es_is_simplified = fields.Boolean("Is Simplified",
                                            compute="_compute_l10n_es_is_simplified", readonly=False, store=True)
 
-    @api.depends('partner_id')
+    @api.depends(
+        'partner_id',
+        'partner_id.country_id',
+        'partner_id.vat',
+    )
     def _compute_l10n_es_is_simplified(self):
         simplified_partner = self.env.ref('l10n_es.partner_simplified', raise_if_not_found=False)
-        if simplified_partner:
-            for move in self:
-                move.l10n_es_is_simplified = (move.partner_id == simplified_partner)
+        for move in self.filtered_domain([('state', '!=', 'posted'), ('partner_id', '!=', False)]):
+            partner = move.partner_id
+            if partner == simplified_partner \
+                or not partner.country_id \
+                or partner.country_id.code == 'ES' and not partner.vat:
+
+                move.l10n_es_is_simplified = True
+            else:
+                move.l10n_es_is_simplified = False
