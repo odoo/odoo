7d3c76d872c958e6f917978a081f2ab4849c1274
Romeo Fragomeli <rfr@odoo.com>
2024-02-15 15:17:43 +0100

    [IMP] web: convert `user menu` tests to HOOT
    task-3705027
    
    closes odoo/odoo#154218
    
    Signed-off-by: Romain Estievenart (res) <res@odoo.com>

---
+++ addons/web/static/tests/webclient/user_menu.test.js
@@ -0,0 +1,163 @@
+import { beforeEach, expect, test } from "@odoo/hoot";
+import {
+    click,
+    queryAll,
+    queryAllAttributes,
+    queryAllTexts,
+    queryAllProperties,
+} from "@odoo/hoot-dom";
+import {
+    clearRegistry,
+    contains,
+    mockService,
+    mountWithCleanup,
+    onRpc,
+    patchWithCleanup,
+} from "@web/../tests/web_test_helpers";
+
+import { browser } from "@web/core/browser/browser";
+import { registry } from "@web/core/registry";
+import { user } from "@web/core/user";
+import { getOrigin } from "@web/core/utils/urls";
+
+import { UserMenu } from "@web/webclient/user_menu/user_menu";
+import { odooAccountItem, preferencesItem } from "@web/webclient/user_menu/user_menu_items";
+
+const userMenuRegistry = registry.category("user_menuitems");
+
+beforeEach(async () => {
+    patchWithCleanup(user, { name: "Sauron", writeDate: "2024-01-01 12:00:00" });
+    clearRegistry(userMenuRegistry);
+});
+
+test.tags("desktop")("can be rendered", async () => {
+    userMenuRegistry.add("bad_item", () => ({
+        type: "item",
+        id: "bad",
+        description: "Bad",
+        callback: () => {
+            expect.step("callback bad_item");
+        },
+        sequence: 10,
+    }));
+    userMenuRegistry.add("ring_item", () => ({
+        type: "item",
+        id: "ring",
+        description: "Ring",
+        callback: () => {
+            expect.step("callback ring_item");
+        },
+        sequence: 5,
+    }));
+    userMenuRegistry.add("frodo_item", () => ({
+        type: "switch",
+        id: "frodo",
+        description: "Frodo",
+        callback: () => {
+            expect.step("callback frodo_item");
+        },
+        sequence: 11,
+    }));
+    userMenuRegistry.add("separator", () => ({
+        type: "separator",
+        sequence: 15,
+    }));
+    userMenuRegistry.add("invisible_item", () => ({
+        type: "item",
+        id: "hidden",
+        description: "Hidden Power",
+        callback: () => {},
+        sequence: 5,
+        hide: true,
+    }));
+    userMenuRegistry.add("eye_item", () => ({
+        type: "item",
+        id: "eye",
+        description: "Eye",
+        callback: () => {
+            expect.step("callback eye_item");
+        },
+    }));
+    await mountWithCleanup(UserMenu);
+    expect("img.o_user_avatar").toHaveCount(1);
+    expect("img.o_user_avatar").toHaveAttribute(
+        "data-src",
+        `${getOrigin()}/web/image/res.partner/17/avatar_128?unique=1704106800000`
+    );
+    expect(".dropdown-menu .dropdown-item").toHaveCount(0);
+    // `visible: false` due to the removed `src` by the test suite (=> size 0x0)
+    await contains("button.dropdown-toggle", { visible: false }).click();
+    expect(".dropdown-menu .dropdown-item").toHaveCount(4);
+    expect(".dropdown-menu .dropdown-item input.form-check-input").toHaveCount(1);
+    expect("div.dropdown-divider").toHaveCount(1);
+    expect(queryAllProperties(".dropdown-menu > *", "tagName")).toEqual([
+        "SPAN",
+        "SPAN",
+        "SPAN",
+        "DIV",
+        "SPAN",
+    ]);
+    expect(queryAllAttributes(".dropdown-menu .dropdown-item", "data-menu")).toEqual([
+        "ring",
+        "bad",
+        "frodo",
+        "eye",
+    ]);
+    expect(queryAllTexts(".dropdown-menu .dropdown-item")).toEqual(["Ring", "Bad", "Frodo", "Eye"]);
+    queryAll(".dropdown-menu .dropdown-item").forEach((el) => click(el));
+    expect([
+        "callback ring_item",
+        "callback bad_item",
+        "callback frodo_item",
+        "callback eye_item",
+    ]).toVerifySteps();
+});
+
+test.tags("desktop")("display the correct name in debug mode", async () => {
+    patchWithCleanup(odoo, { debug: "1" });
+    await mountWithCleanup(UserMenu);
+    expect("img.o_user_avatar").toHaveCount(1);
+    expect("small.oe_topbar_name").toHaveCount(1);
+    expect(".oe_topbar_name").toHaveText("Sauron" + "\n" + "test");
+});
+
+test.tags("desktop")("can execute the callback of settings", async () => {
+    onRpc("/web/dataset/call_kw/res.users/action_get", () => {
+        return Promise.resolve({
+            name: "Change My Preferences",
+            res_id: 0,
+        });
+    });
+    mockService("action", () => ({
+        doAction(actionId) {
+            expect.step("" + actionId.res_id);
+            expect.step(actionId.name);
+            return Promise.resolve(true);
+        },
+    }));
+
+    userMenuRegistry.add("profile", preferencesItem);
+    await mountWithCleanup(UserMenu);
+    await contains("button.dropdown-toggle", { visible: false }).click();
+    expect(".dropdown-menu .dropdown-item").toHaveCount(1);
+    expect(".dropdown-menu .dropdown-item").toHaveText("Preferences");
+    await contains(".dropdown-menu .dropdown-item").click();
+    expect(["7", "Change My Preferences"]).toVerifySteps();
+});
+
+test.tags("desktop")("click on odoo account item", async () => {
+    patchWithCleanup(browser, {
+        open: (url) => expect.step(`open ${url}`),
+    });
+    onRpc("/*", (mock) => {
+        expect.step(new URL(mock.url).pathname);
+        return new Error("not found");
+    });
+    userMenuRegistry.add("odoo_account", odooAccountItem);
+    await mountWithCleanup(UserMenu);
+    await contains("button.dropdown-toggle", { visible: false }).click();
+    expect(".o-dropdown--menu .dropdown-item").toHaveCount(1);
+    expect(".o-dropdown--menu .dropdown-item").toHaveText("My Odoo.com account");
+    await contains(".o-dropdown--menu .dropdown-item").click();
+    expect(["/web/session/account", "open https://accounts.odoo.com/account"]).toVerifySteps();
+});
