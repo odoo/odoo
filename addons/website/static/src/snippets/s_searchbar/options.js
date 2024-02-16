d86092617dbaa393e50527b0fe20918436283d65
Benjamin Vray <bvr@odoo.com>
2024-02-16 15:40:29 +0100

    [FIX] website: fix searchbar snippets colors
    Since this commit [1], the two searchbar snippets are broken.
    
    The search bar input seems invisible because it shares the same color as
    the default snippet background and has no border. This style matches the
    search inputs used in apps (like the "/shop" search bar), which isn't
    affected by the "Input fields" settings in the "Theme" tab.
    
    To resolve this issue, this commit introduces a new option to choose
    between the "light" style (similar to "/shop" search bars) and the
    "default input style" for the 2 "Search" snippets.
    
    The "default input style" is automatically applied when the "Search"
    snippet (excluding saved snippets) is dropped to address the issue
    caused by the light-on-light color scheme.
    
    Steps to reproduce the issue:
    
    - While in Website edit mode, drag and drop a "Search" snippet onto the
    page.
    - Bug: The input appears invisible due to the lack of a border and a
    background color identical to the snippet's section color.
    
    [1]: https://github.com/odoo/odoo/commit/6b1d11a60d8e70b33c63da860bb81b015ce5ea20
    
    task-3662985
    
    closes odoo/odoo#158168
    
    X-original-commit: 773aaf5c7525b84719ff3196cca2ad4c02c29457
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/website/static/src/snippets/s_searchbar/options.js
+++ addons/website/static/src/snippets/s_searchbar/options.js
@@ -1,8 +1,30 @@
 /** @odoo-module **/
 
+import { _t } from "@web/core/l10n/translation";
 import options from '@web_editor/js/editor/snippets.options';
 
 options.registry.SearchBar = options.Class.extend({
+    /**
+     * @override
+     */
+    start() {
+        this.searchInputEl = this.$target[0].querySelector(".oe_search_box");
+        this.searchButtonEl = this.$target[0].querySelector(".oe_search_button");
+        return this._super(...arguments);
+    },
+    /**
+     * @override
+     */
+    onBuilt() {
+        // Fix in stable to remove the hard-coded "Light" style from the search
+        // bar and allow the search bar to adopt the styles of the theme's
+        // inputs. An option "setSearchbarStyle" has also been added to enable
+        // users to set the "Light" style if desired.
+        if (!this.$target[0].closest(".s_custom_snippet")) {
+            this._setSearchbarStyleLight(false);
+        }
+    },
+
     //--------------------------------------------------------------------------
     // Options
     //--------------------------------------------------------------------------
@@ -40,6 +62,65 @@ options.registry.SearchBar = options.Class.extend({
     setOrderBy: function (previewMode, widgetValue, params) {
         const form = this.$target.parents('form');
         form.find(".o_search_order_by").attr("value", widgetValue);
+    },
+    /**
+     * Sets the style of the searchbar.
+     *
+     * @see this.selectClass for parameters
+     */
+    setSearchbarStyle(previewMode, widgetValue, params) {
+        this._setSearchbarStyleLight(widgetValue === "light");
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    _computeWidgetState(methodName, params) {
+        if (methodName === "setSearchbarStyle") {
+            const searchInputIsLight = this.searchInputEl.matches(".border-0.bg-light");
+            const searchButtonIsLight = this.searchButtonEl.matches(".btn-light");
+            return searchInputIsLight && searchButtonIsLight ? "light" : "default";
+        }
+        return this._super(...arguments);
+    },
+    /**
+     * @todo Adapt in the XML directly in master.
+     * @override
+     */
+    async _renderCustomXML(uiFragment) {
+        // Create the <we-select> for the "Style" option, with choices "Default
+        // Input Style" and "Light". This is a stable fix to allow the user to
+        // apply the input style defined in the theme options to the search bar.
+        // Previously, the search bar style was hardcoded with the "Light"
+        // style, which was not visible with the default "Light" background of
+        // the "Search" snippet. Allowing the search bar to have the same style
+        // as the other inputs is also more coherent.
+        const weSelectEl = document.createElement("we-select");
+        weSelectEl.setAttribute("string", _t("Style"));
+        const defaultBtnEl = document.createElement("we-button");
+        defaultBtnEl.dataset.setSearchbarStyle = "default";
+        defaultBtnEl.textContent = _t("Default Input Style");
+        const lightBtnEl = document.createElement("we-button");
+        lightBtnEl.dataset.setSearchbarStyle = "light";
+        lightBtnEl.textContent = _t("Light");
+        weSelectEl.appendChild(defaultBtnEl);
+        weSelectEl.appendChild(lightBtnEl);
+
+        uiFragment.appendChild(weSelectEl);
+    },
+    /**
+     * @private
+     * @param {boolean} light
+     */
+    _setSearchbarStyleLight(light) {
+        this.searchInputEl.classList.toggle("border-0", light);
+        this.searchInputEl.classList.toggle("bg-light", light);
+        this.searchButtonEl.classList.toggle("btn-light", light);
+        this.searchButtonEl.classList.toggle("btn-primary", !light);
     },
 });
 
