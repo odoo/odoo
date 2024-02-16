598118fb437840f75c3892afb5155406ded5bdf2
Lucas Lefèvre (lul) <lul@odoo.com>
2024-02-16 16:02:34 +0100

    [FIX] spreadsheet: update o_spreadsheet to latest version
    ### Contains the following commits:
    
    https://github.com/odoo/o-spreadsheet/commit/d5e57e6d1 [REL] 16.2.32
    https://github.com/odoo/o-spreadsheet/commit/d40f5fb61 [FIX] import: detect date format at import Task: 3729969
    https://github.com/odoo/o-spreadsheet/commit/d2ac0ae94 [IMP] cell: avoid useless UPDATE_CELL Task: 3716689
    https://github.com/odoo/o-spreadsheet/commit/1a67085b7 [FIX] Selection: Loop selection based on content Task: 3709340

--- addons/spreadsheet/static/src/o_spreadsheet/o_spreadsheet.js
+++ addons/spreadsheet/static/src/o_spreadsheet/o_spreadsheet.js
@@ -28200,9 +28200,9 @@
         allowDispatch(cmd) {
             switch (cmd.type) {
                 case "UPDATE_CELL":
-                    return this.checkCellOutOfSheet(cmd);
+                    return this.checkValidations(cmd, this.checkCellOutOfSheet, this.checkUselessUpdateCell);
                 case "CLEAR_CELL":
-                    return this.checkValidations(cmd, this.chainValidations(this.checkCellOutOfSheet, this.checkUselessClearCell));
+                    return this.checkValidations(cmd, this.checkCellOutOfSheet, this.checkUselessClearCell);
                 default:
                     return 0 /* CommandResult.Success */;
             }
@@ -28497,7 +28497,7 @@
             else {
                 style = before ? before.style : undefined;
             }
-            let format = ("format" in after ? after.format : before && before.format) || detectFormat(afterContent);
+            const format = "format" in after ? after.format : before && before.format;
             /* Read the following IF as:
              * we need to remove the cell if it is completely empty, but we can know if it completely empty if:
              * - the command says the new content is empty and has no border/format/style
@@ -28541,7 +28541,7 @@
                 id,
                 content,
                 style,
-                format,
+                format: format || detectFormat(content),
                 isFormula: false,
             };
         }
@@ -28614,6 +28614,18 @@
             if (!cell)
                 return 89 /* CommandResult.NoChanges */;
             if (!cell.content && !cell.style && !cell.format) {
+                return 89 /* CommandResult.NoChanges */;
+            }
+            return 0 /* CommandResult.Success */;
+        }
+        checkUselessUpdateCell(cmd) {
+            const cell = this.getters.getCell(cmd);
+            const hasContent = "content" in cmd || "formula" in cmd;
+            const hasStyle = "style" in cmd;
+            const hasFormat = "format" in cmd;
+            if ((!hasContent || (cell === null || cell === void 0 ? void 0 : cell.content) === cmd.content) &&
+                (!hasStyle || deepEquals(cell === null || cell === void 0 ? void 0 : cell.style, cmd.style)) &&
+                (!hasFormat || (cell === null || cell === void 0 ? void 0 : cell.format) === cmd.format)) {
                 return 89 /* CommandResult.NoChanges */;
             }
             return 0 /* CommandResult.Success */;
@@ -34272,8 +34284,7 @@
             let row = zone.bottom;
             if (col > 0) {
                 let leftPosition = { sheetId, col: col - 1, row };
-                while (this.getters.getEvaluatedCell(leftPosition).type !== CellValueType.empty ||
-                    ((_a = this.getters.getCell(leftPosition)) === null || _a === void 0 ? void 0 : _a.content)) {
+                while ((_a = this.getters.getCell(leftPosition)) === null || _a === void 0 ? void 0 : _a.content) {
                     row += 1;
                     leftPosition = { sheetId, col: col - 1, row };
                 }
@@ -34282,8 +34293,7 @@
                 col = zone.right;
                 if (col <= this.getters.getNumberCols(sheetId)) {
                     let rightPosition = { sheetId, col: col + 1, row };
-                    while (this.getters.getEvaluatedCell(rightPosition).type !== CellValueType.empty ||
-                        ((_b = this.getters.getCell(rightPosition)) === null || _b === void 0 ? void 0 : _b.content)) {
+                    while ((_b = this.getters.getCell(rightPosition)) === null || _b === void 0 ? void 0 : _b.content) {
                         row += 1;
                         rightPosition = { sheetId, col: col + 1, row };
                     }
@@ -43561,11 +43571,10 @@
          * next cluster if the given cell is outside a cluster or at the border of a cluster in the given direction.
          */
         getEndOfCluster(startPosition, dim, dir) {
-            const sheet = this.getters.getActiveSheet();
             let currentPosition = startPosition;
             // If both the current cell and the next cell are not empty, we want to go to the end of the cluster
             const nextCellPosition = this.getNextCellPosition(startPosition, dim, dir);
-            let mode = !this.isCellEmpty(currentPosition, sheet.id) && !this.isCellEmpty(nextCellPosition, sheet.id)
+            let mode = !this.isEvaluatedCellEmpty(currentPosition) && !this.isEvaluatedCellEmpty(nextCellPosition)
                 ? "endOfCluster"
                 : "nextCluster";
             while (true) {
@@ -43575,7 +43584,7 @@
                     currentPosition.row === nextCellPosition.row) {
                     break;
                 }
-                const isNextCellEmpty = this.isCellEmpty(nextCellPosition, sheet.id);
+                const isNextCellEmpty = this.isEvaluatedCellEmpty(nextCellPosition);
                 if (mode === "endOfCluster" && isNextCellEmpty) {
                     break;
                 }
@@ -43589,13 +43598,24 @@
             return dim === "cols" ? currentPosition.col : currentPosition.row;
         }
         /**
-         * Check if a cell is empty or undefined in the model. If the cell is part of a merge,
-         * check if the merge containing the cell is empty.
+         * Check if a cell evaluated value is empty. If the cell is part of a merge,
+         * the check applies to the main cell of the merge.
          */
-        isCellEmpty({ col, row }, sheetId = this.getters.getActiveSheetId()) {
+        isEvaluatedCellEmpty({ col, row }) {
+            const sheetId = this.getters.getActiveSheetId();
             const position = this.getters.getMainCellPosition({ sheetId, col, row });
             const cell = this.getters.getEvaluatedCell(position);
             return cell.type === CellValueType.empty;
+        }
+        /**
+         * Checks if a cell is empty (i.e. does not have a content). If the cell is part of a merge,
+         * the check applies to the main cell of the merge.
+         */
+        isCellEmpty({ col, row }) {
+            var _a;
+            const sheetId = this.getters.getActiveSheetId();
+            const position = this.getters.getMainCellPosition({ sheetId, col, row });
+            return !((_a = this.getters.getCell(position)) === null || _a === void 0 ? void 0 : _a.content);
         }
         /** Computes the next cell position in the given direction by crossing through merges and skipping hidden cells.
          *
@@ -45770,9 +45790,9 @@
     Object.defineProperty(exports, '__esModule', { value: true });
 
 
-    __info__.version = '16.2.31';
-    __info__.date = '2024-02-09T14:28:32.047Z';
-    __info__.hash = '2e76bdf';
+    __info__.version = '16.2.32';
+    __info__.date = '2024-02-16T15:02:28.982Z';
+    __info__.hash = 'd5e57e6';
 
 
 })(this.o_spreadsheet = this.o_spreadsheet || {}, owl);
