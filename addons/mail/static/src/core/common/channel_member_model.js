fcf8d00660bb103461cdea5cf78a1956415c2a70
tsm-odoo <tsm@odoo.com>
2024-02-16 12:14:31 +0100

    [FIX] bus, mail: fix redundant bus subscriptions
    Since 16.3, opening the web client on the Odoo backends results in two
    subscribe events being sent through the bus websocket.
    
    Discuss listens to the thread changes to know when the subscription
    should be refreshed. In order to do so, the last subscription that was
    made is kept. This issue is that this subscription is empty in the
    first place so the first evaluation always considers the subscription
    should be made.
    
    In order to fix this issue, this PR refine the condition to determine
    if the subscription should be renewed:
    - The last subscription is different from the last one
    - The user joined a channel after the bus initialization
    - The user left a channel after the bus initialization
    
    In order to test those scenario in a reliable way, this PR also
    backports odoo#147455
    
    closes odoo/odoo#154292
    
    X-original-commit: 4f0b262dbeda8f8ebb8483f9dac090bac55c041d
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/core/common/channel_member_model.js
+++ addons/mail/static/src/core/common/channel_member_model.js
@@ -1,6 +1,7 @@
 /* @odoo-module */
 
 import { Record } from "@mail/core/common/record";
+import { deserializeDateTime } from "@web/core/l10n/dates";
 
 export class ChannelMember extends Record {
     static id = "id";
@@ -15,6 +16,8 @@ export class ChannelMember extends Record {
         return super.insert(...arguments);
     }
 
+    /** @type {string} */
+    create_date;
     /** @type {number} */
     id;
     persona = Record.one("Persona", { inverse: "channelMembers" });
@@ -28,6 +31,10 @@ export class ChannelMember extends Record {
      */
     getLangName() {
         return this.persona.lang_name;
+    }
+
+    get memberSince() {
+        return deserializeDateTime(this.create_date);
     }
 }
 
