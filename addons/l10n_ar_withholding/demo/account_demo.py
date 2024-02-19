08407be62a86949f96d6173946aad306cdd738e1
Yannick Tivisse <yti@odoo.com>
2024-02-19 16:27:55 +0100

    [IMP] l10n_xx(_hr_payroll): Merge demo company
    The use case is the following:
    - l10n_xx (usually depending on account) creates a company demo data
    - l10n_xx_hr_payroll (not depending on account) creates another company
    
    The goal is to allow defining the data to a foreign module (obvioulsy
    in the common dependencies) like "base", so that each module can easily
    define the same company and use it without having the create another bridge
    module that contains only demo data.
    
    closes odoo/odoo#154780
    
    Taskid: 3714759
    Related: odoo/enterprise#57063
    Related: odoo/upgrade#5719
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/l10n_ar_withholding/demo/account_demo.py
+++ addons/l10n_ar_withholding/demo/account_demo.py
@@ -7,7 +7,7 @@ class AccountChartTemplate(models.AbstractModel):
 
     def _post_load_demo_data(self, company=False):
         result = super()._post_load_demo_data(company)
-        if company == self.env.ref('l10n_ar.company_ri', raise_if_not_found=False):
+        if company == self.env.ref('base.company_ri', raise_if_not_found=False):
             # Because in demo we want to skip the config, while in data we want to require them to configure
             self.env['account.tax'].search([('l10n_ar_withholding_payment_type', '!=', False)]).write({'amount_type': 'percent', 'amount' : 1})
         return result
