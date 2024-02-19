9f319cbc95f8f4cb76df6f2b82e4b43a74f4b753
Guillaume-gdi <gdi@odoo.com>
2024-02-19 15:11:18 +0100

    [IMP] web_editor, website: add database ID to OLG calls
    This commit adds the database ID to the IAP calls made to generate text.
    It permits to prevent abuses of OpenAI calls.
    
    task-3740440
    
    closes odoo/odoo#154615
    
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -656,10 +656,12 @@ class Website(models.Model):
 
         if translated_ratio > 0.8:
             try:
+                database_id = request.env['ir.config_parameter'].sudo().get_param('database.uuid')
                 response = self._OLG_api_rpc('/api/olg/1/generate_placeholder', {
                     'placeholders': list(generated_content.keys()),
                     'lang': website.default_lang_id.name,
                     'industry': industry,
+                    'database_id': database_id,
                 })
                 name_replace_parser = re.compile(r"XXXX", re.MULTILINE)
                 for key in generated_content:
