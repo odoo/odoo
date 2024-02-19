ee7893ed0bf0c53f1017f8d1cc00f646cab58ce9
David (davg) <davg@odoo.com>
2024-02-19 05:28:30 +0100

    [IMP] point_of_sale: invoice link in the receipt
    Clarify the self-service invoicing options by grouping them under a single
    setting in accounting. Offer the choice between a QR code, a link, or both on
    receipts. Adjust the receipt printout based on the selected options.
    
    tasks id: 3709629

--- addons/point_of_sale/static/src/app/models/pos_order.js
+++ addons/point_of_sale/static/src/app/models/pos_order.js
@@ -107,9 +107,8 @@ export class PosOrder extends Base {
             pos_qr_code:
                 this.company.point_of_sale_use_ticket_qr_code &&
                 this.finalized &&
-                qrCodeSrc(`${baseUrl}/pos/ticket/validate?access_token=${this.access_token}`),
-            ticket_code:
-                this.company.point_of_sale_ticket_unique_code && this.finalized && this.ticketCode,
+                qrCodeSrc(`${baseUrl}/pos/ticket/`),
+            ticket_code: this.ticket_code,
             base_url: baseUrl,
             footer: this.config.receipt_footer,
             // FIXME: isn't there a better way to handle this date?
