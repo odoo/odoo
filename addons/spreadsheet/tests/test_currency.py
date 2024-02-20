313dd9a2d56d25efa56def60b9efd7bf51cf1962
Lucas Lefèvre (lul) <lul@odoo.com>
2024-02-20 16:03:38 +0000

    [PERF] spreadsheet: use web_search_read to load list currency
    With this commit, list data is loaded using `web_search_read`
    instead of `search_read`.
    
    The goal is to fetch the currency (symbol, decimal places, etc.) of monetary
    fields in a single request, instead of 2 RPCs.
    
    Pros:
    - less code
    - one evaluation saved
    - one network request saved
    - easier future refactoring (see below)
    
    Cons:
    - overhead of data transferred over network (from 4.5MB to 6.5MB, unzipped
      and from 711kB to 725kB gzipped to fetch a list of 20K crm leads).
    
    Before this commit, here is what it looked like:
    
    1. the list data is fetch (with the currency_field)
    2. the cells are evaluated with the new data
    3. we realize we want to format a currency amount. We already have the
       currency name but not the symbol, etc. So we fetch the currency data
    4. evaluate the cells again with the new currency format
    
    Now:
    1. fetch the list data with everything we need for the currency
    2. evaluate the cells
    
    This commit also serves another goal for a future refactoring: in the hope
    of avoiding throwing "loading errors", I'd like to have an easy way to know
    if a data source is fully loaded or not (the data and the format).
    With this commit, everything is centralized in the list data source with
    a single RPC. The goal is therefore achieved with this commit.
    
    closes odoo/odoo#154949
    
    Task: 3730232
    X-original-commit: 4e6738692103b3803fc826895ceba4bed5835228
    Related: odoo/enterprise#57185
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet/tests/test_currency.py
+++ addons/spreadsheet/tests/test_currency.py
@@ -28,38 +28,6 @@ class TestCurrencyRates(TransactionCase):
         cls.env.user.company_id = eur_company
         cls.usd_company_id = usd_company.id
 
-    def test_get_currencies_for_spreadsheet(self):
-        self.assertEqual(
-            self.env["res.currency"].get_currencies_for_spreadsheet(["MC1", "MC2"]),
-            [
-                {
-                    "code": "MC1",
-                    "symbol": ":D",
-                    "decimalPlaces": 3,
-                    "position": "after",
-                },
-                {
-                    "code": "MC2",
-                    "symbol": "§",
-                    "decimalPlaces": 2,
-                    "position": "after",
-                },
-            ],
-        )
-
-        self.assertEqual(
-            self.env["res.currency"].get_currencies_for_spreadsheet(["ProbablyNotACurrencyName?", "MC2"]),
-            [
-                None,
-                {
-                    "code": "MC2",
-                    "symbol": "§",
-                    "decimalPlaces": 2,
-                    "position": "after",
-                },
-            ],
-        )
-
     def test_get_company_currency_for_spreadsheet(self):
         self.assertEqual(
             self.env["res.currency"].get_company_currency_for_spreadsheet(),
