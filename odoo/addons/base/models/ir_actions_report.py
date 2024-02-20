44a5be4f72643fbe3c935470f1d21da5127c8e10
kdeb@odoo.com <kdeb@odoo.com>
2024-02-20 11:18:47 +0100

    [IMP] base: make wkhtmltopdf right/left margins customizable
    right/left margins were not customizable so wkhtmltopdf always defaults
    to the paperformat margin which results in data being lost on some
    reports.
    
    This PR improves wkhtmltopdf rendering by adding the option to set
    right and left margin.
    
    this PR is linked to this opw-3722018 fix:
    
    https://github.com/odoo/enterprise/pull/57026

--- odoo/addons/base/models/ir_actions_report.py
+++ odoo/addons/base/models/ir_actions_report.py
@@ -287,14 +287,20 @@ class IrActionsReport(models.Model):
             elif paperformat_id.header_spacing:
                 command_args.extend(['--header-spacing', str(paperformat_id.header_spacing)])
 
-            command_args.extend(['--margin-left', str(paperformat_id.margin_left)])
+            if specific_paperformat_args and specific_paperformat_args.get('data-report-margin-left'):
+                command_args.extend(['--margin-left', str(specific_paperformat_args['data-report-margin-left'])])
+            else:
+                command_args.extend(['--margin-left', str(paperformat_id.margin_left)])
 
             if specific_paperformat_args and specific_paperformat_args.get('data-report-margin-bottom'):
                 command_args.extend(['--margin-bottom', str(specific_paperformat_args['data-report-margin-bottom'])])
             else:
                 command_args.extend(['--margin-bottom', str(paperformat_id.margin_bottom)])
 
-            command_args.extend(['--margin-right', str(paperformat_id.margin_right)])
+            if specific_paperformat_args and specific_paperformat_args.get('data-report-margin-right'):
+                command_args.extend(['--margin-right', str(specific_paperformat_args['data-report-margin-right'])])
+            else:
+                command_args.extend(['--margin-right', str(paperformat_id.margin_right)])
             if not landscape and paperformat_id.orientation:
                 command_args.extend(['--orientation', str(paperformat_id.orientation)])
             if paperformat_id.header_line:
