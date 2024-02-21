2eb6611308d6bf7d843f00ffd7ae38ce4bde049e
Chong Wang (cwg) <cwg@odoo.com>
2024-02-21 12:13:05 +0100

    [FIX] core: fix translation typo fixing
    for model_terms translated fields, for sake of typo fixing feature
    if a term is removed after write its translations might be reused by another
    close term which has totally different meaning
    
    this fix removes the typo fixing if the matched closest term exists before write

--- odoo/fields.py
+++ odoo/fields.py
@@ -1823,6 +1823,8 @@ class _String(Field):
                     matches = get_close_matches(old_term_text, text2terms, 1, 0.9)
                     if matches:
                         closest_term = get_close_matches(old_term, text2terms[matches[0]], 1, 0)[0]
+                        if closest_term in translation_dictionary:
+                            continue
                         old_is_text = old_term == self.get_text_content(old_term)
                         closest_is_text = closest_term == self.get_text_content(closest_term)
                         if old_is_text or not closest_is_text:
