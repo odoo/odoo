d9c53430584a17ef0c9c13dd5822bbd01eef6392
Didier (did) <did@odoo.com>
2024-02-21 15:37:05 +0100

    [IMP] mail: don't require optional thread props
    Message props `thread` is optional. This PR adds some missing guards.
    
    closes odoo/odoo#154902
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/message_actions.js
+++ addons/mail/static/src/core/common/message_actions.js
@@ -55,7 +55,7 @@ messageActionsRegistry
     })
     .add("mark-as-unread", {
         condition: (component) =>
-            component.props.thread.model === "discuss.channel" &&
+            component.props.thread?.model === "discuss.channel" &&
             component.store.self.type === "partner",
         icon: "fa-eye-slash",
         title: _t("Mark as Unread"),
