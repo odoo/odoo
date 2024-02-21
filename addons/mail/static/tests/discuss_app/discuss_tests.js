2c008e39cb9d6643a0b257da4b22a749e2ee3f56
tsm-odoo <tsm@odoo.com>
2024-02-21 06:08:49 +0100

    [FIX] mail: fix non deterministic load message test
    Before this PR, the "failure on loading more messages should display
    error" test  was sometimes failing because it was not expecting the
    first messages to be display before loading more messages. This PR
    converts this test to use the `contains` helper which is more robust
    and waits for the messages to be in the DOM before loading more
    messages.
    
    fixes runbot-53127
    
    closes odoo/odoo#148120
    
    X-original-commit: 5b77cfdc28aa9cbcc8a8dc5d18a0c3b6207c02ce
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/mail/static/tests/discuss_app/discuss_tests.js
+++ addons/mail/static/tests/discuss_app/discuss_tests.js
@@ -1668,7 +1668,7 @@ QUnit.test(
 
 QUnit.test(
     "failure on loading more messages should display error and prompt retry button",
-    async (assert) => {
+    async () => {
         // first call needs to be successful as it is the initial loading of messages
         // second call comes from load more and needs to fail in order to show the error alert
         // any later call should work so that retry button and load more clicks would now work
@@ -1695,14 +1695,14 @@ QUnit.test(
             },
         });
         await openDiscuss(channelId);
+        await contains(".o-mail-Message", { count: 30 });
         messageFetchShouldFail = true;
-        await click("button:contains(Load More)");
-        assert.containsOnce(
-            $,
-            ".o-mail-Thread-error:contains(An error occurred while fetching messages.)"
-        );
-        assert.containsOnce($, "button:contains(Click here to retry)");
-        assert.containsNone($, "button:contains(Load More)");
+        await clickContains("button", { text: "Load More" });
+        await contains(".o-mail-Thread-error", {
+            text: "An error occurred while fetching messages.",
+        });
+        await contains("button", { text: "Click here to retry" });
+        await contains("button", { text: "Load More", count: 0 });
     }
 );
 
