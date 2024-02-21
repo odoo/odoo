b65db6941291206218638f53d23bbfb9968c21a0
Giovanni Francesco Capalbo <giovanni@therp.nl>
2024-02-21 15:01:40 +0100

    [ADD] backport base_geocode v10 google API support

--- addons/base_geolocalize/models/res_partner.py
+++ addons/base_geolocalize/models/res_partner.py
@@ -10,24 +10,41 @@ from openerp.tools.translate import _
 from openerp.exceptions import UserError
 
 
-def geo_find(addr):
+def geo_find(addr, apikey=False):
     if not addr:
         return None
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address='
+
+    if not apikey:
+        raise UserError(_('''API key for GeoCoding (Places) required.\n
+                          Save this key in System Parameters with key: google.api_key_geocode, value: <your api key>
+                          Visit https://developers.google.com/maps/documentation/geocoding/get-api-key for more information.
+                          '''))
+
+    url = "https://maps.googleapis.com/maps/api/geocode/json?key=%s&sensor=false&address=" % apikey
     url += urllib2.quote(addr.encode('utf8'))
 
     try:
         result = json.load(urllib2.urlopen(url))
-    except Exception, e:
-        raise UserError(_('Cannot contact geolocation servers. Please make sure that your internet connection is up and running (%s).') % e)
+    except Exception as e:
+        raise UserError(_('Cannot contact geolocation servers. Please make sure that your Internet connection is up and running (%s).') % e)
+
     if result['status'] != 'OK':
-        return None
+        if result.get('error_message'):
+            _logger.error(result['error_message'])
+            error_msg = _('Unable to geolocate, received the error:\n%s'
+                          '\n\nGoogle made this a paid feature.\n'
+                          'You should first enable billing on your Google account.\n'
+                          'Then, go to Developer Console, and enable the APIs:\n'
+                          'Geocoding, Maps Static, Maps Javascript.\n'
+                          % result['error_message'])
+            raise UserError(error_msg)
 
     try:
         geo = result['results'][0]['geometry']['location']
         return float(geo['lat']), float(geo['lng'])
     except (KeyError, ValueError):
         return None
+
 
 
 def geo_query_address(street=None, zip=None, city=None, state=None, country=None):
@@ -51,15 +68,17 @@ class res_partner(osv.osv):
     }
 
     def geo_localize(self, cr, uid, ids, context=None):
+        apikey = self.env['ir.config_parameter'].sudo().get_param('google.api_key_geocode')
         # Don't pass context to browse()! We need country names in english below
         for partner in self.browse(cr, uid, ids):
             if not partner:
                 continue
-            result = geo_find(geo_query_address(street=partner.street,
+            address = geo_query_address(street=partner.street,
                                                 zip=partner.zip,
                                                 city=partner.city,
                                                 state=partner.state_id.name,
-                                                country=partner.country_id.name))
+                                                country=partner.country_id.name)
+            result = geo_find(address, apikey)
             if result:
                 self.write(cr, uid, [partner.id], {
                     'partner_latitude': result[0],
