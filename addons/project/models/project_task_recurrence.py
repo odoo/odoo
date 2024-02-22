e56383dbbbe53fa76ec77a0650e16b5b6b906026
Abdelouahab (abla) <abla@odoo.com>
2024-02-22 16:27:47 +0000

    [FIX] project: fix closing recurring task
    to reproduce:
    =============
    - make a task recureent
    - change its state to a closing one
    -> Validation Error
    
    Problem:
    ========
    - when creating the next occurence, we insert the followers of the original
    task in the new one, but it happens that we create a `mail_followers` twice
    which violates an SQL constraint of `mail_followers`.
    https://github.com/odoo/odoo/blob/ea170be9089ee6c784664475ac4ec1218d23dccd/addons/mail/models/mail_thread.py#L253-L262
    
    the create method will create `mail_followers` for the task and as the condition
    that follows is truthy in this use-case `_insert_followers` will be called
    and create `mail_followers` again.
    
    - In previous versions we didn't face this issue because the creation of
    occurences was done by a cron, so the condition was never truthy thanks to
    `and self.env.user.active`.
    
    Solution:
    =========
    to make sure the conditon stays falsy, we set `mail_create_nosubscribe`
    in context to `True` when creating the next occurence.
    
    opw-3742737
    
    X-original-commit: 64ca453bc36740ce4afcd46c1afbbb31abffdf7c

--- addons/project/models/project_task_recurrence.py
+++ addons/project/models/project_task_recurrence.py
@@ -81,6 +81,8 @@ class ProjectTaskRecurrence(models.Model):
 
     def _create_next_occurrence(self, occurrence_from):
         self.ensure_one()
+        # Prevent double mail_followers creation
+        self = self.with_context(mail_create_nosubscribe=True)
         self.env['project.task'].sudo().create(
             self._create_next_occurrence_values(occurrence_from)
         )
