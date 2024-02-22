57db4bf597ee252a1fb03a214be4d6c3e98fd322
sofiagvaladze <sgv@odoo.com>
2024-02-22 12:15:27 +0100

    [FIX] hr_gamification: allow granting badges to user without employee
    When hr_gamification is installed, it's not possible to grant badges to
    users that don't have associated employees.
    
    Expected behavior: we should be able to grant badges to any users.
    
    This is because user_id was related field with employee_id.
    We remove that dependency.
    
    task - 3593382
    
    closes odoo/odoo#155538
    
    X-original-commit: 743e59d66687fd0fb73b1913f0f5520810e9d8b9
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_gamification/wizard/gamification_badge_user_wizard.py
+++ addons/hr_gamification/wizard/gamification_badge_user_wizard.py
@@ -8,18 +8,14 @@ from odoo.exceptions import UserError, AccessError
 class GamificationBadgeUserWizard(models.TransientModel):
     _inherit = 'gamification.badge.user.wizard'
 
-    employee_id = fields.Many2one('hr.employee', string='Employee', required=True)
-    user_id = fields.Many2one('res.users', string='User', related='employee_id.user_id',
-        store=False, readonly=True, compute_sudo=True)
+    employee_id = fields.Many2one('hr.employee', string='Employee', required=False)
+    user_id = fields.Many2one('res.users', string='User', compute='_compute_user_id',
+        store=True, readonly=False, compute_sudo=True)
 
     def action_grant_badge(self):
         """Wizard action for sending a badge to a chosen employee"""
-        if not self.user_id:
-            raise UserError(_('You can send badges only to employees linked to a user.'))
-
         if self.env.uid == self.user_id.id:
             raise UserError(_('You can not send a badge to yourself.'))
-
         values = {
             'user_id': self.user_id.id,
             'sender_id': self.env.uid,
@@ -29,3 +25,8 @@ class GamificationBadgeUserWizard(models.TransientModel):
         }
 
         return self.env['gamification.badge.user'].create(values)._send_badge()
+
+    @api.depends('employee_id')
+    def _compute_user_id(self):
+        for wizard in self:
+            wizard.user_id = wizard.employee_id.user_id
