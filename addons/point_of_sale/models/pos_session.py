4174bdb08fb7e38e1af59b3d4a68ab21b76d0e93
Harsh Modi <hamo@odoo.com>
2024-02-22 10:38:32 +0000

    [IMP] point_of_sale, sale, website_sale: change lable `VAT` -> `Tax` and fix pos `vat_label`
    point_of_sale:
    This commits fixes the issue where `vat_label` was not visibile
    even if a vat_label is available for a country
    
    point_of_sale, sale, website_sale:
    Replace label `VAT` with `Tax`
    
    task-3653060
    
    closes odoo/odoo#155755
    
    X-original-commit: ef78603bec402f9e8af970cc417a8bfa64becb26
    Signed-off-by: Quentin Lejeune (qle) <qle@odoo.com>
    Signed-off-by: Harsh Modi (hamo) <hamo@odoo.com>

--- addons/point_of_sale/models/pos_session.py
+++ addons/point_of_sale/models/pos_session.py
@@ -215,7 +215,7 @@ class PosSession(models.Model):
             },
             'res.country': {
                 'domain': [],
-                'fields': ['id', 'name', 'code'],
+                'fields': ['id', 'name', 'code', 'vat_label'],
             },
             'res.lang': {
                 'domain': [],
