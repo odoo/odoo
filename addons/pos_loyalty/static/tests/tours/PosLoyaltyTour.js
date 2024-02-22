215fb85ca53eed629c10f79d5cede689bb77d908
Louis (loti) <loti@odoo.com>
2024-02-22 17:19:15 +0100

    [FIX] *_loyalty: consider product domains for min amount to reach
    Product domains were not considered when computing the min amount to reach to get a discount. For
    example, if you created a discount with a minimum amount of 100 for product "X", the discount
    would have been applied, even if the amount for "X" was less than 100, as long as the total amount
    of the SO was at least 100.
    
    After this fix, only products that match the domain will be considered when computing the amount
    that should be compared to the discount's min amount.
    
    opw-3626345
    
    closes odoo/odoo#155085
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/pos_loyalty/static/tests/tours/PosLoyaltyTour.js
+++ addons/pos_loyalty/static/tests/tours/PosLoyaltyTour.js
@@ -406,3 +406,25 @@ registry.category("web_tour.tours").add('PosLoyaltyTour12', {
             PosLoyalty.hasRewardLine('Free Product', '-3.00'),
         ].flat(),
 });
+
+registry.category("web_tour.tours").add('PosLoyaltyMinAmountAndSpecificProductTour', {
+    test: true,
+    url: '/pos/web',
+    steps: () =>
+        [
+            ProductScreen.clickHomeCategory(),
+            ProductScreen.confirmOpeningPopup(),
+
+            ProductScreen.clickDisplayedProduct('Product A'),
+            ProductScreen.selectedOrderlineHas('Product A', '1.00', '20.00'),
+            PosLoyalty.orderTotalIs('20.00'),
+
+            ProductScreen.clickDisplayedProduct('Product B'),
+            ProductScreen.selectedOrderlineHas('Product B', '1.00', '30.00'),
+            PosLoyalty.orderTotalIs('50.00'),
+
+            ProductScreen.clickDisplayedProduct('Product A'),
+            ProductScreen.selectedOrderlineHas('Product A', '2.00', '40.00'),
+            PosLoyalty.orderTotalIs('66.00'),
+        ].flat(),
+});
