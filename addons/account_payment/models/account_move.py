2e7872e73a016cc82cd6fe0fbe84a06870972591
Demesmaeker <edm@odoo.com>
2024-02-22 13:54:03 +0000

    [FIX] account_payment: allows to pay second invoice
    Steps to reproduce the issue
    - order something from the website and pay
    - create an invoice for the SO (invoice is paid) and confirm it
    - add an extra item to initial SO
    - create a second invoice
    - check the invoice on the portal
    ==> second invoice is automatically marked as paid because of the
    transactions on the SO
    
    The transactions that were used on the SO are filled in on the new
    invoice in the transaction_ids field. In the view in the portal the
    method 'get_portal_last_transaction' is called which finds the
    transaction for the older invoice and assumes its paid.
    
    We will now rely on the payment status of the invoice instead of the
    state of the last transaction.
    
    opw-3594556
    
    closes odoo/odoo#156403
    
    X-original-commit: 8aaa25e2ebfbf19c2c8832dbc43eba019f9afa9c
    Signed-off-by: Morgane Demesmaeker <edm@odoo.com>

--- addons/account_payment/models/account_move.py
+++ addons/account_payment/models/account_move.py
@@ -44,22 +44,20 @@ class AccountMove(models.Model):
         self.ensure_one()
         transactions = self.transaction_ids.filtered(lambda tx: tx.state in ('pending', 'authorized', 'done'))
         pending_transactions = transactions.filtered(
-            lambda tx: tx.state == 'pending' and tx.provider_code not in ('none', 'custom'))
+            lambda tx: tx.state in {'pending', 'authorized'}
+                       and tx.provider_code not in {'none', 'custom'})
         enabled_feature = str2bool(
             self.env['ir.config_parameter'].sudo().get_param(
                 'account_payment.enable_portal_payment'
             )
         )
         return enabled_feature and bool(
-            (
-                self.amount_residual
-                or not transactions
-            )
+            (self.amount_residual or not transactions)
             and self.state == 'posted'
             and self.payment_state in ('not_paid', 'partial')
             and self.amount_total
             and self.move_type == 'out_invoice'
-            and (pending_transactions or not transactions or self.amount_paid < self.amount_total)
+            and not pending_transactions
         )
 
     def get_portal_last_transaction(self):
