2948bd6090d6d938b9c36b99bea0c09f15b9d181
Achraf <abz@odoo.com>
2024-02-22 10:14:02 +0000

    [FIX] web: Allow users to make a `many2many_binary` required
    Example of steps with studio:
    	1 - Install `web_studio` and `hr_holidays`
    	2 - Open `hr_holidays`
    	3 - Go to Approvals/Time off
    	4 - Create a new time off
    	5 - Set any user
    	6 - Set a time off type with a support attachment (example sick)
    	7 - Upload any file
    	8 - Save
    	9 - Open studio in this form view
    	10 - Make `supported_attachments_ids` required via studio
    	11 - Try the flow again steps 4 to 8 without uploading a file
    	12 - It should not let you save since `support_attachments_ids` is required
    	13 - But it's not working
    
    Logic is done here
    
    https://github.com/odoo/odoo/blob/1b09a6314dbce9363f4785c670033f41846359ad/addons/web/static/src/legacy/js/views/basic/basic_renderer.js#L355-L367
    
    Before saving, `widget.isSet` is called to check
    if it's not empty in case it's required
    
    This commit fix this issue by adding a proper `isSet` to `FieldMany2ManyBinaryMultiFiles`/"many2many_binary"
    
    X-original-commit: dbb3dc99afaa81bcb5f131ffde33caa22a022a5e

--- addons/web/static/src/views/fields/many2many_binary/many2many_binary_field.js
+++ addons/web/static/src/views/fields/many2many_binary/many2many_binary_field.js
@@ -77,6 +77,7 @@ export const many2ManyBinaryField = {
     ],
     supportedTypes: ["many2many"],
     isEmpty: () => false,
+    isSet: (value) => value.count > 0,
     relatedFields: [
         { name: "name", type: "char" },
         { name: "mimetype", type: "char" },
