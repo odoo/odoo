f6f01a5bceec76db47abcdba39623526874b4424
amdi-odoo <amdi@odoo.com>
2024-02-22 11:41:23 +0100

    [FIX] crm: set revenue default
    Purpose
    =======
    Fix the crm leads which were in the wrong order when
    ordering on the expected revenue or the recurring one.
    
    Specifications
    ==============
    When manually creating leads, the value for the expected and
    the recurring revenue is set to 0. However, when generating some
    leads using the Lead Generation Tool, the leads created don't
    have a value set for the expected or recurring revenue.
    As a consequence, when ordering by those fields in the
    list view, the leads are wrongly ordered, all the generated
    leads are set to the bottom.
    
    Fixing that by specifying a default value of 0 for the
    expected and recurring revenue field.
    
    Task-3753924
    
    closes odoo/odoo#155009
    
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- addons/crm/models/crm_lead.py
+++ addons/crm/models/crm_lead.py
@@ -149,9 +149,9 @@ class Lead(models.Model):
         help="Classify and analyze your lead/opportunity categories like: Training, Service")
     color = fields.Integer('Color Index', default=0)
     # Revenues
-    expected_revenue = fields.Monetary('Expected Revenue', currency_field='company_currency', tracking=True)
+    expected_revenue = fields.Monetary('Expected Revenue', currency_field='company_currency', tracking=True, default=0.0)
     prorated_revenue = fields.Monetary('Prorated Revenue', currency_field='company_currency', store=True, compute="_compute_prorated_revenue")
-    recurring_revenue = fields.Monetary('Recurring Revenues', currency_field='company_currency', tracking=True)
+    recurring_revenue = fields.Monetary('Recurring Revenues', currency_field='company_currency', tracking=True, default=0.0)
     recurring_plan = fields.Many2one('crm.recurring.plan', string="Recurring Plan")
     recurring_revenue_monthly = fields.Monetary('Expected MRR', currency_field='company_currency', store=True,
                                                 compute="_compute_recurring_revenue_monthly")
