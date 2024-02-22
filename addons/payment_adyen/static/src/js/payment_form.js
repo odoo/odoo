f6bcc6af22b08e42d440f29affb25892b685fd0c
Antoine Vandevenne (anv) <anv@odoo.com>
2024-02-22 09:58:20 +0000

    [FIX] payment_adyen: catch and display incorrect payment details errors
    Partial revert of ceb8b785. That commit replaced a generic error message
    with the one received from Adyen and stored in
    `error.message.data.message`, but `error.message` was not always
    defined.
    
    opw-3736725
    
    X-original-commit: 0b9c1024d1d1abf8d2e43f091afde357d1a5cc24

--- addons/payment_adyen/static/src/js/payment_form.js
+++ addons/payment_adyen/static/src/js/payment_form.js
@@ -56,7 +56,7 @@
             if (!this.$('div[name="o_payment_error"]')) { // Don't replace a specific server error.
                 this._displayError(
                     _t("Incorrect Payment Details"),
-                    error.message.data.message
+                    _t("Please verify your payment details."),
                 );
             }
             else {
