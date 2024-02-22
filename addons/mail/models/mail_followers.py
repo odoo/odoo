16102407f3e27cca7431dccf1844966208cd566d
Renaud Thiry <reth@odoo.com>
2024-02-22 14:33:23 +0000

    [FIX] mail: let auto_subscribe use up-to-date values
    `_message_auto_subscribe` relies on `_get_subscription_data` to know who
    is subscribed to what. This method uses a raw SQL query but doesn't flush
    the relevant records beforehand.
    
    This is not an issue in most normal cases, but if followers are edited manually
    for example in order to merge existing records. It may cause issues when
    assigning a user to the record after merging, as the this will attempt to add
    them to the list of followers automatically, when they already were on it.
    
    task-3713472
    original issue in opw-3634410
    
    closes odoo/odoo#155139
    
    X-original-commit: d1b0e46f7d3034c400ba3c60f343c21c68aa011e
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Renaud Thiry (reth) <reth@odoo.com>

--- addons/mail/models/mail_followers.py
+++ addons/mail/models/mail_followers.py
@@ -349,6 +349,8 @@ class Followers(models.Model):
           share status of partner (returned only if include_pshare is True)
           active flag status of partner (returned only if include_active is True)
         """
+        self.env['mail.followers'].flush_model(['partner_id', 'res_id', 'res_model', 'subtype_ids'])
+        self.env['res.partner'].flush_model(['active', 'partner_share'])
         # base query: fetch followers of given documents
         where_clause = ' OR '.join(['fol.res_model = %s AND fol.res_id IN %s'] * len(doc_data))
         where_params = list(itertools.chain.from_iterable((rm, tuple(rids)) for rm, rids in doc_data))
