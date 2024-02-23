af33e126fe701f7612b40ce1748db8480961b217
Rémy Voet (ryv) <ryv@odoo.com>
2024-02-23 15:05:44 +0100

    [FIX] data_recycle: fix test
    Since https://github.com/odoo/odoo/pull/112769, there isn't date
    field on res.partner, but there are still some tests using it (TestDataRecycle).
    Fix the test by using a other model.

--- addons/data_recycle/tests/test_data_recycle.py
+++ addons/data_recycle/tests/test_data_recycle.py
@@ -15,24 +15,24 @@ class TestDataRecycle(TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
 
-        cls.partner_model = cls.env.ref('base.model_res_partner')
+        cls.server_model = cls.env['ir.model']._get('fetchmail.server')
 
         cls.recycle_model = cls.env['data_recycle.model'].create({
-            'name': 'Recyle Test Partner',
-            'res_model_id': cls.partner_model.id,
-            'time_field_id': cls.env['ir.model.fields'].search([('name', '=', 'date'), ('model_id', '=', cls.partner_model.id)], limit=1).id,
+            'name': 'Recycle Test Server',
+            'res_model_id': cls.server_model.id,
+            'time_field_id': cls.env['ir.model.fields'].search([('name', '=', 'date'), ('model_id', '=', cls.server_model.id)], limit=1).id,
             'time_field_delta': 1,
             'time_field_delta_unit': 'years',
             'recycle_action': 'archive',
         })
 
-        cls.old_partners = cls.env['res.partner'].create([{
-            'name': 'Old Partner %s' % (i),
+        cls.old_servers = cls.env['fetchmail.server'].create([{
+            'name': 'Old Server %s' % (i),
             'date': Date.today() - relativedelta(years=2),
         } for i in range(5)])
 
-        cls.new_partners = cls.env['res.partner'].create([{
-            'name': 'New Partner %s' % (i),
+        cls.new_servers = cls.env['fetchmail.server'].create([{
+            'name': 'New Server %s' % (i),
             'date': Date.today(),
         } for i in range(5)])
 
@@ -41,10 +41,10 @@ class TestDataRecycle(TransactionCase):
         self.recycle_model._recycle_records()
 
         self.assertEqual(len(self.recycle_model.recycle_record_ids), 5)
-        self.assertEqual(set(self.recycle_model.recycle_record_ids.mapped('res_id')), set(self.old_partners.ids))
+        self.assertEqual(set(self.recycle_model.recycle_record_ids.mapped('res_id')), set(self.old_servers.ids))
 
         # Test record deletion outside of the recycle scope
-        self.old_partners[0].unlink()
+        self.old_servers[0].unlink()
         self.assertEqual(self.recycle_model.recycle_record_ids[0].name, '**Record Deleted**')
 
     def test_recycle_domain(self):
@@ -52,7 +52,7 @@ class TestDataRecycle(TransactionCase):
         self.recycle_model._recycle_records()
 
         self.assertEqual(len(self.recycle_model.recycle_record_ids), 4)
-        self.assertTrue(self.old_partners[0].id not in self.recycle_model.recycle_record_ids.mapped('res_id'))
+        self.assertTrue(self.old_servers[0].id not in self.recycle_model.recycle_record_ids.mapped('res_id'))
 
     def test_recycle_notification(self):
         self.recycle_model.notify_user_ids = [(4, self.env.ref('base.user_admin').id)]
@@ -65,17 +65,17 @@ class TestDataRecycle(TransactionCase):
         self.recycle_model._recycle_records()
         self.recycle_model.recycle_record_ids.action_validate()
         self.assertFalse(self.recycle_model.recycle_record_ids.exists())
-        self.assertTrue(all(not p.active for p in self.old_partners))
+        self.assertTrue(all(not p.active for p in self.old_servers))
 
     def test_recycle_unlink(self):
         self.recycle_model.recycle_action = 'unlink'
         self.recycle_model._recycle_records()
         self.recycle_model.recycle_record_ids.action_validate()
         self.assertFalse(self.recycle_model.recycle_record_ids.exists())
-        self.assertFalse(self.old_partners.exists())
+        self.assertFalse(self.old_servers.exists())
 
     def test_include_archived(self):
-        self.old_partners[0].active = False
+        self.old_servers[0].active = False
         self.recycle_model._recycle_records()
         self.assertEqual(len(self.recycle_model.recycle_record_ids), 4)
         self.recycle_model.include_archived = True
