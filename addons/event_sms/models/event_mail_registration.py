0dc596d057a062942477822fbff3396bd06bb763
Thibault Delavallée <tde@odoo.com>
2024-02-23 15:36:56 +0000

    [PERF] event(_*): send registration mails / sms in batch
    Improve event schedulers to send communications in batch as much as
    possible to benefit from potential optimizations.
    
    Communications are now grouped by scheduler, as each one uses a single
    template, allowing to call their matching send method on a batch of
    records.
    
    As the same action is done by both event-based and attendee-based
    schedulers, common code is extracted into a small tool to ease future
    change and improvements. Moreover it allows to have consistent result
    when sending communications, whatever the interval type.
    
    Task-3084943: Event: Improve communication scheduler scalability
    
    X-original-commit: 1e1cd14093f62d142228b665ca93547b50d75ceb
    Part-of: odoo/odoo#181818
    Related: odoo/enterprise#70885
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Co-authored-by: Stéphane Debauche <std@odoo.com>

--- addons/event_sms/models/event_mail_registration.py
+++ addons/event_sms/models/event_mail_registration.py
@@ -8,12 +8,8 @@ class EventMailRegistration(models.Model):
         todo = self.filtered(
             lambda r: r.scheduler_id.notification_type == "sms"
         )
-        for reg_mail in todo:
-            reg_mail.registration_id._message_sms_schedule_mass(
-                template=reg_mail.scheduler_id.template_ref,
-                mass_keep_log=True
-            )
-        todo.write({'mail_sent': True})
+        for scheduler, reg_mails in todo.grouped('scheduler_id').items():
+            scheduler._send_sms(reg_mails.registration_id)
+        todo.mail_sent = True
 
         return super(EventMailRegistration, self - todo)._execute_on_registrations()
-
