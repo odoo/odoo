d645815794bd6a7c26fadd61ee57289b73cb8981
duongnguyen-viindoo <daiduongnguyen2709@gmail.com>
2024-02-23 10:06:07 +0700

    [FIX] mail: user_id field of model might not have type many2one
    -Step to reproduce: any custom model that inherit from mail.thread then
    define a field user_id but with type is char and boom error happen at
    method '_message_get_suggested_recipients' because it always expect
    'user_id' to be a many2one field
    
    closes odoo/odoo#155149
    
    -solution: need to check the type of 'user_id' also
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -1653,7 +1653,8 @@ class MailThread(models.AbstractModel):
         """ Returns suggested recipients for ids. Those are a list of
         tuple (partner_id, partner_name, reason), to be managed by Chatter. """
         result = dict((res_id, []) for res_id in self.ids)
-        if 'user_id' in self._fields:
+        user_field = self._fields.get('user_id')
+        if user_field and user_field.type == 'many2one' and user_field.comodel_name == 'res.users':
             for obj in self.sudo():  # SUPERUSER because of a read on res.users that would crash otherwise
                 if not obj.user_id or not obj.user_id.partner_id:
                     continue
