2f4c62941b3088dfdd13c962a2fcbe7c503ddbba
Antoine Boonen <aboo@odoo.com>
2024-02-23 12:52:06 +0000

    [FIX] account_edi_*: Fix EDI addresses
    * = facturx, ubl, ubl_bis3, ubl_cii
    
    Problem
    ---------
    Currently, the EDI use the commercial partner to craft the XML document.
    However, this causes issues when users add, for example, an invoice
    address to a partner. Indeed, the address of the main partner will be
    used and not the invoice address. This caused issue; see the relevant
    OPW-3624205.
    
    Objective
    ---------
    Make sure that the correct address is used when generating the XML.
    
    Solution
    ---------
    Make sure the commercial partner // partner value is used at the correct
    spot.
    - `partner` should be used for addresses
    - `commercial_partner` for everything else.
    
    OPW-3624205
    task-3636315
    
    closes odoo/odoo#158341
    
    X-original-commit: 95d3a5ac16c341acc4b680f19e404a9f2334b582
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>
    Signed-off-by: Antoine Boonen (aboo) <aboo@odoo.com>

--- addons/account_edi_ubl_cii/models/account_edi_xml_ubl_bis3.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_ubl_bis3.py
@@ -113,6 +113,7 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
         # EXTENDS account.edi.xml.ubl_21
         vals = super()._get_partner_party_vals(partner, role)
 
+        partner = partner.commercial_partner_id
         vals.update({
             'endpoint_id': partner.peppol_endpoint,
             'endpoint_id_attrs': {'schemeID': partner.peppol_eas},
@@ -133,7 +134,7 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
     def _get_delivery_vals_list(self, invoice):
         # EXTENDS account.edi.xml.ubl_21
         supplier = invoice.company_id.partner_id.commercial_partner_id
-        customer = invoice.commercial_partner_id
+        customer = invoice.partner_id
 
         economic_area = self.env.ref('base.europe').country_ids.mapped('code') + ['NO']
         intracom_delivery = (customer.country_id.code in economic_area
