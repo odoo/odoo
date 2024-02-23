c9c7bc7f04cf90e8eea70a8a01dd7f4dcc053b2a
David Monnom (moda) <moda@odoo.com>
2024-02-23 15:26:03 +0000

    [IMP] pos_restaurant: improve preparation display functionality
    Behavior before the changes:
    - When a note was modified, it cancelled the old orderline and recreated
    it with the new note.
    - Products were not sorted by category in the order preparation display.
    - Clicking on the header of an order sent it directly to the next stage,
    regardless of whether lines were crossed out or not.
    - There were brackets around the order number.
    
    Behavior after changes:
    - When a note is modified, it is modified directly on the existing
    orderline.
    - Products are now sorted by category in the order.
    - Clicking on an order header sends only the crossed-out lines to the
    next stage. If no line is crossed out, the entire order is sent.
    - There are no longer any brackets around the order name.
    
    taskId: 3764317
    enterprise PR: https://github.com/odoo/enterprise/pull/57385
    
    X-original-commit: c55ac85c0a17eaa1ba9fe3608794850e21b5d2ad

--- addons/point_of_sale/static/src/app/screens/product_screen/control_buttons/customer_note_button/customer_note_button.js
+++ addons/point_of_sale/static/src/app/screens/product_screen/control_buttons/customer_note_button/customer_note_button.js
@@ -5,6 +5,7 @@ import { Component } from "@odoo/owl";
 import { usePos } from "@point_of_sale/app/store/pos_hook";
 import { TextInputPopup } from "@point_of_sale/app/utils/input_popups/text_input_popup";
 import { useService } from "@web/core/utils/hooks";
+import { makeAwaitable } from "@point_of_sale/app/store/make_awaitable_dialog";
 
 export class OrderlineNoteButton extends Component {
     static template = "point_of_sale.OrderlineNoteButton";
@@ -38,14 +39,16 @@ export class OrderlineNoteButton extends Component {
         } else {
             buttons = [];
         }
-        this.dialog.add(TextInputPopup, {
+
+        const oldNote = selectedOrderline.getNote();
+        const payload = await makeAwaitable(this.dialog, TextInputPopup, {
             title: _t("Add %s", this.props.label),
             buttons,
             rows: 4,
             startingValue: this.props.getter(selectedOrderline),
-            getPayload: (note) => {
-                this.props.setter(selectedOrderline, note);
-            },
         });
+
+        this.props.setter(selectedOrderline, payload);
+        return { confirmed: typeof payload === "string", inputNote: payload, oldNote };
     }
 }
