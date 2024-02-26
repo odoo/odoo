45ab62c4afa744de1af87c937b7ca4fba283640f
late-odoo <late@odoo.com>
2024-02-26 13:00:14 +0000

    [FIX] hr,mail: make avatar card popover visible properly for mobile view
    PURPOSE
    -The card is simply too wide for the display in mobile view.
    
    SPECIFICATIONS
    - made the avatar card responsive
    - enabled the avatarcard popover for m2x avatar user widget
    - close avatar card when clicking on 'send message'
    
    Task-3713375
    
    X-original-commit: f46900a1996ed8900713a57122d3c2cfe8848317

--- addons/mail/static/src/discuss/web/avatar_card/avatar_card_popover.js
+++ addons/mail/static/src/discuss/web/avatar_card/avatar_card_popover.js
@@ -30,7 +30,12 @@ export class AvatarCardPopover extends Component {
         return this.user.phone;
     }
 
+    get userId() {
+        return this.user.id;
+    }
+
     onSendClick() {
-        this.openChat(this.user.id);
+        this.openChat(this.userId);
+        this.props.close();
     }
 }
