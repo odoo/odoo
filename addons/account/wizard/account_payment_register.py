d25690685dae2046048f10daa83e38a56e4a1345
Laurent Smet <las@odoo.com>
2024-02-26 09:25:53 +0100

    [IMP] account: Payment register wizard - writeoff to exchange diff
    When a register payment is made with a writeoff in foreign currency
    and when the user set manually an exchange difference account,
    don't create a write-off. Instead, 2 cases to consider:
    - When the payment is expressed in a foreign currency different than
    the invoice's one, play with the rate between amount_currency and
    balance and let odoo making an exchange difference accordingly.
    That way, the payment has no write-off and the invoice is fully paid.
    - When the payment is expressed in company currency but the invoice
    has a foreign currency, force the exchange difference to use a rate
    that will fully paid the invoice without creating any write-off line
    on the payment.
    
    task_id: 3717436
    
    closes odoo/odoo#161192
    
    X-original-commit: bc0e9b380490523b0f05ae12acc6ac5007317c7c
    Related: odoo/enterprise#60365
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account/wizard/account_payment_register.py
+++ addons/account/wizard/account_payment_register.py
@@ -125,6 +125,9 @@ class AccountPaymentRegister(models.TransientModel):
     )
     writeoff_label = fields.Char(string='Journal Item Label', default='Write-Off',
         help='Change label of the counterpart that will hold the payment difference')
+    writeoff_is_exchange_account = fields.Boolean(
+        compute='_compute_writeoff_is_exchange_account',
+    )
 
     # == Display purpose fields ==
     show_partner_bank_account = fields.Boolean(
@@ -364,7 +367,6 @@ class AccountPaymentRegister(models.TransientModel):
                 'untrusted_bank_ids': untrusted_accounts or False,
             })
 
-
     @api.depends('line_ids')
     def _compute_from_lines(self):
         ''' Load initial values from the account.moves passed through the context. '''
@@ -518,6 +520,7 @@ class AccountPaymentRegister(models.TransientModel):
         :return:                An amount in the currency of the wizard.
         """
         self.ensure_one()
+
         comp_curr = self.company_id.currency_id
         if self.source_currency_id == self.currency_id:
             # Same currency (manage the early payment discount).
@@ -590,6 +593,19 @@ class AccountPaymentRegister(models.TransientModel):
                 wizard.payment_difference = total_amount_residual_in_wizard_currency - wizard.amount
             else:
                 wizard.payment_difference = 0.0
+
+    @api.depends('can_edit_wizard', 'writeoff_account_id')
+    def _compute_writeoff_is_exchange_account(self):
+        for wizard in self:
+            wizard.writeoff_is_exchange_account = all((
+                wizard.can_edit_wizard,
+                wizard.currency_id != wizard.source_currency_id,
+                wizard.writeoff_account_id,
+                wizard.writeoff_account_id in (
+                    wizard.company_id.expense_currency_exchange_account_id,
+                    wizard.company_id.income_currency_exchange_account_id,
+                ),
+            ))
 
     @api.depends('early_payment_discount_mode')
     def _compute_payment_difference_handling(self):
@@ -726,21 +742,30 @@ class AccountPaymentRegister(models.TransientModel):
                     payment_vals['write_off_line_vals'] += aml_values_list
 
             elif not self.currency_id.is_zero(self.payment_difference):
-                if self.payment_type == 'inbound':
-                    # Receive money.
-                    write_off_amount_currency = self.payment_difference
-                else: # if self.payment_type == 'outbound':
-                    # Send money.
-                    write_off_amount_currency = -self.payment_difference
 
-                payment_vals['write_off_line_vals'].append({
-                    'name': self.writeoff_label,
-                    'account_id': self.writeoff_account_id.id,
-                    'partner_id': self.partner_id.id,
-                    'currency_id': self.currency_id.id,
-                    'amount_currency': write_off_amount_currency,
-                    'balance': self.currency_id._convert(write_off_amount_currency, self.company_id.currency_id, self.company_id, self.payment_date),
-                })
+                if self.writeoff_is_exchange_account:
+                    # Force the rate when computing the 'balance' only when the payment has a foreign currency.
+                    # If not, the rate is forced during the reconciliation to put the difference directly on the
+                    # exchange difference.
+                    if self.currency_id != self.company_currency_id:
+                        payment_vals['force_balance'] = sum(batch_result['lines'].mapped('amount_residual'))
+                else:
+                    if self.payment_type == 'inbound':
+                        # Receive money.
+                        write_off_amount_currency = self.payment_difference
+                    else:  # if self.payment_type == 'outbound':
+                        # Send money.
+                        write_off_amount_currency = -self.payment_difference
+
+                    payment_vals['write_off_line_vals'].append({
+                        'name': self.writeoff_label,
+                        'account_id': self.writeoff_account_id.id,
+                        'partner_id': self.partner_id.id,
+                        'currency_id': self.currency_id.id,
+                        'amount_currency': write_off_amount_currency,
+                        'balance': self.currency_id._convert(write_off_amount_currency, self.company_id.currency_id, self.company_id, self.payment_date),
+                    })
+
         return payment_vals
 
     def _create_payment_vals_from_batch(self, batch_result):
@@ -889,9 +914,11 @@ class AccountPaymentRegister(models.TransientModel):
         for vals in to_process:
             payment_lines = vals['payment'].line_ids.filtered_domain(domain)
             lines = vals['to_reconcile']
+            extra_context = {'forced_rate_from_register_payment': vals['rate']} if 'rate' in vals else {}
 
             for account in payment_lines.account_id:
                 (payment_lines + lines)\
+                    .with_context(**extra_context)\
                     .filtered_domain([('account_id', '=', account.id), ('reconciled', '=', False)])\
                     .reconcile()
 
@@ -915,11 +942,19 @@ class AccountPaymentRegister(models.TransientModel):
 
         if edit_mode:
             payment_vals = self._create_payment_vals_from_wizard(first_batch_result)
-            to_process.append({
+            to_process_values = {
                 'create_vals': payment_vals,
                 'to_reconcile': first_batch_result['lines'],
                 'batch': first_batch_result,
-            })
+            }
+
+            # Force the rate during the reconciliation to put the difference directly on the
+            # exchange difference.
+            if self.writeoff_is_exchange_account and self.currency_id == self.company_currency_id:
+                total_batch_residual = sum(first_batch_result['lines'].mapped('amount_residual_currency'))
+                to_process_values['rate'] = abs(total_batch_residual / self.amount) if self.amount else 0.0
+
+            to_process.append(to_process_values)
         else:
             # Don't group payments: Create one batch per move.
             if not self.group_payment:
