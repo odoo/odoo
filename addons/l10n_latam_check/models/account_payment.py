0b4b95927bc53f298f9cb07d8aa5c6c0fc2c2244
Laurent Smet <las@odoo.com>
2024-02-26 09:25:53 +0100

    [IMP] account: Payment register wizard - writeoff to exchange diff
    When a register payment is made with a writeoff in foreign currency
    and when the user set manually an exchange difference account,
    don't create a write-off. Instead, 2 cases to consider:
    - When the payment is expressed in a foreign currency different than
    the invoice's one, play with the rate between amount_currency and
    balance and let odoo making an exchange difference accordingly.
    That way, the payment has no write-off and the invoice is fully paid.
    - When the payment is expressed in company currency but the invoice
    has a foreign currency, force the exchange difference to use a rate
    that will fully paid the invoice without creating any write-off line
    on the payment.
    
    task_id: 3717436
    
    closes odoo/odoo#161160
    
    X-original-commit: bc0e9b380490523b0f05ae12acc6ac5007317c7c
    Related: odoo/enterprise#60349
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/l10n_latam_check/models/account_payment.py
+++ addons/l10n_latam_check/models/account_payment.py
@@ -275,9 +275,9 @@ class AccountPayment(models.Model):
         res = super()._get_trigger_fields_to_synchronize()
         return res + ('l10n_latam_check_number',)
 
-    def _prepare_move_line_default_vals(self, write_off_line_vals=None):
+    def _prepare_move_line_default_vals(self, write_off_line_vals=None, force_balance=None):
         """ Add check name and operation on liquidity line """
-        res = super()._prepare_move_line_default_vals(write_off_line_vals=write_off_line_vals)
+        res = super()._prepare_move_line_default_vals(write_off_line_vals=write_off_line_vals, force_balance=force_balance)
         check = self if (self.payment_method_line_id.code == 'new_third_party_checks' or (self.payment_method_line_id.code == 'check_printing' and self.l10n_latam_manual_checks)) \
             else self.l10n_latam_check_id
         if check:
