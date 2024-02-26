a0bd852a3a445ab73117f013961def5a34b7cf97
Mélanie <mepe@odoo.com>
2024-02-26 09:17:36 +0100

    [FIX] web : domain in search view
    STEP TO REPRODUCE :
    ===================
    
        - Set this domain in search view in hr_employee_views for manager field : domain=[('company_id', 'in', allowed_company_ids)]
        - Select only one company
        - Write "marc demo" in employee search bar
    
    You will see two Marc Demo for manager line.
    
    Even if the domain has been restricted in the xml it is not taken into account when back function was called (cf name_search argument in network page).
    This issue occurs because allowed_company_id is undefined for the front-end.
    
    X-original-commit: 2be1b9e46b36740562c93a3430cbfee3ad049343

--- addons/web/static/src/search/search_bar/search_bar.js
+++ addons/web/static/src/search/search_bar/search_bar.js
@@ -283,10 +283,11 @@ export class SearchBar extends Component {
      */
     async computeSubItems(searchItem, query) {
         const field = this.fields[searchItem.fieldName];
+        const context = { ...this.env.searchModel.domainEvalContext, ...field.context };
         let domain = [];
         if (searchItem.domain) {
             try {
-                domain = new Domain(searchItem.domain).toList();
+                domain = new Domain(searchItem.domain).toList(context);
             } catch {
                 // Pass
             }
@@ -298,7 +299,7 @@ export class SearchBar extends Component {
 
         const options = await this.orm.call(relation, "name_search", [], {
             args: domain,
-            context: { ...this.env.searchModel.globalContext, ...field.context },
+            context,
             limit: 8,
             name: query.trim(),
         });
