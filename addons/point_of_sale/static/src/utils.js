3c204a7acbea710e95f300e55de91ef8c748c6e5
Pedram (pebr) <pebr@odoo.com>
2024-02-26 09:39:12 +0000

    [FIX] point_of_sale: ensure QR code loads in auto-printed receipts
    Prior to this commit, enabling auto receipt printing resulted in the QR
    code not being printed on the receipt. This was due to the QR image
    being fetched post printing. This commit addresses the issue by first
    loading the QR code image, then sending it to the printer, ensuring its
    presence on the printed receipt.
    
    opw-3763169
    
    closes odoo/odoo#156059
    
    X-original-commit: ce1fef9113f4ab0e52061fb73261ae8ca5b71976
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/utils.js
+++ addons/point_of_sale/static/src/utils.js
@@ -134,3 +134,33 @@ export function fuzzyLookup(pattern, list, fn) {
     results.sort((a, b) => b.score - a.score);
     return results.map((r) => r.elem);
 }
+
+/**
+ * Loading image is converted to a Promise to allow await when
+ * loading an image. It resolves to the loaded image if successful,
+ * else, resolves to false.
+ *
+ * [Source](https://stackoverflow.com/questions/45788934/how-to-turn-this-callback-into-a-promise-using-async-await)
+ */
+export function loadImage(url, options = {}) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.addEventListener("load", () => resolve(img));
+        img.addEventListener("error", () => {
+            if (options.onError) {
+                options.onError();
+            }
+            reject(new Error(`Failed to load image at ${url}`));
+        });
+        img.src = url;
+    });
+}
+
+/**
+ * Load all images in the given element.
+ * @param {HTMLElement} el
+ */
+export function loadAllImages(el) {
+    const images = el.querySelectorAll("img");
+    return Promise.all(Array.from(images).map(img => loadImage(img.src)));
+}
