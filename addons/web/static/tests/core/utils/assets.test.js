1ca38ba2c6ec91aac248ca48faf8e117660d02a2
Julien Mougenot <jum@odoo.com>
2024-02-27 15:10:04 +0100

    [FIX] web: assets test: fix assertions
    Before this commit, tests written for assets loaders were based on
    incorrect assertions (`toContain` with an empty list of nodes).
    
    This commit adds stronger assertions and cleans up the added nodes since
    they're appended to the head of the document.
    
    closes odoo/odoo#155073
    
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>

--- addons/web/static/tests/core/utils/assets.test.js
+++ addons/web/static/tests/core/utils/assets.test.js
@@ -1,26 +1,55 @@
-import { describe, expect, test } from "@odoo/hoot";
+import { after, describe, expect, test } from "@odoo/hoot";
+import { observe } from "@odoo/hoot-dom";
 
-import { assets, loadJS, loadCSS } from "@web/core/assets";
+import { assets, loadCSS, loadJS } from "@web/core/assets";
 
 describe.current.tags("headless");
 
 test("loadJS: load invalid JS lib", async () => {
+    expect.assertions(4);
+
+    after(
+        observe(document.head, (mutations) => {
+            for (const mutation of mutations) {
+                for (const script of mutation.addedNodes) {
+                    expect(script).toBeInstanceOf(HTMLScriptElement);
+                    expect(script).toHaveAttribute("type", "text/javascript");
+                    expect(script).toHaveAttribute("src", "/some/invalid/file.js");
+
+                    after(() => script.remove());
+                }
+            }
+        })
+    );
+
     await expect(loadJS("/some/invalid/file.js")).rejects.toThrow(
         /The loading of \/some\/invalid\/file.js failed/,
         { message: "Trying to load an invalid file rejects the promise" }
     );
-    expect(document).toContain("script[src='/some/invalid/file.js']", {
-        message: "Document contains a script with the src we asked to load",
-    });
 });
 
 test("loadCSS: load invalid CSS lib", async () => {
+    expect.assertions(4 * 4 + 1);
+
     assets.retries = { count: 3, delay: 1, extraDelay: 1 }; // Fail fast.
+
+    after(
+        observe(document.head, (mutations) => {
+            for (const mutation of mutations) {
+                for (const link of mutation.addedNodes) {
+                    expect(link).toBeInstanceOf(HTMLLinkElement);
+                    expect(link).toHaveAttribute("rel", "stylesheet");
+                    expect(link).toHaveAttribute("type", "text/css");
+                    expect(link).toHaveAttribute("href", "/some/invalid/file.css");
+
+                    after(() => link.remove());
+                }
+            }
+        })
+    );
+
     await expect(loadCSS("/some/invalid/file.css")).rejects.toThrow(
         /The loading of \/some\/invalid\/file.css failed/,
         { message: "Trying to load an invalid file rejects the promise" }
     );
-    expect(document).toContain("script[src='/some/invalid/file.css']", {
-        message: "Document contains a link with the href we asked to load",
-    });
 });
