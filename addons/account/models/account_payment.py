03244fd5ec2f9c9df0535a60d9486003187079ee
guva-odoo <guva@odoo.com>
2024-02-27 13:29:06 +0100

    [FIX] account: seek for lines for old outstanding accounts
    When having reconciled a payment with an invoice
    after having changed the outstanding receipts account
    on the bank journal, we get an error when unreconciliing
    the payment and the move.
    
    Steps:
    
    - Create two new outstanding receipts accounts X and Y
    - Set X as outstandings receipts account on the Bank journal
    - Create a manual customer payment P for $100, confirm
    - On Bank journal, change the outstanding receipts account from
      X to Y
    - Create and confirm an invoice for $100
    - Reconcile with payment P from the widget
    - Unreconcile or reset invoice to draft
    -> Error: "Journal Entry %s is not valid. In order to proceed,
    the journal items must include one and only one outstanding
    payments/receipts account."
    
    opw-3659092
    
    X-original-commit: f648aedd2b804152db4541c0b0d3102d3eb5d152

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -185,20 +185,26 @@ class AccountPayment(models.Model):
         '''
         self.ensure_one()
 
-        liquidity_lines = self.env['account.move.line']
-        counterpart_lines = self.env['account.move.line']
-        writeoff_lines = self.env['account.move.line']
-
+        # liquidity_lines, counterpart_lines, writeoff_lines
+        lines = [self.env['account.move.line'] for _dummy in range(3)]
         valid_account_types = self._get_valid_payment_account_types()
         for line in self.move_id.line_ids:
             if line.account_id in self._get_valid_liquidity_accounts():
-                liquidity_lines += line
-            elif line.account_id.account_type in valid_account_types or line.account_id == self.company_id.transfer_account_id:
-                counterpart_lines += line
+                lines[0] += line  # liquidity_lines
+            elif line.account_id.account_type in valid_account_types or line.account_id == line.company_id.transfer_account_id:
+                lines[1] += line  # counterpart_lines
             else:
-                writeoff_lines += line
+                lines[2] += line  # writeoff_lines
 
-        return liquidity_lines, counterpart_lines, writeoff_lines
+        # In some case, there is no liquidity or counterpart line (after changing an outstanding account on the journal for example)
+        # In that case, and if there is one writeoff line, we take this line and set it as liquidity/counterpart line
+        if len(lines[2]) == 1:
+            for i in (0, 1):
+                if not lines[i]:
+                    lines[i] = lines[2]
+                    lines[2] -= lines[2]
+
+        return lines
 
     def _get_valid_liquidity_accounts(self):
         return (
