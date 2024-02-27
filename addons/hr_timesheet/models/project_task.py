711569c9d584d2007710bfdc92f21b279409ea99
damr <damr@odoo.com>
2024-02-27 12:09:51 +0100

    [FIX] hr_timesheet: make portal view readonly for ts
    Currently, there is an access right problem concerning timesheet. The
    module mrp gives write access to portal user on analytic line, but it is not
    something we want for the project app.
    
    step to reproduce:
    - install hr_timesheet on a saas-16.3 db
    - go to project app, open office design
    - select any task, and add a subtask to it
    - go the the view form of the subtask
    - create a new timesheet for this subtask
    - go back to the view form of the parent task
    a button 'subtask timesheet' is now visible
    - share the project in edit mode with a portal user
    - connect with that portal user
    - open the office design project
    - go to the view form of the parent task and click on the button
    - the user is redirected to a view tree with the timesheet of the
    subtask
    this view tree is editable
    
    Source of the problem:
    in the mrp module, the access rights of the analytic line is overwritten
    to give write access to it to portal user.
    Soluce:
    We have to make the view in readonly mode for timesheet when the
    connected user is a portal one. Unfortunately, it is not 100% possible
    because setting the field to readonly still gives the user the
    opportunity to click on those fields (which triggers access errors). So
    we have to create new views and specifically open those views when the
    user is a portal one.
    
    affected version: saas-16.3 - master
    task - 3751315

--- addons/hr_timesheet/models/project_task.py
+++ addons/hr_timesheet/models/project_task.py
@@ -156,14 +156,32 @@ class Task(models.Model):
 
     def action_view_subtask_timesheet(self):
         self.ensure_one()
+        is_internal_user = self.env.user.has_group('base.group_user')
         task_ids = self.with_context(active_test=False)._get_subtask_ids_per_task_id().get(self.id, [])
         action = self.env["ir.actions.actions"]._for_xml_id("hr_timesheet.timesheet_action_all")
         graph_view_id = self.env.ref("hr_timesheet.view_hr_timesheet_line_graph_by_employee").id
         new_views = []
         for view in action['views']:
+            if not is_internal_user:
+                if view[1] == 'tree':
+                    tree_view_id = self.env['ir.model.data']._xmlid_to_res_id('hr_timesheet.hr_timesheet_line_portal_tree')
+                    if tree_view_id:
+                        new_views.insert(0, (tree_view_id, 'tree'))
+                        continue
+                elif view[1] == 'form':
+                    form_view_id = self.env['ir.model.data']._xmlid_to_res_id('hr_timesheet.timesheet_view_form_portal_user')
+                    if form_view_id:
+                        new_views.append((form_view_id, 'form'))
+                        continue
+                elif view[1] == 'kanban':
+                    kanban_view_id = self.env['ir.model.data']._xmlid_to_res_id('hr_timesheet.view_kanban_account_analytic_line_portal_user')
+                    if kanban_view_id:
+                        new_views.append((kanban_view_id, 'kanban'))
+                        continue
             if view[1] == 'graph':
                 view = (graph_view_id, 'graph')
             new_views.insert(0, view) if view[1] == 'tree' else new_views.append(view)
+
         action.update({
             'display_name': _('Timesheets'),
             'context': {'default_project_id': self.project_id.id, 'grid_range': 'week'},
