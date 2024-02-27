77ed8392acc0b7680d21b7fc009c302177759317
Youssef Bashandy <yoba@odoo.com>
2024-02-27 19:09:23 +0200

    [FIX] account_fleet : store the correct cost value in fleet service
    Steps to reporduce:
    	1- Install Accounting, Fleet modules
    	2- Create a bill in accounting with a different currency than the company's default, and add a line with a chosen vehicle_id.
    	3- Go to the chosen vehicle in Fleet module
    	4- Navigate to the service created for this bill
    
    Current behavior before PR:
    If we create a bill for a vehicle using a different currency than
    the company's default. The fleet service that will be created
    will be having the company's currency but the value will be the amount
    in the currency used in the bill
    
    Desired behavior after PR is merged:
    We now create the fleet service using the value in debit
    not the unit price or the price subtotal.
    
    opw-3734743
    
    closes odoo/odoo#163228
    
    X-original-commit: 0ef2abaa3e926daa71e004e0583503b7e3167a59
    Related: odoo/enterprise#61408
    Signed-off-by: Youssef Bashandy (yoba) <yoba@odoo.com>

---
+++ addons/account_fleet/tests/test_fleet_log_services.py
@@ -0,0 +1,48 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+from odoo.tests import common, tagged
+
+
+@tagged('post_install', '-at_install')
+class TestFleetLogServices(AccountTestInvoicingCommon, common.TransactionCase):
+
+    def test_fleet_log_services_amount(self):
+        brand = self.env["fleet.vehicle.model.brand"].create({
+            "name": "Audi",
+        })
+        model = self.env["fleet.vehicle.model"].create({
+            "brand_id": brand.id,
+            "name": "A3",
+        })
+        car = self.env["fleet.vehicle"].create({
+            "model_id": model.id,
+            "plan_to_change_car": False
+        })
+
+        partner = self.env['res.partner'].create({
+            "name": "Test Partner",
+        })
+
+        move = self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'partner_id': partner.id,
+            'invoice_date': '2019-01-01',
+            'date': '2019-01-01',
+            'currency_id': self.currency_data['currency'].id,
+            'line_ids': [
+                (0, 0, {
+                    'currency_id': self.currency_data['currency'].id,
+                    'account_id': self.company_data['default_account_expense'].id,
+                    'vehicle_id': car.id,
+                    'quantity': 1,
+                    'price_unit': 5000
+                })
+            ],
+        })
+        move.action_post()
+        line = move.line_ids[0]
+        fleet_service = self.env['fleet.vehicle.log.services'].search([('vendor_id', '=', partner.id),
+                                                                       ('description', '=', False)])
+
+        self.assertNotEqual(line.debit, line.price_subtotal)
+        self.assertEqual(fleet_service.amount, line.debit)
