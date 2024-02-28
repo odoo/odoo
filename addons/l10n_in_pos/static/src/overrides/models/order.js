7d27c255aaca2108f87dd2d1c93899fb0e7ecccb
Harsh Modi <hamo@odoo.com>
2024-02-28 11:23:08 +0530

    [IMP] point_of_sale, l10n_in_pos: change label `VAT -> Tax`, show tax info on ticket
    point_of_sale:
    This commit changes label `VAT` -> `Tax`
    because VAT is to be specific where other
    countries do use other taxes i.e. `GST` changing
    it to `Tax` makes it more generic
    
    l10n_in_pos:
    
    In India, it is mandatory to show the tax info
    
    This commit solves the issue where tax info was missing
    on POS ticket
    
    closes odoo/odoo#155699
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/l10n_in_pos/static/src/overrides/models/order.js
+++ addons/l10n_in_pos/static/src/overrides/models/order.js
@@ -6,6 +6,11 @@ import { patch } from "@web/core/utils/patch";
 patch(Order.prototype, {
     export_for_printing() {
         const result = super.export_for_printing(...arguments);
+        if (this.pos.company.country.code === 'IN') {
+            result.tax_details.forEach((tax) => {
+                tax.tax.letter = tax.tax.tax_group_id[1]
+            })
+        }
         if (this.get_partner()) {
             result.partner = this.get_partner();
         }
