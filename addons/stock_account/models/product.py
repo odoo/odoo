104b8b6718603aac93504c522d14c480804cd0c5
Aungkokolin1997 <lin@quartile.co>
2024-02-28 09:46:21 +0630

    [IMP] stock_account: refactoring for checking owner_id
    Refactored the owner_id check in the stock_account module to make it more flexible for future updates.
    Before, any changes or additions to how ownership is checked required overriding the whole method.
    Now, it's easier to extend ownership checks without such heavy modifications,
    paving the way for more straightforward updates and customizations related to inventory ownership.

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -705,7 +705,7 @@ class ProductProduct(models.Model):
         # price to estimate the anglo saxon price unit.
         missing = qty_to_invoice - qty_valued
         for sml in stock_moves.move_line_ids:
-            if not sml.owner_id or sml.owner_id == sml.company_id.partner_id:
+            if not sml._should_exclude_for_valuation():
                 continue
             missing -= sml.product_uom_id._compute_quantity(sml.qty_done, self.uom_id, rounding_method='HALF-UP')
         if float_compare(missing, 0, precision_rounding=self.uom_id.rounding) > 0:
