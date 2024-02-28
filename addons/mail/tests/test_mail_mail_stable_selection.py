14a228b0bf0d4a04fc5ded79b3c8753643a67871
Renaud Thiry <reth@odoo.com>
2024-02-28 17:08:53 +0100

    [FIX] mail: add selection when fetching message type
    A new message type was added in stable. This is usually safe,
    however in cases where there are related fields on that same selection
    fetching them will raise an exception as the ORM has to fetch the translations
    for the selection in DB.
    
    We add a hack on mail.mail to update the selections in DB when fetching
    the message_type field for the first time.
    
    task-3773301
    
    X-original-commit: a8ae1665f771a528761eff62260775c0f464726f

---
+++ addons/mail/tests/test_mail_mail_stable_selection.py
@@ -0,0 +1,16 @@
+
+
+from odoo.tests.common import TransactionCase
+
+class TestMailMailStableSelection(TransactionCase):
+    """Only relevant in stable as a hotfix. May be removed in master."""
+
+    def test_mail_mail_stable_selection(self):
+        # remove all selections
+        message_type_selections = self.env['ir.model.fields']._get('mail.message', 'message_type').selection_ids
+        message_type_selections.filtered(lambda s: s.value == 'auto_comment').unlink()
+        self.env['mail.mail']._fields_get_message_type_update_selection(self.env['mail.message']._fields['message_type'].selection)
+        # force convert to cache with specific language so it has to fetch related from DB
+        mail = self.env['mail.mail'].create({'subject': 'test', 'message_type': 'auto_comment'})
+        self.env['mail.mail'].invalidate_cache(['message_type'])
+        self.assertEqual(mail.with_context(lang="en_US").message_type, 'auto_comment')
