4e5b9c2bf7ba7b40b6f34d598f69df1d50b566b7
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-02-28 15:44:00 +0000

    [FIX] web_editor: don't delete content inside contenteditable=false
    Issue:
    ======
    Currently you can delete content inside contenteditable=false elements
    
    Steps to reproduce the issue:
    =============================
    - Go to website, edit mode, drag a text block
    - Add `contenteditable="false"` to a `p` element
    - Select a word in that `p` element and press on any character, it will
      be deleted
    
    Origin of the issue:
    ====================
    Theres is no check if the element is inside a contenteditable=false or
    not.
    
    Solution:
    =========
    We need to check if the selection is in editable or not to be able to
    apply commands.
    
    task-3605829
    
    X-original-commit: f4af96e43d71c3291dd996699bad6de894bcf827

--- addons/web_editor/static/src/js/editor/odoo-editor/test/spec/editor.test.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/test/spec/editor.test.js
@@ -2049,6 +2049,13 @@ X[]
                         contentAfter: `<p>abc</p><p>[]def</p>`,
                     });
                 });
+                it('should not delete in contenteditable=false', async () => {
+                    await testEditor(BasicEditor, {
+                        contentBefore: `<p contenteditable="false">ab[]cdef</p>`,
+                        stepFunction: deleteBackward,
+                        contentAfter: `<p contenteditable="false">ab[]cdef</p>`,
+                    });
+                });
             });
             describe('Line breaks', () => {
                 describe('Single', () => {
@@ -3386,6 +3393,28 @@ X[]
                         contentAfter: unformat(`
                             <p>before[]after</p>`),
                     });
+                });
+            });
+            it('should not delete in contenteditable=false 1', async () => {
+                await testEditor(BasicEditor, {
+                    contentBefore: `<p contenteditable="false">ab[cd]ef</p>`,
+                    stepFunction: deleteBackward,
+                    contentAfter: `<p contenteditable="false">ab[cd]ef</p>`,
+                });
+            });
+            it('should not delete in contenteditable=false 2', async () => {
+                await testEditor(BasicEditor, {
+                    contentBefore: `<div contenteditable="false">
+                                        <p>a[b</p>
+                                        <p>cd</p>
+                                        <p>e]f</p>
+                                    </div>`,
+                    stepFunction: deleteBackward,
+                    contentAfter: `<div contenteditable="false">
+                                        <p>a[b</p>
+                                        <p>cd</p>
+                                        <p>e]f</p>
+                                    </div>`,
                 });
             });
         });
