554ae1bcda6bfe3b6c1fd6215970fee8c0f918f0
Arnaud Sibille <arsi@odoo.com>
2024-02-28 09:47:58 +0000

    [FIX] account: force _compute_journal_id only when creating payment
    The goal of the `def new` override on `account.payment` is to have the
    `journal_id` computed when creating a payment in the form view.
    
    The problem is that it is also called by the onchange when a field is
    modified.
    This is causing a bug:
    - Go the the cash journal and change the name of its payment methods (so
     you can distinct them from the ones of the bank journal).
    - create a payment (do not save)
    - switch the journal to "Cash" then switch back to "Bank".
    -> the "Payment Method" is still one from the "Cash" journal.
    
    The fix here is a hacky way of checking this is the call on the
    record creation.
    
    closes odoo/odoo#157407
    
    X-original-commit: 816a170a10969a7785fdcf2f9dffe461108ca16c
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/tests/test_account_payment.py
+++ addons/account/tests/test_account_payment.py
@@ -1031,3 +1031,22 @@ class TestAccountPayment(AccountTestInvoicingCommon):
         invoice.button_draft()
         self.assertTrue(invoice.payment_state == 'not_paid', "Invoice should'nt be paid anymore")
         self.assertTrue(invoice.state == 'draft', "Invoice should be draft")
+
+    def test_journal_onchange(self):
+        """Ensure that the payment method line is recomputed when switching journal in form view."""
+
+        context = {
+            'payment_type': 'inbound',
+            'partner_type': 'customer',
+        }
+        with Form(self.env['account.payment'].with_context(context)) as payment:
+            default_journal = payment.journal_id
+            self.assertTrue(default_journal)
+            self.assertEqual(payment.payment_method_line_id.journal_id.id, default_journal.id)
+
+            other_journal = self.bank_journal_2 if default_journal != self.bank_journal_2 else self.bank_journal_1
+            payment.journal_id = other_journal
+            self.assertEqual(payment.payment_method_line_id.journal_id.id, other_journal.id)
+
+            payment.journal_id = default_journal
+            self.assertEqual(payment.payment_method_line_id.journal_id.id, default_journal.id)
