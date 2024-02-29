d26073f351007ce37110d9486a4f7872876a531f
Arnold Moyaux <arm@odoo.com>
2024-02-29 16:24:09 +0100

    [FIX] stock: push in intercompany
    It's not possible to push in intercompany location because the warehouse
    is set base on the initial move. However in intercompany we don't
    have a warehouse in the most cases.
    
    Also there is an issue with the cache holding the route on the product
    for the current company. So we invalidate it to retrieve all the routes
    among the different companies

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -903,7 +903,9 @@ Please change the quantity done or the rounding precision of your unit of measur
             if move.location_dest_id.company_id == self.env.company:
                 rule = self.env['procurement.group']._search_rule(move.route_ids, move.product_packaging_id, move.product_id, warehouse_id, domain)
             else:
-                rule = self.sudo().env['procurement.group']._search_rule(move.route_ids, move.product_packaging_id, move.product_id, warehouse_id, domain)
+                procurement_group = self.env['procurement.group'].sudo()
+                move = move.with_context(allowed_companies=self.env.user.company_ids.ids)
+                rule = procurement_group._search_rule(move.route_ids, move.product_packaging_id, move.product_id, False, domain)
             # Make sure it is not returning the return
             if rule and (not move.origin_returned_move_id or move.origin_returned_move_id.location_dest_id.id != rule.location_dest_id.id):
                 new_move = rule._run_push(move)
