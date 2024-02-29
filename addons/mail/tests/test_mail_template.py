odoo/odoo

commit cf7ae6b9d562622709e2730c1ea8c43816c169bf

[FIX] account, mail: defensively evaluate 'partner_to' on mail templates
MailTemplate model has a 'partner_to' field is dynamically rendered to contain
partners being recipients. After rendering it should hold a comma-separated
list of partner IDs.

However as we are unsure it was correctly written better be defensive. We now
check that we can effectively transform items to IDs using 'isdigit()'.

Task-2612945 (Mail: Defensive email formatting)

X-original-commit: odoo/odoo@f96834b402214a8f51900d26c5845703b81c7501
Part-of: odoo/odoo#134374

addons/mail/models/mail_template.py
@@ -375,10 +375,9 @@ class MailTemplate(models.Model):
 
         # update 'partner_to' rendered value to 'partner_ids'
         all_partner_to = {
-            int(pid)
+            pid
             for record_values in render_results.values()
-            for pid in record_values.get('partner_to', '').split(',')
-            if pid and pid.strip()
+            for pid in self._parse_partner_to(record_values.get('partner_to', ''))
         }
         existing_pids = set()
         if all_partner_to:
@@ -386,7 +385,7 @@ class MailTemplate(models.Model):
         for res_id, record_values in render_results.items():
             partner_to = record_values.pop('partner_to', '')
             if partner_to:
-                tpl_partner_ids = set(int(pid) for pid in partner_to.split(',') if pid) & existing_pids
+                tpl_partner_ids = set(self._parse_partner_to(partner_to)) & existing_pids
                 record_values.setdefault('partner_ids', []).extend(tpl_partner_ids)
 
         return render_results
@@ -525,6 +524,13 @@ class MailTemplate(models.Model):
                 )
 
         return render_results
+
+    @classmethod
+    def _parse_partner_to(cls, partner_to):
+        return [
+            int(pid.strip()) for pid in partner_to.split(',')
+            if (pid and pid.strip().isdigit())
+        ]
 
     # ------------------------------------------------------------
     # EMAIL

addons/account/wizard/account_move_send.py
@@ -121,7 +121,7 @@ class AccountMoveSend(models.Model):
                 partners |= partners.find_or_create(mail_data)
         if mail_template.partner_to:
             partner_to = self._get_mail_default_field_value_from_template(mail_template, lang, move, 'partner_to')
-            partner_ids = [int(pid) for pid in partner_to.split(',') if pid]
+            partner_ids = mail_template._parse_partner_to(partner_to)
             partners |= self.env['res.partner'].sudo().browse(partner_ids).exists()
         return partners
 

commit 01d16c6d7283c135c36d7f75439c176fa8c2f791

[FIX] mail: templates preview wrong recipients
Steps to reproduce:
1. Activate dev mode
2. Go to Email Templates
3. Create a new template
4. Applies to Task
5. Email Configuration > To (Partners)
6. {{[p.id for p in object.message_partner_ids]}}
7. Click on Preview
8. Select a record with followers
9. Look at Recipients
10. => No or not all followers/partners are included

Cause of the issue:
Caused by https://github.com/odoo/odoo/commit/cf7ae6b9d562622709e2730c1ea8c43816c169bf
In the case of partner_to being '[2,3,4]'
Split would output '[2', '3', '4]'
So after the isdigit it would be '3'
It should have been 2, 3, 4

opw-3677069

addons/mail/models/mail_template.py
@@ -4,6 +4,7 @@
 import base64
 import itertools
 import logging
+from ast import literal_eval
 
 from odoo import _, api, fields, models, tools, Command
 from odoo.exceptions import ValidationError, UserError
@@ -527,9 +528,15 @@ class MailTemplate(models.Model):
 
     @classmethod
     def _parse_partner_to(cls, partner_to):
+        try:
+            partner_to = literal_eval(partner_to or '[]')
+        except (ValueError, SyntaxError):
+            partner_to = partner_to.split(',')
+        if not isinstance(partner_to, (list, tuple)):
+            partner_to = [partner_to]
         return [
-            int(pid.strip()) for pid in partner_to.split(',')
-            if (pid and pid.strip().isdigit())
+            int(pid.strip()) if isinstance(pid, str) else int(pid) for pid in partner_to
+            if (isinstance(pid, str) and pid.strip().isdigit()) or (pid and not isinstance(pid, str))
         ]
 
     # ------------------------------------------------------------

addons/mail/tests/test_mail_template.py
@@ -155,6 +155,21 @@ class TestMailTemplate(MailCommon):
 
         employee_template.with_context(lang='fr_FR').sudo().subject = '{{ object.foo }}'
 
+    def test_mail_template_parse_partner_to(self):
+        for partner_to, expected in [
+            ('1', [1]),
+            ('1,2,3', [1, 2, 3]),
+            ('1, 2,  3', [1, 2, 3]),  # remove spaces
+            ('[1, 2, 3]', [1, 2, 3]),  # %r of a list
+            ('(1, 2, 3)', [1, 2, 3]),  # %r of a tuple
+            ('1,[],2,"3"', [1, 2, 3]),  # type tolerant
+            ('(1, "wrong", 2, "partner_name", "3")', [1, 2, 3]),  # fault tolerant
+            ('res.partner(1, 2, 3)', [2]),  # invalid input but avoid crash
+        ]:
+            with self.subTest(partner_to=partner_to):
+                parsed = self.mail_template._parse_partner_to(partner_to)
+                self.assertListEqual(parsed, expected)
+
     def test_server_archived_usage_protection(self):
         """ Test the protection against using archived server (servers used cannot be archived) """
         IrMailServer = self.env['ir.mail_server']