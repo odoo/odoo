4d8be477cb562de3ff3c92d8f089346b54f8d12c
Raja Prasad <rajp@odoo.com>
2024-02-29 18:35:00 +0530

    [IMP] resource: show the resource avatar
    - displayed picture of the employee if resource type is human
    - displayed tool icon if resource type is material
    
    task-3770245

--- addons/resource/static/tests/resource_test_helpers.js
+++ addons/resource/static/tests/resource_test_helpers.js
@@ -2,7 +2,7 @@ import { ResourceTask } from "./mock_server/mock_models/resource_task";
 import { ResourceResource } from "./mock_server/mock_models/resource_resource";
 import { ResourceCalendar } from "./mock_server/mock_models/resource_calendar";
 import { ResourceCalendarAttendance } from "./mock_server/mock_models/resource_calendar_attendance";
-import { defineModels } from "@web/../tests/web_test_helpers";
+import { defineModels, webModels} from "@web/../tests/web_test_helpers";
 
 export const resourceModels = {
     ResourceTask,
@@ -12,5 +12,5 @@ export const resourceModels = {
 };
 
 export function defineResourceModels() {
-    return defineModels(resourceModels);
+    return defineModels({ ...resourceModels, ...webModels });
 }
