bfa27f512c7d38678ae6392c678788ee4fb9a992
Gauthier Wala (gawa) <gawa@odoo.com>
2024-03-01 09:11:30 +0100

    [FIX] account: taxes with amount_type 'group' are not well reloaded
    To reproduce:
    - Install l10n_it
    - Go in settings
    - Fiscal Localization => reload
    - Go to the taxes
    => You will find taxes with [old] prefix
    
    The issue comes that we want to consider that a tax has changed
    if it has different repartition lines.
    We don't consider taxes that are defined without repartition lines.
    The template will have no lines, but the compute
    on actual taxes will generate default ones.
    It then considers that the tax has changed.
    
    task-3777629
    
    closes odoo/odoo#156974
    
    X-original-commit: 55c09d3fab70d241174c44a9f3d123a01211730e
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- addons/account/models/chart_template.py
+++ addons/account/models/chart_template.py
@@ -265,7 +265,11 @@ class AccountChartTemplate(models.AbstractModel):
             return (
                 tax.amount_type != template.get('amount_type', 'percent')
                 or tax.amount != template.get('amount', 0)
-                or len(tax.repartition_line_ids) != len(template.get('repartition_line_ids', []))
+                or (
+                    len(tax.repartition_line_ids) != len(template.get('repartition_line_ids', []))
+                    # Taxes that don't have repartition lines in their templates get theirs created by default
+                    and len(template.get('repartition_line_ids', [])) != 0
+                )
             )
 
         obsolete_xmlid = set()
