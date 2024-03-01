odoo/odoo

commit 858bf9efdd9fcc29a73336863a51227c15cc19f0

[FIX] l10n_mx: Fix account tags for COA SAT(XML) export
Steps to reproduce:
1. Install l10n_mx
2. Go to Accounting > Reporting > Trial balance.
3. Click the COA SAT(XML) export button.
4. Warnings are raised.

Problem: the report should work out-of-the-box on new databases.

Cause:
1. Some tags are missing on auto-generated accounts.
2. Some data were not correct and had the wrong tag. 102 accounts in particular should be debit and not credit.

This PR migrate the incorrect data and add a default naive computation of the tag on newly created accounts.

opw-3283746

closes odoo/odoo#119485

Enterprise: https://github.com/odoo/enterprise/pull/57531
Related: odoo/enterprise#57531
Signed-off-by: Laurent Smet (las) <las@odoo.com>

addons/l10n_mx/models/account_account.py
@@ -1,0 +1,18 @@
+from odoo import Command, models
+
+
+class AccountAccount(models.Model):
+    _inherit = 'account.account'
+
+    def create(self, vals_list):
+        # EXTENDS account - ensure there is a tag on created MX accounts
+        # The computation is a bit naive and might not be correct in all cases.
+        accounts = super().create(vals_list)
+        debit_tag = self.env.ref('l10n_mx.tag_debit_balance_account')
+        credit_tag = self.env.ref('l10n_mx.tag_credit_balance_account')
+        mx_account_no_tags = accounts.filtered(lambda a: a.company_id.country_code == 'MX' and not a.tag_ids & (credit_tag + debit_tag))
+        DEBIT_CODES = ['1', '5', '6', '7']  # all other codes are considered "credit"
+        for account in mx_account_no_tags:
+            tag_id = debit_tag.id if account.code[0] in DEBIT_CODES else credit_tag.id
+            account.tag_ids = [Command.link(tag_id)]
+        return accounts

addons/l10n_mx/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "Mexico - Accounting",
-    "version": "2.0",
+    "version": "2.1",
     "author": "Vauxoo",
     'category': 'Accounting/Localizations/Account Charts',
     "description": """

addons/l10n_mx/migrations/2.1/post-migrate.py
@@ -1,0 +1,15 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, Command, SUPERUSER_ID
+
+
+def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    # Migrate wrong data tag on account cuenta102_02
+    debit_tag = env.ref('l10n_mx.tag_debit_balance_account')
+    credit_tag = env.ref('l10n_mx.tag_credit_balance_account')
+    account_102_ids = env['ir.model.data'].search([
+        ('name', 'ilike', '%_cuenta102_02'),
+        ('model', '=', 'account.account'),
+    ]).mapped('res_id')
+    accounts_102 = env['account.account'].search([('id', 'in', account_102_ids)])
+    accounts_102.tag_ids = [Command.unlink(credit_tag.id), Command.link(debit_tag.id)]

addons/l10n_mx/models/__init__.py
@@ -2,8 +2,8 @@
 # Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+from . import account_account
 from . import account_tax
-from . import account_journal
 from . import res_bank
 from . import res_config_settings
 from . import chart_template

addons/l10n_mx/models/account_journal.py
@@ -1,16 +1,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, api, Command
-
-
-class AccountJournal(models.Model):
-    _inherit = "account.journal"
-
-    @api.model
-    def _prepare_liquidity_account_vals(self, company, code, vals):
-        account_vals = super()._prepare_liquidity_account_vals(company, code, vals)
-        if company.account_fiscal_country_id.code != 'MX':
-            return account_vals
-        # Ensure Cash account has the correct tags in order to export the COA
-        account_vals.setdefault('tag_ids', []).append(Command.link(self.env.ref('l10n_mx.tag_credit_balance_account').id))
-        return account_vals

addons/l10n_mx/models/chart_template.py
@@ -47,15 +47,3 @@ class AccountChartTemplate(models.Model):
             'show_on_dashboard': True,
         })
         return res
-
-    @api.model
-    def _create_liquidity_journal_suspense_account(self, company, code_digits):
-        if not self == self.env.ref('l10n_mx.mx_coa'):
-            return super()._create_liquidity_journal_suspense_account(company, code_digits)
-        return self.env['account.account'].create({
-            'name': _("Bank Suspense Account"),
-            'code': self.env['account.account']._search_new_account_code(company, code_digits, company.bank_account_code_prefix or ''),
-            'account_type': 'asset_current',
-            'company_id': company.id,
-            'tag_ids': self.env.ref('l10n_mx.tag_credit_balance_account')
-        })

commit a428ce9e65679b45f15518db230173445518706b

[FIX] l10n_mx: add missing decorator on create method
Currently, if you install the Mexican localization, you are no longer
able to create  a new account in the chart of accounts.

Steps to reproduce
------------------
* install `l10n_mx`
* attempt to create a new account in the chart of accounts

You should be met with a traceback: `TypeError: AccountAccount.create()
missing 1 required positional argument: 'vals_list'`

Cause
-----
The `@api.model` and `@api.model_create_multi` decorators in indicate
that a method is intended to operate at the model level rather than on
specific record instances. Without these decorators, the system defaults
to treating methods as if they are meant to be executed on the record
level. This distinction significantly impacts how methods are invoked
through RPC.

In RPC scenarios, Odoo's default expectation for record-level methods is
that the RPC call will include the IDs of the records to which the
method applies, alongside any actual method arguments necessary for the
operation.

Here, `create()` is called through RPC, as a model-level method (i.e,
without record IDs). This makes sense because `create()` is always a
model-level method. However, in our case, create is a record-level
method, so the system expects the RPC call to contain record IDs. This
mismatch produces a traceback.

Issue introduced by 858bf9efdd9fcc29a73336863a51227c15cc19f0

opw-3772520
opw-3772469
opw-3772287
opw-3771854
opw-3771361

addons/l10n_mx/models/account_account.py
@@ -1,9 +1,10 @@
-from odoo import Command, models
+from odoo import api, Command, models
 
 
 class AccountAccount(models.Model):
     _inherit = 'account.account'
 
+    @api.model_create_multi
     def create(self, vals_list):
         # EXTENDS account - ensure there is a tag on created MX accounts
         # The computation is a bit naive and might not be correct in all cases.