b13b68886e5025aea7af96e61faed252b491e22b
Florent de Labarre <florent.mirieu@gmail.com>
2024-03-01 07:39:48 +0100

    [IMP] mrp: allow find kits product
    Before this PR the field is_kits is not searchable.
    Before this PR the field is_kits is computed sudo, it should not.

--- addons/mrp/tests/test_bom.py
+++ addons/mrp/tests/test_bom.py
@@ -462,6 +462,115 @@ class TestBoM(TestMrpCommon):
         self.env['stock.quant']._update_available_quantity(product_dozens, location, 1.0)
         self.assertEqual(product_unit.qty_available, 12.0)
 
+    def test_19_bom_kit_field_is_kits_bom_with_product_id(self):
+        kit_products = self.env['product.product'].create({
+            'name': 'No Kit',
+            'type': 'product',
+            'uom_id': self.uom_unit.id,
+        })
+        self.env['mrp.bom'].create({
+            'product_id': kit_products.id,
+            'product_tmpl_id': kit_products.product_tmpl_id.id,
+            'product_uom_id': self.uom_unit.id,
+            'product_qty': 4.0,
+            'type': 'phantom',
+            'bom_line_ids': [
+                (0, 0, {
+                    'product_id': self.product_2.id,
+                    'product_qty': 2,
+                }),
+                (0, 0, {
+                    'product_id': self.product_3.id,
+                    'product_qty': 2,
+                })
+            ]
+        })
+        self.assertTrue(kit_products.is_kits)
+        self.assertTrue(kit_products.product_tmpl_id.is_kits)
+
+        no_kit_products = self.env['product.product'].create({
+            'name': 'No Kit',
+            'type': 'product',
+            'uom_id': self.uom_unit.id,
+        })
+        self.assertFalse(no_kit_products.is_kits)
+        self.assertFalse(no_kit_products.product_tmpl_id.is_kits)
+
+        product_with_kit = self.env['product.product'].search(
+            [('is_kits', '=', True)])
+        product_tmpl_with_kit = self.env['product.template'].search(
+            [('is_kits', '=', True)])
+        self.assertIn(kit_products, product_with_kit)
+        self.assertIn(kit_products.product_tmpl_id, product_tmpl_with_kit)
+        self.assertNotIn(no_kit_products, product_with_kit)
+        self.assertNotIn(no_kit_products.product_tmpl_id,
+                         product_tmpl_with_kit)
+
+        product_without_kit = self.env['product.product'].search(
+            [('is_kits', '=', False)])
+        product_tmpl_without_kit = self.env['product.template'].search(
+            [('is_kits', '=', False)])
+        self.assertIn(no_kit_products, product_without_kit)
+        self.assertIn(no_kit_products.product_tmpl_id,
+                      product_tmpl_without_kit)
+        self.assertNotIn(kit_products, product_without_kit)
+        self.assertNotIn(kit_products.product_tmpl_id,
+                         product_tmpl_without_kit)
+
+    def test_19_bom_kit_field_is_kits_bom_without_product_id(self):
+        kit_products = self.env['product.product'].create({
+            'name': 'No Kit',
+            'type': 'product',
+            'uom_id': self.uom_unit.id,
+        })
+        self.env['mrp.bom'].create({
+            'product_tmpl_id': kit_products.product_tmpl_id.id,
+            'product_uom_id': self.uom_unit.id,
+            'product_qty': 4.0,
+            'type': 'phantom',
+            'bom_line_ids': [
+                (0, 0, {
+                    'product_id': self.product_2.id,
+                    'product_qty': 2,
+                }),
+                (0, 0, {
+                    'product_id': self.product_3.id,
+                    'product_qty': 2,
+                })
+            ]
+        })
+        self.assertTrue(kit_products.is_kits)
+        self.assertTrue(kit_products.product_tmpl_id.is_kits)
+
+        no_kit_products = self.env['product.product'].create({
+            'name': 'No Kit',
+            'type': 'product',
+            'uom_id': self.uom_unit.id,
+        })
+        self.assertFalse(no_kit_products.is_kits)
+        self.assertFalse(no_kit_products.product_tmpl_id.is_kits)
+
+        product_with_kit = self.env['product.product'].search(
+            [('is_kits', '=', True)])
+        product_tmpl_with_kit = self.env['product.template'].search(
+            [('is_kits', '=', True)])
+        self.assertIn(kit_products, product_with_kit)
+        self.assertIn(kit_products.product_tmpl_id, product_tmpl_with_kit)
+        self.assertNotIn(no_kit_products, product_with_kit)
+        self.assertNotIn(no_kit_products.product_tmpl_id,
+                         product_tmpl_with_kit)
+
+        product_without_kit = self.env['product.product'].search(
+            [('is_kits', '=', False)])
+        product_tmpl_without_kit = self.env['product.template'].search(
+            [('is_kits', '=', False)])
+        self.assertIn(no_kit_products, product_without_kit)
+        self.assertIn(no_kit_products.product_tmpl_id,
+                      product_tmpl_without_kit)
+        self.assertNotIn(kit_products, product_without_kit)
+        self.assertNotIn(kit_products.product_tmpl_id,
+                         product_tmpl_without_kit)
+
     def test_20_bom_report(self):
         """ Simulate a crumble receipt with mrp and open the bom structure
         report and check that data insde are correct.
