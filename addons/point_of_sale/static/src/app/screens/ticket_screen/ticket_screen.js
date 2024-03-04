2926acd8d8181bbffcb9792e38f61205b76cf26e
Dylan Kiss (dyki) <dyki@odoo.com>
2024-03-04 09:01:15 +0000

    [I18N] point_of_sale: make some terms translatable
    In the Point of Sale app, some terms were not translatable by our
    translators on Transifex. In this commit we make sure that the missing
    terms are either made translatable or exported in the related .pot file.
    
    closes odoo/odoo#159515
    
    X-original-commit: d05fb016ad00899fdbbeb4613fc06e51941f6224
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>
    Signed-off-by: Dylan Kiss (dyki) <dyki@odoo.com>

--- addons/point_of_sale/static/src/app/screens/ticket_screen/ticket_screen.js
+++ addons/point_of_sale/static/src/app/screens/ticket_screen/ticket_screen.js
@@ -89,9 +89,9 @@ export class TicketScreen extends Component {
     }
     getNumpadButtons() {
         return getButtons(this.env, [
-            { value: "quantity", text: "Qty", class: "active border-primary" },
-            { value: "discount", text: "% Disc", disabled: true },
-            { value: "price", text: "Price", disabled: true },
+            { value: "quantity", text: _t("Qty"), class: "active border-primary" },
+            { value: "discount", text: _t("% Disc"), disabled: true },
+            { value: "price", text: _t("Price"), disabled: true },
             { value: "-", text: "+/-", disabled: true },
         ]);
     }
