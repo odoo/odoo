2e152992ab3ca8d48aaed2527db13b609fd412b8
roen-odoo <roen@odoo.com>
2024-03-04 13:52:25 +0000

    [FIX] pos_sale: take "done" orders into account for the sales count
    Current behavior:
    Orders from a closed pos that were not invoiced are not taken into
    account in the sales count (can be seen in the "Sales" smart button)
    
    Steps to reproduce:
    - Create a new product P1
    - Sell this product in the PoS without invoicing it
    - Close the PoS session
    - Go to the "Sales" smart button of the product P1
    - You will see that the sales count is not incremented
    
    opw-3726153
    
    closes odoo/odoo#157329
    
    X-original-commit: fd321f1df87a4d39d32aad76f0d1ddcd099605b1
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/pos_sale/report/sale_report.py
+++ addons/pos_sale/report/sale_report.py
@@ -10,7 +10,7 @@ class SaleReport(models.Model):
     @api.model
     def _get_done_states(self):
         done_states = super()._get_done_states()
-        done_states.extend(['paid', 'invoiced'])
+        done_states.extend(['paid', 'invoiced', 'done'])
         return done_states
 
     state = fields.Selection(
