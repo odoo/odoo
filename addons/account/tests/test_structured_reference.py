a8aabded9b3272c0373116ceb9e718e441eff18d
SaddemAmine <sana@odoo.com>
2024-03-04 14:54:31 +0100

    [IMP] account: adding a function to check the structure validity of a reference
    This function checks if a reference is valid in regards to the
    countries currently supported in account/tools/structured_reference.py
    
    Task ID: 2982194
    
    closes odoo/odoo#156328
    
    Related: odoo/enterprise#50616
    Part-of: odoo/odoo#199735
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

--- addons/account/tests/test_structured_reference.py
+++ addons/account/tests/test_structured_reference.py
@@ -6,6 +6,7 @@ from odoo.addons.account.tools import (
     is_valid_structured_reference_fi,
     is_valid_structured_reference_no_se,
     is_valid_structured_reference_iso,
+    is_valid_structured_reference,
 )
 
 
@@ -66,3 +67,31 @@ class StructuredReferenceTest(TransactionCase):
         self.assertFalse(is_valid_structured_reference_no_se('1234/5678/97'))
         # Does not validate invalid checksum
         self.assertFalse(is_valid_structured_reference_no_se('1234567898'))
+
+    def test_structured_reference(self):
+        # Accepts references in structured format
+        self.assertTrue(is_valid_structured_reference(' RF18 5390 0754 7034 '))  # ISO
+        self.assertTrue(is_valid_structured_reference(' +++020/3430/57642+++'))  # BE
+        self.assertTrue(is_valid_structured_reference('***020/3430/57642*** '))  # BE
+        self.assertTrue(is_valid_structured_reference('2023 0000 98'))  # FI
+        self.assertTrue(is_valid_structured_reference('1234 5678 97'))  # NO-SE
+        # Accept references in unstructured format
+        self.assertTrue(is_valid_structured_reference(' RF18539007547034'))  # ISO
+        self.assertTrue(is_valid_structured_reference(' 020343057642'))  # BE
+        self.assertTrue(is_valid_structured_reference('2023000098'))  # FI
+        self.assertTrue(is_valid_structured_reference('1234567897'))  # NO-SE
+        # Validates with zero's added in front
+        self.assertTrue(is_valid_structured_reference('RF18000000000539007547034'))  # ISO
+        self.assertTrue(is_valid_structured_reference('00000000002023000098'))  # FI
+        self.assertTrue(is_valid_structured_reference('000001234567897'))  # NO-SE
+
+        # Does not validate invalid structured format
+        self.assertFalse(is_valid_structured_reference('18539007547034RF'))  # ISO
+        self.assertFalse(is_valid_structured_reference('***02/03430/57642***'))  # BE
+        self.assertFalse(is_valid_structured_reference('2023/0000/98'))  # FI
+        self.assertFalse(is_valid_structured_reference('1234/5678/97'))  # NO-SE
+        # Does not validate invalid checksum
+        self.assertFalse(is_valid_structured_reference('RF17539007547034'))  # ISO
+        self.assertFalse(is_valid_structured_reference('020343057641'))  # BE
+        self.assertFalse(is_valid_structured_reference('2023000095'))  # FI
+        self.assertFalse(is_valid_structured_reference('1234567898'))  # NO-SE
