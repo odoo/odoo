9417f9acb8751179a01ac32368374539afb99ae9
Fabien Pinckaers <fp@openerp.com>
2024-03-04 10:15:16 +0530

    [IMP] base: new attachement directory creation
    Fix race-condition when directory created by two processes: usually when
    creating assets (JS, CSS) for the first time; multiple HTTP might
    request the same document at the same time.
    Sentry issue 4417110945
    
    Retro-compatibility code not needed anymore (2014); even if it happens,
    worst case is to store two times the same file if same one is reuploaded.
    (not a big deal)
    
    closes odoo/odoo#156231
    
    Signed-off-by: Fabien Pinckaers (fp) <fp@odoo.com>

--- odoo/addons/base/models/ir_attachment.py
+++ odoo/addons/base/models/ir_attachment.py
@@ -97,19 +97,14 @@ class IrAttachment(models.Model):
 
     @api.model
     def _get_path(self, bin_data, sha):
-        # retro compatibility
-        fname = sha[:3] + '/' + sha
-        full_path = self._full_path(fname)
-        if os.path.isfile(full_path):
-            return fname, full_path        # keep existing path
-
         # scatter files across 256 dirs
         # we use '/' in the db (even on windows)
         fname = sha[:2] + '/' + sha
         full_path = self._full_path(fname)
         dirname = os.path.dirname(full_path)
         if not os.path.isdir(dirname):
-            os.makedirs(dirname)
+            os.makedirs(dirname, exist_ok=True)
+
         # prevent sha-1 collision
         if os.path.isfile(full_path) and not self._same_content(bin_data, full_path):
             raise UserError(_("The attachment collides with an existing file."))
