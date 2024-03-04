ed67af8e35d30c59a8732a6f2da71fac9194c4e4
Loan (LSE) <lse@odoo.com>
2024-03-04 18:12:19 +0000

    [FIX] pos_restaurant: preparation printers receipt does not show variant
    Before this fix:
     Receipts printed with kitchen/preparation printer does not show the details
     of variants products. Just the product template name.
     This is ambigious for kitchen as the variant information is generally
     necessary in order to prepare the order correctly
    
    After this fix:
     Restore the previous behavior regarding the product name used
    
    This bug is a side effect of:
    https://github.com/odoo/odoo/pull/152213
    due to the changes to `set_full_product_name`
    
    Was also a good occasion to add some tests on the changes name & qty
    
    opw-3755391
    
    closes odoo/odoo#156764
    
    X-original-commit: 3b6455eb979078354e3b1009a354ca7010e5fec9
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Loan Sens (lse) <lse@odoo.com>

--- addons/point_of_sale/static/src/app/store/models.js
+++ addons/point_of_sale/static/src/app/store/models.js
@@ -1,6 +1,6 @@
 /** @odoo-module */
 
-import { random5Chars, uuidv4, qrCodeSrc } from "@point_of_sale/utils";
+import { constructFullProductName, random5Chars, uuidv4, qrCodeSrc } from "@point_of_sale/utils";
 // FIXME POSREF - unify use of native parseFloat and web's parseFloat. We probably don't need the native version.
 import { parseFloat as oParseFloat } from "@web/views/fields/parsers";
 import {
@@ -446,6 +446,23 @@ export class Orderline extends PosModel {
     }
     get_full_product_name() {
         return this.full_product_name || this.product.display_name;
+    }
+    /**
+     * Return the full product name with variant details.
+     * 
+     * e.g. Desk Organiser product with variant:
+     * - Size: S
+     * - Fabric: Plastic
+     * 
+     * -> "Desk Organiser (S, Plastic)"
+     * @returns {string}
+     */
+    get_full_product_name_with_variant() {
+        return constructFullProductName(
+            this,
+            this.pos.models["product.template.attribute.value"].getAllBy("id"),
+            this.product.display_name
+        );
     }
     // selects or deselects this orderline
     set_selected(selected) {
@@ -1430,7 +1447,7 @@ export class Order extends PosModel {
                         attribute_value_ids: line.attribute_value_ids,
                         line_uuid: line.uuid,
                         product_id: line.get_product().id,
-                        name: line.get_full_product_name(),
+                        name: line.get_full_product_name_with_variant(),
                         note: note,
                         quantity: line.get_quantity(),
                     };
@@ -1480,7 +1497,7 @@ export class Order extends PosModel {
 
                 if (quantityDiff && orderline.skipChange === skipped) {
                     changes[lineKey] = {
-                        name: orderline.get_full_product_name(),
+                        name: orderline.get_full_product_name_with_variant(),
                         product_id: product.id,
                         attribute_value_ids: orderline.attribute_value_ids,
                         quantity: quantityDiff,
