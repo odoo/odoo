6eafec2680ac7ef6d3d20dc7a2c27be157578981
SaddemAmine <sana@odoo.com>
2024-03-04 14:54:31 +0100

    [IMP] account: adding a function to check the structure validity of a reference
    This function checks if a reference is valid in regards to the
    countries currently supported in account/tools/structured_reference.py
    
    Task ID: 2982194
    
    closes odoo/odoo#156328
    
    Related: odoo/enterprise#50616
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- addons/account/tools/structured_reference.py
+++ addons/account/tools/structured_reference.py
@@ -67,3 +67,19 @@ def is_valid_structured_reference_no_se(reference):
     ref = sanitize_structured_reference(reference)
     no_se_ref = re.match(r'\d+', ref)
     return no_se_ref and luhn.is_valid(ref)
+
+
+def is_valid_structured_reference(reference):
+    """Check whether the provided reference is a valid structured reference.
+    This is currently supporting SEPA enabled countries. More specifically countries covered by functions in this file.
+
+    :param reference: the reference to check
+    """
+    reference = sanitize_structured_reference(reference or '')
+
+    return (
+        is_valid_structured_reference_be(reference) or
+        is_valid_structured_reference_fi(reference) or
+        is_valid_structured_reference_no_se(reference) or
+        is_valid_structured_reference_iso(reference)
+    )
