9f5ad045c2b4cbd5b4090a325551839af5583580
Serge Bayet (seba) <seba@odoo.com>
2024-03-05 11:49:40 +0000

    [FIX] website: Ensure no public users when archiving company
    This commit prohibits the archival of a company if it is associated with
    a website. Otherwise, the website wouldn't be accessible anymore by
    public users (and so by all users if they need to login).
    
    Step to reproduce:
    - On website 2, change the company from SF to Chicago
    - In the website list view (in debug mode), reorder the websites so
      website 2 is the first one in the tree view
    - Archive the Chicago company (from debug > companies and do it from the
      list view)
    - Try to access the website/runbot from an incognito tab
    - It will show a raw 403 error
    
    Note that the companies can only be archives since Odoo 16, thanks to
    this commit: https://github.com/odoo/odoo/commit/7d0996bb151f68647b22719eee5ed4a4c35574bb
    
    opw-3749772
    
    closes odoo/odoo#157118
    
    X-original-commit: 9079b7038754ca4fe547086d3fe8947a4bb6ddce
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website/models/res_company.py
+++ addons/website/models/res_company.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class Company(models.Model):
@@ -18,6 +19,18 @@ class Company(models.Model):
         action = self.env["ir.actions.actions"]._for_xml_id("website.theme_install_kanban_action")
         action['target'] = 'new'
         return action
+
+    @api.constrains('active')
+    def _check_active(self):
+        super()._check_active()
+        for company in self:
+            if not company.active and company.website_id:
+                raise ValidationError(_(
+                    'The company %(company_name)r cannot be archived because it has a linked website %(website_name)r.'
+                    '\nChange that website\'s company first.',
+                    company_name=company.name,
+                    website_name=company.website_id.name
+                ))
 
     def google_map_img(self, zoom=8, width=298, height=298):
         partner = self.sudo().partner_id
