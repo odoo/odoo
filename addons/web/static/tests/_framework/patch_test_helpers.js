82f9a9ab8135632de1f216046f9bf2f8c56383dc
Julien Mougenot <jum@odoo.com>
2024-03-05 14:10:05 +0100

    [IMP] web: patchDate with Luxon
    Before this commit, the `mockDate` and `mockTimeZone` helpers exported
    by HOOT were used to mock the date and timezone in the tests.
    
    It works well on Date but not on the entire Intl API, which is used by
    Luxon to format dates.
    
    This commit adds new helpers (`patchDate` and `patchTimeZone`) that
    call the previous helpers AND patch Luxon settings' default zone.
    
    Part-of: odoo/odoo#156255

--- addons/web/static/tests/_framework/patch_test_helpers.js
+++ addons/web/static/tests/_framework/patch_test_helpers.js
@@ -1,9 +1,35 @@
 import { after } from "@odoo/hoot";
+import { mockDate, mockTimeZone } from "@odoo/hoot-mock";
 import { patch } from "@web/core/utils/patch";
+
+const { FixedOffsetZone, IANAZone, Settings } = luxon;
 
 //-----------------------------------------------------------------------------
 // Exports
 //-----------------------------------------------------------------------------
+
+/** @type {typeof mockDate} */
+export function patchDate(date, tz) {
+    mockDate(date);
+
+    if (tz !== undefined && tz !== null) {
+        patchTimeZone(tz);
+    }
+}
+
+/** @type {typeof mockTimeZone} */
+export function patchTimeZone(tz) {
+    mockTimeZone(tz);
+
+    let defaultZone;
+    if (typeof tz === "string") {
+        defaultZone = IANAZone.create(tz);
+    } else {
+        const offset = new Date().getTimezoneOffset();
+        defaultZone = FixedOffsetZone.instance(-offset);
+    }
+    patchWithCleanup(Settings, { defaultZone });
+}
 
 /** @type {typeof patch} */
 export function patchWithCleanup(obj, patchValue) {
