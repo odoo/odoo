54c8b0bd23438b4f596d723b08391a71fa059643
Dylan Kiss (dyki) <dyki@odoo.com>
2024-03-05 17:33:52 +0100

    [IMP] account: don't raise when field value is not found
    When upgrading a database and thereby upgrading a localization, we might
    reference accounts that were added in a new version, but not in the
    upgraded database for an existing company.
    
    Currently we raise an error in that case. However, it might be better to
    just skip the value we want to set in that case.
    
    task-3419066
    
    closes odoo/odoo#132696
    
    Related: odoo/enterprise#46383
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/models/chart_template.py
+++ addons/account/models/chart_template.py
@@ -427,6 +427,7 @@ class AccountChartTemplate(models.AbstractModel):
             This allows to define all the data before the records even exist in the database.
             """
             fields = ((model._fields[k], k, v) for k, v in values.items() if k in model._fields)
+            failed_fields = []
             for field, fname, value in fields:
                 if not value:
                     values[fname] = False
@@ -438,7 +439,7 @@ class AccountChartTemplate(models.AbstractModel):
                         values[fname] = self.ref(value).id if value not in ('', 'False', 'None') else False
                     except ValueError as e:
                         _logger.warning("Failed when trying to recover %s for field=%s", value, field)
-                        raise e
+                        failed_fields.append(fname)
                 elif field.type in ('one2many', 'many2many') and isinstance(value[0], (list, tuple)):
                     for i, (command, _id, *last_part) in enumerate(value):
                         if last_part:
@@ -459,6 +460,8 @@ class AccountChartTemplate(models.AbstractModel):
                         for v in value.split(',')
                         if v
                     ])]
+            for fname in failed_fields:
+                del values[fname]
             return values
 
         def defer(all_data):
