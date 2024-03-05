935a0bcd689db9b290d8d12a43f098ce7743c486
Benjamin Vray <bvr@odoo.com>
2024-03-05 15:38:24 +0100

    [FIX] web_editor: fix resize column outside of the iframe
    Steps to reproduce the bug:
    
    - Enter Website edit mode.
    - Drag and drop an "Image-Text" snippet onto the page.
    - Grab the right edge of the right column of the snippet and extend it
    to the right until it overflows the iframe and hovers over the editor
    panel.
    - Release the mouse, then hover over the iframe again.
    - Bug: The "mouseUp" event was not triggered, and the column resizing
    still occurs.
    
    This bug occurs because the "mouseUp" event was attached to the
    <body> of the iframe and not to the window of the iframe.
    
    Indeed, for mouse events to be triggered outside of an iframe, they need
    to be attached to the iframe's window.
    
    This fix is not necessary before the version 16.0. Before V16.0, the
    page preview and the options panel shared the same window. It's only
    since this commit [1], where we moved the page preview into an iframe,
    that the bug occurs.
    
    [1]: https://github.com/odoo/odoo/commit/31cc10b91dc7762e23b4bde9b945be0c4ce3fe3b
    
    taks-3562970
    
    closes odoo/odoo#157611
    
    X-original-commit: e6be732450d9ef662a48ba074e1ca1ad32e35c04
    Signed-off-by: Guillaume Dieleman (gdi) <gdi@odoo.com>
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -4511,8 +4511,8 @@ registry.sizing = SnippetOptionWidget.extend({
             self.options.wysiwyg.odooEditor.automaticStepUnactive('resizing');
 
             const cursor = $handle.css('cursor') + '-important';
-            const $body = $(this.ownerDocument.body);
-            $body.addClass(cursor);
+            const $iframeWindow = $(this.ownerDocument.defaultView);
+            $iframeWindow[0].document.body.classList.add(cursor);
 
             const bodyMouseMove = function (ev) {
                 ev.preventDefault();
@@ -4557,9 +4557,9 @@ registry.sizing = SnippetOptionWidget.extend({
                 }
             };
             const bodyMouseUp = function () {
-                $body.off('mousemove', bodyMouseMove);
-                $body.off('mouseup', bodyMouseUp);
-                $body.removeClass(cursor);
+                $iframeWindow.off("mousemove", bodyMouseMove);
+                $iframeWindow.off("mouseup", bodyMouseUp);
+                $iframeWindow[0].document.body.classList.remove(cursor);
                 $handle.removeClass('o_active');
 
                 // If we are in grid mode, removes the background grid.
@@ -4593,8 +4593,8 @@ registry.sizing = SnippetOptionWidget.extend({
                     self.options.wysiwyg.odooEditor.historyStep();
                 }, 0);
             };
-            $body.on('mousemove', bodyMouseMove);
-            $body.on('mouseup', bodyMouseUp);
+            $iframeWindow.on("mousemove", bodyMouseMove);
+            $iframeWindow.on("mouseup", bodyMouseUp);
         });
 
         for (const [key, value] of Object.entries(resizeValues)) {
