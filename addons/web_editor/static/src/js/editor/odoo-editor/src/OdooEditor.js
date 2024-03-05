8ee9f55fb98a8076e1d5c76576f6629bf4ee2220
David Monjoie <dmo@odoo.com>
2024-03-05 14:09:24 +0100

    [FIX] web_editor: fix iOS dictation issues
    In iOS, there is a dictation feature accessible from the keyboard.
    This feature modifies the DOM directly and triggers input events.
    
    From observations made on [1], it seems it triggers each event
    twice for unknown reason. Since there is custom editor code bound
    on this event, the `insertText` function  was called twice, thus
    resulting in the text being duplicated.
    
    Note that the bug is in fact subtle because, if the selection
    reported by iOS was always accurate when triggering those events,
    then calling `insertText` twice would have no visual effect.
    However, in the case where the user chooses to manually stop the
    dictation mechanism through the dedicated button on the keyboard
    before it has finished writing the whole sentence, then the
    selection is not updated accordingly and the second call to
    `insertText` ends up inserting the text at the wrong place, thus
    triggering the symptom of duplicating the text.
    
    This commit fixes the issue by restricting the cases where a
    manual call to `insertText` is needed. The previous comment
    specified that the only case in which it was needed was when
    some text was selected. This is not entirely true. The only case
    in which it is needed is when text is selected in a way that
    spans multiple block elements, as this is the only case where the
    browser can alter those nodes by removing or merging them.
    
    Note that the first line of the `insertText` conditional branch,
    the one that fetches the current selection, actually looks fishy.
    It is possible the bug is caused by the fact that this selection
    is used instead of `this._currentStep.selection`. That being said,
    changing that part of the code in stable would not be worth the
    risk of breaking something that might rely on it, especially
    considering the dictation on iOS is a pretty niche feature.
    
    Also note that, for some reason, the issue only happens when the
    paragraph was empty when the dictation started. If some previous
    text was already present in the paragraph, for example from a
    previous dictation test, then the duplication will not occur. In
    this case however, a traceback can sometimes occur due to the
    fact that when checking for a potential url match, the `pop`
    method is called on an array multiple times then the result value
    is used as an object even though no check were made to make sure
    that the return value was not `undefined` because the array had
    nothing more to pop.
    
    This commit fixes that second issue by adding the missing check.
    
    task-3374520
    opw-3167676
    
    [1]: https://w3c.github.io/uievents/tools/key-event-viewer-ce.html
    
    X-original-commit: e1d296bf50c3e4d9cf6699f2ff26c5eebf11b831
    Part-of: odoo/odoo#164301

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -3764,15 +3764,21 @@ export class OdooEditor extends EventTarget {
             } else if (['insertText', 'insertCompositionText'].includes(ev.inputType)) {
                 // insertCompositionText, courtesy of Samsung keyboard.
                 const selection = this.document.getSelection();
-                // Detect that text was selected and change behavior only if it is the case,
-                // since it is the only text insertion case that may cause problems.
-                const wasTextSelected = anchorNodeOid !== focusNodeOid || anchorOffset !== focusOffset;
+                // Detect a prior selection across different blocks and change
+                // the behavior only in that case, since it is the only text
+                // insertion case that may cause problems.
+                const anchorNode = this.idFind(anchorNodeOid);
+                const focusNode = this.idFind(focusNodeOid);
+                const wasSelectingAcrossDifferentBlocks =
+                    anchorNode &&
+                    focusNode &&
+                    closestBlock(anchorNode) !== closestBlock(focusNode);
                 // Unit tests events are not trusted by the browser,
                 // the insertText has to be done manualy.
                 const isUnitTests = !ev.isTrusted && this.testMode;
                 // we cannot trust the browser to keep the selection inside empty tags.
                 const latestSelectionInsideEmptyTag = this._isLatestComputedSelectionInsideEmptyInlineTag();
-                if (wasTextSelected || isUnitTests || latestSelectionInsideEmptyTag) {
+                if (wasSelectingAcrossDifferentBlocks || isUnitTests || latestSelectionInsideEmptyTag) {
                     ev.preventDefault();
                     if (!isUnitTests) {
                         // First we need to undo the character inserted by the browser.
@@ -4896,7 +4902,7 @@ export class OdooEditor extends EventTarget {
             selection.anchorNode.parentNode.normalize();
             const textSliced = selection.anchorNode.textContent.slice(0, selection.anchorOffset);
             const textNodeSplitted = textSliced.split(/\s/);
-            const potentialUrl = textNodeSplitted.pop();
+            const potentialUrl = textNodeSplitted.pop() || '';
             // In case of multiple matches, only the last one will be converted.
             const match = [...potentialUrl.matchAll(new RegExp(URL_REGEX, 'g'))].pop();
 
