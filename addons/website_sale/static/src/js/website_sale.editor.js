556ae457b02e9c077d09fa9c3f9f1e6c6e26b345
Arthur Detroux (ard) <ard@odoo.com>
2024-03-06 10:04:51 +0100

    [FIX] website_sale, *: only reload once on options changes
    *: web_editor, website
    
    Commit [1] introduced new image settings for the product page.
    When those settings were introduced, the editor was reloaded after the
    RPC to properly reflect when the settings were changed. While this was
    done, a data-reload was also added in the XML template of the options.
    This was not necessary as the data-reload only works for some methods
    which are not used in the image settings. Therefore, the data-reload
    did nothing.
    
    However, with commit [2], the settings were moved and combined with
    page options. So the image settings automatically inherited the page
    options behavior, which is to save and reload the page when a method
    with data-reload is called. This caused the editor to ask for a reload
    twice.
    
    This could cause the page to reload before the RPC was finished:
    - Add a 10 seconds sleep at the start of /shop/config/website controller
    - Go to products page
    - Enter edit mode
    - Change the number of products per row
    => Nothing happens
    Indeed, the page is reloaded immediately, before the 10 seconds rpc is
    actually done.
    
    This is also needed by the conversion of the SnippetsMenu to OWL:
    It seems like the race condition introduces a traceback.
    
    This commit fixes the issue by ignoring any save request from an option
    that will reload anyway. It also awaits the RPC to finish before
    reloading the page.
    
    [1]: https://github.com/odoo/odoo/commit/54c6d36cfbea31fe60b888bbb903b0c6f22216b3
    [2]: https://github.com/odoo/odoo/commit/b274cf2427951761e59eb357fb796b54be375507#diff-754f6c793d6a168d006d2a9da108142b889036031ac1a2e62060c225d060f858
    
    closes odoo/odoo#156658
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website_sale/static/src/js/website_sale.editor.js
+++ addons/website_sale/static/src/js/website_sale.editor.js
@@ -249,7 +249,7 @@ options.registry.WebsiteSaleGridLayout = options.Class.extend({
      */
     setPpr: function (previewMode, widgetValue, params) {
         this.ppr = parseInt(widgetValue);
-        this._rpc({
+        return this._rpc({
             route: '/shop/config/website',
             params: {
                 'shop_ppr': this.ppr,
@@ -261,7 +261,7 @@ options.registry.WebsiteSaleGridLayout = options.Class.extend({
      */
     setDefaultSort: function (previewMode, widgetValue, params) {
         this.default_sort = widgetValue;
-        this._rpc({
+        return this._rpc({
             route: '/shop/config/website',
             params: {
                 'shop_default_sort': this.default_sort,
@@ -700,7 +700,9 @@ options.registry.WebsiteSaleProductPage = options.Class.extend({
     },
 
     _updateWebsiteConfig(params) {
-        this._rpc({
+        // TODO: Remove the request_save in master, it's already done by the
+        // data-page-options set to true in the template.
+        return this._rpc({
             route: '/shop/config/website',
             params,
         }).then(() => this.trigger_up('request_save', {reload: true, optionSelector: this.data.selector}));
@@ -719,11 +721,11 @@ options.registry.WebsiteSaleProductPage = options.Class.extend({
         const zoomOption = this._getZoomOptionData();
         const updateWidth = this._updateWebsiteConfig.bind(this, { product_page_image_width: widgetValue });
         if (!zoomOption || widgetValue !== "100_pc") {
-            updateWidth();
+            await updateWidth();
         } else {
             const defaultZoomOption = "website_sale.product_picture_magnify_click";
             await this._customizeWebsiteData(defaultZoomOption, { possibleValues: zoomOption._methodsParams.optionsPossibleValues["customizeWebsiteViews"] }, true);
-            updateWidth();
+            await updateWidth();
         }
     },
 
@@ -734,7 +736,7 @@ options.registry.WebsiteSaleProductPage = options.Class.extend({
         const zoomOption = this._getZoomOptionData();
         const updateLayout = this._updateWebsiteConfig.bind(this, { product_page_image_layout: widgetValue });
         if (!zoomOption) {
-            updateLayout();
+            await updateLayout();
         } else {
             const imageWidthOption = this.productDetailMain.dataset.image_width;
             let defaultZoomOption = widgetValue === "grid" ? "website_sale.product_picture_magnify_click" : "website_sale.product_picture_magnify_hover";
@@ -742,7 +744,7 @@ options.registry.WebsiteSaleProductPage = options.Class.extend({
                 defaultZoomOption = "website_sale.product_picture_magnify_click";
             }
             await this._customizeWebsiteData(defaultZoomOption, { possibleValues: zoomOption._methodsParams.optionsPossibleValues["customizeWebsiteViews"] }, true);
-            updateLayout();
+            await updateLayout();
         }
     },
 
@@ -832,23 +834,27 @@ options.registry.WebsiteSaleProductPage = options.Class.extend({
             2: 'medium',
             3: 'big',
         }[widgetValue];
-        this._rpc({
+        this.productPageGrid.dataset.image_spacing = spacing;
+        // TODO: Remove the request_save in master, it's already done by the
+        // data-page-options set to true in the template.
+        return this._rpc({
             route: '/shop/config/website',
             params: {
                 'product_page_image_spacing': spacing,
             },
         }).then(() => this.trigger_up('request_save', {reload: true, optionSelector: this.data.selector}));
-        this.productPageGrid.dataset.image_spacing = spacing;
     },
 
     setColumns(previewMode, widgetValue, params) {
-        this._rpc({
+        this.productPageGrid.dataset.grid_columns = widgetValue;
+        // TODO: Remove the request_save in master, it's already done by the
+        // data-page-options set to true in the template.
+        return this._rpc({
             route: '/shop/config/website',
             params: {
                 'product_page_grid_columns': widgetValue,
             },
         }).then(() => this.trigger_up('request_save', {reload: true, optionSelector: this.data.selector}));
-        this.productPageGrid.dataset.grid_columns = widgetValue;
     },
 
     /**
