269dc3b58f182c9306aec1022f51e43b96c4d740
Jeremy Kersten <jke@odoo.com>
2024-03-06 14:26:08 +0100

    [FIX] website_blog: redirect /blog/x/post/y simplify
    Don't try to keep extra params and co. Keep it simple...
    
    Else we should pop from request.params `blog` and `post` keys because
    now they are converted as query param with the slug format:
    
    /blog/name-1/post-1?blog=blog.blog(1,)&question=blog.post(1,)
    
    closes odoo/odoo#156890
    
    Signed-off-by: Jérémy Kersten <jke@odoo.com>

--- addons/website_blog/controllers/main.py
+++ addons/website_blog/controllers/main.py
@@ -244,11 +244,11 @@ class WebsiteBlog(http.Controller):
         return r
 
     @http.route([
-        '''/blog/<model("blog.blog"):blog>/post/<model("blog.post", "[('blog_id','=',blog.id)]"):blog_post>''',
+        '''/blog/<model("blog.blog"):blog>/post/<model("blog.post"):blog_post>''',
     ], type='http', auth="public", website=True, sitemap=False)
-    def old_blog_post(self, blog, blog_post, tag_id=None, page=1, enable_editor=None, **post):
+    def old_blog_post(self, blog, blog_post, **post):
         # Compatibility pre-v14
-        return request.redirect(_build_url_w_params("/blog/%s/%s" % (slug(blog), slug(blog_post)), request.params), code=301)
+        return request.redirect("/blog/%s/%s" % (slug(blog), slug(blog_post)), code=301)
 
     @http.route([
         '''/blog/<model("blog.blog"):blog>/<model("blog.post", "[('blog_id','=',blog.id)]"):blog_post>''',
