e1db22fc8a054dfc770e76564bbfa704fb98f9c1
Thibault Delavallée <tde@odoo.com>
2024-03-06 15:49:37 +0100

    [FIX] mail, various: be defensive with activity types, define master data
    In general, be defensive with activity types: avoid crash when activity
    type has been removed, try to gracefully recover from non existing
    data, protect types linked to business code that should not be unlinked
    or changed from model.
    
    Introduce a generic way to mark some activity types as master data
     * model is fixed and should not be modified, because it is linked
       to specific flows e.g. todo should be generic;
     * data should not be unlinked, because it is used in automated flows
       like plans, business code, ... and cannot easily be replaced;
    
    Mail: make "Call", "Meeting" and "Todo" activity types master data users
    cannot remove as they are required in various flows: fleet, plans,
    voip, ... Also force their model to be False (aka be cross model).
    
    Mail: make "Warning" and "Upload document" activity types in addition
    to "Call" and "Todo", always cross model. As they are used in various
    apps it should not be specific to a model.
    
    Hr holidays: prevent from modifying leave activity types, as they are
    used in business flows and in automated code.
    
    Fleet: prevent from modifying contract activity type (same reason).
    
    Task-3777606
    
    X-original-commit: odoo/odoo@10d16344449fafe1646e085c05233ecab7df48aa
    Part-of: odoo/odoo#218001
    Related: odoo/enterprise#89790
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ addons/hr_holidays/models/mail_activity_type.py
@@ -0,0 +1,14 @@
+from odoo import api, models
+
+
+class MailActivityType(models.Model):
+    _inherit = "mail.activity.type"
+
+    @api.model
+    def _get_model_info_by_xmlid(self):
+        info = super()._get_model_info_by_xmlid()
+        info['hr_holidays.mail_act_leave_approval'] = {'res_model': 'hr.leave', 'unlink': False}
+        info['hr_holidays.mail_act_leave_second_approval'] = {'res_model': 'hr.leave', 'unlink': False}
+        info['hr_holidays.mail_act_leave_allocation_approval'] = {'res_model': 'hr.leave.allocation', 'unlink': False}
+        info['hr_holidays.mail_act_leave_allocation_second_approval'] = {'res_model': 'hr.leave.allocation', 'unlink': False}
+        return info
