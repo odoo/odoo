587d6900284896010ca8c824ae72d9a512b307db
Pierre Paridans <app@odoo.com>
2024-03-07 18:10:41 +0100

    [REF] website,web: Table of Content snippet's ScrollSpy implementation
    Since ScrollSpy's rewrite in Bootstrap 5.2, the IntersectionObverser's
    implementation doesn't match our needs for the Table of Content snippet
    anymore.
    
    This commit reimplements the desired behavior by applying the same logic
    as in Bootstrap 5.1 ScrollSpy in the snippet itself, not relying anymore
    on the BS implementation.
    
    Also, it allows us to remove the differents overrides/hacks applied on the
    BS implementation (cf. `web/static/src/legacy/js/libs/bootstrap.js`).
    
    task-3600468

--- addons/web/static/src/legacy/js/libs/bootstrap.js
+++ addons/web/static/src/legacy/js/libs/bootstrap.js
@@ -88,48 +88,6 @@ Tooltip.prototype.show = function () {
     }
 };
 
-/* Bootstrap scrollspy fix for non-body to spy */
-
-const bootstrapSpyRefreshFunction = ScrollSpy.prototype.refresh;
-ScrollSpy.prototype.refresh = function () {
-    bootstrapSpyRefreshFunction.apply(this, arguments);
-    if (this._scrollElement === window || this._config.method !== 'offset') {
-        return;
-    }
-    const baseScrollTop = this._getScrollTop();
-    for (let i = 0; i < this._offsets.length; i++) {
-        this._offsets[i] += baseScrollTop;
-    }
-};
-
-/**
- * In some cases, we need to keep the first element of navbars selected.
- */
-const bootstrapSpyProcessFunction = ScrollSpy.prototype._process;
-ScrollSpy.prototype._process = function () {
-    bootstrapSpyProcessFunction.apply(this, arguments);
-    if (this._activeTarget === null && this._config.alwaysKeepFirstActive) {
-        this._activate(this._targets[0]);
-    }
-};
-
-/**
- * With bootstrap 5, navigation elements must be in the DOM and be visible.
- * Since in the website editor, the user can hide the table of content block in
- * many different ways, it happens that the navigation element is no longer
- * found by bootstrap. We don't want to dispose scrollspy everywhere the block
- * could be hidden. So this patch imitates the behavior of bootstrap 4.X by not
- * causing an error if the navigation element is not found.
- */
-const bootstrapSpyActivateFunction = ScrollSpy.prototype._activate;
-ScrollSpy.prototype._activate = function (target) {
-    const element = document.querySelector(`[href="${target}"]`);
-    if (!element || $(element).is(':hidden')) {
-        return;
-    }
-    bootstrapSpyActivateFunction.apply(this, arguments);
-};
-
 /**
  * Bootstrap disables dynamic dropdown positioning when it is in a navbar. Here
  * we make this patch to activate this dynamic navbar's dropdown positioning
