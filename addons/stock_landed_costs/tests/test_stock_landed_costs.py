cc55eeb89a5765f7b5399fdca0b46aca81b510fe
Walid <waha@odoo.com>
2024-03-07 17:48:37 +0100

    [FIX] stock_landed_costs: correct move qty
    Steps to reproduce:
    - Create an MO qty = 1000
    - Only produce 900
    - Create a landed cost for that MO
    
    Bug:
    landed cost is applied on 1000 units instead of 900
    starting V17 definition of the field (product_ty) has been changed
    to reflect the intial demand
    https://github.com/odoo/odoo/commit/f9867a5fa572a15fb89c49c61e569427d6388cbc
    
    apply same fix as in:
    https://github.com/odoo/odoo/pull/137864/files#diff-d41327f63c4c3d8d369a3f8622f794aa03f79f5f93d9ec01dfa5c179fd8f31eaR159-R162
    
    opw-3696385
    
    X-original-commit: 08195849ad78df0297b932efeb749f137401e911

--- addons/stock_landed_costs/tests/test_stock_landed_costs.py
+++ addons/stock_landed_costs/tests/test_stock_landed_costs.py
@@ -45,7 +45,7 @@ class TestStockLandedCosts(TestStockLandedCostsCommon):
             'picking_type_id': self.warehouse.out_type_id.id,
             'move_ids': [(0, 0, {
                 'product_id': product_landed_cost_1.id,
-                'product_uom_qty': 5,
+                'product_uom_qty': 15,
                 'product_uom': self.ref('uom.product_uom_unit'),
                 'location_id': self.warehouse.lot_stock_id.id,
                 'location_dest_id': self.ref('stock.stock_location_customers'),
@@ -59,6 +59,7 @@ class TestStockLandedCosts(TestStockLandedCostsCommon):
         picking_landed_cost_1 = self.env['stock.picking'].create(vals)
 
         # Confirm and assign picking
+        picking_landed_cost_1.picking_type_id.create_backorder = 'never'
         self.env.company.anglo_saxon_accounting = True
         picking_landed_cost_1.action_confirm()
         picking_landed_cost_1.action_assign()
