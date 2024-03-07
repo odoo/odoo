9312a988cf48a20641377ad55ec4f59a66317156
unknown <hemeshduggana@gmail.com>
2024-03-07 13:20:10 +0530

    first commit

---
+++ addons/hemesh python/day 12.py
@@ -0,0 +1,24 @@
+#using a list to store employee data
+employee_data =[
+    ('Alice','morning','finance'),
+    ('Bob','Afternooon','Finance'),
+    ('Carlie','evenig','market'),
+    #Add more employee as needed
+]
+#using a tipple to sstore addition employee details
+additional_employee_data = ('david','morning','human resource')
+#using a dictionary to store employee information
+employee_info_dict = {
+    'Alice':{'shhift','morning','domain','IT'},
+    'Bob':{'shift','afternoon','Domain','finaance'},
+    'charle':{'shift','evening','domain','market'},
+    #Add more employee as needed
+}
+
+#using a set to unique domains
+unique_domain_set ={'IT','Finance','marketing'}
+#display stored data
+print("employee data:",employee_data)
+print("additional employee data:",additional_employee_data)
+print("employee info dict:",employee_info_dict)
+print("unique domain set:",unique_domain_set)
