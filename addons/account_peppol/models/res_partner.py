084088e9c902a8ab3379b514ab60d24bd0e3e0b3
aliya <alta@odoo.com>
2024-03-07 10:17:26 +0000

    [FIX] account_peppol: don't validate partners without id
    If a partner doesn't have peppol eas or endpoint set, we still go through with checking participant's registration. This is not an issue if checking on a partner form, as the Verify button is invisible without these details being filled in. However, in the partner list view the button is always visible and it is possible to run verification for non-peppol customers. As a result, all of them show up as valid Peppol participants.
    
    With this commit we only run verification if there are eas and endpoint details.
    
    opw-3784945
    
    closes odoo/odoo#157089
    
    X-original-commit: 8948bd630544d20085d043603cb8155c1a7e24a8
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Aliya Tastemirova (alta) <alta@odoo.com>

--- addons/account_peppol/models/res_partner.py
+++ addons/account_peppol/models/res_partner.py
@@ -90,7 +90,10 @@ class ResPartner(models.Model):
         """
         self.ensure_one()
 
-        edi_identification = f'{self.peppol_eas}:{self.peppol_endpoint}'.lower()
-        self.account_peppol_validity_last_check = fields.Date.context_today(self)
-        self.account_peppol_is_endpoint_valid = self._check_peppol_participant_exists(edi_identification)
+        if not self.peppol_eas and self.peppol_endpoint:
+            self.account_peppol_is_endpoint_valid = False
+        else:
+            edi_identification = f'{self.peppol_eas}:{self.peppol_endpoint}'.lower()
+            self.account_peppol_validity_last_check = fields.Date.context_today(self)
+            self.account_peppol_is_endpoint_valid = self._check_peppol_participant_exists(edi_identification)
         return False
