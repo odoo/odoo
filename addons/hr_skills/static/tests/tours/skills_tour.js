1bded2d7baad7e404d43f585590347158a1e364b
Mélanie <mepe@odoo.com>
2024-03-07 15:22:20 +0100

    [FIX] hr_skills : failling tour
    Context:
    ========
    
    The underterministic error could can from the wizard view :
        1- You click on save button
        2- You close the wizard
    
    If you close the wizard after saving the view, the form view will be dirty instead of saved.
    
    task: 3790876

--- addons/hr_skills/static/tests/tours/skills_tour.js
+++ addons/hr_skills/static/tests/tours/skills_tour.js
@@ -106,6 +106,11 @@ tour.register('hr_skills_tour', {
         run: "click",
     },
     {
+        content: "Check if item is added",
+        trigger: ".o_data_row td.o_data_cell:contains('Fortunate Son')",
+        run: () => {},
+    },
+    {
         content: "Add a new Skill",
         trigger: ".o_field_skills_one2many button:contains('ADD')",
     },
@@ -134,6 +139,11 @@ tour.register('hr_skills_tour', {
         trigger: ".o_form_button_save",
         in_modal: true,
         run: "click",
+    },
+    {
+        content: "Check if item is added",
+        trigger: ".o_data_row td.o_data_cell:contains('Fortunate Son')",
+        run: () => {},
     },
     ...tour.stepUtils.saveForm(),
     {
