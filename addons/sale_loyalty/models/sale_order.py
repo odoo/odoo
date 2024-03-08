5888145b6cc27ccf0c963cf00971d386f0433a11
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2024-03-08 16:00:38 +0100

    [FIX] sale_loyalty: cheapest product discountable
    Create a Loyalty program:
    - Program Type: Loyalty Cards
    - Rules: 0.01 per $ spent
    - Reward: 1.00 per point on cheapest product
    Make a SO with total 339
    Loyalty Card will get 3.39 points
    Make a SO with line having
    - Unit Price 140
    - Tax 15%
    - Total will be 161
    Hit Promotions
    
    Issue:
    The system will add a discount line with unit price 5.0.
    With 15% taxes applied the total discount will be 5.75
    While it should be 5
    Order 1, Total with tax 339 => 3.39 loyalty points
    Order 2, Total with tax 161 => 1.41 loyalty points from current order
                                 + 3.39 from past order
                                 = 5.00 total loyalty points
    When applying the discount on Order 2 the system converts 5 points to 5$,
    but considering 15% tax included, the price unit of the discount line
    should be 4.35
    
    opw-3642795
    
    closes odoo/odoo#157125
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale_loyalty/models/sale_order.py
+++ addons/sale_loyalty/models/sale_order.py
@@ -202,9 +202,11 @@ class SaleOrder(models.Model):
         assert reward.discount_applicability == 'cheapest'
 
         cheapest_line = self._cheapest_line()
-        discountable = cheapest_line.price_unit * (1 - (cheapest_line.discount or 0) / 100)
+        discountable = cheapest_line.price_total
+        discountable_per_taxes = cheapest_line.price_unit * (1 - (cheapest_line.discount or 0) / 100)
         taxes = cheapest_line.tax_id.filtered(lambda t: t.amount_type != 'fixed')
-        return discountable, {taxes: discountable}
+
+        return discountable, {taxes: discountable_per_taxes}
 
     def _get_specific_discountable_lines(self, reward):
         """
