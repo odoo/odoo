5b0a27082adef6338cffc1d787d839bd47b529c9
Mohammed Basioni <basm@odoo.com>
2024-03-08 14:20:55 +0200

    [IMP] t9n: add tab in the menu to list available projects.
    This commit adds a tab in the  menu bar that lists the available
    projects. Also, the form view of the project is configured to have
    two tabs: one for resources and the other one for the description
    of the project. Also, some security permissions have been included
    in the manifest file.

--- addons/t9n/models/project.py
+++ addons/t9n/models/project.py
@@ -1,17 +1,20 @@
-from odoo import fields, models
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class Project(models.Model):
-    """ A project is a collection of Resources to be localized into a given set
-        of Languages.
+    """A project is a collection of Resources to be localized into a given set
+    of Languages.
     """
+
     _name = "t9n.project"
     _description = "Translation project"
 
+    name = fields.Char("Project", required=True)
     src_lang_id = fields.Many2one(
         comodel_name="t9n.language",
         string="Source Language",
-        help="The original language of the messages you want to translate."
+        help="The original language of the messages you want to translate.",
     )
     resource_ids = fields.One2many(
         comodel_name="t9n.resource",
@@ -23,3 +26,11 @@ class Project(models.Model):
         string="Languages",
         help="The list of languages into which the project can be translated.",
     )
+
+    @api.constrains("src_lang_id", "target_lang_ids")
+    def _check_source_and_target_languages(self):
+        for record in self:
+            if record.src_lang_id in record.target_lang_ids:
+                raise ValidationError(
+                    _("Target languages must be different from source language.")
+                )
