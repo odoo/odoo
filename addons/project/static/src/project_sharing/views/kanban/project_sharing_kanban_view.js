1dfdf1aaab0d995281f51a9ae33456c6a36bd144
Prakash Prajapati <ppr@odoo.com>
2024-03-11 17:54:21 +0530

    [IMP] project: add 'project' breadcrumb in  project sharing
    Currently, if the user wants to navigate back to the 'all projects'
     view from the project sharing view, they have to manually add
     'my/projects' to the browser's URL
    
    In this commit, we have added the project to the first position
    in the breadcrumb. As a result, the project will be add in the
    breadcrumb, allowing the user to easily navigate back.
    
    task-3368896

---
+++ addons/project/static/src/project_sharing/views/kanban/project_sharing_kanban_view.js
@@ -0,0 +1,5 @@
+import { kanbanView } from '@web/views/kanban/kanban_view';
+
+import { ProjectSharingControlPanel } from '../../components/control_panel/project_sharing_control_panel';
+
+kanbanView.ControlPanel = ProjectSharingControlPanel;
