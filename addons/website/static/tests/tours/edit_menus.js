c4878a6ac4fff827b75fe891ba5e0860d7490dbe
Antoine Guenet <age@odoo.com>
2024-03-11 12:24:43 +0100

    [FIX] web_editor: pad links with zero-width non-breaking spaces
    This implements a new approach to solve selection issues around links
    (allowing the cursor at the inner _and_ outer edges of links).
    
    In the sanitization process, every link now receives 4 zero-width
    non-breaking spaces (unicode FEFF characters, hereafter referred to as
    ZWNBSP):
    - one before the link
    - one as the link's first child
    - one as the link's last child
    - one after the link
    like so: `//ZWNBSP//<a>//ZWNBSP//label//ZWNBSP//</a>//ZWNBSP`.
    
    An advantage of ZWNBSP over regular ZWSP (unicode 200B) is they're less
    likely to be used deliberately by the user, so much so that we can
    safely assume all of them are technical and can be removed
    indiscriminately. ZWSP and ZWNBSP are used to mark a separation between
    words in languages that don't use spaces for that purpose (eg, Lao).
    ZWNBSP are to ZWSP what NBSP (unicode 00A0) are to regular spaces.
    
    Because of that advantage, we don't need to track the ZWNBSP (so there
    is no need to wrap them in `span` elements), simplifying the code
    considerably. We therefore now remove all ZWNBSP when saving.
    
    There is a possibility to introduce "orphaned" ZWNBSP during the editing
    process, if for instance the link has a big enough padding or margin
    that it's possible to click between two of a link's ZWNBSP (one outer,
    the other inner). Inserting a character or a paragraph break in such a
    position will move the ZWNBSP to a place where it's not useful anymore.
    The sanitizer will then reintroduce the useful ZWNBSP in their rightful
    places. We remove the orphaned ZWNBSP from the sanitizer whenever that
    is possible without risking to break the selection.
    
    This is not applied in the following cases:
    
    - in a navbar (since its links are managed via the snippets system, not
      via pure edition) and, similarly, in .nav-link links
    - in links that have content more complex than simple text (as was the
      case with the previous implementation)
    - on non-editable links or links that are not within the editable area
    
    task-3604728
    
    Co-authored-by: Sébastien Geelen <sge@odoo.com>

--- addons/website/static/tests/tours/edit_menus.js
+++ addons/website/static/tests/tours/edit_menus.js
@@ -93,6 +93,11 @@ wTourUtils.registerWebsitePreviewTour('edit_menus', {
         trigger: 'iframe #top_menu .nav-item a:contains("Random!")',
     },
     {
+        content: "navbar shouldn't have any zwnbsp and no o_link_in_selection class",
+        trigger: 'iframe nav.navbar:not(:has(.o_link_in_selection)):not(:contains("\ufeff"))',
+        run: () => {}, // It's a check.
+    },
+    {
         content: "Click on Edit Link",
         trigger: 'iframe .o_edit_menu_popover a.o_we_edit_link',
     },
