04885db7b7eb1fbba2b7de2b3fa04b242478a391
Mélanie <mepe@odoo.com>
2024-03-11 10:48:48 +0100

    [IMP] account_fleet : add link to bill in fleet
    With this commit, the bill's amount is correctly computed.
    If the service log is generated by a bill, the cost is readonly to be
    consistent.
    If the service log is generated by a bill, it can't be deleted.
    
    The bill smartbutton's color is related to the bill's state.
    
    closes odoo/odoo#157245
    
    Task: 3783017
    Related: odoo/enterprise#60818
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/account_fleet/models/__init__.py
+++ addons/account_fleet/models/__init__.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import account_move
 from . import fleet_vehicle
+from . import fleet_vehicle_log_services
