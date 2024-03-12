5edf3ea8437c3eb8124acc6bb6e1d29f9d85d9dd
phddevelopment <155246606+Peacehomesdevelopment@users.noreply.github.com>
2024-03-12 21:54:19 +0400

    Add files via upload

---
+++ bi_professional_reports_templates-17.0.0.0/bi_professional_reports_templates/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from . import models
+
---
+++ bi_professional_reports_templates-17.0.0.0/bi_professional_reports_templates/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+from . import res_company
+from . import ir_actions_report
+
