7c59e055953aca2a9a3da8a829ebd46b66b23699
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-03-12 14:09:18 +0100

    [FIX] web_editor: expand tablepicker in correct direction
    Issue:
    ======
    Table picker isn't intuitive in rlt language and doesn't expand in the
    correct direction.
    
    Steps to reproduce the issue:
    =============================
    - Install arabic language
    - Go to notes and create a new one
    - Write `/` and choose table
    - Use left/right arrow keys to see how the table expand.
    
    Solution:
    =========
    For rtl direction it's better to make left for increase and right for
    decrease since that's the direction of the langauge. Also the table
    should be fixed on the right and expand on the left. (exactly the
    opposite of ltr direction).
    
    task-3721794
    
    X-original-commit: 9d4c937e67cb72ff7152e01cda23255bf3c4d344
    Part-of: odoo/odoo#159643

--- addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
@@ -2813,6 +2813,7 @@ export function getRangePosition(el, document, options = {}) {
     const selection = document.getSelection();
     if (!selection.rangeCount) return;
     const range = selection.getRangeAt(0);
+    const isRtl = options.direction === 'rtl';
 
     const marginRight = options.marginRight || 20;
     const marginBottom = options.marginBottom || 20;
@@ -2840,6 +2841,18 @@ export function getRangePosition(el, document, options = {}) {
         clonedRange.detach();
     }
 
+    if (isRtl) {
+        // To handle the RTL case we shift the elelement to the left by its size
+        // and handle it the same as left.
+        offset.right = offset.left - el.offsetWidth;
+        const leftMove = Math.max(0, offset.right + el.offsetWidth + marginLeft - window.innerWidth);
+        if (leftMove && offset.right - leftMove > marginRight) {
+            offset.right -= leftMove;
+        } else if (offset.right - leftMove < marginRight) {
+            offset.right = marginRight;
+        }
+    }
+
     const leftMove = Math.max(0, offset.left + el.offsetWidth + marginRight - window.innerWidth);
     if (leftMove && offset.left - leftMove > marginLeft) {
         offset.left -= leftMove;
@@ -2850,6 +2863,9 @@ export function getRangePosition(el, document, options = {}) {
     if (options.parentContextRect) {
         offset.left += options.parentContextRect.left;
         offset.top += options.parentContextRect.top;
+        if (isRtl) {
+            offset.right += options.parentContextRect.left;
+        }
     }
 
     if (
@@ -2864,6 +2880,13 @@ export function getRangePosition(el, document, options = {}) {
     if (offset) {
         offset.top += window.scrollY;
         offset.left += window.scrollX;
+        if (isRtl) {
+            offset.right += window.scrollX;
+        }
+    }
+    if (isRtl) {
+        // Get the actual right value.
+        offset.right = window.innerWidth - offset.right - el.offsetWidth;
     }
 
     return offset;
