aa3815c29fee0db77eeef119184b0a015f512f21
tsm-odoo <tsm@odoo.com>
2024-03-12 11:28:01 +0100

    [FIX] mail: fix non deterministic crosstab test
    The `Channel subscription is renewed when channel is manually added`
    sometimes fails because the first assertion relies on the second the
    channel is created. This assertion is not relevant in the context of
    this test. This PR removes it.
    
    fix runbot-58155
    
    closes odoo/odoo#157361
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/tests/discuss/core/web/crosstab.test.js
+++ addons/mail/static/tests/discuss/core/web/crosstab.test.js
@@ -12,10 +12,7 @@ QUnit.module("crosstab");
 
 test("Channel subscription is renewed when channel is manually added", async () => {
     const pyEnv = await startServer();
-    pyEnv["discuss.channel"].create([
-        { name: "my channel" },
-        { name: "General", channel_member_ids: [] },
-    ]);
+    pyEnv["discuss.channel"].create({ name: "General", channel_member_ids: [] });
     const { env } = await start();
     patchWithCleanup(env.services["bus_service"], {
         forceUpdateChannels() {
@@ -23,8 +20,6 @@ test("Channel subscription is renewed when channel is manually added", async () 
         },
     });
     await openDiscuss();
-    await contains(".o-mail-DiscussSidebarChannel", { text: "my channel" });
-    await assertSteps(["update-channels"]);
     await click("[title='Add or join a channel']");
     await insertText(".o-discuss-ChannelSelector input", "General");
     await click(":nth-child(1 of .o-discuss-ChannelSelector-suggestion)");
