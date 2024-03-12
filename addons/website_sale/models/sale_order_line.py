3ab238bd15c296afc93edf03b3d62573a11501c4
Abdelouahab (abla) <abla@odoo.com>
2024-03-12 10:46:19 +0100

    [FIX] website_sale: fix cart price discount calculation
    To reproduce:
    =============
    
    - In the Settings: Product prices displayed on the website set as "Tax Included".
    - Created a test product (for example price 100$ tax 15%).
    - Created a price list with fixed price 15$ for all products
    - Pricelist configured as "Selectable" and set to "Show public price & discount to the customer".
    - on the product shop page, the price is 17.25$ instead of 115$ (which is the correct).
    - add product to cart and check the cart price, it's 17.25$ instead of 100$.
    
    Problem:
    ========
    
    The undiscounted price displayed on the cart didn't consider the website
    configuration (tax excluded/included).
    
    Solution:
    =========
    
    Consider the website configuration to show the expected undiscounted
    amount.
    
    opw-3771242
    
    Co-authored-by: Victor Feyens <vfe@odoo.com>

--- addons/website_sale/models/sale_order_line.py
+++ addons/website_sale/models/sale_order_line.py
@@ -70,6 +70,14 @@ class SaleOrderLine(models.Model):
             self.shop_warning = ''
         return warn
 
+    def _get_displayed_unit_price(self):
+        show_tax = self.order_id.website_id.show_line_subtotals_tax_selection
+        tax_display = 'total_excluded' if show_tax == 'tax_excluded' else 'total_included'
+
+        return self.tax_id.compute_all(
+            self.price_unit, self.currency_id, 1, self.product_id, self.order_partner_id,
+        )[tax_display]
+
     def _show_in_cart(self):
         self.ensure_one()
         # Exclude delivery line from showing up in the cart
