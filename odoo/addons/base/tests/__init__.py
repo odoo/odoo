afdbad8e384a62cd3c4930e055a913d186afb76f
Xavier Morel <xmo@odoo.com>
2024-03-12 15:50:05 +0100

    [FIX] base: allow finding states by `display_name`
    That is how states are (might be?) exported, so it should be possible
    to find them the same way.
    
    Task-Id: 3644762

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -41,6 +41,7 @@ from . import test_translate
 from . import test_user_has_group
 from . import test_views
 from . import test_xmlrpc
+from . import test_res_country
 from . import test_res_partner
 from . import test_res_partner_bank
 from . import test_res_users
