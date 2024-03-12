cd21a2ebad955e707f8bf5fae17effeb8e0b9feb
roen-odoo <roen@odoo.com>
2024-03-12 16:45:29 +0000

    [FIX] point_of_sale: wrong value in pos sales report
    Current behavior:
    The pos sale report is showing the wrong value in the "Total (VAT Exl)"
    column. The value acutally shown is the total tax included.
    
    Steps to reproduce:
    - Create a product with a tax included in price
    - Create a pos order with this product
    - Validate the order
    - Close session and print the pos sales report
    - Check the value in the "Total (VAT Exl)" column.
    
    opw-3684937
    
    closes odoo/odoo#157917
    
    X-original-commit: fafd931990afba45bd3c003db85b54a1835387e5
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/point_of_sale/tests/test_report_session.py
+++ addons/point_of_sale/tests/test_report_session.py
@@ -12,47 +12,48 @@ class TestReportSession(TestPoSCommon):
 
     def test_report_session(self):
 
-        product1 = self.create_product('Product 1', self.categ_basic, 150)
-        self.open_new_session()
-        session = self.pos_session
+        self.tax1 = self.env['account.tax'].create({
+            'name': 'Tax 1',
+            'amount': 10,
+            'price_include': True,
+        })
+        self.product1 = self.create_product('Product A', self.categ_basic, 110, self.tax1.id)
 
+        self.config.open_ui()
+        session_id = self.config.current_session_id.id
         order = self.env['pos.order'].create({
-            'session_id': session.id,
+            'company_id': self.env.company.id,
+            'session_id': session_id,
             'partner_id': self.partner_a.id,
             'lines': [(0, 0, {
                 'name': "OL/0001",
-                'product_id': product1.id,
-                'price_unit': 150,
+                'product_id': self.product1.id,
+                'price_unit': 110,
                 'discount': 0,
-                'qty': 1.0,
-                'price_subtotal': 150,
-                'price_subtotal_incl': 150,
-            }),],
-            'amount_total': 150.0,
-            'amount_tax': 0.0,
-            'amount_paid': 0.0,
+                'qty': 1,
+                'tax_ids': [[6, False, [self.tax1.id]]],
+                'price_subtotal': 100,
+                'price_subtotal_incl': 110,
+            })],
+            'pricelist_id': self.config.pricelist_id.id,
+            'amount_paid': 110.0,
+            'amount_total': 110.0,
+            'amount_tax': 10.0,
             'amount_return': 0.0,
-            'last_order_preparation_change': '{}'
+            'last_order_preparation_change': '{}',
+            'to_invoice': False,
         })
 
-        payment_context = {"active_ids": order.ids, "active_id": order.id}
-        order_payment = self.env['pos.make.payment'].with_context(**payment_context).create({
-            'amount': 60,
-            'payment_method_id': self.bank_split_pm1.id
-        })
-        order_payment.with_context(**payment_context).check()
+        self.make_payment(order, self.bank_split_pm1, 60)
+        self.make_payment(order, self.bank_pm1, 50)
 
-        order_payment = self.env['pos.make.payment'].with_context(**payment_context).create({
-            'amount': 90,
-            'payment_method_id': self.bank_pm1.id
-        })
-        order_payment.with_context(**payment_context).check()
-
-        session.action_pos_session_closing_control(bank_payment_method_diffs={self.bank_split_pm1.id: 50, self.bank_pm1.id: 40})
+        self.config.current_session_id.action_pos_session_closing_control(bank_payment_method_diffs={self.bank_split_pm1.id: 50, self.bank_pm1.id: 40})
 
         # PoS Orders have negative IDs to avoid conflict, so reports[0] will correspond to the newest order
-        report = self.env['report.point_of_sale.report_saledetails'].get_sale_details(session_ids=[session.id])
+        report = self.env['report.point_of_sale.report_saledetails'].get_sale_details(session_ids=[session_id])
         split_payment_bank = [p for p in report['payments'] if p.get('id', 0) == self.bank_split_pm1.id]
         self.assertEqual(split_payment_bank[0]['cash_moves'][0]['amount'], 50)
         bank_payment = [p for p in report['payments'] if p.get('id', 0) == self.bank_pm1.id]
         self.assertEqual(bank_payment[0]['cash_moves'][0]['amount'], 40)
+        self.assertEqual(report['products_info']['total'], 100, "Total amount of products should be 100, as we want total without tax")
+        self.assertEqual(report['products'][0]['products'][0]['base_amount'], 100, "Base amount of product should be 100, as we want price without tax")
