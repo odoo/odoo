6e3f2236a1ce6131a5e9979f22ed59801f35466e
Benoit Socias <bso@odoo.com>
2024-03-12 16:06:08 +0100

    [FIX] web_editor: fix selectStyle with conflicting extraClass
    Since [1] when `extraClass` was introduced, styles are wrongly applied
    if an `extraClass` is defined on a `selectStyle` option, but both the
    class and the option modify the same CSS property.
    
    Typically, the "Round Corners" option sets the `border-radius` property
    and uses the `rounded` extra class. But that extra class specifies
    values for the `border-radius` properties.
    Without the class, `applyCSS` determines that the style of some corners
    is already `0px` and does therefore not need to be added to the inline
    style.
    But once the class is added, this is not true anymore - and the `0px`
    should have been specified.
    
    This commit avoids this issue by applying the CSS again once the
    `extraClass` is added.
    
    Steps to reproduce:
    - Drop a "Text - Image" snippet.
    - Select the image.
    - Set the "Round Corners" to "50 0 0 0".
    - Press tab to leave the field.
    
    => The entered field values was transformed.
    
    [1]: https://github.com/odoo/odoo/commit/bf5b4b69330747af7b09d48e59218b78a29a4b14
    
    task-3800288

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -3353,10 +3353,14 @@ const SnippetOptionWidget = Widget.extend({
         }
 
         let hasUserValue = false;
-        for (let i = cssProps.length - 1; i > 0; i--) {
-            hasUserValue = applyCSS.call(this, cssProps[i], values.pop(), styles) || hasUserValue;
+        const applyAllCSS = (values) => {
+            for (let i = cssProps.length - 1; i > 0; i--) {
+                hasUserValue = applyCSS.call(this, cssProps[i], values.pop(), styles) || hasUserValue;
+            }
+            hasUserValue = applyCSS.call(this, cssProps[0], values.join(' '), styles) || hasUserValue;
         }
-        hasUserValue = applyCSS.call(this, cssProps[0], values.join(' '), styles) || hasUserValue;
+
+        applyAllCSS([...values]);
 
         function applyCSS(cssProp, cssValue, styles) {
             const forceStyle = (typeof params.forceStyle !== 'undefined');
@@ -3371,6 +3375,13 @@ const SnippetOptionWidget = Widget.extend({
 
         if (params.extraClass) {
             this.$target.toggleClass(params.extraClass, hasUserValue);
+            if (hasUserValue) {
+                // Might have changed because of the class.
+                for (const cssProp of cssProps) {
+                    this.$target[0].style.removeProperty(cssProp);
+                }
+                applyAllCSS(values);
+            }
         }
 
         _restoreTransitions();
