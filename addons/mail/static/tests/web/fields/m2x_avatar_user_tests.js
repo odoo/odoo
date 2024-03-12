35077fb4fec531bd1379b269ed4bb58e33e0f567
tsm-odoo <tsm@odoo.com>
2024-03-12 16:55:48 +0100

    [FIX] mail: fix infinite setTimeout loop
    This PR fixes an infinite setTimout loop in a m2x avatar test.
    Patching the setTimeout method is not a good idea anyway since it
    makes it synchronous which totally changes the flow that is tested.
    
    This PR removes this patch as the assertion that relies on it is too
    technical anyway.
    
    fixes runbot-55891
    
    closes odoo/odoo#157440
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/tests/web/fields/m2x_avatar_user_tests.js
+++ addons/mail/static/tests/web/fields/m2x_avatar_user_tests.js
@@ -6,11 +6,10 @@ import { start } from "@mail/../tests/helpers/test_utils";
 
 import { EventBus } from "@odoo/owl";
 
-import { browser } from "@web/core/browser/browser";
 import { popoverService } from "@web/core/popover/popover_service";
 import { registry } from "@web/core/registry";
 import { tooltipService } from "@web/core/tooltip/tooltip_service";
-import { patchWithCleanup, triggerHotkey } from "@web/../tests/helpers/utils";
+import { triggerHotkey } from "@web/../tests/helpers/utils";
 import { click, contains } from "@web/../tests/utils";
 
 const fakeMultiTab = {
@@ -435,20 +434,13 @@ QUnit.test("avatar card preview", async (assert) => {
         res_id: avatarUserId,
         views: [[false, "kanban"]],
     });
-
-    patchWithCleanup(browser, {
-        setTimeout: (callback, delay) => {
-            assert.step(`setTimeout of ${delay}ms`);
-            callback();
-        },
-    });
     // Open card
     await click(".o_m2o_avatar > img");
     await contains(".o_avatar_card");
     await contains(".o_card_user_infos > span", { text: "Mario" });
     await contains(".o_card_user_infos > a", { text: "Mario@odoo.test" });
     await contains(".o_card_user_infos > a", { text: "+78786987" });
-    assert.verifySteps(["setTimeout of 250ms", "user read"]);
+    assert.verifySteps(["user read"]);
     // Close card
     await click(".o_action_manager");
     await contains(".o_avatar_card", { count: 0 });
