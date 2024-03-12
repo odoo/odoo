c75bc30a57852c9d83cbdb8f554902eb61114fec
Pedram (pebr) <pebr@odoo.com>
2024-03-12 12:08:16 +0000

    [FIX] point_of_sale: include internal notes in product search string
    Prior to this commit, PoS did not support searching for products using
    their internal notes. This limited the search functionality.
    
    opw-3795066
    
    X-original-commit: 5686f64e7662060d6bbdf4eed5aee1a9878fc86a

--- addons/point_of_sale/static/src/app/models/product_product.js
+++ addons/point_of_sale/static/src/app/models/product_product.js
@@ -183,7 +183,7 @@ export class ProductProduct extends Base {
     }
 
     get searchString() {
-        const fields = ["display_name", "description_sale"];
+        const fields = ["display_name", "description_sale", "description"];
         return fields
             .map((field) => this[field] || "")
             .filter(Boolean)
