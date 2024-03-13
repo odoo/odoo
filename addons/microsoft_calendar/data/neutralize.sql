6dd8626a2058e4a795a29f77c40fbc9cf6691c88
Gabriel (gdpf) <gdpf@odoo.com>
2024-03-13 11:43:50 +0100

    [REF] microsoft_calendar: move credentials to res.users.settings
    Before this commit, creating variables inside the `res.users` model was a problem because
    accessing variables in it could block the row while during read.
    
    After this commit, all synchronization credentials are moved to `res.users.settings`, allowing
    the creation of new variables that should not be stored in `res.users` in order to not overload
    the model. After moving the synchronization credentials to the `res.users.settings` model, we
    agreed on dropping the access rule of "allow a normal user (non-admin) to read the token of
    another user" since this feature is not being currently used. Also, we decided on adding the
    `groups=base.group_system` attribute to the synchronization variables in order to add an extra
    security layer in their access. From now on, these variables must be accessed using the
    `sudo()` decorator.
    
    task-3707128

--- addons/microsoft_calendar/data/neutralize.sql
+++ addons/microsoft_calendar/data/neutralize.sql
@@ -3,7 +3,7 @@ UPDATE res_users
     SET microsoft_calendar_token = NULL,
         microsoft_calendar_rtoken = NULL;
 
-UPDATE microsoft_calendar_credentials
-    SET calendar_sync_token = NULL,
-        synchronization_stopped = True,
-        last_sync_date = NULL;
+UPDATE res_users_settings
+    SET microsoft_calendar_sync_token = NULL,
+        microsoft_synchronization_stopped = True,
+        microsoft_last_sync_date = NULL;
