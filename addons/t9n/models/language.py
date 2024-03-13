41388ffb3445c82f2e2bfd67a478e199d07bb11b
Mohammed Basioni <basm@odoo.com>
2024-03-13 17:20:35 +0200

    [IMP] t9n: add languages data when initializing the module.
    This commit add all languages data that will be imported automatically
    into t9n.language model when initializing the module.
    
    Task-3783071

--- addons/t9n/models/language.py
+++ addons/t9n/models/language.py
@@ -5,4 +5,17 @@ class Language(models.Model):
     _name = "t9n.language"
     _description = "Language"
 
-    name = fields.Char("Language", required=True)
+    name = fields.Char("Formal Name", required=True)
+    code = fields.Char("Code", required=True)
+    native_name = fields.Char("Native Name")
+    direction = fields.Selection(
+        required=True,
+        selection=[
+            ("ltr", "left-to-right"),
+            ("rtl", "right-to-left"),
+        ],
+    )
+
+    _sql_constraints = [
+        ("language_code_unique", "unique(code)", "The language code must be unique.")
+    ]
