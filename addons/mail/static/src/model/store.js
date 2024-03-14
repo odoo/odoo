a37edab727d900309761674c022313e85e10dd6c
Alexandre Kühn <aku@odoo.com>
2024-03-14 22:22:01 +0100

    [REF] mail: move even more stuffs in internal

--- addons/mail/static/src/model/store.js
+++ addons/mail/static/src/model/store.js
@@ -1,10 +1,8 @@
-import { deserializeDate, deserializeDateTime } from "@web/core/l10n/dates";
 import { Record } from "./record";
-import { IS_DELETED_SYM, Markup, STORE_SYM, isCommand, isMany } from "./misc";
-import { markup, reactive, toRaw } from "@odoo/owl";
+import { IS_DELETED_SYM, STORE_SYM } from "./misc";
+import { reactive, toRaw } from "@odoo/owl";
 
 /** @typedef {import("./record_list").RecordList} RecordList */
-/** @typedef {import("./misc").RecordField} RecordField */
 
 export class Store extends Record {
     /** @type {import("./store_internal").StoreInternal} */
@@ -21,246 +19,6 @@ export class Store extends Record {
         return this.recordByLocalId.get(localId);
     }
 
-    /**
-     * @param {Record} record
-     * @param {string} fieldName
-     * @param {any} value
-     */
-    updateAttr(record, fieldName, value) {
-        const Model = record.Model;
-        const fieldType = Model._.fieldsType.get(fieldName);
-        const fieldHtml = Model._.fieldsHtml.get(fieldName);
-        // ensure each field write goes through the proxy exactly once to trigger reactives
-        const targetRecord = record._proxyUsed.has(fieldName) ? record : record._proxy;
-        let shouldChange = record[fieldName] !== value;
-        if (fieldType === "datetime" && value) {
-            if (!(value instanceof luxon.DateTime)) {
-                value = deserializeDateTime(value);
-            }
-            shouldChange = !record[fieldName] || !value.equals(record[fieldName]);
-        }
-        if (fieldType === "date" && value) {
-            if (!(value instanceof luxon.DateTime)) {
-                value = deserializeDate(value);
-            }
-            shouldChange = !record[fieldName] || !value.equals(record[fieldName]);
-        }
-        let newValue = value;
-        if (fieldHtml && this._.trusted) {
-            shouldChange =
-                record[fieldName]?.toString() !== value?.toString() ||
-                !(record[fieldName] instanceof Markup);
-            newValue = typeof value === "string" ? markup(value) : value;
-        }
-        if (shouldChange) {
-            record._updateFields.add(fieldName);
-            targetRecord[fieldName] = newValue;
-            record._updateFields.delete(fieldName);
-        }
-    }
-    /**
-     * @param {Record} record
-     * @param {Object} vals
-     */
-    updateFields(record, vals) {
-        for (const [fieldName, value] of Object.entries(vals)) {
-            if (!record.Model._.fields.get(fieldName) || record.Model._.fieldsAttr.get(fieldName)) {
-                this.updateAttr(record, fieldName, value);
-            } else {
-                this.updateRelation(record, fieldName, value);
-            }
-        }
-    }
-    /**
-     * @param {Record} record
-     * @param {string} fieldName
-     * @param {any} value
-     */
-    updateRelation(record, fieldName, value) {
-        /** @type {RecordList<Record>} */
-        const recordList = record._fieldsValue.get(fieldName);
-        if (isMany(record.Model, fieldName)) {
-            this.updateRelationMany(recordList, value);
-        } else {
-            this.updateRelationOne(recordList, value);
-        }
-    }
-    /**
-     * @param {RecordList} recordList
-     * @param {any} value
-     */
-    updateRelationMany(recordList, value) {
-        if (isCommand(value)) {
-            for (const [cmd, cmdData] of value) {
-                if (Array.isArray(cmdData)) {
-                    for (const item of cmdData) {
-                        if (cmd === "ADD") {
-                            recordList.add(item);
-                        } else if (cmd === "ADD.noinv") {
-                            recordList._addNoinv(item);
-                        } else if (cmd === "DELETE.noinv") {
-                            recordList._deleteNoinv(item);
-                        } else {
-                            recordList.delete(item);
-                        }
-                    }
-                } else {
-                    if (cmd === "ADD") {
-                        recordList.add(cmdData);
-                    } else if (cmd === "ADD.noinv") {
-                        recordList._addNoinv(cmdData);
-                    } else if (cmd === "DELETE.noinv") {
-                        recordList._deleteNoinv(cmdData);
-                    } else {
-                        recordList.delete(cmdData);
-                    }
-                }
-            }
-        } else if ([null, false, undefined].includes(value)) {
-            recordList.clear();
-        } else if (!Array.isArray(value)) {
-            recordList.assign([value]);
-        } else {
-            recordList.assign(value);
-        }
-    }
-    /**
-     * @param {RecordList} recordList
-     * @param {any} value
-     * @returns {boolean} whether the value has changed
-     */
-    updateRelationOne(recordList, value) {
-        if (isCommand(value)) {
-            const [cmd, cmdData] = value.at(-1);
-            if (cmd === "ADD") {
-                recordList.add(cmdData);
-            } else if (cmd === "ADD.noinv") {
-                recordList._addNoinv(cmdData);
-            } else if (cmd === "DELETE.noinv") {
-                recordList._deleteNoinv(cmdData);
-            } else {
-                recordList.delete(cmdData);
-            }
-        } else if ([null, false, undefined].includes(value)) {
-            recordList.clear();
-        } else {
-            recordList.add(value);
-        }
-    }
-    /** @param {RecordList<Record>} recordListFullProxy */
-    sortRecordList(recordListFullProxy, func) {
-        const recordList = toRaw(recordListFullProxy)._raw;
-        // sort on copy of list so that reactive observers not triggered while sorting
-        const recordsFullProxy = recordListFullProxy.data.map((localId) =>
-            recordListFullProxy.store.recordByLocalId.get(localId)
-        );
-        recordsFullProxy.sort(func);
-        const data = recordsFullProxy.map((recordFullProxy) => toRaw(recordFullProxy)._raw.localId);
-        const hasChanged = recordList.data.some((localId, i) => localId !== data[i]);
-        if (hasChanged) {
-            recordListFullProxy.data = data;
-        }
-    }
-    /**
-     * @param {"compute"|"sort"|"onAdd"|"onDelete"|"onUpdate"|"hard_delete"} type
-     * @param {...any} params
-     */
-    ADD_QUEUE(type, ...params) {
-        switch (type) {
-            case "delete": {
-                /** @type {import("./record").Record} */
-                const [record] = params;
-                if (!this._.RD_QUEUE.has(record)) {
-                    this._.RD_QUEUE.set(record, true);
-                }
-                break;
-            }
-            case "compute": {
-                /** @type {[import("./record").Record, string]} */
-                const [record, fieldName] = params;
-                let recMap = this._.FC_QUEUE.get(record);
-                if (!recMap) {
-                    recMap = new Map();
-                    this._.FC_QUEUE.set(record, recMap);
-                }
-                recMap.set(fieldName, true);
-                break;
-            }
-            case "sort": {
-                /** @type {[import("./record").Record, string]} */
-                const [record, fieldName] = params;
-                let recMap = this._.FS_QUEUE.get(record);
-                if (!recMap) {
-                    recMap = new Map();
-                    this._.FS_QUEUE.set(record, recMap);
-                }
-                recMap.set(fieldName, true);
-                break;
-            }
-            case "onAdd": {
-                /** @type {[import("./record").Record, string, import("./record").Record]} */
-                const [record, fieldName, addedRec] = params;
-                const Model = record.Model;
-                if (Model._.fieldsSort.get(fieldName)) {
-                    this.ADD_QUEUE("sort", record, fieldName);
-                }
-                if (!Model._.fieldsOnAdd.get(fieldName)) {
-                    return;
-                }
-                let recMap = this._.FA_QUEUE.get(record);
-                if (!recMap) {
-                    recMap = new Map();
-                    this._.FA_QUEUE.set(record, recMap);
-                }
-                let fieldMap = recMap.get(fieldName);
-                if (!fieldMap) {
-                    fieldMap = new Map();
-                    recMap.set(fieldName, fieldMap);
-                }
-                fieldMap.set(addedRec, true);
-                break;
-            }
-            case "onDelete": {
-                /** @type {[import("./record").Record, string, import("./record").Record]} */
-                const [record, fieldName, removedRec] = params;
-                const Model = record.Model;
-                if (!Model._.fieldsOnDelete.get(fieldName)) {
-                    return;
-                }
-                let recMap = this._.FD_QUEUE.get(record);
-                if (!recMap) {
-                    recMap = new Map();
-                    this._.FD_QUEUE.set(record, recMap);
-                }
-                let fieldMap = recMap.get(fieldName);
-                if (!fieldMap) {
-                    fieldMap = new Map();
-                    recMap.set(fieldName, fieldMap);
-                }
-                fieldMap.set(removedRec, true);
-                break;
-            }
-            case "onUpdate": {
-                /** @type {[import("./record").Record, string]} */
-                const [record, fieldName] = params;
-                let recMap = this._.FU_QUEUE.get(record);
-                if (!recMap) {
-                    recMap = new Map();
-                    this._.FU_QUEUE.set(record, recMap);
-                }
-                recMap.set(fieldName, true);
-                break;
-            }
-            case "hard_delete": {
-                /** @type {import("./record").Record} */
-                const [record] = params;
-                if (!this._.RHD_QUEUE.has(record)) {
-                    this._.RHD_QUEUE.set(record, true);
-                }
-                break;
-            }
-        }
-    }
     /** @param {() => any} fn */
     MAKE_UPDATE(fn) {
         this._.UPDATE++;
@@ -357,12 +115,12 @@ export class Store extends Record {
                     /** @type {Record} */
                     const record = RD_QUEUE.keys().next().value;
                     RD_QUEUE.delete(record);
-                    for (const [localId, names] of record.__uses__.data.entries()) {
+                    for (const [localId, names] of record._.uses.data.entries()) {
                         for (const [name2, count] of names.entries()) {
                             const usingRecord2 = toRaw(this.recordByLocalId).get(localId);
                             if (!usingRecord2) {
                                 // record already deleted, clean inverses
-                                record.__uses__.data.delete(localId);
+                                record._.uses.data.delete(localId);
                                 continue;
                             }
                             if (usingRecord2.Model._.fieldsMany.get(name2)) {
@@ -374,7 +132,7 @@ export class Store extends Record {
                             }
                         }
                     }
-                    this.ADD_QUEUE("hard_delete", toRaw(record));
+                    this._.ADD_QUEUE("hard_delete", toRaw(record));
                 }
                 while (RHD_QUEUE.size > 0) {
                     // effectively delete the record
