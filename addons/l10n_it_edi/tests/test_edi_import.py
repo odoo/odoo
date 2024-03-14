65940567a9ae98ed906fc12899c532855e18eb9d
Paolo Gatti (pgi) <pgi@odoo.com>
2024-03-14 17:01:13 +0100

    [FIX] l10n_it_edi: import company to company invoice
    Backport of commit 388466f42fd5dfde3cf44d7b39e768edfc7beba2 .
    (Since the "same" function was changed in the other commit of this PR.)
    
    As we imported invoices from the IAP proxy we refused files when the
    same filename was already present in the database.
    
    Import invoices sent by Company A to Company B on the same Odoo database
    was therefore impeded. The CRON would try to import the Company B's
    vendor bill with the same attachment's filename as the Company A's
    invoice and reject the file.
    
    Now we fill in the `company_id` field on the attachment and we search
    for attachments which belong to the company we're importing for, so the
    case is covered.
    
    An import test has been added.
    
    X-original-commit: 388466f42fd5dfde3cf44d7b39e768edfc7beba2
    
    opw-3673508

--- addons/l10n_it_edi/tests/test_edi_import.py
+++ addons/l10n_it_edi/tests/test_edi_import.py
@@ -3,7 +3,7 @@
 
 from unittest.mock import MagicMock, patch
 
-from odoo import fields, sql_db
+from odoo import fields, sql_db, Command
 from odoo.tests import tagged
 from odoo.addons.l10n_it_edi.tests.common import TestItEdi
 
@@ -69,6 +69,55 @@ class TestItEdiImport(TestItEdi):
                 'price_unit': 1.0,
             }],
         }])
+
+    def test_cron_receives_bill_from_another_company(self):
+        """ Ensure that when from one of your company, you bill the other, the
+        import isn't impeded because of conflicts with the filename """
+        fattura_pa = self.env.ref('l10n_it_edi.edi_fatturaPA')
+        content = self.fake_test_content.encode()
+
+        # Our test content is not encrypted
+        proxy_user = MagicMock()
+        proxy_user.company_id = self.company
+        proxy_user._decrypt_data.return_value = content
+
+        other_company = self.company_data['company']
+        filename = 'IT01234567890_FPR02.xml'
+        def mock_commit(self):
+            pass
+
+        invoice = self.env['account.move'].with_company(other_company).create({
+            'move_type': 'out_invoice',
+            'invoice_line_ids': [
+                Command.create({
+                    'name': "something not price included",
+                    'price_unit': 800.40,
+                    'tax_ids': [Command.set(self.company_data['default_tax_sale'].ids)],
+                }),
+            ],
+        })
+        self.env['ir.attachment'].with_company(other_company).create({
+            'name': filename,
+            'datas': content,
+            'res_model': 'account.move',
+            'res_id': invoice.id,
+        })
+
+        with patch.object(sql_db.Cursor, "commit", mock_commit):
+            fattura_pa._save_incoming_attachment_fattura_pa(
+                proxy_user=proxy_user,
+                id_transaction='9999999999',
+                filename=filename,
+                content=content,
+                key=None)
+
+        attachment = self.env['ir.attachment'].search([
+            ('name', '=', 'IT01234567890_FPR02.xml'),
+            ('res_model', '=', 'account.move'),
+            ('company_id', '=', self.company.id),
+        ])
+        self.assertTrue(attachment)
+        self.assertTrue(self.env['account.move'].browse(attachment.res_id))
 
     def test_receive_same_vendor_bill_twice(self):
         """ Test that the second time we are receiving an SdiCoop invoice, the second is discarded """
