9db995cf2b0989b341c6e50c71e4f3a454e3a6f0
Valeriya(vchu) <vchu@odoo.com>
2024-03-14 16:57:58 +0100

    [FIX] payment: do not store module_state
    Steps to reproduce:
    1) Install any provider and pay with it
    2) Try to uninstall it -> the UserError is raised
    3) Got to the provider view and see that it is broken
    
    The error comes from the fact that there is a module_state field on the
    payment_provider model which is stored and related to state of the
    module (module_id.state). When an error is raised the field is not
    recomputed when the module state goes back from 'to remove' to
    'installed'.
    
    After this fix, the module_state is not stored and synchronised with the
    state of the module.
    
    opw-3721082
    
    closes odoo/odoo#157748
    
    Related: odoo/upgrade#5823
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- addons/payment/models/payment_provider.py
+++ addons/payment/models/payment_provider.py
@@ -173,8 +173,7 @@ class PaymentProvider(models.Model):
 
     # Module-related fields
     module_id = fields.Many2one(string="Corresponding Module", comodel_name='ir.module.module')
-    module_state = fields.Selection(
-        string="Installation State", related='module_id.state', store=True)  # Stored for sorting.
+    module_state = fields.Selection(string="Installation State", related='module_id.state')
     module_to_buy = fields.Boolean(string="Odoo Enterprise Module", related='module_id.to_buy')
 
     # View configuration fields
