34d54de24684c92b10f6bdd3519418d23648e649
Dwi Arifin <mr.dnarifin@gmail.com>
2024-03-15 11:47:12 +0800

    Estate Module

---
+++ custom-addons/estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "license": "LGPL-3",
+    "name": "Employee Punishment",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "John Smith",
+    "category": "",
+    "description": """
+    Description text
+    """,
+    # # data files always loaded at installation
+    "data": [
+        "ir.model.access.csv",
+    ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+}
