66076f9a3d6c9e60ba2b45e8c02467ddac830181
Dwi Arifin <mr.dnarifin@gmail.com>
2024-03-15 14:02:20 +0800

    Update Estate Module

--- custom-addons/estate/__manifest__.py
+++ custom-addons/estate/__manifest__.py
@@ -11,9 +11,14 @@
     # # data files always loaded at installation
     "data": [
         "ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menu.xml",
     ],
     # # data files containing optionally loaded demonstration data
     # 'demo': [
     #     'demo/demo_data.xml',
     # ],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
 }
