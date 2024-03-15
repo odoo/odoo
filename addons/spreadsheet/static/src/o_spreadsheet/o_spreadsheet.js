2ceb84762c30dae8ac273f05307129d4e006aaf4
Vincent Schippefilt <vsc@odoo.com>
2024-03-15 13:11:36 +0100

    [FIX] spreadsheet: update o_spreadsheet to latest version
    ### Contains the following commits:
    
    https://github.com/odoo/o-spreadsheet/commit/b8caa0f27 [REL] 16.4.26
    https://github.com/odoo/o-spreadsheet/commit/1fc912d12 [FIX] figures: deleting a sheet will remove all figures
    https://github.com/odoo/o-spreadsheet/commit/3d5b81567 [FIX] FiguresContainer: chart resizing broken
    https://github.com/odoo/o-spreadsheet/commit/fb4c580c2 [FIX] sheet_interactive: rename sheet in readonly mode
    
    closes odoo/odoo#157854
    
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet/static/src/o_spreadsheet/o_spreadsheet.js
+++ addons/spreadsheet/static/src/o_spreadsheet/o_spreadsheet.js
@@ -24948,7 +24948,7 @@
     function clamp(value, min, max) {
         return Math.min(Math.max(value, min), max);
     }
-    function dragFigureForResize(initialFigure, dirX, dirY, { x: mouseX, y: mouseY }, { x: mouseInitialX, y: mouseInitialY }, keepRatio, minFigSize) {
+    function dragFigureForResize(initialFigure, dirX, dirY, { x: mouseX, y: mouseY }, { x: mouseInitialX, y: mouseInitialY }, keepRatio, minFigSize, { scrollX, scrollY }) {
         let { x, y, width, height } = initialFigure;
         if (keepRatio && dirX != 0 && dirY != 0) {
             const deltaX = Math.min(dirX * (mouseInitialX - mouseX), initialFigure.width - minFigSize);
@@ -24975,14 +24975,14 @@
                 y = initialFigure.y - deltaY;
             }
         }
-        // Restrict resizing if x or y reaches header boundaries
-        if (x < 0) {
-            width += x;
-            x = 0;
+        // Adjusts figure dimensions to ensure it remains within header boundaries and viewport during resizing.
+        if (x + scrollX <= 0) {
+            width = width + x + scrollX;
+            x = -scrollX;
         }
-        if (y < 0) {
-            height += y;
-            y = 0;
+        if (y + scrollY <= 0) {
+            height = height + y + scrollY;
+            y = -scrollY;
         }
         return { ...initialFigure, x, y, width, height };
     }
@@ -25399,7 +25399,7 @@
             const minFigSize = figureRegistry.get(figure.tag).minFigSize || MIN_FIG_SIZE;
             const onMouseMove = (ev) => {
                 const currentMousePosition = { x: ev.clientX, y: ev.clientY };
-                const draggedFigure = dragFigureForResize(initialFig, dirX, dirY, currentMousePosition, initialMousePosition, keepRatio, minFigSize);
+                const draggedFigure = dragFigureForResize(initialFig, dirX, dirY, currentMousePosition, initialMousePosition, keepRatio, minFigSize, this.env.model.getters.getActiveSheetScrollInfo());
                 const otherFigures = this.getOtherFigures(figure.id);
                 const snapResult = snapForResize(this.env.model.getters, dirX, dirY, draggedFigure, otherFigures);
                 this.dnd.draggedFigure = snapResult.snappedFigure;
@@ -32904,6 +32904,15 @@
                     return this.checkFigureExists(cmd.sheetId, cmd.id);
                 default:
                     return 0 /* CommandResult.Success */;
+            }
+        }
+        beforeHandle(cmd) {
+            switch (cmd.type) {
+                case "DELETE_SHEET":
+                    this.getters.getFigures(cmd.sheetId).forEach((figure) => {
+                        this.dispatch("DELETE_FIGURE", { id: figure.id, sheetId: cmd.sheetId });
+                    });
+                    break;
             }
         }
         handle(cmd) {
@@ -46097,6 +46106,9 @@
             this.scrollToSheet();
         }
         onDblClick() {
+            if (this.env.model.getters.isReadonly()) {
+                return;
+            }
             this.startEdition();
         }
         onKeyDown(ev) {
@@ -51721,9 +51733,9 @@
     Object.defineProperty(exports, '__esModule', { value: true });
 
 
-    __info__.version = '16.4.25';
-    __info__.date = '2024-03-12T08:04:20.712Z';
-    __info__.hash = '182ad16';
+    __info__.version = '16.4.26';
+    __info__.date = '2024-03-15T12:11:34.154Z';
+    __info__.hash = 'b8caa0f';
 
 
 })(this.o_spreadsheet = this.o_spreadsheet || {}, owl);
