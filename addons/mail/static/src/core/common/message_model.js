840d6417577f066cd7b359a9ca032deb36e5fb38
Renaud Thiry <reth@odoo.com>
2024-03-15 09:19:57 +0100

    [FIX] mail: prevent deleting other people's attachments
    Currently only the author of a message or an admin is allowed to edit a message
    However any user that can see a message in a chatter can delete
    the attachments linked to it.
    
    While it isn't usually a big issue, it still seems unreasonable to allow anyone
    to delete potentially important attachments from messages.
    
    Assuming existing checks pass, users may delete an attachment:
    - If they created the attachment in the first place, regardless of use in messages.
    - If they authored (all) the message(s) in which it is used.
    
    Meaning they may not delete it if they did not create the attachment and:
    - the attachment is used in messages of multiple people (should not happen)
    - they did not author the message in which it is used
    
    The front-end is also updated to reflect that users may not always delete
    attachments from other user's messages even in threads. Not just in channels
    as was the case before.
    
    task-3519815
    
    closes odoo/odoo#165167
    
    X-original-commit: d357e91dc9489f01357f3276118196ab8cceb2bf
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>
    Signed-off-by: Renaud Thiry (reth) <reth@odoo.com>

--- addons/mail/static/src/core/common/message_model.js
+++ addons/mail/static/src/core/common/message_model.js
@@ -173,8 +173,16 @@ export class Message extends Record {
      */
     now = DateTime.now().set({ milliseconds: 0 });
 
+    /**
+     * True if the backend would technically allow edition
+     * @returns {boolean}
+     */
+    get allowsEdition() {
+        return this.store.self.isAdmin || this.isSelfAuthored;
+    }
+
     get editable() {
-        if (!this.store.self.isAdmin && !this.isSelfAuthored) {
+        if (!this.allowsEdition) {
             return false;
         }
         return this.message_type === "comment";
