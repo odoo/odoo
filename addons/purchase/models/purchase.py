4cfabee4c688bb1ac0cb962cc254ec8d19eccacc
Walid <waha@odoo.com>
2024-03-15 15:00:15 +0100

    [FIX] purchase: fallback to standard price if no pricelist
    Steps to reproduce:
    - Create pricelist with an end date in the past for a stored product
    - Click on manual replenishement
    - The created PO will have a price of 0
    
    Bug:
    if no valid pricelist is found unit price is set to 0
    product standard price is a better fallback
    
    opw-3692985
    
    X-original-commit: 5b255d9f4a9ddcada49da7603cc26d95b8c05a9d

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -1448,8 +1448,9 @@ class PurchaseOrderLine(models.Model):
         product_taxes = product_id.supplier_taxes_id.filtered(lambda x: x.company_id.id == company_id.id)
         taxes = po.fiscal_position_id.map_tax(product_taxes)
 
+        price_unit = seller.price if seller else product_id.standard_price
         price_unit = self.env['account.tax']._fix_tax_included_price_company(
-            seller.price, product_taxes, taxes, company_id) if seller else 0.0
+            price_unit, product_taxes, taxes, company_id)
         if price_unit and seller and po.currency_id and seller.currency_id != po.currency_id:
             price_unit = seller.currency_id._convert(
                 price_unit, po.currency_id, po.company_id, po.date_order or fields.Date.today())
