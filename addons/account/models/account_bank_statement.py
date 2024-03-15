43e9f1dd40a416b8a391f39443406e99a6254197
Victor Piryns (pivi) <pivi@odoo.com>
2024-03-15 16:38:37 +0000

    [PERF] account: speed up dashboard
    Description:
    Add supporting indexes that are used in the queries generated when
    openning the accounting dashboard. A query is tweaked to hit
    those indexes and avoid `JOIN` where possible.
    
    Benchmark:
    Hot loading the default accounting dashboard, default filters and
    1 company selected on a staging database with millions of accounting
    related entries.
    
    |         | Before   | After    |
    |---------|----------|----------|
    | Timings | 7.32 sec | 1.6 sec  |
    
    Reference:
    task-3805835
    
    closes odoo/odoo#162415
    
    X-original-commit: e29b980b36c89ab6d586a1593d362dc7608c412d
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Piryns Victor (pivi) <pivi@odoo.com>

--- addons/account/models/account_bank_statement.py
+++ addons/account/models/account_bank_statement.py
@@ -3,6 +3,7 @@ from contextlib import contextmanager
 
 from odoo import api, fields, models, _, Command
 from odoo.exceptions import UserError
+from odoo.tools import create_index
 from odoo.tools.misc import formatLang
 
 class AccountBankStatement(models.Model):
@@ -101,6 +102,13 @@ class AccountBankStatement(models.Model):
         comodel_name='ir.attachment',
         string="Attachments",
     )
+
+    def init(self):
+        super().init()
+        create_index(self.env.cr,
+                     indexname='account_bank_statement_journal_id_date_desc_id_desc_idx',
+                     tablename='account_bank_statement',
+                     expressions=['journal_id', 'date DESC', 'id DESC'])
 
     # -------------------------------------------------------------------------
     # COMPUTE METHODS
