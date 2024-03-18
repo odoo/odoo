f72ac3a14d76d4fb53ec3a092d08afafe4c35888
Yannick Tivisse <yti@odoo.com>
2024-03-18 15:26:54 +0100

    [IMP] hr_holidays: Compute leave duration in batch
    closes odoo/odoo#158585
    
    Related: odoo/enterprise#59146
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/resource/models/resource_mixin.py
+++ addons/resource/models/resource_mixin.py
@@ -170,7 +170,7 @@ class ResourceMixin(models.AbstractModel):
             for record in self
         }
 
-    def list_work_time_per_day(self, from_datetime, to_datetime, calendar=None, domain=None):
+    def _list_work_time_per_day(self, from_datetime, to_datetime, calendar=None, domain=None):
         """
             By default the resource calendar is used, but it can be
             changed using the `calendar` argument.
@@ -181,21 +181,28 @@ class ResourceMixin(models.AbstractModel):
             Returns a list of tuples (day, hours) for each day
             containing at least an attendance.
         """
-        resource = self.resource_id
-        calendar = calendar or self.resource_calendar_id or self.company_id.resource_calendar_id
+        result = {}
+        records_by_calendar = defaultdict(lambda: self.env[self._name])
+        for record in self:
+            records_by_calendar[calendar or record.resource_calendar_id or record.company_id.resource_calendar_id] += record
 
         # naive datetimes are made explicit in UTC
         if not from_datetime.tzinfo:
             from_datetime = from_datetime.replace(tzinfo=utc)
         if not to_datetime.tzinfo:
             to_datetime = to_datetime.replace(tzinfo=utc)
-
         compute_leaves = self.env.context.get('compute_leaves', True)
-        intervals = calendar._work_intervals_batch(from_datetime, to_datetime, resource, domain, compute_leaves=compute_leaves)[resource.id]
-        result = defaultdict(float)
-        for start, stop, meta in intervals:
-            result[start.date()] += (stop - start).total_seconds() / 3600
-        return sorted(result.items())
+
+        for calendar, records in records_by_calendar.items():
+            resources = self.resource_id
+            all_intervals = calendar._work_intervals_batch(from_datetime, to_datetime, resources, domain, compute_leaves=compute_leaves)
+            for record in records:
+                intervals = all_intervals[record.resource_id.id]
+                record_result = defaultdict(float)
+                for start, stop, _meta in intervals:
+                    record_result[start.date()] += (stop - start).total_seconds() / 3600
+                result[record.id] = sorted(record_result.items())
+        return result
 
     def list_leaves(self, from_datetime, to_datetime, calendar=None, domain=None):
         """
