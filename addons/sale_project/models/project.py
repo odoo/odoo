fad108ff6983dfacb19fa0db5cb9d009ba93b768
lase@odoo.com <lase@odoo.com>
2024-03-18 10:21:36 +0100

    [FIX] sale_project: omit notes SOL in project status
    Reported issue:
    
    Steps to reproduce:
    
    Be sure that 'industry_fsm' is installed.
    
    - Go to Project > Projects and swap to the list view
    - Create and save a new project with a customer
    - Access the related SO via the smart button
    - Add a service product, a storable product a section and a note
    - Go back and access the project status with the smart button
    
    > The SOL generated for the section and the note appear as SO items
    
    Expected behavior:
    
    The purpose of the project status tab is to have an overview at the
    project to help in the analyse its profitability, the time investment,..
    as such, these SOL should not be considered as SO items. In addition,
    these lines lose their entire purpose in the list view used in
    this overview (they can not be moved and display irrelevant infos).
    
    Cause of the issue:
    
    These lines were not filtered out by the current query.
    
    opw-3794386

--- addons/sale_project/models/project.py
+++ addons/sale_project/models/project.py
@@ -338,7 +338,11 @@ class Project(models.Model):
         )
 
         SaleOrderLine = self.env['sale.order.line']
-        sale_order_line_domain = [('order_id', 'any', [('analytic_account_id', 'in', self.analytic_account_id.ids)])]
+        sale_order_line_domain = [
+            '&',
+            ('order_id', 'any', [('analytic_account_id', 'in', self.analytic_account_id.ids)]),
+            ('display_type', '=', False),
+        ]
         sale_order_line_query = SaleOrderLine._where_calc(sale_order_line_domain)
         sale_order_line_sql = sale_order_line_query.select(
             f'{SaleOrderLine._table}.project_id AS id',
