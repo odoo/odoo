528c6d07fa6e5f7c63c81d2d2b0eed0b63c38c86
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2024-03-18 12:19:44 +0100

    [FIX] l10n_sa_edi: missing reason when reversing entries
    With a SA company setup
    Open a jounrnal entry
    Hit 'Reverse Entry' > Reverse
    Error: "For Credit/Debit notes issued in Saudi Arabia, you need to
    specify a Reason"
    
    This occurs because with SA localization we need to provide a reason for
    move reversal but by default the reason field is invisible for journal entries
    
    opw-3789732
    
    closes odoo/odoo#158806
    
    X-original-commit: d660fd5204dcb42c45b466a1fbc0ca7f8bf7cbe6
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Andrea Grazioso (agr) <agr@odoo.com>

--- addons/l10n_sa_edi/__manifest__.py
+++ addons/l10n_sa_edi/__manifest__.py
@@ -25,6 +25,7 @@ E-invoice implementation for the Kingdom of Saudi Arabia
         'data/ubl_21_zatca.xml',
         'data/res_country_data.xml',
         'wizard/l10n_sa_edi_otp_wizard.xml',
+        'wizard/account_move_reversal_views.xml',
         'views/account_tax_views.xml',
         'views/account_journal_views.xml',
         'views/res_partner_views.xml',
