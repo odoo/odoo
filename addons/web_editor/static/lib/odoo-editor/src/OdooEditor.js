bf062140877dabcb6db93d2a4bea2aa36f4b2601
Antoine Guenet <age@odoo.com>
2024-03-18 14:35:44 +0100

    [FIX] web_editor: prevent any traceback in unserialize
    A traceback was spotted when `unserializeNode` was called as a result of
    a call to `resetContent`. That traceback could lead to other tracebacks
    (including one when trying to read a history step's ID in
    `_historyMakeSnapshot`). A traceback at that moment could remove all the
    content of the editor before saving.
    
    This commit ensures the argument passed to `unserializeNode` is defined,
    that `unserializeNode` returns `undefined` if it couldn't return
    anything valid, and that when such is the case the caller handles that
    undefined return value properly.
    
    task-3677431
    
    closes odoo/odoo#158009
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -837,7 +837,10 @@ export class OdooEditor extends EventTarget {
                     toremove.remove();
                 }
             } else if (record.type === 'add') {
-                let node = this.idFind(record.oid) || this.unserializeNode(record.node);
+                let node = this.idFind(record.oid) || (record.node && this.unserializeNode(record.node));
+                if (!node) {
+                    continue;
+                }
                 if (this._collabClientId) {
                     const fakeNode = document.createElement('fake-el');
                     fakeNode.appendChild(node);
@@ -990,6 +993,9 @@ export class OdooEditor extends EventTarget {
                 case 'remove': {
                     let nodeToRemove = this.idFind(mutation.id);
                     if (!nodeToRemove) {
+                        if (!mutation.node) {
+                            continue;
+                        }
                         nodeToRemove = this.unserializeNode(mutation.node);
                         const fakeNode = document.createElement('fake-el');
                         fakeNode.appendChild(nodeToRemove);
