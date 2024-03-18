13f2bdfb6d134dff82ae916a56a98778dd030dab
Dwi Arifin <mr.dnarifin@gmail.com>
2024-03-18 10:54:38 +0800

    Add Custom Addons Biometric

--- custom-addons/odoo-biometric-attendance/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import (
-    controllers,
-    models,
-)
--- custom-addons/odoo-biometric-attendance/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import (
-    device_service_tag,
-    hr_attendance,
-    hr_employee,
-    res_config_settings,
-)
