621e5f8b9d942c2957578c062f5fee7a5ade5866
Andrzej(pian) <pian@odoo.com>
2024-03-18 11:31:07 +0100

    [FIX] mail: update email_from on template change
    Previously, when switching from an email template that overrides the
    email_from field to one that does not, the original email_from value was
    mistakenly passed as an argument. Consequently, emails were sent with
    the wrong sender, using the address from the template instead. Whereas,
    it is expected behavior that when a template does not specify any user,
    the current user's email address should be used as the sender.
    
    [Changes introduced by this commit]
    This commit addresses the issue by updating the sender email to the
    current user's email address, if the template that we update to, does not
    override the sender.
    
    [Reproduce]
    - Install -i contacts
    - Log in as user U
    - Create 2 Email templates that "Applies to" Contact (Settings/Technical/Email/Email Templates):
    	- T1: set "From" field in "Email Configuration" tab (different than U's email)
    	- T2: without set "From" field
    - Go to a contact app, select any user
    - Click "Send message" -> Expand "Full Composer"
    - Select template T1 (with set email)
    - Select template T2 (without set email) and Send
    - BUG: email gets send with email from T1 (even tho we selected T2, without any email)
    
    opw-3769188

--- addons/mail/wizard/mail_compose_message.py
+++ addons/mail/wizard/mail_compose_message.py
@@ -595,6 +595,10 @@ class MailComposer(models.TransientModel):
             default_values = self.with_context(default_composition_mode=composition_mode, default_model=model, default_res_id=res_id).default_get(['composition_mode', 'model', 'res_id', 'parent_id', 'partner_ids', 'subject', 'body', 'email_from', 'reply_to', 'attachment_ids', 'mail_server_id'])
             values = dict((key, default_values[key]) for key in ['subject', 'body', 'partner_ids', 'email_from', 'reply_to', 'attachment_ids', 'mail_server_id'] if key in default_values)
 
+        if template_id:  # Restore default sender if not updated on template switch (for both "mass_mail" and "comment" modes)
+            if 'email_from' not in values:
+                values['email_from'] = self.default_get('email_from').get('email_from')
+
         if values.get('body_html'):
             values['body'] = values.pop('body_html')
 
