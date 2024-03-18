1cecc6ecb8183bd34d85a6f0ff95b05ef66d3d3f
Rémi Rahir (rar) <rar@odoo.com>
2024-03-18 10:49:04 +0100

    [FIX] spreadsheet: Mark function `odoo.pivot.position` as hidden
    This function is not implemented for runtime but just serves as
    placeholder used to convert spreadsheets in templates (and vice-versa).
    It should not be visible to the user as the function is ineffective.
    
    Task: 3810284

--- addons/spreadsheet/static/src/pivot/pivot_functions.js
+++ addons/spreadsheet/static/src/pivot/pivot_functions.js
@@ -140,4 +140,5 @@ functionRegistry
             throw new Error(_t(`[[FUNCTION_NAME]] cannot be called from the spreadsheet.`));
         },
         returns: ["STRING"],
+        hidden: true,
     });
