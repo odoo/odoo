faa0352222dbf48759a31d207ddfe36a314dcb4a
Daniel Kosky (dako) <dako@odoo.com>
2024-03-19 09:49:20 +0100

    [IMP] account_peppol: services wizard
    This change corresponds to a change made on the IAP regarding the
    "services" available on the peppol user. Prior to the change on the IAP,
    the document types that a user could receive were hardcoded, such that
    any user registered on our SMP could only receive two document types:
    - Peppol BIS Billing UBL Invoice V3
    - Peppol BIS Billing UBL CreditNote V3
    
    However, since `account_peppol` depends on `account_edi_ubl_cii`, we
    already know that any peppol recipient can receive a number of other
    document types. Furthermore, additional models might provide support for
    other document types, and so a system has been added to allow the client
    database, and the user to modify the "services" represented on their
    user on the IAP, using a wizard accessible from the res config settings.
    
    This wizard allows users to enable or disable services if they belong in
    the user's supported document types.
    
    Related PRs:
    
    closes odoo/odoo#165385
    
    Iap: odoo/iap-apps#799
    Task-id: 3774424
    Related: odoo/upgrade#6011
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_peppol/models/res_company.py
+++ addons/account_peppol/models/res_company.py
@@ -212,3 +212,45 @@ class ResCompany(models.Model):
         for company in self:
             vals = self._sanitize_peppol_endpoint(vals, company.peppol_eas, company.peppol_endpoint)
         return super().write(vals)
+
+    # -------------------------------------------------------------------------
+    # PEPPOL PARTICIPANT MANAGEMENT
+    # -------------------------------------------------------------------------
+
+    def _peppol_modules_document_types(self):
+        """Override this function to add supported document types as modules are installed.
+
+        :returns: dictionary of the form: {module_name: [(document identifier, document_name)]}
+        """
+        return {
+            'default': {
+                "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0::2.1":
+                    "Peppol BIS Billing UBL Invoice V3",
+                "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0::2.1":
+                    "Peppol BIS Billing UBL CreditNote V3",
+                "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#compliant#urn:fdc:nen.nl:nlcius:v1.0::2.1":
+                    "SI-UBL 2.0 Invoice",
+                "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#compliant#urn:fdc:nen.nl:nlcius:v1.0::2.1":
+                    "SI-UBL 2.0 CreditNote",
+                "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:sg:3.0::2.1":
+                    "SG Peppol BIS Billing 3.0 Invoice",
+                "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:sg:3.0::2.1":
+                    "SG Peppol BIS Billing 3.0 Credit Note",
+                "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0::2.1":
+                    "XRechnung UBL Invoice V2.0",
+                "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0::2.1":
+                    "XRechnung UBL CreditNote V2.0",
+                "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:aunz:3.0::2.1":
+                    "AU-NZ Peppol BIS Billing 3.0 Invoice",
+                "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:aunz:3.0::2.1":
+                    "AU-NZ Peppol BIS Billing 3.0 CreditNote",
+            }
+        }
+
+    def _peppol_supported_document_types(self):
+        """Returns a flattened dictionary of all supported document types."""
+        return {
+            identifier: document_name
+            for module, identifiers in self._peppol_modules_document_types().items()
+            for identifier, document_name in identifiers.items()
+        }
