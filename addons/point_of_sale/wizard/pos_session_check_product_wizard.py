accc6148834853993707d02a106f8e5ac4ded27a
Thomas Lefebvre (thle) <thle@odoo.com>
2024-03-19 08:44:26 +0100

    [FIX] point_of_sale: load demo data if pos user
    We need to belong to the `group_pos_user` group
    in order to load the data correctly.
    
    opw-3933015
    
    closes odoo/odoo#125033
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- addons/point_of_sale/wizard/pos_session_check_product_wizard.py
+++ addons/point_of_sale/wizard/pos_session_check_product_wizard.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models
+from odoo.exceptions import AccessDenied
 from odoo.tools import convert
 
 
@@ -10,6 +11,8 @@ class PosSessionCheckProductWizard(models.TransientModel):
     _description = 'Verify if there are any products for the PoS'
 
     def load_demo_products(self):
+        if not self.env.user.has_group("point_of_sale.group_pos_user"):
+            raise AccessDenied()
         convert.convert_file(self.env.cr, 'point_of_sale', 'data/point_of_sale_onboarding.xml', None, mode='init', kind='data')
         return self.open_ui()
 
