31a66da61ebe4f6d470e46df737cdef41232a2ea
Nicolas Viseur (vin) <vin@odoo.com>
2024-03-19 07:50:07 +0000

    [ADD] l10n_vn_edi: Vietnamese E-Invoicing
    Add support for E-Invoicing in Vietnam by allowing
    to connect Odoo with SInvoice by Viettel.
    
    Task id # 3631616
    
    X-original-commit: 9912e0bbcdd207c0cb9a4ef37b77401b667c2ce8

---
+++ addons/l10n_vn_edi_viettel/wizard/__init__.py
@@ -0,0 +1,5 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import account_move_reversal
+from . import account_move_send
+from . import l10n_vn_edi_cancellation_request
