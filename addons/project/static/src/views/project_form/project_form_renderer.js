dda0e9bc34f8a6cde8fb384ce1c7e553943a28d9
Xavier Bol (xbo) <xbo@odoo.com>
2024-03-19 10:37:08 +0100

    [IMP] resource,project: review form view with html expander
    Since the recent changes made for the new design and in the framework JS
    the form view with html expander has to be adapted.
    
    This commit adapts and improve the form view with html expander.
    it also moves the form view with html expander in resource module to be
    able to use it for form view of `helpdesk.ticket` model
    
    task-3258533
    
    closes odoo/odoo#159496
    
    Related: odoo/enterprise#59552
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/static/src/views/project_form/project_form_renderer.js
+++
@@ -1,7 +0,0 @@
-import { FormRendererWithHtmlExpander } from "../form_with_html_expander/form_renderer_with_html_expander";
-
-export class ProjectFormRenderer extends FormRendererWithHtmlExpander {
-    get htmlFieldQuerySelector() {
-        return '.o_field_html[name="description"]';
-    }
-}
--- addons/project/static/src/views/project_task_form/project_task_form_renderer.js
+++
@@ -1,9 +0,0 @@
-/** @odoo-module */
-
-import { FormRendererWithHtmlExpander } from "../form_with_html_expander/form_renderer_with_html_expander";
-
-export class ProjectTaskFormRenderer extends FormRendererWithHtmlExpander {
-    get htmlFieldQuerySelector() {
-        return '.o_field_html[name="description"]';
-    }
-}
