a9aca0f491efe14254ea2b7267c2407437834d21
Jorge Pinna Puissant <jpp@odoo.com>
2024-03-20 08:44:51 +0100

    [FIX] web: fix multiple run of server action
    Before this commit, when an action is loaded, the server is called to
    find all the information of the action, if the action is a server action
    kind, a new call to the server is executed to run the server action, the
    result of this running is a new action (close action or window action).
    
    Since [1], when a server action is found in the url, it will be executed
    to retrieve the window action when is the current action.
    
    If the server action is found in multiple places in the url, it will be
    executed each time it becomes the current action.
    
    For instance, /crm/12, will execute the server action crm to retrieve
    the window action at the first load, and the same server action will be
    executed again when clicking on the breadcrumb to go back to the
    multi-record view.
    
    Now, when an action is loaded, if the action is a server action, the
    server will execute the action directly and return the resulting action.
    
    This is to avoid a back and forward between the client side and the
    server side.
    
    Note that, this will also use the correct display name on the breadcrumb, the
    one of the window action.
    
    [1] https://github.com/odoo/odoo/commit/c63d14a0485a553b74a8457aee158384e9ae6d3f

--- addons/web/static/tests/legacy/webclient/actions/server_action_tests.js
+++ addons/web/static/tests/legacy/webclient/actions/server_action_tests.js
@@ -15,12 +15,20 @@ QUnit.module("ActionManager", (hooks) => {
     QUnit.module("Server actions");
 
     QUnit.test("can execute server actions from db ID", async function (assert) {
-        assert.expect(10);
+        assert.expect(8);
+        serverData.actions[2].code = () => {
+            return {
+                xml_id: "action_1",
+                name: "Partners Action 1",
+                res_model: "partner",
+                type: "ir.actions.act_window",
+                views: [[1, "kanban"]],
+            };
+        };
         const mockRPC = async (route, args) => {
             assert.step((args && args.method) || route);
-            if (route === "/web/action/run") {
+            if (route === "/web/action/load") {
                 assert.strictEqual(args.action_id, 2, "should call the correct server action");
-                return Promise.resolve(1); // execute action 1
             }
         };
         const webClient = await createWebClient({ serverData, mockRPC });
@@ -30,81 +38,44 @@ QUnit.module("ActionManager", (hooks) => {
         assert.verifySteps([
             "/web/webclient/load_menus",
             "/web/action/load",
-            "/web/action/run",
-            "/web/action/load",
             "get_views",
             "web_search_read",
         ]);
     });
 
-    QUnit.test("handle server actions returning false", async function (assert) {
-        assert.expect(10);
-        const mockRPC = async (route, args) => {
-            assert.step((args && args.method) || route);
-            if (route === "/web/action/run") {
-                return Promise.resolve(false);
-            }
-        };
-        const webClient = await createWebClient({ serverData, mockRPC });
-        // execute an action in target="new"
-        function onClose() {
-            assert.step("close handler");
+    QUnit.test(
+        "send correct context when load it could execute a server action",
+        async function (assert) {
+            assert.expect(1);
+
+            const mockRPC = async (route, args) => {
+                if (route === "/web/action/load") {
+                    assert.deepEqual(args.context, {
+                        // user context
+                        lang: "en",
+                        tz: "taht",
+                        uid: 7,
+                        // action context
+                        someKey: 44,
+                    });
+                }
+            };
+            const webClient = await createWebClient({ serverData, mockRPC });
+            await doAction(webClient, 2, { additionalContext: { someKey: 44 } });
         }
-        await doAction(webClient, 5, { onClose });
-        assert.containsOnce(
-            document.body,
-            ".o_technical_modal .o_form_view",
-            "should have rendered a form view in a modal"
-        );
-        // execute a server action that returns false
-        await doAction(webClient, 2);
-        assert.containsNone(document.body, ".o_technical_modal", "should have closed the modal");
-        assert.verifySteps([
-            "/web/webclient/load_menus",
-            "/web/action/load",
-            "get_views",
-            "onchange",
-            "/web/action/load",
-            "/web/action/run",
-            "close handler",
-        ]);
-    });
-
-    QUnit.test("send correct context when executing a server action", async function (assert) {
-        assert.expect(1);
-
-        serverData.actions[2].context = { someKey: 44 };
-        const mockRPC = async (route, args) => {
-            if (route === "/web/action/run") {
-                assert.deepEqual(args.context, {
-                    // user context
-                    lang: "en",
-                    tz: "taht",
-                    uid: 7,
-                    // action context
-                    someKey: 44,
-                });
-                return Promise.resolve(1); // execute action 1
-            }
-        };
-        const webClient = await createWebClient({ serverData, mockRPC });
-        await doAction(webClient, 2);
-    });
+    );
 
     QUnit.test("action with html help returned by a server action", async function (assert) {
-        serverData.actions[2].context = { someKey: 44 };
-        const mockRPC = async (route, args) => {
-            if (route === "/web/action/run") {
-                return Promise.resolve({
-                    res_model: "partner",
-                    type: "ir.actions.act_window",
-                    views: [[false, "list"]],
-                    help: "<p>I am not a helper</p>",
-                    domain: [[0, "=", 1]],
-                });
-            }
+        serverData.actions[2].code = () => {
+            return {
+                res_model: "partner",
+                type: "ir.actions.act_window",
+                views: [[false, "list"]],
+                help: "<p>I am not a helper</p>",
+                domain: [[0, "=", 1]],
+            };
         };
-        const webClient = await createWebClient({ serverData, mockRPC });
+        const webClient = await createWebClient({ serverData });
         await doAction(webClient, 2);
 
         assert.strictEqual(
