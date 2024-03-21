128a2b4c5a51d345b6ece44951c7589ca5b92056
vlst-odoo <vlst@odoo.com>
2024-03-21 17:02:16 +0100

    [IMP] pos_sale: use list view from web
    In this commit we replace the custom list view for selecting a
    `sale.order` record with the default `view` component from `web`.
    
    This allows us to remove a very large quantity of code, while providing
    for a smoother and more uniform user experience.
    
    Task: 3859301

---
+++ addons/pos_sale/static/src/overrides/components/control_buttons/control_buttons.js
@@ -0,0 +1,18 @@
+/** @odoo-module */
+
+import { patch } from "@web/core/utils/patch";
+import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+import { SelectCreateDialog } from "@web/views/view_dialogs/select_create_dialog";
+
+patch(ControlButtons.prototype, {
+    onClickQuotation() {
+        this.dialog.add(SelectCreateDialog, {
+            resModel: "sale.order",
+            noCreate: true,
+            multiSelect: false,
+            onSelected: async (resIds) => {
+                await this.pos.onClickSaleOrder(resIds[0]);
+            },
+        });
+    },
+});
--- addons/pos_sale_loyalty/static/tests/tours/pos_sale_loyalty_tour.js
+++ addons/pos_sale_loyalty/static/tests/tours/pos_sale_loyalty_tour.js
@@ -1,9 +1,8 @@
 import * as Dialog from "@point_of_sale/../tests/tours/utils/dialog_util";
 import * as PaymentScreen from "@point_of_sale/../tests/tours/utils/payment_screen_util";
 import * as ReceiptScreen from "@point_of_sale/../tests/tours/utils/receipt_screen_util";
-import * as ProductScreenPos from "@point_of_sale/../tests/tours/utils/product_screen_util";
-import * as ProductScreenSale from "@pos_sale/../tests/tours/utils/product_screen_util";
-const ProductScreen = { ...ProductScreenPos, ...ProductScreenSale };
+import * as ProductScreen from "@point_of_sale/../tests/tours/utils/product_screen_util";
+import * as PosSale from "@pos_sale/../tests/tours/utils/pos_sale_utils";
 import { registry } from "@web/core/registry";
 
 registry.category("web_tour.tours").add("PosSaleLoyaltyTour1", {
@@ -11,8 +10,7 @@ registry.category("web_tour.tours").add("PosSaleLoyaltyTour1", {
     steps: () =>
         [
             Dialog.confirm("Open session"),
-            ProductScreen.clickControlButton("Quotation/Order"),
-            ProductScreen.selectFirstOrder(),
+            PosSale.settleNthOrder(1),
             ProductScreen.clickDisplayedProduct("Desk Pad"),
             ProductScreen.clickPayButton(),
             PaymentScreen.clickPaymentMethod("Bank"),
