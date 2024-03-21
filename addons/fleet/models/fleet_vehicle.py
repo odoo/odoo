55b7dcc19953fc9831a61fc7354b07f2fa96a6d4
Flotchet <mafl@odoo.com>
2024-03-21 11:16:17 +0100

    [IMP] fleet: odometer can only increase
    When an odometer is created in fleet_vehicle, il's
    value is now check to be greater than the previous
    value. If not, a UserError is raised.
    The reason for this is that the odometer should only
    increase, and if it decreases, it means that the
    odometer has been tampered with.
    
    Task-3722420
    
    closes odoo/odoo#158540
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/fleet/models/fleet_vehicle.py
+++ addons/fleet/models/fleet_vehicle.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 from dateutil.relativedelta import relativedelta
 
 from odoo import api, fields, models, _
-from odoo.osv import expression
+from odoo.exceptions import UserError
 from odoo.addons.fleet.models.fleet_vehicle_model import FUEL_TYPES
 
 
@@ -301,6 +301,9 @@ class FleetVehicle(models.Model):
         return vehicles
 
     def write(self, vals):
+        if 'odometer' in vals and any(vehicle.odometer > vals['odometer'] for vehicle in self):
+            raise UserError(_('The odometer value cannot be lower than the previous one.'))
+
         if 'driver_id' in vals and vals['driver_id']:
             driver_id = vals['driver_id']
             for vehicle in self.filtered(lambda v: v.driver_id.id != driver_id):
