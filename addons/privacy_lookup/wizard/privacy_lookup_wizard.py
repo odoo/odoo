b1b6be333602f06945f6f7cdfa2bc0660f903cb9
Xavier Morel <xmo@odoo.com>
2024-03-21 12:47:25 +0100

    [IMP] test_lint, privacy_lookup: reliability of SQL checker
    The tracking of callsites & function definitions, and their removal
    after checking, turns out to miss potential inspection sites (which
    makes sense in retrospect).
    
    Avoid removing them, so we can check every callsite x every function
    def, and do so no matter what order the checker works in.
    
    Convert the privacy lookup wizard to SQL object, as its complexity was
    too high for the checker to cope and the callsite in `action_lookup`
    has been unignored.
    
    closes odoo/odoo#158559
    
    Related: odoo/enterprise#59121
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- addons/privacy_lookup/wizard/privacy_lookup_wizard.py
+++ addons/privacy_lookup/wizard/privacy_lookup_wizard.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 
 from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError
-from odoo.tools import pycompat
+from odoo.tools import pycompat, SQL
 
 
 class PrivacyLookupWizard(models.TransientModel):
@@ -44,12 +44,12 @@ class PrivacyLookupWizard(models.TransientModel):
         ]
 
     def _get_query(self):
-        name = "%s" % (self.name.strip())
+        name = self.name.strip()
         email = "%%%s%%" % pycompat.to_text(self.email.strip())
         email_normalized = tools.email_normalize(self.email.strip())
 
         # Step 1: Retrieve users/partners liked to email address or name
-        query = """
+        query = SQL("""
             WITH indirect_references AS (
                 SELECT id
                 FROM res_partner
@@ -74,18 +74,7 @@ class PrivacyLookupWizard(models.TransientModel):
                     SELECT id
                     FROM res_partner
                     WHERE email ilike %s or name ilike %s)))
-        """
-        values = [
-            # Indirect references CTE
-            email_normalized, name,
-            # Search on res.partner
-            self.env['ir.model.data']._xmlid_to_res_id('base.model_res_partner'),
-            # Search on res.users
-            self.env['ir.model.data']._xmlid_to_res_id('base.model_res_users'), email, email, name,
-        ]
-
-        # Step 2: Special case for direct messages
-        query += """
+            -- Step 2: Special case for direct messages
             UNION ALL
             SELECT
                 %s AS res_model_id,
@@ -93,83 +82,89 @@ class PrivacyLookupWizard(models.TransientModel):
                 True AS is_active
             FROM mail_message
             WHERE author_id IN (SELECT id FROM indirect_references)
-        """
-        values += [
+        """,
+            # Indirect references CTE
+            email_normalized, name,
+            # Search on res.partner
+            self.env['ir.model.data']._xmlid_to_res_id('base.model_res_partner'),
+            # Search on res.users
+            self.env['ir.model.data']._xmlid_to_res_id('base.model_res_users'), email, email, name,
+            # Direct messages
             self.env['ir.model.data']._xmlid_to_res_id('mail.model_mail_message'),
-        ]
+        )
 
         # Step 3: Retrieve info on other models
         blacklisted_models = self._get_query_models_blacklist()
         for model_name in self.env:
             if model_name in blacklisted_models:
                 continue
-            table_name = model_name.replace('.', '_')
 
             model = self.env[model_name]
-            if model._transient or model._transient or not model._auto:
+            if model._transient or not model._auto:
                 continue
-            res_model_id = self.env['ir.model'].search([('model', '=', model_name)]).id
-            has_active = 'active' in model
-            has_additional_query = False
-            additional_query = """
-                UNION ALL
-                SELECT
-                    %s AS res_model_id,
-                    id AS res_id,
-                    {active} AS is_active
-                FROM {table_name}
-                WHERE
-                """.format(table_name=table_name, active='active' if has_active else True)
-            additional_values = [
-                res_model_id
-            ]
 
+            table_name = SQL.identifier(model_name.replace('.', '_'))
+
+            conditions = []
             # 3.1 Search Basic Personal Data Records (aka email/name usage)
             for field_name in ['email_normalized', 'email', 'email_from', 'company_email']:
                 if field_name in model and model._fields[field_name].store:
-                    has_additional_query = True
                     rec_name = model._rec_name or 'name'
                     is_normalized = field_name == 'email_normalized' or (model_name == 'mailing.trace' and field_name == 'email')
+
+                    conditions.append(SQL(
+                        "%s %s %s",
+                        SQL.identifier(field_name),
+                        SQL('=') if is_normalized else SQL('ilike'),  # Manage Foo Bar <foo@bar.com>
+                        email_normalized if is_normalized else email
+                    ))
                     if rec_name in model and model._fields[model._rec_name].type == 'char' and not model._fields[model._rec_name].translate:
-                        additional_query += """
-                            {field_name} {search_type} %s OR {rec_name} ilike %s
-                            """.format(
-                                field_name=field_name,
-                                search_type='=' if is_normalized else 'ilike', # Manage Foo Bar <foo@bar.com>
-                                rec_name=rec_name)
-                        additional_values += [email_normalized if is_normalized else email, name]
-                    else:
-                        additional_query += """
-                            {field_name} {search_type} %s
-                            """.format(
-                                field_name=field_name,
-                                search_type='=' if is_normalized else 'ilike') # Manage Foo Bar <foo@bar.com>
-                        additional_values += [email_normalized if is_normalized else email]
+                        conditions.append(SQL(
+                            "%s ilike %s",
+                            SQL.identifier(rec_name),
+                            name,
+                        ))
                     if is_normalized:
                         break
 
             # 3.2 Search Indirect Personal Data References (aka partner_id)
-            partner_fields = [
-                field_name for field_name, field in model._fields.items() \
-                if field.comodel_name == 'res.partner' and field.store and field.type == 'many2one' and field.ondelete != 'cascade']
-            if partner_fields:
-                for field_name in partner_fields:
-                    additional_query += """
-                        {or_clause}{table_field_name} in (SELECT id FROM indirect_references)""".format(
-                            or_clause='OR ' if has_additional_query else '',
-                            table_field_name='"%s"."%s"' % (table_name, field_name))
-                    has_additional_query = True
+            conditions.extend(
+                SQL(
+                    "%s.%s in (SELECT id FROM indirect_references)",
+                    table_name,
+                    SQL.identifier(field_name),
+                )
+                for field_name, field in model._fields.items()
+                if field.comodel_name == 'res.partner'
+                if field.store
+                if field.type == 'many2one'
+                if field.ondelete != 'cascade'
+            )
 
-            if has_additional_query:
-                query += additional_query
-                values += additional_values
-        return query, values
+            if conditions:
+                query = SQL("""
+                    %s
+                    UNION ALL
+                    SELECT
+                        %s AS res_model_id,
+                        id AS res_id,
+                        %s AS is_active
+                    FROM %s
+                    WHERE %s
+                """,
+                    query,
+                    self.env['ir.model'].search([('model', '=', model_name)]).id,
+                    SQL.identifier('active') if 'active' in model else True,
+                    table_name,
+                    SQL(" OR ").join(conditions),
+                )
+        return query
 
     def action_lookup(self):
         self.ensure_one()
-        query, values = self._get_query()
+        query = self._get_query()
         self.env.flush_all()
-        self.env.cr.execute(query, tuple(values))
+        self.env.cr.execute(query)
         results = self.env.cr.dictfetchall()
         self.line_ids = [(5, 0, 0)] + [(0, 0, reference) for reference in results]
         return self.action_open_lines()
