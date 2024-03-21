ae7fee96af319d7d3dfc5f4da51d41c3aae18488
Julien Alardot (jual) <jual@odoo.com>
2024-03-21 14:30:25 +0100

    [IMP] hr_expense: Improve edit rights for admins
    Context:
    During the refactor 68fbdc964038ef6a1cf0d3df773db101ca81794a,
    expense rights were improved but two cases were forgotten.
    
    Before this commit:
    1. sudo would not work as the rights were checking the groups
    2. system admins were considered as being expense admins too, which may
       not be the case if the system admin user changes
       and the correct group `group_hr_expense_manager` is not given.
       System admins should always be able to edit expenses
       when it make sense.
    
    After this commit:
    Sudoed and group_system users are considered as expense admins

--- addons/hr_expense/models/hr_expense_sheet.py
+++ addons/hr_expense/models/hr_expense_sheet.py
@@ -384,7 +384,7 @@ class HrExpenseSheet(models.Model):
     @api.depends_context('uid')
     @api.depends('employee_id', 'user_id', 'state')
     def _compute_is_editable(self):
-        is_hr_admin = self.user_has_groups('hr_expense.group_hr_expense_manager')
+        is_hr_admin = self.user_has_groups('hr_expense.group_hr_expense_manager,base.group_system')
         is_approver = self.user_has_groups('hr_expense.group_hr_expense_user')
         for sheet in self:
             if sheet.state not in {'draft', 'submit', 'approve'}:
@@ -392,7 +392,7 @@ class HrExpenseSheet(models.Model):
                 sheet.is_editable = False
                 continue
 
-            if is_hr_admin:
+            if is_hr_admin or self.env.su:
                 # Administrator-level users are not restricted
                 sheet.is_editable = True
                 continue
