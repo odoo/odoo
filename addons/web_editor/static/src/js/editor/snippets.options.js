15d40c1d132d25a4bc668095f8fe02d31830eb02
Soukéina Bojabza <sobo@odoo.com>
2024-03-21 16:21:40 +0100

    [FIX] website: make the `Carousel` options atomic
    Since commit [1] (fixed by commit [2]), sliding a carousel is no longer
    observed and it therefore does not add a step in the history anymore. It
    was done by deactivating the observer when the `slide` carousel event is
    fired and reactivating it when it is over, with the `slid` event.
    
    However, as sliding a carousel slide is an asynchronous operation, this
    was not the right way to do it, as other operations happening during the
    sliding would also not be observed, which is problematic. Example:
    - Drop the "Carousel" snippet.
    - Slide it with the arrows and quickly drop a "Text-Image" snippet.
    - Undo.
    => The newly added snippet is not removed, as its addition was not
    observed.
    
    Moreover, the sliding must in fact be observed, as it can break other
    carousel operations otherwise. Example of broken behaviors:
    - Add a slide > Undo => The active slide and the active indicator do not
      match.
    - Remove a slide > Undo => The removed slide and its previous slide are
      both active, making them overlap.
    
    These issues also happen because the `Carousel` options do not properly
    wait for the carousel sliding to be over before terminating, which makes
    the call to `historyStep` happen too early. This results in the step
    being added in the history in the middle of the operation. This also
    means that parts of them are not executed in the mutex, which can also
    cause issues.
    
    This commit fixes the `Carousel` options by making them wait properly
    for the end of the sliding. More precisely, they now wait for a promise
    that is resolved only when the `slid` event has been fired. This allows
    to make them fully encapsulated in the mutex.
    
    Note that after making all these operations synchronous, not observing
    the sliding still causes issues with them, which is why it is kept
    observed in the end.
    
    A test ensuring the carousel works correctly will be added in the last
    commit of this PR.
    
    [1]: https://github.com/odoo/odoo/commit/14bc1a9bd1ebdec3b73e268450267320b79d01cd
    [2]: https://github.com/odoo/odoo/commit/bb2c77b3d0f8c7fc74801de54663f373490b9fad
    
    task-3744613
    Related to opw-3675019
    
    X-original-commit: 0eaf1faed30bcb77d37c47103808499a205f02d9
    Part-of: odoo/odoo#173684
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -9615,16 +9615,17 @@ registry.CarouselHandler = registry.GalleryHandler.extend({
             : this.$target[0].querySelector(".carousel");
         carouselEl.classList.remove("slide");
         $(carouselEl).carousel(position);
-        for (const indicatorEl of this.$target[0].querySelectorAll(".carousel-indicators li")) {
-            indicatorEl.classList.remove("active");
-        }
-        this.$target[0].querySelector(`.carousel-indicators li[data-bs-slide-to="${position}"]`)
-                    .classList.add("active");
+        const indicatorEls = this.$target[0].querySelectorAll(".carousel-indicators li");
+        indicatorEls.forEach((indicatorEl, i) => {
+            indicatorEl.classList.toggle("active", i === position);
+        });
         this.trigger_up("activate_snippet", {
             $snippet: $(this.$target[0].querySelector(".carousel-item.active img")),
             ifInactiveOptions: true,
         });
         carouselEl.classList.add("slide");
+        // Prevent the carousel from automatically sliding afterwards.
+        $(carouselEl).carousel("pause");
     },
 });
 
