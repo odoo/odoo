e618ea15dce1fd02717dc542dc64c1819c7f99bc
Louis (loco) <loco@odoo.com>
2024-03-21 09:41:48 +0100

    [FIX] web_editor, *: create attachment on hover effect modification
    *: website
    
    Steps to reproduce:
    - Add an image on the website.
    - Add an "On Hover" animation.
    - Save and edit.
    - Change the animation effect (i.e. from "Overlay" to "Outline").
    - Save.
    
    -> Problem: although the image options have been modified, there is no
    new attachment created for this modified image and its src is still the
    raw data.
    
    The problem appears since [1] as the `_reapplyCurrentShape()` method
    replaces `_applyOptions()`. Due to this, the `o_modified_image_to_save`
    class is not added anymore on the image when changing hover effect
    options such as "effect", "color", "intensity", and "stroke width". To
    solve the problem, the class is added back on the image when changing
    such options. Thanks to it, the system creates a new attachment linked
    to the modified image at the editor save.
    
    [1]: https://github.com/odoo/odoo/commit/5d83672e0aac40d3484ee45f348dfca5de8f186a
    
    task-3820127
    
    X-original-commit: 290d6ac1baef363dce1e39ead708f303ce599fcc

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -6803,6 +6803,9 @@ registry.ImageTools = ImageHandlerOption.extend({
             }
         }
         await this._reapplyCurrentShape();
+        // TODO in master, adapt the '_reapplyCurrentShape()' method to add the
+        // 'o_modified_image_to_save' class on the image.
+        imgEl.classList.add("o_modified_image_to_save");
         // When the hover effects are first activated from the "animationMode"
         // function of the "WebsiteAnimate" class, the history was paused to
         // avoid recording intermediate steps. That's why we unpause it here.
@@ -6818,6 +6821,7 @@ registry.ImageTools = ImageHandlerOption.extend({
         await this._super(...arguments);
         if (["shapeAnimationSpeed", "hoverEffectIntensity", "hoverEffectStrokeWidth"].includes(params.attributeName)) {
             await this._reapplyCurrentShape();
+            this._getImg().classList.add("o_modified_image_to_save");
         }
     },
     /**
@@ -6834,6 +6838,7 @@ registry.ImageTools = ImageHandlerOption.extend({
             defaultColor = "primary";
         }
         img.dataset.hoverEffectColor = this._getCSSColorValue(widgetValue || defaultColor);
+        img.classList.add("o_modified_image_to_save");
         await this._reapplyCurrentShape();
     },
 
