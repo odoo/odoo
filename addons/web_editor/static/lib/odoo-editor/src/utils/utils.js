cb75212d4fe302b54097fbc90da6d6ef8b38205b
Antoine Guenet <age@odoo.com>
2024-03-22 10:58:54 +0100

    [FIX] web_editor: backspace only char with composition menu open
    When typing in Japanese with Romaji input, a traceback occurred when
    hitting backspace to remove the only character in the editable while the
    Japanese character selection menu was open. This is because that case
    triggers an `insertCompositionText` input event with an empty string,
    and tries to delete the editable, which is unremovable. Since we called
    `_applyRawCommand` without wrapping it in `_protect`, the unremovable
    error caused a traceback instead of a rollback. When fixing that, an
    other bug appeared, which was the insertion of a `#` character. This was
    due to the fact that `insertText` was called with empty content, and
    `insertText` defaulted to the character `#` for no particular reason
    since the early proof of concept of the editor.
    
    task-3134566
    
    closes odoo/odoo#158795
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/lib/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/lib/odoo-editor/src/utils/utils.js
@@ -1736,6 +1736,9 @@ export function splitAroundUntil(elements, limitAncestor) {
 }
 
 export function insertText(sel, content) {
+    if (!content) {
+        return;
+    }
     if (sel.anchorNode.nodeType === Node.TEXT_NODE) {
         const pos = [sel.anchorNode.parentElement, splitTextNode(sel.anchorNode, sel.anchorOffset)];
         setSelection(...pos, ...pos, false);
