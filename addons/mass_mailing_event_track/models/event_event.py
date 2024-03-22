4bd313172656aaf23866f21399bd327625fdc3d1
Shaan Thakkar (THSH) <thsh@odoo.com>
2024-03-22 10:29:06 +0530

    [IMP] mass_mailing_event{_track}: propagate event name by default
    After this commit
    =================
    When opening a mailing from the header buttons(invites, Contact Attendees,
    Contact Track Speakers) the subject/title will be set as
    Event: event_name.
    
    e.g. Event: Odoo Experience 2025
    
    Task-3820754
    
    closes odoo/odoo#158763
    
    Signed-off-by: Jérémy Hennecart (jeh) <jeh@odoo.com>

--- addons/mass_mailing_event_track/models/event_event.py
+++ addons/mass_mailing_event_track/models/event_event.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import models, _
 
 
 class Event(models.Model):
@@ -17,6 +17,7 @@ class Event(models.Model):
             context=dict(
                 default_mailing_model_id=self.env.ref('website_event_track.model_event_track').id,
                 default_mailing_domain=repr([('event_id', 'in', self.ids), ('stage_id.is_cancel', '!=', True)]),
+                default_subject=_("Event: %s", self.name),
             ),
         )
         return mass_mailing_action
