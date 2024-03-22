d447cd979351a1e2661ffa2ae0c87a583bc59726
Walid <waha@odoo.com>
2024-03-22 11:59:20 +0100

    [FIX] purchase: fallback to standard price if no pricelist
    Steps to reproduce:
    - Create pricelist with an end date in the past for a stored product
    - Click on manual replenishement
    - The created PO will have a price of 0
    
    Bug:
    if no valid pricelist is found unit price is set to 0
    product standard price is a better fallback
    
    opw-3692985
    
    closes odoo/odoo#159163
    
    X-original-commit: a687031a6f6b7d99964708a938f3e4f0831f9404
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Walid Hanniche (waha) <waha@odoo.com>

--- addons/purchase_stock/tests/test_replenish_wizard.py
+++ addons/purchase_stock/tests/test_replenish_wizard.py
@@ -417,3 +417,37 @@ class TestReplenishWizard(TestStockCommon):
             })
             wizard.supplier_id = supplier
             self.assertEqual(fields.Datetime.from_string('2023-01-08 00:00:00'), wizard.date_planned)
+
+    def test_unit_price_expired_price_list(self):
+        vendor = self.env['res.partner'].create({
+            'name': 'Contact',
+            'type': 'contact',
+        })
+        product = self.env['product.product'].create({
+            'name': 'Product',
+            'standard_price': 60,
+            'seller_ids': [(0, 0, {
+                'partner_id': vendor.id,
+                'price': 1.0,
+                'date_end': '2019-01-01',
+            })],
+            'route_ids': [(6, 0, [
+                self.env.ref('purchase_stock.route_warehouse0_buy').id
+            ])],
+        })
+
+        replenish_wizard = self.env['product.replenish'].create({
+            'product_id': product.id,
+            'product_tmpl_id': product.product_tmpl_id.id,
+            'product_uom_id': self.uom_unit.id,
+            'quantity': 1,
+            'warehouse_id': self.wh.id,
+            'route_id': self.env.ref('purchase_stock.route_warehouse0_buy').id
+        })
+        replenish_wizard.launch_replenishment()
+        last_po_id = self.env['purchase.order'].search([
+            ('origin', 'ilike', '%Manual Replenishment%'),
+        ])[-1]
+
+        self.assertEqual(last_po_id.partner_id, vendor)
+        self.assertEqual(last_po_id.order_line.price_unit, 60)
