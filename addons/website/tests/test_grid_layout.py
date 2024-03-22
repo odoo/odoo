4a7d39de5d6851fa7d9e29fa8244830b31d021dd
Soukéina Bojabza <sobo@odoo.com>
2024-03-22 14:38:41 +0100

    [FIX] web_editor, website: make added grid items always visible
    Before this commit, when adding a new grid item in a grid by using the
    "Add Elements" option, if the page was scrolled such that the top of the
    grid was not visible, this new grid item may not always be visible. This
    is annoying as the user could add an element and not see where it
    appeared and would therefore need to look for it.
    
    This commit fixes this by making the page scroll to the added grid item
    if more than half of it is not visible. A test is also added.
    
    Steps to reproduce:
    - Drop the "Text-Image" snippet and enough snippets under it to have a
      scrollbar.
    - Toggle the "Text-Image" snippet to grid mode.
    - Scroll the page so the top of the grid is not visible.
    - Add a new "Button" with the "Add Elements" option.
    => The button is not visible.
    
    task-3616138
    
    X-original-commit: 891cc3a7119d43e5838d697b36ae8f095b43474a

--- addons/website/tests/test_grid_layout.py
+++ addons/website/tests/test_grid_layout.py
@@ -11,3 +11,7 @@ class TestWebsiteGridLayout(odoo.tests.HttpCase):
         create_image_attachment(self.env, '/web/image/website.s_banner_default_image', 's_banner_default_image.jpg')
         create_image_attachment(self.env, '/web/image/website.s_banner_default_image', 's_banner_default_image2.jpg')
         self.start_tour(self.env['website'].get_client_action_url('/'), 'website_replace_grid_image', login="admin")
+
+    def test_02_scroll_to_new_grid_item(self):
+        create_image_attachment(self.env, '/web/image/website.s_banner_default_image', 's_banner_default_image.jpg')
+        self.start_tour(self.env['website'].get_client_action_url('/'), 'scroll_to_new_grid_item', login='admin')
