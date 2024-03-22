9a3f9441c33e683603602555680def7f756c04b7
jayp-odoo <jayp@odoo.com>
2024-03-22 13:08:43 +0000

    [FIX] mail: partners with no users are displayed in command palette
    Before commit:
    
    if a partner is a member of any channel (eg, whatsapp channel) it will
    be displayed in the command palette even if he does not have any
    associated user or it is inactive. which will cause issues as you can
    not chat with partner who do not have any dedicated user.
    
    After commit:
    
    partners with no dedicated users and inactive users will not be
    displayed in the command palette to chat with when you enter @​.
    
    Task-3815150
    
    closes odoo/odoo#165960
    
    X-original-commit: fe2d0bf564855ee131bb43f4f4973b0c5d8b9591
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/tests/discuss/core/web/command_palette_tests.js
+++ addons/mail/static/tests/discuss/core/web/command_palette_tests.js
@@ -6,6 +6,7 @@ import { start } from "@mail/../tests/helpers/test_utils";
 
 import { commandService } from "@web/core/commands/command_service";
 import { registry } from "@web/core/registry";
+import { Command } from "@mail/../tests/helpers/command";
 import { triggerHotkey } from "@web/../tests/helpers/utils";
 import { click, contains, insertText } from "@web/../tests/utils";
 
@@ -40,4 +41,24 @@ QUnit.test("open the chatWindow of a channel from the command palette", async ()
     await contains(".o_command", { count: 2 });
     await click(".o_command.focused", { text: "general" });
     await contains(".o-mail-ChatWindow", { text: "general" });
+});
+
+QUnit.test("only partners with dedicated users will be displayed in command palette", async () => {
+    const pyEnv = await startServer();
+    const partnerId = pyEnv["res.partner"].create({ name: "test user" });
+    pyEnv["discuss.channel"].create({
+        name: "TestChanel",
+        channel_member_ids: [
+            Command.create({ partner_id: pyEnv.currentPartnerId }),
+            Command.create({ partner_id: partnerId }),
+        ],
+        channel_type: "chat",
+    });
+    const { advanceTime } = await start({ hasTimeControl: true });
+    triggerHotkey("control+k");
+    await insertText(".o_command_palette_search input", "@");
+    advanceTime(commandSetupRegistry.get("@").debounceDelay);
+    await contains(".o_command_name", { text: "Mitchell Admin" });
+    await contains(".o_command_name", { text: "OdooBot" });
+    await contains(".o_command_name", { text: "test user", count: 0 });
 });
