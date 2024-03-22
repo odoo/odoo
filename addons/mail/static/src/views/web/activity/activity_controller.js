010c49868b708030423d6ac05da493f003adfb7b
Pierre-Yves Dufays <pydu@odoo.com>
2024-03-22 07:17:50 +0000

    [FIX] mail: add get modelParam in ActivityController
    We prepare the following fix by adding a modelParam getter that can be
    overridden by sub-classes.
    
    How to reproduce:
    1. Schedule an activity for a document.
    2. Go to activity view.
    3. Select the document to preview in the inspector.
    4. Rename the document.
    5. Refresh the page.
    
    The document name is not renamed.
    
    In the kanban and list view, the document is updated because multiEdit model
    parameter is enabled. It is not the case for the activity view.
    
    Similarly to what is done in list and kanban controller, we add a modelParam
    getter to the activity controller to allow to override the parameters in
    subclasses which will allows to enable the multiEdit parameter in the document
    activitty controller.
    
    Task-3817795
    
    closes odoo/odoo#167312
    
    X-original-commit: 864dcec9d1cf93bf160b4efaf9b41e4d94c530eb
    Related: odoo/enterprise#63492
    Signed-off-by: Stéphane Debauche (std) <std@odoo.com>
    Signed-off-by: Pierre-Yves Dufays (pydu) <pydu@odoo.com>

--- addons/mail/static/src/views/web/activity/activity_controller.js
+++ addons/mail/static/src/views/web/activity/activity_controller.js
@@ -23,16 +23,7 @@ export class ActivityController extends Component {
     static template = "mail.ActivityController";
 
     setup() {
-        const { archInfo, resModel } = this.props;
-        const { activeFields, fields } = extractFieldsFromArchInfo(archInfo, this.props.fields);
-        const modelParams = {
-            config: {
-                activeFields,
-                resModel,
-                fields,
-            },
-        };
-        this.model = useState(useModel(this.props.Model, modelParams));
+        this.model = useState(useModel(this.props.Model, this.modelParams));
 
         this.dialog = useService("dialog");
         this.action = useService("action");
@@ -53,6 +44,18 @@ export class ActivityController extends Component {
                 updateTotal: hasLimitedCount ? () => this.model.root.fetchCount() : undefined,
             };
         });
+    }
+
+    get modelParams() {
+        const { archInfo, resModel } = this.props;
+        const { activeFields, fields } = extractFieldsFromArchInfo(archInfo, this.props.fields);
+        return {
+            config: {
+                activeFields,
+                resModel,
+                fields,
+            },
+        };
     }
 
     getSearchProps() {
