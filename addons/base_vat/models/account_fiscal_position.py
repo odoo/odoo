efa231454804cd0dbfc1f07547ad00b9c03afd08
Thomas Becquevort (thbe) <thbe@odoo.com>
2024-03-25 08:56:59 +0100

    [IMP] account: fiscal positions restrictions adjustments
    Description of the issue/behavior this commit addresses:
    
    The restrictions on fiscal positions using a foreign VAT are too restricitve,
    this commits addresses the issue by allowing new scenarios.
    
    Desired behavior after this commit is merged:
    
    This commit allows to use a foreign VAT with only a country group which
    will automatically set the corresponding country on the FP. For example, you
    can now use a Belgian VAT number on the country group Europe and therefore use
    that fiscal positions for all countries in Europe which do not have their own.
    
    On a fiscal position with a foreign VAT, this commit allows the user to set a
    country which is not the country linked to the foreign VAT. For example, if you
    are belgian, you have a warehouse in France and export goods to Germany, you
    can create a fiscal position with a french foreign vat and germany as country.
    
    This commit also allows a lot more "duplicates". You can set multiple FP inside
    the same country depending on the states/foreign vat you set it up with.
    
    task-3088020

--- addons/base_vat/models/account_fiscal_position.py
+++ addons/base_vat/models/account_fiscal_position.py
@@ -13,12 +13,29 @@ class AccountFiscalPosition(models.Model):
             if not record.foreign_vat:
                 continue
 
-            checked_country_code = self.env['res.partner']._run_vat_test(record.foreign_vat, record.country_id)
+            if record.country_group_id:
+                # Checks the foreign vat is a VAT Number linked to a country of the country group
+                foreign_vat_country_id = next((country_id for country_id in self.country_group_id.country_ids if country_id.code.upper() == self.foreign_vat[:2]), False)
+                if not foreign_vat_country_id:
+                    raise ValidationError(_("The country detected for this foreign VAT number does not match any of the countries composing the country group set on this fiscal position."))
+                if record.country_id:
+                    checked_country_code = self.env['res.partner']._run_vat_test(record.foreign_vat, record.country_id) or self.env['res.partner']._run_vat_test(record.foreign_vat, foreign_vat_country_id)
+                    if not checked_country_code:
+                        record.raise_vat_error_message(foreign_vat_country_id)
+                else:
+                    # If no country is assigned then assign the country of the foreign vat for the mapping
+                    record.country_id = foreign_vat_country_id
+                    checked_country_code = self.env['res.partner']._run_vat_test(record.foreign_vat, foreign_vat_country_id)
+                    if not checked_country_code:
+                        record.raise_vat_error_message(record.country_id)
+            elif record.country_id:
+                foreign_vat_country_id = self.env['res.country'].search([('code', '=', record.foreign_vat[:2].upper())], limit=1)
+                checked_country_code = self.env['res.partner']._run_vat_test(record.foreign_vat, foreign_vat_country_id or record.country_id)
+                if not checked_country_code:
+                    record.raise_vat_error_message()
 
-            if checked_country_code and checked_country_code != record.country_id.code.lower():
-                raise ValidationError(_("The country detected for this foreign VAT number does not match the one set on this fiscal position."))
-
-            if not checked_country_code:
-                fp_label = _("fiscal position [%s]", record.name)
-                error_message = self.env['res.partner']._build_vat_error_message(record.country_id.code.lower(), record.foreign_vat, fp_label)
-                raise ValidationError(error_message)
+    def raise_vat_error_message(self, country=False):
+        fp_label = _("fiscal position [%s]", self.name)
+        country_code = country.code.lower() if country else self.country_id.code.lower()
+        error_message = self.env['res.partner']._build_vat_error_message(country_code, self.foreign_vat, fp_label)
+        raise ValidationError(error_message)
