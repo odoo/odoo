e22b45f54cf8b4c83bf79e0466a99d1c63091b61
Renaud Thiry <reth@odoo.com>
2024-03-25 15:21:04 +0100

    [FIX] {google,microsoft}_calendar: prevent mail...
    when creating an event with an organizer with a non-synced user
    
    When an event is created with an organizer, if the organizer has set up
    calendar syncing with google or microsoft, it will add the event in
    their calendar and send an email to all the attendees.
    
    To prevent attendees from receiving our email too, we use a check
    to avoid the mail logic when the current user is syncing their calendar.
    
    However, this logic only makes sense if we assume the organizer must
    be the current user. Which is note the case.
    
    For appointments, as the event is created by a guest, the code notices
    the current user is not syncing their calendar, and the email is sent
    event though the event will be synced in the organizer's calendar.
    
    We fix this by using the same method used to get the 'current user'
    as the one used for <calendar.event> which takes into account
    the event organizer before the current user.
    
    ----------------------
    We also rework the tests for the module to be able to mock the
    services in a loop.
    
    task-3193021
    
    Part-of: odoo/odoo#129017

--- addons/google_calendar/tests/test_sync_odoo2google.py
+++ addons/google_calendar/tests/test_sync_odoo2google.py
@@ -3,16 +3,19 @@
 
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from freezegun import freeze_time
 from unittest.mock import patch
 
 from odoo.addons.google_calendar.utils.google_event import GoogleEvent
 from odoo.addons.google_calendar.utils.google_calendar import GoogleCalendarService
-from odoo.addons.google_account.models.google_service import GoogleService
 from odoo.addons.google_calendar.models.res_users import User
 from odoo.addons.google_calendar.tests.test_sync_common import TestSyncGoogle, patch_api
+from odoo.addons.mail.tests.common import MailCommon
 from odoo.tests.common import users, warmup
 from odoo.tests import tagged
 from odoo import tools
+
+from .test_token_access import TestTokenAccess
 
 @tagged('odoo2google')
 @patch.object(User, '_get_google_calendar_token', lambda user: 'dummy-token')
@@ -601,34 +604,32 @@ class TestSyncOdoo2Google(TestSyncGoogle):
             'transparency': 'opaque',
         }, timeout=3)
 
-    @patch.object(GoogleService, '_do_request')
-    def test_send_update_do_request(self, mock_do_request):
+    def test_send_update_do_request(self):
         self.env.cr.postcommit.clear()
-        event = self.env['calendar.event'].create({
-            'name': "Event",
-            'allday': True,
-            'start': datetime(2020, 1, 15),
-            'stop': datetime(2020, 1, 15),
-            'need_sync': False,
-        })
-        mock_do_request.return_value = (200, GoogleEvent([event._google_values()]), datetime.now())
-        event.with_context(send_updates=True)._sync_odoo2google(self.google_service)
-        self.call_post_commit_hooks()
-        self.assertGoogleEventSendUpdates(mock_do_request, 'all')
+        with self.mock_google_service():
+            event = self.env['calendar.event'].create({
+                'name': "Event",
+                'allday': True,
+                'start': datetime(2020, 1, 15),
+                'stop': datetime(2020, 1, 15),
+                'need_sync': False,
+            })
+            event.with_context(send_updates=True)._sync_odoo2google(self.google_service)
+            self.call_post_commit_hooks()
+        self.assertGoogleEventSendUpdates('all')
 
-    @patch.object(GoogleService, '_do_request')
-    def test_not_send_update_do_request(self, mock_do_request):
-        event = self.env['calendar.event'].create({
-            'name': "Event",
-            'allday': True,
-            'start': datetime(2020, 1, 15),
-            'stop': datetime(2020, 1, 15),
-            'need_sync': False,
-        })
-        mock_do_request.return_value = (200, GoogleEvent([event._google_values()]), datetime.now())
-        event.with_context(send_updates=False)._sync_odoo2google(self.google_service)
-        self.call_post_commit_hooks()
-        self.assertGoogleEventSendUpdates(mock_do_request, 'none')
+    def test_not_send_update_do_request(self):
+        with self.mock_google_service():
+            event = self.env['calendar.event'].create({
+                'name': "Event",
+                'allday': True,
+                'start': datetime(2020, 1, 15),
+                'stop': datetime(2020, 1, 15),
+                'need_sync': False,
+            })
+            event.with_context(send_updates=False)._sync_odoo2google(self.google_service)
+            self.call_post_commit_hooks()
+        self.assertGoogleEventSendUpdates('none')
 
     @patch_api
     def test_recurrence_delete_single_events(self):
@@ -780,7 +781,7 @@ class TestSyncOdoo2Google(TestSyncGoogle):
             'location' : 'Event Location'
         })
         event._sync_odoo2google(self.google_service)
-        self.assertGoogleEventHasNoConferenceData()
+        self.assertGoogleEventInserted({'conferenceData': False})
 
     @patch_api
     def test_event_available_privacy(self):
@@ -833,3 +834,47 @@ class TestSyncOdoo2Google(TestSyncGoogle):
             'extendedProperties': {'shared': {'%s_odoo_id' % self.env.cr.dbname: event.id}},
             'transparency': 'opaque',
         })
+
+
+@tagged('odoo2google')
+class TestSyncOdoo2GoogleMail(TestTokenAccess, TestSyncGoogle, MailCommon):
+
+    @patch.object(User, '_get_google_calendar_token', lambda user: user.google_calendar_token)
+    @freeze_time("2020-01-01")
+    def test_event_creation_for_user(self):
+        organizer1 = self.users[0]
+        organizer2 = self.users[1]
+        user_root = self.env.ref('base.user_root')
+        organizer1.google_calendar_token = 'abc'
+        organizer2.google_calendar_token = False
+        event_values = {
+            'name': "Event",
+            'start': datetime(2020, 1, 15, 8, 0),
+            'stop': datetime(2020, 1, 15, 18, 0),
+        }
+        partner = self.env['res.partner'].create({'name': 'Jean-Luc', 'email': 'jean-luc@opoo.com'})
+        for create_user, organizer, responsible, expect_mail, is_public in [
+            (user_root, organizer1, organizer1, False, True), (user_root, None, user_root, True, True),
+                (organizer1, None, organizer1, False, False), (organizer1, organizer2, organizer1, False, True)]:
+            with self.subTest(create_uid=create_user.name if create_user else None, user_id=organizer.name if organizer else None):
+                with self.mock_mail_gateway(), self.mock_google_sync(user_id=responsible):
+                    self.env['calendar.event'].with_user(create_user).create({
+                        **event_values,
+                        'partner_ids': [(4, partner.id)],
+                        'user_id': organizer.id if organizer else False,
+                    })
+                if not expect_mail:
+                    self.assertNotSentEmail()
+                    self.assertGoogleEventInserted({
+                        'attendees': [{'email': 'jean-luc@opoo.com', 'responseStatus': 'needsAction'}],
+                        'id': False,
+                        'start': {'dateTime': '2020-01-15T08:00:00+00:00', 'date': None},
+                        'end': {'dateTime': '2020-01-15T18:00:00+00:00', 'date': None},
+                        'guestsCanModify': is_public,
+                        'organizer': {'email': organizer.email, 'self': False} if organizer else False,
+                        'summary': 'Event',
+                        'reminders': {'useDefault': False, 'overrides': []},
+                    }, timeout=3)
+                else:
+                    self.assertGoogleEventNotInserted()
+                    self.assertMailMail(partner, 'sent', author=user_root.partner_id)
