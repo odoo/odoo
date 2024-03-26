e99c65218b55176ee4e0dfa308e5de3f38f0381f
Paolo Gatti (pgi) <pgi@odoo.com>
2024-03-26 10:37:08 +0100

    [IMP] account: ActionableErrors sorting and non-field component
    - SAF-T and other wizards relying on the `critical` attribute
    need sorting.
    jsonb fields don't retain order in PostgreSQL, so we re-sort
    the actionable_errors field by critical and then by error key.
    
    - Furthermore, with this change we enable the re-use of the component
    also for fixed warnings instead of a dedicated Field.
    This will be useful to enable the refactoring also in reports
    like the ones in Intrastat, in a subsequent PR
    *(that will mention this one)*
    
    - Improved the style: reduced padding, introduced border, rounded
    borders on the widget's container.
    
    Enterprise PR: odoo/enterprise#59363
    task-3698183
    
    closes odoo/odoo#159251
    
    Related: odoo/upgrade#5917
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>

--- addons/account/static/src/components/actionable_errors/actionable_errors.js
+++ addons/account/static/src/components/actionable_errors/actionable_errors.js
@@ -3,21 +3,30 @@
 import { registry } from "@web/core/registry";
 import { Component } from "@odoo/owl";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
+import { useService } from "@web/core/utils/hooks";
 
 const WARNING_TYPE_ORDER = ["danger", "warning", "info"];
 
 export class ActionableErrors extends Component {
-    static props = { ...standardFieldProps };
+    static props = { errorData: {type: Object} };
     static template = "account.ActionableErrors";
+
+    setup() {
+        super.setup();
+        this.actionService = useService("action");
+    }
+
+    get errorData() {
+        return this.props.errorData;
+    }
 
     async handleOnClick(errorData){
         this.env.model.action.doAction(errorData.action);
     }
 
     get sortedActionableErrors() {
-        const data = this.props.record.data[this.props.name];
-        return Object.fromEntries(
-            Object.entries(data).sort(
+        return this.errorData && Object.fromEntries(
+            Object.entries(this.errorData).sort(
                 (a, b) =>
                     WARNING_TYPE_ORDER.indexOf(a[1]["level"] || "warning") -
                     WARNING_TYPE_ORDER.indexOf(b[1]["level"] || "warning"),
@@ -26,5 +35,13 @@ export class ActionableErrors extends Component {
     }
 }
 
-export const actionableErrors = {component: ActionableErrors};
-registry.category("fields").add("actionable_errors", actionableErrors);
+export class ActionableErrorsField extends ActionableErrors {
+    static props = { ...standardFieldProps };
+
+    get errorData() {
+        return this.props.record.data[this.props.name];
+    }
+}
+
+export const actionableErrorsField = {component: ActionableErrorsField};
+registry.category("fields").add("actionable_errors", actionableErrorsField);
