fd6d87e99702826de708ddaf4c599f08fc24a8ab
akap-odoo <akap@odoo.com>
2024-03-27 07:04:27 +0000

    [FIX] mail: added a search button for whatsapp channels
    Before this commit:
    
    In mobile view, whatsapp tab had no search button.
    
    After this commit:
    
    In mobile view, added a search button to find whatsapp channel.
    
    task-id:3525542
    
    X-original-commit: a5eeb4c7d0e5432e5110ad1220862f4aafb5c70a

--- addons/mail/static/src/core/web/messaging_menu.js
+++ addons/mail/static/src/core/web/messaging_menu.js
@@ -25,15 +25,10 @@ export class MessagingMenu extends Component {
         this.action = useService("action");
         this.installPrompt = useState(useService("installPrompt"));
         this.ui = useState(useService("ui"));
-        this.state = useState({
-            addingChat: false,
-            addingChannel: false,
-        });
+        this.state = useState({ adding: false });
         this.dropdown = useDropdownState();
 
-        onExternalClick("selector", () => {
-            Object.assign(this.state, { addingChat: false, addingChannel: false });
-        });
+        onExternalClick("selector", () => Object.assign(this.state, { adding: false }));
     }
 
     beforeOpen() {
@@ -143,7 +138,7 @@ export class MessagingMenu extends Component {
 
     onClickNewMessage() {
         if (this.ui.isSmall || this.env.inDiscussApp) {
-            this.state.addingChat = true;
+            Object.assign(this.state, { adding: "chat" });
         } else {
             this.store.openNewMessage();
             this.dropdown.close();
@@ -233,6 +228,10 @@ export class MessagingMenu extends Component {
             value++;
         }
         return value;
+    }
+
+    get displayStartConversation() {
+        return this.store.discuss.activeTab !== "channel" && !this.state.adding;
     }
 }
 
