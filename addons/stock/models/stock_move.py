d75232ef2ffaad2cb1dc685a89df3bffae4cf8a6
Pieter Claeys (clpi) <clpi@odoo.com>
2024-03-27 09:21:36 +0000

    [PERF] stock: only recompute orderpoint quantities for relevant WH
    Due to the `qty_to_order` field on `stock.orderpoint` being a stored
    computed field, it needs to be recalculated every time one or more of
    its dependencies could have changed, to keep a consistent record in the
    database.
    It is, however, dependent on the `qty_forecast` field which is a
    non-stored computed field, depending itself on all the stock moves that
    are linked to the product set on the orderpoint.
    This results in a complete recalculation of the `qty_on_hand`,
    `qty_forecast` and `qty_to_order` on ALL the orderpoints linked to a
    product every time some `stock.move` has been touched or created with
    that product. Even if many of those orderpoints may not even be located
    in the concerned warehouse.
    This situation can create a performance bottleneck when just processing
    pickings, as its constantly (and often pointlessly) recalculating these
    field values to store. This becomes a major issue in case the concerned
    database contains many warehouses with many different orderpoints.
    
    This issue is remedied in a more permanent way for master in [1], but
    due to the stable policy we use a different approach here:
    The `qty_to_order` field on `stock.orderpoint` is now no longer
    dependent on `qty_forecast`. Instead, when creating `stock.moves` or
    writing on its `state`, `product_uom_qty` or `date` fields, we manually
    trigger a recalculation of the `qty_to_order` on all the orderpoints on
    the relevant product, but only in the relevant warehouses.
    In case there is no relevant warehouse for the `stock.move` (none on the
    source or destination move), we fall back to the previous behavior of
    recalculating `qty_to_order` for all orderpoints linked to the product.
    
    [1] https://github.com/odoo/odoo/pull/159432
    
    task-3822497
    
    X-original-commit: b8709b38946c1b661c6d51d3b4bffb52bc68f0bf

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -9,6 +9,7 @@ from operator import itemgetter
 from odoo import _, api, Command, fields, models
 from odoo.exceptions import UserError
 from odoo.osv import expression
+from odoo.osv.expression import OR
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 from odoo.tools.misc import clean_context, OrderedSet, groupby
 
@@ -662,7 +663,9 @@ Please change the quantity done or the rounding precision of your unit of measur
                 picking = self.env['stock.picking'].browse(vals['picking_id'])
                 if picking.group_id:
                     vals['group_id'] = picking.group_id.id
-        return super().create(vals_list)
+        res = super().create(vals_list)
+        res._update_orderpoints()
+        return res
 
     def write(self, vals):
         # Handle the write on the initial demand by updating the reserved quantity and logging
@@ -697,11 +700,16 @@ Please change the quantity done or the rounding precision of your unit of measur
             picking = self.env['stock.picking'].browse(vals['picking_id'])
             if picking.group_id:
                 vals['group_id'] = picking.group_id.id
+        if 'product_id' in vals or 'location_id' in vals or 'location_dest_id' in vals:
+            self._update_orderpoints()
         res = super(StockMove, self).write(vals)
         if move_to_recompute_state:
             move_to_recompute_state._recompute_state()
         if receipt_moves_to_reassign:
             receipt_moves_to_reassign._action_assign()
+        if ('product_id' in vals or 'state' in vals or 'date' in vals or 'product_uom_qty' in vals or
+                'location_id' in vals or 'location_dest_id' in vals):
+            self._update_orderpoints()
         return res
 
     def _propagate_product_packaging(self, product_package_id):
@@ -2318,3 +2326,35 @@ Please change the quantity done or the rounding precision of your unit of measur
             if move.location_id.warehouse_id == self.location_id.warehouse_id:
                 moves |= move._get_moves_orig(moves)
         return moves
+
+    def _update_orderpoints(self):
+        """
+            Manually mark the relevant orderpoints for re-computation.
+            This allows us to only recompute the qty_to_order for the orderpoints in the relevant warehouse(s),
+            instead of all the orderpoints linked to the product.
+        """
+        prods_by_wh = defaultdict(set)
+        prods_no_wh = set()
+        for move in self.exists():
+            source_wh = move.location_id.warehouse_id.id
+            dest_wh = move.location_dest_id.warehouse_id.id
+            if source_wh:
+                prods_by_wh[source_wh].add(move.product_id.id)
+            if dest_wh:
+                prods_by_wh[dest_wh].add(move.product_id.id)
+            if not source_wh and not dest_wh:
+                prods_no_wh.add(move.product_id.id)
+
+        orderpoint_domain = []
+        for wh_id, prod_ids in prods_by_wh.items():
+            orderpoint_domain = OR([orderpoint_domain, [
+                ('warehouse_id', '=', wh_id),
+                ('product_id', 'in', list(prod_ids - prods_no_wh))
+            ]])
+        if prods_no_wh:
+            orderpoint_domain = OR([orderpoint_domain, [('product_id', 'in', list(prods_no_wh))]])
+        if orderpoint_domain:
+            self.env.add_to_compute(
+                self.env['stock.warehouse.orderpoint']._fields['qty_to_order'],
+                self.env['stock.warehouse.orderpoint'].sudo().search(orderpoint_domain, order='id')
+            )
