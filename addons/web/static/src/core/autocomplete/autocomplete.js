f79bafefb22126fd3625543d1f4e19a818b155bf
Julien Carion (juca) <juca@odoo.com>
2024-03-27 15:01:09 +0100

    [FIX] web: autocomplete should always close on click away
    this commit fixes an issue introduced by https://github.com/odoo/odoo/pull/154203
    which could keep the autocomplete options list opened even after a click
    away when the user would "drag and drop" an option out of the list instead
    of simply clicking on it. This is caused by the fact that the onInputBlur
    code is directly terminated in this case (because of ignoreBlur) while
    it is the only way for the autocomplete list to be closed in this case.
    The solution is therefore to add an external listener on pointer down
    which will always close the autocomplete list when clicking away from it.
    
    Steps to reproduce:
    - go to any autocomplete (crm salesperson for example)
    - click on the input
    - drag and drop a result outside of the list
    - try to close the autocomplete list by clicking away
    
    Before the fix, the autocomplete list would only close by scrolling
    or clicking on the input once again.
    
    X-original-commit: 3629cf230828ed47fe51e6a81ca7fd594750492f

--- addons/web/static/src/core/autocomplete/autocomplete.js
+++ addons/web/static/src/core/autocomplete/autocomplete.js
@@ -45,7 +45,8 @@ export class AutoComplete extends Component {
             }
         }, this.constructor.timeout);
 
-        useExternalListener(window, "scroll", this.onWindowScroll, true);
+        useExternalListener(window, "scroll", this.externalClose, true);
+        useExternalListener(window, "pointerdown", this.externalClose, true);
 
         this.hotkey = useService("hotkey");
         this.hotkeysToRemove = [];
@@ -222,6 +223,7 @@ export class AutoComplete extends Component {
 
     onInputBlur() {
         if (this.ignoreBlur) {
+            this.ignoreBlur = false;
             return;
         }
         const value = this.inputRef.el.value;
@@ -338,12 +340,11 @@ export class AutoComplete extends Component {
         this.state.activeSourceOption = null;
     }
     onOptionClick(indices) {
-        this.ignoreBlur = false;
         this.selectOption(indices);
         this.inputRef.el.focus();
     }
 
-    onWindowScroll(ev) {
+    externalClose(ev) {
         if (this.isOpened && !this.root.el.contains(ev.target)) {
             this.close();
         }
