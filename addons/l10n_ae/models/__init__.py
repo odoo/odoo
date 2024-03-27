794e2fca2bdbee7e94edaecb0e76db95a4eabf70
AllePilli <peal@odoo.com>
2024-03-27 14:49:22 +0100

    WIP

--- addons/l10n_ae/models/__init__.py
+++ addons/l10n_ae/models/__init__.py
@@ -1,5 +1,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import template_ae
 from . import account_move_line
-from . import res_company
 from . import res_config_settings
