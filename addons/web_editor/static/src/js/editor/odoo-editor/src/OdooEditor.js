6b7b83d6e7ba8bc9221bfa2f0e9bb71a8aa8ca18
Antoine Guenet <age@odoo.com>
2024-03-28 15:31:16 +0100

    [FIX] web_editor: ensure proper handling of ZWNBSP across the editor
    This adapts functions that accounted for the presence of ZWSP, so they
    also acknowledge ZWNBSP when needed.
    
    task-3604728
    
    Part-of: odoo/odoo#159995

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -83,6 +83,8 @@ import {
     lastLeaf,
     isUnbreakable,
     splitAroundUntil,
+    ZERO_WIDTH_CHARS,
+    ZERO_WIDTH_CHARS_REGEX,
     isLinkEligibleForZwnbsp,
 } from './utils/utils.js';
 import { editorCommands } from './commands/commands.js';
@@ -2220,7 +2222,7 @@ export class OdooEditor extends EventTarget {
             if (range &&
                 range.startContainer === range.endContainer &&
                 range.endContainer.nodeType === Node.TEXT_NODE &&
-                range.cloneContents().textContent === '\u200B'
+                ZERO_WIDTH_CHARS.includes(range.cloneContents().textContent)
             ) {
                 // We Collapse the selection and bypass deleteRange
                 // if the range content is only one ZWS.
@@ -2753,7 +2755,7 @@ export class OdooEditor extends EventTarget {
         const anchorNode = this._latestComputedSelection.anchorNode;
         const focusNode = this._latestComputedSelection.focusNode;
         const parentTextContent = anchorNode.parentElement? anchorNode.parentElement.textContent : null;
-        return anchorNode === focusNode && (parentTextContent === '' || parentTextContent === '\u200B')
+        return anchorNode === focusNode && (['', ...ZERO_WIDTH_CHARS].includes(parentTextContent))
     }
     /**
      * Get the step index in the history to undo.
@@ -3982,7 +3984,7 @@ export class OdooEditor extends EventTarget {
         if (selection.isCollapsed || !range) {
             return false;
         }
-        return range.cloneContents().textContent === '\u200B';
+        return ZERO_WIDTH_CHARS.includes(range.cloneContents().textContent);
     }
 
     getCurrentCollaborativeSelection() {
@@ -4522,7 +4524,7 @@ export class OdooEditor extends EventTarget {
         const targetSupportsHtmlContent = isHtmlContentSupported(sel.anchorNode);
         // Replace entire link if its label is fully selected.
         const link = closestElement(sel.anchorNode, 'a');
-        if (link && sel.toString().replace(/\u200B/g, '') === link.innerText.replace(/\u200B/g, '')) {
+        if (link && sel.toString().replace(ZERO_WIDTH_CHARS_REGEX, '') === link.innerText.replace(ZERO_WIDTH_CHARS_REGEX, '')) {
             const start = leftPos(link);
             link.remove();
             setSelection(...start, ...start, false);
