3ac9130aba08eabf183752e10c9baa1a44799f88
Jorge Pinna Puissant <jpp@odoo.com>
2024-03-28 15:49:45 +0100

    [FIX] base, stock: fix duplicate path
    Since [1], a duplicate path `inventory` was introduced, this leads to
    errors when reloading the page, or when a URL is shared.
    
    Normally, duplicated path should be impossible due to the unique SQL
    constraint, added in [2]. But, the tables ir_act_window,
    ir_act_report_xml, ir_act_url, ir_act_server and ir_act_client inherit
    from table ir_actions (see base_data.sql). And a big limitation of the
    postgresql inheritance feature is that unique indexes only apply to the
    single tables, and not across all the tables, for more information see [3].
    
    This commit, removes the duplicated path, and add a python api
    constraint, to check the uniqueness of the path.
    
    [1] https://github.com/odoo/odoo/commit/814d2dc6ac5c06403ae5d2133d572a6b01edc1c0
    [2] https://github.com/odoo/odoo/commit/c63d14a0485a553b74a8457aee158384e9ae6d3f
    [3] https://www.postgresql.org/docs/14/ddl-inherit.html#DDL-INHERIT-CAVEATS
    
    closes odoo/odoo#160208
    
    X-original-commit: e8efee5c74d9a1eaa24a31d5410174cc3433db79
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- odoo/addons/base/models/ir_actions.py
+++ odoo/addons/base/models/ir_actions.py
@@ -55,7 +55,7 @@ class IrActions(models.Model):
     _order = 'name'
     _allow_sudo_commands = False
 
-    _sql_constraints = [('path_unique', 'unique(path)', "Path to shown in the URL must be unique! Please choose another one.")]
+    _sql_constraints = [('path_unique', 'unique(path)', "Path to show in the URL must be unique! Please choose another one.")]
 
     name = fields.Char(string='Action Name', required=True, translate=True)
     type = fields.Char(string='Action Type', required=True)
@@ -83,6 +83,17 @@ class IrActions(models.Model):
                     raise ValidationError(_("'action-' is a reserved prefix."))
                 if action.path == "new":
                     raise ValidationError(_("'new' is reserved, and can not be used as path."))
+                # Tables ir_act_window, ir_act_report_xml, ir_act_url, ir_act_server and ir_act_client
+                # inherit from table ir_actions (see base_data.sql). The path must be unique across
+                # all these tables. The unique constraint is not enough because a big limitation of
+                # the inheritance feature is that unique indexes only apply to single tables, and
+                # not accross all the tables. So we need to check the uniqueness of the path manually.
+                # For more information, see: https://www.postgresql.org/docs/14/ddl-inherit.html#DDL-INHERIT-CAVEATS
+
+                # Note that, we leave the unique constraint in place to check the uniqueness of the path
+                # within the same table before checking the uniqueness across all the tables.
+                if (self.env['ir.actions.actions'].search_count([('path', '=', action.path)]) > 1):
+                    raise ValidationError(_("Path to show in the URL must be unique! Please choose another one."))
 
     def _compute_xml_id(self):
         res = self.get_external_id()
