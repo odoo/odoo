694a81397475af4af57cf55c2cebc8aeaa055265
Mylyna Hy <myhy@odoo.com>
2024-03-28 13:08:19 -0700

    [FIX] sale: hide product warning
    Problem: When the user sets a warning message for a product,
    the message will always display even if the warning type is
    no-message.
    
    Purpose: The product's warning message should not display
    if the warning type is no message.
    
    Steps to Reproduce on Runbot17:
    1.Install Sales
    2.Enable Sale Warnings in Setting > Sales
    3.Modify the sale warnings for a product
    4.Create a quotation
    5.Click on the catalog and look for the modified product
    6.Observe the warning
    
    Bug from commit: https://github.com/odoo/odoo/commit/a60cced44810d708beaf8ea5a166aa554a7c852b
    opw-3806520
    
    closes odoo/odoo#160828
    
    X-original-commit: 5dc4d5caa23aafdb18e6c65475655c488c573fca
    Signed-off-by: Mylyna Hy (myhy) <myhy@odoo.com>

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -1237,7 +1237,7 @@ class SaleOrderLine(models.Model):
                 'price': self.price_unit,
                 'readOnly': self.order_id._is_readonly() or (self.product_id.sale_line_warn == "block"),
             }
-            if self.product_id.sale_line_warn_msg:
+            if self.product_id.sale_line_warn != 'no-message' and self.product_id.sale_line_warn_msg:
                 res['warning'] = self.product_id.sale_line_warn_msg
             return res
         elif self:
@@ -1262,7 +1262,7 @@ class SaleOrderLine(models.Model):
                     )
                 )
             }
-            if self.product_id.sale_line_warn_msg:
+            if self.product_id.sale_line_warn != 'no-message' and self.product_id.sale_line_warn_msg:
                 res['warning'] = self.product_id.sale_line_warn_msg
             return res
         else:
