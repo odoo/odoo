377c5c482283cd7018ae8d020fd4d6f06f563249
Antoine Vandevenne (anv) <anv@odoo.com>
2024-03-28 10:35:26 +0100

    [FIX] *: retarget documentation links to saas-17.2
    X-original-commit: 0d4d997ba92260ca8763429e9e032cf7035f0f2d

--- addons/l10n_th/__manifest__.py
+++ addons/l10n_th/__manifest__.py
@@ -12,7 +12,7 @@ Chart of Accounts for Thailand.
 Thai accounting chart and localization.
     """,
     'author': 'Almacom (http://almacom.co.th/)',
-    'website': 'https://www.odoo.com/documentation/master/applications/finance/fiscal_localizations/thailand.html',
+    'website': 'https://www.odoo.com/documentation/saas-17.2/applications/finance/fiscal_localizations/thailand.html',
     'depends': [
         'account_qr_code_emv',
         'account',
