odoo/odoo

commit d6695f2892ded178371f6c69cf594037c19ce438

[FIX] account: fix some translations related to chart template
There are currently translation issues related to the chart template.
Due to this the names of some records do not receive the necessary
/ intended translations when installing a localization or new language.

When installing a new localization / chart some records have the following
problem with the values of their translatabe fields:
They are only installed in the language that was active when the localization / chart was installed.
Thus when switching languages (or using a different user with a different language)
the names are displayed in the "installation language".
Translations for all active languages should be installed (for all relevant records).

The same problem happens when installing a new language
(the same records do not receive a translation for the new language).

The translation issue concerns for example (some) accounts and journals;
see the (incomplete) list at the end of this message.

This commit tries to fix the translation issue for the translatable
fields of all relevant models.

Note!
=====
  * The translation mechanism only works for records with xmlid.
    If a module creates a record without xmlid it will not be translated.
  * The problem is only fixed for records with xmlid for which at least 1 of the
    following conditions holds:
        * The record (and the translatable field value) is defined in
          the body of the function decorated with '@template'
        * The translation of the value of the translatable field can
          be found in the module 'account' or in the module that
          is associated with the record (by 'ir.model.data')
    I.e. the problem is not solved for demo data: It is technically
    difficult to determine the module they originate from.
    This makes it difficult to load the right code translation (the
    module information is needed for this).
  * The translation mechanism is not necessarily triggered

addons/account/models/chart_template.py
@@ -9,14 +9,16 @@ from inspect import getmembers
 import logging
 import re
 
+from psycopg2 import sql
 from psycopg2.extras import Json
 
 from odoo import Command, _, models, api
 from odoo.addons.base.models.ir_model import MODULE_UNINSTALL_FLAG
 from odoo.addons.account import SYSCOHADA_LIST
 from odoo.exceptions import AccessError, UserError
-from odoo.tools import file_open, groupby
-from odoo.tools.translate import TranslationImporter
+from odoo.modules import get_resource_from_path
+from odoo.tools import file_open, get_lang, groupby
+from odoo.tools.translate import code_translations, TranslationImporter
 
 _logger = logging.getLogger(__name__)
 
@@ -48,7 +50,15 @@ def template(template=None, model='template_data'):
                 # remove the template code argument as we already know it from the decorator
                 args, kwargs = args[:1], {}
             return func(*args, **kwargs)
-        return api.attrsetter('_l10n_template', (template, model))(wrapper)
+
+        # the module the function originates from is used for code translations
+        path = func.__globals__['__file__']
+        path_info = get_resource_from_path(path)
+        module = path_info[0] if path_info else 'account'
+
+        wrapper._module = module
+        wrapper._l10n_template = (template, model)
+        return wrapper
     return decorator
 
 
@@ -161,11 +171,16 @@ class AccountChartTemplate(models.AbstractModel):
             self.env.reset()  # clear the envs with an old registry
             self = self.env()['account.chart.template']  # create a new env with the new registry
 
+        # To be able to use code translation we load everything in 'en_US'
+        # The demo data is still loaded "normally" since code translations cannot be used for them reliably.
+        # (Since we rely on the "@template functions" to determine the module to take the code translations from.)
+        original_context_lang = self.env.context.get('lang')
         self = self.with_context(
             default_company_id=company.id,
             allowed_company_ids=[company.id],
             tracking_disable=True,
             delay_account_group_sync=True,
+            lang='en_US',
         )
         company = company.with_env(self.env)
 
@@ -184,8 +199,8 @@ class AccountChartTemplate(models.AbstractModel):
             install_demo = False
         data = self._pre_load_data(template_code, company, template_data, data)
         self._load_data(data)
-        self._load_translations(companies=company)
         self._post_load_data(template_code, company, template_data)
+        self._load_translations(companies=company)
 
         # Manual sync because disable above (delay_account_group_sync)
         AccountGroup = self.env['account.group'].with_context(delay_account_group_sync=False)
@@ -196,6 +211,8 @@ class AccountChartTemplate(models.AbstractModel):
         if install_demo and self.ref('base.module_account').demo and not reload_template:
             try:
                 with self.env.cr.savepoint():
+                    self = self.with_context(lang=original_context_lang)
+                    company = company.with_env(self.env)
                     self.sudo()._load_data(self._get_demo_data(company))
                     self._post_load_demo_data(company)
             except Exception:
@@ -391,6 +408,21 @@ class AccountChartTemplate(models.AbstractModel):
             if model in data:
                 data[model] = data.pop(model)
 
+        # Translate the untranslatable fields we want to translate anyway
+        untranslatable_model_fields = self._get_untranslatable_fields_to_translate()
+        untranslatable_target_lang = self._get_untranslatable_fields_target_language(template_code, company)
+        for model_name, records in data.items():
+            untranslatable_fields = untranslatable_model_fields.get(model_name, [])
+            if not untranslatable_fields:
+                continue
+            for _xmlid, record in records.items():
+                for field in untranslatable_fields:
+                    if field not in record:
+                        continue
+                    translation = self._get_field_translation(record, field, untranslatable_target_lang)
+                    if translation:
+                        record[field] = translation
+
         return data
 
     def _load_data(self, data):
@@ -486,7 +518,7 @@ class AccountChartTemplate(models.AbstractModel):
             for xml_id, record in data.items():
                 # Extract the translations from the values
                 for key in list(record):
-                    if '@' in key:
+                    if '@' in key or key == '__translation_module__':
                         del record[key]
 
                 # Manage ids given as database id or xml_id
@@ -560,11 +592,15 @@ class AccountChartTemplate(models.AbstractModel):
     def _get_chart_template_data(self, template_code):
         template_data = defaultdict(lambda: defaultdict(dict))
         template_data['res.company']  # ensure it's the first property when iterating
+        translatable_model_fields = self._get_translatable_template_model_fields()
+        untranslatable_model_fields = self._get_untranslatable_fields_to_translate()
         for code in [None] + self._get_parent_template(template_code):
             for model, funcs in sorted(
                 self._template_register[code].items(),
                 key=lambda i: TEMPLATE_MODELS.index(i[0]) if i[0] in TEMPLATE_MODELS else 1000
             ):
+                translatable_fields = translatable_model_fields.get(model, [])
+                untranslatable_fields = untranslatable_model_fields.get(model, [])
                 for func in funcs:
                     data = func(self, template_code)
                     if data is not None:
@@ -572,6 +608,13 @@ class AccountChartTemplate(models.AbstractModel):
                             template_data[model].update(data)
                         else:
                             for xmlid, record in data.items():
+                                # Store information about which module each field value originates from (for code translations).
+                                # The final value of different fields may be determined by different functions.
+                                # The last function to modify the record may not modify all or any of the translatable fields.
+                                for field in translatable_fields + untranslatable_fields:
+                                    if field in record:
+                                        record.setdefault('__translation_module__', {})[field] = func._module
+
                                 template_data[model][xmlid].update(record)
         return template_data
 
@@ -644,7 +687,14 @@ class AccountChartTemplate(models.AbstractModel):
             if company[fname]:
                 del accounts_data[fname]
 
-        accounts = self.env['account.account'].create(accounts_data.values())
+        accounts = self.env['account.account']._load_records([
+            {
+                'xml_id': f"account.{str(self.env.company.id)}_{xml_id}",
+                'values': values,
+                'noupdate': True,
+            }
+            for xml_id, values in accounts_data.items()
+        ])
         for company_attr_name, account in zip(accounts_data.keys(), accounts):
             company[company_attr_name] = account
 
@@ -999,6 +1049,144 @@ class AccountChartTemplate(models.AbstractModel):
                 _logger.debug("No file %s found for template '%s'", model, module)
         return res
 
+    def _get_untranslatable_fields_target_language(self, template_code, company):
+        """Return the code of the language we want to translate the untranslatable fields into.
+        """
+        # Note: In case this function is called during module installation
+        #   * The active user is the super user.
+        #   * There is no 'lang' in the context.
+        return company.partner_id.lang or get_lang(self.env).code
+
+    def _get_untranslatable_fields_to_translate(self):
+        """Return information about the untranslatable fields we want to translate anyway.
+
+        :param langs: The codes of the languages into which we want to translate the records.
+        :type langs: list[str]
+        :param companies: Records belonging to these companies will be considered.
+        :type companies: Model<res.company>
+        :return: Dictionary (model -> list of fields) where the list of fields contains
+                 all the untranslatable fields of the model we want to translate anyway
+        :rtype: dict[str, list[str]]
+        """
+        return {
+            'account.reconcile.model': [
+                'name',
+            ],
+            'account.journal': [
+                'code',
+            ],
+        }
+
+    def _get_translatable_template_model_fields(self):
+        return {
+            model: [fieldname for (fieldname, field) in self.env[model]._fields.items() if field.translate]
+            for model in TEMPLATE_MODELS
+        }
+
+    def _get_untranslated_translatable_template_model_records(self, langs, companies):
+        """Return information about the records of any model in TEMPLATE_MODELS (and belonging to companies) that need to be translated.
+        Records are in need of translation if they have a translatable field which is missing a translation (into any of the languages given in langs).
+
+        :param langs: The codes of the languages into which we want to translate the records.
+        :type langs: list[str]
+        :param companies: Records belonging to these companies will be considered.
+        :type companies: Model<res.company>
+        :return: The records which information will be returned are those records that have at least 1 untranslated translatable field.
+                 A field is 'untranslated' if it does not have a translation for all languages in langs.
+                 The returned value is a List of tuples:
+                     (model, xmlid (without module prefix), module, dictionary from name to value for each translatable field)
+        :rtype: list[tuple(str, str, str, dict[str, str])]
+        """
+        if not langs or not companies:
+            return []
+
+        company_ids = tuple(companies.ids)
+
+        # Helpers for SQL escaping:
+        # In a last step before executing the query we use 'sql.SQL.format' to
+        #   * escape the SQL identifier names in the query string
+        #   * generate placeholders for the parameters
+        # 'to_escape' stores each value (that needs to be escaped) behind a key that 'sql.SQL.format' and 'execute' can interpolate
+        to_escape = {sql.Identifier: {}, sql.Placeholder: {}}
+        _escape_idx = 0
+        def _escape(_class, value):
+            # '_escape' helps to generate the query string to be formatted with 'sql.SQL.format' and fills 'to_escape' at the same time.
+            # '_class' will be either 'sql.Identifier' or 'sql.Placeholder'
+            nonlocal _escape_idx
+            key = f'_escape_{_escape_idx}'
+            to_escape[_class][key] = value
+            _escape_idx += 1
+            return f"{{{key}}}"
+
+        translatable_model_fields = self._get_translatable_template_model_fields()
+
+        # Generate a list of queries; exactly 1 per model
+        queries = []
+        for model in TEMPLATE_MODELS:
+            translatable_fields = translatable_model_fields[model]
+            if not translatable_fields:
+                continue
+
+            self.env[model].flush_model(['id', 'company_id'] + translatable_model_fields[model])
+
+            # We only want records that have at least 1 missing translation in any of its translatable fields
+            missing_translation_clauses = [
+                f"(model.{_escape(sql.Identifier, field)} ->> {_escape(sql.Placeholder, lang)}) IS NULL"
+                for field in translatable_fields
+                for lang in langs
+            ]
+
+            translatable_field_column_args = []
+            for field in translatable_fields:
+                translatable_field_column_args.extend((f"{_escape(sql.Placeholder, field)}", f"model.{_escape(sql.Identifier, field)}"))
+
+            queries.append(f"""
+                 SELECT {_escape(sql.Placeholder, model)} AS model,
+                        model_data.name AS xmlid,
+                        model_data.module AS module,
+                        (json_build_object({" , ".join(translatable_field_column_args)})) AS fields
+                   FROM {_escape(sql.Identifier, self.env[model]._table)} model
+                   JOIN ir_model_data model_data ON model_data.model = {_escape(sql.Placeholder, model)}
+                                                AND model.id = model_data.res_id
+                  WHERE ({" OR ".join(missing_translation_clauses)})
+                    AND model.company_id IN {_escape(sql.Placeholder, company_ids)}
+            """)
+
+        query = sql.SQL(' UNION ALL '.join(queries)).format(**{
+            **{key: sql.Identifier(identifier) for key, identifier in to_escape[sql.Identifier].items()},
+            **{key: sql.Placeholder(key) for key in to_escape[sql.Placeholder]},  # we only generate the placeholder, not fill it
+        })
+        params = {key: value for key, value in to_escape[sql.Placeholder].items()}
+
+        # the queried models have been flushed already as part of the loop building the queries per model
+        self.env['ir.model.data'].flush_model(['res_id', 'model', 'name'])
+
+        self._cr.execute(query, params)
+        return self._cr.fetchall()
+
+    def _get_field_translation(self, record, fname, lang):
+        """Return the value for language lang for field with fname from record (or None if none exists).
+
+        :param record: record formatted like in the template data (generated by _get_chart_template_data)
+        :type record: dict
+        :param fname: the name of a field (in record) as string
+        :type str
+        :param lang: the code of a res.lang
+        :type str
+        :return record[fname] translated into lang (or None)
+        :rtype str
+        """
+        generic_lang = lang.split('_')[0]  # manage generic locale (i.e. `fr` instead of `fr_BE`)
+        translation_module = record.get('__translation_module__', {}).get(fname, 'account')
+        translation = record.get(f"{fname}@{lang}") or record.get(f"{fname}@{generic_lang}")
+        if translation or fname not in record:
+            return translation
+        else:
+            return (
+                code_translations.get_python_translations(translation_module, lang).get(record[fname])
+                or code_translations.get_python_translations(translation_module, generic_lang).get(record[fname])
+            )
+
     def _load_translations(self, langs=None, companies=None):
         """Load the translations of the chart template.
 
@@ -1013,19 +1201,42 @@ class AccountChartTemplate(models.AbstractModel):
         companies = companies or self.env['res.company'].search([('chart_template', 'in', available_template_codes)])
 
         translation_importer = TranslationImporter(self.env.cr, verbose=False)
+
+        # Gather translations for records that are created from the chart_template data
         for chart_template, chart_companies in groupby(companies, lambda c: c.chart_template):
             template_data = self.env['account.chart.template']._get_chart_template_data(chart_template)
             template_data.pop('template_data', None)
             for mname, data in template_data.items():
                 for _xml_id, record in data.items():
-                    fnames = {fname.split('@')[0] for fname in record}
+                    fnames = {fname.split('@')[0] for fname in record if fname != '__translation_module__'}
                     for lang in langs:
                         for fname in fnames:
-                            value = record.get(f"{fname}@{lang}")
-                            if not value:  # manage generic locale (i.e. `fr` instead of `fr_BE`)
-                                value = record.get(f"{fname}@{lang.split('_')[0]}")
-                            if value:
+                            if not self.env[mname]._fields[fname].translate:
+                                continue
+                            field_translation = self._get_field_translation(record, fname, lang)
+                            if field_translation:
                                 for company in chart_companies:
                                     xml_id = f"account.{company.id}_{_xml_id}"
-                                    translation_importer.model_translations[mname][fname][xml_id][lang] = value
+                                    translation_importer.model_translations[mname][fname][xml_id][lang] = field_translation
+
+        # Gather translations for the TEMPLATE_MODELS records that are not created from the chart_template data
+        translation_langs = [lang for lang in langs if lang != 'en_US']  # there are no code translations for 'en_US' (original language)
+        for (mname, _xml_id, module, fields) in self._get_untranslated_translatable_template_model_records(translation_langs, companies):
+            for (field, value) in fields.items():
+                if not value or 'en_US' not in value:
+                    continue
+                value_en_US = value['en_US']
+                xml_id = f"{module}.{_xml_id}"
+                for lang in [lang for lang in translation_langs if lang not in value]:
+                    if lang in translation_importer.model_translations[mname][field][xml_id]:
+                        continue
+                    value_translated = None
+                    for code_module in ([module, 'account'] if module != 'account' else ['account']):
+                        value_translated = code_translations.get_python_translations(code_module, lang).get(value_en_US)
+                        if not value_translated:  # manage generic locale (i.e. `fr` instead of `fr_BE`)
+                            value_translated = code_translations.get_python_translations(code_module, lang.split('_')[0]).get(value_en_US)
+                        if value_translated:
+                            translation_importer.model_translations[mname][field][xml_id][lang] = value_translated
+                            break
+
         translation_importer.save(overwrite=False)

addons/account/models/account_journal.py
@@ -655,7 +655,15 @@ class AccountJournal(models.Model):
             if not has_liquidity_accounts:
                 default_account_code = self.env['account.account']._search_new_account_code(company, digits, liquidity_account_prefix)
                 default_account_vals = self._prepare_liquidity_account_vals(company, default_account_code, vals)
-                vals['default_account_id'] = self.env['account.account'].create(default_account_vals).id
+                default_account = self.env['account.account'].create(default_account_vals)
+                self.env['ir.model.data']._update_xmlids([
+                    {
+                        'xml_id': f"account.{str(company.id)}_{journal_type}_journal_default_account_{default_account.id}",
+                        'record': default_account,
+                        'noupdate': True,
+                    }
+                ])
+                vals['default_account_id'] = default_account.id
             if journal_type in ('cash', 'bank') and not has_profit_account:
                 vals['profit_account_id'] = company.default_cash_difference_income_account_id.id
             if journal_type in ('cash', 'bank') and not has_loss_account:

addons/account/models/company.py
@@ -514,12 +514,18 @@ class ResCompany(models.Model):
         code = 999999
         while self.env['account.account'].search([('code', '=', str(code)), ('company_id', '=', self.id)]):
             code -= 1
-        return self.env['account.account'].create({
-                'code': str(code),
-                'name': _('Undistributed Profits/Losses'),
-                'account_type': unaffected_earnings_type,
-                'company_id': self.id,
-            })
+        return self.env['account.account']._load_records([
+            {
+                'xml_id': f"account.{str(self.id)}_unaffected_earnings_account",
+                'values': {
+                              'code': str(code),
+                              'name': _('Undistributed Profits/Losses'),
+                              'account_type': unaffected_earnings_type,
+                              'company_id': self.id,
+                          },
+                'noupdate': True,
+            }
+        ])
 
     def get_opening_move_differences(self, opening_move_lines):
         # TO BE REMOVED IN MASTER

commit 89841257ece552bcd01654a55a1b15ff52b11024

[FIX] account: on chart reload: fix matching journals without xmlid
Currently there is the following problem when reloading the chart.
Journals without xmlid may not be matched to chart data correctly
(via code or name).
This then leads to duplicate journals being created / uniqueness
constraint issues on journal codes.
The matching happens in `_pre_reload_data`.

This should only be a problem for upgrade or user created journals
since journals created from the chart data have an xmlid.

The problem was introduced in commit d6695f2892ded178371f6c69cf594037c19ce438 :
(1) We load the chart data in en_US to be able to use the code translations
(2) We switched the language of the loading process to en_US
    (to switch the chart data to en_US for the previous point and to
     avoid inconsistencies)

When matching journals in the DB by code or name to the chart data:
- We fetch the en_US name of the journals in the DB due to (2);
  Code is not translatable.
- We compare those values (journal code / name) against the en_US term
  due to (1).
Thus the matching fails.

This commit improves the matching:
We also compare the name and code (still en_US version)
against the translated values.

addons/account/models/chart_template.py
@@ -237,16 +237,19 @@ class AccountChartTemplate(models.AbstractModel):
                 del data['account.journal'][xmlid]
             else:
                 journal = None
+                lang = self._get_untranslatable_fields_target_language(company.chart_template, company)
+                translated_code = self._get_field_translation(journal_data, 'code', lang)
                 if 'code' in journal_data:
                     journal = self.env['account.journal'].with_context(active_test=False).search([
-                        ('code', '=', journal_data['code']),
+                        ('code', 'in', (journal_data['code'], translated_code)),
                         ('company_id', '=', company.id),
                     ])
                 # Try to match by journal name to avoid conflict in the unique constraint on the mail alias
+                translated_name = self._get_field_translation(journal_data, 'name', lang)
                 if not journal and 'name' in journal_data and 'type' in journal_data:
                     journal = self.env['account.journal'].with_context(active_test=False).search([
                         ('type', '=', journal_data['type']),
-                        ('name', '=', journal_data['name']),
+                        ('name', 'in', (journal_data['name'], translated_name)),
                         ('company_id', '=', company.id),
                     ], limit=1)
                 if journal: