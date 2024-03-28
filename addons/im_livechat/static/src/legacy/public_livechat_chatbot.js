02b26d6f7d375c7db72284b1dbf6809e4c960ad8
tsm-odoo <tsm@odoo.com>
2024-03-28 09:37:59 +0100

    [FIX] {im, website}_livechat: fix chatbot redirection
    Before this PR, the chat bot would stop after redirecting
    a user on the same page that the current one (e.g. anchors)
    because it was counting on the page reload that do not occur
    in this case. This PR fixes the issue.
    
    opw-3801598
    
    X-original-commit: 9f4346180b45779585cf6d9d251575af7e8d8916

--- addons/im_livechat/static/src/legacy/public_livechat_chatbot.js
+++ addons/im_livechat/static/src/legacy/public_livechat_chatbot.js
@@ -160,7 +160,13 @@ LivechatButton.include({
         const selectedAnswer = $target.data("chatbotStepAnswerId");
 
         const redirectLink = $target.data("chatbotStepRedirectLink");
-        this.messaging.publicLivechatGlobal.chatbot.update({ isRedirecting: !!redirectLink });
+        let isRedirecting = false;
+        if (redirectLink && URL.canParse(redirectLink, window.location.href)) {
+            const url = new URL(window.location.href);
+            const nextURL = new URL(redirectLink, window.location.href);
+            isRedirecting = url.pathname !== nextURL.pathname || url.origin !== nextURL.origin;
+        }
+        this.messaging.publicLivechatGlobal.chatbot.update({ isRedirecting });
 
         await this.messaging.publicLivechatGlobal.livechatButtonView.sendMessage({
             content: $target.text().trim(),
