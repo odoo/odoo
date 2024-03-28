340dbcd5cb3d3302e9971176840b5c27757306e8
qsm-odoo <qsm@odoo.com>
2024-03-28 16:52:18 +0000

    [FIX] website, *: make code work wherever the main page scroll is
    *: web, web_editor
    
    Follow-up of [1] (see its own explanation for details).
    This is about fixing the remaining code. In the future, we will probably
    go even further:
    - No more wrapwrap element at all
    - No support of a main scroll which is not left up to the browser (see
      some more details about that in [2] which explains the many problems
      which occurred when the scroll was on the wrapwrap element).
    
    Those final points have yet to be confirmed though.
    
    All in all, this PR should not change any behavior in the standard
    stable versions. But it will fix bugs in some custo trying to change the
    page scrolling behavior, while unifying the versions codebases.
    
    [1]: https://github.com/odoo/odoo/commit/ffc19547c8da2ef7fee8e2ac743ab99a607dcf90
    [2]: https://github.com/odoo/odoo/pull/98429
    
    closes odoo/odoo#163366
    
    X-original-commit: 10df96564fcadd6acde52adf8bf1da32990f3dcf
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/web/static/src/legacy/js/core/dom.js
+++ addons/web/static/src/legacy/js/core/dom.js
@@ -252,6 +252,12 @@ const dom = Object.assign({}, minimalDom, {
         return $().getScrollingElement(...arguments)[0];
     },
     /**
+     * @returns {HTMLElement|Window}
+     */
+    getScrollingTarget() {
+        return $().getScrollingTarget(...arguments)[0];
+    },
+    /**
      * @param {HTMLElement} el
      * @returns {boolean}
      */
