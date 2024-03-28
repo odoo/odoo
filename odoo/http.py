21ffc9602dc08115cd9ad077d84bc26f847959b7
Julien Castiaux <juc@odoo.com>
2024-03-28 13:19:21 +0100

    [FIX] core: NotFound error without warning
    The conditionnal `isinstance(exc, NotFound)` is shadowed by the
    conditionnal `isinstance(exc, HTTPException)` two lines above. Nobody
    ever complained that the warning for NotFound error was gone. Since
    werkzeug 1.0.0, the status code in the response log is colored, 404 is
    colored yellow which should catch the eye. The explicit warning line
    isn't really necessary.
    
    closes odoo/odoo#159881
    
    X-original-commit: 851b91f19b87446662421cb8d801a9472725bc72
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- odoo/http.py
+++ odoo/http.py
@@ -2196,7 +2196,7 @@ class Application:
                     pass
                 elif isinstance(exc, SessionExpiredException):
                     _logger.info(exc)
-                elif isinstance(exc, (UserError, AccessError, NotFound)):
+                elif isinstance(exc, (UserError, AccessError)):
                     _logger.warning(exc)
                 else:
                     _logger.error("Exception during request handling.", exc_info=True)
