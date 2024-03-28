d89a5967340a4f9bd1767b3d658c376436c0a43c
tsm-odoo <tsm@odoo.com>
2024-03-28 09:37:59 +0100

    [FIX] {im, website}_livechat: fix chatbot redirection
    Before this PR, the chat bot would stop after redirecting
    a user on the same page that the current one (e.g. anchors)
    because it was counting on the page reload that do not occur
    in this case. This PR fixes the issue.
    
    opw-3801598
    
    closes odoo/odoo#159494
    
    X-original-commit: 9f4346180b45779585cf6d9d251575af7e8d8916
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

---
+++ addons/website_livechat/static/tests/tours/website_livechat_chatbot_redirect.js
@@ -0,0 +1,48 @@
+/* @odoo-module */
+
+import { registry } from "@web/core/registry";
+
+registry.category("web_tour.tours").add("website_livechat.chatbot_redirect", {
+    test: true,
+    url: "/",
+    steps: [
+        {
+            trigger: ".o_livechat_button",
+        },
+        {
+            trigger: ".o_thread_message_content:contains(Hello, were do you want to go?)",
+        },
+        {
+            trigger: ".o_livechat_chatbot_stepAnswer:contains(Go to the #chatbot-redirect anchor)",
+        },
+        {
+            trigger: ".o_thread_message_content:contains(Tadam, we are on the page you asked for!)",
+            run() {
+                const url = new URL(location.href);
+                if (url.pathname !== "/" || url.hash !== "#chatbot-redirect") {
+                    throw new Error(
+                        "Chatbot should have redirected to the #chatbot-redirect anchor."
+                    );
+                }
+            },
+        },
+        {
+            trigger: ".o_livechat_chatbot_restart",
+        },
+        {
+            trigger: ".o_livechat_chatbot_stepAnswer:contains(Go to the /chabtot-redirect page)",
+        },
+        {
+            trigger:
+                ".o_thread_message_content:contains(Tadam, we are on the page you asked for!):eq(1)",
+            run() {
+                const url = new URL(location.href);
+                if (url.pathname !== "/chatbot-redirect") {
+                    throw new Error(
+                        "Chatbot should have redirected to the /chatbot-redirect page."
+                    );
+                }
+            },
+        },
+    ],
+});
