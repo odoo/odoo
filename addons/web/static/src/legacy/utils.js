191121615e353cd7f6de1a80a1fafa7d82a21d17
David Vidal <david.vidal@tecnativa.com>
2024-03-29 09:18:07 +0000

    [FIX] web: legacy user_context setter
    When writing extension modules it's possible to override the session
    context so we can achieve effects on the overriden functions orm calls.
    This is very handy to avoid ugly patches that doesn't respect proper
    inheritance. This can be done using the `user` service and its
    `updateContext()` function.
    
    Since v15, this is not possible for legacy widgets, as the
    `user_context` key in the session is behind a getter. Before, this was
    possible:
    
    ```javascript
    Object.assign(session.user_context, {"new_key": "new_value"});
    ```
    
    With this patch, a slight variation would be possible:
    
    ```javascript
    session.user_context = Object.assign(
      {}, session.user_context, {"new_key": "new_value"}
    );
    ```
    
    X-original-commit: 38e9036915489f054c9c98f1c2289778dd362123

--- addons/web/static/src/legacy/utils.js
+++ addons/web/static/src/legacy/utils.js
@@ -21,6 +21,7 @@ export async function attachComponent(parent, element, componentClass, props = {
         translateFn: _t,
     });
 
+<<<<<<< HEAD
     if (parent.__parentedMixin) {
         parent.__parentedChildren.push({
             get $el() {
@@ -29,6 +30,274 @@ export async function attachComponent(parent, element, componentClass, props = {
             destroy() {
                 app.destroy();
             },
+||||||| parent of 026f3fabbc80 (temp)
+            function getModalEl(dialog) {
+                return dialog.modalRef ? dialog.modalRef.el : dialog.$modal[0];
+            }
+
+            function getCloseCallback(dialog) {
+                return dialog.modalRef ? () => dialog._close() : () => dialog.$modal.modal("hide");
+            }
+
+            const dialogHotkeyRemoveMap = new Map();
+
+            function onOpenDialog(dialog) {
+                ui.activateElement(getModalEl(dialog));
+                const remove = hotkey.add("escape", getCloseCallback(dialog));
+                dialogHotkeyRemoveMap.set(dialog, remove);
+            }
+
+            function onCloseDialog(dialog) {
+                ui.deactivateElement(getModalEl(dialog));
+                if (dialogHotkeyRemoveMap.has(dialog)) {
+                    const removeHotkey = dialogHotkeyRemoveMap.get(dialog);
+                    removeHotkey();
+                    dialogHotkeyRemoveMap.delete(dialog);
+                }
+            }
+
+            legacyEnv.bus.on("legacy_dialog_opened", null, onOpenDialog);
+            legacyEnv.bus.on("legacy_dialog_destroyed", null, onCloseDialog);
+
+            legacyEnv.bus.on("owl_dialog_mounted", null, onOpenDialog);
+            legacyEnv.bus.on("owl_dialog_willunmount", null, onCloseDialog);
+        },
+    };
+}
+
+/**
+ * Deploys a service allowing legacy to add/remove commands.
+ *
+ * @param {object} legacyEnv
+ * @returns a wowl deployable service
+ */
+export function makeLegacyCommandService(legacyEnv) {
+    return {
+        dependencies: ["command"],
+        start(env) {
+            const { command } = env.services;
+
+            const commandRemoveMap = new Map();
+
+            function setLegacyCommand(uniqueId, getCommandDefinition) {
+                const { name, action, options } = getCommandDefinition(env);
+                removeLegacyCommand(uniqueId);
+                commandRemoveMap.set(uniqueId, command.add(name, action, options));
+            }
+
+            function removeLegacyCommand(uniqueId) {
+                if (commandRemoveMap.has(uniqueId)) {
+                    const removeCommand = commandRemoveMap.get(uniqueId);
+                    removeCommand();
+                    commandRemoveMap.delete(uniqueId);
+                }
+            }
+
+            legacyEnv.bus.on("set_legacy_command", null, setLegacyCommand);
+            legacyEnv.bus.on("remove_legacy_command", null, removeLegacyCommand);
+        },
+    };
+}
+
+export function makeLegacyDropdownService(legacyEnv) {
+    return {
+        dependencies: ["ui", "hotkey"],
+        start(_, { ui, hotkey }) {
+            legacyEnv.services.ui = ui;
+            legacyEnv.services.hotkey = hotkey;
+        },
+    };
+}
+
+export function makeLegacySessionService(legacyEnv, session) {
+    return {
+        dependencies: ["user"],
+        start(env) {
+            // userContext, Object.create is incompatible with legacy new Context
+            function mapContext() {
+                return Object.assign({}, env.services.user.context);
+            }
+            Object.defineProperty(legacyEnv.session, "userContext", {
+                get: () => mapContext(),
+            });
+            Object.defineProperty(session, "user_context", {
+                get: () => mapContext(),
+            });
+        },
+    };
+}
+
+export function mapLegacyEnvToWowlEnv(legacyEnv, wowlEnv) {
+    // rpc
+    legacyEnv.session.rpc = (...args) => {
+        let rejection;
+        const prom = new Promise((resolve, reject) => {
+            const [route, params, settings = {}] = args;
+            // Add user context in kwargs if there are kwargs
+            if (params && params.kwargs) {
+                params.kwargs.context = Object.assign(
+                    {},
+                    legacyEnv.session.user_context,
+                    params.kwargs.context,
+                );
+            }
+            const jsonrpc = wowlEnv.services.rpc(route, params, {
+                silent: settings.shadow,
+                xhr: settings.xhr,
+            });
+            rejection = () => {
+                jsonrpc.abort();
+            };
+            jsonrpc.then(resolve).catch((reason) => {
+                if (reason instanceof RPCError || reason instanceof ConnectionLostError) {
+                    // we do not reject an error here because we want to pass through
+                    // the legacy guardedCatch code
+                    reject({ message: reason, event: $.Event(), legacy: true });
+                } else if (reason instanceof ConnectionAbortedError) {
+                    reject({ message: reason.message, event: $.Event("abort") });
+                } else {
+                    reject(reason);
+                }
+            });
+=======
+            function getModalEl(dialog) {
+                return dialog.modalRef ? dialog.modalRef.el : dialog.$modal[0];
+            }
+
+            function getCloseCallback(dialog) {
+                return dialog.modalRef ? () => dialog._close() : () => dialog.$modal.modal("hide");
+            }
+
+            const dialogHotkeyRemoveMap = new Map();
+
+            function onOpenDialog(dialog) {
+                ui.activateElement(getModalEl(dialog));
+                const remove = hotkey.add("escape", getCloseCallback(dialog));
+                dialogHotkeyRemoveMap.set(dialog, remove);
+            }
+
+            function onCloseDialog(dialog) {
+                ui.deactivateElement(getModalEl(dialog));
+                if (dialogHotkeyRemoveMap.has(dialog)) {
+                    const removeHotkey = dialogHotkeyRemoveMap.get(dialog);
+                    removeHotkey();
+                    dialogHotkeyRemoveMap.delete(dialog);
+                }
+            }
+
+            legacyEnv.bus.on("legacy_dialog_opened", null, onOpenDialog);
+            legacyEnv.bus.on("legacy_dialog_destroyed", null, onCloseDialog);
+
+            legacyEnv.bus.on("owl_dialog_mounted", null, onOpenDialog);
+            legacyEnv.bus.on("owl_dialog_willunmount", null, onCloseDialog);
+        },
+    };
+}
+
+/**
+ * Deploys a service allowing legacy to add/remove commands.
+ *
+ * @param {object} legacyEnv
+ * @returns a wowl deployable service
+ */
+export function makeLegacyCommandService(legacyEnv) {
+    return {
+        dependencies: ["command"],
+        start(env) {
+            const { command } = env.services;
+
+            const commandRemoveMap = new Map();
+
+            function setLegacyCommand(uniqueId, getCommandDefinition) {
+                const { name, action, options } = getCommandDefinition(env);
+                removeLegacyCommand(uniqueId);
+                commandRemoveMap.set(uniqueId, command.add(name, action, options));
+            }
+
+            function removeLegacyCommand(uniqueId) {
+                if (commandRemoveMap.has(uniqueId)) {
+                    const removeCommand = commandRemoveMap.get(uniqueId);
+                    removeCommand();
+                    commandRemoveMap.delete(uniqueId);
+                }
+            }
+
+            legacyEnv.bus.on("set_legacy_command", null, setLegacyCommand);
+            legacyEnv.bus.on("remove_legacy_command", null, removeLegacyCommand);
+        },
+    };
+}
+
+export function makeLegacyDropdownService(legacyEnv) {
+    return {
+        dependencies: ["ui", "hotkey"],
+        start(_, { ui, hotkey }) {
+            legacyEnv.services.ui = ui;
+            legacyEnv.services.hotkey = hotkey;
+        },
+    };
+}
+
+export function makeLegacySessionService(legacyEnv, session) {
+    return {
+        dependencies: ["user"],
+        start(env) {
+            // userContext, Object.create is incompatible with legacy new Context
+            function mapContext() {
+                return Object.assign({}, env.services.user.context);
+            }
+            function setContext(update) {
+                env.services.user.updateContext(update);
+            }
+            Object.defineProperty(legacyEnv.session, "userContext", {
+                get: () => mapContext(),
+                set: (update) => {
+                    setContext(update);
+                },
+            });
+            Object.defineProperty(session, "user_context", {
+                get: () => mapContext(),
+                set: (update) => {
+                    setContext(update);
+                },
+            });
+        },
+    };
+}
+
+export function mapLegacyEnvToWowlEnv(legacyEnv, wowlEnv) {
+    // rpc
+    legacyEnv.session.rpc = (...args) => {
+        let rejection;
+        const prom = new Promise((resolve, reject) => {
+            const [route, params, settings = {}] = args;
+            // Add user context in kwargs if there are kwargs
+            if (params && params.kwargs) {
+                params.kwargs.context = Object.assign(
+                    {},
+                    legacyEnv.session.user_context,
+                    params.kwargs.context,
+                );
+            }
+            const jsonrpc = wowlEnv.services.rpc(route, params, {
+                silent: settings.shadow,
+                xhr: settings.xhr,
+            });
+            rejection = () => {
+                jsonrpc.abort();
+            };
+            jsonrpc.then(resolve).catch((reason) => {
+                if (reason instanceof RPCError || reason instanceof ConnectionLostError) {
+                    // we do not reject an error here because we want to pass through
+                    // the legacy guardedCatch code
+                    reject({ message: reason, event: $.Event(), legacy: true });
+                } else if (reason instanceof ConnectionAbortedError) {
+                    reject({ message: reason.message, event: $.Event("abort") });
+                } else {
+                    reject(reason);
+                }
+            });
+>>>>>>> 026f3fabbc80 (temp)
         });
     }
 
