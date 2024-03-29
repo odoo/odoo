b73031e98e1ecb0303874c9d6e9405c37e946b33
Rémi Rahir (rar) <rar@odoo.com>
2024-03-29 12:22:17 +0000

    [FIX] spreadsheet: Wait for full evaluation before export xlsx
    Task: 3839559
    X-original-commit: d1cd5e10d2ca3b694d6f53e7d19b1d8c398573b0

--- addons/spreadsheet/static/src/actions/spreadsheet_download_action.js
+++ addons/spreadsheet/static/src/actions/spreadsheet_download_action.js
@@ -20,6 +20,80 @@ async function downloadSpreadsheet(env, action) {
     });
 }
 
+<<<<<<< HEAD
+||||||| parent of 6fef8ecc8544 (temp)
+/**
+ * Ensure that the spreadsheet does not contains cells that are in loading state
+ * @param {Model} model
+ * @returns {Promise<void>}
+ */
+async function waitForDataLoaded(model) {
+    const dataSources = model.config.dataSources;
+    return new Promise((resolve, reject) => {
+        function check() {
+            model.dispatch("EVALUATE_CELLS");
+            if (isLoaded(model)) {
+                dataSources.removeEventListener("data-source-updated", check);
+                resolve();
+            }
+        }
+        dataSources.addEventListener("data-source-updated", check);
+        check();
+    });
+}
+
+function isLoaded(model) {
+    for (const sheetId of model.getters.getSheetIds()) {
+        for (const cell of Object.values(model.getters.getCells(sheetId))) {
+            if (
+                cell.evaluated &&
+                cell.evaluated.type === "error" &&
+                cell.evaluated.error.message === _t("Data is loading")
+            ) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+=======
+/**
+ * Ensure that the spreadsheet does not contains cells that are in loading state
+ * @param {Model} model
+ * @returns {Promise<void>}
+ */
+export async function waitForDataLoaded(model) {
+    const dataSources = model.config.dataSources;
+    return new Promise((resolve, reject) => {
+        function check() {
+            model.dispatch("EVALUATE_CELLS");
+            if (isLoaded(model)) {
+                dataSources.removeEventListener("data-source-updated", check);
+                resolve();
+            }
+        }
+        dataSources.addEventListener("data-source-updated", check);
+        check();
+    });
+}
+
+function isLoaded(model) {
+    for (const sheetId of model.getters.getSheetIds()) {
+        for (const cell of Object.values(model.getters.getCells(sheetId))) {
+            if (
+                cell.evaluated &&
+                cell.evaluated.type === "error" &&
+                cell.evaluated.error.message === _t("Data is loading")
+            ) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+>>>>>>> 6fef8ecc8544 (temp)
 registry
     .category("actions")
     .add("action_download_spreadsheet", downloadSpreadsheet, { force: true });
