f1835cd793b0e933c763c2b03ee087eaeebaaa67
Alexandre Kühn <aku@odoo.com>
2024-03-29 15:46:23 +0100

    [PERF] mail: reduce reactive observe in messaging components
    Component handler methods were using reactive owned by
    the component to invoke actions. We want to pass reactive
    as these actions may mutate them and require re-rendering,
    but the component doesn't need to observe everything that
    goes from calling action handler code.
    This commit reduces amount of reactive observes by using
    `toRaw()` on records, which are non-reactive records.
    The internal code of models ensures that writing on record
    internal proxy always write on a reactive if needed, hence
    why it's safe to pass this non-reactive proxy for it being
    potentially muted and properly observed by reactive observers.
    
    Part-of: odoo/odoo#159914

--- addons/mail/static/src/core/common/chat_window.js
+++ addons/mail/static/src/core/common/chat_window.js
@@ -11,7 +11,7 @@ import {
 } from "@mail/utils/common/hooks";
 import { isEventHandled } from "@web/core/utils/misc";
 
-import { Component, useChildSubEnv, useRef, useState } from "@odoo/owl";
+import { Component, toRaw, useChildSubEnv, useRef, useState } from "@odoo/owl";
 
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
@@ -84,6 +84,7 @@ export class ChatWindow extends Component {
     }
 
     onKeydown(ev) {
+        const chatWindow = toRaw(this.props.chatWindow);
         if (ev.target.closest(".o-dropdown") || ev.target.closest(".o-dropdown--menu")) {
             return;
         }
@@ -103,9 +104,7 @@ export class ChatWindow extends Component {
                 this.close({ escape: true });
                 break;
             case "Tab": {
-                const index = this.chatWindowService.visible.findIndex((cw) =>
-                    cw.eq(this.props.chatWindow)
-                );
+                const index = this.chatWindowService.visible.findIndex((cw) => cw.eq(chatWindow));
                 if (index === 0) {
                     this.chatWindowService.visible[this.chatWindowService.visible.length - 1]
                         .autofocus++;
@@ -124,18 +123,20 @@ export class ChatWindow extends Component {
     }
 
     toggleFold() {
+        const chatWindow = toRaw(this.props.chatWindow);
         if (this.ui.isSmall || this.state.actionsMenuOpened) {
             return;
         }
-        if (this.props.chatWindow.hidden) {
-            this.chatWindowService.makeVisible(this.props.chatWindow);
+        if (chatWindow.hidden) {
+            this.chatWindowService.makeVisible(chatWindow);
         } else {
-            this.chatWindowService.toggleFold(this.props.chatWindow);
+            this.chatWindowService.toggleFold(chatWindow);
         }
     }
 
     async close(options) {
-        await this.chatWindowService.close(this.props.chatWindow, options);
+        const chatWindow = toRaw(this.props.chatWindow);
+        await this.chatWindowService.close(chatWindow, options);
     }
 
     get actionsMenuTitleText() {
@@ -143,7 +144,8 @@ export class ChatWindow extends Component {
     }
 
     async renameThread(name) {
-        await this.threadService.renameThread(this.thread, name);
+        const thread = toRaw(this.thread);
+        await this.threadService.renameThread(thread, name);
         this.state.editingName = false;
     }
 
