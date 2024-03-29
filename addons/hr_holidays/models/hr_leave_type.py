cc62a6f555e8bd21178fee56afa5b2764c86c499
Youssef Bashandy <yoba@odoo.com>
2024-03-29 17:06:41 +0200

    [FIX] hr_holidays : saving 'default_date_from' with another key
    Steps to reproduce:
    	1- Create an allocation with validity date (e.g. 01-01-2024 -> 30-06-2024)
    	 and another one starts after the first one (e.g. 01-07-2024 -> 31-12-2024)
    	2- Go to Time off module and select a date in the second allocation's period
    	3- Click on the Time off type dropdown menu
    	4- You will see the first allocation displayed not the second one
    
    Current behavior before PR:
    The display name of some leaves gets computed in a wrong way.
    This is happening because after fetching the right allocation
    we compute the display name but this time we don't have
    the 'default_date_from' in context so since
    it became one of the fields that triggers '_compute_leaves'
    https://github.com/odoo/odoo/blob/17.0/addons/hr_holidays/models/hr_leave_type.py#L218:L219
    we compute the leaves once again but the target_date will be none
    and it will get assigned with today's date in 'get_allocation_data'
    https://github.com/odoo/odoo/blob/17.0/addons/hr_holidays/models/hr_leave_type.py#L380:L381
    
    Desired behavior after PR is merged:
    This has been solved by saving the date attribute in the context
    with another name as when computing the display_name we call sudo so clean_context()
    removes the 'default_' context keys. Now when it gets removed we are
    going to have the same value but with another name.
    
    opw-3797696
    
    closes odoo/odoo#162526
    
    X-original-commit: 9dac77e4142623ae6f228235d8ecace77f5364d3
    Signed-off-by: Youssef Bashandy (yoba) <yoba@odoo.com>

--- addons/hr_holidays/models/hr_leave_type.py
+++ addons/hr_holidays/models/hr_leave_type.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 from datetime import time, datetime
 
 from odoo import api, fields, models
-from odoo.tools import format_date
+from odoo.tools import format_date, frozendict
 from odoo.tools.translate import _
 from odoo.tools.float_utils import float_round
 
@@ -220,6 +220,12 @@ class HolidaysType(models.Model):
     def _compute_leaves(self):
         employee = self.env['hr.employee']._get_contextual_employee()
         target_date = self._context['default_date_from'] if 'default_date_from' in self._context else None
+        # This is a workaround to save the date value in context for next triggers
+        # when context gets cleaned and 'default_' context keys gets removed
+        if target_date:
+            self.env.context = frozendict(self.env.context, leave_date_from=self._context['default_date_from'])
+        else:
+            target_date = self._context.get('leave_date_from', None)
         data_days = self.get_allocation_data(employee, target_date)[employee]
         for holiday_status in self:
             result = [item for item in data_days if item[0] == holiday_status.name]
