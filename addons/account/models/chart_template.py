odoo/odoo

commit f2861b9d8d50f59d06be45ae227abe2463b04379

[FIX] account: check duplicate accounts by normalized code
Function `_pre_reload_data` checks for the existance of accounts with the same
code to avoid creating a duplicate. It does so however comparing with the code
before normalizing it with the length from the template, failing to find
possible duplicates with a normalized code.

closes odoo/odoo#156100

X-original-commit: 181186e84b8d893bc7934f1ee3e56cb6233938b3
Signed-off-by: William André (wan) <wan@odoo.com>

addons/account/models/chart_template.py
@@ -297,18 +297,19 @@ class AccountChartTemplate(models.AbstractModel):
                 elif model_name == 'account.account':
                     # Point or create xmlid to existing record to avoid duplicate code
                     account = self.ref(xmlid, raise_if_not_found=False)
-                    if not account or (account and account.code != values['code']):
-                        existing_account = self.env['account.account'].search([
-                            ('code', '=', values['code']),
-                            ('company_id', '=', company.id),
-                        ])
-                        if existing_account:
+                    normalized_code = f'{values["code"]:<0{int(template_data.get("code_digits", 6))}}'
+                    if not account or not re.match(f'^{values["code"]}0*$', account.code):
+                        query = self.env['account.account']._search([('company_id', '=', company.id)])
+                        query.add_where("account_account.code SIMILAR TO %s", [f'{values["code"]}0*'])
+                        accounts = self.env['account.account'].browse(query)
+                        account = accounts.sorted(key=lambda x: x.code != normalized_code)[0] if accounts else None
+                        if account:
                             self.env['ir.model.data']._update_xmlids([{
                                 'xml_id': f"account.{company.id}_{xmlid}",
-                                'record': existing_account,
+                                'record': account,
                                 'noupdate': True,
                             }])
-                            account = existing_account
+
                     # on existing accounts, only tag_ids are to be updated using default data
                     if account and 'tag_ids' in data[model_name][xmlid]:
                         data[model_name][xmlid] = {'tag_ids': data[model_name][xmlid]['tag_ids']}

commit 9f4fd19cf9015e22dc77fa9828521727a4730b95

[FIX] account: CoA load: only update tags in existing accounts
The intended behavior to only update tags and no other fields on existing
accounts during CoA loading was originally introduced in
https://github.com/odoo/odoo/pull/125320 and has recently been broken by commit
f2861b9d8d50 (https://github.com/odoo/odoo/pull/155256). This has been
recognized because of failing requests in the upgrade platform when it tried to
update the `code` field, which lead to excessive recomputations and in turn to
MemoryErrors.

The problem is that the tests, if the current `xmlid` constitutes a record
update or a new record[^1], need to be correct, no matter if the code above[^2]
managed to update the `xmlid` pointer or not. Thus, restore the previous logic
regarding that by only assigning the alternative record to the `account` var if
it is not None.

[^1]: https://github.com/odoo/odoo/blob/35bb495d5e5a8dac563ba4dee1702d77e4b71461/addons/account/models/chart_template.py#L312
      and
      https://github.com/odoo/odoo/blob/35bb495d5e5a8dac563ba4dee1702d77e4b71461/addons/account/models/chart_template.py#L314

[^2]: https://github.com/odoo/odoo/blob/35bb495d5e5a8dac563ba4dee1702d77e4b71461/addons/account/models/chart_template.py#L303

addons/account/models/chart_template.py
@@ -326,13 +326,14 @@ class AccountChartTemplate(models.AbstractModel):
                         query = self.env['account.account']._search([('company_id', '=', company.id)])
                         query.add_where("account_account.code SIMILAR TO %s", [f'{values["code"]}0*'])
                         accounts = self.env['account.account'].browse(query)
-                        account = accounts.sorted(key=lambda x: x.code != normalized_code)[0] if accounts else None
-                        if account:
+                        existing_account = accounts.sorted(key=lambda x: x.code != normalized_code)[0] if accounts else None
+                        if existing_account:
                             self.env['ir.model.data']._update_xmlids([{
                                 'xml_id': f"account.{company.id}_{xmlid}",
                                 'record': account,
                                 'noupdate': True,
                             }])
+                            account = existing_account
 
                     # on existing accounts, only tag_ids are to be updated using default data
                     if account and 'tag_ids' in data[model_name][xmlid]: