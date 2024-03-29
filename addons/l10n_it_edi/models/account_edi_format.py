odoo/odoo

commit 4bf01e0d778877bb7976af852eb594a7e82124a1

[FIX] l10n_it_edi: enable import of outgoing invoices
Currently we assume that all imported invoices are incoming (i.e. bills).
This was i.e. done since the tax agency only sends users bills.

But some clients import invoices from other software (i.e. onboarding/starting).

After this PR we decide whether the invoice is outgoing or ingoing and
import the invoice correctly in either case.

task-3650355

closes odoo/odoo#159441

X-original-commit: 292521ddd566cd3d49df2dd2eb4e2850dc47620a
Signed-off-by: Josse Colpaert <jco@odoo.com>
Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>

addons/l10n_it_edi/models/account_edi_format.py
@@ -252,6 +252,25 @@ class AccountEdiFormat(models.Model):
             'TD19': dict(move_types=['in_invoice', 'in_refund'], import_type='in_invoice', simplified=False, self_invoice=True, services_or_goods="consu", goods_in_italy=True),
         }
 
+    @api.model
+    def _l10n_it_buyer_seller_info(self):
+        return {
+            'buyer': {
+                'role': 'buyer',
+                'section_xpath': './/CessionarioCommittente',
+                'vat_xpath': '//CessionarioCommittente//IdCodice',
+                'codice_fiscale_xpath': '//CessionarioCommittente//CodiceFiscale',
+                'type_tax_use_domain': [('type_tax_use', '=', 'purchase')],
+            },
+            'seller': {
+                'role': 'seller',
+                'section_xpath': './/CedentePrestatore',
+                'vat_xpath': '//CedentePrestatore//IdCodice',
+                'codice_fiscale_xpath': '//CedentePrestatore//CodiceFiscale',
+                'type_tax_use_domain': [('type_tax_use', '=', 'sale')],
+            },
+        }
+
     def _l10n_it_get_invoice_features_for_document_type_selection(self, invoice):
         """ Returns a dictionary of features to be compared with the TDxx FatturaPA
             document type requirements. """
@@ -388,14 +407,15 @@ class AccountEdiFormat(models.Model):
     def _is_fattura_pa(self, filename, tree=None):
         return self.code == 'fattura_pa' and self._check_filename_is_fattura_pa(filename)
 
-    def _l10n_it_get_partner_invoice(self, tree, company):
-        # Partner (first step to avoid warning 'Warning! You must first select a partner.'). <1.2>
-        elements = tree.xpath('//CedentePrestatore//IdCodice')
+    def _l10n_it_get_partner_invoice(self, tree, company, partner_info=None):
+        if partner_info is None:
+            partner_info = self._l10n_it_buyer_seller_info()['seller']
+        elements = tree.xpath(partner_info['vat_xpath'])
         partner = elements and self.env['res.partner'].search(
             ['&', ('vat', 'ilike', elements[0].text), '|', ('company_id', '=', company.id), ('company_id', '=', False)],
             limit=1)
         if not partner:
-            elements = tree.xpath('//CedentePrestatore//CodiceFiscale')
+            elements = tree.xpath(partner_info['codice_fiscale_xpath'])
             if elements:
                 codice = elements[0].text
                 domains = [[('l10n_it_codice_fiscale', '=', codice)]]
@@ -406,7 +426,7 @@ class AccountEdiFormat(models.Model):
                                      self.env['res.partner']._l10n_it_normalize_codice_fiscale(codice))])
                 partner = elements and self.env['res.partner'].search(
                     AND([OR(domains), OR([[('company_id', '=', company.id)], [('company_id', '=', False)]])]), limit=1)
-        if not partner:
+        if not partner and partner_info['role'] == 'seller':
             elements = tree.xpath('//DatiTrasmissione//Email')
             partner = elements and self.env['res.partner'].search(
                 ['&', '|', ('email', '=', elements[0].text), ('l10n_it_pec_email', '=', elements[0].text), '|',
@@ -432,7 +452,6 @@ class AccountEdiFormat(models.Model):
             ('company_id', '=', company.id),
             ('amount', '=', percentage),
             ('amount_type', '=', 'percent'),
-            ('type_tax_use', '=', 'purchase'),
         ] + (extra_domain or [])
 
         # As we're importing vendor bills, we're excluding Reverse Charge Taxes
@@ -443,10 +462,13 @@ class AccountEdiFormat(models.Model):
 
         return taxes[0] if taxes else taxes
 
-    def _l10n_it_edi_get_extra_info(self, company, document_type, body_tree):
+    def _l10n_it_edi_get_extra_info(self, company, document_type, body_tree, incoming=True):
         """ This function is meant to collect other information that has to be inserted on the invoice lines by submodules.
             :return extra_info, messages_to_log"""
-        return {'simplified': self._l10n_it_is_simplified_document_type(document_type)}, []
+        return {
+            'simplified': self._l10n_it_is_simplified_document_type(document_type),
+            'type_tax_use_domain': [('type_tax_use', '=', 'purchase' if incoming else 'sale')],
+        }, []
 
     def _import_fattura_pa(self, tree, invoice):
         """ Decodes a fattura_pa invoice into an invoice.
@@ -455,7 +477,39 @@ class AccountEdiFormat(models.Model):
         :param invoice: the invoice to update or an empty recordset.
         :returns:       the invoice where the fattura_pa data was imported.
         """
+        buyer_seller_info = self._l10n_it_buyer_seller_info()
+
         company = invoice.company_id
+
+        # There are 2 cases:
+        # - cron:
+        #     * Move direction (incoming / outgoing) flexible (no 'default_move_type')
+        #     * I.e. used for import from tax agency
+        # - "Upload" button (invoices / bills view)
+        #     * Fixed move direction; the button sets the 'default_move_type'
+        default_move_type = self.env.context.get('default_move_type')
+        if default_move_type is None:
+            incoming_possibilities = [True, False]
+        elif default_move_type in invoice.get_purchase_types(include_receipts=True):
+            incoming_possibilities = [True]
+        elif default_move_type in invoice.get_sale_types(include_receipts=True):
+            incoming_possibilities = [False]
+        else:
+            _logger.warning("Cannot handle default_move_type '%s'.", default_move_type)
+            return
+
+        for incoming in incoming_possibilities:
+            company_role, partner_role = ('buyer', 'seller') if incoming else ('seller', 'buyer')
+            company_info = buyer_seller_info[company_role]
+            elements = tree.xpath(company_info['vat_xpath'])
+            if elements and elements[0].text.casefold() in (company.vat or '').casefold():
+                break
+            elements = tree.xpath(company_info['codice_fiscale_xpath'])
+            if elements and elements[0].text.casefold() in (company.l10n_it_codice_fiscale or '').casefold():
+                break
+        else:
+            invoice.message_post(body=_("Your company's VAT number and Fiscal Code haven't been found in the buyer and/or seller sections inside the document."))
+            return
 
         # Refund type.
         # TD01 == invoice
@@ -474,19 +528,22 @@ class AccountEdiFormat(models.Model):
         if not move_type:
             move_type = "in_invoice"
             _logger.info('Document type not managed: %s. Invoice type is set by default.', document_type)
+        if not incoming and move_type.startswith('in_'):
+            move_type = 'out' + move_type[2:]
 
         invoice.move_type = move_type
 
         # Collect extra info from the XML that may be used by submodules to further put information on the invoice lines
-        extra_info, message_to_log = self._l10n_it_edi_get_extra_info(company, document_type, tree)
+        extra_info, message_to_log = self._l10n_it_edi_get_extra_info(company, document_type, tree, incoming=incoming)
 
-        partner = self._l10n_it_get_partner_invoice(tree, company)
+        partner_info = buyer_seller_info[partner_role]
+        partner = self._l10n_it_get_partner_invoice(tree, company, partner_info)
         if partner:
             invoice.partner_id = partner
         else:
             message = Markup("<br/>").join((
-                _("Vendor not found, useful informations from XML file:"),
-                invoice._compose_info_message(tree, '//CedentePrestatore')
+                _("Partner not found, useful informations from XML file:"),
+                invoice._compose_info_message(tree, partner_info['section_xpath'])
             ))
             message_to_log.append(message)
 
@@ -766,11 +823,12 @@ class AccountEdiFormat(models.Model):
         natura_element = element.xpath('.//Natura')
         invoice_line_form.tax_ids = [Command.set([])]
         if percentage is not None:
+            type_tax_use_domain = extra_info.get('type_tax_use_domain', [('type_tax_use', '=', 'purchase')])
             l10n_it_kind_exoneration = bool(natura_element) and natura_element[0].text
             conditions = (
                 l10n_it_kind_exoneration and [('l10n_it_kind_exoneration', '=', l10n_it_kind_exoneration)]
                 or [('l10n_it_has_exoneration', '=', False)]
-            )
+            ) + type_tax_use_domain
             tax = self._l10n_it_edi_search_tax_for_import(company, percentage, conditions)
             if tax:
                 invoice_line_form.tax_ids += tax

addons/l10n_it_edi_withholding/models/account_edi_format.py
@@ -34,8 +34,10 @@ class AccountEdiFormat(models.Model):
                 errors.append(_("Bad tax configuration for line %s, there must be one Withholding tax and one Pension Fund tax at max.", invoice_line.name))
         return errors
 
-    def _l10n_it_edi_get_extra_info(self, company, document_type, body_tree):
-        extra_info, message_to_log = super()._l10n_it_edi_get_extra_info(company, document_type, body_tree)
+    def _l10n_it_edi_get_extra_info(self, company, document_type, body_tree, incoming=True):
+        extra_info, message_to_log = super()._l10n_it_edi_get_extra_info(company, document_type, body_tree, incoming=incoming)
+
+        type_tax_use_domain = extra_info['type_tax_use_domain']
 
         withholding_elements = body_tree.xpath('.//DatiGeneraliDocumento/DatiRitenuta')
         withholding_taxes = []
@@ -49,8 +51,9 @@ class AccountEdiFormat(models.Model):
             withholding_tax = self._l10n_it_edi_search_tax_for_import(
                 company,
                 withholding_percentage,
-                [('l10n_it_withholding_type', '=', withholding_type),
-                 ('l10n_it_withholding_reason', '=', withholding_reason)],
+                ([('l10n_it_withholding_type', '=', withholding_type),
+                  ('l10n_it_withholding_reason', '=', withholding_reason)]
+                 + type_tax_use_domain),
                 vat_only=False)
             if withholding_tax:
                 withholding_taxes.append(withholding_tax)
@@ -73,7 +76,8 @@ class AccountEdiFormat(models.Model):
             pension_fund_tax = self._l10n_it_edi_search_tax_for_import(
                 company,
                 tax_factor_percent,
-                [('l10n_it_pension_fund_type', '=', pension_fund_type)],
+                ([('l10n_it_pension_fund_type', '=', pension_fund_type)]
+                 + type_tax_use_domain),
                 vat_only=False)
             if pension_fund_tax:
                 pension_fund_taxes.append(pension_fund_tax)
@@ -88,6 +92,8 @@ class AccountEdiFormat(models.Model):
 
     def _import_fattura_pa_line(self, element, invoice_line_form, extra_info):
         messages_to_log = super()._import_fattura_pa_line(element, invoice_line_form, extra_info)
+
+        type_tax_use_domain = extra_info['type_tax_use_domain']
 
         for withholding_tax in extra_info.get('withholding_taxes', []):
             withholding_tags = element.xpath("Ritenuta")
@@ -117,7 +123,7 @@ class AccountEdiFormat(models.Model):
             enasarco_tax = self._l10n_it_edi_search_tax_for_import(
                 company,
                 enasarco_percentage,
-                [('l10n_it_pension_fund_type', '=', 'TC07')],
+                [('l10n_it_pension_fund_type', '=', 'TC07')] + type_tax_use_domain,
                 vat_only=False)
             if enasarco_tax:
                 invoice_line_form.tax_ids |= enasarco_tax

addons/l10n_it_edi/i18n/it.po
@@ -592,6 +592,12 @@ msgid "Payment due date invalid in XML file:"
 msgstr "Data Pagamento non valida nel file XML:"
 
 #. module: l10n_it_edi
+#: code:addons/l10n_it_edi/models/account_edi_format.py:0
+#, python-format
+msgid "Partner not found, useful informations from XML file:"
+msgstr "Partner non trovato, informazioni dal file XML:"
+
+#. module: l10n_it_edi
 #: model:ir.model.fields.selection,name:l10n_it_edi.selection__res_company__l10n_it_eco_index_sole_shareholder__sm
 msgid "Più soci"
 msgstr ""
@@ -967,13 +973,6 @@ msgstr "Fatture d'acquisto inviate come autofatture all'SdI richiedono "
 #. odoo-python
 #: code:addons/l10n_it_edi/models/account_edi_format.py:0
 #, python-format
-msgid "Vendor not found, useful informations from XML file:"
-msgstr "Fornitore non trovato, informazioni dal file XML:"
-
-#. module: l10n_it_edi
-#. odoo-python
-#: code:addons/l10n_it_edi/models/account_edi_format.py:0
-#, python-format
 msgid "You are not allowed to check the status of this invoice."
 msgstr "Non sei autorizzato a controllare lo stato di questa fattura."
 
@@ -993,6 +992,17 @@ msgstr ""
 #, python-format
 msgid "You must select a tax representative."
 msgstr "Devi selezionare un rappresentante fiscale"
+
+#. module: l10n_it_edi
+#. odoo-python
+#: code:addons/l10n_it_edi/models/account_edi_format.py:0
+#, python-format
+msgid ""
+"Your company's VAT number and Fiscal Code haven't been found in the buyer "
+"and/or seller sections inside the document."
+msgstr ""
+"La Partita IVA e il Codice Fiscale della tua Azienda non sono stati "
+"trovati nelle sezioni acquirente e/o venditore del documento."
 
 #. module: l10n_it_edi
 #. odoo-python

addons/l10n_it_edi/i18n/l10n_it_edi.pot
@@ -566,6 +566,12 @@ msgid "Payment due date invalid in XML file:"
 msgstr ""
 
 #. module: l10n_it_edi
+#: code:addons/l10n_it_edi/models/account_edi_format.py:0
+#, python-format
+msgid "Partner not found, useful informations from XML file:"
+msgstr ""
+
+#. module: l10n_it_edi
 #: model:ir.model.fields.selection,name:l10n_it_edi.selection__res_company__l10n_it_eco_index_sole_shareholder__sm
 msgid "Più soci"
 msgstr ""
@@ -891,13 +897,6 @@ msgstr ""
 #. odoo-python
 #: code:addons/l10n_it_edi/models/account_edi_format.py:0
 #, python-format
-msgid "Vendor not found, useful informations from XML file:"
-msgstr ""
-
-#. module: l10n_it_edi
-#. odoo-python
-#: code:addons/l10n_it_edi/models/account_edi_format.py:0
-#, python-format
 msgid "You are not allowed to check the status of this invoice."
 msgstr ""
 
@@ -916,6 +915,15 @@ msgstr ""
 #: code:addons/l10n_it_edi/models/res_company.py:0
 #, python-format
 msgid "You must select a tax representative."
+msgstr ""
+
+#. module: l10n_it_edi
+#. odoo-python
+#: code:addons/l10n_it_edi/models/account_edi_format.py:0
+#, python-format
+msgid ""
+"Your company's VAT number and Fiscal Code haven't been found in the buyer "
+"and/or seller sections inside the document."
 msgstr ""
 
 #. module: l10n_it_edi

commit fbb812fb8a7d85ffbd196a28ba79e18f005ac4a5

[FIX] l10n_it_edi: fix info from xml if no partner found
Whe importing an invoice:
In case no partner is found the information from the XML
about the partner is printed to the chatter.
The commit 4bf01e0d778877bb7976af852eb594a7e82124a1 (16.3)
broke this since the xpath of the partner section starts with an '.'
(like in previous versions; FW-port error) but it should not.

This commit fixes the xpath of the partner section.

addons/l10n_it_edi/models/account_edi_format.py
@@ -257,14 +257,14 @@ class AccountEdiFormat(models.Model):
         return {
             'buyer': {
                 'role': 'buyer',
-                'section_xpath': './/CessionarioCommittente',
+                'section_xpath': '//CessionarioCommittente',
                 'vat_xpath': '//CessionarioCommittente//IdCodice',
                 'codice_fiscale_xpath': '//CessionarioCommittente//CodiceFiscale',
                 'type_tax_use_domain': [('type_tax_use', '=', 'purchase')],
             },
             'seller': {
                 'role': 'seller',
-                'section_xpath': './/CedentePrestatore',
+                'section_xpath': '//CedentePrestatore',
                 'vat_xpath': '//CedentePrestatore//IdCodice',
                 'codice_fiscale_xpath': '//CedentePrestatore//CodiceFiscale',
                 'type_tax_use_domain': [('type_tax_use', '=', 'sale')],