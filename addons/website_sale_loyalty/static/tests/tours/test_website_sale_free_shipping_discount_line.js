c8f0014035d278e6e3d5e3430f2a70d8dcf2ca4d
kdeb@odoo.com <kdeb@odoo.com>
2024-04-01 07:00:00 +0000

    [FIX] website_sale(_loyalty)_delivery: set correct delivery prices
    Issue:
    
    - when we have a shipping method that has a discount the shipping price
    and the delivery prices are not displayed correctly
    (see attached screenshot).
    
    Steps To Reproduce:
    
    - configure the sale app to have the discount&loyalty and free
    shipping options enabled.
    - create a product that cost 100$.
    - create a new discount/loyalty where type is promotions and set rule to
     if minimum 300$ spent the reward is free shipping max to 75$.
    - create a shipping method based on rules where the delivery product
    is a service that costs 100$.
    - in your shipping mehtod pricing add a line with if quantity >= 3.0
    then fixed price 100.0 and publish it.
    - in your website shop select 3 items from the product you created. and
    select your delivery method.
    - notice the prices not updating correctly.
    
    Solution:
    
    - Before the fix we always assumed that free_shipping_lines are
    equal to 0.
    
    - free_shipping_lines might be shipping discounts not actual free
    shipping.so I adjusted update_eshop_carrier method to make it handle the
     case of discounted shipping.
    
    opw-3718885
    
    closes odoo/odoo#172430
    
    X-original-commit: 6aafdd95ad5cdbaf02b5e600d9da1662ea427653
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>
    Signed-off-by: Kawtar Drissi El Bouzaidi (kdeb) <kdeb@odoo.com>
    Co-authored-by: Valentin Chevalier <vcr@odoo.com>

---
+++ addons/website_sale_loyalty/static/tests/tours/test_website_sale_free_shipping_discount_line.js
@@ -0,0 +1,58 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import wsTourUtils from '@website_sale/js/tours/tour_utils';
+
+registry.category("web_tour.tours").add('check_shipping_discount', {
+    test: true,
+    url: '/shop?search=Plumbus',
+    steps: () => [
+        {
+            content: "select Plumbus",
+            trigger: '.oe_product a:contains("Plumbus")',
+            run: "click",
+        },
+        {
+            content: "add 3 Plumbus into cart",
+            trigger: '#product_details input[name="add_qty"]',
+            run: "edit 3",
+        },
+        {
+            content: "click on 'Add to Cart' button",
+            trigger: '#product_detail form[action^="/shop/cart/update"] #add_to_cart',
+            run: "click",
+        },
+        wsTourUtils.goToCart({quantity: 3}),
+        {
+            content: "go to checkout",
+            trigger: 'a[name="website_sale_main_button"]',
+            run: "click",
+        },
+        {
+            content: "select delivery with rule",
+            trigger: 'li label:contains("delivery with rule")',
+            run: "click",
+        },
+        {
+            content: "check if delivery price is correct'",
+            trigger: "[name='o_delivery_method']:has(.o_delivery_carrier_label:contains('delivery with rule')) span.o_wsale_delivery_price_badge .oe_currency_value:contains(100.00)",
+        },
+        {
+            content: "confirm shipping method",
+            trigger: 'a[href="/shop/confirm_order"]',
+            run: "click",
+        },
+        {
+            trigger: ".accordion-button",
+            run: "click",
+        },
+        {
+            content: "check if delivery price is correct'",
+            trigger: "#order_delivery .oe_currency_value:contains(100.00)",
+        },
+        {
+            content: "check if delivery price is correct'",
+            trigger: "[data-reward-type='shipping'] .oe_currency_value:contains('- 75.00')",
+        },
+    ],
+});
