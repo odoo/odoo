d7f9d370a3a579974f4f716e4d16597d6f415b63
Xavier-Do <xdo@odoo.com>
2024-04-02 10:16:48 +0200

    [FIX] *: adapt tz for Ubuntu Noble
    Some of the non canononical timezones are not present in Ubuntu Noble,
    it would be a better practice to only use canonical timezones in data
    and tests.
    
    Note that this is not a real fix for all cases since the database that
    ran on Ubuntu Jammy and are moved to an ubuntu Noble server will have
    the issue with timezones already in database.
    
    One of the possible fix would be to manage that during upgrades, but
    this isn't a verry flexible solution since upgrade are meant to manage
    chyange of version, not change of server. If an old 17.0 versions needs
    to be moved to a Noble server, this won't work.
    
    Another solution would be to install package like tzdata-legacy that may
    keep the old timezones but it is not the only think since TAI-10 are
    also in this package. This solution is not ideal because non canonical
    timezone will still be shown in the dropdown. We would need to filter
    them.
    
    A last solution would be to add the support for those old timezones by
    monkeypatching the lib. This way, only new timezones would be shown but
    non canonical one won't crash when used. This is not ideal either
    because we may need to keep this for a while. But in combination with
    the upgrade solution, it may work proprely.
    
    X-original-commit: a38165366088b76573f9d5311523ab3d02f8a309

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -43,6 +43,7 @@ from . import test_res_config
 from . import test_res_lang
 from . import test_search
 from . import test_translate
+from . import test_tz
 # from . import test_uninstall  # loop
 from . import test_user_has_group
 from . import test_views
