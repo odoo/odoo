13237e13e46a3e0b3e5e0609442db792d2670ae0
Rémy Voet (ryv) <ryv@odoo.com>
2024-04-02 16:52:06 +0200

    [FIX] account: fix latest running_balance aggregator
    Since https://github.com/odoo/odoo/pull/127353, the web client only asks
    for the aggregatable field, making the read_group override of
    'account.bank.statement.line' becomes ineffective.
    
    Remove the check done on fields, and return the latest
    running_balance by group when 'show_running_balance_latest' is in the context.

--- addons/account/models/account_bank_statement_line.py
+++ addons/account/models/account_bank_statement_line.py
@@ -404,7 +404,7 @@ class AccountBankStatementLine(models.Model):
         show_running_balance = False
         # We loop over the content of groupby because the groupby date is in the form of "date:granularity"
         for el in groupby:
-            if (el == 'statement_id' or el == 'journal_id' or el.startswith('date')) and 'running_balance' in fields:
+            if (el == 'statement_id' or el == 'journal_id' or el.startswith('date')) and self.env.context.get('show_running_balance_latest'):
                 show_running_balance = True
                 break
         if show_running_balance:
