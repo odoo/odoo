0b19fda83371756736b86d4016ccf1a5af9d1a38
Pedram (pebr) <pebr@odoo.com>
2024-04-02 16:43:28 +0200

    [FIX] point_of_sale: enable customer barcode scanning
    Prior to this commit, scanning a customer's barcode was not functioning
    as expected. This fix ensures that customer barcodes can be
    successfully scanned.
    
    opw-3825471
    
    X-original-commit: e6eddbdd1fca8bfae904f014b308fc9274b975b8

--- addons/point_of_sale/static/tests/tours/helpers/ProductScreenTourMethods.js
+++ addons/point_of_sale/static/tests/tours/helpers/ProductScreenTourMethods.js
@@ -117,6 +117,16 @@ export function clickPartnerButton() {
 export function clickCustomer(name) {
     return [PartnerList.clickPartner(name), goBackToMainScreen()];
 }
+export function customerIsSelected(name) {
+    return [
+        clickReview(),
+        {
+            content: `customer '${name}' is selected`,
+            trigger: `.product-screen .set-partner:contains("${name}")`,
+            isCheck: true,
+        },
+    ];
+}
 export function clickRefund() {
     return [clickReview(), controlButtonMore(), controlButton("Refund")];
 }
