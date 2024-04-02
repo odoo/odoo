odoo/odoo

commit 64a7a80bd8a87e984b73504113b1c31926557c05

[FIX] account: duplicate bill warning in edit mode
The aim of this commit is to make the duplicate bill warning works in
edit mode.
This is a backport of some fixes that were done in the following
commit: cb694599583f5df1667984fee01bbcc4c8b2d409

Before the commit:
The warning couldn't be triggered in edit mode.

After the commit:
The warning is triggered as soon as the field is unfocused.

closes odoo/odoo#151454

Task-id: None
X-original-commit: 57c51b9c7b70cbc19071a8dfb67713a674caa0a0
Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>
Co-authored-by: moerradi <moer@odoo.com>

addons/account/models/account_move.py
@@ -1534,8 +1534,8 @@ class AccountMove(models.Model):
     def _compute_duplicated_ref_ids(self):
         move_to_duplicate_move = self._fetch_duplicate_supplier_reference()
         for move in self:
-            # Uses move._origin.id to handle records in edition/existing records and 0 for new records, for duplicated reference ID assignment.
-            move.duplicated_ref_ids = move_to_duplicate_move.get(move._origin.id or 0, self.env['account.move'])
+            # Uses move._origin.id to handle records in edition/existing records and 0 for new records
+            move.duplicated_ref_ids = move_to_duplicate_move.get(move._origin, self.env['account.move'])
 
     def _fetch_duplicate_supplier_reference(self, matching_states=('draft', 'posted')):
         moves = self.filtered(lambda m: m.is_purchase_document() and m.ref)
@@ -1547,9 +1547,9 @@ class AccountMove(models.Model):
 
         move_table_and_alias = "account_move AS move"
         place_holders = {}
-        if not moves[0].id: # check if record is in creation/edition in UI
-            # Inject the values of the record directly in the query as a record under creation isn't searchable
-            # in the DB and a record under edition isn't up to date in the db before saving it
+        if not moves[0].id:  # check if record is under creation/edition in UI
+            # New record aren't searchable in the DB and record in edition aren't up to date yet
+            # Replace the table by safely injecting the values in the query
             place_holders = {
                 "id": moves._origin.id or 0,
                 **{
@@ -1584,7 +1584,7 @@ class AccountMove(models.Model):
             **place_holders
         })
         return {
-            res['move_id']: self.env['account.move'].browse(res['duplicate_ids'])
+            self.env['account.move'].browse(res['move_id']): self.env['account.move'].browse(res['duplicate_ids'])
             for res in self.env.cr.dictfetchall()
         }
 

commit 4539b7eaa8cf0ccfc1358f454760382a5ec22c10

[FIX] account: correct redirect on duplicate warning
When we want to post a vendor bill having duplicates, we show a
`RedirectError` that should bring us to the list of duplicate bills.
However, the action domain contained records instead of record ids,
causing a traceback when clicking the button. The issue was introduced
in 64a7a80bd8a87e984b73504113b1c31926557c05.

This commit fixes that by providing a valid domain for the moves.

task-3797825

addons/account/models/account_move.py
@@ -20,6 +20,7 @@ from odoo.tools import (
     date_utils,
     email_re,
     email_split,
+    flatten,
     float_compare,
     float_is_zero,
     float_repr,
@@ -2041,11 +2042,9 @@ class AccountMove(models.Model):
         """ Assert the move which is about to be posted isn't a duplicated move from another posted entry"""
         move_to_duplicate_moves = self.filtered(lambda m: m.state == 'posted')._fetch_duplicate_supplier_reference(matching_states=('posted',))
         if any(duplicate_move for duplicate_move in move_to_duplicate_moves.values()):
-            duplicate_move_ids = list(set(
-                move_id
-                for move_ids in ([current_move_id] + duplicate.ids for current_move_id, duplicate in move_to_duplicate_moves.items() if duplicate)
-                for move_id in move_ids
-            ))
+            duplicate_move_ids = list(set(flatten(
+                move.ids + dup_moves.ids for move, dup_moves in move_to_duplicate_moves.items() if dup_moves
+            )))
             action = self.env['ir.actions.actions']._for_xml_id('account.action_move_line_form')
             action['domain'] = [('id', 'in', duplicate_move_ids)]
             action['views'] = [((view_id, 'list') if view_type == 'tree' else (view_id, view_type)) for view_id, view_type in action['views']]

addons/account/tests/__init__.py
@@ -4,6 +4,7 @@ from . import test_account_move_out_invoice
 from . import test_account_move_out_refund
 from . import test_account_move_in_invoice
 from . import test_account_move_in_refund
+from . import test_account_move_duplicate
 from . import test_account_move_entry
 from . import test_account_move_date_algorithm
 from . import test_invoice_tax_totals

addons/account/tests/test_account_move_duplicate.py
@@ -1,0 +1,31 @@
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+from odoo.tests import tagged
+from odoo.exceptions import RedirectWarning
+
+
+@tagged('post_install', '-at_install')
+class TestAccountMoveDuplicate(AccountTestInvoicingCommon):
+    @classmethod
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass(chart_template_ref=chart_template_ref)
+
+        cls.invoice = cls.init_invoice('in_invoice', products=cls.product_a + cls.product_b)
+
+    def test_in_invoice_multiple_duplicate_reference_constrains(self):
+        """ Ensure that an error is raised on post if some invoices with duplicated ref share the same invoice_date """
+        invoice_1 = self.invoice
+        invoice_1.ref = 'a unique supplier reference that will be copied'
+        invoice_2 = invoice_1.copy(default={'invoice_date': invoice_1.invoice_date})
+        invoice_3 = invoice_1.copy(default={'invoice_date': invoice_1.invoice_date})
+
+        # reassign to trigger the compute method
+        invoices = invoice_1 + invoice_2 + invoice_3
+        invoices.ref = invoice_1.ref
+
+        # test constrains: batch without any previous posted invoice
+        with self.assertRaises(RedirectWarning) as cm:
+            (invoice_1 + invoice_2 + invoice_3).action_post()
+        # Check that the RedirectWarning has a correct domain
+        redirection_domain = cm.exception.args[1]["domain"]
+        self.assertEqual(redirection_domain[0][:2], ("id", "in"))
+        self.assertEqual(set(redirection_domain[0][2]), set(invoices.ids))