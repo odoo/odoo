074be1ed36891065361a3cf40f8e189ba5fccb15
Pierre-Yves Dufays <pydu@odoo.com>
2024-04-02 16:58:51 +0200

    [IMP] various: make website_url relative instead of absolute
    In version 16.0, we have fixed the issue of the button “Go to website” that was
    redirecting users to the home page instead of the intended URL for record with
    no website (see odoo/odoo#148103).
    
    However, this fix didn't solve the underlying problem which is that the
    website_url is an absolute URL instead of relative one in some model
    (slide_channel, slide_slide, ...).
    
    We solve here the root cause by making the website_url relative and add a new
    field website_absolute_url. This field replaces website_url in the modified
    projects (mails, views, ...). Of course, we leave website_url untouched in
    other modules where it must be relative. Especially for the method
    open_website_url of website.published.mixin.
    
    Note that in some places the absolute URL could be replaced by a relative one
    (when a link is displayed on the same website) but as it is functionally
    equivalent, we keep them absolute to keep the changes simple.
    
    Task-3844851
    
    Part-of: odoo/odoo#163098
    Related: odoo/enterprise#61354
    Signed-off-by: Stéphane Debauche (std) <std@odoo.com>

--- addons/website_event_meet/models/event_meeting_room.py
+++ addons/website_event_meet/models/event_meeting_room.py
@@ -32,8 +32,7 @@ class EventMeetingRoom(models.Model):
         super(EventMeetingRoom, self)._compute_website_url()
         for meeting_room in self:
             if meeting_room.id:
-                base_url = meeting_room.event_id.get_base_url()
-                meeting_room.website_url = '%s/event/%s/meeting_room/%s' % (base_url, self.env["ir.http"]._slug(meeting_room.event_id), self.env["ir.http"]._slug(meeting_room))
+                meeting_room.website_url = f'/event/{self.env["ir.http"]._slug(meeting_room.event_id)}/meeting_room/{self.env["ir.http"]._slug(meeting_room)}'
 
     @api.model_create_multi
     def create(self, values_list):
@@ -51,6 +50,10 @@ class EventMeetingRoom(models.Model):
             ("room_participant_count", "=", 0),
             ("room_last_activity", "<", fields.Datetime.now() - self._DELAY_CLEAN),
         ]).active = False
+
+    def get_base_url(self):
+        """As website_id is not defined on this record, we rely on event website_id for base URL."""
+        return self.event_id.get_base_url()
 
     def open_website_url(self):
         """ Overridden to use a relative URL instead of an absolute when website_id is False. """
