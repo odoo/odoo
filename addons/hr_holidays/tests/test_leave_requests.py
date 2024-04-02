b6413cd6d622f92ebe5e609fc63503d1253c652d
Xavier-Do <xdo@odoo.com>
2024-04-02 10:16:48 +0200

    [FIX] *: adapt tz for Ubuntu Noble
    Some of the non canononical timezones are not present in Ubuntu Noble,
    it would be a better practice to only use canonical timezones in data
    and tests.
    
    Note that this is not a real fix for all cases since the database that
    ran on Ubuntu Jammy and are moved to an ubuntu Noble server will have
    the issue with timezones already in database.
    
    One of the possible fix would be to manage that during upgrades, but
    this isn't a verry flexible solution since upgrade are meant to manage
    chyange of version, not change of server. If an old 17.0 versions needs
    to be moved to a Noble server, this won't work.
    
    Another solution would be to install package like tzdata-legacy that may
    keep the old timezones but it is not the only think since TAI-10 are
    also in this package. This solution is not ideal because non canonical
    timezone will still be shown in the dropdown. We would need to filter
    them.
    
    A last solution would be to add the support for those old timezones by
    monkeypatching the lib. This way, only new timezones would be shown but
    non canonical one won't crash when used. This is not ideal either
    because we may need to keep this for a while. But in combination with
    the upgrade solution, it may work proprely.

--- addons/hr_holidays/tests/test_leave_requests.py
+++ addons/hr_holidays/tests/test_leave_requests.py
@@ -239,7 +239,7 @@ class TestLeaveRequests(TestHrHolidaysCommon):
     @mute_logger('odoo.models.unlink', 'odoo.addons.mail.models.mail_mail')
     def test_timezone_employee_leave_request(self):
         """ Create a leave request for an employee in another timezone """
-        self.employee_emp.tz = 'NZ'  # GMT+12
+        self.employee_emp.tz = 'Pacific/Auckland'  # GMT+12
         leave = self.env['hr.leave'].new({
             'employee_id': self.employee_emp.id,
             'holiday_status_id': self.holidays_type_1.id,
@@ -256,7 +256,7 @@ class TestLeaveRequests(TestHrHolidaysCommon):
     def test_timezone_company_leave_request(self):
         """ Create a leave request for a company in another timezone """
         company = self.env['res.company'].create({'name': "Hergé"})
-        company.resource_calendar_id.tz = 'NZ'  # GMT+12
+        company.resource_calendar_id.tz = 'Australia/Sydney'  # GMT+12
         leave = self.env['hr.leave'].new({
             'employee_id': self.employee_emp.id,
             'holiday_status_id': self.holidays_type_1.id,
@@ -274,7 +274,7 @@ class TestLeaveRequests(TestHrHolidaysCommon):
     @mute_logger('odoo.models.unlink', 'odoo.addons.mail.models.mail_mail')
     def test_timezone_company_validated(self):
         """ Create a leave request for a company in another timezone and validate it """
-        self.env.user.tz = 'NZ' # GMT+12
+        self.env.user.tz = 'Australia/Sydney' # GMT+12
         company = self.env['res.company'].create({'name': "Hergé"})
         employee = self.env['hr.employee'].create({'name': "Remi", 'company_id': company.id})
         leave_form = Form(self.env['hr.leave'], view='hr_holidays.hr_leave_view_form_manager')
@@ -310,7 +310,7 @@ class TestLeaveRequests(TestHrHolidaysCommon):
     def test_timezone_department_leave_request(self):
         """ Create a leave request for a department in another timezone """
         company = self.env['res.company'].create({'name': "Hergé"})
-        company.resource_calendar_id.tz = 'NZ'  # GMT+12
+        company.resource_calendar_id.tz = 'Australia/Sydney'  # GMT+12
         department = self.env['hr.department'].create({'name': "Museum", 'company_id': company.id})
         leave = self.env['hr.leave'].new({
             'employee_id': self.employee_emp.id,
@@ -480,7 +480,7 @@ class TestLeaveRequests(TestHrHolidaysCommon):
     @mute_logger('odoo.models.unlink', 'odoo.addons.mail.models.mail_mail')
     def test_leave_defaults_with_timezones(self):
         """ Make sure that leaves start with correct defaults for non-UTC timezones """
-        timezones_to_test = ('UTC', 'Pacific/Midway', 'US/Pacific', 'Asia/Taipei', 'Pacific/Kiritimati')  # UTC, UTC -11, UTC -8, UTC +8, UTC +14
+        timezones_to_test = ('UTC', 'Pacific/Midway', 'America/Los_Angeles', 'Asia/Taipei', 'Pacific/Kiritimati')  # UTC, UTC -11, UTC -8, UTC +8, UTC +14
 
         #     January 2020
         # Su Mo Tu We Th Fr Sa
