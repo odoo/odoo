f3dcb15556df4ab98255d42ca346ecbba1e9127f
panv-odoo <panv@odoo.com>
2024-04-02 18:40:05 +0530

    [IMP] website, *: add user group option in conditional visibility * : website_blog
    Specification:
    
    To enhance content delivery customization, this commit introduces a user
    group visibility feature within the website module. By incorporating
    this functionality, user gain greater control over content
    presentation, allowing them to provide specific content to designated
    user groups.
    
    Changes Introduced:
    - Addition of a user-group suboption in conditional visibilty snippet
    options.
    - With the user group option, now user can have the functionality to
    choose the snippet visibility based on user-groups.
    - User group option will be visible only on debug mode.
    - Added tour for the User Group Options.
    
    Changes in Test Case:
    
    Made change in the query count as due to improvement, 3 more queries
    were introduced as below:
    - 2 queries for getting groups display name as it is many2many.
    - 1 for displaying user groups in snippet option selection.
    
    task-3502447
    
    Co-authored-by: divy-odoo <divy@odoo.com>

--- addons/website_blog/tests/test_performance.py
+++ addons/website_blog/tests/test_performance.py
@@ -45,7 +45,8 @@ class TestBlogPerformance(UtilPerf):
         } for blog in blogs])
 
     def test_10_perf_sql_blog_standard_data(self):
-        self.assertLessEqual(self._get_url_hot_query('/blog'), 11)
+        self.assertLessEqual(self._get_url_hot_query('/blog'), 14)
+        self.assertLessEqual(self._get_url_hot_query('/blog', cache=False), 26)
 
     def test_20_perf_sql_blog_bigger_data_scaling(self):
         BlogPost = self.env['blog.post']
@@ -57,10 +58,10 @@ class TestBlogPerformance(UtilPerf):
         for blog_post in blog_posts:
             blog_post.tag_ids += blog_tags
             blog_tags = blog_tags[:-1]
-        self.assertEqual(self._get_url_hot_query('/blog'), 11)
-        self.assertLessEqual(self._get_url_hot_query('/blog', cache=False), 33)
-        self.assertLessEqual(self._get_url_hot_query(blog_post[0].website_url), 16)
-        self.assertLessEqual(self._get_url_hot_query(blog_post[0].website_url, cache=False), 20)
+        self.assertEqual(self._get_url_hot_query('/blog'), 14)
+        self.assertLessEqual(self._get_url_hot_query('/blog', cache=False), 36)
+        self.assertLessEqual(self._get_url_hot_query(blog_post[0].website_url), 18)
+        self.assertLessEqual(self._get_url_hot_query(blog_post[0].website_url, cache=False), 23)
 
     def test_30_perf_sql_blog_bigger_data_scaling(self):
         BlogPost = self.env['blog.post']
@@ -73,6 +74,6 @@ class TestBlogPerformance(UtilPerf):
             blog_post.write({'tag_ids': [[6, 0, random.choices(blog_tags.ids, k=random.randint(0, len(blog_tags)))]]})
 
         self.assertLessEqual(self._get_url_hot_query('/blog'), 29)
-        self.assertLessEqual(self._get_url_hot_query('/blog', cache=False), 71)
+        self.assertLessEqual(self._get_url_hot_query('/blog', cache=False), 73)
         self.assertLessEqual(self._get_url_hot_query(blog_post[0].website_url), 34)
         self.assertLessEqual(self._get_url_hot_query(blog_post[0].website_url, cache=False), 33)
