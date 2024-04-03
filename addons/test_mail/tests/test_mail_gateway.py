45907aa85f27398569c94d06885f436174502d6b
Armin <fara@odoo.com>
2024-04-03 19:13:50 +0200

    [FIX] core: add support for windows-874 encoding
    Outlook and similar Windows based systems use windows-874 for encoding
    Thai characters which is not natively known by Python.
    Simply aliasing the Windows encoding as cp874 adds support for this
    encoding.
    
    opw-3684161
    
    closes odoo/odoo#158060
    
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- addons/test_mail/tests/test_mail_gateway.py
+++ addons/test_mail/tests/test_mail_gateway.py
@@ -11,7 +11,7 @@ from odoo import exceptions
 from odoo.addons.mail.models.mail_thread import MailThread
 from odoo.addons.mail.tests.common import mail_new_test_user
 from odoo.addons.test_mail.data import test_mail_data
-from odoo.addons.test_mail.data.test_mail_data import MAIL_TEMPLATE
+from odoo.addons.test_mail.data.test_mail_data import MAIL_TEMPLATE, THAI_EMAIL_WINDOWS_874
 from odoo.addons.test_mail.models.test_mail_models import MailTestGateway
 from odoo.addons.test_mail.tests.common import TestMailCommon
 from odoo.tests import tagged, RecordCapturer
@@ -1727,6 +1727,18 @@ class TestMailgateway(TestMailCommon):
             capture.records.message_ids.attachment_ids.raw.decode(charset),
             content
         )
+
+    def test_message_windows_874(self):
+        # Email for Thai customers who use Microsoft email service.
+        # The charset is windows-874 which isn't natively supported by
+        # python, check that Odoo is still capable of decoding it.
+        # windows-874 is the Microsoft equivalent of cp874.
+        with RecordCapturer(self.env['mail.test.gateway'], []) as capture:
+            self.env['mail.thread'].message_process('mail.test.gateway', THAI_EMAIL_WINDOWS_874)
+        email = capture.records
+        email.ensure_one()
+        self.assertEqual(email.name, 'เรื่อง')
+        self.assertEqual(str(email.message_ids.body), '<pre>ร่างกาย</pre>\n')
 
     # --------------------------------------------------
     # Corner cases / Bugs during message process
