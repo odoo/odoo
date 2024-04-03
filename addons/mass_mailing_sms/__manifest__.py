c31d4ed837a25e248ba797891f69577fe436f1fb
cacl <cacl@odoo.com>
2024-04-03 16:47:51 +0200

    [IMP] mass-mailing-sms: add website link to manifest
    Task-3775424
    
    Part-of: odoo/odoo#160182

--- addons/mass_mailing_sms/__manifest__.py
+++ addons/mass_mailing_sms/__manifest__.py
@@ -7,6 +7,7 @@
     'version': '1.1',
     'category': 'Marketing/Email Marketing',
     'sequence': 245,
+    'website': 'https://www.odoo.com/app/sms-marketing',
     'depends': [
         'portal',
         'mass_mailing',
