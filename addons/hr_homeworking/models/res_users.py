ebcfbdadf09b5305ce13c3938433f1c365646d3b
Mélanie <mepe@odoo.com>
2024-04-03 15:07:23 +0200

    [FIX] hr_homeworking: fix im_status value and display
    With this commit :
        * Im_status's title icon has the same format for every extension.
        * fix the im_status for user ( before this commit, their im_status
    was not impacted by hr_homeworking installation )
        * The color of presence status was fix to follow the color of im_status
    
    closes odoo/odoo#149384
    
    Related: odoo/upgrade#5796
    Signed-off-by: Sofie Gvaladze (sgv) <sgv@odoo.com>

--- addons/hr_homeworking/models/res_users.py
+++ addons/hr_homeworking/models/res_users.py
@@ -24,3 +24,14 @@ class User(models.Model):
     @property
     def SELF_WRITEABLE_FIELDS(self):
         return super().SELF_WRITEABLE_FIELDS + DAYS
+
+    def _compute_im_status(self):
+        super()._compute_im_status()
+        dayfield = self.env['hr.employee']._get_current_day_location_field()
+        for user in self:
+            location_type = user[dayfield].location_type
+            if not location_type:
+                continue
+            im_status = user.im_status
+            if im_status == "online" or im_status == "away" or im_status == "offline":
+                user.im_status = "presence_" + location_type + "_" + im_status
