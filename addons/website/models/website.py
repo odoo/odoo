8e985e61d6a0b601f523cb5ec4d03674f1cf06cc
Romain Derie <rde@odoo.com>
2024-04-04 09:59:22 +0200

    [FIX] website: prevent crash if no url on menu
    It should not be common, but through custo or in debug mode, one can
    create a menu without an URL since it's not required on the model.
    Through regular flows, it won't be possible since our UI won't let you
    go through when creating a menu if you don't set a URL.
    
    Followup of https://github.com/odoo/odoo/commit/948235079f002794f9837d3cf91e2d20e3254e20

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -199,7 +199,9 @@ class Website(models.Model):
         Checks if the website menu contains a record like url.
         :return: True if the menu contains a record like url
         """
-        return any(self.env['website.menu'].browse(self._get_menu_ids()).filtered(lambda menu: re.search(r"[/](([^/=?&]+-)?[0-9]+)([/]|$)", menu.url)))
+        return any(self.env['website.menu'].browse(self._get_menu_ids()).filtered(
+            lambda menu: menu.url and re.search(r"[/](([^/=?&]+-)?[0-9]+)([/]|$)", menu.url))
+        )
 
     @api.model_create_multi
     def create(self, vals_list):
