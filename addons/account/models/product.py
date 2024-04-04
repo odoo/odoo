d2c0d6e9f0128abaee49bcc57fb5990636a991e2
mathysPaul <matp@odoo.com>
2024-04-04 16:40:30 +0200

    [IMP] * : Add chatter to Product Category account, product, stock, stock_account
    Adds chatter to Product Category to track the following fields:
    - Force Removal Strategy
    - Costing Method
    - Income Account
    - Expense Account
    
    The chatter ensures visibility into when and why significant
    modifications are made, aiding in impact assessment and
    correction of any erroneous changes to sensitive data that
    affects all products within the category.
    
    task-3845508

--- addons/account/models/product.py
+++ addons/account/models/product.py
@@ -13,11 +13,15 @@ class ProductCategory(models.Model):
     property_account_income_categ_id = fields.Many2one('account.account', company_dependent=True,
         string="Income Account",
         domain=ACCOUNT_DOMAIN,
-        help="This account will be used when validating a customer invoice.")
+        help="This account will be used when validating a customer invoice.",
+        tracking=True,
+    )
     property_account_expense_categ_id = fields.Many2one('account.account', company_dependent=True,
         string="Expense Account",
         domain=ACCOUNT_DOMAIN,
-        help="The expense is accounted for when a vendor bill is validated, except in anglo-saxon accounting with perpetual inventory valuation in which case the expense (Cost of Goods Sold account) is recognized at the customer invoice validation.")
+        help="The expense is accounted for when a vendor bill is validated, except in anglo-saxon accounting with perpetual inventory valuation in which case the expense (Cost of Goods Sold account) is recognized at the customer invoice validation.",
+        tracking=True,
+    )
 
 #----------------------------------------------------------
 # Products
