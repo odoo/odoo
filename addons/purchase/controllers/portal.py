5562cdef6a6b353dc67d9a64851e4c13838b020e
ABE-Mark45 <aels@odoo.com>
2024-04-04 16:46:32 +0200

    [IMP] purchase: endpoint to download XML Order file
    To download the BIS 3 order file from odoo, user can
    follow this link format
    http://<host-name>/my/purchase/<order-id>/download_edi
    
    task-2695889
    
    closes odoo/odoo#160545
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/purchase/controllers/portal.py
+++ addons/purchase/controllers/portal.py
@@ -196,3 +196,30 @@ class CustomerPortal(portal.CustomerPortal):
         if updated_dates:
             order_sudo._update_date_planned_for_lines(updated_dates)
         return Response(status=204)
+
+    @http.route(['/my/purchase/<int:order_id>/download_edi'], auth="public", website=True)
+    def portal_my_purchase_order_download_edi(self, order_id=None, access_token=None, **kw):
+        """An endpoint to download EDI file representation.
+        """
+        try:
+            order_sudo = self._document_check_access('purchase.order', order_id, access_token=access_token)
+        except (AccessError, MissingError):
+            return request.redirect('/my')
+
+        builders = order_sudo._get_edi_builders()
+
+        # This handles only one builder for now, more can be added in the future
+        if len(builders) != 1:
+            return request.redirect('/my')
+        builder = builders[0]
+
+        xml_content = builder._export_order(order_sudo)
+
+        download_name = builder._export_purchase_order_filename(order_sudo)
+
+        http_headers = [
+            ('Content-Type', 'text/xml'),
+            ('Content-Length', len(xml_content)),
+            ('Content-Disposition', f'attachment; filename="{download_name}"')
+        ]
+        return request.make_response(xml_content, headers=http_headers)
