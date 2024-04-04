5d9716312072a32e3b0221f0a2613256f0438345
Audric Onockx (auon) <auon@odoo.com>
2024-04-04 17:07:22 +0200

    [REF] sale_[timesheet,project]: split model per file
    Each of those modules now contains a folder `models`,
    which contains one file per odoo model.
    
    closes odoo/odoo#160547
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_project/models/__init__.py
+++ addons/sale_project/models/__init__.py
@@ -1,10 +1,11 @@
-# -*- coding: utf-8 -*-
-
-from . import account_move
-from . import product
-from . import project
+from . import account_move_line
+from . import product_product
+from . import product_template
 from . import project_milestone
-from . import sale_order
+from . import project_project
+from . import project_task_recurrence
+from . import project_task
+from . import res_config_settings
 from . import sale_order_line
 from . import sale_order_template_line
-from . import res_config_settings
+from . import sale_order
