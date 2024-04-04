bf5a99b288b6681dea837d444906a0b7d1ac7bb2
Emilio Pascual <emilio@moduon.team>
2024-04-04 07:54:19 +0200

    [IMP] delivery: disable only delivery carriers from external providers
    Only delivery carriers that can communicate with the outside should be deactivated.
    Every time a database is neutralized, all delivery carriers are deactivated. In staging or test databases this should not be the case, only delivery carriers with an external connection, i.e. delivery carriers with an external provider should be deactivated.
    Delivery carriers with fixed price or based on rules should not be deactivated with every neutralization. This way Odoo can continue to operate with shipping methods without prejudice to the users in neutralized databases.
    
    @moduon MT-5612
    
    closes odoo/odoo#161223
    
    X-original-commit: bdaeae0230fe0da7743f8cc4ed9dcb210b994540
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>

--- addons/delivery/data/neutralize.sql
+++ addons/delivery/data/neutralize.sql
@@ -1,4 +1,7 @@
--- disable delivery carriers
+-- disable prod environment in all delivery carriers
 UPDATE delivery_carrier
-   SET prod_environment = false,
-       active = false;
+   SET prod_environment = false;
+-- disable delivery carriers from external providers
+UPDATE delivery_carrier
+   SET active = false
+   WHERE delivery_type NOT IN ('fixed', 'base_on_rule');
