odoo/odoo

commit d2fc67eef0bd38c5d18a05469942249ab76fbf97

[FIX] website: fix login with RPC on website domain
__Current behavior before commit:__
When the field `website_id` of a user is set, this user should only be
able to login via this website (cfr. [this method][1]). This means that
he can only login if [`get_current_website`][2] returns the same website
as `website_id`.

The issue is that when login with RPC, `dispatch_rpc` is making a
[`borrow_request`][3] which hides the `request` object from the RPC
layer. This has the consequence that `get_current_website` is not able
to retrieve the website based on the url of the request. Thus preventing
a user to login via RPC even if the domain used corresponds to the
website set in its `website_id` field.

__Description of the fix:__
Add the possibility to retrieve the current website via
`threading.current_thread().url` (which is set [here][4]). This way it
is possible to know the current website even if `borrow_request` has
been used.

__Steps tor reproduce:__
- Install `website`
- Go to Website > Configuration > Websites
- Set the domain of the first website (e.g. `http://localhost:8069/`)
- Go to Marc Demo" res.partner form view
- In Sales & Purchase tab set **Website** to "My Website"

Then run this script in another terminal:
```python
#!/usr/bin/env python3
import json
import urllib.request

url = "http://localhost:8069/jsonrpc"
dbname = "db-16.0"

def rpc_login_user_demo():
    """
    Login with demo using JSON-RPC
    :return: the user's id or False if login failed
    """
    req = urllib.request.Request(url=url, data=json.dumps({
        "params": {
            "service": "common",
            "method": "login",
            "args": [dbname, 'demo', 'demo']
        },
    }).encode(), headers={"Content-Type": "application/json"})
    response = json.loads(urllib.request.urlopen(req).read().decode('UTF-8'))
    if response.get("error"):
        raise Exception(response["error"])
    return response['result']

uid = rpc_login_user_demo()
if uid:

addons/website/models/website.py
@@ -8,6 +8,7 @@ import logging
 import operator
 import re
 import requests
+import threading
 
 from collections import defaultdict
 from functools import reduce
@@ -977,7 +978,9 @@ class Website(models.Model):
         # there is one on request) or return a random one.
 
         # The format of `httprequest.host` is `domain:port`
-        domain_name = request and request.httprequest.host or ''
+        domain_name = (request and request.httprequest.host
+            or hasattr(threading.current_thread(), 'url') and threading.current_thread().url
+            or '')
         website_id = self._get_current_website_id(domain_name, fallback=fallback)
         return self.browse(website_id)
 

addons/website/tests/test_get_current_website.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import json
 
 from odoo.tests import tagged
-from odoo.tests.common import TransactionCase
+from odoo.addons.base.tests.common import HttpCaseWithUserDemo
 
 
 @tagged('post_install', '-at_install')
-class TestGetCurrentWebsite(TransactionCase):
+class TestGetCurrentWebsite(HttpCaseWithUserDemo):
 
     def setUp(self):
+        super().setUp()
         # Unlink unused website(s) to avoid messing with the expected results
         self.website = self.env.ref('website.default_website')
         for w in self.env['website'].search([('id', '!=', self.website.id)]):
@@ -85,3 +87,33 @@ class TestGetCurrentWebsite(TransactionCase):
 
         user = self.env['res.users'].create({'website_id': website.id, 'login': 'sad@mail.com', 'name': 'Hope Fully'})
         self.assertTrue(user.website_id == user.partner_id.website_id == website)
+
+    def test_03_rpc_signin_user_website_id(self):
+        def rpc_login_user_demo():
+            """
+            Login with demo using JSON-RPC
+            :return: the user's id or False if login failed
+            """
+            response = self.url_open('/jsonrpc', data=json.dumps({
+                "params": {
+                    "service": "common",
+                    "method": "login",
+                    "args": [self.env.cr.dbname, 'demo', 'demo']
+                },
+            }), headers={"Content-Type": "application/json"})
+            return response.json()['result']
+
+        website1 = self.website
+        website1.domain = self.base_url()
+
+        website2 = self.env['website'].create({'name': 'My Website 2'})
+        website2.domain = False
+
+        # It should login successfully since the host used in the RPC call is
+        # the same as the website set on the user.
+        self.user_demo.website_id = website1
+        self.assertTrue(rpc_login_user_demo())
+
+        # It should not login since the website set on the user has no domain.
+        self.user_demo.website_id = website2
+        self.assertFalse(rpc_login_user_demo())

commit 43bd6103a2e4ec7a088523318853715986debec4

[FIX] website: fix the only linter error in the website.py file
Introduced with https://github.com/odoo/odoo/commit/d2fc67eef0b ...

addons/website/models/website.py
@@ -988,9 +988,11 @@ class Website(models.Model):
         # there is one on request) or return a random one.
 
         # The format of `httprequest.host` is `domain:port`
-        domain_name = (request and request.httprequest.host
+        domain_name = (
+            request and request.httprequest.host
             or hasattr(threading.current_thread(), 'url') and threading.current_thread().url
-            or '')
+            or ''
+        )
         website_id = self._get_current_website_id(domain_name, fallback=fallback)
         return self.browse(website_id)
 