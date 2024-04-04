e8d822bad04273f0c31795d8cfca62587304b4c9
Victor Piryns (pivi) <pivi@odoo.com>
2024-04-04 09:08:57 +0000

    [PERF] analytic: add missing indexes
    Description:
    Add indexes affecting queries related to `account.analytic.line`.
    Based on heuristics like domains in `search`/`read_group` that are
    not really selective, or being an inverse of One2many.
    Some of these searches are on code-paths that are frequent, like
    creation of an SO.
    The goal is to reduce the number of Seq.Scan or non-selective Index
    Scan.
    
    Reference:
    task-3724844
    
    closes odoo/odoo#161479
    
    X-original-commit: 0bd65cc2abeaacb04d63a15cee332bba1dc23277
    Signed-off-by: Olivier Dony (odo) <odo@odoo.com>
    Signed-off-by: Piryns Victor (pivi) <pivi@odoo.com>

--- addons/sale/models/analytic.py
+++ addons/sale/models/analytic.py
@@ -7,7 +7,7 @@ from odoo import fields, models
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
-    so_line = fields.Many2one('sale.order.line', string='Sales Order Item', domain=[('qty_delivered_method', '=', 'analytic')])
+    so_line = fields.Many2one('sale.order.line', string='Sales Order Item', domain=[('qty_delivered_method', '=', 'analytic')], index='btree_not_null')
 
 
 class AccountAnalyticApplicability(models.Model):
