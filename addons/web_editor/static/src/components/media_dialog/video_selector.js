646893efc51bb54c626d2d78628a2a9ad955bedf
Serge Bayet (seba) <seba@odoo.com>
2024-04-04 09:24:10 +0200

    [IMP] web_editor,website: provide video lazy loading
    This commit introduces a method for lazy loading user-uploaded videos by
    using the loading="lazy" attribute on the video iframe tag. This
    improvement positively impacts the loading time, leading to an enhanced
    score in Lighthouse.
    
    task-2924927

--- addons/web_editor/static/src/components/media_dialog/video_selector.js
+++ addons/web_editor/static/src/components/media_dialog/video_selector.js
@@ -256,7 +256,7 @@ export class VideoSelector extends Component {
             div.innerHTML = `
                 <div class="css_editable_mode_display"></div>
                 <div class="media_iframe_video_size" contenteditable="false"></div>
-                <iframe frameborder="0" contenteditable="false" allowfullscreen="allowfullscreen"></iframe>
+                <iframe loading="lazy" frameborder="0" contenteditable="false" allowfullscreen="allowfullscreen"></iframe>
             `;
             div.querySelector('iframe').src = video.src;
             return div;
