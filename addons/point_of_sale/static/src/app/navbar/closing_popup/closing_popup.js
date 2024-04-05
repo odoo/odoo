cc9d1228b7ca7ef5aa73df06896838501ed02ff9
Pedram (pebr) <pebr@odoo.com>
2024-04-05 11:50:52 +0200

    [FIX] pos_hr: display error message when no employees exist
    Prior to this commit, enabling the multi-employee option without any
    employees in the database would result in no action upon selecting
    cashier. This could lead to confusion. With this commit, an error
    message is displayed in such scenarios, enhancing user feedback and
    preventing confusion.
    
    opw-3851823
    
    closes odoo/odoo#160646
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/static/src/app/navbar/closing_popup/closing_popup.js
+++ addons/point_of_sale/static/src/app/navbar/closing_popup/closing_popup.js
@@ -206,7 +206,7 @@ export class ClosePosPopup extends AbstractAwaitablePopup {
             if (!response.successful) {
                 return this.handleClosingError(response);
             }
-            window.location = "/web#action=point_of_sale.action_client_pos_menu";
+            this.pos.redirectToBackend();
         } catch (error) {
             if (error instanceof ConnectionLostError) {
                 // Cannot redirect to backend when offline, let error handlers show the offline popup
@@ -223,7 +223,7 @@ export class ClosePosPopup extends AbstractAwaitablePopup {
                             "You will be redirected to the back-end to manually close the session."
                     ),
                 });
-                window.location = "/web#action=point_of_sale.action_client_pos_menu";
+                this.pos.redirectToBackend();
             }
         }
     }
@@ -234,7 +234,7 @@ export class ClosePosPopup extends AbstractAwaitablePopup {
             sound: response.type !== "alert",
         });
         if (response.redirect) {
-            window.location = "/web#action=point_of_sale.action_client_pos_menu";
+            this.pos.redirectToBackend();
         }
     }
 }
