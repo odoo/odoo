0bcad6e848ea72120d6cd04570d6ff3a2f5649c9
Alexandre Kühn <aku@odoo.com>
2024-04-05 14:38:15 +0200

    [FIX] mail: proper Record.attr defaults with list
    Before this commit, the default value definition of a
    Record.attr() was saved once and given to all records of the
    given model at record creation.
    
    This meant that lists were shared between records, which is
    definitely an undesireable side-effect of the implementation.
    This commit fixes the issue by instead getting default value
    from usual lifecyle as a class instance.
    
    closes odoo/odoo#160802
    
    X-original-commit: 5669e8e806bca38fd96eaf049bb56a6d80f1e9fa
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/model/record_internal.js
+++ addons/mail/static/src/model/record_internal.js
@@ -89,7 +89,7 @@ export class RecordInternal {
             });
             record[fieldName] = recordList;
         } else {
-            record[fieldName] = Model._.fieldsDefault.get(fieldName);
+            record[fieldName] = record[fieldName].default;
         }
         if (Model._.fieldsCompute.get(fieldName)) {
             if (!Model._.fieldsEager.get(fieldName)) {
