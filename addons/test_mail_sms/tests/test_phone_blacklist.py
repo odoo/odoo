0530fba1c6a29470d024666319a6efb1255e5fcc
Andrzej(pian) <pian@odoo.com>
2024-04-05 11:36:30 +0000

    [FIX] phone_validation: blacklisting unblacklisted should re-blacklist them
    It may happen that user will try to blacklist a number that he already
    unblacklisted (archived blacklist record). Before that attempt was ignored. With
    this commit we simply blacklist the number again (so we unarchive existing record)
    
    [Reproduce]
    - Install mass_mailing_sms
    - Open Blacklisted Phone Numbers (Email Marketing / Configuration / **Bla**cklisted Email Addresses)
    - Blacklist a number N
    - Unblacklist number N
    - Go back to Blacklisted Phone Numbers
    - Blacklist a number N (again) -> BUG: no effect
    
    opw-3700268
    
    X-original-commit: 7a45dc47382acd59c4f01d6050d757bfed87ba55

--- addons/test_mail_sms/tests/test_phone_blacklist.py
+++ addons/test_mail_sms/tests/test_phone_blacklist.py
@@ -21,6 +21,16 @@ class TestPhoneBlacklist(SMSCommon, TestSMSRecipients):
         })
         cls.test_record = cls._reset_mail_context(cls.test_record)
 
+    def test_phone_blacklist_create_unblacklisted(self):
+        """Ensure that the API allows creating unblacklisted records."""
+        phone_number = self.test_numbers[0]
+        unblacklisted_record = self.env['phone.blacklist'].sudo().create([{'number': phone_number, 'active': False}])
+        self.assertFalse(unblacklisted_record.active, "Creating an unblacklisted record resulted in a blacklisted one")
+
+        # Make sure that an attempt to re-create unblacklisted number will leave the number as it was (unblacklisted)
+        still_unblacklisted_record = self.env['phone.blacklist'].sudo().create([{'number': phone_number, 'active': False}])
+        self.assertFalse(still_unblacklisted_record.active, "Attempt to re-create the unblacklisted record made it blacklisted")
+
     def test_phone_blacklist_internals(self):
         with self.with_user('employee'):
             test_record = self.env['mail.test.sms.bl'].browse(self.test_record.id)
@@ -50,6 +60,26 @@ class TestPhoneBlacklist(SMSCommon, TestSMSRecipients):
             bl_record.unlink()
             rec = self.env['mail.test.sms.bl'].search([('phone_sanitized_blacklisted', '=', True)])
             self.assertEqual(rec, self.env['mail.test.sms.bl'])
+
+    def test_phone_blacklist_unblacklisted(self):
+        """ This test check for scenario where user:
+            1. Blacklists a number  -> creating new active record)
+            2. Unblacklists it      -> making record unactive (archived)
+            3. Blacklists it again (by attempting to create new record)
+        Last step should just make existing record active again, and this test checks it.
+        """
+        phone_number = self.test_numbers[0]
+        bl_record = self.env['phone.blacklist'].sudo().create([{'number': phone_number, 'active': True}])
+        num_of_records = self.env['phone.blacklist'].with_context(active_test=False).search_count([])
+
+        bl_record.action_archive()
+
+        # Attempt to blacklist unblacklisted
+        self.env['phone.blacklist'].sudo().create([{'number': phone_number}])
+
+        self.assertTrue(bl_record.active, "Attempting to blacklist already-unblacklisted, should make the record active again")
+        self.assertEqual(num_of_records, self.env['phone.blacklist'].with_context(active_test=False).search_count([]),
+            "Number of records shouldn't change. (Records probably were recreated, instead of activated)")
 
     def test_phone_sanitize_api(self):
         with self.with_user('employee'):
