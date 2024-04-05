9976ed7393243fcb2b9eb92dbf9822986a2f8824
Jitendra Prajapat <jipr@odoo.com>
2024-04-05 14:51:11 +0530

    [FIX] point_of_sale: prevent duplicate attribute values in product configurator
    Before this commit:
    ===================
    product configurator in the point of sale displays duplicate attribute values.
    
    After this commit:
    ===================
    with this commit, POS product configurator no longer displays duplicate
    attribute values.
    
    task - 3851020

--- addons/point_of_sale/static/src/app/store/product_configurator_popup/product_configurator_popup.js
+++ addons/point_of_sale/static/src/app/store/product_configurator_popup/product_configurator_popup.js
@@ -5,8 +5,8 @@ import { Component, onMounted, useRef, useState, useSubEnv } from "@odoo/owl";
 export class BaseProductAttribute extends Component {
     setup() {
         this.env.attribute_components.push(this);
-        this.attribute = this.props.attribute;
-        this.values = this.attribute.template_value_ids;
+        this.attribute = this.props.attribute.attribute_id;
+        this.values = this.props.attribute.product_template_value_ids;
         this.state = useState({
             attribute_value_ids: parseFloat(this.values[0].id),
             custom_value: "",
@@ -89,7 +89,7 @@ export class MultiProductAttribute extends BaseProductAttribute {
     initAttribute() {
         const attribute = this.props.attribute;
 
-        for (const value of attribute.template_value_ids) {
+        for (const value of attribute.product_template_value_ids) {
             this.state.attribute_value_ids[value.id] = false;
         }
     }
@@ -108,10 +108,6 @@ export class ProductConfiguratorPopup extends Component {
 
     setup() {
         useSubEnv({ attribute_components: [] });
-    }
-
-    get attributes() {
-        return this.props.product.attribute_line_ids.map((attrLine) => attrLine.attribute_id);
     }
 
     computePayload() {
