d2ae30d82736d518d146fd5849103ab7c367bae9
sesn-odoo <sesn@odoo.com>
2024-04-07 12:08:55 +0200

    [IMP] l10n_ch: improve warning message for invalid reference
    opw-3737715

--- addons/l10n_ch/models/account_payment.py
+++ addons/l10n_ch/models/account_payment.py
@@ -15,13 +15,15 @@ class AccountPayment(models.Model):
         for payment in self:
             if payment.payment_type == 'outbound' and\
                     payment.partner_id.country_code in ['CH', 'LI'] and\
-                    payment.partner_bank_id.l10n_ch_qr_iban and\
-                    not payment._l10n_ch_reference_is_valid(payment.ref):
-                payment.l10n_ch_reference_warning_msg = _("Please fill in a correct QRR reference in the payment reference. The banks will refuse your payment file otherwise.")
+                    payment.partner_bank_id.l10n_ch_qr_iban:
+                payment.l10n_ch_reference_warning_msg = payment._l10n_ch_reference_validation_error(payment.ref)
             else:
                 payment.l10n_ch_reference_warning_msg = False
 
     def _l10n_ch_reference_is_valid(self, payment_reference):
+        return not self._l10n_ch_reference_validation_error(payment_reference)
+
+    def _l10n_ch_reference_validation_error(self, payment_reference):
         """Check if this invoice has a valid reference (for Switzerland)
         e.g.
         000000000000000000000012371
@@ -30,8 +32,10 @@ class AccountPayment(models.Model):
         """
         self.ensure_one()
         if not payment_reference:
-            return False
+            return _("The payment reference cannot be empty.")
         ref = payment_reference.replace(' ', '')
-        if re.match(r'^(\d{2,27})$', ref):
-            return ref == mod10r(ref[:-1])
+        if not re.match(r'^(\d{2,27})$', ref):
+            return _("The payment reference should be between 2 and 27 digits long.")
+        if ref != mod10r(ref[:-1]):
+            return _("We couldn't verify the payment reference. Please ensure it's accurate and free of any typos.")
         return False
