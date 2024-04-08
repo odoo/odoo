b24ff02d1bbca0b972cf7eec8cfb79e02a765e3c
Soukéina Bojabza <sobo@odoo.com>
2024-04-08 20:06:46 +0200

    [FIX] website: add a tour to test the `Carousel` options
    As a follow-up of the fix in commit [1] and to test the previous commits
    of this PR, this commit adds a tour ensuring that the `Carousel` options
    work correctly.
    
    [1]: https://github.com/odoo/odoo/commit/bb2c77b3d0f8c7fc74801de54663f373490b9fad
    
    task-3744613
    related to opw-3675019

--- addons/website/static/tests/tours/carousel_content_removal.js
+++ addons/website/static/tests/tours/carousel_content_removal.js
@@ -1,6 +1,7 @@
 /** @odoo-module */
 
 import tour from 'web_tour.tour';
+import wTourUtils from "website.tour_utils";
 
 tour.register("carousel_content_removal", {
     test: true,
@@ -26,3 +27,72 @@ tour.register("carousel_content_removal", {
     content: "Check for a carousel slide with an empty container tag",
     run: function () {},
 }]);
+
+tour.register("snippet_carousel", {
+    test: true,
+    url: "/",
+}, [
+    ...wTourUtils.clickOnEditAndWaitEditMode(),
+    wTourUtils.dragNDrop({id: "s_carousel", name: "Carousel"}),
+    wTourUtils.clickOnSnippet(".carousel .carousel-item.active"),
+    // Slide to the right.
+    wTourUtils.changeOption("CarouselItem", 'we-button[data-slide="right"]'),
+    {
+        content: "Check if the second slide is active",
+        trigger: ".carousel-inner > div.active:nth-child(2)",
+        run: () => {}, // This is a check.
+    },
+    // Add a slide (with the "CarouselItem" option).
+    wTourUtils.changeOption("CarouselItem", "we-button[data-add-slide-item]"),
+    {
+        content: "Check if there are four slides and if the third one is active",
+        trigger: ".carousel-inner:has(div:nth-child(4)) > div.active:nth-child(3)",
+        run: () => {}, // This is a check.
+    },
+     // Remove a slide.
+     wTourUtils.changeOption("CarouselItem", "we-button[data-remove-slide]"),
+    {
+        content: "Check if there are three slides and if the second one is active",
+        trigger: ".carousel-inner:has(div:nth-child(3)) > div.active:nth-child(2)",
+        run: () => {}, // This is a check.
+    }, {
+        trigger: ".carousel .carousel-control-prev",
+        content: "Slide the carousel to the left with the arrows.",
+    }, {
+        content: "Check if the first slide is active",
+        trigger: ".carousel-inner > div.active:nth-child(1)",
+        run: () => {}, // This is a check.
+    },
+    // Add a slide (with the "Carousel" option).
+    wTourUtils.changeOption("Carousel", "we-button[data-add-slide]"),
+    {
+        content: "Check if there are four slides and if the second one is active",
+        trigger: ".carousel-inner:has(div:nth-child(4)) > div.active:nth-child(2)",
+        run: () => {}, // This is a check.
+    }, {
+        content: "Check if the slide indicator was correctly updated",
+        trigger: "we-customizeblock-options span:contains(' (2/4)')",
+        run: () => {},
+    },
+    // Check if we can still remove a slide.
+    wTourUtils.changeOption("CarouselItem", "we-button[data-remove-slide]"),
+    {
+        content: "Check if there are three slides and if the first one is active",
+        trigger: ".carousel-inner:has(div:nth-child(3)) > div.active:nth-child(1)",
+        run: () => {}, // This is a check.
+    },
+    // Slide to the left.
+    wTourUtils.changeOption("CarouselItem", 'we-button[data-slide="left"]'),
+    {
+        content: "Check if the third slide is active",
+        trigger: ".carousel-inner > div.active:nth-child(3)",
+        run: () => {}, // This is a check.
+    },
+    ...wTourUtils.clickOnSave(),
+    // Check that saving always sets the first slide as active.
+    {
+        content: "Check that the first slide became the active one",
+        trigger: ".carousel-inner > div.active:nth-child(1)",
+        run: () => {}, // This is a check.
+    },
+]);
