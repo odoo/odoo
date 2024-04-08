aa8ec2c4be0a1af2043475cdfc1ba997aa394098
Soukéina Bojabza <sobo@odoo.com>
2024-04-08 20:06:46 +0200

    [FIX] website: add a tour to test the `Carousel` options
    As a follow-up of the fix in commit [1] and to test the previous commits
    of this PR, this commit adds a tour ensuring that the `Carousel` options
    work correctly.
    
    [1]: https://github.com/odoo/odoo/commit/bb2c77b3d0f8c7fc74801de54663f373490b9fad
    
    task-3744613
    related to opw-3675019
    
    X-original-commit: becb6bbb703b7608689ef8a5654be1db4a68f551

--- addons/website/static/tests/tours/carousel_content_removal.js
+++ addons/website/static/tests/tours/carousel_content_removal.js
@@ -2,6 +2,8 @@
 
 import wTourUtils from 'website.tour_utils';
 
+const carouselInnerSelector = "iframe .carousel-inner";
+
 wTourUtils.registerWebsitePreviewTour("carousel_content_removal", {
     test: true,
     url: '/',
@@ -23,3 +25,72 @@ wTourUtils.registerWebsitePreviewTour("carousel_content_removal", {
     content: "Check for a carousel slide with an empty container tag",
     run: function () {},
 }]);
+
+wTourUtils.registerWebsitePreviewTour("snippet_carousel", {
+    test: true,
+    url: "/",
+    edition: true,
+}, [
+    wTourUtils.dragNDrop({id: "s_carousel", name: "Carousel"}),
+    wTourUtils.clickOnSnippet(".carousel .carousel-item.active"),
+    // Slide to the right.
+    wTourUtils.changeOption("CarouselItem", 'we-button[data-switch-to-slide="right"]'),
+    {
+        content: "Check if the second slide is active",
+        trigger: `${carouselInnerSelector} > div.active:nth-child(2)`,
+        run: () => {}, // This is a check.
+    },
+    // Add a slide (with the "CarouselItem" option).
+    wTourUtils.changeOption("CarouselItem", "we-button[data-add-slide-item]"),
+    {
+        content: "Check if there are four slides and if the third one is active",
+        trigger: `${carouselInnerSelector}:has(div:nth-child(4)) > div.active:nth-child(3)`,
+        run: () => {}, // This is a check.
+    },
+     // Remove a slide.
+     wTourUtils.changeOption("CarouselItem", "we-button[data-remove-slide]"),
+    {
+        content: "Check if there are three slides and if the second one is active",
+        trigger: `${carouselInnerSelector}:has(div:nth-child(3)) > div.active:nth-child(2)`,
+        run: () => {}, // This is a check.
+    }, {
+        trigger: "iframe .carousel .carousel-control-prev",
+        content: "Slide the carousel to the left with the arrows.",
+    }, {
+        content: "Check if the first slide is active",
+        trigger: `${carouselInnerSelector} > div.active:nth-child(1)`,
+        run: () => {}, // This is a check.
+    },
+    // Add a slide (with the "Carousel" option).
+    wTourUtils.changeOption("Carousel", "we-button[data-add-slide]"),
+    {
+        content: "Check if there are four slides and if the second one is active",
+        trigger: `${carouselInnerSelector}:has(div:nth-child(4)) > div.active:nth-child(2)`,
+        run: () => {}, // This is a check.
+    }, {
+        content: "Check if the slide indicator was correctly updated",
+        trigger: "we-customizeblock-options span:contains(' (2/4)')",
+        run: () => {},
+    },
+    // Check if we can still remove a slide.
+    wTourUtils.changeOption("CarouselItem", "we-button[data-remove-slide]"),
+    {
+        content: "Check if there are three slides and if the first one is active",
+        trigger: `${carouselInnerSelector}:has(div:nth-child(3)) > div.active:nth-child(1)`,
+        run: () => {}, // This is a check.
+    },
+    // Slide to the left.
+    wTourUtils.changeOption("CarouselItem", 'we-button[data-switch-to-slide="left"]'),
+    {
+        content: "Check if the third slide is active",
+        trigger: `${carouselInnerSelector} > div.active:nth-child(3)`,
+        run: () => {}, // This is a check.
+    },
+    ...wTourUtils.clickOnSave(),
+    // Check that saving always sets the first slide as active.
+    {
+        content: "Check that the first slide became the active one",
+        trigger: `${carouselInnerSelector} > div.active:nth-child(1)`,
+        run: () => {}, // This is a check.
+    },
+]);
