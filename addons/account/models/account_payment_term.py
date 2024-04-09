a72b51d51a5028c4119843a2f3834fee161faea4
Trinh Ngoc Hung <ngochung207@gmail.com>
2024-04-09 16:29:54 +0700

    [FIX] account: wrong currency in payment terms
    Create company A with company currency USD, company B with company
    currency VND.
    Set company B-VND in payment terms
    
    Expect: currency payment terms is VND
    X-original-commit: 2fd2964a31c74ea7e927077fd71978e2ae167bad

--- addons/account/models/account_payment_term.py
+++ addons/account/models/account_payment_term.py
@@ -26,7 +26,7 @@ class AccountPaymentTerm(models.Model):
     company_id = fields.Many2one('res.company', string='Company')
     fiscal_country_codes = fields.Char(compute='_compute_fiscal_country_codes')
     sequence = fields.Integer(required=True, default=10)
-    currency_id = fields.Many2one('res.currency', default=lambda self: self.env.company.currency_id, store=True)
+    currency_id = fields.Many2one('res.currency', compute="_compute_currency_id")
 
     display_on_invoice = fields.Boolean(string='Show installment dates', default=True)
     example_amount = fields.Monetary(currency_field='currency_id', default=1000, store=False, readonly=True)
@@ -50,6 +50,12 @@ class AccountPaymentTerm(models.Model):
         for record in self:
             allowed_companies = record.company_id or self.env.companies
             record.fiscal_country_codes = ",".join(allowed_companies.mapped('account_fiscal_country_id.code'))
+
+    @api.depends_context('company')
+    @api.depends('company_id')
+    def _compute_currency_id(self):
+        for payment_term in self:
+            payment_term.currency_id = payment_term.company_id.currency_id or self.env.company.currency_id
 
     def _get_amount_due_after_discount(self, total_amount, untaxed_amount):
         self.ensure_one()
