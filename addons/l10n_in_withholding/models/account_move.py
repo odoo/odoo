1458a340edfa68966d34a10716a7efa61f5932ab
bhra-odoo <bhra@odoo.com>
2024-04-09 16:10:59 +0530

    [IMP] l10n_in_withholding: implemented alert mechanism for TDS/TCS
    In this commit:
    1. TCS taxes are moved from `l10n_in` -> `l10n_in_withholding`
    
    2.Adding an alert mechanism for TDS/TCS and added a section
    for the TDS  threshold limit, which has two limits per transaction and an
    aggregate limit.  If either limit is exceeded, a banner will appear on the
    Vendor Bill/Customer  Invoice.
    
    task-3707489

--- addons/l10n_in_withholding/models/account_move.py
+++ addons/l10n_in_withholding/models/account_move.py
@@ -1,4 +1,6 @@
 from odoo import api, models, fields
+from odoo.tools import SQL
+from odoo.tools.date_utils import get_month
 
 
 class AccountMove(models.Model):
@@ -30,6 +32,7 @@ class AccountMove(models.Model):
         compute='_compute_l10n_in_total_withholding_amount',
         help="Total withholding amount for the move",
     )
+    l10n_in_tcs_tds_warning = fields.Char('TDC/TCS Warning', compute="_compute_l10n_in_tcs_tds_warning")
 
     # === Compute Methods ===
     @api.depends('line_ids', 'l10n_in_is_withholding')
@@ -55,3 +58,104 @@ class AccountMove(models.Model):
             'view_mode': 'tree,form',
             'domain': [('id', 'in', self.l10n_in_withhold_move_ids.ids)],
         }
+
+    def _get_sections_aggregate_sum_by_pan(self, section_alert, commercial_partner_id):
+        self.ensure_one()
+        month_start_date, month_end_date = get_month(self.date)
+        company_fiscalyear_dates = self.company_id.compute_fiscalyear_dates(self.date)
+        fiscalyear_start_date, fiscalyear_end_date = company_fiscalyear_dates['date_from'], company_fiscalyear_dates['date_to']
+        default_domain = [
+            ('account_id.l10n_in_tds_tcs_section_id', '=', section_alert.id),
+            ('move_id.move_type', '!=', 'entry'),
+            ('company_id', 'child_of', self.company_id.root_id.id),
+        ]
+        if commercial_partner_id.l10n_in_pan:
+            default_domain += [('move_id.commercial_partner_id.l10n_in_pan', '=', commercial_partner_id.l10n_in_pan)]
+        else:
+            default_domain += [('move_id.commercial_partner_id', '=', commercial_partner_id.id)]
+        frequency_domains = {
+            'monthly': [('date', '>=', month_start_date), ('date', '<=', month_end_date)],
+            'fiscal_yearly': [('date', '>=', fiscalyear_start_date), ('date', '<=', fiscalyear_end_date)],
+        }
+        aggregate_result = {}
+        for frequency, frequency_domain in frequency_domains.items():
+            query = self.env['account.move.line']._where_calc(default_domain + frequency_domain)
+            result = self.env.execute_query_dict(SQL(
+                """
+                SELECT sum(account_move_line.balance) as balance,
+                       sum(account_move_line.price_total * am.invoice_currency_rate) as price_total
+                  FROM %s
+                  JOIN account_move AS am ON am.id = account_move_line.move_id
+                 WHERE %s
+                """,
+                query.from_clause,
+                query.where_clause)
+            )
+            aggregate_result[frequency] = result[0]
+        return aggregate_result
+
+    def _l10n_in_is_warning_applicable(self, section_id):
+        self.ensure_one()
+        match section_id.tax_source_type:
+            case 'tcs':
+                return self.journal_id.type == 'sale'
+            case 'tds':
+                return (
+                    self.journal_id.type == 'purchase'
+                    and section_id not in self.l10n_in_withhold_move_ids.mapped('line_ids.tax_ids.l10n_in_section_id')
+                )
+            case _:
+                return False
+
+    @api.depends('invoice_line_ids.price_total')
+    def _compute_l10n_in_tcs_tds_warning(self):
+        def _group_by_section_alert(invoice_lines):
+            group_by_lines = {}
+            for line in invoice_lines:
+                group_key = line.account_id.l10n_in_tds_tcs_section_id
+                if group_key and not line.company_currency_id.is_zero(line.balance):
+                    group_by_lines.setdefault(group_key, [])
+                    group_by_lines[group_key].append(line)
+            return group_by_lines
+
+        def _is_section_applicable(section_alert, threshold_sums, invoice_currency_rate, lines):
+            if section_alert.is_aggregate_limit:
+                aggregate_period_key = section_alert.consider_amount == 'total_amount' and 'price_total' or 'balance'
+                aggregate_total = threshold_sums.get(section_alert.aggregate_period, {}).get(aggregate_period_key)
+                if aggregate_total > section_alert.aggregate_limit:
+                    return True
+            return (
+                section_alert.is_per_transaction_limit
+                and sum(
+                    (line.price_total * invoice_currency_rate) if section_alert.consider_amount == 'total_amount' else line.balance
+                    for line in lines
+                ) > section_alert.per_transaction_limit
+            )
+
+        for move in self:
+            if move.country_code == 'IN' and move.move_type in ['in_invoice', 'out_invoice']:
+                warning = set()
+                commercial_partner_id = move.commercial_partner_id
+                existing_section = (self.l10n_in_withhold_move_ids.line_ids + move.line_ids).tax_ids.l10n_in_section_id
+                for section_alert, lines in _group_by_section_alert(move.invoice_line_ids).items():
+                    if (
+                        section_alert not in existing_section
+                        and move._l10n_in_is_warning_applicable(section_alert)
+                        and _is_section_applicable(
+                            section_alert,
+                            move._get_sections_aggregate_sum_by_pan(
+                                section_alert,
+                                commercial_partner_id
+                            ),
+                            move.invoice_currency_rate,
+                            lines
+                        )
+                    ):
+                        warning.add(section_alert.id)
+                warning_sections = self.env['l10n_in.section.alert'].browse(warning)
+                if warning_sections:
+                    move.l10n_in_tcs_tds_warning = warning_sections._get_warning_message()
+                else:
+                    move.l10n_in_tcs_tds_warning = False
+            else:
+                move.l10n_in_tcs_tds_warning = False
