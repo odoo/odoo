614e970dff5f82c623cd5354f84256036cb3335a
lase@odoo.com <lase@odoo.com>
2024-04-09 15:22:01 +0200

    [FIX] purchase_stock: apply picking type to replenishment stock moves
    Steps to reproduce:
    
    - Create a storable product and add a vendor
    - Go to settings and activate the Multi-step Routes
    - Go to Inventory > Configuration > Warehouse Manag. > Operations Types
    - Create a new Operation type of type Receipt with WH/Stock/Shelf 1 as
      default Destination Location
    - Go to Inventory > Operations > Replenishment
    - Create a new replenishment for one unit of your storable product using
      the Buy route with destination WH/Stock
    - Click on the truck icon > go to the associated Purchase order
    - Add your receipt operation in the deliver to field
    - Confirm the purchase order and go to the associated receipt
    
    Current behavior:
    
    The destination of the stock picking is set to WH/Stock/Shelf1 but the
    detailed operation of the stock move line is set to WH/Stock.
    
    Expected behavior:
    
    The destination of the stock move line should always be more or equally
    as precise as the associated stock picking. In this case the destination
    should be WH/Stock/Shelf1.
    
    Cause of the issue:
    
    Confirming the PO, will first create a stock picking and then generate
    the associated stock moves from the purchase order lines and the
    picking:
    https://github.com/odoo/odoo/blob/3f7b19ba05acf59c5780444be2446fcb0da7a907/addons/purchase_stock/models/purchase.py#L225-L230
    However, since the purchase order was created via an orderpoint
    (our replenishment), the purchase order line is associated with an
    orderpoint, so that the stock move destination will be set to the
    destination of the orderpoint in priority:
    https://github.com/odoo/odoo/blob/3f7b19ba05acf59c5780444be2446fcb0da7a907/addons/purchase_stock/models/purchase.py#L514
    
    opw-3812952

--- addons/purchase_stock/models/purchase.py
+++ addons/purchase_stock/models/purchase.py
@@ -511,7 +511,9 @@ class PurchaseOrderLine(models.Model):
             'date': date_planned,
             'date_deadline': date_planned,
             'location_id': self.order_id.partner_id.property_stock_supplier.id,
-            'location_dest_id': (self.orderpoint_id and not (self.move_ids | self.move_dest_ids)) and self.orderpoint_id.location_id.id or self.order_id._get_destination_location(),
+            'location_dest_id': (self.orderpoint_id and not (self.move_ids | self.move_dest_ids)
+                and (picking.location_dest_id.parent_path in self.orderpoint_id.location_id.parent_path))
+                and self.orderpoint_id.location_id.id or self.order_id._get_destination_location(),
             'picking_id': picking.id,
             'partner_id': self.order_id.dest_address_id.id,
             'move_dest_ids': [(4, x) for x in self.move_dest_ids.ids],
