31807bbcfea6fc30f5c017d00de896da47f706a1
Arnold Moyaux <arm@odoo.com>
2024-04-09 14:03:35 +0200

    [FIX] sale_stock: partial downpayment doesn't create COGS
    Usecase to reproduce:
    - Product wiht a real time valuation
    - Product with invoice on delivered quantity
    - Create a SO for 5 units and 1000$ each
    - Do a full downpayment of 100% of quotation
    - Deliver 3 out of 5 units and create a backorder
    - Create an invoice
    - Validate the invoice
    
    Expected behavior:
    The cogs entries are there
    
    Current behavior:
    No cogs
    
    It only happens with partial downpayment. When the downpayment amount
    equals the quotation amount. An invoice is created instead of a credit
    note and the process works correctly.
    
    It happens because it creates a credit note with a negative quantity to
    invoice so the system doesn't understand it has to create the cogs at
    that point.
    
    X-original-commit: d2a365c2ee9af6a9272d83183fc75fa6914bc560

--- addons/sale_stock/models/account_move.py
+++ addons/sale_stock/models/account_move.py
@@ -15,12 +15,17 @@ class AccountMove(models.Model):
         """ Overridden from stock_account.
         Returns the stock moves associated to this invoice."""
         rslt = super(AccountMove, self)._stock_account_get_last_step_stock_moves()
-        for invoice in self.filtered(lambda x: x.move_type == 'out_invoice'):
-            rslt += invoice.mapped('invoice_line_ids.sale_line_ids.move_ids').filtered(lambda x: x.state == 'done' and x.location_dest_id.usage == 'customer')
-        for invoice in self.filtered(lambda x: x.move_type == 'out_refund'):
-            rslt += invoice.mapped('reversed_entry_id.invoice_line_ids.sale_line_ids.move_ids').filtered(lambda x: x.state == 'done' and x.location_id.usage == 'customer')
-            # Add refunds generated from the SO
-            rslt += invoice.mapped('invoice_line_ids.sale_line_ids.move_ids').filtered(lambda x: x.state == 'done' and x.location_id.usage == 'customer')
+        for invoice in self:
+            if invoice.move_type not in ['out_invoice', 'out_refund']:
+                continue
+            if (invoice.move_type == 'out_invoice' or (
+                invoice.move_type == 'out_refund' and any(invoice.invoice_line_ids.sale_line_ids.mapped('is_downpayment')))
+            ):
+                rslt += invoice.mapped('invoice_line_ids.sale_line_ids.move_ids').filtered(lambda x: x.state == 'done' and x.location_dest_id.usage == 'customer')
+            else:
+                rslt += invoice.mapped('reversed_entry_id.invoice_line_ids.sale_line_ids.move_ids').filtered(lambda x: x.state == 'done' and x.location_id.usage == 'customer')
+                # Add refunds generated from the SO
+                rslt += invoice.mapped('invoice_line_ids.sale_line_ids.move_ids').filtered(lambda x: x.state == 'done' and x.location_id.usage == 'customer')
         return rslt
 
     def _get_invoiced_lot_values(self):
@@ -120,17 +125,32 @@ class AccountMoveLine(models.Model):
         price_unit = super(AccountMoveLine, self)._stock_account_get_anglo_saxon_price_unit()
 
         so_line = self.sale_line_ids and self.sale_line_ids[-1] or False
+        down_payment = self.move_id.invoice_line_ids.filtered(lambda line: line.sale_line_ids.is_downpayment)
         if so_line:
-            is_line_reversing = self.move_id.move_type == 'out_refund'
+            is_line_reversing = False
+            if self.move_id.move_type == 'out_refund' and not down_payment:
+                is_line_reversing = True
             qty_to_invoice = self.product_uom_id._compute_quantity(self.quantity, self.product_id.uom_id)
+            if self.move_id.move_type == 'out_refund' and down_payment:
+                qty_to_invoice = -qty_to_invoice
             account_moves = so_line.invoice_lines.move_id.filtered(lambda m: m.state == 'posted' and bool(m.reversed_entry_id) == is_line_reversing)
 
             posted_cogs = account_moves.line_ids.filtered(lambda l: l.display_type == 'cogs' and l.product_id == self.product_id and l.balance > 0)
-            qty_invoiced = sum([line.product_uom_id._compute_quantity(line.quantity, line.product_id.uom_id) for line in posted_cogs])
+            qty_invoiced = 0
+            product_uom = self.product_id.uom_id
+            for line in posted_cogs:
+                if float_compare(line.quantity, 0, precision_rounding=product_uom.rounding) and line.move_id.move_type == 'out_refund' and any(line.move_id.invoice_line_ids.sale_line_ids.mapped('is_downpayment')):
+                    qty_invoiced += line.product_uom_id._compute_quantity(abs(line.quantity), line.product_id.uom_id)
+                else:
+                    qty_invoiced += line.product_uom_id._compute_quantity(line.quantity, line.product_id.uom_id)
             value_invoiced = sum(posted_cogs.mapped('balance'))
 
             reversal_cogs = posted_cogs.move_id.reversal_move_id.line_ids.filtered(lambda l: l.display_type == 'cogs' and l.product_id == self.product_id and l.balance > 0)
-            qty_invoiced -= sum([line.product_uom_id._compute_quantity(line.quantity, line.product_id.uom_id) for line in reversal_cogs])
+            for line in reversal_cogs:
+                if float_compare(line.quantity, 0, precision_rounding=product_uom.rounding) and line.move_id.move_type == 'out_refund' and any(line.move_id.invoice_line_ids.sale_line_ids.mapped('is_downpayment')):
+                    qty_invoiced -= line.product_uom_id._compute_quantity(abs(line.quantity), line.product_id.uom_id)
+                else:
+                    qty_invoiced -= line.product_uom_id._compute_quantity(line.quantity, line.product_id.uom_id)
             value_invoiced -= sum(reversal_cogs.mapped('balance'))
 
             product = self.product_id.with_company(self.company_id).with_context(value_invoiced=value_invoiced)
