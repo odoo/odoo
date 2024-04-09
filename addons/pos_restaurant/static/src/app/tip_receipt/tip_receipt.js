7a34cdd4c6f00c1512ea6fd18dcd6d04f3ae2970
AH-Yussef <alhy@odoo.com>
2024-04-09 12:25:53 +0200

    [FIX] pos_restaurant: avoid printing empty receipts when tip after payment is active
    Steps to reproduce:
    - Install POS app.
    - Go to POS settings and enable:
        - Is a Bar/Restaurant
        - Tips > Add tip after payment
    - Open a POS session -if first time, add a floor and a table-
    - Add a product
    - Click on payment
    - Choose a payment method
    - Click on Close Tab
    - The print popup is shown twice in a row with an empty subtotal amount.
    
    Investigation:
    - Inside the `TipReceipt` template, the `total` is not shown as the class lacks a getter for it [1]
    - Also when there is no printer, we won't fallback to the web printer as it's annoying to the cashier.
    
    [1]: https://github.com/odoo/odoo/blob/1d49034782e3ff0e4384bad4e927a895e2a97839/addons/pos_restaurant/static/src/app/tip_receipt/tip_receipt.xml#L13-L16
    
    opw-3836549
    
    closes odoo/odoo#163250
    
    X-original-commit: 3aa2d05213b43233e3016899a1f3fdbcc4fe0b76
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/pos_restaurant/static/src/app/tip_receipt/tip_receipt.js
+++ addons/pos_restaurant/static/src/app/tip_receipt/tip_receipt.js
@@ -7,4 +7,8 @@ export class TipReceipt extends Component {
     static template = "pos_restaurant.TipReceipt";
     static components = { ReceiptHeader };
     static props = ["headerData", "data", "total"];
+
+    get total() {
+        return this.props.total;
+    }
 }
