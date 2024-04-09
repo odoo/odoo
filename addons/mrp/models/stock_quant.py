82cd175edfbbccfb7a1ac0c8537b53b113b917bd
Walid <waha@odoo.com>
2024-04-09 16:23:30 +0200

    [FIX] mrp: prevent kit product quant update
    using sudo().is_kits to account for user not having access
    to the bom module and boms in different companies

--- addons/mrp/models/stock_quant.py
+++ addons/mrp/models/stock_quant.py
@@ -6,12 +6,10 @@ class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
     def action_apply_inventory(self):
-        bom_kits = self.env['mrp.bom']._bom_find(self.product_id, bom_type='phantom')
-        for record in self:
-            if record.product_id in bom_kits:
-                raise RedirectWarning(
-                    _('You should update the components quantity instead of directly updating the quantity of the kit product.'),
-                    self.env.ref('stock.action_view_inventory_tree').id,
-                    _("Return to Inventory"),
-                )
+        if self.sudo().product_id.filtered("is_kits"):
+            raise RedirectWarning(
+                _('You should update the components quantity instead of directly updating the quantity of the kit product.'),
+                self.env.ref('stock.action_view_inventory_tree').id,
+                _("Return to Inventory"),
+            )
         return super().action_apply_inventory()
