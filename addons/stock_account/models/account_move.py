60ebf0e44877fea55d0103fc48778a0357f58c4a
axtr <axtr@odoo.com>
2024-04-10 14:33:05 +0200

    [FIX] stock_account: prevent ZeroDivision Error when the quantity is 0
    Problem:
    The error occurs when the user creates a bill with a product quantity of 0
    
    Steps to reproduce:
    - Install "Accounting", "Inventory", "Purchase" and "Sales" apps
    - Navigate to Sales > Products > Products and create a new product
    - Configure the product to be storable
    - Navigate to the "Purchase" tab and set the control policy to "On ordered quantities"
    - Navigate back to the "General Information" tab and create a new product category for this product
        - Set the "Inventory Valuation" to "Automated"
        - Set the "Price Difference Account"
    - Navigate to Accounting > Vendors > Bills
    - Create a new vendor bill and add the lately created product with a quantity of 0
    - Confirm the bill
    
    Cause:
    The quantity of the product is 0 and is the denominator of a fraction
    
    opw-3806378
    
    X-original-commit: 4a10d55d93801d65aa8c2306f96e5ea977692649

--- addons/stock_account/models/account_move.py
+++ addons/stock_account/models/account_move.py
@@ -255,6 +255,9 @@ class AccountMoveLine(models.Model):
         return self.product_id.type == 'product' and self.product_id.valuation == 'real_time'
 
     def _get_gross_unit_price(self):
+        if float_is_zero(self.quantity, precision_rounding=self.product_uom_id.rounding):
+            return self.price_unit
+
         price_unit = self.price_subtotal / self.quantity
         return -price_unit if self.move_id.move_type == 'in_refund' else price_unit
 
