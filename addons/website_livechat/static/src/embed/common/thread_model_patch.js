fdce0f0d41236cc653f86ff1422732e8053d8b7c
tsm-odoo <tsm@odoo.com>
2024-04-10 10:59:06 +0200

    [FIX] im_livechat: chat bot redirection on pages without chat bot
    Before this PR, the chatbot script would hang when redirected to a
    page where the chatbot is not enabled.
    
    When such a scenario occurs, we should restore the chatbot's state to
    where it stopped, and the script should continue.
    
    This PR fixes this issue.
    
    task-3625853
    
    closes odoo/odoo#161121
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/website_livechat/static/src/embed/common/thread_model_patch.js
+++ addons/website_livechat/static/src/embed/common/thread_model_patch.js
@@ -9,4 +9,8 @@ patch(Thread.prototype, {
         super.update(data);
         assignDefined(this, data, ["requested_by_operator"]);
     },
+
+    get displayWelcomeMessage() {
+        return super.displayWelcomeMessage && !this.requested_by_operator;
+    },
 });
