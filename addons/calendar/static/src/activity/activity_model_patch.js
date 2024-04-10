5ebb61bfeb9566773894420c9375cf4553b1d96b
Alexandre Kühn <aku@odoo.com>
2024-04-10 12:52:32 +0200

    [REF] mail: simplify discuss code (remove mail.activity service)
    Discuss code is more complicated than it should. Part of it
    comes from split in arbitrary services, which are both confusing
    and very verbose.
    
    This commit removes the `mail.activity` service: related
    features have been moved to the appropriate model.
    For example:
    
    ```js
    await this.env.services["mail.activity"].edit(activity);
    // =>
    await activity.edit();
    ```
    
    Part-of: odoo/odoo#161286

--- addons/calendar/static/src/activity/activity_model_patch.js
+++ addons/calendar/static/src/activity/activity_model_patch.js
@@ -1,5 +1,3 @@
-/** @odoo-module */
-
 import { Activity } from "@mail/core/web/activity_model";
 import { assignIn } from "@mail/utils/common/misc";
 import { patch } from "@web/core/utils/patch";
@@ -9,5 +7,16 @@ patch(Activity, {
         const activity = super._insert(...arguments);
         assignIn(activity, data, ["calendar_event_id"]);
         return activity;
+    },
+});
+
+patch(Activity.prototype, {
+    async rescheduleMeeting() {
+        const action = await this.store.env.services.orm.call(
+            "mail.activity",
+            "action_create_calendar_event",
+            [[this.id]]
+        );
+        this.store.env.services.action.doAction(action);
     },
 });
