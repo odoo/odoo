649a73d908338437575355b2cea581a5db91eabc
Walid <waha@odoo.com>
2024-04-10 12:00:01 +0200

    [FIX] stock_landed_costs: skip display lines in reconciliation
    Steps to reproduce:
    - Create a vendor bill with landed costs
    - Add section and note lines
    - Try posting the bill (ERROR)
    
    Bug:
    display lines should be skipped
    
    opw-3715660

--- addons/stock_landed_costs/models/stock_landed_cost.py
+++ addons/stock_landed_costs/models/stock_landed_cost.py
@@ -186,7 +186,7 @@ class StockLandedCost(models.Model):
                 for product in cost.cost_lines.product_id:
                     accounts = product.product_tmpl_id.get_product_accounts()
                     input_account = accounts['stock_input']
-                    all_amls.filtered(lambda aml: aml.account_id == input_account and not aml.reconciled).reconcile()
+                    all_amls.filtered(lambda aml: aml.account_id == input_account and not aml.reconciled and not aml.display_type).reconcile()
 
     def get_valuation_lines(self):
         self.ensure_one()
