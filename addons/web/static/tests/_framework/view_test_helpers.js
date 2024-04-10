19b47d1cff1ade66a0350ea2b05282995a5b214e
Michaël Mattiello <mcm@odoo.com>
2024-04-10 09:31:47 +0200

    [IMP] web: set target in mountView helper
    Before this commit, it was not possible to set a target element on
    which we want to mount the view.
    This commit adds a parameter to the mountView helper to set the target.

--- addons/web/static/tests/_framework/view_test_helpers.js
+++ addons/web/static/tests/_framework/view_test_helpers.js
@@ -213,12 +213,14 @@ export async function mountViewInDialog(params) {
 
 /**
  * @param {MountViewParams} params
+ * @param {HTMLElement} [target]
  */
-export async function mountView(params) {
+export async function mountView(params, target = null) {
     const config = { ...getDefaultConfig(), ...params.config };
     return mountWithCleanup(View, {
         env: params.env || getMockEnv() || (await makeMockEnv({ config })),
         props: parseViewProps(params),
+        target,
     });
 }
 
