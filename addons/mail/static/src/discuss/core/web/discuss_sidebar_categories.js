9222d83698fea6a7f98d20a70fc28f2663a938dc
Alexandre Kühn <aku@odoo.com>
2024-04-11 15:13:40 +0000

    [FIX] mail: discuss quick search with custom named chat
    Before this commit, when a DM chat had a custom name, the
    search was not taking it into account.
    
    Steps to reproduce:
    - Log in as Mitchell Admin
    - Open a DM chat with Marc Demo in Discuss app
    - Rename conversation to "Test"
    - Open more than 20 chat/channels (can create 20 channels)
    - type "Test" in the quick search of discuss app sidebar
    => The DM chat with Marc Demo is not visible in filtered sidebar.
    
    This happens because the quick search was relying on `thread.name`,
    which actually only matches exactly the UI when this is the name
    of a channel or when a group chat is explictly named.
    
    The actual thread field used that matches textual name of thread
    is `thread.displayName`, and this field is synced with custom DM
    chat if any.
    
    X-original-commit: b3ac8c39ad2b6c3d0e84c51615456d50c1e80c78

--- addons/mail/static/src/discuss/core/web/discuss_sidebar_categories.js
+++ addons/mail/static/src/discuss/core/web/discuss_sidebar_categories.js
@@ -66,7 +66,7 @@ export class DiscussSidebarCategories extends Component {
             return (
                 (thread.displayToSelf || thread.isLocallyPinned) &&
                 (!this.state.quickSearchVal ||
-                    cleanTerm(thread.name).includes(cleanTerm(this.state.quickSearchVal)))
+                    cleanTerm(thread.displayName).includes(cleanTerm(this.state.quickSearchVal)))
             );
         });
     }
