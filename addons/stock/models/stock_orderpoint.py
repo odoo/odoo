d3974111d4a829016ff2808a9f4cc736834fd46c
clesgow <quwo@odoo.com>
2024-04-11 13:16:57 +0200

    [FIX] stock: display inter-wh notification
    With this condition, a 'classic' inter-warehouse resupply wouldn't
    display any notification, as both the incoming move (i.e.
    inter-warehouse -> Stock) and the orderpoint would have the same
    warehouse.
    
    Instead, display the notification when the the move either comes from a
    transit location or another warehouse.
    
    closes odoo/odoo#161320
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/stock/models/stock_orderpoint.py
+++ addons/stock/models/stock_orderpoint.py
@@ -471,7 +471,8 @@ class StockWarehouseOrderpoint(models.Model):
         if self.env.context.get('written_after'):
             domain = expression.AND([domain, [('write_date', '>=', self.env.context.get('written_after'))]])
         move = self.env['stock.move'].search(domain, limit=1)
-        if move.picking_id and any(move.warehouse_id != self.warehouse_id for move in move):
+        if ((move.location_id.warehouse_id and move.location_id.warehouse_id != self.warehouse_id)
+            or move.location_id.usage == 'transit') and move.picking_id:
             action = self.env.ref('stock.stock_picking_action_picking_type')
             return {
                 'type': 'ir.actions.client',
