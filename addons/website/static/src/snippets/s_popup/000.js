9d45c227e59e0f4cb4a9425c7a7985be4b3a7650
Robin Lejeune (role) <role@odoo.com>
2024-04-11 09:59:56 +0000

    [FIX] website, website_livechat: hide cookies bar on policy page
    With the popup-styled cookies bar, the cookies policy page is
    unreadable. It is also the case with all cookies bar with a backdrop, or
    if they are too tall.
    The "discrete" and "classic" bars can also potentially hide the last
    paragraph(s) of the page.
    To avoid that, this commit adds a button to toggle the bar on that page.
    
    Note that the cookies policy page could be on any URL, but it has to be
    referenced in the `a.o_cookies_bar_text_policy` link within the cookies
    bar.
    
    task-3733860
    
    closes odoo/odoo#164576
    
    X-original-commit: 2110544d4674e16d6a6b6a67889215727ab9e393
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/website/static/src/snippets/s_popup/000.js
+++ addons/website/static/src/snippets/s_popup/000.js
@@ -2,10 +2,12 @@ odoo.define('website.s_popup', function (require) {
 'use strict';
 
 const config = require('web.config');
+const { _t } = require("@web/core/l10n/translation");
 const dom = require('web.dom');
 const publicWidget = require('web.public.widget');
-const {getCookie, setCookie} = require('web.utils.cookies');
+const { getCookie, setCookie, deleteCookie } = require("web.utils.cookies");
 const {setUtmsHtmlDataset} = require('@website/js/content/inject_dom');
+const { cloneContentEls, onceAllImagesLoaded } = require("website.utils");
 
 // TODO In master, export this class too or merge it with PopupWidget
 const SharedPopupWidget = publicWidget.Widget.extend({
@@ -92,7 +94,7 @@ const SharedPopupWidget = publicWidget.Widget.extend({
 publicWidget.registry.SharedPopup = SharedPopupWidget;
 
 const PopupWidget = publicWidget.Widget.extend({
-    selector: '.s_popup',
+    selector: ".s_popup:not(#website_cookies_bar)",
     events: {
         'click .js_close_popup': '_onCloseClick',
         'hide.bs.modal': '_onHideModal',
@@ -314,6 +316,77 @@ publicWidget.registry.cookies_bar = PopupWidget.extend({
         'click #cookies-consent-essential, #cookies-consent-all': '_onAcceptClick',
     }),
 
+    /**
+     * @override
+     */
+    destroy() {
+        if (this.toggleEl) {
+            this.toggleEl.removeEventListener("click", this._onToggleCookiesBar);
+            this.toggleEl.remove();
+        }
+        this._super(...arguments);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    _showPopup() {
+        this._super(...arguments);
+        const policyLinkEl = this.el.querySelector(".o_cookies_bar_text_policy");
+        if (policyLinkEl && window.location.pathname === new URL(policyLinkEl.href).pathname) {
+            this.toggleEl = cloneContentEls(`
+            <button class="o_cookies_bar_toggle btn btn-info btn-sm rounded-circle d-flex align-items-center position-fixed pe-auto">
+                <i class="fa fa-eye" alt="" aria-hidden="true"></i> <span class="o_cookies_bar_toggle_label"></span>
+            </button>
+            `).firstElementChild;
+            this.el.insertAdjacentElement("beforebegin", this.toggleEl);
+            this._toggleCookiesBar();
+            this._onToggleCookiesBar = this._toggleCookiesBar.bind(this);
+            this.toggleEl.addEventListener("click", this._onToggleCookiesBar);
+        }
+    },
+    /**
+     * Toggles the cookies bar with a button so that the page is readable.
+     *
+     * @private
+     */
+    _toggleCookiesBar() {
+        const popupEl = this.el.querySelector(".modal");
+        $(popupEl).modal("toggle");
+        // As we're using Bootstrap's events, the PopupWidget prevents the modal
+        // from being shown after hiding it: override that behavior.
+        this._popupAlreadyShown = false;
+        deleteCookie(this.el.id);
+
+        const hidden = !popupEl.classList.contains("show");
+        this.toggleEl.querySelector(".fa").className = `fa ${hidden ? "fa-eye" : "fa-eye-slash"}`;
+        this.toggleEl.querySelector(".o_cookies_bar_toggle_label").innerText = hidden
+            ? _t("Show the cookies bar")
+            : _t("Hide the cookies bar");
+        if (hidden || !popupEl.classList.contains("s_popup_bottom")) {
+            this.toggleEl.style.removeProperty("--cookies-bar-toggle-inset-block-end");
+        } else {
+            // Lazy-loaded images don't have a height yet. We need to await them
+            onceAllImagesLoaded($(popupEl)).then(() => {
+                const popupHeight = popupEl.querySelector(".modal-content").offsetHeight;
+                const toggleMargin = 8;
+                // Avoid having the toggleEl over another button, but if the
+                // cookies bar is too tall, place it at the bottom anyway.
+                const bottom = document.body.offsetHeight > popupHeight + this.toggleEl.offsetHeight + toggleMargin
+                    ? `calc(
+                        ${getComputedStyle(popupEl.querySelector(".modal-dialog")).paddingBottom}
+                        + ${popupHeight + toggleMargin}px
+                    )`
+                    : "";
+                this.toggleEl.style.setProperty("--cookies-bar-toggle-inset-block-end", bottom);
+            });
+        }
+    },
+
     //--------------------------------------------------------------------------
     // Handlers
     //--------------------------------------------------------------------------
@@ -325,6 +398,7 @@ publicWidget.registry.cookies_bar = PopupWidget.extend({
     _onAcceptClick(ev) {
         this.cookieValue = `{"required": true, "optional": ${ev.target.id === 'cookies-consent-all'}}`;
         this._onHideModal();
+        this.toggleEl && this.toggleEl.remove();
     },
     /**
      * @override
