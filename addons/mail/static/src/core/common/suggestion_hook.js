2036427bf43b0bf38d35321274687d031a0e26f0
Alexandre Kühn <aku@odoo.com>
2024-04-11 12:22:17 +0200

    [REF] mail: slightly improve suggestion hook (class-syntax)
    The useSuggestion hook is complex and hard to work with.
    This commit simplifies by using a class syntax.
    By doing so:
    
    - code has reduced indent level
    - `self` becomes `this` and take advantage of keyword highlight
    - eases QoL improvements, e.g. the many occurrences of
      `comp.props.composer` just become a `get composer()`
    
    Part-of: odoo/odoo#161286

--- addons/mail/static/src/core/common/suggestion_hook.js
+++ addons/mail/static/src/core/common/suggestion_hook.js
@@ -3,190 +3,196 @@ import { status, useComponent, useEffect, useState } from "@odoo/owl";
 
 import { useService } from "@web/core/utils/hooks";
 
-export function useSuggestion() {
-    const comp = useComponent();
-    const sequential = useSequential();
-    const suggestionService = useService("mail.suggestion");
-    const self = {
-        clearRawMentions() {
-            comp.props.composer.mentionedChannels.length = 0;
-            comp.props.composer.mentionedPartners.length = 0;
-        },
-        clearCannedReponses() {
-            comp.props.composer.cannedResponses = [];
-        },
-        clearSearch() {
-            Object.assign(self.search, {
-                delimiter: undefined,
-                position: undefined,
-                term: "",
-            });
-            self.state.items = undefined;
-        },
-        detect() {
-            const selectionEnd = comp.props.composer.selection.end;
-            const selectionStart = comp.props.composer.selection.start;
-            const content = comp.props.composer.textInputContent;
-            if (selectionStart !== selectionEnd) {
-                // avoid interfering with multi-char selection
-                self.clearSearch();
-                return;
-            }
-            const candidatePositions = [];
-            // consider the chars before the current cursor position
-            let numberOfSpaces = 0;
-            for (let index = selectionStart - 1; index >= 0; --index) {
-                if (/\s/.test(content[index])) {
-                    numberOfSpaces++;
-                    if (numberOfSpaces === 2) {
-                        // The consideration stops after the second space since
-                        // a majority of partners have a two-word name. This
-                        // removes the need to check for mentions following a
-                        // delimiter used earlier in the content.
-                        break;
+class UseSuggestion {
+    constructor(comp) {
+        this.comp = comp;
+        useEffect(
+            (delimiter, position, term) => {
+                this.update();
+                if (this.search.position === undefined || !this.search.delimiter) {
+                    return; // nothing else to fetch
+                }
+                this.sequential(async () => {
+                    if (
+                        this.search.delimiter !== delimiter ||
+                        this.search.position !== position ||
+                        this.search.term !== term
+                    ) {
+                        return; // ignore obsolete call
                     }
-                }
-                candidatePositions.push(index);
-            }
-            // keep the current delimiter if it is still valid
-            if (self.search.position !== undefined && self.search.position < selectionStart) {
-                candidatePositions.push(self.search.position);
-            }
-            const supportedDelimiters = suggestionService.getSupportedDelimiters(self.thread);
-            for (const candidatePosition of candidatePositions) {
-                if (candidatePosition < 0 || candidatePosition >= content.length) {
-                    continue;
-                }
-                const candidateChar = content[candidatePosition];
-                if (
-                    !supportedDelimiters.find(
-                        ([delimiter, allowedPosition]) =>
-                            delimiter === candidateChar &&
-                            (allowedPosition === undefined || allowedPosition === candidatePosition)
-                    )
-                ) {
-                    continue;
-                }
-                const charBeforeCandidate = content[candidatePosition - 1];
-                if (charBeforeCandidate && !/\s/.test(charBeforeCandidate)) {
-                    continue;
-                }
-                Object.assign(self.search, {
-                    delimiter: candidateChar,
-                    position: candidatePosition,
-                    term: content.substring(candidatePosition + 1, selectionStart),
+                    await this.suggestionService.fetchSuggestions(this.search, {
+                        thread: this.thread,
+                    });
+                    if (status(comp) === "destroyed") {
+                        return;
+                    }
+                    this.update();
+                    if (
+                        this.search.delimiter === delimiter &&
+                        this.search.position === position &&
+                        this.search.term === term &&
+                        !this.state.items?.suggestions.length
+                    ) {
+                        this.clearSearch();
+                    }
                 });
-                self.state.count++;
-                return;
+            },
+            () => {
+                return [this.search.delimiter, this.search.position, this.search.term];
             }
-            self.clearSearch();
-        },
-        get thread() {
-            return comp.props.composer.thread || comp.props.composer.message.thread;
-        },
-        insert(option) {
-            const cursorPosition = comp.props.composer.selection.start;
-            const content = comp.props.composer.textInputContent;
-            let textLeft = content.substring(0, self.search.position + 1);
-            let textRight = content.substring(cursorPosition, content.length);
-            if (self.search.delimiter === ":") {
-                textLeft = content.substring(0, self.search.position);
-                textRight = content.substring(cursorPosition, content.length);
-            }
-            const recordReplacement = option.label;
-            if (option.partner) {
-                comp.props.composer.mentionedPartners.add({
-                    id: option.partner.id,
-                    type: "partner",
-                });
-            }
-            if (option.thread) {
-                comp.props.composer.mentionedChannels.add({
-                    model: "discuss.channel",
-                    id: option.thread.id,
-                });
-            }
-            if (option.cannedResponse) {
-                comp.props.composer.cannedResponses.push(option.cannedResponse);
-            }
-            self.clearSearch();
-            comp.props.composer.textInputContent = textLeft + recordReplacement + " " + textRight;
-            comp.props.composer.selection.start = textLeft.length + recordReplacement.length + 1;
-            comp.props.composer.selection.end = textLeft.length + recordReplacement.length + 1;
-            comp.props.composer.forceCursorMove = true;
-        },
-        search: {
+        );
+        useEffect(
+            () => {
+                this.detect();
+            },
+            () => [
+                this.composer.selection.start,
+                this.composer.selection.end,
+                this.composer.textInputContent,
+            ]
+        );
+    }
+    /** @type {import("@mail/core/common/composer").Composer} */
+    comp;
+    get composer() {
+        return this.comp.props.composer;
+    }
+    sequential = useSequential();
+    suggestionService = useService("mail.suggestion");
+    state = useState({
+        count: 0,
+        items: undefined,
+    });
+    search = {
+        delimiter: undefined,
+        position: undefined,
+        term: "",
+    };
+    clearRawMentions() {
+        this.composer.mentionedChannels.length = 0;
+        this.composer.mentionedPartners.length = 0;
+    }
+    clearCannedReponses() {
+        this.composer.cannedResponses = [];
+    }
+    clearSearch() {
+        Object.assign(this.search, {
             delimiter: undefined,
             position: undefined,
             term: "",
-        },
-        state: useState({
-            count: 0,
-            items: undefined,
-        }),
-        update() {
-            if (!self.search.delimiter) {
-                return;
-            }
-            const { type, suggestions } = suggestionService.searchSuggestions(self.search, {
-                thread: self.thread,
-                sort: true,
-            });
-            if (!suggestions.length) {
-                self.state.items = undefined;
-                return;
-            }
-            // arbitrary limit to avoid displaying too many elements at once
-            // ideally a load more mechanism should be introduced
-            const limit = 8;
-            suggestions.length = Math.min(suggestions.length, limit);
-            self.state.items = { type, suggestions };
-        },
-    };
-    useEffect(
-        (delimiter, position, term) => {
-            self.update();
-            if (self.search.position === undefined || !self.search.delimiter) {
-                return; // nothing else to fetch
-            }
-            sequential(async () => {
-                if (
-                    self.search.delimiter !== delimiter ||
-                    self.search.position !== position ||
-                    self.search.term !== term
-                ) {
-                    return; // ignore obsolete call
-                }
-                await suggestionService.fetchSuggestions(self.search, {
-                    thread: self.thread,
-                });
-                if (status(comp) === "destroyed") {
-                    return;
-                }
-                self.update();
-                if (
-                    self.search.delimiter === delimiter &&
-                    self.search.position === position &&
-                    self.search.term === term &&
-                    !self.state.items?.suggestions.length
-                ) {
-                    self.clearSearch();
-                }
-            });
-        },
-        () => {
-            return [self.search.delimiter, self.search.position, self.search.term];
+        });
+        this.state.items = undefined;
+    }
+    detect() {
+        const { start, end } = this.composer.selection;
+        const content = this.composer.textInputContent;
+        if (start !== end) {
+            // avoid interfering with multi-char selection
+            this.clearSearch();
+            return;
         }
-    );
-    useEffect(
-        () => {
-            self.detect();
-        },
-        () => [
-            comp.props.composer.selection.start,
-            comp.props.composer.selection.end,
-            comp.props.composer.textInputContent,
-        ]
-    );
-    return self;
+        const candidatePositions = [];
+        // consider the chars before the current cursor position
+        let numberOfSpaces = 0;
+        for (let index = start - 1; index >= 0; --index) {
+            if (/\s/.test(content[index])) {
+                numberOfSpaces++;
+                if (numberOfSpaces === 2) {
+                    // The consideration stops after the second space since
+                    // a majority of partners have a two-word name. This
+                    // removes the need to check for mentions following a
+                    // delimiter used earlier in the content.
+                    break;
+                }
+            }
+            candidatePositions.push(index);
+        }
+        // keep the current delimiter if it is still valid
+        if (this.search.position !== undefined && this.search.position < start) {
+            candidatePositions.push(this.search.position);
+        }
+        const supportedDelimiters = this.suggestionService.getSupportedDelimiters(this.thread);
+        for (const candidatePosition of candidatePositions) {
+            if (candidatePosition < 0 || candidatePosition >= content.length) {
+                continue;
+            }
+            const candidateChar = content[candidatePosition];
+            if (
+                !supportedDelimiters.find(
+                    ([delimiter, allowedPosition]) =>
+                        delimiter === candidateChar &&
+                        (allowedPosition === undefined || allowedPosition === candidatePosition)
+                )
+            ) {
+                continue;
+            }
+            const charBeforeCandidate = content[candidatePosition - 1];
+            if (charBeforeCandidate && !/\s/.test(charBeforeCandidate)) {
+                continue;
+            }
+            Object.assign(this.search, {
+                delimiter: candidateChar,
+                position: candidatePosition,
+                term: content.substring(candidatePosition + 1, start),
+            });
+            this.state.count++;
+            return;
+        }
+        this.clearSearch();
+    }
+    get thread() {
+        return this.composer.thread || this.composer.message.thread;
+    }
+    insert(option) {
+        const position = this.composer.selection.start;
+        const content = this.composer.textInputContent;
+        let before = content.substring(0, this.search.position + 1);
+        let after = content.substring(position, content.length);
+        if (this.search.delimiter === ":") {
+            before = content.substring(0, this.search.position);
+            after = content.substring(position, content.length);
+        }
+        if (option.partner) {
+            this.composer.mentionedPartners.add({
+                id: option.partner.id,
+                type: "partner",
+            });
+        }
+        if (option.thread) {
+            this.composer.mentionedChannels.add({
+                model: "discuss.channel",
+                id: option.thread.id,
+            });
+        }
+        if (option.cannedResponse) {
+            this.composer.cannedResponses.push(option.cannedResponse);
+        }
+        this.clearSearch();
+        this.composer.textInputContent = before + option.label + " " + after;
+        this.composer.selection.start = before.length + option.label.length + 1;
+        this.composer.selection.end = before.length + option.label.length + 1;
+        this.composer.forceCursorMove = true;
+    }
+    update() {
+        if (!this.search.delimiter) {
+            return;
+        }
+        const { type, suggestions } = this.suggestionService.searchSuggestions(this.search, {
+            thread: this.thread,
+            sort: true,
+        });
+        if (!suggestions.length) {
+            this.state.items = undefined;
+            return;
+        }
+        // arbitrary limit to avoid displaying too many elements at once
+        // ideally a load more mechanism should be introduced
+        const limit = 8;
+        suggestions.length = Math.min(suggestions.length, limit);
+        this.state.items = { type, suggestions };
+    }
+}
+
+export function useSuggestion() {
+    return new UseSuggestion(useComponent());
 }
