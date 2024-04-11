0f6bbe98b6dddb80caa2290a9904cf1bc345879a
Katherine Zaoral <kz@adhoc.com.ar>
2024-04-11 16:24:11 -0300

    [FIX] l10n_ar: make journal code unique
    this works this way in 16+. need to be added to avoid error
    in unit test for l10n_ar_edi
    
    closes odoo/odoo#161600
    
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- addons/l10n_ar/tests/common.py
+++ addons/l10n_ar/tests/common.py
@@ -629,7 +629,7 @@ class TestAr(AccountTestInvoicingCommon):
             pos_number = data.get('l10n_ar_afip_pos_number')
         values = {'name': '%s %s' % (afip_ws.replace('WS', ''), pos_number),
                   'type': 'sale',
-                  'code': afip_ws,
+                  'code': pos_number,
                   'l10n_ar_afip_pos_system': self._get_afip_pos_system_real_name().get(afip_ws),
                   'l10n_ar_afip_pos_number': pos_number,
                   'l10n_latam_use_documents': True,
