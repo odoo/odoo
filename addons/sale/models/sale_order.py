8e8ea7e1ec1067815aafc772874267b46a0e390f
Victor Feyens <vfe@odoo.com>
2024-04-11 15:29:11 +0200

    [FIX] sale: discount lines should not be invoiced alone
    Delivery and loyalty discount lines were already disregarded
    when computing the invoice status if they were the only
    lines to invoice, but new logic of discount lines,
    introduced in 17 with bf95b1f2ade01a79263461987e62d3b5b1b09483,
    didn't consider this situation.
    
    This commit harmonizes the existing logic of `loyalty` & `delivery`,
    directly in `sale`, making sure this is correctly handled at once
    for the three cases.
    
    opw-3853012
    
    closes odoo/odoo#163863
    
    X-original-commit: c6dda2b84fec0ba7b9d844135879ff8c975f4202
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -533,20 +533,33 @@ class SaleOrder(models.Model):
         (self - confirmed_orders).invoice_status = 'no'
         if not confirmed_orders:
             return
+        lines_domain = [('is_downpayment', '=', False), ('display_type', '=', False)]
         line_invoice_status_all = [
             (order.id, invoice_status)
-            for order, invoice_status in self.env['sale.order.line']._read_group([
-                    ('order_id', 'in', confirmed_orders.ids),
-                    ('is_downpayment', '=', False),
-                    ('display_type', '=', False),
-                ],
-                ['order_id', 'invoice_status'])]
+            for order, invoice_status in self.env['sale.order.line']._read_group(
+                lines_domain + [('order_id', 'in', confirmed_orders.ids)],
+                ['order_id', 'invoice_status']
+            )
+        ]
         for order in confirmed_orders:
             line_invoice_status = [d[1] for d in line_invoice_status_all if d[0] == order.id]
             if order.state != 'sale':
                 order.invoice_status = 'no'
             elif any(invoice_status == 'to invoice' for invoice_status in line_invoice_status):
-                order.invoice_status = 'to invoice'
+                if any(invoice_status == 'no' for invoice_status in line_invoice_status):
+                    # If only discount/delivery/promotion lines can be invoiced, the SO should not
+                    # be invoiceable.
+                    invoiceable_domain = lines_domain + [('invoice_status', '=', 'to invoice')]
+                    invoiceable_lines = order.order_line.filtered_domain(invoiceable_domain)
+                    special_lines = invoiceable_lines.filtered(
+                        lambda sol: not sol._can_be_invoiced_alone()
+                    )
+                    if invoiceable_lines == special_lines:
+                        order.invoice_status = 'no'
+                    else:
+                        order.invoice_status = 'to invoice'
+                else:
+                    order.invoice_status = 'to invoice'
             elif line_invoice_status and all(invoice_status == 'invoiced' for invoice_status in line_invoice_status):
                 order.invoice_status = 'invoiced'
             elif line_invoice_status and all(invoice_status in ('invoiced', 'upselling') for invoice_status in line_invoice_status):
