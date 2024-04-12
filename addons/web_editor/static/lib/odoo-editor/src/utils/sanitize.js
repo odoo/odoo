f5384f16dbda467d32563f27104e2c2584cecfc8
Antoine Guenet <age@odoo.com>
2024-04-12 12:22:46 +0200

    [FIX] web_editor: do not merge contiguous pictograms on sanitize
    The sanitizer has a mechanism to merge similar nodes together (eg,
    `<span id="a">a</span><span id="a">b</span>` becomes
    `<span id="a">ab</span>`). But the part that prevents merging such nodes
    which contain a pseudo-element is insufficient for preventing the
    merging of two pictograms in tests since the font awesome styles aren't
    imported.
    
    This happened to work before anyway, but merely by accident as the
    sanitizer was applying the `contenteditable="false"` attribute to one
    pictogram then comparing it to its neighbor before having done the same.
    Their attributes not being equal, they could not be merged. But this is
    a false negative: if the sanitizer already ran once and the attribute
    has therefore already been applied, running it again on the same
    document will merge the pictograms.

--- addons/web_editor/static/lib/odoo-editor/src/utils/sanitize.js
+++ addons/web_editor/static/lib/odoo-editor/src/utils/sanitize.js
@@ -60,7 +60,8 @@ export function areSimilarElements(node, node2) {
         isNotNoneValue(getComputedStyle(node, ':before').getPropertyValue('content')) ||
         isNotNoneValue(getComputedStyle(node, ':after').getPropertyValue('content')) ||
         isNotNoneValue(getComputedStyle(node2, ':before').getPropertyValue('content')) ||
-        isNotNoneValue(getComputedStyle(node2, ':after').getPropertyValue('content'))
+        isNotNoneValue(getComputedStyle(node2, ':after').getPropertyValue('content')) ||
+        isFontAwesome(node) || isFontAwesome(node2)
     ) {
         return false;
     }
