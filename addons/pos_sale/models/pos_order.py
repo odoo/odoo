e3c20abc7e453f61eb1d6062846a556b77e879dc
Jitendra Prajapat <jipr@odoo.com>
2024-04-12 15:20:40 +0530

    [FIX] pos_sale: duplicate down paytment lines
    Before this commit:
    ===================
    Duplicate down payment lines were being generated multiple times upon
    clicking the order button after importing the Quotation/Order.
    
    Steps To Reproduced:
    =====================
    - Step 1: Create order in Sales and save it  
    - Step 2: Open POS Restaurant and click on Quotation/Order 
    - Step 3: Select order and Settle order as Down-payment 
    - Step 4: Once added into cart click on the order button multiple times.
    - Step 5: Go to back-end and open Sales order 
    
      You'll see multiple down-payment entries corresponding to each click on the
      order button.
    
    After this commit:
    ===================
    The creation of duplicate down payment lines is prevented.
    
    task - 3877380
    
    closes odoo/odoo#167231
    
    X-original-commit: 3d72c8e75dc0df9a1b34bb80773bb2c4ae678ca9
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Jitendra Kumar Prajapat (jipr) <jipr@odoo.com>

--- addons/pos_sale/models/pos_order.py
+++ addons/pos_sale/models/pos_order.py
@@ -47,6 +47,9 @@ class PosOrder(models.Model):
     @api.model
     def create_from_ui(self, orders, draft=False):
         order_ids = super(PosOrder, self).create_from_ui(orders, draft)
+        if draft:
+            return order_ids
+
         for order in self.sudo().browse([o['id'] for o in order_ids]):
             for line in order.lines.filtered(lambda l: l.product_id == order.config_id.down_payment_product_id and l.qty != 0 and (l.sale_order_origin_id or l.refunded_orderline_id.sale_order_origin_id)):
                 sale_lines = line.sale_order_origin_id.order_line or line.refunded_orderline_id.sale_order_origin_id.order_line
