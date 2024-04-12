43d7d902aeb04caf5dab29467f68c74952c6c72f
Antoine Guenet <age@odoo.com>
2024-04-12 12:36:13 +0200

    [FIX] web_editor: properly sanitize paragraphs in list items
    Paragraphs are not admitted in list items in the editor. But the way
    they were sanitized away was broken in several regards.
    
    Empty paragraphs were completely removed, meaning
    `<li><p><br></p><p>a</p></li>` became `<li>a</li>`, missing vertical
    spacing in the process. We now preserve them.
    
    Contiguous paragraphs in a list item were also just unwrapped, meaning
    their contents became contiguous as well (eg,
    `<li><p>a</p><p>b</p></li>` became `<li>ab</li>`). We now split them
    each into their own new list item with the "oe-nested" class to remove
    the bullet point (the example now becomes `<li>a</li><li
    class="oe-nested">b</li>`).
    
    In the process, some tests were adapted, showing the behavior is now
    more consistent (eg, `<ul><li>a</li><li>[]b</li></ul>` -> backspace is
    supposed to outdent the second list item (`<ul><li>a</li></ul><p>b</p>`)
    but some tests expected the list items to be merged instead, when they
    contained paragraphs originally, since the sanitizing process failed to
    preserve the visual aspect of the content).

--- addons/web_editor/static/src/js/editor/odoo-editor/src/utils/sanitize.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/utils/sanitize.js
@@ -6,8 +6,8 @@ import {
     fillEmpty,
     getListMode,
     isBlock,
-    isEmptyBlock,
     isSelfClosingElement,
+    getAdjacentNextSiblings,
     moveNodes,
     preserveCursor,
     isFontAwesome,
@@ -192,18 +192,33 @@ function sanitizeNode(node, root) {
         !node.parentElement.classList.contains('nav-item')
     ) {
         // Remove empty paragraphs in <li>.
+        const previous = node.previousSibling;
+        const nextSiblings = getAdjacentNextSiblings(node);
         const classes = node.classList;
         const parent = node.parentElement;
         const restoreCursor = shouldPreserveCursor(node, root) && preserveCursor(root.ownerDocument);
-        if (isEmptyBlock(node)) {
-            node.remove();
-        } else if (classes.length) {
-            const spanEl = document.createElement('span');
-            spanEl.setAttribute('class', classes);
-            spanEl.append(...node.childNodes);
-            node.replaceWith(spanEl);
+        if (previous) {
+            const newLi = document.createElement('li');
+            newLi.classList.add('oe-nested');
+            parent.after(newLi);
+            newLi.append(node, ...nextSiblings);
+            if (classes.length) {
+                const spanEl = document.createElement('span');
+                spanEl.setAttribute('class', classes);
+                spanEl.append(...node.childNodes);
+                node.replaceWith(spanEl);
+            } else {
+                unwrapContents(node);
+            }
         } else {
-            unwrapContents(node);
+            if (classes.length) {
+                const spanEl = document.createElement('span');
+                spanEl.setAttribute('class', classes);
+                spanEl.append(...node.childNodes);
+                node.replaceWith(spanEl);
+            } else {
+                unwrapContents(node);
+            }
         }
         fillEmpty(parent);
         restoreCursor && restoreCursor(new Map([[node, parent]]));
