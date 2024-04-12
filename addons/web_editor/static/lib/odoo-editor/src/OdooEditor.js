a2de3acf206037de52d8f0507937317bc14cad2d
Antoine Guenet <age@odoo.com>
2024-04-12 15:17:03 +0200

    [FIX] web_editor: account for multiple ZWNBSP in one text node
    Sometimes the DOM merges text node together and we can end up with
    zwnbsp merged together into one text node. This makes the assumption
    that ZWNBSP characters are always isolated into one text node, wrong.
    
    Part-of: odoo/odoo#161699
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -1425,7 +1425,7 @@ export class OdooEditor extends EventTarget {
         let range = getDeepRange(this.editable, { sel, correctTripleClick: true });
         if (!range) return;
         for (const node of descendants(closestBlock(range.commonAncestorContainer))) {
-            if (node.nodeType === Node.TEXT_NODE && node.textContent === '\uFEFF') {
+            if (node.nodeType === Node.TEXT_NODE && [...node.textContent].every(char => char === '\uFEFF')) {
                 const restore = prepareUpdate(...leftPos(node));
                 node.remove();
                 restore(); // Make sure to make <br>s visible if needed.
