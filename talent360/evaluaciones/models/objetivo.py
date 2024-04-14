6644041bb2945f2f0e28e5a4b3bd425e6f251129
Carlos <A01708634@tec.mx>
2024-04-14 13:05:01 -0600

    feat(objetivos): Agregar restricciones del modelo de objetivos

--- talent360/evaluaciones/models/objetivo.py
+++ talent360/evaluaciones/models/objetivo.py
@@ -1,11 +1,12 @@
-from odoo import models, fields
+from odoo import api, models, fields
+from odoo.exceptions import UserError, ValidationError
 
 class Objetivo(models.Model):
     _name = "objetivo"
     _description = "Objetivos de desempeño"
 
-    titulo = fields.Char(required=True)
-    descripcion = fields.Char(required=True)
+    titulo = fields.Char(required=True, string="Título")
+    descripcion = fields.Char(required=True, string="Descripción")
     metrica = fields.Selection(
         [
             ("porcentaje", "Porcentaje"),
@@ -13,6 +14,7 @@ class Objetivo(models.Model):
         ],
         default="porcentaje",
         required=True,
+        string="Métrica",
     )
 
     tipo = fields.Selection(
@@ -34,8 +36,8 @@ class Objetivo(models.Model):
     )
 
     peso = fields.Integer(required=True)
-    piso_minimo = fields.Integer(required=True)
-    piso_maximo = fields.Integer(required=True)
+    piso_minimo = fields.Integer(required=True, string="Piso Mínimo")
+    piso_maximo = fields.Integer(required=True, string="Piso Máximo")
     resultado = fields.Integer()
 
     usuario_ids = fields.Many2many(
@@ -45,3 +47,32 @@ class Objetivo(models.Model):
         "usuario_id",
         string="Asignados",
     )
+
+    @api.constrains("piso_minimo", "piso_maximo")
+    def _check_pisos(self):
+        for record in self:
+            if record.piso_minimo >= record.piso_maximo:
+                raise ValidationError("El piso mínimo debe ser menor al piso máximo")
+            if record.piso_minimo < 0 or record.piso_maximo < 0:
+                raise ValidationError("Los pisos minimos y maximos deben ser mayores a 0")
+
+    @api.constrains("peso")
+    def _check_peso(self):
+        for record in self:
+            if record.peso > 100 or record.peso <= 0:
+                raise ValidationError("El peso debe estar en el rango de 1 y 100")
+            
+    def write(self, vals):
+        if "piso_minimo" in vals or "piso_maximo" in vals:
+            new_piso_minimo = vals.get("piso_minimo", self.piso_minimo)
+            new_piso_maximo = vals.get("piso_maximo", self.piso_maximo)
+            if new_piso_minimo >= new_piso_maximo:
+                raise ValidationError("El piso mínimo debe ser menor al piso máximo")
+            if new_piso_minimo < 0 or new_piso_maximo < 0:
+                raise ValidationError("Los pisos mínimos y máximos deben ser mayores a 0")
+            
+        if "peso" in vals:
+            new_peso = vals.get("peso", self.peso)
+            if new_peso > 100 or new_peso <= 0:
+                raise ValidationError("El peso debe estar en el rango de 1 y 100")
+        return super(Objetivo, self).write(vals)
