941278eb248bfb2b24ea83fc44437f6c92d3bc90
Romain Estievenart <res@odoo.com>
2024-04-15 08:51:14 +0200

    [IMP] web: convert gauge field tests to hoot
    task-3705027

---
+++ addons/web/static/tests/views/fields/gauge_field.test.js
@@ -0,0 +1,35 @@
+import { defineModels, fields, models, mountView } from "@web/../tests/web_test_helpers";
+import { expect, test } from "@odoo/hoot";
+import { queryAllTexts } from "@odoo/hoot-dom";
+
+class Partner extends models.Model {
+    int_field = fields.Integer({ string: "int_field" });
+    another_int_field = fields.Integer({ string: "another_int_field" });
+    _records = [
+        { id: 1, int_field: 10, another_int_field: 45 },
+        { id: 2, int_field: 4, another_int_field: 10 },
+    ];
+}
+defineModels([Partner]);
+
+test("GaugeField in kanban view", async () => {
+    await mountView({
+        type: "kanban",
+        resModel: "partner",
+        arch: /* xml */ `
+        <kanban>
+            <field name="another_int_field"/>
+            <templates>
+                <t t-name="kanban-box">
+                    <div>
+                        <field name="int_field" widget="gauge" options="{'max_field': 'another_int_field'}"/>
+                    </div>
+                </t>
+            </templates>
+        </kanban>`,
+    });
+
+    expect(".o_kanban_record:not(.o_kanban_ghost)").toHaveCount(2);
+    expect(".o_field_widget[name=int_field] .oe_gauge canvas").toHaveCount(2);
+    expect(queryAllTexts(".o_gauge_value")).toEqual(["10", "4"]);
+});
