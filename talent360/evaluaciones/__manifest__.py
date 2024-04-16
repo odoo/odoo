a3dd159621d7d8bbb5af8c51c760a7d76a12f84f
Diego <sandoval.vargas.diego.1c@gmail.com>
2024-04-15 18:19:48 -0600

    chore: Se crea vista para evaluaciones 360

--- talent360/evaluaciones/__manifest__.py
+++ talent360/evaluaciones/__manifest__.py
@@ -1,9 +1,10 @@
 {
-    "name": "Evaluaciones",
+    "name": "Talent360 - Evaluaciones",
     "application": True,
     "data": [
         "security/ir.model.access.csv",
         "views/evaluaciones_views.xml",
+        "views/crear_evaluaciones_360.xml",
         "views/evaluaciones_menus.xml",
     ],
     "depends": ["base", "mail"],
