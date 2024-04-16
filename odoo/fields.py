0f019487f0d4a0d886e530227664e72409b15a72
Miquel Raïch <miquel.raich@forgeflow.com>
2024-04-16 19:47:38 +0200

    [IMP] fields: check 'check_company' attr is set but 'company_id' field is not in model

--- odoo/fields.py
+++ odoo/fields.py
@@ -2950,6 +2950,11 @@ class _Relational(Field):
             _logger.warning("Field %s with unknown comodel_name %r", self, self.comodel_name)
             self.comodel_name = '_unknown'
 
+    def setup(self, model):
+        super().setup(model)
+        if self.check_company and 'company_id' not in model._fields and model._name != 'res.company' and self.comodel_name != 'res.users' and not self.company_dependent:
+            _logger.warning("%s: check_company attribute will be ignored because the model doesn't have company_id field", self)
+
     def get_domain_list(self, model):
         """ Return a list domain from the domain parameter. """
         domain = self.domain
