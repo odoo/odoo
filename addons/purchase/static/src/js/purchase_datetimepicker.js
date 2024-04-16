ac603052e74d74d9eb543db198fcd5437a5e8a3e
Romain Derie <rde@odoo.com>
2024-04-16 14:42:35 +0200

    [FIX] web, *: make "input group date" calendar icon open the date picker
    *: website, survey
    
    Since commit [1] the calendar icon in input group date is not clickable
    anymore and does not open the date picker anymore.
    
    This commit simply restore that by auto discovering the calendar icon,
    should the input group follow this common pattern:
    ```xml
    <div class="input-group date">
        <input
            type="text"
            class="form-control datetimepicker-input"
        />
        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
    </div>
    ```
    
    This seems enough to cover all cases detected in the website apps:
    - The two datepicker in /my/opportunity when editing the lead to adapt
      the "Expected Closing" and "Next Activity Date" fields
    - The website form date(time) input
    - The ecommerce renting product rental period inputs ("from" and "to"
      dates)
    - The `o_website_sale_daterange_picker` snippet
    - The survey date fields
    - ..maybe other places..
    
    Should there be cases needing this feature for calendar icons which are
    not following the suggested input group layout, they can do it this way:
    ```diff
    -        this.call("datetime_picker", "create", {
    +        const picker = this.call("datetime_picker", "create", {
                // ...
            }).enable();
    +        this.el.parentNode.querySelector(".input-group-text").addEventListener("click", picker.openPicker(0));
    ```
    
    [1]: https://github.com/odoo/odoo/commit/910897fc97d87b08f01627094ec8c159f5267628
    
    closes odoo/odoo#165409
    
    X-original-commit: 5c2db8e7fd921d6d32c55b99cae1a9a5fafdb117
    Related: odoo/enterprise#62635
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/purchase/static/src/js/purchase_datetimepicker.js
+++ addons/purchase/static/src/js/purchase_datetimepicker.js
@@ -5,8 +5,13 @@ import { rpc } from "@web/core/network/rpc";
 
 export const PurchaseDatePicker = PublicWidget.Widget.extend({
     selector: ".o-purchase-datetimepicker",
+    disabledInEditableMode: true,
+
+    /**
+     * @override
+     */
     start() {
-        this.call("datetime_picker", "create", {
+        this.disableDateTimePicker = this.call("datetime_picker", "create", {
             target: this.el,
             onChange: (newDate) => {
                 const { accessToken, orderId, lineId } = this.el.dataset;
@@ -19,6 +24,13 @@ export const PurchaseDatePicker = PublicWidget.Widget.extend({
                 value: luxon.DateTime.fromISO(this.el.dataset.value),
             },
         }).enable();
+    },
+    /**
+     * @override
+     */
+    destroy() {
+        this.disableDateTimePicker();
+        return this._super(...arguments);
     },
 });
 
