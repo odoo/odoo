6150f3ce9cfb4189f046c304e5bdfc2164944355
Benjamin Vray <bvr@odoo.com>
2024-04-16 14:03:22 +0000

    [FIX] web_editor: fix drag and drop in mobile preview
    Steps to reproduce the bug:
    
    - In "Website" edit mode.
    - Click on the "Mobile preview" button in the right panel.
    - Drop the "Text-Image" and "Image-Text" snippets.
    - Drop the "Alert" snippet (so an inner content).
    - Drag the "Alert" snippet and hover the dropzones.
    - Bug: There is no "preview" of the dragged snippet, like in desktop
    view.
    
    In this commit [1], the "drag and drop" feature of snippets was adapted
    following the relocation of the website preview into an iframe (see
    commit [2]). Later, another commit [3] moved the overlay of the snippets
    into the iframe.
    
    It is this latter commit that introduced the bug. Indeed, since the
    overlay is within the iframe, the jQuery Draggable "Helper" is also
    within the iframe when a snippet is dragged from the page (as opposed to
    from the snippets menu). Therefore, it is no longer necessary to
    consider the offset of the iframe when computing the dropzones (as done
    in [1]).
    
    [1]: 457e655
    [2]: 59b96b0
    [3]: 872bb20
    
    task-3854032
    
    X-original-commit: c3a26b23106b1b61090ce87ef096dee39befcd52

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -1180,6 +1180,7 @@ var SnippetEditor = Widget.extend({
             canBeSanitizedUnless: canBeSanitizedUnless,
             toInsertInline: toInsertInline,
             selectorGrids: selectorGrids,
+            fromIframe: true,
         });
 
         this.$body.addClass('move-important');
@@ -2348,7 +2349,7 @@ var SnippetsMenu = Widget.extend({
      *        elements which are in grid mode and for which a grid dropzone
      *        needs to be inserted
      */
-    _activateInsertionZones($selectorSiblings, $selectorChildren, canBeSanitizedUnless, toInsertInline, selectorGrids = []) {
+    _activateInsertionZones($selectorSiblings, $selectorChildren, canBeSanitizedUnless, toInsertInline, selectorGrids = [], fromIframe = false) {
         var self = this;
 
         // If a modal or a dropdown is open, the drop zones must be created
@@ -2498,7 +2499,7 @@ var SnippetsMenu = Widget.extend({
 
         let iframeOffset;
         const bodyWindow = this.$body[0].ownerDocument.defaultView;
-        if (bodyWindow.frameElement && bodyWindow !== this.ownerDocument.defaultView) {
+        if (bodyWindow.frameElement && bodyWindow !== this.ownerDocument.defaultView && !fromIframe) {
             iframeOffset = bodyWindow.frameElement.getBoundingClientRect();
         }
 
@@ -3840,7 +3841,7 @@ var SnippetsMenu = Widget.extend({
      * @param {OdooEvent} ev
      */
     _onActivateInsertionZones: function (ev) {
-        this._activateInsertionZones(ev.data.$selectorSiblings, ev.data.$selectorChildren, ev.data.canBeSanitizedUnless, ev.data.toInsertInline, ev.data.selectorGrids);
+        this._activateInsertionZones(ev.data.$selectorSiblings, ev.data.$selectorChildren, ev.data.canBeSanitizedUnless, ev.data.toInsertInline, ev.data.selectorGrids, ev.data.fromIframe);
     },
     /**
      * Called when a child editor asks to deactivate the current snippet
