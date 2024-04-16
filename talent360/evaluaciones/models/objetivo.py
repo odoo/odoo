4fac81753d22fbfe68772e9e650de3d3de1dcc66
DavidLangarica <a01708936@tec.mx>
2024-04-15 19:17:41 -0600

    feat(objetivos): agrega vista de formulario, validacion de fecha, widget de estado

--- talent360/evaluaciones/models/objetivo.py
+++ talent360/evaluaciones/models/objetivo.py
@@ -1,12 +1,13 @@
-from odoo import api, models, fields
-from odoo.exceptions import UserError, ValidationError
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+from datetime import date
 
 class Objetivo(models.Model):
     _name = "objetivo"
     _description = "Objetivos de desempeño"
 
     titulo = fields.Char(required=True, string="Título")
-    descripcion = fields.Char(required=True, string="Descripción")
+    descripcion = fields.Text(required=True, string="Descripción", help="Descripción del objetivo")
     metrica = fields.Selection(
         [
             ("porcentaje", "Porcentaje"),
@@ -15,6 +16,7 @@ class Objetivo(models.Model):
         default="porcentaje",
         required=True,
         string="Métrica",
+        help="¿Cómo se medirá el objetivo? Ej. Porcentaje o Monto",
     )
 
     tipo = fields.Selection(
@@ -24,6 +26,7 @@ class Objetivo(models.Model):
         ],
         default="puesto",
         required=True,
+        help="Tipo de objetivo",
     )
 
     orden = fields.Selection(
@@ -33,12 +36,25 @@ class Objetivo(models.Model):
         ],
         default="ascendente",
         required=True,
+        help="Si el objetivo es para lograr una meta, es ascendente, si es para reducir algo, es descendente",
     )
 
-    peso = fields.Integer(required=True)
-    piso_minimo = fields.Integer(required=True, string="Piso Mínimo")
-    piso_maximo = fields.Integer(required=True, string="Piso Máximo")
-    resultado = fields.Integer()
+    peso = fields.Integer(required=True, help="Peso del objetivo en la evaluación")
+    piso_minimo = fields.Integer(required=True, string="Piso Mínimo", help="¿Cuál es el mínimo aceptable?")
+    piso_maximo = fields.Integer(required=True, string="Piso Máximo", help="¿Cuál es el máximo aceptable?")
+    fecha_fin = fields.Date(required=True, string="Fecha final", default=fields.Datetime.today() ,help="Fecha en la que se debe cumplir el objetivo")
+    resultado = fields.Integer(store=True)
+    estado = fields.Selection(
+        [
+            ("rojo", "No cumple con las expectativas"),
+            ("amarillo", "Medianamente cumple con las expectativas"),
+            ("verde", "Cumple con las expectativas"),
+            ("azul", "Supera las expectativas")
+        ],
+        default="rojo",
+        compute="_compute_estado",
+        store=True,
+    )
 
     usuario_ids = fields.Many2many(
         "res.users",
@@ -47,6 +63,8 @@ class Objetivo(models.Model):
         "usuario_id",
         string="Asignados",
     )
+    
+    evaluador = fields.Char()
 
     @api.constrains("piso_minimo", "piso_maximo")
     def _check_pisos(self):
@@ -76,3 +94,24 @@ class Objetivo(models.Model):
             if new_peso > 100 or new_peso <= 0:
                 raise ValidationError("El peso debe estar en el rango de 1 y 100")
         return super(Objetivo, self).write(vals)
+    
+    @api.constrains("fecha_fin")
+    def _check_fecha_fin(self):
+        for record in self:
+            if record.fecha_fin and record.fecha_fin < date.today():
+                raise ValidationError("La fecha final debe ser mayor a la fecha de hoy")
+            
+    @api.depends("resultado", "piso_maximo")
+    def _compute_estado(self):
+        for record in self:
+            if record.piso_maximo and record.resultado:
+                ratio = record.resultado / record.piso_maximo
+                if 0 <= ratio <= 0.6:
+                    record.estado = "rojo"
+                elif 0.61 <= ratio <= 0.85:
+                    record.estado = "amarillo"
+                elif 0.851 <= ratio <= 1:
+                    record.estado = "verde"
+                elif ratio > 1:
+                    record.estado = "azul"
+            
