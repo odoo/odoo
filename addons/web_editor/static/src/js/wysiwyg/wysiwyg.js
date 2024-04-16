40a7617140a8dcfeafdce348eecfd7fa9475b441
ruja-odoo <ruja@odoo.com>
2024-04-16 06:30:55 +0000

    [FIX] web_editor: svg image getting disappeared after cropping
    Before this commit:
    
    SVG images disappeared after cropping and clicking elsewhere or saving.
    
    After this commit:
    
    Now the images don't disappear after cropping.
    
    task-3809854
    
    closes odoo/odoo#164192
    
    X-original-commit: 03b4ce77a7e4028f2245b9d87b376398930558aa
    Signed-off-by: Antoine Guenet (age) <age@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -3494,7 +3494,7 @@ export class Wysiwyg extends Component {
                 res_id: parseInt(resId),
                 data: (isBackground ? el.dataset.bgSrc : el.getAttribute('src')).split(',')[1],
                 alt_data: altData,
-                mimetype: el.dataset.mimetype,
+                mimetype: (isBackground ? el.dataset.mimetype : el.getAttribute('src').split(":")[1].split(";")[0]),
                 name: (el.dataset.fileName ? el.dataset.fileName : null),
             },
         );
