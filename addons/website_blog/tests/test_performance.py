72902c051f5537b98ad6c12c06f8d74588406ee2
Guillaume-gdi <gdi@odoo.com>
2024-04-16 11:31:53 +0200

    [FIX] website_blog: remove problematic test without cache
    Since [this commit], the `test_10_perf_sql_blog_standard_data` test
    failed randomly. As testing sql perf for website without cache doesn't
    really make sense (as explained in [this other commit]) we can can just
    remove the test without cache.
    
    [this commit]: https://github.com/odoo/odoo/commit/88b016fdc407e318c43c96df9b582853512f04fa
    [this other commit]: https://github.com/odoo/odoo/commit/9fd3297ae42593cad72634fdda8532f2727b8608
    
    runbot-55755
    
    closes odoo/odoo#162165
    
    X-original-commit: 1d16d3a0951be5eb71e7f9ed135f6d792533f6a3
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>
    Signed-off-by: Guillaume Dieleman (gdi) <gdi@odoo.com>

--- addons/website_blog/tests/test_performance.py
+++ addons/website_blog/tests/test_performance.py
@@ -46,7 +46,6 @@ class TestBlogPerformance(UtilPerf):
 
     def test_10_perf_sql_blog_standard_data(self):
         self.assertLessEqual(self._get_url_hot_query('/blog'), 11)
-        self.assertLessEqual(self._get_url_hot_query('/blog', cache=False), 23)
 
     def test_20_perf_sql_blog_bigger_data_scaling(self):
         BlogPost = self.env['blog.post']
