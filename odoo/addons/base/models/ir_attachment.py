071c3d029d1d23fb1084921110df19cbf491afe1
Daniel Reis <dreis@opensourceintegrators.com>
2024-04-16 13:57:08 +0100

    [FIX] base: no traceback when Attachment file not found
    When an attachment file is not found, an error is logged along with a
    Python traceback.
    
    This is inconvenient when working with a copy of a database where only
    the PosgreSQL dump was restored, and the data files are missing.
    
    With this change, the file not found exception is specifically handled
    to print a warning to the log, without opriting a full stack trace.

--- odoo/addons/base/models/ir_attachment.py
+++ odoo/addons/base/models/ir_attachment.py
@@ -120,6 +120,8 @@ class IrAttachment(models.Model):
         try:
             with open(full_path, 'rb') as f:
                 return f.read()
+        except (FileNotFoundError):
+            _logger.warning("File not found %s", full_path)
         except (IOError, OSError):
             _logger.info("_read_file reading %s", full_path, exc_info=True)
         return b''
