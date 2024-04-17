5229996133c031e7bfd6eb4cee9e1371ada50e74
yosa-odoo <yosa@odoo.com>
2024-04-17 11:40:37 +0200

    [FIX] hr_expense: display expenses amounts to be paid
    Steps to reproduce:
    - create-aprove-post an expense
    - Go to the accounting dashboard
    
    Issue:
    expenses' amount is 0
    
    Cause:
    In `_count_results_and_sum_amounts`, since the expense.currency is the same as the company we don't get the correct result:
    https://github.com/odoo/odoo/blob/d29a622740f6c34d25c52add5367bfdf58bbaf49/addons/account/models/account_journal_dashboard.py#L641-L644
    
    Solution:
    Get the right columns.
    We also change the domain to make sure that expenses partially paid are also displayed.
    
    Note:
    For the test we check that even partially paid expenses are displayed. In Master we want the residual amount to be displayed.
    
    In master:
    Use the amount_residual (discussed with po Laura)
    
    opw-3849036
    
    closes odoo/odoo#164216
    
    X-original-commit: e0d3a6c845c73c26e527d04b946f32a1e085208e
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- addons/hr_expense/tests/__init__.py
+++ addons/hr_expense/tests/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import common
+from . import test_account_journal_dashboard
 from . import test_expenses
 from . import test_expenses_access_rights
 from . import test_expenses_mail_import
