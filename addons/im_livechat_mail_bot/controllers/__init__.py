7976f304bb416f591df13721afbd116644bd8a3f
Maryam Kia <maki@odoo.com>
2024-04-17 07:33:15 +0000

    [FIX] mail, im_livechat_mail_bot: odooBot onboarding crashes on canned response
    From V.14 the `canned_response_ids` is not being passed to `mail_message_post`.
    So checking its availability in odooBot onboarding, crashes the process on the
     canned response step.
    This commit makes sure that we send it with message post data.
    
    Related to odoo/odoo#157997
    
    X-original-commit: eeddf89ff912251540dd658fcb0257c525b44e0b

--- addons/im_livechat_mail_bot/controllers/__init__.py
+++ addons/im_livechat_mail_bot/controllers/__init__.py
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import thread
+||||||| parent of 4daa39b30320 (temp)
+=======
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import discuss
+>>>>>>> 4daa39b30320 (temp)
--- addons/im_livechat_mail_bot/models/mail_bot.py
+++ addons/im_livechat_mail_bot/models/mail_bot.py
@@ -13,8 +13,16 @@ class MailBot(models.AbstractModel):
             if odoobot_state == "onboarding_attachement" and values.get("attachment_ids"):
                 self.env.user.odoobot_failed = False
                 self.env.user.odoobot_state = "onboarding_canned"
+<<<<<<< HEAD
                 return Markup(_("That's me! 🎉<br/>Try typing %s to use canned responses.", "<span class=\"o_odoobot_command\">:</span>"))
             elif odoobot_state == "onboarding_canned" and self.env.context.get("canned_response_ids"):
+||||||| parent of 4daa39b30320 (temp)
+                return _("That's me! 🎉<br/>Try typing <span class=\"o_odoobot_command\">:</span> to use canned responses.")
+            elif odoobot_state == "onboarding_canned" and values.get("canned_response_ids"):
+=======
+                return _("That's me! 🎉<br/>Try typing <span class=\"o_odoobot_command\">:</span> to use canned responses.")
+            elif odoobot_state == "onboarding_canned" and self.env.context.get("canned_response_ids"):
+>>>>>>> 4daa39b30320 (temp)
                 self.env.user.odoobot_failed = False
                 self.env.user.odoobot_state = "idle"
                 return Markup(_("Good, you can customize canned responses in the live chat application.<br/><br/><b>It's the end of this overview</b>, enjoy discovering Odoo!"))
