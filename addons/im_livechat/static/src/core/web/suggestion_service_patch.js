ab76bfcb1889037828b7ccb05efa804f9b7be9b9
Maryam Kia <maki@odoo.com>
2024-04-17 13:01:23 +0000

    [FIX] mail, *: odooBot onboarding crashes on canned response
    *: im_livechat, im_livechat_mail_bot
    
    Since odoo/odoo#110188 `canned_response_ids` is not being passed to
    `mail_message_post` and since odoo/odoo#127382 canned response is enabled in
    only non-channel threads so odooBot onboarding crashes on the canned response
    step.
    This commit allows using `canned responses` in `chat with odooBot` and makes
    sure that we send them with the message post data.
    
    Related to odoo/odoo#157997
    
    X-original-commit: 9af66f9ef02c7412aac5c31eb9ca631b8cf57332

--- addons/im_livechat/static/src/core/web/suggestion_service_patch.js
+++ addons/im_livechat/static/src/core/web/suggestion_service_patch.js
@@ -7,7 +7,7 @@ patch(SuggestionService.prototype, {
     getSupportedDelimiters(thread) {
         const res = super.getSupportedDelimiters(...arguments);
         return thread.channel_type === "livechat"
-            ? [...res, [":"]].filter((delimiter) => delimiter.at(0) !== "#")
+            ? res.filter((delimiter) => delimiter.at(0) !== "#")
             : res;
     },
 });
