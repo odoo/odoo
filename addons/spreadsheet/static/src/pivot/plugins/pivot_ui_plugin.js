aeadd065e869a5cd6b971d6d458ba9d8e1edcc1c
Lucas Lefèvre (lul) <lul@odoo.com>
2024-04-18 07:33:41 +0000

    [FIX] documents_spreadsheet: fix See record menu if not loaded
    Steps to reproduce:
    
    - wrap a pivot function inside a IFERROR e.g. =IFERROR(PIVOT("1", "probability"), 42)
    - reload the spreadsheet
    - before the pivot is loaded (throttle the network in the dev tools): right click the cell
    - click on "See records" menu item
    => boom
    
    closes odoo/odoo#162750
    
    Task: 3847477
    X-original-commit: e36dbd680c438a6dd4741c21f4b0b6b63741fc16
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet/static/src/pivot/plugins/pivot_ui_plugin.js
+++ addons/spreadsheet/static/src/pivot/plugins/pivot_ui_plugin.js
@@ -197,7 +197,7 @@ export default class PivotUIPlugin extends spreadsheet.UIPlugin {
         const cell = this.getters.getCell(position);
         if (cell && cell.isFormula) {
             const pivotFunction = getFirstPivotFunction(cell.content);
-            if (pivotFunction) {
+            if (pivotFunction && pivotFunction.args[0]) {
                 const content = astToFormula(pivotFunction.args[0]);
                 return this.getters.evaluateFormula(content).toString();
             }
