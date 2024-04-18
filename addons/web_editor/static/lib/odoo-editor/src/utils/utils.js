0f36511197eacca78141195935ab5324cdda6539
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-04-18 10:13:48 +0200

    [FIX] web_editor: fix getTraversedNodes selection next to br element
    Issue:
    ======
    Applying bold after copy paste doesn't cover all the selection.
    
    Steps to reproduce the issue:
    =============================
    - Go to notes
    - Add 3 lines
    - Press `CTRL+A` to select all the lines.
    - `CTRL+X` then `CTRL+V` of all the lines
    - Press `CTRL+A` to select all the lines again.
    - Press `CTRL+B` to make all the lines bold
    - The last line doesn't get formatted.
    
    Origin of the issue:
    ====================
    The issue is originating from the fact that paste add `br` in the last `p`
    element so actually the issue can be reproduced by just adding couple of
    lines and at the end of the last line press `SHIFT+enter` (to add br
    element). In the function `getTraversedNodes` we stop as soon as we
    reach the `endContainer` which is the last `p` elemnt because we can't
    have the `br` as endContainer so we don't include its desendants.
    In the case when we don't do copy paste (without the extra br) the
    `endContainer` is the node element so it will be included and it works
    correctly.
    
    Solution:
    ========
    - `<p>ab<br>[</p>...]`, `<p>ab<br>[<br>cd</p>..`: startContainer is p
      element, we need to go to the node at the startOffset.
    - `<p>ab[<br></p>...`: start container is ab we need to skip it and
      start from the br element
    - `[...<p>ab<br>]cd</p>`: endContainer is cd we shouldn't include it
    - `[...<p>ab><br>]</p>`, `[...<p>ab<br>]<br>cd</p>`: encContainer is p
      element, we loop over descendants and include only the ones before
      endOffset.
    
    task-3874926

--- addons/web_editor/static/lib/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/lib/odoo-editor/src/utils/utils.js
@@ -685,10 +685,64 @@ export function getTraversedNodes(editable, range = getDeepRange(editable)) {
     do {
         node = iterator.nextNode();
     } while (node && node !== range.startContainer);
+    if (
+        node &&
+        node.nodeType === Node.ELEMENT_NODE &&
+        node.childNodes.length &&
+        range.startOffset &&
+        node.childNodes[range.startOffset - 1].nodeName === "BR"
+    ) {
+        // Handle the cases:
+        // <p>ab<br>[</p><p>cd</p>] => [p2, cd]
+        // <p>ab<br>[<br>cd</p><p>ef</p>] => [br2, cd, p2, ef]
+        const targetBr = node.childNodes[range.startOffset - 1];
+        while (node != targetBr) {
+            node = iterator.nextNode();
+        }
+        node = iterator.nextNode();
+    }
+    if (
+        node &&
+        !range.collapsed &&
+        node === range.startContainer &&
+        range.startOffset === nodeSize(node) &&
+        node.nextSibling &&
+        node.nextSibling.nodeName === "BR"
+    ) {
+        // Handle the case: <p>ab[<br>cd</p><p>ef</p>] => [br, cd, p2, ef]
+        node = iterator.nextNode();
+    }
     const traversedNodes = new Set([node, ...descendants(node)]);
     while (node && node !== range.endContainer) {
         node = iterator.nextNode();
-        node && traversedNodes.add(node);
+        if (node) {
+            if (
+                !(
+                    // Handle the case: [<p>ab</p><p>cd<br>]ef</p> => [ab, p2, cd, br]
+                    node === range.endContainer &&
+                    range.endOffset === 0 &&
+                    !range.collapsed &&
+                    node.previousSibling &&
+                    node.previousSibling.nodeName === "BR"
+                )
+            ) {
+                traversedNodes.add(node);
+            }
+        }
+    }
+    if (node) {
+        // Handle the cases:
+        // [<p>ab</p><p>cd<br>]</p> => [ab, p2, cd, br]
+        // [<p>ab</p><p>cd<br>]<br>ef</p> => [ab, p2, cd, br1]
+        for (const descendant of descendants(node)) {
+            if (
+                descendant.parentElement === node &&
+                childNodeIndex(descendant) >= range.endOffset
+            ) {
+                break;
+            }
+            traversedNodes.add(descendant);
+        }
     }
     return [...traversedNodes];
 }
