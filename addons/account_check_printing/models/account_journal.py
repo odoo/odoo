18f9e8c23c1f4679fd7317596d5b6a5ae5f99a8d
Seif (segn) <segn@odoo.com>
2024-04-18 15:45:07 +0200

    [IMP] account_check_printing: Specify check layout in bank journal settings
    Allow users to override default check printing layout defined in Accounting settings by specifying check layout in bank journal settings, enabling them to specify check layout for each bank as each bank usually imposes a check layout.
    
    task id: 3869205
    
    closes odoo/odoo#162453
    
    Signed-off-by: Quentin De Paoli <qdp@odoo.com>

--- addons/account_check_printing/models/account_journal.py
+++ addons/account_check_printing/models/account_journal.py
@@ -34,6 +34,16 @@ class AccountJournal(models.Model):
         help="Sequence number of the next printed check.",
     )
 
+    bank_check_printing_layout = fields.Selection(
+        selection='_get_check_printing_layouts',
+        string="Check Layout",
+    )
+
+    def _get_check_printing_layouts(self):
+        """ Returns available check printing layouts for the company, excluding disabled options """
+        selection = self.company_id._fields['account_check_printing_layout'].selection
+        return [(value, label) for value, label in selection if value != 'disabled']
+
     @api.depends('check_manual_sequencing')
     def _compute_check_next_number(self):
         for journal in self:
