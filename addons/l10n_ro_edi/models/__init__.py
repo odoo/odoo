9c603610f7943c679591ad535ba574e2dfca915a
Yosua Nicolaus <yoni@odoo.com>
2024-04-19 19:29:26 +0200

    [IMP] l10n_ro_edi: send E-Factura to ANAF platform
    Romania requires its companies to send all invoices to a specific web
    service on their E-Factura platform. Now that we can generate the file
    (CIUS-RO XML), our next obvious step is to send them to their SPV (the
    ANAF platform).
    
    This commit implements 2 big part:
    
    - the full OAUTH/authentication process and its token generation
    - sending/fetching/downloading data to and from the Romanian SPV
    
    ---
    
    The Authentication
    
    To be able to generate the token needs a special USB key from Romania
    connected with an ANAF account. The full process on what needs to be
    done is explained in the settings view, in the "Romanian E-Factura"
    block.
    
    Once a token is generated, we'll be able to use it in all of our request
    to send/fetch/download data to/from ANAF.
    
    ---
    
    The Full Sending E-Factura Flow (requires access token)
    
    - Upload invoice/credit note to SPV -> get "loading ID"
    - The invoice gets processed by ANAF (State: "invoice_sending")
    - After some time, fetch the invoice using the acquired loading ID.
    - If processed, there will be 2 kind of answer: "OK" or "NOK" (not ok).
      Either way, we will get a "download ID", which we can then use to
      download the answer.
      - If "NOK" -> "invoice_sending_failed", read file & display the error message
      - If "OK" -> "invoice_sent", Save the signature file attachment
    
    Task-id: 3595436
    X-original-commit: bd3da0dae4010da52db1b030b6a11cfa17cea551

--- addons/l10n_ro_edi/models/__init__.py
+++ addons/l10n_ro_edi/models/__init__.py
@@ -1,2 +1,6 @@
 from . import account_edi_xml_ubl_ciusro
+from . import account_move
+from . import ciusro_document
+from . import res_company
+from . import res_config_settings
 from . import res_partner
