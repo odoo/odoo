33e4184935c7110ffbed529622837e04dbc38def
Diego <sandoval.vargas.diego.1c@gmail.com>
2024-04-18 18:36:22 -0600

    chore: se cambia el tipo de herencia de eval 360

--- talent360/evaluaciones/models/evaluacion_360.py
+++ talent360/evaluaciones/models/evaluacion_360.py
@@ -2,28 +2,17 @@ from odoo import models, fields, api
 
 
 class Evaluacion360(models.Model):
-    _name = "evaluacion_360"
+    _name = "evaluacion"
     _description = "Evaluaciones 360"
 
     _inherit = ["evaluacion"]
 
     @api.onchange('competencia_ids')
     def _onchange_competencia_ids(self):
+        if self.tipo == 'NOM_035' or self.tipo == 'CLIMA':
+            return
         if self.competencia_ids:
             competencia_preguntas = self.competencia_ids.mapped('pregunta_ids')
             self.pregunta_ids = competencia_preguntas
         else:
             self.pregunta_ids = False
-
-    pregunta_ids = fields.Many2many(
-        'pregunta', relation='evaluacion_pregunta_rel', string='Preguntas')
-    
-    competencia_ids = fields.Many2many(
-        'competencia', relation='evaluacion_competencia_rel', string='Competencias')
-    
-    usuario_ids = fields.Many2many(
-        'res.users', relation='evaluacion_usuario_rel', string='Usuarios')
-    
-    
-    
-    
