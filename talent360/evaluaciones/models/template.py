86c20f6a2b75ee557097a63aec6a84a26f993bd3
Alejandra Cabrera <alejandraa.cruiz@gmail.com>
2024-04-18 18:45:49 -0600

    docs(evaluaciones): Añadir documentación al modelo template

--- talent360/evaluaciones/models/template.py
+++ talent360/evaluaciones/models/template.py
@@ -2,6 +2,17 @@ from odoo import models, fields
 
 
 class Template(models.Model):
+    """
+    Modelo para representar una plantilla de evaluación.
+
+    Atributos:
+        _name (str): Nombre del modelo en Odoo.
+        _description (str): Descripción del modelo en Odoo.
+        nombre (str): Nombre de la plantilla de evaluación.
+        tipo (str): Tipo de la plantilla de evaluación, opciones son 'nom_035' para NOM 035 y 'clima' para Clima.
+        pregunta_ids (fields.Many2many): Relación de muchos a muchos con el modelo 'pregunta' para almacenar las preguntas asociadas a la plantilla.
+    """
+    
     _name = "template"
     _description = "Plantilla para una evaluación"
 
