a1116c6de0146a6a2e44a493aa79fe41a330799a
Audric Onockx (auon) <auon@odoo.com>
2024-04-19 10:42:52 +0200

    [MOV] web: move graph util methods in a separate non .test.js file
    The methods like `checkLabels` and `checkLegend` are very useful in
    other modules. But it is impossible to import them from a `.test.js`
    file. This PR moves them in a separate basic `.js` file.
    
    task-3879136

--- addons/web/static/tests/views/graph/graph_view.test.js
+++ addons/web/static/tests/views/graph/graph_view.test.js
@@ -1,6 +1,6 @@
-import { afterEach, expect, test } from "@odoo/hoot";
-import { queryAllTexts, queryOne } from "@odoo/hoot-dom";
-import { Deferred, animationFrame, mockDate, runAllTimers } from "@odoo/hoot-mock";
+import { expect, test } from "@odoo/hoot";
+import { queryAllTexts } from "@odoo/hoot-dom";
+import { Deferred, animationFrame } from "@odoo/hoot-mock";
 import { onRendered } from "@odoo/owl";
 
 import {
@@ -8,7 +8,6 @@ import {
     defineModels,
     editFavoriteName,
     fields,
-    findComponent,
     getService,
     makeMockServer,
     mockService,
@@ -29,186 +28,30 @@ import {
 import { DEFAULT_BG, getBorderWhite, getColors, hexToRGBA } from "@web/core/colors/colors";
 import { Domain } from "@web/core/domain";
 import { registry } from "@web/core/registry";
-import { ensureArray } from "@web/core/utils/arrays";
 import { SampleServer } from "@web/model/sample_server";
 import { GraphArchParser } from "@web/views/graph/graph_arch_parser";
-import { GraphController } from "@web/views/graph/graph_controller";
 import { GraphRenderer } from "@web/views/graph/graph_renderer";
 import { graphView } from "@web/views/graph/graph_view";
 import { WebClient } from "@web/webclient/webclient";
+import {
+    checkDatasets,
+    checkModeIs,
+    checkTooltip,
+    clickSort,
+    getChart,
+    getGraphModelMetaData,
+    getModeButton,
+    getScaleY,
+    getXAxisLabel,
+    getYAxisLabel,
+    checkLabels,
+    checkLegend,
+    clickOnDataset,
+    getGraphModel,
+    getGraphRenderer,
+    selectMode,
+} from "./graph_test_helpers";
 
-/**
- * @typedef {"bar" | "line" | "pie"} GraphMode
- *
- * @typedef {import("@web/views/view").View} GraphView
- */
-
-/**
- * @param {GraphView} view
- * @param {string | Iterable<string>} keys
- * @param {Record<string, any> | Iterable<Record<string, any>>} expectedDatasets
- */
-const checkDatasets = (view, keys, expectedDatasets) => {
-    keys = ensureArray(keys);
-
-    const datasets = getChart(view).data.datasets;
-    const actualValues = [];
-    for (const dataset of datasets) {
-        const partialDataset = {};
-        for (const key of keys) {
-            partialDataset[key] = dataset[key];
-        }
-        actualValues.push(partialDataset);
-    }
-    expect(actualValues).toEqual(ensureArray(expectedDatasets));
-};
-
-/**
- * @param {GraphView} view
- * @param {GraphMode} mode
- */
-const checkModeIs = (view, mode) => {
-    expect(getGraphModelMetaData(view).mode).toBe(mode);
-    expect(getChart(view).config.type).toBe(mode);
-    expect(getModeButton(mode)).toHaveClass("active");
-};
-
-/**
- * @param {GraphView} view
- * @param {{ lines: { label: string, value: string }[], title?: string }} expectedTooltip
- * @param {number} index
- * @param {number} datasetIndex
- */
-const checkTooltip = (view, { title, lines }, index, datasetIndex = null) => {
-    // If the tooltip options are changed, this helper should change: we construct the dataPoints
-    // similarly to Chart.js according to the values set for the tooltips options 'mode' and 'intersect'.
-    const chart = getChart(view);
-    const { datasets } = chart.data;
-    const dataPoints = [];
-    for (let i = 0; i < datasets.length; i++) {
-        const dataset = datasets[i];
-        const raw = dataset.data[index];
-        if (raw !== undefined && (datasetIndex === null || datasetIndex === i)) {
-            dataPoints.push({
-                datasetIndex: i,
-                dataIndex: index,
-                raw,
-            });
-        }
-    }
-    chart.config.options.plugins.tooltip.external({
-        tooltip: { opacity: 1, x: 1, y: 1, dataPoints },
-    });
-    const lineLabels = [];
-    const lineValues = [];
-    for (const line of lines) {
-        lineLabels.push(line.label);
-        lineValues.push(String(line.value));
-    }
-
-    expect(`.o_graph_custom_tooltip`).toHaveCount(1);
-    expect(`table thead tr th.o_measure`).toHaveText(title || "Count");
-    expect(queryAllTexts(`table tbody tr td span.o_label`)).toEqual(lineLabels);
-    expect(queryAllTexts(`table tbody tr td.o_value`)).toEqual(lineValues);
-};
-
-/**
- * @param {"asc" | "desc"} direction
- */
-const clickSort = (direction) => contains(`.fa-sort-amount-${direction}`).click();
-
-/**
- * @param {GraphView} view
- */
-const getChart = (view) => getGraphRenderer(view).chart;
-
-/**
- * @param {GraphView} view
- */
-const getGraphModelMetaData = (view) => getGraphModel(view).metaData;
-
-/**
- * @param {GraphMode} mode
- */
-const getModeButton = (mode) => queryOne`.o_graph_button[data-mode=${mode}]`;
-
-/**
- * @param {GraphView} view
- */
-const getScaleY = (view) => getChart(view).config.options.scales.y;
-
-/**
- * @param {GraphView} view
- */
-const getXAxisLabel = (view) => getChart(view).config.options.scales.x.title.text;
-
-/**
- * @param {GraphView} view
- */
-const getYAxisLabel = (view) => getChart(view).config.options.scales.y.title.text;
-
-/**
- * @param {GraphView} view
- * @param {string | Iterable<string>} expectedLabels
- */
-export function checkLabels(view, expectedLabels) {
-    expect(getChart(view).data.labels.map(String)).toEqual(ensureArray(expectedLabels));
-}
-
-/**
- * @param {GraphView} view
- * @param {string | Iterable<string>} expectedLabels
- */
-export function checkLegend(view, expectedLabels) {
-    const chart = getChart(view);
-    const labels = chart.config.options.plugins.legend.labels
-        .generateLabels(chart)
-        .map((o) => o.text);
-    const expectedLabelsList = ensureArray(expectedLabels);
-    expect(labels).toEqual(expectedLabelsList, {
-        message: `Legend should be matching: ${expectedLabelsList
-            .map((label) => `"${label}"`)
-            .join(", ")}`,
-    });
-}
-
-/**
- * @param {GraphView} view
- */
-export async function clickOnDataset(view) {
-    const chart = getChart(view);
-    const point = chart.getDatasetMeta(0).data[0].getCenterPoint();
-    return contains(chart.canvas).click({ position: point, relative: true });
-}
-
-/**
- * @param {GraphView} view
- */
-export function getGraphController(view) {
-    return findComponent(view, (c) => c instanceof GraphController);
-}
-
-/**
- * @param {GraphView} view
- */
-export function getGraphModel(view) {
-    return getGraphController(view).model;
-}
-
-/**
- * @param {GraphView} view
- * @returns {GraphRenderer}
- */
-export function getGraphRenderer(view) {
-    return findComponent(view, (c) => c instanceof GraphRenderer);
-}
-
-/**
- * @param {GraphMode} mode
- */
-export function selectMode(mode) {
-    return contains(getModeButton(mode)).click();
-}
 
 class Color extends models.Model {
     name = fields.Char();
