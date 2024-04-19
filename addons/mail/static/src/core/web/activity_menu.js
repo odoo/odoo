740b914b907565344797a740ff8000a2711b2ba7
bsra-odoo <bsra@odoo.com>
2024-04-19 06:35:37 +0000

    [FIX] mail: Fix activity default filter issue
    In "Task-3890068 OPW-3787054" the customer is facing the issue regarding the
    activity. On clicking on activity they are getting all the records instead of
    the filtered data.
    
    Solution:
    1) Added the code to enable "late" or "today" activity filter by default.
    2) Added the on-click into the XML to get the activity for the appropriate button.
    
    closes odoo/odoo#167166
    
    X-original-commit: 3d18b9faa1fdcf00d48091ad4be244ca00d33524
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/static/src/core/web/activity_menu.js
+++ addons/mail/static/src/core/web/activity_menu.js
@@ -58,7 +58,7 @@ export class ActivityMenu extends Component {
         ];
     }
 
-    openActivityGroup(group) {
+    openActivityGroup(group, filter="all") {
         document.body.click(); // hack to close dropdown
         const context = {
             // Necessary because activity_ids of mail.activity.mixin has auto_join
@@ -83,6 +83,22 @@ export class ActivityMenu extends Component {
                 });
             return;
         }
+
+        if (filter === "all") {
+            context["search_default_activities_overdue"] = 1;
+            context["search_default_activities_today"] = 1;
+        }
+        else if (filter === "overdue") {
+            context["search_default_activities_overdue"] = 1;
+        }
+        else if (filter === "today") {
+            context["search_default_activities_today"] = 1;
+        }
+        else if (filter === "upcoming_all") {
+            context["search_default_activities_upcoming_all"] = 1;
+        }
+
+
         let domain = [["activity_user_id", "=", this.userId]];
         if (group.domain) {
             domain = Domain.and([domain, group.domain]).toList();
