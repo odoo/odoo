4997bf5adb13e17093a183d5e3b805c42221fd23
dhba <dhba@odoo.com>
2024-04-19 15:56:17 +0530

    [FIX] web_editor: ensure checklist changes are saved
    Before this commit:
    
    Clicking the checkbox triggered the mousedown event, where the preventDefault
    function prevented the list from gaining focus. Consequently, without any
    focused element, clicking the breadcrumb (o_back_button) to navigate back failed
    to trigger the blur event, resulting in unsaved changes.
    
    After this commit:
    
    By removing preventDefault from the mousedown event, clicking the checkbox will
    now set focus on it. As a result, when the breadcrumb o_back_button is clicked,
    it will trigger the blur event, leading to the execution of onWysiwygBlur in
    html_field, which will commit(save) our changes.
    
    task-3346155
    
    X-original-commit: be6d71c7a375f9ba22ff295fa4868c0919384f93

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -4430,7 +4430,6 @@ export class OdooEditor extends EventTarget {
 
             if (isMouseInsideCheckboxBox) {
                 toggleClass(node, 'o_checked');
-                ev.preventDefault();
                 this.historyStep();
                 if (!document.getSelection().isCollapsed) {
                     this._updateToolbar(true);
