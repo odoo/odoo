88170b0a3577ad480aba1070aa2d09e039c7e35c
Arthur Detroux (ard) <ard@odoo.com>
2024-04-19 15:15:51 +0200

    [FIX] web_editor, website: fix adding a snippet with the powerbox
    After commit [1], the snippets' base bodies are no longer in the DOM.
    Therefore, outside components and widgets can no longer simply check the
    DOM to copy a Snippet's base body. With [1], cases where this was going
    on should have been adapted but powerbox was left over.
    
    Steps to reproduce:
    - Drop a text snippet
    - Click on the text snippet and type `/`
    - Select a snippet from the powerbox (not a banner but any "Website
      Snippet")
    => The snippet is not added, instead, undefined is in the DOM
    
    This commit fixes that by moving the adding logic on the SnippetsMenu
    
    [1]: https://github.com/odoo/odoo/commit/13449d7eb1f146cda09a9a0529fc3eb7ff66057e
    
    task-3880391
    
    closes odoo/odoo#162637
    
    Signed-off-by: Outagant Mehdi (mou) <mou@odoo.com>

---
+++ addons/website/static/tests/tours/powerbox_snippet.js
@@ -0,0 +1,43 @@
+import wTourUtils from "@website/js/tours/tour_utils";
+
+wTourUtils.registerWebsitePreviewTour("website_powerbox_snippet",{
+    edition: true,
+    test: true,
+},
+() => [
+wTourUtils.dragNDrop({
+    id: "s_text_block",
+    name: "Text",
+}),
+wTourUtils.clickOnSnippet({
+    id: "s_text_block",
+    name: "Text",
+}),
+{
+    content: "Select the last paragraph",
+    trigger: ":iframe .s_text_block p:last-child",
+},
+{
+    content: "Show the powerbox",
+    trigger: ":iframe .s_text_block p:last-child",
+    async run(actions) {
+        actions.editor(`/`);
+        const wrapwrap = this.anchor.closest("#wrapwrap");
+        wrapwrap.dispatchEvent(
+            new InputEvent("input", {
+                inputType: "insertText",
+                data: "/",
+            })
+        );
+    },
+},
+{
+    content: "Click on the alert snippet",
+    trigger: ".oe-powerbox-wrapper .oe-powerbox-commandWrapper:contains('Alert')",
+},
+{
+    content: "Check if s_alert snipept is inserted",
+    trigger: ":iframe .s_alert",
+    isCheck: true,
+}
+]);
