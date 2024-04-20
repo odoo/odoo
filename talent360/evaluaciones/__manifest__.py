f7f99206711c031ca644b606061fb4f5be7e1006
diego llaca <A01704793@tec.mx>
2024-04-20 15:34:17 -0600

    Chore: Add permisions to evaluations360

--- talent360/evaluaciones/__manifest__.py
+++ talent360/evaluaciones/__manifest__.py
@@ -2,6 +2,7 @@
     "name": "Talent360 - Evaluaciones",
     "application": True,
     "data": [
+        "security/security.xml",
         "security/ir.model.access.csv",
         "views/evaluaciones_views.xml",
         "views/crear_evaluaciones_360.xml",
