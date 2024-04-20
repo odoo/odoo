0199fffff752d44db44aed00ba1c3fa829f63f93
Juan Pablo Cabrera <jpcq2002@yahoo.com.mx>
2024-04-19 18:08:45 -0600

    feat(evaluacion): creando la base para las unit tests

---
+++ talent360/evaluaciones/tests/test_evaluacion.py
@@ -0,0 +1,52 @@
+from odoo.tests.common import TransactionCase
+
+"""
+La clase debe de tener el mismo nombre que el modelo
+
+"""
+class test_evaluacion(TransactionCase):
+    
+    
+
+    # Método para inicializar las variables de la clase
+    def setUp(self):
+        super(test_evaluacion, self).setUp()
+        
+    # Función 
+    def crear_evaluacion(self, nombre, estado='borrador'):
+        # Crear y retornar una evaluación con el nombre y estado proporcionados
+        return self.env['evaluacion'].create({
+            'nombre': nombre,
+            'estado': estado,
+        })
+        
+    # Método para finalizar las pruebas
+    def tearDowm(self):
+        super(test_evaluacion, self).tearDown()
+        return
+    
+    
+    def test_copiar_preguntas_de_template_nom035(self):
+        evaluacion = self.crear_evaluacion('Evaluación NOM-035')
+        
+        # Crear preguntas de ejemplo que podrían ser asociadas a un template
+        preguntas = self.env['pregunta'].create([
+            {'nombre': 'Pregunta 1'},
+            {'nombre': 'Pregunta 2'}
+        ])
+        # Crear un template con preguntas predefinidas
+        template = self.env['template'].create({
+            'nombre': 'Template NOM-035',
+            'pregunta_ids': [(6, 0, preguntas.ids)],
+        })
+        # Simular copia de preguntas desde el template
+        evaluacion.pregunta_ids = [(6, 0, template.pregunta_ids.ids)]
+        # Verificar que las preguntas se han copiado correctamente
+        self.assertEqual(len(evaluacion.pregunta_ids), 2)
+        
+        
+    
+    def test_action_nom035(self):
+        return
+    
+    
