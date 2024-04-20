ed8eeb75a7bcd1fd5dd4ca54ee16ba04b7af82f9
ricardo020202 <ricoroca2@hotmail.com>
2024-04-19 20:04:40 -0600

    feat(evaluaciones): add permits

--- talent360/evaluaciones/__manifest__.py
+++ talent360/evaluaciones/__manifest__.py
@@ -3,6 +3,7 @@
     "application": True,
     "data": [
         "security/ir.model.access.csv",
+        "security/security.xml",
         "views/evaluaciones_views.xml",
         "views/evaluaciones_menus.xml",
 
