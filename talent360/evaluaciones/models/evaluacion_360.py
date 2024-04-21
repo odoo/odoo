6a56679b8c7c85227f5687c7a0bfe91f159f87e7
Diego <sandoval.vargas.diego.1c@gmail.com>
2024-04-20 20:51:27 -0600

    chore: se agregan comentarios

--- talent360/evaluaciones/models/evaluacion_360.py
+++ talent360/evaluaciones/models/evaluacion_360.py
@@ -2,6 +2,10 @@ from odoo import models, fields, api
 
 
 class Evaluacion360(models.Model):
+    """
+    Modelo para representar Evaluaciones 360.
+    Hereda de 'evaluacion'.
+    """
     _name = "evaluacion"
     _description = "Evaluaciones 360"
 
@@ -9,6 +13,12 @@ class Evaluacion360(models.Model):
 
     @api.onchange('competencia_ids')
     def _onchange_competencia_ids(self):
+        """
+        Método que se ejecuta cuando cambian las competencias.
+
+        Si el tipo de evaluación no es 'NOM_035' o 'CLIMA', y hay competencias seleccionadas,
+        asigna las preguntas relacionadas con esas competencias a la evaluación.
+        """
         if self.tipo == 'NOM_035' or self.tipo == 'CLIMA':
             return
         if self.competencia_ids:
