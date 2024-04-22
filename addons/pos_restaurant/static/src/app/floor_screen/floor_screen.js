odoo/odoo

commit b42b0f8b541a13621b671b0b7e57812ef11fc1b2

[IMP] pos_restaurant: floor background picture
Before this PR:
It was not possible to use a custom image for the background of a floor
plan.

Now:
It is now possible to add an image to the floor plan in order to use it
as the background.
If a colour is selected when a custom image is used, the image will be
removed from the record.

The custom background image cannot be used in kanban view of the floor plan.

Task-3616978

closes odoo/odoo#156460

Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

addons/pos_restaurant/static/src/app/floor_screen/floor_screen.js
@@ -12,9 +12,12 @@ import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { Table } from "@pos_restaurant/app/floor_screen/table";
 import { usePos } from "@point_of_sale/app/store/pos_hook";
 import { useService } from "@web/core/utils/hooks";
-import { Component, onMounted, useRef, useState, onWillStart } from "@odoo/owl";
+import { Component, onMounted, useRef, useState, onWillStart, useEffect } from "@odoo/owl";
 import { ask } from "@point_of_sale/app/store/make_awaitable_dialog";
 import { unique } from "@web/core/utils/arrays";
+import { loadImage } from "@point_of_sale/utils";
+import { getDataURLFromFile } from "@web/core/utils/urls";
+import { hasTouch } from "@web/core/browser/feature_detection";
 
 export class FloorScreen extends Component {
     static components = { Table };
@@ -28,16 +31,73 @@ export class FloorScreen extends Component {
         const floor = this.pos.currentFloor;
         this.state = useState({
             selectedFloorId: floor ? floor.id : null,
+            floorHeight: "100%",
+            floorWidth: "100%",
             selectedTableIds: this.getTablesSelectedByDefault(),
             isColorPicker: false,
         });
         this.floorMapRef = useRef("floor-map-ref");
+        this.floorScrollBox = useRef("floor-map-scroll");
         const ui = useState(useService("ui"));
         const mode = localStorage.getItem("floorPlanStyle");
         this.pos.floorPlanStyle = ui.isSmall || mode == "kanban" ? "kanban" : "default";
         this.map = useRef("map");
-        onMounted(() => this.pos.openCashControl());
+        onMounted(() => {
+            this.pos.openCashControl();
+        });
         onWillStart(this.onWillStart);
+        useEffect(
+            () => {
+                this.computeFloorSize();
+            },
+            () => [this.activeFloor, this.pos.floorPlanStyle]
+        );
+        useEffect(
+            (tableL) => {
+                if (hasTouch()) {
+                    if (tableL) {
+                        this.floorScrollBox.el.classList.remove("overflow-scroll");
+                        this.floorScrollBox.el.classList.add("overflow-hidden");
+                    } else {
+                        this.floorScrollBox.el.classList.remove("overflow-hidden");
+                        this.floorScrollBox.el.classList.add("overflow-scroll");
+                    }
+                }
+            },
+            () => [this.state.selectedTableIds.length]
+        );
+    }
+    computeFloorSize() {
+        if (this.pos.floorPlanStyle === "kanban") {
+            this.state.floorHeight = "100%";
+            this.state.floorWidth = window.innerWidth + "px";
+            return;
+        }
+        const tables = this.activeFloor.table_ids;
+        const floorV = this.floorMapRef.el.clientHeight;
+        const floorH = this.floorMapRef.el.offsetWidth;
+        const positionH = Math.max(
+            ...tables.map((table) => table.position_h + table.width),
+            floorH
+        );
+        const positionV = Math.max(
+            ...tables.map((table) => table.position_v + table.height),
+            floorV
+        );
+
+        if (this.activeFloor.floor_background_image) {
+            const img = new Image();
+            img.onload = () => {
+                const height = Math.max(img.height, positionV);
+                const width = Math.max(img.width, positionH);
+                this.state.floorHeight = `${height}px`;
+                this.state.floorWidth = `${width}px`;
+            };
+            img.src = "data:image/png;base64," + this.activeFloor.floor_background_image;
+        } else {
+            this.state.floorHeight = `${positionV}px`;
+            this.state.floorWidth = `${positionH}px`;
+        }
     }
     getTablesSelectedByDefault() {
         return this.pos.orderToTransfer ? [this.pos.orderToTransfer.tableId] : [];
@@ -66,6 +126,11 @@ export class FloorScreen extends Component {
             };
         }
         await this.pos.unsetTable();
+    }
+    get floorBackround() {
+        return this.activeFloor.floor_background_image
+            ? "data:image/png;base64," + this.activeFloor.floor_background_image
+            : "none";
     }
     onClickFloorMap() {
         this.state.selectedTableIds = this.getTablesSelectedByDefault();
@@ -303,7 +368,9 @@ export class FloorScreen extends Component {
     }
     unselectTables() {
         if (this.selectedTables.length) {
-            this.pos.updateTables(...this.selectedTables);
+            for (const table of this.selectedTables) {
+                this.pos.data.write("restaurant.table", [table.id], table.serialize(true));
+            }
         }
         this.state.selectedTableIds = [];
     }
@@ -386,8 +453,9 @@ export class FloorScreen extends Component {
                       title: _t("Table Name ?"),
                       getPayload: (newName) => {
                           if (newName !== this.selectedTables[0].name) {
-                              this.selectedTables[0].name = newName;
-                              this.pos.updateTables(this.selectedTables[0]);
+                              this.pos.data.write("restaurant.table", [this.selectedTables[0].id], {
+                                  name: newName,
+                              });
                           }
                       },
                   }
@@ -419,8 +487,9 @@ export class FloorScreen extends Component {
                 const newSeatsNum = parseInt(num, 10);
                 selectedTables.forEach((selectedTable) => {
                     if (newSeatsNum !== selectedTable.seats) {
-                        selectedTable.seats = newSeatsNum;
-                        this.pos.updateTables(selectedTable);
+                        this.pos.data.write("restaurant.table", [selectedTable.id], {
+                            seats: newSeatsNum,
+                        });
                     }
                 });
             },
@@ -434,15 +503,13 @@ export class FloorScreen extends Component {
     }
     changeShape(form) {
         for (const table of this.selectedTables) {
-            table.shape = form;
+            this.pos.data.write("restaurant.table", [table.id], { shape: form });
         }
-        this.pos.updateTables(...this.selectedTables);
     }
     unlinkTables() {
         for (const table of this.selectedTables) {
-            table.update({ parent_id: null });
+            this.pos.data.write("restaurant.table", [table.id], { parent_id: false });
         }
-        this.pos.updateTables(...this.selectedTables);
     }
     linkTables() {
         const parentTable =
@@ -465,14 +532,14 @@ export class FloorScreen extends Component {
     }
     setColor(color) {
         if (this.selectedTables.length > 0) {
-            this.selectedTables.forEach((selectedTable) => {
-                selectedTable.color = color;
-            });
-            this.pos.updateTables(...this.selectedTables);
+            for (const table of this.selectedTables) {
+                this.pos.data.write("restaurant.table", [table.id], { color: color });
+            }
         } else {
             this.activeFloor.background_color = color;
             this.pos.data.write("restaurant.floor", [this.activeFloor.id], {
                 background_color: color,
+                floor_background_image: false,
             });
         }
         this.state.isColorPicker = false;
@@ -621,6 +688,33 @@ export class FloorScreen extends Component {
     }
     getChildren(table) {
         return this.pos.models["restaurant.table"].filter((t) => t.parent_id?.id === table.id);
+    }
+    async uploadImage(event) {
+        const file = event.target.files[0];
+        if (!file.type.match(/image.*/)) {
+            this.dialog.add(AlertDialog, {
+                title: _t("Unsupported File Format"),
+                body: _t("Only web-compatible Image formats such as .png or .jpeg are supported."),
+            });
+        } else {
+            const imageUrl = await getDataURLFromFile(file);
+            const loadedImage = await loadImage(imageUrl);
+            if (loadedImage) {
+                this.env.services.ui.block();
+                await this.pos.data.ormWrite("restaurant.floor", [this.activeFloor.id], {
+                    floor_background_image: imageUrl.split(",")[1],
+                });
+                // A read is added to be sure that we have the same image as the one in backend
+                await this.pos.data.read("restaurant.floor", [this.activeFloor.id]);
+                this.env.services.ui.unblock();
+            } else {
+                this.dialog.add(AlertDialog, {
+                    title: _t("Loading Image Error"),
+                    body: _t("Encountered error when loading image. Please try again."),
+                });
+            }
+            this.state.isColorPicker = false;
+        }
     }
 }
 

addons/pos_restaurant/static/src/app/floor_screen/floor_screen.xml
@@ -39,6 +39,16 @@
                 <t t-set="lighter" t-value="selectedTables.length === 0" />
                 <button t-foreach="Object.entries(getColors())" t-as="color" t-key="color[0]" class="p-4 border-0"
                     t-attf-style="background-color: {{lighter ? getLighterShade(color[0]) : color[1]}}" t-on-click.stop="() => this.setColor(lighter ? this.getLighterShade(color[0]) : color[1])" />
+                <div class="floor-picture position-relative text-center overflow-hidden d-flex flex-column align-items-center justify-content-center">
+                    <t t-if="activeFloor.floor_background_image">
+                        <img t-att-src="floorBackround" style="width: 48px; height: 48px; object-fit: cover;" />
+                    </t>
+                    <t t-else="">
+                        <i class="fa fa-camera" role="img" aria-label="Picture" title="Picture"></i>
+                        File
+                    </t>
+                    <input type="file" class="image-uploader" t-on-change="uploadImage" />
+                </div>
             </div>
             <div class="floor-selector d-flex text-center bg-100 fs-3 w-100">
                 <t t-foreach="pos.models['restaurant.floor'].getAll()" t-as="floor" t-key="floor.id">
@@ -51,25 +61,28 @@
                 <button class="btn btn-secondary p-3 ms-auto rounded-0" t-if="pos.isEditMode or pos.config.floor_ids?.length === 0" t-on-click="addFloor">
                     <i class="fa fa-plus me-2" role="img" aria-label="Add" title="Add" />Add Floor</button>
             </div>
-            <div t-on-click="onClickFloorMap" t-on-touchstart="_onPinchStart" t-on-touchmove="_onPinchMove" t-on-touchend="_onPinchEnd"
-                class="floor-map position-relative flex-grow-1 flex-shrink-1 flex-basis-0 w-auto h-100 overflow-auto"
-                t-ref="floor-map-ref"
-                t-attf-style="background: {{activeFloor ? activeFloor.background_color : 'none'}};">
-                <t t-if="pos.config.floor_ids?.length > 0">
-                    <div t-if="!activeTables?.length" class="empty-floor d-flex align-items-center justify-content-center h-100 fs-3 text-center text-muted" t-ref="map">
-                        <span>Oops! No tables available.<br/>Add a new table to get started.</span>
+            <div t-ref="floor-map-scroll" class="overflow-scroll flex-grow-1 flex-shrink-1 flex-basis-0 w-auto" t-attf-style="background: {{activeFloor.background_color}}"> 
+                <div t-on-click="onClickFloorMap" t-on-touchstart="_onPinchStart" t-on-touchmove="_onPinchMove" t-on-touchend="_onPinchEnd"
+                    class="floor-map position-relative w-100 h-100 overflow-hidden"
+                    t-ref="floor-map-ref"
+                    t-attf-style="height: {{state.floorHeight}} !important; width: {{state.floorWidth}} !important; {{ activeFloor.floor_background_image and pos.floorPlanStyle !== 'kanban' &amp;&amp; 'background-image: url(' + floorBackround + '); background-size: auto; background-repeat: no-repeat; background-attachment: local;' }}">
+                    <t t-if="pos.config.floor_ids?.length > 0">
+                        <div t-if="!activeTables?.length" class="empty-floor d-flex align-items-center justify-content-center h-100 fs-3 text-center text-muted" t-ref="map">
+                            <span>Oops! No tables available.<br/>Add a new table to get started.</span>
+                        </div>
+                        <div t-else="" t-ref="map" t-att-class="{'d-flex align-items-center justify-content-center flex-wrap': pos.floorPlanStyle == 'kanban'}">
+                            <Table t-foreach="activeTables.toSorted((a,b)=>a.id-b.id)" t-as="table" t-key="table_index"
+                                onClick.bind="onSelectTable" table="table"
+                                selectedTables="selectedTables" limit="floorMapRef"
+                                class="pos.floorPlanStyle == 'kanban' ? 'm-2 position-relative' : 'position-absolute'"
+                                style="pos.floorPlanStyle == 'kanban' ? 'width: 120px; height: 120px' :
+                                        `width: ${table.width}px; height: ${table.height}px; top: ${table.position_v}px; left: ${table.position_h}px;`" />
+                            <div t-attf-style="position: absolute; top: {{state.floorHeight}}; right: 0px; height: 1px; width: 1px; background-color: transparent"/>
+                        </div>
+                    </t>
+                    <div t-else="" class="empty-floor d-flex align-items-center justify-content-center h-100 fs-3 text-center text-muted" t-ref="map">
+                        <span>Oops! No floors available.<br/>Add a new floor to get started.</span>
                     </div>
-                    <div t-else="" t-ref="map" t-att-class="{'d-flex align-items-center justify-content-center flex-wrap': pos.floorPlanStyle == 'kanban'}">
-                        <Table t-foreach="activeTables.toSorted((a,b)=>a.id-b.id)" t-as="table" t-key="table_index"
-                            onClick.bind="onSelectTable" table="table"
-                            selectedTables="selectedTables" limit="floorMapRef"
-                            class="pos.floorPlanStyle == 'kanban' ? 'm-2 position-relative' : 'position-absolute'"
-                            style="pos.floorPlanStyle == 'kanban' ? 'width: 120px; height: 120px' :
-                                    `width: ${table.width}px; height: ${table.height}px; top: ${table.position_v}px; left: ${table.position_h}px;`" />
-                    </div>
-                </t>
-                <div t-else="" class="empty-floor d-flex align-items-center justify-content-center h-100 fs-3 text-center text-muted" t-ref="map">
-                    <span>Oops! No floors available.<br/>Add a new floor to get started.</span>
                 </div>
             </div>
         </div>

addons/point_of_sale/static/src/app/main.js
@@ -42,5 +42,7 @@ whenReady(() => {
     }
     if (hasTouch()) {
         classList.add("o_touch_device");
+        classList.add("o_mobile_overscroll");
+        document.documentElement.classList.add("o_mobile_overscroll");
     }
 })();

addons/point_of_sale/static/src/app/models/data_service.js
@@ -197,7 +197,7 @@ export class PosData extends Reactive {
 
             const dataToUpdate = {};
             const keysToUpdate = Object.keys(vals);
-            const serializedRecords = record.serialize();
+            const serializedRecords = record.serialize(true);
 
             for (const key of keysToUpdate) {
                 dataToUpdate[key] = serializedRecords[key];

addons/point_of_sale/static/src/app/models/related_models.js
@@ -583,11 +583,11 @@ export function createRelatedModels(modelDefs, modelClasses = {}, indexes = {}) 
                 for (const name in fields) {
                     const field = fields[name];
                     if (field.type === "many2one") {
-                        result[name] = record[name] ? record[name].id : undefined;
+                        result[name] = record[name] ? record[name].id : null;
                     } else if (X2MANY_TYPES.has(field.type)) {
                         result[name] = [...record[name]].map((record) => record.id);
                     } else {
-                        result[name] = record[name];
+                        result[name] = record[name] || null;
                     }
                 }
                 return result;

addons/point_of_sale/static/src/app/screens/partner_list/partner_editor/partner_editor.js
@@ -130,17 +130,15 @@ export class PartnerEditor extends Component {
             });
         } else {
             const imageUrl = await getDataURLFromFile(file);
-            const loadedImage = await loadImage(imageUrl, {
-                onError: () => {
-                    this.dialog.add(AlertDialog, {
-                        title: _t("Loading Image Error"),
-                        body: _t("Encountered error when loading image. Please try again."),
-                    });
-                },
-            });
+            const loadedImage = await loadImage(imageUrl);
             if (loadedImage) {
                 const resizedImage = await this._resizeImage(loadedImage, 800, 600);
                 this.changes.image_1920 = resizedImage.toDataURL();
+            } else {
+                this.dialog.add(AlertDialog, {
+                    title: _t("Loading Image Error"),
+                    body: _t("Encountered error when loading image. Please try again."),
+                });
             }
         }
     }

addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
@@ -95,26 +95,20 @@ export class ProductScreen extends Component {
             ? [...selectedCategory.child_id]
             : this.pos.models["pos.category"].filter((category) => !category.parent_id);
     }
-    getChildCategoriesInfo(selectedCategory) {
-        return this.getCategoriesInfo(this.getChildCategories(selectedCategory), selectedCategory);
-    }
     getCategoriesAndSub() {
         return this.getAncestorsAndCurrent().flatMap((category) =>
             this.getChildCategoriesInfo(category)
         );
     }
 
-    getCategoriesInfo(categories, selectedCategory) {
-        return categories.map((category) => ({
+    getChildCategoriesInfo(selectedCategory) {
+        return this.getChildCategories(selectedCategory).map((category) => ({
             ...pick(category, "id", "name", "color"),
             imgSrc:
                 this.pos.config.show_category_images && category.has_image
                     ? `/web/image?model=pos.category&field=image_128&id=${category.id}`
                     : undefined,
-            isSelected:
-                selectedCategory &&
-                this.getAncestorsAndCurrent().includes(category) &&
-                !this.ui.isSmall,
+            isSelected: this.getAncestorsAndCurrent().includes(category) && !this.ui.isSmall,
             isChildren: this.getChildCategories(this.pos.selectedCategory).includes(category),
         }));
     }

addons/point_of_sale/static/src/scss/pos.scss
@@ -70,6 +70,9 @@ button {
     /*rtl:ignore*/
     direction: ltr;
 }
+.o_mobile_overscroll {
+    overscroll-behavior: none;
+}
 
 .pos .button.validation.load-order-button {
     height: calc(var(--btn-height-size) * 2);

commit 81a6421d7db9bb9ee1bd0d2a04274b66431cc7ff

[FIX] pos_restaurant: allow opening of PoS session with no floors
Prior to this commit, opening a PoS session was not possible when all
floors were removed. This issue was introduced in commit b42b0f8b541a13621b671b0b7e57812ef11fc1b2.

opw-3870794

addons/pos_restaurant/static/src/app/floor_screen/floor_screen.js
@@ -73,19 +73,18 @@ export class FloorScreen extends Component {
             this.state.floorWidth = window.innerWidth + "px";
             return;
         }
-        const tables = this.activeFloor.table_ids;
+        const tables = this.activeTables;
         const floorV = this.floorMapRef.el.clientHeight;
         const floorH = this.floorMapRef.el.offsetWidth;
-        const positionH = Math.max(
-            ...tables.map((table) => table.position_h + table.width),
-            floorH
-        );
-        const positionV = Math.max(
-            ...tables.map((table) => table.position_v + table.height),
-            floorV
-        );
+        const positionH = tables
+            ? Math.max(...tables.map((table) => table.position_h + table.width), floorH)
+            : floorH;
 
-        if (this.activeFloor.floor_background_image) {
+        const positionV = tables
+            ? Math.max(...tables.map((table) => table.position_v + table.height), floorV)
+            : floorV;
+
+        if (this.activeFloor?.floor_background_image) {
             const img = new Image();
             img.onload = () => {
                 const height = Math.max(img.height, positionV);
@@ -128,7 +127,7 @@ export class FloorScreen extends Component {
         await this.pos.unsetTable();
     }
     get floorBackround() {
-        return this.activeFloor.floor_background_image
+        return this.activeFloor?.floor_background_image
             ? "data:image/png;base64," + this.activeFloor.floor_background_image
             : "none";
     }

addons/pos_restaurant/static/src/app/floor_screen/floor_screen.xml
@@ -40,7 +40,7 @@
                 <button t-foreach="Object.entries(getColors())" t-as="color" t-key="color[0]" class="p-4 border-0"
                     t-attf-style="background-color: {{lighter ? getLighterShade(color[0]) : color[1]}}" t-on-click.stop="() => this.setColor(lighter ? this.getLighterShade(color[0]) : color[1])" />
                 <div class="floor-picture position-relative text-center overflow-hidden d-flex flex-column align-items-center justify-content-center">
-                    <t t-if="activeFloor.floor_background_image">
+                    <t t-if="activeFloor?.floor_background_image">
                         <img t-att-src="floorBackround" style="width: 48px; height: 48px; object-fit: cover;" />
                     </t>
                     <t t-else="">
@@ -61,11 +61,11 @@
                 <button class="btn btn-secondary p-3 ms-auto rounded-0" t-if="pos.isEditMode or pos.config.floor_ids?.length === 0" t-on-click="addFloor">
                     <i class="fa fa-plus me-2" role="img" aria-label="Add" title="Add" />Add Floor</button>
             </div>
-            <div t-ref="floor-map-scroll" class="overflow-scroll flex-grow-1 flex-shrink-1 flex-basis-0 w-auto" t-attf-style="background: {{activeFloor.background_color}}"> 
+            <div t-ref="floor-map-scroll" class="overflow-scroll flex-grow-1 flex-shrink-1 flex-basis-0 w-auto" t-attf-style="background: {{activeFloor?.background_color}}"> 
                 <div t-on-click="onClickFloorMap" t-on-touchstart="_onPinchStart" t-on-touchmove="_onPinchMove" t-on-touchend="_onPinchEnd"
                     class="floor-map position-relative w-100 h-100 overflow-hidden"
                     t-ref="floor-map-ref"
-                    t-attf-style="height: {{state.floorHeight}} !important; width: {{state.floorWidth}} !important; {{ activeFloor.floor_background_image and pos.floorPlanStyle !== 'kanban' &amp;&amp; 'background-image: url(' + floorBackround + '); background-size: auto; background-repeat: no-repeat; background-attachment: local;' }}">
+                    t-attf-style="height: {{state.floorHeight}} !important; width: {{state.floorWidth}} !important; {{ activeFloor?.floor_background_image and pos.floorPlanStyle !== 'kanban' &amp;&amp; 'background-image: url(' + floorBackround + '); background-size: auto; background-repeat: no-repeat; background-attachment: local;' }}">
                     <t t-if="pos.config.floor_ids?.length > 0">
                         <div t-if="!activeTables?.length" class="empty-floor d-flex align-items-center justify-content-center h-100 fs-3 text-center text-muted" t-ref="map">
                             <span>Oops! No tables available.<br/>Add a new table to get started.</span>