e4534c4c82ef7633c141920cd6c5d2cb915fed01
Rémy Voet (ryv) <ryv@odoo.com>
2024-04-22 11:55:44 +0200

    [FIX] core: add invalidation of Environment's `_cache_key`.
    Changing the environment in method create() to force bin_size=False
    looks harmless, but it actually breaks many tests, in particular in
    module account.  The reason is that company_dependent fields are read at
    the wrong place in the cache.  And this is because `env._cache_key` can
    be polluted with old data.
    
    Make sure that `_cache_key` is cleared when resetting all the lazy
    properties on the environment.  Only the change in res_user.py makes it
    work, but let's not tempt the devil.
    
    Side note: I hate caches.

--- odoo/api.py
+++ odoo/api.py
@@ -698,6 +698,7 @@ class Environment(Mapping):
             This may be useful when recovering from a failed ORM operation.
         """
         lazy_property.reset_all(self)
+        self._cache_key.clear()
         self.transaction.clear()
 
     def invalidate_all(self, flush=True):
@@ -890,6 +891,7 @@ class Transaction:
         for env in self.envs:
             env.registry = self.registry
             lazy_property.reset_all(env)
+            env._cache_key.clear()
         self.clear()
 
 
