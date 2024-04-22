95f70f7290de7fa374fc98c99361a3a3bd1819a2
Carlos <A01708634@tec.mx>
2024-04-22 12:12:40 -0600

    fix(objetivos): Corregir manifest para permisos

--- talent360/evaluaciones/__manifest__.py
+++ talent360/evaluaciones/__manifest__.py
@@ -2,8 +2,8 @@
     "name": "Evaluaciones",
     "application": True,
     "data": [
-        "security/ir.model.access.csv",
         "security/security.xml",
+        "security/ir.model.access.csv",
         "views/evaluaciones_views.xml",
         "views/objetivos_views.xml",
         "views/evaluaciones_menus.xml",
