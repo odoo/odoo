a5f633c5c11e12ff9f0093c2533130f02d855cbb
Juan Pablo Cabrera <jpcq2002@yahoo.com.mx>
2024-04-22 16:33:45 -0600

    fix(security): Fixing security issue

--- talent360/evaluaciones/__manifest__.py
+++ talent360/evaluaciones/__manifest__.py
@@ -2,10 +2,10 @@
     "name": "Evaluaciones",
     "application": True,
     "data": [
-        "security/ir.model.access.csv",
         "security/security.xml",
         "views/evaluaciones_views.xml",
         "views/evaluaciones_menus.xml",
+        "security/ir.model.access.csv",
     ],
     "depends": ["base", "mail"],
 }
