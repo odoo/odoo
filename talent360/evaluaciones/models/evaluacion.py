87228b92908f902a220c77cf9064264e7c548cf0
ricardo020202 <ricoroca2@hotmail.com>
2024-04-22 19:30:25 -0600

    fix(evaluaciones): fix code to pass checklists

--- talent360/evaluaciones/models/evaluacion.py
+++ talent360/evaluaciones/models/evaluacion.py
@@ -41,24 +41,6 @@ class Evaluacion(models.Model):
         "usuario_id",
         string="Asignados",
     )
-
-    # do something on new usuario assigned
-
-    def write(self, vals):
-        res = super(Evaluacion, self).write(vals)
-        if "usuario_ids" in vals:
-            for user_change in vals["usuario_ids"]:
-                action, user_id = user_change
-                user = self.env["res.users"].browse(user_id)
-                partner_id = user.partner_id.id
-                if action == 4:
-                    print(f"Se ha asignado la evaluación {self.nombre} a {partner_id}")
-                    # Send email to assigned user
-                    self.message_post(
-                        body=f"Se te ha asignado la evaluación {self.nombre}",
-                        partner_ids=[partner_id],
-                    )
-        return res
     
     # Método para copiar preguntas de la plantilla a la evaluación
     def copiar_preguntas_de_template(self):
@@ -75,17 +57,17 @@ class Evaluacion(models.Model):
         """
 
         if not self:
-            new_evaluation = self.env['evaluacion'].create({
-                'nombre': 'Evaluacion Clima',
+            new_evaluation = self.env["evaluacion"].create({
+                "nombre": "Evaluacion Clima",
             })
             self = new_evaluation
 
         self.pregunta_ids = [(5,)]
 
-        template_id_hardcoded = 332
+        template_id_hardcoded = 1
 
         if template_id_hardcoded:
-            template = self.env['template'].browse(template_id_hardcoded)
+            template = self.env["template"].browse(template_id_hardcoded)
             if template:
                 pregunta_ids = template.pregunta_ids.ids
                 print("IDs de preguntas:", pregunta_ids)
@@ -93,7 +75,7 @@ class Evaluacion(models.Model):
 
         return self
 
-    def action_clima(self):
+    def evaluacion_clima_action_form(self):
         """
         Ejecuta la acción de copiar preguntas de un template a la evaluación actual y devuelve
         un diccionario con los parámetros necesarios para abrir una ventana de acción en Odoo.
@@ -112,16 +94,16 @@ class Evaluacion(models.Model):
 
         # Retornar la acción con la vista como destino
         return {
-            'type': 'ir.actions.act_window',
-            'name': 'Evaluación Clima',
-            'res_model': 'evaluacion',
-            'view_mode': 'form',
-            'view_id': self.env.ref('evaluaciones.clima_form').id,
-            'target': 'current',
-            'res_id': self.id,
+            "type": "ir.actions.act_window",
+            "name": "Evaluación Clima",
+            "res_model": "evaluacion",
+            "view_mode": "form",
+            "view_id": self.env.ref("evaluaciones.evaluacion_clima_form").id,
+            "target": "current",
+            "res_id": self.id,
         }
 
-    def action_redirect_to_evaluacion(self):
+    def evaluacion_action_tree(self):
         """
         Ejecuta la acción de redireccionar a la lista de evaluaciones y devuelve un diccionario
 
@@ -134,9 +116,9 @@ class Evaluacion(models.Model):
         """
 
         return {
-            'name': 'Evaluación',
-            'type': 'ir.actions.act_window',
-            'res_model': 'evaluacion',
-            'view_mode': 'tree,form',
-            'target': 'current',
+            "name": "Evaluación",
+            "type": "ir.actions.act_window",
+            "res_model": "evaluacion",
+            "view_mode": "tree,form",
+            "target": "current",
         }
