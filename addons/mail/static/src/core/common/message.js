8df5fd669a64fc64d16dbc03ceaa0937d594284d
tsm-odoo <tsm@odoo.com>
2024-04-23 13:46:43 +0200

    [IMP] mail: handle message highlight from thread
    This PR focuses on two points to prepare the groundwork for opening a
    thread to the last unseen message:
    - Return the responsibility of scrolling to messages to the thread component.
    - Enhance scrolling performance, which is currently quite stuttered.
    
    Currently, messages are responsible for scrolling themselves into view.
    This is not good conceptually: the thread is the message list i.e.
    the message owner. Its children should not make decisions themselves.
    This can be problematic since the message itself doesn't know its
    sibling.
    
    This architecture has several issues:
    - If two highlights were to be done, a message could not cancel the
    previous one.
    - Code is hard to understand and debug as several messages can conflict
    with each other.
    - Conflicts with the scroll logic which is handled by the thread.
    - Jumping to a message is heavily coupled with highlighting.
    
    Part of task-3551627
    
    closes odoo/odoo#162852
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/core/common/message.js
+++ addons/mail/static/src/core/common/message.js
@@ -16,6 +16,9 @@ import {
     Component,
     markup,
     onMounted,
+    onWillDestroy,
+    onWillStart,
+    onWillUpdateProps,
     toRaw,
     useChildSubEnv,
     useEffect,
@@ -77,9 +80,9 @@ export class Message extends Component {
         showDates: true,
     };
     static props = [
+        "registerMessageRef?",
         "hasActions?",
         "isInChatWindow?",
-        "highlighted?",
         "onParentMessageClick?",
         "message",
         "messageEdition?",
@@ -108,6 +111,12 @@ export class Message extends Component {
         /** @type {ShadowRoot} */
         this.shadowRoot;
         this.root = useRef("root");
+        onWillStart(() => this.props.registerMessageRef?.(this.props.message, this.root));
+        onWillUpdateProps((nextProps) => {
+            this.props.registerMessageRef?.(this.props.message, null);
+            this.props.registerMessageRef?.(nextProps.message, this.root);
+        });
+        onWillDestroy(() => this.props.registerMessageRef?.(this.props.message, null));
         this.hasTouch = hasTouch;
         this.messageBody = useRef("body");
         this.messageActions = useMessageActions();
@@ -128,14 +137,6 @@ export class Message extends Component {
                 }
             },
             () => [this.props.messageEdition?.editingMessage]
-        );
-        useEffect(
-            (highlighted) => {
-                if (highlighted) {
-                    this.root.el.scrollIntoView({ behavior: "smooth", block: "center" });
-                }
-            },
-            () => [this.props.highlighted]
         );
         onMounted(() => {
             if (this.messageBody.el) {
@@ -192,7 +193,6 @@ export class Message extends Component {
     get attClass() {
         return {
             [this.props.className]: true,
-            "o-highlighted bg-view shadow-lg": this.props.highlighted,
             "o-selfAuthored": this.message.isSelfAuthored && !this.env.messageCard,
             "o-selected": this.props.messageToReplyTo?.isSelected(
                 this.props.thread,
