d67ea3548e45e29703c13f76ea432e71a66cdfba
Didier (did) <did@odoo.com>
2024-04-23 12:08:47 +0000

    [FIX] mail: display subtype description for empty body
    Before this PR, a notification without body did not display anything inside the
    messaging menu.
    This PR adds a fallback to the `subtype_description`.
    
    Task-3749144
    
    X-original-commit: 00908783458a439df5d66ced5e81623684dbed1f

--- addons/mail/static/tests/messaging_menu/notification_tests.js
+++ addons/mail/static/tests/messaging_menu/notification_tests.js
@@ -326,3 +326,23 @@ QUnit.test(
         assert.containsNone($, ".o-mail-MessagingMenu-counter-badge");
     }
 );
+
+QUnit.test(
+    "subtype description should be displayed when body is empty",
+    async () => {
+        const pyEnv = await startServer();
+        const partnerId = pyEnv["res.partner"].create({ name: "Partner1" });
+        const channelId = pyEnv["discuss.channel"].create({ name: "Test" });
+        const subtypeId = pyEnv["mail.message.subtype"].create({ description: "hello" });
+        pyEnv["mail.message"].create({
+            author_id: partnerId,
+            body: "",
+            model: "discuss.channel",
+            res_id: channelId,
+            subtype_id: subtypeId,
+        });
+        await start();
+        await click(".o_menu_systray i[aria-label='Messages']");
+        await contains(".o-mail-NotificationItem-text", { text: "Partner1: hello" });
+    }
+);
