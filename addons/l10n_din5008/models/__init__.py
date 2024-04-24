6d36b380315559100fcc3612b2077a5524ac3880
Maximilien (malb) <malb@odoo.com>
2024-04-24 10:47:04 +0200

    [IMP] l10n_din5008: xml instead of python
    A lot of information of the din5008 layout were computed with some compute
    function which made the customization a bit hard to do.
    This commit will modify the l10n_din5008 module to pass all the logic in xml.
    
    Also updating the translation of the module.
    
    task-3888498
    
    Part-of: odoo/odoo#160689

--- addons/l10n_din5008/models/__init__.py
+++ addons/l10n_din5008/models/__init__.py
@@ -2,5 +2,3 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import base_document_layout
-from . import account_move
-from . import hr_timesheet
--- addons/l10n_din5008/models/base_document_layout.py
+++ addons/l10n_din5008/models/base_document_layout.py
@@ -1,8 +1,8 @@
 from markupsafe import Markup
 import re
+from dateutil.relativedelta import relativedelta
 
-from odoo import api, models, fields, _
-from odoo.tools import format_date
+from odoo import api, models, fields
 
 
 class BaseDocumentLayout(models.TransientModel):
@@ -36,17 +36,6 @@ class BaseDocumentLayout(models.TransientModel):
     company_registry = fields.Char(related='company_id.company_registry', readonly=True)
     bank_ids = fields.One2many(related='company_id.partner_id.bank_ids', readonly=True)
     account_fiscal_country_id = fields.Many2one(related='company_id.account_fiscal_country_id', readonly=True)
-    l10n_din5008_template_data = fields.Binary(compute='_compute_l10n_din5008_template_data')
-    l10n_din5008_document_title = fields.Char(compute='_compute_l10n_din5008_document_title')
-
-    def _compute_l10n_din5008_template_data(self):
-        self.l10n_din5008_template_data = [
-            (_("Invoice No."), 'INV/2021/12345'),
-            (_("Invoice Date"), format_date(self.env, fields.Date.today())),
-            (_("Due Date"), format_date(self.env, fields.Date.add(fields.Date.today(), days=7))),
-            (_("Delivery Date"), format_date(self.env, fields.Date.add(fields.Date.today(), days=7))),
-            (_("Reference"), 'SO/2021/45678'),
-        ]
-
-    def _compute_l10n_din5008_document_title(self):
-        self.l10n_din5008_document_title = _('Invoice')
+    l10n_din5008_invoice_date = fields.Date(default=fields.Date.today, store=False)
+    l10n_din5008_due_date = fields.Date(default=fields.Date.today() + relativedelta(day=7), store=False)
+    l10n_din5008_delivery_date = fields.Date(default=fields.Date.today() + relativedelta(day=7), store=False)
