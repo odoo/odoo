0fba920ea51388f458e241bd9f1ab8a13b2debb1
Levi Siuzdak (sile) <sile@odoo.com>
2024-04-24 15:27:21 +0000

    [FIX] stock: round w/ `precision_rounding`
    Versions
    --------
    - 17.0+
    
    Issue
    -----
    `precision_rounding` values were being passed incorrectly as
    `precision_digits` parameters.
    
    Solution
    --------
    Pass them as named `precision_rounding` parameters instead.
    
    closes odoo/odoo#164299
    
    X-original-commit: d78a4f3f0c7c83df93a2aa39dcdb0f26a3e6da9c
    Related: odoo/enterprise#61921
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/stock/tests/test_move_lines.py
+++ addons/stock/tests/test_move_lines.py
@@ -109,3 +109,26 @@ class StockMoveLine(TestStockCommon):
             ml.quant_id = self.quant
 
         self.assertEqual(move.move_line_ids.quantity, 0)
+
+    def test_pick_from_5(self):
+        """ check small quantities get handled correctly """
+        precision = self.env.ref('product.decimal_product_uom')
+        precision.digits = 6
+        self.product.uom_id = self.uom_kg
+        move = self.env['stock.move'].create({
+            'name': 'Test move',
+            'product_id': self.product.id,
+            'location_id': self.stock_location,
+            'location_dest_id': self.stock_location,
+            'product_uom_qty': 1e-5,
+        })
+        move_form = Form(move, view='stock.view_stock_move_operations')
+        with move_form.move_line_ids.new() as ml:
+            ml.quant_id = self.quant
+        move = move_form.save()
+        self.assertAlmostEqual(
+            move.move_line_ids.quantity,
+            1e-5,
+            delta=1e-6,
+            msg="Small line quantity should get detected",
+        )
