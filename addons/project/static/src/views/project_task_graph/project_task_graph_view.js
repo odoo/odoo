2bee44b36442806cc1eb06a6d207f421d6aa8a27
luve-odoo <luve@odoo.com>
2024-04-24 16:42:34 +0200

    [IMP] project: add embedded actions
    This commit implements the embedded  actions for the module project.
    It adds a embedded  action for tasks and for project updates which
    are available when a user opens the tasks of a project.
    
    This commit also remove the project override of the control panel which
    was used to display the project update status.
    
    taskid:3750721

--- addons/project/static/src/views/project_task_graph/project_task_graph_view.js
+++ addons/project/static/src/views/project_task_graph/project_task_graph_view.js
@@ -1,6 +1,5 @@
 /** @odoo-module **/
 
-import { ProjectControlPanel } from "@project/components/project_control_panel/project_control_panel";
 import { registry } from "@web/core/registry";
 import { graphView } from "@web/views/graph/graph_view";
 import { ProjectTaskGraphModel } from "./project_task_graph_model";
@@ -9,7 +8,6 @@ const viewRegistry = registry.category("views");
 
 export const projectTaskGraphView = {
     ...graphView,
-    ControlPanel: ProjectControlPanel,
     Model: ProjectTaskGraphModel,
 };
 
