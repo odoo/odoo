738d54ddf3bad9a51299d9a6ff324f620b786a22
Robin Lejeune (role) <role@odoo.com>
2024-04-25 17:23:16 +0200

    [FIX] web_editor: keep span with pseudo-elements on sanitization
    Commit [1] made sure `span`s without attributes are removed, as they're
    useless. However, it did not take pseudo-elements into account. This
    commit ensures that if there is no attribute but a pseudo-element can be
    detected, the tag is not removed.
    
    [1]: https://github.com/odoo/odoo/commit/a26e27c85e6a0685b6cf4d2dbab90453899df34c
    
    opw-3881401
    
    closes odoo/odoo#163618
    
    X-original-commit: 1dfd4a8e8d40f6d28210bd18a6dac6a342e12c17
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/src/utils/sanitize.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/utils/sanitize.js
@@ -27,6 +27,12 @@ import {
 } from './utils.js';
 
 const NOT_A_NUMBER = /[^\d]/g;
+
+function hasPseudoElementContent (node, pseudoSelector) {
+    const content = getComputedStyle(node, pseudoSelector).getPropertyValue('content');
+    return content && content !== 'none';
+}
+
 export function areSimilarElements(node, node2) {
     if (
         !node ||
@@ -338,7 +344,12 @@ class Sanitize {
 
             let firstChild = node.firstChild;
             // Unwrap the contents of SPAN and FONT elements without attributes.
-            if (['SPAN', 'FONT'].includes(node.nodeName) && !node.hasAttributes()) {
+            if (
+                ['SPAN', 'FONT'].includes(node.nodeName)
+                && !node.hasAttributes()
+                && !hasPseudoElementContent(node, "::before")
+                && !hasPseudoElementContent(node, "::after")
+            ) {
                 getDeepRange(this.root, { select: true });
                 const restoreCursor = shouldPreserveCursor(node, this.root) &&
                     preserveCursor(this.root.ownerDocument);
