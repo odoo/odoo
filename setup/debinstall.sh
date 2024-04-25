19a6514a66a3745256ff9ab07d7724f7455486cd
Xavier-Do <xdo@odoo.com>
2024-04-25 22:37:01 +0000

    [FIX] packaging: adapt lxml and install script
    lxml.hml.clean was moved to a separate package starting in lxml 5.2
    The requirements and debian control needs to be adapted to install the
    new package in ubuntu noble (and after ubuntu noble only)
    
    The solution was to use fallback to try to install lxml_html_clean if it
    exists (noble) and fallback on lxml if not.
    
    The script use in the documentation and available in more recent version
    will take all first packge of the debian conrtol, meaning that after
    this change it won't work in distribution not having lxml_html_clean.
    
    The script is adapted to use fallback in apt-install command
    
    Debian control:
    font1 | font2,
    package_1,
    package_noble | package_fallback,
    
    Before:
    apt install font1 package package_noble
    
    After:
    apt install font1|font2 package package_noble|package_fallback
    
    closes odoo/odoo#167443
    
    X-original-commit: 318df32585b6c23bdc53f2d9ea2bea88a128958d
    Related: odoo/documentation#9555
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- setup/debinstall.sh
+++ setup/debinstall.sh
@@ -2,16 +2,26 @@
 #
 # Install Debian packages needed to run Odoo.
 
-if [ "$(id -u)" -ne "0" ]; then
-   echo "This script must be run as root" >&2
-   exit 1
+
+if [ "$1" = "-l" -o "$1" = "--list" ]; then
+    cmd="echo"
+else
+    cmd="apt-get install -y --no-install-recommends"
+    if [ "$(id -u)" -ne "0" ]; then
+        echo "\033[0;31mThis script must be run as root to install dependencies, starting a dry run.\033[0m" >&2
+        cmd="$cmd -s"
+    else
+        apt-get update
+    fi
+    if [ "$1" = "-q" -o "$1" = "--quiet" ]; then
+        cmd="$cmd -qq"
+    fi
 fi
 
 script_path=$(realpath "$0")
 script_dir=$(dirname "$script_path")
 control_path=$(realpath "$script_dir/../debian/control")
 
-apt-get update
 sed -n '/^Depends:/,/^[A-Z]/p' "$control_path" \
-| awk '/^ [a-z]/ { gsub(/,/,"") ; print $1 }' | sort -u \
-| DEBIAN_FRONTEND=noninteractive xargs apt-get install -y -qq
+| awk '/^ [a-z]/ { gsub(/,/,"") ; gsub(" ", "") ; print $NF }' | sort -u \
+| DEBIAN_FRONTEND=noninteractive xargs $cmd
