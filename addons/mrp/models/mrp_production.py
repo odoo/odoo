cac4a45969b551802370724d37cab5256b3edbeb
Adrien Widart (awt) <awt@odoo.com>
2024-04-25 11:32:41 +0000

    [FIX] mrp{,_repair}: use scrapped and tracked compo in a MO
    It is currently not possible to use a scrapped tracked-by-SN
    component in a new MO
    
    To reproduce the issue:
    1. In Settings, enable "Multi Locations"
    2. Create two products P_finished, P_comp:
        - Storable
        - P_comp tracked by SN
    3. Update the quantity of P_comp:
        - WH/Stock: 1 x SN01
    4. Process a manufacturing order:
        - Product: P_finished
        - Components:
          - 1 x P_comp
    5. Process a repair order:
        - Product: P_finished
        - Remove:
               - Product: P_comp
               - Lot: SN01
               - Destination Location: Virtual Location/scrap
    6. Unscrap SN01 (via an internal transfer from scrap to stock)
    7. Repeat 4
    
    Error: a UserError is displayed because the SN of the component has
    already been consumed. It should be possible to use it
    
    When checking the uniqueness, we are working on the SMLs (see the
    lines removed by this commit).
    `duplicates` contains the SML from step 4. `duplicates_returned` is
    zero since we did not return the component back to the stock.
    `removed` is zero too, because we look at the scrapped SML from an
    internal location. However, step 5 generates an SML that starts from
    the production location. As a result, nothing compensate the value
    of `duplicates`, hence the raised error.
    
    Removing the condition of the source location of `removed` would
    lead to another bug, so we can't do that. So, this commit suggests a
    new implementation based on the following rationale:
    - The uniqueness checking simply ensures that one and only one
      existing finished product consumed a specific SN
    - What happened outside this context does not matter here
    
    That way, we should only look at the SML from/to production
    locations, for tracked-by-sn products used as components only. All
    other SML (scrap, unscrap, SML for SN production, and so on) should
    not be considered here.
    
    OPW-3834835
    
    X-original-commit: 77355e20f174a09510c3dff24e5ef8eab301fd47

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -2511,6 +2511,8 @@ class MrpProduction(models.Model):
                     raise UserError(_('The serial number %(number)s used for byproduct %(product_name)s has already been produced',
                                       number=move_line.lot_id.name, product_name=move_line.product_id.name))
 
+        consumed_sn_ids = []
+        sn_error_msg = {}
         for move in self.move_raw_ids:
             if move.has_tracking != 'serial' or not move.picked:
                 continue
@@ -2518,40 +2520,46 @@ class MrpProduction(models.Model):
                 if not move_line.picked or float_is_zero(move_line.quantity, precision_rounding=move_line.product_uom_id.rounding) or\
                         not move_line.lot_id:
                     continue
+                sml_sn = move_line.lot_id
                 message = _('The serial number %(number)s used for component %(component)s has already been consumed',
-                    number=move_line.lot_id.name,
+                    number=sml_sn.name,
                     component=move_line.product_id.name)
+                consumed_sn_ids.append(sml_sn.id)
+                sn_error_msg[sml_sn.id] = message
                 co_prod_move_lines = self.move_raw_ids.move_line_ids
-
-                # Check presence of same sn in previous productions
-                duplicates = self.env['stock.move.line'].search_count([
-                    ('lot_id', '=', move_line.lot_id.id),
-                    ('quantity', '=', 1),
-                    ('state', '=', 'done'),
-                    ('location_dest_id.usage', '=', 'production'),
-                    ('production_id', '!=', False),
-                ])
-                if duplicates:
-                    # Maybe some move lines have been compensated by unbuild
-                    duplicates_returned = move.product_id._count_returned_sn_products(move_line.lot_id)
-                    removed = self.env['stock.move.line'].search_count([
-                        ('lot_id', '=', move_line.lot_id.id),
-                        ('state', '=', 'done'),
-                        ('location_dest_id.scrap_location', '=', True)
-                    ])
-                    unremoved = self.env['stock.move.line'].search_count([
-                        ('lot_id', '=', move_line.lot_id.id),
-                        ('state', '=', 'done'),
-                        ('location_id.scrap_location', '=', True),
-                        ('location_dest_id.scrap_location', '=', False),
-                    ])
-                    # Either removed or unbuild
-                    if not ((duplicates_returned or removed) and duplicates - duplicates_returned - removed + unremoved == 0):
-                        raise UserError(message)
-                # Check presence of same sn in current production
-                duplicates = co_prod_move_lines.filtered(lambda ml: ml.quantity and ml.lot_id == move_line.lot_id) - move_line
+                duplicates = co_prod_move_lines.filtered(lambda ml: ml.quantity and ml.lot_id == sml_sn) - move_line
                 if duplicates:
                     raise UserError(message)
+
+        if not consumed_sn_ids:
+            return
+
+        consumed_sml_groups = self.env['stock.move.line']._read_group([
+            ('lot_id', 'in', consumed_sn_ids),
+            ('quantity', '=', 1),
+            ('state', '=', 'done'),
+            ('location_dest_id.usage', '=', 'production'),
+            ('production_id', '!=', False),
+        ], ['lot_id'], ['quantity:sum'])
+        consumed_qties = {lot.id: qty for lot, qty in consumed_sml_groups}
+        problematic_sn_ids = list(consumed_qties.keys())
+        if not problematic_sn_ids:
+            return
+
+        cancelled_sml_groups = self.env['stock.move.line']._read_group([    # SML that cancels the SN consumption
+            ('lot_id', 'in', problematic_sn_ids),
+            ('quantity', '=', 1),
+            ('state', '=', 'done'),
+            ('location_id.usage', '=', 'production'),
+            ('move_id.production_id', '=', False),
+        ], ['lot_id'], ['quantity:sum'])
+        cancelled_qties = defaultdict(float, {lot.id: qty for lot, qty in cancelled_sml_groups})
+
+        for sn_id in problematic_sn_ids:
+            consumed_qty = consumed_qties[sn_id]
+            cancelled_qty = cancelled_qties[sn_id]
+            if consumed_qty - cancelled_qty > 0:
+                raise UserError(sn_error_msg[sn_id])
 
     def _is_finished_sn_already_produced(self, lot, excluded_sml=None):
         if not lot:
