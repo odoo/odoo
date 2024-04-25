bd37eb63dbcbd2cf36061162df7e1b3195a95b32
Maruan Aguerdouh (magm) <magm@odoo.com>
2024-04-25 17:03:11 +0200

    [FIX] l10n_ar_pos: no traceback on creating partner pos when argentina localization
    Steps to reproduce:
    
    1. Install l10n_ar_pos.
    2. Make sure we are in the Argentinian company.
    3. Open PoS and create a new session.
    4. When inside, try to create a new partner from here.
    
    The issue is that, since the changes where we retrieve the partner from
    the props, we might have cases where we don't have a partner since
    this field is defined as optional in our point_of_sale PartnerList, from
    we retrieve the props from.
    
    opw-3872968

--- addons/l10n_ar_pos/static/src/overrides/components/partner_list/partner_list.js
+++ addons/l10n_ar_pos/static/src/overrides/components/partner_list/partner_list.js
@@ -6,7 +6,7 @@ import { patch } from "@web/core/utils/patch";
 patch(PartnerList.prototype, {
     createPartner() {
         super.createPartner(...arguments);
-        if (this.pos.isArgentineanCompany()) {
+        if (this.props.partner && this.pos.isArgentineanCompany()) {
             this.props.partner.l10n_latam_identification_type_id = this.pos.models[
                 "l10n_latam.identification.type"
             ].get(this.pos["l10n_latam.identification.type"][0].id);
