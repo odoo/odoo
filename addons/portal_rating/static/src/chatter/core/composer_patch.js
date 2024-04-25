keyboard-slayer/odoo-eval

[IMP] portal, mail, *: Use backend chatter in portal
By this commit:
- Portal chatter widget is deleted
- The chatter in portal is the backend Chatter component (from mail module) now
- Some features in chatter like link preview and reactions are new in portal
environment (number of queries in some tests are increased because of reactions
feature on messages)
- Except of two use cases of portal composer (as a popup composer) all the other
 use cases of composer in portal, are replaced by composer component in mail
 module
- The project sharing doesn't use the previous chatter component in the sharing
webclient view. It's replaced by backend chatter component as well

task-2828744

addons/portal_rating/static/src/chatter/core/composer_patch.js
@@ -1,0 +1,50 @@
+import { Composer } from "@mail/core/common/composer";
+
+import { patch } from "@web/core/utils/patch";
+import { rpc } from "@web/core/network/rpc";
+import { useState } from "@odoo/owl";
+
+patch(Composer.prototype, {
+    setup() {
+        super.setup(...arguments);
+        this.state = useState({
+            ...this.state,
+            ratingValue: 4,
+            starValue: 4,
+        });
+    },
+
+    editMessage() {
+        if (this.props.composer.portalComment) {
+            this.savePublisherComment();
+            return;
+        }
+        super.editMessage();
+    },
+
+    async savePublisherComment() {
+        const data = await rpc("/website/rating/comment", {
+            rating_id: this.message.rating.id,
+            publisher_comment: this.props.composer.textInputContent.trim(),
+        });
+        this.message.rating = data;
+        this.props.onPostCallback();
+    },
+
+    onMoveStar(ev) {
+        var index = parseInt(ev.currentTarget.getAttribute("index"));
+        this.state.starValue = index + 1;
+    },
+
+    onClickStar() {
+        this.state.ratingValue = this.state.starValue;
+    },
+
+    postData(composer) {
+        const postData = super.postData(composer);
+        if (this.env.displayRating && !this.message) {
+            postData.options.rating_value = this.state.ratingValue;
+        }
+        return postData;
+    },
+});

odoo/odoo

addons/portal_rating/static/src/chatter/core/composer_patch.js
@@ -1,0 +1,50 @@
+import { Composer } from "@mail/core/common/composer";
+
+import { patch } from "@web/core/utils/patch";
+import { rpc } from "@web/core/network/rpc";
+import { useState } from "@odoo/owl";
+
+patch(Composer.prototype, {
+    setup() {
+        super.setup(...arguments);
+        this.state = useState({
+            ...this.state,
+            ratingValue: 4,
+            starValue: 4,
+        });
+    },
+
+    editMessage() {
+        if (this.props.composer.portalComment) {
+            this.savePublisherComment();
+            return;
+        }
+        super.editMessage();
+    },
+
+    async savePublisherComment() {
+        const data = await rpc("/website/rating/comment", {
+            rating_id: this.message.rating.id,
+            publisher_comment: this.props.composer.text.trim(),
+        });
+        this.message.rating = data;
+        this.props.onPostCallback();
+    },
+
+    onMoveStar(ev) {
+        var index = parseInt(ev.currentTarget.getAttribute("index"));
+        this.state.starValue = index + 1;
+    },
+
+    onClickStar() {
+        this.state.ratingValue = this.state.starValue;
+    },
+
+    postData(composer) {
+        const postData = super.postData(composer);
+        if (this.env.displayRating && !this.message) {
+            postData.options.rating_value = this.state.ratingValue;
+        }
+        return postData;
+    },
+});