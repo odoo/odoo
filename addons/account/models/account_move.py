0835a6656f11e026e6a286fe7a6b69b540d87dab
sesn-odoo <sesn@odoo.com>
2024-04-25 13:18:24 +0000

    [FIX] account: synchronize Invoice Line Dates with Invoice Date
    Currently, when the `invoice_date` of an invoice is updated (triggering
    the recomputation of `date`) and if a system flush occurs before any
    line's date is accessed, the invoice lines' dates do not get updated.
    The following test illustrates this issue:
    
    ```py
    move = self.init_invoice(
        move_type='in_invoice',
        partner=self.partner_a,
        amounts=[1000.0],
    )
    
    move.invoice_date = fields.Date.from_string('2024-01-01')
    self.env.flush_all()
    
    for line in move.line_ids:
        self.assertEqual(line.date, move.date) # will fail
    ```
    
    Cause
    -----
    The `date` of a move is a computed field dependent on the move's
    `invoice_date`. The `date` of a move line is a related field, pointing
    to its parent move's `date` (note: related fields are computed fields).
    During a flush, the system recomputes all fields that need to be. Here,
    the system first processes 'account.move.date' and calls its computation
    (`_compute_date`). However, the `_affect_tax_report()` call within
    `_compute_date` triggers a recalculation of `account.move.line.date`,
    but as this happens within `_compute_date`, the invoice lines' `date` is
    recalculated using the old invoice `date`.
    
    Fix
    ---
    Force a recalculation of the invoice lines' dates whenever the invoice's
    date is changed.
    
    opw-3759472
    opw-3875405
    opw-3872006
    opw-3884013
    
    X-original-commit: e9d955c5a52902cdac86c6285204c54876c68eac

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -697,6 +697,8 @@ class AccountMove(models.Model):
                 accounting_date = move._get_accounting_date(move.invoice_date, move._affect_tax_report())
             if accounting_date and accounting_date != move.date:
                 move.date = accounting_date
+                # _affect_tax_report may trigger premature recompute of line_ids.date
+                self.env.add_to_compute(move.line_ids._fields['date'], move.line_ids)
                 # might be protected because `_get_accounting_date` requires the `name`
                 self.env.add_to_compute(self._fields['name'], move)
 
