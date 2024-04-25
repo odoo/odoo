c22761ad61f85c902814e0504d5eeafbf5e70061
Samuel Degueldre <sad@odoo.com>
2024-04-25 11:44:10 +0000

    [FIX] web: only show error dialogs for third-party scripts in debug mode
    Previously, if an error occurred in a third-party script loaded from a
    different origin (eg: google analytics or scripts injected into the
    in-app browsers of Meta apps such as Facebook or Instagram), we would
    show an error dialog mentioning a "CORS" error with no details.
    
    There are three problems with this: first, this is not actually a CORS
    error. It is normal error, but in Chrome and Safari, the details of the
    error are unavailable inside of code from scripts hosted on a different
    origin, this unavailability is caused by CORS but the error is *not* a
    CORS error.
    
    The second problem is that we're showing these error dialogs to website
    visitors, who are both likely to be confused or frustrated by the error
    and very unlikely to report it.
    
    Lastly, because the error occurred in a third party script, it's likely
    code that is not even controlled by the Odoo instance in any way, which
    means that it oftentimes cannot be fixed in Odoo.
    
    In Firefox, the first point doesn't hold true, as Firefox does not hide
    the error details, but we still do not want to display an error dialog
    because of the two other points.
    
    This commit fixes this by simply not displaying these errors outside of
    debug mode. Actual visitors will not be in debug mode, but it could be
    useful to still show the error for people developing integrations with
    third-party libraries and scripts. The error dialog will still not
    contain much information in non-Firefox browsers, but we encourage the
    developer to look at the browser console for extra details.
    
    opw-3857323
    
    closes odoo/odoo#164584
    
    X-original-commit: 1c58aed76c0facdc7d6b4bac97f62db5098abd8e
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>
    Signed-off-by: Samuel Degueldre (sad) <sad@odoo.com>

--- addons/web/static/tests/core/errors/error_service_tests.js
+++ addons/web/static/tests/core/errors/error_service_tests.js
@@ -5,7 +5,6 @@ import { dialogService } from "@web/core/dialog/dialog_service";
 import {
     ClientErrorDialog,
     RPCErrorDialog,
-    NetworkErrorDialog,
     standardErrorDialogProps,
 } from "@web/core/errors/error_dialogs";
 import { errorService, UncaughtPromiseError } from "@web/core/errors/error_service";
@@ -373,24 +372,45 @@ QUnit.test("handle uncaught client errors", async (assert) => {
     assert.verifyErrors(["This is an error test"]);
 });
 
-QUnit.test("handle uncaught CORS errors", async (assert) => {
-    assert.expectErrors();
+QUnit.test("don't show dialog for errors in third-party scripts", async (assert) => {
     class TestError extends Error {}
     const error = new TestError();
-    error.message = "This is a cors error";
-    error.name = "CORS error";
+    error.message = "Script error.";
+    error.name = "Script error.";
 
-    function addDialog(dialogClass, props) {
-        assert.strictEqual(dialogClass, NetworkErrorDialog);
-        assert.strictEqual(props.message, "Uncaught CORS Error");
+    function addDialog(_dialogClass, props) {
+        assert.step(props.message);
     }
     serviceRegistry.add("dialog", makeFakeDialogService(addDialog), { force: true });
     await makeTestEnv();
 
-    // CORS error event has no colno, no lineno and no filename
+    // Error events from errors in third-party scripts hav no colno, no lineno and no filename
+    // because of CORS.
     const errorEvent = new ErrorEvent("error", { error, cancelable: true });
     await errorCb(errorEvent);
-    assert.verifyErrors(["This is a cors error"]);
+    assert.verifySteps([]);
+});
+
+QUnit.test("show dialog for errors in third-party scripts in debug mode", async (assert) => {
+    assert.expectErrors();
+    class TestError extends Error {}
+    const error = new TestError();
+    error.message = "Script error.";
+    error.name = "Script error.";
+    patchWithCleanup(odoo, { debug: true });
+
+    function addDialog(_dialogClass, props) {
+        assert.step(props.message);
+    }
+    serviceRegistry.add("dialog", makeFakeDialogService(addDialog), { force: true });
+    await makeTestEnv();
+
+    // Error events from errors in third-party scripts have no colno, no lineno and no filename
+    // because of CORS.
+    const errorEvent = new ErrorEvent("error", { error, cancelable: true });
+    await errorCb(errorEvent);
+    assert.verifySteps(["Uncaught CORS Error"]);
+    assert.verifyErrors(["Script error."]);
 });
 
 QUnit.test("lazy loaded handlers", async (assert) => {
