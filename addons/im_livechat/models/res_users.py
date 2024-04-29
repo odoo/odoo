7ada399b576055a1bbaa13c7d843e6c824ac4e59
Martin Trigaux (mat) <mat@odoo.com>
2024-04-29 11:08:02 +0200

    [FIX] im_livechat: access has_group with sudo
    Since cb31d4352c741, can’t call has_group on another user without the
    su flag
    
    Before this commit, trying to open the user form view of a portal user
    raised an error "You can ony call user.has_group() with your current
    user"

--- addons/im_livechat/models/res_users.py
+++ addons/im_livechat/models/res_users.py
@@ -45,7 +45,7 @@ class Users(models.Model):
             user.res_users_settings_id.livechat_lang_ids = user.livechat_lang_ids
 
     def _compute_has_access_livechat(self):
-        for user in self:
+        for user in self.sudo():
             user.has_access_livechat = user.has_group('im_livechat.im_livechat_group_user')
 
     def _init_store_data(self, store):
