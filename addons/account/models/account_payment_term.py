27c53f1c5664fa6c3f27f7d816764c74274e1b02
Levi Siuzdak (sile) <sile@odoo.com>
2024-04-29 13:05:05 +0200

    [FIX] account: round discount after subtracting
    Versions
    --------
    - saas-16.3+
    
    Issue
    -----
    If early pay discount includes taxes, its amount was calculated by
    first multiplying the total amount by the discount's complementary
    percentage, and take the difference between its result and the total
    amount.
    
    Let's assume discount is 10% and total price is 1725.05.
    - We want to pay 90% of the full amount, which is 1552.545;
    - to get the discount, we subtract it from the full amount:
        1725.05 - 1552.545 = 172.505;
    - we then round this discount to 172.51;
    - and return total amount minus discount as amount due after discount:
        1725.05 - 172.51 = 1552.54
    - elsewhere we round 1552.545 to 1552.55, a 1 cent difference
    
    Cause
    -----
    The `_get_amount_due_after_discount` adds half a cent to the discount by
    rounding half-up, while elsewhere the reduced price gets rounded
    half-up, increasing it by half a cent, this adds up to a 1 cent
    discrepancy.
    
    Solution
    --------
    Call the rounding method after subtracting discount from the total
    amount, so rounding happens in the same direction.
    
    Issue found working on opw-3705546
    
    closes odoo/odoo#163786
    
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account/models/account_payment_term.py
+++ addons/account/models/account_payment_term.py
@@ -56,10 +56,10 @@ class AccountPaymentTerm(models.Model):
         if self.early_discount:
             percentage = self.discount_percentage / 100.0
             if self.early_pay_discount_computation in ('excluded', 'mixed'):
-                discount_amount_currency = self.currency_id.round((total_amount - untaxed_amount) * percentage)
+                discount_amount_currency = (total_amount - untaxed_amount) * percentage
             else:
-                discount_amount_currency = self.currency_id.round(total_amount - (total_amount * (1 - (percentage))))
-            return total_amount - discount_amount_currency
+                discount_amount_currency = total_amount * percentage
+            return self.currency_id.round(total_amount - discount_amount_currency)
         return total_amount
 
     @api.depends('company_id')
