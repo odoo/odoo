83f3c7f172f7747d84e92dc23102ae29d4b61b2d
Sanjay Sharma <shsa@odoo.com>
2024-04-29 13:40:47 +0530

    [IMP] webiste_cf_turnstile: transition from jQuery to vanilla js
    Specification:
    
    This commit focuses on transitioning all frontend code from jQuery to
    Vanilla JavaScript. The changes specifically target files in the
    web.assets_frontend.
    
    task-3928123

--- addons/website_cf_turnstile/static/src/js/turnstile.js
+++ addons/website_cf_turnstile/static/src/js/turnstile.js
@@ -5,51 +5,60 @@ import publicWidget from '@web/legacy/js/public/public_widget';
 import { session } from "@web/session";
 
 publicWidget.registry.s_website_form.include({
-        /**
-         * @override
-         */
-        start: function () {
-            const res = this._super(...arguments);
-            this.cleanTurnstile();
-            if (!this.isEditable && !this.$('.s_turnstile').length && session.turnstile_site_key) {
-                const mode = new URLSearchParams(window.location.search).get('cf') == 'show' ? 'always' : 'interaction-only';
-                $(`<div class="s_turnstile cf-turnstile float-end"
-                         data-action="website_form"
-                         data-appearance="${mode}"
-                         data-response-field-name="turnstile_captcha"
-                         data-sitekey="${session.turnstile_site_key}"
-                         data-error-callback="throwTurnstileError"
-                    ></div>
-                    <script class="s_turnstile">
-                        // Rethrow the error, or we only will catch a "Script error" without any info 
-                        // because of the script api.js originating from a different domain.
-                        function throwTurnstileError(code) {
-                            const error = new Error("Turnstile Error");
-                            error.code = code;
-                            throw error;
-                        }
-                    </script>
-                    <script class="s_turnstile" src="https://challenges.cloudflare.com/turnstile/v0/api.js"></script>
-                `).insertAfter('.s_website_form_send, .o_website_form_send');
-            }
-            return res;
-        },
+    /**
+     * @override
+    */
+    start: function () {
+        const res = this._super(...arguments);
+        this.cleanTurnstile();
+        if (!this.isEditable && !this.el.querySelector(".s_turnstile") && session.turnstile_site_key) {
+            const mode = new URLSearchParams(window.location.search).get("cf") == "show" ? "always" : "interaction-only";
+            const turnstileElement = document.createElement("div");
+            turnstileElement.className = "s_turnstile cf-turnstile float-end";
+            turnstileElement.dataset.action = "website_form";
+            turnstileElement.dataset.appearance = mode;
+            turnstileElement.dataset.responseFieldName = "turnstile_captcha";
+            turnstileElement.dataset.sitekey = session.turnstile_site_key;
+            turnstileElement.dataset.errorCallback = "throwTurnstileError";
 
-        /**
-         * Remove potential existing loaded script/token
-         */
-        cleanTurnstile: function () {
-            if (this.$('.s_turnstile').length) {
-                this.$('.s_turnstile').remove();
-            }
-        },
+            const scriptElement1 = document.createElement("script");
+            scriptElement1.className = "s_turnstile";
+            scriptElement1.textContent = `
+                // Rethrow the error, or we only will catch a "Script error" without any info
+                // because of the script api.js originating from a different domain.
+                function throwTurnstileError(code) {
+                    const error = new Error("Turnstile Error");
+                    error.code = code;
+                    throw error;
+                }
+            `;
 
-        /**
-         * @override
-         * Discard all library changes to reset the state of the Html.
-         */
-        destroy: function () {
-            this.cleanTurnstile();
-            this._super(...arguments);
-        },
+            const scriptElement2 = document.createElement("script");
+            scriptElement2.className = "s_turnstile";
+            scriptElement2.src = "https://challenges.cloudflare.com/turnstile/v0/api.js";
+
+            const formSendElement = this.el.querySelector(".s_website_form_send, .o_website_form_send");
+            formSendElement.parentNode.insertBefore(turnstileElement, formSendElement.nextSibling);
+            formSendElement.parentNode.insertBefore(scriptElement1, formSendElement.nextSibling);
+            formSendElement.parentNode.insertBefore(scriptElement2, formSendElement.nextSibling);
+        }
+        return res;
+    },
+
+    /**
+     * Remove potential existing loaded script/token
+    */
+    cleanTurnstile: function () {
+        const turnstileElements = this.el.querySelectorAll(".s_turnstile");
+        turnstileElements.forEach(element => element.remove());
+    },
+
+    /**
+     * @override
+     * Discard all library changes to reset the state of the Html.
+    */
+    destroy: function () {
+        this.cleanTurnstile();
+        this._super(...arguments);
+    },
 });
