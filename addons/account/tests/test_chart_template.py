18a170c067797f8c987877d0b84cc944fd232c26
Jinane Maksoud <maji@odoo.com>
2024-04-30 16:24:43 +0200

    [FIX] account: manage old tax renamed multiple times
    If we rename the tax to '[old] taxname' everytime we detect a modification
    then we will run into a unique name violation. Taking account of how many
    other taxes with the same name there already are will bypass this
    constraint, like it used to be in previous versions.
    
    Extended a unit test to update the same tax in template twice and check
    that no name conflict occurs. The taxes created during the test had
    a null tax_scope which was not triggering the sql unique constraint
    name_company_uniq even when the name was the same, so a default value
    for the tax scope is added ot test taxes.
    
    closes odoo/odoo#164405
    
    X-original-commit: 1630f14b729d1f605e3620e5978fa9507b5c3533
    Signed-off-by: Aliya Tastemirova (alta) <alta@odoo.com>

--- addons/account/tests/test_chart_template.py
+++ addons/account/tests/test_chart_template.py
@@ -112,7 +112,8 @@ def test_get_data(self, template_code):
         }
     }
 
-def _tax_vals(name, amount, tax_tag_id=None, children_tax_xmlids=None, active=True):
+
+def _tax_vals(name, amount, tax_tag_id=None, children_tax_xmlids=None, active=True, tax_scope="consu"):
     tag_command = [Command.set([tax_tag_id])] if tax_tag_id else None
     tax_vals = {
         'name': name,
@@ -120,6 +121,7 @@ def _tax_vals(name, amount, tax_tag_id=None, children_tax_xmlids=None, active=Tr
         'amount_type': 'percent' if not children_tax_xmlids else 'group',
         'tax_group_id': 'tax_group_taxes',
         'active': active,
+        'tax_scope': tax_scope
     }
     if children_tax_xmlids:
         tax_vals.update({'children_tax_ids': [Command.set(children_tax_xmlids)]})
@@ -365,6 +367,11 @@ class TestChartTemplate(TransactionCase):
             data['account.tax']['test_tax_1_template']['amount'] = 40
             return data
 
+        def local_get_data2(self, template_code):
+            data = test_get_data(self, template_code)
+            data['account.tax']['test_tax_1_template']['amount'] = 15
+            return data
+
         tax_1_existing = self.env['account.tax'].search([('company_id', '=', self.company_1.id), ('name', '=', "Tax 1")])
         with patch.object(AccountChartTemplate, '_get_chart_template_data', side_effect=local_get_data, autospec=True):
             self.env['account.chart.template'].try_loading('test', company=self.company_1, install_demo=False)
@@ -372,6 +379,16 @@ class TestChartTemplate(TransactionCase):
         tax_1_new = self.env['account.tax'].search([('company_id', '=', self.company_1.id), ('name', '=', "Tax 1")])
         self.assertEqual(tax_1_old, tax_1_existing, "Old tax still exists but with a different name.")
         self.assertEqual(len(tax_1_new), 1, "New tax have been created with the original name.")
+
+        with patch.object(AccountChartTemplate, '_get_chart_template_data', side_effect=local_get_data2, autospec=True):
+            self.env['account.chart.template'].try_loading('test', company=self.company_1, install_demo=False)
+        tax_1_old_first = self.env['account.tax'].search([('company_id', '=', self.company_1.id), ('name', '=', "[old] Tax 1")])
+        tax_1_old_second = self.env['account.tax'].search([('company_id', '=', self.company_1.id), ('name', '=', "[old1] Tax 1")])
+        tax_1_latest = self.env['account.tax'].search([('company_id', '=', self.company_1.id), ('name', '=', "Tax 1")])
+
+        self.assertEqual(tax_1_old, tax_1_old_first, "Old renamed tax is still the same.")
+        self.assertEqual(tax_1_old_second, tax_1_new, "Outdated tax is renamed again.")
+        self.assertEqual(len(tax_1_latest), 1, "New tax have been created with the original name.")
 
     def test_update_taxes_multi_company(self):
         """ In a multi-company environment all companies should be correctly updated."""
