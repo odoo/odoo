2673a768f2db7dc63add64f37238fb53cb711a1f
Didier (did) <did@odoo.com>
2024-04-30 14:22:04 +0200

    [IMP] mail: export test models
    The mail module uses models from web, bus and mail for its hoot test
    suite. Every module depending on mail would need to define those models
    to work properly.
    
    This is not robust as any change in the mail part would introduce inconsistencies
    in other modules.
    
    In order to solve this issue, the mail module now exports its required set of
    models so that other modules can reuse it.
    
    closes odoo/odoo#163998
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/im_livechat/static/tests/livechat_test_helpers.js
+++ addons/im_livechat/static/tests/livechat_test_helpers.js
@@ -1,6 +1,5 @@
-import { busModels } from "@bus/../tests/bus_test_helpers";
 import { mailModels } from "@mail/../tests/mail_test_helpers";
-import { webModels, defineModels, serverState } from "@web/../tests/web_test_helpers";
+import { defineModels, serverState } from "@web/../tests/web_test_helpers";
 import { DiscussChannel } from "./mock_server/mock_models/discuss_channel";
 import { DiscussChannelMember } from "./mock_server/mock_models/discuss_channel_member";
 import { LivechatChannel } from "./mock_server/mock_models/im_livechat_channel";
@@ -10,10 +9,11 @@ import { ResPartner } from "./mock_server/mock_models/res_partner";
 import { ResUsers } from "./mock_server/mock_models/res_users";
 
 export function defineLivechatModels() {
-    return defineModels({ ...webModels, ...busModels, ...mailModels, ...livechatModels });
+    return defineModels(livechatModels);
 }
 
 export const livechatModels = {
+    ...mailModels,
     DiscussChannel,
     DiscussChannelMember,
     LivechatChannel,
