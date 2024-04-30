6d1349c6d3a3cdc5960d6ec61deb611866300ca9
Adrien Widart (awt) <awt@odoo.com>
2024-04-30 10:11:14 +0200

    [FIX] mrp_subcontracting_purchase: bill standard product with pdiff
    Fine-tuning of [1]. We want to components cost for one finished
    product, we should therefore use the total produced quantity, not
    the invoiving one
    
    [1] e771535b487e76f4e5a23b767d220d7f640292a6
    
    Indirectly related to OPW-3806378

--- addons/mrp_subcontracting_purchase/models/account_move_line.py
+++ addons/mrp_subcontracting_purchase/models/account_move_line.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models
+from odoo.tools import float_is_zero
 
 
 class AccountMoveLine(models.Model):
@@ -12,7 +13,9 @@ class AccountMoveLine(models.Model):
             components_cost = 0
             subcontract_production = self.purchase_line_id.move_ids._get_subcontract_production()
             components_cost -= sum(subcontract_production.move_raw_ids.stock_valuation_layer_ids.mapped('value'))
-            price_unit_val_dif = price_unit_val_dif + components_cost / relevant_qty
+            qty = sum(mo.product_uom_id._compute_quantity(mo.qty_producing, self.product_uom_id) for mo in subcontract_production if mo.state == 'done')
+            if not float_is_zero(qty, precision_rounding=self.product_uom_id.rounding):
+                price_unit_val_dif = price_unit_val_dif + components_cost / qty
         return price_unit_val_dif, relevant_qty
 
     def _get_valued_in_moves(self):
