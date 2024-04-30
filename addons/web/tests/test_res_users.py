220bf16de9d3faf0be965bc381f5e431a7e7e355
sofiagvaladze <sgv@odoo.com>
2024-04-30 11:06:14 +0200

    [FIX] base: change password
    When we try to change user's password, we get following error
    
    The operation cannot be completed:
    - Create/update: a mandatory field is not set.
    - Delete: another model requires the record being deleted. If possible, archive it instead.
    
    Model: User, Change Password Wizard (change.password.user)
    Field: User (user_id)
    
    This is because invisible user_id was removed here https://github.com/odoo/odoo/pull/162009
    
    This commit fixes this. But instead of putting invisible="1", we put column_invisible="True",
    as this is a tree view and otherwise we will be still seeing invisible field

--- addons/web/tests/test_res_users.py
+++ addons/web/tests/test_res_users.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.tests import TransactionCase
+from odoo.tests import Form, TransactionCase
 
 
 class TestResUsers(TransactionCase):
@@ -37,3 +37,19 @@ class TestResUsers(TransactionCase):
         jean_paul = self.users[1]
         user_ids = [id_ for id_, __ in ResUsers.with_user(jean_paul).name_search('Jean')]
         self.assertEqual(jean_paul.id, user_ids[0], "The current user, Jean-Paul, should be the first in the result")
+
+    def test_change_password(self):
+        '''
+        We should be able to change user password without any issue
+        '''
+        user_internal = self.env['res.users'].create({
+            'name': 'Internal',
+            'login': 'user_internal',
+            'password': 'password',
+            'groups_id': [self.env.ref('base.group_user').id],
+        })
+        with Form(self.env['change.password.wizard'].with_context(active_model='res.users', active_ids=user_internal.ids), view='base.change_password_wizard_view') as form:
+            with form.user_ids.edit(0) as line:
+                line.new_passwd = 'bla'
+        rec = form.save()
+        rec.change_password_button()
