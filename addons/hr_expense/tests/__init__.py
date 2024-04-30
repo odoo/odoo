d4d86632ffd5539afdefad619f19cf8e530f5a0c
Julien Alardot (jual) <jual@odoo.com>
2024-04-30 17:06:51 +0200

    [IMP] hr_expense: Improve tests for expense (sheets) states
    In 92ed88a, we added a fix and tests on hr.expense & hr.expense.sheet
    state fields. This commit forward ports these tests as they are
    interesting to keep in later versions.

--- addons/hr_expense/tests/__init__.py
+++ addons/hr_expense/tests/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import common
@@ -7,3 +6,4 @@ from . import test_expenses_access_rights
 from . import test_expenses_mail_import
 from . import test_expenses_multi_company
 from . import test_expenses_standard_price_update_warning
+from . import test_expenses_states
