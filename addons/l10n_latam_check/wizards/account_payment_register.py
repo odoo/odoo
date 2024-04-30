be1237af0f22c2931d6e9ff58a211325ac6e6482
Martin Quinteros <maq@adhoc.com.ar>
2024-04-30 17:40:45 -0300

    [IMP] Update payment register method in witholding module

--- addons/l10n_latam_check/wizards/account_payment_register.py
+++ addons/l10n_latam_check/wizards/account_payment_register.py
@@ -10,14 +10,22 @@ class AccountPaymentRegister(models.TransientModel):
         string='Checks',
     )
 
-    @api.depends('l10n_latam_new_check_ids', 'l10n_latam_new_check_ids.amount', 'l10n_latam_check_ids')
+    @api.depends('l10n_latam_check_ids.amount', 'l10n_latam_new_check_ids.amount', 'payment_method_code')
     def _compute_amount(self):
         super()._compute_amount()
-        # TODO: filter by new_third_party_checks
-        for wizard in self.filtered('l10n_latam_new_check_ids'):
+        for wizard in self.filtered(lambda x: x._is_latam_check_payment(check_subtype='new_check')):
             wizard.amount = sum(wizard.l10n_latam_new_check_ids.mapped('amount'))
-        for wizard in self.filtered('l10n_latam_check_ids'):
+        for wizard in self.filtered(lambda x: x._is_latam_check_payment(check_subtype='move_check')):
             wizard.amount = sum(wizard.l10n_latam_check_ids.mapped('amount'))
+
+    def _is_latam_check_payment(self, check_subtype=False):
+        if check_subtype == 'move_check':
+            codes = ['in_third_party_checks', 'out_third_party_checks']
+        elif check_subtype == 'new_check':
+            codes = ['new_third_party_checks', 'own_checks']
+        else:
+            codes = ['in_third_party_checks', 'out_third_party_checks', 'new_third_party_checks', 'own_checks']
+        return self.payment_method_code in codes
 
     def _create_payment_vals_from_wizard(self, batch_result):
         vals = super()._create_payment_vals_from_wizard(batch_result)
