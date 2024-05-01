d4db5f57a0480fb9e687489d5f8a780aa89cc76d
Diego <sandoval.vargas.diego.1c@gmail.com>
2024-04-30 18:26:57 -0600

    chore: se actulizan estilos

--- talent360/evaluaciones/models/evaluacion_clima.py
+++ talent360/evaluaciones/models/evaluacion_clima.py
@@ -14,67 +14,69 @@ class EvaluacionClima(models.Model):
     _inherit = ["evaluacion"]
 
     piso_rojo = fields.Float(
-        string="Piso Rojo",
         required=False,
         default=0,
         store=False,
     )
 
     techo_rojo = fields.Float(
-        string="Techo Rojo",
         required=True,
-        default=1,
+        default=50,
+    )
+
+    techo_naranja = fields.Float(
+        required=True,
+        default=70,
     )
 
     techo_amarillo = fields.Float(
-        string="Techo Amarillo",
         required=True,
-        default=2,
+        default=80,
     )
 
     techo_verde = fields.Float(
-        string="Techo Verde",
         required=True,
-        default=3,
+        default=90,
     )
 
     techo_azul = fields.Float(
-        string="Techo Azul",
         required=True,
-        default=4,
+        default=100,
     )
 
     descripcion_rojo = fields.Text(
-        string="Descripción Rojo",
+        required=False,
+        default=None,
+    )
+
+    descripcion_naranja = fields.Text(
         required=False,
         default=None,
     )
 
     descripcion_amarillo = fields.Text(
-        string="Descripción Amarillo",
         required=False,
         default=None,
     )
 
     descripcion_verde = fields.Text(
-        string="Descripción Verde",
         required=False,
         default=None,
     )
 
     descripcion_azul = fields.Text(
-        string="Descripción Azul",
         required=False,
         default=None,
     )
 
-    @api.constrains("techo_rojo", "techo_amarillo", "techo_verde", "techo_azul")
+    @api.constrains("techo_rojo", "techo_naranja", "techo_amarillo", "techo_verde", "techo_azul")
     def _check_techos(self):
         """
         Valida que los techos sean mayores a 0 y estén en orden ascendente.
         """
         techos = [
             ('rojo', self.techo_rojo),
+            ('naranja', self.techo_naranja),
             ('amarillo', self.techo_amarillo),
             ('verde', self.techo_verde),
             ('azul', self.techo_azul),
@@ -83,9 +85,9 @@ class EvaluacionClima(models.Model):
         for techo in techos:
             if techo[1] <= 0:
                 raise ValidationError(
-                    (f"El techo {techo[0]} debe ser mayor a 0"))
+                    (f"El nivel {techo[0]} debe ser mayor a 0"))
 
         for i in range(len(techos) - 1):
             if techos[i][1] >= techos[i + 1][1]:
                 raise ValidationError(
-                    ("Los techos deben ser en orden ascendente"))
+                    ("Los niveles deben ser en orden ascendente"))
