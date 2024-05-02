5fd42993fd7a2e5970843694ff7add0c1fac695b
Valentin Chevalier <vcr@odoo.com>
2024-05-02 19:05:18 +0200

    [FIX] payment_stripe: support outlying territories with Stripe
    Before this commit, outlying territories supported by Stripe couldn't
    create an account using Stripe Connect nor use express checkout. The
    problem is that Stripe asks outlying territories to configure their
    account using their parent territory as a Country.
    
    Now, Odoo will automatically use the parent territory for outlying
    territories supported by Stripe, enabling Stripe Connect and Express
    Checkout for them.
    
    opw-3867753
    opw-3865198

--- addons/website_payment/models/res_country.py
+++ addons/website_payment/models/res_country.py
@@ -1,7 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
-from odoo.addons.payment_stripe.const import SUPPORTED_COUNTRIES as STRIPE_SUPPORTED_COUNTRIES
+
+from odoo.addons.payment_stripe import const
 
 
 class ResCountry(models.Model):
@@ -12,4 +13,6 @@ class ResCountry(models.Model):
     @api.depends('code')
     def _compute_is_stripe_supported_country(self):
         for country in self:
-            country.is_stripe_supported_country = country.code in STRIPE_SUPPORTED_COUNTRIES
+            country.is_stripe_supported_country = const.COUNTRY_MAPPING.get(
+                country.code, country.code
+            ) in const.SUPPORTED_COUNTRIES
