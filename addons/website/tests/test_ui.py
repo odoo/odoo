e65ab8ec474e069668f68cedea583ea423ebc422
Robin Lejeune (role) <role@odoo.com>
2024-05-02 12:42:38 +0200

    [FIX] website: remove external test from standard
    [Commit 1] introduced an external test, but it should have been marked
    with '-standard' to remove it from the standard tests and act as
    expected (i.e. only be launched in nightly builds as it calls an
    external API).
    
    [Commit 1]: https://github.com/odoo/odoo/commit/745c378eb456f1d19ec52ac7b0ecd0e47331ac7b
    
    related to task-3798504
    
    closes odoo/odoo#164155
    
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -154,7 +154,7 @@ class TestUiHtmlEditor(HttpCaseWithUserDemo):
         self.start_tour("/", 'website_media_dialog_undraw', login='admin')
 
 
-@odoo.tests.tagged('external', '-at_install', 'post_install')
+@odoo.tests.tagged('external', '-standard', '-at_install', 'post_install')
 class TestUiHtmlEditorWithExternal(HttpCaseWithUserDemo):
     def test_media_dialog_external_library(self):
         self.start_tour("/", 'website_media_dialog_external_library', login='admin')
