69a184461945165760f75269aeaa5a91b4f350be
Anita (anko) <anko@odoo.com>
2024-05-02 16:32:37 +0200

    [FIX] website_sale_loyalty: recalculate reward after delivery deletion
    If user claimed ewallet during delivery step and came back to cart,
    it would still have discount based on order amount with delivery included
    even though delivery line has been removed from Sale Order which would
    result in negative total. Now, discount is recalculated when delivery
    line is deleted.

--- addons/website_sale_loyalty/controllers/delivery.py
+++ addons/website_sale_loyalty/controllers/delivery.py
@@ -11,9 +11,9 @@ class WebsiteSaleLoyaltyDelivery(WebsiteSaleDelivery):
         result = super()._update_website_sale_delivery_return(order, **post)
         if order:
             free_shipping_lines = order._get_free_shipping_lines()
+            Monetary = request.env['ir.qweb.field.monetary']
+            currency = order.currency_id
             if free_shipping_lines:
-                Monetary = request.env['ir.qweb.field.monetary']
-                currency = order.currency_id
                 amount_free_shipping = sum(free_shipping_lines.mapped('price_subtotal'))
                 result.update({
                     'new_amount_delivery_discount': Monetary.value_to_html(
@@ -24,4 +24,8 @@ class WebsiteSaleLoyaltyDelivery(WebsiteSaleDelivery):
                         amount_free_shipping, currency
                     ),
                 })
+            else:
+                result.update({'new_amount_order_discounted': Monetary.value_to_html(
+                    order.reward_amount, {'display_currency': currency}
+                )})
         return result
