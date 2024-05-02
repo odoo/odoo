87ab9688c4439a1437cdb59eb99380a9d4a97987
Sanjay Sharma <shsa@odoo.com>
2024-05-02 17:34:50 +0530

    [IMP] website_event_booth_sale: transition from jQuery to vanilla JS
    Specification:
    
    This commit focuses on transitioning all frontend code from jQuery to
    Vanilla JavaScript. The changes specifically target files in the
    web.assets_frontend.
    
    task-3930517

--- addons/website_event_booth_sale/static/src/js/booth_register.js
+++ addons/website_event_booth_sale/static/src/js/booth_register.js
@@ -18,7 +18,7 @@ BoothRegistration.include({
     },
 
     _onChangeBoothType(ev) {
-        this.categoryPrice = parseFloat($(ev.currentTarget).data('price'));
+        this.categoryPrice = parseFloat(ev.currentTarget.dataset.price);
         return this._super.apply(this, arguments);
     },
 
@@ -29,8 +29,8 @@ BoothRegistration.include({
      */
     _updateUiAfterBoothChange(boothCount) {
         this._super.apply(this, arguments);
-        let $elem = this.$('.o_wbooth_booth_total_price');
-        $elem.toggleClass('d-none', !boothCount || !this.categoryPrice);
+        const boothTotalPriceEl = this.el.querySelector(".o_wbooth_booth_total_price");
+        boothTotalPriceEl?.classList.toggle("d-none", !boothCount || !this.categoryPrice);
         this._updatePrice(boothCount);
     },
 
@@ -39,8 +39,10 @@ BoothRegistration.include({
     //--------------------------------------------------------------------------
 
     _updatePrice(boothsCount) {
-        let $elem = this.$('.o_wbooth_booth_total_price .oe_currency_value');
-        $elem.text(boothsCount * this.categoryPrice);
+        const boothCurrencyEl = this.el.querySelector(".o_wbooth_booth_total_price .oe_currency_value");
+        if (boothCurrencyEl) {
+            boothCurrencyEl.textContent = `${boothsCount * this.categoryPrice}`;
+        }
     },
 
 });
