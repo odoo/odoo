a294aaf03db10398c48845f10a5374b4399d4932
Luis Torres <luis_t@vauxoo.com>
2024-05-03 05:22:28 +0000

    [IMP] l10n_pe: Welcome bank code field for PE
    The bank code will be used on PLE reports to identify the bank
    institution.
    
    Data for Peruvian banks was completed with the new field.
    
    Extra, based on MX module, added field to show PE fields only for
    Peruvian companies

--- addons/l10n_pe/models/__init__.py
+++ addons/l10n_pe/models/__init__.py
@@ -3,6 +3,7 @@ from . import template_pe
 from . import account_tax
 from . import account_move
 from . import l10n_latam_identification_type
+from . import res_bank
 from . import res_partner
 from . import res_city_district
 from . import res_city
---
+++ addons/l10n_pe/models/res_bank.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResBank(models.Model):
+    _inherit = 'res.bank'
+
+    l10n_pe_edi_code = fields.Char(
+        'Code (PE)',
+        help='Bank code assigned by the SUNAT to identify banking institutions.')
+    country_code = fields.Char(related='country.code', string='Country Code')
