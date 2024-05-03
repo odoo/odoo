7b1f532385e59f9ae078d88a073b3753dba4f59a
Thibault Delavallée <tde@odoo.com>
2024-05-03 17:01:40 +0200

    [FIX] utm: avoid utm name increment when updating with same name
    When update happens on records and updating value is the same as already
    assigned value, [2] gets appended to the value, because write function
    doesn't take into account the written value.
    
    Task-3837272 (#159699) Incorrect UTM source enumeration
    
    Part-of: odoo/odoo#164287
    Co-authored-by: Dato Nefaridze <dane@odoo.com>

--- addons/utm/models/utm_source.py
+++ addons/utm/models/utm_source.py
@@ -72,10 +72,17 @@ class UtmSourceMixin(models.AbstractModel):
         return super().create(vals_list)
 
     def write(self, values):
+        if (values.get(self._rec_name) or values.get('name')) and len(self) > 1:
+            raise ValueError(
+                _('You cannot update multiple records with the same name. The name should be unique!')
+            )
+
         if values.get(self._rec_name) and not values.get('name'):
             values['name'] = self.env['utm.source']._generate_name(self, values[self._rec_name])
         if values.get('name'):
-            values['name'] = self.env['utm.mixin']._get_unique_names("utm.source", [values['name']])[0]
+            values['name'] = self.env['utm.mixin'].with_context(
+                utm_check_skip_record_ids=self.source_id.ids
+            )._get_unique_names("utm.source", [values['name']])[0]
 
         super().write(values)
 
