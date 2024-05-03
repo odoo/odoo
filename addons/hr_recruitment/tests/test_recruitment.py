odoo/odoo

commit 7774822c0ba9edf979cdc741a6c465a8e84e4da6

[IMP] hr_recruitment: sync mobile/phone number to partners
In this commit, the mobile/phone number is in sync with the partner
if the partner is not there then it will be generated based on the applicant
information

Task-3635976

closes odoo/odoo#150820

Signed-off-by: Sofie Gvaladze (sgv) <sgv@odoo.com>

addons/hr_recruitment/models/hr_applicant.py
@@ -65,10 +65,10 @@ class Applicant(models.Model):
     availability = fields.Date("Availability", help="The date at which the applicant will be available to start working", tracking=True)
     partner_name = fields.Char("Applicant's Name")
     partner_phone = fields.Char("Phone", size=32, compute='_compute_partner_phone_email',
-        store=True, readonly=False, index='btree_not_null')
+        store=True, readonly=False, index='btree_not_null', inverse='_inverse_partner_email')
     partner_phone_sanitized = fields.Char(string='Sanitized Phone Number', compute='_compute_partner_phone_sanitized', store=True, index='btree_not_null')
     partner_mobile = fields.Char("Mobile", size=32, compute='_compute_partner_phone_email',
-        store=True, readonly=False, index='btree_not_null')
+        store=True, readonly=False, index='btree_not_null', inverse='_inverse_partner_email')
     partner_mobile_sanitized = fields.Char(string='Sanitized Mobile Number', compute='_compute_partner_mobile_sanitized', store=True, index='btree_not_null')
     type_id = fields.Many2one('hr.recruitment.degree', "Degree")
     department_id = fields.Many2one(
@@ -314,8 +314,23 @@ class Applicant(models.Model):
                 applicant.partner_mobile = applicant.partner_id.mobile
 
     def _inverse_partner_email(self):
-        for applicant in self.filtered(lambda a: a.partner_id and a.email_from):
-            applicant.partner_id.email = applicant.email_from
+        for applicant in self:
+            if not applicant.email_from:
+                continue
+            if not applicant.partner_id:
+                if not applicant.partner_name:
+                    raise UserError(_('You must define a Contact Name for this applicant.'))
+                applicant.partner_id = self.env['res.partner'].create({
+                    'is_company': False,
+                    'name': applicant.partner_name,
+                    'email': applicant.email_from,
+                    'mobile': applicant.partner_mobile,
+                    'phone': applicant.partner_phone,
+                })
+            else:
+                applicant.partner_id.email = applicant.email_from
+                applicant.partner_id.mobile = applicant.partner_mobile
+                applicant.partner_id.phone = applicant.partner_phone
 
     @api.depends('partner_phone')
     def _compute_partner_phone_sanitized(self):

addons/hr_recruitment/tests/test_recruitment.py
@@ -12,14 +12,17 @@ class TestRecruitment(TransactionCase):
         dup1, dup2, no_dup = self.env['hr.applicant'].create([
             {
                 'name': 'Application 1',
+                'partner_name': 'Application 1',
                 'email_from': 'laurie.poiret@aol.ru',
             },
             {
                 'name': 'Application 2',
+                'partner_name': 'Application 2',
                 'email_from': 'laurie.POIRET@aol.ru',
             },
             {
                 'name': 'Application 3',
+                'partner_name': 'Application 3',
                 'email_from': 'laure.poiret@aol.ru',
             },
         ])
@@ -35,37 +38,44 @@ class TestRecruitment(TransactionCase):
             {
                 'active': False,  # Refused/archived application should still count
                 'name': 'Application A',
+                'partner_name': 'Application A',
                 'email_from': 'abc@odoo.com',
                 'partner_phone': '123',
                 'partner_mobile': '14-15-16',
             },
             {
                 'name': 'Application B',
+                'partner_name': 'Application B',
                 'partner_phone': '456',
                 'partner_mobile': '11-12-13',
             },
             {
                 'name': 'Application C',
+                'partner_name': 'Application C',
                 'email_from': 'def@odoo.com',
                 'partner_phone': '123',
                 'partner_mobile': '14-15-16',
             },
             {
                 'name': 'Application D',
+                'partner_name': 'Application D',
                 'email_from': 'def@odoo.com',
                 'partner_phone': '456',
                 'partner_mobile': '14-15-16',
             },
             {
                 'name': 'Application E',
+                'partner_name': 'Application E',
                 'partner_phone': '',
             },
             {
                 'name': 'Application F',
+                'partner_name': 'Application F',
                 'partner_phone': '11-12-13', # In case phone is configured in a wrong field
             },
             {
                 'name': 'Application G',
+                'partner_name': 'Application G',
                 'partner_phone': '',
             },
         ])

commit e1ab53b79ff476f4ad4eba23df621453d2255af9

[FIX] hr_recruitment: avoid duplicating partners
Current behavior:
When applying for a job offer, if the partner already
exists, it will create a new one anyway.

Steps to reproduce:
1. install website_hr_recruitment,contacts
2. Go to contacts => only one admin partner
3. Go to Recruitment, select a job offer
4. Click on Job Page, Apply Now!
5. The form should be prefilled with admin info
6. Add missing information (LinkedIn and resume)
7. Click on I'm feeling lucky
8. Go to contacts => two admin partners

Cause of the issue:
Introduced by: https://github.com/odoo/odoo/commit/7774822c0ba9edf979cdc741a6c465a8e84e4da6
When creating an applicant, partner_id is False

opw-3837388

addons/hr_recruitment/models/hr_applicant.py
@@ -320,23 +320,17 @@ class Applicant(models.Model):
             if not applicant.partner_id:
                 if not applicant.partner_name:
                     raise UserError(_('You must define a Contact Name for this applicant.'))
-                applicant.partner_id = self.env['res.partner'].create({
-                    'is_company': False,
-                    'name': applicant.partner_name,
-                    'email': applicant.email_from,
-                    'mobile': applicant.partner_mobile,
-                    'phone': applicant.partner_phone,
-                })
-            else:
-                if applicant.email_from and \
-                    tools.email_normalize(applicant.email_from) != tools.email_normalize(applicant.partner_id.email):
-                    # change email on a partner will trigger other heavy code, so avoid to change the email when
-                    # it is the same. E.g. "email@example.com" vs "My Email" <email@example.com>""
-                    applicant.partner_id.email = applicant.email_from
-                if applicant.partner_mobile:
-                    applicant.partner_id.mobile = applicant.partner_mobile
-                if applicant.partner_phone:
-                    applicant.partner_id.phone = applicant.partner_phone
+                applicant.partner_id = self.env['res.partner'].find_or_create(applicant.email_from)
+            if applicant.partner_name and not applicant.partner_id.name:
+                applicant.partner_id.name = applicant.partner_name
+            if tools.email_normalize(applicant.email_from) != tools.email_normalize(applicant.partner_id.email):
+                # change email on a partner will trigger other heavy code, so avoid to change the email when
+                # it is the same. E.g. "email@example.com" vs "My Email" <email@example.com>""
+                applicant.partner_id.email = applicant.email_from
+            if applicant.partner_mobile:
+                applicant.partner_id.mobile = applicant.partner_mobile
+            if applicant.partner_phone:
+                applicant.partner_id.phone = applicant.partner_phone
 
     @api.depends('partner_phone')
     def _compute_partner_phone_sanitized(self):

addons/hr_recruitment/tests/test_recruitment.py
@@ -86,3 +86,21 @@ class TestRecruitment(TransactionCase):
         self.assertEqual(D.application_count, 3) # A, B, C
         self.assertEqual(E.application_count, 0) # Should not match with G
         self.assertEqual(F.application_count, 1) # B
+
+    def test_application_no_partner_duplicate(self):
+        """ Test that when applying, the existing partner
+            doesn't get duplicated.
+        """
+        applicant_data = {
+            'name': 'Test - CEO',
+            'partner_name': 'Test',
+            'email_from': 'test@thisisatest.com',
+        }
+        # First application, a partner should be created
+        self.env['hr.applicant'].create(applicant_data)
+        partner_count = self.env['res.partner'].search_count([('email', '=', 'test@thisisatest.com')])
+        self.assertEqual(partner_count, 1)
+        # Second application, no partner should be created
+        self.env['hr.applicant'].create(applicant_data)
+        partner_count = self.env['res.partner'].search_count([('email', '=', 'test@thisisatest.com')])
+        self.assertEqual(partner_count, 1)