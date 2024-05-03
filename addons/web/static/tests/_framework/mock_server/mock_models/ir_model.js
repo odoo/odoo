d4657414e54a8da33d3478810bde9e07d90990f4
Rahul Prajapati <rapr@odoo.com>
2024-05-03 15:12:36 +0530

    [IMP] web: add a default record in ir_model mock model
    Before this commit:
    
    There was no default model record in the `ir.model` mock model.
    Which was causing issues in some test cases.
    
    After this commit:
    
    Added a default `ir.model` record of `res.partner`
    in `ir.model` mock model definition.
    
    Part of task-3818666
    
    X-original-commit: e2efd114d42afbe88fe7e3e76aa6c34622c90cef

--- addons/web/static/tests/_framework/mock_server/mock_models/ir_model.js
+++ addons/web/static/tests/_framework/mock_server/mock_models/ir_model.js
@@ -2,4 +2,12 @@ import { ServerModel } from "../mock_model";
 
 export class IrModel extends ServerModel {
     _name = "ir.model";
+
+    _records = [
+        {
+            id: 1,
+            model: "res.partner",
+            name: "Partner",
+        },
+    ];
 }
