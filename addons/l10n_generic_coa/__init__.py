bd6b82326bca921be07f93c8732d4c242747c19f
Obay Abdelgadir <obayit@gmail.com>
2024-05-04 12:36:52 +0300

    [REF] l10n_generic_coa: remove remaining __init__.py
    closes odoo/odoo#164611
    
    X-original-commit: de8f82b883829c250292c2ad0d8710e0ddbb7911
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/l10n_generic_coa/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-
-def uninstall_hook(env):
-    env.cr.execute(
-        "DELETE FROM ir_model_data WHERE module = 'l10n_generic_coa'"
-    )
