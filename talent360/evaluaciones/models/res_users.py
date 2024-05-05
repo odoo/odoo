722b14b0a00f6979f262616e87d417524a0459b3
danfupo03 <danfupo@outlook.com>
2024-05-05 11:50:16 -0600

    feat(Respuestas):  Redirecciona a una vista con las respuestas

--- talent360/evaluaciones/models/res_users.py
+++ talent360/evaluaciones/models/res_users.py
@@ -17,22 +17,14 @@ class Users(models.Model):
 
         evaluacion_id = self._context.get("current_evaluacion_id")
 
-        # Obtener el modelo de la relación entre evaluacion y usuarios
-        usuario_evaluacion_rel_model = self.env["usuario.evaluacion.rel"]
-    
-        # Crear o actualizar un registro en la relación
-        usuario_evaluacion_rel = usuario_evaluacion_rel_model.create({
-            "evaluacion_id": evaluacion_id,
-            "contestada": "Contestada"  # Establecer el valor para el campo "Contestada" como una de las opciones disponibles
-        })
-
         return {
             "type": "ir.actions.act_window",
-            "name": "Respuesta usuario",
-            "res_model": "repuesta",
-            "view_mode": "kanban",
+            "name": "respuesta_usuario",
+            "res_model": "respuesta",
+            "view_mode": "tree",
             "domain": [
                 ("evaluacion_id", "=", evaluacion_id),
+                ("usuario_id", "=", self.id),
                 ],
             "context": {"group_by": "respuesta_texto"},
         }
--- talent360/evaluaciones/models/respuesta.py
+++ talent360/evaluaciones/models/respuesta.py
@@ -6,7 +6,7 @@ class Respuesta(models.Model):
     _description = "Respuesta a una pregunta"
 
     pregunta_id = fields.Many2one("pregunta", string="Preguntas")
-    user_id = fields.Many2one("res.users", string="Usuario")
+    usuario_id = fields.Many2one("res.users", string="Usuario")
     evaluacion_id = fields.Many2one("evaluacion", string="Evaluacion")
 
     respuesta_texto = fields.Char("Respuesta", required=True)
