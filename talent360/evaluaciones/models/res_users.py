3a59d5fdeb9c1cfed1cc2baea10b8b177ad1d724
danfupo03 <danfupo@outlook.com>
2024-05-04 20:47:39 -0600

    feat(Respuestas): Función que redirecciona vista (No funciona)

--- talent360/evaluaciones/models/res_users.py
+++ talent360/evaluaciones/models/res_users.py
@@ -12,3 +12,27 @@ class Users(models.Model):
         "evaluacion_id",
         string="Evaluaciones",
     )
+
+    def ver_respuestas_usuario(self):
+
+        evaluacion_id = self._context.get("current_evaluacion_id")
+
+        # Obtener el modelo de la relación entre evaluacion y usuarios
+        usuario_evaluacion_rel_model = self.env["usuario.evaluacion.rel"]
+    
+        # Crear o actualizar un registro en la relación
+        usuario_evaluacion_rel = usuario_evaluacion_rel_model.create({
+            "evaluacion_id": evaluacion_id,
+            "contestada": "Contestada"  # Establecer el valor para el campo "Contestada" como una de las opciones disponibles
+        })
+
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Respuesta usuario",
+            "res_model": "repuesta",
+            "view_mode": "kanban",
+            "domain": [
+                ("evaluacion_id", "=", evaluacion_id),
+                ],
+            "context": {"group_by": "respuesta_texto"},
+        }
