7791ddbed5e4511dc0ad176b3c9c26a003f724cc
ricardo020202 <ricoroca2@hotmail.com>
2024-05-04 21:08:32 -0600

    feat(evaluaciones): add logic for conditinal questions

--- talent360/evaluaciones/static/src/js/handle_conditional_question.js
+++ talent360/evaluaciones/static/src/js/handle_conditional_question.js
@@ -11,34 +11,67 @@ document.querySelectorAll('input[name="preguntas_desbloqueadas"]').forEach(funct
     });
 });
 
+// // Escucha los cambios en los elementos de entrada
+// document.querySelectorAll('.o_survey_form_choice_item').forEach(function(input) {
+//     input.addEventListener('change', function(event) {
+//         var userAnswer = this.value;
+//         var respuesta_trigger = document.querySelector('input[name="respuesta_trigger"]').value;
+
+//         // Compara la respuesta del usuario con respuesta_trigger
+//         if (userAnswer == respuesta_trigger) {
+//             document.querySelectorAll('input[name="preguntas_desbloqueadas"]').forEach(function(element) {
+//                 var preguntas_desbloqueadas = element.value.replace(/[\[\]]/g, '').split(',');
+//                 unlockAdditionalQuestions(preguntas_desbloqueadas);
+//             });
+//         } else {
+//             document.querySelectorAll('input[name="preguntas_desbloqueadas"]').forEach(function(element) {
+//                 var preguntas_desbloqueadas = element.value.replace(/[\[\]]/g, '').split(',');
+//                 preguntas_desbloqueadas.forEach(function(pregunta) {
+//                     var preguntaElement = document.querySelector("[id='" + pregunta.trim() + "']");
+//                     if (preguntaElement) {
+//                         preguntaElement.style.display = "none"
+//                         preguntaElement.querySelectorAll('input[required], textarea[required], select[required]').forEach(function(field) {
+//                             field.removeAttribute('required');
+//                         });
+//                     }
+//                 });
+//             });
+//         }
+//     });
+// });
+
 // Escucha los cambios en los elementos de entrada
 document.querySelectorAll('.o_survey_form_choice_item').forEach(function(input) {
     input.addEventListener('change', function(event) {
-        var userAnswer = event.target.value;
-        var respuesta_trigger = document.querySelector('input[name="respuesta_trigger"]').value;
+        var userAnswer = this.value; // Obtener la respuesta del usuario
+        // var preguntaId = this.dataset.preguntaId; // Obtener el ID de la pregunta
+        var preguntaId = this.name.split('_')[0]; // Obtener el ID de la pregunta
+        var preguntaElement = document.getElementById(preguntaId); // Obtener el elemento de la pregunta
 
-        // Compara la respuesta del usuario con respuesta_trigger
-        if (userAnswer == respuesta_trigger) {
-            document.querySelectorAll('input[name="preguntas_desbloqueadas"]').forEach(function(element) {
-                var preguntas_desbloqueadas = element.value.replace(/[\[\]]/g, '').split(',');
+        console.log(userAnswer, preguntaId, preguntaElement);
+        if (preguntaElement && preguntaElement.dataset.respuestaTrigger) {
+            var respuesta_trigger = preguntaElement.dataset.respuestaTrigger; // Obtener la respuesta trigger de la pregunta
+            var preguntas_desbloqueadas = preguntaElement.dataset.preguntasDesbloqueadas.replace(/[\[\]]/g, '').split(','); // Obtener las preguntas desbloqueadas de la pregunta
+
+            console.log(respuesta_trigger, preguntas_desbloqueadas);
+            // Compara la respuesta del usuario con respuesta_trigger
+            if (userAnswer == respuesta_trigger) {
                 unlockAdditionalQuestions(preguntas_desbloqueadas);
-            });
-        } else {
-            document.querySelectorAll('input[name="preguntas_desbloqueadas"]').forEach(function(element) {
-                var preguntas_desbloqueadas = element.value.replace(/[\[\]]/g, '').split(',');
-                preguntas_desbloqueadas.forEach(function(pregunta) {
-                    var preguntaElement = document.querySelector("[id='" + pregunta.trim() + "']");
-                    if (preguntaElement) {
-                        preguntaElement.style.display = "none"
-                        preguntaElement.querySelectorAll('input[required], textarea[required], select[required]').forEach(function(field) {
+            } else {
+                preguntas_desbloqueadas.forEach(function(preguntaDesbloqueadaId) {
+                    var preguntaDesbloqueadaElement = document.getElementById(preguntaDesbloqueadaId);
+                    if (preguntaDesbloqueadaElement) {
+                        preguntaDesbloqueadaElement.style.display = "none";
+                        preguntaDesbloqueadaElement.querySelectorAll('input[required], textarea[required], select[required]').forEach(function(field) {
                             field.removeAttribute('required');
                         });
                     }
                 });
-            });
+            }
         }
     });
 });
+
 
 function unlockAdditionalQuestions(preguntas) {
     // Desbloquea las preguntas adicionales
