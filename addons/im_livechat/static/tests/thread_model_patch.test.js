e76afa810624df4c03094ef3af524c1754042756
Didier (did) <did@odoo.com>
2024-05-06 09:32:55 +0200

    [IMP] im_livechat: add test for custom username
    closes odoo/odoo#164493
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/im_livechat/static/tests/thread_model_patch.test.js
+++ addons/im_livechat/static/tests/thread_model_patch.test.js
@@ -1,5 +1,5 @@
 import { describe, test } from "@odoo/hoot";
-import { click, contains, openDiscuss, start, startServer } from "@mail/../tests/mail_test_helpers";
+import { click, contains, insertText, openDiscuss, start, startServer } from "@mail/../tests/mail_test_helpers";
 import { Command, serverState } from "@web/../tests/web_test_helpers";
 import { defineLivechatModels } from "./livechat_test_helpers";
 
@@ -35,4 +35,26 @@ test("Thread name unchanged when inviting new users", async () => {
     await click("button[title='Show Member List']");
     await contains(".o-discuss-ChannelMember", { text: "James" });
     await contains(".o-mail-Discuss-threadName[title='Visitor #20']");
+});
+
+test("Display livechat custom username if defined", async () => {
+    const pyEnv = await startServer();
+    pyEnv["res.partner"].write(serverState.partnerId, {
+        user_livechat_username: "livechat custom username",
+    });
+    const guestId = pyEnv["mail.guest"].create({ name: "Visitor #20" });
+    const channelId = pyEnv["discuss.channel"].create({
+        anonymous_name: "Visitor #20",
+        channel_member_ids: [
+            Command.create({ partner_id: serverState.partnerId }),
+            Command.create({ guest_id: guestId }),
+        ],
+        channel_type: "livechat",
+        livechat_operator_id: serverState.partnerId,
+    });
+    await start();
+    await openDiscuss(channelId);
+    await insertText(".o-mail-Composer-input", "hello");
+    await click(".o-mail-Composer-send:enabled");
+    await contains(".o-mail-Message-author", { text: "livechat custom username" });
 });
