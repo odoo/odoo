25fb1307e4d92d730973f8a03790db21379e5a57
Aurélien Warnon <awa@odoo.com>
2024-05-06 15:39:51 +0200

    [REF] website_event_exhibitor: rework crawler prevention
    This commit is mostly undoing what was done here:
    https://github.com/odoo/odoo/commit/e17e07030db3e4e9d5907cc00905e224190e5d25
    
    In favor of a more common and easy technique which is to use the "post_link"
    frontend Widget, that achieves the same goal of disguising these links.
    
    This has the advantage of aligning the use cases of "fixing" unnecessary hits
    of crawler bots with other addons but also removes a lot of custom JS code.
    
    Note: simply using rel="nofollow" has proven inefficient as bots will likely
    follow these links anyway, but pounder them differently.
    
    We also used this opportunity to fix an untranslated placeholder.
    
    Task-3908389
    
    X-original-commit: a7b96fece4768088bf88a45f89b38d195a28858d
    Part-of: odoo/odoo#172991
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_event_exhibitor/__manifest__.py
+++ addons/website_event_exhibitor/__manifest__.py
@@ -36,7 +36,6 @@
             'website_event_exhibitor/static/src/scss/event_templates_sponsor.scss',
             'website_event_exhibitor/static/src/scss/event_exhibitor_templates.scss',
             'website_event_exhibitor/static/src/js/event_exhibitor_connect.js',
-            'website_event_exhibitor/static/src/js/event_sponsor_search.js',
             'website_event_exhibitor/static/src/components/exhibitor_connect_closed_dialog/**/*',
         ],
         'web.report_assets_common': [
