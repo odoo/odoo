3ad2e260eac5142d40c18c03a13dd62bd9481705
bt-ojossen <olivier.jossen@braintec.com>
2024-05-06 13:51:23 +0200

    [IMP] account,*: performance on Foreign Keys
    See https://github.com/odoo/odoo/pull/143852 for backport

--- addons/account/models/account_bank_statement_line.py
+++ addons/account/models/account_bank_statement_line.py
@@ -45,6 +45,7 @@ class AccountBankStatementLine(models.Model):
         comodel_name='account.move',
         auto_join=True,
         string='Journal Entry', required=True, readonly=True, ondelete='cascade',
+        index=True,
         check_company=True)
     statement_id = fields.Many2one(
         comodel_name='account.bank.statement',
