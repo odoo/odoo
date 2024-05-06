cfb797d97e12cffcdc9f386a9d5bcd28d791bbc1
aamo-odoo <aamo@odoo.com>
2024-05-06 19:22:36 +0530

    [FIX] pos_self_order: prevent error when opening mobile view
    This error usually occurs when there are no active orders present, and we've
    closed the register and then tried to access the 'Mobile View' feature.
    
    Steps to reproduce:
    
    - Install the 'pos_restaurant' and 'point_of_sale' modules
    - Make sure there are no active orders in the restaurant
    - Dashboard > Open Register > Open session > Close Register
    - Dashboard > Restaurant > Dropdown Menu > Mobile View
    - An Error will be generated in the terminal
    
    Traceback : ValueError
    bus.Bus only string channels are allowed.
    
    This situation arises because we have two self-ordering modes: i) 'Mobile'
    and ii) 'kiosk'. When the condition at [1] is evaluated, it will become false
    because the session has been closed, causing "pos_config.has_active_session" to
    be false. Additionally, since the mode is 'Mobile', the condition in the 'elif'
    statement at [1] also evaluates to false. As a result, we will get the value of
    'config_access_token' as a boolean value from line [2] because the value of
    'config_access_token' will not be set from both of the above conditions.
    Due to 'config_access_token' being a boolean value, the value of 'channels' ends
    up being a boolean value.
    
    This commit will fix the above error by providing an empty string within
    'config_access_token', rather than boolean values. So, the value of 'channels'
    will consistently be a string.
    
    [1]: https://github.com/odoo/odoo/blob/16120774d6e7b16b44f771ca8224426e78676295/addons/pos_self_order/controllers/self_entry.py#L35-L47
    [2]: https://github.com/odoo/odoo/blob/16120774d6e7b16b44f771ca8224426e78676295/addons/pos_self_order/controllers/self_entry.py#L16C8-L23C50
    
    sentry-5136273134

--- addons/pos_self_order/controllers/self_entry.py
+++ addons/pos_self_order/controllers/self_entry.py
@@ -45,6 +45,8 @@ class PosSelfKiosk(http.Controller):
         elif pos_config.self_ordering_mode == 'kiosk':
             if config_access_token:
                 config_access_token = pos_config.access_token
+        else:
+            config_access_token = ''
 
         table = table_sudo.sudo(False).with_company(company).with_user(user) if table_sudo else False
 
