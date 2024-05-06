021f2ebd784f35b1872dc065c081b547920e46ee
Antoine (ande) <ande@odoo.com>
2024-05-06 17:49:08 +0200

    [FIX] phone_validation: colombian phone numbers
    Current behavior:
    Colombian phone numbers starting with 324 are invalid.
    
    Steps to reproduce:
    parsed = phonenumbers.parse('324 1234567', 'CO')
    is_valid = phonenumbers.is_valid_number(parsed)
    is_valid == False
    
    Cause of the issue:
    Old versions of phonenumbers (external library) are not
    up to date with the latest colombian phone system changes
    
    Fix:
    Monkey patched the library
    Similar as: https://github.com/odoo/odoo/commit/b7878038e0aca885aa174ccd74be9ffd4b393a89
    
    opw-3870579
    
    closes odoo/odoo#164792
    
    X-original-commit: 6a8bf1264b06801392cda3e401032c216af0b525
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/phone_validation/lib/phonenumbers_patch/__init__.py
+++ addons/phone_validation/lib/phonenumbers_patch/__init__.py
@@ -54,3 +54,7 @@ else:
     if parse_version(phonenumbers.__version__) < parse_version('8.12.29'):
         # https://github.com/daviddrysdale/python-phonenumbers/blob/v8.12.57/python/phonenumbers/data/region_SN.py
         phonenumbers.phonemetadata.PhoneMetadata.register_region_loader('SN', _local_load_region)
+
+    if parse_version(phonenumbers.__version__) < parse_version('8.12.39'):
+        # https://github.com/daviddrysdale/python-phonenumbers/blob/v8.13.36/python/phonenumbers/data/region_CO.py
+        phonenumbers.phonemetadata.PhoneMetadata.register_region_loader('CO', _local_load_region)
