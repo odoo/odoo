4246d6e93e8450adc727981f3bb2b8c24aebafc9
stanislaw-ladorucki <stanislaw.ladorucki@gmail.com>
2024-05-07 17:17:07 +0200

    [IMP] website_sale_stock: cart quantity getter
    Improve versatility of `_get_cart_qty` method for 'product.product' model.
    As of now, this method automatically returns zero, for all products that allow ordering when out-of-stock. This logic is only necessary in one place (to generate combination info to display in `website_sale_stock.product_availability` view), making the method effectively useless in other places, like community modules.
    This commit simplifies the `_get_cart_qty` method logic, to make it more usable, and delegates the removed check only to the `_get_additionnal_combination_info` override. This also allows the method to be used in `_get_cart_and_free_qty`, to avoid overriding cart quantity acquisition logic in multiple places.
    
    closes odoo/odoo#166609
    
    X-original-commit: 8c5996f2e3e5799acd80ebf01ab966d3f1922187
    Related: odoo/enterprise#63191
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- addons/website_sale_stock/models/sale_order.py
+++ addons/website_sale_stock/models/sale_order.py
@@ -69,7 +69,7 @@ class SaleOrder(models.Model):
         self.ensure_one()
         if not line and not product:
             return 0, 0
-        cart_qty = sum(self._get_common_product_lines(line, product).mapped('product_uom_qty'))
+        cart_qty = (product or line.product_id)._get_cart_qty()
         free_qty = (product or line.product_id).with_context(warehouse=self.warehouse_id.id).free_qty
         return cart_qty, free_qty
 
