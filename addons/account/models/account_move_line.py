odoo/odoo

commit 2d2a7729a77f8004aa046998ce75cd85280b3a6e

[FIX] account: payment error on empty default post exchange difference journal
The issue:
when you make a payment and there is an exchange difference, since the post exchange difference is not set, it will throw a traceback

To reproduce:
- Enable 2 currencies
- Have the exchange difference journal set to NULL (empty)
- Create an invoice with a different currency than the one set for the company
- then register a payment.

The fix:
Throw a user error indicating to set the post exchange difference journal

opw-3783917
opw-3768202

closes odoo/odoo#157735

Signed-off-by: John Laterre (jol) <jol@odoo.com>

addons/account/models/account_move_line.py
@@ -2618,6 +2618,11 @@ class AccountMoveLine(models.Model):
             return
 
         journal = company.currency_exchange_journal_id
+        if not journal:
+            raise UserError(_(
+                    "You have to configure the 'Exchange Gain or Loss Journal' in your company settings, to manage"
+                    " automatically the booking of accounting entries related to differences between exchange rates."
+                ))
         expense_exchange_account = company.expense_currency_exchange_account_id
         income_exchange_account = company.income_currency_exchange_account_id
         accounting_exchange_date = journal.with_context(move_date=exchange_date).accounting_date
@@ -2703,13 +2708,6 @@ class AccountMoveLine(models.Model):
         for exchange_diff_values in exchange_diff_values_list:
             move_vals = exchange_diff_values['move_values']
             exchange_move_values_list.append(move_vals)
-
-            if not move_vals['journal_id']:
-                raise UserError(_(
-                    "You have to configure the 'Exchange Gain or Loss Journal' in your company settings, to manage"
-                    " automatically the booking of accounting entries related to differences between exchange rates."
-                ))
-
             journal_ids.add(move_vals['journal_id'])
 
         if not exchange_move_values_list:

commit 90797625e8cc8103ecffe5d7af7ad0c495937bc0

[FIX] account: bypass exchange journal check for non-forex transactions
When attempting to register a payment in the company's currency for an
invoice also in company currency (where no currency exchange occurs),
the system incorrectly prompts for an exchange journal configuration.
This behavior is observed even when no exchange difference exists,
leading to an unnecessary error message.

Steps to Reproduce
------------------
1. Install `account_accountant`.
2. Ensure no default exchange journal is defined in the settings.
3. Create and confirm an invoice in the company currency.
4. Attempt to register a payment for that invoice in the same currency.

Result: A user error is triggered, incorrectly requiring an exchange
journal.

Cause
-----
This issue was introduced in commit
2d2a7729a77f8004aa046998ce75cd85280b3a6e, which enforced a check for the
exchange journal without considering whether there was an actual
exchange difference.

opw-3901083

addons/account/models/account_move_line.py
@@ -2622,7 +2622,7 @@ class AccountMoveLine(models.Model):
             return
 
         journal = company.currency_exchange_journal_id
-        if not journal:
+        if not journal and amounts_list:
             raise UserError(_(
                     "You have to configure the 'Exchange Gain or Loss Journal' in your company settings, to manage"
                     " automatically the booking of accounting entries related to differences between exchange rates."