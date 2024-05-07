2039a09c49d9e830d34e35aa2a451958c21001fe
Julien Segers (juse) <juse@odoo.com>
2024-05-07 14:47:00 +0000

    [FIX] l10n_es_pos: don't generate pdf for simplified invoices
    In case it's a simplified invoice, the 'to_invoice' is automatically set to True if a corresponding
    journal is set. Simplified invoices then generates a PDF which takes a few seconds to generate and
    slow downs the user experience. This commit avoids generating the PDF in case it's a simplified invoice.
    
    opw-3875944
    
    closes odoo/odoo#172527
    
    X-original-commit: ac5bd0787263cd12b7c59e6b80be1957df594091
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Julien Segers (juse) <juse@odoo.com>

--- addons/l10n_es_pos/static/src/overrides/models/pos_store.js
+++ addons/l10n_es_pos/static/src/overrides/models/pos_store.js
@@ -22,4 +22,16 @@ patch(PosStore.prototype, {
             await this._loadPartners([this.config.raw.simplified_partner_id]);
         }
     },
+    _getCreateOrderContext(orders, options) {
+        let context = super._getCreateOrderContext(...arguments);
+        if (this.config.is_spanish) {
+            const noOrderRequiresInvoicePrinting = orders.every(
+                (order) => !order.to_invoice && order.data.is_l10n_es_simplified_invoice
+            );
+            if (noOrderRequiresInvoicePrinting) {
+                context = { ...context, generate_pdf: false };
+            }
+        }
+        return context;
+    },
 });
