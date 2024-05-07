61586a27d1952829b103735d205b9e965ccf0330
laro-odoo <laro@odoo.com>
2024-05-07 13:29:46 +0000

    [FIX] web: resolve kanban drag-and-drop issue when write access is absent
    Before this commit, drag-and-drop functionality for groups/records in the kanban
    view did not work properly if the user lacked write access.
    
    Steps to Reproduce:
    
    - Log in to Odoo as Marc Demo.
    - Open the Project module.
    - Attempt to drag and drop any group/record in the kanban view.
    
    Observed Behavior:
    The group in the kanban view does not reset to its original position if the user
    lacks the right to reorder the groups/records, rendering the group/record
    immovable.
    
    Expected Behavior:
    The group in the kanban view should reset to its original position if the user
    lacks the right to reorder the groups/records.
    
    After this commit, drag-and-drop functionality for groups/records in the kanban
    view now works properly, even if the user does not have write access.
    
    Task ID: 3865617
    
    X-original-commit: f40d641a5c89382da326f449dbcf93846a7b582f

--- addons/web/static/src/model/relational_model/dynamic_list.js
+++ addons/web/static/src/model/relational_model/dynamic_list.js
@@ -348,6 +348,8 @@ export class DynamicList extends DataPoint {
             }
         }
 
+        // Save the original list in case of error
+        const originalOrder = [...originalList];
         // Perform the resequence in the list of records/groups
         const [dp] = originalList.splice(fromIndex, 1);
         originalList.splice(toIndex, 0, dp);
@@ -380,9 +382,16 @@ export class DynamicList extends DataPoint {
         if (offset) {
             params.offset = offset;
         }
-        const wasResequenced = await rpc("/web/dataset/resequence", params);
-        if (!wasResequenced) {
-            return;
+        // Attempt to resequence the records/groups on the server
+        try {
+            const wasResequenced = await rpc("/web/dataset/resequence", params);
+            if (!wasResequenced) {
+                return;
+            }
+        } catch (error) {
+            // If the server fails to resequence, rollback the original list
+            originalList.splice(0, originalList.length, ...originalOrder);
+            throw error;
         }
 
         // Read the actual values set by the server and update the records/groups
