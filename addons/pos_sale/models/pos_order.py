dcf95dc6d8e299b40296a3fcb664ecc500a223b1
“Ali <alhy@odoo.com>
2024-05-07 15:47:05 +0200

    [FIX] pos_sale: cancel all transfers after settling order
    - Install POS app
    - Change the warehouse to a 2-step delivery method
    - Create a sales order for a product and confirm
    - Ensure the product is reserved for the picking transfer **as** the issue doesn't exist (all transfers are successfully cancelled) if there are no products reserved
    - Go to POS and settle the sales order
    - The delivery transfer is cancelled but the picking transfer (with product reserved) is not cancelled. although all transfers should be cancelled if the order is fully delivered!
    
    - When the product is already reserved, the picking transfer is in the **Ready/assigned** state
    - When updating the steps, the **Ready/assigned** state is not taken into consideration and so the picking transfer is not updated to be **cancelled**
    
    opw-3474929

--- addons/pos_sale/models/pos_order.py
+++ addons/pos_sale/models/pos_order.py
@@ -95,7 +95,7 @@ class PosOrder(models.Model):
                     new_qty = 0
                 stock_move.product_uom_qty = so_line.compute_uom_qty(new_qty, stock_move, False)
                 # If the product is delivered with more than one step, we need to update the quantity of the other steps
-                for move in so_line_stock_move_ids.filtered(lambda m: m.state in ['waiting', 'confirmed'] and m.product_id == stock_move.product_id):
+                for move in so_line_stock_move_ids.filtered(lambda m: m.state in ['waiting', 'confirmed', 'assigned'] and m.product_id == stock_move.product_id):
                     move.product_uom_qty = stock_move.product_uom_qty
                     waiting_picking_ids.add(move.picking_id.id)
                 waiting_picking_ids.add(picking.id)
