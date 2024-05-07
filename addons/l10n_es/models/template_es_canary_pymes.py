a4e8f34e1a096c369b09c3e5bcea90e7a18e6f9e
Yosua Nicolaus <yoni@odoo.com>
2024-05-07 11:51:08 +0100

    [IMP] l10n_es: add Canary Island's CoA
    This commit adds Canary Island CoA from OCA's l10n_es_igic module.
    
    The initial migration was done using the script from:
    `https://github.com/william-andre/transform_coa`
    
    Along that, we also applied few more things:
    
    - use the english translations in `name` and moved the spanish names in
      `name@es`
    - added auto-apply on canary's fiscal position based on states
    - adapt `-100` taxes on some of the taxes in canary
    - refactored the template flow
    
    The template dependencies will be like the following:
    
    ```
                  es_common (withholding taxes + common accounts)
               /            \
      es_canary_common   es_common_mainland
    (taxes + canary accounts)   (taxes)
            |                   |
    (canary-assoc/full/pymes)  (assec/full/pymes/coop_pymes)
     same accounts used
    as in mainland through
    csv loading override
    ```
    
    Thanks to Binhex (specially Christian).
    
    task-id: 3354514

--- addons/l10n_es/models/template_es_canary_pymes.py
+++ addons/l10n_es/models/template_es_canary_pymes.py
@@ -6,23 +6,24 @@ from odoo.addons.account.models.chart_template import template
 class AccountChartTemplate(models.AbstractModel):
     _inherit = 'account.chart.template'
 
-    @template('es_pymes')
-    def _get_es_pymes_template_data(self):
+    @template('es_canary_pymes')
+    def _get_es_canary_pymes_template_data(self):
         return {
-            'name': _('SMEs (2008)'),
-            'parent': 'es_common_mainland',
-            'sequence': 0,
+            'name': _('Canary Islands - SMEs (2008)'),
+            'parent': 'es_canary_common',
         }
 
-    @template('es_pymes', 'res.company')
-    def _get_es_pymes_res_company(self):
+    @template('es_canary_pymes', 'res.company')
+    def _get_es_canary_pymes_res_company(self):
         return {
             self.env.company.id: {
                 'account_fiscal_country_id': 'base.es',
                 'bank_account_code_prefix': '572',
                 'cash_account_code_prefix': '570',
-                'transfer_account_code_prefix': '57299',
-                'account_sale_tax_id': 'account_tax_template_s_iva21b',
-                'account_purchase_tax_id': 'account_tax_template_p_iva21_bc',
+                'transfer_account_code_prefix': '572999',
             },
         }
+
+    @template('es_canary_pymes', 'account.account')
+    def _get_es_canary_pymes_account_account(self):
+        return self._parse_csv('es_pymes', 'account.account', module='l10n_es')
