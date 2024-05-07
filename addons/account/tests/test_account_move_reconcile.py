odoo/odoo

commit 2d2a7729a77f8004aa046998ce75cd85280b3a6e

[FIX] account: payment error on empty default post exchange difference journal
The issue:
when you make a payment and there is an exchange difference, since the post exchange difference is not set, it will throw a traceback

To reproduce:
- Enable 2 currencies
- Have the exchange difference journal set to NULL (empty)
- Create an invoice with a different currency than the one set for the company
- then register a payment.

The fix:
Throw a user error indicating to set the post exchange difference journal

opw-3783917
opw-3768202

closes odoo/odoo#157735

Signed-off-by: John Laterre (jol) <jol@odoo.com>

addons/account/models/account_move_line.py
@@ -2618,6 +2618,11 @@ class AccountMoveLine(models.Model):
             return
 
         journal = company.currency_exchange_journal_id
+        if not journal:
+            raise UserError(_(
+                    "You have to configure the 'Exchange Gain or Loss Journal' in your company settings, to manage"
+                    " automatically the booking of accounting entries related to differences between exchange rates."
+                ))
         expense_exchange_account = company.expense_currency_exchange_account_id
         income_exchange_account = company.income_currency_exchange_account_id
         accounting_exchange_date = journal.with_context(move_date=exchange_date).accounting_date
@@ -2703,13 +2708,6 @@ class AccountMoveLine(models.Model):
         for exchange_diff_values in exchange_diff_values_list:
             move_vals = exchange_diff_values['move_values']
             exchange_move_values_list.append(move_vals)
-
-            if not move_vals['journal_id']:
-                raise UserError(_(
-                    "You have to configure the 'Exchange Gain or Loss Journal' in your company settings, to manage"
-                    " automatically the booking of accounting entries related to differences between exchange rates."
-                ))
-
             journal_ids.add(move_vals['journal_id'])
 
         if not exchange_move_values_list:

commit 0bc8f48b4e2fd7fc0ffd77d4d66414445b31fd3e

[FIX] account: fix exchange diff error on payments
This https://github.com/odoo/odoo/commit/2d2a7729a77f8004aa046998ce75cd85280b3a6e introduced an unwanted side effect by raising an error when it wasn't legitimate.

opw-3901083

addons/account/models/account_move_line.py
@@ -2631,7 +2631,7 @@ class AccountMoveLine(models.Model):
         journal = company.currency_exchange_journal_id
         expense_exchange_account = company.expense_currency_exchange_account_id
         income_exchange_account = company.income_currency_exchange_account_id
-        accounting_exchange_date = journal.with_context(move_date=exchange_date).accounting_date
+        accounting_exchange_date = journal.with_context(move_date=exchange_date).accounting_date if journal else date.min
 
         move_vals = {
             'move_type': 'entry',

addons/account/tests/test_account_move_reconcile.py
@@ -4,6 +4,7 @@ from contextlib import contextmanager
 
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 from odoo.tests import Form, tagged
+from odoo.exceptions import UserError
 from odoo import fields, Command
 
 
@@ -4854,3 +4855,47 @@ class TestAccountMoveReconcile(AccountTestInvoicingCommon):
         lines_to_reconcile.reconcile()
 
         self.assertTrue(all(lines_to_reconcile.mapped('reconciled')), "All lines should be fully reconciled")
+
+    def test_reconcile_payment_with_no_exchange_diff_journal(self):
+        """
+        Test that the currency exchange journal is only required when creating exchange difference entries.
+        """
+
+        # Make sure the currency exchange journal is unset.
+        self.env.company.currency_exchange_journal_id = False
+
+        move_vals = {
+            'move_type': 'out_invoice',
+            'partner_id': self.partner_a.id,
+            'currency_id': self.currency_data['currency'].id,
+            'invoice_line_ids': [
+                Command.create({'product_id': self.product_a.id, 'price_unit': 1000.0, 'tax_ids': []}),
+            ],
+        }
+
+        payment_vals = {
+            'currency_id': self.currency_data['currency'].id,
+            'payment_difference_handling': 'reconcile',
+            'writeoff_account_id': self.env.company.expense_currency_exchange_account_id.id,
+        }
+
+        # Check that a payment can be created if no exchange difference entry is generated.
+        invoice_no_diff = self.env['account.move'].create(
+            {**move_vals, 'date': '2017-01-01', 'invoice_date': '2017-01-01'}
+        )
+        invoice_no_diff.action_post()
+        wizard_no_diff = self.env['account.payment.register']\
+            .with_context(active_model='account.move', active_ids=invoice_no_diff.ids)\
+            .create({**payment_vals, 'payment_date': '2017-01-01', 'amount': 3000})  # 3000 GOL = 1000 USD.
+        wizard_no_diff._create_payments()
+
+        # Then check that an error is raised when trying to create a payment with an exchange difference.
+        invoice_diff = self.env['account.move'].create(
+            {**move_vals, 'date': '2016-01-01', 'invoice_date': '2016-01-01'}
+        )
+        invoice_diff.action_post()
+        wizard_diff = self.env['account.payment.register']\
+            .with_context(active_model='account.move', active_ids=invoice_diff.ids)\
+            .create({**payment_vals, 'payment_date': '2018-01-01', 'amount': 2000})  # 2000 GOL = 1000 USD.
+        with self.assertRaises(UserError):
+            wizard_diff._create_payments()