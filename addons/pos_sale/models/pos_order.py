0d59fee25374708695f382396c2d0872c1c41c4f
“Ali <alhy@odoo.com>
2024-05-07 13:47:05 +0000

    [FIX] pos_sale: cancel all transfers after settling order
    - Install POS app
    - Change the warehouse to a 2-step delivery method
    - Create a sales order for a product and confirm
    - Ensure the product is reserved for the picking transfer **as** the issue doesn't exist (all transfers are successfully cancelled) if there are no products reserved
    - Go to POS and settle the sales order
    - The delivery transfer is cancelled but the picking transfer (with product reserved) is not cancelled. although all transfers should be cancelled if the order is fully delivered!
    
    - When the product is already reserved, the picking transfer is in the **Ready/assigned** state
    - When updating the steps, the **Ready/assigned** state is not taken into consideration and so the picking transfer is not updated to be **cancelled**
    
    opw-3474929
    
    closes odoo/odoo#170164
    
    X-original-commit: 122d3929b6cba7f72c5b90603891f4a5961d53ef
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Ali Hassan Youssef (alhy) <alhy@odoo.com>

--- addons/pos_sale/models/pos_order.py
+++ addons/pos_sale/models/pos_order.py
@@ -83,14 +83,14 @@ class PosOrder(models.Model):
                 so_line_stock_move_ids = so_line.move_ids.group_id.stock_move_ids
                 for stock_move in so_line.move_ids:
                     picking = stock_move.picking_id
-                    if not picking.state in ['waiting', 'confirmed', 'assigned']:
+                    if picking.state not in ['waiting', 'confirmed', 'assigned']:
                         continue
                     new_qty = so_line.product_uom_qty - so_line.qty_delivered
                     if float_compare(new_qty, 0, precision_rounding=stock_move.product_uom.rounding) <= 0:
                         new_qty = 0
                     stock_move.product_uom_qty = so_line.compute_uom_qty(new_qty, stock_move, False)
-                    #If the product is delivered with more than one step, we need to update the quantity of the other steps
-                    for move in so_line_stock_move_ids.filtered(lambda m: m.state in ['waiting', 'confirmed'] and m.product_id == stock_move.product_id):
+                    # If the product is delivered with more than one step, we need to update the quantity of the other steps
+                    for move in so_line_stock_move_ids.filtered(lambda m: m.state in ['waiting', 'confirmed', 'assigned'] and m.product_id == stock_move.product_id):
                         move.product_uom_qty = stock_move.product_uom_qty
                         waiting_picking_ids.add(move.picking_id.id)
                     waiting_picking_ids.add(picking.id)
