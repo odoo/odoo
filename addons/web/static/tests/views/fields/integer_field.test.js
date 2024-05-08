8d301b26a14289a1a2dfce3acbd269bb021c911a
Aaron Bohy <aab@odoo.com>
2024-05-08 14:31:58 +0000

    [FIX] web: input field: always format value on Enter/Tab
    Have a numeric field in a form view. The displayed value is
    formatted, according to the thousands and decimal separators.
    Say the value is 1,240.00. If the user edits the input and types
    the same value, i.e. 1240, and then press "Enter", "Tab" or "Shift-
    Tab", the value won't be formatted as it should be, because we
    detect no change of value (which is true, but the value should
    be formatted anyway).
    
    With this commit, the value is always formatted on "Enter", "Tab"
    and "Shift-Tab".
    
    Issue spotted by fp
    
    closes odoo/odoo#165378
    
    X-original-commit: 4da41131720c271f2ae2b6cc00dcd35e48b853fd
    Signed-off-by: Simon Genin (ges@odoo) <ges@odoo.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/fields/integer_field.test.js
+++ addons/web/static/tests/views/fields/integer_field.test.js
@@ -225,3 +225,34 @@ test("with enable_formatting option as false", async () => {
     await fieldInput("price").edit("1234567890");
     expect(".o_field_widget input").toHaveValue("1234567890");
 });
+
+test("value is formatted on Enter", async () => {
+    // `localization > grouping` required for this test is [3, 0], which is the default in mock server
+    await mountView({
+        type: "form",
+        resModel: "product",
+        arch: '<form><field name="price"/></form>',
+    });
+
+    expect(".o_field_widget input").toHaveValue("0");
+
+    await fieldInput("price").edit("1000", { confirm: "Enter" });
+    expect(".o_field_widget input").toHaveValue("1,000");
+});
+
+test("value is formatted on Enter (even if same value)", async () => {
+    // `localization > grouping` required for this test is [3, 0], which is the default in mock server
+    Product._records = [{ id: 1, price: 8069 }];
+
+    await mountView({
+        type: "form",
+        resModel: "product",
+        resId: 1,
+        arch: '<form><field name="price"/></form>',
+    });
+
+    expect(".o_field_widget input").toHaveValue("8,069");
+
+    await fieldInput("price").edit("8069", { confirm: "Enter" });
+    expect(".o_field_widget input").toHaveValue("8,069");
+});
