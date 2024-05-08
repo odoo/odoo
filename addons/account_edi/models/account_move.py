63de59875349ff2a0c02fa3fa4e22acb6121826a
Yosua Nicolaus <yoni@odoo.com>
2024-05-08 17:01:21 +0100

    [ADD] l10n_es_pos_tbai: add TBai QR code on PoS receipt
    This commit implements a new (bridge) module that adds the spanish TBai
    QR code on the Point of Sale checkout order receipt.
    
    Problem: Right now, with the l10n_es_pos, it works when the EDI is SII
    as it creates the invoice automatically and sends it in the background.
    In the case of simplified invoices, we even skipped the downloading of
    the PDF because that takes time.  But there, you do not need any
    response from the government to show to your client.  With TicketBai
    however, this is different, as we need the QR code on the ticket.
    
    Solution: when processing the data in the PoS payment, if the current
    order is a TicketBai order, we synchronously call the post method and
    get the url. This URL is then transfered to the javascript code and
    eventually the XML of the receipt, where the QR code is shown.
    
    task-id: 3916236

--- addons/account_edi/models/account_move.py
+++ addons/account_edi/models/account_move.py
@@ -264,7 +264,8 @@ class AccountMove(models.Model):
 
         self.env['account.edi.document'].create(edi_document_vals_list)
         posted.edi_document_ids._process_documents_no_web_services()
-        self.env.ref('account_edi.ir_cron_edi_network')._trigger()
+        if not self.env.context.get('skip_account_edi_cron_trigger'):
+            self.env.ref('account_edi.ir_cron_edi_network')._trigger()
         return posted
 
     def button_cancel(self):
