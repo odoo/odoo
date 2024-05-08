e07745824e847fddb365ec5f07585d6627e34155
Nathaniel (naja) <naja@odoo.com>
2024-05-08 16:43:54 +0200

    [FIX] hr_expense: create expenses without employee access rights
    Problem
    ---
    When creating/editing expenses, if the user doesn't have any access rights to
    employee, a ValueError is thrown when clicking the employee field.
    
    The Error happens because the model used for employees for users
    without access is `hr.employee.public` instead of the usual
    `hr.employee` and `hr.employee.public` does not define the
    `filter_for_expense` field, which is used for searching / filtering
    employees to set.
    
    Fix
    ---
    `hr.employee` and `hr.employee.public` both inherit from
    `hr.employee.base`, therefore we can put the search method,
    into an `hr.expense` child model of `hr.employee.base`.
    
    Note that (as far as I understand) this will not break stable because
    the `filter_for_expense` field is `store=False`.
    
    opw-3858951

---
+++ addons/hr_expense/static/tests/tours/expense_form_tours.js
@@ -0,0 +1,50 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+
+registry.category("web_tour.tours").add('create_expense_no_employee_access_tour', {
+    test: true,
+    url: "/web",
+    steps: () => [
+    {
+        content: "Go to Expense",
+        trigger: '.o_app[data-menu-xmlid="hr_expense.menu_hr_expense_root"]',
+    },
+    {
+        content: "Remove filter for own expenses",
+        trigger: '.o_facet_value:contains(My Expense) + button[title="Remove"]',
+    },
+    {
+        content: "Go to form view of pre-prepared record",
+        trigger: '.o_data_cell:contains(expense_for_tour_0)'
+    },
+    {
+        content: "Click employee selection dropdown",
+        trigger: 'input#employee_id_0',
+    },
+    {
+        content: "Delete default search",
+        trigger: 'input#employee_id_0',
+        run() {
+            const dropdown = document.querySelector('input#employee_id_0');
+            dropdown.value = '';
+        }
+    },
+    {
+        content: "Select test expense employee",
+        trigger: 'a.dropdown-item:contains(expense_employee)',
+    },
+    {
+        content: "Save",
+        trigger: '.o_form_button_save',
+    },
+    {
+        content: "Exit form",
+        trigger: '.o_menu_brand',
+    },
+    {
+        content: "Check",
+        trigger: '.o_app[data-menu-xmlid="hr_expense.menu_hr_expense_root"]',
+        isCheck: true,
+    },
+]});
