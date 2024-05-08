0a9ebd5f4ba5a53eb094215ccc8f77a8bd683190
Thibault Delavallée <tde@odoo.com>
2024-05-08 10:51:45 +0200

    [FIX] phone_validation: let phone formatting work even when no country
    Currently when using _phone_format on any model without any country (on record
    or on company) the phone format call is skipped. However if input is already
    a formatted number it works even without the country information. It means
    we can let the call to the formatting tool be done and handle its result
    which is either the sanitized result either False.
    
    In this commit we now always call the format tool even when no country is
    found. It means the returned value might change in some cases (i.e. no country
    -> was returning original number, now returns False). However the method spec
    is that it should return the formatted number or False when no formatting is
    possible.
    
    Calls of _phone_format has been checked and they effectively generally do
    sanitized or original_number, meaning they expect the returned number to be
    False when the formatting is not possible. So there should not be any unwanted
    side effect.
    
    Followup of odoo/odoo@adfdf5ab33e719a55f65e2d9ff34ff265fe1b579 who introduced the tool method on models
    
    Followup of odoo/odoo@3dc8ae3cec37619c5b21933cfe6d0e77420c9dc3 who added the test crashing in no-demo mode
    
    Task-3918417
    Runbot-64570
    
    closes odoo/odoo#164884
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/phone_validation/tests/test_phonenumbers_blacklist.py
+++ addons/phone_validation/tests/test_phonenumbers_blacklist.py
@@ -1,14 +1,32 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
 
 
+@tagged("phone_blacklist")
 class TestPhonenumbersBlacklist(TransactionCase):
+
     def test_sanitize_search(self):
         """ Test that when using search, the number is sanitized """
         blacklist = self.env['phone.blacklist']
-        blacklist.create({'number': '+917589632587'})
+        bl_entry = blacklist.create({'number': '+917589632587'})
+        # be sure there is no company fallback for this test
+        self.env.company.country_id = False
 
-        res = blacklist.search([('number', 'in', ['+917 5896 32587'])])
+        for user_country in [
+            self.env.ref("base.be"),  # other country (should work as complete number)
+            self.env.ref("base.in"),  # correct country
+            self.env["res.country"],  # no country
+        ]:
+            with self.subTest(country_name=user_country.name or "No country"):
+                self.env.user.country_id = user_country
 
-        self.assertEqual(len(res), 1, "There should be one result")
+                res = blacklist.search([('number', 'in', ['+917 5896 32587'])])
+                self.assertEqual(res, bl_entry)
+
+                res = blacklist.search([('number', '=', '+917 5896 32587')])
+                self.assertEqual(res, bl_entry)
+
+                res = blacklist.search([('number', '=', '+917589632587')])
+                self.assertEqual(res, bl_entry)
