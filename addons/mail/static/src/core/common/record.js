0ec3ac6282326632a9429a68e2933cced5b32a54
Alexandre Kühn <aku@odoo.com>
2024-05-08 18:15:37 +0200

    [PERF] mail: faster messaging menu load with many pinned chats
    Before this commit, when a user had many discuss chats pinned on
    the UI (i.e. in Discuss app Sidebar or in Messaging Menu), then
    the initial loading of Discuss could be very slow.
    
    This initial loading happens at page load in version 17.0.
    
    This happens because when inserting the pinned thread data,
    the inner-working of computed fields related to Thread models
    was taking a lot of time. This comes from poor coding in ensuring
    computed fields are registered once in a single `Record.insert()`
    transaction: this uses an array, so it had to be fully parse to
    check whether it was present.
    
    This commit fixes the issue by using a `Set` instead of an array
    for the FC_QUEUE (the inner-queue of registered fields to compute),
    so that checking presence of field is an O(1) time rather than O(n).
    Note that arrays are fast so it needs a lot of threads and a lot
    of computed fields to see a big performance problem.
    
    In version 17.0:
    
    200 pinned threads: 600ms => 400ms (50% faster)
    1k6 pinned threads: 8.85s => 5.25s (70% faster)

--- addons/mail/static/src/core/common/record.js
+++ addons/mail/static/src/core/common/record.js
@@ -1041,8 +1041,8 @@ export class Record {
     static records;
     /** @type {import("models").Store} */
     static store;
-    /** @type {RecordField[]} */
-    static FC_QUEUE = []; // field-computes
+    /** @type {Set<RecordField>} */
+    static FC_QUEUE = new Set(); // field-computes
     /** @type {RecordField[]} */
     static FS_QUEUE = []; // field-sorts
     /** @type {Array<{field: RecordField, records: Record[]}>} */
@@ -1065,7 +1065,7 @@ export class Record {
             // pretend an increased update cycle so that nothing in queue creates many small update cycles
             Record.UPDATE++;
             while (
-                Record.FC_QUEUE.length > 0 ||
+                Record.FC_QUEUE.size > 0 ||
                 Record.FS_QUEUE.length > 0 ||
                 Record.FA_QUEUE.length > 0 ||
                 Record.FD_QUEUE.length > 0 ||
@@ -1073,22 +1073,24 @@ export class Record {
                 Record.RO_QUEUE.length > 0 ||
                 Record.RD_QUEUE.length > 0
             ) {
-                const FC_QUEUE = [...Record.FC_QUEUE];
+                const FC_QUEUE = new Set([...Record.FC_QUEUE]);
                 const FS_QUEUE = [...Record.FS_QUEUE];
                 const FA_QUEUE = [...Record.FA_QUEUE];
                 const FD_QUEUE = [...Record.FD_QUEUE];
                 const FU_QUEUE = [...Record.FU_QUEUE];
                 const RO_QUEUE = [...Record.RO_QUEUE];
                 const RD_QUEUE = [...Record.RD_QUEUE];
-                Record.FC_QUEUE.length = 0;
+                Record.FC_QUEUE.clear();
                 Record.FS_QUEUE.length = 0;
                 Record.FA_QUEUE.length = 0;
                 Record.FD_QUEUE.length = 0;
                 Record.FU_QUEUE.length = 0;
                 Record.RO_QUEUE.length = 0;
                 Record.RD_QUEUE.length = 0;
-                while (FC_QUEUE.length > 0) {
-                    const field = FC_QUEUE.pop();
+                while (FC_QUEUE.size > 0) {
+                    /** @type {[RecordField]} */
+                    const [field] = FC_QUEUE.entries().next().value;
+                    FC_QUEUE.delete(field);
                     field.requestCompute({ force: true });
                 }
                 while (FS_QUEUE.length > 0) {
@@ -1171,8 +1173,8 @@ export class Record {
             const field = fieldOrRecord;
             const rawField = toRaw(field);
             if (type === "compute") {
-                if (!Record.FC_QUEUE.some((f) => toRaw(f) === rawField)) {
-                    Record.FC_QUEUE.push(field);
+                if (!Record.FC_QUEUE.has(rawField)) {
+                    Record.FC_QUEUE.add(rawField);
                 }
             }
             if (type === "sort") {
