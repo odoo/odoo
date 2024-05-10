6cd4cf1d054c13fb7bdc838915642223bf5fc164
adch-odoo <adch@odoo.com>
2024-05-10 09:56:04 +0000

    [FIX] web_editor: preserve table selection on formatting commands
    Before this commit:
    
    On selecting multiple cells in a table and applying
    formatting commands like (Bold, Italic, Underline, and Strikethrough) resulted
    in the loss of selection. The root cause of this issue was identified in the
    `cleanForSave()` function, which triggered `deselectTable()` during command
    execution, removing the entire selection from the table results in
    collapsing the selection to start.
    
    Approach:
    
    This commit addresses the issue by restoring selection using
    `historyResetLatestComputedSelection()` after executing the command, ensuring
    that the selection is preserved as intended.
    
    After this commit:
    
    We maintain the selection after applying formatting commands
    (Bold, Italic, Underline, and Strikethrough) in table.
    
    task-3822527
    
    X-original-commit: 0c9bc6773bfee389cc9e3a6168831a84ddc446f1
    Part-of: odoo/odoo#188756
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -2075,13 +2075,11 @@ export class OdooEditor extends EventTarget {
     bindExecCommand(element) {
         for (const buttonEl of element.querySelectorAll('[data-call]')) {
             buttonEl.addEventListener('click', ev => {
-                if (!this.isSelectionInEditable()) {
-                    this.historyResetLatestComputedSelection(true);
-                }
                 const arg1 = buttonEl.dataset.arg1;
                 const args = arg1 && arg1.split(",") || [];
                 this.execCommand(buttonEl.dataset.call, ...args);
 
+                this.historyResetLatestComputedSelection(true);
                 ev.preventDefault();
                 this._updateToolbar();
             });
@@ -4198,21 +4196,25 @@ export class OdooEditor extends EventTarget {
             ev.preventDefault();
             ev.stopPropagation();
             this.execCommand('bold');
+            this.historyResetLatestComputedSelection(true);
         } else if (IS_KEYBOARD_EVENT_ITALIC(ev)) {
             // Ctrl-I
             ev.preventDefault();
             ev.stopPropagation();
             this.execCommand('italic');
+            this.historyResetLatestComputedSelection(true);
         } else if (IS_KEYBOARD_EVENT_UNDERLINE(ev)) {
             // Ctrl-U
             ev.preventDefault();
             ev.stopPropagation();
             this.execCommand('underline');
+            this.historyResetLatestComputedSelection(true);
         } else if (IS_KEYBOARD_EVENT_STRIKETHROUGH(ev)) {
             // Ctrl-5 / Ctrl-shift-(
             ev.preventDefault();
             ev.stopPropagation();
             this.execCommand('strikeThrough');
+            this.historyResetLatestComputedSelection(true);
         } else if (IS_KEYBOARD_EVENT_LEFT_ARROW(ev) || IS_KEYBOARD_EVENT_RIGHT_ARROW(ev)) {
             const side = ev.key === 'ArrowLeft' ? 'previous' : 'next';
             const selection = this.document.getSelection();
