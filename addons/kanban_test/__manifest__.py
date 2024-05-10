33650a2e59c054bc34e80571734972c40a43927c
Aaron Bohy <aab@odoo.com>
2024-05-10 11:56:57 +0200

    [TO REMOVE] kanban_test module

---
+++ addons/kanban_test/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Kanban Test',
+    'category': 'Hidden',
+    'version': '1.0',
+    'depends': ['contacts', 'event', 'sale_management', 'project', 'crm'],
+    'data': [
+        'views/kanban_test_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
