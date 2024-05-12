9c15b63b753562447a3d0a22afbb0908348c333a
Hesham <hsal@odoo.com>
2024-05-12 20:15:21 +0300

    [IMP] hr_expense: Improved expense onboarding
    Improved onboarding, flow, and UX on desktop and mobile for expense.
    A more detailed overview could be found here https://app.excalidraw.com/l/65VNwvy7c4X/73HONs7UUCz
    
    task-3919542

--- addons/hr_expense/__manifest__.py
+++ addons/hr_expense/__manifest__.py
@@ -62,6 +62,7 @@ This module also uses analytic accounting and is compatible with the invoice on 
             'hr_expense/static/src/views/*.xml',
             'hr_expense/static/src/scss/hr_expense.scss',
             'hr_expense/static/src/js/tours/*.js',
+            'hr_expense/static/src/js/web/*.js',
         ],
         'web.assets_tests': [
             'hr_expense/static/tests/tours/expense_upload_tours.js',
