b9f4e960b7dc2563c0f8588ac2f22d11e16c4f26
ABE-Mark45 <aels@odoo.com>
2024-05-14 00:58:12 +0300

    [IMP] stock: add time horizon section
    In the replenishments list view, the search panel was
    modified to include the horizon section which controls
    the visibility days used to calculate correct lead times.
    
    task-3924995

---
+++ addons/stock/static/src/views/search/stock_orderpoint_search_model.js
@@ -0,0 +1,33 @@
+/** @odoo-module **/
+
+import { useService } from "@web/core/utils/hooks";
+import { SearchModel } from "@web/search/search_model";
+import { debounce } from "@web/core/utils/timing";
+
+
+export class StockOrderpointSearchModel extends SearchModel {
+    static DEBOUNCE_DELAY = 500;
+
+    setup(services) {
+        super.setup(services);
+        this.ui = useService("ui");
+        this.applyGlobalVisibilityDays = debounce(
+            this.applyGlobalVisibilityDays.bind(this),
+            StockOrderpointSearchModel.DEBOUNCE_DELAY
+        );
+    }
+
+    async applyGlobalVisibilityDays(globalVisibilityDays) {
+        this.ui.block();
+        this.globalContext = {
+            ...this.globalContext,
+            global_visibility_days: globalVisibilityDays,
+        };
+        await this.orm.call("stock.warehouse.orderpoint", "action_open_orderpoints", [], {
+            context: this.context,
+        });
+        await this._fetchSections(this.categories, this.filters);
+        this._notify();
+        this.ui.unblock();
+    }
+}
