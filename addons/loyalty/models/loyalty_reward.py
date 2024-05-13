3daee7fb5cc9e81291867eac3283c206ad5a7d7a
Youssef Bashandy <yoba@odoo.com>
2024-05-13 13:53:07 +0200

    [FIX] loyalty : Prevent archiving product when archive discount program
    Steps to reproduce:
    	- Install Sales and Loyalty modules
    	- Create a discount & loyalty program with type of 'Buy X get Y'
    	and a reward product that has a quantity on-hand
    	- Archive the discount & loyalty program
    	- Check the quantity of the product you put as reward
    
    Current behavior before PR:
    The reward product lose its quantity when archiving
    the corresponding discount & loyalty program. This
    is happening because when archiving the program we
    archive the reward product too.
    https://github.com/odoo/odoo/blob/17.0/addons/loyalty/models/loyalty_reward.py#L239
    
    Desired behavior after PR is merged:
    We are now archiving just the free product that got
    created when we were creating the discount & loyalty
    program not the main product. This is happening
    only when the reward type is "Free product"
    
    opw-3909184
    
    closes odoo/odoo#165225
    
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- addons/loyalty/models/loyalty_reward.py
+++ addons/loyalty/models/loyalty_reward.py
@@ -232,9 +232,9 @@ class LoyaltyReward(models.Model):
                 reward.discount_line_product_id.write({'name': reward.description})
         if 'active' in vals:
             if vals['active']:
-                self.reward_product_id.action_unarchive()
+                self.discount_line_product_id.action_unarchive()
             else:
-                self.reward_product_id.action_archive()
+                self.discount_line_product_id.action_archive()
         return res
 
     def unlink(self):
