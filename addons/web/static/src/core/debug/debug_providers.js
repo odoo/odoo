df639d01a8e7cd6e903eb7d8d8e48cb760b261b2
Bruno Boi (boi) <boi@odoo.com>
2024-05-13 15:09:12 +0200

    [FIX] web: make translated debug command work
    - Before this commit
      The "debug" word in the command palette should display the item to
      activate the debugging feature.
      The problem is that the command palette still applies the fuzzy search
      to this item's name. If the lokked up string does not fuzzily match
      the letters "debug", the item will not get displayed.
    
    - After this commit
      The debug command name will now always have the `debug` word as a non
      translated part.
    
    opw-3889254

--- addons/web/static/src/core/debug/debug_providers.js
+++ addons/web/static/src/core/debug/debug_providers.js
@@ -20,13 +20,14 @@ commandProviderRegistry.add("debug", {
                 name: env._t("Deactivate debug mode"),
             });
         } else {
-            if (options.searchValue.toLowerCase() === "debug") {
+            const debugKey = "debug";
+            if (options.searchValue.toLowerCase() === debugKey) {
                 result.push({
                     action() {
                         browser.location.search = "?debug=assets";
                     },
                     category: "debug",
-                    name: env._t("Activate debug mode"),
+                    name: `${env._t("Activate debug mode")} (${debugKey})`,
                 });
             }
         }
