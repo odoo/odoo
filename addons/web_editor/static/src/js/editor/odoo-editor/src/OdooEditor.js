a7fdda5897d3efe6be82218863cd24bcc968ce19
Adnan Saiyed <adsa@odoo.com>
2024-05-13 11:43:57 +0530

    [FIX] web_editor: traceback when selecting multiple tables using mouse
    Current behaviour before commit:
    
    Selecting multiple tables using mouse throws traceback.
    This happens because in _selectTableCells method, range
    has no table as closest element of commonAncestorContainer
    which gives traceback later. This method needs a table as
    closest element to be worked.
    
    Desired behaviour after commit:
    
    There should be no traceback. This can be handled by returning
    the method if we don't find the closest table.
    
    task-3922517

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -2443,8 +2443,11 @@ export class OdooEditor extends EventTarget {
      * @param {Range} range
      */
     _selectTableCells(range) {
-        this.observerUnactive('_selectTableCells');
         const table = closestElement(range.commonAncestorContainer, 'table');
+        if (!table) {
+            return;
+        }
+        this.observerUnactive('_selectTableCells');
         const alreadyHadSelection = table.classList.contains('o_selected_table');
         this.deselectTable(); // Undo previous selection.
         table.classList.toggle('o_selected_table', true);
