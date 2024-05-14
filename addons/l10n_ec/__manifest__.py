0bbe5c5b98995b88b40cca076c3c57c72d289c5e
Ken Luzuriaga <ken.luzuriaga@trescloud.com>
2024-05-14 13:08:05 -0500

    [IMP] l10n_ec: Update codes Tax 5% - May 2024
    - Update tax codes:
      - IVA 5% (411, Bienes) -> IVA 5% (435, Bienes) (code_base: 435, code_applied: 445)
      - IVA 5% (510, Crédito IVA) -> IVA 5% (550, Crédito IVA) (code_base: 550, code_applied: 560)
    
    - Create Report lines:
      - Ventas locales (excluye activos fijos) gravadas tarifa 5%
      - Adquisiciones y pagos locales (excluye activos fijos) gravados con tarifa 5% (con derecho a crédito tributario)

--- addons/l10n_ec/__manifest__.py
+++ addons/l10n_ec/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     "name": "Ecuadorian Accounting",
-    "version": "3.8",
+    "version": "3.9",
     "description": """
 Functional
 ----------
