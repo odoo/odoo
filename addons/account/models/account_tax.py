christian-rb/odoo-dev

[IMP] account: legal notes from taxes on invoices
Add a legal notes field on taxes, which is then displayed on invoices if the tax is used.

Reason of the change:
Some taxes require special notes on invoices. We currently handle those requirements thanks to fiscal positions legal notes but the granularity isn't high enough to cover all business cases. The business need is growing because of the localizations expansion.

Examples:
 - If 0% EU S then "VAT Reverse charge - article 196 of the VAT Directive 2006/112/EC" is required.
 - If 0% EU M then "Exemption intra-community supplies article 138 Council Directive 2006/112/EC" is required.

 task-id: 3873054

addons/account/models/account_tax.py
@@ -183,6 +183,7 @@ class AccountTax(models.Model):
     country_code = fields.Char(related='country_id.code', readonly=True)
     is_used = fields.Boolean(string="Tax used", compute='_compute_is_used')
     repartition_lines_str = fields.Char(string="Repartition Lines", tracking=True, compute='_compute_repartition_lines_str')
+    invoice_legal_notes = fields.Text(string="Legal Notes", help="Legal mentions that have to be printed on the invoices.")
 
     @api.constrains('company_id', 'name', 'type_tax_use', 'tax_scope', 'country_id')
     def _constrains_name(self):

odoo/odoo

addons/account/models/account_tax.py
@@ -184,6 +184,7 @@ class AccountTax(models.Model):
     country_code = fields.Char(related='country_id.code', readonly=True)
     is_used = fields.Boolean(string="Tax used", compute='_compute_is_used')
     repartition_lines_str = fields.Char(string="Repartition Lines", tracking=True, compute='_compute_repartition_lines_str')
+    invoice_legal_notes = fields.Html(string="Legal Notes", help="Legal mentions that have to be printed on the invoices.")
 
     @api.constrains('company_id', 'name', 'type_tax_use', 'tax_scope', 'country_id')
     def _constrains_name(self):