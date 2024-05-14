809bedbadb719355efd95fe568a52a888204b8f1
Benjamin Vray <bvr@odoo.com>
2024-05-14 17:18:17 +0200

    [FIX] website_partner: fix website description field
    Steps to reproduce the bug:
    
    - Go to the "/partners" page.
    - Click on the first partner to go to their page.
    - Enter "edit mode".
    - Drag and drop a "Dynamic products" snippet into the right column of
    the page.
    - Save the page.
    - Bug: the "Dynamic products" snippet is not visible on the page.
    
    The problem is not specific to the "Dynamic products" snippet. The issue
    occurs with most snippets. When the "website description" field is
    saved, all HTML attributes are removed (except the "class" attribute).
    This causes many bugs, which vary depending on the snippets dropped into
    the field.
    
    This commit adds the parameter "sanitize_overridable=True" to the
    "website_description" field of "website_partner". It means that the
    description can now be properly edited as an user with enough rights,
    while keeping the same level of protection for other users editing this
    field (it is in particular possible to edit by the partner itself when
    the website_profile app is installed). This means that it will still be
    buggy for a restricted editor editing that description but it should be
    rare use case. In 17.0 however, an improvement was made so that blocks
    just cannot be dropped in fully sanitized fields.
    
    To sum up:
    
    15.0: the field is fully sanitized and buggy at edition for all users.
    
    16.0: Same but the sanitization can be bypassed for users with enough
          rights. Meaning it is buggy for restricted editor users only,
          which should be a rare flow.
    
    17.0: Same but it does not appear buggy for restricted editor users
          anymore: blocks just cannot be added by them.
    
    opw-3908218
    
    closes odoo/odoo#171019
    
    X-original-commit: 3e3cccb947bff1c5a94a3b4dd840f95dc77703ae
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website_partner/models/res_partner.py
+++ addons/website_partner/models/res_partner.py
@@ -9,7 +9,7 @@ class WebsiteResPartner(models.Model):
     _name = 'res.partner'
     _inherit = ['res.partner', 'website.seo.metadata']
 
-    website_description = fields.Html('Website Partner Full Description', strip_style=True, translate=html_translate)
+    website_description = fields.Html('Website Partner Full Description', strip_style=True, sanitize_overridable=True, translate=html_translate)
     website_short_description = fields.Text('Website Partner Short Description', translate=True)
 
     def _compute_website_url(self):
