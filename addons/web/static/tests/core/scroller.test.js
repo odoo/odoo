e1b6d162adbd5cba20bd99564ed07fa8ce67fde6
Jorge Pinna Puissant <jpp@odoo.com>
2024-05-15 15:23:41 +0200

    [FIX] web: scroll to anchor from load
    - Go to invoicing;
    - Open a non-paid customer invoice with outstanding credits;
    - Click on the outstanding banner;
    - The page will scroll down to the outstanding credits (as expected);
    - Reload the browser, or copy/paste the URL to a new tab.
    
    Before this commit, after the reload (or load), the page will not scroll
    down to the outstanding credits. This occurs because when state loading
    a page, the anchors in the fragment part of the URL are not taken into
    account.
    
    Now, after the reload (or load), the page will correctly scroll down to
    the corresponding anchor found in the URL (in our example, the
    outstanding credits).
    
    opw-3794940
    
    Part-of: odoo/odoo#174976
    Related: odoo/enterprise#67676
    Signed-off-by: Simon Genin (ges) <ges@odoo.com>

--- addons/web/static/tests/core/scroller.test.js
+++ addons/web/static/tests/core/scroller.test.js
@@ -7,6 +7,7 @@ import { browser } from "@web/core/browser/browser";
 import { scrollTo } from "@web/core/utils/scrolling";
 import { WebClient } from "@web/webclient/webclient";
 import { registry } from "@web/core/registry";
+import { redirect } from "@web/core/utils/urls";
 
 test("Ignore empty hrefs", async () => {
     class MyComponent extends Component {
@@ -487,4 +488,56 @@ test("Simple scroll to HTML elements", async () => {
     expect(isVisible("#o-div-4")).toBe(true);
     expect(border("#o-div-4").top).toBe(0);
     expect(border("#sub-scrollable").top).toBe(0);
+});
+
+test("scroll to anchor from load", async () => {
+    class MyComponent extends Component {
+        static template = xml/* xml */ `
+            <div class="o_content" style="overflow:scroll;height:150px;width:400px">
+                <a href="#scrollToHere"  class="alert-link" role="button">sroll to ...</a>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+                    Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed,
+                    dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper
+                    congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est
+                    eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu
+                    massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut
+                    in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent
+                    egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue
+                    blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                    posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque
+                    fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+                </p>
+                <p>
+                    Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa
+                    suscipit pulvinar. Nulla sollicitudin. Fusce varius, ligula non tempus aliquam, nunc
+                    turpis ullamcorper nibh, in tempus sapien eros vitae ligula. Pellentesque rhoncus
+                    nunc et augue. Integer id felis. Curabitur aliquet pellentesque diam. Integer quis
+                    metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing
+                    elit. Morbi vel erat non mauris convallis vehicula. Nulla et sapien. Integer tortor
+                    tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis
+                    vitae erat. Proin feugiat, augue non elementum posuere, metus purus iaculis lectus,
+                    et tristique ligula justo vitae magna.
+                </p>
+                <p>
+                    Aliquam convallis sollicitudin purus. Praesent aliquam, enim at fermentum mollis,
+                    ligula massa adipiscing nisl, ac euismod nibh nisl eu lectus. Fusce vulputate sem
+                    at sapien. Vivamus leo. Aliquam euismod libero eu enim. Nulla nec felis sed leo
+                    placerat imperdiet. Aenean suscipit nulla in justo. Suspendisse cursus rutrum
+                    augue. Nulla tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+                    felis magna fermentum augue, et ultricies lacus lorem varius purus. Curabitur eu amet.
+                </p>
+                <div id="scrollToHere">sroll here!</div>
+            </div>
+        `;
+        static props = ["*"];
+        static path = "my_component";
+    }
+    registry.category("actions").add("my_component", MyComponent);
+    redirect("/odoo/my_component#scrollToHere");
+    await mountWithCleanup(WebClient);
+    await animationFrame();
+
+    const scrollableParent = document.querySelector(".o_content");
+    expect(scrollableParent.scrollTop).not.toBe(0);
 });
