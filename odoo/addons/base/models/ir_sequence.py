d5203ac1300a24c160b2df89808e169d10035df1
mega <mega@odoo.com>
2024-05-15 15:02:40 +0530

    [FIX] ir_sequence: prevent an error when producing a manufacturing order
    Currently, an error occurs when producing a manufacturing order for a product
    whose tracking is by lot.
    
    Step to produce:
    
    - Install the 'mrp' module (ensure developer mode is enabled).
    - Go to Settings / Technical / Sequences & Identifiers / Sequences, Open
    'stock.lot.serial' and add ''%default_code-%(month)s%(y)s-' as a prefix.
    - Create a new product by lots, Open 'Manufacturing Orders'  and create an order
    with that new product.
    - Confirm this order, And then click on mark as done and apply immediate
    production
    
    TypeError: %d format: a real number is required, not dict
    
    An error occurs when attempting to produce a manufacturing order of a product
    which have lot by serial numbers because the system tries to generate an invalid
    serial number at [1].
    
    link [1]: https://github.com/odoo/odoo/blob/c7d420b077490108794f513e9ff21b0d850f57af/addons/stock/models/stock_production_lot.py#L89
    
    To resolve the issue, raise a user error for an invalid sequence.
    
    sentry-5343084412
    
    X-original-commit: 3534009c75a298eecd020989138360c74045f3ad

--- odoo/addons/base/models/ir_sequence.py
+++ odoo/addons/base/models/ir_sequence.py
@@ -232,7 +232,7 @@ class IrSequence(models.Model):
         try:
             interpolated_prefix = _interpolate(self.prefix, d)
             interpolated_suffix = _interpolate(self.suffix, d)
-        except ValueError:
+        except (ValueError, TypeError):
             raise UserError(_('Invalid prefix or suffix for sequence \'%s\'') % self.name)
         return interpolated_prefix, interpolated_suffix
 
