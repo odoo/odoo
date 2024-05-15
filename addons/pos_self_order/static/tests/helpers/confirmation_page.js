365eb9e1152407a7f01c6b291d2c7ac823c8c659
axtr <axtr@odoo.com>
2024-05-15 10:02:21 +0200

    [FIX] pos_self_order: display order number for a command with a price of 0.00
    Problem:
    In kiosk, when a person has a command with a price of 0.00, he doesn't have an order number
    
    Steps to reproduce:
    - Install "Point of Sale" app
    - In POS settings, create a kiosk shop and set "Self Ordering" to "Kiosk"
    - Go to product and set a price of 0.00 to a product
    - Open the kiosk session and order the product with the price of 0.00
    - Click on "Pay" and you'll be back to the reception page without having your order number
    
    opw-3877596

---
+++ addons/pos_self_order/static/tests/helpers/confirmation_page.js
@@ -0,0 +1,9 @@
+/** @odoo-module */
+
+export function orderNumberShown(){
+    return {
+        content: "Check if the order number is shown",
+        trigger: ".number",
+        run: () => {},
+    };
+}
