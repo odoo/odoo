42d443cebdc4158d2bdae9db3ee0c15369b1fb1b
AllePilli <peal@odoo.com>
2024-05-15 09:37:50 +0200

    [ADD] l10n_ro_edi_etransport, l10n_ro_edi_etransport_batch: send transport data to eTransport platform
    Romania requires its companies to send information of all transports of goods on Romanian territory to a specific web service on their eTransport platform.
    
    This commit implements a way to gather all the necessary data needed to send the eTransport document (from a stock.picking or a stock.picking.batch) and the actual sending of this document to the eTransport platform.
    
    Authentication:
    the authentication needed to interact with the eTransport platform is the same as (and was already implemented in) the l10n_ro_efactura module. see [eFactura PR](https://github.com/odoo/odoo/pull/144061)
    
    eTransport flow:
    1. Add the necessary eTransport data to a delivery/batch transfer
    2. send the eTransport document to ANAF
    3. the document gets processed by ANAF
    4. Fetch the status of the document:
        - which can result in an error, in which case the flow starts again from the beginning with the corrected data
        - or in a success
    5. optionally send corrected document data to ANAF in which case we go back to step 3.
    
     task-id: 3810735

---
+++ addons/l10n_ro_edi_stock_batch/models/stock_picking.py
@@ -0,0 +1,26 @@
+from odoo import api, models
+
+
+class Picking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.depends('batch_id', 'l10n_ro_edi_stock_document_message')
+    def _compute_l10n_ro_edi_stock_enable_send_info(self):
+        # OVERRIDES 'l10n_ro_edi_stock'
+        for picking in self:
+            picking.l10n_ro_edi_stock_enable_send_info = not picking.batch_id and picking.l10n_ro_edi_stock_document_message
+
+    @api.depends('batch_id', 'company_id')
+    def _compute_l10n_ro_edi_stock_enable(self):
+        # OVERRIDES 'l10n_ro_edi_stock'
+        for picking in self:
+            picking.l10n_ro_edi_stock_enable = not picking.batch_id and picking.company_id.country_id.code == 'RO'
+
+    @api.model
+    def _l10n_ro_edi_stock_validate_carrier_filter(self, picking):
+        # OVERRIDE l10n_ro_edi_stock
+
+        # Override for when the batch picking calls this function to validate the carriers
+        validate_carrier = self.env.context.get('l10n_ro_edi_stock_validate_carrier', False)
+
+        return picking.company_id.account_fiscal_country_id.code == 'RO' and (picking.l10n_ro_edi_stock_enable or validate_carrier)
