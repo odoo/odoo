d93298157d9cd5f4123664208ab174a6214c40b9
BHanq <beha+github@odoo.com>
2024-05-15 16:34:57 +0000

    [FIX] hr_expense: Broken access rights
    The state changes right check was only done on specific method but it
    wasn't check at write level. Which allowed to bypass it.
    
    The record rule on hr_expense_user without a check on the state is in
    draft allow to change data on approved expense sheets.
    
    X-original-commit: 92284e7df1c40aee25ae7fc5041e1aa9e132a61d

--- addons/hr_expense/models/hr_expense_sheet.py
+++ addons/hr_expense/models/hr_expense_sheet.py
@@ -335,9 +335,9 @@ class HrExpenseSheet(models.Model):
     @api.depends_context('uid')
     @api.depends('employee_id')
     def _compute_can_approve(self):
-        is_team_approver = self.user_has_groups('hr_expense.group_hr_expense_team_approver')
-        is_approver = self.user_has_groups('hr_expense.group_hr_expense_user')
-        is_hr_admin = self.user_has_groups('hr_expense.group_hr_expense_manager')
+        is_team_approver = self.user_has_groups('hr_expense.group_hr_expense_team_approver') or self.env.su
+        is_approver = self.user_has_groups('hr_expense.group_hr_expense_user') or self.env.su
+        is_hr_admin = self.user_has_groups('hr_expense.group_hr_expense_manager') or self.env.su
 
         for sheet in self:
             reason = False
@@ -463,6 +463,22 @@ class HrExpenseSheet(models.Model):
         sheets = super(HrExpenseSheet, self.with_context(context)).create(vals_list)
         sheets.activity_update()
         return sheets
+
+    def write(self, vals):
+        if 'state' in vals or 'approval_state' in vals:
+            # Avoid user with write access on expense sheet in draft state to bypass the validation process
+            valid_states = {'submit', None}
+            if (
+                    not self.user_has_groups('hr_expense.group_hr_expense_manager')
+                    and any(state == 'draft' for state in self.mapped('state'))
+                    and (vals.get('state') not in valid_states or vals.get('approval_state') not in valid_states)
+            ):
+                raise UserError(_("You don't have the rights to bypass the validation process of this expense report."))
+            elif vals.get('state') == 'approve' or vals.get('approval_state') == 'approve':
+                self._check_can_approve()
+            elif vals.get('state') == 'cancel' or vals.get('approval_state') == 'cancel':
+                self._check_can_refuse()
+        return super().write(vals)
 
     @api.ondelete(at_uninstall=False)
     def _unlink_except_posted_or_paid(self):
@@ -696,13 +712,15 @@ class HrExpenseSheet(models.Model):
     def _do_reverse_moves(self):
         self = self.with_context(clean_context(self.env.context))
         moves = self.account_move_ids
-        draft_moves = moves.filtered(lambda m: m.state == 'draft')
-        non_draft_moves = moves - draft_moves
-        non_draft_moves._reverse_moves(
-            default_values_list=[{'invoice_date': fields.Date.context_today(move), 'ref': False} for move in non_draft_moves],
-            cancel=True
-        )
-        draft_moves.unlink()
+        if moves:
+            moves_sudo = self.sudo().account_move_ids
+            draft_moves_sudo = moves_sudo.filtered(lambda m: m.state == 'draft')
+            non_draft_moves_sudo = moves_sudo - draft_moves_sudo
+            non_draft_moves_sudo._reverse_moves(
+                default_values_list=[{'invoice_date': fields.Date.context_today(move), 'ref': False} for move in non_draft_moves_sudo],
+                cancel=True
+            )
+            draft_moves_sudo.unlink()
 
     def _prepare_bills_vals(self):
         self.ensure_one()
