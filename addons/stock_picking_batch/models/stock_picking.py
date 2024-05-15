8814c8727b59bf0c935a6a16e86199248af728f1
AllePilli <peal@odoo.com>
2024-05-15 07:37:50 +0000

    [ADD] l10n_ro_edi_stock, l10n_ro_edi_stock_batch: send transport data to eTransport platform
    Romania requires its companies to send information of all transports of goods on Romanian territory to a specific web service on their eTransport platform.
    
    This commit implements a way to gather all the necessary data needed to send the eTransport document (from a stock.picking or a stock.picking.batch) and the actual sending of this document to the eTransport platform.
    
    Authentication:
    the authentication needed to interact with the eTransport platform is the same as (and was already implemented in) the l10n_ro_efactura module. see [eFactura PR](https://github.com/odoo/odoo/pull/144061)
    
    eTransport flow:
    1. Add the necessary eTransport data to a delivery/batch transfer
    2. send the eTransport document to ANAF
    3. the document gets processed by ANAF
    4. Fetch the status of the document:
        - which can result in an error, in which case the flow starts again from the beginning with the corrected data
        - or in a success
    5. optionally send corrected document data to ANAF in which case we go back to step 3.
    
     task-id: 3810735
    
    closes odoo/odoo#196201
    
    X-original-commit: 68c70be7c0bd8954b3c3c4d45d53c9b1a70de50e
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Allesio Pellicciotta (peal) <peal@odoo.com>

--- addons/stock_picking_batch/models/stock_picking.py
+++ addons/stock_picking_batch/models/stock_picking.py
@@ -286,7 +286,7 @@ class StockPicking(models.Model):
         self.ensure_one()
         description_items = []
         if self.picking_type_id.batch_group_by_partner and self.partner_id:
-            description_items.append(self.partner_id.name)
+            description_items.append(self.partner_id.name or '')
         if self.picking_type_id.batch_group_by_destination and self.partner_id.country_id:
             description_items.append(self.partner_id.country_id.name)
         if self.picking_type_id.batch_group_by_src_loc and self.location_id:
