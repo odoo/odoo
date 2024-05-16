bd098917fdba438ccc8bdd1bacc2136ed6ccdbd0
Thomas Becquevort (thbe) <thbe@odoo.com>
2024-05-16 15:42:14 +0200

    [FIX] sale: Line name compatibility for invoices from SO
    Description of the issue/behavior this commit addresses:
    
    The previous commit changed the compute of the line_name in invoices so they
    do not include the product name anymore. Since then, a discordance has appeared
    between the Invoicing and the Sales apps where the product name is still used.
    
    Since the comute can't be changed alone and requires a new way of displaying
    the product name and line name, the fix is applied while the proper solution
    is implemented and will be removed afterwards.
    
    Desired behavior after this commit is merged:
    
    Invoices in the Invoicing apps created from a Sales Order of the Sales app
    don't have the product.partner_ref in the line name of said product.
    
    task-3888602
    
    Part-of: odoo/odoo#152869

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -3,6 +3,7 @@
 
 from collections import defaultdict
 from datetime import timedelta
+import re
 
 from markupsafe import Markup
 
@@ -1149,10 +1150,18 @@ class SaleOrderLine(models.Model):
         :rtype: dict
         """
         self.ensure_one()
+
+        # Compatibility fix for creating invoices from a SO since the computation of the line name has been changed in the account module.
+        # Has to be removed as soon as the new behavior for the line name has been implemented in the sale module.
+        line_name = self.name
+        if self.product_id.display_name:
+            line_name = re.sub(re.escape(self.product_id.display_name), '', line_name)
+            line_name = re.sub(r'^\n', '', line_name)
+            line_name = re.sub(r'(?<=\n) ', '', line_name)
         res = {
             'display_type': self.display_type or 'product',
             'sequence': self.sequence,
-            'name': self.name,
+            'name': line_name,
             'product_id': self.product_id.id,
             'product_uom_id': self.product_uom.id,
             'quantity': self.qty_to_invoice,
