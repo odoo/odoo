7fe4ebc0abb12b739b4921dc489d0bdc2094a177
Maryam Kia <maki@odoo.com>
2024-05-16 10:18:52 +0200

    [MOV] mail: move non use features from chatter in portal This change is the last move in splitting chatter between portal and web bundle.
    Related to task-2828744

--- addons/mail/static/src/chatter/web_portal/chatter.js
+++ addons/mail/static/src/chatter/web_portal/chatter.js
@@ -1,8 +1,5 @@
-import { AttachmentList } from "@mail/core/common/attachment_list";
 import { Composer } from "@mail/core/common/composer";
 import { Thread } from "@mail/core/common/thread";
-import { useMessageHighlight } from "@mail/utils/common/hooks";
-import { SearchMessagesPanel } from "@mail/core/common/search_messages_panel";
 
 import {
     Component,
@@ -13,11 +10,9 @@ import {
     useState,
 } from "@odoo/owl";
 
-import { Dropdown } from "@web/core/dropdown/dropdown";
 import { _t } from "@web/core/l10n/translation";
 import { useService } from "@web/core/utils/hooks";
 import { useThrottleForAnimation } from "@web/core/utils/timing";
-import { FileUploader } from "@web/views/fields/file_handler";
 
 /**
  * @typedef {Object} Props
@@ -25,37 +20,20 @@ import { FileUploader } from "@web/views/fields/file_handler";
  */
 export class Chatter extends Component {
     static template = "mail.Chatter";
-    static components = {
-        AttachmentList,
-        Dropdown,
-        Thread,
-        Composer,
-        FileUploader,
-        SearchMessagesPanel,
-    };
+    static components = { Thread, Composer };
     static props = ["threadId?", "threadModel"];
     static defaultProps = { threadId: false };
 
     setup() {
-        super.setup();
-        this.action = useService("action");
-        this.attachmentBox = useRef("attachment-box");
         this.store = useState(useService("mail.store"));
-        this.orm = useService("orm");
         this.state = useState({
-            composerType: false,
             jumpThreadPresent: 0,
             /** @type {import("models").Thread} */
             thread: undefined,
-            isSearchOpen: false,
         });
         this.rootRef = useRef("root");
         this.onScrollDebounced = useThrottleForAnimation(this.onScroll);
-        this.messageHighlight = useMessageHighlight();
-        useChildSubEnv({
-            inChatter: true,
-            messageHighlight: this.messageHighlight,
-        });
+        useChildSubEnv(this.childSubEnv);
 
         onMounted(() => {
             this.changeThread(this.props.threadModel, this.props.threadId);
@@ -84,6 +62,10 @@ export class Chatter extends Component {
 
     get afterPostRequestList() {
         return ["messages"];
+    }
+
+    get childSubEnv() {
+        return { inChatter: true };
     }
 
     get requestList() {
@@ -123,15 +105,6 @@ export class Chatter extends Component {
         this.state.jumpThreadPresent++;
         // Load new messages to fetch potential new messages from other users (useful due to lack of auto-sync in chatter).
         this.load(this.state.thread, this.afterPostRequestList);
-    }
-
-    onClickSearch() {
-        this.state.composerType = false;
-        this.state.isSearchOpen = !this.state.isSearchOpen;
-    }
-
-    closeSearch() {
-        this.state.isSearchOpen = false;
     }
 
     onScroll() {
