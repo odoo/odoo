f564ab5af6c0210ecd3b6371cf9f59b287dba34b
tsm-odoo <tsm@odoo.com>
2024-05-16 10:45:38 +0200

    [FIX] mail: retrieve bus last id for unread counter
    partial backport of https://github.com/odoo/odoo/pull/160700, required
    to fix message unread counter: when a message is retrieved, we need to
    know if it was already considered in the member unread counter to
    determine if we have to increment it.
    
    opw-3877370
    
    X-original-commit: 0e04dc0
    Part-of: odoo/odoo#168648
    Related: odoo/enterprise#64212
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/tests/helpers/mock_server/models/discuss_channel.js
+++ addons/mail/static/tests/helpers/mock_server/models/discuss_channel.js
@@ -571,6 +571,7 @@ patch(MockServer.prototype, {
      * @returns {Object[]}
      */
     _mockDiscussChannelChannelInfo(ids) {
+        const bus_last_id = this.lastBusNotificationId;
         const channels = this.getRecords("discuss.channel", [["id", "in", ids]]);
         return channels.map((channel) => {
             const members = this.getRecords("discuss.channel.member", [
@@ -606,6 +607,7 @@ patch(MockServer.prototype, {
                 Object.assign(res, {
                     custom_channel_name: memberOfCurrentUser.custom_channel_name,
                     message_unread_counter: memberOfCurrentUser.message_unread_counter,
+                    message_unread_counter_bus_id: bus_last_id,
                 });
                 if (memberOfCurrentUser.rtc_inviting_session_id) {
                     res["rtcInvitingSession"] = {
