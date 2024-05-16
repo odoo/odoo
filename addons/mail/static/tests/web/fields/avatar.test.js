7925a26b810af9dd283ccc80d715ab31988500d7
saurabh <sauc@odoo.com>
2024-05-16 14:35:36 +0530

    [IMP] mail: open avatar card in activity and gantt view
    Before this commit when clicked on the avatar the discuss
    chatter window is getting opened which is a bit weird and
    inconsistency among other views for example in kanban view
    the avatar card gets opened.
    
    After this commit when clicked on the avatar the avatar card
    will get open which will maintain consistency among other views.
    
    Task-3827385
    
    closes odoo/odoo#165950
    
    Signed-off-by: Saurabh Choraria (sauc) <sauc@odoo.com>

--- addons/mail/static/tests/web/fields/avatar.test.js
+++ addons/mail/static/tests/web/fields/avatar.test.js
@@ -1,6 +1,7 @@
 import { click, contains, defineMailModels, start } from "@mail/../tests/mail_test_helpers";
 import { describe, test } from "@odoo/hoot";
 import { mountWithCleanup } from "@web/../tests/web_test_helpers";
+import { serverState } from "@web/../tests/_framework/mock_server_state.hoot";
 
 import { Avatar } from "@mail/views/web/fields/avatar/avatar";
 
@@ -11,17 +12,17 @@ test("basic rendering", async () => {
     await start();
     await mountWithCleanup(Avatar, {
         props: {
-            resId: 2,
+            resId: serverState.userId,
             resModel: "res.users",
             displayName: "User display name",
         },
     });
     await contains(".o-mail-Avatar");
     await contains(".o-mail-Avatar img");
-    await contains(".o-mail-Avatar img[data-src='/web/image/res.users/2/avatar_128']");
+    await contains(".o-mail-Avatar img[data-src='/web/image/res.users/7/avatar_128']");
     await contains(".o-mail-Avatar span");
     await contains(".o-mail-Avatar span", { text: "User display name" });
-    await contains(".o-mail-ChatWindow", { count: 0 });
+    await contains(".o_avatar_card", { count: 0 });
     await click(".o-mail-Avatar img");
-    await contains(".o-mail-ChatWindow");
+    await contains(".o_avatar_card");
 });
