389e9b74c48eab5e98f13a547dcc89d13f9fbea7
Paul Stroobant <stpa@odoo.com>
2024-05-16 08:53:07 +0200

    [FIX] maintenance: use rawrecord id in calendar_with_recurrence
    Steps to reproduce the issue:
    
    1. In _Maintenance > Maintenance > Maintenance Requests_, set multiple maintenance requests with scheduled dates
    2. In _Maintenance > Maintenance > Maintenance Calendar_, get to a week or month with multiple requests
    3. Drag and drop one of the requests in the calendar
        - You can also double click or resize the request
    4. In most cases, the request that is changed or accessed is not the right one
    
    Explanation:
    
    `rawRecord` is the record as retrieved by `fetchRecords`, it is then processed in `loadRecords`. In `CalendarWithRecurrence`, with the option of having recurring events, `record.id` is nearly always different from `rawRecord.id`.
    https://github.com/odoo/odoo/blob/74a0703671f29fefad02843c0d679f903b812575/addons/maintenance/static/src/views/calendar_with_recurrence/calendar_with_recurrence_model.js#L11-L17
    
    Suggested fix:
    
    Overriding the methods that lead to the wrong model to replace `record.id` with `record.rawRecord.id` solves the issue without breaking other methods.
    For requests with recurrence, any change made using an occurrence in the calendar view would target the original request instead. Accessing it is not an issue, so we will only disable the edition of the occurrences.
    
    opw-3848278
    opw-3849852
    
    closes odoo/odoo#165662
    
    X-original-commit: 4b7a37231b7d18cadf523ae25b64a35b383f5471
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: Stroobant Paul (stpa) <stpa@odoo.com>

---
+++ addons/maintenance/tests/test_calendar_with_recurrence.py
@@ -0,0 +1,80 @@
+from odoo.tests import HttpCase, tagged
+from odoo.tools.float_utils import float_compare
+from datetime import timedelta, date, datetime
+
+
+@tagged('post_install', '-at_install')
+class TestCalendarWithRecurrence(HttpCase):
+
+    def test_dblclick_event_from_calendar(self):
+        self.env['maintenance.team'].create({
+            'name': 'the boys',
+        })
+        equipment = self.env['maintenance.equipment'].create({
+            'name': 'room'
+        })
+        self.env['maintenance.request'].create({
+            'name': 'send the mails',
+            'schedule_date': datetime.today() - timedelta(weeks=2),
+        })
+        request = self.env['maintenance.request'].create({
+            'name': 'clean the room',
+            'schedule_date': datetime.combine(date.today(), (datetime.min + timedelta(hours=10)).time()),  # today at 10.00 AM
+            'equipment_id': equipment.id,  # necessary for the tour to work with mrp_maintenance installed
+            'maintenance_type': 'preventive',
+            'recurring_maintenance': True,
+            'repeat_interval': 1,
+            'repeat_unit': 'day',
+            'duration': 1,
+        })
+        self.env['maintenance.request'].create({
+            'name': 'wash the car',
+            'schedule_date': datetime.today() + timedelta(weeks=1),
+        })
+
+        # The event should have a different id from the record
+        self.assertNotEqual(request.id, 1)
+
+        action = self.env["ir.actions.actions"]._for_xml_id("maintenance.hr_equipment_request_action_cal")
+        url = '/web?#action=%s' % (action['id'])
+        self.start_tour(url, 'test_dblclick_event_from_calendar', login='admin')
+
+        self.assertEqual(request.name, 'make your bed')
+        self.assertEqual(float_compare(request.duration, 2, 0), 0)
+
+    def test_drag_and_drop_calendar_event(self):
+        self.env['maintenance.team'].create({
+            'name': 'the boys',
+        })
+        self.env['maintenance.request'].create({
+            'name': 'send the mails',
+            'schedule_date': datetime.today() - timedelta(weeks=2),
+        })
+        request = self.env['maintenance.request'].create({
+            'name': 'clean the room',
+            'schedule_date': datetime.combine(date.today(), (datetime.min + timedelta(hours=10)).time()),  # today at 10.00 AM
+            'maintenance_type': 'preventive',
+            'recurring_maintenance': True,
+            'repeat_interval': 1,
+            'repeat_unit': 'day',
+            'duration': 1,
+        })
+        self.env['maintenance.request'].create({
+            'name': 'wash the car',
+            'schedule_date': datetime.today() + timedelta(weeks=1),
+        })
+
+        # The event should have a different id from the record
+        self.assertNotEqual(request.id, 1)
+
+        action = self.env["ir.actions.actions"]._for_xml_id("maintenance.hr_equipment_request_action_cal")
+        url = '/web?#action=%s' % (action['id'])
+        self.start_tour(url, 'test_drag_and_drop_event_in_calendar', login='admin')
+
+        today_as_weekday = (date.today().weekday() + 1) % 7  # Sunday is the first day of the week in the calendar
+        today_to_wednesday = 3 - today_as_weekday  # difference between Wednesday and today
+        target_datetime = datetime.combine(
+            date.today() + timedelta(days=today_to_wednesday),
+            (datetime.min + timedelta(hours=13.25)).time()
+        )  # this Wednesday at 1.15 PM
+        self.assertEqual(request.schedule_date, target_datetime)
