ba7f952196fdd769ea8c3978d67ec18402733228
Gabriel (gdpf) <gdpf@odoo.com>
2024-05-16 10:14:33 +0200

    [FIX] calendar: update calendar default privacy non-admins
    Before this commit, non-admin users were not able to update their
    calendar default privacy through the 'res.users' table, throwing
    an Access Rights error when doing so.
    
    After this commit, non-admin users are now able to do it since we
    added the 'calendar_default_privacy' field as WRITEABLE.
    
    Issue from: odoo/odoo#146614

--- addons/calendar/models/res_users.py
+++ addons/calendar/models/res_users.py
@@ -16,6 +16,10 @@ class Users(models.Model):
     def SELF_READABLE_FIELDS(self):
         return super().SELF_READABLE_FIELDS + ['calendar_default_privacy']
 
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + ['calendar_default_privacy']
+
     def _systray_get_calendar_event_domain(self):
         # Determine the domain for which the users should be notified. This method sends notification to
         # events occurring between now and the end of the day. Note that "now" needs to be computed in the
