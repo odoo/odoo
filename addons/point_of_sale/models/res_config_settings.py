63ca25ff24bf20a8a5fea598e206be05ce2c1aab
Olivier Monnom (mool) <mool@odoo.com>
2024-05-16 10:38:44 +0200

    [IMP] pos_mercado_pago: Add check box to install pos_mercado_pago module
    Previously we must install the pos_mercado module manually to be able to use the Mercado Pago payment provider in the POS.
    
    This committee adds a check box in the list of POS payment providers in order to automatically install it from the POS settings.
    
    mool

--- addons/point_of_sale/models/res_config_settings.py
+++ addons/point_of_sale/models/res_config_settings.py
@@ -40,6 +40,7 @@ class ResConfigSettings(models.TransientModel):
     module_pos_razorpay = fields.Boolean(string="Razorpay Payment Terminal", help="The transactions are processed by Razorpay. Set your Razorpay credentials on the related payment method.")
     module_pos_preparation_display = fields.Boolean(string="Preparation Display", help="Show orders on the preparation display screen.")
     module_pos_pricer = fields.Boolean(string="Pricer electronic price tags", help="Display the price of your products through electronic price tags")
+    module_pos_mercado_pago = fields.Boolean(string="Mercado Pago Payment Terminal", help="The transactions are processed by Mercado Pago. Set your Mercado Pago credentials on the related payment method.")
     update_stock_quantities = fields.Selection(related="company_id.point_of_sale_update_stock_quantities", readonly=False)
     account_default_pos_receivable_account_id = fields.Many2one(string='Default Account Receivable (PoS)', related='company_id.account_default_pos_receivable_account_id', readonly=False, check_company=True)
     barcode_nomenclature_id = fields.Many2one('barcode.nomenclature', related='company_id.nomenclature_id', readonly=False)
