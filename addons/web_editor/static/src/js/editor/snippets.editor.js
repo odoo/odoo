39923c2ebf24cd96c3d6a92a04d13c9d70888986
Benjamin Vray <bvr@odoo.com>
2024-05-16 15:42:27 +0200

    [FIX] web_editor: fix no dropzone after hiding a snippet's column
    Steps to reproduce the bug:
    
    - In Website edit mode.
    - Drop 2 "Columns" snippets.
    - Hide one of the columns in the first snippet by clicking on the "Hide
    on desktop" button of the "Column" options in the side panel.
    - Start to drag another snippet.
    - Bug: There is no dropzone between the 2 "Column" snippets.
    
    The issue happens because when the column becomes invisible, after
    clicking the button, the attribute 'data-invisible="1"' is added to the
    column in the DOM thanks the 'snippet_option_visibility_update' event.
    
    However, this event then propagates to the column's parent elements
    (<section> and <main>). So, the attribute 'data-invisible="1"' is also
    added to the <section>, causing the dropzone not to be inserted as
    expected. We should investigate this further later because it doesn't
    seem consistent. For now, we prefer not to make changes at this level to
    avoid introducing other bugs.
    
    In this commit, we change the selector that determines where not to
    insert dropzones so that it no longer considers 'data-invisible="1"'.
    This was added by this commit [1], but it seems unnecessary, and
    ':not(:visible)' in the selector is sufficient to avoid placing
    dropzones between two non-visible elements.
    
    [1]: https://github.com/odoo/odoo/commit/f9bd3033b21dbd1e4487d6d6cd1d8d8fdfbfb2ac
    
    task-3646232
    
    closes odoo/odoo#165797
    
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2435,7 +2435,7 @@ var SnippetsMenu = Widget.extend({
             $selectorSiblings = $(_.uniq(($selectorSiblings || $()).add($selectorChildren.children()).get()));
         }
 
-        var noDropZonesSelector = '[data-invisible="1"], .o_we_no_overlay, :not(:visible)';
+        const noDropZonesSelector = '.o_we_no_overlay, :not(:visible)';
         if ($selectorSiblings) {
             $selectorSiblings.not(`.oe_drop_zone, .oe_drop_clone, ${noDropZonesSelector}`).each(function () {
                 var data;
