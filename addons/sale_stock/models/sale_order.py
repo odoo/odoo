dc12da24d988b135366bc0353eb29c545aa6d2f3
jaha-odoo <jaha@odoo.com>
2024-05-16 16:15:29 +0530

    [IMP] stock: back2basics stock thd
    In this commit:
    ====================
    - Added new SO delivery status as 'preparation'.
    - Added the ability to group by product category in vendor pricelists.
    - Made the company field optional hide and the on-hand quantity  optional show
      in the  Lots/Serials list view.
    - Improved the replenishment page to display a toast notification and refresh
      when clicking on a single replenish action or selecting multiple lines for
      mass replenishment. The refresh action now retains filters.
    - In Inventory Configuration, renamed 'UOM Categories' to 'Unit of Measure'
      and  'Unit of Measure Categories' to 'Unit of Measure' in UOM views.
    - Hide the Routes smart button when routes are not active.
    - In the Purchase tab, made the variant column optional to show and preselected
      the current variant when adding a new line.
    - When a vendor is created from a product vendor pricelist, it is now created as
      a company.
    - Renamed 'Order Date' to 'Receipt Date' in the PO pivot view and added grouping
      by 'Confirmation Date'.
    - When setting a parent on a product category and no products are linked to the
      category yet, the valuation method is now the same as the parent's method.
    - Log cost changes in the chatter if made manually in the product form.
    - Removed the 'New' button when viewing the list of transfers linked to a PO/SO.
    
    task- 3850457

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -31,6 +31,7 @@ class SaleOrder(models.Model):
     delivery_count = fields.Integer(string='Delivery Orders', compute='_compute_picking_ids')
     delivery_status = fields.Selection([
         ('pending', 'Not Delivered'),
+        ('preparation', 'In Preparation'),
         ('partial', 'Partially Delivered'),
         ('full', 'Fully Delivered'),
     ], string='Delivery Status', compute='_compute_delivery_status', store=True,
@@ -80,8 +81,10 @@ class SaleOrder(models.Model):
                 order.delivery_status = False
             elif all(p.state in ['done', 'cancel'] for p in order.picking_ids):
                 order.delivery_status = 'full'
-            elif any(p.state == 'done' for p in order.picking_ids):
+            elif any(p.state == 'done' for p in order.picking_ids) and any(line.qty_delivered > 0 and line.qty_delivered < line.product_uom_qty for line in order.order_line):
                 order.delivery_status = 'partial'
+            elif any(p.state == 'assigned' for p in order.picking_ids):
+                order.delivery_status = 'preparation'
             else:
                 order.delivery_status = 'pending'
 
@@ -225,7 +228,7 @@ class SaleOrder(models.Model):
             picking_id = picking_id[0]
         else:
             picking_id = pickings[0]
-        action['context'] = dict(default_partner_id=self.partner_id.id, default_picking_type_id=picking_id.picking_type_id.id, default_origin=self.name, default_group_id=picking_id.group_id.id)
+        action['context'] = dict(self._context, default_partner_id=self.partner_id.id, default_picking_type_id=picking_id.picking_type_id.id, default_origin=self.name, default_group_id=picking_id.group_id.id, create=False)
         return action
 
     def _prepare_invoice(self):
