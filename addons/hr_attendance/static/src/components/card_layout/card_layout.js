f29524bb06ef174a86e620c64b5c398ec2214550
Xavier Luyckx (xlu) <xlu@odoo.com>
2024-05-17 14:20:59 +0200

    [IMP] hr_attendance: kiosk mode revamp
    This commit revamp the kiosk mode UI in hr_attendance, focusing on
    improving responsiveness, adapting the user experience, and addressing
    UI inconsistencies.
    
    task-3619682

--- addons/hr_attendance/static/src/components/card_layout/card_layout.js
+++ addons/hr_attendance/static/src/components/card_layout/card_layout.js
@@ -1,7 +1,10 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState, onWillUnmount } from "@odoo/owl";
+import { url } from "@web/core/utils/urls";
+import { browser } from "@web/core/browser/browser";
 
+const { DateTime } = luxon;
 export class CardLayout extends Component {
     static template = "hr_attendance.CardLayout";
     static props = {
@@ -11,4 +14,39 @@ export class CardLayout extends Component {
     static defaultProps = {
         kioskModeClasses: "",
     };
+
+    setup() {
+        const now = DateTime.now();
+        this.state = useState({
+            dayOfWeek: now.toFormat('cccc'), // 'Wednesday'
+            date: now.toLocaleString({ ...DateTime.DATE_FULL, weekday: undefined }),
+            time: now.toLocaleString(DateTime.TIME_SIMPLE)
+        });
+        this.timeInterval = setInterval(() => {
+            const now = DateTime.now();
+            this.state.dayOfWeek = now.toFormat('cccc');
+            this.state.date = now.toLocaleString({ ...DateTime.DATE_FULL, weekday: undefined });
+            this.state.time = now.toLocaleString(DateTime.TIME_SIMPLE);
+        }, 1000);
+        this.companyImageUrl = url("/web/binary/company_logo", {
+            company: this.props.companyId,
+        });
+        onWillUnmount(() => {
+            clearInterval(this.timeInterval);
+        });
+    }
+
+    get showDemoMessage() {
+        return true;
+    }
+
+    removeDemoMessage(){
+        browser.localStorage.setItem("hr_attendance.noShowDemoMessage", true);
+        return;
+    }
+
+    getCurrentTime() {
+        return DateTime.now().toLocaleString(DateTime.TIME_SIMPLE);
+    }
+
 }
