e4ccc2c2860dae7762dac4436c69d85341118ef3
Brieuc-brd <brd@odoo.com>
2024-05-17 16:15:43 +0200

    [IMP] website: adapt "thank you" page
    This commit enhances the layout of the "thank you" page of `website` to
    make it more elegant. The purpose is also to use the same design for the
    other pages of the same type to maintain layout consistency (see [1]).
    
    [1]: https://github.com/odoo/enterprise/pull/62839
    
    task-3790303
    
    closes odoo/odoo#166138
    
    Related: odoo/enterprise#62839
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/static/tests/tours/website_form_editor.js
+++ addons/website/static/tests/tours/website_form_editor.js
@@ -873,7 +873,7 @@ function editContactUs(steps) {
             run: "click",
         }, {
             content: "Check the form was sent (success page without form)",
-            trigger: ':iframe body:not(:has([data-snippet="s_website_form"])) .fa-check-circle',
+            trigger: ':iframe body:not(:has([data-snippet="s_website_form"])) .fa-paper-plane',
         }, {
             content: "Go back to the form",
             trigger: ':iframe a.navbar-brand.logo',
@@ -890,7 +890,7 @@ function editContactUs(steps) {
             run: "click",
         }, {
             content: "Check the form was again sent (success page without form)",
-            trigger: ':iframe body:not(:has([data-snippet="s_website_form"])) .fa-check-circle',
+            trigger: ':iframe body:not(:has([data-snippet="s_website_form"])) .fa-paper-plane',
         }
     ]);
 
@@ -1012,7 +1012,7 @@ function editContactUs(steps) {
             run: "click",
         }, {
             content: "Check the form was again sent (success page without form)",
-            trigger: ":iframe body:not(:has([data-snippet='s_website_form'])) .fa-check-circle",
+            trigger: ":iframe body:not(:has([data-snippet='s_website_form'])) .fa-paper-plane",
         },
     ]);
 
