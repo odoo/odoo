92408dccb7b660c1794f1d4f58c752f24a320afe
Carlos <A01708634@tec.mx>
2024-05-18 18:56:37 -0600

    feat(Model): Added model function to usuario_objetivo_rel and added cross functionality between the usuario_objetivo_rel and objetivo models in the view

--- talent360/evaluaciones/models/usuario_objetivo_rel.py
+++ talent360/evaluaciones/models/usuario_objetivo_rel.py
@@ -16,3 +16,20 @@ class UsuarioObjetivoRel(models.Model):
 
     objetivo_id = fields.Many2one("objetivo", string="Objetivos")
     usuario_id = fields.Many2one("res.users", string="Usuario")
+
+    titulo = fields.Char(related='objetivo_id.titulo', string="Título del Objetivo")
+    fecha_fin = fields.Date(related='objetivo_id.fecha_fin', string="Fecha Fin")
+    tipo = fields.Selection(related='objetivo_id.tipo', string="Tipo")
+    estado = fields.Selection(related='objetivo_id.estado', string="Estado")
+    peso = fields.Integer(related='objetivo_id.peso', string="Peso")
+    piso_minimo = fields.Integer(related='objetivo_id.piso_minimo', string="Piso Mínimo")
+    piso_maximo = fields.Integer(related='objetivo_id.piso_maximo', string="Piso Máximo")
+
+    def open_objetivo_form(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'objetivo',
+            'view_mode': 'form',
+            'res_id': self.objetivo_id.id,
+        }
