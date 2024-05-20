9b341d6670cf5e34dfa9a2bc6313d593cb1b1be6
Zeel Patel <zepa@odoo.com>
2024-05-20 12:31:25 +0530

    [ADD] point_of_sale: link order invoices with reversed orders
    In this commit:
    - Added a new field to the Journal Entry to reference the POS order, which is
    also included in the reversed entry of the POS order.
    - Enhances traceability between original and reversed POS orders and their
    associated invoices.
    
    Task Id: 3937517

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -820,6 +820,7 @@ class PosOrder(models.Model):
             'date': fields.Date.context_today(self),
             'ref': _('Reversal of POS closing entry %s for order %s from session %s', self.session_move_id.name, self.name, self.session_id.name),
             'invoice_line_ids': [(0, 0, aml_value) for aml_value in move_lines],
+            'reversed_pos_order_id': self.id
         })
         reversal_entry.action_post()
 
