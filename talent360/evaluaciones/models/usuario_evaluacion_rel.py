a828cb52cbd5eea86f65ec99f3d01b937d7aa54b
dvegaa20 <diego.vega.camacho@gmail.com>
2024-05-21 14:28:24 -0600

    feat(correos): changed mail structure & variables

--- talent360/evaluaciones/models/usuario_evaluacion_rel.py
+++ talent360/evaluaciones/models/usuario_evaluacion_rel.py
@@ -47,12 +47,6 @@ class UsuarioEvaluacionRel(models.Model):
         string="Usuarios de Evaluación",
         readonly=True,
     )
-    fecha_inicio_evaluacion = fields.Date(
-        related="evaluacion_id.fecha_inicio", string="Fecha de inicio de evaluación", store=True
-    )
-    fecha_final_evaluacion = fields.Date(
-        related="evaluacion_id.fecha_final", string="Fecha de finalización de evaluación", store=True
-    )
 
     token = fields.Char()
 
@@ -140,18 +134,17 @@ class UsuarioEvaluacionRel(models.Model):
                     raise ValueError(_("No se encontró un usuario asociado"))
 
                 usuario.write({"token": token, "contestada": "pendiente"})
-
                 evaluacion_url = f"{base_url}/{evaluacion_id}/{token}"
+                contenido_adicional = f"""<hr>{evaluacion.contenido_correo}<hr>""" if evaluacion.contenido_correo else ""
                 mail = {
                     "subject": "Invitación para completar la evaluación",
                     "email_from": "talent360@cr-organizacional.com",
                     "email_to": correo,
                     "body_html": 
                         f"""<p>Hola, <strong>{nombre}</strong>,</p>
-                        <p>En <strong>{self.env.user.company_id.name}</strong> estamos interesados en que contestes la siguiente evaluación, tu participación nos ayudará a mejorar y crecer como organización. La evaluación estará disponible del <strong>{evaluacion.fecha_inicio}</strong> al <strong>{evaluacion.fecha_final}</strong>. Puedes comenzar la evaluación haciendo clic en el siguiente enlace: <a href="{evaluacion_url}">Comenzar Evaluación</a></p>
+                        {contenido_adicional}
+                        <p>En <strong>{self.env.user.company_id.name}</strong> estamos interesados en que contestes la siguiente evaluación, tu participación nos ayudará a mejorar y crecer como organización. La evaluación estará disponible del <strong>{evaluacion.fecha_inicio}</strong> al <strong>{evaluacion.fecha_final}</strong>. Puedes comenzar la evaluación haciendo clic en el siguiente enlace: <a href="{evaluacion_url}">Comenzar {evaluacion.nombre}</a></p>
                         <p>Gracias por tu colaboración.</p>
-                        <hr>
-                        <hr>
                         <p>Atentamente,</p>
                         <p><strong>{self.env.user.name}</strong> from <strong>{self.env.user.company_id.name}</strong></p>
                         <p>Correo de contacto: <a href="mailto:{self.env.user.email}">{self.env.user.email}</a></p>
@@ -161,11 +154,3 @@ class UsuarioEvaluacionRel(models.Model):
                 lista_mails.append(mail)
 
         self.env["mail.mail"].create(lista_mails)
-
-        return {
-            "type": "ir.actions.act_window",
-            "name": "Evaluaciones",
-            "res_model": "evaluacion.evaluacion",
-            "view_mode": "tree,form",
-            "target": "current",
-        }
