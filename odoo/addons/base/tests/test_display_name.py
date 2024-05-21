c95c5bd0f684347dcc9b3197955f2880c31700bd
Rémy Voet (ryv) <ryv@odoo.com>
2024-05-21 16:59:03 +0200

    [IMP] tests: automatically test 'New' button of Form views
    In https://github.com/odoo/odoo/issues/141181, we introduced a generic
    test to ensure the robustness of `_compute_display_name` by using
    onchange(). However, due to the next commit and the naive nature of
    the test, many models failed the test even when the model (or/and the
    _compute_display_name) didn't cause a real problem: we tested models
    without form view or transient model where it is mostly necessary to
    set default context keys.
    
    To find potential new problems caused by the next commit, and to avoid
    falsely failing the test:
    - We only test non-transient models where there is exists a creatable
    form.
    - We use the Form class to simulate the first onchange (for the next
    commit).
    - We changed the implementation of the Form class to add display_name
    for non-transient models, because the form_controller.js implicitly adds
    it to the form (if there is a control panel: almost every time for
    non-transient models). This covers the same range of code as the old
    one.
    
    Also move the test in the web module, because onchange() implementation
    is there.
    
    WIP: There a already failing model

--- odoo/addons/base/tests/test_display_name.py
+++ odoo/addons/base/tests/test_display_name.py
@@ -1,34 +1,46 @@
 import contextlib
+from lxml import etree
 
 from odoo.exceptions import UserError
-from odoo.tests.common import TransactionCase, tagged
+from odoo.tests import TransactionCase, tagged, Form
 
 
 IGNORE_MODEL_NAMES = {
-    'ir.attachment',
-    'test_new_api.attachment',
-    'payment.link.wizard',
-    'account.multicurrency.revaluation.wizard',
-    'account_followup.manual_reminder',
+    'account.report.line',  # only used as wizard, and display_name isn't compute in a wizard but Form add display_name automatically
+    'chatbot.script.step',  # only used as wizard
+    'stock.warehouse',  # avoid warning "Creating a new warehouse will automatically activate the Storage Locations setting"
+    'website.visitor',  # Visitors can only be created through the frontend.
+    'marketing.activity',  # only used as wizard and always used form marketing.campaign
 }
+
 
 @tagged('-at_install', 'post_install')
 class TestEveryModel(TransactionCase):
 
-    def test_display_name_new_record(self):
+    def test_form_new_record(self):
         for model_name in self.registry:
             model = self.env[model_name]
-            if model._abstract or not model._auto or model_name in IGNORE_MODEL_NAMES:
+            if (
+                model._abstract
+                or model._transient
+                or not model._auto
+                or model_name in IGNORE_MODEL_NAMES
+            ):
+                continue
+
+            default_form_id = self.env['ir.ui.view'].default_view(model_name, 'form')
+            if not default_form_id:
+                continue
+
+            default_form = self.env['ir.ui.view'].browse(default_form_id)
+            view_elem = etree.fromstring(default_form.arch)
+            if view_elem.get('create') in ("0", "false"):
                 continue
 
             with self.subTest(
-                msg="`_compute_display_name` doesn't work with new record (first onchange call).",
+                msg="Create a new record from form view doesn't work (first onchange call).",
                 model=model_name,
-            ):
-                # Check that the first onchange with display_name works on every models
-                # OR it will fail anyway when people will use click on New
-                fields_used = model._fields['display_name'].get_depends(model)[0]
-                fields_used = [f.split('.', 1)[0] for f in fields_used]
-                fields_spec = dict.fromkeys(fields_used + ['display_name'], {})
-                with contextlib.suppress(UserError):
-                    model.onchange({}, [], fields_spec)
+            ), contextlib.suppress(UserError):
+                # Test to open the Form view to check first onchange
+                # (including display_name on new records)
+                Form(model)
