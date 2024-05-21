59f459428db2587bd889fd42fbb7aa6d7bb7eccc
Deependra Solanki <deso@odoo.com>
2024-05-21 14:33:14 +0530

    [FIX] web_editor: selection across contenteditable false element
    Before this commit:
    
    Previously, selection using Shift + Arrow keys was not possible across
    contenteditable="false" elements such as banners and clipboards.
    This commit makes sure now selection be done across these elements
    using Shift + Arrow keys.
    
    task-3502724

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -113,6 +113,8 @@ const IS_KEYBOARD_EVENT_UNDERLINE = ev => ev.key === 'u' && (ev.ctrlKey || ev.me
 const IS_KEYBOARD_EVENT_STRIKETHROUGH = ev => ev.key === '5' && (ev.ctrlKey || ev.metaKey);
 const IS_KEYBOARD_EVENT_LEFT_ARROW = ev => ev.key === 'ArrowLeft' && !(ev.ctrlKey || ev.metaKey);
 const IS_KEYBOARD_EVENT_RIGHT_ARROW = ev => ev.key === 'ArrowRight' && !(ev.ctrlKey || ev.metaKey);
+const IS_KEYBOARD_EVENT_UP_ARROW = ev => ev.key === 'ArrowUp' && !(ev.ctrlKey || ev.metaKey);
+const IS_KEYBOARD_EVENT_DOWN_ARROW = ev => ev.key === 'ArrowDown' && !(ev.ctrlKey || ev.metaKey);
 
 const CLIPBOARD_BLACKLISTS = {
     unwrap: ['.Apple-interchange-newline', 'DIV'], // These elements' children will be unwrapped.
@@ -3899,10 +3901,28 @@ export class OdooEditor extends EventTarget {
             this.execCommand('strikeThrough');
         } else if (IS_KEYBOARD_EVENT_LEFT_ARROW(ev) || IS_KEYBOARD_EVENT_RIGHT_ARROW(ev)) {
             const side = ev.key === 'ArrowLeft' ? 'previous' : 'next';
-            const { anchorNode, anchorOffset } = this.document.getSelection() || {};
+            const selection = this.document.getSelection();
+            const { anchorNode, anchorOffset, focusNode, focusOffset } = selection || {};
             // If the selection is at the edge of a code element at the edge of
             // its parent, make sure there's a zws next to it, where the
             // selection can then be set.
+            if (ev.shiftKey) {
+                const currentBlock = closestBlock(focusNode);
+                const isSelectionAtStart = firstLeaf(currentBlock) === focusNode && focusOffset === 0;
+                const isSelectionAtEnd = lastLeaf(currentBlock) === focusNode && focusOffset === nodeSize(focusNode);
+                const adjacentBlock = side === 'previous' ? currentBlock.previousElementSibling : currentBlock.nextElementSibling;
+                const targetBlock = side === 'previous' ? adjacentBlock?.previousElementSibling : adjacentBlock?.nextElementSibling;
+                if (targetBlock && !adjacentBlock.isContentEditable) {
+                    const lastNode = lastLeaf(targetBlock);
+                    if (side === 'previous' && isSelectionAtStart) {
+                        selection.extend(lastNode, nodeSize(lastNode));
+                        ev.preventDefault();
+                    } else if (side === 'next' && isSelectionAtEnd) {
+                        selection.extend(lastNode, 0);
+                        ev.preventDefault();
+                    }
+                }
+            }
             const codeElement = anchorNode && closestElement(anchorNode, 'code');
             const siblingProperty = `${side}Sibling`;
             if (
@@ -3947,6 +3967,17 @@ export class OdooEditor extends EventTarget {
                     ev.preventDefault();
                     ev.stopPropagation();
                 }
+            }
+        } else if ((IS_KEYBOARD_EVENT_UP_ARROW(ev) || IS_KEYBOARD_EVENT_DOWN_ARROW(ev)) && ev.shiftKey) {
+            const selection = this.document.getSelection();
+            const currentBlock = closestBlock(selection.focusNode);
+            const adjacentBlock = ev.key === 'ArrowUp' ? currentBlock.previousElementSibling : currentBlock.nextSibling;
+            const targetBlock = ev.key === 'ArrowUp' ? adjacentBlock?.previousElementSibling : adjacentBlock?.nextElementSibling;
+            if (targetBlock && !adjacentBlock.isContentEditable) {
+                const leafNode = lastLeaf(targetBlock);
+                const offset = ev.key === 'ArrowUp' ? nodeSize(leafNode) : 0;
+                selection.extend(leafNode, offset);
+                ev.preventDefault();
             }
         }
     }
