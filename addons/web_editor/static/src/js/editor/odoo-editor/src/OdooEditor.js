f3861db109c2e5e60681496b9deef6b1197a827d
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-05-22 14:33:59 +0200

    [FIX] web_editor: fix failing test race condition
    Issue:
    ======
    Non-deterministic race condition in test `test_blog_post_tags` and
    `test_10_shop_editor_set_product_ribbon` due to the fact that
    `ownerDocument.defaultView` is null.
    
    Similar issue resolved [here].
    
    [here]: https://github.com/odoo/odoo/blob/9a11717c17b860ec2f1b2e228517c0d3945c474a/addons/web_editor/static/src/js/editor/snippets.editor.js#L277-L282
    
    closes odoo/odoo#166675
    
    X-original-commit: 4d85bc3f0496021ae6c7c09e9f4b85ff1f8d6ab9
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Mahdi Cheikh Rouhou (macr) <macr@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -4196,6 +4196,9 @@ export class OdooEditor extends EventTarget {
      */
     _resetLinkInSelection() {
         const selection = this.document.getSelection();
+        if (!selection) {
+            return;
+        }
         const [anchorLink, focusLink] = [selection.anchorNode, selection.focusNode]
             .map(node => closestElement(node, 'a:not(.btn)'));
         const singleLinkInSelection = anchorLink === focusLink && anchorLink && isLinkEligibleForZwnbsp(this.editable, anchorLink) && anchorLink;
