7d805d12cf93a0e46d099621a675b1a6b378021e
unknown <urigopar889@gmail.com>
2024-05-22 10:26:10 -0600

    fix:condicionales_csv

--- talent360/evaluaciones/wizards/importar_preguntas_wizard.py
+++ talent360/evaluaciones/wizards/importar_preguntas_wizard.py
@@ -8,15 +8,27 @@ class ImportQuestionsWizard(models.TransientModel):
     _description = 'Wizard to import questions from CSV'
 
     archivo = fields.Binary()
-
-    
     nombre_archivo = fields.Char()
 
     required_fields = [
         "Pregunta",
         "Tipo",
-        "Ponderacion",
         "Categoria",
+    ]
+
+    tipo_valido = ["multiple_choice", "open_question", "escala"]
+    ponderacion_valida = ["ascendente", "descendente"]
+    categoria_valida = [
+        "datos_generales",
+        "reclutamiento_y_seleccion_de_personal",
+        "formacion_y_capacitacion",
+        "permanencia_y_ascenso",
+        "corresponsabilidad_en_la_vida_laboral_familiar_y_personal",
+        "clima_laboral_libre_de_violencia",
+        "acoso_y_hostigamiento",
+        "accesibilidad",
+        "respeto_a_la_diversidad",
+        "condiciones_generales_de_trabajo"
     ]
 
     @api.constrains("nombre_archivo")
@@ -50,14 +62,16 @@ class ImportQuestionsWizard(models.TransientModel):
             
             self._validar_fila(fila)
 
-            preguntas.append(
-                {
-                    "pregunta_texto": fila["Pregunta"],
-                    "tipo": fila["Tipo"],
-                    "ponderacion": fila["Ponderacion"],
-                    "categoria": fila["Categoria"],
-                }
-            )
+            pregunta_data = {
+                "pregunta_texto": fila["Pregunta"],
+                "tipo": fila["Tipo"],
+                "categoria": fila["Categoria"],
+            }
+
+            if fila["Tipo"] == "escala":
+                pregunta_data["ponderacion"] = fila["Ponderacion"]
+
+            preguntas.append(pregunta_data)
 
         preguntas_db = self.env["pregunta"].create(preguntas)
         evaluacion = self.env["evaluacion"].browse(self._context.get("active_id"))
@@ -66,8 +80,7 @@ class ImportQuestionsWizard(models.TransientModel):
             evaluacion.write({'pregunta_ids': [(4, pregunta.id) for pregunta in preguntas_db]})
         else:
             raise exceptions.ValidationError(_("No se encontró la evaluación en el contexto."))
-        
-    
+
     def _validate_columns(self, columnas: list[str]):
         # Valida que las columnas del archivo CSV sean las correctas
         columnas_faltantes = []
@@ -101,7 +114,35 @@ class ImportQuestionsWizard(models.TransientModel):
             raise exceptions.ValidationError(
                 f"Los siguientes campos son requeridos: {', '.join(campos)}"
             )
-
+        
+        # Validar que el tipo de pregunta sea válido
+        if row["Tipo"] not in self.tipo_valido:
+            raise exceptions.ValidationError(
+                f"El tipo de pregunta '{row['Tipo']}' no es válido. Los tipos permitidos son: {', '.join(self.tipo_valido)}."
+            )
+        
+        # Validar que la categoría sea válida
+        if row["Categoria"] not in self.categoria_valida:
+            raise exceptions.ValidationError(
+                f"La categoría '{row['Categoria']}' no es válida. Las categorías permitidas son: {', '.join(self.categoria_valida)}."
+            )
+        
+        # Validar que la ponderación sea válida si el tipo es 'escala'
+        if row["Tipo"] == "escala":
+            if not row.get("Ponderacion"):
+                raise exceptions.ValidationError(
+                    "La ponderación es requerida para preguntas de tipo 'escala'."
+                )
+            if row["Ponderacion"] not in self.ponderacion_valida:
+                raise exceptions.ValidationError(
+                    f"La ponderación '{row['Ponderacion']}' no es válida. Las ponderaciones permitidas son: {', '.join(self.ponderacion_valida)}."
+                )
+        else:
+            # No permitir ponderación para otros tipos de preguntas
+            if row.get("Ponderacion"):
+                raise exceptions.ValidationError(
+                    f"No se permite la ponderación para preguntas de tipo '{row['Tipo']}'."
+                )
 
     def descargar_template(self):
         # Define el contenido del archivo CSV de la plantilla
@@ -124,7 +165,7 @@ class ImportQuestionsWizard(models.TransientModel):
                         "name": nombre_archivo,
                         "type": "binary",
                         "datas": base64.b64encode(datos.encode("utf-8")),
-                        "res_model": nombre_archivo,
+                        "res_model": self._name,
                         "res_id": self.id,
                     }
                 )
