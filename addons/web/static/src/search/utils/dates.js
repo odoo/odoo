christian-rb/odoo-dev

[IMP] web: add support for domains on date filters
This commit adds the possibility to define custom domains on date filters,
which was previously only possible on non-date filters.

Whenever a date filter is selected, the domain will be used to further
restrict the displayed record on top of the date domain.

Task-3692123

addons/web/static/src/search/utils/dates.js
@@ -156,7 +156,10 @@ export function constructDateDomain(
         selectedOptions = getSelectedOptions(referenceMoment, searchItem, selectedOptionIds);
     }
     if ("domain" in selectedOptions) {
-        return selectedOptions.domain[0];
+        return {
+            description: selectedOptions.domain[0].description,
+            domain: Domain.and([selectedOptions.domain[0].domain, searchItem.domain]),
+        };
     }
     const yearOptions = selectedOptions.year;
     const otherOptions = [...(selectedOptions.quarter || []), ...(selectedOptions.month || [])];
@@ -192,10 +195,11 @@ export function constructDateDomain(
             ranges.push(range);
         }
     }
-    const domain = Domain.combine(
+    let domain = Domain.combine(
         ranges.map((range) => range.domain),
         "OR"
     );
+    domain = Domain.and([domain, searchItem.domain]);
     const description = ranges.map((range) => range.description).join("/");
     return { domain, description };
 }

addons/web/static/tests/search/search_model.test.js
@@ -214,6 +214,7 @@ test("parsing one filter tag with default_period date attribute", async () => {
         {
             defaultGeneratorIds: ["year_0", "year_-1"],
             description: "Date",
+            domain: "[]",
             fieldName: "date_field",
             fieldType: "date",
             type: "dateFilter",
@@ -360,6 +361,7 @@ test("parsing one filter tag with date attribute ", async () => {
         {
             defaultGeneratorIds: ["month_0"],
             description: "Date",
+            domain: "[]",
             fieldName: "date_field",
             fieldType: "date",
             name: "date_filter",
@@ -892,6 +894,47 @@ test("toggle a custom filter in a date filter", async () => {
     ]);
     model.toggleDateFilter(filterId, "custom_today");
     expect(model.domain).toEqual([["date_field", "=", "2019-01-06"]]);
+});
+
+test("toggle a date filter with a domain", async () => {
+    mockDate("2019-01-06T15:00:00");
+    const model = await createSearchModel({
+        searchViewArch: `
+            <search>
+                <filter name="date_filter" date="date_field" string="DateFilter" domain="[('float_field', '>=', '0')]"/>
+            </search>
+        `,
+    });
+    const filterId = Object.keys(model.searchItems).map((key) => Number(key))[0];
+    expect(model.domain).toEqual([]);
+    model.toggleDateFilter(filterId);
+    expect(model.domain).toEqual([
+        "&",
+        "&",
+        ["date_field", ">=", "2019-01-01"],
+        ["date_field", "<=", "2019-01-31"],
+        ["float_field", ">=", "0"],
+    ]);
+});
+
+test("toggle a custom filter in a date filter with a domain", async () => {
+    mockDate("2019-01-06T15:00:00");
+    const model = await createSearchModel({
+        searchViewArch: `
+            <search>
+                <filter name="date_filter" date="date_field" string="DateFilter" domain="[('float_field', '>=', '0')]">
+                    <filter name="today" string="Today" domain="[('date_field', '=', context_today().strftime('%Y-%m-%d'))]"/>
+                </filter>
+            </search>
+        `,
+    });
+    const filterId = Object.keys(model.searchItems).map((key) => Number(key))[0];
+    model.toggleDateFilter(filterId, "custom_today");
+    expect(model.domain).toEqual([
+        "&",
+        ["date_field", "=", "2019-01-06"],
+        ["float_field", ">=", "0"],
+    ]);
 });
 
 test("toggle a groupBy", async () => {

odoo/odoo

addons/web/static/src/search/utils/dates.js
@@ -104,7 +104,10 @@ export function constructDateDomain(
         selectedOptions = getSelectedOptions(referenceMoment, searchItem, selectedOptionIds);
     }
     if ("withDomain" in selectedOptions) {
-        return selectedOptions.withDomain[0];
+        return {
+            description: selectedOptions.withDomain[0].description,
+            domain: Domain.and([selectedOptions.withDomain[0].domain, searchItem.domain]),
+        };
     }
     const yearOptions = selectedOptions.year;
     const otherOptions = [...(selectedOptions.quarter || []), ...(selectedOptions.month || [])];
@@ -140,10 +143,11 @@ export function constructDateDomain(
             ranges.push(range);
         }
     }
-    const domain = Domain.combine(
+    let domain = Domain.combine(
         ranges.map((range) => range.domain),
         "OR"
     );
+    domain = Domain.and([domain, searchItem.domain]);
     const description = ranges.map((range) => range.description).join("/");
     return { domain, description };
 }

addons/web/static/tests/search/search_model.test.js
@@ -214,6 +214,7 @@ test("parsing one filter tag with default_period date attribute", async () => {
         {
             defaultGeneratorIds: ["year", "year-1"],
             description: "Date",
+            domain: "[]",
             fieldName: "date_field",
             fieldType: "date",
             type: "dateFilter",
@@ -360,6 +361,7 @@ test("parsing one filter tag with date attribute ", async () => {
         {
             defaultGeneratorIds: ["month"],
             description: "Date",
+            domain: "[]",
             fieldName: "date_field",
             fieldType: "date",
             name: "date_filter",
@@ -892,6 +894,47 @@ test("toggle a custom option in a date filter", async () => {
     ]);
     model.toggleDateFilter(filterId, "custom_today");
     expect(model.domain).toEqual([["date_field", "=", "2019-01-06"]]);
+});
+
+test("toggle a date filter with a domain", async () => {
+    mockDate("2019-01-06T15:00:00");
+    const model = await createSearchModel({
+        searchViewArch: `
+            <search>
+                <filter name="date_filter" date="date_field" string="DateFilter" domain="[('float_field', '>=', '0')]"/>
+            </search>
+        `,
+    });
+    const filterId = Object.keys(model.searchItems).map((key) => Number(key))[0];
+    expect(model.domain).toEqual([]);
+    model.toggleDateFilter(filterId);
+    expect(model.domain).toEqual([
+        "&",
+        "&",
+        ["date_field", ">=", "2019-01-01"],
+        ["date_field", "<=", "2019-01-31"],
+        ["float_field", ">=", "0"],
+    ]);
+});
+
+test("toggle a custom option in a date filter with a domain", async () => {
+    mockDate("2019-01-06T15:00:00");
+    const model = await createSearchModel({
+        searchViewArch: `
+            <search>
+                <filter name="date_filter" date="date_field" string="DateFilter" domain="[('float_field', '>=', '0')]">
+                    <filter name="today" string="Today" domain="[('date_field', '=', context_today().strftime('%Y-%m-%d'))]"/>
+                </filter>
+            </search>
+        `,
+    });
+    const filterId = Object.keys(model.searchItems).map((key) => Number(key))[0];
+    model.toggleDateFilter(filterId, "custom_today");
+    expect(model.domain).toEqual([
+        "&",
+        ["date_field", "=", "2019-01-06"],
+        ["float_field", ">=", "0"],
+    ]);
 });
 
 test("toggle a groupBy", async () => {