0d769b5b0608c1b2411a6b5073d539a89b888754
ramh <ramh@odoo.com>
2024-05-22 18:08:44 +0530

    [IMP] hr_skills: convert tests to hoot
    This commit aims to convert QUnit tests which rely on
    hr_skills/test_utils to hoot.
    
    Part of Task-3818666
    
    closes odoo/odoo#174450
    
    X-original-commit: 705da5c0c25496bc9692710981e6df93a467178a
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/hr_skills/static/tests/disable_patch.js
+++
@@ -1,5 +0,0 @@
-/** @odoo-module */
-
-import { unpatchAvatarCardResourcePopover } from "@hr_skills/components/avatar_card_resource/avatar_card_resource_popover_patch";
-
-unpatchAvatarCardResourcePopover();
---
+++ addons/hr_skills/static/tests/mock_server/mock_models/hr_skill.js
@@ -0,0 +1,5 @@
+import { models } from "@web/../tests/web_test_helpers";
+
+export class HrSkill extends models.ServerModel {
+    _name = "hr.skill";
+}
