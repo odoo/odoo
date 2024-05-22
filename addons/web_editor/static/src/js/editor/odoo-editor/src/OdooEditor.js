48577514f44950876191597b8fb6769cbf319008
Deependra Solanki <deso@odoo.com>
2024-05-22 17:06:35 +0530

    [FIX] web_editor: stop unnecessary calls to _handleSelectionInTable
    Previously, if a mouseup event occured outside the editable area,
    `currentMouseState` remained as mousedown, causing unnecessary calls to
    `_handleSelectionInTable` and some other functions such as `getDeepRange`,
    `getDeepestPosition`, `ancestors`, `closestElement`, etc., during mousemove in
    editable area. This commit makes sure that when a mouseup event happens outside
    the editable area, `currentMouseState` is updated to mouseup, preventing these
    unnecessary calls.
    
    X-original-commit: 9c8265f588b0de3d4076e9ccaf53fe15084527a9

--- addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/OdooEditor.js
@@ -4735,7 +4735,8 @@ export class OdooEditor extends EventTarget {
         }
     }
 
-    _onDocumentMouseup() {
+    _onDocumentMouseup(ev) {
+        this._currentMouseState = ev.type;
         if (this.toolbar) {
             this.toolbar.style.pointerEvents = 'auto';
         }
