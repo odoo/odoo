2deb276a2c78611c680cfce93122fe81f25d7dfa
Nishant Jain <niai@odoo.com>
2024-05-22 12:56:40 +0530

    [ADD] l10n_in_withholding: implement withholding for TDS Entry
    - Added Taxes for TDS sale type
    - Added Field in Taxes for TDS Tax Type
    - Add the withholding control account
    - Added wizard for creating withholding(TDS) entries for invoice/bill and
    customer/vendor payment.
    - Also while creating a withholding(TDS) entry for an invoice/bill it will
    reconcile the withholding entry.
    
    Task - 3326777
    
    closes odoo/odoo#168005
    
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

---
+++ addons/l10n_in_withholding/models/__init__.py
@@ -0,0 +1,7 @@
+from . import account_chart_template
+from . import account_move
+from . import account_move_line
+from . import account_payment
+from . import account_tax
+from . import res_company
+from . import res_config_settings
