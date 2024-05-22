45bbcd35f969566dd102d1357e50d8209b961500
Rémi Rahir (rar) <rar@odoo.com>
2024-05-22 15:02:52 +0200

    [FIX] web: fix `mockReadGroup` return
    `mockReadGroup` did not follow the server implementation in the case of
    missing groupby (did not return __domain key) and was returning `0`
    instead of `False` for the aggregate value when the domain did not match
    any records.
    
    X-original-commit: 81173ca1c3399717d4b32019751c054b37fbc061

--- addons/web/static/tests/helpers/mock_server.js
+++ addons/web/static/tests/helpers/mock_server.js
@@ -1048,9 +1048,13 @@ export class MockServer {
                         if (func === "array_agg") {
                             group[name] = records.map((r) => r[fieldName]);
                         } else {
-                            group[name] = 0;
-                            for (const r of records) {
-                                group[name] += r[fieldName];
+                            if (!records.length) {
+                                group[name] = false;
+                            } else {
+                                group[name] = 0;
+                                for (const r of records) {
+                                    group[name] += r[fieldName];
+                                }
                             }
                         }
                         break;
@@ -1126,7 +1130,7 @@ export class MockServer {
         }
 
         if (!groupBy.length) {
-            const group = { __count: records.length };
+            const group = { __count: records.length, __domain: kwargs.domain };
             aggregateFields(group, records);
             return [group];
         }
