32b1349075c98f9ace13bd0fa0112a0608a84ca1
axtr <axtr@odoo.com>
2024-05-23 10:03:10 +0200

    [FIX] pos_sale: remove SO confirmation when saving for later
    Problem:
    When a user saves a quotation in the POS session, the quotation gets confirmed into a SO and its invoice_status is "invoiced" but no invoice was created. This occurs because the POS has another POS as its trusted POS. The user is expected the quotation to stay as a quotation when they save it in the POS.
    
    Steps to reproduce:
    - Install "Point of Sale" and "Sales" apps
    - Create another POS that is not a bar/restaurant
    - On the settings of the "Shop" POS, set the newly created POS as its trusted POS
    - Create a sales quotation and save
    - Open a session in "Shop" POS and click "quotation/order" to import the sales quotation and click on "Settle the order"
    - Click on save
    - The quotation is now confirmed into a SO when it should not be
    
    Cause:
    All quotations with state "draft" or "sent" are confirmed while they shouldn't if the sale is not completed
    
    Solution:
    As the variable "draft" from "create_from_ui" is True when saving a quotation and is False when confirming an order, we can use it to proceed or not the confirmation of the quotation
    
    opw-3885845
    
    closes odoo/odoo#166485
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/pos_sale/models/pos_order.py
+++ addons/pos_sale/models/pos_order.py
@@ -78,8 +78,9 @@ class PosOrder(models.Model):
 
             # confirm the unconfirmed sale orders that are linked to the sale order lines
             sale_orders = so_lines.mapped('order_id')
-            for sale_order in sale_orders.filtered(lambda so: so.state in ['draft', 'sent']):
-                sale_order.action_confirm()
+            if not draft:
+                for sale_order in sale_orders.filtered(lambda so: so.state in ['draft', 'sent']):
+                    sale_order.action_confirm()
 
             # update the demand qty in the stock moves related to the sale order line
             # flush the qty_delivered to make sure the updated qty_delivered is used when
