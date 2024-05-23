8f7decc6a7d657c807102c6555121ac03fffdb94
clesgow <quwo@odoo.com>
2024-05-23 14:09:17 +0000

    [FIX] stock: properly rebuke duplicate lots
    Steps to reproduce:
    - Create a product with serial tracking
    - Go to Lots/Serial Numbers
    - Create a lot '1' without company
    - Create another lot '1' without company
    
    Issue:
    An error should be prompted, as a lot name must be unique by either:
    - company
    - company & 'no-company'
    
    Which means, you can have a lot :
    - In company A & B: YES
    - In company A & 'no-company': NO
    - Duplicate in company A: NO
    - Duplicate in 'no-company': NO
    
    Now properly tracks the cross-company lots to know how many lots are
    already existing in the different use-cases.
    
    closes odoo/odoo#166756
    
    X-original-commit: 53f91419b799927cd0e644c0e520f2b71e289fb4
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/stock/models/stock_lot.py
+++ addons/stock/models/stock_lot.py
@@ -95,15 +95,15 @@ class StockLot(models.Model):
         if any(not lot.company_id for lot in self):
             # We need to check across other companies to not have duplicates between 'no-company' and a company.
             self = self.sudo()
-        records = self._read_group(domain, groupby, order='company_id DESC')
-        error_message_lines = []
-        cross_lots = set()
-        for company, product, name in records:
+        records = self._read_group(domain, groupby, ['__count'], order='company_id DESC')
+        error_message_lines = set()
+        cross_lots = {}
+        for company, product, name, count in records:
             if not company:
-                cross_lots.add((product, name))
-                continue
-            if (product, name) in cross_lots:
-                error_message_lines.append(_(" - Product: %(product)s, Lot/Serial Number: %(lot)s", product=product.display_name, lot=name))
+                cross_lots[(product, name)] = count
+            # For company-specific lots, we check that there is no duplicate with 'no-company' lots, but NOT between specific-company ones.
+            if (company and (cross_lots.get((product, name), 0) + count) > 1) or count > 1:
+                error_message_lines.add(_(" - Product: %(product)s, Lot/Serial Number: %(lot)s", product=product.display_name, lot=name))
         if error_message_lines:
             raise ValidationError(_("The combination of lot/serial number and product must be unique within a company including when no company is defined.\nThe following combinations contain duplicates:\n") + '\n'.join(error_message_lines))
 
