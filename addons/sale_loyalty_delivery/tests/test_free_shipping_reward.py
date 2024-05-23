1dbfe49baa6b94b3c131c5050b95610863d98611
Abdelouahab (abla) <abla@odoo.com>
2024-05-23 11:37:58 +0200

    [FIX] sale_loyalty_delivery: free delivery line should be removed
    To reproduce:
    =============
    - Install sale_loyalty_delivery
    - create a loyalty card where customer gets free delivery for 10 points
     and gets 1 point per 1$ spent
    - create a sale order with more than 10$ and apply the loyalty card
    - free delivery line is added
    - change the price to be less than 10$
    -> free delivery line is not removed
    
    Problem:
    ========
    we have a logic that prevents deleting the free delivery line, it's not
    explained why we have this logic, and there is no test for it.
    on the other hand, the use-case on the support ticket is valid, so we
    should remove this logic.
    
    opw-3879945
    
    closes odoo/odoo#167686
    
    X-original-commit: cbb1ed990bc1236c1c12c4fb40f7d03437cd7b07
    Signed-off-by: Abdelouahab Laaroussi (abla) <abla@odoo.com>

--- addons/sale_loyalty_delivery/tests/test_free_shipping_reward.py
+++ addons/sale_loyalty_delivery/tests/test_free_shipping_reward.py
@@ -400,3 +400,52 @@ class TestSaleCouponProgramRules(TestSaleCouponCommon):
         # Make sure the promotion is NOT added
         err_msg = "No reward lines should be created as the delivery line shouldn't be included in the promotion calculation"
         self.assertEqual(len(order.order_line.ids), 2, err_msg)
+
+    def test_free_shipping_should_be_removed_when_rules_are_not_met(self):
+        p_1 = self.env['loyalty.program'].create({
+            'name': 'Free shipping if > 872 tax excl',
+            'trigger': 'with_code',
+            'rule_ids': [(0, 0, {
+                'mode': 'with_code',
+                'code': 'free_shipping',
+                'minimum_amount': 872,
+            })],
+            'reward_ids': [(0, 0, {
+                'reward_type': 'shipping',
+            })],
+        })
+        programs = (p_1)
+        order = self.empty_order
+        self.iPadMini.taxes_id = self.tax_10pc_incl
+        sol1 = self.env['sale.order.line'].create({
+            'product_id': self.iPadMini.id,
+            'name': 'Large Cabinet',
+            'product_uom_qty': 3.0,
+            'order_id': order.id,
+        })
+
+        # I add delivery cost in Sales order
+        delivery_wizard = Form(self.env['choose.delivery.carrier'].with_context({
+            'default_order_id': order.id,
+            'default_carrier_id': self.carrier.id
+        }))
+        choose_delivery_carrier = delivery_wizard.save()
+        choose_delivery_carrier.button_confirm()
+        self._auto_rewards(order, programs)
+        self.assertEqual(len(order.order_line.ids), 2)
+        self.assertEqual(order.reward_amount, 0)
+        # Shipping is 20 + 15%tax
+        self.assertEqual(sum(line.price_total for line in order._get_no_effect_on_threshold_lines()), 23)
+        self.assertEqual(order.amount_untaxed, 872.73 + 20)
+
+        self._apply_promo_code(order, 'free_shipping')
+        self._auto_rewards(order, programs)
+        self.assertEqual(len(order.order_line.ids), 3, "We should get the delivery line and the free delivery since we are below 872.73$")
+        self.assertEqual(order.reward_amount, -20)
+        self.assertEqual(sum(line.price_total for line in order._get_no_effect_on_threshold_lines()), 0)
+        self.assertEqual(order.amount_untaxed, 872.73)
+
+        sol1.product_uom_qty = 1
+        self._auto_rewards(order, programs)
+        self.assertEqual(len(order.order_line.ids), 2, "We should loose the free delivery reward since we are above 872.73$")
+        self.assertEqual(order.reward_amount, 0)
