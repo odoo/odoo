2dd3a37ee142b8df8563be0400f2fdfba2e43f16
axtr <axtr@odoo.com>
2024-05-23 07:02:59 +0000

    [FIX] pos_self_order: display order number for a command with a price of 0.00
    Problem:
    In kiosk, when a person has a command with a price of 0.00, he doesn't have an order number
    
    Steps to reproduce:
    - Install "Point of Sale" app
    - In POS settings, create a kiosk shop and set "Self Ordering" to "Kiosk"
    - Go to product and set a price of 0.00 to a product
    - Open the kiosk session and order the product with the price of 0.00
    - Click on "Pay" and you'll be back to the reception page without having your order number
    
    opw-3877596
    
    closes odoo/odoo#167985
    
    X-original-commit: 060cafb1d663d852de4b31ddd7b14861eda8dd43
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Axel Trémaudant (axtr) <axtr@odoo.com>

--- addons/pos_self_order/static/tests/tours/self_order_kiosk_tour.js
+++ addons/pos_self_order/static/tests/tours/self_order_kiosk_tour.js
@@ -1,6 +1,7 @@
 import { registry } from "@web/core/registry";
 import * as Utils from "@pos_self_order/../tests/tours/utils/common";
 import * as CartPage from "@pos_self_order/../tests/tours/utils/cart_page_util";
+import * as ConfirmationPage from "@pos_self_order/../tests/tours/utils/confirmation_page_util";
 import * as LandingPage from "@pos_self_order/../tests/tours/utils/landing_page_util";
 import * as ProductPage from "@pos_self_order/../tests/tours/utils/product_page_util";
 import * as Numpad from "@point_of_sale/../tests/tours/utils/numpad_util";
@@ -109,5 +110,19 @@ registry.category("web_tour.tours").add("self_simple_order", {
         Utils.clickBtn("Pay"),
         Utils.clickBtn("Close"),
         Utils.checkIsNoBtn("My Order"),
+    ],
+});
+
+registry.category("web_tour.tours").add("self_order_price_null", {
+    test: true,
+    steps: () => [
+        Utils.checkIsNoBtn("My Order"),
+        Utils.clickBtn("Order Now"),
+        ProductPage.clickProduct("Coca-Cola"),
+        Utils.clickBtn("Order"),
+        CartPage.checkProduct("Coca-Cola", "0.00", "1"),
+        Utils.clickBtn("Pay"),
+        ConfirmationPage.orderNumberShown(),
+        Utils.checkBtn("Close"),
     ],
 });
--- addons/pos_self_order/static/tests/tours/utils/confirmation_page_util.js
+++ addons/pos_self_order/static/tests/tours/utils/confirmation_page_util.js
@@ -5,3 +5,11 @@ export function isShown() {
         run: () => {},
     };
 }
+
+export function orderNumberShown() {
+    return {
+        content: "Check if the order number is shown",
+        trigger: ".number",
+        run: () => {},
+    };
+}
