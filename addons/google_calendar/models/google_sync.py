christian-rb/odoo-dev

[IMP] google_calendar, hr_holidays: remove video call URL for time off event
Before Commit:
When Google Sync is enabled and if you create a time-off request and it gets
validated, Odoo will automatically create a corresponding event in the Odoo
calendar and when it gets synced with the google calendar a video call URL for
that time-off event is also generated.

Steps to reproduce:
- Make sure Google Sync is enabled
- Open the Time off module
- Create a new time off request
- Go to your time offs, approve and validate the time off request
- Go to the calendar module and open the respective event made for the time off
- You can see a video call URL is generated for this event

After Commit:
When you create a time off request and validate it, the corresponding event is
made but no video call URL is generated.

addons/google_calendar/models/google_sync.py
@@ -265,6 +265,9 @@ class GoogleSync(models.AbstractModel):
                 if values:
                     self.exists().with_context(dont_notify=True).need_sync = False
 
+    def _need_video_call(self):
+        return True
+
     @after_commit
     def _google_insert(self, google_service: GoogleCalendarService, values, timeout=TIMEOUT):
         if not values:
@@ -274,7 +277,7 @@ class GoogleSync(models.AbstractModel):
                 try:
                     send_updates = self._context.get('send_updates', True)
                     google_service.google_service = google_service.google_service.with_context(send_updates=send_updates)
-                    google_id = google_service.insert(values, token=token, timeout=timeout)
+                    google_id = google_service.insert(values, token=token, timeout=timeout, need_video_call=self._need_video_call())
                     # Everything went smoothly
                     self.with_context(dont_notify=True).write({
                         'google_id': google_id,

odoo/odoo

addons/google_calendar/models/google_sync.py
@@ -273,6 +273,13 @@ class GoogleSync(models.AbstractModel):
         }
         return writeable_values
 
+    def _need_video_call(self):
+        """ Implement this method to return True if the event needs a video call
+        :return: bool
+        """
+        self.ensure_one()
+        return True
+
     @after_commit
     def _google_insert(self, google_service: GoogleCalendarService, values, timeout=TIMEOUT):
         if not values:
@@ -282,7 +289,7 @@ class GoogleSync(models.AbstractModel):
                 try:
                     send_updates = self._context.get('send_updates', True)
                     google_service.google_service = google_service.google_service.with_context(send_updates=send_updates)
-                    google_values = google_service.insert(values, token=token, timeout=timeout)
+                    google_values = google_service.insert(values, token=token, timeout=timeout, need_video_call=self._need_video_call())
                     self.with_context(dont_notify=True).write(self._get_post_sync_values(values, google_values))
                 except HTTPError as e:
                     if e.response.status_code in (400, 403):