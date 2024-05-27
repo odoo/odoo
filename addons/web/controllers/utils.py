6068fede24337b730873b68d9f0cee9ed104bab1
Julien Castiaux <juc@odoo.com>
2024-05-27 08:12:18 +0000

    [FIX] web: ensure_db compat with werkzeug 3.0
    Ubuntu 24.04 Noble ships werkzeug==3.0.1[^1], that version dropped some
    Py2/Py3 code compatibility to only support Py3. This is the case for the
    `iri_to_uri` function that since 3.0.0 doesn't support bytes
    anymore[^2]. Since Odoo 13 only supports Py3 too, it is fine to always
    decode the query string (which was what `iri_to_uri` was doing anyway).
    
    [^1]: https://packages.ubuntu.com/noble/python3-werkzeug
    [^2]: https://werkzeug.palletsprojects.com/en/3.0.x/urls/
    
    closes odoo/odoo#167124
    
    X-original-commit: 9b73bff9fe0d718596aaa9c479a9a4529e4fdeb7
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- addons/web/controllers/utils.py
+++ addons/web/controllers/utils.py
@@ -78,7 +78,7 @@ def ensure_db(redirect='/web/database/selector', db=None):
         url_redirect = werkzeug.urls.url_parse(r.base_url)
         if r.query_string:
             # in P3, request.query_string is bytes, the rest is text, can't mix them
-            query_string = iri_to_uri(r.query_string)
+            query_string = iri_to_uri(r.query_string.decode())
             url_redirect = url_redirect.replace(query=query_string)
         request.session.db = db
         werkzeug.exceptions.abort(request.redirect(url_redirect.to_url(), 302))
