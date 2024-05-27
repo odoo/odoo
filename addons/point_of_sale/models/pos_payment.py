8f5ea626d19107c93bf5eb2397137648fa5574cd
Nguyễn Đại Dương <daiduongnguyen2709@gmail.com>
2024-05-27 10:02:05 +0000

    [IMP] pos: add missing index for account.move
    Since https://github.com/odoo/odoo/pull/143852 and the backport
    https://github.com/odoo/odoo/pull/163564 , we have added a lot of index
    for account.move, but still there are some missing left, so this commit
    is to add the index btree_not_null for those one.
    
    X-original-commit: 3300b12879a6fd8794a843de2360a1c1bfdb809d

--- addons/point_of_sale/models/pos_payment.py
+++ addons/point_of_sale/models/pos_payment.py
@@ -32,7 +32,7 @@ class PosPayment(models.Model):
     payment_status = fields.Char('Payment Status')
     ticket = fields.Char('Payment Receipt Info')
     is_change = fields.Boolean(string='Is this payment change?', default=False)
-    account_move_id = fields.Many2one('account.move')
+    account_move_id = fields.Many2one('account.move', index='btree_not_null')
     uuid = fields.Char(string='Uuid', readonly=True, copy=False)
 
     @api.model
