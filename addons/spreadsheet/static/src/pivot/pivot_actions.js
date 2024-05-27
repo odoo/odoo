0e13090ad343f62d7db84b850679b0ea4b5d035b
Adrien Minne (adrm) <adrm@odoo.com>
2024-05-27 14:16:07 +0000

    [FIX] spreadsheet: use_global_filter visibility
    The context menu (and clickable cell) `use_global_filter` should
    take the value of the underlying pivot formula, and apply it to the
    matching global filters. This works, but was supposed to work only for
    `ODOO.PIVOT.HEADER` formulas, and not simple `ODOO.PIVOT` formulas.
    
    This commit fixes the visibility of the `use_global_filter` option
    in the context menu, so that it is only visible for `ODOO.PIVOT.HEADER`.
    
    Also removed/changed tests that were testing that the menu was visible
    for positional `ODOO.PIVOT` formulas.
    
    Task: 3714696
    X-original-commit: 7407dcb9bced10a20bdf9385d53079cc86beb603

--- addons/spreadsheet/static/src/pivot/pivot_actions.js
+++ addons/spreadsheet/static/src/pivot/pivot_actions.js
@@ -1,8 +1,17 @@
 // @ts-check
 
 import { navigateTo } from "../actions/helpers";
+<<<<<<< HEAD
 import { helpers } from "@odoo/o-spreadsheet";
 const { getFirstPivotFunction, getNumberOfPivotFunctions } = helpers;
+||||||| parent of d7fb5e0273d6 (temp)
+import {
+  getFirstPivotFunction,
+  getNumberOfPivotFormulas,
+} from "./pivot_helpers";
+=======
+import { getNumberOfPivotFormulas } from "./pivot_helpers";
+>>>>>>> d7fb5e0273d6 (temp)
 
 /**
  * @param {import("@odoo/o-spreadsheet").CellPosition} position
@@ -14,8 +23,16 @@ export const SEE_RECORDS_PIVOT = async (position, env) => {
     const pivot = env.model.getters.getPivot(pivotId);
     await pivot.load();
     const { model } = pivot.definition;
+<<<<<<< HEAD
     const { actionXmlId } = env.model.getters.getPivotCoreDefinition(pivotId);
     const argsDomain = env.model.getters.getPivotDomainArgsFromPosition(position);
+||||||| parent of d7fb5e0273d6 (temp)
+    const { actionXmlId } = env.model.getters.getPivotDefinition(pivotId);
+    const argsDomain = env.model.getters.getPivotDomainArgsFromPosition(position);
+=======
+    const { actionXmlId } = env.model.getters.getPivotDefinition(pivotId);
+    const argsDomain = env.model.getters.getPivotDomainArgsFromPosition(position)?.domainArgs;
+>>>>>>> d7fb5e0273d6 (temp)
     const domain = pivot.getPivotCellDomain(argsDomain);
     const name = await pivot.getModelLabel();
     await navigateTo(
@@ -41,10 +58,34 @@ export const SEE_RECORDS_PIVOT = async (position, env) => {
  * @param {import("@spreadsheet").OdooGetters} getters
  * @returns {boolean}
  */
+<<<<<<< HEAD
 export const SEE_RECORDS_PIVOT_VISIBLE = (position, getters) => {
     const cell = getters.getCorrespondingFormulaCell(position);
     const evaluatedCell = getters.getEvaluatedCell(position);
     const argsDomain = getters.getPivotDomainArgsFromPosition(position);
+||||||| parent of d7fb5e0273d6 (temp)
+export const SEE_RECORDS_PIVOT_VISIBLE = (position, env) => {
+    const cell = env.model.getters.getCorrespondingFormulaCell(position);
+    const evaluatedCell = env.model.getters.getEvaluatedCell(position);
+    const argsDomain = env.model.getters.getPivotDomainArgsFromPosition(position);
+    const pivotId = env.model.getters.getPivotIdFromPosition(position);
+    if (!env.model.getters.isExistingPivot(pivotId)) {
+        return false;
+    }
+    const dataSource = env.model.getters.getPivot(pivotId);
+    const loadingError = dataSource.assertIsValid({ throwOnError: false })
+=======
+export const SEE_RECORDS_PIVOT_VISIBLE = (position, env) => {
+    const cell = env.model.getters.getCorrespondingFormulaCell(position);
+    const evaluatedCell = env.model.getters.getEvaluatedCell(position);
+    const argsDomain = env.model.getters.getPivotDomainArgsFromPosition(position)?.domainArgs;
+    const pivotId = env.model.getters.getPivotIdFromPosition(position);
+    if (!env.model.getters.isExistingPivot(pivotId)) {
+        return false;
+    }
+    const dataSource = env.model.getters.getPivot(pivotId);
+    const loadingError = dataSource.assertIsValid({ throwOnError: false })
+>>>>>>> d7fb5e0273d6 (temp)
     return (
         evaluatedCell.type !== "empty" &&
         evaluatedCell.type !== "error" &&
@@ -68,13 +109,29 @@ export function SET_FILTER_MATCHING_CONDITION(position, getters) {
     if (!SEE_RECORDS_PIVOT_VISIBLE(position, getters)) {
         return false;
     }
+<<<<<<< HEAD
     const cell = getters.getCorrespondingFormulaCell(position);
+||||||| parent of d7fb5e0273d6 (temp)
+    const cell = env.model.getters.getCorrespondingFormulaCell(position);
+=======
+>>>>>>> d7fb5e0273d6 (temp)
 
+<<<<<<< HEAD
     const pivotId = getters.getPivotIdFromPosition(position);
     const domainArgs = getters.getPivotDomainArgsFromPosition(position);
     if (domainArgs === undefined) {
+||||||| parent of d7fb5e0273d6 (temp)
+    const pivotId = env.model.getters.getPivotIdFromPosition(position);
+    const domainArgs = env.model.getters.getPivotDomainArgsFromPosition(position);
+    if (domainArgs === undefined) {
+=======
+    const pivotId = env.model.getters.getPivotIdFromPosition(position);
+    const pivotInfo = env.model.getters.getPivotDomainArgsFromPosition(position);
+    if (pivotInfo?.domainArgs === undefined) {
+>>>>>>> d7fb5e0273d6 (temp)
         return false;
     }
+<<<<<<< HEAD
     const matchingFilters = getters.getFiltersMatchingPivotArgs(pivotId, domainArgs);
     const pivotFunction = getFirstPivotFunction(cell.compiledFormula.tokens).functionName;
     return (
@@ -82,12 +139,26 @@ export function SET_FILTER_MATCHING_CONDITION(position, getters) {
             pivotFunction === "PIVOT.HEADER" ||
             pivotFunction === "PIVOT") &&
         matchingFilters.length > 0
+||||||| parent of d7fb5e0273d6 (temp)
+    const matchingFilters = env.model.getters.getFiltersMatchingPivotArgs(pivotId, domainArgs);
+    const pivotFunction = getFirstPivotFunction(cell.compiledFormula.tokens).functionName;
+    return (
+        (pivotFunction === "ODOO.PIVOT" ||
+            pivotFunction === "ODOO.PIVOT.HEADER" ||
+            pivotFunction === "ODOO.PIVOT.TABLE") &&
+        matchingFilters.length > 0
+=======
+    const matchingFilters = env.model.getters.getFiltersMatchingPivotArgs(
+        pivotId,
+        pivotInfo?.domainArgs
+>>>>>>> d7fb5e0273d6 (temp)
     );
+    return pivotInfo?.isHeader && matchingFilters.length > 0;
 }
 
 export function SET_FILTER_MATCHING(position, env) {
     const pivotId = env.model.getters.getPivotIdFromPosition(position);
-    const domainArgs = env.model.getters.getPivotDomainArgsFromPosition(position);
+    const domainArgs = env.model.getters.getPivotDomainArgsFromPosition(position)?.domainArgs;
     const filters = env.model.getters.getFiltersMatchingPivotArgs(pivotId, domainArgs);
     env.model.dispatch("SET_MANY_GLOBAL_FILTER_VALUE", { filters });
 }
