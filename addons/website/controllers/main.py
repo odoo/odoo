christian-rb/odoo-dev

[IMP] website: verify uploaded font files
This commit verifies if the uploaded font files match their specified
extensions.
For otf, woff and woff2: we check the 4 first bytes magic number.
For ttf: since there is no magic number, we check that the table
directory at the beginning of the file contains mentions all mandatory
tables.

task-3635905

addons/website/controllers/main.py
@@ -889,6 +889,31 @@ class Website(Home):
             - attachment id
             - attachment URL
         """
+        def check_content(filename, data):
+            """ Returns True only if data matches the font extension. """
+            # Do not pollute general guess_mimetype with this.
+            ext = filename.split('.')[-1].lower()
+            if ext == 'otf':
+                return data.startswith(b'OTTO')
+            elif ext == 'ttf':
+                # Based on https://docs.fileformat.com/font/ttf/#true-type-file-format-specifications
+                TOC_OFFSET = 12
+                TOC_ENTRY_LENGTH = 16
+                table_count = int(data[4] << 8) + data[5]  # uint16
+                if TOC_OFFSET + table_count * TOC_ENTRY_LENGTH > len(data):
+                    return False
+                mandatory_tags = {b'cmap', b'glyf', b'head', b'hhea', b'hmtx', b'loca', b'maxp', b'name', b'post'}
+                for offset in range(TOC_OFFSET, TOC_OFFSET + table_count * TOC_ENTRY_LENGTH, TOC_ENTRY_LENGTH):
+                    tag = data[offset:offset + 4]
+                    if tag in mandatory_tags:
+                        mandatory_tags.remove(tag)
+                return not mandatory_tags
+            elif ext == 'woff':
+                return data.startswith(b'wOFF')
+            elif ext == 'woff2':
+                return data.startswith(b'wOF2')
+            return False
+
         def create_attachment(font, data):
             """ Creates font attachments right away to avoid keeping
             several extracted contents in memory. """
@@ -916,10 +941,13 @@ class Website(Home):
                             or entry.filename.startswith('__MACOSX') \
                             or '/.' in entry.filename:
                         continue
+                    data = zip_file.read(entry)
+                    if not check_content(entry.filename, data):
+                        continue
                     result.append(create_attachment({
                         'name': f'{name}-{entry.filename.replace("/", "-")}',
-                    }, zip_file.read(entry)))
-        elif name.split('.')[-1].lower() in SUPPORTED_FONT_EXTENSIONS:
+                    }, data))
+        elif name.split('.')[-1].lower() in SUPPORTED_FONT_EXTENSIONS and check_content(name, binary_data):
             result.append(create_attachment({
                 'name': name,
             }, binary_data))

odoo/odoo

addons/website/controllers/main.py
@@ -895,6 +895,30 @@ class Website(Home):
             - attachment id
             - attachment URL
         """
+        def check_content(filename, data):
+            """ Returns True only if data matches the font extension. """
+            # Do not pollute general guess_mimetype with this.
+            ext = filename.split('.')[-1].lower()
+            if ext == 'otf':
+                return data.startswith(b'OTTO')
+            elif ext == 'ttf':
+                # Based on https://docs.fileformat.com/font/ttf/#true-type-file-format-specifications
+                TOC_OFFSET = 12
+                TOC_ENTRY_LENGTH = 16
+                table_count = int(data[4] << 8) + data[5]  # uint16
+                if TOC_OFFSET + table_count * TOC_ENTRY_LENGTH > len(data):
+                    return False
+                mandatory_tags = {b'cmap', b'glyf', b'head', b'hhea', b'hmtx', b'loca', b'maxp', b'name', b'post'}
+                for offset in range(TOC_OFFSET, TOC_OFFSET + table_count * TOC_ENTRY_LENGTH, TOC_ENTRY_LENGTH):
+                    tag = data[offset:offset + 4]
+                    mandatory_tags.discard(tag)
+                return not mandatory_tags
+            elif ext == 'woff':
+                return data.startswith(b'wOFF')
+            elif ext == 'woff2':
+                return data.startswith(b'wOF2')
+            return False
+
         def create_attachment(font, data):
             """ Creates font attachments right away to avoid keeping
             several extracted contents in memory. """
@@ -922,10 +946,13 @@ class Website(Home):
                             or entry.filename.startswith('__MACOSX') \
                             or '/.' in entry.filename:
                         continue
+                    data = zip_file.read(entry)
+                    if not check_content(entry.filename, data):
+                        continue
                     result.append(create_attachment({
                         'name': f'{name}-{entry.filename.replace("/", "-")}',
-                    }, zip_file.read(entry)))
-        elif name.split('.')[-1].lower() in SUPPORTED_FONT_EXTENSIONS:
+                    }, data))
+        elif name.split('.')[-1].lower() in SUPPORTED_FONT_EXTENSIONS and check_content(name, binary_data):
             result.append(create_attachment({
                 'name': name,
             }, binary_data))