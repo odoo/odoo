b78a71a79744d5c15697e989aef4ec62a680adfb
Riosmena <emi.riosmena@gmail.com>
2024-05-27 17:12:22 -0600

    feat(Objetivos): Display progress register

--- talent360/evaluaciones/wizards/registrar_avance.py
+++ talent360/evaluaciones/wizards/registrar_avance.py
@@ -4,14 +4,18 @@ class RegistrarAvance(models.TransientModel):
     _name = "registrar.avance.wizard"
     _description = "Registrar Avance Wizard"
 
-    id = fields.Integer(string='ID')
-    fecha = fields.Date(string='Fecha')
+    fecha = fields.Date(string='Fecha', default=fields.Date.today())
     avance = fields.Integer(string='Avance')
     archivos = fields.Many2many(comodel_name='ir.attachment', string='Subir Archivo')
     nombres_archivos = fields.Char(string='Nombres de Archivos')
     comentarios = fields.Text(string='Comentarios')
 
     def action_confirm(self):
-        # Lógica del wizard aquí
-        # Puedes añadir cualquier lógica adicional que necesites
-        print("Wizard confirmed")
+        avance = self.avance
+        archivos = self.archivos
+        comentarios = self.comentarios
+        fecha = self.fecha
+
+        message = f"ID: {self.id}\nFecha: {fecha}\nAvance: {avance}\nArchivos: {', '.join(archivos.mapped('name'))}\nComentarios: {comentarios}"
+        
+        print(message)
