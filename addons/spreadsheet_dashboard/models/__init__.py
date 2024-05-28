a31ef1b5e12863046f036ce600b73188cb77e5d1
Lucas Lefèvre (lul) <lul@odoo.com>
2024-05-28 11:27:44 +0200

    [FIX] spreadsheet_* TODO

--- addons/spreadsheet_dashboard/models/__init__.py
+++ addons/spreadsheet_dashboard/models/__init__.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import ir_attachment
 from . import spreadsheet_dashboard_group
 from . import spreadsheet_dashboard
