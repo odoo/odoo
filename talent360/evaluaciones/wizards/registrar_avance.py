7aa2ec0c4695939c8b0eb283d12fe6157fd19f5c
Riosmena <emi.riosmena@gmail.com>
2024-05-27 19:18:51 -0600

    feat(Objetivos): Add functionality to register progress on a specific objective

--- talent360/evaluaciones/wizards/registrar_avance.py
+++ talent360/evaluaciones/wizards/registrar_avance.py
@@ -1,20 +1,25 @@
 from odoo import fields, models, api, exceptions, _
+from odoo.http import request
 
 class RegistrarAvance(models.TransientModel):
     _name = "registrar.avance.wizard"
     _description = "Registrar Avance Wizard"
 
     fecha = fields.Date(string='Fecha', default=fields.Date.today())
-    avance = fields.Integer(string='Avance')
+    avance = fields.Integer(string='Avance', required=True)
     archivos = fields.Many2many(comodel_name='ir.attachment', string='Subir Archivo')
     nombres_archivos = fields.Char(string='Nombres de Archivos')
     comentarios = fields.Text(string='Comentarios')
 
-    def action_confirm(self):
+    def action_confirmar(self):
         avance = self.avance
         archivos = self.archivos
         comentarios = self.comentarios
         fecha = self.fecha
+        
+        objetivo_id = self.env.context.get("objetivo_id")
+        usuario_objetivo_model = self.env["objetivo"].browse(objetivo_id)
+        usuario_objetivo_model.sudo().write({"resultado": avance})
 
         message = f"ID: {self.id}\nFecha: {fecha}\nAvance: {avance}\nArchivos: {', '.join(archivos.mapped('name'))}\nComentarios: {comentarios}"
         
