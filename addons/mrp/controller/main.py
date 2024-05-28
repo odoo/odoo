christian-rb/odoo-dev

[IMP] product,mrp,sale: mrp document refactor
In https://github.com/odoo/odoo/pull/132739, the introduction of product documents generalized
the previous document approach used in MRP.

This commit removes the existing mrp_document model and merge it's data and behaviour with the
product document one.

In addition, a field attached_to_mrp added on the product document  will allow the user to
access documents in spefic places, like in a production or a bill of materials.
This commit also adds those documents to the respective chatters of the productions and bill
of materials.

task-id: 3649991
closes: https://github.com/odoo/odoo/pull/152286
enterprise: https://github.com/odoo/enterprise/pull/55634
upgrade: https://github.com/odoo/upgrade/pull/5622

addons/mrp/controller/main.py
@@ -1,37 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import base64
-import json
-import logging
+from odoo.addons.product.controllers.product_document import ProductDocumentController
 
-from odoo import http
-from odoo.http import request
-from odoo.tools.translate import _
+class MRPProductDocumentController(ProductDocumentController):
 
-logger = logging.getLogger(__name__)
-
-
-class MrpDocumentRoute(http.Controller):
-
-    @http.route('/mrp/upload_attachment', type='http', methods=['POST'], auth="user")
-    def upload_document(self, ufile, **kwargs):
-        files = request.httprequest.files.getlist('ufile')
-        result = {'success': _("All files uploaded")}
-        for ufile in files:
-            try:
-                mimetype = ufile.content_type
-                request.env['mrp.document'].with_context(
-                    disable_product_documents_creation=True
-                ).create({
-                    'name': ufile.filename,
-                    'res_model': kwargs.get('res_model'),
-                    'res_id': int(kwargs.get('res_id')),
-                    'mimetype': mimetype,
-                    'datas': base64.encodebytes(ufile.read()),
-                })
-            except Exception as e:
-                logger.exception("Fail to upload document %s" % ufile.filename)
-                result = {'error': str(e)}
-
-        return json.dumps(result)
+    def get_additional_create_params(self, **kwargs):
+        super_values = super().get_additional_create_params(**kwargs)
+        if kwargs.get('bom_id'):
+            return super_values | {'attached_on_mrp': 'bom'}
+        return super_values

odoo/odoo

addons/mrp/controller/main.py
@@ -1,37 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import base64
-import json
-import logging
-
-from odoo import http
-from odoo.http import request
-from odoo.tools.translate import _
-
-logger = logging.getLogger(__name__)
+from odoo.addons.product.controllers.product_document import ProductDocumentController
 
 
-class MrpDocumentRoute(http.Controller):
+class MRPProductDocumentController(ProductDocumentController):
 
-    @http.route('/mrp/upload_attachment', type='http', methods=['POST'], auth="user")
-    def upload_document(self, ufile, **kwargs):
-        files = request.httprequest.files.getlist('ufile')
-        result = {'success': _("All files uploaded")}
-        for ufile in files:
-            try:
-                mimetype = ufile.content_type
-                request.env['mrp.document'].with_context(
-                    disable_product_documents_creation=True
-                ).create({
-                    'name': ufile.filename,
-                    'res_model': kwargs.get('res_model'),
-                    'res_id': int(kwargs.get('res_id')),
-                    'mimetype': mimetype,
-                    'datas': base64.encodebytes(ufile.read()),
-                })
-            except Exception as e:
-                logger.exception("Fail to upload document %s" % ufile.filename)
-                result = {'error': str(e)}
-
-        return json.dumps(result)
+    def get_additional_create_params(self, **kwargs):
+        super_values = super().get_additional_create_params(**kwargs)
+        if kwargs.get('attached_on_bom'):
+            return super_values | {'attached_on_mrp': 'bom'}
+        return super_values