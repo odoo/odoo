9cdf6371d6d36c27873a5f7049e3d58df8b26ba7
Riosmena <emi.riosmena@gmail.com>
2024-05-28 00:15:15 -0600

    feat(Objetivos): Create validation errors for progress register

--- talent360/evaluaciones/wizards/registrar_avance.py
+++ talent360/evaluaciones/wizards/registrar_avance.py
@@ -9,7 +9,46 @@ class RegistrarAvance(models.TransientModel):
     archivos = fields.Many2many(comodel_name='ir.attachment', string='Subir Archivo')
     nombres_archivos = fields.Char(string='Nombres de Archivos')
     comentarios = fields.Text(string='Comentarios')
+            
+    @api.constrains('avance')
+    def _validar_avance(self):
+        for registro in self:
+            if registro.avance == 0:
+                raise exceptions.ValidationError(_('Por favor registra el avance del objetivo'))
+            elif registro.avance < 0:
+                raise exceptions.ValidationError(_('El avance no puede ser negativo'))
 
+    @api.constrains('archivos')
+    def _validar_peso_archivo(self):
+        maximo = 100 * 1024 * 1024
+        for registro in self:
+            for archivo in registro.archivos:
+                if archivo.file_size > maximo:
+                    raise exceptions.ValidationError(_('El archivo no debe ser mayor a 100 MB'))
+    
+    @api.constrains('archivos')
+    def _validar_tipo_archivo(self):
+        archivos_permitidos = ['csv', 'xlsx', 'txt', 'pdf', 'png', 'jpeg', 'jpg']
+        for archivo in self.archivos:
+            nombre, tipo_archivo = archivo.name.rsplit('.', 1)
+            if tipo_archivo.lower() not in archivos_permitidos:
+                    raise exceptions.ValidationError(_('Solo se pueden subir archivos con extensión pdf, xlsx, csv, txt, png, jpeg'))
+                
+    @api.constrains('comentarios')
+    def _validar_comentarios(self):
+        for registro in self:
+            if registro.comentarios:
+                palabras = len(registro.comentarios.split())
+                if palabras > 200:
+                    raise exceptions.ValidationError(_('Los comentarios no deben exceder las 200 palabras'))
+    
+    @api.constrains('archivos')
+    def _validar_numero_archivos(self):
+        for registro in self:
+            if len(registro.archivos) > 10:
+                raise exceptions.ValidationError(_('Solo se puede subir un máximo de 10 archivos'))
+
+    
     def action_confirmar(self):
         avance = self.avance
         archivos = self.archivos
