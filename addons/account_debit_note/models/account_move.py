ce8690517b4b12a5271f4d374836df68e6658f9e
Urvish Patel <urpv@odoo.com>
2024-05-29 19:02:43 +0530

    [IMP] account_debit_note: introduce dedicated sequence for Debit Notes
    Before this commit, there was no dedicated sequence available for debit notes.
    It was considered as an invoice only.
    
    After this commit, a specific sequence for the Debit note is now available.
    
    task-3906389
    
    Community PR - https://github.com/odoo/odoo/pull/168844
    Enterprise PR - https://github.com/odoo/enterprise/pull/66420
    Upgrade PR - https://github.com/odoo/upgrade/pull/6251
    
    closes odoo/odoo#168844
    
    Related: odoo/enterprise#66420
    Related: odoo/upgrade#6251
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/account_debit_note/models/account_move.py
+++ addons/account_debit_note/models/account_move.py
@@ -32,3 +32,19 @@ class AccountMove(models.Model):
     def action_debit_note(self):
         action = self.env.ref('account_debit_note.action_view_account_move_debit')._get_action_dict()
         return action
+
+    def _get_last_sequence_domain(self, relaxed=False):
+        where_string, param = super()._get_last_sequence_domain(relaxed)
+        if self.journal_id.debit_sequence:
+            where_string += " AND debit_origin_id IS " + ("NOT NULL" if self.debit_origin_id else "NULL")
+        return where_string, param
+
+    def _get_starting_sequence(self):
+        starting_sequence = super()._get_starting_sequence()
+        if (
+            self.journal_id.debit_sequence
+            and self.debit_origin_id
+            and self.move_type in ("in_invoice", "out_invoice")
+        ):
+            starting_sequence = "D" + starting_sequence
+        return starting_sequence
