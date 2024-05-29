8df137ff874c6fb88dee6ed54dc0e4fc1386e93c
kcv-odoo <kcv@odoo.com>
2024-05-29 17:35:33 +0530

    [IMP] sale_{project,timesheet}: improve product ux
    This commit:
     - move project related fields after service_tracking
    field and hide service related info for other types.
    - rework tooltip generation to complete, factorize and harmonize
    phrasing for the different products configuration.
    - prevent quick creation of combos from product form.
    - remove extra product form view.
    - hide invoicing policy if product cannot be sold
    
    task-3959899
    
    Co-authored-by: Victor Feyens <vfe@odoo.com>

--- addons/event_sale/models/product_template.py
+++ addons/event_sale/models/product_template.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -7,6 +7,11 @@ class ProductTemplate(models.Model):
     service_tracking = fields.Selection(selection_add=[
         ('event', 'Event Registration'),
     ], ondelete={'event': 'set default'})
+
+    def _prepare_service_tracking_tooltip(self):
+        if self.service_tracking == 'event':
+            return _("Create an Attendee for the selected Event.")
+        return super()._prepare_service_tracking_tooltip()
 
     @api.onchange('service_tracking')
     def _onchange_type_event(self):
