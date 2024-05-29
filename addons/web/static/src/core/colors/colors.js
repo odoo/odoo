de045316d3f88d008181ae1f5217c6015b15812a
Brieuc-brd <brd@odoo.com>
2024-05-29 13:30:07 +0200

    [IMP] web: update visualization colors
    This commit improves the list of colors used in visualizations to make
    them more elegant and easier to read.
    
    This commit also adds a function that returns a custom color in bright
    mode and its equivalent in a chosen color scheme (eg. in dark mode).
    
    task-3620659
    
    Part-of: odoo/odoo#167296
    Related: odoo/enterprise#63486
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>

--- addons/web/static/src/core/colors/colors.js
+++ addons/web/static/src/core/colors/colors.js
@@ -2,54 +2,110 @@
  * Lists of colors that contrast well with each other to be used in various
  * visualizations (eg. graphs/charts), both in bright and dark themes.
  */
-const COLORS_BRIGHT = [
-    "#1f77b4",
-    "#ff7f0e",
-    "#aec7e8",
-    "#ffbb78",
-    "#2ca02c",
-    "#98df8a",
-    "#d62728",
-    "#ff9896",
-    "#9467bd",
-    "#c5b0d5",
-    "#875a7b", // ~ Enterprise
-    "#c49c94",
-    "#e377c2",
-    "#dcd0d9", // Dashboards Primary
-    "#7f7f7f",
-    "#c7c7c7",
-    "#bcbd22",
-    "#dbdb8d",
-    "#17becf",
-    "#a5d8d7", // Dashboards Secondary
+
+const COLORS_ENT_BRIGHT = ["#875A7B", "#A5D8D7", "#DCD0D9"];
+const COLORS_ENT_DARK = [ "#6B3E66", "#147875", "#5A395A"];
+const COLORS_SM = [
+    "#4EA7F2", // Blue
+    "#EA6175", // Red
+    "#43C5B1", // Teal
+    "#F4A261", // Orange
+    "#8481DD", // Purple
+    "#FFD86D", // Yellow
+];
+const COLORS_MD = [
+    "#4EA7F2", // Blue #1
+    "#3188E6", // Blue #2
+    "#43C5B1", // Teal #1
+    "#00A78D", // Teal #2
+    "#EA6175", // Red #1
+    "#CE4257", // Red #2
+    "#F4A261", // Orange #1
+    "#F48935", // Orange #2
+    "#8481DD", // Purple #1
+    "#5752D1", // Purple #2
+    "#FFD86D", // Yellow #1
+    "#FFBC2C", // Yellow #2
+];
+const COLORS_LG = [
+    "#4EA7F2", // Blue #1
+    "#3188E6", // Blue #2
+    "#056BD9", // Blue #3
+    "#A76DBC", // Violet #1
+    "#7F4295", // Violet #2
+    "#6D2387", // Violet #3
+    "#EA6175", // Red #1
+    "#CE4257", // Red #2
+    "#982738", // Red #3
+    "#43C5B1", // Teal #1
+    "#00A78D", // Teal #2
+    "#0E8270", // Teal #3
+    "#F4A261", // Orange #1
+    "#F48935", // Orange #2
+    "#BE5D10", // Orange #3
+    "#8481DD", // Purple #1
+    "#5752D1", // Purple #2
+    "#3A3580", // Purple #3
+    "#A4A8B6", // Gray #1
+    "#7E8290", // Gray #2
+    "#545B70", // Gray #3
+    "#FFD86D", // Yellow #1
+    "#FFBC2C", // Yellow #2
+    "#C08A16", // Yellow #3
+];
+const COLORS_XL = [
+    "#4EA7F2", // Blue #1
+    "#3188E6", // Blue #2
+    "#056BD9", // Blue #3
+    "#155193", // Blue #4
+    "#A76DBC", // Violet #1
+    "#7F4295", // Violet #1
+    "#6D2387", // Violet #1
+    "#4F1565", // Violet #1
+    "#EA6175", // Red #1
+    "#CE4257", // Red #2
+    "#982738", // Red #3
+    "#791B29", // Red #4
+    "#43C5B1", // Teal #1
+    "#00A78D", // Teal #2
+    "#0E8270", // Teal #3
+    "#105F53", // Teal #4
+    "#F4A261", // Orange #1
+    "#F48935", // Orange #2
+    "#BE5D10", // Orange #3
+    "#7D380D", // Orange #4
+    "#8481DD", // Purple #1
+    "#5752D1", // Purple #2
+    "#3A3580", // Purple #3
+    "#26235F", // Purple #4
+    "#A4A8B6", // Grey #1
+    "#7E8290", // Grey #2
+    "#545B70", // Grey #3
+    "#3F4250", // Grey #4
+    "#FFD86D", // Yellow #1
+    "#FFBC2C", // Yellow #2
+    "#C08A16", // Yellow #3
+    "#936A12", // Yellow #4
 ];
 
-const COLORS_DARK = [
-    "#00ffff",
-    "#ff6347",
-    "#00ced1",
-    "#ffd700",
-    "#29ef29",
-    "#c5fabb",
-    "#fe4b4c",
-    "#ffb6c1",
-    "#ba87e9",
-    "#eadbf6",
-    "#c568af", // ~ Enterprise
-    "#ecc1b8",
-    "#fda9e3",
-    "#BB86FC", // Dashboards Primary
-    "#808080",
-    "#f2e8e8",
-    "#fcfe2d",
-    "#f8f8bc",
-    "#17becf",
-    "#10efed", // Dashboards Secondary
-];
-
-export function getColors(colorScheme) {
-    return colorScheme === "dark" ? COLORS_DARK : COLORS_BRIGHT;
+/**
+ * @param {string} colorScheme
+ * @param {string} paletteName
+ * @returns {array}
+ */
+export function getColors(colorScheme, paletteName) {
+    switch (paletteName) {
+        case "odoo":
+            return colorScheme === "dark" ? COLORS_ENT_DARK : COLORS_ENT_BRIGHT;
+        case "sm":
+            return COLORS_SM;
+        case "md":
+            return COLORS_MD;
+        case "lg":
+            return COLORS_LG;
+        default:
+            return COLORS_XL;
+    }
 }
 
 /**
@@ -57,15 +113,27 @@ export function getColors(colorScheme) {
  * @param {string} colorScheme
  * @returns {string}
  */
-export function getColor(index, colorScheme) {
-    const colors = getColors(colorScheme);
+export function getColor(index, colorScheme, paletteSizeOrName) {
+    let paletteName;
+    if(paletteSizeOrName === "odoo") {
+        paletteName = "odoo";
+    } else if(paletteSizeOrName <= 6 || paletteSizeOrName === "sm") {
+        paletteName = "sm";
+    } else if (paletteSizeOrName <= 12 || paletteSizeOrName === "md") {
+        paletteName = "md";
+    } else if (paletteSizeOrName <= 24 || paletteSizeOrName === "lg") {
+        paletteName = "lg";
+    } else {
+        paletteName = "xl";
+    }
+    const colors = getColors(colorScheme, paletteName);
     return colors[index % colors.length];
 }
 
 export const DEFAULT_BG = "#d3d3d3";
 
 export function getBorderWhite(colorScheme) {
-    return colorScheme === "dark" ? "rgba(0, 0, 0, 0.6)" : "rgba(255,255,255,0.6)";
+    return colorScheme === "dark" ? "rgba(38, 42, 54, .2)" : "rgba(249,250,251, .2)";
 }
 
 const RGB_REGEX = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
