6a43280f92e08a80a11a942b9d532c3f34194355
Pieter Claeys (clpi) <clpi@odoo.com>
2024-05-30 16:20:18 +0200

    [IMP] mrp_account: Separate valuation journal item for MO labor costs
    When marking an MO as done, the valuation of the final product might be
    higher than the valuation of the individual components due to the hourly
    costs on the used workcenters and employees on the different workorders.
    
    Before this commit, these differences would cumulate onto the production
    cost account.
    
    After this commit, these labor costs will instead be put on the expense
    account linked to the relevant workcenters. If no expense account is set
    on a workcenter, the expense account linked to the final product will be
    used instead.
    
    Finally, all of these labour costs will then be moved from the expense
    account back to the cost of production account in a separate 'labour'
    journal entry that is posted upon marking the MO as done.
    
    task-3952309
    
    closes odoo/odoo#169257
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp_account/models/mrp_production.py
+++ addons/mrp_account/models/mrp_production.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from ast import literal_eval
+from collections import defaultdict
 
 from odoo import api, fields, models, _
 from odoo.tools import float_round
@@ -108,4 +109,45 @@ class MrpProduction(models.Model):
     def _get_backorder_mo_vals(self):
         res = super()._get_backorder_mo_vals()
         res['extra_cost'] = self.extra_cost
+        return res
+
+    def _post_labour(self):
+        for mo in self:
+            if mo.with_company(mo.company_id).product_id.valuation != 'real_time':
+                continue
+
+            product_accounts = mo.product_id.product_tmpl_id.get_product_accounts()
+            labour_amounts = defaultdict(float)
+            workorders = defaultdict(self.env['mrp.workorder'].browse)
+            for wo in mo.workorder_ids:
+                account = wo.workcenter_id.expense_account_id or product_accounts['expense']
+                labour_amounts[account] += wo._cal_cost()
+                workorders[account] |= wo
+            workcenter_cost = sum(labour_amounts.values())
+
+            if mo.company_id.currency_id.is_zero(workcenter_cost):
+                continue
+
+            desc = _('%s - Labour', mo.name)
+            account = self.env['account.account'].browse(mo.move_finished_ids[0]._get_src_account(product_accounts))
+            labour_amounts[account] -= workcenter_cost
+            account_move = self.env['account.move'].sudo().create({
+                'journal_id': product_accounts['stock_journal'].id,
+                'date': fields.Date.context_today(self),
+                'ref': desc,
+                'move_type': 'entry',
+                'line_ids': [(0, 0, {
+                    'name': desc,
+                    'ref': desc,
+                    'balance': amt,
+                    'account_id': acc.id,
+                }) for acc, amt in labour_amounts.items()]
+            })
+            account_move._post()
+            for line in account_move.line_ids[:-1]:
+                workorders[line.account_id].time_ids.write({'account_move_line_id': line.id})
+
+    def button_mark_done(self):
+        res = super().button_mark_done()
+        self._post_labour()
         return res
