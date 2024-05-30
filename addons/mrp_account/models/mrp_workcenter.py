2429478f5e7b0728bc7ae96c7049f2c108966177
Pieter Claeys (clpi) <clpi@odoo.com>
2024-05-30 16:20:18 +0200

    [IMP] mrp_account: Separate valuation journal item for MO labor costs
    When marking an MO as done, the valuation of the final product might be
    higher than the valuation of the individual components due to the hourly
    costs on the used workcenters and employees on the different workorders.
    
    Before this commit, these differences would cumulate onto the production
    cost account.
    
    After this commit, these labor costs will instead be put on the expense
    account linked to the relevant workcenters. If no expense account is set
    on a workcenter, the expense account linked to the final product will be
    used instead.
    
    Finally, all of these labour costs will then be moved from the expense
    account back to the cost of production account in a separate 'labour'
    journal entry that is posted upon marking the MO as done.
    
    task-3952309

--- addons/mrp_account/models/mrp_workcenter.py
+++ addons/mrp_account/models/mrp_workcenter.py
@@ -9,6 +9,8 @@ class MrpWorkcenter(models.Model):
     _inherit = ['mrp.workcenter', 'analytic.mixin']
 
     costs_hour_account_ids = fields.Many2many('account.analytic.account', compute="_compute_costs_hour_account_ids", store=True)
+    expense_account_id = fields.Many2one('account.account', string="Expense Account", check_company=True,
+                                         help="The expense is accounted for when the manufacturing order is marked as done. If not set, it is the expense account of the final product that will be used instead.")
 
     @api.depends('analytic_distribution')
     def _compute_costs_hour_account_ids(self):
@@ -16,3 +18,10 @@ class MrpWorkcenter(models.Model):
             record.costs_hour_account_ids = bool(record.analytic_distribution) and self.env['account.analytic.account'].browse(
                 list({int(account_id) for ids in record.analytic_distribution for account_id in ids.split(",")})
             ).exists()
+
+
+class MrpWorkcenterProductivity(models.Model):
+    _name = 'mrp.workcenter.productivity'
+    _inherit = 'mrp.workcenter.productivity'
+
+    account_move_line_id = fields.Many2one('account.move.line')
