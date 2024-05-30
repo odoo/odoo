8d1940279e4d0df169fa204eeec9dd9ac7d7befa
Deependra Solanki <deso@odoo.com>
2024-05-30 07:22:43 +0000

    [FIX] web_editor: lingering zwnbsp tracebacks on save
    Steps to reproduce:
    
    1. Go to website > Edit
    2. Drag and drop 'Banner' snippet and 'Items' snippet > Save
    3. Repeat the step 2 again
    4. Error will be generated in terminal
    
    Before this commit:
    
    Dropping and saving new snippets immediately after snippets containing
    links would generate a traceback. This issue occurred because the
    `callPostSnippetDrop` had not completed execution, and `cleanForSave`,
    responsible for removing `zwnbsp` was called prematurely. Consequently
    `historyStep` after `callPostSnippetDrop` would reintroduce `zwnbsp` to
    links.
    
    After this commit:
    
    Ensure that all post-snippet drop promises are resolved before saving.
    
    task-3954229
    
    X-original-commit: 4af99115e1f50e4bd55ba744ad90e71ddda1b20a

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2432,6 +2432,10 @@ class SnippetsMenu extends Component {
         // the invisible DOM list if needed.
         await this._updateInvisibleDOM();
 
+        if (this.__postSnippetDropExtraActions) {
+            this.__postSnippetDropExtraActions();
+            delete this.__postSnippetDropExtraActions;
+        }
         this._postSnippetDropResolver();
     }
     /**
@@ -3608,14 +3612,15 @@ class SnippetsMenu extends Component {
                         // (mutexed as well).
                         dragAndDropResolve();
 
+                        this.__postSnippetDropExtraActions = () => {
+                            // Restore editor to its normal edition state, also
+                            // make sure the undroppable snippets are updated.
+                            this._disableUndroppableSnippets();
+                            this.options.wysiwyg.odooEditor.unbreakableStepUnactive();
+                            this.options.wysiwyg.odooEditor.historyStep();
+                            this.$el.find('.oe_snippet_thumbnail').removeClass('o_we_already_dragging');
+                        };
                         await this.callPostSnippetDrop($target);
-
-                        // Restore editor to its normal edition state, also
-                        // make sure the undroppable snippets are updated.
-                        this._disableUndroppableSnippets();
-                        this.options.wysiwyg.odooEditor.unbreakableStepUnactive();
-                        this.options.wysiwyg.odooEditor.historyStep();
-                        this.$el.find('.oe_snippet_thumbnail').removeClass('o_we_already_dragging');
                     });
                 } else {
                     $toInsert.remove();
@@ -4379,19 +4384,22 @@ class SnippetsMenu extends Component {
         // If it's an OdooEvent sent by sub-widgets, we prevent the event
         // from triggering the request on the parent.
         ev.stopped = true;
-        this._buttonClick((after) => this._execWithLoadingEffect(() => {
-            const oldOnFailure = data.onFailure;
-            data.onFailure = () => {
-                if (oldOnFailure) {
-                    oldOnFailure();
-                }
-                after();
-            };
-            this.props.trigger_up({
-                name: 'request_save',
-                data
-            });
-        }, true), this.$el[0].querySelector('button[data-action=save]'));
+        this._buttonClick(async (after) => {
+            await this.postSnippetDropPromise;
+            return this._execWithLoadingEffect(() => {
+                const oldOnFailure = data.onFailure;
+                data.onFailure = () => {
+                    if (oldOnFailure) {
+                        oldOnFailure();
+                    }
+                    after();
+                };
+                this.props.trigger_up({
+                    name: 'request_save',
+                    data
+                });
+            }, true);
+        }, this.$el[0].querySelector('button[data-action=save]'));
     }
     /**
      * @private
