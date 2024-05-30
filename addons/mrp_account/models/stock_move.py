f72abefd8d192e8ca5020eec0a1e0b6e31469d4e
Pieter Claeys (clpi) <clpi@odoo.com>
2024-05-30 16:20:18 +0200

    [IMP] mrp_account: Separate valuation journal item for MO labor costs
    When marking an MO as done, the valuation of the final product might be
    higher than the valuation of the individual components due to the hourly
    costs on the used workcenters and employees on the different workorders.
    
    Before this commit, these differences would cumulate onto the production
    cost account.
    
    After this commit, these labor costs will instead be put on the expense
    account linked to the relevant workcenters. If no expense account is set
    on a workcenter, the expense account linked to the final product will be
    used instead.
    
    Finally, all of these labour costs will then be moved from the expense
    account back to the cost of production account in a separate 'labour'
    journal entry that is posted upon marking the MO as done.
    
    task-3952309

--- addons/mrp_account/models/stock_move.py
+++ addons/mrp_account/models/stock_move.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from collections import defaultdict
 
 from odoo import models
+from odoo.tools import float_round
 
 
 class StockMove(models.Model):
@@ -42,3 +44,36 @@ class StockMove(models.Model):
     def _is_production_consumed(self):
         self.ensure_one()
         return self.location_dest_id.usage == 'production' and self.location_id._should_be_valued()
+
+    def _generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id, svl_id, description):
+        rslt = super()._generate_valuation_lines_data(partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id, svl_id, description)
+
+        product_expense_account = self.product_id.product_tmpl_id.get_product_accounts()['expense']
+        labour_amounts = defaultdict(float)
+        for wo in self.production_id.workorder_ids:
+            account = wo.workcenter_id.expense_account_id or product_expense_account
+            labour_amounts[account] += wo._cal_cost()
+        workcenter_cost = sum(labour_amounts.values())
+
+        if self.company_id.currency_id.is_zero(workcenter_cost):
+            return rslt
+
+        cost_share = 1
+        if self.production_id.move_byproduct_ids:
+            if self.cost_share:
+                cost_share = self.cost_share / 100
+            else:
+                cost_share = float_round(1 - sum(self.production_id.move_byproduct_ids.mapped('cost_share')) / 100, precision_rounding=0.0001)
+        rslt['credit_line_vals']['balance'] += workcenter_cost * cost_share
+        for acc, amt in labour_amounts.items():
+            rslt['labour_credit_line_vals_' + acc.code] = {
+                'name': description,
+                'product_id': self.product_id.id,
+                'quantity': qty,
+                'product_uom_id': self.product_id.uom_id.id,
+                'ref': description,
+                'partner_id': partner_id,
+                'balance': -amt * cost_share,
+                'account_id': acc.id,
+            }
+        return rslt
