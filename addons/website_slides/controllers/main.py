a48471c5bd49a67696bd4fe18b51278ed48f5c03
Pierre-Yves Dufays <pydu@odoo.com>
2024-05-30 15:46:37 +0200

    [IMP] website_slides{_survey}: improve course homepage
    We merge the page /slides/all into the home page /slides, allowing to search
    course on the home page while still displaying the ongoing course and
    progression. We also add a pager for the search results.
    
    The new home page is basically the old “slides/all” page with the sidebar of
    the old home page and with the user courses displayed at the top (ordered and
    limited to 5). The user course list is only displayed if there are no active
    search. At the end of that list if the user has more than 5 courses, there is a
    link to activate the “my course” search which will display all user courses
    (paginated without the list on the top as a search is then active).
    
    Task-3893028

--- addons/website_slides/controllers/main.py
+++ addons/website_slides/controllers/main.py
@@ -10,6 +10,7 @@ import math
 import werkzeug
 
 from odoo import fields, http, tools, _
+from odoo.addons.base.models.ir_qweb import keep_query
 from odoo.addons.website.controllers.main import QueryURL
 from odoo.addons.website.models.ir_http import sitemap_qs2dom
 from odoo.addons.website_profile.controllers.main import WebsiteProfile
@@ -337,17 +338,19 @@ class WebsiteSlides(WebsiteProfile):
     # SLIDE.CHANNEL MAIN / SEARCH
     # --------------------------------------------------
 
-    @http.route('/slides', type='http', auth="public", website=True, sitemap=True, readonly=True, list_as_website_content=_lt("eLearning"))
-    def slides_channel_home(self, **post):
-        """ Home page for eLearning platform. Is mainly a container page, does not allow search / filter. """
-        channels_all = tools.lazy(lambda: request.env['slide.channel'].search(request.website.website_domain() & Domain('is_visible', '=', True)))
-        if not request.env.user._is_public():
-            #If a course is completed, we don't want to see it in first position but in last
-            channels_my = tools.lazy(lambda: channels_all.filtered(lambda channel: channel.is_member).sorted(lambda channel: 0 if channel.completed else channel.completion, reverse=True)[:3])
-        else:
-            channels_my = request.env['slide.channel']
-        channels_popular = tools.lazy(lambda: channels_all.sorted('total_votes', reverse=True)[:3])
-        channels_newest = tools.lazy(lambda: channels_all.sorted('create_date', reverse=True)[:3])
+    def _slides_channel_user_values(self, compute_channels_my=True):
+        """ Get user slide values (challenge done, top user to compare to, ...). """
+        render_values = {}
+        if compute_channels_my:
+            if not request.env.user._is_public():
+                channels_my_all = tools.lazy(lambda: request.env['slide.channel'].search(
+                    request.website.website_domain() & Domain([('is_visible', '=', True), ('is_member', '=', True)])))
+                # Order: Started but not finished > Not started > Finished
+                channels_my = tools.lazy(lambda: channels_my_all.filtered(lambda channel: channel.is_member).sorted(
+                    lambda channel: -1 if channel.completed else channel.completion, reverse=True))
+            else:
+                channels_my = request.env['slide.channel']
+            render_values['channels_my'] = channels_my
 
         achievements = tools.lazy(lambda: request.env['gamification.badge.user'].sudo().search([('badge_id.is_published', '=', True)], limit=5))
         if request.env.user._is_public():
@@ -368,23 +371,17 @@ class WebsiteSlides(WebsiteProfile):
             ('karma', '>', 0),
             ('website_published', '=', True)], limit=5, order='karma desc'))
 
-        render_values = self._slide_render_context_base()
-        render_values.update(self._prepare_user_values(**post))
         render_values.update({
-            'channels_my': channels_my,
-            'channels_popular': channels_popular,
-            'channels_newest': channels_newest,
             'achievements': achievements,
             'users': users,
             'top3_users': tools.lazy(self._get_top3_users),
             'challenges': challenges,
             'challenges_done': challenges_done,
             'search_tags': request.env['slide.channel.tag'],
-            'slide_query_url': QueryURL('/slides/all', ['tag']),
+            'slide_query_url': QueryURL('/slides', ['tag']),
             'slugify_tags': self._slugify_tags,
         })
-
-        return request.render('website_slides.courses_home', render_values)
+        return render_values
 
     def _get_slide_channel_search_options(self, my=None, slug_tags=None, slide_category=None, **post):
         return {
@@ -399,8 +396,15 @@ class WebsiteSlides(WebsiteProfile):
             'slide_category': slide_category,
         }
 
-    @http.route(['/slides/all', '/slides/all/tag/<string:slug_tags>'], type='http', auth="public", website=True, sitemap=True, readonly=True)
-    def slides_channel_all(self, slide_category=None, slug_tags=None, my=False, **post):
+    def _has_slide_channel_search(self, my=None, slug_tags=None, slide_category=None, **post):
+        return my or post.get('search') or slug_tags or post.get('tag') or slide_category
+
+    @http.route(['/slides', '/slides/page/<int:page>',
+                 '/slides/tag/<string:slug_tags>', '/slides/tag/<string:slug_tags>/page/<int:page>'],
+                type='http', auth="public", website=True, sitemap=True, readonly=True,
+                list_as_website_content=_lt("eLearning"))
+    def slides_channel(self, slide_category=None, slug_tags=None, my=0, page=1, **post):
+        my = 1 if str(my) == '1' else 0  # if in the URL parameters, it will be a string instead of a number
         if slug_tags and slug_tags.count(',') > 0 and request.httprequest.method == 'GET' and not post.get('prevent_redirect'):
             # Previously, the tags were searched using GET, which caused issues with crawlers (too many hits)
             # We replaced those with POST to avoid that, but it's not sufficient as bots "remember" crawled pages for a while
@@ -408,12 +412,18 @@ class WebsiteSlides(WebsiteProfile):
             # TODO: remove in a few stable versions (v19?), including the "prevent_redirect" param in templates
             # Note: We allow a single tag to be GET, to keep crawlers & indexes on those pages
             # What we really want to avoid is combinatorial explosions
-            return request.redirect('/slides/all', code=301)
+            return request.redirect('/slides', code=301)
 
-        render_values = self.slides_channel_all_values(slide_category=slide_category, slug_tags=slug_tags, my=my, **post)
-        return request.render('website_slides.courses_all', render_values)
+        render_values = self.slides_channel_values(
+            slide_category=slide_category, slug_tags=slug_tags, my=my, page=page, **post)
+        if page > 1 and not render_values['channels']:
+            # Refining search may reduce results; if no results and not on page 1, reset to page 1.
+            if slug_tags:
+                return request.redirect(f"/slides/tag/{slug_tags}?{keep_query('*')}")
+            return request.redirect(f"/slides?{keep_query('*')}")
+        return request.render('website_slides.courses_home', render_values)
 
-    def slides_channel_all_values(self, slide_category=None, slug_tags=None, my=False, **post):
+    def slides_channel_values(self, slide_category=None, slug_tags=None, my=0, page=None, page_size=12, **post):
         """ Home page displaying a list of courses displayed according to some
         criterion and search terms.
 
@@ -425,21 +435,24 @@ class WebsiteSlides(WebsiteProfile):
           :param bool my: if provided, filter the slide.channels for which the
            current user is a member of
           :param dict post: post parameters, including
+          :param int|None page: The current page number. Set to None to disable pagination (default).
+          :param int page_size: number of element per page
 
            * ``search``: filter on course description / name;
         """
-        options = self._get_slide_channel_search_options(
-            my=my,
-            slug_tags=slug_tags,
-            slide_category=slide_category,
+        search_args = {
+            'my': my,
+            'slug_tags': slug_tags,
+            'slide_category': slide_category,
             **post
-        )
+        }
+        options = self._get_slide_channel_search_options(**search_args)
         search = post.get('search')
         order = self._channel_order_by_criterion.get(post.get('sorting'))
-        search_count, details, fuzzy_search_term = request.website._search_with_fuzzy("slide_channels_only", search,
-            limit=1000, order=order, options=options)
-        channels = details[0].get('results', request.env['slide.channel'])
-
+        search_count, details, fuzzy_search_term = request.website._search_with_fuzzy(
+            "slide_channels_only", search, limit=page * page_size if page else 1000, order=order, options=options)
+        channels_all = details[0].get('results', request.env['slide.channel'])
+        channels = channels_all[(page - 1) * page_size:page * page_size] if page else channels_all
         tag_groups = request.env['slide.channel.tag.group'].search(
             ['&', ('tag_ids', '!=', False), ('website_published', '=', True)])
         if slug_tags:
@@ -451,6 +464,8 @@ class WebsiteSlides(WebsiteProfile):
 
         render_values = self._slide_render_context_base()
         render_values.update(self._prepare_user_values(**post))
+        render_values.update(self._slides_channel_user_values(
+            compute_channels_my=not self._has_slide_channel_search(**search_args)))
         render_values.update({
             'channels': channels,
             'tag_groups': tag_groups,
@@ -462,7 +477,14 @@ class WebsiteSlides(WebsiteProfile):
             'search_count': search_count,
             'top3_users': self._get_top3_users(),
             'slugify_tags': self._slugify_tags,
-            'slide_query_url': QueryURL('/slides/all', ['tag']),
+            'slide_query_url': QueryURL('/slides', ['tag']),
+            'pager': request.website.pager(
+                url=request.httprequest.path.partition('/page/')[0],
+                url_args=request.httprequest.args.to_dict(),
+                total=search_count,
+                page=page,
+                step=page_size,
+                scope=3) if page else False,
         })
 
         return render_values
