69784bc0747506c15dfa9ba5e14e14c66970db6d
Habib (ayh) <ayh@odoo.com>
2024-05-31 11:57:39 +0200

    [FIX] account: invoice plus nightly tests
    With the invoice plus PR, nightly tests have started to fail.
    
    - test_01_account_tax_groups_tour: Build Error 66350 - in community, the home icon was clicked twice
    
    closes odoo/odoo#168022
    
    X-original-commit: c66e7eeeea846c1e2b61ed9f14797fa1ec829777
    Related: odoo/enterprise#63811
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>

--- addons/account/static/tests/tours/tax_group_tests.js
+++ addons/account/static/tests/tours/tax_group_tests.js
@@ -7,7 +7,7 @@ import { stepUtils } from "@web_tour/tour_service/tour_utils";
 registry.category("web_tour.tours").add('account_tax_group', {
     test: true,
     url: "/web",
-    steps: () => [stepUtils.showAppsMenuItem(),
+    steps: () => [
     ...accountTourSteps.goToAccountMenu("Go to Invoicing"),
     {
         content: "Go to Vendors",
