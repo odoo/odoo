873e77f000900dbd958a740dae9c5b62d77e9257
Thomas Lefebvre (thle) <thle@odoo.com>
2024-06-03 06:57:48 +0000

    [FIX] sale_project: get customer invoices on project's updates
    Steps to reproduce:
    -------------------
    - create a project
    - create an invoice with Analytic Distribution for the project
    - go to the project's updates
    - in profitability section, click on "Customer Invoices"
    
    Issue:
    ------
    A notification appears:
    ```
    It seems the record with ID X cannot be found. It might have been deleted.
    ```
    
    Cause:
    ------
    The arguments in JSON format are:
    ```
    "["other_invoice_revenues", [["id", "in", [X]]], X]"
    ```
    with X not corresponding to the invoice id.
    
    They are determined by the method `_get_action_for_profitability_section`.
    In which the ids are `invoices_move_lines.ids`
    (in `_get_revenues_items_from_invoices`).
    
    Solution:
    ---------
    Find the `account.move` from the `account.move.line`.
    
    Note:
    Introduced with the commit abd40a461c289bfe092784977077ceb1c7ef12f3
    
    opw-3924836
    
    closes odoo/odoo#168260
    
    X-original-commit: e820e2f886f50df4cbd5c8f692da1aa638c8877c
    Signed-off-by: Audric Onockx (auon) <auon@odoo.com>
    Signed-off-by: Thomas Lefebvre (thle) <thle@odoo.com>

--- addons/sale_project/models/project.py
+++ addons/sale_project/models/project.py
@@ -605,7 +605,7 @@ class Project(models.Model):
                     or self.env.user.has_group('account.group_account_invoice')
                     or self.env.user.has_group('account.group_account_readonly')
                 ):
-                    invoices_revenues['action'] = self._get_action_for_profitability_section(invoices_move_lines.ids, section_id)
+                    invoices_revenues['action'] = self._get_action_for_profitability_section(invoices_move_lines.move_id.ids, section_id)
                 return {
                     'data': [invoices_revenues],
                     'total': {
