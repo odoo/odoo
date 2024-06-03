22dca40ee7af3f1d6b9027313885fb3f57091543
Mohammad Abdulmoneim <abdu@odoo.com>
2024-06-03 14:11:27 +0300

    [FIX] {purchase_}stock: prevent reordering rules for consumable products
    Bug:
    It was possible to create a reordering rule for a non-storable product
    via the import records tool. This is because the domain on the product
    (limiting the selection to only storable products) for the reordering
    rule model was only enforced on the web client.
    
    Fix:
    Enforce this programatically in the `create` method of the model.
    
    Task-3866078

--- addons/purchase_stock/tests/test_purchase_order.py
+++ addons/purchase_stock/tests/test_purchase_order.py
@@ -483,6 +483,7 @@ class TestPurchaseOrder(ValuationReconciliationTestCommon):
 
         product = self.env['product.product'].create({
             'name': 'Super Product',
+            'type': 'product',
             'seller_ids': [(0, 0, {
                 'name': self.partner_a.id,
                 'min_qty': 1,
