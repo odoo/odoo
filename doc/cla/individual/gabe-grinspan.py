e4010414d6323ce52fa6841a4e8729ce082974fb
Gabriel Grinspan <grinspan.gabriel@gmail.com>
2024-06-04 14:28:49 -0400

    [CLA] gabe-grinspan

---
+++ doc/cla/individual/gabe-grinspan.py
@@ -0,0 +1,11 @@
+United States of America, 2024-06-04
+
+I hereby agree to the terms of the Odoo Individual Contributor License
+Agreement v1.0.
+
+I declare that I am authorized and able to make this agreement and sign this
+declaration.
+
+Signed,
+
+Gabriel Grinspan grinspan.gabriel@gmail.com https://github.com/gabe-grinspan
