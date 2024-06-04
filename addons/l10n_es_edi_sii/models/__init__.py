94d3429d26721d5bf47b7673e61170aa056d2904
Anh Thao Pham (pta) <pta@odoo.com>
2024-06-04 09:34:16 +0200

    [FIX] l10n_es_edi_sii: exclude jsondump.json from "SEND & PRINT"
    Steps to reproduce: (!!! Spanish EDI credentials required !!!)
    - Install l10n_es_edi_sii
    - Switch to a Spanish company (e.g. ES Company)
    - Configure Spanish Localization
    - Create an invoice
    - Process it by the ES E-invoicing service
    - Once processed, an electronic document containing the response of the
    E-invoicing service is created (i.e. jsondump.json)
    
    Issue:
    When sending the invoice via "SEND & PRINT" button, the "jsondump.json"
    file is added in the attachments.
    
    opw-3720984
    
    closes odoo/odoo#168459
    
    X-original-commit: 000579b7aa3cb640a642ae62162f651cce621790
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>
    Signed-off-by: Anh Thao Pham (pta) <pta@odoo.com>

--- addons/l10n_es_edi_sii/models/__init__.py
+++ addons/l10n_es_edi_sii/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import account_edi_format
+from . import account_move_send
 from . import account_move
 from . import account_tax
 from . import account_chart_template
