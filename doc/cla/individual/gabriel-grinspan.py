df5b75ec99c87bdb679a98db28d85684fab28dc2
Gabriel <gabriel@rooteam.net>
2024-06-04 14:25:56 -0400

    [CLA] gabriel-grinspan

---
+++ doc/cla/individual/gabriel-grinspan.py
@@ -0,0 +1,11 @@
+United States of America, 2024-06-04
+
+I hereby agree to the terms of the Odoo Individual Contributor License
+Agreement v1.0.
+
+I declare that I am authorized and able to make this agreement and sign this
+declaration.
+
+Signed,
+
+Gabriel Grinspan gabriel@rooteam.net https://github.com/gabriel-grinspan
