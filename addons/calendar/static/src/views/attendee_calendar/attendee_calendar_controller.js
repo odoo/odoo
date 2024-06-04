a22ba6484ed4f1bdf23b50c7f8d1f2e3c409782b
nikj-odoo <nikj@odoo.com>
2024-06-04 16:51:58 +0530

    [IMP] calendar: notify attendees upon event deletion
    Before this commit, when an event was deleted in Odoo, attendees were not
    notified, which could lead to confusion.
    
    This commit introduces the following improvements:
    
    1. Event Deletion Notification:
       When an event with attendees is deleted, a mail wizard prompts the user
       to send a notification to inform attendees about the cancellation.
    
    2. Attendee Status Update:
       Upon sending the notification, the status of all attendees is automatically
       set to "Declined" (assuming there are one or more attendees).
    
    Introduce a configuration option allowing users to choose whether attendees
    should be automatically declined upon event cancellation.The mail wizard can
    provide pre-populated email content, including details about the canceled event
    and a brief cancellation message.
    
    task-3928050

--- addons/calendar/static/src/views/attendee_calendar/attendee_calendar_controller.js
+++ addons/calendar/static/src/views/attendee_calendar/attendee_calendar_controller.js
@@ -87,8 +87,15 @@ export class AttendeeCalendarController extends CalendarController {
         ) {
             if (record.rawRecord.recurrency) {
                 this.openRecurringDeletionWizard(record);
-            } else {
+            } else if (user.partnerId === record.attendeeId &&
+                record.rawRecord.attendees_count == 1) {
                 super.deleteRecord(...arguments);
+            } else {
+                const action = this.orm.call("calendar.event", "action_unlink_event", [
+                    record.id,
+                    record.attendeeId,
+                ]);
+                this.actionService.doAction(action);
             }
         } else {
             // Decline event
@@ -106,7 +113,11 @@ export class AttendeeCalendarController extends CalendarController {
                 views: [[false, "form"]],
                 view_mode: "form",
                 name: "Delete Recurring Event",
-                context: { default_record: record.id },
+                context: {
+                    default_record: record.id,
+                    default_attendee_id: record.attendeeId,
+                    form_view_ref: 'calendar.calendar_popover_delete_view',
+                },
                 target: "new",
             },
             {
--- addons/calendar/static/src/views/calendar_form/calendar_form_controller.js
+++ addons/calendar/static/src/views/calendar_form/calendar_form_controller.js
@@ -6,6 +6,7 @@ export class CalendarFormController extends FormController {
     setup() {
         super.setup();
         const ormService = useService("orm");
+        this.actionService = useService("action");
 
         onWillStart(async () => {
             this.discussVideocallLocation = await ormService.call(
@@ -37,5 +38,25 @@ export class CalendarFormController extends FormController {
             return false; // no continue
         }
         return super.beforeExecuteActionButton(...arguments);
+    }
+
+    /**
+     * Custom delete function for calendar events, which can call the unlink action or not.
+     * When there is only one attendee, who is also the organizer, and the organizer is not listed in the current attendees, it performs the default delete.
+     * Otherwise, it calls the unlink action on the server.
+     */
+    async deleteRecord() {
+        const rootValues = this.model.root._values;
+        if (rootValues.attendees_count == 1 && rootValues.user_id[0] !== rootValues.partner_ids._currentIds[0]) {
+            // Call the default delete if the event has only one attendee and the user is not listed in partner_ids.
+            super.deleteRecord(...arguments);
+        } else {
+            const action = await this.orm.call("calendar.event", "action_unlink_event", [
+                this.model.root.resId,
+                this.model.root.data.partner_ids.resIds,
+                this.model.root.data.recurrence_update,
+            ]);
+            this.actionService.doAction(action);
+        }
     }
 }
