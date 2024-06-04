6863731ac73c43d1a9b691dc2d8a547adbb32e7e
Mohammad Abdulmoneim <abdu@odoo.com>
2024-06-04 11:36:14 +0300

    [FIX] purchase_requisition_stock: include BO description in gen. RFQs
    Steps to reproduce:
    - Create a blanket order with some vendor and a product.
    - Add a custom description to the blanket order line of that product.
    - Create a reordering rule for the same product.
    - Click on "Order Once" for this reordering rule.
    
    Expected behavior:
    The PO line of this product should include the custom description from
    the blanket order.
    
    Current behavior:
    Only the product name is included in the description.
    
    Task-3866078

--- addons/purchase_requisition_stock/models/purchase.py
+++ addons/purchase_requisition_stock/models/purchase.py
@@ -12,3 +12,16 @@ class PurchaseOrder(models.Model):
         super(PurchaseOrder, self)._onchange_requisition_id()
         if self.requisition_id:
             self.picking_type_id = self.requisition_id.picking_type_id.id
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    @api.model
+    def _prepare_purchase_order_line_from_procurement(self, product_id, product_qty, product_uom, company_id, values, po):
+        res = super()._prepare_purchase_order_line_from_procurement(product_id, product_qty, product_uom, company_id, values, po)
+        if po.requisition_id:
+            requisition_line = po.requisition_id.line_ids.filtered(lambda line: line.product_id == product_id)
+            if requisition_line.product_description_variants:
+                res['name'] += '\n' + requisition_line.product_description_variants
+        return res
