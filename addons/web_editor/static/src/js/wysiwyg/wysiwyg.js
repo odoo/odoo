90f6dfbd2df33b0722187d1cafa0a74ddfb03c2b
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-06-04 12:40:14 +0000

    [FIX] web_editor, mass_mailing: wait for snippets to load
    Issue:
    ======
    We can update the content of the editable before we choose the theme
    which results in unexpected behaviors.
    
    Steps to reproduce the issue:
    =============================
    - Open the email marketing app
    - In the browser dev tools, set a big network throttle
    - Create a new mailing
    - Write in the editable zone before the template selection has had a
      chance to appear
    - Click on design tab in the sidebar
    - Traceback
    
    Spec:
    =====
    We should not be able to write in the editable zone before choosing a
    template.
    
    Solution:
    =========
    We add a new option `waitForSnippets` for the `wysiwyg` to force the
    editable element as `contenteditable="false"` until the snippets are
    loaded. Once the snippets are loaded we set `contenteditable="true"`
    back again.
    
    Note 1: Removing the line of `this._activatecontenteditable` from
    `_onMouseDown` in `odooEditor` because it keeps setting the editable
    element as `contenteditable="true"` everytime we click. The line can be
    safely removed because it's a leftorver from when clicking on links
    would set everything to `contenteditable=false` except the link itself,
    then clicking somewhere else would reactivate it. (introduced here
    https://github.com/odoo/odoo/commit/72dd8d6e3c95f614353b1e87322a67c877c255c7)
    . When it was outside the condition, it was hard to guess that it was not
    needed anymore when we removed that `contenteditable` hack for links.
    
    Note 2: the `odoo-editor` div in website have `contenteditable=false`
    so dropped snippets will not be editable too. We call
    `activatecontenteditable` after the drop of the snippets.
    
    task-3901534
    
    X-original-commit: 209457ef793ac2b05f8e9f6cdc674947a9d8c8f5

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -514,6 +514,7 @@ export class Wysiwyg extends Component {
             showExtendedTextStylesOptions: options.showExtendedTextStylesOptions,
             getCSSVariableValue: options.getCSSVariableValue,
             convertNumericToUnit: options.convertNumericToUnit,
+            autoActivateContentEditable: this.options.autoActivateContentEditable,
         }, editorCollaborationOptions));
 
         this.odooEditor.addEventListener('contentChanged', function () {
