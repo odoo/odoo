45a571fb40ae856cc901e883b50d38ff3e8534bf
Lucas Lefèvre (lul) <lul@odoo.com>
2024-06-05 12:18:44 +0200

    [FIX] spreadsheet: export From/To filter when sharing
    Steps to reproduce:
    - insert a pivot in a spreadsheet
    - create a From/To global filter
    - leave the filter values empty
    - click on the Share button
    
    => boom because of `cell.value.toString()`
    `cannot read toString of undefined`
    
    opw-3971278
    
    closes odoo/odoo#168147
    
    Signed-off-by: Vincent Schippefilt (vsc) <vsc@odoo.com>

--- addons/spreadsheet/static/tests/public_spreadsheet/freeze_test.js
+++ addons/spreadsheet/static/tests/public_spreadsheet/freeze_test.js
@@ -160,7 +160,25 @@ QUnit.module("freezing spreadsheet", {}, function () {
         assert.strictEqual(data.globalFilters[0].value, "1/1/2020, 1/1/2021");
     });
 
-    QUnit.test("odoo links are replaced with their label", async function(assert){
+    QUnit.test("from/to global filter without value is exported", async function (assert) {
+        const model = await createModelWithDataSource();
+        await addGlobalFilter(model, {
+            id: "42",
+            type: "date",
+            label: "Date Filter",
+            rangeType: "from_to",
+        });
+        const data = await freezeOdooData(model);
+        const filterSheet = data.sheets[1];
+        assert.strictEqual(filterSheet.cells.A2.content, "Date Filter");
+        assert.strictEqual(filterSheet.cells.B2, undefined);
+        assert.strictEqual(filterSheet.cells.C2, undefined);
+        assert.strictEqual(data.globalFilters.length, 1);
+        assert.strictEqual(data.globalFilters[0].label, "Date Filter");
+        assert.strictEqual(data.globalFilters[0].value, "");
+    });
+
+    QUnit.test("odoo links are replaced with their label", async function (assert) {
         const view = {
             name: "an odoo view",
             viewType: "list",
@@ -176,23 +194,32 @@ QUnit.module("freezing spreadsheet", {}, function () {
                         A1: { content: "[menu_xml](odoo://ir_menu_xml_id/test_menu)" },
                         A2: { content: "[menu_id](odoo://ir_menu_id/12)" },
                         A3: { content: `[odoo_view](odoo://view/${JSON.stringify(view)})` },
-                        A4: { content: "[external_link](https://odoo.com)" } ,
-                        A5: { content: "[internal_link](o-spreadsheet://Sheet1)"}
+                        A4: { content: "[external_link](https://odoo.com)" },
+                        A5: { content: "[internal_link](o-spreadsheet://Sheet1)" },
                     },
-
                 },
             ],
         };
-      registry.category("services")
-          .add("menu", menuService)
-          .add("spreadsheetLinkMenuCell", spreadsheetLinkMenuCellService);
+        registry
+            .category("services")
+            .add("menu", menuService)
+            .add("spreadsheetLinkMenuCell", spreadsheetLinkMenuCellService);
 
-        const model = await createModelWithDataSource({ spreadsheetData: data, serverData: getMenuServerData() });
+        const model = await createModelWithDataSource({
+            spreadsheetData: data,
+            serverData: getMenuServerData(),
+        });
         const frozenData = await freezeOdooData(model);
         assert.strictEqual(frozenData.sheets[0].cells.A1.content, "menu_xml");
         assert.strictEqual(frozenData.sheets[0].cells.A2.content, "menu_id");
         assert.strictEqual(frozenData.sheets[0].cells.A3.content, "odoo_view");
-        assert.strictEqual(frozenData.sheets[0].cells.A4.content, "[external_link](https://odoo.com)");
-        assert.strictEqual(frozenData.sheets[0].cells.A5.content, "[internal_link](o-spreadsheet://Sheet1)");
+        assert.strictEqual(
+            frozenData.sheets[0].cells.A4.content,
+            "[external_link](https://odoo.com)"
+        );
+        assert.strictEqual(
+            frozenData.sheets[0].cells.A5.content,
+            "[internal_link](o-spreadsheet://Sheet1)"
+        );
     });
 });
