christian-rb/odoo-dev

[IMP] product,mrp,sale: add/modify tests
This commit adapts the existing tests and adds new ones for the new
configurator widget

Enterprise: https://github.com/odoo/enterprise/pull/44384

task-2732022

addons/test_sale_product_configurators/tests/test_sale_product_configurator.py
@@ -3,11 +3,11 @@
 from odoo.tests.common import HttpCase, tagged
 
 from odoo.addons.mail.tests.common import mail_new_test_user
-from odoo.addons.sale_product_configurator.tests.common import TestProductConfiguratorCommon
+from odoo.addons.sale_product_configurator.tests.common import TestSaleProductConfiguratorCommon
 
 
 @tagged('post_install', '-at_install')
-class TestProductConfiguratorUi(HttpCase, TestProductConfiguratorCommon):
+class TestProductConfiguratorUi(HttpCase, TestSaleProductConfiguratorCommon):
 
     @classmethod
     def setUpClass(cls):

addons/website_sale_stock/tests/test_website_sale_stock_stock_message.py
@@ -2,11 +2,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.tests import tagged
-from odoo.addons.sale_product_configurator.tests.common import TestProductConfiguratorCommon
+from odoo.addons.sale_product_configurator.tests.common import TestSaleProductConfiguratorCommon
 from odoo.addons.base.tests.common import HttpCaseWithUserPortal
 
 @tagged('post_install', '-at_install')
-class TestWebsiteSaleStockProductConfigurator(TestProductConfiguratorCommon, HttpCaseWithUserPortal):
+class TestWebsiteSaleStockProductConfigurator(TestSaleProductConfiguratorCommon, HttpCaseWithUserPortal):
 
     def test_01_stock_message_update_after_close_with_optional_products(self):
         product_product_with_options = self.env['product.product'].create({

odoo/odoo

addons/test_sale_product_configurators/tests/test_sale_product_configurator.py
@@ -3,11 +3,11 @@
 from odoo.tests.common import HttpCase, tagged
 
 from odoo.addons.mail.tests.common import mail_new_test_user
-from odoo.addons.sale.tests.product_configurator_common import TestProductConfiguratorCommon
+from odoo.addons.sale.tests.product_configurator_common import TestSaleProductConfiguratorCommon
 
 
 @tagged('post_install', '-at_install')
-class TestProductConfiguratorUi(HttpCase, TestProductConfiguratorCommon):
+class TestProductConfiguratorUi(HttpCase, TestSaleProductConfiguratorCommon):
 
     @classmethod
     def setUpClass(cls):

addons/website_sale_stock/tests/test_website_sale_stock_stock_message.py
@@ -2,11 +2,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.tests import tagged
-from odoo.addons.sale.tests.product_configurator_common import TestProductConfiguratorCommon
+
+from odoo.addons.sale.tests.product_configurator_common import TestSaleProductConfiguratorCommon
 from odoo.addons.base.tests.common import HttpCaseWithUserPortal
 
 @tagged('post_install', '-at_install')
-class TestWebsiteSaleStockProductConfigurator(TestProductConfiguratorCommon, HttpCaseWithUserPortal):
+class TestWebsiteSaleStockProductConfigurator(TestSaleProductConfiguratorCommon, HttpCaseWithUserPortal):
 
     def test_01_stock_message_update_after_close_with_optional_products(self):
         product_product_with_options = self.env['product.product'].create({