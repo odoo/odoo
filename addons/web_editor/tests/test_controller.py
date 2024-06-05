210e6f38e0fe7f10c8b99cf228920b485675fa09
Xavier-Do <xdo@odoo.com>
2024-06-05 10:09:02 +0200

    [FIX] web_edtor: fix for pillow < 8.0
    3aa3a24c introduced a regression since textbbox is not available in
    Pillow < 8.0.
    
    Since focal has a lower version, this is breaking on odoosh and in
    PureFocal builds.
    
    Fixing it by making the call conditionnal.

--- addons/web_editor/tests/test_controller.py
+++ addons/web_editor/tests/test_controller.py
@@ -3,8 +3,12 @@
 
 import binascii
 
+from io import BytesIO
+from PIL import Image
+
 from odoo.addons.http_routing.models.ir_http import slug
 import odoo.tests
+from odoo.tools.misc import file_open
 from odoo.tests.common import HttpCase
 
 @odoo.tests.tagged('-at_install', 'post_install')
@@ -59,3 +63,16 @@ class TestController(HttpCase):
         self.assertEqual(len(svg), len(response.content), 'Expect same length as original')
         self.assertTrue('ABCDEF' in str(response.content), 'Expect patched c1')
         self.assertTrue('3AADAA' not in str(response.content), 'Old c1 should not be there anymore')
+
+    def test_font_to_img(self):
+        # This test was introduced because the play button was cropped in noble following some adaptation.
+        # This test is able to reproduce the issue and ensure that the expected result is the right one
+        # comparing image is not ideal, but this should work in most case, maybe adapted if the font is changed.
+
+        response = self.url_open('/web_editor/font_to_img/61802/rgb(0,143,140)/rgb(255,255,255)/190x200')
+
+        img = Image.open(BytesIO(response.content))
+        self.assertEqual(img.size, (201, 200), 'Looks strange regarding request but this is the current result')
+        # Image is a play button
+        img_reference = Image.open(file_open('web_editor/tests/play.png', 'rb'))
+        self.assertEqual(img, img_reference, "Result image should be the play button")

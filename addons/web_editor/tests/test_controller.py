odoo/odoo

commit 3aa3a24c5bce9139a268f40f97784ba9b2af8c19

[FIX] web_editor: make heigher pillow version support
Before this commit:
We used the .textsize method, which returned the height and width of the
icon. However, in the latest version of Pillow, this method was deprecated,
resulting in errors in the log.

After this commit:
We have transitioned to using the textbbox method, which returns the
coordinates of the top, left, bottom, and right of the text's bounding box.
From these coordinates, we calculate the height and width of the text.

task-3502373

X-original-commit: 22c082f9964975115a30c41b95ece778385d4ee1
Part-of: odoo/odoo#159994

addons/web_editor/controllers/main.py
@@ -173,7 +173,9 @@ class Web_Editor(http.Controller):
         image = Image.new("RGBA", (width, height), color)
         draw = ImageDraw.Draw(image)
 
-        boxw, boxh = draw.textsize(icon, font=font_obj)
+        box = draw.textbbox((0, 0), icon, font=font_obj)
+        boxw = box[2] - box[0]
+        boxh = box[3] - box[1]
         draw.text((0, 0), icon, font=font_obj)
         left, top, right, bottom = image.getbbox()
 

commit 210e6f38e0fe7f10c8b99cf228920b485675fa09

[FIX] web_edtor: fix for pillow < 8.0
3aa3a24c introduced a regression since textbbox is not available in
Pillow < 8.0.

Since focal has a lower version, this is breaking on odoosh and in
PureFocal builds.

Fixing it by making the call conditionnal.

addons/web_editor/controllers/main.py
@@ -124,15 +124,22 @@ class Web_Editor(http.Controller):
         image = Image.new("RGBA", (width, height), color)
         draw = ImageDraw.Draw(image)
 
-        box = draw.textbbox((0, 0), icon, font=font_obj)
-        boxw = box[2] - box[0]
-        boxh = box[3] - box[1]
+        if hasattr(draw, 'textbbox'):
+            box = draw.textbbox((0, 0), icon, font=font_obj)
+            left = box[0]
+            top = box[1]
+            boxw = box[2] - box[0]
+            boxh = box[3] - box[1]
+        else:  # pillow < 8.00 (Focal)
+            left, top, _right, _bottom = image.getbbox()
+            boxw, boxh = draw.textsize(icon, font=font_obj)
+
         draw.text((0, 0), icon, font=font_obj)
 
         # Create an alpha mask
         imagemask = Image.new("L", (boxw, boxh), 0)
         drawmask = ImageDraw.Draw(imagemask)
-        drawmask.text((-box[0], -box[1]), icon, font=font_obj, fill=255)
+        drawmask.text((-left, -top), icon, font=font_obj, fill=255)
 
         # Create a solid color image and apply the mask
         if color.startswith('rgba'):
@@ -143,7 +150,7 @@ class Web_Editor(http.Controller):
 
         # Create output image
         outimage = Image.new("RGBA", (boxw, height), bg or (0, 0, 0, 0))
-        outimage.paste(iconimage, (box[0], box[1]), iconimage)
+        outimage.paste(iconimage, (left, top), iconimage)
 
         # output image
         output = io.BytesIO()

addons/web_editor/tests/test_controller.py
@@ -3,8 +3,12 @@
 
 import binascii
 
+from io import BytesIO
+from PIL import Image
+
 from odoo.addons.http_routing.models.ir_http import slug
 import odoo.tests
+from odoo.tools.misc import file_open
 from odoo.tests.common import HttpCase
 
 @odoo.tests.tagged('-at_install', 'post_install')
@@ -59,3 +63,16 @@ class TestController(HttpCase):
         self.assertEqual(len(svg), len(response.content), 'Expect same length as original')
         self.assertTrue('ABCDEF' in str(response.content), 'Expect patched c1')
         self.assertTrue('3AADAA' not in str(response.content), 'Old c1 should not be there anymore')
+
+    def test_font_to_img(self):
+        # This test was introduced because the play button was cropped in noble following some adaptation.
+        # This test is able to reproduce the issue and ensure that the expected result is the right one
+        # comparing image is not ideal, but this should work in most case, maybe adapted if the font is changed.
+
+        response = self.url_open('/web_editor/font_to_img/61802/rgb(0,143,140)/rgb(255,255,255)/190x200')
+
+        img = Image.open(BytesIO(response.content))
+        self.assertEqual(img.size, (201, 200), 'Looks strange regarding request but this is the current result')
+        # Image is a play button
+        img_reference = Image.open(file_open('web_editor/tests/play.png', 'rb'))
+        self.assertEqual(img, img_reference, "Result image should be the play button")