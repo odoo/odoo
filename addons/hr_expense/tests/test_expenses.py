97f69d5d9d8427a13558491d37fa7db3238b0518
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2024-06-06 12:14:32 +0000

    [FIX] hr_expense: mandatory plan not matching product category
    Create an analytic plan [TEST] with default optional availability
    Add an applicability line with:
    - Domain: Expense
    - Product Cateogry: [CATEG]
    - Applicability: Mandatory
    Create an expense with an expense product having categoy [CATEG]
    Create Report, submit and approve
    
    Issue: No message is raised, the action should have been blocked by the
    mandatory applicability
    
    opw-3955683
    
    X-original-commit: 6d823ac88540214f100e28ef279fb026def8cc9a

--- addons/hr_expense/tests/test_expenses.py
+++ addons/hr_expense/tests/test_expenses.py
@@ -6,7 +6,7 @@ from freezegun import freeze_time
 
 from odoo import Command, fields
 from odoo.addons.hr_expense.tests.common import TestExpenseCommon
-from odoo.exceptions import RedirectWarning, UserError
+from odoo.exceptions import RedirectWarning, UserError, ValidationError
 from odoo.tests import tagged, Form
 from odoo.tools.misc import format_date
 
@@ -1206,3 +1206,35 @@ class TestExpenses(TestExpenseCommon):
         with Form(self.env[action_data['res_model']].with_context(action_data['context'])) as wiz_form:
             self.assertEqual(wiz_form.amount, 10)
             self.assertEqual(wiz_form.partner_bank_id, self.expense_employee.bank_account_id)
+
+    def test_expense_mandatory_analytic_plan_product_category(self):
+        """
+        Check that when an analytic plan has a mandatory applicability matching
+        product category this is correctly triggered
+        """
+        self.env['account.analytic.applicability'].create({
+            'business_domain': 'expense',
+            'analytic_plan_id': self.analytic_plan.id,
+            'applicability': 'mandatory',
+            'product_categ_id': self.product_a.categ_id.id,
+        })
+
+        expense_sheet = self.env['hr.expense.sheet'].create({
+            'name': 'Expense for John Smith',
+            'employee_id': self.expense_employee.id,
+            'accounting_date': '2021-01-01',
+            'expense_line_ids': [Command.create({
+                'name': 'Car Travel Expenses',
+                'employee_id': self.expense_employee.id,
+                'product_id': self.product_a.id,
+                'total_amount': 350.00,
+                'payment_mode': 'company_account',
+            })]
+        })
+
+        expense_sheet.action_submit_sheet()
+        with self.assertRaises(ValidationError, msg="One or more lines require a 100% analytic distribution."):
+            expense_sheet.with_context(validate_analytic=True).action_approve_expense_sheets()
+
+        expense_sheet.expense_line_ids.analytic_distribution = {self.analytic_account_1.id: 100.00}
+        expense_sheet.with_context(validate_analytic=True).action_approve_expense_sheets()
