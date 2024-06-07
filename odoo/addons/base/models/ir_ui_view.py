8648c805bcb6ed618494012dcd24c2d7e764c32b
svs-odoo <svs@odoo.com>
2024-06-07 13:20:33 +0200

    [IMP] base: consider string for icon area
    A template like that:
    ```xml
    <i class="fa fa-cogs"/>
    <label for="some_field" string="Some Text"/>
    ```
    
    Before this commit, it triggers a warning because while checking the
    icon accessibility, one of those checks is looking if the icon is
    preceded or followed by some text. To verify that, we look if the
    next/previous element is a span with text or is a <t> with writing attribute (`t-esc`,
    `t-raw`), but labels or fields with the `string` attribute aren't considered.
    
    This commit adds an additional condition for that.

--- odoo/addons/base/models/ir_ui_view.py
+++ odoo/addons/base/models/ir_ui_view.py
@@ -1919,6 +1919,8 @@ actual arch.
                 return False
             if elem.tag == 'span' and elem.text:
                 return True
+            if elem.tag in ['field', 'label'] and elem.get('string'):
+                return True
             if elem.tag == 't' and (elem.get('t-esc') or elem.get('t-raw')):
                 return True
             return False
