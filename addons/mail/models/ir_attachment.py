e9730b55b366efd5c33f77ddf5f6072602c5d705
Thibault Delavallée <tde@odoo.com>
2024-06-07 13:47:07 +0200

    [IMP] mail: make 'register_at_main_attachment' batch-enabled
    Purpose is to allow calling it on an attachment recordset that is not
    necessarily a singleton. It groups attachments per model, allowing to
    benefit from cache.
    
    It also allows to test the method, as it was not the case before.
    
    Task-3599245
    
    Part-of: odoo/odoo#161427
    Related: odoo/enterprise#60499
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/models/ir_attachment.py
+++ addons/mail/models/ir_attachment.py
@@ -53,8 +53,7 @@ class IrAttachment(models.Model):
         """ Overrides behaviour when the attachment is created through the controller
         """
         super()._post_add_create(**kwargs)
-        for attachment in self:
-            attachment.register_as_main_attachment(force=False)
+        self.register_as_main_attachment(force=False)
 
     def register_as_main_attachment(self, force=True):
         """ Registers this attachment as the main one of the model it is
@@ -63,19 +62,20 @@ class IrAttachment(models.Model):
         :param bool force: if set, the method always updates the existing main attachment
             otherwise it only sets the main attachment if there is none.
         """
-        self.ensure_one()
-        if not self.res_model or not self.res_id:
-            return
-        related_record = self.env[self.res_model].browse(self.res_id)
-        if not hasattr(related_record, '_message_set_main_attachment_id'):
+        todo = self.filtered(lambda a: a.res_model and a.res_id)
+        if not todo:
             return
 
-        try:
-            related_record._message_set_main_attachment_id(self, force=force)
-        except AccessError:
+        for model, attachments in todo.grouped("res_model").items():
+            related_records = self.env[model].browse(attachments.mapped("res_id"))
+            if not hasattr(related_records, '_message_set_main_attachment_id'):
+                return
+
             # this action is generic; if user cannot update record do not crash
             # just skip update
-            return
+            for related_record, attachment in zip(related_records, attachments):
+                with contextlib.suppress(AccessError):
+                    related_record._message_set_main_attachment_id(attachment, force=force)
 
     def _delete_and_notify(self, message=None):
         if message:
