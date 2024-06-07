9ba81fba8d46cbbd757f18eb8e32bfce2740146b
Louis Travaux <lotr@odoo.com>
2024-06-07 12:06:13 +0000

    [ADD] point_of_sale: update iotbox image
    - RPI 5 displays detection: `vcgencmd` outdated -> now using python `screeninfo` package,
    - Odoo now automatically restarts on crash: replaced `init.d/odoo` by `systemd/system/odoo.service`,
    - Now using Chromium instead of Firefox,
    - Improved server connection behaviour:
        - prevent from calling `rename_iot.sh` if new name is same as hostname,
        - cached disk read operations,
    - Updated HDMI display UI to improve coherence with IoTBox browser homepage,
    - Updated DNS configuration (use 1.1.1.1),
    - Cached `socket.gethostname()` inside of an `helper` method,
    - Added missing packages leading to warnings and errors in logs: `vobject`, `num2words`, ...,
    - Added missing `__manifest__.py` in `point_of_sale/` leading to a warning for missing licence.
    
    Task: 3947355
    X-original-commit: 16b2bb34c6ec27a76adf63b3f02732c210f91e0e

--- addons/hw_drivers/main.py
+++ addons/hw_drivers/main.py
@@ -4,7 +4,6 @@ from traceback import format_exc
 import json
 import platform
 import logging
-import socket
 from threading import Thread
 import time
 import urllib3
@@ -46,7 +45,7 @@ class Manager(Thread):
             else:
                 domain = helpers.get_ip()
             iot_box = {
-                'name': socket.gethostname(),
+                'name': helpers.get_hostname(),
                 'identifier': helpers.get_mac_address(),
                 'ip': domain,
                 'token': helpers.get_token(),
@@ -96,10 +95,11 @@ class Manager(Thread):
         """
         Thread that will load interfaces and drivers and contact the odoo server with the updates
         """
+        server_url = helpers.get_odoo_server_url()
 
         helpers.start_nginx_server()
         _logger.info("IoT Box Image version: %s", helpers.get_version(detailed_version=True))
-        if platform.system() == 'Linux' and helpers.get_odoo_server_url():
+        if platform.system() == 'Linux' and server_url:
             helpers.check_git_branch()
             helpers.generate_password()
         is_certificate_ok, certificate_details = helpers.get_certificate_status()
@@ -107,7 +107,7 @@ class Manager(Thread):
             _logger.warning("An error happened when trying to get the HTTPS certificate: %s",
                             certificate_details)
 
-        iot_client = helpers.get_odoo_server_url() and WebsocketClient(helpers.get_odoo_server_url())
+        iot_client = server_url and WebsocketClient(server_url)
         # We first add the IoT Box to the connected DB because IoT handlers cannot be downloaded if
         # the identifier of the Box is not found in the DB. So add the Box to the DB.
         self.send_alldevices(iot_client)
@@ -127,7 +127,7 @@ class Manager(Thread):
         schedule and schedule.every().day.at("00:00").do(helpers.get_certificate_status)
 
         #Setup the websocket connection
-        if helpers.get_odoo_server_url():
+        if server_url:
             iot_client.start()
         # Check every 3 secondes if the list of connected devices has changed and send the updated
         # list to the connected DB.
