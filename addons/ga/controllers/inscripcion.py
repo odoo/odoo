41775babfeff4e781bf853f6bf7dcf94da7882f3
matias <matias@matiass-iMac-Pro.local>
2024-06-09 18:45:19 -0700

    Implementacion del endpoint de las notas de las materias de un alumno

--- addons/ga/controllers/inscripcion.py
+++ addons/ga/controllers/inscripcion.py
@@ -21,11 +21,10 @@ class InscripcionController(http.Controller):
         return json.dumps(inscripciones_data)
     
     
-    
-    @http.route('/ga/alumnos/notas-materia<codigo> ',csrf=False,  type='http', auth='public', website=True, methods=['GET'])
-    def get_Alumnos(self, **post):
-        inscripciones = request.env['ga.apoderado'].sudo().search([])
-        inscripciones_data = [{
-            'nombre': inscripcion.nombre,
-        } for inscripcion in inscripciones]
-        return json.dumps(inscripciones_data)
+    # Metodo para obtener las notas de las diferentes materias de un estudiante
+    @http.route('/ga/alumnos/notas-materia/<codigo>',csrf=False,  type='http', auth='public', website=True, methods=['GET'])
+    def get_notas_materias(self,codigo):
+         # Llama al método para obtener las calificaciones por materias del modelo RegistroAcademico
+        calificaciones = request.env['ga.registro.academico'].obtener_calificaciones_por_materias(codigo)
+
+        return json.dumps(calificaciones)
