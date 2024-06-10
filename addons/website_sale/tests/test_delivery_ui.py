bf1d15abf27691cb9c34980ad1c44dba1b513b5a
Andrew Gavgavian <andg@odoo.com>
2024-06-10 21:21:41 +0000

    [FIX] website_sale: Fix traceback on delivery test setup
    Since Commit 4c58d82c7e582c11eb967b6ed7c918a6da3ebd23 refactored `test_delivery_ui`,
    the check for the `payment_custom` module was moved to individual tests, however,
    the new `setUpClass` function now uses code that requires `payment_custom`
    and fails when the server tries to run tests.
    
    Solution:
    Move the initialization of `transfer_provider` to the individual tests
    instead of within the setup function.
    
    opw-3957075
    
    X-original-commit: 7292c2983d7a8f78a32415939db1d0b7af183e35

--- addons/website_sale/tests/test_delivery_ui.py
+++ addons/website_sale/tests/test_delivery_ui.py
@@ -7,6 +7,7 @@ from odoo.fields import Command
 @odoo.tests.tagged('post_install', '-at_install')
 class TestUi(odoo.tests.HttpCase):
 
+<<<<<<< HEAD
     def test_01_free_delivery_when_exceed_threshold(self):
         if self.env['ir.module.module']._get('payment_custom').state != 'installed':
             self.skipTest("Transfer provider is not installed")
@@ -17,6 +18,20 @@ class TestUi(odoo.tests.HttpCase):
             'is_published': True,
         })
         transfer_provider._transfer_ensure_pending_msg_is_set()
+||||||| parent of b99d1a18b16e (temp)
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.transfer_provider = cls.env.ref('payment.payment_provider_transfer')
+        cls.transfer_provider.write({
+            'state': 'enabled',
+            'is_published': True,
+        })
+=======
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+>>>>>>> b99d1a18b16e (temp)
 
         # Avoid Shipping/Billing address page
         self.env.ref('base.partner_admin').write({
@@ -73,4 +88,114 @@ class TestUi(odoo.tests.HttpCase):
             ]
         })
 
+<<<<<<< HEAD
+||||||| parent of b99d1a18b16e (temp)
+    def test_01_free_delivery_when_exceed_threshold(self):
+        if self.env['ir.module.module']._get('payment_custom').state != 'installed':
+            self.skipTest("Transfer provider is not installed")
+        self.transfer_provider._transfer_ensure_pending_msg_is_set()
+        self.env['delivery.price.rule'].create([{
+            'carrier_id': self.carrier.id,
+            'max_value': 5,
+            'list_base_price': 20,
+        }, {
+            'carrier_id': self.carrier.id,
+            'operator': '>=',
+            'max_value': 5,
+            'list_base_price': 50,
+        }, {
+            'carrier_id': self.carrier.id,
+            'operator': '>=',
+            'max_value': 300,
+            'variable': 'price',
+            'list_base_price': 0,
+        }])
+
+=======
+    def test_01_free_delivery_when_exceed_threshold(self):
+        if self.env['ir.module.module']._get('payment_custom').state != 'installed':
+            self.skipTest("Transfer provider is not installed")
+
+        transfer_provider = self.env.ref('payment.payment_provider_transfer')
+        transfer_provider.write({
+            'state': 'enabled',
+            'is_published': True,
+        })
+        transfer_provider._transfer_ensure_pending_msg_is_set()
+
+        self.env['delivery.price.rule'].create([{
+            'carrier_id': self.carrier.id,
+            'max_value': 5,
+            'list_base_price': 20,
+        }, {
+            'carrier_id': self.carrier.id,
+            'operator': '>=',
+            'max_value': 5,
+            'list_base_price': 50,
+        }, {
+            'carrier_id': self.carrier.id,
+            'operator': '>=',
+            'max_value': 300,
+            'variable': 'price',
+            'list_base_price': 0,
+        }])
+
+>>>>>>> b99d1a18b16e (temp)
         self.start_tour("/", 'check_free_delivery', login="admin")
+<<<<<<< HEAD
+||||||| parent of b99d1a18b16e (temp)
+
+    def test_pay_button_disabled_when_carrier_has_error(self):
+        if self.env['ir.module.module']._get('payment_custom').state != 'installed':
+            self.skipTest("Transfer provider is not installed")
+        self.transfer_provider._transfer_ensure_pending_msg_is_set()
+        Monetary = self.env['ir.qweb.field.monetary']
+        usd_currency = self.env.ref('base.USD')
+        with patch.object(WebsiteSaleDelivery, '_get_rate',
+                          lambda controller, *args, **kwargs: {
+                              'success': False,
+                              'price': 0.0,
+                              'error_message': 'this is a test error message',
+                              'warning_message': False
+                          }), \
+             patch.object(WebsiteSaleDelivery, '_update_website_sale_delivery_return',
+                          lambda contoller, *args, **kwargs: {
+                              'status': False,
+                              'error_message': 'this is a test error message',
+                              'carrier_id': self.carrier.id,
+                              'is_free_delivery': True,
+                              'new_amount_delivery': Monetary.value_to_html(0.0, {'display_currency': usd_currency}),
+                          }):
+            self.start_tour("/", 'check_errored_delivery', login="admin")
+=======
+
+    def test_pay_button_disabled_when_carrier_has_error(self):
+        if self.env['ir.module.module']._get('payment_custom').state != 'installed':
+            self.skipTest("Transfer provider is not installed")
+
+        transfer_provider = self.env.ref('payment.payment_provider_transfer')
+        transfer_provider.write({
+            'state': 'enabled',
+            'is_published': True,
+        })
+        transfer_provider._transfer_ensure_pending_msg_is_set()
+
+        Monetary = self.env['ir.qweb.field.monetary']
+        usd_currency = self.env.ref('base.USD')
+        with patch.object(WebsiteSaleDelivery, '_get_rate',
+                          lambda controller, *args, **kwargs: {
+                              'success': False,
+                              'price': 0.0,
+                              'error_message': 'this is a test error message',
+                              'warning_message': False
+                          }), \
+             patch.object(WebsiteSaleDelivery, '_update_website_sale_delivery_return',
+                          lambda contoller, *args, **kwargs: {
+                              'status': False,
+                              'error_message': 'this is a test error message',
+                              'carrier_id': self.carrier.id,
+                              'is_free_delivery': True,
+                              'new_amount_delivery': Monetary.value_to_html(0.0, {'display_currency': usd_currency}),
+                          }):
+            self.start_tour("/", 'check_errored_delivery', login="admin")
+>>>>>>> b99d1a18b16e (temp)
