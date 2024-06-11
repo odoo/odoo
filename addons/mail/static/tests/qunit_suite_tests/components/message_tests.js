acf0cda2440cfaeb17480499f49e3c9adffa9763
Renaud Thiry <reth@odoo.com>
2024-06-11 11:13:13 +0200

    [REV] mail: allow deleting other people's attachments
    reverts e6c55e1683d679efd12e7d1cfca85b736fcbea01
    and 6ba4a50c99ae0a18436e0a1f3f094daa87953d8a
    
    We notice too many usability issues as a result of this commit.
    
    As it was stated that the security concerns were not that bad
    we let users delete any mail attachment they may find necessary.
    
    task-3983664
    
    X-original-commit: 149a2acea96cff14990b667ee611c3afbdc146f2

--- addons/mail/static/tests/qunit_suite_tests/components/message_tests.js
+++ addons/mail/static/tests/qunit_suite_tests/components/message_tests.js
@@ -9,8 +9,6 @@ import {
 } from '@mail/../tests/helpers/test_utils';
 
 import { patchWithCleanup } from '@web/../tests/helpers/utils';
-import { registerCleanup } from "@web/../tests/helpers/cleanup";
-import { session } from '@web/session';
 
 QUnit.module('mail', {}, function () {
 QUnit.module('components', {}, function () {
@@ -621,11 +619,6 @@ QUnit.test('allow attachment delete on authored message', async function (assert
 
 QUnit.test('prevent attachment delete on non-authored message in channels', async function (assert) {
     assert.expect(2);
-
-    // mock a regular user
-    const sessionAdminState = session.is_admin;
-    session.is_admin = false;
-    registerCleanup(() => session.is_admin = sessionAdminState);
 
     const pyEnv = await startServer();
     const partnerId = pyEnv['res.partner'].create({});
