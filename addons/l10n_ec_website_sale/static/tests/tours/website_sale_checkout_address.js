4279e68246b0f8069cfb4211fd585d5901da9a09
Mattis Megevand <mmeg@odoo.com>
2024-06-11 14:28:55 +0000

    [FIX] l10n_ec_website_sale: allow edition of address with VAT
    Steps to reproduce:
    - Install `l10n_ec_website_sale`
    - Go to website and select the website of the "EC Company"
    - Add a product and go to checkout
    - Save the demo address
    - Add an address
    
    Issues:
    When adding a new address VAT is not shown when logged in, however it's
    required by some localization (Ecuador for instance).
    
    opw-3921156
    
    closes odoo/odoo#175591
    
    X-original-commit: 732b4fbbf3009769708f9c9cba57609d22f2a570
    Signed-off-by: Mattis Megevand (mmeg) <mmeg@odoo.com>

---
+++ addons/l10n_ec_website_sale/static/tests/tours/website_sale_checkout_address.js
@@ -0,0 +1,26 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import tourUtils from "@website_sale/js/tours/tour_utils";
+
+registry.category("web_tour.tours").add("shop_checkout_address_ec", {
+    test: true,
+    url: "/shop",
+    steps: () => [
+        ...tourUtils.addToCart({ productName: "Test Product" }),
+        tourUtils.goToCart({ quantity: 1 }),
+        {
+            content: "Go to checkout",
+            trigger: "a:contains('Checkout')",
+            run: "click",
+        },
+        {
+            content: "Check that VAT field is present",
+            trigger: "label:contains('Identification Type')",
+        },
+        {
+            content: "Check that VAT field is present",
+            trigger: "label:contains('Identification Number')",
+        },
+    ],
+});
