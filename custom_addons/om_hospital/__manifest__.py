5d2db9d7985505f99eb68c9047a1431a7540c921
Arunkumar-60 <84034637+Arunkumar-60@users.noreply.github.com>
2024-06-11 13:56:29 +0530

    added custom addon for hospital management
    created a addon and added confid file

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'hospital',
+    'sequence': -100,
+    'summary': 'hospital managment system',
+    'description': """this is a custom hospital managment application for models""",
+    'depends': [],
+    'data': ['veiws/menu.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install':False,
+}
