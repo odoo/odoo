odoo/odoo

commit 96638ebcd20c90b3addbb9c535b791dec9c443c6

[FIX] point_of_sale: pos sale reprot total now include taxes correctly
Current behavior:
If you make a sale of a product that use a taxe not included in the
price. Then go in order analysis in PoS, the total price of the product
will not include the taxes.

Steps to reproduce:
- Create a tax of 15% that is not included in price
- Create a product with 10$ price and add this tax to it. (Total price
  including tax should be 11.5$)
- Sell it in the PoS and close the session
- Go in PoS > Reporting > Order. Open the pivot view and check the total
  price for the product
- The total is 10$ instad of 11.5$

opw-3817535

X-original-commit: 09d7b909d15471439d0c675e213b4d3e0393d5b7

addons/point_of_sale/report/pos_order_report.py
@@ -45,7 +45,7 @@ class PosOrderReport(models.Model):
                 s.date_order AS date,
                 SUM(l.qty) AS product_qty,
                 SUM(l.qty * l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS price_sub_total,
-                SUM(ROUND((l.qty * l.price_unit) * (100 - l.discount) / 100 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END, cu.decimal_places)) AS price_total,
+                SUM(ROUND((l.price_subtotal_incl) * (100 - l.discount) / 100 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END, cu.decimal_places)) AS price_total,
                 SUM((l.qty * l.price_unit) * (l.discount / 100) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS total_discount,
                 CASE
                     WHEN SUM(l.qty * u.factor) = 0 THEN NULL

addons/point_of_sale/tests/test_report_pos_order.py
@@ -13,7 +13,7 @@ class TestReportPoSOrder(TestPoSCommon):
 
     def test_report_pos_order(self):
 
-        product1 = self.create_product('Product 1', self.categ_basic, 150)
+        product1 = self.create_product('Product 1', self.categ_basic, 150, self.taxes['tax10'].id)
 
         self.open_new_session()
         session = self.pos_session
@@ -27,10 +27,10 @@ class TestReportPoSOrder(TestPoSCommon):
                 'discount': 0,
                 'qty': 1.0,
                 'price_subtotal': 150,
-                'price_subtotal_incl': 150,
+                'price_subtotal_incl': 165,
             }),],
-            'amount_total': 150.0,
-            'amount_tax': 0.0,
+            'amount_total': 165.0,
+            'amount_tax': 15.0,
             'amount_paid': 0.0,
             'amount_return': 0.0,
         })
@@ -39,3 +39,4 @@ class TestReportPoSOrder(TestPoSCommon):
         reports = self.env['report.pos.order'].sudo().search([('product_id', '=', product1.id)], order='id')
 
         self.assertEqual(reports[0].margin, 150)
+        self.assertEqual(reports[0].price_total, 165)

commit 00d0c66973b2e87e337923f350531553138a233d

[FIX] point_of_sale: incorrect totals on discount
This fixes a bug introduced in 96638eb, where lines with discount apply it twice.

addons/point_of_sale/report/pos_order_report.py
@@ -45,7 +45,7 @@ class PosOrderReport(models.Model):
                 s.date_order AS date,
                 SUM(l.qty) AS product_qty,
                 SUM(l.qty * l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS price_sub_total,
-                SUM(ROUND((l.price_subtotal_incl) * (100 - l.discount) / 100 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END, cu.decimal_places)) AS price_total,
+                SUM(ROUND((l.price_subtotal_incl) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END, cu.decimal_places)) AS price_total,
                 SUM((l.qty * l.price_unit) * (l.discount / 100) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS total_discount,
                 CASE
                     WHEN SUM(l.qty * u.factor) = 0 THEN NULL

addons/point_of_sale/tests/test_report_pos_order.py
@@ -11,7 +11,38 @@ class TestReportPoSOrder(TestPoSCommon):
         super(TestReportPoSOrder, self).setUp()
         self.config = self.basic_config
 
-    def test_report_pos_order(self):
+    def test_report_pos_order_0(self):
+        """Test the margin and price_total of a PoS Order with no taxes."""
+        product1 = self.create_product('Product 1', self.categ_basic, 150)
+
+        self.open_new_session()
+        session = self.pos_session
+        self.env['pos.order'].create({
+            'session_id': session.id,
+            'lines': [
+                (0, 0, {
+                    'name': "OL/0001",
+                    'product_id': product1.id,
+                    'price_unit': 150,
+                    'discount': 0,
+                    'qty': 1.0,
+                    'price_subtotal': 150,
+                    'price_subtotal_incl': 150,
+                })
+            ],
+            'amount_total': 150.0,
+            'amount_tax': 0.0,
+            'amount_paid': 0.0,
+            'amount_return': 0.0,
+        })
+        # PoS Orders have negative IDs to avoid conflict, so reports[0] will correspond to the newest order
+        reports = self.env['report.pos.order'].sudo().search([('product_id', '=', product1.id)], order='id')
+
+        self.assertEqual(reports[0].margin, 150)
+        self.assertEqual(reports[0].price_total, 150)
+
+    def test_report_pos_order_1(self):
+        """Test the margin and price_total of a PoS Order with taxes."""
 
         product1 = self.create_product('Product 1', self.categ_basic, 150, self.taxes['tax10'].id)
 
@@ -40,3 +71,36 @@ class TestReportPoSOrder(TestPoSCommon):
 
         self.assertEqual(reports[0].margin, 150)
         self.assertEqual(reports[0].price_total, 165)
+
+    def test_report_pos_order_2(self):
+        """Test the margin and price_total of a PoS Order with discount and no taxes"""
+
+        product1 = self.create_product('Product 1', self.categ_basic, 150)
+
+        self.open_new_session()
+        session = self.pos_session
+
+        self.env['pos.order'].create({
+            'session_id': session.id,
+            'lines': [
+                (0, 0, {
+                    'name': "OL/0001",
+                    'product_id': product1.id,
+                    'price_unit': 150,
+                    'discount': 10,
+                    'qty': 1.0,
+                    'price_subtotal': 135,
+                    'price_subtotal_incl': 135,
+                })
+            ],
+            'amount_total': 135.0,
+            'amount_tax': 0.0,
+            'amount_paid': 0.0,
+            'amount_return': 0.0,
+        })
+
+        # PoS Orders have negative IDs to avoid conflict, so reports[0] will correspond to the newest order
+        reports = self.env['report.pos.order'].sudo().search([('product_id', '=', product1.id)], order='id')
+
+        self.assertEqual(reports[0].margin, 135)
+        self.assertEqual(reports[0].price_total, 135)