cb2ad762c56dda8709e5095ef066bae2dbe9afed
muab-odoo <muab@odoo.com>
2024-06-11 18:22:04 +0300

    [IMP] l10n_cz: Fill accounting date with 'taxable_supply_date'
    In CZ, they need to use the taxable supply date as accounting date. currently, That is not implemented!
    
    solution: Set default value for 'taxable_supply_date' as the invoice date and allow the user to modify it.
    Also, fill the accounting date but using the taxable supply date as reference in place of invoice date.
    
    task-id#3983749

--- addons/l10n_cz/models/account_move.py
+++ addons/l10n_cz/models/account_move.py
@@ -1,8 +1,15 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    taxable_supply_date = fields.Date()
+    taxable_supply_date = fields.Date(default=fields.Date.today())
+
+    @api.depends('taxable_supply_date')
+    def _compute_date(self):
+        super()._compute_date()
+        for move in self:
+            if move.taxable_supply_date:
+                move.date = move.taxable_supply_date
---
+++ addons/l10n_cz/models/account_move_line.py
@@ -0,0 +1,22 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import models, fields, api
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.depends('currency_id', 'company_id', 'move_id.taxable_supply_date')
+    def _compute_currency_rate(self):
+        if self.company_id.country_code == "CZ":
+            for line in self:
+                if line.currency_id:
+                    line.currency_rate = self.env['res.currency']._get_conversion_rate(
+                        from_currency=line.company_currency_id,
+                        to_currency=line.currency_id,
+                        company=line.company_id,
+                        date=line.move_id.taxable_supply_date or line.move_id.invoice_date or fields.Date.context_today(line),
+                    )
+                else:
+                    line.currency_rate = 1
+        else:
+            super()._compute_currency_rate()
