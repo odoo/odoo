53f27e61e13cccfe58f8274ba6563753c6b732fa
Renaud Thiry <reth@odoo.com>
2024-06-11 11:13:13 +0200

    [REV] mail: allow deleting other people's attachments
    reverts e6c55e1683d679efd12e7d1cfca85b736fcbea01
    and 6ba4a50c99ae0a18436e0a1f3f094daa87953d8a
    
    We notice too many usability issues as a result of this commit.
    
    As it was stated that the security concerns were not that bad
    we let users delete any mail attachment they may find necessary.
    
    task-3983664
    
    closes odoo/odoo#169891
    
    X-original-commit: 1281752020d60de1ebada89e3c6af73b6d8c80da
    Related: odoo/enterprise#64937
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>
    Signed-off-by: Renaud Thiry (reth) <reth@odoo.com>

--- addons/test_mail/tests/__init__.py
+++ addons/test_mail/tests/__init__.py
@@ -2,7 +2,6 @@
 
 from . import test_invite
 from . import test_ir_actions
-from . import test_ir_attachment
 from . import test_mail_activity
 from . import test_mail_composer
 from . import test_mail_composer_mixin
