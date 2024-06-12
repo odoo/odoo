da97a1c732d958bc469ad305cc8b813da0ab0ee1
Samuel Degueldre <sad@odoo.com>
2024-06-12 09:21:09 +0200

    [FIX] sale_stock: correctly handle multiple warehouses on the same SO
    Previously, when creating an SO and setting different routes on
    different SO lines that pull from different warehouses, any route that
    pulled from a warehouse different from the one on the SO would be
    ignored. While you could work around the issue by creating a virtual
    warehouse that was a parent of both, it would still have its own issues
    as it would create only one picking for both warehouses which is not
    very practical.
    
    This commit fixes this issue by making the warehouse_id on SO lines a
    compute field that will give priority to the route set on the line when
    there is one. This simple change causes the rest to correctly identify
    that there are 2 warehouses and that a picking should be created for
    each warehouse.
    
    task-3980608
    
    closes odoo/odoo#169036
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/sale_stock/tests/__init__.py
+++ addons/sale_stock/tests/__init__.py
@@ -9,6 +9,7 @@ from . import test_sale_stock
 from . import test_sale_stock_lead_time
 from . import test_sale_stock_report
 from . import test_sale_order_dates
+from . import test_sale_stock_multi_warehouse
 from . import test_sale_stock_multicompany
 from . import test_sale_stock_accrued_entries
 from . import test_sale_stock_access_rights
