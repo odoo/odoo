90f8954eba6d412b257de330dc4206c9dc68ced3
Antoine (ande) <ande@odoo.com>
2024-06-12 15:10:58 +0000

    [FIX] phone_validation: phone numbers in brazil
    Current behaviour:
    Brazilian phone numbers are not managed correctly
    following the 2016 changes in Brazil.
    (Adding a 9 to mobile phone numbers)
    
    Cause of the issue:
    Phonenumbers metadata were patched to add 9 in mobile numbers,
    however, when E164 is selected, phone numbers aren't formatted,
    thus patched metadata not being applied.
    See format_number in phonenumbers "Early exit for E164 case"
    
    Fix:
    Backport of: https://github.com/odoo/odoo/commit/53885e41867653ad45ca3aef55886c280240b76a
    In phone_validation, before formatting, phone_parse is called,
    https://github.com/odoo/odoo/blob/bdfb802293d9a3eebb378a15ed65f48b7c3182b2/addons/phone_validation/tools/phone_validation.py#L72
    old brazilian mobile numbers would be invalid, and raise an error.
    https://github.com/odoo/odoo/blob/bdfb802293d9a3eebb378a15ed65f48b7c3182b2/addons/phone_validation/tools/phone_validation.py#L51
    Now if the number is invalid, we force format in international,
    to force apply patched metadata, before re-trying to parse.
    
    opw-3861847
    
    closes odoo/odoo#170757
    
    X-original-commit: 29a4de8e29a2e330b8e5993c9fde23c69a2eff5e
    Related: odoo/enterprise#65449
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/phone_validation/tests/test_phonenumbers.py
+++ addons/phone_validation/tests/test_phonenumbers.py
@@ -44,3 +44,19 @@ class TestPhonenumbers(BaseCase):
                         'phone_code': exp_phone_code,
                     }
                 )
+
+    def test_phone_format_e164_brazil(self):
+        """ In the new brazilian phone numbers system, phone numbers add a '9'
+            in front of the last 8 digits of mobile numbers.
+            Phonenumbers metadata is patched in odoo, however, when E164 is selected,
+            phone numbers aren't formatted, thus patched metadata not being applied.
+            See format_number in phonenumbers "Early exit for E164 case"
+        """
+        for number, expected_number in [
+            ('11 6123 4560', '+5511961234560'),  # mobile number, must have 9 added
+            ('+55 11 6123 4561', '+5511961234561'),  # mobile number, must have 9 added
+            ('11 2345 6789', '+551123456789'),  # landline, must NOT have 9 added
+            ('+55 11 2345 6798', '+551123456798'),  # landline, must NOT have 9 added
+        ]:
+            res = phone_validation.phone_format(number, 'BR', '55', force_format='E164')
+            self.assertEqual(res, expected_number)
