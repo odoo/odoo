80bb450acac6950f460851f25a0cb941cb1eb084
clesgow <quwo@odoo.com>
2024-06-12 11:01:52 +0200

    [FIX] stock: only display one lot field for move lines
    If you select both 'Create New' and 'Use Existing One' for handling
    lots/serial numbers in an incoming picking type, you'll end up with two
    fields called 'Lots/Serial numbers' in the move's details.
    
    When that happens, we only want to display the `lot_name` now, as it
    works with the Generate/Import serial features and the existing lots
    using that lot name will be matched at the validation, making it using
    the existing lots as well.
    
    task-3853055
    
    closes odoo/odoo#179616
    
    Related: odoo/enterprise#69715
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -595,14 +595,15 @@ Please change the quantity done or the rounding precision of your unit of measur
         for move in self:
             move.show_quant = move.picking_code != 'incoming'\
                            and move.product_id.is_storable
-            move.show_lots_m2o = not move.show_quant\
-                and move.has_tracking != 'none'\
-                and (move.picking_type_id.use_existing_lots or move.state == 'done' or move.origin_returned_move_id.id)
             move.show_lots_text = move.has_tracking != 'none'\
                 and move.picking_type_id.use_create_lots\
                 and not move.picking_type_id.use_existing_lots\
                 and move.state != 'done' \
                 and not move.origin_returned_move_id.id
+            move.show_lots_m2o = not move.show_quant\
+                and not move.show_lots_text\
+                and move.has_tracking != 'none'\
+                and (move.picking_type_id.use_existing_lots or move.state == 'done' or move.origin_returned_move_id.id)
 
     @api.constrains('product_uom')
     def _check_uom(self):
