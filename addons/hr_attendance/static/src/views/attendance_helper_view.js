7e47ec4f01f5e11772acf035d148017b9fcb3ba1
Mélanie <mepe@odoo.com>
2024-06-12 16:16:46 +0200

    [FIX] hr_attendance : fix onboarding screen
    STEP TO REPRODUCE:
    ==================
    
        ISSUE 1:
        ========
            0- Create a database without demodata
            1- Go on Attendance Application
            2- Click on Load demo data
            3- Write in "gegegege" in search bar for employee
            4 - Click again on Load demo data
        An error will be raised. To fix that if the demo data have been already launched this button will be hide.
    
        ISSUE 2:
        ========
        To have this issue, today's month need to have less than 31 days.
            0- Create a database without demodata
            1- Go on Attendance Application
            2- Click on Load demo data
        An error about month's number will be raised.
    
    task-3983778
    
    X-original-commit: 294831c56fcc3bb81ac63e21cf695c68ff11187b
    Part-of: odoo/odoo#172094
    Related: odoo/enterprise#66137
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ addons/hr_attendance/static/src/views/attendance_helper_view.js
@@ -0,0 +1,26 @@
+import { useService } from "@web/core/utils/hooks";
+
+import { Component, onWillStart, useState } from "@odoo/owl";
+
+export class AttendanceActionHelper extends Component {
+    static template = "hr_attendance.AttendanceActionHelper";
+    static props = ["noContentHelp"];
+    setup() {
+        this.orm = useService("orm");
+        this.actionService = useService("action");
+        this.state = useState({
+            hasDemoData: false,
+        });
+        onWillStart(async () => {
+            this.state.hasDemoData = await this.orm.call("hr.attendance", "hasDemoData", []);
+        });
+    }
+
+    loadAttendanceScenario() {
+        this.actionService.doAction("hr_attendance.action_load_demo_data");
+    }
+
+    LoadTryKiosk() {
+        this.actionService.doAction("hr_attendance.action_try_kiosk");
+    }
+};
