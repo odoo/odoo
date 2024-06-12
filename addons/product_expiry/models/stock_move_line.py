odoo/odoo

commit f1d4a727a84672d974151f681e43768abd3ac7cc

[FIX] product_expiry: should be based on scheduled
Current behavior:
Expiry dates are computed from today's date and
expiration time, it should be based on scheduled date.

Steps to reproduce:
1. Go to Inventory
2. Go to Configuration > Operations Types
3. Click on Receipts
4. Check "Show Detailed Operations" box
5. Check "Pre-fill Detailed Operations" box
6. Go back to Inventory > Products
7. Create a new product
8. Go to the Inventory tab
9. At Tracking, put "By lots"
10. Check "Expiration date" Box
11. At "Expiration Date" input 10
12. Go back to Inventory > Receipts
13. Make a new receipt
14. At "Scheduled Date" input a new date
15. At "Add a line" add the product
16. Expiration date is today + 10
17. It should be new date + 10

Fix:
Replaced today with scheduled_date

opw-3813132

closes odoo/odoo#167834

X-original-commit: cd5aab60f0ffa5a68dd7a91b3d0742c5be3fb2e0
Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
Signed-off-by: Antoine Demany (ande) <ande@odoo.com>

addons/product_expiry/models/stock_move_line.py
@@ -28,7 +28,7 @@ class StockMoveLine(models.Model):
             create_column(self._cr, "stock_move_line", "expiration_date", "timestamp")
         return super()._auto_init()
 
-    @api.depends('product_id', 'picking_type_use_create_lots', 'lot_id.expiration_date')
+    @api.depends('product_id', 'picking_type_use_create_lots', 'lot_id.expiration_date', 'picking_id.scheduled_date')
     def _compute_expiration_date(self):
         for move_line in self:
             if not move_line.expiration_date and move_line.lot_id.expiration_date:
@@ -36,7 +36,7 @@ class StockMoveLine(models.Model):
             elif move_line.picking_type_use_create_lots:
                 if move_line.product_id.use_expiration_date:
                     if not move_line.expiration_date:
-                        move_line.expiration_date = fields.Datetime.today() + datetime.timedelta(days=move_line.product_id.expiration_time)
+                        move_line.expiration_date = move_line.picking_id.scheduled_date + datetime.timedelta(days=move_line.product_id.expiration_time)
                 else:
                     move_line.expiration_date = False
 
@@ -49,12 +49,12 @@ class StockMoveLine(models.Model):
         else:
             self.expiration_date = False
 
-    @api.onchange('product_id', 'product_uom_id')
+    @api.onchange('product_id', 'product_uom_id', 'picking_id')
     def _onchange_product_id(self):
         res = super()._onchange_product_id()
         if self.picking_type_use_create_lots:
             if self.product_id.use_expiration_date:
-                self.expiration_date = fields.Datetime.today() + datetime.timedelta(days=self.product_id.expiration_time)
+                self.expiration_date = self.picking_id.scheduled_date + datetime.timedelta(days=self.product_id.expiration_time)
             else:
                 self.expiration_date = False
         return res

addons/product_expiry/tests/test_stock_lot.py
@@ -651,3 +651,26 @@ class TestStockLot(TestStockCommon):
 
         picking_out.action_assign()
         self.assertEqual(picking_out.move_line_ids.lot_id, apple_lot)
+
+    def test_compute_expiration_date_from_scheduled_date(self):
+        partner = self.env['res.partner'].create({
+            'name': 'Apple\'s Joe',
+            'company_id': self.env.ref('base.main_company').id,
+        })
+
+        delta = timedelta(seconds=10)
+        new_date = datetime.today() + timedelta(days=42)
+        expiration_date = new_date + timedelta(days=self.apple_product.expiration_time)
+
+        picking_form = Form(self.env['stock.picking'])
+        picking_form.partner_id = partner
+        picking_form.scheduled_date = new_date
+        picking_form.picking_type_id = self.env.ref('stock.picking_type_in')
+
+        with picking_form.move_ids_without_package.new() as move:
+            move.product_id = self.apple_product
+            move.product_uom_qty = 4
+        delivery = picking_form.save()
+        delivery.action_confirm()
+
+        self.assertAlmostEqual(delivery.move_line_ids[0].expiration_date, expiration_date, delta=delta)

commit 88cc03178feb8c495e7e274b06da9dcef46682f3

[FIX] product_expiry: scheduled_date can be false
Current behaviour:
When scheduled_date is not set, we get a traceback

Steps to reproduce:
1. Create Product FNS storable
2. Create Product CMP storable
3. Create quant for 10 units of CMP
4. Set CMP tracking to 'Lot' and 'Use expiration date' to True
5. Create MO for 1 unit of FNS using 1 unit of CMP
6. Try to Confirm: Traceback

Cause of the issue:
Caused by: https://github.com/odoo/odoo/commit/f1d4a727a84672d974151f681e43768abd3ac7cc

opw-3981689

addons/product_expiry/models/stock_move_line.py
@@ -36,7 +36,8 @@ class StockMoveLine(models.Model):
             elif move_line.picking_type_use_create_lots:
                 if move_line.product_id.use_expiration_date:
                     if not move_line.expiration_date:
-                        move_line.expiration_date = move_line.picking_id.scheduled_date + datetime.timedelta(days=move_line.product_id.expiration_time)
+                        from_date = move_line.picking_id.scheduled_date or fields.Datetime.today()
+                        move_line.expiration_date = from_date + datetime.timedelta(days=move_line.product_id.expiration_time)
                 else:
                     move_line.expiration_date = False
 
@@ -54,7 +55,8 @@ class StockMoveLine(models.Model):
         res = super()._onchange_product_id()
         if self.picking_type_use_create_lots:
             if self.product_id.use_expiration_date:
-                self.expiration_date = self.picking_id.scheduled_date + datetime.timedelta(days=self.product_id.expiration_time)
+                from_date = self.picking_id.scheduled_date or fields.Datetime.today()
+                self.expiration_date = from_date + datetime.timedelta(days=self.product_id.expiration_time)
             else:
                 self.expiration_date = False
         return res