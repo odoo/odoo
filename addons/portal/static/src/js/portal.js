1be53d35e349985a9b0df9772138b0bf3f9ded89
Jeremy Kersten <jke@odoo.com>
2024-06-12 13:00:51 +0000

    [IMP] portal: /my/counters performances
    The counters are primarily used as boolean to determine whether or not
    to display the card. Therefore, as soon as you have one record, we can
    show the card without needing to recheck it later. In the worst-case
    scenario, you might see a card pointing to an empty list view.
    This approach helps us avoid recalculating whether there is a record for
    this card on each refresh.
    
    The approach is to cache the counter into the session as a boolean.
    This way, if there are no invoices but a sale order is validated, the
    invoice counter will be recomputed, and you will see the invoice card
    the next time. For the duration of the same session, we won't recompute
    it and will always display the invoice card.
    We only re-request the computation if the counter in the session was
    Falsy or if we need to show the counter, as in this case, we want to
    display the most precise number.
    
    This approach will also avoid flickering when the view is updated.
    Since we already know the count, we can display the card immediately
    without waiting for the RPC callback. To further reduce flickering, we
    have moved the spinner to the bottom, preventing the content from
    shifting upwards once the loader disappears.
    
    On another note, we've updated the policy for the parallel RPC requests
    made to /my/counter. Instead of splitting the counter into 3 RPC calls,
    we now allow 5 counters per RPC, with a maximum of three RPCs.
    
    An update of the view portal.portal_docs_entry is required to benefit
    from this cache for /my/counter, avoiding the call to /my/counters if we
    know the counter.
    
    closes odoo/odoo#169783
    
    X-original-commit: 1f41e7711b6bee6cd51786e7dabf5be1015f9a36
    Signed-off-by: Olivier Dony (odo) <odo@odoo.com>
    Signed-off-by: Jérémy Kersten <jke@odoo.com>

--- addons/portal/static/src/js/portal.js
+++ addons/portal/static/src/js/portal.js
@@ -80,10 +80,10 @@ export const PortalHomeCounters = publicWidget.Widget.extend({
      * @private
      */
     async _updateCounters(elem) {
-        const numberRpc = 3;
         const needed = Object.values(this.el.querySelectorAll('[data-placeholder_count]'))
                                 .map(documentsCounterEl => documentsCounterEl.dataset['placeholder_count']);
-        const counterByRpc = Math.ceil(needed.length / numberRpc);  // max counter, last can be less
+        const numberRpc = Math.min(Math.ceil(needed.length / 5), 3); // max 3 rpc, up to 5 counters by rpc ideally
+        const counterByRpc = Math.ceil(needed.length / numberRpc);
         const countersAlwaysDisplayed = this._getCountersAlwaysDisplayed();
 
         const proms = [...Array(Math.min(numberRpc, needed.length)).keys()].map(async i => {
