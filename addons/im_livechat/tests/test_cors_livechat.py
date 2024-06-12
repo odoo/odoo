0321f761f4a75b396d8ebc498c97fa8bcd2b8bd8
Sébastien Theys <seb@odoo.com>
2024-06-12 11:43:49 +0200

    [REF] im_livechat: move guest_token of get_session to store
    This should not be part of channel info.
    
    Part of task-3605717
    
    Part-of: odoo/odoo#169484
    Related: odoo/enterprise#64700
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/tests/test_cors_livechat.py
+++ addons/im_livechat/tests/test_cors_livechat.py
@@ -26,22 +26,22 @@ class TestCorsLivechat(HttpCase):
 
     def test_ignore_user_cookie(self):
         self.authenticate("admin", "admin")
-        channel_info = self.make_jsonrpc_request(
+        data = self.make_jsonrpc_request(
             "/im_livechat/cors/get_session",
             {
                 "anonymous_name": "Visitor",
                 "channel_id": self.livechat_channel.id,
                 "persisted": True,
             },
-        )["Thread"]
-        channel = self.env["discuss.channel"].browse(channel_info["id"])
+        )
+        channel = self.env["discuss.channel"].browse(data["Thread"][0]["id"])
         self.assertEqual(channel.channel_member_ids[0].partner_id, self.operator.partner_id)
         self.assertFalse(channel.channel_member_ids[1].partner_id)
         self.assertTrue(channel.channel_member_ids[1].guest_id)
 
     def test_ignore_guest_cookie(self):
         guest = self.env["mail.guest"].create({"name": "Visitor"})
-        channel_info = self.make_jsonrpc_request(
+        data = self.make_jsonrpc_request(
             "/im_livechat/cors/get_session",
             {
                 "anonymous_name": "Visitor",
@@ -49,38 +49,38 @@ class TestCorsLivechat(HttpCase):
                 "persisted": True,
             },
             headers={"Cookie": f"{guest._cookie_name}={guest.id}{guest._cookie_separator}{guest.access_token};"},
-        )["Thread"]
-        channel = self.env["discuss.channel"].browse(channel_info["id"])
+        )
+        channel = self.env["discuss.channel"].browse(data["Thread"][0]["id"])
         channel_guest = channel.channel_member_ids.filtered(lambda member: member.guest_id).guest_id
         self.assertNotEqual(channel_guest, guest)
 
     def test_access_routes_with_valid_guest_token(self):
-        channel_info = self.make_jsonrpc_request(
+        data = self.make_jsonrpc_request(
             "/im_livechat/cors/get_session",
             {
                 "anonymous_name": "Visitor",
                 "channel_id": self.livechat_channel.id,
                 "persisted": True,
             },
-        )["Thread"]
+        )
         self.authenticate(None, None)
         self.make_jsonrpc_request(
             "/im_livechat/cors/channel/messages",
             {
-                "guest_token": channel_info["guest_token"],
-                "channel_id": channel_info["id"],
+                "guest_token": data["Store"]["guest_token"],
+                "channel_id": data["Thread"][0]["id"],
             },
         )
 
     def test_access_denied_for_wrong_channel(self):
-        channel_info = self.make_jsonrpc_request(
+        data = self.make_jsonrpc_request(
             "/im_livechat/cors/get_session",
             {
                 "anonymous_name": "Visitor",
                 "channel_id": self.livechat_channel.id,
                 "persisted": True,
             },
-        )["Thread"]
+        )
         guest = self.env["mail.guest"].create({"name": "Visitor"})
         self.authenticate(None, None)
         with self.assertRaises(JsonRpcException, msg="werkzeug.exceptions.NotFound"):
@@ -88,6 +88,6 @@ class TestCorsLivechat(HttpCase):
                 "/im_livechat/cors/channel/messages",
                 {
                     "guest_token": guest.access_token,
-                    "channel_id": channel_info["id"],
+                    "channel_id": data["Thread"][0]["id"],
                 },
             )
