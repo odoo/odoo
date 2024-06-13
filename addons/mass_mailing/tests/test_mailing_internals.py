5c54e8914c8e897155bdcde06e7693cd8fc1e622
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-06-13 08:18:14 +0000

    [FIX] mail, mass_mailing: don't load duplicate attachments
    Issue:
    ======
    Duplicate attachmenets are loaded in chatter.
    
    Steps to reproduce the issue:
    =============================
    - Go to massmailing, create a new email with a template with images.
    - Replace the image by uploading a new one, save.
    - The second and onward uploaded images have two attachments in the
      tab->chat->attachment.
    
    Origin of the issue:
    ====================
    In web_editor, optimized versions of the images are created. In the
    chatter we load all attachments, original and optimized ones which looks
    like we have duplicates.
    
    For .svg images, we create duplicate .png versions in python side.
    
    Spec for Images created from mass_mailing:
    ===========================================
    - For SVG images we show only the png version.
    - For non SVG images we show the original version.
    
    Solution:
    =========
    - For SVG images: If the image doesn't have .png version which means
    added directly in the chatter/attach we show it otherwise we show the
    .png version.
    
    - For non SVG images: We always show the image which doesn't have
      original_id or their original_id image isn't in the list of
      attachment.
    
    task-3639914
    opw-3589477
    
    closes odoo/odoo#180137
    
    X-original-commit: c4d971515c22face16289e7cf6ff9d41d26823b9
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Mahdi Cheikh Rouhou (macr) <macr@odoo.com>

--- addons/mass_mailing/tests/test_mailing_internals.py
+++ addons/mass_mailing/tests/test_mailing_internals.py
@@ -123,10 +123,12 @@ class TestMassMailValues(MassMailCommon):
                                 <div style="color: red; background-image:url(data:image/jpg;base64,{BASE_64_STRING}15); display: block;"/>
                                 <div style="color: red; background-image: url(data:image/jpg;base64,{BASE_64_STRING}16); background: url('data:image/jpg;base64,{BASE_64_STRING}17'); display: block;"/>
                             <![endif]-->
+                            <img src="data:image/png;base64,{BASE_64_STRING}0">
                         </body></html>
                     """,
                 })
-        self.assertEqual(len(attachments), 18)
+        self.assertEqual(len(attachments), 19)
+        self.assertEqual(attachments[0]['id'], attachments[18]['id'])
         self.assertEqual(str(mailing.body_html), f"""
                         <html><body>
                             <img src="/web/image/{attachments[0]['id']}?access_token={attachments[0]['token']}">
@@ -149,6 +151,7 @@ class TestMassMailValues(MassMailCommon):
                                 <div style="color: red; background-image:url(/web/image/{attachments[15]['id']}?access_token={attachments[15]['token']}); display: block;"/>
                                 <div style="color: red; background-image: url(/web/image/{attachments[16]['id']}?access_token={attachments[16]['token']}); background: url('/web/image/{attachments[17]['id']}?access_token={attachments[17]['token']}'); display: block;"/>
                             <![endif]-->
+                            <img src="/web/image/{attachments[18]['id']}?access_token={attachments[18]['token']}">
                         </body></html>
         """.strip())
 
@@ -420,6 +423,61 @@ class TestMassMailValues(MassMailCommon):
         with self.assertRaises(IntegrityError):
             activity.write({'res_id': 0})
             self.env.flush_all()
+
+    def test_mailing_editor_created_attachments(self):
+        mailing = self.env['mailing.mailing'].create({
+            'name': 'TestMailing',
+            'subject': 'Test',
+            'mailing_type': 'mail',
+            'body_html': '<p>Hello</p>',
+            'mailing_model_id': self.env['ir.model']._get('res.partner').id,
+        })
+        blob_b64 = base64.b64encode(b'blob1')
+
+        # Created when uploading an image
+        original_svg_attachment = self.env['ir.attachment'].create({
+            "name": "test SVG",
+            "mimetype": "image/svg+xml",
+            "datas": blob_b64,
+            "public": True,
+            "res_model": "mailing.mailing",
+            "res_id": mailing.id,
+        })
+
+        # Created when saving the mass_mailing
+        png_duplicate_of_svg_attachment = self.env['ir.attachment'].create({
+            "name": "test SVG duplicate",
+            "mimetype": "image/png",
+            "datas": blob_b64,
+            "public": True,
+            "res_model": "mailing.mailing",
+            "res_id": mailing.id,
+            "original_id": original_svg_attachment.id
+        })
+
+        # Created by uploading new image
+        original_png_attachment = self.env['ir.attachment'].create({
+            "name": "test PNG",
+            "mimetype": "image/png",
+            "datas": blob_b64,
+            "public": True,
+            "res_model": "mailing.mailing",
+            "res_id": mailing.id,
+        })
+
+        # Created by modify_image in editor controller
+        self.env['ir.attachment'].create({
+            "name": "test PNG duplicate",
+            "mimetype": "image/png",
+            "datas": blob_b64,
+            "public": True,
+            "res_model": "mailing.mailing",
+            "res_id": mailing.id,
+            "original_id": original_png_attachment.id
+        })
+
+        mail_thread_attachments = mailing._get_mail_thread_data_attachments()
+        self.assertSetEqual(set(mail_thread_attachments.ids), {png_duplicate_of_svg_attachment.id, original_png_attachment.id})
 
 
 @tagged("mass_mailing", "utm")
