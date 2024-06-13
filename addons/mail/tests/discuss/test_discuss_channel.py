e4a13b561c66a2dea6bb4dd6a8c112542a72e6b4
Sébastien Theys <seb@odoo.com>
2024-06-13 16:50:33 +0200

    [REF] mail: make test_channel_info_get store data ready
    Part of task-3605717

--- addons/mail/tests/discuss/test_discuss_channel.py
+++ addons/mail/tests/discuss/test_discuss_channel.py
@@ -8,6 +8,7 @@ from odoo import Command, fields
 from odoo.addons.mail.models.discuss.discuss_channel import channel_avatar, group_avatar
 from odoo.addons.mail.tests.common import mail_new_test_user
 from odoo.addons.mail.tests.common import MailCommon
+from odoo.addons.mail.tools.discuss import StoreData
 from odoo.exceptions import ValidationError
 from odoo.tests import HttpCase, tagged, users
 from odoo.tools import html_escape, mute_logger
@@ -133,27 +134,45 @@ class TestChannelInternals(MailCommon, HttpCase):
     @users('employee_nomail')
     def test_channel_info_get(self):
         # `channel_get` should return a new channel the first time a partner is given
-        initial_channel_info = self.env['discuss.channel'].channel_get(partners_to=self.test_partner.ids)._channel_info()[0]
+        channel = self.env["discuss.channel"].channel_get(partners_to=self.test_partner.ids)
+        init_store = StoreData()
+        init_store.add({"Thread": channel._channel_info()})
+        initial_channel_info = init_store.get_result()["Thread"][0]
         # shape of channelMembers is [('ADD', data...)], [0][1] accesses the data
         self.assertEqual({m['persona']['id'] for m in initial_channel_info['channelMembers'][0][1]}, {self.partner_employee_nomail.id, self.test_partner.id})
 
         # `channel_get` should return the existing channel every time the same partner is given
-        same_channel_info = self.env['discuss.channel'].channel_get(partners_to=self.test_partner.ids)._channel_info()[0]
+        same_channel = self.env['discuss.channel'].channel_get(partners_to=self.test_partner.ids)
+        same_channel_store = StoreData()
+        same_channel_store.add({"Thread": same_channel._channel_info()})
+        same_channel_info = same_channel_store.get_result()["Thread"][0]
         self.assertEqual(same_channel_info['id'], initial_channel_info['id'])
 
         # `channel_get` should return the existing channel when the current partner is given together with the other partner
-        together_channel_info = self.env['discuss.channel'].channel_get(partners_to=(self.partner_employee_nomail + self.test_partner).ids)._channel_info()[0]
+        together_pids = (self.partner_employee_nomail + self.test_partner).ids
+        together_channel = self.env['discuss.channel'].channel_get(partners_to=together_pids)
+        together_channel_store = StoreData()
+        together_channel_store.add({"Thread": together_channel._channel_info()})
+        together_channel_info = together_channel_store.get_result()["Thread"][0]
         self.assertEqual(together_channel_info['id'], initial_channel_info['id'])
 
         # `channel_get` should return a new channel the first time just the current partner is given,
         # even if a channel containing the current partner together with other partners already exists
-        solo_channel_info = self.env['discuss.channel'].channel_get(partners_to=self.partner_employee_nomail.ids)._channel_info()[0]
+        solo_pids = self.partner_employee_nomail.ids
+        solo_channel = self.env['discuss.channel'].channel_get(partners_to=solo_pids)
+        solo_channel_store = StoreData()
+        solo_channel_store.add({"Thread": solo_channel._channel_info()})
+        solo_channel_info = solo_channel_store.get_result()["Thread"][0]
         self.assertNotEqual(solo_channel_info['id'], initial_channel_info['id'])
         # shape of channelMembers is [('ADD', data...)], [0][1] accesses the data
         self.assertEqual({m['persona']['id'] for m in solo_channel_info['channelMembers'][0][1]}, {self.partner_employee_nomail.id})
 
         # `channel_get` should return the existing channel every time the current partner is given
-        same_solo_channel_info = self.env['discuss.channel'].channel_get(partners_to=self.partner_employee_nomail.ids)._channel_info()[0]
+        same_solo_pids = self.partner_employee_nomail.ids
+        same_solo_channel = self.env['discuss.channel'].channel_get(partners_to=same_solo_pids)
+        same_solo_channel_store = StoreData()
+        same_solo_channel_store.add({"Thread": same_solo_channel._channel_info()})
+        same_solo_channel_info = same_solo_channel_store.get_result()["Thread"][0]
         self.assertEqual(same_solo_channel_info['id'], solo_channel_info['id'])
 
     # `channel_get` will pin the channel by default and thus last interest will be updated.
