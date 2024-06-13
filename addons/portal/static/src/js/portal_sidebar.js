566ecbba39d4a48391f2b740ebb67d14c45f1497
Abdessamad Zouiten (abzo) <abzo@odoo.com>
2024-06-13 14:18:14 +0200

    [FIX] portal: format number of due in days
    To reproduce the bug:
    - Go to Accounting app -> Customers -> Invoices
    - Click on an invoice or create a new one
    - Click on the Preview button
    - Chcek due in days. It has number after the decimal point.
    
    This was caused to a miss understanding of the toFixed function. The parameter of the function represent the numbers needed after the digit. And by default its value is 0.
    
    opw-3957112
    
    closes odoo/odoo#172320
    
    X-original-commit: 1f1223733750abce5d7aed146beb820efcd740eb
    Signed-off-by: Luca Vitali (luvi) <luvi@odoo.com>

--- addons/portal/static/src/js/portal_sidebar.js
+++ addons/portal/static/src/js/portal_sidebar.js
@@ -38,9 +38,9 @@ var PortalSidebar = publicWidget.Widget.extend({
                 } else if (diff > 0) {
                     // Workaround: force uniqueness of these two translations. We use %1d because the string
                     // with %d is already used in mail and mail's translations are not sent to the frontend.
-                    displayStr = _t('Due in %s days', Math.abs(diff).toFixed(1));
+                    displayStr = _t('Due in %s days', Math.abs(diff).toFixed());
                 } else {
-                    displayStr = _t('%s days overdue', Math.abs(diff).toFixed(1));
+                    displayStr = _t('%s days overdue', Math.abs(diff).toFixed());
                 }
                 $(el).text(displayStr);
         });
