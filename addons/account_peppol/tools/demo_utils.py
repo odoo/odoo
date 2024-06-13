8b8bc0724f7172bd26291b3e680303e2335aad1e
aliya <alta@odoo.com>
2024-06-13 11:44:08 +0200

    [IMP] account_peppol: refactor partner verification
    Curent partner verification is complex. We do not need three fields
    to handle one verification.
    Another problem is that the field is stored and that value remains on the partner
    regardless of the EDI mode of the active company in the database.
    
    This commit cleans up verification process.
    
    task-4038127
    
    closes odoo/odoo#169650
    
    Related: odoo/upgrade#6164
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_peppol/tools/demo_utils.py
+++ addons/account_peppol/tools/demo_utils.py
@@ -82,8 +82,9 @@ def _mock_make_request(func, self, *args, **kwargs):
 
 def _mock_button_verify_partner_endpoint(func, self, *args, **kwargs):
     self.ensure_one()
-    self.account_peppol_validity_last_check = fields.Date.today()
-    self.account_peppol_is_endpoint_valid = True
+    old_value = self.peppol_verification_state
+    self.peppol_verification_state = 'valid'
+    self._log_verification_state_update(self.env.company, old_value, 'valid')
 
 def _mock_user_creation(func, self, *args, **kwargs):
     func(self, *args, **kwargs)
