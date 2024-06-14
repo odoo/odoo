8b6ac8122bd236cc6fe466ea630b6be69dbdb587
Loan (LSE) <lse@odoo.com>
2024-06-14 18:22:08 +0200

    [FIX] hw_driver: synchronous websocket message handling
    Before this commit:
    Receiving a report call on "Microsoft Print to PDF" printer prevent
    the websocket to receive any other message after due to the fact
    that ghostscript script will pop an input prompt (which we can't
    even interact with).
    
    Although the case is rare, we should not rely on a synchronous
    process to handle all incoming websocket request as any longer
    request would force other incoming request to wait.
    
    After this commit:
    Handle the messages asynchronously by spawning threads in a pool.
    Some logging will also be displayed if a request take too long.
    It will also restart the IoT if the pool contains too many time-
    outing requests.

--- addons/hw_drivers/websocket_client.py
+++ addons/hw_drivers/websocket_client.py
@@ -1,5 +1,11 @@
+import concurrent.futures
+import itertools
 import json
 import logging
+import pprint
+import sys
+import threading
+import traceback
 import time
 import urllib.parse
 import urllib3
@@ -12,6 +18,50 @@ from odoo.addons.hw_drivers.tools import helpers
 
 _logger = logging.getLogger(__name__)
 websocket.enableTrace(True, level=logging.getLevelName(_logger.getEffectiveLevel()))
+
+WS_REQUEST_POOL = concurrent.futures.ThreadPoolExecutor(8)
+
+WS_REQUEST_TIME_TIMEOUT = 5 * 60  # seconds
+WS_REQUEST_COUNTER = itertools.count()
+"""
+unique incrementing identifier each time a new websocket message is received
+"""
+
+def _check_ws_request_pool():
+    """Check if the request pool is not stuck with timeout-ing requests.
+    We can't kill a thread, but if the situation is desperate we will restart the IoT.
+
+    We use a concurrent.futures.ThreadPoolExecutor to handle the websocket messages.
+    Ccenario example:
+     When a ws request R1 is received, it will create a thread T1 in the pool to handle the request
+     When a second ws request is received R2:
+      if R1 finished, T1 is "free" so it will be reused
+      if R1 is still running, T1 will be busy so a second thread T2 will be created to handle R2
+     ...
+     until the maximum number of threads is reached (see `ThreadPoolExecutor._max_workers`).
+     In this case they will wait until a thread is free.
+    """
+    def thread_stack_trace(thread):
+        frame = sys._current_frames().get(thread.ident, None)
+        if frame:
+            return ". Thread stack:\n" + ''.join(traceback.format_stack(frame))
+        return ''
+
+    while True:
+        time.sleep(WS_REQUEST_TIME_TIMEOUT)
+        for ws_thread in WS_REQUEST_POOL._threads:
+            if not getattr(ws_thread, 'starting_time', None):
+                # thread that was started by a previous ws request but currently idle
+                continue
+            thread_ws_request_id = getattr(ws_thread, 'ws_request_id', None)
+            time_differece = time.time() - ws_thread.starting_time
+            if time_differece > WS_REQUEST_TIME_TIMEOUT:
+                _logger.error("Websocket thread %s handling ws request #%d is stuck (busy since %.2f sec)%s", ws_thread, thread_ws_request_id, time_differece, thread_stack_trace(ws_thread))
+            else:
+                _logger.debug("Websocket thread %s handling ws request #%d is busy since %.2f sec", ws_thread, thread_ws_request_id, time_differece)
+
+threading.Thread(name='ws request pool checker', target=_check_ws_request_pool, daemon=True).start()
+
 
 def send_to_controller(print_id, device_identifier):
     server = helpers.get_odoo_server_url()
@@ -34,13 +84,30 @@ def send_to_controller(print_id, device_identifier):
     except Exception:
         _logger.exception('Could not reach configured server: %s', server)
 
-
 def on_message(ws, messages):
     """
-        When a message is receive, this function is triggered
+        When a message is receive by the websocket, this function is triggered
+        We use threads to avoid blocking the main thread on a message with long processing, see:
+        https://websocket-client.readthedocs.io/en/latest/threading.html
+    """
+    ws_request_id = next(WS_REQUEST_COUNTER)
+    _logger.debug("websocket received new message #%d: %s", ws_request_id, messages)
+    WS_REQUEST_POOL.submit(handle_ws_message, messages, ws_request_id)
+    # Be very careful to NOT add sync calls like `ws_request.result()` here
+    # The goal is to avoid blocking the main thread, so we can't wait for the result
+
+
+def handle_ws_message(ws_messages, ws_request_id):
+    """
+        Handle the message received by the websocket synchronously.
+        e.g:
         The message is load and if its type is 'print', is sent to the printer
     """
-    messages = json.loads(messages)
+    current_thread = threading.current_thread()
+    current_thread.starting_time = time.time()
+    current_thread.ws_request_id = ws_request_id
+    messages = json.loads(ws_messages)
+    _logger.debug("websocket thread %s handling ws request #%d:\n%s", current_thread, ws_request_id, pprint.pformat(messages))
     for document in messages:
         if (document['message']['type'] == 'print'):
             payload = document['message']['payload']
@@ -50,6 +117,10 @@ def on_message(ws, messages):
                     if device['identifier'] in main.iot_devices:
                         main.iot_devices[device["identifier"]]._action_default(payload)
                         send_to_controller(payload['print_id'], device['identifier'])
+    _logger.debug("websocket finished handling message #%s", ws_request_id)
+    # let know the pool checker that the thread is free
+    current_thread.starting_time = None
+    current_thread.ws_request_id = None
 
 
 def on_error(ws, error):
