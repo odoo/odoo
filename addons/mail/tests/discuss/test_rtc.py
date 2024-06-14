ff1da8cc5c7d7f8152e4054a02f896c03eaa595a
Sébastien Theys <seb@odoo.com>
2024-06-14 15:01:29 +0200

    [REF] mail: make _rtc_invite_members store data ready
    Part of task-3605717
    
    Part-of: odoo/odoo#169484
    Related: odoo/enterprise#64700
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/tests/discuss/test_rtc.py
+++ addons/mail/tests/discuss/test_rtc.py
@@ -129,7 +129,7 @@ class TestChannelRTC(MailCommon):
                 {
                     'type': 'mail.record/insert',
                     'payload': {
-                        'Thread': {
+                        'Thread': [{
                             'id': channel.id,
                             'model': 'discuss.channel',
                             'invitedMembers': [('ADD', [{
@@ -142,7 +142,7 @@ class TestChannelRTC(MailCommon):
                                     'type': "partner",
                                 },
                             }])],
-                        }
+                        }],
                     },
                 },
             ]
@@ -230,7 +230,7 @@ class TestChannelRTC(MailCommon):
                 {
                     'type': 'mail.record/insert',
                     'payload': {
-                        'Thread': {
+                        'Thread': [{
                             'id': channel.id,
                             'model': 'discuss.channel',
                             'invitedMembers': [('ADD', [
@@ -255,7 +255,7 @@ class TestChannelRTC(MailCommon):
                                     },
                                 },
                             ])],
-                        }
+                        }],
                     },
                 },
             ]
@@ -643,7 +643,7 @@ class TestChannelRTC(MailCommon):
                 {
                     'type': 'mail.record/insert',
                     'payload': {
-                        'Thread': {
+                        'Thread': [{
                             'id': channel.id,
                             'model': 'discuss.channel',
                             'rtcInvitingSession': {
@@ -659,13 +659,13 @@ class TestChannelRTC(MailCommon):
                                     },
                                 },
                             },
-                        }
+                        }],
                     },
                 },
                 {
                     'type': 'mail.record/insert',
                     'payload': {
-                        'Thread': {
+                        'Thread': [{
                             'id': channel.id,
                             'model': 'discuss.channel',
                             'rtcInvitingSession': {
@@ -681,13 +681,13 @@ class TestChannelRTC(MailCommon):
                                     },
                                 },
                             },
-                        }
+                        }],
                     },
                 },
                 {
                     'type': 'mail.record/insert',
                     'payload': {
-                        'Thread': {
+                        'Thread': [{
                             'id': channel.id,
                             'model': 'discuss.channel',
                             'invitedMembers': [('ADD', [
@@ -712,7 +712,7 @@ class TestChannelRTC(MailCommon):
                                     },
                                 },
                             ])],
-                        }
+                        }],
                     },
                 },
             ],
