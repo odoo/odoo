ac5757dfeb802f4615d0f0c07b46c720fbdeaccc
Harsh Modi <hamo@odoo.com>
2024-06-14 11:34:16 +0530

    [FIX] account: fix branch fiscal not auto applying on the invoice
    Before this commit:
    If a company is having a branch then while creating a invoice,
    fiscal position from parent company is auto applied(auto apply)
    even if the branch is having it's own fiscal position with auto
    apply true.
    
    After this commit:
    This commits solves the above issue
    
    task-3614541
    
    Co-authored-by: William Andre <wan@odoo.com>
    Co-authored-by: Harsh Modi <hamo@odoo.com>

--- addons/account/models/partner.py
+++ addons/account/models/partner.py
@@ -176,38 +176,45 @@ class AccountFiscalPosition(models.Model):
     def _get_fpos_by_region(self, country_id=False, state_id=False, zipcode=False, vat_required=False):
         if not country_id:
             return False
+
+        def local_get_fpos_by_region(base_domain):
+            null_state_dom = state_domain = [('state_ids', '=', False)]
+            null_zip_dom = zip_domain = [('zip_from', '=', False), ('zip_to', '=', False)]
+            null_country_dom = [('country_id', '=', False), ('country_group_id', '=', False)]
+
+            if zipcode:
+                zip_domain = [('zip_from', '<=', zipcode), ('zip_to', '>=', zipcode)]
+
+            if state_id:
+                state_domain = [('state_ids', '=', state_id)]
+
+            domain_country = base_domain + [('country_id', '=', country_id)]
+            domain_group = base_domain + [('country_group_id.country_ids', '=', country_id)]
+
+            # Build domain to search records with exact matching criteria
+            fpos = self.search(domain_country + state_domain + zip_domain, limit=1)
+            # return records that fit the most the criteria, and fallback on less specific fiscal positions if any can be found
+            if not fpos and state_id:
+                fpos = self.search(domain_country + null_state_dom + zip_domain, limit=1)
+            if not fpos and zipcode:
+                fpos = self.search(domain_country + state_domain + null_zip_dom, limit=1)
+            if not fpos and state_id and zipcode:
+                fpos = self.search(domain_country + null_state_dom + null_zip_dom, limit=1)
+
+            # fallback: country group with no state/zip range
+            if not fpos:
+                fpos = self.search(domain_group + null_state_dom + null_zip_dom, limit=1)
+
+            if not fpos:
+                # Fallback on catchall (no country, no group)
+                fpos = self.search(base_domain + null_country_dom, limit=1)
+            return fpos
+
         base_domain = self._prepare_fpos_base_domain(vat_required)
-        null_state_dom = state_domain = [('state_ids', '=', False)]
-        null_zip_dom = zip_domain = [('zip_from', '=', False), ('zip_to', '=', False)]
-        null_country_dom = [('country_id', '=', False), ('country_group_id', '=', False)]
-
-        if zipcode:
-            zip_domain = [('zip_from', '<=', zipcode), ('zip_to', '>=', zipcode)]
-
-        if state_id:
-            state_domain = [('state_ids', '=', state_id)]
-
-        domain_country = base_domain + [('country_id', '=', country_id)]
-        domain_group = base_domain + [('country_group_id.country_ids', '=', country_id)]
-
-        # Build domain to search records with exact matching criteria
-        fpos = self.search(domain_country + state_domain + zip_domain, limit=1)
-        # return records that fit the most the criteria, and fallback on less specific fiscal positions if any can be found
-        if not fpos and state_id:
-            fpos = self.search(domain_country + null_state_dom + zip_domain, limit=1)
-        if not fpos and zipcode:
-            fpos = self.search(domain_country + state_domain + null_zip_dom, limit=1)
-        if not fpos and state_id and zipcode:
-            fpos = self.search(domain_country + null_state_dom + null_zip_dom, limit=1)
-
-        # fallback: country group with no state/zip range
-        if not fpos:
-            fpos = self.search(domain_group + null_state_dom + null_zip_dom, limit=1)
-
-        if not fpos:
-            # Fallback on catchall (no country, no group)
-            fpos = self.search(base_domain + null_country_dom, limit=1)
-        return fpos
+        if fpos := local_get_fpos_by_region([('company_id', '=', self.env.company.id)] + base_domain):
+            return fpos
+        else:
+            return local_get_fpos_by_region(base_domain)
 
     def _get_vat_valid(self, delivery, company=None):
         """ Hook for determining VAT validity with more complex VAT requirements """
