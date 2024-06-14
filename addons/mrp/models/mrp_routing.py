4e783e01b7904bfc1a23417c5a520dbc558b9da4
Djamel Touati <otd@odoo.com>
2024-06-14 09:23:34 +0000

    [FIX] mrp: delete blocked_by_operation when changing bom in OP
    Steps to reproduce the bug:
    - Create a storable product “P1” with BoM
        - Operation: OP1, OP2
        - OP2 blocked by OP1
    
    - Navigate to Mrp > Configuration > Operations
    - Select OP1 and select another BoM
    - Return to the BoM of “P1”
    
    Problem:
    The OP2 is still linked to OP1, And a traceback when attempting to
    duplicate the BoM.
    
    Solution:
    Remove the link between OP2 and OP1
    
    opw-3948817
    
    X-original-commit: bd39917863a0cd5c30623acf97987be129a5c906

--- addons/mrp/models/mrp_routing.py
+++ addons/mrp/models/mrp_routing.py
@@ -117,6 +117,7 @@ class MrpRoutingWorkcenter(models.Model):
             for op in self:
                 op.bom_id.bom_line_ids.filtered(lambda line: line.operation_id == op).operation_id = False
                 op.bom_id.byproduct_ids.filtered(lambda byproduct: byproduct.operation_id == op).operation_id = False
+                op.bom_id.operation_ids.filtered(lambda operation: operation.blocked_by_operation_ids == op).blocked_by_operation_ids = False
         return super().write(vals)
 
     def action_archive(self):
