0ceb0033591e2643af6075f289bbd39c5dc055e1
Víctor Martínez <victor.martinez@tecnativa.com>
2024-06-14 06:37:39 +0000

    [IMP] hr_attendance: Show Extra hours information in kiosk only if the "Count Extra Hours" checkbox is selected
    TT49624
    
    X-original-commit: c7fe976977469b2d739e41b53e07bc02bae439e2

--- addons/hr_attendance/models/hr_employee.py
+++ addons/hr_attendance/models/hr_employee.py
@@ -149,7 +149,110 @@ class HrEmployee(models.Model):
             att = employee.last_attendance_id.sudo()
             employee.attendance_state = att and not att.check_out and 'checked_in' or 'checked_out'
 
+<<<<<<< HEAD
     def _attendance_action_change(self, geo_information=None):
+||||||| parent of 1564f07f810a (temp)
+    @api.model
+    def attendance_scan(self, barcode):
+        """ Receive a barcode scanned from the Kiosk Mode and change the attendances of corresponding employee.
+            Returns either an action or a warning.
+        """
+        employee = self.sudo().search([('barcode', '=', barcode)], limit=1)
+        if employee:
+            return employee._attendance_action('hr_attendance.hr_attendance_action_kiosk_mode')
+        return {'warning': _("No employee corresponding to Badge ID '%(barcode)s.'") % {'barcode': barcode}}
+
+    def attendance_manual(self, next_action, entered_pin=None):
+        self.ensure_one()
+        attendance_user_and_no_pin = self.user_has_groups(
+            'hr_attendance.group_hr_attendance_user,'
+            '!hr_attendance.group_hr_attendance_use_pin')
+        can_check_without_pin = attendance_user_and_no_pin or (self.user_id == self.env.user and entered_pin is None)
+        if can_check_without_pin or entered_pin is not None and entered_pin == self.sudo().pin:
+            return self._attendance_action(next_action)
+        if not self.user_has_groups('hr_attendance.group_hr_attendance_user'):
+            return {'warning': _('To activate Kiosk mode without pin code, you must have access right as an Officer or above in the Attendance app. Please contact your administrator.')}
+        return {'warning': _('Wrong PIN')}
+
+    def _attendance_action(self, next_action):
+        """ Changes the attendance of the employee.
+            Returns an action to the check in/out message,
+            next_action defines which menu the check in/out message should return to. ("My Attendances" or "Kiosk Mode")
+        """
+        self.ensure_one()
+        employee = self.sudo()
+        action_message = self.env["ir.actions.actions"]._for_xml_id("hr_attendance.hr_attendance_action_greeting_message")
+        action_message['previous_attendance_change_date'] = employee.last_attendance_id and (employee.last_attendance_id.check_out or employee.last_attendance_id.check_in) or False
+        action_message['employee_name'] = employee.name
+        action_message['barcode'] = employee.barcode
+        action_message['next_action'] = next_action
+        action_message['hours_today'] = employee.hours_today
+        action_message['kiosk_delay'] = employee.company_id.attendance_kiosk_delay * 1000
+
+        if employee.user_id:
+            modified_attendance = employee.with_user(employee.user_id).sudo()._attendance_action_change()
+        else:
+            modified_attendance = employee._attendance_action_change()
+        action_message['attendance'] = modified_attendance.read()[0]
+        action_message['total_overtime'] = employee.total_overtime
+        # Overtime have an unique constraint on the day, no need for limit=1
+        action_message['overtime_today'] = self.env['hr.attendance.overtime'].sudo().search([
+            ('employee_id', '=', employee.id), ('date', '=', fields.Date.context_today(self)), ('adjustment', '=', False)]).duration or 0
+        return {'action': action_message}
+
+    def _attendance_action_change(self):
+=======
+    @api.model
+    def attendance_scan(self, barcode):
+        """ Receive a barcode scanned from the Kiosk Mode and change the attendances of corresponding employee.
+            Returns either an action or a warning.
+        """
+        employee = self.sudo().search([('barcode', '=', barcode)], limit=1)
+        if employee:
+            return employee._attendance_action('hr_attendance.hr_attendance_action_kiosk_mode')
+        return {'warning': _("No employee corresponding to Badge ID '%(barcode)s.'") % {'barcode': barcode}}
+
+    def attendance_manual(self, next_action, entered_pin=None):
+        self.ensure_one()
+        attendance_user_and_no_pin = self.user_has_groups(
+            'hr_attendance.group_hr_attendance_user,'
+            '!hr_attendance.group_hr_attendance_use_pin')
+        can_check_without_pin = attendance_user_and_no_pin or (self.user_id == self.env.user and entered_pin is None)
+        if can_check_without_pin or entered_pin is not None and entered_pin == self.sudo().pin:
+            return self._attendance_action(next_action)
+        if not self.user_has_groups('hr_attendance.group_hr_attendance_user'):
+            return {'warning': _('To activate Kiosk mode without pin code, you must have access right as an Officer or above in the Attendance app. Please contact your administrator.')}
+        return {'warning': _('Wrong PIN')}
+
+    def _attendance_action(self, next_action):
+        """ Changes the attendance of the employee.
+            Returns an action to the check in/out message,
+            next_action defines which menu the check in/out message should return to. ("My Attendances" or "Kiosk Mode")
+        """
+        self.ensure_one()
+        employee = self.sudo()
+        action_message = self.env["ir.actions.actions"]._for_xml_id("hr_attendance.hr_attendance_action_greeting_message")
+        action_message['previous_attendance_change_date'] = employee.last_attendance_id and (employee.last_attendance_id.check_out or employee.last_attendance_id.check_in) or False
+        action_message['employee_name'] = employee.name
+        action_message['barcode'] = employee.barcode
+        action_message['next_action'] = next_action
+        action_message['hours_today'] = employee.hours_today
+        action_message['kiosk_delay'] = employee.company_id.attendance_kiosk_delay * 1000
+
+        if employee.user_id:
+            modified_attendance = employee.with_user(employee.user_id).sudo()._attendance_action_change()
+        else:
+            modified_attendance = employee._attendance_action_change()
+        action_message['attendance'] = modified_attendance.read()[0]
+        action_message['show_total_overtime'] = employee.company_id.hr_attendance_overtime
+        action_message['total_overtime'] = employee.total_overtime
+        # Overtime have an unique constraint on the day, no need for limit=1
+        action_message['overtime_today'] = self.env['hr.attendance.overtime'].sudo().search([
+            ('employee_id', '=', employee.id), ('date', '=', fields.Date.context_today(self)), ('adjustment', '=', False)]).duration or 0
+        return {'action': action_message}
+
+    def _attendance_action_change(self):
+>>>>>>> 1564f07f810a (temp)
         """ Check In/Check Out action
             Check In: create a new attendance record
             Check Out: modify check_out field of appropriate attendance record
