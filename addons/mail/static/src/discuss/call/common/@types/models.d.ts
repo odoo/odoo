c6d37316adea3e584bee0daced20845e9728e920
Sébastien Theys <seb@odoo.com>
2024-06-17 12:21:14 +0200

    [REF] mail: make Rtc Service a model
    In preparation to use store insert instead of custom handlers in future
    commits.
    
    Part of task-3605717
    
    Part-of: odoo/odoo#169691
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/discuss/call/common/@types/models.d.ts
+++ addons/mail/static/src/discuss/call/common/@types/models.d.ts
@@ -1,9 +1,11 @@
 declare module "models" {
+    import { Rtc as RtcClass } from "@mail/discuss/call/common/rtc_service";
     import { RtcSession as RtcSessionClass } from "@mail/discuss/call/common/rtc_session_model";
 
     export interface DiscussApp {
         ringingThreads: Thread[],
     }
+    export interface Rtc extends RtcClass {}
     export interface RtcSession extends RtcSessionClass {}
 
     export interface Models {
