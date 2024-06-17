fe3d45aeae0ec91fba8ac2385b258ba0881d8d1a
Andrzej(pian) <pian@odoo.com>
2024-06-17 17:04:51 +0200

    [FIX] web_editor: won't place font element between if else nodes
    Previously when we updated color on multiple selected nodes, </font> tag
    would get inserted between qweb if and else tags. This caused qweb processor to crash.
    
    After this change we don't insert font on the "t" elements.
    
    [Reproduce]
    - Install account
    - Open Email Template called "Invoice: Sending"
    - Change color of all of its content
    - Open preview
    
    opw-3912434
    
    X-original-commit: 7833ec611f62822e5e25113cc581c0a87043ab80

--- addons/web_editor/static/src/js/editor/odoo-editor/test/spec/color.test.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/test/spec/color.test.js
@@ -157,6 +157,35 @@ describe('applyColor', () => {
             contentAfter: '<p>[abcabc]</p>',
         });
     });
+    it('Shall not apply font tag to t nodes (protects if else nodes separation)', async () => {
+        await testEditor(BasicEditor, {
+            contentBefore: unformat(`[
+                <p>
+                    <t t-if="object.partner_id.parent_id">
+                       <t t-out="object.partner_id.parent_id.name or ''">Azure Interior</t>
+                    </t>
+                    <t t-else="">
+                        <t t-out="object.partner_id.name or ''">Brandon Freeman</t>
+                    </t>
+                </p>
+            ]`),
+            stepFunction: setColor('red', 'backgroundColor'),
+            contentAfter: unformat(`
+                <p>
+                    <t t-if="object.partner_id.parent_id">
+                        <t t-out="object.partner_id.parent_id.name or ''" style="background-color: red;">
+                            <font style="background-color: red;">[AzureInterior</font>
+                        </t>
+                    </t>
+                    <t t-else="">
+                        <t t-out="object.partner_id.name or ''" style="background-color: red;">
+                            <font style="background-color: red;">BrandonFreeman]</font>
+                        </t>
+                    </t>
+                </p>
+            `),
+        });
+    });
 });
 describe('rgbToHex', () => {
     it('should convert an rgb color to hexadecimal', async () => {
