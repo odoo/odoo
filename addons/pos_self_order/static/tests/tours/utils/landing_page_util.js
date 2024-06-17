7c3f3dc75ac044eaabad0ac8ff9eee9bd9fb25c7
axtr <axtr@odoo.com>
2024-06-17 11:17:41 +0200

    [FIX] pos_self_order: remove banner shown for QR menu when restaurant open
    Current behavior:
    When the restaurant is opened and "Self Ordering" is set to "QR menu", the banner "We're currently closed." is shown.
    
    Steps to reproduce:
    - Install "Point of Sale" app and "pos_restaurant" module
    - Start a restaurant session and go to the backend
    - In the settings, set "Self Ordering" to "QR menu" and save
    - Click on "Preview Web interface"
    - Problem: The banner is shown
    
    Solutions:
    Display the banner if the session is not opened rather than when we have a token (in mode "QR menu", there is no token)
    
    opw-3854839
    
    X-original-commit: b2eefcc8852dd71a2c97715f29827068f56c08ba

--- addons/pos_self_order/static/tests/tours/utils/landing_page_util.js
+++ addons/pos_self_order/static/tests/tours/utils/landing_page_util.js
@@ -12,3 +12,11 @@ export function isClosed() {
         run: () => {},
     };
 }
+
+export function isOpened() {
+    return {
+        content: `Check if the POS is opened`,
+        trigger: `body:not(:has(.o-self-closed))`,
+        run: () => {},
+    };
+}
