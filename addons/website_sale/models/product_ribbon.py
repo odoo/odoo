32c98ee2d77892811f49561ebb74975e0e2c7005
rupa-odoo <rupa@odoo.com>
2024-06-18 05:27:51 +0000

    [FIX] website_sale: fix ribbon bg-color on product
    Steps to reproduce:
    - Go to Shop page on website
    - Enable edit mode
    - Pick a product and create a new Badge/Ribbon
    - Change the background color
    - Bug -> the bg-color is not set.
    
    Issue :
    ribbon bg-color is not set on the product.
    
    Cause :
    The !important attribute has been duplicated in the CSS rule since [1]
    and [2]. As a result, the background colour CSS rule for the ribbon is
    broken due to having !important twice.
    
    [1]: https://github.com/odoo/odoo/commit/c6f4929f65b899736c556f8f0bb7824883a6d893
    [2]: https://github.com/odoo/odoo/commit/9ee115b58342b3e0dbc11081e7ff752c10f8bfa9
    
    fix :
    After removing the '!important' attribute from the create and write methods,
    we only encounter the '!important' attribute once. As a result, the ribbon
    background color is applied to the product.
    
    opw-3964071
    
    X-original-commit: 56fd8440707a0f3f2ba298f8bf016577ca08796b

--- addons/website_sale/models/product_ribbon.py
+++ addons/website_sale/models/product_ribbon.py
@@ -18,17 +18,3 @@ class ProductRibbon(models.Model):
     def _compute_display_name(self):
         for ribbon in self:
             ribbon.display_name = f'{tools.html2plaintext(ribbon.html)} (#{ribbon.id})'
-
-    #=== CRUD METHODS ===#
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            if vals.get('bg_color') and not '!important' in vals['bg_color']:
-                vals['bg_color'] += ' !important'
-        return super().create(vals_list)
-
-    def write(self, data):
-        if data.get('bg_color') and not '!important' in data['bg_color']:
-            data['bg_color'] += ' !important'
-        return super().write(data)
