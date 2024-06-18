445b98d0a7fead7f0199585e05594b19e27cc7e1
Pedram (pebr) <pebr@odoo.com>
2024-06-18 09:00:21 +0000

    [FIX] point_of_sale: ensure loading of partners from other sessions
    Prior to this commit, orders loaded from other sessions did not load
    missing partners.
    
    opw-3981458
    
    closes odoo/odoo#169963
    
    X-original-commit: d9e2bee9aa7eb1a32774ab8dc3d8f941867d0101
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/store/pos_store.js
+++ addons/point_of_sale/static/src/app/store/pos_store.js
@@ -982,7 +982,8 @@ export class PosStore extends Reactive {
                 message = messageFp;
             }
         }
-        await this._getMissingProducts(ordersJson);
+        await this._loadMissingProducts(ordersJson);
+        await this._loadMissingPartners(ordersJson);
         const allOrders = [...this.get_order_list()];
         this._replaceOrders(allOrders, ordersJson);
         this.sortOrders();
@@ -1022,18 +1023,6 @@ export class PosStore extends Reactive {
             [odoo.pos_session_id],
             pricelistsToGet,
         ]);
-    }
-    async _getMissingProducts(ordersJson) {
-        const productIds = [];
-        for (const order of ordersJson) {
-            for (const orderline of order.lines) {
-                if (!this.models["product.product"].get(orderline[2].product_id)) {
-                    productIds.push(orderline[2].product_id);
-                }
-            }
-        }
-
-        await this.loadProducts(productIds);
     }
     async _addFiscalPositions(ordersJson) {
         const fiscalPositionToGet = [];
