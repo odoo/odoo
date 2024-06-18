81d0bb0a92cfccde22033b2e73a4ed8675810e3e
muab-odoo <muab@odoo.com>
2024-06-18 17:20:39 +0300

    [IMP] l10n_pl: fill accounting date with delivery date
    It seems that in Eastern European country, they need to use the delivery date as accounting date in majority of cases
    
    solution: So the idea is to use the same feature that we have now but  use the delivery date as reference
    in place of invoice date in the sales (always) and in the purchase if delivery date is earlier than invoice date
    
    Also, in case the period is locked we will get this warning message
    'The date is being set prior to the tax lock date 04/30/2024. The Journal Entry will be accounted on 05/28/2024 upon posting'
    So, in this case we need to set the accounting date and delivery date to the first not locked period
    
    task-id#3953743

--- addons/l10n_pl/models/account_move.py
+++ addons/l10n_pl/models/account_move.py
@@ -16,3 +16,38 @@ class AccountMove(models.Model):
         string='B_MPV_Prowizja',
         help="Supply of agency and other services pertaining to the transfer of a single-purpose voucher",
     )
+    l10n_pl_delivery_date_onchange = fields.Boolean(
+        compute='_compute_delivery_date_onchange',
+        export_string_translation=False,
+    )
+
+    @api.depends('delivery_date')
+    def _compute_show_delivery_date(self):
+        for move in self:
+            move.show_delivery_date = move.delivery_date
+
+    @api.depends('delivery_date')
+    def _compute_delivery_date_onchange(self):
+        for move in self:
+            move.show_delivery_date = move.delivery_date
+
+    @api.depends('l10n_pl_delivery_date_onchange')
+    def _compute_date(self):
+        super()._compute_date()
+        for move in self:
+            if move.delivery_date:
+                if move.invoice_filter_type_domain == 'sale':
+                    move.date = move.delivery_date
+                elif move.invoice_filter_type_domain == 'purchase':
+                    move.date = min(move.delivery_date, move.invoice_date)
+                self.env.add_to_compute(move.line_ids._fields['date'], move.line_ids)
+                self.env.add_to_compute(self._fields['name'], move)
+
+    def action_post(self):
+        for move in self:
+            affects_tax_report = move._affect_tax_report()
+            lock_dates = move._get_violated_lock_dates(move.date, affects_tax_report)
+            if lock_dates:
+                move.date = move._get_accounting_date(move.invoice_date or move.date, affects_tax_report)
+                move.delivery_date = move._get_accounting_date(move.invoice_date or move.delivery_date, affects_tax_report)
+        return super().action_post()
