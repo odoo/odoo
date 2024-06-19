c00678913774089c2d7a7fbdd8ad3b05c5e0ba1c
Shubham Thanki <shut@odoo.com>
2024-06-19 12:06:49 +0530

    [IMP] various: improve Login and Password reset UX
    - Added a `fa-eye` icon to reveal the password to the user when the user clicks on it.
      Also added this to the portal view (login as a portal user, then click the
     `Connection & Security` tab)
    - Also added an alert message below the password input field, notifying the user
      if he has CAPS LOCK active.
      Also added this to the portal view, and can be accessed the same way as mentioned
      above.
    - Since we added the CAPS LOCK alert below the password input field, we had to move
      the password meter above the input field.
    - Added password strength titles (Weak, Medium, Strong) just beside the password meter.
    - Also made sure the password meter becomes visible only when we start typing
      the password.
    - In the reset password window, the fields with the label `Your Name` & `Your Email`
      have been removed.
      (This would make sure the user data is not revealed even if
      the reset password link is compromised/ made public)
    - After the user has successfully reset the password, the user needs to login again.
      (Previously the user would login automatically, but that should not happen if
      incase the reset password link gets compromised.)
    - On successfully resetting the password, we display a text-success message stating
      the password was successfully reset.
    - Before this commit, the user would get only one attempt to reset his password.
      Let's say the user clicks the reset password link, types in a password that is not
      acceptable, then he tries again, but then gets the invalid token error.
         - Now the user can have as many tries as he wants to reset his password.
    - Switched the positions of the YOUR NAME and YOUR EMAIL field.
    - Wrote some custom CSS to ensure the box shadow on the input fields also wrap around
      the `fa-eye` button.
    - Center aligned the `Log in with Odoo.com` button contents. (now the contents inside
      every external login button will be aligned center)
    - Also modified the tour according to the new reset password flow.
    
    Task-3959739

---
+++ addons/web/static/src/public/show_password.js
@@ -0,0 +1,32 @@
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
+
+export class ShowPassword extends Interaction {
+    static selector = "#showPass";
+    dynamicContent = {
+        _root: { "t-on-pointerdown": this.showText },
+    };
+
+    setup() {
+        const inputGroupEl = this.el.closest(".input-group");
+        this.passwordEl = inputGroupEl.querySelector("input[type='password']");
+        this.iconEl = inputGroupEl.querySelector("i");
+    }
+
+    showText() {
+        this.passwordEl.type = "text";
+        this.iconEl.classList.replace("fa-eye", "fa-eye-slash");
+        document.body.addEventListener(
+            "pointerup",
+            () => {
+                this.passwordEl.type = "password";
+                this.iconEl.classList.replace("fa-eye-slash", "fa-eye");
+            },
+            { once: true }
+        );
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("website.show_password", ShowPassword);
