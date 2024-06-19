68a620cac8443d50336be649b1681b94b5de3bbe
Renaud Thiry <reth@odoo.com>
2024-06-19 15:44:50 +0200

    [MOV] mail: move field path resolver-formatter to mail
    Resolving a field path to a usable string
    is globally useful for dynamic messages
    that cannot rely on qweb to format their output.
    
    The Model method introduced in whatsapp to do this
    is moved to mail so it can be used in other modules.
    
    task-3495279

--- addons/mail/models/models.py
+++ addons/mail/models/models.py
@@ -2,10 +2,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from collections import defaultdict
+from datetime import datetime
 from lxml.builder import E
 from markupsafe import Markup
 
-from odoo import api, models, tools, _
+from odoo import api, exceptions, models, tools, _
 from odoo.addons.mail.tools.alias_error import AliasError
 
 import logging
@@ -418,6 +419,58 @@ class BaseModel(models.AbstractModel):
         return Markup("<a href=# data-oe-model='%s' data-oe-id='%s'>%s</a>") % (
             self._name, self.id, title or self.display_name)
 
+    def _mail_map_and_format(self, field_path):
+        """Get the value of field, returning display_name(s) if the field is a
+        model. Can be called on a void recordset, in which case it mainly serves
+        as a field path validation."""
+        if self:
+            self.ensure_one()
+
+        # as we use mapped(False) returns record, better return a void string
+        if not field_path:
+            return ''
+
+        try:
+            field_value = self.mapped(field_path)
+        except KeyError as err:
+            raise exceptions.UserError(
+                _("'%(field)s' does not seem to be a valid field path", field=field_path)
+            ) from err
+        except Exception as err:  # noqa: BLE001
+            raise exceptions.UserError(
+                _("We were not able to fetch the value of field '%(field)s'", field=field_path)
+            ) from err
+        if isinstance(field_value, models.Model):
+            return ' '.join((value.display_name or '') for value in field_value)
+        if any(isinstance(value, datetime) for value in field_value):
+            tz = self._mail_get_record_timezone()
+            return ' '.join([f"{tools.format_datetime(self.env, value, tz=tz)} {tz}"
+                             for value in field_value if value and isinstance(value, datetime)])
+        # find last field / last model when having chained fields
+        # e.g. 'partner_id.country_id.state' -> ['partner_id.country_id', 'state']
+        field_path_models = field_path.rsplit('.', 1)
+        if len(field_path_models) > 1:
+            last_model_path, last_fname = field_path_models
+            last_model = self.mapped(last_model_path)
+        else:
+            last_model, last_fname = self, field_path
+        last_field = last_model._fields[last_fname]
+        # if selection -> return value, not the key
+        if last_field.type == 'selection':
+            return ' '.join(
+                last_field.convert_to_export(value, last_model)
+                for value in field_value
+            )
+        return ' '.join(str(value if value is not False and value is not None else '') for value in field_value)
+
+    def _mail_get_record_timezone(self):
+        """To be override to get desired timezone of the model.
+
+        :return str: selected timezone (e.g. 'UTC' or 'Asia/Kolkata')
+        """
+        if self:
+            self.ensure_one()
+        return self.env.user.tz or 'UTC'
     # ------------------------------------------------------
     # CONTROLLERS
     # ------------------------------------------------------
