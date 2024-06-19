963f9e834c4285ac7893589d953555672c083464
Codie Petersen <petersen.codie@gmail.com>
2024-06-18 19:55:45 -0700

    Removed barcodes, translation, units of measure, utm tracker addons.

--- addons/barcodes/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import barcode_events_mixin
-from . import barcode_nomenclature
-from . import barcode_rule
-from . import ir_http
-from . import res_company
--- addons/barcodes_gs1_nomenclature/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import barcode_nomenclature
-from . import barcode_rule
-from . import ir_http
