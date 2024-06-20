a7de93627f71e309c103ba80d66a0d8a9535d869
Pierre Paridans <app@odoo.com>
2024-06-20 12:07:11 +0200

    [IMP] fixup: disable project todo breadcrumb customization

--- addons/project_todo/static/src/views/todo_form/todo_form_view.js
+++ addons/project_todo/static/src/views/todo_form/todo_form_view.js
@@ -2,12 +2,12 @@
 
 import { registry } from "@web/core/registry";
 import { formView } from "@web/views/form/form_view";
-import { TodoFormControlPanel } from "./todo_form_control_panel";
+// import { TodoFormControlPanel } from "./todo_form_control_panel";
 import { TodoFormController } from "./todo_form_controller";
 
 export const todoFormView = {
     ...formView,
-    ControlPanel: TodoFormControlPanel,
+    // ControlPanel: TodoFormControlPanel,
     Controller: TodoFormController,
 };
 
