4be4a35fb336dbb4813b28d69863d003e991bbd6
Benoit Socias <bso@odoo.com>
2024-06-20 10:53:06 +0200

    [FIX] website, test_website: fix restricted editor edition rights
    In [1] when the translation conditions were adapted to allow restricted
    editors to translate, the general editability right was wrongly
    impacted, making the user believe a page could be edited when it was not
    the case.
    
    This commit restores the editability condition and limits the
    translatability to records that can be modified by the restricted
    editor user.
    A `_check_user_can_modify` is introduced to enable customization.
    
    To make it possible for those users to edit a list page such as the
    `/event` page, the check is run against the `main_object` as well as
    the first found record of each type.
    
    Steps to reproduce (described in terms of expected behavior):
    - Install `website_sale` and `website_event`
    - Add a language to the website
    - Assign the following rights to a non-admin user:
      - Website: Restricted editor
      - Sale: Administrator
      - Event: No right
    - Log in as that user
    - Go to the contact us page:
      - Edit: nothing should be possible
      - Translate: not available
    - Go to a product page:
      - Edit: blocks can be dropped in fields, texts can be changed
      - Translate: available and texts can be changed
    - Go to an event page:
      - Edit: nothing should be possible
      - Translate: not available
    
    [1]: https://github.com/odoo/odoo/commit/1bcc0733c9af52c6cf38b12f24ff6ed96314bec4
    
    closes odoo/odoo#172163
    
    X-original-commit: 4ce74fc1c35188e213974aad54f4b98b76d54a84
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/website/static/src/systray_items/edit_website.js
+++ addons/website/static/src/systray_items/edit_website.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 import { _t } from "@web/core/l10n/translation";
+import { rpc } from "@web/core/network/rpc";
 import { registry } from "@web/core/registry";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
@@ -41,6 +42,42 @@ class EditWebsiteSystray extends Component {
 
     get label() {
         return _t("Edit");
+    }
+
+    attemptStartTranslate() {
+        if (this.websiteService.isRestrictedEditor && !this.websiteService.isDesigner) {
+            if (!this.websiteService.websiteRootInstance) {
+                // Root instance might not be there yet if user clicks too fast.
+                // Let's have him click again rather than apply different rules.
+                return;
+            }
+            const object = this.websiteService.currentWebsite.metadata.mainObject;
+            const objects = {
+                [object.model]: object.id,
+            };
+            const otherRecordEls = this.websiteService.websiteRootInstance.el.querySelectorAll(
+                "[data-res-model][data-res-id]:not([data-res-model='ir.ui.view']), [data-oe-model][data-oe-id]:not([data-oe-model='ir.ui.view'])"
+            );
+            for (const el of otherRecordEls) {
+                const model = el.dataset.resModel || el.dataset.oeModel;
+                if (!objects[model]) {
+                    // Keep one record of each type.
+                    objects[model] = parseInt(el.dataset.resId || el.dataset.oeId);
+                }
+            }
+            rpc('/website/check_can_modify_any', {
+                records: Object.entries(objects).map((kv) => {
+                    return {
+                        res_model: kv[0],
+                        res_id: kv[1],
+                    };
+                }),
+            }).then(() => {
+                this.startTranslate();
+            });
+        } else {
+            this.startTranslate();
+        }
     }
 
     startTranslate() {
