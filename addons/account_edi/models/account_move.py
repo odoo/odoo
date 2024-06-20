5c12c0c11e1ad30e136e6647489e2219bb0c4e62
Naman Shah <nash@odoo.com>
2024-06-20 11:06:04 +0530

    [IMP] account_edi,l10n_in_*: force-cancel button if request cancellation fails
    l10n_in_* = l10n_in_edi,l10n_in_edi_ewaybill
    
    In the context of Indian EDI and e-waybill regulations, the cancellation service
    fails if more than 24 hours have passed since the document was submitted.
    However, these documents can still be canceled through other channels. To ensure
    users can cancel their invoices in Odoo even if the initial cancellation request
    fails, we've implemented a new feature.
    
    A 'Force Cancel' button has been added to the EDI cancellation flow. If a
    cancellation request fails, the user can click this button to cancel the invoice
    immediately. Additionally, we have provided a hook that can be inherited to
    specify whether this force-cancellation option is applicable to each invoice.
    
    task-3997285
    
    closes odoo/odoo#170257
    
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/account_edi/models/account_move.py
+++ addons/account_edi/models/account_move.py
@@ -36,6 +36,8 @@ class AccountMove(models.Model):
         compute='_compute_edi_show_cancel_button')
     edi_show_abandon_cancel_button = fields.Boolean(
         compute='_compute_edi_show_abandon_cancel_button')
+    edi_show_force_cancel_button = fields.Boolean(
+        compute='_compute_edi_show_force_cancel_button')
 
     @api.depends('edi_document_ids.state')
     def _compute_edi_state(self):
@@ -51,6 +53,11 @@ class AccountMove(models.Model):
                 move.edi_state = 'to_cancel'
             else:
                 move.edi_state = False
+
+    @api.depends('edi_document_ids.state')
+    def _compute_edi_show_force_cancel_button(self):
+        for move in self:
+            move.edi_show_force_cancel_button = move._can_force_cancel()
 
     @api.depends('edi_document_ids.error')
     def _compute_edi_error_count(self):
@@ -267,6 +274,14 @@ class AccountMove(models.Model):
         if not self.env.context.get('skip_account_edi_cron_trigger'):
             self.env.ref('account_edi.ir_cron_edi_network')._trigger()
         return posted
+
+    def button_force_cancel(self):
+        """ Cancel the invoice without waiting for the cancellation request to succeed.
+        """
+        for move in self:
+            to_cancel_edi_documents = move.edi_document_ids.filtered(lambda doc: doc.state == 'to_cancel')
+            move.message_post(body=_("This invoice was canceled while the EDIs %s still had a pending cancellation request.", ", ".join(to_cancel_edi_documents.mapped('edi_format_id.name'))))
+        self.button_cancel()
 
     def button_cancel(self):
         # OVERRIDE
