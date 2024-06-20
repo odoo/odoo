43b12984533afae97cb19cd1064b29d3d60aebdc
vlst-odoo <vlst@odoo.com>
2024-06-20 13:44:56 +0200

    [IMP] point_of_sale: improve pos order form view
    In this commit we add a couple of small improvements to the `pos.order`
    form view.
    
    closes odoo/odoo#170124
    
    Task: 4001823
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/point_of_sale/__manifest__.py
+++ addons/point_of_sale/__manifest__.py
@@ -71,7 +71,6 @@
         # web assets
         'web.assets_backend': [
             'point_of_sale/static/src/scss/pos_dashboard.scss',
-            'point_of_sale/static/src/scss/pos_monetary_field.scss',
             'point_of_sale/static/src/backend/tours/point_of_sale.js',
             'point_of_sale/static/src/backend/debug_manager.js',
             'point_of_sale/static/src/backend/pos_kanban_view/*',
