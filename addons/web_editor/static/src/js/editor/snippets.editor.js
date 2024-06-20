b75fd3cc8c442fa314deb6cc3e2a2d4396c54ab0
paru-odoo <paru@odoo.com>
2024-06-20 05:39:29 +0000

    [FIX] web_editor: nested invisible element should show on click
    Steps to reproduce:
    1. Drag & drop a popup
    2. Click on its inner block
    3. Set the visibility hidden on desktop
    4. Save & enter edit mode (not really needed)
    5. Click "Block" to show, popup remains hidden
    
    Issue:
    Nested elements do not toggle visibility correctly. Clicking "Block"
    fails to show the popup. Clicking the popup first, then the block, to
    make it visible.
    
    Key changes:
    Previously, only parent-child paths were stored, limiting navigation
    from parent to child. Now, paths from parent to children are connected
    and stored.
    
    Solution:
    Ensure clicking a child element makes itself and its parents visible.
    Conversely, clicking on a parent element should hide itself and all its
    children.
    
    This commit aims to resolve the visibility toggle bug by creating proper
    parent-to-child connectivity. Clicking on an invisible element now
    toggles all its parents visible. Similarly, clicking on a visible element
    toggles all its descendants invisible.
    
    task-3806730
    
    closes odoo/odoo#188402
    
    X-original-commit: 66d98ffebad7dfc19115f88729eb00218554e20c
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2767,7 +2767,7 @@ class SnippetsMenu extends Component {
                 return false;
             });
             // Insert an invisible snippet in its "parentEl" element.
-            const createInvisibleElement = async (invisibleSnippetEl, isRootParent, isDescendant) => {
+            const createInvisibleElement = async (invisibleSnippetEl, isRootParent, isDescendant, parents) => {
                 const editor = await this._createSnippetEditor($(invisibleSnippetEl), true);
                 return {
                     editor,
@@ -2778,6 +2778,7 @@ class SnippetsMenu extends Component {
                     invisibleSnippetEl,
                     isVisible: editor.isTargetVisible(),
                     children: [],
+                    parents: isDescendant ? parents : null,
                 };
             };
             // Insert all the invisible snippets contained in "snippetEls" as
@@ -2789,15 +2790,15 @@ class SnippetsMenu extends Component {
             //     └ descendantInvisibleSnippet
             //          └ descendantOfDescendantInvisibleSnippet
             //               └ etc...
-            const createInvisibleElements = (snippetEls, isDescendant) => {
+            const createInvisibleElements = (snippetEls, isDescendant, parents) => {
                 return Promise.all((snippetEls).map(async (snippetEl) => {
                     const descendantSnippetEls = descendantPerSnippet.get(snippetEl);
                     // An element is considered as "RootParent" if it has one or
                     // more invisible descendants but is not a descendant.
                     const invisibleElement = await createInvisibleElement(snippetEl,
-                        !isDescendant && !!descendantSnippetEls, isDescendant);
+                        !isDescendant && !!descendantSnippetEls, isDescendant, parents);
                     if (descendantSnippetEls) {
-                        invisibleElement.children = await createInvisibleElements(descendantSnippetEls, true);
+                        invisibleElement.children = await createInvisibleElements(descendantSnippetEls, true, invisibleElement);
                     }
                     return invisibleElement;
                 }));
@@ -4324,15 +4325,30 @@ class SnippetsMenu extends Component {
      * @param {Event} ev
      */
     async onInvisibleEntryClick(invisibleEntry) {
-        const $snippet = $(invisibleEntry.snippetEl);
-        const isVisible = await this._execWithLoadingEffect(async () => {
-            const editor = await this._createSnippetEditor($snippet, true);
-            const show = editor.toggleTargetVisibility();
-            this._disableUndroppableSnippets();
-            return show;
-        }, true);
-        invisibleEntry.isVisible = isVisible;
-        return this._activateSnippet(isVisible ? $snippet : false);
+        const toggleVisibility = async (snippetEl) => {
+            const isVisible = await this._execWithLoadingEffect(async () => {
+                const editor = await this._createSnippetEditor($(snippetEl));
+                const show = editor.toggleTargetVisibility();
+                this._disableUndroppableSnippets();
+                return show;
+            }, true);
+            invisibleEntry.isVisible = isVisible;
+            this._activateSnippet(isVisible ? $(snippetEl) : false);
+        };
+
+        // Toggle all its descendants to invisible (Hide)
+        if (invisibleEntry.isVisible) {
+            invisibleEntry.children.forEach((child) => {
+                if (child.isVisible) {
+                    this.onInvisibleEntryClick(child);
+                }
+            });
+        } else if (invisibleEntry.parents && !invisibleEntry.parents.isVisible) {
+            // Toggle all its parents to visible (show)
+            this.onInvisibleEntryClick(invisibleEntry.parents);
+        }
+
+        await toggleVisibility(invisibleEntry.snippetEl);
     }
     /**
      * @private
