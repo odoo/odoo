christian-rb/odoo-dev

[FIX] web_editor: issue with image content type validation
Description:

Previously, valid image URLs were incorrectly flagged as unsupported. This was
due to a validation method based solely on file extensions. As a result, users
were unable to insert valid images into the editor. URLs not ending with an
image extension were assigned false src by [1] tested on [2] as they contained
an invalid MIME type.This issue arose from the system's reliance on file
extensions for validation,leading to the misidentification of valid image URLs.

This commit enhances the URL validation process on the client side. URLs are
now validated based on their content type. If the content is a valid image,
users can successfully insert the image via URL. If content type validation
fails, the system checks whether the URL loads an image when used as a source.
The URL is considered invalid for image insertion only if both validations fail

After this commit:

- A loading effect is displayed while the system checks the UR's validity.
- Users are now able to successfully insert images from valid URLs.

[1] : https://github.com/odoo/odoo/blob/17.0/addons/web_editor/models/ir_attachment.py#L41
[2] : https://www.odoo.com/web/image/6049005

task-3323862

addons/web_editor/static/src/components/media_dialog/image_selector.js
@@ -160,10 +160,28 @@ export class ImageSelector extends FileSelector {
         await this.uploadService.uploadFiles(files, { resModel: this.props.resModel, resId: this.props.resId, isImage: true }, (attachment) => this.onUploaded(attachment));
     }
 
-    validateUrl(...args) {
+    async validateUrl(...args) {
         const { isValidUrl, path } = super.validateUrl(...args);
-        const isValidFileFormat = IMAGE_EXTENSIONS.some(format => path.endsWith(format));
-        return { isValidFileFormat, isValidUrl };
+        const isValidFileFormat = isValidUrl && await new Promise(resolve => {
+            // Validate that the URL is an image.
+            fetch(path, { method: 'HEAD' })
+            .then(response => {
+                const contentType = response.headers.get('content-type');
+                resolve(contentType && IMAGE_MIMETYPES.includes(contentType));
+            })
+            .catch(() => {
+                // If the server hosting the image doesn't allow cross-origin
+                // requests, the fetch request will fail due to the browser's
+                // same-origin policy, even if the image URL is valid and the
+                // image exists. In that case we trade-off the check by creating
+                // an Image object and checking its onload and onerror events.
+                const img = new Image();
+                img.src = path;
+                img.onload = () => resolve(true);
+                img.onerror = () => resolve(false);
+            });
+        });
+        return { isValidUrl, isValidFileFormat };
     }
 
     isInitialMedia(attachment) {

odoo/odoo

addons/web_editor/static/src/components/media_dialog/image_selector.js
@@ -187,10 +187,27 @@ export class ImageSelector extends FileSelector {
         });
     }
 
-    validateUrl(...args) {
+    async validateUrl(...args) {
         const { isValidUrl, path } = super.validateUrl(...args);
-        const isValidFileFormat = IMAGE_EXTENSIONS.some(format => path.endsWith(format));
-        return { isValidFileFormat, isValidUrl };
+        const isValidFileFormat = isValidUrl && await new Promise(resolve => {
+            const img = new Image();
+            img.src = path;
+            img.onload = () => resolve(true);
+            img.onerror = async () => {
+                try {
+                    const response = await fetch(path, {method: 'HEAD'});
+                    if (response.ok) {
+                        const contentType = response.headers.get('content-type');
+                        resolve(contentType && IMAGE_EXTENSIONS.includes(contentType));
+                    } else {
+                        resolve(false)
+                    }
+                } catch {
+                    resolve(false);
+                }
+            }
+        });
+        return { isValidUrl, isValidFileFormat };
     }
 
     isInitialMedia(attachment) {