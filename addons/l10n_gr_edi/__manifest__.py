christian-rb/odoo-dev

[ADD] l10n_gr_mydata: Send Classification to MyDATA
MyDATA is a platform created by Greece's tax authority, The Independent
Authority for Public Revenue (IAPR), to digitize business tax and
accounting information declaration. All companies performing accounting,
according to Greek accounting standards, will be required to transmit
the information to the myDATA platform beginning in July of 2021.

- Customer Invoices

Customer Invoice form for Greece companies will now have a few fields
required for specifying classification data to be send to MyDATA.

In the Send & Print Wizard, a checkbox "Send to MyDATA"is added, and
when activated, Odoo will generate an XML of the selected invoice(s) and
send them to the MyDATA API.

Any errors detected before and after the send will then be saved in a
document object, displayed in the 'myDATA' tab in the move form view.

- Vendor Bills

In Greece, one's expense is another's invoice. When reporting to MyDATA
for our expense classification, we need to fetch the invoice(s) issue
by third-party issuer, fill the classification needed and send them
back to MyDATA.

To implement this behavior, we created a daily cron to fetch xml data
from MyDATA related to the designated Greece companies, and create draft
vendor bills based on that data. The user can then fill the myDATA
classification data needed and send them back to MyDATA via a button
in vendor bills view form after confirming the bill.

task-id: 3358316

addons/l10n_gr_edi/__manifest__.py
@@ -1,0 +1,36 @@
+{
+    'author': 'Odoo',
+    'name': 'Greece - MyDATA',
+    'version': '1.0',
+    'category': 'Accounting/Localizations',
+    'summary': """Connect to MyDATA API implementation for Greece""",
+    'description': """
+        MyDATA is a platform created by Greece's tax authority,
+        The Independent Authority for Public Revenue (IAPR),
+        to digitize business tax and accounting information declaration.
+    """,
+    'countries': ['gr'],
+    'depends': [
+        'account',
+        'l10n_gr',
+    ],
+    'data': [
+        'data/ir_cron.xml',
+        'data/template.xml',
+        'security/ir.model.access.csv',
+        'views/account_journal_views.xml',
+        'views/account_move_views.xml',
+        'views/mydata_document_views.xml',
+        'views/product_template_views.xml',
+        'views/res_config_settings_views.xml',
+        'wizard/account_move_send_views.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+    'assets': {
+        'web.assets_backend': [
+            'l10n_gr_edi/static/src/components/**/*',
+        ],
+    },
+}

addons/l10n_gr_edi/models/mydata_document.py
@@ -1,0 +1,143 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import requests
+
+from lxml import etree
+from odoo import models, fields, api, _
+from odoo.tools import cleanup_xml_node
+
+
+class MyDataDocument(models.Model):
+    _name = 'mydata.document'
+    _description = "Greece document object for tracking all sent XML to MyDATA"
+    _order = 'datetime DESC, id DESC'
+
+    state = fields.Selection(
+        selection=[('sent', 'Sent'), ('error', 'Error')],
+        string='MyDATA state',
+    )
+    attachment_id = fields.Binary(string='XML file')
+    datetime = fields.Datetime()
+    move_id = fields.Many2one('account.move', required=True)
+    message = fields.Char()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        documents = super().create(vals_list)
+
+        for document in documents:
+            # Automatically link document to move after creation
+            document.move_id.l10n_gr_edi_document_ids |= document
+            document.move_id.l10n_gr_edi_active_document_id = document
+
+        return documents
+
+    def _get_attachment_file_name(self):
+        self.ensure_one()
+        return f"{self.move_id.name.replace('/', '_')}_xml_{self.id}.xml"
+
+    def _generate_xml_content(self, xml_vals, send_classification=False):
+        xml_template = 'l10n_gr_edi.mydata_send_invoices' if not send_classification else \
+            'l10n_gr_edi.mydata_send_expense_classification'
+        xml_content = self.env['ir.qweb']._render(xml_template, xml_vals)
+        xml_content = etree.tostring(cleanup_xml_node(xml_content), encoding='ISO-8859-7', standalone='yes')
+        xml_content = xml_content.decode('iso8859_7')
+        return xml_content
+
+    def _generate_attachments_per_document(self, xml_vals, send_classification=False):
+        """ Isolate xml_vals to each individual invoices and generate attachment file for each document """
+        for document_index, invoice_vals in enumerate(xml_vals['invoices']):
+            single_xml_vals = {'invoices': [invoice_vals]}
+            xml_content = self._generate_xml_content(single_xml_vals, send_classification)
+
+            # Generate attachment file per document
+            document_id = self[document_index]
+            document_id.env['ir.attachment'].create({
+                'name': document_id._get_attachment_file_name(),
+                'raw': xml_content,
+                'mimetype': 'application/xml',
+                'res_model': document_id._name,
+                'res_id': document_id.id,
+                'res_field': 'attachment_id',
+            })
+            document_id.invalidate_recordset(fnames=['attachment_id'])
+
+    def _request_to_mydata(self, xml_content, send_classification=False):
+        """ Make a POST request to MyDATA API """
+        endpoint = 'sendexpensesclassification' if send_classification else 'sendinvoices'
+
+        # Record the request time in the datetime field
+        self.datetime = fields.Datetime.now()
+
+        try:
+            # Send XML to MyDATA API
+            response = requests.post(
+                url=self.move_id.company_id._l10n_gr_edi_get_mydata_url(endpoint),
+                data=xml_content,
+                headers=self.move_id.company_id._l10n_gr_edi_get_headers_credentials())
+        except ConnectionError as err:
+            # Handle any connection errors
+            self.state = 'error'
+            self.message = str(err)
+            return
+
+        return response
+
+    def _handle_response_xml(self, response):
+        """ Handle XML response and update document's state accordingly """
+        if not response:
+            # In case of status 500 (problem from the server)
+            self.state = 'error'
+            self.message = _('No response from MyDATA, please try again later.')
+            return
+
+        root = etree.fromstring(response.content)
+        pretty_xml = etree.tostring(root, encoding='unicode', pretty_print=True)
+        print(pretty_xml)  # todo remove - monkey testing
+
+        # Handle success/error response per document
+        for response_element in root.xpath('//response'):
+            response_index = int(response_element.findtext('index'))
+            document_id = self[response_index - 1]
+            status_code = response_element.findtext('statusCode')
+
+            if status_code == 'Success':
+                document_id.state = 'sent'
+                document_id.message = False
+                document_id.move_id.l10n_gr_edi_mark = response_element.findtext('invoiceMark')
+                # Delete all previous error documents and keep the latest successfully sent document
+                document_id.move_id.l10n_gr_edi_document_ids.filtered(lambda d: d.state != 'sent').unlink()
+            else:
+                document_id.state = 'error'
+                error_elements = response_element.xpath('./errors/error')
+                errors = (f"[{element.findtext('code')}] {element.findtext('message')}." for element in error_elements)
+                document_id.message = '\n'.join(errors)
+
+    def _send_mydata_invoices_xml(self, xml_vals):
+        """ Send Customer Invoice XML batches to MyDATA. """
+        self._generate_attachments_per_document(xml_vals)
+
+        xml_content = self._generate_xml_content(xml_vals)
+        print(xml_content)
+
+        response = self._request_to_mydata(xml_content)
+
+        self._handle_response_xml(response)
+
+    def _send_mydata_expense_classifications_xml(self, xml_vals):
+        """ Send Vendor Bill Expense Classification XML batches to MyDATA. """
+        self._generate_attachments_per_document(xml_vals, send_classification=True)
+
+        xml_content = self._generate_xml_content(xml_vals, send_classification=True)
+        print(xml_content)
+
+        response = self._request_to_mydata(xml_content, send_classification=True)
+
+        self._handle_response_xml(response)
+
+    def action_download(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'/web/content/mydata.document/{self.id}/attachment_id/{self._get_attachment_file_name()}?download=true',
+        }

odoo/odoo

addons/l10n_gr_edi/__manifest__.py
@@ -1,0 +1,38 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'author': 'Odoo',
+    'name': 'Greece - MyDATA',
+    'version': '1.0',
+    'category': 'Accounting/Localizations',
+    'summary': """Connect to MyDATA API implementation for Greece""",
+    'description': """
+        MyDATA is a platform created by Greece's tax authority,
+        The Independent Authority for Public Revenue (IAPR),
+        to digitize business tax and accounting information declaration.
+    """,
+    'countries': ['gr'],
+    'depends': [
+        'account',
+        'l10n_gr',
+    ],
+    'data': [
+        'data/ir_cron.xml',
+        'data/template.xml',
+        'security/ir.model.access.csv',
+        'views/account_fiscal_position_views.xml',
+        'views/account_journal_views.xml',
+        'views/account_move_views.xml',
+        'views/account_tax_views.xml',
+        'views/mydata_document_views.xml',
+        'views/product_template_views.xml',
+        'views/res_company_views.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+    'assets': {
+        'web.assets_backend': [
+            'l10n_gr_edi/static/src/components/**/*',
+        ],
+    },
+}

addons/l10n_gr_edi/models/mydata_document.py
@@ -1,0 +1,144 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import requests
+
+from lxml import etree
+from odoo import models, fields, api, _
+from odoo.tools import cleanup_xml_node
+
+
+class GreeceEDIDocument(models.Model):
+    _name = 'l10n_gr_edi.document'
+    _description = "Greece document object for tracking all sent XML to MyDATA"
+    _order = 'datetime DESC, id DESC'
+
+    state = fields.Selection(
+        selection=[('sent', 'Sent'), ('error', 'Error')],
+        string='MyDATA Status',
+    )
+    attachment_id = fields.Binary(string='XML file')
+    datetime = fields.Datetime()
+    move_id = fields.Many2one('account.move', required=True)
+    message = fields.Char()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        documents = super().create(vals_list)
+
+        for document in documents:
+            # Automatically link document to move after creation
+            document.move_id.l10n_gr_edi_document_ids |= document
+            document.move_id.l10n_gr_edi_active_document_id = document
+
+        return documents
+
+    def _get_attachment_file_name(self):
+        self.ensure_one()
+        return f"{self.move_id.name.replace('/', '_')}_xml_{self.id}.xml"
+
+    def _generate_xml_content(self, xml_vals, send_classification=False):
+        xml_template = 'l10n_gr_edi.l10n_gr_edi_send_invoices' if not send_classification else \
+            'l10n_gr_edi.l10n_gr_edi_send_expense_classification'
+        xml_content = self.env['ir.qweb']._render(xml_template, xml_vals)
+        xml_content = etree.tostring(cleanup_xml_node(xml_content), encoding='ISO-8859-7', standalone='yes')
+        xml_content = xml_content.decode('iso8859_7')
+        return xml_content
+
+    def _generate_attachments_per_document(self, xml_vals, send_classification=False):
+        """ Isolate xml_vals to each individual invoices and generate attachment file for each document """
+        for document_index, invoice_vals in enumerate(xml_vals['invoices']):
+            single_xml_vals = {'invoices': [invoice_vals]}
+            xml_content = self._generate_xml_content(single_xml_vals, send_classification)
+
+            # Generate attachment file per document
+            document_id = self[document_index]
+            document_id.env['ir.attachment'].create({
+                'name': document_id._get_attachment_file_name(),
+                'raw': xml_content,
+                'mimetype': 'application/xml',
+                'res_model': document_id._name,
+                'res_id': document_id.id,
+                'res_field': 'attachment_id',
+            })
+            document_id.invalidate_recordset(fnames=['attachment_id'])
+
+    def _request_to_mydata(self, xml_content, send_classification=False):
+        """ Make a POST request to MyDATA API """
+        endpoint = 'sendexpensesclassification' if send_classification else 'sendinvoices'
+
+        # Record the request time in the datetime field
+        self.datetime = fields.Datetime.now()
+
+        try:
+            # Send XML to MyDATA API
+            response = requests.post(
+                url=self.move_id.company_id._l10n_gr_edi_get_mydata_url(endpoint),
+                data=xml_content,
+                timeout=5,
+                headers=self.move_id.company_id._l10n_gr_edi_get_headers_credentials())
+        except ConnectionError as err:
+            # Handle any connection errors
+            self.state = 'error'
+            self.message = str(err)
+            return
+
+        return response
+
+    def _handle_response_xml(self, response):
+        """ Handle XML response and update document's state accordingly """
+        if not response:
+            # In case of status 500 (problem from the server)
+            self.state = 'error'
+            self.message = _('No response from MyDATA, please try again later.')
+            return
+
+        root = etree.fromstring(response.content)
+        pretty_xml = etree.tostring(root, encoding='unicode', pretty_print=True)
+        print(pretty_xml)  # todo remove - monkey testing
+
+        # Handle success/error response per document
+        for response_element in root.xpath('//response'):
+            response_index = int(response_element.findtext('index'))
+            document_id = self[response_index - 1]
+            status_code = response_element.findtext('statusCode')
+
+            if status_code == 'Success':
+                document_id.state = 'sent'
+                document_id.message = False
+                document_id.move_id.l10n_gr_edi_mark = response_element.findtext('invoiceMark')
+                # Delete all previous error documents and keep the latest successfully sent document
+                document_id.move_id.l10n_gr_edi_document_ids.filtered(lambda d: d.state != 'sent').unlink()
+            else:
+                document_id.state = 'error'
+                error_elements = response_element.xpath('./errors/error')
+                errors = (f"[{element.findtext('code')}] {element.findtext('message')}." for element in error_elements)
+                document_id.message = '\n'.join(errors)
+
+    def _send_mydata_invoices_xml(self, xml_vals):
+        """ Send Customer Invoice XML batches to MyDATA. """
+        self._generate_attachments_per_document(xml_vals)
+
+        xml_content = self._generate_xml_content(xml_vals)
+        print(xml_content)
+
+        response = self._request_to_mydata(xml_content)
+
+        self._handle_response_xml(response)
+
+    def _send_mydata_expense_classifications_xml(self, xml_vals):
+        """ Send Vendor Bill Expense Classification XML batches to MyDATA. """
+        self._generate_attachments_per_document(xml_vals, send_classification=True)
+
+        xml_content = self._generate_xml_content(xml_vals, send_classification=True)
+        print(xml_content)
+
+        response = self._request_to_mydata(xml_content, send_classification=True)
+
+        self._handle_response_xml(response)
+
+    def action_download(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'/web/content/l10n_gr_edi.document/{self.id}/attachment_id/{self._get_attachment_file_name()}?download=true',
+        }