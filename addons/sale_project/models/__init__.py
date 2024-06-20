97f471121eacdad3040d037ea9e930228cec7dcd
rajh-odoo <rajh@odoo.com>
2024-06-20 18:54:41 +0530

    [IMP] project, _*:  improve the project update form view description
    _*: sale_timesheet, sale_project
    
    Currently, profitability isn't displayed in the project update description
    form without the sale_timesheet module. In this commit, we have improved
    the project description according the project side panel.
    
    task-3916169

--- addons/sale_project/models/__init__.py
+++ addons/sale_project/models/__init__.py
@@ -6,6 +6,7 @@ from . import project_milestone
 from . import project_project
 from . import project_task_recurrence
 from . import project_task
+from . import project_update
 from . import res_config_settings
 from . import sale_order_line
 from . import sale_order_template_line
