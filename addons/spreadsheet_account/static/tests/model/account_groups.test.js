ac6c2c2b8f2a2bb64bdbc2ce5c454ed82999c136
Adrien Minne (adrm) <adrm@odoo.com>
2024-06-21 12:07:01 +0200

    [REF] spreadsheet_account: convert tests to Hoot
    closes odoo/odoo#170316
    
    Task: 4004998
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- addons/spreadsheet_account/static/tests/model/account_groups.test.js
+++ addons/spreadsheet_account/static/tests/model/account_groups.test.js
@@ -1,36 +1,37 @@
-/** @odoo-module */
-
-import { setCellContent } from "@spreadsheet/../tests/legacy/utils/commands";
-import { createModelWithDataSource } from "@spreadsheet/../tests/legacy/utils/model";
-import { getCellValue } from "@spreadsheet/../tests/legacy/utils/getters";
-import { getAccountingData } from "@spreadsheet_account/../tests/legacy/accounting_test_data";
+import { defineSpreadsheetModels } from "@spreadsheet/../tests/helpers/data";
+import { describe, expect, test } from "@odoo/hoot";
+import { setCellContent } from "@spreadsheet/../tests/helpers/commands";
+import { createModelWithDataSource } from "@spreadsheet/../tests/helpers/model";
+import { getCellValue } from "@spreadsheet/../tests/helpers/getters";
+import {
+    getAccountingData,
+    defineSpreadsheetAccountModels,
+} from "@spreadsheet_account/../tests/accounting_test_data";
 import { waitForDataLoaded } from "@spreadsheet/helpers/model";
 
-let serverData;
+describe.current.tags("headless");
+defineSpreadsheetModels();
+defineSpreadsheetAccountModels();
 
-function beforeEach() {
-    serverData = getAccountingData();
-}
+const serverData = getAccountingData();
 
-QUnit.module("spreadsheet_account > account groups", { beforeEach }, () => {
-    QUnit.test("get no account", async (assert) => {
-        const model = await createModelWithDataSource({ serverData });
-        setCellContent(model, "A1", `=ODOO.ACCOUNT.GROUP("test")`);
-        await waitForDataLoaded(model);
-        assert.equal(getCellValue(model, "A1"), "");
-    });
+test("get no account", async () => {
+    const model = await createModelWithDataSource({ serverData });
+    setCellContent(model, "A1", `=ODOO.ACCOUNT.GROUP("test")`);
+    await waitForDataLoaded(model);
+    expect(getCellValue(model, "A1")).toBe("");
+});
 
-    QUnit.test("get one account", async (assert) => {
-        const model = await createModelWithDataSource({ serverData });
-        setCellContent(model, "A1", `=ODOO.ACCOUNT.GROUP("income_other")`);
-        await waitForDataLoaded(model);
-        assert.equal(getCellValue(model, "A1"), "100105");
-    });
+test("get one account", async () => {
+    const model = await createModelWithDataSource({ serverData });
+    setCellContent(model, "A1", `=ODOO.ACCOUNT.GROUP("income_other")`);
+    await waitForDataLoaded(model);
+    expect(getCellValue(model, "A1")).toBe("100105");
+});
 
-    QUnit.test("get multiple accounts", async (assert) => {
-        const model = await createModelWithDataSource({ serverData });
-        setCellContent(model, "A1", `=ODOO.ACCOUNT.GROUP("income")`);
-        await waitForDataLoaded(model);
-        assert.equal(getCellValue(model, "A1"), "100104,200104");
-    });
+test("get multiple accounts", async () => {
+    const model = await createModelWithDataSource({ serverData });
+    setCellContent(model, "A1", `=ODOO.ACCOUNT.GROUP("income")`);
+    await waitForDataLoaded(model);
+    expect(getCellValue(model, "A1")).toBe("100104,200104");
 });
