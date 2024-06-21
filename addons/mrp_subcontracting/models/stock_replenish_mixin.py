c9a5b9a835ccb6206a7b72bd5dc4ce82797cf1e0
aamo-odoo <aamo@odoo.com>
2024-06-21 12:20:20 +0530

    [FIX] mrp_subcontracting: prevent error when Replenish product
    This error occurs when we delete the specific 'Resupply Subcontractor on Order'
    route and subsequently attempt to replenish the product.
    
    Steps to reproduce:
    - Install the 'stock' and 'mrp_subcontracting' module
    - Inventory > Configuration > Settings > Warehouse > Activate Multi-Step Routes
    - Go to routes and delete 'Resupply Subcontractor on Order'
    - Now go to any product > Click on 'Replenish'
    
    Traceback: AttributeError
    'NoneType' object has no attribute 'id'
    
    At [1], this error occurs when we try to access an attribute called 'id' on an
    object that is actually None.
    
    This commit will fix the above error by returning the value of 'domains' if the
    route is not present in the '_get_allowed_route_domain' method.
    
    [1]: https://github.com/odoo/odoo/blob/731766aa03ad4cf14349169faab3957c710bc002/addons/mrp_subcontracting/wizard/product_replenish.py#L13
    
    sentry-5514962307
    
    closes odoo/odoo#173001
    
    X-original-commit: fe5f6be97560e4529b922169cd43bc176a354f2b
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>
    Signed-off-by: Aayush Modi (aamo) <aamo@odoo.com>

--- addons/mrp_subcontracting/models/stock_replenish_mixin.py
+++ addons/mrp_subcontracting/models/stock_replenish_mixin.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import models, _
 from odoo.osv import expression
 
 
@@ -9,4 +9,5 @@ class ProductReplenishMixin(models.AbstractModel):
 
     def _get_allowed_route_domain(self):
         domains = super()._get_allowed_route_domain()
-        return expression.AND([domains, [('id', '!=', self.env.ref('mrp_subcontracting.route_resupply_subcontractor_mto', raise_if_not_found=False).id)]])
+        route_id = self.env['stock.warehouse']._find_or_create_global_route('mrp_subcontracting.route_resupply_subcontractor_mto', _('Resupply Subcontractor on Order')).id
+        return expression.AND([domains, [('id', '!=', route_id)]])
