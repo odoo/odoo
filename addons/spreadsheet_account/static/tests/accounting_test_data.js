553016c7db3d0b2ebad343ae66a703a8a91bdb4e
Adrien Minne (adrm) <adrm@odoo.com>
2024-06-21 12:07:01 +0200

    [REF] spreadsheet_account: convert tests to Hoot
    closes odoo/odoo#170316
    
    Task: 4004998
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- addons/spreadsheet_account/static/tests/accounting_test_data.js
+++ addons/spreadsheet_account/static/tests/accounting_test_data.js
@@ -1,33 +1,50 @@
-/** @odoo-module */
+import { getBasicData } from "@spreadsheet/../tests/helpers/data";
+import { defineModels, fields, models } from "@web/../tests/web_test_helpers";
 
-import { getBasicData } from "@spreadsheet/../tests/legacy/utils/data";
+export class AccountMoveLine extends models.Model {
+    _name = "account.move.line";
+
+    account_id = fields.Many2one({ relation: "account.account" });
+    date = fields.Date({ string: "Date" });
+    name = fields.Char({ string: "Name" });
+
+    _records = [
+        { id: 1, name: "line1", account_id: 1, date: "2022-06-01" },
+        { id: 2, name: "line2", account_id: 2, date: "2022-06-23" },
+    ];
+}
+
+export class AccountAccount extends models.Model {
+    _name = "account.account";
+
+    code = fields.Char({ string: "Code" });
+    account_type = fields.Char({ string: "Account type" });
+
+    spreadsheet_fetch_debit_credit(args) {
+        return new Array(args.length).fill({ credit: 0, debit: 0 });
+    }
+
+    get_account_group(accountTypes) {
+        const data = accountTypes.map((accountType) => {
+            const records = this.env["account.account"].search_read(
+                [["account_type", "=", accountType]],
+                ["code"]
+            );
+            return records.map((record) => record.code);
+        });
+        return data;
+    }
+
+    _records = [
+        { id: 1, code: "100104", account_type: "income" },
+        { id: 2, code: "100105", account_type: "income_other" },
+        { id: 3, code: "200104", account_type: "income" },
+    ];
+}
 
 export function getAccountingData() {
     return {
-        models: {
-            ...getBasicData(),
-            "account.move.line": {
-                fields: {
-                    account_id: { type: "many2one", relation: "account.account" },
-                    date: { string: "Date", type: "date" },
-                },
-                records: [
-                    { id: 1, name: "line1", account_id: 1, date: "2022-06-01" },
-                    { id: 2, name: "line2", account_id: 2, date: "2022-06-23" },
-                ],
-            },
-            "account.account": {
-                fields: {
-                    code: { string: "Code", type: "string" },
-                    account_type: { string: "Account type", type: "string" },
-                },
-                records: [
-                    { id: 1, code: "100104", account_type: "income" },
-                    { id: 2, code: "100105", account_type: "income_other" },
-                    { id: 3, code: "200104", account_type: "income" },
-                ],
-            },
-        },
+        models: { ...getBasicData() },
         views: {
             "account.move.line,false,list": /* xml */ `
                     <tree string="Move Lines">
@@ -39,4 +56,8 @@ export function getAccountingData() {
             "account.move.line,false,search": /* xml */ `<search/>`,
         },
     };
+}
+
+export function defineSpreadsheetAccountModels() {
+    defineModels([AccountMoveLine, AccountAccount]);
 }
--- addons/spreadsheet_account/static/tests/model/fiscal_year.test.js
+++ addons/spreadsheet_account/static/tests/model/fiscal_year.test.js
@@ -1,127 +1,126 @@
-/** @odoo-module */
-import { setCellContent } from "@spreadsheet/../tests/legacy/utils/commands";
-import { createModelWithDataSource } from "@spreadsheet/../tests/legacy/utils/model";
-import { getEvaluatedCell } from "@spreadsheet/../tests/legacy/utils/getters";
+import { defineSpreadsheetModels } from "@spreadsheet/../tests/helpers/data";
+import { describe, expect, test } from "@odoo/hoot";
+import { setCellContent } from "@spreadsheet/../tests/helpers/commands";
+import { createModelWithDataSource } from "@spreadsheet/../tests/helpers/model";
+import { getEvaluatedCell } from "@spreadsheet/../tests/helpers/getters";
 import "@spreadsheet_account/index";
 
 import * as spreadsheet from "@odoo/o-spreadsheet";
 import { waitForDataLoaded } from "@spreadsheet/helpers/model";
+
+describe.current.tags("headless");
+defineSpreadsheetModels();
+
 const { DEFAULT_LOCALE } = spreadsheet.constants;
 
-QUnit.module("spreadsheet_account > fiscal year", {}, () => {
-    QUnit.test("Basic evaluation", async (assert) => {
-        const model = await createModelWithDataSource({
-            mockRPC: async function (route, args) {
-                if (args.method === "get_fiscal_dates") {
-                    assert.step("get_fiscal_dates");
-                    assert.deepEqual(args.args, [
-                        [
-                            {
-                                date: "2020-11-11",
-                                company_id: null,
-                            },
-                        ],
-                    ]);
-                    return [{ start: "2020-01-01", end: "2020-12-31" }];
-                }
-            },
-        });
-        setCellContent(model, "A1", `=ODOO.FISCALYEAR.START("11/11/2020")`);
-        setCellContent(model, "A2", `=ODOO.FISCALYEAR.END("11/11/2020")`);
-        await waitForDataLoaded(model);
-        assert.verifySteps(["get_fiscal_dates"]);
-        assert.equal(getEvaluatedCell(model, "A1").formattedValue, "1/1/2020");
-        assert.equal(getEvaluatedCell(model, "A2").formattedValue, "12/31/2020");
+test("Basic evaluation", async () => {
+    const model = await createModelWithDataSource({
+        mockRPC: async function (route, args) {
+            if (args.method === "get_fiscal_dates") {
+                expect.step("get_fiscal_dates");
+                expect(args.args).toEqual([
+                    [
+                        {
+                            date: "2020-11-11",
+                            company_id: null,
+                        },
+                    ],
+                ]);
+                return [{ start: "2020-01-01", end: "2020-12-31" }];
+            }
+        },
     });
+    setCellContent(model, "A1", `=ODOO.FISCALYEAR.START("11/11/2020")`);
+    setCellContent(model, "A2", `=ODOO.FISCALYEAR.END("11/11/2020")`);
+    await waitForDataLoaded(model);
+    expect(["get_fiscal_dates"]).toVerifySteps();
+    expect(getEvaluatedCell(model, "A1").formattedValue).toBe("1/1/2020");
+    expect(getEvaluatedCell(model, "A2").formattedValue).toBe("12/31/2020");
+});
 
-    QUnit.test("with a given company id", async (assert) => {
-        const model = await createModelWithDataSource({
-            mockRPC: async function (route, args) {
-                if (args.method === "get_fiscal_dates") {
-                    assert.step("get_fiscal_dates");
-                    assert.deepEqual(args.args, [
-                        [
-                            {
-                                date: "2020-11-11",
-                                company_id: 1,
-                            },
-                        ],
-                    ]);
-                    return [{ start: "2020-01-01", end: "2020-12-31" }];
-                }
-            },
-        });
-        setCellContent(model, "A1", `=ODOO.FISCALYEAR.START("11/11/2020", 1)`);
-        setCellContent(model, "A2", `=ODOO.FISCALYEAR.END("11/11/2020", 1)`);
-        await waitForDataLoaded(model);
-        assert.verifySteps(["get_fiscal_dates"]);
-        assert.equal(getEvaluatedCell(model, "A1").formattedValue, "1/1/2020");
-        assert.equal(getEvaluatedCell(model, "A2").formattedValue, "12/31/2020");
+test("with a given company id", async () => {
+    const model = await createModelWithDataSource({
+        mockRPC: async function (route, args) {
+            if (args.method === "get_fiscal_dates") {
+                expect.step("get_fiscal_dates");
+                expect(args.args).toEqual([
+                    [
+                        {
+                            date: "2020-11-11",
+                            company_id: 1,
+                        },
+                    ],
+                ]);
+                return [{ start: "2020-01-01", end: "2020-12-31" }];
+            }
+        },
     });
+    setCellContent(model, "A1", `=ODOO.FISCALYEAR.START("11/11/2020", 1)`);
+    setCellContent(model, "A2", `=ODOO.FISCALYEAR.END("11/11/2020", 1)`);
+    await waitForDataLoaded(model);
+    expect(["get_fiscal_dates"]).toVerifySteps();
+    expect(getEvaluatedCell(model, "A1").formattedValue).toBe("1/1/2020");
+    expect(getEvaluatedCell(model, "A2").formattedValue).toBe("12/31/2020");
+});
 
-    QUnit.test("with a wrong company id", async (assert) => {
-        const model = await createModelWithDataSource({
-            mockRPC: async function (route, args) {
-                if (args.method === "get_fiscal_dates") {
-                    assert.step("get_fiscal_dates");
-                    assert.deepEqual(args.args, [
-                        [
-                            {
-                                date: "2020-11-11",
-                                company_id: 100,
-                            },
-                        ],
-                    ]);
-                    return [false];
-                }
-            },
-        });
-        setCellContent(model, "A1", `=ODOO.FISCALYEAR.START("11/11/2020", 100)`);
-        setCellContent(model, "A2", `=ODOO.FISCALYEAR.END("11/11/2020", 100)`);
-        await waitForDataLoaded(model);
-        assert.verifySteps(["get_fiscal_dates"]);
-        assert.equal(
-            getEvaluatedCell(model, "A1").message,
-            "The company fiscal year could not be found."
-        );
-        assert.equal(
-            getEvaluatedCell(model, "A2").message,
-            "The company fiscal year could not be found."
-        );
+test("with a wrong company id", async () => {
+    const model = await createModelWithDataSource({
+        mockRPC: async function (route, args) {
+            if (args.method === "get_fiscal_dates") {
+                expect.step("get_fiscal_dates");
+                expect(args.args).toEqual([
+                    [
+                        {
+                            date: "2020-11-11",
+                            company_id: 100,
+                        },
+                    ],
+                ]);
+                return [false];
+            }
+        },
     });
+    setCellContent(model, "A1", `=ODOO.FISCALYEAR.START("11/11/2020", 100)`);
+    setCellContent(model, "A2", `=ODOO.FISCALYEAR.END("11/11/2020", 100)`);
+    await waitForDataLoaded(model);
+    expect(["get_fiscal_dates"]).toVerifySteps();
+    expect(getEvaluatedCell(model, "A1").message).toBe(
+        "The company fiscal year could not be found."
+    );
+    expect(getEvaluatedCell(model, "A2").message).toBe(
+        "The company fiscal year could not be found."
+    );
+});
 
-    QUnit.test("with wrong input arguments", async (assert) => {
-        const model = await createModelWithDataSource();
-        setCellContent(model, "A1", `=ODOO.FISCALYEAR.START("not a number")`);
-        setCellContent(model, "A2", `=ODOO.FISCALYEAR.END("11/11/2020", "not a number")`);
-        assert.equal(
-            getEvaluatedCell(model, "A1").message,
-            "The function ODOO.FISCALYEAR.START expects a number value, but 'not a number' is a string, and cannot be coerced to a number."
-        );
-        assert.equal(
-            getEvaluatedCell(model, "A2").message,
-            "The function ODOO.FISCALYEAR.END expects a number value, but 'not a number' is a string, and cannot be coerced to a number."
-        );
+test("with wrong input arguments", async () => {
+    const model = await createModelWithDataSource();
+    setCellContent(model, "A1", `=ODOO.FISCALYEAR.START("not a number")`);
+    setCellContent(model, "A2", `=ODOO.FISCALYEAR.END("11/11/2020", "not a number")`);
+    expect(getEvaluatedCell(model, "A1").message).toBe(
+        "The function ODOO.FISCALYEAR.START expects a number value, but 'not a number' is a string, and cannot be coerced to a number."
+    );
+    expect(getEvaluatedCell(model, "A2").message).toBe(
+        "The function ODOO.FISCALYEAR.END expects a number value, but 'not a number' is a string, and cannot be coerced to a number."
+    );
+});
+
+test("Date format is locale dependant", async () => {
+    const model = await createModelWithDataSource({
+        mockRPC: async function (route, args) {
+            if (args.method === "get_fiscal_dates") {
+                return [{ start: "2020-01-01", end: "2020-12-31" }];
+            }
+        },
     });
+    setCellContent(model, "A1", `=ODOO.FISCALYEAR.START("11/11/2020", 1)`);
+    setCellContent(model, "A2", `=ODOO.FISCALYEAR.END("11/11/2020", 1)`);
+    await waitForDataLoaded(model);
 
-    QUnit.test("Date format is locale dependant", async (assert) => {
-        const model = await createModelWithDataSource({
-            mockRPC: async function (route, args) {
-                if (args.method === "get_fiscal_dates") {
-                    return [{ start: "2020-01-01", end: "2020-12-31" }];
-                }
-            },
-        });
-        setCellContent(model, "A1", `=ODOO.FISCALYEAR.START("11/11/2020", 1)`);
-        setCellContent(model, "A2", `=ODOO.FISCALYEAR.END("11/11/2020", 1)`);
-        await waitForDataLoaded(model);
+    expect(getEvaluatedCell(model, "A1").format).toBe("m/d/yyyy");
+    expect(getEvaluatedCell(model, "A2").format).toBe("m/d/yyyy");
 
-        assert.equal(getEvaluatedCell(model, "A1").format, "m/d/yyyy");
-        assert.equal(getEvaluatedCell(model, "A2").format, "m/d/yyyy");
+    model.dispatch("UPDATE_LOCALE", { locale: { ...DEFAULT_LOCALE, dateFormat: "d/m/yyyy" } });
 
-        model.dispatch("UPDATE_LOCALE", { locale: { ...DEFAULT_LOCALE, dateFormat: "d/m/yyyy" } });
-
-        assert.equal(getEvaluatedCell(model, "A1").format, "d/m/yyyy");
-        assert.equal(getEvaluatedCell(model, "A2").format, "d/m/yyyy");
-    });
+    expect(getEvaluatedCell(model, "A1").format).toBe("d/m/yyyy");
+    expect(getEvaluatedCell(model, "A2").format).toBe("d/m/yyyy");
 });
