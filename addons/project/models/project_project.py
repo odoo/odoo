459032d031cc5df8c213af2e3b60ad4f0ce28f50
suth-odoo <suth@odoo.com>
2024-06-21 19:08:41 +0530

    [FIX] project: fix traceback when sharing project in SO
    After this commit:
    fixes the traceback issue when sharing the project from SO.
    the issue due to the wrong active_model and active_id,
    by passing the active_model, active_id in 'action_open_share_project_wizard'
    the issue will be fixed.
    
    task-3998576
    
    closes odoo/odoo#172137
    
    X-original-commit: 32c083f09af0438deb6e23992555bc080f91e23d
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/models/project_project.py
+++ addons/project/models/project_project.py
@@ -664,6 +664,8 @@ class Project(models.Model):
         local_context = self.env.context | {
             'default_template_id': template.id if template else False,
             'default_email_layout_xmlid': 'mail.mail_notification_light',
+            'active_id': self.id,
+            'active_model': 'project.project',
         }
         action = self.env["ir.actions.actions"]._for_xml_id("project.project_share_wizard_action")
         if self.env.context.get('default_access_mode'):
