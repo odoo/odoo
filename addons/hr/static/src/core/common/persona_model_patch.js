d4fc350c18713a2a6e134206781383daf1ea16f0
Codie Petersen <petersen.codie@gmail.com>
2024-06-23 14:57:22 -0700

    Removed HR.

--- addons/hr/static/src/core/common/persona_model_patch.js
+++
@@ -1,9 +0,0 @@
-/* @odoo-module */
-
-import { Persona } from "@mail/core/common/persona_model";
-
-import { patch } from "@web/core/utils/patch";
-
-patch(Persona.prototype, {
-    employeeId: undefined,
-});
--- addons/hr/static/tests/disable_patch.js
+++
@@ -1,5 +0,0 @@
-/** @odoo-module */
-
-import { unpatchAvatarCardPopover } from "@hr/components/avatar_card/avatar_card_popover_patch";
-
-unpatchAvatarCardPopover();
