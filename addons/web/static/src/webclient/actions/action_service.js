dca146b4dd55003725c1d14d6d623f2e4b821d79
Jorge Pinna Puissant <jpp@odoo.com>
2024-06-24 12:38:49 +0200

    [FIX] web: avoid updating controllers when target is "new"
    - Open the Employees app;
    - Open the record of an employee, that have the onbording plan link in
        the chatter (for instance Abigail Peterson in runbot);
    - Click on the onbording plan;
    - Close the onbording plan dialog;
    - Click on the Employees link in the breadcrumb to came back to the
        employees multi-record view;
    
    Before this commit, a traceback was raised : `Invalid controller to
    restore`. This issue occurs because when we load from a link/URL
    (loadState), the controllers list is updated from the actions found in
    the URL, this is done to be able to recreate the controllers (the
    breadcrumb) from the URL.
    This behavior is correct when the action has a target other than “new”
    (i.e., when it's not a dialog). In the case of a target "new" (a dialog)
    the controllers shouldn't be modified, to be able to interact with the
    breadcrumb when the dialog is closed.
    
    Now, only when the action has a target different from "new", the
    controllers are updated from the URL.
    
    X-original-commit: 74e5dc0a4cbd13162b142582505e0d123ba1f659

--- addons/web/static/src/webclient/actions/action_service.js
+++ addons/web/static/src/webclient/actions/action_service.js
@@ -784,6 +784,9 @@ export function makeActionManager(env, router = _router) {
             reject = _rej;
         });
         const action = controller.action;
+        if (action.target !== "new" && "newStack" in options) {
+            controllerStack = options.newStack;
+        }
         const index = _computeStackIndex(options);
         const nextStack = [...controllerStack.slice(0, index), controller];
         // Compute breadcrumbs
@@ -1104,14 +1107,7 @@ export function makeActionManager(env, router = _router) {
         };
         action.controllers[view.type] = controller;
 
-        return _updateUI(controller, {
-            index: options.index,
-            clearBreadcrumbs: options.clearBreadcrumbs,
-            onClose: options.onClose,
-            stackPosition: options.stackPosition,
-            onActionReady: options.onActionReady,
-            noEmptyTransition: options.noEmptyTransition,
-        });
+        return _updateUI(controller, options);
     }
 
     /**
@@ -1156,15 +1152,7 @@ export function makeActionManager(env, router = _router) {
                 ..._getActionInfo(action, options.props),
             };
             controller.displayName ||= clientAction.displayName?.toString() || "";
-            const updateUIOptions = {
-                index: options.index,
-                clearBreadcrumbs: options.clearBreadcrumbs,
-                stackPosition: options.stackPosition,
-                onClose: options.onClose,
-                onActionReady: options.onActionReady,
-                noEmptyTransition: options.noEmptyTransition,
-            };
-            return _updateUI(controller, updateUIOptions);
+            return _updateUI(controller, options);
         } else {
             const next = await clientAction(env, action);
             if (next) {
@@ -1195,13 +1183,7 @@ export function makeActionManager(env, router = _router) {
             ..._getActionInfo(action, props),
         };
 
-        const updateUIOptions = {
-            clearBreadcrumbs: options.clearBreadcrumbs,
-            stackPosition: options.stackPosition,
-            onClose: options.onClose,
-            index: options.index,
-        };
-        return _updateUI(controller, updateUIOptions);
+        return _updateUI(controller, options);
     }
 
     /**
@@ -1536,11 +1518,12 @@ export function makeActionManager(env, router = _router) {
      * @returns {Promise<boolean>} true if doAction was performed
      */
     async function loadState() {
-        controllerStack = await _controllersFromState();
+        const newStack = await _controllersFromState();
         const actionParams = _getActionParams();
         if (actionParams) {
             // Params valid => performs a "doAction"
             const { actionRequest, options } = actionParams;
+            options.newStack = newStack;
             await doAction(actionRequest, options);
             return true;
         }
