20da7d137c048141de89f888a66ce7cbb6765672
Abdelouahab (abla) <abla@odoo.com>
2024-06-24 08:20:39 +0000

    [FIX] project: allow portal user to add image in task description
    To reproduce:
    =============
    - share a project with a portal user
    - connect as the portal user
    - on a task paste an image in the description
    - save -> AccessError
    
    Problem:
    ========
    Portal user doesn't have the right to create attachments
    
    Solution:
    =========
    - when saving the record, editor tries to save the image as an attachment
    by `POST` request on the route `/web_editor/attachment/add_image`
    
    - `/web_editor/attachment/add_image` is not dedicated to portal user,
    so the save method is patched to modify the route to custom one
    `/project_sharing/attachment/add_image`
    
    opw-3774447
    
    closes odoo/odoo#171812
    
    X-original-commit: 8b5b8383a2af89283bde568c16b93e98f475a7ed
    Signed-off-by: Abdelouahab Laaroussi (abla) <abla@odoo.com>
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/project/static/src/project_sharing/views/form/project_sharing_form_controller.js
+++ addons/project/static/src/project_sharing/views/form/project_sharing_form_controller.js
@@ -1,11 +1,21 @@
 /** @odoo-module */
 
+import { _t } from "@web/core/l10n/translation";
 import { FormController } from '@web/views/form/form_controller';
+import { useService } from '@web/core/utils/hooks';
+import { useExternalListener } from "@odoo/owl";
 
 export class ProjectSharingFormController extends FormController {
     static components = {
         ...FormController.components,
     };
+
+    setup() {
+        super.setup();
+        this.notification = useService('notification');
+        useExternalListener(window, "paste", this.onGlobalPaste, { capture: true });
+        useExternalListener(window, "drop", this.onGlobalDrop, { capture: true });
+    }
 
     get actionMenuItems() {
         return {};
@@ -13,5 +23,35 @@ export class ProjectSharingFormController extends FormController {
 
     get translateAlert() {
         return null;
+    }
+
+    onGlobalPaste(ev) {
+        ev.preventDefault();
+        if (ev.target.closest('.o_field_widget[name="description"]')) {
+            const items = ev.clipboardData.items;
+            for (let i = 0; i < items.length; i++) {
+                if (items[i].type.indexOf('image') !== -1 && !this.model.root.resId) {
+                    this.notification.add(
+                        _t("Save the task to be able to paste images in description"),
+                        { type: 'warning' },
+                    )
+                    ev.stopImmediatePropagation();
+                    return;
+                }
+            }
+        }
+    }
+
+    onGlobalDrop(ev) {
+        ev.preventDefault();
+        if (ev.target.closest('.o_field_widget[name="description"]')) {
+            if(ev.dataTransfer.files.length > 0 && !this.model.root.resId){
+                this.notification.add(
+                    _t("Save the task to be able to drag images in description"),
+                    { type: 'warning' },
+                )
+                ev.stopImmediatePropagation();
+            }
+        }
     }
 }
