80b48978bd039c422b0832a007443c86a02e4692
Abdelouahab (abla) <abla@odoo.com>
2024-06-24 08:20:39 +0000

    [FIX] project: allow portal user to add image in task description
    To reproduce:
    =============
    - share a project with a portal user
    - connect as the portal user
    - on a task paste an image in the description
    - save -> AccessError
    
    Problem:
    ========
    Portal user doesn't have the right to create attachments
    
    Solution:
    =========
    - when saving the record, editor tries to save the image as an attachment
    by `POST` request on the route `/web_editor/attachment/add_image`
    
    - `/web_editor/attachment/add_image` is not dedicated to portal user,
    so the save method is patched to modify the route to custom one
    `/project_sharing/attachment/add_image`
    
    opw-3774447
    
    X-original-commit: 7e79e1e5fc538944bc08be01121b5e4c0dae6161

--- addons/project/static/src/project_sharing/views/form/project_sharing_form_controller.js
+++ addons/project/static/src/project_sharing/views/form/project_sharing_form_controller.js
@@ -1,11 +1,62 @@
 /** @odoo-module */
 
+<<<<<<< HEAD
+||||||| parent of 1ad8dc291c49 (temp)
+import { useService } from '@web/core/utils/hooks';
+import { createElement } from "@web/core/utils/xml";
+=======
+import { _t } from "@web/core/l10n/translation";
+import { useService } from '@web/core/utils/hooks';
+import { createElement } from "@web/core/utils/xml";
+>>>>>>> 1ad8dc291c49 (temp)
 import { FormController } from '@web/views/form/form_controller';
+<<<<<<< HEAD
+||||||| parent of 1ad8dc291c49 (temp)
+import { useViewCompiler } from '@web/views/view_compiler';
+import { ProjectSharingChatterCompiler } from './project_sharing_form_compiler';
+import { ChatterContainer } from '../../components/chatter/chatter_container';
+=======
+import { useViewCompiler } from '@web/views/view_compiler';
+import { ProjectSharingChatterCompiler } from './project_sharing_form_compiler';
+import { ChatterContainer } from '../../components/chatter/chatter_container';
+import { useExternalListener } from "@odoo/owl";
+>>>>>>> 1ad8dc291c49 (temp)
 
 export class ProjectSharingFormController extends FormController {
+<<<<<<< HEAD
     static components = {
         ...FormController.components,
     };
+||||||| parent of 1ad8dc291c49 (temp)
+    setup() {
+        super.setup();
+        this.uiService = useService('ui');
+        const { xmlDoc } = this.archInfo;
+        const template = createElement('t');
+        const xmlDocChatter = xmlDoc.querySelector("div.oe_chatter");
+        if (xmlDocChatter && xmlDocChatter.parentNode.nodeName === "form") {
+            template.appendChild(xmlDocChatter.cloneNode(true));
+        }
+        const mailTemplates = useViewCompiler(ProjectSharingChatterCompiler, { Mail: template });
+        this.mailTemplate = mailTemplates.Mail;
+    }
+=======
+    setup() {
+        super.setup();
+        this.uiService = useService('ui');
+        this.notification = useService('notification');
+        const { xmlDoc } = this.archInfo;
+        const template = createElement('t');
+        const xmlDocChatter = xmlDoc.querySelector("div.oe_chatter");
+        if (xmlDocChatter && xmlDocChatter.parentNode.nodeName === "form") {
+            template.appendChild(xmlDocChatter.cloneNode(true));
+        }
+        const mailTemplates = useViewCompiler(ProjectSharingChatterCompiler, { Mail: template });
+        this.mailTemplate = mailTemplates.Mail;
+        useExternalListener(window, "paste", this.onGlobalPaste, { capture: true });
+        useExternalListener(window, "drop", this.onGlobalDrop, { capture: true });
+    }
+>>>>>>> 1ad8dc291c49 (temp)
 
     get actionMenuItems() {
         return {};
@@ -13,5 +64,35 @@ export class ProjectSharingFormController extends FormController {
 
     get translateAlert() {
         return null;
+    }
+
+    onGlobalPaste(ev) {
+        ev.preventDefault();
+        if (ev.target.closest('.o_field_widget[name="description"]')) {
+            const items = ev.clipboardData.items;
+            for (let i = 0; i < items.length; i++) {
+                if (items[i].type.indexOf('image') !== -1 && !this.model.root.resId) {
+                    this.notification.add(
+                        _t("Save the task to be able to paste images in description"),
+                        { type: 'warning' },
+                    )
+                    ev.stopImmediatePropagation();
+                    return;
+                }
+            }
+        }
+    }
+
+    onGlobalDrop(ev) {
+        ev.preventDefault();
+        if (ev.target.closest('.o_field_widget[name="description"]')) {
+            if(ev.dataTransfer.files.length > 0 && !this.model.root.resId){
+                this.notification.add(
+                    _t("Save the task to be able to drag images in description"),
+                    { type: 'warning' },
+                )
+                ev.stopImmediatePropagation();
+            }
+        }
     }
 }
