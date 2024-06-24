07db89d0d7e2727284a45b74ee61e6cc1c30cda6
Alice Gaudon <agau@odoo.com>
2024-06-24 14:12:12 +0000

    [FIX] web, website: use the right CodeEditor mode for js files
    Trying to edit custom javascript:
    - Outside of developer mode, doesn't highlight the code
    - In developer mode, fails with a props validation error
    
    Steps to reproduce:
    - Stay in normal mode / Switch to developer mode
    - Go to Website > Homepage
    - Click on the "Site" menu
    - Click on "HTML / CSS Editor"
    - Click on "Edit HTML anyway"
    - In the file type dropdown (top left of the code editor right panel),
      select "JS"
    
    Old behavior: Code is not highlighted / CodeEditor props validation
    error (invalid mode)
    New behavior: Code is properly highlighted, no error
    
    This issue is happening since commit [1] because the ace CodeEditor
    mode "js" was renamed to "javascript".
    
    This commit fixes this by mapping the `ResourceEditor` type `js` to the
    `CodeEditor` mode `javascript`.
    
    This commit adds a class to the CodeEditor component usable by tests to
    detect when the ace editor has changed mode.
    
    [1]: 3e6a6d34702b2145ff998abcdf0a60ff9fbcd873
    
    opw-3963421
    
    X-original-commit: 0898f965df28fef809a2e4720b0b226e5f617ed1

--- addons/website/static/tests/tours/html_editor.js
+++ addons/website/static/tests/tours/html_editor.js
@@ -243,3 +243,61 @@ wTourUtils.registerWebsitePreviewTour('test_html_editor_scss_2', {
         },
     ]
 );
+
+wTourUtils.registerWebsitePreviewTour(
+    "website_code_editor_usable",
+    {
+        // TODO: enable debug mode when failing tests have been fixed (props validation)
+        url: "/",
+        test: true,
+    },
+    () => [
+        {
+            content: "Open Site menu",
+            trigger: 'button[data-menu-xmlid="website.menu_site"]',
+            run: "click",
+        },
+        {
+            content: "Open HTML / CSS Editor",
+            trigger: 'a[data-menu-xmlid="website.menu_ace_editor"]',
+            run: "click",
+        },
+        {
+            content: "Bypass warning",
+            trigger: ".o_resource_editor_wrapper div:nth-child(2) button:nth-child(3)",
+            run: "click",
+        },
+        // Test all 3 file type options
+        ...[{
+            menuItemIndex: 1,
+            editorMode: 'qweb',
+        }, {
+            menuItemIndex: 2,
+            editorMode: 'scss',
+        }, {
+            menuItemIndex: 3,
+            editorMode: 'javascript',
+        }]
+            .map(({ menuItemIndex, editorMode }) => [
+                {
+                    content: "Open file type dropdown",
+                    trigger: ".o_resource_editor_type_switcher .dropdown-toggle",
+                    run: "click",
+                },
+                {
+                    content: `Select type ${menuItemIndex}`,
+                    trigger: `.o-overlay-container .o-dropdown--menu .dropdown-item:nth-child(${menuItemIndex})`,
+                    run: "click",
+                },
+                {
+                    content: "Wait for editor mode to change",
+                    trigger: `.ace_editor[data-mode="${editorMode}"]`,
+                },
+                {
+                    content: "Make sure text is being highlighted",
+                    trigger: ".ace_content .ace_text-layer .ace_line:first-child span",
+                },
+            ])
+            .flat(),
+    ]
+);
