edf8097911376e2604386c4aede98f0cccbe0206
Jorge Pinna Puissant <jpp@odoo.com>
2024-06-24 10:38:49 +0000

    [FIX] web: avoid updating controllers when target is "new"
    - Open the Employees app;
    - Open the record of an employee, that have the onbording plan link in
        the chatter (for instance Abigail Peterson in runbot);
    - Click on the onbording plan;
    - Close the onbording plan dialog;
    - Click on the Employees link in the breadcrumb to came back to the
        employees multi-record view;
    
    Before this commit, a traceback was raised : `Invalid controller to
    restore`. This issue occurs because when we load from a link/URL
    (loadState), the controllers list is updated from the actions found in
    the URL, this is done to be able to recreate the controllers (the
    breadcrumb) from the URL.
    This behavior is correct when the action has a target other than “new”
    (i.e., when it's not a dialog). In the case of a target "new" (a dialog)
    the controllers shouldn't be modified, to be able to interact with the
    breadcrumb when the dialog is closed.
    
    Now, only when the action has a target different from "new", the
    controllers are updated from the URL.
    
    closes odoo/odoo#171152
    
    X-original-commit: 74e5dc0a4cbd13162b142582505e0d123ba1f659
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- addons/web/static/tests/webclient/actions/load_state.test.js
+++ addons/web/static/tests/webclient/actions/load_state.test.js
@@ -106,6 +106,15 @@ defineActions([
         ],
     },
     {
+        id: 5,
+        xml_id: "action_5",
+        name: "Create a Partner",
+        res_model: "partner",
+        target: "new",
+        type: "ir.actions.act_window",
+        views: [[false, "form"]],
+    },
+    {
         id: 1001,
         tag: "__test__client__action__",
         target: "main",
@@ -1226,6 +1235,56 @@ describe(`new urls`, () => {
             "Partners Action 28",
             "First record",
         ]);
+    });
+
+    test(`don't load controllers when load action new`, async () => {
+        stepAllNetworkCalls();
+        redirect("/odoo/action-3/2");
+        Partner._views["form,false"] = /* xml */ `
+            <form string="Partner">
+                <sheet>
+                    <a href="http://example.com/odoo/action-5" class="clickMe">clickMe</a>
+                    <group>
+                        <field name="display_name"/>
+                        <field name="foo"/>
+                    </group>
+                </sheet>
+            </form>
+        `;
+        await mountWebClient();
+        expect(`.o_form_view`).toHaveCount(1);
+        expect(queryAllTexts(".breadcrumb-item, .o_breadcrumb .active")).toEqual([
+            "Partners",
+            "Second record",
+        ]);
+        expect([
+            "/web/webclient/translations",
+            "/web/webclient/load_menus",
+            "/web/action/load",
+            "get_views",
+            "web_read",
+        ]).toVerifySteps();
+        expect(browser.location.href).toBe("http://example.com/odoo/action-3/2", {
+            message: "url did not change",
+        });
+
+        // Open the dialog
+        await contains(`.clickMe`).click();
+        await animationFrame();
+        expect(`.o_dialog .o_form_view`).toHaveCount(1);
+        expect(["/web/action/load", "get_views", "onchange"]).toVerifySteps();
+        expect(browser.location.href).toBe("http://example.com/odoo/action-3/2", {
+            message: "url did not change",
+        });
+
+        // Close te dialog
+        await contains(`.o_dialog .o_form_button_cancel`).click();
+
+        // Go back to the multi-record view
+        await contains(`.breadcrumb-item`).click();
+        await animationFrame();
+        expect(`.o_list_view`).toHaveCount(1);
+        expect(["web_search_read", "has_group"]).toVerifySteps();
     });
 });
 
