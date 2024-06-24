odoo/odoo

commit bec4f6f336970503e2738a1c3cc04727b1b940bc

[FIX] website_sale: product unavailable in snippet
Introduced by f94337dec4440998e77e501bef526551c50f049f,
we were wrongly displaying the 'not available for sale'
message when it was available.

closes odoo/odoo#166204

Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

addons/website_sale/data/product_snippet_template_data.xml
@@ -436,6 +436,9 @@
         </template>
         <template id="price_dynamic_filter_template_product_product" name="Dynamic Product Filter Price">
             <t t-if="data['prevent_zero_price_sale']">
+                <span t-field="website.prevent_zero_price_sale_text"/>
+            </t>
+            <t t-else="">
                 <span t-esc="data['price']"
                       class="fw-bold"
                       name="product_price"
@@ -443,9 +446,6 @@
                 <del t-if="data.get('has_discounted_price')" class="text-danger ms-1 h6" style="white-space: nowrap;"
                      t-esc="data['list_price']"
                      t-options="{'widget': 'monetary', 'display_currency': website.currency_id}"/>
-            </t>
-            <t t-else="">
-                <span t-field="website.prevent_zero_price_sale_text"/>
             </t>
         </template>
 

commit 24ccf144e2d9bdc3b6e5961de64a06fb1882898d

[FIX] website_sale: products snippets crash when samples displayed
Introduced by bec4f6f336970503e2738a1c3cc04727b1b940bc,
sample data does not have 'prevent_zero_price_sale' value
added an 'is_sample' boolean to help identify the sample data

closes odoo/odoo#170134

X-original-commit: 8546843fc43161d5ef8dc119791fd105638b5d89
Signed-off-by: Lina Eweis (liew) <liew@odoo.com>

addons/website_sale/data/product_snippet_template_data.xml
@@ -435,10 +435,10 @@
             </t>
         </template>
         <template id="price_dynamic_filter_template_product_product" name="Dynamic Product Filter Price">
-            <t t-if="data['prevent_zero_price_sale']">
+            <t t-if="data.get('prevent_zero_price_sale')">
                 <span t-field="website.prevent_zero_price_sale_text"/>
             </t>
-            <t t-else="">
+            <t t-elif="not data.get('is_sample')">
                 <span t-esc="data['price']"
                       class="fw-bold"
                       name="product_price"

addons/website_sale/models/website_snippet_filter.py
@@ -84,6 +84,10 @@ class WebsiteSnippetFilter(models.Model):
 
                     if records.env.context.get('add2cart_rerender'):
                         res_product['_add2cart_rerender'] = True
+                else:
+                    res_product.update({
+                        'is_sample': True,
+                    })
         return res_products
 
     @api.model