43b206831610dcc65098640ad90ffc42c10cf208
Jinjiu Liu <jili@odoo.com>
2024-06-25 17:46:49 +0200

    [IMP] html_editor: link preview new feature
    -----------------
    Description
    -----------------
    
    This commit introduces the new feature to preview link elements upon
    clicking and assigns customized link titles for project.task.
    
    -----------------
    Implementation
    -----------------
    External Links: The preview metadata for external links is retrieved
    using an existing method from the mail module. If the external link is
    pointing to an image, we display it in the preview popover and title it
    with the link label.
    
    Internal Links: To display customized titles for internal links, a
    computed field named link_preview_name is introduced. This allows
    displaying titles in a format specific to the model, such as
    "TaskName | ProjectName" for tasks within projects. The
    link_preview_name field is model-specific and is determined by the
    preferred data representation for the preview. In cases where
    link_preview_name is not defined, the system falls back to display_name,
    and subsequently to name, or ultimately uses the webpage's title if none
    of these fields are available.
    
    When the displayed link label differs from the preview title, a banner
    is displayed at the bottom of the preview indicating the change to the
    preview title.
    
    -----------------
    Error Handling
    -----------------
    The approach to error handling is designed to ensure that the preview
    functionality does not interrupt the user flow.
    
    External Links: If metadata retrieval fails for an external link, the
    preview is removed, and only the favicon along with the URL is displayed
    
    Internal Links: For internal links, specific errors such as value or
    index errors indicate incorrect record links, triggering a warning
    notification. Other types of errors are logged to the front-end console
    without raising an exception in the controller, to prevent any
    disruption to the user interface.
    
    task-3678777
    
    Part-of: odoo/odoo#174505
    Signed-off-by: Florent Dardenne (dafl) <dafl@odoo.com>

--- addons/html_editor/controllers/main.py
+++ addons/html_editor/controllers/main.py
@@ -15,6 +15,7 @@ from odoo.http import request
 from odoo.tools.mimetypes import guess_mimetype
 from odoo.tools.misc import file_open
 from odoo.addons.iap.tools import iap_tools
+from odoo.addons.mail.tools import link_preview
 
 from ..models.ir_attachment import SUPPORTED_IMAGE_MIMETYPES
 
@@ -546,3 +547,42 @@ class HTML_Editor(http.Controller):
         bus_data.update({'model_name': model_name, 'field_name': field_name, 'res_id': res_id})
         request.env['bus.bus']._sendone(channel, 'editor_collaboration', bus_data)
 
+    @http.route('/html_editor/link_preview_external', type="json", auth="public", methods=['POST'])
+    def link_preview_metadata(self, preview_url):
+        return link_preview.get_link_preview_from_url(preview_url)
+
+    @http.route('/html_editor/link_preview_internal', type="json", auth="user", methods=['POST'])
+    def link_preview_metadata_internal(self, preview_url):
+        try:
+            Actions = request.env['ir.actions.actions']
+            context = dict(request.env.context)
+            words = preview_url.strip('/').split('/')
+
+            record_id = int(words.pop())
+            action_name = words.pop()
+            action = Actions.sudo().search([('path', '=', action_name)])
+            if not action:
+                return {'error_msg': _("Action %s not found, link preview is not available, please check your url is correct", action_name)}
+            action_type = action.type
+            if action_type != 'ir.actions.act_window':
+                return {'other_error_msg': _("Action %s is not a window action, link preview is not available", action_name)}
+            action = request.env[action_type].browse(action.id)
+
+            model = request.env[action.res_model].with_context(context)
+            record = model.browse(record_id)
+
+            result = {}
+            if 'description' in record:
+                result['description'] = record.description
+
+            if 'link_preview_name' in record:
+                result['link_preview_name'] = record.link_preview_name
+            elif 'display_name' in record:
+                result['display_name'] = record.display_name
+
+            return result
+        except (MissingError) as e:
+            return {'error_msg': _("Link preview is not available because %s, please check if your url is correct", str(e))}
+        # catch all other exceptions and return the error message to display in the console but not blocking the flow
+        except Exception as e:  # noqa: BLE001
+            return {'other_error_msg': str(e)}
