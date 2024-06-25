ec25276eae048f160045073c855e1c653d72b4c1
guva-odoo <guva@odoo.com>
2024-06-25 14:58:45 +0200

    [FIX] point_of_sale, l10n_ch_pos: missing bank recipient pos invoice
    Steps:
    
    - Install l10n_fr and make sure fr company has tax id filled
    - Go to bank journal and set a bank account number
    - Go to Customer invoice journal and check `Factur-X` in the
      Advenced Settings tab
    - Create a french customer F with an email address
    - Open a pos session, and make an invoice forF for a product with tax,
      select Bank payment method and confirm
    - Go to invoices and select the newly created invoice
    -> "Error occured while creating the EDI document"
    
    Cause:
    
    The field `partner_bank_id` is not set from the pos order to the invoice
    
    cbe1cf5b1044827a058a4cde5d0492a545ac6da4 added a condition specifically
    for swiss localisation.
    
    Fix:
    
    Movr the condition to a bridge module for swiss localisation
    
    opw-3992498
    
    X-original-commit: ac005d53dc9477dd4e7b7da4d7ed6fec86acd0c7

---
+++ addons/l10n_ch_pos/__manifest__.py
@@ -0,0 +1,14 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Swiss - Point of Sale',
+    'author': 'Odoo PS',
+    'category': 'Accounting/Localizations/Point of Sale',
+    'description': """
+Swiss POS Localization
+=======================================================
+    """,
+    'depends': ['l10n_ch', 'point_of_sale'],
+    'auto_install': True,
+    'installable': True,
+    'license': 'LGPL-3',
+}
