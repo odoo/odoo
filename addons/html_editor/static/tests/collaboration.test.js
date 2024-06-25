bb6b87552e5f25e1b69d6bf78a33e220c990c4fd
abd-msyukyu-odoo <abd@odoo.com>
2024-06-25 16:13:37 +0200

    [ADD] html_editor: test makeSavePoint in collaboration
    Add a collaborative test for `makeSavePoint` to show that it handles external
    steps incorrectly.

--- addons/html_editor/static/tests/collaboration.test.js
+++ addons/html_editor/static/tests/collaboration.test.js
@@ -169,6 +169,35 @@ test("should not revert the step of another peer", async () => {
         },
     });
 });
+describe("collaborative makeSavePoint", () => {
+    test.todo(
+        "After a savePoint, local steps should be discarded in collaboration and external steps should not",
+        async () => {
+            const peerInfos = await setupMultiEditor({
+                peerIds: ["c1", "c2"],
+                contentBefore: `<p>[c1}{c1]<br></p><p>[c2}{c2]<br></p>`,
+            });
+            const savepoint = peerInfos.c1.editor.shared.makeSavePoint();
+            insert(peerInfos.c2.editor, "a");
+            mergePeersSteps(peerInfos);
+            insert(peerInfos.c1.editor, "z");
+            mergePeersSteps(peerInfos);
+            insert(peerInfos.c2.editor, "b");
+            mergePeersSteps(peerInfos);
+            savepoint();
+            mergePeersSteps(peerInfos);
+            peerInfos.c1.editor.dispatch("CLEAN", { root: peerInfos.c1.editor.editable });
+            peerInfos.c2.editor.dispatch("CLEAN", { root: peerInfos.c2.editor.editable });
+            renderTextualSelection(peerInfos);
+            expect(peerInfos.c1.editor.editable.innerHTML).toBe(
+                `<p>[c1}{c1]<br></p><p>ab[c2}{c2]<br></p>`
+            );
+            expect(peerInfos.c2.editor.editable.innerHTML).toBe(
+                `<p>[c1}{c1]<br></p><p>ab[c2}{c2]<br></p>`
+            );
+        }
+    );
+});
 test("should reset from snapshot", async () => {
     await testMultiEditor({
         peerIds: ["c1", "c2"],
