13486c465305a7faede4d7fcc22059781b4c119b
kinhosz <jcsc@odoo.com>
2024-06-26 13:07:36 +0200

    [ADD] purchase_repair: add MTO links on mrp <> repair <> purchase
    When we creating a repair with ADD type, if the products
    have a MTO route enabled, a smart button will appear in
    the header of the repair/purchase form, linking the two.
    If there are multiple repairs or purchases, the button
    will redirect to a list of the relevant repairs or purchases.
    
    The same behaviour will occur when the product has the 'manufacturing'
    option enabled. A link between the MO and the repair will appear
    in the header of each form.
    
    This commit introduces a new bridge module that connects the
    purchase and repair functionalities.
    
    Task: 3935079
    Part-of: odoo/odoo#168348
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/purchase_repair/models/__init__.py
+++ addons/purchase_repair/models/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import repair
-from . import production
+from . import purchase_order
+from . import repair_order
