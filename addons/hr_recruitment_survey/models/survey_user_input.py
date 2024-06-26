28337893161bc937494f97127cf1ff4934389024
Victor Piryns (pivi) <pivi@odoo.com>
2024-06-26 15:32:33 +0200

    [PERF] hr_recruitement_survey: add missing index on `applicant_id`
    Description:
    Following a1bc13b39559fa82763a9f0b3b603f6026b8a151 the field
    `One2many: applicant_ids` becomes `Many2one: applicant_id`.
    The field is used in lookups (ir.rules) and is also the inverse of
    `hr.applicant.response_ids`, and is present on the model
    `survey.user_input`, which is a large table, therefor should be indexed.
    
    Reference:
    task-4011294
    
    closes odoo/odoo#171438
    
    X-original-commit: 9fed4385f45cb9d82b42916cc4bcf51740a464dc
    Signed-off-by: Sofie Gvaladze (sgv) <sgv@odoo.com>
    Signed-off-by: Piryns Victor (pivi) <pivi@odoo.com>

--- addons/hr_recruitment_survey/models/survey_user_input.py
+++ addons/hr_recruitment_survey/models/survey_user_input.py
@@ -6,7 +6,7 @@ from odoo import fields, models, _
 class SurveyUserInput(models.Model):
     _inherit = "survey.user_input"
 
-    applicant_id = fields.Many2one('hr.applicant', string='Applicant')
+    applicant_id = fields.Many2one('hr.applicant', string='Applicant', index='btree_not_null')
 
     def _mark_done(self):
         odoobot = self.env.ref('base.partner_root')
