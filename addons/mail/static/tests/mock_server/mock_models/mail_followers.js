ec51a0c5aa82db6ff3961dc94ea9e8f42e1db6e7
Sébastien Theys <seb@odoo.com>
2024-06-27 13:07:26 +0200

    [REF] mail: convert followers format to store
    Part of task-3605717
    
    Part-of: odoo/odoo#170709
    Related: odoo/enterprise#65411
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/tests/mock_server/mock_models/mail_followers.js
+++ addons/mail/static/tests/mock_server/mock_models/mail_followers.js
@@ -3,7 +3,7 @@ import { models } from "@web/../tests/web_test_helpers";
 export class MailFollowers extends models.ServerModel {
     _name = "mail.followers";
 
-    _format_for_chatter(ids) {
+    _to_store(ids, store) {
         /** @type {import("mock_models").MailFollowers} */
         const MailFollowers = this.env["mail.followers"];
         /** @type {import("mock_models").ResPartner} */
@@ -12,19 +12,23 @@ export class MailFollowers extends models.ServerModel {
         const followers = MailFollowers._filter([["id", "in", ids]]);
         // sorted from lowest ID to highest ID (i.e. from least to most recent)
         followers.sort((f1, f2) => (f1.id < f2.id ? -1 : 1));
-        const partnerFormats = ResPartner.mail_partner_format(
-            followers.map((follower) => follower.partner_id)
+        store.add(
+            "Persona",
+            Object.values(
+                ResPartner.mail_partner_format(followers.map((follower) => follower.partner_id))
+            )
         );
-        return followers.map((follower) => {
-            return {
-                id: follower.id,
-                partner_id: follower.partner_id,
-                name: follower.name,
+        for (const follower of followers) {
+            store.add("Follower", {
                 display_name: follower.display_name,
                 email: follower.email,
+                id: follower.id,
                 is_active: follower.is_active,
-                partner: partnerFormats[follower.partner_id],
-            };
-        });
+                name: follower.name,
+                partner_id: follower.partner_id,
+                partner: { id: follower.partner_id, type: "partner" },
+                thread: { id: follower.res_id, model: follower.res_model },
+            });
+        }
     }
 }
