872a9a040882ce9cd039dcf5a21c33413cfbe834
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2024-06-27 11:58:58 +0200

    [FIX] l10n_es_edi_tbai: traceback on invoice process
    Open tax "IVA 0% Entregas Intracomunitarias exentas"
    Set Tax Scope to False
    Create an invoice to an EU partner
    Add an invoice line (select the mentioned tax)
    Confirm
    Send for Validation
    
    Traceback:
    odoo.addons.base.models.ir_qweb.QWebException: Error while render the template
    KeyError: 'DesgloseTipoOperacion'
    Template: l10n_es_edi_tbai.template_invoice_factura
    Path: /t/TipoDesglose/DesgloseTipoOperacion/PrestacionServicios
    Node: <PrestacionServicios t-if="invoice_info.get(\'PrestacionServicios\')"/>
    
    This occurs because tax info was not fetched properly
    When invoicing to a foreign partner the tax scope needs to be properly
    configured
    
    opw-3877924

--- addons/l10n_es_edi_tbai/models/account_edi_format.py
+++ addons/l10n_es_edi_tbai/models/account_edi_format.py
@@ -115,6 +115,15 @@ class AccountEdiFormat(models.Model):
             if invoice.move_type == 'out_refund' and not invoice.reversed_entry_id._l10n_es_tbai_is_in_chain():
                 error_msg = _("TicketBAI: Cannot post a reversal move while the source document (%s) has not been posted", invoice.reversed_entry_id.name)
 
+            # Tax configuration check: In case of foreign customer we need the tax scope to be set
+            com_partner = invoice.commercial_partner_id
+            if (com_partner.country_id.code not in ('ES', False) or (com_partner.vat or '').startswith("ESN")) and\
+                    invoice.line_ids.tax_ids.filtered(lambda t: not t.tax_scope):
+                error_msg = _(
+                    "In case of a foreign customer, you need to configure the tax scope on taxes:\n%s",
+                    "\n".join(invoice.line_ids.tax_ids.mapped('name'))
+                )
+
             if error_msg:
                 return {
                     invoice: {
