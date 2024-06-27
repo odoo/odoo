1a321f3f7ebb3903059f636705c5c9a397bda6df
sovi-odoo <sovi@odoo.com>
2024-06-27 02:08:45 +0200

    [IMP] l10n_fr*: add method `_get_france_country_codes` to `res.company`
    France in Odoo can be represented through many country codes which makes
    detecting whether a company is french overly difficult. There already
    exists a method called `_get_unalterable_country` which returned these
    country codes but its name is confusing
    
    This commit adds `_get_france_country_codes` which replaces
    `_get_unalterable_country`
    
    X-original-commit: 2507b58dbb7ba98754568fc93e5a1256719bf8af
    Part-of: odoo/odoo#171340
    Signed-off-by: Quentin De Paoli (qdp) <qdp@odoo.com>
    Signed-off-by: Solal Vignal (sovi) <sovi@odoo.com>

--- addons/l10n_fr_account/__init__.py
+++ addons/l10n_fr_account/__init__.py
@@ -19,6 +19,6 @@ def _setup_inalterability(env):
     # enable ping for this module
     env['publisher_warranty.contract'].update_notification(cron_mode=True)
 
-    fr_companies = env['res.company'].search([('partner_id.country_id.code', 'in', env['res.company']._get_unalterable_country())])
+    fr_companies = env['res.company'].search([('partner_id.country_id.code', 'in', env['res.company']._get_france_country_codes())])
     if fr_companies:
         fr_companies._create_secure_sequence(['l10n_fr_closing_sequence_id'])
