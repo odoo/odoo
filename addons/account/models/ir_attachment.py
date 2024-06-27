d78bb2f9ea96c0b0783dbee01a35dafff6ade163
casp-odoo <casp@odoo.com>
2024-06-27 14:04:11 +0200

    [IMP] account: allow decoding of files from attachment
    It was possible to upload a file and decode it by attaching it to a message in the chatter (factur-x, ulb...), but not by attaching it directly to the invoice.
    
    This commit allows for the same behaviour in both ways.
    
    task-3997526
    
    closes odoo/odoo#171393
    
    Signed-off-by: Julien Van Roy (juvr) <juvr@odoo.com>

--- addons/account/models/ir_attachment.py
+++ addons/account/models/ir_attachment.py
@@ -175,3 +175,10 @@ class IrAttachment(models.Model):
         # To be extended by localisations, where they can download their necessary XSD files
         # Note: they should always return super().action_download_xsd_files()
         return
+
+    def _post_add_create(self, **kwargs):
+        move_attachments = self.filtered(lambda attachment: attachment.res_model == 'account.move')
+        moves_per_id = self.env['account.move'].browse([attachment.res_id for attachment in move_attachments]).grouped('id')
+        for attachment in move_attachments:
+            moves_per_id[attachment.res_id]._check_and_decode_attachment(attachment)
+        super()._post_add_create()
