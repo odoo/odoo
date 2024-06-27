6a860b9ec80db220b99d4c9877ada83ace539aab
dere-odoo <dere@odoo.com>
2024-06-27 09:43:31 +0000

    [FIX] sale: price rules form behavior
    Steps :
    - Set the company of the first pricelist in list view to false
    - Create a product and set it's company and the go to extra price tab
      and group by product
    - Now when adding a new, opens form view, try saving the record
    
    Issue :
    Returns a warning message now allowing to save
    
    Cause :
    The company for pricelist that is default selected and the product have
    different companies
    
    Fix :
    Updated the action so that it does not open form view as the pricelist
    tree view is multi-edit
    
    opw-3943830
    
    closes odoo/odoo#182250
    
    X-original-commit: b3d61b372a8cd315f9063b6add634ad194f3341b
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>
    Signed-off-by: Dev Reshamiya (dere) <dere@odoo.com>

--- addons/product/models/product_product.py
+++ addons/product/models/product_product.py
@@ -621,7 +621,7 @@ class ProductProduct(models.Model):
         return {
             'name': _('Price Rules'),
             'view_mode': 'list,form',
-            'views': [(self.env.ref('product.product_pricelist_item_tree_view_from_product').id, 'list'), (False, 'form')],
+            'views': [(self.env.ref('product.product_pricelist_item_tree_view_from_product').id, 'list')],
             'res_model': 'product.pricelist.item',
             'type': 'ir.actions.act_window',
             'target': 'current',
