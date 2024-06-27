eb5621116851bc066a82e268361f86d7845af6bc
FrancoisGe <fge@odoo.com>
2024-06-27 14:22:42 +0200

    [FIX] html_editor: undo emoji/signature
    Before this commit, in the html editor, adding an emoji/signature and then
    removing it with undo didn't work properly. It removed one step more
    than adding the emoji/signature.

--- addons/html_editor/static/tests/emoji.test.js
+++ addons/html_editor/static/tests/emoji.test.js
@@ -3,7 +3,7 @@ import { click, press, waitFor } from "@odoo/hoot-dom";
 import { loadBundle } from "@web/core/assets";
 import { setupEditor } from "./_helpers/editor";
 import { getContent } from "./_helpers/selection";
-import { insertText } from "./_helpers/user_actions";
+import { insertText, undo } from "./_helpers/user_actions";
 
 test("add an emoji with powerbox", async () => {
     const { el, editor } = await setupEditor("<p>ab[]</p>");
@@ -19,4 +19,20 @@ test("add an emoji with powerbox", async () => {
 
     await click(".o-EmojiPicker .o-Emoji");
     expect(getContent(el)).toBe("<p>ab😀[]</p>");
+});
+
+test("undo an emoji", async () => {
+    const { el, editor } = await setupEditor("<p>ab[]</p>");
+    await loadBundle("web.assets_emoji");
+    expect(getContent(el)).toBe("<p>ab[]</p>");
+
+    insertText(editor, "test");
+    insertText(editor, "/emoji");
+    press("enter");
+    await waitFor(".o-EmojiPicker");
+    await click(".o-EmojiPicker .o-Emoji");
+    expect(getContent(el)).toBe("<p>abtest😀[]</p>");
+
+    undo(editor);
+    expect(getContent(el)).toBe("<p>abtest[]</p>");
 });
--- addons/html_editor/static/tests/signature.test.js
+++ addons/html_editor/static/tests/signature.test.js
@@ -4,7 +4,7 @@ import { animationFrame, tick } from "@odoo/hoot-mock";
 import { defineModels, fields, models, serverState } from "@web/../tests/web_test_helpers";
 import { setupEditor } from "./_helpers/editor";
 import { getContent } from "./_helpers/selection";
-import { insertText } from "./_helpers/user_actions";
+import { insertText, undo } from "./_helpers/user_actions";
 class ResUsers extends models.Model {
     _name = "res.users";
 
@@ -27,4 +27,16 @@ test("apply 'Signature' command", async () => {
     press("enter");
     await tick();
     expect(getContent(el)).toBe("<p>ab</p><h1>Hello</h1><p>[]cd</p>");
+});
+
+test("undo a 'Signature' command", async () => {
+    const { el, editor } = await setupEditor("<p>ab[]cd</p>");
+    insertText(editor, "test");
+    insertText(editor, "/signature");
+    press("enter");
+    await tick();
+    expect(getContent(el)).toBe("<p>abtest</p><h1>Hello</h1><p>[]cd</p>");
+
+    undo(editor);
+    expect(getContent(el)).toBe("<p>abtest[]cd</p>");
 });
