46a412800f66d4360daa7dd8c2692b6545ae39bd
DiegoVGV <149200870+DiegoVGV@users.noreply.github.com>
2024-06-27 17:09:23 -0600

    [FIX] maintenance: error ensure_one
    call to _get_activity_note should be request not self
    
    X-original-commit: b2946092163c3b4000843bfe5835ad4672e08d44
    Part-of: odoo/odoo#171860
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/maintenance/models/maintenance.py
+++ addons/maintenance/models/maintenance.py
@@ -367,7 +367,7 @@ class MaintenanceRequest(models.Model):
                 date_deadline=date_dl,
                 new_user_id=request.user_id.id or request.owner_user_id.id or self.env.uid)
             if not updated:
-                note = self._get_activity_note()
+                note = request._get_activity_note()
                 request.activity_schedule(
                     'maintenance.mail_act_maintenance_request',
                     fields.Datetime.from_string(request.schedule_date).date(),
