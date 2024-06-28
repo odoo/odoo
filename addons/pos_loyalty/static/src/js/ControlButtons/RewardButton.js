bba80fe41f6a98f826f8db7b7946a053734a17fb
David Monnom (moda) <moda@odoo.com>
2024-06-28 12:26:10 +0200

    [FIX] pos_loyalty: underterministic error

--- addons/pos_loyalty/static/src/js/ControlButtons/RewardButton.js
+++ addons/pos_loyalty/static/src/js/ControlButtons/RewardButton.js
@@ -43,9 +43,12 @@ export class RewardButton extends Component {
         const discountRewards = rewards.filter(({ reward }) => reward.reward_type == "discount");
         const freeProductRewards = rewards.filter(({ reward }) => reward.reward_type == "product");
         const potentialFreeProductRewards = this.pos.getPotentialFreeProductRewards();
-        return discountRewards.concat(
+        const test = discountRewards.concat(
             this._mergeFreeProductRewards(freeProductRewards, potentialFreeProductRewards)
         );
+        console.warn(test);
+        console.warn(test.length);
+        return test;
     }
 
     _isDisabled() {}
