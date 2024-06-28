c2322b68494156e0da1dfc7f71c94070c0c2d8f9
Julien Mougenot <jum@odoo.com>
2024-06-28 15:07:18 +0200

    [FIX] web: HOOT - deepCopy steps & matcher values
    Before this commit, values given to `expect()`, `expect.step()` and any
    matcher was not/incorrectly copied.
    
    This meant that if the value changed between when the value has been
    registered and when we check the value (in the UI or with
    `expect.verifySteps`), there would be a mismatch between values.
    
    This commit ensures that all of these values are correctly deep-copied.
    
    X-original-commit: b3267bc1e6f712c92d6fbf35364cdbd0644a84b4

--- addons/web/static/lib/hoot/ui/hoot_technical_value.js
+++ addons/web/static/lib/hoot/ui/hoot_technical_value.js
@@ -3,7 +3,7 @@
 import { Component, xml as owlXml, toRaw, useState } from "@odoo/owl";
 import { isNode, toSelector } from "@web/../lib/hoot-dom/helpers/dom";
 import { isIterable } from "@web/../lib/hoot-dom/hoot_dom_utils";
-import { Markup, deepCopy, toExplicitString } from "../hoot_utils";
+import { Markup, toExplicitString } from "../hoot_utils";
 
 /**
  * @typedef {{
@@ -51,7 +51,7 @@ export class HootTechnicalValue extends Component {
     static template = xml`
         <t t-if="isMarkup">
             <t t-if="value.technical">
-                <pre class="hoot-technical m-0" t-att-class="value.className">
+                <pre class="hoot-technical" t-att-class="value.className">
                     <t t-foreach="value.content" t-as="subValue" t-key="subValue_index">
                         <HootTechnicalValue value="subValue" />
                     </t>
@@ -65,28 +65,33 @@ export class HootTechnicalValue extends Component {
         <t t-elif="isNode(value)">
             <t t-set="elParts" t-value="toSelector(value, { object: true })" />
             <button class="hoot-html" t-on-click="log">
-                <t>&lt;</t>
-                    <span class="hoot-html-tag" t-esc="elParts.tag" />
-                    <t t-if="elParts.id">
-                        <span class="hoot-html-id" t-esc="elParts.id" />
-                    </t>
-                    <t t-if="elParts.class">
-                        <span class="hoot-html-class" t-esc="elParts.class" />
-                    </t>
+                <t>&lt;<t t-esc="elParts.tag" /></t>
+                <t t-if="elParts.id">
+                    <span class="hoot-html-id" t-esc="elParts.id" />
+                </t>
+                <t t-if="elParts.class">
+                    <span class="hoot-html-class" t-esc="elParts.class" />
+                </t>
                 <t>/&gt;</t>
             </button>
         </t>
         <t t-elif="value and typeof value === 'object'">
-            <pre class="hoot-technical m-0">
-                <button class="hoot-object-type inline-flex items-center" t-on-click="onClick">
-                    <t t-esc="getConstructor()" />
-                    <i t-attf-class="fa fa-caret-{{ state.open ? 'up' : 'down' }}" />
+            <t t-set="objectSize" t-value="getSize()" />
+            <pre class="hoot-technical">
+                <button class="hoot-object-type inline-flex items-center gap-1 me-1" t-on-click="onClick">
+                    <t t-if="objectSize">
+                        <i
+                            class="fa fa-caret-right flex justify-center w-2 transition"
+                            t-att-class="{ 'rotate-90': state.open }"
+                        />
+                    </t>
+                    <t t-esc="value.constructor.name" />
+                    (<t t-esc="objectSize" />)
                 </button>
-                <t> </t>
-                <t t-if="state.open">
+                <t t-if="state.open and objectSize">
                     <t t-if="isIterable(value)">
                         <t>[</t>
-                        <ul class="ps-3">
+                        <ul class="ps-4">
                             <t t-foreach="value" t-as="subValue" t-key="subValue_index">
                                 <li class="flex">
                                     <HootTechnicalValue value="subValue" />
@@ -98,10 +103,12 @@ export class HootTechnicalValue extends Component {
                     </t>
                     <t t-else="">
                         <t>{</t>
-                        <ul class="ps-3">
+                        <ul class="ps-4">
                             <t t-foreach="value" t-as="key" t-key="key">
                                 <li class="flex">
-                                    <span class="hoot-key" t-esc="key" />: <HootTechnicalValue value="value[key]" />
+                                    <span class="hoot-key" t-esc="key" />
+                                    <span class="me-1">:</span>
+                                    <HootTechnicalValue value="value[key]" />
                                     <t t-esc="displayComma(value[key])" />
                                 </li>
                             </t>
@@ -129,7 +136,7 @@ export class HootTechnicalValue extends Component {
     setup() {
         this.logged = false;
         this.isMarkup = Markup.isMarkup(this.props.value);
-        this.value = deepCopy(toRaw(this.props.value));
+        this.value = toRaw(this.props.value);
         this.state = useState({ open: false });
     }
 
@@ -138,9 +145,9 @@ export class HootTechnicalValue extends Component {
         this.state.open = !this.state.open;
     }
 
-    getConstructor() {
-        const { name } = this.value.constructor;
-        return `${name}(${$keys(this.value).length})`;
+    getSize() {
+        const values = isIterable(this.value) ? [...this.value] : $keys(this.value);
+        return values.length;
     }
 
     displayComma(value) {
