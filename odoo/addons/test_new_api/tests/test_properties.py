b4d64c5fbf9b846cbbacd5b795151ef90d8da4c9
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-06-28 16:42:37 +0000

    [IMP] core: test expression complement domain
    Add tests that check if "domain | ~domain = TRUE"
    
    task-4017289

--- odoo/addons/test_new_api/tests/test_properties.py
+++ odoo/addons/test_new_api/tests/test_properties.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import babel.dates
@@ -8,13 +7,14 @@ import unittest
 
 from unittest.mock import patch
 
-from odoo.fields import Command, Domain
+from odoo.addons.base.tests.test_expression import TransactionExpressionCase
 from odoo.exceptions import AccessError, UserError
-from odoo.tests import Form, TransactionCase, users
+from odoo.fields import Command, Domain
+from odoo.tests import Form, users
 from odoo.tools import mute_logger, get_lang
 
 
-class TestPropertiesMixin(TransactionCase):
+class TestPropertiesMixin(TransactionExpressionCase):
 
     @classmethod
     def setUpClass(cls):
@@ -1968,17 +1968,17 @@ class PropertiesSearchCase(TestPropertiesMixin):
         }]
         self.message_2.attributes = {'mychar': 'Helene'}
 
-        result = Model.search([('attributes.mychar', 'ilike', 'Helene')])
+        result = self._search(Model, [('attributes.mychar', 'ilike', 'Helene')])
         self.assertEqual(self.message_1 | self.message_2, result)
 
-        result = Model.search([('attributes.mychar', 'ilike', 'hélène')])
+        result = self._search(Model, [('attributes.mychar', 'ilike', 'hélène')])
         self.assertEqual(self.message_1 | self.message_2, result)
 
-        result = Model.search([('attributes.mychar', 'not ilike', 'Helene')])
+        result = self._search(Model, [('attributes.mychar', 'not ilike', 'Helene')])
         self.assertNotIn(self.message_1, result)
         self.assertNotIn(self.message_2, result)
 
-        result = Model.search([('attributes.mychar', 'not ilike', 'hélène')])
+        result = self._search(Model, [('attributes.mychar', 'not ilike', 'hélène')])
         self.assertNotIn(self.message_1, result)
         self.assertNotIn(self.message_2, result)
 
