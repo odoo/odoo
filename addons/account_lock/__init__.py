6bc10b353db75d5f0301522585a843af4d1e5eaa
william-andre <wan@odoo.com>
2024-06-28 15:44:15 +0200

    [FIX] account{,_audit_trail},l10n_de,mail: tracking of journal items
    Multiple fixes related to Audit Trail:
    * track more models, on top of only <account.move>, adding search and
      display fields in the Audit Trail
      * <account.account>
      * <account.tax>
      * <res.partner>
      * <res.company>
    * track more fields:
      * <account.account>.tag_ids
      * <account.move.line>.balance
      * <account.move.line>.tax_tag_ids
      * <account.tax>.l10n_de_datev_code
      * <res.company>.l10n_de_widnr
      * <res.partner>.name
    * Track newly created lines once the move has been posted
    * Prevent deleting tracking messages/values linked to the audit trail
    * Prevent deleting PDFs linked to invoices, only for Germany (GoBD).
      Still allow to detach the generated PDF so that we can re-generate new
      ones.
    
    Note: the query counter of the tests had to be bumped up because
    `_except_audit_log` has to read the values of `self`, which it didn't
    before.
    
    task-4018800
    
    closes odoo/odoo#171244
    
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- addons/account_lock/__init__.py
+++ addons/account_lock/__init__.py
@@ -3,6 +3,7 @@
 from . import models
 from odoo.exceptions import UserError
 
+
 def uninstall_hook(env):
     if not env.ref('base.module_base').demo:
         raise UserError("This module cannot be uninstalled.")
