23cd513a17dad4ed7daa857236b8a95a80cd3444
Loan (LSE) <lse@odoo.com>
2024-07-01 07:28:55 +0000

    [FIX] pos_adyen,pos_viva_wallet: this.pos is undefined
    Setting a adyen/viva-wallet terminal PoS setup and using it would
    cause the JS traceback:
    `TypeError: Cannot read properties of undefined (reading 'getPendingPaymentLine')`
    
    This error happen starting 17.2 due to the change of patched class
    PosBus (17.1) -> PosStore (17.2)
    see: https://github.com/odoo/odoo/pull/136621
    
    opw-3977273
    
    X-original-commit: 514e40c45fcdb2ad5d615b0312747e66b3c725aa

--- addons/pos_adyen/static/src/app/pos_store.js
+++ addons/pos_adyen/static/src/app/pos_store.js
@@ -7,7 +7,15 @@ patch(PosStore.prototype, {
         this.onNotified("ADYEN_LATEST_RESPONSE", () => {
             this.getPendingPaymentLine(
                 "adyen"
+<<<<<<< saas-17.4
             ).payment_method_id.payment_terminal.handleAdyenStatusResponse();
+||||||| a1a0dff03b52bb3ce23f163f435985bbc4ba4f3e
+            this.pos
+                .getPendingPaymentLine("adyen")
+                .payment_method.payment_terminal.handleAdyenStatusResponse();
+=======
+            ).payment_method.payment_terminal.handleAdyenStatusResponse();
+>>>>>>> d0957749dc775c6b1540d741549e98f80a961575
         });
     },
 });
--- addons/pos_viva_wallet/static/src/overrides/pos_store.js
+++ addons/pos_viva_wallet/static/src/overrides/pos_store.js
@@ -7,7 +7,15 @@ patch(PosStore.prototype, {
         this.onNotified("VIVA_WALLET_LATEST_RESPONSE", () => {
             this.getPendingPaymentLine(
                 "viva_wallet"
+<<<<<<< saas-17.4
             ).payment_method_id.payment_terminal.handleVivaWalletStatusResponse();
+||||||| a1a0dff03b52bb3ce23f163f435985bbc4ba4f3e
+            this.pos
+                .getPendingPaymentLine("viva_wallet")
+                .payment_method.payment_terminal.handleVivaWalletStatusResponse();
+=======
+            ).payment_method.payment_terminal.handleVivaWalletStatusResponse();
+>>>>>>> d0957749dc775c6b1540d741549e98f80a961575
         });
     },
 });
