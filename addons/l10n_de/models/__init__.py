50c62eab72678d51fb88c88fbeb81da2decca6f2
william-andre <wan@odoo.com>
2024-07-01 15:21:27 +0200

    [FIX] l10n_de: prevent changing the code of accounts used
    The constraint was already added but only for hashed entries.
    There was no reason to restrict hashed entries, and removing that domain
    was asked during the certification process.
    While changing the code, also do a small perf improvement by not
    fetching the fields linked to the company if not needed.
    On the other hand, changing the name is allowed.
    
    In the same order of idea, we can't delete an attachment linked to a
    move because we need to be able to retrieve easily the source of a
    document.
    
    Also mention the SKR04 in the manifest of the module.
    
    task-4018800
    
    Part-of: odoo/odoo#171244
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- addons/l10n_de/models/__init__.py
+++ addons/l10n_de/models/__init__.py
@@ -7,6 +7,7 @@ from . import account_move
 from . import datev
 from . import chart_template
 from . import ir_actions_report
+from . import ir_attachment
 from . import res_company
 from . import template_de_skr03
 from . import template_de_skr04
