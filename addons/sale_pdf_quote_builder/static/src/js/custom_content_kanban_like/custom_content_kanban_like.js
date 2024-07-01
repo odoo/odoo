88fb3c164b26366ac158d0282a5afcc9dc73aec0
Valentin Chevalier <vcr@odoo.com>
2024-07-01 16:31:08 +0200

    fixup! [IMP] sale_pdf_quote_builder: use custom zones

--- addons/sale_pdf_quote_builder/static/src/js/custom_content_kanban_like/custom_content_kanban_like.js
+++ addons/sale_pdf_quote_builder/static/src/js/custom_content_kanban_like/custom_content_kanban_like.js
@@ -2,41 +2,23 @@
 
 import { JsonField, jsonField } from "@web/views/fields/jsonb/jsonb";
 import { _t } from "@web/core/l10n/translation";
-import { onWillStart, useState } from "@odoo/owl";
+import { reactive } from "@odoo/owl";
 import { registry } from '@web/core/registry';
-import { useService } from "@web/core/utils/hooks";
-
-import { ContentEditionDialog } from "@sale_pdf_quote_builder/js/content_edition/content_edition";
 
 class CustomContentKanbanLike extends JsonField {
     static template = "sale_pdf_quote_builder.CustomContentKanbanLike";
 
     setup() {
-        this.state = useState({
-            datas: {},
-        });
-        this.dialog = useService("dialog");
+        this.state = reactive(
+            JSON.parse(this.props.record.data[this.props.name]),
+            () => this.onChange(JSON.stringify(this.state))
+        );
         this.placeholderLabel = _t("Click to write content for the PDF quote...");
-
-        onWillStart(async () => {
-            this.state.datas = await this._loadData();
-        });
     }
 
-    async _loadData() {
-        return JSON.parse(this.props.record.data.customizable_pdf_form_fields);
+    onChange(newValue) {
+        this.props.record.update({ [this.props.name]: newValue });
     }
-
-    async onClickEditText(documentType, formField, content) {
-        this.dialog.add(ContentEditionDialog, {
-            saleOrderId: this.props.record.evalContext.id,
-            documentType: documentType,
-            formField: formField,
-            content: content,
-            placeholderLabel : this.placeholderLabel,
-        });
-    }
-
 }
 
 export const CustomContentKanbanLikeField = {
