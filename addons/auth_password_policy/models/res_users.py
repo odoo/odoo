odoo/odoo

commit c1f277a7d04d9d6035413e315556b41a44d0b5b2

[IMP] auth_password_policy: reword too short password error message
Previous message:
"Passwords must have at least %d characters, got %d.""

New message:
"Your password must contain at least %d characters and only has %d."

Task-3775424

Part-of: odoo/odoo#160182

addons/auth_password_policy/models/res_users.py
@@ -27,7 +27,7 @@ class ResUsers(models.Model):
             if not password:
                 continue
             if len(password) < minlength:
-                failures.append(_("Passwords must have at least %d characters, got %d.", minlength, len(password)))
+                failures.append(_("Your password must contain at least %(minimal_length)d characters and only has %(current_count)d.", minlength, len(password)))
 
         if failures:
             raise UserError(u'\n\n '.join(failures))

commit 1cac8de57802593430ec74a4c1f40f30f0039702

[FIX] auth_password_policy: show correct error message
This commit fixes an issue introduced in c1f277a.
The issue was that the translation method wasn't correctly called leading to a
formatting error to be raised instead of the correct failure.

This commit fixes this call so that the actual error message can be displayed to
the user.

task-4013509

addons/auth_password_policy/models/res_users.py
@@ -27,7 +27,7 @@ class ResUsers(models.Model):
             if not password:
                 continue
             if len(password) < minlength:
-                failures.append(_("Your password must contain at least %(minimal_length)d characters and only has %(current_count)d.", minlength, len(password)))
+                failures.append(_("Your password must contain at least %(minimal_length)d characters and only has %(current_count)d.", minimal_length=minlength, current_count=len(password)))
 
         if failures:
             raise UserError(u'\n\n '.join(failures))