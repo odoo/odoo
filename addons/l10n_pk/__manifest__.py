1fddb50bc6c6d108d916d278a1d51be7cccaf0e2
muab-odoo <muab@odoo.com>
2024-07-01 11:22:59 +0300

    [FIX] l10n_pk: script to auto trigger try_loading
    here->https://github.com/odoo/odoo/pull/164128/commits/049f2a2fad240e738b15794763b48e0e79bf50d3
    We did a big improvement in the pk package but if the user tries to use the new stuff
    He won't have them because he needs to manually trigger the reload button in the configuration page
    
    solution: bump-up the package version and add a local script to automatically trigger that try_loading functionality
    
    task-id#3871597
    
    closes odoo/odoo#171361
    
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/l10n_pk/__manifest__.py
+++ addons/l10n_pk/__manifest__.py
@@ -4,7 +4,7 @@
     'website': 'https://www.odoo.com/documentation/17.0/applications/finance/fiscal_localizations.html',
     'icon': '/account/static/description/l10n.png',
     'countries': ['pk'],
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Accounting/Localizations/Account Charts',
     'description': ' This is the base module to manage chart of accounts and localization for the Pakistan ',
     'depends': [
