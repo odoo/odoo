a6e3f0678e9fb7c2913339b690825bb209bda60a
Lucas Perais <lpe@odoo.com>
2024-07-01 13:23:00 +0200

    [IMP] website: website.controller.page is more usable
    Before this commit, the model that exposes arbitrary model to the world
    (website.controller.page) lacked a few features.
    This commit implement them:
    - added sql unique constrain on the url of the pages
    - open url
    - list and kanban
    
    task-3997658

--- addons/website/models/website_controller_page.py
+++ addons/website/models/website_controller_page.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo.addons.http_routing.models.ir_http import slugify
+from ast import literal_eval
+
+from odoo.addons.http_routing.models.ir_http import slugify, slug
 from odoo import api, fields, models
 
 
@@ -23,7 +25,8 @@ class WebsiteControllerPage(models.Model):
     # Route: /model/<string:page_name_slugified>
     page_name = fields.Char(string="Name", help="The name is used to generate the URL and is shown in the browser title bar", required=True)
     name_slugified = fields.Char(compute="_compute_name_slugified", store=True,
-        string="URL", help="The name of the page usable in a URL")
+        string="URL", help="The name of the page usable in a URL", inverse="_inverse_name_slugified")
+    url_demo = fields.Char(string="Demo URL", compute="_compute_url_demo")
     page_type = fields.Selection(selection=[("listing", "Listing"), ("single", "Single record")],
         default="listing", string="Page Type",
         help="The type of the page. If set, it indicates whether the page displays a list of records or a single record")
@@ -36,6 +39,10 @@ class WebsiteControllerPage(models.Model):
         default="grid",
     )
 
+    _sql_constraints = [
+        ('unique_name_slugified', 'UNIQUE(page_type, name_slugified)', 'url should be unique')
+    ]
+
     @api.constrains('view_id', 'model_id', "model")
     def _check_user_has_model_access(self):
         for record in self:
@@ -45,8 +52,32 @@ class WebsiteControllerPage(models.Model):
     def _compute_name_slugified(self):
         for rec in self:
             if not rec.model_id or not rec.page_type:
+                rec.name_slugified = False
                 continue
             rec.name_slugified = slugify(rec.page_name or '')
+
+    def _inverse_name_slugified(self):
+        for rec in self:
+            rec.name_slugified = slugify(rec.name_slugified)
+
+    @api.depends("name_slugified")
+    def _compute_url_demo(self):
+        for rec in self:
+            if not rec.name_slugified:
+                rec.url_demo = ""
+                continue
+            url = ["", "model", rec.name_slugified]
+            if rec.page_type == "single":
+                url.append("record-slug-[id]")
+            rec.url_demo = "/".join(url)
+
+    def write(self, vals):
+        res = super().write(vals)
+        self.menu_ids.write({
+            "url": f"/model/{self.name_slugified}",
+            "name": self.page_name,
+        })
+        return res
 
     def unlink(self):
         # When a website_controller_page is deleted, the ORM does not delete its
@@ -62,3 +93,14 @@ class WebsiteControllerPage(models.Model):
         # Make sure website._get_menu_ids() will be recomputed
         self.env.registry.clear_cache()
         return super().unlink()
+
+    def open_website_url(self):
+        url = f"/model/{self.name_slugified}"
+        if self.page_type == "single":
+            rec = self.env[self.model_id.model].search(literal_eval(self.record_domain or "[]"), limit=1)
+            if rec:
+                url += f"/{slug(rec)}"
+        return {
+            "type": "ir.actions.act_url",
+            "url": url
+        }
