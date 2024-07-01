christian-rb/odoo-dev

[IMP] spreadsheet_account: Add formula ODOO.RESIDUAL
Problem
---------
There is currently no formula to get the residual amount of accounts in
a given period of time in spreadsheets.

Objective
---------
Add the formula ODOO.RESIDUAL that does just that.

It should take the following paramters:
- account_codes (mandatory)(can be more than one separate by ,): could
be optional, in that case it could set the domain to all
receivables/payables accounts
- due_date_range or expected_date (optional): date range referred to due
dates (not AML date)
- offset (optional): adding the possibility of the offset to be not only
“Year” but also, a specific date or other periods like Month, Quarter.
- company_id (optional)
- include_unposted (optional) (default: false)

Solution
---------
1. Define the spreadsheet function with the correct parameters in
`accounting_function.js`
2. Hook it to the data source so that it can perform server calls
3. Craft that server python function to returns the list a residual
amounts for the given list of parameters.
4. Adds some tests.

Task-3679808

addons/spreadsheet_account/models/account.py
@@ -214,3 +214,48 @@ class AccountMove(models.Model):
         )
         mapped = dict(data)
         return [mapped.get(account_type, []) for account_type in account_types]
+
+    @api.model
+    def get_residual_amount(self, args_list):
+
+        all_accounts, all_lines = self._get_accounts_and_lines_for_all_cells(
+            args_list,
+            extra_aggregates=['amount_residual:array_agg']
+        )
+
+        lines_dict = defaultdict(
+            lambda: (),
+            {(company.id, state, account.id): tuple(val) for company, state, account, *val in all_lines}
+        )
+
+        results = []
+
+        for args in args_list:
+            company_id = args['company_id'] or self.env.company.id
+            company = self.env["res.company"].browse(company_id)
+            states = ['posted', 'draft'] if args['include_unposted'] else ['posted']
+            subcodes = {subcode for subcode in args["codes"] if subcode}
+            start, end = self._get_date_period_boundaries(
+                args['date_range'], company
+            )
+
+            accounts = self.env['account.account']
+            if subcodes:
+                for subcode in subcodes:
+                    accounts += all_accounts.filtered(lambda acc: acc.code.startswith(subcode))
+            else:
+                accounts += all_accounts.filtered(lambda acc: acc.account_type in ["liability_payable", "asset_receivable"])
+
+            cell_amount_residual = 0.0
+            for account in accounts:
+                include_initial_balance = account.include_initial_balance
+                for state in states:
+                    for line_date, line_amount_residual in zip(*lines_dict[(company_id, state, account.id)]):
+                        if (include_initial_balance and line_date > end) \
+                           or (not include_initial_balance and (line_date < start or line_date > end)):
+                            continue
+
+                        cell_amount_residual += line_amount_residual
+
+            results.append(cell_amount_residual)
+        return results

addons/spreadsheet_account/static/src/plugins/accounting_plugin.js
@@ -17,6 +17,7 @@ export class AccountingPlugin extends OdooUIPlugin {
         "getAccountGroupCodes",
         "getFiscalStartDate",
         "getFiscalEndDate",
+        "getAccountResidual",
     ]);
     constructor(config) {
         super(config);
@@ -132,6 +133,29 @@ export class AccountingPlugin extends OdooUIPlugin {
         });
         if (result === false) {
             throw new EvaluationError(_t("The company fiscal year could not be found."));
+        }
+        return result;
+    }
+
+    /**
+     * Gets the residual amount for given account code prefixes over a given period
+     * @param {string[]} codes prefixes of the accounts codes
+     * @param {DateRange} dateRange start date of the period to search
+     * @param {number} offset end date of the period to search
+     * @param {number} companyId specific company to target
+     * @param {boolean} includeUnposted whether or not select unposted entries
+     * @returns {number | undefined}
+     */
+    getAccountResidual(codes, dateRange, offset, companyId, includeUnposted) {
+        dateRange.year += offset;
+
+        const result = this.serverData.batch.get(
+            "account.account",
+            "get_residual_amount",
+            camelToSnakeObject({ codes, dateRange, companyId, includeUnposted })
+        );
+        if (result === false) {
+            throw new EvaluationError(_t("The residual amount for given accounts could not be computed."));
         }
         return result;
     }

odoo/odoo

addons/spreadsheet_account/models/account.py
@@ -224,3 +224,51 @@ class AccountMove(models.Model):
         )
         mapped = dict(data)
         return [mapped.get(account_type, []) for account_type in account_types]
+
+    @api.model
+    def get_residual_amount(self, args_list):
+
+        all_accounts, all_lines = self._get_accounts_and_lines_for_all_cells(
+            args_list,
+            extra_aggregates=['amount_residual:array_agg']
+        )
+
+        lines_dict = defaultdict(
+            lambda: (),
+            {(company.id, state, account.id): tuple(val) for company, state, account, *val in all_lines}
+        )
+
+        results = []
+
+        for args in args_list:
+            company_id = args['company_id'] or self.env.company.id
+            company = self.env["res.company"].browse(company_id)
+            states = ['posted', 'draft'] if args['include_unposted'] else ['posted']
+            subcodes = {subcode for subcode in args["codes"] if subcode}
+            start, _end = self._get_date_period_boundaries(
+                args['date_from'], company
+            )
+            _start, end = self._get_date_period_boundaries(
+                args['date_to'], company
+            )
+
+            accounts = self.env['account.account']
+            if subcodes:
+                for subcode in subcodes:
+                    accounts += all_accounts.filtered(lambda acc: acc.code.startswith(subcode))
+            else:
+                accounts += all_accounts.filtered(lambda acc: acc.account_type in ["liability_payable", "asset_receivable"])
+
+            cell_amount_residual = 0.0
+            for account in accounts:
+                include_initial_balance = account.include_initial_balance
+                for state in states:
+                    for line_date, line_amount_residual in zip(*lines_dict[(company_id, state, account.id)]):
+                        if (include_initial_balance and line_date > end) \
+                           or (not include_initial_balance and (line_date < start or line_date > end)):
+                            continue
+
+                        cell_amount_residual += line_amount_residual
+
+            results.append(cell_amount_residual)
+        return results

addons/spreadsheet_account/static/src/plugins/accounting_plugin.js
@@ -17,6 +17,7 @@ export class AccountingPlugin extends OdooUIPlugin {
         "getAccountGroupCodes",
         "getFiscalStartDate",
         "getFiscalEndDate",
+        "getAccountResidual",
     ]);
     constructor(config) {
         super(config);
@@ -139,6 +140,31 @@ export class AccountingPlugin extends OdooUIPlugin {
         });
         if (result === false) {
             throw new EvaluationError(_t("The company fiscal year could not be found."));
+        }
+        return result;
+    }
+
+    /**
+     * Gets the residual amount for given account code prefixes over a given period
+     * @param {string[]} codes prefixes of the accounts codes
+     * @param {DateRange} dateFrom start date of the period to search
+     * @param {DateRange} dateTo end date of the period to search
+     * @param {number} offset year offset of the period to search
+     * @param {number} companyId specific company to target
+     * @param {boolean} includeUnposted whether or not select unposted entries
+     * @returns {number | undefined}
+     */
+    getAccountResidual(codes, dateFrom, dateTo, offset, companyId, includeUnposted) {
+        dateFrom.year += offset;
+        dateTo.year += offset;
+
+        const result = this.serverData.batch.get(
+            "account.account",
+            "get_residual_amount",
+            camelToSnakeObject({ codes, dateFrom, dateTo, companyId, includeUnposted })
+        );
+        if (result === false) {
+            throw new EvaluationError(_t("The residual amount for given accounts could not be computed."));
         }
         return result;
     }