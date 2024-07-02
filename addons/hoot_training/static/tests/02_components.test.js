e24e9afdc1521f29a2f194bec851f9620680bd63
Julien Mougenot <jum@odoo.com>
2024-07-02 14:03:08 +0200

    [REF] hoot_training: break test 02

--- addons/hoot_training/static/tests/02_components.test.js
+++ addons/hoot_training/static/tests/02_components.test.js
@@ -1,30 +1,28 @@
 import { expect, test } from "@odoo/hoot";
-import { click } from "@odoo/hoot-dom";
-import { animationFrame } from "@odoo/hoot-mock";
-import { mountWithCleanup } from "@web/../tests/web_test_helpers";
+import { mount } from "@odoo/owl";
 
 import { Counter } from "../src/counter";
 
 /**
  * @hint components rendering is ASYNChronous and must be AWAITed
- * @hint `mountWithCleanup()` ("@web/../tests/web_test_helpers")
+ * @hint `mountWithCleanup()` and `animationFrame` ("@web/../tests/web_test_helpers")
+ * @hint input values are strings
  */
-test("counter is properly mounted", async () => {
-    await mountWithCleanup(Counter);
+test.todo("counter is properly mounted", () => {
+    mount(Counter, document.body);
 
     expect("button").toHaveCount(1);
     expect("button").toHaveText("Count:");
-    expect("input").toHaveValue("0");
+    expect("input").toHaveValue(0);
 });
 
 /**
  * @hint `click()` ("@odoo/hoot-dom")
  */
-test("counter is incremented on clicks", async () => {
-    await mountWithCleanup(Counter);
+test.todo("counter is incremented on clicks", () => {
+    mount(Counter, document.body);
 
-    click("button");
-    await animationFrame();
+    document.querySelector("button").click();
 
-    expect("input").toHaveValue("1");
+    expect("input").toHaveValue(1);
 });
