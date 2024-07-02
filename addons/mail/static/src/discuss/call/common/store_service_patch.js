44b6737dfda928e092405c1aaec86e0bf71ade45
tsm-odoo <tsm@odoo.com>
2024-07-02 13:06:20 +0200

    [IMP] mail: move ringing threads field from discuss to store
    The `ringingThreads` field is stored in the discuss model. However,
    rtc calls exist out discuss (e.g. chat windows). Currently, this is
    not an issue since the discuss model is loaded even when not needed
    (e.g. live chat). However, bundles are being refactored to ease the
    development of environment-specific features. As a result, this field
    should be moved to the store, which is always available regardless of
    the environment.
    
    Part of task-3972988
    
    closes odoo/odoo#171533
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/discuss/call/common/store_service_patch.js
+++ addons/mail/static/src/discuss/call/common/store_service_patch.js
@@ -15,6 +15,18 @@ const StorePatch = {
                 return {};
             },
         });
+        this.ringingThreads = Record.many("Thread", {
+            /** @this {import("models").Store} */
+            onUpdate() {
+                if (this.ringingThreads.length > 0) {
+                    this.env.services["mail.sound_effects"].play("incoming-call", {
+                        loop: true,
+                    });
+                } else {
+                    this.env.services["mail.sound_effects"].stop("incoming-call");
+                }
+            },
+        });
         this.allActiveRtcSessions = Record.many("RtcSession");
     },
     onStarted() {
