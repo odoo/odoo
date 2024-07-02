ff505c552f3afdd404272268aa9acf28489e1a6c
Deependra Solanki <deso@odoo.com>
2024-07-02 06:24:31 +0000

    [FIX] web_editor: line breaks at edges of block anchor nested within li
    Commit [1] handled cases of pressing enter at the edge of an anchor, which is a
    child of an unbreakable element. The commit inserted the `br`'s after anchors;
    however, it missed the situation where the anchor tags are block elements nested
    inside an unbreakable element inside a `li`. In this specific case, inserting
    two `br` tags after a anchor block resulted in the creation of two new lines.
    This commit handles that case by only inserting one `br` tag after the anchor,
    rather than both.
    
    [1]: df6f8dd
    
    task-3631910
    
    X-original-commit: 39b517c2794eebed0e77d9e44c130627db4604be

--- addons/web_editor/static/src/js/editor/odoo-editor/test/spec/editor.test.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/test/spec/editor.test.js
@@ -3978,6 +3978,11 @@ X[]
                         contentAfter: '<div><a>ab</a><br><br>[]</div>',
                     });
                     await testEditor(BasicEditor, {
+                        contentBefore: '<div><a style="display: block;">ab[]</a></div>',
+                        stepFunction: pressEnter,
+                        contentAfter: '<div><a style="display: block;">ab</a>[]<br></div>'
+                    })
+                    await testEditor(BasicEditor, {
                         contentBefore: '<div><a>ab[]</a>cd</div>',
                         stepFunction: pressEnter,
                         contentAfter: '<div><a>ab</a><br>[]cd</div>',
