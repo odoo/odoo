d0991085a459361df3370e94e726e86b3d42110f
sabr <sabr@odoo.com>
2024-07-02 05:32:51 +0000

    [FIX] base_automation: fetch ID and improve button functionality
    Specification:
    The automation name and ID are not visible. Additionally, the
    'Disable Automation Rule' and 'Edit Automation Rule' buttons
    are not working in the error dialog.
    
    Expected behavior:
    The automation rule ID and name are visible, and both buttons work as expected.
    
    Task-3959044
    
    X-original-commit: a3873e2ffdb27a4ef47dcec3c2eca1ae282f490d

--- addons/base_automation/static/tests/base_automation_error_dialog.test.js
+++ addons/base_automation/static/tests/base_automation_error_dialog.test.js
@@ -1,6 +1,6 @@
 import { BaseAutomationErrorDialog } from "@base_automation/base_automation_error_dialog";
 import { defineMailModels } from "@mail/../tests/mail_test_helpers";
-import { expect, test } from "@odoo/hoot";
+import { expect, test, getFixture } from "@odoo/hoot";
 import { animationFrame } from "@odoo/hoot-mock";
 import {
     makeServerError,
@@ -60,4 +60,38 @@ test("Error not due to an automation rule", async () => {
     expect(".modal .fa-clipboard").toHaveCount(1);
     expect(".modal .o_disable_action_button").toHaveCount(0);
     expect(".modal .o_edit_action_button").toHaveCount(0);
+});
+
+test("display automation rule id and name in Error dialog", async () => {
+    expect.errors(1);
+    const target = getFixture();
+    const errorContext = {
+        exception_class: "base_automation",
+        base_automation: {
+            id: 1,
+            name: "Test base automation error dialog",
+        },
+    };
+    const error = makeServerError({
+        subType: "Odoo Client Error",
+        message: "Message",
+        context: errorContext,
+        errorName: "automation error",
+    });
+
+    patchWithCleanup(BaseAutomationErrorDialog.prototype, {
+        setup() {
+            expect(this.props.data.context).toEqual(errorContext);
+            expect.step("error setup");
+            super.setup();
+        },
+    });
+    await mountWithCleanup(MainComponentsContainer);
+    Promise.reject(error);
+    await animationFrame();
+    expect.verifyErrors(["Message"]);
+    expect.verifySteps(["error setup"]);
+    expect(target.querySelector(".modal-body p:nth-child(5)").textContent).toBe(
+        " The error occurred during the execution of the automation rule \"Test base automation error dialog\" (ID: 1). "
+    );
 });
