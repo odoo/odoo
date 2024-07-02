2f1d522830c4b964729f32a054deb5142d214363
dhba <dhba@odoo.com>
2024-07-02 15:20:44 +0530

    [ADD] google_address_autocomplete, *: added autocomplete on address field
    *website_sale, website_sale_autocomplete
    
    Purpose:
    Writing addresses manually is error-prone, and a wrong address could lead to
    disappointments in case of on-site meetings, deliveries, snail mail, etc. This
    feature aims to provide accurate address suggestions based on user input to
    enhance the reliability of address entries in various modules such as
    res.company, res.partner, and crm.lead.
    
    Specifications:
    
    - Introduced a new widget google_address_autocomplete compatible with char fields.
    - Integrated new widget with Google Places API to fetch address suggestions
      dynamically based on user input.
    - Automatically populates additional address fields such as zip_field,
      city_field, and country_field when a user selects an address suggestion from
      the dropdown.
    - Made the widget and its options available in Studio.
    - The widget provides real-time address suggestions as the user types and
      automatically fills in the specified address fields upon selection.
    
    task-3851045

--- addons/website_sale_autocomplete/static/src/js/address_form.js
+++ addons/website_sale_autocomplete/static/src/js/address_form.js
@@ -89,14 +89,14 @@ publicWidget.registry.AddressForm = publicWidget.Widget.extend({
 
         // Selects based on odoo ids
         if (address.country) {
-            this.countrySelect.value = address.country;
+            this.countrySelect.value = address.country[0];
             // Let the state select know that the country has changed so that it may fetch the correct states or disappear.
             this.countrySelect.dispatchEvent(new Event('change', {bubbles: true}));
         }
         if (address.state) {
             // Waits for the stateSelect to update before setting the state.
             new MutationObserver((entries, observer) => {
-                this.stateSelect.value = address.state;
+                this.stateSelect.value = address.state[0];
                 observer.disconnect();
             }).observe(this.stateSelect, {
                 childList: true, // Trigger only if the options change
