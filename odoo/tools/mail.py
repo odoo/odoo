christian-rb/odoo-dev

[IMP] mail, im_livechat: copy message link
PURPOSE:

Easily share the content of a message that is in another thread via a link.

SPECIFICAITONS:

- Add a 'copy message link' action under the More menu on message hover.
- When user clicks on this action display a green toast notification stating
  message copied to your clipboard.
- User should be able to paste this link wherever he wants
- Clicking on this should redirect the user to the message and highlight it.
- When clicking on the link, if the message was posted in a channel/discuss
  the user doesn't have access to, display a red toast notification stating
  you don't have access.
- If the message linked was deleted in the meantime, then display
  a red toast notification stating message no longer exists.
- If the person trying to access the page is not connected to Odoo:
  - If the channel is public: redirect the user to the corresponding message.
  - If the channel is private: redirect to the sign up page and then
    to the corresponding message.
- If the user clicks on the link from a chat window,
  open the message in another chat window.
- If the message redirects the user to a channel he has the rights to access
  but that he hasn't pinned yet, it should pin the channel in the process.
- Same behaviour should be available in Mobile view.
- In Chatter:
  - Links should work the same way for messages and notes posted on the chatter.
  - Redirection should be made in a new tab.

task-2365623

odoo/tools/mail.py
@@ -32,7 +32,7 @@ _logger = logging.getLogger(__name__)
 safe_attrs = clean.defs.safe_attrs | frozenset(
     ['style',
      'data-o-mail-quote', 'data-o-mail-quote-node',  # quote detection
-     'data-oe-model', 'data-oe-id', 'data-oe-field', 'data-oe-type', 'data-oe-expression', 'data-oe-translation-initial-sha', 'data-oe-nodeid',
+     'data-oe-model', 'data-oe-id', 'data-oe-res-id', 'data-oe-field', 'data-oe-type', 'data-oe-expression', 'data-oe-translation-initial-sha', 'data-oe-nodeid',
      'data-last-history-steps', 'data-oe-protected', 'data-oe-transient-content',
      'data-publish', 'data-id', 'data-res_id', 'data-interval', 'data-member_id', 'data-scroll-background-ratio', 'data-view-id',
      'data-class', 'data-mimetype', 'data-original-src', 'data-original-id', 'data-gl-filter', 'data-quality', 'data-resize-width',

odoo/odoo

odoo/tools/mail.py
@@ -32,7 +32,7 @@ _logger = logging.getLogger(__name__)
 safe_attrs = defs.safe_attrs | frozenset(
     ['style',
      'data-o-mail-quote', 'data-o-mail-quote-node',  # quote detection
-     'data-oe-model', 'data-oe-id', 'data-oe-field', 'data-oe-type', 'data-oe-expression', 'data-oe-translation-source-sha', 'data-oe-nodeid',
+     'data-oe-model', 'data-oe-id', 'data-oe-res-id', 'data-oe-field', 'data-oe-type', 'data-oe-expression', 'data-oe-translation-source-sha', 'data-oe-nodeid',
      'data-last-history-steps', 'data-oe-protected', 'data-oe-transient-content',
      'data-publish', 'data-id', 'data-res_id', 'data-interval', 'data-member_id', 'data-scroll-background-ratio', 'data-view-id',
      'data-class', 'data-mimetype', 'data-original-src', 'data-original-id', 'data-gl-filter', 'data-quality', 'data-resize-width',