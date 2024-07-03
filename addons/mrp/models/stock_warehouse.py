christian-rb/odoo-dev

[PERF] mrp: refactor bom_find
Confirming a purchase order of 1000 lines
- with only stock & purchase : time is 6.3secs
- by adding mrp : time grows to 8.1secs

Part of the difference comes from the time it takes to explode the
(potential) kits.

The refactor of _bom_find consists of:
- signature change : company_id -> company_ids to allow searching
  within multiple companies in one go
- return value change : the returned dict's key is now a tuple
  ( product, company_id or False)

Calling _bom_find with no company given
: retrieve result with ( product, False)
Calling _bom_find with one or more company_id's
: retrieve result with ( product, company)
-> for _bom_find on record sets having the fields product_id & company_id

With this, use case time falls to 6.9secs

This will obviously have performance impact on many use cases.

task: 3865681

addons/mrp/models/stock_warehouse.py
@@ -297,7 +297,7 @@ class Orderpoint(models.Model):
     def _get_orderpoint_products(self):
         non_kit_ids = []
         for products in split_every(2000, super()._get_orderpoint_products().ids, self.env['product.product'].browse):
-            kit_ids = set(k.id for k in self.env['mrp.bom']._bom_find(products, bom_type='phantom').keys())
+            kit_ids = {p.id for (p, c) in self.env['mrp.bom']._bom_find(products, bom_type='phantom')}
             non_kit_ids.extend(id_ for id_ in products.ids if id_ not in kit_ids)
             products.invalidate_recordset()
         return self.env['product.product'].browse(non_kit_ids)

odoo/odoo

addons/mrp/models/stock_warehouse.py
@@ -297,7 +297,7 @@ class Orderpoint(models.Model):
     def _get_orderpoint_products(self):
         non_kit_ids = []
         for products in split_every(2000, super()._get_orderpoint_products().ids, self.env['product.product'].browse):
-            kit_ids = set(k.id for k in self.env['mrp.bom']._bom_find(products, bom_type='phantom').keys())
+            kit_ids = {p.id for (p, dummy) in self.env['mrp.bom']._bom_find(products, bom_type='phantom')}
             non_kit_ids.extend(id_ for id_ in products.ids if id_ not in kit_ids)
             products.invalidate_recordset()
         return self.env['product.product'].browse(non_kit_ids)