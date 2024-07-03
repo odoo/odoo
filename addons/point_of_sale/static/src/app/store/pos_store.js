ab5cec7bdd6af62dd3f473ff8c4e97fa59cf7311
Pedram (pebr) <pebr@odoo.com>
2024-07-03 14:16:47 +0000

    [FIX] point_of_sale: clear search on new order
    Before this commit, after searching for an article via the search
    button, selecting it, and completing payment, the search term persisted
    in the search field when starting a new order. This behavior could lead
    to confusion or slower workflows for cashiers.
    
    opw-4015200
    
    closes odoo/odoo#172282
    
    X-original-commit: 29dd600d545113b5cf59b58da3b4add10eadfbfa
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/store/pos_store.js
+++ addons/point_of_sale/static/src/app/store/pos_store.js
@@ -388,8 +388,7 @@ export class PosStore extends Reactive {
         window.addEventListener("beforeunload", () =>
             this.db.save("TO_REFUND_LINES", this.toRefundLines)
         );
-        const { start_category, iface_start_categ_id } = this.config;
-        this.selectedCategoryId = (start_category && iface_start_categ_id?.[0]) || 0;
+        this.resetProductScreenSearch();
         // Push orders in background, do not await
         this.push_orders();
         // This method is to load the demo datas.
@@ -2087,6 +2086,12 @@ export class PosStore extends Reactive {
 
     redirectToBackend() {
         window.location = "/web#action=point_of_sale.action_client_pos_menu";
+    }
+
+    resetProductScreenSearch() {
+        this.searchProductWord = "";
+        const { start_category, iface_start_categ_id } = this.config;
+        this.selectedCategoryId = (start_category && iface_start_categ_id?.[0]) || 0;
     }
 }
 
