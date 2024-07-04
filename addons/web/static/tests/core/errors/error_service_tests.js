8efbceb7d5de238c25ed2e00382fd0df5f73a8e1
Pierre Paridans <app@odoo.com>
2024-07-04 11:29:46 +0200

    [FIX] web: make error formatting more resilient cross browsers
    Before this commit, the error service attempted to adapt the
    `error.stack` format to match the one used by Chrome.
    
    But even then, the error_service_tests (that compare exactly the
    traceback) didn't pass on Firefox.
    
    Since version 125, even Chrome itself doesn't pass that test.
    
    This commit adapts the stack formatting code to ensure the actual
    error name is present and its message, but without trying to match a
    (too) specific format.
    
    Also in this commit, the related tests are adapted to be a little less
    strict (and more resilient), but stil ensuring the actual error name and
    message are present.
    
    closes odoo/odoo#172330
    
    X-original-commit: cb363cdaf9c1d9a72b3bb2303d94c2a18005238f
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- addons/web/static/tests/core/errors/error_service_tests.js
+++ addons/web/static/tests/core/errors/error_service_tests.js
@@ -75,7 +75,7 @@ QUnit.test("can handle rejected promise errors with a string as reason", async (
 });
 
 QUnit.test("handle RPC_ERROR of type='server' and no associated dialog class", async (assert) => {
-    assert.expect(2);
+    assert.expect(4);
     const error = new RPCError();
     error.code = 701;
     error.message = "Some strange error occured";
@@ -83,7 +83,7 @@ QUnit.test("handle RPC_ERROR of type='server' and no associated dialog class", a
     error.subType = "strange_error";
     function addDialog(dialogClass, props) {
         assert.strictEqual(dialogClass, RPCErrorDialog);
-        assert.deepEqual(props, {
+        assert.deepEqual(_.omit(props, "traceback"), {
             name: "RPC_ERROR",
             type: "server",
             code: 701,
@@ -93,8 +93,9 @@ QUnit.test("handle RPC_ERROR of type='server' and no associated dialog class", a
             subType: "strange_error",
             message: "Some strange error occured",
             exceptionName: null,
-            traceback: error.stack,
         });
+        assert.ok(props.traceback.indexOf("RPC_ERROR")>= 0);
+        assert.ok(props.traceback.indexOf("Some strange error occured")>= 0);
     }
     serviceRegistry.add("dialog", makeFakeDialogService(addDialog), { force: true });
     await makeTestEnv();
@@ -109,7 +110,7 @@ QUnit.test("handle RPC_ERROR of type='server' and no associated dialog class", a
 QUnit.test(
     "handle custom RPC_ERROR of type='server' and associated custom dialog class",
     async (assert) => {
-        assert.expect(2);
+        assert.expect(4);
         class CustomDialog extends Component {}
         CustomDialog.template = xml`<RPCErrorDialog title="'Strange Error'"/>`;
         CustomDialog.components = { RPCErrorDialog };
@@ -123,7 +124,7 @@ QUnit.test(
         error.data = errorData;
         function addDialog(dialogClass, props) {
             assert.strictEqual(dialogClass, CustomDialog);
-            assert.deepEqual(props, {
+            assert.deepEqual(_.omit(props, "traceback"), {
                 name: "RPC_ERROR",
                 type: "server",
                 code: 701,
@@ -131,8 +132,9 @@ QUnit.test(
                 subType: null,
                 message: "Some strange error occured",
                 exceptionName: null,
-                traceback: error.stack,
             });
+            assert.ok(props.traceback.indexOf("RPC_ERROR")>= 0);
+            assert.ok(props.traceback.indexOf("Some strange error occured")>= 0);
         }
         serviceRegistry.add("dialog", makeFakeDialogService(addDialog), { force: true });
         await makeTestEnv();
@@ -149,7 +151,7 @@ QUnit.test(
 QUnit.test(
     "handle normal RPC_ERROR of type='server' and associated custom dialog class",
     async (assert) => {
-        assert.expect(2);
+        assert.expect(4);
         class CustomDialog extends Component {}
         CustomDialog.template = xml`<RPCErrorDialog title="'Strange Error'"/>`;
         CustomDialog.components = { RPCErrorDialog };
@@ -166,7 +168,7 @@ QUnit.test(
         error.data = errorData;
         function addDialog(dialogClass, props) {
             assert.strictEqual(dialogClass, NormalDialog);
-            assert.deepEqual(props, {
+            assert.deepEqual(_.omit(props, "traceback"), {
                 name: "RPC_ERROR",
                 type: "server",
                 code: 701,
@@ -174,8 +176,9 @@ QUnit.test(
                 subType: null,
                 message: "A normal error occured",
                 exceptionName: "normal_error",
-                traceback: error.stack,
             });
+            assert.ok(props.traceback.indexOf("RPC_ERROR")>= 0);
+            assert.ok(props.traceback.indexOf("A normal error occured")>= 0);
         }
         serviceRegistry.add("dialog", makeFakeDialogService(addDialog), { force: true });
         await makeTestEnv();
@@ -317,11 +320,12 @@ QUnit.test("handle uncaught promise errors", async (assert) => {
 
     function addDialog(dialogClass, props) {
         assert.strictEqual(dialogClass, ClientErrorDialog);
-        assert.deepEqual(props, {
+        assert.deepEqual(_.omit(props, "traceback"), {
             name: "UncaughtPromiseError > TestError",
             message: "Uncaught Promise > This is an error test",
-            traceback: error.stack,
         });
+        assert.ok(props.traceback.indexOf("TestError")>= 0);
+        assert.ok(props.traceback.indexOf("This is an error test")>= 0);
     }
     serviceRegistry.add("dialog", makeFakeDialogService(addDialog), { force: true });
     await makeTestEnv();
