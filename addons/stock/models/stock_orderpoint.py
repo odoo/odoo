7121add5f25ddf06a82662dfce36f56bffb8db64
Djamel Touati <otd@odoo.com>
2024-07-04 16:42:37 +0200

    [FIX] purchase_stock, mrp: hide the "order once" button after clicking
    Steps to reproduce the bug:
    - Create a storable product “P1”:
        - Create a reorder rule:
            - Min qty: 5
            - Route: Buy
        - Click the “order once” button
    
    Problem:
    The button does not become invisible; the page must be refreshed to see
    the update. This can lead to user error if the user clicks the
    “order once” button a second time.
    
    opw-3994600
    
    X-original-commit: 9dda3c335c6963111a9472b1957b6213bede4b16

--- addons/stock/models/stock_orderpoint.py
+++ addons/stock/models/stock_orderpoint.py
@@ -519,6 +519,7 @@ class StockWarehouseOrderpoint(models.Model):
                         'url': f'/web#action={action.id}&id={move.picking_id.id}&model=stock.picking&view_type=form'
                     }],
                     'sticky': False,
+                    'next': {'type': 'ir.actions.act_window_close'},
                 }
             }
         return False
