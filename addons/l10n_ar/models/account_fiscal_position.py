06b3ba4fc27fc06fd58fae37afcaaf230179df8b
Anh Thao Pham (pta) <pta@odoo.com>
2024-07-04 10:51:06 +0000

    [FIX] l10n_ar: only select "AFIP Responsibility" fiscal position if set
    Steps to reproduce:
    - Install l10n_ar and website_sale
    - Switch to an Argentinian company
    - Create a product with a customer tax (e.g. IVA 21%)
    With a public user or a user without "AFIP Responsibility":
    - Go to eShop and add the created product to the cart
    - Go to the cart
    
    Issue:
    No tax is applied.
    
    Cause:
    There are some fiscal positions from Argentinian localization that map
    IVA 21% to IVA Exento (i.e. 0%).
    These fiscal positions are linked to specific "AFIP Responsibility" types
    and should only be applied for users having these "AFIP Responsibility"
    types configured on their contact form.
    However, when this field is not set, it is ignored in the domain computing
    the fiscal position and one of these Argentinian fiscal positions satisfies
    the resulting domain.
    
    Solution:
    Filter on "AFIP Responsibility" in the domain computing the fiscal position
    if "l10n_ar_afip_responsibility_type_id" key is present in the context, no
    matter what its value.
    
    opw-3943108
    
    X-original-commit: ff3089951323dca66c0b44100754c7d768e0d4af

--- addons/l10n_ar/models/account_fiscal_position.py
+++ addons/l10n_ar/models/account_fiscal_position.py
@@ -13,9 +13,31 @@ class AccountFiscalPosition(models.Model):
 
     def _get_fpos_ranking_functions(self, partner):
         if self.env.company.country_id.code != "AR":
+<<<<<<< saas-17.4
             return super()._get_fpos_ranking_functions(partner)
         return [
             ('l10n_ar_afip_responsibility_type_id', lambda fpos: (
                 partner.l10n_ar_afip_responsibility_type_id in fpos.l10n_ar_afip_responsibility_type_ids
             ))
         ] + super()._get_fpos_ranking_functions(partner)
+||||||| d8566590ca012f6e6e751cbab883fc4a645e08c7
+            return super()._get_fiscal_position(partner, delivery=delivery)
+        return super(AccountFiscalPosition, self.with_context(l10n_ar_afip_responsibility_type_id=partner.l10n_ar_afip_responsibility_type_id.id))._get_fiscal_position(partner, delivery=delivery)
+
+    def _prepare_fpos_base_domain(self, vat_required):
+        domain = super()._prepare_fpos_base_domain(vat_required)
+        if self._context.get('l10n_ar_afip_responsibility_type_id'):
+            domain += [('l10n_ar_afip_responsibility_type_ids', '=', self._context.get('l10n_ar_afip_responsibility_type_id'))]
+        return domain
+=======
+            return super()._get_fiscal_position(partner, delivery=delivery)
+        return super(AccountFiscalPosition, self.with_context(l10n_ar_afip_responsibility_type_id=partner.l10n_ar_afip_responsibility_type_id.id))._get_fiscal_position(partner, delivery=delivery)
+
+    def _prepare_fpos_base_domain(self, vat_required):
+        domain = super()._prepare_fpos_base_domain(vat_required)
+        if 'l10n_ar_afip_responsibility_type_id' in self._context:
+            domain += ['|',
+                ('l10n_ar_afip_responsibility_type_ids', '=', False),
+                ('l10n_ar_afip_responsibility_type_ids', '=', self._context.get('l10n_ar_afip_responsibility_type_id'))]
+        return domain
+>>>>>>> 83858421e769af0c9a7b47499dea0ae600d304b6
