920b8f3534aef311da44383c326c1224392e0fff
Géry Debongnie <ged@odoo.com>
2024-07-05 14:44:13 +0200

    [REF] html_editor: update DOMpurify library

--- addons/html_editor/static/src/core/sanitize_plugin.js
+++ addons/html_editor/static/src/core/sanitize_plugin.js
@@ -9,7 +9,18 @@ export class SanitizePlugin extends Plugin {
         }
         this.DOMPurify = DOMPurify(this.document.defaultView);
     }
-    sanitize(...args) {
-        return this.DOMPurify.sanitize(...args);
+    /**
+     * Sanitizes in place an html element. Current implementation uses the
+     * DOMPurify library.
+     *
+     * @param {HTMLElement} elem
+     * @returns {HTMLElement} the element itself
+     */
+    sanitize(elem) {
+        return this.DOMPurify.sanitize(elem, {
+            IN_PLACE: true,
+            ADD_TAGS: ["#document-fragment", "fake-el"],
+            ADD_ATTR: ["contenteditable"],
+        });
     }
 }
