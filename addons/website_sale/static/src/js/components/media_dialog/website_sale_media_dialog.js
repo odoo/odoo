christian-rb/odoo-dev

[IMP] website_sale: added support for multiple image uploads
Before this commit:
- Uploading multiple images on a product from backend was tricky as it
only allowed single file upload.
- There are no quick options on the media attached for edit or delete.

After this commit:
- Old form view for uploading images got replaced by MediaDialog, which
supports multiple image uploads, and has a cleaner view for image and video
upload screens.
- Addition of Edit and Remove button over the image, for quick re-opening
of MediaDialog to replace or remove the item respectively.

Task-3390792

addons/website_sale/static/src/js/components/media_dialog/website_sale_media_dialog.js
@@ -1,0 +1,38 @@
+import { MediaDialog, TABS } from "@web_editor/components/media_dialog/media_dialog";
+
+export class MultiMediaDialog extends MediaDialog {
+    setup() {
+        super.setup();
+        this.addTab(TABS.VIDEOS);
+    }
+
+    /**
+     * @override
+     */
+    async save() {
+        if (this.errorMessages[this.state?.activeTab]) {
+            this.notificationService.add(this.errorMessages[this.state.activeTab], {
+                type: "danger",
+            });
+            return;
+        }
+        if (this.state.activeTab == "IMAGES") {
+            const attachments = this.selectedMedia[this.state.activeTab];
+            const preloadedAttachments = attachments.filter((attachment) => attachment.res_model);
+            this.selectedMedia[this.state.activeTab] = attachments.filter(
+                (attachment) => !preloadedAttachments.includes(attachment)
+            );
+            if (this.selectedMedia[this.state.activeTab].length > 0) {
+                await super.save();
+                const newAttachments = this.selectedMedia[this.state.activeTab];
+                this.props.customImageSave(newAttachments);
+            }
+            if (preloadedAttachments.length) {
+                this.props.customImageSave(preloadedAttachments);
+            }
+        } else {
+            this.props.customVideoSave(this.selectedMedia[this.state.activeTab]);
+        }
+        this.props.close();
+    }
+}

addons/website_sale/static/src/js/components/media_dialog/website_sale_one2many.js
@@ -1,0 +1,96 @@
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { X2ManyField, x2ManyField } from "@web/views/fields/x2many/x2many_field";
+import { MultiMediaDialog } from "./website_sale_media_dialog";
+
+/*
+ * This method is copied from enterprise/knowledge/static/src/js/knowledge_utils.js
+ * We can't import it directly because it adds a dependency on enterprise.
+ */
+export function getVideoUrl(platform, videoId, params) {
+    let url;
+    switch (platform) {
+        case "youtube":
+            url = new URL(`https://www.youtube.com/embed/${videoId}`);
+            break;
+        case "vimeo":
+            url = new URL(`https://player.vimeo.com/video/${videoId}`);
+            break;
+        case "dailymotion":
+            url = new URL(`https://www.dailymotion.com/embed/video/${videoId}`);
+            break;
+        case "instagram":
+            url = new URL(`https://www.instagram.com/p/${videoId}/embed`);
+            break;
+        case "youku":
+            url = new URL(`https://player.youku.com/embed/${videoId}`);
+            break;
+        default:
+            throw new Error();
+    }
+    url.search = new URLSearchParams(params);
+    return url;
+}
+
+export class ProductMediaViewer extends X2ManyField {
+    static template = "website_sale.ProductMediaViewer";
+
+    setup() {
+        super.setup();
+        this.dialogs = useService("dialog");
+        this.orm = useService("orm");
+        this.supportedFields = ["image_1920", "image_1024", "image_512", "image_256", "image_128"];
+    }
+
+    addMedia() {
+        this.dialogs.add(MultiMediaDialog, {
+            save: (el) => {}, // Simple rebound to fake its execution
+            multiImages: true,
+            customImageSave: this.onCustomImageSave.bind(this),
+            customVideoSave: this.onCustomVideoSave.bind(this),
+        });
+    }
+
+    onCustomVideoSave(videoInfo) {
+        const url = getVideoUrl(videoInfo[0].platform, videoInfo[0].videoId, videoInfo[0].params);
+        const videoList = this.props.record.data[this.props.name];
+        videoList.addNewRecord({ position: "bottom" }).then((record) => {
+            record.update({ name: videoInfo[0].platform + " - [Video]", video_url: url.href });
+        });
+    }
+
+    async onCustomImageSave(attachments) {
+        const attachmentIds = attachments.map((attachment) => attachment.id);
+        const attachmentRecords = await this.orm.searchRead(
+            "ir.attachment",
+            [["id", "in", attachmentIds]],
+            ["id", "datas", "name"],
+            {}
+        );
+        attachmentRecords.forEach((attachment) => {
+            const imageList = this.props.record.data[this.props.name];
+            imageList.addNewRecord({ position: "bottom" }).then((record) => {
+                const activeFields = imageList.activeFields;
+                const updateData = {};
+                for (const field in activeFields) {
+                    if (attachment.datas && this.supportedFields.includes(field)) {
+                        updateData[field] = attachment.datas;
+                        updateData["name"] = attachment.name;
+                    }
+                }
+                record.update(updateData);
+            });
+        });
+    }
+
+    async onAdd({ context, editable } = {}) {
+        this.addMedia();
+    }
+}
+
+export const productMediaViewer = {
+    ...x2ManyField,
+    component: ProductMediaViewer,
+};
+
+registry.category("fields").add("product_media_viewer", productMediaViewer);

odoo/odoo

addons/website_sale/static/src/js/components/media_dialog/website_sale_media_dialog.js
@@ -1,0 +1,38 @@
+import { MediaDialog, TABS } from '@web_editor/components/media_dialog/media_dialog';
+
+export class MultiMediaDialog extends MediaDialog {
+    setup() {
+        super.setup();
+        this.addTab(TABS.VIDEOS);
+    }
+
+    /**
+     * @override
+     */
+    async save() {
+        if (this.errorMessages[this.state?.activeTab]) {
+            this.notificationService.add(this.errorMessages[this.state.activeTab], {
+                type: "danger",
+            });
+            return;
+        }
+        if (this.state.activeTab == "IMAGES") {
+            const attachments = this.selectedMedia[this.state.activeTab];
+            const preloadedAttachments = attachments.filter((attachment) => attachment.res_model);
+            this.selectedMedia[this.state.activeTab] = attachments.filter(
+                (attachment) => !preloadedAttachments.includes(attachment)
+            );
+            if (this.selectedMedia[this.state.activeTab].length > 0) {
+                await super.save();
+                const newAttachments = this.selectedMedia[this.state.activeTab];
+                this.props.imageSave(newAttachments);
+            }
+            if (preloadedAttachments.length) {
+                this.props.imageSave(preloadedAttachments);
+            }
+        } else {
+            this.props.videoSave(this.selectedMedia[this.state.activeTab]);
+        }
+        this.props.close();
+    }
+}

addons/website_sale/static/src/js/components/media_dialog/website_sale_one2many.js
@@ -1,0 +1,96 @@
+import { registry } from '@web/core/registry';
+import { useService } from '@web/core/utils/hooks';
+import { X2ManyField, x2ManyField } from '@web/views/fields/x2many/x2many_field';
+import { MultiMediaDialog } from './website_sale_media_dialog';
+
+/*
+ * This method is copied from enterprise/knowledge/static/src/js/knowledge_utils.js
+ * We can't import it directly because it adds a dependency on enterprise.
+ */
+export function getVideoUrl(platform, videoId, params) {
+    let url;
+    switch (platform) {
+        case "youtube":
+            url = new URL(`https://www.youtube.com/embed/${videoId}`);
+            break;
+        case "vimeo":
+            url = new URL(`https://player.vimeo.com/video/${videoId}`);
+            break;
+        case "dailymotion":
+            url = new URL(`https://www.dailymotion.com/embed/video/${videoId}`);
+            break;
+        case "instagram":
+            url = new URL(`https://www.instagram.com/p/${videoId}/embed`);
+            break;
+        case "youku":
+            url = new URL(`https://player.youku.com/embed/${videoId}`);
+            break;
+        default:
+            throw new Error();
+    }
+    url.search = new URLSearchParams(params);
+    return url;
+}
+
+export class ProductMediaViewer extends X2ManyField {
+    static template = "website_sale.ProductMediaViewer";
+
+    setup() {
+        super.setup();
+        this.dialogs = useService("dialog");
+        this.orm = useService("orm");
+        this.supportedFields = ["image_1920", "image_1024", "image_512", "image_256", "image_128"];
+    }
+
+    addMedia() {
+        this.dialogs.add(MultiMediaDialog, {
+            save: (el) => {}, // Simple rebound to fake its execution
+            multiImages: true,
+            imageSave: this.onImageSave.bind(this),
+            videoSave: this.onVideoSave.bind(this),
+        });
+    }
+
+    onVideoSave(videoInfo) {
+        const url = getVideoUrl(videoInfo[0].platform, videoInfo[0].videoId, videoInfo[0].params);
+        const videoList = this.props.record.data[this.props.name];
+        videoList.addNewRecord({ position: "bottom" }).then((record) => {
+            record.update({ name: videoInfo[0].platform + " - [Video]", video_url: url.href });
+        });
+    }
+
+    async onImageSave(attachments) {
+        const attachmentIds = attachments.map((attachment) => attachment.id);
+        const attachmentRecords = await this.orm.searchRead(
+            "ir.attachment",
+            [["id", "in", attachmentIds]],
+            ["id", "datas", "name"],
+            {}
+        );
+        attachmentRecords.forEach((attachment) => {
+            const imageList = this.props.record.data[this.props.name];
+            imageList.addNewRecord({ position: "bottom" }).then((record) => {
+                const activeFields = imageList.activeFields;
+                const updateData = {};
+                for (const field in activeFields) {
+                    if (attachment.datas && this.supportedFields.includes(field)) {
+                        updateData[field] = attachment.datas;
+                        updateData["name"] = attachment.name;
+                    }
+                }
+                record.update(updateData);
+            });
+        });
+    }
+
+    async onAdd({ context, editable } = {}) {
+        this.addMedia();
+    }
+}
+
+export const productMediaViewer = {
+    ...x2ManyField,
+    component: ProductMediaViewer,
+};
+
+registry.category("fields").add("product_media_viewer", productMediaViewer);