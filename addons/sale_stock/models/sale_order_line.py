a093ddc4a75454d9f9b626ba1684acb0854be26e
Mohammad Abdulmoneim <abdu@odoo.com>
2024-07-05 20:02:34 +0300

    [IMP] {,sale_}stock: enable selecting routes from different companies
    This commit enables selecting a route from a different company on sale
    order lines.
    
    Task-4034713
    
    Part-of: odoo/odoo#172457
    Related: odoo/enterprise#66334
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/sale_stock/models/sale_order_line.py
+++ addons/sale_stock/models/sale_order_line.py
@@ -14,7 +14,7 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     qty_delivered_method = fields.Selection(selection_add=[('stock_move', 'Stock Moves')])
-    route_id = fields.Many2one('stock.route', string='Route', domain=[('sale_selectable', '=', True)], ondelete='restrict', check_company=True)
+    route_id = fields.Many2one('stock.route', string='Route', domain=[('sale_selectable', '=', True)], ondelete='restrict')
     move_ids = fields.One2many('stock.move', 'sale_line_id', string='Stock Moves')
     virtual_available_at_date = fields.Float(compute='_compute_qty_at_date', digits='Product Unit of Measure')
     scheduled_date = fields.Datetime(compute='_compute_qty_at_date')
