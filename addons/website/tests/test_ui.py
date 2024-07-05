5ab8e7a27112b5f6390911b1d006f8f111ea6f6d
paru-odoo <paru@odoo.com>
2024-07-05 12:04:56 +0000

    [FIX] web_editor,website : remove URL when video is inserted in options
    Steps to reproduce:
    1.Add any image block ( here Text - Image )
    2.Add the URL on the image
    3.Now edit the image and add the video to it; you will notice that the
    URL remains as it is in the toolbar.
    4.Click on Save
    
    Issue:
    For versions 15.0-16.0, the link and URL both remain.
    For version 17.0-Master, the video element gets removed unexpectedly.
    
    Expected Behavior:
    When a video is inserted, the URL should be automatically removed to
    avoid element removal or loss of responsiveness.
    
    Issue:
    The issue of element get removed or loss of responsivness is because of
    not removing url link when we insert video.
    
    Solution:
    This commit addresses the issue by removing the URL link when a video is
    inserted in place of an image, ensuring the video element is retained
    and options are hidden appropriately.
    
    task-4023555
    
    X-original-commit: 36594d04a8909dd40ab6384f387c372ddae62345

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -643,3 +643,6 @@ class TestUi(odoo.tests.HttpCase):
 
     def test_snippet_carousel(self):
         self.start_tour('/', 'snippet_carousel', login='admin')
+
+    def test_media_iframe_video(self):
+        self.start_tour("/", "website_media_iframe_video", login="admin")
