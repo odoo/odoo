afe30b249d61308b9e1cf1aecb810c4b1cb64de2
Deependra Solanki <deso@odoo.com>
2024-07-05 14:57:50 +0530

    [FIX] web_editor: prevent text overflow in banner
    Before this commit:
    
    Long texts overflowed beyond the banner's boundaries.
    
    After this commit:
    
    Ensure that text remains within the banner's limits to improve readability and
    visual consistency.
    
    task-4037185
    
    closes odoo/odoo#172071
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/tests/banner_tests.js
+++ addons/web_editor/static/tests/banner_tests.js
@@ -75,7 +75,7 @@ QUnit.module(
                 editable.innerHTML,
                 `<p>Test</p><div class="o_editor_banner o_not_editable lh-1 d-flex align-items-center alert alert-info pb-0 pt-3" role="status" data-oe-protected="true" contenteditable="false">
                         <i class="fs-4 fa fa-info-circle mb-3" aria-label="Banner Info"></i>
-                        <div class="w-100 ms-3" data-oe-protected="false" contenteditable="true">
+                        <div class="w-100 px-3" data-oe-protected="false" contenteditable="true">
                             <p placeholder=\"Type &quot;/&quot; for commands\" class=\"oe-hint oe-command-temporary-hint\"><br></p>
                         </div>
                     </div><p><br></p>`,
@@ -94,7 +94,7 @@ QUnit.module(
                 editable.innerHTML,
                 `<p>Test</p><div class="o_editor_banner o_not_editable lh-1 d-flex align-items-center alert alert-info pb-0 pt-3" role="status" data-oe-protected="true" contenteditable="false">
                         <i class="fs-4 fa fa-info-circle mb-3" aria-label="Banner Info"></i>
-                        <div class="w-100 ms-3" data-oe-protected="false" contenteditable="true">
+                        <div class="w-100 px-3" data-oe-protected="false" contenteditable="true">
                             <p placeholder=\"Type &quot;/&quot; for commands\" class=\"oe-hint oe-command-temporary-hint\"><br></p>
                         </div>
                     </div><p><br></p>`,
@@ -122,7 +122,7 @@ QUnit.module(
                 editable.innerHTML,
                 `<p>Test</p><div class="o_editor_banner o_not_editable lh-1 d-flex align-items-center alert alert-info pb-0 pt-3" role="status" data-oe-protected="true" contenteditable="false">
                         <i class="fs-4 fa fa-info-circle mb-3" aria-label="Banner Info"></i>
-                        <div class="w-100 ms-3" data-oe-protected="false" contenteditable="true">
+                        <div class="w-100 px-3" data-oe-protected="false" contenteditable="true">
                             <p placeholder=\"Type &quot;/&quot; for commands\" class=\"oe-hint oe-command-temporary-hint\"><br></p></div>
                     </div><p><br></p>`,
             );
