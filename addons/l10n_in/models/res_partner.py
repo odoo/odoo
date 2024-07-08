e3c0e6e55b5abf300a4a7081e3b94c921a59f715
Zeel Patel <zepa@odoo.com>
2024-07-08 12:44:37 +0530

    [IMP] l10n_in, account: enhance UoM Indian Code and Refine Journal Dashboard Template
    In this commit:
    
    - **UoM Indian Code Update:** Revised and standardized the Indian-specific
    unit of measure (UoM) code for improved accuracy.
    - **Journal Dashboard Template:** Enhanced the `JournalBodySalePurchase`
    template for better clarity and usability.
    - **Method Refactoring:** Relocated the `_get_partner_vals_by_vat()` method to
    a common module for reusability across different modules.
    
    Task ID: 3551097
    
    closes odoo/odoo#175644
    
    Related: odoo/enterprise#67936
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/l10n_in/models/res_partner.py
+++ addons/l10n_in/models/res_partner.py
@@ -72,3 +72,28 @@ class ResPartner(models.Model):
         if vat == TEST_GST_NUMBER:
             return True
         return super().check_vat_in(vat)
+
+    @api.model
+    def _l10n_in_get_partner_vals_by_vat(self, vat):
+        partner_details = self.read_by_vat(vat)
+        partner_data = partner_details[0] if partner_details else {}
+        if partner_data:
+            partner_gid = partner_data.get('partner_gid')
+            if partner_gid:
+                partner_data = self.enrich_company(company_domain=None, partner_gid=partner_gid, vat=partner_data.get('vat'))
+                partner_data = self._iap_replace_logo(partner_data)
+            return {
+                'name': partner_data.get('name'),
+                'company_type': 'company',
+                'partner_gid': partner_gid,
+                'vat': partner_data.get('vat'),
+                'l10n_in_gst_treatment': 'regular',
+                'image_1920': partner_data.get('image_1920'),
+                'street': partner_data.get('street'),
+                'street2': partner_data.get('street2'),
+                'city': partner_data.get('city'),
+                'state_id': partner_data.get('state_id', {}).get('id', False),
+                'country_id': partner_data.get('country_id', {}).get('id', False),
+                'zip': partner_data.get('zip'),
+            }
+        return {}
