baef0053bab93f1049e6e63a3b17df2603171248
Arnold Moyaux <arm@odoo.com>
2024-07-08 16:30:17 +0200

    [ADD] stock_fleet: dispatch management for transport

---
+++ addons/stock_fleet/__manifest__.py
@@ -0,0 +1,21 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Stock Transport',
+    'summary': 'Stock Transport: Dispatch Management System',
+    'description': 'Transport Management: organize packs in your fleet, or carriers.',
+    'version': '1.0',
+    'depends': ['stock_picking_batch', 'fleet'],
+    'demo': [
+        'data/stock_fleet_demo.xml',
+    ],
+    'data': [
+        'views/fleet_vehicle_model.xml',
+        'views/stock_picking_batch.xml',
+        'views/stock_picking_type.xml',
+        'views/stock_picking_view.xml',
+        'report/report_picking_batch.xml',
+        'views/stock_location.xml',
+    ],
+    'license': 'LGPL-3',
+}
