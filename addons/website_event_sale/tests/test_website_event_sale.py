335664bec2fcac842abb388248af10682cb812e0
Clément Cardot <cacl@odoo.com>
2024-07-08 12:29:01 +0200

    [FIX] website_event_sale: free tickets in SO if mix with paid ticket
    In case of a mix of free/paid tickets, we wants to consider free tickets in the
    SO if their is also paid ticket from the same event. Otherwise we just want to
    trigger the event confirmation page without triggering the SO.
    (event if it contains tickets for other events)
    
    task-4019577

--- addons/website_event_sale/tests/test_website_event_sale.py
+++ addons/website_event_sale/tests/test_website_event_sale.py
@@ -5,16 +5,16 @@ from odoo.addons.website_event_sale.tests.common import TestWebsiteEventSaleComm
 
 class TestWebsiteEventSale(HttpCaseWithUserPortal, TestWebsiteEventSaleCommon):
 
-    def test_website_event_sale(self):
+    def test_website_event_sale_free_tickets(self):
         """ Test saleorder is created for tickets having price only """
         self.authenticate('portal', 'portal')
         free_ticket = self.env['event.event.ticket'].create({
             'event_id': self.event.id,
             'name': 'Free',
-            'product_id': self.product_event.id,
             'price': 0,
         })
         event_questions = self.event.question_ids
+        event_registration_count = len(self.event.registration_ids)
         name_question = event_questions.filtered(lambda q: q.question_type == 'name')
         email_question = event_questions.filtered(lambda q: q.question_type == 'email')
         phone_question = event_questions.filtered(lambda q: q.question_type == 'phone')
@@ -29,8 +29,22 @@ class TestWebsiteEventSale(HttpCaseWithUserPortal, TestWebsiteEventSaleCommon):
             ('partner_id', '=', self.partner_portal.id),
             ('order_line.event_ticket_id', '=', free_ticket.id)
         ]), "Sale order should not be created for the free tickets")
-        self.assertEqual(len(self.event.registration_ids), 1)
+        self.assertEqual(len(self.event.registration_ids), event_registration_count + 1)
 
+    def test_website_event_sale_free_paid_mix(self):
+        """ Test saleorder is created for tickets having price only """
+        self.authenticate('portal', 'portal')
+        free_ticket = self.env['event.event.ticket'].create({
+            'event_id': self.event.id,
+            'name': 'Free',
+            # 'product_id': self.product_event.id,
+            'price': 0,
+        })
+        event_questions = self.event.question_ids
+        event_registration_count = len(self.event.registration_ids)
+        name_question = event_questions.filtered(lambda q: q.question_type == 'name')
+        email_question = event_questions.filtered(lambda q: q.question_type == 'email')
+        phone_question = event_questions.filtered(lambda q: q.question_type == 'phone')
         self.url_open(f'/event/{self.event.id}/registration/confirm', data={
             f'1-name-{name_question.id}': 'Bob',
             f'1-email-{email_question.id}': 'bob@test.lan',
@@ -43,12 +57,9 @@ class TestWebsiteEventSale(HttpCaseWithUserPortal, TestWebsiteEventSaleCommon):
             'csrf_token': http.Request.csrf_token(self),
         })
 
-        self.assertEqual(len(self.event.registration_ids), 3)
-        self.assertFalse(self.env['sale.order'].search([
-            ('partner_id', '=', self.partner_portal.id),
-            ('order_line.event_ticket_id', '=', free_ticket.id)
-        ]), "Sale order should not be created for the free tickets")
+        self.assertEqual(len(self.event.registration_ids), event_registration_count + 2)
         self.assertTrue(self.env['sale.order'].search([
             ('partner_id', '=', self.partner_portal.id),
-            ('order_line.event_ticket_id', '=', self.ticket.id)
-        ]), "Sale order should be created for the paid tickets")
+            ('order_line.event_ticket_id', '=', self.ticket.id),
+            ('order_line.event_ticket_id', '=', free_ticket.id)
+        ]), "Sale order should be created for the free/paid tickets mix")
