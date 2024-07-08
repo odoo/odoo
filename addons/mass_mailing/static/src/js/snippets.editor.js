c12e4438a7b229c4c3459da27f9e74c1074028eb
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-07-08 15:04:33 +0000

    [FIX] mass_mailing: auto  save after image change
    Issue:
    ======
    Image changes aren't auto saved.
    
    Steps to reproduce the issue:
    =============================
    - Add cover snippet and text-image snipets
    - Change cover image or the image in the text-image snippet
    - Don't click anywhere after that
    - Switch tab to A/B testing for example
    - Go back to mail body
    - The image changes aren't saved
    
    Origin of the issue and solution:
    =================================
    
    When we choose an image from the media dialog, we loose the focus from
    the editable which means `onWysiwygBlur` is not called and as
    consequence `commitChanges` isn't called too.
    
    Since the flow of the destroy and the `commitChanges` in `onWysiwygBlur`
    are in parallel, the following happens:
    - We don't update the value in the blur flow because we will wait for
      some promises which may take some time and the component gets
      destroyed and we never call the `updateValue` at the end of the
      function.
    - Now in the `commitChanges` coming from `onWillUnmount` we need to pass
      the `urgent` flag in mass_mailing too.
    
    opw-3947516
    
    closes odoo/odoo#177067
    
    X-original-commit: 3f5232c3222f84adfcc7f1b8488253b293243e35
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Mahdi Cheikh Rouhou (macr) <macr@odoo.com>

--- addons/mass_mailing/static/src/js/snippets.editor.js
+++ addons/mass_mailing/static/src/js/snippets.editor.js
@@ -153,7 +153,7 @@ export class MassMailingSnippetsMenu extends snippetsEditor.SnippetsMenu {
             $oEditable.attr('contenteditable', true);
         }
         // Refocus again to save updates when calling `_onWysiwygBlur`
-        this.$editable.get(0).ownerDocument.defaultView.focus();
+        this.$editable.focus();
     }
     /**
      * @override
