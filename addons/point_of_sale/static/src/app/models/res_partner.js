b0934c7283fc1bab150a94f27d5dad144ea478fe
axtr <axtr@odoo.com>
2024-07-09 15:20:36 +0200

    [FIX] point_of_sale: allow to search a partner with his address
    Problem:
    The searching doesn't work for the address
    
    Steps to reproduce:
    - Install "point_of_sale" app
    - Start a shop session
    - Click on "Customer"
    - Click on the search input and type "Belgium" (for instance)
    - Belgian partners won't be on top of the list and may not be displayed at all (or it is a lucky shot)
    
    opw-4032931
    
    closes odoo/odoo#172386
    
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>

--- addons/point_of_sale/static/src/app/models/res_partner.js
+++ addons/point_of_sale/static/src/app/models/res_partner.js
@@ -6,7 +6,7 @@ export class ResPartner extends Base {
     static pythonModel = "res.partner";
 
     get searchString() {
-        const fields = ['name', 'barcode', 'phone', 'mobile', 'email', 'vat', 'parent_name'];
+        const fields = ['name', 'barcode', 'phone', 'mobile', 'email', 'vat', 'parent_name', 'contact_address'];
         return fields.map((field) => {
             if ((field === 'phone' || field === 'mobile') && this[field]) {
                 return this[field].split(" ").join("");
