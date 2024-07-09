ac2efc8c20a247897d2cdb0105f62282228950df
Pedram (pebr) <pebr@odoo.com>
2024-07-09 11:18:14 +0200

    [FIX] pos*: correctly set default values for a new partner
    Before this commit, creating a new partner incorrectly wrote the
    default values into `this.props.partner`. This commit ensures that
    default values are correctly set for new partners.
    
    opw-4042039
    
    closes odoo/odoo#172399
    
    Related: odoo/enterprise#66310
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/screens/partner_list/partner_list.js
+++ addons/point_of_sale/static/src/app/screens/partner_list/partner_list.js
@@ -148,12 +148,15 @@ export class PartnerList extends Component {
     }
     createPartner() {
         // initialize the edit screen with default details about country, state, and lang
+        this.editPartner(this.newPartnerDefaults());
+    }
+    newPartnerDefaults() {
         const { country_id, state_id } = this.pos.company;
-        this.editPartner({
+        return {
             country_id,
             state_id,
             lang: user.lang,
-        });
+        };
     }
     async saveChanges(processedChanges) {
         let partner;
