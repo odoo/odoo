a009825cb78b4140eb33ceef54533756f9304b45
Pedram (pebr) <pebr@odoo.com>
2024-07-09 09:18:14 +0000

    [FIX] pos*: correctly set default values for a new partner
    Before this commit, creating a new partner incorrectly wrote the
    default values into `this.props.partner`. This commit ensures that
    default values are correctly set for new partners.
    
    opw-4042039
    
    closes odoo/odoo#172504
    
    X-original-commit: ac2efc8c20a247897d2cdb0105f62282228950df
    Related: odoo/enterprise#66362
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/l10n_ar_pos/static/src/overrides/components/partner_list/partner_list.js
+++ addons/l10n_ar_pos/static/src/overrides/components/partner_list/partner_list.js
@@ -4,16 +4,17 @@ import { PartnerList } from "@point_of_sale/app/screens/partner_list/partner_lis
 import { patch } from "@web/core/utils/patch";
 
 patch(PartnerList.prototype, {
-    createPartner() {
-        super.createPartner(...arguments);
-        if (this.props.partner && this.pos.isArgentineanCompany()) {
-            this.props.partner.l10n_latam_identification_type_id = this.pos.models[
+    newPartnerDefaults() {
+        const newPartner = super.newPartnerDefaults(...arguments);
+        if (this.pos.isArgentineanCompany()) {
+            newPartner.l10n_latam_identification_type_id = this.pos.models[
                 "l10n_latam.identification.type"
             ].get(this.pos["l10n_latam.identification.type"][0].id);
 
-            this.props.partner.l10n_ar_afip_responsibility_type_id = this.pos.models[
+            newPartner.l10n_ar_afip_responsibility_type_id = this.pos.models[
                 "l10n_ar.afip.responsibility.type"
             ].get(this.pos["l10n_ar.afip.responsibility.type"][0].id);
         }
+        return newPartner;
     },
 });
