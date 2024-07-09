3792f942091e05808bcf83d1b0953280ec176302
axtr <axtr@odoo.com>
2024-07-09 10:06:28 +0200

    [FIX] point_of_sale: add partner address to partner list
    Problem:
    Partner address is never display in the partner list
    
    Steps to reproduce:
    - Install "point_of_sale" app
    - Start a shop session
    - Click on "Customer"
    - The address is not displayed for any partner
    
    Solution:
    Use field contact_address
    
    opw-4032931

--- addons/point_of_sale/static/tests/tours/helpers/PartnerListTourMethods.js
+++ addons/point_of_sale/static/tests/tours/helpers/PartnerListTourMethods.js
@@ -30,3 +30,44 @@ export function clickPartnerDetailsButton(name) {
         },
     ];
 }
+
+export function checkContactValues(name, address = "", phone = "", mobile = "", email = "") {
+    const steps = [
+        {
+            content: `Check partner "${name}" from partner list screen`,
+            trigger: `.partner-list td:contains("${name}")`,
+            run: () => {},
+        },
+        {
+            content: `Check address "${address}" for partner "${name}"`,
+            trigger: `.partner-list tr:contains("${name}") .partner-line-adress:contains("${address}")`,
+            run: () => {},
+        },
+    ];
+
+    if(phone) {
+        steps.push({
+            content: `Check phone number "${phone}" for partner "${name}"`,
+            trigger: `.partner-list tr:contains("${name}") .partner-line-email:contains("${phone}")`,
+            run: () => {},
+        });
+    }
+
+    if(mobile) {
+        steps.push({
+            content: `Check mobile number "${mobile}" for partner "${name}"`,
+            trigger: `.partner-list tr:contains("${name}") .partner-line-email:contains("${mobile}")`,
+            run: () => {},
+        });
+    }
+
+    if(email) {
+        steps.push({
+            content: `Check email address "${email}" for partner "${name}"`,
+            trigger: `.partner-list tr:contains("${name}") .partner-line-email .email-field:contains("${email}")`,
+            run: () => {},
+        });
+    }
+
+    return steps;
+}
