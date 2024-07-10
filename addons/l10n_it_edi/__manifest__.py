623d87192ee803f8d4c10162548c40be4eef21f9
sergiocorato <sergiocorato@gmail.com>
2024-07-10 10:19:58 +0200

    [16.0][FIX] l10n_it_edi remove auto_install

--- addons/l10n_it_edi/__manifest__.py
+++ addons/l10n_it_edi/__manifest__.py
@@ -12,7 +12,7 @@
         'account_edi',
         'account_edi_proxy_client',
     ],
-    'auto_install': ['l10n_it', 'account_edi'],
+    'auto_install': False,
     'author': 'Odoo',
     'description': """
 E-invoice implementation
