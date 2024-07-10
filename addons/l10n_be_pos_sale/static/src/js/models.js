c6d5fd6b1bae801c9918be06fcddf351a256d32c
odoostan <stgu@odoo.com>
2024-07-10 11:12:53 +0200

    [FIX] l10n_be_pos_sale: force invoice for intracom
    Previously, settling a sale.order from the POS of a Belgian company
    would require the creation of an invoice.
    
    This was actually only necessary when the sale contains intra-community
    (EU) taxes. Those are the taxes that are mapped to (dest_tax_id) by the
    Intra-Community fiscal position.
    See 70f3b74
    
    This commit narrows down the enforcement of the invoice creation to only
    target sales made with such taxes.
    
    opw-3986443

--- addons/l10n_be_pos_sale/static/src/js/models.js
+++ addons/l10n_be_pos_sale/static/src/js/models.js
@@ -2,6 +2,7 @@
 
 import { Order } from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
+import { PosStore } from "@point_of_sale/app/store/pos_store";
 
 patch(Order.prototype, {
     async pay() {
@@ -11,4 +12,13 @@ patch(Order.prototype, {
         }
         return super.pay(...arguments);
     }
+});
+
+patch(PosStore.prototype, {
+    async processServerData(loadedData) {
+        await super.processServerData(...arguments);
+        if (this.company.country_id?.code == "BE") {
+            this.intracom_tax_ids = this.data.custom.intracom_tax_ids;
+        }
+    },
 });
