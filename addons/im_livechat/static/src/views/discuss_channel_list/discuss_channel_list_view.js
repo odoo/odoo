d3202510605118828d908237502cf56ea1e44b44
maad-odoo <maad@odoo.com>
2024-07-10 15:53:06 +0530

    [IMP] im_livechat: add kanban view for session history
    Purpose of this commit:
    Introduce the Kanban view for live chat session history to enable
    easy navigation through sessions.
    
    task-4043438
    
    closes odoo/odoo#172598
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/src/views/discuss_channel_list/discuss_channel_list_view.js
+++ addons/im_livechat/static/src/views/discuss_channel_list/discuss_channel_list_view.js
@@ -1,7 +1,9 @@
-import { DiscussChannelListController } from "@im_livechat/views/discuss_channel_list/discuss_channel_list_view_controller";
-
+import { ListController } from "@web/views/list/list_controller";
+import { LivechatViewControllerMixin } from "../livechat_view_controller_mixin";
 import { listView } from "@web/views/list/list_view";
 import { registry } from "@web/core/registry";
+
+class DiscussChannelListController extends LivechatViewControllerMixin(ListController) {}
 
 const discussChannelListView = {
     ...listView,
