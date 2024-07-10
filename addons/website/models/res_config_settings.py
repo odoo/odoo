christian-rb/odoo-dev

[IMP] website, web_editor: prevent 3rd-party trackers
Commit [1] added support for optional vs required cookies. However, the
website builder did not check those before embedding videos / maps /
snippets of social media even though the services we support all set
3rd-party cookies.

With this commit, we set a few default domains (social platforms, video
hosts and Google services) that should trigger a check of the user's
cookies consent before being loaded. We also add a website setting to
let admins / website editors add other 3rd-party domains to this
checklist.
The complete checklist is compared against iframes' and scripts' `src`.

The commit also adds a fallback message if the elements could not be
loaded in order to inform the user of the situation. The message is
clickable to reopen the cookies bar and accept the cookies afterwards.

Scripts injected client-side are also controlled. In order to be able to
do that, we override the `HTMLScriptElement.src` setter before any other
script is launched on the website. This solution works with most
3rd-party scripts inserted in the `<head>` (typically, Google Tag
Manager asks to add an IIFE that then generates the script tags on page
load: with this solution, the generation is hijacked until optional
cookies are accepted).

Notes:
1. A user connected as a website editor will be shown everything, no
matter their cookie preference.
2. This commit talks about "cookies", but the same logic applies for
other types of trackers.

[1]: https://github.com/odoo/odoo/commit/2cbda6c98ee947cea1d06c09880eee8c758304a8

task-3027209

addons/website/models/res_config_settings.py
@@ -58,6 +58,10 @@ class ResConfigSettings(models.TransientModel):
     website_cookies_bar = fields.Boolean(
         related='website_id.cookies_bar',
         readonly=False)
+    website_block_third_party_domains = fields.Boolean(
+        'Block 3rd-party domains',
+        related='website_id.block_third_party_domains',
+        readonly=False)
     google_analytics_key = fields.Char(
         'Google Analytics Key',
         related='website_id.google_analytics_key',
@@ -223,5 +227,16 @@ class ResConfigSettings(models.TransientModel):
             'res_model': 'website.robots',
             'type': 'ir.actions.act_window',
             "views": [[False, "form"]],
+            'target': 'new',
+        }
+
+    def action_open_blocked_third_party_domains(self):
+        self.website_id._force()
+        return {
+            'name': _("Add external websites"),
+            'view_mode': 'form',
+            'res_model': 'website.blocked_third_party_domains',
+            'type': 'ir.actions.act_window',
+            'views': [[False, "form"]],
             'target': 'new',
         }

odoo/odoo

addons/website/models/res_config_settings.py
@@ -58,6 +58,10 @@ class ResConfigSettings(models.TransientModel):
     website_cookies_bar = fields.Boolean(
         related='website_id.cookies_bar',
         readonly=False)
+    website_block_third_party_domains = fields.Boolean(
+        'Block 3rd-party domains',
+        related='website_id.block_third_party_domains',
+        readonly=False)
     google_analytics_key = fields.Char(
         'Google Analytics Key',
         related='website_id.google_analytics_key',
@@ -223,5 +227,16 @@ class ResConfigSettings(models.TransientModel):
             'res_model': 'website.robots',
             'type': 'ir.actions.act_window',
             "views": [[False, "form"]],
+            'target': 'new',
+        }
+
+    def action_open_blocked_third_party_domains(self):
+        self.website_id._force()
+        return {
+            'name': _("Add external websites"),
+            'view_mode': 'form',
+            'res_model': 'website.custom_blocked_third_party_domains',
+            'type': 'ir.actions.act_window',
+            'views': [[False, "form"]],
             'target': 'new',
         }