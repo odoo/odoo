4117a47945ccc991b23d6f62eab5e4be5c004c8b
Hubert Van De Walle <huvw@odoo.com>
2024-07-10 13:30:30 +0000

    [FIX] project: always evaluate column_invisible in action_project_sharing
    Steps to reproduce
    ==================
    
    As Admin:
    - Install project
    - Go to project
    - Click on the three dots on a project card
    - Select "Share"
    - Copy the link
    - Select the edit mode
    - Add "Joel Willis" to the recipients
    - Click on "Send"
    
    As portal in another browser (or in private navigation)
    - Paste the link
    - Type some random characters in the search bar so that no records are
      matched
    - remove the current filter
    => Every single column is displayed, for example
      `<field name="sequence" readonly="1" column_invisible="True"/>`
    
    As Admin:
    - Go to the shared project settings
    - Disable the "Milestones" checkbox
    
    As portal:
    - Refresh the page
    => The milestone column is still displayed
    
    Cause of the issue
    ==================
    
    The difference between invisible and column_invisible is that
    
    `invisible` is meant to hide a cell in a row and is evaluated with the
    record data (`record.evalContextWithVirtualIds`).
    
    `column_invisible` is meant to remove a column completely for the list,
    but is is not evaluated with the record. It only uses the context and a
    few more keys (`this.model.root.evalContext`).
    
    It is thus not possible to hide an entire column depending on record
    values. It makes sense as the values could be different for every record
    displayed.
    
    In this case though, there are a few fields that have the same values
    for every record. They are in fact related fields, declared on the project.
    
    Those fields are
    - allow_milestones
    - allow_timesheets
    
    The [ProjectSharingListRenderer] has been created to hide some columns
    from being displayed when a feature is disabled on the project
    displayed.
    
    It works by evaluating the column_invisible with the first record.
    If there are no records, we skip any column_invisible processing,
    
    When calling `setColumns` from `onWillUpdateProps`, we use `nextProps`
    for the columns, but still `this.props` to get the first record.
    
    This means that we use an outdated first record, and this is why every
    column is displayed after removing the filter.
    
    Another issue is that
    
    During [View-Pocalypse],
    In 16.0, the milestone_id field was
    
    ```xml
    <field name="milestone_id" attrs="{'column_invisible': [('allow_milestones', '=', False)]}"/>
    ```
    
    In 17.0, it is
    
    ```xml
    <field name="milestone_id" invisible="not allow_milestones" context="{'default_project_id': project_id}" groups="project.group_project_milestone" optional="hide"/>
    ```
    
    Solution
    ========
    
    As there are some limitations to the js approach (when no records are in
    the list for example) and there is already a context key for the
    [allow_timesheets], we use a simpler approach to add the missing keys.
    
    Finally, we put back the column_invisible attributes
    
    ---
    
    [ProjectSharingListRenderer]: https://github.com/odoo/odoo/commit/ab2b5d1fd1f09d804ab410bc326cfebf26d5a7c6
    [View-Pocalypse]: https://github.com/odoo/odoo/pull/104741
    [allow_timesheets]: https://github.com/odoo/odoo/blob/d2a428c07fd728691e3ddd60fe4b7cc5e94455a6/addons/hr_timesheet/models/project_project.py#L290
    
    opw-4015035
    
    X-original-commit: 87c7fb5818853cd3d7d07f57cbdccb0ee69c5360

--- addons/project/static/src/project_sharing/views/list/list_view.js
+++ addons/project/static/src/project_sharing/views/list/list_view.js
@@ -2,8 +2,6 @@
 
 import { listView } from "@web/views/list/list_view";
 
-import { ProjectSharingListRenderer } from "./list_renderer";
-
 const props = listView.props;
 listView.props = function (genericProps, view) {
     const result = props(genericProps, view);
@@ -12,4 +10,3 @@ listView.props = function (genericProps, view) {
         allowSelectors: false,
     };
 };
-listView.Renderer = ProjectSharingListRenderer;
