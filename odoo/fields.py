ccb3aeb6d22f1be7c4ef2ba3c1a81f98ef287f3a
Antoine (ande) <ande@odoo.com>
2024-07-10 18:25:59 +0000

    [FIX] base: force None values to be False
    Current behaviour:
    When trying to make a xmlrpc call to search_read
    a crm lead that has a property, we get a traceback
    
    Steps to reproduce:
    1. Install CRM
    2. Go to the Pipeline
    3. Open any lead
    4. Click on Actions > Add properties
    5. Input anything as value, then save
    6. Make a xmlrpc call to retrieve leads (see below)
    7. Traceback
    
    models.execute_kw(db, uid, password, 'crm.lead', 'search_read', [])
    
    Cause of the issue:
    Feature introduction: https://github.com/odoo/odoo/commit/87307a9010c6c872fe6c746d5ece378ad90d038f
    Change from None to False: https://github.com/odoo/odoo/commit/608bffff59c9444f59a282dbdc43c207704223cc
    Issue caused by: https://github.com/odoo/odoo/commit/da48700b59da49cdc810eac31a8617fcaa0dc2a8
    (Due to the modification of convert_to_read_multi)
    
    When adding PropertiesDefinition to an existing record,
    it doesn't go through _add_default_values again, thus
    making None not being replaced by False.
    https://github.com/odoo/odoo/blob/cd9c69bd7d9b21e6220a49f88f22160de33c5e06/odoo/fields.py#L3616
    
    opw-3940841
    
    X-original-commit: b64a507697381fd7bb205f4a2b2217322d31811a

--- odoo/fields.py
+++ odoo/fields.py
@@ -3792,8 +3792,7 @@ class Properties(Field):
                 # E.G. convert zero to False
                 property_value = bool(property_value)
 
-            elif property_type == 'char' and not isinstance(property_value, str) \
-                    and property_value is not None:
+            elif property_type == 'char' and not isinstance(property_value, str):
                 property_value = False
 
             elif property_value and property_type == 'selection':
