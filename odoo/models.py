odoo/odoo

commit d8fe54d9ac96fc0123ab997f4b4a3fc2f380d34d

[IMP] base: read_group date part number granularity
When doing reporting, especially in spreadsheet, it is usefull to be able
to group dates by different granularities.
Today, Odoo supports Year, Quarter, Month, Week and Day. Those granularities are
"absolute" in the sence that b.e. Quarter will return "Q1 2021"

In this commit, I introduce new granularities.
These granularities will span different years and collect the all dates with specific month, quarter or week:
"month_number", will return  the month of the year of all dates: a number between 1 and 12;
"quarter_number", will return the quarter of the year: a number between 1 and 4;
"iso_week_number", will return the week of the year, according to ISO 8601: a number between 1 and 53,
etc.

Usage:
In read_group or _read_group, the group_by and order by can now be ['date_field:month_number'].
In a domain, you can write [('date_field.month_number','=',2)].
Note that read_group will return a __domain containing the correct relative granularity.

Example:
self.env["my_model"].create(({"value": "1", "date": "2021-02-09"},
		             {"value": "2", "date": "2021-06-01"},
                	     {"value": "3", "date": "2022-02-01"}))
self.env["my_model"].read_group([],fields=['date', 'value'],groupby=['date:month_number'])
-->
[
    {
        'date:month_number': 2.0,
        'date_count': 2,
        'value': 4,
        '__domain': [('date.month_number', '=', 2.0)]
    }, {
        'date:month_number': 6.0,
        'date_count': 1,
        'value': 2,
        '__domain': [('date.month_number', '=', 6.0)]
    }
])

Task: 3836802

odoo/models.py
@@ -349,6 +349,23 @@ READ_GROUP_TIME_GRANULARITY = {
     'year': dateutil.relativedelta.relativedelta(years=1)
 }
 
+READ_GROUP_NUMBER_GRANULARITY = {
+    'year_number': 'year',
+    'quarter_number': 'quarter',
+    'month_number': 'month',
+    'iso_week_number': 'week',  # ISO week number because anything else than ISO is nonsense
+    'day_of_year': 'doy',
+    'day_of_month': 'day',
+    'day_of_week': 'dow',
+    'hour_number': 'hour',
+    'minute_number': 'minute',
+    'second_number': 'second',
+}
+
+READ_GROUP_ALL_TIME_GRANULARITY = READ_GROUP_TIME_GRANULARITY | READ_GROUP_NUMBER_GRANULARITY
+
+
+
 # valid SQL aggregation functions
 READ_GROUP_AGGREGATE = {
     'sum': lambda table, expr: SQL('SUM(%s)', expr),
@@ -1980,7 +1997,7 @@ class BaseModel(metaclass=MetaModel):
         if field.type in ('datetime', 'date') or (field.type == 'properties' and granularity):
             if not granularity:
                 raise ValueError(f"Granularity not set on a date(time) field: {groupby_spec!r}")
-            if granularity not in READ_GROUP_TIME_GRANULARITY:
+            if granularity not in READ_GROUP_ALL_TIME_GRANULARITY:
                 raise ValueError(f"Granularity specification isn't correct: {granularity!r}")
 
             if granularity == 'week':
@@ -1992,10 +2009,32 @@ class BaseModel(metaclass=MetaModel):
                     "(date_trunc('week', %s::timestamp - INTERVAL %s) + INTERVAL %s)",
                     sql_expr, interval, interval,
                 )
+            elif spec := READ_GROUP_NUMBER_GRANULARITY.get(granularity):
+                if granularity == 'day_of_week':
+                    """
+                    formula: ((7 - first_day_of_week_in_odoo) + result_from_SQL) %  --> 0 based first day of week
+                                    week start on
+                                monday   sunday    sat
+                                  1     |  7    |  6   <-- first day of week in odoo
+                           SQL | -----------------------
+                    Monday  1  |  0     |  1    |  2
+                    tuesday 2  |  1     |  2    |  3
+                    wed     3  |  3     |  4    |  4
+                    thurs   4  |  3     |  4    |  5
+                    friday  5  |  4     |  5    |  6
+                    sat     6  |  6     |  0    |  0
+                    sun     7  |  0     |  1    |  1
+                    """
+                    first_week_day = int(get_lang(self.env, self.env.context.get('tz')).week_start)
+                    sql_expr = SQL("mod(7 - %s + date_part(%s, %s)::int, 7)", first_week_day, spec, sql_expr)
+                else:
+                    sql_expr = SQL("date_part(%s, %s)::int", spec, sql_expr)
             else:
                 sql_expr = SQL("date_trunc(%s, %s::timestamp)", granularity, sql_expr)
 
-            if field.type == 'date':
+            # If the granularity is a part number, the result is a number (double) so no conversion is needed
+            if field.type == 'date' and granularity not in READ_GROUP_NUMBER_GRANULARITY:
+                # If the granularity uses date_trunc, we need to convert the timestamp back to a date.
                 sql_expr = SQL("%s::date", sql_expr)
 
         elif field.type == 'boolean':
@@ -2427,11 +2466,11 @@ class BaseModel(metaclass=MetaModel):
             field = self._fields[field_name]
 
             if field.type in ('date', 'datetime'):
-                locale = get_lang(self.env).code
-                fmt = DEFAULT_SERVER_DATETIME_FORMAT if field.type == 'datetime' else DEFAULT_SERVER_DATE_FORMAT
                 granularity = group.split(':')[1] if ':' in group else 'month'
-                interval = READ_GROUP_TIME_GRANULARITY[granularity]
-
+                if granularity in READ_GROUP_TIME_GRANULARITY:
+                    locale = get_lang(self.env).code
+                    fmt = DEFAULT_SERVER_DATETIME_FORMAT if field.type == 'datetime' else DEFAULT_SERVER_DATE_FORMAT
+                    interval = READ_GROUP_TIME_GRANULARITY[granularity]
             elif field.type == "properties":
                 self._read_group_format_result_properties(rows_dict, group)
                 continue
@@ -2482,6 +2521,8 @@ class BaseModel(metaclass=MetaModel):
                                 (field_name, '>=', range_start),
                                 (field_name, '<', range_end),
                         ]
+                    elif value is not None and granularity in READ_GROUP_NUMBER_GRANULARITY:
+                        additional_domain = [(f"{field_name}.{granularity}", '=', value)]
                     elif not value:
                         # Set the __range of the group containing records with an unset
                         # date/datetime field value to False.
@@ -2621,10 +2662,12 @@ class BaseModel(metaclass=MetaModel):
                 `PostgreSQL <https://www.postgresql.org/docs/current/static/functions-aggregate.html>`_
                 and 'count_distinct', with the expected meaning.
         :param list groupby: list of groupby descriptions by which the records will be grouped.
-                A groupby description is either a field (then it will be grouped by that field)
-                or a string 'field:granularity'. Right now, the only supported granularities
-                are 'day', 'week', 'month', 'quarter' or 'year', and they only make sense for
-                date/datetime fields.
+                A groupby description is either a field (then it will be grouped by that field).
+                For the dates an datetime fields, you can specify a granularity using the syntax 'field:granularity'.
+                The supported granularities are 'hour', 'day', 'week', 'month', 'quarter' or 'year';
+                Read_group also supports integer date parts:
+                'year_number', 'quarter_number', 'month_number' 'iso_week_number', 'day_of_year', 'day_of_month',
+                'day_of_week', 'hour_number', 'minute_number' and 'second_number'.
         :param int offset: optional number of groups to skip
         :param int limit: optional max number of groups to return
         :param str orderby: optional ``order by`` specification, for

odoo/addons/test_read_group/models.py
@@ -67,6 +67,7 @@ class Order(models.Model):
     _description = 'Sales order'
 
     line_ids = fields.One2many('test_read_group.order.line', 'order_id')
+    date = fields.Date()
 
 
 class OrderLine(models.Model):
@@ -75,6 +76,7 @@ class OrderLine(models.Model):
 
     order_id = fields.Many2one('test_read_group.order', ondelete='cascade')
     value = fields.Integer()
+    date = fields.Date(related='order_id.date')
 
 
 class User(models.Model):
@@ -103,6 +105,7 @@ class Task(models.Model):
         'user_id',
         string="Collaborators",
     )
+    date = fields.Date()
 
 
 class Partner(models.Model):

odoo/addons/test_new_api/models/test_unity_read.py
@@ -27,6 +27,7 @@ class Lesson(models.Model):
     course_id = fields.Many2one('test_new_api.course')
     attendee_ids = fields.Many2many('test_new_api.person', 'lesson_ids')
     teacher_id = fields.Many2one('test_new_api.person')
+    teacher_birthdate = fields.Date(related='teacher_id.birthday')
     date = fields.Date()
 
     def _compute_display_name(self):
@@ -47,6 +48,7 @@ class Person(models.Model):
     name = fields.Char('Name')
     lesson_ids = fields.Many2many('test_new_api.lesson', 'course_id')
     employer_id = fields.Many2one('test_new_api.employer')
+    birthday = fields.Date()
 
     def _compute_display_name(self):
         """
@@ -73,3 +75,4 @@ class PersonAccount(models.Model):
 
     person_id = fields.Many2one('test_new_api.person', required=True, ondelete='cascade')
     login = fields.Char()
+    activation_date = fields.Date()

odoo/addons/test_new_api/tests/test_search.py
@@ -1079,3 +1079,60 @@ class TestFlushSearch(TransactionCase):
         self.env['test_new_api.custom.table_query_sql'].invalidate_model()
         child.quantity = 25
         self.assertEqual(self.env['test_new_api.custom.table_query_sql'].search([]).sum_quantity, 25)
+
+
+class TestDatePartNumber(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.person = cls.env["test_new_api.person"].create({"name": "that person", "birthday": "1990-02-09"})
+        cls.lesson = cls.env["test_new_api.lesson"].create({"teacher_id": cls.person.id, "attendee_ids": [(4, cls.person.id)]})
+
+    def test_basic_cases(self):
+        with self.assertQueries(["""
+            SELECT "test_new_api_person"."id"
+            FROM "test_new_api_person"
+            WHERE date_part(%s, "test_new_api_person"."birthday") = %s
+            ORDER BY "test_new_api_person"."id"
+        """]):
+            result = self.env["test_new_api.person"].search([('birthday.month_number', '=', '2')])
+            self.assertEqual(result, self.person)
+
+        with self.assertQueries(["""
+            SELECT "test_new_api_person"."id"
+            FROM "test_new_api_person"
+            WHERE date_part(%s, "test_new_api_person"."birthday") = %s
+            ORDER BY "test_new_api_person"."id"
+        """]):
+            result = self.env["test_new_api.person"].search([('birthday.quarter_number', '=', '1')])
+            self.assertEqual(result, self.person)
+
+        with self.assertQueries(["""
+            SELECT "test_new_api_person"."id"
+            FROM "test_new_api_person"
+            WHERE date_part(%s, "test_new_api_person"."birthday") = %s
+            ORDER BY "test_new_api_person"."id"
+        """]):
+            result = self.env["test_new_api.person"].search([('birthday.iso_week_number', '=', '6')])
+            self.assertEqual(result, self.person)
+
+    def test_many2one(self):
+        result = self.env["test_new_api.lesson"].search([('teacher_id.birthday.month_number', '=', 2)])
+        self.assertEqual(result, self.lesson)
+
+    def test_many2many(self):
+        result = self.env["test_new_api.lesson"].search([('attendee_ids.birthday.month_number', '=', 2)])
+        self.assertEqual(result, self.lesson)
+
+    def test_related_field(self):
+        result = self.env["test_new_api.lesson"].search([('teacher_birthdate.month_number', '=', 2)])
+        self.assertEqual(result, self.lesson)
+
+    def test_inherit(self):
+        account = self.env["test_new_api.person.account"].create({"person_id": self.person.id, "activation_date": "2020-03-09"})
+
+        result = self.env["test_new_api.person.account"].search([('activation_date.quarter_number', '=', 1)])
+        self.assertEqual(result, account)
+
+        result = self.env["test_new_api.person.account"].search([('person_id.birthday.month_number', '=', 2)])
+        self.assertEqual(result, account)

odoo/addons/test_read_group/tests/test_date_range.py
@@ -126,3 +126,299 @@ class TestDateRange(common.TransactionCase):
         groups = self.Model.read_group(
             [('id', 'in', records.ids)], fields=['date', 'value'], groupby=['date:month', 'date:month'], lazy=False)
         self.assertEqual(groups, expected)
+
+
+class TestRelativeDateGranularity(common.TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.Model = cls.env['test_read_group.fill_temporal']
+        cls.Model.create([{"value": "1", "date": "2021-02-09", "datetime": "2021-02-09 15:55:12"},
+                          {"value": "20", "date": "2021-06-01", "datetime": "2021-06-01 16:55:14"},
+                          {"value": "300", "date": "2022-02-01", "datetime": "2022-02-01 15:22:12"}])
+
+    def test_full_usecase_month(self):
+        result = self.Model.read_group([],
+                                       fields=['date', 'value'],
+                                       groupby=['date:month_number'])
+        self.assertEqual(result, [
+            {
+                'date:month_number': 2,
+                'date_count': 2,
+                'value': 301,
+                '__domain': [('date.month_number', '=', 2)]
+            }, {
+                'date:month_number': 6,
+                'date_count': 1,
+                'value': 20,
+                '__domain': [('date.month_number', '=', 6)]
+            },
+        ])
+        res = self.Model.search(result[0]['__domain'])
+        self.assertEqual(len(res), 2)
+        self.assertEqual(res.mapped('value'), [1, 300])
+
+    def test_full_usecase_iso_week(self):
+        result = self.Model.read_group([],
+                                       fields=['date', 'value'],
+                                       groupby=['date:iso_week_number'])
+        self.assertEqual(result, [
+            {
+                'date:iso_week_number': 5,
+                'date_count': 1,
+                'value': 300,
+                '__domain': [('date.iso_week_number', '=', 5)]
+            }, {
+                'date:iso_week_number': 6,
+                'date_count': 1,
+                'value': 1,
+                '__domain': [('date.iso_week_number', '=', 6)]
+            }, {
+                'date:iso_week_number': 22,
+                'date_count': 1,
+                'value': 20,
+                '__domain': [('date.iso_week_number', '=', 22)]
+            },
+        ])
+        res = self.Model.search(result[0]['__domain'])
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res.mapped('value'), [300])
+
+    def test_full_usecase_quarter(self):
+        result = self.Model.read_group([],
+                                       fields=['date', 'value'],
+                                       groupby=['date:quarter_number'])
+        self.assertEqual(result, [
+            {
+                'date:quarter_number': 1,
+                'date_count': 2,
+                'value': 301,
+                '__domain': [('date.quarter_number', '=', 1)]
+            }, {
+                'date:quarter_number': 2,
+                'date_count': 1,
+                'value': 20,
+                '__domain': [('date.quarter_number', '=', 2)]
+            },
+        ])
+        res = self.Model.search(result[0]['__domain'])
+        self.assertEqual(len(res), 2)
+        self.assertEqual(res.mapped('value'), [1, 300])
+
+    def test_full_usecase_year(self):
+        result = self.Model.read_group([],
+                                       fields=['date', 'value'],
+                                       groupby=['date:year_number'])
+        self.assertEqual(result, [
+            {
+                'date:year_number': 2021,
+                'date_count': 2,
+                'value': 21,
+                '__domain': [('date.year_number', '=', 2021)]
+            }, {
+                'date:year_number': 2022,
+                'date_count': 1,
+                'value': 300,
+                '__domain': [('date.year_number', '=', 2022)]
+            },
+        ])
+        res = self.Model.search(result[0]['__domain'])
+        self.assertEqual(len(res), 2)
+        self.assertEqual(res.mapped('value'), [1, 20])
+
+    def test_full_usecase_day_of_year(self):
+        result = self.Model.read_group([],
+                                       fields=['date', 'value'],
+                                       groupby=['date:day_of_year'])
+        self.assertEqual(result, [
+            {
+                'date:day_of_year': 32,
+                'date_count': 1,
+                'value': 300,
+                '__domain': [('date.day_of_year', '=', 32)]
+            }, {
+                'date:day_of_year': 40,
+                'date_count': 1,
+                'value': 1,
+                '__domain': [('date.day_of_year', '=', 40)]
+            }, {
+                'date:day_of_year': 152,
+                'date_count': 1,
+                'value': 20,
+                '__domain': [('date.day_of_year', '=', 152)]
+            },
+        ])
+        res = self.Model.search(result[0]['__domain'])
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res.mapped('value'), [300])
+
+    def test_full_usecase_day_of_month(self):
+        result = self.Model.read_group([],
+                                       fields=['date', 'value'],
+                                       groupby=['date:day_of_month'])
+        self.assertEqual(result, [
+            {
+                'date:day_of_month': 1,
+                'date_count': 2,
+                'value': 320,
+                '__domain': [('date.day_of_month', '=', 1)]
+            }, {
+                'date:day_of_month': 9,
+                'date_count': 1,
+                'value': 1,
+                '__domain': [('date.day_of_month', '=', 9)]
+            },
+        ])
+        res = self.Model.search(result[0]['__domain'])
+        self.assertEqual(len(res), 2)
+        self.assertEqual(res.mapped('value'), [20, 300])
+
+    def test_full_usecase_day_of_week(self):
+        result = self.Model.read_group([],
+                                       fields=['date', 'value'],
+                                       groupby=['date:day_of_week'])
+        self.assertEqual(result, [
+            {
+                'date:day_of_week': 2,
+                'date_count': 3,
+                'value': 321,
+                '__domain': [('date.day_of_week', '=', 2)]
+            },
+        ])
+        res = self.Model.search(result[0]['__domain'])
+        self.assertEqual(len(res), 3)
+        self.assertEqual(res.mapped('value'), [1, 20, 300])
+
+    def test_full_usecase_hour_number(self):
+        result = self.Model.read_group([],
+                                       fields=['datetime', 'value'],
+                                       groupby=['datetime:hour_number'])
+        self.assertEqual(result, [
+            {
+                'datetime:hour_number': 15,
+                'datetime_count': 2,
+                'value': 301,
+                '__domain': [('datetime.hour_number', '=', 15)]
+            }, {
+                'datetime:hour_number': 16,
+                'datetime_count': 1,
+                'value': 20,
+                '__domain': [('datetime.hour_number', '=', 16)]
+            },
+        ])
+        res = self.Model.search(result[0]['__domain'])
+        self.assertEqual(len(res), 2)
+        self.assertEqual(res.mapped('value'), [1, 300])
+
+    def test_full_usecase_minute_number(self):
+        result = self.Model.read_group([],
+                                       fields=['datetime', 'value'],
+                                       groupby=['datetime:minute_number'])
+        self.assertEqual(result, [
+            {
+                'datetime:minute_number': 22,
+                'datetime_count': 1,
+                'value': 300,
+                '__domain': [('datetime.minute_number', '=', 22)]
+            }, {
+                'datetime:minute_number': 55,
+                'datetime_count': 2,
+                'value': 21,
+                '__domain': [('datetime.minute_number', '=', 55)]
+            },
+        ])
+        res = self.Model.search(result[0]['__domain'])
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res.mapped('value'), [300])
+
+    def test_full_usecase_second_number(self):
+        result = self.Model.read_group([],
+                                       fields=['datetime', 'value'],
+                                       groupby=['datetime:second_number'])
+        self.assertEqual(result, [
+            {
+                'datetime:second_number': 12,
+                'datetime_count': 2,
+                'value': 301,
+                '__domain': [('datetime.second_number', '=', 12)]
+            }, {
+                'datetime:second_number': 14,
+                'datetime_count': 1,
+                'value': 20,
+                '__domain': [('datetime.second_number', '=', 14)]
+            },
+        ])
+        res = self.Model.search(result[0]['__domain'])
+        self.assertEqual(len(res), 2)
+        self.assertEqual(res.mapped('value'), [1, 300])
+
+    def test_unknown_granularity(self):
+        with self.assertRaises(ValueError):
+            self.Model.read_group([], fields=['date', 'value'], groupby=['date:unknown_number'])
+
+
+class TestRelativeDateGranularityWithTimezones(common.TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        # execute a read_group with a relative granularity, it will give us back a domain
+        # that contains the relative granularity to find the origin records. Use this domain
+        # to find those records. This is exactly the way the pivot view behaves.
+        super().setUpClass()
+        cls.Model = cls.env['test_read_group.fill_temporal']
+        cls.env['res.lang']._activate_lang('fr_BE')
+        cls.env['res.lang']._activate_lang('NZ')
+
+    def test_usecase_with_timezones(self):
+        # Monday, it is the 5th week in UTC and the 6th in NZ
+        self.Model.create({"value": "98", "datetime": "2023-02-05 23:55:00"})
+        result = (self.Model.with_context({'tz': 'NZ'})  # GMT+12
+                            .read_group([],
+                                        fields=['datetime', 'value'],
+                                        groupby=['datetime:iso_week_number']))
+        self.assertEqual(result, [
+                    {
+                        'datetime:iso_week_number': 6,
+                        'datetime_count': 1,
+                        'value': 98,
+                        '__domain': [('datetime.iso_week_number', '=', 6)]
+                    }])
+        result = self.Model.with_context({'tz': 'NZ'}).search(result[0]['__domain'])
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result.value, 98)
+
+    def test_day_of_week_with_monday_as_first_day_of_week(self):
+        self.Model.create({"value": "98", "date": "2023-02-05"})  # Sunday
+
+        result = (self.Model.with_context({'tz': 'fr_BE'})  # GMT+1, first day of week is Monday
+                  .read_group([],
+                              fields=['date', 'value'],
+                              groupby=['date:day_of_week']))
+        self.assertEqual(result, [
+            {
+                'date:day_of_week': 6,
+                'date_count': 1,
+                'value': 98,
+                '__domain': [('date.day_of_week', '=', 6)]
+            }])
+        res = self.Model.with_context({'tz': 'fr_BE'}).search(result[0]['__domain'])
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res.mapped('value'), [98])
+
+    def test_day_of_week_with_sunday_as_first_day_of_week(self):
+        self.Model.create({"value": "98", "date": "2023-02-05"})  # Sunday
+
+        result = (self.Model.with_context({'tz': 'NZ'})  # GMT+12, first day of week is Sunday
+                  .read_group([],
+                              fields=['date', 'value'],
+                              groupby=['date:day_of_week']))
+        self.assertEqual(result, [
+            {
+                'date:day_of_week': 0,
+                'date_count': 1,
+                'value': 98,
+                '__domain': [('date.day_of_week', '=', 0)]
+            }])
+
+        res = self.Model.with_context({'tz': 'NZ'}).search(result[0]['__domain'])
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res.mapped('value'), [98])

commit 61eff0529468e83af2adf787578e4299c474dfec

[FIX] models: fix granularity explanation
Commit d8fe54d9ac96fc0123ab997f4b4a3fc2f380d34d introduced granularity
`day_of_week` with a wrong explanation. This commit fixes the explanation.

odoo/models.py
@@ -2021,11 +2021,11 @@ class BaseModel(metaclass=MetaModel):
                            SQL | -----------------------
                     Monday  1  |  0     |  1    |  2
                     tuesday 2  |  1     |  2    |  3
-                    wed     3  |  3     |  4    |  4
+                    wed     3  |  2     |  3    |  4
                     thurs   4  |  3     |  4    |  5
                     friday  5  |  4     |  5    |  6
-                    sat     6  |  6     |  0    |  0
-                    sun     7  |  0     |  1    |  1
+                    sat     6  |  5     |  6    |  0
+                    sun     7  |  6     |  0    |  1
                     """
                     first_week_day = int(get_lang(self.env, self.env.context.get('tz')).week_start)
                     sql_expr = SQL("mod(7 - %s + date_part(%s, %s)::int, 7)", first_week_day, spec, sql_expr)