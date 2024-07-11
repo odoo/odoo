b58bf827e7b27d07f5d87a9edd8f6b6d0ac9d69e
Gabriel (gdpf) <gdpf@odoo.com>
2024-07-11 10:12:06 +0200

    [FIX] google_calendar: events disappearing reset account
    Before this commit, when performing the Google Calendar account reset
    in Odoo, some events and recurrences were disappearing from Odoo when
    they shouldn't. This was happening because we were not removing the
    'google_id' information of deleted recurrences or even unlinking
    events incorrectly in Odoo side. Additionally, the synchronization of
    only 'new' or 'all existing' events were not correctly being handled
    because the 'need_sync' write should change according with the selected
    'sync_policy'.
    
    After this commit, we correctly remove the 'google_id' of recurrences
    when the deleting them and we now consider correctly the 'sync_policy'
    of synchronizing only 'new' or 'all existing' events by updating the
    'need_sync' status of all existing synchronized events.
    
    task-3731683

--- addons/google_calendar/wizard/reset_account.py
+++ addons/google_calendar/wizard/reset_account.py
@@ -30,20 +30,35 @@ class ResetGoogleAccount(models.TransientModel):
         events = self.env['calendar.event'].search([
             ('user_id', '=', self.user_id.id),
             ('google_id', '!=', False)])
+        recurrences = self.env['calendar.recurrence'].search([
+            ('base_event_id', 'in', events.ids),
+            ('google_id', '!=', False)])
+
         if self.delete_policy in ('delete_google', 'delete_both'):
             with google_calendar_token(self.user_id) as token:
                 for event in events:
                     google.delete(event.google_id, token=token)
 
-        if self.delete_policy in ('delete_odoo', 'delete_both'):
+        # Delete events according to the selected policy. If the deletion is only in
+        # Google, we won't keep track of the 'google_id' field for events and recurrences.
+        if self.delete_policy in ('delete_odoo', 'delete_both', 'delete_google'):
             events.google_id = False
-            events.unlink()
+            recurrences.google_id = False
+            if self.delete_policy != 'delete_google':
+                events.unlink()
 
+        # Define which events must be synchronized in the next synchronization:
+        # in 'all' sync policy we activate the sync for all events and in the 'new'
+        # sync policy we set it as False for skipping existing events synchronization.
+        next_sync_update = {}
         if self.sync_policy == 'all':
-            events.write({
-                'google_id': False,
-                'need_sync': True,
-            })
+            next_sync_update['need_sync'] = True
+        elif self.sync_policy == 'new':
+            next_sync_update['need_sync'] = False
+
+        # Write the next sync update attribute on the existing events.
+        if self.delete_policy not in ('delete_odoo', 'delete_both'):
+            events.write(next_sync_update)
 
         self.user_id.google_calendar_account_id._set_auth_tokens(False, False, 0)
         self.user_id.write({
