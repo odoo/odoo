7422c71b2f8c684620a36b344bcae7f0f4f801c1
vlst-odoo <vlst@odoo.com>
2024-07-11 12:37:12 +0200

    [IMP] pos_restaurant: imp table linking effects
    In this commit we improve the graphical effects that are present when
    linking tables in `pos_restaurant`
    
    closes odoo/odoo#174643
    
    Signed-off-by: Robin Heinz (rhe) <rhe@odoo.com>

--- addons/pos_restaurant/static/tests/tours/utils/floor_screen_util.js
+++ addons/pos_restaurant/static/tests/tours/utils/floor_screen_util.js
@@ -73,7 +73,25 @@ export function linkTables(child, parent) {
     return {
         content: `Drag table ${child} onto table ${parent} in order to link them`,
         trigger: table({ name: child }).trigger,
-        run: `drag_and_drop(${table({ name: parent }).trigger})`,
+        async run(helpers) {
+            helpers.delay = 500;
+            await helpers.drag_multiple_and_then_drop(
+                [
+                    table({ name: parent }).trigger,
+                    {
+                        position: "top",
+                        relative: true,
+                    },
+                ],
+                [
+                    table({ name: parent }).trigger,
+                    {
+                        position: "center",
+                        relative: true,
+                    },
+                ]
+            );
+        },
     };
 }
 export function isChildTable(child) {
@@ -82,3 +100,40 @@ export function isChildTable(child) {
         trigger: table({ name: child }).trigger + ` .info.opacity-25`,
     };
 }
+
+import { TourHelpers } from "@web_tour/tour_service/tour_helpers";
+import { patch } from "@web/core/utils/patch";
+import * as hoot from "@odoo/hoot-dom";
+
+patch(TourHelpers.prototype, {
+    async drag_multiple_and_then_drop(...drags) {
+        const dragEffectDelay = async () => {
+            console.log(this.delay);
+            await new Promise((resolve) => requestAnimationFrame(resolve));
+            await new Promise((resolve) => setTimeout(resolve, this.delay));
+        };
+        const element = this.anchor;
+        this._ensureEnabled(element, "drag and drop");
+        const { drop, moveTo } = hoot.drag(element);
+        await dragEffectDelay();
+        hoot.hover(element, {
+            position: {
+                top: 20,
+                left: 20,
+            },
+            relative: true,
+        });
+        await dragEffectDelay();
+        for (const [selector, options] of drags) {
+            console.log("Selector", selector, options);
+            const target = await hoot.waitFor(selector, {
+                visible: true,
+                timeout: 500,
+            });
+            moveTo(target, options);
+            await dragEffectDelay();
+        }
+        drop();
+        await dragEffectDelay();
+    },
+});
