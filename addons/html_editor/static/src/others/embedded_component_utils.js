6af95709b4f19cd30177040a732174a2830d535e
abd-msyukyu-odoo <abd@odoo.com>
2024-07-11 11:22:31 +0200

    [IMP] html_editor: improve wording for editable descendants

--- addons/html_editor/static/src/others/embedded_component_utils.js
+++ addons/html_editor/static/src/others/embedded_component_utils.js
@@ -2,8 +2,8 @@ import { onMounted, onPatched, useRef } from "@odoo/owl";
 
 /**
  * Get all element children with `data-embedded-editable` attribute which are
- * props of the host's own embedded component and not of other embedded
- * component children. (an embedded component can contain others).
+ * descendants of the host's own embedded component and not part of another
+ * embedded component descendant (an embedded component can contain others).
  * If multiple elements have the same `data-embedded-editable`, only the last
  * one is considered.
  * @param {HTMLElement} host
@@ -21,16 +21,17 @@ export function getEditableDescendants(host) {
 
 /**
  * Handle the rendering of an editableDescendants:
- * It is a node owned by the editor which will be inserted under a ref of
- * the same name in the component's template. This allows to use editor features
- * inside an embedded component. EditableDescendants are shared in collaboration
- * and are saved between edition sessions.
+ * It is a node owned by the editor, which will be inserted under a ref of
+ * the same name as the attribute `data-embedded-editable` of that node, in the
+ * component's template. This allows to use editor features inside an embedded
+ * component. EditableDescendants are shared in collaboration and are saved
+ * between edition sessions.
  *
  * Warning: there must be a ref in the template for every editableDescendants,
  * available at all times no matter the component state to guarantee that the
  * editor can save their values at any given time, synchronously.
  *
- * @param {HTMLElement} host embedded component host
+ * @param {HTMLElement} host for the embedded component
  */
 export function useEditableDescendants(host) {
     const editableDescendants = Object.freeze(getEditableDescendants(host));
