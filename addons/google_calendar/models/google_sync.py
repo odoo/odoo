c859b7e7e569bb240733ecdf482c7304dcfe1727
Gabriel (gdpf) <gdpf@odoo.com>
2024-07-11 14:12:18 +0000

    [FIX] google_calendar: duplicated event 'all day'
    Before this commit, recurrent events created with the 'All Day' option
    were duplicating the first event of the recurrence in Google side. This
    was happening because we wrongly synchronized the single event with Google
    before the synchronization of its recurrence (which already synchronize the
    events in Google side itself).
    
    After this commit, the recurrence is created normally using the 'All Day'
    option. We achieve that by skipping the useless synchronization we were
    doing in the single event: only the recurrence must be synchronized in this
    specific flow.
    
    task-3768121
    
    X-original-commit: 6ea41963440a68793aec52ca0dc1e0c0b647f4a9

--- addons/google_calendar/models/google_sync.py
+++ addons/google_calendar/models/google_sync.py
@@ -84,6 +84,7 @@ class GoogleSync(models.AbstractModel):
             for vals in vals_list:
                 vals.update({'need_sync': False})
         records = super().create(vals_list)
+        self._handle_allday_recurrences_edge_case(records, vals_list)
 
         google_service = GoogleCalendarService(self.env['google.service'])
         if self.env.user._get_google_sync_status() != "sync_paused":
@@ -91,6 +92,18 @@ class GoogleSync(models.AbstractModel):
                 if record.need_sync and record.active:
                     record.with_user(record._get_event_user())._google_insert(google_service, record._google_values(), timeout=3)
         return records
+
+    def _handle_allday_recurrences_edge_case(self, records, vals_list):
+        """
+        When creating 'All Day' recurrent event, the first event is wrongly synchronized as
+        a single event and then its recurrence creates a duplicated event. We must manually
+        set the 'need_sync' attribute as False in order to avoid this unwanted behavior.
+        """
+        if vals_list and self._name == 'calendar.event':
+            forbid_sync = all(not vals.get('need_sync', True) for vals in vals_list)
+            records_to_skip = records.filtered(lambda r: r.need_sync and r.allday and r.recurrency and not r.recurrence_id)
+            if forbid_sync and records_to_skip:
+                records_to_skip.with_context(send_updates=False).need_sync = False
 
     def unlink(self):
         """We can't delete an event that is also in Google Calendar. Otherwise we would
