241997f17044ea1674e25749276c5ce245608db4
vlst-odoo <vlst@odoo.com>
2024-07-11 12:37:12 +0200

    [IMP] pos_restaurant: imp table linking effects
    In this commit we improve the graphical effects that are present when
    linking tables in `pos_restaurant`
    
    closes odoo/odoo#174643
    
    Signed-off-by: Robin Heinz (rhe) <rhe@odoo.com>

--- addons/pos_restaurant/static/src/app/models/restaurant_table.js
+++ addons/pos_restaurant/static/src/app/models/restaurant_table.js
@@ -31,7 +31,7 @@ export class RestaurantTable extends Base {
         if (!this.parent_id) {
             return this.position_h;
         }
-        const parent_side = this.getParentSide();
+        const parent_side = this.parent_side || this.getParentSide();
         if (["top", "bottom"].includes(parent_side)) {
             return this.parent_id.getX();
         }
@@ -44,7 +44,8 @@ export class RestaurantTable extends Base {
         if (!this.parent_id) {
             return this.position_v;
         }
-        const parent_side = this.getParentSide();
+        const parent_side = this.parent_side || this.getParentSide();
+        this.parent_side = parent_side;
         if (["left", "right"].includes(parent_side)) {
             return this.parent_id.getY();
         }
@@ -61,6 +62,14 @@ export class RestaurantTable extends Base {
     }
     getOrder() {
         return this["<-pos.order.table_id"][0];
+    }
+    setPositionAsIfLinked(parent, side) {
+        // console.log("132")
+        this.parent_id = parent;
+        this.parent_side = side;
+        this.position_h = this.getX();
+        this.position_v = this.getY();
+        this.parent_id = null;
     }
 }
 registry.category("pos_available_models").add(RestaurantTable.pythonModel, RestaurantTable);
