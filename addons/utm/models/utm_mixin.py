88d90c81920f0c313687fbdde481e73e0fad720e
dane@odoo.com <dane@odoo.com>
2024-07-11 13:27:17 +0200

    [PERF] utm: improve performance
    The commit adds indexing to frequently accessed fields as records tend
    to be lare which causes long waiting loading.
    
    fetching and counting benchmarks:
    ~11M reords were created:
    - 4k with campaign_id = 4
    - 11M with campaign_id = 5
    
    before indexing:
    - 1889.532 ms
    after indexing:
    - 10.617 ms
    
    task-3980514
    
    closes odoo/odoo#172978
    
    X-original-commit: e889bd2c993a456d3077beaa3271f73e3510665e
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Dato Nefaridze (dane) <dane@odoo.com>

--- addons/utm/models/utm_mixin.py
+++ addons/utm/models/utm_mixin.py
@@ -15,11 +15,11 @@ class UtmMixin(models.AbstractModel):
     _name = 'utm.mixin'
     _description = 'UTM Mixin'
 
-    campaign_id = fields.Many2one('utm.campaign', 'Campaign',
+    campaign_id = fields.Many2one('utm.campaign', 'Campaign', index='btree_not_null',
                                   help="This is a name that helps you keep track of your different campaign efforts, e.g. Fall_Drive, Christmas_Special")
-    source_id = fields.Many2one('utm.source', 'Source',
+    source_id = fields.Many2one('utm.source', 'Source', index='btree_not_null',
                                 help="This is the source of the link, e.g. Search Engine, another domain, or name of email list")
-    medium_id = fields.Many2one('utm.medium', 'Medium',
+    medium_id = fields.Many2one('utm.medium', 'Medium', index='btree_not_null',
                                 help="This is the method of delivery, e.g. Postcard, Email, or Banner Ad")
 
     @api.model
