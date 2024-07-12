a33312fe15093522754e56dbeafbf334006337e0
Xavier Morel <xmo@odoo.com>
2024-07-12 08:00:10 +0200

    [IMP] *: don't optionally import non-optional requirements
    - openssl is a hard dependency of base (ir_mail_server)
    - cryptography is a dependency of openssl
    - stdnum is a hard dependency of `partner_autocomplete`, which is auto_install
    
    Also move the patching of xlsxwriter and xlwt to the lazy
    monkeypatcher, and stop importing xlsxwriter from the patch
    location (where it was unwittingly exposed as a side effect).
    
    Part-of: odoo/odoo#172761
    Related: odoo/enterprise#66490
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- addons/portal/__init__.py
+++ addons/portal/__init__.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Updating mako environement in order to be able to use slug
-try:
-    from odoo.tools.rendering_tools import template_env_globals
-    from odoo.http import request
+from odoo.tools.rendering_tools import template_env_globals
+from odoo.http import request
 
-    template_env_globals.update({
-        'slug': lambda value: request.env['ir.http']._slug(value)  # noqa: PLW0108
-    })
-except ImportError:
-    pass
+template_env_globals.update({
+    'slug': lambda value: request.env['ir.http']._slug(value)  # noqa: PLW0108
+})
 
 from . import controllers
 from . import models
