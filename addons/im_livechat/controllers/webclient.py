7dd8cddb9e05b8b4a274f7dc6d6ad6bdaf6b7ef8
Sébastien Theys <seb@odoo.com>
2024-07-12 13:27:25 +0200

    [FIX] im_livechat: add missing `view_info` in test suite
    Follow up of https://github.com/odoo/odoo/pull/171073
    
    closes odoo/odoo#172939
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/controllers/webclient.py
+++ addons/im_livechat/controllers/webclient.py
@@ -8,9 +8,13 @@ from odoo.addons.mail.models.discuss.mail_guest import add_guest_to_context
 class WebClient(WebclientController):
     @route("/web/tests/livechat", type="http", auth="user")
     def test_external_livechat(self, **kwargs):
-        return request.render("im_livechat.unit_embed_suite", {
-            "server_url": request.env["ir.config_parameter"].get_base_url(),
-        })
+        return request.render(
+            "im_livechat.unit_embed_suite",
+            {
+                "server_url": request.env["ir.config_parameter"].get_base_url(),
+                "session_info": {"view_info": request.env["ir.ui.view"].get_view_info()},
+            },
+        )
 
     def _process_request_for_internal_user(self, store, **kwargs):
         super()._process_request_for_internal_user(store, **kwargs)
