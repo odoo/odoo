79d513108d9edc029dd2389eaf07ec63d466441a
Thibault Delavallée <tde@odoo.com>
2024-07-15 16:17:45 +0200

    [FIX] crm: do not propagate void email/phone to partner
    Resetting lead contact information (email and/or phone) may happen.
    Examples are lead being reset through import, bad manipulation or
    even wil to remove contact information to avoid sending messages or
    SMSes.
    
    However this should not prevent from using the linked partner in other
    records, hence not reset partner email and/or phone fields. Indeed
    partners are used in a lot of models e.g. resetting a lead should not
    impact sending SO emails.
    
    In this commit we therefore prevent from propagating false values to
    the linked partner through the sync mechanism.
    
    Task-
    
    closes odoo/odoo#173195
    
    Related: odoo/enterprise#66673
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/crm/static/tests/tours/crm_email_and_phone_propagation.js
+++ addons/crm/static/tests/tours/crm_email_and_phone_propagation.js
@@ -26,38 +26,3 @@
             run: 'click',
         },
     ]});
-
-    registry.category("web_tour.tours").add('crm_email_and_phone_propagation_remove_email_and_phone', {
-        test: true,
-        url: '/odoo',
-        steps: () => [
-        stepUtils.showAppsMenuItem(),
-        {
-            trigger: '.o_app[data-menu-xmlid="crm.crm_menu_root"]',
-            content: 'open crm app',
-            run: "click",
-        }, {
-            trigger: '.o_kanban_record:contains(Test Lead Propagation)',
-            content: 'Open the first lead',
-            run: 'click',
-        },
-        {
-            trigger: ".o_form_editable .o_field_widget[name=phone] input",
-        },
-        {
-            trigger: '.o_form_editable .o_field_widget[name=email_from] input',
-            content: 'Remove the email and the phone',
-            run: "clear && clear .o_form_editable .o_field_widget[name=phone] input",
-        },
-        {
-            trigger: ".o_form_sheet_bg .fa-exclamation-triangle:not(.o_invisible_modifier)",
-        },
-        {
-            trigger: '.o_back_button',
-            // wait the warning message to be visible
-            content: 'Save the lead and exit to kanban',
-            run: 'click',
-        },{
-            trigger: '.o_kanban_renderer',
-        }
-    ]});
