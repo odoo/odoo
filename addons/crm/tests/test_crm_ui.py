66e3810e64c616474345ec0e89e30edde7a1f6fa
Thibault Delavallée <tde@odoo.com>
2024-07-15 16:17:45 +0200

    [FIX] crm: do not propagate void email/phone to partner
    Resetting lead contact information (email and/or phone) may happen.
    Examples are lead being reset through import, bad manipulation or
    even wil to remove contact information to avoid sending messages or
    SMSes.
    
    However this should not prevent from using the linked partner in other
    records, hence not reset partner email and/or phone fields. Indeed
    partners are used in a lot of models e.g. resetting a lead should not
    impact sending SO emails.
    
    In this commit we therefore prevent from propagating false values to
    the linked partner through the sync mechanism.
    
    Task-

--- addons/crm/tests/test_crm_ui.py
+++ addons/crm/tests/test_crm_ui.py
@@ -86,39 +86,3 @@ class TestUi(HttpCase, TestCrmCommon):
         self.assertEqual(lead.phone, '+32 494 44 44 44', 'Should not have changed the lead phone')
         self.assertEqual(partner.email, 'test@example.com', 'Should have propagated the lead email on the partner')
         self.assertEqual(partner.phone, '+32 494 44 44 44', 'Should have propagated the lead phone on the partner')
-
-    def test_email_and_phone_propagation_remove_email_and_phone(self):
-        """Test the propagation of the email / phone on the partner.
-
-        If we remove the email and phone on the lead, it should be removed on the
-        partner. This test check that we correctly detect field values changes in JS
-        (aka undefined VS falsy).
-        """
-        self.env['crm.lead'].search([]).unlink()
-        user_admin = self.env['res.users'].search([('login', '=', 'admin')])
-
-        partner = self.env['res.partner'].create({'name': 'Test Partner'})
-        lead = self.env['crm.lead'].create({
-            'name': 'Test Lead Propagation',
-            'type': 'opportunity',
-            'user_id': user_admin.id,
-            'partner_id': partner.id,
-            'email_from': 'test@example.com',
-            'phone': '+32 494 44 44 44',
-        })
-
-        # Check initial state
-        self.assertEqual(partner.email, 'test@example.com')
-        self.assertEqual(lead.phone, '+32 494 44 44 44')
-        self.assertEqual(lead.email_from, 'test@example.com')
-        self.assertEqual(lead.phone, '+32 494 44 44 44')
-
-        self.assertFalse(lead.partner_email_update)
-        self.assertFalse(lead.partner_phone_update)
-
-        self.start_tour('/odoo', 'crm_email_and_phone_propagation_remove_email_and_phone', login='admin')
-
-        self.assertFalse(lead.email_from, 'Should have removed the email')
-        self.assertFalse(lead.phone, 'Should have removed the phone')
-        self.assertFalse(partner.email, 'Should have removed the email')
-        self.assertFalse(partner.phone, 'Should have removed the phone')
