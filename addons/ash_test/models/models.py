8a91575fc618421fd0b3d011aada37ce6cb11d7c
AakashShiperoo <aakash@shiperoo.com>
2024-07-16 15:09:35 +1000

    Fixing fields
    added custom fields relevant to the product and also fixed the import feature and now it allows import with custom fields

--- addons/ash_test/models/models.py
+++ addons/ash_test/models/models.py
@@ -1,7 +1,19 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'  # Inherit from `product.template`
+    _inherit = 'product.template'  # Inherit from product.template
 
-    custom_field_1 = fields.Char('Custom Field 1')
-    custom_field_2 = fields.Integer('Custom Field 2')
+    # Define new fields
+    tenant_id = fields.Char('Tenant ID')
+    sku_code = fields.Char('SKU Code')
+    description = fields.Char('Description')
+    ean = fields.Char('EAN')
+    brand = fields.Char('Brand')
+    season_code = fields.Char('Season Code')
+    color = fields.Char('Color')
+    size = fields.Integer('Size')
+    merchandise_category_code = fields.Char('Merchandise Category Code')
+    style = fields.Char('Style')
+    style_code = fields.Char('Style Code')
+    weight = fields.Float('Weight')
+    returnable = fields.Boolean('Returnable')
