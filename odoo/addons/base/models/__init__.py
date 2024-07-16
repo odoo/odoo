25a51bfde2ba479fed9889f1242ae404f5ba1b09
Raphael Collet <rco@odoo.com>
2024-07-16 11:19:11 +0200

    [IMP] base: adapt code to use new model ir.access

--- odoo/addons/base/models/__init__.py
+++ odoo/addons/base/models/__init__.py
@@ -17,7 +17,6 @@ from . import ir_cron
 from . import ir_filters
 from . import ir_default
 from . import ir_exports
-from . import ir_rule
 from . import ir_config_parameter
 from . import ir_autovacuum
 from . import ir_mail_server
