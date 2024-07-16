263f24e8f3eb61a61f96198d17385bfd1ee508ab
Chrysanthe (chgo) <chgo@odoo.com>
2024-07-16 11:01:34 +0200

    [IMP] website: review `s_text_image` and `s_image_text` snippets
    This commit improves the design of the `s_text_image` and `s_image_text`
    snippets, mainly by adding new images and applying small tweak to
    the current design.
    
    task-3665301
    
    closes odoo/odoo#173311
    
    Related: odoo/design-themes#830
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>

--- addons/website/static/tests/tours/snippet_image_quality.js
+++ addons/website/static/tests/tours/snippet_image_quality.js
@@ -30,10 +30,10 @@ wTourUtils.registerWebsitePreviewTour('website_image_quality', {
         content: "Check image size",
         // Reached size cannot be hardcoded because it changes with
         // different versions of Chrome.
-        trigger: 'we-customizeblock-options:has(we-title:contains("Image")) .o_we_image_weight:contains(" kb"):not(:contains("41.5 kb"))',
+        trigger: 'we-customizeblock-options:has(we-title:contains("Image")) .o_we_image_weight:contains(" kb"):not(:contains("42.9 kb"))',
         run() {
             // Make sure the reached size is smaller than the original one.
-            if (parseFloat(this.anchor.innerText) >= 47.5) {
+            if (parseFloat(this.anchor.innerText) >= 42.9) {
                 console.error("Image size should be smaller than original");
             }
         },
@@ -45,10 +45,10 @@ wTourUtils.registerWebsitePreviewTour('website_image_quality', {
     },
     {
         content: "Wait for image update: back to original image",
-        trigger: ':iframe .s_text_image img[src$="0sOnkdNPFFV0lRVRLK+B7PJ5F4If2IY8ngQsDP//Z"]',
+        trigger: ':iframe .s_text_image img[src$="LRQeyj8rLxXuVHZYnLENbMShuqm5A0slYRpf/AP/Z"]',
     },
     {
         content: "Check image size",
-        trigger: 'we-customizeblock-options:has(we-title:contains("Image")) .o_we_image_weight:contains("41.5 kb")',
+        trigger: 'we-customizeblock-options:has(we-title:contains("Image")) .o_we_image_weight:contains("42.9")',
     },
 ]);
