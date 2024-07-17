40ee42ae2645a3b8e76e309449ba45584fca38da
Pedram (pebr) <pebr@odoo.com>
2024-07-17 13:55:48 +0200

    [FIX] pos_loyalty: exclude rewards with archived reward products
    Before this commit, rewards associated with archived products were
    still accessible, leading to errors when attempting to claim them due
    to the non-existence of the reward product.
    
    opw-4055792
    
    closes odoo/odoo#173499
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/pos_loyalty/static/src/overrides/models/pos_store.js
+++ addons/pos_loyalty/static/src/overrides/models/pos_store.js
@@ -183,7 +183,7 @@ patch(PosStore.prototype, {
             const points = order._getRealCouponPoints(couponProgram.coupon_id);
             const hasLine = order.orderlines.filter((line) => !line.is_reward_line).length > 0;
             for (const reward of program.rewards.filter(
-                (reward) => reward.reward_type == "product"
+                (reward) => reward.reward_type == "product" && reward.reward_product_ids.length > 0
             )) {
                 if (points < reward.required_points) {
                     continue;
