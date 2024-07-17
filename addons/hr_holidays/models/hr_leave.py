bcebf07016144e531b1f360d4bd3a96e3e6ff2e6
Miquel Raïch <miquel.raich@forgeflow.com>
2024-07-17 03:02:52 +0200

    [IMP] hr_holidays: avoid useless call to self.env.ref()

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -1119,6 +1119,8 @@ Attempting to double-book your time off won't magically make your vacation 2x be
         split_leaves.filtered(lambda l: l.state in 'validate')._validate_leave_request()
 
     def action_validate(self, check_state=True):
+        if not self:
+            return
         current_employee = self.env.user.employee_id
         leaves = self._get_leaves_on_public_holiday()
         if leaves:
@@ -1328,6 +1330,8 @@ Attempting to double-book your time off won't magically make your vacation 2x be
         return responsible
 
     def activity_update(self):
+        if not self:
+            return
         to_clean, to_do, to_do_confirm_activity = self.env['hr.leave'], self.env['hr.leave'], self.env['hr.leave']
         activity_vals = []
         today = fields.Date.today()
