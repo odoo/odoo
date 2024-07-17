f2d286352ea86332502eea2b6dfc97696f4532f2
AlienAtSystem <gerald.malsch@manatec.de>
2024-07-17 07:38:20 +0200

    [FIX] mrp_subcontracting: Removed filtering since it excludes wanted cases

--- addons/mrp_subcontracting/models/product.py
+++ addons/mrp_subcontracting/models/product.py
@@ -13,7 +13,7 @@ class SupplierInfo(models.Model):
     def _compute_is_subcontractor(self):
         for supplier in self:
             boms = supplier.product_id.variant_bom_ids
-            boms |= supplier.product_tmpl_id.bom_ids.filtered(lambda b: not b.product_id)
+            boms |= supplier.product_tmpl_id.bom_ids
             supplier.is_subcontractor = supplier.name in boms.subcontractor_ids
 
 
