a6c8d3a58487507b244b573c4530e0fa9aa5101e
oco-odoo <oco@odoo.com>
2024-07-18 13:21:21 +0200

    [FIX] account: properly update tag names without l10n_multilang
    When l10n_multilang isn't installed, the rewriting of the tag name due to the modification of a tax_tags expression did not work. This was due to the fact we called _update_field_translations on a not translatable field, which had no effect.
    
    Because of that test_write_single_line_tagname_not_shared failed on Community builds of the nightly tests.
    
    closes odoo/odoo#173627
    
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

--- addons/account/models/account_report.py
+++ addons/account/models/account_report.py
@@ -557,8 +557,12 @@ class AccountReportExpression(models.Model):
                     if former_tax_tags and all(tag_expr in self for tag_expr in former_tax_tags._get_related_tax_report_expressions()):
                         # If we're changing the formula of all the expressions using that tag, rename the tag
                         positive_tags, negative_tags = former_tax_tags.sorted(lambda x: x.tax_negate)
-                        positive_tags._update_field_translations('name', {'en_US': f"+{vals['formula']}"})
-                        negative_tags._update_field_translations('name', {'en_US': f"-{vals['formula']}"})
+                        if self.pool['account.tax'].name.translate:
+                            positive_tags._update_field_translations('name', {'en_US': f"+{vals['formula']}"})
+                            negative_tags._update_field_translations('name', {'en_US': f"-{vals['formula']}"})
+                        else:
+                            positive_tags.name = f"+{vals['formula']}"
+                            negative_tags.name = f"-{vals['formula']}"
                     else:
                         # Else, create a new tag. Its the compute functions will make sure it is properly linked to the expressions
                         tag_vals = self.env['account.report.expression']._get_tags_create_vals(vals['formula'], country.id)
