9f4f1af719134d0f5c9b4b75bc20bd1ebf468b95
Ahmad (khah) <khah@odoo.com>
2024-07-18 09:54:03 +0200

    [IMP] fleet: exclude vehicles with new contracts from reminders
    Before this commit, if a new contract was created for a vehicle
    which had an overdue one, it always showed up in the "Need Action"
    filter.
    
    This commit excludes the vehicles that are resolved from the
    reminders.
    
    closes odoo/odoo#173588
    
    Task: 4016972
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/fleet/tests/test_overdue.py
+++ addons/fleet/tests/test_overdue.py
@@ -43,3 +43,35 @@ class TestFleet(common.TransactionCase):
         })
         res = self.env["fleet.vehicle"].search([('contract_renewal_overdue', '=', True), ('id', '=', car_1.id)])
         self.assertEqual(res, car_1)
+
+    def test_exclude_resolved_vehicles_from_overdue(self):
+        """
+            if there is an expired contract for the car, but it also has an open contract
+            it should not be considered overdue
+        """
+        user = new_test_user(self.env, "test base user", groups="base.group_user")
+        brand = self.env["fleet.vehicle.model.brand"].create({
+            "name": "Audi",
+        })
+        model = self.env["fleet.vehicle.model"].create({
+            "brand_id": brand.id,
+            "name": "A3",
+        })
+        car_1 = self.env["fleet.vehicle"].create({
+            "model_id": model.id,
+            "driver_id": user.partner_id.id,
+            "plan_to_change_car": False
+        })
+
+        Log = self.env['fleet.vehicle.log.contract']
+        Log.create({
+            'vehicle_id': car_1.id,
+            'expiration_date': fields.Date.add(fields.Date.today(), days=-2)
+        })
+        Log.create({
+            'vehicle_id': car_1.id,
+            'expiration_date': fields.Date.add(fields.Date.today(), days=365)
+        })
+
+        res = self.env["fleet.vehicle"].search([('contract_renewal_overdue', '=', True), ('id', '=', car_1.id)])
+        self.assertFalse(res)
