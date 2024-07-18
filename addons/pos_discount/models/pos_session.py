a054663a9f12cb9d45f209d6a076da5049e3575d
Pedram (pebr) <pebr@odoo.com>
2024-07-18 08:53:12 +0000

    [FIX] pos_discount: ensure discount product is always loaded
    Prior to this commit, enabling limited product loading could result in
    the discount product not being loaded.
    
    opw-4057657
    
    closes odoo/odoo#174957
    
    X-original-commit: b48a4323cd12983c6a1bc61f790bae5eb3359bfe
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/pos_discount/models/pos_session.py
+++ addons/pos_discount/models/pos_session.py
@@ -8,9 +8,19 @@ from odoo.osv.expression import OR
 class PosSession(models.Model):
     _inherit = 'pos.session'
 
-    def _load_data_params(self, config_id):
-        params = super(PosSession, self)._load_data_params(config_id)
-        if self.config_id.module_pos_discount:
-            curr_domain = params['product.product']['domain']
-            params['product.product']['domain'] = OR([curr_domain, [('id', '=', self.config_id.discount_product_id.id)]])
-        return params
+    def load_data(self, models_to_load, only_data=False):
+        result = super().load_data(models_to_load, only_data)
+
+        # adapt product
+        if len(models_to_load) == 0 or 'product.product' in models_to_load:
+            product_params = self._load_data_params(self.config_id)['product.product']
+            discount_product_id = self.config_id.discount_product_id.id
+            product_ids_set = {product['id'] for product in result['data']['product.product']}
+
+            if self.config_id.module_pos_discount and discount_product_id not in product_ids_set:
+                productModel = self.env['product.product'].with_context(**product_params['context'])
+                product = productModel.search_read([('id', '=', discount_product_id)], fields=product_params['fields'], load=False)
+                self._process_pos_ui_product_product(product)
+                result['data']['product.product'].extend(product)
+
+        return result
