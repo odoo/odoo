f0a1b04d9cadd5295a33a73353ac6b4f7275650f
Pedram (pebr) <pebr@odoo.com>
2024-07-18 08:53:12 +0000

    [FIX] pos_discount: ensure discount product is always loaded
    Prior to this commit, enabling limited product loading could result in
    the discount product not being loaded.
    
    opw-4057657
    
    closes odoo/odoo#175685
    
    X-original-commit: 9c122f86cb7f9e62585debd1d166e8516fc79c25
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/pos_discount/models/product_product.py
+++ addons/pos_discount/models/product_product.py
@@ -1,14 +1,19 @@
-from odoo import api, models
-from odoo.osv.expression import OR
+from odoo import models
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    @api.model
-    def _load_pos_data_domain(self, data):
-        params = super()._load_pos_data_domain(data)
+    def _load_pos_data(self, data):
+        res = super()._load_pos_data(data)
         config_id = self.env['pos.config'].browse(data['pos.config']['data'][0]['id'])
-        if config_id.module_pos_discount:
-            params = OR([params, [('id', '=', config_id.discount_product_id.id)]])
-        return params
+        discount_product_id = config_id.discount_product_id.id
+        product_ids_set = {product['id'] for product in res['data']}
+
+        if config_id.module_pos_discount and discount_product_id not in product_ids_set:
+            productModel = self.env['product.product'].with_context({**self.env.context, 'display_default_code': False})
+            product = productModel.search_read([('id', '=', discount_product_id)], fields=res['fields'], load=False)
+            self._process_pos_ui_product_product(product, config_id)
+            res['data'].extend(product)
+
+        return res
