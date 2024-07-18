16bc70c973b5a112f9a165837680bc1011491c68
Juan José Scarafia <jjs@adhoc.com.ar>
2024-07-18 13:33:01 -0300

    [IMP] new updates

--- addons/l10n_ar_withholding/demo/account_demo.py
+++ addons/l10n_ar_withholding/demo/account_demo.py
@@ -8,6 +8,29 @@ class AccountChartTemplate(models.AbstractModel):
     def _post_load_demo_data(self, company=False):
         result = super()._post_load_demo_data(company)
         if company == self.env.ref('base.company_ri', raise_if_not_found=False):
+            earnings_withholding_tax_119 = self.env.ref(f'account.{company.id}_ri_tax_withholding_earnings_incurred_group_119_insc')
+            arba_withholding_applied = self.env.ref(f'account.{company.id}_ri_tax_withholding_iibb_ba_applied')
+            # Create demo sequence for earnings withholding
+            seq_earnings_withholding = self.env['ir.sequence'].create({
+                'name': 'Earnings withholding sequence',
+                'padding': 1,
+                'number_increment': 1,
+                'implementation': 'standard',
+                'company_id': self.env.ref('base.company_ri').id
+            })
+            withholding_sequence_id = seq_earnings_withholding.id
+
+            # Add to partner Adhoc: 1) withholding tax 119 and it`s sequence, 2) arba withholding
+            self.env['l10n_ar.partner.tax'].create({'tax_id': earnings_withholding_tax_119.id, 'partner_id': self.env.ref('l10n_ar.res_partner_adhoc').id, 'company_id': company.id})
+            earnings_withholding_tax_119.l10n_ar_withholding_sequence_id = withholding_sequence_id
+            self.env['l10n_ar.partner.tax'].create({'tax_id': arba_withholding_applied.id, 'partner_id': self.env.ref('l10n_ar.res_partner_adhoc').id, 'company_id': company.id})
+
+            # Add to partner Belgrano: 1) withholding tax 78 and it`s sequence, 2) arba withholding
+            earnings_withholding_tax_78 = self.env.ref(f'account.{company.id}_ri_tax_withholding_earnings_incurred_group_78_insc')
+            self.env['l10n_ar.partner.tax'].create({'tax_id': earnings_withholding_tax_78.id, 'partner_id': self.env.ref('l10n_ar.res_partner_mipyme').id, 'company_id': company.id})
+            earnings_withholding_tax_78.l10n_ar_withholding_sequence_id = withholding_sequence_id
+            self.env['l10n_ar.partner.tax'].create({'tax_id': arba_withholding_applied.id, 'partner_id': self.env.ref('l10n_ar.res_partner_mipyme').id, 'company_id': company.id})
+
             # Because in demo we want to skip the config, while in data we want to require them to configure
-            self.env['account.tax'].search([('l10n_ar_withholding_payment_type', '!=', False)]).write({'amount_type': 'percent', 'amount' : 1})
+            self.env['account.tax'].search([('l10n_ar_withholding_payment_type', '!=', False), ('l10n_ar_tax_type', 'in', ('iibb_withholding_untaxed', 'iibb_withholding_total'))]).write({'amount_type': 'percent', 'amount' : 1})
         return result
--- addons/l10n_ar_withholding/models/account_tax.py
+++ addons/l10n_ar_withholding/models/account_tax.py
@@ -7,13 +7,36 @@ class AccountTax(models.Model):
     _inherit = 'account.tax'
 
     l10n_ar_withholding_payment_type = fields.Selection(
-        [('supplier', 'Supplier'), ('customer', 'Customer')], 'Argentinean Withholding type',
-        compute="_compute_l10n_ar_withholding_payment_type", store=True, readonly=False)
-
+        [('supplier', 'Vendor Payment'), ('customer', 'Customer Payment')], 'AFIP Argentinean Withholding Type',
+        compute="_compute_l10n_ar_withholding_payment_type", store=True, readonly=False, help="Withholding tax for supplier or customer payments.")
+    l10n_ar_tax_type = fields.Selection(string='Argentinean Tax Type', selection=[
+        ('earnings_withholding', 'Earnings'),
+        ('earnings_withholding_scale', 'Earnings Scale'),
+        ('iibb_withholding_untaxed', 'IIBB Untaxed'),
+        ('iibb_withholding_total', 'IIBB Total Amount'),
+    ],
+        compute="_compute_l10n_ar_tax_type", store=True, readonly=False)
     l10n_ar_withholding_sequence_id = fields.Many2one(
-        'ir.sequence', 'Withholding Number Sequence', copy=False, check_company=True,
+        'ir.sequence', 'AFIP Withholding Sequence', copy=False, check_company=True,
         help='If no sequence provided then it will be required for you to enter withholding number when registering one.')
+    l10n_ar_code = fields.Char('Argentinean Code', help="Earning withholding regimen.")
+    l10n_ar_non_taxable_amount = fields.Float(
+        'Argentinean Non Taxable Amount',
+        digits='Account',
+        help="Until this base amount, the tax is not applied."
+    )
+    l10n_ar_state_id = fields.Many2one(
+        'res.country.state', string="Argentinean State", ondelete='restrict', domain="[('country_id', '=?', country_id)]")
+    l10n_ar_scale_id = fields.Many2one(
+        comodel_name='l10n_ar.earnings.scale',
+        help="Earnings table scale if tax type is 'Earnings Scale'."
+    )
+    l10n_ar_withholding_minimum_threshold = fields.Float(default=0.0, help="If the calculated withholding tax amount is lower than minimum withholding threshold then it is 0.0.")
 
     @api.depends('type_tax_use', 'country_code')
     def _compute_l10n_ar_withholding_payment_type(self):
         self.filtered(lambda x: not x.l10n_ar_withholding_payment_type or x.type_tax_use != 'none' or x.country_code != 'AR').l10n_ar_withholding_payment_type = False
+
+    @api.depends('l10n_ar_withholding_payment_type')
+    def _compute_l10n_ar_tax_type(self):
+        self.filtered(lambda x: not x.l10n_ar_withholding_payment_type).l10n_ar_tax_type = False
