2f040fd2c804bc8770c36b35618175d30bc92cb3
kcv-odoo <kcv@odoo.com>
2024-07-18 13:05:39 +0000

    [FIX] sale_loyalty: fix discount issue
    Steps:
    - Create group tax (20%) and add two tax in it (10% each) with
    Tax included true.
    - Create product with 100 price and set that 20% tax on it.
    - Create coupon program with 90 amount on order and generate
    coupon.
    - Create SO and add SOL with that product now apply
    coupon on that SO.
    
    Issue:
    - Amount computation is wrong for coupon value which is greater
    then that product's tax_excluded(80) and it is only applying
    80 instead of 90 because we were not properly computing amount
    of child taxed.
    
    Cause:
    - Discountable was not properly computed in commit [1]. it was
    not taking child tax values into account.
    
    Fix:
    - Add condition to properly check discountable values and take
    child tax amount into consideration.
    
    [1] https://github.com/odoo/odoo/pull/156897/commits/443097b4db29a4909bb0d0c44040140f08d1e433
    
    opw-4033803
    
    closes odoo/odoo#174305
    
    X-original-commit: d3c73ff7e18b860053b10f8f38a621f5773d572c
    Signed-off-by: Kartik Chavda (kcv) <kcv@odoo.com>

--- addons/sale_loyalty/tests/common.py
+++ addons/sale_loyalty/tests/common.py
@@ -2,8 +2,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from collections import defaultdict
-from odoo.addons.sale.tests.test_sale_product_attribute_value_config import TestSaleProductAttributeValueCommon
+
+from odoo import Command
 from odoo.exceptions import ValidationError
+
+from odoo.addons.sale.tests.test_sale_product_attribute_value_config import TestSaleProductAttributeValueCommon
 
 
 class TestSaleCouponCommon(TestSaleProductAttributeValueCommon):
@@ -66,6 +69,12 @@ class TestSaleCouponCommon(TestSaleProductAttributeValueCommon):
             'price_include': False,
         })
 
+        cls.tax_group = cls.env['account.tax'].create({
+            'name': "tax_group",
+            'amount_type': 'group',
+            'children_tax_ids': [Command.set((cls.tax_10pc_incl + cls.tax_10pc_base_incl).ids)],
+        })
+
         #products
         cls.product_A = cls.env['product.product'].create({
             'name': 'Product A',
@@ -86,6 +95,13 @@ class TestSaleCouponCommon(TestSaleProductAttributeValueCommon):
             'list_price': 100,
             'sale_ok': True,
             'taxes_id': [(6, 0, [])],
+        })
+
+        cls.product_D = cls.env['product.product'].create({
+            'name': 'Product D',
+            'list_price': 100,
+            'sale_ok': True,
+            'taxes_id': [(6, 0, [cls.tax_group.id])],
         })
 
         cls.product_gift_card = cls.env['product.product'].create({
