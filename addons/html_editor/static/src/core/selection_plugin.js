4dc488e4afa8f9c524653f38bf92cd65a8b37b77
Sébastien Geelen (sge) <sge@odoo.com>
2024-07-19 13:36:30 +0200

    [FIX] html_editor: fix loss of focus
    When the powerbox open the dynamic placeholder
    and there is some links inside the current document,
    the `removeFEFFs` function trigger by the CLEAN was
    stealing the focus from the dynamic placeholder popover.
    
    To fix this we need ensure `inEditable` is synchronously calculated.
    So the `restore()` function from `preserveSelection()` can correctly
    prevent a focus steal. (see selection_plugin.js :: 433)

--- addons/html_editor/static/src/core/selection_plugin.js
+++ addons/html_editor/static/src/core/selection_plugin.js
@@ -308,7 +308,8 @@ export class SelectionPlugin extends Plugin {
      */
     getEditableSelection({ deep = false } = {}) {
         const selection = this.document.getSelection();
-        if (selection && selection.rangeCount && this.isSelectionInEditable(selection)) {
+        const inEditable = selection && this.isSelectionInEditable(selection);
+        if (inEditable) {
             this.activeSelection = this.makeSelection(selection, true);
         } else if (!this.activeSelection.anchorNode.isConnected) {
             this.activeSelection = this.makeSelection();
@@ -345,6 +346,7 @@ export class SelectionPlugin extends Plugin {
             startOffset,
             endContainer,
             endOffset,
+            inEditable,
             commonAncestorContainer: range.commonAncestorContainer,
             cloneContents: () => range.cloneContents(),
         });
