12636e80776a54a978c34be10187cec5813d6cf0
Mélanie <mepe@odoo.com>
2024-07-19 11:26:47 +0200

    [FIX] hr_calendar: fix unavailable partner for an allday event
    STEP TO REPRODUCE:
    =================
        1- Go on calendar Application
        2- Click on sunday all day box
    
    You will have a traceback about unavailable partner computation.
    
    Expect Behaviour:
    =================
    If the employee is not supposed to work during an entire day; when you
    want to create an all day event during this day the employee's partner
    will be unavailable.
    
    task-4042363

--- addons/hr_calendar/tests/test_event_interval.py
+++ addons/hr_calendar/tests/test_event_interval.py
@@ -41,3 +41,83 @@ class TestEventInterval(TestHrCalendarCommon):
                 self.env['resource.calendar']
             )
         ])
+
+    def test_allday_event_during_working_day(self):
+        event = self.env['calendar.event'].with_context(company_id=self.company_A.id).create([
+            {
+                'start': datetime(2024, 7, 12),
+                'stop': datetime(2024, 7, 12, 23, 59, 59),
+                'allday': True,
+                'name': "Event 1"
+            }
+        ])
+        result = event._get_events_interval()
+        self.assertEqual(result.get(event)._items, [
+            (
+                timezone('Europe/Brussels').localize(datetime(2024, 7, 12, 8, 0, 0)),
+                timezone('Europe/Brussels').localize(datetime(2024, 7, 12, 12, 0, 0)),
+                self.env['resource.calendar']
+            ),
+            (
+                timezone('Europe/Brussels').localize(datetime(2024, 7, 12, 13, 0, 0)),
+                timezone('Europe/Brussels').localize(datetime(2024, 7, 12, 16, 0, 0)),
+                self.env['resource.calendar']
+            )
+        ])
+
+    def test_allday_event_during_no_working_day(self):
+        """
+        An allday event is calculated using the company's calendar. If the event is scheduled on a day when the company
+        is closed, the duration of the event will be set to zero.
+        """
+
+        # A : Saturday
+        # B : Friday - Saturday
+        # C : Sunday - Monday
+        # D : Friday - Saturday - Sunday - Monday
+        events = self.env['calendar.event'].with_context(company_id=self.company_A.id).create([
+            {
+                'start': datetime(2024, 7, 13),
+                'stop': datetime(2024, 7, 13, 23, 59, 59),
+                'allday': True,
+                'name': "Event A"
+            },
+            {
+                'start': datetime(2024, 7, 12),
+                'stop': datetime(2024, 7, 13, 23, 59, 59),
+                'allday': True,
+                'name': "Event B"
+            },
+            {
+                'start': datetime(2024, 7, 14),
+                'stop': datetime(2024, 7, 15, 23, 59, 59),
+                'allday': True,
+                'name': "Event C"
+            },
+            {
+                'start': datetime(2024, 7, 12),
+                'stop': datetime(2024, 7, 15, 23, 59, 59),
+                'allday': True,
+                'name': "Event D"
+            }
+        ])
+        result = events._get_events_interval()
+        for interval in result.values():
+            self.assertEqual(interval._items, [])
+
+    def test_event_during_working_day(self):
+        event = self.env['calendar.event'].with_context(company_id=self.company_A.id).create([
+            {
+                'start': datetime(2024, 7, 12, 8, 30, 0),
+                'stop': datetime(2024, 7, 12, 9, 30, 0),
+                'name': "Event 3"
+            }
+        ])
+        result = event._get_events_interval()
+        self.assertEqual(result.get(event)._items, [
+            (
+                timezone('UTC').localize(datetime(2024, 7, 12, 8, 30, 0)),
+                timezone('UTC').localize(datetime(2024, 7, 12, 9, 30, 0)),
+                self.env['resource.calendar']
+            )
+        ])
