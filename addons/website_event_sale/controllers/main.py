7a96cb289646957fd135477779152269b8a7dac0
Mattis Megevand <mmeg@odoo.com>
2024-07-19 12:36:23 +0200

    [FIX] website_event_sale: check seats availability before payment
    Steps to reproduce:
    - Install `website_event_sale`
    - Go to an event and for one ticket set maximum as 1
    - Open event page and purchase a ticket, but don't complete the payment
    - Do the same in private navigation
    - Click on pay on the two page
    
    Issues:
    If you check with stripe payment is processed meaning that the two
    people are charged however one of them can't get a ticket as there isn't
    enough tickets available. Fix is to check before sending payment to
    provider and return error if there's not enough seats. Concurrency issue
    is not fully solved but would require much more precise timing, and a
    proper solution wouldn't be stable.
    
    opw-3772093

--- addons/website_event_sale/controllers/main.py
+++ addons/website_event_sale/controllers/main.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 from odoo.http import request, route
 
 from odoo.addons.website_event.controllers.main import WebsiteEventController
+from odoo.addons.website_sale.controllers.main import PaymentPortal
 
 
 class WebsiteEventSaleController(WebsiteEventController):
@@ -70,3 +71,19 @@ class WebsiteEventSaleController(WebsiteEventController):
                 request.website.sale_reset()
 
         return res
+
+
+class PaymentPortalOnsite(PaymentPortal):
+
+    def _validate_transaction_for_order(self, transaction, sale_order_id):
+        """
+        Throws a ValidationError if the user tries to pay for a ticket which isn't available
+        """
+        super()._validate_transaction_for_order(transaction, sale_order_id)
+        sale_order = request.env['sale.order'].browse(sale_order_id).exists()
+        count_per_ticket = request.env['event.registration'].sudo()._read_group(
+            [('sale_order_id', 'in', sale_order.ids), ('state', '!=', 'cancel')],
+            ['event_ticket_id'], ['__count']
+        )
+        for ticket, count in count_per_ticket:
+            ticket._check_seats_availability(minimal_availability=count)
