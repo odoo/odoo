88554c1c568d0a112e47ca0f6ee6b1537300bfdf
Mattis Megevand <mmeg@odoo.com>
2024-07-19 10:36:23 +0000

    [FIX] website_event_sale: check seats availability before payment
    Steps to reproduce:
    - Install `website_event_sale`
    - Go to an event and for one ticket set maximum as 1
    - Open event page and purchase a ticket, but don't complete the payment
    - Do the same in private navigation
    - Click on pay on the two page
    
    Issues:
    If you check with stripe payment is processed meaning that the two
    people are charged however one of them can't get a ticket as there isn't
    enough tickets available. Fix is to check before sending payment to
    provider and return error if there's not enough seats. Concurrency issue
    is not fully solved but would require much more precise timing, and a
    proper solution wouldn't be stable.
    
    opw-3772093
    
    closes odoo/odoo#185845
    
    X-original-commit: 2ef5e4f240e94037665ec00066072ba5988a05bc
    Signed-off-by: Renaud Thiry (reth) <reth@odoo.com>
    Signed-off-by: Mattis Megevand (mmeg) <mmeg@odoo.com>

--- addons/website_event_sale/tests/test_frontend_buy_tickets.py
+++ addons/website_event_sale/tests/test_frontend_buy_tickets.py
@@ -4,7 +4,9 @@ from datetime import timedelta
 
 import odoo.tests
 from odoo.addons.base.tests.common import HttpCaseWithUserDemo
+from odoo.addons.payment.tests.http_common import PaymentHttpCommon
 from odoo.fields import Datetime
+from odoo.tools import mute_logger
 
 from .common import TestWebsiteEventSaleCommon
 
@@ -26,28 +28,13 @@ class TestUi(HttpCaseWithUserDemo, TestWebsiteEventSaleCommon):
             'is_published': True,
         })
 
-        cls.event_2 = cls.env['event.event'].create({
-            'name': 'Conference for Architects TEST',
-            'user_id': cls.env.ref('base.user_admin').id,
-            'date_begin': (Datetime.today() + timedelta(days=5)).strftime('%Y-%m-%d 07:00:00'),
-            'date_end': (Datetime.today() + timedelta(days=5)).strftime('%Y-%m-%d 16:30:00'),
-            'website_published': True,
-        })
-
-        cls.env['event.event.ticket'].create([{
-            'name': 'Standard',
-            'event_id': cls.event_2.id,
-            'product_id': cls.env.ref('event_product.product_product_event').id,
-            'start_sale_datetime': (Datetime.today() - timedelta(days=5)).strftime('%Y-%m-%d 07:00:00'),
-            'end_sale_datetime': (Datetime.today() + timedelta(90)).strftime('%Y-%m-%d'),
-            'price': 1000.0,
-        }, {
+        cls.env['event.event.ticket'].create({
             'name': 'VIP',
             'event_id': cls.event_2.id,
             'product_id': cls.env.ref('event_product.product_product_event').id,
             'end_sale_datetime': (Datetime.today() + timedelta(90)).strftime('%Y-%m-%d'),
             'price': 1500.0,
-        }])
+        })
 
         cls.event_3 = cls.env['event.event'].create({
             'name': 'Last ticket test',
@@ -128,3 +115,79 @@ class TestUi(HttpCaseWithUserDemo, TestWebsiteEventSaleCommon):
         self.env.user.groups_id += self.env.ref('product.group_product_pricelist')
         self.start_tour("/", 'event_sale_pricelists_different_currencies', login='admin')
     # TO DO - add public test with new address when convert to web.tour format.
+
+
+@odoo.tests.common.tagged('post_install', '-at_install')
+class TestRoutes(HttpCaseWithUserDemo, TestWebsiteEventSaleCommon, PaymentHttpCommon):
+
+    @mute_logger('odoo.http')
+    def test_check_seats_avail_before_purchase(self):
+        self.authenticate(None, None)
+
+        so_line_1, so_line_2 = self.env['sale.order.line'].create([
+            {
+                'event_id': self.event.id,
+                'event_ticket_id': self.ticket.id,
+                'name': self.event.name,
+                'order_id': self.so.id,
+                'product_id': self.ticket.product_id.id,
+                'product_uom_qty': 2,
+            },
+            {
+                'event_id': self.event_2.id,
+                'event_ticket_id': self.ticket_2.id,
+                'name': self.event_2.name,
+                'order_id': self.so.id,
+                'product_id': self.ticket_2.product_id.id,
+            },
+        ])
+        self.so._cart_update(line_id=so_line_1.id, product_id=self.ticket.product_id.id)
+        self.so._cart_update(line_id=so_line_2.id, product_id=self.ticket_2.product_id.id)
+        self.so.order_line.product_uom_qty = 2
+
+        url = self._build_url(f'/shop/payment/transaction/{self.so.id}')
+        self.assertEqual(self.event.seats_taken, 0)
+        self.assertEqual(self.event_2.seats_taken, 0)
+        self.env['event.registration'].create([
+            {
+                'event_id': self.event.id,
+                'event_ticket_id': self.ticket.id,
+                'name': 'reg1',
+                'state': 'done',
+            },
+            {
+                'event_id': self.event_2.id,
+                'event_ticket_id': self.ticket_2.id,
+                'name': 'reg2',
+                'state': 'done',
+            }
+        ])
+        self.assertEqual(self.event.seats_taken, 1)
+        self.assertEqual(self.event_2.seats_taken, 1)
+        self.ticket.write({
+            'seats_max': 2,
+            'seats_limited': True,
+        })
+        self.ticket_2.write({
+            'seats_max': 2,
+            'seats_limited': True,
+        })
+        self.env['event.registration'].create([
+            {'event_id': e.id, 'sale_order_id': self.so.id, 'partner_id': p.id, 'event_ticket_id': t.id}
+            for p in [(self.partner), (self.partner_admin)]
+            for e, t in [(self.event, self.ticket), (self.event_2, self.ticket_2)]
+        ])
+        route_kwargs = {
+            'provider_id': self.provider.id,
+            'payment_method_id': self.payment_method.id,
+            'token_id': None,
+            'amount': self.so.amount_total,
+            'flow': 'direct',
+            'tokenization_requested': False,
+            'landing_route': '/shop/payment/validate',
+            'is_validation': False,
+            'csrf_token': odoo.http.Request.csrf_token(self),
+            'access_token': self.so._portal_ensure_token(),
+        }
+        with self.assertRaisesRegex(odoo.tests.JsonRpcException, 'odoo.exceptions.ValidationError'):
+            self.make_jsonrpc_request(url, route_kwargs)
