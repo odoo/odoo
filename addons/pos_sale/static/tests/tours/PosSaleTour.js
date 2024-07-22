0f9c07d1a988fadc2b55550e6742491f773c886a
roen-odoo <roen@odoo.com>
2024-07-22 14:53:31 +0000

    [FIX] pos_sale: create downpayment line for unique tax combination
    When importing a sale in PoS and doing a downpayment there was only one
    downpayment line created. And because of this the taxes where not always
    correct. If you do a 100% downpayment you would end up with negative
    untaxed amount on the invoice.
    
    Steps to reproduce:
    -------------------
    * Create 2 taxes, A and B with 5% and 10%
    * Create 2 products, A and B. Assign the 2 taxes to the products
    * Create a sale order with the 2 products
    * Open the sale order in PoS and make a downpayment for it
    > Observation: There is only one downpayment line when it should have 2.
    
    Why the fix:
    ------------
    If you do the same flow in the sales app, you get 2 downpayment lines.
    We do this to allign the behavior in sales and PoS. For each unique tax
    combination we compute what part of the total downpayment should be
    assigned to it. Then we make sure that the taxes are assigned to the
    line and that the line has the right value.
    
    opw-3999047
    
    X-original-commit: afe4a2b0167d753c43887f9ba32d689934f42f9a

--- addons/pos_sale/static/tests/tours/PosSaleTour.js
+++ addons/pos_sale/static/tests/tours/PosSaleTour.js
@@ -261,3 +261,28 @@ registry
             ProductScreen.selectedOrderlineHas('Test service product', '1.00', '50.00'),
         ].flat(),
     });
+
+registry.category("web_tour.tours").add("PoSDownPaymentLinesPerTax", {
+    test: true,
+    steps: () =>
+        [
+            Dialog.confirm("Open session"),
+            ProductScreen.controlButton("Quotation/Order"),
+            ProductScreen.downPayment20PercentFirstOrder(),
+            Order.hasLine({
+                productName: "Down Payment",
+                quantity: "1.0",
+                price: "2.20",
+            }),
+            Order.hasLine({
+                productName: "Down Payment",
+                quantity: "1.0",
+                price: "1.00",
+            }),
+            Order.hasLine({
+                productName: "Down Payment",
+                quantity: "1.0",
+                price: "3.00",
+            }),
+        ].flat(),
+});
