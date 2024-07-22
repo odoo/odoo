788aebda8fd6aa9487ab05bb1af19d754278521e
Vivek Pathak <vivp@odoo.com>
2024-07-22 14:31:57 +0530

    [IMP] l10n_in: add a pan field for indian company
    This commit adds a field `l10n_in_pan` for the company having country India.
    PAN field can be used to install relevant COA and Taxes via localisation package.
    Type of PAN is also determined using `l10n_in_pan` field and validation error
    for invalid PAN.
    
    Also PAN number is computed from GSTIN.
    
    Task-3930028

--- addons/l10n_in/models/company.py
+++ addons/l10n_in/models/company.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+from stdnum.in_ import pan, gstin
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
@@ -12,7 +14,7 @@ class ResCompany(models.Model):
             ("8", "8 Digits"),
         ],
         string="HSN Code Digit",
-        compute="_compute_l10n_in_hsn_code_digit",
+        compute="_compute_l10n_in_hsn_code_digit_and_l10n_in_pan",
         store=True,
         readonly=False,
     )
@@ -21,14 +23,35 @@ class ResCompany(models.Model):
         help="Enable the use of production credentials",
         groups="base.group_system",
     )
+    l10n_in_pan = fields.Char(
+        string="PAN",
+        compute="_compute_l10n_in_hsn_code_digit_and_l10n_in_pan",
+        store=True,
+        readonly=False,
+        help="PAN enables the department to link all transactions of the person with the department.\n"
+             "These transactions include taxpayments, TDS/TCS credits, returns of income/wealth/gift/FBT,"
+             "specified transactions, correspondence, and so on.\n"
+             "Thus, PAN acts as an identifier for the person with the tax department.",
+    )
+    l10n_in_pan_type = fields.Char(string="PAN Type", compute="_compute_l10n_in_pan_type")
 
     @api.depends('vat')
-    def _compute_l10n_in_hsn_code_digit(self):
+    def _compute_l10n_in_hsn_code_digit_and_l10n_in_pan(self):
         for record in self:
-            if record.vat:
+            if record.country_code == "IN" and record.vat:
                 record.l10n_in_hsn_code_digit = "4"
+                record.l10n_in_pan = gstin.to_pan(record.vat) if gstin.is_valid(record.vat) else False
             else:
                 record.l10n_in_hsn_code_digit = False
+                record.l10n_in_pan = False
+
+    @api.depends('l10n_in_pan')
+    def _compute_l10n_in_pan_type(self):
+        for record in self:
+            if pan.is_valid(record.l10n_in_pan):
+                record.l10n_in_pan_type = pan.info(record.l10n_in_pan)['holder_type']
+            else:
+                record.l10n_in_pan_type = False
 
     def create(self, vals):
         res = super().create(vals)
@@ -49,3 +72,9 @@ class ResCompany(models.Model):
             ChartTemplate = self.env['account.chart.template'].with_company(company)
             fiscal_position_data = ChartTemplate._get_in_account_fiscal_position()
             ChartTemplate._load_data({'account.fiscal.position': fiscal_position_data})
+
+    @api.constrains('l10n_in_pan')
+    def _check_l10n_in_pan(self):
+        for record in self:
+            if record.l10n_in_pan and not pan.is_valid(record.l10n_in_pan):
+                raise ValidationError(_('The entered PAN seems invalid. Please enter a valid PAN.'))
