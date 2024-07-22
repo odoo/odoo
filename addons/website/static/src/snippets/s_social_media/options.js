2e924e9d2e10970c78d12b031dff6118d1563475
Sanjay Sharma <shsa@odoo.com>
2024-07-22 19:00:24 +0530

    [IMP] social_media, *: add Discord to the social networks
    This commit introduces support for Discord as a new social network
    within the application. Users of the Website platform can now link
    their Discord profiles to their websites. Additionally, this update
    includes modifications to templates and the social media section to
    seamlessly integrate Discord alongside existing networks.
    
    *: web, website, website_blog, website_event.
    
    task-3503084
    
    Part-of: odoo/odoo#174528
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>

--- addons/website/static/src/snippets/s_social_media/options.js
+++ addons/website/static/src/snippets/s_social_media/options.js
@@ -90,6 +90,7 @@ options.registry.SocialMedia = options.Class.extend({
             "instagram": _t("Instagram"),
             "github": _t("GitHub"),
             "tiktok": _t("TikTok"),
+            "discord": _t("Discord"),
         };
         const setAriaLabelOfSocialNetwork = (el, name, url) => {
             let ariaLabel = ariaLabelsOfSocialNetworks[name];
@@ -293,6 +294,7 @@ options.registry.SocialMedia = options.Class.extend({
                 "social_instagram",
                 "social_github",
                 "social_tiktok",
+                "social_discord",
             ]).then(function (values) {
                 [dbSocialValues] = values;
                 delete dbSocialValues.id;
@@ -313,6 +315,7 @@ options.registry.SocialMedia = options.Class.extend({
             ['facebook', /^(https?:\/\/)(www\.)?(facebook|fb|m\.facebook)\.(com|me).*$/],
             ['youtube', /^(https?:\/\/)(www\.)?(youtube.com|youtu.be).*$/],
             ['instagram', /^(https?:\/\/)(www\.)?(instagram.com|instagr.am|instagr.com).*$/],
+            ['discord', /^(https?:\/\/)(www\.)?(discord.com|discord.gg).*$/],
         ];
         for (const [socialMedia, regex] of supportedSocialMedia) {
             if (regex.test(url)) {
