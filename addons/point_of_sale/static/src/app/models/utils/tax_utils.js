ee3b2ddd016ef10876f6acbc2a8b517b9a7693fe
roen-odoo <roen@odoo.com>
2024-07-22 14:53:31 +0000

    [FIX] pos_sale: create downpayment line for unique tax combination
    When importing a sale in PoS and doing a downpayment there was only one
    downpayment line created. And because of this the taxes where not always
    correct. If you do a 100% downpayment you would end up with negative
    untaxed amount on the invoice.
    
    Steps to reproduce:
    -------------------
    * Create 2 taxes, A and B with 5% and 10%
    * Create 2 products, A and B. Assign the 2 taxes to the products
    * Create a sale order with the 2 products
    * Open the sale order in PoS and make a downpayment for it
    > Observation: There is only one downpayment line when it should have 2.
    
    Why the fix:
    ------------
    If you do the same flow in the sales app, you get 2 downpayment lines.
    We do this to allign the behavior in sales and PoS. For each unique tax
    combination we compute what part of the total downpayment should be
    assigned to it. Then we make sure that the taxes are assigned to the
    line and that the line has the right value.
    
    opw-3999047
    
    X-original-commit: 42893f8b85eac6b18dec142dadf8c513f1336cf7

--- addons/point_of_sale/static/src/app/models/utils/tax_utils.js
+++ addons/point_of_sale/static/src/app/models/utils/tax_utils.js
@@ -1,5 +1,36 @@
 import { accountTaxHelpers } from "@account/helpers/account_tax";
 
+/**
+ * This method will return a new price so that if you apply the taxes the price will remain the same
+ * For example if the original price is 50. It will compute a new price so that if you apply the tax_ids
+ * the price would still be 50.
+ */
+export const compute_price_force_price_include = (
+    tax_ids,
+    price,
+    product,
+    product_default_values,
+    company,
+    currency,
+    models
+) => {
+    const tax_res = getTaxesValues(
+        tax_ids,
+        price,
+        1,
+        product,
+        product_default_values,
+        company,
+        currency,
+        "total_included"
+    );
+    let new_price = tax_res.total_excluded;
+    new_price += tax_res.taxes_data
+        .filter((tax) => models["account.tax"].get(tax.id).price_include)
+        .reduce((sum, tax) => (sum += tax.tax_amount), 0);
+    return new_price;
+};
+
 export const getTaxesValues = (
     taxes,
     priceUnit,
