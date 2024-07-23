1587e4f6e400f7c1efa070950ab1f359e749e58d
Louis Travaux <lotr@odoo.com>
2024-07-23 11:27:12 +0200

    [FIX] hw_drivers: drivers/interfaces compatibility rpi5
    Fixed printer and serial devices detection: both listing more devices than expected.
    
    Task: 3947355

--- addons/hw_drivers/iot_handlers/interfaces/PrinterInterface_L.py
+++ addons/hw_drivers/iot_handlers/interfaces/PrinterInterface_L.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from cups import Connection as cups_connection
+from cups import Connection as CupsConnection
 from re import sub
 from threading import Lock
 
 from odoo.addons.hw_drivers.interface import Interface
 
-conn = cups_connection()
+conn = CupsConnection()
 PPDs = conn.getPPDs()
 cups_lock = Lock()  # We can only make one call to Cups at a time
+
 
 class PrinterInterface(Interface):
     _loop_delay = 120
@@ -24,19 +25,22 @@ class PrinterInterface(Interface):
             for printer_name, printer in printers.items():
                 path = printer.get('device-uri', False)
                 if printer_name != self.get_identifier(path):
-                    printer.update({'supported': True}) # these printers are automatically supported
-                    device_class = 'network'
-                    if 'usb' in printer.get('device-uri'):
-                        device_class = 'direct'
-                    printer.update({'device-class': device_class})
-                    printer.update({'device-make-and-model': printer_name}) # give name setted in Cups
-                    printer.update({'device-id': ''})
+                    device_class = 'direct' if 'usb' in printer.get('device-uri') else 'network'
+                    printer.update({
+                        'supported': True,
+                        'device-class': device_class,
+                        'device-make-and-model': printer_name,  # give name set in Cups
+                        'device-id': '',
+                    })
                     devices.update({printer_name: printer})
+
         for path, device in devices.items():
             identifier = self.get_identifier(path)
-            device.update({'identifier': identifier})
-            device.update({'url': path})
-            device.update({'disconnect_counter': 0})
+            device.update({
+                'identifier': identifier,
+                'url': path,
+                'disconnect_counter': 0,
+            })
             discovered_devices.update({identifier: device})
         self.printer_devices.update(discovered_devices)
         # Deal with devices which are on the list but were not found during this call of "get_devices"
