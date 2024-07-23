600239e1faf49c62740c76e081d1972bd1c9db35
zel-odoo <zel@odoo.com>
2024-07-23 12:54:55 +0200

    [IMP] mail: use ResizablePanel in discuss
    This commit introduces the ResizablePanel for ActionPanel in Discuss.
    This allows the user to resize the ActionPanel in Discuss.
    
    closes odoo/odoo#173856
    
    Signed-off-by: Zelong Lin (zel) <zel@odoo.com>

--- addons/mail/static/src/discuss/core/common/action_panel.js
+++ addons/mail/static/src/discuss/core/common/action_panel.js
@@ -1,4 +1,5 @@
 import { Component } from "@odoo/owl";
+import { ResizablePanel } from "@web/core/resizable_panel/resizable_panel";
 
 /**
  * @typedef {Object} Props
@@ -8,8 +9,19 @@ import { Component } from "@odoo/owl";
  */
 export class ActionPanel extends Component {
     static template = "mail.ActionPanel";
+    static components = { ResizablePanel };
     static props = {
         title: { type: String, optional: true },
+        resizable: { type: Boolean, optional: true },
         slots: { type: Object, optional: true },
     };
+    static defaultProps = {
+        resizable: true,
+    };
+
+    get classNames() {
+        return `o-mail-ActionPanel overflow-auto d-flex flex-column flex-shrink-0 position-relative py-3 pt-0 h-100 ${
+            !this.env.inChatter ? " px-3 bg-view" : " o-mail-ActionPanel-chatter"
+        }`;
+    }
 }
