50c9c3d55a5ab2a023f6ff7fdbafa6b39cec5e4f
Walid <waha@odoo.com>
2024-07-23 12:24:30 +0200

    [FIX] mrp: is_kits Multicompany
    Steps to reproduce:
    - Create a product that is a kit in company A and not in company B
    - Try updating quantity on hand in company B
    
    Bug:
    since [1] it's not possible to update kit quantites directly
    but is_kits is not company dependant
    
    Fix:
    is_kits depends on the selected companies
    
    opw-3946361
    [1]:https://github.com/odoo/odoo/pull/161124/files
    
    X-original-commit: 5861de1f855e41926062e033e1dfee5988688dfe

--- addons/mrp/models/product.py
+++ addons/mrp/models/product.py
@@ -5,7 +5,7 @@ import collections
 from datetime import timedelta
 from itertools import groupby
 import operator as py_operator
-from odoo import fields, models, _
+from odoo import api, fields, models, _
 from odoo.tools import groupby
 from odoo.tools.float_utils import float_round, float_is_zero
 
@@ -43,6 +43,7 @@ class ProductTemplate(models.Model):
         for product in self:
             product.bom_count = self.env['mrp.bom'].search_count(['|', ('product_tmpl_id', '=', product.id), ('byproduct_ids.product_id.product_tmpl_id', '=', product.id)])
 
+    @api.depends_context('company')
     def _compute_is_kits(self):
         domain = [('company_id', 'in', [False] + self.env.companies.ids),
                   ('product_tmpl_id', 'in', self.ids),
@@ -141,6 +142,7 @@ class ProductProduct(models.Model):
         for product in self:
             product.bom_count = self.env['mrp.bom'].search_count(['|', '|', ('byproduct_ids.product_id', '=', product.id), ('product_id', '=', product.id), '&', ('product_id', '=', False), ('product_tmpl_id', '=', product.product_tmpl_id.id)])
 
+    @api.depends_context('company')
     def _compute_is_kits(self):
         domain = [
             '&', ('company_id', 'in', [False] + self.env.companies.ids),
