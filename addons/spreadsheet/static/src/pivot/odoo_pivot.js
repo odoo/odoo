fda845361b4118d0d83288e2c6b5e70d9847ef11
Pierre Rousseau <pro@odoo.com>
2024-07-23 11:15:24 +0200

    [IMP] spreadsheet: allow to partially reload the pivot
    Currently, when a pivot is updated via an UPDATE_PIVOT command, the
    entire pivot object is recreated from scratch. As a result, all the
    setup of the pivot is executed (e.g. RPCs in Odoo, fields extraction
    from dataSet, etc.) with each update. This is necessary when the pivot
    'structure' changes (columns, rows, measures, etc.), but it is
    unnecessary when only the name is updated. This will become an issue
    with renaming pivot measures, adding calculated fields, and measures
    shown as 'x', as these three operations do not require the setup to be
    re-executed.
    
    See https://github.com/odoo/o-spreadsheet/pull/4695
    
    Task: 4070141
    Part-of: odoo/odoo#179992
    Related: odoo/enterprise#69907
    Signed-off-by: Vincent Schippefilt (vsc) <vsc@odoo.com>

--- addons/spreadsheet/static/src/pivot/odoo_pivot.js
+++ addons/spreadsheet/static/src/pivot/odoo_pivot.js
@@ -6,7 +6,7 @@ import { user } from "@web/core/user";
 import { NO_RECORD_AT_THIS_POSITION, OdooPivotModel } from "./pivot_model";
 import { EvaluationError, PivotRuntimeDefinition, registries, helpers } from "@odoo/o-spreadsheet";
 import { LOADING_ERROR } from "@spreadsheet/data_sources/data_source";
-import { omit } from "@web/core/utils/objects";
+import { deepEqual, omit } from "@web/core/utils/objects";
 import { OdooPivotLoader } from "./odoo_pivot_loader";
 
 const { pivotRegistry, supportedPivotPositionalFormulaRegistry } = registries;
@@ -23,6 +23,7 @@ const { pivotTimeAdapter, toString, areDomainArgsFieldsValid, toNormalizedPivotV
  * @typedef {import("@spreadsheet").OdooPivotCoreDefinition} OdooPivotCoreDefinition
  * @typedef {import("@spreadsheet").SortedColumn} SortedColumn
  * @typedef {import("@spreadsheet").OdooGetters} OdooGetters
+ * @typedef {import("@spreadsheet/data_sources/odoo_data_provider").OdooDataProvider} OdooDataProvider
  */
 
 /**
@@ -40,33 +41,84 @@ export class OdooPivot {
     constructor(services, { definition, getters }) {
         /** @type {"ODOO"} */
         this.type = "ODOO";
+
+        /** @type {OdooPivotCoreDefinition} @protected */
         this.coreDefinition = definition;
+
         this.needsReevaluation = false;
+
         /** @type {OdooPivotRuntimeDefinition | undefined} @protected */
         this.runtimeDefinition = undefined;
+
         /** @type {OdooPivotModel | undefined} @protected */
         this.model = undefined;
-        /** @type {OdooGetters} */
+
+        /** @type {OdooGetters} @protected */
         this.getters = getters;
 
+        /** @protected */
         this.loader = new OdooPivotLoader(services.odooDataProvider, this._load.bind(this));
 
-        /** @type {OdooFields|undefined} @private */
+        /** @type {OdooFields | undefined} @protected */
         this._fields = undefined;
 
-        /** @protected */
+        /** @protected @type {OdooDataProvider}*/
         this.odooDataProvider = services.odooDataProvider;
 
-        /** @protected */
+        /** @protected @type {Object} */
         this.context = omit(definition.context, ...Object.keys(user.context));
 
         /** @protected */
         this.domainWithGlobalFilters = this.coreDefinition.domain;
-
-        this.setup();
     }
 
-    setup() {}
+    /**
+     * @param {OdooPivotCoreDefinition} nextDefinition
+     */
+    onDefinitionChange(nextDefinition) {
+        this.context = omit(nextDefinition.context, ...Object.keys(user.context));
+        this.domainWithGlobalFilters = nextDefinition.domain;
+        const actualDefinition = this.coreDefinition;
+        this.coreDefinition = nextDefinition;
+        if (
+            deepEqual(actualDefinition.columns, nextDefinition.columns) &&
+            deepEqual(actualDefinition.rows, nextDefinition.rows) &&
+            deepEqual(actualDefinition.sortedColumn, nextDefinition.sortedColumn) &&
+            deepEqual(actualDefinition.domain, nextDefinition.domain) &&
+            deepEqual(actualDefinition.context, nextDefinition.context) &&
+            deepEqual(actualDefinition.actionXmlId, nextDefinition.actionXmlId) &&
+            deepEqual(actualDefinition.model, nextDefinition.model)
+        ) {
+            if (deepEqual(actualDefinition.measures, nextDefinition.measures)) {
+                // Nothing change for the table structure, no need to reload the data
+                return;
+            }
+            if (this.isMeasuresTheSameForData(actualDefinition.measures, nextDefinition.measures)) {
+                this.coreDefinition = nextDefinition;
+                const runtimeDefinition = new OdooPivotRuntimeDefinition(
+                    this.coreDefinition,
+                    this.getFields()
+                );
+                this.model.updateMeasures(runtimeDefinition.measures);
+                return;
+            }
+        }
+        this.load({ reload: true });
+    }
+
+    isMeasuresTheSameForData(actualMeasures, nextMeasures) {
+        for (const measure of actualMeasures) {
+            const updatedMeasure = nextMeasures.find((m) => m.id === measure.id);
+            if (
+                !updatedMeasure ||
+                updatedMeasure.fieldName !== measure.fieldName ||
+                updatedMeasure.aggregator !== measure.aggregator
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     async loadMetadata() {
         this._fields = await this.loader.getFields(this.coreDefinition.model);
@@ -103,7 +155,7 @@ export class OdooPivot {
         const model = new OdooPivotModel(
             { _t },
             {
-                metaData: { fields: this.getFields() },
+                fields: this.getFields(),
                 definition,
                 searchParams: {
                     context: this.context,
