58aa1eebbb6da27661dd81e9c51da23bdff554a9
axtr <axtr@odoo.com>
2024-07-23 07:55:56 +0000

    [FIX] point_of_sale: display an error popup if a payment line value has more precision than the rounding precision
    Current behavior:
    When a cashier enter a payment value with more precision than the rounding precision (eg. rounding=0.10 and payment value=1.25), he just cannot proceed to the payment.
    
    Steps to reproduce:
    - Install "Point of Sale" app
    - Go to the settings and enable the cash rounding
    - Go the shop settings and enable the cash rounding and create a down rounding method with a precision of 0.10
    - Start a shop session, select a product and go to the payment screen
    - Select a payment method and write a number on the numpad so that the value goes up to the cent (eg. 1.25)
    - Select another payment method to pay the rest
    
    Solution:
    With this commit, an error popup will be displayed explaining why the cashier cannot proceed the payment and what he could change to authorize the payment. This popup should appear when selecting another payment method and when validating the payment.
    
    opw-3992018
    
    X-original-commit: 3c556f83423013705620daa97d1b72a832f03a5d

--- addons/point_of_sale/tests/test_frontend.py
+++ addons/point_of_sale/tests/test_frontend.py
@@ -1242,6 +1242,28 @@ class TestUi(TestPointOfSaleHttpCommon):
         self.main_pos_config.with_user(self.pos_user).open_ui()
         self.start_tour(f"/pos/ui?config_id={self.main_pos_config.id}", 'PosComboChangeFP', login="pos_user")
 
+    def test_cash_rounding_payment(self):
+        """Verify than an error popup is shown if the payment value is more precise than the rounding method"""
+        rounding_method = self.env['account.cash.rounding'].create({
+            'name': 'Down 0.10',
+            'rounding': 0.10,
+            'strategy': 'add_invoice_line',
+            'profit_account_id': self.company_data['default_account_revenue'].copy().id,
+            'loss_account_id': self.company_data['default_account_expense'].copy().id,
+            'rounding_method': 'DOWN',
+        })
+
+        self.main_pos_config.write({
+            'cash_rounding': True,
+            'only_round_cash_method': False,
+            'rounding_method': rounding_method.id,
+        })
+
+        self.env['ir.config_parameter'].sudo().set_param('barcode.max_time_between_keys_in_ms', 1)
+        self.main_pos_config.open_ui()
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'CashRoundingPayment', login="accountman")
+
+
 # This class just runs the same tests as above but with mobile emulation
 class MobileTestUi(TestUi):
     browser_size = '375x667'
