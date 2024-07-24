3ba4ecd5b85c83a0fe577473405a93565ac658a1
roen-odoo <roen@odoo.com>
2024-07-24 08:48:58 +0000

    [FIX] pos_hr: keep cashier after rehresh
    When refreshing the PoS when logged in with a cashier would disconnect
    the cashier.
    
    Steps to reproduce:
    -------------------
    * Install pos_hr
    * Log in as any cashier
    * Refresh the page
    > Observation: You are not logged in anymore
    
    Why the fix:
    ------------
    We save the current cashier_id in the session storage. This way when we
    refresh the page we can get it back. We use the session storage instead
    of local storage because it's specific to one tab.
    
    opw-4005556
    
    X-original-commit: a19aee88a3387cbd8e4c16eec13be6a3943ab941

--- addons/pos_hr/static/tests/tours/pos_hr_tour.js
+++ addons/pos_hr/static/tests/tours/pos_hr_tour.js
@@ -85,3 +85,25 @@ registry.category("web_tour.tours").add("PosHrTour", {
             Dialog.is("Closing Register"),
         ].flat(),
 });
+
+registry.category("web_tour.tours").add("CashierStayLogged", {
+    test: true,
+    steps: () =>
+        [
+            PosHr.loginScreenIsShown(),
+            PosHr.clickLoginButton(),
+            SelectionPopup.has("Pos Employee1"),
+            SelectionPopup.has("Pos Employee2"),
+            SelectionPopup.has("Mitchell Admin"),
+            SelectionPopup.has("Mitchell Admin", { run: "click" }),
+            Dialog.confirm("Open session"),
+            PosHr.cashierNameIs("Mitchell Admin"),
+            PosHr.refreshPage(),
+            ProductScreen.isShown(),
+            PosHr.cashierNameIs("Mitchell Admin"),
+            Chrome.clickMenuButton(),
+            PosHr.clickLockButton(),
+            PosHr.refreshPage(),
+            PosHr.loginScreenIsShown(),
+        ].flat(),
+});
