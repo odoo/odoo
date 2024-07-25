e64df680c2a944559553a65d8c24c2fdd3778478
william-andre <wan@odoo.com>
2024-07-25 14:18:24 +0200

    [FIX] account_audit_trail: allow merging partners
    When the audit trail feature was enabled, it was impossible to merge the
    partners.
    Since we don't lose any information, we allow that action specifically.
    
    Because it should never be allowed in non controlled settings, we use a
    python token so that the context key cannot be used with RPC calls.
    
    closes odoo/odoo#174533
    
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

--- addons/account_audit_trail/models/__init__.py
+++ addons/account_audit_trail/models/__init__.py
@@ -5,5 +5,6 @@ from . import account_bank_statement_line
 from . import account_move
 from . import mail_message
 from . import mail_tracking_value
+from . import merge_partner_automatic
 from . import res_company
 from . import res_config_settings
