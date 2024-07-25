24a3a9857799b12a5b75d2760d9be2dc68d0e33a
paan-odoo <paan@odoo.com>
2024-07-25 19:07:47 +0530

    [IMP] pos_*: introducing refund with razorpay payment terminal
    pos_*: point_of_sale, pos_razorpay
    
    Before this commit:
    
    - There is no refund option with the Razorpay payment terminal. Users need to
      refund orders paid with the Razorpay payment method using cash or another.
    - We were taking request timeout from the config parameter the Razorpay API
      request.
    
    After this commit:
    
    - Users will have the option to refund Razorpay terminal-based paid orders using
      Razorpay.
    - A refund button will be provided on the terminal refund payment line. Clicking
      on this button will initiate the terminal refund process, and the order will
      be validated after the successful refund process.
    - We will use static request timeout for the Razorpay API request.
    
    task-4061153
    
    closes odoo/odoo#175844
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/pos_razorpay/static/src/app/screens/payment_screen/payment_screen.js
+++ addons/pos_razorpay/static/src/app/screens/payment_screen/payment_screen.js
@@ -1,6 +1,7 @@
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
 import { patch } from "@web/core/utils/patch";
 import { onMounted } from "@odoo/owl";
+import { _t } from "@web/core/l10n/translation";
 
 patch(PaymentScreen.prototype, {
     setup() {
@@ -22,5 +23,36 @@ patch(PaymentScreen.prototype, {
                 }
             }
         });
+    },
+    async addNewPaymentLine(paymentMethod) {
+        if (paymentMethod.use_payment_terminal === "razorpay" && this.isRefundOrder) {
+            const refundedOrder = this.currentOrder.lines[0]?.refunded_orderline_id?.order_id;
+            const razorpayPaymentlines = refundedOrder.payment_ids.filter(
+                (pi) => pi.payment_method_id.use_payment_terminal === "razorpay"
+            );
+            const current_due = this.currentOrder.getDue();
+            if (
+                razorpayPaymentlines.length !== 1 ||
+                Math.abs(current_due) < razorpayPaymentlines[0].amount ||
+                current_due === 0
+            ) {
+                this.pos.notification.add(
+                    _t(
+                        "Adding a new Razorpay payment line is not allowed under the current conditions."
+                    ),
+                    { type: "warning", sticky: false }
+                );
+                return false;
+            }
+            const res = await super.addNewPaymentLine(paymentMethod);
+            const newPaymentLine = this.paymentLines.at(-1);
+            if (res) {
+                newPaymentLine.setAmount(-razorpayPaymentlines[0].amount);
+                newPaymentLine.updateRefundPaymentLine(razorpayPaymentlines[0]);
+            }
+            return res;
+        } else {
+            return await super.addNewPaymentLine(paymentMethod);
+        }
     },
 });
