945153c5bb8260e79b3ec28242fb007398e82e21
Juan José Scarafia <jjs@adhoc.com.ar>
2024-07-25 17:19:01 -0300

    [IMP] add withholding scale

--- addons/l10n_ar_withholding/models/account_tax.py
+++ addons/l10n_ar_withholding/models/account_tax.py
@@ -11,16 +11,18 @@ class AccountTax(models.Model):
         compute="_compute_l10n_ar_withholding_payment_type", store=True, readonly=False)
     l10n_ar_tax_type = fields.Selection(string='Argentinean Tax Type', selection=[
         ('earnings_withholding', 'Earnings Withholding'),
+        ('earnings_withholding_scale', 'Earnings Withholding Scale'),
         ('iibb_withholding', 'IIBB Withholding'),
-    ])
+    ],
+        compute="_compute_l10n_ar_tax_type", store=True, readonly=False)
     l10n_ar_withholding_sequence_id = fields.Many2one(
         'ir.sequence', 'Argentinean Withholding Sequence', copy=False, check_company=True,
         help='If no sequence provided then it will be required for you to enter withholding number when registering one.')
-
-    l10n_ar_withholding_non_taxable_amount = fields.Float(
-        'Argentinean Non-taxable Amount',
+    l10n_ar_code = fields.Char('Argentinean Code')
+    l10n_ar_withholding_auxiliary_amount = fields.Float(
+        'Argentinean Withholding Auxiliary Amount',
         digits='Account',
-        help="Amount to be substracted before applying alicuot"
+        help="This field represents differents things regarding the l10n_ar_tax_type"
     )
     l10n_ar_state_id = fields.Many2one(
         'res.country.state', string="Argentinean State", ondelete='restrict', domain="[('country_id', '=?', country_id)]")
@@ -28,3 +30,7 @@ class AccountTax(models.Model):
     @api.depends('type_tax_use', 'country_code')
     def _compute_l10n_ar_withholding_payment_type(self):
         self.filtered(lambda x: not x.l10n_ar_withholding_payment_type or x.type_tax_use != 'none' or x.country_code != 'AR').l10n_ar_withholding_payment_type = False
+
+    @api.depends('l10n_ar_withholding_payment_type')
+    def _compute_l10n_ar_tax_type(self):
+        self.filtered(lambda x: not x.l10n_ar_withholding_payment_type).l10n_ar_tax_type = False
