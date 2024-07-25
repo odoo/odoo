24184f516643c7612eec239feaf5149fb974c188
Deependra Solanki <deso@odoo.com>
2024-07-25 12:24:49 +0530

    [IMP] html_editor: translate content with a LLM
    Purpose:
    
    This commit introduces a new feature in html editor allowing users to translate
    content using ChatGPT. The translation functionality is accessible via a dialog
    that can be opened from a dropdown in the toolbar.
    
    The dropdown lists only the languages that are currently installed. The user can
    select one of the active languages to translate the text, which can be replaced
    with the selected content.
    
    Use Cases:
    
    1. Email Writing
    2. Barcode lookup where product descriptions in various languages are received
    and need to be published in a specific language.
    
    task-3961141
    
    Part-of: odoo/odoo#174401
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

---
+++ addons/html_editor/static/src/main/chatgpt/language_selector.js
@@ -0,0 +1,29 @@
+import { Component, onWillStart, useState } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
+import { loadLanguages } from "@web/core/l10n/translation";
+
+export class LanguageSelector extends Component {
+    static template = "html_editor.LanguageSelector";
+    static props = {
+        dispatch: Function,
+        getSelection: Function,
+    };
+    static components = { Dropdown, DropdownItem };
+
+    setup() {
+        this.orm = useService("orm");
+        this.state = useState({
+            languages: [],
+        });
+        onWillStart(() => {
+            loadLanguages(this.orm).then((res) => {
+                this.state.languages = res;
+            });
+        });
+    }
+    onSelected(language) {
+        this.props.dispatch("OPEN_CHATGPT_DIALOG", { language });
+    }
+}
