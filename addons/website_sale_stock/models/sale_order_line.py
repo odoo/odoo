6b72e2c86ebe2dcd94d898f4d81ffd0964c55237
amcr-odoo <amcr@odoo.com>
2024-07-26 05:53:06 +0000

    [FIX] website_sale_stock: add product name in payment error
    __Current behavior before commit:__
    When you check out your cart with a product not in the stock anymore you have an error message. There is no product name in this message so if you have multiple products in your cart you don't know which one to remove.
    
    __Description of the fix:__
    Add the product name in the error message, so it's easy to identify which product to remove of the cart.
    The warning method is linked to sale.order.line so we can access to the product directly.
    
    __Steps to reproduce the issue:__
    - add products to your cart
    - remove one or many products of the stock
    - try to validate your payment
    
    opw-4016059 (upgrade issues)
    
    closes odoo/odoo#177043
    
    X-original-commit: 2e6639bef50aec13e3eaaa3cc7f4ccb59aa9abd5
    Related: odoo/enterprise#68546
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/website_sale_stock/models/sale_order_line.py
+++ addons/website_sale_stock/models/sale_order_line.py
@@ -9,8 +9,8 @@ class SaleOrderLine(models.Model):
     def _set_shop_warning_stock(self, desired_qty, new_qty):
         self.ensure_one()
         self.shop_warning = _(
-            'You ask for %(desired_qty)s products but only %(new_qty)s is available',
-            desired_qty=desired_qty, new_qty=new_qty
+            'You ask for %(desired_qty)s %(product_name)s but only %(new_qty)s is available',
+            desired_qty=desired_qty, product_name=self.product_id.name, new_qty=new_qty
         )
         return self.shop_warning
 
