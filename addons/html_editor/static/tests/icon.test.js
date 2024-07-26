6103028f63335d7258340b6490d6946761541066
fdardenne <dafl@odoo.com>
2024-07-26 15:43:28 +0200

    [IMP] html_editor: implement color selector for icons
    This commit adds the color selector buttons in the toolbar
    when clicking on an icon.
    
    closes odoo/odoo#174640
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/html_editor/static/tests/icon.test.js
+++ addons/html_editor/static/tests/icon.test.js
@@ -1,6 +1,7 @@
 import { expect, test } from "@odoo/hoot";
 import { click, waitFor } from "@odoo/hoot-dom";
 import { setupEditor } from "./_helpers/editor";
+import { animationFrame } from "@odoo/hoot-mock";
 
 test("icon toolbar is displayed", async () => {
     await setupEditor(`<p><span class="fa fa-glass">[]</span></p>`);
@@ -33,4 +34,18 @@ test("Can spin an icon", async () => {
     expect("span.fa-glass").toHaveCount(1);
     click("button[name='icon_spin']");
     expect("span.fa-glass").toHaveClass("fa-spin");
+});
+
+test("Can set icon color", async () => {
+    await setupEditor(`<p><span class="fa fa-glass">[]</span></p>`);
+    await waitFor(".o-we-toolbar");
+    expect(".o_font_color_selector").toHaveCount(0);
+    click(".o-select-color-foreground");
+    await animationFrame();
+    expect(".o_font_color_selector").toHaveCount(1);
+    click(".o_color_button[data-color='#6BADDE']");
+    await animationFrame();
+    expect(".o-we-toolbar").toHaveCount(1); // toolbar still open
+    expect(".o_font_color_selector").toHaveCount(0); // selector closed
+    expect("span.fa-glass").toHaveStyle({ color: "rgb(107, 173, 222)" });
 });
