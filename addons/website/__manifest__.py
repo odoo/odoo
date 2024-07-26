2e2e922598bdda6eaab9d1f54692dc3e405cd621
Chrysanthe (chgo) <chgo@odoo.com>
2024-07-26 10:21:24 +0200

    [IMP] website: introduce `s_shape_image` snippet
    This commit introduces a new snippet named `s_shape_image`.
    
    task-4094366
    Part of task-4077427

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -104,6 +104,7 @@
         'views/snippets/s_striped.xml',
         'views/snippets/s_intro_pill.xml',
         'views/snippets/s_big_number.xml',
+        'views/snippets/s_shape_image.xml',
         'views/snippets/s_text_highlight.xml',
         'views/snippets/s_pricelist_cafe.xml',
         'views/snippets/s_progress_bar.xml',
