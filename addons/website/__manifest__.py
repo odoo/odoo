34864eb4473b04916d01c7aad998cc2c2f8ec2e0
Chrysanthe (chgo) <chgo@odoo.com>
2024-07-26 10:21:24 +0200

    [IMP] website: add the new `s_shape_image` snippet
    task-4094366
    Part of task-4077427
    
    Part-of: odoo/odoo#180736
    Related: odoo/design-themes#919
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -115,6 +115,7 @@
         'views/snippets/s_big_number.xml',
         'views/snippets/s_wavy_grid.xml',
         'views/snippets/s_images_mosaic.xml',
+        'views/snippets/s_shape_image.xml',
         'views/snippets/s_text_highlight.xml',
         'views/snippets/s_pricelist_cafe.xml',
         'views/snippets/s_progress_bar.xml',
