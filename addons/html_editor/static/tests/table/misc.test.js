0d9330ba97278183bef9ed8da0beb536f892aedf
fdardenne <dafl@odoo.com>
2024-07-26 15:31:51 +0200

    [FIX] html_editor: allow to color table cells
    Before, when selecting a cell it was not possible to color its
    background color.
    
    This commit fix it to allow users to color cells background.
    This commit also introduce the `color_apply` resource, this resource
    allows other plugin to take the hand on how certain element
    can be colored. Instead of supporting creating an if each time
    in the `colorApply` method from the `color_plugin`.
    
    Part-of: odoo/odoo#174640
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/html_editor/static/tests/table/misc.test.js
+++ addons/html_editor/static/tests/table/misc.test.js
@@ -1,5 +1,7 @@
 import { expect, test } from "@odoo/hoot";
 import { setupEditor } from "../_helpers/editor";
+import { click, waitFor } from "@odoo/hoot-dom";
+import { animationFrame } from "@odoo/hoot-mock";
 
 function insertTable(editor, cols, rows) {
     editor.dispatch("INSERT_TABLE", { cols, rows });
@@ -10,4 +12,29 @@ test("can insert a table", async () => {
     insertTable(editor, 4, 3);
     expect(el.querySelectorAll("tr").length).toBe(3);
     expect(el.querySelectorAll("td").length).toBe(12);
+});
+
+test("can color cells", async () => {
+    await setupEditor(`
+        <table>
+            <tbody>
+                <tr>
+                    <td>[ab</td>
+                    <td>c]</td>
+                    <td>ef</td>
+                </tr>
+            </tbody>
+        </table>`);
+
+    await waitFor(".o-we-toolbar");
+    expect(".o_font_color_selector").toHaveCount(0);
+    click(".o-select-color-background");
+    await animationFrame();
+    expect(".o_font_color_selector").toHaveCount(1);
+
+    click(".o_color_button[data-color='#6BADDE']");
+    await animationFrame();
+    expect(".o-we-toolbar").toHaveCount(1); // toolbar still open
+    expect(".o_font_color_selector").toHaveCount(0); // selector closed
+    expect("td.o_selected_td").toHaveStyle({ "background-color": "rgb(107, 173, 222)" });
 });
