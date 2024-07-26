42ba9f4f5df6e863e61656d818749265d567707b
Louis Wicket (wil) <wil@odoo.com>
2024-07-26 14:50:02 +0000

    [IMP] tools, *: export props with .translate modifier to .pot files
    v2.3.0 of Owl added the .translate modifier to props, allowing them to
    be translated. This commit updates the script that generates the .pot
    files (the list of source terms to translate) in order to take into
    account props with the .translate modifier.
    
    *: website
    
    Task-3980675
    
    X-original-commit: 3189288beddc2355fc43592351c168cbe7863425

--- odoo/tools/translate.py
+++ odoo/tools/translate.py
@@ -939,18 +939,12 @@ def _extract_translatable_qweb_terms(element, callback):
                 and el.get("t-translation", '').strip() != "off"):
 
             _push(callback, el.text, el.sourceline)
-            # Do not export terms contained on the Component directive of OWL
-            # attributes in this context are most of the time variables,
-            # not real HTML attributes.
-            # Node tags starting with a capital letter are considered OWL Components
-            # and a widespread convention and good practice for DOM tags is to write
-            # them all lower case.
-            # https://www.w3schools.com/html/html5_syntax.asp
-            # https://github.com/odoo/owl/blob/master/doc/reference/component.md#composition
-            if not el.tag[0].isupper() and 't-component' not in el.attrib and 't-set-slot' not in el.attrib:
-                for att in TRANSLATED_ATTRS:
-                    if att in el.attrib:
-                        _push(callback, el.attrib[att], el.sourceline)
+            # heuristic: tags with names starting with an uppercase letter are
+            # component nodes
+            is_component = el.tag[0].isupper() or "t-component" in el.attrib or "t-set-slot" in el.attrib
+            for attr in el.attrib:
+                if (not is_component and attr in TRANSLATED_ATTRS) or (is_component and attr.endswith(".translate")):
+                    _push(callback, el.attrib[attr], el.sourceline)
             _extract_translatable_qweb_terms(el, callback)
         _push(callback, el.tail, el.sourceline)
 
