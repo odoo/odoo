9b95872fc9996e2a8d2f60c9e61a3448e041d16e
omra-odoo <omra@odoo.com>
2024-07-26 12:22:40 +0000

    [FIX] {google, microsoft}_calendar: fix traceback for rapid clicks calendar view
    This issue arises when multiple clicks are made on the calendar view button.
    
    Steps to reproduce:
    - Install either the google_calendar or microsoft_calendar module.
    - Go to calendar > Click multiple times on the calendar view button.
    - Error will be generated
    
    This issue specifically occurs if either the google_calendar or
    microsoft_calendar module has been installed.
    this issue occurs because here
    https://github.com/odoo/odoo/blob/79666cbab0ab0947b3d853ab58a929635c309bab/addons/google_calendar/static/src/views/google_calendar/google_calendar_model.js#L28
    google or microsoft sync will taking longer then 1s, this promise will resolve
    after 1s and the  component is destroyed.
    
    so to address this issue, we need to eliminate the race condition and await the
    completion of the google/microsoft sync.
    
    Task-3768492
    
    closes odoo/odoo#183811
    
    X-original-commit: 4009ec484b695000711a0cd7489b4ef156814665
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/microsoft_calendar/static/tests/microsoft_calendar_tests.js
+++ addons/microsoft_calendar/static/tests/microsoft_calendar_tests.js
@@ -1,7 +1,7 @@
 /** @odoo-module **/
 
 import { click, getFixture, patchDate, makeDeferred, nextTick} from "@web/../tests/helpers/utils";
-import { makeView, setupViewRegistries } from "@web/../tests/views/helpers";
+import { setupViewRegistries } from "@web/../tests/views/helpers";
 import { patchUserWithCleanup } from "@web/../tests/helpers/mock_services";
 import { createWebClient, doAction } from "@web/../tests/webclient/helpers";
 
@@ -9,7 +9,7 @@ let target;
 let serverData;
 const uid = -1;
 
-QUnit.module('Google Calendar', {
+QUnit.module('Microsoft Calendar', {
     beforeEach: function () {
         patchDate(2016, 11, 12, 8, 0, 0);
         serverData = {
@@ -20,21 +20,10 @@ QUnit.module('Google Calendar', {
                         user_id: {string: "user", type: "many2one", relation: 'user'},
                         partner_id: {string: "user", type: "many2one", relation: 'partner', related: 'user_id.partner_id'},
                         name: {string: "name", type: "char"},
-                        start_date: {string: "start date", type: "date"},
-                        stop_date: {string: "stop date", type: "date"},
                         start: {string: "start datetime", type: "datetime"},
                         stop: {string: "stop datetime", type: "datetime"},
-                        allday: {string: "allday", type: "boolean"},
                         partner_ids: {string: "attendees", type: "one2many", relation: 'partner'},
-                        type: {string: "type", type: "integer"},
                     },
-                    records: [
-                        {id: 5, user_id: uid, partner_id: 4, name: "event 1", start: "2016-12-13 15:55:05", stop: "2016-12-15 18:55:05", allday: false, partner_ids: [4], type: 2},
-                        {id: 6, user_id: uid, partner_id: 5, name: "event 2", start: "2016-12-18 08:00:00", stop: "2016-12-18 09:00:00", allday: false, partner_ids: [4], type: 3}
-                    ],
-                    check_access_rights: function () {
-                        return Promise.resolve(true);
-                    }
                 },
                 'appointment.type': {
                     fields: {},
@@ -86,74 +75,7 @@ QUnit.module('Google Calendar', {
     }
 }, function () {
 
-    QUnit.test('sync google calendar', async function (assert) {
-        assert.expect(13);
-
-        let id = 7;
-        await makeView({
-            type: "calendar",
-            resModel: 'calendar.event',
-            serverData,
-            arch:
-            '<calendar class="o_calendar_test" '+
-                'js_class="attendee_calendar" '+
-                'date_start="start" '+
-                'date_stop="stop" '+
-                'attendee="partner_ids" '+
-                'mode="month">'+
-                    '<field name="name"/>'+
-                    '<field name="partner_ids" write_model="filter_partner" write_field="partner_id"/>'+
-            '</calendar>',
-            mockRPC: async function (route, args) {
-                if (route === '/google_calendar/sync_data') {
-                    assert.step(route);
-                    serverData.models['calendar.event'].records.push(
-                        {id: id++, user_id: uid, partner_id: 4, name: "event from google calendar", start: "2016-12-28 15:55:05", stop: "2016-12-29 18:55:05", allday: false, partner_ids: [4], type: 4}
-                    );
-                    return Promise.resolve({status: 'need_refresh'});
-                } else if (route === '/web/dataset/call_kw/calendar.event/search_read') {
-                    assert.step(route);
-                } else if (route === '/web/dataset/call_kw/res.partner/get_attendee_detail') {
-                    return Promise.resolve([]);
-                } else if (route === '/web/dataset/call_kw/res.users/has_group') {
-                    return Promise.resolve(true);
-                } else if (route === '/calendar/check_credentials') {
-                    return Promise.resolve({
-                        google_calendar: true,
-                    });
-                } else if (route === "/web/dataset/call_kw/calendar.event/get_default_duration") {
-                    return 3.25;
-                }
-            },
-        });
-        // select the partner filter
-        await click(target.querySelector('.o_calendar_filter_item[data-value=all] input'));
-        // sync_data was called a first time without filter, event from google calendar was created twice
-        assert.containsN(target, '.fc-event', 4, "should display 4 events on the month");
-
-        await click(target.querySelector('.o_datetime_picker_header .o_next'));
-        await click(target.querySelector('.o_datetime_picker .o_date_item_cell'));
-        await click(target.querySelector('.o_view_scale_selector .dropdown-toggle'));
-        await click(target.querySelector('.o_scale_button_month'));
-        await click(target.querySelector('.o_calendar_button_today'));
-
-        assert.verifySteps([
-            '/google_calendar/sync_data',
-            '/web/dataset/call_kw/calendar.event/search_read',
-            '/google_calendar/sync_data',
-            '/web/dataset/call_kw/calendar.event/search_read',
-            '/google_calendar/sync_data',
-            '/web/dataset/call_kw/calendar.event/search_read',
-            '/google_calendar/sync_data',
-            '/web/dataset/call_kw/calendar.event/search_read',
-            "/google_calendar/sync_data",
-            "/web/dataset/call_kw/calendar.event/search_read",
-        ], 'should do a search_read before and after the call to sync_data');
-
-        assert.containsN(target, '.fc-event', 7, "should now display 7 events on the month");
-    });
-
-    QUnit.test("component is destroyed while sync google calendar", async function (assert) {
+    QUnit.test("component is destroyed while sync microsoft calendar", async function (assert) {
         assert.expect(4);
         const def = makeDeferred();
         serverData.actions = {
@@ -182,7 +104,7 @@ QUnit.module('Google Calendar', {
         const webClient = await createWebClient({
             serverData,
             async mockRPC(route, args) {
-                if (route === '/google_calendar/sync_data') {
+                if (route === '/microsoft_calendar/sync_data') {
                     assert.step(route);
                     return def;
                 } else if (route === '/web/dataset/call_kw/calendar.event/search_read') {
@@ -193,7 +115,7 @@ QUnit.module('Google Calendar', {
                     return Promise.resolve(true);
                 } else if (route === '/calendar/check_credentials') {
                     return Promise.resolve({
-                        google_calendar: true,
+                        microsoft_calendar: true,
                     });
                 } else if (route === "/web/dataset/call_kw/calendar.event/get_default_duration") {
                     return 3.25;
@@ -213,8 +135,8 @@ QUnit.module('Google Calendar', {
         await nextTick();
 
         assert.verifySteps([
-            "/google_calendar/sync_data",
-            "/google_calendar/sync_data",
+            "/microsoft_calendar/sync_data",
+            "/microsoft_calendar/sync_data",
             "/web/dataset/call_kw/calendar.event/search_read"
         ], "Correct RPC calls were made");
     });
