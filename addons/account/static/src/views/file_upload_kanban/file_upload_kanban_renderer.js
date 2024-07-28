8911d8e4007250176d8f1c36149c68828f97c42f
kcv-odoo <kcv@odoo.com>
2024-07-28 12:10:34 +0530

    [IMP] account: create documents on pasting files in views
    This commit allows to add copy/pasting files in the list
    and kanban view of invoice and sales views to easily create
    invoices and orders from views.
    
    task-3791197
    
    Part-of: odoo/odoo#172450
    Related: odoo/enterprise#66469
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- addons/account/static/src/views/file_upload_kanban/file_upload_kanban_renderer.js
+++ addons/account/static/src/views/file_upload_kanban/file_upload_kanban_renderer.js
@@ -1,6 +1,7 @@
 import { KanbanRenderer } from "@web/views/kanban/kanban_renderer";
 import { UploadDropZone } from "@account/components/upload_drop_zone/upload_drop_zone";
 import { useState } from "@odoo/owl";
+import { uploadFileFromData } from "../upload_file_from_data_hook";
 
 export class FileUploadKanbanRenderer extends KanbanRenderer {
     static template = "account.FileUploadKanbanRenderer";
@@ -12,5 +13,14 @@ export class FileUploadKanbanRenderer extends KanbanRenderer {
     setup() {
         super.setup();
         this.dropzoneState = useState({ visible: false });
+        this.uploadFileFromData = uploadFileFromData();
+    }
+
+    async onPaste(ev) {
+        if (!ev.clipboardData?.items) {
+            return;
+        }
+        ev.preventDefault();
+        this.uploadFileFromData(ev.clipboardData);
     }
 }
