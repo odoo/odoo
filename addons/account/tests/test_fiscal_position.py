10d5efac603076042199bce2b445ea3b34e0d8e1
guva-odoo <guva@odoo.com>
2024-07-29 15:54:44 +0200

    [FIX] account: fiscal pos differents bill/delivery addresses
    Steps:
    
    - With a belgian company
    - Activate custom addresses
    - Create 2 customer with vat, 1 from NL, another from ZA
    - Create an invoice with billing address ZA and
      delivery address NL
    -> Fiscal position is set to intra-communautaire instead of
       extra-communautaire
    
    opw-4072691

--- addons/account/tests/test_fiscal_position.py
+++ addons/account/tests/test_fiscal_position.py
@@ -211,6 +211,11 @@ class TestFiscalPosition(common.TransactionCase):
             'name': 'NL NO VAT',
             'country_id': self.nl.id,
         })
+        partner_us_vat = self.env['res.partner'].create({
+            'name': 'US VAT',
+            'vat': '34567',
+            'country_id': self.us.id,
+        })
         partner_us_no_vat = self.env['res.partner'].create({
             'name': 'US NO VAT',
             'country_id': self.us.id,
@@ -267,6 +272,24 @@ class TestFiscalPosition(common.TransactionCase):
         # Expected FP : Régime Extra-Communautaire
         self.assertEqual(
             self.env['account.fiscal.position'].get_fiscal_position(partner_us_no_vat.id, partner_us_no_vat.id),
+            fp_eu_extra
+        )
+
+        # Case : 7
+        # Billing (VAT/country) : None/US
+        # Delivery (VAT/country) : NL/NL
+        # Expected FP : Régime Extra-Communautaire
+        self.assertEqual(
+            self.env['account.fiscal.position'].get_fiscal_position(partner_us_no_vat.id, partner_nl_vat.id),
+            fp_eu_extra
+        )
+
+        # Case : 8
+        # Billing (VAT/country) : US/US
+        # Delivery (VAT/country) : NL/NL
+        # Expected FP : Régime Extra-Communautaire
+        self.assertEqual(
+            self.env['account.fiscal.position'].get_fiscal_position(partner_us_vat.id, partner_nl_vat.id),
             fp_eu_extra
         )
 
