522be784453905b95d481670720624a7f9cf830b
Xavier Morel <xmo@odoo.com>
2024-07-29 12:10:31 +0200

    [FIX] test_assetsbundle: avoid blowing up if website is not installed
    `test_assetsbundle` does *not* depend on `website`.

--- odoo/addons/test_assetsbundle/tests/test_assetsbundle.py
+++ odoo/addons/test_assetsbundle/tests/test_assetsbundle.py
@@ -2014,7 +2014,8 @@ class TestErrorManagement(HttpCase):
             self.start_tour('/web', 'css_error_tour', login='admin')
 
     def test_assets_bundle_css_error_frontend(self):
-        self.env['ir.qweb']._get_asset_bundle('web.assets_frontend', assets_params={'website_id': self.env['website'].search([], limit=1).id}).css() # force pregeneration so that we have the base style
+        whatever = {'website_id': website.search([], limit=1).id} if (website := self.env.get('website')) else {}
+        self.env['ir.qweb']._get_asset_bundle('web.assets_frontend', assets_params=whatever).css() # force pregeneration so that we have the base style
         self.env['ir.asset'].create({
             'name': 'Css error',
             'bundle': 'web.assets_frontend',
