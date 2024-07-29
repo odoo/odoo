9ab14ad39e1f4c9d7c1839d1f2ea493d85f51d23
Olivier Dony <odo@odoo.com>
2024-07-29 18:50:34 +0200

    [IMP] auth*: refine credentials types
    As of #168892 we support multiple types of credentials instead of the
    former "password-only" kind.
    
    For auth mechanisms that don't use a password, we can now refine the
    type of credentials being processed, to better differentiate them.
    
    We should also make all credential type checks explicit, to make
    debugging easier in case of password check failure.
    
    Third-party authentication modules should do the same when they upgrade
    to the new credential mechanism.

--- addons/auth_oauth/models/res_users.py
+++ addons/auth_oauth/models/res_users.py
@@ -134,9 +134,11 @@ class ResUsers(models.Model):
         try:
             return super()._check_credentials(credential, env)
         except AccessDenied:
+            if not (credential['type'] == 'oauth_token' and credential['token']):
+                raise
             passwd_allowed = env['interactive'] or not self.env.user._rpc_api_keys_only()
             if passwd_allowed and self.env.user.active:
-                res = self.sudo().search([('id', '=', self.env.uid), ('oauth_access_token', '=', credential['password'])])
+                res = self.sudo().search([('id', '=', self.env.uid), ('oauth_access_token', '=', credential['token'])])
                 if res:
                     return {
                         'uid': self.env.user.id,
