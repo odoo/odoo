4f2f757ecbaffe96d6e84c64b2959e4446ab81e4
Vincent Larcin <vila@odoo.com>
2024-07-30 16:59:52 +0200

    [IMP] sale_project: copy the company of the project template
    When a product in a sales order creates a project, we set the company of the sales order on the project.
    However, it would make sense to use the company of the project template, if there is one, so that we can for instance use a template with no company to make all the generated projects public to all companies.
    
    With this commit, when a SO confirmation creates exactly one project and this project comes from a template, we set the company of the template project on the created project.
    
    Limitation: We can't do it if there is more than one project being created, as they will share the same analytic account, and we can't have projects from different companies linked to the same account.
    
    Task-3940637

--- addons/sale_project/models/sale_order.py
+++ addons/sale_project/models/sale_order.py
@@ -130,6 +130,16 @@ class SaleOrder(models.Model):
             # Orders from different companies are confirmed together
             for order in self:
                 order.order_line.sudo().with_company(order.company_id)._timesheet_service_generation()
+
+        # If the order has exactly one project and that project comes from a template, set the company of the template
+        # on the project.
+        for order in self:
+            if len(order.project_ids) == 1:
+                project = order.project_ids[0]
+                for sol in order.order_line:
+                    if project == sol.project_id and (project_template := sol.product_template_id.project_template_id):
+                        project.sudo().company_id = project_template.sudo().company_id
+                        break
         return super()._action_confirm()
 
     def action_view_task(self):
