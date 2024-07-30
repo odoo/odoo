05fb108bfb0fc6e1341be8cdcbf1ffdb4f9dfeab
Hesham <hsal@odoo.com>
2024-07-30 08:05:31 +0000

    [FIX] mail: Attachment preview buttons not clickable
    In chromium based browsers, after poping out the attachment and closing the new window, the buttons (next, previous, popout) in the attachment preview are not clickable any more.
    This was happening because the mail_popout_service was relying on DOM manipulations instead of using owl framework.
    This commit fixes this problem.
    
    task-4032316
    
    closes odoo/odoo#183457
    
    X-original-commit: e0bb2fafc804b205d145c00b5139d8c948b9d482
    Related: odoo/enterprise#71835
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/mail/static/src/core/common/mail_popout_service.js
+++ addons/mail/static/src/core/common/mail_popout_service.js
@@ -1,84 +1,95 @@
 import { registry } from "@web/core/registry";
-import { reactive } from "@odoo/owl";
+import { App } from "@odoo/owl";
+import { getTemplate } from "@web/core/templates";
+import { browser } from "@web/core/browser/browser";
 
 export const mailPopoutService = {
-    dependencies: ["ui"],
-    start(env, { ui }) {
-        const anchor = document.createElement("div");
+    start(env) {
         let externalWindow;
-        let currentReplacement;
+        let beforeFn;
+        let afterFn;
+        let app;
 
         /**
          * Reset the external window to its initial state:
-         * - put the current replacement back in the document
+         * - Reset the external window header from main window (for appropriate title and other meta data)
          * - clear the external window's document body
-         * - set the current replacement to null
+         * - destroy the current app mounted on the window
          */
         function reset() {
-            if (externalWindow?.document?.body?.firstChild && anchor.isConnected) {
-                anchor.after(externalWindow.document.body.firstChild);
-            }
             if (externalWindow) {
+                externalWindow.document.head.innerHTML = "";
+                externalWindow.document.write(window.document.head.outerHTML);
                 externalWindow.document.body = externalWindow.document.createElement("body");
             }
-            currentReplacement = null;
+            if (app) {
+                app.destroy();
+                app = null;
+            }
         }
 
         /**
          * Poll the external window to detect when it is closed.
-         * Trigger a resize event on the main window when the external window is closed.
+         * the afterPopout hook (afterFn) is then called after the window is closed
          */
         async function pollClosedWindow() {
             while (externalWindow) {
                 await new Promise((r) => setTimeout(r, 1000));
                 if (externalWindow.closed) {
-                    reset();
                     externalWindow = null;
-                    ui.bus.trigger("resize");
+                    afterFn();
                 }
             }
         }
 
         /**
-         * Swap the given element to the external window.
+         * This function registers hooks (before/after the window popout)
+         * @param {Function} beforePopout: this function is called before the component is initially mounted on the external window.
+         * @param {Function} afterPopout: this function is called after the external window is closed.
+         */
+        function addHooks(beforePopout = () => {}, afterPopout = () => {}) {
+            beforeFn = beforePopout;
+            afterFn = afterPopout;
+        }
+
+        /**
+         * Mounts the passed component (with its props) on an external window.
          * If the external window does not exist, it is created.
-         * If an element was already swapped, it is put back in the document before the new element is swapped.
-         * @param {Element} element: The element to swap to the external window
-         * @param {Boolean} triggerResize: Whether to trigger a resize event on the main window
+         * @param {class} component: The component to be mounted.
+         * @param {Props} props: The props of the component.
          * @returns {Window} The external window
          */
-        function popout(element, triggerResize = true) {
+        function popout(component, props) {
             if (!externalWindow || externalWindow.closed) {
-                externalWindow = window.open("about:blank", "_blank", "popup=yes");
+                externalWindow = browser.open("about:blank", "_blank", "popup=yes");
                 window.addEventListener("beforeunload", () => {
                     if (externalWindow && !externalWindow.closed) {
                         externalWindow.close();
                     }
                 });
                 pollClosedWindow();
-                externalWindow.document.write(window.document.head.outerHTML);
             }
-            if (element !== currentReplacement) {
-                reset();
-                if (element) {
-                    currentReplacement = element;
-                    element.after(anchor);
-                    externalWindow.document.body.append(element);
-                }
-            }
-            if (triggerResize) {
-                ui.bus.trigger("resize");
-            }
+
+            beforeFn();
+            reset();
+            app = new App(component, {
+                name: "Popout",
+                env,
+                props,
+                getTemplate,
+            });
+            app.mount(externalWindow.document.body);
             return externalWindow;
         }
 
-        return reactive({
+        return {
             get externalWindow() {
                 return externalWindow && externalWindow.closed ? null : externalWindow;
             },
             popout,
             reset,
-        });
+            addHooks,
+        };
     },
 };
 
