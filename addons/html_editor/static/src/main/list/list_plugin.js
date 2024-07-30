f3c7e6d8b5d541f30de55ca4f656cd9766dc4402
dhba <dhba@odoo.com>
2024-07-30 16:44:33 +0530

    [IMP] html_editor: automatic checklist detection
    Purpose:
    
    This commit implement the automatic checklist detection feature. User can
    now easily generate checklist by typing '[]' followed by a space.
    
    task-4002195
    
    closes odoo/odoo#174999
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -144,8 +144,9 @@ export class ListPlugin extends Plugin {
         const stringToConvert = blockEl.textContent.substring(0, selection.anchorOffset);
         const shouldCreateNumberList = /^(?:[1aA])[.)]\s$/.test(stringToConvert);
         const shouldCreateBulletList = /^[-*]\s$/.test(stringToConvert);
+        const shouldCreateCheckList = /^\[\]\s$/.test(stringToConvert);
         if (
-            (shouldCreateNumberList || shouldCreateBulletList) &&
+            (shouldCreateNumberList || shouldCreateBulletList || shouldCreateCheckList) &&
             !closestElement(selection.anchorNode, "li")
         ) {
             this.shared.setSelection({
@@ -173,6 +174,8 @@ export class ListPlugin extends Plugin {
                 }
             } else if (shouldCreateBulletList) {
                 this.toggleList("UL");
+            } else if (shouldCreateCheckList) {
+                this.toggleList("CL");
             }
             this.dispatch("ADD_STEP");
         }
