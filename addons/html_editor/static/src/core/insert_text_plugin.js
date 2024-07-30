311ca6b00656060ed53526e0d042498b3b1b9ef7
Rodolpho Lima <rcdl@odoo.com>
2024-07-30 17:26:36 +0200

    [FIX] html_editor: insert text with format
    Before this commit, toggling on a format on a collapsed selection (e.g.
    pressing ctrl+b) followed by typing a character would insert the
    character before the formatting element (thus, outside it), resulting on
    not applying the format to the inserted text. This is due to an
    undesired behavior of the contenteditable when typing inside an empty
    inline element.
    
    This commit circumvents such behavior by adjusting the selection before
    the input event takes place.
    
    Part-of: odoo/odoo#175039
    Signed-off-by: Nicolas Bayet (nby) <nby@odoo.com>

--- addons/html_editor/static/src/core/insert_text_plugin.js
+++ addons/html_editor/static/src/core/insert_text_plugin.js
@@ -1,4 +1,6 @@
+import { boundariesIn } from "@html_editor/utils/position";
 import { Plugin } from "../plugin";
+import { closestElement } from "@html_editor/utils/dom_traversal";
 
 export class InsertTextPlugin extends Plugin {
     static name = "insertText";
@@ -12,6 +14,17 @@ export class InsertTextPlugin extends Plugin {
             const selection = this.shared.getEditableSelection();
             if (!selection.isCollapsed) {
                 this.dispatch("DELETE_SELECTION");
+            } else {
+                const element = closestElement(selection.anchorNode);
+                if (element.hasAttribute("data-oe-zws-empty-inline")) {
+                    // Select its ZWS content to make sure the text will be
+                    // inserted inside the element, and not before (outside) it.
+                    // This addresses an undesired behavior of the
+                    // contenteditable.
+                    const [anchorNode, anchorOffset, focusNode, focusOffset] =
+                        boundariesIn(element);
+                    this.shared.setSelection({ anchorNode, anchorOffset, focusNode, focusOffset });
+                }
             }
             // Default behavior: insert text and trigger input event
         }
