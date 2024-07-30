f59ed8d13e75868f58b33cd52ddd17590b690773
Simon Genin (ges) <ges@odoo.com>
2024-07-30 12:36:02 +0200

    [FIX] web: no urgent save for forms in dialog
    When a form view is in a dialog, there is a save and discard button.
    The current implementation would autosave the form. The autosave
    feature really doesn't make sense in that case.
    So now, form views in dialog do not autosave.
    
    closes odoo/odoo#174966
    
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>

--- addons/web/static/src/views/form/form_controller.js
+++ addons/web/static/src/views/form/form_controller.js
@@ -305,11 +305,13 @@ export class FormController extends Component {
             );
         }
 
-        useExternalListener(document, "visibilitychange", () => {
-            if (document.visibilityState === "hidden") {
-                this.model.root.save();
-            }
-        });
+        if (!this.env.inDialog) {
+            useExternalListener(document, "visibilitychange", () => {
+                if (document.visibilityState === "hidden") {
+                    this.model.root.save();
+                }
+            });
+        }
     }
 
     get modelParams() {
