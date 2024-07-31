f25b88f2dd33438424cbaf29d23f16b6ff2662a5
FrancoisGe <fge@odoo.com>
2024-07-31 13:26:03 +0200

    [IMP] web: header button with hotkey in list/kanban
    The purpose of this commit is to make it possible to add hotkeys
    to header buttons in the list and kanban views.

--- addons/web/static/tests/views/kanban/kanban_view.test.js
+++ addons/web/static/tests/views/kanban/kanban_view.test.js
@@ -17,7 +17,7 @@ import {
     setInputFiles,
 } from "@odoo/hoot-dom";
 import { FileInput } from "@web/core/file_input/file_input";
-import { Deferred, animationFrame, runAllTimers } from "@odoo/hoot-mock";
+import { Deferred, animationFrame, runAllTimers, tick } from "@odoo/hoot-mock";
 import { Component, onRendered, onWillRender, xml } from "@odoo/owl";
 import {
     MockServer,
@@ -13348,6 +13348,38 @@ test.tags("desktop")("scroll on group unfold and progressbar click", async () =>
     await contains(getKanbanColumn(1)).click();
 
     expect.verifySteps(["web_search_read", "scrolled"]);
+});
+
+test.tags("desktop")(`kanban view: press "hotkey" to execute header button action`, async () => {
+    mockService("action", {
+        doActionButton(params) {
+            const { name } = params;
+            expect.step(`execute_action: ${name}`);
+        },
+    });
+
+    await mountView({
+        type: "kanban",
+        resModel: "partner",
+        arch: `
+            <kanban class="o_kanban_test">
+                <header>
+                    <button name="display" type="object" class="display" string="display" display="always" data-hotkey="a"/>
+                </header>
+                <field name="bar" />
+                <templates>
+                    <t t-name="kanban-box">
+                        <div>
+                            <field name="foo" />
+                        </div>
+                    </t>
+                </templates>
+            </kanban>`,
+    });
+
+    press(["alt", "a"]);
+    await tick();
+    expect.verifySteps(["execute_action: display"]);
 });
 
 test.tags("desktop")("action button in controlPanel with display='always'", async () => {
