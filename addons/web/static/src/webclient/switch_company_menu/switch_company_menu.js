e2a6524e4a25e25f30f2e7313bed5e506c78ecde
German Loredo [Vauxoo] <german@vauxoo.com>
2024-07-31 09:15:13 +0000

    [IMP] web: make company selector inheritable
    Currently, it is not possible to inherit the company selector widget.
    To modify the behavior of the widget, developers need to create a new
    one instead of inheriting it, which is not ideal. This commit introduces
    changes that allow inheriting the company selector widget and also makes
    it easier to replace or extend the CompanySelector class within the
    SwitchCompanyMenu.
    
    X-original-commit: ae9872ec60250b5be16ba06412b7c817212c5423

--- addons/web/static/src/webclient/switch_company_menu/switch_company_menu.js
+++ addons/web/static/src/webclient/switch_company_menu/switch_company_menu.js
@@ -7,7 +7,7 @@ import { Component, useChildSubEnv, useState } from "@odoo/owl";
 import { debounce } from "@web/core/utils/timing";
 import { useService } from "@web/core/utils/hooks";
 
-class CompanySelector {
+export class CompanySelector {
     constructor(companyService, toggleDelay) {
         this.companyService = companyService;
         this.selectedCompaniesIds = companyService.activeCompanyIds.slice();
@@ -153,12 +153,13 @@ export class SwitchCompanyMenu extends Component {
     static components = { Dropdown, DropdownItem, SwitchCompanyItem, DropdownGroup };
     static props = {};
     static toggleDelay = 1000;
+    static CompanySelector = CompanySelector;
 
     setup() {
         this.companyService = useService("company");
 
         this.companySelector = useState(
-            new CompanySelector(this.companyService, this.constructor.toggleDelay)
+            new this.constructor.CompanySelector(this.companyService, this.constructor.toggleDelay)
         );
         useChildSubEnv({ companySelector: this.companySelector });
     }
