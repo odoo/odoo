4b0fbe83d06189380926772a9f5973d849cbfc86
gito-odoo <gito@odoo.com>
2024-07-31 16:53:09 +0200

    [FIX] mail: access error when guests removes reaction
    Before this commit, when a guest tried removing a
    reaction you would get an access error.
    Steps to reproduce:
    - Make a channel public
    - Join channel as guest
    - Add reaction to message
    - Remove reaction to message
    
    This happens because the content of the notification sent on the bus
    has data from the `mail.guest` table,which the guest has no access right to.
    This data was added in 5633f799c577cc531944376a1f348577668e1519.
    
    This commit fixes the issue by adding sudo privileges.

--- addons/mail/static/tests/tours/discuss_channel_public_tour.js
+++ addons/mail/static/tests/tours/discuss_channel_public_tour.js
@@ -67,6 +67,36 @@ registry.category("web_tour.tours").add("discuss_channel_public_tour.js", {
             trigger: '.o-mail-Message .o-mail-AttachmentCard:contains("text.txt")',
         },
         {
+            trigger: ".o-mail-Message [title='Add a Reaction']",
+            run: "click",
+        },
+        {
+            trigger: ".o-EmojiPicker .o-Emoji:contains('🙂')",
+            run: "click",
+        },
+        {
+            content: "Reload page (fetch reactions)",
+            trigger: ".o-mail-Message",
+            run() {
+                location.reload();
+            },
+        },
+        {
+            content: "Remove reaction",
+            trigger: ".o-mail-MessageReaction:contains('🙂')",
+            run: "click",
+        },
+        {
+            content: "Reload page (fetch reactions)",
+            trigger: ".o-mail-Message",
+            run() {
+                location.reload();
+            },
+        },
+        {
+            trigger: ".o-mail-Message:not(:has(.o-mail-MessageReaction))",
+        },
+        {
             content: "Click on more menu",
             trigger: ".o-mail-Message [title='Expand']",
             run: "click",
