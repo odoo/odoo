b4cfac71315be0750d30b51103d83fe1240ba5c4
Brieuc-brd <brd@odoo.com>
2024-08-01 08:48:34 +0000

    [FIX] website_event_track: reduce cell height and fix scroll bar
    This commit optimizes cell height by moving the bell icon next to the
    tags.
    
    This commit also allows to have a fixed horizontal scroll bar when an
    agenda has hidden content. This removes the need to go to the bottom of
    the agenda to scroll horizontally.
    
    task-4086340
    
    X-original-commit: 7dcdbf87df6dbeaf97b73b3d00a9bafe599d83ea
    Part-of: odoo/odoo#175587
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>
    Signed-off-by: Brieuc Dejean (brd) <brd@odoo.com>
    Co-authored-by: awa-odoo <awa@odoo.com>

--- addons/website_event_track/static/src/js/website_event_track.js
+++ addons/website_event_track/static/src/js/website_event_track.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 import publicWidget from "@web/legacy/js/public/public_widget";
+import { debounce } from "@web/core/utils/timing";
 
 publicWidget.registry.websiteEventTrack = publicWidget.Widget.extend({
     selector: '.o_wevent_event',
@@ -19,22 +20,81 @@ publicWidget.registry.websiteEventTrack = publicWidget.Widget.extend({
         this._super.apply(this, arguments).then(() => {
             this.$el.find('[data-bs-toggle="popover"]').popover();
 
-            const agendas = Array.from(this.target.getElementsByClassName('o_we_online_agenda'));
+            this.agendas = Array.from(this.target.getElementsByClassName('o_we_online_agenda'));
 
-            if (agendas.length > 0) {
-                this._checkAgendasOverflow(agendas);
+            if (this.agendas.length > 0) {
+                this._checkAgendasOverflow(this.agendas);
+                this.agendaScroller = this.$el[0].querySelector('.o_we_agenda_horizontal_scroller_container');
+                this.agendaScrollerElement = this.agendaScroller.querySelector('.o_we_agenda_horizontal_scroller');
 
-                agendas.forEach(agenda => {
+                this.agendas.forEach(agenda => {
                     agenda.addEventListener('scroll', event => {
                         this._onAgendaScroll(agenda, event);
+                        if(this.agendaScroller && this.visibleAgenda) {
+                            this.agendaScroller.scrollLeft = this.visibleAgenda.scrollLeft;
+                        }
                     });
                 });
 
-                window.addEventListener('resize', () => {
-                    this._checkAgendasOverflow(agendas);
-                });
+                if (this.agendaScroller) {
+                    this._updateAgendaScroll = debounce(this._updateAgendaScroll, 50);
+                    document.querySelector('#wrapwrap.event').addEventListener('scroll',
+                        this._updateAgendaScroll.bind(this)
+                    );
+
+                    window.addEventListener('resize', () => {
+                        this._updateAgendaScroll();
+                    });
+
+                    this.agendaScroller.addEventListener('scroll', () => {
+                        if (this.visibleAgenda) {
+                            this.visibleAgenda.scrollLeft = this.agendaScroller.scrollLeft;
+                        }
+                    });
+
+                    this._updateAgendaScroll();
+                }
             }
-        })
+        });
+    },
+
+    /**
+     * Dynamic horizontal scrollbar.
+     * It's meant the show up as a sticky scrollbar at the bottom of the screen, to allow scrolling
+     * the agenda horizontally even if you've not reached the bottom of the agenda container.
+     * Makes the user experience much smoother.
+     * 
+     * Technically, the code checks "what is the last agenda on the screen" and enables our sticky
+     * scrollbar based on that.
+     */
+    _updateAgendaScroll() {
+        // reverse the agendas, we always want the last agenda "on screen" to be the scrolled one
+        this.visibleAgenda = this.agendas.toReversed().find((el) => {
+            const rect = el.getBoundingClientRect();
+            let containerOffset = {
+                top: rect.top + window.scrollY + 30,  // some offset for a better experience
+                bottom: rect.bottom + window.scrollY
+            };
+            let windowOffset = {
+                top: window.scrollY,
+                bottom: window.scrollY + window.innerHeight
+            };
+
+            // if the top of the container if visible but NOT the bottom
+            return (containerOffset.top < windowOffset.bottom) &&
+                !(containerOffset.bottom < windowOffset.bottom);
+        });
+
+        if (this.visibleAgenda && this.visibleAgenda.classList.contains('o_we_online_agenda_has_content_hidden')) {
+            // need to account for vertical scrollbar width
+            const mainContainer = document.querySelector('#wrapwrap');
+            const verticalScrollbarWidth = mainContainer.offsetWidth - mainContainer.clientWidth;
+
+            this.agendaScroller.classList.remove('d-none');
+            this.agendaScrollerElement.style.width = (this.visibleAgenda.scrollWidth + verticalScrollbarWidth) + 'px';
+        } else {
+            this.agendaScroller.classList.add('d-none');
+        }
     },
 
     /**
@@ -57,7 +117,7 @@ publicWidget.registry.websiteEventTrack = publicWidget.Widget.extend({
      */
     _onAgendaScroll: function (agendaEl, event) {
         const tableEl = agendaEl.querySelector('table');
-        const gutter = 15; // = half $grid-gutter-width
+        const gutter = 4; // = map-get($spacers, 1)
         const gap = tableEl.clientWidth - agendaEl.clientWidth - gutter;
 
         agendaEl.classList.add('o_we_online_agenda_is_scrolling');
