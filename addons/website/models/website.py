9cee65ac044f9d858aa7edf2f716fb8fffc28dd7
Antoine (anso) <anso@odoo.com>
2024-08-01 16:49:18 +0200

    [IMP] website: add new `s_striped_top` snippet
    This commit introduces the new `s_striped_top` intro snippet.
    
    Part of task-4077427
    task-4077621
    
    closes odoo/odoo#175525
    
    Related: odoo/design-themes#850
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -849,6 +849,7 @@ class Website(models.Model):
             #       to close to OXP.
             fallback_create_missing_industry_image('s_banner_default_image_2', 's_image_text_default_image')
             fallback_create_missing_industry_image('s_banner_default_image_3', 's_product_list_default_image_1')
+            fallback_create_missing_industry_image('s_striped_top_default_image', 's_picture_default_image')
             fallback_create_missing_industry_image('s_text_cover_default_image', 's_cover_default_image')
             fallback_create_missing_industry_image('s_showcase_default_image', 's_image_text_default_image')
             fallback_create_missing_industry_image('s_image_hexagonal_default_image', 's_cover_default_image')
