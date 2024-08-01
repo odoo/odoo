odoo/odoo

commit 65e373c583b7ec4ac7718a5b8b5b431a1b610d4e

[IMP] pos_*: allow arbitrary data in related models
*: All pos related modules

This commit allows to access arbitrary data in related models. This is
useful for data that are computed on the server side and that are not
recomputed on the client side.

Before this commit, it was only possible to access data that are linked
to a field in the model.

Now custom data coming from the server can be added to any record with
the prefix `_` in the key. For example, if the server sends the
following data for `pos.order`:

Classic fields:
```
{
  id: 1,
  name: 'Order 1',
  partner_id: 1,
  partner_name: 'Partner 1',
}
```

Custom fields:
```
{
  id: 1,
  name: 'Order 1',
  partner_id: 1,
  partner_name: 'Partner 1',
  _custom_field: 'Custom value',
}
```

closes odoo/odoo#160169

Related: odoo/enterprise#60017
Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

addons/l10n_pe_pos/models/res_partner.py
@@ -16,3 +16,10 @@ class ResPartner(models.Model):
                     consumidor_final_anonimo.display_name,
                 )
             )
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        params = super()._load_pos_data_fields(config_id)
+        if self.env.company.country_id.code == "PE":
+            params['fields'] += ["city_id", "l10n_latam_identification_type_id", "l10n_pe_district"]
+        return params

addons/l10n_pe_pos/static/src/overrides/models/pos_store.js
@@ -9,9 +9,6 @@ patch(PosStore.prototype, {
         await super.processServerData(...arguments);
         if (this.isPeruvianCompany()) {
             this["res.city"] = this.data["res.city"];
-            this.consumidorFinalAnonimoId = this.data.custom["consumidor_final_anonimo_id"];
-            this.default_l10n_latam_identification_type_id =
-                this.data.custom["default_l10n_latam_identification_type_id"];
             this["l10n_latam.identification.type"] = this.data["l10n_latam.identification.type"];
             this["l10n_pe.res.city.district"] = this.data["l10n_pe.res.city.district"];
         }
@@ -23,7 +20,7 @@ patch(PosStore.prototype, {
         const order = super.createNewOrder(...arguments);
 
         if (this.isPeruvianCompany() && !order.partner_id) {
-            this.update({ partner_id: this.consumidorFinalAnonimoId });
+            this.update({ partner_id: this.session._consumidor_final_anonimo_id });
         }
 
         return order;

addons/l10n_ar_pos/models/res_partner.py
@@ -13,3 +13,10 @@ class ResPartner(models.Model):
         for partner in self.ids:
             if partner == consumidor_final_anonimo:
                 raise UserError(_('Deleting this partner is not allowed.'))
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        params = super()._load_pos_data_fields(config_id)
+        if self.env.company.country_id.code == 'AR':
+            params += ['l10n_ar_afip_responsibility_type_id', 'l10n_latam_identification_type_id']
+        return params

addons/l10n_ar_pos/static/src/overrides/models/pos_store.js
@@ -9,13 +9,6 @@ patch(PosStore.prototype, {
         await super.processServerData();
 
         if (this.isArgentineanCompany()) {
-            this.consumidor_final_anonimo_id = this.models["res.partner"].get(
-                this.data.custom.consumidor_final_anonimo_id
-            );
-            this.config.consumidor_final_anonimo_id = this.models["res.partner"].get(
-                this.data.custom.consumidor_final_anonimo_id
-            );
-
             this["l10n_latam.identification.type"] =
                 this.models["l10n_latam.identification.type"].getFirst();
             this["l10n_ar.afip.responsibility.type"] =

addons/point_of_sale/models/res_partner.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _name = 'res.partner'
+    _inherit = ['res.partner', 'pos.load.mixin']
 
     pos_order_count = fields.Integer(
         compute='_compute_pos_order',
@@ -13,6 +12,18 @@ class ResPartner(models.Model):
         groups="point_of_sale.group_pos_user",
     )
     pos_order_ids = fields.One2many('pos.order', 'partner_id', readonly=True)
+
+    @api.model
+    def _load_pos_data_domain(self, data):
+        config_id = self.env['pos.config'].browse(data['pos.config']['data'][0]['id'])
+        return [('id', 'in', config_id.get_limited_partners_loading() + [self.env.user.partner_id.id])]
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        return [
+            'id', 'name', 'street', 'city', 'state_id', 'country_id', 'vat', 'lang', 'phone', 'zip', 'mobile', 'email',
+            'barcode', 'write_date', 'property_account_position_id', 'property_product_pricelist', 'parent_name'
+        ]
 
     def _compute_pos_order(self):
         # retrieve all children partners and prefetch 'parent_id' on them

addons/point_of_sale/static/src/app/store/pos_store.js
@@ -189,15 +189,6 @@ export class PosStore extends Reactive {
         this.user = this.data.models["res.users"].getFirst();
         this.currency = this.data.models["res.currency"].getFirst();
         this.pickingType = this.data.models["stock.picking.type"].getFirst();
-
-        // Custom data
-        this.partner_commercial_fields = this.data.custom.partner_commercial_fields;
-        this.server_version = this.data.custom.server_version;
-        this.base_url = this.data.custom.base_url;
-        this.has_cash_move_perm = this.data.custom.has_cash_move_perm;
-        this.has_available_products = this.data.custom.has_available_products;
-        this.pos_special_products_ids = this.data.custom.pos_special_products_ids;
-        this.open_orders_json = this.data.custom.open_orders;
         this.models = this.data.models;
 
         // Add Payment Interface to Payment Method
@@ -292,10 +283,6 @@ export class PosStore extends Reactive {
                 : this.add_new_order().uuid;
         }
 
-        if (this.open_orders_json) {
-            // This method is to load the demo datas.
-            this.loadOpenOrders(this.open_orders_json);
-        }
         this.markReady();
     }
 
@@ -696,7 +683,7 @@ export class PosStore extends Reactive {
     }
 
     posHasValidProduct() {
-        return this.has_available_products;
+        return this.session._has_available_products;
     }
 
     setSelectedCategory(categoryId) {
@@ -742,7 +729,7 @@ export class PosStore extends Reactive {
         return new Set();
     }
     cashierHasPriceControlRights() {
-        return !this.config.restrict_price_control || this.get_cashier().role == "manager";
+        return !this.config.restrict_price_control || this.get_cashier()._role == "manager";
     }
     generate_unique_id() {
         // Generates a public identification number for the order.
@@ -1173,7 +1160,7 @@ export class PosStore extends Reactive {
                 taxes,
                 price,
                 product,
-                this.company._product_default_values,
+                this.session._product_default_values,
                 order.fiscal_position_id,
                 this.models
             );
@@ -1186,7 +1173,7 @@ export class PosStore extends Reactive {
             price,
             1,
             product,
-            this.company._product_default_values,
+            this.session._product_default_values,
             this.company,
             this.currency
         );
@@ -1278,7 +1265,7 @@ export class PosStore extends Reactive {
     }
     orderExportForPrinting(order) {
         const headerData = this.getReceiptHeaderData(order);
-        const baseUrl = this.base_url;
+        const baseUrl = this.session._base_url;
         return order.export_for_printing(baseUrl, headerData);
     }
     async printReceipt() {

addons/pos_hr/static/src/overrides/models/pos_store.js
@@ -14,7 +14,6 @@ patch(PosStore.prototype, {
         await super.processServerData(...arguments);
         if (this.config.module_pos_hr) {
             this.reset_cashier();
-            this.employee_security = this.data.custom.employee_security;
         }
     },
     async actionAfterIdle() {
@@ -42,7 +41,6 @@ patch(PosStore.prototype, {
     },
     set_cashier(employee) {
         this.cashier = employee;
-        this.cashier.role = this.employee_security[employee.id].role;
         const o = this.get_order();
         if (o && !o.get_orderlines().length) {
             // Order without lines can be considered to be un-owned by any employee.

addons/pos_self_order/static/src/overrides/models/pos_store.js
@@ -7,10 +7,9 @@ patch(PosStore.prototype, {
     // @Override
     async processServerData(loadedData) {
         await super.processServerData(...arguments);
-        this.self_ordering = this.data.custom.self_ordering;
     },
     async getServerOrders() {
-        if (this.self_ordering) {
+        if (this.session._self_ordering) {
             await this.data.callRelated("pos.order", "get_standalone_self_order", []);
         }
 

commit ba8fb1a807069af042ca8f8521b39e654147203c

[FIX] l10n_pe_pos: various bugs preventing opening the POS session
1. Loading the POS with l10n_pe_pos installed will simply fail because
`_load_pos_data_fields` returns a list of fields, not a dict
("TypeError: list indices must be integers or slices, not str")

2. After fixing this first bug, we are met with another traceback in the
JS code: "TypeError: this.update is not a function". The update function
should be called on an order.

Bug 1 was introduced in 65e373c583b7ec4ac7718a5b8b5b431a1b610d4e

X-original-commit: 974a0ccef1d3fac14d7656d262dcd2cb539628af
Part-of: odoo/odoo#175222
Related: odoo/enterprise#67738
Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
Signed-off-by: Julien Van Roy (juvr) <juvr@odoo.com>

addons/l10n_pe_pos/models/res_partner.py
@@ -19,7 +19,7 @@ class ResPartner(models.Model):
 
     @api.model
     def _load_pos_data_fields(self, config_id):
-        params = super()._load_pos_data_fields(config_id)
+        fields = super()._load_pos_data_fields(config_id)
         if self.env.company.country_id.code == "PE":
-            params['fields'] += ["city_id", "l10n_latam_identification_type_id", "l10n_pe_district"]
-        return params
+            fields += ["city_id", "l10n_latam_identification_type_id", "l10n_pe_district"]
+        return fields

addons/l10n_pe_pos/static/src/overrides/models/pos_store.js
@@ -18,7 +18,7 @@ patch(PosStore.prototype, {
         const order = super.createNewOrder(...arguments);
 
         if (this.isPeruvianCompany() && !order.partner_id) {
-            this.update({ partner_id: this.session._consumidor_final_anonimo_id });
+            order.update({ partner_id: this.session._consumidor_final_anonimo_id });
         }
 
         return order;