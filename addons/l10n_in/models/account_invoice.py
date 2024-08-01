4dba18ac85645caf57f03a17cc160fc50e7fb467
Mathieu (mcou) <mcou@odoo.com>
2024-08-01 10:49:25 +0200

    [FIX] l10n_in: consider discounts in hsn summary
    Steps to reproduce:
    - Install l10n_in module and switch to an Indian company
    - Create a new customer invoice
    - Add a line with a discount
    - Click "Preview"
    - In the document under HSN Summary the "Taxable Value" does not take the discount into account
    
    Cause:
    To form the hsm_summary_table, only the price_unit is given without taking the discount into account.
    
    Solution:
    Instead of giving the price_unit, we can give the calculated price_unit with discount.
    
    opw-4045609
    
    closes odoo/odoo#175252
    
    X-original-commit: 51174a7fa6d61f3c76e54432257efa4ba65ca184
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Mathieu Coutant (mcou) <mcou@odoo.com>

--- addons/l10n_in/models/account_invoice.py
+++ addons/l10n_in/models/account_invoice.py
@@ -183,6 +183,7 @@ class AccountMove(models.Model):
                 'l10n_in_hsn_code': line.l10n_in_hsn_code,
                 'quantity': line.quantity,
                 'price_unit': line.price_unit,
+                'discount': line.discount or 0.0,
                 'product': line.product_id,
                 'uom': line.product_uom_id,
                 'taxes': line.tax_ids,
