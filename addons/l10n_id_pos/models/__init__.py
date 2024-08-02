3b7d91d4699ba71cff5022966a96ddccf3097336
nni-odoo <nni@odoo.com>
2024-08-02 11:43:57 +0800

    [ADD] l10n_id_pos: QRIS on PoS
    Extending the QRIS capability to PoS, allowing payment verification after user
    clicks on either confirm or cancel button
    
    3709018
    
    closes odoo/odoo#175455
    
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_id_pos/models/__init__.py
+++ addons/l10n_id_pos/models/__init__.py
@@ -1,6 +1,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import account_move
-from . import template_id
-from . import res_bank
+from . import pos_payment_method
 from . import qris_transaction
+from . import pos_order
