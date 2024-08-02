509e172e1116f25075d8daa9cf9146de7a8b071b
amdi-odoo <amdi@odoo.com>
2024-08-02 09:11:06 +0000

    [FIX] mass_mailing: fix template compressed mode
    Purpose
    =======
    Fix the theme templates which are displayed in compressed mode
    on large screen sizes.
    
    Specification
    =============
    Following the extension of the chatter's width, the iframe width became
    too small to be considered as a large viewport meaning all the bootstrap
    lg rules aren't correctly applied.
    
    Making sure the iframe width is large enough by making its position absolute
    so that it takes the full form width.
    Preventing the website snippets from overlapping the template by adding a
    padding right to the layout.
    
    Choosing the iframe to be position absolute instead of the website snippets
    to make sure the snippets drag to scroll feature still works.
    
    related commits:
    916ac17
    cf6cd5e
    
    Task-4086474
    
    X-original-commit: 3aa9a36ed47d0446e51c8dc991d0f7f6f2bacc80

--- addons/mass_mailing/static/src/js/mass_mailing_html_field.js
+++ addons/mass_mailing/static/src/js/mass_mailing_html_field.js
@@ -436,7 +436,10 @@ export class MassMailingHtmlField extends HtmlField {
 
             $themeSelectorNew.remove();
 
-            this.wysiwyg.setSnippetsMenuFolded(uiUtils.isSmall() || themeName === 'basic');
+            const isSnippetsFolded = uiUtils.isSmall() || themeName === 'basic';
+            this.wysiwyg.setSnippetsMenuFolded(isSnippetsFolded);
+            // Inform the iframe content of the snippets menu visibility
+            this.wysiwyg.$iframeBody.closest('body').toggleClass("has_snippets_sidebar", !isSnippetsFolded);
 
             const $editable = this.wysiwyg.$editable.find('.o_editable');
             this.$editorMessageElements = $editable
