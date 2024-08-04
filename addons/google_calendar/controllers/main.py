8be11bff0e2762bf2f6eeedbd83f31e54aa5db5b
Félix DENIS <felix.denis@outlook.com>
2024-08-04 20:36:08 +0200

    bug fix: take web base url instead of bullshit in google calendar module

--- addons/google_calendar/controllers/main.py
+++ addons/google_calendar/controllers/main.py
@@ -19,7 +19,7 @@ class GoogleCalendarController(CalendarController):
             this URL for authorization for example
         """
         if model == 'calendar.event':
-            base_url = request.httprequest.url_root.strip('/')
+            base_url = kw['fromurl'].strip('/')
             GoogleCal = GoogleCalendarService(request.env['google.service'].with_context(base_url=base_url))
 
             # Checking that admin have already configured Google API for google synchronization !
