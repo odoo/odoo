c7f46403ccae96ed302f2a805d69515a2d99a141
Benjamin Vray <bvr@odoo.com>
2024-08-05 12:41:50 +0000

    [FIX] web_editor: remove option to hide YouTube logo in video player
    After this commit, the option to hide the YouTube logo is removed from
    the video options because the parameter is no longer supported since
    August 15, 2023, in the YouTube API.
    
    For more information, see:
    https://developers.google.com/youtube/player_parameters#modestbranding
    
    opw-4078086
    
    X-original-commit: 759554beaaa8f5bda7e8ff34aba52c0effb87911

--- addons/html_editor/controllers/main.py
+++ addons/html_editor/controllers/main.py
@@ -303,13 +303,12 @@ class HTML_Editor(http.Controller):
 
     @http.route(['/web_editor/video_url/data', '/html_editor/video_url/data'], type='json', auth='user', website=True)
     def video_url_data(self, video_url, autoplay=False, loop=False,
-                       hide_controls=False, hide_fullscreen=False, hide_yt_logo=False,
+                       hide_controls=False, hide_fullscreen=False,
                        hide_dm_logo=False, hide_dm_share=False):
         return get_video_url_data(
             video_url, autoplay=autoplay, loop=loop,
             hide_controls=hide_controls, hide_fullscreen=hide_fullscreen,
-            hide_yt_logo=hide_yt_logo, hide_dm_logo=hide_dm_logo,
-            hide_dm_share=hide_dm_share
+            hide_dm_logo=hide_dm_logo, hide_dm_share=hide_dm_share
         )
 
     @http.route(['/web_editor/attachment/add_data', '/html_editor/attachment/add_data'], type='json', auth='user', methods=['POST'], website=True)
--- addons/html_editor/tools.py
+++ addons/html_editor/tools.py
@@ -48,7 +48,7 @@ def get_video_source_data(video_url):
     return None
 
 
-def get_video_url_data(video_url, autoplay=False, loop=False, hide_controls=False, hide_fullscreen=False, hide_yt_logo=False, hide_dm_logo=False, hide_dm_share=False):
+def get_video_url_data(video_url, autoplay=False, loop=False, hide_controls=False, hide_fullscreen=False, hide_dm_logo=False, hide_dm_share=False):
     """ Computes the platform name, the embed_url, the video id and the video params of the given URL
         (or error message in case of invalid URL).
     """
@@ -79,8 +79,6 @@ def get_video_url_data(video_url, autoplay=False, loop=False, hide_controls=Fals
             params['playlist'] = video_id
         if hide_fullscreen:
             params['fs'] = 0
-        if hide_yt_logo:
-            params['modestbranding'] = 1
         yt_extra = platform_match[1] or ''
         embed_url = f'//www.youtube{yt_extra}.com/embed/{video_id}'
     elif platform == 'vimeo':
