19ced8ddc93c312f75af6d7fddc79f72362b466f
Brieuc-brd <brd@odoo.com>
2024-08-05 12:04:06 +0200

    [IMP] website: introduce `s_numbers_showcase` snippet
    This commit adds the new `s_numbers_showcase`
    snippet.
    
    task-4094383
    Part of task-4077427

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -76,6 +76,7 @@
         'views/snippets/s_timeline.xml',
         'views/snippets/s_process_steps.xml',
         'views/snippets/s_accordion.xml',
+        'views/snippets/s_numbers_showcase.xml',
         'views/snippets/s_text_highlight.xml',
         'views/snippets/s_progress_bar.xml',
         'views/snippets/s_blockquote.xml',
