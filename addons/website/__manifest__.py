8e409df4770098bcc06ab110fa16471a2f73f090
Antoine (anso) <anso@odoo.com>
2024-08-05 11:20:41 +0200

    [IMP] website: add new `s_image_text_box` snippet
    This commit introduces the new `s_image_text_box` content snippet.
    
    Part of task-4077427
    task-4094363
    
    closes odoo/odoo#175657
    
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -37,6 +37,7 @@
         'views/snippets/s_title.xml',
         'views/snippets/s_cover.xml',
         'views/snippets/s_text_cover.xml',
+        'views/snippets/s_image_text_box.xml',
         'views/snippets/s_text_image.xml',
         'views/snippets/s_image_text.xml',
         'views/snippets/s_mockup_image.xml',
