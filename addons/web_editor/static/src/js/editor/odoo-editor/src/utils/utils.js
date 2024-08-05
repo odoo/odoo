d13a3f985ae8e7e85e52e22d6f54a28f0a065614
dhba <dhba@odoo.com>
2024-08-05 12:20:43 +0530

    [FIX] web_editor: placeholder loop when removing image
    Commit [1] fixed a bug where selecting across two paragraphs separated by
    whitespace resulted in multiple indented lists. However, this fix introduced
    a new issue: if a first child of block node was whitespace followed by a <br>,
    the list was not created on the first attempt. Instead, only the whitespace was
    removed, and the list was created on the second attempt.
    
    Commit [2] addressed this issue but introduced a new problem: if a block node had
    whitespace as its first child, the placeholder for that block would enter an
    infinite loop, causing the page to become unresponsive.
    
    This commit reverts the changes made in Commit [2] and introduces a new approach
    that resolves the list creation issue when a block node had whitespace as its
    first child, without causing the placeholder to get stuck in a loop.
    
    [1]: https://github.com/odoo-dev/odoo-editor/pull/229/commits/58a7fadcaaaa7322c6177276ddc22b62da96d1f5
    [2]: 1e982dab025fb86e759b249433f41619a442b5cf
    
    task-4082867
    
    closes odoo/odoo#176178
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
@@ -896,12 +896,7 @@ export function getSelectedNodes(editable) {
  */
 export function getDeepRange(editable, { range, sel, splitText, select, correctTripleClick } = {}) {
     sel = sel || editable.parentElement && editable.ownerDocument.getSelection();
-    if (
-        sel &&
-        sel.isCollapsed &&
-        sel.anchorNode &&
-        (sel.anchorNode.nodeName === "BR" || (sel.anchorNode.nodeType === Node.TEXT_NODE && sel.anchorNode.textContent === ''))
-    ) {
+    if (sel && sel.isCollapsed && sel.anchorNode && sel.anchorNode.nodeName === "BR") {
         setSelection(sel.anchorNode.parentElement, childNodeIndex(sel.anchorNode));
     }
     range = range ? range.cloneRange() : sel && sel.rangeCount && sel.getRangeAt(0).cloneRange();
