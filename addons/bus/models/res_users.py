36d1f4e39e580395eb92b1925eba2f4c4577b6fe
tsm-odoo <tsm@odoo.com>
2024-08-06 11:39:31 +0200

    [PERF] bus, *: only update presence when needed
    *: hr_holidays, mail.
    
    Before this PR, the main tab updated the user presence every minute.
    As a result of this update, it would also receive the IM statuses it
    was interested in.
    
    However, this approach puts significant pressure on the gevent worker,
    especially when it handles more databases than the registry can hold.
    
    To address this issue, this PR improves the way presences are updated:
    - Presences are sent and broadcasted when they change
    (e.g. away => online).
    - When a websocket disconnects, the user is considered disconnected. If
    other devices receive this notification, they will update the user
    presence immediately.
    
    These changes greatly reduce traffic caused by presence updates while
    keeping the IM statuses reliable.
    
    X-original-commit: 3a5d61645ddf5dbebd1b5a4865fe12dd572a7f3e

--- addons/bus/models/res_users.py
+++ addons/bus/models/res_users.py
@@ -1,31 +1,22 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
-from odoo.addons.bus.models.bus_presence import AWAY_TIMER
-from odoo.addons.bus.models.bus_presence import DISCONNECTION_TIMER
 
 
 class ResUsers(models.Model):
     _name = "res.users"
     _inherit = ["res.users", "bus.listener.mixin"]
 
-    im_status = fields.Char('IM Status', compute='_compute_im_status')
+    im_status = fields.Char("IM Status", compute="_compute_im_status")
 
     def _compute_im_status(self):
-        """ Compute the im_status of the users """
-        self.env.cr.execute("""
-            SELECT
-                user_id as id,
-                CASE WHEN age(now() AT TIME ZONE 'UTC', last_poll) > interval %s THEN 'offline'
-                     WHEN age(now() AT TIME ZONE 'UTC', last_presence) > interval %s THEN 'away'
-                     ELSE 'online'
-                END as status
-            FROM bus_presence
-            WHERE user_id IN %s
-        """, ("%s seconds" % DISCONNECTION_TIMER, "%s seconds" % AWAY_TIMER, tuple(self.ids)))
-        res = dict(((status['id'], status['status']) for status in self.env.cr.dictfetchall()))
+        """Compute the im_status of the users"""
+        presence_by_user = {
+            presence.user_id: presence.status
+            for presence in self.env["bus.presence"].search([("user_id", "in", self.ids)])
+        }
         for user in self:
-            user.im_status = res.get(user.id, 'offline')
+            user.im_status = presence_by_user.get(user, "offline")
 
     def _bus_channel(self):
         return self.partner_id._bus_channel()
