c55a4e62c245ff051895387d4165d1f5336903fe
Chrysanthe (chgo) <chgo@odoo.com>
2024-08-06 16:49:54 +0200

    [IMP] website: introduce s_numbers_list snippet
    This commit introduces a new `s_numbers_list` snippet.
    
    task-4094388
    Part of task-4077427

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -120,6 +120,7 @@
         'views/snippets/s_unveil.xml',
         'views/snippets/s_image_hexagonal.xml',
         'views/snippets/s_striped_center_top.xml',
+        'views/snippets/s_numbers_list.xml',
         'views/new_page_template_templates.xml',
         'views/website_views.xml',
         'views/website_pages_views.xml',
