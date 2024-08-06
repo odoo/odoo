6bb3367e9d3c07a4b8a15d19d9728cd0fb40af09
Antoine (anso) <anso@odoo.com>
2024-08-06 18:11:52 +0200

    [IMP] website: add new `s_faq_list` snippet
    This commit introduces the new `s_faq_list` text snippet.
    
    Part of task-4077427
    task-4094399
    
    closes odoo/odoo#175916
    
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -71,6 +71,7 @@
         'views/snippets/s_call_to_action.xml',
         'views/snippets/s_references.xml',
         'views/snippets/s_popup.xml',
+        'views/snippets/s_faq_list.xml',
         'views/snippets/s_faq_collapse.xml',
         'views/snippets/s_features_grid.xml',
         'views/snippets/s_tabs.xml',
