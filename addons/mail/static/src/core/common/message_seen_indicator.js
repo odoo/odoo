2afc6831fe80bb2ca6fb4ef7facbef48b2ec214c
gito-odoo <gito@odoo.com>
2024-08-07 16:11:49 +0200

    [FIX] mail: seen by tooltip when no one has seen message
    Before this commit, the tooltip shown upon hovering the
    single tick on a sent message showed "undefined".
    Steps to reproduce:
    - Send message on a chat with user demo as admin
    - Log in as user demo
    - As the admin user hover the single tick on the message
    
    This happens because the switch case is not covering the case
    of the message being fetched but not seen by anybody.
    
    This commit fixes the issue by adding a case for such instance,
    showing "Sent" as the tooltip.
    
    closes odoo/odoo#176057
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/message_seen_indicator.js
+++ addons/mail/static/src/core/common/message_seen_indicator.js
@@ -47,6 +47,8 @@ export class MessageSeenIndicator extends Component {
         const seenMembers = this.props.message.channelMemberHaveSeen;
         const [user1, user2, user3] = seenMembers.map((member) => member.name);
         switch (seenMembers.length) {
+            case 0:
+                return _t("Sent");
             case 1:
                 return _t("Seen by %(user)s", { user: user1 });
             case 2:
@@ -70,6 +72,9 @@ export class MessageSeenIndicator extends Component {
     }
 
     openDialog() {
+        if (this.props.message.channelMemberHaveSeen.length === 0) {
+            return;
+        }
         this.dialog.add(MessageSeenIndicatorDialog, { message: this.props.message });
     }
 }
