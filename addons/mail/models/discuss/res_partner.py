b66c4d3dac688b4b153db1abd228b89e987d0897
jayp-odoo <jayp@odoo.com>
2024-08-07 17:15:10 +0530

    [IMP] mail: should be able to mention non-members in accessible channels
    before commit:
    
    In channel it was not possible to mention internal users which are non-members
    of channel.
    
    after commit:
    
    now it is possible to mention internal users who has access to the channel,
    also mentioned user will discover it in inbox
    
    task-4100364

--- addons/mail/models/discuss/res_partner.py
+++ addons/mail/models/discuss/res_partner.py
@@ -77,11 +77,42 @@ class ResPartner(models.Model):
                 [("channel_ids", "in", channel.id)],
             ]
         )
-        partners = self._search_mention_suggestions(domain, limit)
+        extra_domain = expression.AND([
+            [('user_ids', '!=', False)],
+            [('user_ids.active', '=', True)],
+            [('partner_share', '=', False)]
+        ])
+        if channel.group_public_id.id:
+            extra_domain = expression.AND(
+                [
+                    extra_domain,
+                    [("user_ids.groups_id", "in", channel.group_public_id.id)],
+                ]
+            )
+        partners = self._search_mention_suggestions(domain, limit, extra_domain)
         members = self.env["discuss.channel.member"].search(
             [
                 ("channel_id", "=", channel.id),
                 ("partner_id", "in", partners.ids),
             ]
         )
-        return Store(members, fields={"channel": [], "persona": []}).add(partners).get_result()
+        store = Store(
+            members,
+            fields={
+                "id": True,
+                "channel": {"id": True},
+                "persona": {"partner": {"id": True}},
+            },
+        )
+        store.add("Thread", {
+            "id": channel.id,
+            "model": "discuss.channel",
+            "authorizedGroupId": channel.group_public_id.id or None
+        })
+        for p in partners:
+            store.add("Persona",{
+                "id": p.id,
+                'type': "partner",
+                "group_id": next((group.id for group in p.user_ids.groups_id if group.id == channel.group_public_id.id), None)
+            })
+        return store.get_result()
