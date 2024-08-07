c3598fe223a874d8e9e857680188ec993be2d2f4
Victorien (viso) <viso@odoo.com>
2024-08-07 10:37:53 +0000

    [FIX] l10n_it_edi: fix multi-currency xml generation
    Following commit https://github.com/odoo/odoo/pull/171765/files#
    multi-currencies are not supported anymore by our edi engine.
    
    Using a currency other than euro leads to an invalid XML file.
    
    Steps to reproduce:
    
    1. install Italian E-invoicing
    2. make sure your have access to another currency, along with EUR
    3. create a partner outside of Europe (e.g. US-based - using USD)
    4. create an invoice for that partner
    5. confirm + send and print the invoice (generate XML file)
    6. go to https://www.fatturacheck.it/ to verify the XML
    7. Error 2.2.2.5 should be returned
    
    opw-4038850
    opw-4129380
    opw-4123758
    opw-4095910
    
    X-original-commit: bc0d50e847f6b2bfeb223db83bad3742d2093bec

--- addons/l10n_it_edi/tests/test_edi_export.py
+++ addons/l10n_it_edi/tests/test_edi_export.py
@@ -386,3 +386,110 @@ class TestItEdiExport(TestItEdi):
         invoice.action_post()
 
         self._assert_export_invoice(invoice, 'invoice_decimal_precision_product.xml')
+
+    def test_export_foreign_currency(self):
+
+        tax_zero_percent_us = self.env['account.tax'].with_company(self.company).create({
+            'name': '0 % US',
+            'amount': 0.0,
+            'amount_type': 'percent',
+            'l10n_it_exempt_reason': 'N3.1',
+            'l10n_it_law_reference': 'Art. 8, c.1, lett.a - D.P.R. 633/1972',
+        })
+
+        american_partner_b = self.env['res.partner'].create({
+            'name': 'US Partner',
+            'city': 'Test city',
+            'country_id': self.env.ref('base.us').id,
+            'zip': '12345',
+            'street': '123 Rainbow Road',
+            'is_company': True,
+        })
+
+        # =============== create invoices ===============
+
+        # usd simple discount % on the product
+        invoice = self.env['account.move'].with_company(self.company).create({
+            'move_type': 'out_invoice',
+            'invoice_date': '2024-08-07',
+            'invoice_date_due': '2024-08-07',
+            'partner_id': american_partner_b.id,
+            'currency_id': self.env.ref('base.USD').id,
+            'invoice_line_ids': [
+                Command.create({
+                    'name': 'A productive product',
+                    'price_unit': 1068.11,
+                    'balance': -841.5,
+                    'quantity': 1,
+                    'tax_ids': [Command.set(tax_zero_percent_us.ids)],
+                    'currency_rate': 1.0789,
+                    'discount': 15,
+                    'currency_id': self.env.ref('base.USD').id,
+                }),
+            ],
+        })
+        invoice.action_post()
+        self._assert_export_invoice(invoice, 'export_foreign_currency_simple_discount.xml')
+
+        # usd discount both on product in % + a global one (negative aml)
+        invoice = self.env['account.move'].with_company(self.company).create({
+            'move_type': 'out_invoice',
+            'invoice_date': '2024-08-06',
+            'invoice_date_due': '2024-08-06',
+            'partner_id': american_partner_b.id,
+            'currency_id': self.env.ref('base.USD').id,
+            'invoice_line_ids': [
+                Command.create({
+                    'name': 'A productive product',
+                    'price_unit': 712.07,
+                    'balance': -561,
+                    'quantity': 1,
+                    'tax_ids': [Command.set(tax_zero_percent_us.ids)],
+                    'currency_rate': 1.0789,
+                    'discount': 15,
+                    'currency_id': self.env.ref('base.USD').id,
+                }),
+                Command.create({
+                    'name': 'A global discount',
+                    'price_unit': -100,
+                    'balance': 92.69,
+                    'quantity': 1,
+                    'tax_ids': [Command.set(tax_zero_percent_us.ids)],
+                    'currency_rate': 1.0789,
+                    'currency_id': self.env.ref('base.USD').id,
+                }),
+            ],
+        })
+        invoice.action_post()
+        self._assert_export_invoice(invoice, 'export_foreign_currency_global_simple_discount.xml')
+
+        # usd discount global (negative aml)
+        invoice = self.env['account.move'].with_company(self.company).create({
+            'move_type': 'out_invoice',
+            'invoice_date': '2024-08-07',
+            'invoice_date_due': '2024-08-07',
+            'partner_id': american_partner_b.id,
+            'currency_id': self.env.ref('base.USD').id,
+            'invoice_line_ids': [
+                Command.create({
+                    'name': 'A productive product',
+                    'price_unit': 712.07,
+                    'balance': -660,
+                    'quantity': 1,
+                    'tax_ids': [Command.set(tax_zero_percent_us.ids)],
+                    'currency_rate': 1.0789,
+                    'currency_id': self.env.ref('base.USD').id,
+                }),
+                Command.create({
+                    'name': 'A global discount',
+                    'price_unit': -200,
+                    'balance': 185.37,
+                    'quantity': 1,
+                    'tax_ids': [Command.set(tax_zero_percent_us.ids)],
+                    'currency_rate': 1.0789,
+                    'currency_id': self.env.ref('base.USD').id,
+                }),
+            ],
+        })
+        invoice.action_post()
+        self._assert_export_invoice(invoice, 'export_foreign_currency_global_discount.xml')
