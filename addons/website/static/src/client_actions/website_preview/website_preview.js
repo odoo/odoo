9f954fb2885334c297f0008f0cc6d797ac512862
Benjamin Vray <bvr@odoo.com>
2024-08-08 15:30:17 +0200

    [FIX] website: fix empty page if blocks have no text
    Steps to reproduce the bug:
    
    - In edit mode, drop a snippet having no text (e.g. Images Wall) or
    remove the text of a snippet (so none of the text tags remain).
    - Save the page.
    - Bug: The snippet is not there. If you refresh, the snippet is here,
    but once you click on "Editor" (to have the purple navbar above), it
    disappears. It is also not there when going in edit mode.
    
    The bug was introduced by this commit [1]. By removing jQuery, this
    commit replaced jQuery's ".html()" with "textContent" instead of
    "innerHTML". As a result, the code that detects if the page is empty to
    add the "welcome message" stopped working properly.
    
    [1]: https://github.com/odoo/odoo/commit/8d29c4a42adf007f5663a94341231d1221408004
    
    task-4104978
    
    closes odoo/odoo#176199
    
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/website/static/src/client_actions/website_preview/website_preview.js
+++ addons/website/static/src/client_actions/website_preview/website_preview.js
@@ -274,7 +274,7 @@ export class WebsitePreview extends Component {
     addWelcomeMessage() {
         if (this.websiteService.isRestrictedEditor) {
             const wrap = this.iframe.el.contentDocument.querySelector('#wrapwrap.homepage #wrap');
-            if (wrap && !wrap.textContent.trim()) {
+            if (wrap && !wrap.innerHTML.trim()) {
                 this.welcomeMessage = renderToElement('website.homepage_editor_welcome_message');
                 this.welcomeMessage.classList.add('o_homepage_editor_welcome_message', 'h-100');
                 while (wrap.firstChild) {
