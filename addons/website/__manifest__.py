9b407cdc9c3ba2a97513f3827dccc1cf00a41be3
Xavier Luyckx (xlu) <xlu@odoo.com>
2024-08-08 13:30:28 +0200

    [IMP] website: introduce `s_unveil` snippet
    This commit adds the new `s_unveil` snippet.
    
    task-4104471
    
    closes odoo/odoo#176167
    
    Part-of: task-4077427
    Related: odoo/design-themes#860
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -103,6 +103,7 @@
         'views/snippets/s_image.xml',
         'views/snippets/s_video.xml',
         'views/snippets/s_cta_badge.xml',
+        'views/snippets/s_unveil.xml',
         'views/new_page_template_templates.xml',
         'views/website_views.xml',
         'views/website_pages_views.xml',
