9b7841da4ecc5760ad3cd107be8d818e2ea1b534
Pedram (pebr) <pebr@odoo.com>
2024-08-08 11:39:29 +0000

    [FIX] point_of_sale: ensure optional products display from info popup
    Prior to this commit, clicking on an optional product within the
    product info popup would fail to locate the optional product due to
    product template filtering applied to product variants. This commit
    resolves the issue by applying product template filtering exclusively
    for variants, ensuring optional products are correctly displayed from
    the product info popup.
    
    opw-4097824
    
    X-original-commit: ad2b28330cf7327094ca06a88f4881ccaf17f131

--- addons/point_of_sale/static/src/app/screens/product_screen/product_info_popup/product_info_popup.js
+++ addons/point_of_sale/static/src/app/screens/product_screen/product_info_popup/product_info_popup.js
@@ -13,9 +13,19 @@ export class ProductInfoPopup extends Component {
         this.pos = usePos();
     }
     searchProduct(productName) {
+<<<<<<< saas-17.2
         this.pos.setSelectedCategory(0);
         this.pos.searchProductWord = productName;
         this.props.close();
+||||||| b64a507697381fd7bb205f4a2b2217322d31811a
+        this.pos.setSelectedCategoryId(0);
+        this.pos.searchProductWord = productName + ";product_tmpl_id:" + this.props.product.product_tmpl_id;
+        this.cancel();
+=======
+        this.pos.setSelectedCategoryId(0);
+        this.pos.searchProductWord = productName;
+        this.cancel();
+>>>>>>> 113bedef2cb2559412539c0a1ffb8f1f147e44ce
     }
     _hasMarginsCostsAccessRights() {
         const isAccessibleToEveryUser = this.pos.config.is_margins_costs_accessible_to_every_user;
