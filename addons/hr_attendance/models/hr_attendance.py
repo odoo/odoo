f70206c4ad5fa64b3f7e294e198b92de63a9517d
Yash Vaishnav <yava@odoo.com>
2024-08-09 12:13:07 +0000

    [FIX] hr_attendance: fix the 12 hour clock on the attendance
    Before this commit, if you changed the time format in the language,
    the Gantt and the form view of the attendance did not follow those formats.
    
    With this commit, the dates displayed in the Gantt and form view are
    based on the time format set in the language.
    
    Task-4098672
    
    closes odoo/odoo#184007
    
    X-original-commit: 3449286e3269440bb8668d6686b681081a455d17
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/hr_attendance/models/hr_attendance.py
+++ addons/hr_attendance/models/hr_attendance.py
@@ -10,13 +10,13 @@ from operator import itemgetter
 from pytz import timezone
 from random import randint
 
+from odoo.http import request
 from odoo import models, fields, api, exceptions, _
 from odoo.addons.resource.models.utils import Intervals
-from odoo.tools import format_datetime
 from odoo.osv.expression import AND, OR
 from odoo.tools.float_utils import float_is_zero
-from odoo.exceptions import AccessDenied, AccessError
-from odoo.tools import convert, format_duration
+from odoo.exceptions import AccessError
+from odoo.tools import convert, format_duration, format_time, format_datetime
 
 def get_google_maps_url(latitude, longitude):
     return "https://maps.google.com?q=%s,%s" % (latitude, longitude)
@@ -174,18 +174,19 @@ class HrAttendance(models.Model):
 
     @api.depends('employee_id', 'check_in', 'check_out')
     def _compute_display_name(self):
+        tz = request.httprequest.cookies.get('tz') if request else None
         for attendance in self:
             if not attendance.check_out:
                 attendance.display_name = _(
                     "From %s",
-                    format_datetime(self.env, attendance.check_in, dt_format="HH:mm"),
+                    format_time(self.env, attendance.check_in, time_format=None, tz=tz, lang_code=self.env.lang),
                 )
             else:
                 attendance.display_name = _(
                     "%(worked_hours)s (%(check_in)s-%(check_out)s)",
                     worked_hours=format_duration(attendance.worked_hours),
-                    check_in=format_datetime(self.env, attendance.check_in, dt_format="HH:mm"),
-                    check_out=format_datetime(self.env, attendance.check_out, dt_format="HH:mm"),
+                    check_in=format_time(self.env, attendance.check_in, time_format=None, tz=tz, lang_code=self.env.lang),
+                    check_out=format_time(self.env, attendance.check_out, time_format=None, tz=tz, lang_code=self.env.lang),
                 )
 
     def _get_employee_calendar(self):
