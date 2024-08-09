c4da2600f3b926c1eef8352b98d2c671e88a6301
roen-odoo <roen@odoo.com>
2024-08-09 11:36:13 +0000

    [FIX] pos_sale: ship later disabled by default
    When settling a sale order in the PoS the shiplater option was always
    turned on.
    
    Steps to reproduce:
    -------------------
    * Activate ship later option in PoS config
    * Create a sale order and confirm it
    * Open the PoS
    * Settle the order
    * Click on pay
    > Observation: The ship later button is already activated
    
    Why the fix:
    ------------
    The button activation is based on the presence or not of a delivery date
    to fix it we just make sure not to import the delivery date from the
    original sale order.
    
    opw-3987515
    
    X-original-commit: a963652dbdb8cc9103b44999bf6e075979fb771d

--- addons/point_of_sale/static/tests/tours/utils/payment_screen_util.js
+++ addons/point_of_sale/static/tests/tours/utils/payment_screen_util.js
@@ -363,10 +363,8 @@ export function clickCustomer(name) {
 }
 
 export function shippingLaterHighlighted() {
-    return [
-        {
-            content: "Shipping later button is highlighted",
-            trigger: ".button:contains('Ship Later').highlight",
-        },
-    ];
+    return {
+        content: "Shipping later button is highlighted",
+        trigger: ".button:contains('Ship Later').highlight",
+    };
 }
