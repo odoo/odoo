26468564ed5f1f4d62545a49aa13054a4000edad
Brieuc-brd <brd@odoo.com>
2024-08-12 13:10:21 +0200

    [IMP] website: introduce `s_images_mosaic` snippet
    This commit adds the new `s_images_mosaic` snippet.
    
    task-4105446
    Part of task-4077427

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -80,6 +80,7 @@
         'views/snippets/s_accordion.xml',
         'views/snippets/s_numbers_showcase.xml',
         'views/snippets/s_accordion_image.xml',
+        'views/snippets/s_images_mosaic.xml',
         'views/snippets/s_text_highlight.xml',
         'views/snippets/s_progress_bar.xml',
         'views/snippets/s_blockquote.xml',
