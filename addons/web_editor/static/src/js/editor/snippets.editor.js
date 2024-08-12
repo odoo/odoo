fd2fcc957d2b7b5b15a6770b1d8c5031ab03d351
Benoit Socias <bso@odoo.com>
2024-08-12 14:49:58 +0000

    [FIX] web_editor: hide tags option on blogs (keep on posts)
    When a blog cover is edited, a tag option is displayed while there is no
    `tag_ids` inside the `blog.blog` model because the
    `o_wblog_post_page_cover` appears on both the `blog.blog` covers and on
    the `blog.post` covers.
    
    This commit hides the blog tags option if the model of the cover is
    not `blog.post`.
    
    Steps to reproduce:
    - Go to blogs.
    - Select the "Travel" blog.
    - Edit.
    - Select the blog's cover.
    
    => A tag option could be specified.
    
    opw-4107748
    
    closes odoo/odoo#177178
    
    X-original-commit: 3f29e94aed5fe35fc516aff8c8414abb468ed4ba
    Signed-off-by: Arthur Detroux (ard) <ard@odoo.com>
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -3030,6 +3030,14 @@ var SnippetsMenu = Widget.extend({
         this._patchForComputeSnippetTemplates($html);
         var $scroll = $html.siblings('#o_scroll');
 
+        // TODO adapt in master. This patches the BlogPostTagSelection option
+        // in stable versions. Done here to avoid converting the html back to
+        // a string.
+        const optionEl = $html.find('[data-js="BlogPostTagSelection"][data-selector=".o_wblog_post_page_cover"]')[0];
+        if (optionEl) {
+            optionEl.dataset.selector = '.o_wblog_post_page_cover[data-res-model="blog.post"]';
+        }
+
         this.templateOptions = [];
         var selectors = [];
         var $styles = $html.find('[data-selector]');
