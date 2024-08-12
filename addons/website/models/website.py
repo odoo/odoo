091132255bc4fd785ce85c1bca041e80ae092050
Brieuc-brd <brd@odoo.com>
2024-08-12 09:46:02 +0200

    [IMP] website: introduce `s_pricelist_boxed` snippet
    This commit adds the new `s_pricelist_boxed` snippet.
    It also introduces new shared options available for all snippets with
    price list content.
    
    task-4094410
    Part of task-4077427
    
    Part-of: odoo/odoo#176145
    Related: odoo/design-themes#858
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -852,6 +852,8 @@ class Website(models.Model):
             fallback_create_missing_industry_image('s_text_cover_default_image', 's_cover_default_image')
             fallback_create_missing_industry_image('s_showcase_default_image', 's_image_text_default_image')
             fallback_create_missing_industry_image('s_accordion_image_default_image', 's_image_text_default_image')
+            fallback_create_missing_industry_image('s_pricelist_boxed_default_background', 's_product_catalog_default_image')
+
         except Exception:
             pass
 
