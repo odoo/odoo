fa09f9560b64dea77a09f7e6db53bbe14af8c97a
Brieuc-brd <brd@odoo.com>
2024-08-13 17:24:58 +0200

    [IMP] website: introduce `s_key_images` snippet
    This commit adds the new `s_key_images` snippet.
    
    task-4105059
    Part of task-4077427
    
    closes odoo/odoo#177463
    
    Related: odoo/design-themes#878
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -856,6 +856,10 @@ class Website(models.Model):
             fallback_create_missing_industry_image('s_accordion_image_default_image', 's_image_text_default_image')
             fallback_create_missing_industry_image('s_pricelist_boxed_default_background', 's_product_catalog_default_image')
             fallback_create_missing_industry_image('s_image_title_default_image', 's_cover_default_image')
+            fallback_create_missing_industry_image('s_key_images_default_image_1', 's_media_list_default_image_1')
+            fallback_create_missing_industry_image('s_key_images_default_image_2', 's_image_text_default_image')
+            fallback_create_missing_industry_image('s_key_images_default_image_3', 's_media_list_default_image_2')
+            fallback_create_missing_industry_image('s_key_images_default_image_4', 's_text_image_default_image')
 
         except Exception:
             pass
