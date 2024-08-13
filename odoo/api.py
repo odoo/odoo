acbaba305963082ed195fa96604c71edcabc002f
Thomas Lefebvre (thle) <thle@odoo.com>
2024-08-13 17:45:32 +0200

    [FIX] api: move assert cursor
    It is necessary to check that the cursor used to create
    the instance of the environment is an instance of `BaseCursor`
    directly at the start of the method (in order to be sure to
    check it before its first use).
    
    task-4268673

--- odoo/api.py
+++ odoo/api.py
@@ -491,6 +491,7 @@ class Environment(Mapping):
         self.transaction.reset()
 
     def __new__(cls, cr, uid, context, su=False, uid_origin=None):
+        assert isinstance(cr, BaseCursor)
         if uid == SUPERUSER_ID:
             su = True
 
@@ -512,7 +513,6 @@ class Environment(Mapping):
                 return env
 
         # otherwise create environment, and add it in the set
-        assert isinstance(cr, BaseCursor)
         self = object.__new__(cls)
         self.cr, self.uid, self.context, self.su = self.args = (cr, uid, frozendict(context), su)
         self.uid_origin = uid_origin
