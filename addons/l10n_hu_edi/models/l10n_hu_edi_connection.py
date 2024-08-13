7854a23053907a0f3b936ca4e644eba07e5c05a9
Antoine Dupuis (andu) <andu@odoo.com>
2024-08-13 15:11:35 +0200

    [FIX] l10n_hu_edi: fix `parse_error_response` method
    The parse_error_response method wasn't correctly parsing error results,
    leading to empty error messages.
    
    Taskid: 3985127
    Part-of: odoo/odoo#168932
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- addons/l10n_hu_edi/models/l10n_hu_edi_connection.py
+++ addons/l10n_hu_edi/models/l10n_hu_edi_connection.py
@@ -421,19 +421,13 @@ class L10nHuEdiConnection:
     # === Helpers: Response parsing === #
 
     def _parse_error_response(self, response_xml):
-        if response_xml.tag == '{http://schemas.nav.gov.hu/OSA/3.0/api}GeneralErrorResponse':
-            errors = []
+        error_code = response_xml.findtext('common:result/common:errorCode', namespaces=XML_NAMESPACES)
+        message = response_xml.findtext('common:result/common:message', namespaces=XML_NAMESPACES)
+        if error_code:
+            errors = [f'{error_code}: {message}']
             for message_xml in response_xml.iterfind('api:technicalValidationMessages', namespaces=XML_NAMESPACES):
                 message = message_xml.findtext('api:message', namespaces=XML_NAMESPACES)
                 error_code = message_xml.findtext('api:validationErrorCode', namespaces=XML_NAMESPACES)
                 errors.append(f'{error_code}: {message}')
+
             raise L10nHuEdiConnectionError(errors)
-
-        if response_xml.tag == '{http://schemas.nav.gov.hu/OSA/3.0/api}GeneralExceptionResponse':
-            message = response_xml.findtext('api:message', namespaces=XML_NAMESPACES)
-            code = response_xml.findtext('api:errorCode', namespaces=XML_NAMESPACES)
-            raise L10nHuEdiConnectionError(f'{code}: {message}')
-
-        func_code = response_xml.findtext('common:result/common:funcCode', namespaces=XML_NAMESPACES)
-        if func_code != 'OK':
-            raise L10nHuEdiConnectionError(_('NAV replied with non-OK funcCode: %s', func_code))
