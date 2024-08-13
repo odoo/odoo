a46f9bbb77f66150e8a955cf32de1a7497d461cf
Brieuc-brd <brd@odoo.com>
2024-08-13 17:24:58 +0200

    [IMP] website: introduce `s_key_images` snippet
    This commit adds the new `s_key_images` snippet.
    
    task-4105059
    Part of task-4077427

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -88,6 +88,7 @@
         'views/snippets/s_pricelist_boxed.xml',
         'views/snippets/s_adventure.xml',
         'views/snippets/s_image_title.xml',
+        'views/snippets/s_key_images.xml',
         'views/snippets/s_text_highlight.xml',
         'views/snippets/s_progress_bar.xml',
         'views/snippets/s_blockquote.xml',
