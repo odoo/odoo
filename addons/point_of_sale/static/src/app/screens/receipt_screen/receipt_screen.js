3a110f7276c58d634bd9ffe25ed7e88f7c85cb26
Adrien Guilliams (adgu) <adgu@odoo.com>
2024-08-13 13:20:29 +0000

    [FIX] point_of_sale: fix resume order condition
    The condition for showing the resume order button on the receipt_screen
    was not working properly. The condition was checking if there was 2 or
    more open orders but we want to show it if there is 1 or more open
    orders.
    
    closes odoo/odoo#176911
    
    X-original-commit: 1937e3912e885000b557761db8fbcfd3fa322f18
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/screens/receipt_screen/receipt_screen.js
+++ addons/point_of_sale/static/src/app/screens/receipt_screen/receipt_screen.js
@@ -91,7 +91,7 @@ export class ReceiptScreen extends Component {
         this.pos.showScreen(name, props);
     }
     isResumeVisible() {
-        return this.pos.get_open_orders().length > 1;
+        return this.pos.get_open_orders().length > 0;
     }
     async _sendReceiptToCustomer({ action }) {
         const order = this.currentOrder;
