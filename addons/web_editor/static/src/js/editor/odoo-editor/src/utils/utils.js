684128cda9062e569b286764c4a6d4e7c26e87ec
dhba <dhba@odoo.com>
2024-08-14 12:17:05 +0530

    [FIX] web_editor: table picker opens at wrong position
    Before this commit:
    
    When creating a table in email marketing using the powerbox command '/table' the
    table picker would open at an incorrect position.
    
    After this commit:
    
    Now, when using the '/table' powerbox command to create a table in email
    marketing, the table picker will open at the cursor position.
    
    task-4113199
    
    closes odoo/odoo#176712
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
@@ -3071,11 +3071,12 @@ export function getRangePosition(el, document, options = {}) {
         offset.left = marginLeft;
     }
 
-    if (options.parentContextRect) {
-        offset.left += options.parentContextRect.left;
-        offset.top += options.parentContextRect.top;
+    if (options.getContextFromParentRect) {
+        const parentContextRect = options.getContextFromParentRect();
+        offset.left += parentContextRect.left;
+        offset.top += parentContextRect.top;
         if (isRtl) {
-            offset.right += options.parentContextRect.left;
+            offset.right += parentContextRect.left;
         }
     }
 
