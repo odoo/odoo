cd2f4a33abf51c3c25328130947cf10809896984
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2024-08-14 12:07:49 +0200

    [FIX] account: reverse moves on payslip cancel
    Create a new payslip
    Select Employee and contract
    Compute Sheet
    Create Draft Entry > Post Draft Entry
    Cancel payslip
    
    Issue: The system will attempt to reset to draft and unlink the
    associated invoice
    In some cases this is not ideal: In Indian government for example, requires
    audit trail report for private limited companies, so user can't delete journal
    entries after posting once.
    
    Backporting method `_unlink_or_reverse` from 17.0 [1] to be used in the
    related PR https://github.com/odoo/enterprise/pull/68044
    
    [1] https://github.com/odoo/odoo/blob/66bfab54748a56072ed9ff144f4a39366b606a06/addons/account/models/account_move.py#L3772-L3785
    
    opw-4075244
    
    closes odoo/odoo#176792
    
    Related: odoo/enterprise#68044
    Signed-off-by: Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -3457,6 +3457,25 @@ class AccountMove(models.Model):
 
         return reverse_moves
 
+    def _can_be_unlinked(self):
+        self.ensure_one()
+        lock_date = self.company_id._get_user_fiscal_lock_date()
+        return not self.inalterable_hash and self.date > lock_date
+
+    def _unlink_or_reverse(self):
+        if not self:
+            return
+        to_reverse = self.env['account.move']
+        to_unlink = self.env['account.move']
+        for move in self:
+            if move._can_be_unlinked():
+                to_unlink += move
+            else:
+                to_reverse += move
+        to_unlink.filtered(lambda m: m.state in ('posted', 'cancel')).button_draft()
+        to_unlink.filtered(lambda m: m.state == 'draft').unlink()
+        return to_reverse._reverse_moves(cancel=True)
+
     def _post(self, soft=True):
         """Post/Validate the documents.
 
