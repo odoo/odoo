ca5c45d5bb63e048948fba64981a748c5bc486f9
Ali Hassan (alhy) <alhy@odoo.com>
2024-08-14 11:13:17 +0200

    [IMP] point_of_sale, pos_sms: send receipt by email/phone new design
    improve the send receipt screen by introducing two fields:
    - one for the email with a send by email button
    - one for the phone number with two buttons one for sending by SMS and one for sending by whatsapp
    
    task-4197852
    
    closes odoo/odoo#177343
    
    Related: odoo/enterprise#68667
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>

--- addons/pos_sms/static/src/overrides/receipt_screen.js
+++ addons/pos_sms/static/src/overrides/receipt_screen.js
@@ -1,39 +1,18 @@
 import { patch } from "@web/core/utils/patch";
 import { ReceiptScreen } from "@point_of_sale/app/screens/receipt_screen/receipt_screen";
-import { _t } from "@web/core/l10n/translation";
 
 patch(ReceiptScreen.prototype, {
     setup() {
         super.setup(...arguments);
-        if (this.pos.config.module_pos_sms) {
-            this.state.input ||= this.currentOrder.get_partner()?.mobile || "";
-        }
     },
-    isValidPhoneNumber(x) {
-        return x && /^\+?[()\d\s-.]{8,18}$/.test(x);
+    showPhoneInput() {
+        return super.showPhoneInput() || this.pos.config.module_pos_sms;
     },
-    actionSendReceipt() {
-        if (this.state.mode === "phone" && this.isValidPhoneNumber(this.state.input)) {
-            this.sendReceipt.call({ action: "action_sent_message_on_sms", name: "SMS" });
-        } else if (this.state.mode === "phone") {
-            this.notification.add(_t("Please enter a valid phone number"), {
-                type: "danger",
-            });
-        } else {
-            super.actionSendReceipt(...arguments);
-        }
-    },
-    changeMode(mode) {
-        if (mode !== "phone" || !this.pos.config.module_pos_sms) {
-            return super.changeMode(mode);
-        }
-
-        this.state.mode = mode;
-        this.state.input = this.currentOrder.partner_id?.phone || this.state.input || "";
-    },
-    get isValidInput() {
-        return this.state.mode === "phone"
-            ? this.isValidPhoneNumber(this.state.input)
-            : super.isValidInput;
+    actionSendReceiptOnSMS() {
+        this.sendReceipt.call({
+            action: "action_sent_message_on_sms",
+            destination: this.state.phone,
+            name: "SMS",
+        });
     },
 });
