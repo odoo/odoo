aeacd7833f925c10a77725a000c2ee6ece09c1a4
tong-odoo <tong@odoo.com>
2024-08-15 10:24:42 +0000

    [FIX] web: MultiRecordSelector should pass domain to SelectCreateDialog
    When using MultiRecordSelector with domain, the domain will not automatically
    apply to SelectCreateDialog.
    
    See odoo/enterprise#68484
    
    closes odoo/odoo#176850
    
    closes odoo/odoo#191271
    
    X-original-commit: dd1dfbc1f2075c8f4b414fc6fe5287f6e0bf76c3
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/web/static/tests/legacy/core/record_selectors/record_selector_tests.js
+++ addons/web/static/tests/legacy/core/record_selectors/record_selector_tests.js
@@ -9,30 +9,22 @@ import { hotkeyService } from "@web/core/hotkeys/hotkey_service";
 import { Component, useState, xml } from "@odoo/owl";
 import { nameService } from "@web/core/name_service";
 import { dialogService } from "@web/core/dialog/dialog_service";
+import { MainComponentsContainer } from "@web/core/main_components_container";
+import { viewService } from "@web/views/view_service";
+import { popoverService } from "@web/core/popover/popover_service";
 
 QUnit.module("Web Components", (hooks) => {
     QUnit.module("RecordSelector");
 
     let target;
-    const serverData = {
-        models: {
-            partner: {
-                fields: {
-                    display_name: { string: "Display name", type: "char" },
-                },
-                records: [
-                    { id: 1, display_name: "Alice" },
-                    { id: 2, display_name: "Bob" },
-                    { id: 3, display_name: "Charlie" },
-                ],
-            },
-        },
-    };
+    let serverData;
 
     async function makeRecordSelector(props, { mockRPC } = {}) {
         class Parent extends Component {
-            static components = { RecordSelector };
-            static template = xml`<RecordSelector t-props="recordProps" />`;
+            static components = { RecordSelector, MainComponentsContainer };
+            static template = xml`
+                <MainComponentsContainer />
+                <RecordSelector t-props="recordProps" />`;
             static props = ["*"];
             setup() {
                 this.state = useState({ resId: props.resId });
@@ -56,6 +48,20 @@ QUnit.module("Web Components", (hooks) => {
     }
 
     hooks.beforeEach(async () => {
+        serverData = {
+            models: {
+                partner: {
+                    fields: {
+                        display_name: { string: "Display name", type: "char" },
+                    },
+                    records: [
+                        { id: 1, display_name: "Alice" },
+                        { id: 2, display_name: "Bob" },
+                        { id: 3, display_name: "Charlie" },
+                    ],
+                },
+            },
+        };
         target = getFixture();
         registry.category("services").add("hotkey", hotkeyService);
         registry.category("services").add("dialog", dialogService);
@@ -156,5 +162,65 @@ QUnit.module("Web Components", (hooks) => {
         });
         const input = target.querySelector(".o_record_selector input");
         assert.strictEqual(input.placeholder, "Select a partner");
+    });
+
+    QUnit.test("domain is passed to search more", async (assert) => {
+        serverData.models.partner.records = [...new Array(10)].map((el, i) => {
+            return {
+                id: i + 1,
+                display_name: `a_${i + 1}`,
+            };
+        });
+        serverData.views = {
+            "partner,false,list": `<tree><field name="display_name" /></tree>`,
+            "partner,false,search": "<search />",
+        };
+
+        const fakeService = {
+            start() {},
+        };
+        registry.category("services").add("view", viewService);
+        registry.category("services").add("action", {
+            start() {
+                return { doAction: () => {} };
+            },
+        });
+        registry.category("services").add("field", fakeService);
+        registry.category("services").add("company", {
+            start() {
+                return { currentCompany: {} };
+            },
+        });
+        registry.category("services").add("notification", fakeService);
+        registry.category("services").add("popover", popoverService);
+
+        await makeRecordSelector(
+            {
+                resModel: "partner",
+                resId: false,
+                domain: [["display_name", "!=", "some name"]],
+                placeholder: "Select a partner",
+            },
+            {
+                mockRPC: (route, args) => {
+                    if (args.method === "has_group") {
+                        return true;
+                    }
+                    if (args.method === "web_search_read") {
+                        assert.step("web_search_read");
+                        assert.deepEqual(args.kwargs.domain, [
+                            "&",
+                            ["display_name", "!=", "some name"],
+                            "!",
+                            ["id", "in", []],
+                        ]);
+                    }
+                },
+            }
+        );
+        await click(target, ".o-autocomplete--input.o_input");
+        await click(target, ".o_m2o_dropdown_option a");
+        assert.containsOnce(target, ".modal .o_list_view");
+        assert.verifySteps(["web_search_read"]);
     });
 });
