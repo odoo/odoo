0d4384dfb610e271a0fa88a176715806e36b2a59
Logan Staelens <lost@odoo.com>
2024-08-16 09:29:22 +0000

    [IMP] account_reports: Enforce constraint on carryover target
    We need to add constraints on the expression label and the carryover_target from account report so that we ensure that it is used correctly.
    
    Currently, it may have happened that when we used the carryover mechanism, we were using the wrong label.
    That could create some issues where the carryover would simply not work or partially.
    
    X-original-commit: cdd2c9737b98f8cb8a588d1585c84b2aecb2924c

--- addons/account/models/account_report.py
+++ addons/account/models/account_report.py
@@ -552,8 +552,7 @@ class AccountReportExpression(models.Model):
     carryover_target = fields.Char(
         string="Carry Over To",
         help="Formula in the form line_code.expression_label. This allows setting the target of the carryover for this expression "
-             "(on a _carryover_*-labeled expression), in case it is different from the parent line. 'custom' is also allowed as value"
-             " in case the carryover destination requires more complex logic."
+             "(on a _carryover_*-labeled expression), in case it is different from the parent line."
     )
 
     _sql_constraints = [
@@ -568,6 +567,14 @@ class AccountReportExpression(models.Model):
             "The expression label must be unique per report line."
         ),
     ]
+
+    @api.constrains('carryover_target', 'label')
+    def _check_carryover_target(self):
+        for expression in self:
+            if expression.carryover_target and not expression.label.startswith('_carryover_'):
+                raise UserError(_("You cannot use the field carryover_target in an expression that does not have the label starting with _carryover_"))
+            elif expression.carryover_target and not expression.carryover_target.split('.')[1].startswith('_applied_carryover_'):
+                raise UserError(_("When targetting an expression for carryover, the label of that expression must starts with _applied_caryyover_"))
 
     @api.constrains('formula')
     def _check_domain_formula(self):
