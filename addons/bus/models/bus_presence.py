2dffb070290e31df1b14873be13dd768bfa2f002
tsm-odoo <tsm@odoo.com>
2024-08-16 13:42:21 +0200

    [FIX] bus: send missed presences to current user only
    Since [1], bus presences are only sent to interested users. Since
    notifications about presences can be missed between the subscribe
    client side and the subscribe server side, missed presences are
    sent when subscribing to new presences.
    
    However, the missed presences are broadcasted to every subscriber
    while it should only be done for the user that just subscribed.
    This PR fixes this issue.
    
    [1]: https://github.com/odoo/odoo/pull/175972

--- addons/bus/models/bus_presence.py
+++ addons/bus/models/bus_presence.py
@@ -101,7 +101,7 @@ class BusPresence(models.Model):
         self.user_id.invalidate_recordset(["im_status"])
         self.user_id.partner_id.invalidate_recordset(["im_status"])
 
-    def _send_presence(self, im_status=None):
+    def _send_presence(self, im_status=None, bus_target=None):
         """Send notification related to bus presence update.
 
         :param im_status: 'online', 'away' or 'offline'
@@ -110,10 +110,11 @@ class BusPresence(models.Model):
         for presence in self:
             identity_data = presence._get_identity_data()
             target = presence._get_bus_target()
+            target = bus_target or (target and (target, "presence"))
             if identity_data and target:
                 notifications.append(
                     (
-                        (target, "presence"),
+                        target,
                         "bus.bus/im_status_updated",
                         {"im_status": im_status or presence.status, **identity_data},
                     )
