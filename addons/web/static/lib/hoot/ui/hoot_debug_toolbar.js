f4b23542544285765561813eda6e4b77062f96f6
Julien Mougenot <jum@odoo.com>
2024-08-19 15:52:01 +0200

    [IMP] web: Hoot - show test fixture in headless
    Before this commit, "headless" mode simply replaced the UI by a blank
    div with a small label and a link to toggle off the headless mode.
    
    Just like the normal UI, this div sat on top of the text fixture, hiding
    it from the user.
    
    Since there is no point in hiding tests in headless anyway (because it's
    mostly meant to be used on the server), the headless div has been
    reduced to a small bar on the bottom of the viewport, so that the
    fixture can now be visible at all times in headless.
    
    This may also allow the RunBot to take proper screenshots of issues when
    running JS unit tests.
    
    Part-of: odoo/odoo#177744
    Related: odoo/enterprise#68827
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/web/static/lib/hoot/ui/hoot_debug_toolbar.js
+++ addons/web/static/lib/hoot/ui/hoot_debug_toolbar.js
@@ -26,57 +26,59 @@ export class HootDebugToolBar extends Component {
     };
 
     static template = xml`
-        <div class="${HootDebugToolBar.name} absolute start-0 bottom-0 p-4 z-4 max-w-full max-h-full">
-            <div class="flex flex-col overflow-hidden rounded shadow bg-gray-200 dark:bg-gray-800">
-                <div class="flex items-center gap-2 px-2">
-                    <i class="fa fa-bug text-skip" />
-                    <div class="flex gap-px rounded my-1 overflow-hidden min-w-fit">
-                        <button class="bg-btn px-2 py-1" title="Exit debug mode (Ctrl + Esc)" t-on-click="exitDebugMode">
-                            <i class="fa fa-sign-out" />
-                        </button>
-                        <t t-if="done">
-                            <button class="bg-btn px-2 py-1 animate-slide-left" title="Restart test (F5)" t-on-click="restart">
-                                <i class="fa fa-refresh" />
-                            </button>
-                        </t>
-                    </div>
-                    <button
-                        class="flex items-center gap-1 truncate"
-                        t-on-click="() => state.open = !state.open"
-                        title="Click to toggle details"
-                    >
-                        status:
-                        <strong
-                            t-attf-class="text-{{ info.className }}"
-                            t-esc="info.status"
-                        />
-                        <span class="text-muted">-</span>
-                        assertions:
-                        <span class="flex gap-1 text-pass">
-                            <strong t-esc="info.passed" />
-                            <span class="hidden sm:inline">passed</span>
-                        </span>
-                        <t t-if="info.failed">
-                            <span class="text-muted">/</span>
-                            <span class="flex gap-1 text-fail">
-                                <strong t-esc="info.failed" />
-                                <span class="hidden sm:inline">failed</span>
-                            </span>
-                        </t>
-                        <span class="text-muted">-</span>
-                        time:
-                        <span
-                            class="text-primary"
-                            t-esc="formatTime(props.test.lastResults?.duration, 'ms')"
-                        />
+        <div class="${HootDebugToolBar.name}
+            absolute start-0 bottom-0 max-w-full max-h-full
+            flex flex-col overflow-hidden m-4 z-4
+            rounded shadow bg-gray-200 dark:bg-gray-800"
+        >
+            <div class="flex items-center gap-2 px-2">
+                <i class="fa fa-bug text-skip" />
+                <div class="flex gap-px rounded my-1 overflow-hidden min-w-fit">
+                    <button class="bg-btn px-2 py-1" title="Exit debug mode (Ctrl + Esc)" t-on-click="exitDebugMode">
+                        <i class="fa fa-sign-out" />
                     </button>
+                    <t t-if="done">
+                        <button class="bg-btn px-2 py-1 animate-slide-left" title="Restart test (F5)" t-on-click="restart">
+                            <i class="fa fa-refresh" />
+                        </button>
+                    </t>
                 </div>
-                <t t-if="state.open">
-                    <HootTestResult open="true" test="props.test" t-key="done">
-                        <HootTestPath canCopy="true" full="true" test="props.test" />
-                    </HootTestResult>
-                </t>
+                <button
+                    class="flex items-center gap-1 truncate"
+                    t-on-click="() => state.open = !state.open"
+                    title="Click to toggle details"
+                >
+                    status:
+                    <strong
+                        t-attf-class="text-{{ info.className }}"
+                        t-esc="info.status"
+                    />
+                    <span class="text-muted">-</span>
+                    assertions:
+                    <span class="flex gap-1 text-pass">
+                        <strong t-esc="info.passed" />
+                        <span class="hidden sm:inline">passed</span>
+                    </span>
+                    <t t-if="info.failed">
+                        <span class="text-muted">/</span>
+                        <span class="flex gap-1 text-fail">
+                            <strong t-esc="info.failed" />
+                            <span class="hidden sm:inline">failed</span>
+                        </span>
+                    </t>
+                    <span class="text-muted">-</span>
+                    time:
+                    <span
+                        class="text-primary"
+                        t-esc="formatTime(props.test.lastResults?.duration, 'ms')"
+                    />
+                </button>
             </div>
+            <t t-if="state.open">
+                <HootTestResult open="true" test="props.test" t-key="done">
+                    <HootTestPath canCopy="true" full="true" test="props.test" />
+                </HootTestResult>
+            </t>
         </div>
     `;
 
