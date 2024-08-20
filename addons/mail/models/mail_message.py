73380c62d812b0b7b29080ae4e969ca6d0f2cbd7
Thomas Lefebvre (thle) <thle@odoo.com>
2024-08-20 11:01:27 +0200

    [FIX] mail: check access if modify model or res_id on mail message
    It is necessary to be an administrator to modify `model` or `res_id` field.
    
    task-4122628

--- addons/mail/models/mail_message.py
+++ addons/mail/models/mail_message.py
@@ -636,6 +636,8 @@ class Message(models.Model):
 
     def write(self, vals):
         record_changed = 'model' in vals or 'res_id' in vals
+        if record_changed and not self.env.is_system():
+            raise AccessError(_("Only administrators can modify 'model' and 'res_id' fields."))
         if record_changed or 'message_type' in vals:
             self._invalidate_documents()
         res = super(Message, self).write(vals)
