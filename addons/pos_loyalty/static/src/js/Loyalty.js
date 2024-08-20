aca99207bafc24494c5cb6b725b27c36383d7a2c
Zachary Hanham <zaha@odoo.com>
2024-08-20 17:16:39 -0400

    [FIX] pos_loyalty: dont count refund lines points
    Before this commit, `pointsForPrograms` would calculate based on every order line present in the POS. However, this could lead to situations where lines that shouldn't be counted do get counted. For instance, a refund line should not be counted, as it would add a negative amount to the total amount.
    
    After this commit, only non refund lines are used in `pointsForPrograms`, which are defined as lines that do not have a `refunded_orderline_id` set.
    
    opw-4080144
    
    closes odoo/odoo#177312
    
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>

--- addons/pos_loyalty/static/src/js/Loyalty.js
+++ addons/pos_loyalty/static/src/js/Loyalty.js
@@ -855,7 +855,7 @@ const PosLoyaltyOrder = (Order) => class PosLoyaltyOrder extends Order {
      */
     pointsForPrograms(programs) {
         pointsForProgramsCountedRules = {};
-        const orderLines = this.get_orderlines();
+        const orderLines = this.get_orderlines().filter((line) => !line.refunded_orderline_id);
         const linesPerRule = {};
         for (const line of orderLines) {
             const reward = line.reward_id
