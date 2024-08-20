97e859a56e9a5768131568132284688adac2a7d4
Alexandre Kühn <aku@odoo.com>
2024-08-20 14:57:14 +0200

    [FIX] mail: more visible unread message count in tab title
    Before this commit, when receiving new messages with browser tab
    out-of-focus, the tab title was visibly not changing.
    
    This happens because the tab title uses action name, and when there
    are new messages, it adds a new part "1 Message", "2 Messages", etc.
    but it is added after action name rather than before.
    
    Action name is a static info, whereas "1 Message" is a dynamic and
    important information. This should have precedence over everything
    else.
    
    The title service combines parts of title. Action has part name
    "action", while "1 Message" had part "_chat". Supposedly there was
    already intent for it being before action part, `_` prefix looks
    like an attempt to put precedence. However, this was not working
    because title service orders by object keys, and browsers order
    object keys by insert (`_chat` is after `action`)... except for
    numbers which are increasingly ordered and have precedence over
    strings.
    
    This commit fixes the issue by using a number for part name
    instead of `_chat`, so that it has precedence over action name.
    
    Also make the title blink when there are new messages while
    out-of-focus, so that it's more obvious that important messages
    and actions are required there.

--- addons/mail/static/src/core/web/out_of_focus_service_patch.js
+++ addons/mail/static/src/core/web/out_of_focus_service_patch.js
@@ -3,21 +3,43 @@ import { patch } from "@web/core/utils/patch";
 import { _t } from "@web/core/l10n/translation";
 import { sprintf } from "@web/core/utils/strings";
 
+export const UNREAD_MSG_TITLE = 100;
+
 patch(OutOfFocusService.prototype, {
     setup(env, services) {
         super.setup(env, services);
         this.titleService = services.title;
         this.counter = 0;
-        env.bus.addEventListener("window_focus", () => {
-            this.counter = 0;
-            this.titleService.setParts({ _chat: undefined });
+        env.bus.addEventListener("window_focus", () => this.clearUnreadMessage());
+        this.intervalShowing = true;
+    },
+    clearUnreadMessage() {
+        this.counter = 0;
+        this.titleService.setParts({ [UNREAD_MSG_TITLE]: undefined });
+        this.intervalShowing = true;
+        this.titlePattern = undefined;
+        clearInterval(this.newMessageInterval);
+    },
+    setInterval(func, duration) {
+        return setInterval(func, duration);
+    },
+    updateTitle() {
+        this.titleService.setParts({
+            [UNREAD_MSG_TITLE]: this.intervalShowing
+                ? sprintf(this.titlePattern, this.counter)
+                : undefined,
         });
     },
     notify() {
         super.notify(...arguments);
+        clearInterval(this.newMessageInterval);
         this.counter++;
-        const titlePattern = this.counter === 1 ? _t("%s Message") : _t("%s Messages");
-        this.titleService.setParts({ _chat: sprintf(titlePattern, this.counter) });
+        this.titlePattern = this.counter === 1 ? _t("%s Message") : _t("%s Messages");
+        this.updateTitle();
+        this.newMessageInterval = this.setInterval(() => {
+            this.updateTitle();
+            this.intervalShowing = !this.intervalShowing;
+        }, 1_000);
     },
 });
 outOfFocusService.dependencies = [...outOfFocusService.dependencies, "title"];
