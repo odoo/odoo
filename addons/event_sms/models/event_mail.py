7e1bfd902baf95d2477331f48c014ff51b904e73
Thibault Delavallée <tde@odoo.com>
2024-08-20 14:18:08 +0000

    [PERF] event_*: split filter from execution in event communications
    RATIONALE
    
    Currently scheduler code does several times filtering and execution
    which is not optimized. Basically there are two cases: event-based
    schedulers (before and after event) and attendee-based schedulers
    (contact each attendee specifically). We should filter once and send
    communication depending on type without having to filter again.
    
    SPECIFICATIONS
    
    Execute methods on event.mail and event.mail.registration models are
    now split into two
      * main method behaves like previously, filter schedulers / registrations
        based on scheduled date, ...
      * call private method that does the mail / sms / social /whatsapp
        sending (see enterprise modules for those two), for event-based
        and attendee-based models;
    
    It also allows to remove filtering done thrice in the code and have it
    done only once, improving performances and avoiding useless computation.
    It also eases inheritance as there is less duplicated code through
    various addons.
    
    Task-3084943: Event: Improve communication scheduler scalability
    
    X-original-commit: 3c9d6affacbb79c0cd76ec319d1eda9d15a52dc5
    Part-of: odoo/odoo#181818
    Related: odoo/enterprise#70885
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/event_sms/models/event_mail.py
+++ addons/event_sms/models/event_mail.py
@@ -27,24 +27,10 @@ class EventMailScheduler(models.Model):
         sms_schedulers = self.filtered(lambda scheduler: scheduler.template_ref and scheduler.template_ref._name == 'sms.template')
         sms_schedulers.notification_type = 'sms'
 
-    def execute(self):
-        for scheduler in self:
-            now = fields.Datetime.now()
-            if scheduler.interval_type != 'after_sub' and scheduler.notification_type == 'sms':
-                # before or after event -> one shot email
-                if scheduler.mail_done:
-                    continue
-                # Do not send SMS if the communication was scheduled before the event but the event is over
-                if scheduler.scheduled_date <= now and (scheduler.interval_type != 'before_event' or scheduler.event_id.date_end > now):
-                    scheduler.event_id.registration_ids.filtered(
-                        lambda registration: registration.state not in ('cancel', 'draft')
-                    )._message_sms_schedule_mass(
-                        template=scheduler.template_ref,
-                        mass_keep_log=True
-                    )
-                    scheduler.update({
-                        'mail_done': True,
-                        'mail_count_done': scheduler.event_id.seats_taken,
-                    })
-
-        return super(EventMailScheduler, self).execute()
+    def _execute_event_based_for_registrations(self, registrations):
+        if self.notification_type == "sms":
+            registrations._message_sms_schedule_mass(
+                template=self.template_ref,
+                mass_keep_log=True
+            )
+        return super()._execute_event_based_for_registrations(registrations)
