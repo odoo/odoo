f04bd13fc7da6a5cabbeb7d440d98ef09fe83fab
Aaron Bohy <aab@odoo.com>
2024-08-20 11:22:15 +0000

    [FIX] web: remove embedded actions animation flicker
    In a view with embedded actions (e.g. kanban view of tasks in a
    project), toggle it on s.t. it is visible, and then click on the
    kanban view switcher (basically to reload the kanban view). Before
    this commit, the embedded actions animation was played at each
    reload, causing a weird flickering.
    
    The reason is that when the view is reloaded, the ControlPanel is
    destroyed and a new one is created, with the embedded actions to
    be displayed by default (as they had been toggled beforehand).
    Before this commit, in such a situation, the Transition component
    (and the useTransition hook) played the animation.
    
    This behavior is only useful (and relevant) for a single usecase:
    Notifications. The NotificationContain uses a t-foreach to iterate
    over the notifications it has to display, and wrap them inside a
    Transition component. That means that when a notification pops, it
    must animate directly (it is always visible). For the remaining
    usecases in Odoo, we don't want to animate initially, if the element
    is visible by default.
    
    This commit introduces an option "immediate" (in the HOC and the
    hook), which is false by default. When set to true, if the element
    is initially visible, the animation is played.
    
    task~4035839
    
    closes odoo/odoo#177291
    
    X-original-commit: 3531ae2f9be9d5e3c574760ed5158cae2ffa5797
    Signed-off-by: Bruno Boi (boi) <boi@odoo.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/core/transition.test.js
+++ addons/web/static/tests/core/transition.test.js
@@ -5,7 +5,7 @@ import { mountWithCleanup, patchWithCleanup } from "@web/../tests/web_test_helpe
 import { Component, xml, useState } from "@odoo/owl";
 import { animationFrame, runAllTimers } from "@odoo/hoot-mock";
 
-test("useTransition hook", async () => {
+test("useTransition hook (default params)", async () => {
     patchWithCleanup(transitionConfig, {
         disabled: false,
     });
@@ -15,6 +15,39 @@ test("useTransition hook", async () => {
         setup() {
             this.transition = useTransition({
                 name: "test",
+                onLeave: () => expect.step("leave"),
+            });
+        }
+    }
+
+    // noMainContainer, because the await for the mount of the main container
+    // will already change the transition
+    const parent = await mountWithCleanup(Parent, { noMainContainer: true });
+
+    expect(".test.test-enter-active:not(.test-enter)").toHaveCount(1);
+    parent.transition.shouldMount = false;
+    await animationFrame();
+
+    // Leaving: -leave but not -enter-active
+    expect(".test.test-leave:not(.test-enter-active)").toHaveCount(1);
+    expect.verifySteps([]);
+    await runAllTimers();
+    expect.verifySteps(["leave"]);
+    await animationFrame();
+    expect(".test").toHaveCount(0);
+});
+
+test("useTransition hook (initially visible and immediate=true)", async () => {
+    patchWithCleanup(transitionConfig, {
+        disabled: false,
+    });
+    class Parent extends Component {
+        static template = xml`<div t-if="transition.shouldMount" t-att-class="transition.className"/>`;
+        static props = ["*"];
+        setup() {
+            this.transition = useTransition({
+                name: "test",
+                immediate: true,
                 onLeave: () => expect.step("leave"),
             });
         }
@@ -41,13 +74,47 @@ test("useTransition hook", async () => {
     expect(".test").toHaveCount(0);
 });
 
+test("useTransition hook (initially not visible)", async () => {
+    patchWithCleanup(transitionConfig, {
+        disabled: false,
+    });
+    class Parent extends Component {
+        static template = xml`<div t-if="transition.shouldMount" t-att-class="transition.className"/>`;
+        static props = ["*"];
+        setup() {
+            this.transition = useTransition({
+                name: "test",
+                initialVisibility: false,
+                onLeave: () => expect.step("leave"),
+            });
+        }
+    }
+
+    // noMainContainer, because the await for the mount of the main container
+    // will already change the transition
+    const parent = await mountWithCleanup(Parent, { noMainContainer: true });
+    expect(".test").toHaveCount(0);
+
+    parent.transition.shouldMount = true;
+    await animationFrame();
+
+    // Leaving: -leave but not -enter-active
+    expect(".test.test-enter:not(.test-enter-active)").toHaveCount(1);
+    await animationFrame();
+    // No longer has -enter class but now has -enter-active
+    expect(".test.test-enter-active:not(.test-enter)").toHaveCount(1);
+    await runAllTimers();
+    expect.verifySteps([]);
+    await animationFrame();
+});
+
 test("Transition HOC", async () => {
     patchWithCleanup(transitionConfig, {
         disabled: false,
     });
     class Parent extends Component {
         static template = xml`
-            <Transition name="'test'" visible="state.show" t-slot-scope="transition" onLeave="onLeave">
+            <Transition name="'test'" visible="state.show" immediate="true" t-slot-scope="transition" onLeave="onLeave">
                 <div t-att-class="transition.className"/>
             </Transition>
         `;
