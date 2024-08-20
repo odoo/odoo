85474ff692ef59cda6c51437da4e6b6509b36116
Alexandre Kühn <aku@odoo.com>
2024-08-20 18:51:30 +0200

    [FIX] mail: do not allow sending message while files are uploading
    Before this commit, when attempting to post a message that contains
    text and had a file uploading, this fails to send the message.
    
    Steps to reproduce:
    - Use slow network (e.g. "Slow 4g")
    - Type some text in composer
    - Copy-paste a file with CTRL-V in input
    - Send message with ENTER
    
    => The attachment is sent but the text failed to post.
    
    Even though clicking on error can attempt to resend the textual
    message, it never works because it is linked to uploading attachment
    but it doesn't exist anymore, so the message is permanently lost.
    
    There is in fact some code that should prevent posting a message
    while there are file that are still uploading. This was not working
    because typing some text was wrongfully a condition enough to not
    put alert.
    
    This commit fixes the issue by properly preventing sending message
    while the attachment is being uploading.
    
    closes odoo/odoo#177287
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/mail/static/src/core/common/composer.js
+++ addons/mail/static/src/core/common/composer.js
@@ -569,9 +569,13 @@ export class Composer extends Component {
     async processMessage(cb) {
         const el = this.ref.el;
         const attachments = this.props.composer.attachments;
-        if (
+        if (attachments.some(({ uploading }) => uploading)) {
+            this.env.services.notification.add(_t("Please wait while the file is uploading."), {
+                type: "warning",
+            });
+        } else if (
             this.props.composer.text.trim() ||
-            (attachments.length > 0 && attachments.every(({ uploading }) => !uploading)) ||
+            attachments.length > 0 ||
             (this.message && this.message.attachments.length > 0)
         ) {
             if (!this.state.active) {
@@ -585,10 +589,6 @@ export class Composer extends Component {
             this.clear();
             this.state.active = true;
             el.focus();
-        } else if (attachments.some(({ uploading }) => Boolean(uploading))) {
-            this.env.services.notification.add(_t("Please wait while the file is uploading."), {
-                type: "warning",
-            });
         }
     }
 
