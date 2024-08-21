42edbe66834841fb0033cb22e8be718ad730f838
Iván Todorovich <ivan.todorovich@gmail.com>
2024-08-21 10:24:46 -0300

    [IMP] web: customizable classes for the `remaining_days` field
    This commit adds the ability to customize the classes of the `remaining_days`
    field through the `classes` option.
    
    Usage:
    
    ```xml
    <field
        name="fname"
        widget="remaining_days"
        options="{'classes': {'success': 'days &gt;= 10'}"
    />
    ```

--- addons/web/static/src/views/fields/remaining_days/remaining_days_field.js
+++ addons/web/static/src/views/fields/remaining_days/remaining_days_field.js
@@ -1,7 +1,9 @@
 /** @odoo-module **/
 
 import { DatePicker, DateTimePicker } from "@web/core/datepicker/datepicker";
+import { evaluateExpr } from "@web/core/py_js/py";
 import { formatDate, formatDateTime } from "@web/core/l10n/dates";
+import { getClassNameFromDecoration } from "@web/views/utils";
 import { localization } from "@web/core/l10n/localization";
 import { _lt, _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
@@ -54,6 +56,19 @@ export class RemainingDaysField extends Component {
             : formatDate(this.props.value);
     }
 
+    get classNames() {
+        if (!this.props.value) {
+            return false;
+        }
+        const classNames = {};
+        const evalContext = {days: this.diffDays, record: this.props.record.evalContext};
+        for (const decoration in this.props.classes) {
+            const value = evaluateExpr(this.props.classes[decoration], evalContext);
+            classNames[getClassNameFromDecoration(decoration)] = value;
+        }
+        return classNames;
+    }
+
     onDateTimeChanged(datetime) {
         this.props.update(datetime || false);
     }
@@ -62,6 +77,19 @@ export class RemainingDaysField extends Component {
 RemainingDaysField.template = "web.RemainingDaysField";
 RemainingDaysField.props = {
     ...standardFieldProps,
+    classes: { type: Object, optional: true },
+};
+RemainingDaysField.defaultProps = {
+    classes: {
+        'bf': 'days <= 0',
+        'danger': 'days < 0',
+        'warning': 'days == 0',
+    },
+};
+RemainingDaysField.extractProps = ({ attrs }) => {
+    return {
+        classes: attrs.options.classes,
+    };
 };
 
 RemainingDaysField.displayName = _lt("Remaining Days");
