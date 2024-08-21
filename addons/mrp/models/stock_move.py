b6d6e3ccfb505d902b44bcd6625c0c8ca6fd6a5b
Ethan Vincent <etvi@odoo.com>
2024-08-21 11:29:43 +0000

    [FIX] mrp,stock: save kit bom location
    **Current behavior:**
    When a kit bom product move line is broken down into move lines
    for its component products, changes written to the move line
    (not on the move) will not carry over to the new move nor move
    lines.
    
    **Expected behavior:**
    The change should be observed beyond the decomposition.
    
    **Steps to reproduce:**
    1. Create a new internal transfer in barcode
    
    2. Add a product with some bom via form, also edit the
         destination location in the form to be something
         non-default.
    
    3. Save the form, validate the transfer
    
    4. See that the broken down move lines don't keep the changed
         destination location
    
    **Cause of the issue:**
    When the kit bom moves are exploded, the location information of
    its move lines is not taken into account at any point- and thus
    it's lost.
    
    **Fix:**
    For kit bom products, use a move line's location information
    during creation of a move as opposed to the picking. Only link a
    new move line for a kit bom product to an existing move if (in
    addition to the product) the location source and destination
    values match.
    
    opw-4016702
    
    X-original-commit: 8d6d36b88318dc3914f68fb1ced8b9d3b47e266c

--- addons/mrp/models/stock_move.py
+++ addons/mrp/models/stock_move.py
@@ -156,6 +156,28 @@ class StockMoveLine(models.Model):
 
         return aggregated_move_lines
 
+    def _prepare_stock_move_vals(self):
+        move_vals = super()._prepare_stock_move_vals()
+        if self.env['product.product'].browse(move_vals['product_id']).is_kits:
+            move_vals['location_id'] = self.location_id.id
+            move_vals['location_dest_id'] = self.location_dest_id.id
+        return move_vals
+
+    def _get_linkable_moves(self):
+        """ Don't linke move lines with kit products to moves with dissimilar locations so that
+        post `action_explode()` move lines will have accurate location data.
+        """
+        self.ensure_one()
+        if self.product_id and self.product_id.is_kits:
+            moves = self.picking_id.move_ids.filtered(lambda move:
+                move.product_id == self.product_id and
+                move.location_id == self.location_id and
+                move.location_dest_id == self.location_dest_id
+            )
+            return sorted(moves, key=lambda m: m.quantity < m.product_qty, reverse=True)
+        else:
+            return super()._get_linkable_moves()
+
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
