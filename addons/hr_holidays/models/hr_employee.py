f9dc5de60290784c042a8560285bc0c5eeb8a70d
Duy (Đỗ Anh) <doanhduyxavie@gmail.com>
2024-08-22 11:48:06 +0000

    [FIX] hr_holidays: typo
    During refactoring of hr_holidays for 17.0 (commit 8f87e10) a typo was introduced.
    
    X-original-commit: bc36f05fec522d02aba061385cd6aa31754a3641

--- addons/hr_holidays/models/hr_employee.py
+++ addons/hr_holidays/models/hr_employee.py
@@ -556,7 +556,7 @@ class HrEmployee(models.Model):
                                 'leave_id': leave.id,
                             }
                     else:
-                        if leave_unit == 'hour':
+                        if leave_unit == 'hours':
                             allocated_time = leave.number_of_hours
                         else:
                             allocated_time = leave.number_of_days
