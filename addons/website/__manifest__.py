bc4c4ffae8171250c5cf75da2f725302e0383029
Xavier Luyckx (xlu) <xlu@odoo.com>
2024-08-22 17:30:28 +0200

    [IMP] website: introduce `s_cards_soft` snippet
    This commit adds the new `s_cards_soft` snippet.
    
    task-4104941
    Part of task-4077427

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -58,6 +58,7 @@
         'views/snippets/s_alert.xml',
         'views/snippets/s_motto.xml',
         'views/snippets/s_card.xml',
+        'views/snippets/s_cards_soft.xml',
         'views/snippets/s_share.xml',
         'views/snippets/s_social_media.xml',
         'views/snippets/s_rating.xml',
