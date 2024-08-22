215818ed61586f22ac4edff7117778b3510dd5f3
Pedram (pebr) <pebr@odoo.com>
2024-08-22 15:44:18 +0200

    [FIX] point_of_sale: handle inaccessible paid orders
    Before this commit, when a user without access to certain orders
    attempted to load paid orders, the process would fail and result in an
    error. This was particularly problematic if a POS order was linked to a
    sale order that the user did not have permission to access, causing the
    entire loading process to halt.
    
    This commit addresses the issue by filtering out paid orders that the
    user cannot access.
    
    opw-4108044

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -13,7 +13,7 @@ import re
 
 from odoo import api, fields, models, tools, _
 from odoo.tools import float_is_zero, float_round, float_repr, float_compare
-from odoo.exceptions import ValidationError, UserError
+from odoo.exceptions import ValidationError, UserError, AccessError
 from odoo.osv.expression import AND
 import base64
 
@@ -1143,7 +1143,14 @@ class PosOrder(models.Model):
             `export_as_JSON` of models.Order. This is useful for back-and-forth communication
             between the pos frontend and backend.
         """
-        return self.mapped(self._export_for_ui) if self else []
+        results = []
+        for order in self:
+            try:
+                results.append(self._export_for_ui(order))
+            except AccessError:
+                # Skip the order in case of AccessError
+                continue
+        return results
 
 
 class PosOrderLine(models.Model):
