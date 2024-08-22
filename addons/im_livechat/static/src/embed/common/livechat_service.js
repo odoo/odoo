odoo/odoo

commit 82e3295d43f7809661a40d1b21ffbcbce23c700b

[IMP] mail,im_livechat: Modernization of the chat windows
Redesign of chat window management for internal users.

Folded conversations are now displayed as ordered bubbles. The
footprint is drastically reduced and browsing active chats is easier.

closes odoo/odoo#161922

Related: odoo/enterprise#65211
Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

addons/im_livechat/static/src/embed/common/livechat_service.js
@@ -119,16 +119,14 @@ export class LivechatService {
         const temporaryThread = this.thread;
         await this._createThread({ persist: true });
         if (temporaryThread) {
-            const chatWindow = this.store.discuss.chatWindows.find(
-                (c) => c.thread?.id === temporaryThread.id
-            );
+            const chatWindow = this.store.ChatWindow.get({ thread: temporaryThread });
             temporaryThread.delete();
             chatWindow.close();
         }
         if (!this.thread) {
             return;
         }
-        this.store.ChatWindow.insert({ thread: this.thread }).autofocus++;
+        this.store.chatHub.opened.add({ thread: this.thread }).autofocus++;
         await this.busService.addChannel(`mail.guest_${this.guestToken}`);
         await this.env.services["mail.store"].initialize();
         return this.thread;

commit 6a90b3c10bf95b895b831b5a9c77eed75cad5e79

[FIX] im_livechat: prevent window from closing on mobile
Current behaviour:
On mobile (or small screen), after opening a chat window,
if you interact with it, it will close.

Steps to reproduce:
1. Open odoo on a small screen
2. Click on the chat button
3. Click on an option or write something
4. The chat window will close

Cause of the issue:
Caused by: https://github.com/odoo/odoo/commit/82e3295d43f7809661a40d1b21ffbcbce23c700b
When calling persist, this.store.chatHub.opened.add is called before
chatWindow.close can finish, so when onRecompute is called,
this.opened.length is 2, and because this.maxOpened is 1 on mobile,
this.opened.pop() closes the current chat.

Fix:
Added await to chatWindow.close to make sure that chat is
closed and deleted before calling onRecompute.

opw-4088730

addons/im_livechat/static/src/embed/common/livechat_service.js
@@ -123,7 +123,7 @@ export class LivechatService {
         if (temporaryThread) {
             const chatWindow = this.store.ChatWindow.get({ thread: temporaryThread });
             temporaryThread.delete();
-            chatWindow.close();
+            await chatWindow.close();
         }
         if (!this.thread) {
             return;