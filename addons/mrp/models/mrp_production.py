38b79915573ad940fe42e0a12d46c8719ca998bf
lase@odoo.com <lase@odoo.com>
2024-08-23 14:16:35 +0000

    [FIX] mrp: update durations on workorders once the MO is done
    Steps to reproduce:
    
    - Enable workorders in the settings
    - Create a BOM for a product P with an operation: - OP1: Assembly line 1, duration 10 minutes
    - Create and confirm an MO for 5 units of P
    - Set a producing quantity of 2
    - Validate the MO and create a back order
    
    Issues:
    
    On each of the operations of the MO:
    
    1) The quantity displayed is 0
    2) The expected duration is the expected duration corresponding to the
    full initial demand e.g. 50 minutes
    
    Note: Issues 1 is present even if the produced qty match the full demand
    of the MO.
    
    Cause of the issues:
    
    1) The quantity displayed on the operations is the 'qty_remaining'
    independantly of the state of the MO. However, once the MO is done, this
    qty is set to 0 because the product have been produced.
    
    2) The expected duration is a computed field that is not updated once
    the operation is done:
    https://github.com/odoo/odoo/blob/45154cfdb044aea85ad72bb36348af7b77afd844/addons/mrp/models/mrp_workorder.py#L329-L333 However, finishing the operation will change the state to 'done' before the MO is backordered and the `qty_production` updated. In addition, the `_get_duration_expected` relied solely on the qty_production rather than also relying on the actual 'qty_produced':
    https://github.com/odoo/odoo/blob/45154cfdb044aea85ad72bb36348af7b77afd844/addons/mrp/models/mrp_workorder.py#L786
    
    Fix:
    
    1) Since we can not show different columns for each operation depending
    on their state, we rely on the state of the MO to determine which qty is
    the most relevant: the qty_remaining or the qty_produced
    
    2) We update the expected duration depending on the qty_produced in case
    it was not manually set (which can be determined by comparing its
    current value with the expected value before the workorder is set as
    done)
    
    opw-3873108
    opw-4010550
    ---
    I confirm I have signed the CLA and read the PR guidelines at www.odoo.com/submit-pr
    
    Forward-Port-Of: odoo/odoo#171771

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -1719,7 +1719,7 @@ class MrpProduction(models.Model):
                 if workorder.state not in ('done', 'cancel'):
                     workorder.duration_expected = workorder._get_duration_expected()
                 if workorder.duration == 0.0:
-                    workorder.duration = workorder.duration_expected * order.qty_producing / order.product_qty
+                    workorder.duration = workorder.duration_expected
                     workorder.duration_unit = round(workorder.duration / max(workorder.qty_produced, 1), 2)
             order._cal_price(moves_to_do_by_order[order.id])
         moves_to_finish = self.move_finished_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
@@ -1984,7 +1984,8 @@ class MrpProduction(models.Model):
             # Adapt quantities produced
             for workorder in production.workorder_ids.sorted('id'):
                 initial_workorder_remaining_qty.append(max(initial_qty - workorder.qty_reported_from_previous_wo - workorder.qty_produced, 0))
-                workorder.qty_produced = min(workorder.qty_produced, workorder.qty_production)
+                if workorder.production_id.id not in self.env.context.get('mo_ids_to_backorder', []):
+                    workorder.qty_produced = min(workorder.qty_produced, workorder.qty_production)
             workorders_len = len(production.workorder_ids)
             for index, workorder in enumerate(bo.workorder_ids):
                 remaining_qty = initial_workorder_remaining_qty[index % workorders_len]
