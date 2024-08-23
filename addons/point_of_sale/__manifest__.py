bd138ad496443025518c3338f7aa6f3e00eac9d3
vlst-odoo <vlst@odoo.com>
2024-08-23 14:20:59 +0200

    [REF] point_of_sale: refactor unit tests to hoot
    In this commit we refactor the pos unit tests from qunit to hoot.
    
    The test for the number buffer and the test that mounts the `Chrome`
    component have been removed, as they provide no added value. The same
    code is tested already in the tours.
    
    closes odoo/odoo#177754
    
    Task: 4122518
    Related: odoo/enterprise#69008
    Related: odoo/upgrade#6427
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/point_of_sale/__manifest__.py
+++ addons/point_of_sale/__manifest__.py
@@ -21,7 +21,6 @@
         'wizard/pos_close_session_wizard.xml',
         'wizard/pos_daily_sales_reports.xml',
         'views/pos_assets_index.xml',
-        'views/pos_assets_qunit.xml',
         'views/point_of_sale_report.xml',
         'views/point_of_sale_view.xml',
         'views/pos_note_view.xml',
@@ -66,20 +65,29 @@
         # correct spot.
         #
         # Files in /static/src/backend will be loaded in the backend
-        # Files in /static/src/app will be loaded in the PoS UI and unit tests
+        # Files in /static/src/app will be loaded in the PoS UI
         # Files in /static/tests/tours will be loaded in the backend in test mode
-        # Files in /static/tests/unit will be loaded in the qunit tests (/pos/ui/tests)
+        # Files in /static/tests/unit will be loaded in the unit tests
 
         # web assets
         'web.assets_backend': [
             'point_of_sale/static/src/scss/pos_dashboard.scss',
             'point_of_sale/static/src/backend/tours/point_of_sale.js',
-            'point_of_sale/static/src/backend/debug_manager.js',
             'point_of_sale/static/src/backend/pos_kanban_view/*',
             'point_of_sale/static/src/app/utils/hooks.js',
         ],
         'web.assets_tests': [
             'point_of_sale/static/tests/tours/**/*',
+        ],
+        'web.assets_unit_tests': [
+            # for the related_models.test.js
+            'point_of_sale/static/src/app/models/related_models.js',
+            # for the data_service.test.js
+            'point_of_sale/static/src/app/models/utils/indexed_db.js',
+            'point_of_sale/static/src/app/models/data_service_options.js',
+            'point_of_sale/static/src/utils.js',
+            'point_of_sale/static/src/app/models/data_service.js',
+            'point_of_sale/static/tests/unit/**/*',
         ],
 
         # PoS assets
@@ -109,7 +117,7 @@
             'bus/static/src/workers/*',
         ],
 
-        # Main PoS assets, they are loaded in the PoS UI and in the PoS unit tests
+        # Main PoS assets, they are loaded in the PoS UI
         'point_of_sale._assets_pos': [
             'web/static/src/scss/functions.scss',
 
@@ -180,73 +188,6 @@
         'point_of_sale.assets_prod': [
             ('include', 'point_of_sale._assets_pos'),
             'point_of_sale/static/src/app/main.js',
-        ],
-        # Bundle for the unit tests at /pos/ui/tests
-        'point_of_sale.assets_qunit_tests': [
-            ('include', 'point_of_sale._assets_pos'),
-            "web/static/src/core/browser/router.js",
-            "web/static/src/core/debug/**/*",
-            # dependencies of web.tests_assets (in the web tests, these come from assets_backend)
-            'web/static/tests/legacy/patch_translations.js',
-            'web/static/lib/jquery/jquery.js',
-            'web/static/src/legacy/js/**/*',
-            ('remove', 'web/static/src/legacy/js/libs/**/*'),
-            ('remove', 'web/static/src/legacy/js/public/**/*'),
-            'web/static/src/search/**/*',
-            'web/static/src/views/fields/field_tooltip.js',
-            'web/static/src/views/fields/field.js',
-            'web/static/src/views/utils.js',
-            'web/static/src/views/view_hook.js',
-            'web/static/src/views/view_service.js',
-            'web/static/src/views/view.js',
-            'web/static/src/model/relational_model/utils.js',
-            'web/static/src/webclient/actions/action_container.js',
-            'web/static/src/webclient/actions/action_dialog.js',
-            'web/static/src/webclient/actions/action_service.js',
-            'web/static/src/webclient/actions/reports/report_action.js',
-            'web/static/src/webclient/actions/reports/report_hook.js',
-            'web/static/src/webclient/menus/menu_service.js',
-            'web/static/src/webclient/navbar/navbar.js',
-            'web/static/src/webclient/webclient.js',
-            'web/static/src/views/view_dialogs/form_view_dialog.js',
-            'web/static/src/views/view_dialogs/select_create_dialog.js',
-            # BEGIN copy of web.tests_assets. We don't 'include' it because other modules add their
-            # own test helpers in this module that depend on files that they add in assets_backend
-            'web/static/lib/qunit/qunit-2.9.1.css',
-            'web/static/lib/qunit/qunit-2.9.1.js',
-            'web/static/tests/legacy/legacy_tests/helpers/**/*',
-            ('remove', 'web/static/tests/legacy/legacy_tests/helpers/test_utils_tests.js'),
-
-            'web/static/lib/fullcalendar/core/index.global.js',
-            'web/static/lib/fullcalendar/interaction/index.global.js',
-            'web/static/lib/fullcalendar/daygrid/index.global.js',
-            'web/static/lib/fullcalendar/timegrid/index.global.js',
-            'web/static/lib/fullcalendar/list/index.global.js',
-            'web/static/lib/fullcalendar/luxon3/index.global.js',
-
-            'web/static/lib/zxing-library/zxing-library.js',
-
-            'web/static/lib/ace/ace.js',
-            'web/static/lib/ace/mode-python.js',
-            'web/static/lib/ace/mode-xml.js',
-            'web/static/lib/ace/mode-javascript.js',
-            'web/static/lib/ace/mode-qweb.js',
-            'web/static/lib/stacktracejs/stacktrace.js',
-            ('include', "web.chartjs_lib"),
-
-            # 'web/static/tests/legacy/main_tests.js',
-            'web/static/tests/legacy/helpers/**/*.js',
-            'web/static/tests/legacy/views/helpers.js',
-            'web/static/tests/legacy/search/helpers.js',
-            'web/static/tests/legacy/views/calendar/helpers.js',
-            'web/static/tests/legacy/webclient/**/helpers.js',
-            'web/static/tests/legacy/qunit.js',
-            'web/static/tests/legacy/main.js',
-            'web/static/tests/legacy/setup.js',
-
-            ## END copy of web.tests_assets
-            # pos unit tests
-            'point_of_sale/static/tests/unit/**/*',
         ],
         'point_of_sale.customer_display_assets': [
             ('include', 'point_of_sale.base_app'),
