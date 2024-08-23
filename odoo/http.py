2044dd36e64a13a7ab43c46d3acd4559933febff
Julien Castiaux <juc@odoo.com>
2024-08-23 11:09:46 +0200

    [IMP] core: sanitize request cookies
    Changing the format of a data stored inside a cookie makes is dangerous
    as there always is a possibility that someone has a valid session with
    that cookie but with the previous format. Ideally in all places where we
    use that cookie, we should test for all formats.
    
    In this work we propose a more definitive solution to this problem.
    Whenever one is going to use `request.cookies` we run code to sanitize
    the existing cookies to converge all possible data format to a single
    one.
    
    This way the applications can not bother about what format their data is
    and always process a single unified format.
    
    Part-of: odoo/odoo#177723
    Related: odoo/enterprise#68837
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- odoo/http.py
+++ odoo/http.py
@@ -1576,6 +1576,13 @@ class Request:
         except (ValueError, KeyError):
             return None
 
+    @lazy_property
+    def cookies(self):
+        cookies = werkzeug.datastructures.MultiDict(self.httprequest.cookies)
+        if self.registry:
+            self.registry['ir.http']._sanitize_cookies(cookies)
+        return werkzeug.datastructures.ImmutableMultiDict(cookies)
+
     # =====================================================
     # Helpers
     # =====================================================
