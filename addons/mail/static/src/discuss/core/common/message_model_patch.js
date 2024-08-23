878e7bea84b3f92fd4962d33366176c477610202
zel-odoo <zel@odoo.com>
2024-08-23 12:43:09 +0200

    [FIX] mail: channel mentions are lost when editing When editing a message, the mentions of the original message were lost.
    The reason is that the channel mentions were not stored with the message
    in the database.
    
    The fix is to parse the body directly before editing and get
    mentionedChannels from the it.
    
    follow-up of task-4104895

---
+++ addons/mail/static/src/discuss/core/common/message_model_patch.js
@@ -0,0 +1,9 @@
+/* @odoo-module */
+
+import { Message } from "@mail/core/common/message_model";
+import { patch } from "@web/core/utils/patch";
+
+patch(Message.prototype, {
+    /** @type {Promise[]} */
+    mentionedChannelPromises: [],
+});
