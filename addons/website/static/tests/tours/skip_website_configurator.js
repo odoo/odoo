4dda2c42845ce879972c6d41e553cdc27a32adc9
Louis (loco) <loco@odoo.com>
2024-08-26 17:14:25 +0200

    [FIX] website: adapt the automatic editor on new website test
    The goal of this commit is to simplify the
    `automatic_editor_on_new_website` test. Indeed, the test was added by
    [1] to check if the editor was correctly started after the creation of a
    website under some circonstences but since [2], the editor is not
    started after creating a website.
    The part that tests the installation of a new language through the theme
    editor has been removed as it is already implemented in the
    `rte_translator` test. The remaining part that tests the creation of a
    website with the skip of the configurator is kept as it is not
    implemented in another test.
    
    [1]: https://github.com/odoo/odoo/commit/81f5c768278d15d4ef943deffa397736b867c825
    [2]: https://github.com/odoo/odoo/commit/e38808e1a77dab291beb4020a2bf3c248f88cdef
    
    runbot-60056
    runbot-69009
    
    closes odoo/odoo#178529
    
    X-original-commit: https://github.com/odoo/odoo/commit/0224f6f4e1764a5891061974118084fd052a8c89
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>

--- addons/website/static/tests/tours/skip_website_configurator.js
+++ addons/website/static/tests/tours/skip_website_configurator.js
@@ -1,55 +1,11 @@
 /** @odoo-module **/
 
-import wTourUtils from "@website/js/tours/tour_utils";
+import { registry } from "@web/core/registry";
 
-wTourUtils.registerWebsitePreviewTour('automatic_editor_on_new_website', {
+registry.category("web_tour.tours").add('skip_website_configurator', {
     test: true,
-    edition: true,
-    url: '/',
-},
-() => [
-    wTourUtils.goToTheme(),
-    {
-        content: "click on Add a language",
-        trigger: "we-button[data-add-language]"
-    },
-    {
-        content: "confirm leave editor",
-        trigger: ".modal-dialog button.btn-primary"
-    },
-    {
-        content: "type Parseltongue",
-        trigger: 'div[name="lang_ids"] .o_input_dropdown input',
-        run: 'text Parseltongue',
-    },
-    {
-        content: 'select Parseltongue',
-        trigger: '.dropdown-item:contains(Parseltongue)',
-        in_modal: false,
-    },
-    {
-        content: "load parseltongue",
-        extra_trigger: '.modal div[name="lang_ids"] .rounded-pill .o_tag_badge_text:contains(Parseltongue)',
-        trigger: '.modal-footer button[name=lang_install]',
-    },
-    {
-        content: "Select the language dropdown",
-        trigger: ':iframe .js_language_selector .dropdown-toggle',
-    },
-    {
-        content: "Select parseltongue",
-        trigger: ':iframe a.js_change_lang[data-url_code=pa_GB]',
-    },
-    {
-        content: "Check that we're on parseltongue and then go to settings",
-        trigger: ':iframe html[lang=pa-GB]',
-        run: () => {
-            // Now go through the settings for a new website. A frontend_lang
-            // cookie was set during previous steps. It should not be used when
-            // redirecting to the frontend in the following steps.
-            window.location.href = '/web#action=website.action_website_configuration';
-        }
-    },
+    url: '/web#action=website.action_website_configuration',
+    steps: () => [
     {
         content: "create a new website",
         trigger: 'button[name="action_website_create_new"]',
@@ -89,4 +45,4 @@ wTourUtils.registerWebsitePreviewTour('automatic_editor_on_new_website', {
         timeout: 30000,
         isCheck: true,
     },
-]);
+]});
