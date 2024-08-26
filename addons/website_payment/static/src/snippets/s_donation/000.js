odoo/odoo

commit fd5760d37e73b6e88916753a2a59b9e1f7b9b62b

[IMP] *: always name gettext placeholders when there are more than one
The Problem
===========
Translating text in language sometimes involves reordering words.
Positional arguments are always inserted in the same order, which order
matches English syntax, but this isn't necessarily appropriate for every
syntax of every language in the world/

Here's an eloquent example of the problem, taken from the GNU gettext
documentation:
```c
printf(gettext("String '%s' has %d characters\n"), s, strlen(s));
```
which one could translate in German as follows:
```
"%d Zeichen lang ist die Zeichenkette `%s'"
```
Even without knowing any of German, you could notice that the positions
of %d and %s have been swapped; yet printf would still insert its
arguments in the same order.

That's the reason why, whenever there is more than one "format
specifier" (%s), you should always use keyworded arguments so that the
translators can freely reorder the words without any problems.

The Fix
=======
This commit fixes the problem by providing a name to the placeholders of
every call to gettext with more than one placeholders.

It also adds a linter check to prevent people from repeating the
mistake.

Part of task-3869533

closes odoo/odoo#166014

Related: odoo/enterprise#62769
Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

addons/website_payment/static/src/snippets/s_donation/000.js
@@ -1,5 +1,4 @@
-/** @odoo-module **/
-
+import { formatCurrency } from "@web/core/currency";
 import { _t } from "@web/core/l10n/translation";
 import publicWidget from '@web/legacy/js/public/public_widget';
 import { rpc } from "@web/core/network/rpc";
@@ -137,9 +136,12 @@ publicWidget.registry.DonationSnippet = publicWidget.Widget.extend({
                 if (!amount) {
                     errorMessage = _t("Please select or enter an amount");
                 } else if (amount < parseFloat(minAmount)) {
-                    const before = this.currency.position === "before" ? this.currency.symbol : "";
-                    const after = this.currency.position === "after" ? this.currency.symbol : "";
-                    errorMessage = _t("The minimum donation amount is %s%s%s", before, minAmount, after);
+                    errorMessage = _t(
+                        "The minimum donation amount is %(amount)s",
+                        {
+                            amount: formatCurrency(minAmount, this.currency.id),
+                        }
+                    );
                 }
                 if (errorMessage) {
                     $(ev.currentTarget).before($('<p>', {

addons/website/static/src/snippets/s_website_form/000.js
@@ -288,9 +288,8 @@ import wUtils from '@website/js/utils';
                             this.fileInputError.limit
                         )
                         : _t(
-                            "Please fill in the form correctly. The file \"%s\" is too big. (Maximum %s MB)", 
-                            this.fileInputError.fileName,
-                            this.fileInputError.limit
+                            "Please fill in the form correctly. The file “%(file name)s” is too large. (Maximum %(max)s MB)", 
+                            { "file name": this.fileInputError.fileName, max:this.fileInputError.limit }
                         );
                     this.update_status("error", errorMessage);
                     delete this.fileInputError;

commit 5ba9c73bcf1f3f342f54b33cd2f5ff83f6bfc34d

[FIX] website_payment: prevent crash when amount < min amount
Starting from version 17.4, setting a minimum donation amount causes
the app to crash if the selected amount is below this minimum. The
issue arises due to the use of `formatCurrency`, introduced in
commit fd5760d37e73b6e88916753a2a59b9e1f7b9b62b, which requires a
number as its parameter. However, `minAmount` was being passed as
a string, leading to the crash.

This commit ensures that `minAmount` is correctly parsed as a number
before being used in `formatCurrency`.

Steps to Reproduce:
1. Set a minimum donation amount.
2. Attempt to donate an amount below the minimum.
3. Observe the app crash due to a TypeError.

task-4134736

closes odoo/odoo#177804

X-original-commit: 2917be42dcc8497777c89cad2e8f3ee374b3becb
Signed-off-by: Benoit Socias (bso) <bso@odoo.com>
Signed-off-by: Alessandro Caldonazzi (alca) <alca@odoo.com>

addons/website_payment/static/src/snippets/s_donation/000.js
@@ -132,10 +132,10 @@ publicWidget.registry.DonationSnippet = publicWidget.Widget.extend({
             } else if ($buttons.length) {
                 amount = parseFloat(this.$('#s_donation_amount_input').val());
                 let errorMessage = '';
-                const minAmount = this.el.dataset.minimumAmount;
+                const minAmount = parseFloat(this.el.dataset.minimumAmount);
                 if (!amount) {
                     errorMessage = _t("Please select or enter an amount");
-                } else if (amount < parseFloat(minAmount)) {
+                } else if (amount < minAmount) {
                     errorMessage = _t(
                         "The minimum donation amount is %(amount)s",
                         {