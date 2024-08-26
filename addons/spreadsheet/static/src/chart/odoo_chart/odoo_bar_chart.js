fe8381095aef8b8fd1c57ad81dec1a6fb1af8afd
dhrp-odoo <dhrp@odoo.com>
2024-08-26 04:07:33 +0000

    [FIX] spreadsheet: odoo chart legend labels color
    With the upgrade to Chart.js version 4.3, the configuration for setting
    legend label colors has changed. The fontColor key is now deprecated,
    and the color key must be used instead. This update was previously
    overlooked in the Odoo chart plugin.
    
    This commit resolves the issue by updating the Odoo chart plugin to
    correctly use the color key for setting legend label colors.
    
    Task: 4111089
    X-original-commit: 5d690539ddcb56ecef4a45ad7eaa28c3f0eb0019

--- addons/spreadsheet/static/src/chart/odoo_chart/odoo_bar_chart.js
+++ addons/spreadsheet/static/src/chart/odoo_chart/odoo_bar_chart.js
@@ -63,13 +63,13 @@ function createOdooChartRuntime(chart, getters) {
 }
 
 function getBarConfiguration(chart, labels, locale) {
-    const fontColor = chartFontColor(chart.background);
-    const config = getDefaultChartJsRuntime(chart, labels, fontColor, { locale });
+    const color = chartFontColor(chart.background);
+    const config = getDefaultChartJsRuntime(chart, labels, color, { locale });
     config.type = chart.type.replace("odoo_", "");
     const legend = {
         ...config.options.legend,
         display: chart.legendPosition !== "none",
-        labels: { fontColor },
+        labels: { color },
     };
     legend.position = chart.legendPosition;
     config.options.plugins = config.options.plugins || {};
@@ -85,15 +85,13 @@ function getBarConfiguration(chart, labels, locale) {
                 minRotation: 15,
                 padding: 5,
                 labelOffset: 2,
-                color: fontColor,
+                color,
             },
             title: getChartAxisTitleRuntime(chart.axesDesign?.x),
         },
         y: {
             position: chart.verticalAxisPosition,
-            ticks: {
-                color: fontColor,
-            },
+            ticks: { color },
             beginAtZero: true, // the origin of the y axis is always zero
             title: getChartAxisTitleRuntime(chart.axesDesign?.y),
         },
