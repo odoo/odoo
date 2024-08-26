9e55cd8138d475c6639fba18202213eea2553458
TSO <tso@odoo.com>
2024-08-26 07:18:26 +0200

    [IMP] update SFU bundle to v1.3.1
    closes odoo/odoo#177855
    
    See: https://github.com/odoo/sfu/compare/v1.2.0...v1.3.1
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/lib/odoo_sfu/odoo_sfu.js
+++ addons/mail/static/lib/odoo_sfu/odoo_sfu.js
@@ -15691,6 +15691,8 @@ const SERVER_REQUEST = {
 };
 
 const SERVER_MESSAGE = {
+    /** Signals that the server wants to send a message to all the other members of that channel */
+    BROADCAST: "BROADCAST",
     /** Signals the clients that one of the session in their channel has left. */
     SESSION_LEAVE: "SESSION_LEAVE",
     /**  Signals the clients that the info (talking, mute,...) of one of the session in their channel has changed. */
@@ -15707,6 +15709,8 @@ const CLIENT_REQUEST = {
 };
 
 const CLIENT_MESSAGE = {
+    /** Signals that the client wants to send a message to all the other members of that channel */
+    BROADCAST: "BROADCAST",
     /** Signals that the client wants to change how it consumes a track (like pausing or ending the download) */
     CONSUMPTION_CHANGE: "CONSUMPTION_CHANGE",
     /** Signals that the info (talking, mute,...) of this client has changed. */
@@ -15745,7 +15749,7 @@ const DEFAULT_PRODUCER_OPTIONS = {
  */
 
 /**
- * @typedef {'audio' | 'camera' | 'video' } streamType
+ * @typedef {'audio' | 'camera' | 'screen' } streamType
  */
 
 const SFU_CLIENT_STATE = Object.freeze({
@@ -15862,6 +15866,19 @@ class SfuClient extends EventTarget {
     }
 
     /**
+     * @param message any JSON serializable object
+     */
+    broadcast(message) {
+        this._bus.send(
+            {
+                name: CLIENT_MESSAGE.BROADCAST,
+                payload: message,
+            },
+            { batch: true }
+        );
+    }
+
+    /**
      * @param {string} url
      * @param {string} jsonWebToken
      * @param {Object} [options]
@@ -15918,8 +15935,7 @@ class SfuClient extends EventTarget {
      * @param {boolean} [param0.needRefresh] true if the server should refresh the local info from all sessions of this channel
      */
     updateInfo(info, { needRefresh } = {}) {
-        this._info = info;
-        this._bus.send(
+        this._bus?.send(
             {
                 name: CLIENT_MESSAGE.INFO_CHANGE,
                 payload: { info, needRefresh },
@@ -15960,7 +15976,7 @@ class SfuClient extends EventTarget {
         if (!hasChanged) {
             return;
         }
-        this._bus.send(
+        this._bus?.send(
             {
                 name: CLIENT_MESSAGE.CONSUMPTION_CHANGE,
                 payload: { sessionId, states },
@@ -16215,7 +16231,7 @@ class SfuClient extends EventTarget {
     /**
      * dispatches an event, intended for the client
      *
-     * @param { "disconnect" | "info_change" | "track" | "error"} name
+     * @param { "disconnect" | "info_change" | "track" | "error" | "broadcast"} name
      * @param [payload]
      * @fires SfuClient#update
      */
@@ -16264,6 +16280,9 @@ class SfuClient extends EventTarget {
      */
     async _handleMessage({ name, payload }) {
         switch (name) {
+            case SERVER_MESSAGE.BROADCAST:
+                this._updateClient("broadcast", payload);
+                break;
             case SERVER_MESSAGE.SESSION_LEAVE:
                 {
                     const { sessionId } = payload;
@@ -16337,7 +16356,7 @@ export { SFU_CLIENT_STATE, SfuClient };
 
 
 export const __info__ = {
-    date: '2024-05-08T07:22:00.423Z',
-    hash: '0f4f216',
+    date: '2024-08-26T05:07:14.489Z',
+    hash: 'fcd3031',
     url: 'https://github.com/odoo/sfu',
 };
