5c312d906afd7655df55f8772f84a75f20485bba
Vincent Larcin <vila@odoo.com>
2024-08-26 15:03:01 +0000

    [IMP] project: enable milestones for technical tours
    Some project tours rely on the milestones feature being enabled.
    While this is the case when demo data are installed, it's not when they aren't, and the tours fail.
    To solve this issue, and prevent future ones from being created, this commit enables the feature before each tour.
    
    Task-4132639
    
    closes odoo/odoo#182448
    
    X-original-commit: 57532442fba1fdf7996361c72c8cf22116b8f494
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/tests/test_project_ui.py
+++ addons/project/tests/test_project_ui.py
@@ -6,6 +6,11 @@ import odoo.tests
 @odoo.tests.tagged('post_install', '-at_install')
 class TestUi(odoo.tests.HttpCase):
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env['res.config.settings'].create({'group_project_milestone': True}).execute()
+
     def test_01_project_tour(self):
         self.start_tour("/odoo", 'project_tour', login="admin")
 
