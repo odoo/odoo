64307ca640080df22eab852165580aa77ef98b33
Zachary Hanham <zaha@odoo.com>
2024-08-26 17:08:13 +0000

    [FIX] barcodes_gs1_nomenclature: escape FNC1 ascii
    This commit adds ascii code 29 (FNC1) to the list of strings to be escaped from in gs1 decomposition.
    
    In PR #163404, logic was added to escape barcode scan results that start with FNC1. This is only specifically checking ']C1', but it is possible for the literal ascii code 29 to be present in the beginning of the string as well. This should also be escaped.
    
    opw-3965925
    
    closes odoo/odoo#179881
    
    X-original-commit: b5a0560639efb6659762a22c7008d2f25bba1d8e
    Signed-off-by: Steve Van Essche <svs@odoo.com>
    Signed-off-by: Zachary Hanham (zaha) <zaha@odoo.com>

--- addons/barcodes_gs1_nomenclature/tests/test_barcodes_gs1_nomenclature.py
+++ addons/barcodes_gs1_nomenclature/tests/test_barcodes_gs1_nomenclature.py
@@ -29,7 +29,7 @@ class TestBarcodeGS1Nomenclature(TransactionCase):
     def test_gs1_extanded_barcode_1(self):
         barcode_nomenclature = self.env['barcode.nomenclature'].browse(self.ref('barcodes_gs1_nomenclature.default_gs1_nomenclature'))
         # (01)94019097685457(10)33650100138(3102)002004(15)131018
-        code128 = "01940190976854571033650100138\x1D310200200415131018"
+        code128 = "\x1D01940190976854571033650100138\x1D310200200415131018"
         res = barcode_nomenclature.gs1_decompose_extanded(code128)
         self.assertEqual(len(res), 4)
         self.assertEqual(res[0]["ai"], "01")
