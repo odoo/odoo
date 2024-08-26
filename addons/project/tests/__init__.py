0b0a1b1468979ad5697b79bda7b09e96615a70f2
Jinjiu Liu <jili@odoo.com>
2024-08-26 17:27:31 +0200

    [FIX] html_editor, project: split internal link preview controller test
    Before this commit: The link preview controller test was using a task
    record to test the link_preview_name field, leading to a dependency
    issue
    
    After this commit: The testing of the link_preview_name field has been
    moved to the project module, resolving the dependency issue
    
    closes odoo/odoo#178006
    
    Signed-off-by: Florent Dardenne (dafl) <dafl@odoo.com>

--- addons/project/tests/__init__.py
+++ addons/project/tests/__init__.py
@@ -29,3 +29,4 @@ from . import test_project_report
 from . import test_project_task_quick_create
 from . import test_task_state
 from . import test_project_task_mail_tracking_duration
+from . import test_task_link_preview_name
