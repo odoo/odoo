b8bd68caed6472c6e3416bec9017aa2d23194fb4
qsm-odoo <qsm@odoo.com>
2024-08-27 11:57:25 +0000

    [FIX] web_editor: remove extra small memory leak
    No visual bug, just a small coding mistake made with [1].
    
    [1]: https://github.com/odoo/odoo/commit/e2cd6940bd29670ea0e76cd4bb95489a12588f55
    
    Related to task-3957198
    
    closes odoo/odoo#178095
    
    X-original-commit: d176cac47d76eb7f5d6ac6a60b5174956ada76cf
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2039,8 +2039,9 @@ var SnippetsMenu = Widget.extend({
                 return;
             }
             const range = selection.getRangeAt(0);
-            $(range.startContainer).closest('.o_default_snippet_text').removeClass('o_default_snippet_text');
-            alreadySelectedElements.delete(range.startContainer);
+            const $defaultTextEl = $(range.startContainer).closest('.o_default_snippet_text');
+            $defaultTextEl.removeClass('o_default_snippet_text');
+            alreadySelectedElements.delete($defaultTextEl[0]);
         });
         const refreshSnippetEditors = _.debounce(() => {
             for (const snippetEditor of this.snippetEditors) {
