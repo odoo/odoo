3a02b7cb96104b90bad6556f850312ab7763540b
qsm-odoo <qsm@odoo.com>
2024-08-27 13:15:29 +0000

    [FIX] web_editor: restore font application on default text
    Since [1], applying a font-size on some snippet default text was not
    working properly anymore. As soon as the actual text was changed, the
    features were working again.
    
    Steps to reproduce:
    - Enter website edit mode.
    - Add a "Title" snippet.
    - Triple-click on the default text.
    - Hit the font-size "Default" button.
     => Nothing happens, you cannot restore the default title size.
    
    Other steps to reproduce:
    - Enter website edit mode.
    - Add a "Title" snippet.
    - Triple-click on the default text.
    - Choose a font-size (like "8").
     => It is applied but inside the pre-existing 62px environment instead
        of replacing it, meaning the DOM looks like this:
        ```
        <font style="font-size: 62px;">
            <font style="font-size: 8px;">Title</font>
        </font>
        ```
        While it seems insignificant, this creates vertical misalignment.
        Depending on the use case, it might be problematic (e.g. a long
        paragraph instead of a title: it would have a big line spacing for
        no apparent reason). Note that this one is actually a more generic
        problem already but [1] worsened it. We might fully solve that issue
        in future versions.
    
    To fix the issue, we now ensure that applying any editor command on some
    default text makes it not-default text anymore.
    
    opw-3957198
    
    X-original-commit: 8d7fc372c0d876bb28497e58ccbbfbb59ea4ebd5

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -487,6 +487,7 @@ export class Wysiwyg extends Component {
             preHistoryUndo: () => {
                 this.destroyLinkTools();
             },
+            beforeAnyCommand: this._beforeAnyCommand.bind(this),
             commands: powerboxOptions.commands,
             categories: powerboxOptions.categories,
             plugins: options.editorPlugins,
@@ -3557,6 +3558,23 @@ export class Wysiwyg extends Component {
             el.setAttribute('src', newAttachmentSrc);
             // Also update carousel thumbnail.
             weUtils.forwardToThumbnail(el);
+        }
+    }
+
+    /**
+     * @private
+     */
+    _beforeAnyCommand() {
+        // Remove any marker of default text in the selection on which the
+        // command is being applied. Note that this needs to be done *before*
+        // the command and not after because some commands (e.g. font-size)
+        // rely on some elements not to have the class to fully work.
+        for (const node of OdooEditorLib.getSelectedNodes(this.$editable[0])) {
+            const el = node.nodeType === Node.TEXT_NODE ? node.parentElement : node;
+            const defaultTextEl = el.closest('.o_default_snippet_text');
+            if (defaultTextEl) {
+                defaultTextEl.classList.remove('o_default_snippet_text');
+            }
         }
     }
 
