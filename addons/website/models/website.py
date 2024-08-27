334aa83d1858306ad0b6ee37d47554f7677dbaef
Brieuc-brd <brd@odoo.com>
2024-08-27 09:22:20 +0200

    [IMP] website: introduce `s_website_form_overlay` snippet
    This commit adds the new `s_website_form_overlay` snippet.
    
    task-4138795
    Part of task-4077427
    
    closes odoo/odoo#183568
    
    Related: odoo/design-themes#989
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -1027,7 +1027,7 @@ class Website(models.Model):
             fallback_create_missing_industry_image('s_carousel_intro_default_image_1', 's_cover_default_image')
             fallback_create_missing_industry_image('s_carousel_intro_default_image_2', 's_image_text_default_image')
             fallback_create_missing_industry_image('s_carousel_intro_default_image_3', 's_text_image_default_image')
-
+            fallback_create_missing_industry_image('s_website_form_overlay_default_image', 's_cover_default_image')
             fallback_create_missing_industry_image('s_framed_intro_default_image', 's_cover_default_image')
             fallback_create_missing_industry_image('s_wavy_grid_default_image_1', 's_cover_default_image')
             fallback_create_missing_industry_image('s_wavy_grid_default_image_2', 's_image_text_default_image')
