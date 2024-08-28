9fcf4ae0a17773e6a77a114d3402b4a3f3314a7e
Levi Siuzdak (sile) <sile@odoo.com>
2024-08-28 14:34:59 +0000

    [FIX] website_sale_loyalty: only show `ecommerce_ok` programs in cart
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Create a loyalty program that applies on future orders;
    2. disable it for eCommerce;
    3. create a card for you with adequate points for a reward;
    4. go to website shop;
    5. add product to cart;
    6. go to shopping cart view.
    
    Issue
    -----
    Claimable reward is display despite being disabled for eCommerce.
    
    Cause
    -----
    The `_get_claimable_and_showable_rewards` method doesn't take a
    program's `ecommerce_ok` field into account when searching for cards.
    
    Solution
    --------
    In the domain used, use the result of `_get_program_domain` to ensure
    only applicable cards from applicable program's are retrieved.
    
    opw-3997371
    
    X-original-commit: 037eaa0ce5968c6ee85da9410fea7443176cbe3d

--- addons/website_sale_loyalty/tests/test_shop_sale_coupon.py
+++ addons/website_sale_loyalty/tests/test_shop_sale_coupon.py
@@ -217,12 +217,12 @@ class WebsiteSaleLoyaltyTestUi(TestSaleProductAttributeValueCommon, HttpCase):
         self.assertEqual(len(gift_card_program.coupon_ids), 2, 'There should be two coupons, one with points, one without')
         self.assertEqual(len(gift_card_program.coupon_ids.filtered('points')), 1, 'There should be two coupons, one with points, one without')
 
-    def test_02_admin_shop_ewallet_tour(self):
+    def test_03_admin_shop_ewallet_tour(self):
         public_category = self.env['product.public.category'].create({'name': 'Public Category'})
         self.env['product.product'].create({
-            'name': 'TEST - Small Drawer',
+            'name': "TEST - Gift Card",
             'list_price': 50,
-            'type': 'consu',
+            'type': 'service',
             'is_published': True,
             'sale_ok': True,
             'public_categ_ids': [(4, public_category.id)],
@@ -230,25 +230,23 @@ class WebsiteSaleLoyaltyTestUi(TestSaleProductAttributeValueCommon, HttpCase):
         })
         # Disable any other program
         self.env['loyalty.program'].search([]).write({'active': False})
-        ewallet_program = self.env['loyalty.program'].create({
-            'name': 'ewallet - test',
+        ewallet_programs = self.env['loyalty.program'].create([{
+            'name': f"ewallet - test - {ecommerce_ok=}",
             'applies_on': 'future',
             'trigger': 'auto',
             'program_type': 'ewallet',
+            'ecommerce_ok': ecommerce_ok,
             'reward_ids': [Command.create({
                 'reward_type': 'discount',
                 'discount_mode': 'per_point',
                 'discount': 1,
             })],
-        })
-        ewallet_program.currency_id = self.env.ref('base.USD')
-        self.env['loyalty.card'].create({
+        } for ecommerce_ok in (True, False)])
+        self.env['loyalty.card'].create([{
             'partner_id': self.env.ref('base.partner_admin').id,
-            'program_id': ewallet_program.id,
+            'program_id': program_id,
             'points': 1000,
-        })
-        default_carrier = self.env.ref('delivery.free_delivery_carrier')
-        self.env['delivery.carrier'].search([('id', '!=', default_carrier.id)]).write({'website_published': False})
+        } for program_id in ewallet_programs.ids])
         self.start_tour('/', 'shop_sale_ewallet', login='admin')
 
 
