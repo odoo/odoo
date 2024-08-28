d4bb19e27f24952dff4b641e8325741e10c4b66a
Jinane Maksoud <maji@odoo.com>
2024-08-28 15:05:40 +0200

    [FIX] mail: avoid duplicates in channel members
    The compute for channel_partner_ids keeps only the active
    partners while 'channel.group_ids.users.partner_id' may include
    inactive partners, so we end up with an existing member getting
    recreated violating the constraint
    discuss_channel_member_partner_unique.

--- addons/mail/models/discuss/discuss_channel.py
+++ addons/mail/models/discuss/discuss_channel.py
@@ -316,7 +316,7 @@ class Channel(models.Model):
     def _subscribe_users_automatically_get_members(self):
         """ Return new members per channel ID """
         return dict(
-            (channel.id, (channel.group_ids.users.partner_id - channel.channel_partner_ids).ids)
+            (channel.id, (channel.group_ids.users.partner_id - channel.channel_member_ids.partner_id).ids)
             for channel in self
         )
 
