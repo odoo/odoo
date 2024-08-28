3d3fee78c7442e057fb843cfa802704ed8eb4137
Xavier Luyckx (xlu) <xlu@odoo.com>
2024-08-28 10:02:11 +0200

    [IMP] website: introduce `s_contact_info` snippet
    This commit adds the new `s_contact_info` snippet.
    
    task-4142017
    Part-of: task-4077427

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -79,6 +79,7 @@
         'views/snippets/s_parallax.xml',
         'views/snippets/s_quotes_carousel.xml',
         'views/snippets/s_numbers.xml',
+        'views/snippets/s_contact_info.xml',
         'views/snippets/s_cta_box.xml',
         'views/snippets/s_masonry_block.xml',
         'views/snippets/s_sidegrid.xml',
