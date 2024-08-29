11844e9abc5d942aa2f3ce44fa8436ad5e296c7a
paan-odoo <paan@odoo.com>
2024-08-29 12:05:01 +0000

    [FIX] pos_loyalty: loyalty reward product quantity and loyalty points
    Before this commit:
    
    - An issue existed with the quantity of reward products where there were more
      reward products applied than configured in the backend.
    - Rewards were being applied even when loyalty points were insufficient for the
      redemption of loyalty rewards.
    - In a loyalty program configuration with a reward point mode per order, loyalty
      points were consistently decreasing.
    - We were not showing a balance of loyalty points for the customers.
    
    After this commit:
    
    - The quantity of the applied reward product will now match the configuration
      set in the backend.
    - Reward products will not be applied if the customer's loyalty points are
      insufficient for the specific reward product.
    - Loyalty points will be computed correctly in the case of the reward point mode
      per order.
    - We will show a balance of loyalty points for the customers.
    
    task-4126886
    
    closes odoo/odoo#180850
    
    X-original-commit: 606f6ee435ae2d79b3cb522f46ce52e9eb41e12a
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Anantkumar Parmar (paan) <paan@odoo.com>

--- addons/pos_loyalty/static/tests/tours/PosLoyaltyLoyaltyProgramTour.js
+++ addons/pos_loyalty/static/tests/tours/PosLoyaltyLoyaltyProgramTour.js
@@ -158,7 +158,7 @@ registry.category("web_tour.tours").add("PosLoyaltyChangeRewardQty", {
             ProductScreen.addOrderline("Desk Organizer", "1"),
             PosLoyalty.isRewardButtonHighlighted(true),
             PosLoyalty.claimReward("Free Product - Whiteboard Pen"),
-            PosLoyalty.hasRewardLine("Free Product - Whiteboard Pen", "-80", "25.00"),
+            PosLoyalty.hasRewardLine("Free Product - Whiteboard Pen", "-3.20", "1.00"),
             ProductScreen.pressNumpad("Qty"),
             ProductScreen.pressNumpad("1"),
             PosLoyalty.hasRewardLine("Free Product - Whiteboard Pen", "-3.20", "1"),
@@ -182,7 +182,7 @@ registry.category("web_tour.tours").add("PosLoyaltyLoyaltyProgram3", {
             // The reward button should be highlighted.
             PosLoyalty.isRewardButtonHighlighted(true),
             PosLoyalty.claimReward("Free Product - Whiteboard Pen"),
-            PosLoyalty.hasRewardLine("Free Product - Whiteboard Pen", "0.0", "2.00"),
+            PosLoyalty.hasRewardLine("Free Product - Whiteboard Pen", "0.0", "1.00"),
 
             PosLoyalty.orderTotalIs("10.2"),
             PosLoyalty.finalizeOrder("Cash", "10.2"),
