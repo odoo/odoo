26730cd459ed0413a51b736bb70564c805bad5c7
Alexandre Kühn <aku@odoo.com>
2024-08-29 18:12:40 +0200

    [IMP] mail: improve slightly 'new message' chat window
    - Add new pencil icon next to "new message"
    - Size chat window header similarly to chat windows with thread
    - Remove "fold" actions and clickable header, as new message is
      not foldable.
    - Align input color with border

--- addons/mail/static/src/discuss/core/web/channel_selector.js
+++ addons/mail/static/src/discuss/core/web/channel_selector.js
@@ -12,7 +12,7 @@ import { useSequential } from "@mail/utils/common/hooks";
 
 export class ChannelSelector extends Component {
     static components = { TagsList, NavigableList };
-    static props = ["category", "onValidate?", "autofocus?", "multiple?", "close?", "class?"];
+    static props = ["category", "onValidate?", "autofocus?", "multiple?", "close?"];
     static defaultProps = { multiple: true };
     static template = "discuss.ChannelSelector";
 
