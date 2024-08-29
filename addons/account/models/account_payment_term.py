59cf90cccd43aba87997a95258469cc78bfdc22d
Sven Fuehr <svfu@odoo.com>
2024-08-29 12:54:51 +0000

    [FIX] account: apply cash rounding to payment terms
    Consider an invoice with a cash rounding method selected.
    The cash rounding is applied to the total of the invoice but not not the payment terms.
    Currently unrounded amounts can appear in the following cases
    - Multiple payment term lines
    - Discount on payment term lines
    
    This is can also cause problems for the reconciliation.
    The cash rounded payment can not be reconciled with the non-cash-rounded payment term amount
    (since the amounts do not match exactly).
    
    After this commit the cash rounding is applied to the payment terms:
    - The amount of each payment term line is cash rounded
    - The discounted amount of each payment term line is cash rounded
    
    Reproduce:
      1. Install module `account` (or `account_accountant`)
      2. Create a payment term with 1 line with 10% discount when paid within 30 days.
         Ensure "Display terms on invoice" is on.
         (Invoicing / Accounting -> Configuration (menu) -> Payment Terms)
      3. Enable "Cash Rounding" in the settings
      4. Create a cash rounding with precision 0.05 and rounding method HALF-UP
         (Invoicing / Accounting -> Configuration (menu) -> Cash Roundings)
      5. Create an invoice
         - Payment term from step 2
         - Cash rounding from step 3 (set in "Other Info" tab)
         - A line s.t. the total is 868.35 (This value does not need cash rounding)
      6. Confirm the invoice
      7. Print the invoice:
         On the PDF the value 781.52 is shown for the payment terms.
         But the value should be 781.50 due to the cash rounding.
      8. Register payment: The amount 781.52 is suggested.
         But the value should be 781.50 due to the cash rounding.
         Changing the amount to 781.50 loses the automatic reconciliation.
    
    task-4095505
    opw-3985470
    
    closes odoo/odoo#179142
    
    X-original-commit: 051ebfa6aea2963d055af62f80801eb60f608aa3
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>

--- addons/account/models/account_payment_term.py
+++ addons/account/models/account_payment_term.py
@@ -159,7 +159,7 @@ class AccountPaymentTerm(models.Model):
             if terms.early_discount and terms.discount_days <= 0:
                 raise ValidationError(_("The Early Payment Discount days must be strictly positive."))
 
-    def _compute_terms(self, date_ref, currency, company, tax_amount, tax_amount_currency, sign, untaxed_amount, untaxed_amount_currency):
+    def _compute_terms(self, date_ref, currency, company, tax_amount, tax_amount_currency, sign, untaxed_amount, untaxed_amount_currency, cash_rounding=None):
         """Get the distribution of this payment term.
         :param date_ref: The move date to take into account
         :param currency: the move's currency
@@ -169,6 +169,10 @@ class AccountPaymentTerm(models.Model):
         :param untaxed_amount: the signed untaxed amount for the move
         :param untaxed_amount_currency: the signed untaxed amount for the move in the move's currency
         :param sign: the sign of the move
+        :param cash_rounding: the cash rounding that should be applied (or None).
+            We assume that the input total in move currency (tax_amount_currency + untaxed_amount_currency) is already cash rounded.
+            The cash rounding does not change the totals: Consider the sum of all the computed payment term amounts in move / company currency.
+            It is the same as the input total in move / company currency.
         :return (list<tuple<datetime.date,tuple<float,float>>>): the amount in the company's currency and
             the document's currency, respectively for each required payment date
         """
@@ -176,6 +180,7 @@ class AccountPaymentTerm(models.Model):
         company_currency = company.currency_id
         total_amount = tax_amount + untaxed_amount
         total_amount_currency = tax_amount_currency + untaxed_amount_currency
+        rate = abs(total_amount_currency / total_amount) if total_amount else 0.0
 
         pay_term = {
             'total_amount': total_amount,
@@ -195,7 +200,12 @@ class AccountPaymentTerm(models.Model):
                 pay_term['discount_balance'] = company_currency.round(total_amount * (1 - discount_percentage))
                 pay_term['discount_amount_currency'] = currency.round(total_amount_currency * (1 - discount_percentage))
 
-        rate = abs(total_amount_currency / total_amount) if total_amount else 0.0
+            if cash_rounding:
+                cash_rounding_difference_currency = cash_rounding.compute_difference(currency, pay_term['discount_amount_currency'])
+                if not currency.is_zero(cash_rounding_difference_currency):
+                    pay_term['discount_amount_currency'] += cash_rounding_difference_currency
+                    pay_term['discount_balance'] = company_currency.round(pay_term['discount_amount_currency'] / rate) if rate else 0.0
+
         residual_amount = total_amount
         residual_amount_currency = total_amount_currency
 
@@ -206,8 +216,9 @@ class AccountPaymentTerm(models.Model):
                 'foreign_amount': 0,
             }
 
-            if i == len(self.line_ids) - 1:
-                # The last line is always the balance, no matter the type
+            # The last line is always the balance, no matter the type
+            on_balance_line = i == len(self.line_ids) - 1
+            if on_balance_line:
                 term_vals['company_amount'] = residual_amount
                 term_vals['foreign_amount'] = residual_amount_currency
             elif line.value == 'fixed':
@@ -220,6 +231,16 @@ class AccountPaymentTerm(models.Model):
                 line_amount_currency = currency.round(total_amount_currency * (line.value_amount / 100.0))
                 term_vals['company_amount'] = line_amount
                 term_vals['foreign_amount'] = line_amount_currency
+
+            if cash_rounding and not on_balance_line:
+                # The value `residual_amount_currency` is always cash rounded (in case of cash rounding).
+                #   * We assume `total_amount_currency` is cash rounded.
+                #   * We only subtract cash rounded amounts.
+                # Thus the balance line is cash rounded.
+                cash_rounding_difference_currency = cash_rounding.compute_difference(currency, term_vals['foreign_amount'])
+                if not currency.is_zero(cash_rounding_difference_currency):
+                    term_vals['foreign_amount'] += cash_rounding_difference_currency
+                    term_vals['company_amount'] = company_currency.round(term_vals['foreign_amount'] / rate) if rate else 0.0
 
             residual_amount -= term_vals['company_amount']
             residual_amount_currency -= term_vals['foreign_amount']
