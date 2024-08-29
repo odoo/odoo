c9c634f5afd14b48730f56e10666f3acc93b90b3
muab-odoo <muab@odoo.com>
2024-08-29 12:23:37 +0000

    [ADD] l10n_qa: Add localization package for qatar
    Add CoA and account groups with all the default account mappings for qatar
    
    Improve user experience in qatar
    
    task-3927918
    
    closes odoo/odoo#180473
    
    X-original-commit: e5b92ee221df7f52bd9b1a0be0779285b4c4427a
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>
    Signed-off-by: Muhamed Abdalla (muab) <muab@odoo.com>

---
+++ addons/l10n_qa/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Qatar - Accounting',
+    'countries': ['qa'],
+    'description': """
+This is the base module to manage the accounting chart for Qatar in Odoo.
+==============================================================================
+Qatar accounting basic charts and localization.
+Activates:
+- Chart of accounts
+    """,
+    'category': 'Accounting/Localizations/Account Charts',
+    'version': '1.0',
+    'depends': [
+        'account',
+    ],
+    'demo': [
+        'demo/demo_company.xml',
+    ],
+    'license': 'LGPL-3',
+}
