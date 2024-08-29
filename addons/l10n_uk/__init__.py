aecc5af61f9a6feb662b7d27f0390c98f0addc8e
Mathieu (mcou) <mcou@odoo.com>
2024-08-29 09:03:59 +0000

    [FIX] l10n_uk: auto-install l10n_uk_bacs
    Steps to reproduce:
    - Install the 'l10n_uk' module
    - The module "l10n_uk_bacs" is not installed, but it should
    
    Solution:
    Add a post init hook in `l10n_uk` to download `l10n_uk_bacs`.
    
    opw-4122475
    
    closes odoo/odoo#178884
    
    X-original-commit: a766f20fd3ba576e592ae800abf91c8ecd6e4f36
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Anh Thao Pham (pta) <pta@odoo.com>

--- addons/l10n_uk/__init__.py
+++ addons/l10n_uk/__init__.py
@@ -1,2 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import models
+
+
+def _l10n_uk_post_init(env):
+    bacs_module = env['ir.module.module'].search([('name', '=', 'l10n_uk_bacs'), ('state', '=', 'uninstalled')])
+    if bacs_module:
+        bacs_module.sudo().button_install()
