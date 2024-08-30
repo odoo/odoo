2eae8bdeebbd62e20681f48bef9e514eaa7bfffe
tsm-odoo <tsm@odoo.com>
2024-08-30 08:00:36 +0200

    [IMP] bus, *: avoid running NOTIFY multiple times
    * = account, im_livechat, mail, test_mail
    
    Every time we called `_sendone` or `_sendmany`, it would create a new
    cursor, then call `NOTIFY` for all the channels.
    By using `postcommit.data`, we can reduce the number of calls.
    
    Since everything is delayed and batched by default, we can now also
    deprecat status
    te `_sendmany`.
    
    backport of https://github.com/odoo/odoo/pull/176340
    
    closes odoo/odoo#178437
    
    Related: odoo/enterprise#69159
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/mail/tests/discuss/test_discuss_channel.py
+++ addons/mail/tests/discuss/test_discuss_channel.py
@@ -207,8 +207,7 @@ class TestChannelInternals(MailCommon):
         chat = self.env['discuss.channel'].with_user(self.user_admin).channel_get((self.partner_employee | self.user_admin.partner_id).ids)
         msg_1 = self._add_messages(chat, 'Body1', author=self.user_employee.partner_id)
         member = chat.channel_member_ids.filtered(lambda m: m.partner_id == self.user_admin.partner_id)
-
-        self.env['bus.bus'].sudo().search([]).unlink()
+        self._reset_bus()
         with self.assertBus(
             [
                 (self.env.cr.dbname, "discuss.channel", chat.id),
@@ -257,7 +256,7 @@ class TestChannelInternals(MailCommon):
             chat._channel_seen(msg_1.id)
         # There should be no channel member to be set as seen in the second time
         # So no notification should be sent
-        self.env['bus.bus'].sudo().search([]).unlink()
+        self._reset_bus()
         with self.assertBus([], []):
             chat._channel_seen(msg_1.id)
 
@@ -385,7 +384,7 @@ class TestChannelInternals(MailCommon):
 
     def test_channel_write_should_send_notification(self):
         channel = self.env['discuss.channel'].create({"name": "test", "description": "test"})
-        self.env['bus.bus'].search([]).unlink()
+        self._reset_bus()
         with self.assertBus(
             [(self.cr.dbname, 'discuss.channel', channel.id)],
             [{
@@ -407,7 +406,7 @@ class TestChannelInternals(MailCommon):
         channel.image_128 = base64.b64encode(("<svg/>").encode())
         avatar_cache_key = channel._get_avatar_cache_key()
         channel.image_128 = False
-        self.env['bus.bus'].search([]).unlink()
+        self._reset_bus()
         with self.assertBus(
             [(self.cr.dbname, 'discuss.channel', channel.id)],
             [{
@@ -469,7 +468,7 @@ class TestChannelInternals(MailCommon):
         """Ensures the command '/help' works in a channel"""
         channel = self.env["discuss.channel"].browse(self.test_channel.ids)
         channel.name = "<strong>R&D</strong>"
-        self.env['bus.bus'].sudo().search([]).unlink()
+        self._reset_bus()
         with self.assertBus(
             [(self.env.cr.dbname, "res.partner", self.env.user.partner_id.id)],
             [
@@ -507,7 +506,7 @@ class TestChannelInternals(MailCommon):
             'channel_partner_ids': [(6, 0, test_user.partner_id.id)]
         })
         test_group.add_members(self.partner_employee_nomail.ids)
-        self.env['bus.bus'].sudo().search([]).unlink()
+        self._reset_bus()
         with self.assertBus(
             [(self.env.cr.dbname, "res.partner", self.env.user.partner_id.id)],
             [
