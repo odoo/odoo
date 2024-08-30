2e301a7924e815be75af8c3618f9e24275e32ecd
Victor Feyens <vfe@odoo.com>
2024-08-30 15:53:23 +0200

    [REV] website_sale_picking: adjust fpos based on warehouse location
    This commit reverts dd331e7bb27c533808a01b53f80e0ee7f1dfed7f
    
    This change has caused a lot of issues recently, and despite some other
    fixes, we believe it's safer to revert it for now.
    
    opw-4126140
    opw-4114139
    opw-4115510
    opw-4117520
    opw-4109625
    opw-4115415
    opw-4143237
    opw-4123382
    opw-4136888
    opw-4125807

--- addons/website_sale_picking/static/tests/tours/onsite_payment_tour.js
+++ addons/website_sale_picking/static/tests/tours/onsite_payment_tour.js
@@ -52,37 +52,3 @@ registry.category("web_tour.tours").add('onsite_payment_tour', {
         },
     ]
 });
-
-registry.category("web_tour.tours").add('onsite_payment_fiscal_change_tour', {
-    test: true,
-    url: '/shop',
-    steps: () => [
-        ...wsTourUtils.addToCart({ productName: 'Super Product' }),
-        wsTourUtils.goToCart(),
-        wsTourUtils.goToCheckout(),
-        ...wsTourUtils.fillAdressForm(),
-        ...wsTourUtils.assertCartAmounts({
-            taxes: '10.00',
-            untaxed: '100.00',
-            total: '110.00',
-        }),
-        wTourUtils.clickOnElement(
-            'Example shipping On Site',
-            '.o_delivery_carrier_select:contains("Example shipping On Site")',
-        ),
-        ...wsTourUtils.assertCartAmounts({
-            taxes: '5.00',
-            untaxed: '100.00',
-            total: '105.00',
-        }),
-        wTourUtils.clickOnElement(
-            '"Standard delivery"',
-            '.o_delivery_carrier_select:contains("Standard delivery")',
-        ),
-        ...wsTourUtils.assertCartAmounts({
-            taxes: '10.00',
-            untaxed: '100.00',
-            total: '110.00',
-        }),
-    ]
-});
