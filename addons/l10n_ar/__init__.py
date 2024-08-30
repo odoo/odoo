2b84aaaf9d8391127900a5cf36d1eee3808cf081
Josse Colpaert <jco@odoo.com>
2024-08-30 10:26:25 +0200

    [IMP] l10n_ar: remove dead code
    Before 17 process_coa_translations might have been needed
    in multi_lang localizations, but it is no longer needed.
    
    It is also not called.
    
    No task linked.  Just saw it checking other stuff.
    
    closes odoo/odoo#178508
    
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/l10n_ar/__init__.py
+++ addons/l10n_ar/__init__.py
@@ -3,8 +3,3 @@
 from . import models
 from . import report
 from . import demo
-from odoo import api, SUPERUSER_ID
-
-def load_translations(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env.ref('l10n_ar.l10nar_ri_chart_template').process_coa_translations()
