ca926c33dae090ba13b272680b0c448b0b50d9d0
Julien Mougenot <jum@odoo.com>
2024-08-30 10:04:03 +0000

    [FIX] web: remove datetime service events cleanup
    Before this commit, event listeners set on inputs by the datetime picker
    service would be removed in the callback of the `useEffect` hook,
    meaning that these listeners could be unregistered before the actual
    elements would be removed.
    
    This is an issue in the specific case where the deletion of the element
    occurs before the "change" event has been dispatched and after the
    removal of the listeners. The thing is: the removal of the element also
    triggers the "change" event (if it hasn't been triggered before), and
    with the listener gone this means that the value is lost.
    
    This commit deletes the removal of event listeners attached on inputs
    by the hook. This has been done since using the datetime picker service
    implies that the inputs affected by the feature will never be in an
    interactive state without the datetime picker actively listening on
    them.
    
    Although a test case setup is easy to reproduce, this behavior is
    unfortunately impossible to reproduce programmatically as the "change"
    event dispatched by removing an element only works when a trusted
    "input" event was triggered to change its value (setting the internal
    browser "changed" value of the input).
    
    Task 4104407
    
    X-original-commit: 7b65efda171db216e93afa509e7e9a5b822d350c

--- addons/web/static/src/core/datetime/datetimepicker_service.js
+++ addons/web/static/src/core/datetime/datetimepicker_service.js
@@ -43,6 +43,8 @@ const formatters = {
     datetime: formatDateTime,
 };
 
+const listenedElements = new WeakSet();
+
 const parsers = {
     date: parseDate,
     datetime: parseDateTime,
@@ -441,16 +443,6 @@ export const datetimePickerService = {
                 let restoreTargetMargin = null;
                 let shouldFocus = false;
 
-                /**
-                 * @param {HTMLElement} el
-                 * @param {string} type
-                 * @param {(ev: Event) => any} listener
-                 */
-                const addListener = (el, type, listener) => {
-                    el.addEventListener(type, listener);
-                    return () => el.removeEventListener(type, listener);
-                };
-
                 return {
                     state: pickerProps,
                     open: openPicker,
@@ -462,18 +454,18 @@ export const datetimePickerService = {
                     },
                     enable() {
                         let editableInputs = 0;
-                        const cleanups = [];
                         for (const [el, value] of zip(
                             getInputs(),
                             ensureArray(pickerProps.value),
                             true
                         )) {
                             updateInput(el, value);
-                            if (el && !el.disabled && !el.readOnly) {
-                                cleanups.push(addListener(el, "change", onInputChange));
-                                cleanups.push(addListener(el, "click", onInputClick));
-                                cleanups.push(addListener(el, "focus", onInputFocus));
-                                cleanups.push(addListener(el, "keydown", onInputKeydown));
+                            if (el && !el.disabled && !el.readOnly && !listenedElements.has(el)) {
+                                listenedElements.add(el);
+                                el.addEventListener("change", onInputChange);
+                                el.addEventListener("click", onInputClick);
+                                el.addEventListener("focus", onInputFocus);
+                                el.addEventListener("keydown", onInputKeydown);
                                 editableInputs++;
                             }
                         }
@@ -482,16 +474,12 @@ export const datetimePickerService = {
                         );
                         if (calendarIconGroupEl) {
                             calendarIconGroupEl.classList.add("cursor-pointer");
-                            cleanups.push(
-                                addListener(calendarIconGroupEl, "click", () => {
-                                    openPicker(0);
-                                })
-                            );
+                            calendarIconGroupEl.addEventListener("click", () => openPicker(0));
                         }
                         if (!editableInputs && popover.isOpen) {
                             saveAndClose();
                         }
-                        return () => cleanups.forEach((cleanup) => cleanup());
+                        return () => {};
                     },
                     get isOpen() {
                         return popover.isOpen;
