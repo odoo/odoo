1c9dda4bd484253395b17e7a1531c55ce24aedb2
Nathaniel (naja) <naja@odoo.com>
2024-08-30 12:43:20 +0000

    [FIX] sale_stock: correct delivered quantity for multi-step returns
    Steps
    ---
    * install `stock`, `sale_management`
    * configure reception in 2 steps
      (*Setting > Multi-step routes* + *Configure Warehouse Routes*)
    * make *Stock/Input* a return location
      (*Inventory* > *Configuration* > *Stock/Input* form)
    * create an SO for 5 units of a storable product (say P)
    * (put at least 5 of P available in stock)
    * confirm it > go to the delivery > validate
    * *Return* > from the wizard set *Stock/Input* as the return location
    * validate the `Customers -> Input` receipt
    * validate the `Input -> Stock` internal transfer
    * => on the orignal SO the delivered quantity is now -5 instead of 0
    
    Cause
    ---
    When generating moves in a push rules, we copy the fields of the
    generating move, including `to_refund`, so both moves affect the
    delivered quantity.
    But when we check if a return move should affect the delivered/received
    qty on SO/POs, we only check the `usage` of the destination location;
    so in the case of multi-step routes, moves further down the chain
    will affect the qty twice or more, and we will have a negative
    delivered quantity
    
    Fix
    ---
    Use a stricter check to determined which moves should affect SO
    quantities
    
    opw-4129497
    
    X-original-commit: ccf2a14d8e06dbe89d5614f7e75b97e8cadb739d

--- addons/sale_stock/tests/test_sale_stock.py
+++ addons/sale_stock/tests/test_sale_stock.py
@@ -2080,3 +2080,63 @@ class TestSaleStock(TestSaleStockCommon, ValuationReconciliationTestCommon):
         self.assertTrue(delivery_2)
         self.assertEqual(delivery_2.move_ids.product_uom_qty, 3.0)
         self.assertEqual(so.order_line.qty_delivered, 5.0)
+
+    def test_return_from_customer_multi_step(self):
+        """
+        Check that, when using multi-step routes, returned quantities are counted on
+        the corresponding SO's delivered quantity only once for the whole move chain
+        """
+        # Set-up multi-step routes
+        self.env.user.groups_id += self.env.ref('stock.group_stock_multi_locations')
+        self.env.user.groups_id += self.env.ref('stock.group_adv_location')
+        warehouse = self.company_data['default_warehouse']
+
+        with Form(warehouse) as w:
+            w.reception_steps = 'two_steps'
+
+        # Make *Stock/Input* a return location
+        loc_input = warehouse.wh_input_stock_loc_id
+        loc_input.return_location = True
+
+        # Make SO and confirm
+        product = self.env['product.product'].create({
+            'name': 'Delivered Product',
+            'type': 'product',
+        })
+        so = self._get_new_sale_order(product=product)
+        so.action_confirm()
+
+        # Validate delivery
+        picking = so.picking_ids
+        self.assertEqual(len(picking), 1)
+
+        def validate_picking(picking, qty=10):
+            picking.move_ids.quantity = qty
+            picking.move_ids.picked = True
+            picking.button_validate()
+
+        validate_picking(picking)
+        self.assertEqual(so.order_line[0].qty_delivered, 10)
+
+        # Create return picking chain
+        stock_return_picking_form = Form(
+            self.env['stock.return.picking'].with_context(
+                active_ids=picking.ids,
+                active_id=picking.ids[0],
+                active_model='stock.picking'
+            )
+        )
+
+        stock_return_picking_form.location_id = loc_input
+        stock_return_picking = stock_return_picking_form.save()
+        stock_return_picking.create_returns()
+
+        return_1 = so.picking_ids.filtered(lambda r: r.location_dest_id.name == 'Input')
+        return_2 = so.picking_ids.filtered(lambda r: r.location_dest_id.name == 'Stock')
+
+        # Check that validating returns correctly updates the SO's delivered qty
+        validate_picking(return_1)
+        self.assertEqual(so.order_line[0].qty_delivered, 0)
+
+        validate_picking(return_2)
+        self.assertEqual(so.order_line[0].qty_delivered, 0)
