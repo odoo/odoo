f76677799b5da163e5496fe36de697074faa06a6
qsm-odoo <qsm@odoo.com>
2024-08-30 09:37:34 +0000

    [FIX] portal, portal_rating: restore rating filtering
    /!\
    Starting from 17.0, the following steps to reproduce cannot be followed
    because of another bug, probably introduced by [1]. Indeed: the
    "filtering tool" mentioned does not even appear anymore. Still, if you
    tweak the JS to make it appear, this forward-ported commit fixes the
    original issue. That other problem will be fixed with another PR later.
    
    Steps to reproduce:
    - Go to a product page
    - Enable "Discussions and rating" in the eCommerce
    - Add some comments and ratings on the product
    - Use the filtering tool on the left
    => It does not work anymore
    
    [1]: https://github.com/odoo/odoo/commit/23bbd8860bd12c9e62942415a8e3cff084380d73
    
    task-4149745
    
    closes odoo/odoo#178729
    
    X-original-commit: 71248e0c1314c411544ca0f57d24f2d581545d67
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/portal_rating/controllers/portal_chatter.py
+++ addons/portal_rating/controllers/portal_chatter.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo.osv import expression
+
 from odoo.addons.portal.controllers import mail
 
 
@@ -9,3 +11,9 @@ class PortalChatter(mail.PortalChatter):
         fields = super(PortalChatter, self)._portal_post_filter_params()
         fields += ['rating_value']
         return fields
+
+    def _setup_portal_message_fetch_extra_domain(self, data):
+        domains = [super()._setup_portal_message_fetch_extra_domain(data)]
+        if data.get('rating_value', False) is not False:
+            domains.append([('rating_value', '=', float(data['rating_value']))])
+        return expression.AND(domains)
