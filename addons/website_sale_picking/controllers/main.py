26795828f470b713912f5e8f2177f11b4899f0d0
Victor Feyens <vfe@odoo.com>
2024-08-30 09:26:08 +0000

    [FIX] website_sale_picking: unable to edit warehouse address
    Some fixes already landed with 7cbf7f94263a31d316b9c5a0efad57ad357aa5a0,
    but if the address is incomplete, the user would still be redirected
    to a 403 page because he cannot update the warehouse address to fill
    the missing fields.
    
    This commit makes sure that for onsite flows, the warehouse address
    is considered complete.
    
    opw-4126140
    opw-4114139
    opw-4115510
    opw-4117520
    opw-4109625
    opw-4115415
    opw-4143237
    opw-4123382
    opw-4136888
    opw-4125807
    
    closes odoo/odoo#178608
    
    X-original-commit: b3b5dc92219729283ed7a075849507b523704dba
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/website_sale_picking/controllers/main.py
+++ addons/website_sale_picking/controllers/main.py
@@ -2,7 +2,9 @@
 
 from odoo import _
 from odoo.exceptions import ValidationError
+from odoo.http import request
 
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.addons.website_sale.controllers.payment import PaymentPortal
 
 
@@ -29,3 +31,14 @@ class PaymentPortalOnsite(PaymentPortal):
         # TODO should be managed in a `_compute_warehouse_id` or `update_eshop_carrier` override
         if sale_order.carrier_id.delivery_type == 'onsite' and sale_order.carrier_id.warehouse_id:
             sale_order.warehouse_id = sale_order.carrier_id.warehouse_id
+
+
+class WebsiteSalePicking(WebsiteSale):
+
+    def _check_shipping_partner_mandatory_fields(self, partner_id):
+        order_sudo = request.website.sale_get_order()
+        carrier_sudo = order_sudo.carrier_id
+        if carrier_sudo.delivery_type == 'onsite' and partner_id == carrier_sudo.warehouse_id.partner_id:
+            return True
+
+        return super()._check_shipping_partner_mandatory_fields(partner_id)
