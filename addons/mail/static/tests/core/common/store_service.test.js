1c361c640c8df6f3c61a4e9ad9c8262a03bbb90d
tsm-odoo <tsm@odoo.com>
2024-09-02 14:59:05 +0200

    [FIX] mail: insert() multiple PY records sharing the same JS model
    Before this PR, inserting data related to two Python models that
    shared the same JavaScript model would return only the records
    associated with the last inserted model, instead of returning all
    inserted records. This issue could arise when inserting both
    "discuss.channel" and "mail.thread" records, for instance.
    
    The root cause of this problem was in the `storeService.insert`
    method, which stored the result in a dictionary keyed by the JS model.
    When two Python models shared the same JS model, the records for the
    earlier model would be overwritten by those of the later one.
    
    This PR ensures that the `storeService.insert` method properly handles
    this scenario, allowing it to return records from all inserted models,
    even if they share the same JS model.
    
    closes odoo/odoo#178801
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/mail/static/tests/core/common/store_service.test.js
+++ addons/mail/static/tests/core/common/store_service.test.js
@@ -52,3 +52,27 @@ test("store.insert deletes record after relation created it", async () => {
     await assertSteps(["new-1"]);
     expect(store.Message.get({ id: 1 })?.id).toBe(undefined);
 });
+
+test("store.insert different PY model having same JS model", async () => {
+    await start();
+    const store = getService("mail.store");
+    const data = {
+        "discuss.channel": [
+            { id: 1, name: "General" },
+            { id: 2, name: "Sales" },
+        ],
+        "mail.thread": [
+            { id: 1, model: "discuss.channel" },
+            { id: 3, name: "R&D", model: "discuss.channel" },
+        ],
+    };
+
+    const { Thread: threads } = store.insert(data);
+    expect(threads).toHaveLength(3);
+    const general = store.Thread.get({ id: 1, model: "discuss.channel" });
+    const sales = store.Thread.get({ id: 2, model: "discuss.channel" });
+    const rd = store.Thread.get({ id: 3, model: "discuss.channel" });
+    expect(general.in(threads)).toBe(true);
+    expect(sales.in(threads)).toBe(true);
+    expect(rd.in(threads)).toBe(true);
+});
