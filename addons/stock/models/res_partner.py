d8990677c51de5ca9e4d453979043a1d962b2328
vava-odoo <vava@odoo.com>
2024-09-02 15:12:40 +0200

    [IMP] stock: add customer lots/SN report
    For complex products installers, we need to help them managing the
    warranties, maintenance and track serial numbers. We therefore add a
    Lot/Serial link in res.partner to access a new report listing all
    products delivered to this customer.
    
    task-4196049
    
    closes odoo/odoo#180402
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/stock/models/res_partner.py
+++ addons/stock/models/res_partner.py
@@ -19,3 +19,10 @@ class Partner(models.Model):
         help="The stock location used as source when receiving goods from this contact.")
     picking_warn = fields.Selection(WARNING_MESSAGE, 'Stock Picking', help=WARNING_HELP, default='no-message')
     picking_warn_msg = fields.Text('Message for Stock Picking')
+
+    def action_view_stock_lots(self):
+        action = self.env['ir.actions.act_window']._for_xml_id('stock.action_lot_report')
+        all_child = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])
+        action["domain"] = [("partner_id", "in", all_child.ids)]
+        action["context"] = {'search_default_filter_not_has_return': True}
+        return action
--- addons/stock/report/__init__.py
+++ addons/stock/report/__init__.py
@@ -7,3 +7,4 @@ from . import report_stock_reception
 from . import report_stock_rule
 from . import stock_traceability
 from . import product_label_report
+from . import stock_lot_customer
