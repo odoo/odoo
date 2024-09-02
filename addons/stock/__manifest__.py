41beec88d5262dc5dd48c5e89e5a27cfba5ee2d3
vava-odoo <vava@odoo.com>
2024-09-02 15:12:40 +0200

    [IMP] stock: add customer lots/SN report
    For complex products installers, we need to help them managing the
    warranties, maintenance and track serial numbers. We therefore add a
    Lot/Serial link in res.partner to access a new report listing all
    products delivered to this customer.
    
    task-4196049
    
    closes odoo/odoo#180402
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/stock/__manifest__.py
+++ addons/stock/__manifest__.py
@@ -37,6 +37,7 @@
         'report/report_deliveryslip.xml',
         'report/report_stockinventory.xml',
         'report/report_stock_rule.xml',
+        'report/stock_lot_customer.xml',
         'report/package_templates.xml',
         'report/picking_templates.xml',
         'report/product_templates.xml',
