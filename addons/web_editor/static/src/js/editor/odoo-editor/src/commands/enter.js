36f36fd365e2179beda9a02f870cfa13a555600b
Deependra Solanki <deso@odoo.com>
2024-09-03 08:50:12 +0000

    [FIX] web_editor: pasting within blockquote and pre tags
    Before this commit:
    
    Pasting content within `blockquote` and `pre` elements resulted in each node
    being converted into separate `blockquote` or `pre` elements.
    
    After this commit:
    
    `blockquote` and `pre` should be considered like wrappers, allowing
    paragraph-related elements to be pasted inside them.
    
    task-4154465
    
    X-original-commit: aa48a9386f383df7bea8d7f9ef039f5c11783119
    Part-of: odoo/odoo#187936
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Deependra Solanki (deso) <deso@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/src/commands/enter.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/commands/enter.js
@@ -38,6 +38,19 @@ HTMLElement.prototype.oEnter = function (offset, firstSplit = true) {
     if (isUnbreakable(this)) {
         throw UNBREAKABLE_ROLLBACK_CODE;
     }
+    if (
+        !this.textContent &&
+        ['BLOCKQUOTE', 'PRE'].includes(this.parentElement.nodeName) &&
+        !this.nextSibling
+    ) {
+        const parent = this.parentElement;
+        const index = childNodeIndex(this);
+        if (this.previousElementSibling) {
+            this.remove();
+            return parent.oEnter(index, !didSplit);
+        }
+        return parent.oEnter(index + 1, !didSplit);
+    }
     let restore;
     if (firstSplit) {
         restore = prepareUpdate(this, offset);
@@ -101,7 +114,7 @@ HTMLElement.prototype.oEnter = function (offset, firstSplit = true) {
  */
 HTMLHeadingElement.prototype.oEnter = function () {
     const newEl = HTMLElement.prototype.oEnter.call(this, ...arguments);
-    if (!descendants(newEl).some(isVisibleTextNode)) {
+    if (newEl && !descendants(newEl).some(isVisibleTextNode)) {
         const node = setTagName(newEl, 'P');
         node.replaceChildren(document.createElement('br'));
         setCursorStart(node);
