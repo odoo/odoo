65aee453bdc2779f05b1d57240563d1580670ef1
Simon Goffaux (sigo) <sigo@odoo.com>
2024-09-03 14:12:43 +0000

    [FIX] hr_holidays: add correct follower when importing leaves
    Before this commit, when importing multiple leaves for different
    employee, the last employee in the list will be set as a follower on
    all the created leaves.
    
    This commit fixes this behavior by checking for the employee assigned
    to each leave and using its ID when adding the follower.
    
    opw-4023073
    
    X-original-commit: dcf2eb8fe765aba81a4dcc9125bf40bf299c95ee

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -965,7 +965,7 @@ class HolidaysRequest(models.Model):
                 # eg : holidays_user can create a leave request with validation_type = 'manager' for someone else
                 # but they can only write on it if they are leave_manager_id
                 holiday_sudo = holiday.sudo()
-                holiday_sudo.add_follower(employee_id)
+                holiday_sudo.add_follower(holiday.employee_id.id)
                 if holiday.validation_type == 'manager':
                     holiday_sudo.message_subscribe(partner_ids=holiday.employee_id.leave_manager_id.partner_id.ids)
                 if holiday.validation_type == 'no_validation':
