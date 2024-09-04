87a989c50f4467811e240293e40f5c4f277f819b
Antoine Dupuis (andu) <andu@odoo.com>
2024-09-04 11:38:40 +0200

    [IMP] account: Convert account merge to wizard.
    The wizard proposes groupings of accounts to merge:
    - the user selects as many accounts as they want, then opens the wizard;
    - the wizard presents a preview of the accounts that will be merged;
    - the user can then select or deselect accounts in the wizard;
    - clicking on 'merge' merges all the selected accounts at once.
    
    The grouping is created based on account type, trade/non-trade
    receivable status, account currency, reconcilable status, and deprecated
    status, as these are the fields which must be identical in order for
    accounts to be merged. Additionally, there is a checkbox which allows
    the grouping to be done by name in addition to the above fields.
    
    We don't allow merging accounts that belong to the same company anymore.
    For that, the user should use the mass-edit functionality on journal
    items.
    
    Taskid: 4119457
    Part-of: odoo/odoo#177296
    Signed-off-by: William André (wan) <wan@odoo.com>

---
+++ addons/account/static/tests/tours/account_merge_wizard_tour.js
@@ -0,0 +1,66 @@
+/** @odoo-module */
+
+import { accountTourSteps } from "@account/js/tours/account";
+import { registry } from "@web/core/registry";
+
+registry.category("web_tour.tours").add("account_merge_wizard_tour", {
+    test: true,
+    url: "/odoo",
+    steps: () => [
+        ...accountTourSteps.goToAccountMenu("Go to Accounting"),
+        {
+            content: "Go to Configuration",
+            trigger: 'span:contains("Configuration")',
+            run: "click",
+        },
+        {
+            content: "Go to Chart of Accounts",
+            trigger: 'a:contains("Chart of Accounts")',
+            run: "click",
+        },
+        {
+            trigger: '.o_breadcrumb .text-truncate:contains("Chart of Accounts")',
+        },
+        {
+            content: "Select accounts",
+            trigger: "thead .o_list_record_selector",
+            run: "click",
+        },
+        {
+            content: "Check that exactly 4 accounts are present and selected",
+            trigger: ".o_list_selection_box:contains(4):contains(selected)",
+        },
+        {
+            content: "Open Actions menu",
+            trigger: ".o_cp_action_menus .dropdown-toggle",
+            run: "click",
+        },
+        {
+            content: "Open Merge accounts wizard",
+            trigger: 'span:contains("Merge accounts")',
+            run: "click",
+        },
+        {
+            content: "Group by name",
+            trigger: 'div[name="is_group_by_name"] input',
+            run: "click",
+        },
+        {
+            content: "Wait for content to be updated",
+            trigger: 'td:contains("Current Assets (Current Assets)")',
+        },
+        {
+            content: "Merge accounts",
+            trigger: 'button:not([disabled]) span:contains("Merge")',
+            run: "click",
+        },
+        {
+            content: "Wait for modal to close",
+            trigger: ".o-main-components-container:not(:has(.modal-dialog))",
+        },
+        {
+            content: "Check that there are now exactly 2 accounts",
+            trigger: "tr.o_data_row:contains(2)",
+        },
+    ],
+});
