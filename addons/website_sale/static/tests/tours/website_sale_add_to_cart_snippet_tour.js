fd1d3f30521495acd6e1f73c1143c15116a1218e
Pierre Pulinckx (PIPU) <pipu@odoo.com>
2024-09-04 11:22:21 +0200

    [FIX] website_sale: fix add_to_cart_snippet_tour
    In this commit, we fix undeterministic bugs in tour
    add_to_cart_snippet_tour. To fix this behavior, we add few additionnal
    steps to check the state of DOM before continuing the tour.

--- addons/website_sale/static/tests/tours/website_sale_add_to_cart_snippet_tour.js
+++ addons/website_sale/static/tests/tours/website_sale_add_to_cart_snippet_tour.js
@@ -23,29 +23,67 @@ wTourUtils.registerWebsitePreviewTour('add_to_cart_snippet_tour', {
         ...wTourUtils.selectElementInWeSelectWidget('product_template_picker_opt', 'Acoustic Bloc Screens', true),
         ...wTourUtils.clickOnSave(),
         wTourUtils.clickOnElement('add to cart button', 'iframe .s_add_to_cart_btn'),
-
+        {
+            trigger: "iframe nav li.o_wsale_my_cart sup:contains(1)",
+            run: () => null,
+        },
         // Product with 2 variants with visitor choice (will open modal)
         ...editAddToCartSnippet(),
         ...wTourUtils.selectElementInWeSelectWidget('product_template_picker_opt', 'Conference Chair', true),
         ...wTourUtils.clickOnSave(),
         wTourUtils.clickOnElement('add to cart button', 'iframe .s_add_to_cart_btn'),
         wTourUtils.clickOnElement('continue shopping', 'iframe span:contains(Continue Shopping)'),
+        {
+            trigger: "body:not(:has(.modal))",
+            run: () => null,
+        },
+        {
+            trigger: "iframe nav li.o_wsale_my_cart sup:contains(2)",
+            run: () => null,
+        },
 
         // Product with 2 variants with a variant selected
         ...editAddToCartSnippet(),
         ...wTourUtils.selectElementInWeSelectWidget('product_template_picker_opt', 'Conference Chair', true),
+        {
+            run: () => null,
+            trigger:
+                `we-select[data-name=product_variant_picker_opt] we-toggler:contains("Visitor's Choice")`,
+        },
         ...wTourUtils.selectElementInWeSelectWidget('product_variant_picker_opt', 'Conference Chair (Aluminium)'),
         ...wTourUtils.clickOnSave(),
         wTourUtils.clickOnElement('add to cart button', 'iframe .s_add_to_cart_btn'),
+        {
+            trigger: "iframe nav li.o_wsale_my_cart sup:contains(3)",
+            run: () => null,
+        },
 
         // Basic product with no variants and action=buy now
         ...editAddToCartSnippet(),
         ...wTourUtils.selectElementInWeSelectWidget('product_template_picker_opt', 'Acoustic Bloc Screens', true),
+        {
+            run: () => null,
+            trigger:
+                `we-select[data-name=action_picker_opt] we-toggler:contains("Add to Cart")`,
+        },
         ...wTourUtils.selectElementInWeSelectWidget('action_picker_opt', 'Buy Now'),
         ...wTourUtils.clickOnSave(),
         wTourUtils.clickOnElement('add to cart button', 'iframe .s_add_to_cart_btn'),
-        wTourUtils.assertPathName('/shop/payment', 'button[name=o_payment_submit_button]'),
-
+        {
+            // wait for the page to load, as the next check was sometimes too fast
+            content: "Wait for the redirection to the payment page",
+            trigger: "div:contains(Choose a delivery method)",
+            run: () => null,
+        },
+        {
+            content: `Check if we have been redirected to /shop/payment`,
+            trigger: `button[name=o_payment_submit_button]`,
+            run: () => {
+                if (!window.location.pathname.startsWith("/shop/payment")) {
+                    console.error(`We should be on /shop/payment.`);
+                }
+            },
+        },
         wsTourUtils.goToCart({quantity: 4, backend: false}),
         wsTourUtils.assertCartContains({productName: 'Acoustic Bloc Screens'}),
         wsTourUtils.assertCartContains({productName: 'Conference Chair (Steel)'}),
