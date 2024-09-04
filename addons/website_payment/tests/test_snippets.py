5b75ba08f110ae8c09872d0908511c0df5455039
odoo <angv@odoo.com>
2024-09-04 10:49:19 +0200

    [FIX] website_payment: fix test_01_donation test
    demo_provider does not always have code defined, which
    causes issues in multiple Single app nightly builds.
    This commit fixes the issue by searching with name instead
    of code.

--- addons/website_payment/tests/test_snippets.py
+++ addons/website_payment/tests/test_snippets.py
@@ -6,6 +6,9 @@ import odoo.tests
 class TestSnippets(odoo.tests.HttpCase):
 
     def test_01_donation(self):
+        if self.env['ir.module.module']._get('payment_demo').state != 'installed':
+            self.skipTest("Payment Provider: Demo module is not installed")
+
         demo_provider = self.env['payment.provider'].search([('code', '=', "demo")])
         demo_provider.write({'state': 'test'})
 
