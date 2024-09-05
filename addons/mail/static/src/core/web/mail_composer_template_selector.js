dbdb43d3be4862a1b302be8895d0366f37abf3bf
Julien Banken <jbn@odoo.com>
2024-09-05 15:26:52 +0200

    [IMP] mail: add section to overwrite and delete templates
    task-3748652
    
    Part-of: odoo/odoo#171942
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/static/src/core/web/mail_composer_template_selector.js
+++ addons/mail/static/src/core/web/mail_composer_template_selector.js
@@ -1,9 +1,11 @@
 import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
+import { sprintf } from "@web/core/utils/strings";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { useService } from "@web/core/utils/hooks";
 
 import { Component, useState, onWillStart } from "@odoo/owl";
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { SelectCreateDialog } from "@web/views/view_dialogs/select_create_dialog";
@@ -42,6 +44,48 @@ export class MailComposerTemplateSelector extends Component {
         });
     }
 
+    /**
+     * @param {Object} template
+     * @param {integer} template.id
+     * @param {string} template.display_name
+     */
+    async onDeleteTemplate(template) {
+        this.env.services.dialog.add(ConfirmationDialog, {
+            body: sprintf(_t('Are you sure you want to delete "%(template_name)s"?'), {
+                template_name: template.display_name,
+            }),
+            confirmLabel: _t("Delete Template"),
+            confirm: async () => {
+                await this.orm.unlink("mail.template", [template.id]);
+                this.state.templates = this.state.templates.filter(current => {
+                    return current.id !== template.id;
+                });
+            },
+            cancel: () => {},
+        });
+    }
+
+    /**
+     * @param {Object} template
+     * @param {integer} template.id
+     * @param {string} template.display_name
+     */
+    async onOverwriteTemplate(template) {
+        this.env.services.dialog.add(ConfirmationDialog, {
+            body: sprintf(_t('Are your sure you want to update "%(template_name)s"?'), {
+                template_name: template.display_name,
+            }),
+            confirmLabel: _t("Update Template"),
+            confirm: async () => {
+                await this.orm.write("mail.template", [template.id], {
+                    subject: this.props.record.data.subject,
+                    body_html: this.props.record.data.body,
+                });
+            },
+            cancel: () => {},
+        });
+    }
+
     async onSaveTemplate() {
         await this.props.record.save();
         await this.action.doActionButton({
@@ -62,6 +106,38 @@ export class MailComposerTemplateSelector extends Component {
             onSelected: async templateIds => {
                 await this.props.record.update({
                     template_id: templateIds
+                });
+            },
+        });
+    }
+
+    onDeleteTemplateSearchMoreBtnClick() {
+        this.env.services.dialog.add(SelectCreateDialog, {
+            resModel: "mail.template",
+            title: _t("Delete Template"),
+            multiSelect: true,
+            noCreate: true,
+            domain: [["model", "=", this.props.record.data.render_model]],
+            onSelected: async templateIds => {
+                await this.orm.unlink("mail.template", templateIds);
+                this.state.templates = this.state.templates.filter(current => {
+                    return !templateIds.includes(current.id);
+                });
+            },
+        });
+    }
+
+    onOverwriteTemplateSearchMoreBtnClick() {
+        this.env.services.dialog.add(SelectCreateDialog, {
+            resModel: "mail.template",
+            title: _t("Overwrite Template"),
+            multiSelect: false,
+            noCreate: true,
+            domain: [["model", "=", this.props.record.data.render_model]],
+            onSelected: async templateIds => {
+                await this.orm.write("mail.template", templateIds, {
+                    subject: this.props.record.data.subject,
+                    body_html: this.props.record.data.body,
                 });
             },
         });
