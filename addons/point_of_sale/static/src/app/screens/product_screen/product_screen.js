7e67b30ea2941611f5389007e9c44e9108bda90a
Pedram (pebr) <pebr@odoo.com>
2024-09-05 11:53:35 +0000

    [FIX] point_of_sale: ensure main product appears in search with variant
    Before this commit, searching for a product with attributes that create
    variants in the PoS interface did not display the main product. This
    occurred because it was designed to hide variants and show a single
    product representing all variants, requiring the user to select options
    in a popup. However, this approach prevented the main product from
    appearing in search results when attempting to find one of its
    variants.
    
    This commit fixes the issue by adjusting the search functionality to
    ensure that the main product is displayed in the search results, even
    when searching for its variants, improving the user's ability to locate
    and select products with variants in the PoS interface.
    
    opw-4149878
    
    closes odoo/odoo#179822
    
    X-original-commit: 581d7d75a20f6ec2673d56ab051d1a3d40e0536c
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
+++ addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
@@ -312,7 +312,7 @@ export class ProductScreen extends Component {
         let list = [];
 
         if (this.searchWord !== "") {
-            list = this.getProductsBySearchWord(this.searchWord);
+            list = this.addMainProductsToDisplay(this.getProductsBySearchWord(this.searchWord));
         } else if (this.pos.selectedCategory?.id) {
             list = this.getProductsByCategory(this.pos.selectedCategory);
         } else {
@@ -343,6 +343,16 @@ export class ProductScreen extends Component {
         return fuzzyLookup(unaccent(searchWord, false), this.products, (product) =>
             unaccent(product.searchString, false)
         );
+    }
+
+    addMainProductsToDisplay(products) {
+        const uniqueProducts = new Set(products);
+        for (const product of products) {
+            if (product.id in this.pos.mainProductVariant) {
+                uniqueProducts.add(this.pos.mainProductVariant[product.id]);
+            }
+        }
+        return Array.from(uniqueProducts);
     }
 
     getProductsByCategory(category) {
