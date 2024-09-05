bfe725f1b7c237b9df58ebfe79d48fb5130034cb
jayp-odoo <jayp@odoo.com>
2024-09-05 09:20:07 +0000

    [FIX] mail: reload_on_attachment should not close attachment box
    before commit:
    
    prior to this commit after posting an attachment, if the attachment box was
    opened it closes after the reload.
    
    after commit:
    
    this commit addresses this issue by updating the behavior to ensure that the
    attachment box remains open even after the reload.
    
    task-4161477
    
    closes odoo/odoo#202463
    
    X-original-commit: 5fc7130e90890a21f7e20df061731d13d7bdad59
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Jay Patel (jayp) <jayp@odoo.com>

--- addons/mail/static/src/chatter/web/chatter_patch.js
+++ addons/mail/static/src/chatter/web/chatter_patch.js
@@ -161,7 +161,8 @@ patch(Chatter.prototype, {
                 } else {
                     this.state.showAttachmentLoading = false;
                     this.state.isAttachmentBoxOpened =
-                        this.props.isAttachmentBoxVisibleInitially && this.attachments.length > 0;
+                        this.state.isAttachmentBoxOpened ||
+                        (this.props.isAttachmentBoxVisibleInitially && this.attachments.length > 0);
                 }
                 return () => browser.clearTimeout(this.loadingAttachmentTimeout);
             },
