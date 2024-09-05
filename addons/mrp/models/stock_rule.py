12d789aa7ad16add31846326a68b408d3093e820
Nathaniel (naja) <naja@odoo.com>
2024-09-05 14:05:57 +0000

    [FIX] mrp: compute planned MO finish date at create time
    Steps
    ---
    * Create a product P with the following bom:
            * 1 component
            * 1 operation with duration = 1 hr
    * Create a MO for 10 P
    * The MO's finished move date is now instead of in 10 hours
      (inspect with shell or in db)
    
    X-original-commit: 23022f48de08ae0456292f72d1b8c9cfea2032dd

--- addons/mrp/models/stock_rule.py
+++ addons/mrp/models/stock_rule.py
@@ -169,7 +169,6 @@ class StockRule(models.Model):
             'bom_id': bom.id,
             'date_deadline': date_deadline,
             'date_start': date_planned,
-            'date_finished': fields.Datetime.from_string(values['date_planned']),
             'procurement_group_id': False,
             'propagate_cancel': self.propagate_cancel,
             'orderpoint_id': values.get('orderpoint_id', False) and values.get('orderpoint_id').id,
