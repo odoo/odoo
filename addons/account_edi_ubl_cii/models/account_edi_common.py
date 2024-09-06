4a384ec28ab11b68109d3b02b7f452adc3f20384
Seif (segn) <segn@odoo.com>
2024-09-06 16:28:43 +0300

    [IMP] account_edi_ubl_cii: Import Postal Address of partner from XML
    Previously, partner addresses were not imported from the e-invoice XML. This commit ensures that the partner's address is populated if it is missing.
    
    task-4142055
    
    closes odoo/odoo#179523
    
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/account_edi_ubl_cii/models/account_edi_common.py
+++ addons/account_edi_ubl_cii/models/account_edi_common.py
@@ -363,7 +363,7 @@ class AccountEdiCommon(models.AbstractModel):
 
         return attachments
 
-    def _import_partner(self, company_id, name, phone, email, vat, country_code=False, peppol_eas=False, peppol_endpoint=False):
+    def _import_partner(self, company_id, name, phone, email, vat, peppol_eas=False, peppol_endpoint=False, postal_address={}):
         """ Retrieve the partner, if no matching partner is found, create it (only if he has a vat and a name) """
         logs = []
         if peppol_eas and peppol_endpoint:
@@ -373,17 +373,30 @@ class AccountEdiCommon(models.AbstractModel):
         partner = self.env['res.partner'] \
             .with_company(company_id) \
             ._retrieve_partner(name=name, phone=phone, email=email, vat=vat, domain=domain)
+        country_code = postal_address.get('country_code')
+        country = self.env['res.country'].search([('code', '=', country_code.upper())]) if country_code else self.env['res.country']
+        state_code = postal_address.get('state_code')
+        state = self.env['res.country.state'].search(
+            [('country_id', '=', country.id), ('code', '=', state_code)],
+            limit=1,
+        ) if state_code and country else self.env['res.country.state']
         if not partner and name and vat:
             partner_vals = {'name': name, 'email': email, 'phone': phone}
             if peppol_eas and peppol_endpoint:
                 partner_vals.update({'peppol_eas': peppol_eas, 'peppol_endpoint': peppol_endpoint})
-            country = self.env.ref(f'base.{country_code.lower()}', raise_if_not_found=False) if country_code else False
-            if country:
-                partner_vals['country_id'] = country.id
             partner = self.env['res.partner'].create(partner_vals)
             if vat and self.env['res.partner']._run_vat_test(vat, country, partner.is_company):
                 partner.vat = vat
             logs.append(_("Could not retrieve a partner corresponding to '%s'. A new partner was created.", name))
+        if not partner.country_id and not partner.street and not partner.street2 and not partner.city and not partner.zip and not partner.state_id:
+            partner.write({
+                'country_id': country.id,
+                'street': postal_address.get('street'),
+                'street2': postal_address.get('additional_street'),
+                'city': postal_address.get('city'),
+                'zip': postal_address.get('zip'),
+                'state_id': state.id,
+            })
         return partner, logs
 
     def _import_partner_bank(self, invoice, bank_details):
