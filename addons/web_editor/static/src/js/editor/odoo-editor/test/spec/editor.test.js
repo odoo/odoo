b826920191070e762badc8e73a36a8c6f746c173
dhba <dhba@odoo.com>
2024-09-06 11:43:08 +0000

    [FIX] web_editor: deleteforward before contenteditable
    Before this commit:
    
    When an elements next sibling is a whitespace node, followed by a
    contentEditable=false element, performing the deleteforward action
    did not remove the contentEditable=false element.
    
    After this commit:
    
    When an elements next sibling is a whitespace node, followed by a
    contentEditable=false element, performing the deleteforward action will
    now remove both the whitespace and the entire contentEditable=false element.
    
    task:4058770
    
    X-original-commit: b6f78c13189e340462fb86d503f667a43912c8c9

--- addons/web_editor/static/src/js/editor/odoo-editor/test/spec/editor.test.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/test/spec/editor.test.js
@@ -490,6 +490,24 @@ X[]
                         contentAfter: `<p>[]&nbsp;def</p>`,
                     });
                 });
+                it('should remove the contentEditable false element', async () => {
+                    await testEditor(BasicEditor, {
+                        contentBefore: `<p>test[]</p>\n<div contentEditable="false"><p>abc</p></div><p>abc</p>`,
+                        stepFunction: async editor => {
+                            await deleteForward(editor);
+                        },
+                        contentAfter: `<p>test[]</p><p>abc</p>`,
+                    });
+                });
+                it('should remove whitespace and merge paragraph with heading', async () => {
+                    await testEditor(BasicEditor, {
+                        contentBefore: `<h1><strong>abc[]</strong>\n</h1><p>abc</p>`,
+                        stepFunction: async editor => {
+                            await deleteForward(editor);
+                        },
+                        contentAfter: `<h1><strong>abc</strong>[]abc</h1>`,
+                    });
+                });
             });
             describe('white spaces', () => {
                 describe('no intefering spaces', () => {
