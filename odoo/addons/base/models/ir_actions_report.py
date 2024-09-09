488f8ba0dcfb295ad849a79afdfbe25149b977f7
Thomas Lefebvre (thle) <thle@odoo.com>
2024-09-09 15:17:39 +0200

    [FIX] base: prevent wkhtmltopdf from generating device logs
    During pdf generation, `wkhtmltopdf` creates device logs via a request whose
    user agent is:
    `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.34 (KHTML, like Gecko) wkhtmltopdf Safari/534.34`.
    The result is that a `Linux - Safari` device log is created.
    
    This must be prevented (using the `_trace_disable` server-side mechanism of the session object).

--- odoo/addons/base/models/ir_actions_report.py
+++ odoo/addons/base/models/ir_actions_report.py
@@ -567,7 +567,8 @@ class IrActionsReport(models.Model):
         try:
             wkhtmltopdf = [_get_wkhtmltopdf_bin()] + command_args + files_command_args + paths + [pdf_report_path]
             process = subprocess.Popen(wkhtmltopdf, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out, err = process.communicate()
+            with request.session.no_trace():
+                _, err = process.communicate()
             err = ustr(err)
 
             if process.returncode not in [0, 1]:
