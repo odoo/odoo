6184a49016043b7a7c20aa7c47f6f2738c1ec2a6
Maximilien (malb) <malb@odoo.com>
2024-09-09 08:51:48 +0000

    [IMP] account, stock_landed_costs, web:  Invoice line display
    This commit will do multiple things:
    - Put a min-width and max width on the product label selected widget
    - Change the label of analytic distribution
    - Move landed costs before quantity and put it in optional hide
    - Fix the accepted props type of listViewWidth to accept list of two elements
    
    closes odoo/odoo#181077
    
    Task: 4170328
    X-original-commit: f269b37fb265bff3ffb899f8b8107cc447307850
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

--- addons/web/static/src/views/fields/field.js
+++ addons/web/static/src/views/fields/field.js
@@ -67,7 +67,7 @@ fieldRegistry.addValidation({
     },
     useSubView: { type: Boolean, optional: true },
     label: { type: [String, { value: false }], optional: true },
-    listViewWidth: { type: [Number, [Number, Number], Function], optional: true },
+    listViewWidth: { type: [Number, { type: Array, element: Number, validate: (array) => array.length === 2 }, Function], optional: true },
 });
 
 class DefaultField extends Component {
