778dc29247732ee07d78dcc72ce9c82805504d8b
ramh <ramh@odoo.com>
2024-09-09 18:04:59 +0530

    [IMP] hr: convert tests to hoot
    This commit aims to convert QUnit tests which rely on mail/test_utils to hoot.
    
    Part of Task-3818666

--- addons/hr/static/tests/hr_test_helpers.js
+++ addons/hr/static/tests/hr_test_helpers.js
@@ -1,5 +1,20 @@
+import { HrDepartment } from "@hr/../tests/mock_server/mock_models/hr_department";
 import { HrEmployee } from "@hr/../tests/mock_server/mock_models/hr_employee";
 import { HrEmployeePublic } from "@hr/../tests/mock_server/mock_models/hr_employee_public";
+import { M2xAvatarEmployee } from "@hr/../tests/mock_server/mock_models/m2x_avatar_employee";
 import { mailModels } from "@mail/../tests/mail_test_helpers";
+import { defineModels } from "@web/../tests/web_test_helpers";
+import { FakeUser } from "@hr/../tests/mock_server/mock_models/fake_user";
 
-export const hrModels = { ...mailModels, HrEmployee, HrEmployeePublic };
+export function defineHrModels() {
+    return defineModels(hrModels);
+}
+
+export const hrModels = {
+    ...mailModels,
+    M2xAvatarEmployee,
+    HrDepartment,
+    HrEmployee,
+    HrEmployeePublic,
+    FakeUser,
+};
