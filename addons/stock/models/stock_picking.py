29ed3702e8788f051aebc10ea4148f59cd8d9c66
Nguyễn Đại Dương <daiduongnguyen2709@gmail.com>
2024-09-10 06:34:06 +0000

    [FIX] stock: no copy on creation date
    * PROPBLEM: When duplicating a picking, the creation date is not updated
    to latest date (fields.datetime.now) instead it was taken from the copy
    value. This will be a problem if user try to search for it, they do not
    know 'creation_date' actually is because it is not present on picking
    view
    * SOLUTION: this commit add copy=False to update 'creation_date' when
    duplicating picking
    
    closes odoo/odoo#180580
    
    X-original-commit: 58e78141654585f039b8f515e1a1243e323ece98
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -449,7 +449,7 @@ class Picking(models.Model):
         help="Is late or will be late depending on the deadline and scheduled date")
     date = fields.Datetime(
         'Creation Date',
-        default=fields.Datetime.now, tracking=True,
+        default=fields.Datetime.now, tracking=True, copy=False,
         help="Creation Date, usually the time of the order")
     date_done = fields.Datetime('Date of Transfer', copy=False, readonly=True, help="Date at which the transfer has been processed or cancelled.")
     delay_alert_date = fields.Datetime('Delay Alert Date', compute='_compute_delay_alert_date', search='_search_delay_alert_date')
