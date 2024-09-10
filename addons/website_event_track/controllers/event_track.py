43589d7155ce48b682d2aca9105a2a9b509c2929
aath-odoo <aath@odoo.com>
2024-09-10 23:21:32 +0530

    [IMP] website_event_track: smoothen talks page
    Improve talks page in event frontend :
    
     - Sidebar now hides completed talks if ongoing or upcoming talks are available.
     - Automatically expand past days when filters return results in those days for
       quick access to past talk details.
     - Expose 'All Talks' and 'Favorites' shortcuts directly.
     - Keep search filter cross visible at all times.
     - Make the entire talk tile clickable, not just the title.
    
    Task-4102026

--- addons/website_event_track/controllers/event_track.py
+++ addons/website_event_track/controllers/event_track.py
@@ -14,7 +14,7 @@ import json
 import operator
 import pytz
 
-from odoo import exceptions, http, fields, tools, _
+from odoo import http, fields, tools, _
 from odoo.http import request
 from odoo.osv import expression
 from odoo.tools import is_html_empty, plaintext2html
@@ -150,12 +150,13 @@ class EventTrackController(http.Controller):
         if tracks_announced:
             tracks_announced = tracks_announced.sorted('wishlisted_by_default', reverse=True)
             tracks_by_day.append({'date': False, 'name': _('Coming soon'), 'tracks': tracks_announced})
+        # Check if there are any ongoing or upcoming tracks
+        has_upcoming_or_ongoing = any(track for track in tracks_sudo if not track.is_track_done)
 
         for tracks_group in tracks_by_day:
-            # the tracks group is folded if all tracks are done (and if it's not "today")
-            tracks_group['default_collapsed'] = (today_tz != tracks_group['date']) and all(
-                track.is_track_done and not track.is_track_live
-                for track in tracks_group['tracks']
+           tracks_group['default_collapsed'] = (
+                has_upcoming_or_ongoing and
+                tracks_group['date'] and all(track.is_track_done for track in tracks_group['tracks'])
             )
 
         # return rendering values
