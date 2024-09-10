7f6df42cd7066fc56137c62e960cb619e95c660e
Xavier Morel <xmo@odoo.com>
2024-09-10 08:57:03 +0200

    [REM] core, web: Odoo 16 deprecations
    Remove symbols and options deprecated as of Odoo 16.
    
    `odoo.web.controllers.main` is a bit dodgy as all its contents got
    deprecated but the module itself remained, so technically it's
    possible that there are dead imports lying around. Remove all the
    contents but only deprecate the module.

--- addons/web/controllers/__init__.py
+++ addons/web/controllers/__init__.py
@@ -17,4 +17,16 @@ from . import view
 from . import webclient
 from . import webmanifest
 
-from . import main  # deprecated
+
+def __getattr__(attr):
+    if attr != 'main':
+        raise AttributeError(f"Module {__name__!r} has not attribute {attr!r}.")
+
+    import sys  # noqa: PLC0415
+    mod = __name__ + '.main'
+    if main := sys.modules.get(mod):
+        return main
+
+    # can't use relative import as that triggers a getattr first
+    import odoo.addons.web.controllers.main as main  # noqa: PLC0415
+    return main
