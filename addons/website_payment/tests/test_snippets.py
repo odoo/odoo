c4418cdbf82627fa7c0dc43087de790d61c01208
Alessandro Caldonazzi (alca) <alca@odoo.com>
2024-09-11 09:16:52 +0200

    [FIX] website_payment: split test to avoid iframe issue
    On slow networks, users or runbot may click checkboxes before the
    JavaScript is fully loaded by the lazy loader, causing event handlers
    to not be attached.
    
    While test tours are supposed to be robust enough to wait for that, it
    was decided to split the test into two separate tours, as testing the
    donation form use from a non-admin is also better. So: one part while
    logged in as admin for donation configuration, and another while logged
    in as portal user to test it (we use a portal user so that the form
    still autocompletes with base user information).
    
    runbot-77224
    
    X-original-commit: e1162d232707896431a26b2048377bc8bb187f0e
    Part-of: odoo/odoo#210238
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/website_payment/tests/test_snippets.py
+++ addons/website_payment/tests/test_snippets.py
@@ -1,12 +1,9 @@
-import odoo
-import odoo.tests
-import logging
-
-_logger = logging.getLogger(__name__)
+from odoo.tests.common import tagged
+from odoo.addons.base.tests.common import HttpCaseWithUserPortal
 
 
-@odoo.tests.common.tagged('post_install', '-at_install')
-class TestSnippets(odoo.tests.HttpCase):
+@tagged('post_install', '-at_install')
+class TestSnippets(HttpCaseWithUserPortal):
 
     def test_01_donation(self):
         payment_demo = self.env['ir.module.module']._get('payment_demo')
@@ -15,11 +12,20 @@ class TestSnippets(odoo.tests.HttpCase):
 
         demo_provider = self.env['payment.provider'].search([('code', '=', "demo")])
         demo_provider.write({'state': 'test'})
+
+        belgium = self.env.ref('base.be')
+
         self.env.ref('base.user_admin').write({
-            'country_id': self.env.ref('base.be').id,
+            'country_id': belgium.id,
             'email': 'mitchell.admin@example.com',
         })
         self.env.company.write({
             'email': 'no-reply@company.com',
         })
+
+        self.user_portal.country_id = belgium.id
+
         self.start_tour("/?enable_editor=1", "donation_snippet_edition", login='admin')
+        self.start_tour("/", "donation_snippet_use", login="portal")
+        self.start_tour("/?enable_editor=1", "donation_snippet_edition_2", login='admin')
+        self.start_tour("/", "donation_snippet_use_2", login="portal")
