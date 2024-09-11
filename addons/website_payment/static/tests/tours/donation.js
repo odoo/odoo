1fcd3d5e923d93c0ce8b54800292e04b083d4f52
Alessandro Caldonazzi (alca) <alca@odoo.com>
2024-09-11 07:16:52 +0000

    [FIX] website_payment: split test to avoid iframe issue
    On slow networks, users or runbot may click checkboxes before the
    JavaScript is fully loaded by the lazy loader, causing event handlers
    to not be attached.
    
    While test tours are supposed to be robust enough to wait for that, it
    was decided to split the test into two separate tours, as testing the
    donation form use from a non-admin is also better. So: one part while
    logged in as admin for donation configuration, and another while logged
    in as portal user to test it (we use a portal user so that the form
    still autocompletes with base user information).
    
    runbot-77224
    
    X-original-commit: c4418cdbf82627fa7c0dc43087de790d61c01208
    Part-of: odoo/odoo#210494
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/website_payment/static/tests/tours/donation.js
+++ addons/website_payment/static/tests/tours/donation.js
@@ -1,5 +1,5 @@
+import { registry } from "@web/core/registry";
 import {
-    clickOnEditAndWaitEditMode,
     clickOnSave,
     registerWebsitePreviewTour,
     insertSnippet,
@@ -19,94 +19,103 @@ registerWebsitePreviewTour(
             groupName: "Contact & Forms",
         }),
         ...clickOnSave(),
+    ],
+);
+
+registry.category('web_tour.tours').add('donation_snippet_use', {
+    url: '/',
+    steps: () => [
         // -- Testing the minimum amount --
         {
             content: "Enter a negative custom amount, testing the minimum amount",
-            trigger: ":iframe #s_donation_amount_input",
+            trigger: "#s_donation_amount_input",
             run: "edit 1",
         },
         {
             content: "Donate with custom amount set",
-            trigger: ":iframe .s_donation_donate_btn",
+            trigger: ".s_donation_donate_btn",
             run: "click",
         },
         {
             content: "Check if alert-danger element exists",
-            trigger: ":iframe p.alert-danger",
+            trigger: "p.alert-danger",
         },
         // -- End of testing the minimum amount --
         {
             content: "Enter a custom amount",
-            trigger: ":iframe #s_donation_amount_input",
+            trigger: "#s_donation_amount_input",
             run: "edit 55",
         },
         {
             content: "Donate with custom amount set",
-            trigger: ":iframe .s_donation_donate_btn",
+            trigger: ".s_donation_donate_btn",
             run: "click",
         },
         {
             content: "Check if custom amount radio input is selected",
-            trigger: ":iframe input#other_amount:checked",
+            trigger: "input#other_amount:checked",
         },
         {
             content: "Check if custom amount radio input has value 55",
-            trigger: ':iframe input#other_amount[value="55.0"]',
+            trigger: 'input#other_amount[value="55.0"]',
         },
         {
             content: "Select the amount of 25",
-            trigger: ":iframe input#amount_1",
+            trigger: "input#amount_1",
             run: "click",
         },
         {
             content: "Verify that amount_1 is checked",
-            trigger: ":iframe input#amount_1:checked",
+            trigger: "input#amount_1:checked",
         },
         {
             content: "Verify that other_amount is not checked",
-            trigger: ":iframe input#other_amount:not(:checked)",
+            trigger: "input#other_amount:not(:checked)",
         },
         {
             content: "Click on the custom amount radio button",
-            trigger: ":iframe input#other_amount",
+            trigger: "input#other_amount",
             run: "click",
         },
         {
             content: "Submit the donation form",
-            trigger: ":iframe button[name='o_payment_submit_button']",
+            trigger: "button[name='o_payment_submit_button']",
             run: "click",
         },
         {
             content: "Check if the warning message is displayed",
-            trigger: ":iframe .o_donation_payment_form:has(small#warning_message_id)",
+            trigger: ".o_donation_payment_form:has(small#warning_message_id)",
         },
         {
             content: "Enter an amount less than the minimum value",
-            trigger: ":iframe input#other_amount_value",
+            trigger: "input#other_amount_value",
             run: "edit 1",
         },
         {
             content:
                 "Verify whether the minimum value warning message is displayed while other warning messages remain hidden",
             trigger:
-                ":iframe small#warning_min_message_id:contains('The minimum donation amount is $5.'), :iframe .o_donation_payment_form:has(small#warning_message_id.d-none)",
+                "small#warning_min_message_id:contains('The minimum donation amount is $5.'), .o_donation_payment_form:has(small#warning_message_id.d-none)",
         },
         {
             content: "Click on the first radio button",
-            trigger: ":iframe input[name='o_donation_amount']:first-child",
+            trigger: "input[name='o_donation_amount']:first-child",
             run: "click",
         },
         {
             content: "Ensure the custom amount value is cleared",
-            trigger:
-                ":iframe input#other_amount_value:empty, :iframe #warning_min_message_id.d-none",
+            trigger: "input#other_amount_value:empty, #warning_min_message_id.d-none",
         },
-        {
-            content: "Go to home page",
-            trigger: ":iframe a[href='/'].nav-link",
-            run: "click",
-        },
-        ...clickOnEditAndWaitEditMode(),
+    ],
+});
+
+registerWebsitePreviewTour(
+    "donation_snippet_edition_2",
+    {
+        url: "/",
+        edition: true,
+    },
+    () => [
         {
             content: "Click on 'Custom Amount' button",
             trigger: ":iframe .s_donation_donate_btn",
@@ -115,45 +124,51 @@ registerWebsitePreviewTour(
         changeOption("Donation", "we-toggler"),
         changeOption("Donation", '[data-name="slider_opt"]'),
         ...clickOnSave(),
+    ],
+);
+
+registry.category('web_tour.tours').add('donation_snippet_use_2', {
+    url: '/',
+    steps: () => [
         {
             content: "Click on $10 button",
-            trigger: ":iframe .s_donation_btn_description button",
+            trigger: ".s_donation_btn_description button",
             run: "click",
         },
         {
             content: "Donate using the selected amount",
-            trigger: ":iframe .s_donation_donate_btn",
+            trigger: ".s_donation_donate_btn",
             run: "click",
         },
         {
             content: "Click on the 'Amount to donate' input field",
-            trigger: ":iframe input#other_amount_value",
+            trigger: "input#other_amount_value",
             run: "click",
         },
         {
             content: "Change custom amount to 67",
-            trigger: ":iframe input[name='o_donation_amount'][type='number']",
+            trigger: "input[name='o_donation_amount'][type='number']",
             run: "edit 67",
         },
         {
             content: "Select the custom amount radio button",
-            trigger: ":iframe input#other_amount",
+            trigger: "input#other_amount",
             run: "click",
         },
         {
             content: "Submit the donation form",
-            trigger: ":iframe button[name='o_payment_submit_button']",
+            trigger: "button[name='o_payment_submit_button']",
             run: "click",
         },
         {
-            trigger: ":iframe body:contains(Your payment has been processed.)",
+            trigger: "body:contains(Your payment has been processed.)",
         },
         {
             content: "Verify that the amount displayed is 67",
-            trigger: ':iframe span.oe_currency_value:contains("67.00")',
+            trigger: 'span.oe_currency_value:contains("67.00")',
         },
         {
-            trigger: ":iframe [name=o_payment_status_alert]:contains(thank you!)",
+            trigger: "[name=o_payment_status_alert]:contains(thank you!)",
         },
-    ]
-);
+    ],
+});
