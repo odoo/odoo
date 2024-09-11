491bee728968d3e471da510c761a608ed50bc39d
Pierre Paridans <app@odoo.com>
2024-09-11 10:46:05 +0000

    [IMP] web: list selection box on mobile
    Ths List view allows to select one or multiple records. While doing so,
    an indicator is displayed, showing how much records are selected and
    providing actions to perform on them.
    
    On regular computer's screen, the central part of the ControlPanel is
    used to display this indicator and its related buttons, hiding the
    SearchBar. On smaller screens, all those visual elements take a lot of
    space, easily triggering an horizontal scrolling and taking an
    additional line in the ControlPanel, which push the records a bit down
    on the screen.
    
    This commit reuses the CogMenu paradigm to group all the actions/buttons
    in a single element. To avoid the records being pushed down, the
    selection indicator is transformed into a floating "Toast" as an overlay
    to the Navbar (similar to what was previously done to the Pager).
    
    task-3336242
    
    closes odoo/odoo#182756
    
    X-original-commit: 3dde51951d1164cfaa534d6640c31b83d7cf4d5d
    Signed-off-by: Romeo Fragomeli (rfr) <rfr@odoo.com>
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

---
+++ addons/web/static/src/views/list/list_cog_menu.js
@@ -0,0 +1,13 @@
+import { CogMenu } from "../../search/cog_menu/cog_menu";
+
+export class ListCogMenu extends CogMenu {
+    static template = "web.ListCogMenu";
+    static props = {
+        ...CogMenu.props,
+        hasSelectedRecords: { type: Number, optional: true },
+        slots: { type: Object, optional: true },
+    };
+    _registryItems() {
+        return this.props.hasSelectedRecords ? [] : super._registryItems();
+    }
+}
