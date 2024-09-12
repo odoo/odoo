fe6a3d127963de4106efb79266480d1a09099ab8
lase@odoo.com <lase@odoo.com>
2024-09-12 13:58:10 +0000

    [FIX] stock: unreserve backwards on qty decrease
    Steps to reproduce:
    
    - Create a product tracked by lot and register 4 lots of 10 units in
      stock : LOT001, LOT002, LOT003, LOT004
    - Inventory > Configuration > Products > Product Categories
    - Change the Removal strategy of the "All" category to LIFO
    - Create and confirm a delivery order for 40 units of your product
    > Move lines are created for reservation in order LOT003, LOT002, LOT001
    - Do not click on the detailed operation to see these LOTS and decrease
      the quantity of the move to 15 units
    
    Expected behavior:
    
    Since the lines were created respecting the removal strategy, they
    should be removed accordingly from the last one created to the first
    created > LOT001 and LOT002 should be removed and 5 units should be
    decreased from LOT003
    
    Current behavior:
    
    LOT004 and LOT004 are removed and 5 units is removed from LOT002
    
    Cause of the issue:
    
    On qty decrease, the `_process_decrease` of the `set_quantity` is called
    to decrease the qties on the associated sml and to unlink the one
    resulting with a 0 qty. However, the loop used to make this action is
    not based on the reversed order of creation of the lines but on their
    order itself.
    
    Note:
    
    This does not solve the issue for complex removal strategy that would
    require a complete recompute of the reservation on qty decrease such as
    "use the minimal amout of packages".
    
    Note 2:
    
    An other issue exists when you increase the demand of the move instead
    of deacresing it: while the qty done is increased no assigments are done
    so that a move line without lot id would be created to fulfill the
    increase of qty. This is because the `_action_assign` can not be
    triggered again in this process increase since the stockpocalypse the
    line was even commented here:
    https://github.com/odoo/odoo/blob/9f364cde276b8b3be73c30617515bd55a9f25aba/addons/stock/models/stock_move.py#L376-L378
    he reason is that if an `_action_assign` was performed instead and if
    the units were not in stock to fullfill it the qty would only be
    increased by the available qty and the desired flow would be bloqued.
    THe behavior is being improved in master (18.0+) but the required change
    of the `_process_increase` is to risky to be applied in stable.
    
    opw-4074174
    opw-4071479
    
    co-authored by @naja628
    
    closes odoo/odoo#180349
    
    X-original-commit: 98de31995b533a1a6d302b42982d48adde97c56d
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -394,7 +394,9 @@ class StockMove(models.Model):
     def _set_quantity(self):
         def _process_decrease(move, quantity):
             mls_to_unlink = set()
-            for ml in move.move_line_ids:
+            # Since the move lines might have been created in a certain order to respect
+            # a removal strategy, they need to be unreserved in the opposite order
+            for ml in reversed(move.move_line_ids.sorted('id')):
                 if float_is_zero(quantity, precision_rounding=move.product_uom.rounding):
                     break
                 qty_ml_dec = min(ml.quantity, ml.product_uom_id._compute_quantity(quantity, ml.product_uom_id, round=False))
