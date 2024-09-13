d422267c6d30aeeebb83a4d1969a512d78f0c3b3
jeep-odoo <jeep@odoo.com>
2024-09-13 15:46:02 +0530

    [IMP] sale_management: improve SOL description logic
    Before:
    When a product is added to a quotation template, the description is generated
    based on the current product name and sale description and then saved on the
    template. This means that if the product sales description changes in the
    future, it will not be updated on the template. As a result, any new sales order
    created from the template will have the old version of the description.
    
    After:
    - By default, the description in the quotation template remains empty.
    - When creating a sale order line (SOL) from the template, the product
      description is computed dynamically using the current product name
      and sales description.
    - If a custom sales description is provided in the template, then it will be
      used in the SOL instead of computing the new one.
    
    task-4169433
    
    closes odoo/odoo#180206
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale_management/models/sale_order_line.py
+++ addons/sale_management/models/sale_order_line.py
@@ -17,7 +17,9 @@ class SaleOrderLine(models.Model):
         for line in self:
             if line.product_id and line.order_id.sale_order_template_id and line._use_template_name():
                 for template_line in line.order_id.sale_order_template_id.sale_order_template_line_ids:
-                    if line.product_id == template_line.product_id:
+                    if line.product_id == template_line.product_id and template_line.name:
+                        # If a specific description was set on the template, use it
+                        # Otherwise the description is handled by the super call
                         lang = line.order_id.partner_id.lang
                         line.name = template_line.with_context(lang=lang).name + line.with_context(lang=lang)._get_sale_order_line_multiline_description_variants()
                         break
