9968e4acd9c5c515c7ad657f8985f70f13bd8e5b
roen-odoo <roen@odoo.com>
2024-09-13 09:36:52 +0000

    [FIX] barcodes: remove usage of jQuery in barcode service
    On chrome mobile, when going in the pos self order and trying to do a
    search on the products you would get a traceback. This was happening
    because jQuery was not defined.
    
    Steps to reproduce:
    -------------------
    * Install pos_self_order and barcodes
    * Open the PoS on a chrome mobile device (use browserstack)
    * Try to do a search on the products page
    > Observation: You get a traceback
    
    Why the fix:
    ------------
    jQuery is not always defined, so we remove the use of jQuery.
    
    opw-4107823
    
    X-original-commit: a0ab0ce39bf4cba5e90eae109bcd006b39e31c6c

--- addons/barcodes/static/src/barcode_service.js
+++ addons/barcodes/static/src/barcode_service.js
@@ -110,8 +110,12 @@ export const barcodeService = {
             if (ev.key === "Unidentified") {
                 return;
             }
-            if (document.activeElement && !document.activeElement.matches('input:not([type]), input[type="text"], textarea, [contenteditable], ' +
-                '[type="email"], [type="number"], [type="password"], [type="tel"], [type="search"]')) {
+            if (
+                !document.activeElement.matches(
+                    'input, [type="text"], textarea, [contenteditable], ' +
+                        '[type="email"], [type="number"], [type="password"], [type="tel"], [type="search"]'
+                )
+            ) {
                 barcodeInput.focus();
             }
             keydownHandler(ev);
