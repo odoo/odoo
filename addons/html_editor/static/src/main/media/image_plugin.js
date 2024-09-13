29d9ddbc9c298cddf22fbdb7dbd67a09e64209a4
adch-odoo <adch@odoo.com>
2024-09-13 18:15:39 +0530

    [FIX] html_editor: traceback on escape to discard image transformation
    Steps to Reproduce:
    
    - Open the to-do app
    - Add an image (/image, upload an image)
    - Select the image
    - Click the Transform button
    - Press the Escape key, and a traceback occurs
    
    Behavior after this commit:
    
    - Clicking the image transform button in `toolbar` clears
    the image selection and enables the image transformation.
    - Pressing the Escape key discards the image transformation.
    
    task-4184571

--- addons/html_editor/static/src/main/media/image_plugin.js
+++ addons/html_editor/static/src/main/media/image_plugin.js
@@ -294,7 +294,11 @@ export class ImagePlugin extends Plugin {
         }
     }
 
-    onSelectionChange() {
+    onSelectionChange(selectionData) {
+        const { anchorNode, focusNode } = selectionData.documentSelection;
+        if (!anchorNode && !focusNode) {
+            return;
+        }
         this.closeImageTransformation();
     }
 
@@ -358,10 +362,12 @@ export class ImagePlugin extends Plugin {
             props: {
                 image,
                 document: this.document,
-                destroy: this.closeImageTransformation,
+                destroy: () => this.closeImageTransformation(),
                 onChange: () => this.dispatch("ADD_STEP"),
             },
         });
+        const selection = this.document.getSelection();
+        selection && selection.removeAllRanges();
     }
 
     isImageTransformationOpen() {
