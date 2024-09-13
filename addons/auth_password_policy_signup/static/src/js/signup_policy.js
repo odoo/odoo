136b0ab59b73ee60defec771f24f6413e6b2c7ef
Florent de Labarre <florent.mirieu@gmail.com>
2024-09-13 06:34:55 +0000

    [FIX] auth_password_policy_signup: error on reset page
    On mobile go to the reset password page.
    A javascript error appear.
    "TypeError: null is not an object (evaluating 'password.getAttribute')"
    
    opw-4184038
    
    X-original-commit: 9ed3d2b184ab75b1ea6e60c7fcea5a4464b8ed60
    Co-authored-by: Pierre Paridans <pierre@paridans.org>

---
+++ addons/auth_password_policy_signup/static/src/js/signup_policy.js
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+/** @odoo-module */
+
+import "web.dom_ready";
+import { ConcretePolicy, recommendations } from "@auth_password_policy/password_policy";
+import PasswordMeter from "@auth_password_policy_signup/js/password_meter";
+
+const signupForm = document.querySelector('.oe_signup_form, .oe_reset_password_form');
+if (signupForm) {
+    const password = document.querySelector("[type=password][minlength]");
+    const minlength = password ? Number(password.getAttribute("minlength")) : NaN;
+    if (!isNaN(minlength)) {
+        const meter = new PasswordMeter(null, new ConcretePolicy({minlength}), recommendations);
+        meter.insertAfter(password);
+        password.addEventListener("input", (e) => {
+            meter.update(e.target.value);
+        });
+    }
+}
+
+>>>>>>> FORWARD PORTED
