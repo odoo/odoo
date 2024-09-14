0349e64e0042c5bde9a8aed7a4d384eb4b16eb03
Yoshi Tashiro <tashiro@quartile.co>
2024-09-14 04:34:50 +0000

    [REF] stock_account: add _get_qty_taken_on_candidate()
    This change allows us to practically revive the 'real price' costing
    method via customization, specifically using the
    stock_valuation_fifo_lot OCA module. By making qty_taken_on_candidate
    adjustable in the _run_fifo() method, we can fine-tune the FIFO
    calculation to better suit real price costing needs.
    
    closes odoo/odoo#181629
    
    X-original-commit: 9e21846bb3fc09e679f281ec0f66e65134d48ca6
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -336,6 +336,9 @@ class ProductProduct(models.Model):
         candidates_domain = self._get_fifo_candidates_domain(company)
         return self.env["stock.valuation.layer"].sudo().search(candidates_domain)
 
+    def _get_qty_taken_on_candidate(self, qty_to_take_on_candidates, candidate):
+        return min(qty_to_take_on_candidates, candidate.remaining_qty)
+
     def _run_fifo(self, quantity, company):
         self.ensure_one()
 
@@ -345,7 +348,7 @@ class ProductProduct(models.Model):
         new_standard_price = 0
         tmp_value = 0  # to accumulate the value taken on the candidates
         for candidate in candidates:
-            qty_taken_on_candidate = min(qty_to_take_on_candidates, candidate.remaining_qty)
+            qty_taken_on_candidate = self._get_qty_taken_on_candidate(qty_to_take_on_candidates, candidate)
 
             candidate_unit_cost = candidate.remaining_value / candidate.remaining_qty
             new_standard_price = candidate_unit_cost
