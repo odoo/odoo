13a64a4a9f1a7460cff3298876d7f376145b5dcc
reka-odoo <reka@odoo.com>
2024-09-16 12:56:15 +0000

    [FIX] website_sale_loyalty: prevent error when applying discount code in order
    When user tries to apply a discount code in order,
    a traceback will appear.
    
    Steps to reproduce the error:
    - Install "website_sale_loyalty"
    - Activate "Demo" payment provider
    - Go to Website > Shop > Add a product to cart > View cart
    - Pay with Demo > Pay
    - Click the back button
    - Apply Coupon code
    
    Error: A traceback appears:
    "ValueError: Expected singleton: sale.order()"
    
    https://github.com/odoo/odoo/blob/9a9063a2d1f385b02f8453b766ec225def531d7e/addons/website_sale_loyalty/controllers/main.py#L16
    When user clicks the back button and applies the coupon, "order" will be empty,
    So, it will lead to the above traceback.
    
    sentry-5856780654
    
    X-original-commit: c16ec37c777d82072d2bcfe9f525013dd8cd78ee

--- addons/website_sale_loyalty/controllers/main.py
+++ addons/website_sale_loyalty/controllers/main.py
@@ -14,6 +14,8 @@ class WebsiteSale(main.WebsiteSale):
     @route()
     def pricelist(self, promo, **post):
         order = request.website.sale_get_order()
+        if not order:
+            return request.redirect('/shop')
         coupon_status = order._try_apply_code(promo)
         if coupon_status.get('not_found'):
             return super().pricelist(promo, **post)
