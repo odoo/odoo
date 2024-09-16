e5f02608a0a2243db436b2cba4dbe1a6904d3c2e
st-yes <soty@odoo.com>
2024-09-16 14:47:08 +0200

    [FIX] website_forum: ensure forum description teaser translation
    On the forum page, the forum's description teaser fails to get translated.
    
    Steps to reproduce:
    1. Navigate to the website.
    2. Go to the forum page.
    3. change the website language
    
    OPW-4143090

--- addons/website_forum/models/forum.py
+++ addons/website_forum/models/forum.py
@@ -4,6 +4,7 @@
 import logging
 import math
 import re
+import textwrap
 
 from datetime import datetime
 
@@ -48,6 +49,7 @@ class Forum(models.Model):
     faq = fields.Html('Guidelines', translate=html_translate, sanitize=True, sanitize_overridable=True)
     description = fields.Text('Description', translate=True)
     teaser = fields.Text('Teaser', compute='_compute_teaser', store=True)
+    teaser_non_stored = fields.Text('Teaser Non Stored', compute='_compute_teaser_non_stored')
     welcome_message = fields.Html(
         'Welcome Message',
         translate=True,
@@ -142,14 +144,16 @@ class Forum(models.Model):
     @api.depends('description')
     def _compute_teaser(self):
         for forum in self:
-            if forum.description:
-                desc = forum.description.replace('\n', ' ')
-                if len(forum.description) > 180:
-                    forum.teaser = desc[:180] + '...'
-                else:
-                    forum.teaser = forum.description
-            else:
-                forum.teaser = ""
+            forum.teaser = forum._generate_teaser()
+
+    @api.depends_context('lang')
+    @api.depends('description')
+    def _compute_teaser_non_stored(self):
+        for forum in self:
+            forum.teaser_non_stored = forum._generate_teaser()
+
+    def _generate_teaser(self):
+        return textwrap.shorten(self.description, 180) if self.description else ""
 
     @api.depends('post_ids.state', 'post_ids.views', 'post_ids.child_count', 'post_ids.favourite_count')
     def _compute_forum_statistics(self):
