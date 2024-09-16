996c48eb3fa1e25409ce6a100b201ce01c40e0a4
Levi Siuzdak (sile) <sile@odoo.com>
2024-09-16 11:37:35 +0000

    [FIX] website_sale_loyalty: remove erroneous `else`
    Versions
    --------
    - 17.0+
    
    Note: For 17.4+, I had to add a value provided by the delivery
    controller in order to fix this bug, as well as fix a regression caused
    by its removal in a1e58e9d9a11
    
    Steps
    -----
    1. Create a coupon with free shipping reward;
    2. create an eWallet with enough points for a free purchase;
    3. create at least 2 shipping methods with different prices;
    4. in eCommerce, add a product to cart, and go to checkout;
    5. apply coupon;
    6. go to payment & apply eWallet;
    7. switch between shipping methods.
    
    Issue
    -----
    The eWallet amount on the right doesn't get updated after changing
    shipping costs.
    
    Cause
    -----
    Commit e08449e42a01 allowed for eWallet to update after changes, using
    a `_handleCarrierUpdateResultBadge` override.
    
    Commit 453c6169e080 merged the `_handleCarrierUpdateResult` &
    `_handleCarrierUpdateResultBadge` methods. Because of an `else` that was
    left in by accident, it either displays a shipping discount *or*
    it updates the eWallet.
    
    Solution
    --------
    Remove the stray `else` so that the conditional branches are no longer
    mutually exclusive.
    
    opw-4150258
    
    X-original-commit: 49f7f7beb72945ea4902350aa6f564ff6269afbc

--- addons/website_sale_loyalty/controllers/delivery.py
+++ addons/website_sale_loyalty/controllers/delivery.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from functools import partial
+
 from odoo.http import request
 
 from odoo.addons.website_sale.controllers.delivery import Delivery
@@ -8,12 +10,18 @@ from odoo.addons.website_sale.controllers.delivery import Delivery
 class WebsiteSaleLoyaltyDelivery(Delivery):
 
     def _order_summary_values(self, order, **post):
-        Monetary = request.env['ir.qweb.field.monetary']
+        to_html = partial(
+            request.env['ir.qweb.field.monetary'].value_to_html,
+            options={'display_currency': order.currency_id},
+        )
         res = super()._order_summary_values(order, **post)
         free_shipping_lines = order._get_free_shipping_lines()
-        if free_shipping_lines:
-            res['amount_delivery_discounted'] = Monetary.value_to_html(
-                sum(free_shipping_lines.mapped('price_subtotal')),
-                {'display_currency': order.currency_id}
-            )
+        shipping_discount = sum(free_shipping_lines.mapped('price_subtotal'))
+        if shipping_discount:
+            res['amount_delivery_discounted'] = to_html(shipping_discount)
+        res['discount_reward_amounts'] = [
+            to_html(line.price_subtotal)
+            for line in order.order_line
+            if line.reward_id.reward_type == 'discount'
+        ]
         return res
--- addons/website_sale_loyalty/static/tests/tours/test_website_sale_free_shipping_discount_line.js
+++ addons/website_sale_loyalty/static/tests/tours/test_website_sale_free_shipping_discount_line.js
@@ -22,20 +22,16 @@ registry.category("web_tour.tours").add('check_shipping_discount', {
             trigger: '#product_detail form[action^="/shop/cart/update"] #add_to_cart',
             run: "click",
         },
-        wsTourUtils.goToCart({quantity: 3}),
+        wsTourUtils.goToCart({ quantity: 3 }),
+        wsTourUtils.goToCheckout(),
         {
-            content: "go to checkout",
-            trigger: 'a[name="website_sale_main_button"]',
-            run: "click",
-        },
-        {
-            content: "select delivery with rule",
-            trigger: 'li label:contains("delivery with rule")',
+            content: "select delivery2",
+            trigger: "li label:contains(delivery2)",
             run: "click",
         },
         {
             content: "check if delivery price is correct'",
-            trigger: "[name='o_delivery_method']:has(.o_delivery_carrier_label:contains('delivery with rule')) span.o_wsale_delivery_price_badge .oe_currency_value:contains(100.00)",
+            trigger: "[name=o_delivery_method]:has(label:contains(delivery2)) .oe_currency_value:contains(10.00)",
         },
         {
             content: "confirm shipping method",
@@ -43,16 +39,48 @@ registry.category("web_tour.tours").add('check_shipping_discount', {
             run: "click",
         },
         {
+            content: "open cart overview",
             trigger: ".accordion-button",
             run: "click",
         },
         {
-            content: "check if delivery price is correct'",
-            trigger: "#order_delivery .oe_currency_value:contains(100.00)",
+            content: "check if delivery price is correct",
+            trigger: "#order_delivery .oe_currency_value:contains(10.00)",
         },
         {
-            content: "check if delivery price is correct'",
-            trigger: "[data-reward-type='shipping'] .oe_currency_value:contains('- 75.00')",
+            content: "check if shipping discount is correct",
+            trigger: "[data-reward-type=shipping] .oe_currency_value:contains(- 6.00)",
+        },
+        {
+            content: "pay with eWallet",
+            trigger: "a.btn-primary[role=button]:contains(Pay with eWallet)",
+            run: 'click',
+        },
+        {
+            content: "wait for accordion to collapse, then reopen",
+            trigger: ".accordion-button.collapsed",
+            run: 'click',
+        },
+        {
+            content: "check eWallet discount",
+            trigger: "[data-reward-type=discount] .oe_currency_value:contains(- 349.60)",
+        },
+        {
+            content: "select delivery1",
+            trigger: "li label:contains(delivery1)",
+            run: 'click',
+        },
+        {
+            content: "check for eWallet update after shipping cost change",
+            trigger: "[data-reward-type=discount] .oe_currency_value:contains(- 345.00)",
+        },
+        {
+            content: "check if new delivery price is correct",
+            trigger: "[name=o_delivery_method]:has(label:contains(delivery1)) .oe_currency_value:contains(5.00)",
+        },
+        {
+            content: "check if new shipping discount is correct",
+            trigger: "[data-reward-type=shipping] .oe_currency_value:contains(- 5.00)",
         },
     ],
 });
