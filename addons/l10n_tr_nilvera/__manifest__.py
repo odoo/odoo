5119c2d02b3541f2dbe274a9599ceeed1cabb67f
Ali Alfie (alal) <alal@odoo.com>
2024-09-16 10:52:42 +0200

    [ADD] l10n_tr_nilvera: base for all nilvera modules
    Contains all the basic things needed for other Nilvera modules like a
    client for contacting Nilvera and environment settings.
    
    task-3952234

---
+++ addons/l10n_tr_nilvera/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Türkiye - Nilvera',
+    'version': '1.0',
+    'category': 'Accounting/Accounting',
+    'description': """
+Base module containing core functionalities required by other Nilvera modules.
+    """,
+    'depends': ['l10n_tr'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_config_settings_views.xml',
+        'views/res_partner_views.xml',
+    ],
+    'license': 'OEEL-1',
+}
