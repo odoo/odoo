3acc7a77f0accb3d6b90f6043a6ca6bc1f82cd20
Levi Siuzdak (sile) <sile@odoo.com>
2024-09-16 11:37:35 +0000

    [FIX] website_sale_loyalty: remove erroneous `else`
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Create a coupon with free shipping reward;
    2. create an eWallet with enough points for a free purchase;
    3. create at least 2 shipping methods with different prices;
    4. in eCommerce, add a product to cart, and go to checkout;
    5. apply coupon;
    6. go to payment & apply eWallet;
    7. switch between shipping methods.
    
    Issue
    -----
    The eWallet amount on the right doesn't get updated after changing
    shipping costs.
    
    Cause
    -----
    Commit e08449e42a01 allowed for eWallet to update after changes, using
    a `_handleCarrierUpdateResultBadge` override.
    
    Commit 453c6169e080 merged the `_handleCarrierUpdateResult` &
    `_handleCarrierUpdateResultBadge` methods. Because of an `else` that was
    left in by accident, it either displays a shipping discount *or*
    it updates the eWallet.
    
    Solution
    --------
    Remove the stray `else` so that the conditional branches are no longer
    mutually exclusive.
    
    opw-4150258
    
    X-original-commit: 49f7f7beb72945ea4902350aa6f564ff6269afbc

--- addons/website_sale_loyalty/controllers/delivery.py
+++ addons/website_sale_loyalty/controllers/delivery.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from functools import partial
 
 from odoo.http import request
 
@@ -8,12 +9,15 @@ from odoo.addons.website_sale.controllers.delivery import Delivery
 class WebsiteSaleLoyaltyDelivery(Delivery):
 
     def _order_summary_values(self, order, **post):
-        Monetary = request.env['ir.qweb.field.monetary']
+        to_html = partial(
+            request.env['ir.qweb.field.monetary'].value_to_html,
+            options={'display_currency': order.currency_id},
+        )
         res = super()._order_summary_values(order, **post)
         free_shipping_lines = order._get_free_shipping_lines()
-        if free_shipping_lines:
-            res['amount_delivery_discounted'] = Monetary.value_to_html(
-                sum(free_shipping_lines.mapped('price_subtotal')),
-                {'display_currency': order.currency_id}
-            )
+        shipping_discount = sum(free_shipping_lines.mapped('price_subtotal'))
+        if shipping_discount:
+            res['amount_delivery_discounted'] = to_html(shipping_discount)
+        if order.reward_amount:
+            res['amount_order_discounted'] = to_html(order.reward_amount - shipping_discount)
         return res
--- addons/website_sale_loyalty/static/src/js/checkout.js
+++ addons/website_sale_loyalty/static/src/js/checkout.js
@@ -9,11 +9,19 @@ websiteSaleCheckout.include({
         this._super.apply(this, arguments);
         if (result.amount_delivery_discounted) {
             // Update discount of the order
-            const cart_summary_discount_line = document.querySelector(
+            const cart_summary_shipping_reward = document.querySelector(
                 '[data-reward-type="shipping"]'
             )
-            if (cart_summary_discount_line) {
-                cart_summary_discount_line.innerHTML = result.amount_delivery_discounted;
+            if (cart_summary_shipping_reward) {
+                cart_summary_shipping_reward.innerHTML = result.amount_delivery_discounted;
+            }
+        }
+        if (result.amount_order_discounted) {
+            const cart_summary_discount_reward = document.querySelector(
+                "[data-reward-type=discount]"
+            );
+            if (cart_summary_discount_reward) {
+                cart_summary_discount_reward.innerHTML = result.amount_order_discounted;
             }
         }
     },
