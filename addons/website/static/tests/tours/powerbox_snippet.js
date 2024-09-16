594a038864da9bbc16bf9e558ddd8778fafa4ccd
risr-odoo <risr@odoo.com>
2024-09-16 10:26:21 +0000

    [IMP] web_editor, website: remove banner from website
    This commit removes four banner-related commands: Banner Info, Banner
    Success, Banner Warning, and Banner Danger. These commands have been
    removed in favor of using the Alert command, which offers the same
    functionality through a fully customizable Alert snippet. Now, when
    users search for terms like 'Banner', 'Info', 'Success', 'Warning' or
    'Danger', the Alert command will appear in the Powerbox instead of the
    previous banner commands.
    
    task-3572344
    
    closes odoo/odoo#206629
    
    X-original-commit: 6fecc5511345ef9aa368ebaa419636b4e144444d
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/website/static/tests/tours/powerbox_snippet.js
+++ addons/website/static/tests/tours/powerbox_snippet.js
@@ -42,3 +42,91 @@ registerWebsitePreviewTour("website_powerbox_snippet",{
     trigger: ":iframe .s_alert",
 }
 ]);
+
+registerWebsitePreviewTour(
+    "website_powerbox_keyword",
+    {
+        edition: true,
+    },
+    () => [
+        ...insertSnippet({
+            id: "s_text_block",
+            name: "Text",
+            groupName: "Text",
+        }),
+        ...clickOnSnippet({
+            id: "s_text_block",
+            name: "Text",
+        }),
+        {
+            content: "Select the last paragraph",
+            trigger: ":iframe .s_text_block p:last-child",
+            run: "click",
+        },
+        {
+            content: "Show the powerbox",
+            trigger: ":iframe .s_text_block p:last-child",
+            async run(actions) {
+                await actions.editor(`/`);
+                const wrapwrapEl = this.anchor.closest("#wrapwrap");
+                wrapwrapEl.dispatchEvent(
+                    new InputEvent("input", {
+                        inputType: "insertText",
+                        data: "/",
+                    })
+                );
+            },
+        },
+        {
+            content: "Initially alert snippet should be present in the powerbox",
+            trigger: ".oe-powerbox-wrapper .oe-powerbox-commandName:contains('Alert')",
+        },
+        {
+            content: "Change the content to '/table' so that alert snippet should not be present in the powerbox",
+            trigger: ":iframe .s_text_block p:last-child",
+            run() {
+                const wrapwrapEl = this.anchor.closest("#wrapwrap");
+                this.anchor.textContent = "/table";
+                wrapwrapEl.ownerDocument.dispatchEvent(
+                    new KeyboardEvent("keyup", {
+                        key: "DummyKey",
+                        code: "KeyDummy",
+                        cancelable: true,
+                    })
+                );
+            },
+        },
+        {
+            content: "Alert snippet should not be present in the powerbox",
+            trigger: ".oe-powerbox-wrapper .oe-powerbox-commandName:not(:contains('Alert'))",
+        },
+        {
+            content: "Change the content to '/banner'",
+            trigger: ":iframe .s_text_block p:last-child",
+            run() {
+                const wrapwrapEl = this.anchor.closest("#wrapwrap");
+                this.anchor.textContent = "/banner";
+                wrapwrapEl.ownerDocument.dispatchEvent(
+                    new KeyboardEvent("keyup", {
+                        key: "DummyKey",
+                        code: "KeyDummy",
+                        cancelable: true,
+                    })
+                );
+            },
+        },
+        {
+            content: "Alert snippet should be present in the powerbox",
+            trigger: ".oe-powerbox-wrapper .oe-powerbox-commandName:contains('Alert')",
+        },
+        {
+            content: "Click on the alert snippet",
+            trigger: ".oe-powerbox-wrapper .oe-powerbox-commandName:contains('Alert')",
+            run: "click",
+        },
+        {
+            content: "Check if s_alert snippet is inserted",
+            trigger: ":iframe .s_alert",
+        },
+    ]
+);
