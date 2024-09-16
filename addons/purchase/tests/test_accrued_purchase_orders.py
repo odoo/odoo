527662024a8cefc92435da7e1924501bd67a33c6
guva-odoo <guva@odoo.com>
2024-09-16 09:43:12 +0000

    [FIX] account, purchase: accrued entry returned order
    When returning a purchase/sale order, we can not create
    accrued entry for the returned order
    
    Steps:
    
    - Create a purchase order
    - Receive product
    - Create and confirm bill
    - Set received quantity to 0
    - From the action menu, select "Accrued Expense Entry"
    -> There is no line, it should be a line for the vendor credit not
    
    This is because we filter the order lines to get only the lines
    that have a positive quantity to invoice, in that case the quantity
    to invoice is negative and should be taken into account
    
    opw-4176706
    
    closes odoo/odoo#180502
    
    X-original-commit: fc8875ace95d41606b8ea90c0a3d708c59b13b97
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>
    Signed-off-by: Guillaume Vanleynseele (guva) <guva@odoo.com>

--- addons/purchase/tests/test_accrued_purchase_orders.py
+++ addons/purchase/tests/test_accrued_purchase_orders.py
@@ -141,3 +141,39 @@ class TestAccruedPurchaseOrders(AccountTestInvoicingCommon):
             {'account_id': self.alt_exp_account.id, 'debit': 909.09, 'credit': 0.0},
             {'account_id': self.account_revenue.id, 'debit': 0.0, 'credit': 5454.54},
         ])
+
+    def test_accrued_order_returned(self):
+        self.purchase_order.order_line.qty_received = 10
+        # received products billed, nothing to bill left
+        move = self.env['account.move'].browse(self.purchase_order.action_create_invoice()['res_id'])
+        move.invoice_date = '2020-01-01'
+        move.action_post()
+
+        with self.assertRaises(UserError):
+            self.wizard.create_entries()
+
+        self.purchase_order.order_line.qty_received = 5
+        res = self.env['account.move'].search(self.wizard.create_entries()['domain']).line_ids
+        self.assertRecordValues(res, [
+            # reverse move lines
+            {'account_id': self.account_expense.id, 'debit': 5000.0, 'credit': 0.0},
+            {'account_id': self.alt_exp_account.id, 'debit': 1000.0, 'credit': 0.0},
+            {'account_id': self.account_revenue.id, 'debit': 0.0, 'credit': 6000.0},
+            # move lines
+            {'account_id': self.account_expense.id, 'debit': 0.0, 'credit': 5000.0},
+            {'account_id': self.alt_exp_account.id, 'debit': 0.0, 'credit': 1000.0},
+            {'account_id': self.account_revenue.id, 'debit': 6000.0, 'credit': 0.0},
+        ])
+
+        self.purchase_order.order_line.qty_received = 0
+        res = self.env['account.move'].search(self.wizard.create_entries()['domain']).line_ids
+        self.assertRecordValues(res, [
+            # reverse move lines
+            {'account_id': self.account_expense.id, 'debit': 5000.0, 'credit': 0.0},
+            {'account_id': self.alt_exp_account.id, 'debit': 1000.0, 'credit': 0.0},
+            {'account_id': self.account_revenue.id, 'debit': 0.0, 'credit': 6000.0},
+            # move lines
+            {'account_id': self.account_expense.id, 'debit': 0.0, 'credit': 5000.0},
+            {'account_id': self.alt_exp_account.id, 'debit': 0.0, 'credit': 1000.0},
+            {'account_id': self.account_revenue.id, 'debit': 6000.0, 'credit': 0.0},
+        ])
