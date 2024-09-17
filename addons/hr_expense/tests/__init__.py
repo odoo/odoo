02f78498972a6fc7d560c0e6c0a5b3bf63eb58d3
igbe <igbe@odoo.com>
2024-09-17 15:10:17 +0200

    [IMP] expense : Remove expense warning on accounting dashboard
    This commit remove the warnings we have on the accounting dashboard,
    as the journals entries are already included in the vendor bills
    warnings.
    
    task:3877069
    
    closes odoo/odoo#169820
    
    Related: odoo/enterprise#67370
    Related: odoo/upgrade#6283
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/hr_expense/tests/__init__.py
+++ addons/hr_expense/tests/__init__.py
@@ -1,7 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import common
-from . import test_account_journal_dashboard
 from . import test_expenses
 from . import test_expenses_access_rights
 from . import test_expenses_mail_import
