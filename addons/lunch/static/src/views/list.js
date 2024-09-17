7f0497a9c7074958308f71b46b59b914f3277cd8
Vincent Sevestre <vise@odoo.com>
2024-09-17 07:38:41 +0000

    [FIX] lunch: prevent error when deleting all Location records
    When all Lunch location records are deleted, the LunchLocation JS component fails to build, raising an error when accessing the Lunch App.
    
    Steps to Reproduce:
    - Install the `lunch` module
    - Go to Lunch > Configuration > Locations
    - Delete all records
    - Access the Lunch App again
    
    Traceback: OwlError
    Invalid props for component 'AutoComplete': 'value' is not a string
    
    This error occurs at [1], where the given props are empty.
    
    This commit will resolve the above error by displaying a text message instead of trying to build a Many2XAutocomplete with empty values.
    
    opw-4104561

--- addons/lunch/static/src/views/list.js
+++ addons/lunch/static/src/views/list.js
@@ -12,6 +12,14 @@ import { LunchSearchModel } from './search_model';
 
 
 export class LunchListRenderer extends LunchRendererMixin(ListRenderer) {
+    setup() {
+        super.setup();
+        const { locationId } = this.env.searchModel.lunchState;
+        if (!locationId) {
+            this.props.list.records = [];
+        }
+    }
+
     onCellClicked(record, column) {
         this.openOrderLine(record.resId);
     }
