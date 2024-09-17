301035acc45fbbb3f18374782a4dd1f79812d74a
Denis Ledoux <dle@odoo.com>
2024-09-17 17:34:10 +0200

    [IMP] auth_totp_portal: use `TestTOTPMixin` to install totp_hook
    To avoid this pure and simple copy/pasted code already
    available in that `TestTOTPMixin.install_totphook`
    
    closes odoo/odoo#180428
    
    Signed-off-by: Toufik Benjaa (tbe) <tbe@odoo.com>

--- addons/auth_totp_portal/tests/test_tour.py
+++ addons/auth_totp_portal/tests/test_tour.py
@@ -1,10 +1,6 @@
 import logging
-import time
 
-from passlib.totp import TOTP
-
-from odoo import http
-from odoo.addons.auth_totp.controllers.home import Home
+from odoo.addons.auth_totp.tests.test_totp import TestTOTPMixin
 from odoo.addons.base.tests.common import HttpCaseWithUserPortal
 from odoo.tests import tagged
 
@@ -12,35 +8,12 @@ _logger = logging.getLogger(__name__)
 
 
 @tagged('post_install', '-at_install')
-class TestTOTPortal(HttpCaseWithUserPortal):
+class TestTOTPortal(HttpCaseWithUserPortal, TestTOTPMixin):
     """
     Largely replicates TestTOTP
     """
     def test_totp(self):
-        totp = None
-        # test endpoint as doing totp on the client side is not really an option
-        # (needs sha1 and hmac + BE packing of 64b integers)
-        def totp_hook(self, secret=None):
-            nonlocal totp
-            if totp is None:
-                totp = TOTP(secret)
-            if secret:
-                return totp.generate().token
-            else:
-                # on check, take advantage of window because previous token has been
-                # "burned" so we can't generate the same, but tour is so fast
-                # we're pretty certainly within the same 30s
-                return totp.generate(time.time() + 30).token
-        # because not preprocessed by ControllerType metaclass
-        totp_hook.routing_type = 'json'
-        # patch Home to add test endpoint
-        Home.totp_hook = http.route('/totphook', type='json', auth='none')(totp_hook)
-        self.env.registry.clear_cache('routing')
-        # remove endpoint and destroy routing map
-        @self.addCleanup
-        def _cleanup():
-            del Home.totp_hook
-            self.env.registry.clear_cache('routing')
+        self.install_totphook()
 
         self.start_tour('/my/security', 'totportal_tour_setup', login='portal')
         # also disables totp otherwise we can't re-login
