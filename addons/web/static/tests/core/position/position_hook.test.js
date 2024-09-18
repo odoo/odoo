8ada4adfb4f0f123d8b63a2a17521daccd95ab37
Julien Carion (juca) <juca@odoo.com>
2024-09-18 08:43:54 +0000

    [FIX] web: fix position hook invalid iframe test
    This commit fixes a test that even though would pass with the current
    implementation of the position hook, was behaving in a completely
    different way from before the conversion.
    
    closes odoo/odoo#180601
    
    X-original-commit: a3a7a73762ca2f091e5d8580784a15e560b5eca5
    Signed-off-by: Bastien Pierre (ipb) <ipb@odoo.com>
    Signed-off-by: Julien Carion (juca) <juca@odoo.com>

--- addons/web/static/tests/core/position/position_hook.test.js
+++ addons/web/static/tests/core/position/position_hook.test.js
@@ -401,7 +401,9 @@ test("is positioned relative to its containing block", async () => {
 });
 
 test("iframe: popper is outside, target inside", async () => {
-    await mountWithCleanup(`<div id="container"/>`);
+    await mountWithCleanup(
+        `<div id="container" style="background-color: salmon; display: flex; align-items: center; justify-content: center; width: 450px; height: 450px; margin: 25px"/>`
+    );
 
     const iframe = document.createElement("iframe");
     Object.assign(iframe.style, {
@@ -409,10 +411,11 @@ test("iframe: popper is outside, target inside", async () => {
         width: "400px",
         margin: "25px",
     });
-    iframe.srcdoc = `<div id="target-iframe" />`;
+    iframe.srcdoc = `<div id="target" style="background-color: tomato; width: 50px; height: 50px"/>`;
     const def = new Deferred();
     iframe.onload = () => def.resolve();
-    queryOne("#container").appendChild(iframe);
+    const container = queryOne("#container");
+    container.appendChild(iframe);
     await def;
 
     const iframeBody = iframe.contentDocument.body;
@@ -426,28 +429,46 @@ test("iframe: popper is outside, target inside", async () => {
         overflowX: "hidden",
     });
 
+    function getPopperComponent(popperOptions, target) {
+        class PopperComp extends Component {
+            static template = xml`
+                <div id="popper" t-ref="popper" style="background-color: plum; height: 100px; width: 100px">
+                    <div id="popper-content" style="background-color: coral; height: 50px; width: 50px"/>
+                </div>
+            `;
+            static props = ["*"];
+            setup() {
+                usePosition("popper", () => target?.el || target, {
+                    ...popperOptions,
+                    container: () => popperOptions?.container,
+                });
+            }
+        }
+        return PopperComp;
+    }
+
     // Prepare popper outside iframe
-    const popperTarget = iframe.contentDocument.getElementById("target-iframe");
+    const popperTarget = iframe.contentDocument.getElementById("target");
     let onPositionedArgs;
-    const Popper = getTestComponent(
+    const Popper = getPopperComponent(
         {
+            container,
             onPositioned: (el, solution) => {
                 onPositionedArgs = { el, solution };
                 expect.step(`${solution.direction}-${solution.variant}`);
             },
         },
-        {}, // style
         popperTarget
     );
+    await mountWithCleanup(Popper, { target: container, noMainContainer: true });
 
-    await mountWithCleanup(Popper, { noMainContainer: true });
     expect.verifySteps(["bottom-middle"]);
 
     expect("#popper").toHaveCount(1);
-    expect("#target-iframe").toHaveCount(0);
+    expect("#target").toHaveCount(0);
 
     expect(":iframe #popper").toHaveCount(0);
-    expect(":iframe #target-iframe").toHaveCount(1);
+    expect(":iframe #target").toHaveCount(1);
 
     // Check the expected position
     const { top: iframeTop, left: iframeLeft } = iframe.getBoundingClientRect();
