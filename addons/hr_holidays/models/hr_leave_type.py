b0f0986e14ba4219ab8d72b59f83870e89fd41df
bary <bary@odoo.com>
2024-09-18 07:09:02 +0000

    [FIX] hr_holidays: prevent in-use leave type allocation requirement edit
    Steps to reproduce:
    - Time off > Configuration > Time off types > New
    - Set 'Requires Allocation' to 'No Limit'
    - Takes leaves of this type
    - Set 'Requires Allocation' to 'Yes'
    - Approvals > Allocations > New > Select your time off type
    - Edit validity period to start after the leaves you took
    - Confirm > Click on the newly created allocation
    - Displays -X remaining out of Y
    
    The allocations try to cover leaves taken before the requirement was
    changed, when we'd expect leaves taken under No Limit mode not to
    require any. This is problematic because it can happen with past leaves
    as well, which we cannot get rid of on production databases.
    
    Since Odoo does not track which allocation requirement mode leave are
    taken under nor the date and content of edits to that parameter, we
    cannot tell apart leaves of the same type when distributing allocated
    days. Tracking either would certainly require edits to the model which
    rules out stable versions, so instead the edits to allocation
    requirement should be restricted in favor of creating new time off types
    when we want to switch that parameter.
    
    opw-3919886
    
    closes odoo/odoo#180930
    
    X-original-commit: e81d099a1cf6d892b51c05be6e2421669db34887
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>
    Signed-off-by: Yoann Baron (bary) <bary@odoo.com>

--- addons/hr_holidays/models/hr_leave_type.py
+++ addons/hr_holidays/models/hr_leave_type.py
@@ -9,6 +9,7 @@ from collections import defaultdict
 from datetime import date, datetime, time
 
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 from odoo.tools import format_date, frozendict
 from odoo.tools.translate import _
 from odoo.tools.float_utils import float_round
@@ -173,6 +174,11 @@ class HolidaysType(models.Model):
                 holiday_type.has_valid_allocation = bool(allocations)
             else:
                 holiday_type.has_valid_allocation = True
+
+    @api.constrains('requires_allocation')
+    def check_allocation_requirement_edit_validity(self):
+        if self.env['hr.leave'].search_count([('holiday_status_id', 'in', self.ids)], limit=1):
+            raise UserError(_("The allocation requirement of a time off type cannot be changed once leaves of that type have been taken. You should create a new time off type instead."))
 
     def _search_max_leaves(self, operator, value):
         value = float(value)
