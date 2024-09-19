a16ef39ae24bc264e33aed6f4238a5bfa6cbf02e
David Monnom (moda) <moda@odoo.com>
2024-09-19 13:23:37 +0200

    [FIX] point_of_sale, pos_hr: cash move wasn't displayed on closing popup
    Before this commit, the cash move wasn't displayed on the closing popup.
    
    This commit fixes this issue by adding the cash move.
    
    closes odoo/odoo#180734
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/pos_hr/static/src/overrides/components/closing_popup/closing_popup.js
+++ addons/pos_hr/static/src/overrides/components/closing_popup/closing_popup.js
@@ -1,11 +1,10 @@
 import { ClosePosPopup } from "@point_of_sale/app/navbar/closing_popup/closing_popup";
 import { _t } from "@web/core/l10n/translation";
 import { patch } from "@web/core/utils/patch";
-import { PaymentMethodBreakdown } from "./payment_method_breakdown/payment_method_breakdown";
 import { AccordionItem } from "@point_of_sale/app/generic_components/accordion_item/accordion_item";
 
 patch(ClosePosPopup, {
-    components: { ...ClosePosPopup.components, PaymentMethodBreakdown, AccordionItem },
+    components: { ...ClosePosPopup.components, AccordionItem },
     get paymentMethodBreakdownTitle() {
         return _t("Payments in %(paymentMethod)s", {
             paymentMethod: this.props.default_cash_details.name,
