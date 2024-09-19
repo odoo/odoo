31fca42c9b022b9e233cfbfd4473d309f0b38afb
Julien Mougenot <jum@odoo.com>
2024-09-19 11:24:42 +0200

    [FIX] web: Hoot - limit console overrides
    This commit changes the `MockConsole` to only override "tracing"
    methods, i.e. `error`, `trace` and `warn`.
    
    This is because the devtools points to the last call to the logging
    function (i.e. the overridden methods), and the initiator was completely
    lost for non-tracing methods.
    
    closes odoo/odoo#181967
    
    Related: odoo/enterprise#70986
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/web/static/lib/hoot/mock/console.js
+++ addons/web/static/lib/hoot/mock/console.js
@@ -7,16 +7,26 @@
 const { console } = globalThis;
 
 //-----------------------------------------------------------------------------
+// Internal
+//-----------------------------------------------------------------------------
+
+const DISPATCHING_METHODS = ["error", "trace", "warn"];
+
+//-----------------------------------------------------------------------------
 // Exports
 //-----------------------------------------------------------------------------
 
 export class MockConsole extends EventTarget {
     static {
-        for (const [name, method] of Object.entries(console)) {
-            this.prototype[name] = function (...args) {
-                this.dispatchEvent(new CustomEvent(name, { detail: args }));
-                return method.call(this, ...args);
-            };
+        for (const [name, value] of Object.entries(console)) {
+            if (DISPATCHING_METHODS.includes(name)) {
+                this.prototype[name] = function (...args) {
+                    this.dispatchEvent(new CustomEvent(name, { detail: args }));
+                    return value.apply(this, arguments);
+                };
+            } else {
+                this.prototype[name] = value;
+            }
         }
     }
 }
