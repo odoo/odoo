637e098b847df031aa121a296c16376682160a02
Valentin Chevalier <vcr@odoo.com>
2024-09-19 21:37:11 +0200

    fixup! [IMP] website_sale: mega menu for ecommerce categories

--- addons/website_sale/static/src/js/editor/snippets.options.js
+++ addons/website_sale/static/src/js/editor/snippets.options.js
@@ -1,39 +1,22 @@
 import options from "@web_editor/js/editor/snippets.options";
 
 options.registry.MegaMenuLayout = options.registry.MegaMenuLayout.extend({
+    _fetchECommerceCategories() {
+        return this.containerEl.classList.contains('fetchEComCategories');
+    },
+
     /**
-     * @override
+     * Override of `website` TODO VCR...
+     * Retrieves a template either from cache or through RPC.
+     *
+     * @private
+     * @param {string} xmlid
+     * @returns {string}
      */
-    events: {
-        'click .fetch_ecom_categories': '_onClick',
-    },
-
-    _onClick() {
-        const selectedButton = document.querySelector('.mega_menu_template we-button.active');
-        if (!selectedButton) return;
-
-        const currentTemplateId = selectedButton.getAttribute('data-select-template');
-        const newTemplateId = this._getNewTemplateId(currentTemplateId);
-
-        if (newTemplateId) {
-            this._switchTemplate(newTemplateId);
+    async _getTemplate(xmlid) {
+        if (this._fetchECommerceCategories()) {
+            xmlid.replace('website.', 'website_sale.');
         }
-    },
-
-    _getNewTemplateId(currentTemplateId) {
-        return currentTemplateId.startsWith('website.')
-            ? currentTemplateId.replace('website.', 'website_sale.')
-            : null;
-    },
-
-    _switchTemplate(newTemplateId) {
-        this._getTemplate(newTemplateId)
-            .then(template => {
-                const sectionEl = this.containerEl.querySelector('section');
-                if (sectionEl) {
-                    sectionEl.outerHTML = template;
-                }
-            })
-            .catch(error => console.error('Error switching template:', error));
+        return this._super(xmlid);
     },
 });
