aa0f127a7b577fc880816a0a5806f1d67f5c6bf3
Hesham <hsal@odoo.com>
2024-09-19 16:14:45 +0300

    [IMP] l10n_tr: Renamed Turkey to Türkiye
    In 2022, Republic of Turkey officially renamed itself to Republic of Türkiye.
    So, its name needed to be changed in its Odoo modules.
    Moreover, updated Turkish modules descriptions according to this excalidraw:
    https://link.excalidraw.com/readonly/rbesZEAkXUS8rVgXplgm?darkMode=true
    
    task-4182931
    
    closes odoo/odoo#180758
    
    Related: odoo/enterprise#70304
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/l10n_tr/__manifest__.py
+++ addons/l10n_tr/__manifest__.py
@@ -2,18 +2,20 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Turkey - Accounting',
+    'name': 'Türkiye - Accounting',
     'version': '1.1',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
-Türkiye için Tek düzen hesap planı şablonu Odoo Modülü.
-==========================================================
+This is the base module to manage the accounting chart for Türkiye in Odoo
+==========================================================================
+Türkiye accounting basic charts and localization.
+-------------------------------------------------
+Activates:
 
-Bu modül kurulduktan sonra, Muhasebe yapılandırma sihirbazı çalışır
-    * Sihirbaz sizden hesap planı şablonu, planın kurulacağı şirket, banka hesap
-      bilgileriniz, ilgili para birimi gibi bilgiler isteyecek.
+- Chart of Accounts
+
+- Taxes
     """,
-    'author': 'Ahmet Altınışık, Can Tecim',
     'maintainer':'https://launchpad.net/~openerp-turkey, http://www.cantecim.com',
     'depends': [
         'account',
