6f679b889d3feda049acc2fc465fe50dc816655b
Damien <ultrarushgame@gmail.com>
2024-09-20 11:22:23 +0200

    [FIX] pos_report,make the total number of orders under analysis acceptable for accounting purposes
    The current total calculation method rounds up each order at the point of sale, taking into account the currency used. With a large number of orders, you end up with cumulative rounding and large discrepancies between the figures in the accounts and the total in the pivot view.
    
    [FIX] added cu.decimal_places in group_by
    
    squash commit between group_by (cu.decimal_places) an sql query

--- addons/point_of_sale/report/pos_order_report.py
+++ addons/point_of_sale/report/pos_order_report.py
@@ -45,7 +45,7 @@ class PosOrderReport(models.Model):
                 s.date_order AS date,
                 SUM(l.qty) AS product_qty,
                 SUM(l.qty * l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS price_sub_total,
-                SUM(ROUND((l.price_subtotal_incl) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END, cu.decimal_places)) AS price_total,
+                ROUND(SUM(l.price_subtotal_incl / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END), cu.decimal_places) AS price_total,
                 SUM((l.qty * l.price_unit) * (l.discount / 100) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS total_discount,
                 CASE
                     WHEN SUM(l.qty * u.factor) = 0 THEN NULL
@@ -84,13 +84,14 @@ class PosOrderReport(models.Model):
     def _group_by(self):
         return """
             GROUP BY
-                s.id, s.date_order, s.partner_id,s.state, pt.categ_id,
+                s.id, s.date_order, s.partner_id, s.state, pt.categ_id,
                 s.user_id, s.company_id, s.sale_journal,
                 s.pricelist_id, s.account_move, s.create_date, s.session_id,
                 l.product_id,
                 pt.categ_id, pt.pos_categ_id,
                 p.product_tmpl_id,
-                ps.config_id
+                ps.config_id,
+                cu.decimal_places
         """
 
     def init(self):
