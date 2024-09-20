ce6503da4958a5175598cf3d8501f352a1f3b39b
Levi Siuzdak (sile) <sile@odoo.com>
2024-09-20 09:02:58 +0000

    [FIX] account: clear attachment ids to proccess on import error
    Versions
    --------
    - 16.0+
    
    Steps
    -----
    1. Try to import a malformed OFX file into a bank journal;
    2. close traceback;
    3. try to import a valid OFX or CSV file.
    
    Issue
    -----
    Same traceback as first upload.
    
    Cause
    -----
    There's no error handling on the RPC call. If the file is successfully
    imported, the `attachmentIdsToProcess` gets cleared, but if there's an
    error, the bad file remains in the queue until leaving the view.
    
    Solution
    --------
    Put the RPC call in a `try` block, followed by a `finally` which ensures
    `attachmentIdsToProcess` get cleared, even on error.
    
    opw-4113740
    
    closes odoo/odoo#180964
    
    X-original-commit: 81d8257e016684c303baa00444c658e336c9f075
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/account/static/src/components/document_file_uploader/document_file_uploader.js
+++ addons/account/static/src/components/document_file_uploader/document_file_uploader.js
@@ -48,19 +48,25 @@ export class DocumentFileUploader extends Component {
 
     async onUploadComplete() {
         const resModal = this.getResModel();
-        const action = await this.orm.call(resModal, "create_document_from_attachment", ["", this.attachmentIdsToProcess], {
-            context: { ...this.extraContext, ...this.env.searchModel.context },
-        });
-        this.attachmentIdsToProcess = [];
+        let action;
+        try {
+            action = await this.orm.call(
+                resModal,
+                "create_document_from_attachment",
+                ["", this.attachmentIdsToProcess],
+                { context: { ...this.extraContext, ...this.env.searchModel.context } }
+            );
+        } finally {
+            // ensures attachments are cleared on success as well as on error
+            this.attachmentIdsToProcess = [];
+        }
         if (action.context && action.context.notifications) {
-            for (let [file, msg] of Object.entries(action.context.notifications)) {
-                this.notification.add(
-                    msg,
-                    {
-                        title: file,
-                        type: "info",
-                        sticky: true,
-                    });
+            for (const [file, msg] of Object.entries(action.context.notifications)) {
+                this.notification.add(msg, {
+                    title: file,
+                    type: "info",
+                    sticky: true,
+                });
             }
             delete action.context.notifications;
         }
