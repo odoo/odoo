90cd6a768a4c2719e5025aeefebaa5358b56405f
Jay Savaliya <jasa@odoo.com>
2024-09-20 11:12:12 +0530

    [IMP] l10n_in_(withholding): move TCS-TDS reports
    This commit will move TCS-TDS reports to `l10n_in_withholding` from `l10n_in`.
    
    task-4037680
    
    closes odoo/odoo#180862
    
    Related: odoo/enterprise#70374
    Related: odoo/upgrade#6544
    Signed-off-by: Quentin De Paoli (qdp) <qdp@odoo.com>

--- addons/l10n_in_withholding/__manifest__.py
+++ addons/l10n_in_withholding/__manifest__.py
@@ -10,6 +10,8 @@
     'data': [
         'security/ir.model.access.csv',
         'data/l10n_in.section.alert.csv',
+        'data/account_tax_report_tcs_data.xml',
+        'data/account_tax_report_tds_data.xml',
         'wizard/l10n_in_withhold_wizard.xml',
         'views/l10n_in_section_alert_views.xml',
         'views/account_account_views.xml',
