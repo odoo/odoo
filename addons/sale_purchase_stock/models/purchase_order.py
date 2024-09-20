b13e46f06c1d3166fd64fc72cacd8af1f3673ae6
clesgow <quwo@odoo.com>
2024-09-20 14:15:24 +0000

    [FIX] sale_purchase_stock,purchase_mrp: Keep MTO link in multi-step
    Follow-up on #179964, but this time properly checking if the link is
    still usable from both sides (i.e. both PO *and* SO).
    
    Also fixes the issue with the link from PO <-> MO in the same way.
    
    closes odoo/odoo#181188
    
    X-original-commit: 5df53a747f668579b43540b13b33b79c091d6c60
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/sale_purchase_stock/models/purchase_order.py
+++ addons/sale_purchase_stock/models/purchase_order.py
@@ -12,4 +12,6 @@ class PurchaseOrder(models.Model):
         super(PurchaseOrder, self)._compute_sale_order_count()
 
     def _get_sale_orders(self):
-        return super(PurchaseOrder, self)._get_sale_orders() | self.order_line.move_dest_ids.group_id.sale_id | self.order_line.move_ids.move_dest_ids.group_id.sale_id
+        return super()._get_sale_orders() \
+            | self.order_line.move_dest_ids.group_id.sale_id \
+            | self.env['stock.move'].browse(self.order_line.move_ids._rollup_move_dests()).group_id.sale_id
