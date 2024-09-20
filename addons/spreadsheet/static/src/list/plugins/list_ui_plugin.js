odoo/odoo

commit 5b6c2f7589131c59fdd27368aaf1fbb79bdd9b55

[PERF] spreadsheet: fetch all records with one RPC
Let's say you insert a list in a spreadsheet with an initial limit of
10 records.

Next, you write in a cell '=ODOO.LIST(1, 11, "foo")',
in another you write '=ODOO.LIST(1, A30, "foo")'
where A30 contains 111

Currently, this list is loaded with 2 RPCs.
The first with a limit of 11 (because 11 was extracted statically from
the formula before evaluation), and the second with the correct limit of
111.

This 2 network requests, and therefore 3 full evaluations (initial evaluation
+ 1/RPC).

With this commit, all positions are aggregated during the first evaluation.
We save one RPC and one evaluation.

It also allows to remove the code which extracted the positions statically
to gain even more performance.
See next commit

Task: 4134859
Part-of: odoo/odoo#177769
Related: odoo/enterprise#68956
Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

addons/spreadsheet/static/src/list/plugins/list_ui_plugin.js
@@ -8,6 +8,7 @@ import { globalFiltersFieldMatchers } from "@spreadsheet/global_filters/plugins/
 import { OdooUIPlugin } from "@spreadsheet/plugins";
 
 const { astToFormula } = spreadsheet;
+const { isEvaluationError, toString } = spreadsheet.helpers;
 
 /**
  * @typedef {import("./list_core_plugin").SpreadsheetList} SpreadsheetList
@@ -342,11 +343,10 @@ export class ListUIPlugin extends OdooUIPlugin {
     getListCellValueAndFormat(listId, position, fieldName) {
         const dataSource = this.getters.getListDataSource(listId);
         dataSource.addFieldToFetch(fieldName);
-        const error = dataSource.assertIsValid({ throwOnError: false });
-        if (error) {
-            return error;
-        }
         const value = dataSource.getListCellValue(position, fieldName);
+        if (isEvaluationError(toString(value))) {
+            return value;
+        }
         const field = dataSource.getField(fieldName);
         const format = this._getListFormat(listId, position, field);
         return { value, format };

addons/spreadsheet/static/src/data_sources/data_source.js
@@ -97,6 +97,10 @@ export class LoadableDataSource {
         return this._isFullyLoaded;
     }
 
+    isLoading() {
+        return !!this._loadPromise && !this.isReady();
+    }
+
     isValid() {
         return this.isReady() && this._isValid;
     }

addons/spreadsheet/static/src/list/list_data_source.js
@@ -1,7 +1,6 @@
 /** @odoo-module */
 
 import { OdooViewsDataSource } from "@spreadsheet/data_sources/odoo_views_data_source";
-import { LoadingDataError } from "@spreadsheet/o_spreadsheet/errors";
 import { EvaluationError } from "@odoo/o-spreadsheet";
 import { _t } from "@web/core/l10n/translation";
 import {
@@ -13,6 +12,7 @@ import {
 import { orderByToString } from "@web/search/utils/order_by";
 
 import * as spreadsheet from "@odoo/o-spreadsheet";
+import { LOADING_ERROR } from "@spreadsheet/data_sources/data_source";
 
 const { toNumber } = spreadsheet.helpers;
 const { DEFAULT_LOCALE } = spreadsheet.constants;
@@ -42,7 +42,7 @@ export class ListDataSource extends OdooViewsDataSource {
      */
     constructor(services, params) {
         super(services, params);
-        this.maxPosition = params.limit;
+        this.maxPosition = 0;
         this.maxPositionFetched = 0;
         this.data = [];
         this.fieldsToFetch = new Set();
@@ -64,6 +64,18 @@ export class ListDataSource extends OdooViewsDataSource {
             return;
         }
         this.fieldsToFetch.add(fieldName);
+    }
+
+    async load(params) {
+        if (this._fetchingPromise) {
+            // if fetching is already scheduled for the next tick,
+            // wait the fetching promise to trigger the data source loading
+            // and then await the loading.
+            await this._fetchingPromise;
+            await this._loadPromise;
+            return;
+        }
+        return super.load(params);
     }
 
     async _load() {
@@ -134,9 +146,13 @@ export class ListDataSource extends OdooViewsDataSource {
 
     /**
      * @param {string} fieldName
-     * @returns {string}
+     * @returns {string | EvaluationError}
      */
     getListHeaderValue(fieldName) {
+        if (!this.isMetaDataLoaded()) {
+            this._triggerFetching();
+            return LOADING_ERROR;
+        }
         this.assertIsValid();
         const field = this.getField(fieldName);
         return field ? field.string : fieldName;
@@ -145,15 +161,20 @@ export class ListDataSource extends OdooViewsDataSource {
     /**
      * @param {number} position
      * @param {string} fieldName
-     * @returns {string|number|undefined}
+     * @returns {string|number|undefined|EvaluationError}
      */
     getListCellValue(position, fieldName) {
-        this.assertIsValid();
+        if (this.isLoading()) {
+            return LOADING_ERROR;
+        }
+        if (!this._isValid) {
+            return this._loadError;
+        }
         if (position >= this.maxPositionFetched) {
             this.increaseMaxPosition(position + 1);
             // A reload is needed because the asked position is not already loaded.
             this._triggerFetching();
-            throw new LoadingDataError();
+            return LOADING_ERROR;
         }
         const record = this.data[position];
         if (!record) {
@@ -161,15 +182,16 @@ export class ListDataSource extends OdooViewsDataSource {
         }
         const field = this.getField(fieldName);
         if (!field) {
-            throw new EvaluationError(
+            return new EvaluationError(
                 _t("The field %s does not exist or you do not have access to that field", fieldName)
             );
         }
         if (!(fieldName in record)) {
             this.addFieldToFetch(fieldName);
             this._triggerFetching();
-            throw new LoadingDataError();
+            return LOADING_ERROR;
         }
+        this.assertIsValid();
         switch (field.type) {
             case "many2one":
                 return record[fieldName].display_name ?? "";
@@ -200,7 +222,7 @@ export class ListDataSource extends OdooViewsDataSource {
                 return properties.map((property) => property.string).join(", ");
             }
             case "json":
-                throw new EvaluationError(_t('Fields of type "%s" are not supported', "json"));
+                return new EvaluationError(_t('Fields of type "%s" are not supported', "json"));
             default:
                 return record[fieldName] || "";
         }
@@ -253,9 +275,9 @@ export class ListDataSource extends OdooViewsDataSource {
             return;
         }
         this._fetchingPromise = Promise.resolve().then(() => {
-            new Promise((resolve) => {
-                this.load({ reload: true });
+            return new Promise((resolve) => {
                 this._fetchingPromise = undefined;
+                this.load({ reload: true });
                 resolve();
             });
         });

addons/spreadsheet/static/src/list/list_functions.js
@@ -46,11 +46,6 @@ const ODOO_LIST_HEADER = {
         const id = toString(listId);
         const field = toString(fieldName);
         assertListsExists(id, this.getters);
-        const dataSource = this.getters.getListDataSource(id);
-        const error = dataSource.assertIsValid({ throwOnError: false });
-        if (error) {
-            return error;
-        }
         return this.getters.getListHeaderValue(id, field);
     },
     returns: ["NUMBER", "STRING"],

addons/spreadsheet/static/tests/lists/list_plugin.test.js
@@ -718,6 +718,41 @@ test("fetch all and only required fields", async function () {
     expect.verifySteps(["data-fetched"]);
 });
 
+test("fetch all required positions, including the evaluated ones", async function () {
+    const spreadsheetData = {
+        sheets: [
+            {
+                id: "sheet1",
+                cells: {
+                    A1: { content: '=ODOO.LIST(1, 11, "foo")' },
+                    A2: { content: '=ODOO.LIST(1, A3, "foo")' },
+                    A3: { content: "111" },
+                },
+            },
+        ],
+        lists: {
+            1: {
+                id: 1,
+                columns: ["foo"],
+                domain: [],
+                model: "partner",
+                orderBy: [],
+                context: {},
+            },
+        },
+    };
+    await createModelWithDataSource({
+        spreadsheetData,
+        mockRPC: function (route, args) {
+            if (args.method === "web_search_read" && args.model === "partner") {
+                expect.step("data-fetched");
+                expect(args.kwargs.limit).toBe(111);
+            }
+        },
+    });
+    expect.verifySteps(["data-fetched"]);
+});
+
 test("list with both a monetary field and the related currency field 1", async function () {
     const { model } = await createSpreadsheetWithList({
         columns: ["pognon", "currency_id"],

commit b6070cde90cb499aca340687b04207eca19b8759

[PERF] spreadsheet: don't throw error when loading list
This `if` is meant to detect when the list is loading and return the error
instead of throwing it (throwing is slow)
...except `toString` throws if the value is an error :man_facepalming:

Issue introduced with 5b6c2f7589131c59fdd27368aaf1fbb79bdd9b55

Task: 4199066

addons/spreadsheet/static/src/list/plugins/list_ui_plugin.js
@@ -8,7 +8,7 @@ import { globalFiltersFieldMatchers } from "@spreadsheet/global_filters/plugins/
 import { OdooUIPlugin } from "@spreadsheet/plugins";
 
 const { astToFormula } = spreadsheet;
-const { isEvaluationError, toString } = spreadsheet.helpers;
+const { isEvaluationError } = spreadsheet.helpers;
 
 /**
  * @typedef {import("./list_core_plugin").SpreadsheetList} SpreadsheetList
@@ -297,7 +297,7 @@ export class ListUIPlugin extends OdooUIPlugin {
         const dataSource = this.getters.getListDataSource(listId);
         dataSource.addFieldToFetch(fieldName);
         const value = dataSource.getListCellValue(position, fieldName);
-        if (isEvaluationError(toString(value))) {
+        if (typeof value === "object" && isEvaluationError(value.value)) {
             return value;
         }
         const field = dataSource.getField(fieldName);