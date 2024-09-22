cbc52872f7d9bd0279906e719091e84460889f0c
abd-msyukyu-odoo <abd@odoo.com>
2024-09-22 15:16:41 +0000

    [FIX] html_editor: disable editor features for protected related elements
    Protected nodes in the editor should never be maniuplated through standard
    editor flows. Instead, they should be handled by a specific plugin which takes
    care of all behaviors related to that protected node.
    
    The most obvious example is a node managed by OWL can never be altered/removed
    by the editor, or else OWL will crash (see embedded components).
    
    This commit goes through multiple editor plugins to restrict what it can do to
    protected nodes.
    
    In the future, this "protection handling" should be abstracted and native
    JavaScript function usage should be avoided.
    
    task-3672747
    
    X-original-commit: 2f4e1b9b33e219ad16a739f484203518ffaa495a
    Part-of: odoo/odoo#182145
    Related: odoo/enterprise#71082
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>
    Co-authored-by: Damien Abeloos <abd@odoo.com>
    Co-authored-by: Adrien Schoffeniels <adsc@odoo.com>
    Co-authored-by: Julien Banken <jbn@odoo.com>
    Co-authored-by: Thomas Josse <thjo@odoo.com>

--- addons/html_editor/static/src/utils/dom_info.js
+++ addons/html_editor/static/src/utils/dom_info.js
@@ -3,6 +3,9 @@ import { closestElement, firstLeaf, lastLeaf } from "./dom_traversal";
 import { DIRECTIONS, nodeSize } from "./position";
 
 export function isEmpty(el) {
+    if (isProtecting(el) || isProtected(el)) {
+        return false;
+    }
     const content = el.innerHTML.trim();
     if (content === "" || content === "<br>") {
         return true;
@@ -151,6 +154,9 @@ export function isVisibleTextNode(testedNode) {
     if (!testedNode || !testedNode.length || testedNode.nodeType !== Node.TEXT_NODE) {
         return false;
     }
+    if (isProtected(testedNode)) {
+        return true;
+    }
     if (
         visibleCharRegex.test(testedNode.textContent) ||
         (isInPre(testedNode) && isWhitespace(testedNode))
@@ -248,7 +254,8 @@ export function isVisible(node) {
             isSelfClosingElement(node) ||
             // @todo: handle it in resources?
             isMediaElement(node) ||
-            hasVisibleContent(node))
+            hasVisibleContent(node) ||
+            isProtecting(node))
     );
 }
 export function hasVisibleContent(node) {
@@ -418,6 +425,10 @@ export function isProtecting(node) {
     );
 }
 
+export function isUnprotecting(node) {
+    return node.nodeType === Node.ELEMENT_NODE && node.dataset.oeProtected === "false";
+}
+
 // This is a list of "paragraph-related elements", defined as elements that
 // behave like paragraphs.
 export const paragraphRelatedElements = [
@@ -498,7 +509,7 @@ export function isEmptyBlock(blockEl) {
     if (blockEl.querySelectorAll("br").length >= 2) {
         return false;
     }
-    if (isProtecting(blockEl)) {
+    if (isProtecting(blockEl) || isProtected(blockEl)) {
         // Protecting nodes should never be considered empty for editor
         // operations, as their content is a "black box". Their content should
         // be managed by a specialized plugin.
