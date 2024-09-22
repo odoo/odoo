10f2c2ebccbb66a5ae34864eb2a4bc21eabc8379
abd-msyukyu-odoo <abd@odoo.com>
2024-09-22 15:05:23 +0200

    [IMP] html_editor, project: use the HtmlViewer in the history dialog
    Using the HtmlViewer in the History Dialog will allow to mount embedded
    components in the comparison previews. This will offer a far better
    representation of the state of the html_field and its changes.
    
    History Dialog assets are moved to the `html_editor` module to remove them from
    the `components` folder of `web_editor`, which bundled the History Dialog into
    more assets than necessary.
    
    task-3672747
    
    Part-of: odoo/odoo#173997
    Related: odoo/enterprise#67083
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Co-authored-by: Damien Abeloos <abd@odoo.com>
    Co-authored-by: Adrien Schoffeniels <adsc@odoo.com>
    Co-authored-by: Julien Banken <jbn@odoo.com>
    Co-authored-by: Thomas Josse <thjo@odoo.com>

--- addons/html_editor/static/src/components/history_dialog/history_dialog.js
+++ addons/html_editor/static/src/components/history_dialog/history_dialog.js
@@ -1,19 +1,20 @@
 /** @odoo-module **/
-
-import { Dialog } from '@web/core/dialog/dialog';
-import { Notebook } from '@web/core/notebook/notebook';
-import { formatDateTime } from '@web/core/l10n/dates';
-import { useService } from '@web/core/utils/hooks';
-import { memoize } from '@web/core/utils/functions';
-import { Component, onMounted, useState, markup } from '@odoo/owl';
-import { _t } from '@web/core/l10n/translation';
+import { Dialog } from "@web/core/dialog/dialog";
+import { Notebook } from "@web/core/notebook/notebook";
+import { formatDateTime } from "@web/core/l10n/dates";
+import { useService } from "@web/core/utils/hooks";
+import { memoize } from "@web/core/utils/functions";
+import { Component, onMounted, useState, markup } from "@odoo/owl";
+import { _t } from "@web/core/l10n/translation";
 import { user } from "@web/core/user";
+import { HtmlViewer } from "@html_editor/fields/html_viewer";
+import { READONLY_MAIN_EMBEDDINGS } from "@html_editor/others/embedded_components/embedding_sets";
 
 const { DateTime } = luxon;
 
-class HistoryDialog extends Component {
-    static template = 'web_editor.HistoryDialog';
-    static components = { Dialog, Notebook };
+export class HistoryDialog extends Component {
+    static template = "html_editor.HistoryDialog";
+    static components = { Dialog, HtmlViewer, Notebook };
     static props = {
         recordId: Number,
         recordModel: String,
@@ -23,32 +24,41 @@ class HistoryDialog extends Component {
         versionedFieldName: String,
         title: { String, optional: true },
         noContentHelper: { String, optional: true }, //Markup
+        embeddedComponents: { Array, optional: true },
     };
 
     static defaultProps = {
         title: _t("History"),
         noContentHelper: markup(""),
+        embeddedComponents: [...READONLY_MAIN_EMBEDDINGS],
     };
 
     state = useState({
         revisionsData: [],
         revisionContent: null,
         revisionComparison: null,
-        revisionId: null
+        revisionId: null,
     });
 
     setup() {
-        this.size = 'xl';
+        this.size = "xl";
         this.title = this.props.title;
-        this.orm = useService('orm');
+        this.orm = useService("orm");
         this.notebookTabs = [_t("Content"), _t("Comparison")];
 
         onMounted(() => this.init());
     }
 
+    getConfig(value) {
+        return {
+            value: this.state[value],
+            embeddedComponents: this.props.embeddedComponents,
+        };
+    }
+
     async init() {
         this.state.revisionsData = this.props.historyMetadata;
-        await this.updateCurrentRevision(this.props.historyMetadata[0]['revision_id']);
+        await this.updateCurrentRevision(this.props.historyMetadata[0]["revision_id"]);
     }
 
     async updateCurrentRevision(revisionId) {
@@ -58,9 +68,7 @@ class HistoryDialog extends Component {
         this.env.services.ui.block();
         this.state.revisionId = revisionId;
         this.state.revisionContent = await this.getRevisionContent(revisionId);
-        this.state.revisionComparison = await this.getRevisionComparison(
-            revisionId
-        );
+        this.state.revisionComparison = await this.getRevisionComparison(revisionId);
         this.env.services.ui.unblock();
     }
 
@@ -68,7 +76,7 @@ class HistoryDialog extends Component {
         async function getRevisionComparison(revisionId) {
             const comparison = await this.orm.call(
                 this.props.recordModel,
-                'html_field_history_get_comparison_at_revision',
+                "html_field_history_get_comparison_at_revision",
                 [this.props.recordId, this.props.versionedFieldName, revisionId]
             );
             return markup(comparison);
@@ -79,7 +87,7 @@ class HistoryDialog extends Component {
         async function getRevisionContent(revisionId) {
             const content = await this.orm.call(
                 this.props.recordModel,
-                'html_field_history_get_content_at_revision',
+                "html_field_history_get_content_at_revision",
                 [this.props.recordId, this.props.versionedFieldName, revisionId]
             );
             return markup(content);
@@ -88,9 +96,7 @@ class HistoryDialog extends Component {
 
     async _onRestoreRevisionClick() {
         this.env.services.ui.block();
-        const restoredContent = await this.getRevisionContent(
-            this.state.revisionId
-        );
+        const restoredContent = await this.getRevisionContent(this.state.revisionId);
         this.props.restoreRequested(restoredContent, this.props.close);
         this.env.services.ui.unblock();
     }
@@ -99,8 +105,8 @@ class HistoryDialog extends Component {
      * Getters
      **/
     getRevisionDate(revision) {
-        return formatDateTime(DateTime.fromISO(revision['create_date'], { zone: 'utc' }).setZone(user.tz));
+        return formatDateTime(
+            DateTime.fromISO(revision["create_date"], { zone: "utc" }).setZone(user.tz)
+        );
     }
 }
-
-export default HistoryDialog;
