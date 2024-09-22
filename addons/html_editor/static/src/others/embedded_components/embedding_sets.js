e22a2e1c58d91536efa9ba4febe500703ef6348b
abd-msyukyu-odoo <abd@odoo.com>
2024-09-22 15:28:20 +0000

    [IMP] html-editor: add the video embedded component
    Move the video_behavior from knowledge to html_editor as an embedded component.
    This simple component renders an iframe containing a trusted video provider
    URL (like youtube), to allow displaying videos inside the HtmlField.
    
    task-3672747
    
    X-original-commit: b267db63292528d8e40f74c75a02d6e5e286bc85
    Part-of: odoo/odoo#182145
    Related: odoo/enterprise#71082
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>
    Co-authored-by: Damien Abeloos <abd@odoo.com>
    Co-authored-by: Adrien Schoffeniels <adsc@odoo.com>
    Co-authored-by: Julien Banken <jbn@odoo.com>
    Co-authored-by: Thomas Josse <thjo@odoo.com>

--- addons/html_editor/static/src/others/embedded_components/embedding_sets.js
+++ addons/html_editor/static/src/others/embedded_components/embedding_sets.js
@@ -6,15 +6,18 @@ import {
     readonlyTableOfContentEmbedding,
     tableOfContentEmbedding,
 } from "@html_editor/others/embedded_components/core/table_of_content/table_of_content";
+import { videoEmbedding } from "@html_editor/others/embedded_components/core/video/video";
 
 export const MAIN_EMBEDDINGS = [
     excalidrawEmbedding,
     fileEmbedding,
     tableOfContentEmbedding,
+    videoEmbedding,
 ];
 
 export const READONLY_MAIN_EMBEDDINGS = [
     readonlyExcalidrawEmbedding,
     readonlyFileEmbedding,
     readonlyTableOfContentEmbedding,
+    videoEmbedding,
 ];
