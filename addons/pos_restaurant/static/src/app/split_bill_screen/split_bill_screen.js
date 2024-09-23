keyboard-slayer/odoo-eval

[IMP] point_of_sale, pos_restaurant: improve split order name
when an order is split from:
- a table, the new order name is (table number + char starting from b and increasing with more splits)
- a floating order, the new order name is (floating order name + char starting from b and increasing with more splits)

task-4196117

addons/pos_restaurant/static/src/app/split_bill_screen/split_bill_screen.js
@@ -52,13 +52,43 @@ export class SplitBillScreen extends Component {
         }
     }
 
+    _getOrderName(order) {
+        return order.table_id?.table_number.toString() || order.getFloatingOrderName() || "";
+    }
+
+    _getLatestOrderNameStartingWith(name) {
+        return (
+            this.pos
+                .get_open_orders()
+                .map((order) => this._getOrderName(order))
+                .filter((orderName) => orderName.slice(0, -1) === name)
+                .sort((a, b) => a.slice(-1).localeCompare(b.slice(-1)))
+                .at(-1) || name
+        );
+    }
+
+    _getSplitOrderName(originalOrderName) {
+        const latestOrderName = this._getLatestOrderNameStartingWith(originalOrderName);
+        if (latestOrderName === originalOrderName) {
+            return `${originalOrderName}B`;
+        }
+        const lastChar = latestOrderName[latestOrderName.length - 1];
+        if (lastChar === "Z") {
+            throw new Error("You cannot split the order into more than 26 parts!");
+        }
+        const nextChar = String.fromCharCode(lastChar.charCodeAt(0) + 1);
+        return `${latestOrderName.slice(0, -1)}${nextChar}`;
+    }
+
     createSplittedOrder() {
         const curOrderUuid = this.currentOrder.uuid;
         const originalOrder = this.pos.models["pos.order"].find((o) => o.uuid === curOrderUuid);
         this.pos.selectedTable = null;
+        const originalOrderName = this._getOrderName(originalOrder);
+        const newOrderName = this._getSplitOrderName(originalOrderName);
+
         const newOrder = this.pos.createNewOrder();
-        const originalOrderName = originalOrder.getOrderName();
-        newOrder.note = `${newOrder.tracking_number} Split from ${originalOrderName}`;
+        newOrder.note = newOrderName;
         newOrder.uiState.splittedOrderUuid = curOrderUuid;
         newOrder.originalSplittedOrder = originalOrder;
 

addons/pos_restaurant/static/tests/tours/control_buttons_tour.js
@@ -7,14 +7,10 @@ import * as ProductScreenResto from "@pos_restaurant/../tests/tours/utils/produc
 const ProductScreen = { ...ProductScreenPos, ...ProductScreenResto };
 import * as SplitBillScreen from "@pos_restaurant/../tests/tours/utils/split_bill_screen_util";
 import * as Order from "@point_of_sale/../tests/tours/utils/generic_components/order_widget_util";
-import * as Chrome from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as ChromePos from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as ChromeRestaurant from "@pos_restaurant/../tests/tours/utils/chrome";
+const Chrome = { ...ChromePos, ...ChromeRestaurant };
 import { registry } from "@web/core/registry";
-
-function activeTableIs(tableNumber) {
-    return {
-        trigger: `.table-free-order-label:contains("${tableNumber}")`,
-    };
-}
 
 registry.category("web_tour.tours").add("ControlButtonsTour", {
     test: true,
@@ -24,15 +20,15 @@ registry.category("web_tour.tours").add("ControlButtonsTour", {
             Chrome.startPoS(),
             Dialog.confirm("Open Register"),
             FloorScreen.clickTable("2"),
-            activeTableIs("2"),
+            Chrome.activeTableOrOrderrIs("2"),
             ProductScreen.addOrderline("Water", "5", "2", "10.0"),
             ProductScreen.clickControlButton("Transfer"),
             FloorScreen.clickTable("4"),
-            activeTableIs("4"),
+            Chrome.activeTableOrOrderrIs("4"),
             Order.hasLine({ productName: "Water", withClass: ".selected" }),
             Chrome.clickPlanButton(),
             FloorScreen.clickTable("2"),
-            activeTableIs("2"),
+            Chrome.activeTableOrOrderrIs("2"),
             ProductScreen.orderIsEmpty(),
             Chrome.clickPlanButton(),
             FloorScreen.isShown(),

addons/pos_restaurant/static/tests/tours/split_bill_screen_tour.js
@@ -1,7 +1,9 @@
 import * as PaymentScreen from "@point_of_sale/../tests/tours/utils/payment_screen_util";
 import * as Dialog from "@point_of_sale/../tests/tours/utils/dialog_util";
 import * as ReceiptScreen from "@point_of_sale/../tests/tours/utils/receipt_screen_util";
-import * as Chrome from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as ChromePos from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as ChromeRestaurant from "@pos_restaurant/../tests/tours/utils/chrome";
+const Chrome = { ...ChromePos, ...ChromeRestaurant };
 import * as FloorScreen from "@pos_restaurant/../tests/tours/utils/floor_screen_util";
 import * as Order from "@point_of_sale/../tests/tours/utils/generic_components/order_widget_util";
 import * as ProductScreenPos from "@point_of_sale/../tests/tours/utils/product_screen_util";
@@ -42,6 +44,7 @@ registry.category("web_tour.tours").add("SplitBillScreenTour", {
 
             // click pay to split, go back to check the lines
             SplitBillScreen.clickPay(),
+            Chrome.activeTableOrOrderrIs("2B"),
             ProductScreen.clickOrderline("Water", "3.0"),
             ProductScreen.clickOrderline("Coca-Cola", "1.0"),
 
@@ -52,6 +55,29 @@ registry.category("web_tour.tours").add("SplitBillScreenTour", {
             ProductScreen.isShown(),
             ProductScreen.clickOrderline("Water", "2.0"),
             ProductScreen.clickOrderline("Minute Maid", "3.0"),
+
+            // Split the order of table 2 again
+            Chrome.clickPlanButton(),
+            FloorScreen.clickTable("2"),
+            ProductScreen.clickControlButton("Split"),
+
+            SplitBillScreen.clickOrderline("Water"),
+            SplitBillScreen.orderlineHas("Water", "2", "1"),
+            SplitBillScreen.subtotalIs("2.0"),
+            SplitBillScreen.clickOrderline("Minute Maid"),
+            SplitBillScreen.orderlineHas("Minute Maid", "3", "1"),
+            SplitBillScreen.subtotalIs("4.0"),
+
+            SplitBillScreen.clickPay(),
+            Chrome.activeTableOrOrderrIs("2C"),
+
+            // go back to the original order and see if the order is changed
+            Chrome.clickMenuOption("Orders"),
+            TicketScreen.selectOrder("-0001"),
+            TicketScreen.loadSelectedOrder(),
+            ProductScreen.isShown(),
+            ProductScreen.clickOrderline("Water", "1.0"),
+            ProductScreen.clickOrderline("Minute Maid", "2.0"),
         ].flat(),
 });
 

addons/pos_restaurant/static/tests/tours/utils/chrome.js
@@ -6,3 +6,9 @@ export function isTabActive(tabText) {
         },
     ];
 }
+
+export function activeTableOrOrderrIs(label) {
+    return {
+        trigger: `.table-free-order-label:contains("${label}")`,
+    };
+}

odoo/odoo

addons/pos_restaurant/static/src/app/split_bill_screen/split_bill_screen.js
@@ -52,14 +52,43 @@ export class SplitBillScreen extends Component {
         }
     }
 
+    _getOrderName(order) {
+        return order.table_id?.table_number.toString() || order.getFloatingOrderName() || "";
+    }
+
+    _getLatestOrderNameStartingWith(name) {
+        return (
+            this.pos
+                .get_open_orders()
+                .map((order) => this._getOrderName(order))
+                .filter((orderName) => orderName.slice(0, -1) === name)
+                .sort((a, b) => a.slice(-1).localeCompare(b.slice(-1)))
+                .at(-1) || name
+        );
+    }
+
+    _getSplitOrderName(originalOrderName) {
+        const latestOrderName = this._getLatestOrderNameStartingWith(originalOrderName);
+        if (latestOrderName === originalOrderName) {
+            return `${originalOrderName}B`;
+        }
+        const lastChar = latestOrderName[latestOrderName.length - 1];
+        if (lastChar === "Z") {
+            throw new Error("You cannot split the order into more than 26 parts!");
+        }
+        const nextChar = String.fromCharCode(lastChar.charCodeAt(0) + 1);
+        return `${latestOrderName.slice(0, -1)}${nextChar}`;
+    }
+
     createSplittedOrder() {
         const curOrderUuid = this.currentOrder.uuid;
         const originalOrder = this.pos.models["pos.order"].find((o) => o.uuid === curOrderUuid);
         this.pos.selectedTable = null;
+        const originalOrderName = this._getOrderName(originalOrder);
+        const newOrderName = this._getSplitOrderName(originalOrderName);
+
         const newOrder = this.pos.createNewOrder();
-        const originalOrderName = originalOrder.getFloatingOrderName();
-        newOrder.floating_order_name = `${newOrder.tracking_number} Split from ${originalOrderName}`;
-        newOrder.general_note = originalOrder.general_note || "";
+        newOrder.floating_order_name = newOrderName;
         newOrder.uiState.splittedOrderUuid = curOrderUuid;
         newOrder.originalSplittedOrder = originalOrder;
 

addons/pos_restaurant/static/tests/tours/control_buttons_tour.js
@@ -7,14 +7,10 @@ import * as ProductScreenResto from "@pos_restaurant/../tests/tours/utils/produc
 const ProductScreen = { ...ProductScreenPos, ...ProductScreenResto };
 import * as SplitBillScreen from "@pos_restaurant/../tests/tours/utils/split_bill_screen_util";
 import * as Order from "@point_of_sale/../tests/tours/utils/generic_components/order_widget_util";
-import * as Chrome from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as ChromePos from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as ChromeRestaurant from "@pos_restaurant/../tests/tours/utils/chrome";
+const Chrome = { ...ChromePos, ...ChromeRestaurant };
 import { registry } from "@web/core/registry";
-
-function activeTableIs(tableNumber) {
-    return {
-        trigger: `.table-free-order-label:contains("${tableNumber}")`,
-    };
-}
 
 registry.category("web_tour.tours").add("ControlButtonsTour", {
     test: true,
@@ -24,15 +20,15 @@ registry.category("web_tour.tours").add("ControlButtonsTour", {
             Chrome.startPoS(),
             Dialog.confirm("Open Register"),
             FloorScreen.clickTable("2"),
-            activeTableIs("2"),
+            Chrome.activeTableOrOrderIs("2"),
             ProductScreen.addOrderline("Water", "5", "2", "10.0"),
             Chrome.clickPlanButton(),
             FloorScreen.clickTable("4"),
-            activeTableIs("4"),
+            Chrome.activeTableOrOrderIs("4"),
             ProductScreen.addOrderline("Minute Maid", "3", "2", "6.0"),
             ProductScreen.clickControlButton("Transfer"),
             FloorScreen.clickTable("2"),
-            activeTableIs("2"),
+            Chrome.activeTableOrOrderIs("2"),
             Order.hasLine({ productName: "Water", quantity: "5" }),
             Order.hasLine({ productName: "Minute Maid", quantity: "3" }),
 

addons/pos_restaurant/static/tests/tours/split_bill_screen_tour.js
@@ -1,7 +1,9 @@
 import * as PaymentScreen from "@point_of_sale/../tests/tours/utils/payment_screen_util";
 import * as Dialog from "@point_of_sale/../tests/tours/utils/dialog_util";
 import * as ReceiptScreen from "@point_of_sale/../tests/tours/utils/receipt_screen_util";
-import * as Chrome from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as ChromePos from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as ChromeRestaurant from "@pos_restaurant/../tests/tours/utils/chrome";
+const Chrome = { ...ChromePos, ...ChromeRestaurant };
 import * as FloorScreen from "@pos_restaurant/../tests/tours/utils/floor_screen_util";
 import * as Order from "@point_of_sale/../tests/tours/utils/generic_components/order_widget_util";
 import * as ProductScreenPos from "@point_of_sale/../tests/tours/utils/product_screen_util";
@@ -42,6 +44,7 @@ registry.category("web_tour.tours").add("SplitBillScreenTour", {
 
             // click pay to split, go back to check the lines
             SplitBillScreen.clickPay(),
+            Chrome.activeTableOrOrderIs("2B"),
             ProductScreen.clickOrderline("Water", "3.0"),
             ProductScreen.clickOrderline("Coca-Cola", "1.0"),
 
@@ -52,6 +55,29 @@ registry.category("web_tour.tours").add("SplitBillScreenTour", {
             ProductScreen.isShown(),
             ProductScreen.clickOrderline("Water", "2.0"),
             ProductScreen.clickOrderline("Minute Maid", "3.0"),
+
+            // Split the order of table 2 again
+            Chrome.clickPlanButton(),
+            FloorScreen.clickTable("2"),
+            ProductScreen.clickControlButton("Split"),
+
+            SplitBillScreen.clickOrderline("Water"),
+            SplitBillScreen.orderlineHas("Water", "2", "1"),
+            SplitBillScreen.subtotalIs("2.0"),
+            SplitBillScreen.clickOrderline("Minute Maid"),
+            SplitBillScreen.orderlineHas("Minute Maid", "3", "1"),
+            SplitBillScreen.subtotalIs("4.0"),
+
+            SplitBillScreen.clickPay(),
+            Chrome.activeTableOrOrderIs("2C"),
+
+            // go back to the original order and see if the order is changed
+            Chrome.clickMenuOption("Orders"),
+            TicketScreen.selectOrder("-0001"),
+            TicketScreen.loadSelectedOrder(),
+            ProductScreen.isShown(),
+            ProductScreen.clickOrderline("Water", "1.0"),
+            ProductScreen.clickOrderline("Minute Maid", "2.0"),
         ].flat(),
 });
 

addons/pos_restaurant/static/tests/tours/utils/chrome.js
@@ -6,3 +6,9 @@ export function isTabActive(tabText) {
         },
     ];
 }
+
+export function activeTableOrOrderIs(label) {
+    return {
+        trigger: `.table-free-order-label:contains("${label}")`,
+    };
+}