113836e48addc177ec8c5a42b2f3696cc7cbde25
David Monnom (moda) <moda@odoo.com>
2024-09-24 15:16:25 +0200

    [FIX] pos_hr: fix employee cash in/out modal
    Adding condition before adding employee_id information in the cash move
    values. If pos_hr module is not installed, then employee_id will be an
    user_id instead of an employee_id
    
    closes odoo/odoo#181225
    
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>

--- addons/pos_hr/static/src/overrides/components/navbar/cash_move_popup/cash_move_popup.js
+++ addons/pos_hr/static/src/overrides/components/navbar/cash_move_popup/cash_move_popup.js
@@ -4,9 +4,10 @@ import { patch } from "@web/core/utils/patch";
 patch(CashMovePopup.prototype, {
     _prepare_try_cash_in_out_payload() {
         const result = super._prepare_try_cash_in_out_payload(...arguments);
-        const employee_id = this.pos.get_cashier().id;
-        // replace the extras with an object containing "employee_id"
-        result[result.length - 1] = { ...result[result.length - 1], employee_id };
+        if (this.pos.config.module_pos_hr) {
+            const employee_id = this.pos.get_cashier().id;
+            result[result.length - 1] = { ...result[result.length - 1], employee_id };
+        }
         return result;
     },
 });
