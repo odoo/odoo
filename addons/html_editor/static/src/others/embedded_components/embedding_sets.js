1fb3ba5bd7c550e5bae7643fe85edc9b7245b739
abd-msyukyu-odoo <abd@odoo.com>
2024-09-24 07:35:50 +0000

    [IMP] html_editor: add the excalidraw embedded component
    This commits moves the `draw_behavior` from knowledge to html_editor.
    It is a simple component with an iframe containing an excalidraw URL.
    It brings the ability to display interactive drawing boards in any HtmlField
    (i.e. a task mockup) so that the user does not need to leave odoo to consult
    them.
    
    task-3672747
    
    X-original-commit: cd760f40f0268746e70079eb9dad305cd41410d3
    Part-of: odoo/odoo#182145
    Related: odoo/enterprise#71082
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>
    Co-authored-by: Damien Abeloos <abd@odoo.com>
    Co-authored-by: Adrien Schoffeniels <adsc@odoo.com>
    Co-authored-by: Julien Banken <jbn@odoo.com>
    Co-authored-by: Thomas Josse <thjo@odoo.com>

--- addons/html_editor/static/src/others/embedded_components/embedding_sets.js
+++ addons/html_editor/static/src/others/embedded_components/embedding_sets.js
@@ -1,3 +1,10 @@
-export const MAIN_EMBEDDINGS = [];
+import { excalidrawEmbedding } from "@html_editor/others/embedded_components/backend/excalidraw/excalidraw";
+import { readonlyExcalidrawEmbedding } from "@html_editor/others/embedded_components/core/excalidraw/readonly_excalidraw";
 
-export const READONLY_MAIN_EMBEDDINGS = [];
+export const MAIN_EMBEDDINGS = [
+    excalidrawEmbedding,
+];
+
+export const READONLY_MAIN_EMBEDDINGS = [
+    readonlyExcalidrawEmbedding,
+];
