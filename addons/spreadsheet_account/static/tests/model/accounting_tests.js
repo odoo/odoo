67d2da67fa066f89faed149fce3e011365ed190c
Rémi Rahir (rar) <rar@odoo.com>
2024-09-24 14:44:37 +0000

    [FIX] spreadsheet_account: Fix ODOO.BALANCE offset computation
    The computation of ODOO.BALANCE with year offsets was wrong as we would
    apply the offset once to query the debit data and TWICE when querying
    the credit data, making the computation of the delta completely
    meaningless.
    
    Task-4207414
    
    X-original-commit: f228e01c5b79895aedf4c069be427c3ec797afb1

--- addons/spreadsheet_account/static/tests/model/accounting_tests.js
+++ addons/spreadsheet_account/static/tests/model/accounting_tests.js
@@ -188,6 +188,7 @@ QUnit.module("spreadsheet_account > Accounting", { beforeEach }, () => {
         setCellContent(model, "A7", `=ODOO.DEBIT("5", "05/04/2021", 1)`);
         setCellContent(model, "A8", `=ODOO.BALANCE("5", "2022",,,FALSE)`);
         setCellContent(model, "A9", `=ODOO.BALANCE("100", "05/05/2022",,,TRUE)`);
+        setCellContent(model, "A10", `=ODOO.BALANCE(33,2021,-2)`);
         await waitForDataLoaded(model);
 
         assert.verifySteps([
@@ -253,6 +254,14 @@ QUnit.module("spreadsheet_account > Accounting", { beforeEach }, () => {
                     codes: ["100"],
                     companyId: null,
                     includeUnposted: true,
+                })
+            ),
+            JSON.stringify(
+                camelToSnakeObject({
+                    dateRange: parseAccountingDate({value: "2019" }, locale),
+                    codes: ["33"],
+                    companyId: null,
+                    includeUnposted: false,
                 })
             ),
         ]);
