74321165c3ac085bd474a89ffa4abba73b8fd818
lase@odoo.com <lase@odoo.com>
2024-09-24 12:33:14 +0000

    [FIX] mrp: not unreserve twice during mass production
    Steps to reproduce:
    
    - Enable Multi-Step Routes
    - Inventory > Configuration > Warehouse Management > Warehouses
    - Change your warehouse settings to Manufacturing in 2 steps
    - Create a product P tracked by serial number with a BOM:
        - 1 x COMP (a storable product with 10 units in stock)
    - Create and confirm an MO for 10 units of P
    - Validate the delivery from stock to pre-production
    - Click on `Mass Produce` > `Generate` only 7 SN
    - Click No back order (split and generate only 7 MO's)
    > Looking at the quant of the product you see that 0 units are reserved
    in pre-production but 7 should be
    - validate one of the 7 MO
    > Looking at the quant of the product you see that -1 units are reserved
    in pre-production but 6 should be
    
    Cause of the issue:
    
    By processing the "Mass produce" wizzard you will call the
    `_assign_serial_numbers` method and hence the `split_productions`:
    https://github.com/odoo/odoo/blob/af4055e2ccce399f94bc465e2dd529c9f14953e0/addons/mrp/wizard/stock_assign_serial_numbers.py#L65-L68
    During this `split_productions` call, the quant reserved quantity will
    be updated by these lines:
    https://github.com/odoo/odoo/blob/af4055e2ccce399f94bc465e2dd529c9f14953e0/addons/mrp/models/mrp_production.py#L1931-L1938
    However, since the stock pocalypse, the quant is already updated once
    you write and change the quantity of the stock move line of the raw move
    of the inital MO because of these lines:
    https://github.com/odoo/odoo/blob/af4055e2ccce399f94bc465e2dd529c9f14953e0/addons/stock/models/stock_move_line.py#L437-L439
    Hence, the stock.quant reserved quantity will end up to be updated twice
    
    opw-4145680
    
    closes odoo/odoo#181864
    
    X-original-commit: d8768c5284a2d746ca6f2d7291913699882d5bc7
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -1954,15 +1954,6 @@ class MrpProduction(models.Model):
                         move = moves and moves.pop(0)
                         move_qty_to_reserve = move and move.product_qty or 0
 
-                # Unreserve the quantity removed from initial `stock.move.line` and
-                # not assigned to a move anymore. In case of a split smaller than initial
-                # quantity and fully reserved
-                if quantity and not move_line.move_id._should_bypass_reservation():
-                    self.env['stock.quant']._update_reserved_quantity(
-                        move_line.product_id, move_line.location_id, -quantity,
-                        lot_id=move_line.lot_id, package_id=move_line.package_id,
-                        owner_id=move_line.owner_id, strict=True)
-
             if move and move_qty_to_reserve != move.product_qty:
                 partially_assigned_moves.add(move.id)
 
