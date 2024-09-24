55b324331264b6d4e3b9788df723a2d3761b964c
william-andre <wan@odoo.com>
2024-09-24 13:12:59 +0200

    [FIX] account: audit_trail activated without any companies with it
    To reproduce:
    Go to Accounting/Invoicing, Customers and create a new Customer.
    Delete it.
    You get an error talking about audit trail while you don't have
    it activated.
    
    The issue is that we block based on customer/supplier rank when
    there is no company on the partner.
    
    We now check if we should block the deletion of a message of a
    partner if this partner has at least one journal entry with a
    company that has audit trail activated.

--- addons/account_audit_trail/models/mail_message.py
+++ addons/account_audit_trail/models/mail_message.py
@@ -175,7 +175,19 @@ class Message(models.Model):
     def _except_audit_log(self):
         if self.env.context.get('bypass_audit') is bypass_token:
             return
-        for message in self:
+        to_check = self
+        partner_message = self.filtered(lambda m: m.account_audit_log_partner_id)
+        if partner_message:
+            # The audit trail uses the cheaper check on `customer_rank`, but that field could be set
+            # without actually having an invoice linked (i.e. creation of the contact through the
+            # Invoicing/Customers menu)
+            has_related_move = self.env['account.move'].sudo().search_count([
+                ('partner_id', 'in', partner_message.account_audit_log_partner_id.ids),
+                ('company_id.check_account_audit_trail', '=', True),
+            ], limit=1)
+            if not has_related_move:
+                to_check -= partner_message
+        for message in to_check:
             if message.show_audit_log and not (
                 message.account_audit_log_move_id
                 and not message.account_audit_log_move_id.posted_before
