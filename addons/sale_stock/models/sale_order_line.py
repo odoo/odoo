8a148e85d7a89f54a670e27cecce64490c91224b
clesgow <quwo@odoo.com>
2024-09-25 10:20:03 +0000

    [FIX] {sale_,}stock: see delivered quantities for interco sales
    Currently, we only check delivered quantities in a Sale Order based on
    the `usage` of the destination location of the related delivery. This
    means that in the case of Inter-company transactions, we won't consider
    them as deliveries, as its delivery location will be 'Inter-Company
    Transit', which itself is a 'transit' location.
    
    Task-4207132
    
    closes odoo/odoo#182046
    
    X-original-commit: 444dfdfe2eae3bf3ecfba524af22f9ad5297ef24
    Related: odoo/enterprise#71024
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/sale_stock/models/sale_order_line.py
+++ addons/sale_stock/models/sale_order_line.py
@@ -311,11 +311,11 @@ class SaleOrderLine(models.Model):
             moves = moves.filtered(lambda r: fields.Date.context_today(r, r.date) <= self._context['accrual_entry_date'])
 
         for move in moves:
-            if (strict and move.location_dest_id.usage == "customer") or \
-               (not strict and move.rule_id.id in triggering_rule_ids and (move.location_final_id or move.location_dest_id).usage == "customer"):
+            if (strict and move.location_dest_id._is_outgoing()) or \
+               (not strict and move.rule_id.id in triggering_rule_ids and (move.location_final_id or move.location_dest_id)._is_outgoing()):
                 if not move.origin_returned_move_id or (move.origin_returned_move_id and move.to_refund):
                     outgoing_moves |= move
-            elif move.location_id.usage == "customer" and move.to_refund:
+            elif move.location_id._is_outgoing() and move.to_refund:
                 incoming_moves |= move
 
         return outgoing_moves, incoming_moves
