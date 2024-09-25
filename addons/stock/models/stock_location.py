3e1a6191729304d1fd2acfc61d8dd62e080df061
temo-odoo <temo@odoo.com>
2024-09-25 12:58:53 +0530

    [IMP] stock: simplify location form
    In this commit:
    ====================
    - The current location form includes fields that may confuse users, especially
      with virtual location types. By removing non-essential field like
      'dock location' from various location types, we reduce complexity and make the
      form more intuitive.
    - Additionally, 'Is a Subcontracting location' is moved to the normal mode to
      increase visibility for users, as it is a commonly used field.
    - The tooltips have been corrected and clarified to improve user understanding.
    
    task- 4149824

--- addons/stock/models/stock_location.py
+++ addons/stock/models/stock_location.py
@@ -79,9 +79,10 @@ class Location(models.Model):
              "and a fallback is made on the parent locations if none is set here.\n\n"
              "FIFO: products/lots that were stocked first will be moved out first.\n"
              "LIFO: products/lots that were stocked last will be moved out first.\n"
-             "Closet location: products/lots closest to the target location will be moved out first.\n"
+             "Closest Location: products/lots closest to the target location will be moved out first.\n"
              "FEFO: products/lots with the closest removal date will be moved out first "
-             "(the availability of this method depends on the \"Expiration Dates\" setting).")
+             "(the availability of this method depends on the \"Expiration Dates\" setting).\n"
+             "Least Packages: products/lots that were stocked in package with least amount of qty will be moved out first")
     putaway_rule_ids = fields.One2many('stock.putaway.rule', 'location_in_id', 'Putaway Rules')
     barcode = fields.Char('Barcode', copy=False)
     quant_ids = fields.One2many('stock.quant', 'location_id')
