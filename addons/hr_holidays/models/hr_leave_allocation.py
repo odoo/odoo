2d077a47e54b9455b731e3c9cee15c6d705aa3ab
akha <akha@odoo.com>
2024-09-25 16:17:59 +0300

    [FIX] hr_holidays: Update employee's time off balance expiry computation.
    1. In the Time Off app dashboard, employees can view
    whether some of their unused time off days are set
    to expire and the exact expiration date.
    
    2. At the accrual plan level, a validity period can
    be defined for carried-over days, after which they expire.
    However, the expiration date of these carried-over
    days hasn't been considered when calculating the
    employee's expiring balance.
    
    Steps to reproduce:
    1. Create a new accrual plan.
    2. Create a new accrual plan level.
    3. Set the level to accrue 10 days yearly.
    4. Set carryover validity to 2 months.
    5. Leave other options as is.
    6. Create a new allocation.
    7. Use the accrual plan defined above.
    8. Set the start date as 01/01/20xx
    where xx is the previous year.
    9. The employee's balance is now 10 days.
    10. Go to the dashboard.
    11. Set the date to 01/01/ (20xx + 2)
    which is a carryover date.
    12. No expiration date is shown on the
    dashboard.
    13. The expiration date should be set
    to 01/03/(20xx + 2).
    
    This update addresses the issue by incorporating
    the expiration dates of carried-over days into the
    calculation.
    
    task-4207987
    
    closes odoo/odoo#181502
    
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/hr_holidays/models/hr_leave_allocation.py
+++ addons/hr_holidays/models/hr_leave_allocation.py
@@ -6,7 +6,6 @@ from datetime import datetime, date, time
 from dateutil.relativedelta import relativedelta
 
 from odoo import api, fields, models, _
-from odoo.addons.resource.models.utils import HOURS_PER_DAY
 from odoo.addons.hr_holidays.models.hr_leave import get_employee_from_context
 from odoo.exceptions import AccessError, UserError, ValidationError
 from odoo.tools.float_utils import float_round
