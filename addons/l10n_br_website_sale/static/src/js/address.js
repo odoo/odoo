08e1857b36cdcc347d014ac58f2908bf7960258b
Joren Van Onder <jov@odoo.com>
2024-09-25 09:55:46 -0700

    [IMP] l10n_br_website_sale: select cities with SelectMenu
    Customers have to select a res.city in eCommerce for EDI. This was
    implemented previously [1]. An auto-completion mechanism was
    implemented to select city and state based on the zip, because there
    are many cities in the city <select> element.
    
    This improves the selection further by making the cities searchable
    with the SelectMenu widget. It's especially useful for customers who
    may try to select the city before entering the zip.
    
    [1] https://github.com/odoo/odoo/pull/177124
    
    task-4198405
    
    closes odoo/odoo#183439
    
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/l10n_br_website_sale/static/src/js/address.js
+++ addons/l10n_br_website_sale/static/src/js/address.js
@@ -1,40 +1,88 @@
-import websiteSaleAddress from '@website_sale/js/address';
+/** @odoo-module **/
+
+import websiteSaleAddress from "@website_sale/js/address";
+import { Component, useState } from "@odoo/owl";
+import { SelectMenu } from "@web/core/select_menu/select_menu";
+import { attachComponent } from "@web_editor/js/core/owl_utils";
+
+class SelectMenuWrapper extends Component {
+    static template = "l10n_br_website_sale.SelectMenuWrapper";
+    static components = { SelectMenu };
+    static props = {
+        el: { optional: true, type: Object },
+    };
+
+    setup() {
+        this.state = useState({
+            choices: [],
+            value: this.props.el.value,
+        });
+        this.state.choices = [...this.props.el.querySelectorAll("option")].filter((x) => x.value);
+        this.props.el.classList.add("d-none");
+    }
+
+    onSelect(value) {
+        this.state.value = value;
+        this.props.el.value = value;
+        // Manually trigger the change event
+        const event = new Event("change", { bubbles: true });
+        this.props.el.dispatchEvent(event);
+    }
+}
 
 websiteSaleAddress.include({
-    events: Object.assign(
-        {},
-        websiteSaleAddress.prototype.events,
-        {
-            'input input[name="zip"]': '_onChangeZip',
-        }
-    ),
+    events: Object.assign({}, websiteSaleAddress.prototype.events, {
+        'input input[name="zip"]': "_onChangeZip",
+        "change .o_select_city": "_onChangeBrazilianCity",
+    }),
 
-    _selectState: function(id) {
-        this.addressForm.querySelector(`select[name="state_id"] > option[value="${id}"]`).selected = 'selected';
+    start: async function () {
+        this._super.apply(this, arguments);
+
+        if (this.countryCode === "BR") {
+            const selectEl = this.el.querySelector("select[name='city_id']");
+            this.selectMenuWrapper = await attachComponent(
+                this,
+                selectEl.parentElement,
+                SelectMenuWrapper,
+                {
+                    el: selectEl,
+                }
+            );
+            await this._changeCountry();
+        }
     },
 
-    _onChangeZip: function() {
-        if (this.countryCode !== 'BR') {
+    _selectState: function (id) {
+        this.addressForm.querySelector(`select[name="state_id"] > option[value="${id}"]`).selected =
+            "selected";
+    },
+
+    _onChangeZip: function () {
+        if (this.countryCode !== "BR") {
             return;
         }
 
-        const newZip = this.addressForm.zip.value.padEnd(5, '0');
+        const newZip = this.addressForm.zip.value.padEnd(5, "0");
 
-        for (let option of this.addressForm.querySelectorAll('select[name="city_id"]:not(.d-none) > option')) {
-            const ranges = option.getAttribute('zip-ranges');
+        for (const option of this.addressForm.querySelectorAll(".o_select_city option")) {
+            const ranges = option.getAttribute("zip-ranges");
             if (ranges) {
                 // Parse the l10n_br_zip_ranges field (e.g. "[01000-001 05999-999] [08000-000 08499-999]").
                 // Loop over each range that is enclosed in [] (e.g. "[01000-001 05999-999]" followed by "[08000-000 08499-999]").
-                for (let range of ranges.matchAll(/\[[^\[]+\]/g)) {
+                for (let range of ranges.matchAll(/\[[^[]+]/g)) {
                     // Remove square brackets (after this, range is e.g. "01000-001 05999-999")
-                    range = range[0].replace(/[\[\]]/g, '');
+                    range = range[0].replace(/[[\]]/g, "");
 
-                    let [start, end] = range.split(' ');
+                    const [start, end] = range.split(" ");
 
                     // Rely on lexicographical order to figure out if the new zip is in this range.
                     if (newZip >= start && newZip <= end) {
-                        option.selected = 'selected';
-                        this._selectState(option.getAttribute('state-id'));
+                        if (this.selectMenuWrapper) {
+                            this.selectMenuWrapper.component.onSelect(option.value);
+                        }
+                        option.selected = "selected";
+                        this._selectState(option.getAttribute("state-id"));
                         return;
                     }
                 }
@@ -42,42 +90,58 @@ websiteSaleAddress.include({
         }
     },
 
+    _onChangeBrazilianCity: function () {
+        if (this.countryCode !== "BR") {
+            return;
+        }
+
+        if (this.addressForm.city_id.value) {
+            this._selectState(
+                this.addressForm.city_id
+                    .querySelector(`option[value='${this.addressForm.city_id.value}']`)
+                    .getAttribute("state-id")
+            );
+        }
+    },
+
     _setVisibility(selector, should_show) {
-        this.addressForm.querySelectorAll(selector).forEach(el => {
+        this.addressForm.querySelectorAll(selector).forEach((el) => {
             if (should_show) {
-                el.classList.remove('d-none');
+                el.classList.remove("d-none");
             } else {
-                el.classList.add('d-none');
+                el.classList.add("d-none");
             }
 
             // Disable hidden inputs to avoid sending back e.g. an empty street when street_name and street_number is
             // filled. It causes street_name and street_number to be lost.
-            if (el.tagName === 'INPUT') {
+            if (el.tagName === "INPUT") {
                 el.disabled = !should_show;
             }
 
-            el.querySelectorAll('input').forEach(input => input.disabled = !should_show);
-        })
+            el.querySelectorAll("input").forEach((input) => (input.disabled = !should_show));
+        });
     },
 
     async _changeCountry(ev) {
         const res = await this._super(...arguments);
-        if (this.countryCode !== 'BR') {
+        if (this.countryCode !== "BR") {
             return res;
         }
 
         const countryOption = this.addressForm.country_id;
-        const selectedCountryCode = countryOption.value ? countryOption.selectedOptions[0].getAttribute('code') : '';
+        const selectedCountryCode = countryOption.value
+            ? countryOption.selectedOptions[0].getAttribute("code")
+            : "";
 
-        if (selectedCountryCode === 'BR') {
-            this._setVisibility('.o_standard_address', false); // hide
-            this._setVisibility('.o_extended_address', true); // show
+        if (selectedCountryCode === "BR") {
+            this._setVisibility(".o_standard_address", false); // hide
+            this._setVisibility(".o_extended_address", true); // show
             this._onChangeZip();
         } else {
-            this._setVisibility('.o_standard_address', true); // show
-            this._setVisibility('.o_extended_address', false); // hide
+            this._setVisibility(".o_standard_address", true); // show
+            this._setVisibility(".o_extended_address", false); // hide
         }
 
         return res;
-    }
+    },
 });
