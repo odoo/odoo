c3b52e45e8e0e707d64a3f26eea1ec52d343893b
Serge Bayet (seba) <seba@odoo.com>
2024-09-25 14:06:24 +0200

    [FIX] website: handle language variants in SEO suggestions API request
    Steps to reproduce:
    
    - Install serbian (latin) language
    - Add a keyword in serbian via SEO optimize menu
    - Traceback occurs
    
    This fix ensures that language variants, such as "sr@latin", are
    properly handled when making requests to the Google Autocomplete API for
    SEO suggestions.
    A test has been added.
    
    task-4210707
    
    closes odoo/odoo#181400
    
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -664,7 +664,38 @@ class Website(Home):
 
     @http.route(['/website/seo_suggest'], type='json', auth="user", website=True)
     def seo_suggest(self, keywords=None, lang=None):
-        language = lang.split("_")
+        """
+        Suggests search keywords based on a given input using Google's
+        autocomplete API.
+
+        This method takes in a `keywords` string and an optional `lang`
+        parameter that defines the language and geographical region for
+        tailoring search suggestions. It sends a request to Google's
+        autocomplete service and returns the search suggestions in JSON format.
+
+        :param str keywords: the keyword string for which suggestions
+            are needed.
+        :param str lang: a string representing the language and geographical
+            location, formatted as:
+            - `language_territory@modifier`, where:
+                - `language`: 2-letter ISO language code (e.g., "en" for
+                  English).
+                - `territory`: Optional, 2-letter country code (e.g., "US" for
+                  United States).
+                - `modifier`: Optional, generally script variant (e.g.,
+                  "latin").
+            If `lang` is not provided or does not match the expected format, the
+            default language is set to English (`en`) and the territory to the
+            United States (`US`).
+
+        :returns: JSON list of strings
+            A list of suggested keywords returned by Google's autocomplete
+            service. If no suggestions are found or if there's an error (e.g.,
+            connection issues), an empty list is returned.
+        """
+        pattern = r'^([a-zA-Z]+)(?:_(\w+))?(?:@(\w+))?$'
+        match = re.match(pattern, lang)
+        language = [match.group(1), match.group(2) or ''] if match else ['en', 'US']
         url = "http://google.com/complete/search"
         try:
             req = requests.get(url, params={
